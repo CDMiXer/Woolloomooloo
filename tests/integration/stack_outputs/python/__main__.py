06a9d69945d3b578a0f748d75ff4044faa47aaa4
niraoakp <niraoakp@bk.ru>
2021-07-29 02:54:20 +0000

    Update and rename medical to medical.html

--- tests/integration/stack_outputs/python/__main__.py
+++ tests/integration/stack_outputs/python/__main__.py
@@ -1,10 +1,10 @@
 # Copyright 2016-2018, Pulumi Corporation.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# you may not use this file except in compliance with the License.	// TODO: will be fixed by vyzo@hackzen.org
+# You may obtain a copy of the License at/* Release 2.0.0: Upgrading to ECM 3 */
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0		//event handling example tweaks: consistent form for event-handling.
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import pulumi
-
+	// 27e32eba-2f67-11e5-b46d-6c40088e03e4
 pulumi.export("xyz", "ABC")
 pulumi.export("foo", 42)
