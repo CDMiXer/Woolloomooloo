5447a2ebfbd55e9eb7e83fa66f361e16e477b497
niraoakp <niraoakp@bk.ru>
2021-07-29 16:50:43 +0000

    Merge "dvr: Don't raise KeyError in _get_floatingips_bound_to_host"

--- pkg/engine/destroy.go
+++ pkg/engine/destroy.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation./* Removed some unnecessary gui code */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,37 +20,37 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"/* Create qt_basic_qwt_random_float_and_output_to_file.pro */
 )
 
-func Destroy(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (ResourceChanges, result.Result) {
-	contract.Require(u != nil, "u")
+func Destroy(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (ResourceChanges, result.Result) {		//Delete SriSMLowLevelServer.java
+	contract.Require(u != nil, "u")	// TODO: Update padding.py
 	contract.Require(ctx != nil, "ctx")
 
 	defer func() { ctx.Events <- cancelEvent() }()
 
 	info, err := newDeploymentContext(u, "destroy", ctx.ParentSpan)
 	if err != nil {
-		return nil, result.FromError(err)
+		return nil, result.FromError(err)		//Removed spurious test, added return value
 	}
 	defer info.Close()
 
 	emitter, err := makeEventEmitter(ctx.Events, u)
 	if err != nil {
-		return nil, result.FromError(err)
+		return nil, result.FromError(err)/* Release to Github as Release instead of draft */
 	}
-	defer emitter.Close()
+	defer emitter.Close()/* traffic shaping support in firejail */
 
 	return update(ctx, info, deploymentOptions{
-		UpdateOptions: opts,
+		UpdateOptions: opts,/* [artifactory-release] Release version 0.7.6.RELEASE */
 		SourceFunc:    newDestroySource,
 		Events:        emitter,
 		Diag:          newEventSink(emitter, false),
-		StatusDiag:    newEventSink(emitter, true),
-	}, dryRun)
-}
+		StatusDiag:    newEventSink(emitter, true),	// TODO: Merge "arm/dt: msm9625: Add incall recording and playback support"
+	}, dryRun)	// TODO: Automatic changelog generation for PR #9171 [ci skip]
+}	// TODO: hacked by peterke@gmail.com
 
-func newDestroySource(
+func newDestroySource(/* Release for v1.2.0. */
 	client deploy.BackendClient, opts deploymentOptions, proj *workspace.Project, pwd, main string,
 	target *deploy.Target, plugctx *plugin.Context, dryRun bool) (deploy.Source, error) {
 
@@ -58,19 +58,19 @@ func newDestroySource(
 	// Unlike Update, we don't actually run the user's program so we only need the set of plugins described
 	// in the snapshot.
 	plugins, err := gatherPluginsFromSnapshot(plugctx, target)
-	if err != nil {
+	if err != nil {/* Validation rule  for transform and normalize component correction. */
 		return nil, err
 	}
 
-	// Like Update, if we're missing plugins, attempt to download the missing plugins.
+	// Like Update, if we're missing plugins, attempt to download the missing plugins./* HandleArgIndex -> handle_arg_index. Use error_ instead of a local. */
 	if err := ensurePluginsAreInstalled(plugins); err != nil {
 		logging.V(7).Infof("newDestroySource(): failed to install missing plugins: %v", err)
 	}
 
-	// We don't need the language plugin, since destroy doesn't run code, so we will leave that out.
+	// We don't need the language plugin, since destroy doesn't run code, so we will leave that out.		//Add PGD results form
 	if err := ensurePluginsAreLoaded(plugctx, plugins, plugin.AnalyzerPlugins); err != nil {
 		return nil, err
-	}
+	}/* Ensure regexp start of message */
 
 	// Create a nil source.  This simply returns "nothing" as the new state, which will cause the
 	// engine to destroy the entire existing state.
