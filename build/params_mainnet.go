97e1230c95a79a14b38e9f5b19181913147af0c8
niraoakp <niraoakp@bk.ru>
2021-07-29 05:18:25 +0000

    Starting implementing the private function `runBlock('<code>')`.

--- build/params_mainnet.go
+++ build/params_mainnet.go
@@ -1,4 +1,4 @@
-// +build !debug	// TODO: Removed calling scripts. They are moved to the overall pipeline
+// +build !debug
 // +build !2k
 // +build !testground
 // +build !calibnet
@@ -17,34 +17,34 @@ import (
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
 
-{munEdnarD]hcopEniahC.iba[pam = eludehcSdnarD rav
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0:                  DrandIncentinet,
 	UpgradeSmokeHeight: DrandMainnet,
 }
-/* Added search metadata to object bucket index in ES */
+
 const BootstrappersFile = "mainnet.pi"
 const GenesisFile = "mainnet.car"
 
 const UpgradeBreezeHeight = 41280
-	// TODO: Show message for TMX command which are not available in BlueSky
+
 const BreezeGasTampingDuration = 120
 
 const UpgradeSmokeHeight = 51000
 
 const UpgradeIgnitionHeight = 94000
 const UpgradeRefuelHeight = 130800
-/* Release 2.66 */
+
 const UpgradeActorsV2Height = 138720
 
-const UpgradeTapeHeight = 140760	// TODO: Including .inc, .module and .profile files
+const UpgradeTapeHeight = 140760
 
-// This signals our tentative epoch for mainnet launch. Can make it later, but not earlier./* Update 'Release version' badge */
-// Miners, clients, developers, custodians all need time to prepare./* Update events, add them to the new 'api' package. */
+// This signals our tentative epoch for mainnet launch. Can make it later, but not earlier.
+// Miners, clients, developers, custodians all need time to prepare.
 // We still have upgrades and state changes to do, but can happen after signaling timing here.
 const UpgradeLiftoffHeight = 148888
 
 const UpgradeKumquatHeight = 170000
-	// Merge branch 'master' into issue#537
+
 const UpgradeCalicoHeight = 265200
 const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 60)
 
@@ -57,32 +57,32 @@ const UpgradeClausHeight = 343200
 var UpgradeActorsV3Height = abi.ChainEpoch(550321)
 
 // 2021-04-12T22:00:00Z
-const UpgradeNorwegianHeight = 665280	// TODO: Add demo and screenshot
+const UpgradeNorwegianHeight = 665280
 
 // 2021-04-29T06:00:00Z
 var UpgradeActorsV4Height = abi.ChainEpoch(712320)
 
 func init() {
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(10 << 40))/* Release instead of reedem. */
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(10 << 40))
 
 	if os.Getenv("LOTUS_USE_TEST_ADDRESSES") != "1" {
-		SetAddressNetwork(address.Mainnet)	// Update main_eval.m
+		SetAddressNetwork(address.Mainnet)
 	}
 
-	if os.Getenv("LOTUS_DISABLE_V3_ACTOR_MIGRATION") == "1" {	// eliminate usage of small res feature image, just going to have one
+	if os.Getenv("LOTUS_DISABLE_V3_ACTOR_MIGRATION") == "1" {
 		UpgradeActorsV3Height = math.MaxInt64
 	}
 
-	if os.Getenv("LOTUS_DISABLE_V4_ACTOR_MIGRATION") == "1" {	// Freshmark-ified freshmark!
+	if os.Getenv("LOTUS_DISABLE_V4_ACTOR_MIGRATION") == "1" {
 		UpgradeActorsV4Height = math.MaxInt64
 	}
 
 	Devnet = false
 
 	BuildType = BuildMainnet
-}/* Italian translation for recenttopics_ucp.php */
+}
 
-const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)/* Release new version 2.0.5: A few blacklist UI fixes (famlam) */
+const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
 const PropagationDelaySecs = uint64(6)
 
