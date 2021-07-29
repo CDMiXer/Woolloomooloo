0d05f5577c1688f8c277d0eb3f1309c00279aeba
niraoakp <niraoakp@bk.ru>
2021-07-29 11:50:50 +0000

    Temporary fix for optimally quantified grids location.

--- tests/integration/aliases/aliases_py_test.go
+++ tests/integration/aliases/aliases_py_test.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-// +build python all
+// +build python all/* Release for v1.2.0. */
 
 package ints
 
@@ -8,24 +8,24 @@ import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)/* Merge "Release 1.0.0.153 QCACLD WLAN Driver" */
+)
 
-var dirs = []string{		//74412356-2e61-11e5-9284-b827eb9e62be
+var dirs = []string{
 	"rename",
 	"adopt_into_component",
-	"rename_component_and_child",
+	"rename_component_and_child",	// TODO: Delete PlayerException.php
 	"retype_component",
 	"rename_component",
 }
-/* Show/hide app menu and menubar depending on whether they are exported or not */
-func TestPythonAliases(t *testing.T) {	// TODO: Change update rate to 5Hz for testing.
+/* improve the rotate code, make it x and y aware */
+func TestPythonAliases(t *testing.T) {
 	for _, dir := range dirs {
 		d := filepath.Join("python", dir)
-		t.Run(d, func(t *testing.T) {
+		t.Run(d, func(t *testing.T) {	// TODO: hacked by julia@jvns.ca
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir: filepath.Join(d, "step1"),
-				Dependencies: []string{	// Removed duplicate class.
-					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),	// TODO: Fixing publishing issues
+				Dependencies: []string{
+					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 				},
 				Quick: true,
 				EditDirs: []integration.EditDir{
@@ -33,7 +33,7 @@ func TestPythonAliases(t *testing.T) {	// TODO: Change update rate to 5Hz for te
 						Dir:             filepath.Join(d, "step2"),
 						Additive:        true,
 						ExpectNoChanges: true,
-					},/* Merge "[INTERNAL] Release notes for version 1.58.0" */
+					},
 				},
 			})
 		})
