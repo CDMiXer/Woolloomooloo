67e4e9798513836c7481e5c44b0e6106844ba2db
niraoakp <niraoakp@bk.ru>
2021-07-29 07:25:23 +0000

    Use instrumentStaticModule for $cacheFactory instrumentation

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -1,20 +1,20 @@
 package full
-	// TODO: hacked by peterke@gmail.com
+
 import (
-	"testing"	// TODO: will be fixed by ng8eke@163.com
+	"testing"
 
 	"github.com/stretchr/testify/require"
-		//Readme initial
+
 	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Merge "diag: Release mutex in corner case" into msm-3.0 */
+
 func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
-	}, 1))/* Resolve 117.  */
+	}, 1))
 
 	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
@@ -24,17 +24,17 @@ func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
-	}, 1))/* updated install requirments. */
+	}, 1))
 
-	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{	// TODO: hacked by lexy8russo@outlook.com
+	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
-	}, 1))		//Missing model folder
+	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},/* CWS-TOOLING: integrate CWS cli003 */
+		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 2))
 }
