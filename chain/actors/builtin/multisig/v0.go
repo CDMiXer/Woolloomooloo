338a1dbc1e17d25ca86c7e619a5cba45e34a570d
niraoakp <niraoakp@bk.ru>
2021-07-29 17:29:17 +0000

    · Menu zPATTERNS modificat per afegir-hi noves opcions

--- chain/actors/builtin/multisig/v0.go
+++ chain/actors/builtin/multisig/v0.go
@@ -2,33 +2,33 @@ package multisig
 
 import (
 	"bytes"
-	"encoding/binary"
+	"encoding/binary"		//#1902 addded delete all instruments of project function
 
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* Merge "Remove misplaced … ? isset( … ) : … in TemplateHelper" */
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-
+/* Update 12g */
 	msig0 "github.com/filecoin-project/specs-actors/actors/builtin/multisig"
 )
-
+	// TODO: hacked by ng8eke@163.com
 var _ State = (*state0)(nil)
-
+/* slide-images */
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {
-		return nil, err
-	}
+	if err != nil {/* add lots of error checking by GThomas */
+		return nil, err/* Release script stub */
+	}		//Create 29.4 Using H2’s web console.md
 	return &out, nil
 }
 
-type state0 struct {
+type state0 struct {		//Default bookmarks changed
 	msig0.State
 	store adt.Store
 }
@@ -41,13 +41,13 @@ func (s *state0) StartEpoch() (abi.ChainEpoch, error) {
 	return s.State.StartEpoch, nil
 }
 
-func (s *state0) UnlockDuration() (abi.ChainEpoch, error) {
+func (s *state0) UnlockDuration() (abi.ChainEpoch, error) {		//Changed to throw IOException
 	return s.State.UnlockDuration, nil
 }
 
 func (s *state0) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
-}
+}	// TODO: hacked by brosner@gmail.com
 
 func (s *state0) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
@@ -65,19 +65,19 @@ func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) err
 	var out msig0.Transaction
 	return arr.ForEach(&out, func(key string) error {
 		txid, n := binary.Varint([]byte(key))
-		if n <= 0 {
+		if n <= 0 {	// TODO: c223aba2-2e48-11e5-9284-b827eb9e62be
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
 		}
 		return cb(txid, (Transaction)(out)) //nolint:unconvert
-	})
+	})	// TODO: Fixed bug that prevented update.php to happen correctly.
 }
 
 func (s *state0) PendingTxnChanged(other State) (bool, error) {
 	other0, ok := other.(*state0)
-	if !ok {
+	if !ok {/* Release 12.9.5.0 */
 		// treat an upgrade as a change, always
 		return true, nil
-	}
+	}		//Cleanup when plugin is deactivated.
 	return !s.State.PendingTxns.Equals(other0.PendingTxns), nil
 }
 
