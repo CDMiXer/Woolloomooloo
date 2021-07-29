645d1e7a399ed1d7e1f7519c35edb1b0b9eb731b
niraoakp <niraoakp@bk.ru>
2021-07-29 04:41:11 +0000

    Allow retrieving data from the JDI object reified ProducedType

--- hack/parse_examples.go
+++ hack/parse_examples.go
@@ -1,42 +1,42 @@
-package main/* [PAXWEB-348] - Upgrade to pax-exam 2.4.0.RC1 or RC2 or Release */
+package main
 
 import (
 	"io/ioutil"
 	"regexp"
-)/* changes to !add/saymeme and imdb */
+)
 
 const (
-	newHeader = `<summary>Examples with this field (click to open)</summary>		//Fix basic header size
+	newHeader = `<summary>Examples with this field (click to open)</summary>
 <br>
 <ul>`
 	newHeaderAlt = `<summary>Examples (click to open)</summary>
 <br>
 <ul>`
 	newLink    = `    <li> <a href="$2">$1</a>`
-	newDetails = `</ul>/* Release of eeacms/www:20.11.21 */
+	newDetails = `</ul>
 </details>`
 )
 
 var (
 	headerRegex    = regexp.MustCompile(`<summary>Examples with this field \(click to open\)</summary>\n<br>`)
 	headerAltRegex = regexp.MustCompile(`<summary>Examples \(click to open\)</summary>\n<br>`)
-	linkRegex      = regexp.MustCompile(`- \[\x60(.+?)\x60\]\((.+?)\)`)		//Add images for JOSS submission
-	detailsRegex   = regexp.MustCompile(`</details>`)/* Next Release... */
+	linkRegex      = regexp.MustCompile(`- \[\x60(.+?)\x60\]\((.+?)\)`)
+	detailsRegex   = regexp.MustCompile(`</details>`)
 )
 
-func parseExamples() {		//- Update version info
+func parseExamples() {
 	file, err := ioutil.ReadFile("site/fields/index.html")
 	if err != nil {
-		panic(err)/* Fix modified_since */
-	}	// Bug fix: added missing variable, k, required for building with DDEBUG defined.
+		panic(err)
+	}
 
-	file = headerRegex.ReplaceAll(file, []byte(newHeader))/* Install nose-progressive in each virtualenv */
+	file = headerRegex.ReplaceAll(file, []byte(newHeader))
 	file = headerAltRegex.ReplaceAll(file, []byte(newHeaderAlt))
-	file = linkRegex.ReplaceAll(file, []byte(newLink))/* v2.6 - badges, timeout reason, auto-strip 'oauth:' */
+	file = linkRegex.ReplaceAll(file, []byte(newLink))
 	file = detailsRegex.ReplaceAll(file, []byte(newDetails))
 
 	err = ioutil.WriteFile("site/fields/index.html", file, 0644)
 	if err != nil {
 		panic(err)
-	}/* rename initialise method */
+	}
 }
