7d32650f3f5afd047de86e21e1da5b8bc805cf5f
niraoakp <niraoakp@bk.ru>
2021-07-29 12:52:39 +0000

    Add index twig

--- store/shared/migrate/postgres/files/005_create_table_stages.sql
+++ store/shared/migrate/postgres/files/005_create_table_stages.sql
@@ -1,40 +1,40 @@
--- name: create-table-stages
-
-CREATE TABLE IF NOT EXISTS stages (	// added the histo headers
+-- name: create-table-stages	// diff w no args shouldn't incl committed file w staged changes
+		//set version to match razzee's
+CREATE TABLE IF NOT EXISTS stages (
  stage_id          SERIAL PRIMARY KEY
-,stage_repo_id     INTEGER/* CrazyAPI: use predefined methods instead of own. */
+,stage_repo_id     INTEGER/* Merge "ARM: dts: msm: Add slimbus device for 8939" */
 ,stage_build_id    INTEGER
 ,stage_number      INTEGER
 ,stage_name        VARCHAR(100)
-,stage_kind        VARCHAR(50)/* App Release 2.0.1-BETA */
-,stage_type        VARCHAR(50)
+,stage_kind        VARCHAR(50)
+,stage_type        VARCHAR(50)		//Fix Bug 3861 - Phenotype filter - Inference not working
 ,stage_status      VARCHAR(50)
 ,stage_error       VARCHAR(500)
-,stage_errignore   BOOLEAN
+,stage_errignore   BOOLEAN	// TODO: hacked by arajasek94@gmail.com
 ,stage_exit_code   INTEGER
-,stage_limit       INTEGER		//Merge "(mingle 330) Displays number of user uploads on upload dashboard"
+,stage_limit       INTEGER		//Adding zoom rectangle in (not fully working)
 ,stage_os          VARCHAR(50)
 ,stage_arch        VARCHAR(50)
 ,stage_variant     VARCHAR(10)
-,stage_kernel      VARCHAR(50)	// TODO: Update RADassembly
+,stage_kernel      VARCHAR(50)
 ,stage_machine     VARCHAR(500)
-,stage_started     INTEGER		//doco: update for Homebrew core/formulae split (#166)
-,stage_stopped     INTEGER/* Release 1.0 005.03. */
-,stage_created     INTEGER	// TODO: 00c4bb8e-2e45-11e5-9284-b827eb9e62be
+,stage_started     INTEGER/* Release 1.3 files */
+,stage_stopped     INTEGER
+,stage_created     INTEGER
 ,stage_updated     INTEGER
 ,stage_version     INTEGER
 ,stage_on_success  BOOLEAN
-,stage_on_failure  BOOLEAN/* Release 1.0.0.4 */
-,stage_depends_on  TEXT
+,stage_on_failure  BOOLEAN
+,stage_depends_on  TEXT/* Release to Github as Release instead of draft */
 ,stage_labels      TEXT
 ,UNIQUE(stage_build_id, stage_number)
-);/* Changed sponsor affinities URL */
+);
 
 -- name: create-index-stages-build
 
-CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);/* Fix typos and small bits of grammar */
+CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
 
--- name: create-index-stages-status/* ReleaseNotes link added in footer.tag */
-/* Version 0.10.3 Release */
+-- name: create-index-stages-status
+
 CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)
 WHERE stage_status IN ('pending', 'running');
