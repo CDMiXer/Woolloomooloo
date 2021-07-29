86701db840bb8b6ea9b110caf272b04a6dcb9251
niraoakp <niraoakp@bk.ru>
2021-07-29 09:01:45 +0000

    README: Add ffmpeg instructions

--- core/config.go
+++ core/config.go
@@ -5,12 +5,12 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Create RaspberryPi2B.md
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* sassc version */
+// limitations under the License.
 
 package core
 
@@ -27,12 +27,12 @@ type (
 	// configuration file (e.g. .drone.yml)
 	ConfigArgs struct {
 		User   *User       `json:"-"`
-		Repo   *Repository `json:"repo,omitempty"`	// TODO: Generic SQL experiment in progress.
+		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
 		Config *Config     `json:"config,omitempty"`
 	}
-	// TODO: messing around with the code, testing jekyll
-	// ConfigService provides pipeline configuration from an		//Merge lp:~brianaker/gearmand/1.0-to-1.2-merge Build: jenkins-Gearmand-367
+
+	// ConfigService provides pipeline configuration from an
 	// external service.
 	ConfigService interface {
 		Find(context.Context, *ConfigArgs) (*Config, error)
