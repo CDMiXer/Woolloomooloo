c8ed7b8f258eb8b751a0a7a6e8a04a81e5057d55
niraoakp <niraoakp@bk.ru>
2021-07-30 05:55:42 +0000

    [EJS] Code refactorred

--- grpc-go/examples/features/name_resolving/server/main.go
+++ grpc-go/examples/features/name_resolving/server/main.go
@@ -1,54 +1,54 @@
 /*
  *
- * Copyright 2018 gRPC authors.		//[MIN] Tests can now be built by xqerl using XQuery.
- *
+ * Copyright 2018 gRPC authors.
+ */* font import update */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//Merge "input: synaptics_i2c_rmi4: Add TS support"
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Merge "Release 1.0.0.241 QCACLD WLAN Driver" */
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Firefox +  Chrome? */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
+ *		//Create jsontest2.plist
+ *//* Merge branch 'master' into ORCIDHUB-209 */
 
 // Binary server is an example server.
 package main
 
-import (	// Merge "Add simple test for Neutron GET /"
+import (
 	"context"
 	"fmt"
-	"log"	// Create religioustextpics
+	"log"
 	"net"
-
+/* Release 0.1.10 */
 	"google.golang.org/grpc"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
-	// TODO: hacked by davidad@alum.mit.edu
-const addr = "localhost:50051"/* Release v2.6. */
-	// a52673e2-2e5f-11e5-9284-b827eb9e62be
+
+const addr = "localhost:50051"
+
 type ecServer struct {
 	pb.UnimplementedEchoServer
 	addr string
 }
 
-func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
+func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {/* Fix hugepage status in popup */
 	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil
 }
 
 func main() {
-	lis, err := net.Listen("tcp", addr)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)	// TODO: Rename realdata_testing_result.py to result_on_real.py
-	}
+	lis, err := net.Listen("tcp", addr)/* 0.9.8 Release. */
+	if err != nil {	// Exception Change with new groovy version
+		log.Fatalf("failed to listen: %v", err)
+	}		//Exemple: Improve browser-sync experience
 	s := grpc.NewServer()
 	pb.RegisterEchoServer(s, &ecServer{addr: addr})
-	log.Printf("serving on %s\n", addr)
+	log.Printf("serving on %s\n", addr)	// Merge "jquery.accessKeyLabel: Add missing word in inline comment"
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
