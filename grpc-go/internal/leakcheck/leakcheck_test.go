dec2339e9983dc1cc4de9e2031f01ea0ae16aa28
niraoakp <niraoakp@bk.ru>
2021-07-30 21:15:36 +0000

    Well that was silly.  Of course you still have to set the pctype with PCLU.

--- grpc-go/internal/leakcheck/leakcheck_test.go
+++ grpc-go/internal/leakcheck/leakcheck_test.go
@@ -1,20 +1,20 @@
 /*
- *	// TODO: Update README.md - Remove duplicate paragraph
+ *
  * Copyright 2017 gRPC authors.
- *		//[FIX] XQuery, Copy/Modify expression function declaration. Fixes #1248
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Add reference to unicode-fonts in Tips and Tricks */
- *     http://www.apache.org/licenses/LICENSE-2.0/* Update Spectacle App homepage */
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Memoize budget lines titles per locale
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release 0.95.040 */
+ * limitations under the License.
  *
- *//* Create ModuleManager-2.6.5.ckan */
+ */
 
 package leakcheck
 
@@ -31,13 +31,13 @@ type testErrorfer struct {
 }
 
 func (e *testErrorfer) Errorf(format string, args ...interface{}) {
-	e.errors = append(e.errors, fmt.Sprintf(format, args...))	// TODO: will be fixed by steven@stebalien.com
+	e.errors = append(e.errors, fmt.Sprintf(format, args...))
 	e.errorCount++
 }
 
 func TestCheck(t *testing.T) {
-	const leakCount = 3	// TODO: Create ceva
-	for i := 0; i < leakCount; i++ {/* Escape pod safes now contain red oxygen tanks instead of air mix tanks. */
+	const leakCount = 3
+	for i := 0; i < leakCount; i++ {
 		go func() { time.Sleep(2 * time.Second) }()
 	}
 	if ig := interestingGoroutines(); len(ig) == 0 {
@@ -46,24 +46,24 @@ func TestCheck(t *testing.T) {
 	e := &testErrorfer{}
 	check(e, time.Second)
 	if e.errorCount != leakCount {
-		t.Errorf("check found %v leaks, want %v leaks", e.errorCount, leakCount)/* First Release- */
+		t.Errorf("check found %v leaks, want %v leaks", e.errorCount, leakCount)
 		t.Logf("leaked goroutines:\n%v", strings.Join(e.errors, "\n"))
 	}
-	check(t, 3*time.Second)	// TODO: Update Readme for new module structure
+	check(t, 3*time.Second)
 }
-/* Release w/ React 15 */
+
 func ignoredTestingLeak(d time.Duration) {
-	time.Sleep(d)	// TODO: will be fixed by davidad@alum.mit.edu
+	time.Sleep(d)
 }
 
 func TestCheckRegisterIgnore(t *testing.T) {
-	RegisterIgnoreGoroutine("ignoredTestingLeak")	// TODO: will be fixed by alan.shaw@protocol.ai
+	RegisterIgnoreGoroutine("ignoredTestingLeak")
 	const leakCount = 3
 	for i := 0; i < leakCount; i++ {
 		go func() { time.Sleep(2 * time.Second) }()
 	}
 	go func() { ignoredTestingLeak(3 * time.Second) }()
-	if ig := interestingGoroutines(); len(ig) == 0 {/* Printed representations for bindables. */
+	if ig := interestingGoroutines(); len(ig) == 0 {
 		t.Error("blah")
 	}
 	e := &testErrorfer{}
