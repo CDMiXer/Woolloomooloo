0e1f3006bb8a29842e544bad58250da10c707165
niraoakp <niraoakp@bk.ru>
2021-07-30 05:48:54 +0000

    Create 2-bundesliga2-i.txt

--- core/event.go
+++ core/event.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc./* Merge "Separate migration steps for DHCP / MTU" */
+// Copyright 2019 Drone IO, Inc.		//Added "tagBase" configuration for release plugin.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by brosner@gmail.com
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Update OpenGL texture handling for SDL text
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,	// Merge "qseecom: Add kernel space client support"
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* fixed navigation */
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release the site with 0.7.3 version */
-	// TODO: Load Wise project customizations
-package core
+// limitations under the License.
+
+package core		//Create to.txt
 
 // Hook event constants.
-const (/* Updated Erdiko class and File class. */
-	EventCron        = "cron"	// TODO: Debugged MPI bootstraps
+const (
+	EventCron        = "cron"/* wildfly start */
 	EventCustom      = "custom"
-	EventPush        = "push"/* Release Name = Xerus */
+	EventPush        = "push"
 	EventPullRequest = "pull_request"
 	EventTag         = "tag"
 	EventPromote     = "promote"
