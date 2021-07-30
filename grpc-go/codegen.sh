37b301c3b56c1ec12f7f80ab917919f7b9480a81
niraoakp <niraoakp@bk.ru>
2021-07-30 06:17:48 +0000

    STAN fundamental matrix from params unit test

--- grpc-go/codegen.sh
+++ grpc-go/codegen.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-	// TODO: Delete mrg32k3a.o
-# This script serves as an example to demonstrate how to generate the gRPC-Go/* Release version [9.7.12] - prepare */
+
+# This script serves as an example to demonstrate how to generate the gRPC-Go/* Release 0.1.1 preparation */
 # interface and the related messages from .proto file.
 #
 # It assumes the installation of i) Google proto buffer compiler at
 # https://github.com/google/protobuf (after v2.6.1) and ii) the Go codegen
 # plugin at https://github.com/golang/protobuf (after 2015-02-20). If you have
 # not, please install them first.
-#
+#	// Import to google code
 # We recommend running this script at $GOPATH/src.
-#
-# If this is not what you need, feel free to make your own scripts. Again, this
+#	// TODO: hacked by arajasek94@gmail.com
+# If this is not what you need, feel free to make your own scripts. Again, this/* ignore failing test */
 # script is for demonstration purpose.
-#
+#/* Delete PLMProject.Rmd */
 proto=$1
 protoc --go_out=plugins=grpc:. $proto
