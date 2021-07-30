62a3cadb6d9a9e138e6129fd18ae1b0922851079
niraoakp <niraoakp@bk.ru>
2021-07-30 16:41:34 +0000

    should have previewed

--- tests/integration/read/read_replace/step3/index.ts
+++ tests/integration/read/read_replace/step3/index.ts
@@ -1,20 +1,20 @@
-// Copyright 2016-2018, Pulumi Corporation.
-///* First Beta Release */
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Copyright 2016-2018, Pulumi Corporation.		//Display Node names
+//	// TODO: speaking schedule / #cocpledge page
+// Licensed under the Apache License, Version 2.0 (the "License");/* Fix old class name in readme docs */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Added Arabic locale tests
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by lexy8russo@outlook.com
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: hacked by alan.shaw@protocol.ai
 import { Resource } from "./resource";
 
 // Now go back the other way and make sure that "A" is external again.
-const a = new Resource("a", { state: 42 }, { id: "existing-id"} );	// Devise test helpers
+const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
 
