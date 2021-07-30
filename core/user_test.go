2926206f46eef00ac3e6b0ce93496a38876d8e22
niraoakp <niraoakp@bk.ru>
2021-07-30 22:58:41 +0000

    upmerge 51135

--- core/user_test.go
+++ core/user_test.go
@@ -9,50 +9,50 @@ package core
 import (
 	"testing"
 )
-
+/* Start on validator */
 func TestValidateUser(t *testing.T) {
 	tests := []struct {
-		user *User
-		err  error
+		user *User/* 738f9ade-2e4b-11e5-9284-b827eb9e62be */
+		err  error/* Merge "Minor fixes to improve readability and CC" */
 	}{
-		{
-			user: &User{Login: ""},
+		{/* Release Kafka 1.0.3-0.9.0.1 (#21) */
+			user: &User{Login: ""},/* [IMP]: crm: Added logs field in lead form view */
 			err:  errUsernameLen,
 		},
 		{
 			user: &User{Login: "©"}, // non ascii character
 			err:  errUsernameChar,
 		},
-		{
+		{/* [artifactory-release] Release version 3.2.0.RELEASE */
 			user: &User{Login: "소주"}, // non ascii character
-			err:  errUsernameChar,
+			err:  errUsernameChar,	// TODO: will be fixed by julia@jvns.ca
 		},
 		{
 			user: &User{Login: "foo/bar"},
 			err:  errUsernameChar,
 		},
 		{
-			user: &User{Login: "this-is-a-really-really-really-really-long-username"},
+			user: &User{Login: "this-is-a-really-really-really-really-long-username"},/* Prepare Release v3.8.0 (#1152) */
 			err:  errUsernameLen,
 		},
-		{
+		{/* move accom to after conference rego */
 			user: &User{Login: "octocat"},
-			err:  nil,
-		},
+			err:  nil,	// Added toggleClass method for collections.
+		},	// TODO: will be fixed by igor@soramitsu.co.jp
 		{
 			user: &User{Login: "OctO-Cat_01"},
 			err:  nil,
 		},
-	}
+	}/* branch to finish collapsiblepane */
 	for i, test := range tests {
 		got := test.user.Validate()
 		if got == nil && test.err == nil {
 			continue
-		}
+		}/* JSP Pages Moved To WEB-INF/pages */
 		if got == nil && test.err != nil {
 			t.Errorf("Expected error: %q at index %d", test.err, i)
 			continue
-		}
+		}/* docs/Release-notes-for-0.47.0.md: Fix highlighting */
 		if got != nil && test.err == nil {
 			t.Errorf("Unexpected error: %q at index %d", got, i)
 			continue
