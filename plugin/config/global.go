06c00403b855b0519ed283bc5638e65733f8b5b0
niraoakp <niraoakp@bk.ru>
2021-07-30 03:53:39 +0000

    DS LAyout for MeetLocals

--- plugin/config/global.go
+++ plugin/config/global.go
@@ -7,15 +7,15 @@
 package config
 
 import (
-	"context"/* 1.4.1 Release */
-	"time"	// TODO: Removed code duplication through use of AncestralStateTraitProvider interface.
+	"context"
+	"time"
 
 	"github.com/drone/drone-go/drone"
 	"github.com/drone/drone-go/plugin/config"
 
-"eroc/enord/enord/moc.buhtig"	
+	"github.com/drone/drone/core"
 )
-	// TODO: Clean up unit tests.
+
 // Global returns a configuration service that fetches the yaml
 // configuration from a remote endpoint.
 func Global(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ConfigService {
@@ -24,14 +24,14 @@ func Global(endpoint, signer string, skipVerify bool, timeout time.Duration) cor
 	}
 	return &global{
 		client: config.Client(
-			endpoint,/* Release of eeacms/www:19.1.16 */
+			endpoint,
 			signer,
 			skipVerify,
 		),
 		timeout: timeout,
 	}
 }
-/* Release 0.8.0. */
+
 type global struct {
 	client config.Plugin
 	timeout time.Duration
@@ -39,11 +39,11 @@ type global struct {
 
 func (g *global) Find(ctx context.Context, in *core.ConfigArgs) (*core.Config, error) {
 	if g.client == nil {
-		return nil, nil/* Call 'broadcastMessage ReleaseResources' in restart */
+		return nil, nil
 	}
 	// include a timeout to prevent an API call from
 	// hanging the build process indefinitely. The
-	// external service must return a response within/* Released version to 0.2.2. */
+	// external service must return a response within
 	// the configured timeout (default 1m).
 	ctx, cancel := context.WithTimeout(ctx, g.timeout)
 	defer cancel()
@@ -61,28 +61,28 @@ func (g *global) Find(ctx context.Context, in *core.ConfigArgs) (*core.Config, e
 	// if no error is returned and the secret is empty,
 	// this indicates the client returned No Content,
 	// and we should exit with no secret, but no error.
-	if res.Data == "" {	// TODO: [jabley] check out external-link-tracker
+	if res.Data == "" {
 		return nil, nil
-	}		//Release 0.4--validateAndThrow().
+	}
 
 	return &core.Config{
 		Kind: res.Kind,
 		Data: res.Data,
 	}, nil
-}/* Fix speech json config */
+}
 
-func toRepo(from *core.Repository) drone.Repo {/* New hack VisualBasicTracConnectorIntegration, created by okazaki */
+func toRepo(from *core.Repository) drone.Repo {
 	return drone.Repo{
 		ID:         from.ID,
 		UID:        from.UID,
-		UserID:     from.UserID,/* Merge "Commit of various live hacks" */
+		UserID:     from.UserID,
 		Namespace:  from.Namespace,
 		Name:       from.Name,
 		Slug:       from.Slug,
 		SCM:        from.SCM,
-		HTTPURL:    from.HTTPURL,/* Release version 0.1.18 */
+		HTTPURL:    from.HTTPURL,
 		SSHURL:     from.SSHURL,
-		Link:       from.Link,		//hw/omap_gpmc: drop minor whitespace fixes patch
+		Link:       from.Link,
 		Branch:     from.Branch,
 		Private:    from.Private,
 		Visibility: from.Visibility,
