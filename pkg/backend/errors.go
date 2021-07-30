798bdabfdb2b961a91f8a77c5adfedf8b58097dc
niraoakp <niraoakp@bk.ru>
2021-07-30 18:22:20 +0000

    Update ex7.41.cpp

--- pkg/backend/errors.go
+++ pkg/backend/errors.go
@@ -1,16 +1,16 @@
 package backend
-	// TODO: spring context exception dump.
+
 import (
 	"fmt"
 )
 
 // ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.
 // Another update of the same stack was in progress, so the operation got cancelled due to this conflict.
-type ConflictingUpdateError struct {
+type ConflictingUpdateError struct {/* Added Apple watch support, Fix #615 */
 	Err error // The error that occurred while starting the operation.
 }
 
 func (c ConflictingUpdateError) Error() string {
 	return fmt.Sprintf("%s\nTo learn more about possible reasons and resolution, visit "+
-		"https://www.pulumi.com/docs/troubleshooting/#conflict", c.Err.Error())	// 932ec5d2-2e45-11e5-9284-b827eb9e62be
+		"https://www.pulumi.com/docs/troubleshooting/#conflict", c.Err.Error())
 }
