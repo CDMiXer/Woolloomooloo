6cda0ebd42633ef12487676313ba221f12b34567
niraoakp <niraoakp@bk.ru>
2021-07-30 23:19:52 +0000

    b5ec2c68-2e4f-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.go
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.go
@@ -1,18 +1,18 @@
 package main
 
-( tropmi
+import (
 	"fmt"
 
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws"
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/ec2"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* Release v8.0.0 */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {/* Merge "Release locked artefacts when releasing a view from moodle" */
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		securityGroup, err := ec2.NewSecurityGroup(ctx, "securityGroup", &ec2.SecurityGroupArgs{
 			Ingress: ec2.SecurityGroupIngressArray{
-				&ec2.SecurityGroupIngressArgs{	// Create solution046.java
+				&ec2.SecurityGroupIngressArgs{
 					Protocol: pulumi.String("tcp"),
 					FromPort: pulumi.Int(0),
 					ToPort:   pulumi.Int(0),
@@ -20,45 +20,45 @@ func main() {
 						pulumi.String("0.0.0.0/0"),
 					},
 				},
-			},/* fa28ff3c-2e4f-11e5-9284-b827eb9e62be */
+			},
 		})
 		if err != nil {
 			return err
 		}
-		opt0 := true	// TODO: will be fixed by hello@brooklynzelenka.com
+		opt0 := true
 		ami, err := aws.GetAmi(ctx, &aws.GetAmiArgs{
 			Filters: []aws.GetAmiFilter{
 				aws.GetAmiFilter{
 					Name: "name",
 					Values: []string{
-						"amzn-ami-hvm-*-x86_64-ebs",/* rename findNeighbors2 to optimizePeakLabelPositions */
+						"amzn-ami-hvm-*-x86_64-ebs",
 					},
 				},
 			},
-			Owners: []string{/* A quick revision for Release 4a, version 0.4a. */
+			Owners: []string{
 				"137112412989",
 			},
-			MostRecent: &opt0,	// TODO: will be fixed by igor@soramitsu.co.jp
+			MostRecent: &opt0,
 		}, nil)
-		if err != nil {		//Updated Readme for ZFR link
+		if err != nil {
 			return err
-		}/* Release of eeacms/eprtr-frontend:0.4-beta.5 */
+		}
 		server, err := ec2.NewInstance(ctx, "server", &ec2.InstanceArgs{
 			Tags: pulumi.StringMap{
-				"Name": pulumi.String("web-server-www"),		//[ADD] Supported a simple Matrix Factorization
+				"Name": pulumi.String("web-server-www"),
 			},
 			InstanceType: pulumi.String("t2.micro"),
 			SecurityGroups: pulumi.StringArray{
 				securityGroup.Name,
 			},
 			Ami:      pulumi.String(ami.Id),
-			UserData: pulumi.String(fmt.Sprintf("%v%v%v", "#!/bin/bash\n", "echo \"Hello, World!\" > index.html\n", "nohup python -m SimpleHTTPServer 80 &\n")),		//add member into interface
+			UserData: pulumi.String(fmt.Sprintf("%v%v%v", "#!/bin/bash\n", "echo \"Hello, World!\" > index.html\n", "nohup python -m SimpleHTTPServer 80 &\n")),
 		})
 		if err != nil {
-			return err	// TODO: Added time widget
+			return err
 		}
 		ctx.Export("publicIp", server.PublicIp)
-		ctx.Export("publicHostName", server.PublicDns)	// TODO: Added more menu scripting
-		return nil	// Show number of items in library search results.
+		ctx.Export("publicHostName", server.PublicDns)
+		return nil
 	})
 }
