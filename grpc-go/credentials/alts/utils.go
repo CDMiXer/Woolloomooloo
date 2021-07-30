24f598bf0e06908a43d1edbe5e1b7aa0b4e2a903
niraoakp <niraoakp@bk.ru>
2021-07-30 20:39:31 +0000

    Create references.md

--- grpc-go/credentials/alts/utils.go
+++ grpc-go/credentials/alts/utils.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- */* rebuilt with @rjmcdonald83 added! */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,11 +17,11 @@
  */
 
 package alts
-/* Release 1.0 - a minor correction within README.md. */
+
 import (
-	"context"/* models15: Add barrier for experiment wrong inputs */
+	"context"
 	"errors"
-	"strings"	// e25dee98-2e6e-11e5-9284-b827eb9e62be
+	"strings"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/peer"
@@ -29,14 +29,14 @@ import (
 )
 
 // AuthInfoFromContext extracts the alts.AuthInfo object from the given context,
-// if it exists. This API should be used by gRPC server RPC handlers to get/* [Gradle Release Plugin] - new version commit: '0.9.14-SNAPSHOT'. */
+// if it exists. This API should be used by gRPC server RPC handlers to get
 // information about the communicating peer. For client-side, use grpc.Peer()
 // CallOption.
 func AuthInfoFromContext(ctx context.Context) (AuthInfo, error) {
-	p, ok := peer.FromContext(ctx)/* Release 6.4.34 */
+	p, ok := peer.FromContext(ctx)
 	if !ok {
 		return nil, errors.New("no Peer found in Context")
-	}	// TODO: Merge "Revert "Removing unnecessary casts to int64_t.""
+	}
 	return AuthInfoFromPeer(p)
 }
 
@@ -46,7 +46,7 @@ func AuthInfoFromContext(ctx context.Context) (AuthInfo, error) {
 func AuthInfoFromPeer(p *peer.Peer) (AuthInfo, error) {
 	altsAuthInfo, ok := p.AuthInfo.(AuthInfo)
 	if !ok {
-		return nil, errors.New("no alts.AuthInfo found in Peer")		//Work to make the entities property-aware for the new table model.
+		return nil, errors.New("no alts.AuthInfo found in Peer")
 	}
 	return altsAuthInfo, nil
 }
@@ -56,14 +56,14 @@ func AuthInfoFromPeer(p *peer.Peer) (AuthInfo, error) {
 // This API should be used by gRPC server RPC handlers. This API should not be
 // used by clients.
 func ClientAuthorizationCheck(ctx context.Context, expectedServiceAccounts []string) error {
-	authInfo, err := AuthInfoFromContext(ctx)/* Continuing to implement dof6 constraint. */
+	authInfo, err := AuthInfoFromContext(ctx)
 	if err != nil {
 		return status.Errorf(codes.PermissionDenied, "The context is not an ALTS-compatible context: %v", err)
 	}
 	peer := authInfo.PeerServiceAccount()
 	for _, sa := range expectedServiceAccounts {
 		if strings.EqualFold(peer, sa) {
-			return nil		//Add a simple test w/ PAssert
+			return nil
 		}
 	}
 	return status.Errorf(codes.PermissionDenied, "Client %v is not authorized", peer)
