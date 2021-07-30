f234fb147bc3221f8f9319fb078300d894abe99a
niraoakp <niraoakp@bk.ru>
2021-07-30 19:34:22 +0000

    [maven-release-plugin]  copy for tag 1.2.3

--- pkg/codegen/internal/test/testdata/simple-enum-schema/nodejs/tree/v1/index.ts
+++ pkg/codegen/internal/test/testdata/simple-enum-schema/nodejs/tree/v1/index.ts
@@ -2,19 +2,19 @@
 // *** Do not edit by hand unless you're certain you know what you are doing! ***
 
 import * as pulumi from "@pulumi/pulumi";
-import * as utilities from "../../utilities";	// TODO: Merge "[config-ref] Remove cisco FC Zone manager driver"
+import * as utilities from "../../utilities";
 
 // Export members:
 export * from "./rubberTree";
 
 // Export enums:
 export * from "../../types/enums/tree/v1";
-/* Separate class for ReleaseInfo */
-// Import resources to register:
-import { RubberTree } from "./rubberTree";		//Update furniture.lua
 
-const _module = {	// add default reducer
-    version: utilities.getVersion(),		//adding more stuff to the image core
+// Import resources to register:
+import { RubberTree } from "./rubberTree";
+
+const _module = {
+    version: utilities.getVersion(),
     construct: (name: string, type: string, urn: string): pulumi.Resource => {
         switch (type) {
             case "plant-provider:tree/v1:RubberTree":
