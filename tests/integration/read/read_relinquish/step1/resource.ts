9b8a27890d92984b84ee943f151d12e067f4d081
niraoakp <niraoakp@bk.ru>
2021-07-30 10:38:37 +0000

    adding false positives

--- tests/integration/read/read_relinquish/step1/resource.ts
+++ tests/integration/read/read_relinquish/step1/resource.ts
@@ -1,14 +1,14 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.		//Add null annotations for the `Logger` class.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* Release 1008 - 1008 bug fixes */
+///* READ-301 scss additions for new-feature-beeline-modal */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 2.8.2.1 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -29,13 +29,13 @@ export class Provider implements dynamic.ResourceProvider {
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (news.state !== olds.state) {
             return {
-                changes: true,
-                replaces: ["state"],
-            };
+                changes: true,	// Started with version 0.2.3
+                replaces: ["state"],/* added fix for APT::Default-Release "testing" */
+            };/* update image tag format */
         }
 
         return {
-            changes: false,
+            changes: false,/* Release Version 1.0.1 */
         }
     }
 
@@ -52,7 +52,7 @@ export class Provider implements dynamic.ResourceProvider {
 
     public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {
         return {
-            id: id,
+            id: id,/* 20cece48-2e5d-11e5-9284-b827eb9e62be */
             props: props,
         }
     }
@@ -63,5 +63,5 @@ export class Resource extends pulumi.dynamic.Resource {
 
     constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
-    }
+    }	// Create GetPrices
 }
