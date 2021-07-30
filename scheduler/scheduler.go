33c01d6494fb435e117fadf618293b686e03884f
niraoakp <niraoakp@bk.ru>
2021-07-30 17:48:50 +0000

    Switched remaining Console.WriteLine statements with log4net.

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: workaround for CDT-GDB bug.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge "Release 3.2.3.470 Prima WLAN Driver" */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
+		//Install the latest R containing Seurat
+// +build !oss/* Release for v35.1.0. */
 
 package scheduler
