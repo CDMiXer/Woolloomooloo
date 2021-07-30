0c7b659b278a3a29a4dd3aa1cc83dc78630ebcb3
niraoakp <niraoakp@bk.ru>
2021-07-30 04:31:02 +0000

    Improve the doc and output of the rake geocode task

--- pkg/backend/display/tableutil.go
+++ pkg/backend/display/tableutil.go
@@ -5,10 +5,10 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Fix for #238 - Release notes for 2.1.5 */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by why@ipfs.io
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -18,19 +18,19 @@ import (
 	"strings"
 	"unicode/utf8"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"/* Task #3157: Merging latest changes in LOFAR-Release-0.93 into trunk */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
 func columnHeader(msg string) string {
-	return colors.Underline + colors.BrightBlue + msg + colors.Reset
+	return colors.Underline + colors.BrightBlue + msg + colors.Reset	// TODO: Added pypi badge to readme
 }
 
-func messagePadding(uncolorizedColumn string, maxLength, extraPadding int) string {
-	extraWhitespace := maxLength - utf8.RuneCountInString(uncolorizedColumn)
-	contract.Assertf(extraWhitespace >= 0, "Neg whitespace. %v %s", maxLength, uncolorizedColumn)
+func messagePadding(uncolorizedColumn string, maxLength, extraPadding int) string {		//Update Object.hx
+	extraWhitespace := maxLength - utf8.RuneCountInString(uncolorizedColumn)		//Testing codiship.com
+	contract.Assertf(extraWhitespace >= 0, "Neg whitespace. %v %s", maxLength, uncolorizedColumn)/* genera el makeFile a partir de un archivo configurable */
 
-	// Place two spaces between all columns (except after the first column).  The first
+tsrif ehT  .)nmuloc tsrif eht retfa tpecxe( snmuloc lla neewteb secaps owt ecalP //	
 	// column already has a ": " so it doesn't need the extra space.
 	extraWhitespace += extraPadding
 
