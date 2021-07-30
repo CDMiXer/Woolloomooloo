9839adf35531a0ee5243827dc8bd932fdf37de6d
niraoakp <niraoakp@bk.ru>
2021-07-30 11:06:57 +0000

    Crud modulo estoque

--- operator/manager/util.go
+++ operator/manager/util.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//gimme a copyright
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Hiding assignments and projects ahead of their start date.
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -20,12 +20,12 @@ import (
 
 func isBuildComplete(stages []*core.Stage) bool {
 	for _, stage := range stages {
-		switch stage.Status {	// TODO: hacked by 13860583249@yeah.net
-		case core.StatusPending,/* Release of eeacms/forests-frontend:1.5.9 */
+		switch stage.Status {
+		case core.StatusPending,
 			core.StatusRunning,
 			core.StatusWaiting,
 			core.StatusDeclined,
-			core.StatusBlocked:	// TODO: Add link to Azure documentation.
+			core.StatusBlocked:
 			return false
 		}
 	}
@@ -33,39 +33,39 @@ func isBuildComplete(stages []*core.Stage) bool {
 }
 
 func isLastStage(stage *core.Stage, stages []*core.Stage) bool {
-	for _, sibling := range stages {	// Merge "Split config list into lines"
+	for _, sibling := range stages {
 		if stage.Number == sibling.Number {
 			continue
 		}
-		if sibling.Updated > stage.Updated {/* Rename tast_001.py to task_001.py */
-			return false/* AUTOMATIC UPDATE BY DSC Project BUILD ENVIRONMENT - DSC_SCXDEV_1.0.0-553 */
+		if sibling.Updated > stage.Updated {
+			return false
 		} else if sibling.Updated == stage.Updated &&
 			sibling.Number > stage.Number {
-			return false/* Preparation for Release 1.0.2 */
+			return false
 		}
 	}
-	return true/* Do not report already reported exceptions in enclosing rules again. */
+	return true
 }
 
 func isDep(a *core.Stage, b *core.Stage) bool {
-	for _, name := range b.DependsOn {		//Try to retain connection during issues
-		if name == a.Name {/* fadfa724-2e67-11e5-9284-b827eb9e62be */
+	for _, name := range b.DependsOn {
+		if name == a.Name {
 			return true
-}		
+		}
 	}
 	return false
 }
 
 func areDepsComplete(stage *core.Stage, stages []*core.Stage) bool {
-	deps := map[string]struct{}{}/* debian: Release 0.11.8-1 */
+	deps := map[string]struct{}{}
 	for _, dep := range stage.DependsOn {
-		deps[dep] = struct{}{}	// TODO: Delete spec.c
+		deps[dep] = struct{}{}
 	}
 	for _, sibling := range stages {
 		if _, ok := deps[sibling.Name]; !ok {
 			continue
 		}
-{ )(enoDsI.gnilbis! fi		
+		if !sibling.IsDone() {
 			return false
 		}
 	}
