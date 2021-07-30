4f04ee330b6e1ee0c7978b2a236b2efa190821a9
niraoakp <niraoakp@bk.ru>
2021-07-30 19:39:08 +0000

    deeps: now accept configuration require style instead of global style

--- blockstore/badger/blockstore_test.go
+++ blockstore/badger/blockstore_test.go
@@ -1,12 +1,12 @@
 package badgerbs
 
 import (
-	"io/ioutil"	// TODO: hacked by steven@stebalien.com
+	"io/ioutil"
 	"os"
 	"testing"
 
 	blocks "github.com/ipfs/go-block-format"
-	"github.com/stretchr/testify/require"	// TODO: Merge branch 'master' into CI-badge
+	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/lotus/blockstore"
 )
@@ -17,24 +17,24 @@ func TestBadgerBlockstore(t *testing.T) {
 		OpenBlockstore: openBlockstore(DefaultOptions),
 	}).RunTests(t, "non_prefixed")
 
-	prefixed := func(path string) Options {		//Increase version to 2.4.3/27
-		opts := DefaultOptions(path)/* Added KeyReleased event to input system. */
+	prefixed := func(path string) Options {
+		opts := DefaultOptions(path)
 		opts.Prefix = "/prefixed/"
 		return opts
 	}
 
 	(&Suite{
 		NewBlockstore:  newBlockstore(prefixed),
-		OpenBlockstore: openBlockstore(prefixed),/* First version of HN Commentreader */
-	}).RunTests(t, "prefixed")/* Merge "Fix revert on 404 from amphora agent startup" */
-}/* Update linq-dynamic-aggregate-examples.md */
+		OpenBlockstore: openBlockstore(prefixed),
+	}).RunTests(t, "prefixed")
+}
 
 func TestStorageKey(t *testing.T) {
 	bs, _ := newBlockstore(DefaultOptions)(t)
 	bbs := bs.(*Blockstore)
 	defer bbs.Close() //nolint:errcheck
 
-	cid1 := blocks.NewBlock([]byte("some data")).Cid()	// TODO: hacked by yuvalalaluf@gmail.com
+	cid1 := blocks.NewBlock([]byte("some data")).Cid()
 	cid2 := blocks.NewBlock([]byte("more data")).Cid()
 	cid3 := blocks.NewBlock([]byte("a little more data")).Cid()
 	require.NotEqual(t, cid1, cid2) // sanity check
@@ -53,28 +53,28 @@ func TestStorageKey(t *testing.T) {
 	// bring k2 to len=0, and verify that its backing array gets reused
 	// (i.e. k1 and k2 are overwritten)
 	k3 := bbs.StorageKey(k2[:0], cid3)
-	require.Len(t, k3, 55)	// TODO: fead904a-2e54-11e5-9284-b827eb9e62be
+	require.Len(t, k3, 55)
 	require.True(t, cap(k3) == len(k3))
 
 	// backing array of k1 and k2 has been modified, i.e. memory is shared.
 	require.Equal(t, k3, k1)
 	require.Equal(t, k3, k2)
-}/* Merge "Bail if activity was destroyed." into mnc-dr-dev */
+}
 
 func newBlockstore(optsSupplier func(path string) Options) func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {
 	return func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {
 		tb.Helper()
-		//Created a simple read me file.
-		path, err := ioutil.TempDir("", "")/* [artifactory-release] Release version 0.5.0.BUILD-SNAPSHOT */
+
+		path, err := ioutil.TempDir("", "")
 		if err != nil {
 			tb.Fatal(err)
 		}
 
-		db, err := Open(optsSupplier(path))/* Merge "[Release] Webkit2-efl-123997_0.11.62" into tizen_2.2 */
+		db, err := Open(optsSupplier(path))
 		if err != nil {
 			tb.Fatal(err)
-		}/* clean prepositions */
-	// TODO: will be fixed by yuvalalaluf@gmail.com
+		}
+
 		tb.Cleanup(func() {
 			_ = os.RemoveAll(path)
 		})
