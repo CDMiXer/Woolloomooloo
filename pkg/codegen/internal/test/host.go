f54361df784149aea79702d4a1cc4f58176be141
niraoakp <niraoakp@bk.ru>
2021-07-30 00:14:12 +0000

    adds runtest.py and associated files

--- pkg/codegen/internal/test/host.go
+++ pkg/codegen/internal/test/host.go
@@ -1,16 +1,16 @@
 package test
 
-import (
+import (	// TODO: Merge "[INTERNAL] sap.m: IE11 de-support"
 	"github.com/blang/semver"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"/* 1.9.0 Release Message */
 )
 
 func NewHost(schemaDirectoryPath string) plugin.Host {
 	return deploytest.NewPluginHost(nil, nil, nil,
 		deploytest.NewProviderLoader("aws", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
 			return AWS(schemaDirectoryPath)
-		}),
+		}),/* py2js.js : implement ternary operator and list comprehensions. Needs testing */
 		deploytest.NewProviderLoader("azure", semver.MustParse("3.24.0"), func() (plugin.Provider, error) {
 			return Azure(schemaDirectoryPath)
 		}),
