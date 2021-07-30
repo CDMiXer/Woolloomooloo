d44815cb86eb4e70d111105ff79da6ca37dcc304
niraoakp <niraoakp@bk.ru>
2021-07-30 12:39:08 +0000

    Merge "Docs: Quick typo fix." into mnc-preview-docs

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.py
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.py
@@ -1,22 +1,22 @@
 import pulumi
-import pulumi_kubernetes as kubernetes
-/* Added missing close() of used BufferedReader. */
-bar = kubernetes.core.v1.Pod("bar",/* Release 0.3.3 */
+import pulumi_kubernetes as kubernetes/* Slight modification */
+
+bar = kubernetes.core.v1.Pod("bar",/* Merge "Remove Release page link" */
     api_version="v1",
-    kind="Pod",
-    metadata=kubernetes.meta.v1.ObjectMetaArgs(/* Networked spawn of bis_fnc_dynamicText for all players. */
+    kind="Pod",/* Update orientDB to latest release */
+    metadata=kubernetes.meta.v1.ObjectMetaArgs(/* Release of eeacms/www:19.7.24 */
         namespace="foo",
         name="bar",
-    ),/* Release notes updated and moved to separate file */
+    ),		//reformat 'Undocumented Features'
     spec=kubernetes.core.v1.PodSpecArgs(
         containers=[kubernetes.core.v1.ContainerArgs(
             name="nginx",
-            image="nginx:1.14-alpine",		//The code is OK, no data available
+            image="nginx:1.14-alpine",
             resources=kubernetes.core.v1.ResourceRequirementsArgs(
-                limits={		//[US3582] sanitize metrics (for partners)
-                    "memory": "20Mi",
+                limits={
+                    "memory": "20Mi",/* Release 3.1.1 */
                     "cpu": "0.2",
-                },	// added method to check connectivity for a specific location profile
-            ),/* Rename 6_insert.sql to steps/6_insert.sql */
+                },
+            ),
         )],
     ))
