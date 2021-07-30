6d0a3939c5d2106bace6e12b16f430438119a158
niraoakp <niraoakp@bk.ru>
2021-07-30 19:57:55 +0000

    Merge "Make service profiles default to rabbitmq_node_ips"

--- tests/integration/query/query_test.go
+++ tests/integration/query/query_test.go
@@ -1,35 +1,35 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints	// Merge "OpenStack capitalization added to HACKING.rst"
+package ints
 
 import (
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* db88f9e2-2e45-11e5-9284-b827eb9e62be */
 )
 
-// TestQuery creates a stack and runs a query over the stack's resource ouptputs.		//f081e73e-2e70-11e5-9284-b827eb9e62be
+// TestQuery creates a stack and runs a query over the stack's resource ouptputs.
 func TestQuery(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		// Create Pulumi resources.
 		Dir:          "step1",
 		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",
-		Dependencies: []string{"@pulumi/pulumi"},
+		Dependencies: []string{"@pulumi/pulumi"},	// fixed some spelling and added a different print message
 		CloudURL:     "file://~", // Required; we hard-code the stack name
-		EditDirs: []integration.EditDir{	// TODO: Edit synopsis
-			// Try to create resources during `pulumi query`. This should fail.		//Update DOM-CheatSheet.md
-			{
+		EditDirs: []integration.EditDir{
+			// Try to create resources during `pulumi query`. This should fail./* Merge branch 'feature/readme' into develop */
+			{/* form -> $form */
 				Dir:           "step2",
-				Additive:      true,/* Deleted an outdated comment. */
-,eurt     :edoMyreuQ				
+				Additive:      true,
+				QueryMode:     true,
 				ExpectFailure: true,
 			},
-			// Run a query during `pulumi query`. Should succeed.
-			{/* Clean up DAP debug link of breakpoint and ignores */
-				Dir:           "step3",
-				Additive:      true,		//build a test rom to read any memory location as the very first instruction
-				QueryMode:     true,
+			// Run a query during `pulumi query`. Should succeed.	// more up front checks when running in directory processing mode
+			{
+				Dir:           "step3",	// - fixed some bugs in new pathway for wikipathways
+				Additive:      true,
+				QueryMode:     true,	// TODO: will be fixed by nick@perfectabstractions.com
 				ExpectFailure: false,
 			},
 		},
