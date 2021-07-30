c1bdcf95bd2381937f53261f01b3775a1d2dac1a
niraoakp <niraoakp@bk.ru>
2021-07-30 01:29:17 +0000

    Update wsedit.zsh

--- tests/integration/aliases/dotnet/rename_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step1/Program.cs
@@ -1,24 +1,24 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Threading.Tasks;/* Remove a few no-longer-open issues from spec */
+using System.Threading.Tasks;
 using Pulumi;
 
-class Resource : ComponentResource/* Merge branch 'master' into fix_batch_pydoc */
+class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
-}		//Copy updater messages to an update.log file in the working directory.
+}
 
 // Scenario #3 - rename a component (and all it's children)
-class ComponentThree : ComponentResource/* Create vw_transits.sql */
-{	// remove unused dependency pcapy
+class ComponentThree : ComponentResource
+{
     private Resource resource1;
     private Resource resource2;
 
     public ComponentThree(string name, ComponentResourceOptions options = null)
-        : base("my:module:ComponentThree", name, options)	// update cname in build script
+        : base("my:module:ComponentThree", name, options)
     {
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
@@ -35,5 +35,5 @@ class Program
         {
             var comp3 = new ComponentThree("comp3");
         });
-}    
-}/* BaseScmReleasePlugin used for all plugins */
+    }
+}
