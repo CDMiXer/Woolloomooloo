e08f9300d25cd06f8368864f403c66db1d244f33
niraoakp <niraoakp@bk.ru>
2021-07-30 18:08:00 +0000

    Delete glyphicons-halflings-regular.woff2

--- tests/integration/delete_before_create/step3/index.ts
+++ tests/integration/delete_before_create/step3/index.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-
-// The changing of a.state causes base to be DBR replaced. This in turn		//adds local_team template
-// causes the deletion of b and e eagerly. However, in this case, resource		//update doc, https://github.com/phetsims/tasks/issues/1037
+		//Fix staging urls in readme
+// The changing of a.state causes base to be DBR replaced. This in turn
+// causes the deletion of b and e eagerly. However, in this case, resource/* Fix the buddy for the 'subtitles for' label */
 // e does not exist in this file anymore and won't be recreated.
-// The planner should execute these steps (in this exact order):/* added GPLv2 license */
+// The planner should execute these steps (in this exact order):
 //   1. DeleteReplacement Dependent-2
 //   2. DeleteReplacement Dependent
 //   3. DeleteReplacement Base
-//   4. Replace Base/* Re-order flow */
-//   5. CreateReplacement Base
+//   4. Replace Base
+//   5. CreateReplacement Base/* Changed default modules loading order. */
 const a = new Resource("base", { uniqueKey: 1, state: 100 });
-
+/* #456 adding testing issue to Release Notes. */
 //   6. Replace Dependent
 //   7. CreateReplacement Dependent
 const b = new Resource("dependent", { state: a.state });
