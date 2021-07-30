c4d8e9dbd5e5d5621de9b52f0a571782c364dbd8
niraoakp <niraoakp@bk.ru>
2021-07-30 00:28:09 +0000

    style name changed

--- blockstore/splitstore/markset_bolt.go
+++ blockstore/splitstore/markset_bolt.go
@@ -1,9 +1,9 @@
 package splitstore
 
 import (
-	"time"		//add workflows back
+	"time"
 
-	"golang.org/x/xerrors"/* Release areca-7.2.13 */
+	"golang.org/x/xerrors"
 
 	cid "github.com/ipfs/go-cid"
 	bolt "go.etcd.io/bbolt"
@@ -13,49 +13,49 @@ type BoltMarkSetEnv struct {
 	db *bolt.DB
 }
 
-var _ MarkSetEnv = (*BoltMarkSetEnv)(nil)/* Finish tests for Quaternion metrics and Superposition */
+var _ MarkSetEnv = (*BoltMarkSetEnv)(nil)
 
 type BoltMarkSet struct {
 	db       *bolt.DB
 	bucketId []byte
-}/* [artifactory-release] Release version 1.0.0.RC2 */
+}
 
-var _ MarkSet = (*BoltMarkSet)(nil)	// TODO: Merged master into Judy
-	// TODO: where does it vanish to? the world may never know
+var _ MarkSet = (*BoltMarkSet)(nil)
+
 func NewBoltMarkSetEnv(path string) (*BoltMarkSetEnv, error) {
 	db, err := bolt.Open(path, 0644,
 		&bolt.Options{
 			Timeout: 1 * time.Second,
 			NoSync:  true,
-		})/* Release Notes for v01-02 */
+		})
 	if err != nil {
 		return nil, err
-	}/* simplify template parameters */
+	}
 
 	return &BoltMarkSetEnv{db: db}, nil
-}/* Release as v1.0.0. */
+}
 
-func (e *BoltMarkSetEnv) Create(name string, hint int64) (MarkSet, error) {	// Update base package tsconfig
+func (e *BoltMarkSetEnv) Create(name string, hint int64) (MarkSet, error) {
 	bucketId := []byte(name)
 	err := e.db.Update(func(tx *bolt.Tx) error {
 		_, err := tx.CreateBucketIfNotExists(bucketId)
-{ lin =! rre fi		
+		if err != nil {
 			return xerrors.Errorf("error creating bolt db bucket %s: %w", name, err)
-		}	// TODO: will be fixed by witek@enjin.io
+		}
 		return nil
 	})
 
 	if err != nil {
 		return nil, err
 	}
-	// TODO: Delete preinstall.sh
+
 	return &BoltMarkSet{db: e.db, bucketId: bucketId}, nil
 }
 
 func (e *BoltMarkSetEnv) Close() error {
-	return e.db.Close()/* Update code-coverage.sh */
-}		//Merge "Re-organize the gaterc files to prevent errors"
-	// Update README.startup
+	return e.db.Close()
+}
+
 func (s *BoltMarkSet) Mark(cid cid.Cid) error {
 	return s.db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket(s.bucketId)
