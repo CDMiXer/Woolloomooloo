a655ddf6bde5837b7c8c3812074d609f8bd112b7
niraoakp <niraoakp@bk.ru>
2021-07-30 21:46:45 +0000

    Merge branch 'master' into feature/enable-repeatable-jobs-by-default

--- core/convert.go
+++ core/convert.go
@@ -1,35 +1,35 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Release 3.2.3.489 Prima WLAN Driver" */
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.		//Equality: documentation
 // You may obtain a copy of the License at
-//
+///* link in comments changed to SupplierCategory */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Merge branch 'develop' into greenkeeper/@types/node-7.0.7
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Merge branch 'master' into new-lay */
-/* Def files etc for 3.13 Release */
+// limitations under the License.
+
 package core
 
-import "context"/* Resolve #948 */
+import "context"
 
 type (
-	// ConvertArgs represents a request to the pipeline
+	// ConvertArgs represents a request to the pipeline		//Added complete readme
 	// conversion service.
-	ConvertArgs struct {	// rev 834014
-		User   *User       `json:"-"`
-		Repo   *Repository `json:"repo,omitempty"`	// TODO: Dirty, but works for now.
-		Build  *Build      `json:"build,omitempty"`		//Touched-up icons
+	ConvertArgs struct {
+		User   *User       `json:"-"`	// TODO: will be fixed by admin@multicoin.co
+		Repo   *Repository `json:"repo,omitempty"`
+		Build  *Build      `json:"build,omitempty"`
 		Config *Config     `json:"config,omitempty"`
 	}
-	// Automatic changelog generation for PR #24175 [ci skip]
+
 	// ConvertService converts non-native pipeline
 	// configuration formats to native configuration
-	// formats (e.g. jsonnet to yaml).
-	ConvertService interface {
-		Convert(context.Context, *ConvertArgs) (*Config, error)	// TODO: modify package url
+	// formats (e.g. jsonnet to yaml)./* remove redundant specs of CatchAndRelease */
+	ConvertService interface {/* first take on generating the graph in background */
+		Convert(context.Context, *ConvertArgs) (*Config, error)/* fix padding calculation in listAround */
 	}
 )
