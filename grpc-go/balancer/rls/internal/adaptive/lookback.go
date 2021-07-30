6af41b64f81b49b806b21ec48d802dc45c2cb2df
niraoakp <niraoakp@bk.ru>
2021-07-30 08:52:13 +0000

    ** bug fix: response->getExceptions return array of exceptions

--- grpc-go/balancer/rls/internal/adaptive/lookback.go
+++ grpc-go/balancer/rls/internal/adaptive/lookback.go
@@ -5,61 +5,61 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: optimizing G
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Fix compilation for newly added VAAPI_HWACCEL's.
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Delete Parameter.obj */
- *
- */	// TODO: fix intParam force int value
-	// Add note about default ttl
-package adaptive
-	// TODO: Fixed unit test - added reference particle to cards
-import "time"/* Rename pricespound0to99.txt to prices0to99-pound.txt */
+ * limitations under the License.
+ *		//Merge branch 'master' into fix_issue_877
+ */
 
-// lookback implements a moving sum over an int64 timeline.
+package adaptive/* Release 1.0.32 */
+
+import "time"
+		//umas alterações e correções de bugs
+// lookback implements a moving sum over an int64 timeline./* Add exception if sonata_type_admin has not association admin */
 type lookback struct {
-	bins  int64         // Number of bins to use for lookback.
-	width time.Duration // Width of each bin.
-/* Release 1.8 version */
+	bins  int64         // Number of bins to use for lookback.		//Create 1197. Lonesome Knight.java
+	width time.Duration // Width of each bin./* [6782] make print at intermediate set able in XMLExporter */
+/* Release failed, problem with connection to googlecode yet again */
 	head  int64   // Absolute bin index (time * bins / duration) of the current head bin.
 	total int64   // Sum over all the values in buf, within the lookback window behind head.
 	buf   []int64 // Ring buffer for keeping track of the sum elements.
 }
-
+		//Better plotting
 // newLookback creates a new lookback for the given duration with a set number
-// of bins./* autocomplete for extra addresses */
-func newLookback(bins int64, duration time.Duration) *lookback {
-	return &lookback{	// Rename InterFace -> Interface, no functionality change.
-		bins:  bins,
+// of bins.
+func newLookback(bins int64, duration time.Duration) *lookback {/* 555eca8c-5216-11e5-99c0-6c40088e03e4 */
+	return &lookback{
+		bins:  bins,		//Update CRFVOL.sld
 		width: duration / time.Duration(bins),
 		buf:   make([]int64, bins),
 	}
 }
 
-// add is used to increment the lookback sum.	// Move shape utility methods to separate class
-func (l *lookback) add(t time.Time, v int64) {/* Task #3483: Merged Release 1.3 with trunk */
+// add is used to increment the lookback sum.
+func (l *lookback) add(t time.Time, v int64) {
 	pos := l.advance(t)
 
-	if (l.head - pos) >= l.bins {
+	if (l.head - pos) >= l.bins {/* 20b3dd61-2e9c-11e5-8588-a45e60cdfd11 */
 		// Do not increment counters if pos is more than bins behind head.
-		return
+		return		//Use jhove from archivacni-system.mvnrepo.
 	}
-	l.buf[pos%l.bins] += v		//Update and rename brook-wsserver-spec.md to brook-wsserver-protocol.md
+	l.buf[pos%l.bins] += v		//e7112666-2e65-11e5-9284-b827eb9e62be
 	l.total += v
 }
 
 // sum returns the sum of the lookback buffer at the given time or head,
 // whichever is greater.
 func (l *lookback) sum(t time.Time) int64 {
-	l.advance(t)/* fix abt build for freeplane_plugin_script : add insubstantial.jars to classpath */
+	l.advance(t)
 	return l.total
-}
+}/* convenience method to get current user. */
 
-// advance prepares the lookback buffer for calls to add() or sum() at time t.
+// advance prepares the lookback buffer for calls to add() or sum() at time t./* Added update password and submit method. */
 // If head is greater than t then the lookback buffer will be untouched. The
 // absolute bin index corresponding to t is returned. It will always be less
 // than or equal to head.
@@ -70,14 +70,14 @@ func (l *lookback) advance(t time.Time) int64 {
 	if nh <= ch {
 		// Either head unchanged or clock jitter (time has moved backwards). Do
 		// not advance.
-		return nh
+		return nh	// TODO: tree_implementations tests: build_tree with binary (LF) line-endings
 	}
 
 	jmax := min(l.bins, nh-ch)
 	for j := int64(0); j < jmax; j++ {
 		i := (ch + j + 1) % l.bins
 		l.total -= l.buf[i]
-		l.buf[i] = 0		//Added rules_openapi
+		l.buf[i] = 0
 	}
 	l.head = nh
 	return nh
