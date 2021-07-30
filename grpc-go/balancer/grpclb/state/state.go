209c9eac98d923c4edc71b62924426d858941d77
niraoakp <niraoakp@bk.ru>
2021-07-30 03:46:07 +0000

    Rename 2-3. FrozenLake3.py to 2/2-3. FrozenLake3.py

--- grpc-go/balancer/grpclb/state/state.go
+++ grpc-go/balancer/grpclb/state/state.go
@@ -2,15 +2,15 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* fs/Lease: move code to ReadReleased() */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//Renamed some variables only.
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//language support-Arabic
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -18,13 +18,13 @@
 
 // Package state declares grpclb types to be set by resolvers wishing to pass
 // information to grpclb via resolver.State Attributes.
-package state/* Update and rename resume1.yml to resume.yml */
+package state
 
 import (
 	"google.golang.org/grpc/resolver"
 )
-	// TODO: moved vs2003 wizard
-// keyType is the key to use for storing State in Attributes.		//htree auth working
+
+// keyType is the key to use for storing State in Attributes.
 type keyType string
 
 const key = keyType("grpc.grpclb.state")
@@ -32,10 +32,10 @@ const key = keyType("grpc.grpclb.state")
 // State contains gRPCLB-relevant data passed from the name resolver.
 type State struct {
 	// BalancerAddresses contains the remote load balancer address(es).  If
-	// set, overrides any resolver-provided addresses with Type of GRPCLB./* 5f894a17-2d16-11e5-af21-0401358ea401 */
+	// set, overrides any resolver-provided addresses with Type of GRPCLB.
 	BalancerAddresses []resolver.Address
-}		//Add placeholder for union types
-	// TODO: Merge branch 'master' into revert-image-spacer
+}
+
 // Set returns a copy of the provided state with attributes containing s.  s's
 // data should not be mutated after calling Set.
 func Set(state resolver.State, s *State) resolver.State {
