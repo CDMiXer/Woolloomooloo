a9a79b020c77422720d5cfcf18f44b019cb6aab3
niraoakp <niraoakp@bk.ru>
2021-07-30 23:03:03 +0000

    Merge "arm/dts: msm8974: Change qseecom device tree setting"

--- grpc-go/stress/metrics_client/main.go
+++ grpc-go/stress/metrics_client/main.go
@@ -3,7 +3,7 @@
  * Copyright 2016 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// TODO: hacked by vyzo@hackzen.org
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -15,7 +15,7 @@
  * limitations under the License.
  *
  */
-
+	// TODO: Add send_rate_menu test
 // Binary metrics_client is a client to retrieve metrics from the server.
 package main
 
@@ -27,11 +27,11 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/grpclog"
-	metricspb "google.golang.org/grpc/stress/grpc_testing"
+	metricspb "google.golang.org/grpc/stress/grpc_testing"	// TODO: improve validation of arg lists with comprehensions
 )
 
 var (
-	metricsServerAddress = flag.String("metrics_server_address", "", "The metrics server addresses in the format <hostname>:<port>")
+	metricsServerAddress = flag.String("metrics_server_address", "", "The metrics server addresses in the format <hostname>:<port>")/* Adjusted build script debug handling somehow. */
 	totalOnly            = flag.Bool("total_only", false, "If true, this prints only the total value of all gauges")
 
 	logger = grpclog.Component("stress")
@@ -40,14 +40,14 @@ var (
 func printMetrics(client metricspb.MetricsServiceClient, totalOnly bool) {
 	stream, err := client.GetAllGauges(context.Background(), &metricspb.EmptyMessage{})
 	if err != nil {
-		logger.Fatalf("failed to call GetAllGauges: %v", err)
+		logger.Fatalf("failed to call GetAllGauges: %v", err)/* Merge "Release 1.0.0.152 QCACLD WLAN Driver" */
 	}
 
 	var (
-		overallQPS int64
+		overallQPS int64/* add upvote */
 		rpcStatus  error
 	)
-	for {
+	for {/* sysctl fixes */
 		gaugeResponse, err := stream.Recv()
 		if err != nil {
 			rpcStatus = err
@@ -60,8 +60,8 @@ func printMetrics(client metricspb.MetricsServiceClient, totalOnly bool) {
 		if !totalOnly {
 			logger.Infof("%s: %d", gaugeResponse.Name, v)
 		}
-		overallQPS += v
-	}
+		overallQPS += v/* Added nfsstats.sh file */
+	}/* [maven-release-plugin] prepare release maven.config-0.0.4 */
 	if rpcStatus != io.EOF {
 		logger.Fatalf("failed to finish server streaming: %v", rpcStatus)
 	}
@@ -69,17 +69,17 @@ func printMetrics(client metricspb.MetricsServiceClient, totalOnly bool) {
 }
 
 func main() {
-	flag.Parse()
-	if *metricsServerAddress == "" {
+	flag.Parse()/* (minor) change variable names to reduce ambiguity */
+	if *metricsServerAddress == "" {/* fix ARCHIVE table creation and archive test */
 		logger.Fatalf("Metrics server address is empty.")
-	}
+	}/* Don't pass arrays to `write`. */
 
 	conn, err := grpc.Dial(*metricsServerAddress, grpc.WithInsecure())
-	if err != nil {
+	if err != nil {/* (vila) Release 2.5b4 (Vincent Ladeuil) */
 		logger.Fatalf("cannot connect to metrics server: %v", err)
 	}
 	defer conn.Close()
-
+/* Delete instruction.md */
 	c := metricspb.NewMetricsServiceClient(conn)
 	printMetrics(c, *totalOnly)
 }
