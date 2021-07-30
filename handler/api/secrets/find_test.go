08d7c921b065365ad3b2b22e0468d30088829705
niraoakp <niraoakp@bk.ru>
2021-07-30 07:09:36 +0000

    Improved content skipping (in case on Content-Type header absence)

--- handler/api/secrets/find_test.go
+++ handler/api/secrets/find_test.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//sortownie dla wizyt zaplanowanych
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//Fix RDoc Deprecation Warning
 // +build !oss
-
+		//Add documentation for "open project .tm_properties"
 package secrets
-
+		//docs: update install instructions for new packages
 import (
 	"context"
-	"encoding/json"
+	"encoding/json"	// TODO: Overview should be ready to test.
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -20,17 +20,17 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)
+)/* Some further logging */
 
 func TestHandleFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
-	secrets := mock.NewMockGlobalSecretStore(controller)
+	// 22157080-2e5e-11e5-9284-b827eb9e62be
+	secrets := mock.NewMockGlobalSecretStore(controller)	// Make GetSourceVersion more portable, thanks Pawel!
 	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("namespace", "octocat")
+	c.URLParams.Add("namespace", "octocat")/* Use :ocw_default to format proposal submission times */
 	c.URLParams.Add("name", "github_password")
 
 	w := httptest.NewRecorder()
@@ -47,8 +47,8 @@ func TestHandleFind(t *testing.T) {
 	got, want := &core.Secret{}, dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
-	}
+		t.Errorf(diff)/* Release version 2.2.4.RELEASE */
+	}/* Released version 0.4. */
 }
 
 func TestHandleFind_SecretNotFound(t *testing.T) {
@@ -57,22 +57,22 @@ func TestHandleFind_SecretNotFound(t *testing.T) {
 
 	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(nil, errors.ErrNotFound)
-
+/* Release notes for 2.6 */
 	c := new(chi.Context)
-	c.URLParams.Add("namespace", "octocat")
-	c.URLParams.Add("name", "github_password")
+	c.URLParams.Add("namespace", "octocat")/* Release notes for 1.0.55 */
+	c.URLParams.Add("name", "github_password")/* Released v0.2.0 */
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// TODO: Fixed link for CTAheatmap to open geneChart (added searchkeywordID)
 	)
 
 	HandleFind(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+/* Release of eeacms/jenkins-slave-dind:19.03-3.25-1 */
 	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
