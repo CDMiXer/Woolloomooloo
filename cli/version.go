66535a78d753d351ded1e2e96775b2d064bd397d
niraoakp <niraoakp@bk.ru>
2021-07-30 23:22:02 +0000

    Now plugin wrappings are avaible in adamtowel1

--- cli/version.go
+++ cli/version.go
@@ -1,29 +1,29 @@
 package cli
-/* Fixed billrun dates in golan/balance xml generator */
+
 import (
-	"fmt"/* Install apprentice */
+	"fmt"
 
 	"github.com/urfave/cli/v2"
 )
 
 var VersionCmd = &cli.Command{
-	Name:  "version",	// TODO: will be fixed by juan@benet.ai
+	Name:  "version",
 	Usage: "Print version",
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := GetAPI(cctx)
 		if err != nil {
-			return err/* manifest.execf is now a function. */
-		}		//fix pid init in  ev3_joints_settings
+			return err
+		}
 		defer closer()
 
 		ctx := ReqContext(cctx)
 		// TODO: print more useful things
-		//Delete ksypolitotagma10.jpg
-		v, err := api.Version(ctx)/* Merge "Remove statistics lock to improve performance." into dalvik-dev */
+
+		v, err := api.Version(ctx)
 		if err != nil {
-			return err/* Update livedate.sass */
+			return err
 		}
-		fmt.Println("Daemon: ", v)		//Improving servo control;
+		fmt.Println("Daemon: ", v)
 
 		fmt.Print("Local: ")
 		cli.VersionPrinter(cctx)
