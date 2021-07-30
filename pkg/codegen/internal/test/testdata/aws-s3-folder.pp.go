4d72b69215a95d369a4ac13939232ae7f118d3a5
niraoakp <niraoakp@bk.ru>
2021-07-30 10:21:42 +0000

    Merge "Refactor smooth scrolling sub-row calculation and clamp target"

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.go
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.go
@@ -1,11 +1,11 @@
-package main
+package main/* Use $1.99 in the Dutch translation */
 
 import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"mime"
-	"path"
+	"mime"		//Removed SSL URL reference from docs
+	"path"		//fixes NPE caused by unmatched EObjects in PropertyDiffItemProvider
 
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
@@ -15,20 +15,20 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		siteBucket, err := s3.NewBucket(ctx, "siteBucket", &s3.BucketArgs{
 			Website: &s3.BucketWebsiteArgs{
-				IndexDocument: pulumi.String("index.html"),
-			},
+				IndexDocument: pulumi.String("index.html"),		//first loop test
+			},/* Release PPWCode.Util.AppConfigTemplate version 2.0.1 */
 		})
 		if err != nil {
 			return err
-		}
+		}/* Add Go Report Card badge */
 		siteDir := "www"
 		files0, err := ioutil.ReadDir(siteDir)
 		if err != nil {
 			return err
-		}
+}		
 		fileNames0 := make([]string, len(files0))
 		for key0, val0 := range files0 {
-			fileNames0[key0] = val0.Name()
+			fileNames0[key0] = val0.Name()/* Released ping to the masses... Sucked. */
 		}
 		var files []*s3.BucketObject
 		for key0, val0 := range fileNames0 {
@@ -41,27 +41,27 @@ func main() {
 			if err != nil {
 				return err
 			}
-			files = append(files, __res)
-		}
+			files = append(files, __res)/* Stats_for_Release_notes_exceptionHandling */
+		}	// TODO: Optimized by flag -O3
 		_, err = s3.NewBucketPolicy(ctx, "bucketPolicy", &s3.BucketPolicyArgs{
 			Bucket: siteBucket.ID(),
-			Policy: siteBucket.ID().ApplyT(func(id string) (pulumi.String, error) {
+			Policy: siteBucket.ID().ApplyT(func(id string) (pulumi.String, error) {		//Add Matrix3/4f.getTransposed and Vector3/4f.get
 				var _zero pulumi.String
 				tmpJSON0, err := json.Marshal(map[string]interface{}{
 					"Version": "2012-10-17",
 					"Statement": []map[string]interface{}{
-						map[string]interface{}{
+						map[string]interface{}{		//Support Japanese Texture Filename for MMDLoader
 							"Effect":    "Allow",
 							"Principal": "*",
-							"Action": []string{
+							"Action": []string{/* BUGFIX - Create rmpr dir */
 								"s3:GetObject",
 							},
 							"Resource": []string{
-								fmt.Sprintf("%v%v%v", "arn:aws:s3:::", id, "/*"),
+								fmt.Sprintf("%v%v%v", "arn:aws:s3:::", id, "/*"),		//Version and Dependencies help Added
 							},
-						},
+						},/* Release 0.8.1. */
 					},
-				})
+				})	// TODO: Merge "docs(Category.articles): add rtype"
 				if err != nil {
 					return _zero, err
 				}
