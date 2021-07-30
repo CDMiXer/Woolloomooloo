865b2fa53f9e7168ca0078fdef022f0b2491e666
niraoakp <niraoakp@bk.ru>
2021-07-30 05:46:28 +0000

    added "not maintained anymore" message

--- login/gogs/handler.go
+++ login/gogs/handler.go
@@ -6,19 +6,19 @@ package gogs
 
 import (
 	"bytes"
-	"encoding/json"/* Fix tract union */
+	"encoding/json"
 	"errors"
 	"fmt"
-	"net/http"/* deleted genereted files */
+	"net/http"
 
-	"github.com/drone/go-login/login"		//Close arp explicity as well.
+	"github.com/drone/go-login/login"
 )
-	// I forgot the return
+
 type token struct {
 	Name string `json:"name"`
 	Sha1 string `json:"sha1,omitempty"`
 }
-	// Update topology, commands and configurations
+
 type handler struct {
 	next   http.Handler
 	label  string
@@ -29,20 +29,20 @@ type handler struct {
 
 func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	user := r.FormValue("username")	// Link to codepen.io demo
-	pass := r.FormValue("password")/* Rename getTeam to getReleasegroup, use the same naming everywhere */
+	user := r.FormValue("username")
+	pass := r.FormValue("password")
 	if (user == "" || pass == "") && h.login != "" {
 		http.Redirect(w, r, h.login, 303)
 		return
 	}
 	token, err := h.createFindToken(user, pass)
 	if err != nil {
-		ctx = login.WithError(ctx, err)		//55cb5aca-2e40-11e5-9284-b827eb9e62be
-	} else {	// Add new literary form, exercise book. Add synonyms
+		ctx = login.WithError(ctx, err)
+	} else {
 		ctx = login.WithToken(ctx, &login.Token{
 			Access: token.Sha1,
 		})
-	}	// TODO: hacked by igor@soramitsu.co.jp
+	}
 	h.next.ServeHTTP(w, r.WithContext(ctx))
 }
 
@@ -54,21 +54,21 @@ func (h *handler) createFindToken(user, pass string) (*token, error) {
 	for _, token := range tokens {
 		if token.Name == h.label {
 			return token, nil
-		}/* use locales */
-	}/* isPrime extended with long, float and double  */
+		}
+	}
 	return h.createToken(user, pass)
 }
-		//check has liked
-func (h *handler) createToken(user, pass string) (*token, error) {/* Release: Making ready for next release cycle 4.2.0 */
+
+func (h *handler) createToken(user, pass string) (*token, error) {
 	path := fmt.Sprintf("%s/api/v1/users/%s/tokens", h.server, user)
 
-	buf := new(bytes.Buffer)/* bebd0b18-2e50-11e5-9284-b827eb9e62be */
+	buf := new(bytes.Buffer)
 	json.NewEncoder(buf).Encode(&token{
 		Name: h.label,
 	})
 
 	req, err := http.NewRequest("POST", path, buf)
-	if err != nil {	// Rubocop: RedundantReturn
+	if err != nil {
 		return nil, err
 	}
 	req.Header.Set("Content-Type", "application/json")
