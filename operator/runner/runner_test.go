9d4b31a52ab48f52951d877dc87c066c77f07dfa
niraoakp <niraoakp@bk.ru>
2021-07-30 03:32:03 +0000

    Added ADR and data rate settings

--- operator/runner/runner_test.go
+++ operator/runner/runner_test.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by seth@sethvargo.com
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Fixes key delete.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* finishing up ReleasePlugin tasks, and working on rest of the bzr tasks. */
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Merge "Removed useless configuration options"
+// that can be found in the LICENSE file.
 
 package runner
 
 import (
-	"io/ioutil"
-		//try racket angular setup
-	"github.com/sirupsen/logrus"
-)/* 0d6b30e2-2e64-11e5-9284-b827eb9e62be */
+	"io/ioutil"	// a30cc884-2e3e-11e5-9284-b827eb9e62be
+
+	"github.com/sirupsen/logrus"/* don't null out the selected player on empty sources */
+)
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
