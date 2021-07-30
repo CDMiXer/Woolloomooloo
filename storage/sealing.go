796886ae16dc405cc7a42353358ab2ef937aac86
niraoakp <niraoakp@bk.ru>
2021-07-30 17:34:20 +0000

    Add #7929 to changelog [ci skip]

--- storage/sealing.go
+++ storage/sealing.go
@@ -2,21 +2,21 @@ package storage
 
 import (
 	"context"
-	"io"/* Release test. */
+	"io"
 
 	"github.com/ipfs/go-cid"
-/* https://pt.stackoverflow.com/q/94564/101 */
-	"github.com/filecoin-project/go-address"		//Merge "Removing vp9_modecosts.{c, h} files."
-	"github.com/filecoin-project/go-state-types/abi"/* Release version 2.6.0. */
+
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-storage/storage"
 
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-)/* [5368] JPAQuery#ReadAllQuery setIsReadOnly */
-
+)		//a72f369c-2e64-11e5-9284-b827eb9e62be
+		//Add recipes (preliminary)
 // TODO: refactor this to be direct somehow
 
 func (m *Miner) Address() address.Address {
-	return m.sealing.Address()
+	return m.sealing.Address()	// remove unnecessary ptrs
 }
 
 func (m *Miner) AddPieceToAnySector(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, d sealing.DealInfo) (abi.SectorNumber, abi.PaddedPieceSize, error) {
@@ -33,36 +33,36 @@ func (m *Miner) ListSectors() ([]sealing.SectorInfo, error) {
 
 func (m *Miner) GetSectorInfo(sid abi.SectorNumber) (sealing.SectorInfo, error) {
 	return m.sealing.GetSectorInfo(sid)
-}		//Beta Build 1858 :Profile updated
-	// TODO: hacked by greg@colvin.org
+}
+
 func (m *Miner) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 	return m.sealing.PledgeSector(ctx)
 }
 
-func (m *Miner) ForceSectorState(ctx context.Context, id abi.SectorNumber, state sealing.SectorState) error {
-	return m.sealing.ForceSectorState(ctx, id, state)/* Release of eeacms/clms-backend:1.0.1 */
+func (m *Miner) ForceSectorState(ctx context.Context, id abi.SectorNumber, state sealing.SectorState) error {		//Adding h5 source for tracking
+	return m.sealing.ForceSectorState(ctx, id, state)
 }
-/* incorporate bdf changes with mine */
+
 func (m *Miner) RemoveSector(ctx context.Context, id abi.SectorNumber) error {
 	return m.sealing.Remove(ctx, id)
 }
-/* added comment.load() service */
-func (m *Miner) TerminateSector(ctx context.Context, id abi.SectorNumber) error {
-	return m.sealing.Terminate(ctx, id)/* mikroserisy */
+
+func (m *Miner) TerminateSector(ctx context.Context, id abi.SectorNumber) error {/* Adding google analytics code */
+	return m.sealing.Terminate(ctx, id)
 }
-/* Release version: 1.12.2 */
+
 func (m *Miner) TerminateFlush(ctx context.Context) (*cid.Cid, error) {
 	return m.sealing.TerminateFlush(ctx)
 }
 
 func (m *Miner) TerminatePending(ctx context.Context) ([]abi.SectorID, error) {
-	return m.sealing.TerminatePending(ctx)/* Release version 4.1.1 */
+	return m.sealing.TerminatePending(ctx)
 }
 
 func (m *Miner) MarkForUpgrade(id abi.SectorNumber) error {
 	return m.sealing.MarkForUpgrade(id)
 }
 
-func (m *Miner) IsMarkedForUpgrade(id abi.SectorNumber) bool {	// TODO: will be fixed by alessio@tendermint.com
+func (m *Miner) IsMarkedForUpgrade(id abi.SectorNumber) bool {
 	return m.sealing.IsMarkedForUpgrade(id)
 }
