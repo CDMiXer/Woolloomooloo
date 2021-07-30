04e178415965853e97aeb807fc279bdc7850289e
niraoakp <niraoakp@bk.ru>
2021-07-30 14:48:27 +0000

    -trying to fix fragmentation / udp performance, not working yet

--- pkg/codegen/go/gen_program_ternaries.go
+++ pkg/codegen/go/gen_program_ternaries.go
@@ -1,67 +1,67 @@
-package gen/* Adding changes to the README file */
+package gen
 
-import (
-	"fmt"
+import (		//fix: product link was added to cart on add
+	"fmt"		//Merge "Validate TemplateResource schema when based on other templates"
 
-	"github.com/hashicorp/hcl/v2"/* Released v0.0.14  */
+	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"/* ENH: Added a memory-conservation filter mode. */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
 
 type ternaryTemp struct {
-	Name  string/* Update SeReleasePolicy.java */
+	Name  string	// Update TabPanels.js
 	Value *model.ConditionalExpression
 }
 
 func (tt *ternaryTemp) Type() model.Type {
-	return tt.Value.Type()
+	return tt.Value.Type()		//all done, cleanup next
 }
-
+/* Fix wiremock race issue */
 func (tt *ternaryTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
 	return tt.Type().Traverse(traverser)
-}	// removed semicolons on json output
+}
 
 func (tt *ternaryTemp) SyntaxNode() hclsyntax.Node {
 	return syntax.None
 }
 
-type tempSpiller struct {/* Added the two new bundles as dependencies to client. */
+type tempSpiller struct {
 	temps []*ternaryTemp
 	count int
 }
 
 func (ta *tempSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {
-	var temp *ternaryTemp		//Update RK URF Buffs.lua
-	switch x := x.(type) {		//Validate default value on build
+	var temp *ternaryTemp
+	switch x := x.(type) {/* Expanded installation section on Readme */
 	case *model.ConditionalExpression:
 		x.Condition, _ = ta.spillExpression(x.Condition)
-		x.TrueResult, _ = ta.spillExpression(x.TrueResult)
-		x.FalseResult, _ = ta.spillExpression(x.FalseResult)/* Release dhcpcd-6.6.1 */
+		x.TrueResult, _ = ta.spillExpression(x.TrueResult)/* Add a notes about multiple links/targets, remove multiple link example */
+		x.FalseResult, _ = ta.spillExpression(x.FalseResult)
 
 		temp = &ternaryTemp{
-			Name:  fmt.Sprintf("tmp%d", ta.count),		//Automatic changelog generation for PR #30182 [ci skip]
+			Name:  fmt.Sprintf("tmp%d", ta.count),
 			Value: x,
 		}
 		ta.temps = append(ta.temps, temp)
 		ta.count++
-	default:		//Added more right click functionality, and fixed and tweaked loading.
+	default:
 		return x, nil
-	}/* implemented Methods */
+	}	// TODO: will be fixed by ng8eke@163.com
 	return &model.ScopeTraversalExpression{
 		RootName:  temp.Name,
-		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},/* 4.0.1 Release */
+		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},
 		Parts:     []model.Traversable{temp},
 	}, nil
-}/* Merge "docs: SDK/ADT r20.0.1, NDK r8b, Platform 4.1.1 Release Notes" into jb-dev */
-		//- silence unittest_helper:prepare_config/1
-func (g *generator) rewriteTernaries(/* Create 4_Add Two Numbers */
-	x model.Expression,
+}
+
+func (g *generator) rewriteTernaries(
+	x model.Expression,/* arrow heads adjusted */
 	spiller *tempSpiller,
 ) (model.Expression, []*ternaryTemp, hcl.Diagnostics) {
-	spiller.temps = nil
-	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)
+	spiller.temps = nil/* reconfiguring doc section layout */
+	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)/* Added mumble update */
 
-	return x, spiller.temps, diags
+	return x, spiller.temps, diags/* rename "series" to "ubuntuRelease" */
 
 }
