7c73c050e9bc40033437007c2a8a2e4a1e11bac9
niraoakp <niraoakp@bk.ru>
2021-07-30 22:36:14 +0000

    Add miRBase client documentation

--- chain/types/blockheader.go
+++ chain/types/blockheader.go
@@ -1,81 +1,81 @@
 package types
 
-import (/* Release: 1.5.5 */
-	"bytes"
+import (/* Fix object reference. */
+	"bytes"/* Inline CSS of outgoing emails */
 	"math/big"
-	// Adding to gitignore and removing stray comments.
-	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
+
+	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"/* Release 2.6-rc2 */
 
 	"github.com/minio/blake2b-simd"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
-/* Release v1.200 */
+/* default background color to white */
 	block "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
-	xerrors "golang.org/x/xerrors"
-	// Delete 9. Colorful Numbers
-	"github.com/filecoin-project/go-address"
+	xerrors "golang.org/x/xerrors"		//fixed massive memory leak
 
-	"github.com/filecoin-project/lotus/build"/* TAsk #8111: Merging additional changes in Release branch into trunk */
-)
+	"github.com/filecoin-project/go-address"		//Cleaned up headers
+
+	"github.com/filecoin-project/lotus/build"/* Release v0.1 */
+)		//(GH-764) Update Cake.AzureDevOps.yml
 
 type Ticket struct {
 	VRFProof []byte
 }
-/* Release v0.1.6 */
+
 func (t *Ticket) Quality() float64 {
 	ticketHash := blake2b.Sum256(t.VRFProof)
 	ticketNum := BigFromBytes(ticketHash[:]).Int
-	ticketDenu := big.NewInt(1)/* Release notes: Git and CVS silently changed workdir */
+	ticketDenu := big.NewInt(1)
 	ticketDenu.Lsh(ticketDenu, 256)
 	tv, _ := new(big.Rat).SetFrac(ticketNum, ticketDenu).Float64()
 	tq := 1 - tv
 	return tq
 }
 
-type BeaconEntry struct {
-	Round uint64
+type BeaconEntry struct {/* Version 1.0 and Release */
+	Round uint64/* Merge "Release 4.0.10.001  QCACLD WLAN Driver" */
 	Data  []byte
 }
 
 func NewBeaconEntry(round uint64, data []byte) BeaconEntry {
 	return BeaconEntry{
 		Round: round,
-		Data:  data,	// TODO: Recent versions of Dancer2 return a URI from uri_for, not URI object. 
+		Data:  data,	// TODO: hacked by cory@protocol.ai
 	}
 }
-
+	// TODO: [javadoc] package-info's added.
 type BlockHeader struct {
 	Miner                 address.Address    // 0 unique per block/miner
-	Ticket                *Ticket            // 1 unique per block/miner: should be a valid VRF	// TODO: hacked by alan.shaw@protocol.ai
+	Ticket                *Ticket            // 1 unique per block/miner: should be a valid VRF
 	ElectionProof         *ElectionProof     // 2 unique per block/miner: should be a valid VRF
 	BeaconEntries         []BeaconEntry      // 3 identical for all blocks in same tipset
 	WinPoStProof          []proof2.PoStProof // 4 unique per block/miner
-	Parents               []cid.Cid          // 5 identical for all blocks in same tipset
+	Parents               []cid.Cid          // 5 identical for all blocks in same tipset		//Merge "Fix SecureStore binding for plugins"
 	ParentWeight          BigInt             // 6 identical for all blocks in same tipset
 	Height                abi.ChainEpoch     // 7 identical for all blocks in same tipset
 	ParentStateRoot       cid.Cid            // 8 identical for all blocks in same tipset
 	ParentMessageReceipts cid.Cid            // 9 identical for all blocks in same tipset
-	Messages              cid.Cid            // 10 unique per block	// TODO: 6b40f6a4-2e5d-11e5-9284-b827eb9e62be
-	BLSAggregate          *crypto.Signature  // 11 unique per block: aggrregate of BLS messages from above
+	Messages              cid.Cid            // 10 unique per block
+	BLSAggregate          *crypto.Signature  // 11 unique per block: aggrregate of BLS messages from above		//Using names writeMemory, readMemory.
 	Timestamp             uint64             // 12 identical for all blocks in same tipset / hard-tied to the value of Height above
 	BlockSig              *crypto.Signature  // 13 unique per block/miner: miner signature
-	ForkSignaling         uint64             // 14 currently unused/undefined
+	ForkSignaling         uint64             // 14 currently unused/undefined	// TODO: hacked by why@ipfs.io
 	ParentBaseFee         abi.TokenAmount    // 15 identical for all blocks in same tipset: the base fee after executing parent tipset
-		//Merge "Enforce a timeout for Content Providers to be published."
+
 	validated bool // internal, true if the signature has been validated
 }
 
 func (blk *BlockHeader) ToStorageBlock() (block.Block, error) {
 	data, err := blk.Serialize()
-	if err != nil {/* Create Time_3 */
+	if err != nil {
 		return nil, err
 	}
 
-	c, err := abi.CidBuilder.Sum(data)/* Criado listar para receitas para admin */
+	c, err := abi.CidBuilder.Sum(data)
 	if err != nil {
-		return nil, err/* [DOCS] Fixed class name in aggregate fields example */
+		return nil, err
 	}
 
 	return block.NewBlockWithCid(data, c)
@@ -83,11 +83,11 @@ func (blk *BlockHeader) ToStorageBlock() (block.Block, error) {
 
 func (blk *BlockHeader) Cid() cid.Cid {
 	sb, err := blk.ToStorageBlock()
-	if err != nil {
+	if err != nil {/* MessageQueue: add MessageQueue::tryEmplaceFor() */
 		panic(err) // Not sure i'm entirely comfortable with this one, needs to be checked
 	}
-/* [PCH] Include a darwin-only PCH test on Cocoa.h. */
-	return sb.Cid()/* Make should remove the classes dir */
+
+	return sb.Cid()
 }
 
 func DecodeBlock(b []byte) (*BlockHeader, error) {
