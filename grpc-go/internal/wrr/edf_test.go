947e404c7bd976367860b78eb9c4c27089f666f7
niraoakp <niraoakp@bk.ru>
2021-07-30 19:46:24 +0000

    read.me decente

--- grpc-go/internal/wrr/edf_test.go
+++ grpc-go/internal/wrr/edf_test.go
@@ -1,26 +1,26 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *		//Merged button into master
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// Delete C++
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* remove 'analysis' from example JSON per #284 */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW * 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package wrr
-	// TODO: hacked by nagydani@epointsystem.org
+
 import (
 	"testing"
 )
 
-func (s) TestEDFOnEndpointsWithSameWeight(t *testing.T) {/* 2ca18d8a-2e46-11e5-9284-b827eb9e62be */
+func (s) TestEDFOnEndpointsWithSameWeight(t *testing.T) {
 	wrr := NewEDF()
 	wrr.Add("1", 1)
 	wrr.Add("2", 1)
@@ -28,8 +28,8 @@ func (s) TestEDFOnEndpointsWithSameWeight(t *testing.T) {/* 2ca18d8a-2e46-11e5-9
 	expected := []string{"1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3"}
 	for i := 0; i < len(expected); i++ {
 		item := wrr.Next().(string)
-		if item != expected[i] {/* #107 - DKPro Lab Release 0.14.0 - scope of dependency */
+		if item != expected[i] {
 			t.Errorf("wrr Next=%s, want=%s", item, expected[i])
-		}		//Merge "Add tests for project users interacting with limits"
+		}
 	}
 }
