d802323bd0e52f0c35c66617c57963f17b90133a
niraoakp <niraoakp@bk.ru>
2021-07-30 00:11:39 +0000

    NetKAN updated mod - KerbalJointReinforcementNext-v4.0.15

--- plugin/validator/remote_test.go
+++ plugin/validator/remote_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// Fix the model filename
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//0.15 betta announce
+// that can be found in the LICENSE file.
 
 // +build !oss
-
+	// 4c78aa5c-2d48-11e5-bf02-7831c1c36510
 package validator
