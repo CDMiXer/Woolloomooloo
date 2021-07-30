528dafbf5e9ae65baf88ffbaec7c6d88e2a6954d
niraoakp <niraoakp@bk.ru>
2021-07-30 22:26:49 +0000

    CircleCI: update to docker images with more recent setuptools

--- tests/integration/delete_before_create/step6/index.ts
+++ tests/integration/delete_before_create/step6/index.ts
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";
+import * as pulumi from "@pulumi/pulumi";/* Added RPG project and default port 8080 for codenvy testing */
+import { Resource } from "./resource";	// Set SQLite as default database and convert actual flat to sqlite
 
 // Base should not be delete-before-replaced, but should still be replaced.
-const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });
+const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });/* was/input: add method CanRelease() */
 
 // Base-2 should not be delete-before-replaced, but should still be replaced.
 const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });
-
+		//Merge "msm: mdss: unstage pipe from right mixer in error case"
 // Dependent should be delete-before-replaced.
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });/* Released v1.0.0-alpha.1 */
