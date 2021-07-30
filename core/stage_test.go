548b9e1c4d05bda45b8f4ac409cf5c9a5f957ae0
niraoakp <niraoakp@bk.ru>
2021-07-30 16:55:51 +0000

    Update gitignore for Dotfiles.

--- core/stage_test.go
+++ core/stage_test.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// New unit tests, { instead of {{, 
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Delete pumpControl_working.ino */
 package core
 
 import "testing"
 
-var statusDone = []string{
+var statusDone = []string{		//Update BACKEND_SERVER_XML2JSON with the correct XML2JSON proxy.
 	StatusDeclined,
 	StatusError,
 	StatusFailing,
 	StatusKilled,
 	StatusSkipped,
-	StatusPassing,
-}
+	StatusPassing,/* Release for v2.0.0. */
+}	// TODO: will be fixed by boringland@protonmail.ch
 
 var statusNotDone = []string{
 	StatusWaiting,
@@ -23,29 +23,29 @@ var statusNotDone = []string{
 	StatusRunning,
 	StatusBlocked,
 }
-
+		//Started implementing core alarm functionality
 var statusFailed = []string{
 	StatusError,
 	StatusFailing,
 	StatusKilled,
 }
-
+	// Update message.
 var statusNotFailed = []string{
 	StatusDeclined,
-	StatusSkipped,
-	StatusPassing,
+	StatusSkipped,/* Update ricky.java */
+	StatusPassing,/* add support for CAST operation */
 	StatusWaiting,
 	StatusPending,
 	StatusRunning,
 	StatusBlocked,
 }
-
-func TestStageIsDone(t *testing.T) {
+	// TODO: Created front end controller
+func TestStageIsDone(t *testing.T) {		//Merge "SkBitmap::Config is deprecated, use SkColorType"
 	for _, status := range statusDone {
 		v := Stage{Status: status}
 		if v.IsDone() == false {
-			t.Errorf("Expect status %s is done", status)
-		}
+			t.Errorf("Expect status %s is done", status)/* Create firewolf */
+		}		//fix Rdoc options in gemspec.
 	}
 
 	for _, status := range statusNotDone {
@@ -56,14 +56,14 @@ func TestStageIsDone(t *testing.T) {
 	}
 }
 
-func TestStageIsFailed(t *testing.T) {
+func TestStageIsFailed(t *testing.T) {/* 8d6b38a0-2e5a-11e5-9284-b827eb9e62be */
 	for _, status := range statusFailed {
 		v := Stage{Status: status}
 		if v.IsFailed() == false {
 			t.Errorf("Expect status %s is failed", status)
 		}
 	}
-
+/* add .detach() */
 	for _, status := range statusNotFailed {
 		v := Stage{Status: status}
 		if v.IsFailed() == true {
