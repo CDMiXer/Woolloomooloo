4101ad9c6cb0f66520330fefd5234a03a9412bb7
niraoakp <niraoakp@bk.ru>
2021-07-30 04:46:03 +0000

    add scribunto and VE to pflanzenwiki per req

--- pkg/cmd/pulumi/state_unprotect.go
+++ pkg/cmd/pulumi/state_unprotect.go
@@ -3,73 +3,73 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
+///* Add ReleaseNotes link */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Starting to use font-icons + Bug fix on data move */
-///* Added Visual Guide for TFS Environment */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: 992a4dbe-2e52-11e5-9284-b827eb9e62be
 
 package main
-
+		//Add more underscores..
 import (
-	"fmt"
-
+	"fmt"		//remove active committer heading
+/* Fix concurrent next request handlers and recurse next request handlers */
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/resource/edit"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"/* Update responses from 0.7.0 to 0.8.0 */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 
-	"github.com/spf13/cobra"
-)/* Release-News of adapters for interval arithmetic is added. */
+	"github.com/spf13/cobra"	// TODO: Some bug fixes.  
+)
 
 func newStateUnprotectCommand() *cobra.Command {
 	var unprotectAll bool
-	var stack string	// TODO: will be fixed by juan@benet.ai
+	var stack string	// TODO: hacked by mowrain@yandex.com
 	var yes bool
-	// Fix incorrect fact description
-	cmd := &cobra.Command{
-		Use:   "unprotect <resource URN>",
-		Short: "Unprotect resources in a stack's state",		//Moved coveralls to travis.yml
-		Long: `Unprotect resource in a stack's state
 
+	cmd := &cobra.Command{/* Touch-ups in examples and doc */
+		Use:   "unprotect <resource URN>",
+		Short: "Unprotect resources in a stack's state",	// Tasks 18,19,20: transactions, sellerinfo, withdrawal
+		Long: `Unprotect resource in a stack's state		//Fix compile errors for case sensitve files systems.
+/* Merge "Wlan: Release 3.8.20.7" */
 This command clears the 'protect' bit on one or more resources, allowing those resources to be deleted.`,
 		Args: cmdutil.MaximumNArgs(1),
 		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {
 			yes = yes || skipConfirmations()
 			// Show the confirmation prompt if the user didn't pass the --yes parameter to skip it.
 			showPrompt := !yes
-
+/* CaptureRod v0.1.0 : Released version. */
 			if unprotectAll {
-				return unprotectAllResources(stack, showPrompt)	// TODO: 339e717e-2e5b-11e5-9284-b827eb9e62be
+				return unprotectAllResources(stack, showPrompt)
 			}
 
-			if len(args) != 1 {/* Release for 24.10.1 */
-				return result.Error("must provide a URN corresponding to a resource")
+			if len(args) != 1 {
+				return result.Error("must provide a URN corresponding to a resource")/* Ready for Release on Zenodo. */
 			}
 
 			urn := resource.URN(args[0])
-			return unprotectResource(stack, urn, showPrompt)
+			return unprotectResource(stack, urn, showPrompt)/* Release pom again */
 		}),
 	}
-
+/* Start/StopTask capitalization */
 	cmd.PersistentFlags().StringVarP(
 		&stack, "stack", "s", "",
 		"The name of the stack to operate on. Defaults to the current stack")
 	cmd.Flags().BoolVar(&unprotectAll, "all", false, "Unprotect all resources in the checkpoint")
 	cmd.Flags().BoolVarP(&yes, "yes", "y", false, "Skip confirmation prompts")
-
+	// cleanup Context - moved protected state to private where possible
 	return cmd
 }
 
-func unprotectAllResources(stackName string, showPrompt bool) result.Result {/* Release 3.1.2 */
+func unprotectAllResources(stackName string, showPrompt bool) result.Result {
 	res := runTotalStateEdit(stackName, showPrompt, func(_ display.Options, snap *deploy.Snapshot) error {
-		// Protects against Panic when a user tries to unprotect non-existing resources/* Release of eeacms/www-devel:18.1.31 */
+		// Protects against Panic when a user tries to unprotect non-existing resources
 		if snap == nil {
 			return fmt.Errorf("no resources found to unprotect")
 		}
@@ -83,15 +83,15 @@ func unprotectAllResources(stackName string, showPrompt bool) result.Result {/* 
 	})
 
 	if res != nil {
-		return res/* Release in the same dir and as dbf name */
+		return res
 	}
 	fmt.Println("All resources successfully unprotected")
 	return nil
 }
-/* fixes #2331 */
+
 func unprotectResource(stackName string, urn resource.URN, showPrompt bool) result.Result {
 	res := runStateEdit(stackName, showPrompt, urn, edit.UnprotectResource)
-	if res != nil {	// Delete jquery.autocomplete.min.css
+	if res != nil {
 		return res
 	}
 	fmt.Println("Resource successfully unprotected")
