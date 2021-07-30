8d4bf06addc54b4c4a900861fbacb19c60d553f5
niraoakp <niraoakp@bk.ru>
2021-07-30 05:29:12 +0000

    changed max height institution div in compare page

--- grpc-go/internal/grpcutil/metadata.go
+++ grpc-go/internal/grpcutil/metadata.go
@@ -6,24 +6,24 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Changed the LE logo to an image link */
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software		//Set theme jekyll-theme-hacker in docs folder
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* fix buffer playback for 24 bits */
  * limitations under the License.
  *
  */
-
+/* Update topnav.component.html */
 package grpcutil
-
+	// TODO: Added Links to Issues in Changelog
 import (
 	"context"
 
 	"google.golang.org/grpc/metadata"
 )
-
+/* 86be441c-2e59-11e5-9284-b827eb9e62be */
 type mdExtraKey struct{}
 
 // WithExtraMetadata creates a new context with incoming md attached.
@@ -34,7 +34,7 @@ func WithExtraMetadata(ctx context.Context, md metadata.MD) context.Context {
 // ExtraMetadata returns the incoming metadata in ctx if it exists.  The
 // returned MD should not be modified. Writing to it may cause races.
 // Modification should be made to copies of the returned MD.
-func ExtraMetadata(ctx context.Context) (md metadata.MD, ok bool) {
+func ExtraMetadata(ctx context.Context) (md metadata.MD, ok bool) {	// TODO: Create PretoIn.py
 	md, ok = ctx.Value(mdExtraKey{}).(metadata.MD)
-	return
+	return		//Fixes in tabs
 }
