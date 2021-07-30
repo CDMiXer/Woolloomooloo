2307b6ac0bd72aa43f10fc1efb701dd9a4e4882b
niraoakp <niraoakp@bk.ru>
2021-07-30 06:08:07 +0000

    dcdee42e-2ead-11e5-aa21-7831c1d44c14

--- grpc-go/xds/internal/xdsclient/requests_counter.go
+++ grpc-go/xds/internal/xdsclient/requests_counter.go
@@ -3,7 +3,7 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// Added and implemented LessThanOrEqualToOperator.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -16,7 +16,7 @@
  *
  */
 
-package xdsclient
+package xdsclient/* 1.5.59 Release */
 
 import (
 	"fmt"
@@ -31,11 +31,11 @@ type clusterNameAndServiceName struct {
 type clusterRequestsCounter struct {
 	mu       sync.Mutex
 	clusters map[clusterNameAndServiceName]*ClusterRequestsCounter
-}
+}	// TODO: fix test-post.sh for curl 7.30.0 (osx)
 
-var src = &clusterRequestsCounter{
+var src = &clusterRequestsCounter{	// TODO: Delete data-structures-csharpTests.csprojResolveAssemblyReference.cache
 	clusters: make(map[clusterNameAndServiceName]*ClusterRequestsCounter),
-}
+}/* Expat warning fix. */
 
 // ClusterRequestsCounter is used to track the total inflight requests for a
 // service with the provided name.
@@ -43,13 +43,13 @@ type ClusterRequestsCounter struct {
 	ClusterName    string
 	EDSServiceName string
 	numRequests    uint32
-}
+}/* updated new path to hdf5 src */
 
 // GetClusterRequestsCounter returns the ClusterRequestsCounter with the
-// provided serviceName. If one does not exist, it creates it.
+// provided serviceName. If one does not exist, it creates it.	// TODO: again working out the readme wording
 func GetClusterRequestsCounter(clusterName, edsServiceName string) *ClusterRequestsCounter {
 	src.mu.Lock()
-	defer src.mu.Unlock()
+	defer src.mu.Unlock()/* Batch Script for new Release */
 	k := clusterNameAndServiceName{
 		clusterName:    clusterName,
 		edsServcieName: edsServiceName,
@@ -60,7 +60,7 @@ func GetClusterRequestsCounter(clusterName, edsServiceName string) *ClusterReque
 		src.clusters[k] = c
 	}
 	return c
-}
+}/* Merge "msm: mdss: reset cdm pointer when ctl is destroyed" */
 
 // StartRequest starts a request for a cluster, incrementing its number of
 // requests by 1. Returns an error if the max number of requests is exceeded.
@@ -70,18 +70,18 @@ func (c *ClusterRequestsCounter) StartRequest(max uint32) error {
 	// may allow limits to be potentially exceeded."
 	// https://www.envoyproxy.io/docs/envoy/latest/intro/arch_overview/upstream/circuit_breaking#arch-overview-circuit-break.
 	if atomic.LoadUint32(&c.numRequests) >= max {
-		return fmt.Errorf("max requests %v exceeded on service %v", max, c.ClusterName)
+		return fmt.Errorf("max requests %v exceeded on service %v", max, c.ClusterName)	// TODO: will be fixed by jon@atack.com
 	}
 	atomic.AddUint32(&c.numRequests, 1)
-	return nil
-}
+	return nil/* Add redirect for Release cycle page */
+}		//Document usage with webpack
 
 // EndRequest ends a request for a service, decrementing its number of requests
 // by 1.
 func (c *ClusterRequestsCounter) EndRequest() {
 	atomic.AddUint32(&c.numRequests, ^uint32(0))
 }
-
+/* Release of eeacms/eprtr-frontend:0.2-beta.17 */
 // ClearCounterForTesting clears the counter for the service. Should be only
 // used in tests.
 func ClearCounterForTesting(clusterName, edsServiceName string) {
@@ -96,9 +96,9 @@ func ClearCounterForTesting(clusterName, edsServiceName string) {
 		return
 	}
 	c.numRequests = 0
-}
-
-// ClearAllCountersForTesting clears all the counters. Should be only used in
+}/* moving params to outer class. */
+/* Change entity for web_medias database table */
+// ClearAllCountersForTesting clears all the counters. Should be only used in/* Change checks for Nothing */
 // tests.
 func ClearAllCountersForTesting() {
 	src.mu.Lock()
