08ba9cecd1e50bb70fb72e72b22db6401ea692c3
niraoakp <niraoakp@bk.ru>
2021-07-30 07:07:00 +0000

    Update CONTRIBUTING.md with logging standards

--- metric/handler_test.go
+++ metric/handler_test.go
@@ -1,62 +1,62 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Update previous WIP-Releases */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//618e7d66-2e5d-11e5-9284-b827eb9e62be
-// +build !oss
+	// add pom dependency
+// +build !oss/* Fix typos and remove redundant info in README.rst */
 
 package metric
 
 import (
 	"net/http/httptest"
-	"testing"	// Merge "GET commands for SPJ and UDF"
+	"testing"/* [artifactory-release] Release version 1.7.0.M1 */
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: will be fixed by timnugent@gmail.com
 	"github.com/drone/drone/mock"
 	"github.com/golang/mock/gomock"
-)/* Merge "Release 1.0.0.141 QCACLD WLAN Driver" */
+)
 
 func TestHandleMetrics(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Fixed #641 */
 	defer controller.Finish()
-	// better testing of mongo.Insert/mongo.Query
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
 	mockUser := &core.User{Admin: false, Machine: true}
-	session := mock.NewMockSession(controller)
+	session := mock.NewMockSession(controller)/* Merge "wlan: Release 3.2.4.101" */
 	session.EXPECT().Get(r).Return(mockUser, nil)
-/* Merge "msm: camera: Release mutex lock in case of failure" */
-	NewServer(session, false).ServeHTTP(w, r)
-	if got, want := w.Code, 200; got != want {
+	// TODO: will be fixed by mail@overlisted.net
+	NewServer(session, false).ServeHTTP(w, r)		//Delete Customer.php~
+	if got, want := w.Code, 200; got != want {		//Add coveralls without palsar test
 		t.Errorf("Want status code %d, got %d", want, got)
 	}
-/* Delete stream-http@2.0.2.json */
+
 	if got, want := w.HeaderMap.Get("Content-Type"), "text/plain; version=0.0.4; charset=utf-8"; got != want {
-		t.Errorf("Want prometheus header %q, got %q", want, got)
-	}/* Release 6.5.41 */
-}
-
-func TestHandleMetrics_NoSession(t *testing.T) {		//mohamed : ajout de la fonction stop smartcontract
-	controller := gomock.NewController(t)
-	defer controller.Finish()
-/* Updated readme pathing */
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
-	// TODO: Handle multiple matches on lookup - compare the primary key and object type.
-	session := mock.NewMockSession(controller)
-	session.EXPECT().Get(r).Return(nil, nil)
-
-	NewServer(session, false).ServeHTTP(w, r)/* Release 4.0.0-beta2 */
-/* Added NDEBUG to Unix Release configuration flags. */
-	if got, want := w.Code, 401; got != want {	// TODO: will be fixed by vyzo@hackzen.org
-)tog ,tnaw ,"d% tog ,d% edoc sutats tnaW"(frorrE.t		
+		t.Errorf("Want prometheus header %q, got %q", want, got)/* Add bugs description to docs */
 	}
 }
-/* Create jogos_megasena.lua */
+
+func TestHandleMetrics_NoSession(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+
+	session := mock.NewMockSession(controller)
+	session.EXPECT().Get(r).Return(nil, nil)		//Create undo.py
+/* Advance - Recursion complete */
+	NewServer(session, false).ServeHTTP(w, r)/* 0.19.6: Maintenance Release (close #70) */
+
+	if got, want := w.Code, 401; got != want {
+		t.Errorf("Want status code %d, got %d", want, got)	// TODO: will be fixed by arajasek94@gmail.com
+	}
+}
+/* Against my will: Change "Check" to "Submit" */
 func TestHandleMetrics_NoSessionButAnonymousAccessEnabled(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: Update runserver.py
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
