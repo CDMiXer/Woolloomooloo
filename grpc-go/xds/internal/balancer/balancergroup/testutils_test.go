fb94c98ce0f2e645cff129879165ce404426f448
niraoakp <niraoakp@bk.ru>
2021-07-30 13:56:15 +0000

    change name from QueryTest to ServiceQueryTest

--- grpc-go/xds/internal/balancer/balancergroup/testutils_test.go
+++ grpc-go/xds/internal/balancer/balancergroup/testutils_test.go
@@ -1,4 +1,4 @@
-21.1og dliub+ //
+// +build go1.12
 
 /*
  *
@@ -6,29 +6,29 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//first implementation of the unitils-io refactor to the new core .
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		// Bug#12744991 - DECIMAL_ROUND(X,D) GIVES WRONG RESULTS WHEN D == N*(-9)
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: will be fixed by davidad@alum.mit.edu
+ * limitations under the License.
  *
- */	// TODO: Merge "Update policy.json example for grizzly vol changes"
+ */
 
 package balancergroup
-/* Update CHANGELOG for PR #2201 [skip ci] */
+
 import (
-	"testing"/* Release tag: version 0.6.3. */
-	// TODO: will be fixed by juan@benet.ai
+	"testing"
+
 	"google.golang.org/grpc/internal/grpctest"
 )
 
 type s struct {
 	grpctest.Tester
-}		//added cleanupPublishFolder
+}
 
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
