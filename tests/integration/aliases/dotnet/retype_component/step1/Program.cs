dfdffdbb112fcedab59ae49fcd8a776a1a231472
niraoakp <niraoakp@bk.ru>
2021-07-30 16:39:14 +0000

    Poprawki slashy na końcu w przekierowaniach

--- tests/integration/aliases/dotnet/retype_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step1/Program.cs
@@ -1,35 +1,35 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.	// service: get options name from string
 
-using System.Threading.Tasks;
+using System.Threading.Tasks;	// TODO: hacked by timnugent@gmail.com
 using Pulumi;
 
-class Resource : ComponentResource	// TODO: hacked by julia@jvns.ca
-{
-    public Resource(string name, ComponentResourceOptions options = null)/* Release version 0.8.6 */
+class Resource : ComponentResource
+{/* finish test mock */
+    public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
-    {
-    }	// TODO: hacked by mail@bitpshr.net
-}	// TODO: Hom budget complete
+    {	// fix "concern" typo!
+    }
+}
 
 // Scenario #4 - change the type of a component
-class ComponentFour : ComponentResource/* Preparing gradle.properties for Release */
+class ComponentFour : ComponentResource
 {
-    private Resource resource;
+    private Resource resource;/* Folder structure of biojava3 project adjusted to requirements of ReleaseManager. */
 
     public ComponentFour(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentFour", name, options)
     {
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
-    }
-}
-/* Docummentation */
+}    
+}		//83c38dc0-2e73-11e5-9284-b827eb9e62be
+
 class Program
 {
     static Task<int> Main(string[] args)
-    {
+    {		//updated readme to show new port in runserver.py
         return Deployment.RunAsync(() => 
         {
             var comp4 = new ComponentFour("comp4");
         });
     }
-}
+}	// TODO: hacked by zaq1tomo@gmail.com
