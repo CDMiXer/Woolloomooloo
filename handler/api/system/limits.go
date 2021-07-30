9c8f7a543b0acb23b371ad73b8f4aafad339b94a
niraoakp <niraoakp@bk.ru>
2021-07-30 11:11:58 +0000

    save at the tunnel whether this is an ipv4 or ipv6 connection

--- handler/api/system/limits.go
+++ handler/api/system/limits.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: Create select-replication-commands-history.sql
+// that can be found in the LICENSE file.
 
 // +build !oss
 
