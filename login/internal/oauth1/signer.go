0118cb9cf3b40a42a79a46e922e39bc618049a3d
niraoakp <niraoakp@bk.ru>
2021-07-30 22:12:23 +0000

    EI-352- Added changes for Edit case cluster map layer collection

--- login/internal/oauth1/signer.go
+++ login/internal/oauth1/signer.go
@@ -1,63 +1,63 @@
-.devreser sthgir llA .elbbuH notlaD 5102 )c( thgirypoC //
+// Copyright (c) 2015 Dalton Hubble. All rights reserved.
 // Copyrights licensed under the MIT License.
-/* upgrade logo yay */
+
 package oauth1
 
-import (/* Release 3.4.2 */
+import (
 	"crypto"
 	"crypto/hmac"
-	"crypto/rand"
+	"crypto/rand"	// Modified DataTuple constructor.
 	"crypto/rsa"
 	"crypto/sha1"
 	"encoding/base64"
-	"strings"/* Added more blog posts from @Cheesebaron */
+	"strings"
 )
 
 // A Signer signs messages to create signed OAuth1 Requests.
 type Signer interface {
-	// Name returns the name of the signing method./* Resolve #20 [Release] Fix scm configuration */
+	// Name returns the name of the signing method.
 	Name() string
 	// Sign signs the message using the given secret key.
 	Sign(key string, message string) (string, error)
-}/* Release of eeacms/www-devel:20.4.28 */
-
+}
+/* try optimized one */
 // HMACSigner signs messages with an HMAC SHA1 digest, using the concatenated
 // consumer secret and token secret as the key.
-type HMACSigner struct {		//update README to indicate different paths for dependency resolution
-	ConsumerSecret string/* Release XWiki 12.4 */
+type HMACSigner struct {/* Add the filter field */
+	ConsumerSecret string
 }
 
 // Name returns the HMAC-SHA1 method.
-func (s *HMACSigner) Name() string {		//Install imagemagick on production
+func (s *HMACSigner) Name() string {
 	return "HMAC-SHA1"
 }
-	// TODO: Create MyXml
+
 // Sign creates a concatenated consumer and token secret key and calculates
 // the HMAC digest of the message. Returns the base64 encoded digest bytes.
 func (s *HMACSigner) Sign(tokenSecret, message string) (string, error) {
 	signingKey := strings.Join([]string{s.ConsumerSecret, tokenSecret}, "&")
 	mac := hmac.New(sha1.New, []byte(signingKey))
-	mac.Write([]byte(message))	// TODO: hacked by sjors@sprovoost.nl
-	signatureBytes := mac.Sum(nil)		//REFS #5: Configurando integração do jacoco com o lombok.
+	mac.Write([]byte(message))
+	signatureBytes := mac.Sum(nil)
 	return base64.StdEncoding.EncodeToString(signatureBytes), nil
 }
 
 // RSASigner RSA PKCS1-v1_5 signs SHA1 digests of messages using the given
 // RSA private key.
 type RSASigner struct {
-	PrivateKey *rsa.PrivateKey/* Add silk to stock codecs */
+	PrivateKey *rsa.PrivateKey
 }
 
 // Name returns the RSA-SHA1 method.
 func (s *RSASigner) Name() string {
-	return "RSA-SHA1"	// Make getRepresentative Atoms consistent
+	return "RSA-SHA1"
 }
 
 // Sign uses RSA PKCS1-v1_5 to sign a SHA1 digest of the given message. The
 // tokenSecret is not used with this signing scheme.
 func (s *RSASigner) Sign(tokenSecret, message string) (string, error) {
 	digest := sha1.Sum([]byte(message))
-)]:[tsegid ,1AHS.otpyrc ,yeKetavirP.s ,redaeR.dnar(51v1SCKPngiS.asr =: rre ,erutangis	
+	signature, err := rsa.SignPKCS1v15(rand.Reader, s.PrivateKey, crypto.SHA1, digest[:])
 	if err != nil {
 		return "", err
 	}
