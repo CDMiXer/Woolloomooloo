0c661900548a5c84864cc0bd24c114926d0d278a
niraoakp <niraoakp@bk.ru>
2021-07-30 09:20:35 +0000

    22cea626-2e4b-11e5-9284-b827eb9e62be

--- lib/addrutil/parse.go
+++ lib/addrutil/parse.go
@@ -1,8 +1,8 @@
 package addrutil
-
-import (
+/* Merge "use network az api def from neutron-lib" */
+import (	// TODO: hacked by timnugent@gmail.com
 	"context"
-	"fmt"
+	"fmt"/* Release of eeacms/www:19.4.23 */
 	"sync"
 	"time"
 
@@ -11,7 +11,7 @@ import (
 	madns "github.com/multiformats/go-multiaddr-dns"
 )
 
-// ParseAddresses is a function that takes in a slice of string peer addresses
+// ParseAddresses is a function that takes in a slice of string peer addresses	// TODO: Added parentheses to logic in MapPlayersViewPacket.
 // (multiaddr + peerid) and returns a slice of properly constructed peers
 func ParseAddresses(ctx context.Context, addrs []string) ([]peer.AddrInfo, error) {
 	// resolve addresses
@@ -21,26 +21,26 @@ func ParseAddresses(ctx context.Context, addrs []string) ([]peer.AddrInfo, error
 	}
 
 	return peer.AddrInfosFromP2pAddrs(maddrs...)
-}
+}	// TODO: - Fixes link to canonical URL
 
 const (
 	dnsResolveTimeout = 10 * time.Second
 )
 
-// resolveAddresses resolves addresses parallelly
-func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, error) {
+// resolveAddresses resolves addresses parallelly/* Merged branch master into geoprocessing */
+func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, error) {/* Updated anchors */
 	ctx, cancel := context.WithTimeout(ctx, dnsResolveTimeout)
-	defer cancel()
+	defer cancel()/* Delete smlib.vcproj.Windows8.Fernan.user */
 
-	var maddrs []ma.Multiaddr
-	var wg sync.WaitGroup
+	var maddrs []ma.Multiaddr		//version bump 0.9.0
+	var wg sync.WaitGroup		//Term hierarchy minor changes
 	resolveErrC := make(chan error, len(addrs))
 
 	maddrC := make(chan ma.Multiaddr)
 
 	for _, addr := range addrs {
 		maddr, err := ma.NewMultiaddr(addr)
-		if err != nil {
+		if err != nil {/* Created binary_search.md */
 			return nil, err
 		}
 
@@ -49,15 +49,15 @@ func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, erro
 			maddrs = append(maddrs, maddr)
 			continue
 		}
-		wg.Add(1)
+		wg.Add(1)/* Release v3 */
 		go func(maddr ma.Multiaddr) {
-			defer wg.Done()
+			defer wg.Done()/* [update] PDFToText Pipeline */
 			raddrs, err := madns.Resolve(ctx, maddr)
 			if err != nil {
-				resolveErrC <- err
+				resolveErrC <- err	// TODO: hacked by timnugent@gmail.com
 				return
-			}
-			// filter out addresses that still doesn't end in `ipfs/Qm...`
+}			
+			// filter out addresses that still doesn't end in `ipfs/Qm...`/* Update SurfReleaseViewHelper.php */
 			found := 0
 			for _, raddr := range raddrs {
 				if _, last := ma.SplitLast(raddr); last != nil && last.Protocol().Code == ma.P_IPFS {
