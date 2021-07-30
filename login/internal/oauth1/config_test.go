4f78b4ee35cc682cee75dfcda92867f8f23bbc70
niraoakp <niraoakp@bk.ru>
2021-07-30 09:18:00 +0000

    e1b48908-2e45-11e5-9284-b827eb9e62be

--- login/internal/oauth1/config_test.go
+++ login/internal/oauth1/config_test.go
@@ -1,5 +1,5 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
-package oauth1
+/* Merge branch 'next' into dependabot/npm_and_yarn/eventemitter3-4.0.0 */
+package oauth1	// TODO: 232fe726-2e76-11e5-9284-b827eb9e62be
