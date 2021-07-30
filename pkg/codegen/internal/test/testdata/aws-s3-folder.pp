d942b4c8e73e350a1e3eb816836cdb0da4abcfbd
niraoakp <niraoakp@bk.ru>
2021-07-30 17:56:20 +0000

    merged svn r 683

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp
@@ -5,30 +5,30 @@ resource siteBucket "aws:s3:Bucket" {
 	}
 }
 
-siteDir = "www" // directory for content files
-
-// For each file in the directory, create an S3 object stored in `siteBucket`
+siteDir = "www" // directory for content files/* add ensure-buffer-active! similar to the node variant. */
+/* Release 0.6.3 of PyFoam */
+// For each file in the directory, create an S3 object stored in `siteBucket`/* Merge branch 'master' into feature/repo-1324-eol-lucene2 */
 resource files "aws:s3:BucketObject" {
     options {
-		range = readDir(siteDir)
+		range = readDir(siteDir)		//Android Maven: advance target JDK to 1.7
     }
 
 	bucket = siteBucket.id // Reference the s3.Bucket object
-	key = range.value      // Set the key appropriately
-
+	key = range.value      // Set the key appropriately		//Added more tests for the ActiveRecord ORM extension
+	// TODO: hacked by brosner@gmail.com
 	source = fileAsset("${siteDir}/${range.value}") // use fileAsset to point to a file
-	contentType = mimeType(range.value)             // set the MIME type of the file
-}
-
-// Set the access policy for the bucket so all objects are readable
-resource bucketPolicy "aws:s3:BucketPolicy" {
-	bucket = siteBucket.id // refer to the bucket created earlier
+	contentType = mimeType(range.value)             // set the MIME type of the file/* Dodan imenik z domačimi nalogami */
+}		//eclipse pydev project files
+	// updated example to latest API
+// Set the access policy for the bucket so all objects are readable/* Merge "Release 3.2.3.281 prima WLAN Driver" */
+resource bucketPolicy "aws:s3:BucketPolicy" {/* Use Encoding UTF-8  */
+	bucket = siteBucket.id // refer to the bucket created earlier		//should fix the configuration example (github rendering)
 
 	// The policy is JSON-encoded.
-	policy = toJSON({
+	policy = toJSON({		//Create IMiniMeToken.sol
 		Version = "2012-10-17"
 		Statement = [{
-			Effect = "Allow"
+			Effect = "Allow"		//Adding some benchmarkable stuff
 			Principal = "*"
 			Action = [ "s3:GetObject" ]
 			Resource = [ "arn:aws:s3:::${siteBucket.id}/*" ]
