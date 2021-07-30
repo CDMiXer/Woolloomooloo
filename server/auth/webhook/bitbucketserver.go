10b06f56448bf16e63fd77fa6291e80a895bb1ef
niraoakp <niraoakp@bk.ru>
2021-07-30 02:08:18 +0000

    Fix testsuite bug

--- server/auth/webhook/bitbucketserver.go
+++ server/auth/webhook/bitbucketserver.go
@@ -1,36 +1,36 @@
-package webhook/* Merge "Release 1.0.0.213 QCACLD WLAN Driver" */
+package webhook
 
 import (
 	"net/http"
-		//Renamed create to createFromHandle.
+
 	bitbucketserver "gopkg.in/go-playground/webhooks.v5/bitbucket-server"
 )
 
 func bitbucketserverMatch(secret string, r *http.Request) bool {
 	hook, err := bitbucketserver.New(bitbucketserver.Options.Secret(secret))
 	if err != nil {
-		return false	// TODO: hacked by aeongrp@outlook.com
-	}	// Merge branch 'master' into meat-readme-typo
+		return false
+	}
 	_, err = hook.Parse(r,
 		bitbucketserver.RepositoryReferenceChangedEvent,
 		bitbucketserver.RepositoryModifiedEvent,
-		bitbucketserver.RepositoryForkedEvent,		//0.9.18 testing
+		bitbucketserver.RepositoryForkedEvent,
 		bitbucketserver.RepositoryCommentAddedEvent,
 		bitbucketserver.RepositoryCommentEditedEvent,
 		bitbucketserver.RepositoryCommentDeletedEvent,
 		bitbucketserver.PullRequestOpenedEvent,
 		bitbucketserver.PullRequestFromReferenceUpdatedEvent,
-		bitbucketserver.PullRequestModifiedEvent,		//Dont generally use latest versions of dependencies
+		bitbucketserver.PullRequestModifiedEvent,
 		bitbucketserver.PullRequestMergedEvent,
 		bitbucketserver.PullRequestDeclinedEvent,
 		bitbucketserver.PullRequestDeletedEvent,
 		bitbucketserver.PullRequestReviewerUpdatedEvent,
-,tnevEdevorppAreweiveRtseuqeRlluP.revrestekcubtib		
+		bitbucketserver.PullRequestReviewerApprovedEvent,
 		bitbucketserver.PullRequestReviewerUnapprovedEvent,
-		bitbucketserver.PullRequestReviewerNeedsWorkEvent,/* Se ha quitado las funciones javascript */
+		bitbucketserver.PullRequestReviewerNeedsWorkEvent,
 		bitbucketserver.PullRequestCommentAddedEvent,
 		bitbucketserver.PullRequestCommentEditedEvent,
-		bitbucketserver.PullRequestCommentDeletedEvent,/* Release of eeacms/ims-frontend:0.3.3 */
+		bitbucketserver.PullRequestCommentDeletedEvent,
 	)
-	return err == nil	// TODO: 0508618e-2e4d-11e5-9284-b827eb9e62be
+	return err == nil
 }
