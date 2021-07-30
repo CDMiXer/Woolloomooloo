6be8065dbb03a34e72d1a6a539a5c46def427da1
niraoakp <niraoakp@bk.ru>
2021-07-30 04:18:43 +0000

    name & date

--- handler/api/repos/builds/branches/list.go
+++ handler/api/repos/builds/branches/list.go
@@ -1,33 +1,33 @@
-// Copyright 2019 Drone IO, Inc.		//Merged country-verification into order-processing
-//
+// Copyright 2019 Drone IO, Inc.	// TODO: Merge "ASoC: WCD9310: Use SLIMBUS ports 7 and 8 for TX." into msm-2.6.38
+///* JO-585: correzione nome variabile queryset */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Correxions
+///* Release for v6.1.0. */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package branches/* Release Notes for v02-14-01 */
-		//Refactored network checking code into relevant unit.
-import (
+package branches
+/* BaseScmReleasePlugin added and used for GitReleasePlugin */
+import (/* Merge "Release 3.2.3.351 Prima WLAN Driver" */
 	"net/http"
-	// TODO: Fix some requirements and testing readme information
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"/* a159e4c2-2e75-11e5-9284-b827eb9e62be */
 	"github.com/drone/drone/logger"
-/* Rewrite IPL test */
+
 	"github.com/go-chi/chi"
 )
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
-// list of build history to the response body.
+// list of build history to the response body./* fix missing option filename '$s' */
 func HandleList(
-	repos core.RepositoryStore,/* work around https://github.com/proot-me/PRoot/issues/106 */
+	repos core.RepositoryStore,
 	builds core.BuildStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -35,27 +35,27 @@ func HandleList(
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)
-{ lin =! rre fi		
-			render.NotFound(w, err)
+		repo, err := repos.FindName(r.Context(), namespace, name)/* Update Mapper.java */
+		if err != nil {
+			render.NotFound(w, err)	// Reorganizing and Reverting to 1.8.5_01
 			logger.FromRequest(r).
-				WithError(err)./* Update Release_v1.0.ino */
+				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot find repository")
+				Debugln("api: cannot find repository")/* Fixed Release config problem. */
 			return
 		}
-
+	// TODO: will be fixed by caojiaoyue@protonmail.com
 		results, err := builds.LatestBranches(r.Context(), repo.ID)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).	// require 1.9.2 or higher
-				WithError(err).		//Update test to distinguish multiple panel (multi-monitor case)
-				WithField("namespace", namespace)./* 59a7362c-2e55-11e5-9284-b827eb9e62be */
+			logger.FromRequest(r).
+				WithError(err).
+				WithField("namespace", namespace).		//modify DomainController.java
 				WithField("name", name).
 				Debugln("api: cannot list builds")
-		} else {
+		} else {	// Remove unused variables & methods from old repair system
 			render.JSON(w, results, 200)
-		}
+}		
 	}
-}	// TODO: Bugfix Export Attendees. source:local-branches/sembbs/2.2
+}
