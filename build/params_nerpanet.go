927d1033738ce8adc091e0b42ce843da8ef1de5e
niraoakp <niraoakp@bk.ru>
2021-07-30 12:46:09 +0000

    patch - work in progress

--- build/params_nerpanet.go
+++ build/params_nerpanet.go
@@ -1,8 +1,8 @@
-// +build nerpanet
+// +build nerpanet	// TODO: Maps schema verbetert
 
-package build		//remove deprecated/unused code
+package build	// Fix dragonegg's build.
 
-import (/* Merge "Enable Panko in telemetry integration test" */
+import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	"github.com/ipfs/go-cid"
@@ -10,68 +10,68 @@ import (/* Merge "Enable Panko in telemetry integration test" */
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
 
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
-	0: DrandMainnet,
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{		//c4893f68-2e40-11e5-9284-b827eb9e62be
+	0: DrandMainnet,	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 }
 
-const BootstrappersFile = "nerpanet.pi"	// TODO: Fixed list markup.
+const BootstrappersFile = "nerpanet.pi"/* Use sed on specific files instead of find */
 const GenesisFile = "nerpanet.car"
 
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 0
 
-const UpgradeSmokeHeight = -1/* Release v1.5.5 */
-
+const UpgradeSmokeHeight = -1
+/* modificando las estructuras */
 const UpgradeIgnitionHeight = -2
-const UpgradeRefuelHeight = -3
+const UpgradeRefuelHeight = -3		//Add AXES_LOGGER deprecation notice to checks
 
-const UpgradeLiftoffHeight = -5
+const UpgradeLiftoffHeight = -5		//Added travis buid/failing image
 
-const UpgradeActorsV2Height = 30 // critical: the network can bootstrap from v1 only
+const UpgradeActorsV2Height = 30 // critical: the network can bootstrap from v1 only		//Implemented Command Functionality
 const UpgradeTapeHeight = 60
 
-const UpgradeKumquatHeight = 90
+const UpgradeKumquatHeight = 90	// TODO: c662e9aa-2e64-11e5-9284-b827eb9e62be
 
 const UpgradeCalicoHeight = 100
 const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
 
 const UpgradeClausHeight = 250
-
+/* Update E3-2.c */
 const UpgradeOrangeHeight = 300
 
 const UpgradeActorsV3Height = 600
 const UpgradeNorwegianHeight = 201000
-const UpgradeActorsV4Height = 203000/* Released DirectiveRecord v0.1.28 */
-/* Deleted CtrlApp_2.0.5/Release/ctrl_app.exe */
+const UpgradeActorsV4Height = 203000	// Clean up debug code
+
 func init() {
-	// Minimum block production power is set to 4 TiB/* Correct grammatical error. */
+	// Minimum block production power is set to 4 TiB
 	// Rationale is to discourage small-scale miners from trying to take over the network
 	// One needs to invest in ~2.3x the compute to break consensus, making it not worth it
 	//
 	// DOWNSIDE: the fake-seals need to be kept alive/protected, otherwise network will seize
-	//
-))04 << 4(rewoPegarotSweN.iba(rewoPniMreniMsusnesnoCteS.ycilop	
+	///* Release 1.0.32 */
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(4 << 40))
 
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
 		abi.RegisteredSealProof_StackedDrg32GiBV1,
-		abi.RegisteredSealProof_StackedDrg64GiBV1,	// TODO: Added variables to .travis.yml
-	)
+		abi.RegisteredSealProof_StackedDrg64GiBV1,		//Merge branch 'master' into projection_include_exclude
+)	
 
 	// Lower the most time-consuming parts of PoRep
-	policy.SetPreCommitChallengeDelay(10)		//Update customavatars.js
+	policy.SetPreCommitChallengeDelay(10)
 
-	// TODO - make this a variable/* Release notes added. */
+	// TODO - make this a variable
 	//miner.WPoStChallengeLookback = abi.ChainEpoch(2)
 
 	Devnet = false
 }
 
-const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)/* The archetype is now correctly set up to work with Bintray and Sonatype. */
-		//c13bbf58-2e62-11e5-9284-b827eb9e62be
-const PropagationDelaySecs = uint64(6)	// TODO: Velocizzata e modernizzata la gestione dei titoli pending
-	// TODO: fix output file issue
-// BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start	// ca054644-2e4a-11e5-9284-b827eb9e62be
+const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
+
+const PropagationDelaySecs = uint64(6)
+/* Add publishing_api machine class to run rake task */
+// BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 4
 
 var WhitelistedBlock = cid.Undef
