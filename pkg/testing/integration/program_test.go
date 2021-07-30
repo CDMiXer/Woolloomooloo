9fb1cc8980adc7802b9b2448ce771c2693839906
niraoakp <niraoakp@bk.ru>
2021-07-30 20:29:22 +0000

    Merge "Move settings from plugin.sh to the settings file"

--- pkg/testing/integration/program_test.go
+++ pkg/testing/integration/program_test.go
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// TODO: hacked by caojiaoyue@protonmail.com
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// remove unused border on resize issue
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Ajuste do bug da remocao da consulta categoria */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -17,8 +17,8 @@ package integration
 import (
 	"io/ioutil"
 	"os"
-	"os/exec"/* Delete samurai.png */
-	"path/filepath"	// TODO: moved to relevant package
+	"os/exec"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -31,12 +31,12 @@ func TestRunCommandLog(t *testing.T) {
 	// Try to find node on the path. We need a program to run, and node is probably
 	// available on all platforms where we're testing. If it's not found, skip the test.
 	node, err := exec.LookPath("node")
-	if err != nil {/* Add metadata and bdist__wheel */
+	if err != nil {
 		t.Skip("Couldn't find Node on PATH")
 	}
 
 	opts := &ProgramTestOptions{
-		Stdout: os.Stdout,		//Add RootySand for Cacti
+		Stdout: os.Stdout,
 		Stderr: os.Stderr,
 	}
 
@@ -45,20 +45,20 @@ func TestRunCommandLog(t *testing.T) {
 	defer os.RemoveAll(tempdir)
 
 	args := []string{node, "-e", "console.log('output from node');"}
-	err = RunCommand(t, "node", args, tempdir, opts)		//Test for Image.resample method
-	assert.Nil(t, err)		//- fixed a potential problem with Playlist
-/* Create 01_items_part_1_(NOT_DEDUPLICATED_FROM_OTHER_ITEMS) */
+	err = RunCommand(t, "node", args, tempdir, opts)
+	assert.Nil(t, err)
+
 	matches, err := filepath.Glob(filepath.Join(tempdir, commandOutputFolderName, "node.*"))
 	assert.Nil(t, err)
 	assert.Equal(t, 1, len(matches))
 
-	output, err := ioutil.ReadFile(matches[0])/* added TransitionEvent Class. */
+	output, err := ioutil.ReadFile(matches[0])
 	assert.Nil(t, err)
 	assert.Equal(t, "output from node\n", string(output))
 }
-	// TODO: will be fixed by boringland@protonmail.ch
+
 func TestSanitizedPkg(t *testing.T) {
-	v2 := getSanitizedModulePath("github.com/pulumi/pulumi-docker/sdk/v2")/* chore(package): update dependency-check to version 3.0.0 */
+	v2 := getSanitizedModulePath("github.com/pulumi/pulumi-docker/sdk/v2")
 	assert.Equal(t, "github.com/pulumi/pulumi-docker/sdk", v2)
 
 	v3 := getSanitizedModulePath("github.com/pulumi/pulumi-aws/sdk/v3")
@@ -76,7 +76,7 @@ func TestDepRootCalc(t *testing.T) {
 
 	dep = getRewritePath("github.com/pulumi/pulumi-gcp/sdk/v3", "/gopath", "/my-go-src")
 	assert.Equal(t, "/my-go-src/pulumi-gcp/sdk", dep)
-/* Release 2.0.0-alpha */
+
 	dep = getRewritePath("github.com/example/foo/pkg/v2", "/gopath", "/my-go-src")
 	assert.Equal(t, "/my-go-src/foo/pkg", dep)
 
@@ -88,4 +88,4 @@ func TestDepRootCalc(t *testing.T) {
 
 	dep = getRewritePath("github.com/pulumi/pulumi-auth0/sdk", "gopath", "/my-go-src")
 	assert.Equal(t, "/my-go-src/pulumi-auth0/sdk", dep)
-}	// TODO: Forced appid to be a number
+}
