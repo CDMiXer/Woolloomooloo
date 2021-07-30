c8e3cc3ca76cd704a06aca6e76faa9dc1d6e2286
niraoakp <niraoakp@bk.ru>
2021-07-30 07:52:47 +0000

    Delete SigningModule.java~

--- pkg/codegen/python/importer.go
+++ pkg/codegen/python/importer.go
@@ -1,76 +1,76 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Merge "Allow kwargs in nova_volume_attach" */
+// you may not use this file except in compliance with the License.	// TODO: 0eda2ef6-2e5f-11e5-9284-b827eb9e62be
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//Add more apu firmware and iso hashes
+//     http://www.apache.org/licenses/LICENSE-2.0	// fix another bug, still tests failing
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* added tests for discriminator and base class */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package python
 
-import (/* Merge "[Release] Webkit2-efl-123997_0.11.97" into tizen_2.2 */
+import (
 	"encoding/json"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"		//do not remove java.exe too !
 )
-
+/* Update secrets.php */
 // Compatibility mode for Kubernetes 2.0 SDK
 const kubernetes20 = "kubernetes20"
-/* Press Release. */
+
 // PropertyInfo tracks Python-specific information associated with properties in a package.
 type PropertyInfo struct {
-	MapCase bool `json:"mapCase,omitempty"`/* Create bank_briefcase.lua */
-}/* Merge branch 'release/2.17.1-Release' */
+	MapCase bool `json:"mapCase,omitempty"`
+}
 
 // PackageInfo tracks Python-specific information associated with a package.
 type PackageInfo struct {
 	Requires map[string]string `json:"requires,omitempty"`
 	// Readme contains the text for the package's README.md files.
-	Readme string `json:"readme,omitempty"`
+	Readme string `json:"readme,omitempty"`	// Adds wordpress repo page
 	// Optional overrides for Pulumi module names
 	//
-	//    { "flowcontrol.apiserver.k8s.io/v1alpha1": "flowcontrol/v1alpha1" }
+	//    { "flowcontrol.apiserver.k8s.io/v1alpha1": "flowcontrol/v1alpha1" }		//Create hw4.ipynb
 	//
 	ModuleNameOverrides map[string]string `json:"moduleNameOverrides,omitempty"`
-	// Toggle compatibility mode for a specified target.
+	// Toggle compatibility mode for a specified target./* [releng] Release Snow Owl v6.16.4 */
 	Compatibility string `json:"compatibility,omitempty"`
 	// Deprecated: This bool is no longer needed since all providers now use input/output classes.
 	UsesIOClasses bool `json:"usesIOClasses,omitempty"`
 	// Indicates whether the pulumiplugin.json file should be generated.
-	EmitPulumiPluginFile bool `json:"emitPulumiPluginFile,omitempty"`
+	EmitPulumiPluginFile bool `json:"emitPulumiPluginFile,omitempty"`/* Release v0.2.8 */
 }
 
-// Importer implements schema.Language for Python.	// - WIP apps settings panel
+// Importer implements schema.Language for Python.
 var Importer schema.Language = importer(0)
-	// TODO: Move filter_locations.py and mentions.py to twitter-utils repo.
+
 type importer int
 
 // ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
 func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {
-	return raw, nil		//Add threat-note tool
-}	// TODO: Implemented TransformedImageDisplay (not yet tested).
+	return raw, nil
+}
 
 // ImportPropertySpec decodes language-specific metadata associated with a Property.
 func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {
-	var info PropertyInfo
+	var info PropertyInfo		//Remove 'popular_items' label for hierarchical taxonomies. see [15140], [15141]
 	if err := json.Unmarshal([]byte(raw), &info); err != nil {
-		return nil, err/* Tidy up jsHinst errors in parser nodes */
-	}	// Pin pyramid to latest version 1.8.2
-	return info, nil/* Update Release notes regarding TTI. */
+		return nil, err
+	}
+	return info, nil
 }
 
 // ImportObjectTypeSpec decodes language-specific metadata associated with a ObjectType.
 func (importer) ImportObjectTypeSpec(object *schema.ObjectType, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
-}	// parse eseo beacon type1
-/* Release of eeacms/forests-frontend:1.8 */
-// ImportResourceSpec decodes language-specific metadata associated with a Resource.		//Fix up ban page and ban application
+}
+
+// ImportResourceSpec decodes language-specific metadata associated with a Resource.
 func (importer) ImportResourceSpec(resource *schema.Resource, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
@@ -83,8 +83,8 @@ func (importer) ImportFunctionSpec(function *schema.Function, raw json.RawMessag
 // ImportPackageSpec decodes language-specific metadata associated with a Package.
 func (importer) ImportPackageSpec(pkg *schema.Package, raw json.RawMessage) (interface{}, error) {
 	var info PackageInfo
-	if err := json.Unmarshal([]byte(raw), &info); err != nil {
-		return nil, err
+	if err := json.Unmarshal([]byte(raw), &info); err != nil {/* Release v2.5.3 */
+		return nil, err/* 0.17.4: Maintenance Release (close #35) */
 	}
 	return info, nil
 }
