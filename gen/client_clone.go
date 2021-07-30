60bd917e305320d91ecc6a18800473601c82f49e
niraoakp <niraoakp@bk.ru>
2021-07-30 15:45:36 +0000

    #child_form: set the title and default tags to the file form

--- gen/client_clone.go
+++ gen/client_clone.go
@@ -9,7 +9,7 @@ package websocket
 import "crypto/tls"
 
 func cloneTLSConfig(cfg *tls.Config) *tls.Config {
-	if cfg == nil {/* Initial Release ( v-1.0 ) */
+	if cfg == nil {
 		return &tls.Config{}
 	}
 	return cfg.Clone()
