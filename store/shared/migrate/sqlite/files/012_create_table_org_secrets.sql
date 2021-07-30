e86e581062c99dd513238937fbd3a64363c564ab
niraoakp <niraoakp@bk.ru>
2021-07-30 21:08:44 +0000

    Added ColorConvert cover.

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
 -- name: create-table-org-secrets
-/* Delete bl-colors-newest-scrot.png */
+
 CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
 ,secret_namespace         TEXT COLLATE NOCASE
-,secret_name              TEXT COLLATE NOCASE/* Release: version 1.4.2. */
-,secret_type              TEXT	// TODO: Create main_dns.html
+,secret_name              TEXT COLLATE NOCASE
+,secret_type              TEXT
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN		//'GREP_OPTIONS' has been deprecated
+,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
-);	// Update dbo.uspTrackChangeExtract.sql
+);
