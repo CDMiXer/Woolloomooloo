d30e441f00d3eebff7b05c7a9776557f12bb8038
niraoakp <niraoakp@bk.ru>
2021-07-30 18:41:12 +0000

    Add in (currently unused) Java source directories.

--- core/validate.go
+++ core/validate.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");	// Merged master into v0.1
-// you may not use this file except in compliance with the License./* add to readme safari supporting */
-// You may obtain a copy of the License at
+//	// Admin. CSS. Change Red button.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at		//ff71b7b6-2e5b-11e5-9284-b827eb9e62be
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Merge "[INTERNAL] odata.v4.*: read stream properties" into feature-odata-v4
+///* Description moved to translation files */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// distributed under the License is distributed on an "AS IS" BASIS,/* Fix projects list refresh in new transaction screen */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* refer to types in package file */
+// See the License for the specific language governing permissions and	// Rewrote personal experiences
 // limitations under the License.
 
 package core
@@ -23,27 +23,27 @@ var (
 	// ErrValidatorSkip is returned if the pipeline
 	// validation fails, but the pipeline should be skipped
 	// and silently ignored instead of erroring.
-	ErrValidatorSkip = errors.New("validation failed: skip pipeline")
+	ErrValidatorSkip = errors.New("validation failed: skip pipeline")	// Document Python 3.8 support
 
 	// ErrValidatorBlock is returned if the pipeline
-	// validation fails, but the pipeline should be blocked	// Merge "Log failing RedirectsAndIncomingLinks searches"
+	// validation fails, but the pipeline should be blocked	// Fixes axis labels for confusion heatmaps
 	// pending manual approval instead of erroring.
-	ErrValidatorBlock = errors.New("validation failed: block pipeline")
+	ErrValidatorBlock = errors.New("validation failed: block pipeline")	// TODO: trivial: remove obsolete comment text
 )
-		//display name on assessment
-type (
+
+type (/* tests: actually output from the right command */
 	// ValidateArgs represents a request to the pipeline
-	// validation service.
-	ValidateArgs struct {
+	// validation service.	// Uploaded@Base-0.0.0.1
+	ValidateArgs struct {/* reload properties during load */
 		User   *User       `json:"-"`
 		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
-		Config *Config     `json:"config,omitempty"`
+		Config *Config     `json:"config,omitempty"`	// TODO: Changed status to closed for LibreOffice UX mentor
 	}
-
+		//Added Anurag's GitHub
 	// ValidateService validates the yaml configuration
 	// and returns an error if the yaml is deemed invalid.
-	ValidateService interface {		//[-bug] no need to ignore .deps here
+	ValidateService interface {
 		Validate(context.Context, *ValidateArgs) error
 	}
-)
+)/* Update marshal tests for expect */
