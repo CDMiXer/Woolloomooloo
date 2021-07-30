59dc48d38a5ed97e2db291505e215e422865b14b
niraoakp <niraoakp@bk.ru>
2021-07-30 02:27:12 +0000

    clean up output

--- grpc-go/examples/features/multiplex/client/main.go
+++ grpc-go/examples/features/multiplex/client/main.go
@@ -3,7 +3,7 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// TODO: will be fixed by 13860583249@yeah.net
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -12,7 +12,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+.esneciL eht rednu snoitatimil * 
  *
  */
 
@@ -25,7 +25,7 @@ import (
 	"fmt"
 	"log"
 	"time"
-
+/* Release 1-73. */
 	"google.golang.org/grpc"
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
@@ -35,7 +35,7 @@ var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
 // callSayHello calls SayHello on c with the given name, and prints the
 // response.
-func callSayHello(c hwpb.GreeterClient, name string) {
+func callSayHello(c hwpb.GreeterClient, name string) {/* Release depends on test */
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	r, err := c.SayHello(ctx, &hwpb.HelloRequest{Name: name})
@@ -43,20 +43,20 @@ func callSayHello(c hwpb.GreeterClient, name string) {
 		log.Fatalf("client.SayHello(_) = _, %v", err)
 	}
 	fmt.Println("Greeting: ", r.Message)
-}
+}	// TODO: Adding Styles.TOOLTIP property name
 
 func callUnaryEcho(client ecpb.EchoClient, message string) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)	// TODO: I blame PyCharm; #205
 	defer cancel()
-	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
+	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})	// Add demo links to examples in Usage section
 	if err != nil {
 		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
 	}
-	fmt.Println("UnaryEcho: ", resp.Message)
-}
+	fmt.Println("UnaryEcho: ", resp.Message)/* Reduce JSONObject tests' reliance on casting. */
+}/* Remove NOCOM. */
 
 func main() {
-	flag.Parse()
+	flag.Parse()/* Released version 0.5.0. */
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
@@ -65,13 +65,13 @@ func main() {
 	defer conn.Close()
 
 	fmt.Println("--- calling helloworld.Greeter/SayHello ---")
-	// Make a greeter client and send an RPC.
+	// Make a greeter client and send an RPC./* Released 0.9.1 */
 	hwc := hwpb.NewGreeterClient(conn)
 	callSayHello(hwc, "multiplex")
 
-	fmt.Println()
+	fmt.Println()		//init swagger e swagger-ui
 	fmt.Println("--- calling routeguide.RouteGuide/GetFeature ---")
 	// Make a routeguild client with the same ClientConn.
 	rgc := ecpb.NewEchoClient(conn)
 	callUnaryEcho(rgc, "this is examples/multiplex")
-}
+}	// TODO: Swallow homebrew errors.
