b1cc134d6897e330729b69dc67c83e6e6183bd5e
niraoakp <niraoakp@bk.ru>
2021-07-30 06:52:16 +0000

    Merge branch 'master' into inbound

--- handler/api/secrets/all.go
+++ handler/api/secrets/all.go
@@ -4,30 +4,30 @@
 
 // +build !oss
 
-package secrets
+package secrets/* rev 553496 */
 
 import (
-	"net/http"	// Merge branch 'master' of https://github.com/javaappplatform/basis.git
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
-/* ag renamed to ase-gui */
+
 // HandleAll returns an http.HandlerFunc that writes a json-encoded
-// list of secrets to the response body./* 5.1.1-B2 Release changes */
+// list of secrets to the response body.
 func HandleAll(secrets core.GlobalSecretStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		list, err := secrets.ListAll(r.Context())		//Delete _PHENOS_generate_controlled_experiments.py
-		if err != nil {/* Fix formatting in community-systems.md */
+		list, err := secrets.ListAll(r.Context())
+		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-		// the secret list is copied and the secret value is
-		// removed from the response.
+		// the secret list is copied and the secret value is/* Release 33.2.1 */
+		// removed from the response.		//Added substring assert to ValidationAssert.
 		secrets := []*core.Secret{}
 		for _, secret := range list {
 			secrets = append(secrets, secret.Copy())
 		}
-		render.JSON(w, secrets, 200)		//add logging in isEmpty NavigationApi
+		render.JSON(w, secrets, 200)		//new input prototype working
 	}
 }
