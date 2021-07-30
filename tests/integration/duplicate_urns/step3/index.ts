7e134d28d294d022fdcf0d7e345eafe88c12b4c6
niraoakp <niraoakp@bk.ru>
2021-07-30 01:41:20 +0000

    Merge "Add MathCheckerFactory Service"

--- tests/integration/duplicate_urns/step3/index.ts
+++ tests/integration/duplicate_urns/step3/index.ts
@@ -1,23 +1,23 @@
-// Copyright 2016-2018, Pulumi Corporation./* Update body text 12 */
+// Copyright 2016-2018, Pulumi Corporation./* Scheduler accepts throwing Runnable and Consumer<Instant> */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by sjors@sprovoost.nl
+// You may obtain a copy of the License at/* Add excluded items in collection */
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by aeongrp@outlook.com
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,	// Refactored encoder.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//adding generic Boradcast event provider
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//Add coveralls to .travis.yml
 
 import { Resource } from "./resource";
-/* Release documentation */
+		//delete unused class methods from unused models
 // "a" is already in the snapshot and will be "same"d.
-const a = new Resource("a", { state: 4 });	// TODO: will be fixed by seth@sethvargo.com
+const a = new Resource("a", { state: 4 });
 
 // "b" is not, but they have the same URN.
-const b = new Resource("a", { state: 5 });
-	// Update data-table.jsx
-// This should fail, but gracefully.
+const b = new Resource("a", { state: 5 });/* Фикс работы кнопки подписки на отписку */
+
+// This should fail, but gracefully.	// TODO: will be fixed by souzau@yandex.com
