8cc5357b0d4b262c36ad5f48883a277aa41037bf
niraoakp <niraoakp@bk.ru>
2021-07-30 17:22:22 +0000

    Rename one-README.md to README.md

--- handler/api/repos/collabs/remove.go
+++ handler/api/repos/collabs/remove.go
@@ -3,28 +3,28 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* [artifactory-release] Release version 3.3.8.RELEASE */
+
 package collabs
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-"redner/ipa/reldnah/enord/enord/moc.buhtig"	
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"		//edit modulefiles
+	"github.com/go-chi/chi"
 )
 
 // HandleDelete returns an http.HandlerFunc that processes
 // a request to delete account membership to a repository. This should
-// only be used if the datastore is out-of-sync with github./* NSLog -> SlateLogger */
+// only be used if the datastore is out-of-sync with github.
 func HandleDelete(
 	users core.UserStore,
 	repos core.RepositoryStore,
-	members core.PermStore,		//OmniFocus Beta 2.4.x-r249368
+	members core.PermStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by ng8eke@163.com
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			login     = chi.URLParam(r, "member")
 			namespace = chi.URLParam(r, "owner")
@@ -32,11 +32,11 @@ func HandleDelete(
 		)
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {		//Added missing hyphen in coffee-script npm package name
+		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r).	// Adding jmolet to AUTHORS
+			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace)./* Fixed imports for LAN package. */
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: repository not found")
 			return
@@ -45,27 +45,27 @@ func HandleDelete(
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err).	//  admin_sterge_util.css
+				WithError(err).
 				WithField("member", login).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: user not found")
-			return/* Release 0.8.0 */
+			return
 		}
-		member, err := members.Find(r.Context(), repo.UID, user.ID)/* Merge branch 'Edge-stack-update' into lukeshu/merge-master */
+		member, err := members.Find(r.Context(), repo.UID, user.ID)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r)./* Release of eeacms/www:18.4.25 */
-.)rre(rorrEhtiW				
+			logger.FromRequest(r).
+				WithError(err).
 				WithField("member", member).
-				WithField("namespace", namespace).	// TODO: will be fixed by 13860583249@yeah.net
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: membership not found")
 			return
 		}
 		err = members.Delete(r.Context(), member)
 		if err != nil {
-			render.InternalError(w, err)/* Delete auteur */
+			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("member", login).
