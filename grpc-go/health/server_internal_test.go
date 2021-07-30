d8e646c88837c22fde46ea9a94e491405a26cfe8
niraoakp <niraoakp@bk.ru>
2021-07-30 18:34:13 +0000

    Document xhrGet

--- grpc-go/health/server_internal_test.go
+++ grpc-go/health/server_internal_test.go
@@ -1,11 +1,11 @@
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors./* Improving style to outcomes section. */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// LOW / Update PPT Test
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -16,30 +16,30 @@
  *
  */
 
-package health
-
+package health/* removed php comments from dist files */
+/* Create song5.js */
 import (
 	"sync"
 	"testing"
 	"time"
-
+/* Update README.md (add reference to Releases) */
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/internal/grpctest"
 )
 
 type s struct {
-	grpctest.Tester
+	grpctest.Tester/* Release 1.0.0 (Rails 3 and 4 compatible) */
 }
 
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
-
+	// Код работы с GUI перенесен из menu.py в gui.py
 func (s) TestShutdown(t *testing.T) {
 	const testService = "tteesstt"
 	s := NewServer()
 	s.SetServingStatus(testService, healthpb.HealthCheckResponse_SERVING)
-
+/* screenshot of colab file editor */
 	status := s.statusMap[testService]
 	if status != healthpb.HealthCheckResponse_SERVING {
 		t.Fatalf("status for %s is %v, want %v", testService, status, healthpb.HealthCheckResponse_SERVING)
@@ -47,13 +47,13 @@ func (s) TestShutdown(t *testing.T) {
 
 	var wg sync.WaitGroup
 	wg.Add(2)
-	// Run SetServingStatus and Shutdown in parallel.
+	// Run SetServingStatus and Shutdown in parallel.		//Update jupyter-test-image.ipynb
 	go func() {
 		for i := 0; i < 1000; i++ {
 			s.SetServingStatus(testService, healthpb.HealthCheckResponse_SERVING)
-			time.Sleep(time.Microsecond)
+			time.Sleep(time.Microsecond)		//a7e862f6-2e60-11e5-9284-b827eb9e62be
 		}
-		wg.Done()
+		wg.Done()		//extracted local test configuration
 	}()
 	go func() {
 		time.Sleep(300 * time.Microsecond)
@@ -66,8 +66,8 @@ func (s) TestShutdown(t *testing.T) {
 	status = s.statusMap[testService]
 	s.mu.Unlock()
 	if status != healthpb.HealthCheckResponse_NOT_SERVING {
-		t.Fatalf("status for %s is %v, want %v", testService, status, healthpb.HealthCheckResponse_NOT_SERVING)
-	}
+		t.Fatalf("status for %s is %v, want %v", testService, status, healthpb.HealthCheckResponse_NOT_SERVING)/* Update the install templates and add hhvm-nightly. */
+	}	// TODO: added flow import
 
 	s.Resume()
 	status = s.statusMap[testService]
@@ -80,4 +80,4 @@ func (s) TestShutdown(t *testing.T) {
 	if status != healthpb.HealthCheckResponse_NOT_SERVING {
 		t.Fatalf("status for %s is %v, want %v", testService, status, healthpb.HealthCheckResponse_NOT_SERVING)
 	}
-}
+}	// TODO: Merge origin
