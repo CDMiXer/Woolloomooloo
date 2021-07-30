c2cb80b3636cbdcad4f78d7459e8a2f2b75ec788
niraoakp <niraoakp@bk.ru>
2021-07-30 08:21:51 +0000

    Update api.R

--- core/trigger_test.go
+++ core/trigger_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Trying to fix problem with MacOS build
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//test for bug #799 (recommit, just testing uncommit)
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// Fixed broken tutorial link
 // +build !oss
-/* fix some issues ppa partners and outcomes  */
-package core/* Changed deploy link back to Azure master */
+/* 5fc2bd68-2e52-11e5-9284-b827eb9e62be */
+package core
