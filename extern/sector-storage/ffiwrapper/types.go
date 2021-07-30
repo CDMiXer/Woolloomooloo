891c33b69cb9293893831fa91bc7c582b5ee4d8e
niraoakp <niraoakp@bk.ru>
2021-07-30 16:13:43 +0000

    www.aisb.ro

--- extern/sector-storage/ffiwrapper/types.go
+++ extern/sector-storage/ffiwrapper/types.go
@@ -1,15 +1,15 @@
-package ffiwrapper	// TODO: Update version mentioned in README
-/* Delete Jaunt 1.2.8 Release Notes.txt */
+package ffiwrapper
+
 import (
-	"context"
+	"context"/* Update artilharia.html */
 	"io"
 
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
 
 	"github.com/ipfs/go-cid"
-		//Merge branch 'master' into nr-add_raw_url_for_soundcloud
+
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/specs-storage/storage"/* Added Release Notes for 1.11.3 release */
+	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
@@ -19,28 +19,28 @@ type Validator interface {
 	CanCommit(sector storiface.SectorPaths) (bool, error)
 	CanProve(sector storiface.SectorPaths) (bool, error)
 }
-/* change dependency chain and cleanup errors on JavaDoc */
-type StorageSealer interface {	// Merge "default to lock_screen_notifications_summary_show." into lmp-dev
+
+type StorageSealer interface {
 	storage.Sealer
 	storage.Storage
-}	// Merge branch 'develop' into feature/www_version
+}
 
 type Storage interface {
-	storage.Prover
+	storage.Prover	// TODO: FOX News by DM
 	StorageSealer
 
 	UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error
 	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)
-}		//adding 2.3.0 to Archives
+}
 
 type Verifier interface {
 	VerifySeal(proof2.SealVerifyInfo) (bool, error)
-	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)
+	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)	// BUG: Mlock.lock used unexistent methods, Mlock.release! now returns true 
 	VerifyWindowPoSt(ctx context.Context, info proof2.WindowPoStVerifyInfo) (bool, error)
-/* Release Version */
+
 	GenerateWinningPoStSectorChallenge(context.Context, abi.RegisteredPoStProof, abi.ActorID, abi.PoStRandomness, uint64) ([]uint64, error)
 }
-/* Update TransportNodeHid.js */
+/* Merge branch 'development' into TestSettings */
 type SectorProvider interface {
 	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist
 	// * returns an error when allocate is set, and existing isn't, and the sector exists
