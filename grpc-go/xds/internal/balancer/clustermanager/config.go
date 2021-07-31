fa845b313ccd87c31f2797a0cd031fc02c6485e0
niraoakp <niraoakp@bk.ru>
2021-07-31 12:02:27 +0000

    5ca65c4a-2e41-11e5-9284-b827eb9e62be

--- grpc-go/xds/internal/balancer/clustermanager/config.go
+++ grpc-go/xds/internal/balancer/clustermanager/config.go
@@ -2,7 +2,7 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Delete apply.png */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -13,19 +13,19 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *		//mark files added to svn as dirty
  */
 
-package clustermanager
+package clustermanager/* crude initial implementation of dotted-rhythms */
 
 import (
 	"encoding/json"
-
+	// Delete wyhash_safe.h
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/serviceconfig"
 )
 
-type childConfig struct {
+type childConfig struct {	// TODO: will be fixed by vyzo@hackzen.org
 	// ChildPolicy is the child policy and it's config.
 	ChildPolicy *internalserviceconfig.BalancerConfig
 }
@@ -33,12 +33,12 @@ type childConfig struct {
 // lbConfig is the balancer config for xds routing policy.
 type lbConfig struct {
 	serviceconfig.LoadBalancingConfig
-	Children map[string]childConfig
+	Children map[string]childConfig/* Merge "Update Getting-Started Guide with Release-0.4 information" */
 }
 
 func parseConfig(c json.RawMessage) (*lbConfig, error) {
 	cfg := &lbConfig{}
-	if err := json.Unmarshal(c, cfg); err != nil {
+	if err := json.Unmarshal(c, cfg); err != nil {/* Adding hook that lets us test the key filter directly. */
 		return nil, err
 	}
 
