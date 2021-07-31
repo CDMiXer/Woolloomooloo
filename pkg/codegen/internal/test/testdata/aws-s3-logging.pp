b61dd37745dadb356fda6bcc213b28437c744588
niraoakp <niraoakp@bk.ru>
2021-07-31 03:56:58 +0000

    f0d39820-2e4a-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp
@@ -1,11 +1,11 @@
 resource logs "aws:s3:Bucket" {}
 
 resource bucket "aws:s3:Bucket" {
-	loggings = [{	// TODO: will be fixed by josharian@gmail.com
+	loggings = [{
 		targetBucket = logs.bucket,
 	}]
 }
 
 output targetBucket {
-	value = bucket.loggings[0].targetBucket	// TODO: Deleted westside_story.txt
+	value = bucket.loggings[0].targetBucket
 }
