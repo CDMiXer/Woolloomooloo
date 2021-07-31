24012183164d6ca82a736a9567146dc2e4c7ecc1
niraoakp <niraoakp@bk.ru>
2021-07-31 13:33:04 +0000

    Update pytest from 3.10.0 to 4.0.1

--- handler/api/queue/items.go
+++ handler/api/queue/items.go
@@ -1,31 +1,31 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Prep for final transition */
+
 // +build !oss
 
 package queue
 
 import (
 	"net/http"
-
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Upgrade keybinding resolver to fix deprecation warnings in specs */
+	// Initialize Project
+	"github.com/drone/drone/core"	// Bump to Development5 formats.
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-)
+)	// TODO: hacked by sbrichards@gmail.com
 
-// HandleItems returns an http.HandlerFunc that writes a	// TODO: hacked by cory@protocol.ai
+// HandleItems returns an http.HandlerFunc that writes a
 // json-encoded list of queue items to the response body.
 func HandleItems(store core.StageStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {	// update derivative (fixed)
+	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		items, err := store.ListIncomplete(ctx)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot get running items")
-			return
+			return/* Updated  Release */
 		}
-		render.JSON(w, items, 200)/* Support insert / delete lines ansi sequences */
+		render.JSON(w, items, 200)
 	}
 }
