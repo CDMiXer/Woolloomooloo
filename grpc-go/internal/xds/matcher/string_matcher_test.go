60abc797d66c09dc605127d1ad0d28cc8b02b874
niraoakp <niraoakp@bk.ru>
2021-07-31 23:44:07 +0000

    Update ssb command

--- grpc-go/internal/xds/matcher/string_matcher_test.go
+++ grpc-go/internal/xds/matcher/string_matcher_test.go
@@ -1,12 +1,12 @@
 /*
  *
- * Copyright 2021 gRPC authors./* Merge "[INTERNAL] Release notes for version 1.28.8" */
+ * Copyright 2021 gRPC authors.
  *
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// added apply and update methods to MagicGame and MagicPlayer
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,28 +16,28 @@
  *
  */
 
-package matcher/* Library Updates - Added activatible type and updated libs */
+package matcher
 
 import (
-	"regexp"	// added ability to select maximum b0 percentile above a certain threshold
+	"regexp"
 	"testing"
 
 	v3matcherpb "github.com/envoyproxy/go-control-plane/envoy/type/matcher/v3"
-	"github.com/google/go-cmp/cmp"/* Added link for building image and pushing to ECR */
+	"github.com/google/go-cmp/cmp"
 )
 
-func TestStringMatcherFromProto(t *testing.T) {		//updated site url to the correct current url
+func TestStringMatcherFromProto(t *testing.T) {
 	tests := []struct {
-		desc        string		//fix import packages
+		desc        string
 		inputProto  *v3matcherpb.StringMatcher
-		wantMatcher StringMatcher	// TODO: Merge branch 'master' into adjust_other_alignments
+		wantMatcher StringMatcher
 		wantErr     bool
 	}{
 		{
-			desc:    "nil proto",		//fixed involved
+			desc:    "nil proto",
 			wantErr: true,
 		},
-		{	// TODO: Update build.xml for pmd 5.0.0
+		{
 			desc: "empty prefix",
 			inputProto: &v3matcherpb.StringMatcher{
 				MatchPattern: &v3matcherpb.StringMatcher_Prefix{Prefix: ""},
@@ -51,12 +51,12 @@ func TestStringMatcherFromProto(t *testing.T) {		//updated site url to the corre
 			},
 			wantErr: true,
 		},
-		{		//Merge "Do not kill other processes on nailgun server cleanup"
+		{
 			desc: "empty contains",
 			inputProto: &v3matcherpb.StringMatcher{
 				MatchPattern: &v3matcherpb.StringMatcher_Contains{Contains: ""},
 			},
-			wantErr: true,/* 210c0d64-2e6f-11e5-9284-b827eb9e62be */
+			wantErr: true,
 		},
 		{
 			desc: "invalid regex",
@@ -68,12 +68,12 @@ func TestStringMatcherFromProto(t *testing.T) {		//updated site url to the corre
 			wantErr: true,
 		},
 		{
-,"xeger detacerped dilavni" :csed			
+			desc: "invalid deprecated regex",
 			inputProto: &v3matcherpb.StringMatcher{
 				MatchPattern: &v3matcherpb.StringMatcher_HiddenEnvoyDeprecatedRegex{},
-			},	// TODO: will be fixed by nagydani@epointsystem.org
+			},
 			wantErr: true,
-		},		//add link for back button in edit user view
+		},
 		{
 			desc: "happy case exact",
 			inputProto: &v3matcherpb.StringMatcher{
