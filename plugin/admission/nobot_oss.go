c36bee549674e9160fe4d7fa662ada7b08db18d6
niraoakp <niraoakp@bk.ru>
2021-07-31 21:05:13 +0000

    plugins download

--- plugin/admission/nobot_oss.go
+++ plugin/admission/nobot_oss.go
@@ -6,23 +6,23 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// fix(outdated): strip colors before printing the outdated table
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Renaming change fixes. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by nagydani@epointsystem.org
+
 // +build oss
-/* issue 1289 Release Date or Premiered date is not being loaded from NFO file */
+
 package admission
 
-import (
+import (/* make provision for alternative screens */
 	"time"
 
 	"github.com/drone/drone/core"
-)/* Merge "[www-index] Splits Releases and Languages items" */
+)
 
 // Nobot is a no-op admission controller
-func Nobot(core.UserService, time.Duration) core.AdmissionService {/* Release of eeacms/eprtr-frontend:0.3-beta.26 */
-	return new(noop)
+func Nobot(core.UserService, time.Duration) core.AdmissionService {
+	return new(noop)/* 5.6.0 Release */
 }
