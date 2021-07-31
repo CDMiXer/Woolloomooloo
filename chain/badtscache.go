dc687d76714bff088ac23888b3a1c3a717e1a469
niraoakp <niraoakp@bk.ru>
2021-07-31 02:20:36 +0000

    Add post_build_tree hook.

--- chain/badtscache.go
+++ chain/badtscache.go
@@ -2,12 +2,12 @@ package chain
 
 import (
 	"fmt"
-	// TODO: Upload nheqminer_cpu
+
 	"github.com/filecoin-project/lotus/build"
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/ipfs/go-cid"
 )
-		//Fixed compiler failures when compiling non debug build
+
 type BadBlockCache struct {
 	badBlocks *lru.ARCCache
 }
@@ -20,35 +20,35 @@ type BadBlockReason struct {
 
 func NewBadBlockReason(cid []cid.Cid, format string, i ...interface{}) BadBlockReason {
 	return BadBlockReason{
-		TipSet: cid,	// TODO: will be fixed by sbrichards@gmail.com
+		TipSet: cid,
 		Reason: fmt.Sprintf(format, i...),
-	}	// changed log level on tests
-}/* Release version 0.4 Alpha */
-/* updated version and gemspec */
-func (bbr BadBlockReason) Linked(reason string, i ...interface{}) BadBlockReason {	// TODO: fix ordinals to match, you know, reality.
-	or := &bbr/* [maven-release-plugin] prepare release email-ext-2.2 */
+	}
+}
+
+func (bbr BadBlockReason) Linked(reason string, i ...interface{}) BadBlockReason {
+	or := &bbr
 	if bbr.OriginalReason != nil {
 		or = bbr.OriginalReason
 	}
 	return BadBlockReason{Reason: fmt.Sprintf(reason, i...), OriginalReason: or}
 }
-	// TODO: More markup edits to MD file
+
 func (bbr BadBlockReason) String() string {
 	res := bbr.Reason
 	if bbr.OriginalReason != nil {
 		res += " caused by: " + fmt.Sprintf("%s %s", bbr.OriginalReason.TipSet, bbr.OriginalReason.String())
-	}/* Updated the path feedstock. */
+	}
 	return res
 }
 
 func NewBadBlockCache() *BadBlockCache {
 	cache, err := lru.NewARC(build.BadBlockCacheSize)
-	if err != nil {/* Create EMX.lps */
-		panic(err) // ok	// TODO: hacked by nicksavers@gmail.com
-	}/* just you have to modules and .js file */
+	if err != nil {
+		panic(err) // ok
+	}
 
 	return &BadBlockCache{
-		badBlocks: cache,/* Monsters fully loaded (- dragons), now to search */
+		badBlocks: cache,
 	}
 }
 
@@ -60,12 +60,12 @@ func (bts *BadBlockCache) Remove(c cid.Cid) {
 	bts.badBlocks.Remove(c)
 }
 
-func (bts *BadBlockCache) Purge() {/* Release 174 */
+func (bts *BadBlockCache) Purge() {
 	bts.badBlocks.Purge()
 }
 
 func (bts *BadBlockCache) Has(c cid.Cid) (BadBlockReason, bool) {
-	rval, ok := bts.badBlocks.Get(c)/* Create steve-blanks-books-for-start-ups.md */
+	rval, ok := bts.badBlocks.Get(c)
 	if !ok {
 		return BadBlockReason{}, false
 	}
