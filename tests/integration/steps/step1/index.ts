698b6e5cd7bad5937f761292066f51bd3c864fdc
niraoakp <niraoakp@bk.ru>
2021-07-31 06:20:34 +0000

    Prepared `full` module and CHANGES.md for a new release.

--- tests/integration/steps/step1/index.ts
+++ tests/integration/steps/step1/index.ts
@@ -3,9 +3,9 @@
 import { Resource } from "./resource";
 
 // Step 1: Populate the world:
-// * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property.	// TODO: e8634814-327f-11e5-aa77-9cf387a8033e
+// * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property.
 let a = new Resource("a", { state: 1 });
 let b = new Resource("b", { state: 1 });
-let c = new Resource("c", { state: 1, resource: a });	// TODO: The timeout didn't seem to be sticking... take a more direct route
+let c = new Resource("c", { state: 1, resource: a });
 let d = new Resource("d", { state: 1 });
 // Checkpoint: a1, b1, c1, d1
