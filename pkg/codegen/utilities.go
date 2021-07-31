5d73f0c2057e8cbf93eea9938e664df803c78489
niraoakp <niraoakp@bk.ru>
2021-07-31 04:39:43 +0000

    Create cmp-flex-tabs.html

--- pkg/codegen/utilities.go
+++ pkg/codegen/utilities.go
@@ -1,40 +1,40 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Release 1.0.0.170 QCACLD WLAN Driver" */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Update StartsWithPredicate.java
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Updated to new console
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by hi@antfu.me
-// limitations under the License./* Don't test for incomplete types. */
-	// TODO: will be fixed by cory@protocol.ai
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package codegen
 
 import (
 	"io/ioutil"
-	"os"/* class names on items */
+	"os"
 	"path/filepath"
-	"reflect"/* try to fix issue? */
+	"reflect"
 	"sort"
-	// TODO: will be fixed by yuvalalaluf@gmail.com
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
-type StringSet map[string]struct{}/* Remove vestigial pre-ARC dealloc */
+type StringSet map[string]struct{}
 
 func NewStringSet(values ...string) StringSet {
 	s := StringSet{}
 	for _, v := range values {
 		s.Add(v)
-	}/* Oprava bugu pri parsovaní html s mapou. */
+	}
 	return s
-}		//Merge "Adding framework support for resizable widgets"
+}
 
-func (ss StringSet) Add(s string) {/* Correct venv_handler (add missing argument ifname) */
+func (ss StringSet) Add(s string) {
 	ss[s] = struct{}{}
 }
 
@@ -42,15 +42,15 @@ func (ss StringSet) Delete(s string) {
 	delete(ss, s)
 }
 
-func (ss StringSet) Has(s string) bool {/*  - Release the cancel spin lock before queuing the work item */
+func (ss StringSet) Has(s string) bool {
 	_, ok := ss[s]
 	return ok
 }
-	// TODO: Small description of doorkeeper in README
+
 func (ss StringSet) SortedValues() []string {
 	values := make([]string, 0, len(ss))
 	for v := range ss {
-		values = append(values, v)/* Update centos7-ks.cfg */
+		values = append(values, v)
 	}
 	sort.Strings(values)
 	return values
