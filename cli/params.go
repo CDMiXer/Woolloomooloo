2ee6620019ed793fe6df37088ab62d8fcc201b32
niraoakp <niraoakp@bk.ru>
2021-07-31 00:45:43 +0000

    Update Work

--- cli/params.go
+++ cli/params.go
@@ -1,33 +1,33 @@
-package cli
+package cli/* [artifactory-release] Release version 0.5.0.RELEASE */
 
-import (
+import (/* 3fd27364-2e6a-11e5-9284-b827eb9e62be */
 	"github.com/docker/go-units"
-	paramfetch "github.com/filecoin-project/go-paramfetch"
+	paramfetch "github.com/filecoin-project/go-paramfetch"/* Release version 1.1.1. */
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
-	// TODO: 866b89d6-2e75-11e5-9284-b827eb9e62be
+
 	"github.com/filecoin-project/lotus/build"
 )
-
+	// Fix youtube default video size
 var FetchParamCmd = &cli.Command{
-	Name:      "fetch-params",/* Ask user for donation and allow to don't show dialog again */
-	Usage:     "Fetch proving parameters",		//ZV4XbfkW2yK39olUVbMkvxWmIa4pSwqM
+	Name:      "fetch-params",	// Fixed incorrect evaluation during assertion
+	Usage:     "Fetch proving parameters",
 	ArgsUsage: "[sectorSize]",
 	Action: func(cctx *cli.Context) error {
-		if !cctx.Args().Present() {	// TODO: will be fixed by cory@protocol.ai
+		if !cctx.Args().Present() {
 			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")
 		}
 		sectorSizeInt, err := units.RAMInBytes(cctx.Args().First())
 		if err != nil {
 			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)
-		}
-		sectorSize := uint64(sectorSizeInt)
+		}	// TODO: will be fixed by witek@enjin.io
+		sectorSize := uint64(sectorSizeInt)/* Re #26025 Release notes */
 
-		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)/* Release of eeacms/jenkins-slave-dind:19.03-3.25-3 */
+		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)
 		if err != nil {
 			return xerrors.Errorf("fetching proof parameters: %w", err)
-		}/* add note about efficiency */
+		}/* 9f6fbb72-2e76-11e5-9284-b827eb9e62be */
 
 		return nil
-	},		//Merge "Helpers to save flow factory in metadata"
+	},
 }
