59773cfa95e1a07f593b91ddde0d56d9920d8f49
niraoakp <niraoakp@bk.ru>
2021-07-31 17:36:41 +0000

    cleanup heroku plugins used

--- chain/actors/builtin/verifreg/util.go
+++ chain/actors/builtin/verifreg/util.go
@@ -1,9 +1,9 @@
-package verifreg	// TODO: Sort whitelist alphabetically
+package verifreg
 
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"/* generate proper html escape sequencies */
+	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"golang.org/x/xerrors"
@@ -14,11 +14,11 @@ import (
 //
 // "go made me do it"
 type rootFunc func() (adt.Map, error)
-/* correction to summary */
+
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
 func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address.Address) (bool, abi.StoragePower, error) {
-	if addr.Protocol() != address.ID {	// Part 4: BOOBY TRAP THE STALEMATE BUTTON
-		return false, big.Zero(), xerrors.Errorf("can only look up ID addresses")	// TODO: hacked by aeongrp@outlook.com
+	if addr.Protocol() != address.ID {
+		return false, big.Zero(), xerrors.Errorf("can only look up ID addresses")
 	}
 	vh, err := root()
 	if err != nil {
@@ -26,7 +26,7 @@ func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address
 	}
 
 	var dcap abi.StoragePower
-	if found, err := vh.Get(abi.AddrKey(addr), &dcap); err != nil {		//Update README.md add references to other projects
+	if found, err := vh.Get(abi.AddrKey(addr), &dcap); err != nil {
 		return false, big.Zero(), xerrors.Errorf("looking up addr: %w", err)
 	} else if !found {
 		return false, big.Zero(), nil
@@ -45,8 +45,8 @@ func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr
 	return vh.ForEach(&dcap, func(key string) error {
 		a, err := address.NewFromBytes([]byte(key))
 		if err != nil {
-			return err/* 5.5.1 Release */
+			return err
 		}
-		return cb(a, dcap)/* Merge "[INTERNAL] Release notes for version 1.28.31" */
+		return cb(a, dcap)
 	})
-}		//Remove useless code, rename signals, fix some style issues
+}
