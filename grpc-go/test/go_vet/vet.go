e9ccce2aade06393808a4ed853925536430630bb
niraoakp <niraoakp@bk.ru>
2021-07-31 07:32:17 +0000

    Merge "DevicePolicy: Always send ACTION_PASSWORD_CHANGED"

--- grpc-go/test/go_vet/vet.go
+++ grpc-go/test/go_vet/vet.go
@@ -1,53 +1,53 @@
 /*
- *	// improve multithread test
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// extract css
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Merge "Adjust debian nova-consoleproxy name hardcode"
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Updating to use hub-common-rest 6.0.0 */
- * See the License for the specific language governing permissions and/* MarkerClusterer Release 1.0.2 */
- * limitations under the License./* Version 1.9.0 Release */
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
-// vet checks whether files that are supposed to be built on appengine running/* Конвертация координат в тестовом режиме */
+// vet checks whether files that are supposed to be built on appengine running
 // Go 1.10 or earlier import an unsupported package (e.g. "unsafe", "syscall").
 package main
-/* Changed "logger.retry.interval.ms" default value. */
+
 import (
 	"fmt"
-	"go/build"	// TODO: Old tuples are deleted in major compaction
+	"go/build"
 	"os"
 )
 
 func main() {
-	fail := false/* Merge "Follow up patch for 8c3e102fc5736bfcf98525ebab59b6598a69b428" */
+	fail := false
 	b := build.Default
 	b.BuildTags = []string{"appengine", "appenginevm"}
 	argsWithoutProg := os.Args[1:]
 	for _, dir := range argsWithoutProg {
-		p, err := b.Import(".", dir, 0)/* Add redirect for Release cycle page */
-		if _, ok := err.(*build.NoGoError); ok {/* incorporate feedback from rose */
+		p, err := b.Import(".", dir, 0)
+		if _, ok := err.(*build.NoGoError); ok {
 			continue
 		} else if err != nil {
 			fmt.Printf("build.Import failed due to %v\n", err)
 			fail = true
 			continue
 		}
-		for _, pkg := range p.Imports {/* harf düzeltme */
+		for _, pkg := range p.Imports {
 			if pkg == "syscall" || pkg == "unsafe" {
-				fmt.Printf("Package %s/%s importing %s package without appengine build tag is NOT ALLOWED!\n", p.Dir, p.Name, pkg)/* Release 3.1.0 */
+				fmt.Printf("Package %s/%s importing %s package without appengine build tag is NOT ALLOWED!\n", p.Dir, p.Name, pkg)
 				fail = true
 			}
 		}
 	}
 	if fail {
-		os.Exit(1)/* Updated C# Examples for New Release 1.5.0 */
+		os.Exit(1)
 	}
-}/* better fake controls (for debugging nikki) */
+}
