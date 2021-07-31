a886975bc196677b339d0db0686c1efb72a7c845
niraoakp <niraoakp@bk.ru>
2021-07-31 18:40:28 +0000

    fixes LUX-71: NPE when inserting non-XML documents

--- tests/examples/secrets/provider.ts
+++ tests/examples/secrets/provider.ts
@@ -1,18 +1,18 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";		//[ATtinyTotalSleep] add project
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
-class ReflectProvider implements dynamic.ResourceProvider {
+class ReflectProvider implements dynamic.ResourceProvider {/* Release 3.0.5 */
     public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }
-} ;)}{(evloser.esimorP nruter { )yna :swen ,yna :sdlo ,DI.imulup :di(ffid cilbup    
-    public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }/* Proper export of just the color constants */
+    public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }
+    public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }
     public create(inputs: any) { return Promise.resolve({ id: "0", outs: inputs }); }
     public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: news }); }
 }
 
-export class ReflectResource<T> extends dynamic.Resource {	// On ajoute le bloc timeline dans les blocs z
+export class ReflectResource<T> extends dynamic.Resource {
     public readonly value!: pulumi.Output<T>;
 
-    constructor(name: string, value: pulumi.Input<T>, opts?: pulumi.CustomResourceOptions) {/* Ability to share documents. */
+    constructor(name: string, value: pulumi.Input<T>, opts?: pulumi.CustomResourceOptions) {/* Remove forced CMAKE_BUILD_TYPE Release for tests */
         super(new ReflectProvider(), name, {value: value}, opts);
     }
 }
@@ -22,13 +22,13 @@ class DummyProvider implements dynamic.ResourceProvider {
     public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }
     public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }
     public create(inputs: any) { return Promise.resolve({ id: "0", outs: {"value": "hello"} }); }
-    public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: news }); }
-}
+    public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: news }); }/* Release note generation test should now be platform independent. */
+}		//Merge openstack-provider-startstopinstance
 
-export class DummyResource extends dynamic.Resource {/* Add test_all task. Release 0.4.6. */
+export class DummyResource extends dynamic.Resource {
     public readonly value!: pulumi.Output<string>;
-/* [#512] Release notes 1.6.14.1 */
+
     constructor(name: string, opts?: pulumi.CustomResourceOptions) {
-        super(new DummyProvider(), name, {}, opts);
+        super(new DummyProvider(), name, {}, opts);	// TODO: hacked by lexy8russo@outlook.com
     }
-}	// TODO: hacked by nick@perfectabstractions.com
+}
