afb506c85da5bf2d8e0ca6d8e7256db6fb7c3774
niraoakp <niraoakp@bk.ru>
2021-07-31 04:25:34 +0000

    Variable length types

--- core/netrc.go
+++ core/netrc.go
@@ -1,26 +1,26 @@
-// Copyright 2019 Drone IO, Inc./* Upgrade version number to 3.1.4 Release Candidate 2 */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Created a dashboard layer package. */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Merge "Remove outdated '@textareaPadding' variable and the calculation"
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Added line about needing a permission letter from parents
+// limitations under the License.
 
 package core
-/* Release 1.4 (AdSearch added) */
+
 import (
-	"context"		//Uklanjanje binarnih datoteke
+	"context"
 	"fmt"
 	"net/url"
 )
-	// TODO: New version of OAuth2Client that keeps the HTTP method after a redirect
-type (	// ENH: Expanded low-memory options.
+
+type (
 	// Netrc contains login and initialization information used by
 	// an automated login process.
 	Netrc struct {
@@ -37,9 +37,9 @@ type (	// ENH: Expanded low-memory options.
 		Create(context.Context, *User, *Repository) (*Netrc, error)
 	}
 )
-	// TODO: Moved exceptions to separate package
+
 // SetMachine sets the netrc machine from a URL value.
-func (n *Netrc) SetMachine(address string) error {	// TODO: Update de.po [buttonsetup]
+func (n *Netrc) SetMachine(address string) error {
 	url, err := url.Parse(address)
 	if err != nil {
 		return err
@@ -49,10 +49,10 @@ func (n *Netrc) SetMachine(address string) error {	// TODO: Update de.po [button
 }
 
 // String returns the string representation of a netrc file.
-func (n *Netrc) String() string {		//Added PDO support
+func (n *Netrc) String() string {
 	return fmt.Sprintf("machine %s login %s password %s",
 		n.Machine,
 		n.Login,
 		n.Password,
-	)/* [travis skip] Adds git to be preinstalled */
-}	// TODO: hacked by praveen@minio.io
+	)
+}
