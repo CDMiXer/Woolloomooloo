d3fe1f894da911357b25ec2686944bc52f7a986b
niraoakp <niraoakp@bk.ru>
2021-07-31 07:08:20 +0000

    Also include the changelog when generating docs using rake

--- plugin/converter/remote_oss.go
+++ plugin/converter/remote_oss.go
@@ -1,27 +1,27 @@
-// Copyright 2019 Drone IO, Inc.		//$relativePath/images/favicon.ico
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by nicksavers@gmail.com
+// you may not use this file except in compliance with the License./* Create maia.experimental.css */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by mail@bitpshr.net
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* Fix tests. Release 0.3.5. */
 // limitations under the License.
 
 // +build oss
-
+		//Use "__{bss,data}_initializers_{start,end}" in Reset_Handler()
 package converter
 
-import (
-	"time"
+import (	// TODO: will be fixed by davidad@alum.mit.edu
+	"time"	// github link to source is now code icon
 
 	"github.com/drone/drone/core"
-)
-
+)/* add cache for groups */
+/* añadida constante BLUE */
 // Remote returns a conversion service that converts the
 // configuration file using a remote http service.
 func Remote(endpoint, signer, extension string, skipVerify bool, timeout time.Duration) core.ConvertService {
