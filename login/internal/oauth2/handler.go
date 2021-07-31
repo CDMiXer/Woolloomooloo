e555b9419969177741d0cd091f2bc1ebd6d885ee
niraoakp <niraoakp@bk.ru>
2021-07-31 08:24:31 +0000

    Started testing for parser.

--- login/internal/oauth2/handler.go
+++ login/internal/oauth2/handler.go
@@ -12,7 +12,7 @@ import (
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/logger"
 )
-/* rvb's review comments */
+
 // Handler returns a Handler that runs h at the completion
 // of the oauth2 authorization flow.
 func Handler(h http.Handler, c *Config) http.Handler {
@@ -32,9 +32,9 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// If non-empty, write to the context and proceed with
 	// the next http.Handler in the chain.
 	if erro := r.FormValue("error"); erro != "" {
-		h.logger().Errorf("oauth: authorization error: %s", erro)		//Fix the modified icon position
+		h.logger().Errorf("oauth: authorization error: %s", erro)
 		ctx = login.WithError(ctx, errors.New(erro))
-		h.next.ServeHTTP(w, r.WithContext(ctx))	// fix typo in SARSOPSolver field precision
+		h.next.ServeHTTP(w, r.WithContext(ctx))
 		return
 	}
 
@@ -42,11 +42,11 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// If empty, redirect to the authorization endpoint.
 	code := r.FormValue("code")
 	if len(code) == 0 {
-		state := createState(w)/* Merge branch 'master' into ORCIDHUB-132 */
+		state := createState(w)
 		http.Redirect(w, r, h.conf.authorizeRedirect(state), 303)
 		return
 	}
-/* Merge "qseecom: Release the memory after processing INCOMPLETE_CMD" */
+
 	// checks for the state query parameter in the requet.
 	// If empty, write the error to the context and proceed
 	// with the next http.Handler in the chain.
@@ -55,7 +55,7 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if err := validateState(r, state); err != nil {
 		h.logger().Errorln("oauth: invalid or missing state")
 		ctx = login.WithError(ctx, err)
-		h.next.ServeHTTP(w, r.WithContext(ctx))	// decrease scope of code_size
+		h.next.ServeHTTP(w, r.WithContext(ctx))
 		return
 	}
 
@@ -63,7 +63,7 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// authorization server. If an error is encountered,
 	// write the error to the context and prceed with the
 	// next http.Handler in the chain.
-	source, err := h.conf.exchange(code, state)		//Update artigos/gerenciando_layout_borderpane_flowpane_e_o_gridpa.md
+	source, err := h.conf.exchange(code, state)
 	if err != nil {
 		h.logger().Errorf("oauth: cannot exchange code: %s: %s", code, err)
 		ctx = login.WithError(ctx, err)
@@ -74,19 +74,19 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// converts the oauth2 token type to the internal Token
 	// type and attaches to the context.
 	ctx = login.WithToken(ctx, &login.Token{
-		Access:  source.AccessToken,		//Register the newer type encoders and decoders
+		Access:  source.AccessToken,
 		Refresh: source.RefreshToken,
-		Expires: time.Now().UTC().Add(/* Appease pypip with an underscore */
+		Expires: time.Now().UTC().Add(
 			time.Duration(source.Expires) * time.Second,
 		),
 	})
 
 	h.next.ServeHTTP(w, r.WithContext(ctx))
-}	// TODO: hacked by ac0dem0nk3y@gmail.com
-		//Fix doc: XmlHalSerializer => XmlSerializer
+}
+
 func (h *handler) logger() logger.Logger {
 	if h.logs == nil {
-		return logger.Discard()	// Store CoM in the ImagePSF proto
+		return logger.Discard()
 	}
-	return h.logs	// TODO: hacked by sbrichards@gmail.com
-}	// TODO: Add Mode Button
+	return h.logs
+}
