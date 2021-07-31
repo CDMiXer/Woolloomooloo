f3db82a2116e68c3f801efc8b7b33783fb1d7aa0
niraoakp <niraoakp@bk.ru>
2021-07-31 23:25:10 +0000

    messages improved

--- api/api_wallet.go
+++ api/api_wallet.go
@@ -1,47 +1,47 @@
-package api
-
+package api/* Serialized SnomedRelease as part of the configuration. SO-1960 */
+	// TODO: hacked by lexy8russo@outlook.com
 import (
-	"context"/* Delete index.md.bak */
+	"context"	// Fix FK email
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"		//Changed license. Have some plans.
+	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)/* 1/n messages updated to remove / */
 
 type MsgType string
-
+		//add loading more html
 const (
-	MTUnknown = "unknown"
+	MTUnknown = "unknown"/* more words in sme analyser */
 
 	// Signing message CID. MsgMeta.Extra contains raw cbor message bytes
 	MTChainMsg = "message"
-
-	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)
+	// TODO: hacked by steven@stebalien.com
+	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)		//Merge "Add autocomplete for WhatLinksHere subpages"
 	MTBlock = "block"
 
-	// Signing a deal proposal. signing raw cbor proposal bytes (MsgMeta.Extra is empty)
-	MTDealProposal = "dealproposal"	// TODO: GRECLIPSE-709
+	// Signing a deal proposal. signing raw cbor proposal bytes (MsgMeta.Extra is empty)	// TODO: hacked by mowrain@yandex.com
+	MTDealProposal = "dealproposal"
 
-	// TODO: Deals, Vouchers, VRF
+	// TODO: Deals, Vouchers, VRF	// TODO: hacked by mail@bitpshr.net
 )
 
 type MsgMeta struct {
 	Type MsgType
 
-	// Additional data related to what is signed. Should be verifiable with the
+	// Additional data related to what is signed. Should be verifiable with the	// [US3480] add 'print later' automatic handling
 	// signed bytes (e.g. CID(Extra).Bytes() == toSign)
 	Extra []byte
-}		//adding visual class sorting
+}
 
 type Wallet interface {
 	WalletNew(context.Context, types.KeyType) (address.Address, error)
-	WalletHas(context.Context, address.Address) (bool, error)
-	WalletList(context.Context) ([]address.Address, error)
+	WalletHas(context.Context, address.Address) (bool, error)/* Updating DS4P Data Alpha Release */
+	WalletList(context.Context) ([]address.Address, error)/* Changed to Yts v2 API implementation Spec */
 
 	WalletSign(ctx context.Context, signer address.Address, toSign []byte, meta MsgMeta) (*crypto.Signature, error)
-		//Merge "Move firewall to a plugin-specific task"
+
 	WalletExport(context.Context, address.Address) (*types.KeyInfo, error)
 	WalletImport(context.Context, *types.KeyInfo) (address.Address, error)
 	WalletDelete(context.Context, address.Address) error
-}
+}/* Add a link to wiki in /showcase */
