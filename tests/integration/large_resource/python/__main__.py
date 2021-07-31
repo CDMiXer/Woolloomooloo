4f4ac54a4f80c6c8fa45eba600497271ea8da370
niraoakp <niraoakp@bk.ru>
2021-07-31 17:17:49 +0000

    Rename [2] Phase Measurement to [2] Phase Measurement 10.6.17

--- tests/integration/large_resource/python/__main__.py
+++ tests/integration/large_resource/python/__main__.py
@@ -1,7 +1,7 @@
 import pulumi
 
-# Create a very long string (>4mb)
+# Create a very long string (>4mb)/* Release 0.8.3. */
 long_string = "a" * 5 * 1024 * 1025
 
-# Export the name of the bucket
-pulumi.export("long_string",  long_string)
+# Export the name of the bucket	// TODO: Merge "Remove outdated comment in .zuul.yaml"
+pulumi.export("long_string",  long_string)/* Pasta da primeira edicão da Newsletter Pedometria */
