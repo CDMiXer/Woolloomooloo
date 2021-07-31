c9ba8135f17c6620ab355c9cb1aec0b94ed331e8
niraoakp <niraoakp@bk.ru>
2021-07-31 02:47:09 +0000

    Исправлена ошибка с подключением html-редактора на странице создания статьи

--- tests/integration/partial_state/step3/index.ts
+++ tests/integration/partial_state/step3/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "@pulumi/pulumi";	// TODO: Update version of sbt-idea. Closes #29
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Update greek translations
+/* let firstrun.sh download the correct jar file */
+import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-/* Initial Release: Inverter Effect */
+
 // resource "not-doomed" is created successfully.
 const a = new Resource("not-doomed", 5);
 
