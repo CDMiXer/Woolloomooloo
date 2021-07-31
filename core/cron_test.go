e37d5a90dab03ed4c7973565894b87edcc021988
niraoakp <niraoakp@bk.ru>
2021-07-31 22:31:07 +0000

    Added JSSymbolicRegressionProblemTest.

--- core/cron_test.go
+++ core/cron_test.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* Release 1.4.0.1 */
 
 package core
