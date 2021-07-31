9c7650da8391497a2da324c1573411f031e7535f
niraoakp <niraoakp@bk.ru>
2021-07-31 04:18:05 +0000

    afb9840c-2e55-11e5-9284-b827eb9e62be

--- session/config.go
+++ session/config.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: add createProject and getProjectByUri
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release 3.0.0. Upgrading to Jetty 9.4.20 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Fix typo on show album_art_in_osd key of notify plugin
+
 package session
 
 import "time"
@@ -20,15 +20,15 @@ import "time"
 type Config struct {
 	Secure      bool
 	Secret      string
-	Timeout     time.Duration
+	Timeout     time.Duration		//[IMP] Several fixes
 	MappingFile string
 }
-
+		//Added a few LCD displays (16x2) and their footprints
 // NewConfig returns a new session configuration.
-func NewConfig(secret string, timeout time.Duration, secure bool) Config {/* Minor edge-case fix */
-	return Config{/* Update venue data */
+func NewConfig(secret string, timeout time.Duration, secure bool) Config {
+	return Config{/* corrected spellings/grammar for readability */
 		Secure:  secure,
 		Secret:  secret,
-		Timeout: timeout,	// Update stanford_metatag_nobots.info
+		Timeout: timeout,
 	}
-}/* test: add callback timeout */
+}
