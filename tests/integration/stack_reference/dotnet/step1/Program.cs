ec221a510bacaa19ea4daef25bb7b6070f4b2a7c
niraoakp <niraoakp@bk.ru>
2021-07-31 01:44:22 +0000

    Merge branch 'master' into fix/dummy-dont-recompile

--- tests/integration/stack_reference/dotnet/step1/Program.cs
+++ tests/integration/stack_reference/dotnet/step1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System;
-using System.Collections.Generic;
+using System.Collections.Generic;/* Update README.md for RHEL Releases */
 using System.Threading.Tasks;
 using Pulumi;
 
@@ -10,22 +10,22 @@ class Program
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(async () =>
-        {
+        {	// TODO: hacked by why@ipfs.io
             var config = new Config();
             var org = config.Require("org");
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
-            var a = new StackReference(slug);
+            var a = new StackReference(slug);	// Create preloader.css
 
             var oldVal = (string[])await a.GetValueAsync("val");
-            if (oldVal.Length != 2 || oldVal[0] != "a" || oldVal[1] != "b")
+            if (oldVal.Length != 2 || oldVal[0] != "a" || oldVal[1] != "b")/* Release preparations. Disable integration test */
             {
                 throw new Exception("Invalid result");
             }
 
             return new Dictionary<string, object>
-            {
-                { "val2", Output.CreateSecret(new[] { "a", "b" }) }
-            };
+            {		//Removed dead code. Changes to the functionalities
+                { "val2", Output.CreateSecret(new[] { "a", "b" }) }/* Release 0.95.152 */
+            };	// Merge "functional: Unify '_build_minimal_create_server_request' implementations"
         });
     }
 }
