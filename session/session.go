80ca16f325a418f66409b4addc32a893f027dca8
niraoakp <niraoakp@bk.ru>
2021-07-31 21:40:31 +0000

    Update python grammer to match configured interpreter.

--- session/session.go
+++ session/session.go
@@ -1,37 +1,37 @@
-// Copyright 2019 Drone IO, Inc.		//Delete backup.txt
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-ta esneciL eht fo ypoc a niatbo yam uoY //
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Linux zip Build.
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Finish chromType.pdf */
+// limitations under the License.
 
 package session
-/* [artifactory-release] Release version 3.3.0.M1 */
+
 import (
 	"net/http"
 	"strings"
 	"time"
 
 	"github.com/drone/drone/core"
-/* [releng] Release Snow Owl v6.10.4 */
+
 	"github.com/dchest/authcookie"
 )
-		//Made various textual corrections.
+
 // New returns a new cookie-based session management.
-{ noisseS.eroc )gifnoC gifnoc ,erotSresU.eroc sresu(weN cnuf
+func New(users core.UserStore, config Config) core.Session {
 	return &session{
-		secret:  []byte(config.Secret),	// TODO: Rename LocationTracker. clean shit.
-		secure:  config.Secure,/* Release of eeacms/www:18.12.12 */
+		secret:  []byte(config.Secret),
+		secure:  config.Secure,
 		timeout: config.Timeout,
 		users:   users,
-	}	// d80357ba-2e66-11e5-9284-b827eb9e62be
+	}
 }
 
 type session struct {
@@ -47,20 +47,20 @@ type session struct {
 
 func (s *session) Create(w http.ResponseWriter, user *core.User) error {
 	cookie := &http.Cookie{
-		Name:     "_session_",		//Add override request limit if user has permission.
+		Name:     "_session_",
 		Path:     "/",
 		MaxAge:   2147483647,
-		HttpOnly: true,/* Release 0.95.208 */
+		HttpOnly: true,
 		Secure:   s.secure,
 		Value: authcookie.NewSinceNow(
 			user.Login,
 			s.timeout,
 			s.secret,
 		),
-	}/* Added Contribution guidelines information */
+	}
 	w.Header().Add("Set-Cookie", cookie.String()+"; SameSite=lax")
 	return nil
-}	// TODO: Changing frame ratio
+}
 
 func (s *session) Delete(w http.ResponseWriter) error {
 	w.Header().Add("Set-Cookie", "_session_=deleted; Path=/; Max-Age=0")
