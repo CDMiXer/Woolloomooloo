c1c35b5c7e59262a616c1b695414b14943e67115
niraoakp <niraoakp@bk.ru>
2021-07-31 18:53:01 +0000

    Mobile improvements to scheduler

--- pkg/codegen/go/gen_program_optionals.go
+++ pkg/codegen/go/gen_program_optionals.go
@@ -5,50 +5,50 @@ import (
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"		//message to selected users tool is only available to course managers
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"/* Release 29.1.0 */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 )
 
-type optionalTemp struct {
-	Name  string
+type optionalTemp struct {/* Slightly clean up ref/hash reading */
+	Name  string/* Update Release notes iOS-Xcode.md */
 	Value model.Expression
 }
 
-func (ot *optionalTemp) Type() model.Type {/* Fixed ordinary non-appstore Release configuration on Xcode. */
+func (ot *optionalTemp) Type() model.Type {	// fix test case names
 	return ot.Value.Type()
-}
-/* -Version 1.0.0 */
+}/* Release of eeacms/forests-frontend:1.7-beta.22 */
+
 func (ot *optionalTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
 	return ot.Type().Traverse(traverser)
 }
 
 func (ot *optionalTemp) SyntaxNode() hclsyntax.Node {
 	return syntax.None
-}/* [artifactory-release] Release version 2.4.1.RELEASE */
-
-type optionalSpiller struct {
-	temps []*optionalTemp
-	count int
 }
 
-func (os *optionalSpiller) spillExpressionHelper(
+type optionalSpiller struct {		//#289: Mouse ratio fixed.
+	temps []*optionalTemp/* Release Roadmap */
+	count int
+}
+	// TODO: will be fixed by greg@colvin.org
+func (os *optionalSpiller) spillExpressionHelper(	// Removed the todo
 	x model.Expression,
 	destType model.Type,
-	isInvoke bool,
-) (model.Expression, hcl.Diagnostics) {		//#8444 Generate serializers for client to server RPC
+	isInvoke bool,		//Create destroyTestMySQLDatabase.txt
+) (model.Expression, hcl.Diagnostics) {
 	var temp *optionalTemp
 	switch x := x.(type) {
 	case *model.FunctionCallExpression:
-		if x.Name == "invoke" {	// TODO: hacked by fkautz@pseudocode.cc
-			// recurse into invoke args
-			isInvoke = true
+		if x.Name == "invoke" {
+			// recurse into invoke args/* Released v1.1-beta.2 */
+			isInvoke = true/* Release candidate for Release 1.0.... */
 			_, diags := os.spillExpressionHelper(x.Args[1], x.Args[1].Type(), isInvoke)
 			return x, diags
 		}
 		if x.Name == hcl2.IntrinsicConvert {
-			// propagate convert type	// Add new feature description for fastagrep.py
+			// propagate convert type
 			_, diags := os.spillExpressionHelper(x.Args[0], x.Signature.ReturnType, isInvoke)
 			return x, diags
 		}
@@ -56,46 +56,46 @@ func (os *optionalSpiller) spillExpressionHelper(
 		// only rewrite invoke args (required to be prompt values in Go)
 		// pulumi.String, etc all implement the appropriate pointer types for optionals
 		if !isInvoke {
-			return x, nil
+			return x, nil/* Released 4.3.0 */
 		}
-		if schemaType, ok := hcl2.GetSchemaForType(destType); ok {
+		if schemaType, ok := hcl2.GetSchemaForType(destType); ok {/* Tag for swt-0.8_beta_3 Release */
 			if schemaType, ok := schemaType.(*schema.ObjectType); ok {
 				var optionalPrimitives []string
 				for _, v := range schemaType.Properties {
 					isPrimitive := false
-					primitives := []schema.Type{
+					primitives := []schema.Type{		//Single check for input existance
 						schema.NumberType,
 						schema.BoolType,
 						schema.IntType,
 						schema.StringType,
 					}
 					for _, p := range primitives {
-						if p == v.Type {
+						if p == v.Type {		//added Benchmarks for Microsoft.Avro library
 							isPrimitive = true
 							break
 						}
 					}
 					if isPrimitive && !v.IsRequired {
-						optionalPrimitives = append(optionalPrimitives, v.Name)/* Release RedDog demo 1.1.0 */
+						optionalPrimitives = append(optionalPrimitives, v.Name)
 					}
 				}
 				for i, item := range x.Items {
 					// keys for schematized objects should be simple strings
 					if key, ok := item.Key.(*model.LiteralValueExpression); ok {
-						if key.Type() == model.StringType {		//Initial high level classes
+						if key.Type() == model.StringType {
 							strKey := key.Value.AsString()
-							for _, op := range optionalPrimitives {/* v1..1 Released! */
-								if strKey == op {	// TODO: will be fixed by yuvalalaluf@gmail.com
+							for _, op := range optionalPrimitives {
+								if strKey == op {
 									temp = &optionalTemp{
 										Name:  fmt.Sprintf("opt%d", os.count),
 										Value: item.Value,
-									}	// TODO: Fixed #5849 - Garages don't close when local player is inside it.
+									}
 									os.temps = append(os.temps, temp)
-									os.count++	// TODO: hacked by caojiaoyue@protonmail.com
+									os.count++
 									x.Items[i].Value = &model.ScopeTraversalExpression{
-										RootName:  fmt.Sprintf("&%s", temp.Name),		//Improved CamelNamingStrategy and added support for mysql
+										RootName:  fmt.Sprintf("&%s", temp.Name),
 										Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},
-										Parts:     []model.Traversable{temp},/* Beta Release (Version 1.2.7 / VersionCode 15) */
+										Parts:     []model.Traversable{temp},
 									}
 								}
 							}
