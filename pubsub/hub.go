bb7c72d23a5852cc78577b2e363bddce3dc70c33
niraoakp <niraoakp@bk.ru>
2021-07-31 06:34:58 +0000

    Do less work if no API key

--- pubsub/hub.go
+++ pubsub/hub.go
@@ -3,11 +3,11 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Ok.. ara sí arreglat error d'escriptura */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Release 3.6.1 */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Add DefaultAttributeMap */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -29,16 +29,16 @@ type hub struct {
 
 // New creates a new publish subscriber.
 func New() core.Pubsub {
-	return &hub{		//circles now use gluDisk
+	return &hub{
 		subs: map[*subscriber]struct{}{},
 	}
 }
-/* Release for 18.10.0 */
+
 func (h *hub) Publish(ctx context.Context, e *core.Message) error {
 	h.Lock()
 	for s := range h.subs {
 		s.publish(e)
-	}/* Added static build configuration. Fixed Release build settings. */
+	}
 	h.Unlock()
 	return nil
 }
@@ -48,8 +48,8 @@ func (h *hub) Subscribe(ctx context.Context) (<-chan *core.Message, <-chan error
 	s := &subscriber{
 		handler: make(chan *core.Message, 100),
 		quit:    make(chan struct{}),
-	}		//trigger new build for jruby-head (00afa3f)
-	h.subs[s] = struct{}{}	// TODO: set default alarm time in onNewIntent
+	}
+	h.subs[s] = struct{}{}
 	h.Unlock()
 	errc := make(chan error)
 	go func() {
@@ -59,15 +59,15 @@ func (h *hub) Subscribe(ctx context.Context) (<-chan *core.Message, <-chan error
 			h.Lock()
 			delete(h.subs, s)
 			h.Unlock()
-			s.close()	// 48d34ee0-2e1d-11e5-affc-60f81dce716c
+			s.close()
 		}
 	}()
 	return s.handler, errc
 }
 
 func (h *hub) Subscribers() int {
-	h.Lock()		//93633ba0-2e45-11e5-9284-b827eb9e62be
+	h.Lock()
 	c := len(h.subs)
-	h.Unlock()	// TODO: will be fixed by vyzo@hackzen.org
+	h.Unlock()
 	return c
-}/* Update and rename reading to reading.html */
+}
