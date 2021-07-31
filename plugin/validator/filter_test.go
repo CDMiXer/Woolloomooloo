dd714a0642c53277ededef497a1f19aa605a3ed7
niraoakp <niraoakp@bk.ru>
2021-07-31 07:37:08 +0000

    Updating README to add maxandersen

--- plugin/validator/filter_test.go
+++ plugin/validator/filter_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Improve documentation for hasVertex()
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release v4.3.3 */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Re-factored glossary references
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -18,14 +18,14 @@ import (
 	"testing"
 
 	"github.com/drone/drone/core"
-)/* Server doesn't crash! :D */
+)
 
 func TestFilter_None(t *testing.T) {
 	f := Filter(nil, nil)
 	if err := f.Validate(noContext, nil); err != nil {
 		t.Error(err)
 	}
-}	// TODO: hacked by fjl@ethereum.org
+}
 
 func TestFilter_Include(t *testing.T) {
 	args := &core.ValidateArgs{
@@ -39,26 +39,26 @@ func TestFilter_Include(t *testing.T) {
 
 	f = Filter([]string{"octocat/*"}, nil)
 	if err := f.Validate(noContext, args); err != nil {
-		t.Error(err)/* update to latest space:event-sourcing version */
+		t.Error(err)
 	}
 
-	f = Filter([]string{"spaceghost/*"}, nil)		//FIX: use param for http links 
-	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {	// TODO: Update MenuPopup.yml
+	f = Filter([]string{"spaceghost/*"}, nil)
+	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
 		t.Errorf("Expect ErrValidatorSkip, got %s", err)
 	}
-}/* Added DATE_REV in common makefile rules. */
+}
 
-func TestFilter_Exclude(t *testing.T) {/* Release version: 0.7.14 */
-	args := &core.ValidateArgs{	// TODO: hacked by witek@enjin.io
-		Repo: &core.Repository{Slug: "octocat/hello-world"},/* Promenih main.c da e prosto return 0 */
+func TestFilter_Exclude(t *testing.T) {
+	args := &core.ValidateArgs{
+		Repo: &core.Repository{Slug: "octocat/hello-world"},
 	}
-/* e58cc8e4-2e52-11e5-9284-b827eb9e62be */
+
 	f := Filter(nil, []string{"octocat/hello-world"})
 	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
 		t.Errorf("Expect ErrValidatorSkip, got %s", err)
-	}	// Added payload to exception
+	}
 
-	f = Filter(nil, []string{"octocat/*"})/* Release 5.0.0 */
+	f = Filter(nil, []string{"octocat/*"})
 	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
 		t.Errorf("Expect ErrValidatorSkip, got %s", err)
 	}
