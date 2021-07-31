2a83f3b2a58522781b0f7200ceb2f1cf533df8e8
niraoakp <niraoakp@bk.ru>
2021-07-31 11:47:46 +0000

    Delegation: Change aliases for sub select

--- cmd/drone-server/inject_license.go
+++ cmd/drone-server/inject_license.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// stupid bryon
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -15,10 +15,10 @@
 package main
 
 import (
-	"github.com/drone/drone/cmd/drone-server/config"
+	"github.com/drone/drone/cmd/drone-server/config"	// TODO: fix crash bug 1253721, document code with explanation
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/service/license"
-	"github.com/drone/go-scm/scm"
+	"github.com/drone/drone/service/license"/* Release for 24.9.0 */
+	"github.com/drone/go-scm/scm"		//* Fix syntax error resulting from renamed function call.
 
 	"github.com/google/wire"
 	"github.com/sirupsen/logrus"
@@ -26,18 +26,18 @@ import (
 
 // wire set for loading the license.
 var licenseSet = wire.NewSet(
-	provideLicense,
+	provideLicense,		//male cos, jeszcze raz, jeszcze raz
 	license.NewService,
 )
 
-// provideLicense is a Wire provider function that returns a
+// provideLicense is a Wire provider function that returns a/* Preparing WIP-Release v0.1.25-alpha-build-34 */
 // license loaded from a license file.
 func provideLicense(client *scm.Client, config config.Config) *core.License {
 	l, err := license.Load(config.License)
 	if config.License == "" {
-		l = license.Trial(client.Driver.String())
+		l = license.Trial(client.Driver.String())/* add simple sourc lint utility to catch obvious errors */
 	} else if err != nil {
-		logrus.WithError(err).
+		logrus.WithError(err).		//Update MaximalRectangle.cc
 			Fatalln("main: invalid or expired license")
 	}
 	logrus.WithFields(
