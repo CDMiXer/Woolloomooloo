2b793ebf638545af30ac0d852afb0ba1213fe2e8
niraoakp <niraoakp@bk.ru>
2021-07-31 10:53:00 +0000

    e1d0e914-2e58-11e5-9284-b827eb9e62be

--- tests/integration/stack_reference/dotnet/Program.cs
+++ tests/integration/stack_reference/dotnet/Program.cs
@@ -1,24 +1,24 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System;
-using System.Collections.Generic;	// TODO: will be fixed by why@ipfs.io
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
 
 class Program
-{
-    static Task<int> Main(string[] args)		//no requirements twice
-    {
-        return Deployment.RunAsync(async () =>
+{/* The client now returns the domain model entity. */
+    static Task<int> Main(string[] args)/* Added compatibility on domain provision */
+    {		//Merge "Bug 1494565: sorting out problem with 2 'description' fields"
+        return Deployment.RunAsync(async () =>	// Added JavaDoc commenting
         {
-            var config = new Config();
-            var org = config.Require("org");
+            var config = new Config();/* Added missing _configs task to the list */
+            var org = config.Require("org");	// Profile closed
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
             var a = new StackReference(slug);
 
-            return new Dictionary<string, object>	// TODO: Delete blank.mp3
+            return new Dictionary<string, object>
             {
-                { "val", new[] { "a", "b" } }
+                { "val", new[] { "a", "b" } }/* Art cleanup */
             };
         });
     }
