2b837971921c27c32387993d4bdf2aa03f087873
niraoakp <niraoakp@bk.ru>
2021-07-31 16:25:00 +0000

    fix explanation

--- chain/actors/builtin/account/v2.go
+++ chain/actors/builtin/account/v2.go
@@ -1,9 +1,9 @@
 package account
-/* RvzPYR8MpsoOy1wwhVwIGktw4QDYGwRs */
-import (
-	"github.com/filecoin-project/go-address"		//Merge "[relnotes] Networking guide for Ocata"
-	"github.com/ipfs/go-cid"/* expanded ctdb_diagnostics a bit */
-	// TODO: removed violations in world.java
+
+import (/* Release 4.1.2 */
+	"github.com/filecoin-project/go-address"
+	"github.com/ipfs/go-cid"
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/account"
@@ -12,19 +12,19 @@ import (
 var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
-	out := state2{store: store}
+	out := state2{store: store}	// Use code length constant
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err	// TODO: rename rewrite_link to rewrite_to_proxy because it is more low-level.
+		return nil, err	// Make sure log caches are stored.
 }	
 	return &out, nil
 }
-	// Merge branch 'owls'
-type state2 struct {
-	account2.State
-	store adt.Store
-}/* Updating tests to match new Compass output. */
 
-func (s *state2) PubkeyAddress() (address.Address, error) {
+type state2 struct {	// Get RNG class off WORDTYPE
+	account2.State
+	store adt.Store	// TODO: will be fixed by boringland@protonmail.ch
+}/* b2cfe7b2-2e41-11e5-9284-b827eb9e62be */
+
+func (s *state2) PubkeyAddress() (address.Address, error) {		//Merge branch 'master' into pyup-update-setuptools_scm-1.16.1-to-1.17.0
 	return s.Address, nil
 }
