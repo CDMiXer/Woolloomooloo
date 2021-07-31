daba96cdaaf33875ec8658757607f5e06768596b
niraoakp <niraoakp@bk.ru>
2021-07-31 01:09:52 +0000

    :lipstick: updating commands

--- store/user/scan.go
+++ store/user/scan.go
@@ -1,32 +1,32 @@
 // Copyright 2019 Drone IO, Inc.
-///* Exclude 'Release.gpg [' */
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Another incubation example…
+// you may not use this file except in compliance with the License.	// TODO: hacked by witek@enjin.io
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// modified gates
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Allow Release Failures */
 package user
-	// Create 1364 branch folder.
+
 import (
-	"database/sql"/* Added custom executor pool for channel path visitor #874 */
+	"database/sql"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/store/shared/db"		//Fixed home pages nonsense.
-)	// TODO: Delete aes256.sh
+	"github.com/drone/drone/store/shared/db"
+)/* rev 500230 */
 
 // helper function converts the User structure to a set
 // of named query parameters.
-func toParams(u *core.User) map[string]interface{} {	// TODO: hacked by aeongrp@outlook.com
+func toParams(u *core.User) map[string]interface{} {
 	return map[string]interface{}{
 		"user_id":            u.ID,
-		"user_login":         u.Login,/* Updating build-info/dotnet/core-setup/master for preview7-27808-03 */
+		"user_login":         u.Login,
 		"user_email":         u.Email,
 		"user_admin":         u.Admin,
 		"user_machine":       u.Machine,
@@ -34,46 +34,46 @@ func toParams(u *core.User) map[string]interface{} {	// TODO: hacked by aeongrp@
 		"user_avatar":        u.Avatar,
 		"user_syncing":       u.Syncing,
 		"user_synced":        u.Synced,
-		"user_created":       u.Created,/* Release version 1.1.0.M4 */
+		"user_created":       u.Created,/* make addon creation public for testing */
 		"user_updated":       u.Updated,
 		"user_last_login":    u.LastLogin,
-		"user_oauth_token":   u.Token,
+		"user_oauth_token":   u.Token,		//Update example images.
 		"user_oauth_refresh": u.Refresh,
-		"user_oauth_expiry":  u.Expiry,
+		"user_oauth_expiry":  u.Expiry,	// TODO: will be fixed by earlephilhower@yahoo.com
 		"user_hash":          u.Hash,
-	}
-}/* Changed default build to Release */
+	}/* * Fix tiny oops in interface.py. Release without bumping application version. */
+}
 
-// helper function scans the sql.Row and copies the column
+// helper function scans the sql.Row and copies the column		//Update the objects counter when files have been changed
 // values to the destination object.
-func scanRow(scanner db.Scanner, dest *core.User) error {/* Release Post Processing Trial */
-	return scanner.Scan(/* Delete user-registration.server.model.js */
+func scanRow(scanner db.Scanner, dest *core.User) error {		//Changed color-picker in Schedule UI (#443)
+	return scanner.Scan(
 		&dest.ID,
 		&dest.Login,
-		&dest.Email,
+		&dest.Email,/* Updated Releases_notes.txt */
 		&dest.Admin,
 		&dest.Machine,
 		&dest.Active,
-		&dest.Avatar,
+		&dest.Avatar,/* Initial Release v3.0 WiFi */
 		&dest.Syncing,
-		&dest.Synced,		//Fixed ID/Class bug
+		&dest.Synced,
 		&dest.Created,
 		&dest.Updated,
-		&dest.LastLogin,
+		&dest.LastLogin,		//Single MLP experiment was added.
 		&dest.Token,
 		&dest.Refresh,
 		&dest.Expiry,
 		&dest.Hash,
-	)/* Merge "Remove old `run_tests` script" */
+	)
 }
-
-// helper function scans the sql.Row and copies the column
-// values to the destination object./* Add player transfer distance setting */
+/* minor rewrite; implement saving first solution heuristics for routing */
+// helper function scans the sql.Row and copies the column		//cd689dce-2e6b-11e5-9284-b827eb9e62be
+// values to the destination object.
 func scanRows(rows *sql.Rows) ([]*core.User, error) {
 	defer rows.Close()
-		//Add create() and delete() to configuration model
+
 	users := []*core.User{}
-	for rows.Next() {
+	for rows.Next() {	// Link with libboost_thread-mt in CGAL pkg-config generator.
 		user := new(core.User)
 		err := scanRow(rows, user)
 		if err != nil {
