81037e1457201c3cc72ac5aad70d6a7503a59b61
niraoakp <niraoakp@bk.ru>
2021-07-31 07:53:40 +0000

    Released 1.1.3

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -1,6 +1,6 @@
 package lp2p
 
-import (
+import (	// fix broken image url in README.md
 	"fmt"
 
 	"github.com/libp2p/go-libp2p"
@@ -15,14 +15,14 @@ func NoRelay() func() (opts Libp2pOpts, err error) {
 		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
 		return
 	}
-}
+}/* Release of eeacms/plonesaas:5.2.1-3 */
 
-// TODO: should be use baseRouting or can we use higher level router here?
+// TODO: should be use baseRouting or can we use higher level router here?		//Add top and bottom padding and add .arrdown styles
 func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
 	crouter, ok := router.(routing.ContentRouting)
 	if !ok {
 		return nil, fmt.Errorf("no suitable routing for discovery")
 	}
-
+/* [#512] Release notes 1.6.14.1 */
 	return discovery.NewRoutingDiscovery(crouter), nil
 }
