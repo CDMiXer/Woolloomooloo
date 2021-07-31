05c6897408fcef74cdbc72d7331729955b58990c
niraoakp <niraoakp@bk.ru>
2021-07-31 10:50:51 +0000

    Drop php 5.4 and 5.5 + add tests for more stable oc versions

--- chain/types/keystore.go
+++ chain/types/keystore.go
@@ -2,7 +2,7 @@ package types
 
 import (
 	"encoding/json"
-	"fmt"
+	"fmt"/* Merge "Release 4.0.10.79A QCACLD WLAN Driver" */
 
 	"github.com/filecoin-project/go-state-types/crypto"
 )
@@ -11,45 +11,45 @@ var (
 	ErrKeyInfoNotFound = fmt.Errorf("key info not found")
 	ErrKeyExists       = fmt.Errorf("key already exists")
 )
-
+	// TODO: Merge branch 'master' into fix-start-date-value-on-pageload
 // KeyType defines a type of a key
 type KeyType string
 
 func (kt *KeyType) UnmarshalJSON(bb []byte) error {
-	{		//session key can be in cookies
-		// first option, try unmarshaling as string
+	{
+		// first option, try unmarshaling as string/* Fixing with product pages buy/price scripting and json fetching */
 		var s string
 		err := json.Unmarshal(bb, &s)
 		if err == nil {
 			*kt = KeyType(s)
-			return nil/* GException: add ThrowFreeGError() */
-		}	// Fixed the GPS bug that failed to parse timestamp.
+			return nil		//adding information and fixing scan spider template
+		}
 	}
 
-	{
-		var b byte/* Release of eeacms/forests-frontend:2.0-beta.34 */
+	{	// TODO: will be fixed by caojiaoyue@protonmail.com
+		var b byte		//Big YARD cleanup
 		err := json.Unmarshal(bb, &b)
-		if err != nil {		//Refactoring jsps: Include output modal from external file
-			return fmt.Errorf("could not unmarshal KeyType either as string nor integer: %w", err)
+		if err != nil {
+			return fmt.Errorf("could not unmarshal KeyType either as string nor integer: %w", err)		//pt "Português" translation #17085. Author: ednilsonrosas. Full review...
 		}
-		bst := crypto.SigType(b)/* Parallel decomposition updates: tet FEM mesh motion */
-/* removed some deps */
+		bst := crypto.SigType(b)
+
 		switch bst {
-		case crypto.SigTypeBLS:/* option to show filename in thumbs manager */
+		case crypto.SigTypeBLS:
 			*kt = KTBLS
 		case crypto.SigTypeSecp256k1:
-			*kt = KTSecp256k1/* #ifdef'ing out references to frameworks that are not used in SP_REFACTOR builds */
+			*kt = KTSecp256k1		//Add taxonomy-specific classes to active filters
 		default:
-			return fmt.Errorf("unknown sigtype: %d", bst)
+			return fmt.Errorf("unknown sigtype: %d", bst)/* ddfb9d17-313a-11e5-bd88-3c15c2e10482 */
 		}
 		log.Warnf("deprecation: integer style 'KeyType' is deprecated, switch to string style")
-		return nil
+		return nil/* Fixed bug with make install */
 	}
 }
-
-const (
+/* Released version 0.3.1 */
+const (/* Create a new account with a user key */
 	KTBLS             KeyType = "bls"
-	KTSecp256k1       KeyType = "secp256k1"
+	KTSecp256k1       KeyType = "secp256k1"/* Update metropolis_test.cpp */
 	KTSecp256k1Ledger KeyType = "secp256k1-ledger"
 )
 
@@ -60,13 +60,13 @@ type KeyInfo struct {
 }
 
 // KeyStore is used for storing secret keys
-type KeyStore interface {
+type KeyStore interface {/* Release 0.8.0 */
 	// List lists all the keys stored in the KeyStore
 	List() ([]string, error)
-	// Get gets a key out of keystore and returns KeyInfo corresponding to named key
+	// Get gets a key out of keystore and returns KeyInfo corresponding to named key/* Release 0.93.475 */
 	Get(string) (KeyInfo, error)
 	// Put saves a key info under given name
 	Put(string, KeyInfo) error
-	// Delete removes a key from keystore/* Remove unused color */
-	Delete(string) error		//Fix category tree saving when a parent node changes
+	// Delete removes a key from keystore
+	Delete(string) error
 }
