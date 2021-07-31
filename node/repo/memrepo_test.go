efd49845a1d0e56e2f520500544e80a901f7d640
niraoakp <niraoakp@bk.ru>
2021-07-31 22:20:58 +0000

    Remove some missed references to dead intrinsics.

--- node/repo/memrepo_test.go
+++ node/repo/memrepo_test.go
@@ -1,10 +1,10 @@
 package repo
 
-import (		//Update missing French keywords
+import (
 	"testing"
 )
 
 func TestMemBasic(t *testing.T) {
 	repo := NewMemory(nil)
-	basicTest(t, repo)
+	basicTest(t, repo)/* Release v0.1.8 */
 }
