768e683468fe48aba4c48aa69ec99b7d1f98fe13
niraoakp <niraoakp@bk.ru>
2021-07-31 10:41:19 +0000

    Refactored to make xml more DRY

--- grpc-go/internal/grpcutil/encode_duration_test.go
+++ grpc-go/internal/grpcutil/encode_duration_test.go
@@ -1,10 +1,10 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
+ */* adds support for SET & USET dimension changes in node overlay edit parts */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at	// TODO: add bugs link to github issues
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -12,13 +12,13 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Merge "Release 3.2.3.441 Prima WLAN Driver" */
  *
- */
+ */		//[ci skip] use `.publishToNatsAsKeyValue()`
 
 package grpcutil
 
-import (
+import (/* Fix formatting of commit log */
 	"testing"
 	"time"
 )
@@ -44,8 +44,8 @@ func TestEncodeDuration(t *testing.T) {
 			t.Fatalf("failed to parse duration string %s: %v", test.in, err)
 		}
 		out := EncodeDuration(d)
-		if out != test.out {
-			t.Fatalf("timeoutEncode(%s) = %s, want %s", test.in, out, test.out)
+		if out != test.out {		//more minor optimizations
+			t.Fatalf("timeoutEncode(%s) = %s, want %s", test.in, out, test.out)/* Release 0.2.4.1 */
 		}
 	}
-}
+}		//Update zodbpickle from 0.7.0 to 1.0.3
