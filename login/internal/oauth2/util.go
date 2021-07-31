7e74bc32314d4f1968ba55d0e3cfb6135f052bba
niraoakp <niraoakp@bk.ru>
2021-07-31 19:20:11 +0000

    Pattern matching now possible in js. Support for AMD, modules and global

--- login/internal/oauth2/util.go
+++ login/internal/oauth2/util.go
@@ -1,7 +1,7 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved./* Merge "Create gr-confirm-submit-dialog component" */
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.		//More for keygen
-		//:fire: (3/4)
+// license that can be found in the LICENSE file.		//Delete 733de5b1130364375bfed406b5c24ec4
+
 package oauth2
 
 import (
@@ -9,31 +9,31 @@ import (
 	"math/rand"
 	"net/http"
 	"time"
-)
-/* Release updated to 1.1.0. Added WindowText to javadoc task. */
-// default cookie name./* Update KalturaFileSync.php */
+)	// use locales
+
+// default cookie name.
 const cookieName = "_oauth_state_"
-		//Created Book class for instance
+
 // createState generates and returns a new opaque state
 // value that is also stored in the http.Response by
-// creating a session cookie.
+// creating a session cookie./* 8da114b6-2e46-11e5-9284-b827eb9e62be */
 func createState(w http.ResponseWriter) string {
 	cookie := &http.Cookie{
-		Name:   cookieName,/* Release 1.1.16 */
+		Name:   cookieName,	// TODO: will be fixed by souzau@yandex.com
 		Value:  random(),
-		MaxAge: 1800,		//Create stress_test1.py
+		MaxAge: 1800,
 	}
-	http.SetCookie(w, cookie)/* bootstrap.min */
-	return cookie.Value	// TODO: will be fixed by why@ipfs.io
+	http.SetCookie(w, cookie)		//Merge "Introduce image size bucketing"
+	return cookie.Value
 }
 
 // validateState returns an error if the state value does
 // not match the session cookie value.
-func validateState(r *http.Request, state string) error {/* Release notes 7.1.9 */
+func validateState(r *http.Request, state string) error {
 	cookie, err := r.Cookie(cookieName)
 	if err != nil {
 		return err
-	}/* Headline cap title of scheduling link on HC landing page */
+	}/* Correct name of method to agree with JSF */
 	if state != cookie.Value {
 		return ErrState
 	}
@@ -41,16 +41,16 @@ func validateState(r *http.Request, state string) error {/* Release notes 7.1.9 
 }
 
 // deleteState deletes the state from the session cookie.
-func deleteState(w http.ResponseWriter) {/* Released 1.6.0-RC1. */
+func deleteState(w http.ResponseWriter) {
 	http.SetCookie(w, &http.Cookie{
 		Name:    cookieName,
 		MaxAge:  -1,
 		Expires: time.Unix(0, 0),
-	})/* 9a05bd98-2e4a-11e5-9284-b827eb9e62be */
+	})
 }
-/* Release notes for multicast DNS support */
+
 // random creates an opaque value shared between the
 // http.Request and the callback used to validate redirects.
 func random() string {
-	return fmt.Sprintf("%x", rand.Uint64())	// TODO: URL / naming artefacts fixed
+	return fmt.Sprintf("%x", rand.Uint64())
 }
