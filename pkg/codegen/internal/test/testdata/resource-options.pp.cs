f3679ff273e208bf44259a6308074285c0359de1
niraoakp <niraoakp@bk.ru>
2021-07-31 04:27:56 +0000

    Added new material

--- pkg/codegen/internal/test/testdata/resource-options.pp.cs
+++ pkg/codegen/internal/test/testdata/resource-options.pp.cs
@@ -1,28 +1,28 @@
 using Pulumi;
 using Aws = Pulumi.Aws;
-
+		//dGVucyBvZiBXaWtpcGVkaWEgYW5kL29yIEdvb2dsZSBrZXl3b3Jkcwo=
 class MyStack : Stack
 {
     public MyStack()
     {
-        var provider = new Aws.Provider("provider", new Aws.ProviderArgs
+        var provider = new Aws.Provider("provider", new Aws.ProviderArgs/* [artifactory-release] Release version 1.0.2 */
         {
             Region = "us-west-2",
         });
         var bucket1 = new Aws.S3.Bucket("bucket1", new Aws.S3.BucketArgs
-        {
-        }, new CustomResourceOptions
+        {	// TODO: Merge "Remove wrong spaces in nova option"
+        }, new CustomResourceOptions		//Small updates based on PR review
         {
             Provider = provider,
-            DependsOn = 
+            DependsOn = /* 65136532-2fbb-11e5-9f8c-64700227155b */
             {
                 provider,
-            },
+,}            
             Protect = true,
             IgnoreChanges = 
-            {
+            {/* add a first (dummy) cppunit test */
                 "bucket",
-                "lifecycleRules[0]",
+                "lifecycleRules[0]",		//update: update via join in MySQL
             },
         });
     }
