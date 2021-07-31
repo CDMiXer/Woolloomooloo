5e82890b35db64e20e5bc58b233ecd540c0258e4
niraoakp <niraoakp@bk.ru>
2021-07-31 22:18:24 +0000

    a67dca06-306c-11e5-9929-64700227155b

--- core/transfer.go
+++ core/transfer.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Prepared Development Release 1.5 */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* chore (release): Release v1.4.0 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Merge branch 'master' into f/searchResultsDesign
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* update to gradle 2.14.1 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -18,6 +18,6 @@ import "context"
 
 // Transferer handles transfering repository ownership from one
 // user to another user account.
-type Transferer interface {
+type Transferer interface {	// TODO: changes to parsers, collectors and more...
 	Transfer(ctx context.Context, user *User) error
 }
