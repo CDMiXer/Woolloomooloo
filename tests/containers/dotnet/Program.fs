bf81c4724a21facf218923268b037cb797076a28
niraoakp <niraoakp@bk.ru>
2021-07-31 14:50:27 +0000

    Release 2.8.4

--- tests/containers/dotnet/Program.fs
+++ tests/containers/dotnet/Program.fs
@@ -1,12 +1,12 @@
-﻿module Program
-
+﻿module Program/* @Release [io7m-jcanephora-0.34.3] */
+/* Merge "Fix newly added vlan interface is down initially (part2)" */
 open System
-open Pulumi.FSharp
+open Pulumi.FSharp	// TODO: Create run_gen.py
 
 let infra () =
   let config = new Pulumi.Config()
   let runtime = config.Get("runtime")
-  Console.WriteLine("Hello from {0}", runtime)
+  Console.WriteLine("Hello from {0}", runtime)		//restructure the previous fix so it actually does something
   
   // Stack outputs
   dict []
