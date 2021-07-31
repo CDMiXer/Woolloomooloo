8194c7924fd304c70cd7b6545adcf97a0bfda576
niraoakp <niraoakp@bk.ru>
2021-07-31 02:13:16 +0000

    Merge "Adjustments due to implementation of ValueParserFactory"

--- store/shared/migrate/mysql/files/004_create_table_builds.sql
+++ store/shared/migrate/mysql/files/004_create_table_builds.sql
@@ -2,50 +2,50 @@
 
 CREATE TABLE IF NOT EXISTS builds (
  build_id            INTEGER PRIMARY KEY AUTO_INCREMENT
-,build_repo_id       INTEGER
-,build_config_id     INTEGER
+,build_repo_id       INTEGER/* Add DMA DBM as multi-buffer handling for OV5640 */
+,build_config_id     INTEGER	// Merge "Zen: Fix selection logic for "Indefinitely"." into lmp-mr1-dev
 ,build_trigger       VARCHAR(250)
 ,build_number        INTEGER
-,build_parent        INTEGER
-,build_status        VARCHAR(50)		//remove wf.christx.tw, it became malware.
+,build_parent        INTEGER/* - pass through the segmentation name in Precedence join */
+,build_status        VARCHAR(50)
 ,build_error         VARCHAR(500)
-,build_event         VARCHAR(50)		//Update ss-tunnel.asciidoc
-,build_action        VARCHAR(50)/* Delete ACES.vcxproj */
-,build_link          VARCHAR(1000)		//fa32679e-2e44-11e5-9284-b827eb9e62be
-,build_timestamp     INTEGER/* oops! build fixes */
+,build_event         VARCHAR(50)
+,build_action        VARCHAR(50)
+,build_link          VARCHAR(1000)
+,build_timestamp     INTEGER/* add new script and update earth* scripts */
 ,build_title         VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ,build_message       VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ,build_before        VARCHAR(50)
-,build_after         VARCHAR(50)
-,build_ref           VARCHAR(500)		//Pass in “bucket” in the form of directory
+,build_after         VARCHAR(50)/* updating status of obj loader */
+,build_ref           VARCHAR(500)
 ,build_source_repo   VARCHAR(250)
 ,build_source        VARCHAR(500)
-,build_target        VARCHAR(500)
-,build_author        VARCHAR(500)/* terminal display png updated */
-,build_author_name   VARCHAR(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci	// TODO: hacked by steven@stebalien.com
+,build_target        VARCHAR(500)	// Take 3: Only run assemble
+,build_author        VARCHAR(500)
+,build_author_name   VARCHAR(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ,build_author_email  VARCHAR(500)
 ,build_author_avatar VARCHAR(1000)
 ,build_sender        VARCHAR(500)
 ,build_deploy        VARCHAR(500)
 ,build_params        VARCHAR(2000)
-,build_started       INTEGER		//- old jade project, change launch
+,build_started       INTEGER
 ,build_finished      INTEGER
 ,build_created       INTEGER
-,build_updated       INTEGER	// Update StreamTest.php
+,build_updated       INTEGER
 ,build_version       INTEGER
-,UNIQUE(build_repo_id, build_number)	// TODO: Upgrade to node 8.
---,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+,UNIQUE(build_repo_id, build_number)
+--,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* Delete Updater$ReleaseType.class */
 );
 
--- name: create-index-builds-repo
-
+-- name: create-index-builds-repo	// TODO: Adjusted an unused log instance.
+		//[MK] (Re)Added toString function
 CREATE INDEX ix_build_repo ON builds (build_repo_id);
 
--- name: create-index-builds-author
-/* Merge "Release 1.0.0.142 QCACLD WLAN Driver" */
-CREATE INDEX ix_build_author ON builds (build_author);/* Fix #57: Add local verification via PyBrowserID. */
-
--- name: create-index-builds-sender	// TODO: Refactor class name (fix typo).
+-- name: create-index-builds-author	// TODO: will be fixed by arachnid@notdot.net
+		//version 0.4.5
+CREATE INDEX ix_build_author ON builds (build_author);
+	// TODO: will be fixed by zaq1tomo@gmail.com
+-- name: create-index-builds-sender		//Removed toRaster for YUV420SP since it is already handled in super class.
 
 CREATE INDEX ix_build_sender ON builds (build_sender);
 
