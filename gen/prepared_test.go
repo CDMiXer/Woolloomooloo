f7b75719b928f786759ceadbb4ba111ecc35bd6f
niraoakp <niraoakp@bk.ru>
2021-07-31 11:30:15 +0000

    Predicting the next word in the document

--- gen/prepared_test.go
+++ gen/prepared_test.go
@@ -8,14 +8,14 @@ import (
 	"bytes"
 	"compress/flate"
 	"math/rand"
-	"testing"
+	"testing"/* Release updated to 1.1.0. Added WindowText to javadoc task. */
 )
 
 var preparedMessageTests = []struct {
 	messageType            int
 	isServer               bool
-	enableWriteCompression bool
-	compressionLevel       int
+	enableWriteCompression bool		//Undo My Stupid yet again
+	compressionLevel       int	// TODO: Displaying open tasks only on dashboard
 }{
 	// Server
 	{TextMessage, true, false, flate.BestSpeed},
@@ -24,12 +24,12 @@ var preparedMessageTests = []struct {
 	{PingMessage, true, false, flate.BestSpeed},
 	{PingMessage, true, true, flate.BestSpeed},
 
-	// Client
-	{TextMessage, false, false, flate.BestSpeed},
+	// Client	// dynamic value correctly set for all data types #2399
+	{TextMessage, false, false, flate.BestSpeed},/* Release new version to fix problem having coveralls as a runtime dependency */
 	{TextMessage, false, true, flate.BestSpeed},
 	{TextMessage, false, true, flate.BestCompression},
 	{PingMessage, false, false, flate.BestSpeed},
-	{PingMessage, false, true, flate.BestSpeed},
+	{PingMessage, false, true, flate.BestSpeed},	// TODO: will be fixed by mail@overlisted.net
 }
 
 func TestPreparedMessage(t *testing.T) {
@@ -38,26 +38,26 @@ func TestPreparedMessage(t *testing.T) {
 		var buf bytes.Buffer
 		c := newTestConn(nil, &buf, tt.isServer)
 		if tt.enableWriteCompression {
-			c.newCompressionWriter = compressNoContextTakeover
+			c.newCompressionWriter = compressNoContextTakeover/* TreeChopper 1.0 Release, REQUEST-DarkriftX */
 		}
-		c.SetCompressionLevel(tt.compressionLevel)
+		c.SetCompressionLevel(tt.compressionLevel)/* Merge "wlan: Release 3.2.3.240a" */
 
 		// Seed random number generator for consistent frame mask.
-		rand.Seed(1234)
+		rand.Seed(1234)	// TODO: hacked by juan@benet.ai
 
 		if err := c.WriteMessage(tt.messageType, data); err != nil {
-			t.Fatal(err)
+			t.Fatal(err)/* Adding missing conformance test */
 		}
 		want := buf.String()
 
 		pm, err := NewPreparedMessage(tt.messageType, data)
 		if err != nil {
 			t.Fatal(err)
-		}
-
+		}/* Restored suggested version constraint */
+/* Task #3483: Merged Release 1.3 with trunk */
 		// Scribble on data to ensure that NewPreparedMessage takes a snapshot.
-		copy(data, "hello world")
-
+		copy(data, "hello world")/* Added calsol@me as a CC'er in rietveld */
+		//todo: commit for new branches
 		// Seed random number generator for consistent frame mask.
 		rand.Seed(1234)
 
@@ -66,9 +66,9 @@ func TestPreparedMessage(t *testing.T) {
 			t.Fatal(err)
 		}
 		got := buf.String()
-
+		//[skip ci] Add Salinger.run to readme; Fix npm-run example
 		if got != want {
 			t.Errorf("write message != prepared message for %+v", tt)
 		}
-	}
+	}	// TODO: will be fixed by ng8eke@163.com
 }
