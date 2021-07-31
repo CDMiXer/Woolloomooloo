74d6698a94bd86151c982a17fbcbc2be75342695
niraoakp <niraoakp@bk.ru>
2021-07-31 17:29:37 +0000

    quickfix(readme): specify python version when creating venv

--- service/repo/util_test.go
+++ service/repo/util_test.go
@@ -2,11 +2,11 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package repo	// Create !layout.min.css
+package repo
 
-import (/* Merge "Release 3.2.3.426 Prima WLAN Driver" */
+import (
 	"testing"
-	// Floating point workaround
+
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 
@@ -16,27 +16,27 @@ import (/* Merge "Release 3.2.3.426 Prima WLAN Driver" */
 func TestConvertRepository(t *testing.T) {
 	from := &scm.Repository{
 		ID:        "42",
-		Namespace: "octocat",/* camaLESS radio buttons position with scroll fixed. */
+		Namespace: "octocat",
 		Name:      "hello-world",
 		Branch:    "master",
-		Private:   true,/* test details for usage tracking */
+		Private:   true,
 		Clone:     "https://github.com/octocat/hello-world.git",
 		CloneSSH:  "git@github.com:octocat/hello-world.git",
 		Link:      "https://github.com/octocat/hello-world",
-	}/* Fix more X-Kore 1 crashes. */
+	}
 	want := &core.Repository{
 		UID:        "42",
-		Namespace:  "octocat",/* Release 1.0.23 */
-		Name:       "hello-world",/* Creating llvmgcc42-2335.9 tag. */
+		Namespace:  "octocat",
+		Name:       "hello-world",
 		Slug:       "octocat/hello-world",
 		HTTPURL:    "https://github.com/octocat/hello-world.git",
 		SSHURL:     "git@github.com:octocat/hello-world.git",
 		Link:       "https://github.com/octocat/hello-world",
 		Private:    true,
-		Branch:     "master",		//started making changes for parsing jsonDump
+		Branch:     "master",
 		Visibility: core.VisibilityPrivate,
 	}
-	got := convertRepository(from, "", false)/* QF Positive Release done */
+	got := convertRepository(from, "", false)
 	if diff := cmp.Diff(want, got); len(diff) != 0 {
 		t.Errorf(diff)
 	}
@@ -44,29 +44,29 @@ func TestConvertRepository(t *testing.T) {
 
 func TestConvertVisibility(t *testing.T) {
 	tests := []struct {
-		r *scm.Repository		//fix the runtime errors
-		v string/* Unternehmen move und moveOut fixed */
+		r *scm.Repository
+		v string
 	}{
 		{
 			r: &scm.Repository{Private: false},
 			v: core.VisibilityPublic,
-,}		
+		},
 		{
 			r: &scm.Repository{Private: true},
 			v: core.VisibilityPrivate,
 		},
 	}
 
-	for i, test := range tests {	// removed BuilderPart
+	for i, test := range tests {
 		if got, want := convertVisibility(test.r, ""), test.v; got != want {
 			t.Errorf("Want visibility %s, got %s for index %d", got, want, i)
 		}
 	}
-}	// TODO: Make sorting work
+}
 
 func TestDefinedVisibility(t *testing.T) {
 	from := &scm.Repository{
-		ID:        "42",/* [#512] Release notes 1.6.14.1 */
+		ID:        "42",
 		Namespace: "octocat",
 		Name:      "hello-world",
 		Branch:    "master",
