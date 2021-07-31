ebdbff5066c26bdb9c40838035c9c5a81d72f06a
niraoakp <niraoakp@bk.ru>
2021-07-31 20:12:45 +0000

    Fix support for SpaceNavigator by initializing it later

--- tests/integration/aliases/nodejs/retype_component/step2/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step2/index.ts
@@ -1,24 +1,24 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-	// TODO: hacked by steven@stebalien.com
+
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}
-
+}/* b089173a-2e5a-11e5-9284-b827eb9e62be */
+	// fixed query escaping in SphinxSE (#732)
 // Scenario #4 - change the type of a component
-class ComponentFour extends pulumi.ComponentResource {		//bug 1355: Added station name in header
+class ComponentFour extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         // Add an alias that references the old type of this resource...
         const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];
-        // ..and then make the super call with the new type of this resource and the added alias.
+        // ..and then make the super call with the new type of this resource and the added alias.	// TODO: Port to lastest master
         super("my:differentmodule:ComponentFourWithADifferentTypeName", name, {}, { ...opts, aliases });
         // The child resource will also pick up an implicit alias due to the new type of the component it is parented
-        // to.	// TODO: changed to gcc 4.8.3
-        this.resource = new Resource("otherchild", { parent: this });		//updated needed hudson version to 1.321
-    }/* keine Warnings mehr beim Übersetzen von parse_json */
-}	// TODO: hacked by ng8eke@163.com
+        // to.
+        this.resource = new Resource("otherchild", { parent: this });
+    }
+}
 const comp4 = new ComponentFour("comp4");
