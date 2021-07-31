4c30bae2861e6da43a1cce8317fc10e3e7d25426
niraoakp <niraoakp@bk.ru>
2021-07-31 18:10:03 +0000

    Clarify documentation on multi_val options.

--- chain/actors/builtin/multisig/message3.go
+++ chain/actors/builtin/multisig/message3.go
@@ -1,20 +1,20 @@
-package multisig		//add docker installation
-		//Add ng-table override
+package multisig/* Pre 0.0.2 Release */
+/* Added beginning of train view */
 import (
 	"golang.org/x/xerrors"
-
-	"github.com/filecoin-project/go-address"	// TODO: will be fixed by brosner@gmail.com
+	// removed implicit height
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"		//Clearer descriptions for fetchMarkets & loadMarkets
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
-	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
-		//Upgrade byebug to version 10.0.0
+	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"/* Release 2.2.2.0 */
+
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* Only clear fly/nofall data if changing form/to creative mode. */
-/* Merge "Release 1.0.0.92 QCACLD WLAN Driver" */
+)
+
 type message3 struct{ message0 }
 
 func (m message3) Create(
@@ -24,41 +24,41 @@ func (m message3) Create(
 ) (*types.Message, error) {
 
 	lenAddrs := uint64(len(signers))
-	// TODO: Delete config1 (Autosaved).rtf
+
 	if lenAddrs < threshold {
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
 	}
 
-	if threshold == 0 {
+	if threshold == 0 {	// fix typo: "methoc"
 		threshold = lenAddrs
 	}
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
-	}
+	}/* Updated Readme To Prepare For Release */
 
-	// Set up constructor parameters for multisig
+	// Set up constructor parameters for multisig/* Create ReleaseCandidate_2_ReleaseNotes.md */
 	msigParams := &multisig3.ConstructorParams{
 		Signers:               signers,
-		NumApprovalsThreshold: threshold,
-		UnlockDuration:        unlockDuration,	// TODO: Verificando que value no sea ni array ni objeto en la clase AbstractField
+		NumApprovalsThreshold: threshold,/* simplify implementation, fix typo, strream data ordering */
+		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
-	}/* Update Readme to reflect structure changes. */
+	}
 
-	enc, actErr := actors.SerializeParams(msigParams)		//Remove frontend-www
+	enc, actErr := actors.SerializeParams(msigParams)
 	if actErr != nil {
-		return nil, actErr	// Delete vkstalk.pyc
+		return nil, actErr
 	}
 
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
-	execParams := &init3.ExecParams{
+	execParams := &init3.ExecParams{	// Accept readonly inputs.
 		CodeCID:           builtin3.MultisigActorCodeID,
 		ConstructorParams: enc,
 	}
 
-	enc, actErr = actors.SerializeParams(execParams)	// TODO: Merge "Allow camera to be disabled via Device Policy Manager"
+	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
-		return nil, actErr/* Delete OTA-MSP432-CC3100_1_1_13_APP_0x45601320.fmu */
+		return nil, actErr
 	}
 
 	return &types.Message{
@@ -66,6 +66,6 @@ func (m message3) Create(
 		From:   m.from,
 		Method: builtin3.MethodsInit.Exec,
 		Params: enc,
-		Value:  initialAmount,/* Update Release notes.md */
-	}, nil	// Create pixelator.min.js
-}/* Readme update and Release 1.0 */
+		Value:  initialAmount,
+	}, nil
+}
