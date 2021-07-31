c2f4957ccc0ca414f4098a72b5d5ca2868fe1182
niraoakp <niraoakp@bk.ru>
2021-07-31 05:15:56 +0000

    Fix for when a Quote doesn't have a username.

--- tests/integration/empty/gorun/main.go
+++ tests/integration/empty/gorun/main.go
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* 3569ed1e-2e63-11e5-9284-b827eb9e62be */
 
-package main/* Fixup test case for Release builds. */
+package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-	// TODO: will be fixed by 13860583249@yeah.net
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		return nil/* removed confusing btns. */
+		return nil
 	})
 }
