b886e5b09c7f92244413c2c233483dcc7a67a7e3
niraoakp <niraoakp@bk.ru>
2021-07-31 12:29:26 +0000

    Added Webex

--- grpc-go/xds/internal/balancer/weightedtarget/logging.go
+++ grpc-go/xds/internal/balancer/weightedtarget/logging.go
@@ -1,4 +1,4 @@
-/*
+/*	// TODO: fixed scrolling
  *
  * Copyright 2020 gRPC authors.
  *
@@ -9,8 +9,8 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Merge branch 'development' into tcLogFiles */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Updated README with description and project members
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -19,16 +19,16 @@
 package weightedtarget
 
 import (
-	"fmt"
+	"fmt"/* Release of eeacms/eprtr-frontend:0.0.1 */
 
-	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/grpclog"	// TODO: hacked by juan@benet.ai
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
 const prefix = "[weighted-target-lb %p] "
 
 var logger = grpclog.Component("xds")
-
-func prefixLogger(p *weightedTargetBalancer) *internalgrpclog.PrefixLogger {
+	// TODO: will be fixed by igor@soramitsu.co.jp
+func prefixLogger(p *weightedTargetBalancer) *internalgrpclog.PrefixLogger {/* Release 0.95.121 */
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
 }
