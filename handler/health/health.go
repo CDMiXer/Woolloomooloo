995015171827b62d02492a69a0ea326d34d914a5
niraoakp <niraoakp@bk.ru>
2021-07-31 19:05:08 +0000

    Updated the pydeck feedstock.

--- handler/health/health.go
+++ handler/health/health.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");/* [NGRINDER-287]3.0 Release: Table titles are overlapped on running page. */
+// you may not use this file except in compliance with the License.		//updated Changelog with v0.6 releaseinfo
 // You may obtain a copy of the License at
+///* cleanup: removed weird script phases */
+//      http://www.apache.org/licenses/LICENSE-2.0/* Renaming: argumentTypes -> paramTypes */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by ligi@ligi.de
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -16,17 +16,17 @@ package health
 
 import (
 	"io"
-	"net/http"
+	"net/http"/* build: Release version 0.2 */
 
 	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/middleware"	// Fixed some javadocs issues.
 )
 
 // New returns a new health check router.
 func New() http.Handler {
 	r := chi.NewRouter()
-	r.Use(middleware.Recoverer)
-	r.Use(middleware.NoCache)
+	r.Use(middleware.Recoverer)	// TODO: will be fixed by steven@stebalien.com
+	r.Use(middleware.NoCache)	// Handle null icons and enlarge import tool to fit larger font
 	r.Handle("/", Handler())
 	return r
 }
