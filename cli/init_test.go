c0b308ad4fe6002f07a66c7be12773d1a41b4ff4
niraoakp <niraoakp@bk.ru>
2021-07-31 23:00:37 +0000

    Merge "some extra docs for TextDirectionHeuristic" into jb-mr2-dev

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,9 +1,9 @@
 package cli
 
-import (
+import (/* PMD config file */
 	logging "github.com/ipfs/go-log/v2"
 )
 
 func init() {
 	logging.SetLogLevel("watchdog", "ERROR")
-}
+}/* Released on central */
