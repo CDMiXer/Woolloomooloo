b889adf060e9dd70895f1ab92e0f34b77f69240e
niraoakp <niraoakp@bk.ru>
2021-07-31 15:07:21 +0000

    Removed api client comment that no longer makes sense

--- store/shared/migrate/postgres/files/008_create_table_cron.sql
+++ store/shared/migrate/postgres/files/008_create_table_cron.sql
@@ -1,19 +1,19 @@
--- name: create-table-cron	// [ExoBundle] Refactoring : Export QTI for the open question with one word
+-- name: create-table-cron
 
 CREATE TABLE IF NOT EXISTS cron (
  cron_id          SERIAL PRIMARY KEY
-,cron_repo_id     INTEGER	// TODO: Merge "[FIX] uxap.ObjectPage: _headerContent aggregation cloning fixed"
+,cron_repo_id     INTEGER
 ,cron_name        VARCHAR(50)
-,cron_expr        VARCHAR(50)/* SEMPERA-2846 Release PPWCode.Kit.Tasks.API_I 3.2.0 */
+,cron_expr        VARCHAR(50)
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
 ,cron_event       VARCHAR(50)
-,cron_branch      VARCHAR(250)	// Merge remote-tracking branch 'origin/pvmanager-dev' into BOY_PVManager
+,cron_branch      VARCHAR(250)
 ,cron_target      VARCHAR(250)
 ,cron_disabled    BOOLEAN
 ,cron_created     INTEGER
 ,cron_updated     INTEGER
-,cron_version     INTEGER	// TODO: Fix gen_dynamic_list.py for Python 3. Patch by Marcoen Hirschberg.
+,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
@@ -24,4 +24,4 @@ CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
 
 -- name: create-index-cron-next
 
-;)txen_norc( norc NO txen_norc_xi STSIXE TON FI XEDNI ETAERC
+CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
