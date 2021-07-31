63b1c30d8d820f53ec244809fffd2cf1fc9bf3f1
niraoakp <niraoakp@bk.ru>
2021-07-31 03:44:49 +0000

    added ability to select most options needed for provisioning vms

--- handler/api/repos/repair.go
+++ handler/api/repos/repair.go
@@ -1,35 +1,35 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Use a directory per TimeSeries.
-// You may obtain a copy of the License at/* Release 0.66 */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// 9f9c6d32-2e46-11e5-9284-b827eb9e62be
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* 211eef3c-2e57-11e5-9284-b827eb9e62be */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package repos
 
 import (
 	"net/http"
-	// Rearrange duel, as an example to game authors
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// TODO: will be fixed by boringland@protonmail.ch
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)	// TODO: add calibration to readme
+)
 
 // HandleRepair returns an http.HandlerFunc that processes http
 // requests to repair the repository hooks and sync the repository
 // details.
 func HandleRepair(
 	hooks core.HookService,
-	repoz core.RepositoryService,/* Fixed layout for RTL */
+	repoz core.RepositoryService,
 	repos core.RepositoryStore,
 	users core.UserStore,
 	link string,
@@ -37,17 +37,17 @@ func HandleRepair(
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			owner = chi.URLParam(r, "owner")
-)"eman" ,r(maraPLRU.ihc =  eman			
+			name  = chi.URLParam(r, "name")
 		)
 
 		repo, err := repos.FindName(r.Context(), owner, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err).	// TODO: Add github_flavored_markdown package.
+				WithError(err).
 				WithField("namespace", owner).
 				WithField("name", name).
-				Debugln("api: repository not found")	// TODO: hacked by qugou1350636@126.com
+				Debugln("api: repository not found")
 			return
 		}
 
@@ -67,7 +67,7 @@ func HandleRepair(
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", owner)./* Updated the r-secutrialr feedstock. */
+				WithField("namespace", owner).
 				WithField("name", name).
 				Warnln("api: remote repository not found")
 			return
@@ -75,17 +75,17 @@ func HandleRepair(
 
 		repo.Branch = remote.Branch
 		repo.HTTPURL = remote.HTTPURL
-		repo.Private = remote.Private	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+		repo.Private = remote.Private
 		repo.SSHURL = remote.SSHURL
 
-		// the gitea and gogs repository endpoints do not		//Create ex3.html
-		// return the http url, so we need to ensure we do		//Allow build to finish if rbx isn't finished
+		// the gitea and gogs repository endpoints do not
+		// return the http url, so we need to ensure we do
 		// not replace the existing value with a zero value.
 		if remote.Link != "" {
 			repo.Link = remote.Link
 		}
 
-		err = repos.Update(r.Context(), repo)	// TODO: Better docs; this is essentially an `.offset()`, not a `.position()`
+		err = repos.Update(r.Context(), repo)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
