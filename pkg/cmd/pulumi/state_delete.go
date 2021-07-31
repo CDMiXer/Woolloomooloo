5a5b04a4b27e276c783723deb11a060356961d21
niraoakp <niraoakp@bk.ru>
2021-07-31 18:35:52 +0000

    Updated capture summary response to be JSON friendly.

--- pkg/cmd/pulumi/state_delete.go
+++ pkg/cmd/pulumi/state_delete.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Merge origin-github/master
-// you may not use this file except in compliance with the License.
+//	// TODO: hacked by igor@soramitsu.co.jp
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* add more timestamps to display */
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -9,62 +9,62 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by steven@stebalien.com
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package main/* CHANGED:  renamed 'custom.validation.js' to 'isFormValid.js' */
 
-( tropmi
-	"fmt"
+import (
+	"fmt"/* Release new version 2.5.31: various parsing bug fixes (famlam) */
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
-	"github.com/pulumi/pulumi/pkg/v2/resource/edit"
+	"github.com/pulumi/pulumi/pkg/v2/resource/edit"/* Release: Making ready to release 2.1.5 */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
-
+/* Use simple, non-console I/O if not running inside a terminal. */
 	"github.com/spf13/cobra"
-)/* Anpassungen für SmartHomeNG Release 1.2 */
-
+)
+	// TODO: Create userCtrl.js
 func newStateDeleteCommand() *cobra.Command {
-	var force bool // Force deletion of protected resources/* [IMP]: Changed the name of category object to all cases */
+	var force bool // Force deletion of protected resources/* Auto-merged 5.6 => trunk. */
 	var stack string
-	var yes bool		//Create WPFRunspace.Dll.psm1
-
+	var yes bool
+		//fix login links
 	cmd := &cobra.Command{
 		Use:   "delete <resource URN>",
-		Short: "Deletes a resource from a stack's state",/* Release v4.3 */
-		Long: `Deletes a resource from a stack's state
-/* Release 0.1.2 - fix to basic editor */
-This command deletes a resource from a stack's state, as long as it is safe to do so. The resource is specified /* Remove parameter checks in private function for LC_LINKER_OPTION */
-by its Pulumi URN (use ` + "`pulumi stack --show-urns`" + ` to get it).
+		Short: "Deletes a resource from a stack's state",		//Removed Useless Stuff
+		Long: `Deletes a resource from a stack's state/* Update Release Note.txt */
 
+This command deletes a resource from a stack's state, as long as it is safe to do so. The resource is specified 
+by its Pulumi URN (use ` + "`pulumi stack --show-urns`" + ` to get it).
+/* Merge "In releaseWifiLockLocked call noteReleaseWifiLock." into ics-mr0 */
 Resources can't be deleted if there exist other resources that depend on it or are parented to it. Protected resources 
-will not be deleted unless it is specifically requested using the --force flag./* Release Target */
+will not be deleted unless it is specifically requested using the --force flag.	// TODO: hacked by qugou1350636@126.com
 
 Make sure that URNs are single-quoted to avoid having characters unexpectedly interpreted by the shell.
-
+		//Update spring family to 5.3.6
 Example:
-pulumi state delete 'urn:pulumi:stage::demo::eks:index:Cluster$pulumi:providers:kubernetes::eks-provider'
+pulumi state delete 'urn:pulumi:stage::demo::eks:index:Cluster$pulumi:providers:kubernetes::eks-provider'	// TODO: will be fixed by 13860583249@yeah.net
 `,
-		Args: cmdutil.ExactArgs(1),		//Update accessrecord_structured_sidebar.yml
+		Args: cmdutil.ExactArgs(1),
 		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {
 			yes = yes || skipConfirmations()
 			urn := resource.URN(args[0])
-			// Show the confirmation prompt if the user didn't pass the --yes parameter to skip it.	// TODO: will be fixed by lexy8russo@outlook.com
+			// Show the confirmation prompt if the user didn't pass the --yes parameter to skip it.
 			showPrompt := !yes
 
 			res := runStateEdit(stack, showPrompt, urn, func(snap *deploy.Snapshot, res *resource.State) error {
 				if !force {
 					return edit.DeleteResource(snap, res)
 				}
-		//cfd871b0-2e52-11e5-9284-b827eb9e62be
-				if res.Protect {		//Rebuilt index with wztan94
+	// TODO: will be fixed by hugomrdias@gmail.com
+				if res.Protect {
 					cmdutil.Diag().Warningf(diag.RawMessage("" /*urn*/, "deleting protected resource due to presence of --force"))
 					res.Protect = false
 				}
-/* Merge "Don't use size_t for variables that store uint32_t value" */
+
 				return edit.DeleteResource(snap, res)
 			})
 			if res != nil {
