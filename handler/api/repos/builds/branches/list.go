726f8e097b6b4d7ee86d1a200ddc2e7b4c3aa78f
niraoakp <niraoakp@bk.ru>
2021-07-31 14:18:39 +0000

    [TRAVIS] Minor fixes

--- handler/api/repos/builds/branches/list.go
+++ handler/api/repos/builds/branches/list.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Merge "ASoC: WCD9310: Use SLIMBUS ports 7 and 8 for TX." into msm-2.6.38
-///* JO-585: correzione nome variabile queryset */
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Correxions
-///* Release for v6.1.0. */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,19 +13,19 @@
 // limitations under the License.
 
 package branches
-/* BaseScmReleasePlugin added and used for GitReleasePlugin */
-import (/* Merge "Release 3.2.3.351 Prima WLAN Driver" */
+
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* a159e4c2-2e75-11e5-9284-b827eb9e62be */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
-// list of build history to the response body./* fix missing option filename '$s' */
+// list of build history to the response body.
 func HandleList(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
@@ -35,27 +35,27 @@ func HandleList(
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)/* Update Mapper.java */
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)	// Reorganizing and Reverting to 1.8.5_01
+			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot find repository")/* Fixed Release config problem. */
+				Debugln("api: cannot find repository")
 			return
 		}
-	// TODO: will be fixed by caojiaoyue@protonmail.com
+
 		results, err := builds.LatestBranches(r.Context(), repo.ID)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).		//modify DomainController.java
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot list builds")
-		} else {	// Remove unused variables & methods from old repair system
+		} else {
 			render.JSON(w, results, 200)
-}		
+		}
 	}
 }
