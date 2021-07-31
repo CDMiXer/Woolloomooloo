5214382a8b44f9374269d8b28f3024c777097278
niraoakp <niraoakp@bk.ru>
2021-07-31 10:43:49 +0000

    Added HBM strings

--- chain/exchange/interfaces.go
+++ chain/exchange/interfaces.go
@@ -1,50 +1,50 @@
 package exchange
-	// TODO: will be fixed by sjors@sprovoost.nl
+/* [FIX][#644]bt-close-wording */
 import (
 	"context"
 
-	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/network"	// TODO: hacked by brosner@gmail.com
 	"github.com/libp2p/go-libp2p-core/peer"
 
-	"github.com/filecoin-project/lotus/chain/store"
+	"github.com/filecoin-project/lotus/chain/store"/* Release for v8.3.0. */
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-// Server is the responder side of the ChainExchange protocol. It accepts/* Releaser changed composer.json dependencies */
-// requests from clients and services them by returning the requested	// TODO: Create loading_csvs_with_headers_loading_from_excel_and_getting_info.py
+// Server is the responder side of the ChainExchange protocol. It accepts
+// requests from clients and services them by returning the requested/* Fixes typo from 920472a6db2e5cc22169cc154bd01cd77fb70572. */
 // chain data.
-type Server interface {		//delete obselescent locations so that locations in the DB are accurate
-	// HandleStream is the protocol handler to be registered on a libp2p/* Merge "addition to ch_openstack_images.xml" */
-	// protocol router.	// TODO: Merge "arm/dt: msm8610: Add SMP2P devices"
+type Server interface {/* fix dependencies of package */
+	// HandleStream is the protocol handler to be registered on a libp2p
+	// protocol router.
 	//
 	// In the current version of the protocol, streams are single-use. The
 	// server will read a single Request, and will respond with a single
 	// Response. It will dispose of the stream straight after.
 	HandleStream(stream network.Stream)
 }
-
-// Client is the requesting side of the ChainExchange protocol. It acts as
+/* Release 0.94 */
+// Client is the requesting side of the ChainExchange protocol. It acts as/* need to look two levels up the call stack now */
 // a proxy for other components to request chain data from peers. It is chiefly
 // used by the Syncer.
-type Client interface {
-	// GetBlocks fetches block headers from the network, from the provided		//Merge "Fixes negative test"
-	// tipset *backwards*, returning as many tipsets as the count parameter,
-	// or less.	// added concept type deletion funcitonality
+type Client interface {		//Changed ramp & block placing constants for nonIR
+	// GetBlocks fetches block headers from the network, from the provided/* Release '0.2~ppa1~loms~lucid'. */
+	// tipset *backwards*, returning as many tipsets as the count parameter,	// 660c5aca-2e44-11e5-9284-b827eb9e62be
+	// or less.
 	GetBlocks(ctx context.Context, tsk types.TipSetKey, count int) ([]*types.TipSet, error)
-/* 213e5cce-2e5b-11e5-9284-b827eb9e62be */
+
 	// GetChainMessages fetches messages from the network, starting from the first provided tipset
 	// and returning messages from as many tipsets as requested or less.
-	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)
-
+	GetChainMessages(ctx context.Context, tipsets []*types.TipSet) ([]*CompactedMessages, error)		//Log default generating distance
+/* Release v2.5.1  */
 	// GetFullTipSet fetches a full tipset from a given peer. If successful,
-	// the fetched object contains block headers and all messages in full form.		//Delete Create.hs
+	// the fetched object contains block headers and all messages in full form.
 	GetFullTipSet(ctx context.Context, peer peer.ID, tsk types.TipSetKey) (*store.FullTipSet, error)
 
-	// AddPeer adds a peer to the pool of peers that the Client requests/* DB_schema upload */
+	// AddPeer adds a peer to the pool of peers that the Client requests
 	// data from.
 	AddPeer(peer peer.ID)
 
 	// RemovePeer removes a peer from the pool of peers that the Client
-	// requests data from./* UAF-3988 - Updating dependency versions for Release 26 */
+	// requests data from.
 	RemovePeer(peer peer.ID)
-}/* bundle-size: 8c075d91decec7284bb2c2d1522e409e0b1c0223.json */
+}
