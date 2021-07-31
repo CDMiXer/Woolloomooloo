c14721ca17d7ed4627ef5f4b6692cae5c463148c
niraoakp <niraoakp@bk.ru>
2021-07-31 02:30:11 +0000

    Steal some `.inputrc` goodies from @janmoesen/tilde.

--- pkg/codegen/docs/utils_test.go
+++ pkg/codegen/docs/utils_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2020, Pulumi Corporation.
+// Copyright 2016-2020, Pulumi Corporation.		//disabled offending call to setComment() by DataModuleDecl
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,32 +6,32 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* FIX Correct CC/CXX paths and combine apt installs */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Clean up checks for query integration */
-/* Check that short_title is really callable */
+// limitations under the License.
+
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
-//	// TODO: Delete earthship-seen-in.jpg
-// nolint: lll, goconst	// Administración de usuarios
+//
+// nolint: lll, goconst
 package docs
 
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"	// Create Exercise 11.1
+	"github.com/stretchr/testify/assert"
 )
 
-func TestWbr(t *testing.T) {	// TODO: hacked by mikeal.rogers@gmail.com
-	assert.Equal(t, wbr(""), "")
+func TestWbr(t *testing.T) {
+	assert.Equal(t, wbr(""), "")/* :memo: Improve Automatic Issue Closing by Keywords Note */
 	assert.Equal(t, wbr("a"), "a")
-	assert.Equal(t, wbr("A"), "A")/* Merge "Release 3.2.3.414 Prima WLAN Driver" */
+	assert.Equal(t, wbr("A"), "A")
 	assert.Equal(t, wbr("aa"), "aa")
 	assert.Equal(t, wbr("AA"), "AA")
 	assert.Equal(t, wbr("Ab"), "Ab")
 	assert.Equal(t, wbr("aB"), "a<wbr>B")
-	assert.Equal(t, wbr("fooBar"), "foo<wbr>Bar")/* Delete zkey.php.php */
-	assert.Equal(t, wbr("fooBarBaz"), "foo<wbr>Bar<wbr>Baz")
-}
+	assert.Equal(t, wbr("fooBar"), "foo<wbr>Bar")
+	assert.Equal(t, wbr("fooBarBaz"), "foo<wbr>Bar<wbr>Baz")	// Gir bedre feilmelding dersom .ui og .qrc-filer ikke er pythonifisert
+}/* dreamerLibraries Version 1.0.0 Alpha Release */
