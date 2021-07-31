85ca7997305eee107b3b8b7d5590e513ab01c0e2
niraoakp <niraoakp@bk.ru>
2021-07-31 00:52:47 +0000

    Merged with filehandling branch.

--- tests/integration/invalid_package_json/index.ts
+++ tests/integration/invalid_package_json/index.ts
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: will be fixed by lexy8russo@outlook.com
+
 import { Config } from "@pulumi/pulumi";
 import * as runtime from "@pulumi/pulumi/runtime"
 
-(async function() {/* App formatting */
-    const config = new Config();/* Release 2.8.4 */
+(async function() {
+    const config = new Config();
 
     // Ensure we get the right set of dependencies back.  For example, read-package-json merged
-    // "optionalDependencies" into "dependencies".  We want to make sure we still follow that/* Release of eeacms/www-devel:18.8.1 */
+    // "optionalDependencies" into "dependencies".  We want to make sure we still follow that
     // behavior.
     const deps = await runtime.computeCodePaths();
 
     const actual = JSON.stringify([...deps.keys()].sort());
-    const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";	// TODO: will be fixed by 13860583249@yeah.net
+    const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";
 
     if (actual !== expected) {
-        throw new Error(`Got '${actual}' expected '${expected}'`)	// TODO: created MIRA-4.0.2_fix-ads-include.patch
+        throw new Error(`Got '${actual}' expected '${expected}'`)
     }
-})()	// TODO: add experimental _on_create_new_window()
+})()
