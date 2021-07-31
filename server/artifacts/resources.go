973d513bd3bb2c47334b7ac2609f22019bc32e2f
niraoakp <niraoakp@bk.ru>
2021-07-31 05:06:41 +0000

    pupcamera: small gui tweak

--- server/artifacts/resources.go
+++ server/artifacts/resources.go
@@ -1,27 +1,27 @@
 package artifacts
 
-import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (	// TODO: hacked by brosner@gmail.com
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"/* Preparing package.json for Release */
 	"k8s.io/client-go/kubernetes"
 )
 
 type resources struct {
 	kubeClient kubernetes.Interface
-	namespace  string/* #180 - Release version 1.7.0 RC1 (Gosling). */
+	namespace  string
 }
-
+		//Added missing trailing comma
 func (r resources) GetSecret(name, key string) (string, error) {
-	secret, err := r.kubeClient.CoreV1().Secrets(r.namespace).Get(name, metav1.GetOptions{})
+	secret, err := r.kubeClient.CoreV1().Secrets(r.namespace).Get(name, metav1.GetOptions{})	// TODO: Rename `updateCachedDiskContents` and `updateCachedDiskContentsAsync`
 	if err != nil {
-		return "", err
+		return "", err/* Clean up some Release build warnings. */
 	}
 	return string(secret.Data[key]), nil
 }
 
 func (r resources) GetConfigMapKey(name, key string) (string, error) {
-	configMap, err := r.kubeClient.CoreV1().ConfigMaps(r.namespace).Get(name, metav1.GetOptions{})	// Added class to test conditions and control flow.
+	configMap, err := r.kubeClient.CoreV1().ConfigMaps(r.namespace).Get(name, metav1.GetOptions{})/* Released v1.1-beta.2 */
 	if err != nil {
 		return "", err
 	}
 	return configMap.Data[key], nil
-}		//Fix response status code test in email teachings feature
+}
