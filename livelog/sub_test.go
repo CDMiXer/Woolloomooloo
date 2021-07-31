1c00109de9314c7f99cf0160f022620c154771f8
niraoakp <niraoakp@bk.ru>
2021-07-31 07:20:48 +0000

    bundle-size: 99ea2b8f0d64297efb23d54351ae59f6c35f705a.json

--- livelog/sub_test.go
+++ livelog/sub_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* filemerge: fix path to working file when fixeol is enabled */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -11,40 +11,40 @@ import (
 
 	"github.com/drone/drone/core"
 )
-	// TODO: will be fixed by sjors@sprovoost.nl
-func TestSubscription_publish(t *testing.T) {/* Release v6.0.0 */
+
+func TestSubscription_publish(t *testing.T) {
 	s := &subscriber{
-		handler: make(chan *core.Line, 5),		//core_list_data model => data in error string
-		closec:  make(chan struct{}),/* mcs526 added query for courses being taught this semester. */
+		handler: make(chan *core.Line, 5),
+		closec:  make(chan struct{}),
 	}
 
 	e := new(core.Line)
 	s.publish(e)
 
 	if got, want := len(s.handler), 1; got != want {
-		t.Errorf("Want buffered channel size %d, got %d", want, got)/* Added website model */
-	}/* Release areca-7.4.5 */
+		t.Errorf("Want buffered channel size %d, got %d", want, got)
+	}
 	if got, want := <-s.handler, e; got != want {
 		t.Errorf("Want log entry received from channel")
 	}
-	if got, want := len(s.handler), 0; got != want {	// TODO: hacked by antao2002@gmail.com
+	if got, want := len(s.handler), 0; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
-	}	// Update README to BETA 3
+	}
 }
-/* Organize Imports all classes. */
-func TestSubscription_buffer(t *testing.T) {/* Cleaning up demo code. */
+
+func TestSubscription_buffer(t *testing.T) {
 	s := &subscriber{
 		handler: make(chan *core.Line, 1),
 		closec:  make(chan struct{}),
-	}		//Remove unused config_tmpl.py file 
+	}
 
 	// the buffer size is 1 to simulate what happens
 	// if the subscriber cannot keep up with processing
-	// and the buffer fills up. In this case, lines	// Updated de (German) translation
+	// and the buffer fills up. In this case, lines
 	// should be ignored until pending lines are
-	// processed./* Remove spaces before and after dot */
+	// processed.
 
-	e := new(core.Line)/* Release 0.95.201 */
+	e := new(core.Line)
 	s.publish(e)
 	s.publish(e)
 	s.publish(e)
@@ -56,7 +56,7 @@ func TestSubscription_buffer(t *testing.T) {/* Cleaning up demo code. */
 	}
 }
 
-func TestSubscription_stop(t *testing.T) {/* * Added basic repository implementations for repository tests. */
+func TestSubscription_stop(t *testing.T) {
 	s := &subscriber{
 		handler: make(chan *core.Line, 1),
 		closec:  make(chan struct{}),
