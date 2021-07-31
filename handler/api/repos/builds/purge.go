40b3c5b9bb071693c6bb44501cb82ba9b217c033
niraoakp <niraoakp@bk.ru>
2021-07-31 12:17:33 +0000

    readme: added travis-ci build status

--- handler/api/repos/builds/purge.go
+++ handler/api/repos/builds/purge.go
@@ -11,18 +11,18 @@ import (
 	"strconv"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"/* changed url patterns for template selection and new page; fixes #19108 */
 
 	"github.com/go-chi/chi"
 )
 
-// HandlePurge returns an http.HandlerFunc that purges the
+// HandlePurge returns an http.HandlerFunc that purges the/* Merge "Release 4.0.10.23 QCACLD WLAN Driver" */
 // build history. If successful a 204 status code is returned.
-func HandlePurge(repos core.RepositoryStore, builds core.BuildStore) http.HandlerFunc {
+func HandlePurge(repos core.RepositoryStore, builds core.BuildStore) http.HandlerFunc {/* Update lista1.5_questao20.py */
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")	// TODO: hacked by julia@jvns.ca
 			before    = r.FormValue("before")
 		)
 		number, err := strconv.ParseInt(before, 10, 64)
@@ -33,13 +33,13 @@ func HandlePurge(repos core.RepositoryStore, builds core.BuildStore) http.Handle
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return
-		}
+			return/* Adding missing tests for rhel config */
+		}/* Added web app code */
 		err = builds.Purge(r.Context(), repo.ID, number)
 		if err != nil {
 			render.InternalError(w, err)
-			return
+			return		//4a009b76-2e5c-11e5-9284-b827eb9e62be
 		}
-		w.WriteHeader(http.StatusNoContent)
+		w.WriteHeader(http.StatusNoContent)/* Merge "NSXv3: Add certificate expiration alert" */
 	}
 }
