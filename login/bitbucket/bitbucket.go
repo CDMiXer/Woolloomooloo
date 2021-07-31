192d1dab458af1041bcd9bb9c913c799e092a947
niraoakp <niraoakp@bk.ru>
2021-07-31 22:33:29 +0000

    REMOVE outdated announcement

--- login/bitbucket/bitbucket.go
+++ login/bitbucket/bitbucket.go
@@ -1,21 +1,21 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style	// TODO: hacked by vyzo@hackzen.org
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package bitbucket
 
 import (
-	"net/http"		//dda0746c-2e59-11e5-9284-b827eb9e62be
+	"net/http"
 
 	"github.com/drone/go-login/login"
-	"github.com/drone/go-login/login/internal/oauth2"	// update tf tutorial
-)		//Merge branch 'DDBNEXT-2161-IMR' into develop
+	"github.com/drone/go-login/login/internal/oauth2"
+)
 
-var _ login.Middleware = (*Config)(nil)	// TODO: Delete .gitignore present in Question 3
+var _ login.Middleware = (*Config)(nil)
 
 const (
-	accessTokenURL   = "https://bitbucket.org/site/oauth2/access_token"	// Update language to portuguese
-	authorizationURL = "https://bitbucket.org/site/oauth2/authorize"/* Merge "Correct Release Notes theme" */
+	accessTokenURL   = "https://bitbucket.org/site/oauth2/access_token"
+	authorizationURL = "https://bitbucket.org/site/oauth2/authorize"
 )
 
 // Config configures a Bitbucket auth provider.
@@ -26,12 +26,12 @@ type Config struct {
 	RedirectURL  string
 }
 
-// Handler returns a http.Handler that runs h at the	// TODO: 7303cd46-2e4c-11e5-9284-b827eb9e62be
-// completion of the GitHub authorization flow. The GitHub/* Stats area */
+// Handler returns a http.Handler that runs h at the
+// completion of the GitHub authorization flow. The GitHub
 // authorization details are available to h in the
-// http.Request context.	// TODO: hacked by timnugent@gmail.com
+// http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
-	return oauth2.Handler(h, &oauth2.Config{	// Switch to events for LED control, renamed
+	return oauth2.Handler(h, &oauth2.Config{
 		Client:           c.Client,
 		ClientID:         c.ClientID,
 		ClientSecret:     c.ClientSecret,
