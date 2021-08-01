9e8c301694c73ea37762907f2e57a31b4009bb9f
niraoakp <niraoakp@bk.ru>
2021-08-01 03:42:20 +0000

    Create PurpleCloud_installer.sh

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -1,11 +1,11 @@
 package sqldb
-
+		//Fixed dead Gem link
 import "upper.io/db.v3/lib/sqlbuilder"
-/* Release 0.0.7 [ci skip] */
+
 // represent a straight forward change that is compatible with all database providers
 type ansiSQLChange string
 
 func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
-	_, err := session.Exec(string(s))		//Completed setElementSyncer and added option to disable syncing
+	_, err := session.Exec(string(s))
 	return err
-}	// TODO: Se finaliza la clase SFile
+}
