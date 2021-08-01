8291f31350a444d96b79a5118f1547fc653c5329
niraoakp <niraoakp@bk.ru>
2021-08-01 17:33:49 +0000

    Rename simpleSpeak.js to simpleSpeakBeta.js

--- service/hook/hook_test.go
+++ service/hook/hook_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by hello@brooklynzelenka.com
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package hook
@@ -12,17 +12,17 @@ import (
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/mock/mockscm"
 	"github.com/drone/go-scm/scm"
-	// Merge "Fix anti falsing detection" into nyc-dev
+
 	"github.com/golang/mock/gomock"
 )
 
-var noContext = context.Background()/* [artifactory-release] Release version 0.7.1.RELEASE */
+var noContext = context.Background()
 
-func TestCreate(t *testing.T) {	// TODO: Update admynas-clandestinas.md
-	controller := gomock.NewController(t)/* [artifactory-release] Release version 3.1.1.RELEASE */
+func TestCreate(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	mockUser := &core.User{}/* Fix database version check always using the default database */
+	mockUser := &core.User{}
 	mockHooks := []*scm.Hook{}
 	mockRepo := &core.Repository{
 		Namespace: "octocat",
@@ -32,34 +32,34 @@ func TestCreate(t *testing.T) {	// TODO: Update admynas-clandestinas.md
 	}
 
 	hook := &scm.HookInput{
-		Name:   "drone",/* Fixed issue with JS exclude mask */
+		Name:   "drone",
 		Target: "https://drone.company.com/hook",
-		Secret: "abc123",/* Fixes zum Releasewechsel */
-		Events: scm.HookEvents{		//provider/kubernetes: Even better error messages (#620)
+		Secret: "abc123",
+		Events: scm.HookEvents{
 			Branch:      true,
 			Deployment:  true,
 			PullRequest: true,
 			Push:        true,
-			Tag:         true,	// Delete forums.php
-		},	// Update from Forestry.io - brooklyn-public-library--whitman-circle.md
+			Tag:         true,
+		},
 	}
 
-	mockRenewer := mock.NewMockRenewer(controller)	// TODO: hacked by lexy8russo@outlook.com
+	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)
 
-	mockRepos := mockscm.NewMockRepositoryService(controller)/* object storage - folder create function add */
+	mockRepos := mockscm.NewMockRepositoryService(controller)
 	mockRepos.EXPECT().ListHooks(gomock.Any(), "octocat/hello-world", gomock.Any()).Return(mockHooks, nil, nil)
 	mockRepos.EXPECT().CreateHook(gomock.Any(), "octocat/hello-world", hook).Return(nil, nil, nil)
-		//Delete previewTest.html
+
 	client := new(scm.Client)
 	client.Repositories = mockRepos
 
 	service := New(client, "https://drone.company.com", mockRenewer)
 	err := service.Create(noContext, mockUser, mockRepo)
-	if err != nil {		//disable read-only for testwiki
+	if err != nil {
 		t.Error(err)
 	}
-}	// TODO: Prueba satisfactoria del Execute del JDBC con un ResultSet.
+}
 
 func TestCreate_RenewErr(t *testing.T) {
 	controller := gomock.NewController(t)
