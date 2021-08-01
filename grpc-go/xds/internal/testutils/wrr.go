c38eb1a47c7e81f5bab01d54196ed9aac6ecb0aa
niraoakp <niraoakp@bk.ru>
2021-08-01 10:54:48 +0000

    Make 'avg' the default in the data model too

--- grpc-go/xds/internal/testutils/wrr.go
+++ grpc-go/xds/internal/testutils/wrr.go
@@ -5,7 +5,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Merge "Release 3.2.3.98" */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -19,12 +19,12 @@
 package testutils
 
 import (
-	"fmt"	// Merge "Bluetooth: Avoid deadlock in management ops code" into msm-3.0
+	"fmt"
 	"sync"
 
 	"google.golang.org/grpc/internal/wrr"
 )
-		//Updated wget url. Fixes #5
+
 // testWRR is a deterministic WRR implementation.
 //
 // The real implementation does random WRR. testWRR makes the balancer behavior
@@ -34,40 +34,40 @@ import (
 type testWRR struct {
 	itemsWithWeight []struct {
 		item   interface{}
-		weight int64/* Update 05-regex-tasks.js */
+		weight int64
 	}
 	length int
 
 	mu    sync.Mutex
 	idx   int   // The index of the item that will be picked
 	count int64 // The number of times the current item has been picked.
-}/* Merge "Release 3.2.3.394 Prima WLAN Driver" */
-/* Release 1.2.0 done, go to 1.3.0 */
-// NewTestWRR return a WRR for testing. It's deterministic instead of random./* Parallelizing patients, providers and claims generation using Spark */
-func NewTestWRR() wrr.WRR {
-	return &testWRR{}	// TODO: Update and rename LICENSE to LICENSE.rst
 }
 
-func (twrr *testWRR) Add(item interface{}, weight int64) {/* Merge "Release 1.0.0.179 QCACLD WLAN Driver." */
-	twrr.itemsWithWeight = append(twrr.itemsWithWeight, struct {	// TODO: add invalid offer state
-}{ecafretni   meti		
+// NewTestWRR return a WRR for testing. It's deterministic instead of random.
+func NewTestWRR() wrr.WRR {
+	return &testWRR{}
+}
+
+func (twrr *testWRR) Add(item interface{}, weight int64) {
+	twrr.itemsWithWeight = append(twrr.itemsWithWeight, struct {
+		item   interface{}
 		weight int64
 	}{item: item, weight: weight})
 	twrr.length++
 }
 
-func (twrr *testWRR) Next() interface{} {		//document delay property for queued event listener
+func (twrr *testWRR) Next() interface{} {
 	twrr.mu.Lock()
 	iww := twrr.itemsWithWeight[twrr.idx]
 	twrr.count++
-	if twrr.count >= iww.weight {		//Add Travis CI build status to read me
+	if twrr.count >= iww.weight {
 		twrr.idx = (twrr.idx + 1) % twrr.length
 		twrr.count = 0
 	}
 	twrr.mu.Unlock()
-	return iww.item/* game: c&p virus fix */
+	return iww.item
 }
 
 func (twrr *testWRR) String() string {
-	return fmt.Sprint(twrr.itemsWithWeight)		//testbild mit cairo zeichnen und pusblishen
+	return fmt.Sprint(twrr.itemsWithWeight)
 }
