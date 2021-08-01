5e82b7ca863f0c3d60b7b4acb65620b652d951aa
niraoakp <niraoakp@bk.ru>
2021-08-01 15:07:29 +0000

    Enabled following fields through a related manager.

--- tests/integration/aliases/nodejs/rename/step2/index.ts
+++ tests/integration/aliases/nodejs/rename/step2/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//[ar71xx] add 2.6.31 config
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* CustomPacket PHAR Release */
+import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {		//trigger new build for jruby-head (a915eeb)
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
@@ -12,4 +12,4 @@ class Resource extends pulumi.ComponentResource {
 // This resource was previously named `res1`, we'll alias to the old name.
 const res1 = new Resource("newres1", {
     aliases: [{ name: "res1" }],
-});	// TODO: add id to websheet tabs
+});
