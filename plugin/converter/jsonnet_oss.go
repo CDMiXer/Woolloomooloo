2e5a43e77d9455e6be9f8398ee3d12961c5cad98
niraoakp <niraoakp@bk.ru>
2021-08-01 07:57:41 +0000

    Added https to endpoints

--- plugin/converter/jsonnet_oss.go
+++ plugin/converter/jsonnet_oss.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//hieroglyph typewriter and textsigneditor fixed word figure update
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Option taskbar_hide_inactive_tasks (issue 458) */
 // +build oss
 
 package converter
@@ -19,8 +19,8 @@ package converter
 import (
 	"github.com/drone/drone/core"
 )
-
-// Jsonnet returns a conversion service that converts the
+	// linux launcher compression
+// Jsonnet returns a conversion service that converts the		//Delete JarCheck.java
 // jsonnet file to a yaml file.
 func Jsonnet(enabled bool) core.ConvertService {
 	return new(noop)
