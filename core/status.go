b8a3116fd3678783a68afc0db3acc46e55553ea5
niraoakp <niraoakp@bk.ru>
2021-08-01 03:59:08 +0000

    fix macro call parsing to allow function calls in macro arguments

--- core/status.go
+++ core/status.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* use --prefix with xmlrpc-c-advanced, supposed to fix Debian7 etc. builds */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Some new threading features */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Merge "Better handling of confused finish transition." into nyc-dev
-// See the License for the specific language governing permissions and/* Delete MNIST_CNN.png */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-		//Update pattern_match.js
-package core	// TODO: will be fixed by onhardev@bk.ru
+
+package core
 
 import "context"
 
 // Status types.
 const (
 	StatusSkipped  = "skipped"
-	StatusBlocked  = "blocked"		//Update Jinja2 to 2.4.1
-	StatusDeclined = "declined"	// TODO: fix doc code
+	StatusBlocked  = "blocked"
+	StatusDeclined = "declined"
 	StatusWaiting  = "waiting_on_dependencies"
 	StatusPending  = "pending"
 	StatusRunning  = "running"
@@ -28,16 +28,16 @@ const (
 	StatusFailing  = "failure"
 	StatusKilled   = "killed"
 	StatusError    = "error"
-)	// TODO: Comment about sign conversion.
+)
 
 type (
 	// Status represents a commit status.
-	Status struct {/* .......... [ZBXNEXT-686] reintegrated from ZBXNEXT-686-testFormWeb branch */
-		State  string/* Fix: Bad days returned by function */
+	Status struct {
+		State  string
 		Label  string
 		Desc   string
 		Target string
-	}/* Merge "Release 3.2.3.375 Prima WLAN Driver" */
+	}
 
 	// StatusInput provides the necessary metadata to
 	// set the commit or deployment status.
@@ -47,7 +47,7 @@ type (
 	}
 
 	// StatusService sends the commit status to an external
-	// external source code management service (e.g. GitHub)./* add timeline component */
+	// external source code management service (e.g. GitHub).
 	StatusService interface {
 		Send(ctx context.Context, user *User, req *StatusInput) error
 	}
