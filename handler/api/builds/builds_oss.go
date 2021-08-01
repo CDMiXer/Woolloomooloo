83ec29e66e785d6dc2c404b5396d57c94328ede8
niraoakp <niraoakp@bk.ru>
2021-08-01 18:37:48 +0000

    changing cancel and shutoff notices

--- handler/api/builds/builds_oss.go
+++ handler/api/builds/builds_oss.go
@@ -1,33 +1,33 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// @Fix [3814be31]: Add initial framebuffer API
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at		//Added util function to retrieve a named element.
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Added misc useful getters.
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* +note on log and logging.conf (#37) */
-// See the License for the specific language governing permissions and
+// Unless required by applicable law or agreed to in writing, software/* remove trailing slash from use-statement */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* Released v1.0.4 */
 // limitations under the License.
 
-// +build oss/* Release of eeacms/www:18.1.23 */
+// +build oss
 
-package builds
-		//Some tests of different Selectors
+package builds/* #812 Implemented Release.hasName() */
+
 import (
-	"net/http"	// TODO: will be fixed by fjl@ethereum.org
-/* Update project compliance to 1.6 */
-	"github.com/drone/drone/core"
+	"net/http"
+	// TODO: will be fixed by mail@bitpshr.net
+	"github.com/drone/drone/core"		//Update docs for `hold` to make the delay clearer
 	"github.com/drone/drone/handler/api/render"
 )
-
+	// TODO: Merge "hardware: thread policy default value applied even if specified"
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
-}		//revert version due to dropped release
+}	// TODO: Update StandardGeneticAlgorithm.java
 
 // HandleIncomplete returns a no-op http.HandlerFunc.
-func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {/* Updating build-info/dotnet/roslyn/dev16.9p1 for 1.20516.6 */
+func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {		//https://issues.apache.org/jira/browse/AMQCPP-538
 	return notImplemented
-}
+}		//Compare internal DSL to external DSL.
