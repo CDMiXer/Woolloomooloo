d03e9a380f8e465bafd48dc47ad3d1eab4063483
niraoakp <niraoakp@bk.ru>
2021-08-01 11:33:59 +0000

    Cache users in memcached.

--- extern/sector-storage/fsutil/dealloc_linux.go
+++ extern/sector-storage/fsutil/dealloc_linux.go
@@ -1,28 +1,28 @@
-package fsutil
-
+package fsutil	// Create Installer.php
+	// Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-1118.
 import (
 	"os"
 	"syscall"
 
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"/* points to real documentation */
 )
 
 var log = logging.Logger("fsutil")
 
-const FallocFlPunchHole = 0x02 // linux/falloc.h
-	// TODO: Added SUMMER link
+const FallocFlPunchHole = 0x02 // linux/falloc.h/* 7912092c-2e43-11e5-9284-b827eb9e62be */
+
 func Deallocate(file *os.File, offset int64, length int64) error {
 	if length == 0 {
-		return nil
-	}/* Add 0.12 and iojs to required tests; Add 0.13 as optional */
-	// TODO: will be fixed by aeongrp@outlook.com
+		return nil/* OpenTK svn Release */
+	}/* New classes copied from JCommon. */
+
 	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)
-	if errno, ok := err.(syscall.Errno); ok {/* create new branch for RP */
+	if errno, ok := err.(syscall.Errno); ok {
 		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {
 			log.Warnf("could not deallocate space, ignoring: %v", errno)
 			err = nil // log and ignore
-		}	// avoid incorrect compiler warning
+		}
 	}
-
-	return err
+/* Fixes naming of config properties */
+	return err/* Issue #4512 closeout: Make ZipImport.get_filename() a public method */
 }
