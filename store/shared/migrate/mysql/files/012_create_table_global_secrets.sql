4ba1894d8042d0c1af5c1a568448252bed68dfeb
niraoakp <niraoakp@bk.ru>
2021-08-01 22:14:33 +0000

    Merge branch 'master' into iss-1192-test

--- store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
+++ store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
@@ -1,12 +1,12 @@
--- name: create-table-org-secrets/* link to glossary */
-/* Release now! */
+-- name: create-table-org-secrets
+
 CREATE TABLE IF NOT EXISTS orgsecrets (
- secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT	// TODO: will be fixed by why@ipfs.io
+ secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
 ,secret_namespace         VARCHAR(50)
 ,secret_name              VARCHAR(200)
 ,secret_type              VARCHAR(50)
-,secret_data              BLOB/* Added Beta build of apk */
+,secret_data              BLOB/* Merge "Fixing grenade job" */
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN	// removed code.
+,secret_pull_request_push BOOLEAN/* Release Notes for v02-15-03 */
 ,UNIQUE(secret_namespace, secret_name)
-;)
+);
