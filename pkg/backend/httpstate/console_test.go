1ddd6dbb256926ccb5f4e4afaf23e134a150a65d
niraoakp <niraoakp@bk.ru>
2021-08-01 20:51:05 +0000

    delta matrix initialization ordered by measurement

--- pkg/backend/httpstate/console_test.go
+++ pkg/backend/httpstate/console_test.go
@@ -1,27 +1,27 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* (jam) Release bzr-1.7.1 final */
-// You may obtain a copy of the License at/* Updated quick links for symplicity, banssb, connquest */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Merge "msm_fb: display: Add delay kickoff to MDDI" into android-msm-2.6.35
+// See the License for the specific language governing permissions and
 // limitations under the License.
-package httpstate/* Updated Showcase Examples for Release 3.1.0 with Common Comparison Operations */
+package httpstate
 
 import (
 	"os"
-	"testing"	// TODO: will be fixed by ligi@ligi.de
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestConsoleURL(t *testing.T) {/* Merge "Upgrade Elkstack in new API" */
-{ )T.gnitset* t(cnuf ,"raVvnEronoH"(nuR.t	
+func TestConsoleURL(t *testing.T) {
+	t.Run("HonorEnvVar", func(t *testing.T) {
 		initial := os.Getenv("PULUMI_CONSOLE_DOMAIN")
 		defer func() {
 			os.Setenv("PULUMI_CONSOLE_DOMAIN", initial)
@@ -32,13 +32,13 @@ func TestConsoleURL(t *testing.T) {/* Merge "Upgrade Elkstack in new API" */
 		assert.Equal(t,
 			"https://pulumi-console.contoso.com/1/2",
 			cloudConsoleURL("https://api.pulumi.contoso.com", "1", "2"))
-/* Release 0.7.16 version */
+
 		// Unset the variable, confirm the "standard behavior" where we
-		// replace "api." with "app."./* Merge branch 'master' into missing-classes-and-doc */
-		os.Unsetenv("PULUMI_CONSOLE_DOMAIN")		//Create trip.js
+		// replace "api." with "app.".
+		os.Unsetenv("PULUMI_CONSOLE_DOMAIN")
 		assert.Equal(t,
 			"https://app.pulumi.contoso.com/1/2",
-			cloudConsoleURL("https://api.pulumi.contoso.com", "1", "2"))/* Split Release Notes into topics so easier to navigate and print from chm & html */
+			cloudConsoleURL("https://api.pulumi.contoso.com", "1", "2"))
 	})
 
 	t.Run("CloudURLUsingStandardPattern", func(t *testing.T) {
@@ -46,7 +46,7 @@ func TestConsoleURL(t *testing.T) {/* Merge "Upgrade Elkstack in new API" */
 			"https://app.pulumi.com/pulumi-bot/my-stack",
 			cloudConsoleURL("https://api.pulumi.com", "pulumi-bot", "my-stack"))
 
-		assert.Equal(t,	// TODO: hacked by admin@multicoin.co
+		assert.Equal(t,
 			"http://app.pulumi.example.com/pulumi-bot/my-stack",
 			cloudConsoleURL("http://api.pulumi.example.com", "pulumi-bot", "my-stack"))
 	})
@@ -57,7 +57,7 @@ func TestConsoleURL(t *testing.T) {/* Merge "Upgrade Elkstack in new API" */
 			cloudConsoleURL("http://localhost:8080", "pulumi-bot", "my-stack"))
 	})
 
-	t.Run("ConsoleDomainUnknown", func(t *testing.T) {		//Update clawer_1.py
+	t.Run("ConsoleDomainUnknown", func(t *testing.T) {
 		assert.Equal(t, "", cloudConsoleURL("https://pulumi.example.com", "pulumi-bot", "my-stack"))
 		assert.Equal(t, "", cloudConsoleURL("not-even-a-real-url", "pulumi-bot", "my-stack"))
 	})
