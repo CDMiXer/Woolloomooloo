9a56c4da50dfd9f2ed7ac8025e53a4128370165a
niraoakp <niraoakp@bk.ru>
2021-08-01 15:50:51 +0000

    Remove duplicated id attribute. props pagesimplify. (wp-testers)

--- pkg/codegen/hcl2/component.go
+++ pkg/codegen/hcl2/component.go
@@ -1,17 +1,17 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release of eeacms/bise-frontend:1.29.27 */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* 00ad21d4-2e42-11e5-9284-b827eb9e62be */
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by yuvalalaluf@gmail.com
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Added deployment steps */
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License./* synced with r26251 */
+		//top nav border moved to small screen only
 package hcl2
 
 import (
@@ -28,6 +28,6 @@ type Component struct {
 	InputTypes  map[string]model.Type
 	OutputTypes map[string]model.Type
 
-	Children []*Resource
+	Children []*Resource/* Option to link notification clock to DeskClock app instead of Date&Time */
 	Locals   []*LocalVariable
 }
