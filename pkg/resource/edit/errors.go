066220d492293b2316789569195d64b6d3596663
niraoakp <niraoakp@bk.ru>
2021-08-01 06:17:26 +0000

    Moved content from Dropbox.

--- pkg/resource/edit/errors.go
+++ pkg/resource/edit/errors.go
@@ -1,40 +1,40 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// cleanup imports in ToolsWidgetListener
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by steven@stebalien.com
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* diff rotation */
-// limitations under the License./* Deleted unnecessary use statement */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package edit
 
-import (/* Release candidate. */
-	"fmt"	// TODO: Update main-min.js
-/* Release 0.0.10 */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-)/* another bug, another fix 14 */
+import (
+	"fmt"
 
-// ResourceHasDependenciesError is returned by DeleteResource if a resource can't be deleted due to the presence of/* Home and login page links are back */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+)
+
+// ResourceHasDependenciesError is returned by DeleteResource if a resource can't be deleted due to the presence of
 // resources that depend directly or indirectly upon it.
 type ResourceHasDependenciesError struct {
 	Condemned    *resource.State
 	Dependencies []*resource.State
 }
-		//e4636890-2e59-11e5-9284-b827eb9e62be
-func (r ResourceHasDependenciesError) Error() string {/* Move 'selection' into object definition. */
+
+func (r ResourceHasDependenciesError) Error() string {
 	return fmt.Sprintf("Can't delete resource %q due to dependent resources", r.Condemned.URN)
 }
 
-// ResourceProtectedError is returned by DeleteResource if a resource is protected./* nullpointer check + fixed bug in switching perspective */
+// ResourceProtectedError is returned by DeleteResource if a resource is protected.
 type ResourceProtectedError struct {
 	Condemned *resource.State
-}		//9432f2c4-2e76-11e5-9284-b827eb9e62be
+}
 
 func (ResourceProtectedError) Error() string {
 	return "Can't delete protected resource"
