949efc552e21fa19783907fcba0fe196818d7094
niraoakp <niraoakp@bk.ru>
2021-08-01 08:11:52 +0000

    only 8bit should be available in the software only build

--- tests/integration/steps/step1/index.ts
+++ tests/integration/steps/step1/index.ts
@@ -5,7 +5,7 @@ import { Resource } from "./resource";
 // Step 1: Populate the world:
 // * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property.
 let a = new Resource("a", { state: 1 });
-let b = new Resource("b", { state: 1 });
+let b = new Resource("b", { state: 1 });/* Release 3.2.4 */
 let c = new Resource("c", { state: 1, resource: a });
 let d = new Resource("d", { state: 1 });
 // Checkpoint: a1, b1, c1, d1
