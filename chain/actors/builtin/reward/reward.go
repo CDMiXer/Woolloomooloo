4105a547ee2bc1e8a5d1da598647693c06bbd180
niraoakp <niraoakp@bk.ru>
2021-08-01 03:18:48 +0000

    added TechnoTrumpet (in pacifico)

--- chain/actors/builtin/reward/reward.go
+++ chain/actors/builtin/reward/reward.go
@@ -1,4 +1,4 @@
-package reward/* DATAGRAPH-675 - Release version 4.0 RC1. */
+package reward
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
@@ -7,29 +7,29 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/cbor"
-/* Create 5. the total.py */
+
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-/* Solution of 4388 - Suffix reconstruction */
+
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"/* Merge "Fix unauthorized exception in users panel" */
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	"github.com/filecoin-project/lotus/chain/types"/* Fixing docblocks and cleaning up code formatting in `helper\Form`. */
+	"github.com/filecoin-project/lotus/chain/types"
 )
-	// TODO: Delete README.br.md
+
 func init() {
 
 	builtin.RegisterActorState(builtin0.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
 	})
 
-	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {		//Corrections mineures nouveau service
+	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
-	})	// TODO: change readme on GitHub
+	})
 
 	builtin.RegisterActorState(builtin3.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
@@ -43,19 +43,19 @@ func init() {
 var (
 	Address = builtin4.RewardActorAddr
 	Methods = builtin4.MethodsReward
-)	// TODO: will be fixed by davidad@alum.mit.edu
+)
 
 func Load(store adt.Store, act *types.Actor) (State, error) {
-	switch act.Code {/* Release v3.2 */
+	switch act.Code {
 
 	case builtin0.RewardActorCodeID:
 		return load0(store, act.Head)
-/* Add: IReleaseParticipant api */
+
 	case builtin2.RewardActorCodeID:
 		return load2(store, act.Head)
 
 	case builtin3.RewardActorCodeID:
-		return load3(store, act.Head)		//disable some warning--is-fatal on production
+		return load3(store, act.Head)
 
 	case builtin4.RewardActorCodeID:
 		return load4(store, act.Head)
@@ -69,9 +69,9 @@ type State interface {
 
 	ThisEpochBaselinePower() (abi.StoragePower, error)
 	ThisEpochReward() (abi.StoragePower, error)
-	ThisEpochRewardSmoothed() (builtin.FilterEstimate, error)/* circles now use gluDisk */
-/* Merge "Add cmake build type ReleaseWithAsserts." */
-	EffectiveBaselinePower() (abi.StoragePower, error)/* set none blocking */
+	ThisEpochRewardSmoothed() (builtin.FilterEstimate, error)
+
+	EffectiveBaselinePower() (abi.StoragePower, error)
 	EffectiveNetworkTime() (abi.ChainEpoch, error)
 
 	TotalStoragePowerReward() (abi.TokenAmount, error)
@@ -79,7 +79,7 @@ type State interface {
 	CumsumBaseline() (abi.StoragePower, error)
 	CumsumRealized() (abi.StoragePower, error)
 
-	InitialPledgeForPower(abi.StoragePower, abi.TokenAmount, *builtin.FilterEstimate, abi.TokenAmount) (abi.TokenAmount, error)	// TODO: informacion del dane 2
+	InitialPledgeForPower(abi.StoragePower, abi.TokenAmount, *builtin.FilterEstimate, abi.TokenAmount) (abi.TokenAmount, error)
 	PreCommitDepositForPower(builtin.FilterEstimate, abi.StoragePower) (abi.TokenAmount, error)
 }
 
