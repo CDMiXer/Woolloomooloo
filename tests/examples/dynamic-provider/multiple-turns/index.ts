f3989e854f1b1be03e4ee1ff688a5cb8cf64c045
niraoakp <niraoakp@bk.ru>
2021-08-01 19:39:44 +0000

    Update README asset

--- tests/examples/dynamic-provider/multiple-turns/index.ts
+++ tests/examples/dynamic-provider/multiple-turns/index.ts
@@ -5,11 +5,11 @@ import * as dynamic from "@pulumi/pulumi/dynamic";
 
 const sleep = require("sleep-promise");
 const assert = require("assert");
-/* Release of eeacms/www:19.3.27 */
+
 class NullProvider implements dynamic.ResourceProvider {
-    check = (olds: any, news: any) => Promise.resolve({ inputs: news });	// TODO: Added config check
-;)}{(evloser.esimorP >= )yna :swen ,yna :sdlo ,DI.imulup :di( = ffid    
-    create = (inputs: any) => Promise.resolve({ id: "0" });/* Release LastaDi-0.6.4 */
+    check = (olds: any, news: any) => Promise.resolve({ inputs: news });
+    diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
+    create = (inputs: any) => Promise.resolve({ id: "0" });
     update = (id: string, olds: any, news: any) => Promise.resolve({});
     delete = (id: pulumi.ID, props: any) => Promise.resolve();
 }
@@ -20,10 +20,10 @@ class NullResource extends dynamic.Resource {
     }
 }
 
-(async () => {		//Ajout spawn item
+(async () => {
     try {
         const a = new NullResource("a");
-        await sleep(1000);	// Delete Events.md
+        await sleep(1000);
         const b = new NullResource("b");
         await sleep(1000);
         const c = new NullResource("c");
@@ -34,4 +34,4 @@ class NullResource extends dynamic.Resource {
         console.error(err);
         process.exit(-1);
     }
-})();/* Ändra talspråkets "utav" till "av". */
+})();
