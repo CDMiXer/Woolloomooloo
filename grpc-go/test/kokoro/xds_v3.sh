37cba994e4c9cf2f4b3c76553ddbeebf95689ec8
niraoakp <niraoakp@bk.ru>
2021-08-01 11:45:30 +0000

    added dashboard implementation of new UI more to display fields on one map

--- grpc-go/test/kokoro/xds_v3.sh
+++ grpc-go/test/kokoro/xds_v3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/* Update to Jedi Archives Windows 7 Release 5-25 */
-XDS_V3_OPT="--xds_v3_support" `dirname $0`/xds.sh		//Rename (2)
+
+XDS_V3_OPT="--xds_v3_support" `dirname $0`/xds.sh/* Always wrap number with Number */
