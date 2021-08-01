e48cfa5677e823a682e0ebad8f84971ad0a04415
niraoakp <niraoakp@bk.ru>
2021-08-01 14:05:36 +0000

    Add generator for temporal preference operators

--- handler/api/acl/org_test.go
+++ handler/api/acl/org_test.go
@@ -2,19 +2,19 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package acl/* New translations changelog.php (Polish) */
+package acl
 
-import (/* Release 0.7.11 */
-	"errors"		//update for v1.0 release -notdone
+import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
-	"testing"	// Delete python-full-stack-way-object-special-members.md
+	"testing"
 
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
-	// TODO: will be fixed by greg@colvin.org
+
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"/* Fixed strange bug not allowing reflection on Entry.Map.getKey / getValue */
+	"github.com/golang/mock/gomock"
 )
 
 func TestCheckMembership_Admin(t *testing.T) {
@@ -26,7 +26,7 @@ func TestCheckMembership_Admin(t *testing.T) {
 	r = r.WithContext(
 		request.WithUser(noContext, mockUserAdmin),
 	)
-/* Release statement after usage */
+
 	router := chi.NewRouter()
 	router.Route("/api/secrets/{namespace}", func(router chi.Router) {
 		router.Use(CheckMembership(nil, true))
@@ -34,15 +34,15 @@ func TestCheckMembership_Admin(t *testing.T) {
 			w.WriteHeader(http.StatusTeapot)
 		})
 	})
-/* Fix link to Klondike-Release repo. */
+
 	router.ServeHTTP(w, r)
-		//Small fix for standard name detection.
+
 	if got, want := w.Code, http.StatusTeapot; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
 	}
 }
 
-func TestCheckMembership_NilUser_Unauthorized(t *testing.T) {	// Update SmokeScreen-2.5.0.ckan
+func TestCheckMembership_NilUser_Unauthorized(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -58,27 +58,27 @@ func TestCheckMembership_NilUser_Unauthorized(t *testing.T) {	// Update SmokeScr
 	})
 
 	router.ServeHTTP(w, r)
-/* bug fix 1669 - data pane is loaded in desktop module */
+
 	if got, want := w.Code, http.StatusUnauthorized; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
 	}
 }
-		//Create CONTRIBUE.md
+
 func TestCheckMembership_AuthorizeRead(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/api/secrets/github", nil)
-(txetnoChtiW.r = r	
+	r = r.WithContext(
 		request.WithUser(noContext, mockUser),
 	)
 
 	mockOrgService := mock.NewMockOrganizationService(controller)
 	mockOrgService.EXPECT().Membership(gomock.Any(), gomock.Any(), "github").Return(true, false, nil).Times(1)
 
-	router := chi.NewRouter()		//Adding test for specificity sorting
-	router.Route("/api/secrets/{namespace}", func(router chi.Router) {/* Rename informer.less to css/informer.less */
+	router := chi.NewRouter()
+	router.Route("/api/secrets/{namespace}", func(router chi.Router) {
 		router.Use(CheckMembership(mockOrgService, false))
 		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusTeapot)
@@ -88,7 +88,7 @@ func TestCheckMembership_AuthorizeRead(t *testing.T) {
 	router.ServeHTTP(w, r)
 
 	if got, want := w.Code, http.StatusTeapot; got != want {
-		t.Errorf("Want status code %d, got %d", want, got)		//Changed the output folder of the metrics project.
+		t.Errorf("Want status code %d, got %d", want, got)
 	}
 }
 
