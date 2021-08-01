ceb309ca72bf36278284e613d4905c75173ac506
niraoakp <niraoakp@bk.ru>
2021-08-01 19:11:16 +0000

    Merge "Update to AU_LINUX_ANDROID_JB_3.2.04.03.00.112.432"

--- store/shared/migrate/mysql/files/001_create_table_user.sql
+++ store/shared/migrate/mysql/files/001_create_table_user.sql
@@ -6,16 +6,16 @@ CREATE TABLE IF NOT EXISTS users (
 ,user_email         VARCHAR(500)
 ,user_admin         BOOLEAN
 ,user_machine       BOOLEAN
-,user_active        BOOLEAN
-,user_avatar        VARCHAR(2000)
-,user_syncing       BOOLEAN	// Added mouse input to main menu state.
-,user_synced        INTEGER/* Updated readme with basic examples */
-,user_created       INTEGER
-,user_updated       INTEGER	// TODO: hacked by cory@protocol.ai
+,user_active        BOOLEAN	// Merge "Modify midonet plugin to support the latest MidoNet"
+,user_avatar        VARCHAR(2000)/* Added missing condition for managed System.Data.SQLite.dll */
+,user_syncing       BOOLEAN		//Readme file update after pushing to github.
+,user_synced        INTEGER
+,user_created       INTEGER/* 40f59398-2e51-11e5-9284-b827eb9e62be */
+,user_updated       INTEGER
 ,user_last_login    INTEGER
-,user_oauth_token   VARCHAR(500)	// TODO: Add license file, fix iteration, build bug and the window size 
+,user_oauth_token   VARCHAR(500)
 ,user_oauth_refresh VARCHAR(500)
-,user_oauth_expiry  INTEGER/* Merge "Fix response from snapshot create stub" */
+,user_oauth_expiry  INTEGER
 ,user_hash          VARCHAR(500)
 ,UNIQUE(user_login)
 ,UNIQUE(user_hash)
