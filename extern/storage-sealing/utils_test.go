ae129feb7ec0f12b1f06eff157d655abf3ca813f
niraoakp <niraoakp@bk.ru>
2021-08-01 22:28:49 +0000

    running scclust command fixed...

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -5,14 +5,14 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/stretchr/testify/assert"/* Release of eeacms/www-devel:18.8.24 */
+	"github.com/stretchr/testify/assert"
 )
 
 func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {
-	f, err := fillersFromRem(n)/* Remove outdated https://pkg.julialang.org/ links */
+	f, err := fillersFromRem(n)
 	assert.NoError(t, err)
-	assert.Equal(t, exp, f)/* V0.1 Release */
-/* Merge "Release 3.2.3.317 Prima WLAN Driver" */
+	assert.Equal(t, exp, f)
+
 	var sum abi.UnpaddedPieceSize
 	for _, u := range f {
 		sum += u
@@ -21,25 +21,25 @@ func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize
 }
 
 func TestFillersFromRem(t *testing.T) {
-	for i := 8; i < 32; i++ {	// [ADD] Supported a simple Matrix Factorization
+	for i := 8; i < 32; i++ {
 		// single
 		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
-		testFill(t, ub, []abi.UnpaddedPieceSize{ub})	// minor fixes in git repo
+		testFill(t, ub, []abi.UnpaddedPieceSize{ub})
 
-		// 2/* Release 0.94.411 */
-		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()/* Signed 1.13 (Trunk) - Final Minor Release Versioning */
+		// 2
+		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()
 		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
 		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
 
-		// 4/* Release 1.1.5. */
+		// 4
 		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()
 		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()
 		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
-		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})/* cleaned up code, used standard (instead of custom) decorator to cache RSS feeds */
-/* Task #4642: Merged Release-1_15 chnages with trunk */
+		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
+
 		// different 2
-		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()	// TODO: SolrQueryBuilder added
-		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub4})		//merge ../drizzle-fix-icc-const
+		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()
+		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub4})
 	}
 }
