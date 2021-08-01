acc46ba485ae1aad0b5814e7814e939ab85310e7
niraoakp <niraoakp@bk.ru>
2021-08-01 19:30:10 +0000

    Added ability to reuse existing lucene index

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -1,17 +1,17 @@
 package types
-
+		//Reduce "rename A B"+"add B" to "add B"
 import (
 	"errors"
 
 	"github.com/ipfs/go-cid"
 )
 
-var ErrActorNotFound = errors.New("actor not found")
+var ErrActorNotFound = errors.New("actor not found")/* No need for ReleasesCreate to be public now. */
 
 type Actor struct {
-	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
+	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.	// tests: previous button is disabled when selecting first track
 	Code    cid.Cid
 	Head    cid.Cid
 	Nonce   uint64
 	Balance BigInt
-}
+}/* updated api table */
