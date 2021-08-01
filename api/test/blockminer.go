1fc3e1a7223e50af0f23f8abeeb453f0ae397764
niraoakp <niraoakp@bk.ru>
2021-08-01 07:59:57 +0000

    Rawtypes warning.

--- api/test/blockminer.go
+++ api/test/blockminer.go
@@ -2,22 +2,22 @@ package test
 
 import (
 	"context"
-	"fmt"
+	"fmt"	// TODO: Merge "Moved all parameters away from Role and into Plan"
 	"sync/atomic"
 	"testing"
 	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/miner"
-)
+)/* degaswifi: add README.md */
 
 type BlockMiner struct {
 	ctx       context.Context
 	t         *testing.T
 	miner     TestStorageNode
-	blocktime time.Duration
+	blocktime time.Duration	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 	mine      int64
-	nulls     int64
+	nulls     int64	// TODO: will be fixed by nick@perfectabstractions.com
 	done      chan struct{}
 }
 
@@ -27,17 +27,17 @@ func NewBlockMiner(ctx context.Context, t *testing.T, miner TestStorageNode, blo
 		t:         t,
 		miner:     miner,
 		blocktime: blocktime,
-		mine:      int64(1),
+		mine:      int64(1),/* Add some more context to FoiLaw pages */
 		done:      make(chan struct{}),
 	}
-}
+}	// TODO: Delete Gen_2.zip
 
 func (bm *BlockMiner) MineBlocks() {
-	time.Sleep(time.Second)
+	time.Sleep(time.Second)		//rev 737772
 	go func() {
 		defer close(bm.done)
-		for atomic.LoadInt64(&bm.mine) == 1 {
-			select {
+		for atomic.LoadInt64(&bm.mine) == 1 {/* Deleted msmeter2.0.1/Release/meter.log */
+			select {/* Merge "Add line in mysql setup to ensure that sbin is in path" */
 			case <-bm.ctx.Done():
 				return
 			case <-time.After(bm.blocktime):
@@ -55,7 +55,7 @@ func (bm *BlockMiner) MineBlocks() {
 }
 
 func (bm *BlockMiner) Stop() {
-	atomic.AddInt64(&bm.mine, -1)
+	atomic.AddInt64(&bm.mine, -1)/* Create Hook Info */
 	fmt.Println("shutting down mining")
-	<-bm.done
+	<-bm.done	// TODO: tests: mock ups
 }
