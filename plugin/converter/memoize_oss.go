546dc7aab6353d316f54493391210e29dcb5da20
niraoakp <niraoakp@bk.ru>
2021-08-01 23:14:35 +0000

    Update README.md: adding link to docs.forj.io

--- plugin/converter/memoize_oss.go
+++ plugin/converter/memoize_oss.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone IO, Inc./* added FAQ section to README. Using latest APIs for GetLock and ReleaseLock */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by magik6k@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Imported Upstream version 7.5
-// Unless required by applicable law or agreed to in writing, software/* Added further unit tests for ReleaseUtil */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//LensFlarePlugin: Clean up and only create program when needed.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -21,7 +21,7 @@ import (
 )
 
 // Memoize caches the conversion results for subsequent calls.
-// This micro-optimization is intended for multi-pipeline/* Release 3. */
+// This micro-optimization is intended for multi-pipeline
 // projects that would otherwise covert the file for each
 // pipeline execution.
 func Memoize(base core.ConvertService) core.ConvertService {
