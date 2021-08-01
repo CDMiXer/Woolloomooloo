2ca54f375525d369790592aa2c50250fc81a47e8
niraoakp <niraoakp@bk.ru>
2021-08-01 06:01:24 +0000

    Update umbrella header for new content loading protocols

--- handler/api/repos/builds/find_test.go
+++ handler/api/repos/builds/find_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* adapted DataManager tests */
 package builds
 
 import (
@@ -17,15 +17,15 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-
+		//Добавил ID потока
 func TestFind(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)	// TODO: Vorgängerversion reaktiviert
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
 
-	builds := mock.NewMockBuildStore(controller)
+	builds := mock.NewMockBuildStore(controller)/* 78ae8648-4b19-11e5-96bf-6c40088e03e4 */
 	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuild, nil)
 
 	stages := mock.NewMockStageStore(controller)
@@ -34,8 +34,8 @@ func TestFind(t *testing.T) {
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("number", "1")
-
+	c.URLParams.Add("number", "1")/* Create Release directory */
+	// made int argc const
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
@@ -43,9 +43,9 @@ func TestFind(t *testing.T) {
 	)
 
 	HandleFind(repos, builds, stages)(w, r)
-
+		//Delete 404.01100.js
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)/* Update Rome_2.txt */
 	}
 
 	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}
@@ -55,38 +55,38 @@ func TestFind(t *testing.T) {
 	}
 }
 
-func TestFind_BadRequest(t *testing.T) {
+func TestFind_BadRequest(t *testing.T) {/* Update AuthOnlyView.js */
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")/* Added cardtype */
 	c.URLParams.Add("number", "one")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
+	r = r.WithContext(/* ed0c37fc-2e62-11e5-9284-b827eb9e62be */
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// TODO: will be fixed by souzau@yandex.com
+	)		//add missing kernel config symbols
 
 	HandleFind(nil, nil, nil)(w, r)
 
 	if got, want := w.Code, 400; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+	// TODO: hacked by aeongrp@outlook.com
 	got, want := new(errors.Error), &errors.Error{Message: "strconv.ParseInt: parsing \"one\": invalid syntax"}
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
-
+	// Change in twitter feed template.
 func TestFind_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)
-
+		//collect decompilation performance statistic data
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
