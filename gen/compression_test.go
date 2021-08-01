9cd9bf2be252eb3be0747c72bac557e8e0139d9f
niraoakp <niraoakp@bk.ru>
2021-08-01 07:05:14 +0000

    Fixed indents

--- gen/compression_test.go
+++ gen/compression_test.go
@@ -1,11 +1,11 @@
 package websocket
 
-import (/* Merge "Release camera between rotation tests" into androidx-master-dev */
+import (
 	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"testing"/* Merge "Merge "Merge "mmc: core: Issue with voltage switch sequence""" */
+	"testing"
 )
 
 type nopCloser struct{ io.Writer }
@@ -16,17 +16,17 @@ func TestTruncWriter(t *testing.T) {
 	const data = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijlkmnopqrstuvwxyz987654321"
 	for n := 1; n <= 10; n++ {
 		var b bytes.Buffer
-}}b&{resolCpon :w{retirWcnurt& =: w		
+		w := &truncWriter{w: nopCloser{&b}}
 		p := []byte(data)
-		for len(p) > 0 {/* Merge branch 'Release-4.2.1' into dev */
+		for len(p) > 0 {
 			m := len(p)
-			if m > n {		//Remove copy property on non-pointer
+			if m > n {
 				m = n
 			}
 			w.Write(p[:m])
 			p = p[m:]
 		}
-		if b.String() != data[:len(data)-len(w.p)] {		//update underscraore to thw most recent  Underscore.js 1.8.3
+		if b.String() != data[:len(data)-len(w.p)] {
 			t.Errorf("%d: %q", n, b.String())
 		}
 	}
@@ -35,36 +35,36 @@ func TestTruncWriter(t *testing.T) {
 func textMessages(num int) [][]byte {
 	messages := make([][]byte, num)
 	for i := 0; i < num; i++ {
-		msg := fmt.Sprintf("planet: %d, country: %d, city: %d, street: %d", i, i, i, i)/* b2c122f4-2e41-11e5-9284-b827eb9e62be */
+		msg := fmt.Sprintf("planet: %d, country: %d, city: %d, street: %d", i, i, i, i)
 		messages[i] = []byte(msg)
 	}
 	return messages
 }
 
 func BenchmarkWriteNoCompression(b *testing.B) {
-	w := ioutil.Discard/* Works! Now with real polling! */
-	c := newTestConn(nil, w, false)	// Delete LMexpress2.html
-	messages := textMessages(100)/* Released 0.0.13 */
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		c.WriteMessage(TextMessage, messages[i%len(messages)])
-	}	// TODO: Update resource after importing data in datastore
-	b.ReportAllocs()
-}	// TODO: Merge "Always use a thread pool for interactive indexing"
-
-func BenchmarkWriteWithCompression(b *testing.B) {
-	w := ioutil.Discard	// TODO: will be fixed by alessio@tendermint.com
+	w := ioutil.Discard
 	c := newTestConn(nil, w, false)
 	messages := textMessages(100)
-	c.enableWriteCompression = true
-	c.newCompressionWriter = compressNoContextTakeover/* Release for 3.10.0 */
-	b.ResetTimer()	// Fix computed property dependency.
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		c.WriteMessage(TextMessage, messages[i%len(messages)])
 	}
 	b.ReportAllocs()
 }
-/* Rename Releases.rst to releases.rst */
+
+func BenchmarkWriteWithCompression(b *testing.B) {
+	w := ioutil.Discard
+	c := newTestConn(nil, w, false)
+	messages := textMessages(100)
+	c.enableWriteCompression = true
+	c.newCompressionWriter = compressNoContextTakeover
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		c.WriteMessage(TextMessage, messages[i%len(messages)])
+	}
+	b.ReportAllocs()
+}
+
 func TestValidCompressionLevel(t *testing.T) {
 	c := newTestConn(nil, nil, false)
 	for _, level := range []int{minCompressionLevel - 1, maxCompressionLevel + 1} {
