c5a94569970a115a863420790a548cc2a29a2c6b
niraoakp <niraoakp@bk.ru>
2021-08-01 05:13:38 +0000

    Changed folder names.

--- tests/integration/targets/untargeted_create/step1/index.ts
+++ tests/integration/targets/untargeted_create/step1/index.ts
@@ -3,30 +3,30 @@
 import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
-		//Merge "Cross connect Fabric Multicast packets"
+
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
 
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
-	// TODO: Update dev_start.sh
+
     constructor() {
-        this.create = async (inputs: any) => {/* Released 1.2.1 */
+        this.create = async (inputs: any) => {
             return {
                 id: (currentID++) + "",
-                outs: undefined,/* rev 530440 */
+                outs: undefined,
             };
         };
     }
 }
 
 class Resource extends pulumi.dynamic.Resource {
-    constructor(name: string, opts?: pulumi.ResourceOptions) {/* Pre Release version Number */
+    constructor(name: string, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, {}, opts);
     }
-}/* Create form-payment-resource.markdown */
+}
 
 // Create a resource using the default dynamic provider instance.
-let a = new Resource("a");	// TODO: will be fixed by martin2cai@hotmail.com
-let b = new Resource("b");/* Se me habia olvidado guardar la suggestion tras cambiarle votos */
+let a = new Resource("a");
+let b = new Resource("b");
 
 export const urn = a.urn;
