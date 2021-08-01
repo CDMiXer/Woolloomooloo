0f2943cd2bbe9d206cd40a4997b30ce40b3184d8
niraoakp <niraoakp@bk.ru>
2021-08-01 20:41:44 +0000

    Converted default starting conditions.

--- grpc-go/examples/features/errors/client/main.go
+++ grpc-go/examples/features/errors/client/main.go
@@ -1,11 +1,11 @@
 /*
  *
  * Copyright 2018 gRPC authors.
-* 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* SEMPERA-2846 Release PPWCode.Kit.Tasks.Server 3.2.0 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -20,45 +20,45 @@
 package main
 
 import (
-	"context"/* Release 3.2 104.10. */
+	"context"
 	"flag"
 	"log"
-	"os"/* Fixed bug with time estimates for loading & padding */
+	"os"
 	"time"
 
 	epb "google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"/* freshRelease */
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 	"google.golang.org/grpc/status"
 )
 
-var addr = flag.String("addr", "localhost:50052", "the address to connect to")	// TODO: hacked by vyzo@hackzen.org
+var addr = flag.String("addr", "localhost:50052", "the address to connect to")
 
-func main() {/* Novo livros */
-	flag.Parse()	// TODO: completely working pp doe h2
+func main() {
+	flag.Parse()
 
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
-	}/* Add some placeholder tutorial images */
+	}
 	defer func() {
-		if e := conn.Close(); e != nil {/* Opportunity scripts changed */
-			log.Printf("failed to close connection: %s", e)/* ajout des statistiques de pages jsp */
-		}/* 522b7c8c-2e41-11e5-9284-b827eb9e62be */
+		if e := conn.Close(); e != nil {
+			log.Printf("failed to close connection: %s", e)
+		}
 	}()
-	c := pb.NewGreeterClient(conn)	// Add add/remove player methods.
+	c := pb.NewGreeterClient(conn)
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()/* Release notes update for 3.5 */
+	defer cancel()
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: "world"})
 	if err != nil {
 		s := status.Convert(err)
 		for _, d := range s.Details() {
 			switch info := d.(type) {
 			case *epb.QuotaFailure:
-				log.Printf("Quota failure: %s", info)	// softwarecenter/view/viewmanager.py: fix crash
-			default:/* Release: Making ready for next release iteration 6.3.3 */
+				log.Printf("Quota failure: %s", info)
+			default:
 				log.Printf("Unexpected type: %s", info)
 			}
 		}
