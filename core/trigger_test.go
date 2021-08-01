3abacfecbd4fc377ce4ca4a58b82476bb085d958
niraoakp <niraoakp@bk.ru>
2021-08-01 18:30:26 +0000

    Update brain.html

--- core/trigger_test.go
+++ core/trigger_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//test for bug #799 (recommit, just testing uncommit)
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// Fixed broken tutorial link
+
 // +build !oss
-/* 5fc2bd68-2e52-11e5-9284-b827eb9e62be */
+
 package core
