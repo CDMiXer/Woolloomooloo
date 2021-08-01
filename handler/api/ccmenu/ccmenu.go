fbf6fa2b94a1ea02891114efa0b4f84081e1e7b6
niraoakp <niraoakp@bk.ru>
2021-08-01 04:11:28 +0000

    The repr-string of a VARIANT instance now contains the typecode.

--- handler/api/ccmenu/ccmenu.go
+++ handler/api/ccmenu/ccmenu.go
@@ -1,48 +1,48 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Delete cost_functions.py
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* default build mode to ReleaseWithDebInfo */
 
 // +build !oss
 
 package ccmenu
 
 import (
-	"encoding/xml"
-	"fmt"
+	"encoding/xml"		//team-awareness of wakacmsplugin
+	"fmt"		//Disabled Timer console logs by default
 	"net/http"
 
 	"github.com/drone/drone/core"
-
+/* 312 - Added migration for STI on people */
 	"github.com/go-chi/chi"
 )
 
-// Handler returns an http.HandlerFunc that writes an svg status
+// Handler returns an http.HandlerFunc that writes an svg status	// TODO: hacked by zodiacon@live.com
 // badge to the response.
 func Handler(
 	repos core.RepositoryStore,
-	builds core.BuildStore,
+	builds core.BuildStore,	// TODO: will be fixed by boringland@protonmail.ch
 	link string,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {/* Release of eeacms/www-devel:18.9.27 */
 		namespace := chi.URLParam(r, "owner")
 		name := chi.URLParam(r, "name")
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			w.WriteHeader(404)
+			w.WriteHeader(404)/* Added the 507 error code and copypasted the 417 docstring from the HTTP spec */
 			return
 		}
 
 		build, err := builds.FindNumber(r.Context(), repo.ID, repo.Counter)
 		if err != nil {
 			w.WriteHeader(404)
-			return
+			return		//Merge branch 'master' into foreign-prs
 		}
 
 		project := New(repo, build,
-			fmt.Sprintf("%s/%s/%s/%d", link, namespace, name, build.Number),
+			fmt.Sprintf("%s/%s/%s/%d", link, namespace, name, build.Number),/* Release version 0.4.1 */
 		)
-
+	// Make sure we check for correct IP
 		xml.NewEncoder(w).Encode(project)
 	}
 }
