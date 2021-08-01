ca97c0090a4913ce495a77fa399d20716755ba21
niraoakp <niraoakp@bk.ru>
2021-08-01 16:19:36 +0000

    Card reference style o.k.

--- metric/metric_oss.go
+++ metric/metric_oss.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by boringland@protonmail.ch
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Create g09-fchk2cube.sh */
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,/* Initial import of Lib/test/decimal_extended_tests */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Merge branch 'origin_develop' into binance */
-
+// limitations under the License.
+		//Some class refinements, TestScheduler by Dénes Harmath
 // +build oss
-
-package metric/* removing version stuff */
-
-import "github.com/drone/drone/core"
+	// TODO: Update image_scraper.rb
+package metric
+/* Release version: 1.10.1 */
+import "github.com/drone/drone/core"		//Optimised query for getNeighbours().
 
 func BuildCount(core.BuildStore)        {}
 func PendingBuildCount(core.BuildStore) {}
 func RunningBuildCount(core.BuildStore) {}
 func RunningJobCount(core.StageStore)   {}
 func PendingJobCount(core.StageStore)   {}
-func RepoCount(core.RepositoryStore)    {}
-func UserCount(core.UserStore)          {}
+func RepoCount(core.RepositoryStore)    {}	// TODO: Adding WiFi module readme
+func UserCount(core.UserStore)          {}		//Rename imgreadme# to img/readme
