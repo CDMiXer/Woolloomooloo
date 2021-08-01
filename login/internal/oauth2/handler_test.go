765d17b99a4ce88b0c3d9fafffeb9eef4a3d93c2
niraoakp <niraoakp@bk.ru>
2021-08-01 03:44:40 +0000

    Fixed a TODO within a test that I happened to be looking at.

--- login/internal/oauth2/handler_test.go
+++ login/internal/oauth2/handler_test.go
@@ -1,5 +1,5 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.		//Inclusão da dependência do Weld SE
-// Use of this source code is governed by a BSD-style
+// Copyright 2017 Drone.IO Inc. All rights reserved.	// TODO: Create update-FruityWifi.sh
+// Use of this source code is governed by a BSD-style/* Merge "pyghmi: Move to singlespec" */
 // license that can be found in the LICENSE file.
-
+	// UOL: Domainkontrolle in Veranstaltungen deaktiviert
 package oauth2
