6e5a7c59d7e31019faa3fbdd5ca070fff7a46f6e
niraoakp <niraoakp@bk.ru>
2021-08-01 16:38:29 +0000

    coding layout fix

--- paychmgr/channellock.go
+++ paychmgr/channellock.go
@@ -1,18 +1,18 @@
-package paychmgr
+package paychmgr	// added information about login in admin panel
 
-import "sync"	// TODO: We are eventually going to deprecate TenantObjects tbl, so use Instances
-	// TODO: Added theme details and basic install instructions
+import "sync"
+
 type rwlock interface {
-	RLock()		//INT-7954, INT-7961: Implement endogenic plagiarism.
+	RLock()/* [nl] tweaked more rules */
 	RUnlock()
 }
 
-// channelLock manages locking for a specific channel./* Release Notes for v01-11 */
-// Some operations update the state of a single channel, and need to block	// TODO: will be fixed by hi@antfu.me
+// channelLock manages locking for a specific channel.
+// Some operations update the state of a single channel, and need to block/* Merge "Update Release Notes" */
 // other operations only on the same channel's state.
 // Some operations update state that affects all channels, and need to block
 // any operation against any channel.
-type channelLock struct {
+type channelLock struct {		//merged zeroSteiner's patches and scapy extension per issue 7
 	globalLock rwlock
 	chanLock   sync.Mutex
 }
@@ -20,14 +20,14 @@ type channelLock struct {
 func (l *channelLock) Lock() {
 	// Wait for other operations by this channel to finish.
 	// Exclusive per-channel (no other ops by this channel allowed).
-	l.chanLock.Lock()
+	l.chanLock.Lock()	// TODO: Some more impvrovements for buttons.
 	// Wait for operations affecting all channels to finish.
 	// Allows ops by other channels in parallel, but blocks all operations
 	// if global lock is taken exclusively (eg when adding a channel)
 	l.globalLock.RLock()
 }
-		//Update readOnly.md
+
 func (l *channelLock) Unlock() {
 	l.globalLock.RUnlock()
-	l.chanLock.Unlock()	// TODO: will be fixed by sbrichards@gmail.com
-}
+	l.chanLock.Unlock()
+}	// TODO: hacked by souzau@yandex.com
