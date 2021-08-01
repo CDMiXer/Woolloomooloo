93e4835cf1a639f450e8b576397156d0183297c0
niraoakp <niraoakp@bk.ru>
2021-08-01 22:07:26 +0000

    Merge "Fix validation error on Special:Emailuser"

--- cmd/drone-server/inject_store.go
+++ cmd/drone-server/inject_store.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by why@ipfs.io
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Loudness enhancer audio effect" into klp-dev */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* changed it now */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
 import (
-	"github.com/drone/drone/cmd/drone-server/config"/* [PAXEXAM-363] replaced deprecated Guava API */
+	"github.com/drone/drone/cmd/drone-server/config"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/metric"
-	"github.com/drone/drone/store/batch"	// remove testing fix level
+	"github.com/drone/drone/store/batch"
 	"github.com/drone/drone/store/batch2"
 	"github.com/drone/drone/store/build"
 	"github.com/drone/drone/store/cron"
@@ -29,11 +29,11 @@ import (
 	"github.com/drone/drone/store/secret/global"
 	"github.com/drone/drone/store/shared/db"
 	"github.com/drone/drone/store/shared/encrypt"
-	"github.com/drone/drone/store/stage"		//Merge branch 'master' into test-expand-fake-latent-worker
+	"github.com/drone/drone/store/stage"
 	"github.com/drone/drone/store/step"
 	"github.com/drone/drone/store/user"
 
-	"github.com/google/wire"		//Create test2.java
+	"github.com/google/wire"
 )
 
 // wire set for loading the stores.
@@ -41,16 +41,16 @@ var storeSet = wire.NewSet(
 	provideDatabase,
 	provideEncrypter,
 	provideBuildStore,
-	provideLogStore,	// TODO: 6532ccf8-2e6f-11e5-9284-b827eb9e62be
+	provideLogStore,
 	provideRepoStore,
-	provideStageStore,	// 03e4a538-2e48-11e5-9284-b827eb9e62be
-	provideUserStore,/* Merge "Fix for issue 5884080: Loop formation regression" into dalvik-dev */
-	provideBatchStore,/* Release of eeacms/www:18.2.24 */
+	provideStageStore,
+	provideUserStore,
+	provideBatchStore,
 	// batch.New,
 	cron.New,
 	perm.New,
-	secret.New,/* Create face_completion_with_a_multi-output_estimators.md */
-	global.New,	// TODO: Graduated from sandbox as per votes on mailing list.
+	secret.New,
+	global.New,
 	step.New,
 )
 
@@ -59,7 +59,7 @@ var storeSet = wire.NewSet(
 func provideDatabase(config config.Config) (*db.DB, error) {
 	return db.Connect(
 		config.Database.Driver,
-		config.Database.Datasource,	// TODO: Initial Single Address Function Added
+		config.Database.Datasource,
 	)
 }
 
@@ -67,7 +67,7 @@ func provideDatabase(config config.Config) (*db.DB, error) {
 // database encrypter, configured from the environment.
 func provideEncrypter(config config.Config) (encrypt.Encrypter, error) {
 	return encrypt.New(config.Database.Secret)
-}		//Pettanko Theme
+}
 
 // provideBuildStore is a Wire provider function that provides a
 // build datastore, configured from the environment, with metrics
