16a482d9ddfc41e88137ff12c49d7bacd0794056
niraoakp <niraoakp@bk.ru>
2021-08-01 18:20:58 +0000

    New translations friends.php (Polish)

--- tests/integration/query/step2/index.ts
+++ tests/integration/query/step2/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";/* Merge "Wlan:  Release 3.8.20.23" */
+import { Resource } from "./resource";
 
-// Step 2: Create resources during `pulumi query` -- should error.
+// Step 2: Create resources during `pulumi query` -- should error.		//Give the styles more weight. Fixes #18.
 const b = new Resource("b", { state: 2 });
 const a = new Resource("a", { state: 1, resource: b });
