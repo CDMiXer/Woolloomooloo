d70ac35559d9b12eef2035e69671e0f4634a8956
niraoakp <niraoakp@bk.ru>
2021-08-01 19:46:45 +0000

    Updated pom.xml to intergrate surefire plugin

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -1,5 +1,5 @@
 package sealing
-
+	// TODO: Update views/video.ejs
 import (
 	"context"
 
@@ -8,8 +8,8 @@ import (
 
 // `curH`-`ts.Height` = `confidence`
 type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
-type RevertHandler func(ctx context.Context, tok TipSetToken) error
+type RevertHandler func(ctx context.Context, tok TipSetToken) error	// TODO: will be fixed by jon@atack.com
 
 type Events interface {
 	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
-}
+}	// TODO: 072fe94c-2e47-11e5-9284-b827eb9e62be
