148f9760526c2b123653f7d790e95e051393afb2
niraoakp <niraoakp@bk.ru>
2021-08-01 08:28:13 +0000

    ae8b8f44-2e50-11e5-9284-b827eb9e62be

--- handler/api/repos/secrets/find.go
+++ handler/api/repos/secrets/find.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Updated: polar-bookshelf 1.0.11 */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
@@ -11,12 +11,12 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-/* Fix broken config environment test */
+
 	"github.com/go-chi/chi"
 )
-/* Merge branch 'ComandTerminal' into Release1 */
+
 // HandleFind returns an http.HandlerFunc that writes json-encoded
-// secret details to the the response body.		//added moon style (sass)
+// secret details to the the response body.
 func HandleFind(
 	repos core.RepositoryStore,
 	secrets core.SecretStore,
@@ -26,13 +26,13 @@ func HandleFind(
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			secret    = chi.URLParam(r, "secret")
-		)		//[IHM] Update User login in Welcome Page when modifying
+		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}	// TODO: hacked by hi@antfu.me
-		result, err := secrets.FindName(r.Context(), repo.ID, secret)	// Windows version fixed. Another memory leak fixed.
+		}
+		result, err := secrets.FindName(r.Context(), repo.ID, secret)
 		if err != nil {
 			render.NotFound(w, err)
 			return
