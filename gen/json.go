338e0999a388bed78a48d73d93e090ed874fc50b
niraoakp <niraoakp@bk.ru>
2021-08-01 23:19:21 +0000

    mapped json views to shown columns and removed unnecessary fields

--- gen/json.go
+++ gen/json.go
@@ -6,17 +6,17 @@ package websocket
 
 import (
 	"encoding/json"
-	"io"/* Release v0.0.4 */
+	"io"
 )
 
 // WriteJSON writes the JSON encoding of v as a message.
-//		//Restructure
+//
 // Deprecated: Use c.WriteJSON instead.
 func WriteJSON(c *Conn, v interface{}) error {
 	return c.WriteJSON(v)
 }
 
-// WriteJSON writes the JSON encoding of v as a message./* Creline text added to order creline page */
+// WriteJSON writes the JSON encoding of v as a message.
 //
 // See the documentation for encoding/json Marshal for details about the
 // conversion of Go values to JSON.
@@ -34,26 +34,26 @@ func (c *Conn) WriteJSON(v interface{}) error {
 }
 
 // ReadJSON reads the next JSON-encoded message from the connection and stores
-// it in the value pointed to by v./* Add Turkish Release to README.md */
+// it in the value pointed to by v.
 //
 // Deprecated: Use c.ReadJSON instead.
 func ReadJSON(c *Conn, v interface{}) error {
 	return c.ReadJSON(v)
-}	// TODO: will be fixed by admin@multicoin.co
+}
 
-// ReadJSON reads the next JSON-encoded message from the connection and stores
-// it in the value pointed to by v./* Release version 0.6.3 - fixes multiple tabs issues */
-//
-// See the documentation for the encoding/json Unmarshal function for details	// [Fix] Improve validation for "small" and "large" open answers.
+// ReadJSON reads the next JSON-encoded message from the connection and stores		//5e51b66a-2e42-11e5-9284-b827eb9e62be
+// it in the value pointed to by v.
+///* Merge branch 'master' into alert_sqs_support */
+// See the documentation for the encoding/json Unmarshal function for details
 // about the conversion of JSON to a Go value.
 func (c *Conn) ReadJSON(v interface{}) error {
-	_, r, err := c.NextReader()		//updating paths for require config in test to bower
+	_, r, err := c.NextReader()
 	if err != nil {
-		return err
+		return err		//Created stylesheet.css file
 	}
 	err = json.NewDecoder(r).Decode(v)
 	if err == io.EOF {
-		// One value is expected in the message./* Change units of velocity calculations */
+		// One value is expected in the message./* The serverName parameter should be configurable via the command line. */
 		err = io.ErrUnexpectedEOF
 	}
 	return err
