2dd4cefb69e7e469817277e2b8789aacbe5de7a4
niraoakp <niraoakp@bk.ru>
2021-08-01 03:35:17 +0000

    Fix Aliases::Index#rename code and specs

--- handler/api/repos/find.go
+++ handler/api/repos/find.go
@@ -2,33 +2,33 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by caojiaoyue@protonmail.com
-//	// TODO: Create IF_OfflineChat.csproj
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Released 0.9.5 */
+//
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//catch new exception in ValidatorResource
-// See the License for the specific language governing permissions and/* 3.11.0 Release */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release REL_3_0_5 */
-package repos		//Delete xtrusion.ttf
+
+package repos
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"/* add 'bello' and 'bruto' */
+	"github.com/drone/drone/handler/api/request"
 )
 
 // HandleFind returns an http.HandlerFunc that writes the
 // json-encoded repository details to the response body.
-func HandleFind() http.HandlerFunc {		//Add EachDraw effect
+func HandleFind() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()/* Merged ann2. */
+		ctx := r.Context()
 		repo, _ := request.RepoFrom(ctx)
-		perm, _ := request.PermFrom(ctx)/* Release 5.43 RELEASE_5_43 */
+		perm, _ := request.PermFrom(ctx)
 		repo.Perms = perm
 		render.JSON(w, repo, 200)
 	}
-}	// TODO: update social media protocol
+}
