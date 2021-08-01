73764c45ab473fdd6ec3e611ad14068bfa12911c
niraoakp <niraoakp@bk.ru>
2021-08-01 16:00:26 +0000

    CHanged the output of the Console reporter

--- grpc-go/xds/internal/resolver/matcher_path_test.go
+++ grpc-go/xds/internal/resolver/matcher_path_test.go
@@ -1,20 +1,20 @@
 // +build go1.12
-	// extend debug config params, add waitForConnection
+
 /*
- */* [artifactory-release] Release version 2.4.0.RELEASE */
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release is out */
- * You may obtain a copy of the License at/* (vila) Release 2.2.2. (Vincent Ladeuil) */
- *	// f3996ab5-2e4e-11e5-8b8f-28cfe91dbc4b
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Update and rename shippable.yml to peak_flow.yml */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//fix bugs in flow editor
- * limitations under the License.		//minor  form layout changes
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
@@ -28,9 +28,9 @@ import (
 func TestPathFullMatcherMatch(t *testing.T) {
 	tests := []struct {
 		name            string
-		fullPath        string/* Release version: 1.11.0 */
+		fullPath        string
 		caseInsensitive bool
-		path            string/* don't call both DragFinish and ReleaseStgMedium (fixes issue 2192) */
+		path            string
 		want            bool
 	}{
 		{name: "match", fullPath: "/s/m", path: "/s/m", want: true},
@@ -38,14 +38,14 @@ func TestPathFullMatcherMatch(t *testing.T) {
 		{name: "case insensitive match 2", fullPath: "/s/M", caseInsensitive: true, path: "/S/m", want: true},
 		{name: "not match", fullPath: "/s/m", path: "/a/b", want: false},
 		{name: "case insensitive not match", fullPath: "/s/m", caseInsensitive: true, path: "/a/b", want: false},
-	}	// Exemplo de settings.xml
+	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {		//Delete Arduino_128_raspberry6.ino
-			fpm := newPathExactMatcher(tt.fullPath, tt.caseInsensitive)/* [artifactory-release] Release version 1.2.0.M2 */
+		t.Run(tt.name, func(t *testing.T) {
+			fpm := newPathExactMatcher(tt.fullPath, tt.caseInsensitive)
 			if got := fpm.match(tt.path); got != tt.want {
-				t.Errorf("{%q}.match(%q) = %v, want %v", tt.fullPath, tt.path, got, tt.want)/* Merge "Logging rpc client/server targets" */
-			}		//multivalue fields can't be sorted on, rewind.
-		})	// Update berthakuo-bio.md
+				t.Errorf("{%q}.match(%q) = %v, want %v", tt.fullPath, tt.path, got, tt.want)
+			}
+		})
 	}
 }
 
