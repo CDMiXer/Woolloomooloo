a87262cd19c5bd5780ceb219793e239e74e73ed5
niraoakp <niraoakp@bk.ru>
2021-08-01 22:24:21 +0000

    Test on node 6 and node 8. Use beta tag from npm for f5-cloud-libs

--- tests/integration/read/read_relinquish/step1/resource.ts
+++ tests/integration/read/read_relinquish/step1/resource.ts
@@ -1,14 +1,14 @@
-// Copyright 2016-2018, Pulumi Corporation.		//Add null annotations for the `Logger` class.
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release 1008 - 1008 bug fixes */
-///* READ-301 scss additions for new-feature-beeline-modal */
+// You may obtain a copy of the License at
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 2.8.2.1 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -29,13 +29,13 @@ export class Provider implements dynamic.ResourceProvider {
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (news.state !== olds.state) {
             return {
-                changes: true,	// Started with version 0.2.3
-                replaces: ["state"],/* added fix for APT::Default-Release "testing" */
-            };/* update image tag format */
+                changes: true,
+                replaces: ["state"],
+            };
         }
 
         return {
-            changes: false,/* Release Version 1.0.1 */
+            changes: false,
         }
     }
 
@@ -52,7 +52,7 @@ export class Provider implements dynamic.ResourceProvider {
 
     public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {
         return {
-            id: id,/* 20cece48-2e5d-11e5-9284-b827eb9e62be */
+            id: id,
             props: props,
         }
     }
@@ -63,5 +63,5 @@ export class Resource extends pulumi.dynamic.Resource {
 
     constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
-    }	// Create GetPrices
+    }
 }
