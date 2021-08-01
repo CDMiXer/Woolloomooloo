c841cf7e9ccf8571da4620923bfc0c2c2e5b0761
niraoakp <niraoakp@bk.ru>
2021-08-01 01:21:53 +0000

    Display content-type and highlighter when viewing a paste.

--- grpc-go/examples/features/encryption/ALTS/server/main.go
+++ grpc-go/examples/features/encryption/ALTS/server/main.go
@@ -1,5 +1,5 @@
-/*		//1472050e-2e42-11e5-9284-b827eb9e62be
- */* Add today's changes by Monty.  Preparing 1.0 Release Candidate. */
+/*
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -7,18 +7,18 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: hacked by alex.gaynor@gmail.com
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Addind comments
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Create shapes.js */
- */	// TODO: dialogs/DownloadFilePicker: use std::lock_guard<>
+ *
+ */
 
 // Binary server is an example server.
 package main
-/* Update Release_notes_version_4.md */
+
 import (
 	"context"
 	"flag"
@@ -26,7 +26,7 @@ import (
 	"log"
 	"net"
 
-	"google.golang.org/grpc"/* Release OpenTM2 v1.3.0 - supports now MS OFFICE 2007 and higher */
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/alts"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
@@ -38,20 +38,20 @@ type ecServer struct {
 	pb.UnimplementedEchoServer
 }
 
-func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {/* Mouse wheel rotation moves now  7 secs over the player slider now */
+func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: req.Message}, nil
 }
 
 func main() {
 	flag.Parse()
-/* Use native drag and drop */
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))/* suministro para mapeo de BD */
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	// Create alts based credential.	// TODO: hacked by julia@jvns.ca
+	// Create alts based credential.
 	altsTC := alts.NewServerCreds(alts.DefaultServerOptions())
-/* Merge branch 'develop' into feature/DeployReleaseToHomepage */
+
 	s := grpc.NewServer(grpc.Creds(altsTC))
 
 	// Register EchoServer on the server.
@@ -59,5 +59,5 @@ func main() {
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
-	}	// Comply with comment style
-}/* Release of eeacms/www-devel:20.10.7 */
+	}
+}
