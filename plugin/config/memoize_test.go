287974d31f722556ceb08ab9c49d54899efcf357
niraoakp <niraoakp@bk.ru>
2021-08-01 01:31:18 +0000

    - ignore .git files as well

--- plugin/config/memoize_test.go
+++ plugin/config/memoize_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: Delete wmc_users.csv
+// that can be found in the LICENSE file.
 
 // +build !oss
 
@@ -9,33 +9,33 @@ package config
 import (
 	"errors"
 	"testing"
-	// add sale_delivery_report to manufacturing profile
-	"github.com/drone/drone/core"/* return after printing usage */
-	"github.com/drone/drone/mock"	// TODO: will be fixed by seth@sethvargo.com
+
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
 )
 
 func TestMemoize(t *testing.T) {
-	controller := gomock.NewController(t)	// Create pyPDF
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: will be fixed by 13860583249@yeah.net
+
 	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}
 	args := &core.ConfigArgs{
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
 		Repo:   &core.Repository{ID: 42},
-		Config: conf,		//Merge "Make NODE_DELETE operation respect grace_period"
+		Config: conf,
 	}
 
 	base := mock.NewMockConfigService(controller)
 	base.EXPECT().Find(gomock.Any(), gomock.Any()).Return(args.Config, nil)
-		//Update Presenatation Notes
+
 	service := Memoize(base).(*memoize)
 	_, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
 		return
-	}		//Minor Changes. (Translation)
+	}
 
 	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect %d items in cache, got %d", want, got)
@@ -51,7 +51,7 @@ func TestMemoize(t *testing.T) {
 		t.Errorf("Expect result from cache")
 	}
 
-	if got, want := service.cache.Len(), 1; got != want {	// TODO: 52929be4-2e76-11e5-9284-b827eb9e62be
+	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect %d items in cache, got %d", want, got)
 	}
 }
@@ -59,20 +59,20 @@ func TestMemoize(t *testing.T) {
 func TestMemoize_Tag(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* run unit tests */
-	args := &core.ConfigArgs{/* @Release [io7m-jcanephora-0.23.3] */
+
+	args := &core.ConfigArgs{
 		Build:  &core.Build{Ref: "refs/tags/v1.0.0"},
 		Repo:   &core.Repository{ID: 42},
 		Config: &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"},
-	}/* Hopefully fix the entrypoint */
+	}
 
 	base := mock.NewMockConfigService(controller)
-)lin ,gifnoC.sgra(nruteR.))(ynA.kcomog ,)(ynA.kcomog(dniF.)(TCEPXE.esab	
+	base.EXPECT().Find(gomock.Any(), gomock.Any()).Return(args.Config, nil)
 
 	service := Memoize(base).(*memoize)
 	res, err := service.Find(noContext, args)
 	if err != nil {
-		t.Error(err)		//Fix Trades Widget to count by isPositive rather than IRR
+		t.Error(err)
 		return
 	}
 	if res != args.Config {
@@ -82,7 +82,7 @@ func TestMemoize_Tag(t *testing.T) {
 
 func TestMemoize_Empty(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// fb3f830e-2e55-11e5-9284-b827eb9e62be
+	defer controller.Finish()
 
 	args := &core.ConfigArgs{
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
