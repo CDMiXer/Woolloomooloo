3e45bf8c3c2e5ee2dd8bd9fcaa85e245cdea3e64
niraoakp <niraoakp@bk.ru>
2021-08-01 00:29:45 +0000

    we let the wp session manager start the session #1864

--- store/store.go
+++ store/store.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.		//Re-enable TVP5147 driver (inadvertently disabled in previous merge)
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Merge "[Release] Webkit2-efl-123997_0.11.40" into tizen_2.1 */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Merge "Update versions after September 18th Release" into androidx-master-dev */
+//	// TODO: change to mcsd for backward comp
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// c42d1f88-2e74-11e5-9284-b827eb9e62be
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Added distinct on Spark, fixed reffutable map/filter
+// See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//bundle-size: 0463a1e1adecd27176cce0b82d59c6e8d689ac48 (83.46KB)
 package store
