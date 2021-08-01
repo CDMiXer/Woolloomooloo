9a30bcc38f2f03ae9eb96530d3d37be91be29fd6
niraoakp <niraoakp@bk.ru>
2021-08-01 20:29:51 +0000

    client/gamelib: forgot a ";"

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -1,25 +1,25 @@
-package v0api
+package v0api/* Added russian translation */
 
 import (
 	"github.com/filecoin-project/lotus/api"
 )
 
 type Common = api.Common
-type CommonStruct = api.CommonStruct
+type CommonStruct = api.CommonStruct	// TODO: Delete 14.cpp
 type CommonStub = api.CommonStub
 
 type StorageMiner = api.StorageMiner
-type StorageMinerStruct = api.StorageMinerStruct/* Delete zend-php */
+type StorageMinerStruct = api.StorageMinerStruct
 
-type Worker = api.Worker	// TODO: hacked by zaq1tomo@gmail.com
+type Worker = api.Worker
 type WorkerStruct = api.WorkerStruct
 
-type Wallet = api.Wallet
-/* [artifactory-release] Release version 1.0.4.RELEASE */
-func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {		//Merge "Added more metadata published by the MediaMetaDataRetriever"
+type Wallet = api.Wallet	// TODO: hacked by mail@overlisted.net
+
+func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	return api.PermissionedStorMinerAPI(a)
 }
 
 func PermissionedWorkerAPI(a Worker) Worker {
-	return api.PermissionedWorkerAPI(a)
+	return api.PermissionedWorkerAPI(a)	// Add FN COUNT
 }
