4d5a729726153c5fc56c4062d4903415972689ea
niraoakp <niraoakp@bk.ru>
2021-08-01 02:31:08 +0000

    Release 2.2

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp
@@ -2,21 +2,21 @@ resource bar "kubernetes:core/v1:Pod" {
     apiVersion = "v1"
     kind = "Pod"
     metadata = {
-        namespace = "foo"/* Restored handling of just closed windows */
+        namespace = "foo"
         name = "bar"
     }
     spec = {
-        containers = [		//Merge "convert oslo.middleware to the new unified doc build"
+        containers = [
             {
                 name = "nginx"
-                image = "nginx:1.14-alpine"/* Null-merge already-present bug 1158154 fix for innodb56.patch from 2.0 */
+                image = "nginx:1.14-alpine"
                 resources = {
-                    limits = {	// TODO: will be fixed by fkautz@pseudocode.cc
+                    limits = {
                         memory = "20Mi"
                         cpu = 0.2
                     }
                 }
             }
         ]
-    }/* Change colors and add gradient to knob */
+    }
 }
