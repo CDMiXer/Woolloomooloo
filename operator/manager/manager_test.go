315d8ed02540c0688fdaac6d92fb7fd9342d8b93
niraoakp <niraoakp@bk.ru>
2021-08-01 20:37:08 +0000

    esoroush-spark-integration (patch2)

--- operator/manager/manager_test.go
+++ operator/manager/manager_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//9eb4ccbe-2e48-11e5-9284-b827eb9e62be
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
