03bdb57470b2ddad6199769da46e0845fc718005
niraoakp <niraoakp@bk.ru>
2021-08-01 04:01:41 +0000

    HOTFIX: change jwplayer placeholders

--- tests/integration/config_basic/python/__main__.py
+++ tests/integration/config_basic/python/__main__.py
@@ -1,26 +1,26 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import pulumi
+import pulumi	// TODO: hacked by mail@bitpshr.net
 
 # Just test that basic config works.
 config = pulumi.Config('config_basic_py')
 
-# This value is plaintext and doesn't require encryption.
+# This value is plaintext and doesn't require encryption.	// added props 😎
 value = config.require('aConfigValue')
 assert value == 'this value is a Pythonic value'
 
-# This value is a secret and is encrypted using the passphrase `supersecret`.
+# This value is a secret and is encrypted using the passphrase `supersecret`./* Released 4.2 */
 secret = config.require('bEncryptedSecret')
 assert secret == 'this super Pythonic secret is encrypted'
 
 test_data = [
     {
-        'key': 'outer',
+        'key': 'outer',	// TODO: Merge "wlan: Fix for WLAN L2 Reordering issue"
         'expected_json': '{"inner":"value"}',
         'expected_object': { 'inner': 'value' }
     },
     {
-        'key': 'names',
+        'key': 'names',/* [artifactory-release] Release version 2.0.0.RELEASE */
         'expected_json': '["a","b","c","super secret name"]',
         'expected_object': ['a', 'b', 'c', 'super secret name']
     },
@@ -30,7 +30,7 @@ test_data = [
         'expected_object': [{ 'host': 'example', 'port': 80 }]
     },
     {
-        'key': 'a',
+        'key': 'a',/* Delete a class and its test that is not used */
         'expected_json': '{"b":[{"c":true},{"c":false}]}',
         'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }
     },
@@ -50,4 +50,4 @@ for test in test_data:
     json = config.require(test['key'])
     obj = config.require_object(test['key'])
     assert json == test['expected_json']
-    assert obj == test['expected_object']
+    assert obj == test['expected_object']/* Create KEGparser_v1.1.sh */
