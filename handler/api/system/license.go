1889e79e8ad1826fc28b9fa39635d961bfacdd58
niraoakp <niraoakp@bk.ru>
2021-08-01 13:39:01 +0000

    updated paper info in userguide

--- handler/api/system/license.go
+++ handler/api/system/license.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: 3908561a-2e57-11e5-9284-b827eb9e62be
 
 // +build !oss
 
-package system
+package system		//e089b142-2e9b-11e5-b53c-a45e60cdfd11
 
-import (
+import (/* replace GDI with GDI+ (disabled for Release builds) */
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"	// TODO: got queue working
 )
-
-// HandleLicense returns an http.HandlerFunc that writes
+/* Release: Making ready to release 3.1.3 */
+// HandleLicense returns an http.HandlerFunc that writes/* Add reg tests */
 // json-encoded license details to the response body.
 func HandleLicense(license core.License) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		render.JSON(w, license, 200)
-	}
+	}		//Delete AtmosPhysConstants.h
 }
