4f70a3779cb8839cfa11fb1f759b370696e81579
niraoakp <niraoakp@bk.ru>
2021-08-01 23:48:23 +0000

    updated client.common runtime export classpath

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
@@ -3,6 +3,6 @@ import pulumi_aws as aws
 
 logs = aws.s3.Bucket("logs")
 bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(
-    target_bucket=logs.bucket,		//XmlHelper kann jetzt auch mit mehrzeiligem Inhalt umgegen
-)])		//Fixed sounds.json, API update
-pulumi.export("targetBucket", bucket.loggings[0].target_bucket)
+    target_bucket=logs.bucket,/* Merge "Release 3.2.3.304 prima WLAN Driver" */
+)])
+pulumi.export("targetBucket", bucket.loggings[0].target_bucket)		//Merge branch 'version-12-hotfix' into email-camp-end-date-hotfix
