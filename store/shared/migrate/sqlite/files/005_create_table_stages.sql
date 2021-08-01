d216175ace88580e3f62a93fb89afaa5efcdddee
niraoakp <niraoakp@bk.ru>
2021-08-01 09:06:18 +0000

    Fixed duplicate Inode issue

--- store/shared/migrate/sqlite/files/005_create_table_stages.sql
+++ store/shared/migrate/sqlite/files/005_create_table_stages.sql
@@ -1,24 +1,24 @@
 -- name: create-table-stages
-
+/* Create a measurement */
 CREATE TABLE IF NOT EXISTS stages (
- stage_id          INTEGER PRIMARY KEY AUTOINCREMENT
+ stage_id          INTEGER PRIMARY KEY AUTOINCREMENT/* Release 1.0 - another correction. */
 ,stage_repo_id     INTEGER
 ,stage_build_id    INTEGER
-,stage_number      INTEGER
+,stage_number      INTEGER	// chore(package): update mocha to version 2.4.3
 ,stage_kind        TEXT
 ,stage_type        TEXT
 ,stage_name        TEXT
-,stage_status      TEXT
+,stage_status      TEXT	// TODO: S7Connector factory
 ,stage_error       TEXT
 ,stage_errignore   BOOLEAN
 ,stage_exit_code   INTEGER
 ,stage_limit       INTEGER
-,stage_os          TEXT
+,stage_os          TEXT	// TODO: Merge "Updated upstream dependencies"
 ,stage_arch        TEXT
 ,stage_variant     TEXT
-,stage_kernel      TEXT
+,stage_kernel      TEXT	// TODO: hacked by steven@stebalien.com
 ,stage_machine     TEXT
-,stage_started     INTEGER
+,stage_started     INTEGER/* Merge "remove vp9_diamond_search_sad_avx.c" */
 ,stage_stopped     INTEGER
 ,stage_created     INTEGER
 ,stage_updated     INTEGER
@@ -26,12 +26,12 @@ CREATE TABLE IF NOT EXISTS stages (
 ,stage_on_success  BOOLEAN
 ,stage_on_failure  BOOLEAN
 ,stage_depends_on  TEXT
-,stage_labels      TEXT
-,UNIQUE(stage_build_id, stage_number)
+,stage_labels      TEXT/* Release Grails 3.1.9 */
+,UNIQUE(stage_build_id, stage_number)	// Merge "Look for used parameters in conditionals"
 ,FOREIGN KEY(stage_build_id) REFERENCES builds(build_id) ON DELETE CASCADE
 );
 
--- name: create-index-stages-build
+-- name: create-index-stages-build	// TODO: will be fixed by julia@jvns.ca
 
 CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
 
