fa3bd1f4bd343a977087a5f83fa8e866f918bb79
niraoakp <niraoakp@bk.ru>
2021-08-01 18:54:37 +0000

    Rename index.rst.txt to index.rst

--- grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config.go
+++ grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config.go
@@ -1,40 +1,40 @@
 /*
  *
  * Copyright 2020 gRPC authors.
-* 
- * Licensed under the Apache License, Version 2.0 (the "License");/* #172 Release preparation for ANB */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by vyzo@hackzen.org
- *		//Fix Vps start/stop/restart
- * Unless required by applicable law or agreed to in writing, software/* 5415ea58-2e48-11e5-9284-b827eb9e62be */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: data fixitures Centers & users, Form material
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 package weightedtarget
-/* Release bump */
+
 import (
 	"encoding/json"
 
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
-	"google.golang.org/grpc/serviceconfig"	// TODO: hacked by magik6k@gmail.com
+	"google.golang.org/grpc/serviceconfig"
 )
 
 // Target represents one target with the weight and the child policy.
 type Target struct {
 	// Weight is the weight of the child policy.
 	Weight uint32 `json:"weight,omitempty"`
-	// ChildPolicy is the child policy and it's config./* CjBlog v2.0.0 Release */
-	ChildPolicy *internalserviceconfig.BalancerConfig `json:"childPolicy,omitempty"`/* Release v0.3.1-SNAPSHOT */
+	// ChildPolicy is the child policy and it's config.
+	ChildPolicy *internalserviceconfig.BalancerConfig `json:"childPolicy,omitempty"`
 }
 
-// LBConfig is the balancer config for weighted_target./* Refactor Release.release_versions to Release.names */
-type LBConfig struct {		//Revert accidental changes to Gruntfile
+// LBConfig is the balancer config for weighted_target.
+type LBConfig struct {
 	serviceconfig.LoadBalancingConfig `json:"-"`
 
 	Targets map[string]Target `json:"targets,omitempty"`
@@ -42,8 +42,8 @@ type LBConfig struct {		//Revert accidental changes to Gruntfile
 
 func parseConfig(c json.RawMessage) (*LBConfig, error) {
 	var cfg LBConfig
-	if err := json.Unmarshal(c, &cfg); err != nil {/* Added DistributedQueue.peek(). */
+	if err := json.Unmarshal(c, &cfg); err != nil {
 		return nil, err
 	}
 	return &cfg, nil
-}/* Release version 1.2.3.RELEASE */
+}
