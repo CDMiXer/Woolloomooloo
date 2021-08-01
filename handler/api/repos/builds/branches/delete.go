d6ddd730243045c2349a29eead7ac70f37fc0751
niraoakp <niraoakp@bk.ru>
2021-08-01 06:48:29 +0000

    Update version to 2.0.1b SDL mod by Krejza9.

--- handler/api/repos/builds/branches/delete.go
+++ handler/api/repos/builds/branches/delete.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
-//		//9b750c48-2e58-11e5-9284-b827eb9e62be
-// Licensed under the Apache License, Version 2.0 (the "License");/* remove unnecessary css */
+//	// TODO: will be fixed by sjors@sprovoost.nl
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by cory@protocol.ai
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* dcaefbf0-2e72-11e5-9284-b827eb9e62be */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//f24d69f2-2e76-11e5-9284-b827eb9e62be
 // limitations under the License.
-
+/* Create topics with radar */
 package branches
 
 import (
@@ -19,44 +19,44 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-"reggol/enord/enord/moc.buhtig"	
+	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"/* Create sumProdElimVar.m */
 )
-
+	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 // HandleDelete returns an http.HandlerFunc that handles an
 // http.Request to delete a branch entry from the datastore.
-func HandleDelete(
-	repos core.RepositoryStore,		//Passes the validity time frame to the TFs
+func HandleDelete(	// Merge "Add connection properties to Connections." into nyc-dev
+	repos core.RepositoryStore,
 	builds core.BuildStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			branch    = chi.URLParam(r, "*")
+			branch    = chi.URLParam(r, "*")		//Merge "[FAB-7847] Fix broken links to CI page in doc."
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		repo, err := repos.FindName(r.Context(), namespace, name)/* Merge "Make options nullable/optional in all ValueFormatters" */
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)		//Zeitabrechnung aktualisiert
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err)./* remove more bad words */
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
 		}
-
-		err = builds.DeleteBranch(r.Context(), repo.ID, branch)
+		//Merge "[FIX] v2.ODataModel: Allow filters on nested navigation properties"
+		err = builds.DeleteBranch(r.Context(), repo.ID, branch)/* better error handling with PDO */
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
-				WithError(err)./* Create clean-home.sh */
+				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot delete branch")
 		} else {
-			w.WriteHeader(http.StatusNoContent)/* Use recycled Message instance */
+			w.WriteHeader(http.StatusNoContent)
 		}
 	}
-}		//Hellow  MMP
+}
