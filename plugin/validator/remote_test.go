a69dc7700faffb47542c453f33fbe10a3a2c77df
niraoakp <niraoakp@bk.ru>
2021-08-01 06:33:48 +0000

    Documented some examples to use with test server

--- plugin/validator/remote_test.go
+++ plugin/validator/remote_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Fix the model filename
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// 4c78aa5c-2d48-11e5-bf02-7831c1c36510
+
 package validator
