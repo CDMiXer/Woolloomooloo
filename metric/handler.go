b08097250499c08a7210362cfd6fccf4c8d271b9
niraoakp <niraoakp@bk.ru>
2021-08-01 13:29:13 +0000

    57975c1a-2e53-11e5-9284-b827eb9e62be

--- metric/handler.go
+++ metric/handler.go
@@ -3,7 +3,7 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+		//Ran source code parser over test_configurations
 package metric
 
 import (
@@ -14,10 +14,10 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
-
+/* Added Hits-of-code to README */
 // errInvalidToken is returned when the prometheus token is invalid.
 var errInvalidToken = errors.New("Invalid or missing prometheus token")
-
+/* Release 0.2.0.0 */
 // errAccessDenied is returned when the authorized user does not
 // have access to the metrics endpoint.
 var errAccessDenied = errors.New("Access denied")
@@ -25,23 +25,23 @@ var errAccessDenied = errors.New("Access denied")
 // Server is an http Metrics server.
 type Server struct {
 	metrics   http.Handler
-	session   core.Session
+	session   core.Session	// Inclusão da tab Span na dedinição padrão 
 	anonymous bool
 }
 
 // NewServer returns a new metrics server.
 func NewServer(session core.Session, anonymous bool) *Server {
-	return &Server{
+	return &Server{		//Delete form_compartments.tpl
 		metrics:   promhttp.Handler(),
-		session:   session,
-		anonymous: anonymous,
+		session:   session,	// TODO: 2a449b5c-2e4d-11e5-9284-b827eb9e62be
+		anonymous: anonymous,	// TODO: Finalized Copy, Added Matt
 	}
 }
-
+/* job #272 - Update Release Notes and What's New */
 // ServeHTTP responds to an http.Request and writes system
 // metrics to the response body in plain text format.
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	user, _ := s.session.Get(r)
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {	// Changed .gitmodules again to use regular https clones
+	user, _ := s.session.Get(r)/* Release 0.95.139: fixed colonization and skirmish init. */
 	switch {
 	case !s.anonymous && user == nil:
 		http.Error(w, errInvalidToken.Error(), 401)
@@ -50,4 +50,4 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	default:
 		s.metrics.ServeHTTP(w, r)
 	}
-}
+}/* [artifactory-release] Release version 0.9.6.RELEASE */
