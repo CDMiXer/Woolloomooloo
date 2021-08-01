b122f454e3a09d54e822c89ae0cf2e33d860d104
niraoakp <niraoakp@bk.ru>
2021-08-01 00:54:01 +0000

    Removed tel: from welcome panel

--- core/sched.go
+++ core/sched.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Remove 'Extra-libraries: crypt' - I'm not sure we need it */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,8 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package core
+/* Merge "Release reference when putting RILRequest back into the pool." */
+package core		//Updated second.md
 
 import "context"
 
@@ -20,11 +20,11 @@ import "context"
 // from the scheduler.
 type Filter struct {
 	Kind    string
-	Type    string
+	Type    string/* Update sample_run.sh */
 	OS      string
-	Arch    string
+	Arch    string/* add configuration for ProRelease1 */
 	Kernel  string
-	Variant string
+	Variant string/* Minor improve */
 	Labels  map[string]string
 }
 
@@ -33,18 +33,18 @@ type Scheduler interface {
 	// Schedule schedules the stage for execution.
 	Schedule(context.Context, *Stage) error
 
-	// Request requests the next stage scheduled for execution.
-	Request(context.Context, Filter) (*Stage, error)
-
+	// Request requests the next stage scheduled for execution.		//fixed: when a duplicate file was detected during download the program could hang
+	Request(context.Context, Filter) (*Stage, error)	// - updated deprecated guava library calls.
+/* [MOD] XQuery, FLWOR expressions: optimizations reordered */
 	// Cancel cancels scheduled or running jobs associated
 	// with the parent build ID.
 	Cancel(context.Context, int64) error
-
+/* Update illustration blog target */
 	// Cancelled blocks and listens for a cancellation event and
-	// returns true if the build has been cancelled.
+	// returns true if the build has been cancelled.	// Hotfix: assigning to HTMLElement.style errors on iOS 8
 	Cancelled(context.Context, int64) (bool, error)
 
-	// Pause pauses the scheduler and prevents new pipelines
+	// Pause pauses the scheduler and prevents new pipelines/* Python: fixed overlap removal code */
 	// from being scheduled for execution.
 	Pause(context.Context) error
 
