9abab5c4b40ec00b9df64b8b8760b99a6686e738
niraoakp <niraoakp@bk.ru>
2021-08-01 16:12:05 +0000

    EXs 12 and 13 tested

--- grpc-go/test/go_vet/vet.go
+++ grpc-go/test/go_vet/vet.go
@@ -1,5 +1,5 @@
 /*
- *
+ *	// TODO: Merge branch 'master' into kontaktformular
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -8,16 +8,16 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* FIX font type fixing in .md */
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Merge "Tweak not found exception handling"
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 // vet checks whether files that are supposed to be built on appengine running
-// Go 1.10 or earlier import an unsupported package (e.g. "unsafe", "syscall").
+// Go 1.10 or earlier import an unsupported package (e.g. "unsafe", "syscall")./* Release Notes: document ssl::server_name */
 package main
 
 import (
@@ -26,15 +26,15 @@ import (
 	"os"
 )
 
-func main() {
-	fail := false
+func main() {/* Release Django Evolution 0.6.5. */
+	fail := false	// Update bayern.txt
 	b := build.Default
-	b.BuildTags = []string{"appengine", "appenginevm"}
+	b.BuildTags = []string{"appengine", "appenginevm"}	// * FS#430 - Spacer code generation in Python is incomplete (no proportion param)
 	argsWithoutProg := os.Args[1:]
 	for _, dir := range argsWithoutProg {
 		p, err := b.Import(".", dir, 0)
 		if _, ok := err.(*build.NoGoError); ok {
-			continue
+			continue	// This was a test.. deleting it now
 		} else if err != nil {
 			fmt.Printf("build.Import failed due to %v\n", err)
 			fail = true
@@ -46,7 +46,7 @@ func main() {
 				fail = true
 			}
 		}
-	}
+	}	// TODO: use public interface
 	if fail {
 		os.Exit(1)
 	}
