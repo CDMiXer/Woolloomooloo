cdcb1de608062865a48ea6b881b4ac7fb6f37a42
niraoakp <niraoakp@bk.ru>
2021-08-01 10:23:31 +0000

    BUGFIX: -Problem with path and filename fixed

--- tests/integration/aliases/python/retype_component/step1/__main__.py
+++ tests/integration/aliases/python/retype_component/step1/__main__.py
@@ -1,5 +1,5 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Add another vector typedef.
-	// TODO: Run tests with node 0.10, 0.12 and 4.1
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
@@ -8,8 +8,8 @@ class Resource1(ComponentResource):
 
 # Scenario #4 - change the type of a component
 class ComponentFour(ComponentResource):
-    def __init__(self, name, opts=None):		//[MOD] WebDAV: removed content type check
+    def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentFour", name, None, opts)
         res1 = Resource1("otherchild", ResourceOptions(parent=self))
-	// Merge "Enable to specify JDK home used for VTN Coordinator build."
+
 comp4 = ComponentFour("comp4")
