8ea958e9b0b68eee683223b61f2a08fe673f7b39
niraoakp <niraoakp@bk.ru>
2021-08-01 22:35:53 +0000

    Update broker and plan links

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.cs
@@ -1,36 +1,36 @@
-using Pulumi;		//Tidied up Set System Clock fixture
+using Pulumi;
 using Kubernetes = Pulumi.Kubernetes;
-	// Delete Eclipse-Kepler-est-arrive.html
-class MyStack : Stack	// TODO: will be fixed by brosner@gmail.com
-{/* Release note update */
+
+class MyStack : Stack
+{
     public MyStack()
     {
         var bar = new Kubernetes.Core.V1.Pod("bar", new Kubernetes.Types.Inputs.Core.V1.PodArgs
-        {		//Add Groovy nature to Eclipse project
-            ApiVersion = "v1",/* Merge "Dry run: Read content properly when doing consistency check" */
+        {
+            ApiVersion = "v1",
             Kind = "Pod",
             Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
-            {/* Merge "[Ironic] Add ironic logs collector" */
+            {
                 Namespace = "foo",
                 Name = "bar",
             },
-            Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs	// Change layout pages load content
-            {/* Added Cello Parameters to Global Constants */
-                Containers = 	// Added github actions build
+            Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
+            {
+                Containers = 
                 {
                     new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
                     {
                         Name = "nginx",
                         Image = "nginx:1.14-alpine",
-                        Resources = new Kubernetes.Types.Inputs.Core.V1.ResourceRequirementsArgs	// TODO: Merging of 2 related Docker logs (dockerBuild & dockerPackage)
+                        Resources = new Kubernetes.Types.Inputs.Core.V1.ResourceRequirementsArgs
                         {
                             Limits = 
                             {
                                 { "memory", "20Mi" },
                                 { "cpu", "0.2" },
                             },
-                        },	// Updating README.md regarding packaged version issue.
-                    },/* Release note for #818 */
+                        },
+                    },
                 },
             },
         });
