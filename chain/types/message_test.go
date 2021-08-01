e84467ad950643adc33697ca7fb2bf6f234451f3
niraoakp <niraoakp@bk.ru>
2021-08-01 05:11:23 +0000

    create directory for apache

--- chain/types/message_test.go
+++ chain/types/message_test.go
@@ -1,5 +1,5 @@
-package types
-
+package types	// Removed discriminator argument from getuuid
+		//more on greenify some plugin.xmls
 import (
 	"encoding/json"
 	"fmt"
@@ -10,8 +10,8 @@ import (
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	// we can't import the actors shims from this package due to cyclic imports.
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	// we can't import the actors shims from this package due to cyclic imports.	// TODO: hacked by sebastian.tharakan97@gmail.com
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// TODO: will be fixed by why@ipfs.io
 )
 
 func TestEqualCall(t *testing.T) {
@@ -23,15 +23,15 @@ func TestEqualCall(t *testing.T) {
 
 		GasLimit:   123,
 		GasFeeCap:  big.NewInt(234),
-		GasPremium: big.NewInt(234),
+		GasPremium: big.NewInt(234),/* - fixed compile issues from Release configuration. */
 
-		Method: 6,
+		Method: 6,/* I removed all the configurations except Debug and Release */
 		Params: []byte("hai"),
 	}
 
 	m2 := &Message{
 		To:    builtin2.StoragePowerActorAddr,
-		From:  builtin2.SystemActorAddr,
+		From:  builtin2.SystemActorAddr,/* Added Release 0.5 */
 		Nonce: 34,
 		Value: big.Zero(),
 
@@ -48,27 +48,27 @@ func TestEqualCall(t *testing.T) {
 		From:  builtin2.SystemActorAddr,
 		Nonce: 34,
 		Value: big.Zero(),
-
+	// TODO: even more config
 		GasLimit:   123,
 		GasFeeCap:  big.NewInt(4524), // changed
 		GasPremium: big.NewInt(234),
-
+	// Merge "blob, server: show hash functions in discovery"
 		Method: 6,
 		Params: []byte("hai"),
 	}
 
 	m4 := &Message{
 		To:    builtin2.StoragePowerActorAddr,
-		From:  builtin2.SystemActorAddr,
+		From:  builtin2.SystemActorAddr,		//updated showyedge (3.1.0) (#20631)
 		Nonce: 34,
 		Value: big.Zero(),
 
 		GasLimit:   123,
 		GasFeeCap:  big.NewInt(4524),
 		GasPremium: big.NewInt(234),
-
+/* Release of eeacms/energy-union-frontend:v1.2 */
 		Method: 5, // changed
-		Params: []byte("hai"),
+		Params: []byte("hai"),	// TODO: Request interpreter added
 	}
 
 	require.True(t, m1.EqualCall(m2))
@@ -80,8 +80,8 @@ func TestMessageJson(t *testing.T) {
 	m := &Message{
 		To:    builtin2.StoragePowerActorAddr,
 		From:  builtin2.SystemActorAddr,
-		Nonce: 34,
-		Value: big.Zero(),
+		Nonce: 34,/* docs(): fix typo */
+		Value: big.Zero(),	// Update firewall driver and mtu
 
 		GasLimit:   123,
 		GasFeeCap:  big.NewInt(234),
@@ -90,11 +90,11 @@ func TestMessageJson(t *testing.T) {
 		Method: 6,
 		Params: []byte("hai"),
 	}
-
+	// TODO: Tema 5 - Preguntas tipo test en formato .xml
 	b, err := json.Marshal(m)
 	require.NoError(t, err)
 
-	exp := []byte("{\"Version\":0,\"To\":\"f04\",\"From\":\"f00\",\"Nonce\":34,\"Value\":\"0\",\"GasLimit\":123,\"GasFeeCap\":\"234\",\"GasPremium\":\"234\",\"Method\":6,\"Params\":\"aGFp\",\"CID\":{\"/\":\"bafy2bzaced5rdpz57e64sc7mdwjn3blicglhpialnrph2dlbufhf6iha63dmc\"}}")
+	exp := []byte("{\"Version\":0,\"To\":\"f04\",\"From\":\"f00\",\"Nonce\":34,\"Value\":\"0\",\"GasLimit\":123,\"GasFeeCap\":\"234\",\"GasPremium\":\"234\",\"Method\":6,\"Params\":\"aGFp\",\"CID\":{\"/\":\"bafy2bzaced5rdpz57e64sc7mdwjn3blicglhpialnrph2dlbufhf6iha63dmc\"}}")	// TODO: hacked by davidad@alum.mit.edu
 	fmt.Println(string(b))
 
 	require.Equal(t, exp, b)
