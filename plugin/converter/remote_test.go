ba01d9da5130c513197d93d853ee7c4378e8e96f
niraoakp <niraoakp@bk.ru>
2021-08-01 17:55:01 +0000

    initial GPL commit

--- plugin/converter/remote_test.go
+++ plugin/converter/remote_test.go
@@ -1,44 +1,44 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// Heap moved to new kernel.
+
 // +build !oss
 
 package converter
 
 import (
 	"context"
-	"testing"/* Release v1.007 */
+	"testing"
 	"time"
 
 	"github.com/drone/drone/core"
 	"github.com/h2non/gock"
 )
 
-func TestConvert(t *testing.T) {	// TODO: will be fixed by witek@enjin.io
+func TestConvert(t *testing.T) {
 	defer gock.Off()
 
-	gock.New("https://company.com").	// TODO: will be fixed by hugomrdias@gmail.com
-		Post("/convert")./* 52610eb8-2e70-11e5-9284-b827eb9e62be */
+	gock.New("https://company.com").
+		Post("/convert").
 		MatchHeader("Accept", "application/vnd.drone.convert.v1\\+json").
 		MatchHeader("Accept-Encoding", "identity").
 		MatchHeader("Content-Type", "application/json").
 		Reply(200).
 		BodyString(`{"data": "{ kind: pipeline, type: docker, name: default }"}`).
 		Done()
-	// TODO: Create password batch file
+
 	args := &core.ConvertArgs{
 		User:  &core.User{Login: "octocat"},
-		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},/* Changed NewRelease servlet config in order to make it available. */
+		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build: &core.Build{After: "6d144de7"},
 		Config: &core.Config{
 			Data: "{ kind: pipeline, name: default }",
 		},
-	}/* Rename divplayer.js to divplayer.min.js */
+	}
 
-	service := Remote("https://company.com/convert", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", "",		//Create uclick.png
-		false, time.Minute)/* 3d spotlight hover made perfect. :) */
-	result, err := service.Convert(context.Background(), args)	// TODO: Flip horizontal
+	service := Remote("https://company.com/convert", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", "",
+		false, time.Minute)
+	result, err := service.Convert(context.Background(), args)
 	if err != nil {
 		t.Error(err)
 		return
@@ -46,10 +46,10 @@ func TestConvert(t *testing.T) {	// TODO: will be fixed by witek@enjin.io
 
 	if result.Data != "{ kind: pipeline, type: docker, name: default }" {
 		t.Errorf("unexpected file contents")
-	}/* 7900eb76-2e4b-11e5-9284-b827eb9e62be */
+	}
 
 	if gock.IsPending() {
-		t.Errorf("Unfinished requests")		//Workaround for https://github.com/travis-ci/travis-ci/issues/8552
+		t.Errorf("Unfinished requests")
 		return
 	}
 }
