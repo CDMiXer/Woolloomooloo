2745dd04d00e27178d26ee00b32c3f84189977b4
niraoakp <niraoakp@bk.ru>
2021-08-01 05:30:26 +0000

    Update create_your_account_complete.html

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
@@ -1,22 +1,22 @@
 import pulumi
-import pulumi_kubernetes as kubernetes/* bugfix $USER_PIC_PATH */
-		//Use the full flask theme
-argocd_server_deployment = kubernetes.apps.v1.Deployment("argocd_serverDeployment",	// TODO: hacked by sbrichards@gmail.com
-    api_version="apps/v1",
-    kind="Deployment",/* Merge "WiP: Release notes for Gerrit 2.8" */
+import pulumi_kubernetes as kubernetes
+
+argocd_server_deployment = kubernetes.apps.v1.Deployment("argocd_serverDeployment",
+    api_version="apps/v1",	// Rename Generators/PostType.php to src/PostType.php
+    kind="Deployment",
     metadata=kubernetes.meta.v1.ObjectMetaArgs(
         name="argocd-server",
     ),
     spec=kubernetes.apps.v1.DeploymentSpecArgs(
-        template=kubernetes.core.v1.PodTemplateSpecArgs(
+        template=kubernetes.core.v1.PodTemplateSpecArgs(/* The DBX model is now used to calculate costs */
             spec=kubernetes.core.v1.PodSpecArgs(
                 containers=[kubernetes.core.v1.ContainerArgs(
                     readiness_probe={
                         "http_get": {
                             "port": 8080,
-                        },
-                    },
+                        },/* Upadte README with links to video and Release */
+                    },	// TODO: hacked by seth@sethvargo.com
                 )],
-            ),
-        ),/* Fixed Compound not appearing. */
+            ),	// TODO: will be fixed by ng8eke@163.com
+        ),	// TODO: hacked by alex.gaynor@gmail.com
     ))
