5b15c2c0188b36ca4030f337fba3cdef311352ea
niraoakp <niraoakp@bk.ru>
2021-08-01 07:29:22 +0000

    The 5 minute logo was starting to annoy me :S

--- tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
+++ tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
@@ -1,27 +1,27 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Switched from Script-based stage-in to resource-map-based stage-in */
 
 import * as pulumi from "@pulumi/pulumi";
-
+		//Github Incorporated Netbeans
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }
+    }	// TODO: hacked by zaq1tomo@gmail.com
 }
-
+/* Release dhcpcd-6.9.3 */
 // Scenario #2 - adopt a resource into a component.  The component author is the same as the component user, and changes
 // the component to be able to adopt the resource that was previously defined separately...
-class Component extends pulumi.ComponentResource {
-    resource: Resource;
+class Component extends pulumi.ComponentResource {/* Merge "Remove explicit depend on distribute." */
+    resource: Resource;/* Merge "Release 4.0.10.68 QCACLD WLAN Driver." */
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Component", name, {}, opts);
+        super("my:module:Component", name, {}, opts);		//editor support for category.
         // The resource creation was moved from top level to inside the component.
-        this.resource = new Resource(`${name}-child`, {
+        this.resource = new Resource(`${name}-child`, {		//SearchResultView.cs: added placeholder for empty volumetitles
             // With a new parent
-            parent: this,
+            parent: this,	// TODO: will be fixed by hugomrdias@gmail.com
             // But with an alias provided based on knowing where the resource existing before - in this case at top
             // level.  We use an absolute URN instead of a relative `Alias` because we are referencing a fixed resource
             // that was in some arbitrary other location in the hierarchy prior to being adopted into this component.
-            aliases: [pulumi.createUrn("res2", "my:module:Resource")],
+            aliases: [pulumi.createUrn("res2", "my:module:Resource")],/* - Some fixes on the permissions */
         });
     }
 }
@@ -29,12 +29,12 @@ class Component extends pulumi.ComponentResource {
 const comp2 = new Component("comp2");
 
 // Scenario 3: adopt this resource into a new parent.
-class Component2 extends pulumi.ComponentResource {
+class Component2 extends pulumi.ComponentResource {		//Include version info in tags
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Component2", name, {}, opts);
+        super("my:module:Component2", name, {}, opts);/* Release 1.0.14.0 */
     }
 }
-
+/* Doxygen Documentation Added */
 // validate that "parent: undefined" means "i didn't have a parent previously"
 new Component2("unparented", {
     aliases: [{ parent: pulumi.rootStackResource }],
@@ -44,7 +44,7 @@ new Component2("unparented", {
 
 // Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
 // in the next step to be parented by this.  Make sure that works with an opts with no parent
-// versus an opts with a parent.
+// versus an opts with a parent./* Updating to bom version 1.16.29 */
 
 class Component3 extends pulumi.ComponentResource {
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
@@ -52,13 +52,13 @@ class Component3 extends pulumi.ComponentResource {
         new Component2(name + "-child", { aliases: [{ parent: opts.parent}], parent: this });
     }
 }
-
+/* d9c6988a-4b19-11e5-a415-6c40088e03e4 */
 new Component3("parentedbystack");
 new Component3("parentedbycomponent", { parent: comp2 });
 
 // Scenario 5: Allow multiple aliases to the same resource.
 class Component4 extends pulumi.ComponentResource {
-    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
+    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {/* Updated: aws-tools-for-dotnet 3.15.844 */
         super("my:module:Component4", name, {}, {
             aliases: [
                 { parent: pulumi.rootStackResource },
