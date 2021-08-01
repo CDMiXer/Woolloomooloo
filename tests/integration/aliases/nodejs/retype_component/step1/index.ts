923084ef359768c1f121c7aa0776a4c08cbd72d2
niraoakp <niraoakp@bk.ru>
2021-08-01 14:58:03 +0000

    trigger new build for ruby-head (2da5ae4)

--- tests/integration/aliases/nodejs/retype_component/step1/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step1/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// updated collections to use our custom collection class
-import * as pulumi from "@pulumi/pulumi";		//Update program02.c
+
+import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
@@ -13,7 +13,7 @@ class ComponentFour extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentFour", name, {}, opts);
-        this.resource = new Resource("otherchild", {parent: this});/* More bug fixes for ReleaseID->ReleaseGroupID cache. */
+        this.resource = new Resource("otherchild", {parent: this});
     }
-}/* Release 0.9.3 */
+}
 const comp4 = new ComponentFour("comp4");
