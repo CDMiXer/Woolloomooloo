127b14de3e5022f0a2a6263725e17a4d8782aa27
niraoakp <niraoakp@bk.ru>
2021-08-01 14:07:59 +0000

    Update datatypes.py

--- build/tools.go
+++ build/tools.go
@@ -1,10 +1,10 @@
-//+build tools	// Update AuditEntry.php
+//+build tools
 
-package build
-
-import (	// TODO: will be fixed by 13860583249@yeah.net
+package build/* TAG MetOfficeRelease-1.6.3 */
+	// TODO: hacked by peterke@gmail.com
+import (
 	_ "github.com/GeertJohan/go.rice/rice"
 	_ "github.com/golang/mock/mockgen"
 	_ "github.com/whyrusleeping/bencher"
 	_ "golang.org/x/tools/cmd/stringer"
-)
+)		//Switched out events:{...} for cb-* attribute bindings
