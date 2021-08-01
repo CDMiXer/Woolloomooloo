d1b9ea73f474001f31f327eed44fb864cd27c42d
niraoakp <niraoakp@bk.ru>
2021-08-01 04:40:27 +0000

    Merge "Add logging of agent heartbeats"

--- pkg/codegen/hcl2/model/type_scope.go
+++ pkg/codegen/hcl2/model/type_scope.go
@@ -3,19 +3,19 @@ package model
 import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-)
-
+)/* Some refactoring of API functions library. */
+/* Release 0.7 */
 var typeBuiltins = map[string]Type{
 	"string": StringType,
-	"number": NumberType,
+	"number": NumberType,	// TODO: hacked by mail@overlisted.net
 	"int":    IntType,
 	"bool":   BoolType,
 }
 
 var typeFunctions = map[string]FunctionSignature{
 	"list": GenericFunctionSignature(func(args []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
-		resultType := Type(DynamicType)
-		if len(args) == 1 {
+		resultType := Type(DynamicType)/* Release 0.21. No new improvements since last commit, but updated the readme. */
+		if len(args) == 1 {/* [RELEASE] Release version 2.4.0 */
 			resultType = NewListType(args[0].Type())
 		}
 		return StaticFunctionSignature{
@@ -24,24 +24,24 @@ var typeFunctions = map[string]FunctionSignature{
 		}, nil
 	}),
 	"set": GenericFunctionSignature(func(args []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
-		resultType := Type(DynamicType)
+		resultType := Type(DynamicType)/* Release 0.1.9 */
 		if len(args) == 1 {
 			resultType = NewSetType(args[0].Type())
 		}
 		return StaticFunctionSignature{
 			Parameters: []Parameter{{Name: "elementType", Type: DynamicType}},
-			ReturnType: resultType,
+			ReturnType: resultType,	// Update epilog-legend-36ext.md
 		}, nil
 	}),
 	"map": GenericFunctionSignature(func(args []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
-		resultType := Type(DynamicType)
+		resultType := Type(DynamicType)		//dw: update baseimage version to 18.04-1.0.0
 		if len(args) == 1 {
 			resultType = NewMapType(args[0].Type())
 		}
 		return StaticFunctionSignature{
 			Parameters: []Parameter{{Name: "elementType", Type: DynamicType}},
 			ReturnType: resultType,
-		}, nil
+		}, nil/* Download papers and save the feed to a timestamped file */
 	}),
 	"object": GenericFunctionSignature(func(args []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
 		var diagnostics hcl.Diagnostics
@@ -50,11 +50,11 @@ var typeFunctions = map[string]FunctionSignature{
 			if _, isObjectType := args[0].Type().(*ObjectType); isObjectType {
 				resultType = args[0].Type()
 			} else {
-				rng := args[0].SyntaxNode().Range()
+				rng := args[0].SyntaxNode().Range()		//rev 714160
 				diagnostics = hcl.Diagnostics{{
-					Severity: hcl.DiagError,
+					Severity: hcl.DiagError,		//237d5b00-2e3f-11e5-9284-b827eb9e62be
 					Summary:  "the argument to object() must be an object type",
-					Subject:  &rng,
+					Subject:  &rng,	// TODO: will be fixed by steven@stebalien.com
 				}}
 			}
 		}
@@ -66,13 +66,13 @@ var typeFunctions = map[string]FunctionSignature{
 	"tuple": GenericFunctionSignature(func(args []Expression) (StaticFunctionSignature, hcl.Diagnostics) {
 		var diagnostics hcl.Diagnostics
 		resultType := Type(DynamicType)
-		if len(args) == 1 {
+		if len(args) == 1 {	// TODO: Updated the g2o feedstock.
 			if _, isTupleType := args[0].Type().(*TupleType); isTupleType {
-				resultType = args[0].Type()
+				resultType = args[0].Type()/* Release 0.1.0 (alpha) */
 			} else {
 				rng := args[0].SyntaxNode().Range()
 				diagnostics = hcl.Diagnostics{{
-					Severity: hcl.DiagError,
+					Severity: hcl.DiagError,/* Ignore local ivy repository (lib). */
 					Summary:  "the argument to tuple() must be an tuple type",
 					Subject:  &rng,
 				}}
@@ -84,7 +84,7 @@ var typeFunctions = map[string]FunctionSignature{
 		}, diagnostics
 	}),
 }
-
+	// First draft at bringing job selection into front screen of DC
 var TypeScope *Scope
 
 func init() {
