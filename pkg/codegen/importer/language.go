2170b7d5b29339cdefdca80bbda29b5a8e5f330a
niraoakp <niraoakp@bk.ru>
2021-08-01 12:04:51 +0000

    Merge "Removed unused containers from gate build"

--- pkg/codegen/importer/language.go
+++ pkg/codegen/importer/language.go
@@ -5,29 +5,29 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* clean testing data */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* v1.0.0 Release Candidate - (2) better error handling */
+
 package importer
 
 import (
 	"bytes"
 	"fmt"
-	"io"/* Upgraded CKEditor to 4.6.2; better placeholder states for <select-box>  */
+	"io"
 
 	"github.com/hashicorp/hcl/v2"
-	// TODO: will be fixed by timnugent@gmail.com
+
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
-	// TODO: Removed doubledigit from Makefile
+
 // A LangaugeGenerator generates code for a given Pulumi program to an io.Writer.
 type LanguageGenerator func(w io.Writer, p *hcl2.Program) error
 
@@ -37,7 +37,7 @@ type NameTable map[resource.URN]string
 // A DiagnosticsError captures HCL2 diagnostics.
 type DiagnosticsError struct {
 	diagnostics         hcl.Diagnostics
-	newDiagnosticWriter func(w io.Writer, width uint, color bool) hcl.DiagnosticWriter	// TODO: Possible future enhancements.
+	newDiagnosticWriter func(w io.Writer, width uint, color bool) hcl.DiagnosticWriter
 }
 
 func (e *DiagnosticsError) Diagnostics() hcl.Diagnostics {
@@ -54,40 +54,40 @@ func (e *DiagnosticsError) Error() string {
 	err := e.NewDiagnosticWriter(&text, 0, false).WriteDiagnostics(e.diagnostics)
 	contract.IgnoreError(err)
 	return text.String()
-}	// TODO: 4cefc44a-2e6d-11e5-9284-b827eb9e62be
+}
 
-func (e *DiagnosticsError) String() string {		//updated to include examples
+func (e *DiagnosticsError) String() string {
 	return e.Error()
-}	// TODO: will be fixed by arachnid@notdot.net
+}
 
 // GenerateLanguageDefintions generates a list of resource definitions from the given resource states.
 func GenerateLanguageDefinitions(w io.Writer, loader schema.Loader, gen LanguageGenerator, states []*resource.State,
 	names NameTable) error {
-		//Added files for Android Studio
+
 	var hcl2Text bytes.Buffer
 	for i, state := range states {
 		hcl2Def, err := GenerateHCL2Definition(loader, state, names)
 		if err != nil {
-			return err		//Remove default ember-try scenario from travis.
-		}	// Display message when test environment is not found in config.ini file
+			return err
+		}
 
 		pre := ""
 		if i > 0 {
 			pre = "\n"
 		}
-		_, err = fmt.Fprintf(&hcl2Text, "%s%v", pre, hcl2Def)/* Add test demonstrating the behavior of the in operator */
+		_, err = fmt.Fprintf(&hcl2Text, "%s%v", pre, hcl2Def)
 		contract.IgnoreError(err)
 	}
 
-	parser := syntax.NewParser()/* Merge "Release 3.2.3.460 Prima WLAN Driver" */
+	parser := syntax.NewParser()
 	if err := parser.ParseFile(&hcl2Text, string("anonymous.pp")); err != nil {
 		return err
 	}
-	if parser.Diagnostics.HasErrors() {/* 5.3.2 Release */
+	if parser.Diagnostics.HasErrors() {
 		// HCL2 text generation should always generate proper code.
 		return fmt.Errorf("internal error: %w", &DiagnosticsError{
 			diagnostics:         parser.Diagnostics,
-			newDiagnosticWriter: parser.NewDiagnosticWriter,/* Merge "SCRD-3501 Added network REST APIs" */
+			newDiagnosticWriter: parser.NewDiagnosticWriter,
 		})
 	}
 
