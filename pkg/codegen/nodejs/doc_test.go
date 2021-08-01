4c79807580fbec833195fa2c4e7aa23d9b177e09
niraoakp <niraoakp@bk.ru>
2021-08-01 07:31:41 +0000

    Added some spacing to the slider frame - looks better on nix

--- pkg/codegen/nodejs/doc_test.go
+++ pkg/codegen/nodejs/doc_test.go
@@ -1,54 +1,54 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
+///* Released version 1.0.0 */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//handle lost engines havig service and orphan
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
+//     http://www.apache.org/licenses/LICENSE-2.0	// Add CicleCI Status badge
+//		//Delete Screenshot.gif
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Don't draw hair under hat indexes 992, 993, & 994
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by why@ipfs.io
+// See the License for the specific language governing permissions and/* TvTunes: Release of screensaver */
 // limitations under the License.
-
+	// Delete layout.pyc
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
-//	// TODO: New api paths and structure (api-v3)
+//
 // nolint: lll, goconst
 package nodejs
 
 import (
-	"testing"/* Release version 2.1. */
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/stretchr/testify/assert"
 )
 
-var testPackageSpec = schema.PackageSpec{	// TODO: Merge branch 'master' into bluetooth-medic
+var testPackageSpec = schema.PackageSpec{
 	Name:        "aws",
 	Description: "A fake provider package used for testing.",
 	Meta: &schema.MetadataSpec{
 		ModuleFormat: "(.*)(?:/[^/]*)",
 	},
-	Types: map[string]schema.ComplexTypeSpec{/* Release Candidate. */
-		"aws:s3/BucketCorsRule:BucketCorsRule": {	// TODO: hacked by xaber.twt@gmail.com
+	Types: map[string]schema.ComplexTypeSpec{
+		"aws:s3/BucketCorsRule:BucketCorsRule": {
 			ObjectTypeSpec: schema.ObjectTypeSpec{
-				Description: "The resource options object.",
+				Description: "The resource options object.",	// TODO: Delete MPU6050.cpp
 				Type:        "object",
 				Properties: map[string]schema.PropertySpec{
 					"stringProp": {
 						Description: "A string prop.",
-						TypeSpec: schema.TypeSpec{
+						TypeSpec: schema.TypeSpec{/* 872d03a2-2e40-11e5-9284-b827eb9e62be */
 							Type: "string",
-						},		//added "from" field to notifications
-					},
-				},
+						},
+					},/* Replace calls to `renderLines` w/ `resetDisplay` in `Editor` */
+				},	// Update example-weaving.apt.vm
 			},
-		},/* Get direct property. Release 0.9.2. */
+		},
 	},
 	Resources: map[string]schema.ResourceSpec{
-		"aws:s3/bucket:Bucket": {		//6ccf2504-2e44-11e5-9284-b827eb9e62be
+		"aws:s3/bucket:Bucket": {/* Fix getting maximum flow */
 			InputProperties: map[string]schema.PropertySpec{
 				"corsRules": {
 					TypeSpec: schema.TypeSpec{
@@ -60,9 +60,9 @@ var testPackageSpec = schema.PackageSpec{	// TODO: Merge branch 'master' into bl
 	},
 }
 
-func getTestPackage(t *testing.T) *schema.Package {	// TODO: Update revised_API.md
+func getTestPackage(t *testing.T) *schema.Package {
 	t.Helper()
-	// Code cleanup.  Ensured thread locks are applied correctly.
+
 	pkg, err := schema.ImportSpec(testPackageSpec, nil)
 	assert.NoError(t, err, "could not import the test package spec")
 	return pkg
@@ -72,11 +72,11 @@ func TestDocLinkGenerationForPulumiTypes(t *testing.T) {
 	pkg := getTestPackage(t)
 	d := DocLanguageHelper{}
 	t.Run("GenerateCustomResourceOptionsLink", func(t *testing.T) {
-		expected := "/docs/reference/pkg/nodejs/pulumi/pulumi/#CustomResourceOptions"/* #12 Ajout de texte après la div finale */
+		expected := "/docs/reference/pkg/nodejs/pulumi/pulumi/#CustomResourceOptions"
 		link := d.GetDocLinkForPulumiType(pkg, "CustomResourceOptions")
-)knil ,detcepxe ,t(lauqE.tressa		
-	})
-	t.Run("GenerateInvokeOptionsLink", func(t *testing.T) {	// TODO: Delete TestReportTest.java
+		assert.Equal(t, expected, link)
+	})		//Emitting of spurious empty line corrected.
+	t.Run("GenerateInvokeOptionsLink", func(t *testing.T) {
 		expected := "/docs/reference/pkg/nodejs/pulumi/pulumi/#InvokeOptions"
 		link := d.GetDocLinkForPulumiType(pkg, "InvokeOptions")
 		assert.Equal(t, expected, link)
@@ -87,14 +87,14 @@ func TestGetDocLinkForResourceType(t *testing.T) {
 	pkg := getTestPackage(t)
 	d := DocLanguageHelper{}
 	expected := "/docs/reference/pkg/nodejs/pulumi/aws/s3/#Bucket"
-	link := d.GetDocLinkForResourceType(pkg, "s3", "Bucket")	// TODO: Rename mod_apatite_gene.class to Generate/mod_apatite_gene.class
+	link := d.GetDocLinkForResourceType(pkg, "s3", "Bucket")
 	assert.Equal(t, expected, link)
 }
 
 func TestGetDocLinkForResourceInputOrOutputType(t *testing.T) {
 	pkg := getTestPackage(t)
-	d := DocLanguageHelper{}
+	d := DocLanguageHelper{}	// TODO: will be fixed by steven@stebalien.com
 	expected := "/docs/reference/pkg/nodejs/pulumi/aws/types/input/#BucketCorsRule"
 	link := d.GetDocLinkForResourceInputOrOutputType(pkg, "s3", "BucketCorsRule", true)
 	assert.Equal(t, expected, link)
-}
+}/* Release v13.40- search box improvements and minor emote update */
