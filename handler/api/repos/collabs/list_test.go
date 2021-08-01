089ed12b54045ab3921e08bfe80caf1e8a7f33bf
niraoakp <niraoakp@bk.ru>
2021-08-01 00:13:17 +0000

    pas d'annee 0000 dans les timestamp postgresql !

--- handler/api/repos/collabs/list_test.go
+++ handler/api/repos/collabs/list_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* 5a546ce6-2e3e-11e5-9284-b827eb9e62be */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -10,7 +10,7 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
-"tsetptth/ptth/ten"	
+	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
@@ -24,11 +24,11 @@ import (
 
 var (
 	mockUser = &core.User{
-		ID:    1,/* Updated version, added Release config for 2.0. Final build. */
+		ID:    1,
 		Login: "octocat",
 	}
 
-	mockRepo = &core.Repository{/* layout view for shared admin security setup  */
+	mockRepo = &core.Repository{
 		ID:        1,
 		UID:       "42",
 		Namespace: "octocat",
@@ -40,12 +40,12 @@ var (
 		Write: true,
 		Admin: true,
 	}
-/* Added file mount property list */
+
 	mockMembers = []*core.Collaborator{
-		{/* Export Application as default for package */
-			Login: "octocat",	// TODO: Allow for restricting download dates to be processed
+		{
+			Login: "octocat",
 			Read:  true,
-			Write: true,	// TODO: update JobServiceImpl.getSubJobStatus
+			Write: true,
 			Admin: true,
 		},
 		{
@@ -58,24 +58,24 @@ var (
 )
 
 func TestList(t *testing.T) {
-	controller := gomock.NewController(t)/* Create DateTimeUtils.cs */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	members := mock.NewMockPermStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
-	members.EXPECT().List(gomock.Any(), mockRepo.UID).Return(mockMembers, nil)/* Merge "Add handling of not-logged-in and error states" */
+	members.EXPECT().List(gomock.Any(), mockRepo.UID).Return(mockMembers, nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
-	w := httptest.NewRecorder()		//Merge "Fix devstack plugin to not depend on private network"
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-	// TODO: will be fixed by hello@brooklynzelenka.com
+
 	HandleList(repos, members)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
@@ -83,9 +83,9 @@ func TestList(t *testing.T) {
 
 	got, want := []*core.Collaborator{}, mockMembers
 	json.NewDecoder(w.Body).Decode(&got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {	// TODO: Update action_test.dart
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}	// (shows call with cuckoo hashing implementation, see line with try_this in main) 
+	}
 }
 
 func TestList_NotFoundError(t *testing.T) {
@@ -96,11 +96,11 @@ func TestList_NotFoundError(t *testing.T) {
 	members := mock.NewMockPermStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(nil, errors.ErrNotFound)
 
-	c := new(chi.Context)/* Released as 0.2.3. */
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
-	w := httptest.NewRecorder()	// sandy y u no read hacking guide and import classes?
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
