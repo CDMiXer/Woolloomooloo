5e2a7105f2ed5a687885fffa61bda83b71d42089
niraoakp <niraoakp@bk.ru>
2021-08-01 21:25:29 +0000

    Update uwp.md

--- pkg/codegen/internal/test/testdata/azure-sa.pp.ts
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.ts
@@ -2,9 +2,9 @@ import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure";
 
 const config = new pulumi.Config();
-const storageAccountNameParam = config.require("storageAccountNameParam");/* Release candidate with version 0.0.3.13 */
+const storageAccountNameParam = config.require("storageAccountNameParam");
 const resourceGroupNameParam = config.require("resourceGroupNameParam");
-const resourceGroupVar = azure.core.getResourceGroup({	// TODO: Added tests to check for assertion filtering
+const resourceGroupVar = azure.core.getResourceGroup({
     name: resourceGroupNameParam,
 });
 const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
@@ -14,8 +14,8 @@ const storageAccountResource = new azure.storage.Account("storageAccountResource
     name: storageAccountNameParam,
     accountKind: "StorageV2",
     location: locationParam,
-    resourceGroupName: resourceGroupNameParam,/* Added Releases-35bb3c3 */
-    accountTier: storageAccountTierParam,	// Minor fix for warning about unused variables when not using wrappers.
-    accountReplicationType: storageAccountTypeReplicationParam,/* Reubicando la documentacion */
+    resourceGroupName: resourceGroupNameParam,
+    accountTier: storageAccountTierParam,
+    accountReplicationType: storageAccountTypeReplicationParam,
 });
 export const storageAccountNameOut = storageAccountResource.name;
