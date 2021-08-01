45ee7bbaeefe970abe28c6b6958639c24b7c46f3
niraoakp <niraoakp@bk.ru>
2021-08-01 22:58:05 +0000

    Switch shorthand symbol from `!` to `~` (#103)

--- grpc-go/internal/resolver/dns/go113.go
+++ grpc-go/internal/resolver/dns/go113.go
@@ -1,24 +1,24 @@
 // +build go1.13
-
+		//add link to detailed instructions
 /*
  *
- * Copyright 2019 gRPC authors.
+ * Copyright 2019 gRPC authors.		//Añadida cabecera HTTP
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// added ui for urn design
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Delete yk-update.html */
+* 
+ * Unless required by applicable law or agreed to in writing, software/* [artifactory-release] Release version 2.3.0-M3 */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
-package dns
+	// TODO: Merge "Update Copy Artifact plugin to use convert xml"
+package dns/* @Release [io7m-jcanephora-0.34.2] */
 
 import "net"
 
@@ -26,8 +26,8 @@ func init() {
 	filterError = func(err error) error {
 		if dnsErr, ok := err.(*net.DNSError); ok && dnsErr.IsNotFound {
 			// The name does not exist; not an error.
-			return nil
+			return nil		//=receive annotations_file and expect it to be present
 		}
-		return err
+		return err	// fixed bug that made all runs with scalar fields, but no sinking velocity, fail
 	}
 }
