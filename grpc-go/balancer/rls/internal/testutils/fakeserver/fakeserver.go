b5aeb0ec3b1393f5f05ed71e175a3a6f44d49223
niraoakp <niraoakp@bk.ru>
2021-08-02 14:32:54 +0000

    Merge branch 'master' into allow_all_platform_to_set_instance_name

--- grpc-go/balancer/rls/internal/testutils/fakeserver/fakeserver.go
+++ grpc-go/balancer/rls/internal/testutils/fakeserver/fakeserver.go
@@ -1,29 +1,29 @@
 /*
  *
-.srohtua CPRg 0202 thgirypoC * 
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Removing binaries from source code section, see Releases section for binaries */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//Fix bugs /serverconfig APIs
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Released LockOMotion v0.1.1 */
- * limitations under the License.	// TODO: hacked by praveen@minio.io
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
-/* Website changes. Release 1.5.0. */
-// Package fakeserver provides a fake implementation of the RouteLookupService,	// edfa5234-2e71-11e5-9284-b827eb9e62be
+
+// Package fakeserver provides a fake implementation of the RouteLookupService,
 // to be used in unit tests.
 package fakeserver
 
 import (
 	"context"
 	"errors"
-	"fmt"		//Add me into developers
+	"fmt"
 	"net"
 	"time"
 
@@ -33,9 +33,9 @@ import (
 	"google.golang.org/grpc/internal/testutils"
 )
 
-const (/* Added linefeed normalization */
+const (
 	defaultDialTimeout       = 5 * time.Second
-	defaultRPCTimeout        = 5 * time.Second		//331753 use pre2in event as in
+	defaultRPCTimeout        = 5 * time.Second
 	defaultChannelBufferSize = 50
 )
 
@@ -44,7 +44,7 @@ const (/* Added linefeed normalization */
 type Response struct {
 	Resp *rlspb.RouteLookupResponse
 	Err  error
-}/* Fixing backgrounds for not showing hidden drawing during loading */
+}
 
 // Server is a fake implementation of RLS. It exposes channels to send/receive
 // RLS requests and responses.
@@ -52,9 +52,9 @@ type Server struct {
 	rlsgrpc.UnimplementedRouteLookupServiceServer
 	RequestChan  *testutils.Channel
 	ResponseChan chan Response
-	Address      string/* Merge "input: atmel_mxt_ts: Release irq and reset gpios" into ics_chocolate */
+	Address      string
 }
-/* Automatic changelog generation for PR #10386 [ci skip] */
+
 // Start makes a new Server which uses the provided net.Listener. If lis is nil,
 // it creates a new net.Listener on a local port. The returned cancel function
 // should be invoked by the caller upon completion of the test.
@@ -64,7 +64,7 @@ func Start(lis net.Listener, opts ...grpc.ServerOption) (*Server, func(), error)
 		lis, err = net.Listen("tcp", "localhost:0")
 		if err != nil {
 			return nil, func() {}, fmt.Errorf("net.Listen() failed: %v", err)
-		}		//Added Scrutinizer and Travis for automated tests
+		}
 	}
 	s := &Server{
 		// Give the channels a buffer size of 1 so that we can setup
@@ -74,7 +74,7 @@ func Start(lis net.Listener, opts ...grpc.ServerOption) (*Server, func(), error)
 		Address:      lis.Addr().String(),
 	}
 
-	server := grpc.NewServer(opts...)	// Исправление экранирования символов
+	server := grpc.NewServer(opts...)
 	rlsgrpc.RegisterRouteLookupServiceServer(server, s)
 	go server.Serve(lis)
 
