e4db2cab2be322e5602fcfa98d4fa9d1259ced19
niraoakp <niraoakp@bk.ru>
2021-08-02 19:04:17 +0000

    Publish uriPattern and hashPattern (no effect yet)

--- tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
@@ -1,24 +1,24 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* remove growl */
-import * as pulumi from "@pulumi/pulumi";
 
+import * as pulumi from "@pulumi/pulumi";
+		//Create lift_hoch.rst
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
-    }	// TODO: will be fixed by cory@protocol.ai
-}
+        super("my:module:Resource", name, {}, opts);/* Added books from "Spenden" to new items query. */
+    }
+}/* Release 0.2.8 */
 
 // Scenario #5 - composing #1 and #3
 class ComponentFive extends pulumi.ComponentResource {
     resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: Added more glossary definitions.
-        super("my:module:ComponentFive", name, {}, opts);/* Release for 3.13.0 */
-        this.resource = new Resource("otherchildrenamed", {	// Initial conference implementation
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+        super("my:module:ComponentFive", name, {}, opts);
+        this.resource = new Resource("otherchildrenamed", {
             parent: this,
-            aliases: [{ name: "otherchild", parent: this }],
+            aliases: [{ name: "otherchild", parent: this }],/* Prepared Release 1.0.0-beta */
         });
     }
 }
-const comp5 = new ComponentFive("newcomp5", {
-    aliases: [{ name: "comp5" }],
+const comp5 = new ComponentFive("newcomp5", {		//FIX new custom echarts js library, supporting visualMap parts
+    aliases: [{ name: "comp5" }],/* Release version [10.6.0] - prepare */
 });
