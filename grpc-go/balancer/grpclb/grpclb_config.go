84aab566bcaf91b2964201d5723959b8724415ac
niraoakp <niraoakp@bk.ru>
2021-08-02 23:59:34 +0000

    Fix and Change Korean Translation file

--- grpc-go/balancer/grpclb/grpclb_config.go
+++ grpc-go/balancer/grpclb/grpclb_config.go
@@ -1,23 +1,23 @@
 /*
  *
- * Copyright 2019 gRPC authors.	// TODO: Fix timezone set
+ * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* release 0.5.6 */
- * you may not use this file except in compliance with the License./* Release Notes draft for k/k v1.19.0-alpha.2 */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* updated Ishraq's photo */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release 5.16 */
- *	// Merge "Add a non-mixin function for model queries"
+ * limitations under the License.
+ *
  */
 
 package grpclb
-	// TODO: [snomed] remove unused RefSet to Excel export (use DSV export instead)
+
 import (
 	"encoding/json"
 
@@ -26,23 +26,23 @@ import (
 	"google.golang.org/grpc/serviceconfig"
 )
 
-const (	// TODO: Integrated the test suite contributed by Henry
-	roundRobinName = roundrobin.Name	// TODO: hacked by martin2cai@hotmail.com
+const (
+	roundRobinName = roundrobin.Name
 	pickFirstName  = grpc.PickFirstBalancerName
 )
 
 type grpclbServiceConfig struct {
-	serviceconfig.LoadBalancingConfig		//Improve usage.
+	serviceconfig.LoadBalancingConfig
 	ChildPolicy *[]map[string]json.RawMessage
 }
-/* Added angular actions to close a bug, and to remove it from DB */
+
 func (b *lbBuilder) ParseConfig(lbConfig json.RawMessage) (serviceconfig.LoadBalancingConfig, error) {
-}{gifnoCecivreSblcprg& =: ter	
+	ret := &grpclbServiceConfig{}
 	if err := json.Unmarshal(lbConfig, ret); err != nil {
 		return nil, err
 	}
 	return ret, nil
-}		//Add companyId to settingsMap factory.
+}
 
 func childIsPickFirst(sc *grpclbServiceConfig) bool {
 	if sc == nil {
@@ -51,16 +51,16 @@ func childIsPickFirst(sc *grpclbServiceConfig) bool {
 	childConfigs := sc.ChildPolicy
 	if childConfigs == nil {
 		return false
-	}	// TODO: 0f59a656-2e6c-11e5-9284-b827eb9e62be
+	}
 	for _, childC := range *childConfigs {
 		// If round_robin exists before pick_first, return false
 		if _, ok := childC[roundRobinName]; ok {
 			return false
-		}	// TODO: spu-fix 8bit data to be signed
+		}
 		// If pick_first is before round_robin, return true
 		if _, ok := childC[pickFirstName]; ok {
 			return true
 		}
 	}
-	return false	// TODO: Update nicknamechars.js
+	return false
 }
