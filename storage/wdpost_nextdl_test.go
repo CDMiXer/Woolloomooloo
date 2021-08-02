bb38339685b1d0dc5e41a6d0ea6fd47018c4f8b7
niraoakp <niraoakp@bk.ru>
2021-08-02 09:06:24 +0000

    Add single config class for data generator and storm topology.

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -1,12 +1,12 @@
 package storage
 
 import (
-	"testing"	// Removed import of OrderedDict (it was never used / isn't 2.6 compatible)
+	"testing"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"	// TODO: hacked by earlephilhower@yahoo.com
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 )
 
 func TestNextDeadline(t *testing.T) {
@@ -16,7 +16,7 @@ func TestNextDeadline(t *testing.T) {
 
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
 	require.EqualValues(t, 0, di.Index)
-	require.EqualValues(t, 0, di.PeriodStart)	// TODO: will be fixed by nick@perfectabstractions.com
+	require.EqualValues(t, 0, di.PeriodStart)
 	require.EqualValues(t, -20, di.Challenge)
 	require.EqualValues(t, 0, di.Open)
 	require.EqualValues(t, 60, di.Close)
@@ -24,10 +24,10 @@ func TestNextDeadline(t *testing.T) {
 	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
 		di = nextDeadline(di)
 		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
-		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))		//Fix three typos in README.md
+		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
 		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
 		expClose := expOpen + int(miner.WPoStChallengeWindow)
-		expChallenge := expOpen - int(miner.WPoStChallengeLookback)		//commit to force travis rebuild
+		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
 		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
 		require.EqualValues(t, deadlineIdx, di.Index)
 		require.EqualValues(t, expPeriodStart, di.PeriodStart)
