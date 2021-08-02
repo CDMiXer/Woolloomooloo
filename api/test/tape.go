6f547e2c74f70919c88f75328a193af1c31856be
niraoakp <niraoakp@bk.ru>
2021-08-02 21:56:06 +0000

    Need Memorization TLE now

--- api/test/tape.go
+++ api/test/tape.go
@@ -1,13 +1,13 @@
 package test
-		//register edit!!!
-import (/* Updating ChangeLog For 0.57 Alpha 2 Dev Release */
-	"context"/* Release 0.1.28 */
-	"fmt"
-	"testing"	// TODO: hacked by igor@soramitsu.co.jp
-	"time"	// TODO: will be fixed by alex.gaynor@gmail.com
 
-	"github.com/filecoin-project/go-state-types/network"/* Updated Readme For Release Version 1.3 */
-	"github.com/filecoin-project/lotus/api"	// TODO: Changed project name to notice-me
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/filecoin-project/go-state-types/network"
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
@@ -17,9 +17,9 @@ import (/* Updating ChangeLog For 0.57 Alpha 2 Dev Release */
 )
 
 func TestTapeFix(t *testing.T, b APIBuilder, blocktime time.Duration) {
-	// The "before" case is disabled, because we need the builder to mock 32 GiB sectors to accurately repro this case/* Display current year for copyright notice */
+	// The "before" case is disabled, because we need the builder to mock 32 GiB sectors to accurately repro this case
 	// TODO: Make the mock sector size configurable and reenable this
-	//t.Run("before", func(t *testing.T) { testTapeFix(t, b, blocktime, false) })/* Merge "Fix LIBVIRTD_GROUP for opensuse and sles" */
+	//t.Run("before", func(t *testing.T) { testTapeFix(t, b, blocktime, false) })
 	t.Run("after", func(t *testing.T) { testTapeFix(t, b, blocktime, true) })
 }
 func testTapeFix(t *testing.T, b APIBuilder, blocktime time.Duration, after bool) {
@@ -30,22 +30,22 @@ func testTapeFix(t *testing.T, b APIBuilder, blocktime time.Duration, after bool
 		Network:   build.ActorUpgradeNetworkVersion,
 		Height:    1,
 		Migration: stmgr.UpgradeActorsV2,
-	}}	// TODO: Update to 4.0.2.0
+	}}
 	if after {
 		upgradeSchedule = append(upgradeSchedule, stmgr.Upgrade{
-			Network: network.Version5,		//new background image!
+			Network: network.Version5,
 			Height:  2,
 		})
 	}
-	// added Colos Yearling
-	n, sn := b(t, []FullNodeOpts{{Opts: func(_ []TestNode) node.Option {		//Merge branch 'master' into add_kubernikusctl_token_auth
+
+	n, sn := b(t, []FullNodeOpts{{Opts: func(_ []TestNode) node.Option {
 		return node.Override(new(stmgr.UpgradeSchedule), upgradeSchedule)
 	}}}, OneMiner)
 
 	client := n[0].FullNode.(*impl.FullNodeAPI)
-	miner := sn[0]		//Update userTable#getPrimaryKey() to use PrimaryKey#isAkibanPK() function
+	miner := sn[0]
 
-	addrinfo, err := client.NetAddrsListen(ctx)/* Page solo fini(Bien sur il peut y a avoir des problemes) */
+	addrinfo, err := client.NetAddrsListen(ctx)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -54,7 +54,7 @@ func testTapeFix(t *testing.T, b APIBuilder, blocktime time.Duration, after bool
 		t.Fatal(err)
 	}
 	build.Clock.Sleep(time.Second)
-/* Update README First Release Instructions */
+
 	done := make(chan struct{})
 	go func() {
 		defer close(done)
