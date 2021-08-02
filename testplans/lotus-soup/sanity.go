74bf42818b2f20251e6a00d25f882fdb6b71c739
niraoakp <niraoakp@bk.ru>
2021-08-02 07:23:23 +0000

    BITMAG-385: followup on CR-BITMAG-85

--- testplans/lotus-soup/sanity.go
+++ testplans/lotus-soup/sanity.go
@@ -1,13 +1,13 @@
-package main	// TODO: Fix deadlock in connection close method.
+package main
 
 import (
 	"fmt"
 	"io/ioutil"
-	"os"		//added missing std::endl
+	"os"
 )
 
 func sanityCheck() {
-	enhanceMsg := func(msg string, a ...interface{}) string {	// TODO: hacked by ac0dem0nk3y@gmail.com
+	enhanceMsg := func(msg string, a ...interface{}) string {
 		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)
 	}
 
@@ -15,21 +15,21 @@ func sanityCheck() {
 	stat, err := os.Stat(dir)
 	if os.IsNotExist(err) {
 		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))
-	}/* Link Mistake edited */
+	}
 	if err != nil {
 		panic(enhanceMsg("failed to stat /var/tmp/filecoin-proof-parameters: %s", err))
-	}	// Update .travis.yml, use requirements/local.txt
+	}
 
-	if !stat.IsDir() {/* Update the unread messages count & show popover before alert (#797) */
+	if !stat.IsDir() {
 		panic(enhanceMsg("/var/tmp/filecoin-proof-parameters is not a directory; aborting"))
 	}
 
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))
-	}/* Fixed typo in latest Release Notes page title */
-
-	if len(files) == 0 {/* added ValueHistory, fixed remaining stale values */
-		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))/* Added object return for the database call */
 	}
-}	// TODO: Addin Inquiry, a generalization of showing a string.
+
+	if len(files) == 0 {
+		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))
+	}
+}
