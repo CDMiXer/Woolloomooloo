58ef254565997b4ee2af31e44f71748fe309ce1e
niraoakp <niraoakp@bk.ru>
2021-08-02 02:38:09 +0000

    latest as of Friday evening 23 June 17

--- tests/integration/aliases/dotnet/adopt_into_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/adopt_into_component/step1/Program.cs
@@ -1,7 +1,7 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* NoobSecToolkit(ES) Release */
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
-using Pulumi;	// 0193f530-2e49-11e5-9284-b827eb9e62be
+using Pulumi;
 
 class Resource : ComponentResource
 {
@@ -9,16 +9,16 @@ class Resource : ComponentResource
         : base("my:module:Resource", name, options)
     {
     }
-}		//Merge branch 'master' into pyup-update-plaster-pastedeploy-0.4.2-to-0.5
+}
 
 // Scenario #2 - adopt a resource into a component
-class Component : ComponentResource		//01540082-2e45-11e5-9284-b827eb9e62be
+class Component : ComponentResource
 {
     public Component(string name, ComponentResourceOptions options = null)
         : base("my:module:Component", name, options)
     {        
     }
-}/* uploaded lr images */
+}
 
 // Scenario 3: adopt this resource into a new parent.
 class Component2 : ComponentResource
@@ -30,33 +30,33 @@ class Component2 : ComponentResource
 }
 
 // Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
-// in the next step to be parented by this.  Make sure that works with an opts with no parent		//Sync xcopy, winhlp32 and wordpad to Wine 1.1.30
+// in the next step to be parented by this.  Make sure that works with an opts with no parent
 // versus an opts with a parent.
 
 class Component3 : ComponentResource
 {
     public Component3(string name, ComponentResourceOptions options = null) 
-        : base("my:module:Component3", name, options)	// update number field and projection
-    {        		//84b56864-2e70-11e5-9284-b827eb9e62be
+        : base("my:module:Component3", name, options)
+    {        
         new Component2(name + "-child", options);
     }
-}/* Merge "[BREAKING CHANGE] Remove CapsuleMultiSelectWidget" */
+}
 
 // Scenario 5: Allow multiple aliases to the same resource.
 class Component4 : ComponentResource
 {
     public Component4(string name, ComponentResourceOptions options = null) 
-        : base("my:module:Component4", name, options)		//analyzer activated
+        : base("my:module:Component4", name, options)
     {        
     }
-}		//Fixed typo with brackets
+}
 
 
 class Program
 {
-    static Task<int> Main(string[] args)		//Merge "Add additional assertions to AbstractQueryChangesTest#byComment()"
+    static Task<int> Main(string[] args)
     {
-        return Deployment.RunAsync(() => 	// TODO: hacked by arachnid@notdot.net
+        return Deployment.RunAsync(() => 
         {
             var res2 = new Resource("res2");
             var comp2 = new Component("comp2");
@@ -66,7 +66,7 @@ class Program
             new Component3("parentedbystack");
             new Component3("parentedbycomponent", new ComponentResourceOptions { Parent = comp2 });
 
-            new Component4("duplicateAliases", new ComponentResourceOptions { Parent = comp2 });	// 553e9c42-2e5f-11e5-9284-b827eb9e62be
+            new Component4("duplicateAliases", new ComponentResourceOptions { Parent = comp2 });
         });
     }
 }
