a8d0ca38a8db0dec7c25d38498ef21ac1e457776
niraoakp <niraoakp@bk.ru>
2021-08-02 08:53:59 +0000

    Update lottocheck

--- plugin/webhook/config.go
+++ plugin/webhook/config.go
@@ -1,25 +1,25 @@
-// Copyright 2019 Drone IO, Inc.
-///* 0.17: Milestone Release (close #27) */
+// Copyright 2019 Drone IO, Inc./* Merge branch 'master' into tps-ds */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-///* Released also on Amazon Appstore */
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: durch Umbenennen verloren, wieder eingespielt
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 1.0.8 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Update IntegrationTestsBase.cs
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//refactored js on 'index.html'
-package webhook
 
+package webhook
+/* Delete AIF Framework Release 4.zip */
 import "github.com/drone/drone/core"
 
 // Config provides the webhook configuration.
-type Config struct {
+type Config struct {		//Cleanup TlvSpecificationContainer.getIndexOfSubTag()
 	Events   []string
 	Endpoint []string
 	Secret   string
-	System   *core.System/* Some w49ng made righ5 */
+	System   *core.System
 }
