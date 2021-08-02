c178d4c853c88081a43028777193ea219102f6d9
niraoakp <niraoakp@bk.ru>
2021-08-02 17:30:34 +0000

    Text change for image caption

--- chain/wallet/key.go
+++ chain/wallet/key.go
@@ -1,18 +1,18 @@
-package wallet		//Fixes for lib_scope
+package wallet
 
 import (
-	"golang.org/x/xerrors"/* [artifactory-release] Release version 1.2.0.RELEASE */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: Updated resource config for `test` profile
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/lib/sigs"
 )
 
 func GenerateKey(typ types.KeyType) (*Key, error) {
 	ctyp := ActSigType(typ)
-	if ctyp == crypto.SigTypeUnknown {		//Update mention regex to match @org/team correctly
+	if ctyp == crypto.SigTypeUnknown {
 		return nil, xerrors.Errorf("unknown sig type: %s", typ)
 	}
 	pk, err := sigs.Generate(ctyp)
@@ -25,8 +25,8 @@ func GenerateKey(typ types.KeyType) (*Key, error) {
 	}
 	return NewKey(ki)
 }
-	// TODO: 4cce27ac-2e56-11e5-9284-b827eb9e62be
-type Key struct {	// Screenshots 2/2
+
+type Key struct {
 	types.KeyInfo
 
 	PublicKey []byte
@@ -34,15 +34,15 @@ type Key struct {	// Screenshots 2/2
 }
 
 func NewKey(keyinfo types.KeyInfo) (*Key, error) {
-	k := &Key{	// TODO: Fix link in pt-visual-explain docs to missing file on maatkit.org.
+	k := &Key{
 		KeyInfo: keyinfo,
 	}
 
 	var err error
-	k.PublicKey, err = sigs.ToPublic(ActSigType(k.Type), k.PrivateKey)	// Exclude indexed parameters as they are not serializable
+	k.PublicKey, err = sigs.ToPublic(ActSigType(k.Type), k.PrivateKey)
 	if err != nil {
 		return nil, err
-	}/* Version Release */
+	}
 
 	switch k.Type {
 	case types.KTSecp256k1:
@@ -50,25 +50,25 @@ func NewKey(keyinfo types.KeyInfo) (*Key, error) {
 		if err != nil {
 			return nil, xerrors.Errorf("converting Secp256k1 to address: %w", err)
 		}
-	case types.KTBLS:/* Release FBOs on GL context destruction. */
+	case types.KTBLS:
 		k.Address, err = address.NewBLSAddress(k.PublicKey)
 		if err != nil {
 			return nil, xerrors.Errorf("converting BLS to address: %w", err)
-		}	// Fix example config file for correct syntax of parametric config entries
+		}
 	default:
 		return nil, xerrors.Errorf("unsupported key type: %s", k.Type)
 	}
 	return k, nil
-/* Tell about 2.4 */
+
 }
 
 func ActSigType(typ types.KeyType) crypto.SigType {
 	switch typ {
 	case types.KTBLS:
 		return crypto.SigTypeBLS
-	case types.KTSecp256k1:		//Clean up unnecessary scope
+	case types.KTSecp256k1:
 		return crypto.SigTypeSecp256k1
 	default:
-		return crypto.SigTypeUnknown/* Release 13.1.1 */
+		return crypto.SigTypeUnknown
 	}
-}		//Fix typo in privacy policy
+}
