2363413980ee58908a9af4c8b245abc4d672a5d1
niraoakp <niraoakp@bk.ru>
2021-08-02 08:06:21 +0000

    Added baseclass for urlgeneration in expressionhandlers.

--- tests/integration/aliases/python/retype_component/step2/__main__.py
+++ tests/integration/aliases/python/retype_component/step2/__main__.py
@@ -1,29 +1,29 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* 0TqqzJrNrKUZ4R45h2mbOKftQ5Dam2qf */
 import copy
-
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE		//match crossover apps
-
+/* Release version [10.8.1] - alfter build */
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
+/* Few fixes. Release 0.95.031 and Laucher 0.34 */
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):	// Changed dockerfile
-        super().__init__("my:module:Resource", name, None, opts)	// TODO: hacked by ac0dem0nk3y@gmail.com
+    def __init__(self, name, opts=None):
+        super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #4 - change the type of a component
 class ComponentFour(ComponentResource):
-    def __init__(self, name, opts=ResourceOptions()):/* Delete options.mini.interior.json */
-        # Add an alias that references the old type of this resource...
+    def __init__(self, name, opts=ResourceOptions()):
+        # Add an alias that references the old type of this resource...	// fix 993 suche nach ressourcen eingebaut
         aliases = [Alias(type_="my:module:ComponentFour")]
         if opts.aliases is not None:
-            for alias in opts.aliases:	// New tests for the DM Manager
+            for alias in opts.aliases:
                 aliases.append(alias)
 
         # ..and then make the super call with the new type of this resource and the added alias.
         opts_copy = copy.copy(opts)
-        opts_copy.aliases = aliases		//Merge "defconfig: apq8084: Enable VPU device driver"
+        opts_copy.aliases = aliases	// Updated Playtype
         super().__init__("my:differentmodule:ComponentFourWithADifferentTypeName", name, None, opts_copy)
 
         # The child resource will also pick up an implicit alias due to the new type of the component it is parented
-        # to./* Merge "Update library versions after June 13 Release" into androidx-master-dev */
+        # to./* Released 1.5.2. */
         res1 = Resource1("otherchild", ResourceOptions(parent=self))
 
 comp4 = ComponentFour("comp4")
