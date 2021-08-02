855540b96c4e267ccd9c44ffc710be4b21cff385
niraoakp <niraoakp@bk.ru>
2021-08-02 05:29:07 +0000

    LoanAccountDisbursePage

--- tests/integration/stack_reference/go/step1/main.go
+++ tests/integration/stack_reference/go/step1/main.go
@@ -1,22 +1,22 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 package main
-		//New version, 5.1.25
+
 import (
 	"fmt"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* Release 2.6.0.6 */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
-)/* Adding an integration test for dealing with multiple files simulataneously */
-	// TODO: will be fixed by davidad@alum.mit.edu
+)
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
 		cfg := config.New(ctx, ctx.Project())
 
-		org := cfg.Require("org")	// Excluded config.local.neon from tests
+		org := cfg.Require("org")
 		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
-		stackRef, err := pulumi.NewStackReference(ctx, slug, nil)		//Interface.m: Move MoL aliased function declarations into MoL.m
+		stackRef, err := pulumi.NewStackReference(ctx, slug, nil)
 
 		if err != nil {
 			return fmt.Errorf("error reading stack reference: %v", err)
@@ -32,16 +32,16 @@ func main() {
 				errChan <- fmt.Errorf("invalid result")
 				return nil, fmt.Errorf("invalid result")
 			}
-			results <- v/* Order starts-with-whole-word before ends-with-whole-word. */
+			results <- v
 			return v, nil
 		})
 		ctx.Export("val2", pulumi.ToSecret(val))
-	// TODO: hacked by davidad@alum.mit.edu
+
 		select {
-		case err = <-errChan:		//create month_report with persisted event_source if demanded
+		case err = <-errChan:
 			return err
 		case <-results:
 			return nil
-}		
+		}
 	})
-}	// TODO: update camwhores, anon-v, camvideos, ps
+}
