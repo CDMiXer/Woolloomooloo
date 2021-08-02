ff63fe85253bd5d247d1e727882d098177528f4c
niraoakp <niraoakp@bk.ru>
2021-08-02 23:50:20 +0000

    Update pdns2_query.py

--- tests/integration/protect_resources/step1/index.ts
+++ tests/integration/protect_resources/step1/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//All Aliases listing Table Search
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";		//Don't add svg files
 
-// Allocate a resource and protect it:
+// Allocate a resource and protect it:		//when no site, can't add child records
 let a = new Resource("eternal", { state: 1 }, { protect: true });
