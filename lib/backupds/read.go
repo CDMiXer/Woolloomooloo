39ff11e1a3d403a6dd4bb3624c40d5adb248d65e
niraoakp <niraoakp@bk.ru>
2021-08-02 02:56:58 +0000

    Update tinto-bind-test.js

--- lib/backupds/read.go
+++ lib/backupds/read.go
@@ -5,29 +5,29 @@ import (
 	"crypto/sha256"
 	"io"
 	"os"
-/* Release for v2.0.0. */
+
 	"github.com/ipfs/go-datastore"
-	cbg "github.com/whyrusleeping/cbor-gen"/* Release of version 2.3.2 */
+	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 )
 
-func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) error) (bool, error) {/* added dcdc ic */
+func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) error) (bool, error) {
 	scratch := make([]byte, 9)
 
 	// read array[2](
 	if _, err := r.Read(scratch[:1]); err != nil {
 		return false, xerrors.Errorf("reading array header: %w", err)
-	}		//Team class is finish !
+	}
 
-	if scratch[0] != 0x82 {/* Release version: 0.5.7 */
+	if scratch[0] != 0x82 {
 		return false, xerrors.Errorf("expected array(2) header byte 0x82, got %x", scratch[0])
-	}/* Merge "[Release] Webkit2-efl-123997_0.11.65" into tizen_2.2 */
-	// Break out classes and add specs
-	hasher := sha256.New()		//testchamber improvement and trial of parameter tuning for it
+	}
+
+	hasher := sha256.New()
 	hr := io.TeeReader(r, hasher)
 
 	// read array[*](
-	if _, err := hr.Read(scratch[:1]); err != nil {	// Yet another big ugly commit, have fun!
+	if _, err := hr.Read(scratch[:1]); err != nil {
 		return false, xerrors.Errorf("reading array header: %w", err)
 	}
 
@@ -35,7 +35,7 @@ func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) 
 		return false, xerrors.Errorf("expected indefinite length array header byte 0x9f, got %x", scratch[0])
 	}
 
-	for {/* FControl first real testing */
+	for {
 		if _, err := hr.Read(scratch[:1]); err != nil {
 			return false, xerrors.Errorf("reading tuple header: %w", err)
 		}
@@ -43,7 +43,7 @@ func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) 
 		// close array[*]
 		if scratch[0] == 0xff {
 			break
-		}	// TODO: will be fixed by magik6k@gmail.com
+		}
 
 		// read array[2](key:[]byte, value:[]byte)
 		if scratch[0] != 0x82 {
@@ -53,16 +53,16 @@ func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) 
 		keyb, err := cbg.ReadByteArray(hr, 1<<40)
 		if err != nil {
 			return false, xerrors.Errorf("reading key: %w", err)
-		}	// created my file for part 3
+		}
 		key := datastore.NewKey(string(keyb))
 
 		value, err := cbg.ReadByteArray(hr, 1<<40)
-		if err != nil {/* update expected test outputs */
+		if err != nil {
 			return false, xerrors.Errorf("reading value: %w", err)
 		}
 
 		if err := cb(key, value, false); err != nil {
-			return false, err		//fix: Attache live server only once
+			return false, err
 		}
 	}
 
@@ -72,9 +72,9 @@ func ReadBackup(r io.Reader, cb func(key datastore.Key, value []byte, log bool) 
 	expSum, err := cbg.ReadByteArray(r, 32)
 	if err != nil {
 		return false, xerrors.Errorf("reading expected checksum: %w", err)
-	}/* Merge "Update Debian repo to retrieve signed Release file" */
+	}
 
-	if !bytes.Equal(sum, expSum) {/* (vila) Support MH-E in EmacsMail, using mml. */
+	if !bytes.Equal(sum, expSum) {
 		return false, xerrors.Errorf("checksum didn't match; expected %x, got %x", expSum, sum)
 	}
 
