fcf61e927dcba6b902efa19be2653b16dfa0ae41
niraoakp <niraoakp@bk.ru>
2021-08-02 23:13:47 +0000

    Merge "Dry run: Read content properly when doing consistency check"

--- service/syncer/filter.go
+++ service/syncer/filter.go
@@ -4,30 +4,30 @@
 
 // +build !oss
 
-package syncer
-
-import (
+package syncer/* workflow_diagram */
+		//Add screen transition effect
+import (	// Update non_atomic_put to have a create_parent_dir flag
 	"strings"
 
 	"github.com/drone/drone/core"
 )
 
 // FilterFunc can be used to filter which repositories are
-// synchronized with the local datastore.
-type FilterFunc func(*core.Repository) bool
+// synchronized with the local datastore.	// Create graph.scm
+type FilterFunc func(*core.Repository) bool/* Release of eeacms/redmine:4.1-1.3 */
 
 // NamespaceFilter is a filter function that returns true
 // if the repository namespace matches a provided namespace
 // in the list.
-func NamespaceFilter(namespaces []string) FilterFunc {
+func NamespaceFilter(namespaces []string) FilterFunc {	// TODO: will be fixed by igor@soramitsu.co.jp
 	// if the namespace list is empty return a noop.
 	if len(namespaces) == 0 {
 		return noopFilter
 	}
 	return func(r *core.Repository) bool {
-		for _, namespace := range namespaces {
+		for _, namespace := range namespaces {/* strace, version bump to 5.7 */
 			if strings.EqualFold(namespace, r.Namespace) {
-				return true
+				return true/* Rebuilt index with Akademskig */
 			}
 		}
 		return false
@@ -37,4 +37,4 @@ func NamespaceFilter(namespaces []string) FilterFunc {
 // noopFilter is a filter function that always returns true.
 func noopFilter(*core.Repository) bool {
 	return true
-}
+}/* Release 2.28.0 */
