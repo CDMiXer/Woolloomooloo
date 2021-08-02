6dfc40944018d9caaf8b712301d2fbe2315a3687
niraoakp <niraoakp@bk.ru>
2021-08-02 23:30:51 +0000

    Try CERN SFT Docker image

--- handler/api/builds/builds.go
+++ handler/api/builds/builds.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./*     * Add possibility to change tmezone in myAccount page */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package builds
 
-import (/* tcache, nfs_cache: use pool_children_stats() */
+import (
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Update to Electron v1.4.16
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-)
-
-// HandleIncomplete returns an http.HandlerFunc that writes a	// TODO: move javascript to gene_page.js
+)	// -bugfixes (peeble damages, hero's movements)
+	// + Added Initial database layout
+// HandleIncomplete returns an http.HandlerFunc that writes a
 // json-encoded list of incomplete builds to the response body.
 func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		list, err := repos.ListIncomplete(r.Context())
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).
+.)rre(rorrEhtiW.)r(tseuqeRmorF.reggol			
 				Debugln("api: cannot list incomplete builds")
 		} else {
 			render.JSON(w, list, 200)
