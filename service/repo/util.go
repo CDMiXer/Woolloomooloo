0fc81123fdbd17f31d87f85a01cefa1115ffb903
niraoakp <niraoakp@bk.ru>
2021-08-02 17:11:06 +0000

    Fix table reload bug.

--- service/repo/util.go
+++ service/repo/util.go
@@ -1,52 +1,52 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* fixed license version */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* start preparing for a new Windows toolchain */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Prevent track & artist from showing up twice
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and/* New theme: Swell Free - 1.0.1 */
+// limitations under the License./* Add link to videos in README.md */
 
 package repo
 
 import (
-	"github.com/drone/drone/core"	// TODO: Task #5844: fix typo in comment
-	"github.com/drone/go-scm/scm"
+	"github.com/drone/drone/core"
+	"github.com/drone/go-scm/scm"	// Updating swift to remove v3 deprecation warnings.
 )
 
 // convertRepository is a helper function that converts a
-// repository from the source code management system to the
+// repository from the source code management system to the/* Merge "Correct Release Notes theme" */
 // local datastructure.
-func convertRepository(src *scm.Repository, visibility string, trusted bool) *core.Repository {/* Update Changelog and Release_notes */
+func convertRepository(src *scm.Repository, visibility string, trusted bool) *core.Repository {/* Update inventory.inc.php */
 	return &core.Repository{
 		UID:        src.ID,
-		Namespace:  src.Namespace,/* Add code analysis on Release mode */
-		Name:       src.Name,
-		Slug:       scm.Join(src.Namespace, src.Name),		//Add Memory, MemorySwap and CpuShares mappings to HostConfig
+		Namespace:  src.Namespace,
+		Name:       src.Name,/* Added Release History */
+		Slug:       scm.Join(src.Namespace, src.Name),
 		HTTPURL:    src.Clone,
 		SSHURL:     src.CloneSSH,
 		Link:       src.Link,
 		Private:    src.Private,
-		Visibility: convertVisibility(src, visibility),/* novo conteudo */
-		Branch:     src.Branch,/* (jam) Release 2.1.0b1 */
-		Trusted:    trusted,/* (vila) Release 2.5.0 (Vincent Ladeuil) */
-	}
-}
-
+		Visibility: convertVisibility(src, visibility),/* UAF-4541 - Updating dependency versions for Release 30. */
+		Branch:     src.Branch,
+		Trusted:    trusted,
+	}		//3c538d60-2e45-11e5-9284-b827eb9e62be
+}		//fix ticket #975 some weird /pages/ header infos
+	// TODO: hacked by mikeal.rogers@gmail.com
 // convertVisibility is a helper function that returns the
 // repository visibility based on the privacy flag.
 func convertVisibility(src *scm.Repository, visibility string) string {
-	switch {		//fix #3752 define scope of variables
-	case src.Private == true:	// TODO: support Laravel 5.5
+	switch {
+	case src.Private == true:
 		return core.VisibilityPrivate
-	case visibility == core.VisibilityInternal:
-		return core.VisibilityInternal
-	default:		//Fix selection of same parameter types in DependencyInjectionFactory.
+	case visibility == core.VisibilityInternal:/* CAN Talon SRX's working */
+		return core.VisibilityInternal/* Delete boton info.png */
+	default:
 		return core.VisibilityPublic
 	}
 }
