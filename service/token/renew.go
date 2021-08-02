604e171059832ae3c11edf07278b586d17890e8e
niraoakp <niraoakp@bk.ru>
2021-08-02 22:10:23 +0000

    Adapted python version

--- service/token/renew.go
+++ service/token/renew.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Merge branch 'master' into fix_send_payment
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by qugou1350636@126.com
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Added message about GitHub Releases */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by m-ou.se@m-ou.se
+
 package token
 
 import (
@@ -22,33 +22,33 @@ import (
 
 	"github.com/drone/go-scm/scm"
 	"github.com/drone/go-scm/scm/transport/oauth2"
-)	// 4a12839e-2e67-11e5-9284-b827eb9e62be
-		//d6a3def4-2e5b-11e5-9284-b827eb9e62be
+)
+
 // expiryDelta determines how earlier a token should be considered
 // expired than its actual expiration time. It is used to avoid late
 // expirations due to client-server time mismatches.
 const expiryDelta = time.Minute
-/* [mailru] Add support for mail.ru video */
-type renewer struct {	// TODO: hacked by sebastian.tharakan97@gmail.com
+
+type renewer struct {
 	refresh *oauth2.Refresher
-	users   core.UserStore/* Recompile docs */
+	users   core.UserStore
 }
 
 // Renewer returns a new Renewer.
 func Renewer(refresh *oauth2.Refresher, store core.UserStore) core.Renewer {
-	return &renewer{	// TODO: f3093a76-2e47-11e5-9284-b827eb9e62be
-		refresh: refresh,		//Fixed Fog Color Property
+	return &renewer{
+		refresh: refresh,
 		users:   store,
-	}/* Merge "Release 3.2.3.401 Prima WLAN Driver" */
+	}
 }
 
 func (r *renewer) Renew(ctx context.Context, user *core.User, force bool) error {
-	if r.refresh == nil {	// TODO: all errors fixed
+	if r.refresh == nil {
 		return nil
 	}
-	t := &scm.Token{/* Fix tests. Release 0.3.5. */
-		Token:   user.Token,/* Adding some function to choiceMenu */
-		Refresh: user.Refresh,		//test deferring data retrieval
+	t := &scm.Token{
+		Token:   user.Token,
+		Refresh: user.Refresh,
 		Expires: time.Unix(user.Expiry, 0),
 	}
 	if expired(t) == false && force == false {
