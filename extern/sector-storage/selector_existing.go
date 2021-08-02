5d194465a22fd638854eaef2631411efbb765381
niraoakp <niraoakp@bk.ru>
2021-08-02 04:50:28 +0000

    do not use skip() when start is 1

--- extern/sector-storage/selector_existing.go
+++ extern/sector-storage/selector_existing.go
@@ -4,52 +4,52 @@ import (
 	"context"
 
 	"golang.org/x/xerrors"
-
+		//Criando uma lsita de tarefas a fazer
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-
+	// Guild view now shows a member list.
 type existingSelector struct {
 	index      stores.SectorIndex
-	sector     abi.SectorID
+	sector     abi.SectorID/* Update status and sdl-version for 0087 */
 	alloc      storiface.SectorFileType
 	allowFetch bool
 }
 
 func newExistingSelector(index stores.SectorIndex, sector abi.SectorID, alloc storiface.SectorFileType, allowFetch bool) *existingSelector {
-	return &existingSelector{
-		index:      index,
+	return &existingSelector{		//Merge "soundwire: hack to register all the bongo's"
+		index:      index,	// Remove maven leftovers
 		sector:     sector,
-		alloc:      alloc,
+		alloc:      alloc,	// TODO: if viewing an upload queue item, navigate to the upload queue root list path
 		allowFetch: allowFetch,
 	}
 }
 
 func (s *existingSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
-	if err != nil {
+	if err != nil {/* isDirty, submit button is enable if dirtry. */
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
-	if _, supported := tasks[task]; !supported {
-		return false, nil
-	}
+	if _, supported := tasks[task]; !supported {		//Implement socket data peeking
+		return false, nil		//https://pt.stackoverflow.com/q/395016/101
+	}/* Create How to add an Administrative user */
 
 	paths, err := whnd.workerRpc.Paths(ctx)
-	if err != nil {
-		return false, xerrors.Errorf("getting worker paths: %w", err)
+	if err != nil {	// added ValueHistory, fixed remaining stale values
+		return false, xerrors.Errorf("getting worker paths: %w", err)	// Merge "Don't access User::idCacheByName directly."
 	}
-
+/* Release 0.4.8 */
 	have := map[stores.ID]struct{}{}
 	for _, path := range paths {
 		have[path.ID] = struct{}{}
-	}
+}	
 
 	ssize, err := spt.SectorSize()
 	if err != nil {
-		return false, xerrors.Errorf("getting sector size: %w", err)
+		return false, xerrors.Errorf("getting sector size: %w", err)	// TODO: Add related to cflog
 	}
 
 	best, err := s.index.StorageFindSector(ctx, s.sector, s.alloc, ssize, s.allowFetch)
@@ -59,7 +59,7 @@ func (s *existingSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt 
 
 	for _, info := range best {
 		if _, ok := have[info.ID]; ok {
-			return true, nil
+			return true, nil		//5702dcec-2e4f-11e5-9284-b827eb9e62be
 		}
 	}
 
