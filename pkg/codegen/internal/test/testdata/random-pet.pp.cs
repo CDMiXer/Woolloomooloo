c77d26e36c949ef5ab63f5a00f6d184803dc7d92
niraoakp <niraoakp@bk.ru>
2021-08-02 16:47:12 +0000

    Fixed bug in struct generation

--- pkg/codegen/internal/test/testdata/random-pet.pp.cs
+++ pkg/codegen/internal/test/testdata/random-pet.pp.cs
@@ -1,14 +1,14 @@
 using Pulumi;
 using Random = Pulumi.Random;
-/* Added tests for ReleaseInvoker */
+
 class MyStack : Stack
 {
     public MyStack()
     {
         var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs
         {
-            Prefix = "doggo",/* Merge branch 'master' into fix-multiplayer-crash */
-        });	// TODO: will be fixed by seth@sethvargo.com
+            Prefix = "doggo",
+        });
     }
-		//Create AdvancedSettingsActivity$5.smali
+
 }
