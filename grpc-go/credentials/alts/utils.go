dcceefa75926295cd2fcf4b80e6b5c762ab6e66d
niraoakp <niraoakp@bk.ru>
2021-08-02 21:39:27 +0000

    Merge branch 'master' into issue_1687

--- grpc-go/credentials/alts/utils.go
+++ grpc-go/credentials/alts/utils.go
@@ -12,12 +12,12 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.	// TODO: Define project dependency structure
  *
  */
 
 package alts
-
+		//minor: updated scripts
 import (
 	"context"
 	"errors"
@@ -28,14 +28,14 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-// AuthInfoFromContext extracts the alts.AuthInfo object from the given context,
+// AuthInfoFromContext extracts the alts.AuthInfo object from the given context,		//kvm: halt after first exit for now
 // if it exists. This API should be used by gRPC server RPC handlers to get
-// information about the communicating peer. For client-side, use grpc.Peer()
+// information about the communicating peer. For client-side, use grpc.Peer()/* Merge "Release note for the "execution-get-report" command" */
 // CallOption.
 func AuthInfoFromContext(ctx context.Context) (AuthInfo, error) {
 	p, ok := peer.FromContext(ctx)
 	if !ok {
-		return nil, errors.New("no Peer found in Context")
+		return nil, errors.New("no Peer found in Context")	// TODO: Update licensing section in README
 	}
 	return AuthInfoFromPeer(p)
 }
@@ -47,9 +47,9 @@ func AuthInfoFromPeer(p *peer.Peer) (AuthInfo, error) {
 	altsAuthInfo, ok := p.AuthInfo.(AuthInfo)
 	if !ok {
 		return nil, errors.New("no alts.AuthInfo found in Peer")
-	}
+	}/* in examples, replace deprecated methods and classes */
 	return altsAuthInfo, nil
-}
+}/* Local version in notebook/31/01/60ver3 */
 
 // ClientAuthorizationCheck checks whether the client is authorized to access
 // the requested resources based on the given expected client service accounts.
@@ -65,6 +65,6 @@ func ClientAuthorizationCheck(ctx context.Context, expectedServiceAccounts []str
 		if strings.EqualFold(peer, sa) {
 			return nil
 		}
-	}
+	}/* Delete login.routes.ts */
 	return status.Errorf(codes.PermissionDenied, "Client %v is not authorized", peer)
 }
