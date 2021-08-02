cd938d08e6b25f49530f83961f7b4993e1941914
niraoakp <niraoakp@bk.ru>
2021-08-02 13:59:38 +0000

    Create blacklisted-ip-xml-rcp.txt

--- tests/integration/delete_before_create/step1/index.ts
+++ tests/integration/delete_before_create/step1/index.ts
@@ -8,12 +8,12 @@ const a = new Resource("base", { uniqueKey: 1, state: 42 });
 
 // Dependent depends on Base.
 const b = new Resource("dependent", { state: a.state });
-
+	// TODO: b3d3207c-2e49-11e5-9284-b827eb9e62be
 // Dependent-2 depends on Base and Dependent via dependsOn.
 const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });
 
 // Dependent-3 depends on Base and Dependent via a no-replace property.
 const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });
 
-// Dependent-4 depends on Dependent and Dependent-2 via a replace property.
+// Dependent-4 depends on Dependent and Dependent-2 via a replace property./* Release Notes for v01-15 */
 const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });
