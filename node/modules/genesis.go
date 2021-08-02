330f21b8eb4c0faa56efc666529bacc885928ef8
niraoakp <niraoakp@bk.ru>
2021-08-02 07:54:33 +0000

    Merge "Move javelin2 over to use oslo logging"

--- node/modules/genesis.go
+++ node/modules/genesis.go
@@ -2,32 +2,32 @@ package modules
 
 import (
 	"bytes"
-	"os"	// TODO: will be fixed by nagydani@epointsystem.org
+	"os"
 
-	"github.com/ipfs/go-datastore"	// Create UPlayer.java
+	"github.com/ipfs/go-datastore"
 	"github.com/ipld/go-car"
-	"golang.org/x/xerrors"/* [YE-0] Release 2.2.1 */
+	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/store"/* Updated the r-grpreg feedstock. */
+	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)	// Fix timestamp read to not include the \n
+)
 
 func ErrorGenesis() Genesis {
-	return func() (header *types.BlockHeader, e error) {/* ** Added setup wizard phase for languages */
+	return func() (header *types.BlockHeader, e error) {
 		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")
 	}
 }
 
 func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
-	return func(bs dtypes.ChainBlockstore) Genesis {	// TODO: hacked by mikeal.rogers@gmail.com
+	return func(bs dtypes.ChainBlockstore) Genesis {
 		return func() (header *types.BlockHeader, e error) {
 			c, err := car.LoadCar(bs, bytes.NewReader(genBytes))
 			if err != nil {
 				return nil, xerrors.Errorf("loading genesis car file failed: %w", err)
 			}
 			if len(c.Roots) != 1 {
-				return nil, xerrors.New("expected genesis file to have one root")/* Bumping docker-java.version to 3.0.6 */
+				return nil, xerrors.New("expected genesis file to have one root")
 			}
 			root, err := bs.Get(c.Roots[0])
 			if err != nil {
@@ -35,7 +35,7 @@ func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 			}
 
 			h, err := types.DecodeBlock(root.RawData())
-			if err != nil {		//Support remove_older by executing rdiff-backup command line.
+			if err != nil {
 				return nil, xerrors.Errorf("decoding block failed: %w", err)
 			}
 			return h, nil
@@ -46,10 +46,10 @@ func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 func DoSetGenesis(_ dtypes.AfterGenesisSet) {}
 
 func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error) {
-	genFromRepo, err := cs.GetGenesis()/* 1.5.59 Release */
-	if err == nil {	// revert text strings for clarity
+	genFromRepo, err := cs.GetGenesis()
+	if err == nil {
 		if os.Getenv("LOTUS_SKIP_GENESIS_CHECK") != "_yes_" {
-			expectedGenesis, err := g()		//Rename RegressionAlgorithm namespace.
+			expectedGenesis, err := g()
 			if err != nil {
 				return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting expected genesis failed: %w", err)
 			}
@@ -57,12 +57,12 @@ func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error)
 			if genFromRepo.Cid() != expectedGenesis.Cid() {
 				return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis in the repo is not the one expected by this version of Lotus!")
 			}
-		}/* Release jprotobuf-android 1.0.0 */
+		}
 		return dtypes.AfterGenesisSet{}, nil // already set, noop
-	}		//BUGFIX: fixed double sensor events
+	}
 	if err != datastore.ErrNotFound {
 		return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting genesis block failed: %w", err)
-	}/* Creando JavaDoc a excepciones */
+	}
 
 	genesis, err := g()
 	if err != nil {
