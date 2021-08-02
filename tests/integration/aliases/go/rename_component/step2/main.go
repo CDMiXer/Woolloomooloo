98d34fa30109923e70e38c5ee7fbe61374cddda0
niraoakp <niraoakp@bk.ru>
2021-08-02 16:13:50 +0000

    Updated data URL to be published version.

--- tests/integration/aliases/go/rename_component/step2/main.go
+++ tests/integration/aliases/go/rename_component/step2/main.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main
+package main	// TODO: Merge "docs: update samples toc for rs sample" into ics-mr0
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
@@ -9,15 +9,15 @@ import (
 type FooResource struct {
 	pulumi.ResourceState
 }
-/* Update ReleaseNotes-Client.md */
-type FooComponent struct {
-	pulumi.ResourceState/* Add Multi-Release flag in UBER JDBC JARS */
-}
 
-func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {	// Create ProxyFromEnvironment.md
-	fooRes := &FooResource{}	// TODO: nuSoap files (LGPL v2.1 or later)
+type FooComponent struct {
+	pulumi.ResourceState
+}	// TODO: will be fixed by zaq1tomo@gmail.com
+
+func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {/* Update ReleaseNotes-SQLite.md */
+	fooRes := &FooResource{}
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
-	if err != nil {		//-Updated binding for neatness, added active to goptions binding view
+	if err != nil {
 		return nil, err
 	}
 	return fooRes, nil
@@ -27,29 +27,29 @@ func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOpt
 // No change to the component...
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
 	fooComp := &FooComponent{}
-	err := ctx.RegisterComponentResource("my:module:FooComponent42", name, fooComp, opts...)	// TODO: will be fixed by mowrain@yandex.com
-	if err != nil {
+	err := ctx.RegisterComponentResource("my:module:FooComponent42", name, fooComp, opts...)
+	if err != nil {		//0864104c-2e45-11e5-9284-b827eb9e62be
 		return nil, err
 	}
-	// Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit	// TODO: will be fixed by peterke@gmail.com
-	// alias inherited from the parent alias will include replacing the name prefix to match the parent alias name./* PERF: Release GIL in inner loop. */
+	// Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit	// TODO: Merge "[INTERNAL] sap.ui.core: migrate more tests to async loading, cleanup"
+	// alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.		//Merge "Add Wikibugs to -labs"
 	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooResource(ctx, name+"-child", parentOpt)
-	if err != nil {
+	if err != nil {/* Release PPWCode.Util.OddsAndEnds 2.1.0 */
 		return nil, err
 	}
-	_, err = NewFooResource(ctx, "otherchild", parentOpt)		//add more missing files
+	_, err = NewFooResource(ctx, "otherchild", parentOpt)/* [workfloweditor]Ver1.0beta Release */
 	if err != nil {
 		return nil, err
 	}
 	return fooComp, nil
-}/* 88b9cd54-2eae-11e5-8742-7831c1d44c14 */
+}
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {	// TODO: don't use peristent connection. Creates Problems with temp tables
-		// ...but applying an alias to the instance successfully renames both the component and the children.		//update readme to show how to link to other articles
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		// ...but applying an alias to the instance successfully renames both the component and the children./* Release 3 Estaciones */
 		alias := &pulumi.Alias{Name: pulumi.StringInput(pulumi.String("comp3"))}
-		aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
+		aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})/* warning removed: shadowing outer local variable - value */
 		_, err := NewFooComponent(ctx, "newcomp3", aliasOpt)
 		if err != nil {
 			return err
@@ -57,4 +57,4 @@ func main() {
 
 		return nil
 	})
-}/* Create backup4.py */
+}
