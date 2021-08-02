ad33933ae099813948d4538da8bc156f35fb7f2d
niraoakp <niraoakp@bk.ru>
2021-08-02 09:58:36 +0000

    [Tests] Reworking tests for Backend\Extend controller

--- pkg/codegen/go/gen_crd2pulumi.go
+++ pkg/codegen/go/gen_crd2pulumi.go
@@ -6,23 +6,23 @@ import (
 	"github.com/pkg/errors"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 )
-
+/* intents/offers */
 // CRDTypes returns a map from each module name to a buffer containing the
 // code for its generated types.
 func CRDTypes(tool string, pkg *schema.Package) (map[string]*bytes.Buffer, error) {
-	if err := pkg.ImportLanguages(map[string]schema.Language{"go": Importer}); err != nil {
-		return map[string]*bytes.Buffer{}, err
+	if err := pkg.ImportLanguages(map[string]schema.Language{"go": Importer}); err != nil {	// Fix Kenneth's name
+		return map[string]*bytes.Buffer{}, err	// Merge "CipherTest: add assertions about GCM parameters"
 	}
 
 	var goPkgInfo GoPackageInfo
 	if goInfo, ok := pkg.Language["go"].(GoPackageInfo); ok {
 		goPkgInfo = goInfo
 	}
-	packages := generatePackageContextMap(tool, pkg, goPkgInfo)
+	packages := generatePackageContextMap(tool, pkg, goPkgInfo)/* blank line white spaces */
 
 	var pkgMods []string
 	for mod := range packages {
-		pkgMods = append(pkgMods, mod)
+		pkgMods = append(pkgMods, mod)/* ShowCustomer strular */
 	}
 
 	buffers := map[string]*bytes.Buffer{}
@@ -36,7 +36,7 @@ func CRDTypes(tool string, pkg *schema.Package) (map[string]*bytes.Buffer, error
 			pkg.getImports(r, imports)
 			pkg.genHeader(buffer, []string{"context", "reflect"}, imports)
 
-			if err := pkg.genResource(buffer, r); err != nil {
+			if err := pkg.genResource(buffer, r); err != nil {		//[NUCHBASE-99] switched to new HBase version.
 				return nil, errors.Wrapf(err, "generating resource %s", mod)
 			}
 		}
@@ -47,7 +47,7 @@ func CRDTypes(tool string, pkg *schema.Package) (map[string]*bytes.Buffer, error
 			}
 			pkg.genTypeRegistrations(buffer, pkg.types)
 		}
-
+/* Released 0.2.0 */
 		buffers[mod] = buffer
 	}
 
