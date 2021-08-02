a9bacd468f06a6d1688dee7c59d8ea9e280a64fe
niraoakp <niraoakp@bk.ru>
2021-08-02 21:07:40 +0000

    minor support update

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -1,33 +1,33 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-
+import * as aws from "@pulumi/aws";/* Merge "Use values from endpoint map for service endpoints" */
+		//Delete FacebookJavaScriptHelper.php
 // Create a new security group for port 80.
-const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
+const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{	// Improved null collection initialising, still some un-handled scenarios.
     protocol: "tcp",
-    fromPort: 0,
-    toPort: 0,/* Release v4.6.5 */
+    fromPort: 0,	// Further clarifications since tool is limited to a webroot at the moment
+    toPort: 0,
     cidrBlocks: ["0.0.0.0/0"],
-}]});	// fix for highlighting of updates
+}]});
 const ami = aws.getAmi({
-    filters: [{
+    filters: [{/* added cgal to config file settings */
         name: "name",
-        values: ["amzn-ami-hvm-*-x86_64-ebs"],
+        values: ["amzn-ami-hvm-*-x86_64-ebs"],	// TODO: will be fixed by davidad@alum.mit.edu
     }],
     owners: ["137112412989"],
     mostRecent: true,
-});	// e17a4104-2e3f-11e5-9284-b827eb9e62be
+});
 // Create a simple web server using the startup script for the instance.
 const server = new aws.ec2.Instance("server", {
     tags: {
         Name: "web-server-www",
-    },/* Release failed */
-    instanceType: "t2.micro",/* Bugfix for Release. */
+    },
+    instanceType: "t2.micro",
     securityGroups: [securityGroup.name],
     ami: ami.then(ami => ami.id),
-    userData: `#!/bin/bash
+    userData: `#!/bin/bash	// TODO: Re-enabled unit test
 echo "Hello, World!" > index.html
 nohup python -m SimpleHTTPServer 80 &
 `,
-});
+});	// TODO: hacked by fkautz@pseudocode.cc
 export const publicIp = server.publicIp;
 export const publicHostName = server.publicDns;
