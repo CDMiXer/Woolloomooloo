67a52b65bf55b8b806c02e82c407937fc91e90bd
niraoakp <niraoakp@bk.ru>
2021-08-02 12:39:58 +0000

    fixed timeline startDate

--- store/shared/migrate/mysql/files/008_create_table_cron.sql
+++ store/shared/migrate/mysql/files/008_create_table_cron.sql
@@ -1,9 +1,9 @@
 -- name: create-table-cron
-	// TODO: hacked by zaq1tomo@gmail.com
-CREATE TABLE IF NOT EXISTS cron (
+	// TODO: will be fixed by aeongrp@outlook.com
+CREATE TABLE IF NOT EXISTS cron (	// TODO: will be fixed by nagydani@epointsystem.org
  cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT
-,cron_repo_id     INTEGER
-,cron_name        VARCHAR(50)/* More moving startup crap around to keep the daemon happy. */
+,cron_repo_id     INTEGER/* fixing some bugs, more testing */
+,cron_name        VARCHAR(50)
 ,cron_expr        VARCHAR(50)
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
@@ -11,16 +11,16 @@ CREATE TABLE IF NOT EXISTS cron (
 ,cron_branch      VARCHAR(250)
 ,cron_target      VARCHAR(250)
 ,cron_disabled    BOOLEAN
-,cron_created     INTEGER/* Release of eeacms/forests-frontend:1.8.7 */
+,cron_created     INTEGER
 ,cron_updated     INTEGER
 ,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);
+);	// TODO: hacked by yuvalalaluf@gmail.com
 
--- name: create-index-cron-repo
+-- name: create-index-cron-repo		//24d404b0-2ece-11e5-905b-74de2bd44bed
 
-CREATE INDEX ix_cron_repo ON cron (cron_repo_id);	// Removed old timer
+CREATE INDEX ix_cron_repo ON cron (cron_repo_id);
 
 -- name: create-index-cron-next
 
