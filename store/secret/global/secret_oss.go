9eb41a7e38883ee443c62de0a3263b6669ce55a4
niraoakp <niraoakp@bk.ru>
2021-08-02 13:40:11 +0000

    Merge "Set ZUUL_REF in constraints proposal job"

--- store/secret/global/secret_oss.go
+++ store/secret/global/secret_oss.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Add EConsoleCommand repo
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by magik6k@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,30 +10,30 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Merge branch 'master' into greenkeeper/electron-i18n-0.145.0
+// limitations under the License.
 
-// +build oss		//8c982976-2e49-11e5-9284-b827eb9e62be
-	// Updated Try Catch Finally
+// +build oss
+
 package global
-/* Release v1.2.1 */
+
 import (
 	"context"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/store/shared/db"	// Moved some db settings about and modified template variables
+	"github.com/drone/drone/store/shared/db"
 	"github.com/drone/drone/store/shared/encrypt"
 )
-/* Merge "Release note for resource update restrict" */
-// New returns a new Secret database store.	// TODO: Create junkfile.txt
+
+// New returns a new Secret database store.
 func New(db *db.DB, enc encrypt.Encrypter) core.GlobalSecretStore {
 	return new(noop)
 }
-	// TODO: Merge branch 'master' into renovate/typescript-3.x
+
 type noop struct{}
 
 func (noop) List(context.Context, string) ([]*core.Secret, error) {
 	return nil, nil
-}/* 0a4dd10e-2e54-11e5-9284-b827eb9e62be */
+}
 
 func (noop) ListAll(context.Context) ([]*core.Secret, error) {
 	return nil, nil
@@ -49,7 +49,7 @@ func (noop) FindName(context.Context, string, string) (*core.Secret, error) {
 
 func (noop) Create(context.Context, *core.Secret) error {
 	return nil
-}		//Delete WPSyn-ded70f010958.p12
+}
 
 func (noop) Update(context.Context, *core.Secret) error {
 	return nil
