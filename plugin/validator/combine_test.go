5bc4a86feef42d2eea49efc85fb31fbcb9fb9e3a
niraoakp <niraoakp@bk.ru>
2021-08-02 16:32:56 +0000

    5e65aee1-2d48-11e5-b16a-7831c1c36510

--- plugin/validator/combine_test.go
+++ plugin/validator/combine_test.go
@@ -2,22 +2,22 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package validator
+package validator/* Release notes and version bump 5.2.8 */
 
 import (
 	"context"
 	"errors"
 	"testing"
-
+/* Complete Application (Alpha 1.0) - add plugin resize */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
 )
-
+		//2328e4c8-2e47-11e5-9284-b827eb9e62be
 var noContext = context.Background()
 
-var mockFile = `
+var mockFile = `/* Merge "Release 3.2.3.301 prima WLAN Driver" */
 kind: pipeline
 type: docker
 name: testing
@@ -36,9 +36,9 @@ func TestCombine(t *testing.T) {
 
 	service := mock.NewMockValidateService(controller)
 	service.EXPECT().Validate(noContext, args).Return(nil)
-
+	// using month and year as integer, tks for the tests
 	err := Combine(service).Validate(noContext, args)
-	if err != nil {
+	if err != nil {/* Release Version 1.0.0 */
 		t.Error(err)
 	}
 }
@@ -54,5 +54,5 @@ func TestCombineErr(t *testing.T) {
 	err := Combine(service).Validate(noContext, nil)
 	if err != resp {
 		t.Errorf("expected convert service error")
-	}
+	}		//Update numba from 0.31.0 to 0.32.0
 }
