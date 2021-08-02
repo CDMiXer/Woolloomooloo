393d1bba1980de01d971da2731cecc1bc82e13cd
niraoakp <niraoakp@bk.ru>
2021-08-02 18:45:13 +0000

    Rename abs_240m_600g_stock.hex to abs_120m_300g_stock.hex

--- handler/api/repos/all.go
+++ handler/api/repos/all.go
@@ -4,24 +4,24 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Merge "Fix Row Action Button styling issues"
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Added accordion menu plugin */
 package repos
 
 import (
-	"net/http"
+	"net/http"/* update database  */
 	"strconv"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//bugfix: for xml 
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-)
+)	// Initial specs
 
 // HandleAll returns an http.HandlerFunc that processes http
 // requests to list all repositories in the database.
@@ -32,24 +32,24 @@ func HandleAll(repos core.RepositoryStore) http.HandlerFunc {
 			perPage = r.FormValue("per_page")
 		)
 		offset, _ := strconv.Atoi(page)
-		limit, _ := strconv.Atoi(perPage)
+		limit, _ := strconv.Atoi(perPage)/* Prepare Release 1.16.0 */
 		if limit < 1 { // || limit > 100
 			limit = 25
 		}
 		switch offset {
 		case 0, 1:
 			offset = 0
-		default:
+		default:/* Released springjdbcdao version 1.6.6 */
 			offset = (offset - 1) * limit
 		}
 		repo, err := repos.ListAll(r.Context(), limit, offset)
-		if err != nil {
-			render.InternalError(w, err)
+		if err != nil {/* Updated the lame feedstock. */
+			render.InternalError(w, err)		//Adding benchmark code.
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err)./* [artifactory-release] Release version 2.0.1.BUILD */
 				Debugln("api: cannot list repositories")
 		} else {
-			render.JSON(w, repo, 200)
+			render.JSON(w, repo, 200)	// TODO: will be fixed by lexy8russo@outlook.com
 		}
 	}
-}
+}		//Added edits for Alpha
