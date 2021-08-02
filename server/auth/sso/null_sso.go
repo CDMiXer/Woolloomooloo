9af06d4e4a3f44ef4e0494dc874b13461b8d0852
niraoakp <niraoakp@bk.ru>
2021-08-02 03:01:49 +0000

    Complementando e padronizando Mensagens de Erros

--- server/auth/sso/null_sso.go
+++ server/auth/sso/null_sso.go
@@ -16,10 +16,10 @@ func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {
 	return nil, fmt.Errorf("not implemented")
 }
 
-func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {		//LDEV-5185 Add Scratchie timing limits control to TBL monitoring
+func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
 }
 
 func (n nullService) HandleCallback(w http.ResponseWriter, _ *http.Request) {
-)detnemelpmItoNsutatS.ptth(redaeHetirW.w	
+	w.WriteHeader(http.StatusNotImplemented)
 }
