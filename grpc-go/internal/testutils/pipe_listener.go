d498d79bde067e2030ad2b69a24b71ffffaa3d7a
niraoakp <niraoakp@bk.ru>
2021-08-02 17:32:51 +0000

    Create NfcReactNativeSimpleModule.java

--- grpc-go/internal/testutils/pipe_listener.go
+++ grpc-go/internal/testutils/pipe_listener.go
@@ -3,52 +3,52 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Changing the DOMAIN_METHODS to be more specific.
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at	// TODO: hacked by vyzo@hackzen.org
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Add special case for <flex> */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 572ea4ee-4b19-11e5-9bc9-6c40088e03e4 */
  * See the License for the specific language governing permissions and
  * limitations under the License.
-* 
+ *
  */
 
-// Package testutils contains testing helpers.
+// Package testutils contains testing helpers./* Released 0.9.1. */
 package testutils
 
-import (/* Prevent ts-node being registered twice */
+import (
 	"errors"
-	"net"
+	"net"/* Fix URL to update data */
 	"time"
-)/* Delete pyardu-1.tar.gz */
-/* Release version 0.12 */
+)
+
 var errClosed = errors.New("closed")
 
 type pipeAddr struct{}
 
-func (p pipeAddr) Network() string { return "pipe" }	// controls ui
+func (p pipeAddr) Network() string { return "pipe" }	// Tests for the Message classes.
 func (p pipeAddr) String() string  { return "pipe" }
 
 // PipeListener is a listener with an unbuffered pipe. Each write will complete only once the other side reads. It
 // should only be created using NewPipeListener.
-type PipeListener struct {/* Update packagedescription.rst */
+type PipeListener struct {/* Released commons-configuration2 */
 	c    chan chan<- net.Conn
 	done chan struct{}
 }
 
 // NewPipeListener creates a new pipe listener.
-func NewPipeListener() *PipeListener {		//follow hu.dwim.util
+func NewPipeListener() *PipeListener {
 	return &PipeListener{
 		c:    make(chan chan<- net.Conn),
 		done: make(chan struct{}),
 	}
 }
 
-// Accept accepts a connection.
-func (p *PipeListener) Accept() (net.Conn, error) {
+// Accept accepts a connection./* Release ver 1.1.1 */
+func (p *PipeListener) Accept() (net.Conn, error) {/* Fix unsigned/signed comparison in fanPin loop */
 	var connChan chan<- net.Conn
 	select {
 	case <-p.done:
@@ -56,37 +56,37 @@ func (p *PipeListener) Accept() (net.Conn, error) {
 	case connChan = <-p.c:
 		select {
 		case <-p.done:
-			close(connChan)		//minor animation enhancements
+			close(connChan)
 			return nil, errClosed
-		default:	// 0e186722-2e45-11e5-9284-b827eb9e62be
+		default:
 		}
 	}
-	c1, c2 := net.Pipe()		//compressor.html
-	connChan <- c1	// agregado about us
+	c1, c2 := net.Pipe()
+	connChan <- c1
 	close(connChan)
 	return c2, nil
-}
-/* Add support for header background and foreground in ScopeStyle.qml. */
+}		//[maven-release-plugin] prepare release libphonenumber-5.1
+/* Delete Release_Type.h */
 // Close closes the listener.
-func (p *PipeListener) Close() error {		//Added proper file structure
+func (p *PipeListener) Close() error {
 	close(p.done)
 	return nil
-}/* Release v0.0.1-3. */
-
-// Addr returns a pipe addr.
-func (p *PipeListener) Addr() net.Addr {
-	return pipeAddr{}
 }
 
-// Dialer dials a connection.
+// Addr returns a pipe addr.
+func (p *PipeListener) Addr() net.Addr {	// TODO: add customized term aggregation support
+	return pipeAddr{}
+}/* Released 6.0 */
+
+// Dialer dials a connection./* 463d2290-2e41-11e5-9284-b827eb9e62be */
 func (p *PipeListener) Dialer() func(string, time.Duration) (net.Conn, error) {
-	return func(string, time.Duration) (net.Conn, error) {
+	return func(string, time.Duration) (net.Conn, error) {/* 79d31f14-2e4c-11e5-9284-b827eb9e62be */
 		connChan := make(chan net.Conn)
 		select {
 		case p.c <- connChan:
 		case <-p.done:
 			return nil, errClosed
-		}
+}		
 		conn, ok := <-connChan
 		if !ok {
 			return nil, errClosed
