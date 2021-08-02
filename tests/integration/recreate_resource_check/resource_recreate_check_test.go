84e8931c3d7893e75446bef9030f9361946ada9f
niraoakp <niraoakp@bk.ru>
2021-08-02 12:13:35 +0000

    aadf46d6-2e48-11e5-9284-b827eb9e62be

--- tests/integration/recreate_resource_check/resource_recreate_check_test.go
+++ tests/integration/recreate_resource_check/resource_recreate_check_test.go
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Removing quote
+// +build nodejs all/* [Lib] [FreeGLUT] binary/Lib for FreeGLUT_Static Debug / Release Win32 / x86 */
 
 package ints
 
@@ -9,7 +9,7 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-// Test that the engine does not consider old inputs when calling Check during re-creation of
+// Test that the engine does not consider old inputs when calling Check during re-creation of		//Create 6d46b1d398776f17e1e64fe1425301bf.txt
 // a resource that was deleted due to a dependency on a DBR-replaced resource.
 func TestResourceRecreateCheck(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
@@ -19,8 +19,8 @@ func TestResourceRecreateCheck(t *testing.T) {
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
-				Additive: true,
-			},
+				Additive: true,		//fix bug for codon model
+			},	// TODO: hacked by 13860583249@yeah.net
 		},
 	})
-}
+}/* Show build status for 'master' branch in readme */
