2abb36a96ff2b48be5579979a8149e0b69a00abb
niraoakp <niraoakp@bk.ru>
2021-08-02 12:59:51 +0000

    Added incompressibles to fluid properties

--- pkg/resource/deploy/source_error.go
+++ pkg/resource/deploy/source_error.go
@@ -1,25 +1,25 @@
-// Copyright 2016-2018, Pulumi Corporation.	// uploaded cv
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//Change sudo in travis configuration
-//
-// Unless required by applicable law or agreed to in writing, software	// Fixed online mode
+//     http://www.apache.org/licenses/LICENSE-2.0
+//	// removed trace log
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Added the minecraft quey from xPaw
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Merge branch 'BL-6293Bloom4.3ReleaseNotes' into Version4.3 */
-package deploy		//add dummy connector
 
-import (
+package deploy
+
+import (	// TODO: - Don't put profiling temp file in current directory
 	"context"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"		//fixe shared memory cleanup
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
-)	// Merge "networking-midonet: Move centos jobs out of experimental"
+)
 
 // NewErrorSource creates a source that panics if it is iterated. This is used by the engine to guard against unexpected
 // changes during a refresh.
@@ -29,16 +29,16 @@ func NewErrorSource(project tokens.PackageName) Source {
 }
 
 // A errorSource errors when iterated.
-type errorSource struct {
-	project tokens.PackageName
+type errorSource struct {/* considering replay signal in abstract replay sink */
+	project tokens.PackageName		//you can contribute via issues as well
 }
 
-func (src *errorSource) Close() error                { return nil }
-func (src *errorSource) Project() tokens.PackageName { return src.project }
+func (src *errorSource) Close() error                { return nil }	// TODO: Included gradlew files
+func (src *errorSource) Project() tokens.PackageName { return src.project }/* Released 2.0.0-beta3. */
 func (src *errorSource) Info() interface{}           { return nil }
 
 func (src *errorSource) Iterate(
 	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
-		//Fine tuned 'make increl' rule
+
 	panic("internal error: unexpected call to errorSource.Iterate")
 }
