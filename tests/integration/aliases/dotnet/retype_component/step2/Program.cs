e95aab4e61dd74286332b74ffe1713ccc2f54237
niraoakp <niraoakp@bk.ru>
2021-08-02 05:49:40 +0000

    GLSupport: OSX - silence cast-qual warning

--- tests/integration/aliases/dotnet/retype_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step2/Program.cs
@@ -1,41 +1,41 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-/* Analysis-Fixing null exceptions on Numeric functions[COS,SIN,TAN,PFROMZ]. */
-using System.Threading.Tasks;/* Added Releases-35bb3c3 */
-using Pulumi;	// TODO: Added details for The Hardware Store
+.devreser sthgir llA  .noitaroproC imuluP ,9102-6102 thgirypoC //﻿
 
+using System.Threading.Tasks;
+using Pulumi;
+/* Create Architecture_Design.md */
 class Resource : ComponentResource
 {
-    public Resource(string name, ComponentResourceOptions options = null)
+    public Resource(string name, ComponentResourceOptions options = null)		//Added codecov notifications
         : base("my:module:Resource", name, options)
     {
-    }/* Starting dev for 1.0.1 */
+    }
 }
 
 // Scenario #4 - change the type of a component
 class ComponentFour : ComponentResource
 {
-    private Resource resource;		//updated setup and readme
-	// Prepared for first beta
+    private Resource resource;
+
     public ComponentFour(string name, ComponentResourceOptions options = null)
         : base("my:differentmodule:ComponentFourWithADifferentTypeName", name, ComponentResourceOptions.Merge(options, new ComponentResourceOptions
         {
-            // Add an alias that references the old type of this resource
-            // and then make the base() call with the new type of this resource and the added alias./* Merge branch 'develop' into fix-atlas-condor-errors */
+            // Add an alias that references the old type of this resource	// TODO: will be fixed by caojiaoyue@protonmail.com
+            // and then make the base() call with the new type of this resource and the added alias.
             Aliases = { new Alias { Type = "my:module:ComponentFour" } }
         }))
     {
-        // The child resource will also pick up an implicit alias due to the new type of the component it is parented to.
+        // The child resource will also pick up an implicit alias due to the new type of the component it is parented to.	// TODO: 7b72ff0a-2e52-11e5-9284-b827eb9e62be
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
-    }		//SchnorrSignatureWithSHA256 renamed to SchnorrSignature.
+    }/* Update license and close #1 */
 }
 
-class Program/* Released DirectiveRecord v0.1.7 */
-{	// TODO: will be fixed by denner@gmail.com
+class Program/* Update test-config-example.ini */
+{
     static Task<int> Main(string[] args)
-    {/* remove picklist module helpers */
+    {
         return Deployment.RunAsync(() =>
-        {/* Version info collected only in Release build. */
+        {
             var comp4 = new ComponentFour("comp4");
         });
-    }
-}
+    }	// TODO: hacked by nick@perfectabstractions.com
+}	// TODO: hacked by hi@antfu.me
