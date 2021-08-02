e197b4c4c07ca22fd5179679335f5d691ed1ad92
niraoakp <niraoakp@bk.ru>
2021-08-02 08:17:58 +0000

    Typo Haha-Banach > Hahn-Banach

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -1,14 +1,14 @@
-import * as pulumi from "@pulumi/pulumi";/* Release v0.6.5 */
-/* Fix rule 081. */
-export const normal = pulumi.output("normal");	// TODO: will be fixed by cory@protocol.ai
+import * as pulumi from "@pulumi/pulumi";
+
+export const normal = pulumi.output("normal");
 export const secret = pulumi.secret("secret");
 
 // Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
 // deployment.
 const org = new pulumi.Config().require("org");
-const project = pulumi.getProject();/* Release 0.17.0 */
+const project = pulumi.getProject();
 const stack = pulumi.getStack();
 const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
-	// TODO: will be fixed by ng8eke@163.com
-export const refNormal = sr.getOutput("normal");	// TODO: Merge "[FUEL-177] fix horizon ordering"
+
+export const refNormal = sr.getOutput("normal");
 export const refSecret = sr.getOutput("secret");
