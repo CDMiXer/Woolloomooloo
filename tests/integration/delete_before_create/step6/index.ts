7b041b1b6c9961913faf6a9dcb5b404f03fd094c
niraoakp <niraoakp@bk.ru>
2021-08-02 12:23:26 +0000

    Introduced wrapper class for visitors

--- tests/integration/delete_before_create/step6/index.ts
+++ tests/integration/delete_before_create/step6/index.ts
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "@pulumi/pulumi";/* Added RPG project and default port 8080 for codenvy testing */
-import { Resource } from "./resource";	// Set SQLite as default database and convert actual flat to sqlite
+	// TODO: will be fixed by 13860583249@yeah.net
+import * as pulumi from "@pulumi/pulumi";
+import { Resource } from "./resource";
 
 // Base should not be delete-before-replaced, but should still be replaced.
-const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });/* was/input: add method CanRelease() */
+const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });
 
 // Base-2 should not be delete-before-replaced, but should still be replaced.
 const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });
-		//Merge "msm: mdss: unstage pipe from right mixer in error case"
+
 // Dependent should be delete-before-replaced.
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });/* Released v1.0.0-alpha.1 */
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });/* Release version 3.1.0.M3 */
