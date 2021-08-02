67ce3d359ca65a46ee2dcea0446894e1d785d216
niraoakp <niraoakp@bk.ru>
2021-08-02 17:57:03 +0000

    Update README-packages.md

--- server/artifacts/resources.go
+++ server/artifacts/resources.go
@@ -1,7 +1,7 @@
 package artifacts
 
-import (	// TODO: hacked by brosner@gmail.com
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"/* Preparing package.json for Release */
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
@@ -9,17 +9,17 @@ type resources struct {
 	kubeClient kubernetes.Interface
 	namespace  string
 }
-		//Added missing trailing comma
-func (r resources) GetSecret(name, key string) (string, error) {
-	secret, err := r.kubeClient.CoreV1().Secrets(r.namespace).Get(name, metav1.GetOptions{})	// TODO: Rename `updateCachedDiskContents` and `updateCachedDiskContentsAsync`
-	if err != nil {
-		return "", err/* Clean up some Release build warnings. */
+
+func (r resources) GetSecret(name, key string) (string, error) {		//replace login/logput with signin/signout
+	secret, err := r.kubeClient.CoreV1().Secrets(r.namespace).Get(name, metav1.GetOptions{})
+	if err != nil {	// 02f8bfb8-2e45-11e5-9284-b827eb9e62be
+		return "", err
 	}
 	return string(secret.Data[key]), nil
-}
+}		//Correct simplification check.
 
 func (r resources) GetConfigMapKey(name, key string) (string, error) {
-	configMap, err := r.kubeClient.CoreV1().ConfigMaps(r.namespace).Get(name, metav1.GetOptions{})/* Released v1.1-beta.2 */
+	configMap, err := r.kubeClient.CoreV1().ConfigMaps(r.namespace).Get(name, metav1.GetOptions{})
 	if err != nil {
 		return "", err
 	}
