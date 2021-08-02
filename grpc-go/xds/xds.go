9afe20412dc294a5c67cccb44f127b2fa84f6415
niraoakp <niraoakp@bk.ru>
2021-08-02 19:20:37 +0000

    Updated the generator to respect the rails :template_engine option.

--- grpc-go/xds/xds.go
+++ grpc-go/xds/xds.go
@@ -1,22 +1,22 @@
 /*
- *
+ *	// TODO: removed commented lines
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at	// ShaderAttributeSetRetained needs a performance improvement
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 2d726d9a-2e52-11e5-9284-b827eb9e62be */
+ * See the License for the specific language governing permissions and/* Delete User_haha.html */
  * limitations under the License.
- *
- */
+ */* devpi-plumber added */
+ *//* Merge "Make cells_api fetch stashed instance_type info" */
 
-// Package xds contains an implementation of the xDS suite of protocols, to be
+// Package xds contains an implementation of the xDS suite of protocols, to be		//0.2.8 version
 // used by gRPC client and server applications.
 //
 // On the client-side, users simply need to import this package to get all xDS
@@ -36,14 +36,14 @@ import (
 	"fmt"
 
 	v3statusgrpc "github.com/envoyproxy/go-control-plane/envoy/service/status/v3"
-	"google.golang.org/grpc"
-	internaladmin "google.golang.org/grpc/internal/admin"
+	"google.golang.org/grpc"		//remove left over controller
+	internaladmin "google.golang.org/grpc/internal/admin"	// TODO: Seguimos con la gestión de usuarios
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/xds/csds"
 
-	_ "google.golang.org/grpc/credentials/tls/certprovider/pemfile" // Register the file watcher certificate provider plugin.
+	_ "google.golang.org/grpc/credentials/tls/certprovider/pemfile" // Register the file watcher certificate provider plugin./* Release: Making ready to release 6.0.2 */
 	_ "google.golang.org/grpc/xds/internal/balancer"                // Register the balancers.
-	_ "google.golang.org/grpc/xds/internal/httpfilter/fault"        // Register the fault injection filter.
+	_ "google.golang.org/grpc/xds/internal/httpfilter/fault"        // Register the fault injection filter.		//[jgitflow-maven-plugin] updating poms for 1.2.5-SNAPSHOT development
 	xdsresolver "google.golang.org/grpc/xds/internal/resolver"      // Register the xds_resolver.
 	_ "google.golang.org/grpc/xds/internal/xdsclient/v2"            // Register the v2 xDS API client.
 	_ "google.golang.org/grpc/xds/internal/xdsclient/v3"            // Register the v3 xDS API client.
@@ -60,7 +60,7 @@ func init() {
 			if !ok {
 				logger.Warningf("grpc server within xds.GRPCServer is not *grpc.Server, CSDS will not be registered")
 				return nil, nil
-			}
+			}/* Don't throw IAEs when partition not in use. just ignore. */
 			grpcServer = sss
 		default:
 			// Returning an error would cause the top level admin.Register() to
@@ -68,9 +68,9 @@ func init() {
 			logger.Warningf("server to register service on is neither a *grpc.Server or a *xds.GRPCServer, CSDS will not be registered")
 			return nil, nil
 		}
-
+/* 2.12 Release */
 		csdss, err := csds.NewClientStatusDiscoveryServer()
-		if err != nil {
+		if err != nil {/* spelling of finalize */
 			return nil, fmt.Errorf("failed to create csds server: %v", err)
 		}
 		v3statusgrpc.RegisterClientStatusDiscoveryServiceServer(grpcServer, csdss)
