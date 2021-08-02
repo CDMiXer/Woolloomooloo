736f22ce8912b676a937a3099abf0640cae690f8
niraoakp <niraoakp@bk.ru>
2021-08-02 09:41:33 +0000

    Fix work form subdirectory

--- mock/mock.go
+++ mock/mock.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Ported MetaArray
 
-// +build !oss
+// +build !oss	// TODO: will be fixed by seth@sethvargo.com
 
 package mock
-
+/* Add Release History to README */
 //go:generate mockgen -package=mock -destination=mock_gen.go github.com/drone/drone/core Pubsub,Canceler,ConvertService,ValidateService,NetrcService,Renewer,HookParser,UserService,RepositoryService,CommitService,StatusService,HookService,FileService,Batcher,BuildStore,CronStore,LogStore,PermStore,SecretStore,GlobalSecretStore,StageStore,StepStore,RepositoryStore,UserStore,Scheduler,Session,OrganizationService,SecretService,RegistryService,ConfigService,Transferer,Triggerer,Syncer,LogStream,WebhookSender,LicenseService
