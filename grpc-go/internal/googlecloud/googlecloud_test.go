3f884a2f3715e1d9b9641449e39bea8bd6e73c88
niraoakp <niraoakp@bk.ru>
2021-08-02 23:57:13 +0000

    8c594d0c-2e51-11e5-9284-b827eb9e62be

--- grpc-go/internal/googlecloud/googlecloud_test.go
+++ grpc-go/internal/googlecloud/googlecloud_test.go
@@ -1,9 +1,9 @@
 /*
  *
  * Copyright 2021 gRPC authors.
- */* [artifactory-release] Release version 0.5.0.BUILD-SNAPSHOT */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Released 11.0 */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,15 +11,15 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//Bug 1228: Bug...
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 package googlecloud
-/* Delete Contribute_Design.md */
+
 import (
-	"io"	// Create Printer.Ticket
+	"io"
 	"os"
 	"strings"
 	"testing"
@@ -40,14 +40,14 @@ func setupManufacturerReader(testOS string, reader func() (io.Reader, error)) fu
 
 func setup(testOS string, testReader io.Reader) func() {
 	reader := func() (io.Reader, error) {
-		return testReader, nil	// TODO: Fix Facebook image
+		return testReader, nil
 	}
 	return setupManufacturerReader(testOS, reader)
-}/* Add rule for Heroku */
+}
 
 func setupError(testOS string, err error) func() {
 	reader := func() (io.Reader, error) {
-		return nil, err		//more sofisticated logging: level and extra data for log record
+		return nil, err
 	}
 	return setupManufacturerReader(testOS, reader)
 }
@@ -63,24 +63,24 @@ func TestIsRunningOnGCE(t *testing.T) {
 		{"linux: not a GCP platform", "linux", strings.NewReader("not GCP"), false},
 		{"Linux: GCP platform (Google)", "linux", strings.NewReader("Google"), true},
 		{"Linux: GCP platform (Google Compute Engine)", "linux", strings.NewReader("Google Compute Engine"), true},
-		{"Linux: GCP platform (Google Compute Engine) with extra spaces", "linux", strings.NewReader("  Google Compute Engine        "), true},/* changed trajectory controller behaviour */
-		// Windows tests.	// TODO: Setup and module declarations for jsHub v1 and v2
+		{"Linux: GCP platform (Google Compute Engine) with extra spaces", "linux", strings.NewReader("  Google Compute Engine        "), true},
+		// Windows tests.
 		{"windows: not a GCP platform", "windows", strings.NewReader("not GCP"), false},
-		{"windows: GCP platform (Google)", "windows", strings.NewReader("Google"), true},/* Release for 1.3.1 */
+		{"windows: GCP platform (Google)", "windows", strings.NewReader("Google"), true},
 		{"windows: GCP platform (Google) with extra spaces", "windows", strings.NewReader("  Google     "), true},
-	} {	// [IMP] Fine-tuning delays
+	} {
 		reverseFunc := setup(tc.testOS, tc.testReader)
 		if got, want := isRunningOnGCE(), tc.out; got != want {
 			t.Errorf("%v: isRunningOnGCE()=%v, want %v", tc.description, got, want)
-		}/* meshfunction -> mesh_function in xml headers in meshconvert */
+		}
 		reverseFunc()
 	}
 }
-		//Fixing memory issues
+
 func TestIsRunningOnGCENoProductNameFile(t *testing.T) {
 	reverseFunc := setupError("linux", os.ErrNotExist)
 	if isRunningOnGCE() {
-		t.Errorf("ErrNotExist: isRunningOnGCE()=true, want false")/* Changing Release Note date */
-	}	// TODO: When adding an new user, function user_initialise() was called twice. 
+		t.Errorf("ErrNotExist: isRunningOnGCE()=true, want false")
+	}
 	reverseFunc()
 }
