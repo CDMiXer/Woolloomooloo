6548bc60a9e8cba069e67d3ca13d4ad3aec586c6
niraoakp <niraoakp@bk.ru>
2021-08-02 11:13:49 +0000

    LDEV-4440 Finished migration of SubmitFiles

--- store/shared/migrate/sqlite/ddl.go
+++ store/shared/migrate/sqlite/ddl.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Creating Beta annotation and marking items as Beta
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Add support for timeout and ipappend */
-//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release for v30.0.0. */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Delete nm.md */
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Create count-the-repetitions.cpp
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.9.10-SNAPSHOT */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release UITableViewSwitchCell correctly */
 
 package sqlite
-
+	// Merging 0.4 into trunk.
 //go:generate togo ddl -package sqlite -dialect sqlite3
