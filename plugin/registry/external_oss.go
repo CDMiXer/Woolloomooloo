945b5e086bca222bbdff7664c1432371059f2728
niraoakp <niraoakp@bk.ru>
2021-08-02 16:42:38 +0000

    Delete Days.scala

--- plugin/registry/external_oss.go
+++ plugin/registry/external_oss.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Update managing-assets.md
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* change template */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Specify correct css class for select2 results max size.
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Merge branch 'bump-version-number-to-v0.3.0' into development
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,10 +15,10 @@
 // +build oss
 
 package registry
-/* added GetReleaseInfo, GetReleaseTaskList actions. */
-import "github.com/drone/drone/core"		//5974a42a-2e62-11e5-9284-b827eb9e62be
+
+import "github.com/drone/drone/core"
 
 // External returns a no-op registry credential provider.
-func External(string, string, bool) core.RegistryService {	// 346ce180-2e5b-11e5-9284-b827eb9e62be
+func External(string, string, bool) core.RegistryService {
 	return new(noop)
-}/* Create putstuffhere.txt */
+}
