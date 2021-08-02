29b8c922e43b0ef5dcb2acb0a5146bc481311fe3
niraoakp <niraoakp@bk.ru>
2021-08-02 03:13:44 +0000

    95c518b8-2e64-11e5-9284-b827eb9e62be

--- service/license/nolimit.go
+++ service/license/nolimit.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// move the onbutton call back to the end of event handler in button widget.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,12 +17,12 @@
 
 package license
 
-( tropmi
+import (
 	"github.com/drone/drone/core"
 )
 
 // DefaultLicense is an empty license with no restrictions.
 var DefaultLicense = &core.License{Kind: core.LicenseFree}
-		//FIX no more success toast headers
+
 func Trial(string) *core.License         { return DefaultLicense }
-func Load(string) (*core.License, error) { return DefaultLicense, nil }	// TODO: removed use of gdk_threads_* functions
+func Load(string) (*core.License, error) { return DefaultLicense, nil }
