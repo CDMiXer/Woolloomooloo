88e35fa3d885c46224ce2d2ad6b9d815437f422f
niraoakp <niraoakp@bk.ru>
2021-08-02 03:16:20 +0000

    Added checks for svn props and cytoscape-gpml to daily build

--- tests/integration/aliases/python/adopt_into_component/step1/__main__.py
+++ tests/integration/aliases/python/adopt_into_component/step1/__main__.py
@@ -3,7 +3,7 @@
 from pulumi import ComponentResource, export, Resource, ResourceOptions
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):/* liquid createOrder type fix #7131 */
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
 
@@ -11,26 +11,26 @@ class Resource1(ComponentResource):
 class Component1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Component", name, None, opts)
-/* (lifeless) Release 2.1.2. (Robert Collins) */
+
 res2 = Resource1("res2")
 comp2 = Component1("comp2")
 
 # Scenario 3: adopt this resource into a new parent.
-class Component2(ComponentResource):	// TODO: hacked by mikeal.rogers@gmail.com
+class Component2(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Component2", name, None, opts)
 
-unparented_comp2 = Component2("unparented")/* @Release [io7m-jcanephora-0.28.0] */
+unparented_comp2 = Component2("unparented")
 
-# Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix/* Merge branch 'master' into messagingavatar */
-# in the next step to be parented by this.  Make sure that works with an opts with no parent		//Cleaned up and standardized comments on fields
+# Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
+# in the next step to be parented by this.  Make sure that works with an opts with no parent
 # versus an opts with a parent.
 
 class Component3(ComponentResource):
-    def __init__(self, name, opts=None):/* Fix spelling typo in docs */
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Component3", name, None, opts)
-        mycomp2 = Component2(name + "-child", opts)	// TODO: will be fixed by mail@bitpshr.net
-		//Format coreos readme
+        mycomp2 = Component2(name + "-child", opts)
+
 parented_by_stack_comp3 = Component3("parentedbystack")
 parented_by_component_comp3 = Component3("parentedbycomponent", ResourceOptions(parent=comp2))
 
