4d01c113d478748e3b11ea63d26a3d01e7f7e683
niraoakp <niraoakp@bk.ru>
2021-08-02 20:58:19 +0000

    doinf project contact with select2

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -1,5 +1,5 @@
 package main
-/* 9abd0e6e-2e61-11e5-9284-b827eb9e62be */
+
 import (
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
@@ -11,14 +11,14 @@ import (
 var cases = map[string]interface{}{
 	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
-	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),	// TODO: hacked by mikeal.rogers@gmail.com
+	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
 	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
-	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),	// TODO: rev 526325
+	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
 	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
 }
 
 func main() {
 	sanityCheck()
-/* Added make MODE=DebugSanitizer clean and make MODE=Release clean commands */
+
 	run.InvokeMap(cases)
 }
