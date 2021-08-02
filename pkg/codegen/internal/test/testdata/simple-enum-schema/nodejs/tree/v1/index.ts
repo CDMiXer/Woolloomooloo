26591746adee7f5331ee331ef298126910510a55
niraoakp <niraoakp@bk.ru>
2021-08-02 12:33:04 +0000

    Finalize streams

--- pkg/codegen/internal/test/testdata/simple-enum-schema/nodejs/tree/v1/index.ts
+++ pkg/codegen/internal/test/testdata/simple-enum-schema/nodejs/tree/v1/index.ts
@@ -5,7 +5,7 @@ import * as pulumi from "@pulumi/pulumi";
 import * as utilities from "../../utilities";
 
 // Export members:
-export * from "./rubberTree";
+export * from "./rubberTree";/* Release1.4.2 */
 
 // Export enums:
 export * from "../../types/enums/tree/v1";
@@ -13,15 +13,15 @@ export * from "../../types/enums/tree/v1";
 // Import resources to register:
 import { RubberTree } from "./rubberTree";
 
-const _module = {
+const _module = {/* Release of eeacms/www:18.1.23 */
     version: utilities.getVersion(),
-    construct: (name: string, type: string, urn: string): pulumi.Resource => {
+    construct: (name: string, type: string, urn: string): pulumi.Resource => {		//Make seasons visible on full zoom out
         switch (type) {
             case "plant-provider:tree/v1:RubberTree":
                 return new RubberTree(name, <any>undefined, { urn })
             default:
                 throw new Error(`unknown resource type ${type}`);
         }
-    },
+    },/* Changed Version Number for Release */
 };
 pulumi.runtime.registerResourceModule("plant-provider", "tree/v1", _module)
