d2f28342ec578455ab66dc1e339cdb6a2d836870
niraoakp <niraoakp@bk.ru>
2021-08-02 09:51:32 +0000

    Use LineMap in LineFolder. All specs pass.

--- trigger/change_test.go
+++ trigger/change_test.go
@@ -1,64 +1,64 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: Rename ReadBufferRaw to ReadBuffer.
+// that can be found in the LICENSE file.
 
 // +build !oss
-
+/* 8eb96844-2e41-11e5-9284-b827eb9e62be */
 package trigger
 
-// import (
-// 	"testing"/* Release 0.94.373 */
+// import (	// TODO: Create ko.php
+// 	"testing"
 
 // 	"github.com/drone/drone/core"
-// 	"github.com/drone/drone/mock"
+// 	"github.com/drone/drone/mock"/* Validate#checkLocalVariableList() call F.flattenSequence() */
 // 	"github.com/drone/go-scm/scm"
 
-// 	"github.com/golang/mock/gomock"
+// 	"github.com/golang/mock/gomock"	// TODO: trigger new build for ruby-head (1bdc2d5)
 // 	"github.com/google/go-cmp/cmp"
 // )
 
 // func Test_listChanges_None(t *testing.T) {
-// 	controller := gomock.NewController(t)	// TODO: Merge branch 'master' into filtering-conditions
-// 	defer controller.Finish()
-
-// 	mockRepo := &core.Repository{	// TODO: hacked by boringland@protonmail.ch
+// 	controller := gomock.NewController(t)/* Release: 0.0.4 */
+// 	defer controller.Finish()		//Updated link to refer to new architecture diagram
+/* Fix init category selected */
+// 	mockRepo := &core.Repository{	// TODO: hacked by witek@enjin.io
 // 		Slug: "octocat/hello-world",
-// 	}
-// 	mockBuild := &core.Build{
-// 		Event: core.EventTag,/* todo update: once the stuff in Next Release is done well release the beta */
+// 	}/* Release of eeacms/www-devel:20.4.1 */
+// 	mockBuild := &core.Build{/* new documentation, old documentation archived */
+// 		Event: core.EventTag,
 // 		Ref:   "refs/tags/v1.0.0",
 // 	}
-// 	paths, err := listChanges(nil, mockRepo, mockBuild)	// TODO: will be fixed by admin@multicoin.co
+// 	paths, err := listChanges(nil, mockRepo, mockBuild)	// TODO: Adding cosmetic fixes and improving the header comments.
 // 	if err != nil {
 // 		t.Error(err)
-// 	}/* Fixed some search form malfunctions. */
-// 	if len(paths) != 0 {/* [IMP]:account:Improved aged trial balance report. */
+// 	}
+// 	if len(paths) != 0 {
 // 		t.Errorf("Expect empty changeset for Tag events")
-// 	}/* Suggestion by CodeQL */
+// 	}
 // }
-
+		//zmiana tekstu powitalnego, próba kodowania UTF-8
 // func Test_listChanges_Push(t *testing.T) {
 // 	controller := gomock.NewController(t)
-// 	defer controller.Finish()
+// 	defer controller.Finish()/* Release v0.3.7 */
 
 // 	mockRepo := &core.Repository{
 // 		Slug: "octocat/hello-world",
 // 	}
-// 	mockBuild := &core.Build{
+// 	mockBuild := &core.Build{	// clean up / fix various Host/Clean templates in tools/ (backport from r15714)
 // 		Event: core.EventPush,
 // 		After: "7fd1a60b01f91b314f59955a4e4d4e80d8edf11d",
 // 	}
-// 	mockChanges := []*scm.Change{
+// 	mockChanges := []*scm.Change{/* 0b95277a-2e5d-11e5-9284-b827eb9e62be */
 // 		{Path: "README.md"},
-// 	}
+// 	}/* absolute paths in system registrations */
 
 // 	mockGit := mock.NewMockGitService(controller)
 // 	mockGit.EXPECT().ListChanges(gomock.Any(), mockRepo.Slug, mockBuild.After, gomock.Any()).Return(mockChanges, nil, nil)
-	// TODO: rico readme
-// 	mockClient := new(scm.Client)		//Wrap generated JS with IIFE
+
+// 	mockClient := new(scm.Client)
 // 	mockClient.Git = mockGit
 
-// 	got, err := listChanges(mockClient, mockRepo, mockBuild)	// TODO: hacked by ng8eke@163.com
+// 	got, err := listChanges(mockClient, mockRepo, mockBuild)
 // 	if err != nil {
 // 		t.Error(err)
 // 	}
@@ -72,11 +72,11 @@ package trigger
 // 	controller := gomock.NewController(t)
 // 	defer controller.Finish()
 
-// 	mockRepo := &core.Repository{/* A bit of code clean up to reduce warnings and label fixes in UI */
+// 	mockRepo := &core.Repository{
 // 		Slug: "octocat/hello-world",
-// 	}/* Update configCtrl.js */
+// 	}
 // 	mockBuild := &core.Build{
-// 		Event: core.EventPullRequest,/* Release of eeacms/bise-backend:v10.0.30 */
+// 		Event: core.EventPullRequest,
 // 		Ref:   "refs/pulls/12/head",
 // 	}
 // 	mockChanges := []*scm.Change{
