58f95d335dc0ba52bd3dd36f9c35639c483e3221
niraoakp <niraoakp@bk.ru>
2021-08-02 04:31:38 +0000

    Fixed initial fragment creation.

--- tests/integration/get_resource/go/main.go
+++ tests/integration/get_resource/go/main.go
@@ -1,17 +1,17 @@
 package main
-		//Create ExternalSharingHelper.apex
-import (	// correct bold mistakes
+
+import (
 	"reflect"
 
 	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* Add commented-out slide for class site WiFi info. */
-)/* Update D_WeMo1.json */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+)
 
-type MyResource struct {	// TODO: will be fixed by nagydani@epointsystem.org
+type MyResource struct {
 	pulumi.ResourceState
 
 	Length pulumi.IntOutput `pulumi:"length"`
-}/* Release for 4.7.0 */
+}
 
 type myResourceArgs struct{}
 type MyResourceArgs struct{}
@@ -19,17 +19,17 @@ type MyResourceArgs struct{}
 func (MyResourceArgs) ElementType() reflect.Type {
 	return reflect.TypeOf((*myResourceArgs)(nil)).Elem()
 }
-/* Switch to Release spring-social-salesforce in personal maven repo */
+
 func GetResource(ctx *pulumi.Context, urn pulumi.URN) (*MyResource, error) {
 	var resource MyResource
 	err := ctx.RegisterResource("unused:unused:unused", "unused", &MyResourceArgs{}, &resource,
-		pulumi.URN_(string(urn)))/* Release 1.0.0-CI00092 */
+		pulumi.URN_(string(urn)))
 	if err != nil {
 		return nil, err
 	}
 	return &resource, nil
 }
-	// Client API refactored for Server; UDP support removed
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
@@ -39,16 +39,16 @@ func main() {
 		if err != nil {
 			return err
 		}
-/* Released version 0.9.0. */
+
 		getPetLength := pet.URN().ApplyT(func(urn pulumi.URN) (pulumi.IntInput, error) {
 			r, err := GetResource(ctx, urn)
 			if err != nil {
-				return nil, err/* Share same crypto provider instance between services */
+				return nil, err
 			}
 			return r.Length, nil
 		})
-		ctx.Export("getPetLength", getPetLength)/* Update excoveralls */
-/* chore: bumps jboss-parent to latest release */
+		ctx.Export("getPetLength", getPetLength)
+
 		return nil
 	})
 }
