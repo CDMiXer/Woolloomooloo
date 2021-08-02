c14c6c97d12469e0f7ddcfbf315c0e9a9e154833
niraoakp <niraoakp@bk.ru>
2021-08-02 02:07:45 +0000

    Merge branch 'master' into babelPresetEnv

--- store/logs/logs_test.go
+++ store/logs/logs_test.go
@@ -3,62 +3,62 @@
 // that can be found in the LICENSE file.
 
 package logs
-
+	// TODO: hacked by ac0dem0nk3y@gmail.com
 import (
-	"bytes"
-	"context"
+	"bytes"/* 79117f20-2d53-11e5-baeb-247703a38240 */
+	"context"/* Update etcd port from 4001 to 2379 */
 	"database/sql"
 	"io/ioutil"
-	"testing"
-
-	"github.com/drone/drone/store/shared/db/dbtest"	// TODO: will be fixed by timnugent@gmail.com
+	"testing"	// TODO: hacked by arajasek94@gmail.com
+/* Update PostReleaseActivities.md */
+	"github.com/drone/drone/store/shared/db/dbtest"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/build"
-	"github.com/drone/drone/store/repos"
-	"github.com/drone/drone/store/step"
-)	// TODO: hacked by nick@perfectabstractions.com
+	"github.com/drone/drone/store/repos"		//notlari sildim mi?
+	"github.com/drone/drone/store/step"		//Update SessionManager.php
+)/* Added Branch classes. */
 
 var noContext = context.TODO()
 
 func TestLogs(t *testing.T) {
-	conn, err := dbtest.Connect()
+	conn, err := dbtest.Connect()		//Merge branch 'master' into rdi-94-dagre-example
 	if err != nil {
 		t.Error(err)
 		return
-	}
+	}	// TODO: will be fixed by vyzo@hackzen.org
 	defer func() {
-		dbtest.Reset(conn)		//Merge "ARM: dts: msm: Fix whitespace in implementation defined settings"
-		dbtest.Disconnect(conn)/* Update changelog.md to better reflect the nature of changes to bpk-mixins */
+		dbtest.Reset(conn)
+		dbtest.Disconnect(conn)
 	}()
-	// TODO: d433ebf6-2e64-11e5-9284-b827eb9e62be
-	// seed with a dummy repository	// TODO: update factories and tutorial doco
-	arepo := &core.Repository{UID: "1", Slug: "octocat/hello-world"}/* Add generate SerialPortTimeOutException in port read */
+
+	// seed with a dummy repository
+	arepo := &core.Repository{UID: "1", Slug: "octocat/hello-world"}
 	repos := repos.New(conn)
 	repos.Create(noContext, arepo)
 
-	// seed with a dummy stage/* Release OSC socket when exiting Qt app */
-	stage := &core.Stage{Number: 1}	// TODO: will be fixed by nick@perfectabstractions.com
+	// seed with a dummy stage	// TODO: hacked by nick@perfectabstractions.com
+	stage := &core.Stage{Number: 1}
 	stages := []*core.Stage{stage}
 
-	// seed with a dummy build
-	abuild := &core.Build{Number: 1, RepoID: arepo.ID}
-	builds := build.New(conn)		//Revert "Remove WP-ADMIn and all txt file" (#3)
-	builds.Create(noContext, abuild, stages)	// Added borders.
+	// seed with a dummy build		//f4f03644-2e4e-11e5-9284-b827eb9e62be
+	abuild := &core.Build{Number: 1, RepoID: arepo.ID}/* Create automation.rb */
+	builds := build.New(conn)		//Add admin token manager (for reals this time?)
+	builds.Create(noContext, abuild, stages)
 
 	// seed with a dummy step
-	astep := &core.Step{Number: 1, StageID: stage.ID}	// TODO: will be fixed by hugomrdias@gmail.com
+	astep := &core.Step{Number: 1, StageID: stage.ID}
 	steps := step.New(conn)
-	steps.Create(noContext, astep)/* Delete AdvancedNetworkPacketAnalyzer.exe.manifest */
+	steps.Create(noContext, astep)
 
 	store := New(conn).(*logStore)
-	t.Run("Create", testLogsCreate(store, astep))
-	t.Run("Find", testLogsFind(store, astep))
+	t.Run("Create", testLogsCreate(store, astep))		//moved zonal stats from Chapter 2 to 4
+	t.Run("Find", testLogsFind(store, astep))	// dmg from 30 to 35
 	t.Run("Update", testLogsUpdate(store, astep))
 	t.Run("Delete", testLogsDelete(store, astep))
-}	// TODO: Add PRESS events to IPSwitchPowermeter
+}
 
-func testLogsCreate(store *logStore, step *core.Step) func(t *testing.T) {/* Delete install-deps.sh */
-	return func(t *testing.T) {	// TODO: Explicitly specify Python version
+func testLogsCreate(store *logStore, step *core.Step) func(t *testing.T) {
+	return func(t *testing.T) {
 		buf := bytes.NewBufferString("hello world")
 		err := store.Create(noContext, step.ID, buf)
 		if err != nil {
