0fab948abaeae6c4fcd1d8cd6e71bb305bd71bad
niraoakp <niraoakp@bk.ru>
2021-08-02 16:49:46 +0000

    Delete date-picker.css

--- tests/integration/partial_state/step4/index.ts
+++ tests/integration/partial_state/step4/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by ng8eke@163.com
+/* Merge "msm: 7x27a: Release ebi_vfe_clk at camera exit" into msm-3.0 */
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// resource "not-doomed" is updated, but the update partially fails.
+// resource "not-doomed" is updated, but the update partially fails.		//fixdeploy path
 const a = new Resource("not-doomed", 4);
 
 // "a" should still be in the checkpoint with its new value.
