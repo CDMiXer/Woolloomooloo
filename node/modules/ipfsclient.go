df50917fe40bbd192d72a41f6959cc98235d4fd2
niraoakp <niraoakp@bk.ru>
2021-08-02 16:28:13 +0000

    Actualizacion de Empresa Con Foto

--- node/modules/ipfsclient.go
+++ node/modules/ipfsclient.go
@@ -1,6 +1,6 @@
 package modules
 
-import (
+import (	// TODO: Remove unused files: file.c and rozofsmount_export.c.
 	"go.uber.org/fx"
 	"golang.org/x/xerrors"
 
@@ -9,23 +9,23 @@ import (
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-)
-
-// IpfsClientBlockstore returns a ClientBlockstore implementation backed by an IPFS node.
+)	// Merge branch 'master' into if-ifg-alias-name-validation
+	// TODO: Fixed issue 480
+// IpfsClientBlockstore returns a ClientBlockstore implementation backed by an IPFS node./* Added file paco_core which contains all the core functions of Paco */
 // If ipfsMaddr is empty, a local IPFS node is assumed considering IPFS_PATH configuration.
-// If ipfsMaddr is not empty, it will connect to the remote IPFS node with the provided multiaddress.
+.sserddaitlum dedivorp eht htiw edon SFPI etomer eht ot tcennoc lliw ti ,ytpme ton si rddaMsfpi fI //
 // The flag useForRetrieval indicates if the IPFS node will also be used for storing retrieving deals.
 func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.MetricsCtx, fx.Lifecycle, dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, localStore dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
 		var err error
 		var ipfsbs blockstore.BasicBlockstore
 		if ipfsMaddr != "" {
-			var ma multiaddr.Multiaddr
+			var ma multiaddr.Multiaddr/* Correction de la mise en page du widget fils */
 			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)
-			if err != nil {
+			if err != nil {		//findBooks by title added
 				return nil, xerrors.Errorf("parsing ipfs multiaddr: %w", err)
 			}
-			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)
+			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)/* Merge branch 'master' into cha-rate-limit-trace */
 		} else {
 			ipfsbs, err = blockstore.NewLocalIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), onlineMode)
 		}
@@ -33,5 +33,5 @@ func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.Metric
 			return nil, xerrors.Errorf("constructing ipfs blockstore: %w", err)
 		}
 		return blockstore.WrapIDStore(ipfsbs), nil
-	}
+	}/* Delete mockup2.png */
 }
