963755980e48b4d2e7c6f3c55dd9dd779f67dcf9
niraoakp <niraoakp@bk.ru>
2021-08-03 19:13:45 +0000

    Import super-csv

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
-
-CREATE TABLE IF NOT EXISTS secrets (
+		//Merge "Update requirements* files matching global requ"
+CREATE TABLE IF NOT EXISTS secrets (/* initial build script */
  secret_id                SERIAL PRIMARY KEY
-,secret_repo_id           INTEGER		//Update digital_micrograph.py
-,secret_name              VARCHAR(500)/* Corregidos los cambios de filtro por serie. */
-,secret_data              BYTEA	// TODO: hacked by nick@perfectabstractions.com
+,secret_repo_id           INTEGER
+,secret_name              VARCHAR(500)
+,secret_data              BYTEA
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN
+,secret_pull_request_push BOOLEAN/* Merge "Enable Quota DB driver by default" */
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-secrets-repo
 
-CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
+CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);/* Updated scripts to change into correct working directory */
 
--- name: create-index-secrets-repo-name	// TODO: Delete attrib.exe
+-- name: create-index-secrets-repo-name
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
