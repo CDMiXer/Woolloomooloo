ec0d72e8aac1adf46ff8b3de96b17b7c4d350243
niraoakp <niraoakp@bk.ru>
2021-08-03 20:41:28 +0000

    86faa98c-2e47-11e5-9284-b827eb9e62be

--- tests/integration/read/import_acquire/import_acquire_test.go
+++ tests/integration/read/import_acquire/import_acquire_test.go
@@ -1,16 +1,16 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints
+package ints/* BulkLoaderClient now logs server-side errors at ERROR level, not INFO. */
 
 import (
 	"testing"
-
+	// TODO: Added Concerns::Initializable
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 // Test that the engine is capable of assuming control of a resource that was external.
-func TestImportAcquire(t *testing.T) {
+func TestImportAcquire(t *testing.T) {/* Release 0.4--validateAndThrow(). */
 	t.Skipf("import does not yet work with dynamic providers")
 
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
@@ -20,8 +20,8 @@ func TestImportAcquire(t *testing.T) {
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
-				Additive: true,
+				Additive: true,/* Oops forgot to delete this shit */
 			},
-		},
-	})
+		},	// Create configServer.md
+	})	// fix twig version range
 }
