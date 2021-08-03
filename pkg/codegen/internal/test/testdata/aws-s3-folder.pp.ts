2ae273da0afade59daad670e5364404b9e30a441
niraoakp <niraoakp@bk.ru>
2021-08-03 15:25:34 +0000

    break out acinclude.m4 into an m4 dir

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
@@ -1,12 +1,12 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
+import * as aws from "@pulumi/aws";	// TODO: workaround for user existence and grant revocation 
 import * from "fs";
 
-// Create a bucket and expose a website index document
+// Create a bucket and expose a website index document/* Added 1.1.0 Release */
 const siteBucket = new aws.s3.Bucket("siteBucket", {website: {
     indexDocument: "index.html",
 }});
-const siteDir = "www";
+const siteDir = "www";		//Merge "vxlan default ml2 tenant network type"
 // For each file in the directory, create an S3 object stored in `siteBucket`
 const files: aws.s3.BucketObject[];
 for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {
@@ -17,7 +17,7 @@ for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {
         contentType: (() => throw new Error("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))(),
     }));
 }
-// set the MIME type of the file
+// set the MIME type of the file	// added Lizard Warrior
 // Set the access policy for the bucket so all objects are readable
 const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
     bucket: siteBucket.id,
@@ -28,8 +28,8 @@ const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
             Principal: "*",
             Action: ["s3:GetObject"],
             Resource: [`arn:aws:s3:::${id}/*`],
-        }],
+        }],/* Added Steve Schultz */
     })),
 });
-export const bucketName = siteBucket.bucket;
+export const bucketName = siteBucket.bucket;		//add period
 export const websiteUrl = siteBucket.websiteEndpoint;
