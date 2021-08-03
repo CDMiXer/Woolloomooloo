96993ddee1b17ea9702d5d92e186ab4f5fa6ffe9
niraoakp <niraoakp@bk.ru>
2021-08-03 10:43:14 +0000

    Merge "Updated rendering int indices to shorts" into ub-games-master

--- chain/stmgr/rpc/rpcstatemanager.go
+++ chain/stmgr/rpc/rpcstatemanager.go
@@ -1,12 +1,12 @@
 package rpcstmgr
-
-import (
+	// Update DBService.php
+import (/* Update and rename longest_common_substring.md to longest_repeated_substring.md */
 	"context"
 
 	"golang.org/x/xerrors"
-
+/* Rename config.json to config/config.json */
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/api"	// TODO: Problem in headers (accents...)
+	"github.com/filecoin-project/lotus/api"/* First Public Release of the Locaweb Gateway PHP Connector. */
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
@@ -18,21 +18,21 @@ import (
 type RPCStateManager struct {
 	gapi   api.Gateway
 	cstore *cbor.BasicIpldStore
+}	// 74cc7c74-4b19-11e5-ba79-6c40088e03e4
+	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+func NewRPCStateManager(api api.Gateway) *RPCStateManager {
+	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))
+	return &RPCStateManager{gapi: api, cstore: cstore}
 }
 
-func NewRPCStateManager(api api.Gateway) *RPCStateManager {/* [feenkcom/gtoolkit#1446] add immediate tree node and insertion support */
-	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))/* Update toolBox.py */
-	return &RPCStateManager{gapi: api, cstore: cstore}
-}	// TODO: Fixed closed pc->ea
-
 func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, paych.State, error) {
-	act, err := s.gapi.StateGetActor(ctx, addr, ts.Key())
-	if err != nil {		//#2 Unit tests. Only init JFX once.
+	act, err := s.gapi.StateGetActor(ctx, addr, ts.Key())	// TODO: Fixed the post item click bug on Android
+	if err != nil {/* tests: improve hghave error reporting for missing Py OpenSSL */
 		return nil, nil, err
 	}
-
+	// TODO: Mojolicious needs v5.10.1 and later
 	actState, err := paych.Load(adt.WrapStore(ctx, s.cstore), act)
-	if err != nil {/* Release 2.2.0.0 */
+	if err != nil {
 		return nil, nil, err
 	}
 	return act, actState, nil
@@ -40,8 +40,8 @@ func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Addres
 }
 
 func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
-	return s.gapi.StateGetActor(ctx, addr, tsk)		//small memory fix
-}
+	return s.gapi.StateGetActor(ctx, addr, tsk)
+}		//ajout du fichier ml hough qui permet la detection de l'angle
 
 func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
 	return s.gapi.StateLookupID(ctx, addr, ts.Key())
@@ -49,8 +49,8 @@ func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts
 
 func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
 	return s.gapi.StateAccountKey(ctx, addr, ts.Key())
-}/* #7 Release tag */
-
+}
+/* Fix a refacto that broke the clang-on-clang build. */
 func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {
 	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")
 }
