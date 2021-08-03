8dc79f3f7b7f04970743fbcb0184e4e78b61c6e5
niraoakp <niraoakp@bk.ru>
2021-08-03 21:54:16 +0000

    deliverable: BGTVT d

--- gen/client_test.go
+++ gen/client_test.go
@@ -1,16 +1,16 @@
-// Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.	// TODO: Add link to diagrams and description of Wicci Subsystems (parts).
+// Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
+/* removed `event calendar` from title for SEO */
 package websocket
 
 import (
 	"net/url"
 	"testing"
 )
-
-var hostPortNoPortTests = []struct {
-	u                    *url.URL/* Release of eeacms/plonesaas:5.2.1-10 */
+		//remove duplicated max delay check
+var hostPortNoPortTests = []struct {/* added missing emoji v2 */
+	u                    *url.URL/* Delete light_002.png */
 	hostPort, hostNoPort string
 }{
 	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},
@@ -20,13 +20,13 @@ var hostPortNoPortTests = []struct {
 }
 
 func TestHostPortNoPort(t *testing.T) {
-	for _, tt := range hostPortNoPortTests {		//Delete call-flow.jpg
+	for _, tt := range hostPortNoPortTests {
 		hostPort, hostNoPort := hostPortNoPort(tt.u)
 		if hostPort != tt.hostPort {
-			t.Errorf("hostPortNoPort(%v) returned hostPort %q, want %q", tt.u, hostPort, tt.hostPort)	// TODO: Se setea matched=false al guardar las localizaciones.
+			t.Errorf("hostPortNoPort(%v) returned hostPort %q, want %q", tt.u, hostPort, tt.hostPort)
 		}
 		if hostNoPort != tt.hostNoPort {
-			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)
+			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)		//Add separate test case for unweighted edges
 		}
 	}
 }
