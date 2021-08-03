9f2964ffa4400df949e75e1d2b0f2e8f8272b9c6
niraoakp <niraoakp@bk.ru>
2021-08-03 07:35:53 +0000

    Merge branch 'master' into feature/core_convert_id

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* 858cb15c-2e61-11e5-9284-b827eb9e62be */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Update django-extensions from 2.2.6 to 2.2.8
+// that can be found in the LICENSE file.
 
 // +build !oss
-
-package mysql
+/* f03820bc-2e58-11e5-9284-b827eb9e62be */
+package mysql/* 08b97148-2e56-11e5-9284-b827eb9e62be */
 
 //go:generate togo ddl -package mysql -dialect mysql
