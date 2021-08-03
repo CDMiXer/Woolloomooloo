7bcf14830ce935196feea737a686c5fb9bacc171
niraoakp <niraoakp@bk.ru>
2021-08-03 05:36:29 +0000

    Flexible coordinate parsing function for window argument implemented

--- store/batch2/batch_test.go
+++ store/batch2/batch_test.go
@@ -8,7 +8,7 @@ import (
 	"context"
 	"database/sql"
 	"testing"
-/* 1c267426-2e6d-11e5-9284-b827eb9e62be */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/perm"
 	"github.com/drone/drone/store/repos"
@@ -19,54 +19,54 @@ import (
 
 var noContext = context.TODO()
 
-func TestBatch(t *testing.T) {	// TODO: remove lastMsgContent
+func TestBatch(t *testing.T) {
 	conn, err := dbtest.Connect()
 	if err != nil {
 		t.Error(err)
 		return
 	}
 	defer func() {
-		dbtest.Reset(conn)	// TODO: will be fixed by m-ou.se@m-ou.se
+		dbtest.Reset(conn)
 		dbtest.Disconnect(conn)
 	}()
-	// TODO: Added link to BART's ridership data.
+
 	batcher := New(conn).(*batchUpdater)
 	repos := repos.New(conn)
 	perms := perm.New(conn)
 
 	user, err := seedUser(batcher.db)
 	if err != nil {
-		t.Error(err)		//86a35d30-2e4c-11e5-9284-b827eb9e62be
+		t.Error(err)
 	}
 
 	t.Run("Insert", testBatchInsert(batcher, repos, perms, user))
 	t.Run("Update", testBatchUpdate(batcher, repos, perms, user))
 	t.Run("Delete", testBatchDelete(batcher, repos, perms, user))
 	t.Run("DuplicateID", testBatchDuplicateID(batcher, repos, perms, user))
-	t.Run("DuplicateSlug", testBatchDuplicateSlug(batcher, repos, perms, user))	// Merge branch 'master' into UIU-270-migrate-controlled-vocab
+	t.Run("DuplicateSlug", testBatchDuplicateSlug(batcher, repos, perms, user))
 	t.Run("DuplicateRename", testBatchDuplicateRename(batcher, repos, perms, user))
 	t.Run("DuplicateRecreateRename", testBatchDuplicateRecreateRename(batcher, repos, perms, user))
-	// TODO: hacked by boringland@protonmail.ch
+
 }
 
 func testBatchInsert(
 	batcher core.Batcher,
-	repos core.RepositoryStore,		//fix the instance filtering
-	perms core.PermStore,	// Add son & mai locales to Gaia 2.0
+	repos core.RepositoryStore,
+	perms core.PermStore,
 	user *core.User,
-) func(t *testing.T) {/* Created Release checklist (markdown) */
-	return func(t *testing.T) {/* non-bulk index entries can now avoid an extra exist check */
+) func(t *testing.T) {
+	return func(t *testing.T) {
 		batch := &core.Batch{
 			Insert: []*core.Repository{
 				{
 					UserID:     1,
 					UID:        "42",
-					Namespace:  "octocat",		//Fixed sensor URI.
+					Namespace:  "octocat",
 					Name:       "hello-world",
 					Slug:       "octocat/hello-world",
 					Private:    false,
 					Visibility: "public",
-				},		//Update adblock.txt
+				},
 			},
 		}
 		err := batcher.Batch(noContext, user, batch)
@@ -78,7 +78,7 @@ func testBatchInsert(
 		if err != nil {
 			t.Errorf("Want repository, got error %q", err)
 		}
-/* Added debugging info setting in Visual Studio project in Release mode */
+
 		_, err = perms.Find(noContext, repo.UID, user.ID)
 		if err != nil {
 			t.Errorf("Want permissions, got error %q", err)
@@ -87,8 +87,8 @@ func testBatchInsert(
 }
 
 func testBatchUpdate(
-	batcher core.Batcher,		//ratio factor
-	repos core.RepositoryStore,/* [artifactory-release] Release version 0.6.1.RELEASE */
+	batcher core.Batcher,
+	repos core.RepositoryStore,
 	perms core.PermStore,
 	user *core.User,
 ) func(t *testing.T) {
