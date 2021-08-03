d12f3e787fc7b0bb1b3d6a9c48acf3f69cbcc70b
niraoakp <niraoakp@bk.ru>
2021-08-03 04:28:42 +0000

    Resources and README

--- tests/integration/aliases/dotnet/rename_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step2/Program.cs
@@ -1,29 +1,29 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Threading.Tasks;
-using Pulumi;
+using System.Threading.Tasks;/* Solved critical issues 2 */
+using Pulumi;/* Delete viggen.jpg */
 
-class Resource : ComponentResource
+class Resource : ComponentResource/* Fix: Pb with firstname/lastname and font size */
 {
-    public Resource(string name, ComponentResourceOptions options = null)
+    public Resource(string name, ComponentResourceOptions options = null)/* Soft and Hard Assertion support */
         : base("my:module:Resource", name, options)
     {
     }
 }
 
-// Scenario #3 - rename a component (and all it's children)
+// Scenario #3 - rename a component (and all it's children)	// TODO: hacked by onhardev@bk.ru
 // No change to the component itself.
 class ComponentThree : ComponentResource
-{		//Initail Commit
-    private Resource resource1;	// TODO: metis-grabber (wip)
+{
+    private Resource resource1;
     private Resource resource2;
-		//internal name for multiple images: section_ima_key
+
     public ComponentThree(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentThree", name, options)
-    {/* [artifactory-release] Release version 1.4.0.RC1 */
-        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
+    {/* Release package imports */
+        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit	// Add testall Makefile target to test all of froide
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
-        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });/* Fixed debug macros so debugging is printed only when --trace-fulltext is given. */
+        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });/* add separate email for creator verification */
         this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
@@ -31,11 +31,11 @@ class ComponentThree : ComponentResource
 
 class Program
 {
-    static Task<int> Main(string[] args)/* Fix `Too many arguments` warning */
-    {/* Release 1.4.5 */
+    static Task<int> Main(string[] args)
+    {
         return Deployment.RunAsync(() =>
         {
-            // Applying an alias to the instance successfully renames both the component and the children.
+            // Applying an alias to the instance successfully renames both the component and the children./* Custom reactions */
             var comp3 = new ComponentThree("newcomp3", new ComponentResourceOptions
             {
                 Aliases = { new Alias { Name = "comp3" } },
