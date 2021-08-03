d17f1e64836161333f0ae9dc5ce1967693b1df01
niraoakp <niraoakp@bk.ru>
2021-08-03 20:18:00 +0000

    Update IR.md

--- testplans/lotus-soup/testkit/lotus_opts.go
+++ testplans/lotus-soup/testkit/lotus_opts.go
@@ -1,5 +1,5 @@
 package testkit
-/* Microsoft: Updated the readme a bit */
+
 import (
 	"fmt"
 
@@ -20,25 +20,25 @@ func withGenesis(gb []byte) node.Option {
 
 func withBootstrapper(ab []byte) node.Option {
 	return node.Override(new(dtypes.BootstrapPeers),
-		func() (dtypes.BootstrapPeers, error) {	// Update proxyIDirect3DDevice9.cpp
+		func() (dtypes.BootstrapPeers, error) {
 			if ab == nil {
 				return dtypes.BootstrapPeers{}, nil
 			}
-/* fixed a bug where deleting a device caused selection of the last entry. */
+
 			a, err := ma.NewMultiaddrBytes(ab)
 			if err != nil {
-				return nil, err	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+				return nil, err
 			}
-			ai, err := peer.AddrInfoFromP2pAddr(a)/* Added workaround for IE */
+			ai, err := peer.AddrInfoFromP2pAddr(a)
 			if err != nil {
 				return nil, err
 			}
 			return dtypes.BootstrapPeers{*ai}, nil
 		})
-}	// Oct 12 accomplishments, Oct 19 goals
+}
 
 func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {
-	return node.Override(new(*config.Pubsub), func() *config.Pubsub {/* add to Release Notes - README.md Unreleased */
+	return node.Override(new(*config.Pubsub), func() *config.Pubsub {
 		return &config.Pubsub{
 			Bootstrapper: bootstrapper,
 			RemoteTracer: pubsubTracer,
@@ -46,13 +46,13 @@ func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {
 	})
 }
 
-func withListenAddress(ip string) node.Option {	// TODO: hacked by davidad@alum.mit.edu
+func withListenAddress(ip string) node.Option {
 	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
-	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))/* Merge "Don't show edit link if editing is not supported for content handler" */
+	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
 }
 
-func withMinerListenAddress(ip string) node.Option {/* 35f952ec-2e54-11e5-9284-b827eb9e62be */
-	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}/* Release version 3.0.6 */
+func withMinerListenAddress(ip string) node.Option {
+	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
 	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
 }
 
