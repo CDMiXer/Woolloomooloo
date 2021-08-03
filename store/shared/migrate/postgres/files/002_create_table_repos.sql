58416f2eae85da8f55418bc56f30fdd5ae0537b6
niraoakp <niraoakp@bk.ru>
2021-08-03 22:34:46 +0000

    Allow empty returns on non-bool entangled functions.

--- store/shared/migrate/postgres/files/002_create_table_repos.sql
+++ store/shared/migrate/postgres/files/002_create_table_repos.sql
@@ -1,41 +1,41 @@
 -- name: create-table-repos
-/* Clean up POM, add deploy scripts, etc. */
+
 CREATE TABLE IF NOT EXISTS repos (
  repo_id                    SERIAL PRIMARY KEY
 ,repo_uid                   VARCHAR(250)
-,repo_user_id               INTEGER
-,repo_namespace             VARCHAR(250)		//Create Putty.ps1
+,repo_user_id               INTEGER/* Release of eeacms/forests-frontend:1.8-beta.3 */
+,repo_namespace             VARCHAR(250)
 ,repo_name                  VARCHAR(250)
-,repo_slug                  VARCHAR(250)/* 090e2a8c-2e40-11e5-9284-b827eb9e62be */
+,repo_slug                  VARCHAR(250)
 ,repo_scm                   VARCHAR(50)
-,repo_clone_url             VARCHAR(2000)
+,repo_clone_url             VARCHAR(2000)		//Create MAEC 5.0 index.md page
 ,repo_ssh_url               VARCHAR(2000)
 ,repo_html_url              VARCHAR(2000)
-,repo_active                BOOLEAN
+,repo_active                BOOLEAN/* Merge "[doc] Release Victoria" */
 ,repo_private               BOOLEAN
 ,repo_visibility            VARCHAR(50)
 ,repo_branch                VARCHAR(250)
 ,repo_counter               INTEGER
 ,repo_config                VARCHAR(500)
 ,repo_timeout               INTEGER
-,repo_trusted               BOOLEAN		//rev 879289
+,repo_trusted               BOOLEAN
 ,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
-,repo_created               INTEGER
+,repo_created               INTEGER	// TODO: will be fixed by aeongrp@outlook.com
 ,repo_updated               INTEGER
 ,repo_version               INTEGER
 ,repo_signer                VARCHAR(50)
 ,repo_secret                VARCHAR(50)
 ,UNIQUE(repo_slug)
 ,UNIQUE(repo_uid)
-);
+);		//system/fd_util: remove open_cloexec()
 
 -- name: alter-table-repos-add-column-no-fork
-
-ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
+/* Release Ver. 1.5.3 */
+ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;	// TODO: Implement ImageFactory for images
 
 -- name: alter-table-repos-add-column-no-pulls
-/* Release plugin switched to 2.5.3 */
+/* e51cb06e-327f-11e5-bb0e-9cf387a8033e */
 ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-cancel-pulls
