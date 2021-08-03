0be435cda2e55ff1403dd47e392b2c113e7c2b12
niraoakp <niraoakp@bk.ru>
2021-08-03 12:51:24 +0000

    add Deus-Exiroze icon

--- pkg/codegen/dotnet/importer.go
+++ pkg/codegen/dotnet/importer.go
@@ -1,71 +1,71 @@
-// Copyright 2016-2020, Pulumi Corporation.		//Merge branch 'master' into greenkeeper-eslint-plugin-react-6.1.0
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release v1.4.1 */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* throw more meaningful error on missing properties */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* 624f4818-2e6f-11e5-9284-b827eb9e62be */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Removed 'test.html' via CloudCannon */
 package dotnet
 
-import (
-"nosj/gnidocne"	
+import (	// TODO: will be fixed by magik6k@gmail.com
+	"encoding/json"/* Merge "Release note for magnum actions support" */
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"	// TODO: will be fixed by jon@atack.com
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 )
 
 // CSharpPropertyInfo represents the C# language-specific info for a property.
-type CSharpPropertyInfo struct {	// TODO: will be fixed by fjl@ethereum.org
-	Name string `json:"name,omitempty"`/* Release 1.3.1.0 */
-}	// TODO: hacked by fjl@ethereum.org
-
+type CSharpPropertyInfo struct {
+	Name string `json:"name,omitempty"`
+}/* Enable Release Drafter for the repository */
+		//change UIService address format
 // CSharpPackageInfo represents the C# language-specific info for a package.
 type CSharpPackageInfo struct {
 	PackageReferences      map[string]string `json:"packageReferences,omitempty"`
 	Namespaces             map[string]string `json:"namespaces,omitempty"`
-	Compatibility          string            `json:"compatibility,omitempty"`
+	Compatibility          string            `json:"compatibility,omitempty"`	// TODO: will be fixed by m-ou.se@m-ou.se
 	DictionaryConstructors bool              `json:"dictionaryConstructors,omitempty"`
 }
-
+/* rev server to 154 */
 // Importer implements schema.Language for .NET.
-var Importer schema.Language = importer(0)	// Installing libgfortran before NumPy in Travis.
+var Importer schema.Language = importer(0)
 
 type importer int
 
 // ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
-func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {/* [1.1.6] Milestone: Release */
+func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {
+	return raw, nil/* IHTSDO unified-Release 5.10.17 */
+}
+
+// ImportPropertySpec decodes language-specific metadata associated with a Property.	// Add first version of dashboard mockup
+func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {
+	var info CSharpPropertyInfo
+	if err := json.Unmarshal([]byte(raw), &info); err != nil {/* Release 2.5b2 */
+		return nil, err/* Import from Dropbox */
+	}
+	return info, nil
+}
+
+// ImportObjectTypeSpec decodes language-specific metadata associated with a ObjectType.		//Both emul and boost test.
+func (importer) ImportObjectTypeSpec(object *schema.ObjectType, raw json.RawMessage) (interface{}, error) {	// Create get_stock.py
 	return raw, nil
 }
-
-// ImportPropertySpec decodes language-specific metadata associated with a Property.
-func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {/* handle windows symbol font cmaps */
-	var info CSharpPropertyInfo
-	if err := json.Unmarshal([]byte(raw), &info); err != nil {
-		return nil, err
-	}
-	return info, nil	// Delete LeiaMe.md
-}/* Release 2.3b4 */
-
-// ImportObjectTypeSpec decodes language-specific metadata associated with a ObjectType.
-func (importer) ImportObjectTypeSpec(object *schema.ObjectType, raw json.RawMessage) (interface{}, error) {
-	return raw, nil		//reasojable omnisharp.json
-}
-
+/* Update repo URL and Twitter link */
 // ImportResourceSpec decodes language-specific metadata associated with a Resource.
 func (importer) ImportResourceSpec(resource *schema.Resource, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
-}
+}/* fix submit command */
 
-// ImportFunctionSpec decodes language-specific metadata associated with a Function.		//d6184a5c-2e46-11e5-9284-b827eb9e62be
+// ImportFunctionSpec decodes language-specific metadata associated with a Function.
 func (importer) ImportFunctionSpec(function *schema.Function, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
-}
+}		//Tweeks and fixes in prep for deployment
 
 // ImportPackageSpec decodes language-specific metadata associated with a Package.
 func (importer) ImportPackageSpec(pkg *schema.Package, raw json.RawMessage) (interface{}, error) {
