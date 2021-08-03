6e2e6d3558290a1c8d2ffd92cd7dc4676bc8bb88
niraoakp <niraoakp@bk.ru>
2021-08-03 00:11:02 +0000

    Latest fix to install script

--- plugin/secret/combine_test.go
+++ plugin/secret/combine_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//6f48784a-2e75-11e5-9284-b827eb9e62be
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Add --[no-]color to History.txt.
-/* Update get_ip.py */
+// that can be found in the LICENSE file.
+
 package secret
