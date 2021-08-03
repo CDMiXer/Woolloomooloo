7ea368508f1a95d501e8d9b399073965c492f5f8
niraoakp <niraoakp@bk.ru>
2021-08-03 16:43:40 +0000

    Merge "[FIX] sap.m.StepInput: Buttons accessing with VoiceOver fixed"

--- handler/api/errors/errors_test.go
+++ handler/api/errors/errors_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Change order of styles in freeplane.mm
 // that can be found in the LICENSE file.
 
 package errors
-
+		//Added more to the tool description
 import "testing"
-/* enumerated some incomplete tests ("todos"), cleaned up some tests */
-func TestError(t *testing.T) {
+
+func TestError(t *testing.T) {		//Clear document body between tests
 	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message
 	if got != want {
 		t.Errorf("Want error string %q, got %q", got, want)
