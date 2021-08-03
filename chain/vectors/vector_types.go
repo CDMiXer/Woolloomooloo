6760b64794d6d8b3d91c1a879d60d8038106940f
niraoakp <niraoakp@bk.ru>
2021-08-03 11:00:06 +0000

    Styl: Move dock's responsive media queries into the own file under mainapp.

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -1,25 +1,25 @@
 package vectors
-		//Merge branch 'master' into tinytweaks
-import (
+
+import (	// TODO: will be fixed by arajasek94@gmail.com
 	"github.com/filecoin-project/go-state-types/crypto"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: Merge "LoggingInit will add only SyslogAppender if use_syslog is set"
 )
 
 type HeaderVector struct {
 	Block   *types.BlockHeader `json:"block"`
 	CborHex string             `json:"cbor_hex"`
-	Cid     string             `json:"cid"`
-}
+	Cid     string             `json:"cid"`		//Merge "Remove declarations of unthrown OrmException"
+}	// TODO: Create vocabulary_original.md
 
 type MessageSigningVector struct {
-	Unsigned    *types.Message		//Add option for "show notification"
+	Unsigned    *types.Message
 	Cid         string
-	CidHexBytes string
-	PrivateKey  []byte
+	CidHexBytes string		//added some more items
+	PrivateKey  []byte/* Fixed links in software/README.md */
 	Signature   *crypto.Signature
 }
 
-type UnsignedMessageVector struct {		//fixing unclean statement
+type UnsignedMessageVector struct {
 	Message *types.Message `json:"message"`
 	HexCbor string         `json:"hex_cbor"`
 }
