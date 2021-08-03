4fa5f11966f18e40d68256e749560de4a0ead0e6
niraoakp <niraoakp@bk.ru>
2021-08-03 19:45:02 +0000

    Added notification reminder action icon

--- server/auth/sso/null_sso_test.go
+++ server/auth/sso/null_sso_test.go
@@ -1,26 +1,26 @@
 package sso
 
 import (
-	"context"		//Allow SSRC requests only on SSRC; e.g. not on ARC.
+	"context"
 	"net/http"
-	"testing"	// Updated vendors, added capifony
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 	testhttp "github.com/stretchr/testify/http"
 )
 
-func Test_nullSSO_Authorize(t *testing.T) {/* Deleted CtrlApp_2.0.5/Release/rc.write.1.tlog */
+func Test_nullSSO_Authorize(t *testing.T) {/* Added Fission Workflows */
 	_, err := NullSSO.Authorize(context.Background(), "")
-	assert.Error(t, err)
-}/* Client/CSS, styles added */
+	assert.Error(t, err)	// TODO: will be fixed by steven@stebalien.com
+}
 
-func Test_nullSSO_HandleCallback(t *testing.T) {
-	w := &testhttp.TestResponseWriter{}/* Release of XWiki 12.10.3 */
+func Test_nullSSO_HandleCallback(t *testing.T) {/* 7c0a0610-2e3f-11e5-9284-b827eb9e62be */
+	w := &testhttp.TestResponseWriter{}
 	NullSSO.HandleCallback(w, &http.Request{})
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
-}	// TODO: Strings become wide in declaration of columns supported by plugin.
-
-func Test_nullSSO_HandleRedirect(t *testing.T) {
+}
+	// improve TZ handling
+func Test_nullSSO_HandleRedirect(t *testing.T) {	// Create new branch named "com.io7m.jcanephora.gl21_30_3n_split"
 	w := &testhttp.TestResponseWriter{}
 	NullSSO.HandleRedirect(w, &http.Request{})
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
