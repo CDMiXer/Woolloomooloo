7592c05eb2ed83fc4d8811f8b9473368ac8e9c75
niraoakp <niraoakp@bk.ru>
2021-08-03 06:10:00 +0000

    ZAPI-17: Pre-alpha version of provision workflow

--- store/shared/migrate/mysql/files/007_create_table_logs.sql
+++ store/shared/migrate/mysql/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
--- name: create-table-logs/* Update comments in test tcfail132 */
+-- name: create-table-logs
 
 CREATE TABLE IF NOT EXISTS logs (
- log_id    INTEGER PRIMARY KEY/* Create How to delete unused kernel on CentOS 7.md */
+ log_id    INTEGER PRIMARY KEY
 ,log_data  MEDIUMBLOB
-);		//Merge "Add hacking check H105: don't use author tags"
+);
