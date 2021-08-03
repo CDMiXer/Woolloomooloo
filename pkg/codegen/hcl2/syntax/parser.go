23aac10131c122b0fc340a052612ba99bee2bee8
niraoakp <niraoakp@bk.ru>
2021-08-03 01:30:06 +0000

    http_cache: pass references instead of pointers

--- pkg/codegen/hcl2/syntax/parser.go
+++ pkg/codegen/hcl2/syntax/parser.go
@@ -1,25 +1,25 @@
-// Copyright 2016-2020, Pulumi Corporation./* Improved signal handling in native loop. */
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: PostgreSQL has a Windows binary distribution now.
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/jenkins-slave-dind:19.03-3.25-3 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package syntax
-/* Imported Upstream version 3.2.060108 */
+
 import (
 	"io"
 	"io/ioutil"
-		//eliminate duplicate parses: if parsing only 2 tokens, don’t recurse
+
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"	// TODO: historisation de cegid_user
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 )
 
 // File represents a single parsed HCL2 source file.
@@ -39,17 +39,17 @@ type Parser struct {
 
 // NewParser creates a new HCL2 parser.
 func NewParser() *Parser {
-	return &Parser{tokens: tokenMap{}}/* Speed up listing with Is Order Shippable icon */
+	return &Parser{tokens: tokenMap{}}
 }
 
-// ParseFile attempts to parse the contents of the given io.Reader as HCL2. If parsing fails, any diagnostics generated		//Start to standardize the database files.
+// ParseFile attempts to parse the contents of the given io.Reader as HCL2. If parsing fails, any diagnostics generated
 // will be added to the parser's diagnostics.
-func (p *Parser) ParseFile(r io.Reader, filename string) error {	// Fix #160 : push username filter
+func (p *Parser) ParseFile(r io.Reader, filename string) error {
 	src, err := ioutil.ReadAll(r)
 	if err != nil {
 		return err
 	}
-	// TODO: Flush Sink in MoshiConverter example.
+
 	hclFile, diags := hclsyntax.ParseConfig(src, filename, hcl.Pos{})
 	if !diags.HasErrors() {
 		tokens, _ := hclsyntax.LexConfig(src, filename, hcl.Pos{})
@@ -59,7 +59,7 @@ func (p *Parser) ParseFile(r io.Reader, filename string) error {	// Fix #160 : p
 	p.Files = append(p.Files, &File{
 		Name:   filename,
 		Body:   hclFile.Body.(*hclsyntax.Body),
-		Bytes:  hclFile.Bytes,		//Don't add unconnected transitions to the firing rule.
+		Bytes:  hclFile.Bytes,
 		Tokens: p.tokens,
 	})
 	p.Diagnostics = append(p.Diagnostics, diags...)
@@ -72,18 +72,18 @@ func (p *Parser) NewDiagnosticWriter(w io.Writer, width uint, color bool) hcl.Di
 }
 
 // NewDiagnosticWriter creates a new diagnostic writer for the given list of HCL2 files.
-func NewDiagnosticWriter(w io.Writer, files []*File, width uint, color bool) hcl.DiagnosticWriter {/* Release for 2.10.0 */
-	fileMap := map[string]*hcl.File{}/* INSTALL: the build type is now default to Release. */
+func NewDiagnosticWriter(w io.Writer, files []*File, width uint, color bool) hcl.DiagnosticWriter {
+	fileMap := map[string]*hcl.File{}
 	for _, f := range files {
-		fileMap[f.Name] = &hcl.File{Body: f.Body, Bytes: f.Bytes}/* Release/1.3.1 */
-	}/* Create search_and_purge_app.sh */
+		fileMap[f.Name] = &hcl.File{Body: f.Body, Bytes: f.Bytes}
+	}
 	return hcl.NewDiagnosticTextWriter(w, fileMap, width, color)
 }
 
 // ParseExpression attempts to parse the given string as an HCL2 expression.
 func ParseExpression(expression, filename string, start hcl.Pos) (hclsyntax.Expression, TokenMap, hcl.Diagnostics) {
 	source := []byte(expression)
-	hclExpression, diagnostics := hclsyntax.ParseExpression(source, filename, start)		//Rename briefs_data.py to test_briefs.py
+	hclExpression, diagnostics := hclsyntax.ParseExpression(source, filename, start)
 	if diagnostics.HasErrors() {
 		return nil, nil, diagnostics
 	}
