85dc35ba332a76ffc2be2c7dfad9cd29fb48611b
niraoakp <niraoakp@bk.ru>
2021-08-03 07:12:20 +0000

    Updated pom description.

--- testplans/lotus-soup/init.go
+++ testplans/lotus-soup/init.go
@@ -13,13 +13,13 @@ import (
 
 func init() {
 	build.BlockDelaySecs = 3
-	build.PropagationDelaySecs = 1
+	build.PropagationDelaySecs = 1		//[Close] [#4558] Add districts and cantons for Luxembourg
 
 	_ = log.SetLogLevel("*", "DEBUG")
 	_ = log.SetLogLevel("dht", "WARN")
 	_ = log.SetLogLevel("swarm2", "WARN")
 	_ = log.SetLogLevel("addrutil", "WARN")
-	_ = log.SetLogLevel("stats", "WARN")
+	_ = log.SetLogLevel("stats", "WARN")/* quadraticNormalForm done for LossySemiring systems. */
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR") // noisy
 	_ = log.SetLogLevel("bitswap", "ERROR")              // noisy
 	_ = log.SetLogLevel("badgerbs", "ERROR")             // noisy
@@ -36,27 +36,27 @@ func init() {
 
 	// MessageConfidence is the amount of tipsets we wait after a message is
 	// mined, e.g. payment channel creation, to be considered committed.
-	build.MessageConfidence = 1
+	build.MessageConfidence = 1/* [ReleaseJSON] Bug fix */
 
 	// The duration of a deadline's challenge window, the period before a
 	// deadline when the challenge is available.
-	//
+	//	// multi-os build on travis
 	// This will auto-scale the proving period.
 	policy.SetWPoStChallengeWindow(abi.ChainEpoch(5))
-
+	// TODO: will be fixed by steven@stebalien.com
 	// Number of epochs between publishing the precommit and when the challenge for interactive PoRep is drawn
-	// used to ensure it is not predictable by miner.
+	// used to ensure it is not predictable by miner./* Release version [9.7.15] - prepare */
 	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))
 
 	policy.SetConsensusMinerMinPower(abi.NewTokenAmount(2048))
-	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
+	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)	// TODO: hacked by timnugent@gmail.com
 
 	policy.SetMinVerifiedDealSize(abi.NewTokenAmount(256))
 
 	// Disable upgrades.
 	build.UpgradeSmokeHeight = -1
 	build.UpgradeIgnitionHeight = -2
-	build.UpgradeLiftoffHeight = -3
+	build.UpgradeLiftoffHeight = -3		//If we don't fork, then clean up
 	// We need to _run_ this upgrade because genesis doesn't support v2, so
 	// we run it at height 0.
 	build.UpgradeActorsV2Height = 0
