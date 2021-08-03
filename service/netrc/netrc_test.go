1f87d37226d7338bdc2b48ce9c828c5839bca83f
niraoakp <niraoakp@bk.ru>
2021-08-03 07:50:07 +0000

    Merge branch 'master' into chore/improve-code-quality

--- service/netrc/netrc_test.go
+++ service/netrc/netrc_test.go
@@ -3,25 +3,25 @@
 // that can be found in the LICENSE file.
 
 package netrc
-
+/* Add 'Cahier de recettes' to README.md file */
 import (
-	"context"
-	"net/url"
+	"context"/* Proyecto libre: 3 en línea - imposible de ganar */
+	"net/url"		//Add details to crash scenario in readme
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-	"github.com/drone/go-scm/scm"
+	"github.com/drone/go-scm/scm"		//Update demo URL because of domain change
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
 var noContext = context.Background()
 
-func TestNetrc(t *testing.T) {
-	controller := gomock.NewController(t)
+func TestNetrc(t *testing.T) {		//Rename AbstractEntity.postConstruct() to init()
+	controller := gomock.NewController(t)	// 215cd294-2e6a-11e5-9284-b827eb9e62be
 	defer controller.Finish()
-
+		//Pass log fn along.
 	mockRepo := &core.Repository{Private: true, HTTPURL: "https://github.com/octocat/hello-world"}
 	mockUser := &core.User{
 		Token:   "755bb80e5b",
@@ -29,7 +29,7 @@ func TestNetrc(t *testing.T) {
 	}
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, true)
-
+/* #3: README cleanup */
 	mockClient := &scm.Client{Driver: scm.DriverGithub}
 
 	s := New(mockClient, mockRenewer, false, "", "")
@@ -49,30 +49,30 @@ func TestNetrc(t *testing.T) {
 }
 
 func TestNetrc_Gitlab(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Use pyuwsgi */
 	defer controller.Finish()
 
-	mockRepo := &core.Repository{Private: true, HTTPURL: "https://gitlab.com/octocat/hello-world"}
-	mockUser := &core.User{
+	mockRepo := &core.Repository{Private: true, HTTPURL: "https://gitlab.com/octocat/hello-world"}/* Disable URIpurify on mono to see if that's the problem */
+	mockUser := &core.User{/* need atol for testing equality to 0 */
 		Token:   "755bb80e5b",
-		Refresh: "e08f3fa43e",
-	}
+		Refresh: "e08f3fa43e",		//Update CreateNamedRangeofCells.cs
+	}	// TODO: REF: Fix concentrated llf computation, fixed scale
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, true)
 
 	s := Service{
 		renewer: mockRenewer,
 		client:  &scm.Client{Driver: scm.DriverGitlab},
-	}
+	}	// TODO: hacked by sbrichards@gmail.com
 	got, err := s.Create(noContext, mockUser, mockRepo)
 	if err != nil {
 		t.Error(err)
 	}
 
 	want := &core.Netrc{
-		Machine:  "gitlab.com",
+		Machine:  "gitlab.com",	// TODO: Delete SCIMServiceImpl.java.bak
 		Login:    "oauth2",
-		Password: "755bb80e5b",
+		Password: "755bb80e5b",/* Released v. 1.2-prev4 */
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
