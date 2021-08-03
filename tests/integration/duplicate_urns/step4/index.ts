94d87641f5def00f56e9406507d13a7865422808
niraoakp <niraoakp@bk.ru>
2021-08-03 18:23:33 +0000

    Create stop-learner-daemon

--- tests/integration/duplicate_urns/step4/index.ts
+++ tests/integration/duplicate_urns/step4/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//		//Merge "msm: pcie: optimize the link training latency"
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by martin2cai@hotmail.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Merge "ReleaseNotes: Add section for 'ref-update' hook" into stable-2.6 */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -12,15 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";/* Release: 0.95.006 */
+import { Resource } from "./resource";
 
 // "a" is already in the snapshot and will be replaced.
 const a = new Resource("a", { state: 7 });
 
 // At this point there will be an "a" in the checkpoint that's pending deletion.
-/* chore(README): Update Widevine install insructions */
+
 // "b" is not in the snapshot. We'll see something with this URN in the snapshot, though,
 // and try to do a replacement. This is bad because the thing we're replacing is pending deletion.
-const b = new Resource("a", { state: 5 }, { dependsOn: a });		//Fix Twitter Subscriber
+const b = new Resource("a", { state: 5 }, { dependsOn: a });
 
 // This should fail, but gracefully.
