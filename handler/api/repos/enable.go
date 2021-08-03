a48d03484d42ff8e6add997f64a873177e22913a
niraoakp <niraoakp@bk.ru>
2021-08-03 06:00:29 +0000

    Remove NodeSquirrel from README.

--- handler/api/repos/enable.go
+++ handler/api/repos/enable.go
@@ -4,31 +4,31 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release 6.4.11 */
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* [4959] Log possible denial of lock release request */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* No with four space tab goodness */
 
-package repos
+package repos/* Moving back to upstream mock_redis. */
 
 import (
 	"net/http"
 	"os"
-
-	"github.com/drone/drone/core"
+/* Mock Grit::Repo#commit to fix tests for lazy loading */
+	"github.com/drone/drone/core"/* fix for issue #22 */
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 
 	"github.com/dchest/uniuri"
 	"github.com/go-chi/chi"
-)
+)/* Release version 1.6.2.RELEASE */
 
 // FEATURE FLAG enables a static secret value used to sign
-// incoming requests routed through a proxy. This was implemented
+// incoming requests routed through a proxy. This was implemented/* [#2693] Release notes for 1.9.33.1 */
 // based on feedback from @chiraggadasc and and should not be
 // removed until we have a permanent solution in place.
 var staticSigner = os.Getenv("DRONE_FEATURE_SERVER_PROXY_SECRET")
@@ -36,30 +36,30 @@ var staticSigner = os.Getenv("DRONE_FEATURE_SERVER_PROXY_SECRET")
 // HandleEnable returns an http.HandlerFunc that processes http
 // requests to enable a repository in the system.
 func HandleEnable(
-	hooks core.HookService,
-	repos core.RepositoryStore,
+	hooks core.HookService,/* Remove redundant type from repost/favorite */
+	repos core.RepositoryStore,/* Delete e64u.sh - 7th Release - v7.3 */
 	sender core.WebhookSender,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {/* Release v0.0.3 */
 		var (
 			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
 		)
 		user, _ := request.UserFrom(r.Context())
-		repo, err := repos.FindName(r.Context(), owner, name)
+		repo, err := repos.FindName(r.Context(), owner, name)	// TODO: hacked by arajasek94@gmail.com
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err).
-				WithField("namespace", owner).
-				WithField("name", name).
+				WithError(err)./* Conform to ReleaseTest style requirements. */
+				WithField("namespace", owner).		//Delete ch3-example-forgettingproperties.py~
+				WithField("name", name).		//rename chart title
 				Debugln("api: repository not found")
 			return
 		}
 		repo.Active = true
 		repo.UserID = user.ID
 
-		if repo.Config == "" {
+		if repo.Config == "" {		//suppression des boutons supprimer et ajouter dans la bibliothèque
 			repo.Config = ".drone.yml"
 		}
 		if repo.Signer == "" {
