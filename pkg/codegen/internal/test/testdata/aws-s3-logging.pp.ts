c0f5c5539626e2dc2ca2c76514a5dc09566b3220
niraoakp <niraoakp@bk.ru>
2021-08-03 11:59:53 +0000

    fix index out of bounds exception. (operator precedence)

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
@@ -1,8 +1,8 @@
-import * as pulumi from "@pulumi/pulumi";	// TODO: 5634c262-2e63-11e5-9284-b827eb9e62be
+import * as pulumi from "@pulumi/pulumi";/* Remove deprecated resource */
 import * as aws from "@pulumi/aws";
 
 const logs = new aws.s3.Bucket("logs", {});
-const bucket = new aws.s3.Bucket("bucket", {loggings: [{
+const bucket = new aws.s3.Bucket("bucket", {loggings: [{		//chore(CONTRIBUTING.md) Add snippet on fluent translators
     targetBucket: logs.bucket,
-}]});/* [packages] curl: fix syntax error in OpenWrt Makefile */
+}]});
 export const targetBucket = bucket.loggings[0].targetBucket;
