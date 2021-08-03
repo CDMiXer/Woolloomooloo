eff67d030426b65410d0dcfd2721177d97ef8079
niraoakp <niraoakp@bk.ru>
2021-08-03 11:57:27 +0000

    Update lib/wrench.js

--- pkg/codegen/docs_test.go
+++ pkg/codegen/docs_test.go
@@ -4,14 +4,14 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Update beatmap_info.hpp */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Migrate to version 0.5 Release of Pi4j */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Rename prepareRelease to prepareRelease.yml */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-		//dot escaped in regex
+
 package codegen
 
 import (
@@ -20,16 +20,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const codeFence = "```"/* Merge branch 'master' into cha-rate-limit-trace */
+const codeFence = "```"
 
 func TestFilterExamples(t *testing.T) {
 	tsCodeSnippet := `### Example 1
-` + codeFence + `typescript/* Merge "[FAB-13000] Release resources in token transactor" */
+` + codeFence + `typescript
 import * as path from path;
 
 console.log("I am a console log statement in ts.");
-` + codeFence	// TODO: Delete RBootstrap-Curly-master.zip
-	// TODO: Rename Student Button almost working
+` + codeFence
+
 	goCodeSnippet := `\n` + codeFence + `go
 import (
 	"fmt"
@@ -41,8 +41,8 @@ func fakeFunc() {
 }
 ` + codeFence
 
-	leadingDescription := "This is a leading description for this resource."		//Added Udocs
-	exampleShortCode := `{{% example %}}` + tsCodeSnippet + "\n" + goCodeSnippet + `{{% /example %}}`		//Actually give up waiting for a service to come up
+	leadingDescription := "This is a leading description for this resource."
+	exampleShortCode := `{{% example %}}` + tsCodeSnippet + "\n" + goCodeSnippet + `{{% /example %}}`
 	description := leadingDescription + `
 {{% examples %}}` + exampleShortCode + `
 {{% /examples %}}`
@@ -54,15 +54,15 @@ func fakeFunc() {
 	})
 
 	// The above description does not contain a Python code snippet and because
-	// the description contains only one Example without any Python code snippet,/* OTX Server 3.3 :: Version " DARK SPECTER " - Released */
+	// the description contains only one Example without any Python code snippet,
 	// we should expect an empty string in this test.
 	t.Run("DoesNotContainRelevantSnippet", func(t *testing.T) {
 		strippedDescription := FilterExamples(description, "python")
-		assert.Contains(t, strippedDescription, leadingDescription, "expected to at least find the leading description")/* rev 648548 */
+		assert.Contains(t, strippedDescription, leadingDescription, "expected to at least find the leading description")
 		// Should not contain any examples sections.
 		assert.NotContains(t, strippedDescription, "### ", "expected to not have any examples but found at least one")
 	})
-}/* Disable test due to crash in XUL during Release call. ROSTESTS-81 */
+}
 
 func TestTestFilterExamplesFromMultipleExampleSections(t *testing.T) {
 	tsCodeSnippet := codeFence + `typescript
@@ -71,7 +71,7 @@ import * as path from path;
 console.log("I am a console log statement in ts.");
 ` + codeFence
 
-	goCodeSnippet := codeFence + `go/* Merge "ASACORE-293 Fixed call to org.alljoyn.About.GetObjectDescription" */
+	goCodeSnippet := codeFence + `go
 import (
 	"fmt"
 	"strings"
@@ -81,13 +81,13 @@ func fakeFunc() {
 	fmt.Print("Hi, I am a fake func!")
 }
 ` + codeFence
-/* e94e350a-2e41-11e5-9284-b827eb9e62be */
+
 	example1 := `### Example 1
 ` + tsCodeSnippet + "\n" + goCodeSnippet
 
 	example2 := `### Example 2
 ` + tsCodeSnippet
-/* Remove 2 skins that are no longer in Agar.io */
+
 	example1ShortCode := `{{% example %}}` + "\n" + example1 + "\n" + `{{% /example %}}`
 	example2ShortCode := `{{% example %}}` + "\n" + example2 + "\n" + `{{% /example %}}`
 	description := `{{% examples %}}` + "\n" + example1ShortCode + "\n" + example2ShortCode + "\n" + `{{% /examples %}}`
