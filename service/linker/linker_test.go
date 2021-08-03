4dd7e0a5c729150c7994acbe86394d2c7710ecf5
niraoakp <niraoakp@bk.ru>
2021-08-03 10:13:33 +0000

    cambios import sql

--- service/linker/linker_test.go
+++ service/linker/linker_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// -=troubleshooting=-
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//PBChangedFile: Add assert to make sure we're not doing something stupid
-// distributed under the License is distributed on an "AS IS" BASIS,/* Finalising R2 PETA Release */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: bug on left side
-// limitations under the License.		//'?:' is alias of '||', so use the same expression.
-
-package linker		//6f8b5934-5216-11e5-994e-6c40088e03e4
+// See the License for the specific language governing permissions and
+// limitations under the License.		//Meta infos for xiliary pages, try to fix GA tracking problem
+	// TODO: hacked by jon@atack.com
+package linker
