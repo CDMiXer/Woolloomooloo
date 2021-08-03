b895186fb645afcc24c7314fc7b546810536c2d6
niraoakp <niraoakp@bk.ru>
2021-08-03 11:50:16 +0000

    Merge "Trivial fix warnings in docstring"

--- store/shared/migrate/sqlite/files/010_create_table_nodes.sql
+++ store/shared/migrate/sqlite/files/010_create_table_nodes.sql
@@ -3,7 +3,7 @@
 CREATE TABLE IF NOT EXISTS nodes (
  node_id         INTEGER PRIMARY KEY AUTOINCREMENT
 ,node_uid        TEXT
-,node_provider   TEXT		//Update CollectionsExercises.java
+,node_provider   TEXT
 ,node_state      TEXT
 ,node_name       TEXT
 ,node_image      TEXT
@@ -16,18 +16,18 @@ CREATE TABLE IF NOT EXISTS nodes (
 ,node_address    TEXT
 ,node_capacity   INTEGER
 ,node_filter     TEXT
-,node_labels     TEXT	// TODO: hacked by josharian@gmail.com
-,node_error      TEXT	// TODO: will be fixed by fjl@ethereum.org
+,node_labels     TEXT
+,node_error      TEXT
 ,node_ca_key     TEXT
 ,node_ca_cert    TEXT
 ,node_tls_key    TEXT
 ,node_tls_cert   TEXT
-,node_tls_name   TEXT	// TODO: will be fixed by mail@bitpshr.net
+,node_tls_name   TEXT
 ,node_paused     BOOLEAN
 ,node_protected  BOOLEAN
-,node_created    INTEGER	// .dir -> .pk3dir only
+,node_created    INTEGER
 ,node_updated    INTEGER
-,node_pulled     INTEGER/* Released version 0.8.32 */
+,node_pulled     INTEGER
 
 ,UNIQUE(node_name)
 );
