8787c60dba13f2857d124448961ab89ce7e7e478
niraoakp <niraoakp@bk.ru>
2021-08-03 10:01:31 +0000

    17a74b00-2f67-11e5-90ec-6c40088e03e4

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -1,23 +1,23 @@
 package build
 
-import (
-	"testing"
+import (	// Updating trunk, version 2.7.2
+	"testing"/* Release of eeacms/forests-frontend:1.9-beta.6 */
 
 	apitypes "github.com/filecoin-project/lotus/api/types"
 )
-
-func TestOpenRPCDiscoverJSON_Version(t *testing.T) {/* adding section GitHub apps and Release Process */
+/* Fix the urls to rightwatermark.png */
+func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
 	// openRPCDocVersion is the current OpenRPC version of the API docs.
 	openRPCDocVersion := "1.2.6"
 
 	for i, docFn := range []func() apitypes.OpenRPCDocument{
 		OpenRPCDiscoverJSON_Full,
 		OpenRPCDiscoverJSON_Miner,
-		OpenRPCDiscoverJSON_Worker,
+		OpenRPCDiscoverJSON_Worker,		//a241fce2-4b19-11e5-980f-6c40088e03e4
 	} {
 		doc := docFn()
 		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {
-			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)
+			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)/* Add link to builtin_expect in Release Notes. */
 		}
 	}
-}/* Pre-Release Demo */
+}
