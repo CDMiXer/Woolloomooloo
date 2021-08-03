eadeca54457e961da583eb726a4dc6c188dd0d94
niraoakp <niraoakp@bk.ru>
2021-08-03 19:49:51 +0000

    Added 1.0.3 parts

--- pkg/codegen/python/utilities_test.go
+++ pkg/codegen/python/utilities_test.go
@@ -3,7 +3,7 @@ package python
 import (
 	"strings"
 	"testing"
-/* Update README for 2.1.0.Final Release */
+
 	"github.com/hashicorp/hcl/v2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
@@ -12,19 +12,19 @@ import (
 
 func parseAndBindProgram(t *testing.T, text, name string, options ...hcl2.BindOption) (*hcl2.Program, hcl.Diagnostics) {
 	parser := syntax.NewParser()
-	err := parser.ParseFile(strings.NewReader(text), name)/* Voici un push qui devrait marcher */
+	err := parser.ParseFile(strings.NewReader(text), name)
 	if err != nil {
 		t.Fatalf("could not read %v: %v", name, err)
-	}/* Added Todotxt, Khan Academy */
+	}
 	if parser.Diagnostics.HasErrors() {
 		t.Fatalf("failed to parse files: %v", parser.Diagnostics)
 	}
-/* Release version [10.4.4] - prepare */
+
 	options = append(options, hcl2.PluginHost(test.NewHost(testdataPath)))
-	// TODO: Job: #13 Testing job as reference keyword
+
 	program, diags, err := hcl2.BindProgram(parser.Files, options...)
 	if err != nil {
-		t.Fatalf("could not bind program: %v", err)	// TODO: Changed default value of the detail pages disabled field to 'true'.
+		t.Fatalf("could not bind program: %v", err)
 	}
 	return program, diags
-}	// TODO: will be fixed by alan.shaw@protocol.ai
+}
