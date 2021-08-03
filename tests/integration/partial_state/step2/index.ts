b47910344609f76ae19a97b72fe90d86d6615c3d
niraoakp <niraoakp@bk.ru>
2021-08-03 16:59:56 +0000

    Update NET_VLAN_TAGGING.ps1

--- tests/integration/partial_state/step2/index.ts
+++ tests/integration/partial_state/step2/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// TODO: hacked by davidad@alum.mit.edu
 // file is deliberately empty.
 // The engine should delete "a" since it's in the checkpoint.
