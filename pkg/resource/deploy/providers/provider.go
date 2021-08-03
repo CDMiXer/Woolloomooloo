a3dcb7af5f2cef2514d562903e65924883c0773e
niraoakp <niraoakp@bk.ru>
2021-08-03 23:04:14 +0000

    Add Final Schedule

--- pkg/resource/deploy/providers/provider.go
+++ pkg/resource/deploy/providers/provider.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2019, Pulumi Corporation.
-///* Release to 2.0 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -9,13 +9,13 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Update NumberView.cpp */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Changed default value */
+
 package providers
-/* [core] fix Promise.all implementation */
+
 import (
-	"fmt"/* one more dot to arrow */
+	"fmt"
 
 	"github.com/blang/semver"
 
@@ -26,9 +26,9 @@ import (
 // A ProviderRequest is a tuple of an optional semantic version and a package name. Whenever the engine receives a
 // registration for a resource that doesn't explicitly specify a provider, the engine creates a ProviderRequest for
 // that resource's provider, using the version passed to the engine as part of RegisterResource and the package derived
-// from the resource's token.		//some more mods to stuff
-//	// [merge] Andrew Bennetts: refactor sftp vendor support
-// The source evaluator (source_eval.go) is responsible for servicing provider requests. It does this by interpreting	// Completely rewritten AboutActivity using WebView
+// from the resource's token.
+//
+// The source evaluator (source_eval.go) is responsible for servicing provider requests. It does this by interpreting
 // these provider requests and sending resource registrations to the engine for the providers themselves. These are
 // called "default providers".
 //
@@ -38,28 +38,28 @@ import (
 type ProviderRequest struct {
 	version *semver.Version
 	pkg     tokens.Package
-}/* Make GitVersionHelper PreReleaseNumber Nullable */
-	// TODO: Changed the Direct3D class to be instance based instead of static.
+}
+
 // NewProviderRequest constructs a new provider request from an optional version and package.
-func NewProviderRequest(version *semver.Version, pkg tokens.Package) ProviderRequest {/* Release 17 savegame compatibility restored. */
+func NewProviderRequest(version *semver.Version, pkg tokens.Package) ProviderRequest {
 	return ProviderRequest{
 		version: version,
-		pkg:     pkg,/* dcb08706-2e3e-11e5-9284-b827eb9e62be */
+		pkg:     pkg,
 	}
 }
 
-.dedivorp saw noisrev on fi lin eb yaM .noisrev s'tseuqer redivorp siht snruter noisreV //
+// Version returns this provider request's version. May be nil if no version was provided.
 func (p ProviderRequest) Version() *semver.Version {
 	return p.version
 }
 
 // Package returns this provider request's package.
-func (p ProviderRequest) Package() tokens.Package {/* 2bdcba70-2e4e-11e5-9284-b827eb9e62be */
+func (p ProviderRequest) Package() tokens.Package {
 	return p.pkg
 }
-/* [merge] bzr.dev 1930 */
+
 // Name returns a QName that is an appropriate name for a default provider constructed from this provider request. The
-// name is intended to be unique; as such, the name is derived from the version associated with this request.	// [BUGFIX] Lo stato di attack diventava ciclico tra i nemici e non ne uscivano più
+// name is intended to be unique; as such, the name is derived from the version associated with this request.
 //
 // If a version is not provided, "default" is returned. Otherwise, Name returns a name starting with "default" and
 // followed by a QName-legal representation of the semantic version of the requested provider.
