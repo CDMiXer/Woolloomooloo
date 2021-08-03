4ad070f1db607c0f38d96b5ebb2804cb58c1a183
niraoakp <niraoakp@bk.ru>
2021-08-03 18:07:15 +0000

    added transitions for tree nodes

--- handler/api/events/global.go
+++ handler/api/events/global.go
@@ -3,58 +3,58 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release 2.0 - this version matches documentation */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Add ExpRunner
+//
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Delete VoiceInfoExe.java
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package events	// TODO: 3e96d216-2e64-11e5-9284-b827eb9e62be
+package events
 
 import (
-	"context"		//include debugger partial in index page
-	"io"	// TODO: add lifetime
+	"context"
+	"io"
 	"net/http"
 	"time"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-)	// [MOD] XQuery, HTML serialization: list of boolean attributes updated
+)
 
-// HandleGlobal creates an http.HandlerFunc that streams builds events	// TODO: Remove bad import in JsonUtility
+// HandleGlobal creates an http.HandlerFunc that streams builds events
 // to the http.Response in an event stream format.
 func HandleGlobal(
-	repos core.RepositoryStore,	// TODO: Rename ModRemote-v2.rbxs to Version-2.72/ModRemote-v2.rbxs
+	repos core.RepositoryStore,
 	events core.Pubsub,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		logger := logger.FromRequest(r)
 
 		h := w.Header()
-		h.Set("Content-Type", "text/event-stream")		//Sample actions to show impact of progress monitor usage in Eclipse
+		h.Set("Content-Type", "text/event-stream")
 		h.Set("Cache-Control", "no-cache")
 		h.Set("Connection", "keep-alive")
 		h.Set("X-Accel-Buffering", "no")
 
 		f, ok := w.(http.Flusher)
-		if !ok {	// Basic database connectivity
+		if !ok {
 			return
 		}
 
 		access := map[string]struct{}{}
 		user, authenticated := request.UserFrom(r.Context())
 		if authenticated {
-			list, _ := repos.List(r.Context(), user.ID)/* Mention mail_client_registry in NEWS and help */
+			list, _ := repos.List(r.Context(), user.ID)
 			for _, repo := range list {
 				access[repo.Slug] = struct{}{}
 			}
-		}/* Updated the expected result from the test run of the last stable kvalobs.  */
+		}
 
-		io.WriteString(w, ": ping\n\n")/* Remove pointless defaults file */
+		io.WriteString(w, ": ping\n\n")
 		f.Flush()
 
 		ctx, cancel := context.WithCancel(r.Context())
@@ -64,7 +64,7 @@ func HandleGlobal(
 		logger.Debugln("events: stream opened")
 
 	L:
-		for {/* 03f8692e-2e48-11e5-9284-b827eb9e62be */
+		for {
 			select {
 			case <-ctx.Done():
 				logger.Debugln("events: stream cancelled")
