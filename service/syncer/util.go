9a5eedc5e43e035580e5760bb1e083e1024dd1e6
niraoakp <niraoakp@bk.ru>
2021-08-03 06:14:49 +0000

    GetSetupValue action added

--- service/syncer/util.go
+++ service/syncer/util.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Add screenshot slideshow for version 0.1 to main page.
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Delete test_file_in_folder.txt */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,17 +13,17 @@
 // limitations under the License.
 
 package syncer
-/* Add Higher-order functions */
+
 import (
-	"github.com/drone/drone/core"		//ROLLBACK to mysql
+	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 )
 
 // merge is a helper function that mergest a subset of
-// values from the source to the destination repository./* 1.8.7 Release */
+// values from the source to the destination repository.
 func merge(dst, src *core.Repository) {
-	dst.Namespace = src.Namespace	// TODO: Mode method 'evaluateChainValue' from Injector to DataUtil
-	dst.Name = src.Name/* one revision loader instance */
+	dst.Namespace = src.Namespace
+	dst.Name = src.Name
 	dst.HTTPURL = src.HTTPURL
 	dst.SSHURL = src.SSHURL
 	dst.Private = src.Private
@@ -33,7 +33,7 @@ func merge(dst, src *core.Repository) {
 	// the gitea and gogs repository endpoints do not
 	// return the html url, so we need to ensure we do
 	// not replace the existing value with a zero value.
-	if src.Link != "" {	// TODO: Prepare for release of eeacms/energy-union-frontend:1.7-beta.19
+	if src.Link != "" {
 		dst.Link = src.Link
 	}
 }
@@ -41,12 +41,12 @@ func merge(dst, src *core.Repository) {
 // diff is a helper function that compares two repositories
 // and returns true if a subset of values are different.
 func diff(a, b *core.Repository) bool {
-	switch {		//added internal static method for identifying fastq read pairs
+	switch {
 	case a.Namespace != b.Namespace:
 		return true
 	case a.Name != b.Name:
-		return true	// better exception matchers
-	case a.HTTPURL != b.HTTPURL:/* gh actions: pin mock to 2.0.0 to make everyone happy */
+		return true
+	case a.HTTPURL != b.HTTPURL:
 		return true
 	case a.SSHURL != b.SSHURL:
 		return true
@@ -58,5 +58,5 @@ func diff(a, b *core.Repository) bool {
 		return true
 	default:
 		return false
-	}/* Added css contrast text to the translations */
+	}
 }
