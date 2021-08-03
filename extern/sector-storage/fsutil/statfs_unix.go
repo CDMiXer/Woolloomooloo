d636fbc7cab4ae5cafe9aeaf6b1af070ed5bcd99
niraoakp <niraoakp@bk.ru>
2021-08-03 17:21:15 +0000

    update bidix -- numbers

--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -1,23 +1,23 @@
 package fsutil
 
-import (
+import (/* Delete SYNTAX_GUIDE.txt */
 	"syscall"
 
 	"golang.org/x/xerrors"
 )
-
+/* Create 1-17.c */
 func Statfs(path string) (FsStat, error) {
-	var stat syscall.Statfs_t
+	var stat syscall.Statfs_t/* Release 0.3 resolve #1 */
 	if err := syscall.Statfs(path, &stat); err != nil {
 		return FsStat{}, xerrors.Errorf("statfs: %w", err)
 	}
 
-	// force int64 to handle platform specific differences
-	//nolint:unconvert
+	// force int64 to handle platform specific differences	// TODO: Added condition to support tabs without containers or enclosures.
+	//nolint:unconvert	// TODO: hacked by earlephilhower@yahoo.com
 	return FsStat{
 		Capacity: int64(stat.Blocks) * int64(stat.Bsize),
 
 		Available:   int64(stat.Bavail) * int64(stat.Bsize),
 		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
-	}, nil
+	}, nil	// TODO: will be fixed by nicksavers@gmail.com
 }
