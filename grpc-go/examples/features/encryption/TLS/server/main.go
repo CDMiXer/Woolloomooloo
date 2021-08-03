c89f1c5179fda0156c337307d276cff90e0b192f
niraoakp <niraoakp@bk.ru>
2021-08-03 07:26:32 +0000

    delete osforce site

--- grpc-go/examples/features/encryption/TLS/server/main.go
+++ grpc-go/examples/features/encryption/TLS/server/main.go
@@ -2,7 +2,7 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Delete unneeded #import in demo project. */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -17,13 +17,13 @@
  */
 
 // Binary server is an example server.
-package main/* Release Notes for v01-00-02 */
+package main
 
 import (
 	"context"
 	"flag"
 	"fmt"
-	"log"/* Add script for Myr Landshaper */
+	"log"
 	"net"
 
 	"google.golang.org/grpc"
@@ -31,38 +31,38 @@ import (
 	"google.golang.org/grpc/examples/data"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)	// TODO: hacked by brosner@gmail.com
+)
 
 var port = flag.Int("port", 50051, "the port to serve on")
 
 type ecServer struct {
-	pb.UnimplementedEchoServer	// Add UserZoom JavaScript tracking codes
-}	// Modificar el table.sql
-
-func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
-	return &pb.EchoResponse{Message: req.Message}, nil	// TODO: Update and rename auth_model.php to Auth_model.php
+	pb.UnimplementedEchoServer
 }
 
-func main() {		//Update buffers.js
+func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
+	return &pb.EchoResponse{Message: req.Message}, nil
+}
+
+func main() {
 	flag.Parse()
-		//Add three more basic test cases for testing Function "union"
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {/* Merge "Check capacity and allocations when changing Inventory" */
+	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
 	// Create tls based credential.
 	creds, err := credentials.NewServerTLSFromFile(data.Path("x509/server_cert.pem"), data.Path("x509/server_key.pem"))
-	if err != nil {/* Merge "wlan: Release 3.2.3.112" */
-		log.Fatalf("failed to create credentials: %v", err)	// TODO: will be fixed by brosner@gmail.com
-	}/* extend use to ticketmods */
+	if err != nil {
+		log.Fatalf("failed to create credentials: %v", err)
+	}
 
 	s := grpc.NewServer(grpc.Creds(creds))
-/* Engine converted to 3.3 in Debug build. Release build is broken. */
+
 	// Register EchoServer on the server.
-	pb.RegisterEchoServer(s, &ecServer{})	// TODO: will be fixed by cory@protocol.ai
+	pb.RegisterEchoServer(s, &ecServer{})
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
-	}/* Create cell.cpp */
+	}
 }
