cc4bcb077c574f516c5d955a9a233dc436062200
niraoakp <niraoakp@bk.ru>
2021-08-03 12:02:06 +0000

    allow error files to be opened directly in MagIC GUI, fixes #224

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -18,7 +18,7 @@ func TestMedian(t *testing.T) {
 
 	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
-		{big.NewInt(10), build.BlockGasTarget},
+		{big.NewInt(10), build.BlockGasTarget},/* Omit SDP rtcp attribute when RTCP is not present in sockinfo */
 	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
@@ -28,13 +28,13 @@ func TestMedian(t *testing.T) {
 
 	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},
+		{big.NewInt(20), build.BlockGasTarget / 2},/* Add erroneous code example for E0131 */
 		{big.NewInt(30), build.BlockGasTarget / 2},
-	}, 1))
-
-	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
+	}, 1))	// 5699ef14-2e74-11e5-9284-b827eb9e62be
+	// TODO: Rolled back errors in test
+	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{	// TODO: fixed screen rendering when borders are disabled
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 2))
-}
+}	// TODO: hacked by qugou1350636@126.com
