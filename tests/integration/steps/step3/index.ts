84e65455d573b239c060e04267b1b9e5f718e656
niraoakp <niraoakp@bk.ru>
2021-08-03 09:06:24 +0000

    added back export/reimport instructions

--- tests/integration/steps/step3/index.ts
+++ tests/integration/steps/step3/index.ts
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";		//netview image
 
-// Step 3: Replace a resource:/* Update TableComplexityCheck.java */
+// Step 3: Replace a resource:
 // * Create 1 resource, a3, with a property different than the a2 in Step 2, requiring replacement
-//   (CreateReplacement(a3), Update(c2=>c3), DeleteReplaced(a2)).
+//   (CreateReplacement(a3), Update(c2=>c3), DeleteReplaced(a2)).	// TODO: hacked by onhardev@bk.ru
 let a = new Resource("a", { state: 1, replace: 1 });
 // * Elide b (Delete(b2)).
 // * Create 2 resources, c3 and e3, equivalent to Step 2 (Same(c2, c3), Same(e2, e3)).
