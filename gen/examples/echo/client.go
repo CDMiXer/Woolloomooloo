8b1fe6259fc7a7d845a002215b14f86cc8cab228
niraoakp <niraoakp@bk.ru>
2021-08-03 04:48:14 +0000

    Merge "Remove threadlocal engine strategy, engine strategies pool threadlocal"

--- gen/examples/echo/client.go
+++ gen/examples/echo/client.go
@@ -1,12 +1,12 @@
-// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.	// TODO: Redirect after successful Entry upload
-// Use of this source code is governed by a BSD-style/* Listando evento atual, todos apenas para a api. */
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 // +build ignore
-	// TODO: Update ContatoServidor.java
-package main/* Release version 2.2.0.RELEASE */
-/* ec38f694-2e62-11e5-9284-b827eb9e62be */
-import (		//Se agregó date picker y timepicker
+
+package main
+
+import (
 	"flag"
 	"log"
 	"net/url"
@@ -16,30 +16,30 @@ import (		//Se agregó date picker y timepicker
 
 	"github.com/gorilla/websocket"
 )
-	// TODO: Removing some more unnecessary manual quotes from attribute diagnostics.
-var addr = flag.String("addr", "localhost:8080", "http service address")	// TODO: 5yb3V6WOwvn7LBcqFv3iIfveVXPhZnBK
-		//Passing path to homerun
+
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
 func main() {
 	flag.Parse()
-	log.SetFlags(0)/* Drop mtune flags */
+	log.SetFlags(0)
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
-	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}		//Clean Ids after delete.
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
 	log.Printf("connecting to %s", u.String())
-/* Озвучивание анекдотов */
+
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
 		log.Fatal("dial:", err)
-	}/* Release 0.22.2. */
+	}
 	defer c.Close()
 
 	done := make(chan struct{})
-		//Added disqussion
+
 	go func() {
 		defer close(done)
-		for {	// TODO: add exists to check
+		for {
 			_, message, err := c.ReadMessage()
 			if err != nil {
 				log.Println("read:", err)
@@ -52,7 +52,7 @@ func main() {
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
 
-	for {	// TODO: will be fixed by davidad@alum.mit.edu
+	for {
 		select {
 		case <-done:
 			return
