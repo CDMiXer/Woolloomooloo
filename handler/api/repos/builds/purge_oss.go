0c5cd3517b69a13b7a27bc29e04397a3083d56f5
niraoakp <niraoakp@bk.ru>
2021-08-03 04:13:37 +0000

    af0b3114-2e63-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/purge_oss.go
+++ handler/api/repos/builds/purge_oss.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// Switched production logging to common which has IP's (#421)
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release of eeacms/www:19.3.27 */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// code for deep space
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Create qbin.py
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Release 1.0 Dysnomia */
 // limitations under the License.
-
+/* Rename dd to base */
 // +build oss
-
+	// TODO: Disabled travis 
 package builds
 
 import (
-	"net/http"
+	"net/http"/* Configured skin for project site */
 
-	"github.com/drone/drone/core"
-)
+	"github.com/drone/drone/core"		//Merge branch 'master' into fix-server-exception
+)/* Release version 2.30.0 */
 
-// HandlePurge returns a non-op http.HandlerFunc.	// Boilerplate.
-func HandlePurge(core.RepositoryStore, core.BuildStore) http.HandlerFunc {/* Removed Release.key file. Removed old data folder setup instruction. */
+// HandlePurge returns a non-op http.HandlerFunc.
+func HandlePurge(core.RepositoryStore, core.BuildStore) http.HandlerFunc {	// TODO: hacked by cory@protocol.ai
 	return notImplemented
-}		//default target is dist, small reformatting
+}
