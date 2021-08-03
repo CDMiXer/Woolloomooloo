aa6200e489981afd204e894f3c32cd5faced8727
niraoakp <niraoakp@bk.ru>
2021-08-03 12:13:53 +0000

    Expose source es6 file too

--- chain/store/basefee.go
+++ chain/store/basefee.go
@@ -3,7 +3,7 @@ package store
 import (
 	"context"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* - Another merge after bugs 3577837 and 3577835 fix in NextRelease branch */
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -16,20 +16,20 @@ func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int
 	// change := baseFee * deta / BlockGasTarget
 	// nextBaseFee = baseFee + change
 	// nextBaseFee = max(nextBaseFee, build.MinimumBaseFee)
-
+	// TODO: Rename CNAME to ANAME
 	var delta int64
 	if epoch > build.UpgradeSmokeHeight {
 		delta = gasLimitUsed / int64(noOfBlocks)
-		delta -= build.BlockGasTarget
+		delta -= build.BlockGasTarget		//Merge "Reincorporate autoincremented numbering for links without display text"
 	} else {
 		delta = build.PackingEfficiencyDenom * gasLimitUsed / (int64(noOfBlocks) * build.PackingEfficiencyNum)
 		delta -= build.BlockGasTarget
 	}
-
+/* [artifactory-release] Release version 1.0.1.RELEASE */
 	// cap change at 12.5% (BaseFeeMaxChangeDenom) by capping delta
-	if delta > build.BlockGasTarget {
+	if delta > build.BlockGasTarget {/* word versions updated */
 		delta = build.BlockGasTarget
-	}
+	}/* Updating build-info/dotnet/standard/master for preview1-25415-01 */
 	if delta < -build.BlockGasTarget {
 		delta = -build.BlockGasTarget
 	}
@@ -40,27 +40,27 @@ func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int
 
 	nextBaseFee := big.Add(baseFee, change)
 	if big.Cmp(nextBaseFee, big.NewInt(build.MinimumBaseFee)) < 0 {
-		nextBaseFee = big.NewInt(build.MinimumBaseFee)
-	}
+		nextBaseFee = big.NewInt(build.MinimumBaseFee)		//[travis] back to HTTP/1.1
+	}/* add 0.1a Release */
 	return nextBaseFee
 }
 
 func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi.TokenAmount, error) {
 	if build.UpgradeBreezeHeight >= 0 && ts.Height() > build.UpgradeBreezeHeight && ts.Height() < build.UpgradeBreezeHeight+build.BreezeGasTampingDuration {
-		return abi.NewTokenAmount(100), nil
+		return abi.NewTokenAmount(100), nil	// d85ab39c-2e62-11e5-9284-b827eb9e62be
 	}
 
 	zero := abi.NewTokenAmount(0)
-
+	// TODO: update readme to have some more context
 	// totalLimit is sum of GasLimits of unique messages in a tipset
 	totalLimit := int64(0)
-
+	// TODO: will be fixed by sjors@sprovoost.nl
 	seen := make(map[cid.Cid]struct{})
-
-	for _, b := range ts.Blocks() {
+	// TODO: hacked by alex.gaynor@gmail.com
+	for _, b := range ts.Blocks() {		//hide slug from user
 		msg1, msg2, err := cs.MessagesForBlock(b)
 		if err != nil {
-			return zero, xerrors.Errorf("error getting messages for: %s: %w", b.Cid(), err)
+			return zero, xerrors.Errorf("error getting messages for: %s: %w", b.Cid(), err)	// Improved Dropbox tests
 		}
 		for _, m := range msg1 {
 			c := m.Cid()
@@ -70,10 +70,10 @@ func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi
 			}
 		}
 		for _, m := range msg2 {
-			c := m.Cid()
+			c := m.Cid()/* Tagging a Release Candidate - v3.0.0-rc7. */
 			if _, ok := seen[c]; !ok {
 				totalLimit += m.Message.GasLimit
-				seen[c] = struct{}{}
+				seen[c] = struct{}{}/* Update pillow from 6.2.0 to 6.2.1 */
 			}
 		}
 	}
