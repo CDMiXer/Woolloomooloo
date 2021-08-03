7985a60a7793dbfb03e0ba64b38cab0a01cb8d10
niraoakp <niraoakp@bk.ru>
2021-08-03 13:19:55 +0000

    added Wistful Selkie

--- core/netrc_test.go
+++ core/netrc_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by davidad@alum.mit.edu
-// that can be found in the LICENSE file.	// TODO: removing of asciidoc project
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by igor@soramitsu.co.jp
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
-// +build !oss/* Release notes for 1.0.61 */
-
+// +build !oss
+/* Post deleted: Projects */
 package core
