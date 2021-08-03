41dff7382ae0a2d06a5c8b3861add12ed179f786
niraoakp <niraoakp@bk.ru>
2021-08-03 02:32:36 +0000

    Spring + Rest Service initialization example

--- handler/api/secrets/create_test.go
+++ handler/api/secrets/create_test.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release Process: Change pom.xml version to 1.4.0-SNAPSHOT. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Merge branch 'dev' into feature-window
 
 // +build !oss
 
@@ -10,68 +10,68 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"net/http"	// TODO: Merge "VPNaaS Service Driver for Cisco CSR"
-	"net/http/httptest"	// TODO: a2625c60-2e47-11e5-9284-b827eb9e62be
-	"testing"
+	"net/http"
+	"net/http/httptest"
+	"testing"/* Release 5.0.4 */
 
-	"github.com/drone/drone/core"	// Fix scripted test
-	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/errors"/* All links updated */
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"		//docker: build cmake for xenial in parallel
-	"github.com/google/go-cmp/cmp"
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-cmp/cmp"		//Mistake in testing with Python 2.
 )
-
+	// TODO: will be fixed by steven@stebalien.com
 func TestHandleCreate(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Release: Splat 9.0 */
+	defer controller.Finish()
 
 	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)
 
-	c := new(chi.Context)	// TODO: hacked by ligi@ligi.de
+	c := new(chi.Context)	// Cws koheidatapilot02 is in dev300-m37
 	c.URLParams.Add("namespace", "octocat")
 
-	in := new(bytes.Buffer)/* Delete flag */
+	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(dummySecret)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
-	r = r.WithContext(
+	r = r.WithContext(	// If there are two Server reconnect!
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-
+		//Update errors.md
 	HandleCreate(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := &core.Secret{}, dummySecretScrubbed	// TODO: Delete brunton.theme.bash~
-	json.NewDecoder(w.Body).Decode(got)
+	got, want := &core.Secret{}, dummySecretScrubbed
+	json.NewDecoder(w.Body).Decode(got)/* Merge "Setup Keystone catalog information for Ceilometer" */
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
-	// TODO: will be fixed by vyzo@hackzen.org
+
 func TestHandleCreate_ValidationError(t *testing.T) {
-)t(rellortnoCweN.kcomog =: rellortnoc	
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	c := new(chi.Context)
-	c.URLParams.Add("namespace", "octocat")	// Panels now have thin outer borders.
+	c := new(chi.Context)		//Add pool/add and pool/remove API methods.
+	c.URLParams.Add("namespace", "octocat")	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(&core.Secret{Name: "", Data: "pa55word"})
-
-	w := httptest.NewRecorder()
+	// TODO: Move [Interface]s from 'internal' to 'core' package.
+	w := httptest.NewRecorder()		//touchstart
 	r := httptest.NewRequest("GET", "/", in)
-	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	r = r.WithContext(/* Merge branch 'master' into AVAB_array */
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),		//Update #1184
 	)
 
 	HandleCreate(nil).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusBadRequest; want != got {
+	if got, want := w.Code, http.StatusBadRequest; want != got {		//implement top level defaulting.
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
@@ -79,8 +79,8 @@ func TestHandleCreate_ValidationError(t *testing.T) {
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}/* A new Release jar */
-}	// TODO: Improve SingleConverter + tests
+	}
+}
 
 func TestHandleCreate_BadRequest(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -89,7 +89,7 @@ func TestHandleCreate_BadRequest(t *testing.T) {
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
 
-	w := httptest.NewRecorder()/* Adding GitHoard to "Made with" section */
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
