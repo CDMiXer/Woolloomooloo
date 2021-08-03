10a4fa080212277aec98d578bb0333033449162e
niraoakp <niraoakp@bk.ru>
2021-08-03 02:58:42 +0000

    fixing script src to point to correct js file main.js

--- plugin/config/jsonnet_oss.go
+++ plugin/config/jsonnet_oss.go
@@ -2,12 +2,12 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by arajasek94@gmail.com
+// You may obtain a copy of the License at/* Acknowledgements should mention Lanyon */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software	// TODO: Merge "Limit the number of preloader sessions to one."
+// distributed under the License is distributed on an "AS IS" BASIS,		//Shorten README.md to minimum in favor of readme.io
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -19,6 +19,6 @@ package config
 import "github.com/drone/drone/core"
 
 // Jsonnet returns a no-op configuration service.
-func Jsonnet(service core.FileService, enabled bool) core.ConfigService {/* Release version 1.2.4 */
-	return new(noop)
-}		//Create datepickr.js
+func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
+	return new(noop)	// TODO: will be fixed by alex.gaynor@gmail.com
+}
