1ecd056a338bff319a1ce5fc4ce3ad1741076b21
niraoakp <niraoakp@bk.ru>
2021-08-03 20:15:41 +0000

    Adding FastDTW1.1.0 jar (newer version)

--- tests/integration/construct_component/python/__main__.py
+++ tests/integration/construct_component/python/__main__.py
@@ -1,6 +1,6 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-from component import Component/* Update api-mailinglists.rst */
+from component import Component
 
 component_a = Component("a", echo=42)
 component_b = Component("b", echo=component_a.echo)
