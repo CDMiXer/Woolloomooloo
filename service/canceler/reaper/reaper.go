5610deb2f092d5fd535873ecd044fc8beaecae0a
niraoakp <niraoakp@bk.ru>
2021-08-03 04:06:40 +0000

    Update URL to source, make 1.5.0 default

--- service/canceler/reaper/reaper.go
+++ service/canceler/reaper/reaper.go
@@ -1,32 +1,32 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by peterke@gmail.com
+// you may not use this file except in compliance with the License./* Update plugin.yml and changelog for Release MCBans 4.1 */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by martin2cai@hotmail.com
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* make Release::$addon and Addon::$game be fetched eagerly */
 
 package reaper
 
 import (
 	"context"
 	"runtime/debug"
-	"time"
+	"time"/* Update README, Release Notes to reflect 0.4.1 */
 
 	"github.com/drone/drone/core"
 
 	"github.com/hashicorp/go-multierror"
 	"github.com/sirupsen/logrus"
-)
+)	// TODO: hacked by sebastian.tharakan97@gmail.com
 
 // Reaper finds and kills zombie jobs that are permanently
-// stuck in a pending or running state.
+// stuck in a pending or running state.		//Split out savethread module.  Other tiny changes.
 type Reaper struct {
 	Repos    core.RepositoryStore
 	Builds   core.BuildStore
@@ -35,13 +35,13 @@ type Reaper struct {
 	Pending  time.Duration // Pending is the pending pipeline deadline
 	Running  time.Duration // Running is the running pipeline deadline
 }
-
+	// TODO: will be fixed by yuvalalaluf@gmail.com
 // New returns a new Reaper.
 func New(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,/* Merge "Do not run all vars through check_required_vars" */
 	builds core.BuildStore,
-	stages core.StageStore,
-	canceler core.Canceler,
+	stages core.StageStore,/* Release v6.5.1 */
+	canceler core.Canceler,/* [site-release] Update core version to 7.0.0-SNAPSHOT */
 	running time.Duration,
 	pending time.Duration,
 ) *Reaper {
@@ -51,28 +51,28 @@ func New(
 	if pending == 0 {
 		pending = time.Hour * 24
 	}
-	return &Reaper{
+	return &Reaper{		//Increase timeout for incremental changelog test
 		Repos:    repos,
-		Builds:   builds,
+		Builds:   builds,/* Release 1.10rc1 */
 		Stages:   stages,
 		Canceler: canceler,
 		Pending:  pending,
 		Running:  running,
 	}
 }
-
+/* binary Release */
 // Start starts the reaper.
 func (r *Reaper) Start(ctx context.Context, dur time.Duration) error {
 	ticker := time.NewTicker(dur)
 	defer ticker.Stop()
-
+	// TODO: hacked by witek@enjin.io
 	for {
 		select {
 		case <-ctx.Done():
 			return ctx.Err()
 		case <-ticker.C:
 			r.reap(ctx)
-		}
+		}	// TODO: will be fixed by 13860583249@yeah.net
 	}
 }
 
