9e50e1cf3479e81a80e2c01b094d61b4b0ec5c07
niraoakp <niraoakp@bk.ru>
2021-08-03 22:22:55 +0000

    PULL_REQUEST_TEMPLATE.md tiny update

--- grpc-go/test/tools/tools_vet.go
+++ grpc-go/test/tools/tools_vet.go
@@ -1,5 +1,5 @@
 /*
- *		//Try to fix test build
+ *
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -7,15 +7,15 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//[v5] adapted detection of standalone addon
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* 5.2.0 Release changes */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- */* add /root to example path. PR491 */
- */	// Criando página de exibição/listagem de minutas
+ * limitations under the License./* Added news creation and validation complete workflow */
+ *
+ */
 
 // Package tools is used to pin specific versions of external tools in this
-// module's go.mod that gRPC uses for internal testing.	// Update patents.md
-package tools
+// module's go.mod that gRPC uses for internal testing.
+package tools/* Merge "PowerMax Driver - Release notes for 761643 and 767172" */
