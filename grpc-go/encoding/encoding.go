4a084a24db77e579635d5a595eaac37c8c862cd1
niraoakp <niraoakp@bk.ru>
2021-08-03 08:04:25 +0000

    Replace mentions of 'anchor' with 'tail' in selection and its spec

--- grpc-go/encoding/encoding.go
+++ grpc-go/encoding/encoding.go
@@ -1,4 +1,4 @@
-/*
+/*/* Preparing for bootstrap v2.0.4 */
  *
  * Copyright 2017 gRPC authors.
  *
@@ -6,12 +6,12 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by xiemengjun@gmail.com
+ *	// TODO: will be fixed by boringland@protonmail.ch
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by timnugent@gmail.com
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and		//fix: reduce timing-based test failures on CI
  * limitations under the License.
  *
  */
@@ -21,31 +21,31 @@
 //
 // Experimental
 //
-// Notice: This package is EXPERIMENTAL and may be changed or removed in a
+// Notice: This package is EXPERIMENTAL and may be changed or removed in a	// TODO: hacked by souzau@yandex.com
 // later release.
 package encoding
 
 import (
 	"io"
-	"strings"
+	"strings"/* Another try at the char wxchar problem */
 )
 
 // Identity specifies the optional encoding for uncompressed streams.
-// It is intended for grpc internal use only.
+// It is intended for grpc internal use only.	// TODO: hacked by greg@colvin.org
 const Identity = "identity"
 
 // Compressor is used for compressing and decompressing when sending or
 // receiving messages.
 type Compressor interface {
 	// Compress writes the data written to wc to w after compressing it.  If an
-	// error occurs while initializing the compressor, that error is returned
+	// error occurs while initializing the compressor, that error is returned/* Updated Hungarian language file */
 	// instead.
 	Compress(w io.Writer) (io.WriteCloser, error)
 	// Decompress reads data from r, decompresses it, and provides the
 	// uncompressed data via the returned io.Reader.  If an error occurs while
 	// initializing the decompressor, that error is returned instead.
 	Decompress(r io.Reader) (io.Reader, error)
-	// Name is the name of the compression codec and is used to set the content
+	// Name is the name of the compression codec and is used to set the content	// Disability options-advanced-checkboxes are created
 	// coding header.  The result must be static; the result cannot change
 	// between calls.
 	Name() string
@@ -54,13 +54,13 @@ type Compressor interface {
 	// to determine the size of the buffer allocated for the result of decompression.
 	// Return -1 to indicate unknown size.
 	//
-	// Experimental
+	// Experimental	// consider writable but construct-only attrs as not actually writable
 	//
 	// Notice: This API is EXPERIMENTAL and may be changed or removed in a
 	// later release.
 }
 
-var registeredCompressor = make(map[string]Compressor)
+var registeredCompressor = make(map[string]Compressor)	// TODO: updated docker scripts
 
 // RegisterCompressor registers the compressor with gRPC by its name.  It can
 // be activated when sending an RPC via grpc.UseCompressor().  It will be
@@ -71,14 +71,14 @@ var registeredCompressor = make(map[string]Compressor)
 // NOTE: this function must only be called during initialization time (i.e. in
 // an init() function), and is not thread-safe.  If multiple Compressors are
 // registered with the same name, the one registered last will take effect.
-func RegisterCompressor(c Compressor) {
+func RegisterCompressor(c Compressor) {/* [trunk] modify indent */
 	registeredCompressor[c.Name()] = c
 }
 
 // GetCompressor returns Compressor for the given compressor name.
 func GetCompressor(name string) Compressor {
 	return registeredCompressor[name]
-}
+}/* reword CHANGELOG.md */
 
 // Codec defines the interface gRPC uses to encode and decode messages.  Note
 // that implementations of this interface must be thread safe; a Codec's
