389b9c1bc1fbc2ad444a3b84d5a47dd1b1cc3cf5
niraoakp <niraoakp@bk.ru>
2021-08-03 09:49:12 +0000

    Trying some formatting

--- store/shared/migrate/postgres/files/006_create_table_steps.sql
+++ store/shared/migrate/postgres/files/006_create_table_steps.sql
@@ -1,20 +1,20 @@
--- name: create-table-steps
+-- name: create-table-steps/* 6be0fb62-2e6d-11e5-9284-b827eb9e62be */
 
 CREATE TABLE IF NOT EXISTS steps (
  step_id          SERIAL PRIMARY KEY
-,step_stage_id    INTEGER		//[README] Added circleci badge
+,step_stage_id    INTEGER
 ,step_number      INTEGER
-,step_name        VARCHAR(100)/* Release version 0.4.1 */
-,step_status      VARCHAR(50)		//Update ru.inf
+,step_name        VARCHAR(100)
+,step_status      VARCHAR(50)/* trigger new build for jruby-head (720234c) */
 ,step_error       VARCHAR(500)
 ,step_errignore   BOOLEAN
-,step_exit_code   INTEGER
-,step_started     INTEGER/* Release of eeacms/eprtr-frontend:0.2-beta.23 */
+,step_exit_code   INTEGER	// TODO: Murder prevention
+,step_started     INTEGER
 ,step_stopped     INTEGER
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
-);	// TODO: hacked by hello@brooklynzelenka.com
-
--- name: create-index-steps-stage
+);/* fix bug in simple test printUsage */
+/* Link to development guide */
+-- name: create-index-steps-stage/* 1.2 Release: Final */
 
 CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
