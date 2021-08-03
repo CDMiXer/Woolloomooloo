c366c9d4473fba6795c0c11c2c4af60aec9264cd
niraoakp <niraoakp@bk.ru>
2021-08-03 20:01:20 +0000

    Use new-style cabal syntax

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//8ab115f6-2e48-11e5-9284-b827eb9e62be
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by why@ipfs.io
 // that can be found in the LICENSE file.
-/* * NEWS: Release 0.2.10 */
+	// TODO: hacked by witek@enjin.io
 // +build !oss
 
 package mockscm
 
-//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService		//Extend hardware support notes in README
+//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService
