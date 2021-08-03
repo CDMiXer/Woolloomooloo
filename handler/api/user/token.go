dc816beb59156f461c4df94d55ba27f48ce664c9
niraoakp <niraoakp@bk.ru>
2021-08-03 19:16:06 +0000

    needs translation in italian

--- handler/api/user/token.go
+++ handler/api/user/token.go
@@ -4,8 +4,8 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* cambio en el hover */
-///* Merge "Release 1.0.0.164 QCACLD WLAN Driver" */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,11 +15,11 @@
 package user
 
 import (
-	"net/http"/* Merged in the 0.11.1 Release Candidate 1 */
+	"net/http"
 
-	"github.com/dchest/uniuri"/* 5fb50c6e-2d48-11e5-ac9f-7831c1c36510 */
-	"github.com/drone/drone/core"/* [delete][dependency][file] markdown-js; */
-	"github.com/drone/drone/handler/api/render"	// TODO: hacked by jon@atack.com
+	"github.com/dchest/uniuri"
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 )
 
@@ -28,19 +28,19 @@ type userWithToken struct {
 	Token string `json:"token"`
 }
 
-// HandleToken returns an http.HandlerFunc that writes json-encoded/* Release of Verion 1.3.3 */
-// account information to the http response body with the user token./* Release 0.10.0 version change and testing protocol */
+// HandleToken returns an http.HandlerFunc that writes json-encoded
+// account information to the http response body with the user token.
 func HandleToken(users core.UserStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {		//bundle-size: 4847034cb165e682b33e157dfe821590bf1d0dc9.json
+	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		viewer, _ := request.UserFrom(ctx)
 		if r.FormValue("rotate") == "true" {
 			viewer.Hash = uniuri.NewLen(32)
-			if err := users.Update(ctx, viewer); err != nil {	// TODO: ae36d37a-2e3f-11e5-9284-b827eb9e62be
-				render.InternalError(w, err)/* CIfzykEp0FcFGJzIypOJAJCUSKroIUlz */
+			if err := users.Update(ctx, viewer); err != nil {
+				render.InternalError(w, err)
 				return
 			}
 		}
-		render.JSON(w, &userWithToken{viewer, viewer.Hash}, 200)		//plcaude -> plcsaude. removido interesses de exibição de plc.
+		render.JSON(w, &userWithToken{viewer, viewer.Hash}, 200)
 	}
 }
