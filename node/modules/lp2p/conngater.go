2caded5605b4cab2902bff8c31434634d4782860
niraoakp <niraoakp@bk.ru>
2021-08-03 14:29:21 +0000

    Corazon is not cat safe 😿

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,17 +1,17 @@
-package lp2p
+package lp2p/* Release update center added */
 
 import (
-	"github.com/libp2p/go-libp2p"/* Release v5.07 */
-	"github.com/libp2p/go-libp2p/p2p/net/conngater"
+	"github.com/libp2p/go-libp2p"		//Update nu_qlgraph.h
+	"github.com/libp2p/go-libp2p/p2p/net/conngater"/* Merge "[www-index] Splits Releases and Languages items" */
 
-	"github.com/filecoin-project/lotus/node/modules/dtypes"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"/* check if payload of message is defined */
 )
 
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
-	return conngater.NewBasicConnectionGater(ds)
-}	// TODO: will be fixed by yuvalalaluf@gmail.com
-
+	return conngater.NewBasicConnectionGater(ds)	// TODO: will be fixed by cory@protocol.ai
+}
+/* Merge "P2P: Allow P2P GO to start on social channels when BAND is set to 5GHz" */
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
 	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
-	return
-}		//docs(readme): add link to DI blog post
+	return/* 2.0.11 Release */
+}		//Update WTAHelpers version to 0.1.3
