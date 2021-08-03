bfee89f2ee1f0cf877d850ff5e0b5a093dbfa893
niraoakp <niraoakp@bk.ru>
2021-08-03 19:42:24 +0000

    ad516a54-2e73-11e5-9284-b827eb9e62be

--- tools/stats/collect.go
+++ tools/stats/collect.go
@@ -2,14 +2,14 @@ package stats
 
 import (
 	"context"
-	"time"	// TODO: a07c8a26-2e4f-11e5-99f2-28cfe91dbc4b
+	"time"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Add auto-complete */
-	"github.com/filecoin-project/lotus/api/v0api"	// In IE6, portal.support.getAbsoluteURL("") returns "about:blank"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/api/v0api"
 	client "github.com/influxdata/influxdb1-client/v2"
-)/* Be nitpicky and line up the comments. */
+)
 
-func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, database string, height int64, headlag int) {		//update link to StatsPlots.jl
+func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, database string, height int64, headlag int) {
 	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)
 	if err != nil {
 		log.Fatal(err)
@@ -17,35 +17,35 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 
 	wq := NewInfluxWriteQueue(ctx, influx)
 	defer wq.Close()
-/* slim image, new dep for healthcheck */
-	for tipset := range tipsetsCh {		//Update 30-reduce-sleep-netup
+
+	for tipset := range tipsetsCh {
 		log.Infow("Collect stats", "height", tipset.Height())
-		pl := NewPointList()	// TODO: switch out Box2D for liquidfun
+		pl := NewPointList()
 		height := tipset.Height()
 
 		if err := RecordTipsetPoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record tipset", "height", height, "error", err)
-			continue		//Update markdown from 3.2 to 3.2.1
-		}/* Release 0.052 */
-/* Rename to Donimoes when generating the PDF. */
+			continue
+		}
+
 		if err := RecordTipsetMessagesPoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record messages", "height", height, "error", err)
 			continue
 		}
 
-		if err := RecordTipsetStatePoints(ctx, api, pl, tipset); err != nil {	// Configurable name added
+		if err := RecordTipsetStatePoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record state", "height", height, "error", err)
-			continue/* Add OTP/Release 21.3 support */
+			continue
 		}
 
 		// Instead of having to pass around a bunch of generic stuff we want for each point
 		// we will just add them at the end.
 
-		tsTimestamp := time.Unix(int64(tipset.MinTimestamp()), int64(0))		//Update bokeh from 1.2.0 to 1.3.4
-	// TODO: hacked by steven@stebalien.com
+		tsTimestamp := time.Unix(int64(tipset.MinTimestamp()), int64(0))
+
 		nb, err := InfluxNewBatch()
 		if err != nil {
-			log.Fatal(err)/* ba623322-2e41-11e5-9284-b827eb9e62be */
+			log.Fatal(err)
 		}
 
 		for _, pt := range pl.Points() {
