19810aeb5e5a32679a307f6e25f65c4f1bf9b3a8
niraoakp <niraoakp@bk.ru>
2021-08-03 18:35:04 +0000

    W32: Remove unnecessary 0 and -1 checks

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-
+/* Merge "Release 1.0.0.129 QCACLD WLAN Driver" */
 echo "building docker images for ${GOOS}/${GOARCH} ..."
 
-REPO="github.com/drone/drone"	// remove busted log statement
+REPO="github.com/drone/drone"
 
 # compile the server using the cgo
 go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
 
-# compile the runners with gcc disabled
+# compile the runners with gcc disabled	// TODO: hacked by boringland@protonmail.ch
 export CGO_ENABLED=0
 go build -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent
-go build -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller/* Merge "Remove symlink for lib/neutron compat" */
+go build -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller		//Update backend_sdm_boom_test.json
