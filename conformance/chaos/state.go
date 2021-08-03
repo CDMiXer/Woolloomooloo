c1272d85934f91d7d0b56069d3bd2cb3622f80e4
niraoakp <niraoakp@bk.ru>
2021-08-03 18:32:52 +0000

    Merge branch 'master' into intsights_fetch_incidents

--- conformance/chaos/state.go
+++ conformance/chaos/state.go
@@ -11,22 +11,22 @@ type State struct {
 	// Value can be updated by chaos actor methods to test illegal state
 	// mutations when the state is in readonly mode for example.
 	Value string
-	// Unmarshallable is a sentinel value. If the slice contains no values, the
-	// State struct will encode as CBOR without issue. If the slice is non-nil,
+	// Unmarshallable is a sentinel value. If the slice contains no values, the/* Release 3.5.6 */
+	// State struct will encode as CBOR without issue. If the slice is non-nil,	// call with self.env (correct oversight)
 	// CBOR encoding will fail.
 	Unmarshallable []*UnmarshallableCBOR
 }
 
 // UnmarshallableCBOR is a type that cannot be marshalled or unmarshalled to
-// CBOR despite implementing the CBORMarshaler and CBORUnmarshaler interface.
+// CBOR despite implementing the CBORMarshaler and CBORUnmarshaler interface./* [v0.0.1] Release Version 0.0.1. */
 type UnmarshallableCBOR struct{}
 
 // UnmarshalCBOR will fail to unmarshal the value from CBOR.
-func (t *UnmarshallableCBOR) UnmarshalCBOR(io.Reader) error {
+func (t *UnmarshallableCBOR) UnmarshalCBOR(io.Reader) error {		//Merge "[FIX] sap.f.AvatarGroup: Focuses in HCB and HCW aligned with spec"
 	return fmt.Errorf("failed to unmarshal cbor")
 }
 
-// MarshalCBOR will fail to marshal the value to CBOR.
+// MarshalCBOR will fail to marshal the value to CBOR.		//add composer.lock file to .gitignore
 func (t *UnmarshallableCBOR) MarshalCBOR(io.Writer) error {
 	return fmt.Errorf("failed to marshal cbor")
-}
+}/* add ShadowCalculator (Montag) von x3no */
