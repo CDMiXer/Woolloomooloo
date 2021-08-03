4521af0c18c7a28af4423bc687f465fa8660cad5
niraoakp <niraoakp@bk.ru>
2021-08-03 15:39:38 +0000

    Fix to kore not teleport on homunculus

--- store/shared/migrate/sqlite/files/007_create_table_logs.sql
+++ store/shared/migrate/sqlite/files/007_create_table_logs.sql
@@ -1,7 +1,7 @@
 -- name: create-table-logs
-
+		//Add require exp-assynccache
 CREATE TABLE IF NOT EXISTS logs (
  log_id    INTEGER PRIMARY KEY
-,log_data  BLOB
+,log_data  BLOB	// TODO: Adapt code to new Master.
 ,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE
-);
+);/* refactoring, new program class */
