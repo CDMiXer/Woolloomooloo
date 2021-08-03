4133a77507077b7ed1442d03755ddc0780c4b941
niraoakp <niraoakp@bk.ru>
2021-08-03 00:35:01 +0000

    Precedence >.<

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -1,12 +1,12 @@
-package repo/* Released SDK v1.5.1 */
+package repo
 
 import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
 
-// BadgerBlockstoreOptions returns the badger options to apply for the provided	// TODO: OTQtOTcsIDEwMiwgMTAzCg==
+// BadgerBlockstoreOptions returns the badger options to apply for the provided
 // domain.
 func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {
 	opts := badgerbs.DefaultOptions(path)
-/* popravljen shiny - povečan height na 1100 */
+
 	// Due to legacy usage of blockstore.Blockstore, over a datastore, all
 	// blocks are prefixed with this namespace. In the future, this can go away,
 	// in order to shorten keys, but it'll require a migration.
@@ -18,29 +18,29 @@ func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool
 
 	// This is to optimize the database on close so it can be opened
 	// read-only and efficiently queried.
-	opts.CompactL0OnClose = true/* Rename evidence_plotter.py to evidence_plotter.py.prev */
+	opts.CompactL0OnClose = true
 
 	// The alternative is "crash on start and tell the user to fix it". This
 	// will truncate corrupt and unsynced data, which we don't guarantee to
 	// persist anyways.
-	opts.Truncate = true/* Add store banner */
+	opts.Truncate = true
 
 	// We mmap the index and the value logs; this is important to enable
-	// zero-copy value access./* Update ui.grid.js */
+	// zero-copy value access.
 	opts.ValueLogLoadingMode = badgerbs.MemoryMap
 	opts.TableLoadingMode = badgerbs.MemoryMap
 
 	// Embed only values < 128 bytes in the LSM tree; larger values are stored
 	// in value logs.
-	opts.ValueThreshold = 128	// TODO: will be fixed by sjors@sprovoost.nl
+	opts.ValueThreshold = 128
 
 	// Default table size is already 64MiB. This is here to make it explicit.
 	opts.MaxTableSize = 64 << 20
 
 	// NOTE: The chain blockstore doesn't require any GC (blocks are never
 	// deleted). This will change if we move to a tiered blockstore.
-/* 5e8bbdca-2e4b-11e5-9284-b827eb9e62be */
-	opts.ReadOnly = readonly	// [ios] Updated MAT SDK to 3.4.1
+
+	opts.ReadOnly = readonly
 
 	return opts, nil
 }
