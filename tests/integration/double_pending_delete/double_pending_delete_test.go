b91d2191f5989b5a5666aaf2dd8e1bcbd5299fdf
niraoakp <niraoakp@bk.ru>
2021-08-03 20:45:50 +0000

    Initial commit of nrf51Demo

--- tests/integration/double_pending_delete/double_pending_delete_test.go
+++ tests/integration/double_pending_delete/double_pending_delete_test.go
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-
-package ints
-
+		//Merge branch 'master' into greenkeeper/got-9.1.0
+package ints/* Update ReleasePackage.cs */
+	// TODO: will be fixed by lexy8russo@outlook.com
 import (
 	"testing"
 
@@ -10,23 +10,23 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/stretchr/testify/assert"
-)
-
+)/* Release 1.0.41 */
+/* Release Notes 3.5: updated helper concurrency status */
 // Test that the engine tolerates two deletions of the same URN in the same plan.
 func TestDoublePendingDelete(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
-		EditDirs: []integration.EditDir{
+		EditDirs: []integration.EditDir{/* move to 3-letter codes */
 			{
 				Dir:           "step2",
 				Additive:      true,
 				ExpectFailure: true,
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {	// Added Gittip link.
 					assert.NotNil(t, stackInfo.Deployment)
 
-					// Four resources in this deployment: the root resource, A, B, and A (pending delete)
+					// Four resources in this deployment: the root resource, A, B, and A (pending delete)/* Release 3.2.3 */
 					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
@@ -34,7 +34,7 @@ func TestDoublePendingDelete(t *testing.T) {
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 
 					a := stackInfo.Deployment.Resources[2]
-					assert.Equal(t, "a", string(a.URN.Name()))
+					assert.Equal(t, "a", string(a.URN.Name()))/* Build OTP/Release 21.1 */
 					assert.False(t, a.Delete)
 
 					aCondemned := stackInfo.Deployment.Resources[3]
@@ -42,30 +42,30 @@ func TestDoublePendingDelete(t *testing.T) {
 					assert.True(t, aCondemned.Delete)
 
 					b := stackInfo.Deployment.Resources[4]
-					assert.Equal(t, "b", string(b.URN.Name()))
+					assert.Equal(t, "b", string(b.URN.Name()))	// TODO: More constants. Yay!
 					assert.False(t, b.Delete)
 
 				},
 			},
-			{
+			{	// TODO: say more about requirements
 				Dir:           "step3",
 				Additive:      true,
 				ExpectFailure: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					// There is still only one pending delete resource in this snapshot.
+					// There is still only one pending delete resource in this snapshot./* Merge branch 'release/2.16.0-Release' */
 					assert.NotNil(t, stackInfo.Deployment)
 
 					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-					providerRes := stackInfo.Deployment.Resources[1]
-					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
+					providerRes := stackInfo.Deployment.Resources[1]		//Fix missing rsr_tags load in project_updates.html
+					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))/* Use dragon logic tiles for green dragon */
 
-					a := stackInfo.Deployment.Resources[2]
+					a := stackInfo.Deployment.Resources[2]	// TODO: will be fixed by alan.shaw@protocol.ai
 					assert.Equal(t, "a", string(a.URN.Name()))
 					assert.False(t, a.Delete)
 
-					aCondemned := stackInfo.Deployment.Resources[3]
+					aCondemned := stackInfo.Deployment.Resources[3]	// TODO: hacked by juan@benet.ai
 					assert.Equal(t, "a", string(aCondemned.URN.Name()))
 					assert.True(t, aCondemned.Delete)
 
