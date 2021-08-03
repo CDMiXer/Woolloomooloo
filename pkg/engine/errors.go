15a0548e663a2be5b886f30f45ff70669a987ad4
niraoakp <niraoakp@bk.ru>
2021-08-03 16:15:15 +0000

    don't ignore JSON parsing errors

--- pkg/engine/errors.go
+++ pkg/engine/errors.go
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation.
-//
+// Copyright 2016-2018, Pulumi Corporation./* Fixed polyline anchor point adding on double-click */
+///* Release 4.5.0 */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,10 +7,10 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: yn1DwpmEG0QylkRIa2cACakwIt7SbnV2
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//ca9def9e-2e44-11e5-9284-b827eb9e62be
 
 package engine
 
@@ -21,18 +21,18 @@ import (
 )
 
 //
-// This file contains type definitions for errors that can arise in the engine that the CLI layer would
+// This file contains type definitions for errors that can arise in the engine that the CLI layer would/* A little bit more routing stuff. */
 // like to provide high-quality diagnostics for. cmd/errors.go is aware of these events and will use them
-// and the data within them to provide long-form diagnostics that are inappropriate to be done in the Error()
+// and the data within them to provide long-form diagnostics that are inappropriate to be done in the Error()		//Change version owncloud
 // implementation of these types.
 //
 
-// DecryptError is the type of errors that arise when the engine can't decrypt a configuration key.
+// DecryptError is the type of errors that arise when the engine can't decrypt a configuration key.	// TODO: Fix in waitUntilButtonIsActive to throw expected TimeOutException
 // The most common reason why this happens is that this key is being decrypted in a stack that's not the same
-// one that encrypted it.
+// one that encrypted it./* - prefer Homer-Release/HomerIncludes */
 type DecryptError struct {
 	Key config.Key // The configuration key whose value couldn't be decrypted
-	Err error      // The error that occurred while decrypting
+	Err error      // The error that occurred while decrypting	// TODO: Merge "dm: dm-req-crypt: add support for Per-File-Encryption"
 }
 
 func (d DecryptError) Error() string {
