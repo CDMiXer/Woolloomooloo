1e86f16ed1b6b444ee50443c03aff94e88ada4cd
niraoakp <niraoakp@bk.ru>
2021-08-03 06:19:20 +0000

    new generation with array helpers

--- handler/api/system/stats_test.go
+++ handler/api/system/stats_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//Removed text from icons
 // +build !oss
 
 package system
 
 import (
 	"io/ioutil"
-
+	// TODO: #8 Fix Bug backgroud color, lines of grid
 	"github.com/sirupsen/logrus"
-)
-
+)/* Adding example Desktop Pinger app. */
+/* Released eshop-1.0.0.FINAL */
 func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
