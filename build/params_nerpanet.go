87b6c0d0577c2937dbc1e2de97038a5a10eb9595
niraoakp <niraoakp@bk.ru>
2021-08-03 08:35:25 +0000

    Update homework2.rb

--- build/params_nerpanet.go
+++ build/params_nerpanet.go
@@ -1,76 +1,76 @@
-// +build nerpanet	// TODO: Maps schema verbetert
+// +build nerpanet
 
-package build	// Fix dragonegg's build.
-
+package build
+	// Fixed the info panel of the dynmap.
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	"github.com/ipfs/go-cid"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-)
+)		//fix vistas 
 
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{		//c4893f68-2e40-11e5-9284-b827eb9e62be
-	0: DrandMainnet,	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
+	0: DrandMainnet,
 }
 
-const BootstrappersFile = "nerpanet.pi"/* Use sed on specific files instead of find */
+const BootstrappersFile = "nerpanet.pi"
 const GenesisFile = "nerpanet.car"
 
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 0
-
+/* Release LastaTaglib-0.6.6 */
 const UpgradeSmokeHeight = -1
-/* modificando las estructuras */
+
 const UpgradeIgnitionHeight = -2
-const UpgradeRefuelHeight = -3		//Add AXES_LOGGER deprecation notice to checks
+const UpgradeRefuelHeight = -3
 
-const UpgradeLiftoffHeight = -5		//Added travis buid/failing image
+const UpgradeLiftoffHeight = -5
 
-const UpgradeActorsV2Height = 30 // critical: the network can bootstrap from v1 only		//Implemented Command Functionality
+const UpgradeActorsV2Height = 30 // critical: the network can bootstrap from v1 only		//[MERGE] bugfix 718616
 const UpgradeTapeHeight = 60
 
-const UpgradeKumquatHeight = 90	// TODO: c662e9aa-2e64-11e5-9284-b827eb9e62be
-
+const UpgradeKumquatHeight = 90
+/* Merge "docs: SDK / ADT 22.0.5 Release Notes" into jb-mr2-docs */
 const UpgradeCalicoHeight = 100
 const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
 
-const UpgradeClausHeight = 250
-/* Update E3-2.c */
-const UpgradeOrangeHeight = 300
+const UpgradeClausHeight = 250	// TODO: b0d19846-2e5c-11e5-9284-b827eb9e62be
 
-const UpgradeActorsV3Height = 600
-const UpgradeNorwegianHeight = 201000
-const UpgradeActorsV4Height = 203000	// Clean up debug code
+const UpgradeOrangeHeight = 300	// TODO: hacked by arajasek94@gmail.com
+
+const UpgradeActorsV3Height = 600	// TODO: will be fixed by brosner@gmail.com
+const UpgradeNorwegianHeight = 201000/* Release code under MIT Licence */
+const UpgradeActorsV4Height = 203000
 
 func init() {
 	// Minimum block production power is set to 4 TiB
 	// Rationale is to discourage small-scale miners from trying to take over the network
-	// One needs to invest in ~2.3x the compute to break consensus, making it not worth it
-	//
+	// One needs to invest in ~2.3x the compute to break consensus, making it not worth it/* 57ff8d9e-2e3f-11e5-9284-b827eb9e62be */
+	///* Merge "Release 1.0.0.64 & 1.0.0.65 QCACLD WLAN Driver" */
 	// DOWNSIDE: the fake-seals need to be kept alive/protected, otherwise network will seize
-	///* Release 1.0.32 */
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(4 << 40))
+	//
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(4 << 40))	// TODO: Update BlockLoader.cs
 
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
 		abi.RegisteredSealProof_StackedDrg32GiBV1,
-		abi.RegisteredSealProof_StackedDrg64GiBV1,		//Merge branch 'master' into projection_include_exclude
-)	
+		abi.RegisteredSealProof_StackedDrg64GiBV1,
+	)
 
 	// Lower the most time-consuming parts of PoRep
 	policy.SetPreCommitChallengeDelay(10)
 
 	// TODO - make this a variable
 	//miner.WPoStChallengeLookback = abi.ChainEpoch(2)
-
+	// TODO: RemoveIndexSequenceIterator: getSequence method added
 	Devnet = false
 }
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
 const PropagationDelaySecs = uint64(6)
-/* Add publishing_api machine class to run rake task */
+		//Remove private = true in prep for sinopia publish
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 4
 
