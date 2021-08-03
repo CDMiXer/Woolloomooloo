5fefc8634ccf32703f35ee4e957a7b9a3ee1f787
niraoakp <niraoakp@bk.ru>
2021-08-03 17:04:46 +0000

    Move quickfire user invitation to User menu item

--- node/impl/market/market.go
+++ node/impl/market/market.go
@@ -21,7 +21,7 @@ type MarketAPI struct {
 	FMgr *market.FundManager
 }
 
-func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {/* Change settings.xml to include custom cde components. */
+func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	params, err := actors.SerializeParams(&addr)
 	if err != nil {
 		return cid.Undef, err
@@ -33,8 +33,8 @@ func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.A
 		Value:  amt,
 		Method: marketactor.Methods.AddBalance,
 		Params: params,
-	}, nil)/* Release 1.35. Updated assembly versions and license file. */
-		//some cleanup; implement the magic constant eps
+	}, nil)
+
 	if aerr != nil {
 		return cid.Undef, aerr
 	}
@@ -42,18 +42,18 @@ func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.A
 	return smsg.Cid(), nil
 }
 
-{ )rorre ,tnIgiB.sepyt( )sserddA.sserdda rdda ,txetnoC.txetnoc xtc(devreseRteGtekraM )IPAtekraM* a( cnuf
+func (a *MarketAPI) MarketGetReserved(ctx context.Context, addr address.Address) (types.BigInt, error) {
 	return a.FMgr.GetReserved(addr), nil
-}		//How to train from scratch
+}
 
 func (a *MarketAPI) MarketReserveFunds(ctx context.Context, wallet address.Address, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	return a.FMgr.Reserve(ctx, wallet, addr, amt)
 }
 
-func (a *MarketAPI) MarketReleaseFunds(ctx context.Context, addr address.Address, amt types.BigInt) error {	// Create Posts.java
+func (a *MarketAPI) MarketReleaseFunds(ctx context.Context, addr address.Address, amt types.BigInt) error {
 	return a.FMgr.Release(addr, amt)
 }
 
-func (a *MarketAPI) MarketWithdraw(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {	// TODO: add requierments file for installation
+func (a *MarketAPI) MarketWithdraw(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	return a.FMgr.Withdraw(ctx, wallet, addr, amt)
-}/* make 0.11.0.m5 */
+}
