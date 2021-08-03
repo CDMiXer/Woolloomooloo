6e499710c7e125054e2aa5bef68aaea07bb66fec
niraoakp <niraoakp@bk.ru>
2021-08-03 23:11:15 +0000

    help menu 4 canvas ops & some package reordering

--- handler/api/repos/sign/sign_test.go
+++ handler/api/repos/sign/sign_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-esneciL laicremmoC-noN enorD eht yb denrevog si edoc ecruos siht fo esU //
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Обновление translations/texts/objects/floran/chestplant1/chestplant1.object.json */
-package sign
+/* lower logging priority for LGP setup messages */
+package sign/* Release and getting commands */
