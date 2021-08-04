f5a3f1e8334370686240ea49e4ad3737e19b5f32
niraoakp <niraoakp@bk.ru>
2021-08-04 12:45:19 +0000

    chore(package): update vscode to version 1.1.11

--- handler/api/repos/builds/deploys/create.go
+++ handler/api/repos/builds/deploys/create.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Create flowquest.pm */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//	// TODO: hacked by xiemengjun@gmail.com
+// You may obtain a copy of the License at/* Decrease sleep time 1000ms to 500ms */
+//		//CLOUDSTACK-2470: listVolume throws db exception.
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// 1aad1b23-2e9d-11e5-a839-a45e60cdfd11
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Create jsoncad_example_4.html
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//650b11f2-2e62-11e5-9284-b827eb9e62be
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package deploys
