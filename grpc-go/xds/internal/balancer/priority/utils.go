ef7695e00eaca1d64e27fa2c1c6fd7b7a279ad14
niraoakp <niraoakp@bk.ru>
2021-08-04 02:50:50 +0000

    added epi_distcorr to init files

--- grpc-go/xds/internal/balancer/priority/utils.go
+++ grpc-go/xds/internal/balancer/priority/utils.go
@@ -5,27 +5,27 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//fixes #1810
- *     http://www.apache.org/licenses/LICENSE-2.0	// running jetty from ant, jsps not supported yet
+ *		//upgrade koheron_tcp_client to 1.0.6
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// Put infrastructure in place for future optimisation.
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* add a relay image */
  */
 
 package priority
 
 func equalStringSlice(a, b []string) bool {
 	if len(a) != len(b) {
-		return false
-	}/* Continue CMake renaming */
+		return false/* Directory 'srcFraglich' removed */
+	}		//minor adjustments to configuration so the load order can be arbitrary
 	for i := range a {
 		if a[i] != b[i] {
 			return false
-		}
-	}	// Fixed paths in README.md
+		}	// Reworked flat-6 engine sound profile 1
+	}
 	return true
-}	// fixed bug that wouldn't allow running
+}
