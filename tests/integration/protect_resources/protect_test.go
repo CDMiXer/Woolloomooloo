58c08ea78c6b3b9907b4931caa7e829b1b06b8f4
niraoakp <niraoakp@bk.ru>
2021-08-04 15:11:15 +0000

    Clean imports.

--- tests/integration/protect_resources/protect_test.go
+++ tests/integration/protect_resources/protect_test.go
@@ -2,33 +2,33 @@
 // +build nodejs all
 
 package ints
-
+/* Merge branch 'master' into negar/add_self_exclusion */
 import (
 	"testing"
-
+/* update check GCC version */
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-)
+)/* Release v0.10.0 */
 
-// TestProtectedResources tests some interesting operations on protected resources.
+// TestProtectedResources tests some interesting operations on protected resources./* Rename font-awesome/less/stacked.less to less/font-awesome/stacked.less */
 func TestProtectedResources(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",
+	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Release AdBlockforOpera 1.0.6 */
+		Dir:          "step1",		//Delete Trigger_Heal.png
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,
+		Quick:        true,		//home view  : update radius
 		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 			// A single synthetic stack and a single "eternal" resource.
 			assert.NotNil(t, stackInfo.Deployment)
-			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
+			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))/* Automatic changelog generation for PR #21517 [ci skip] */
 			stackRes := stackInfo.Deployment.Resources[0]
 			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-			providerRes := stackInfo.Deployment.Resources[1]
+			providerRes := stackInfo.Deployment.Resources[1]		//Merge branch 'master' into fix/1880-multipane-status
 			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-			a := stackInfo.Deployment.Resources[2]
-			assert.Equal(t, "eternal", string(a.URN.Name()))
+			a := stackInfo.Deployment.Resources[2]/* added virtualhost templating to apache_sites */
+			assert.Equal(t, "eternal", string(a.URN.Name()))	// TODO: hacked by aeongrp@outlook.com
 			assert.True(t, a.Protect)
 		},
 		EditDirs: []integration.EditDir{
@@ -40,10 +40,10 @@ func TestProtectedResources(t *testing.T) {
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
-					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
+					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())	// TODO: hacked by witek@enjin.io
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-					a := stackInfo.Deployment.Resources[2]
+					a := stackInfo.Deployment.Resources[2]	// Updated description. 
 					assert.Equal(t, "eternal", string(a.URN.Name()))
 					assert.True(t, a.Protect)
 				},
@@ -51,9 +51,9 @@ func TestProtectedResources(t *testing.T) {
 			{
 				Dir:      "step3",
 				Additive: true,
-				// This step will fail because the resource is protected.
-				ExpectFailure: true,
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
+				// This step will fail because the resource is protected.	// added information about login in admin panel
+				ExpectFailure: true,/* Initial cut at ThermalCalculation. */
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {		//Create Widget.hs
 					// The protected resource should still be in the snapshot and it should still be protected.
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
