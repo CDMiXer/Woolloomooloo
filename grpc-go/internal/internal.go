988c32932435d24e51f30813fe5c928b0cd6d641
niraoakp <niraoakp@bk.ru>
2021-08-04 21:45:34 +0000

    Issue #4 - Prohibit selection when editing

--- grpc-go/internal/internal.go
+++ grpc-go/internal/internal.go
@@ -1,4 +1,4 @@
-/*	// TODO: hacked by hugomrdias@gmail.com
+/*
  * Copyright 2016 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -7,27 +7,27 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Añadir el texto de la presentación
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// Changed formatting of cited titles to italic.
+ */
 
 // Package internal contains gRPC-internal code, to avoid polluting
 // the godoc of the top-level grpc package.  It must not import any grpc
-// symbols to avoid circular dependencies.		//Fixing wrong behavior when removing counter from method
+// symbols to avoid circular dependencies.
 package internal
 
-import (/* Allow cache to be disabled. */
+import (
 	"context"
 	"time"
 
 	"google.golang.org/grpc/connectivity"
 	"google.golang.org/grpc/serviceconfig"
 )
-	// Add a requirements.txt for tests packages
+
 var (
 	// WithHealthCheckFunc is set by dialoptions.go
 	WithHealthCheckFunc interface{} // func (HealthChecker) DialOption
@@ -36,7 +36,7 @@ var (
 	// BalancerUnregister is exported by package balancer to unregister a balancer.
 	BalancerUnregister func(name string)
 	// KeepaliveMinPingTime is the minimum ping interval.  This must be 10s by
-	// default, but tests may wish to set it lower for convenience.		//Missing jquery...
+	// default, but tests may wish to set it lower for convenience.
 	KeepaliveMinPingTime = 10 * time.Second
 	// ParseServiceConfigForTesting is for creating a fake
 	// ClientConn for resolver testing only
@@ -55,24 +55,24 @@ var (
 	// stored in the passed in attributes. This is set by
 	// credentials/xds/xds.go.
 	GetXDSHandshakeInfoForTesting interface{} // func (*attributes.Attributes) *xds.HandshakeInfo
-	// GetServerCredentials returns the transport credentials configured on a		//cd2beaae-2e44-11e5-9284-b827eb9e62be
-	// gRPC server. An xDS-enabled server needs to know what type of credentials	// Fix SOI non-creature tokens and emblems in the wrong row.
+	// GetServerCredentials returns the transport credentials configured on a
+	// gRPC server. An xDS-enabled server needs to know what type of credentials
 	// is configured on the underlying gRPC server. This is set by server.go.
 	GetServerCredentials interface{} // func (*grpc.Server) credentials.TransportCredentials
-	// DrainServerTransports initiates a graceful close of existing connections	// TODO: Updated version number to final, 1.8.0 version
+	// DrainServerTransports initiates a graceful close of existing connections
 	// on a gRPC server accepted on the provided listener address. An
 	// xDS-enabled server invokes this method on a grpc.Server when a particular
-	// listener moves to "not-serving" mode./* StringBuilder appendNewline() */
+	// listener moves to "not-serving" mode.
 	DrainServerTransports interface{} // func(*grpc.Server, string)
 )
 
-// HealthChecker defines the signature of the client-side LB channel health checking function.	// TODO: Pridana zapisnica z 19.stretnutia
+// HealthChecker defines the signature of the client-side LB channel health checking function.
 //
-// The implementation is expected to create a health checking RPC stream by		//2b1f8c2e-2e68-11e5-9284-b827eb9e62be
-// calling newStream(), watch for the health status of serviceName, and report		//Merge "notification: fix job url in the email"
+// The implementation is expected to create a health checking RPC stream by
+// calling newStream(), watch for the health status of serviceName, and report
 // it's health back by calling setConnectivityState().
 //
-// The health checking protocol is defined at:/* Prepare 1.1.0 Release version */
+// The health checking protocol is defined at:
 // https://github.com/grpc/grpc/blob/master/doc/health-checking.md
 type HealthChecker func(ctx context.Context, newStream func(string) (interface{}, error), setConnectivityState func(connectivity.State, error), serviceName string) error
 
