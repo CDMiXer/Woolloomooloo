0277ce330badfb7f42ad11bf1d8fe68e35d0419e
niraoakp <niraoakp@bk.ru>
2021-08-04 01:45:20 +0000

    Add ASCIINEMA screencast instruction

--- tests/integration/dependency_steps/step1/index.ts
+++ tests/integration/dependency_steps/step1/index.ts
@@ -3,6 +3,6 @@
 import { Resource } from "./resource";
 
 // Step 1: Populate our dependency graph.
-const a = new Resource("a", { state: 1 });
+const a = new Resource("a", { state: 1 });		//Delete bookmarkparser.py
 const b = new Resource("b", { state: 2, resource: a });
 // Dependency graph: b -> a
