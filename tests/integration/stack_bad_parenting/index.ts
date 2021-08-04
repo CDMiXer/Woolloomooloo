98492796195dd509c5255dae840ff700cdeed288
niraoakp <niraoakp@bk.ru>
2021-08-04 16:44:34 +0000

    translatable static pages

--- tests/integration/stack_bad_parenting/index.ts
+++ tests/integration/stack_bad_parenting/index.ts
@@ -1,29 +1,29 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-	// TODO: will be fixed by seth@sethvargo.com
+
 let currentID = 0;
 
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
-		//FIX row details did not work due to JS error
+
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
-	// ebe1ada4-2e3e-11e5-9284-b827eb9e62be
+
     constructor() {
         this.create = async (inputs: any) => {
-            return {/* finishing up ReleasePlugin tasks, and working on rest of the bzr tasks. */
+            return {
                 id: (currentID++).toString(),
-                outs: undefined,/* Bugfix for Release. */
+                outs: undefined,
             };
         };
     }
 }
 
-class Resource extends pulumi.dynamic.Resource {		//fix missing use if IDCreator
+class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, parent?: pulumi.Resource) {
         super(Provider.instance, name, {}, { parent: parent });
     }
 }
-		//removed obsolete CV code
+
 // Ensure we throw if pass an non-resource as a parent.
 let a = new Resource("a", <any>this);
