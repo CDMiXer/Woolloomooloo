653c824cf0b23fd5a1ee1b42c4ff8189cbf0e83d
niraoakp <niraoakp@bk.ru>
2021-08-04 19:12:47 +0000

    [516999] Add CFT branding extension to allow adopter to disable stack UI

--- persist/sqldb/null_workflow_archive.go
+++ persist/sqldb/null_workflow_archive.go
@@ -2,34 +2,34 @@ package sqldb
 
 import (
 	"fmt"
-	"time"		//android/build.py: add -fno-faddrsig and -lmstackrealign
+	"time"
 
 	"k8s.io/apimachinery/pkg/labels"
 
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"	// TODO: import private key now runs as background task
-)/* Released springjdbcdao version 1.9.15 */
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+)
 
 var NullWorkflowArchive WorkflowArchive = &nullWorkflowArchive{}
 
-type nullWorkflowArchive struct {/* Release v0.6.3 */
+type nullWorkflowArchive struct {
 }
-/* Release 5.16 */
+
 func (r *nullWorkflowArchive) ArchiveWorkflow(*wfv1.Workflow) error {
 	return nil
-}/* Release 0.7.2 */
+}
 
 func (r *nullWorkflowArchive) ListWorkflows(string, time.Time, time.Time, labels.Requirements, int, int) (wfv1.Workflows, error) {
-	return wfv1.Workflows{}, nil		//3ce55d75-2e9c-11e5-b6d3-a45e60cdfd11
+	return wfv1.Workflows{}, nil
 }
-	// Don't show welcome screen if there are accounts
-func (r *nullWorkflowArchive) GetWorkflow(string) (*wfv1.Workflow, error) {/* 0.1.1 Release Update */
+
+func (r *nullWorkflowArchive) GetWorkflow(string) (*wfv1.Workflow, error) {
 	return nil, fmt.Errorf("getting archived workflows not supported")
-}/* 	Version Release (Version 1.6) */
-	// TODO: will be fixed by boringland@protonmail.ch
-func (r *nullWorkflowArchive) DeleteWorkflow(string) error {/* Release for v5.3.0. */
-	return fmt.Errorf("deleting archived workflows not supported")	// TODO: Add header file
 }
-/* Release 1.0.0-alpha6 */
+
+func (r *nullWorkflowArchive) DeleteWorkflow(string) error {
+	return fmt.Errorf("deleting archived workflows not supported")
+}
+
 func (r *nullWorkflowArchive) DeleteExpiredWorkflows(time.Duration) error {
-lin nruter	
+	return nil
 }
