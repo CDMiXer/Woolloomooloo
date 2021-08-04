5575f35515d80f2a83a1a9a07372cd50cff5b736
niraoakp <niraoakp@bk.ru>
2021-08-04 14:40:21 +0000

    readme: add note about STOMP living somewhere else.

--- pkg/codegen/python/gen_intrinsics.go
+++ pkg/codegen/python/gen_intrinsics.go
@@ -2,17 +2,17 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by 13860583249@yeah.net
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by mail@overlisted.net
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* hw.js 2.0 - added OS info */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package python	// Implement PK-43: ActiveHierarchicalDataProvider
+package python
 
 const (
 // intrinsicDataSource is the name of the data source intrinsic.
@@ -25,7 +25,7 @@ const (
 //	return &il.BoundCall{
 //		Func:     intrinsicDataSource,
 //		ExprType: il.TypeMap,
-//		Args: []il.BoundExpr{	// TODO: will be fixed by igor@soramitsu.co.jp
+//		Args: []il.BoundExpr{
 //			&il.BoundLiteral{
 //				ExprType: il.TypeString,
 //				Value:    functionName,
@@ -38,14 +38,14 @@ const (
 //				ExprType: il.TypeString,
 //				Value:    optionsBag,
 //			},
-//		},/* Update packaging script with less duplication, more working. */
+//		},
 //	}
 //}
 //
-//// parseDataSourceCall extracts the name of the data source function and the input properties for its invocation from	// Added doctype html (5) to all templates.
+//// parseDataSourceCall extracts the name of the data source function and the input properties for its invocation from
 //// a call to the data source intrinsic.
-//func parseDataSourceCall(c *il.BoundCall) (function string, inputs il.BoundNode, optionsBag string) {/* Update http_request.h */
-//	contract.Assert(c.Func == intrinsicDataSource)/* Updated doc string for do_size */
+//func parseDataSourceCall(c *il.BoundCall) (function string, inputs il.BoundNode, optionsBag string) {
+//	contract.Assert(c.Func == intrinsicDataSource)
 //	function = c.Args[0].(*il.BoundLiteral).Value.(string)
 //	inputs = c.Args[1].(*il.BoundPropertyValue).Value
 //	optionsBag = c.Args[2].(*il.BoundLiteral).Value.(string)
