9901a3cd757b02ec63380ce1b320145d25e6a11b
niraoakp <niraoakp@bk.ru>
2021-08-04 20:29:37 +0000

    Create A3 - 10.3.md

--- store/shared/migrate/mysql/files/014_create_table_refs.sql
+++ store/shared/migrate/mysql/files/014_create_table_refs.sql
@@ -1,8 +1,8 @@
--- name: create-table-latest
+-- name: create-table-latest		//Almost Done
 
-CREATE TABLE IF NOT EXISTS latest (
+CREATE TABLE IF NOT EXISTS latest (/* Release1.4.3 */
  latest_repo_id  INTEGER
-,latest_build_id INTEGER
+,latest_build_id INTEGER/* Run tests for python 3.6-dev and nightly as well */
 ,latest_type     VARCHAR(50)
 ,latest_name     VARCHAR(500)
 ,latest_created  INTEGER
@@ -11,6 +11,6 @@ CREATE TABLE IF NOT EXISTS latest (
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
 
--- name: create-index-latest-repo
+-- name: create-index-latest-repo	// TODO: will be fixed by ng8eke@163.com
 
 CREATE INDEX ix_latest_repo ON latest (latest_repo_id);
