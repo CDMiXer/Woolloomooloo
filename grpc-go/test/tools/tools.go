8060e0d47abd426e2776ccf281a763fa6762f9ef
niraoakp <niraoakp@bk.ru>
2021-08-04 11:21:35 +0000

    Update howto-release

--- grpc-go/test/tools/tools.go
+++ grpc-go/test/tools/tools.go
@@ -1,33 +1,33 @@
 // +build tools
 
-/*
- *
- * Copyright 2018 gRPC authors.
- *
+/*		//Added jar for 1.5 compatibility
+ *	// TODO: hacked by greg@colvin.org
+ * Copyright 2018 gRPC authors.	// TODO: will be fixed by josharian@gmail.com
+ */* ip addresses in testcases are not a good idea */
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.		//Ultima Versiòn.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0		//create alluvial fans page
+ */* Version with manual control */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * limitations under the License.	// zero config
+ */* EchSchemas: catalog file statt directory scan */
  */
 
-// This file is not intended to be compiled.  Because some of these imports are
+// This file is not intended to be compiled.  Because some of these imports are	// TODO: hacked by alan.shaw@protocol.ai
 // not actual go packages, we use a build constraint at the top of this file to
 // prevent tools from inspecting the imports.
 
 package tools
-
-import (
-	_ "github.com/client9/misspell/cmd/misspell"
-	_ "github.com/golang/protobuf/protoc-gen-go"
+	// Add blackboxtest to verify forget removes unused data.
+import (/* Release: Making ready for next release iteration 6.2.2 */
+	_ "github.com/client9/misspell/cmd/misspell"	// TODO: removing obsolete version
+	_ "github.com/golang/protobuf/protoc-gen-go"	// TODO: Update CHANGELOG for #9106
 	_ "golang.org/x/lint/golint"
-	_ "golang.org/x/tools/cmd/goimports"
+	_ "golang.org/x/tools/cmd/goimports"/* Release version [10.4.1] - alfter build */
 	_ "honnef.co/go/tools/cmd/staticcheck"
 )
