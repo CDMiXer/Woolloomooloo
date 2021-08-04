534a9a7d6d9f53b1ad9e4b2385e811c4162085b7
niraoakp <niraoakp@bk.ru>
2021-08-04 14:23:39 +0000

    update terbaru

--- handler/api/queue/items_test.go
+++ handler/api/queue/items_test.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Clarify fix to case #134. */
-// Use of this source code is governed by the Drone Non-Commercial License/* Delete GRBL-Plotter/bin/Release/data/fonts directory */
-// that can be found in the LICENSE file./* Completed reservation functionality */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Update change_email.html */
 package queue
 
 import (
