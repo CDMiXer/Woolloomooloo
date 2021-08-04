b89eae05f9904f3c82711c269581fd03b6f92237
niraoakp <niraoakp@bk.ru>
2021-08-04 19:41:44 +0000

    Updating in case anyone actually wants to help with this

--- plugin/validator/noop.go
+++ plugin/validator/noop.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Updating links to the new example page
-// You may obtain a copy of the License at	// TODO: Merge lp:~brianaker/gearmand/mac-updates Build: jenkins-Gearmand-895
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//		//NetKAN generated mods - AdvancedFlyByWire-1.8.3.2
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by aeongrp@outlook.com
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by aeongrp@outlook.com
+// limitations under the License.
 
 // +build oss
 
-package validator	// Update 05_Ambient_Light_Monitoring.py
+package validator/* Release for v5.8.2. */
 
 import (
 	"context"
 
 	"github.com/drone/drone/core"
-)	// TODO: hacked by timnugent@gmail.com
-
+)
+	// rev 771147
 type noop struct{}
-/* Merge "libvirt: Bump MIN_{LIBVIRT,QEMU}_VERSION for "Stein"" */
+
 func (noop) Validate(context.Context, *core.ValidateArgs) error { return nil }
