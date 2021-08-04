1ac8e98702e057ba5aa288078c0067108e89f1dd
niraoakp <niraoakp@bk.ru>
2021-08-04 12:27:02 +0000

    Revamped the logging subsystem

--- store/shared/migrate/postgres/files/007_create_table_logs.sql
+++ store/shared/migrate/postgres/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
--- name: create-table-logs
+-- name: create-table-logs	// TODO: will be fixed by cory@protocol.ai
 
-CREATE TABLE IF NOT EXISTS logs (
+CREATE TABLE IF NOT EXISTS logs (	// Removed unstable tools
  log_id    SERIAL PRIMARY KEY
-,log_data  BYTEA
+,log_data  BYTEA/* no use of minfs */
 );
