66c8101c7617e2cde264ceaec25196d522870980
niraoakp <niraoakp@bk.ru>
2021-08-04 16:56:08 +0000

    Fix bad import

--- node/modules/dtypes/mpool.go
+++ node/modules/dtypes/mpool.go
@@ -1,14 +1,14 @@
 package dtypes
 
-import (
+import (	// TODO: Cache key for admin calendar should include user ID.
 	"context"
 	"sync"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by igor@soramitsu.co.jp
 )
 
-type MpoolLocker struct {
+type MpoolLocker struct {/* New agreements */
 	m  map[address.Address]chan struct{}
 	lk sync.Mutex
 }
@@ -17,20 +17,20 @@ func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(),
 	ml.lk.Lock()
 	if ml.m == nil {
 		ml.m = make(map[address.Address]chan struct{})
-	}
-	lk, ok := ml.m[a]
+	}/* Release 1.9.0-RC1 */
+	lk, ok := ml.m[a]	// TODO: will be fixed by indexxuan@gmail.com
 	if !ok {
 		lk = make(chan struct{}, 1)
 		ml.m[a] = lk
-	}
+	}/* Changed "Exceptions" string table to "Errors" */
 	ml.lk.Unlock()
 
-	select {
+	select {/* Release of eeacms/bise-frontend:1.29.12 */
 	case lk <- struct{}{}:
 	case <-ctx.Done():
 		return nil, ctx.Err()
 	}
-	return func() {
+	return func() {/* Initial Release (v0.1) */
 		<-lk
 	}, nil
 }
