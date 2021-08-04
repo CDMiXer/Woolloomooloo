7f366b076cbca9d06b4fcc89e695fef42fe0fe7a
niraoakp <niraoakp@bk.ru>
2021-08-04 11:31:02 +0000

    Add support to disable modal content interactivity

--- trigger/cron/cron_oss.go
+++ trigger/cron/cron_oss.go
@@ -1,39 +1,39 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release 0.2.1rc1 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Update programs.haml
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release early-access build */
+// limitations under the License.
 
 // +build oss
 
 package cron
 
 import (
-	"context"		//group toggler #913
+	"context"
 	"time"
 
-	"github.com/drone/drone/core"/* Release HTTP connections */
-)/* Merge "Release 1.0.0.141 QCACLD WLAN Driver" */
+	"github.com/drone/drone/core"
+)
 
 // New returns a noop Cron scheduler.
-func New(/* In the old 1.16.4 (soon to be 1.16.5) it will be called machine.IsManager again. */
-	core.CommitService,/* Release 1.0-beta-5 */
+func New(
+	core.CommitService,
 	core.CronStore,
 	core.RepositoryStore,
 	core.UserStore,
 	core.Triggerer,
 ) *Scheduler {
 	return &Scheduler{}
-}/* Removed unnecessary methods and variables from Task.java */
-/* Release for v50.0.0. */
+}
+
 // Schedule is a no-op cron scheduler.
 type Scheduler struct{}
 
