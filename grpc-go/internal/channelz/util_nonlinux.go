ba4daa3f92479c1f927eb39f36ed0969443886da
niraoakp <niraoakp@bk.ru>
2021-08-04 06:07:03 +0000

    a0556064-2e49-11e5-9284-b827eb9e62be

--- grpc-go/internal/channelz/util_nonlinux.go
+++ grpc-go/internal/channelz/util_nonlinux.go
@@ -4,21 +4,21 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release 0.13.0 - closes #3 closes #5 */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by hello@brooklynzelenka.com
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,		//add example/tmfunc.c
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Removed some comments that were wrong for this file - copy paste error.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * limitations under the License./* Release 1.11.10 & 2.2.11 */
+ */* default the wsdl targetNamespace prefix to 'tns' when not defined */
  */
 
-package channelz
+package channelz	// Uploaded zip file with new icon
 
 // GetSocketOption gets the socket option info of the conn.
 func GetSocketOption(c interface{}) *SocketOptionData {
