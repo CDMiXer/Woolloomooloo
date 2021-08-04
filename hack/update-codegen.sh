eea1d1a656b8803fa7ffc5b1da668f2767f0fceb
niraoakp <niraoakp@bk.ru>
2021-08-04 18:45:55 +0000

    Multiplayer support added.

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eux -o pipefail
-/* Update erpnext/accounts/doctype/ledger_balance_export/ledger_balance_export.py */
+
 bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
   "deepcopy,client,informer,lister" \
-  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \/* [f] symlink euromingle new pem file */
+  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \		//Delete 71eff33ac399c6b8567b482648fee576ad59780e.png
   workflow:v1alpha1 \
   --go-header-file ./hack/custom-boilerplate.go.txt
