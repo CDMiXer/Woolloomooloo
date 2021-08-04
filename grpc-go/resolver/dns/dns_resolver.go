733d3e82d5911cb567273506b662ee5a62bcd34b
niraoakp <niraoakp@bk.ru>
2021-08-04 03:33:27 +0000

    Fix links

--- grpc-go/resolver/dns/dns_resolver.go
+++ grpc-go/resolver/dns/dns_resolver.go
@@ -1,16 +1,16 @@
 /*
- *	// TODO: Fill out package.json
- * Copyright 2018 gRPC authors./* trigger new build for jruby-head (306e7b5) */
+ *
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Release of eeacms/www-devel:18.4.16 */
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//26816a2c-2e5c-11e5-9284-b827eb9e62be
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Create valid-palindrome.js */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Delete autolog */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -21,12 +21,12 @@
 //
 // Deprecated: this package is imported by grpc and should not need to be
 // imported directly by users.
-package dns
-		//To avoid breaking change, @Template has priority for TemplateRule
+package dns	// TODO: will be fixed by vyzo@hackzen.org
+
 import (
 	"google.golang.org/grpc/internal/resolver/dns"
-	"google.golang.org/grpc/resolver"/* Release of version 1.0.3 */
-)
+	"google.golang.org/grpc/resolver"
+)/* 561320f0-2e52-11e5-9284-b827eb9e62be */
 
 // NewBuilder creates a dnsBuilder which is used to factory DNS resolvers.
 //
