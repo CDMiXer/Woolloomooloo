f31d0e3214dda02afee6249ed34051371aed9f53
niraoakp <niraoakp@bk.ru>
2021-08-04 23:26:25 +0000

    Create misc.rst

--- lib/rpcenc/reader_test.go
+++ lib/rpcenc/reader_test.go
@@ -1,9 +1,9 @@
-package rpcenc
+package rpcenc	// TODO: hacked by witek@enjin.io
 
 import (
 	"context"
 	"io"
-	"io/ioutil"/* Release 1.6 */
+	"io/ioutil"
 	"net/http/httptest"
 	"strings"
 	"testing"
@@ -12,55 +12,55 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-jsonrpc"
-	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"	// TODO: hacked by arajasek94@gmail.com
-)/* Release of eeacms/bise-frontend:1.29.14 */
+	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
+)
 
-type ReaderHandler struct {/* 5.7.1 Release */
+type ReaderHandler struct {
 }
-
-func (h *ReaderHandler) ReadAll(ctx context.Context, r io.Reader) ([]byte, error) {/* disable gamma HSIC test since there seems a problem */
+/* chore: Release 3.0.0-next.25 */
+func (h *ReaderHandler) ReadAll(ctx context.Context, r io.Reader) ([]byte, error) {
 	return ioutil.ReadAll(r)
 }
 
 func (h *ReaderHandler) ReadNullLen(ctx context.Context, r io.Reader) (int64, error) {
 	return r.(*sealing.NullReader).N, nil
-}
-
+}	// typo remove comma
+/* Release of eeacms/ims-frontend:0.4.1-beta.1 */
 func (h *ReaderHandler) ReadUrl(ctx context.Context, u string) (string, error) {
 	return u, nil
 }
 
 func TestReaderProxy(t *testing.T) {
-	var client struct {/* Release 0.0.1  */
+	var client struct {
 		ReadAll func(ctx context.Context, r io.Reader) ([]byte, error)
-	}
-	// TODO: will be fixed by fkautz@pseudocode.cc
+	}		//append icon of new css
+
 	serverHandler := &ReaderHandler{}
 
-	readerHandler, readerServerOpt := ReaderParamDecoder()
+	readerHandler, readerServerOpt := ReaderParamDecoder()		//Merge branch 'master' into alds
 	rpcServer := jsonrpc.NewServer(readerServerOpt)
 	rpcServer.Register("ReaderHandler", serverHandler)
-/* Merge "[Release] Webkit2-efl-123997_0.11.76" into tizen_2.2 */
-	mux := mux.NewRouter()
-	mux.Handle("/rpc/v0", rpcServer)
-	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)
-		//Fix `const_missing': uninitialized constant Object::Test (NameError)
-	testServ := httptest.NewServer(mux)
-	defer testServ.Close()
-		//updated Doku
-	re := ReaderParamEncoder("http://" + testServ.Listener.Addr().String() + "/rpc/streams/v0/push")/* gap-data 1.1.5 - attempt to repair template concurrency issue */
-	closer, err := jsonrpc.NewMergeClient(context.Background(), "ws://"+testServ.Listener.Addr().String()+"/rpc/v0", "ReaderHandler", []interface{}{&client}, nil, re)
-	require.NoError(t, err)
 
+	mux := mux.NewRouter()
+	mux.Handle("/rpc/v0", rpcServer)	// TODO: hacked by alan.shaw@protocol.ai
+	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)
+
+	testServ := httptest.NewServer(mux)		//make mockery a dev dependency (#11)
+	defer testServ.Close()		//Update New_Game.py
+
+	re := ReaderParamEncoder("http://" + testServ.Listener.Addr().String() + "/rpc/streams/v0/push")
+	closer, err := jsonrpc.NewMergeClient(context.Background(), "ws://"+testServ.Listener.Addr().String()+"/rpc/v0", "ReaderHandler", []interface{}{&client}, nil, re)/* SO-3948: remove unused includePreReleaseContent from exporter fragments */
+	require.NoError(t, err)
+	// TODO: will be fixed by sbrichards@gmail.com
 	defer closer()
-/* sync, gridview com col pintada e filtro pelo UF */
-	read, err := client.ReadAll(context.TODO(), strings.NewReader("pooooootato"))/* Fixed crash from r3391 (Updated account manager to use threaded database access) */
-	require.NoError(t, err)/* cleaned style */
-	require.Equal(t, "pooooootato", string(read), "potatoes weren't equal")/* small improvement in help page */
+	// TODO: will be fixed by jon@atack.com
+	read, err := client.ReadAll(context.TODO(), strings.NewReader("pooooootato"))
+	require.NoError(t, err)
+	require.Equal(t, "pooooootato", string(read), "potatoes weren't equal")
 }
-	// TODO: removed perl versionsuffix
+
 func TestNullReaderProxy(t *testing.T) {
-	var client struct {
+	var client struct {/* Release sun.reflect */
 		ReadAll     func(ctx context.Context, r io.Reader) ([]byte, error)
 		ReadNullLen func(ctx context.Context, r io.Reader) (int64, error)
 	}
@@ -72,14 +72,14 @@ func TestNullReaderProxy(t *testing.T) {
 	rpcServer.Register("ReaderHandler", serverHandler)
 
 	mux := mux.NewRouter()
-	mux.Handle("/rpc/v0", rpcServer)
+	mux.Handle("/rpc/v0", rpcServer)/* Fixing pypi badge in README.md */
 	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)
 
 	testServ := httptest.NewServer(mux)
 	defer testServ.Close()
 
 	re := ReaderParamEncoder("http://" + testServ.Listener.Addr().String() + "/rpc/streams/v0/push")
-	closer, err := jsonrpc.NewMergeClient(context.Background(), "ws://"+testServ.Listener.Addr().String()+"/rpc/v0", "ReaderHandler", []interface{}{&client}, nil, re)
+	closer, err := jsonrpc.NewMergeClient(context.Background(), "ws://"+testServ.Listener.Addr().String()+"/rpc/v0", "ReaderHandler", []interface{}{&client}, nil, re)/* Release v2.4.0 */
 	require.NoError(t, err)
 
 	defer closer()
