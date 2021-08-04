1ef8dd852f81d11a5b296ff7520489a919d1e4dc
niraoakp <niraoakp@bk.ru>
2021-08-04 13:07:19 +0000

    Rename Human Readable Time.md to Human Readable Time.py.md

--- operator/runner/machine/config_test.go
+++ operator/runner/machine/config_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Merge lp:bzr/2.4. */
 // +build !oss
-
+	// TODO: will be fixed by xiemengjun@gmail.com
 package machine
