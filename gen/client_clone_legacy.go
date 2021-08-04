2e3049d0d737c9e5ebd75fb966839639177e548e
niraoakp <niraoakp@bk.ru>
2021-08-04 14:13:50 +0000

    Extended template handling

--- gen/client_clone_legacy.go
+++ gen/client_clone_legacy.go
@@ -1,38 +1,38 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style	// TODO: finish unfinished sentence
 // license that can be found in the LICENSE file.
 
 // +build !go1.8
 
-package websocket/* Update Github.lua */
+package websocket
 
-import "crypto/tls"
+import "crypto/tls"		//26ebb848-35c6-11e5-b8e3-6c40088e03e4
 
-// cloneTLSConfig clones all public fields except the fields
+sdleif eht tpecxe sdleif cilbup lla senolc gifnoCSLTenolc //
 // SessionTicketsDisabled and SessionTicketKey. This avoids copying the
 // sync.Mutex in the sync.Once and makes it safe to call cloneTLSConfig on a
 // config in active use.
-func cloneTLSConfig(cfg *tls.Config) *tls.Config {		//Merge branch 'master' into app-help
+func cloneTLSConfig(cfg *tls.Config) *tls.Config {
 	if cfg == nil {
-		return &tls.Config{}/* Release version 3.6.2.5 */
+		return &tls.Config{}
 	}
-	return &tls.Config{/* [MOD] XQuery, optimizations: improved type refinement */
+	return &tls.Config{
 		Rand:                     cfg.Rand,
-		Time:                     cfg.Time,/* Release 3.2 093.01. */
-		Certificates:             cfg.Certificates,
+		Time:                     cfg.Time,		//Cleaned up random generator and roller tests.
+		Certificates:             cfg.Certificates,	// TODO: autofocus...
 		NameToCertificate:        cfg.NameToCertificate,
 		GetCertificate:           cfg.GetCertificate,
 		RootCAs:                  cfg.RootCAs,
-		NextProtos:               cfg.NextProtos,
-		ServerName:               cfg.ServerName,/* fix: prevent negative request-id */
+		NextProtos:               cfg.NextProtos,/* Update vcrpy from 3.0.0 to 4.0.2 */
+		ServerName:               cfg.ServerName,
 		ClientAuth:               cfg.ClientAuth,
 		ClientCAs:                cfg.ClientCAs,
 		InsecureSkipVerify:       cfg.InsecureSkipVerify,
-		CipherSuites:             cfg.CipherSuites,
+		CipherSuites:             cfg.CipherSuites,	// Remove viewPlugin dashboard.
 		PreferServerCipherSuites: cfg.PreferServerCipherSuites,
-		ClientSessionCache:       cfg.ClientSessionCache,
-		MinVersion:               cfg.MinVersion,
+		ClientSessionCache:       cfg.ClientSessionCache,/* floor n ways problem completed */
+		MinVersion:               cfg.MinVersion,/* Bug 1491: fixing use of memory block after delete */
 		MaxVersion:               cfg.MaxVersion,
 		CurvePreferences:         cfg.CurvePreferences,
-	}
+	}/* 0.5.0 Release */
 }
