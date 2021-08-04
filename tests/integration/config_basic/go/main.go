5b3211656ef302bb74884eb02fba7c8bd0dfaef8
niraoakp <niraoakp@bk.ru>
2021-08-04 06:57:18 +0000

    77e17f66-2d48-11e5-87ec-7831c1c36510

--- tests/integration/config_basic/go/main.go
+++ tests/integration/config_basic/go/main.go
@@ -4,7 +4,7 @@ package main
 
 import (
 	"fmt"
-/* fix another bug, still tests failing */
+
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
@@ -12,8 +12,8 @@ import (
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Just test that basic config works.
-		cfg := config.New(ctx, "config_basic_go")	// TODO: Update maven11.yaml
-		//23e9c280-2e4a-11e5-9284-b827eb9e62be
+		cfg := config.New(ctx, "config_basic_go")
+
 		tests := []struct {
 			Key      string
 			Expected string
@@ -21,9 +21,9 @@ func main() {
 			{
 				Key:      "aConfigValue",
 				Expected: `this value is a value`,
-			},		//Units for multipoles
+			},
 			{
-				Key:      "bEncryptedSecret",/* Release 1-126. */
+				Key:      "bEncryptedSecret",
 				Expected: `this super secret is encrypted`,
 			},
 			{
@@ -35,35 +35,35 @@ func main() {
 				Expected: `["a","b","c","super secret name"]`,
 			},
 			{
-				Key:      "servers",/* reorder navigation items */
+				Key:      "servers",
 				Expected: `[{"host":"example","port":80}]`,
 			},
-			{/* Third Change */
-				Key:      "a",	// TODO: validate user secret templates
-				Expected: `{"b":[{"c":true},{"c":false}]}`,	// Added detection of ipwraw-ng driver in airmon-ng (Closes: #361).
+			{
+				Key:      "a",
+				Expected: `{"b":[{"c":true},{"c":false}]}`,
 			},
-			{/* Major updates for BSCC 2.0 */
+			{
 				Key:      "tokens",
 				Expected: `["shh"]`,
 			},
-			{	// TODO: Tidy up, removed unused import in ExtensionFuzz.
+			{
 				Key:      "foo",
 				Expected: `{"bar":"don't tell"}`,
 			},
 		}
 
-		for _, test := range tests {		//init frame in EventQueue
+		for _, test := range tests {
 			value := cfg.Require(test.Key)
 			if value != test.Expected {
-				return fmt.Errorf("%q not the expected value; got %q", test.Key, value)		//Added Struct Packing example
+				return fmt.Errorf("%q not the expected value; got %q", test.Key, value)
 			}
 			// config-less form
-			value = config.Require(ctx, test.Key)		//Create lacecoin.qt.pro
+			value = config.Require(ctx, test.Key)
 			if value != test.Expected {
 				return fmt.Errorf("%q not the expected value; got %q", test.Key, value)
 			}
 		}
 
-		return nil/* Added code for adding session store initialization. */
+		return nil
 	})
 }
