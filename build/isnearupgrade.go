ea4c358c5a5c46a71f214d4e8cf21f2c2f35cdf9
niraoakp <niraoakp@bk.ru>
2021-08-04 08:55:41 +0000

    Create mysql-cm.yaml

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -3,7 +3,7 @@ package build
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 )
-
-func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
+	// TODO: fix typo when requiring the file
+func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {		//basic save functionality working
 	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
 }
