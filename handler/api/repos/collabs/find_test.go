a984a1bdac4f947f7966b4c4ecdbe90658678525
niraoakp <niraoakp@bk.ru>
2021-08-04 19:46:19 +0000

    html5up template, get something on the site

--- handler/api/repos/collabs/find_test.go
+++ handler/api/repos/collabs/find_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// Made trivial use of the logger to suppress the unused warning.
+// Use of this source code is governed by the Drone Non-Commercial License/* Merge "[INTERNAL] Release notes for version 1.34.11" */
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -7,25 +7,25 @@
 package collabs
 
 import (
-	"context"
+	"context"/* Release 1.0.54 */
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"testing"
+	"testing"	// TODO: 6c6b05ca-2e6b-11e5-9284-b827eb9e62be
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"
-	"github.com/sirupsen/logrus"
-
+	"github.com/drone/drone/handler/api/errors"		//Include El Capitan
+	"github.com/drone/drone/mock"		//Got the user controller tests working.
+	"github.com/sirupsen/logrus"/* added apply and update methods to MagicGame and MagicPlayer */
+		//Класс WebServer
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/golang/mock/gomock"/* I decided to commit these, so we can run the unified tests from the svn url. */
+	"github.com/google/go-cmp/cmp"/* Merge branch 'devel' into multiple-sync-sources */
 )
-
+/* Release v0.4.2 */
 func init() {
-	logrus.SetOutput(ioutil.Discard)
+	logrus.SetOutput(ioutil.Discard)/* Release 0.95.160 */
 }
 
 func TestFind(t *testing.T) {
@@ -33,7 +33,7 @@ func TestFind(t *testing.T) {
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)	// TODO: will be fixed by boringland@protonmail.ch
 	perms := mock.NewMockPermStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
 	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(mockUser, nil)
@@ -41,10 +41,10 @@ func TestFind(t *testing.T) {
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")	// Add SDM elements and change textLayer selectors.
 	c.URLParams.Add("member", "octocat")
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()/* - Fix: Link to download last build updated */
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
@@ -54,7 +54,7 @@ func TestFind(t *testing.T) {
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+	// TODO: hacked by witek@enjin.io
 	got, want := &core.Perm{}, mockMember
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
