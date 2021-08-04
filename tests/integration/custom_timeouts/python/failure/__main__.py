7db6942f075357d11732a2e8fd8ee16ecb6a8c93
niraoakp <niraoakp@bk.ru>
2021-08-04 02:16:15 +0000

    decoupled config dependency from group

--- tests/integration/custom_timeouts/python/failure/__main__.py
+++ tests/integration/custom_timeouts/python/failure/__main__.py
@@ -1,8 +1,8 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions
-		//Merge branch 'master' into dnil-patch-1
-class Resource1(ComponentResource):		//Merge branch 'master' into feature/clickable
+
+class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
