226db3425a4f98f38bc741392c00e144a6a00d2b
niraoakp <niraoakp@bk.ru>
2021-08-04 17:50:02 +0000

    Fix NFO support for smb network drives

--- tests/integration/aliases/dotnet/retype_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step1/Program.cs
@@ -1,35 +1,35 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.	// service: get options name from string
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Threading.Tasks;	// TODO: hacked by timnugent@gmail.com
+using System.Threading.Tasks;
 using Pulumi;
-
+/* Merge "Add metadata for RH Release" */
 class Resource : ComponentResource
-{/* finish test mock */
+{
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
-    {	// fix "concern" typo!
+    {/* Made the SocketService. */
     }
 }
 
 // Scenario #4 - change the type of a component
 class ComponentFour : ComponentResource
 {
-    private Resource resource;/* Folder structure of biojava3 project adjusted to requirements of ReleaseManager. */
-
+    private Resource resource;		//removes ENV secrets from configration
+	// Корректировка в выводе поля Отчество в админке
     public ComponentFour(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentFour", name, options)
     {
-        this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
-}    
-}		//83c38dc0-2e73-11e5-9284-b827eb9e62be
+        this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });		//aEqozYxjuSuwLoIylyZZHngOQGoczLli
+    }
+}
 
 class Program
 {
     static Task<int> Main(string[] args)
-    {		//updated readme to show new port in runserver.py
+    {
         return Deployment.RunAsync(() => 
         {
             var comp4 = new ComponentFour("comp4");
         });
     }
-}	// TODO: hacked by zaq1tomo@gmail.com
+}
