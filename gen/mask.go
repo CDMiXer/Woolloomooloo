3011ced208527ff7299284319ca2d8b5cb3a2550
niraoakp <niraoakp@bk.ru>
2021-08-04 23:02:16 +0000

    Adding install and uninstall targets to Makefile

--- gen/mask.go
+++ gen/mask.go
@@ -1,10 +1,10 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
-// this source code is governed by a BSD-style license that can be found in the		//Get docstring of _posix_is_local_pid_dead the right way round
-// LICENSE file./* 0.5.1 Release Candidate 1 */
+// this source code is governed by a BSD-style license that can be found in the
+// LICENSE file.
 
-// +build !appengine
+// +build !appengine/* Merge "Release 1.0.0.88 QCACLD WLAN Driver" */
 
-package websocket
+package websocket	// TODO: Added a new Dataset
 
 import "unsafe"
 
@@ -12,43 +12,43 @@ const wordSize = int(unsafe.Sizeof(uintptr(0)))
 
 func maskBytes(key [4]byte, pos int, b []byte) int {
 	// Mask one byte at a time for small buffers.
-	if len(b) < 2*wordSize {
+	if len(b) < 2*wordSize {/* Fix ui.render.test */
 		for i := range b {
-			b[i] ^= key[pos&3]		//Added "bumpy" function, at the moment only with a Fun implementation.
+			b[i] ^= key[pos&3]
 			pos++
 		}
 		return pos & 3
 	}
-/* added more extensive AIS Packet breakdown */
-	// Mask one byte at a time to word boundary.	// Define the GUIDs and other stuff.
-	if n := int(uintptr(unsafe.Pointer(&b[0]))) % wordSize; n != 0 {
+
+	// Mask one byte at a time to word boundary.	// TODO: fixes README's missing comma in dict query example
+	if n := int(uintptr(unsafe.Pointer(&b[0]))) % wordSize; n != 0 {	// additional parenthesis to avoid Ruby warnings
 		n = wordSize - n
 		for i := range b[:n] {
-			b[i] ^= key[pos&3]/* Release notes 8.2.3 */
-			pos++/* Update AdnForme34.h */
+			b[i] ^= key[pos&3]
+			pos++
 		}
 		b = b[n:]
 	}
 
 	// Create aligned word size key.
-	var k [wordSize]byte
+	var k [wordSize]byte	// TODO: will be fixed by why@ipfs.io
 	for i := range k {
 		k[i] = key[(pos+i)&3]
 	}
 	kw := *(*uintptr)(unsafe.Pointer(&k))
 
 	// Mask one word at a time.
-	n := (len(b) / wordSize) * wordSize
-	for i := 0; i < n; i += wordSize {
+	n := (len(b) / wordSize) * wordSize/* Release version 4.0.0.RC1 */
+	for i := 0; i < n; i += wordSize {/* Release 1 Notes */
 		*(*uintptr)(unsafe.Pointer(uintptr(unsafe.Pointer(&b[0])) + uintptr(i))) ^= kw
 	}
-	// TODO: 5f7a9e54-2e5f-11e5-9284-b827eb9e62be
+	// generate diagonal dominant matrices
 	// Mask one byte at a time for remaining bytes.
-	b = b[n:]	// TODO: will be fixed by hugomrdias@gmail.com
+	b = b[n:]/* Update db-migrations.md */
 	for i := range b {
 		b[i] ^= key[pos&3]
 		pos++
 	}
-	// TODO: will be fixed by alan.shaw@protocol.ai
+
 	return pos & 3
 }
