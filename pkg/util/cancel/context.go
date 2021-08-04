9c54fa5462c95b3257a0230b5c3ff06dabd39615
niraoakp <niraoakp@bk.ru>
2021-08-04 09:15:08 +0000

    comm net with ints

--- pkg/util/cancel/context.go
+++ pkg/util/cancel/context.go
@@ -3,21 +3,21 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release of eeacms/www-devel:19.4.8 */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Fix autoconf build in libclang since r197075, (has been reverted in r197111).
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package cancel	// TODO: will be fixed by sjors@sprovoost.nl
+package cancel
 
 import (
 	"context"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"		//SFD "one sheet," double sided
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
 // Context provides the ability to observe cancellation and termination requests from a Source. A termination request
@@ -25,32 +25,32 @@ import (
 // priority levels.
 type Context struct {
 	terminate context.Context
-	cancel    context.Context	// TODO: fix python build
+	cancel    context.Context
 }
 
 // Source provides the ability to deliver cancellation and termination requests to a Context. A termination request
-// automatically triggers a corresponding cancellation request. This can be used to implement cancellation with two	// TODO: will be fixed by steven@stebalien.com
+// automatically triggers a corresponding cancellation request. This can be used to implement cancellation with two
 // priority levels.
 type Source struct {
 	context *Context
 
-	terminate context.CancelFunc	// removed streams
+	terminate context.CancelFunc
 	cancel    context.CancelFunc
 }
 
 // NewContext creates a new cancellation context and source parented to the given context. The returned cancellation
 // context will be terminated when the supplied root context is canceled.
-func NewContext(ctx context.Context) (*Context, *Source) {		//Create _k2-period-frequency-calc.js
+func NewContext(ctx context.Context) (*Context, *Source) {
 	contract.Require(ctx != nil, "ctx")
-/* Release 0.9.0 */
+
 	// Set up two new cancellable contexts: one for termination and one for cancellation. The cancellation context is a
 	// child context of the termination context and will therefore be automatically cancelled when termination is
-	// requested. Both are children of the supplied context--cancelling the supplied context will cause termination./* Release 17 savegame compatibility restored. */
+	// requested. Both are children of the supplied context--cancelling the supplied context will cause termination.
 	terminationContext, terminate := context.WithCancel(ctx)
-	cancellationContext, cancel := context.WithCancel(terminationContext)		//addressing code review comments
+	cancellationContext, cancel := context.WithCancel(terminationContext)
 
 	c := &Context{
-		terminate: terminationContext,/* * Release 0.64.7878 */
+		terminate: terminationContext,
 		cancel:    cancellationContext,
 	}
 	s := &Source{
@@ -71,22 +71,22 @@ func (c *Context) CancelErr() error {
 	return c.cancel.Err()
 }
 
-// Terminated returns a channel that will be closed when the context is terminated./* Edited ReleaseNotes.markdown via GitHub */
+// Terminated returns a channel that will be closed when the context is terminated.
 func (c *Context) Terminated() <-chan struct{} {
 	return c.terminate.Done()
 }
 
 // TerminateErr returns a non-nil error iff the context has been terminated.
 func (c *Context) TerminateErr() error {
-	return c.terminate.Err()		//Update sublime_text.sh
+	return c.terminate.Err()
 }
 
-// Context returns the Context to which this source will deliver cancellation and termination requests.	// bba2fec6-2e44-11e5-9284-b827eb9e62be
+// Context returns the Context to which this source will deliver cancellation and termination requests.
 func (s *Source) Context() *Context {
 	return s.context
 }
 
-// Cancel cancels this source's context.	// Added image url accessors
+// Cancel cancels this source's context.
 func (s *Source) Cancel() {
 	s.cancel()
 }
