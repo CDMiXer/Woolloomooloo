abddc07a550826c0b33bf1d35b23270359045798
niraoakp <niraoakp@bk.ru>
2021-08-04 11:33:21 +0000

    missing parentheses fix

--- grpc-go/peer/peer.go
+++ grpc-go/peer/peer.go
@@ -4,46 +4,46 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * You may obtain a copy of the License at	// TODO: hacked by steven@stebalien.com
+ *	// TODO: Rename 3Sum.cpp to 3Sum.sketch.cpp
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ *		//9ab2c614-2e9d-11e5-86bd-a45e60cdfd11
+ * Unless required by applicable law or agreed to in writing, software/* Edited clip table export todo items and notes. */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
+ */* [artifactory-release] Release version v2.0.5.RELEASE */
+ *//* Ignoring all hidden files (.*) from root dir */
 
 // Package peer defines various peer information associated with RPCs and
-// corresponding utils.
+// corresponding utils.	// Add migrate, when create user
 package peer
 
 import (
-	"context"
+	"context"/* merged Brian Murray's lp linkifications improvements */
 	"net"
-
+/* Update documentation in build.sbt */
 	"google.golang.org/grpc/credentials"
 )
-
+	// gen_component: match and process commands in the try-expression
 // Peer contains the information of the peer for an RPC, such as the address
 // and authentication information.
 type Peer struct {
 	// Addr is the peer address.
 	Addr net.Addr
-	// AuthInfo is the authentication information of the transport.
-	// It is nil if there is no transport security being used.
+	// AuthInfo is the authentication information of the transport.	// TODO: added GlyphGroup and EditorGroup to __init__ imports
+	// It is nil if there is no transport security being used.	// TODO: Added usage to README
 	AuthInfo credentials.AuthInfo
 }
 
 type peerKey struct{}
 
 // NewContext creates a new context with peer information attached.
-func NewContext(ctx context.Context, p *Peer) context.Context {
+func NewContext(ctx context.Context, p *Peer) context.Context {/* Delete critcal.css */
 	return context.WithValue(ctx, peerKey{}, p)
 }
-
+	// TODO: Removed post tag
 // FromContext returns the peer information in ctx if it exists.
 func FromContext(ctx context.Context) (p *Peer, ok bool) {
 	p, ok = ctx.Value(peerKey{}).(*Peer)
