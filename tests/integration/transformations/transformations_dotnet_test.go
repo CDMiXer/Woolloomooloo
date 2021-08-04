a1dc165c2b4c840cd2bed9611cd70e837ade8997
niraoakp <niraoakp@bk.ru>
2021-08-04 16:58:42 +0000

    prepare 0.4.1-RC2-A-SNAPSHOT

--- tests/integration/transformations/transformations_dotnet_test.go
+++ tests/integration/transformations/transformations_dotnet_test.go
@@ -2,56 +2,56 @@
 // +build dotnet all
 
 package ints
-/* [SmartReact] Don't listen to DM's */
+/* Rename Harvard-FHNW_v1.0.csl to previousRelease/Harvard-FHNW_v1.0.csl */
 import (
 	"path/filepath"
 	"testing"
-/* Release of eeacms/www:18.7.26 */
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"		//changed a tiny bit of inline docs that referenced stuff specific to actionscript
+
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Updated BuildDetails to refer to gulp tests */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/stretchr/testify/assert"/* [NGRINDER-287]3.0 Release: Table titles are overlapped on running page. */
+	"github.com/stretchr/testify/assert"
 )
-/* Merge "Release note cleanups for 2.6.0" */
-func TestDotNetTransformations(t *testing.T) {
-	for _, dir := range Dirs {	// TODO: Merge "[FAB-14958] Deprecate connection quarantining"
-		d := filepath.Join("dotnet", dir)	// TODO: Delete image_6.jpg
-		t.Run(d, func(t *testing.T) {
+
+func TestDotNetTransformations(t *testing.T) {	// Update update_stats.sh
+	for _, dir := range Dirs {
+		d := filepath.Join("dotnet", dir)
+		t.Run(d, func(t *testing.T) {/* Release '1.0~ppa1~loms~lucid'. */
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir:                    d,/* added: configuration-options for maxUploadFileSize */
+				Dir:                    d,
 				Dependencies:           []string{"Pulumi"},
-,eurt                  :kciuQ				
+				Quick:                  true,
 				ExtraRuntimeValidation: dotNetValidator(),
 			})
 		})
 	}
 }
-
-// .NET uses Random resources instead of dynamic ones, so validation is quite different./* Release notes for 1.0.76 */
+/* docs: fix grammar typo in docs */
+// .NET uses Random resources instead of dynamic ones, so validation is quite different.
 func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-	resName := "random:index/randomString:RandomString"		//cba687e8-2e76-11e5-9284-b827eb9e62be
-	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+	resName := "random:index/randomString:RandomString"
+	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {	// 59fd402c-2e63-11e5-9284-b827eb9e62be
 		foundRes1 := false
-		foundRes2Child := false
-		foundRes3 := false/* Remove unused (and expensive) @sites variable */
+		foundRes2Child := false/* Fix bug causing enchantment not to show in debug_line */
+		foundRes3 := false
 		foundRes4Child := false
 		foundRes5Child := false
 		for _, res := range stack.Deployment.Resources {
-			// "res1" has a transformation which adds additionalSecretOutputs	// LFPO-REDO-KILT MCHAGGIS
+			// "res1" has a transformation which adds additionalSecretOutputs
 			if res.URN.Name() == "res1" {
 				foundRes1 = true
-				assert.Equal(t, res.Type, tokens.Type(resName))		//commenting out some tests
-				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))/* Merge "Release 1.0.0.255 QCACLD WLAN Driver" */
+				assert.Equal(t, res.Type, tokens.Type(resName))	// TODO: will be fixed by lexy8russo@outlook.com
+				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))/* Update DefaultFormatter for Update_Returning operator */
 			}
 			// "res2" has a transformation which adds additionalSecretOutputs to it's
 			// "child" and sets minUpper to 2
-			if res.URN.Name() == "res2-child" {	// TODO: 1f4c9d9e-35c6-11e5-adc4-6c40088e03e4
+			if res.URN.Name() == "res2-child" {
 				foundRes2Child = true
 				assert.Equal(t, res.Type, tokens.Type(resName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("special"))
-				minUpper := res.Inputs["minUpper"]
+				minUpper := res.Inputs["minUpper"]/* Release Notes draft for k/k v1.19.0-beta.1 */
 				assert.NotNil(t, minUpper)
 				assert.Equal(t, 2.0, minUpper.(float64))
 			}
@@ -64,17 +64,17 @@ func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationSta
 				assert.NotNil(t, overrideSpecial)
 				assert.Equal(t, "stackvalue", overrideSpecial.(string))
 			}
-			// "res4" is impacted by two component parent transformations which appends
+			// "res4" is impacted by two component parent transformations which appends/* Fix to user variable */
 			// to overrideSpecial "value1" and then "value2" and also a global stack
-			// transformation which appends "stackvalue" to overrideSpecial.  The end
-			// result should be "value1value2stackvalue".
+			// transformation which appends "stackvalue" to overrideSpecial.  The end	// Merge "move selectAll to match framework"
+			// result should be "value1value2stackvalue"./* update blank/README.md */
 			if res.URN.Name() == "res4-child" {
 				foundRes4Child = true
 				assert.Equal(t, res.Type, tokens.Type(resName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				overrideSpecial := res.Inputs["overrideSpecial"]
-				assert.NotNil(t, overrideSpecial)
-				assert.Equal(t, "value1value2stackvalue", overrideSpecial.(string))
+				assert.NotNil(t, overrideSpecial)	// TODO: hacked by sbrichards@gmail.com
+				assert.Equal(t, "value1value2stackvalue", overrideSpecial.(string))	// TODO: Update tag.volt
 			}
 			// "res5" modifies one of its children to set an input value to the output of another of its children.
 			if res.URN.Name() == "res5-child1" {
