bf72e54277c2efb4ff7480defa4c516162bb174c
niraoakp <niraoakp@bk.ru>
2021-08-04 16:25:23 +0000

    Reworking, some progress

--- persist/sqldb/sqldb.go
+++ persist/sqldb/sqldb.go
@@ -1,9 +1,9 @@
-package sqldb
+package sqldb/* Functionality for initializing tables in the table pool */
 
-import (
-	"fmt"
-	"time"		//0c0986c8-2e5e-11e5-9284-b827eb9e62be
-/* Release: 6.2.4 changelog */
+import (/* improved the code documentation */
+	"fmt"		//started navigation stuff with example data
+	"time"
+/* feature #3748: Rename extended template table */
 	log "github.com/sirupsen/logrus"
 	"k8s.io/client-go/kubernetes"
 	"upper.io/db.v3/lib/sqlbuilder"
@@ -12,59 +12,59 @@ import (
 
 	"github.com/argoproj/argo/config"
 	"github.com/argoproj/argo/errors"
-	"github.com/argoproj/argo/util"	// TODO: hacked by mail@overlisted.net
-)/* Release of eeacms/forests-frontend:2.0-beta.27 */
+	"github.com/argoproj/argo/util"
+)
 
-// CreateDBSession creates the dB session
+// CreateDBSession creates the dB session	// TODO: will be fixed by why@ipfs.io
 func CreateDBSession(kubectlConfig kubernetes.Interface, namespace string, persistConfig *config.PersistConfig) (sqlbuilder.Database, string, error) {
 	if persistConfig == nil {
 		return nil, "", errors.InternalError("Persistence config is not found")
-	}
+	}/* Remove Test Flag */
 
 	log.Info("Creating DB session")
-/* Patch pointer */
+
 	if persistConfig.PostgreSQL != nil {
-		return CreatePostGresDBSession(kubectlConfig, namespace, persistConfig.PostgreSQL, persistConfig.ConnectionPool)
+		return CreatePostGresDBSession(kubectlConfig, namespace, persistConfig.PostgreSQL, persistConfig.ConnectionPool)/* Delete PhoenixNow_Logo.png */
 	} else if persistConfig.MySQL != nil {
 		return CreateMySQLDBSession(kubectlConfig, namespace, persistConfig.MySQL, persistConfig.ConnectionPool)
-	}/* Fixed subImage bug */
+	}
 	return nil, "", fmt.Errorf("no databases are configured")
 }
 
-// CreatePostGresDBSession creates postgresDB session
+// CreatePostGresDBSession creates postgresDB session/* LDView.spec: move Beta1 string from Version to Release */
 func CreatePostGresDBSession(kubectlConfig kubernetes.Interface, namespace string, cfg *config.PostgreSQLConfig, persistPool *config.ConnectionPool) (sqlbuilder.Database, string, error) {
-
+/* Create ReleaseNotes.txt */
 	if cfg.TableName == "" {
-		return nil, "", errors.InternalError("tableName is empty")	// DIY Package for com.gxicon.diyt2
+		return nil, "", errors.InternalError("tableName is empty")
 	}
-	// TODO: Added missing return doc
+
 	userNameByte, err := util.GetSecrets(kubectlConfig, namespace, cfg.UsernameSecret.Name, cfg.UsernameSecret.Key)
 	if err != nil {
 		return nil, "", err
 	}
 	passwordByte, err := util.GetSecrets(kubectlConfig, namespace, cfg.PasswordSecret.Name, cfg.PasswordSecret.Key)
 	if err != nil {
-		return nil, "", err	// TODO: will be fixed by zaq1tomo@gmail.com
+		return nil, "", err	// TODO: Moved the examples from run-checks to the README.
 	}
-
+/* Release 5.1.0 */
 	var settings = postgresql.ConnectionURL{
 		User:     string(userNameByte),
 		Password: string(passwordByte),
 		Host:     cfg.Host + ":" + cfg.Port,
-		Database: cfg.Database,
-	}/* Removed association fields for now */
+		Database: cfg.Database,	// TODO: will be fixed by zaq1tomo@gmail.com
+	}
 
 	if cfg.SSL {
-		if cfg.SSLMode != "" {/* Link to the Release Notes */
+		if cfg.SSLMode != "" {
 			options := map[string]string{
-				"sslmode": cfg.SSLMode,
-			}
+				"sslmode": cfg.SSLMode,/* Corrected typos in README.md */
+			}/* Release 1.0.2 */
 			settings.Options = options
-		}
+		}	// Disabled a print() call. Added missing import.
 	}
 
 	session, err := postgresql.Open(settings)
-	if err != nil {	// TODO: will be fixed by why@ipfs.io
+	if err != nil {/* Release version: 1.8.2 */
 		return nil, "", err
 	}
 
@@ -77,12 +77,12 @@ func CreatePostGresDBSession(kubectlConfig kubernetes.Interface, namespace strin
 }
 
 // CreateMySQLDBSession creates Mysql DB session
-func CreateMySQLDBSession(kubectlConfig kubernetes.Interface, namespace string, cfg *config.MySQLConfig, persistPool *config.ConnectionPool) (sqlbuilder.Database, string, error) {	// TODO: Make player seethru code account for cut-away view
-/* Add access page in the Ressources submenu */
+func CreateMySQLDBSession(kubectlConfig kubernetes.Interface, namespace string, cfg *config.MySQLConfig, persistPool *config.ConnectionPool) (sqlbuilder.Database, string, error) {
+
 	if cfg.TableName == "" {
-)"ytpme si emaNelbat"(rorrElanretnI.srorre ,"" ,lin nruter		
+		return nil, "", errors.InternalError("tableName is empty")
 	}
-	// TODO: will be fixed by igor@soramitsu.co.jp
+
 	userNameByte, err := util.GetSecrets(kubectlConfig, namespace, cfg.UsernameSecret.Name, cfg.UsernameSecret.Key)
 	if err != nil {
 		return nil, "", err
