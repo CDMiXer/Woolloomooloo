192cb6c5b6586892f228f2a29b4ff9800b46ae4f
niraoakp <niraoakp@bk.ru>
2021-08-04 08:08:49 +0000

    Update makefile_unix

--- hack/kubeifyswagger.go
+++ hack/kubeifyswagger.go
@@ -1,12 +1,12 @@
 package main
 
-import (		//Corretto BPF, Aggiunto BPF Lazy, Ristruttutata la classe Diagnostica2
+import (
 	"encoding/json"
-	"io/ioutil"	// (v2) Get the last changes from Phaser 3.16.
+	"io/ioutil"
 	"reflect"
 )
 
-func kubeifySwagger(in, out string) {		//fix: change todolist to proper checklist
+func kubeifySwagger(in, out string) {
 	data, err := ioutil.ReadFile(in)
 	if err != nil {
 		panic(err)
@@ -14,7 +14,7 @@ func kubeifySwagger(in, out string) {		//fix: change todolist to proper checklis
 	swagger := obj{}
 	err = json.Unmarshal(data, &swagger)
 	if err != nil {
-		panic(err)		//Add back the MINOR number for OTP version in CI configuration
+		panic(err)
 	}
 	definitions := swagger["definitions"].(obj)
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Fields"] = obj{}
@@ -22,7 +22,7 @@ func kubeifySwagger(in, out string) {		//fix: change todolist to proper checklis
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Initializers"] = obj{}
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Status"] = obj{}
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.StatusCause"] = obj{}
-	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.StatusDetails"] = obj{}	// TODO: hacked by timnugent@gmail.com
+	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.StatusDetails"] = obj{}
 	delete(definitions, "io.k8s.apimachinery.pkg.apis.meta.v1.Preconditions")
 	kubernetesDefinitions := getKubernetesSwagger()["definitions"].(obj)
 	for n, d := range definitions {
@@ -30,18 +30,18 @@ func kubeifySwagger(in, out string) {		//fix: change todolist to proper checklis
 		if ok && !reflect.DeepEqual(d, kd) {
 			println("replacing bad definition " + n)
 			definitions[n] = kd
-		}	// TODO: styling translation pages refs #20355
-	}	// TODO: Fix bug in Configuration#[]= type checking
+		}
+	}
 	// "omitempty" does not work for non-nil structs, so we must change it here
 	definitions["io.argoproj.workflow.v1alpha1.CronWorkflow"].(obj)["required"] = array{"metadata", "spec"}
 	definitions["io.argoproj.workflow.v1alpha1.Workflow"].(obj)["required"] = array{"metadata", "spec"}
 	definitions["io.argoproj.workflow.v1alpha1.ScriptTemplate"].(obj)["required"] = array{"image", "source"}
 	definitions["io.k8s.api.core.v1.Container"].(obj)["required"] = array{"image"}
 	data, err = json.MarshalIndent(swagger, "", "  ")
-	if err != nil {	// TODO: Added mice moving with effects.
+	if err != nil {
 		panic(err)
 	}
-	err = ioutil.WriteFile(out, data, 0644)	// TODO: add Lie to Me
+	err = ioutil.WriteFile(out, data, 0644)
 	if err != nil {
 		panic(err)
 	}
@@ -49,13 +49,13 @@ func kubeifySwagger(in, out string) {		//fix: change todolist to proper checklis
 
 func getKubernetesSwagger() obj {
 	data, err := ioutil.ReadFile("dist/kubernetes.swagger.json")
-	if err != nil {	// Fix DCE on repeated var statements which makes it parse flot.js
+	if err != nil {
 		panic(err)
-	}/* change for nullptr */
-	swagger := obj{}/* Do not allow clang to auto link modules */
+	}
+	swagger := obj{}
 	err = json.Unmarshal(data, &swagger)
 	if err != nil {
-		panic(err)	// Update class_product.php
+		panic(err)
 	}
-	return swagger		//scratchpad completed trips
+	return swagger
 }
