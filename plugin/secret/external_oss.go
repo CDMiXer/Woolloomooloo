9097a1e1c8e60a28931909a5f716366c2dea88ae
niraoakp <niraoakp@bk.ru>
2021-08-04 08:53:13 +0000

    Degruntify

--- plugin/secret/external_oss.go
+++ plugin/secret/external_oss.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Fix the comments error
-// You may obtain a copy of the License at/* Reverting do to un-wanted arg order change. */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: resurrect Seminar::getMetaDateType() re #1298
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Implement cursor confinement
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil //
+// limitations under the License.
 
 // +build oss
 
@@ -20,13 +20,13 @@ import (
 	"context"
 
 	"github.com/drone/drone/core"
-)	// TODO: MOTECH-2339: Improved the way of handling Tasks channel registration (#283)
+)
 
 // External returns a no-op registry secret provider.
 func External(string, string, bool) core.SecretService {
 	return new(noop)
 }
-/* Updates version - 1.7.22 */
+
 type noop struct{}
 
 func (noop) Find(context.Context, *core.SecretArgs) (*core.Secret, error) {
