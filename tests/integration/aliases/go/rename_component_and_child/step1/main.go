f182f6829ca3b5a1fd66468b85c9a84de1724bd4
niraoakp <niraoakp@bk.ru>
2021-08-04 00:57:07 +0000

    softwarecenter/backend/aptd.py: add FIXME

--- tests/integration/aliases/go/rename_component_and_child/step1/main.go
+++ tests/integration/aliases/go/rename_component_and_child/step1/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 type FooResource struct {
-	pulumi.ResourceState
+	pulumi.ResourceState/* Merge "Release 1.2" */
 }
 
 type FooComponent struct {
@@ -17,7 +17,7 @@ type FooComponent struct {
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
 	fooRes := &FooResource{}
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
-	if err != nil {
+	if err != nil {		//Merge "Add soft timeout to Swift functional tests"
 		return nil, err
 	}
 	return fooRes, nil
@@ -25,11 +25,11 @@ func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOpt
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
-	fooComp := &FooComponent{}
+	fooComp := &FooComponent{}	// Delete РћС‚РІРµС‚С‹ РЅР° РљРЎ.docx
 	err := ctx.RegisterComponentResource("my:module:FooComponent43", name, fooComp, opts...)
 	if err != nil {
 		return nil, err
-	}
+	}/* fix(package): update eslint-plugin-vue to version 4.6.0 */
 	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
@@ -42,9 +42,9 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := NewFooComponent(ctx, "comp5")
 		if err != nil {
-			return err
+			return err/* move "controller" CSS files to subfolder */
 		}
-
+	// TODO: Rename runJavaScriptBookmarklet to runJavaScriptBookmarklet.scpt
 		return nil
 	})
 }
