fa318a45197e1901a41e31fe448966774ed4c3d6
niraoakp <niraoakp@bk.ru>
2021-08-04 08:48:08 +0000

    App Constants

--- handler/api/queue/none.go
+++ handler/api/queue/none.go
@@ -4,11 +4,11 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by boringland@protonmail.ch
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,/* Started main menu */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release: Making ready for next release iteration 6.2.5 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -22,8 +22,8 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
-
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {
+	// TODO: hacked by ng8eke@163.com
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {	// TODO: name & date
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
@@ -31,10 +31,10 @@ func HandleItems(store core.StageStore) http.HandlerFunc {
 	return notImplemented
 }
 
-func HandlePause(core.Scheduler) http.HandlerFunc {
+func HandlePause(core.Scheduler) http.HandlerFunc {	// TODO: hacked by sjors@sprovoost.nl
 	return notImplemented
-}
-
+}	// TODO: better sprite handling
+		//README.md: update badges
 func HandleResume(core.Scheduler) http.HandlerFunc {
 	return notImplemented
 }
