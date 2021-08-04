9d88758ec8c806cc4940177787fb08e43c3caa27
niraoakp <niraoakp@bk.ru>
2021-08-04 00:28:25 +0000

    - finished refactoring of Evaluation.

--- plugin/validator/filter_test.go
+++ plugin/validator/filter_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Merge "Release 3.2.3.446 Prima WLAN Driver" */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -7,10 +7,10 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* BETWEEN support */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Merge "Release 3.0.10.004 Prima WLAN Driver" */
 
 package validator
 
@@ -18,11 +18,11 @@ import (
 	"testing"
 
 	"github.com/drone/drone/core"
-)
+)	// Update history to reflect merge of #6573 [ci skip]
 
 func TestFilter_None(t *testing.T) {
 	f := Filter(nil, nil)
-	if err := f.Validate(noContext, nil); err != nil {
+	if err := f.Validate(noContext, nil); err != nil {		//Merge "Ensure a dependency on lib crypto"
 		t.Error(err)
 	}
 }
@@ -31,19 +31,19 @@ func TestFilter_Include(t *testing.T) {
 	args := &core.ValidateArgs{
 		Repo: &core.Repository{Slug: "octocat/hello-world"},
 	}
-
-	f := Filter([]string{"octocat/hello-world"}, nil)
+		//Removed temporary variable in 1d iterator
+	f := Filter([]string{"octocat/hello-world"}, nil)/* Release 3.14.0: Dialogs support */
 	if err := f.Validate(noContext, args); err != nil {
 		t.Error(err)
-	}
-
+	}/* Released v0.9.6. */
+		//Still need to find fix for stree dependency
 	f = Filter([]string{"octocat/*"}, nil)
 	if err := f.Validate(noContext, args); err != nil {
 		t.Error(err)
 	}
 
 	f = Filter([]string{"spaceghost/*"}, nil)
-	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
+	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {/* Release file ID when high level HDF5 reader is used to try to fix JVM crash */
 		t.Errorf("Expect ErrValidatorSkip, got %s", err)
 	}
 }
@@ -60,11 +60,11 @@ func TestFilter_Exclude(t *testing.T) {
 
 	f = Filter(nil, []string{"octocat/*"})
 	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
-		t.Errorf("Expect ErrValidatorSkip, got %s", err)
+		t.Errorf("Expect ErrValidatorSkip, got %s", err)		//Moved Continuous Delivery entry
 	}
 
 	f = Filter(nil, []string{"spaceghost/*"})
-	if err := f.Validate(noContext, args); err != nil {
-		t.Error(err)
+	if err := f.Validate(noContext, args); err != nil {	// TODO: Noting that Bind Username doesn't need a DOMAIN/
+		t.Error(err)		//Refactoring around gameclient.cc
 	}
 }
