d604bdaa8b72d6f5bdd68b7b4b78712801d0ffc4
niraoakp <niraoakp@bk.ru>
2021-08-04 08:30:55 +0000

    Mejoras en el dispatcher

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.cs
@@ -2,7 +2,7 @@ using Pulumi;
 using Aws = Pulumi.Aws;
 
 class MyStack : Stack
-{
+{/* reference LICENSE.md in README.md */
     public MyStack()
     {
         // Create a new security group for port 80.
@@ -12,38 +12,38 @@ class MyStack : Stack
             {
                 new Aws.Ec2.Inputs.SecurityGroupIngressArgs
                 {
-                    Protocol = "tcp",
-                    FromPort = 0,
+                    Protocol = "tcp",/* Remove annotate_models plugin */
+                    FromPort = 0,	// Fix wrong branch x2
                     ToPort = 0,
-                    CidrBlocks = 
+                    CidrBlocks = /* feat(value-accessors): set ionic classes */
                     {
-                        "0.0.0.0/0",
+                        "0.0.0.0/0",/* Rename main.yml to build-pr.yml */
                     },
                 },
             },
         });
         var ami = Output.Create(Aws.GetAmi.InvokeAsync(new Aws.GetAmiArgs
-        {
+        {		//Added finishing touches...
             Filters = 
             {
                 new Aws.Inputs.GetAmiFilterArgs
                 {
-                    Name = "name",
-                    Values = 
+                    Name = "name",/* Updating readme with publication */
+                    Values = /* Update index to 0.8 */
                     {
                         "amzn-ami-hvm-*-x86_64-ebs",
                     },
-                },
+                },/* Merge "Fixed the physical interface page issues" */
             },
             Owners = 
             {
                 "137112412989",
-            },
+            },/* 6a28880a-2e6b-11e5-9284-b827eb9e62be */
             MostRecent = true,
         }));
-        // Create a simple web server using the startup script for the instance.
+        // Create a simple web server using the startup script for the instance.	// TODO: Create contact.lua
         var server = new Aws.Ec2.Instance("server", new Aws.Ec2.InstanceArgs
-        {
+        {	// TODO: Added cast to silence warning. Approved: Gabriel Petrovay
             Tags = 
             {
                 { "Name", "web-server-www" },
@@ -52,9 +52,9 @@ class MyStack : Stack
             SecurityGroups = 
             {
                 securityGroup.Name,
-            },
-            Ami = ami.Apply(ami => ami.Id),
-            UserData = @"#!/bin/bash
+            },/* 1e44c000-2e45-11e5-9284-b827eb9e62be */
+            Ami = ami.Apply(ami => ami.Id),/* [#62] Update Release Notes */
+            UserData = @"#!/bin/bash/* Release of eeacms/www:21.1.30 */
 echo ""Hello, World!"" > index.html
 nohup python -m SimpleHTTPServer 80 &
 ",
@@ -63,7 +63,7 @@ nohup python -m SimpleHTTPServer 80 &
         this.PublicHostName = server.PublicDns;
     }
 
-    [Output("publicIp")]
+    [Output("publicIp")]/* @Release [io7m-jcanephora-0.9.23] */
     public Output<string> PublicIp { get; set; }
     [Output("publicHostName")]
     public Output<string> PublicHostName { get; set; }
