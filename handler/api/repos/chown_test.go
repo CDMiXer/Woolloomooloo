0e3a4f0c70da07f77682528fcb763cca20859cbb
niraoakp <niraoakp@bk.ru>
2021-08-04 12:43:03 +0000

    Rename browser.html to myBrowser.html

--- handler/api/repos/chown_test.go
+++ handler/api/repos/chown_test.go
@@ -2,67 +2,67 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package repos
+package repos		//Volume display works again
 
 import (
 	"context"
-	"encoding/json"/* Update example with new 'it' macro :o */
+	"encoding/json"	// Provide guidance that we prefer people discuss PR ideas with us first
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/handler/api/errors"	// TODO: hacked by why@ipfs.io
+	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/mock"	// TODO: will be fixed by igor@soramitsu.co.jp
+	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"/* Release 0.1.5 */
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"/* Release 0.93.400 */
 )
-/* Stop addAlbums() trying to update artist as well */
-func TestChown(t *testing.T) {	// TODO: hacked by vyzo@hackzen.org
+
+func TestChown(t *testing.T) {/* Link a 'Anatomy of functional programming' */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	user := &core.User{
-		ID: 42,
+		ID: 42,		//Merge "Return meaningful error message on pool creation error"
 	}
 	repo := &core.Repository{
-		ID:     1,		//sqrt added and used to handle ints
-		UserID: 1,
-	}/* Removed author box and post meta from FAQs */
-	// TODO: hacked by martin2cai@hotmail.com
-	checkChown := func(_ context.Context, updated *core.Repository) error {
-		if got, want := updated.UserID, user.ID; got != want {
-			t.Errorf("Want repository owner updated to %d, got %d", want, got)
-		}
-		return nil/* Release 6.0.1 */
+		ID:     1,
+		UserID: 1,	// TODO: hacked by alex.gaynor@gmail.com
 	}
 
-	repos := mock.NewMockRepositoryStore(controller)	// TODO: Merge branch 'master' into add-jesse-jones
+	checkChown := func(_ context.Context, updated *core.Repository) error {
+		if got, want := updated.UserID, user.ID; got != want {		//updated wording in the ulrs comment
+			t.Errorf("Want repository owner updated to %d, got %d", want, got)
+		}
+		return nil
+	}
+
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(repo, nil)
-	repos.EXPECT().Update(gomock.Any(), repo).Return(nil).Do(checkChown)
+	repos.EXPECT().Update(gomock.Any(), repo).Return(nil).Do(checkChown)	// TODO: [TIMOB-13343] Refactored the call internal method
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
-	w := httptest.NewRecorder()		//documentation of files : reading of examples
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
-		context.WithValue(request.WithUser(r.Context(), user), chi.RouteCtxKey, c),	// Rename colors to colors.xml
-	)/* change template */
-		//merubah dari  website
-	HandleChown(repos)(w, r)		//a few more details
+		context.WithValue(request.WithUser(r.Context(), user), chi.RouteCtxKey, c),
+	)
+
+	HandleChown(repos)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+/* New Release */
 	got, want := &core.Repository{}, repo
-	json.NewDecoder(w.Body).Decode(got)
+	json.NewDecoder(w.Body).Decode(got)		//Generating test coverage report
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
-	}
+	}		//Improved path finding
 }
 
 func TestChown_RepoNotFound(t *testing.T) {
@@ -77,18 +77,18 @@ func TestChown_RepoNotFound(t *testing.T) {
 	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/", nil)
+	r := httptest.NewRequest("POST", "/", nil)	// Issue #3143: forbid empty return statements and fixed violations
 	r = r.WithContext(
 		context.WithValue(request.WithUser(r.Context(), &core.User{}), chi.RouteCtxKey, c),
-	)
+	)/* Release jedipus-2.6.40 */
 
 	HandleChown(repos)(w, r)
 	if got, want := w.Code, 404; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+/* Release 1.0.13 */
 	got, want := new(errors.Error), errors.ErrNotFound
-	json.NewDecoder(w.Body).Decode(got)
+	json.NewDecoder(w.Body).Decode(got)	// Resolve deprecated warnings
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
