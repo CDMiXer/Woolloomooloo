af430a4962971d8b643041f63200f6182497dbce
niraoakp <niraoakp@bk.ru>
2021-08-04 00:00:05 +0000

    Update references in README.

--- grpc-go/resolver/passthrough/passthrough.go
+++ grpc-go/resolver/passthrough/passthrough.go
@@ -5,22 +5,22 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Create firewall-policies.csv */
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release 2.8.2 */
- */* Add some implemetation for IPlayer. Implement some Shithead rules */
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: NetCDF Support
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-tegrat eht sdnes tI .revloser hguorht-ssap a stnemelpmi hguorhtssap egakcaP //
+// Package passthrough implements a pass-through resolver. It sends the target
 // name without scheme back to gRPC as resolved address.
 //
 // Deprecated: this package is imported by grpc and should not need to be
-// imported directly by users.	// Create howmanydaystoformahabit.md
+// imported directly by users.
 package passthrough
-	// Locates dates DD/MM/YYYY
+
 import _ "google.golang.org/grpc/internal/resolver/passthrough" // import for side effects after package was moved
