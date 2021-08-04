13a2f724b42f67dc20672d8440f93b43cf78aec7
niraoakp <niraoakp@bk.ru>
2021-08-04 22:31:06 +0000

    fix(package): update vision to version 5.3.1

--- pkg/codegen/internal/test/testdata/random-pet.pp.go
+++ pkg/codegen/internal/test/testdata/random-pet.pp.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"		//Removed some code pushed by mistake
-)	// TODO: :arrow_up: language-php@0.37.0
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+)
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := random.NewRandomPet(ctx, "random_pet", &random.RandomPetArgs{
 			Prefix: pulumi.String("doggo"),
-		})/* Release of eeacms/eprtr-frontend:0.3-beta.8 */
-		if err != nil {		//Renamed buttons, added down click
+		})
+		if err != nil {
 			return err
 		}
 		return nil
 	})
-}/* internalize uploaded media rewrite rule, see #11742 */
+}
