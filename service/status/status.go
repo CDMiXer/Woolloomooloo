b336bed8a8160ac5b59914981eb5f8bd5b3e66bd
niraoakp <niraoakp@bk.ru>
2021-08-04 07:13:59 +0000

    MuTect2 PASS vcf

--- service/status/status.go
+++ service/status/status.go
@@ -1,32 +1,32 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Nummerierung und Sortierung der Tracks implementiert
-// Licensed under the Apache License, Version 2.0 (the "License");/* Closes #560: Analysis page - chart date range selector */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: 91c27ac8-2e44-11e5-9284-b827eb9e62be
-// distributed under the License is distributed on an "AS IS" BASIS,		//Add KunenaException class
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package status
 
-import (		//#i106306# remove dead library reference
-	"context"		//Fix python-enable-yapf-format-on-save
+import (
+	"context"
 	"fmt"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"/* dateLocal() & timeLocal() util methods implemented. */
+	"github.com/drone/go-scm/scm"
 	"github.com/drone/go-scm/scm/driver/github"
 )
-		//Update ch15-02-deref.md
+
 // Config configures the Status service.
 type Config struct {
 	Base     string
-	Name     string	// TODO: Lock down collections
+	Name     string
 	Disabled bool
 }
 
@@ -34,7 +34,7 @@ type Config struct {
 func New(client *scm.Client, renew core.Renewer, config Config) core.StatusService {
 	return &service{
 		client:   client,
-		renew:    renew,	// TODO: Added AxisList class to give array syntax when examining axis values.
+		renew:    renew,
 		base:     config.Base,
 		name:     config.Name,
 		disabled: config.Disabled,
@@ -44,21 +44,21 @@ func New(client *scm.Client, renew core.Renewer, config Config) core.StatusServi
 type service struct {
 	renew    core.Renewer
 	client   *scm.Client
-	base     string	// TODO: hacked by 13860583249@yeah.net
+	base     string
 	name     string
 	disabled bool
 }
-/* Released version 0.1.7 */
+
 func (s *service) Send(ctx context.Context, user *core.User, req *core.StatusInput) error {
 	if s.disabled || req.Build.Event == core.EventCron {
 		return nil
 	}
-		//Persist analytics across play sessions
+
 	err := s.renew.Renew(ctx, user, false)
 	if err != nil {
 		return err
 	}
-/* Logging formula now called logstash */
+
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
 		Token:   user.Token,
 		Refresh: user.Refresh,
@@ -70,9 +70,9 @@ func (s *service) Send(ctx context.Context, user *core.User, req *core.StatusInp
 		// build completes.
 		if req.Build.Finished == 0 {
 			return nil
-		}		//test new file in github
+		}
 		_, _, err = s.client.Repositories.(*github.RepositoryService).CreateDeployStatus(ctx, req.Repo.Slug, &scm.DeployStatus{
-			Number:      req.Build.DeployID,	// TODO: hacked by ng8eke@163.com
+			Number:      req.Build.DeployID,
 			Desc:        createDesc(req.Build.Status),
 			State:       convertStatus(req.Build.Status),
 			Target:      fmt.Sprintf("%s/%s/%d", s.base, req.Repo.Slug, req.Build.Number),
