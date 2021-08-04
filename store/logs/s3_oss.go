bcf98b0e416311896d9a5aedcb7c0d3d2790991a
niraoakp <niraoakp@bk.ru>
2021-08-04 03:45:46 +0000

    added file encoding property and maven assembly

--- store/logs/s3_oss.go
+++ store/logs/s3_oss.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: remove directory, pretty, and random bits from base for nhc98
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Tagging a Release Candidate - v4.0.0-rc3. */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release Checklist > Bugzilla  */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -19,6 +19,6 @@ package logs
 import "github.com/drone/drone/core"
 
 // New returns a zero value LogStore.
-func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {
+func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {		//amberc.js: remove now unneeded init.js code + formatting
 	return nil
 }
