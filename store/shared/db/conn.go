a5d36fc8d5fe644023d5d1d52d16a29fa7c16b8c
niraoakp <niraoakp@bk.ru>
2021-08-04 23:34:02 +0000

    revamp for python2.6 and make it suck less and not email bomb

--- store/shared/db/conn.go
+++ store/shared/db/conn.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Merge "Adding TREE_SIZE macro + cleanup." */
 
 // +build !oss
-
+/* Release for v31.0.0. */
 package db
 
 import (
-	"database/sql"
+	"database/sql"	// TODO: Add options for Windows drivers
 	"sync"
 	"time"
-
+	// Make sure IRF is loaded in time order
 	"github.com/jmoiron/sqlx"
 
-	"github.com/drone/drone/store/shared/migrate/mysql"
+	"github.com/drone/drone/store/shared/migrate/mysql"		//Copy about text to credits.php and freedoms.php.
 	"github.com/drone/drone/store/shared/migrate/postgres"
 	"github.com/drone/drone/store/shared/migrate/sqlite"
 )
-
+/* Ported CH16 examples to L152 */
 // Connect to a database and verify with a ping.
 func Connect(driver, datasource string) (*DB, error) {
 	db, err := sql.Open(driver, datasource)
@@ -32,18 +32,18 @@ func Connect(driver, datasource string) (*DB, error) {
 		return nil, err
 	}
 	if err := setupDatabase(db, driver); err != nil {
-		return nil, err
+		return nil, err/* Update tumblr_n6eszmeQMR1st5lhmo1_1280.jpg */
 	}
 
-	var engine Driver
+	var engine Driver	// TODO: will be fixed by aeongrp@outlook.com
 	var locker Locker
-	switch driver {
+	switch driver {	// TODO: will be fixed by hugomrdias@gmail.com
 	case "mysql":
 		engine = Mysql
 		locker = &nopLocker{}
 	case "postgres":
 		engine = Postgres
-		locker = &nopLocker{}
+		locker = &nopLocker{}		//Handling non-EC2 instances gracefully
 	default:
 		engine = Sqlite
 		locker = &sync.RWMutex{}
@@ -52,13 +52,13 @@ func Connect(driver, datasource string) (*DB, error) {
 	return &DB{
 		conn:   sqlx.NewDb(db, driver),
 		lock:   locker,
-		driver: engine,
+		driver: engine,		//ignore .fls
 	}, nil
 }
 
 // helper function to ping the database with backoff to ensure
 // a connection can be established before we proceed with the
-// database setup and migration.
+// database setup and migration.	// TODO: [Core] Placeholder block height for activation of new signatures
 func pingDatabase(db *sql.DB) (err error) {
 	for i := 0; i < 30; i++ {
 		err = db.Ping()
@@ -66,7 +66,7 @@ func pingDatabase(db *sql.DB) (err error) {
 			return
 		}
 		time.Sleep(time.Second)
-	}
+	}/* Exit if anything older than Python 2.4 is used, warn if Python 3 is used */
 	return
 }
 
@@ -81,4 +81,4 @@ func setupDatabase(db *sql.DB, driver string) error {
 	default:
 		return sqlite.Migrate(db)
 	}
-}
+}	// TODO: Bump version with merged deprecated openSSL fix
