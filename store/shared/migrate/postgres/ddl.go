eeda6e6ab592cceea70766c61304a3229f25d263
niraoakp <niraoakp@bk.ru>
2021-08-04 15:27:42 +0000

    Allow passing in tuples as file ids in various places.

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Finish tweaking decode_name */
+// +build !oss
 
 package postgres
-/* Release 1.1. Requires Anti Brute Force 1.4.6. */
+
 //go:generate togo ddl -package postgres -dialect postgres
