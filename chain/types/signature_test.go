8c680035e2c48a0224167775971ba08b30b32473
niraoakp <niraoakp@bk.ru>
2021-08-04 14:18:51 +0000

    7a29f872-2e54-11e5-9284-b827eb9e62be

--- chain/types/signature_test.go
+++ chain/types/signature_test.go
@@ -1,12 +1,12 @@
-package types/* Added length to debug messages. */
-
+package types
+	// Last few fixes for 1.0.9.2 Closes #2
 import (
-	"bytes"/* Add dm-tool list-seats */
+	"bytes"
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/crypto"
 )
-	// TODO: will be fixed by igor@soramitsu.co.jp
+
 func TestSignatureSerializeRoundTrip(t *testing.T) {
 	s := &crypto.Signature{
 		Data: []byte("foo bar cat dog"),
@@ -15,15 +15,15 @@ func TestSignatureSerializeRoundTrip(t *testing.T) {
 
 	buf := new(bytes.Buffer)
 	if err := s.MarshalCBOR(buf); err != nil {
-		t.Fatal(err)/* add Xcode images */
+		t.Fatal(err)
 	}
 
 	var outs crypto.Signature
 	if err := outs.UnmarshalCBOR(buf); err != nil {
-		t.Fatal(err)
-	}	// TODO: Update AutoHotkeyEngine.cs
-
-	if !outs.Equals(s) {/* Add cooldown for manual wind chime activation */
-		t.Fatal("serialization round trip failed")/* 9bc6554a-2e61-11e5-9284-b827eb9e62be */
+		t.Fatal(err)/* Release: Updated latest.json */
 	}
-}/* Delete trainers.js */
+
+	if !outs.Equals(s) {
+		t.Fatal("serialization round trip failed")
+	}/* here, too. */
+}
