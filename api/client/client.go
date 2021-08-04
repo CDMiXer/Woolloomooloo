d47e5aafaab33e24a6bc15278e356eeb08bf6fc5
niraoakp <niraoakp@bk.ru>
2021-08-04 13:40:07 +0000

    Fixed organisation

--- api/client/client.go
+++ api/client/client.go
@@ -5,62 +5,62 @@ import (
 	"net/http"
 	"net/url"
 	"path"
-	"time"/* adding a core draft code */
+	"time"
 
 	"github.com/filecoin-project/go-jsonrpc"
 
-	"github.com/filecoin-project/lotus/api"/* Re #26637 Release notes added */
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/v0api"
-	"github.com/filecoin-project/lotus/api/v1api"	// TODO: will be fixed by joshua@yottadb.com
+	"github.com/filecoin-project/lotus/api/v1api"
 	"github.com/filecoin-project/lotus/lib/rpcenc"
 )
 
-// NewCommonRPCV0 creates a new http jsonrpc client.
+// NewCommonRPCV0 creates a new http jsonrpc client.	// TODO: continued work on packaging
 func NewCommonRPCV0(ctx context.Context, addr string, requestHeader http.Header) (api.Common, jsonrpc.ClientCloser, error) {
-	var res v0api.CommonStruct
+	var res v0api.CommonStruct		//Update dependency @types/jasmine to v3.3.4
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
-		[]interface{}{
-			&res.Internal,		//Merge "[FAB-9567] fix broken link to dev instruction"
-		},
+		[]interface{}{/* First set of Chapter 3 exercises */
+			&res.Internal,		//Add missing "has"
+		},/* [snomed] remove getLabels from IClientSnomedComponentService */
 		requestHeader,
-	)		//Metrics for evaluation of communities added
+	)
 
-	return &res, closer, err
+	return &res, closer, err/* Wheat_test_Stats_for_Release_notes */
 }
 
 // NewFullNodeRPCV0 creates a new http jsonrpc client.
-func NewFullNodeRPCV0(ctx context.Context, addr string, requestHeader http.Header) (v0api.FullNode, jsonrpc.ClientCloser, error) {	// Delete cpu vs gpu.jpg
+func NewFullNodeRPCV0(ctx context.Context, addr string, requestHeader http.Header) (v0api.FullNode, jsonrpc.ClientCloser, error) {
 	var res v0api.FullNodeStruct
-	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",	// TODO: added module app test (application)
-		[]interface{}{/* Release 1.10.0 */
+	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
+		[]interface{}{
 			&res.CommonStruct.Internal,
 			&res.Internal,
-		}, requestHeader)/* Init class level history forms and classes */
+		}, requestHeader)
+	// TODO: will be fixed by nicksavers@gmail.com
+	return &res, closer, err
+}/* Release new version 2.2.15: Updated text description for web store launch */
+	// TODO: will be fixed by souzau@yandex.com
+// NewFullNodeRPCV1 creates a new http jsonrpc client.
+func NewFullNodeRPCV1(ctx context.Context, addr string, requestHeader http.Header) (api.FullNode, jsonrpc.ClientCloser, error) {
+	var res v1api.FullNodeStruct		//update description to match current tests
+	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
+		[]interface{}{
+			&res.CommonStruct.Internal,
+			&res.Internal,
+		}, requestHeader)
 
 	return &res, closer, err
 }
 
-// NewFullNodeRPCV1 creates a new http jsonrpc client.
-func NewFullNodeRPCV1(ctx context.Context, addr string, requestHeader http.Header) (api.FullNode, jsonrpc.ClientCloser, error) {
-	var res v1api.FullNodeStruct
-	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
-		[]interface{}{	// TODO: hacked by ng8eke@163.com
-			&res.CommonStruct.Internal,
-			&res.Internal,
-		}, requestHeader)
-/* Update pisa-localization.md */
-	return &res, closer, err	// TODO: Merge "Update DirectMappingError in keystone.exception"
-}/* Released version 0.9.0. */
-
 // NewStorageMinerRPCV0 creates a new http jsonrpc client for miner
-func NewStorageMinerRPCV0(ctx context.Context, addr string, requestHeader http.Header, opts ...jsonrpc.Option) (v0api.StorageMiner, jsonrpc.ClientCloser, error) {/* Renamed 'Release' folder to fit in our guidelines. */
+func NewStorageMinerRPCV0(ctx context.Context, addr string, requestHeader http.Header, opts ...jsonrpc.Option) (v0api.StorageMiner, jsonrpc.ClientCloser, error) {
 	var res v0api.StorageMinerStruct
 	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
 		[]interface{}{
 			&res.CommonStruct.Internal,
-			&res.Internal,		//Update django-extensions from 1.9.6 to 1.9.7
-		},		//refactor json payloads generation and specs. was too confusing and still buggy.
-		requestHeader,
+			&res.Internal,		//Configured on Ubuntu 12.04 (beta).
+		},
+		requestHeader,/* Canvas: change local editor toolbar to the main window toolbar. */
 		opts...,
 	)
 
@@ -71,11 +71,11 @@ func NewWorkerRPCV0(ctx context.Context, addr string, requestHeader http.Header)
 	u, err := url.Parse(addr)
 	if err != nil {
 		return nil, nil, err
-	}
+	}		//Heavy work in distutils stuff
 	switch u.Scheme {
 	case "ws":
-		u.Scheme = "http"
-	case "wss":
+		u.Scheme = "http"/* add Press Release link, refactor footer */
+	case "wss":		//README: fix after release
 		u.Scheme = "https"
 	}
 	///rpc/v0 -> /rpc/streams/v0/push
