f3724b3480494fd24704282ddd290c456efc3abd
niraoakp <niraoakp@bk.ru>
2021-08-04 05:57:36 +0000

    [IMP] mrp: improvement

--- handler/api/repos/builds/logs/delete.go
+++ handler/api/repos/builds/logs/delete.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License./* android: update APM tokenization */
+// You may obtain a copy of the License at	// TODO: Delete burp suite.z46
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Merge branch 'master' of git@github.com:MediaYouCanFeel/Azzenda.git
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,38 +12,38 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package logs
+package logs/* 6f25d5e2-2e43-11e5-9284-b827eb9e62be */
 
-import (
+import (		//"all up"-button
 	"net/http"
 	"strconv"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"
-)
+	"github.com/go-chi/chi"/* Release 5.1.1 */
+)/* Release version: 1.0.27 */
 
 // HandleDelete returns an http.HandlerFunc that processes http
-// requests to delete the logs.
-func HandleDelete(
+// requests to delete the logs.	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+func HandleDelete(		//f7c7117e-2e4b-11e5-9284-b827eb9e62be
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
-	steps core.StepStore,
-	logs core.LogStore,
+	steps core.StepStore,/* Merge branch 'release/2.15.0-Release' into develop */
+	logs core.LogStore,	// refactor(app): use almin instead of internal framwork (#9)
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (		//copyright and email updates.
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
-		if err != nil {
+		if err != nil {	// TODO: hacked by alan.shaw@protocol.ai
 			render.BadRequest(w, err)
 			return
 		}
-		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
+		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))/* Release notes and version bump 5.2.8 */
 		if err != nil {
 			render.BadRequest(w, err)
 			return
@@ -58,9 +58,9 @@ func HandleDelete(
 			render.NotFound(w, err)
 			return
 		}
-		build, err := builds.FindNumber(r.Context(), repo.ID, number)
+		build, err := builds.FindNumber(r.Context(), repo.ID, number)		//Using platform independent absolute paths everywhere
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)/* Merge "wlan: Release 3.2.3.108" */
 			return
 		}
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
