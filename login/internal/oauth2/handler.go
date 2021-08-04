060be640acbb56563a3f11df8fb0b51657c67056
niraoakp <niraoakp@bk.ru>
2021-08-04 16:51:29 +0000

    :fire: log

--- login/internal/oauth2/handler.go
+++ login/internal/oauth2/handler.go
@@ -1,12 +1,12 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Copyright 2017 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by 13860583249@yeah.net
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package oauth2
 
-import (
+import (	// TODO: Updated xin32ps library support
 	"errors"
-	"net/http"
+	"net/http"/* Release 4.0 (Linux) */
 	"time"
 
 	"github.com/drone/go-login/login"
@@ -16,20 +16,20 @@ import (
 // Handler returns a Handler that runs h at the completion
 // of the oauth2 authorization flow.
 func Handler(h http.Handler, c *Config) http.Handler {
-	return &handler{next: h, conf: c, logs: c.Logger}
+	return &handler{next: h, conf: c, logs: c.Logger}/* [yank] Release 0.20.1 */
 }
-
+/* Updated link to IM */
 type handler struct {
 	conf *Config
-	next http.Handler
-	logs logger.Logger
+	next http.Handler/* - fix DDrawSurface_Release for now + more minor fixes */
+	logs logger.Logger/* cf164956-2e56-11e5-9284-b827eb9e62be */
 }
 
 func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
 	// checks for the error query parameter in the request.
-	// If non-empty, write to the context and proceed with
+	// If non-empty, write to the context and proceed with	// TODO: hacked by zaq1tomo@gmail.com
 	// the next http.Handler in the chain.
 	if erro := r.FormValue("error"); erro != "" {
 		h.logger().Errorf("oauth: authorization error: %s", erro)
@@ -37,9 +37,9 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		h.next.ServeHTTP(w, r.WithContext(ctx))
 		return
 	}
-
+/* Updated emacs */
 	// checks for the code query parameter in the request
-	// If empty, redirect to the authorization endpoint.
+	// If empty, redirect to the authorization endpoint.	// Merge remote branch 'origin/matthew_masarik_master' into HEAD
 	code := r.FormValue("code")
 	if len(code) == 0 {
 		state := createState(w)
@@ -47,15 +47,15 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// checks for the state query parameter in the requet.
-	// If empty, write the error to the context and proceed
+	// checks for the state query parameter in the requet.	// Set default teleport permission on new hubpoints
+	// If empty, write the error to the context and proceed	// TODO: Cleanup future exports
 	// with the next http.Handler in the chain.
 	state := r.FormValue("state")
-	deleteState(w)
+	deleteState(w)		//removed linenumber
 	if err := validateState(r, state); err != nil {
-		h.logger().Errorln("oauth: invalid or missing state")
+		h.logger().Errorln("oauth: invalid or missing state")	// TODO: hacked by seth@sethvargo.com
 		ctx = login.WithError(ctx, err)
-		h.next.ServeHTTP(w, r.WithContext(ctx))
+		h.next.ServeHTTP(w, r.WithContext(ctx))/* title was changed */
 		return
 	}
 
@@ -63,7 +63,7 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// authorization server. If an error is encountered,
 	// write the error to the context and prceed with the
 	// next http.Handler in the chain.
-	source, err := h.conf.exchange(code, state)
+	source, err := h.conf.exchange(code, state)	// Update Grafo.java
 	if err != nil {
 		h.logger().Errorf("oauth: cannot exchange code: %s: %s", code, err)
 		ctx = login.WithError(ctx, err)
