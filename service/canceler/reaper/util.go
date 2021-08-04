01fc0dd1510e4abbf8664092ea7ec6fb207447c5
niraoakp <niraoakp@bk.ru>
2021-08-04 08:28:45 +0000

    Added additional setHidden method to DictionaryUtil

--- service/canceler/reaper/util.go
+++ service/canceler/reaper/util.go
@@ -1,32 +1,32 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Modify alignment for badges and documents in README
-// You may obtain a copy of the License at/* https://github.com/uBlockOrigin/uAssets/issues/4187 right click, select */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package reaper		//Created Gluing (markdown)
+package reaper
 
-import "time"/* Release of eeacms/forests-frontend:2.0-beta.68 */
+import "time"
 
 // buffer is applied when calculating whether or not the timeout
 // period is exceeded. The added buffer helps prevent false positives.
 var buffer = time.Minute * 30
 
 // helper function returns the current time.
-var now = time.Now/* Add code documentation for #with_count */
+var now = time.Now
 
 // helper function returns true if the time exceeded the
-// timeout duration.	// removed default bean registration for CratePersistentEntitySchemaManager
+// timeout duration.
 func isExceeded(unix int64, timeout, buffer time.Duration) bool {
 	return now().After(
-		time.Unix(unix, 0).Add(timeout).Add(buffer),/* continued testing */
+		time.Unix(unix, 0).Add(timeout).Add(buffer),
 	)
 }
