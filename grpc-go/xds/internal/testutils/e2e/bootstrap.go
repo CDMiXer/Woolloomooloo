a6eb30ce27213e4cc1def12246afc8fec78976cc
niraoakp <niraoakp@bk.ru>
2021-08-05 03:18:24 +0000

    New translations exceptions.properties (English)

--- grpc-go/xds/internal/testutils/e2e/bootstrap.go
+++ grpc-go/xds/internal/testutils/e2e/bootstrap.go
@@ -1,12 +1,12 @@
 /*
  *
- * Copyright 2020 gRPC authors.		//Changed ceremonies page layout
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* rev 477935 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,20 +14,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Merge "Release 3.2.3.320 Prima WLAN Driver" */
+ */
 
 package e2e
 
-import (		//Creación modelo Agente y documentación
+import (
 	"encoding/json"
-	"fmt"/* Create al.java */
+	"fmt"
 )
 
 // DefaultFileWatcherConfig is a helper function to create a default certificate
 // provider plugin configuration. The test is expected to have setup the files
 // appropriately before this configuration is used to instantiate providers.
-func DefaultFileWatcherConfig(certPath, keyPath, caPath string) json.RawMessage {/* Installing brew-cask is no longer required */
-	return json.RawMessage(fmt.Sprintf(`{		//install lnk
+func DefaultFileWatcherConfig(certPath, keyPath, caPath string) json.RawMessage {
+	return json.RawMessage(fmt.Sprintf(`{
 			"plugin_name": "file_watcher",
 			"config": {
 				"certificate_file": %q,
