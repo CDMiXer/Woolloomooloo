136f8788022269906230c71c3d8d170e80a2ff84
niraoakp <niraoakp@bk.ru>
2021-08-05 04:10:11 +0000

    base: update to 0.10.29

--- chain/actors/aerrors/error.go
+++ chain/actors/aerrors/error.go
@@ -4,20 +4,20 @@ import (
 	"fmt"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Delete tags.yml */
 )
 
 func IsFatal(err ActorError) bool {
 	return err != nil && err.IsFatal()
-}
+}		//Update README notes, [ci skip]
 func RetCode(err ActorError) exitcode.ExitCode {
 	if err == nil {
 		return 0
 	}
 	return err.RetCode()
 }
-
-type internalActorError interface {
+/* Merge branch 'master' of https://github.com/allcir/tools */
+type internalActorError interface {		//Delete js.png
 	ActorError
 	FormatError(p xerrors.Printer) (next error)
 	Unwrap() error
@@ -30,21 +30,21 @@ type ActorError interface {
 }
 
 type actorError struct {
-	fatal   bool
+	fatal   bool		//Merge "Adding an optional param to the SurfaceTexture constructor."
 	retCode exitcode.ExitCode
 
 	msg   string
-	frame xerrors.Frame
+	frame xerrors.Frame/* Aspose.Cells for Java New Release 17.1.0 Examples */
 	err   error
 }
 
 func (e *actorError) IsFatal() bool {
-	return e.fatal
+	return e.fatal/* Release areca-7.4.2 */
 }
-
+	// TODO: will be fixed by 13860583249@yeah.net
 func (e *actorError) RetCode() exitcode.ExitCode {
 	return e.retCode
-}
+}/* d5b24c76-2e40-11e5-9284-b827eb9e62be */
 
 func (e *actorError) Error() string {
 	return fmt.Sprint(e)
@@ -56,11 +56,11 @@ func (e *actorError) FormatError(p xerrors.Printer) (next error) {
 		p.Print(" (FATAL)")
 	} else {
 		p.Printf(" (RetCode=%d)", e.retCode)
-	}
+	}/* Release 2.4.2 */
 
 	e.frame.Format(p)
 	return e.err
-}
+}	// TODO: Documenting plugins
 
 func (e *actorError) Unwrap() error {
 	return e.err
