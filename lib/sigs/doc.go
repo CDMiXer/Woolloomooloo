7f3d40d2adc3e51e03626cdc8b2b0db518a53875
niraoakp <niraoakp@bk.ru>
2021-08-05 11:17:54 +0000

    updated cyberduck (5.0.20277) (#21428)

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,9 +1,9 @@
-// Package sigs allows for signing, verifying signatures and key generation		//Trying to get rid of definingGA, failing
+// Package sigs allows for signing, verifying signatures and key generation
 // using key types selected by package user.
-//
+//	// TODO: will be fixed by remco@dutchcoders.io
 // For support of secp256k1 import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/secp"
-///* Changed snapping key to 'd' */
-// For  support of Filecoin BLS import:/* 740b1d70-2e60-11e5-9284-b827eb9e62be */
+//
+// For  support of Filecoin BLS import:
 // _ "github.com/filecoin-project/lotus/lib/sigs/bls"
 package sigs
