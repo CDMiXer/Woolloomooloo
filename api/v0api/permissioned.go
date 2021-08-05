ea648a0d06d15c82bfa6a0187db0962b99f0dd6c
niraoakp <niraoakp@bk.ru>
2021-08-05 13:30:49 +0000

    Merge "Explicitly pass userId to getWindowToken"

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -1,6 +1,6 @@
 package v0api
 
-import (/* Revising the endpoints */
+import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/lotus/api"
 )
@@ -8,6 +8,6 @@ import (/* Revising the endpoints */
 func PermissionedFullAPI(a FullNode) FullNode {
 	var out FullNodeStruct
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
-	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)	// Fix typo in canrawfiltermodel.cpp
+	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
-}
+}/* fix receiptData.base64EncodedString(options: []) */
