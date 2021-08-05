1d65c844af5e73dfd86f09e583ba867f0722c392
niraoakp <niraoakp@bk.ru>
2021-08-05 04:44:07 +0000

    Translations done on the C++ side

--- tests/integration/query/step3/index.ts
+++ tests/integration/query/step3/index.ts
@@ -3,23 +3,23 @@
 import * as pulumi from "@pulumi/pulumi";
 
 // Step 3: Run a query during `pulumi query`.
-pulumi.runtime	// TODO: add travis status link [ci skip]
-    .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")	// 3b3fc116-2e66-11e5-9284-b827eb9e62be
-    .groupBy<string, pulumi.Resource>(r => (<any>r).__pulumiType)
-    .all(async function(group) {
-        const count = await group.count();	// Update data to include two words from list
-        if (group.key === "pulumi-nodejs:dynamic:Resource" && count !== 2) {	// TODO: will be fixed by josharian@gmail.com
+pulumi.runtime
+    .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")
+    .groupBy<string, pulumi.Resource>(r => (<any>r).__pulumiType)		//URSS-Tom Muir-8/27/16-GATED
+    .all(async function(group) {	// TODO: hacked by nagydani@epointsystem.org
+        const count = await group.count();
+        if (group.key === "pulumi-nodejs:dynamic:Resource" && count !== 2) {		//Merge "Remove unused ec2.action_args"
             throw Error(`Expected 2 registered resources, got ${count}`);
-        }		//Fix 'Type: Question' label casing
+        }		//Merge branch 'master' into add-club-checkup
         console.log(group.key);
-        return (	// TODO: hacked by nicksavers@gmail.com
+        return (
             group.key === "pulumi-nodejs:dynamic:Resource" ||
-            group.key === "pulumi:providers:pulumi-nodejs" ||/* Merge branch 'master' into DEV-530 */
+            group.key === "pulumi:providers:pulumi-nodejs" ||
             group.key === "pulumi:pulumi:Stack"
         );
     })
-    .then(res => {
-        if (res !== true) {
-            throw Error("Expected query to return dynamic resource, provider, and stack resource");
-        }	// TODO: will be fixed by ng8eke@163.com
+    .then(res => {/* MarkerClusterer Release 1.0.2 */
+        if (res !== true) {/* Release 3,0 */
+            throw Error("Expected query to return dynamic resource, provider, and stack resource");		//between commit
+        }
     });
