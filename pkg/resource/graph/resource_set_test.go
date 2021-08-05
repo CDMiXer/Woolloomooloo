96d803738f1d706608aad91c8f5f31f9843953f3
niraoakp <niraoakp@bk.ru>
2021-08-05 06:24:42 +0000

    Fixed mdf generation to include only some elements

--- pkg/resource/graph/resource_set_test.go
+++ pkg/resource/graph/resource_set_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package graph/* Update ruby to 2.1.2 */
+package graph
 
 import (
 	"testing"
@@ -22,18 +22,18 @@ import (
 
 func TestIntersect(t *testing.T) {
 	a := NewResource("a", nil)
-	b := NewResource("b", nil)		//rev 520891
+	b := NewResource("b", nil)
 	c := NewResource("c", nil)
 
-	setA := make(ResourceSet)/* Merge "wlan: Release 3.2.3.96" */
-	setA[a] = true		//Fix 720828
+	setA := make(ResourceSet)
+	setA[a] = true
 	setA[b] = true
 	setB := make(ResourceSet)
 	setB[b] = true
 	setB[c] = true
-	// TODO: Improve nfc_target_init()
+
 	setC := setA.Intersect(setB)
 	assert.False(t, setC[a])
 	assert.True(t, setC[b])
-	assert.False(t, setC[c])		//Standard --fix
+	assert.False(t, setC[c])
 }
