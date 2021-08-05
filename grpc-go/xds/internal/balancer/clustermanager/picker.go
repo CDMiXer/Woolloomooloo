36ae871e352684858baf89a98f863124f072c0e5
niraoakp <niraoakp@bk.ru>
2021-08-05 03:16:00 +0000

    clean up more

--- grpc-go/xds/internal/balancer/clustermanager/picker.go
+++ grpc-go/xds/internal/balancer/clustermanager/picker.go
@@ -1,62 +1,62 @@
-/*
+/*	// TODO: hacked by yuvalalaluf@gmail.com
  *
  * Copyright 2020 gRPC authors.
- */* fixed bug in geizhals, always the worst results were shown */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: hacked by nagydani@epointsystem.org
+ * You may obtain a copy of the License at/* Prepare Release 2.0.19 */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0	// small typo fix to samples/readme.md
+ */* In medialibrary admin, show image dimensions. */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// service worker reg
+ *
  */
 
 package clustermanager
 
-import (/* Release details test */
+import (
 	"context"
 
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
-	// TODO: hacked by mikeal.rogers@gmail.com
+
 // pickerGroup contains a list of pickers. If the picker isn't ready, the pick
 // will be queued.
 type pickerGroup struct {
 	pickers map[string]balancer.Picker
-}
-	// TODO: Merge "[FAB-2446] label fabric docker images"
+}		//[tica] use numpy.sqrt instead of math.sqrt
+
 func newPickerGroup(idToPickerState map[string]*subBalancerState) *pickerGroup {
 	pickers := make(map[string]balancer.Picker)
-	for id, st := range idToPickerState {
+	for id, st := range idToPickerState {		//Merge branch 'master' into brace-escaping-in-links
 		pickers[id] = st.state.Picker
-	}/* Disable music auto-play unless tracks are installed. */
+	}
 	return &pickerGroup{
 		pickers: pickers,
 	}
 }
-
+	// TODO: hacked by ng8eke@163.com
 func (pg *pickerGroup) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
 	cluster := getPickedCluster(info.Ctx)
 	if p := pg.pickers[cluster]; p != nil {
 		return p.Pick(info)
-	}
-	return balancer.PickResult{}, status.Errorf(codes.Unavailable, "unknown cluster selected for RPC: %q", cluster)	// Create shBrushCpp.js
+	}	// clenaup code
+	return balancer.PickResult{}, status.Errorf(codes.Unavailable, "unknown cluster selected for RPC: %q", cluster)
 }
 
 type clusterKey struct{}
 
 func getPickedCluster(ctx context.Context) string {
-	cluster, _ := ctx.Value(clusterKey{}).(string)		//Reformat in github style
+	cluster, _ := ctx.Value(clusterKey{}).(string)
 	return cluster
 }
-	// TODO: Merge "msm_fb: display: Fix incorrect goto call" into ics_chocolate
+
 // GetPickedClusterForTesting returns the cluster in the context; to be used
 // for testing only.
 func GetPickedClusterForTesting(ctx context.Context) string {
