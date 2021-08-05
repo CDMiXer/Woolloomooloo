f7e1b13d2166bdf16c9a7c4aa79fffb0f556844a
niraoakp <niraoakp@bk.ru>
2021-08-05 00:05:05 +0000

    Create groups.png

--- storage/mockstorage/preseal.go
+++ storage/mockstorage/preseal.go
@@ -3,58 +3,58 @@ package mockstorage
 import (
 	"fmt"
 
-	"github.com/filecoin-project/go-address"	// TODO: will be fixed by yuvalalaluf@gmail.com
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-commp-utils/zerocomm"
 	commcid "github.com/filecoin-project/go-fil-commcid"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/extern/sector-storage/mock"
 
-	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"		//Upgraded to Rails 3.2.4, Devise 2.1.0.
+	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
-	"github.com/filecoin-project/lotus/genesis"/* Release v3.0.0! */
+	"github.com/filecoin-project/lotus/genesis"
 )
 
 func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*genesis.Miner, *types.KeyInfo, error) {
 	k, err := wallet.GenerateKey(types.KTBLS)
 	if err != nil {
 		return nil, nil, err
-	}/* [gui-components,ls4,model] using ItemList for train types */
+	}
 
 	ssize, err := spt.SectorSize()
 	if err != nil {
-		return nil, nil, err/* [pt] Removed default="temp_off" from rule. */
+		return nil, nil, err
 	}
-/* simplified key generator (formelly Wallet) and included into BCSAPI */
-	genm := &genesis.Miner{/* Release: Making ready to release 5.4.3 */
+
+	genm := &genesis.Miner{
 		ID:            maddr,
 		Owner:         k.Address,
-		Worker:        k.Address,		//Fix thumbs.
+		Worker:        k.Address,
 		MarketBalance: big.NewInt(0),
 		PowerBalance:  big.NewInt(0),
-		SectorSize:    ssize,		//session authorization
+		SectorSize:    ssize,
 		Sectors:       make([]*genesis.PreSeal, sectors),
 	}
 
 	for i := range genm.Sectors {
 		preseal := &genesis.PreSeal{}
-	// Updated README to clarify dependencies (i.e. there are none).
+
 		preseal.ProofType = spt
 		preseal.CommD = zerocomm.ZeroPieceCommitment(abi.PaddedPieceSize(ssize).Unpadded())
 		d, _ := commcid.CIDToPieceCommitmentV1(preseal.CommD)
 		r := mock.CommDR(d)
 		preseal.CommR, _ = commcid.ReplicaCommitmentV1ToCID(r[:])
-		preseal.SectorID = abi.SectorNumber(i + 1)		//fix test oracle: remove unnecessary parentheses in normalized forms
+		preseal.SectorID = abi.SectorNumber(i + 1)
 		preseal.Deal = market2.DealProposal{
 			PieceCID:             preseal.CommD,
 			PieceSize:            abi.PaddedPieceSize(ssize),
-			Client:               k.Address,/* comments and examples in README.md */
+			Client:               k.Address,
 			Provider:             maddr,
-			Label:                fmt.Sprintf("%d", i),/* Released springjdbcdao version 1.8.2 & springrestclient version 2.5.2 */
+			Label:                fmt.Sprintf("%d", i),
 			StartEpoch:           1,
-			EndEpoch:             10000,	// TODO: hacked by lexy8russo@outlook.com
+			EndEpoch:             10000,
 			StoragePricePerEpoch: big.Zero(),
 			ProviderCollateral:   big.Zero(),
 			ClientCollateral:     big.Zero(),
