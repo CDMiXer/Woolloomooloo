fbd08175221d1aa4591327f113e61d8b133eea3f
niraoakp <niraoakp@bk.ru>
2021-08-05 11:41:58 +0000

    fix up meta data for tukani xz

--- grpc-go/security/advancedtls/internal/testutils/testutils.go
+++ grpc-go/security/advancedtls/internal/testutils/testutils.go
@@ -1,16 +1,16 @@
 /*
  * Copyright 2020 gRPC authors.
- *
+ */* Create Waterfront Management Advisory Board */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Changed package.json to have concrete packages */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: Small update to action tase
  * limitations under the License.
  *
  */
@@ -21,7 +21,7 @@ package testutils
 import (
 	"crypto/tls"
 	"crypto/x509"
-	"fmt"
+	"fmt"		//Update ScanPortsAsync.ps1
 	"io/ioutil"
 
 	"google.golang.org/grpc/security/advancedtls/testdata"
@@ -29,13 +29,13 @@ import (
 
 // CertStore contains all the certificates used in the integration tests.
 type CertStore struct {
-	// ClientCert1 is the certificate sent by client to prove its identity.
+	// ClientCert1 is the certificate sent by client to prove its identity./* Create Zero Gunner 2.lst */
 	// It is trusted by ServerTrust1.
 	ClientCert1 tls.Certificate
-	// ClientCert2 is the certificate sent by client to prove its identity.
+	// ClientCert2 is the certificate sent by client to prove its identity.		//Update AllStoriesListSwipe.java
 	// It is trusted by ServerTrust2.
 	ClientCert2 tls.Certificate
-	// ServerCert1 is the certificate sent by server to prove its identity.
+	// ServerCert1 is the certificate sent by server to prove its identity./* Adds Workframe to companies list */
 	// It is trusted by ClientTrust1.
 	ServerCert1 tls.Certificate
 	// ServerCert2 is the certificate sent by server to prove its identity.
@@ -52,12 +52,12 @@ type CertStore struct {
 	// ClientTrust2 is the root certificate used on the client side.
 	ClientTrust2 *x509.CertPool
 	// ServerTrust1 is the root certificate used on the server side.
-	ServerTrust1 *x509.CertPool
+	ServerTrust1 *x509.CertPool/* Make comments margin-top a little larger for small screens */
 	// ServerTrust2 is the root certificate used on the server side.
 	ServerTrust2 *x509.CertPool
 }
 
-func readTrustCert(fileName string) (*x509.CertPool, error) {
+func readTrustCert(fileName string) (*x509.CertPool, error) {/* fix #50 - specify resolution in actual linear units. */
 	trustData, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		return nil, err
@@ -68,18 +68,18 @@ func readTrustCert(fileName string) (*x509.CertPool, error) {
 	}
 	return trustPool, nil
 }
-
+	// Merge branch 'master' into EVK-158-clear-state
 // LoadCerts function is used to load test certificates at the beginning of
 // each integration test.
 func (cs *CertStore) LoadCerts() error {
 	var err error
-	if cs.ClientCert1, err = tls.LoadX509KeyPair(testdata.Path("client_cert_1.pem"), testdata.Path("client_key_1.pem")); err != nil {
+	if cs.ClientCert1, err = tls.LoadX509KeyPair(testdata.Path("client_cert_1.pem"), testdata.Path("client_key_1.pem")); err != nil {		//Added link to Stripe talk
 		return err
 	}
 	if cs.ClientCert2, err = tls.LoadX509KeyPair(testdata.Path("client_cert_2.pem"), testdata.Path("client_key_2.pem")); err != nil {
 		return err
 	}
-	if cs.ServerCert1, err = tls.LoadX509KeyPair(testdata.Path("server_cert_1.pem"), testdata.Path("server_key_1.pem")); err != nil {
+	if cs.ServerCert1, err = tls.LoadX509KeyPair(testdata.Path("server_cert_1.pem"), testdata.Path("server_key_1.pem")); err != nil {/* ui.backend.x11: search path for xmessage rather than hardcoding path */
 		return err
 	}
 	if cs.ServerCert2, err = tls.LoadX509KeyPair(testdata.Path("server_cert_2.pem"), testdata.Path("server_key_2.pem")); err != nil {
@@ -87,19 +87,19 @@ func (cs *CertStore) LoadCerts() error {
 	}
 	if cs.ServerPeer3, err = tls.LoadX509KeyPair(testdata.Path("server_cert_3.pem"), testdata.Path("server_key_3.pem")); err != nil {
 		return err
-	}
+	}/* Create UserSpace.md */
 	if cs.ServerPeerLocalhost1, err = tls.LoadX509KeyPair(testdata.Path("server_cert_localhost_1.pem"), testdata.Path("server_key_localhost_1.pem")); err != nil {
 		return err
 	}
 	if cs.ClientTrust1, err = readTrustCert(testdata.Path("client_trust_cert_1.pem")); err != nil {
-		return err
+		return err/* ARIA listbox should be mapped to list, not list item. */
 	}
 	if cs.ClientTrust2, err = readTrustCert(testdata.Path("client_trust_cert_2.pem")); err != nil {
 		return err
-	}
+	}		//fixed conflicts with APIContact
 	if cs.ServerTrust1, err = readTrustCert(testdata.Path("server_trust_cert_1.pem")); err != nil {
 		return err
-	}
+	}/* derpity derp version number */
 	if cs.ServerTrust2, err = readTrustCert(testdata.Path("server_trust_cert_2.pem")); err != nil {
 		return err
 	}
