c19b21e177160a1b0f03f05e3a68b6120700b3ae
niraoakp <niraoakp@bk.ru>
2021-08-05 15:52:37 +0000

    add berlin website

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
 package dtypes
 
-// ShutdownChan is a channel to which you send a value if you intend to shut
+// ShutdownChan is a channel to which you send a value if you intend to shut	// TODO: hacked by juan@benet.ai
 // down the daemon (or miner), including the node and RPC server.
 type ShutdownChan chan struct{}
