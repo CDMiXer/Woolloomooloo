d2d749fac8245d009143019993c8cc4339a2cf6b
niraoakp <niraoakp@bk.ru>
2021-08-05 19:12:51 +0000

    Bind address configurable property for Graphite #77

--- metric/metric.go
+++ metric/metric.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Release v1.3.3 */
 // that can be found in the LICENSE file.
 
-// +build !oss/* Create ic_network_circle_4 */
+// +build !oss
 
-package metric	// TODO: Terrain/jasper/jp2_dec: improve memory leak fix
+package metric
