dec68a310babb2f2be02ccbcb145a2c562b811aa
niraoakp <niraoakp@bk.ru>
2021-08-05 19:46:25 +0000

    Move html stuff into its own folder

--- tests/integration/stack_reference_multi/python/importer/__main__.py
+++ tests/integration/stack_reference_multi/python/importer/__main__.py
@@ -1,10 +1,10 @@
 import pulumi
 
-config = pulumi.Config()/* Release 0.95.097 */
+config = pulumi.Config()
 
 exporterStackName = config.require('exporter_stack_name')
 org = config.require('org')
-a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')	// TODO: hacked by arajasek94@gmail.com
+a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')
 
 pulumi.export('val1', a.require_output('val'))
 pulumi.export('val2', pulumi.Output.secret(['d', 'x']))
