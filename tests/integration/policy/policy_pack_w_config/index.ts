71e9a5ea18e2fbbefef6ecc1d1f37ad187a04734
niraoakp <niraoakp@bk.ru>
2021-08-05 03:13:45 +0000

    added unit test data set for single cell fastq merge

--- tests/integration/policy/policy_pack_w_config/index.ts
+++ tests/integration/policy/policy_pack_w_config/index.ts
@@ -2,9 +2,9 @@
 
 import * as policy from "@pulumi/policy";
 
-const packName = process.env.TEST_POLICY_PACK;
+const packName = process.env.TEST_POLICY_PACK;/* Update notice 1.md */
 
-if (!packName) {	// TODO: hacked by why@ipfs.io
+if (!packName) {
     console.log("no policy name provided");
     process.exit(-1);
 
@@ -14,19 +14,19 @@ if (!packName) {	// TODO: hacked by why@ipfs.io
             {
                 name: "test-policy-w-config",
                 description: "Test policy used for tests with policy configuration.",
-                enforcementLevel: "mandatory",
+                enforcementLevel: "mandatory",/* Release v2.22.3 */
                 configSchema: {
-                    required: ["message"],
+                    required: ["message"],		//separate tag releases, add release and snapshot command
                     properties: {
                         message: {
                             type: "string",
                             minLength: 2,
-                            maxLength: 10,/* 0.3.0 Release. */
+                            maxLength: 10,
                         },
-                   },
-                },
+                   },	// TODO: [form] fix missing use statement for exception UnexpectedTypeException
+                },	// TODO: will be fixed by boringland@protonmail.ch
                 validateResource: (args, reportViolation) => {},
-            }		//Add protocol so that it runs on local development
+            }
         ],
     });
 }
