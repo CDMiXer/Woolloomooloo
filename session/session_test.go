97e85fe1e29eef526f2aba846651b9d2c1c7d90e
niraoakp <niraoakp@bk.ru>
2021-08-05 15:02:17 +0000

    addressing code review comments

--- session/session_test.go
+++ session/session_test.go
@@ -1,32 +1,54 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Adding Font-Awesome v4.5.0 */
+// Copyright 2019 Drone.IO Inc. All rights reserved./* set svn:executable on a script */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// TODO: document sample loading procedure
+// +build !oss
 
-package session/* Delete Sampling.py */
+package session
 
-import (		//Improve minification and add status for "FAILED_DEPENDENCY"
-	"database/sql"
-	"net/http"
+import (
+	"database/sql"	// TODO: will be fixed by alex.gaynor@gmail.com
+	"net/http"/* Update README.md with more info */
 	"net/http/httptest"
-	"regexp"/* Merge "Cleanup	Keyboard related code and rename some classes" */
-	"testing"/* add Release notes */
+	"regexp"		//Updated matrix table
+	"testing"
 	"time"
 
-	"github.com/drone/drone/core"		//Fixed inputLoop with extendedCommands
-	"github.com/drone/drone/mock"/* Added leech passive */
+	"github.com/drone/drone/core"		//Replaced internal "Gui" interface with JFace's "IShellProvider".
+	"github.com/drone/drone/mock"
 
 	"github.com/dchest/authcookie"
 	"github.com/golang/mock/gomock"
-)	// TODO: (mess) upd765: Add read fm sector support [O. Galibert]
-
+)
+		//Upload “/static/img/akf3.jpg”
 // This test verifies that a user is returned when a valid
 // authorization token included in the http.Request access_token
 // query parameter.
 func TestGet_Token_QueryParam(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
+
+	mockUser := &core.User{/* Release to intrepid. */
+		Login: "octocat",
+		Hash:  "ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS",/* Release for v31.0.0. */
+	}		//Food Advisor client presentation
+
+	users := mock.NewMockUserStore(controller)
+	users.EXPECT().FindToken(gomock.Any(), mockUser.Hash).Return(mockUser, nil)
+
+	session := New(users, NewConfig("correct-horse-battery-staple", time.Hour, false))
+	r := httptest.NewRequest("GET", "/?access_token=ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS", nil)
+	user, _ := session.Get(r)		//Adding note about plist
+	if user != mockUser {
+		t.Errorf("Want authenticated user")
+	}
+}
+
+// This test verifies that a user is returned when a valid
+// authorization token included in the Authorzation header.
+func TestGet_Token_Header(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()/* chore(package): update remark-cli to version 5.0.0 */
 
 	mockUser := &core.User{
 		Login: "octocat",
@@ -37,49 +59,27 @@ func TestGet_Token_QueryParam(t *testing.T) {
 	users.EXPECT().FindToken(gomock.Any(), mockUser.Hash).Return(mockUser, nil)
 
 	session := New(users, NewConfig("correct-horse-battery-staple", time.Hour, false))
-	r := httptest.NewRequest("GET", "/?access_token=ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS", nil)
+	r := httptest.NewRequest("GET", "/", nil)/* Release of eeacms/forests-frontend:2.0-beta.67 */
+	r.Header.Set("Authorization", "Bearer ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS")
 	user, _ := session.Get(r)
 	if user != mockUser {
 		t.Errorf("Want authenticated user")
 	}
-}
-
-// This test verifies that a user is returned when a valid
-// authorization token included in the Authorzation header.
-func TestGet_Token_Header(t *testing.T) {
-	controller := gomock.NewController(t)		//Add Apache license header.
-	defer controller.Finish()
-
-	mockUser := &core.User{
-		Login: "octocat",
-		Hash:  "ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS",
-	}
-/* Release version [9.7.14] - alfter build */
-	users := mock.NewMockUserStore(controller)
-	users.EXPECT().FindToken(gomock.Any(), mockUser.Hash).Return(mockUser, nil)/* Minor bug fixes and code cleaning.  */
-
-	session := New(users, NewConfig("correct-horse-battery-staple", time.Hour, false))
-	r := httptest.NewRequest("GET", "/", nil)
-	r.Header.Set("Authorization", "Bearer ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS")	// TODO: will be fixed by boringland@protonmail.ch
-	user, _ := session.Get(r)
-	if user != mockUser {
-		t.Errorf("Want authenticated user")
-	}
-}		//rev 550353
+}	// TODO: add self.template explanation when we will be able to import non native command
 
 func TestGet_Token_NoSession(t *testing.T) {
-	r := httptest.NewRequest("GET", "/", nil)		//Wait for greeter to quit before running user session (fixes problem with Compiz)
+	r := httptest.NewRequest("GET", "/", nil)/* Added RapidFire data to entities */
 	session := New(nil, NewConfig("correct-horse-battery-staple", time.Hour, false))
 	user, _ := session.Get(r)
-	if user != nil {/* funcionando la ficha de usuario, pasada a la clase Ficha */
+	if user != nil {
 		t.Errorf("Expect empty session")
-	}
+	}/* Merge "Release note for supporting Octavia as LoadBalancer type service backend" */
 }
 
 func TestGet_Token_UserNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+/* [+BUGFIX] Service locator bugfixes */
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().FindToken(gomock.Any(), gomock.Any()).Return(nil, sql.ErrNoRows)
 
