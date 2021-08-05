9797edca191f4e78315aefa59a732d7fc51fb9b4
niraoakp <niraoakp@bk.ru>
2021-08-05 02:21:48 +0000

    fixes #5195

--- session/legacy_test.go
+++ session/legacy_test.go
@@ -1,77 +1,77 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//508316dc-2e5b-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// TODO: hacked by mail@bitpshr.net
+
 package session
 
 import (
 	"net/http/httptest"
 	"testing"
-	"time"
+	"time"/* Delete filtertable.min.js */
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//c7ac50c4-2e3e-11e5-9284-b827eb9e62be
 	"github.com/drone/drone/mock"
 	"github.com/golang/mock/gomock"
-)	// Force building RGB.NET release builds
+)
 
 func TestLegacyGet_NotLegacy(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
-	mockUser := &core.User{
+/* Added appraisal to test across multiple ActiveRecord versions. */
+	mockUser := &core.User{		//Renamed aspnet frontend project
 		Login: "octocat",
 		Hash:  "ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS",
 	}
 
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().FindToken(gomock.Any(), mockUser.Hash).Return(mockUser, nil)
-
+/* 4bdd6002-2e4b-11e5-9284-b827eb9e62be */
 	r := httptest.NewRequest("GET", "/", nil)
-	r.Header.Set("Authorization", "Bearer ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS")	// TODO: Delete Our-users.md
-/* - Commit after merge with NextRelease branch at release 22512 */
-	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})
+	r.Header.Set("Authorization", "Bearer ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS")/* avoid memory requirements for DBRelease files */
+
+	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})/* Latest Release 2.6 */
 	user, _ := session.Get(r)
-	if user != mockUser {/* pom: bump version to 1.9.1-SNAPSHOT */
-		t.Errorf("Want authenticated user")/* Now setWorkloadBufferSize is re-exported. */
+	if user != mockUser {
+		t.Errorf("Want authenticated user")
 	}
 }
 
-func TestLegacyGet(t *testing.T) {/* Updated 1.1 Release notes */
-	controller := gomock.NewController(t)/* Update sudo-node-command-not-found.md */
+func TestLegacyGet(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+	// TODO: No counter, yet
 	mockUser := &core.User{
 		Login: "octocat",
 		Hash:  "ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS",
-	}
-/* Corrected setCurrentTime of source svgElement */
+	}/* was/Client: ReleaseControlStop() returns bool */
+
 	users := mock.NewMockUserStore(controller)
-	users.EXPECT().FindLogin(gomock.Any(), gomock.Any()).Return(mockUser, nil)/* Merge "regulator: qpnp-labibb-regulator: fix the delay setting for LAB/IBB" */
-	r := httptest.NewRequest("GET", "/?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidGV4dCI6Im9jdG9jYXQiLCJpYXQiOjE1MTYyMzkwMjJ9.jf17GpOuKu-KAhuvxtjVvmZfwyeC7mEpKNiM6_cGOvo", nil)/* FT csv export fixed. Combo Languages */
+	users.EXPECT().FindLogin(gomock.Any(), gomock.Any()).Return(mockUser, nil)
+	r := httptest.NewRequest("GET", "/?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidGV4dCI6Im9jdG9jYXQiLCJpYXQiOjE1MTYyMzkwMjJ9.jf17GpOuKu-KAhuvxtjVvmZfwyeC7mEpKNiM6_cGOvo", nil)
 
 	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})
-	user, err := session.Get(r)
-	if err != nil {
+	user, err := session.Get(r)/* sites addition */
+	if err != nil {/* Release v4.1.10 [ci skip] */
 		t.Error(err)
 		return
 	}
 	if user != mockUser {
 		t.Errorf("Want authenticated user")
-	}/* Merge "Release 3.2.3.354 Prima WLAN Driver" */
-}/* Release new version 2.5.48: Minor bugfixes and UI changes */
-
-func TestLegacyGet_UserNotFound(t *testing.T) {		//adts header fix - used LC profile instead of main
-	controller := gomock.NewController(t)	// TODO: will be fixed by hello@brooklynzelenka.com
+	}
+}
+/* using ndarray.take */
+func TestLegacyGet_UserNotFound(t *testing.T) {/* Merge "README.rst updated" */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	users := mock.NewMockUserStore(controller)
+	users := mock.NewMockUserStore(controller)/* Release v0.93 */
 	r := httptest.NewRequest("GET", "/?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidGV4dCI6ImJpbGx5aWRvbCIsImlhdCI6MTUxNjIzOTAyMn0.yxTCucstDM7BaixXBMAJCXup9zBaFr02Kalv_PqCDM4", nil)
 
 	session, _ := Legacy(users, Config{Secure: false, Timeout: time.Hour, MappingFile: "testdata/mapping.json"})
-	_, err := session.Get(r)
-	if err == nil || err.Error() != "Legacy token: cannot lookup user" {
+	_, err := session.Get(r)/* just trying things out... */
+	if err == nil || err.Error() != "Legacy token: cannot lookup user" {		//Automatic changelog generation for PR #46776 [ci skip]
 		t.Errorf("Expect user lookup error, got %v", err)
 		return
 	}
