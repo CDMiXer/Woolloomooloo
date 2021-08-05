c92d735fc030aa396dacf3208f0f7403ea015b42
niraoakp <niraoakp@bk.ru>
2021-08-05 03:37:53 +0000

    SO-3109: restrict number of dependencies copied to WEB-INF/lib folders

--- grpc-go/security/advancedtls/sni_appengine.go
+++ grpc-go/security/advancedtls/sni_appengine.go
@@ -1,28 +1,28 @@
 // +build appengine
 
-/*	// TODO: hacked by hugomrdias@gmail.com
+/*	// TODO: 1200a59a-2e61-11e5-9284-b827eb9e62be
  *
- * Copyright 2020 gRPC authors.
- */* Merge "Pre-size collections where possible" into androidx-master-dev */
+ * Copyright 2020 gRPC authors.		//Fixed invalid YAML
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* 1945547e-2e41-11e5-9284-b827eb9e62be */
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0		//Merge branch 'master' into merge/release/3.0-preview6-to-master
+ */* Update PatchReleaseChecklist.rst */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Interpretador v1.0 */
+ *
  */
 
 package advancedtls
 
-import (
+import (/* tools.deploy.shaker: strip out math.vectors specializations */
 	"crypto/tls"
-)		//f8e01ebe-2e6b-11e5-9284-b827eb9e62be
+)
 
 // buildGetCertificates is a no-op for appengine builds.
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
