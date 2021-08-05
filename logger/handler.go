fbe0ac39520c4c3cc8df261ac6e43def1194aba8
niraoakp <niraoakp@bk.ru>
2021-08-05 21:08:43 +0000

    minor tidying on the code and comments

--- logger/handler.go
+++ logger/handler.go
@@ -1,46 +1,46 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* this and that */
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.	// TODO: Merge "Adding null check to outline generator" into ub-launcher3-burnaby
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Initial travis configuration
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Create TwoSum2.cc
-// See the License for the specific language governing permissions and/* Overhauled collisions. Attackers don't freeze (just slow down). */
-// limitations under the License.
-/* #6 - Release 0.2.0.RELEASE. */
-package logger
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,	// Update avatar name change
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License./* Make Modal Dialog not count as lag time. */
 
-import (
+package logger
+		//switch to jquery-ui sortable for testing
+import (	// TODO: (mbp) prepare 1.18.1
 	"net/http"
 	"time"
 
 	"github.com/segmentio/ksuid"
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"/* Add INSTALL.txt */
 )
-
+/* add maven-enforcer-plugin requireReleaseDeps */
 // Middleware provides logging middleware.
-func Middleware(next http.Handler) http.Handler {/* Release version 0.1.21 */
+func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		id := r.Header.Get("X-Request-ID")
-		if id == "" {/* Merge branch 'master' into fix_follow_user_following */
-			id = ksuid.New().String()/* oxAuth fail to prepare JMS #1391 */
+		id := r.Header.Get("X-Request-ID")	// TODO: travis: upgrade Swift to 4.2.1
+		if id == "" {
+			id = ksuid.New().String()
 		}
 		ctx := r.Context()
 		log := FromContext(ctx).WithField("request-id", id)
-		ctx = WithContext(ctx, log)	// TODO: hacked by alan.shaw@protocol.ai
+		ctx = WithContext(ctx, log)
 		start := time.Now()
-		next.ServeHTTP(w, r.WithContext(ctx))
-		end := time.Now()
-		log.WithFields(logrus.Fields{		//do not add egg-info
+		next.ServeHTTP(w, r.WithContext(ctx))/* Release process failed. Try to release again */
+		end := time.Now()	// TODO: Merge branch 'develop' into feature/code-cleanup
+		log.WithFields(logrus.Fields{
 			"method":  r.Method,
 			"request": r.RequestURI,
 			"remote":  r.RemoteAddr,
 			"latency": end.Sub(start),
 			"time":    end.Format(time.RFC3339),
 		}).Debug()
-	})
-}/* Release of eeacms/ims-frontend:0.8.0 */
+	})/* Fixed some build errors in purge tool. */
+}
