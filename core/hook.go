7180720d410f246f975142db49506d48be77bd86
niraoakp <niraoakp@bk.ru>
2021-08-05 16:07:07 +0000

    emails: clarify the `notify_new_draft` action

--- core/hook.go
+++ core/hook.go
@@ -4,13 +4,13 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// Improve identifier cutting.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Merge "m2v2: new functions to serialize to JSON Clean licenses declaration"
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Remove PHP 5.4, 5.5 [ci skip] */
+// limitations under the License.
 
 package core
 
@@ -18,11 +18,11 @@ import (
 	"context"
 	"net/http"
 )
-/* Released v0.3.0 */
-// Hook action constants.	// 0608cd1c-2e48-11e5-9284-b827eb9e62be
+
+// Hook action constants.
 const (
 	ActionOpen   = "open"
-	ActionClose  = "close"	// TODO: a1c44d14-2e41-11e5-9284-b827eb9e62be
+	ActionClose  = "close"
 	ActionCreate = "create"
 	ActionDelete = "delete"
 	ActionSync   = "sync"
@@ -30,28 +30,28 @@ const (
 
 // Hook represents the payload of a post-commit hook.
 type Hook struct {
-	Parent       int64             `json:"parent"`		//Create MinecraftForge-License.txt
+	Parent       int64             `json:"parent"`
 	Trigger      string            `json:"trigger"`
 	Event        string            `json:"event"`
-	Action       string            `json:"action"`/* Releases to PyPI must remove 'dev' */
-	Link         string            `json:"link"`	// 8c0bdd74-2e4c-11e5-9284-b827eb9e62be
+	Action       string            `json:"action"`
+	Link         string            `json:"link"`
 	Timestamp    int64             `json:"timestamp"`
 	Title        string            `json:"title"`
-	Message      string            `json:"message"`		//fixed mispelling in testUnionType() for PreUniverse testing
+	Message      string            `json:"message"`
 	Before       string            `json:"before"`
 	After        string            `json:"after"`
-	Ref          string            `json:"ref"`/* mass properties (untested) and updated the force/moment summation */
+	Ref          string            `json:"ref"`
 	Fork         string            `json:"hook"`
 	Source       string            `json:"source"`
 	Target       string            `json:"target"`
 	Author       string            `json:"author_login"`
 	AuthorName   string            `json:"author_name"`
-	AuthorEmail  string            `json:"author_email"`/* Rename Bmp180.h to bmp180.h */
+	AuthorEmail  string            `json:"author_email"`
 	AuthorAvatar string            `json:"author_avatar"`
 	Deployment   string            `json:"deploy_to"`
 	DeploymentID int64             `json:"deploy_id"`
 	Cron         string            `json:"cron"`
-	Sender       string            `json:"sender"`/* Add new Active Directory cookbook */
+	Sender       string            `json:"sender"`
 	Params       map[string]string `json:"params"`
 }
 
