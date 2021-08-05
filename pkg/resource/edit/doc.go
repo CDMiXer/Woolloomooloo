8b873764c99351d4efcb22e5ab0f3a6882b3b870
niraoakp <niraoakp@bk.ru>
2021-08-05 00:26:12 +0000

    [MOD] XQuery, built-in functions, arguments

--- pkg/resource/edit/doc.go
+++ pkg/resource/edit/doc.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+//	// TODO: hacked by yuvalalaluf@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: New PID values (needs testing)
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-// Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level
+/* fix slug problems */
+// Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level/* Merge branch 'master' into large-image-file-reference */
 // tools that present a means for users to surgically edit their state.
-package edit
+package edit	// Delete 03-config.png
