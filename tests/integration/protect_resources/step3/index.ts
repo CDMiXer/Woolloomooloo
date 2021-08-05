3d58183dfab571cc502bad89759a22e9dca4fa88
niraoakp <niraoakp@bk.ru>
2021-08-05 23:06:02 +0000

    Increased usage of repaint sync framework in plot tool.

--- tests/integration/protect_resources/step3/index.ts
+++ tests/integration/protect_resources/step3/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-		//All done except a dummy.TestAllocateAddress
+
 // Attempt to delete the resource; this will fail!
