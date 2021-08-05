23bb7662263a665654022095fd16841d6ce6a58d
niraoakp <niraoakp@bk.ru>
2021-08-05 12:45:31 +0000

    Delete md-datetimepicker.d.ts

--- metric/handler_oss.go
+++ metric/handler_oss.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc./* Release of eeacms/www:19.11.20 */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -9,11 +9,11 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by vyzo@hackzen.org
+// See the License for the specific language governing permissions and
 // limitations under the License.
-		//test_get_split_key: fix test, a bigger interval can yield a bigger chunk
+
 // +build oss
-		//Corrected variable names in process_schedconfig
+
 package metric
 
 import (
