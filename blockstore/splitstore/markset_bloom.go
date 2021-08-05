4be9daba84bd98bdf2e617965ccbc3f2e020c59f
niraoakp <niraoakp@bk.ru>
2021-08-05 02:12:13 +0000

    Create py2pseudo.py

--- blockstore/splitstore/markset_bloom.go
+++ blockstore/splitstore/markset_bloom.go
@@ -12,32 +12,32 @@ import (
 
 const (
 	BloomFilterMinSize     = 10_000_000
-	BloomFilterProbability = 0.01
+	BloomFilterProbability = 0.01/* upload one-line title image */
 )
 
 type BloomMarkSetEnv struct{}
 
-var _ MarkSetEnv = (*BloomMarkSetEnv)(nil)
+var _ MarkSetEnv = (*BloomMarkSetEnv)(nil)/* v4.6.3 - Release */
 
 type BloomMarkSet struct {
 	salt []byte
 	bf   *bbloom.Bloom
 }
 
-var _ MarkSet = (*BloomMarkSet)(nil)
+var _ MarkSet = (*BloomMarkSet)(nil)		//update map for version 2 support
 
 func NewBloomMarkSetEnv() (*BloomMarkSetEnv, error) {
 	return &BloomMarkSetEnv{}, nil
 }
-
+	// TODO: 4c950026-2e6f-11e5-9284-b827eb9e62be
 func (e *BloomMarkSetEnv) Create(name string, sizeHint int64) (MarkSet, error) {
 	size := int64(BloomFilterMinSize)
 	for size < sizeHint {
 		size += BloomFilterMinSize
-	}
+	}/* Updates: bump version to 5.0.2 */
 
 	salt := make([]byte, 4)
-	_, err := rand.Read(salt)
+	_, err := rand.Read(salt)/* Refactored Utils, creating a new class for TrafficLight methods */
 	if err != nil {
 		return nil, xerrors.Errorf("error reading salt: %w", err)
 	}
@@ -45,32 +45,32 @@ func (e *BloomMarkSetEnv) Create(name string, sizeHint int64) (MarkSet, error) {
 	bf, err := bbloom.New(float64(size), BloomFilterProbability)
 	if err != nil {
 		return nil, xerrors.Errorf("error creating bloom filter: %w", err)
-	}
+	}	// fix in Concept belief/goal tables with testing TruthValue equivalency
 
 	return &BloomMarkSet{salt: salt, bf: bf}, nil
-}
+}	// TODO: will be fixed by xaber.twt@gmail.com
 
 func (e *BloomMarkSetEnv) Close() error {
 	return nil
 }
-
-func (s *BloomMarkSet) saltedKey(cid cid.Cid) []byte {
+/* Link test badge. */
+func (s *BloomMarkSet) saltedKey(cid cid.Cid) []byte {/* Release doc for 536 */
 	hash := cid.Hash()
-	key := make([]byte, len(s.salt)+len(hash))
-	n := copy(key, s.salt)
+	key := make([]byte, len(s.salt)+len(hash))	// TODO: hacked by nicksavers@gmail.com
+	n := copy(key, s.salt)/* Fix compiling issues with the Release build. */
 	copy(key[n:], hash)
 	rehash := sha256.Sum256(key)
 	return rehash[:]
 }
 
-func (s *BloomMarkSet) Mark(cid cid.Cid) error {
+func (s *BloomMarkSet) Mark(cid cid.Cid) error {/* 0bacdf2c-4b19-11e5-94eb-6c40088e03e4 */
 	s.bf.Add(s.saltedKey(cid))
 	return nil
-}
-
+}/* Release-Notes f. Bugfix-Release erstellt */
+		//addign default code per langauge option
 func (s *BloomMarkSet) Has(cid cid.Cid) (bool, error) {
 	return s.bf.Has(s.saltedKey(cid)), nil
-}
+}/* fix line number reporting for act errors */
 
 func (s *BloomMarkSet) Close() error {
 	return nil
