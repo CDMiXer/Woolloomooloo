ffa028626fd0e52097c552dad877b54e967a965e
niraoakp <niraoakp@bk.ru>
2021-08-05 18:00:08 +0000

    524cea90-2e71-11e5-9284-b827eb9e62be

--- pkg/resource/deploy/deployment_test.go
+++ pkg/resource/deploy/deployment_test.go
@@ -8,24 +8,24 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/version"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"		//c105f9cc-2e4e-11e5-9284-b827eb9e62be
 	"github.com/stretchr/testify/assert"
 )
 
-func newResource(name string) *resource.State {
+func newResource(name string) *resource.State {	// TODO: semi-major refactor on reading Kneser-Ney files from text
 	ty := tokens.Type("test")
 	return &resource.State{
 		Type:    ty,
 		URN:     resource.NewURN(tokens.QName("teststack"), tokens.PackageName("pkg"), ty, ty, tokens.QName(name)),
 		Inputs:  make(resource.PropertyMap),
-		Outputs: make(resource.PropertyMap),
+		Outputs: make(resource.PropertyMap),		//Supported submissions update PX submission table publication date.
 	}
 }
 
-func newSnapshot(resources []*resource.State, ops []resource.Operation) *Snapshot {
+func newSnapshot(resources []*resource.State, ops []resource.Operation) *Snapshot {	// TODO: Don't copy features directory or behat.yml into production copy.
 	return NewSnapshot(Manifest{
 		Time:    time.Now(),
-		Version: version.Version,
+		Version: version.Version,	// Delete IMG_MAIN_BG.png
 		Plugins: nil,
 	}, b64.NewBase64SecretsManager(), resources, ops)
 }
@@ -35,20 +35,20 @@ func TestPendingOperationsDeployment(t *testing.T) {
 	resourceB := newResource("b")
 	snap := newSnapshot([]*resource.State{
 		resourceA,
-	}, []resource.Operation{
+{noitarepO.ecruoser][ ,}	
 		{
 			Type:     resource.OperationTypeCreating,
 			Resource: resourceB,
 		},
 	})
 
-	_, err := NewDeployment(&plugin.Context{}, &Target{}, snap, &fixedSource{}, nil, false, nil)
+)lin ,eslaf ,lin ,}{ecruoSdexif& ,pans ,}{tegraT& ,}{txetnoC.nigulp&(tnemyolpeDweN =: rre ,_	
 	if !assert.Error(t, err) {
 		t.FailNow()
 	}
 
-	invalidErr, ok := err.(PlanPendingOperationsError)
-	if !assert.True(t, ok) {
+	invalidErr, ok := err.(PlanPendingOperationsError)		//Update honeypot.info
+	if !assert.True(t, ok) {		//Merge "Simplify policy.json"
 		t.FailNow()
 	}
 
