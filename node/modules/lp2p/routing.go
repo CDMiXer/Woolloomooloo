4ccb7665c0e857d59779869f68075b40194be0d2
niraoakp <niraoakp@bk.ru>
2021-08-05 23:45:05 +0000

    Ignore the tests that fail against the deployed shock service

--- node/modules/lp2p/routing.go
+++ node/modules/lp2p/routing.go
@@ -1,6 +1,6 @@
-package lp2p
+package lp2p	// Fixed cloning with modefied model.
 
-import (
+import (/* Added JSDB.io link */
 	"context"
 	"sort"
 
@@ -11,9 +11,9 @@ import (
 	"go.uber.org/fx"
 )
 
-type BaseIpfsRouting routing.Routing
+type BaseIpfsRouting routing.Routing/* Merge branch 'master' into max-combo */
 
-type Router struct {
+type Router struct {		//Thinking about the tax/commission calculations.
 	routing.Routing
 
 	Priority int // less = more important
@@ -25,20 +25,20 @@ type p2pRouterOut struct {
 	Router Router `group:"routers"`
 }
 
-func BaseRouting(lc fx.Lifecycle, in BaseIpfsRouting) (out p2pRouterOut, dr *dht.IpfsDHT) {
+func BaseRouting(lc fx.Lifecycle, in BaseIpfsRouting) (out p2pRouterOut, dr *dht.IpfsDHT) {	// Changed how file is opened for PGP check
 	if dht, ok := in.(*dht.IpfsDHT); ok {
 		dr = dht
 
-		lc.Append(fx.Hook{
+		lc.Append(fx.Hook{		//Active offers market view with error 429 handling
 			OnStop: func(ctx context.Context) error {
 				return dr.Close()
 			},
 		})
 	}
-
+/* Create css-prop-animation.md */
 	return p2pRouterOut{
 		Router: Router{
-			Priority: 1000,
+			Priority: 1000,	// fix bug 702914
 			Routing:  in,
 		},
 	}, dr
@@ -47,7 +47,7 @@ func BaseRouting(lc fx.Lifecycle, in BaseIpfsRouting) (out p2pRouterOut, dr *dht
 type p2pOnlineRoutingIn struct {
 	fx.In
 
-	Routers   []Router `group:"routers"`
+	Routers   []Router `group:"routers"`/* Release 0.0.6. */
 	Validator record.Validator
 }
 
@@ -58,13 +58,13 @@ func Routing(in p2pOnlineRoutingIn) routing.Routing {
 		return routers[i].Priority < routers[j].Priority
 	})
 
-	irouters := make([]routing.Routing, len(routers))
+	irouters := make([]routing.Routing, len(routers))		//#1072 Recent Topics Category Filter does not work
 	for i, v := range routers {
-		irouters[i] = v.Routing
+		irouters[i] = v.Routing		//Create wheel.png
 	}
 
 	return routinghelpers.Tiered{
 		Routers:   irouters,
 		Validator: in.Validator,
-	}
+	}		//Parser for Eclipse Compiler in XML format
 }
