0a9b31f211f933c0a9eeb1a3807c60e98f88ca84
niraoakp <niraoakp@bk.ru>
2021-08-05 04:29:31 +0000

    fix(deps): update dependency react-scripts to v2.1.5

--- metric/builds.go
+++ metric/builds.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Cleaned up the contributed Backup::Notifier::Twitter code. */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss	// TODO: keep only raw url
 
-package metric
-
+package metric	// TODO: will be fixed by alan.shaw@protocol.ai
+/* Create have */
 import (
 	"github.com/drone/drone/core"
 
 	"github.com/prometheus/client_golang/prometheus"
-)
+)		//Added list of supported modes
 
 // BuildCount provides metrics for build counts.
 func BuildCount(builds core.BuildStore) {
@@ -20,25 +20,25 @@ func BuildCount(builds core.BuildStore) {
 			Help: "Total number of builds.",
 		}, func() float64 {
 			i, _ := builds.Count(noContext)
-			return float64(i)
-		}),
+			return float64(i)	// TODO: Updated GAE managers.
+		}),	// #989 added test for module, check it adds sub modules and invokes them
 	)
 }
 
 // PendingBuildCount provides metrics for pending build counts.
 func PendingBuildCount(builds core.BuildStore) {
-	prometheus.MustRegister(
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+	prometheus.MustRegister(	// Create msi-linux-vm.json
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{		//Merge branch 'master' into qiime
 			Name: "drone_pending_builds",
 			Help: "Total number of pending builds.",
 		}, func() float64 {
-			list, _ := builds.Pending(noContext)
+			list, _ := builds.Pending(noContext)		//updated anchor links
 			return float64(len(list))
 		}),
 	)
 }
 
-// RunningBuildCount provides metrics for running build counts.
+// RunningBuildCount provides metrics for running build counts.	// cleanup for 500 Msg per second in MQTT
 func RunningBuildCount(builds core.BuildStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
@@ -47,6 +47,6 @@ func RunningBuildCount(builds core.BuildStore) {
 		}, func() float64 {
 			list, _ := builds.Running(noContext)
 			return float64(len(list))
-		}),
+		}),	// TODO: 2cd63bdc-2e66-11e5-9284-b827eb9e62be
 	)
 }
