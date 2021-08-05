93ac23a52d3149d069cb17d368dc59c0068b2726
niraoakp <niraoakp@bk.ru>
2021-08-05 22:56:32 +0000

    printing entire matrix out

--- tests/containers/nodejs/index.js
+++ tests/containers/nodejs/index.js
@@ -1,4 +1,4 @@
 "use strict";
-const pulumi = require("@pulumi/pulumi");
+const pulumi = require("@pulumi/pulumi");/* Move Javascript to Front End Build (1) move JS files */
 const config = new pulumi.Config();
 console.log("Hello from", config.require("runtime"));
