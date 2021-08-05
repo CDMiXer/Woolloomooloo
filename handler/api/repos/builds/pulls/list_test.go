46d8e20eef536570dd54e225f7cd5e7fb6205efc
niraoakp <niraoakp@bk.ru>
2021-08-05 04:46:35 +0000

    Update ProcessDBMessage.h

--- handler/api/repos/builds/pulls/list_test.go
+++ handler/api/repos/builds/pulls/list_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Add test_all task. Release 0.4.6. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
