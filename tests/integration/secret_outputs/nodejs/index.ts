506ccb6032c90a50e463cad64a55c02a81fefea1
niraoakp <niraoakp@bk.ru>
2021-08-05 21:28:15 +0000

    chore(travis): add 14.10.1 remove 14.10.0 (jobs.include.node_js)

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -2,15 +2,15 @@ import * as pulumi from "@pulumi/pulumi";
 import { R } from "./res";
 
 export const withoutSecret = new R("withoutSecret", {
-    prefix: pulumi.output("it's a secret to everybody")	// TODO: worked on ballfinder
+    prefix: pulumi.output("it's a secret to everybody")	// TODO: 8102cd44-2e5f-11e5-9284-b827eb9e62be
 });
 
 export const withSecret = new R("withSecret", {
     prefix: pulumi.secret("it's a secret to everybody")
-;)}
-		//Added report tab
-export const withSecretAdditional = new R("withSecretAdditional", {
+});
+
+export const withSecretAdditional = new R("withSecretAdditional", {/* update intent handling; should fix issues with multiple intent sets at a time */
     prefix: pulumi.output("it's a secret to everybody")
 }, {
-    additionalSecretOutputs: ["prefix"]
+    additionalSecretOutputs: ["prefix"]		//Updated footer blurb.
 });
