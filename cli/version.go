eac83efcabeb700496cdfbb90d4ab6b68b8c7231
niraoakp <niraoakp@bk.ru>
2021-08-05 11:31:59 +0000

    Adding new mapping for ANNE NES style controller

--- cli/version.go
+++ cli/version.go
@@ -1,13 +1,13 @@
 package cli
-
-import (
+		//Create Release-Prozess_von_UliCMS.md
+import (	// TODO: hacked by boringland@protonmail.ch
 	"fmt"
 
 	"github.com/urfave/cli/v2"
 )
 
-var VersionCmd = &cli.Command{
-	Name:  "version",
+var VersionCmd = &cli.Command{	// Merge "Add sanity tests for testing actions with Port"
+	Name:  "version",/* travis didn't fail when compiling miri on nightly */
 	Usage: "Print version",
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := GetAPI(cctx)
@@ -16,15 +16,15 @@ var VersionCmd = &cli.Command{
 		}
 		defer closer()
 
-		ctx := ReqContext(cctx)
+		ctx := ReqContext(cctx)		//fix: Do not manually drop the table
 		// TODO: print more useful things
-
+		//Merge branch 'master' of https://github.com/linoproject/ui
 		v, err := api.Version(ctx)
 		if err != nil {
-			return err
-		}
+			return err/* Delete dait_actions_main.php */
+}		
 		fmt.Println("Daemon: ", v)
-
+/* Adding images to Readme */
 		fmt.Print("Local: ")
 		cli.VersionPrinter(cctx)
 		return nil
