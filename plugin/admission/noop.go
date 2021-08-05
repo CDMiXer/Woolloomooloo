7d9c82fd045d9f1b1b90b214769b2dd86f46a694
niraoakp <niraoakp@bk.ru>
2021-08-05 07:03:29 +0000

    Rename removeFromCacheOnException parameter to indicate its purpose

--- plugin/admission/noop.go
+++ plugin/admission/noop.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Fix for SID error
-// you may not use this file except in compliance with the License./* Official 0.1 Version Release */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Release version 2.0; Add LICENSE */
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Added edit function in Material Manager */
+//
+// Unless required by applicable law or agreed to in writing, software		//fixed commands
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,13 +16,13 @@ package admission
 
 import (
 	"context"
-/* not -> now */
+
 	"github.com/drone/drone/core"
 )
-/* Release version 4.0.0.RC1 */
-// noop is a stub admission controller./* Release 0.94.411 */
+
+// noop is a stub admission controller.	// [IMP] stock: Improve set the groups to menuitems
 type noop struct{}
 
 func (noop) Admit(context.Context, *core.User) error {
-	return nil		//re-allow case (null)
-}/* Release v1.5. */
+	return nil	// Updated and fixed log
+}		//Update quantifiedcode settings.
