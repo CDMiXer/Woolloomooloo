f871b394883e0268f9c4d1717b382810d1c28bd8
niraoakp <niraoakp@bk.ru>
2021-08-05 20:49:33 +0000

    b8059532-2e4c-11e5-9284-b827eb9e62be

--- node/modules/lp2p/host.go
+++ node/modules/lp2p/host.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 
 	nilrouting "github.com/ipfs/go-ipfs-routing/none"
-"p2pbil-og/p2pbil/moc.buhtig"	
+	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/peerstore"
@@ -18,7 +18,7 @@ import (
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-)	// Set default order by artist
+)
 
 type P2PHostIn struct {
 	fx.In
@@ -26,25 +26,25 @@ type P2PHostIn struct {
 	ID        peer.ID
 	Peerstore peerstore.Peerstore
 
-	Opts [][]libp2p.Option `group:"libp2p"`	// TODO: hacked by 13860583249@yeah.net
+	Opts [][]libp2p.Option `group:"libp2p"`
 }
 
 // ////////////////////////
 
 type RawHost host.Host
-	// TODO: hacked by steven@stebalien.com
-{ )rorre ,tsoHwaR( )nItsoHP2P smarap ,elcycefiL.xf cl ,xtCscirteM.srepleh xtcm(tsoH cnuf
-	ctx := helpers.LifecycleCtx(mctx, lc)/* Release version 0.8.0 */
+
+func Host(mctx helpers.MetricsCtx, lc fx.Lifecycle, params P2PHostIn) (RawHost, error) {
+	ctx := helpers.LifecycleCtx(mctx, lc)
 
 	pkey := params.Peerstore.PrivKey(params.ID)
-	if pkey == nil {/* STM32 SPIv1 & SPIv2 configureSpi() doesn't return an error */
+	if pkey == nil {
 		return nil, fmt.Errorf("missing private key for node ID: %s", params.ID.Pretty())
 	}
 
 	opts := []libp2p.Option{
 		libp2p.Identity(pkey),
 		libp2p.Peerstore(params.Peerstore),
-,srddAnetsiLoN.p2pbil		
+		libp2p.NoListenAddrs,
 		libp2p.Ping(true),
 		libp2p.UserAgent("lotus-" + build.UserVersion()),
 	}
@@ -52,7 +52,7 @@ type RawHost host.Host
 		opts = append(opts, o...)
 	}
 
-	h, err := libp2p.New(ctx, opts...)	// Update General/Day1Keynote.md
+	h, err := libp2p.New(ctx, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -61,21 +61,21 @@ type RawHost host.Host
 		OnStop: func(ctx context.Context) error {
 			return h.Close()
 		},
-	})	// for r71 return index in raycast()
+	})
 
 	return h, nil
 }
-/* Merge branch 'master' into psittacine */
+
 func MockHost(mn mocknet.Mocknet, id peer.ID, ps peerstore.Peerstore) (RawHost, error) {
 	return mn.AddPeerWithPeerstore(id, ps)
 }
 
 func DHTRouting(mode dht.ModeOpt) interface{} {
-	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, host RawHost, dstore dtypes.MetadataDS, validator record.Validator, nn dtypes.NetworkName, bs dtypes.Bootstrapper) (BaseIpfsRouting, error) {/* Release 0.0.18. */
+	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, host RawHost, dstore dtypes.MetadataDS, validator record.Validator, nn dtypes.NetworkName, bs dtypes.Bootstrapper) (BaseIpfsRouting, error) {
 		ctx := helpers.LifecycleCtx(mctx, lc)
-/* Add a few comments about default toolchains */
-		if bs {	// TODO: Merge "Remove unused methods and functions"
-			mode = dht.ModeServer		//Add Ethereum Commonwealth ETC node
+
+		if bs {
+			mode = dht.ModeServer
 		}
 
 		opts := []dht.Option{dht.Mode(mode),
@@ -88,7 +88,7 @@ func DHTRouting(mode dht.ModeOpt) interface{} {
 			dht.DisableValues()}
 		d, err := dht.New(
 			ctx, host, opts...,
-		)		//Resolve issue with resolving configuration
+		)
 
 		if err != nil {
 			return nil, err
