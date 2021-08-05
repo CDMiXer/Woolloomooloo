8324ecde04df8632db7c1caae7d9e01c6a971413
niraoakp <niraoakp@bk.ru>
2021-08-05 15:33:15 +0000

    Update 02-flow-basics.md

--- server/auth/authorizer.go
+++ server/auth/authorizer.go
@@ -2,11 +2,11 @@ package auth
 
 import (
 	"context"
-
+	// TODO: will be fixed by aeongrp@outlook.com
 	authUtil "github.com/argoproj/argo/util/auth"
 )
 
-func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {
+func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {/* Unit tests. REST Update and create controllers return new json object. */
 	kubeClientset := GetKubeClient(ctx)
 	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)
 	if err != nil {
