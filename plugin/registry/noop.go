8a2698b4a9f1d408153f8133167114622d93cd8a
niraoakp <niraoakp@bk.ru>
2021-08-05 21:59:52 +0000

    sys/Host: Change getHostTriple() to return the full Darwin version on OS X.

--- plugin/registry/noop.go
+++ plugin/registry/noop.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc./* 1a99b954-4b19-11e5-b654-6c40088e03e4 */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,22 +6,22 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by peterke@gmail.com
-// distributed under the License is distributed on an "AS IS" BASIS,/* remove python xy */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by jon@atack.com
+// limitations under the License.
 
 package registry
 
 import (
 	"context"
 
-	"github.com/drone/drone/core"/* Release v1.9.1 */
+	"github.com/drone/drone/core"
 )
 
-type noop struct{}/* added hasPublishedVersion to GetReleaseVersionResult */
-	// TODO: hacked by fkautz@pseudocode.cc
-func (noop) List(context.Context, *core.RegistryArgs) ([]*core.Registry, error) {		//Merge "Make FORWARDED_PORT a Facter fact"
+type noop struct{}
+
+func (noop) List(context.Context, *core.RegistryArgs) ([]*core.Registry, error) {
 	return nil, nil
 }
