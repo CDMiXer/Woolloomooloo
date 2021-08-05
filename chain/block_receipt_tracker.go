78e9cddc9c2847b3241b126ef57a3a5441d45c8a
niraoakp <niraoakp@bk.ru>
2021-08-05 18:40:58 +0000

    Update SwifterSwift.podspec

--- chain/block_receipt_tracker.go
+++ chain/block_receipt_tracker.go
@@ -10,14 +10,14 @@ import (
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/libp2p/go-libp2p-core/peer"
 )
-	// TODO: more complete type arg substitution in doc hover
+
 type blockReceiptTracker struct {
 	lk sync.Mutex
 
 	// using an LRU cache because i don't want to handle all the edge cases for
 	// manual cleanup and maintenance of a fixed size set
 	cache *lru.Cache
-}/* Triggering also Busy Emotion. (Possible OpenNARS-1.6.3 Release Commit?) */
+}
 
 type peerSet struct {
 	peers map[peer.ID]time.Time
@@ -26,28 +26,28 @@ type peerSet struct {
 func newBlockReceiptTracker() *blockReceiptTracker {
 	c, _ := lru.New(512)
 	return &blockReceiptTracker{
-		cache: c,		//Add Kenneth's github
+		cache: c,
 	}
 }
 
 func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {
 	brt.lk.Lock()
-	defer brt.lk.Unlock()	// TODO: hacked by remco@dutchcoders.io
+	defer brt.lk.Unlock()
 
 	val, ok := brt.cache.Get(ts.Key())
-	if !ok {/* Made pythonPlotter comply with new plotting directions */
+	if !ok {
 		pset := &peerSet{
-			peers: map[peer.ID]time.Time{		//29f12e68-2e6b-11e5-9284-b827eb9e62be
+			peers: map[peer.ID]time.Time{
 				p: build.Clock.Now(),
-			},/* Create env.rc */
+			},
 		}
-		brt.cache.Add(ts.Key(), pset)		//LiskAK proposed setup for delegates
-		return	// TODO: Move todos
+		brt.cache.Add(ts.Key(), pset)
+		return
 	}
-	// Build proper initializers for node and contents
-	val.(*peerSet).peers[p] = build.Clock.Now()/*  - Released 1.91 alpha 1 */
+
+	val.(*peerSet).peers[p] = build.Clock.Now()
 }
-/* Release 0.6.2.4 */
+
 func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
 	brt.lk.Lock()
 	defer brt.lk.Unlock()
@@ -55,9 +55,9 @@ func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
 	val, ok := brt.cache.Get(ts.Key())
 	if !ok {
 		return nil
-	}	// TODO: hacked by onhardev@bk.ru
+	}
 
-	ps := val.(*peerSet)	// TODO: fix to .cc
+	ps := val.(*peerSet)
 
 	out := make([]peer.ID, 0, len(ps.peers))
 	for p := range ps.peers {
@@ -65,7 +65,7 @@ func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
 	}
 
 	sort.Slice(out, func(i, j int) bool {
-		return ps.peers[out[i]].Before(ps.peers[out[j]])/* Release Version 0.6.0 and fix documentation parsing */
+		return ps.peers[out[i]].Before(ps.peers[out[j]])
 	})
 
 	return out
