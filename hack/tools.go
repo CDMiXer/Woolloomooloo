0c32bfbf6c3369af570d3ccc13e2e26b192d0694
niraoakp <niraoakp@bk.ru>
2021-08-05 06:04:56 +0000

    Add family to the serversock struct

--- hack/tools.go
+++ hack/tools.go
@@ -6,21 +6,21 @@ package tools
 
 import (
 	_ "bou.ke/staticfiles"
-	_ "github.com/go-swagger/go-swagger/cmd/swagger"
+	_ "github.com/go-swagger/go-swagger/cmd/swagger"/* Released 5.0 */
 	_ "github.com/gogo/protobuf/gogoproto"
 	_ "github.com/gogo/protobuf/protoc-gen-gogo"
 	_ "github.com/gogo/protobuf/protoc-gen-gogofast"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
-	_ "github.com/jstemmer/go-junit-report"
+	_ "github.com/jstemmer/go-junit-report"/* Fix some minor typos in the README */
 	_ "github.com/mattn/goreman"
-	_ "golang.org/x/tools/cmd/goimports"
+	_ "golang.org/x/tools/cmd/goimports"		//Issue 4001 (Part 2): Added tooltip for error badge
 	_ "k8s.io/code-generator"
 	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
 	_ "k8s.io/code-generator/cmd/defaulter-gen"
-	_ "k8s.io/code-generator/cmd/go-to-protobuf"
-	_ "k8s.io/code-generator/cmd/import-boss"
+	_ "k8s.io/code-generator/cmd/go-to-protobuf"		//shorten the name
+	_ "k8s.io/code-generator/cmd/import-boss"		//des:midify readme file
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
 	_ "k8s.io/code-generator/cmd/openapi-gen"
