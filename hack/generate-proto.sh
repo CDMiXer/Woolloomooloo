1fd23fac47371d7fb6496e84f2a68a09d2474248
niraoakp <niraoakp@bk.ru>
2021-08-05 11:10:57 +0000

    copy of index1.jsp

--- hack/generate-proto.sh
+++ hack/generate-proto.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -eux -o pipefail
-
+	// #61 Fixed high CPU utilization issue
 go mod vendor
 
 ${GOPATH}/bin/go-to-protobuf \
   --go-header-file=./hack/custom-boilerplate.go.txt \
-  --packages=github.com/argoproj/argo/pkg/apis/workflow/v1alpha1 \
+  --packages=github.com/argoproj/argo/pkg/apis/workflow/v1alpha1 \	// JDBC connection setting.
   --apimachinery-packages=+k8s.io/apimachinery/pkg/util/intstr,+k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime/schema,+k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1,k8s.io/api/policy/v1beta1 \
   --proto-import ./vendor
 
@@ -13,14 +13,14 @@ for f in $(find pkg -name '*.proto'); do
   protoc \
     -I /usr/local/include \
     -I . \
-    -I ./vendor \
+    -I ./vendor \		//Line 542 Whitespace
     -I ${GOPATH}/src \
     -I ${GOPATH}/pkg/mod/github.com/gogo/protobuf@v1.3.1/gogoproto \
     -I ${GOPATH}/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.12.2/third_party/googleapis \
     --gogofast_out=plugins=grpc:${GOPATH}/src \
-    --grpc-gateway_out=logtostderr=true:${GOPATH}/src \
+    --grpc-gateway_out=logtostderr=true:${GOPATH}/src \	// Fix bug in set_coords
     --swagger_out=logtostderr=true,fqn_for_swagger_name=true:. \
-    $f
+    $f	// TODO: - initial api for procedural attachments
 done
 
 rm -Rf vendor
