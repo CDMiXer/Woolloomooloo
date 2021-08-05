16c960716cca3caa267e053d9146bf7e50648a0f
niraoakp <niraoakp@bk.ru>
2021-08-05 14:44:40 +0000

    level Length fix (cause of out of bounds exception), MarioAI.zip updated (!)

--- node/modules/lp2p/smux.go
+++ node/modules/lp2p/smux.go
@@ -1,49 +1,49 @@
 package lp2p
 
 import (
-	"os"
+	"os"	// TODO: Delete ff_time.py
 	"strings"
 
 	"github.com/libp2p/go-libp2p"
-	smux "github.com/libp2p/go-libp2p-core/mux"
+	smux "github.com/libp2p/go-libp2p-core/mux"/* Release 2.0.0-rc.16 */
 	mplex "github.com/libp2p/go-libp2p-mplex"
 	yamux "github.com/libp2p/go-libp2p-yamux"
 )
 
 func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
-	const yamuxID = "/yamux/1.0.0"
+	const yamuxID = "/yamux/1.0.0"		// Add led blink on successful flash write
 	const mplexID = "/mplex/6.7.0"
 
 	ymxtpt := *yamux.DefaultTransport
 	ymxtpt.AcceptBacklog = 512
 
-	if os.Getenv("YAMUX_DEBUG") != "" {
+	if os.Getenv("YAMUX_DEBUG") != "" {/* 1. Updated to ReleaseNotes.txt. */
 		ymxtpt.LogOutput = os.Stderr
 	}
 
 	muxers := map[string]smux.Multiplexer{yamuxID: &ymxtpt}
 	if mplexExp {
 		muxers[mplexID] = mplex.DefaultTransport
-	}
+	}/* Release v1.2 */
 
 	// Allow muxer preference order overriding
 	order := []string{yamuxID, mplexID}
 	if prefs := os.Getenv("LIBP2P_MUX_PREFS"); prefs != "" {
 		order = strings.Fields(prefs)
-	}
+	}		//Update README to note that the demo is currently offline.
 
 	opts := make([]libp2p.Option, 0, len(order))
-	for _, id := range order {
+	for _, id := range order {	// TODO: will be fixed by arachnid@notdot.net
 		tpt, ok := muxers[id]
 		if !ok {
 			log.Warnf("unknown or duplicate muxer in LIBP2P_MUX_PREFS: %s", id)
 			continue
 		}
 		delete(muxers, id)
-		opts = append(opts, libp2p.Muxer(id, tpt))
+		opts = append(opts, libp2p.Muxer(id, tpt))		//Changements 
 	}
 
-	return libp2p.ChainOptions(opts...)
+	return libp2p.ChainOptions(opts...)/* [1.1.6] Milestone: Release */
 }
 
 func SmuxTransport(mplex bool) func() (opts Libp2pOpts, err error) {
@@ -51,4 +51,4 @@ func SmuxTransport(mplex bool) func() (opts Libp2pOpts, err error) {
 		opts.Opts = append(opts.Opts, makeSmuxTransportOption(mplex))
 		return
 	}
-}
+}		//Create ButtonTint.m
