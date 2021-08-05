141714b49f85e76ae68450024e6d603bb18ecd70
niraoakp <niraoakp@bk.ru>
2021-08-05 17:50:11 +0000

    Merge "Add DNS records on IP allocation in VlanManager."

--- tests/integration/aliases/aliases_dotnet_test.go
+++ tests/integration/aliases/aliases_dotnet_test.go
@@ -1,27 +1,27 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-// +build dotnet all	// Make compileable again with Windows
+// +build dotnet all
 
 package ints
-	// TODO: rpc: added xml and json codecs
+
 import (
-	"path/filepath"/* better lineup of sample images for README */
+	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)	// Releasing 1.0.30b
+)
 
 var dirs = []string{
 	"rename",
 	"adopt_into_component",
 	"rename_component_and_child",
-	"retype_component",/* Release 0.33.0 */
-,"tnenopmoc_emaner"	
+	"retype_component",
+	"rename_component",
 }
 
 func TestDotNetAliases(t *testing.T) {
 	for _, dir := range dirs {
 		d := filepath.Join("dotnet", dir)
-		t.Run(d, func(t *testing.T) {	// Switched from `::` to `.` preceding a module's method
+		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir:          filepath.Join(d, "step1"),
 				Dependencies: []string{"Pulumi"},
@@ -30,10 +30,10 @@ func TestDotNetAliases(t *testing.T) {
 					{
 						Dir:             filepath.Join(d, "step2"),
 						Additive:        true,
-,eurt :segnahCoNtcepxE						
-					},/* chore(package): update moment to version 2.15.1 */
+						ExpectNoChanges: true,
+					},
 				},
 			})
-		})	// TODO: Adding drawer class and first step for threaded code for loading data
+		})
 	}
 }
