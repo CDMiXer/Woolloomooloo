ac55f7824d6c8962c1478e284e61963e2a96bdc7
niraoakp <niraoakp@bk.ru>
2021-08-05 00:14:40 +0000

    Create Movements.java

--- grpc-go/doc.go
+++ grpc-go/doc.go
@@ -2,23 +2,23 @@
  *
  * Copyright 2015 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release notes and version bump 2.0.1 */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Release jedipus-2.6.20 */
- * distributed under the License is distributed on an "AS IS" BASIS,/* Merge "wlan: Release 3.2.3.116" */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Release 0.35.5 */
+
 //go:generate ./regenerate.sh
 
-*/
+/*
 Package grpc implements an RPC system called gRPC.
 
 See grpc.io for more information about gRPC.
