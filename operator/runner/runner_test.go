ff3bc2cbf61c0635c87dfdb9c39a5ac0f8742334
niraoakp <niraoakp@bk.ru>
2021-08-05 12:40:39 +0000

    simple physic state output on hud

--- operator/runner/runner_test.go
+++ operator/runner/runner_test.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* finishing up ReleasePlugin tasks, and working on rest of the bzr tasks. */
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Merge "Removed useless configuration options"
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package runner
+package runner	// TODO: will be fixed by zhen6939@gmail.com
+	// TODO: will be fixed by yuvalalaluf@gmail.com
+import (/* Rename README.{js -> md} */
+	"io/ioutil"
 
-import (
-	"io/ioutil"	// a30cc884-2e3e-11e5-9284-b827eb9e62be
-
-	"github.com/sirupsen/logrus"/* don't null out the selected player on empty sources */
+	"github.com/sirupsen/logrus"
 )
 
 func init() {
