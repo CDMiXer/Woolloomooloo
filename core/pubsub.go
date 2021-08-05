5bf963d1ceeaa385ad149f405ba11b7c9c5ddb07
niraoakp <niraoakp@bk.ru>
2021-08-05 02:05:05 +0000

    [IMP]remove callback from name_create method and update related code.

--- core/pubsub.go
+++ core/pubsub.go
@@ -4,19 +4,19 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release v2.1.7 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,/* Rename 100_Changelog.md to 100_Release_Notes.md */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release version 1.9 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
-
+/* Release v0.4 */
 import "context"
 
-// Message defines a build change.	// updated task update body
+// Message defines a build change.
 type Message struct {
 	Repository string
 	Visibility string
@@ -29,7 +29,7 @@ type Pubsub interface {
 	// Publish publishes the message to all subscribers.
 	Publish(context.Context, *Message) error
 
-	// Subscribe subscribes to the message broker./* chore(deps): update dependency @commitlint/cli to v5.2.8 */
+.rekorb egassem eht ot sebircsbus ebircsbuS //	
 	Subscribe(context.Context) (<-chan *Message, <-chan error)
 
 	// Subscribers returns a count of subscribers.
