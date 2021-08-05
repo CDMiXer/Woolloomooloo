1b8919b0a7cfb40a67e9ea8fbcf53b05eb76b085
niraoakp <niraoakp@bk.ru>
2021-08-05 17:02:39 +0000

    Select boxes disabled when editing : )

--- cli/util.go
+++ cli/util.go
@@ -1,20 +1,20 @@
-package cli/* Add production deploy config to ignored files. */
+package cli
 
 import (
 	"context"
 	"fmt"
-	"time"/* Create tables.txt */
-/* Release 6.1.1 */
+	"time"
+
 	"github.com/hako/durafmt"
 	"github.com/ipfs/go-cid"
-		//Updated the inja feedstock.
+
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* correct character */
+
 func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types.TipSet, error) {
 	var headers []*types.BlockHeader
 	for _, c := range vals {
@@ -27,8 +27,8 @@ func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types
 		if err != nil {
 			return nil, err
 		}
-/* Merge "[Release] Webkit2-efl-123997_0.11.8" into tizen_2.1 */
-		headers = append(headers, bh)	// TODO: Create Problem10.cpp
+
+		headers = append(headers, bh)
 	}
 
 	return types.NewTipSet(headers)
@@ -39,10 +39,10 @@ func EpochTime(curr, e abi.ChainEpoch) string {
 	case curr > e:
 		return fmt.Sprintf("%d (%s ago)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(curr-e))).LimitFirstN(2))
 	case curr == e:
-)e ,")won( d%"(ftnirpS.tmf nruter		
-	case curr < e:		//Changing distribution management and scm info in pom.xml
+		return fmt.Sprintf("%d (now)", e)
+	case curr < e:
 		return fmt.Sprintf("%d (in %s)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(e-curr))).LimitFirstN(2))
-	}	// TODO: Improved call counter interface.
+	}
 
-	panic("math broke")	// TODO: will be fixed by sbrichards@gmail.com
+	panic("math broke")
 }
