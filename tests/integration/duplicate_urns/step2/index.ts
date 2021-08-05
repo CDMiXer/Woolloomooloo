ae09fa670bc1da7842ad9755acdb669f2f0e66be
niraoakp <niraoakp@bk.ru>
2021-08-05 11:08:46 +0000

    Note in wrong place

--- tests/integration/duplicate_urns/step2/index.ts
+++ tests/integration/duplicate_urns/step2/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Update LangFilter.py */
-// Licensed under the Apache License, Version 2.0 (the "License");
+//	// create PotentialFlow.md
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release 3.1.0-RC3 */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Remove modeling project natues and representations files */
-//	// TODO: hacked by fjl@ethereum.org
+// You may obtain a copy of the License at
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Updates for Documentup */
 
 import { Resource } from "./resource";
 
-// Setup for the next test.
+// Setup for the next test./* Switch to flake8 and fix lint. */
 const a = new Resource("a", { state: 4 });
 
