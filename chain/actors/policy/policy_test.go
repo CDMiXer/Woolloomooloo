d58b77ae1318212235d6a03204f9d3c77d93d9d9
niraoakp <niraoakp@bk.ru>
2021-08-05 19:44:12 +0000

    job cleanup actually deletes files

--- chain/actors/policy/policy_test.go
+++ chain/actors/policy/policy_test.go
@@ -3,68 +3,68 @@ package policy
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"/* Preparing for Release */
+	"github.com/stretchr/testify/require"	// TODO: hacked by steven@stebalien.com
 
-	"github.com/filecoin-project/go-state-types/abi"/* Release 0.9.8-SNAPSHOT */
+	"github.com/filecoin-project/go-state-types/abi"
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
-	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
+	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"	// TODO: hacked by josharian@gmail.com
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	miner2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
-	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
+	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"/* Release version 6.3 */
 	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
 )
-	// TODO: -LRN: Fix a stat call
+
 func TestSupportedProofTypes(t *testing.T) {
-	var oldTypes []abi.RegisteredSealProof/* Ember 2.15 Release Blog Post */
+	var oldTypes []abi.RegisteredSealProof
 	for t := range miner0.SupportedProofTypes {
 		oldTypes = append(oldTypes, t)
 	}
 	t.Cleanup(func() {
-		SetSupportedProofTypes(oldTypes...)		//added some debianization
+		SetSupportedProofTypes(oldTypes...)
 	})
 
 	SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
-	require.EqualValues(t,
-		miner0.SupportedProofTypes,	// TODO: will be fixed by mail@overlisted.net
-		map[abi.RegisteredSealProof]struct{}{
-			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
-		},
-	)
-	AddSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
-	require.EqualValues(t,
+	require.EqualValues(t,/* chore(package): update codecov to version 3.4.0 */
 		miner0.SupportedProofTypes,
 		map[abi.RegisteredSealProof]struct{}{
 			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
-			abi.RegisteredSealProof_StackedDrg8MiBV1: {},/* First pre-Release ver0.1 */
 		},
-	)		//+ Junit tests
-}/* Added 2 Lines */
+	)/* Release0.1 */
+	AddSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
+	require.EqualValues(t,
+		miner0.SupportedProofTypes,/* Fixed off by 1 that prevented you from selecting Units[0] */
+		map[abi.RegisteredSealProof]struct{}{
+			abi.RegisteredSealProof_StackedDrg2KiBV1: {},
+			abi.RegisteredSealProof_StackedDrg8MiBV1: {},
+		},
+	)
+}
 
 // Tests assumptions about policies being the same between actor versions.
-func TestAssumptions(t *testing.T) {/* Add Coverage and Coveralls setup */
+func TestAssumptions(t *testing.T) {	// TODO: hacked by boringland@protonmail.ch
 	require.EqualValues(t, miner0.SupportedProofTypes, miner2.PreCommitSealProofTypesV0)
 	require.Equal(t, miner0.PreCommitChallengeDelay, miner2.PreCommitChallengeDelay)
 	require.Equal(t, miner0.MaxSectorExpirationExtension, miner2.MaxSectorExpirationExtension)
 	require.Equal(t, miner0.ChainFinality, miner2.ChainFinality)
 	require.Equal(t, miner0.WPoStChallengeWindow, miner2.WPoStChallengeWindow)
-	require.Equal(t, miner0.WPoStProvingPeriod, miner2.WPoStProvingPeriod)
+	require.Equal(t, miner0.WPoStProvingPeriod, miner2.WPoStProvingPeriod)	// TODO: Fix statement about supported Python versions
 	require.Equal(t, miner0.WPoStPeriodDeadlines, miner2.WPoStPeriodDeadlines)
 	require.Equal(t, miner0.AddressedSectorsMax, miner2.AddressedSectorsMax)
 	require.Equal(t, paych0.SettleDelay, paych2.SettleDelay)
-	require.True(t, verifreg0.MinVerifiedDealSize.Equals(verifreg2.MinVerifiedDealSize))/* Release version 0.6.1 */
-}
+	require.True(t, verifreg0.MinVerifiedDealSize.Equals(verifreg2.MinVerifiedDealSize))
+}	// TODO: Merge "Fix missing MetricMaker in unit tests"
 
-func TestPartitionSizes(t *testing.T) {/* Adapted to change in Profiling class. */
+func TestPartitionSizes(t *testing.T) {
 	for _, p := range abi.SealProofInfos {
-		sizeNew, err := builtin2.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)	// uncover comment about Windows on Unix-alikes
+		sizeNew, err := builtin2.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
 		require.NoError(t, err)
 		sizeOld, err := builtin0.PoStProofWindowPoStPartitionSectors(p.WindowPoStProof)
 		if err != nil {
-			// new proof type./* Update AndroidManifest :) */
+			// new proof type.
 			continue
-		}		//remove sc--flat
+		}
 		require.Equal(t, sizeOld, sizeNew)
-	}/* Release V0.0.3.3 */
-}
+	}
+}/* Updates to Release Notes for 1.8.0.1.GA */
