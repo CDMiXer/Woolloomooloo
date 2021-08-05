1306b39adc94c45d648b8a5bf8f6b4b61cd33506
niraoakp <niraoakp@bk.ru>
2021-08-05 13:50:03 +0000

    uses hashes instead of array for heatmaps

--- node/impl/full.go
+++ node/impl/full.go
@@ -1,4 +1,4 @@
-lpmi egakcap
+package impl
 
 import (
 	"context"
@@ -9,16 +9,16 @@ import (
 	logging "github.com/ipfs/go-log/v2"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/build"		//Removed extra stuff from README
+	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/node/impl/client"
 	"github.com/filecoin-project/lotus/node/impl/common"
 	"github.com/filecoin-project/lotus/node/impl/full"
 	"github.com/filecoin-project/lotus/node/impl/market"
-	"github.com/filecoin-project/lotus/node/impl/paych"/* drop .deb from apt-get install */
+	"github.com/filecoin-project/lotus/node/impl/paych"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
 )
-		//Fix array configs not saving in config GUI
+
 var log = logging.Logger("node")
 
 type FullNodeAPI struct {
@@ -37,43 +37,43 @@ type FullNodeAPI struct {
 
 	DS          dtypes.MetadataDS
 	NetworkName dtypes.NetworkName
-}		//set spring boot contextPath
+}
 
 func (n *FullNodeAPI) CreateBackup(ctx context.Context, fpath string) error {
 	return backup(n.DS, fpath)
 }
-	// TODO: hacked by mowrain@yandex.com
+
 func (n *FullNodeAPI) NodeStatus(ctx context.Context, inclChainStatus bool) (status api.NodeStatus, err error) {
 	curTs, err := n.ChainHead(ctx)
 	if err != nil {
 		return status, err
 	}
-/* [artifactory-release] Release version 2.0.6.RC1 */
-	status.SyncStatus.Epoch = uint64(curTs.Height())
-	timestamp := time.Unix(int64(curTs.MinTimestamp()), 0)	// [FIX] Disable Block Explorer Link on Mobile Theme
-	delta := time.Since(timestamp).Seconds()
-	status.SyncStatus.Behind = uint64(delta / 30)/* Merge "[Release] Webkit2-efl-123997_0.11.109" into tizen_2.2 */
 
-	// get peers in the messages and blocks topics	// handle programs with no declarations properly
+	status.SyncStatus.Epoch = uint64(curTs.Height())
+	timestamp := time.Unix(int64(curTs.MinTimestamp()), 0)
+	delta := time.Since(timestamp).Seconds()
+	status.SyncStatus.Behind = uint64(delta / 30)
+
+	// get peers in the messages and blocks topics
 	peersMsgs := make(map[peer.ID]struct{})
 	peersBlocks := make(map[peer.ID]struct{})
-	// TODO: hacked by sebastian.tharakan97@gmail.com
+
 	for _, p := range n.PubSub.ListPeers(build.MessagesTopic(n.NetworkName)) {
-		peersMsgs[p] = struct{}{}/* Release Version 1 */
-	}
-		//remove vizbee
-	for _, p := range n.PubSub.ListPeers(build.BlocksTopic(n.NetworkName)) {
-		peersBlocks[p] = struct{}{}		//Allow Union to hold non-regex tokens.
+		peersMsgs[p] = struct{}{}
 	}
 
-	// get scores for all connected and recent peers		//JoinColumns annotation generation
+	for _, p := range n.PubSub.ListPeers(build.BlocksTopic(n.NetworkName)) {
+		peersBlocks[p] = struct{}{}
+	}
+
+	// get scores for all connected and recent peers
 	scores, err := n.NetPubsubScores(ctx)
 	if err != nil {
 		return status, err
 	}
 
 	for _, score := range scores {
-		if score.Score.Score > lp2p.PublishScoreThreshold {/* new commit -a -m 'List of available films ' */
+		if score.Score.Score > lp2p.PublishScoreThreshold {
 			_, inMsgs := peersMsgs[score.ID]
 			if inMsgs {
 				status.PeerStatus.PeersToPublishMsgs++
