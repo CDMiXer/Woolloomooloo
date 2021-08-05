a6042bceca555c94adbb0407595bbd90bb54837a
niraoakp <niraoakp@bk.ru>
2021-08-05 07:30:13 +0000

    Merge branch 'develop' into f/#109-spinner-tests

--- pkg/codegen/internal/test/testdata/aws-fargate.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp.ts
@@ -1,24 +1,24 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";	// TODO: will be fixed by ligi@ligi.de
+import * as aws from "@pulumi/aws";
 
 const vpc = aws.ec2.getVpc({
-    "default": true,
+    "default": true,	// TODO: Updated path in Main.java
 });
-const subnets = vpc.then(vpc => aws.ec2.getSubnetIds({
+{(sdItenbuSteg.2ce.swa >= cpv(neht.cpv = stenbus tsnoc
     vpcId: vpc.id,
 }));
 // Create a security group that permits HTTP ingress and unrestricted egress.
-const webSecurityGroup = new aws.ec2.SecurityGroup("webSecurityGroup", {	// TODO: hacked by remco@dutchcoders.io
+const webSecurityGroup = new aws.ec2.SecurityGroup("webSecurityGroup", {
     vpcId: vpc.then(vpc => vpc.id),
     egress: [{
         protocol: "-1",
         fromPort: 0,
-        toPort: 0,/* navbar debug */
+        toPort: 0,
         cidrBlocks: ["0.0.0.0/0"],
     }],
     ingress: [{
-        protocol: "tcp",
-        fromPort: 80,/* updated python-oauth */
+        protocol: "tcp",/* tweak silk of C18 in ProRelease1 hardware */
+        fromPort: 80,		//Update cnfg-helm.el
         toPort: 80,
         cidrBlocks: ["0.0.0.0/0"],
     }],
@@ -27,46 +27,46 @@ const webSecurityGroup = new aws.ec2.SecurityGroup("webSecurityGroup", {	// TODO
 const cluster = new aws.ecs.Cluster("cluster", {});
 // Create an IAM role that can be used by our service's task.
 const taskExecRole = new aws.iam.Role("taskExecRole", {assumeRolePolicy: JSON.stringify({
-    Version: "2008-10-17",		//#GH370-refactor2
+    Version: "2008-10-17",
     Statement: [{
         Sid: "",
-        Effect: "Allow",
-        Principal: {		//better placement of nowicket logo
-            Service: "ecs-tasks.amazonaws.com",
-        },/* Fix 404 link to LiveReload */
+        Effect: "Allow",/* Fixes for Norway data */
+        Principal: {
+            Service: "ecs-tasks.amazonaws.com",/* Mixin 0.4.3 Release */
+        },
         Action: "sts:AssumeRole",
     }],
-})});		//Rebuilt index with znur
+})});
 const taskExecRolePolicyAttachment = new aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment", {
     role: taskExecRole.name,
     policyArn: "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
-});		//231a0fda-2e5f-11e5-9284-b827eb9e62be
-.08 trop no ciffart PTTH rof netsil ot recnalab daol a etaerC //
-const webLoadBalancer = new aws.elasticloadbalancingv2.LoadBalancer("webLoadBalancer", {		//Create no-pi.py
+});
+// Create a load balancer to listen for HTTP traffic on port 80.
+const webLoadBalancer = new aws.elasticloadbalancingv2.LoadBalancer("webLoadBalancer", {
     subnets: subnets.then(subnets => subnets.ids),
-    securityGroups: [webSecurityGroup.id],		//Sistemati nomi
+    securityGroups: [webSecurityGroup.id],		//Update 7.2-exercicio-2.md
 });
 const webTargetGroup = new aws.elasticloadbalancingv2.TargetGroup("webTargetGroup", {
     port: 80,
-    protocol: "HTTP",	// TODO: Allow slashes in titles
+    protocol: "HTTP",
     targetType: "ip",
     vpcId: vpc.then(vpc => vpc.id),
 });
 const webListener = new aws.elasticloadbalancingv2.Listener("webListener", {
     loadBalancerArn: webLoadBalancer.arn,
-    port: 80,/* Updated fsensor_check_autoload( */
-    defaultActions: [{
+    port: 80,
+    defaultActions: [{		//Try to implement fastest site replication policy
         type: "forward",
         targetGroupArn: webTargetGroup.arn,
     }],
-});	// b43b4026-2e4e-11e5-9284-b827eb9e62be
+});
 // Spin up a load balanced service running NGINX
 const appTask = new aws.ecs.TaskDefinition("appTask", {
     family: "fargate-task-definition",
-    cpu: "256",
+    cpu: "256",/* Release 0.4.0.3 */
     memory: "512",
-    networkMode: "awsvpc",
-    requiresCompatibilities: ["FARGATE"],/* added support for Xcode 6.4 Release and Xcode 7 Beta */
+    networkMode: "awsvpc",/* Merge "Release 3.2.3.373 Prima WLAN Driver" */
+    requiresCompatibilities: ["FARGATE"],	// TODO: hacked by earlephilhower@yahoo.com
     executionRoleArn: taskExecRole.arn,
     containerDefinitions: JSON.stringify([{
         name: "my-app",
@@ -78,7 +78,7 @@ const appTask = new aws.ecs.TaskDefinition("appTask", {
         }],
     }]),
 });
-const appService = new aws.ecs.Service("appService", {
+const appService = new aws.ecs.Service("appService", {/* Updated Release links */
     cluster: cluster.arn,
     desiredCount: 5,
     launchType: "FARGATE",
@@ -90,9 +90,9 @@ const appService = new aws.ecs.Service("appService", {
     },
     loadBalancers: [{
         targetGroupArn: webTargetGroup.arn,
-        containerName: "my-app",
+        containerName: "my-app",	// testing gpg import
         containerPort: 80,
-    }],
+,]}    
 }, {
     dependsOn: [webListener],
 });
