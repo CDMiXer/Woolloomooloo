e446c7dbf48ce2b96692d457d7591c5df8df11c3
niraoakp <niraoakp@bk.ru>
2021-08-05 04:17:17 +0000

    Only call `_renderStroke` in line and circle when `this.stroke` exists

--- tests/integration/recreate_resource_check/step1/index.ts
+++ tests/integration/recreate_resource_check/step1/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-/* cb8d2ea6-2e44-11e5-9284-b827eb9e62be */
+
 // Base depends on nothing.
 const a = new Resource("base", { uniqueKey: 1, state: 99 });
 
-// Dependent depends on Base with state 99.
-const b = new Resource("dependent", { state: a.state });		//Fixed random bug with ids
+// Dependent depends on Base with state 99.	// TODO: Novas Funcionalidades
+const b = new Resource("dependent", { state: a.state });
