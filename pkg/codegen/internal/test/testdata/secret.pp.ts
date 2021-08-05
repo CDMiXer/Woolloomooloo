e2b5a72ed861b07f5d73dfa6fc9ef65f0b36fcff
niraoakp <niraoakp@bk.ru>
2021-08-05 09:37:49 +0000

    forgot to commit this comment

--- pkg/codegen/internal/test/testdata/secret.pp.ts
+++ pkg/codegen/internal/test/testdata/secret.pp.ts
@@ -1,4 +1,4 @@
-import * as pulumi from "@pulumi/pulumi";/* 0.7.0 Release changelog */
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
 const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});
