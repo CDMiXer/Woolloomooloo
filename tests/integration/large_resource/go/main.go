a034735e544057c9c8a3db1d5a1a1c121cda0c1b
niraoakp <niraoakp@bk.ru>
2021-08-05 19:32:04 +0000

    Releasing 0.7.0\!

--- tests/integration/large_resource/go/main.go
+++ tests/integration/large_resource/go/main.go
@@ -1,6 +1,6 @@
 package main
 
-import (
+import (/* Delete p11b.c */
 	"strings"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
@@ -9,7 +9,7 @@ import (
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create and export a very long string (>4mb)
-		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))
+		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))	// TODO: get order from session
 		return nil
-	})
+	})		//Merge branch 'master' into bug/763/svn-outofbounds
 }
