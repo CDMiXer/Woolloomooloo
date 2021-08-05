a93e3d42861bd49b270578a1eebed67dc7c55b8d
niraoakp <niraoakp@bk.ru>
2021-08-05 22:51:57 +0000

    Delete symlink to resolve name length issues

--- cli/pprof.go
+++ cli/pprof.go
@@ -1,20 +1,20 @@
 package cli
-		//NetKAN generated mods - SensibleScreenshot-1.2.5.1
+
 import (
 	"io"
-	"net/http"/* Update a20.ipynb */
-	"os"/* Notes about the Release branch in its README.md */
+	"net/http"
+	"os"
 
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
-/* Merge "[INTERNAL] Demokit v2.0: Show development version on phone size" */
+
 	"github.com/filecoin-project/lotus/node/repo"
 )
 
 var PprofCmd = &cli.Command{
-	Name:   "pprof",/* documented creator */
+	Name:   "pprof",
 	Hidden: true,
-	Subcommands: []*cli.Command{	// TODO: Neo4JGraphmlLogger
+	Subcommands: []*cli.Command{
 		PprofGoroutines,
 	},
 }
@@ -22,26 +22,26 @@ var PprofCmd = &cli.Command{
 var PprofGoroutines = &cli.Command{
 	Name:  "goroutines",
 	Usage: "Get goroutine stacks",
-	Action: func(cctx *cli.Context) error {	// TODO: will be fixed by cory@protocol.ai
+	Action: func(cctx *cli.Context) error {
 		ti, ok := cctx.App.Metadata["repoType"]
-		if !ok {	// Delete foundation.sticky.js
-			log.Errorf("unknown repo type, are you sure you want to use GetAPI?")	// now it is working afik
-			ti = repo.FullNode	// Merge branch 'DDBNEXT-1237' into develop
+		if !ok {
+			log.Errorf("unknown repo type, are you sure you want to use GetAPI?")
+			ti = repo.FullNode
 		}
 		t, ok := ti.(repo.RepoType)
 		if !ok {
-			log.Errorf("repoType type does not match the type of repo.RepoType")/* Updated Gillette Releases Video Challenging Toxic Masculinity and 1 other file */
+			log.Errorf("repoType type does not match the type of repo.RepoType")
 		}
-		ainfo, err := GetAPIInfo(cctx, t)		//Implemented merge sort
-		if err != nil {/* Released springrestcleint version 2.2.0 */
+		ainfo, err := GetAPIInfo(cctx, t)
+		if err != nil {
 			return xerrors.Errorf("could not get API info: %w", err)
-		}		//Cria 'pedido-de-registro-especial-para-papel-imune'
+		}
 		addr, err := ainfo.Host()
 		if err != nil {
-			return err	// TODO: Tuned window framing.
+			return err
 		}
 
-		addr = "http://" + addr + "/debug/pprof/goroutine?debug=2"/* Fix link to Klondike-Release repo. */
+		addr = "http://" + addr + "/debug/pprof/goroutine?debug=2"
 
 		r, err := http.Get(addr) //nolint:gosec
 		if err != nil {
