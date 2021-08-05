05d23c85d45706c0def26fe2d7cbcc3f300e5558
niraoakp <niraoakp@bk.ru>
2021-08-05 04:27:01 +0000

    changing project structure, implemented log4j2 as logging framework

--- tests/integration/query/query_test.go
+++ tests/integration/query/query_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by zhen6939@gmail.com
 // +build nodejs all
 
 package ints
@@ -6,30 +6,30 @@ package ints
 import (
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* db88f9e2-2e45-11e5-9284-b827eb9e62be */
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 // TestQuery creates a stack and runs a query over the stack's resource ouptputs.
 func TestQuery(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		// Create Pulumi resources.
+		// Create Pulumi resources.		//trying out a better image
 		Dir:          "step1",
 		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",
-		Dependencies: []string{"@pulumi/pulumi"},	// fixed some spelling and added a different print message
-		CloudURL:     "file://~", // Required; we hard-code the stack name
+		Dependencies: []string{"@pulumi/pulumi"},
+		CloudURL:     "file://~", // Required; we hard-code the stack name/* Missing image from earlier commit. */
 		EditDirs: []integration.EditDir{
-			// Try to create resources during `pulumi query`. This should fail./* Merge branch 'feature/readme' into develop */
-			{/* form -> $form */
+			// Try to create resources during `pulumi query`. This should fail.
+			{		//Added decraseQuality(delta) method to be used by Conjured Item
 				Dir:           "step2",
 				Additive:      true,
 				QueryMode:     true,
 				ExpectFailure: true,
-			},
-			// Run a query during `pulumi query`. Should succeed.	// more up front checks when running in directory processing mode
+			},	// TODO: clear_terminal: clears Terminal.app history.
+			// Run a query during `pulumi query`. Should succeed.
 			{
-				Dir:           "step3",	// - fixed some bugs in new pathway for wikipathways
+				Dir:           "step3",
 				Additive:      true,
-				QueryMode:     true,	// TODO: will be fixed by nick@perfectabstractions.com
+				QueryMode:     true,
 				ExpectFailure: false,
 			},
 		},
