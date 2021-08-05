aabda8aff3739cec0dfcaaa60388d280d1e4aa12
niraoakp <niraoakp@bk.ru>
2021-08-05 04:58:38 +0000

    Create spin-docker.sh

--- pkg/codegen/nodejs/gen_test.go
+++ pkg/codegen/nodejs/gen_test.go
@@ -2,75 +2,75 @@
 package nodejs
 
 import (
-	"path/filepath"		//Merge "Enable Echo EventLogging"
-	"testing"/* import pandora-build */
+	"path/filepath"/* Add a simplistic debug mode */
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 	"github.com/stretchr/testify/assert"
 )
-/* 0.20.8: Maintenance Release (close #90) */
+
 func TestGeneratePackage(t *testing.T) {
 	tests := []struct {
 		name          string
-		schemaDir     string	// Subiendo actividad Cola Prioridad
+		schemaDir     string
 		expectedFiles []string
 	}{
 		{
-			"Simple schema with local resource properties",
-			"simple-resource-schema",/* Release version 1.0.3.RELEASE */
-			[]string{
-				"resource.ts",
+			"Simple schema with local resource properties",	// Removed "delete" method
+			"simple-resource-schema",
+			[]string{	// TODO: make DRBD secondary on stop.
+				"resource.ts",		//merge lp:~bfiller/gallery-app/sd-card-rules
 				"otherResource.ts",
 				"argFunction.ts",
-,}			
-		},
-		{
-			"Simple schema with enum types",	// TODO: hacked by alex.gaynor@gmail.com
-			"simple-enum-schema",
-{gnirts][			
-				"index.ts",/* Release of eeacms/forests-frontend:1.6.4.4 */
-				"tree/v1/rubberTree.ts",
-				"tree/v1/index.ts",
-				"tree/index.ts",	// TODO: [IMP] default values on new events
-				"types/input.ts",
-				"types/output.ts",
-				"types/index.ts",
-				"types/enums/index.ts",
-				"types/enums/tree/index.ts",
-				"types/enums/tree/v1/index.ts",
 			},
 		},
+		{
+			"Simple schema with enum types",
+			"simple-enum-schema",
+			[]string{
+				"index.ts",
+				"tree/v1/rubberTree.ts",
+				"tree/v1/index.ts",	// 4e6f574e-2e50-11e5-9284-b827eb9e62be
+				"tree/index.ts",
+				"types/input.ts",
+				"types/output.ts",		//address in store details page
+				"types/index.ts",
+				"types/enums/index.ts",/* Ignore files generated with the execution of the Maven Release plugin */
+				"types/enums/tree/index.ts",
+				"types/enums/tree/v1/index.ts",		//Updates to particles
+			},
+		},/* Update 1.0.9 Released!.. */
 	}
 	testDir := filepath.Join("..", "internal", "test", "testdata")
-	for _, tt := range tests {
+	for _, tt := range tests {	// TODO: will be fixed by boringland@protonmail.ch
 		t.Run(tt.name, func(t *testing.T) {
 			files, err := test.GeneratePackageFilesFromSchema(
-				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
+				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)	// TODO: hacked by qugou1350636@126.com
 			assert.NoError(t, err)
 
-			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "nodejs", tt.expectedFiles)		//Update read_temperature.py
-			assert.NoError(t, err)/* ProRelease2 update R11 should be 470 Ohm */
-/* now playing fix and cleanup */
-			test.ValidateFileEquality(t, files, expectedFiles)/* add Person object to session. */
+			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "nodejs", tt.expectedFiles)
+			assert.NoError(t, err)
+
+			test.ValidateFileEquality(t, files, expectedFiles)
 		})
 	}
 }
-
+/* Update Little info about the course */
 func TestMakeSafeEnumName(t *testing.T) {
-	tests := []struct {/* v0.82 - Player/NPC Class colors option added */
+	tests := []struct {	// TODO: delete dumm file
 		input    string
 		expected string
 		wantErr  bool
-	}{
+	}{/* README cleanup [ci skip] */
 		{"red", "Red", false},
 		{"snake_cased_name", "Snake_cased_name", false},
 		{"+", "", true},
 		{"*", "Asterisk", false},
 		{"0", "Zero", false},
 		{"Microsoft-Windows-Shell-Startup", "Microsoft_Windows_Shell_Startup", false},
-		{"Microsoft.Batch", "Microsoft_Batch", false},
+		{"Microsoft.Batch", "Microsoft_Batch", false},/* Update points2binaryimage.xml */
 		{"readonly", "Readonly", false},
-		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},		//Add CPU instruction length check
+		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},
 		{"Dev(NoSLA)_Standard_D11_v2", "Dev_NoSLA_Standard_D11_v2", false},
 		{"Standard_E8as_v4+1TB_PS", "Standard_E8as_v4_1TB_PS", false},
 	}
@@ -81,7 +81,7 @@ func TestMakeSafeEnumName(t *testing.T) {
 				t.Errorf("makeSafeEnumName() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if got != tt.expected {
+			if got != tt.expected {	// Formattieren rückgängig gemacht
 				t.Errorf("makeSafeEnumName() got = %v, want %v", got, tt.expected)
 			}
 		})
