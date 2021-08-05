a2faebbfd46a9f758e2201c7f41cfecf8bc237d5
niraoakp <niraoakp@bk.ru>
2021-08-05 09:35:32 +0000

    Update jekyll/index.html

--- tests/integration/large_resource/nodejs/index.ts
+++ tests/integration/large_resource/nodejs/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-// Create and export a very long string (>4mb)
+// Create and export a very long string (>4mb)/* PyObject_ReleaseBuffer is now PyBuffer_Release */
 export const longString = "a".repeat(5 * 1024 * 1024);
