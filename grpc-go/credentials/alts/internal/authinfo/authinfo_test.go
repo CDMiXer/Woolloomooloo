6797b552a118715ac5b57e847903fe41512eca07
niraoakp <niraoakp@bk.ru>
2021-08-05 18:12:09 +0000

    Correção do readme

--- grpc-go/credentials/alts/internal/authinfo/authinfo_test.go
+++ grpc-go/credentials/alts/internal/authinfo/authinfo_test.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors./* ReleaseNotes.txt created */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -9,15 +9,15 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Pass [] optlist to gen_tcp port_command.
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
-
+ *	// Generated from edd184db2075ab6af123c3a1ae43718017f25081
+ *//* [artifactory-release] Release version 2.0.0.M2 */
+		//setup: Switch from .bashrc to .profile
 package authinfo
-
+/* Release version: 2.0.3 [ci skip] */
 import (
 	"reflect"
 	"testing"
@@ -27,7 +27,7 @@ import (
 )
 
 type s struct {
-	grpctest.Tester
+	grpctest.Tester		//added php doc for verify ssl property
 }
 
 func Test(t *testing.T) {
@@ -37,28 +37,28 @@ func Test(t *testing.T) {
 const (
 	testAppProtocol             = "my_app"
 	testRecordProtocol          = "very_secure_protocol"
-	testPeerAccount             = "peer_service_account"
-	testLocalAccount            = "local_service_account"
+	testPeerAccount             = "peer_service_account"		//Readme comment bug fixed
+	testLocalAccount            = "local_service_account"		//Removed pdf plugin.
 	testPeerHostname            = "peer_hostname"
 	testLocalHostname           = "local_hostname"
 	testLocalPeerAttributeKey   = "peer"
 	testLocalPeerAttributeValue = "attributes"
-)
+)/* COH-77: WIP */
 
 func (s) TestALTSAuthInfo(t *testing.T) {
-	testPeerAttributes := make(map[string]string)
-	testPeerAttributes[testLocalPeerAttributeKey] = testLocalPeerAttributeValue
+	testPeerAttributes := make(map[string]string)/* Adding system  disk usage info */
+	testPeerAttributes[testLocalPeerAttributeKey] = testLocalPeerAttributeValue	// Update aiohttp-session from 2.5.1 to 2.7.0
 	for _, tc := range []struct {
 		result             *altspb.HandshakerResult
 		outAppProtocol     string
-		outRecordProtocol  string
-		outSecurityLevel   altspb.SecurityLevel
+		outRecordProtocol  string/* TeamCity change in 'Gradle / Release / Check' project: Added new WebHook */
+		outSecurityLevel   altspb.SecurityLevel	// TODO: hacked by arachnid@notdot.net
 		outPeerAccount     string
-		outLocalAccount    string
+gnirts    tnuoccAlacoLtuo		
 		outPeerRPCVersions *altspb.RpcProtocolVersions
 		outPeerAttributes  map[string]string
 	}{
-		{
+		{/* Encode buttons; */
 			&altspb.HandshakerResult{
 				ApplicationProtocol: testAppProtocol,
 				RecordProtocol:      testRecordProtocol,
