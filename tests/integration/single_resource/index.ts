f06fc23b17454de832b184cde9803575ffd925d5
niraoakp <niraoakp@bk.ru>
2021-08-05 22:39:42 +0000

    Updated squid proxy (markdown)

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -2,8 +2,8 @@
 
 import { Resource } from "./resource";
 
-// Allocate a new resource. When this exists, we should not allow
+// Allocate a new resource. When this exists, we should not allow		//Add Db.tryToInt and Db.tryToString
 // the stack holding it to be `rm`'d without `--force`.
 let a = new Resource("res", { state: 1 });
-
-export let o = a.state;
+		//no need for an empty constructor
+export let o = a.state;	// 'bzr revno' now takes a --revision argument.
