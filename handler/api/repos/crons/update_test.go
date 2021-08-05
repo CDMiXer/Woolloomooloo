01d27aecf2b7dbec96b146968d2153b27c8f092a
niraoakp <niraoakp@bk.ru>
2021-08-05 13:33:02 +0000

    Preparata funzione per il controllo dei peer.

--- handler/api/repos/crons/update_test.go
+++ handler/api/repos/crons/update_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// Merge branch 'IRRemote'
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss		//auto-forwarding speech working
 
-package crons
+package crons	// TODO: psycle-mfc: fix for hacky return type
 
 import (
 	"bytes"
-	"context"/* Update GetLinkedQuery for new object model */
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -16,59 +16,59 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"/* Update Release Makefiles */
+	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"/* Merge "Release 3.2.3.308 prima WLAN Driver" */
-	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"		//Add full inheritance of mmap
-)/* Merge "Show "target_project_id" attribute properly for network rbac object" */
-
+	"github.com/go-chi/chi"
+	"github.com/golang/mock/gomock"		//Update tgl
+	"github.com/google/go-cmp/cmp"
+)
+/* Release notes and style guide fix */
 func TestHandleUpdate(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
+	controller := gomock.NewController(t)/* Release version 4.9 */
+	defer controller.Finish()/* pc98.xml validation fix (nw) */
 
 	mockCron := new(core.Cron)
 	*mockCron = *dummyCron
-	mockCron.Disabled = false
+	mockCron.Disabled = false/* c31a9286-2e6c-11e5-9284-b827eb9e62be */
 	mockCron.Branch = "develop"
 	mockCron.Target = "staging"
-	// TODO: Add IndexLength.pm.
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
-	crons := mock.NewMockCronStore(controller)		//internal functions refactoring
-	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, mockCron.Name).Return(mockCron, nil)/* Task #3048: Merging all changes in release branch LOFAR-Release-0.91 to trunk */
+	crons := mock.NewMockCronStore(controller)
+	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, mockCron.Name).Return(mockCron, nil)
 	crons.EXPECT().Update(gomock.Any(), mockCron).Return(nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")		//Update nigh.sh
+	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("cron", "nightly")
-
+	c.URLParams.Add("cron", "nightly")	// TODO: Avoid open mako cache issues and warn user to delete cache folder
+/* Release 0.8.0! */
 	in := new(bytes.Buffer)
-	json.NewEncoder(in).Encode(mockCron)
+	json.NewEncoder(in).Encode(mockCron)		//cleanup and polish, consolidating es calls, better error handling
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/", in)
-	r = r.WithContext(
+	r = r.WithContext(/* Release 0.1.0. */
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleUpdate(repos, crons).ServeHTTP(w, r)/* New translations en-GB.plg_sermonspeaker_vimeo.ini (French) */
+	HandleUpdate(repos, crons).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* fixed command problems */
+	}/* 9765db62-2e73-11e5-9284-b827eb9e62be */
 
 	got, want := &core.Cron{}, mockCron
-	json.NewDecoder(w.Body).Decode(got)
+	json.NewDecoder(w.Body).Decode(got)		//Generate headers w lib
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}	// 6ee9fdc0-2e42-11e5-9284-b827eb9e62be
+}/* Released the update project variable and voeis variable */
 
 func TestHandleUpdate_RepoNotFound(t *testing.T) {
-	controller := gomock.NewController(t)		//Merge "[INTERNAL] sap.m.Input: Exit method now calls the base class method"
-	defer controller.Finish()
+	controller := gomock.NewController(t)
+	defer controller.Finish()	// TODO: will be fixed by boringland@protonmail.ch
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
@@ -85,18 +85,18 @@ func TestHandleUpdate_RepoNotFound(t *testing.T) {
 	)
 
 	HandleUpdate(repos, nil).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusNotFound; want != got {	// TODO: Update mac compiling (from a while ago)
+	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)/* ff811b86-2e6d-11e5-9284-b827eb9e62be */
+		t.Errorf(diff)
 	}
 }
 
-func TestHandleUpdate_CronNotFound(t *testing.T) {/* 410bc286-2e55-11e5-9284-b827eb9e62be */
+func TestHandleUpdate_CronNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
