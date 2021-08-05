c6c00ea9788003d8cf1e16558a89d0dd3a24d5f8
niraoakp <niraoakp@bk.ru>
2021-08-05 14:06:52 +0000

    update properties

--- tests/integration/get_resource/dotnet/Program.cs
+++ tests/integration/get_resource/dotnet/Program.cs
@@ -1,19 +1,19 @@
-﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: update api URL
+﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 using System.Collections.Generic;
-using System.Threading.Tasks;		//Minor language improvement
-using Pulumi;	// TODO: will be fixed by fjl@ethereum.org
-using Pulumi.Random;
+using System.Threading.Tasks;
+using Pulumi;
+using Pulumi.Random;	// Run pdflatex twice. Refs #749
 
-class GetResource : CustomResource
-{	// TODO: hacked by xiemengjun@gmail.com
-    [Output("length")]		//Added project file to index
+class GetResource : CustomResource/* fixed bug that led to only first five consumptions to be read in turns */
+{
+    [Output("length")]/* Release for 21.1.0 */
     public Output<int> Length { get; private set; } = null!;
 
-    public GetResource(string urn)
-        : base("unused:unused:unused", "unused", ResourceArgs.Empty, new CustomResourceOptions {Urn = urn})	// TODO: will be fixed by brosner@gmail.com
+    public GetResource(string urn)/* Merge "Certmonger: Make postsave command configurable" */
+        : base("unused:unused:unused", "unused", ResourceArgs.Empty, new CustomResourceOptions {Urn = urn})
     {
-    }
+    }/* gaps with hills */
 }
 
 class Program
@@ -21,15 +21,15 @@ class Program
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
-        {
+        {	// TODO: 7ac3c074-2e54-11e5-9284-b827eb9e62be
             var pet = new RandomPet("cat");
-	// TODO: add fare and leggere.
+
             var getPetLength = pet.Urn.Apply(urn => new GetResource(urn).Length);
-            
+            	// TODO: will be fixed by boringland@protonmail.ch
             return new Dictionary<string, object>
             {
                 {"getPetLength", getPetLength}
             };
-        });		//Update DefaultMethodProvider.java
+        });
     }
-}	// RELEASE 4.0.86.
+}
