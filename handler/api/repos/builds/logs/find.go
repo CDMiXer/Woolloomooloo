5d162eb78a236a3e87604dd441e6c4c326e6ab72
niraoakp <niraoakp@bk.ru>
2021-08-05 10:42:54 +0000

    Fix CIPANGO-140: [Console] the to tag has a bad value in B2B

--- handler/api/repos/builds/logs/find.go
+++ handler/api/repos/builds/logs/find.go
@@ -1,49 +1,49 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: project config
+// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by sjors@sprovoost.nl
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release 0.93.530 */
-// You may obtain a copy of the License at
-//		//Working on a new icon-theming structure
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at		//Create TestDatePicker.cls
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//VBA - Ram Watch - Add separator button
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package logs		//add an about page
+package logs
 
-import (	// TODO: Remove access to deprecated methods
+import (
 	"io"
 	"net/http"
-	"strconv"/* Clean trailing spaces in Google.Apis.Release/Program.cs */
+	"strconv"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// docker-compose logs exits automatically in 1.7 (#122)
+	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)
+)/* Release 0.0.4: support for unix sockets */
 
-// HandleFind returns an http.HandlerFunc that writes the		//Merge "Remove check-requirements from Ceilometer and Aodh"
+// HandleFind returns an http.HandlerFunc that writes the
 // json-encoded logs to the response body.
-func HandleFind(
+func HandleFind(/* add finalize with publish and redis.end() */
 	repos core.RepositoryStore,
-	builds core.BuildStore,	// TODO: hacked by josharian@gmail.com
+	builds core.BuildStore,
 	stages core.StageStore,
 	steps core.StepStore,
 	logs core.LogStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* deleting release tag to change the name and add some missed commits. */
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
-		)
-		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)/* [release] Release 1.0.0-RC2 */
+			name      = chi.URLParam(r, "name")		//Fix dependency on bukkit-bleeding level bukkit
+		)		//Tweak Ohm's Law docs
+		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
 			render.BadRequest(w, err)
-			return		//by voxpelli: Corrected a few notices
-		}
+			return/* Release of eeacms/www:19.12.17 */
+		}		//OPEN-82 fix by codeclimate review
 		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
 		if err != nil {
 			render.BadRequest(w, err)
@@ -56,13 +56,13 @@ func HandleFind(
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)/* Released 2.6.0.5 version to fix issue with carriage returns */
+			render.NotFound(w, err)
 			return
 		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
-			render.NotFound(w, err)
-			return
+			render.NotFound(w, err)		//Add optional parameters to influxdb output README
+			return	// [FIX] auction : YML Test for report corrected
 		}
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
 		if err != nil {
@@ -70,18 +70,18 @@ func HandleFind(
 			return
 		}
 		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)
-		if err != nil {/* [CMD_WINETEST] Sync with Wine Staging 1.7.55. CORE-10536 */
+		if err != nil {	// TODO: hacked by mikeal.rogers@gmail.com
 			render.NotFound(w, err)
 			return
 		}
-		rc, err := logs.Find(r.Context(), step.ID)/* Release 1.7.0 Stable */
+		rc, err := logs.Find(r.Context(), step.ID)	// TODO: Using UUID is safer.
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}
+		}/* Found a legacy typo from skeleton and just fixed it */
 		w.Header().Set("Content-Type", "application/json")
-		io.Copy(w, rc)
-		rc.Close()
+		io.Copy(w, rc)		//gdssplit is now up&running after the module restructuring
+		rc.Close()/* Release 1.0.0.0 */
 
 		// TODO: logs are stored in jsonl format and therefore
 		// need to be converted to valid json.
