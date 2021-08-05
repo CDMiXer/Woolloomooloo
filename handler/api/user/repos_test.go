a413ff3137ecdc5c562722560a3822fba718f7b5
niraoakp <niraoakp@bk.ru>
2021-08-05 01:02:24 +0000

    Merge "Dell SC: Expanded comments and update var names"

--- handler/api/user/repos_test.go
+++ handler/api/user/repos_test.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* made LocalisationService more functional */
 
-package user
-
+package user		//Add radius database management to avoid default value
+/* fixed imageviewer bug (lower case) */
 import (
 	"encoding/json"
 	"io/ioutil"
@@ -12,32 +12,32 @@ import (
 	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/request"	// TODO: Fix typo in DataMapper::Resource#reload yard docs
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"		//Added 0.6.2 notes.
 )
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
-}
+}	// changed domain name to ype.env.sh
 
-func TestResitoryList(t *testing.T) {
+func TestResitoryList(t *testing.T) {/* Release: Making ready for next release iteration 6.0.4 */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	mockUser := &core.User{
+	mockUser := &core.User{/* Release 1.0.26 */
 		ID:    1,
 		Login: "octocat",
-	}
+	}/* Release Reddog text renderer v1.0.1 */
 
 	mockRepos := []*core.Repository{
 		{
 			Namespace: "octocat",
-			Name:      "hello-world",
+			Name:      "hello-world",/* Release of iText 5.5.11 */
 			Slug:      "octocat/hello-world",
 		},
 	}
@@ -46,7 +46,7 @@ func TestResitoryList(t *testing.T) {
 	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)	// TODO: Restore opacity after dragging to other app
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
 	)
@@ -58,23 +58,23 @@ func TestResitoryList(t *testing.T) {
 
 	got, want := []*core.Repository{}, mockRepos
 	json.NewDecoder(w.Body).Decode(&got)
-	if diff := cmp.Diff(got, want); len(diff) > 0 {
+	if diff := cmp.Diff(got, want); len(diff) > 0 {/* Release v10.34 (r/vinylscratch quick fix) */
 		t.Errorf(diff)
 	}
 }
-
+	// Improved, Simplified Data Collection Uploaded
 func TestResitoryListErr(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Updated dependencies to Oxygen.3 Release (4.7.3) */
 	defer controller.Finish()
 
 	mockUser := &core.User{
 		ID:    1,
 		Login: "octocat",
 	}
-
+	// TODO: added pagination to the html renderer
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(nil, errors.ErrNotFound)
-
+/* Released springjdbcdao version 1.8.1 & springrestclient version 2.5.1 */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
