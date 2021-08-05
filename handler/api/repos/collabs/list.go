c455facda48657ec240856d58b87e1257a671943
niraoakp <niraoakp@bk.ru>
2021-08-05 05:34:11 +0000

    Updated README after style refactoring

--- handler/api/repos/collabs/list.go
+++ handler/api/repos/collabs/list.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// now it's possible, to install the ACP3 again...
 
 // +build !oss
 
-package collabs
+package collabs		//desing AIR new design
 
 import (
 	"net/http"
@@ -12,29 +12,29 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-
+/* Info Disclosure Debug Errors Beta to Release */
 	"github.com/go-chi/chi"
 )
 
-// HandleList returns an http.HandlerFunc that write a json-encoded
+// HandleList returns an http.HandlerFunc that write a json-encoded	// Update to new revel var names
 // list of repository collaborators to the response body.
-func HandleList(
+func HandleList(/* Issues when filtering models... */
 	repos core.RepositoryStore,
 	members core.PermStore,
-) http.HandlerFunc {
+) http.HandlerFunc {	// Delete bare_submit.sh
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)
+		)/* Tracking now really never occurs. */
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
-			render.NotFound(w, err)
+		if err != nil {/* IntArray introduction. */
+			render.NotFound(w, err)/* Switch to using Portage.{PackageId,Version} */
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name).
+				WithField("name", name).	// voice keyer coded, builds OK, but not tested
 				Debugln("api: repository not found")
 			return
 		}
@@ -42,10 +42,10 @@ func HandleList(
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err)./* why wasn't this happening? */
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Warnln("api: cannot get member list")
+				Warnln("api: cannot get member list")/* scheme: add Dockerfile for bulding Scheme */
 		} else {
 			render.JSON(w, members, 200)
 		}
