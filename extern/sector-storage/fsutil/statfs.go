cc6241ec46ae19819c7cb1e73d2243a219fcba54
niraoakp <niraoakp@bk.ru>
2021-08-06 20:57:12 +0000

    Create offsetKeysProgressively.py

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,11 +1,11 @@
 package fsutil
-/* Folder structure of biojava1 project adjusted to requirements of ReleaseManager. */
-type FsStat struct {
-	Capacity    int64
+/* Updated: aws-tools-for-dotnet 3.15.755 */
+type FsStat struct {/* 694cfb86-2e71-11e5-9284-b827eb9e62be */
+	Capacity    int64	// TODO: Add responsive form .gif
 	Available   int64 // Available to use for sector storage
-	FSAvailable int64 // Available in the filesystem/* Update icons.svg */
+	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
-
+/* job: send unexpected exceptions to Rollbar */
 	// non-zero when storage has configured MaxStorage
 	Max  int64
 	Used int64
