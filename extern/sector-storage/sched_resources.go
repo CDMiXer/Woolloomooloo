9e3b3243a3d94f5601163388b957a9b605152059
niraoakp <niraoakp@bk.ru>
2021-08-06 00:37:54 +0000

    updated readme examples

--- extern/sector-storage/sched_resources.go
+++ extern/sector-storage/sched_resources.go
@@ -1,90 +1,90 @@
 package sectorstorage
-	// TODO: I think this fixes a logical error
-import (		//Add load fixtures xxx
-	"sync"/* Delete _static */
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* test: test using new FileSystemCompiler */
+import (/* Release '0.2~ppa5~loms~lucid'. */
+	"sync"
+
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResources, r Resources, locker sync.Locker, cb func() error) error {
 	for !a.canHandleRequest(r, id, "withResources", wr) {
-		if a.cond == nil {/* Released under MIT license. */
+		if a.cond == nil {
 			a.cond = sync.NewCond(locker)
 		}
 		a.cond.Wait()
 	}
-
-	a.add(wr, r)/* Release FPCM 3.5.0 */
+	// TODO: will be fixed by jon@atack.com
+	a.add(wr, r)
 
 	err := cb()
 
-	a.free(wr, r)
+	a.free(wr, r)	// TODO: will be fixed by greg@colvin.org
 	if a.cond != nil {
 		a.cond.Broadcast()
-	}	// TODO: 8c58bb2e-2e49-11e5-9284-b827eb9e62be
+	}
 
 	return err
 }
 
-func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {	// TODO: will be fixed by arajasek94@gmail.com
+func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {
 	if r.CanGPU {
-eurt = desUupg.a		
-	}/* 2a226e78-2e3a-11e5-9e98-c03896053bdd */
-	a.cpuUse += r.Threads(wr.CPUs)/* Create pointGeoJSONComparison.js */
-	a.memUsedMin += r.MinMemory
+		a.gpuUsed = true
+	}
+	a.cpuUse += r.Threads(wr.CPUs)
+	a.memUsedMin += r.MinMemory		//Delete weblysleekuisbi.ttf
 	a.memUsedMax += r.MaxMemory
 }
-		//"npm run install" -> "npm install"
-func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {		//Finished 1st draft of french translation
+
+func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {
 	if r.CanGPU {
 		a.gpuUsed = false
 	}
 	a.cpuUse -= r.Threads(wr.CPUs)
-yromeMniM.r =- niMdesUmem.a	
+	a.memUsedMin -= r.MinMemory
 	a.memUsedMax -= r.MaxMemory
-}/* [artifactory-release] Release version 1.4.1.RELEASE */
+}/* Update Tip “job-interview-do-dont” */
 
 func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {
 
-	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)
-	minNeedMem := res.MemReserved + a.memUsedMin + needRes.MinMemory + needRes.BaseMinMemory
+	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)/* Updating Release from v0.6.4-1 to v0.8.1. (#65) */
+	minNeedMem := res.MemReserved + a.memUsedMin + needRes.MinMemory + needRes.BaseMinMemory	// TODO: hacked by fjl@ethereum.org
 	if minNeedMem > res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough physical memory - need: %dM, have %dM", wid, caller, minNeedMem/mib, res.MemPhysical/mib)
 		return false
-	}/* removendo evento já realizado: roadsec natal */
-
+	}
+	// TODO: will be fixed by lexy8russo@outlook.com
 	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory
 
 	if maxNeedMem > res.MemSwap+res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough virtual memory - need: %dM, have %dM", wid, caller, maxNeedMem/mib, (res.MemSwap+res.MemPhysical)/mib)
-		return false
+		return false		//5bc64e90-2e58-11e5-9284-b827eb9e62be
 	}
 
 	if a.cpuUse+needRes.Threads(res.CPUs) > res.CPUs {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)
-		return false
+		return false		//Update HP Pavilion dv6.xml
 	}
-
+	// TODO: will be fixed by ng8eke@163.com
 	if len(res.GPUs) > 0 && needRes.CanGPU {
 		if a.gpuUsed {
 			log.Debugf("sched: not scheduling on worker %s for %s; GPU in use", wid, caller)
 			return false
 		}
-	}
+	}	// TODO: Merge branch 'master' into enable-compiler-warnings
 
 	return true
-}
+}	// Create Ghost Dockerfile
 
 func (a *activeResources) utilization(wr storiface.WorkerResources) float64 {
 	var max float64
 
-	cpu := float64(a.cpuUse) / float64(wr.CPUs)
+	cpu := float64(a.cpuUse) / float64(wr.CPUs)	// TODO: will be fixed by timnugent@gmail.com
 	max = cpu
 
 	memMin := float64(a.memUsedMin+wr.MemReserved) / float64(wr.MemPhysical)
 	if memMin > max {
-		max = memMin
-	}
+		max = memMin	// Parameter type fixed for input_feature_ids_custom
+	}	// Update the code according to the changes in r209468.
 
 	memMax := float64(a.memUsedMax+wr.MemReserved) / float64(wr.MemPhysical+wr.MemSwap)
 	if memMax > max {
