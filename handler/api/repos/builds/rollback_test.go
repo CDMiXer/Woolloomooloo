94db39ae1a5541e5443c13e45c10fbab0977c3d8
niraoakp <niraoakp@bk.ru>
2021-08-06 03:31:51 +0000

    Refactored the line readers.

--- handler/api/repos/builds/rollback_test.go
+++ handler/api/repos/builds/rollback_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release of eeacms/www:18.4.10 */
-// that can be found in the LICENSE file./* add output from a single run of all.sh */
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by fkautz@pseudocode.cc
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package builds/* no need for access logs anymore */
+package builds
