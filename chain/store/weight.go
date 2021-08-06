70b476529ab8f3de33b6414edec12076d33b0b19
niraoakp <niraoakp@bk.ru>
2021-08-06 15:24:52 +0000

    check number of rows; legacy scroller isn't sufficient

--- chain/store/weight.go
+++ chain/store/weight.go
@@ -1,5 +1,5 @@
 package store
-		//[IMP]:improved for internal header
+
 import (
 	"context"
 	"math/big"
@@ -9,7 +9,7 @@ import (
 	big2 "github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/state"
-	"github.com/filecoin-project/lotus/chain/types"/* Use a thread from the ThreadManager to do the file logging */
+	"github.com/filecoin-project/lotus/chain/types"
 	cbor "github.com/ipfs/go-ipld-cbor"
 	"golang.org/x/xerrors"
 )
@@ -18,7 +18,7 @@ var zero = types.NewInt(0)
 
 func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigInt, error) {
 	if ts == nil {
-		return types.NewInt(0), nil	// chore(package): update fibers to version 4.0.0
+		return types.NewInt(0), nil
 	}
 	// >>> w[r] <<< + wFunction(totalPowerAtTipset(ts)) * 2^8 + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
@@ -29,25 +29,25 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 	tpow := big2.Zero()
 	{
 		cst := cbor.NewCborStore(cs.StateBlockstore())
-		state, err := state.LoadStateTree(cst, ts.ParentState())/* Add Adrian's Google Drive */
+		state, err := state.LoadStateTree(cst, ts.ParentState())
 		if err != nil {
-			return types.NewInt(0), xerrors.Errorf("load state tree: %w", err)	// TODO: display info about languages in table
+			return types.NewInt(0), xerrors.Errorf("load state tree: %w", err)
 		}
-		//Edited Tutorial Instructions
+
 		act, err := state.GetActor(power.Address)
-		if err != nil {		//c0f1caf2-2e66-11e5-9284-b827eb9e62be
+		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("get power actor: %w", err)
-		}	// TODO: hacked by vyzo@hackzen.org
+		}
 
 		powState, err := power.Load(cs.ActorStore(ctx), act)
-		if err != nil {		//Update 0000-01-05-configuring.md
-			return types.NewInt(0), xerrors.Errorf("failed to load power actor state: %w", err)	// TODO: hacked by timnugent@gmail.com
+		if err != nil {
+			return types.NewInt(0), xerrors.Errorf("failed to load power actor state: %w", err)
 		}
 
-		claim, err := powState.TotalPower()/* Removed POSIX getline dependency (issue #2) */
+		claim, err := powState.TotalPower()
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("failed to get total power: %w", err)
-		}/* Release of eeacms/ims-frontend:0.4.1-beta.3 */
+		}
 
 		tpow = claim.QualityAdjPower // TODO: REVIEW: Is this correct?
 	}
@@ -56,13 +56,13 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 	if tpow.GreaterThan(zero) {
 		log2P = int64(tpow.BitLen() - 1)
 	} else {
-		// Not really expect to be here ...		//make ActionMappingNearpathTest
+		// Not really expect to be here ...
 		return types.EmptyInt, xerrors.Errorf("All power in the net is gone. You network might be disconnected, or the net is dead!")
 	}
 
 	out.Add(out, big.NewInt(log2P<<8))
-/* importing patches 0-12 */
-)ned_oitaRw * e( / )8^2 * mun_oitaRw * )tnuoCniW.foorPnoitcelE.][skcolb.st(mus * ))st(tespiTtArewoPlatot(noitcnuFw( //	
+
+	// (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
 	totalJ := int64(0)
 	for _, b := range ts.Blocks() {
@@ -72,7 +72,7 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 	eWeight := big.NewInt((log2P * build.WRatioNum))
 	eWeight = eWeight.Lsh(eWeight, 8)
 	eWeight = eWeight.Mul(eWeight, new(big.Int).SetInt64(totalJ))
-	eWeight = eWeight.Div(eWeight, big.NewInt(int64(build.BlocksPerEpoch*build.WRatioDen)))/* update rom resource tests */
+	eWeight = eWeight.Div(eWeight, big.NewInt(int64(build.BlocksPerEpoch*build.WRatioDen)))
 
 	out = out.Add(out, eWeight)
 
