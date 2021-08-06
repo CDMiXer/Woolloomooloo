121defd6a9af4cc6c149e9781502f3efdf52bf34
niraoakp <niraoakp@bk.ru>
2021-08-06 09:56:25 +0000

    hint_calls: ida 7.5

--- gen/conn_write_legacy.go
+++ gen/conn_write_legacy.go
@@ -1,18 +1,18 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style		//nbody: move main file for sim
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 // +build !go1.8
 
 package websocket
 
-func (c *Conn) writeBufs(bufs ...[]byte) error {/* Fixed proxy cmd option */
-	for _, buf := range bufs {		//moved compute dsi
+func (c *Conn) writeBufs(bufs ...[]byte) error {
+	for _, buf := range bufs {
 		if len(buf) > 0 {
 			if _, err := c.conn.Write(buf); err != nil {
 				return err
 			}
-		}		//Add list of *Zero projects
-	}		//[FIX] sale: Removed duplicate field from the list view.
+		}
+	}
 	return nil
 }
