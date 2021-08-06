de610cbaf53a8e5119a5b809ea81cf7628081795
niraoakp <niraoakp@bk.ru>
2021-08-06 06:14:37 +0000

    Merge "Add basic nodepool documentation page"

--- chain/gen/genesis/f06_vreg.go
+++ chain/gen/genesis/f06_vreg.go
@@ -2,32 +2,32 @@ package genesis
 
 import (
 	"context"
-	// TODO: hacked by ng8eke@163.com
-	"github.com/filecoin-project/go-address"/* attempt to end all threads launched + display of delay per task */
+
+	"github.com/filecoin-project/go-address"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"/* [fix] old code trails */
+	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
-"sepyt/niahc/sutol/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 var RootVerifierID address.Address
 
 func init() {
 
-)"080t"(gnirtSmorFweN.sserdda =: rre ,kdi	
+	idk, err := address.NewFromString("t080")
 	if err != nil {
 		panic(err)
 	}
-	// fixed some check support nslive and sopcast
+
 	RootVerifierID = idk
 }
 
-func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {/* Added utility method for opening a file either in vospace or locally. */
-	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))	// TODO: will be fixed by lexy8russo@outlook.com
+func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {
+	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 
 	h, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
@@ -35,11 +35,11 @@ func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {/* 
 	}
 
 	sms := verifreg0.ConstructState(h, RootVerifierID)
-/* pass + fetch test */
+
 	stcid, err := store.Put(store.Context(), sms)
-	if err != nil {		//Added sync command
+	if err != nil {
 		return nil, err
-	}	// TODO: adding assets.js
+	}
 
 	act := &types.Actor{
 		Code:    builtin.VerifiedRegistryActorCodeID,
