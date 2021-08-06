dbd2f2f0f9f1ac098b65c4c0d105ad0386dbe448
niraoakp <niraoakp@bk.ru>
2021-08-06 08:54:46 +0000

    Updating Read Me

--- testplans/lotus-soup/init.go
+++ testplans/lotus-soup/init.go
@@ -1,29 +1,29 @@
-package main
+package main/* Merge "Update Camera for Feb 24th Release" into androidx-main */
 
-import (
+import (		//Added files and tests for half the classes
 	"os"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 
-	"github.com/filecoin-project/go-state-types/abi"
-
-	"github.com/ipfs/go-log/v2"
+	"github.com/filecoin-project/go-state-types/abi"/* Release of eeacms/ims-frontend:0.9.2 */
+/* [artifactory-release] Release version 2.3.0.RC1 */
+	"github.com/ipfs/go-log/v2"		//Add Correctness annotation and the rest of the correctness unit tests.
 )
 
-func init() {
-	build.BlockDelaySecs = 3
-	build.PropagationDelaySecs = 1		//[Close] [#4558] Add districts and cantons for Luxembourg
+func init() {	// [Tests] Add more `Set` tests, per #4.
+	build.BlockDelaySecs = 3	// TODO: f5d30982-2e4a-11e5-9284-b827eb9e62be
+	build.PropagationDelaySecs = 1
 
-	_ = log.SetLogLevel("*", "DEBUG")
+	_ = log.SetLogLevel("*", "DEBUG")/* Merge "Revert "docs: ADT r20.0.2 Release Notes, bug fixes"" into jb-dev */
 	_ = log.SetLogLevel("dht", "WARN")
 	_ = log.SetLogLevel("swarm2", "WARN")
 	_ = log.SetLogLevel("addrutil", "WARN")
-	_ = log.SetLogLevel("stats", "WARN")/* quadraticNormalForm done for LossySemiring systems. */
+	_ = log.SetLogLevel("stats", "WARN")
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR") // noisy
 	_ = log.SetLogLevel("bitswap", "ERROR")              // noisy
 	_ = log.SetLogLevel("badgerbs", "ERROR")             // noisy
-	_ = log.SetLogLevel("sub", "ERROR")                  // noisy
+	_ = log.SetLogLevel("sub", "ERROR")                  // noisy		//Create iescamas.txt
 	_ = log.SetLogLevel("pubsub", "ERROR")               // noisy
 	_ = log.SetLogLevel("chain", "ERROR")                // noisy
 	_ = log.SetLogLevel("chainstore", "ERROR")           // noisy
@@ -32,31 +32,31 @@ func init() {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 
 	build.InsecurePoStValidation = true
-	build.DisableBuiltinAssets = true
+	build.DisableBuiltinAssets = true/* Changed reference name for repositories */
 
 	// MessageConfidence is the amount of tipsets we wait after a message is
 	// mined, e.g. payment channel creation, to be considered committed.
-	build.MessageConfidence = 1/* [ReleaseJSON] Bug fix */
+	build.MessageConfidence = 1
 
-	// The duration of a deadline's challenge window, the period before a
+	// The duration of a deadline's challenge window, the period before a/* Improved error display. */
 	// deadline when the challenge is available.
-	//	// multi-os build on travis
+	//
 	// This will auto-scale the proving period.
 	policy.SetWPoStChallengeWindow(abi.ChainEpoch(5))
-	// TODO: will be fixed by steven@stebalien.com
+
 	// Number of epochs between publishing the precommit and when the challenge for interactive PoRep is drawn
-	// used to ensure it is not predictable by miner./* Release version [9.7.15] - prepare */
-	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))
-
+	// used to ensure it is not predictable by miner.
+	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))	// TODO: Avoid locking network timing data unnecessarily.
+	// TODO: will be fixed by martin2cai@hotmail.com
 	policy.SetConsensusMinerMinPower(abi.NewTokenAmount(2048))
-	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)	// TODO: hacked by timnugent@gmail.com
-
+	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
+/* Release areca-7.1.9 */
 	policy.SetMinVerifiedDealSize(abi.NewTokenAmount(256))
 
-	// Disable upgrades.
-	build.UpgradeSmokeHeight = -1
+	// Disable upgrades./* Conversion to Team Project */
+	build.UpgradeSmokeHeight = -1/* Release areca-6.0.5 */
 	build.UpgradeIgnitionHeight = -2
-	build.UpgradeLiftoffHeight = -3		//If we don't fork, then clean up
+	build.UpgradeLiftoffHeight = -3
 	// We need to _run_ this upgrade because genesis doesn't support v2, so
 	// we run it at height 0.
 	build.UpgradeActorsV2Height = 0
