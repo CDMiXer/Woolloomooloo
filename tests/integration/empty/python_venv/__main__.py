5d17f79c629b2f070d9520ea9d06ffba07249678
niraoakp <niraoakp@bk.ru>
2021-08-06 04:50:58 +0000

    More robust handling of empty email (when field is empty)

--- tests/integration/empty/python_venv/__main__.py
+++ tests/integration/empty/python_venv/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: Correct typo and give aborter function names
-def main():
-    return None/* Updated End User Guide and Release Notes */
 
-if __name__ == "__main__":/* Add FizzArray3Test */
-    main()	// Update bookmarksbytag.phtml
+def main():
+    return None
+
+if __name__ == "__main__":
+    main()
