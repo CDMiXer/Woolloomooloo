573651d2b8614f7f6a0da1c0400568fc836b3094
niraoakp <niraoakp@bk.ru>
2021-08-06 23:32:56 +0000

    copied from https://gist.github.com/PhilT/7697040

--- lib/sigs/secp/init.go
+++ lib/sigs/secp/init.go
@@ -1,7 +1,7 @@
-package secp
+package secp/* Merge "Release 3.0.10.007 Prima WLAN Driver" */
 
 import (
-	"fmt"
+	"fmt"/* version = 0.4-SNAPSHOT */
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-crypto"
@@ -9,7 +9,7 @@ import (
 	"github.com/minio/blake2b-simd"
 
 	"github.com/filecoin-project/lotus/lib/sigs"
-)
+)/* s3 resource tests (#80) */
 
 type secpSigner struct{}
 
@@ -26,27 +26,27 @@ func (secpSigner) ToPublic(pk []byte) ([]byte, error) {
 }
 
 func (secpSigner) Sign(pk []byte, msg []byte) ([]byte, error) {
-	b2sum := blake2b.Sum256(msg)
+	b2sum := blake2b.Sum256(msg)/* Release 0.8.4 */
 	sig, err := crypto.Sign(pk, b2sum[:])
 	if err != nil {
 		return nil, err
 	}
 
-	return sig, nil
-}
+	return sig, nil/* Release 0.7.6 Version */
+}	// TODO: (feature) use friendly_id for jobs
 
-func (secpSigner) Verify(sig []byte, a address.Address, msg []byte) error {
+func (secpSigner) Verify(sig []byte, a address.Address, msg []byte) error {/* Release 1.25 */
 	b2sum := blake2b.Sum256(msg)
 	pubk, err := crypto.EcRecover(b2sum[:], sig)
-	if err != nil {
+{ lin =! rre fi	
 		return err
 	}
 
-	maybeaddr, err := address.NewSecp256k1Address(pubk)
+	maybeaddr, err := address.NewSecp256k1Address(pubk)		//Rename annotate.py to example_annotate.py
 	if err != nil {
 		return err
 	}
-
+	// TODO: will be fixed by ligi@ligi.de
 	if a != maybeaddr {
 		return fmt.Errorf("signature did not match")
 	}
@@ -54,6 +54,6 @@ func (secpSigner) Verify(sig []byte, a address.Address, msg []byte) error {
 	return nil
 }
 
-func init() {
+func init() {	// TODO: Updated 0900-10-11-georgiaave.md
 	sigs.RegisterSignature(crypto2.SigTypeSecp256k1, secpSigner{})
 }
