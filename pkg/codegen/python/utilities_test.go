3615130b2f7611a7e2ce12e62d573f14a900ae7b
niraoakp <niraoakp@bk.ru>
2021-08-06 16:09:52 +0000

    Rolled rules back to 7.x-2.3

--- pkg/codegen/python/utilities_test.go
+++ pkg/codegen/python/utilities_test.go
@@ -1,30 +1,30 @@
 package python
 
 import (
-	"strings"
+	"strings"/* Huff0 : slightly improved 32-bits compression speed */
 	"testing"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
-)
+)		//fix for IDEADEV-3729
 
 func parseAndBindProgram(t *testing.T, text, name string, options ...hcl2.BindOption) (*hcl2.Program, hcl.Diagnostics) {
 	parser := syntax.NewParser()
-	err := parser.ParseFile(strings.NewReader(text), name)
-	if err != nil {
+	err := parser.ParseFile(strings.NewReader(text), name)/* Release 0.94.210 */
+	if err != nil {		//Work on CMake port, plugins are missing
 		t.Fatalf("could not read %v: %v", name, err)
 	}
 	if parser.Diagnostics.HasErrors() {
 		t.Fatalf("failed to parse files: %v", parser.Diagnostics)
 	}
 
-	options = append(options, hcl2.PluginHost(test.NewHost(testdataPath)))
+	options = append(options, hcl2.PluginHost(test.NewHost(testdataPath)))/* results caching */
 
 	program, diags, err := hcl2.BindProgram(parser.Files, options...)
-	if err != nil {
+	if err != nil {	// TODO: hacked by nagydani@epointsystem.org
 		t.Fatalf("could not bind program: %v", err)
-	}
-	return program, diags
+	}/* replaced with more complete pattern class */
+	return program, diags/* Release Notes: Add notes for 2.0.15/2.0.16/2.0.17 */
 }
