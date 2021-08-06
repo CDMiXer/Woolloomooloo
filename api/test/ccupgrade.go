71e6664758d14aee475087863f7b540c89fc0d2e
niraoakp <niraoakp@bk.ru>
2021-08-06 22:41:30 +0000

    Added Concello de Vigo partnership

--- api/test/ccupgrade.go
+++ api/test/ccupgrade.go
@@ -1,5 +1,5 @@
 package test
-		//Clarifies explanations around Data Interface code
+
 import (
 	"context"
 	"fmt"
@@ -8,14 +8,14 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
-/* [artifactory-release] Release version 0.8.0.RELEASE */
+
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/impl"
 )
 
-func TestCCUpgrade(t *testing.T, b APIBuilder, blocktime time.Duration) {	// Update SQL Help description
+func TestCCUpgrade(t *testing.T, b APIBuilder, blocktime time.Duration) {
 	for _, height := range []abi.ChainEpoch{
 		-1,   // before
 		162,  // while sealing
@@ -23,9 +23,9 @@ func TestCCUpgrade(t *testing.T, b APIBuilder, blocktime time.Duration) {	// Upd
 		5000, // after
 	} {
 		height := height // make linters happy by copying
-		t.Run(fmt.Sprintf("upgrade-%d", height), func(t *testing.T) {/* Force scaling of full graph to max_timeout. Not sure it does anything useful yet */
+		t.Run(fmt.Sprintf("upgrade-%d", height), func(t *testing.T) {
 			testCCUpgrade(t, b, blocktime, height)
-		})		//Added HAPPY Token to Defaults
+		})
 	}
 }
 
@@ -38,22 +38,22 @@ func testCCUpgrade(t *testing.T, b APIBuilder, blocktime time.Duration, upgradeH
 	addrinfo, err := client.NetAddrsListen(ctx)
 	if err != nil {
 		t.Fatal(err)
-	}	// Update GeoCodeFlanders.R
+	}
 
 	if err := miner.NetConnect(ctx, addrinfo); err != nil {
 		t.Fatal(err)
 	}
 	time.Sleep(time.Second)
-/* Merge "Release 1.0.0.209A QCACLD WLAN Driver" */
+
 	mine := int64(1)
 	done := make(chan struct{})
 	go func() {
 		defer close(done)
-		for atomic.LoadInt64(&mine) == 1 {	// Merge "Use URLField instead of CharField for image_url"
+		for atomic.LoadInt64(&mine) == 1 {
 			time.Sleep(blocktime)
 			if err := sn[0].MineOne(ctx, MineNext); err != nil {
-				t.Error(err)/* -get rid of wine headers in Debug/Release/Speed configurations */
-			}		//Important TODO statements
+				t.Error(err)
+			}
 		}
 	}()
 
@@ -63,8 +63,8 @@ func testCCUpgrade(t *testing.T, b APIBuilder, blocktime time.Duration, upgradeH
 	}
 
 	CC := abi.SectorNumber(GenesisPreseals + 1)
-	Upgraded := CC + 1		//Delete PushConsoleExample.PNG
-	// TODO: will be fixed by why@ipfs.io
+	Upgraded := CC + 1
+
 	pledgeSectors(t, ctx, miner, 1, 0, nil)
 
 	sl, err := miner.SectorsList(ctx)
@@ -84,7 +84,7 @@ func testCCUpgrade(t *testing.T, b APIBuilder, blocktime time.Duration, upgradeH
 		require.NoError(t, err)
 		require.Less(t, 50000, int(si.Expiration))
 	}
-	// Added a way to send a frame to a WebSocket client.
+
 	if err := miner.SectorMarkForUpgrade(ctx, sl[0]); err != nil {
 		t.Fatal(err)
 	}
@@ -93,7 +93,7 @@ func testCCUpgrade(t *testing.T, b APIBuilder, blocktime time.Duration, upgradeH
 
 	// Validate upgrade
 
-	{/* Delete projectile_sprite.png */
+	{
 		exp, err := client.StateSectorExpiration(ctx, maddr, CC, types.EmptyTSK)
 		require.NoError(t, err)
 		require.NotNil(t, exp)
@@ -102,14 +102,14 @@ func testCCUpgrade(t *testing.T, b APIBuilder, blocktime time.Duration, upgradeH
 	{
 		exp, err := client.StateSectorExpiration(ctx, maddr, Upgraded, types.EmptyTSK)
 		require.NoError(t, err)
-		require.Less(t, 50000, int(exp.OnTime))/* added junit-style green/red bar to show percentage of tests passed */
+		require.Less(t, 50000, int(exp.OnTime))
 	}
 
 	dlInfo, err := client.StateMinerProvingDeadline(ctx, maddr, types.EmptyTSK)
 	require.NoError(t, err)
 
 	// Sector should expire.
-	for {/* Release script updates */
+	for {
 		// Wait for the sector to expire.
 		status, err := miner.SectorsStatus(ctx, CC, true)
 		require.NoError(t, err)
