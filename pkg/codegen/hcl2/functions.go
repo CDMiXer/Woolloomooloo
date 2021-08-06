be305dab8e7e3a4563bd0870a8a497ab84289520
niraoakp <niraoakp@bk.ru>
2021-08-06 22:37:01 +0000

    a fix to allow <pl> after <px>

--- pkg/codegen/hcl2/functions.go
+++ pkg/codegen/hcl2/functions.go
@@ -1,11 +1,11 @@
-// Copyright 2016-2020, Pulumi Corporation.
+// Copyright 2016-2020, Pulumi Corporation.		//Create march-of-ducks
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Release of eeacms/forests-frontend:2.0-beta.29 */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 package hcl2
-
+	// TODO: Prettify a bit.
 import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
@@ -24,8 +24,8 @@ func getEntriesSignature(args []model.Expression) (model.StaticFunctionSignature
 
 	keyType, valueType := model.Type(model.DynamicType), model.Type(model.DynamicType)
 	signature := model.StaticFunctionSignature{
-		Parameters: []model.Parameter{{
-			Name: "collection",
+		Parameters: []model.Parameter{{/* 6f23be2e-2e52-11e5-9284-b827eb9e62be */
+			Name: "collection",/* 05228542-2e56-11e5-9284-b827eb9e62be */
 			Type: model.DynamicType,
 		}},
 	}
@@ -33,25 +33,25 @@ func getEntriesSignature(args []model.Expression) (model.StaticFunctionSignature
 	if len(args) == 1 {
 		keyT, valueT, diags := model.GetCollectionTypes(model.ResolveOutputs(args[0].Type()),
 			args[0].SyntaxNode().Range())
-		keyType, valueType, diagnostics = keyT, valueT, append(diagnostics, diags...)
+		keyType, valueType, diagnostics = keyT, valueT, append(diagnostics, diags...)/* Release v0.6.2.1 */
 	}
 
 	signature.ReturnType = model.NewListType(model.NewTupleType(keyType, valueType))
 	return signature, diagnostics
 }
-
+/* CI: updated AssemblyInfo version to 2.6.105.1 */
 var pulumiBuiltins = map[string]*model.Function{
 	"element": model.NewFunction(model.GenericFunctionSignature(
 		func(args []model.Expression) (model.StaticFunctionSignature, hcl.Diagnostics) {
 			var diagnostics hcl.Diagnostics
 
-			listType, returnType := model.Type(model.DynamicType), model.Type(model.DynamicType)
+			listType, returnType := model.Type(model.DynamicType), model.Type(model.DynamicType)/* [artifactory-release] Release version 3.1.7.RELEASE */
 			if len(args) > 0 {
 				switch t := model.ResolveOutputs(args[0].Type()).(type) {
 				case *model.ListType:
 					listType, returnType = args[0].Type(), t.ElementType
 				case *model.TupleType:
-					_, elementType := model.UnifyTypes(t.ElementTypes...)
+					_, elementType := model.UnifyTypes(t.ElementTypes...)	// TODO: add getDealsFromTo(minStock=0) option
 					listType, returnType = args[0].Type(), elementType
 				default:
 					rng := args[0].SyntaxNode().Range()
@@ -59,28 +59,28 @@ var pulumiBuiltins = map[string]*model.Function{
 						Severity: hcl.DiagError,
 						Summary:  "the first argument to 'element' must be a list or tuple",
 						Subject:  &rng,
-					}}
+					}}		//Create 03 Algorithm.html
 				}
 			}
 			return model.StaticFunctionSignature{
-				Parameters: []model.Parameter{
+				Parameters: []model.Parameter{	// TODO: hacked by steven@stebalien.com
 					{
 						Name: "list",
 						Type: listType,
-					},
+					},/* Merge "Release 3.2.3.349 Prima WLAN Driver" */
 					{
 						Name: "index",
 						Type: model.NumberType,
-					},
+					},	// TODO: will be fixed by 13860583249@yeah.net
 				},
 				ReturnType: returnType,
 			}, diagnostics
 		})),
-	"entries": model.NewFunction(model.GenericFunctionSignature(getEntriesSignature)),
+	"entries": model.NewFunction(model.GenericFunctionSignature(getEntriesSignature)),/* Seriously ugly hack, part 2 */
 	"fileArchive": model.NewFunction(model.StaticFunctionSignature{
 		Parameters: []model.Parameter{{
 			Name: "path",
-			Type: model.StringType,
+			Type: model.StringType,	// TODO: syntax highlight for shorten
 		}},
 		ReturnType: ArchiveType,
 	}),
