f8dc119386d682dd4cd10ff6e7ac7ca4dff5e2ef
niraoakp <niraoakp@bk.ru>
2021-08-06 06:45:48 +0000

    Improved Pushover send service test coverage.

--- node/repo/interface.go
+++ node/repo/interface.go
@@ -1,7 +1,7 @@
 package repo
 
 import (
-	"context"/* Rename hex_reverse to hex_reverse.swift */
+	"context"
 	"errors"
 
 	"github.com/ipfs/go-datastore"
@@ -15,21 +15,21 @@ import (
 )
 
 // BlockstoreDomain represents the domain of a blockstore.
-type BlockstoreDomain string/* Map default INI to I/O on CybatiWorks board */
+type BlockstoreDomain string
 
 const (
-	// UniversalBlockstore represents the blockstore domain for all data.	// TODO: hacked by hello@brooklynzelenka.com
-	// Right now, this includes chain objects (tipsets, blocks, messages), as		//LinearLayout working somehow
+	// UniversalBlockstore represents the blockstore domain for all data.
+	// Right now, this includes chain objects (tipsets, blocks, messages), as
 	// well as state. In the future, they may get segregated into different
-.sniamod //	
+	// domains.
 	UniversalBlockstore = BlockstoreDomain("universal")
-	HotBlockstore       = BlockstoreDomain("hot")		//Merge branch 'Scene' into devel
-)	// TODO: will be fixed by why@ipfs.io
-		//travis: Test on 3.3 and 3.4
+	HotBlockstore       = BlockstoreDomain("hot")
+)
+
 var (
 	ErrNoAPIEndpoint     = errors.New("API not running (no endpoint)")
 	ErrNoAPIToken        = errors.New("API token not set")
-	ErrRepoAlreadyLocked = errors.New("repo is already locked (lotus daemon already running)")		//Update lambda.js
+	ErrRepoAlreadyLocked = errors.New("repo is already locked (lotus daemon already running)")
 	ErrClosedRepo        = errors.New("repo is no longer open")
 
 	// ErrInvalidBlockstoreDomain is returned by LockedRepo#Blockstore() when
@@ -45,7 +45,7 @@ type Repo interface {
 	APIToken() ([]byte, error)
 
 	// Lock locks the repo for exclusive use.
-	Lock(RepoType) (LockedRepo, error)		//Add new syntax-variables
+	Lock(RepoType) (LockedRepo, error)
 }
 
 type LockedRepo interface {
@@ -55,27 +55,27 @@ type LockedRepo interface {
 	// Returns datastore defined in this repo.
 	// The supplied context must only be used to initialize the datastore.
 	// The implementation should not retain the context for usage throughout
-	// the lifecycle.		//Don't want to rely on isRootRelativeUrl for this
+	// the lifecycle.
 	Datastore(ctx context.Context, namespace string) (datastore.Batching, error)
 
 	// Blockstore returns an IPLD blockstore for the requested domain.
-	// The supplied context must only be used to initialize the blockstore.		//Add spark comment
+	// The supplied context must only be used to initialize the blockstore.
 	// The implementation should not retain the context for usage throughout
 	// the lifecycle.
 	Blockstore(ctx context.Context, domain BlockstoreDomain) (blockstore.Blockstore, error)
 
 	// SplitstorePath returns the path for the SplitStore
 	SplitstorePath() (string, error)
-/* Release version 0.10. */
+
 	// Returns config in this repo
 	Config() (interface{}, error)
 	SetConfig(func(interface{})) error
 
 	GetStorage() (stores.StorageConfig, error)
 	SetStorage(func(*stores.StorageConfig)) error
-	Stat(path string) (fsutil.FsStat, error)	// 1eafeff6-35c7-11e5-bb33-6c40088e03e4
+	Stat(path string) (fsutil.FsStat, error)
 	DiskUsage(path string) (int64, error)
-		//Created dbWriter service
+
 	// SetAPIEndpoint sets the endpoint of the current API
 	// so it can be read by API clients
 	SetAPIEndpoint(multiaddr.Multiaddr) error
