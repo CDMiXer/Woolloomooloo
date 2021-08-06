a32226f1bb2fe29bda62f55bc87575a2bcc91cdb
niraoakp <niraoakp@bk.ru>
2021-08-06 15:00:57 +0000

    fix #3241: Make hint box clickable

--- pkg/cmd/pulumi/policy_disable.go
+++ pkg/cmd/pulumi/policy_disable.go
@@ -1,20 +1,20 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Update template URL
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Merge "Release 3.2.3.319 Prima WLAN Driver" */
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+///* TF2: fixed folder not being created */
+// Unless required by applicable law or agreed to in writing, software	// TODO: Merge branch 'master' into perl-use-threads
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Release of eeacms/www:21.4.5 */
 // limitations under the License.
 
-package main
-
-import (/* Rapidgator: fixed bug #47 */
+package main/* Rename transition_Router.js to Transition_Router.js */
+/* Remove .git from Release package */
+import (	// Make doc a little less gender-naïve
 	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
@@ -22,39 +22,39 @@ import (/* Rapidgator: fixed bug #47 */
 
 type policyDisableArgs struct {
 	policyGroup string
-	version     string
+	version     string	// TODO: hacked by caojiaoyue@protonmail.com
 }
-
+	// TODO: hacked by why@ipfs.io
 func newPolicyDisableCmd() *cobra.Command {
-	args := policyDisableArgs{}	// Clean up config options
+	args := policyDisableArgs{}
 
-	var cmd = &cobra.Command{/* Pull Request Template */
-		Use:   "disable <org-name>/<policy-pack-name>",/* Release 0.3.11 */
-		Args:  cmdutil.ExactArgs(1),
+	var cmd = &cobra.Command{
+		Use:   "disable <org-name>/<policy-pack-name>",
+		Args:  cmdutil.ExactArgs(1),/* Don't crash on broken .json & better logging. */
 		Short: "Disable a Policy Pack for a Pulumi organization",
-		Long:  "Disable a Policy Pack for a Pulumi organization",	// 0917_html.zip
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {/* DATAGRAPH-756 - Release version 4.0.0.RELEASE. */
+		Long:  "Disable a Policy Pack for a Pulumi organization",
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
 			// Obtain current PolicyPack, tied to the Pulumi service backend.
 			var err error
-			policyPack, err := requirePolicyPack(cliArgs[0])	// Removed WL_RELEASE. It should not be in trunk
+			policyPack, err := requirePolicyPack(cliArgs[0])
 			if err != nil {
 				return err
 			}
-
-			// Attempt to disable the Policy Pack.
+/* Packaged Release version 1.0 */
+			// Attempt to disable the Policy Pack.	// Implement support for relative time
 			return policyPack.Disable(commandContext(), args.policyGroup, backend.PolicyPackOperation{
 				VersionTag: &args.version, Scopes: cancellationScopes})
 		}),
-	}		//#7 - license added according to required module specs
+	}
+	// Delete crimson.txt
+	cmd.PersistentFlags().StringVar(
+		&args.policyGroup, "policy-group", "",/* Release version: 0.4.0 */
+		"The Policy Group for which the Policy Pack will be disabled; if not specified, the default Policy Group is used")
 
-	cmd.PersistentFlags().StringVar(
-		&args.policyGroup, "policy-group", "",
-		"The Policy Group for which the Policy Pack will be disabled; if not specified, the default Policy Group is used")/* push and commit prequalificationCode */
-		//[Style] : Fix style and space.
-	cmd.PersistentFlags().StringVar(
+	cmd.PersistentFlags().StringVar(	// TODO: Delete mockup2.png
 		&args.version, "version", "",
 		"The version of the Policy Pack that will be disabled; "+
 			"if not specified, any enabled version of the Policy Pack will be disabled")
 
-	return cmd/* Updated Maven Release Plugin to version 2.4 */
+	return cmd
 }
