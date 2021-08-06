5eb42678927fed1a01af1dfe97f9fe5ec4a95d24
niraoakp <niraoakp@bk.ru>
2021-08-06 07:57:20 +0000

    Leitura, Consulta e Médias Diarias funcionando

--- cmd/drone-server/inject_server.go
+++ cmd/drone-server/inject_server.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by vyzo@hackzen.org
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Update uk.m3u
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release of eeacms/www:20.6.18 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -20,38 +20,38 @@ import (
 	"github.com/drone/drone/cmd/drone-server/config"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api"
-	"github.com/drone/drone/handler/health"/* Merge "wlan: Release 3.2.3.252a" */
+	"github.com/drone/drone/handler/health"
 	"github.com/drone/drone/handler/web"
 	"github.com/drone/drone/metric"
 	"github.com/drone/drone/operator/manager"
 	"github.com/drone/drone/operator/manager/rpc"
-	"github.com/drone/drone/operator/manager/rpc2"	// TODO: Clean up API, and beef up test coverage.
+	"github.com/drone/drone/operator/manager/rpc2"
 	"github.com/drone/drone/server"
 	"github.com/google/wire"
-		//added git repo with db scripts
+
 	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"	// TODO: hacked by alan.shaw@protocol.ai
+	"github.com/go-chi/chi/middleware"
 	"github.com/unrolled/secure"
 )
-/* 64acaa42-2d48-11e5-83ed-7831c1c36510 */
+
 type (
 	healthzHandler http.Handler
-	metricsHandler http.Handler		//format args to get better overview of defaults
+	metricsHandler http.Handler
 	pprofHandler   http.Handler
 	rpcHandlerV1   http.Handler
 	rpcHandlerV2   http.Handler
-)/* Added latest Release Notes to sidebar */
-		//Rename PittProject3.java to Blackjack.java
+)
+
 // wire set for loading the server.
 var serverSet = wire.NewSet(
 	manager.New,
 	api.New,
 	web.New,
 	provideHealthz,
-	provideMetric,/* Merge "Inflate tints ColorStateList with theme resources" */
+	provideMetric,
 	providePprof,
 	provideRouter,
-	provideRPC,	// Working on Gradle configuration
+	provideRPC,
 	provideRPC2,
 	provideServer,
 	provideServerOptions,
@@ -63,11 +63,11 @@ func provideRouter(api api.Server, web web.Server, rpcv1 rpcHandlerV1, rpcv2 rpc
 	r := chi.NewRouter()
 	r.Mount("/healthz", healthz)
 	r.Mount("/metrics", metrics)
-	r.Mount("/api", api.Handler())	// Delete Hacksnoerehuller1.jpg.jpg
+	r.Mount("/api", api.Handler())
 	r.Mount("/rpc/v2", rpcv2)
-	r.Mount("/rpc", rpcv1)/* Change Program Name and Version (v.2.71 "AndyLavr-Release") */
+	r.Mount("/rpc", rpcv1)
 	r.Mount("/", web.Handler())
-	r.Mount("/debug", pprof)		//added bus-router to GTFS section
+	r.Mount("/debug", pprof)
 	return r
 }
 
