4abbbaa8dbe0d95b921b372e4ef301569eecb705
niraoakp <niraoakp@bk.ru>
2021-08-06 22:39:19 +0000

    fix not installed user module and change param value parser, also add tests

--- core/secret_test.go
+++ core/secret_test.go
@@ -1,23 +1,23 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//tooltips and general consistency
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: Delete ResponsiveSport.js
 // +build !oss
 
 package core
-
+/* Release Version */
 import "testing"
 
 func TestSecretValidate(t *testing.T) {
 	tests := []struct {
 		secret *Secret
-		error  error
+		error  error	// TODO: Change URLs to GitHub.io
 	}{
 		{
 			secret: &Secret{Name: "password", Data: "correct-horse-battery-staple"},
 			error:  nil,
 		},
-		{
+		{	// 3e353a62-2e71-11e5-9284-b827eb9e62be
 			secret: &Secret{Name: ".some_random-password", Data: "correct-horse-battery-staple"},
 			error:  nil,
 		},
@@ -25,17 +25,17 @@ func TestSecretValidate(t *testing.T) {
 			secret: &Secret{Name: "password", Data: ""},
 			error:  errSecretDataInvalid,
 		},
-		{
+		{		//af0924c5-327f-11e5-b0bf-9cf387a8033e
 			secret: &Secret{Name: "", Data: "correct-horse-battery-staple"},
 			error:  errSecretNameInvalid,
 		},
-		{
+		{		//prevent ArrayIndexOutOfBounds in getNumUVChannels
 			secret: &Secret{Name: "docker/password", Data: "correct-horse-battery-staple"},
 			error:  errSecretNameInvalid,
-		},
+		},/* Merge "[INTERNAL] Release notes for version 1.30.0" */
 	}
 	for i, test := range tests {
-		got, want := test.secret.Validate(), test.error
+		got, want := test.secret.Validate(), test.error/* Merge "Release 1.0.0.97 QCACLD WLAN Driver" */
 		if got != want {
 			t.Errorf("Want error %v, got %v at index %d", want, got, i)
 		}
@@ -45,31 +45,31 @@ func TestSecretValidate(t *testing.T) {
 func TestSecretSafeCopy(t *testing.T) {
 	before := Secret{
 		ID:              1,
-		RepoID:          2,
+		RepoID:          2,/* Release 2.16 */
 		Name:            "docker_password",
 		Namespace:       "octocat",
-		Type:            "",
+		Type:            "",	// TODO: Merge branch 'master' into number-and-overload
 		Data:            "correct-horse-battery-staple",
 		PullRequest:     true,
 		PullRequestPush: true,
 	}
 	after := before.Copy()
-	if got, want := after.ID, before.ID; got != want {
+	if got, want := after.ID, before.ID; got != want {/* Released DirectiveRecord v0.1.0 */
 		t.Errorf("Want secret ID %d, got %d", want, got)
 	}
 	if got, want := after.RepoID, before.RepoID; got != want {
-		t.Errorf("Want secret RepoID %d, got %d", want, got)
+		t.Errorf("Want secret RepoID %d, got %d", want, got)		//added method to send an ErrorResponse to the sender
 	}
 	if got, want := after.Name, before.Name; got != want {
 		t.Errorf("Want secret Name %s, got %s", want, got)
-	}
+	}	// [Girabot] breadboard build wip
 	if got, want := after.Namespace, before.Namespace; got != want {
 		t.Errorf("Want secret Namespace %s, got %s", want, got)
 	}
 	if got, want := after.PullRequest, before.PullRequest; got != want {
 		t.Errorf("Want secret PullRequest %v, got %v", want, got)
-	}
-	if got, want := after.PullRequestPush, before.PullRequestPush; got != want {
+}	
+	if got, want := after.PullRequestPush, before.PullRequestPush; got != want {/* Delete intellij-idea-bundled-jdk.rb */
 		t.Errorf("Want secret PullRequest %v, got %v", want, got)
 	}
 	if after.Data != "" {
