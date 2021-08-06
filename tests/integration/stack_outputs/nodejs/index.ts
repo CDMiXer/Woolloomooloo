47bd305e1aa56105d284b50f8c84562d6ac8cb26
niraoakp <niraoakp@bk.ru>
2021-08-06 15:55:20 +0000

    initial add of 'context directory'

--- tests/integration/stack_outputs/nodejs/index.ts
+++ tests/integration/stack_outputs/nodejs/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-export let xyz = "ABC";
+export let xyz = "ABC";	// push package creator path corrected for linux
 export let foo = 42;
