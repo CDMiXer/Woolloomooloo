0859eaa215741e2e4a3583f8a7232a7f6e8244a5
niraoakp <niraoakp@bk.ru>
2021-08-06 02:11:24 +0000

    18HI-New by OscarConklin

--- handler/web/link/link_test.go
+++ handler/web/link/link_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc.		//Merge branch 'integration' into tf_issue_461
+//	// TODO: will be fixed by juan@benet.ai
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0	// Explicitly specify Python version
-//
+///* do not send an empty list to Cloud Spanner */
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* Create triangulator.py */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Never meant to pick up swp files
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* Release precompile plugin 1.2.3 */
 // limitations under the License.
 
 package link
