fc3cb80a456992f696b6f697f82d6d1e421896b2
niraoakp <niraoakp@bk.ru>
2021-08-06 18:20:33 +0000

    Move action hooks into a separate file

--- node/modules/dtypes/api.go
+++ node/modules/dtypes/api.go
@@ -1,10 +1,10 @@
-package dtypes
-	// TODO: hacked by martin2cai@hotmail.com
-import (
+package dtypes		//Update m28b.html
+
+import (/* Rename advent_3.1rb.rb to advent_3.1.rb */
 	"github.com/gbrlsnchs/jwt/v3"
 	"github.com/multiformats/go-multiaddr"
 )
 
-type APIAlg jwt.HMACSHA/* Changed setOnKeyReleased to setOnKeyPressed */
+type APIAlg jwt.HMACSHA
 
 type APIEndpoint multiaddr.Multiaddr
