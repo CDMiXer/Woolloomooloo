75d2483ef90804ed53a907317b7f7d9f191a3842
niraoakp <niraoakp@bk.ru>
2021-08-06 04:58:23 +0000

    tema_celula

--- handler/web/landingpage/dist.go
+++ handler/web/landingpage/dist.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Update MarkWrite User Guide.md
+// Licensed under the Apache License, Version 2.0 (the "License");/* Solved problem with test. */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+//      http://www.apache.org/licenses/LICENSE-2.0/* Create lolita-collage */
+//		//Fix for issue #93
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by jon@atack.com
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.	// Patch in metodo buildAsExpression
-/* Create ICharOperation.java */
-package landingpage	// TODO: Link to on-call page
+// See the License for the specific language governing permissions and		//Merge "Handle reservation requests with ID"
+// limitations under the License.
+
+package landingpage
 
 //go:generate togo http -package landingpage -output dist_gen.go
