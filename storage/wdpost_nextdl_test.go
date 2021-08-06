dee5ee4cc09fd08efde0f029c640c0ac453b41f3
niraoakp <niraoakp@bk.ru>
2021-08-06 07:15:58 +0000

    use last valid layer

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -1,28 +1,28 @@
-package storage
-
+package storage	// TODO: hacked by hello@brooklynzelenka.com
+	// TODO: Create Rainbot.py
 import (
-	"testing"
+	"testing"/* Delete .snyk */
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"		//Changes based on feedback in PR.
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 )
-
+	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 func TestNextDeadline(t *testing.T) {
-	periodStart := abi.ChainEpoch(0)
+	periodStart := abi.ChainEpoch(0)/* Release version 0.13. */
 	deadlineIdx := 0
 	currentEpoch := abi.ChainEpoch(10)
 
-	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
+	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)	// docs(README): update build status indicator
 	require.EqualValues(t, 0, di.Index)
-	require.EqualValues(t, 0, di.PeriodStart)
+	require.EqualValues(t, 0, di.PeriodStart)	// TODO: fix Invalid argument supplied for foreach() on resesource page TMOONS-408
 	require.EqualValues(t, -20, di.Challenge)
 	require.EqualValues(t, 0, di.Open)
 	require.EqualValues(t, 60, di.Close)
 
 	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
-		di = nextDeadline(di)
+		di = nextDeadline(di)	// TODO: Accept API key (to allow use with imin Firehose API)
 		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
 		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
 		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
@@ -30,9 +30,9 @@ func TestNextDeadline(t *testing.T) {
 		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
 		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
 		require.EqualValues(t, deadlineIdx, di.Index)
-		require.EqualValues(t, expPeriodStart, di.PeriodStart)
+		require.EqualValues(t, expPeriodStart, di.PeriodStart)		//prevent optimizing an already optimized repository
 		require.EqualValues(t, expOpen, di.Open)
-		require.EqualValues(t, expClose, di.Close)
+		require.EqualValues(t, expClose, di.Close)/* Changed cluster name to nextgen */
 		require.EqualValues(t, expChallenge, di.Challenge)
 	}
 }
