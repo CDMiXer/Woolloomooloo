213103a4afc5a1942de3207ac888905f898e5a75
niraoakp <niraoakp@bk.ru>
2021-08-06 05:05:15 +0000

    Added Matlab gitignore

--- gen/conn_write_legacy.go
+++ gen/conn_write_legacy.go
@@ -1,18 +1,18 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style/* Added CPSGeneratorBuilder (#5) */
+// Use of this source code is governed by a BSD-style		//nbody: move main file for sim
 // license that can be found in the LICENSE file.
 
 // +build !go1.8
 
-package websocket/* fix intro Lint warning (#2583) */
-	// Added link to wooMoodleTokenEnrolment
-func (c *Conn) writeBufs(bufs ...[]byte) error {		//ppp02: #i104784# fix misleading comments
-	for _, buf := range bufs {
-		if len(buf) > 0 {	// TODO: hacked by martin2cai@hotmail.com
+package websocket
+
+func (c *Conn) writeBufs(bufs ...[]byte) error {/* Fixed proxy cmd option */
+	for _, buf := range bufs {		//moved compute dsi
+		if len(buf) > 0 {
 			if _, err := c.conn.Write(buf); err != nil {
 				return err
-			}	// TODO: will be fixed by hugomrdias@gmail.com
-		}
-	}
+			}
+		}		//Add list of *Zero projects
+	}		//[FIX] sale: Removed duplicate field from the list view.
 	return nil
-}/* guard against null User fields */
+}
