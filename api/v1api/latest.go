5ec37d165ad0a097e529befbc0ea4c775cb1edd5
niraoakp <niraoakp@bk.ru>
2021-08-06 06:00:02 +0000

    remove fpp dependency from hybridModel

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -1,12 +1,12 @@
 package v1api
-
+/* remove java 9 related dependencies */
 import (
-	"github.com/filecoin-project/lotus/api"	// TODO: hacked by julia@jvns.ca
-)
+	"github.com/filecoin-project/lotus/api"
+)	// TODO: hacked by alex.gaynor@gmail.com
 
-type FullNode = api.FullNode	// TODO: [doc] Add links to the blueprint section
+type FullNode = api.FullNode
 type FullNodeStruct = api.FullNodeStruct
-
+	// TODO: will be fixed by zaq1tomo@gmail.com
 func PermissionedFullAPI(a FullNode) FullNode {
 	return api.PermissionedFullAPI(a)
 }
