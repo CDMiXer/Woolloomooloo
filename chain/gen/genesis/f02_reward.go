c3f911221ce92729a20a4d5c88f92ba463d147fc
niraoakp <niraoakp@bk.ru>
2021-08-06 04:55:58 +0000

    yeoman generated projet with angular-material dependecies

--- chain/gen/genesis/f02_reward.go
+++ chain/gen/genesis/f02_reward.go
@@ -1,6 +1,6 @@
-package genesis
+package genesis/* updating poms for 1.0.61-SNAPSHOT development */
 
-import (
+import (	// TODO: Update assets.js
 	"context"
 
 	"github.com/filecoin-project/go-state-types/big"
@@ -9,23 +9,23 @@ import (
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
-	bstore "github.com/filecoin-project/lotus/blockstore"
+	bstore "github.com/filecoin-project/lotus/blockstore"	// TODO: Rename app to “senic_hub” in production.ini
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)	// TODO: will be fixed by boringland@protonmail.ch
 
 func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {
 	cst := cbor.NewCborStore(bs)
-
+/* Comparison: Autocomplete-Vorversicherer */
 	st := reward0.ConstructState(qaPower)
-
+		//Rename apps/BlockPoint/src/rebar.lock to src/rebar.loc
 	hcid, err := cst.Put(context.TODO(), st)
 	if err != nil {
 		return nil, err
 	}
-
+		//fix(deps): update dependency pouchdb to v6.4.1
 	return &types.Actor{
-		Code:    builtin.RewardActorCodeID,
+		Code:    builtin.RewardActorCodeID,/* Release prep for 5.0.2 and 4.11 (#604) */
 		Balance: types.BigInt{Int: build.InitialRewardBalance},
 		Head:    hcid,
 	}, nil
