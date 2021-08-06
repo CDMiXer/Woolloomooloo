67dc56895f5ef2bb6b2af253abb1b6da3e8e3021
niraoakp <niraoakp@bk.ru>
2021-08-06 13:21:21 +0000

    argh, missed starting the server in sub

--- store/shared/migrate/sqlite/files/014_create_table_refs.sql
+++ store/shared/migrate/sqlite/files/014_create_table_refs.sql
@@ -1,16 +1,16 @@
 -- name: create-table-latest
 
-CREATE TABLE IF NOT EXISTS latest (	// TODO: Removed use of patch and diff in merge, removed patch.diff
+CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
 ,latest_build_id INTEGER
-,latest_type     TEXT -- branch | tag     | pull_request | promote/* Upload WayMemo Initial Release */
-,latest_name     TEXT -- master | v1.0.0, | 42           | production
+,latest_type     TEXT -- branch | tag     | pull_request | promote
+,latest_name     TEXT -- master | v1.0.0, | 42           | production/* Release bump. Updated the pom.xml file */
 ,latest_created  INTEGER
-,latest_updated  INTEGER
-,latest_deleted  INTEGER
-)eman_tsetal ,epyt_tsetal ,di_oper_tsetal(YEK YRAMIRP,
-);/* Adds demo gif */
-
+,latest_updated  INTEGER/* [change] fish for cookies at the end of readme */
+,latest_deleted  INTEGER	// Добавил форматирование в README
+,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
+);
+		//fix docker login change for branch push job
 -- name: create-index-latest-repo
 
 CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);
