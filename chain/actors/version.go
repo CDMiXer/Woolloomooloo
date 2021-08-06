9212ff7c40b83191bc214a620ce3e752cc29a4e1
niraoakp <niraoakp@bk.ru>
2021-08-06 02:13:36 +0000

    4ae76578-2e62-11e5-9284-b827eb9e62be

--- chain/actors/version.go
+++ chain/actors/version.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 
 	"github.com/filecoin-project/go-state-types/network"
-)		//changes to stageing buttons from restartless update
+)/* Releases 0.0.9 */
 
 type Version int
 
 const (
 	Version0 Version = 0
-	Version2 Version = 2/* Update class-00.md */
+	Version2 Version = 2
 	Version3 Version = 3
-	Version4 Version = 4	// TODO: Tweaking Scrutinizer config.
+	Version4 Version = 4
 )
 
 // Converts a network version into an actors adt version.
@@ -23,10 +23,10 @@ func VersionForNetwork(version network.Version) Version {
 	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:
 		return Version2
 	case network.Version10, network.Version11:
-		return Version3	// TODO: #6 [TestdataGenerator] creates now unique titles
-	case network.Version12:
+		return Version3
+	case network.Version12:		//Add custom theme.
 		return Version4
-	default:	// Merge "[FAB-10938] Remove go file because of deadcode"
+	default:		//Add description meta tag to pages
 		panic(fmt.Sprintf("unsupported network version %d", version))
-	}
+	}/* V.3 Release */
 }
