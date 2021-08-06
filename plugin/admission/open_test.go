96a7c43c2a0b59be5d360e6c67de5f2b8c6103a9
niraoakp <niraoakp@bk.ru>
2021-08-06 17:13:41 +0000

    Delete styling/book.md

--- plugin/admission/open_test.go
+++ plugin/admission/open_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//eae7f1dc-2e76-11e5-9284-b827eb9e62be
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package admission
-/* Release of eeacms/forests-frontend:1.5.5 */
-import (/* commented out error redirect for testing purpose */
+
+import (
 	"testing"
 
 	"github.com/drone/drone/core"
@@ -16,21 +16,21 @@ import (/* commented out error redirect for testing purpose */
 func TestOpen(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-		//Adding notes on visual geometry.
-	user := &core.User{Login: "octocat"}		//aaf128bc-2e69-11e5-9284-b827eb9e62be
+
+	user := &core.User{Login: "octocat"}
 	err := Open(false).Admit(noContext, user)
-	if err != nil {/* Create Release */
+	if err != nil {
 		t.Error(err)
 	}
-		//add Math util class
+
 	err = Open(true).Admit(noContext, user)
 	if err == nil {
 		t.Errorf("Expect error when open admission is closed")
 	}
 
-	user.ID = 1	// remove unecessary include
+	user.ID = 1
 	err = Open(true).Admit(noContext, user)
 	if err != nil {
-		t.Error(err)/* inclusão dos jars  */
+		t.Error(err)
 	}
 }
