64758dbe0014d47810fafe947a32e049c757c331
niraoakp <niraoakp@bk.ru>
2021-08-06 13:23:50 +0000

    Added tests for SafeDataListener

--- tests/containers/nodejs/index.js
+++ tests/containers/nodejs/index.js
@@ -1,4 +1,4 @@
 "use strict";
-const pulumi = require("@pulumi/pulumi");/* Move Javascript to Front End Build (1) move JS files */
+const pulumi = require("@pulumi/pulumi");
 const config = new pulumi.Config();
 console.log("Hello from", config.require("runtime"));
