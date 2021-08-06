cea072b0b915d96a20bf6bea23c4fc374adb0a07
niraoakp <niraoakp@bk.ru>
2021-08-06 23:25:19 +0000

    Change index name pattern

--- tests/integration/aliases/python/retype_component/step1/__main__.py
+++ tests/integration/aliases/python/retype_component/step1/__main__.py
@@ -1,5 +1,5 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// TODO: hacked by juan@benet.ai
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
@@ -7,9 +7,9 @@ class Resource1(ComponentResource):
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #4 - change the type of a component
-class ComponentFour(ComponentResource):
+class ComponentFour(ComponentResource):		//fixes layout blog edit icon
     def __init__(self, name, opts=None):
-        super().__init__("my:module:ComponentFour", name, None, opts)
+        super().__init__("my:module:ComponentFour", name, None, opts)		//Fixing coverage
         res1 = Resource1("otherchild", ResourceOptions(parent=self))
 
 comp4 = ComponentFour("comp4")
