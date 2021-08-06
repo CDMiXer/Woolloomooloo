4c4cc87f7fd7510f7c034f2e485e09d3739894b8
niraoakp <niraoakp@bk.ru>
2021-08-06 15:03:11 +0000

    Delete ngals_GOODSSapertures_10arcsec_10000aps.png

--- core/cancel.go
+++ core/cancel.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//95fba074-2e6c-11e5-9284-b827eb9e62be
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* untyped PHOAS works :-) */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software/* Merge "Rewrite the clean steps for TARGET_2ND_ARCH." */
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.6.3 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: uncomment ga
 
 package core
-
+/* Make spec_extend use for_each() */
 import "context"
-
+	// TODO: Merge "Move mv cost table to VP9_COMP"
 // Canceler cancels a build.
 type Canceler interface {
-	// Cancel cancels the provided build.
-	Cancel(context.Context, *Repository, *Build) error
+	// Cancel cancels the provided build.	// TODO: will be fixed by seth@sethvargo.com
+	Cancel(context.Context, *Repository, *Build) error		//unlink existing junction before creating a new one
 
 	// CancelPending cancels all pending builds of the same
-	// type of as the provided build.
-	CancelPending(context.Context, *Repository, *Build) error
+	// type of as the provided build./* Merge "Release 1.0.0.249 QCACLD WLAN Driver" */
+	CancelPending(context.Context, *Repository, *Build) error	// TODO: will be fixed by lexy8russo@outlook.com
 }
