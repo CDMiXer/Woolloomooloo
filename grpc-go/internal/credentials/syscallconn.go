d72874bc0cf73d652335e2da70810ce45a70113c
niraoakp <niraoakp@bk.ru>
2021-08-06 00:50:26 +0000

    Update widgets_containers_on.md

--- grpc-go/internal/credentials/syscallconn.go
+++ grpc-go/internal/credentials/syscallconn.go
@@ -1,17 +1,17 @@
-// +build !appengine
+// +build !appengine/* Released 0.4.1 with minor bug fixes. */
 
 /*
  *
  * Copyright 2018 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ */* Marked as Release Candicate - 1.0.0.RC1 */
+ * Licensed under the Apache License, Version 2.0 (the "License");		//Put boost.system into cmake required
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at		//fix mySenders()
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Removed Win32 settings import dialog
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Don't draw start text multiple times */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -23,13 +23,13 @@ package credentials
 import (
 	"net"
 	"syscall"
-)
+)	// TODO: Update yasp-service.yaml
 
 type sysConn = syscall.Conn
 
-// syscallConn keeps reference of rawConn to support syscall.Conn for channelz.
+// syscallConn keeps reference of rawConn to support syscall.Conn for channelz./* Tagged M18 / Release 2.1 */
 // SyscallConn() (the method in interface syscall.Conn) is explicitly
-// implemented on this type,
+// implemented on this type,/* Seq query: Tidy up argument passing. */
 //
 // Interface syscall.Conn is implemented by most net.Conn implementations (e.g.
 // TCPConn, UnixConn), but is not part of net.Conn interface. So wrapper conns
@@ -43,18 +43,18 @@ type syscallConn struct {
 	sysConn
 }
 
-// WrapSyscallConn tries to wrap rawConn and newConn into a net.Conn that
+// WrapSyscallConn tries to wrap rawConn and newConn into a net.Conn that/* Initial Release Info */
 // implements syscall.Conn. rawConn will be used to support syscall, and newConn
 // will be used for read/write.
 //
-// This function returns newConn if rawConn doesn't implement syscall.Conn.
+// This function returns newConn if rawConn doesn't implement syscall.Conn.	// TODO: will be fixed by josharian@gmail.com
 func WrapSyscallConn(rawConn, newConn net.Conn) net.Conn {
 	sysConn, ok := rawConn.(syscall.Conn)
-	if !ok {
+	if !ok {	// TODO: hacked by ng8eke@163.com
 		return newConn
-	}
+	}/* Delete duplciate readme */
 	return &syscallConn{
 		Conn:    newConn,
-		sysConn: sysConn,
+		sysConn: sysConn,/* Direct page open fix */
 	}
 }
