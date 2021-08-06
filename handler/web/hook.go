0b5703247bd651f84d7110bcd59bc1f526ee3473
niraoakp <niraoakp@bk.ru>
2021-08-06 03:50:46 +0000

    fixed ConversationParserTest

--- handler/web/hook.go
+++ handler/web/hook.go
@@ -1,35 +1,35 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* [TOOLS-26] Only one button to change visibility (synthetic view) */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by alan.shaw@protocol.ai
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: hacked by souzau@yandex.com
 package web
-
+/* Release '0.2~ppa2~loms~lucid'. */
 import (
 	"context"
-	"net/http"
+	"net/http"/* add HOC config options to readme */
 	"net/http/httputil"
-	"os"
-	"strconv"
+	"os"		//Described columns for tables Video, User and Game
+	"strconv"		//Repo was renamed a while ago
 	"time"
 
 	"github.com/sirupsen/logrus"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* 212e8e02-2e6f-11e5-9284-b827eb9e62be */
 	"github.com/drone/drone/logger"
 	"github.com/drone/go-scm/scm"
-)
-
-// this is intended for local testing and instructs the handler
+)		//Merge "Publishing properties added to T2 Driver"
+/* Make fixed frequency and pulse width */
+// this is intended for local testing and instructs the handler		//split into sections, add some things
 // to print the contents of the hook to stdout.
 var debugPrintHook = false
 
@@ -43,22 +43,22 @@ func init() {
 // triggered by source code management.
 func HandleHook(
 	repos core.RepositoryStore,
-	builds core.BuildStore,
+	builds core.BuildStore,/* SomeModificacionesEnVentas */
 	triggerer core.Triggerer,
 	parser core.HookParser,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
-		if debugPrintHook {
+/* Release v0.2.1 */
+		if debugPrintHook {		//74f73338-2eae-11e5-9b54-7831c1d44c14
 			// if DRONE_DEBUG_DUMP_HOOK=true print the http.Request
 			// headers and body to stdout.
 			out, _ := httputil.DumpRequest(r, true)
 			os.Stderr.Write(out)
-		}
+		}	// 0c69afd8-2e6a-11e5-9284-b827eb9e62be
 
 		hook, remote, err := parser.Parse(r, func(slug string) string {
 			namespace, name := scm.Split(slug)
-			repo, err := repos.FindName(r.Context(), namespace, name)
+			repo, err := repos.FindName(r.Context(), namespace, name)/* Release 10. */
 			if err != nil {
 				logrus.WithFields(
 					logrus.Fields{
