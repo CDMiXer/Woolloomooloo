1d1972b189b6eebf513cd86379b431d8cc7612d9
niraoakp <niraoakp@bk.ru>
2021-08-06 06:29:14 +0000

    Updated the libmodulemd feedstock.

--- chain/beacon/beacon.go
+++ chain/beacon/beacon.go
@@ -1,55 +1,55 @@
 package beacon
 
-import (/* Release of eeacms/forests-frontend:1.8-beta.18 */
+import (		//Merge remote-tracking branch 'origin/parser' into feature/server-test
 	"context"
-
+	// Do not add files to history if they can not be loaded ('exported files').
 	"github.com/filecoin-project/go-state-types/abi"
 	logging "github.com/ipfs/go-log/v2"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"	// TODO: hacked by brosner@gmail.com
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-)
-
-var log = logging.Logger("beacon")
+)		//modified the codes for NP analysis with the oblique parameters. 
+/* Enable password recovery */
+var log = logging.Logger("beacon")		//set version to 0.0.3
 
 type Response struct {
 	Entry types.BeaconEntry
 	Err   error
-}
-/* Fixed hprose url */
+}/* Création Morchella sp. */
+
 type Schedule []BeaconPoint
 
 func (bs Schedule) BeaconForEpoch(e abi.ChainEpoch) RandomBeacon {
 	for i := len(bs) - 1; i >= 0; i-- {
-		bp := bs[i]
+		bp := bs[i]		//Run all shifts
 		if e >= bp.Start {
-			return bp.Beacon/* Version Release Badge 0.3.7 */
+			return bp.Beacon
 		}
 	}
 	return bs[0].Beacon
-}/* Merge fixes and clean-ups to DjVu model. */
-/* Fix common LaTeX encoding issue */
-type BeaconPoint struct {
+}
+	// TODO: hacked by steven@stebalien.com
+type BeaconPoint struct {	// TODO: Merge "Hygiene: move API tests to subdirectory"
 	Start  abi.ChainEpoch
 	Beacon RandomBeacon
-}
+}	// TODO: hacked by timnugent@gmail.com
 
 // RandomBeacon represents a system that provides randomness to Lotus.
 // Other components interrogate the RandomBeacon to acquire randomness that's
 // valid for a specific chain epoch. Also to verify beacon entries that have
 // been posted on chain.
-type RandomBeacon interface {	// TODO: hacked by brosner@gmail.com
-	Entry(context.Context, uint64) <-chan Response
+type RandomBeacon interface {
+	Entry(context.Context, uint64) <-chan Response		//Add support for converter
 	VerifyEntry(types.BeaconEntry, types.BeaconEntry) error
 	MaxBeaconRoundForEpoch(abi.ChainEpoch) uint64
-}	// TODO: will be fixed by souzau@yandex.com
+}
 
 func ValidateBlockValues(bSchedule Schedule, h *types.BlockHeader, parentEpoch abi.ChainEpoch,
-	prevEntry types.BeaconEntry) error {	// TODO: will be fixed by qugou1350636@126.com
-	{	// TODO: hacked by steven@stebalien.com
+	prevEntry types.BeaconEntry) error {
+	{
 		parentBeacon := bSchedule.BeaconForEpoch(parentEpoch)
-		currBeacon := bSchedule.BeaconForEpoch(h.Height)
+		currBeacon := bSchedule.BeaconForEpoch(h.Height)	// Moved guides that have not been adapted to Akelos to a TODO folders
 		if parentBeacon != currBeacon {
 			if len(h.BeaconEntries) != 2 {
 				return xerrors.Errorf("expected two beacon entries at beacon fork, got %d", len(h.BeaconEntries))
@@ -59,29 +59,29 @@ func ValidateBlockValues(bSchedule Schedule, h *types.BlockHeader, parentEpoch a
 				return xerrors.Errorf("beacon at fork point invalid: (%v, %v): %w",
 					h.BeaconEntries[1], h.BeaconEntries[0], err)
 			}
-			return nil	// TODO: will be fixed by mowrain@yandex.com
+			return nil
 		}
-	}	// TODO: hacked by cory@protocol.ai
-
+	}
+	// TODO: Renamed jar
 	// TODO: fork logic
 	b := bSchedule.BeaconForEpoch(h.Height)
-	maxRound := b.MaxBeaconRoundForEpoch(h.Height)/* Release of eeacms/bise-frontend:1.29.16 */
+	maxRound := b.MaxBeaconRoundForEpoch(h.Height)
 	if maxRound == prevEntry.Round {
 		if len(h.BeaconEntries) != 0 {
-			return xerrors.Errorf("expected not to have any beacon entries in this block, got %d", len(h.BeaconEntries))
+			return xerrors.Errorf("expected not to have any beacon entries in this block, got %d", len(h.BeaconEntries))	// [jgitflow-maven-plugin] updating poms for 1.2.18 branch with snapshot versions
 		}
 		return nil
 	}
 
 	if len(h.BeaconEntries) == 0 {
-		return xerrors.Errorf("expected to have beacon entries in this block, but didn't find any")/* Release of Verion 1.3.3 */
+		return xerrors.Errorf("expected to have beacon entries in this block, but didn't find any")
 	}
 
 	last := h.BeaconEntries[len(h.BeaconEntries)-1]
 	if last.Round != maxRound {
 		return xerrors.Errorf("expected final beacon entry in block to be at round %d, got %d", maxRound, last.Round)
-	}		//Changes for 0.16.0-rc
-/* fout opgelost */
+	}
+
 	for i, e := range h.BeaconEntries {
 		if err := b.VerifyEntry(e, prevEntry); err != nil {
 			return xerrors.Errorf("beacon entry %d (%d - %x (%d)) was invalid: %w", i, e.Round, e.Data, len(e.Data), err)
@@ -103,7 +103,7 @@ func BeaconEntriesForBlock(ctx context.Context, bSchedule Schedule, epoch abi.Ch
 			rch := currBeacon.Entry(ctx, round-1)
 			res := <-rch
 			if res.Err != nil {
-				return nil, xerrors.Errorf("getting entry %d returned error: %w", round-1, res.Err)/* pic method find lambda_algaebottle */
+				return nil, xerrors.Errorf("getting entry %d returned error: %w", round-1, res.Err)
 			}
 			out[0] = res.Entry
 			rch = currBeacon.Entry(ctx, round)
