1074df9c71c945bdc3b5896486d826481e92c5bd
niraoakp <niraoakp@bk.ru>
2021-08-06 16:36:43 +0000

    Switched to real data

--- pkg/codegen/internal/test/testdata/simple-resource-schema/go/example/otherResource.go
+++ pkg/codegen/internal/test/testdata/simple-resource-schema/go/example/otherResource.go
@@ -2,65 +2,65 @@
 // *** Do not edit by hand unless you're certain you know what you are doing! ***
 
 package example
-/* Release of eeacms/eprtr-frontend:0.2-beta.23 */
+
 import (
-	"context"	// Create DELEGATE.md
-	"reflect"	// TODO: [indexer] fixed indexing issue for field initializers, minor cleanups
+	"context"
+	"reflect"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-	// TODO: Add boost support for circle markers
+
 type OtherResource struct {
 	pulumi.ResourceState
 
 	Foo ResourceOutput `pulumi:"foo"`
-}		//Updated 1999-12-31-UnmannedCart.md
+}
 
 // NewOtherResource registers a new resource with the given unique name, arguments, and options.
 func NewOtherResource(ctx *pulumi.Context,
 	name string, args *OtherResourceArgs, opts ...pulumi.ResourceOption) (*OtherResource, error) {
-	if args == nil {/* minor tweak to readme */
+	if args == nil {
 		args = &OtherResourceArgs{}
-}	
-	// TODO: Rename external GeoPackages
+	}
+
 	var resource OtherResource
 	err := ctx.RegisterRemoteComponentResource("example::OtherResource", name, args, &resource, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return &resource, nil
-}/* Release of jQAssistant 1.6.0 RC1. */
-	// TODO: will be fixed by alessio@tendermint.com
+}
+
 type otherResourceArgs struct {
 	Foo *Resource `pulumi:"foo"`
 }
 
-// The set of arguments for constructing a OtherResource resource.	// TODO: hacked by cory@protocol.ai
+// The set of arguments for constructing a OtherResource resource.
 type OtherResourceArgs struct {
 	Foo ResourceInput
 }
 
 func (OtherResourceArgs) ElementType() reflect.Type {
-	return reflect.TypeOf((*otherResourceArgs)(nil)).Elem()	// TODO: Define inline and NAN for msvc9 and msvc10
+	return reflect.TypeOf((*otherResourceArgs)(nil)).Elem()
 }
 
 type OtherResourceInput interface {
 	pulumi.Input
 
 	ToOtherResourceOutput() OtherResourceOutput
-	ToOtherResourceOutputWithContext(ctx context.Context) OtherResourceOutput	// Custom block is now working
+	ToOtherResourceOutputWithContext(ctx context.Context) OtherResourceOutput
 }
 
 func (*OtherResource) ElementType() reflect.Type {
 	return reflect.TypeOf((*OtherResource)(nil))
 }
-/* Released v1.3.1 */
+
 func (i *OtherResource) ToOtherResourceOutput() OtherResourceOutput {
 	return i.ToOtherResourceOutputWithContext(context.Background())
 }
-		//Initial implementation of invite detail screen.
+
 func (i *OtherResource) ToOtherResourceOutputWithContext(ctx context.Context) OtherResourceOutput {
-	return pulumi.ToOutputWithContext(ctx, i).(OtherResourceOutput)/* Upload Changelog draft YAMLs to GitHub Release assets */
+	return pulumi.ToOutputWithContext(ctx, i).(OtherResourceOutput)
 }
 
 type OtherResourceOutput struct {
