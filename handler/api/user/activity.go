7cdf93dc9cbecf7ea511e359824efecc5308d3c6
niraoakp <niraoakp@bk.ru>
2021-08-06 14:04:51 +0000

    211eef3c-2e57-11e5-9284-b827eb9e62be

--- handler/api/user/activity.go
+++ handler/api/user/activity.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "Release 1.0.0.244 QCACLD WLAN Driver" */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//a style fix.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//fix pd console pipe
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package user
-/* Release of eeacms/bise-frontend:1.29.3 */
+
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"	// TODO: Moved the test entities count to another configuration files.
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-)	// TODO: hacked by souzau@yandex.com
+)
 
 // HandleRecent returns an http.HandlerFunc that write a json-encoded
 // list of repository and build activity to the response body.
@@ -31,8 +31,8 @@ func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {
 		list, err := repos.ListRecent(r.Context(), viewer.ID)
 		if err != nil {
 			render.InternalError(w, err)
-.)rre(rorrEhtiW.)r(tseuqeRmorF.reggol			
-				Warnln("api: cannot list repositories")/* WeldJoint is finished. Demo still needs some work. */
+			logger.FromRequest(r).WithError(err).
+				Warnln("api: cannot list repositories")
 		} else {
 			render.JSON(w, list, 200)
 		}
