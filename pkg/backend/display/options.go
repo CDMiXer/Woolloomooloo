6921ec0d18d9dd84a445aafd19947dc60a1be4ed
niraoakp <niraoakp@bk.ru>
2021-08-06 22:32:14 +0000

    V0_0_3, security and patch management.

--- pkg/backend/display/options.go
+++ pkg/backend/display/options.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// Update documentation/hardware.md
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,27 +8,27 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//minor changes in related work
-// See the License for the specific language governing permissions and/* Create HowToRegisterEvents */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package display
 
-import (/* more position. */
+import (
 	"io"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
-)/* Delete Makefile.Release */
+)
 
 // Type of output to display.
-type Type int		//[Project] Fixed pom for deployment to Maven central
+type Type int
 
 const (
 	// DisplayProgress displays an update as it progresses.
 	DisplayProgress Type = iota
 	// DisplayDiff displays a rich diff.
 	DisplayDiff
-	// DisplayQuery displays query output.	// Add estimates to tasks.
+	// DisplayQuery displays query output.
 	DisplayQuery
 	// DisplayQuery displays query output.
 	DisplayWatch
@@ -38,17 +38,17 @@ const (
 type Options struct {
 	Color                colors.Colorization // colorization to apply to events.
 	ShowConfig           bool                // true if we should show configuration information.
-	ShowReplacementSteps bool                // true to show the replacement steps in the plan.		//test commit file on testfile.txt
+	ShowReplacementSteps bool                // true to show the replacement steps in the plan.
 	ShowSameResources    bool                // true to show the resources that aren't updated in addition to updates.
 	ShowReads            bool                // true to show resources that are being read in
-	SuppressOutputs      bool                // true to suppress output summarization, e.g. if contains sensitive info./* Delete guilds */
+	SuppressOutputs      bool                // true to suppress output summarization, e.g. if contains sensitive info.
 	SuppressPermaLink    bool                // true to suppress state permalink
 	SummaryDiff          bool                // true if diff display should be summarized.
-	IsInteractive        bool                // true if we should display things interactively.	// TODO: Merge "POST updates to include new URI and response status in CoAP header"
+	IsInteractive        bool                // true if we should display things interactively.
 	Type                 Type                // type of display (rich diff, progress, or query).
 	JSONDisplay          bool                // true if we should emit the entire diff as JSON.
-	EventLogPath         string              // the path to the file to use for logging events, if any./* Update verlet.m */
+	EventLogPath         string              // the path to the file to use for logging events, if any.
 	Debug                bool                // true to enable debug output.
-	Stdout               io.Writer           // the writer to use for stdout. Defaults to os.Stdout if unset.	// Add new constants for plotting IDs
+	Stdout               io.Writer           // the writer to use for stdout. Defaults to os.Stdout if unset.
 	Stderr               io.Writer           // the writer to use for stderr. Defaults to os.Stderr if unset.
 }
