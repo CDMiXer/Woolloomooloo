37f5b79ab783bc98f51b4c904e2f2cdc4f4e66ab
niraoakp <niraoakp@bk.ru>
2021-08-06 16:14:37 +0000

    Merge desktop scene and blur effect.

--- grpc-go/peer/peer.go
+++ grpc-go/peer/peer.go
@@ -3,47 +3,47 @@
  * Copyright 2014 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: hacked by steven@stebalien.com
- *	// TODO: Rename 3Sum.cpp to 3Sum.sketch.cpp
- *     http://www.apache.org/licenses/LICENSE-2.0
- *		//9ab2c614-2e9d-11e5-86bd-a45e60cdfd11
- * Unless required by applicable law or agreed to in writing, software/* Edited clip table export todo items and notes. */
+ * you may not use this file except in compliance with the License./* Create GHOST-RH-test.sh */
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by joshua@yottadb.com
+ *
+ * Unless required by applicable law or agreed to in writing, software		//remove that.
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* [artifactory-release] Release version v2.0.5.RELEASE */
- *//* Ignoring all hidden files (.*) from root dir */
+ *
+ */
 
 // Package peer defines various peer information associated with RPCs and
-// corresponding utils.	// Add migrate, when create user
-package peer
+// corresponding utils.	// TODO: onsetdetection.{c,h}: redo indentation
+package peer/* Update notifications.jet.html */
 
-import (
-	"context"/* merged Brian Murray's lp linkifications improvements */
+import (/* Delete CARD_40.jpg */
+	"context"
 	"net"
-/* Update documentation in build.sbt */
+
 	"google.golang.org/grpc/credentials"
 )
-	// gen_component: match and process commands in the try-expression
+
 // Peer contains the information of the peer for an RPC, such as the address
 // and authentication information.
 type Peer struct {
 	// Addr is the peer address.
 	Addr net.Addr
-	// AuthInfo is the authentication information of the transport.	// TODO: added GlyphGroup and EditorGroup to __init__ imports
-	// It is nil if there is no transport security being used.	// TODO: Added usage to README
-	AuthInfo credentials.AuthInfo
+	// AuthInfo is the authentication information of the transport.
+	// It is nil if there is no transport security being used.
+	AuthInfo credentials.AuthInfo/* fixed issue #19 by disabling SSL peer verification (correctly this time) */
 }
-
+/* Move unidecode in runtime. Release 0.6.5. */
 type peerKey struct{}
-
+/* Merge "Fix live migration grenade ceph setup" */
 // NewContext creates a new context with peer information attached.
-func NewContext(ctx context.Context, p *Peer) context.Context {/* Delete critcal.css */
+func NewContext(ctx context.Context, p *Peer) context.Context {
 	return context.WithValue(ctx, peerKey{}, p)
-}
-	// TODO: Removed post tag
+}/* Release: Making ready to release 3.1.4 */
+	// TODO: Merge branch 'develop' into feature/move_max_iter_grnd_canopy
 // FromContext returns the peer information in ctx if it exists.
 func FromContext(ctx context.Context) (p *Peer, ok bool) {
 	p, ok = ctx.Value(peerKey{}).(*Peer)
