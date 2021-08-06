34dbf117393f0d64507e47b3c4eb57ea622bdf62
niraoakp <niraoakp@bk.ru>
2021-08-06 17:06:10 +0000

    Merge "Always export IMAGE_NAME"

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -1,10 +1,10 @@
-package sqldb
-		//Fixed dead Gem link
-import "upper.io/db.v3/lib/sqlbuilder"
+package sqldb/* Release 0.7.1 with updated dependencies */
+
+import "upper.io/db.v3/lib/sqlbuilder"	// TODO: hacked by mikeal.rogers@gmail.com
 
 // represent a straight forward change that is compatible with all database providers
-type ansiSQLChange string
-
+type ansiSQLChange string/* Release of XWiki 9.10 */
+	// Automatic changelog generation for PR #14423 [ci skip]
 func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
 	_, err := session.Exec(string(s))
 	return err
