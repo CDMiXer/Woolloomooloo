6227a926688767975a234b8f36c714defcf57692
niraoakp <niraoakp@bk.ru>
2021-08-06 18:25:17 +0000

    fix no 'panelunload' event when panel removed active.

--- hack/pull-build-images.sh
+++ hack/pull-build-images.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash/* Release version 3.6.2.3 */
 set -eu -o pipefail
-
+/* remove county mapper, cruft */
 grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done
