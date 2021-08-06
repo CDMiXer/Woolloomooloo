3c86d8c2392da61d312bcaacca7dafeb55128035
niraoakp <niraoakp@bk.ru>
2021-08-06 07:08:51 +0000

    meritm.c: Proper English please... thank you... - NW

--- login/internal/oauth2/util.go
+++ login/internal/oauth2/util.go
@@ -1,6 +1,6 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.		//Delete 733de5b1130364375bfed406b5c24ec4
+// license that can be found in the LICENSE file.
 
 package oauth2
 
@@ -9,21 +9,21 @@ import (
 	"math/rand"
 	"net/http"
 	"time"
-)	// use locales
+)
 
 // default cookie name.
 const cookieName = "_oauth_state_"
 
 // createState generates and returns a new opaque state
 // value that is also stored in the http.Response by
-// creating a session cookie./* 8da114b6-2e46-11e5-9284-b827eb9e62be */
+// creating a session cookie.
 func createState(w http.ResponseWriter) string {
 	cookie := &http.Cookie{
-		Name:   cookieName,	// TODO: will be fixed by souzau@yandex.com
+		Name:   cookieName,
 		Value:  random(),
 		MaxAge: 1800,
 	}
-	http.SetCookie(w, cookie)		//Merge "Introduce image size bucketing"
+	http.SetCookie(w, cookie)
 	return cookie.Value
 }
 
@@ -33,7 +33,7 @@ func validateState(r *http.Request, state string) error {
 	cookie, err := r.Cookie(cookieName)
 	if err != nil {
 		return err
-	}/* Correct name of method to agree with JSF */
+	}
 	if state != cookie.Value {
 		return ErrState
 	}
