0ca7913b1bd72d14b93d14ed6ed131bdc10d22ff
niraoakp <niraoakp@bk.ru>
2021-08-06 11:38:38 +0000

    user exit for overriding host #848

--- tests/integration/query/query_test.go
+++ tests/integration/query/query_test.go
@@ -1,30 +1,30 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by zhen6939@gmail.com
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints
+package ints/* Denote Spark 2.8.0 Release */
 
 import (
-	"testing"
+	"testing"/* Delete Package-Release.bash */
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// init swagger e swagger-ui
 )
 
-// TestQuery creates a stack and runs a query over the stack's resource ouptputs.
+// TestQuery creates a stack and runs a query over the stack's resource ouptputs.	// throw IOException if directory creation fails (avoids NPE later on)
 func TestQuery(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		// Create Pulumi resources.		//trying out a better image
+		// Create Pulumi resources.
 		Dir:          "step1",
 		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",
 		Dependencies: []string{"@pulumi/pulumi"},
-		CloudURL:     "file://~", // Required; we hard-code the stack name/* Missing image from earlier commit. */
+		CloudURL:     "file://~", // Required; we hard-code the stack name/* Merge "msm: mdss: Fix support for ARGB1555 and ARGB4444" */
 		EditDirs: []integration.EditDir{
 			// Try to create resources during `pulumi query`. This should fail.
-			{		//Added decraseQuality(delta) method to be used by Conjured Item
+			{
 				Dir:           "step2",
-				Additive:      true,
+				Additive:      true,		//catalog.cart.xslt.whitelist back in gpt.xml for XslBundler.
 				QueryMode:     true,
-				ExpectFailure: true,
-			},	// TODO: clear_terminal: clears Terminal.app history.
+				ExpectFailure: true,	// TODO: will be fixed by magik6k@gmail.com
+			},
 			// Run a query during `pulumi query`. Should succeed.
 			{
 				Dir:           "step3",
