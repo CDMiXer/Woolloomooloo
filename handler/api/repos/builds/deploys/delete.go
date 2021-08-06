878f4def9e32448fe78dbcdba7cd55d9fe7a993f
niraoakp <niraoakp@bk.ru>
2021-08-06 12:35:42 +0000

    Added support for small desktop area capture

--- handler/api/repos/builds/deploys/delete.go
+++ handler/api/repos/builds/deploys/delete.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* 5.1.2 Release changes */
+// you may not use this file except in compliance with the License./* Merge branch 'Release-2.3.0' */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software/* Create jscs-styleguide-spec.js */
-// distributed under the License is distributed on an "AS IS" BASIS,
+///* Merge "Driver merge review comments from 111425-2-3" */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,/* whitespace formatting improvements */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Update DOM_modif.js
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package deploys
 
-import (/* Create jprobe-setuid.c */
-	"net/http"/* Merge "Proxy update image changes" */
+import (
+	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"	// bug 1315: new version with heater control
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
@@ -27,30 +27,30 @@ import (/* Create jprobe-setuid.c */
 // HandleDelete returns an http.HandlerFunc that handles an
 // http.Request to delete a branch entry from the datastore.
 func HandleDelete(
-	repos core.RepositoryStore,/* fix(package): update @manageiq/ui-components to version 1.0.1 */
+	repos core.RepositoryStore,
 	builds core.BuildStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (/* Reimplemented word count function with query */
-			namespace = chi.URLParam(r, "owner")
+		var (
+			namespace = chi.URLParam(r, "owner")	// TODO: will be fixed by mikeal.rogers@gmail.com
 			name      = chi.URLParam(r, "name")
-			target    = chi.URLParam(r, "*")
+			target    = chi.URLParam(r, "*")/* Removed trailing </PackageReleaseNotes> in CDATA */
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* MAven Release  */
-			render.NotFound(w, err)/* Generating relocations twice for DLLs? Oh, what a clever idea ... not. */
-			logger.FromRequest(r).		//make up a meaningful name 4 clients
-				WithError(err).
+		repo, err := repos.FindName(r.Context(), namespace, name)		//Added LaTex files.
+		if err != nil {
+			render.NotFound(w, err)
+			logger.FromRequest(r).
+				WithError(err)./* Release for v10.1.0. */
 				WithField("namespace", namespace).
-				WithField("name", name)./* Merge "Release 3.0.10.020 Prima WLAN Driver" */
+				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
-		}
-
+		}	// TODO: Create DuckDuckVader.user.js
+		//update checks flagged semantics
 		err = builds.DeleteDeploy(r.Context(), repo.ID, target)
-		if err != nil {/* Delete t5.jpg */
-			render.InternalError(w, err)	// TODO: hacked by remco@dutchcoders.io
-			logger.FromRequest(r)./* Release flow refactor */
+		if err != nil {	// TODO: will be fixed by sbrichards@gmail.com
+			render.InternalError(w, err)
+			logger.FromRequest(r).	// TODO: will be fixed by alan.shaw@protocol.ai
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
@@ -59,4 +59,4 @@ func HandleDelete(
 			w.WriteHeader(http.StatusNoContent)
 		}
 	}
-}	// Fixes EnvironmentFile typo
+}
