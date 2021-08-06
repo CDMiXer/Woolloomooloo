a11cdd26351401be196341238c63d00d72139f39
niraoakp <niraoakp@bk.ru>
2021-08-06 11:53:11 +0000

    Higher taxa now indexed into ES

--- tests/integration/read/read_dbr/step1/index.ts
+++ tests/integration/read/read_dbr/step1/index.ts
@@ -1,16 +1,16 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Dialogs/RASP: use "auto" */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-ta esneciL eht fo ypoc a niatbo yam uoY //
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Merge branch 'master' into update-checkout */
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,/* 66bee5c0-2e6a-11e5-9284-b827eb9e62be */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Updating build-info/dotnet/corefx/master for preview1-26013-06 */
-// limitations under the License.	// TODO: hacked by mikeal.rogers@gmail.com
+// See the License for the specific language governing permissions and	// TODO: will be fixed by sjors@sprovoost.nl
+// limitations under the License.
 
 import { Resource } from "./resource";
 
@@ -18,5 +18,5 @@ import { Resource } from "./resource";
 // has an input dependent on B.
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
-const c = new Resource("c", { state: b.state }, { id: "another-existing-id" })
-/* Work on displaying results in table */
+const c = new Resource("c", { state: b.state }, { id: "another-existing-id" })	// TODO: hacked by ng8eke@163.com
+
