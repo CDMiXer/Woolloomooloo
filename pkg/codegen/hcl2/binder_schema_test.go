5764ad1e365548836ae7f813f9b29f17489002ad
niraoakp <niraoakp@bk.ru>
2021-08-06 22:22:53 +0000

    cursor -> converter

--- pkg/codegen/hcl2/binder_schema_test.go
+++ pkg/codegen/hcl2/binder_schema_test.go
@@ -1,13 +1,13 @@
 package hcl2
 
-import (
+import (		//Included gradlew files
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"/* be972a94-2e62-11e5-9284-b827eb9e62be */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"	// Create ChallengeBackground.md
 )
-
+/* fixed crsah in singleplayer generation script, thanks dizekat! */
 func BenchmarkLoadPackage(b *testing.B) {
 	loader := schema.NewPluginLoader(test.NewHost(testdataPath))
 
