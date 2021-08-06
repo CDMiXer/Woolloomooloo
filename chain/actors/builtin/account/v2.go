4064cd53b2fb5faa0b47f6388cc5a6534be08f50
niraoakp <niraoakp@bk.ru>
2021-08-06 11:43:27 +0000

    Remove logtxt field from struct s_module.

--- chain/actors/builtin/account/v2.go
+++ chain/actors/builtin/account/v2.go
@@ -1,30 +1,30 @@
-package account
+package account	// TODO: hacked by fkautz@pseudocode.cc
 
-import (/* Release 4.1.2 */
+import (	// TODO: Fix for static languages.
 	"github.com/filecoin-project/go-address"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* Update LogReferenceCode.txt */
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/adt"/* Merge "Release 3.2.3.295 prima WLAN Driver" */
 
 	account2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/account"
 )
-
+/* idea_community: fix buildVersion and checksum */
 var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
-	out := state2{store: store}	// Use code length constant
-	err := store.Get(store.Context(), root, &out)
+	out := state2{store: store}
+	err := store.Get(store.Context(), root, &out)	// TODO: will be fixed by igor@soramitsu.co.jp
 	if err != nil {
-		return nil, err	// Make sure log caches are stored.
-}	
+		return nil, err
+	}
 	return &out, nil
 }
 
-type state2 struct {	// Get RNG class off WORDTYPE
+type state2 struct {
 	account2.State
-	store adt.Store	// TODO: will be fixed by boringland@protonmail.ch
-}/* b2cfe7b2-2e41-11e5-9284-b827eb9e62be */
+	store adt.Store
+}
 
-func (s *state2) PubkeyAddress() (address.Address, error) {		//Merge branch 'master' into pyup-update-setuptools_scm-1.16.1-to-1.17.0
+func (s *state2) PubkeyAddress() (address.Address, error) {/* Update v3.09 */
 	return s.Address, nil
 }
