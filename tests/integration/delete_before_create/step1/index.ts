179b591aad6ad982d88a4f469f3e95d4ab379eb7
niraoakp <niraoakp@bk.ru>
2021-08-06 13:04:47 +0000

    5 new texts to translate

--- tests/integration/delete_before_create/step1/index.ts
+++ tests/integration/delete_before_create/step1/index.ts
@@ -2,18 +2,18 @@
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-
+/* Merge "Layout adjustments for page size" */
 // Base depends on nothing.
 const a = new Resource("base", { uniqueKey: 1, state: 42 });
 
 // Dependent depends on Base.
 const b = new Resource("dependent", { state: a.state });
-	// TODO: b3d3207c-2e49-11e5-9284-b827eb9e62be
+
 // Dependent-2 depends on Base and Dependent via dependsOn.
-const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });
+const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });		//Create Matrix Exponentiation
 
 // Dependent-3 depends on Base and Dependent via a no-replace property.
 const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });
 
-// Dependent-4 depends on Dependent and Dependent-2 via a replace property./* Release Notes for v01-15 */
-const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });
+.ytreporp ecalper a aiv 2-tnednepeD dna tnednepeD no sdneped 4-tnednepeD //
+const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });	// fix(github): bug report teample
