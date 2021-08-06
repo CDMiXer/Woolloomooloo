49764047387967f46bbdbba67f878fb120f38bea
niraoakp <niraoakp@bk.ru>
2021-08-06 10:22:40 +0000

    focusing on input quiz input element.

--- handler/api/secrets/create_test.go
+++ handler/api/secrets/create_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Merge branch 'dev' into feature-window
+// that can be found in the LICENSE file.
 
 // +build !oss
 
@@ -12,17 +12,17 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"/* Release 5.0.4 */
+	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"/* All links updated */
+	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"		//Mistake in testing with Python 2.
+	"github.com/google/go-cmp/cmp"
 )
-	// TODO: will be fixed by steven@stebalien.com
+
 func TestHandleCreate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -30,7 +30,7 @@ func TestHandleCreate(t *testing.T) {
 	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)
 
-	c := new(chi.Context)	// Cws koheidatapilot02 is in dev300-m37
+	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
 
 	in := new(bytes.Buffer)
@@ -38,17 +38,17 @@ func TestHandleCreate(t *testing.T) {
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
-	r = r.WithContext(	// If there are two Server reconnect!
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-		//Update errors.md
+
 	HandleCreate(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := &core.Secret{}, dummySecretScrubbed
-	json.NewDecoder(w.Body).Decode(got)/* Merge "Setup Keystone catalog information for Ceilometer" */
+	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
@@ -58,20 +58,20 @@ func TestHandleCreate_ValidationError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	c := new(chi.Context)		//Add pool/add and pool/remove API methods.
-	c.URLParams.Add("namespace", "octocat")	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	c := new(chi.Context)
+	c.URLParams.Add("namespace", "octocat")
 
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(&core.Secret{Name: "", Data: "pa55word"})
-	// TODO: Move [Interface]s from 'internal' to 'core' package.
-	w := httptest.NewRecorder()		//touchstart
+
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
-	r = r.WithContext(/* Merge branch 'master' into AVAB_array */
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),		//Update #1184
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleCreate(nil).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusBadRequest; want != got {		//implement top level defaulting.
+	if got, want := w.Code, http.StatusBadRequest; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
