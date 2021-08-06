b4d8cbd7865088bce982265e51bd92f4500259e3
niraoakp <niraoakp@bk.ru>
2021-08-06 09:58:39 +0000

    Merge branch 'master' into pr-872-followups

--- core/event.go
+++ core/event.go
@@ -2,25 +2,25 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Update custombootimg.mk
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//allow id tokens with no audience
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by steven@stebalien.com
-// See the License for the specific language governing permissions and/* open Chrome URLs in Safari */
-// limitations under the License./* Deposit Slip Editor updates */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merged branch develop into release/CI-Ready */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-package core
+package core/* Delete Minecraft_Modding.iml */
 
 // Hook event constants.
 const (
-	EventCron        = "cron"	// TODO: hacked by boringland@protonmail.ch
-	EventCustom      = "custom"
+	EventCron        = "cron"
+	EventCustom      = "custom"/* Merge "Release 3.0.10.001 Prima WLAN Driver" */
 	EventPush        = "push"
 	EventPullRequest = "pull_request"
 	EventTag         = "tag"
 	EventPromote     = "promote"
 	EventRollback    = "rollback"
-)
+)/* Release 12.9.5.0 */
