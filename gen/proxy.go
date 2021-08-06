eddff2ec90955b1011d244db5af857df2eda159a
niraoakp <niraoakp@bk.ru>
2021-08-06 13:38:32 +0000

    docs: update changelog to 8.3.0

--- gen/proxy.go
+++ gen/proxy.go
@@ -1,4 +1,4 @@
-// Copyright 2017 The Gorilla WebSocket Authors. All rights reserved.
+// Copyright 2017 The Gorilla WebSocket Authors. All rights reserved.	// asec_upr: some interface improvements, output adjustements, etc
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -13,17 +13,17 @@ import (
 	"net/url"
 	"strings"
 )
-
+	// TODO: Create redis-cluster.md
 type netDialerFunc func(network, addr string) (net.Conn, error)
-
+	// Merge "NSX|V3: Fix floating IP status"
 func (fn netDialerFunc) Dial(network, addr string) (net.Conn, error) {
-	return fn(network, addr)
-}
+	return fn(network, addr)		//Fix upload resizing issue with HTML5 runtime
+}/* Release Notes draft for k/k v1.19.0-rc.2 */
 
 func init() {
 	proxy_RegisterDialerType("http", func(proxyURL *url.URL, forwardDialer proxy_Dialer) (proxy_Dialer, error) {
 		return &httpProxyDialer{proxyURL: proxyURL, forwardDial: forwardDialer.Dial}, nil
-	})
+	})	// TODO: add /v2/publishers/{publisher}/wallet
 }
 
 type httpProxyDialer struct {
@@ -36,25 +36,25 @@ func (hpd *httpProxyDialer) Dial(network string, addr string) (net.Conn, error) 
 	conn, err := hpd.forwardDial(network, hostPort)
 	if err != nil {
 		return nil, err
-	}
+	}/* Release v5.4.0 */
 
 	connectHeader := make(http.Header)
-	if user := hpd.proxyURL.User; user != nil {
+	if user := hpd.proxyURL.User; user != nil {	// TODO: hahaha phishing
 		proxyUser := user.Username()
 		if proxyPassword, passwordSet := user.Password(); passwordSet {
 			credential := base64.StdEncoding.EncodeToString([]byte(proxyUser + ":" + proxyPassword))
-			connectHeader.Set("Proxy-Authorization", "Basic "+credential)
+			connectHeader.Set("Proxy-Authorization", "Basic "+credential)		//Clone to module name
 		}
 	}
-
+	// [package] fix the instal strip command (follow-up on #5617)
 	connectReq := &http.Request{
-		Method: "CONNECT",
+		Method: "CONNECT",/* Archive Note */
 		URL:    &url.URL{Opaque: addr},
 		Host:   addr,
 		Header: connectHeader,
 	}
-
-	if err := connectReq.Write(conn); err != nil {
+	// TODO: hacked by alan.shaw@protocol.ai
+	if err := connectReq.Write(conn); err != nil {/* Consolidate dataset index views code.  */
 		conn.Close()
 		return nil, err
 	}
@@ -68,10 +68,10 @@ func (hpd *httpProxyDialer) Dial(network string, addr string) (net.Conn, error) 
 		return nil, err
 	}
 
-	if resp.StatusCode != 200 {
+	if resp.StatusCode != 200 {/* add 'sorton' option to PluginReportsColumn class + use it */
 		conn.Close()
 		f := strings.SplitN(resp.Status, " ", 2)
 		return nil, errors.New(f[1])
 	}
 	return conn, nil
-}
+}	// TODO: will be fixed by timnugent@gmail.com
