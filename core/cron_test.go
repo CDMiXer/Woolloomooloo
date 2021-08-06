5d7ec368a0544dd81b0c7f989f56df6f7054e0f7
niraoakp <niraoakp@bk.ru>
2021-08-06 12:26:04 +0000

    Create supported-orchestrators.json

--- core/cron_test.go
+++ core/cron_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// docs: copy edit to landing page
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Release 1.4.0.1 */
+// +build !oss
 
 package core
