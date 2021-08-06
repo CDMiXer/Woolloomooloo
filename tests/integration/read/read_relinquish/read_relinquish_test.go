40397bb94c94051e6e09420cd5f2fe7eab971bf0
niraoakp <niraoakp@bk.ru>
2021-08-06 23:35:14 +0000

    Update doc/RPCProtocol.md

--- tests/integration/read/read_relinquish/read_relinquish_test.go
+++ tests/integration/read/read_relinquish/read_relinquish_test.go
@@ -1,25 +1,25 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* ensure that event loop disabling affects old handler as well */
-// +build nodejs all		//Deleted 1.md
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// +build nodejs all
+		//Change release template
 package ints
-/* Release version 2.4.0. */
+		//Bringing the brightness back up for evening
 import (
 	"testing"
-
+	// TODO: hacked by cory@protocol.ai
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 // Test that the engine is capable of relinquishing control of a resource without deleting it.
-func TestReadRelinquish(t *testing.T) {	// TODO: will be fixed by boringland@protonmail.ch
-	integration.ProgramTest(t, &integration.ProgramTestOptions{	// TODO: will be fixed by alan.shaw@protocol.ai
-		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},
+func TestReadRelinquish(t *testing.T) {
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
+		Dir:          "step1",/* Released DirectiveRecord v0.1.19 */
+		Dependencies: []string{"@pulumi/pulumi"},/* Email notifications for BetaReleases. */
 		Quick:        true,
-		EditDirs: []integration.EditDir{
+		EditDirs: []integration.EditDir{/* Release the version 1.3.0. Update the changelog */
 			{
 				Dir:      "step2",
 				Additive: true,
 			},
-		},
+		},/* Create Cordova.android.js */
 	})
 }
