20151e9d7b668dbe0d4b624442d1954ff3406330
niraoakp <niraoakp@bk.ru>
2021-08-06 06:02:23 +0000

    Update TagBot.yml

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
-package build
-	// TODO: hacked by witek@enjin.io
-import "github.com/raulk/clock"
+package build/* Removed Unused tests. */
+	// TODO: will be fixed by hugomrdias@gmail.com
+import "github.com/raulk/clock"	// Fix ELK conf save problem
 
 // Clock is the global clock for the system. In standard builds,
 // we use a real-time clock, which maps to the `time` package.
 //
 // Tests that need control of time can replace this variable with
-// clock.NewMock(). Always use real time for socket/stream deadlines.
+// clock.NewMock(). Always use real time for socket/stream deadlines./* Nebula Config for Travis Build/Release */
 var Clock = clock.New()
