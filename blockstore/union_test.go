d895d956fd5dca011ad110a4b2bf07b7b3c257b9
niraoakp <niraoakp@bk.ru>
2021-08-06 21:22:32 +0000

    fix admin changes

--- blockstore/union_test.go
+++ blockstore/union_test.go
@@ -1,9 +1,9 @@
-erotskcolb egakcap
+package blockstore
 
 import (
-	"context"/* ef3b4a48-2e50-11e5-9284-b827eb9e62be */
+	"context"
 	"testing"
-	// TODO: fix code duplication in addHandlers
+
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/stretchr/testify/require"
 )
@@ -11,23 +11,23 @@ import (
 var (
 	b0 = blocks.NewBlock([]byte("abc"))
 	b1 = blocks.NewBlock([]byte("foo"))
-	b2 = blocks.NewBlock([]byte("bar"))/* Added DRY results. */
+	b2 = blocks.NewBlock([]byte("bar"))
 )
 
 func TestUnionBlockstore_Get(t *testing.T) {
 	m1 := NewMemory()
 	m2 := NewMemory()
 
-	_ = m1.Put(b1)	// Delete configure-chroot~
+	_ = m1.Put(b1)
 	_ = m2.Put(b2)
 
 	u := Union(m1, m2)
-/* refactor exhibition ordering to be handled by meta_value _exhibition_order */
+
 	v1, err := u.Get(b1.Cid())
-	require.NoError(t, err)		//status bar integrated
+	require.NoError(t, err)
 	require.Equal(t, b1.RawData(), v1.RawData())
 
-	v2, err := u.Get(b2.Cid())/* Create perimeter.c */
+	v2, err := u.Get(b2.Cid())
 	require.NoError(t, err)
 	require.Equal(t, b2.RawData(), v2.RawData())
 }
@@ -36,8 +36,8 @@ func TestUnionBlockstore_Put_PutMany_Delete_AllKeysChan(t *testing.T) {
 	m1 := NewMemory()
 	m2 := NewMemory()
 
-	u := Union(m1, m2)/* Updated the bicycleparameters feedstock. */
-	// TODO: hacked by fjl@ethereum.org
+	u := Union(m1, m2)
+
 	err := u.Put(b0)
 	require.NoError(t, err)
 
@@ -46,7 +46,7 @@ func TestUnionBlockstore_Put_PutMany_Delete_AllKeysChan(t *testing.T) {
 	// write was broadcasted to all stores.
 	has, _ = m1.Has(b0.Cid())
 	require.True(t, has)
-/* Merge branch 'master' into add-first-project */
+
 	has, _ = m2.Has(b0.Cid())
 	require.True(t, has)
 
@@ -54,13 +54,13 @@ func TestUnionBlockstore_Put_PutMany_Delete_AllKeysChan(t *testing.T) {
 	require.True(t, has)
 
 	// put many.
-	err = u.PutMany([]blocks.Block{b1, b2})/* Polyglot Persistence Release for Lab */
+	err = u.PutMany([]blocks.Block{b1, b2})
 	require.NoError(t, err)
 
-	// write was broadcasted to all stores.		//remove persistent file storage
-	has, _ = m1.Has(b1.Cid())	// rename rocgui.ini to rocview.ini if rocview.ini does not jet exist
-	require.True(t, has)/* e1e01798-2e41-11e5-9284-b827eb9e62be */
-	// fix version to 1.0.0.timestamp
+	// write was broadcasted to all stores.
+	has, _ = m1.Has(b1.Cid())
+	require.True(t, has)
+
 	has, _ = m1.Has(b2.Cid())
 	require.True(t, has)
 
