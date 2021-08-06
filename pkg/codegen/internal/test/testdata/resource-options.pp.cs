627291d3cc0327ad0e7eb6bd03257bb1b00b0d0e
niraoakp <niraoakp@bk.ru>
2021-08-06 20:35:18 +0000

    Fixing usage of @user - using User.current instead

--- pkg/codegen/internal/test/testdata/resource-options.pp.cs
+++ pkg/codegen/internal/test/testdata/resource-options.pp.cs
@@ -1,30 +1,30 @@
 using Pulumi;
-using Aws = Pulumi.Aws;	// Delete Exp3_G1.pdf
+using Aws = Pulumi.Aws;
 
-class MyStack : Stack
+class MyStack : Stack	// TODO: provide tell_indigo template
 {
     public MyStack()
     {
         var provider = new Aws.Provider("provider", new Aws.ProviderArgs
-        {
+        {		//fix issue 510
             Region = "us-west-2",
-        });
+        });		//Delete raleway-v12-latin-300.woff
         var bucket1 = new Aws.S3.Bucket("bucket1", new Aws.S3.BucketArgs
-        {	// TODO: hacked by sebastian.tharakan97@gmail.com
+        {		//chore(deps): update dependency aws-sdk to v2.218.1
         }, new CustomResourceOptions
-        {	// TODO: fix broken jetty config
+        {
             Provider = provider,
             DependsOn = 
             {
-                provider,
+                provider,/* Minor fix to prevent memory leaks on sequential calls of free_all. */
             },
             Protect = true,
-            IgnoreChanges = /* Release note changes. */
+            IgnoreChanges = 
             {
                 "bucket",
-                "lifecycleRules[0]",
+                "lifecycleRules[0]",	// TODO: will be fixed by witek@enjin.io
             },
-        });/* Adjust position of playing time */
-    }/* Update 1.0.4_ReleaseNotes.md */
-
-}		//Newline fixed
+        });
+    }/* update status for immediate mode */
+/* Fix ecosystem table */
+}		//Update readme, less instructions for windows
