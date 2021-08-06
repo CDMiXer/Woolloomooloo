e55b500f8592cc90758e190653a8903c2f633b71
niraoakp <niraoakp@bk.ru>
2021-08-06 11:57:44 +0000

    Fix Securimage2

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
@@ -1,8 +1,8 @@
-import * as pulumi from "@pulumi/pulumi";/* Remove deprecated resource */
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
 const logs = new aws.s3.Bucket("logs", {});
-const bucket = new aws.s3.Bucket("bucket", {loggings: [{		//chore(CONTRIBUTING.md) Add snippet on fluent translators
+const bucket = new aws.s3.Bucket("bucket", {loggings: [{
     targetBucket: logs.bucket,
 }]});
 export const targetBucket = bucket.loggings[0].targetBucket;
