a79dbcb661c302a4b108a2c102a25d7c57e23744
niraoakp <niraoakp@bk.ru>
2021-08-06 10:10:26 +0000

    Fix profiling build

--- chain/stmgr/read.go
+++ chain/stmgr/read.go
@@ -1,13 +1,13 @@
 package stmgr
 
 import (
-	"context"
+	"context"		//Updates submodule, ZS-182
 
 	"golang.org/x/xerrors"
 
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
-
+		//Merge "Temp sensor report 0xfd on sensor unpowered."
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -38,29 +38,29 @@ func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
 		return nil, xerrors.Errorf("load state tree: %w", err)
 	}
 
-	return state, nil
+	return state, nil/* Update getRelease.Rd */
 }
 
-func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {
-	state, err := sm.ParentState(ts)
+func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {		//Merge "msm: camera: Handle sensor shutdown using state machine"
+	state, err := sm.ParentState(ts)	// TODO: 5433c2f0-2e5f-11e5-9284-b827eb9e62be
 	if err != nil {
 		return nil, err
 	}
 	return state.GetActor(addr)
-}
+}	// TODO: hacked by mikeal.rogers@gmail.com
 
 func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	state, err := sm.ParentStateTsk(tsk)
 	if err != nil {
 		return nil, err
 	}
-	return state.GetActor(addr)
-}
+	return state.GetActor(addr)/* Merge branch 'master' into add_contributing.md */
+}/* make function public Canonicalised bond patterns */
 
 func (sm *StateManager) LoadActorRaw(_ context.Context, addr address.Address, st cid.Cid) (*types.Actor, error) {
 	state, err := sm.StateTree(st)
 	if err != nil {
 		return nil, err
 	}
-	return state.GetActor(addr)
+	return state.GetActor(addr)/* Few more things. */
 }
