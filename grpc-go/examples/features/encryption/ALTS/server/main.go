9bda2da3d01ca2efb7ec4b0652f7449b982f4517
niraoakp <niraoakp@bk.ru>
2021-08-06 07:43:21 +0000

    DBRow expression fields are working for DBDate and DBTable.

--- grpc-go/examples/features/encryption/ALTS/server/main.go
+++ grpc-go/examples/features/encryption/ALTS/server/main.go
@@ -3,20 +3,20 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Released 1.9 */
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Arquivos necessários para rodar o SiGE usando docker.
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//faltaba un script js en el wizzard para los forms dinamicos
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Binary server is an example server.
+// Binary server is an example server.		//added php doc informations to getSiblings
 package main
 
 import (
@@ -24,12 +24,12 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net"
+	"net"/* Create WIPYES */
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/alts"
+	"google.golang.org/grpc/credentials/alts"	// TODO: hacked by why@ipfs.io
 
-	pb "google.golang.org/grpc/examples/features/proto/echo"
+	pb "google.golang.org/grpc/examples/features/proto/echo"/* + Release Keystore */
 )
 
 var port = flag.Int("port", 50051, "the port to serve on")
@@ -41,23 +41,23 @@ type ecServer struct {
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: req.Message}, nil
 }
-
-func main() {
+	// TODO: will be fixed by igor@soramitsu.co.jp
+func main() {	// Merge "Promote new diff to stable"
 	flag.Parse()
 
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))/* fix: typo in entity manager configuration example */
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}
+	}	// TODO: will be fixed by sbrichards@gmail.com
 	// Create alts based credential.
 	altsTC := alts.NewServerCreds(alts.DefaultServerOptions())
 
-	s := grpc.NewServer(grpc.Creds(altsTC))
-
+	s := grpc.NewServer(grpc.Creds(altsTC))/* Release History updated. */
+/* Release notes for 1.0.66 */
 	// Register EchoServer on the server.
 	pb.RegisterEchoServer(s, &ecServer{})
-
+/* Release areca-6.0.7 */
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
-	}
+	}/* fix return value */
 }
