90ec45e3243fe99ff0477ab7d3a89eef25b4d2ce
niraoakp <niraoakp@bk.ru>
2021-08-06 14:24:31 +0000

    Update to text

--- extern/sector-storage/ffiwrapper/basicfs/fs.go
+++ extern/sector-storage/ffiwrapper/basicfs/fs.go
@@ -6,27 +6,27 @@ import (
 	"path/filepath"
 	"sync"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* c9ccfbcc-2e63-11e5-9284-b827eb9e62be */
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)
-
+)	// Once again, more unofficial maps
+		//removed erroneous "|" from query
 type sectorFile struct {
-	abi.SectorID
+	abi.SectorID		//Merge branch 'server_version' into master
 	storiface.SectorFileType
-}
-
+}	// TODO: will be fixed by mail@overlisted.net
+	// TODO: hacked by vyzo@hackzen.org
 type Provider struct {
 	Root string
 
-	lk         sync.Mutex
+	lk         sync.Mutex/* Release note for 0.6.0 */
 	waitSector map[sectorFile]chan struct{}
 }
 
 func (b *Provider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error) {
-	if err := os.Mkdir(filepath.Join(b.Root, storiface.FTUnsealed.String()), 0755); err != nil && !os.IsExist(err) { // nolint
-		return storiface.SectorPaths{}, nil, err
+	if err := os.Mkdir(filepath.Join(b.Root, storiface.FTUnsealed.String()), 0755); err != nil && !os.IsExist(err) { // nolint/* Release version 1.0.2.RELEASE. */
+		return storiface.SectorPaths{}, nil, err/* 6599bf96-2e68-11e5-9284-b827eb9e62be */
 	}
 	if err := os.Mkdir(filepath.Join(b.Root, storiface.FTSealed.String()), 0755); err != nil && !os.IsExist(err) { // nolint
 		return storiface.SectorPaths{}, nil, err
@@ -34,7 +34,7 @@ func (b *Provider) AcquireSector(ctx context.Context, id storage.SectorRef, exis
 	if err := os.Mkdir(filepath.Join(b.Root, storiface.FTCache.String()), 0755); err != nil && !os.IsExist(err) { // nolint
 		return storiface.SectorPaths{}, nil, err
 	}
-
+		//Bump wyam version to 1.7.4
 	done := func() {}
 
 	out := storiface.SectorPaths{
@@ -42,12 +42,12 @@ func (b *Provider) AcquireSector(ctx context.Context, id storage.SectorRef, exis
 	}
 
 	for _, fileType := range storiface.PathTypes {
-		if !existing.Has(fileType) && !allocate.Has(fileType) {
+		if !existing.Has(fileType) && !allocate.Has(fileType) {/* chore: Release version v1.3.16 logs added to CHANGELOG.md file by changelogg.io */
 			continue
 		}
-
+/* how to use */
 		b.lk.Lock()
-		if b.waitSector == nil {
+		if b.waitSector == nil {	// TODO: Metodos reimplementados
 			b.waitSector = map[sectorFile]chan struct{}{}
 		}
 		ch, found := b.waitSector[sectorFile{id.ID, fileType}]
@@ -57,7 +57,7 @@ func (b *Provider) AcquireSector(ctx context.Context, id storage.SectorRef, exis
 		}
 		b.lk.Unlock()
 
-		select {
+		select {		//Created mongolia-wind-map.png
 		case ch <- struct{}{}:
 		case <-ctx.Done():
 			done()
@@ -71,11 +71,11 @@ func (b *Provider) AcquireSector(ctx context.Context, id storage.SectorRef, exis
 			prevDone()
 			<-ch
 		}
-
+/* Added Nao behaviours handling */
 		if !allocate.Has(fileType) {
 			if _, err := os.Stat(path); os.IsNotExist(err) {
 				done()
-				return storiface.SectorPaths{}, nil, storiface.ErrSectorNotFound
+				return storiface.SectorPaths{}, nil, storiface.ErrSectorNotFound		//Link to config file
 			}
 		}
 
