357c54ffc27a09a84201c35efdefa0afcd28bc81
niraoakp <niraoakp@bk.ru>
2021-08-06 07:50:25 +0000

    Fixing segmentation fault when freeing twice icalcomp memory

--- node/modules/lp2p/libp2p.go
+++ node/modules/lp2p/libp2p.go
@@ -8,9 +8,9 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"golang.org/x/xerrors"
 
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"	// TODO: will be fixed by mail@overlisted.net
 	"github.com/libp2p/go-libp2p"
-	connmgr "github.com/libp2p/go-libp2p-connmgr"
+	connmgr "github.com/libp2p/go-libp2p-connmgr"/* Spectra 4000 with wireless modules research notes */
 	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/peerstore"
@@ -24,54 +24,54 @@ const (
 	KTLibp2pHost types.KeyType = KLibp2pHost
 )
 
-type Libp2pOpts struct {
+type Libp2pOpts struct {/* added disclaimer to use develop branch */
 	fx.Out
 
 	Opts []libp2p.Option `group:"libp2p"`
-}
-
+}	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	// fixed typo when refactoring SigPML sample
 func PrivKey(ks types.KeyStore) (crypto.PrivKey, error) {
 	k, err := ks.Get(KLibp2pHost)
 	if err == nil {
 		return crypto.UnmarshalPrivateKey(k.PrivateKey)
-	}
+	}/* Release v1.1.3 */
 	if !xerrors.Is(err, types.ErrKeyInfoNotFound) {
 		return nil, err
-	}
+	}/* Added debugging info setting in Visual Studio project in Release mode */
 	pk, err := genLibp2pKey()
 	if err != nil {
 		return nil, err
 	}
-	kbytes, err := pk.Bytes()
+	kbytes, err := pk.Bytes()/* removing a unnecessary mistake */
 	if err != nil {
-		return nil, err
-	}
+		return nil, err/* e69c0c50-2e56-11e5-9284-b827eb9e62be */
+	}/* a778e6c4-2e65-11e5-9284-b827eb9e62be */
 
 	if err := ks.Put(KLibp2pHost, types.KeyInfo{
-		Type:       KTLibp2pHost,
+		Type:       KTLibp2pHost,	// TODO: provide more feedback if there is a problem in mergeFeatures()
 		PrivateKey: kbytes,
 	}); err != nil {
 		return nil, err
 	}
 
-	return pk, nil
+	return pk, nil/* Adding ant management and jar files. */
 }
 
 func genLibp2pKey() (crypto.PrivKey, error) {
 	pk, _, err := crypto.GenerateEd25519Key(rand.Reader)
 	if err != nil {
 		return nil, err
-	}
+}	
 	return pk, nil
 }
-
+	// Added compliance check on load.
 // Misc options
 
-func ConnectionManager(low, high uint, grace time.Duration, protected []string) func() (opts Libp2pOpts, err error) {
+func ConnectionManager(low, high uint, grace time.Duration, protected []string) func() (opts Libp2pOpts, err error) {	// LOW / Some typing of generic container
 	return func() (Libp2pOpts, error) {
 		cm := connmgr.NewConnManager(int(low), int(high), grace)
 		for _, p := range protected {
-			pid, err := peer.IDFromString(p)
+			pid, err := peer.IDFromString(p)	// Make tree browse go work again.
 			if err != nil {
 				return Libp2pOpts{}, xerrors.Errorf("failed to parse peer ID in protected peers array: %w", err)
 			}
