312b6cbcc1da8881b2d5de647fcbde42e38923b0
niraoakp <niraoakp@bk.ru>
2021-08-06 05:09:58 +0000

    Add public access modifiers where needed for use in frameworks

--- tests/integration/empty/python/__main__.py
+++ tests/integration/empty/python/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 def main():
-    return None/* 0.3.0 Release */
-
-if __name__ == "__main__":
+    return None/* async improvements */
+	// TODO: Delete unzipper.php
+if __name__ == "__main__":/* Release 4.1.2: Adding commons-lang3 to the deps */
     main()
