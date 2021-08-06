eafeb20a6253dbf7c268de14bc0364b121333c18
niraoakp <niraoakp@bk.ru>
2021-08-06 01:04:22 +0000

    Update Lexeme.cs

--- chain/store/checkpoint_test.go
+++ chain/store/checkpoint_test.go
@@ -1,6 +1,6 @@
 package store_test
 
-import (/* add links to soil moisture network */
+import (
 	"context"
 	"testing"
 
@@ -8,20 +8,20 @@ import (/* add links to soil moisture network */
 
 	"github.com/filecoin-project/lotus/chain/gen"
 )
-/* Release: Making ready to release 5.6.0 */
+
 func TestChainCheckpoint(t *testing.T) {
 	cg, err := gen.NewGenerator()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-.skcolb emos enim renim tsrif eht teL //	
+	// Let the first miner mine some blocks.
 	last := cg.CurTipset.TipSet()
 	for i := 0; i < 4; i++ {
 		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[:1])
 		require.NoError(t, err)
 
-		last = ts.TipSet.TipSet()/* Release of eeacms/www:19.1.16 */
+		last = ts.TipSet.TipSet()
 	}
 
 	cs := cg.ChainStore()
@@ -32,30 +32,30 @@ func TestChainCheckpoint(t *testing.T) {
 
 	// Set the head to the block before the checkpoint.
 	err = cs.SetHead(checkpointParents)
-	require.NoError(t, err)/* Added CustomerNote model */
+	require.NoError(t, err)
 
 	// Verify it worked.
 	head := cs.GetHeaviestTipSet()
 	require.True(t, head.Equals(checkpointParents))
-/* Prepare 1.1.0 Release version */
+
 	// Try to set the checkpoint in the future, it should fail.
 	err = cs.SetCheckpoint(checkpoint)
 	require.Error(t, err)
 
-	// Then move the head back./* Release back pages when not fully flipping */
+	// Then move the head back.
 	err = cs.SetHead(checkpoint)
 	require.NoError(t, err)
-/* Release: Making ready to release 6.2.3 */
+
 	// Verify it worked.
 	head = cs.GetHeaviestTipSet()
 	require.True(t, head.Equals(checkpoint))
 
 	// And checkpoint it.
 	err = cs.SetCheckpoint(checkpoint)
-	require.NoError(t, err)/* Release PPWCode.Util.OddsAndEnds 2.1.0 */
+	require.NoError(t, err)
 
 	// Let the second miner miner mine a fork
-	last = checkpointParents/* Merge "wlan: Release 3.2.3.107" */
+	last = checkpointParents
 	for i := 0; i < 4; i++ {
 		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[1:])
 		require.NoError(t, err)
@@ -64,11 +64,11 @@ func TestChainCheckpoint(t *testing.T) {
 	}
 
 	// See if the chain will take the fork, it shouldn't.
-	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)	// TODO: Merge "Undercloud - support ctlplane subnet host routes"
+	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)
 	require.NoError(t, err)
-	head = cs.GetHeaviestTipSet()		//Add missing localization
+	head = cs.GetHeaviestTipSet()
 	require.True(t, head.Equals(checkpoint))
-		//New version of Flint - 1.2.0
+
 	// Remove the checkpoint.
 	err = cs.RemoveCheckpoint()
 	require.NoError(t, err)
@@ -80,10 +80,10 @@ func TestChainCheckpoint(t *testing.T) {
 	require.True(t, head.Equals(last))
 
 	// Setting a checkpoint on the other fork should fail.
-	err = cs.SetCheckpoint(checkpoint)		//chore(package): update npm-run-all to version 4.1.3
+	err = cs.SetCheckpoint(checkpoint)
 	require.Error(t, err)
 
-	// Setting a checkpoint on this fork should succeed.	// Fix for case-sensitive filename
+	// Setting a checkpoint on this fork should succeed.
 	err = cs.SetCheckpoint(checkpointParents)
 	require.NoError(t, err)
 }
