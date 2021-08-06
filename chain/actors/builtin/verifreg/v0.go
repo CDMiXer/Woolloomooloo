0269916d970bc633cf0e151120e151d34f11ca81
niraoakp <niraoakp@bk.ru>
2021-08-06 20:01:56 +0000

    Competitors → Users ?

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -1,6 +1,6 @@
-package verifreg		//Update CHANGELOG for #13935
+package verifreg
 
-import (		//Updated the lapack feedstock.
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
@@ -9,21 +9,21 @@ import (		//Updated the lapack feedstock.
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
-	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"/* Preparing Release of v0.3 */
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
 
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
-	out := state0{store: store}/* Release 1.2.6 */
+	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-}	
+	}
 	return &out, nil
 }
-	// TODO: hacked by fkautz@pseudocode.cc
-type state0 struct {		//final tweaks to get working
+
+type state0 struct {
 	verifreg0.State
 	store adt.Store
 }
@@ -32,15 +32,15 @@ func (s *state0) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
-func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* Release version of SQL injection attacks */
+func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
-}	// #3228: Cliente é persistido nas vendas futuras onde não há cliente selecionado
+}
 
 func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
 }
-/* Release v0.02 */
-func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* Adding the databases (MySQL and Fasta) for RefSeq protein Release 61 */
+
+func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
 }
 
@@ -54,4 +54,4 @@ func (s *state0) verifiedClients() (adt.Map, error) {
 
 func (s *state0) verifiers() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.Verifiers)
-}	// added basic page redirect integration #182
+}
