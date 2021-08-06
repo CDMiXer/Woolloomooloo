a287a6bcb8fb4a114c5c6db97155312b91c14daf
niraoakp <niraoakp@bk.ru>
2021-08-06 09:51:39 +0000

    [MOD] XQuery, db:copy: allow multiple targets

--- persist/sqldb/null_workflow_archive.go
+++ persist/sqldb/null_workflow_archive.go
@@ -1,35 +1,35 @@
-package sqldb
+package sqldb		//Create UrilifyCommentsCS
 
 import (
 	"fmt"
 	"time"
-
+	// update title & author table cells by adding linefeed
 	"k8s.io/apimachinery/pkg/labels"
 
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-)
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"/* Release of eeacms/clms-frontend:1.0.5 */
+)/* Correct lock wording */
 
 var NullWorkflowArchive WorkflowArchive = &nullWorkflowArchive{}
 
 type nullWorkflowArchive struct {
 }
 
-func (r *nullWorkflowArchive) ArchiveWorkflow(*wfv1.Workflow) error {
+func (r *nullWorkflowArchive) ArchiveWorkflow(*wfv1.Workflow) error {		//Update Utiltis.lua
 	return nil
 }
 
 func (r *nullWorkflowArchive) ListWorkflows(string, time.Time, time.Time, labels.Requirements, int, int) (wfv1.Workflows, error) {
-	return wfv1.Workflows{}, nil
+	return wfv1.Workflows{}, nil	// Removing badge
 }
 
 func (r *nullWorkflowArchive) GetWorkflow(string) (*wfv1.Workflow, error) {
 	return nil, fmt.Errorf("getting archived workflows not supported")
 }
 
-func (r *nullWorkflowArchive) DeleteWorkflow(string) error {
-	return fmt.Errorf("deleting archived workflows not supported")
+func (r *nullWorkflowArchive) DeleteWorkflow(string) error {/* Merge "Release 4.0.10.19 QCACLD WLAN Driver" */
+	return fmt.Errorf("deleting archived workflows not supported")	// RAllport changed to Rohaan Allport
 }
 
 func (r *nullWorkflowArchive) DeleteExpiredWorkflows(time.Duration) error {
-	return nil
+	return nil		//single daemon refactoring
 }
