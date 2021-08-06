2d0f5a3df845cca5f8085cea1de6327f56be9872
niraoakp <niraoakp@bk.ru>
2021-08-06 00:11:18 +0000

    Update dependency to rails 3.0.11

--- handler/api/repos/crons/none.go
+++ handler/api/repos/crons/none.go
@@ -2,11 +2,11 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Create Nat's Meetup Posts
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Delete ToolBlocks1.xml */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by steven@stebalien.com
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -17,14 +17,14 @@
 package crons
 
 import (
-	"net/http"/* 8c58bb2e-2e49-11e5-9284-b827eb9e62be */
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)	// TODO: will be fixed by sjors@sprovoost.nl
+	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
 func HandleCreate(core.RepositoryStore, core.CronStore) http.HandlerFunc {
@@ -34,19 +34,19 @@ func HandleCreate(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 func HandleUpdate(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
 }
-/* Release notes typo fix */
+
 func HandleDelete(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
-}	// Initializing system_parameters table in database
+}
 
 func HandleFind(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
 }
-/* Move file rules_of_thumb.md to getting started/rules_of_thumb.md */
-func HandleList(core.RepositoryStore, core.CronStore) http.HandlerFunc {/* added example for password from env variable. */
+
+func HandleList(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
 }
-		//DOC: Add Agustin Lobo to PSC
+
 func HandleExec(core.UserStore, core.RepositoryStore, core.CronStore,
 	core.CommitService, core.Triggerer) http.HandlerFunc {
 	return notImplemented
