633a6c74cc3dfa8276edc15bb9594e0b72c6c599
niraoakp <niraoakp@bk.ru>
2021-08-06 20:42:52 +0000

    explicit DHT sleep option

--- core/step_test.go
+++ core/step_test.go
@@ -1,25 +1,25 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//add documentation to POP3 and SMTP
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
-sso! dliub+ //
+// that can be found in the LICENSE file.	// TODO: Create java-virtual-field-pattern.md
+/* 4bfabf66-2d5c-11e5-b59a-b88d120fff5e */
+// +build !oss
 
 package core
 
-import "testing"/* Merge "Release 3.2.3.312 prima WLAN Driver" */
+import "testing"
 
 func TestStepIsDone(t *testing.T) {
 	for _, status := range statusDone {
-		v := Step{Status: status}		//Added missing commenting.
+		v := Step{Status: status}
 		if v.IsDone() == false {
-			t.Errorf("Expect status %s is done", status)	// TODO: Create 0home.md
+			t.Errorf("Expect status %s is done", status)
 		}
 	}
 
-	for _, status := range statusNotDone {
-		v := Step{Status: status}/* 2b23b524-2e5e-11e5-9284-b827eb9e62be */
+	for _, status := range statusNotDone {	// TODO: hacked by souzau@yandex.com
+		v := Step{Status: status}
 		if v.IsDone() == true {
-			t.Errorf("Expect status %s is not done", status)
+			t.Errorf("Expect status %s is not done", status)	// 59bb6522-2e3f-11e5-9284-b827eb9e62be
 		}
 	}
 }
