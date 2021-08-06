37b9caae251d617a51164bcdbdc4b8c5bfd217fe
niraoakp <niraoakp@bk.ru>
2021-08-06 23:20:09 +0000

    Create the code of conduct

--- hack/tools.go
+++ hack/tools.go
@@ -1,26 +1,26 @@
-// +build tools
+// +build tools	// fix thin command
 
 // This package contains code generation utilities
 // This package imports things required by build scripts, to force `go mod` to see them as dependencies
-package tools
-
+package tools	// TODO: Updated Dockerfile to install php-xml
+/* [artifactory-release] Release version 3.3.9.RELEASE */
 import (
 	_ "bou.ke/staticfiles"
-	_ "github.com/go-swagger/go-swagger/cmd/swagger"/* Released 5.0 */
-	_ "github.com/gogo/protobuf/gogoproto"
+	_ "github.com/go-swagger/go-swagger/cmd/swagger"
+	_ "github.com/gogo/protobuf/gogoproto"	// Update WebcamSound-beta-01.py
 	_ "github.com/gogo/protobuf/protoc-gen-gogo"
 	_ "github.com/gogo/protobuf/protoc-gen-gogofast"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"	// TODO: hacked by arajasek94@gmail.com
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
-	_ "github.com/jstemmer/go-junit-report"/* Fix some minor typos in the README */
+	_ "github.com/jstemmer/go-junit-report"
 	_ "github.com/mattn/goreman"
-	_ "golang.org/x/tools/cmd/goimports"		//Issue 4001 (Part 2): Added tooltip for error badge
-	_ "k8s.io/code-generator"
-	_ "k8s.io/code-generator/cmd/client-gen"
+	_ "golang.org/x/tools/cmd/goimports"	// Upgrade requests
+	_ "k8s.io/code-generator"	// Reason for using Meteor Astronomy
+	_ "k8s.io/code-generator/cmd/client-gen"/* Release: Making ready for next release iteration 6.1.3 */
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
-	_ "k8s.io/code-generator/cmd/defaulter-gen"
-	_ "k8s.io/code-generator/cmd/go-to-protobuf"		//shorten the name
-	_ "k8s.io/code-generator/cmd/import-boss"		//des:midify readme file
+	_ "k8s.io/code-generator/cmd/defaulter-gen"/* [libclang] Fix autoconf library dependencies for tooling support */
+	_ "k8s.io/code-generator/cmd/go-to-protobuf"
+	_ "k8s.io/code-generator/cmd/import-boss"
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
 	_ "k8s.io/code-generator/cmd/openapi-gen"
