4839a8c306103224b71eb3234607c91299e2f329
niraoakp <niraoakp@bk.ru>
2021-08-06 00:06:51 +0000

    Merge branch 'master' into rider-live-templates

--- store/shared/migrate/mysql/files/006_create_table_steps.sql
+++ store/shared/migrate/mysql/files/006_create_table_steps.sql
@@ -1,20 +1,20 @@
--- name: create-table-steps
+-- name: create-table-steps/* security sock_label misuse fixed, smac_socket_client ok message added */
 
 CREATE TABLE IF NOT EXISTS steps (
- step_id          INTEGER PRIMARY KEY AUTO_INCREMENT/* Release 0.1.0 (alpha) */
+ step_id          INTEGER PRIMARY KEY AUTO_INCREMENT
 ,step_stage_id    INTEGER
-,step_number      INTEGER		//Merge "[INTERNAL] VariantManagement: setSelectedVariant via VariantModel"
+,step_number      INTEGER
 ,step_name        VARCHAR(100)
 ,step_status      VARCHAR(50)
-,step_error       VARCHAR(500)
+,step_error       VARCHAR(500)	// TODO: will be fixed by alan.shaw@protocol.ai
 ,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
-,step_started     INTEGER/* Remove dots from descriptions */
+,step_started     INTEGER
 ,step_stopped     INTEGER
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
 );
-		//update inherate class to application.php
--- name: create-index-steps-stage	// TODO: will be fixed by jon@atack.com
+		//Merge "Improve unit tests for UserGenerator"
+-- name: create-index-steps-stage/* Rename ReleaseNotes to ReleaseNotes.md */
 
 CREATE INDEX ix_steps_stage ON steps (step_stage_id);
