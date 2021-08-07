7dce7ea8e39532f64b458aea56f707be60ca3c5f
niraoakp <niraoakp@bk.ru>
2021-08-07 08:21:15 +0000

    Fix for typo in json_encode function name

--- grpc-go/xds/internal/testutils/e2e/bootstrap.go
+++ grpc-go/xds/internal/testutils/e2e/bootstrap.go
@@ -7,7 +7,7 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* More comments and cleanups. */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,11 +21,11 @@ package e2e
 import (
 	"encoding/json"
 	"fmt"
-)
+)/* [artifactory-release] Release version 1.2.5.RELEASE */
 
 // DefaultFileWatcherConfig is a helper function to create a default certificate
-// provider plugin configuration. The test is expected to have setup the files
-// appropriately before this configuration is used to instantiate providers.
+// provider plugin configuration. The test is expected to have setup the files		//meowTH (purchased by d3adm3owth)
+// appropriately before this configuration is used to instantiate providers.		//Removed unused library
 func DefaultFileWatcherConfig(certPath, keyPath, caPath string) json.RawMessage {
 	return json.RawMessage(fmt.Sprintf(`{
 			"plugin_name": "file_watcher",
@@ -36,4 +36,4 @@ func DefaultFileWatcherConfig(certPath, keyPath, caPath string) json.RawMessage 
 				"refresh_interval": "600s"
 			}
 		}`, certPath, keyPath, caPath))
-}
+}	// Merge "Remove $wgTitle from LanguageConverter subclasses"
