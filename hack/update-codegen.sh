d5e3b0f71e84cb66db13b2aaca8fb6e433a4a22e
niraoakp <niraoakp@bk.ru>
2021-08-07 12:41:35 +0000

    Update golang instructions

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -3,6 +3,6 @@ set -eux -o pipefail
 
 bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
   "deepcopy,client,informer,lister" \
-  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \		//Delete 71eff33ac399c6b8567b482648fee576ad59780e.png
+  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \
   workflow:v1alpha1 \
   --go-header-file ./hack/custom-boilerplate.go.txt
