62ca390638f22e1ee65b8a3f1e66b16956611906
niraoakp <niraoakp@bk.ru>
2021-08-07 23:44:02 +0000

    Different variable in loop and its body

--- tests/integration/recreate_resource_check/step1/index.ts
+++ tests/integration/recreate_resource_check/step1/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* FIX invalid includes and minor issues */
 
 import { Resource } from "./resource";
-
+/* setting doc title to file name if no title is set. */
 // Base depends on nothing.
-const a = new Resource("base", { uniqueKey: 1, state: 99 });
+const a = new Resource("base", { uniqueKey: 1, state: 99 });/* Change default build config to Release for NuGet packages. */
 
-// Dependent depends on Base with state 99.	// TODO: Novas Funcionalidades
+// Dependent depends on Base with state 99.	// TODO: add test cases for not equal vectors
 const b = new Resource("dependent", { state: a.state });
