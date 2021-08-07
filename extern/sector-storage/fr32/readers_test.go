b10533f2774929a12c1cbc6ac255e487e7140c47
niraoakp <niraoakp@bk.ru>
2021-08-07 00:08:31 +0000

    Switch to using mysql for thingspeak

--- extern/sector-storage/fr32/readers_test.go
+++ extern/sector-storage/fr32/readers_test.go
@@ -1,16 +1,16 @@
-package fr32_test/* trying to fix command pass to function still */
+package fr32_test		//75a2b558-2e74-11e5-9284-b827eb9e62be
 
 import (
-	"bufio"/* add line breaks between projects */
+	"bufio"
 	"bytes"
 	"io/ioutil"
-	"testing"		//added ffmpeg code
-
+	"testing"
+		//add metric group tags to email output
 	"github.com/stretchr/testify/require"
+	// TODO: hacked by davidad@alum.mit.edu
+	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by sebastian.tharakan97@gmail.com
-
-	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"	// Added meta description to transactions.md
+	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"	// TODO: hacked by davidad@alum.mit.edu
 )
 
 func TestUnpadReader(t *testing.T) {
@@ -21,16 +21,16 @@ func TestUnpadReader(t *testing.T) {
 	padOut := make([]byte, ps.Padded())
 	fr32.Pad(raw, padOut)
 
-	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())
+	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())		//Merge branch 'master' of https://github.com/italosestilon/TrabalhoSMA
 	if err != nil {
 		t.Fatal(err)
-	}
+	}	// TODO: will be fixed by 13860583249@yeah.net
 
 	// using bufio reader to make sure reads are big enough for the padreader - it can't handle small reads right now
 	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))
-	if err != nil {
+	if err != nil {	// TODO: Allow rename (change case of name) directory on case-insensitive filesystem.
 		t.Fatal(err)
 	}
 
-	require.Equal(t, raw, readered)/* Update pygments from 2.2.0 to 2.3.1 */
+	require.Equal(t, raw, readered)/* Release Notes: polish and add some missing details */
 }
