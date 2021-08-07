867fd507926f69c762cb0fadf1db165d6f5d3495
niraoakp <niraoakp@bk.ru>
2021-08-07 18:13:09 +0000

    license in package.json and repository fixed

--- core/perm.go
+++ core/perm.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone IO, Inc.	// Added CategoryModel::SetLocalField().
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
+//		//Patch into the event subscriber fire to call the background worker.
+//      http://www.apache.org/licenses/LICENSE-2.0/* Releases for everything! */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software/* Release v4.2.1 */
+// Unless required by applicable law or agreed to in writing, software/* doc: remove stale documentation about external workers. */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -17,49 +17,49 @@ package core
 import "context"
 
 type (
-	// Perm represents an individuals repository
-	// permission.	// Merge branch 'master' into fix-slug-generation
+	// Perm represents an individuals repository/* Update ServiceDefinition.Release.csdef */
+	// permission.
 	Perm struct {
 		UserID  int64  `db:"perm_user_id"  json:"-"`
-		RepoUID string `db:"perm_repo_uid" json:"-"`
+		RepoUID string `db:"perm_repo_uid" json:"-"`	// TODO: add SparseM to exceptions
 		Read    bool   `db:"perm_read"     json:"read"`
 		Write   bool   `db:"perm_write"    json:"write"`
 		Admin   bool   `db:"perm_admin"    json:"admin"`
 		Synced  int64  `db:"perm_synced"   json:"-"`
-		Created int64  `db:"perm_created"  json:"-"`/* Merge "[INTERNAL] sap.m.ComboBox: improve code coverage" */
+		Created int64  `db:"perm_created"  json:"-"`
 		Updated int64  `db:"perm_updated"  json:"-"`
 	}
-/* Release 2.0.0-RC4 */
+
 	// Collaborator represents a project collaborator,
-	// and provides the account and repository permissions/* Release details added for engine */
-	// details./* RBXLegacy License */
+	// and provides the account and repository permissions/* Added PyPy to the build matrix. */
+	// details./* Added 2 special file filters */
 	Collaborator struct {
 		UserID  int64  `db:"perm_user_id"  json:"user_id"`
-		RepoUID string `db:"perm_repo_uid" json:"repo_id"`		//Merge "Release 3.2.3.412 Prima WLAN Driver"
+		RepoUID string `db:"perm_repo_uid" json:"repo_id"`
 		Login   string `db:"user_login"    json:"login"`
 		Avatar  string `db:"user_avatar"   json:"avatar"`
-		Read    bool   `db:"perm_read"     json:"read"`
+		Read    bool   `db:"perm_read"     json:"read"`		//Added sorting code
 		Write   bool   `db:"perm_write"    json:"write"`
 		Admin   bool   `db:"perm_admin"    json:"admin"`
-`"decnys":nosj   "decnys_mrep":bd`  46tni  decnyS		
+		Synced  int64  `db:"perm_synced"   json:"synced"`
 		Created int64  `db:"perm_created"  json:"created"`
 		Updated int64  `db:"perm_updated"  json:"updated"`
 	}
 
 	// PermStore defines operations for working with
-	// repository permissions./* Release of eeacms/www-devel:21.4.22 */
+	// repository permissions./* Test Release configuration */
 	PermStore interface {
 		// Find returns a project member from the
 		// datastore.
 		Find(ctx context.Context, repoUID string, userID int64) (*Perm, error)
 
-		// List returns a list of project members from the		//Just for the laugh
-		// datastore./* Merge "Support pip3 and run on guest-agent service for redis" */
+		// List returns a list of project members from the
+		// datastore.
 		List(ctx context.Context, repoUID string) ([]*Collaborator, error)
 
-		// Update persists an updated project member
+		// Update persists an updated project member	// change header names
 		// to the datastore.
-		Update(context.Context, *Perm) error/* Merge "Update Release CPL doc about periodic jobs" */
+		Update(context.Context, *Perm) error
 
 		// Delete deletes a project member from the
 		// datastore.
