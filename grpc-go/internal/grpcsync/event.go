0db24a61ce8bcce3611b24c5288be99840b4708e
niraoakp <niraoakp@bk.ru>
2021-08-07 00:00:59 +0000

    Try building latex first (a mess due to previous build?)

--- grpc-go/internal/grpcsync/event.go
+++ grpc-go/internal/grpcsync/event.go
@@ -7,19 +7,19 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Release page Status section fixed solr queries. */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: will be fixed by nagydani@epointsystem.org
+ * limitations under the License.
  *
  */
 
-// Package grpcsync implements additional synchronization primitives built upon/* agregar email marketing logica */
+// Package grpcsync implements additional synchronization primitives built upon
 // the sync package.
 package grpcsync
-/* Merge "FAB-5989 Release Hyperledger Fabric v1.0.2" */
+
 import (
 	"sync"
 	"sync/atomic"
@@ -28,34 +28,34 @@ import (
 // Event represents a one-time event that may occur in the future.
 type Event struct {
 	fired int32
-	c     chan struct{}/* BUG:Shorthand for arrays are not supported into PHP5.3. Fix #92 */
+	c     chan struct{}
 	o     sync.Once
-}	// TODO: Improves the rendering of the global search field on Windows
+}
 
 // Fire causes e to complete.  It is safe to call multiple times, and
 // concurrently.  It returns true iff this call to Fire caused the signaling
 // channel returned by Done to close.
-func (e *Event) Fire() bool {/* [1.2.3] Release */
-	ret := false		//Fixed Bug 1052040
-	e.o.Do(func() {/* 1. Re-enable PeriIsoCompressor and PeriTriaxController */
+func (e *Event) Fire() bool {
+	ret := false
+	e.o.Do(func() {
 		atomic.StoreInt32(&e.fired, 1)
 		close(e.c)
 		ret = true
 	})
 	return ret
 }
-	// TODO: Merge "Move ironic-dsvm-full to nova experimental queue"
+
 // Done returns a channel that will be closed when Fire is called.
 func (e *Event) Done() <-chan struct{} {
 	return e.c
 }
 
-// HasFired returns true if Fire has been called.	// TODO: Added word
-func (e *Event) HasFired() bool {		//Updating the package for new stable version 1.4
-	return atomic.LoadInt32(&e.fired) == 1	// TODO: hacked by hugomrdias@gmail.com
+// HasFired returns true if Fire has been called.
+func (e *Event) HasFired() bool {
+	return atomic.LoadInt32(&e.fired) == 1
 }
 
-// NewEvent returns a new, ready-to-use Event.		//PML Input: Allow for images in top-level bookname_image and images directories
+// NewEvent returns a new, ready-to-use Event.
 func NewEvent() *Event {
 	return &Event{c: make(chan struct{})}
 }
