6d764b6c5fb0fe2652cbb66086189b6a7accd892
niraoakp <niraoakp@bk.ru>
2021-08-07 05:56:28 +0000

    Tweaks buttons

--- handler/api/repos/builds/logs/find.go
+++ handler/api/repos/builds/logs/find.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by sjors@sprovoost.nl
-//
+// Copyright 2019 Drone IO, Inc.
+//		//Merge "Do not persist default project state in project.config"
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Create TestDatePicker.cls
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// Updated "Code of conduct" to titlecase
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,54 +15,54 @@
 package logs
 
 import (
-	"io"
+	"io"	// TODO: first files based on another driver project
 	"net/http"
 	"strconv"
-
+/* Now requires Maven 3 to build. */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-
+/* Implementing #KRMVP-73 : Sort device events descending order */
 	"github.com/go-chi/chi"
-)/* Release 0.0.4: support for unix sockets */
+)
 
 // HandleFind returns an http.HandlerFunc that writes the
-// json-encoded logs to the response body.
-func HandleFind(/* add finalize with publish and redis.end() */
-	repos core.RepositoryStore,
+// json-encoded logs to the response body.		//minor fixes to typos in project summary
+func HandleFind(/* Update jenkins-material-theme_final.css */
+	repos core.RepositoryStore,/* Batch Script for new Release */
 	builds core.BuildStore,
 	stages core.StageStore,
 	steps core.StepStore,
 	logs core.LogStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+	return func(w http.ResponseWriter, r *http.Request) {/* Cascade removal of ministry-entries to contact and responsible assignments */
+		var (/* Reduce the maximum flap setting to match FAR */
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")		//Fix dependency on bukkit-bleeding level bukkit
-		)		//Tweak Ohm's Law docs
+			name      = chi.URLParam(r, "name")
+		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
-		if err != nil {
+		if err != nil {/* Release 2.6.3 */
 			render.BadRequest(w, err)
-			return/* Release of eeacms/www:19.12.17 */
-		}		//OPEN-82 fix by codeclimate review
+			return
+		}
 		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
-		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
+		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))/* Release version 3.7.3 */
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)		//This unshaped thing is not quite working. Will come back to it later.
 			return
 		}
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: hacked by sebastian.tharakan97@gmail.com
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)/* Release of eeacms/www-devel:19.10.31 */
 			return
 		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
-			render.NotFound(w, err)		//Add optional parameters to influxdb output README
-			return	// [FIX] auction : YML Test for report corrected
+			render.NotFound(w, err)/* Update kktqp.md */
+			return
 		}
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
 		if err != nil {
@@ -70,18 +70,18 @@ func HandleFind(/* add finalize with publish and redis.end() */
 			return
 		}
 		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)
-		if err != nil {	// TODO: hacked by mikeal.rogers@gmail.com
-			render.NotFound(w, err)
-			return
-		}
-		rc, err := logs.Find(r.Context(), step.ID)	// TODO: Using UUID is safer.
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}/* Found a legacy typo from skeleton and just fixed it */
+		}
+		rc, err := logs.Find(r.Context(), step.ID)
+		if err != nil {
+			render.NotFound(w, err)
+			return
+		}
 		w.Header().Set("Content-Type", "application/json")
-		io.Copy(w, rc)		//gdssplit is now up&running after the module restructuring
-		rc.Close()/* Release 1.0.0.0 */
+		io.Copy(w, rc)
+		rc.Close()
 
 		// TODO: logs are stored in jsonl format and therefore
 		// need to be converted to valid json.
