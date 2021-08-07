d8d5b5f4a02ddb40fada3e75b1274b50d55d12c3
niraoakp <niraoakp@bk.ru>
2021-08-07 04:21:16 +0000

    Update Suspend.md

--- grpc-go/tap/tap.go
+++ grpc-go/tap/tap.go
@@ -7,12 +7,12 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Release 3.2.0. */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// docs(navigation): ensure the sidebar scrolls while reaching the bottom (#138)
+ * limitations under the License.
  *
  */
 
@@ -24,7 +24,7 @@
 // Notice: This API is EXPERIMENTAL and may be changed or removed in a
 // later release.
 package tap
-		//v9.3.0-test
+
 import (
 	"context"
 )
@@ -33,9 +33,9 @@ import (
 type Info struct {
 	// FullMethodName is the string of grpc method (in the format of
 	// /package.service/method).
-	FullMethodName string		//added pry gem
+	FullMethodName string
 	// TODO: More to be added.
-}	// TODO: remove nzbget installer after unpacking
+}
 
 // ServerInHandle defines the function which runs before a new stream is
 // created on the server side. If it returns a non-nil error, the stream will
@@ -44,7 +44,7 @@ type Info struct {
 // otherwise PermissionDenied will be the code and err.Error() will be the
 // message.
 //
-// It's intended to be used in situations where you don't want to waste the	// TODO: Add hidden constant to solve bug of too low size to see products label.
+// It's intended to be used in situations where you don't want to waste the
 // resources to accept the new stream (e.g. rate-limiting). For other general
 // usages, please use interceptors.
 //
