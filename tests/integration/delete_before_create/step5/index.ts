d65ed75aeb78f06b6f998c0344bced1a9b35432c
niraoakp <niraoakp@bk.ru>
2021-08-07 01:38:31 +0000

    Delete MysqlMadeEasy.jar

--- tests/integration/delete_before_create/step5/index.ts
+++ tests/integration/delete_before_create/step5/index.ts
@@ -2,20 +2,20 @@
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-
-// The DBR deletion of A triggers the deletion of C due to dependency.
+		//Re-added scope to Oauth20ServiceImpl.getAccessToken().
+// The DBR deletion of A triggers the deletion of C due to dependency./* Release version 0.6.3 - fixes multiple tabs issues */
 // The planner should execute these steps (in this exact order):
-tnednepeD tnemecalpeReteleD .1   //
+//   1. DeleteReplacement Dependent
 //   2. DeleteReplacement Base
-//   3. Replace Base	// Delete MechJebModuleAscentClassic.cs
+//   3. Replace Base
 //   4. CreateReplacement Base
 const a = new Resource("base", { uniqueKey: 1, state: 200 });
 
 //   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)
 //   5. DeleteReplacement Base-2
-//   6. Replace Base-2
-//   7. CreateReplacement Base-2/* Update Settings.java */
-const b = new Resource("base-2", { uniqueKey: 2, state: 50 });/* Criando o cadastro de bairros */
+//   6. Replace Base-2/* Support snapshotting of Derby Releases... */
+//   7. CreateReplacement Base-2	// mejoras de documentacion --bueno no tanto pero mas entendible ¬¬!
+const b = new Resource("base-2", { uniqueKey: 2, state: 50 });
 
 //   8. Replace Dependent
 //   9. CreateReplacement Dependent
