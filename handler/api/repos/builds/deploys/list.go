11665e9a19819866126afc98ca3bf825fe956cef
niraoakp <niraoakp@bk.ru>
2021-08-07 12:31:56 +0000

    starton braking do and extract to two scripts

--- handler/api/repos/builds/deploys/list.go
+++ handler/api/repos/builds/deploys/list.go
@@ -5,10 +5,10 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Add Release plugin */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 0.8.14.1 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -18,12 +18,12 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Create DGrade.java */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
-/* Release Notes for v02-13-01 */
+
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of build history to the response body.
 func HandleList(
@@ -32,30 +32,30 @@ func HandleList(
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")	// 3rd times a charm
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err)./* Cover another scenario for issue #99. */
+				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
 		}
-	// TODO: will be fixed by hugomrdias@gmail.com
+
 		results, err := builds.LatestDeploys(r.Context(), repo.ID)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace)./* Updated Latest Release */
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot list builds")
 		} else {
 			render.JSON(w, results, 200)
 		}
-	}		//Delete Dipswitch.JPG
+	}
 }
