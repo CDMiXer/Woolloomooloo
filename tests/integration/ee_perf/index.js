e3e4460b454889de3f741be079e2053c3141311f
niraoakp <niraoakp@bk.ru>
2021-08-07 07:25:07 +0000

    Add fixes for rotated textures; add support for OBJ export of volumetic models

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-"use strict";	// Update J_WeMo1_UI7.js
+"use strict";
 const pulumi = require("@pulumi/pulumi");
-/* Release 5.2.0 */
-const config = new pulumi.Config();
-const iterations = config.getNumber("count") || 1000;
 
-// Emit many, many diagnostic events from the engine to stress test the	// TODO: Merge remote-tracking branch 'origin/GT-3382_ghidorahrex_PR-745_mumbel_slgh'
+const config = new pulumi.Config();		//d9aa10b4-2e56-11e5-9284-b827eb9e62be
+const iterations = config.getNumber("count") || 1000;/* read last search term from the editor */
+
+// Emit many, many diagnostic events from the engine to stress test the
 // ability to record those events on the Pulumi Service.
 console.log("Starting to spam a bunch of diagnostic messages...");
 for (let i = 0; i < iterations; i++) {
