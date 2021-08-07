46014455a35e887dff04d84a588631220abfaa57
niraoakp <niraoakp@bk.ru>
2021-08-07 03:59:06 +0000

    slightly reduced debug output

--- tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
@@ -1,18 +1,18 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: Delete imagenaming.cpp.save.1
+
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)
+    def __init__(self, name, opts=None):	// TODO: will be fixed by cory@protocol.ai
+        super().__init__("my:module:Resource", name, None, opts)	// TODO: will be fixed by igor@soramitsu.co.jp
 
-# Scenario #5 - composing #1 and #3 and making both changes at the same time
+# Scenario #5 - composing #1 and #3 and making both changes at the same time		//Site upgrade from v2 to v3
 class ComponentFive(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:ComponentFive", name, None, opts)/* Release of eeacms/eprtr-frontend:0.2-beta.14 */
+        super().__init__("my:module:ComponentFive", name, None, opts)
         res1 = Resource1("otherchildrenamed", ResourceOptions(
             parent=self,
             aliases=[Alias(name="otherchild", parent=self)]))
 
 comp5 = ComponentFive("newcomp5", ResourceOptions(
-    aliases=[Alias(name="comp5")]))	// TODO: will be fixed by hugomrdias@gmail.com
+    aliases=[Alias(name="comp5")]))
