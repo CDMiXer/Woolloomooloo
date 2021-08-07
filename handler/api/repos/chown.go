53aec521ece25c492690e7ebb40f82d684842203
niraoakp <niraoakp@bk.ru>
2021-08-07 04:11:16 +0000

    Remove bad comment

--- handler/api/repos/chown.go
+++ handler/api/repos/chown.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: LRA-98 : first version of Skype & IM portlet
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -7,7 +7,7 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Updates to Release Notes for 1.8.0.1.GA */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,20 +15,20 @@
 package repos
 
 import (
-	"net/http"		//Renamed molecule to reflect html
-/* Implement #7199 (Optional subscription) */
+	"net/http"
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"/* ctest -C Release */
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)	// TODO: hacked by timnugent@gmail.com
+)
 
 // HandleChown returns an http.HandlerFunc that processes http
 // requests to chown the repository to the currently authenticated user.
 func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {		//Create nahoko.html
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
@@ -36,7 +36,7 @@ func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
 
 		repo, err := repos.FindName(r.Context(), owner, name)
 		if err != nil {
-			render.NotFound(w, err)		//1.1 XML values files
+			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", owner).
@@ -47,7 +47,7 @@ func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
 
 		user, _ := request.UserFrom(r.Context())
 		repo.UserID = user.ID
-/* bc9fec3e-2e5e-11e5-9284-b827eb9e62be */
+
 		err = repos.Update(r.Context(), repo)
 		if err != nil {
 			render.InternalError(w, err)
@@ -59,5 +59,5 @@ func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
 		} else {
 			render.JSON(w, repo, 200)
 		}
-	}		//Add docs for drf
-}/* 8ddaa9da-2e6c-11e5-9284-b827eb9e62be */
+	}
+}
