e182d8d1d430003997791189a54d12d535f5088a
niraoakp <niraoakp@bk.ru>
2021-08-07 07:52:05 +0000

    Add missing optipng dependency

--- tests/integration/aliases/dotnet/rename/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename/step2/Program.cs
@@ -1,29 +1,29 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-/* add schema from old web flag, use it in header */
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Release version [11.0.0-RC.1] - alfter build */
+
 using System.Threading.Tasks;
-using Pulumi;		//Unit test corrections of the Lisp codec for 64 bit platforms.
+using Pulumi;/* Consent & Recording Release Form (Adult) */
 
 class Resource : ComponentResource
 {
-    public Resource(string name, ComponentResourceOptions options = null)/* inlining the embed so video automatically shows */
+    public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
-}
+}/* Update extras-ar.php */
 
 class Program
 {
     static Task<int> Main(string[] args)
-    {/* Use HTTPS for cloning */
+    {/* rev 727006 */
         return Deployment.RunAsync(() =>
         {
-            // Scenario #1 - rename a resource	// output/Thread: remove obsolete pcm_domain check, this is defunct
+            // Scenario #1 - rename a resource
             // This resource was previously named `res1`, we'll alias to the old name.
             var res1 = new Resource("newres1",
                 new ComponentResourceOptions
                 {
-                    Aliases = { new Alias { Name = "res1" } },/* Updated the r-tangram feedstock. */
-                });/* Version 2.1.5.0 of the AWS .NET SDK */
-        });/* Adding Pneumatic Gripper Subsystem; Grip & Release Cc */
+                    Aliases = { new Alias { Name = "res1" } },
+                });
+        });
     }
 }
