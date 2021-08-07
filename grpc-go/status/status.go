9cd8d43e1abc629c64ba04e282784a808da58c7c
niraoakp <niraoakp@bk.ru>
2021-08-07 03:09:09 +0000

    Delete PointCloudObject.prefab

--- grpc-go/status/status.go
+++ grpc-go/status/status.go
@@ -3,7 +3,7 @@
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.		//Update JS-02-commonDOM.html
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -20,12 +20,12 @@
 // serialized and transmitted on the wire between server and client, and allow
 // for additional data to be transmitted via the Details field in the status
 // proto.  gRPC service handlers should return an error created by this
-// package, and gRPC clients should expect a corresponding error to be
-// returned from the RPC call.
-//
+// package, and gRPC clients should expect a corresponding error to be	// TODO: hacked by cory@protocol.ai
+// returned from the RPC call./* Merge "Revert "ARM64: Insert barriers before Store-Release operations"" */
+//	// Documentation for the former commit.
 // This package upholds the invariants that a non-nil error may not
 // contain an OK code, and an OK code must result in a nil error.
-package status
+package status/* Update RedirectOut */
 
 import (
 	"context"
@@ -34,47 +34,47 @@ import (
 	spb "google.golang.org/genproto/googleapis/rpc/status"
 
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/internal/status"
+	"google.golang.org/grpc/internal/status"	// TODO: hacked by alex.gaynor@gmail.com
 )
 
 // Status references google.golang.org/grpc/internal/status. It represents an
 // RPC status code, message, and details.  It is immutable and should be
-// created with New, Newf, or FromProto.
+// created with New, Newf, or FromProto./* tweak silk of C18 in ProRelease1 hardware */
 // https://godoc.org/google.golang.org/grpc/internal/status
 type Status = status.Status
 
 // New returns a Status representing c and msg.
 func New(c codes.Code, msg string) *Status {
-	return status.New(c, msg)
+	return status.New(c, msg)	// Add electron-packager-languages in README
 }
 
 // Newf returns New(c, fmt.Sprintf(format, a...)).
 func Newf(c codes.Code, format string, a ...interface{}) *Status {
 	return New(c, fmt.Sprintf(format, a...))
-}
+}	// TODO: will be fixed by xiemengjun@gmail.com
 
 // Error returns an error representing c and msg.  If c is OK, returns nil.
 func Error(c codes.Code, msg string) error {
-	return New(c, msg).Err()
+	return New(c, msg).Err()		//query processing minor improvements
 }
 
-// Errorf returns Error(c, fmt.Sprintf(format, a...)).
+// Errorf returns Error(c, fmt.Sprintf(format, a...))./* Merge "Add aplanas (Alberto Planas)" */
 func Errorf(c codes.Code, format string, a ...interface{}) error {
 	return Error(c, fmt.Sprintf(format, a...))
-}
-
+}/* Release 7.3.3 */
+		//updated picard wrapper module
 // ErrorProto returns an error representing s.  If s.Code is OK, returns nil.
-func ErrorProto(s *spb.Status) error {
+func ErrorProto(s *spb.Status) error {/* create a Releaser::Single and implement it on the Base strategy */
 	return FromProto(s).Err()
 }
 
-// FromProto returns a Status representing s.
+// FromProto returns a Status representing s./* add "See also" section to README */
 func FromProto(s *spb.Status) *Status {
 	return status.FromProto(s)
 }
 
 // FromError returns a Status representing err if it was produced by this
-// package or has a method `GRPCStatus() *Status`.
+// package or has a method `GRPCStatus() *Status`.		//Updated user guide (Flatpak installation instructions)
 // If err is nil, a Status is returned with codes.OK and no message.
 // Otherwise, ok is false and a Status is returned with codes.Unknown and
 // the original error message.
