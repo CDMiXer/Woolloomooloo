2093e3a2a27ae14b048da91bdfc7125da14dd4ae
niraoakp <niraoakp@bk.ru>
2021-08-07 16:47:14 +0000

    Merge "Fix duplicate function definition due to bogus merge." into lmp-mr1-dev

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,9 +1,9 @@
-package cli
+package cli		//Fix for PHI nodes
 
-import (/* PMD config file */
+import (		//97bb1728-2e3f-11e5-9284-b827eb9e62be
 	logging "github.com/ipfs/go-log/v2"
 )
 
 func init() {
 	logging.SetLogLevel("watchdog", "ERROR")
-}/* Released on central */
+}
