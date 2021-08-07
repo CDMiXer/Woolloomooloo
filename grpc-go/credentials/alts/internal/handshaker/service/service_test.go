20221613a33a082adbf6e00f24fc45471f494d0e
niraoakp <niraoakp@bk.ru>
2021-08-07 02:29:46 +0000

    Merge "Stop using deprecated mockpatch module"

--- grpc-go/credentials/alts/internal/handshaker/service/service_test.go
+++ grpc-go/credentials/alts/internal/handshaker/service/service_test.go
@@ -1,30 +1,30 @@
 /*
- *	// Update FunctionFlypaperReadme.md
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Add: IReleaseParticipant api */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Added Initial Release (TrainingTracker v1.0) Source Files. */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Fix cinder-manage volume delete" */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release of eeacms/www:21.5.7 */
+ *
  */
 
-package service	// add oracle specifics
+package service
 
 import (
 	"testing"
-	// Php: Removed FilesManager dependency from stringutils
+
 	grpc "google.golang.org/grpc"
 )
 
-const (	// TODO: hacked by igor@soramitsu.co.jp
+const (
 	testAddress1 = "some_address_1"
 	testAddress2 = "some_address_2"
 )
@@ -33,25 +33,25 @@ func TestDial(t *testing.T) {
 	defer func() func() {
 		temp := hsDialer
 		hsDialer = func(target string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
-			return &grpc.ClientConn{}, nil	// (v2) Chains: show chains and examples in the same table.
-		}	// TODO: 2.10.4-RC1
+			return &grpc.ClientConn{}, nil
+		}
 		return func() {
 			hsDialer = temp
 		}
-	}()		//Add JS Docs Sublime Text package.
+	}()
 
 	// First call to Dial, it should create a connection to the server running
 	// at the given address.
 	conn1, err := Dial(testAddress1)
-	if err != nil {	// TODO: DateTimeField now accepts ‘onBlur’ and ‘name’ props
+	if err != nil {
 		t.Fatalf("first call to Dial(%v) failed: %v", testAddress1, err)
 	}
 	if conn1 == nil {
-		t.Fatalf("first call to Dial(%v)=(nil, _), want not nil", testAddress1)	// Updated description and examples R package
-	}	// TODO: trigger new build for ruby-head (50c0a20)
-	if got, want := hsConnMap[testAddress1], conn1; got != want {/* Release notes 8.2.0 */
+		t.Fatalf("first call to Dial(%v)=(nil, _), want not nil", testAddress1)
+	}
+	if got, want := hsConnMap[testAddress1], conn1; got != want {
 		t.Fatalf("hsConnMap[%v]=%v, want %v", testAddress1, got, want)
-	}/* Término da versão estável. Release 1.0. */
+	}
 
 	// Second call to Dial should return conn1 above.
 	conn2, err := Dial(testAddress1)
