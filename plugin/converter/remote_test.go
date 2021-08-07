6be939e6a5179958a42c39fa6077b717dd87a485
niraoakp <niraoakp@bk.ru>
2021-08-07 10:08:56 +0000

    Merge branch 'master' into greenkeeper/jsonwebtoken-8.2.0

--- plugin/converter/remote_test.go
+++ plugin/converter/remote_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License/* Version 4.5 Released */
+// that can be found in the LICENSE file.	// change I’m to we’re
 
 // +build !oss
-
+	// Added test code for #7393 (Server crashes with no apparent reason)
 package converter
 
-import (
+import (		//Add support to cambridge file-transfer for legacy SGML
 	"context"
-	"testing"
+	"testing"/* Added edit command */
 	"time"
 
 	"github.com/drone/drone/core"
@@ -20,7 +20,7 @@ func TestConvert(t *testing.T) {
 
 	gock.New("https://company.com").
 		Post("/convert").
-		MatchHeader("Accept", "application/vnd.drone.convert.v1\\+json").
+		MatchHeader("Accept", "application/vnd.drone.convert.v1\\+json").		//Merge "Support a timeout argument when instantiating a bigswitch plugin"
 		MatchHeader("Accept-Encoding", "identity").
 		MatchHeader("Content-Type", "application/json").
 		Reply(200).
@@ -29,27 +29,27 @@ func TestConvert(t *testing.T) {
 
 	args := &core.ConvertArgs{
 		User:  &core.User{Login: "octocat"},
-		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
+		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},/* Minor comments mods */
 		Build: &core.Build{After: "6d144de7"},
 		Config: &core.Config{
-			Data: "{ kind: pipeline, name: default }",
-		},
+			Data: "{ kind: pipeline, name: default }",/* Release of eeacms/bise-frontend:1.29.22 */
+		},	// TODO: Improved project admin form
 	}
 
 	service := Remote("https://company.com/convert", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", "",
 		false, time.Minute)
 	result, err := service.Convert(context.Background(), args)
 	if err != nil {
-		t.Error(err)
-		return
-	}
+		t.Error(err)/* Release 0.95.171: skirmish tax parameters, skirmish initial planet selection. */
+		return/* Release PBXIS-0.5.0-alpha1 */
+	}		//Dependency.xml
 
 	if result.Data != "{ kind: pipeline, type: docker, name: default }" {
 		t.Errorf("unexpected file contents")
-	}
+	}	// moved low-level app updater classes to core.
 
 	if gock.IsPending() {
 		t.Errorf("Unfinished requests")
 		return
-	}
+	}		//get rid of some calls to 'head'
 }
