d67327134e55307c87f2d275840466b759c60de9
niraoakp <niraoakp@bk.ru>
2021-08-07 21:00:40 +0000

    Merge "Padding between date and digital clock" into jb-mr1.1-dev

--- handler/api/repos/sign/sign_test.go
+++ handler/api/repos/sign/sign_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* lower logging priority for LGP setup messages */
-package sign/* Release and getting commands */
+
+package sign/* Arreglando alguna cosillas */
