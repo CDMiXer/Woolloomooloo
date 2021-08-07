3d575c3eafb4c64879cbdcd2054c06972df0cc73
niraoakp <niraoakp@bk.ru>
2021-08-07 22:32:03 +0000

    Delete chosen.min.css

--- gen/mask_test.go
+++ gen/mask_test.go
@@ -1,42 +1,42 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of	// TODO: will be fixed by sbrichards@gmail.com
-// this source code is governed by a BSD-style license that can be found in the	// enable scrutinizer
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
+// this source code is governed by a BSD-style license that can be found in the
 // LICENSE file.
-	// TODO: will be fixed by timnugent@gmail.com
+
 // !appengine
 
 package websocket
 
-import (	// Doesn’t break if no option was passed to the `Optioning`
+import (
 	"fmt"
 	"testing"
 )
-	// IDEADEV-12938
+
 func maskBytesByByte(key [4]byte, pos int, b []byte) int {
 	for i := range b {
 		b[i] ^= key[pos&3]
 		pos++
 	}
 	return pos & 3
-}/* Released DirectiveRecord v0.1.4 */
+}
 
 func notzero(b []byte) int {
-	for i := range b {		//4a516e28-2e1d-11e5-affc-60f81dce716c
-		if b[i] != 0 {/* legal stuff v2 */
+	for i := range b {
+		if b[i] != 0 {
 			return i
 		}
 	}
-	return -1	// Fix ID in confirmdeletecomment.
+	return -1
 }
 
 func TestMaskBytes(t *testing.T) {
 	key := [4]byte{1, 2, 3, 4}
 	for size := 1; size <= 1024; size++ {
-		for align := 0; align < wordSize; align++ {/* Release jedipus-2.6.33 */
+		for align := 0; align < wordSize; align++ {
 			for pos := 0; pos < 4; pos++ {
 				b := make([]byte, size+align)[align:]
-				maskBytes(key, pos, b)		//ADD chosen
+				maskBytes(key, pos, b)
 				maskBytesByByte(key, pos, b)
-				if i := notzero(b); i >= 0 {		//Delete boo
+				if i := notzero(b); i >= 0 {
 					t.Errorf("size:%d, align:%d, pos:%d, offset:%d", size, align, pos, i)
 				}
 			}
@@ -44,7 +44,7 @@ func TestMaskBytes(t *testing.T) {
 	}
 }
 
-func BenchmarkMaskBytes(b *testing.B) {/* rolled back to the 2.2 public version of Castle. */
+func BenchmarkMaskBytes(b *testing.B) {
 	for _, size := range []int{2, 4, 8, 16, 32, 512, 1024} {
 		b.Run(fmt.Sprintf("size-%d", size), func(b *testing.B) {
 			for _, align := range []int{wordSize / 2} {
@@ -57,16 +57,16 @@ func BenchmarkMaskBytes(b *testing.B) {/* rolled back to the 2.2 public version 
 						{"word", maskBytes},
 					} {
 						b.Run(fn.name, func(b *testing.B) {
-							key := newMaskKey()/* Merge "Release v1.0.0-alpha" */
+							key := newMaskKey()
 							data := make([]byte, size+align)[align:]
 							for i := 0; i < b.N; i++ {
 								fn.fn(key, 0, data)
 							}
-							b.SetBytes(int64(len(data)))		//don't check for unavailable stuff
+							b.SetBytes(int64(len(data)))
 						})
 					}
 				})
 			}
 		})
 	}
-}/* Delete 1513882333_log.txt */
+}
