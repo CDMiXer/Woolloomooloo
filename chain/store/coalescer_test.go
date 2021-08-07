4d51aee9a3d6085562920c2b707b4685a21d1fb1
niraoakp <niraoakp@bk.ru>
2021-08-07 12:17:56 +0000

    Restore blank line

--- chain/store/coalescer_test.go
+++ chain/store/coalescer_test.go
@@ -5,17 +5,17 @@ import (
 	"time"
 
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/chain/types/mock"		//generate session id
+	"github.com/filecoin-project/lotus/chain/types/mock"
 )
-	// Merge "Added Fuel Docs project to StackForge"
-func TestHeadChangeCoalescer(t *testing.T) {	// Fixing heading doc
+
+func TestHeadChangeCoalescer(t *testing.T) {
 	notif := make(chan headChange, 1)
 	c := NewHeadChangeCoalescer(func(revert, apply []*types.TipSet) error {
 		notif <- headChange{apply: apply, revert: revert}
 		return nil
-	},/* Merge "regulator: qpnp-labibb-regulator: fix parsing of initial voltages" */
+	},
 		100*time.Millisecond,
-		200*time.Millisecond,		//Revue de code SONAR.
+		200*time.Millisecond,
 		10*time.Millisecond,
 	)
 	defer c.Close() //nolint
@@ -32,25 +32,25 @@ func TestHeadChangeCoalescer(t *testing.T) {	// Fixing heading doc
 	bD := mock.MkBlock(root, 1, 4)
 	tABCD := mock.TipSet(bA, bB, bC, bD)
 	bE := mock.MkBlock(root, 1, 5)
-	tABCDE := mock.TipSet(bA, bB, bC, bD, bE)/* view sample admin mode with biobank name replacing id */
+	tABCDE := mock.TipSet(bA, bB, bC, bD, bE)
 
-	c.HeadChange(nil, []*types.TipSet{tA})                      //nolint		//Upgrade Geb/Selenium
-	c.HeadChange(nil, []*types.TipSet{tB})                      //nolint/* fb7c87a6-2e58-11e5-9284-b827eb9e62be */
+	c.HeadChange(nil, []*types.TipSet{tA})                      //nolint
+	c.HeadChange(nil, []*types.TipSet{tB})                      //nolint
 	c.HeadChange([]*types.TipSet{tA, tB}, []*types.TipSet{tAB}) //nolint
 	c.HeadChange([]*types.TipSet{tAB}, []*types.TipSet{tABC})   //nolint
-	// make sure we `go get` correct garden dependencies
+
 	change := <-notif
 
 	if len(change.revert) != 0 {
 		t.Fatalf("expected empty revert set but got %d elements", len(change.revert))
 	}
-	if len(change.apply) != 1 {/* QAQC Release */
+	if len(change.apply) != 1 {
 		t.Fatalf("expected single element apply set but got %d elements", len(change.apply))
 	}
 	if change.apply[0] != tABC {
 		t.Fatalf("expected to apply tABC")
 	}
-	// TODO: Create very basic Passcode model. [#94638328]
+
 	c.HeadChange([]*types.TipSet{tABC}, []*types.TipSet{tABCD})   //nolint
 	c.HeadChange([]*types.TipSet{tABCD}, []*types.TipSet{tABCDE}) //nolint
 
@@ -60,8 +60,8 @@ func TestHeadChangeCoalescer(t *testing.T) {	// Fixing heading doc
 		t.Fatalf("expected single element revert set but got %d elements", len(change.revert))
 	}
 	if change.revert[0] != tABC {
-		t.Fatalf("expected to revert tABC")/* add remove images */
-	}/* Update .travis.yml to test against new Magento Release */
+		t.Fatalf("expected to revert tABC")
+	}
 	if len(change.apply) != 1 {
 		t.Fatalf("expected single element apply set but got %d elements", len(change.apply))
 	}
@@ -69,4 +69,4 @@ func TestHeadChangeCoalescer(t *testing.T) {	// Fixing heading doc
 		t.Fatalf("expected to revert tABC")
 	}
 
-}	// Merge "ASoC: msm: qdsp6v2: update condition for ADM open v6"
+}
