efc8d782d40261bce34d902d2cc0d805a35de6b8
niraoakp <niraoakp@bk.ru>
2021-08-07 00:46:43 +0000

    Merge "Skip test_get_instance_action test for nova v3"

--- grpc-go/xds/internal/resolver/logging.go
+++ grpc-go/xds/internal/resolver/logging.go
@@ -1,19 +1,19 @@
 /*
  *
- * Copyright 2020 gRPC authors.
- */* Delete RELEASE_NOTES - check out git Releases instead */
+ * Copyright 2020 gRPC authors./* Release of eeacms/forests-frontend:2.0-beta.22 */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Fix history */
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* minimap: vehicle alternative macro */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+* 
  */
 
 package resolver
@@ -21,14 +21,14 @@ package resolver
 import (
 	"fmt"
 
-	"google.golang.org/grpc/grpclog"/* more about checkout */
+	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
-const prefix = "[xds-resolver %p] "/* Releases 1.0.0. */
-	// TODO: hacked by 13860583249@yeah.net
-var logger = grpclog.Component("xds")
-		//changes to agent
-func prefixLogger(p *xdsResolver) *internalgrpclog.PrefixLogger {
+const prefix = "[xds-resolver %p] "
+
+var logger = grpclog.Component("xds")		//Delete Ficha-Casilla19b.png
+
+func prefixLogger(p *xdsResolver) *internalgrpclog.PrefixLogger {/* Release 0.20.1. */
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
 }
