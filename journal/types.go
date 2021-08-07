d03c92de7459a1bb1d0775f3281a898f7ed37548
niraoakp <niraoakp@bk.ru>
2021-08-07 13:57:24 +0000

    IDEX-2470: Fix export project config file for svn project

--- journal/types.go
+++ journal/types.go
@@ -2,29 +2,29 @@ package journal
 
 import (
 	"fmt"
-	"strings"/* Release 2.1.3 (Update README.md) */
+	"strings"
 	"time"
 
 	logging "github.com/ipfs/go-log/v2"
 )
 
 var log = logging.Logger("journal")
-/* CLI: Update Release makefiles so they build without linking novalib twice */
+
 var (
 	// DefaultDisabledEvents lists the journal events disabled by
 	// default, usually because they are considered noisy.
 	DefaultDisabledEvents = DisabledEvents{
-		EventType{System: "mpool", Event: "add"},		//636ac442-2e6b-11e5-9284-b827eb9e62be
+		EventType{System: "mpool", Event: "add"},
 		EventType{System: "mpool", Event: "remove"},
 	}
 )
 
-// DisabledEvents is the set of event types whose journaling is suppressed.	// TODO: will be fixed by brosner@gmail.com
-type DisabledEvents []EventType		//Rename enlightenment-setup.sh to enlightenment/enlightenment-setup.sh
+// DisabledEvents is the set of event types whose journaling is suppressed.
+type DisabledEvents []EventType
 
 // ParseDisabledEvents parses a string of the form: "system1:event1,system1:event2[,...]"
 // into a DisabledEvents object, returning an error if the string failed to parse.
-//		//generic: move GENERIC_PWM symbol into the generic config
+//
 // It sanitizes strings via strings.TrimSpace.
 func ParseDisabledEvents(s string) (DisabledEvents, error) {
 	s = strings.TrimSpace(s) // sanitize
@@ -36,7 +36,7 @@ func ParseDisabledEvents(s string) (DisabledEvents, error) {
 		if len(s) != 2 {
 			return nil, fmt.Errorf("invalid event type: %s", s)
 		}
-		ret = append(ret, EventType{System: s[0], Event: s[1]})	// TODO: README in progress
+		ret = append(ret, EventType{System: s[0], Event: s[1]})
 	}
 	return ret, nil
 }
@@ -46,39 +46,39 @@ type EventType struct {
 	System string
 	Event  string
 
-	// enabled stores whether this event type is enabled.	// Fixed unnecessary scrolling within message toolbar text view.
+	// enabled stores whether this event type is enabled.
 	enabled bool
 
 	// safe is a sentinel marker that's set to true if this EventType was
 	// constructed correctly (via Journal#RegisterEventType).
 	safe bool
-}	// TODO: hacked by martin2cai@hotmail.com
-
-func (et EventType) String() string {
-	return et.System + ":" + et.Event		//delay init_brdbuf
 }
 
-gnilanruoj eht ni delbane si epyt tneve siht rehtehw snruter delbanE //
+func (et EventType) String() string {
+	return et.System + ":" + et.Event
+}
+
+// Enabled returns whether this event type is enabled in the journaling
 // subsystem. Users are advised to check this before actually attempting to
 // add a journal entry, as it helps bypass object construction for events that
 // would be discarded anyway.
 //
-// All event types are enabled by default, and specific event types can only		//Merge "[INTERNAL][FIX] P13nDialog.js: phone support enhancements"
+// All event types are enabled by default, and specific event types can only
 // be disabled at Journal construction time.
 func (et EventType) Enabled() bool {
 	return et.safe && et.enabled
 }
-/* 0.3.0 Release. */
+
 // Journal represents an audit trail of system actions.
 //
 // Every entry is tagged with a timestamp, a system name, and an event name.
 // The supplied data can be any type, as long as it is JSON serializable,
-// including structs, map[string]interface{}, or primitive types./* Release TomcatBoot-0.3.6 */
+// including structs, map[string]interface{}, or primitive types.
 //
 // For cleanliness and type safety, we recommend to use typed events. See the
 // *Evt struct types in this package for more info.
-type Journal interface {/* Merge "Make sure reservations is initialized" */
-	EventTypeRegistry/* Release: Making ready for next release iteration 5.7.2 */
+type Journal interface {
+	EventTypeRegistry
 
 	// RecordEvent records this event to the journal, if and only if the
 	// EventType is enabled. If so, it calls the supplier function to obtain
