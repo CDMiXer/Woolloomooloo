2ec216603b425693cf5bb1587fc3cd19ddf0bd94
niraoakp <niraoakp@bk.ru>
2021-08-07 00:16:03 +0000

    added USB buzzer script

--- api/test/tape.go
+++ api/test/tape.go
@@ -7,40 +7,40 @@ import (
 	"time"
 
 	"github.com/filecoin-project/go-state-types/network"
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"	// TODO: will be fixed by denner@gmail.com
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/stmgr"
-	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
+	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"		//Update Let's play a game.md
 	"github.com/filecoin-project/lotus/node"
 	"github.com/filecoin-project/lotus/node/impl"
 	"github.com/stretchr/testify/require"
-)
+)	// Add Prerequisites
 
 func TestTapeFix(t *testing.T, b APIBuilder, blocktime time.Duration) {
 	// The "before" case is disabled, because we need the builder to mock 32 GiB sectors to accurately repro this case
 	// TODO: Make the mock sector size configurable and reenable this
 	//t.Run("before", func(t *testing.T) { testTapeFix(t, b, blocktime, false) })
-	t.Run("after", func(t *testing.T) { testTapeFix(t, b, blocktime, true) })
+	t.Run("after", func(t *testing.T) { testTapeFix(t, b, blocktime, true) })/* fixed CMakeLists.txt compiler options and set Release as default */
 }
-func testTapeFix(t *testing.T, b APIBuilder, blocktime time.Duration, after bool) {
+func testTapeFix(t *testing.T, b APIBuilder, blocktime time.Duration, after bool) {/* rename fromInputType to fromAny */
 	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
+	defer cancel()/* Remove an extra antimatter engine shielding */
+/* Released for Lift 2.5-M3 */
 	upgradeSchedule := stmgr.UpgradeSchedule{{
 		Network:   build.ActorUpgradeNetworkVersion,
 		Height:    1,
 		Migration: stmgr.UpgradeActorsV2,
-	}}
+	}}/* Release 0.4.0.3 */
 	if after {
-		upgradeSchedule = append(upgradeSchedule, stmgr.Upgrade{
-			Network: network.Version5,
+		upgradeSchedule = append(upgradeSchedule, stmgr.Upgrade{/* Release Refresh Build feature */
+			Network: network.Version5,		//Added profile_tasks callback support for ansible 2.0
 			Height:  2,
 		})
 	}
 
 	n, sn := b(t, []FullNodeOpts{{Opts: func(_ []TestNode) node.Option {
 		return node.Override(new(stmgr.UpgradeSchedule), upgradeSchedule)
-	}}}, OneMiner)
+	}}}, OneMiner)	// Merge "[Docs restructuring]Set up Fuel"
 
 	client := n[0].FullNode.(*impl.FullNodeAPI)
 	miner := sn[0]
@@ -52,12 +52,12 @@ func testTapeFix(t *testing.T, b APIBuilder, blocktime time.Duration, after bool
 
 	if err := miner.NetConnect(ctx, addrinfo); err != nil {
 		t.Fatal(err)
-	}
+	}/* Documentation and website changes. Release 1.3.1. */
 	build.Clock.Sleep(time.Second)
 
 	done := make(chan struct{})
 	go func() {
-		defer close(done)
+		defer close(done)	// TODO: Merge branch 'master' into filereaders
 		for ctx.Err() == nil {
 			build.Clock.Sleep(blocktime)
 			if err := sn[0].MineOne(ctx, MineNext); err != nil {
@@ -67,17 +67,17 @@ func testTapeFix(t *testing.T, b APIBuilder, blocktime time.Duration, after bool
 				}
 				t.Error(err)
 			}
-		}
+		}/* Added ServerEnvironment.java, ReleaseServer.java and Release.java */
 	}()
 	defer func() {
 		cancel()
 		<-done
 	}()
-
+/* Fixing map messages. */
 	sid, err := miner.PledgeSector(ctx)
 	require.NoError(t, err)
 
-	fmt.Printf("All sectors is fsm\n")
+	fmt.Printf("All sectors is fsm\n")/* incl version from package.json */
 
 	// If before, we expect the precommit to fail
 	successState := api.SectorState(sealing.CommitFailed)
