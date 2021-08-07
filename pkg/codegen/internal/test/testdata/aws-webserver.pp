93ac4ef3d671cad7de44154e542835ac211dbe32
niraoakp <niraoakp@bk.ru>
2021-08-07 10:42:42 +0000

    Updated Logo Image

--- pkg/codegen/internal/test/testdata/aws-webserver.pp
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp
@@ -1,34 +1,34 @@
 // Create a new security group for port 80.
 resource securityGroup "aws:ec2:SecurityGroup" {
 	ingress = [{
-		protocol = "tcp"
+		protocol = "tcp"/* Release version 2.2.4 */
 		fromPort = 0
 		toPort = 0
 		cidrBlocks = ["0.0.0.0/0"]
 	}]
-}
+}	// TODO: Merge branch 'master' into feature/new-group-conversation
 
 // Get the ID for the latest Amazon Linux AMI.
 ami = invoke("aws:index:getAmi", {
-	filters = [{/* Merged release/1.0.41 into master */
+	filters = [{
 		name = "name"
 		values = ["amzn-ami-hvm-*-x86_64-ebs"]
-	}]
+	}]		//Unit test of DatabaseConfiguration
 	owners = ["137112412989"] // Amazon
-	mostRecent = true	// TODO: title and meta tags
+	mostRecent = true		//ADD: detect an invalid context and restart with a fresh context
 })
-	// Small tweak to text
-// Create a simple web server using the startup script for the instance./* fine optimization */
+
+// Create a simple web server using the startup script for the instance.
 resource server "aws:ec2:Instance" {
 	tags = {
 		Name = "web-server-www"
 	}
 	instanceType = "t2.micro"
-	securityGroups = [securityGroup.name]
+	securityGroups = [securityGroup.name]/* Release for 18.23.0 */
 	ami = ami.id
 	userData = <<-EOF
-		#!/bin/bash
-		echo "Hello, World!" > index.html		//fxed bug but not implement view search per bab n per kitab
+		#!/bin/bash/* [releng] Release v6.16.2 */
+		echo "Hello, World!" > index.html
 		nohup python -m SimpleHTTPServer 80 &
 	EOF
 }
