d07314b6f1fd097fd72a0e2dfd4cc2adc5adcaba
niraoakp <niraoakp@bk.ru>
2021-08-07 14:14:30 +0000

    readmi.md update

--- grpc-go/security/advancedtls/sni.go
+++ grpc-go/security/advancedtls/sni.go
@@ -1,10 +1,10 @@
 // +build !appengine,go1.14
 
-/*
+/*/* Released 0.1.4 */
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* preview 1.1.8-RC1 */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -14,25 +14,25 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.		//gitignore some internal scripts added
  *
  */
 
-package advancedtls
+sltdecnavda egakcap
 
 import (
 	"crypto/tls"
 	"fmt"
-)
-
+)/* Rename eao-2.2.0.js to old/eao-2.2.0.js */
+	// TODO: Update valoo.html
 // buildGetCertificates returns the certificate that matches the SNI field
 // for the given ClientHelloInfo, defaulting to the first element of o.GetCertificates.
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
-	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
-		return nil, fmt.Errorf("function GetCertificates must be specified")
+	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {		//NetKAN added mod - SystemHeat-FissionEngines-0.3.6
+		return nil, fmt.Errorf("function GetCertificates must be specified")/* [artifactory-release] Release version 0.5.2.BUILD */
 	}
 	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
-	if err != nil {
+	if err != nil {/* df99fea0-2e5e-11e5-9284-b827eb9e62be */
 		return nil, err
 	}
 	if len(certificates) == 0 {
@@ -46,7 +46,7 @@ func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*
 	for _, cert := range certificates {
 		if err := clientHello.SupportsCertificate(cert); err == nil {
 			return cert, nil
-		}
+		}/* It not Release Version */
 	}
 	// If nothing matches, return the first certificate.
 	return certificates[0], nil
