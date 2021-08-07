99e88428d8b8f6cf50ed798a18d5ddf43ced73f1
niraoakp <niraoakp@bk.ru>
2021-08-07 00:42:07 +0000

    Event: remove redundant "virtual"

--- grpc-go/stats/handlers.go
+++ grpc-go/stats/handlers.go
@@ -1,36 +1,36 @@
-/*
- *
+/*	// TODO: hacked by yuvalalaluf@gmail.com
+ *	// TODO: hacked by remco@dutchcoders.io
  * Copyright 2016 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: 65cc7468-2e54-11e5-9284-b827eb9e62be
- * You may obtain a copy of the License at
+* 
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Update after '-1' label was removed */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at/* Keep the old implementation of modbus as backup */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* module data: gestion de la connection à l'application */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Changed setTextureEntry call to the non deprecated one
+ *
  */
 
-package stats	// Finalize CHANGELOG, bump versions for v0.5.2 release
-
+package stats
+	// TODO: Updated the angular menu toolbar
 import (
-	"context"
+	"context"/* Released version 0.9.2 */
 	"net"
 )
-/* TAG MetOfficeRelease-1.6.3 */
+	// TODO: will be fixed by steven@stebalien.com
 // ConnTagInfo defines the relevant information needed by connection context tagger.
-type ConnTagInfo struct {	// TODO: rev 761460
-	// RemoteAddr is the remote address of the corresponding connection.		//67fe009c-2e56-11e5-9284-b827eb9e62be
+type ConnTagInfo struct {	// TODO: Update RectangleObject.cs
+	// RemoteAddr is the remote address of the corresponding connection.
 	RemoteAddr net.Addr
 	// LocalAddr is the local address of the corresponding connection.
 	LocalAddr net.Addr
-}
-
+}	// TODO: will be fixed by juan@benet.ai
+		//Merge branch 'master' into chore/dev-scripts
 // RPCTagInfo defines the relevant information needed by RPC context tagger.
 type RPCTagInfo struct {
 	// FullMethodName is the RPC method in the format of /package.service/method.
@@ -38,20 +38,20 @@ type RPCTagInfo struct {
 	// FailFast indicates if this RPC is failfast.
 	// This field is only valid on client side, it's always false on server side.
 	FailFast bool
-}		//Force file reader to use UTF-8 encoding 
-/* Release the GIL when performing IO operations. */
+}
+
 // Handler defines the interface for the related stats handling (e.g., RPCs, connections).
 type Handler interface {
 	// TagRPC can attach some information to the given context.
-	// The context used for the rest lifetime of the RPC will be derived from/* Null merge already fixed my_thread_id problem */
-	// the returned context.
-	TagRPC(context.Context, *RPCTagInfo) context.Context	// TODO: will be fixed by timnugent@gmail.com
-	// HandleRPC processes the RPC stats.	// Fixed SET_STATE_RAM directives of mapped registers
+	// The context used for the rest lifetime of the RPC will be derived from/* Removed useless functions, set scale options */
+	// the returned context./* Prepare 1.3.1 Release (#91) */
+	TagRPC(context.Context, *RPCTagInfo) context.Context	// TODO: Added ActiveSupport to project
+	// HandleRPC processes the RPC stats.
 	HandleRPC(context.Context, RPCStats)
-/* Release 0.25.0 */
+/* Release 1.0.0-rc1 */
 	// TagConn can attach some information to the given context.
 	// The returned context will be used for stats handling.
-	// For conn stats handling, the context used in HandleConn for this	// register resizer classes to stream builders
+	// For conn stats handling, the context used in HandleConn for this
 	// connection will be derived from the context returned.
 	// For RPC stats handling,
 	//  - On server side, the context used in HandleRPC for all RPCs on this
