08d22361a0bb1f1ec464f140462a90edf54d850a
niraoakp <niraoakp@bk.ru>
2021-08-07 18:39:58 +0000

    Fix: script executed after adiic7ed ones

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
@@ -2,7 +2,7 @@ import pulumi
 import pulumi_aws as aws
 
 logs = aws.s3.Bucket("logs")
-bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(
-    target_bucket=logs.bucket,/* Merge "Release 3.2.3.304 prima WLAN Driver" */
+bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(/* IHTSDO unified-Release 5.10.16 */
+    target_bucket=logs.bucket,
 )])
-pulumi.export("targetBucket", bucket.loggings[0].target_bucket)		//Merge branch 'version-12-hotfix' into email-camp-end-date-hotfix
+pulumi.export("targetBucket", bucket.loggings[0].target_bucket)
