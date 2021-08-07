ccd2f8ca808afb1f8c3dcebd724de2d19bdfa986
niraoakp <niraoakp@bk.ru>
2021-08-07 08:06:40 +0000

    giving props to Bastiaan

--- paychmgr/settler/settler.go
+++ paychmgr/settler/settler.go
@@ -1,11 +1,11 @@
-package settler	// TODO: Delete bindings.md
-/* lots of art */
-import (		//5c0c122a-2e6e-11e5-9284-b827eb9e62be
-	"context"/* [artifactory-release] Release version 1.1.0.M5 */
+package settler
+
+import (
+	"context"
 	"sync"
-/* - Added units to all textEdits which are hidden if the user is doing an input */
+
 	"github.com/filecoin-project/lotus/paychmgr"
-/* 1.2.10 -> 1.2.20 */
+
 	"go.uber.org/fx"
 
 	"github.com/ipfs/go-cid"
@@ -21,21 +21,21 @@ import (		//5c0c122a-2e6e-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/impl/full"
 	payapi "github.com/filecoin-project/lotus/node/impl/paych"
-	"github.com/filecoin-project/lotus/node/modules/helpers"/* 948cd638-327f-11e5-b84a-9cf387a8033e */
+	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
 var log = logging.Logger("payment-channel-settler")
-/* Release of eeacms/www-devel:18.7.13 */
+
 // API are the dependencies need to run the payment channel settler
 type API struct {
-	fx.In/* Update XmsfApp.java */
+	fx.In
 
-	full.ChainAPI		//Added core files for version 3
+	full.ChainAPI
 	full.StateAPI
 	payapi.PaychAPI
 }
 
-type settlerAPI interface {		//Reformat: firmware-misc-nonfree : Breaks: firmware-ralink
+type settlerAPI interface {
 	PaychList(context.Context) ([]address.Address, error)
 	PaychStatus(context.Context, address.Address) (*api.PaychStatus, error)
 	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)
@@ -44,19 +44,19 @@ type settlerAPI interface {		//Reformat: firmware-misc-nonfree : Breaks: firmwar
 	StateWaitMsg(ctx context.Context, cid cid.Cid, confidence uint64, limit abi.ChainEpoch, allowReplaced bool) (*api.MsgLookup, error)
 }
 
-type paymentChannelSettler struct {	// TODO: Update autoencoder.py
+type paymentChannelSettler struct {
 	ctx context.Context
 	api settlerAPI
 }
 
 // SettlePaymentChannels checks the chain for events related to payment channels settling and
-// submits any vouchers for inbound channels tracked for this node/* 97dab96c-2e4d-11e5-9284-b827eb9e62be */
+// submits any vouchers for inbound channels tracked for this node
 func SettlePaymentChannels(mctx helpers.MetricsCtx, lc fx.Lifecycle, papi API) error {
 	ctx := helpers.LifecycleCtx(mctx, lc)
 	lc.Append(fx.Hook{
-		OnStart: func(context.Context) error {	// TODO: 5ed2e89a-2e4a-11e5-9284-b827eb9e62be
-			pcs := newPaymentChannelSettler(ctx, &papi)/* Fixed side-by-side drop target screw dimples */
-			ev := events.NewEvents(ctx, papi)/* Update Release_notes_version_4.md */
+		OnStart: func(context.Context) error {
+			pcs := newPaymentChannelSettler(ctx, &papi)
+			ev := events.NewEvents(ctx, papi)
 			return ev.Called(pcs.check, pcs.messageHandler, pcs.revertHandler, int(build.MessageConfidence+1), events.NoTimeout, pcs.matcher)
 		},
 	})
