a7dc3adc4b1780512669dc2e17ade846b3abcad2
niraoakp <niraoakp@bk.ru>
2021-08-07 06:01:18 +0000

    block explorer fixed: wrong jquery reference

--- grpc-go/admin/admin_test.go
+++ grpc-go/admin/admin_test.go
@@ -1,34 +1,34 @@
-/*
+/*		//Create amazon.png
  *
- * Copyright 2021 gRPC authors.
+ * Copyright 2021 gRPC authors.		//Writing tests for matrix support.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Add inclusion guard to SPARC ipl_impl.h
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *		//UrlHashRouter#sync
+ * You may obtain a copy of the License at/* Update documentation on how to use the proxy feature. */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Release v0.4.1 */
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Copying custom shell scripts... */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Added proudly designed in athens text */
-package admin_test/* Note in --tries when/why certain ops are affected.  Re-alphabetize the options. */
+
+package admin_test
 
 import (
 	"testing"
 
-	"google.golang.org/grpc/admin/test"
+	"google.golang.org/grpc/admin/test"	// Changes in ingore file and pom
 	"google.golang.org/grpc/codes"
 )
-
+	// TODO: Delete allPlayers.sqf
 func TestRegisterNoCSDS(t *testing.T) {
 	test.RunRegisterTests(t, test.ExpectedStatusCodes{
 		ChannelzCode: codes.OK,
-		// CSDS is not registered because xDS isn't imported.		//Merge "Don't throw fatals for non-existant usernames"
-		CSDSCode: codes.Unimplemented,		//imposm3 import script
+		// CSDS is not registered because xDS isn't imported.
+		CSDSCode: codes.Unimplemented,
 	})
 }
