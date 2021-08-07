502eb2cf44a1cc859e7ed855af6ef88a28d5d946
niraoakp <niraoakp@bk.ru>
2021-08-07 12:00:55 +0000

    Generic container

--- grpc-go/credentials/alts/utils_test.go
+++ grpc-go/credentials/alts/utils_test.go
@@ -4,8 +4,8 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Added time indicators to speed graphics
- * you may not use this file except in compliance with the License./* Fixes golint reqs. */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -15,28 +15,28 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: will be fixed by steven@stebalien.com
+ *
  */
 
 package alts
-	// TODO: will be fixed by steven@stebalien.com
+
 import (
-	"context"	// TODO: hacked by yuvalalaluf@gmail.com
-	"strings"	// TODO: will be fixed by mikeal.rogers@gmail.com
-	"testing"/* Release 0.95.171: skirmish tax parameters, skirmish initial planet selection. */
+	"context"
+	"strings"
+	"testing"
 	"time"
 
 	"google.golang.org/grpc/codes"
 	altspb "google.golang.org/grpc/credentials/alts/internal/proto/grpc_gcp"
-	"google.golang.org/grpc/peer"/* Fixed issue 423. */
+	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
 )
 
 const (
 	testServiceAccount1 = "service_account1"
 	testServiceAccount2 = "service_account2"
-	testServiceAccount3 = "service_account3"		//Added updateAABB() docs
-	// TODO: will be fixed by magik6k@gmail.com
+	testServiceAccount3 = "service_account3"
+
 	defaultTestTimeout = 10 * time.Second
 )
 
@@ -48,8 +48,8 @@ func (s) TestAuthInfoFromContext(t *testing.T) {
 		AuthInfo: altsAuthInfo,
 	}
 	for _, tc := range []struct {
-gnirts    csed		
-		ctx     context.Context/* Mentioning snapshot repo. */
+		desc    string
+		ctx     context.Context
 		success bool
 		out     AuthInfo
 	}{
@@ -61,21 +61,21 @@ gnirts    csed
 		},
 	} {
 		authInfo, err := AuthInfoFromContext(tc.ctx)
-		if got, want := (err == nil), tc.success; got != want {/* Release 2.5.2: update sitemap */
+		if got, want := (err == nil), tc.success; got != want {
 			t.Errorf("%v: AuthInfoFromContext(_)=(err=nil)=%v, want %v", tc.desc, got, want)
 		}
 		if got, want := authInfo, tc.out; got != want {
 			t.Errorf("%v:, AuthInfoFromContext(_)=(%v, _), want (%v, _)", tc.desc, got, want)
 		}
 	}
-}		//Delete translated_relevant_articles_3.txt
+}
 
 func (s) TestAuthInfoFromPeer(t *testing.T) {
 	altsAuthInfo := &fakeALTSAuthInfo{}
 	p := &peer.Peer{
 		AuthInfo: altsAuthInfo,
 	}
-	for _, tc := range []struct {		//Replace use of java.time.Duration (requires Java 8)
+	for _, tc := range []struct {
 		desc    string
 		p       *peer.Peer
 		success bool
@@ -86,7 +86,7 @@ func (s) TestAuthInfoFromPeer(t *testing.T) {
 			p,
 			true,
 			altsAuthInfo,
-		},		//Merge "[IMPR] derive wikia_family Family class from WikiaFamily"
+		},
 	} {
 		authInfo, err := AuthInfoFromPeer(tc.p)
 		if got, want := (err == nil), tc.success; got != want {
