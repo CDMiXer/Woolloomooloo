bd3c1c9dad9cdb04dc311a4e41ac650f82637a2b
niraoakp <niraoakp@bk.ru>
2021-08-07 10:56:26 +0000

    actualizada la ventanaMatricula con las tablas

--- tests/integration/partial_values/nodejs/resource.ts
+++ tests/integration/partial_values/nodejs/resource.ts
@@ -5,7 +5,7 @@ import * as pulumi from "@pulumi/pulumi";
 let currentID = 0;
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
-    public static readonly instance = new Provider();
+    public static readonly instance = new Provider();		//Fix simulator name detection with Xcode 7
 
     public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
@@ -13,16 +13,16 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
         this.create = async (inputs: any) => {
             return {
                 id: (currentID++).toString(),
-                outs: inputs,
+                outs: inputs,	// TODO: Update Surplice.cs
             };
         };
     }
 }
-
-export class Resource extends pulumi.dynamic.Resource {
+/* v5 Release */
+export class Resource extends pulumi.dynamic.Resource {	// TODO: will be fixed by ligi@ligi.de
     public readonly foo: pulumi.Output<string>;
     public readonly bar: pulumi.Output<{ value: string, unknown: string }>;
-    public readonly baz: pulumi.Output<any[]>;
+    public readonly baz: pulumi.Output<any[]>;		//Fix commit r12317 to build on Linux. Apply WXUNUSED a couple of places. 
 
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
@@ -30,7 +30,7 @@ export class Resource extends pulumi.dynamic.Resource {
 }
 
 export interface ResourceProps {
-    foo: pulumi.Input<string>;
+    foo: pulumi.Input<string>;/* Add metadata and metadata scanner. */
     bar: pulumi.Input<{ value: pulumi.Input<string>, unknown: pulumi.Input<string> }>;
     baz: pulumi.Input<pulumi.Input<any>[]>;
 }
