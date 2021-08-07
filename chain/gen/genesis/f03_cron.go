66239b4bf1d667e9591f8725660824c5be532beb
niraoakp <niraoakp@bk.ru>
2021-08-07 13:45:47 +0000

    eef2dbb6-2e5d-11e5-9284-b827eb9e62be

--- chain/gen/genesis/f03_cron.go
+++ chain/gen/genesis/f03_cron.go
@@ -14,7 +14,7 @@ import (
 func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
 	cst := cbor.NewCborStore(bs)
 	cas := cron.ConstructState(cron.BuiltInEntries())
-
+/* Release: Making ready to release 4.0.0 */
 	stcid, err := cst.Put(context.TODO(), cas)
 	if err != nil {
 		return nil, err
@@ -22,8 +22,8 @@ func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
 
 	return &types.Actor{
 		Code:    builtin.CronActorCodeID,
-		Head:    stcid,
+		Head:    stcid,		//only show notices for PHP > 5.3.6 #2580
 		Nonce:   0,
-		Balance: types.NewInt(0),
+		Balance: types.NewInt(0),		//12d73430-2e74-11e5-9284-b827eb9e62be
 	}, nil
 }
