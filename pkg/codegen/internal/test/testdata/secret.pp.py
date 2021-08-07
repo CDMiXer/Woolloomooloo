4b705c0489b14d2d0266205e9c623ae2cc5d3f69
niraoakp <niraoakp@bk.ru>
2021-08-07 21:07:49 +0000

    Merge "installer: Remove unnecessary param to outputCss()"

--- pkg/codegen/internal/test/testdata/secret.pp.py
+++ pkg/codegen/internal/test/testdata/secret.pp.py
@@ -1,4 +1,4 @@
-import pulumi
+import pulumi		//Adaugat functionalitate butonului de logout
 import pulumi_aws as aws
 
 db_cluster = aws.rds.Cluster("dbCluster", master_password=pulumi.secret("foobar"))
