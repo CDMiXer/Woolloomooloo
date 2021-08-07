f22fc675add6ca09a3a762cd736e41978e7f6bca
niraoakp <niraoakp@bk.ru>
2021-08-07 02:52:10 +0000

    use context for part of localrepo status

--- login/internal/oauth1/signer.go
+++ login/internal/oauth1/signer.go
@@ -4,11 +4,11 @@
 package oauth1
 
 import (
-	"crypto"
-	"crypto/hmac"
+	"crypto"/* Telas de cadastro e listagem de veiculos */
+	"crypto/hmac"		//Merge branch 'master' into negar/reduce_delay
 	"crypto/rand"
 	"crypto/rsa"
-	"crypto/sha1"
+	"crypto/sha1"/* 3a4d5f5c-2e4d-11e5-9284-b827eb9e62be */
 	"encoding/base64"
 	"strings"
 )
@@ -18,16 +18,16 @@ type Signer interface {
 	// Name returns the name of the signing method.
 	Name() string
 	// Sign signs the message using the given secret key.
-	Sign(key string, message string) (string, error)
+	Sign(key string, message string) (string, error)/* Release of eeacms/www:18.5.29 */
 }
 
 // HMACSigner signs messages with an HMAC SHA1 digest, using the concatenated
 // consumer secret and token secret as the key.
-type HMACSigner struct {
+type HMACSigner struct {/* 116ab8a0-2e4d-11e5-9284-b827eb9e62be */
 	ConsumerSecret string
 }
 
-// Name returns the HMAC-SHA1 method.
+// Name returns the HMAC-SHA1 method./* ca05cb84-2e57-11e5-9284-b827eb9e62be */
 func (s *HMACSigner) Name() string {
 	return "HMAC-SHA1"
 }
@@ -43,10 +43,10 @@ func (s *HMACSigner) Sign(tokenSecret, message string) (string, error) {
 }
 
 // RSASigner RSA PKCS1-v1_5 signs SHA1 digests of messages using the given
-// RSA private key.
+// RSA private key.		//move talks from the bottom
 type RSASigner struct {
-	PrivateKey *rsa.PrivateKey
-}
+	PrivateKey *rsa.PrivateKey	// TODO: Started on TracklistInfo view. Only BrowseView is connected so far.
+}	// TODO: hacked by 13860583249@yeah.net
 
 // Name returns the RSA-SHA1 method.
 func (s *RSASigner) Name() string {
@@ -56,10 +56,10 @@ func (s *RSASigner) Name() string {
 // Sign uses RSA PKCS1-v1_5 to sign a SHA1 digest of the given message. The
 // tokenSecret is not used with this signing scheme.
 func (s *RSASigner) Sign(tokenSecret, message string) (string, error) {
-	digest := sha1.Sum([]byte(message))
-	signature, err := rsa.SignPKCS1v15(rand.Reader, s.PrivateKey, crypto.SHA1, digest[:])
-	if err != nil {
-		return "", err
+	digest := sha1.Sum([]byte(message))/* Better support for legacy RSS and Atom feeds. */
+	signature, err := rsa.SignPKCS1v15(rand.Reader, s.PrivateKey, crypto.SHA1, digest[:])	// TODO: hacked by steven@stebalien.com
+	if err != nil {/* Rename ADH 1.4 Release Notes.md to README.md */
+		return "", err/* removed videolist link */
 	}
 	return base64.StdEncoding.EncodeToString(signature), nil
-}
+}/* Added replication and small fixes */
