b69ce668c2e7b01e9021511995871b4bbaf23e93
niraoakp <niraoakp@bk.ru>
2021-08-07 15:18:48 +0000

    Fixes link to CityBikes in the README

--- pubsub/doc.go
+++ pubsub/doc.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: Added install of pip3 for building on Ubuntu
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//chore: Disable test cache.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//add: website "Angular Test Patterns"
+//
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* deep symbolize cells options */
 
 // Package pubsub implements a thread safe publish subscriber,
-// allowing multiple publishers to distribute messages to		//Remove unused class constant defaults
+// allowing multiple publishers to distribute messages to		//Added Compress now
 // multiple subscribers.
-package pubsub	// TODO: rename test.
+package pubsub
