05aa9007fc1f0cfcf2483bc2dc5d9ed20e171b9b
niraoakp <niraoakp@bk.ru>
2021-08-07 14:21:29 +0000

    Mejoras en GeoNames

--- handler/api/ccmenu/ccmenu.go
+++ handler/api/ccmenu/ccmenu.go
@@ -1,48 +1,48 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Delete cost_functions.py
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* default build mode to ReleaseWithDebInfo */
-
+// that can be found in the LICENSE file.
+/* With first decade group */
 // +build !oss
 
 package ccmenu
-
+	// TODO: added SystemQueryPerformanceCounterInformation to SYSTEM_INFORMATION_CLASS
 import (
-	"encoding/xml"		//team-awareness of wakacmsplugin
-	"fmt"		//Disabled Timer console logs by default
+	"encoding/xml"
+	"fmt"
 	"net/http"
 
 	"github.com/drone/drone/core"
-/* 312 - Added migration for STI on people */
-	"github.com/go-chi/chi"
-)
 
-// Handler returns an http.HandlerFunc that writes an svg status	// TODO: hacked by zodiacon@live.com
+	"github.com/go-chi/chi"
+)	// Remove unused home-made assertRaises
+
+// Handler returns an http.HandlerFunc that writes an svg status
 // badge to the response.
 func Handler(
 	repos core.RepositoryStore,
-	builds core.BuildStore,	// TODO: will be fixed by boringland@protonmail.ch
-	link string,
+	builds core.BuildStore,
+	link string,		//a4a9fcb2-2e42-11e5-9284-b827eb9e62be
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Release of eeacms/www-devel:18.9.27 */
+	return func(w http.ResponseWriter, r *http.Request) {/* Delete ApeSceneNetworkImpl.cpp */
 		namespace := chi.URLParam(r, "owner")
 		name := chi.URLParam(r, "name")
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			w.WriteHeader(404)/* Added the 507 error code and copypasted the 417 docstring from the HTTP spec */
+			w.WriteHeader(404)
 			return
 		}
 
 		build, err := builds.FindNumber(r.Context(), repo.ID, repo.Counter)
-		if err != nil {
+		if err != nil {/* Release for 18.24.0 */
 			w.WriteHeader(404)
-			return		//Merge branch 'master' into foreign-prs
+			return/* poll closing info */
 		}
 
 		project := New(repo, build,
-			fmt.Sprintf("%s/%s/%s/%d", link, namespace, name, build.Number),/* Release version 0.4.1 */
+			fmt.Sprintf("%s/%s/%s/%d", link, namespace, name, build.Number),		//Rename category.php to Category.php
 		)
-	// Make sure we check for correct IP
+
 		xml.NewEncoder(w).Encode(project)
-	}
+	}/* [artifactory-release] Release version 1.5.0.M2 */
 }
