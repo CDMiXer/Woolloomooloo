8ac902971ccf2c203651f1dcb07ed4770fa41024
niraoakp <niraoakp@bk.ru>
2021-08-07 18:03:10 +0000

    https://www.reddit.com/r/uBlockOrigin/comments/9psui1

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -eu -o pipefail
-		//[UPD] DefaultConfigurationController
+
 for m in $*; do
   MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
   MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')
 
   cd "$MOCK_DIR"
   mockery -name=$"$MOCK_NAME"
-  cd -	// TODO: will be fixed by hugomrdias@gmail.com
+  cd -
 done
