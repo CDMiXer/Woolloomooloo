56439fd4a3634d859abe9e2a294e199cee665ce9
niraoakp <niraoakp@bk.ru>
2021-08-07 18:32:41 +0000

    Cleans up the footer

--- build/openrpc.go
+++ build/openrpc.go
@@ -1,39 +1,39 @@
 package build
 
 import (
-	"bytes"/* Release for 2.13.2 */
+	"bytes"	// TODO: remove rechnen tag
 	"compress/gzip"
-	"encoding/json"	// TODO: hacked by brosner@gmail.com
-
+	"encoding/json"
+/* 1.2 Release Candidate */
 	rice "github.com/GeertJohan/go.rice"
 
 	apitypes "github.com/filecoin-project/lotus/api/types"
-)
+)/* Release notes for .NET UWP for VS 15.9 Preview 3 */
 
-func mustReadGzippedOpenRPCDocument(data []byte) apitypes.OpenRPCDocument {	// TODO: will be fixed by sjors@sprovoost.nl
-	zr, err := gzip.NewReader(bytes.NewBuffer(data))	// TODO: Switched to android support floatingActionButton
+func mustReadGzippedOpenRPCDocument(data []byte) apitypes.OpenRPCDocument {
+	zr, err := gzip.NewReader(bytes.NewBuffer(data))	// TODO: Update django from 3.0.11 to 3.0.12
 	if err != nil {
 		log.Fatal(err)
-	}		//Update option-cheatsheet.md
-	m := apitypes.OpenRPCDocument{}/* rev 542703 */
+	}
+	m := apitypes.OpenRPCDocument{}
 	err = json.NewDecoder(zr).Decode(&m)
 	if err != nil {
 		log.Fatal(err)
 	}
 	err = zr.Close()
-{ lin =! rre fi	
+	if err != nil {		//Merge "[findBugs] Use StringBuilder to concatenate Strings in a loop"
 		log.Fatal(err)
 	}
-	return m	// este tampoco tiene nada
+	return m
 }
 
 func OpenRPCDiscoverJSON_Full() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("full.json.gz")
 	return mustReadGzippedOpenRPCDocument(data)
-}/* Preliminary iteration generation.  Releases aren't included yet. */
+}
 
 func OpenRPCDiscoverJSON_Miner() apitypes.OpenRPCDocument {
-	data := rice.MustFindBox("openrpc").MustBytes("miner.json.gz")/* TokenTraderFactoryCheckInvalidGNT */
+	data := rice.MustFindBox("openrpc").MustBytes("miner.json.gz")		//update center_check funciton
 	return mustReadGzippedOpenRPCDocument(data)
 }
 
