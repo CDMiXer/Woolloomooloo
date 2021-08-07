4bf3c0bb8009a5b5be3eba48ab5aa7cc39b029b6
niraoakp <niraoakp@bk.ru>
2021-08-07 18:57:52 +0000

    Update bot_detect.py

--- pkg/codegen/internal/test/testdata/azure-sa.pp.go
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.go
@@ -1,9 +1,9 @@
 package main
-
+	// TODO: New Git-specific test class
 import (
-	"github.com/pulumi/pulumi-azure/sdk/v3/go/azure/core"
+	"github.com/pulumi/pulumi-azure/sdk/v3/go/azure/core"/* Bug 3941: Release notes typo */
 	"github.com/pulumi/pulumi-azure/sdk/v3/go/azure/storage"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* Preparando la versión 0.4.6: domotica_servidor.py */
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
 
@@ -13,24 +13,24 @@ func main() {
 		storageAccountNameParam := cfg.Require("storageAccountNameParam")
 		resourceGroupNameParam := cfg.Require("resourceGroupNameParam")
 		resourceGroupVar, err := core.LookupResourceGroup(ctx, &core.LookupResourceGroupArgs{
-			Name: resourceGroupNameParam,
+			Name: resourceGroupNameParam,		//Create non_activerecord.markdown
 		}, nil)
 		if err != nil {
 			return err
 		}
 		locationParam := resourceGroupVar.Location
 		if param := cfg.Get("locationParam"); param != "" {
-			locationParam = param
+			locationParam = param	// TODO: will be fixed by arajasek94@gmail.com
 		}
 		storageAccountTierParam := "Standard"
 		if param := cfg.Get("storageAccountTierParam"); param != "" {
 			storageAccountTierParam = param
 		}
 		storageAccountTypeReplicationParam := "LRS"
-		if param := cfg.Get("storageAccountTypeReplicationParam"); param != "" {
+		if param := cfg.Get("storageAccountTypeReplicationParam"); param != "" {/* added Molten Ravager */
 			storageAccountTypeReplicationParam = param
 		}
-		storageAccountResource, err := storage.NewAccount(ctx, "storageAccountResource", &storage.AccountArgs{
+		storageAccountResource, err := storage.NewAccount(ctx, "storageAccountResource", &storage.AccountArgs{/* Release 0.95.165: changes due to fleet name becoming null. */
 			Name:                   pulumi.String(storageAccountNameParam),
 			AccountKind:            pulumi.String("StorageV2"),
 			Location:               pulumi.String(locationParam),
@@ -42,6 +42,6 @@ func main() {
 			return err
 		}
 		ctx.Export("storageAccountNameOut", storageAccountResource.Name)
-		return nil
+		return nil		//correct release md
 	})
 }
