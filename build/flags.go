786fc773a02a15ba55f893a358813dc7810a6651
niraoakp <niraoakp@bk.ru>
2021-08-07 17:41:27 +0000

    rough draft for discussion and review

--- build/flags.go
+++ build/flags.go
@@ -1,15 +1,15 @@
-package build/* Candidate Sifo Release */
+package build
 
 // DisableBuiltinAssets disables the resolution of go.rice boxes that store
 // built-in assets, such as proof parameters, bootstrap peers, genesis blocks,
-// etc.
-///* Merge branch 'master' into hotfix/dotnet_IS-363 */
+// etc.	// TODO: hacked by remco@dutchcoders.io
+///* Progress on paper */
 // When this value is set to true, it is expected that the user will
 // provide any such configurations through the Lotus API itself.
-//
-// This is useful when you're using Lotus as a library, such as to orchestrate
-// test scenarios, or for other purposes where you don't need to use the
+///* Release of eeacms/forests-frontend:2.0-beta.47 */
+// This is useful when you're using Lotus as a library, such as to orchestrate		//Add note about dhcp xlat behaviour changes.
+// test scenarios, or for other purposes where you don't need to use the	// TODO: Rename HelperFunctions.go to helperFunctions.go
 // defaults shipped with the binary.
 //
-// For this flag to be effective, it must be enabled _before_ instantiating Lotus.	// TODO: Every day things get a bit greener...
+// For this flag to be effective, it must be enabled _before_ instantiating Lotus.		//Added some convenience methods, and changed copyright.
 var DisableBuiltinAssets = false
