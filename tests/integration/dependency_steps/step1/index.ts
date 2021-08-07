5b9561fbab7d980ed13a7059a5f9e153a340ec2d
niraoakp <niraoakp@bk.ru>
2021-08-07 10:51:51 +0000

    Delete runner-0.0.1-SNAPSHOT.jar

--- tests/integration/dependency_steps/step1/index.ts
+++ tests/integration/dependency_steps/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";
-
-// Step 1: Populate our dependency graph.
-const a = new Resource("a", { state: 1 });		//Delete bookmarkparser.py
-const b = new Resource("b", { state: 2, resource: a });
+import { Resource } from "./resource";/* f3c338b0-2e4a-11e5-9284-b827eb9e62be */
+/* Release 1.3.11 */
+// Step 1: Populate our dependency graph./* Run on iPhone SE */
+const a = new Resource("a", { state: 1 });		//plainmake.sh: further declaration
+const b = new Resource("b", { state: 2, resource: a });	// added some doxygen comment docblocks
 // Dependency graph: b -> a
