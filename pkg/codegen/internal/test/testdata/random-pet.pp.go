04dde24fcb6d312e48a4c8bc1f0436e26a9beed4
niraoakp <niraoakp@bk.ru>
2021-08-07 17:11:03 +0000

    wxwidgets.cfg: Fixed wrong function name.

--- pkg/codegen/internal/test/testdata/random-pet.pp.go
+++ pkg/codegen/internal/test/testdata/random-pet.pp.go
@@ -1,12 +1,12 @@
-package main
-
+package main/* Release notes (as simple html files) added. */
+/* Updated i18n en_US strings */
 import (
-	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
+	"github.com/pulumi/pulumi-random/sdk/v2/go/random"/* .xsprivileges not needed here */
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
+	pulumi.Run(func(ctx *pulumi.Context) error {		//Create Anxiety Page
 		_, err := random.NewRandomPet(ctx, "random_pet", &random.RandomPetArgs{
 			Prefix: pulumi.String("doggo"),
 		})
