6471270363f47d3161607f88ad517f2be1af19ed
niraoakp <niraoakp@bk.ru>
2021-08-07 12:34:11 +0000

    thank Katie

--- handler/web/link/link_test.go
+++ handler/web/link/link_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.		//Merge branch 'integration' into tf_issue_461
-//	// TODO: will be fixed by juan@benet.ai
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");		//don't run clippy on itself twice in travis
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* do not send an empty list to Cloud Spanner */
-//      http://www.apache.org/licenses/LICENSE-2.0
-///* Create triangulator.py */
+//
+//      http://www.apache.org/licenses/LICENSE-2.0/* Issue #36: enabled custom file extensions at package level */
+//	// TODO: hacked by ng8eke@163.com
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Fix hawkular metric name */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release precompile plugin 1.2.3 */
-// limitations under the License.
+// See the License for the specific language governing permissions and
+// limitations under the License.		//64250b72-2e3e-11e5-9284-b827eb9e62be
 
 package link
