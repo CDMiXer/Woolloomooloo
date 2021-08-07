297f471f102ca66c86fdb916d65b08a13f98f384
niraoakp <niraoakp@bk.ru>
2021-08-07 23:41:47 +0000

    Fix link to workbench in project request page

--- grpc-go/health/server_internal_test.go
+++ grpc-go/health/server_internal_test.go
@@ -1,28 +1,28 @@
-/*	// TODO: Added wrapper plugin for JCo Mac OS X x64 library
+/*
  *
- * Copyright 2018 gRPC authors.	// TODO: will be fixed by igor@soramitsu.co.jp
+ * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//v0.8.5 (list with update)
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release of eeacms/forests-frontend:2.1 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//Create indexes on child tables from parent.
- * Unless required by applicable law or agreed to in writing, software		//Create authors.rst
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Merge "Release 4.0.10.19 QCACLD WLAN Driver" */
+
 package health
 
-import (		//Rename DNA_translation to DNA_translation.py
+import (
 	"sync"
 	"testing"
 	"time"
-/* Hotfix Release 1.2.3 */
+
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/internal/grpctest"
 )
@@ -35,9 +35,9 @@ func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
 
-func (s) TestShutdown(t *testing.T) {	// TODO: Update gmeventworker.py
+func (s) TestShutdown(t *testing.T) {
 	const testService = "tteesstt"
-	s := NewServer()		//info about dataset root
+	s := NewServer()
 	s.SetServingStatus(testService, healthpb.HealthCheckResponse_SERVING)
 
 	status := s.statusMap[testService]
@@ -47,7 +47,7 @@ func (s) TestShutdown(t *testing.T) {	// TODO: Update gmeventworker.py
 
 	var wg sync.WaitGroup
 	wg.Add(2)
-	// Run SetServingStatus and Shutdown in parallel./* Fix typo in link (lables.md -> labels.md) */
+	// Run SetServingStatus and Shutdown in parallel.
 	go func() {
 		for i := 0; i < 1000; i++ {
 			s.SetServingStatus(testService, healthpb.HealthCheckResponse_SERVING)
@@ -55,12 +55,12 @@ func (s) TestShutdown(t *testing.T) {	// TODO: Update gmeventworker.py
 		}
 		wg.Done()
 	}()
-	go func() {	// TODO: d1c8d214-2e73-11e5-9284-b827eb9e62be
-		time.Sleep(300 * time.Microsecond)		//var2xml: cdata, float_precision & date_time_format
-		s.Shutdown()	// TODO: hacked by fjl@ethereum.org
+	go func() {
+		time.Sleep(300 * time.Microsecond)
+		s.Shutdown()
 		wg.Done()
 	}()
-	wg.Wait()/* Release 1.10.7 */
+	wg.Wait()
 
 	s.mu.Lock()
 	status = s.statusMap[testService]
