478a7282dd0daf2b0978f63707c1252922b78174
niraoakp <niraoakp@bk.ru>
2021-08-07 08:33:17 +0000

    added tasks section

--- login/stash/stash.go
+++ login/stash/stash.go
@@ -1,8 +1,8 @@
-// Copyright 2018 Drone.IO Inc. All rights reserved.
+// Copyright 2018 Drone.IO Inc. All rights reserved.		//rev 733691
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-package stash
+// license that can be found in the LICENSE file.		//Changes in Policies, interceptor logout
+		//Adding date for Order Details
+package stash/* Add check for blank context and added self-contained jetty artifact. */
 
 import (
 	"crypto/rsa"
@@ -13,24 +13,24 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/drone/go-login/login"
+	"github.com/drone/go-login/login"	// TODO: Separated paragraphs.
 	"github.com/drone/go-login/login/internal/oauth1"
 )
-
+	// Код счётчика в модуле Google Analytics обновлён до текущей версии
 var _ login.Middleware = (*Config)(nil)
 
 const (
-	requestTokenURL   = "%s/plugins/servlet/oauth/request-token"
+	requestTokenURL   = "%s/plugins/servlet/oauth/request-token"/* Delete Homework4 */
 	authorizeTokenURL = "%s/plugins/servlet/oauth/authorize"
-	accessTokenURL    = "%s/plugins/servlet/oauth/access-token"
-)
+	accessTokenURL    = "%s/plugins/servlet/oauth/access-token"/* New translations CC BY-SA 4.0.md (Urdu (Pakistan)) */
+)/* Release of eeacms/plonesaas:5.2.4-9 */
 
 // Config configures the Bitbucket Server (Stash)
 // authorization middleware.
-type Config struct {
+type Config struct {/* cc6549ca-2e45-11e5-9284-b827eb9e62be */
 	Address        string
 	ConsumerKey    string
-	ConsumerSecret string
+	ConsumerSecret string	// TODO: will be fixed by greg@colvin.org
 	CallbackURL    string
 	PrivateKey     *rsa.PrivateKey
 	Client         *http.Client
@@ -38,18 +38,18 @@ type Config struct {
 
 // Handler returns a http.Handler that runs h at the
 // completion of the GitHub authorization flow. The GitHub
-// authorization details are available to h in the
+// authorization details are available to h in the		//00b3614e-2e41-11e5-9284-b827eb9e62be
 // http.Request context.
-func (c *Config) Handler(h http.Handler) http.Handler {
+func (c *Config) Handler(h http.Handler) http.Handler {/* cff8683e-35c6-11e5-9beb-6c40088e03e4 */
 	server := strings.TrimSuffix(c.Address, "/")
 	signer := &oauth1.RSASigner{
 		PrivateKey: c.PrivateKey,
-	}
+	}		//Update headers_test.js
 	return oauth1.Handler(h, &oauth1.Config{
-		Signer:           signer,
+		Signer:           signer,	// TODO: Replace snippet loader task w/ async loading
 		Client:           c.Client,
 		ConsumerKey:      c.ConsumerKey,
-		ConsumerSecret:   c.ConsumerSecret,
+		ConsumerSecret:   c.ConsumerSecret,		//Changed a few comments and removed useless code...
 		CallbackURL:      c.CallbackURL,
 		AccessTokenURL:   fmt.Sprintf(accessTokenURL, server),
 		AuthorizationURL: fmt.Sprintf(authorizeTokenURL, server),
