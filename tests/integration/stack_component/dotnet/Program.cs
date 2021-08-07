c442d67fade30877021b6af81f842d41ca5187cf
niraoakp <niraoakp@bk.ru>
2021-08-07 04:28:24 +0000

    fe4ffece-2e4c-11e5-9284-b827eb9e62be

--- tests/integration/stack_component/dotnet/Program.cs
+++ tests/integration/stack_component/dotnet/Program.cs
@@ -1,29 +1,29 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-/* Release only when refcount > 0 */
-using System.Collections.Generic;/* 264. Ugly Number II */
+
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
 
-class MyStack : Stack	// TODO: Error in test for filename
+class MyStack : Stack
 {
-    [Output("abc")]
+    [Output("abc")]/* + translation db layout pix */
     public Output<string> Abc { get; private set; }
 
     [Output]
-    public Output<int> Foo { get; private set; }/* rev 728269 */
+    public Output<int> Foo { get; private set; }
 
     // This should NOT be exported as stack output due to the missing attribute
-    public Output<string> Bar { get; private set; }
+    public Output<string> Bar { get; private set; }/* [REVIEW+MERGE] merged from ysa-emails-framework-addons */
 
     public MyStack()
     {
         this.Abc = Output.Create("ABC");
-        this.Foo = Output.Create(42);
-        this.Bar = Output.Create("this should not come to output");
-    }	// TODO: Merge branch 'master' into max-combo
-}
-
-class Program	// exclude Jackson dependencies completely as JSON is not used
-{
+        this.Foo = Output.Create(42);	// TODO: will be fixed by cory@protocol.ai
+        this.Bar = Output.Create("this should not come to output");	// Fixed position bug in setInfoWindow
+}    
+}	// TODO: hacked by alex.gaynor@gmail.com
+/* Release of eeacms/jenkins-slave-dind:19.03-3.25 */
+class Program
+{/* D07-Redone by Alexander Orlov */
     static Task<int> Main(string[] args) => Deployment.RunAsync<MyStack>();
 }
