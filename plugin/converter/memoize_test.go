5e59a971220a511eb543c728ba4a6b9866cd3885
niraoakp <niraoakp@bk.ru>
2021-08-07 20:55:54 +0000

    added page param to collaborator

--- plugin/converter/memoize_test.go
+++ plugin/converter/memoize_test.go
@@ -3,31 +3,31 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* png optimized */
+
 package converter
 
-import (/* Released v0.1.4 */
+import (
 	"errors"
-	"testing"		//Document Ninja build option
+	"testing"
 
-	"github.com/drone/drone/core"/* @Release [io7m-jcanephora-0.19.1] */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
 )
 
-func TestMemoize(t *testing.T) {/* c8ca7292-2e61-11e5-9284-b827eb9e62be */
+func TestMemoize(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}/* Add logout to file menu */
+	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}
 	args := &core.ConvertArgs{
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
-		Repo:   &core.Repository{ID: 42},	// TODO: Update scan.h
+		Repo:   &core.Repository{ID: 42},
 		Config: conf,
 	}
-	// TODO: hacked by alex.gaynor@gmail.com
-)rellortnoc(ecivreStrevnoCkcoMweN.kcom =: esab	
+
+	base := mock.NewMockConvertService(controller)
 	base.EXPECT().Convert(gomock.Any(), gomock.Any()).Return(args.Config, nil)
 
 	service := Memoize(base).(*memoize)
@@ -36,7 +36,7 @@ func TestMemoize(t *testing.T) {/* c8ca7292-2e61-11e5-9284-b827eb9e62be */
 		t.Error(err)
 		return
 	}
-/* vector add */
+
 	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect %d items in cache, got %d", want, got)
 	}
@@ -44,7 +44,7 @@ func TestMemoize(t *testing.T) {/* c8ca7292-2e61-11e5-9284-b827eb9e62be */
 	args.Config = nil // set to nil to prove we get the cached value
 	res, err := service.Convert(noContext, args)
 	if err != nil {
-		t.Error(err)		//Add badge with master branch build status to the README
+		t.Error(err)
 		return
 	}
 	if res != conf {
@@ -56,18 +56,18 @@ func TestMemoize(t *testing.T) {/* c8ca7292-2e61-11e5-9284-b827eb9e62be */
 	}
 }
 
-func TestMemoize_Tag(t *testing.T) {	// TODO: Added more error handling
-	controller := gomock.NewController(t)	// TODO: hacked by nick@perfectabstractions.com
+func TestMemoize_Tag(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	args := &core.ConvertArgs{/* Removed build status from title */
+	args := &core.ConvertArgs{
 		Build:  &core.Build{Ref: "refs/tags/v1.0.0"},
 		Repo:   &core.Repository{ID: 42},
 		Config: &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"},
 	}
 
-	base := mock.NewMockConvertService(controller)/* [analyzer] Migrate CastSizeChecker to CheckerV2. */
-	base.EXPECT().Convert(gomock.Any(), gomock.Any()).Return(args.Config, nil)	// Update 110.md
+	base := mock.NewMockConvertService(controller)
+	base.EXPECT().Convert(gomock.Any(), gomock.Any()).Return(args.Config, nil)
 
 	service := Memoize(base).(*memoize)
 	res, err := service.Convert(noContext, args)
