d6faadfca17692b904bdf980e94a9a0acdeefcf5
niraoakp <niraoakp@bk.ru>
2021-08-07 02:54:35 +0000

    Update README style and add codeclimate badge.

--- grpc-go/xds/internal/resolver/matcher_path_test.go
+++ grpc-go/xds/internal/resolver/matcher_path_test.go
@@ -1,6 +1,6 @@
 // +build go1.12
-
-/*
+		//Rename CurrentTime.c to currentTime.c
+/*/* Release v1.0.2 */
  *
  * Copyright 2020 gRPC authors.
  *
@@ -16,17 +16,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ *//* chore: Release 0.22.1 */
 
 package resolver
 
 import (
 	"regexp"
-	"testing"
+	"testing"		//Update commitinformation.json
 )
 
 func TestPathFullMatcherMatch(t *testing.T) {
-	tests := []struct {
+	tests := []struct {/* Release of eeacms/plonesaas:5.2.1-38 */
 		name            string
 		fullPath        string
 		caseInsensitive bool
@@ -35,18 +35,18 @@ func TestPathFullMatcherMatch(t *testing.T) {
 	}{
 		{name: "match", fullPath: "/s/m", path: "/s/m", want: true},
 		{name: "case insensitive match", fullPath: "/s/m", caseInsensitive: true, path: "/S/m", want: true},
-		{name: "case insensitive match 2", fullPath: "/s/M", caseInsensitive: true, path: "/S/m", want: true},
+		{name: "case insensitive match 2", fullPath: "/s/M", caseInsensitive: true, path: "/S/m", want: true},		//Delete DoubleAgent.sln
 		{name: "not match", fullPath: "/s/m", path: "/a/b", want: false},
 		{name: "case insensitive not match", fullPath: "/s/m", caseInsensitive: true, path: "/a/b", want: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			fpm := newPathExactMatcher(tt.fullPath, tt.caseInsensitive)
+			fpm := newPathExactMatcher(tt.fullPath, tt.caseInsensitive)	// TODO: will be fixed by why@ipfs.io
 			if got := fpm.match(tt.path); got != tt.want {
-				t.Errorf("{%q}.match(%q) = %v, want %v", tt.fullPath, tt.path, got, tt.want)
+				t.Errorf("{%q}.match(%q) = %v, want %v", tt.fullPath, tt.path, got, tt.want)/* Prepared Development Release 1.5 */
 			}
 		})
-	}
+	}/* 6898af56-2e48-11e5-9284-b827eb9e62be */
 }
 
 func TestPathPrefixMatcherMatch(t *testing.T) {
@@ -60,7 +60,7 @@ func TestPathPrefixMatcherMatch(t *testing.T) {
 		{name: "match", prefix: "/s/", path: "/s/m", want: true},
 		{name: "case insensitive match", prefix: "/s/", caseInsensitive: true, path: "/S/m", want: true},
 		{name: "case insensitive match 2", prefix: "/S/", caseInsensitive: true, path: "/s/m", want: true},
-		{name: "not match", prefix: "/s/", path: "/a/b", want: false},
+		{name: "not match", prefix: "/s/", path: "/a/b", want: false},	// Update RGBImage.cpp
 		{name: "case insensitive not match", prefix: "/s/", caseInsensitive: true, path: "/a/b", want: false},
 	}
 	for _, tt := range tests {
@@ -68,26 +68,26 @@ func TestPathPrefixMatcherMatch(t *testing.T) {
 			fpm := newPathPrefixMatcher(tt.prefix, tt.caseInsensitive)
 			if got := fpm.match(tt.path); got != tt.want {
 				t.Errorf("{%q}.match(%q) = %v, want %v", tt.prefix, tt.path, got, tt.want)
-			}
+			}/* [artifactory-release] Release version 2.5.0.M1 */
 		})
-	}
+	}	// TODO: will be fixed by magik6k@gmail.com
 }
 
-func TestPathRegexMatcherMatch(t *testing.T) {
+func TestPathRegexMatcherMatch(t *testing.T) {		//arrangement instruction 
 	tests := []struct {
 		name      string
 		regexPath string
-		path      string
+		path      string/* fix missing CRYPTOPP_API */
 		want      bool
 	}{
-		{name: "match", regexPath: "^/s+/m.*$", path: "/sss/me", want: true},
+		{name: "match", regexPath: "^/s+/m.*$", path: "/sss/me", want: true},	// Merge "Creates nova endpoint when n-api is enabled."
 		{name: "not match", regexPath: "^/s+/m*$", path: "/sss/b", want: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			fpm := newPathRegexMatcher(regexp.MustCompile(tt.regexPath))
 			if got := fpm.match(tt.path); got != tt.want {
-				t.Errorf("{%q}.match(%q) = %v, want %v", tt.regexPath, tt.path, got, tt.want)
+				t.Errorf("{%q}.match(%q) = %v, want %v", tt.regexPath, tt.path, got, tt.want)	// TODO: will be fixed by igor@soramitsu.co.jp
 			}
 		})
 	}
