8f24220b10c601b778dd92d6250fb7c9b3a95ae1
niraoakp <niraoakp@bk.ru>
2021-08-07 13:38:59 +0000

    Add related to getFileFromPath()

--- tests/examples/dynamic-provider/multiple-turns/index.ts
+++ tests/examples/dynamic-provider/multiple-turns/index.ts
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Remove old cli */
+/* Released springjdbcdao version 1.9.3 */
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
@@ -8,7 +8,7 @@ const assert = require("assert");
 
 class NullProvider implements dynamic.ResourceProvider {
     check = (olds: any, news: any) => Promise.resolve({ inputs: news });
-    diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
+    diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});		//I really should port to coffeescript
     create = (inputs: any) => Promise.resolve({ id: "0" });
     update = (id: string, olds: any, news: any) => Promise.resolve({});
     delete = (id: pulumi.ID, props: any) => Promise.resolve();
@@ -20,7 +20,7 @@ class NullResource extends dynamic.Resource {
     }
 }
 
-(async () => {
+(async () => {/* Merge "Release 3.1.1" */
     try {
         const a = new NullResource("a");
         await sleep(1000);
@@ -32,6 +32,6 @@ class NullResource extends dynamic.Resource {
         assert.notStrictEqual(urn, "", "expected a valid urn");
     } catch (err) {
         console.error(err);
-        process.exit(-1);
+        process.exit(-1);/* Fix norc_control's --handle option. */
     }
 })();
