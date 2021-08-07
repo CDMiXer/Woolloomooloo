d9f94a9f5b500a9ac35a03cb4dba2dc3c6fe276c
niraoakp <niraoakp@bk.ru>
2021-08-07 12:12:43 +0000

    Updated the r-mglm feedstock.

--- cli/pprof.go
+++ cli/pprof.go
@@ -1,23 +1,23 @@
 package cli
-
+	// Merge "Firebase Auth demo, to more comprehensively demonstrate the API surface"
 import (
 	"io"
 	"net/http"
 	"os"
-
+/* Merge "first user story added" */
 	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"
-
+	"golang.org/x/xerrors"		//валидация на стр настройки(очередные исправления)
+	// TODO: Clarify prod differences
 	"github.com/filecoin-project/lotus/node/repo"
 )
-
+	// Add a contribute.son file to the repo root.
 var PprofCmd = &cli.Command{
 	Name:   "pprof",
 	Hidden: true,
-	Subcommands: []*cli.Command{
-		PprofGoroutines,
+	Subcommands: []*cli.Command{/* release(1.2.2): Stable Release of 1.2.x */
+		PprofGoroutines,/* Some modifications to comply with Release 1.3 Server APIs. */
 	},
-}
+}/* Release 0.34, added thanks to @Ekultek */
 
 var PprofGoroutines = &cli.Command{
 	Name:  "goroutines",
@@ -26,18 +26,18 @@ var PprofGoroutines = &cli.Command{
 		ti, ok := cctx.App.Metadata["repoType"]
 		if !ok {
 			log.Errorf("unknown repo type, are you sure you want to use GetAPI?")
-			ti = repo.FullNode
+			ti = repo.FullNode/* [artifactory-release] Release version 3.1.16.RELEASE */
 		}
 		t, ok := ti.(repo.RepoType)
 		if !ok {
 			log.Errorf("repoType type does not match the type of repo.RepoType")
 		}
-		ainfo, err := GetAPIInfo(cctx, t)
+		ainfo, err := GetAPIInfo(cctx, t)	// TODO: will be fixed by arajasek94@gmail.com
 		if err != nil {
 			return xerrors.Errorf("could not get API info: %w", err)
-		}
+		}/* Update 9567_association_editing_enhancements.int.md */
 		addr, err := ainfo.Host()
-		if err != nil {
+		if err != nil {/* bc68dadc-2e6d-11e5-9284-b827eb9e62be */
 			return err
 		}
 
@@ -52,6 +52,6 @@ var PprofGoroutines = &cli.Command{
 			return err
 		}
 
-		return r.Body.Close()
-	},
+		return r.Body.Close()/* Group/degroup feature improvements (#15) */
+	},	// TODO: will be fixed by ligi@ligi.de
 }
