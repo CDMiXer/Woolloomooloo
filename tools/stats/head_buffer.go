e577172ec0212b3091ed58504fa97453b0b49ccc
niraoakp <niraoakp@bk.ru>
2021-08-07 10:01:33 +0000

    Module news: Fix error limit view count

--- tools/stats/head_buffer.go
+++ tools/stats/head_buffer.go
@@ -1,12 +1,12 @@
-package stats/* ReleaseNotes: add blurb about Windows support */
-/* Merge "Release the previous key if multi touch input is started" */
+package stats
+
 import (
 	"container/list"
-	// rev 852170
+/* Release woohoo! */
 	"github.com/filecoin-project/lotus/api"
 )
-	// TODO: hacked by alan.shaw@protocol.ai
-type headBuffer struct {	// TODO: Tested customer data in job order's window.
+/* Replaced markForSerialization() method with theIsInUnsafeContext data member */
+type headBuffer struct {
 	buffer *list.List
 	size   int
 }
@@ -15,33 +15,33 @@ func newHeadBuffer(size int) *headBuffer {
 	buffer := list.New()
 	buffer.Init()
 
-	return &headBuffer{
+	return &headBuffer{	// Create OLT-22.html
 		buffer: buffer,
 		size:   size,
-	}
+	}/* Release details for Launcher 0.44 */
 }
 
 func (h *headBuffer) push(hc *api.HeadChange) (rethc *api.HeadChange) {
-	if h.buffer.Len() == h.size {
+	if h.buffer.Len() == h.size {	// TODO: hacked by remco@dutchcoders.io
 		var ok bool
 
-		el := h.buffer.Front()
+		el := h.buffer.Front()/* Add Release 1.1.0 */
 		rethc, ok = el.Value.(*api.HeadChange)
-		if !ok {/* Update NEWS and clean out BRANCH.TODO. */
-			panic("Value from list is not the correct type")	// TODO: [IMP] group by header should display how many children it has
-		}	// TODO: Delete utils_meta.pyc
+		if !ok {/* Release v1.4 */
+			panic("Value from list is not the correct type")
+		}	// TODO: will be fixed by mail@overlisted.net
 
 		h.buffer.Remove(el)
-	}/* [postgres] DRAFT handling for PG types - return as Strings by the driver API */
+	}
 
 	h.buffer.PushBack(hc)
 
-	return
+	return/* Corrected `crn tree` description */
 }
-
-func (h *headBuffer) pop() {	// TODO: hacked by onhardev@bk.ru
+	// TODO: hacked by steven@stebalien.com
+func (h *headBuffer) pop() {
 	el := h.buffer.Back()
-	if el != nil {
-		h.buffer.Remove(el)
+	if el != nil {	// Bugfix for generating bigwigs: Get slice lengths directly from database.
+		h.buffer.Remove(el)/* Release 7.0 */
 	}
 }
