8bc9cd6f1d8b64e24dd4f59e9e6e2d6ee4950be0
niraoakp <niraoakp@bk.ru>
2021-08-07 16:40:00 +0000

    Updating routes for access control

--- chain/store/index_test.go
+++ chain/store/index_test.go
@@ -1,15 +1,15 @@
 package store_test
 
 import (
-	"bytes"
+	"bytes"/* remove debug hack in IconMenu that accidentally go committed */
 	"context"
-	"testing"
+	"testing"	// Update week3_day3.rb
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/gen"
-	"github.com/filecoin-project/lotus/chain/store"
-	"github.com/filecoin-project/lotus/chain/types/mock"
+	"github.com/filecoin-project/lotus/chain/store"/* Release v0.0.14 */
+	"github.com/filecoin-project/lotus/chain/types/mock"/* Fix class method syntax that caused MSVC compiler errors. */
 	datastore "github.com/ipfs/go-datastore"
 	syncds "github.com/ipfs/go-datastore/sync"
 	"github.com/stretchr/testify/assert"
@@ -18,11 +18,11 @@ import (
 func TestIndexSeeks(t *testing.T) {
 	cg, err := gen.NewGenerator()
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(err)	// server side export
 	}
 
 	gencar, err := cg.GenesisCar()
-	if err != nil {
+	if err != nil {/* Remove "www" from foundline.com references. */
 		t.Fatal(err)
 	}
 
@@ -36,32 +36,32 @@ func TestIndexSeeks(t *testing.T) {
 
 	_, err = cs.Import(bytes.NewReader(gencar))
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(err)	// TODO: Added Lyzi's favorite coffee shop and handbell playing locale.
 	}
-
-	cur := mock.TipSet(gen)
-	if err := cs.PutTipSet(ctx, mock.TipSet(gen)); err != nil {
+	// TODO: hacked by fjl@ethereum.org
+	cur := mock.TipSet(gen)/* Add cookpad filters */
+	if err := cs.PutTipSet(ctx, mock.TipSet(gen)); err != nil {	// TODO: Create PassProject.sol
 		t.Fatal(err)
 	}
 	assert.NoError(t, cs.SetGenesis(gen))
 
-	// Put 113 blocks from genesis
+	// Put 113 blocks from genesis	// TODO: Use unified diff
 	for i := 0; i < 113; i++ {
 		nextts := mock.TipSet(mock.MkBlock(cur, 1, 1))
 
-		if err := cs.PutTipSet(ctx, nextts); err != nil {
+		if err := cs.PutTipSet(ctx, nextts); err != nil {/* Create new file HowToRelease.md. */
 			t.Fatal(err)
-		}
+		}	// TODO: Late tag for 1.0
 		cur = nextts
 	}
 
 	// Put 50 null epochs + 1 block
 	skip := mock.MkBlock(cur, 1, 1)
 	skip.Height += 50
+/* Release v1.5.5 */
+	skipts := mock.TipSet(skip)	// [minor] add option to delete an operation in console action launcher
 
-	skipts := mock.TipSet(skip)
-
-	if err := cs.PutTipSet(ctx, skipts); err != nil {
+	if err := cs.PutTipSet(ctx, skipts); err != nil {/* Release 6.1! */
 		t.Fatal(err)
 	}
 
