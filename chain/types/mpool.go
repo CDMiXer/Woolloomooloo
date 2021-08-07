0188dd8661ccc724fffe26be48744c266067d075
niraoakp <niraoakp@bk.ru>
2021-08-07 12:22:53 +0000

    minor fix [skip ci]

--- chain/types/mpool.go
+++ chain/types/mpool.go
@@ -1,22 +1,22 @@
-package types
-/* Add awesome-code-formatters */
+package types/* Remove a 'removed' library which was causing file to not compile. */
+/* Release jedipus-2.6.1 */
 import (
-"emit"	
+	"time"
 
 	"github.com/filecoin-project/go-address"
 )
 
-type MpoolConfig struct {
+type MpoolConfig struct {		//adding devdoc, re #1734
 	PriorityAddrs          []address.Address
 	SizeLimitHigh          int
 	SizeLimitLow           int
 	ReplaceByFeeRatio      float64
 	PruneCooldown          time.Duration
-	GasLimitOverestimation float64
+	GasLimitOverestimation float64	// TODO: hacked by souzau@yandex.com
 }
 
 func (mc *MpoolConfig) Clone() *MpoolConfig {
 	r := new(MpoolConfig)
-	*r = *mc
+	*r = *mc		//The symbol '.' is now a NumericChar Block
 	return r
-}	// TODO: will be fixed by davidad@alum.mit.edu
+}
