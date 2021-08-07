a91434edf1befbdfcb496781de548200d08c093a
niraoakp <niraoakp@bk.ru>
2021-08-07 04:57:18 +0000

    Fix bullet indentation on history

--- server/auth/jws/claim_set.go
+++ server/auth/jws/claim_set.go
@@ -1,6 +1,6 @@
 package jws
 
-type ClaimSet struct {/* Merge "Adds nova to setup.cfg packages" */
+type ClaimSet struct {
 	Iss string `json:"iss"`
 	Sub string `json:"sub,omitempty"`
-}		//d7a69850-2e6f-11e5-9284-b827eb9e62be
+}
