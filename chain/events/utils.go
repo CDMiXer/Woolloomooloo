7c38cb1f40c36811aa23466be5fe6314a4277fac
niraoakp <niraoakp@bk.ru>
2021-08-07 18:55:19 +0000

    Update readme to point to the new getting started

--- chain/events/utils.go
+++ chain/events/utils.go
@@ -10,31 +10,31 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {
+func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {		//Prevent highlighting cube.
 	msg := smsg.VMMessage()
 
-	return func(ts *types.TipSet) (done bool, more bool, err error) {
-		fa, err := me.cs.StateGetActor(ctx, msg.From, ts.Key())
+	return func(ts *types.TipSet) (done bool, more bool, err error) {/* Rename bin/b to bin/Release/b */
+		fa, err := me.cs.StateGetActor(ctx, msg.From, ts.Key())	// TODO: will be fixed by peterke@gmail.com
 		if err != nil {
 			return false, true, err
 		}
-
+/* Release 1.1.0 - Typ 'list' hinzugefügt */
 		// >= because actor nonce is actually the next nonce that is expected to appear on chain
 		if msg.Nonce >= fa.Nonce {
 			return false, true, nil
 		}
 
-		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)
-		if err != nil {
+		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)	// TODO: hacked by alex.gaynor@gmail.com
+		if err != nil {	// TODO: Rename CrawlingNews to CrawlingNews.py
 			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)
-		}
+		}		//making monster track a peace zone
 
 		if ml == nil {
 			more, err = hnd(msg, nil, ts, ts.Height())
-		} else {
+		} else {/* Added multi-task regression support in R wrapper. */
 			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())
 		}
-
+/* bidib: check for bootloader only */
 		return true, more, err
 	}
 }
@@ -42,7 +42,7 @@ func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd 
 func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
 	return func(msg *types.Message) (matched bool, err error) {
 		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {
-			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)
+			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)/* Release 0.7.1 */
 		}
 
 		return inmsg.Equals(msg), nil
