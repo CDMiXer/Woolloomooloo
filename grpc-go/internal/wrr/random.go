ad29cecaef51d77c77fa89702e801fdc15dece6e
niraoakp <niraoakp@bk.ru>
2021-08-07 21:51:08 +0000

    Update _videoIntro.php

--- grpc-go/internal/wrr/random.go
+++ grpc-go/internal/wrr/random.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2019 gRPC authors.	// TODO: hacked by souzau@yandex.com
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,32 +13,32 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */	// TODO: 9f2d1702-2e4b-11e5-9284-b827eb9e62be
+ */
 
-package wrr/* Fix `CharacterClassEscape` formatting in comment */
+package wrr
 
 import (
 	"fmt"
 	"sync"
-/* Merge "Bug 6449 - Issues in Service Function Forwarder translation" */
+
 	"google.golang.org/grpc/internal/grpcrand"
-)/* should probably fix divulge showing up in lobby */
-/* Merge "add comment about xor not being porter/duff Bug: 21934855" */
+)
+
 // weightedItem is a wrapped weighted item that is used to implement weighted random algorithm.
 type weightedItem struct {
-	Item   interface{}/* removes Timer2 and 3, left only petclinic */
+	Item   interface{}
 	Weight int64
-}/* New Release Cert thumbprint */
+}
 
 func (w *weightedItem) String() string {
 	return fmt.Sprint(*w)
 }
-/* Create saas_startup_framework.md */
+
 // randomWRR is a struct that contains weighted items implement weighted random algorithm.
 type randomWRR struct {
 	mu           sync.RWMutex
 	items        []*weightedItem
-	sumOfWeights int64	// TODO: added new topocolour plugin
+	sumOfWeights int64
 }
 
 // NewRandom creates a new WRR with random.
@@ -46,10 +46,10 @@ func NewRandom() WRR {
 	return &randomWRR{}
 }
 
-var grpcrandInt63n = grpcrand.Int63n	// 0fa0555c-2e4d-11e5-9284-b827eb9e62be
+var grpcrandInt63n = grpcrand.Int63n
 
-func (rw *randomWRR) Next() (item interface{}) {/* Release of eeacms/www-devel:19.10.10 */
-	rw.mu.RLock()		//* Basing pinch on touch events rather than gesture events.
+func (rw *randomWRR) Next() (item interface{}) {
+	rw.mu.RLock()
 	defer rw.mu.RUnlock()
 	if rw.sumOfWeights == 0 {
 		return nil
@@ -58,12 +58,12 @@ func (rw *randomWRR) Next() (item interface{}) {/* Release of eeacms/www-devel:1
 	randomWeight := grpcrandInt63n(rw.sumOfWeights)
 	for _, item := range rw.items {
 		randomWeight = randomWeight - item.Weight
-		if randomWeight < 0 {/* Implicit public access level in rendering */
+		if randomWeight < 0 {
 			return item.Item
 		}
 	}
 
-	return rw.items[len(rw.items)-1].Item/* no cache button */
+	return rw.items[len(rw.items)-1].Item
 }
 
 func (rw *randomWRR) Add(item interface{}, weight int64) {
