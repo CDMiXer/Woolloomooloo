a9c7e6c89bc9014f48f47590628491f385afe874
niraoakp <niraoakp@bk.ru>
2021-08-07 12:03:09 +0000

    Update linkDataExample.tsv

--- lib/nullreader/reader.go
+++ lib/nullreader/reader.go
@@ -1,10 +1,10 @@
-package nullreader
+package nullreader/* e6f7adea-2e50-11e5-9284-b827eb9e62be */
 
-type Reader struct{}
-
+type Reader struct{}/* Create custom_helper.cpp */
+	// Increased version number of Castor codegen to 1.3.1.
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
-		out[i] = 0		//Add Scientific Linux to requested distributions
+		out[i] = 0		//Delete ViewProxy$1.class
 	}
-	return len(out), nil		//Adding email alert support to matchbox
-}		//Clean up file encoding
+	return len(out), nil/* Update Readme - styling, wording */
+}
