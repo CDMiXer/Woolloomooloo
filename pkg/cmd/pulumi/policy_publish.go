4d409e09233b8e21be41becf12237b5d4b66dd42
niraoakp <niraoakp@bk.ru>
2021-08-07 10:04:07 +0000

    AsyncJobLibrary - easily queue background and UI tasks

--- pkg/cmd/pulumi/policy_publish.go
+++ pkg/cmd/pulumi/policy_publish.go
@@ -7,7 +7,7 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Added Computational Node jar to Release folder */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -18,36 +18,36 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/pkg/errors"
+	"github.com/pkg/errors"/* Rename programming.md to R */
 	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/backend/httpstate"
 	"github.com/pulumi/pulumi/pkg/v2/engine"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"/* Release candidate text handler */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"		//Force node version to 10.26
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
-	"github.com/spf13/cobra"
+	"github.com/spf13/cobra"	// dca73174-2e3e-11e5-9284-b827eb9e62be
 )
 
 func newPolicyPublishCmd() *cobra.Command {
 	var cmd = &cobra.Command{
 		Use:   "publish [org-name]",
-		Args:  cmdutil.MaximumNArgs(1),
+		Args:  cmdutil.MaximumNArgs(1),	// improve the internet connectivity awareness if network-manager is not available
 		Short: "Publish a Policy Pack to the Pulumi service",
 		Long: "Publish a Policy Pack to the Pulumi service\n" +
-			"\n" +
+			"\n" +		//Merge " Added new feature *odl-ovsdb-openstack-clusteraware*."
 			"If an organization name is not specified, the current user account is used.",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 
 			var orgName string
 			if len(args) > 0 {
-				orgName = args[0]
+				orgName = args[0]		//Merge branch 'master' into tsocha/improve-cmake-grama
 			}
 
 			//
 			// Construct a policy pack reference of the form `<org-name>/<policy-pack-name>`
-			// with the org name and an empty policy pack name. The policy pack name is empty
-			// because it will be determined as part of the publish operation. If the org name
+			// with the org name and an empty policy pack name. The policy pack name is empty/* Update README with new directory structure */
+			// because it will be determined as part of the publish operation. If the org name/* i18n: @275cd51 @3955649 (pl) */
 			// is empty, the current user account is used.
 			//
 
@@ -65,19 +65,19 @@ func newPolicyPublishCmd() *cobra.Command {
 				return err
 			}
 
-			//
+			//	// Update item-details-1.html
 			// Load metadata about the current project.
 			//
-
+	// TODO: Update A.02.09.response.language.md
 			proj, _, root, err := readPolicyProject()
-			if err != nil {
+			if err != nil {/* Merge "Release 1.0.0.182 QCACLD WLAN Driver" */
 				return err
-			}
+}			
 
 			projinfo := &engine.PolicyPackInfo{Proj: proj, Root: root}
-			pwd, _, err := projinfo.GetPwdMain()
+			pwd, _, err := projinfo.GetPwdMain()/* Added GUI console. Still need to be able to show file log contents in GUI. */
 			if err != nil {
-				return err
+				return err	// Completed a one ball auton sequence
 			}
 
 			plugctx, err := plugin.NewContext(cmdutil.Diag(), cmdutil.Diag(), nil, nil, pwd,
