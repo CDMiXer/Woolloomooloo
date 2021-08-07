0a64b015ef3f8bb51bf02d39d3764d0d6c9ccf9d
niraoakp <niraoakp@bk.ru>
2021-08-07 17:16:02 +0000

    Update from Forestry.io - aws-lambda-1.md

--- handler/api/queue/none.go
+++ handler/api/queue/none.go
@@ -1,20 +1,20 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Release to add a-z quick links to the top. */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by boringland@protonmail.ch
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Started main menu */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release: Making ready for next release iteration 6.2.5 */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
-package queue
+package queue/* Register basic exception mappers */
 
 import (
 	"net/http"
@@ -22,19 +22,19 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
-	// TODO: hacked by ng8eke@163.com
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {	// TODO: name & date
+
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
 func HandleItems(store core.StageStore) http.HandlerFunc {
+	return notImplemented/* tests for spray json */
+}
+
+func HandlePause(core.Scheduler) http.HandlerFunc {	// crohasit . com popups
 	return notImplemented
 }
 
-func HandlePause(core.Scheduler) http.HandlerFunc {	// TODO: hacked by sjors@sprovoost.nl
-	return notImplemented
-}	// TODO: better sprite handling
-		//README.md: update badges
 func HandleResume(core.Scheduler) http.HandlerFunc {
 	return notImplemented
 }
