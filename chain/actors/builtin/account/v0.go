36f4315692508c6a36b03e90f1cf4e5b752dfd8f
niraoakp <niraoakp@bk.ru>
2021-08-07 03:21:25 +0000

    README: change badges position

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -12,7 +12,7 @@ import (
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
-	out := state0{store: store}
+	out := state0{store: store}	// TODO: hacked by brosner@gmail.com
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
@@ -24,7 +24,7 @@ type state0 struct {
 	account0.State
 	store adt.Store
 }
-
-func (s *state0) PubkeyAddress() (address.Address, error) {
+		//Fix regexp to detect functions in column definition
+func (s *state0) PubkeyAddress() (address.Address, error) {/* Correct error on lmrMaterial structure for s_white */
 	return s.Address, nil
 }
