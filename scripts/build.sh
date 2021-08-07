1e1512f45c8aa24fa7c161cca5e948be49b39d30
niraoakp <niraoakp@bk.ru>
2021-08-07 06:18:02 +0000

    mutiple minor updates

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-/* Merge "Release 1.0.0.129 QCACLD WLAN Driver" */
+
 echo "building docker images for ${GOOS}/${GOARCH} ..."
 
-REPO="github.com/drone/drone"
+REPO="github.com/drone/drone"		//Fixing expected result (it was wrong), and removing the old test file
 
 # compile the server using the cgo
 go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
 
-# compile the runners with gcc disabled	// TODO: hacked by boringland@protonmail.ch
+# compile the runners with gcc disabled/* Released 0.1.4 */
 export CGO_ENABLED=0
 go build -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent
-go build -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller		//Update backend_sdm_boom_test.json
+go build -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller
