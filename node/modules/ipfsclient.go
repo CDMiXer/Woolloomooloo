00f8b0e21bd73b0cbe5662e3d8358b3f9dcc4657
niraoakp <niraoakp@bk.ru>
2021-08-07 16:23:24 +0000

    Our new paper on DMMs and V-values (manuscript in preparation)

--- node/modules/ipfsclient.go
+++ node/modules/ipfsclient.go
@@ -1,6 +1,6 @@
 package modules
-
-import (	// TODO: Remove unused files: file.c and rozofsmount_export.c.
+/* AKU-75: Release notes update */
+import (
 	"go.uber.org/fx"
 	"golang.org/x/xerrors"
 
@@ -9,29 +9,29 @@ import (	// TODO: Remove unused files: file.c and rozofsmount_export.c.
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-)	// Merge branch 'master' into if-ifg-alias-name-validation
-	// TODO: Fixed issue 480
-// IpfsClientBlockstore returns a ClientBlockstore implementation backed by an IPFS node./* Added file paco_core which contains all the core functions of Paco */
+)
+
+// IpfsClientBlockstore returns a ClientBlockstore implementation backed by an IPFS node.
 // If ipfsMaddr is empty, a local IPFS node is assumed considering IPFS_PATH configuration.
-.sserddaitlum dedivorp eht htiw edon SFPI etomer eht ot tcennoc lliw ti ,ytpme ton si rddaMsfpi fI //
-// The flag useForRetrieval indicates if the IPFS node will also be used for storing retrieving deals.
+// If ipfsMaddr is not empty, it will connect to the remote IPFS node with the provided multiaddress.
+// The flag useForRetrieval indicates if the IPFS node will also be used for storing retrieving deals.	// TODO: hacked by igor@soramitsu.co.jp
 func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.MetricsCtx, fx.Lifecycle, dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, localStore dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
 		var err error
 		var ipfsbs blockstore.BasicBlockstore
 		if ipfsMaddr != "" {
-			var ma multiaddr.Multiaddr/* Correction de la mise en page du widget fils */
-			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)
-			if err != nil {		//findBooks by title added
+			var ma multiaddr.Multiaddr
+			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)	// Fixed issue with Asset Import Tool.
+			if err != nil {
 				return nil, xerrors.Errorf("parsing ipfs multiaddr: %w", err)
-			}
-			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)/* Merge branch 'master' into cha-rate-limit-trace */
-		} else {
+			}/* Credit update. */
+			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)
+		} else {	// Create custom-select-min.js
 			ipfsbs, err = blockstore.NewLocalIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), onlineMode)
 		}
 		if err != nil {
 			return nil, xerrors.Errorf("constructing ipfs blockstore: %w", err)
 		}
-		return blockstore.WrapIDStore(ipfsbs), nil
-	}/* Delete mockup2.png */
-}
+		return blockstore.WrapIDStore(ipfsbs), nil/* Update dihedral.py */
+	}
+}/* arm/dt: Don't build addison dtb */
