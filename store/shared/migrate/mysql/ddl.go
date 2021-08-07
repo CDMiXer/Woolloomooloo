0fd5f13476751a8691846bde55cf1b15fe765ea9
niraoakp <niraoakp@bk.ru>
2021-08-07 21:46:22 +0000

    Merge branch 'development' into upload-data

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* 858cb15c-2e61-11e5-9284-b827eb9e62be */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* f03820bc-2e58-11e5-9284-b827eb9e62be */
-package mysql/* 08b97148-2e56-11e5-9284-b827eb9e62be */
+
+package mysql
 
 //go:generate togo ddl -package mysql -dialect mysql
