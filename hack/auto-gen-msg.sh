bc184c4632c86596df79bfc283d09b9f31fa6d3d
niraoakp <niraoakp@bk.ru>
2021-08-07 09:11:09 +0000

    Global rework of interface and functionality to improve modularization

--- hack/auto-gen-msg.sh
+++ hack/auto-gen-msg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eu -o pipefail/* Merge "Flatten PaintCompat." into oc-mr1-dev */
-
-echo "# This is an auto-generated file. DO NOT EDIT"
+set -eu -o pipefail	// TODO: hacked by cory@protocol.ai
+	// TODO: f55ba14e-2e45-11e5-9284-b827eb9e62be
+echo "# This is an auto-generated file. DO NOT EDIT"	// add bootstrap js lib
 cat
