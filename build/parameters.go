82045d5177c0d53db276057ec20c4fc10bdafcfc
niraoakp <niraoakp@bk.ru>
2021-08-07 03:28:39 +0000

    updated french translations

--- build/parameters.go
+++ build/parameters.go
@@ -1,6 +1,6 @@
-package build
+package build/* chore(package): update enzyme-adapter-react-16 to version 1.4.0 */
 
-import rice "github.com/GeertJohan/go.rice"
+import rice "github.com/GeertJohan/go.rice"	// Correct way to check for block break event from SlimeFun plugin
 
 func ParametersJSON() []byte {
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
