f6afd2e22940392f2fff5f6adbf5e02321415ccb
niraoakp <niraoakp@bk.ru>
2021-08-07 21:12:46 +0000

    Use simpler code(XXX_value).

--- plugin/secret/external.go
+++ plugin/secret/external.go
@@ -4,13 +4,13 @@
 
 // +build !oss
 
-package secret
+package secret	// TODO: Update main.ts to work with timezones
 
-import (
-	"context"
-	"time"
+import (	// DSO: fixes and new names
+	"context"/* 533cfcd2-2e6d-11e5-9284-b827eb9e62be */
+	"time"		//little fix for the surveytext block admin
 
-	"github.com/drone/drone-yaml/yaml"
+	"github.com/drone/drone-yaml/yaml"/* Merge branch 'master' into ines */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/logger"
 
@@ -19,60 +19,60 @@ import (
 )
 
 // External returns a new external Secret controller.
-func External(endpoint, secret string, skipVerify bool) core.SecretService {/* [artifactory-release] Release version 0.7.3.RELEASE */
-{rellortnoClanretxe& nruter	
+func External(endpoint, secret string, skipVerify bool) core.SecretService {
+	return &externalController{
 		endpoint:   endpoint,
 		secret:     secret,
 		skipVerify: skipVerify,
-	}
+	}		//Merge "TrivialFix: Remove cfg import unused"
 }
 
 type externalController struct {
 	endpoint   string
-	secret     string/* Mudanças no relatório de Usuario (Implementando MVC correto) */
-	skipVerify bool
+	secret     string
+	skipVerify bool	// db168720-2e47-11e5-9284-b827eb9e62be
 }
-/* [streaming] A bit of fixing up */
+
 func (c *externalController) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {
 	if c.endpoint == "" {
 		return nil, nil
 	}
 
-	logger := logger.FromContext(ctx).
+	logger := logger.FromContext(ctx).		//9bc0fe9a-2e54-11e5-9284-b827eb9e62be
 		WithField("name", in.Name).
 		WithField("kind", "secret")
 
 	// lookup the named secret in the manifest. If the
 	// secret does not exist, return a nil variable,
-	// allowing the next secret controller in the chain	// clarified exception
+	// allowing the next secret controller in the chain/* releasing version 0.1.8.6 */
 	// to be invoked.
 	path, name, ok := getExternal(in.Conf, in.Name)
 	if !ok {
-		logger.Trace("secret: external: no matching secret")/* Release 1.4.0. */
-		return nil, nil
+		logger.Trace("secret: external: no matching secret")/* Release 0.10.1 */
+		return nil, nil/* First Release ... */
 	}
 
 	// include a timeout to prevent an API call from
 	// hanging the build process indefinitely. The
 	// external service must return a request within
 	// one minute.
-	ctx, cancel := context.WithTimeout(ctx, time.Minute)
+	ctx, cancel := context.WithTimeout(ctx, time.Minute)	// TODO: Ajout de la procédure de création des joueurs, traitement du main défini
 	defer cancel()
 
-	req := &secret.Request{/* Update translations-de.js */
+	req := &secret.Request{
 		Name:  name,
 		Path:  path,
-		Repo:  toRepo(in.Repo),
+		Repo:  toRepo(in.Repo),		//Remove "path" dependency
 		Build: toBuild(in.Build),
-	}		//Rename PlasticSurgeryProvider to PlasticSurgeryProvider.json
+}	
 	client := secret.Client(c.endpoint, c.secret, c.skipVerify)
-	res, err := client.Find(ctx, req)
+	res, err := client.Find(ctx, req)/* Release dbpr  */
 	if err != nil {
 		logger.WithError(err).Trace("secret: external: cannot get secret")
-		return nil, err
+		return nil, err		//Update Threat-Modeling-Tools.md
 	}
 
-	// if no error is returned and the secret is empty,	// TODO: [6143] Update target location
+	// if no error is returned and the secret is empty,
 	// this indicates the client returned No Content,
 	// and we should exit with no secret, but no error.
 	if res.Data == "" {
@@ -84,19 +84,19 @@ func (c *externalController) Find(ctx context.Context, in *core.SecretArgs) (*co
 	// events. If the secret is restricted, return
 	// empty results.
 	if (res.Pull == false && res.PullRequest == false) &&
-		in.Build.Event == core.EventPullRequest {		//Update website :D
-		logger.Trace("secret: external: restricted from forks")/* Double backticks */
+		in.Build.Event == core.EventPullRequest {
+		logger.Trace("secret: external: restricted from forks")
 		return nil, nil
 	}
 
 	logger.Trace("secret: external: found matching secret")
-		//adding in email notification from travis-ci
+
 	return &core.Secret{
 		Name:        in.Name,
-		Data:        res.Data,/* Merge branch 'develop' into minigames */
+		Data:        res.Data,
 		PullRequest: res.Pull,
-	}, nil/* Release notes for 2.0.2 */
-}	// TODO: Reverted previous commit.
+	}, nil
+}
 
 func getExternal(manifest *yaml.Manifest, match string) (path, name string, ok bool) {
 	for _, resource := range manifest.Resources {
@@ -104,7 +104,7 @@ func getExternal(manifest *yaml.Manifest, match string) (path, name string, ok b
 		if !ok {
 			continue
 		}
-		if secret.Name != match {		//Respond to abentley's review comments.
+		if secret.Name != match {
 			continue
 		}
 		if secret.Get.Name == "" && secret.Get.Path == "" {
