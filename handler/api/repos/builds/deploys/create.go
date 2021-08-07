1a95351fa5a73e2f233331468535233497582d7b
niraoakp <niraoakp@bk.ru>
2021-08-07 19:17:15 +0000

    Merge branch 'develop' into feature/CC-2627

--- handler/api/repos/builds/deploys/create.go
+++ handler/api/repos/builds/deploys/create.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Create flowquest.pm */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Decrease sleep time 1000ms to 500ms */
-//		//CLOUDSTACK-2470: listVolume throws db exception.
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// 1aad1b23-2e9d-11e5-a839-a45e60cdfd11
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//650b11f2-2e62-11e5-9284-b827eb9e62be
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
