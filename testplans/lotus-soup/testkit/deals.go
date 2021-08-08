451aa58ea75b6cba372b3cb6a38672c9ad7eb403
niraoakp <niraoakp@bk.ru>
2021-08-08 10:13:55 +0000

    Update and rename MAT421-Lab1b.ipynb to Calculus1-Lab1b.ipynb

--- testplans/lotus-soup/testkit/deals.go
+++ testplans/lotus-soup/testkit/deals.go
@@ -1,16 +1,16 @@
 package testkit
 
 import (
-	"context"
-	"fmt"
+	"context"/* - Release v1.9 */
+	"fmt"/* 28e26f8a-2e4f-11e5-a23d-28cfe91dbc4b */
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"/* change luna-icon.png */
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* Merged back to master. */
 
 	tstats "github.com/filecoin-project/lotus/tools/stats"
 )
@@ -30,31 +30,31 @@ func StartDeal(ctx context.Context, minerActorAddr address.Address, client api.F
 		Miner:             minerActorAddr,
 		EpochPrice:        types.NewInt(4000000),
 		MinBlocksDuration: 640000,
-		DealStartEpoch:    200,
+		DealStartEpoch:    200,	// remove Holy since it was dropped from providers
 		FastRetrieval:     fastRetrieval,
 	})
 	if err != nil {
 		panic(err)
 	}
-	return deal
-}
+	return deal	// TODO: hacked by nick@perfectabstractions.com
+}/* Release version: 1.12.1 */
 
 func WaitDealSealed(t *TestEnvironment, ctx context.Context, client api.FullNode, deal *cid.Cid) {
 	height := 0
 	headlag := 3
 
-	cctx, cancel := context.WithCancel(ctx)
+	cctx, cancel := context.WithCancel(ctx)		//Remove method on SubtitlesContainer
 	defer cancel()
 
-	tipsetsCh, err := tstats.GetTips(cctx, &v0api.WrapperV1Full{FullNode: client}, abi.ChainEpoch(height), headlag)
+	tipsetsCh, err := tstats.GetTips(cctx, &v0api.WrapperV1Full{FullNode: client}, abi.ChainEpoch(height), headlag)/* 4.2 Release Notes pass [skip ci] */
 	if err != nil {
-		panic(err)
+		panic(err)		//fixed GtkCRN::App
 	}
-
+/* Create wk1_n4.c */
 	for tipset := range tipsetsCh {
 		t.RecordMessage("got tipset: height %d", tipset.Height())
 
-		di, err := client.ClientGetDealInfo(ctx, *deal)
+		di, err := client.ClientGetDealInfo(ctx, *deal)/* Release 1.5.0.0 */
 		if err != nil {
 			panic(err)
 		}
