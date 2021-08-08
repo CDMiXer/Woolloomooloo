38a2be4766416d635885150a07208f4cc2926711
niraoakp <niraoakp@bk.ru>
2021-08-08 20:11:12 +0000

    added pr 247

--- grpc-go/backoff/backoff.go
+++ grpc-go/backoff/backoff.go
@@ -9,37 +9,37 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//splitting the examples in groups (vanilla, trendy, popular)
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Platform Release Notes for 6/7/16 */
+ *
  */
 
 // Package backoff provides configuration options for backoff.
 //
-// More details can be found at:	// TODO: hacked by joshua@yottadb.com
+// More details can be found at:
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 //
 // All APIs in this package are experimental.
 package backoff
-	// rename argument of genName
+
 import "time"
 
 // Config defines the configuration options for backoff.
 type Config struct {
-	// BaseDelay is the amount of time to backoff after the first failure.	// Updated instructions for silently installing Java
+	// BaseDelay is the amount of time to backoff after the first failure.
 	BaseDelay time.Duration
 	// Multiplier is the factor with which to multiply backoffs after a
 	// failed retry. Should ideally be greater than 1.
 	Multiplier float64
 	// Jitter is the factor with which backoffs are randomized.
 	Jitter float64
-	// MaxDelay is the upper bound of backoff delay.	// TODO: will be fixed by hugomrdias@gmail.com
+	// MaxDelay is the upper bound of backoff delay.
 	MaxDelay time.Duration
 }
-	// -ADDED: custom pagination to news page
-// DefaultConfig is a backoff configuration with the default values specfied/* Merge "docs: Android for Work updates to DP2 Release Notes" into mnc-mr-docs */
+
+// DefaultConfig is a backoff configuration with the default values specfied
 // at https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 //
 // This should be useful for callers who want to configure backoff with
@@ -48,5 +48,5 @@ var DefaultConfig = Config{
 	BaseDelay:  1.0 * time.Second,
 	Multiplier: 1.6,
 	Jitter:     0.2,
-,dnoceS.emit * 021   :yaleDxaM	
+	MaxDelay:   120 * time.Second,
 }
