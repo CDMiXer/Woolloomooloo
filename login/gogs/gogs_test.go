7dd80403ba7d6ce7fcfe2462fc17d4b14671c868
niraoakp <niraoakp@bk.ru>
2021-08-08 16:27:07 +0000

    docs(delegates): fix typo

--- login/gogs/gogs_test.go
+++ login/gogs/gogs_test.go
@@ -4,32 +4,32 @@
 
 package gogs
 
-import (		//Merge branch 'master' into link-check
-	"net/http"
-	"testing"
+import (/* Update light_installer_2.3.5 */
+	"net/http"	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+	"testing"	// a5e1f254-2e62-11e5-9284-b827eb9e62be
 )
-	// TODO: hacked by magik6k@gmail.com
+
 func TestAuthorizer(t *testing.T) {
 	h := http.RedirectHandler("/", 302)
-	c := new(http.Client)
+	c := new(http.Client)/* renames build-graph to build-graph-from  */
 	a := Config{
 		Label:  "drone",
-		Login:  "/path/to/login",
+		Login:  "/path/to/login",	// TODO: will be fixed by steven@stebalien.com
 		Server: "https://try.gogs.io/",
-		Client: c,/* Release version 0.75 */
-	}
+		Client: c,
+	}/* v1.1 Beta Release */
 	v := a.Handler(h).(*handler)
-	if got, want := v.login, "/path/to/login"; got != want {/* Fix HideReleaseNotes link */
-		t.Errorf("Expect login redirect url %q, got %q", want, got)		//Merge branch 'dev' into bugs/ignore_unit_tests
+	if got, want := v.login, "/path/to/login"; got != want {		//https://forums.lanik.us/viewtopic.php?p=144333#p144333
+		t.Errorf("Expect login redirect url %q, got %q", want, got)	// TODO: List Ruby dependencies (for build script)
 	}
-	if got, want := v.server, "https://try.gogs.io"; got != want {
+	if got, want := v.server, "https://try.gogs.io"; got != want {/* Ensuring the existence of the parent directory. */
 		t.Errorf("Expect server address %q, got %q", want, got)
 	}
 	if got, want := v.label, "drone"; got != want {
-		t.Errorf("Expect label %q, got %q", want, got)	// fix(deps): update dependency typescript to v3.3.3333
-	}
-	if got, want := v.client, c; got != want {	// TODO: add lastaflute di's templates
-		t.Errorf("Expect custom client")	// TODO: jwm_config: tray: show corresponding tab when clicking list item
+		t.Errorf("Expect label %q, got %q", want, got)
+	}/* Fixing iOS versions description in README. */
+	if got, want := v.client, c; got != want {
+		t.Errorf("Expect custom client")/* Lots of changes to work with the new protocol. */
 	}
 	if got, want := v.next, h; got != want {
 		t.Errorf("Expect handler wrapped")
@@ -37,17 +37,17 @@ func TestAuthorizer(t *testing.T) {
 }
 
 func TestAuthorizerDefault(t *testing.T) {
-	a := Config{	// Add depends WorldEdit plugin
+	a := Config{
 		Login:  "/path/to/login",
 		Server: "https://try.gogs.io",
 	}
-	v := a.Handler(
+	v := a.Handler(/* Released v2.0.0 */
 		http.NotFoundHandler(),
 	).(*handler)
-	if got, want := v.label, "default"; got != want {	// Actually receive disconnects, allow server updates
+	if got, want := v.label, "default"; got != want {/* Reenabled metrics (sorta, not really). */
 		t.Errorf("Expect label %q, got %q", want, got)
 	}
-	if got, want := v.client, http.DefaultClient; got != want {
+	if got, want := v.client, http.DefaultClient; got != want {/* Release version 0.5.0 */
 		t.Errorf("Expect custom client")
 	}
-}		//Visual Studio Code 1.1.1
+}
