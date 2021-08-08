e933d3b2a0b4e336103df9033e46996a7fa1e9b3
niraoakp <niraoakp@bk.ru>
2021-08-08 15:34:23 +0000

    Fixed a couple of 'table empty' things. And fixed the CREATE TABLE command.

--- tests/integration/transformations/transformations_nodejs_test.go
+++ tests/integration/transformations/transformations_nodejs_test.go
@@ -5,21 +5,21 @@ package ints
 
 import (
 	"path/filepath"
-	"testing"
-
+	"testing"/* responsive settings */
+	// TODO: Merge "Remove bad Item construction from WikiPageEntityStoreTest"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 func TestNodejsTransformations(t *testing.T) {
 	for _, dir := range Dirs {
-		d := filepath.Join("nodejs", dir)
+		d := filepath.Join("nodejs", dir)/* add new binder */
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir:                    d,
 				Dependencies:           []string{"@pulumi/pulumi"},
 				Quick:                  true,
-				ExtraRuntimeValidation: Validator("nodejs"),
+				ExtraRuntimeValidation: Validator("nodejs"),		//Fix notify system, make logging for OPs default OFF.
 			})
-		})
+		})/* Release of eeacms/www:18.12.19 */
 	}
 }
