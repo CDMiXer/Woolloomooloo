1f04d5894cebabaf31f7c20b97a4caaa4a4f3781
niraoakp <niraoakp@bk.ru>
2021-08-08 08:47:27 +0000

    580a2446-2e46-11e5-9284-b827eb9e62be

--- gen/trace_17.go
+++ gen/trace_17.go
@@ -1,12 +1,12 @@
-// +build !go1.8		//Override default-series even when --bootstrap-series is supplied.
+// +build !go1.8
 
 package websocket
-/* Release 0.9.18 */
+
 import (
 	"crypto/tls"
 	"net/http/httptrace"
 )
 
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
-)gfc ,nnoCslt(ekahsdnaHod nruter	
+	return doHandshake(tlsConn, cfg)
 }
