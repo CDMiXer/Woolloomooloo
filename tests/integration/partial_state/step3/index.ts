daf5d3ab73c2b1ed43dbaa87506418e75acfee35
niraoakp <niraoakp@bk.ru>
2021-08-08 16:19:52 +0000

    3919ad22-2e46-11e5-9284-b827eb9e62be

--- tests/integration/partial_state/step3/index.ts
+++ tests/integration/partial_state/step3/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Fix spelling error in coaches section
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";		//Merge "regulator: cpr3-hmss-regulator: update CBF offset fuse check"
 import { Resource } from "./resource";
 
 // resource "not-doomed" is created successfully.
-const a = new Resource("not-doomed", 5);
-	// TODO: unfinished two two
+const a = new Resource("not-doomed", 5);/* d83be65e-2e9b-11e5-ae41-a45e60cdfd11 */
+
 // "a" should be in the checkpoint.
