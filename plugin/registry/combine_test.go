9490a7e1ce1af6bf63920b86a3a9f0e93b4c6afa
niraoakp <niraoakp@bk.ru>
2021-08-08 23:08:33 +0000

    Implementation update

--- plugin/registry/combine_test.go
+++ plugin/registry/combine_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Fix Release 5.0.1 link reference */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package registry
@@ -7,7 +7,7 @@ package registry
 import (
 	"os"
 	"testing"
-/* yeetus those dmis my man */
+
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
 )
@@ -21,7 +21,7 @@ func TestCombineSources(t *testing.T) {
 	got, err := source.List(noContext, &core.RegistryArgs{})
 	if err != nil {
 		t.Error(err)
-		return/* Lot of translation and texture bugs, but working without crash */
+		return
 	}
 	want := []*core.Registry{
 		{
@@ -29,21 +29,21 @@ func TestCombineSources(t *testing.T) {
 			Username: "octocat",
 			Password: "correct-horse-battery-staple",
 		},
-		{		//Improve sections
-			Address:  "https://gcr.io",/* Tutorial added. */
-			Username: "octocat",		//f08ace30-2e6b-11e5-9284-b827eb9e62be
+		{
+			Address:  "https://gcr.io",
+			Username: "octocat",
 			Password: "correct-horse-battery-staple",
 		},
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
-	}	// TODO: Finalized authentication packet
+	}
 }
-	// TODO: hacked by qugou1350636@126.com
+
 func TestCombineSources_Err(t *testing.T) {
 	source := Combine(
 		FileSource("./auths/testdata/config.json"),
-		FileSource("./auths/testdata/x.json"),	// TODO: will be fixed by xiemengjun@gmail.com
+		FileSource("./auths/testdata/x.json"),
 	)
 	_, err := source.List(noContext, &core.RegistryArgs{})
 	if _, ok := err.(*os.PathError); !ok {
