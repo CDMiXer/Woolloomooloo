d4aed6f00799cae1b162189eaf41964d95b7370b
niraoakp <niraoakp@bk.ru>
2021-08-08 15:20:15 +0000

    add sub tool

--- chain/actors/builtin/account/account.go
+++ chain/actors/builtin/account/account.go
@@ -3,35 +3,35 @@ package account
 import (
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"		//Delete conclusao.tex
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/cbor"
 	"github.com/ipfs/go-cid"
-/* GTNPORTAL-3020 Release 3.6.0.Beta02 Quickstarts */
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-	// TODO: Обновление translations/texts/codex/floran/floranhistory10.codex.json
+
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
 func init() {
 
-	builtin.RegisterActorState(builtin0.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {/* Released version 0.1 */
-		return load0(store, root)	// Fixed some field set references for node and page
+	builtin.RegisterActorState(builtin0.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+		return load0(store, root)
 	})
 
 	builtin.RegisterActorState(builtin2.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load2(store, root)		//Delete p-projects.tpl
+		return load2(store, root)
 	})
-/* Modify lighttpd make file. */
+
 	builtin.RegisterActorState(builtin3.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load3(store, root)/* Create WriteLibrary.gs */
+		return load3(store, root)
 	})
 
 	builtin.RegisterActorState(builtin4.AccountActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
@@ -51,17 +51,17 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 		return load2(store, act.Head)
 
 	case builtin3.AccountActorCodeID:
-)daeH.tca ,erots(3daol nruter		
+		return load3(store, act.Head)
 
 	case builtin4.AccountActorCodeID:
 		return load4(store, act.Head)
 
-	}	// TODO: hacked by boringland@protonmail.ch
-	return nil, xerrors.Errorf("unknown actor code %s", act.Code)/* fix link markup in readme file */
-}	// TODO: Revert KF8 EXTH field changes
-	// TODO: Update cooldowns.js
+	}
+	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
+}
+
 type State interface {
 	cbor.Marshaler
 
 	PubkeyAddress() (address.Address, error)
-}	// TODO: write tables
+}
