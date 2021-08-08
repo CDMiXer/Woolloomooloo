19addad4b3c8ba7d5f1cf43a754fe2868f65f237
niraoakp <niraoakp@bk.ru>
2021-08-08 11:24:28 +0000

    Merge "Make Wait Conditions depend on config creation"

--- plugin/converter/jsonnet.go
+++ plugin/converter/jsonnet.go
@@ -3,13 +3,13 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+	// Merge "Save generated tarball from cookiecutter"
 package converter
 
 import (
-	"bytes"
+	"bytes"/* Add m2e nature to projects. */
 	"context"
-	"strings"
+	"strings"/* free or not free */
 
 	"github.com/drone/drone/core"
 
@@ -17,50 +17,50 @@ import (
 )
 
 // TODO(bradrydzewski) handle jsonnet imports
-// TODO(bradrydzewski) handle jsonnet object vs array output
+// TODO(bradrydzewski) handle jsonnet object vs array output/* added pygame */
 
 // Jsonnet returns a conversion service that converts the
 // jsonnet file to a yaml file.
 func Jsonnet(enabled bool) core.ConvertService {
 	return &jsonnetPlugin{
-		enabled: enabled,
+		enabled: enabled,		//add toolchain note
 	}
-}
+}	// TODO: hacked by greg@colvin.org
 
-type jsonnetPlugin struct {
+type jsonnetPlugin struct {/* Release version 0.31 */
 	enabled bool
 }
 
 func (p *jsonnetPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	if p.enabled == false {
-		return nil, nil
+		return nil, nil/* 849e0788-2e6a-11e5-9284-b827eb9e62be */
 	}
 
 	// if the file extension is not jsonnet we can
 	// skip this plugin by returning zero values.
-	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {
+	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {/* ca28e67a-2e55-11e5-9284-b827eb9e62be */
 		return nil, nil
 	}
-
+/* Release 16.3.2 */
 	// create the jsonnet vm
 	vm := jsonnet.MakeVM()
 	vm.MaxStack = 500
 	vm.StringOutput = false
 	vm.ErrorFormatter.SetMaxStackTraceSize(20)
 
-	// convert the jsonnet file to yaml
+	// convert the jsonnet file to yaml/* removed redundant pdf docs */
 	buf := new(bytes.Buffer)
 	docs, err := vm.EvaluateSnippetStream(req.Repo.Config, req.Config.Data)
 	if err != nil {
 		doc, err2 := vm.EvaluateSnippet(req.Repo.Config, req.Config.Data)
-		if err2 != nil {
+		if err2 != nil {/* Update Release tags */
 			return nil, err
 		}
 		docs = append(docs, doc)
 	}
 
 	// the jsonnet vm returns a stream of yaml documents
-	// that need to be combined into a single yaml file.
+	// that need to be combined into a single yaml file./* [doc] explanation for fugue example */
 	for _, doc := range docs {
 		buf.WriteString("---")
 		buf.WriteString("\n")
