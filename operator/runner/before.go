78b302fed0c49c3ec24499dd993956d6cc189a5c
niraoakp <niraoakp@bk.ru>
2021-08-08 17:18:55 +0000

    Added .DS_Store to gitignore file for OSX users.

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Added new actions, invite, acceptInvite and rejectInvite */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Better naming on classes. Links only on touchscreens. */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+///* Add a summary and update mechanic key structure */
+// Unless required by applicable law or agreed to in writing, software/* model: topological sort moved to new class PartialOrderAlgorithms */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package runner
+/* 8975b051-2d5f-11e5-9ddd-b88d120fff5e */
+package runner		//Gamut clipping in sRGB improved
