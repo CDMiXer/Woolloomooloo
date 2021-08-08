7650af392df9b3a34d32287f7d92587209d83f77
niraoakp <niraoakp@bk.ru>
2021-08-08 06:02:30 +0000

    Update discord.yml

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -5,10 +5,10 @@ export const secret = pulumi.secret("secret");
 
 // Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
 // deployment.
-const org = new pulumi.Config().require("org");
-const project = pulumi.getProject();
+const org = new pulumi.Config().require("org");	// TODO: hacked by why@ipfs.io
+const project = pulumi.getProject();	// TODO: Merge sort initial draft
 const stack = pulumi.getStack();
 const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
 
 export const refNormal = sr.getOutput("normal");
-export const refSecret = sr.getOutput("secret");
+export const refSecret = sr.getOutput("secret");		//corner case bugfix
