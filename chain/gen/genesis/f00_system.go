c80b2f4ededccc02a65a295fbcd9363c7cea14c7
niraoakp <niraoakp@bk.ru>
2021-08-08 03:08:34 +0000

    Change BranchNodeModel to not destroy children nodes by default

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -1,24 +1,24 @@
 package genesis
-/* Updating build-info/dotnet/corefx/release/3.0 for preview8.19369.5 */
+
 import (
-	"context"/* Merge "Release 1.0.0.105 QCACLD WLAN Driver" */
+	"context"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin/system"/* Create PreviewReleaseHistory.md */
+	"github.com/filecoin-project/specs-actors/actors/builtin/system"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"/* added new query formulation method */
+	"github.com/filecoin-project/specs-actors/actors/builtin"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {/* Release 0.95.173: skirmish randomized layout */
+func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
 	var st system.State
-	// TODO: will be fixed by mikeal.rogers@gmail.com
+
 	cst := cbor.NewCborStore(bs)
 
 	statecid, err := cst.Put(context.TODO(), &st)
-	if err != nil {/* Add ASF embedded image support */
+	if err != nil {
 		return nil, err
 	}
 
