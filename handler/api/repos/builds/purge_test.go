f452d7e45c66f67d1db7caa4d2af3b18a6975f4f
niraoakp <niraoakp@bk.ru>
2021-08-08 12:26:48 +0000

    Merge "add source of leaflet from upstream version 0.5.1"

--- handler/api/repos/builds/purge_test.go
+++ handler/api/repos/builds/purge_test.go
@@ -1,69 +1,69 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-/* Apply the patch for the get content tag: issue 70 fixed */
-// +build !oss	// TODO: will be fixed by alex.gaynor@gmail.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by greg@colvin.org
+// Use of this source code is governed by the Drone Non-Commercial License		//Implemented PingArgument
+// that can be found in the LICENSE file./* Decrease timeout of EKS operations */
+
+// +build !oss
 
 package builds
 
 import (
 	"context"
-	"encoding/json"
+	"encoding/json"/* Merge "Release 1.0.0.238 QCACLD WLAN Driver" */
 	"net/http"
-	"net/http/httptest"
-	"testing"/* Copy of the NOTICE also at the top level if people check out everything */
-	// Adjusting decoding coefficients to ensure in-phase decoding
+	"net/http/httptest"	// Worked on StudentPersistenceHandler
+	"testing"
+
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/request"/* Deleting wiki page Release_Notes_v2_1. */
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"		//854ce950-2e52-11e5-9284-b827eb9e62be
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestPurge(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()	// Update INSTALL links out of code block
+	controller := gomock.NewController(t)	// Implement marker interface for BooleanCondition
+	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
-/* Release 9. */
-	builds := mock.NewMockBuildStore(controller)
-	builds.EXPECT().Purge(gomock.Any(), mockRepo.ID, int64(50)).Return(nil)		//return proper response dict from module_reload for api results
+	// TODO: will be fixed by sbrichards@gmail.com
+	builds := mock.NewMockBuildStore(controller)/* Merge branch 'master' into CodeCleanup */
+	builds.EXPECT().Purge(gomock.Any(), mockRepo.ID, int64(50)).Return(nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")/* Release: Making ready for next release cycle 5.2.0 */
-	c.URLParams.Add("name", "hello-world")/* Reworked CloudKitty. */
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")/* Release for v6.3.0. */
 
-	w := httptest.NewRecorder()/* add calibration to readme */
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/?before=50", nil)
 	r = r.WithContext(
 		context.WithValue(request.WithUser(r.Context(), mockUser), chi.RouteCtxKey, c),
 	)
 
 	HandlePurge(repos, builds)(w, r)
-	if got, want := w.Code, http.StatusNoContent; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+	if got, want := w.Code, http.StatusNoContent; want != got {		//Merge "Use default quota values in test_quotas"
+		t.Errorf("Want response code %d, got %d", want, got)/* Merge "Fix pxelinux.0 package name for Xenial" */
 	}
 }
-	// Update getsys
+
 // The test verifies that a 404 Not Found error is returned
-// if the repository store returns an error./* Add Corp API Management Nav */
-func TestPurge_NotFound(t *testing.T) {
+// if the repository store returns an error.
+func TestPurge_NotFound(t *testing.T) {/* Release 0.0.99 */
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()		//CriteriaFilter can now optionally specify sort order
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)
-
-	c := new(chi.Context)
+	// TODO: hacked by juan@benet.ai
+	c := new(chi.Context)	// TODO: Path: Add tests for existing file magics.
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	// TODO: Added new pynest api's to readthedocs documentation
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/?before=50", nil)
 	r = r.WithContext(
-		context.WithValue(request.WithUser(r.Context(), mockUser), chi.RouteCtxKey, c),	// TODO: hacked by steven@stebalien.com
+		context.WithValue(request.WithUser(r.Context(), mockUser), chi.RouteCtxKey, c),
 	)
 
 	HandlePurge(repos, nil)(w, r)
