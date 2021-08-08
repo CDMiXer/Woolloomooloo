4c9de54890318c7dc33a012e66d4bee18f69a478
niraoakp <niraoakp@bk.ru>
2021-08-08 23:22:29 +0000

    Excluded project.lock.json

--- tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
@@ -1,24 +1,24 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+/* Release of eeacms/ims-frontend:0.9.5 */
+import * as pulumi from "@pulumi/pulumi";		//mui: implement most of the vertical/horizontal layout
 
-import * as pulumi from "@pulumi/pulumi";
-		//Create lift_hoch.rst
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);/* Added books from "Spenden" to new items query. */
+        super("my:module:Resource", name, {}, opts);
     }
-}/* Release 0.2.8 */
+}
 
 // Scenario #5 - composing #1 and #3
 class ComponentFive extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFive", name, {}, opts);
+        super("my:module:ComponentFive", name, {}, opts);		//Fixs the donation link.
         this.resource = new Resource("otherchildrenamed", {
             parent: this,
-            aliases: [{ name: "otherchild", parent: this }],/* Prepared Release 1.0.0-beta */
+            aliases: [{ name: "otherchild", parent: this }],
         });
     }
 }
-const comp5 = new ComponentFive("newcomp5", {		//FIX new custom echarts js library, supporting visualMap parts
-    aliases: [{ name: "comp5" }],/* Release version [10.6.0] - prepare */
+const comp5 = new ComponentFive("newcomp5", {
+    aliases: [{ name: "comp5" }],
 });
