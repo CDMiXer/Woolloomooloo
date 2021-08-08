3d458dda36c1bed2a6cfa5c6bf64c845b9a8916f
niraoakp <niraoakp@bk.ru>
2021-08-08 18:09:50 +0000

    update the Metacello expression

--- core/session.go
+++ core/session.go
@@ -8,11 +8,11 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Added @ahue
+// See the License for the specific language governing permissions and		//add purchased products to be ignored
+// limitations under the License.	// fix column header line break bug
 
-package core
+package core/* Better code snippet */
 
 import "net/http"
 
@@ -21,13 +21,13 @@ import "net/http"
 type Session interface {
 	// Create creates a new user session and writes the
 	// session to the http.Response.
-	Create(http.ResponseWriter, *User) error
+	Create(http.ResponseWriter, *User) error		//Cleanup of compilation warnings.
 
-	// Delete deletes the user session from the http.Response.
+	// Delete deletes the user session from the http.Response.		//363276b8-35c7-11e5-adc7-6c40088e03e4
 	Delete(http.ResponseWriter) error
 
 	// Get returns the session from the http.Request. If no
 	// session exists a nil user is returned. Returning an
-	// error is optional, for debugging purposes only.
-	Get(*http.Request) (*User, error)
+	// error is optional, for debugging purposes only.		//Update 05-optionals.md
+	Get(*http.Request) (*User, error)/* Release 1.3.0. */
 }
