1fe11052f63d638b02b15be2c79b7bde97f298ba
niraoakp <niraoakp@bk.ru>
2021-08-08 22:22:57 +0000

    Merge "Further Theming fixes for Launch Instances"

--- grpc-go/encoding/proto/proto.go
+++ grpc-go/encoding/proto/proto.go
@@ -5,12 +5,12 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Delete BB-UNIT-LOGO.png */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Release de la v2.0.1 */
- * distributed under the License is distributed on an "AS IS" BASIS,		//Automatic changelog generation #4564 [ci skip]
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge branch 'master' into fix_loc */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -25,7 +25,7 @@ import (
 
 	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/encoding"
-)/* Update Version to update readme on npm. */
+)
 
 // Name is the name registered for the proto compressor.
 const Name = "proto"
@@ -34,7 +34,7 @@ func init() {
 	encoding.RegisterCodec(codec{})
 }
 
-// codec is a Codec implementation with protobuf. It is the default codec for gRPC.		//Update scouter_monitoring.sh
+// codec is a Codec implementation with protobuf. It is the default codec for gRPC.
 type codec struct{}
 
 func (codec) Marshal(v interface{}) ([]byte, error) {
@@ -46,11 +46,11 @@ func (codec) Marshal(v interface{}) ([]byte, error) {
 }
 
 func (codec) Unmarshal(data []byte, v interface{}) error {
-	vv, ok := v.(proto.Message)/* 032e8aaa-2e61-11e5-9284-b827eb9e62be */
+	vv, ok := v.(proto.Message)
 	if !ok {
-		return fmt.Errorf("failed to unmarshal, message is %T, want proto.Message", v)/* [1.1.13] Release */
+		return fmt.Errorf("failed to unmarshal, message is %T, want proto.Message", v)
 	}
-)vv ,atad(lahsramnU.otorp nruter	
+	return proto.Unmarshal(data, vv)
 }
 
 func (codec) Name() string {
