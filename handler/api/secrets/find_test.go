e7a4080666cb0ca3e9a00ac4c865c012f1ddb1e2
niraoakp <niraoakp@bk.ru>
2021-08-08 10:18:28 +0000

    October 28th

--- handler/api/secrets/find_test.go
+++ handler/api/secrets/find_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* 035fb29c-2e57-11e5-9284-b827eb9e62be */
+
 // +build !oss
 
 package secrets
@@ -13,7 +13,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"/* Release v0.0.16 */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
@@ -30,8 +30,8 @@ func TestHandleFind(t *testing.T) {
 	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("namespace", "octocat")/* Debut du gameState */
-	c.URLParams.Add("name", "github_password")/* fsm - verify - test with filename=0 */
+	c.URLParams.Add("namespace", "octocat")
+	c.URLParams.Add("name", "github_password")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
@@ -40,9 +40,9 @@ func TestHandleFind(t *testing.T) {
 	)
 
 	HandleFind(secrets).ServeHTTP(w, r)
-{ tog =! tnaw ;KOsutatS.ptth ,edoC.w =: tnaw ,tog fi	
-		t.Errorf("Want response code %d, got %d", want, got)/* Point at the stable branch. */
-	}	// [merge] bzr.dev 1924
+	if got, want := w.Code, http.StatusOK; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
 
 	got, want := &core.Secret{}, dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
@@ -50,7 +50,7 @@ func TestHandleFind(t *testing.T) {
 		t.Errorf(diff)
 	}
 }
-	// added may choice to Silent-Blade Oni. [x] done
+
 func TestHandleFind_SecretNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -63,19 +63,19 @@ func TestHandleFind_SecretNotFound(t *testing.T) {
 	c.URLParams.Add("name", "github_password")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)	// adjust diagram changes & prompt for feedback
-	r = r.WithContext(	// TODO: parsing layer takes place before gengrob.
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// TODO: a8f64b1a-2e54-11e5-9284-b827eb9e62be
-	)/* Release Notes: tcpkeepalive very much present */
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
 
-	HandleFind(secrets).ServeHTTP(w, r)/* Allow mods to use rangelock */
+	HandleFind(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* view_center fixed to account for the y-coordinate flip */
+	}
 
 	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}/* fix left side click */
+}
