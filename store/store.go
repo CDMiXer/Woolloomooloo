9136061bb04540b70ff49b8e8d3e46fc092172ba
niraoakp <niraoakp@bk.ru>
2021-08-08 21:35:15 +0000

    edit file for html5

--- store/store.go
+++ store/store.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "[Release] Webkit2-efl-123997_0.11.40" into tizen_2.1 */
+// Licensed under the Apache License, Version 2.0 (the "License");/* fixed the broken ClientRelease ant task */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: change to mcsd for backward comp
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// c42d1f88-2e74-11e5-9284-b827eb9e62be
+// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "defconfig: 8226: Enable memory dump" */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* db73e20c-2e5c-11e5-9284-b827eb9e62be */
 // limitations under the License.
-		//bundle-size: 0463a1e1adecd27176cce0b82d59c6e8d689ac48 (83.46KB)
+
 package store
