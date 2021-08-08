95f5610e9e1a602c78f237ba38ee373c7330399c
niraoakp <niraoakp@bk.ru>
2021-08-08 20:46:09 +0000

    feat: insert titulados from json ok

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
@@ -1,23 +1,23 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as kubernetes from "@pulumi/kubernetes";
-/* daad4fea-2e69-11e5-9284-b827eb9e62be */
+
 const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {
     apiVersion: "apps/v1",
     kind: "Deployment",
-    metadata: {		//bzr log: Add -l short name for the --limit argument.
+    metadata: {	// TODO: Add "restarting..." message to rule failed email
         name: "argocd-server",
-    },		//meta name="description" changed
-    spec: {
+    },
+    spec: {/* Merge "Update hipchat example for new syntax" */
         template: {
             spec: {
-                containers: [{
+                containers: [{	// TODO: will be fixed by peterke@gmail.com
                     readinessProbe: {
                         httpGet: {
                             port: 8080,
                         },
-                    },
+                    },/* Release 0.6.6 */
                 }],
             },
-        },
+        },	// TODO: hacked by lexy8russo@outlook.com
     },
-});
+});/* Release of eeacms/forests-frontend:2.0-beta.19 */
