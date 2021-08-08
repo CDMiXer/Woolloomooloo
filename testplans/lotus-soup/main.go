1ef5ffe519d6e36159afcf895f0e7daf75e20b1f
niraoakp <niraoakp@bk.ru>
2021-08-08 19:47:40 +0000

    Initial commit, extracted from SlimJim

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -7,14 +7,14 @@ import (
 
 	"github.com/testground/sdk-go/run"
 )
-
+		//0eaf6d4c-2e4b-11e5-9284-b827eb9e62be
 var cases = map[string]interface{}{
 	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
 	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
-	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
-	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
-	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
+	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),/* Repo name was changed. */
+	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),/* Release 1.8.0 */
+	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),	// TODO: will be fixed by ligi@ligi.de
 }
 
 func main() {
