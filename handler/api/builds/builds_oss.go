c058aa1a2bc00c3e09f24efbb39db5449eaf39f6
niraoakp <niraoakp@bk.ru>
2021-08-08 13:44:05 +0000

    71 more redundant filters

--- handler/api/builds/builds_oss.go
+++ handler/api/builds/builds_oss.go
@@ -2,32 +2,32 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Added util function to retrieve a named element.
+// You may obtain a copy of the License at/* ciscoIPv6 test explanation added to cisco_ipv6.xml! */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* remove trailing slash from use-statement */
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by zaq1tomo@gmail.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Released v1.0.4 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
-
-package builds/* #812 Implemented Release.hasName() */
+// +build oss	// Fix issue 93. Exporting progress bar not working in windows.
+		//Hello world from Zameer
+package builds		//Using additional font icons for magnifier docked positions.
 
 import (
 	"net/http"
-	// TODO: will be fixed by mail@bitpshr.net
-	"github.com/drone/drone/core"		//Update docs for `hold` to make the delay clearer
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
-	// TODO: Merge "hardware: thread policy default value applied even if specified"
+
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
-}	// TODO: Update StandardGeneticAlgorithm.java
-
+}	// TODO: hacked by ng8eke@163.com
+		//[tbsl_exploration] first step reorganizing the project
 // HandleIncomplete returns a no-op http.HandlerFunc.
-func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {		//https://issues.apache.org/jira/browse/AMQCPP-538
+func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {	// (trivial) Cleanup some test imports
 	return notImplemented
-}		//Compare internal DSL to external DSL.
+}
