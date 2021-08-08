6bdeaf35fca9a9868be80a984754382947abf038
niraoakp <niraoakp@bk.ru>
2021-08-08 11:31:07 +0000

    Fix filename in header comment

--- tests/integration/empty/dotnet/Program.cs
+++ tests/integration/empty/dotnet/Program.cs
@@ -2,11 +2,11 @@
 
 using System.Threading.Tasks;
 using Pulumi;
-
+		//Made the README easier to understand.
 class Program
 {
     static Task<int> Main(string[] args)
-    {
+    {	// TODO: fixed observables templates
         return Deployment.RunAsync(() => {});
     }
 }
