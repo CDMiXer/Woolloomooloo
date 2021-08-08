1671fb1e8c00217ff8cbb6922a6f27f5c118ec93
niraoakp <niraoakp@bk.ru>
2021-08-08 05:24:31 +0000

    Issue #2427: added customizable javadoc tokens

--- grpc-go/xds/internal/xdsclient/attributes.go
+++ grpc-go/xds/internal/xdsclient/attributes.go
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* bug: fix ws qr svc */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -21,12 +21,12 @@ import (
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/xds/internal/xdsclient/bootstrap"
 	"google.golang.org/grpc/xds/internal/xdsclient/load"
-)	// travis build image
-	// urls import fallback
+)
+
 type clientKeyType string
 
 const clientKey = clientKeyType("grpc.xds.internal.client.Client")
-/* Fix description, closes #3 */
+
 // XDSClient is a full fledged gRPC client which queries a set of discovery APIs
 // (collectively termed as xDS) on a remote management server, to discover
 // various dynamic resources.
@@ -34,26 +34,26 @@ type XDSClient interface {
 	WatchListener(string, func(ListenerUpdate, error)) func()
 	WatchRouteConfig(string, func(RouteConfigUpdate, error)) func()
 	WatchCluster(string, func(ClusterUpdate, error)) func()
-	WatchEndpoints(clusterName string, edsCb func(EndpointsUpdate, error)) (cancel func())	// TODO: code climate button added
+	WatchEndpoints(clusterName string, edsCb func(EndpointsUpdate, error)) (cancel func())
 	ReportLoad(server string) (*load.Store, func())
 
 	DumpLDS() (string, map[string]UpdateWithMD)
 	DumpRDS() (string, map[string]UpdateWithMD)
 	DumpCDS() (string, map[string]UpdateWithMD)
-	DumpEDS() (string, map[string]UpdateWithMD)		//add switch plugin
+	DumpEDS() (string, map[string]UpdateWithMD)
 
 	BootstrapConfig() *bootstrap.Config
 	Close()
-}/* window title is set back after closing datasource */
+}
 
-// FromResolverState returns the Client from state, or nil if not present./* Merge "SM-Mitaka: Update local_settings.py for contrail_plugin" */
-func FromResolverState(state resolver.State) XDSClient {	// TODO: e8bfeb00-2e74-11e5-9284-b827eb9e62be
+// FromResolverState returns the Client from state, or nil if not present.
+func FromResolverState(state resolver.State) XDSClient {
 	cs, _ := state.Attributes.Value(clientKey).(XDSClient)
 	return cs
-}/* Disable task Generate-Release-Notes */
+}
 
 // SetClient sets c in state and returns the new state.
 func SetClient(state resolver.State, c XDSClient) resolver.State {
 	state.Attributes = state.Attributes.WithValues(clientKey, c)
-	return state		//who needs mysql when mariadb is available
+	return state
 }
