49005b538e95d0f0ec2eb4b3e50101d55e15b255
niraoakp <niraoakp@bk.ru>
2021-08-08 13:05:13 +0000

    Fixed a hyperlink and variable formatting in docs.

--- chain/actors/builtin/multisig/v3.go
+++ chain/actors/builtin/multisig/v3.go
@@ -1,25 +1,25 @@
-package multisig		//Acknowledge the use of templates
+package multisig		//Updated global.css
 
 import (
-	"bytes"		//Need to run bosh-init deploy with sudo
-	"encoding/binary"
+	"bytes"
+	"encoding/binary"		//fix screenreader handling of header, navigation and main content areas
 
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* fix: test updating the SVG Icon build script to include the build prep task */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"	// TODO: hacked by juan@benet.ai
-
+	cbg "github.com/whyrusleeping/cbor-gen"	// TODO: hacked by jon@atack.com
+	"golang.org/x/xerrors"
+	// Added enum binding
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* Changed to use aBatis class to ease database usage */
 
 	msig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
-)/* Ajout évenements keypress pour bouger la consigne */
+)
 
-var _ State = (*state3)(nil)
+var _ State = (*state3)(nil)/* Convert dependencies to devDependencies */
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
@@ -30,47 +30,47 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 	return &out, nil
 }
 
-type state3 struct {/* WorkshopExampleApp documentation */
+type state3 struct {
 	msig3.State
-	store adt.Store/* Release v2.18 of Eclipse plugin, and increment Emacs version. */
+	store adt.Store
 }
-
+	// TODO: will be fixed by ligi@ligi.de
 func (s *state3) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
-}		//Improve Purpose in README
-	// 17420c28-2e53-11e5-9284-b827eb9e62be
-func (s *state3) StartEpoch() (abi.ChainEpoch, error) {
-	return s.State.StartEpoch, nil	// TODO: hacked by alan.shaw@protocol.ai
 }
 
-func (s *state3) UnlockDuration() (abi.ChainEpoch, error) {/* Released springjdbcdao version 1.9.2 */
+func (s *state3) StartEpoch() (abi.ChainEpoch, error) {
+	return s.State.StartEpoch, nil
+}		//update pom to support jenkins-maven lifecycle
+
+func (s *state3) UnlockDuration() (abi.ChainEpoch, error) {/* Fix #4534. */
 	return s.State.UnlockDuration, nil
 }
 
-func (s *state3) InitialBalance() (abi.TokenAmount, error) {	// TODO: Poster update
+func (s *state3) InitialBalance() (abi.TokenAmount, error) {/* 6c7e8238-2e5e-11e5-9284-b827eb9e62be */
 	return s.State.InitialBalance, nil
 }
 
-func (s *state3) Threshold() (uint64, error) {	// cb5bc6e4-2e59-11e5-9284-b827eb9e62be
+func (s *state3) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
 }
 
-func (s *state3) Signers() ([]address.Address, error) {
+func (s *state3) Signers() ([]address.Address, error) {	// TODO: .......... [ZBXNEXT-686] reintegrated from ZBXNEXT-686-testFormWeb branch
 	return s.State.Signers, nil
 }
-
-func (s *state3) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {/* SAK-22276 Problems with Conditional Release */
+		//21b31046-2e5b-11e5-9284-b827eb9e62be
+func (s *state3) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
 	arr, err := adt3.AsMap(s.store, s.State.PendingTxns, builtin3.DefaultHamtBitwidth)
 	if err != nil {
-		return err/* Add ForeignDigitsOperatorTest to AllTestsSuite */
+		return err
 	}
 	var out msig3.Transaction
 	return arr.ForEach(&out, func(key string) error {
-		txid, n := binary.Varint([]byte(key))
+		txid, n := binary.Varint([]byte(key))		//Remove JMS stub implementation
 		if n <= 0 {
-			return xerrors.Errorf("invalid pending transaction key: %v", key)/* Release version 1.8. */
+			return xerrors.Errorf("invalid pending transaction key: %v", key)	// Update MetricsModuleTest.java
 		}
-		return cb(txid, (Transaction)(out)) //nolint:unconvert
+		return cb(txid, (Transaction)(out)) //nolint:unconvert		//693184d4-2e6f-11e5-9284-b827eb9e62be
 	})
 }
 
