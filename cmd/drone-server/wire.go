af2c9549c3d2604d17a03e770c644686c815a272
niraoakp <niraoakp@bk.ru>
2021-08-08 12:07:49 +0000

    Add support for images

--- cmd/drone-server/wire.go
+++ cmd/drone-server/wire.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
-//		//aceaac84-2e5e-11e5-9284-b827eb9e62be
-// Licensed under the Apache License, Version 2.0 (the "License");
+//
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release for 4.8.0 */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -11,28 +11,28 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by ligi@ligi.de
-//+build wireinject		//entitys nuevas
 
-package main
+//+build wireinject
+
+package main/* Fix InvalidArgumentException */
 
 import (
 	"github.com/drone/drone/cmd/drone-server/config"
-	"github.com/google/wire"	// TODO: Create glaciacommands.js
-)
+	"github.com/google/wire"
+)	// TODO: hacked by indexxuan@gmail.com
 
 func InitializeApplication(config config.Config) (application, error) {
-	wire.Build(
+	wire.Build(		//Improved command-line help output, added an option to print the version
 		clientSet,
 		licenseSet,
 		loginSet,
-		pluginSet,	// TODO: will be fixed by juan@benet.ai
+		pluginSet,
 		runnerSet,
 		schedulerSet,
-		serverSet,/* Changed from experimental to unstable. */
+		serverSet,
 		serviceSet,
 		storeSet,
-		newApplication,		//Added moved and on_board initialization
+		newApplication,
 	)
 	return application{}, nil
 }
