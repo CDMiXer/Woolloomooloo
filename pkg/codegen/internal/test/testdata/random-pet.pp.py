5303ba7b902fe36da0d32d9ac335638fd9d33b5b
niraoakp <niraoakp@bk.ru>
2021-08-08 02:08:26 +0000

    Wheat client pom reset and update

--- pkg/codegen/internal/test/testdata/random-pet.pp.py
+++ pkg/codegen/internal/test/testdata/random-pet.pp.py
@@ -1,4 +1,4 @@
 import pulumi
 import pulumi_random as random
-
-random_pet = random.RandomPet("random_pet", prefix="doggo")
+/* Released 0.1.15 */
+random_pet = random.RandomPet("random_pet", prefix="doggo")	// TODO: will be fixed by greg@colvin.org
