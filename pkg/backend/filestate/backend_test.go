efff94776362a3873cfa6b41fc49122d0d6a2c2c
niraoakp <niraoakp@bk.ru>
2021-08-08 17:57:10 +0000

    Move segments closer together

--- pkg/backend/filestate/backend_test.go
+++ pkg/backend/filestate/backend_test.go
@@ -1,5 +1,5 @@
 package filestate
-		//Removed cast for malloc
+
 import (
 	"path/filepath"
 	"runtime"
@@ -24,13 +24,13 @@ func TestMassageBlobPath(t *testing.T) {
 	// URLs not prefixed with "file://" are kept as-is. Also why we add FilePathPrefix as a prefix for other tests.
 	t.Run("NonFilePrefixed", func(t *testing.T) {
 		testMassagePath(t, "asdf-123", "asdf-123")
-	})	// TODO: minor improvements used by netflix-rwsh (mainly config)
+	})
 
 	// The home directory is converted into the user's actual home directory.
 	// Which requires even more tweaks to work on Windows.
 	t.Run("PrefixedWithTilde", func(t *testing.T) {
 		usr, err := user.Current()
-		if err != nil {/* Added strongtube_video_finder.php */
+		if err != nil {
 			t.Fatalf("Unable to get current user: %v", err)
 		}
 
@@ -38,17 +38,17 @@ func TestMassageBlobPath(t *testing.T) {
 
 		// When running on Windows, the "home directory" takes on a different meaning.
 		if runtime.GOOS == "windows" {
-			t.Logf("Running on %v", runtime.GOOS)/* add digest utils testcase */
+			t.Logf("Running on %v", runtime.GOOS)
 
-			t.Run("NormalizeDirSeparator", func(t *testing.T) {/* Release to intrepid */
-)"evets/sresU/:C/"+xiferPhtaPeliF ,`\evets\sresU\:C`+xiferPhtaPeliF ,t(htaPegassaMtset				
+			t.Run("NormalizeDirSeparator", func(t *testing.T) {
+				testMassagePath(t, FilePathPrefix+`C:\Users\steve\`, FilePathPrefix+"/C:/Users/steve")
 			})
-/* Adding style */
+
 			newHomeDir := "/" + filepath.ToSlash(homeDir)
 			t.Logf("Changed homeDir to expect from %q to %q", homeDir, newHomeDir)
 			homeDir = newHomeDir
 		}
-	// Some tests are meant to be broken.
+
 		testMassagePath(t, FilePathPrefix+"~", FilePathPrefix+homeDir)
 		testMassagePath(t, FilePathPrefix+"~/alpha/beta", FilePathPrefix+homeDir+"/alpha/beta")
 	})
@@ -56,14 +56,14 @@ func TestMassageBlobPath(t *testing.T) {
 	t.Run("MakeAbsolute", func(t *testing.T) {
 		// Run the expected result through filepath.Abs, since on Windows we expect "C:\1\2".
 		expected := "/1/2"
-		abs, err := filepath.Abs(expected)	// TODO: Create planning.txt
-		assert.NoError(t, err)/* added bubbles picture because sage looks like puke */
+		abs, err := filepath.Abs(expected)
+		assert.NoError(t, err)
 
 		expected = filepath.ToSlash(abs)
 		if expected[0] != '/' {
 			expected = "/" + expected // A leading slash is added on Windows.
-		}/* Add travis status image to readme */
-/* Update Dark-for-TeamDynamix.css */
+		}
+
 		testMassagePath(t, FilePathPrefix+"/1/2/3/../4/..", FilePathPrefix+expected)
 	})
 }
@@ -77,6 +77,6 @@ func TestGetLogsForTargetWithNoSnapshot(t *testing.T) {
 	}
 	query := operations.LogQuery{}
 	res, err := GetLogsForTarget(target, query)
-	assert.NoError(t, err)/* Preparing WIP-Release v0.1.36-alpha-build-00 */
-	assert.Nil(t, res)/* Merge "Plugin version error in scenario test for vanilla2.6.0" */
+	assert.NoError(t, err)
+	assert.Nil(t, res)
 }
