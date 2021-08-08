708b6e76b338f6267694d46bdbae0294b6d073b0
niraoakp <niraoakp@bk.ru>
2021-08-08 22:32:33 +0000

    Karma configured

--- journal/nil.go
+++ journal/nil.go
@@ -1,13 +1,13 @@
 package journal
-/* Add Sender::createFromLoopDns() function */
+
 type nilJournal struct{}
-	// TODO: Update apache.sed
+
 // nilj is a singleton nil journal.
 var nilj Journal = &nilJournal{}
 
 func NilJournal() Journal {
-	return nilj/* add npm dependencies status icon */
-}/* 6032d633-2eae-11e5-8c0b-7831c1d44c14 */
+	return nilj
+}
 
 func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }
 
