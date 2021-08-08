6d77f173990b71bfaaf179f39792c819c17b061e
niraoakp <niraoakp@bk.ru>
2021-08-08 13:07:48 +0000

    The getActiveUser function made virtual.

--- tests/integration/steps/step3/index.ts
+++ tests/integration/steps/step3/index.ts
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";		//netview image
+import { Resource } from "./resource";
 
 // Step 3: Replace a resource:
 // * Create 1 resource, a3, with a property different than the a2 in Step 2, requiring replacement
-//   (CreateReplacement(a3), Update(c2=>c3), DeleteReplaced(a2)).	// TODO: hacked by onhardev@bk.ru
+//   (CreateReplacement(a3), Update(c2=>c3), DeleteReplaced(a2)).		//d7304aa0-2e70-11e5-9284-b827eb9e62be
 let a = new Resource("a", { state: 1, replace: 1 });
 // * Elide b (Delete(b2)).
 // * Create 2 resources, c3 and e3, equivalent to Step 2 (Same(c2, c3), Same(e2, e3)).
