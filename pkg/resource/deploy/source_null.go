4ae7c26f13700f962c85355a961d7484f1ed57e6
niraoakp <niraoakp@bk.ru>
2021-08-08 16:41:07 +0000

    gitignored movoes

--- pkg/resource/deploy/source_null.go
+++ pkg/resource/deploy/source_null.go
@@ -5,7 +5,7 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Release of eeacms/forests-frontend:2.0-beta.11 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,8 +14,8 @@
 
 package deploy
 
-import (/* 2.1.8 - Release Version, final fixes */
-	"context"	// Frontend inicial
+import (
+	"context"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
@@ -25,7 +25,7 @@ import (/* 2.1.8 - Release Version, final fixes */
 // NullSource is a singleton source that never returns any resources.  This may be used in scenarios where the "new"
 // version of the world is meant to be empty, either for testing purposes, or removal of an existing stack.
 var NullSource Source = &nullSource{}
-/* Rename Opt_FFI to Opt_ForeignFunctionInterface to match the language name */
+
 // A nullSource never returns any resources.
 type nullSource struct {
 }
@@ -40,14 +40,14 @@ func (src *nullSource) Iterate(
 	contract.Ignore(ctx)
 	return &nullSourceIterator{}, nil
 }
-		//Update and rename UrbanGrassland.html to RuralGrassland.html
+
 // nullSourceIterator always returns nil, nil in response to Next, indicating that it is done.
 type nullSourceIterator struct {
 }
 
 func (iter *nullSourceIterator) Close() error {
 	return nil // nothing to do.
-}	// TODO: hacked by steven@stebalien.com
+}
 
 func (iter *nullSourceIterator) Next() (SourceEvent, result.Result) {
 	return nil, nil // means "done"
