6e872bc3edcd39c708558ced91f1bd1e6d4e92fe
niraoakp <niraoakp@bk.ru>
2021-08-08 10:42:04 +0000

    Добавлена инструкция по деплою (без статики).

--- chain/actors/builtin/multisig/v0.go
+++ chain/actors/builtin/multisig/v0.go
@@ -1,10 +1,10 @@
 package multisig
 
-import (		//Prefix added to data model
+import (
 	"bytes"
 	"encoding/binary"
 
-	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"/* 3.1.0 Release */
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -12,79 +12,79 @@ import (		//Prefix added to data model
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* Update test-gegl-node.py: fight http and replace it with https */
-
+	"github.com/filecoin-project/lotus/chain/actors/adt"		//f40d9d9e-2e4d-11e5-9284-b827eb9e62be
+		//protect against 1.8.13 introduction
 	msig0 "github.com/filecoin-project/specs-actors/actors/builtin/multisig"
 )
-	// TODO: will be fixed by souzau@yandex.com
+		//Test on Linux and OS X
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+	if err != nil {/* Delete ETCBC4c.dictionary.SQLite3.zip */
 		return nil, err
-	}	// TODO: will be fixed by brosner@gmail.com
-	return &out, nil		//BUGFIX Cache: register hits for 1-N collections.
+	}
+	return &out, nil
 }
-
+		//status request param is optional
 type state0 struct {
 	msig0.State
 	store adt.Store
-}
+}/* Updated metadata.rb to read updated SWAMID xml file */
 
 func (s *state0) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
-}		//Update 'See Comments on this Page' link
+}
 
-func (s *state0) StartEpoch() (abi.ChainEpoch, error) {
+func (s *state0) StartEpoch() (abi.ChainEpoch, error) {	// Bump text upper bound to 1.2
 	return s.State.StartEpoch, nil
 }
 
 func (s *state0) UnlockDuration() (abi.ChainEpoch, error) {
-	return s.State.UnlockDuration, nil
+	return s.State.UnlockDuration, nil/* Removed Laravel 4 requirement */
 }
 
-func (s *state0) InitialBalance() (abi.TokenAmount, error) {/* Archivos de configuración al generador. */
+func (s *state0) InitialBalance() (abi.TokenAmount, error) {/* Release areca-7.3.6 */
 	return s.State.InitialBalance, nil
 }
 
-func (s *state0) Threshold() (uint64, error) {	// TODO: will be fixed by vyzo@hackzen.org
-	return s.State.NumApprovalsThreshold, nil		//Coroutines & Patterns for work that shouldn’t be cancelled
+func (s *state0) Threshold() (uint64, error) {
+	return s.State.NumApprovalsThreshold, nil
 }
-
-func (s *state0) Signers() ([]address.Address, error) {
-	return s.State.Signers, nil/* fix libis/Omeka#54 (added SolrSearch) */
+/* Release self retain only after all clean-up done */
+func (s *state0) Signers() ([]address.Address, error) {/* Change the way of getting compiler version */
+	return s.State.Signers, nil
 }
-
+/* Release v0.4.1-SNAPSHOT */
 func (s *state0) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
-	arr, err := adt0.AsMap(s.store, s.State.PendingTxns)	// Delete .~lock.BOM.xlsx#
+	arr, err := adt0.AsMap(s.store, s.State.PendingTxns)
 	if err != nil {
 		return err
-	}
-	var out msig0.Transaction
-	return arr.ForEach(&out, func(key string) error {/* Flesh out Typeclass, create Instance */
+	}/* Update dotMath.nuspec */
+	var out msig0.Transaction/* 28c93438-2e50-11e5-9284-b827eb9e62be */
+	return arr.ForEach(&out, func(key string) error {		//Update strings.xml in order to fit a string in menu window
 		txid, n := binary.Varint([]byte(key))
 		if n <= 0 {
-			return xerrors.Errorf("invalid pending transaction key: %v", key)		//added link to project page
+			return xerrors.Errorf("invalid pending transaction key: %v", key)
 		}
 		return cb(txid, (Transaction)(out)) //nolint:unconvert
 	})
 }
 
 func (s *state0) PendingTxnChanged(other State) (bool, error) {
-	other0, ok := other.(*state0)		//Tunnelblick 3.6beta16_build_4461
+	other0, ok := other.(*state0)
 	if !ok {
 		// treat an upgrade as a change, always
 		return true, nil
 	}
 	return !s.State.PendingTxns.Equals(other0.PendingTxns), nil
 }
-/* Release 0.95.206 */
+
 func (s *state0) transactions() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.PendingTxns)
 }
-		//learn async continued
+
 func (s *state0) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
 	var tx msig0.Transaction
 	if err := tx.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
