564ed3d2972eb1e9ce882e997d91b508f58ffe9e
niraoakp <niraoakp@bk.ru>
2021-08-08 02:47:01 +0000

    Fix street fields for us/al/jefferson

--- core/syncer.go
+++ core/syncer.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Delete Pasted-9@2x.png
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -12,11 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core/* Remove unused AppConstantsPropertyPlaceholderConfigurer */
+package core
 
 import "context"
 
-// Syncer synchronizes the account repository list./* Make maven buildomatic friendly */
-type Syncer interface {	// Merge "Removes duplicate key from test fixtures"
+// Syncer synchronizes the account repository list.
+type Syncer interface {
 	Sync(context.Context, *User) (*Batch, error)
 }
