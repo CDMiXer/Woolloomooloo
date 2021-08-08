29f1ebddd8616b048d0e1fb551b4ba39de9065a9
niraoakp <niraoakp@bk.ru>
2021-08-08 10:09:08 +0000

    Merge "Add flag to hide disabled keyguard options."

--- handler/api/repos/builds/find_test.go
+++ handler/api/repos/builds/find_test.go
@@ -1,92 +1,92 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* adapted DataManager tests */
-package builds
 
+package builds
+/* Release 1.3 check in */
 import (
 	"context"
-	"encoding/json"
-	"net/http/httptest"
+	"encoding/json"/* Re #26025 Release notes */
+	"net/http/httptest"		//Added :use-local-compiler:
 	"testing"
 
-	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/errors"	// Merge "Add solum-tempest-plugin to required-projects"
 	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"/* Fix heading markdown syntax */
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"		//Fix error in adapter.js
 )
-		//Добавил ID потока
+
 func TestFind(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: Vorgängerversion reaktiviert
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
 
-	builds := mock.NewMockBuildStore(controller)/* 78ae8648-4b19-11e5-96bf-6c40088e03e4 */
+	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuild, nil)
 
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("owner", "octocat")	// TODO: Renamed Purchases Dashboard menuitem,action-view,portal name.
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("number", "1")/* Create Release directory */
-	// made int argc const
+	c.URLParams.Add("number", "1")
+/* Release 2.1.1. */
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)/* Now we can register your functions from plugin. */
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleFind(repos, builds, stages)(w, r)
-		//Delete 404.01100.js
+/* Release version 2.1. */
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* Update Rome_2.txt */
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
+		t.Errorf(diff)/* Release conf compilation fix */
 	}
 }
 
-func TestFind_BadRequest(t *testing.T) {/* Update AuthOnlyView.js */
+func TestFind_BadRequest(t *testing.T) {/* Upgrade to 0.4.6 of HSE */
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")/* Added cardtype */
+	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("number", "one")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(/* ed0c37fc-2e62-11e5-9284-b827eb9e62be */
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// TODO: will be fixed by souzau@yandex.com
-	)		//add missing kernel config symbols
-
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+/* Changed back to 'same'. */
 	HandleFind(nil, nil, nil)(w, r)
 
 	if got, want := w.Code, 400; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-	// TODO: hacked by aeongrp@outlook.com
+
 	got, want := new(errors.Error), &errors.Error{Message: "strconv.ParseInt: parsing \"one\": invalid syntax"}
-	json.NewDecoder(w.Body).Decode(&got)
+	json.NewDecoder(w.Body).Decode(&got)/* no extra space in "not TRUE" (stopifnot) */
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
+		t.Errorf(diff)/* Released 0.9.45 and moved to 0.9.46-SNAPSHOT */
 	}
 }
-	// Change in twitter feed template.
+
 func TestFind_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)
-		//collect decompilation performance statistic data
+
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
