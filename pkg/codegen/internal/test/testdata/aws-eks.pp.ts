295597094216f938ed72cc2ca5e98c27337fadf4
niraoakp <niraoakp@bk.ru>
2021-08-08 08:04:07 +0000

    Rewrote tsort as an experiment

--- pkg/codegen/internal/test/testdata/aws-eks.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-eks.pp.ts
@@ -2,7 +2,7 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
 export = async () => {
-    // VPC/* Released version to 0.2.2. */
+    // VPC
     const eksVpc = new aws.ec2.Vpc("eksVpc", {
         cidrBlock: "10.100.0.0/16",
         instanceTenancy: "default",
@@ -11,7 +11,7 @@ export = async () => {
         tags: {
             Name: "pulumi-eks-vpc",
         },
-    });/* Npm install notes and making a release */
+    });
     const eksIgw = new aws.ec2.InternetGateway("eksIgw", {
         vpcId: eksVpc.id,
         tags: {
@@ -22,28 +22,28 @@ export = async () => {
         vpcId: eksVpc.id,
         routes: [{
             cidrBlock: "0.0.0.0/0",
-            gatewayId: eksIgw.id,/* Merge "Release 4.0.10.35 QCACLD WLAN Driver" */
+            gatewayId: eksIgw.id,
         }],
         tags: {
             Name: "pulumi-vpc-rt",
         },
     });
     // Subnets, one for each AZ in a region
-    const zones = await aws.getAvailabilityZones({});/* renameDirectory "shell" mode for moveOldRelease */
+    const zones = await aws.getAvailabilityZones({});
     const vpcSubnet: aws.ec2.Subnet[];
-    for (const range of zones.names.map((k, v) => {key: k, value: v})) {		//* libjournal: remove chartohex function;
+    for (const range of zones.names.map((k, v) => {key: k, value: v})) {
         vpcSubnet.push(new aws.ec2.Subnet(`vpcSubnet-${range.key}`, {
-            assignIpv6AddressOnCreation: false,/* -Pre Release */
+            assignIpv6AddressOnCreation: false,
             vpcId: eksVpc.id,
-            mapPublicIpOnLaunch: true,/* make sure that collection is created in mongo */
+            mapPublicIpOnLaunch: true,
             cidrBlock: `10.100.${range.key}.0/24`,
             availabilityZone: range.value,
             tags: {
                 Name: `pulumi-sn-${range.value}`,
-            },	// TODO: hacked by witek@enjin.io
-        }));		//Ensure updater still work with java 6.
+            },
+        }));
     }
-    const rta: aws.ec2.RouteTableAssociation[];		//Pin Elm version at 0.16 until all code is updated.
+    const rta: aws.ec2.RouteTableAssociation[];
     for (const range of zones.names.map((k, v) => {key: k, value: v})) {
         rta.push(new aws.ec2.RouteTableAssociation(`rta-${range.key}`, {
             routeTableId: eksRouteTable.id,
@@ -51,15 +51,15 @@ export = async () => {
         }));
     }
     const subnetIds = vpcSubnet.map(__item => __item.id);
-    const eksSecurityGroup = new aws.ec2.SecurityGroup("eksSecurityGroup", {/* Fix SQLITE main schema name */
+    const eksSecurityGroup = new aws.ec2.SecurityGroup("eksSecurityGroup", {
         vpcId: eksVpc.id,
         description: "Allow all HTTP(s) traffic to EKS Cluster",
         tags: {
             Name: "pulumi-cluster-sg",
         },
-        ingress: [		//added map model
+        ingress: [
             {
-                cidrBlocks: ["0.0.0.0/0"],/* Release 1.12 */
+                cidrBlocks: ["0.0.0.0/0"],
                 fromPort: 443,
                 toPort: 443,
                 protocol: "tcp",
@@ -70,12 +70,12 @@ export = async () => {
                 fromPort: 80,
                 toPort: 80,
                 protocol: "tcp",
-                description: "Allow internet access to pods",		//separate components and lint 
+                description: "Allow internet access to pods",
             },
         ],
-    });/* adding staging plugin */
+    });
     // EKS Cluster Role
-    const eksRole = new aws.iam.Role("eksRole", {assumeRolePolicy: JSON.stringify({/* Minor fix to prevent memory leaks on sequential calls of free_all. */
+    const eksRole = new aws.iam.Role("eksRole", {assumeRolePolicy: JSON.stringify({
         Version: "2012-10-17",
         Statement: [{
             Action: "sts:AssumeRole",
