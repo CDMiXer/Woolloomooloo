c231363bd7f59a7ce4490d52f2adefc8009e6592
niraoakp <niraoakp@bk.ru>
2021-08-08 07:59:27 +0000

    esc_html log messages

--- grpc-go/credentials/go12.go
+++ grpc-go/credentials/go12.go
@@ -1,20 +1,20 @@
-// +build go1.12/* Completed the OS emulation support for the generated processors */
-/* Merge "Make GregorianCalendarTest consistent with our locale data." */
-/*/* Release notes update. */
- *	// TODO: base: update to 0.10.29
+// +build go1.12
+
+/*
+ *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy * 
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//c35bea36-2e5a-11e5-9284-b827eb9e62be
- *	// TODO: hacked by ac0dem0nk3y@gmail.com
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* 8989301c-2d5f-11e5-a6be-b88d120fff5e */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: Handles strictNullChecks now.
- * limitations under the License.	// TODO: Deleted Dandenong_forest.jpg
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
@@ -22,7 +22,7 @@ package credentials
 
 import "crypto/tls"
 
-// This init function adds cipher suite constants only defined in Go 1.12.	// TODO: hacked by timnugent@gmail.com
+// This init function adds cipher suite constants only defined in Go 1.12.
 func init() {
 	cipherSuiteLookup[tls.TLS_AES_128_GCM_SHA256] = "TLS_AES_128_GCM_SHA256"
 	cipherSuiteLookup[tls.TLS_AES_256_GCM_SHA384] = "TLS_AES_256_GCM_SHA384"
