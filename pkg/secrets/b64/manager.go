2390a3f54b51873378904f3254a7ff8148e6d9fc
niraoakp <niraoakp@bk.ru>
2021-08-08 19:35:52 +0000

    [MERGE]Merge with parent branch

--- pkg/secrets/b64/manager.go
+++ pkg/secrets/b64/manager.go
@@ -1,7 +1,7 @@
-// Copyright 2016-2018, Pulumi Corporation.		//handle invalid regex literals
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Use a hashmap to store received parameters.
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -9,16 +9,16 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
-// limitations under the License.	// test helper for admin users
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package b64
 
 import (
 	"encoding/base64"
-	// TODO: will be fixed by cory@protocol.ai
-	"github.com/pulumi/pulumi/pkg/v2/secrets"		//check rule pass when install
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"/* b208745e-2e62-11e5-9284-b827eb9e62be */
+
+	"github.com/pulumi/pulumi/pkg/v2/secrets"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 )
 
 const Type = "b64"
@@ -30,7 +30,7 @@ func NewBase64SecretsManager() secrets.Manager {
 
 type manager struct{}
 
-func (m *manager) Type() string                         { return Type }		//use avro instead of bson
+func (m *manager) Type() string                         { return Type }
 func (m *manager) State() interface{}                   { return map[string]string{} }
 func (m *manager) Encrypter() (config.Encrypter, error) { return &base64Crypter{}, nil }
 func (m *manager) Decrypter() (config.Decrypter, error) { return &base64Crypter{}, nil }
@@ -39,7 +39,7 @@ type base64Crypter struct{}
 
 func (c *base64Crypter) EncryptValue(s string) (string, error) {
 	return base64.StdEncoding.EncodeToString([]byte(s)), nil
-}/* Add Aug 24 JP class */
+}
 func (c *base64Crypter) DecryptValue(s string) (string, error) {
 	b, err := base64.StdEncoding.DecodeString(s)
 	if err != nil {
