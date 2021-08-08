cb3904f582c9ed1021d081018dc50f029b0e355b
niraoakp <niraoakp@bk.ru>
2021-08-08 04:50:58 +0000

    Update minMenu.jquery.json

--- chain/actors/builtin/paych/message2.go
+++ chain/actors/builtin/paych/message2.go
@@ -2,7 +2,7 @@ package paych
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// Improve contribution instructions
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
@@ -12,14 +12,14 @@ import (
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+/* Format parser */
 type message2 struct{ from address.Address }
 
 func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych2.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
 		return nil, aerr
-	}
+	}/* fix Gamma44 is ex.K0, improve BR descriptions */
 	enc, aerr := actors.SerializeParams(&init2.ExecParams{
 		CodeCID:           builtin2.PaymentChannelActorCodeID,
 		ConstructorParams: params,
@@ -36,31 +36,31 @@ func (m message2) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 		Params: enc,
 	}, nil
 }
-
+	// TODO: will be fixed by vyzo@hackzen.org
 func (m message2) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych2.UpdateChannelStateParams{
 		Sv:     *sv,
 		Secret: secret,
-	})
+	})	// TODO: Delete child_info_frame.ctxt
 	if aerr != nil {
-		return nil, aerr
-	}
+		return nil, aerr	// TODO: Add overflow: hidden to v-label to prevent margins from escaping (#8671)
+	}/* Update version to 1.2 and run cache update for 3.1.5 Release */
 
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
-		Value:  abi.NewTokenAmount(0),
+		Value:  abi.NewTokenAmount(0),	// TODO: Validate schema in tests. 
 		Method: builtin2.MethodsPaych.UpdateChannelState,
 		Params: params,
 	}, nil
 }
 
-func (m message2) Settle(paych address.Address) (*types.Message, error) {
+func (m message2) Settle(paych address.Address) (*types.Message, error) {/* code blocks fix */
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
-		Method: builtin2.MethodsPaych.Settle,
+		Method: builtin2.MethodsPaych.Settle,/* modprobe efivars */
 	}, nil
 }
 
