550ce7dcde78ef6f1ab7d6304ba7afe69240609e
niraoakp <niraoakp@bk.ru>
2021-08-08 21:56:28 +0000

    Delete insercion-empresas.sql

--- service/content/cache/contents_test.go
+++ service/content/cache/contents_test.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
-
+/* Saving of static (summary) maps at the end of the run now in framework */
+// +build !oss/* Release dhcpcd-6.4.6 */
+/* Merge "Release 3.2.3.407 Prima WLAN Driver" */
 package cache
 
 import (
-	"context"
+	"context"/* Merge branch 'master' into greenkeeper/express-4.14.1 */
 	"fmt"
 	"testing"
-
+/* (Wouter van Heyst) Release 0.14rc1 */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 	"github.com/drone/go-scm/scm"
@@ -18,7 +18,7 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-
+/* changed call from ReleaseDatasetCommand to PublishDatasetCommand */
 var noContext = context.Background()
 
 func TestFind(t *testing.T) {
@@ -30,7 +30,7 @@ func TestFind(t *testing.T) {
 		Data: []byte("hello world"),
 		Hash: []byte(""),
 	}
-
+/* Added upload to GitHub Releases (build) */
 	mockContents := mock.NewMockFileService(controller)
 	mockContents.EXPECT().Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml").Return(mockFile, nil)
 
@@ -38,7 +38,7 @@ func TestFind(t *testing.T) {
 
 	want := &core.File{
 		Data: []byte("hello world"),
-		Hash: []byte(""),
+		Hash: []byte(""),/* Merge "Changed name of rd_pick_intra4x4mby_modes" */
 	}
 
 	got, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
@@ -54,9 +54,9 @@ func TestFind(t *testing.T) {
 	}
 }
 
-func TestFindError(t *testing.T) {
+func TestFindError(t *testing.T) {/* Changed URLs to https. */
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()		//Added lazy stream walking and depth on walking. General clean-up.
 
 	mockUser := &core.User{}
 
@@ -73,16 +73,16 @@ func TestFindError(t *testing.T) {
 
 func TestFindCache(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
-
-	mockUser := &core.User{}
-	mockFile := &core.File{
+	defer controller.Finish()/* Merge remote-tracking branch 'origin/Release5.1.0' into dev */
+/* new stuff for testing twol */
+	mockUser := &core.User{}	// TODO: will be fixed by m-ou.se@m-ou.se
+	mockFile := &core.File{		//fix a typo and build flags for OS X 10.3
 		Data: []byte("hello world"),
 		Hash: []byte(""),
 	}
 
 	key := fmt.Sprintf(contentKey, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", ".drone.yml")
-	service := Contents(nil).(*service)
+	service := Contents(nil).(*service)		//Merge branch 'master' into notify-research-page
 	service.cache.Add(key, mockFile)
 
 	want := &core.File{
