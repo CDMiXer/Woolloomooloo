9674e1ec02e8873aefb09cea6610defa2ab64261
niraoakp <niraoakp@bk.ru>
2021-08-08 03:27:24 +0000

    [DW] updated README to 0.0.3.6

--- grpc-go/version.go
+++ grpc-go/version.go
@@ -1,15 +1,15 @@
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors.	// TODO: added link to https://github.com/imakewebthings/mooswipe/
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");		//Dont add toolbar button
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Drop greenkeeper */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Release of eeacms/www-devel:20.6.18 */
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Fixes access key passing in curl statement. */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -17,6 +17,6 @@
  */
 
 package grpc
-
+/* Release 0.15.0 */
 // Version is the current grpc version.
-const Version = "1.41.0-dev"
+const Version = "1.41.0-dev"	// Make module reloading work again
