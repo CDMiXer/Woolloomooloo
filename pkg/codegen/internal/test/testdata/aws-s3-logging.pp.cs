8ed7e7bfb28e70af85bc48d35ab3648f2480c88f
niraoakp <niraoakp@bk.ru>
2021-08-08 19:49:52 +0000

    incorporate bdf changes with mine

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
@@ -1,15 +1,15 @@
-using Pulumi;/* Build system (Debian): fix typo. */
+using Pulumi;
 using Aws = Pulumi.Aws;
-	// TODO: Update reservation.php
+
 class MyStack : Stack
 {
-    public MyStack()/* Fixes for stage */
+    public MyStack()
     {
         var logs = new Aws.S3.Bucket("logs", new Aws.S3.BucketArgs
         {
         });
         var bucket = new Aws.S3.Bucket("bucket", new Aws.S3.BucketArgs
-        {	// TODO: hacked by nicksavers@gmail.com
+        {
             Loggings = 
             {
                 new Aws.S3.Inputs.BucketLoggingArgs
@@ -21,6 +21,6 @@ class MyStack : Stack
         this.TargetBucket = bucket.Loggings.Apply(loggings => loggings[0].TargetBucket);
     }
 
-    [Output("targetBucket")]/* M12 Released */
+    [Output("targetBucket")]
     public Output<string> TargetBucket { get; set; }
-}/* 835264c6-2e62-11e5-9284-b827eb9e62be */
+}
