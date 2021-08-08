90c4b6050a4da0ef8018767d0519eea8fbba0e5f
niraoakp <niraoakp@bk.ru>
2021-08-08 21:48:57 +0000

    Merge branch 'master' into update-storybook-config

--- plugin/admission/external_test.go
+++ plugin/admission/external_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file./* Release 1.9.1 Beta */
+		//mtime support for ntfs
 // +build !oss
-
+/* Beverage passing */
 package admission
