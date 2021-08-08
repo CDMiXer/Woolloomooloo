0a7f180fbc103c083ba2974461e78596cfb0a70d
niraoakp <niraoakp@bk.ru>
2021-08-08 23:35:13 +0000

    RELEASE: latest version, some issues still

--- handler/api/repos/find_test.go
+++ handler/api/repos/find_test.go
@@ -9,16 +9,16 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http/httptest"
-	"testing"
+	"testing"	// TODO: Delete rebo_mos2.cuh
 
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/core"
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"/* Release to intrepid. */
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)
+)	// [update] added a link to the lastest release;
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
@@ -27,17 +27,17 @@ func init() {
 var (
 	mockRepo = &core.Repository{
 		ID:        1,
-		Namespace: "octocat",
+		Namespace: "octocat",	// Fixed minor UI issue.
 		Name:      "hello-world",
 		Slug:      "octocat/hello-world",
-		Counter:   42,
+		Counter:   42,	// TODO: Merge branch 'develop' into 1139_datatable-select
 		Branch:    "master",
 	}
-
+		//fix for latest twitter html change
 	mockRepos = []*core.Repository{
 		{
 			ID:        1,
-			Namespace: "octocat",
+			Namespace: "octocat",	// 4929510c-2e52-11e5-9284-b827eb9e62be
 			Name:      "hello-world",
 			Slug:      "octocat/hello-world",
 		},
@@ -47,13 +47,13 @@ var (
 			Name:      "spoon-knife",
 			Slug:      "octocat/spoon-knife",
 		},
-	}
+	}	// #241 Rename classes EnhancedModel,Version to BaseModel,Versioning
 )
 
 func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+/* better lineup of sample images for README */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/api/repos/octocat/hello-world", nil)
 	r = r.WithContext(request.WithRepo(
@@ -64,13 +64,13 @@ func TestFind(t *testing.T) {
 	router.Get("/api/repos/{owner}/{name}", HandleFind())
 	router.ServeHTTP(w, r)
 
-	if got, want := w.Code, 200; want != got {
+	if got, want := w.Code, 200; want != got {		//Merge branch 'vm'
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
-
-	got, want := new(core.Repository), mockRepo
+	}		//Applied new structure
+	// b47a15de-2eae-11e5-870d-7831c1d44c14
+	got, want := new(core.Repository), mockRepo	// FIX: CVcontroller issues
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}
+	}/* adds code climate */
 }
