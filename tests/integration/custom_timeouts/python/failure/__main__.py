58b6cdc2772ed4e6a82e7cdf7a81ed0d299e4bfd
niraoakp <niraoakp@bk.ru>
2021-08-08 17:02:33 +0000

    065388d2-2e40-11e5-9284-b827eb9e62be

--- tests/integration/custom_timeouts/python/failure/__main__.py
+++ tests/integration/custom_timeouts/python/failure/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions
-
+/* quick fix that isn't needed but I want to sleep at night */
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
