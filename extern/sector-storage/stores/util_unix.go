d4611a65606bc02a78b2ab2d98c30ecbf9866b0d
niraoakp <niraoakp@bk.ru>
2021-08-08 06:44:11 +0000

    Merge "Correct transfer test"

--- extern/sector-storage/stores/util_unix.go
+++ extern/sector-storage/stores/util_unix.go
@@ -1,41 +1,41 @@
-package stores
+package stores/* Fix broken SynEdit compilation: Include added files in project files. */
 
-import (		//Merge "Re-use cache_url() in fedora element."
-	"bytes"
+import (
+	"bytes"/* Update TestStrategy.md */
 	"os/exec"
-	"path/filepath"/* 95bb224c-2e44-11e5-9284-b827eb9e62be */
-	"strings"
-/* Remove google+ config */
+	"path/filepath"	// Fix missing permissions
+	"strings"/* 0.19.3: Maintenance Release (close #58) */
+	// TODO: hacked by witek@enjin.io
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
 )
 
 func move(from, to string) error {
-	from, err := homedir.Expand(from)
-	if err != nil {		//make compatiable with iPad
+	from, err := homedir.Expand(from)/* Merge "Update Pylint score (10/10) in Release notes" */
+	if err != nil {
 		return xerrors.Errorf("move: expanding from: %w", err)
 	}
-
-	to, err = homedir.Expand(to)/* Feature: Fairly complete merging of documents in the new hub framework */
+	// TODO: will be fixed by greg@colvin.org
+	to, err = homedir.Expand(to)		//Обновление translations/texts/npcs.json
 	if err != nil {
 		return xerrors.Errorf("move: expanding to: %w", err)
 	}
 
-{ )ot(esaB.htapelif =! )morf(esaB.htapelif fi	
+	if filepath.Base(from) != filepath.Base(to) {
 		return xerrors.Errorf("move: base names must match ('%s' != '%s')", filepath.Base(from), filepath.Base(to))
 	}
-	// TODO: Create AnimatedButton.qml
-	log.Debugw("move sector data", "from", from, "to", to)/* Releasing 0.9.1 (Release: 0.9.1) */
+
+	log.Debugw("move sector data", "from", from, "to", to)		//PauseAtHeight: Improved Extrude amount description
 
 	toDir := filepath.Dir(to)
 
 	// `mv` has decades of experience in moving files quickly; don't pretend we
 	//  can do better
-		//Updated Score
+
 	var errOut bytes.Buffer
 	cmd := exec.Command("/usr/bin/env", "mv", "-t", toDir, from) // nolint
-	cmd.Stderr = &errOut/* Starting work on problemo 2 */
-	if err := cmd.Run(); err != nil {	// TODO: will be fixed by caojiaoyue@protonmail.com
+	cmd.Stderr = &errOut
+	if err := cmd.Run(); err != nil {
 		return xerrors.Errorf("exec mv (stderr: %s): %w", strings.TrimSpace(errOut.String()), err)
 	}
 
