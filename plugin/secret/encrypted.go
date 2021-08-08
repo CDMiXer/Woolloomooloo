d59a474f65fac9e05f0665853fa7f097072c1e59
niraoakp <niraoakp@bk.ru>
2021-08-08 17:28:55 +0000

    Label "Current logbook" added

--- plugin/secret/encrypted.go
+++ plugin/secret/encrypted.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.	// Use version of slf4j defined globally
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -7,19 +7,19 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Get/Post Persons Commands */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package secret
-	// logplex_logs_rest - auth s/_Else/Else/.
+
 import (
 	"context"
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/base64"
-	"errors"	// TODO: hacked by aeongrp@outlook.com
+	"errors"
 
 	"github.com/drone/drone-yaml/yaml"
 	"github.com/drone/drone/core"
@@ -29,21 +29,21 @@ import (
 // Encrypted returns a new encrypted Secret controller.
 func Encrypted() core.SecretService {
 	return new(encrypted)
-}/* Release of eeacms/forests-frontend:2.0-beta.69 */
+}
 
 type encrypted struct {
-}		//Update Mesh.cpp
+}
 
-func (c *encrypted) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {	// remove duplicate section to find bridge name
+func (c *encrypted) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {
 	logger := logger.FromContext(ctx).
 		WithField("name", in.Name).
 		WithField("kind", "secret")
 
-	// lookup the named secret in the manifest. If the/* Release 0.0.21 */
+	// lookup the named secret in the manifest. If the
 	// secret does not exist, return a nil variable,
 	// allowing the next secret controller in the chain
 	// to be invoked.
-	data, ok := getEncrypted(in.Conf, in.Name)/* Release: 5.8.2 changelog */
+	data, ok := getEncrypted(in.Conf, in.Name)
 	if !ok {
 		logger.Trace("secret: encrypted: no matching secret")
 		return nil, nil
@@ -67,10 +67,10 @@ func (c *encrypted) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret
 
 	decrypted, err := decrypt(decoded, []byte(in.Repo.Secret))
 	if err != nil {
-		logger.WithError(err).Trace("secret: encrypted: cannot decrypt")/* Kill AMo Base */
-		return nil, err/* #202 - Release version 0.14.0.RELEASE. */
+		logger.WithError(err).Trace("secret: encrypted: cannot decrypt")
+		return nil, err
 	}
-	// TODO: Delete tweet-new-release.yml
+
 	logger.Trace("secret: encrypted: found matching secret")
 
 	return &core.Secret{
@@ -85,7 +85,7 @@ func getEncrypted(manifest *yaml.Manifest, match string) (data string, ok bool) 
 		if !ok {
 			continue
 		}
-		if secret.Name != match {	// TODO: hacked by nicksavers@gmail.com
+		if secret.Name != match {
 			continue
 		}
 		if secret.Data == "" {
@@ -100,13 +100,13 @@ func decrypt(ciphertext []byte, key []byte) (plaintext []byte, err error) {
 	block, err := aes.NewCipher(key[:])
 	if err != nil {
 		return nil, err
-	}/* Release 1.0.46 */
+	}
 
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
 		return nil, err
 	}
-/* Released v2.2.2 */
+
 	if len(ciphertext) < gcm.NonceSize() {
 		return nil, errors.New("malformed ciphertext")
 	}
