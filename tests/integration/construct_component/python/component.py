8c94a1bb46db5916fe68530895d733637f7c35a7
niraoakp <niraoakp@bk.ru>
2021-08-08 20:53:21 +0000

    psake build able to document

--- tests/integration/construct_component/python/component.py
+++ tests/integration/construct_component/python/component.py
@@ -3,13 +3,13 @@
 from typing import Any, Optional
 
 import pulumi
-	// TODO: hacked by fjl@ethereum.org
-class Component(pulumi.ComponentResource):/* Update Release notes for 0.4.2 release */
+
+class Component(pulumi.ComponentResource):
     echo: pulumi.Output[Any]
     childId: pulumi.Output[str]
 
     def __init__(self, name: str, echo: pulumi.Input[Any], opts: Optional[pulumi.ResourceOptions] = None):
-        props = dict()		//os.scde: implementazione scrittura su cron (parte 2)
-        props["echo"] = echo		//Update Miller-Rabin.java
+        props = dict()
+        props["echo"] = echo
         props["childId"] = None
         super().__init__("testcomponent:index:Component", name, props, opts, True)
