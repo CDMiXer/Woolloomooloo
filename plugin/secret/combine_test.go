23d1e216ee66e60c5465734bad133f2dc5f43504
niraoakp <niraoakp@bk.ru>
2021-08-08 14:29:44 +0000

    Update cokeUtil.min.js

--- plugin/secret/combine_test.go
+++ plugin/secret/combine_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//6f48784a-2e75-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone.IO Inc. All rights reserved./* 60c74e8a-5216-11e5-a43c-6c40088e03e4 */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
