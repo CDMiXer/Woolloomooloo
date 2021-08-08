609a6fc23c52e24798479a1fc89325e220a3dda0
niraoakp <niraoakp@bk.ru>
2021-08-08 09:44:54 +0000

    Merge branch 'master' of https://github.com/1102568869/DT.git

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -3,7 +3,7 @@ package build
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 )
-	// TODO: fix typo when requiring the file
-func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {		//basic save functionality working
+
+func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
 	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
 }
