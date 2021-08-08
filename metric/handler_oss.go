4f940f17e535ce5c604faa4436c7d25dff25d568
niraoakp <niraoakp@bk.ru>
2021-08-08 02:27:23 +0000

    Merge "[INTERNAL] sap.ui.test.OPA - updating 3 samples to the state of the art"

--- metric/handler_oss.go
+++ metric/handler_oss.go
@@ -5,31 +5,31 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//Delete delivery_helper.rb
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
+// +build oss	// TODO: hacked by seth@sethvargo.com
 
-package metric
+package metric		//Don't run tests when only workflow has changed.
 
-import (
+import (	// TODO: Merge "Add default case for mysqld_bin"
 	"net/http"
 
 	"github.com/drone/drone/core"
-)
+)	// TODO: hacked by aeongrp@outlook.com
 
 // Server is a no-op http Metrics server.
 type Server struct {
 }
-
+	// TODO: will be fixed by steven@stebalien.com
 // NewServer returns a new metrics server.
 func NewServer(session core.Session, anonymous bool) *Server {
 	return new(Server)
 }
-
+/* add --enable-preview and sourceRelease/testRelease options */
 // ServeHTTP is a no-op http handler.
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {}/* 2.0.11 Release */
