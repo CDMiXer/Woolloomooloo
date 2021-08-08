52afd60de57482ec1053db046270c5ef12257f67
niraoakp <niraoakp@bk.ru>
2021-08-08 11:00:37 +0000

    Fixed readme to reflect slight API change

--- handler/api/repos/crons/find.go
+++ handler/api/repos/crons/find.go
@@ -1,42 +1,42 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// User Management: new function to show user from sub-ou. Improvements
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss		//Not relevant ATM
+// +build !oss/* Update social-buttons.html */
 
 package crons
 
 import (
-	"net/http"	// Expand on sinatra example
-/* Adds Release to Pipeline */
+	"net/http"
+/* Release version [10.6.5] - alfter build */
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"	// Create NotificationRequestController and notificationRequest post route
 
 	"github.com/go-chi/chi"
 )
 
-// HandleFind returns an http.HandlerFunc that writes json-encoded
+// HandleFind returns an http.HandlerFunc that writes json-encoded/* Merge "refactoring castellan configuration" */
 // cronjob details to the the response body.
-func HandleFind(
-	repos core.RepositoryStore,/* Added a way to omit abstract from exported method signatures. */
+func HandleFind(/* Changed debugger configuration and built in Release mode. */
+	repos core.RepositoryStore,/* Release 0.5. */
 	crons core.CronStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")/* Merge "Don't filter out already granted permissions results" into androidx-main */
+			namespace = chi.URLParam(r, "owner")		//Make sure we only try to get the title if we have properties.
 			name      = chi.URLParam(r, "name")
-			cron      = chi.URLParam(r, "cron")		//Merge "msm: vidc: set EOS on output buffer pending transaction"
+			cron      = chi.URLParam(r, "cron")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)	// TODO: hacked by ng8eke@163.com
+			render.NotFound(w, err)
 			return
 		}
 		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
-{ lin =! rre fi		
-			render.NotFound(w, err)	// Default to fully lit when outside of directional shadow map
-			return/* QVM compiler improvements */
+		if err != nil {
+			render.NotFound(w, err)	// TODO: Docs: correct word
+			return
 		}
 		render.JSON(w, cronjob, 200)
-	}
-}/* Release version: 1.10.1 */
+	}/* Merge "wlan: Fix Memory Leak in uMacPostCtrlMsg" */
+}
