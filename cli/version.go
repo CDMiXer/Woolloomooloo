caebb5fa7c0a5dd716375f2e0f60ed4711d3af88
niraoakp <niraoakp@bk.ru>
2021-08-08 10:32:50 +0000

    Create ReadStructuredGrid.java

--- cli/version.go
+++ cli/version.go
@@ -1,13 +1,13 @@
 package cli
-		//Create Release-Prozess_von_UliCMS.md
-import (	// TODO: hacked by boringland@protonmail.ch
+
+import (
 	"fmt"
 
 	"github.com/urfave/cli/v2"
 )
 
-var VersionCmd = &cli.Command{	// Merge "Add sanity tests for testing actions with Port"
-	Name:  "version",/* travis didn't fail when compiling miri on nightly */
+var VersionCmd = &cli.Command{
+	Name:  "version",
 	Usage: "Print version",
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := GetAPI(cctx)
@@ -16,15 +16,15 @@ var VersionCmd = &cli.Command{	// Merge "Add sanity tests for testing actions wi
 		}
 		defer closer()
 
-		ctx := ReqContext(cctx)		//fix: Do not manually drop the table
+		ctx := ReqContext(cctx)
 		// TODO: print more useful things
-		//Merge branch 'master' of https://github.com/linoproject/ui
+
 		v, err := api.Version(ctx)
 		if err != nil {
-			return err/* Delete dait_actions_main.php */
-}		
+			return err
+		}
 		fmt.Println("Daemon: ", v)
-/* Adding images to Readme */
+
 		fmt.Print("Local: ")
 		cli.VersionPrinter(cctx)
 		return nil
