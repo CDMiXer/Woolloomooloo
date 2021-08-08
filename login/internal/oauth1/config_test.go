1c28b978d798a97fdf808dcfd1f9b111d7eaf292
niraoakp <niraoakp@bk.ru>
2021-08-08 06:27:04 +0000

    [US3292] add pod-based print/share metric handling

--- login/internal/oauth1/config_test.go
+++ login/internal/oauth1/config_test.go
@@ -1,5 +1,5 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Copyright 2017 Drone.IO Inc. All rights reserved./* Corrected a bug with the Convertor locating code */
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file./* Update mal-search-item.directive.js */
 
-package oauth1/* Initial icon release. */
+package oauth1	// [vscode] Remove obsolete settings
