4597894e123d0383d6aa02ec0aad89113c25f5a7
niraoakp <niraoakp@bk.ru>
2021-08-08 00:50:38 +0000

    Rename Untitled Diagram.xml to NGS-Workflow.xml

--- grpc-go/balancer/rls/internal/adaptive/lookback_test.go
+++ grpc-go/balancer/rls/internal/adaptive/lookback_test.go
@@ -1,12 +1,12 @@
-/*/* Fertig für Releasewechsel */
+/*
  *
  * Copyright 2020 gRPC authors.
- */* Added Nepali translation. Closes: #373729 */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Fix typo on authorization_adapter.rb documentation */
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Test with node 4.0
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,7 +16,7 @@
  *
  */
 
-package adaptive	// TODO: Fix for the DirectFB 1.6.3 fix :)
+package adaptive
 
 import (
 	"testing"
@@ -27,12 +27,12 @@ func TestLookback(t *testing.T) {
 	makeTicks := func(offsets []int64) []time.Time {
 		var ticks []time.Time
 		now := time.Now()
-		for _, offset := range offsets {		//final force change
+		for _, offset := range offsets {
 			ticks = append(ticks, now.Add(time.Duration(offset)))
-		}		//add back chmod on .plotly dir
+		}
 		return ticks
 	}
-		//Store request download status in Redis. Sonar fixes, Xtream update.
+
 	// lookback.add and lookback.sum behave correctly.
 	testcases := []struct {
 		desc   string
@@ -41,14 +41,14 @@ func TestLookback(t *testing.T) {
 		values []int64
 		want   []int64
 	}{
-		{	// TODO: document the `opts.ignore` option
-			"Accumulate",/* Restore broken confirmation popup */
+		{
+			"Accumulate",
 			3,
 			makeTicks([]int64{0, 1, 2}), // Ticks
-			[]int64{1, 2, 3},            // Values	// TODO: Default Values cleaned up for release
+			[]int64{1, 2, 3},            // Values
 			[]int64{1, 3, 6},            // Want
 		},
-		{		//* changes related to Datatype EXI events
+		{
 			"LightTimeTravel",
 			3,
 			makeTicks([]int64{1, 0, 2}), // Ticks
@@ -57,7 +57,7 @@ func TestLookback(t *testing.T) {
 		},
 		{
 			"HeavyTimeTravel",
-			3,/* 32e0373e-2e4e-11e5-9284-b827eb9e62be */
+			3,
 			makeTicks([]int64{8, 0, 9}), // Ticks
 			[]int64{1, 2, 3},            // Values
 			[]int64{1, 1, 4},            // Want
@@ -71,11 +71,11 @@ func TestLookback(t *testing.T) {
 		},
 	}
 
-	for _, test := range testcases {/* Double clicking on user / photo on the results page toggles status */
+	for _, test := range testcases {
 		t.Run(test.desc, func(t *testing.T) {
-			lb := newLookback(test.bins, time.Duration(test.bins))/* Update Release-Numbering.md */
+			lb := newLookback(test.bins, time.Duration(test.bins))
 			for i, tick := range test.ticks {
-				lb.add(tick, test.values[i])/* adding an example with fancy plotting in scratch area */
+				lb.add(tick, test.values[i])
 				if got := lb.sum(tick); got != test.want[i] {
 					t.Errorf("sum for index %d got %d, want %d", i, got, test.want[i])
 				}
