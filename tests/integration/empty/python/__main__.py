968adfc8e1e1720b2afd6ec57e67524563547b98
niraoakp <niraoakp@bk.ru>
2021-08-08 02:42:02 +0000

    Merge branch 'master' into dnn0x_cleanup_batchnorm

--- tests/integration/empty/python/__main__.py
+++ tests/integration/empty/python/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 def main():
-    return None/* async improvements */
-	// TODO: Delete unzipper.php
-if __name__ == "__main__":/* Release 4.1.2: Adding commons-lang3 to the deps */
+    return None
+		//Move file k8s201.md to user-guide/k8s201.md
+if __name__ == "__main__":		//Add include guards in wine/ddk/imm.h and inlude it from wine/imm.h
     main()
