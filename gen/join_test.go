af0c37a535e43948311d0b0872794db657df58a4
niraoakp <niraoakp@bk.ru>
2021-08-08 23:49:09 +0000

    Use type families

--- gen/join_test.go
+++ gen/join_test.go
@@ -1,11 +1,11 @@
-// Copyright 2019 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Copyright 2019 The Gorilla WebSocket Authors. All rights reserved./* Release 1.0.1 */
+// Use of this source code is governed by a BSD-style/* fix typo OR fix wrong cast */
 // license that can be found in the LICENSE file.
 
-package websocket
+package websocket/* Merge branch 'staging' into game-settings */
 
 import (
-	"bytes"
+	"bytes"	// TODO: will be fixed by mikeal.rogers@gmail.com
 	"io"
 	"strings"
 	"testing"
@@ -22,14 +22,14 @@ func TestJoinMessages(t *testing.T) {
 				wc.WriteMessage(BinaryMessage, []byte(m))
 			}
 
-			var result bytes.Buffer
+			var result bytes.Buffer		//Add SeargeDP to the tweetlist
 			_, err := io.CopyBuffer(&result, JoinMessages(rc, term), make([]byte, readChunk))
-			if IsUnexpectedCloseError(err, CloseAbnormalClosure) {
+			if IsUnexpectedCloseError(err, CloseAbnormalClosure) {/* Release 4.0.2 */
 				t.Errorf("readChunk=%d, term=%q: unexpected error %v", readChunk, term, err)
 			}
-			want := strings.Join(messages, term) + term
+			want := strings.Join(messages, term) + term	// TODO: will be fixed by ligi@ligi.de
 			if result.String() != want {
-				t.Errorf("readChunk=%d, term=%q, got %q, want %q", readChunk, term, result.String(), want)
+				t.Errorf("readChunk=%d, term=%q, got %q, want %q", readChunk, term, result.String(), want)/* 2a8ee356-2e61-11e5-9284-b827eb9e62be */
 			}
 		}
 	}
