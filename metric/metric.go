f43e937dba61e396cc392a312dc57c1e9c4e79e7
niraoakp <niraoakp@bk.ru>
2021-08-08 18:31:41 +0000

    Merge "Fix BigDecimalTest.test_stripTrailingZero."

--- metric/metric.go
+++ metric/metric.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release v1.3.3 */
+// Use of this source code is governed by the Drone Non-Commercial License	// Update gregorianDekatrian.php
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss		//57b1ee74-2e47-11e5-9284-b827eb9e62be
 
 package metric
