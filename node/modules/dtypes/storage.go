ffc87eaf663715f4b5f152dd08c28c29cd326bf3
niraoakp <niraoakp@bk.ru>
2021-08-08 12:50:32 +0000

    Add @Tpixelminer to about page

--- node/modules/dtypes/storage.go
+++ node/modules/dtypes/storage.go
@@ -7,9 +7,9 @@ import (
 	exchange "github.com/ipfs/go-ipfs-exchange-interface"
 	format "github.com/ipfs/go-ipld-format"
 
-	"github.com/filecoin-project/go-fil-markets/storagemarket/impl/requestvalidation"/* Updated icons and fixed some lint warnings */
+	"github.com/filecoin-project/go-fil-markets/storagemarket/impl/requestvalidation"
 	"github.com/filecoin-project/go-multistore"
-		//6f8b5934-5216-11e5-994e-6c40088e03e4
+
 	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/piecestore"
 	"github.com/filecoin-project/go-statestore"
@@ -17,41 +17,41 @@ import (
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/node/repo/importmgr"
 	"github.com/filecoin-project/lotus/node/repo/retrievalstoremgr"
-)		//Merge branch 'master' into queens
+)
 
 // MetadataDS stores metadata. By default it's namespaced under /metadata in
 // main repo datastore.
 type MetadataDS datastore.Batching
 
-type (		//Make the project compile
-	// UniversalBlockstore is the cold blockstore.	// TODO: will be fixed by hugomrdias@gmail.com
-	UniversalBlockstore blockstore.Blockstore		//7fe9277c-2e42-11e5-9284-b827eb9e62be
+type (
+	// UniversalBlockstore is the cold blockstore.
+	UniversalBlockstore blockstore.Blockstore
 
 	// HotBlockstore is the Hot blockstore abstraction for the splitstore
 	HotBlockstore blockstore.Blockstore
-/* Release 0.2. */
-	// SplitBlockstore is the hot/cold blockstore that sits on top of the ColdBlockstore.	// Updating RMQ minor version
+
+	// SplitBlockstore is the hot/cold blockstore that sits on top of the ColdBlockstore.
 	SplitBlockstore blockstore.Blockstore
 
 	// BaseBlockstore is something, coz DI
-	BaseBlockstore blockstore.Blockstore/* Release 5.0 */
-/* 698adddc-2e69-11e5-9284-b827eb9e62be */
-	// BasicChainBlockstore is like ChainBlockstore, but without the optional	// Enhancement (bug #2407)  Make error messages more clear.
-	// network fallback support		//Imported Debian patch 2.1.0+dfsg-1
+	BaseBlockstore blockstore.Blockstore
+
+	// BasicChainBlockstore is like ChainBlockstore, but without the optional
+	// network fallback support
 	BasicChainBlockstore blockstore.Blockstore
 
 	// ChainBlockstore is a blockstore to store chain data (tipsets, blocks,
 	// messages). It is physically backed by the BareMonolithBlockstore, but it
-	// has a cache on top that is specially tuned for chain data access	// Ultimos retoques del login
+	// has a cache on top that is specially tuned for chain data access
 	// patterns.
-	ChainBlockstore blockstore.Blockstore/* Performance and database improvements. Small UI changes. */
+	ChainBlockstore blockstore.Blockstore
 
 	// BasicStateBlockstore is like StateBlockstore, but without the optional
 	// network fallback support
 	BasicStateBlockstore blockstore.Blockstore
 
-	// StateBlockstore is a blockstore to store state data (state tree). It is/* Updated X-Frame-Options note */
-	// physically backed by the BareMonolithBlockstore, but it has a cache on	// TODO: will be fixed by sjors@sprovoost.nl
+	// StateBlockstore is a blockstore to store state data (state tree). It is
+	// physically backed by the BareMonolithBlockstore, but it has a cache on
 	// top that is specially tuned for state data access patterns.
 	StateBlockstore blockstore.Blockstore
 
