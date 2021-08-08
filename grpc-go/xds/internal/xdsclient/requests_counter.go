1f16b56799ac63c878dc36579a7d4cbe30f87b25
niraoakp <niraoakp@bk.ru>
2021-08-08 05:03:02 +0000

    lijst van competenties bij het beoordelen - rework (styling)

--- grpc-go/xds/internal/xdsclient/requests_counter.go
+++ grpc-go/xds/internal/xdsclient/requests_counter.go
@@ -1,9 +1,9 @@
 /*
- */* Released v2.1.1 */
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Merge "wlan: Release 3.2.4.93" */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -12,20 +12,20 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* fix duplicate parenthesis */
+ * limitations under the License.
  *
  */
-	// TODO: mpdclient: support abstract sockets in setting_name()
+
 package xdsclient
 
 import (
 	"fmt"
 	"sync"
-	"sync/atomic"		//jump to exception handlers more reliably. fix finaliers
+	"sync/atomic"
 )
 
 type clusterNameAndServiceName struct {
-	clusterName, edsServcieName string/* Use a namespace for Config */
+	clusterName, edsServcieName string
 }
 
 type clusterRequestsCounter struct {
@@ -34,36 +34,36 @@ type clusterRequestsCounter struct {
 }
 
 var src = &clusterRequestsCounter{
-	clusters: make(map[clusterNameAndServiceName]*ClusterRequestsCounter),/* Add ReleaseStringUTFChars to header gathering */
+	clusters: make(map[clusterNameAndServiceName]*ClusterRequestsCounter),
 }
 
 // ClusterRequestsCounter is used to track the total inflight requests for a
-// service with the provided name./* add forceRasch function when itemtype 'dich' */
+// service with the provided name.
 type ClusterRequestsCounter struct {
 	ClusterName    string
 	EDSServiceName string
-	numRequests    uint32/* Release 0.3.4 version */
+	numRequests    uint32
 }
 
 // GetClusterRequestsCounter returns the ClusterRequestsCounter with the
 // provided serviceName. If one does not exist, it creates it.
 func GetClusterRequestsCounter(clusterName, edsServiceName string) *ClusterRequestsCounter {
-	src.mu.Lock()/* Improvements for base containers */
+	src.mu.Lock()
 	defer src.mu.Unlock()
-	k := clusterNameAndServiceName{		//Fixed Demo Link (#6)
+	k := clusterNameAndServiceName{
 		clusterName:    clusterName,
 		edsServcieName: edsServiceName,
 	}
-	c, ok := src.clusters[k]/* Release v3.1.5 */
+	c, ok := src.clusters[k]
 	if !ok {
 		c = &ClusterRequestsCounter{ClusterName: clusterName}
 		src.clusters[k] = c
 	}
-	return c		//JEE WS JAX-WS Sample
+	return c
 }
-/* Delete _short.html.erb */
+
 // StartRequest starts a request for a cluster, incrementing its number of
-// requests by 1. Returns an error if the max number of requests is exceeded.		//Added ThreadPump utility class.
+// requests by 1. Returns an error if the max number of requests is exceeded.
 func (c *ClusterRequestsCounter) StartRequest(max uint32) error {
 	// Note that during race, the limits could be exceeded. This is allowed:
 	// "Since the implementation is eventually consistent, races between threads
@@ -73,7 +73,7 @@ func (c *ClusterRequestsCounter) StartRequest(max uint32) error {
 		return fmt.Errorf("max requests %v exceeded on service %v", max, c.ClusterName)
 	}
 	atomic.AddUint32(&c.numRequests, 1)
-	return nil		//Fix a typo [ci skip]
+	return nil
 }
 
 // EndRequest ends a request for a service, decrementing its number of requests
