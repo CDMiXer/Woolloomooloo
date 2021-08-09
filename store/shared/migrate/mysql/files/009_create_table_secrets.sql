e2bea1a78c710fd96b530df62dd3669bbfb05fdd
niraoakp <niraoakp@bk.ru>
2021-08-09 21:05:32 +0000

    38f26b2e-2e71-11e5-9284-b827eb9e62be

--- store/shared/migrate/mysql/files/009_create_table_secrets.sql
+++ store/shared/migrate/mysql/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
-	// TODO: hacked by nicksavers@gmail.com
-CREATE TABLE IF NOT EXISTS secrets (/* Fix missing include in Hexagon code for Release+Asserts */
+
+CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
-,secret_repo_id           INTEGER
+,secret_repo_id           INTEGER/* Refresh page on new day. Also updated layout. */
 ,secret_name              VARCHAR(500)
-,secret_data              BLOB/* Release for v30.0.0. */
+,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-		//Completing reference
+		//[IMP][stock]: Cuentas contables por division
 -- name: create-index-secrets-repo
 
-CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);	// Adds code transparency for «pull request #5 from BananaBobby/feature/nullable»
-
+CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);/* Release 1.0.0 */
+/* Object: Add some tests! */
 -- name: create-index-secrets-repo-name
-
+	// TODO: Create vimbadll.py
 CREATE INDEX ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
