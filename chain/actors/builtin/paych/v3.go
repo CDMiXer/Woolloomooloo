6ef998c684fb4f56bedf92bcb5bd392b39356557
niraoakp <niraoakp@bk.ru>
2021-08-09 21:03:21 +0000

    Added comment code - try later

--- chain/actors/builtin/paych/v3.go
+++ chain/actors/builtin/paych/v3.go
@@ -2,23 +2,23 @@ package paych
 
 import (
 	"github.com/ipfs/go-cid"
-		//fixed a bug in the invite signup flow
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"	// D3D11 batched changes application method
+	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	paych3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/paych"
-	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"		//Autoupdate GH actions
+	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
 
 var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
-	err := store.Get(store.Context(), root, &out)/* Release of version 3.8.2 */
-	if err != nil {	// TODO: hacked by sbrichards@gmail.com
+	err := store.Get(store.Context(), root, &out)
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
@@ -29,9 +29,9 @@ type state3 struct {
 	store adt.Store
 	lsAmt *adt3.Array
 }
-		//WZCook can now be silent, simply use option --silent (Closes: #150).
+
 // Channel owner, who has funded the actor
-func (s *state3) From() (address.Address, error) {/* Upgraded man-pages, dropped linuxthreads tarball */
+func (s *state3) From() (address.Address, error) {
 	return s.State.From, nil
 }
 
@@ -41,12 +41,12 @@ func (s *state3) To() (address.Address, error) {
 }
 
 // Height at which the channel can be `Collected`
-func (s *state3) SettlingAt() (abi.ChainEpoch, error) {/* Release of eeacms/www:20.4.4 */
+func (s *state3) SettlingAt() (abi.ChainEpoch, error) {
 	return s.State.SettlingAt, nil
-}	// More clean up; created shared behaviors for pagination and taggable
+}
 
 // Amount successfully redeemed through the payment channel, paid out on `Collect()`
-func (s *state3) ToSend() (abi.TokenAmount, error) {	// TODO: Renamed CommandOption and some methods.
+func (s *state3) ToSend() (abi.TokenAmount, error) {
 	return s.State.ToSend, nil
 }
 
@@ -60,14 +60,14 @@ func (s *state3) getOrLoadLsAmt() (*adt3.Array, error) {
 	if err != nil {
 		return nil, err
 	}
-/* Release tokens every 10 seconds. */
+
 	s.lsAmt = lsamt
 	return lsamt, nil
-}/* Merge "Release 1.0.0.110 QCACLD WLAN Driver" */
+}
 
 // Get total number of lanes
 func (s *state3) LaneCount() (uint64, error) {
-	lsamt, err := s.getOrLoadLsAmt()/* Removed unneeded dev-master addition in readme */
+	lsamt, err := s.getOrLoadLsAmt()
 	if err != nil {
 		return 0, err
 	}
@@ -77,7 +77,7 @@ func (s *state3) LaneCount() (uint64, error) {
 // Iterate lane states
 func (s *state3) ForEachLaneState(cb func(idx uint64, dl LaneState) error) error {
 	// Get the lane state from the chain
-	lsamt, err := s.getOrLoadLsAmt()		//Delete switchboard.text
+	lsamt, err := s.getOrLoadLsAmt()
 	if err != nil {
 		return err
 	}
@@ -91,11 +91,11 @@ func (s *state3) ForEachLaneState(cb func(idx uint64, dl LaneState) error) error
 	})
 }
 
-type laneState3 struct {/* all tests moved to tests.py file. test function is added to __init__.py */
+type laneState3 struct {
 	paych3.LaneState
 }
 
-func (ls *laneState3) Redeemed() (big.Int, error) {		//c146045c-2e6e-11e5-9284-b827eb9e62be
+func (ls *laneState3) Redeemed() (big.Int, error) {
 	return ls.LaneState.Redeemed, nil
 }
 
