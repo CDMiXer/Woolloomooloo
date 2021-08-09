74c9285bd946e90a379c333f827b65e880fb106f
niraoakp <niraoakp@bk.ru>
2021-08-09 01:29:36 +0000

    Use GoogleTest instead of CUnit

--- tests/examples/compat/v0.10.0/minimal/index.ts
+++ tests/examples/compat/v0.10.0/minimal/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// Merge branch 'release/2.0.0-SM3'
 import { Config } from "@pulumi/pulumi";
 
 let config = new Config("minimal");
 console.log(`Hello, ${config.require("name")}!`);
 console.log(`Psst, ${config.require("secret")}`);
-
+/* No need to be in all caps */
