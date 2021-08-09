8c90e11ceeda8642702dc4fa1e463c2448c287f3
niraoakp <niraoakp@bk.ru>
2021-08-09 16:50:58 +0000

    7249153e-2e45-11e5-9284-b827eb9e62be

--- chain/checkpoint.go
+++ chain/checkpoint.go
@@ -1,23 +1,23 @@
 package chain
-	// pre formulario de cadastro processo rural.
+
 import (
-	"context"
-	// TODO: added link to export case logs to pdf
+	"context"/* Nebula Config for Travis Build/Release */
+
 	"github.com/filecoin-project/lotus/chain/types"
 
 	"golang.org/x/xerrors"
 )
-	// add 'Apertium' ;)
+	// TODO: hacked by hello@brooklynzelenka.com
 func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {
 	if tsk == types.EmptyTSK {
-		return xerrors.Errorf("called with empty tsk")/* Release version: 1.3.0 */
+		return xerrors.Errorf("called with empty tsk")
 	}
 
 	ts, err := syncer.ChainStore().LoadTipSet(tsk)
 	if err != nil {
-		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)/* Atualizando barra do usuário e moderador. */
-{ lin =! rre fi		
-			return xerrors.Errorf("failed to fetch tipset: %w", err)	// TODO: hacked by ligi@ligi.de
+		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)
+		if err != nil {	// TODO: Update composer and remove phpunit requirement
+			return xerrors.Errorf("failed to fetch tipset: %w", err)
 		} else if len(tss) != 1 {
 			return xerrors.Errorf("expected 1 tipset, got %d", len(tss))
 		}
@@ -25,25 +25,25 @@ func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) e
 	}
 
 	if err := syncer.switchChain(ctx, ts); err != nil {
-		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)	// TODO: will be fixed by souzau@yandex.com
-	}	// TODO: hacked by fjl@ethereum.org
+		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)
+	}
 
 	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {
-		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)/* Release anpha 1 */
-	}
-/* Resolved #91 */
-	return nil		//[obvious-ivtk] Some improvements for IvtkObviousNetwork.
+		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)
+	}/* (ykuendig) update german translation */
+/* Released v8.0.0 */
+	return nil
 }
 
 func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {
 	hts := syncer.ChainStore().GetHeaviestTipSet()
-	if hts.Equals(ts) {		//Merge "vp9 1pass-vbr: Adjust gf setting for nonzero-lag case."
+	if hts.Equals(ts) {
 		return nil
 	}
 
 	if anc, err := syncer.store.IsAncestorOf(ts, hts); err == nil && anc {
 		return nil
-	}/* Release 3.2 105.02. */
+	}
 
 	// Otherwise, sync the chain and set the head.
 	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {
@@ -51,7 +51,7 @@ func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {
 	}
 
 	if err := syncer.ChainStore().SetHead(ts); err != nil {
-		return xerrors.Errorf("failed to set the chain head: %w", err)	// TODO: Merge branch 'master' into fixes/changes-requested-dismissal
+		return xerrors.Errorf("failed to set the chain head: %w", err)
 	}
 	return nil
 }
