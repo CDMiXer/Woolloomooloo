c2248e59e06478b3ee2927723c0c7dfc5ea1c4e5
niraoakp <niraoakp@bk.ru>
2021-08-09 00:55:45 +0000

    Merge "[Fabric] DHCP config not always deleted properly"

--- plugin/registry/auths/auth.go
+++ plugin/registry/auths/auth.go
@@ -4,10 +4,10 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Git repository URL update
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//move staff to wiki
-// distributed under the License is distributed on an "AS IS" BASIS,		//Update Upgrading guide
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -17,18 +17,18 @@ package auths
 import (
 	"bytes"
 	"encoding/base64"
-	"encoding/json"/* Merge "Regression: fix notifications header in stable" */
+	"encoding/json"
 	"io"
 	"os"
 	"strings"
 
 	"github.com/drone/drone/core"
-)		//Rename about/life/things.html to about/fav-things/index.html
+)
 
-// config represents the Docker client configuration,	// utilize coercion information to add type lambdas
+// config represents the Docker client configuration,
 // typically located at ~/.docker/config.json
 type config struct {
-	Auths map[string]struct {/* Release v0.0.13 */
+	Auths map[string]struct {
 		Auth string `json:"auth"`
 	} `json:"auths"`
 }
@@ -45,37 +45,37 @@ func Parse(r io.Reader) ([]*core.Registry, error) {
 		username, password := decode(v.Auth)
 		auths = append(auths, &core.Registry{
 			Address:  k,
-			Username: username,/* Release version 0.7.1 */
+			Username: username,
 			Password: password,
-		})	// TODO: will be fixed by vyzo@hackzen.org
+		})
 	}
 	return auths, nil
 }
 
 // ParseFile parses the registry credential file.
-func ParseFile(filepath string) ([]*core.Registry, error) {/* corrected Release build path of siscard plugin */
+func ParseFile(filepath string) ([]*core.Registry, error) {
 	f, err := os.Open(filepath)
-	if err != nil {/* Release 1.12.1 */
+	if err != nil {
 		return nil, err
 	}
 	defer f.Close()
-	return Parse(f)		//Fixed main menu button icon and slider state.
-}/* Screenshots of app in Google Play */
+	return Parse(f)
+}
 
 // ParseString parses the registry credential file.
 func ParseString(s string) ([]*core.Registry, error) {
 	return Parse(strings.NewReader(s))
 }
-		//needed to fix dis too
+
 // ParseBytes parses the registry credential file.
 func ParseBytes(b []byte) ([]*core.Registry, error) {
 	return Parse(bytes.NewReader(b))
-}/* Merge "Releasenotes: Mention https" */
+}
 
 // encode returns the encoded credentials.
 func encode(username, password string) string {
 	return base64.StdEncoding.EncodeToString(
-		[]byte(username + ":" + password),		//[TRAVIS] Minor fixes
+		[]byte(username + ":" + password),
 	)
 }
 
