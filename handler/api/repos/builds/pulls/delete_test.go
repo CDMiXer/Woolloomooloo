d5c2e1fb253453305370b1d29f44f7b6854e8ca0
niraoakp <niraoakp@bk.ru>
2021-08-09 16:56:06 +0000

    Patched downloader.php to work with MySQL 5.7

--- handler/api/repos/builds/pulls/delete_test.go
+++ handler/api/repos/builds/pulls/delete_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Improved copyright detection with trailing "Released" word */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by mowrain@yandex.com
+
 package pulls
