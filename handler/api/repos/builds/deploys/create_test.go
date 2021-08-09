22e52a669c1413413476deed4a3d905123b36d83
niraoakp <niraoakp@bk.ru>
2021-08-09 17:51:04 +0000

    Modificação arquivo token

--- handler/api/repos/builds/deploys/create_test.go
+++ handler/api/repos/builds/deploys/create_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Now error message for invalid email, ip or url are human-readable. */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Released MotionBundler v0.1.6 */
+// Use of this source code is governed by the Drone Non-Commercial License/* Release 2.1.0: All Liquibase settings are available via configuration */
 // that can be found in the LICENSE file.
 
 package deploys
