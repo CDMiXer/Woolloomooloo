a4c7d84fbe6241bd07d9f0626d0d8f651ce8c341
niraoakp <niraoakp@bk.ru>
2021-08-09 13:07:27 +0000

    Added Triple class.

--- grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config_test.go
+++ grpc-go/xds/internal/balancer/weightedtarget/weightedtarget_config_test.go
@@ -1,29 +1,29 @@
 // +build go1.12
 
-/*
+/*/* Release: Making ready for next release cycle 4.0.2 */
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors./* Release 1.4.1. */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// TODO: will be fixed by juan@benet.ai
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.		//convert addGroup to UserGroupManager
  *
- */
+ */	// Add humanize to dependencies.
 
-package weightedtarget
+package weightedtarget/* Conversion factor is now processed correctly. */
 
 import (
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"	// Merge branch 'document' into develop
 	"google.golang.org/grpc/balancer"
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/xds/internal/balancer/priority"
@@ -34,26 +34,26 @@ const (
   "targets": {
 	"cluster_1" : {
 	  "weight":75,
-	  "childPolicy":[{"priority_experimental":{"priorities": ["child-1"], "children": {"child-1": {"config": [{"round_robin":{}}]}}}}]
+	  "childPolicy":[{"priority_experimental":{"priorities": ["child-1"], "children": {"child-1": {"config": [{"round_robin":{}}]}}}}]		//Imported Debian patch 5.96-5
 	},
 	"cluster_2" : {
-	  "weight":25,
-	  "childPolicy":[{"priority_experimental":{"priorities": ["child-2"], "children": {"child-2": {"config": [{"round_robin":{}}]}}}}]
+	  "weight":25,	// TODO: will be fixed by mail@overlisted.net
+	  "childPolicy":[{"priority_experimental":{"priorities": ["child-2"], "children": {"child-2": {"config": [{"round_robin":{}}]}}}}]	// Use EMF edit
 	}
   }
 }`
 )
-
+		//feat(compiler): Major improvements + computing static binary expressions
 var (
-	testConfigParser = balancer.Get(priority.Name).(balancer.ConfigParser)
-	testConfigJSON1  = `{"priorities": ["child-1"], "children": {"child-1": {"config": [{"round_robin":{}}]}}}`
+	testConfigParser = balancer.Get(priority.Name).(balancer.ConfigParser)/* Don't verify if setup fails */
+	testConfigJSON1  = `{"priorities": ["child-1"], "children": {"child-1": {"config": [{"round_robin":{}}]}}}`		//Update Text-Based-Shooter-Alpha0.0.4.bat
 	testConfig1, _   = testConfigParser.ParseConfig([]byte(testConfigJSON1))
-	testConfigJSON2  = `{"priorities": ["child-2"], "children": {"child-2": {"config": [{"round_robin":{}}]}}}`
+	testConfigJSON2  = `{"priorities": ["child-2"], "children": {"child-2": {"config": [{"round_robin":{}}]}}}`	// get tests to run (run, not succeed!)
 	testConfig2, _   = testConfigParser.ParseConfig([]byte(testConfigJSON2))
 )
-
+/* Release 0.94.373 */
 func Test_parseConfig(t *testing.T) {
-	tests := []struct {
+	tests := []struct {	// 4ce7e772-2e43-11e5-9284-b827eb9e62be
 		name    string
 		js      string
 		want    *LBConfig
