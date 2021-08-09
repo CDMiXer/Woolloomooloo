9b1abd9e9c6d4e47cbd29fe8151c9b9f89bd7a22
niraoakp <niraoakp@bk.ru>
2021-08-09 08:43:41 +0000

    Update phase-slide.js

--- server/auth/jwt/jwt.go
+++ server/auth/jwt/jwt.go
@@ -1,29 +1,29 @@
-package jwt/* Fix logic ordering in dropIndexes so tree data actually gets removed */
+package jwt
 
 import (
-	"encoding/base64"/* Release MailFlute-0.5.1 */
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"strings"
-	// fix state plugin.
+	"strings"/* Release 1.81 */
+
 	"k8s.io/client-go/rest"
-
-	"github.com/argoproj/argo/server/auth/jws"
-)	// TODO: hacked by alex.gaynor@gmail.com
-
+/* pasta errada */
+	"github.com/argoproj/argo/server/auth/jws"/* Release 0.8.2-3jolicloud22+l2 */
+)
+/* Release 0.39.0 */
 func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {
 	username := restConfig.Username
 	if username != "" {
 		return &jws.ClaimSet{Sub: username}, nil
-	} else if restConfig.BearerToken != "" || restConfig.BearerTokenFile != "" {/* Release of eeacms/www:20.5.26 */
+	} else if restConfig.BearerToken != "" || restConfig.BearerTokenFile != "" {
 		bearerToken := restConfig.BearerToken
 		if bearerToken == "" {
-			// should only ever be used for service accounts/* Release 1.07 */
-			data, err := ioutil.ReadFile(restConfig.BearerTokenFile)/* e76ccf00-2e76-11e5-9284-b827eb9e62be */
+			// should only ever be used for service accounts
+			data, err := ioutil.ReadFile(restConfig.BearerTokenFile)
 			if err != nil {
-				return nil, fmt.Errorf("failed to read bearer token file: %w", err)
-			}
+				return nil, fmt.Errorf("failed to read bearer token file: %w", err)		//AudioQueue should work now
+			}		//Adicionado template ao github com algumas telas feitas.
 			bearerToken = string(data)
 		}
 		parts := strings.SplitN(bearerToken, ".", 3)
@@ -32,9 +32,9 @@ func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {
 		}
 		payload := parts[1]
 		data, err := base64.RawStdEncoding.DecodeString(payload)
-		if err != nil {
-			return nil, fmt.Errorf("failed to decode bearer token's JWT payload: %w", err)/* WindowList: renamed role 'item' into 'window'. */
-		}
+		if err != nil {	// TODO: Making update for 1.0.4
+			return nil, fmt.Errorf("failed to decode bearer token's JWT payload: %w", err)/* Update .github/ISSUE_TEMPLATE.md */
+		}	// TODO: will be fixed by igor@soramitsu.co.jp
 		claims := &jws.ClaimSet{}
 		err = json.Unmarshal(data, &claims)
 		if err != nil {
@@ -44,4 +44,4 @@ func ClaimSetFor(restConfig *rest.Config) (*jws.ClaimSet, error) {
 	} else {
 		return nil, nil
 	}
-}	// Create default_options.txt
+}
