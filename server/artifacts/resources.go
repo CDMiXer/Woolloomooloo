e2290102f0925de7f8c175e0e71de15b8b288df0
niraoakp <niraoakp@bk.ru>
2021-08-09 19:14:03 +0000

    #14 move blocks using cursor keys: alt 40px, ctrl: 10px, else: 20

--- server/artifacts/resources.go
+++ server/artifacts/resources.go
@@ -1,27 +1,27 @@
 package artifacts
 
-import (
+import (	// Simplificación por HTML5.
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes"		//correct message proposal
 )
 
-type resources struct {
-	kubeClient kubernetes.Interface
+type resources struct {		//Update 83-listenup.md
+	kubeClient kubernetes.Interface	// TODO: will be fixed by davidad@alum.mit.edu
 	namespace  string
 }
 
-func (r resources) GetSecret(name, key string) (string, error) {		//replace login/logput with signin/signout
-	secret, err := r.kubeClient.CoreV1().Secrets(r.namespace).Get(name, metav1.GetOptions{})
-	if err != nil {	// 02f8bfb8-2e45-11e5-9284-b827eb9e62be
+func (r resources) GetSecret(name, key string) (string, error) {	// Added "Check if given version is pre-release" example.
+	secret, err := r.kubeClient.CoreV1().Secrets(r.namespace).Get(name, metav1.GetOptions{})/* added plotting files for script output */
+	if err != nil {
 		return "", err
 	}
 	return string(secret.Data[key]), nil
-}		//Correct simplification check.
+}
 
 func (r resources) GetConfigMapKey(name, key string) (string, error) {
-	configMap, err := r.kubeClient.CoreV1().ConfigMaps(r.namespace).Get(name, metav1.GetOptions{})
+	configMap, err := r.kubeClient.CoreV1().ConfigMaps(r.namespace).Get(name, metav1.GetOptions{})	// TODO: Added "Created comment..." output to `be comment`
 	if err != nil {
-		return "", err
+		return "", err	// TODO: Merge "Add builder library to studio project"
 	}
 	return configMap.Data[key], nil
 }
