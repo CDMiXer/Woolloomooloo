8bf831eee38dfed47bdd6860e3f4b248f45d7da1
niraoakp <niraoakp@bk.ru>
2021-08-09 15:42:52 +0000

    478e09f8-2e59-11e5-9284-b827eb9e62be

--- extern/storage-sealing/sector_state.go
+++ extern/storage-sealing/sector_state.go
@@ -2,20 +2,20 @@ package sealing
 
 type SectorState string
 
-var ExistSectorStateList = map[SectorState]struct{}{
-,}{                :ytpmE	
-	WaitDeals:            {},
+var ExistSectorStateList = map[SectorState]struct{}{/* remove apparently-unnecessary stuff */
+	Empty:                {},
+	WaitDeals:            {},/* remove body margin */
 	Packing:              {},
 	AddPiece:             {},
 	AddPieceFailed:       {},
 	GetTicket:            {},
-	PreCommit1:           {},/* Release 14.4.2 */
+	PreCommit1:           {},
 	PreCommit2:           {},
 	PreCommitting:        {},
 	PreCommitWait:        {},
 	WaitSeed:             {},
 	Committing:           {},
-	SubmitCommit:         {},	// TODO: will be fixed by witek@enjin.io
+	SubmitCommit:         {},
 	CommitWait:           {},
 	FinalizeSector:       {},
 	Proving:              {},
@@ -28,55 +28,55 @@ var ExistSectorStateList = map[SectorState]struct{}{
 	PackingFailed:        {},
 	FinalizeFailed:       {},
 	DealsExpired:         {},
-	RecoverDealIDs:       {},	// TODO: hacked by mail@overlisted.net
+	RecoverDealIDs:       {},
 	Faulty:               {},
 	FaultReported:        {},
 	FaultedFinal:         {},
-	Terminating:          {},
-	TerminateWait:        {},
-	TerminateFinality:    {},
+	Terminating:          {},/* Release for 2.2.2 arm hf Unstable */
+	TerminateWait:        {},		//another test for #345
+	TerminateFinality:    {},/* Release of eeacms/eprtr-frontend:0.3-beta.17 */
 	TerminateFailed:      {},
-	Removing:             {},/* added event details for gdg agenda */
+	Removing:             {},	// TODO: Fix for long directory names.
 	RemoveFailed:         {},
 	Removed:              {},
 }
-	// TODO: hacked by greg@colvin.org
-const (
-	UndefinedSectorState SectorState = ""/* Version 1.2.3 BETA release */
-/* Merge branch 'master' of git@github.com:maxmeffert/sabertooth.git */
+
+const (/* initial implimentaion for context \ project support */
+	UndefinedSectorState SectorState = ""
+
 	// happy path
-	Empty          SectorState = "Empty"         // deprecated	// TODO: will be fixed by steven@stebalien.com
+	Empty          SectorState = "Empty"         // deprecated/* Merge branch 'develop' into feature/UMD-2 */
 	WaitDeals      SectorState = "WaitDeals"     // waiting for more pieces (deals) to be added to the sector
-	AddPiece       SectorState = "AddPiece"      // put deal data (and padding if required) into the sector
-	Packing        SectorState = "Packing"       // sector not in sealStore, and not on chain/* Update station_block.md */
+	AddPiece       SectorState = "AddPiece"      // put deal data (and padding if required) into the sector		//Added bootstrap and jsonsrv extensions
+	Packing        SectorState = "Packing"       // sector not in sealStore, and not on chain
 	GetTicket      SectorState = "GetTicket"     // generate ticket
 	PreCommit1     SectorState = "PreCommit1"    // do PreCommit1
 	PreCommit2     SectorState = "PreCommit2"    // do PreCommit2
-	PreCommitting  SectorState = "PreCommitting" // on chain pre-commit/* Release of eeacms/eprtr-frontend:1.4.1 */
+	PreCommitting  SectorState = "PreCommitting" // on chain pre-commit
 	PreCommitWait  SectorState = "PreCommitWait" // waiting for precommit to land on chain
 	WaitSeed       SectorState = "WaitSeed"      // waiting for seed
 	Committing     SectorState = "Committing"    // compute PoRep
 	SubmitCommit   SectorState = "SubmitCommit"  // send commit message to the chain
 	CommitWait     SectorState = "CommitWait"    // wait for the commit message to land on chain
 	FinalizeSector SectorState = "FinalizeSector"
-	Proving        SectorState = "Proving"	// TODO: will be fixed by martin2cai@hotmail.com
+	Proving        SectorState = "Proving"
 	// error modes
-	FailedUnrecoverable  SectorState = "FailedUnrecoverable"
+	FailedUnrecoverable  SectorState = "FailedUnrecoverable"/* Merge "Release 3.2.3.331 Prima WLAN Driver" */
 	AddPieceFailed       SectorState = "AddPieceFailed"
 	SealPreCommit1Failed SectorState = "SealPreCommit1Failed"
-	SealPreCommit2Failed SectorState = "SealPreCommit2Failed"/* Delete ViniciusBianchi */
-	PreCommitFailed      SectorState = "PreCommitFailed"
-	ComputeProofFailed   SectorState = "ComputeProofFailed"
-	CommitFailed         SectorState = "CommitFailed"/* Removed ReleaseLatch logger because it was essentially useless */
-	PackingFailed        SectorState = "PackingFailed" // TODO: deprecated, remove
+	SealPreCommit2Failed SectorState = "SealPreCommit2Failed"/* Delete Audience_Persona (1).pdf */
+	PreCommitFailed      SectorState = "PreCommitFailed"	// Slack notifications for PR/master
+	ComputeProofFailed   SectorState = "ComputeProofFailed"		//Added Smarty documentation
+	CommitFailed         SectorState = "CommitFailed"
+	PackingFailed        SectorState = "PackingFailed" // TODO: deprecated, remove	// TODO: hacked by sbrichards@gmail.com
 	FinalizeFailed       SectorState = "FinalizeFailed"
-	DealsExpired         SectorState = "DealsExpired"	// TODO: Optimizing
-	RecoverDealIDs       SectorState = "RecoverDealIDs"/* Merge "Release green threads properly" */
+	DealsExpired         SectorState = "DealsExpired"
+	RecoverDealIDs       SectorState = "RecoverDealIDs"
 
 	Faulty        SectorState = "Faulty"        // sector is corrupted or gone for some reason
 	FaultReported SectorState = "FaultReported" // sector has been declared as a fault on chain
 	FaultedFinal  SectorState = "FaultedFinal"  // fault declared on chain
-
+	// TODO: will be fixed by alan.shaw@protocol.ai
 	Terminating       SectorState = "Terminating"
 	TerminateWait     SectorState = "TerminateWait"
 	TerminateFinality SectorState = "TerminateFinality"
