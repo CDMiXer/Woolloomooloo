49dc21a9a6a0727a3babc735f7c5e1f3d4ad423c
niraoakp <niraoakp@bk.ru>
2021-08-09 12:38:20 +0000

    Add link to conference paper

--- login/internal/oauth2/handler_test.go
+++ login/internal/oauth2/handler_test.go
@@ -1,5 +1,5 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.	// TODO: Create update-FruityWifi.sh
-// Use of this source code is governed by a BSD-style/* Merge "pyghmi: Move to singlespec" */
-// license that can be found in the LICENSE file.
-	// UOL: Domainkontrolle in Veranstaltungen deaktiviert
+// Copyright 2017 Drone.IO Inc. All rights reserved./* CUDA-ed POCS stepest descend */
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.	// TODO: Create CompanyDetails.java
+
 package oauth2
