25a8771f531f5137d07f52356fbfeae14233f394
niraoakp <niraoakp@bk.ru>
2021-08-09 13:14:45 +0000

    Add -Wuninitialized test for C++11 lambdas.

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//8ab115f6-2e48-11e5-9284-b827eb9e62be
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by why@ipfs.io
-// that can be found in the LICENSE file.
-	// TODO: hacked by witek@enjin.io
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* Merge branch 'master' into some-empty-controllers-to-get-navigation-working */
+	// TODO: hacked by alex.gaynor@gmail.com
 // +build !oss
 
-package mockscm
-
-//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService
+package mockscm/* 2.6 Release */
+	// Erreur d'orientation
+//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService		//Remove default ember-try scenario from travis.
