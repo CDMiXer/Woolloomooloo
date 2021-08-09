38029dbe84599f1bee1b782e082bdab996d99562
niraoakp <niraoakp@bk.ru>
2021-08-09 16:29:09 +0000

    0d6b30e2-2e64-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/purge_oss.go
+++ handler/api/repos/builds/purge_oss.go
@@ -1,28 +1,28 @@
-// Copyright 2019 Drone IO, Inc.
-//	// Switched production logging to common which has IP's (#421)
+// Copyright 2019 Drone IO, Inc./* Update selaz.py */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+//	// Installation du mod FAQ_MANAGER
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by witek@enjin.io
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release 1.0 Dysnomia */
-// limitations under the License.
-/* Rename dd to base */
+// See the License for the specific language governing permissions and
+// limitations under the License.	// just making sure
+
 // +build oss
-	// TODO: Disabled travis 
+/* go less often against file for latest value and next value */
 package builds
 
 import (
-	"net/http"/* Configured skin for project site */
+	"net/http"
 
-	"github.com/drone/drone/core"		//Merge branch 'master' into fix-server-exception
-)/* Release version 2.30.0 */
-
+	"github.com/drone/drone/core"
+)	// Travis: disabling osx tests for now
+/* SAE-411 Release 1.0.4 */
 // HandlePurge returns a non-op http.HandlerFunc.
-func HandlePurge(core.RepositoryStore, core.BuildStore) http.HandlerFunc {	// TODO: hacked by cory@protocol.ai
+func HandlePurge(core.RepositoryStore, core.BuildStore) http.HandlerFunc {
 	return notImplemented
 }
