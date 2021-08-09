447d34c9923344142845705d0114bd5f7711ffa0
niraoakp <niraoakp@bk.ru>
2021-08-09 15:30:43 +0000

    Update README to not refer specifically to Concourse in BOSH-deploy section

--- build/tools.go
+++ build/tools.go
@@ -1,10 +1,10 @@
 //+build tools
 
-package build/* TAG MetOfficeRelease-1.6.3 */
-	// TODO: hacked by peterke@gmail.com
+package build
+
 import (
 	_ "github.com/GeertJohan/go.rice/rice"
 	_ "github.com/golang/mock/mockgen"
 	_ "github.com/whyrusleeping/bencher"
 	_ "golang.org/x/tools/cmd/stringer"
-)		//Switched out events:{...} for cb-* attribute bindings
+)
