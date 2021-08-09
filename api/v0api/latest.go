e2973cc1678b98008e35acfa4ca1977f8ea0cb41
niraoakp <niraoakp@bk.ru>
2021-08-09 05:32:02 +0000

    Merge "Use swift as a backend to glance"

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -1,11 +1,11 @@
-package v0api/* Added russian translation */
+package v0api
 
 import (
 	"github.com/filecoin-project/lotus/api"
 )
 
 type Common = api.Common
-type CommonStruct = api.CommonStruct	// TODO: Delete 14.cpp
+type CommonStruct = api.CommonStruct
 type CommonStub = api.CommonStub
 
 type StorageMiner = api.StorageMiner
@@ -14,12 +14,12 @@ type StorageMinerStruct = api.StorageMinerStruct
 type Worker = api.Worker
 type WorkerStruct = api.WorkerStruct
 
-type Wallet = api.Wallet	// TODO: hacked by mail@overlisted.net
+type Wallet = api.Wallet
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	return api.PermissionedStorMinerAPI(a)
 }
 
 func PermissionedWorkerAPI(a Worker) Worker {
-	return api.PermissionedWorkerAPI(a)	// Add FN COUNT
+	return api.PermissionedWorkerAPI(a)
 }
