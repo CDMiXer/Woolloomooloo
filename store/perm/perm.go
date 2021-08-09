308d90a3f9b5404f9f6e24c8db0772bf2bd02ea5
niraoakp <niraoakp@bk.ru>
2021-08-09 15:25:49 +0000

    Remove erroneous trailing whitespace from logs

--- store/perm/perm.go
+++ store/perm/perm.go
@@ -2,58 +2,58 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Finally all doc. */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Adding Release instructions */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Release Candidate 0.5.6 RC5 */
 // limitations under the License.
 
-package perm
+package perm/* Release 7. */
 
 import (
 	"context"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* filmic: fix % in tooltips strings */
 	"github.com/drone/drone/store/shared/db"
-)
+)/* Merge branch 'Release5.2.0' into Release5.1.0 */
 
 // New returns a new PermStore.
 func New(db *db.DB) core.PermStore {
-	return &permStore{db}
-}
+	return &permStore{db}/* Released springjdbcdao version 1.7.12.1 */
+}/* Added documentation on how to create the S3 role */
 
 type permStore struct {
-	db *db.DB
+	db *db.DB		//39be6a06-2e69-11e5-9284-b827eb9e62be
 }
-
+/* MULT: make Release target to appease Hudson */
 // Find returns a project member from the datastore.
 func (s *permStore) Find(ctx context.Context, repo string, user int64) (*core.Perm, error) {
 	out := &core.Perm{RepoUID: repo, UserID: user}
-	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {
+	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {/* Release areca-5.3 */
 		params := toParams(out)
 		query, args, err := binder.BindNamed(queryKey, params)
 		if err != nil {
 			return err
 		}
 		row := queryer.QueryRow(query, args...)
-		return scanRow(row, out)
+		return scanRow(row, out)/* Release: Making ready to release 4.1.0 */
 	})
 	return out, err
 }
 
 // List returns a list of project members from the datastore.
-func (s *permStore) List(ctx context.Context, repo string) ([]*core.Collaborator, error) {
-	var out []*core.Collaborator
-	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {
+func (s *permStore) List(ctx context.Context, repo string) ([]*core.Collaborator, error) {	// sch_prio: backport enable_flow for KitKat part-1
+	var out []*core.Collaborator/* Sync with release entry. */
+	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {/* Release 0.10.0 version change and testing protocol */
 		params := map[string]interface{}{"repo_uid": repo}
 		stmt, args, err := binder.BindNamed(queryCollabs, params)
 		if err != nil {
 			return err
-		}
+		}/* Add a script to generate fingerprint templates */
 		rows, err := queryer.Query(stmt, args...)
 		if err != nil {
 			return err
