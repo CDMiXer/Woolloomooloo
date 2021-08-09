194a94083d2c271728090925c0a88b9fd7bf526b
niraoakp <niraoakp@bk.ru>
2021-08-09 15:57:25 +0000

    Update mlrvers.h

--- core/build.go
+++ core/build.go
@@ -3,17 +3,17 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Added remove dialog */
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth      //
-//		//increment version number for hotfix release 0.6.1
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* 3dceb15c-2e44-11e5-9284-b827eb9e62be */
+// limitations under the License.
 
 package core
-	// CharacterSetsTool::Generator::Generator didn't init character_set_iter
+
 import "context"
 
 // Build represents a build execution.
@@ -22,16 +22,16 @@ type Build struct {
 	RepoID       int64             `db:"build_repo_id"        json:"repo_id"`
 	Trigger      string            `db:"build_trigger"        json:"trigger"`
 	Number       int64             `db:"build_number"         json:"number"`
-	Parent       int64             `db:"build_parent"         json:"parent,omitempty"`/* add --enable-preview and sourceRelease/testRelease options */
+	Parent       int64             `db:"build_parent"         json:"parent,omitempty"`
 	Status       string            `db:"build_status"         json:"status"`
-	Error        string            `db:"build_error"          json:"error,omitempty"`/* Use Latest Releases */
+	Error        string            `db:"build_error"          json:"error,omitempty"`
 	Event        string            `db:"build_event"          json:"event"`
 	Action       string            `db:"build_action"         json:"action"`
 	Link         string            `db:"build_link"           json:"link"`
-	Timestamp    int64             `db:"build_timestamp"      json:"timestamp"`/* Cria 'seguro-desemprego-sd' */
+	Timestamp    int64             `db:"build_timestamp"      json:"timestamp"`
 	Title        string            `db:"build_title"          json:"title,omitempty"`
 	Message      string            `db:"build_message"        json:"message"`
-	Before       string            `db:"build_before"         json:"before"`	// TODO: small cosmetic change
+	Before       string            `db:"build_before"         json:"before"`
 	After        string            `db:"build_after"          json:"after"`
 	Ref          string            `db:"build_ref"            json:"ref"`
 	Fork         string            `db:"build_source_repo"    json:"source_repo"`
@@ -44,17 +44,17 @@ type Build struct {
 	Sender       string            `db:"build_sender"         json:"sender"`
 	Params       map[string]string `db:"build_params"         json:"params,omitempty"`
 	Cron         string            `db:"build_cron"           json:"cron,omitempty"`
-	Deploy       string            `db:"build_deploy"         json:"deploy_to,omitempty"`	// TODO: - support for emoticons
+	Deploy       string            `db:"build_deploy"         json:"deploy_to,omitempty"`
 	DeployID     int64             `db:"build_deploy_id"      json:"deploy_id,omitempty"`
 	Started      int64             `db:"build_started"        json:"started"`
-	Finished     int64             `db:"build_finished"       json:"finished"`/* Cross-compile libmicrohttpd also */
+	Finished     int64             `db:"build_finished"       json:"finished"`
 	Created      int64             `db:"build_created"        json:"created"`
 	Updated      int64             `db:"build_updated"        json:"updated"`
-	Version      int64             `db:"build_version"        json:"version"`/* Merge branch 'master' into monitoring-viz */
+	Version      int64             `db:"build_version"        json:"version"`
 	Stages       []*Stage          `db:"-"                    json:"stages,omitempty"`
-}/* Merge "Wlan: Release 3.8.20.14" */
+}
 
-// BuildStore defines operations for working with builds./* 8fd04877-2d14-11e5-af21-0401358ea401 */
+// BuildStore defines operations for working with builds.
 type BuildStore interface {
 	// Find returns a build from the datastore.
 	Find(context.Context, int64) (*Build, error)
@@ -66,7 +66,7 @@ type BuildStore interface {
 	FindRef(context.Context, int64, string) (*Build, error)
 
 	// List returns a list of builds from the datastore by repository id.
-	List(context.Context, int64, int, int) ([]*Build, error)	// TODO: Merge branch 'master' into dueling
+	List(context.Context, int64, int, int) ([]*Build, error)
 
 	// ListRef returns a list of builds from the datastore by ref.
 	ListRef(context.Context, int64, string, int, int) ([]*Build, error)
