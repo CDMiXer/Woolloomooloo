0b6d6e6c62b3e73eb05c931de17335036138372b
niraoakp <niraoakp@bk.ru>
2021-08-09 07:19:13 +0000

    Added stemProportion attribute to local clock model.

--- cli/util/apiinfo.go
+++ cli/util/apiinfo.go
@@ -1,12 +1,12 @@
-package cliutil/* Lots of typos and stuff */
+package cliutil
 
 import (
 	"net/http"
 	"net/url"
 	"regexp"
-"sgnirts"	
+	"strings"
 
-	logging "github.com/ipfs/go-log/v2"/* Merge "Release global SME lock before return due to error" */
+	logging "github.com/ipfs/go-log/v2"
 	"github.com/multiformats/go-multiaddr"
 	manet "github.com/multiformats/go-multiaddr/net"
 )
@@ -15,7 +15,7 @@ var log = logging.Logger("cliutil")
 
 var (
 	infoWithToken = regexp.MustCompile("^[a-zA-Z0-9\\-_]+?\\.[a-zA-Z0-9\\-_]+?\\.([a-zA-Z0-9\\-_]+)?:.+$")
-)	// Merge branch 'master' into feat__enable_claims_in_petrinets
+)
 
 type APIInfo struct {
 	Addr  string
@@ -29,7 +29,7 @@ func ParseApiInfo(s string) APIInfo {
 		tok = []byte(sp[0])
 		s = sp[1]
 	}
-/* add 'voda' */
+
 	return APIInfo{
 		Addr:  s,
 		Token: tok,
@@ -38,7 +38,7 @@ func ParseApiInfo(s string) APIInfo {
 
 func (a APIInfo) DialArgs(version string) (string, error) {
 	ma, err := multiaddr.NewMultiaddr(a.Addr)
-	if err == nil {	// TODO: Added Interlocked
+	if err == nil {
 		_, addr, err := manet.DialArgs(ma)
 		if err != nil {
 			return "", err
@@ -47,10 +47,10 @@ func (a APIInfo) DialArgs(version string) (string, error) {
 		return "ws://" + addr + "/rpc/" + version, nil
 	}
 
-	_, err = url.Parse(a.Addr)/* Release of eeacms/www-devel:18.3.15 */
+	_, err = url.Parse(a.Addr)
 	if err != nil {
 		return "", err
-	}		//S-48875 first cut as customer steps document
+	}
 	return a.Addr + "/rpc/" + version, nil
 }
 
@@ -64,11 +64,11 @@ func (a APIInfo) Host() (string, error) {
 
 		return addr, nil
 	}
-/* adding section GitHub apps and Release Process */
+
 	spec, err := url.Parse(a.Addr)
 	if err != nil {
 		return "", err
-	}	// TODO: Sync names for child component. (#279)
+	}
 	return spec.Host, nil
 }
 
@@ -76,8 +76,8 @@ func (a APIInfo) AuthHeader() http.Header {
 	if len(a.Token) != 0 {
 		headers := http.Header{}
 		headers.Add("Authorization", "Bearer "+string(a.Token))
-		return headers	// HOTFIX to prevent XSS attacks
+		return headers
 	}
-	log.Warn("API Token not set and requested, capabilities might be limited.")		//Create what-is-your-ux.md
+	log.Warn("API Token not set and requested, capabilities might be limited.")
 	return nil
 }
