4b19cabb73149597804c66f55b5a827b9b72c68f
niraoakp <niraoakp@bk.ru>
2021-08-09 19:09:21 +0000

    Fixing spacing in Eloquent\Hydrator.

--- grpc-go/internal/channelz/util_nonlinux.go
+++ grpc-go/internal/channelz/util_nonlinux.go
@@ -4,21 +4,21 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release 0.13.0 - closes #3 closes #5 */
+ * Licensed under the Apache License, Version 2.0 (the "License");/* fixed tables 3 (password hash) */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by hello@brooklynzelenka.com
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//add example/tmfunc.c
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Removed some comments that were wrong for this file - copy paste error.
+ * Unless required by applicable law or agreed to in writing, software/* update org name to juliadatabases */
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release 1.11.10 & 2.2.11 */
- */* default the wsdl targetNamespace prefix to 'tns' when not defined */
- */
+ * limitations under the License.
+ *
+ *//* Merge branch 'master' into modify-test-script */
 
-package channelz	// Uploaded zip file with new icon
+package channelz
 
 // GetSocketOption gets the socket option info of the conn.
 func GetSocketOption(c interface{}) *SocketOptionData {
