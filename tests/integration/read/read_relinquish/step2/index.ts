cc8ba8ec41f73dcd12537ce8e22a885da0cfa6ce
niraoakp <niraoakp@bk.ru>
2021-08-09 17:00:48 +0000

    adding easyconfigs: Nextflow-21.03.0.eb

--- tests/integration/read/read_relinquish/step2/index.ts
+++ tests/integration/read/read_relinquish/step2/index.ts
@@ -1,24 +1,24 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* fix 5630: caches from EC shown as offline */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Create IP_Renew.bat
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
-
-// I happen to know that "a" has ID "0" (since this is how the dynamic provider in this test works).
+/* Delete storable.median */
+// I happen to know that "a" has ID "0" (since this is how the dynamic provider in this test works).	// TODO: Removed "not sorted signs" on some fields.
 //
-// Here I "relinquish" control of "a" by doing a resource read, but with an ID that is	// TODO: will be fixed by remco@dutchcoders.io
+// Here I "relinquish" control of "a" by doing a resource read, but with an ID that is		//Fix SQL, apertura da varchar ad int
 // exactly equal to the resource that I already own. The resource will recognize this
 // and not delete "a".
-///* Release v6.3.1 */
-// This test will fail if the engine does try to delete "a", since "a" is protected.
+///* Merge lp:~tangent-org/libmemcached/1.0-build Build: jenkins-Libmemcached-1.0-107 */
+// This test will fail if the engine does try to delete "a", since "a" is protected.		//connected groups to ticket metrics
 const a = new Resource("a", { state: 99 }, { id: "0" });
