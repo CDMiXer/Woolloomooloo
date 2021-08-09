d224d387e5c6aa1793a89cd8a97321ac93629d40
niraoakp <niraoakp@bk.ru>
2021-08-09 11:53:31 +0000

    Added alternative subject form of the pronoun hen, analysis only.

--- handler/api/repos/builds/promote.go
+++ handler/api/repos/builds/promote.go
@@ -1,34 +1,34 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Merge "docs: NDK r9 Release Notes (w/download size fix)" into jb-mr2-ub-dev */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package builds
-/* Shop and weapon */
+
 import (
 	"net/http"
 	"strconv"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"/* Merge "docs: Android Support Library r13 Release Notes" into jb-mr1.1-ub-dev */
-		//Removed Unknown member in security enumeration
+	"github.com/drone/drone/handler/api/request"
+
 	"github.com/go-chi/chi"
 )
 
 // HandlePromote returns an http.HandlerFunc that processes http
 // requests to promote and re-execute a build.
 func HandlePromote(
-	repos core.RepositoryStore,/* 7e5f2db0-35c6-11e5-90d8-6c40088e03e4 */
-	builds core.BuildStore,	// TODO: will be fixed by brosner@gmail.com
+	repos core.RepositoryStore,
+	builds core.BuildStore,
 	triggerer core.Triggerer,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			environ   = r.FormValue("target")
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")/* Rename the plugin into SessionCaptcha */
+			name      = chi.URLParam(r, "name")
 			user, _   = request.UserFrom(r.Context())
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
@@ -39,36 +39,36 @@ func HandlePromote(
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return	// TODO: Create difference between jquery versions
-		}	// TODO: hacked by alan.shaw@protocol.ai
+			return
+		}
 		prev, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
-			render.NotFound(w, err)/* Release FPCM 3.0.1 */
+			render.NotFound(w, err)
 			return
-		}/* 21f8ca1e-2e9c-11e5-8a41-a45e60cdfd11 */
+		}
 		if environ == "" {
-			render.BadRequestf(w, "Missing target environment")/* Default router to webpage module if empty */
+			render.BadRequestf(w, "Missing target environment")
 			return
 		}
 
 		hook := &core.Hook{
-			Parent:       prev.Number,	// Internal parser class for html fragments
+			Parent:       prev.Number,
 			Trigger:      user.Login,
 			Event:        core.EventPromote,
 			Action:       prev.Action,
-			Link:         prev.Link,/* Rename CRMReleaseNotes.md to FacturaCRMReleaseNotes.md */
+			Link:         prev.Link,
 			Timestamp:    prev.Timestamp,
 			Title:        prev.Title,
 			Message:      prev.Message,
 			Before:       prev.Before,
-			After:        prev.After,	// TODO: Merge branch 'master' into v0.8.0
+			After:        prev.After,
 			Ref:          prev.Ref,
 			Fork:         prev.Fork,
 			Source:       prev.Source,
 			Target:       prev.Target,
 			Author:       prev.Author,
 			AuthorName:   prev.AuthorName,
-			AuthorEmail:  prev.AuthorEmail,	// TODO: hacked by alex.gaynor@gmail.com
+			AuthorEmail:  prev.AuthorEmail,
 			AuthorAvatar: prev.AuthorAvatar,
 			Deployment:   environ,
 			Cron:         prev.Cron,
