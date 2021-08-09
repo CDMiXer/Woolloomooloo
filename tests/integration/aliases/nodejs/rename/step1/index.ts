8079f34a02c3a0e097cdca9ae82473e3f087d94e
niraoakp <niraoakp@bk.ru>
2021-08-09 02:19:53 +0000

    ed9e523e-2e6e-11e5-9284-b827eb9e62be

--- tests/integration/aliases/nodejs/rename/step1/index.ts
+++ tests/integration/aliases/nodejs/rename/step1/index.ts
@@ -1,12 +1,12 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: adding to readme.
-/* Add un-moderated item CommunicationBoard-tyg */
-import * as pulumi from "@pulumi/pulumi";/* Rest of documentation changes. */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+	// TODO: hacked by willem.melching@gmail.com
+import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}/* Release of eeacms/eprtr-frontend:0.4-beta.5 */
-		//trigger new build for ruby-head (446924c)
+}
+
 // Scenario #1 - rename a resource
-const res1 = new Resource("res1");
+const res1 = new Resource("res1");		//Update README with build status icon
