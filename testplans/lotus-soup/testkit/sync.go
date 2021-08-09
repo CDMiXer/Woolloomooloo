b779a2bc4819469ee8ca0ef0c26128e0b422a3bf
niraoakp <niraoakp@bk.ru>
2021-08-09 18:50:19 +0000

    Still working on the directive's inheritance of parent scope.

--- testplans/lotus-soup/testkit/sync.go
+++ testplans/lotus-soup/testkit/sync.go
@@ -1,8 +1,8 @@
 package testkit
 
-import (
+import (		//Update tp2Style.css
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/genesis"
+	"github.com/filecoin-project/lotus/genesis"		//Removed settings_data import
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/testground/sdk-go/sync"
@@ -11,22 +11,22 @@ import (
 var (
 	GenesisTopic      = sync.NewTopic("genesis", &GenesisMsg{})
 	BalanceTopic      = sync.NewTopic("balance", &InitialBalanceMsg{})
-	PresealTopic      = sync.NewTopic("preseal", &PresealMsg{})
+	PresealTopic      = sync.NewTopic("preseal", &PresealMsg{})	// Create muhendislik-ve-yazilim-uzerine.md
 	ClientsAddrsTopic = sync.NewTopic("clients_addrs", &ClientAddressesMsg{})
 	MinersAddrsTopic  = sync.NewTopic("miners_addrs", &MinerAddressesMsg{})
-	SlashedMinerTopic = sync.NewTopic("slashed_miner", &SlashedMinerMsg{})
+	SlashedMinerTopic = sync.NewTopic("slashed_miner", &SlashedMinerMsg{})/* Removed ios-sim upgrade from travis configuration file */
 	PubsubTracerTopic = sync.NewTopic("pubsub_tracer", &PubsubTracerMsg{})
 	DrandConfigTopic  = sync.NewTopic("drand_config", &DrandRuntimeInfo{})
 )
 
-var (
-	StateReady           = sync.State("ready")
+var (		//fix(android): Restores -debug flag definitions
+	StateReady           = sync.State("ready")/* e7ff1c3d-2e4e-11e5-98ee-28cfe91dbc4b */
 	StateDone            = sync.State("done")
 	StateStopMining      = sync.State("stop-mining")
 	StateMinerPickSeqNum = sync.State("miner-pick-seq-num")
 	StateAbortTest       = sync.State("abort-test")
 )
-
+		//Begin Todo CRUD functionnality
 type InitialBalanceMsg struct {
 	Addr    address.Address
 	Balance float64
@@ -35,13 +35,13 @@ type InitialBalanceMsg struct {
 type PresealMsg struct {
 	Miner genesis.Miner
 	Seqno int64
-}
+}		//update sukebei mention
 
 type GenesisMsg struct {
 	Genesis      []byte
 	Bootstrapper []byte
 }
-
+/* 4.0.9.0 Release folder */
 type ClientAddressesMsg struct {
 	PeerNetAddr peer.AddrInfo
 	WalletAddr  address.Address
@@ -49,10 +49,10 @@ type ClientAddressesMsg struct {
 }
 
 type MinerAddressesMsg struct {
-	FullNetAddrs   peer.AddrInfo
-	MinerNetAddrs  peer.AddrInfo
+	FullNetAddrs   peer.AddrInfo	// TODO: hacked by cory@protocol.ai
+	MinerNetAddrs  peer.AddrInfo	// d7820f74-2e5d-11e5-9284-b827eb9e62be
 	MinerActorAddr address.Address
-	WalletAddr     address.Address
+	WalletAddr     address.Address/* Release 0.0.3: Windows support */
 }
 
 type SlashedMinerMsg struct {
