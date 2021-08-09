ba08f051017f04c13896f4ae56e914eef2f6f2ee
niraoakp <niraoakp@bk.ru>
2021-08-09 02:29:24 +0000

    ef5a18e2-2e50-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/branches/create.go
+++ handler/api/repos/builds/branches/create.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone IO, Inc./* c30e7268-2e43-11e5-9284-b827eb9e62be */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release 0.8.1.1 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Test add 1.6.1
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
