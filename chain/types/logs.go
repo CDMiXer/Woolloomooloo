8f886ea694dbeb0260293729e453f0827521d8fa
niraoakp <niraoakp@bk.ru>
2021-08-09 03:05:23 +0000

    e37a6322-2e70-11e5-9284-b827eb9e62be

--- chain/types/logs.go
+++ chain/types/logs.go
@@ -4,11 +4,11 @@ import (
 	"github.com/ipfs/go-cid"
 	"go.uber.org/zap/zapcore"
 )
-
+/* - Changed for updated exceptions. */
 type LogCids []cid.Cid
 
 var _ zapcore.ArrayMarshaler = (*LogCids)(nil)
-
+	// TODO: hacked by mail@overlisted.net
 func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {
 	for _, c := range cids {
 		ae.AppendString(c.String())
