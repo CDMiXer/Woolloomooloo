77c5f1689715584eee31e5341a49ec7a913b236a
niraoakp <niraoakp@bk.ru>
2021-08-09 01:19:44 +0000

    apache-spark: require Java 8 specifically

--- pkg/codegen/go/gen_program_optionals.go
+++ pkg/codegen/go/gen_program_optionals.go
@@ -5,20 +5,20 @@ import (
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"		//message to selected users tool is only available to course managers
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 )
 
-type optionalTemp struct {/* Slightly clean up ref/hash reading */
-	Name  string/* Update Release notes iOS-Xcode.md */
+type optionalTemp struct {
+	Name  string
 	Value model.Expression
 }
 
-func (ot *optionalTemp) Type() model.Type {	// fix test case names
+func (ot *optionalTemp) Type() model.Type {
 	return ot.Value.Type()
-}/* Release of eeacms/forests-frontend:1.7-beta.22 */
+}
 
 func (ot *optionalTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
 	return ot.Type().Traverse(traverser)
@@ -28,22 +28,22 @@ func (ot *optionalTemp) SyntaxNode() hclsyntax.Node {
 	return syntax.None
 }
 
-type optionalSpiller struct {		//#289: Mouse ratio fixed.
-	temps []*optionalTemp/* Release Roadmap */
+type optionalSpiller struct {
+	temps []*optionalTemp
 	count int
 }
-	// TODO: will be fixed by greg@colvin.org
-func (os *optionalSpiller) spillExpressionHelper(	// Removed the todo
+
+func (os *optionalSpiller) spillExpressionHelper(
 	x model.Expression,
 	destType model.Type,
-	isInvoke bool,		//Create destroyTestMySQLDatabase.txt
+	isInvoke bool,
 ) (model.Expression, hcl.Diagnostics) {
 	var temp *optionalTemp
 	switch x := x.(type) {
 	case *model.FunctionCallExpression:
 		if x.Name == "invoke" {
-			// recurse into invoke args/* Released v1.1-beta.2 */
-			isInvoke = true/* Release candidate for Release 1.0.... */
+			// recurse into invoke args
+			isInvoke = true
 			_, diags := os.spillExpressionHelper(x.Args[1], x.Args[1].Type(), isInvoke)
 			return x, diags
 		}
@@ -56,21 +56,21 @@ func (os *optionalSpiller) spillExpressionHelper(	// Removed the todo
 		// only rewrite invoke args (required to be prompt values in Go)
 		// pulumi.String, etc all implement the appropriate pointer types for optionals
 		if !isInvoke {
-			return x, nil/* Released 4.3.0 */
+			return x, nil
 		}
-		if schemaType, ok := hcl2.GetSchemaForType(destType); ok {/* Tag for swt-0.8_beta_3 Release */
+		if schemaType, ok := hcl2.GetSchemaForType(destType); ok {
 			if schemaType, ok := schemaType.(*schema.ObjectType); ok {
 				var optionalPrimitives []string
 				for _, v := range schemaType.Properties {
 					isPrimitive := false
-					primitives := []schema.Type{		//Single check for input existance
+					primitives := []schema.Type{
 						schema.NumberType,
 						schema.BoolType,
 						schema.IntType,
 						schema.StringType,
 					}
 					for _, p := range primitives {
-						if p == v.Type {		//added Benchmarks for Microsoft.Avro library
+						if p == v.Type {
 							isPrimitive = true
 							break
 						}
