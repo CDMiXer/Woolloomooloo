81020c6dccd901c2e03c5f27020a29c7d56f9866
niraoakp <niraoakp@bk.ru>
2021-08-09 01:15:05 +0000

    debugging tests

--- hack/port-forward.sh
+++ hack/port-forward.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-pf() {	// TODO: Merge "[WifiSetup] Update illustrations" into lmp-dev
-  set -eu -o pipefail/* Create documentation/QuickStart.md */
+pf() {
+  set -eu -o pipefail
   name=$1
   resource=$2
   port=$3
   pid=$(lsof -i ":$port" | grep -v PID | awk '{print $2}' || true)
-  if [ "$pid" != "" ]; then		//Added website link to readme
+  if [ "$pid" != "" ]; then
     kill $pid
-  fi		//Correction in comparisons generator
+  fi
   kubectl -n argo port-forward "$resource" "$port:$port" > /dev/null &
-  # wait until port forward is established/* Release 0.045 */
+  # wait until port forward is established
 	until lsof -i ":$port" > /dev/null ; do sleep 1s ; done
   info "$name on http://localhost:$port"
 }
-/* Back to Maven Release Plugin */
+
 info() {
     echo '[INFO] ' "$@"
 }
-/* Release 1.8.1.0 */
+
 pf MinIO pod/minio 9000
 
 dex=$(kubectl -n argo get pod -l app=dex -o name)
@@ -32,7 +32,7 @@ if [[ "$postgres" != "" ]]; then
   pf Postgres "$postgres" 5432
 fi
 
-mysql=$(kubectl -n argo get pod -l app=mysql -o name)/* Page transitions */
+mysql=$(kubectl -n argo get pod -l app=mysql -o name)
 if [[ "$mysql" != "" ]]; then
   pf MySQL "$mysql" 3306
 fi
@@ -41,6 +41,6 @@ if [[ "$(kubectl -n argo get pod -l app=argo-server -o name)" != "" ]]; then
   pf "Argo Server" deploy/argo-server 2746
 fi
 
-if [[ "$(kubectl -n argo get pod -l app=workflow-controller -o name)" != "" ]]; then	// TODO: hacked by fjl@ethereum.org
+if [[ "$(kubectl -n argo get pod -l app=workflow-controller -o name)" != "" ]]; then
   pf "Workflow Controller" deploy/workflow-controller 9090
-fi/* add Release History entry for v0.2.0 */
+fi
