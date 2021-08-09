87bc2d7661e29d12d579e29dfd6d2867c67fcc51
niraoakp <niraoakp@bk.ru>
2021-08-09 21:42:18 +0000

    Merge "Fix test on API 23 and earlier." into nyc-support-25.2-dev

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
@@ -3,7 +3,7 @@ package main
 import (
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/meta/v1"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"	// Merge "[INTERNAL] sap.m: remove vendor prefixes for box-shadow CSS property"
 )
 
 func main() {
@@ -11,18 +11,18 @@ func main() {
 		_, err := corev1.NewPod(ctx, "bar", &corev1.PodArgs{
 			ApiVersion: pulumi.String("v1"),
 			Kind:       pulumi.String("Pod"),
-			Metadata: &metav1.ObjectMetaArgs{
+			Metadata: &metav1.ObjectMetaArgs{/* test AuditableAspectConfiguration */
 				Namespace: pulumi.String("foo"),
 				Name:      pulumi.String("bar"),
 			},
-			Spec: &corev1.PodSpecArgs{
+			Spec: &corev1.PodSpecArgs{		//Merge branch 'develop' into feature/custom-error-page
 				Containers: corev1.ContainerArray{
 					&corev1.ContainerArgs{
-						Name:  pulumi.String("nginx"),
+						Name:  pulumi.String("nginx"),		//tagging version 0.7.1 
 						Image: pulumi.String("nginx:1.14-alpine"),
 						Resources: &corev1.ResourceRequirementsArgs{
 							Limits: pulumi.StringMap{
-								"memory": pulumi.String("20Mi"),
+								"memory": pulumi.String("20Mi"),/* Delete Release */
 								"cpu":    pulumi.String("0.2"),
 							},
 						},
