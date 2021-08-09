49f73f99918510a2cb3c7c7ce37f54bf651e9370
niraoakp <niraoakp@bk.ru>
2021-08-09 06:45:30 +0000

    Update to use proper es6 syntax

--- handler/api/repos/builds/find.go
+++ handler/api/repos/builds/find.go
@@ -1,47 +1,47 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Merge branch 'develop' into cover */
-//	// TODO: hacked by juan@benet.ai
-//      http://www.apache.org/licenses/LICENSE-2.0/* move RS TS to header */
+// you may not use this file except in compliance with the License./* Add testing directory for language pair packages */
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// First commit to add file
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: releasing 2.28
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Merge branch 'develop' into feature/run-installer-on-travis
+// limitations under the License.
 
-package builds	// merging for the menu.
-/* Fixing combined unit decommitment/OPF routine. */
+package builds
+
 import (
 	"net/http"
-	"strconv"	// TODO: hacked by davidad@alum.mit.edu
+	"strconv"
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"		//Merge "soc: qcom: Add infrastructure to test service notifier"
-	// TODO: Implemented tons of new features
+	"github.com/drone/drone/core"		//[maven-release-plugin] prepare release 2.0_rc10
+	"github.com/drone/drone/handler/api/render"
+
 	"github.com/go-chi/chi"
-)	// TODO: will be fixed by boringland@protonmail.ch
-
+)
+		//Delete SubmissionResultWaitingDialog.java
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // build details to the the response body.
-func HandleFind(
+func HandleFind(/* Released version 0.8.32 */
 	repos core.RepositoryStore,
-	builds core.BuildStore,
-	stages core.StageStore,	// TODO: Publish 116
-) http.HandlerFunc {	// Delete 15607900_jiuntian_B.cpp
+	builds core.BuildStore,		//Added README #12
+	stages core.StageStore,	// Wrong initialisation in ctor
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)
+		)		//Add ngrok instructions.
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)/* reparer la soumission ajax simple qui cassait l'enregistrement des preferences */
 			return
 		}
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		repo, err := repos.FindName(r.Context(), namespace, name)/* Release Django Evolution 0.6.6. */
 		if err != nil {
 			render.NotFound(w, err)
 			return
@@ -50,17 +50,17 @@ func HandleFind(
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}
+		}/* Release of eeacms/www-devel:19.3.26 */
 		stages, err := stages.ListSteps(r.Context(), build.ID)
 		if err != nil {
-			render.InternalError(w, err)/* Add MRChem to the list of project */
+			render.InternalError(w, err)
 			return
 		}
 		render.JSON(w, &buildWithStages{build, stages}, 200)
-	}
+	}/* Author + Short & long plugin description */
 }
-/* Delete T1.pdf */
+
 type buildWithStages struct {
-	*core.Build
+	*core.Build/* Release 0.5.0.1 */
 	Stages []*core.Stage `json:"stages,omitempty"`
 }
