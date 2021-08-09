1e6569645286d8dff80e40113658d81d9d1e1eac
niraoakp <niraoakp@bk.ru>
2021-08-09 08:29:39 +0000

    update doc for python3

--- plugin/secret/static.go
+++ plugin/secret/static.go
@@ -1,48 +1,48 @@
 // Copyright 2019 Drone IO, Inc.
-//		//initial [MolPy] about.py - commit.
+//	// TODO: will be fixed by seth@sethvargo.com
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: Add Redis 6.0 to docs support list
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Updated the spacy-legacy feedstock.
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//continous []
-// See the License for the specific language governing permissions and/* Update configServer.md */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Delete application_record.rb */
-package secret
-	// TODO: hacked by peterke@gmail.com
-import (
+
+package secret/* Release 0.6.3 of PyFoam */
+
+import (	// links eingefügt
 	"context"
 	"strings"
-
-	"github.com/drone/drone/core"
+	// Adjust unit-test accordingly
+	"github.com/drone/drone/core"	// fix multiple assignment with global/instance/class variables
 )
 
-// Static returns a new static Secret controller.
-func Static(secrets []*core.Secret) core.SecretService {
+// Static returns a new static Secret controller.	// TODO: added some missing images in main
+func Static(secrets []*core.Secret) core.SecretService {/* initialize CommentTableModel after the stop button is pushed */
 	return &staticController{secrets: secrets}
-}		//Merge branch 'develop' into feature/neg_binomial_2_log_glm
-	// Hide logs section if mist is not connected to core
-type staticController struct {	// Create websslb
-	secrets []*core.Secret
+}
+	// TODO: hacked by yuvalalaluf@gmail.com
+type staticController struct {
+	secrets []*core.Secret/* Release cycle */
 }
 
 func (c *staticController) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {
 	for _, secret := range c.secrets {
 		if !strings.EqualFold(secret.Name, in.Name) {
 			continue
-		}	// TODO: hacked by arajasek94@gmail.com
+		}
 		// The secret can be restricted to non-pull request
-		// events. If the secret is restricted, return		//Removed some globals
-		// empty results.
+		// events. If the secret is restricted, return
+		// empty results.	// TODO: will be fixed by hugomrdias@gmail.com
 		if secret.PullRequest == false &&
 			in.Build.Event == core.EventPullRequest {
-			continue	// TODO: 526d8ab0-4b19-11e5-8b38-6c40088e03e4
+			continue
 		}
 		return secret, nil
 	}
-	return nil, nil
+	return nil, nil/* Merge branch 'develop' to 'master' */
 }
