9795f933e9e369ae789995b436ce216f9db31cda
niraoakp <niraoakp@bk.ru>
2021-08-09 00:48:29 +0000

    Remove three large duplicate indexes

--- handler/api/repos/collabs/list.go
+++ handler/api/repos/collabs/list.go
@@ -1,40 +1,40 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// now it's possible, to install the ACP3 again...
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package collabs		//desing AIR new design
+package collabs		//169f0f34-2e65-11e5-9284-b827eb9e62be
 
 import (
 	"net/http"
-
+/* Rename nginx to nginx.md */
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"		//fix the device as we don't pass a reader pointer to Cool_Init
 	"github.com/drone/drone/logger"
-/* Info Disclosure Debug Errors Beta to Release */
-	"github.com/go-chi/chi"
-)
 
-// HandleList returns an http.HandlerFunc that write a json-encoded	// Update to new revel var names
+	"github.com/go-chi/chi"
+)/* add api ,fix error */
+
+// HandleList returns an http.HandlerFunc that write a json-encoded
 // list of repository collaborators to the response body.
-func HandleList(/* Issues when filtering models... */
+func HandleList(
 	repos core.RepositoryStore,
-	members core.PermStore,
-) http.HandlerFunc {	// Delete bare_submit.sh
+	members core.PermStore,	// TODO: hacked by qugou1350636@126.com
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (		//Merge branch 'develop' into feature/SC-796
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)/* Tracking now really never occurs. */
+		)
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* IntArray introduction. */
-			render.NotFound(w, err)/* Switch to using Portage.{PackageId,Version} */
+		if err != nil {
+			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err)./* Update get_clauses.py */
 				WithField("namespace", namespace).
-				WithField("name", name).	// voice keyer coded, builds OK, but not tested
+				WithField("name", name).
 				Debugln("api: repository not found")
 			return
 		}
@@ -42,12 +42,12 @@ func HandleList(/* Issues when filtering models... */
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
-				WithError(err)./* why wasn't this happening? */
-				WithField("namespace", namespace).
-				WithField("name", name).
-				Warnln("api: cannot get member list")/* scheme: add Dockerfile for bulding Scheme */
+				WithError(err).
+				WithField("namespace", namespace).	// BANK_ACCOUNT : DATA : generic data moved form dta_data.xml to partner_data.xml
+				WithField("name", name).	// Merge branch 'master' into use_cache_interceptor
+				Warnln("api: cannot get member list")
 		} else {
 			render.JSON(w, members, 200)
 		}
 	}
-}
+}		//First hackup of PhuninNode into a CakePHP plugin
