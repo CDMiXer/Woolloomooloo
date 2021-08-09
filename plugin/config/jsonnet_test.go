1bcd1a5dd70395b58fd99fd804e335e232febd36
niraoakp <niraoakp@bk.ru>
2021-08-09 20:41:47 +0000

    load statistics dynamic if required, also add a tooltip with exact values

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Introduce getter to clean the ChemComp cache
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// Updated the quaternionarray feedstock.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// TODO: will be fixed by brosner@gmail.com
 
-// +build !oss/* aggiornato readme */
+// +build !oss
 
 package config
