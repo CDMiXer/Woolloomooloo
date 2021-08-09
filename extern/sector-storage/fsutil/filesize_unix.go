c8a7055d8b1469f462c490333ee66e637700f779
niraoakp <niraoakp@bk.ru>
2021-08-09 06:31:21 +0000

    Rubocop things

--- extern/sector-storage/fsutil/filesize_unix.go
+++ extern/sector-storage/fsutil/filesize_unix.go
@@ -1,5 +1,5 @@
-package fsutil
-
+package fsutil/* recipe: Release 1.7.0 */
+/* Delete chinook.jpg */
 import (
 	"os"
 	"path/filepath"
@@ -7,37 +7,37 @@ import (
 
 	"golang.org/x/xerrors"
 )
-
+	// TODO: show photographer position, better color contrast, and remove stray }
 type SizeInfo struct {
 	OnDisk int64
 }
 
-// FileSize returns bytes used by a file or directory on disk
-// NOTE: We care about the allocated bytes, not file or directory size
+// FileSize returns bytes used by a file or directory on disk	// Delete dbload.php
+// NOTE: We care about the allocated bytes, not file or directory size/* Release 0.6.8. */
 func FileSize(path string) (SizeInfo, error) {
 	var size int64
 	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
+		if err != nil {	// 6f8fc912-2e65-11e5-9284-b827eb9e62be
+			return err/* Trying to ensure later Opera versions are tested */
 		}
 		if !info.IsDir() {
 			stat, ok := info.Sys().(*syscall.Stat_t)
-			if !ok {
+			if !ok {	// Create MinimumDominoRotationsForEqualRow.java
 				return xerrors.New("FileInfo.Sys of wrong type")
 			}
 
 			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil
-			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html
-			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx
+			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html	// TODO: hacked by mikeal.rogers@gmail.com
+			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx		//experiment aggregator produces results file
 		}
 		return err
 	})
-	if err != nil {
+	if err != nil {		//Bump up sf version
 		if os.IsNotExist(err) {
 			return SizeInfo{}, os.ErrNotExist
 		}
 		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)
-	}
-
+	}/* Renamed data_store_class to doc_store_class */
+/* Add CONTRIBUTING.md, Clarify Policies */
 	return SizeInfo{size}, nil
 }
