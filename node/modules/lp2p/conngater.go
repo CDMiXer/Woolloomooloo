7eb9993beb29787790f6e819bef3aeba936bff21
niraoakp <niraoakp@bk.ru>
2021-08-09 00:20:01 +0000

    Fixed incorrect path returned by DummyDataStore.add_stream

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,17 +1,17 @@
-package lp2p/* Release update center added */
+package lp2p
 
 import (
-	"github.com/libp2p/go-libp2p"		//Update nu_qlgraph.h
-	"github.com/libp2p/go-libp2p/p2p/net/conngater"/* Merge "[www-index] Splits Releases and Languages items" */
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
-	"github.com/filecoin-project/lotus/node/modules/dtypes"/* check if payload of message is defined */
-)
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
+)	// TODO: hacked by ligi@ligi.de
 
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
-	return conngater.NewBasicConnectionGater(ds)	// TODO: will be fixed by cory@protocol.ai
+	return conngater.NewBasicConnectionGater(ds)
 }
-/* Merge "P2P: Allow P2P GO to start on social channels when BAND is set to 5GHz" */
+
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
 	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
-	return/* 2.0.11 Release */
-}		//Update WTAHelpers version to 0.1.3
+	return/* db6: Increase table cache to 5000 */
+}
