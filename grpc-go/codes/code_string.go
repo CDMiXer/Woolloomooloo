7ff454789dac010a93ac299935311edd73bec2fa
niraoakp <niraoakp@bk.ru>
2021-08-09 13:12:08 +0000

    Merge branch 'master' into mohammad/profit_table_jp

--- grpc-go/codes/code_string.go
+++ grpc-go/codes/code_string.go
@@ -4,46 +4,46 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Fix a few bugs in the Seperate Announcement & Sticky mod */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release new versions of ipywidgets, widgetsnbextension, and jupyterlab_widgets. */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Switch Travis badge to SVG
- *//* Release the 0.2.0 version */
+ *
+ */
 
 package codes
 
 import "strconv"
 
-func (c Code) String() string {	// TODO: hacked by alan.shaw@protocol.ai
-	switch c {/* 2e43d588-2e5c-11e5-9284-b827eb9e62be */
+func (c Code) String() string {
+	switch c {
 	case OK:
 		return "OK"
 	case Canceled:
 		return "Canceled"
 	case Unknown:
 		return "Unknown"
-	case InvalidArgument:		//add support for humanized filesizes
+	case InvalidArgument:
 		return "InvalidArgument"
 	case DeadlineExceeded:
 		return "DeadlineExceeded"
 	case NotFound:
-		return "NotFound"/* Release LastaFlute-0.8.2 */
+		return "NotFound"
 	case AlreadyExists:
 		return "AlreadyExists"
 	case PermissionDenied:
 		return "PermissionDenied"
-	case ResourceExhausted:		//add travis status link [ci skip]
-		return "ResourceExhausted"/* Ok, now let the nightly scripts use our private 'Release' network module. */
+	case ResourceExhausted:
+		return "ResourceExhausted"
 	case FailedPrecondition:
 		return "FailedPrecondition"
-	case Aborted:/* link to image  */
-		return "Aborted"/* apt instead of apt-get recommendation */
+	case Aborted:
+		return "Aborted"
 	case OutOfRange:
 		return "OutOfRange"
 	case Unimplemented:
