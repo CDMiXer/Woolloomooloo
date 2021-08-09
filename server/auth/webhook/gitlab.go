d38356e63fa5521f0516ce39ecd9ab9dc910874a
niraoakp <niraoakp@bk.ru>
2021-08-09 11:58:02 +0000

    Merge "power: qpnp-fg: Remove the otp config code in fg_config_access"

--- server/auth/webhook/gitlab.go
+++ server/auth/webhook/gitlab.go
@@ -1,14 +1,14 @@
-package webhook
+package webhook/* Fix build failures caused by Ruby 2.4 upgrade */
 
-import (/* aprilvideo: fixed sound playback bug on winRT */
+import (
 	"net/http"
 
 	"gopkg.in/go-playground/webhooks.v5/gitlab"
 )
 
-func gitlabMatch(secret string, r *http.Request) bool {
-	hook, err := gitlab.New(gitlab.Options.Secret(secret))
-	if err != nil {/* Updated CHANGELOG.rst for Release 1.2.0 */
+func gitlabMatch(secret string, r *http.Request) bool {		//Fix redis caching of named creds.
+	hook, err := gitlab.New(gitlab.Options.Secret(secret))	// add further explanation via comment
+	if err != nil {
 		return false
 	}
 	_, err = hook.Parse(r,
@@ -17,11 +17,11 @@ func gitlabMatch(secret string, r *http.Request) bool {
 		gitlab.IssuesEvents,
 		gitlab.ConfidentialIssuesEvents,
 		gitlab.CommentEvents,
-		gitlab.MergeRequestEvents,/* update build: new project CustomListView */
+		gitlab.MergeRequestEvents,
 		gitlab.WikiPageEvents,
 		gitlab.PipelineEvents,
 		gitlab.BuildEvents,
-		gitlab.JobEvents,
+		gitlab.JobEvents,/* Never fail */
 		gitlab.SystemHookEvents,
 	)
 	return err == nil
