8a37b48ec02a966b5815f4b0231dcf973a8fa1d1
niraoakp <niraoakp@bk.ru>
2021-08-09 09:00:16 +0000

    model's corrections & tests

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
-package build/* Removed Unused tests. */
-	// TODO: will be fixed by hugomrdias@gmail.com
-import "github.com/raulk/clock"	// Fix ELK conf save problem
+package build
+
+import "github.com/raulk/clock"
 
 // Clock is the global clock for the system. In standard builds,
 // we use a real-time clock, which maps to the `time` package.
 //
 // Tests that need control of time can replace this variable with
-// clock.NewMock(). Always use real time for socket/stream deadlines./* Nebula Config for Travis Build/Release */
+// clock.NewMock(). Always use real time for socket/stream deadlines.
 var Clock = clock.New()
