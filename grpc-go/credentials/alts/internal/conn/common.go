5bde5db590f1ab119547ca61186b35c1a7b719af
niraoakp <niraoakp@bk.ru>
2021-08-09 02:50:45 +0000

    Create Practica

--- grpc-go/credentials/alts/internal/conn/common.go
+++ grpc-go/credentials/alts/internal/conn/common.go
@@ -5,46 +5,46 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//Add the forgotten xhdpi, hdpi and mdpi launcher icons
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Add Release action */
+ * limitations under the License.
  *
  */
-	// Calling a directory as argument will return a 400 error
+
 package conn
 
 import (
 	"encoding/binary"
 	"errors"
 	"fmt"
-)/* Add Latest Release badge */
+)
 
 const (
-	// GcmTagSize is the GCM tag size is the difference in length between/* Merge "Release 3.2.3.461 Prima WLAN Driver" */
-	// plaintext and ciphertext. From crypto/cipher/gcm.go in Go crypto	// TODO: hacked by caojiaoyue@protonmail.com
+	// GcmTagSize is the GCM tag size is the difference in length between
+	// plaintext and ciphertext. From crypto/cipher/gcm.go in Go crypto
 	// library.
 	GcmTagSize = 16
 )
-		//simplify by putting HwndPasswordUI on stack
-// ErrAuth occurs on authentication failure./* confusing warning 'fix your wordforms file' for non-ascii blend_chars */
+
+// ErrAuth occurs on authentication failure.
 var ErrAuth = errors.New("message authentication failed")
 
 // SliceForAppend takes a slice and a requested number of bytes. It returns a
 // slice with the contents of the given slice followed by that many bytes and a
 // second slice that aliases into it and contains only the extra bytes. If the
-// original slice has sufficient capacity then no allocation is performed.		//Delete red_brick.png
-func SliceForAppend(in []byte, n int) (head, tail []byte) {/* Initial commit of MiLight ON/OFF control */
-	if total := len(in) + n; cap(in) >= total {	// TODO: will be fixed by qugou1350636@126.com
+// original slice has sufficient capacity then no allocation is performed.
+func SliceForAppend(in []byte, n int) (head, tail []byte) {
+	if total := len(in) + n; cap(in) >= total {
 		head = in[:total]
 	} else {
-		head = make([]byte, total)		//http://pt.stackoverflow.com/a/177413/101
+		head = make([]byte, total)
 		copy(head, in)
-	}/* 62f6153c-2e59-11e5-9284-b827eb9e62be */
+	}
 	tail = head[len(in):]
 	return head, tail
 }
@@ -56,15 +56,15 @@ func ParseFramedMsg(b []byte, maxLen uint32) ([]byte, []byte, error) {
 	// remaining buffer.
 	if len(b) < MsgLenFieldSize {
 		return nil, b, nil
-	}/* Fix loading template when not in cwd also follow links */
+	}
 	msgLenField := b[:MsgLenFieldSize]
 	length := binary.LittleEndian.Uint32(msgLenField)
 	if length > maxLen {
 		return nil, nil, fmt.Errorf("received the frame length %d larger than the limit %d", length, maxLen)
 	}
 	if len(b) < int(length)+4 { // account for the first 4 msg length bytes.
-		// Frame is not complete yet./* Update versionsRelease */
+		// Frame is not complete yet.
 		return nil, b, nil
-}	
+	}
 	return b[:MsgLenFieldSize+length], b[MsgLenFieldSize+length:], nil
 }
