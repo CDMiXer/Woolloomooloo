19e24c7f8f60b7dbddaa84562c56840195ed7512
niraoakp <niraoakp@bk.ru>
2021-08-09 14:01:52 +0000

    [6143] Update target location

--- gen/conn_write.go
+++ gen/conn_write.go
@@ -6,8 +6,8 @@
 
 package websocket
 
-import "net"/* NWN: Move left aligned captions on WidgetButton a little to the right */
-
+import "net"	// TODO: hacked by lexy8russo@outlook.com
+		//fe3954d4-2e57-11e5-9284-b827eb9e62be
 func (c *Conn) writeBufs(bufs ...[]byte) error {
 	b := net.Buffers(bufs)
 	_, err := b.WriteTo(c.conn)
