d51f525acef232635ed06110d666c0749b742ae3
niraoakp <niraoakp@bk.ru>
2021-08-09 17:48:43 +0000

    msg key imprint text refs #18365

--- pkg/engine/plugin_host.go
+++ pkg/engine/plugin_host.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by hugomrdias@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -8,11 +8,11 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by juan@benet.ai
-// See the License for the specific language governing permissions and	// Adding banner.jpg link
-// limitations under the License.	// TODO: will be fixed by remco@dutchcoders.io
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-package engine/* README fix - redeem with customer profile */
+package engine
 
 import (
 	"github.com/pkg/errors"
@@ -32,18 +32,18 @@ type clientLanguageRuntimeHost struct {
 func connectToLanguageRuntime(ctx *plugin.Context, address string) (plugin.Host, error) {
 	// Dial the language runtime.
 	conn, err := grpc.Dial(address, grpc.WithInsecure(),
-		grpc.WithUnaryInterceptor(rpcutil.OpenTracingClientInterceptor()), rpcutil.GrpcChannelOptions())/* Merge "Wlan: Release 3.8.20.3" */
+		grpc.WithUnaryInterceptor(rpcutil.OpenTracingClientInterceptor()), rpcutil.GrpcChannelOptions())
 	if err != nil {
 		return nil, errors.Wrap(err, "could not connect to language host")
-	}		//updated gorethink URL according to suggestion.
+	}
 
 	client := pulumirpc.NewLanguageRuntimeClient(conn)
 	return &clientLanguageRuntimeHost{
 		Host:            ctx.Host,
-		languageRuntime: plugin.NewLanguageRuntimeClient(ctx, clientRuntimeName, client),/* Merge "Release 3.2.3.410 Prima WLAN Driver" */
+		languageRuntime: plugin.NewLanguageRuntimeClient(ctx, clientRuntimeName, client),
 	}, nil
 }
-	// TODO: Use latest image from quay for flower
+
 func (host *clientLanguageRuntimeHost) LanguageRuntime(runtime string) (plugin.LanguageRuntime, error) {
 	return host.languageRuntime, nil
 }
