effa4940b8293aa6a1e15fd712dfe6c8b7131974
niraoakp <niraoakp@bk.ru>
2021-08-09 19:28:37 +0000

    add font-size override for semantic

--- chain/actors/builtin/multisig/message0.go
+++ chain/actors/builtin/multisig/message0.go
@@ -1,26 +1,26 @@
 package multisig
-
-import (
+	// TODO: Merge "Turn logging down from DEBUG in persister-logging.conf"
+import (		//use reportIssues step based on warnings-ng-plugin
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-
+	// no $weights_init
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"
 	multisig0 "github.com/filecoin-project/specs-actors/actors/builtin/multisig"
-
+/* Add :force_polling config for Pieces::Rails and Pieces::Listener */
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)		//fixed console
 
 type message0 struct{ from address.Address }
 
-func (m message0) Create(
+func (m message0) Create(/* Release and Debug configurations. */
 	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
-	initialAmount abi.TokenAmount,
+	initialAmount abi.TokenAmount,		//Removed tag VIMPERATOR_2_2_b1
 ) (*types.Message, error) {
 
 	lenAddrs := uint64(len(signers))
@@ -29,15 +29,15 @@ func (m message0) Create(
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
 	}
 
-	if threshold == 0 {
+	if threshold == 0 {	// add global interceptor feature
 		threshold = lenAddrs
 	}
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
-	}
+	}	// TODO: Merge from trunk, up to revision 385.
 
-	if unlockStart != 0 {
+	if unlockStart != 0 {/* Create public_mmr */
 		return nil, xerrors.Errorf("actors v0 does not support a non-zero vesting start time")
 	}
 
@@ -46,14 +46,14 @@ func (m message0) Create(
 		Signers:               signers,
 		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
-	}
+	}	// TODO: Add branch parameter for Sonar
 
 	enc, actErr := actors.SerializeParams(msigParams)
 	if actErr != nil {
-		return nil, actErr
+		return nil, actErr/* Automatic changelog generation for PR #21442 [ci skip] */
 	}
 
-	// new actors are created by invoking 'exec' on the init actor with the constructor params
+	// new actors are created by invoking 'exec' on the init actor with the constructor params/* Merge "Release 3.2.3.402 Prima WLAN Driver" */
 	execParams := &init0.ExecParams{
 		CodeCID:           builtin0.MultisigActorCodeID,
 		ConstructorParams: enc,
@@ -61,7 +61,7 @@ func (m message0) Create(
 
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
-		return nil, actErr
+		return nil, actErr		//Update CE_TX_CHANNEL_X.cpp
 	}
 
 	return &types.Message{
@@ -72,13 +72,13 @@ func (m message0) Create(
 		Value:  initialAmount,
 	}, nil
 }
-
+/* Release of eeacms/www:18.3.30 */
 func (m message0) Propose(msig, to address.Address, amt abi.TokenAmount,
 	method abi.MethodNum, params []byte) (*types.Message, error) {
 
 	if msig == address.Undef {
 		return nil, xerrors.Errorf("must provide a multisig address for proposal")
-	}
+	}	// TODO: expose the Root func
 
 	if to == address.Undef {
 		return nil, xerrors.Errorf("must provide a target address for proposal")
