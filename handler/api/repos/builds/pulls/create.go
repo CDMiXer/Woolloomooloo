730ea20a86f8fd26688e3a8f2a097d1f1c94cb18
niraoakp <niraoakp@bk.ru>
2021-08-09 15:40:27 +0000

    79ee5d72-2e5d-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/pulls/create.go
+++ handler/api/repos/builds/pulls/create.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by nagydani@epointsystem.org
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+///* Update ServiceConfiguration.Release.cscfg */
+// Unless required by applicable law or agreed to in writing, software	// Update Airport.java
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 0.2.5. */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
