5b33269fa55615e598e40b670ace1c4d5aa72112
niraoakp <niraoakp@bk.ru>
2021-08-09 23:15:27 +0000

    Merge "ASoC: msm8x16-wcd: modify interrupt handling"

--- store/shared/migrate/sqlite/files/002_create_table_repos.sql
+++ store/shared/migrate/sqlite/files/002_create_table_repos.sql
@@ -1,30 +1,30 @@
 -- name: create-table-repos
-/* Edited audio signal generator script docstring. */
+	// TODO: Add incron for stop and start wifi
 CREATE TABLE IF NOT EXISTS repos (
  repo_id                    INTEGER PRIMARY KEY AUTOINCREMENT
 ,repo_uid                   TEXT
-,repo_user_id               INTEGER
+,repo_user_id               INTEGER/* fixed bug in scripts/Makefile */
 ,repo_namespace             TEXT
-,repo_name                  TEXT
+,repo_name                  TEXT	// TODO: replacing google code link for leveldb with github link
 ,repo_slug                  TEXT
-,repo_scm                   TEXT
+,repo_scm                   TEXT	// TODO: setting version to 3.0.4
 ,repo_clone_url             TEXT
 ,repo_ssh_url               TEXT
-,repo_html_url              TEXT/* rev 868747 */
+,repo_html_url              TEXT
 ,repo_active                BOOLEAN
 ,repo_private               BOOLEAN
-,repo_visibility            TEXT
+,repo_visibility            TEXT	// Added pub sub history, changed to ApplicationContext, NanoTimer
 ,repo_branch                TEXT
 ,repo_counter               INTEGER
-,repo_config                TEXT
+,repo_config                TEXT/* Implemented convolutional neural networks */
 ,repo_timeout               INTEGER
-,repo_trusted               BOOLEAN	// TODO: 78857740-5216-11e5-948b-6c40088e03e4
+,repo_trusted               BOOLEAN
 ,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
 ,repo_created               INTEGER
 ,repo_updated               INTEGER
 ,repo_version               INTEGER
-,repo_signer                TEXT		//add circle.yml
+,repo_signer                TEXT
 ,repo_secret                TEXT
 ,UNIQUE(repo_slug)
 ,UNIQUE(repo_uid)
@@ -32,16 +32,16 @@ CREATE TABLE IF NOT EXISTS repos (
 
 -- name: alter-table-repos-add-column-no-fork
 
-ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT 0;
-		//Create COG_scrambler.pl
--- name: alter-table-repos-add-column-no-pulls	// TODO: will be fixed by aeongrp@outlook.com
+ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT 0;		//missing line extension
+
+-- name: alter-table-repos-add-column-no-pulls
 
 ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT 0;
-
+	// TODO: better var scoping.   
 -- name: alter-table-repos-add-column-cancel-pulls
-	// ccbcb008-2e49-11e5-9284-b827eb9e62be
-ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT 0;/* Merge "Release 1.0.0.84 QCACLD WLAN Driver" */
-/* Easy ajax handling. Release plan checked */
+
+ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT 0;
+
 -- name: alter-table-repos-add-column-cancel-push
 
-ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT 0;
+ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT 0;/* switch images to imgur */
