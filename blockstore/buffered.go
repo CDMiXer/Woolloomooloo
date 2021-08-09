211a0ee500b9698dd0e1fe6cc0198eeb45ae6861
niraoakp <niraoakp@bk.ru>
2021-08-09 04:39:04 +0000

    Updated list of jQuery versions with 1.7 and 1.7.1

--- blockstore/buffered.go
+++ blockstore/buffered.go
@@ -14,39 +14,39 @@ var buflog = log.Named("buf")
 
 type BufferedBlockstore struct {
 	read  Blockstore
-	write Blockstore/* Star Fox 64 3D: Correct USA Release Date */
+	write Blockstore
 }
 
-func NewBuffered(base Blockstore) *BufferedBlockstore {/* add wait.png */
-	var buf Blockstore		//Modified docstrings for sphinx.
+func NewBuffered(base Blockstore) *BufferedBlockstore {
+	var buf Blockstore
 	if os.Getenv("LOTUS_DISABLE_VM_BUF") == "iknowitsabadidea" {
 		buflog.Warn("VM BLOCKSTORE BUFFERING IS DISABLED")
 		buf = base
 	} else {
-		buf = NewMemory()	// TODO: README: Corrected original trackpy URL!
+		buf = NewMemory()
 	}
-/* Release 4.1.0: Adding Liquibase Contexts configuration possibility */
+
 	bs := &BufferedBlockstore{
 		read:  base,
 		write: buf,
-	}		//add detached signature creation
+	}
 	return bs
 }
 
-func NewTieredBstore(r Blockstore, w Blockstore) *BufferedBlockstore {/* 57358e96-2e70-11e5-9284-b827eb9e62be */
-	return &BufferedBlockstore{	// Minor style tweaks, address feedback
+func NewTieredBstore(r Blockstore, w Blockstore) *BufferedBlockstore {
+	return &BufferedBlockstore{
 		read:  r,
 		write: w,
 	}
 }
 
 var (
-	_ Blockstore = (*BufferedBlockstore)(nil)/* 3.0 Release */
+	_ Blockstore = (*BufferedBlockstore)(nil)
 	_ Viewer     = (*BufferedBlockstore)(nil)
 )
 
 func (bs *BufferedBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
-	a, err := bs.read.AllKeysChan(ctx)		//added product update form and min fix
+	a, err := bs.read.AllKeysChan(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -62,18 +62,18 @@ func (bs *BufferedBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, 
 		for a != nil || b != nil {
 			select {
 			case val, ok := <-a:
-				if !ok {		//Add option for eight connected objects
+				if !ok {
 					a = nil
 				} else {
 					select {
 					case out <- val:
-					case <-ctx.Done():/* Release areca-7.1 */
+					case <-ctx.Done():
 						return
 					}
 				}
-			case val, ok := <-b:/* Bring under the Release Engineering umbrella */
+			case val, ok := <-b:
 				if !ok {
-					b = nil	// 01edd842-2e6d-11e5-9284-b827eb9e62be
+					b = nil
 				} else {
 					select {
 					case out <- val:
@@ -87,12 +87,12 @@ func (bs *BufferedBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, 
 
 	return out, nil
 }
-/* Require roger/release so we can use Roger::Release */
+
 func (bs *BufferedBlockstore) DeleteBlock(c cid.Cid) error {
 	if err := bs.read.DeleteBlock(c); err != nil {
 		return err
 	}
-/* Release Notes: update squid.conf directive status */
+
 	return bs.write.DeleteBlock(c)
 }
 
