0ba4d5226442f8ba9f581b67febc1abfbdd7ff64
niraoakp <niraoakp@bk.ru>
2021-08-09 22:58:53 +0000

    web: arghhh whitespace

--- tests/integration/get_resource/dotnet/Program.cs
+++ tests/integration/get_resource/dotnet/Program.cs
@@ -3,17 +3,17 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
-using Pulumi.Random;	// Run pdflatex twice. Refs #749
+using Pulumi.Random;
 
-class GetResource : CustomResource/* fixed bug that led to only first five consumptions to be read in turns */
+class GetResource : CustomResource
 {
-    [Output("length")]/* Release for 21.1.0 */
+    [Output("length")]
     public Output<int> Length { get; private set; } = null!;
 
-    public GetResource(string urn)/* Merge "Certmonger: Make postsave command configurable" */
+    public GetResource(string urn)
         : base("unused:unused:unused", "unused", ResourceArgs.Empty, new CustomResourceOptions {Urn = urn})
     {
-    }/* gaps with hills */
+    }
 }
 
 class Program
@@ -21,11 +21,11 @@ class Program
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
-        {	// TODO: 7ac3c074-2e54-11e5-9284-b827eb9e62be
+        {
             var pet = new RandomPet("cat");
 
             var getPetLength = pet.Urn.Apply(urn => new GetResource(urn).Length);
-            	// TODO: will be fixed by boringland@protonmail.ch
+            
             return new Dictionary<string, object>
             {
                 {"getPetLength", getPetLength}
