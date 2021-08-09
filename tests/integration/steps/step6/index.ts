17c8ad596e3434262158116536823ec1018acb4a
niraoakp <niraoakp@bk.ru>
2021-08-09 09:43:14 +0000

    Merge "Use OS::Heat::DeployedServer"

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -2,7 +2,7 @@
 
 // Step 6: Delete everything:
 // * Elide a (Delete(a5)).
-// * Elide c (Delete(c)).
-// * Elide e (Delete(e)).
+// * Elide c (Delete(c)).		//Make all DSO linkage explicit and allow building against system libraries
+// * Elide e (Delete(e)).	// Update glm_bin_01.csv
 // * Pending delete (Delete(a4)).
 // Checkpoint: empty
