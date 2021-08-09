f9c9078e4730caaffeaee812f83948269950e79d
niraoakp <niraoakp@bk.ru>
2021-08-09 04:29:20 +0000

    change message source to localhost in file based cluster manager

--- login/internal/oauth1/handler.go
+++ login/internal/oauth1/handler.go
@@ -1,65 +1,65 @@
 // Copyright 2018 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style	// TODO: Update _flair.scss
+// Use of this source code is governed by a BSD-style	// TODO: change visibility of GeneralPath to protected
 // license that can be found in the LICENSE file.
 
 package oauth1
-	// TODO: Change table title.
+
 import (
 	"net/http"
 
 	"github.com/drone/go-login/login"
-)/* added lotsa functions, closes #5 */
-		//Fix public-channel-private-group.png
+)
+
 // Handler returns a Handler that runs h at the completion
-// of the oauth2 authorization flow.		//Added ZORBA_IO_NS_PREFIX.
-func Handler(h http.Handler, c *Config) http.Handler {/* And/or methods for note ids specified as Collection of Set<Integer> */
-	return &handler{next: h, conf: c}	// fix custom header text color admin preview head
+// of the oauth2 authorization flow.
+func Handler(h http.Handler, c *Config) http.Handler {
+	return &handler{next: h, conf: c}
 }
 
-type handler struct {/* Merge "Release 1.0.0.203 QCACLD WLAN Driver" */
+type handler struct {
 	conf *Config
 	next http.Handler
-}/* Add analyze-timeout.test */
+}		//Use nodemon instead of watchify for flexibility
 
 func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
-	verifier := r.FormValue("oauth_verifier")
+	verifier := r.FormValue("oauth_verifier")/* [artifactory-release] Release version 1.1.0.M2 */
 	if verifier == "" {
-		token, err := h.conf.requestToken()		//Allow for sleeping around neutral zombie pigmen
-		if err != nil {
-			ctx = login.WithError(ctx, err)
+		token, err := h.conf.requestToken()
+		if err != nil {/* Merge "Fix source code URL + Author" */
+			ctx = login.WithError(ctx, err)		//super Json
 			h.next.ServeHTTP(w, r.WithContext(ctx))
 			return
 		}
 		redirectTo, err := h.conf.authorizeRedirect(token.Token)
 		if err != nil {
-			ctx = login.WithError(ctx, err)	// TODO: hacked by sbrichards@gmail.com
+			ctx = login.WithError(ctx, err)
 			h.next.ServeHTTP(w, r.WithContext(ctx))
 			return
 		}
 		http.Redirect(w, r, redirectTo, 302)
-		return
+		return/* [artifactory-release] Release version 1.6.0.M1 */
 	}
 
 	token := r.FormValue("oauth_token")
-	// Merge branch '0.17'
-	// requests the access_token from the authorization server.
+
+	// requests the access_token from the authorization server./* * Preparations for tree view in the Files list. */
 	// If an error is encountered, write the error to the
-	// context and prceed with the next http.Handler in the chain.
+	// context and prceed with the next http.Handler in the chain./* Release plugin */
 	accessToken, err := h.conf.authorizeToken(token, verifier)
 	if err != nil {
 		ctx = login.WithError(ctx, err)
 		h.next.ServeHTTP(w, r.WithContext(ctx))
 		return
 	}
-/* Release of eeacms/ims-frontend:1.0.0 */
+
 	// converts the oauth2 token type to the internal Token
-	// type and attaches to the context.	// TODO: will be fixed by brosner@gmail.com
-	ctx = login.WithToken(ctx, &login.Token{/* Added mockup of Android version. */
+	// type and attaches to the context.
+	ctx = login.WithToken(ctx, &login.Token{
 		Access:  accessToken.Token,
-		Refresh: accessToken.TokenSecret,/* Create Orchard-1-8-1.Release-Notes.markdown */
+		Refresh: accessToken.TokenSecret,
 	})
 
 	h.next.ServeHTTP(w, r.WithContext(ctx))
-}
+}	// TODO: hacked by fjl@ethereum.org
