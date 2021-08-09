91be3f4bc1ba6a0664f0b1c260ffba64ae6a4601
niraoakp <niraoakp@bk.ru>
2021-08-09 23:54:00 +0000

    rev 489502

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release 6.4.0 */
 
 // +build !oss
 
-package postgres
+package postgres	// Small fixes for build service (Makefile.am).
 
 //go:generate togo ddl -package postgres -dialect postgres
