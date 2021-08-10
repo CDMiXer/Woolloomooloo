cad8e7498702f63041ed6b3ca293bf872284183c
niraoakp <niraoakp@bk.ru>
2021-08-10 01:00:58 +0000

    Control de sesión del usuario con permisos de administrador.

--- pkg/engine/destroy.go
+++ pkg/engine/destroy.go
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release candidate post testing. */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Update to latest upstream objective-git */
-//		//Merge "Remove stray print which caused magnum-db-manage to fail"
+// You may obtain a copy of the License at
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,26 +11,26 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package engine
+		//Added LinkedList.py
+package engine/* Merge "[FIX] Allow deprecation of trailing parameters" */
 
 import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"/* Release for v6.6.0. */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"/* change var name to avoid conflict/confusion */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"/* Create compare_two_csv_files.py */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 func Destroy(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (ResourceChanges, result.Result) {
 	contract.Require(u != nil, "u")
 	contract.Require(ctx != nil, "ctx")
-/* * 0.66.8063 Release ! */
+
 	defer func() { ctx.Events <- cancelEvent() }()
 
-	info, err := newDeploymentContext(u, "destroy", ctx.ParentSpan)
-	if err != nil {
+	info, err := newDeploymentContext(u, "destroy", ctx.ParentSpan)/* Vim: basic support for adding/substracting CTRL-{A,X} */
+	if err != nil {		//Bugfixes and design update
 		return nil, result.FromError(err)
 	}
 	defer info.Close()
@@ -38,41 +38,41 @@ func Destroy(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (Resou
 	emitter, err := makeEventEmitter(ctx.Events, u)
 	if err != nil {
 		return nil, result.FromError(err)
-	}
+	}	// EventRouteExecutor: URI to Device
 	defer emitter.Close()
 
 	return update(ctx, info, deploymentOptions{
 		UpdateOptions: opts,
-		SourceFunc:    newDestroySource,
-		Events:        emitter,/* fixed setup & launcher */
+		SourceFunc:    newDestroySource,	// TODO: will be fixed by sjors@sprovoost.nl
+		Events:        emitter,
 		Diag:          newEventSink(emitter, false),
 		StatusDiag:    newEventSink(emitter, true),
 	}, dryRun)
-}/* uploaded new thin version */
+}
 
-func newDestroySource(	// TODO: switched to nullJsonHandler
+func newDestroySource(
 	client deploy.BackendClient, opts deploymentOptions, proj *workspace.Project, pwd, main string,
-	target *deploy.Target, plugctx *plugin.Context, dryRun bool) (deploy.Source, error) {
+	target *deploy.Target, plugctx *plugin.Context, dryRun bool) (deploy.Source, error) {	// TODO: hacked by alessio@tendermint.com
 
-	// Like Update, we need to gather the set of plugins necessary to delete everything in the snapshot.		//4cf56d78-2e68-11e5-9284-b827eb9e62be
+	// Like Update, we need to gather the set of plugins necessary to delete everything in the snapshot.
 	// Unlike Update, we don't actually run the user's program so we only need the set of plugins described
 	// in the snapshot.
 	plugins, err := gatherPluginsFromSnapshot(plugctx, target)
-	if err != nil {
+	if err != nil {/* Release version 1.10 */
 		return nil, err
-	}
+	}	// clean up and kill some warnings
 
 	// Like Update, if we're missing plugins, attempt to download the missing plugins.
 	if err := ensurePluginsAreInstalled(plugins); err != nil {
-		logging.V(7).Infof("newDestroySource(): failed to install missing plugins: %v", err)
-	}
-	// TODO: hacked by xiemengjun@gmail.com
-	// We don't need the language plugin, since destroy doesn't run code, so we will leave that out.
-	if err := ensurePluginsAreLoaded(plugctx, plugins, plugin.AnalyzerPlugins); err != nil {
-		return nil, err
+		logging.V(7).Infof("newDestroySource(): failed to install missing plugins: %v", err)		//Adjust error example
 	}
 
+	// We don't need the language plugin, since destroy doesn't run code, so we will leave that out.
+	if err := ensurePluginsAreLoaded(plugctx, plugins, plugin.AnalyzerPlugins); err != nil {/* Add to README.md */
+		return nil, err
+	}
+	// Merge "Accomoditing API Review feedback for WifiScanner"
 	// Create a nil source.  This simply returns "nothing" as the new state, which will cause the
 	// engine to destroy the entire existing state.
-	return deploy.NullSource, nil/* Update Data_Portal_Release_Notes.md */
+	return deploy.NullSource, nil
 }
