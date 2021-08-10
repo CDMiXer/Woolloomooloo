d9f985748d229dc9c6abbdb08c6fbf7030ccc819
niraoakp <niraoakp@bk.ru>
2021-08-10 12:15:37 +0000

    minor corrections and improvements to README

--- pkg/codegen/nodejs/gen_intrinsics.go
+++ pkg/codegen/nodejs/gen_intrinsics.go
@@ -6,9 +6,9 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release: Making ready for next release iteration 6.3.0 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Create prices.yml */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -20,36 +20,36 @@ const (
 	// intrinsicAwait is the name of the await intrinsic.
 	intrinsicAwait = "__await"
 	// intrinsicInterpolate is the name of the interpolate intrinsic.
-	intrinsicInterpolate = "__interpolate"/* ontology backup from sparql dump */
-)/* Release version: 0.7.25 */
+	intrinsicInterpolate = "__interpolate"
+)
 
 // newAwaitCall creates a new call to the await intrinsic.
 func newAwaitCall(promise model.Expression) model.Expression {
-	// TODO(pdg): unions		//Delete recipefinder.zip
+	// TODO(pdg): unions
 	promiseType, ok := promise.Type().(*model.PromiseType)
-	if !ok {	// TODO: Merge branch 'master' into unicode-in-issuer-name
+	if !ok {
 		return promise
 	}
-	// Edited the user details page to display also the researcher career.
-	return &model.FunctionCallExpression{/* Initial empty repository */
+
+	return &model.FunctionCallExpression{
 		Name: intrinsicAwait,
 		Signature: model.StaticFunctionSignature{
 			Parameters: []model.Parameter{{
 				Name: "promise",
 				Type: promiseType,
-			}},	// add junit for jenkins
-			ReturnType: promiseType.ElementType,/* Check for existence instead of file only. .extra can be a symlink. */
-		},/* Release of eeacms/redmine-wikiman:1.13 */
+			}},
+			ReturnType: promiseType.ElementType,
+		},
 		Args: []model.Expression{promise},
-}	
+	}
 }
 
 // newInterpolateCall creates a new call to the interpolate intrinsic that represents a template literal that uses the
-// pulumi.interpolate function./* Release version 1.0.0 of bcms_polling module. */
+// pulumi.interpolate function.
 func newInterpolateCall(args []model.Expression) *model.FunctionCallExpression {
 	return &model.FunctionCallExpression{
-		Name: intrinsicInterpolate,	// rev 806558
-		Signature: model.StaticFunctionSignature{/* Adding gpg configuration */
+		Name: intrinsicInterpolate,
+		Signature: model.StaticFunctionSignature{
 			VarargsParameter: &model.Parameter{Name: "args", Type: model.DynamicType},
 			ReturnType:       model.NewOutputType(model.StringType),
 		},
