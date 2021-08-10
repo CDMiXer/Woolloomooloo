c4ed0fc1dc76173e01553cf79c215c36df71f27d
niraoakp <niraoakp@bk.ru>
2021-08-10 05:23:28 +0000

    Delete prueba.rdoc

--- store/shared/db/nop.go
+++ store/shared/db/nop.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Update percolation.py
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -12,11 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package db/* Release of eeacms/jenkins-slave:3.12 */
+package db
 
 type nopLocker struct{}
-		//Create Controller.swift
-func (nopLocker) Lock()    {}	// TODO: Updating minimal store.
+
+func (nopLocker) Lock()    {}
 func (nopLocker) Unlock()  {}
 func (nopLocker) RLock()   {}
 func (nopLocker) RUnlock() {}
