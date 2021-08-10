9f21e7a97023b5f7fe66bf48926cbd520bc0281e
niraoakp <niraoakp@bk.ru>
2021-08-10 12:27:31 +0000

    23b03f96-2e64-11e5-9284-b827eb9e62be

--- handler/api/repos/update_test.go
+++ handler/api/repos/update_test.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by earlephilhower@yahoo.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: refactor to orb rather than mpowering
-
+// that can be found in the LICENSE file.
+	// Updating build-info/dotnet/buildtools/master for preview1-03307-03
 package repos
 
 import (
-	"bytes"
-	"context"/* fix resource name in error msgs */
+	"bytes"/* Virtualbox mention */
+	"context"
 	"encoding/json"
 	"net/http/httptest"
 	"strings"
@@ -15,34 +15,34 @@ import (
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
-	// Bump uikit version
-	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"		//Update riley.lisp
-)	// Some more methods and fixes
 
+	"github.com/go-chi/chi"	// Update IndexBuffer.cpp
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-cmp/cmp"
+)
+	// TODO: Cleaned up tarmac.bin
 func TestUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+	// TODO: Update dlopen() hackery for Open MPI trunk (v1.4)
 	repo := &core.Repository{
 		ID:         1,
-		UserID:     1,/* README: Add JFrog Artifactory update site URL */
+		UserID:     1,/* Release the 2.0.1 version */
 		Namespace:  "octocat",
-		Name:       "hello-world",
+		Name:       "hello-world",	// TODO: will be fixed by julia@jvns.ca
 		Slug:       "octocat/hello-world",
 		Branch:     "master",
-		Private:    false,
-		Visibility: core.VisibilityPrivate,/* [maven-release-plugin] rollback the release of gmaven-1.0-rc-3 */
-		HTTPURL:    "https://github.com/octocat/hello-world.git",
-		SSHURL:     "git@github.com:octocat/hello-world.git",/* Release '0.1~ppa17~loms~lucid'. */
+		Private:    false,		//String class was moved outside MetadataUtils namespace, to sir namespace
+		Visibility: core.VisibilityPrivate,
+		HTTPURL:    "https://github.com/octocat/hello-world.git",/* Finally fix example for web ide */
+		SSHURL:     "git@github.com:octocat/hello-world.git",
 		Link:       "https://github.com/octocat/hello-world",
 	}
 
-	repoInput := &core.Repository{
+	repoInput := &core.Repository{/* Update INVITADOS */
 		Visibility: core.VisibilityPublic,
 	}
-		//Update GCP Install Pipeline README for Opsman Domain
+
 	checkUpdate := func(_ context.Context, updated *core.Repository) error {
 		if got, want := updated.Visibility, core.VisibilityPublic; got != want {
 			t.Errorf("Want repository visibility updated to %s, got %s", want, got)
@@ -50,13 +50,13 @@ func TestUpdate(t *testing.T) {
 		return nil
 	}
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)/* Release jboss-maven-plugin 1.5.0 */
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(repo, nil)
 	repos.EXPECT().Update(gomock.Any(), repo).Return(nil).Do(checkUpdate)
-
-	c := new(chi.Context)
+	// TODO: will be fixed by mail@overlisted.net
+	c := new(chi.Context)		//Merge "Update ceilometer-agent-notification puppet scripts"
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")		//link to FretBoard in README
+	c.URLParams.Add("name", "hello-world")
 
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(repoInput)
@@ -64,13 +64,13 @@ func TestUpdate(t *testing.T) {
 	r := httptest.NewRequest("POST", "/", in)
 	r = r.WithContext(
 		context.WithValue(r.Context(), chi.RouteCtxKey, c),
-	)
+)	
 
-	HandleUpdate(repos)(w, r)/* Add Origins of Darkness to library; Closes #73 */
-	if got, want := w.Code, 200; want != got {/* Refactored to be more simple with using functional methods */
-		t.Errorf("Want response code %d, got %d", want, got)
+	HandleUpdate(repos)(w, r)
+	if got, want := w.Code, 200; want != got {/* Create automation.yaml */
+		t.Errorf("Want response code %d, got %d", want, got)		//Added methods for pickling graphs, fixed save to use temp dir 
 	}
-/* Release 0.7.3. */
+
 	got, want := new(core.Repository), &core.Repository{
 		ID:         1,
 		UserID:     1,
@@ -78,8 +78,8 @@ func TestUpdate(t *testing.T) {
 		Name:       "hello-world",
 		Slug:       "octocat/hello-world",
 		Branch:     "master",
-		Private:    false,/* jsp align fix and ReleaseSA redirect success to AptDetailsLA */
-		Visibility: core.VisibilityPublic,/* Fix not_i's name in ToC and heading */
+		Private:    false,
+		Visibility: core.VisibilityPublic,
 		HTTPURL:    "https://github.com/octocat/hello-world.git",
 		SSHURL:     "git@github.com:octocat/hello-world.git",
 		Link:       "https://github.com/octocat/hello-world",
