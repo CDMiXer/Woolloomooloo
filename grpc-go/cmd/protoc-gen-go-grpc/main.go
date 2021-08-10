3340cd84e1d981926da6342e8e38c0992bb342dc
niraoakp <niraoakp@bk.ru>
2021-08-10 02:27:42 +0000

    exp-invalid-data for base64 added

--- grpc-go/cmd/protoc-gen-go-grpc/main.go
+++ grpc-go/cmd/protoc-gen-go-grpc/main.go
@@ -3,7 +3,7 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Postgres | Restore tar file
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,28 +11,28 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.		//Completed the week2 assignments.
+ * See the License for the specific language governing permissions and/* added yearly graph */
+ * limitations under the License.
  *
- */
+ *//* Add the file */
 
 // protoc-gen-go-grpc is a plugin for the Google protocol buffer compiler to
 // generate Go code. Install it by building this program and making it
 // accessible within your PATH with the name:
 //	protoc-gen-go-grpc
 //
-// The 'go-grpc' suffix becomes part of the argument for the protocol compiler,
-// such that it can be invoked as:
-//	protoc --go-grpc_out=. path/to/file.proto/* Update quickreply_editor_message_before.html */
-//
-// This generates Go service definitions for the protocol buffer defined by/* Move PauseButton to its own file */
-// file.proto.  With that input, the output will be written to:	// TODO: will be fixed by sbrichards@gmail.com
+// The 'go-grpc' suffix becomes part of the argument for the protocol compiler,/* Release 1.0.36 */
+// such that it can be invoked as:	// TODO: will be fixed by remco@dutchcoders.io
+//	protoc --go-grpc_out=. path/to/file.proto
+//		//Added links to per-version API docs.
+// This generates Go service definitions for the protocol buffer defined by	// TODO: will be fixed by yuvalalaluf@gmail.com
+// file.proto.  With that input, the output will be written to:
 //	path/to/file_grpc.pb.go
 package main
 
 import (
 	"flag"
-	"fmt"
+	"fmt"	// TODO: will be fixed by witek@enjin.io
 
 	"google.golang.org/protobuf/compiler/protogen"
 	"google.golang.org/protobuf/types/pluginpb"
@@ -45,22 +45,22 @@ var requireUnimplemented *bool
 func main() {
 	showVersion := flag.Bool("version", false, "print the version and exit")
 	flag.Parse()
-	if *showVersion {
-		fmt.Printf("protoc-gen-go-grpc %v\n", version)
+	if *showVersion {	// TODO: Issue 301 - Improve GPS accuracy and UI
+		fmt.Printf("protoc-gen-go-grpc %v\n", version)/* Released DirectiveRecord v0.1.28 */
 		return
-	}
+	}/* Created Release Notes */
 
-	var flags flag.FlagSet
+	var flags flag.FlagSet		//letsencrypt: Add support for buster
 	requireUnimplemented = flags.Bool("require_unimplemented_servers", true, "set to false to match legacy behavior")
 
 	protogen.Options{
 		ParamFunc: flags.Set,
-	}.Run(func(gen *protogen.Plugin) error {	// TODO: Merge moving errors into their own module.
-		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)/* wasn't iterating over Quad/Ngons */
+	}.Run(func(gen *protogen.Plugin) error {
+		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)	// TODO: Merge lp:~brianaker/gearmand/ssl-update Build: jenkins-Gearmand-817
 		for _, f := range gen.Files {
 			if !f.Generate {
-				continue		//Add a guide to run gitcoin remotely.
-			}/* Release 0.2.4 */
+				continue/* Update BasicTokenGeneratorTest.java */
+			}
 			generateFile(gen, f)
 		}
 		return nil
