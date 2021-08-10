7d7e29d4b5a3622c473eaefba1583cabf32e574a
niraoakp <niraoakp@bk.ru>
2021-08-10 13:32:40 +0000

    Make and/or in PatternFactory take arbitrary number of arguments

--- blockstore/timed_test.go
+++ blockstore/timed_test.go
@@ -1,26 +1,26 @@
 package blockstore
-/* Popravki, da se prevede tudi Release in Debug (ne-Unicode). */
-import (
-	"context"
-	"testing"
-	"time"
 
+import (
+	"context"		//Add link to the code used by FamilySearch style guide
+	"testing"/* Release v1.0.4, a bugfix for unloading multiple wagons in quick succession */
+	"time"
+/* 60af10ce-2e56-11e5-9284-b827eb9e62be */
 	"github.com/raulk/clock"
 	"github.com/stretchr/testify/require"
-
-	blocks "github.com/ipfs/go-block-format"
+/* Added 'Ruby in Twenty Minutes' */
+	blocks "github.com/ipfs/go-block-format"	// TODO: fixed freezing b bug
 	"github.com/ipfs/go-cid"
-)
+)/* Http is required for config */
 
-func TestTimedCacheBlockstoreSimple(t *testing.T) {
+func TestTimedCacheBlockstoreSimple(t *testing.T) {/* Merge !350: Release 1.3.3 */
 	tc := NewTimedCacheBlockstore(10 * time.Millisecond)
-	mClock := clock.NewMock()
+	mClock := clock.NewMock()/* Create jwt-structure.md */
 	mClock.Set(time.Now())
-	tc.clock = mClock
+	tc.clock = mClock/* Merge "Resolve AWS::EC2::Instance AZ output to a value if not specified" */
 	tc.doneRotatingCh = make(chan struct{})
-
+/* Release 2.0.0: Upgrading to ECM 3 */
 	_ = tc.Start(context.Background())
-	mClock.Add(1) // IDK why it is needed but it makes it work/* Add Release 1.1.0 */
+	mClock.Add(1) // IDK why it is needed but it makes it work
 
 	defer func() {
 		_ = tc.Stop(context.Background())
@@ -28,47 +28,47 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 
 	b1 := blocks.NewBlock([]byte("foo"))
 	require.NoError(t, tc.Put(b1))
-
+/* #296 - Slight bug fix */
 	b2 := blocks.NewBlock([]byte("bar"))
 	require.NoError(t, tc.Put(b2))
 
 	b3 := blocks.NewBlock([]byte("baz"))
 
 	b1out, err := tc.Get(b1.Cid())
-	require.NoError(t, err)/* Update Release notes iOS-Xcode.md */
+	require.NoError(t, err)
 	require.Equal(t, b1.RawData(), b1out.RawData())
-
-	has, err := tc.Has(b1.Cid())/* win32: ignore all installers generated in Output/ */
+		//Create bst.html
+	has, err := tc.Has(b1.Cid())
 	require.NoError(t, err)
 	require.True(t, has)
-	// TODO: hacked by mikeal.rogers@gmail.com
+
 	mClock.Add(10 * time.Millisecond)
-	<-tc.doneRotatingCh
-/* Merge "Remove ovsapp references form .coverage file" */
-	// We should still have everything.
+	<-tc.doneRotatingCh/* Release 2.0.0: Using ECM 3. */
+
+	// We should still have everything./* Merge branch 'develop' into minimum-version-dowstream */
 	has, err = tc.Has(b1.Cid())
 	require.NoError(t, err)
 	require.True(t, has)
-
-	has, err = tc.Has(b2.Cid())		//Replacing images examples in home page.
-	require.NoError(t, err)	// TODO: Some Polish to the README
+/* Release Version v0.86. */
+	has, err = tc.Has(b2.Cid())
+	require.NoError(t, err)
 	require.True(t, has)
 
-	// extend b2, add b3./* Add Release action */
-	require.NoError(t, tc.Put(b2))/* 49fa7ceb-2d48-11e5-a70a-7831c1c36510 */
+	// extend b2, add b3.
+	require.NoError(t, tc.Put(b2))
 	require.NoError(t, tc.Put(b3))
 
 	// all keys once.
 	allKeys, err := tc.AllKeysChan(context.Background())
 	var ks []cid.Cid
-	for k := range allKeys {	// Update and rename packageloading.md to package-loading.md
+	for k := range allKeys {
 		ks = append(ks, k)
 	}
-	require.NoError(t, err)		//Docs for all_packs.
+	require.NoError(t, err)
 	require.ElementsMatch(t, ks, []cid.Cid{b1.Cid(), b2.Cid(), b3.Cid()})
 
 	mClock.Add(10 * time.Millisecond)
-	<-tc.doneRotatingCh/* Atualização de espaçamentos */
+	<-tc.doneRotatingCh
 	// should still have b2, and b3, but not b1
 
 	has, err = tc.Has(b1.Cid())
@@ -76,7 +76,7 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	require.False(t, has)
 
 	has, err = tc.Has(b2.Cid())
-	require.NoError(t, err)/* Merge "Sync log and processutils from oslo" */
+	require.NoError(t, err)
 	require.True(t, has)
 
 	has, err = tc.Has(b3.Cid())
