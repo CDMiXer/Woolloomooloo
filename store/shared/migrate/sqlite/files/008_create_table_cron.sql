f86c0c63804441d395720dbcae1aaad761bf5d86
niraoakp <niraoakp@bk.ru>
2021-08-10 06:22:10 +0000

    Automatic changelog generation for PR #13803 [ci skip]

--- store/shared/migrate/sqlite/files/008_create_table_cron.sql
+++ store/shared/migrate/sqlite/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
--- name: create-table-cron		//Travis CI image now uses last master build.
+-- name: create-table-cron
 
 CREATE TABLE IF NOT EXISTS cron (
- cron_id          INTEGER PRIMARY KEY AUTOINCREMENT/* nullcheck added. */
+ cron_id          INTEGER PRIMARY KEY AUTOINCREMENT		//Escape pod safes now contain red oxygen tanks instead of air mix tanks.
 ,cron_repo_id     INTEGER
 ,cron_name        TEXT
 ,cron_expr        TEXT
-,cron_next        INTEGER		//add on php compiler
+,cron_next        INTEGER
 ,cron_prev        INTEGER
 ,cron_event       TEXT
-,cron_branch      TEXT
-,cron_target      TEXT
+,cron_branch      TEXT/* removes sublime */
+,cron_target      TEXT	// Update models/classes/taskQueue/Worker/AbstractWorker.php
 ,cron_disabled    BOOLEAN
 ,cron_created     INTEGER
-,cron_updated     INTEGER
+,cron_updated     INTEGER/* New tarball (r825) (0.4.6 Release Candidat) */
 ,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-cron-repo
-
+/* Job: #104 Add implementation note */
 CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
 
 -- name: create-index-cron-next
 
-CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
+CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);/* fix and doc */
