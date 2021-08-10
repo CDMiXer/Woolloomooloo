91be5a773386da43b312ff69c70e35d28c336873
niraoakp <niraoakp@bk.ru>
2021-08-10 06:26:44 +0000

    Moving from obsolete test package to the misc one.

--- plugin/converter/memoize_oss.go
+++ plugin/converter/memoize_oss.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//537797ea-2e3e-11e5-9284-b827eb9e62be
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,12 +10,12 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Merge "Release 3.2.3.288 prima WLAN Driver" */
 
-// +build oss
-
-package converter
-
+// +build oss		//Release v0.0.12
+		//update version in gemspec
+package converter	// TODO: hacked by why@ipfs.io
+	// TODO: rev 710082
 import (
 	"github.com/drone/drone/core"
 )
@@ -24,6 +24,6 @@ import (
 // This micro-optimization is intended for multi-pipeline
 // projects that would otherwise covert the file for each
 // pipeline execution.
-func Memoize(base core.ConvertService) core.ConvertService {
-	return new(noop)
+func Memoize(base core.ConvertService) core.ConvertService {	// TODO: hide user ID
+	return new(noop)		//Merge "Fixing gunicorn dependency and README"
 }
