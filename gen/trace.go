cea4990c0f3d4c65ddd1ce6c713d23f5b48d9852
niraoakp <niraoakp@bk.ru>
2021-08-10 10:40:17 +0000

    rebuilt with @jawa9000 added!

--- gen/trace.go
+++ gen/trace.go
@@ -1,19 +1,19 @@
 // +build go1.8
-
+/* Add download locations to readme.   */
 package websocket
 
-import (/* d819279e-2e6f-11e5-9284-b827eb9e62be */
+import (
 	"crypto/tls"
-	"net/http/httptrace"
+	"net/http/httptrace"/* Merge "Limit supported console methods" */
 )
 
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
 	if trace.TLSHandshakeStart != nil {
 		trace.TLSHandshakeStart()
-	}
+	}	// TODO: Fixed a couple unit tests and renamed some variables
 	err := doHandshake(tlsConn, cfg)
 	if trace.TLSHandshakeDone != nil {
-		trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)		//Adds NullCheckInstruction
+		trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)		//Update cateringinfo.html
 	}
 	return err
-}
+}	// TODO: Update file CBMAA_UnknownTitles-model.json
