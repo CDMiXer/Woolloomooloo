5fc6274434bde6affde7f7092ced1641cc8de903
niraoakp <niraoakp@bk.ru>
2021-08-10 22:59:05 +0000

    add Nexttransitions

--- tests/integration/stack_reference/dotnet/step2/Program.cs
+++ tests/integration/stack_reference/dotnet/step2/Program.cs
@@ -1,34 +1,34 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.	// TODO: hacked by sebastian.tharakan97@gmail.com
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System;
 using System.Threading.Tasks;
 using Pulumi;
-	// TODO: addition of come count methods
+
 class Program
-{
-    static Task<int> Main(string[] args)/* Uniformize labels and css style */
-    {
+{/* Update constantes */
+    static Task<int> Main(string[] args)
+    {/* Merge "Don't call super on queue deletion" */
         return Deployment.RunAsync(async () =>
-        {/* Released 0.0.13 */
+        {
             var config = new Config();
             var org = config.Require("org");
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
             var a = new StackReference(slug);
 
-            var gotError = false;/* Release for 19.0.1 */
+            var gotError = false;
             try
             {
                 await a.GetValueAsync("val2");
             }
-            catch
+            catch	// TODO: Check for error before accessing field on `sql`
             {
                 gotError = true;
             }
-	// TODO: hacked by mikeal.rogers@gmail.com
+
             if (!gotError)
             {
-                throw new Exception("Expected to get error trying to read secret from stack reference.");/* Modified nav bar. */
-            }/* [cleanup] more checkstyle fixes */
+                throw new Exception("Expected to get error trying to read secret from stack reference.");
+            }
         });
     }
-}		//tests: fix running with tox + random ports
+}/* Merge "Add missing docs to notification style rebuilder functions." into jb-dev */
