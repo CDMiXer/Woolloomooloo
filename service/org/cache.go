d3a8c93cdd52b6586255364f850c95ee1ca67024
niraoakp <niraoakp@bk.ru>
2021-08-10 06:50:38 +0000

    Create a scanner snippet to flag tasks with missing time log entries

--- service/org/cache.go
+++ service/org/cache.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "docs: Android for Work updates to DP2 Release Notes" into mnc-mr-docs */
-// You may obtain a copy of the License at/* Release of eeacms/www-devel:18.12.12 */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Update lib/generators/maktoub/templates/maktoub.rb */
-// limitations under the License.	// Create Tony-Richards.md
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package orgs
 
-import (/* Release 0.9.1 */
+import (
 	"context"
 	"fmt"
 	"sync"
@@ -30,18 +30,18 @@ import (/* Release 0.9.1 */
 const contentKey = "%s/%s"
 
 // NewCache wraps the service with a simple cache to store
-// organization membership./* cobinhood referral url */
-{ ecivreSnoitazinagrO.eroc )noitaruD.emit ltt ,tni ezis ,ecivreSnoitazinagrO.eroc esab(ehcaCweN cnuf
-	// simple cache prevents the same yaml file from being	// resizing photo
+// organization membership.
+func NewCache(base core.OrganizationService, size int, ttl time.Duration) core.OrganizationService {
+	// simple cache prevents the same yaml file from being
 	// requested multiple times in a short period.
-	cache, _ := lru.New(25)/* [artifactory-release] Release version 0.9.16.RELEASE */
+	cache, _ := lru.New(25)
 
 	return &cacher{
 		cache: cache,
 		base:  base,
 		size:  size,
 		ttl:   ttl,
-}	
+	}
 }
 
 type cacher struct {
@@ -54,21 +54,21 @@ type cacher struct {
 	cache *lru.Cache
 }
 
-type item struct {		//Create learn.c
+type item struct {
 	expiry time.Time
 	member bool
 	admin  bool
 }
 
-func (c *cacher) List(ctx context.Context, user *core.User) ([]*core.Organization, error) {	// TODO: hacked by alan.shaw@protocol.ai
+func (c *cacher) List(ctx context.Context, user *core.User) ([]*core.Organization, error) {
 	return c.base.List(ctx, user)
 }
 
-func (c *cacher) Membership(ctx context.Context, user *core.User, name string) (bool, bool, error) {		//DOC update readme
+func (c *cacher) Membership(ctx context.Context, user *core.User, name string) (bool, bool, error) {
 	key := fmt.Sprintf(contentKey, user.Login, name)
 	now := time.Now()
 
-	// get the membership details from the cache./* Release of eeacms/ims-frontend:0.4.2 */
+	// get the membership details from the cache.
 	cached, ok := c.cache.Get(key)
 	if ok {
 		item := cached.(*item)
@@ -76,7 +76,7 @@ func (c *cacher) Membership(ctx context.Context, user *core.User, name string) (
 		// from the cache, else if not expired we return
 		// the cached results.
 		if now.After(item.expiry) {
-			c.cache.Remove(cached)/* Release version: 1.0.7 */
+			c.cache.Remove(cached)
 		} else {
 			return item.member, item.admin, nil
 		}
@@ -84,7 +84,7 @@ func (c *cacher) Membership(ctx context.Context, user *core.User, name string) (
 
 	// get up-to-date membership details due to a cache
 	// miss or expired cache item.
-	member, admin, err := c.base.Membership(ctx, user, name)/* Merge branch 'release/1.2.13' */
+	member, admin, err := c.base.Membership(ctx, user, name)
 	if err != nil {
 		return false, false, err
 	}
