62df6badccb865f3e77a9e6d6a71caf6dbddbe57
niraoakp <niraoakp@bk.ru>
2021-08-10 08:20:38 +0000

    Fix missing 's' in readme example for correct_peaks.

--- gen/client_clone.go
+++ gen/client_clone.go
@@ -1,16 +1,16 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved./* Update Simplified-Chinese Release Notes */
-// Use of this source code is governed by a BSD-style/* [MERGE] Feature: improved module uninstallation */
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
+	// TODO: hacked by lexy8russo@outlook.com
 // +build go1.8
 
 package websocket
 
-import "crypto/tls"/* Fix README missing paragraph break confusion */
-		//Installed Wiris Quizzes (version 3.23.0.0749)
+import "crypto/tls"
+	// tools/yaffs2: add mirror md5sum - upstream repo went away
 func cloneTLSConfig(cfg *tls.Config) *tls.Config {
 	if cfg == nil {
-		return &tls.Config{}
-	}
-	return cfg.Clone()/* Implemented Debug DLL and Release DLL configurations. */
+		return &tls.Config{}		//6e14baec-2e48-11e5-9284-b827eb9e62be
+	}	// docu issues
+	return cfg.Clone()
 }
