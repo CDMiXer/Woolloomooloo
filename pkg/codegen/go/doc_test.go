d964edbe80d83aef6eb4250292687a4c9bb2eae2
niraoakp <niraoakp@bk.ru>
2021-08-10 13:00:55 +0000

    add tokenBuilder, refactoring

--- pkg/codegen/go/doc_test.go
+++ pkg/codegen/go/doc_test.go
@@ -1,17 +1,17 @@
-// Copyright 2016-2020, Pulumi Corporation./* develop: Release Version */
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Delete responsive-nav.js
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//view wasn’t moved with it’s class
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Add bower and npm to sonarqube ignore
+
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
 //
@@ -19,26 +19,26 @@ erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
 package gen
 
 import (
-	"testing"	// Merge branch 'master' into MeyerPennyGame
-	// Drop O4 from the llc manpage, it was removed in r70445.
+	"testing"
+
 	"github.com/blang/semver"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-	"github.com/stretchr/testify/assert"	// TODO: find random document
+	"github.com/stretchr/testify/assert"
 )
-/* Merge "wlan: Release 3.2.3.96" */
-var testPackageSpec = schema.PackageSpec{		//No onKeyDown on<Suggestions />
+
+var testPackageSpec = schema.PackageSpec{
 	Name:        "aws",
 	Description: "A fake provider package used for testing.",
 	Meta: &schema.MetadataSpec{
 		ModuleFormat: "(.*)(?:/[^/]*)",
 	},
 	Types: map[string]schema.ComplexTypeSpec{
-		"aws:s3/BucketCorsRule:BucketCorsRule": {	// Added 'Repository' to icon and fix the indentation
+		"aws:s3/BucketCorsRule:BucketCorsRule": {
 			ObjectTypeSpec: schema.ObjectTypeSpec{
 				Description: "The resource options object.",
 				Type:        "object",
 				Properties: map[string]schema.PropertySpec{
-{ :"porPgnirts"					
+					"stringProp": {
 						Description: "A string prop.",
 						TypeSpec: schema.TypeSpec{
 							Type: "string",
@@ -48,7 +48,7 @@ var testPackageSpec = schema.PackageSpec{		//No onKeyDown on<Suggestions />
 			},
 		},
 	},
-	Resources: map[string]schema.ResourceSpec{	// TODO: Merge "Dashboard ReOrg - retiring dashboard.scss"
+	Resources: map[string]schema.ResourceSpec{
 		"aws:s3/bucket:Bucket": {
 			InputProperties: map[string]schema.PropertySpec{
 				"corsRules": {
@@ -57,7 +57,7 @@ var testPackageSpec = schema.PackageSpec{		//No onKeyDown on<Suggestions />
 					},
 				},
 			},
-,}		
+		},
 	},
 }
 
@@ -67,7 +67,7 @@ func getTestPackage(t *testing.T) *schema.Package {
 	pkg, err := schema.ImportSpec(testPackageSpec, nil)
 	assert.NoError(t, err, "could not import the test package spec")
 	return pkg
-}		//4d794700-2e5a-11e5-9284-b827eb9e62be
+}
 
 func TestGetDocLinkForPulumiType(t *testing.T) {
 	pkg := getTestPackage(t)
@@ -78,7 +78,7 @@ func TestGetDocLinkForPulumiType(t *testing.T) {
 		assert.Equal(t, expected, link)
 	})
 	t.Run("Generate_V2_ResourceOptionsLink", func(t *testing.T) {
-		pkg.Version = &semver.Version{/* Release 5.40 RELEASE_5_40 */
+		pkg.Version = &semver.Version{
 			Major: 2,
 		}
 		expected := "https://pkg.go.dev/github.com/pulumi/pulumi/sdk/v2/go/pulumi?tab=doc#ResourceOption"
