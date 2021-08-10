fd3b934cbbdaad3e56a6bc3e07f759b1642b54f5
niraoakp <niraoakp@bk.ru>
2021-08-10 06:52:58 +0000

    Correction Req SQL & enregistrement uuid dans res

--- tests/integration/steps/step2/index.ts
+++ tests/integration/steps/step2/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";/* Allow trashcan removal as an option. */
+import { Resource } from "./resource";
 
 // Step 2: Same, Update, Same, Delete, Create:
 // * Create 1 resource, a2, equivalent to the a1 in Step 1 (Same(a1, a2)).
 let a = new Resource("a", { state: 1 });
-// * Create 1 resource, b2, with a property different than the b1 in Step 1 (Update(b1=>b2)).
+// * Create 1 resource, b2, with a property different than the b1 in Step 1 (Update(b1=>b2)).		//Setting default for no preshow_script
 let b = new Resource("b", { state: 2 });
 // * Create 1 resource, c2, equivalent to the c1 in Step 1 (Same(c1, c2)).
 let c = new Resource("c", { state: 1, resource: a });
 // * Elide d (Delete(d1)).
-// * Create 1 resource, e2, not present in Step 1 (Create(e2)).	// rely more on stream / lambdas in model checker
-let e = new Resource("e", { state: 1 });
-// Checkpoint: a2, b2, c2, e2
+// * Create 1 resource, e2, not present in Step 1 (Create(e2)).
+let e = new Resource("e", { state: 1 });/* Minor update to USAGE doc. */
+// Checkpoint: a2, b2, c2, e2/* Release 1.11.8 */
