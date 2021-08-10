edbbcfea8dd26a16bfce259c6896eedfcac21757
niraoakp <niraoakp@bk.ru>
2021-08-10 04:13:21 +0000

    Some improvements and fixes

--- tests/containers/python_venv/__main__.py
+++ tests/containers/python_venv/__main__.py
@@ -1,4 +1,4 @@
 import pulumi
-	// TODO: will be fixed by jon@atack.com
+
 config = pulumi.Config()
-print("Hello from %s" % (config.require("runtime")))		//Add TODO comment
+print("Hello from %s" % (config.require("runtime")))
