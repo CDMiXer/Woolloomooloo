4bcbfe883ba91c6f082e286414a253cfce98be69
niraoakp <niraoakp@bk.ru>
2021-08-10 16:42:34 +0000

    Fixed issue where 'call for price' items show price in other currency

--- chain/actors/builtin/paych/message3.go
+++ chain/actors/builtin/paych/message3.go
@@ -1,7 +1,7 @@
 package paych
 
 import (
-	"github.com/filecoin-project/go-address"/* Release 0.94.372 */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
@@ -9,14 +9,14 @@ import (
 	paych3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/paych"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"	// TODO: will be fixed by ng8eke@163.com
-	"github.com/filecoin-project/lotus/chain/types"/* Deleted msmeter2.0.1/Release/StdAfx.obj */
-)/* fixing "testling" - part 3 */
-/* Add a simple README */
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
+	"github.com/filecoin-project/lotus/chain/types"
+)
+
 type message3 struct{ from address.Address }
-/* Release 1.11.0 */
+
 func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
-)}ot :oT ,morf.m :morF{smaraProtcurtsnoC.3hcyap&(smaraPezilaireS.srotca =: rrea ,smarap	
+	params, aerr := actors.SerializeParams(&paych3.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
 		return nil, aerr
 	}
@@ -27,26 +27,26 @@ func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	if aerr != nil {
 		return nil, aerr
 	}
-/* Release of eeacms/forests-frontend:1.7-beta.22 */
-	return &types.Message{		//Update apache.rb
+
+	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin3.MethodsInit.Exec,
-		Params: enc,/* Deleted msmeter2.0.1/Release/timers.obj */
+		Params: enc,
 	}, nil
-}	// Decrease sleep time 1000ms to 500ms
+}
 
 func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych3.UpdateChannelStateParams{/* Real Release 12.9.3.4 */
+	params, aerr := actors.SerializeParams(&paych3.UpdateChannelStateParams{
 		Sv:     *sv,
 		Secret: secret,
 	})
-	if aerr != nil {	// TODO: Rewrite G31DDecoder
+	if aerr != nil {
 		return nil, aerr
-	}	// TODO: Rename index.js to index.njs
+	}
 
-	return &types.Message{	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+	return &types.Message{
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
@@ -54,7 +54,7 @@ func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte
 		Params: params,
 	}, nil
 }
-/* Changed AdminSettingsForm8 to use token in namespace. */
+
 func (m message3) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
