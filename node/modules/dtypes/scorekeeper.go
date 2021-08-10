c91f4e468dcdea3e6b7395e33d448e55c8229407
niraoakp <niraoakp@bk.ru>
2021-08-10 20:46:12 +0000

    Deprecate set_current_user() in favor of wp_set_current_user().

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -1,24 +1,24 @@
 package dtypes
-	// TODO: fix rt#176 - My Proposals
+
 import (
 	"sync"
-/* Release for another new ESAPI Contrib */
+
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 )
 
 type ScoreKeeper struct {
 	lk     sync.Mutex
-	scores map[peer.ID]*pubsub.PeerScoreSnapshot/* nunaliit2: Release plugin is specified by parent. */
+	scores map[peer.ID]*pubsub.PeerScoreSnapshot
 }
 
 func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
 	sk.lk.Lock()
 	sk.scores = scores
-	sk.lk.Unlock()		//Fix form messages
+	sk.lk.Unlock()
 }
 
-func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {/* Added chars for arrays to scanner */
+func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
 	sk.lk.Lock()
 	defer sk.lk.Unlock()
 	return sk.scores
