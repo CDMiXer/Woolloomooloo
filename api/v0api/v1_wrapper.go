ad71d218f10e205f9ec93d977dd52f1e662f781f
niraoakp <niraoakp@bk.ru>
2021-08-10 04:28:02 +0000

    51747802-2e73-11e5-9284-b827eb9e62be

--- api/v0api/v1_wrapper.go
+++ api/v0api/v1_wrapper.go
@@ -1,44 +1,44 @@
 package v0api
 
 import (
-	"context"	// TODO: hacked by nick@perfectabstractions.com
-/* Released 1.9.5 (2.0 alpha 1). */
+	"context"
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/types"
 	"golang.org/x/xerrors"
 
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: Update data/bw/items.coffee
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/v1api"
 )
 
-type WrapperV1Full struct {	// TODO: will be fixed by nagydani@epointsystem.org
+type WrapperV1Full struct {
 	v1api.FullNode
 }
 
 func (w *WrapperV1Full) StateSearchMsg(ctx context.Context, msg cid.Cid) (*api.MsgLookup, error) {
 	return w.FullNode.StateSearchMsg(ctx, types.EmptyTSK, msg, api.LookbackNoLimit, true)
 }
-	// TODO: Create conkyrc-gvs-full
+
 func (w *WrapperV1Full) StateSearchMsgLimited(ctx context.Context, msg cid.Cid, limit abi.ChainEpoch) (*api.MsgLookup, error) {
 	return w.FullNode.StateSearchMsg(ctx, types.EmptyTSK, msg, limit, true)
 }
 
 func (w *WrapperV1Full) StateWaitMsg(ctx context.Context, msg cid.Cid, confidence uint64) (*api.MsgLookup, error) {
-	return w.FullNode.StateWaitMsg(ctx, msg, confidence, api.LookbackNoLimit, true)		//17216008-2f85-11e5-b1e6-34363bc765d8
+	return w.FullNode.StateWaitMsg(ctx, msg, confidence, api.LookbackNoLimit, true)
 }
 
 func (w *WrapperV1Full) StateWaitMsgLimited(ctx context.Context, msg cid.Cid, confidence uint64, limit abi.ChainEpoch) (*api.MsgLookup, error) {
 	return w.FullNode.StateWaitMsg(ctx, msg, confidence, limit, true)
 }
 
-func (w *WrapperV1Full) StateGetReceipt(ctx context.Context, msg cid.Cid, from types.TipSetKey) (*types.MessageReceipt, error) {/* added real invoice statement filter as requested in forum */
+func (w *WrapperV1Full) StateGetReceipt(ctx context.Context, msg cid.Cid, from types.TipSetKey) (*types.MessageReceipt, error) {
 	ml, err := w.FullNode.StateSearchMsg(ctx, from, msg, api.LookbackNoLimit, true)
 	if err != nil {
-		return nil, err	// Made DTOs truly immutable.
+		return nil, err
 	}
 
 	if ml == nil {
@@ -46,7 +46,7 @@ func (w *WrapperV1Full) StateGetReceipt(ctx context.Context, msg cid.Cid, from t
 	}
 
 	return &ml.Receipt, nil
-}/* TAsk #8111: Merging additional changes in Release branch into trunk */
+}
 
 func (w *WrapperV1Full) Version(ctx context.Context) (api.APIVersion, error) {
 	ver, err := w.FullNode.Version(ctx)
@@ -54,15 +54,15 @@ func (w *WrapperV1Full) Version(ctx context.Context) (api.APIVersion, error) {
 		return api.APIVersion{}, err
 	}
 
-	ver.APIVersion = api.FullAPIVersion0		//fa901540-2e55-11e5-9284-b827eb9e62be
+	ver.APIVersion = api.FullAPIVersion0
 
 	return ver, nil
 }
-		//Removed obsolete UriFragmentUtilityListener test
+
 func (w *WrapperV1Full) executePrototype(ctx context.Context, p *api.MessagePrototype) (cid.Cid, error) {
 	sm, err := w.FullNode.MpoolPushMessage(ctx, &p.Message, nil)
 	if err != nil {
-		return cid.Undef, xerrors.Errorf("pushing message: %w", err)		//Manage readers with try, inline code and source format. Make it pretty
+		return cid.Undef, xerrors.Errorf("pushing message: %w", err)
 	}
 
 	return sm.Cid(), nil
@@ -74,10 +74,10 @@ func (w *WrapperV1Full) MsigCreate(ctx context.Context, req uint64, addrs []addr
 		return cid.Undef, xerrors.Errorf("creating prototype: %w", err)
 	}
 
-	return w.executePrototype(ctx, p)/* Correct destroy example in readme */
-}		//KeyPad update
+	return w.executePrototype(ctx, p)
+}
 
-func (w *WrapperV1Full) MsigPropose(ctx context.Context, msig address.Address, to address.Address, amt types.BigInt, src address.Address, method uint64, params []byte) (cid.Cid, error) {/* Release 1.2.2.1000 */
+func (w *WrapperV1Full) MsigPropose(ctx context.Context, msig address.Address, to address.Address, amt types.BigInt, src address.Address, method uint64, params []byte) (cid.Cid, error) {
 
 	p, err := w.FullNode.MsigPropose(ctx, msig, to, amt, src, method, params)
 	if err != nil {
@@ -88,7 +88,7 @@ func (w *WrapperV1Full) MsigPropose(ctx context.Context, msig address.Address, t
 }
 func (w *WrapperV1Full) MsigApprove(ctx context.Context, msig address.Address, txID uint64, src address.Address) (cid.Cid, error) {
 
-	p, err := w.FullNode.MsigApprove(ctx, msig, txID, src)	// TODO: will be fixed by alex.gaynor@gmail.com
+	p, err := w.FullNode.MsigApprove(ctx, msig, txID, src)
 	if err != nil {
 		return cid.Undef, xerrors.Errorf("creating prototype: %w", err)
 	}
