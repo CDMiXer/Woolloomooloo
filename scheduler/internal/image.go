f3b135aed11ed3639f69d9f7113da9d1a1c56aff
niraoakp <niraoakp@bk.ru>
2021-08-10 18:50:00 +0000

    Agregados comentarios a clases comunes

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//FIX: added missing gLogger import
 
 // +build !oss
 
 package internal
 
 var defaultImage = "drone/controller:1"
-
+	// TODO: He 111 : Modification of the canopy.
 // DefaultImage returns the default dispatch image if none
 // is specified.
 func DefaultImage(image string) string {
 	if image == "" {
 		return defaultImage
-	}
+	}/* Added patch for upgrading from 0.4 to 0.5. */
 	return image
 }
