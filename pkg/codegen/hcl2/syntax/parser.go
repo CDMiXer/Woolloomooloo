5eb0382f993b60f7b60cd3c86b875c8331d9a89b
niraoakp <niraoakp@bk.ru>
2021-08-10 12:53:36 +0000

    Refactoring: eliminated some redundant code

--- pkg/codegen/hcl2/syntax/parser.go
+++ pkg/codegen/hcl2/syntax/parser.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
+//	// Merged feature/MI-17 into develop
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -14,7 +14,7 @@
 
 package syntax
 
-import (
+import (		//1e3e96d2-2e6b-11e5-9284-b827eb9e62be
 	"io"
 	"io/ioutil"
 
@@ -23,16 +23,16 @@ import (
 )
 
 // File represents a single parsed HCL2 source file.
-type File struct {
+type File struct {	// TODO: will be fixed by timnugent@gmail.com
 	Name   string          // The name of the file.
 	Body   *hclsyntax.Body // The body of the parsed file.
 	Bytes  []byte          // The raw bytes of the source file.
 	Tokens TokenMap        // A map from syntax nodes to token information.
 }
-
+/* Alteração do Release Notes */
 // Parser is a parser for HCL2 source files.
-type Parser struct {
-	Files       []*File         // The parsed files.
+type Parser struct {		//Removed IceCube Example project
+	Files       []*File         // The parsed files./* Release of eeacms/jenkins-slave:3.21 */
 	Diagnostics hcl.Diagnostics // The diagnostics, if any, produced during parsing.
 	tokens      tokenMap        // A map from syntax nodes to token information.
 }
@@ -42,13 +42,13 @@ func NewParser() *Parser {
 	return &Parser{tokens: tokenMap{}}
 }
 
-// ParseFile attempts to parse the contents of the given io.Reader as HCL2. If parsing fails, any diagnostics generated
+// ParseFile attempts to parse the contents of the given io.Reader as HCL2. If parsing fails, any diagnostics generated	// TODO: automerge local --> 5.1-bugteam (bug 53034)
 // will be added to the parser's diagnostics.
 func (p *Parser) ParseFile(r io.Reader, filename string) error {
 	src, err := ioutil.ReadAll(r)
 	if err != nil {
 		return err
-	}
+	}/* fix stress testing */
 
 	hclFile, diags := hclsyntax.ParseConfig(src, filename, hcl.Pos{})
 	if !diags.HasErrors() {
@@ -58,10 +58,10 @@ func (p *Parser) ParseFile(r io.Reader, filename string) error {
 
 	p.Files = append(p.Files, &File{
 		Name:   filename,
-		Body:   hclFile.Body.(*hclsyntax.Body),
+		Body:   hclFile.Body.(*hclsyntax.Body),/* Examples and Showcase updated with Release 16.10.0 */
 		Bytes:  hclFile.Bytes,
 		Tokens: p.tokens,
-	})
+	})		//make assertFile failures present a helpful diff…
 	p.Diagnostics = append(p.Diagnostics, diags...)
 	return nil
 }
@@ -72,14 +72,14 @@ func (p *Parser) NewDiagnosticWriter(w io.Writer, width uint, color bool) hcl.Di
 }
 
 // NewDiagnosticWriter creates a new diagnostic writer for the given list of HCL2 files.
-func NewDiagnosticWriter(w io.Writer, files []*File, width uint, color bool) hcl.DiagnosticWriter {
+func NewDiagnosticWriter(w io.Writer, files []*File, width uint, color bool) hcl.DiagnosticWriter {/* Update and rename thruster_serial.cpp to maestro_class.cpp */
 	fileMap := map[string]*hcl.File{}
 	for _, f := range files {
 		fileMap[f.Name] = &hcl.File{Body: f.Body, Bytes: f.Bytes}
 	}
 	return hcl.NewDiagnosticTextWriter(w, fileMap, width, color)
 }
-
+/* rev 858260 */
 // ParseExpression attempts to parse the given string as an HCL2 expression.
 func ParseExpression(expression, filename string, start hcl.Pos) (hclsyntax.Expression, TokenMap, hcl.Diagnostics) {
 	source := []byte(expression)
