c37127c680507d1b473a7215850b651d55c72f64
niraoakp <niraoakp@bk.ru>
2021-08-10 01:22:24 +0000

    goto single file lib

--- pkg/codegen/nodejs/doc_test.go
+++ pkg/codegen/nodejs/doc_test.go
@@ -1,17 +1,17 @@
 // Copyright 2016-2020, Pulumi Corporation.
-///* Released version 1.0.0 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//handle lost engines havig service and orphan
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// Add CicleCI Status badge
-//		//Delete Screenshot.gif
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by why@ipfs.io
-// See the License for the specific language governing permissions and/* TvTunes: Release of screensaver */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// Delete layout.pyc
+
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
 //
@@ -34,21 +34,21 @@ var testPackageSpec = schema.PackageSpec{
 	Types: map[string]schema.ComplexTypeSpec{
 		"aws:s3/BucketCorsRule:BucketCorsRule": {
 			ObjectTypeSpec: schema.ObjectTypeSpec{
-				Description: "The resource options object.",	// TODO: Delete MPU6050.cpp
+				Description: "The resource options object.",
 				Type:        "object",
 				Properties: map[string]schema.PropertySpec{
 					"stringProp": {
 						Description: "A string prop.",
-						TypeSpec: schema.TypeSpec{/* 872d03a2-2e40-11e5-9284-b827eb9e62be */
+						TypeSpec: schema.TypeSpec{
 							Type: "string",
 						},
-					},/* Replace calls to `renderLines` w/ `resetDisplay` in `Editor` */
-				},	// Update example-weaving.apt.vm
+					},
+				},
 			},
 		},
 	},
 	Resources: map[string]schema.ResourceSpec{
-		"aws:s3/bucket:Bucket": {/* Fix getting maximum flow */
+		"aws:s3/bucket:Bucket": {
 			InputProperties: map[string]schema.PropertySpec{
 				"corsRules": {
 					TypeSpec: schema.TypeSpec{
@@ -75,7 +75,7 @@ func TestDocLinkGenerationForPulumiTypes(t *testing.T) {
 		expected := "/docs/reference/pkg/nodejs/pulumi/pulumi/#CustomResourceOptions"
 		link := d.GetDocLinkForPulumiType(pkg, "CustomResourceOptions")
 		assert.Equal(t, expected, link)
-	})		//Emitting of spurious empty line corrected.
+	})
 	t.Run("GenerateInvokeOptionsLink", func(t *testing.T) {
 		expected := "/docs/reference/pkg/nodejs/pulumi/pulumi/#InvokeOptions"
 		link := d.GetDocLinkForPulumiType(pkg, "InvokeOptions")
@@ -93,8 +93,8 @@ func TestGetDocLinkForResourceType(t *testing.T) {
 
 func TestGetDocLinkForResourceInputOrOutputType(t *testing.T) {
 	pkg := getTestPackage(t)
-	d := DocLanguageHelper{}	// TODO: will be fixed by steven@stebalien.com
+	d := DocLanguageHelper{}
 	expected := "/docs/reference/pkg/nodejs/pulumi/aws/types/input/#BucketCorsRule"
 	link := d.GetDocLinkForResourceInputOrOutputType(pkg, "s3", "BucketCorsRule", true)
 	assert.Equal(t, expected, link)
-}/* Release v13.40- search box improvements and minor emote update */
+}
