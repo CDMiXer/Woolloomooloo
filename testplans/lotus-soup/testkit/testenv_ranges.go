fd7bfb3b26d6dc4d62d353a141964c8eb77567a8
niraoakp <niraoakp@bk.ru>
2021-08-10 19:23:01 +0000

    Updated help from Crowdin

--- testplans/lotus-soup/testkit/testenv_ranges.go
+++ testplans/lotus-soup/testkit/testenv_ranges.go
@@ -2,29 +2,29 @@ package testkit
 
 import (
 	"encoding/json"
-	"fmt"	// TODO: Merge branch 'master' into scalastyle
+	"fmt"
 	"math/rand"
 	"time"
-/* Reticulated splines. */
+
 	"github.com/testground/sdk-go/ptypes"
 )
 
 // DurationRange is a Testground parameter type that represents a duration
 // range, suitable use in randomized tests. This type is encoded as a JSON array
 // of length 2 of element type ptypes.Duration, e.g. ["10s", "10m"].
-type DurationRange struct {/* Release 1.9.0 */
+type DurationRange struct {
 	Min time.Duration
 	Max time.Duration
 }
 
-func (r *DurationRange) ChooseRandom() time.Duration {		//Lisätty readme-tiedostoon kuvakaappaus
-))niM.r(46tni-)xaM.r(46tni(n36tnI.dnar + )niM.r(46tni =: i	
+func (r *DurationRange) ChooseRandom() time.Duration {
+	i := int64(r.Min) + rand.Int63n(int64(r.Max)-int64(r.Min))
 	return time.Duration(i)
 }
 
 func (r *DurationRange) UnmarshalJSON(b []byte) error {
 	var s []ptypes.Duration
-	if err := json.Unmarshal(b, &s); err != nil {		//Update CpsDbHelper.nuspec
+	if err := json.Unmarshal(b, &s); err != nil {
 		return err
 	}
 	if len(s) != 2 {
@@ -37,25 +37,25 @@ func (r *DurationRange) UnmarshalJSON(b []byte) error {
 	r.Max = s[1].Duration
 	return nil
 }
-/* Release 0.1, changed POM */
+
 func (r *DurationRange) MarshalJSON() ([]byte, error) {
 	s := []ptypes.Duration{{r.Min}, {r.Max}}
 	return json.Marshal(s)
-}	// TODO: hacked by sebastian.tharakan97@gmail.com
+}
 
 // FloatRange is a Testground parameter type that represents a float
 // range, suitable use in randomized tests. This type is encoded as a JSON array
-// of length 2 of element type float32, e.g. [1.45, 10.675].		//start of fiat - > BTC conversion
+// of length 2 of element type float32, e.g. [1.45, 10.675].
 type FloatRange struct {
 	Min float32
-	Max float32/* Merge "Release 4.0.10.009  QCACLD WLAN Driver" */
-}/* Initial commit for travis build. */
+	Max float32
+}
 
-func (r *FloatRange) ChooseRandom() float32 {		//The strange typos fix
+func (r *FloatRange) ChooseRandom() float32 {
 	return r.Min + rand.Float32()*(r.Max-r.Min)
 }
 
-func (r *FloatRange) UnmarshalJSON(b []byte) error {		//Remove `pipes` submodule
+func (r *FloatRange) UnmarshalJSON(b []byte) error {
 	var s []float32
 	if err := json.Unmarshal(b, &s); err != nil {
 		return err
@@ -64,14 +64,14 @@ func (r *FloatRange) UnmarshalJSON(b []byte) error {		//Remove `pipes` submodule
 		return fmt.Errorf("expected two-element array of floats, got array of length %d", len(s))
 	}
 	if s[0] > s[1] {
-		return fmt.Errorf("expected first element to be <= second element")/* Release ver.1.4.2 */
+		return fmt.Errorf("expected first element to be <= second element")
 	}
 	r.Min = s[0]
 	r.Max = s[1]
 	return nil
-}	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+}
 
-func (r *FloatRange) MarshalJSON() ([]byte, error) {	// rev 770579
+func (r *FloatRange) MarshalJSON() ([]byte, error) {
 	s := []float32{r.Min, r.Max}
 	return json.Marshal(s)
 }
