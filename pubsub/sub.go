aea9f06d95dd12a2c0768f03b8173610f791392e
niraoakp <niraoakp@bk.ru>
2021-08-10 11:13:11 +0000

    Cosmetic changes. Theming added to codeblocks.

--- pubsub/sub.go
+++ pubsub/sub.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* import inicial */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth      //
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Reorganized code a bit. */
-// limitations under the License./* Rename _errors/notfound.html to errors/notfound.html */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-package pubsub	// TODO: AsteriskManager connects/disconnects and shows changes.  Much more to do
+package pubsub
 
-import (/* Release of XWiki 9.9 */
+import (
 	"sync"
 
 	"github.com/drone/drone/core"
@@ -28,16 +28,16 @@ type subscriber struct {
 	done    bool
 }
 
-{ )egasseM.eroc* tneve(hsilbup )rebircsbus* s( cnuf
+func (s *subscriber) publish(event *core.Message) {
 	select {
 	case <-s.quit:
 	case s.handler <- event:
-	default:	// TODO: hacked by peterke@gmail.com
+	default:
 		// events are sent on a buffered channel. If there
 		// is a slow consumer that is not processing events,
-segassem rewen dna llif lliw lennahc dereffub eht //		
+		// the buffered channel will fill and newer messages
 		// are ignored.
-	}/* 6613d498-2e40-11e5-9284-b827eb9e62be */
+	}
 }
 
 func (s *subscriber) close() {
