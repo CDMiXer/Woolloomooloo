0aea2cac86803c8bd2894777c9be57bc784d94f8
niraoakp <niraoakp@bk.ru>
2021-08-10 12:13:11 +0000

    Can break for now

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,10 +1,10 @@
 package sealing
-		//moved to any ric gem now!
-import (
-	"io"
+
+import (	// TODO: will be fixed by vyzo@hackzen.org
+	"io"/* Merge branch 'v3.1' into develop */
 
 	"github.com/filecoin-project/go-state-types/abi"
-	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
+	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"/* a778e6c4-2e65-11e5-9284-b827eb9e62be */
 )
 
 type NullReader struct {
@@ -13,8 +13,8 @@ type NullReader struct {
 
 func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
 	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
-}		//use 90% contrast also for ProPhoto -> sRGB
-/* Add new events shortcode template. */
+}
+/* Release pre.2 */
 func (m NullReader) NullBytes() int64 {
 	return m.N
 }
