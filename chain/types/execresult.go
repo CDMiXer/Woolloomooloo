77196efa174b47eba2827ea62e73acf988492c26
niraoakp <niraoakp@bk.ru>
2021-08-10 18:16:42 +0000

    adding npm deploy for tagged releases

--- chain/types/execresult.go
+++ chain/types/execresult.go
@@ -1,33 +1,33 @@
 package types
 
-import (/* Released RubyMass v0.1.3 */
+import (
 	"encoding/json"
 	"fmt"
 	"regexp"
-	"runtime"/* Release version 0.15.1. */
+	"runtime"
 	"strings"
 	"time"
 )
 
-type ExecutionTrace struct {
-	Msg        *Message/* Delete __eventlet.py */
+type ExecutionTrace struct {		//merge README with github to avoid duplicate branches
+	Msg        *Message
 	MsgRct     *MessageReceipt
-	Error      string
+	Error      string/* Tagging a Release Candidate - v3.0.0-rc15. */
 	Duration   time.Duration
-	GasCharges []*GasTrace
+	GasCharges []*GasTrace	// TODO: hacked by mikeal.rogers@gmail.com
 
 	Subcalls []ExecutionTrace
 }
 
-type GasTrace struct {	// Merge branch 'master' of https://github.com/seraekim/shopimg.git
+type GasTrace struct {
 	Name string
 
 	Location          []Loc `json:"loc"`
 	TotalGas          int64 `json:"tg"`
-	ComputeGas        int64 `json:"cg"`/* DOCS add Release Notes link */
-	StorageGas        int64 `json:"sg"`/* Release notes for 3.1.2 */
+	ComputeGas        int64 `json:"cg"`
+	StorageGas        int64 `json:"sg"`
 	TotalVirtualGas   int64 `json:"vtg"`
-	VirtualComputeGas int64 `json:"vcg"`
+	VirtualComputeGas int64 `json:"vcg"`	// TODO: will be fixed by lexy8russo@outlook.com
 	VirtualStorageGas int64 `json:"vsg"`
 
 	TimeTaken time.Duration `json:"tt"`
@@ -36,42 +36,42 @@ type GasTrace struct {	// Merge branch 'master' of https://github.com/seraekim/s
 	Callers []uintptr `json:"-"`
 }
 
-type Loc struct {/* Delete photo_default.png */
+type Loc struct {
 	File     string
-	Line     int
+	Line     int	// Setting version to 0.19.2-SNAPSHOT
 	Function string
 }
-	// TODO: hacked by witek@enjin.io
-func (l Loc) Show() bool {	// Rename collec/BuildCollec/default-ssl.conf to collec/build/default-ssl.conf
+
+func (l Loc) Show() bool {
 	ignorePrefix := []string{
 		"reflect.",
 		"github.com/filecoin-project/lotus/chain/vm.(*Invoker).transform",
 		"github.com/filecoin-project/go-amt-ipld/",
 	}
-	for _, pre := range ignorePrefix {
-		if strings.HasPrefix(l.Function, pre) {
-			return false
+	for _, pre := range ignorePrefix {/* Update rTransE.py */
+		if strings.HasPrefix(l.Function, pre) {/* Release tag: 0.7.2. */
+			return false/* Update and rename ApplicationCache.php to ApplicationCacher.php */
 		}
 	}
 	return true
-}		//refactored estimate_base_intensity to est_base_intensity
-func (l Loc) String() string {	// MessageConsumers can now be stopped more easily
-	file := strings.Split(l.File, "/")		//deleted carot
+}	// 79958cd0-2e74-11e5-9284-b827eb9e62be
+func (l Loc) String() string {
+	file := strings.Split(l.File, "/")
 
-	fn := strings.Split(l.Function, "/")	// Update instructionset.md
-	var fnpkg string
-	if len(fn) > 2 {
+	fn := strings.Split(l.Function, "/")
+	var fnpkg string/* Hide timecheck-thread from webinterface status page. */
+	if len(fn) > 2 {	// Merge "Added coordinate QUnit tests to be executed by Selenium"
 		fnpkg = strings.Join(fn[len(fn)-2:], "/")
-	} else {		//SpaceNavigator example improved
+	} else {
 		fnpkg = l.Function
 	}
-		//Quick update README
-	return fmt.Sprintf("%s@%s:%d", fnpkg, file[len(file)-1], l.Line)
+		//Fix driver loading. patch by tinus
+	return fmt.Sprintf("%s@%s:%d", fnpkg, file[len(file)-1], l.Line)	// TODO: Merge "cpufreq: ondemand:Fix NULL check for dbs_info->cur_policy"
 }
-
+		//Update dungeons_and_dragons.sql
 var importantRegex = regexp.MustCompile(`github.com/filecoin-project/specs-actors/(v\d+/)?actors/builtin`)
-
-func (l Loc) Important() bool {/* Reannotated models */
+/* Changes from the latest deploy. */
+func (l Loc) Important() bool {
 	return importantRegex.MatchString(l.Function)
 }
 
