0e353202a551eac30b65876fc66bb05ce9f54f97
niraoakp <niraoakp@bk.ru>
2021-08-10 16:01:28 +0000

    added first draft of a product system statistics page

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -1,27 +1,27 @@
 package ffiwrapper
 
 import (
-	"golang.org/x/xerrors"/* Fixed typo in latest Release Notes page title */
+	"golang.org/x/xerrors"/* Release 3.9.1. */
 
 	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
-	// TODO: hacked by witek@enjin.io
+
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)
+)/* Deleted Tag01 */
 
 // merge gaps between ranges which are close to each other
 //  TODO: more benchmarking to come up with more optimal number
-const mergeGaps = 32 << 20
+const mergeGaps = 32 << 20		//Fix use with current bzr.dev.
 
 // TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
 
-func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
+func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {	// Update js/i18n/grid.locale-pl.js
 	todo := pieceRun(offset.Padded(), size.Padded())
 	todo, err := rlepluslazy.Subtract(todo, unsealed)
-	if err != nil {/* Released Lift-M4 snapshots. Added support for Font Awesome v3.0.0 */
+	if err != nil {
 		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
-	}
+	}	// Added login link to sidebar
 
 	return rlepluslazy.JoinClose(todo, mergeGaps)
-}		//61862c20-2e3e-11e5-9284-b827eb9e62be
+}
