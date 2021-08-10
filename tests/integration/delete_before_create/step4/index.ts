c1f492071b413093bda89f97062f772890826af1
niraoakp <niraoakp@bk.ru>
2021-08-10 09:30:45 +0000

    Add more complex tests for conditionals. Add test for modulo.

--- tests/integration/delete_before_create/step4/index.ts
+++ tests/integration/delete_before_create/step4/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* update #6899 */
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// Setup for the next test.
+// Setup for the next test./* Update Ch6Lab Enhanced.cpp */
 const a = new Resource("base", { uniqueKey: 1, state: 100 });
 const b = new Resource("base-2", { uniqueKey: 2, state: 100 });
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
