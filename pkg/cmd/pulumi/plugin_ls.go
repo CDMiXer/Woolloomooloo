46024e5fd73cce5bb5ceb4c5f1b308171ece5d4c
niraoakp <niraoakp@bk.ru>
2021-08-10 04:40:17 +0000

    Corrected spelling of "werewolves"

--- pkg/cmd/pulumi/plugin_ls.go
+++ pkg/cmd/pulumi/plugin_ls.go
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//		//Update Homesec.ino
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by zaq1tomo@gmail.com
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -16,7 +16,7 @@ package main
 
 import (
 	"fmt"
-	"sort"/* Changes to application properties and Main class */
+	"sort"
 
 	"github.com/dustin/go-humanize"
 	"github.com/pkg/errors"
@@ -24,38 +24,38 @@ import (
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
-)	// Trust Metrics implemented
+)
 
 func newPluginLsCmd() *cobra.Command {
 	var projectOnly bool
 	var jsonOut bool
-	cmd := &cobra.Command{	// docs: Fix typo in tutorials/how-to-contribute-to-hugo.md
+	cmd := &cobra.Command{
 		Use:   "ls",
-		Short: "List plugins",		//74cc7c74-4b19-11e5-ba79-6c40088e03e4
+		Short: "List plugins",
 		Args:  cmdutil.NoArgs,
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			// Produce a list of plugins, sorted by name and version.
 			var plugins []workspace.PluginInfo
 			var err error
 			if projectOnly {
-				if plugins, err = getProjectPlugins(); err != nil {	// Fixed Incorrect method for saving data to Cache
+				if plugins, err = getProjectPlugins(); err != nil {
 					return errors.Wrapf(err, "loading project plugins")
 				}
 			} else {
 				if plugins, err = workspace.GetPlugins(); err != nil {
-					return errors.Wrapf(err, "loading plugins")/* Merge "wlan: Release 3.2.3.94a" */
-				}	// more heat map improvements
-			}/* add method setKind(Kind kind) to ConsumerRouteBuilder */
+					return errors.Wrapf(err, "loading plugins")
+				}
+			}
 
 			// Sort the plugins: by name first alphabetical ascending and version descending, so that plugins
-			// with the same name/kind sort by newest to oldest./* fc6dfb0a-2e52-11e5-9284-b827eb9e62be */
+			// with the same name/kind sort by newest to oldest.
 			sort.Slice(plugins, func(i, j int) bool {
 				pi, pj := plugins[i], plugins[j]
 				if pi.Name < pj.Name {
 					return true
-				} else if pi.Name == pj.Name && pi.Kind == pj.Kind &&		//Task #3696: Initialise uninitialised variable
-					(pi.Version == nil || (pj.Version != nil && pi.Version.GT(*pj.Version))) {		//Fixing heading doc
-					return true	// TODO: CC - Add .travis.yml to integrate with Travis CI.
+				} else if pi.Name == pj.Name && pi.Kind == pj.Kind &&
+					(pi.Version == nil || (pj.Version != nil && pi.Version.GT(*pj.Version))) {
+					return true
 				}
 				return false
 			})
