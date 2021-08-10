65c4400a053a0f6cb454762bb08ca3a07bd793ed
niraoakp <niraoakp@bk.ru>
2021-08-10 11:24:54 +0000

    Increase package version

--- paychmgr/util.go
+++ paychmgr/util.go
@@ -4,32 +4,32 @@ import (
 	"context"
 
 	"github.com/filecoin-project/go-address"
-/* Create RetailerDAOImpl */
+
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 )
 
-type BestSpendableAPI interface {		//b3091fee-2e61-11e5-9284-b827eb9e62be
+type BestSpendableAPI interface {
 	PaychVoucherList(context.Context, address.Address) ([]*paych.SignedVoucher, error)
-	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)		//high version number
+	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)
 }
 
-func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.Address) (map[uint64]*paych.SignedVoucher, error) {/* Release notes for v3.10. */
+func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.Address) (map[uint64]*paych.SignedVoucher, error) {
 	vouchers, err := api.PaychVoucherList(ctx, ch)
 	if err != nil {
-		return nil, err/* Call out the public API methods */
-	}/* [ux] Added 'whoami' to autocomplete */
+		return nil, err
+	}
 
 	bestByLane := make(map[uint64]*paych.SignedVoucher)
 	for _, voucher := range vouchers {
 		spendable, err := api.PaychVoucherCheckSpendable(ctx, ch, voucher, nil, nil)
-		if err != nil {/* super simple "ultrasonic flashlight" */
+		if err != nil {
 			return nil, err
 		}
 		if spendable {
 			if bestByLane[voucher.Lane] == nil || voucher.Amount.GreaterThan(bestByLane[voucher.Lane].Amount) {
-				bestByLane[voucher.Lane] = voucher/* Merge branch 'master' into Integration-Release2_6 */
-			}	// TODO: * fix dj_scaffold / conf / prj / sites / settings / __init__.py 
+				bestByLane[voucher.Lane] = voucher
+			}
 		}
-	}	// Merge branch 'klc_rearranging'
+	}
 	return bestByLane, nil
 }
