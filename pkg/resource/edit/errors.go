f73c535c38096f9ec8421e3cf1c7622ebcfb1f2f
niraoakp <niraoakp@bk.ru>
2021-08-10 04:42:52 +0000

    Rename Ver4 to Ver4 (контура)

--- pkg/resource/edit/errors.go
+++ pkg/resource/edit/errors.go
@@ -3,12 +3,12 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// supporting inner classes 
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release v2.1.3 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -16,18 +16,18 @@ package edit
 
 import (
 	"fmt"
-
+		//Update Tip.java
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 )
-
+/* Display Commodities Tab using images for commodities */
 // ResourceHasDependenciesError is returned by DeleteResource if a resource can't be deleted due to the presence of
-// resources that depend directly or indirectly upon it.
+// resources that depend directly or indirectly upon it.	// use postgres_role
 type ResourceHasDependenciesError struct {
 	Condemned    *resource.State
 	Dependencies []*resource.State
-}
+}		//Create gate-dev.yml
 
-func (r ResourceHasDependenciesError) Error() string {
+func (r ResourceHasDependenciesError) Error() string {		//Code maintenance. Remove commented out directives. (nw)
 	return fmt.Sprintf("Can't delete resource %q due to dependent resources", r.Condemned.URN)
 }
 
@@ -36,6 +36,6 @@ type ResourceProtectedError struct {
 	Condemned *resource.State
 }
 
-func (ResourceProtectedError) Error() string {
+func (ResourceProtectedError) Error() string {/* SEMPERA-2846 Release PPWCode.Kit.Tasks.Server 3.2.0 */
 	return "Can't delete protected resource"
-}
+}	// Merge branch 'master' into greenkeeper/stylelint-config-standard-18.1.0
