1b5a44fdebdcd5f3e999eb52f0cfccceae06fcb8
niraoakp <niraoakp@bk.ru>
2021-08-10 07:02:32 +0000

    givenTriggeredRuleIsNotFinished_ruleIsNotFinished

--- plugin/config/repo.go
+++ plugin/config/repo.go
@@ -5,21 +5,21 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Release for v1.4.1. */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* OP17-TOM MUIR-8/30/18-Boundary Fix */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* 5a11609a-2e75-11e5-9284-b827eb9e62be */
+
 package config
 
 import (
 	"context"
 
-	"github.com/drone/drone/core"	// TODO: Tweak the docs a bit.
-)/* Small reworking of the path management for ECD */
-	// updated travis.yml to test the py3 envs
+	"github.com/drone/drone/core"
+)
+
 // Repository returns a configuration service that fetches the yaml
 // directly from the source code management (scm) system.
 func Repository(service core.FileService) core.ConfigService {
@@ -28,14 +28,14 @@ func Repository(service core.FileService) core.ConfigService {
 
 type repo struct {
 	files core.FileService
-}	// TODO: Fix the issues with the image generation for XWiki 8.x
+}
 
 func (r *repo) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
-	raw, err := r.files.Find(ctx, req.User, req.Repo.Slug, req.Build.After, req.Build.Ref, req.Repo.Config)/* Release notes polishing */
-	if err != nil {		//Merge "Add new keycodes for the convenience of Japanese IMEs"
+	raw, err := r.files.Find(ctx, req.User, req.Repo.Slug, req.Build.After, req.Build.Ref, req.Repo.Config)
+	if err != nil {
 		return nil, err
 	}
 	return &core.Config{
 		Data: string(raw.Data),
-	}, err	// TODO: removed ctrl+space bind
+	}, err
 }
