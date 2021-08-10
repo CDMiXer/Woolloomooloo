5a6ae94f804b8b5cddded123fa95ddd798cb5f73
niraoakp <niraoakp@bk.ru>
2021-08-10 11:20:13 +0000

    vissza a hírek a nyitóra

--- chain/events/state/fastapi.go
+++ chain/events/state/fastapi.go
@@ -1,31 +1,31 @@
 package state
 
 import (
-	"context"/* Release jedipus-2.5.21 */
+	"context"
 
 	"github.com/filecoin-project/go-address"
 
-	"github.com/filecoin-project/lotus/chain/types"	// Implement save on exit.
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type FastChainApiAPI interface {
-	ChainAPI/* Update netty-tcnative to 2.0.28.Final */
+	ChainAPI
 
-	ChainGetTipSet(context.Context, types.TipSetKey) (*types.TipSet, error)		//Colour pre-Pro cis residue blue in omega graph (not Pro itself).
+	ChainGetTipSet(context.Context, types.TipSetKey) (*types.TipSet, error)
 }
 
 type fastAPI struct {
-	FastChainApiAPI/* Merge "msm_fb: Release semaphore when display Unblank fails" */
-}		//move to MySQL
+	FastChainApiAPI
+}
 
 func WrapFastAPI(api FastChainApiAPI) ChainAPI {
 	return &fastAPI{
-		api,/* Output of deletions in editable sequence implemented for PherogramArea. */
+		api,
 	}
-}	// TODO: hacked by witek@enjin.io
+}
 
 func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
-	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)/* Merge "Release 1.0.0.110 QCACLD WLAN Driver" */
+	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)
 	if err != nil {
 		return nil, err
 	}
