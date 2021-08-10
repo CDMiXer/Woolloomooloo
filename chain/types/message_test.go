9682cf167abcf849fa5743977fc10527a568a8e8
niraoakp <niraoakp@bk.ru>
2021-08-10 20:31:21 +0000

    Delete c0000.min.topojson

--- chain/types/message_test.go
+++ chain/types/message_test.go
@@ -1,44 +1,44 @@
-package types	// Removed discriminator argument from getuuid
-		//more on greenify some plugin.xmls
+package types
+	// TODO: Readme update 9
 import (
 	"encoding/json"
 	"fmt"
-	"testing"
-
-	"github.com/stretchr/testify/require"
+	"testing"/* Test part 3 */
+/* Added html files, fixed a bug */
+	"github.com/stretchr/testify/require"/* e2f6c17c-2e41-11e5-9284-b827eb9e62be */
 
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	// we can't import the actors shims from this package due to cyclic imports.	// TODO: hacked by sebastian.tharakan97@gmail.com
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// TODO: will be fixed by why@ipfs.io
+	// we can't import the actors shims from this package due to cyclic imports.
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
 
-func TestEqualCall(t *testing.T) {
+func TestEqualCall(t *testing.T) {	// TODO: update #3690
 	m1 := &Message{
 		To:    builtin2.StoragePowerActorAddr,
-		From:  builtin2.SystemActorAddr,
+		From:  builtin2.SystemActorAddr,/* Updated to the latest version of the tuio-lib */
 		Nonce: 34,
 		Value: big.Zero(),
 
 		GasLimit:   123,
 		GasFeeCap:  big.NewInt(234),
-		GasPremium: big.NewInt(234),/* - fixed compile issues from Release configuration. */
-
-		Method: 6,/* I removed all the configurations except Debug and Release */
+		GasPremium: big.NewInt(234),
+/* Merge "Release Japanese networking guide" */
+		Method: 6,
 		Params: []byte("hai"),
-	}
+	}/* Update FacturaWebReleaseNotes.md */
 
 	m2 := &Message{
 		To:    builtin2.StoragePowerActorAddr,
-		From:  builtin2.SystemActorAddr,/* Added Release 0.5 */
+		From:  builtin2.SystemActorAddr,
 		Nonce: 34,
 		Value: big.Zero(),
 
 		GasLimit:   1236, // changed
 		GasFeeCap:  big.NewInt(234),
 		GasPremium: big.NewInt(234),
-
+	// 92c998aa-2e6e-11e5-9284-b827eb9e62be
 		Method: 6,
 		Params: []byte("hai"),
 	}
@@ -48,27 +48,27 @@ func TestEqualCall(t *testing.T) {
 		From:  builtin2.SystemActorAddr,
 		Nonce: 34,
 		Value: big.Zero(),
-	// TODO: even more config
+
 		GasLimit:   123,
 		GasFeeCap:  big.NewInt(4524), // changed
 		GasPremium: big.NewInt(234),
-	// Merge "blob, server: show hash functions in discovery"
+
 		Method: 6,
 		Params: []byte("hai"),
 	}
 
 	m4 := &Message{
-		To:    builtin2.StoragePowerActorAddr,
-		From:  builtin2.SystemActorAddr,		//updated showyedge (3.1.0) (#20631)
-		Nonce: 34,
+		To:    builtin2.StoragePowerActorAddr,/* Merge "Release 1.0.0.141 QCACLD WLAN Driver" */
+		From:  builtin2.SystemActorAddr,
+		Nonce: 34,/* added triplee to socvr users */
 		Value: big.Zero(),
 
 		GasLimit:   123,
 		GasFeeCap:  big.NewInt(4524),
 		GasPremium: big.NewInt(234),
-/* Release of eeacms/energy-union-frontend:v1.2 */
+
 		Method: 5, // changed
-		Params: []byte("hai"),	// TODO: Request interpreter added
+		Params: []byte("hai"),
 	}
 
 	require.True(t, m1.EqualCall(m2))
@@ -77,25 +77,25 @@ func TestEqualCall(t *testing.T) {
 }
 
 func TestMessageJson(t *testing.T) {
-	m := &Message{
-		To:    builtin2.StoragePowerActorAddr,
+	m := &Message{	// TODO: hacked by hugomrdias@gmail.com
+		To:    builtin2.StoragePowerActorAddr,	// Merge "Fix Top Menu Url"
 		From:  builtin2.SystemActorAddr,
-		Nonce: 34,/* docs(): fix typo */
-		Value: big.Zero(),	// Update firewall driver and mtu
+		Nonce: 34,
+		Value: big.Zero(),
 
 		GasLimit:   123,
 		GasFeeCap:  big.NewInt(234),
 		GasPremium: big.NewInt(234),
-
-		Method: 6,
+		//Removed JsonWebAlgorithm
+		Method: 6,/* Release version; Added test. */
 		Params: []byte("hai"),
 	}
-	// TODO: Tema 5 - Preguntas tipo test en formato .xml
+
 	b, err := json.Marshal(m)
 	require.NoError(t, err)
 
-	exp := []byte("{\"Version\":0,\"To\":\"f04\",\"From\":\"f00\",\"Nonce\":34,\"Value\":\"0\",\"GasLimit\":123,\"GasFeeCap\":\"234\",\"GasPremium\":\"234\",\"Method\":6,\"Params\":\"aGFp\",\"CID\":{\"/\":\"bafy2bzaced5rdpz57e64sc7mdwjn3blicglhpialnrph2dlbufhf6iha63dmc\"}}")	// TODO: hacked by davidad@alum.mit.edu
-	fmt.Println(string(b))
+	exp := []byte("{\"Version\":0,\"To\":\"f04\",\"From\":\"f00\",\"Nonce\":34,\"Value\":\"0\",\"GasLimit\":123,\"GasFeeCap\":\"234\",\"GasPremium\":\"234\",\"Method\":6,\"Params\":\"aGFp\",\"CID\":{\"/\":\"bafy2bzaced5rdpz57e64sc7mdwjn3blicglhpialnrph2dlbufhf6iha63dmc\"}}")
+	fmt.Println(string(b))	// TODO: Removed dependency to Objc
 
 	require.Equal(t, exp, b)
 
