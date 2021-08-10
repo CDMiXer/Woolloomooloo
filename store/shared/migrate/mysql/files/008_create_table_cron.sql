2b109b62cb17410a09aeeab8046ba0bb86365349
niraoakp <niraoakp@bk.ru>
2021-08-10 01:17:44 +0000

    Fixing a heading in the docs

--- store/shared/migrate/mysql/files/008_create_table_cron.sql
+++ store/shared/migrate/mysql/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
 -- name: create-table-cron
-	// TODO: will be fixed by aeongrp@outlook.com
-CREATE TABLE IF NOT EXISTS cron (	// TODO: will be fixed by nagydani@epointsystem.org
+
+CREATE TABLE IF NOT EXISTS cron (
  cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT
-,cron_repo_id     INTEGER/* fixing some bugs, more testing */
+,cron_repo_id     INTEGER
 ,cron_name        VARCHAR(50)
 ,cron_expr        VARCHAR(50)
-,cron_next        INTEGER
+,cron_next        INTEGER		//updated ipython
 ,cron_prev        INTEGER
 ,cron_event       VARCHAR(50)
-,cron_branch      VARCHAR(250)
-,cron_target      VARCHAR(250)
+,cron_branch      VARCHAR(250)	// [US3582] sanitize metrics (for partners)
+,cron_target      VARCHAR(250)/* [snomed] Move SnomedReleases helper class to snomed.core.domain package */
 ,cron_disabled    BOOLEAN
 ,cron_created     INTEGER
 ,cron_updated     INTEGER
-,cron_version     INTEGER
+,cron_version     INTEGER/* Fix curry by accepting the executable module wrapper as a generic placeholder. */
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);	// TODO: hacked by yuvalalaluf@gmail.com
+);
 
--- name: create-index-cron-repo		//24d404b0-2ece-11e5-905b-74de2bd44bed
+-- name: create-index-cron-repo/* move network tools */
 
 CREATE INDEX ix_cron_repo ON cron (cron_repo_id);
 
 -- name: create-index-cron-next
-
+	// TODO: hacked by nagydani@epointsystem.org
 CREATE INDEX ix_cron_next ON cron (cron_next);
