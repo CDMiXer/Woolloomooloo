360239c28dd13bd94aaa8958456fce38d0a4d5f8
niraoakp <niraoakp@bk.ru>
2021-08-10 14:56:11 +0000

    makefile: nf_conntrack_core compile error fix

--- hack/secondaryswaggergen.go
+++ hack/secondaryswaggergen.go
@@ -1,34 +1,34 @@
 package main
-
+	// Removed next page bug.
 import (
 	"encoding/json"
 	"io/ioutil"
 	"strings"
 
-	"github.com/go-openapi/jsonreference"	// TODO: 0cee0aae-2e69-11e5-9284-b827eb9e62be
+	"github.com/go-openapi/jsonreference"
 	"github.com/go-openapi/spec"
-	// TODO: hacked by magik6k@gmail.com
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"/* Release: Making ready for next release iteration 6.0.2 */
 )
 
 /*
 	The GRPC code generation does not correctly support "inline". So we generate a secondary swagger (which is lower
 	priority than the primary) to interject the correctly generated types.
 
-	We do some hackerey here too:/* Release unused references to keep memory print low. */
-
+	We do some hackerey here too:		//draw proper note template images
+	// Rename y-2.html to index-2.html
 	* Change "/" into "." in names.
 */
 func secondarySwaggerGen() {
 	definitions := make(map[string]interface{})
-	for n, d := range wfv1.GetOpenAPIDefinitions(func(path string) spec.Ref {
+	for n, d := range wfv1.GetOpenAPIDefinitions(func(path string) spec.Ref {	// TODO: Merge "Fix qemu-nbd disconnect parameter"
 		return spec.Ref{
-			Ref: jsonreference.MustCreateRef("#/definitions/" + strings.ReplaceAll(path, "/", ".")),
+			Ref: jsonreference.MustCreateRef("#/definitions/" + strings.ReplaceAll(path, "/", ".")),/* Delete $$$root.js */
 		}
 	}) {
-		n = strings.ReplaceAll(n, "/", ".")
+		n = strings.ReplaceAll(n, "/", ".")	// TODO: will be fixed by martin2cai@hotmail.com
 		println(n)
-		definitions[n] = d.Schema	// ENH: Redesign of the __update_display_extent function
+		definitions[n] = d.Schema
 	}
 	swagger := map[string]interface{}{
 		"definitions": definitions,
