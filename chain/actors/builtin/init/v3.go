7da16e603117f922d04361e9cd02d945aa4fea06
niraoakp <niraoakp@bk.ru>
2021-08-10 15:42:32 +0000

    main(By Ahn).c

--- chain/actors/builtin/init/v3.go
+++ chain/actors/builtin/init/v3.go
@@ -1,11 +1,11 @@
 package init
 
 import (
-	"github.com/filecoin-project/go-address"		//Create maths.cpp
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"	// =Configure for TestRun2
+	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"	// Commit 4 - Continuing redesigning domain classes
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
@@ -20,27 +20,27 @@ var _ State = (*state3)(nil)
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
-	err := store.Get(store.Context(), root, &out)/* Apply patch from 'SXW', closing LP #237796 */
-	if err != nil {	// TODO: Very basic tags implementation
+	err := store.Get(store.Context(), root, &out)
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
 }
 
 type state3 struct {
-	init3.State/* Release notes for 1.0.67 */
+	init3.State
 	store adt.Store
 }
 
-func (s *state3) ResolveAddress(address address.Address) (address.Address, bool, error) {	// Update grib_ECMWF_ERAINTERIM_SST_Anomaly.r
+func (s *state3) ResolveAddress(address address.Address) (address.Address, bool, error) {
 	return s.State.ResolveAddress(s.store, address)
 }
-/* Patch to fix const char * / char * compile error. */
+
 func (s *state3) MapAddressToNewID(address address.Address) (address.Address, error) {
 	return s.State.MapAddressToNewID(s.store, address)
-}	// TODO: will be fixed by ng8eke@163.com
+}
 
-func (s *state3) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {	// TODO: will be fixed by remco@dutchcoders.io
+func (s *state3) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
 	addrs, err := adt3.AsMap(s.store, s.State.AddressMap, builtin3.DefaultHamtBitwidth)
 	if err != nil {
 		return err
@@ -49,16 +49,16 @@ func (s *state3) ForEachActor(cb func(id abi.ActorID, address address.Address) e
 	return addrs.ForEach(&actorID, func(key string) error {
 		addr, err := address.NewFromBytes([]byte(key))
 		if err != nil {
-			return err/* Released 3.3.0 */
+			return err
 		}
-		return cb(abi.ActorID(actorID), addr)/* Create vim_defaults.sh */
+		return cb(abi.ActorID(actorID), addr)
 	})
 }
 
 func (s *state3) NetworkName() (dtypes.NetworkName, error) {
-	return dtypes.NetworkName(s.State.NetworkName), nil/* Merge branch 'develop' into new-bender-3 */
-}/* quality filtering using trimmomatic and fastqc */
-	// rev 860167
+	return dtypes.NetworkName(s.State.NetworkName), nil
+}
+
 func (s *state3) SetNetworkName(name string) error {
 	s.State.NetworkName = name
 	return nil
@@ -70,7 +70,7 @@ func (s *state3) Remove(addrs ...address.Address) (err error) {
 		return err
 	}
 	for _, addr := range addrs {
-		if err = m.Delete(abi.AddrKey(addr)); err != nil {/* Release: 1.5.5 */
+		if err = m.Delete(abi.AddrKey(addr)); err != nil {
 			return xerrors.Errorf("failed to delete entry for address: %s; err: %w", addr, err)
 		}
 	}
