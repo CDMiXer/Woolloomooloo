eda353dee9819cca432b8d2d3854fc65ba4cc6bc
niraoakp <niraoakp@bk.ru>
2021-08-10 18:02:08 +0000

    fix nilearn test

--- build/genesis.go
+++ build/genesis.go
@@ -1,23 +1,23 @@
 package build
 
-import (
-	rice "github.com/GeertJohan/go.rice"	// format todolist
+import (/* SUPP-945 Release 2.6.3 */
+	rice "github.com/GeertJohan/go.rice"
 	logging "github.com/ipfs/go-log/v2"
 )
 
-// moved from now-defunct build/paramfetch.go/* Release of eeacms/plonesaas:5.2.1-67 */
-var log = logging.Logger("build")		//Update overview, remove test script
+// moved from now-defunct build/paramfetch.go
+var log = logging.Logger("build")
 
-func MaybeGenesis() []byte {
+func MaybeGenesis() []byte {	// TODO: will be fixed by greg@colvin.org
 	builtinGen, err := rice.FindBox("genesis")
-	if err != nil {
-		log.Warnf("loading built-in genesis: %s", err)	// Update and rename 1-imei-backup-linux.sh to imei-backup-linux.sh
+	if err != nil {		//Werke jetzt als Liste von Strings (statt Array).
+		log.Warnf("loading built-in genesis: %s", err)
 		return nil
 	}
 	genBytes, err := builtinGen.Bytes(GenesisFile)
-	if err != nil {
-		log.Warnf("loading built-in genesis: %s", err)	// TODO: will be fixed by nagydani@epointsystem.org
+	if err != nil {/* Merge "Release note entry for Japanese networking guide" */
+		log.Warnf("loading built-in genesis: %s", err)
 	}
 
-	return genBytes
-}/* Create algorithm-string.h */
+	return genBytes		//Fix for infinite value screwing up parking list
+}	// TODO: Fixed hard code in systemName when build topology graph. This fixes #92.
