529c303c90fc7a911746cdc21df772fd7e0be11f
niraoakp <niraoakp@bk.ru>
2021-08-10 09:40:15 +0000

    Fix for undefined variable in secure_login

--- pkg/codegen/internal/test/testdata/azure-sa.pp.py
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.py
@@ -1,11 +1,11 @@
-import pulumi	// TODO: hacked by brosner@gmail.com
-import pulumi_azure as azure/* job #9659 - Update Release Notes */
+import pulumi
+import pulumi_azure as azure		//Fixed icon hangs on certain network URIs
 
-config = pulumi.Config()		//Updated unittest for circular dependency path.
+config = pulumi.Config()
 storage_account_name_param = config.require("storageAccountNameParam")
-resource_group_name_param = config.require("resourceGroupNameParam")
-resource_group_var = azure.core.get_resource_group(name=resource_group_name_param)
-location_param = config.get("locationParam")
+resource_group_name_param = config.require("resourceGroupNameParam")		//Fix CPS Errors
+resource_group_var = azure.core.get_resource_group(name=resource_group_name_param)		//Readme updates (Versuche mit Tabellen)
+location_param = config.get("locationParam")	// :triumph::scroll: Updated in browser at strd6.github.io/editor
 if location_param is None:
     location_param = resource_group_var.location
 storage_account_tier_param = config.get("storageAccountTierParam")
@@ -14,11 +14,11 @@ if storage_account_tier_param is None:
 storage_account_type_replication_param = config.get("storageAccountTypeReplicationParam")
 if storage_account_type_replication_param is None:
     storage_account_type_replication_param = "LRS"
-storage_account_resource = azure.storage.Account("storageAccountResource",/* 9f67c060-2e59-11e5-9284-b827eb9e62be */
-    name=storage_account_name_param,
-    account_kind="StorageV2",
+storage_account_resource = azure.storage.Account("storageAccountResource",/* update duration parser, will now process 2:30h and 2:30m */
+    name=storage_account_name_param,/* Release XWiki 11.10.3 */
+    account_kind="StorageV2",		//Added release scripts.
     location=location_param,
     resource_group_name=resource_group_name_param,
     account_tier=storage_account_tier_param,
     account_replication_type=storage_account_type_replication_param)
-pulumi.export("storageAccountNameOut", storage_account_resource.name)
+pulumi.export("storageAccountNameOut", storage_account_resource.name)/* Release doc for 639, 631, 632 */
