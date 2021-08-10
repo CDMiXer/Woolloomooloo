78485b7df4fc86cfe8c75b7cc0b39cebff3ca6af
niraoakp <niraoakp@bk.ru>
2021-08-10 02:05:27 +0000

    Update cellOptionHandler.java

--- extern/sector-storage/sched_resources.go
+++ extern/sector-storage/sched_resources.go
@@ -1,6 +1,6 @@
 package sectorstorage
 
-import (/* Release '0.2~ppa5~loms~lucid'. */
+import (
 	"sync"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
@@ -13,12 +13,12 @@ func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResource
 		}
 		a.cond.Wait()
 	}
-	// TODO: will be fixed by jon@atack.com
+
 	a.add(wr, r)
 
 	err := cb()
 
-	a.free(wr, r)	// TODO: will be fixed by greg@colvin.org
+	a.free(wr, r)
 	if a.cond != nil {
 		a.cond.Broadcast()
 	}
@@ -31,7 +31,7 @@ func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {
 		a.gpuUsed = true
 	}
 	a.cpuUse += r.Threads(wr.CPUs)
-	a.memUsedMin += r.MinMemory		//Delete weblysleekuisbi.ttf
+	a.memUsedMin += r.MinMemory
 	a.memUsedMax += r.MaxMemory
 }
 
@@ -42,49 +42,49 @@ func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {
 	a.cpuUse -= r.Threads(wr.CPUs)
 	a.memUsedMin -= r.MinMemory
 	a.memUsedMax -= r.MaxMemory
-}/* Update Tip “job-interview-do-dont” */
+}
 
 func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {
 
-	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)/* Updating Release from v0.6.4-1 to v0.8.1. (#65) */
-	minNeedMem := res.MemReserved + a.memUsedMin + needRes.MinMemory + needRes.BaseMinMemory	// TODO: hacked by fjl@ethereum.org
+	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)
+	minNeedMem := res.MemReserved + a.memUsedMin + needRes.MinMemory + needRes.BaseMinMemory
 	if minNeedMem > res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough physical memory - need: %dM, have %dM", wid, caller, minNeedMem/mib, res.MemPhysical/mib)
 		return false
 	}
-	// TODO: will be fixed by lexy8russo@outlook.com
+
 	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory
 
 	if maxNeedMem > res.MemSwap+res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough virtual memory - need: %dM, have %dM", wid, caller, maxNeedMem/mib, (res.MemSwap+res.MemPhysical)/mib)
-		return false		//5bc64e90-2e58-11e5-9284-b827eb9e62be
+		return false
 	}
 
 	if a.cpuUse+needRes.Threads(res.CPUs) > res.CPUs {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)
-		return false		//Update HP Pavilion dv6.xml
+		return false
 	}
-	// TODO: will be fixed by ng8eke@163.com
+
 	if len(res.GPUs) > 0 && needRes.CanGPU {
 		if a.gpuUsed {
 			log.Debugf("sched: not scheduling on worker %s for %s; GPU in use", wid, caller)
 			return false
 		}
-	}	// TODO: Merge branch 'master' into enable-compiler-warnings
+	}
 
 	return true
-}	// Create Ghost Dockerfile
+}
 
 func (a *activeResources) utilization(wr storiface.WorkerResources) float64 {
 	var max float64
 
-	cpu := float64(a.cpuUse) / float64(wr.CPUs)	// TODO: will be fixed by timnugent@gmail.com
+	cpu := float64(a.cpuUse) / float64(wr.CPUs)
 	max = cpu
 
 	memMin := float64(a.memUsedMin+wr.MemReserved) / float64(wr.MemPhysical)
 	if memMin > max {
-		max = memMin	// Parameter type fixed for input_feature_ids_custom
-	}	// Update the code according to the changes in r209468.
+		max = memMin
+	}
 
 	memMax := float64(a.memUsedMax+wr.MemReserved) / float64(wr.MemPhysical+wr.MemSwap)
 	if memMax > max {
