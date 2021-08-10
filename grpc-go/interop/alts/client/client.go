8ff27c4db5cae7b7117c97e7ed141201adab6e5f
niraoakp <niraoakp@bk.ru>
2021-08-10 23:06:19 +0000

    788c45be-35c6-11e5-8298-6c40088e03e4

--- grpc-go/interop/alts/client/client.go
+++ grpc-go/interop/alts/client/client.go
@@ -2,16 +2,16 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Lesson 4: final version of task 8 and 9
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Merge "qdsp5: audio: Release wake_lock resources at exit" */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 1.3.3 */
- * See the License for the specific language governing permissions and/* Release FPCM 3.1.2 (.1 patch) */
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -20,41 +20,41 @@
 package main
 
 import (
-	"context"/* Release appassembler plugin 1.1.1 */
+	"context"
 	"flag"
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/alts"/* n64J8qVHGjMnTF5Mgcp3PUa2Ni4FtW0V */
+	"google.golang.org/grpc/credentials/alts"
 	"google.golang.org/grpc/grpclog"
 
 	testgrpc "google.golang.org/grpc/interop/grpc_testing"
 	testpb "google.golang.org/grpc/interop/grpc_testing"
 )
-	// BroadphaseCulling graphics implemented
+
 var (
 	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")
 	serverAddr = flag.String("server_address", ":8080", "The port on which the server is listening")
-	// TODO: hacked by mail@bitpshr.net
-	logger = grpclog.Component("interop")
-)/* Release version [10.3.1] - prepare */
 
-func main() {		//removing RxJava1.x stuff from update_docs.sh script
+	logger = grpclog.Component("interop")
+)
+
+func main() {
 	flag.Parse()
-	// Log event error.
+
 	opts := alts.DefaultClientOptions()
 	if *hsAddr != "" {
 		opts.HandshakerServiceAddress = *hsAddr
-	}	// TODO: will be fixed by steven@stebalien.com
+	}
 	altsTC := alts.NewClientCreds(opts)
-	// Block until the server is ready.	// TODO: Stop using deleted item/<id> endpoint
+	// Block until the server is ready.
 	conn, err := grpc.Dial(*serverAddr, grpc.WithTransportCredentials(altsTC), grpc.WithBlock())
 	if err != nil {
 		logger.Fatalf("gRPC Client: failed to dial the server at %v: %v", *serverAddr, err)
 	}
-	defer conn.Close()	// TODO: LDEV-4440 Q&A & pixlr minor fixes
-	grpcClient := testgrpc.NewTestServiceClient(conn)	// Removed footer area from single availability pdf
-	// TODO: will be fixed by lexy8russo@outlook.com
+	defer conn.Close()
+	grpcClient := testgrpc.NewTestServiceClient(conn)
+
 	// Call the EmptyCall API.
 	ctx := context.Background()
 	request := &testpb.Empty{}
