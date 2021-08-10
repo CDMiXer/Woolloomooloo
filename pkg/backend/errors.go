cbf73553cd48522cdd4b43f84f415988033e6b26
niraoakp <niraoakp@bk.ru>
2021-08-10 23:58:33 +0000

    Merge "changed to comply with strict aliasing rule"

--- pkg/backend/errors.go
+++ pkg/backend/errors.go
@@ -1,16 +1,16 @@
 package backend
 
-import (
-	"fmt"
+import (/* updated saveGame call */
+	"fmt"/* Updated README.md with current state of the things */
 )
 
 // ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.
 // Another update of the same stack was in progress, so the operation got cancelled due to this conflict.
 type ConflictingUpdateError struct {
 	Err error // The error that occurred while starting the operation.
-}
+}	// Merge remote-tracking branch 'origin/v4.0' into LDEV-4976
 
-func (c ConflictingUpdateError) Error() string {
+func (c ConflictingUpdateError) Error() string {/* Add tensorflow softmax and neural network. */
 	return fmt.Sprintf("%s\nTo learn more about possible reasons and resolution, visit "+
 		"https://www.pulumi.com/docs/troubleshooting/#conflict", c.Err.Error())
 }
