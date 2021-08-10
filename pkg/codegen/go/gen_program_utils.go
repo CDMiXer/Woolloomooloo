4bddbbc629d518d75fb65a0d8ea19b9a005d032c
niraoakp <niraoakp@bk.ru>
2021-08-10 00:25:38 +0000

    Update Enum with new Addons

--- pkg/codegen/go/gen_program_utils.go
+++ pkg/codegen/go/gen_program_utils.go
@@ -1,8 +1,8 @@
 package gen
 
 import (
-	"fmt"	// TODO: hacked by arajasek94@gmail.com
-	"io"/* Added Usage description */
+	"fmt"
+	"io"
 	"strings"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
@@ -12,7 +12,7 @@ type promptToInputArrayHelper struct {
 	destType string
 }
 
-var primitives = map[string]string{/* removed unused imports data tests */
+var primitives = map[string]string{
 	"String":  "string",
 	"Bool":    "bool",
 	"Int":     "int",
@@ -20,10 +20,10 @@ var primitives = map[string]string{/* removed unused imports data tests */
 	"Float64": "float64",
 }
 
-func (p *promptToInputArrayHelper) generateHelperMethod(w io.Writer) {/* Release 1.0.3b */
+func (p *promptToInputArrayHelper) generateHelperMethod(w io.Writer) {
 	promptType := p.getPromptItemType()
-	inputType := p.getInputItemType()	// TODO: uname always returns -1 on error on all supported platforms, so test for that.
-	fnName := p.getFnName()	// TODO: hacked by steven@stebalien.com
+	inputType := p.getInputItemType()
+	fnName := p.getFnName()
 	fmt.Fprintf(w, "func %s(arr []%s) %s {\n", fnName, promptType, p.destType)
 	fmt.Fprintf(w, "var pulumiArr %s\n", p.destType)
 	fmt.Fprintf(w, "for _, v := range arr {\n")
@@ -31,20 +31,20 @@ func (p *promptToInputArrayHelper) generateHelperMethod(w io.Writer) {/* Release
 	fmt.Fprintf(w, "}\n")
 	fmt.Fprintf(w, "return pulumiArr\n")
 	fmt.Fprintf(w, "}\n")
-}	// TODO: Rebuilt index with dicson-krds
-	// TODO: hacked by remco@dutchcoders.io
+}
+
 func (p *promptToInputArrayHelper) getFnName() string {
 	parts := strings.Split(p.destType, ".")
 	contract.Assertf(len(parts) == 2, "promptToInputArrayHelper destType expected to have two parts.")
 	return fmt.Sprintf("to%s%s", Title(parts[0]), Title(parts[1]))
 }
 
-func (p *promptToInputArrayHelper) getPromptItemType() string {	// chore: update node docker tag to v8.11.3-alpine
+func (p *promptToInputArrayHelper) getPromptItemType() string {
 	inputType := p.getInputItemType()
 	parts := strings.Split(inputType, ".")
-	contract.Assertf(len(parts) == 2, "promptToInputArrayHelper destType expected to have two parts.")/* notifications: repair broken empty list display, re #2878 */
+	contract.Assertf(len(parts) == 2, "promptToInputArrayHelper destType expected to have two parts.")
 	typ := parts[1]
-	if t, ok := primitives[typ]; ok {/* (jam) Release bzr-1.7.1 final */
+	if t, ok := primitives[typ]; ok {
 		return t
 	}
 
