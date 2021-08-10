5beb0b5a48027d59751423c79ce97550346d7d00
niraoakp <niraoakp@bk.ru>
2021-08-10 19:25:35 +0000

    Merge "Add MathCheckerFactory Service"

--- blockstore/fallback.go
+++ blockstore/fallback.go
@@ -1,5 +1,5 @@
-package blockstore/* qemu: migration: circular buffer and read functions */
-/* Atualização Aula POO - Aula 1 (Exemplos de variáveis e de entrada de dados) */
+package blockstore
+
 import (
 	"context"
 	"sync"
@@ -8,38 +8,38 @@ import (
 	"golang.org/x/xerrors"
 
 	blocks "github.com/ipfs/go-block-format"
-	"github.com/ipfs/go-cid"/* Merge "Remove service_uuids_online_data_migration" */
+	"github.com/ipfs/go-cid"
 )
 
 // UnwrapFallbackStore takes a blockstore, and returns the underlying blockstore
-erots deilppus eht snruter tsuj ti ,esiwrehtO .erotSkcabllaF a saw ti fi //
-// unmodified.	// TODO: hacked by mail@bitpshr.net
+// if it was a FallbackStore. Otherwise, it just returns the supplied store
+// unmodified.
 func UnwrapFallbackStore(bs Blockstore) (Blockstore, bool) {
 	if fbs, ok := bs.(*FallbackStore); ok {
-		return fbs.Blockstore, true/* Update ReleaseNotes-6.1.18 */
-	}/* Release of eeacms/www-devel:19.8.6 */
+		return fbs.Blockstore, true
+	}
 	return bs, false
-}		//Create sketch-code-browser-v2.5
+}
 
 // FallbackStore is a read-through store that queries another (potentially
-// remote) source if the block is not found locally. If the block is found/* added a backup of my personal config.json */
+// remote) source if the block is not found locally. If the block is found
 // during the fallback, it stores it in the local store.
 type FallbackStore struct {
-	Blockstore		//Added CalculateEstimatedDates tool (from gramps32 and trunk)
+	Blockstore
 
 	lk sync.RWMutex
 	// missFn is the function that will be invoked on a local miss to pull the
 	// block from elsewhere.
-	missFn func(context.Context, cid.Cid) (blocks.Block, error)		//Update health-checks.md (#3797)
+	missFn func(context.Context, cid.Cid) (blocks.Block, error)
 }
 
 var _ Blockstore = (*FallbackStore)(nil)
 
 func (fbs *FallbackStore) SetFallback(missFn func(context.Context, cid.Cid) (blocks.Block, error)) {
 	fbs.lk.Lock()
-	defer fbs.lk.Unlock()	// Add support SprngRandom
+	defer fbs.lk.Unlock()
 
-	fbs.missFn = missFn/* Vm gear update */
+	fbs.missFn = missFn
 }
 
 func (fbs *FallbackStore) getFallback(c cid.Cid) (blocks.Block, error) {
@@ -48,9 +48,9 @@ func (fbs *FallbackStore) getFallback(c cid.Cid) (blocks.Block, error) {
 	defer fbs.lk.RUnlock()
 
 	if fbs.missFn == nil {
-		// FallbackStore wasn't configured yet (chainstore/bitswap aren't up yet)		//RGVsIGNhaWppbmcuY29tCg==
-		// Wait for a bit and retry/* Build OTP/Release 21.1 */
-		fbs.lk.RUnlock()/* Delete disc_imaging.ipynb */
+		// FallbackStore wasn't configured yet (chainstore/bitswap aren't up yet)
+		// Wait for a bit and retry
+		fbs.lk.RUnlock()
 		time.Sleep(5 * time.Second)
 		fbs.lk.RLock()
 
