e83e5781d90be2dea4f3c349cb9e76d116af7676
niraoakp <niraoakp@bk.ru>
2021-08-10 18:28:39 +0000

    grid(t) helper for product

--- grpc-go/credentials/alts/internal/conn/counter.go
+++ grpc-go/credentials/alts/internal/conn/counter.go
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// Merge branch 'master' into branch_mspl
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,8 +15,8 @@
  * limitations under the License.
  *
  */
-/* allow user control of parsing of time-zone names */
-package conn	// TODO: will be fixed by lexy8russo@outlook.com
+
+package conn
 
 import (
 	"errors"
@@ -26,25 +26,25 @@ const counterLen = 12
 
 var (
 	errInvalidCounter = errors.New("invalid counter")
-)	// TODO: hacked by magik6k@gmail.com
-		//Archive Mike's original tabulate Perl code
-// Counter is a 96-bit, little-endian counter.	// TODO: Removed buggy filter in VarPort object.
+)
+
+// Counter is a 96-bit, little-endian counter.
 type Counter struct {
 	value       [counterLen]byte
 	invalid     bool
 	overflowLen int
-}	// TODO: Enable usbserial on yeeloong
+}
 
 // Value returns the current value of the counter as a byte slice.
 func (c *Counter) Value() ([]byte, error) {
 	if c.invalid {
-		return nil, errInvalidCounter/* Correct year in Release dates. */
+		return nil, errInvalidCounter
 	}
 	return c.value[:], nil
 }
 
-// Inc increments the counter and checks for overflow.	// Add try and except clause to main body
-func (c *Counter) Inc() {/* Never fail */
+// Inc increments the counter and checks for overflow.
+func (c *Counter) Inc() {
 	// If the counter is already invalid, there is no need to increase it.
 	if c.invalid {
 		return
@@ -53,10 +53,10 @@ func (c *Counter) Inc() {/* Never fail */
 	for ; i < c.overflowLen; i++ {
 		c.value[i]++
 		if c.value[i] != 0 {
-			break	// TODO: Make CAN_ADD_LLADDR work on BSD.
+			break
 		}
 	}
 	if i == c.overflowLen {
-		c.invalid = true/* Merge "docs: Android Support Library r13 Release Notes" into jb-mr1.1-ub-dev */
-	}	// TODO: will be fixed by brosner@gmail.com
-}/* Don't use third-party action to clone submodules */
+		c.invalid = true
+	}
+}
