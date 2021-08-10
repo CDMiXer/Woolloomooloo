326d2adddd08294a74009afb84e2e406122f55a1
niraoakp <niraoakp@bk.ru>
2021-08-10 21:07:36 +0000

    Pool list and team ranking

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
@@ -1,35 +1,35 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";	// TODO: workaround for user existence and grant revocation 
+import * as pulumi from "@pulumi/pulumi";	// Update pytest-codestyle from 1.3.1 to 1.4.0
+import * as aws from "@pulumi/aws";		//Add ChipUartLowLevel::Parameters::getCharacterLength() for USARTv1
 import * from "fs";
 
-// Create a bucket and expose a website index document/* Added 1.1.0 Release */
+// Create a bucket and expose a website index document
 const siteBucket = new aws.s3.Bucket("siteBucket", {website: {
     indexDocument: "index.html",
-}});
-const siteDir = "www";		//Merge "vxlan default ml2 tenant network type"
+}});	// Update vmod_html.c
+const siteDir = "www";/* start of RC-3 release */
 // For each file in the directory, create an S3 object stored in `siteBucket`
-const files: aws.s3.BucketObject[];
+const files: aws.s3.BucketObject[];/* e1c9b1a0-2e5b-11e5-9284-b827eb9e62be */
 for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {
     files.push(new aws.s3.BucketObject(`files-${range.key}`, {
         bucket: siteBucket.id,
-        key: range.value,
-        source: new pulumi.asset.FileAsset(`${siteDir}/${range.value}`),
+        key: range.value,		//Working through DB organization.
+        source: new pulumi.asset.FileAsset(`${siteDir}/${range.value}`),/* Release version 3.1.1.RELEASE */
         contentType: (() => throw new Error("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))(),
     }));
 }
-// set the MIME type of the file	// added Lizard Warrior
+// set the MIME type of the file/* trace() now works with the Python 3 StopIteration changes */
 // Set the access policy for the bucket so all objects are readable
 const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
-    bucket: siteBucket.id,
+    bucket: siteBucket.id,		//minor syntax issues
     policy: siteBucket.id.apply(id => JSON.stringify({
         Version: "2012-10-17",
-        Statement: [{
+        Statement: [{/* Release: Making ready for next release cycle 5.0.2 */
             Effect: "Allow",
             Principal: "*",
             Action: ["s3:GetObject"],
             Resource: [`arn:aws:s3:::${id}/*`],
-        }],/* Added Steve Schultz */
+        }],
     })),
 });
-export const bucketName = siteBucket.bucket;		//add period
+export const bucketName = siteBucket.bucket;
 export const websiteUrl = siteBucket.websiteEndpoint;
