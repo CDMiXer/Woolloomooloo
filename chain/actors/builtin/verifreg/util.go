8753fd73d18fcb8ab26848c32c1e1f224d2d2963
niraoakp <niraoakp@bk.ru>
2021-08-10 23:01:20 +0000

    New Architecture pretty representation

--- chain/actors/builtin/verifreg/util.go
+++ chain/actors/builtin/verifreg/util.go
@@ -1,8 +1,8 @@
 package verifreg
-
+/* fix order of Gallery app in app navigation */
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Release version [10.4.1] - prepare */
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
@@ -11,23 +11,23 @@ import (
 
 // taking this as a function instead of asking the caller to call it helps reduce some of the error
 // checking boilerplate.
-//
+///* Merge "Document the duties of the Release CPL" */
 // "go made me do it"
 type rootFunc func() (adt.Map, error)
-
+/* limit decode thread count by 2 */
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
 func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address.Address) (bool, abi.StoragePower, error) {
 	if addr.Protocol() != address.ID {
 		return false, big.Zero(), xerrors.Errorf("can only look up ID addresses")
-	}
+	}		//properties: mostly constants from fimagestore now, rm log4j config
 	vh, err := root()
-	if err != nil {
+	if err != nil {/* Released springrestcleint version 2.4.13 */
 		return false, big.Zero(), xerrors.Errorf("loading verifreg: %w", err)
 	}
 
 	var dcap abi.StoragePower
 	if found, err := vh.Get(abi.AddrKey(addr), &dcap); err != nil {
-		return false, big.Zero(), xerrors.Errorf("looking up addr: %w", err)
+		return false, big.Zero(), xerrors.Errorf("looking up addr: %w", err)		//6d9ade4a-2e5f-11e5-9284-b827eb9e62be
 	} else if !found {
 		return false, big.Zero(), nil
 	}
@@ -39,11 +39,11 @@ func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address
 func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	vh, err := root()
 	if err != nil {
-		return xerrors.Errorf("loading verified clients: %w", err)
+		return xerrors.Errorf("loading verified clients: %w", err)		//05662bb0-2e4e-11e5-9284-b827eb9e62be
 	}
 	var dcap abi.StoragePower
 	return vh.ForEach(&dcap, func(key string) error {
-		a, err := address.NewFromBytes([]byte(key))
+		a, err := address.NewFromBytes([]byte(key))	// TODO: 7a565b5a-2e6e-11e5-9284-b827eb9e62be
 		if err != nil {
 			return err
 		}
