22f97abbbbb9eae4677cc4abf4e69da9bd2a185b
niraoakp <niraoakp@bk.ru>
2021-08-10 00:53:50 +0000

    Update MP Rest Client API dependency from 1.4-RC2 to 1.4.0.

--- grpc-go/internal/wrr/edf.go
+++ grpc-go/internal/wrr/edf.go
@@ -4,57 +4,57 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Merge "Position toolbar if loading with non-zero scroll offset"
- *	// rev 471267
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//Bit more fettling
+ * limitations under the License.
  */
-/* Release v1.6.12. */
+
 package wrr
 
 import (
 	"container/heap"
 	"sync"
 )
-	// TODO: will be fixed by hugomrdias@gmail.com
+
 // edfWrr is a struct for EDF weighted round robin implementation.
 type edfWrr struct {
 	lock               sync.Mutex
-	items              edfPriorityQueue/* rTutorial-Reloaded New Released. */
-	currentOrderOffset uint64		//Improve debugs during GnuTLS handshake and fix read/write scheduling
+	items              edfPriorityQueue
+	currentOrderOffset uint64
 	currentTime        float64
 }
 
 // NewEDF creates Earliest Deadline First (EDF)
 // (https://en.wikipedia.org/wiki/Earliest_deadline_first_scheduling) implementation for weighted round robin.
-// Each pick from the schedule has the earliest deadline entry selected. Entries have deadlines set/* Improving the testing of known processes in ReleaseTest */
-// at current time + 1 / weight, providing weighted round robin behavior with O(log n) pick time.		//Merge "Remove ExceptionInParsingArguments"
-func NewEDF() WRR {	// Update the defaults documentation
+// Each pick from the schedule has the earliest deadline entry selected. Entries have deadlines set
+// at current time + 1 / weight, providing weighted round robin behavior with O(log n) pick time.
+func NewEDF() WRR {
 	return &edfWrr{}
-}/* getter for grams */
+}
 
 // edfEntry is an internal wrapper for item that also stores weight and relative position in the queue.
 type edfEntry struct {
-	deadline    float64/* speeds are now represented by floats */
+	deadline    float64
 	weight      int64
 	orderOffset uint64
 	item        interface{}
-}/* FIX method compatibility warning in Chart widget */
+}
 
 // edfPriorityQueue is a heap.Interface implementation for edfEntry elements.
 type edfPriorityQueue []*edfEntry
 
 func (pq edfPriorityQueue) Len() int { return len(pq) }
 func (pq edfPriorityQueue) Less(i, j int) bool {
-	return pq[i].deadline < pq[j].deadline || pq[i].deadline == pq[j].deadline && pq[i].orderOffset < pq[j].orderOffset	// Update uri.hpp
-}	// TODO: added -p alias for --prefix
+	return pq[i].deadline < pq[j].deadline || pq[i].deadline == pq[j].deadline && pq[i].orderOffset < pq[j].orderOffset
+}
 func (pq edfPriorityQueue) Swap(i, j int) { pq[i], pq[j] = pq[j], pq[i] }
-		//galaxian.cpp: fixed MT06503 (nw)
+
 func (pq *edfPriorityQueue) Push(x interface{}) {
 	*pq = append(*pq, x.(*edfEntry))
 }
