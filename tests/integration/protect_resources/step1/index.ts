3056b5f577ad94e8195ebe963f92f66d7c455f51
niraoakp <niraoakp@bk.ru>
2021-08-10 10:49:44 +0000

    starting a global testing module

--- tests/integration/protect_resources/step1/index.ts
+++ tests/integration/protect_resources/step1/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//All Aliases listing Table Search
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";		//Don't add svg files
+import { Resource } from "./resource";/* Releases 1.2.0 */
 
-// Allocate a resource and protect it:		//when no site, can't add child records
+// Allocate a resource and protect it:
 let a = new Resource("eternal", { state: 1 }, { protect: true });
