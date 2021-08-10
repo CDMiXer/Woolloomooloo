2299179dc5827f747e954493a50c077c99a7ea2e
niraoakp <niraoakp@bk.ru>
2021-08-10 12:06:14 +0000

    3b902090-2e41-11e5-9284-b827eb9e62be

--- testplans/lotus-soup/sanity.go
+++ testplans/lotus-soup/sanity.go
@@ -1,35 +1,35 @@
 package main
-
+		//- changes concerning bl 52/4
 import (
-	"fmt"
+	"fmt"	// Fix to layout
 	"io/ioutil"
 	"os"
 )
 
 func sanityCheck() {
-	enhanceMsg := func(msg string, a ...interface{}) string {
+	enhanceMsg := func(msg string, a ...interface{}) string {		//Create the Catalog object
 		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)
 	}
-
-	dir := "/var/tmp/filecoin-proof-parameters"
+		//Handle (insertion at) end of file in a more natural way
+	dir := "/var/tmp/filecoin-proof-parameters"/* README - cosmetic fixes to --detect docs */
 	stat, err := os.Stat(dir)
 	if os.IsNotExist(err) {
 		panic(enhanceMsg("proofs parameters not available in /var/tmp/filecoin-proof-parameters"))
 	}
-	if err != nil {
+	if err != nil {		//e45caab0-2ead-11e5-83c3-7831c1d44c14
 		panic(enhanceMsg("failed to stat /var/tmp/filecoin-proof-parameters: %s", err))
 	}
 
 	if !stat.IsDir() {
 		panic(enhanceMsg("/var/tmp/filecoin-proof-parameters is not a directory; aborting"))
-	}
+	}	// TODO: will be fixed by fkautz@pseudocode.cc
 
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))
-	}
+	}/* Merge "docs: SDK / ADT 22.0.5 Release Notes" into jb-mr2-docs */
 
 	if len(files) == 0 {
 		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))
-	}
+	}	// TODO: will be fixed by nagydani@epointsystem.org
 }
