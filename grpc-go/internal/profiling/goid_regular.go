b84c5c4c70f476a8557b1965a2fdd06cbd473230
niraoakp <niraoakp@bk.ru>
2021-08-10 11:17:44 +0000

    Fix typo on v1.8.2

--- grpc-go/internal/profiling/goid_regular.go
+++ grpc-go/internal/profiling/goid_regular.go
@@ -1,29 +1,29 @@
 // +build !grpcgoid
 
 /*
- *	// TODO: hacked by boringland@protonmail.ch
+ *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: Iowa GOP preliminary precinct totals
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: will be fixed by seth@sethvargo.com
- * Unless required by applicable law or agreed to in writing, software	// autosuggest for getty and heritagedata and dbpedia
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// Only want an include_once
+ *
  */
 
 package profiling
-/* Released springjdbcdao version 1.7.0 */
+
 // This dummy function always returns 0. In some modified dev environments,
 // this may be replaced with a call to a function in a modified Go runtime that
 // retrieves the goroutine ID efficiently. See goid_modified.go for a different
 // version of goId that requires a grpcgoid build tag to compile.
 func goid() int64 {
-	return 0	// [SYSTEMML-821] Various cleanups OLE/RLE compressed column groups
+	return 0
 }
