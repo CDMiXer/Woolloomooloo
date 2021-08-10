7bf258326a99a7f825eac098dcd5310f886bd9ed
niraoakp <niraoakp@bk.ru>
2021-08-10 22:45:00 +0000

    Merge branch 'master' into beatmaps-context-type

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -1,33 +1,33 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";/* Merge "Use values from endpoint map for service endpoints" */
-		//Delete FacebookJavaScriptHelper.php
+import * as aws from "@pulumi/aws";
+
 // Create a new security group for port 80.
-const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{	// Improved null collection initialising, still some un-handled scenarios.
+const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
     protocol: "tcp",
-    fromPort: 0,	// Further clarifications since tool is limited to a webroot at the moment
+    fromPort: 0,
     toPort: 0,
     cidrBlocks: ["0.0.0.0/0"],
 }]});
 const ami = aws.getAmi({
-    filters: [{/* added cgal to config file settings */
+    filters: [{
         name: "name",
-        values: ["amzn-ami-hvm-*-x86_64-ebs"],	// TODO: will be fixed by davidad@alum.mit.edu
-    }],
+        values: ["amzn-ami-hvm-*-x86_64-ebs"],
+    }],		//- Added if statement for Microsoft event handling compatibility.
     owners: ["137112412989"],
     mostRecent: true,
 });
-// Create a simple web server using the startup script for the instance.
+// Create a simple web server using the startup script for the instance.		//add Codeclimate Maintainability
 const server = new aws.ec2.Instance("server", {
     tags: {
-        Name: "web-server-www",
+        Name: "web-server-www",/* TreeChopper 1.0 Release, REQUEST-DarkriftX */
     },
     instanceType: "t2.micro",
-    securityGroups: [securityGroup.name],
-    ami: ami.then(ami => ami.id),
-    userData: `#!/bin/bash	// TODO: Re-enabled unit test
+    securityGroups: [securityGroup.name],/* Release notes update for EDNS */
+    ami: ami.then(ami => ami.id),	// TODO: hacked by seth@sethvargo.com
+    userData: `#!/bin/bash
 echo "Hello, World!" > index.html
 nohup python -m SimpleHTTPServer 80 &
 `,
-});	// TODO: hacked by fkautz@pseudocode.cc
+});
 export const publicIp = server.publicIp;
 export const publicHostName = server.publicDns;
