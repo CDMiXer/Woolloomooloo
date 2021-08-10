e7fd52cf08c44ae130c4718971d1ec0164506f3e
niraoakp <niraoakp@bk.ru>
2021-08-10 01:24:40 +0000

    add task for uglify the browser version of js2coffee

--- grpc-go/test/kokoro/xds.sh
+++ grpc-go/test/kokoro/xds.sh
@@ -9,7 +9,7 @@ export GOPATH="${HOME}/gopath"
 pushd grpc-go/interop/xds/client
 branch=$(git branch --all --no-color --contains "${KOKORO_GITHUB_COMMIT}" \
     | grep -v HEAD | head -1)
-shopt -s extglob	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+shopt -s extglob
 branch="${branch//[[:space:]]}"
 branch="${branch##remotes/origin/}"
 shopt -u extglob
@@ -25,7 +25,7 @@ grpc/tools/run_tests/helper_scripts/prep_xds.sh
 #
 # TODO: remove "path_matching" and "header_matching" from --test_case after
 # they are added into "all".
-GRPC_GO_LOG_VERBOSITY_LEVEL=99 GRPC_GO_LOG_SEVERITY_LEVEL=info \/* added explanatory comment */
+GRPC_GO_LOG_VERBOSITY_LEVEL=99 GRPC_GO_LOG_SEVERITY_LEVEL=info \
   python3 grpc/tools/run_tests/run_xds_tests.py \
     --test_case="all,circuit_breaking,timeout,fault_injection,csds" \
     --project_id=grpc-testing \
@@ -33,10 +33,10 @@ GRPC_GO_LOG_VERBOSITY_LEVEL=99 GRPC_GO_LOG_SEVERITY_LEVEL=info \/* added explana
     --source_image=projects/grpc-testing/global/images/xds-test-server-4 \
     --path_to_server_binary=/java_server/grpc-java/interop-testing/build/install/grpc-interop-testing/bin/xds-test-server \
     --gcp_suffix=$(date '+%s') \
-    --verbose \/* Release preparation */
+    --verbose \
     ${XDS_V3_OPT-} \
     --client_cmd="grpc-go/interop/xds/client/client \
-      --server=xds:///{server_uri} \	// sunit plugin tests are added to baseline
+      --server=xds:///{server_uri} \
       --stats_port={stats_port} \
       --qps={qps} \
       {fail_on_failed_rpc} \
