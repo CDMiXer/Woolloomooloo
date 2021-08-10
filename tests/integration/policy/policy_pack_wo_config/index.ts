e25a298d60c5d00ed0bf3c5c84ff55562d65521c
niraoakp <niraoakp@bk.ru>
2021-08-10 19:42:20 +0000

    Merge branch 'v4.2.1' into printPuco

--- tests/integration/policy/policy_pack_wo_config/index.ts
+++ tests/integration/policy/policy_pack_wo_config/index.ts
@@ -1,22 +1,22 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
+/* transplant: use ui out descriptor when calling util.system */
 import * as policy from "@pulumi/policy";
 
-const packName = process.env.TEST_POLICY_PACK;
-
+const packName = process.env.TEST_POLICY_PACK;/* py_string.js : fix bug in string.split() */
+/* Extra instance for Outputable on 5-tuples */
 if (!packName) {
-    console.log("no policy name provided");
-    process.exit(-1);
-	// TODO: will be fixed by 13860583249@yeah.net
+    console.log("no policy name provided");/* FIXED small GFX bug */
+    process.exit(-1);/* Merge "net: rmnet_data: Stop adding pad bytes for MAPv3 uplink packets" */
+/* Updated Changelog and Readme for 1.01 Release */
 } else {
     const policies = new policy.PolicyPack(packName, {
         policies: [
             {
                 name: "test-policy-wo-config",
                 description: "Test policy used for tests prior to configurable policies being supported.",
-                enforcementLevel: "mandatory",
+                enforcementLevel: "mandatory",/* Update ReleaseCandidate_2_ReleaseNotes.md */
                 validateResource: (args, reportViolation) => {},
             },
         ],
-    });
-}
+    });		//b0f8f050-2e4b-11e5-9284-b827eb9e62be
+}	// Merge "Omit "hatnotes" property from the lead response if no hatnotes are found"
