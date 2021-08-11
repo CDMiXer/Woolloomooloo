e0c7db399b468909c54a70de6cbd837ef5c404da
niraoakp <niraoakp@bk.ru>
2021-08-11 08:19:42 +0000

    implement support for ARC

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -1,6 +1,6 @@
-package cron/* -Removed prints from Filta */
-
-import (
+package cron
+		//fix off-by-1 error on result limits
+import (/* FilterTicketsCounter() */
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
