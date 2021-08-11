d44fa11473bf551adf427465643cae86a17f22f4
niraoakp <niraoakp@bk.ru>
2021-08-11 17:44:28 +0000

    Use explicit DISPATCH_QUEUE_SERIAL parameters when creating queues

--- core/batch.go
+++ core/batch.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by steven@stebalien.com
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// Moving gaussian processes to bayes-scala-gp
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Delete projeto1.jpg
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// #178 - Refactored company_id
+// See the License for the specific language governing permissions and
 // limitations under the License.
-
-package core
+	// TODO: hacked by magik6k@gmail.com
+package core		//Removed all global variables from Conditional Plot
 
 import "context"
 
-// Batch represents a Batch request to synchronize the local
-// repository and permission store for a user account./* Merge "docs: update OS majors in Makefile Releases section" into develop */
+// Batch represents a Batch request to synchronize the local		//set_next_ecp_state method improovments
+// repository and permission store for a user account.
 type Batch struct {
 	Insert []*Repository `json:"insert"`
 	Update []*Repository `json:"update"`
-	Rename []*Repository `json:"rename"`/* Merge "[INTERNAL] Release notes for version 1.28.2" */
-	Revoke []*Repository `json:"revoke"`
+	Rename []*Repository `json:"rename"`
+	Revoke []*Repository `json:"revoke"`		//Setup basic testing for param filters.
 }
 
 // Batcher batch updates the user account.
