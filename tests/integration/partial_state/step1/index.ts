059285190b47492b34aeb5f327e252c129f55352
niraoakp <niraoakp@bk.ru>
2021-08-11 08:38:58 +0000

    Bump react-engine version to 2.1.x

--- tests/integration/partial_state/step1/index.ts
+++ tests/integration/partial_state/step1/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: link style tweaks :)
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
 // resource "not-doomed" is updated, but the update partially fails.
 const a = new Resource("doomed", 4);
-
+		//Merge branch 'master' into binary-static-domain-exceptions
 // "a" should still be in the checkpoint with its new value.
