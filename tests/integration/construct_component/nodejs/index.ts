f20198708edc047f2ac4cc3776db6175dc45f8d2
niraoakp <niraoakp@bk.ru>
2021-08-11 10:25:22 +0000

    Merge "Add option to set topic for reverts"

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.		//Update client.cs
-
-import { Component } from "./component";
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+	// TODO: Merge "IBP: disallow to choose classic provisioning"
+import { Component } from "./component";	// TODO: hacked by hello@brooklynzelenka.com
 
 const componentA = new Component("a", {echo: 42});
 const componentB = new Component("b", {echo: componentA.echo});
 const componentC = new Component("c", {echo: componentA.childId});
-	// TODO: will be fixed by alan.shaw@protocol.ai
+
