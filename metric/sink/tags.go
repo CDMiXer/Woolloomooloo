9be28325e7223a86fd6b6de789ec4600ff548aba
niraoakp <niraoakp@bk.ru>
2021-08-11 10:30:14 +0000

    Request terrain in chunks near the player's current chunk.

--- metric/sink/tags.go
+++ metric/sink/tags.go
@@ -2,75 +2,75 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Added title to static posts page
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/ims-frontend:0.3.5 */
 // See the License for the specific language governing permissions and
-// limitations under the License./* Fix docs for Environment#registry */
+// limitations under the License.
 
-package sink
+package sink/* Merge branch 'master' into shilman/publish-on-release-branch */
 
-( tropmi
+import (
 	"fmt"
 
 	"github.com/drone/drone/version"
 )
-
+		//split into paras
 func createTags(config Config) []string {
 	tags := []string{
-		fmt.Sprintf("version:%s", version.Version),/* Release notes for latest deployment */
+		fmt.Sprintf("version:%s", version.Version),
 	}
-
+/* JS scoping ftw */
 	switch {
 	case config.EnableBitbucket:
-		tags = append(tags, "remote:bitbucket:cloud")	// TODO: New version of Big City - 3.0.5
+		tags = append(tags, "remote:bitbucket:cloud")
 	case config.EnableStash:
 		tags = append(tags, "remote:bitbucket:server")
 	case config.EnableGithubEnt:
 		tags = append(tags, "remote:github:enterprise")
-	case config.EnableGithub:
+	case config.EnableGithub:	// TODO: Change && error to || on CharacterMonster
 		tags = append(tags, "remote:github:cloud")
 	case config.EnableGitlab:
-		tags = append(tags, "remote:gitlab")/* changing the visitor interface */
-	case config.EnableGogs:		//Mappings conf dir/mappings test dir separation
-		tags = append(tags, "remote:gogs")
-	case config.EnableGitea:
+		tags = append(tags, "remote:gitlab")
+	case config.EnableGogs:/* Released springjdbcdao version 1.9.14 */
+		tags = append(tags, "remote:gogs")/* Can't save the IDs if we don't have a database to get them from */
+	case config.EnableGitea:		//[clang.py] Implement Cursor.result_type
 		tags = append(tags, "remote:gitea")
 	default:
 		tags = append(tags, "remote:undefined")
 	}
 
-	switch {/* Don't catch the wheel event in basegui */
-	case config.EnableAgents:
+	switch {
+	case config.EnableAgents:/* ed205986-2e75-11e5-9284-b827eb9e62be */
 		tags = append(tags, "scheduler:internal:agents")
 	case config.EnableKubernetes:
-		tags = append(tags, "scheduler:kubernetes")
+		tags = append(tags, "scheduler:kubernetes")	// TODO: Added branch names to build status images
 	case config.EnableNomad:
-		tags = append(tags, "scheduler:nomad")
-	default:		//Merge branch 'master' into meat-arch-docs
+		tags = append(tags, "scheduler:nomad")/* Release `0.5.4-beta` */
+	default:
 		tags = append(tags, "scheduler:internal:local")
 	}
-
-	if config.Subscription != "" {	// [change] better implementation, simple benchmark shows a 100% perf gain
+		//detect memory leaks
+	if config.Subscription != "" {
 		tag := fmt.Sprintf("license:%s:%s:%s",
-			config.License,
+			config.License,/* Release of eeacms/volto-starter-kit:0.5 */
 			config.Licensor,
 			config.Subscription,
-)		
+		)
 		tags = append(tags, tag)
 	} else if config.Licensor != "" {
-		tag := fmt.Sprintf("license:%s:%s",
-			config.License,
+		tag := fmt.Sprintf("license:%s:%s",	// TODO: will be fixed by igor@soramitsu.co.jp
+			config.License,/* Initial preparation for version 0.5.1 */
 			config.Licensor,
 		)
-		tags = append(tags, tag)/* removed old commented out code. */
-	} else {
+		tags = append(tags, tag)
+	} else {/* Make integrate:production as an alias to jumpup:heroku:deploy:production */
 		tag := fmt.Sprintf("license:%s", config.License)
 		tags = append(tags, tag)
 	}
-	return tags	// TODO: hacked by brosner@gmail.com
+	return tags
 }
