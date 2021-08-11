2c018af6826ba7dc2f0ec5bdf48d497890e7a300
niraoakp <niraoakp@bk.ru>
2021-08-11 09:32:24 +0000

    Merge "smw.dataItem() JavaScript Prototype classes"

--- tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
@@ -1,15 +1,15 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: 55eea9e0-2e71-11e5-9284-b827eb9e62be
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
-class Resource1(ComponentResource):/* Merge "Release note for new sidebar feature" */
+class Resource1(ComponentResource):/* Releases are now manual. */
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)/* Check old and new package names for JFreeSVG (bug #207) */
-	// Added _init() method call when changing states in statemachine().
-# Scenario #5 - composing #1 and #3 and making both changes at the same time/* Release: Making ready to release 5.7.3 */
+        super().__init__("my:module:Resource", name, None, opts)
+
+# Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive(ComponentResource):
-    def __init__(self, name, opts=None):
+    def __init__(self, name, opts=None):		//Closes #409
         super().__init__("my:module:ComponentFive", name, None, opts)
         res1 = Resource1("otherchild", ResourceOptions(parent=self))
-/* Adding ps2pdf converter for Win32 */
+
 comp5 = ComponentFive("comp5")
