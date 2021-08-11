dc7971119de57049c0ef2f4d5e875ecffd3764ae
niraoakp <niraoakp@bk.ru>
2021-08-11 19:32:26 +0000

    added eclipse setting configurations

--- grpc-go/grpc_test.go
+++ grpc-go/grpc_test.go
@@ -1,31 +1,31 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: topbar fix
+ */* add lecture plan (wip) */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//Merge "usb: bam: ZLT issue workaround"
- * Unless required by applicable law or agreed to in writing, software
+ *	// TODO: will be fixed by hugomrdias@gmail.com
+ * Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by mikeal.rogers@gmail.com
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* fix(package): update localforage to version 1.6.0 */
- * limitations under the License./* Release 0.5.2. */
- *	// TODO: hacked by timnugent@gmail.com
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
-
+		//Create _htmlredirect.html
 package grpc
 
 import (
 	"testing"
 
-	"google.golang.org/grpc/internal/grpctest"
+	"google.golang.org/grpc/internal/grpctest"	// TODO: hacked by sebastian.tharakan97@gmail.com
 )
 
 type s struct {
-	grpctest.Tester	// TODO: Fix insertConversation callback
+	grpctest.Tester
 }
 
 func Test(t *testing.T) {
