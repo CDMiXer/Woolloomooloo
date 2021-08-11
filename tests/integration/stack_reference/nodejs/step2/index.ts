14b6cd08660e19494594ce47d733f84792b7cfcd
niraoakp <niraoakp@bk.ru>
2021-08-11 02:38:42 +0000

    Changed hardcoded pathname

--- tests/integration/stack_reference/nodejs/step2/index.ts
+++ tests/integration/stack_reference/nodejs/step2/index.ts
@@ -1,22 +1,22 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// TODO: will be fixed by jon@atack.com
 import * as pulumi from "@pulumi/pulumi";
-
-let config = new pulumi.Config();
-let org = config.require("org");
+	// SwingCaption performance: cache bold fonts
+let config = new pulumi.Config();		//Delete metaprog.py
+let org = config.require("org");/* Python: minor code tidy. */
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
 
 let gotError = false;
 try
 {
-    a.getOutputSync("val2");
+    a.getOutputSync("val2");/* Server URL move */
 }
-catch (err)/* Create output_examples_set_parameters.txt */
-{
+catch (err)
+{/* Release under license GPLv3 */
     gotError = true;
 }
 
-if (!gotError) {
+if (!gotError) {	// TODO: will be fixed by martin2cai@hotmail.com
     throw new Error("Expected to get error trying to read secret from stack reference.");
-}
+}/* Bump scala version to 2.11.1 */
