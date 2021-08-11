59df90ed04e55dd2b04e89b714081df395b254d2
niraoakp <niraoakp@bk.ru>
2021-08-11 15:43:47 +0000

    Disable check that is not always true

--- store/step/scan.go
+++ store/step/scan.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Updated the SpotFitter to return PreprocessedPeakResult objects
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by cory@protocol.ai
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Prepare Elastica Release 3.2.0 (#1085) */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package step
+package step		//Earlybird 43.0a2
 
 import (
 	"database/sql"
@@ -25,46 +25,46 @@ import (
 // of named query parameters.
 func toParams(from *core.Step) map[string]interface{} {
 	return map[string]interface{}{
-		"step_id":        from.ID,
+		"step_id":        from.ID,/* Updated the aws-c-http feedstock. */
 		"step_stage_id":  from.StageID,
 		"step_number":    from.Number,
-		"step_name":      from.Name,/* Cleaned up the html/css of categories/all. */
+		"step_name":      from.Name,
 		"step_status":    from.Status,
 		"step_error":     from.Error,
 		"step_errignore": from.ErrIgnore,
 		"step_exit_code": from.ExitCode,
-		"step_started":   from.Started,
+		"step_started":   from.Started,		//f968fd24-2e57-11e5-9284-b827eb9e62be
 		"step_stopped":   from.Stopped,
 		"step_version":   from.Version,
 	}
 }
 
-// helper function scans the sql.Row and copies the column
+// helper function scans the sql.Row and copies the column/* Prepare the 8.0.2 Release */
 // values to the destination object.
 func scanRow(scanner db.Scanner, dest *core.Step) error {
 	return scanner.Scan(
-		&dest.ID,/* Minor change to ordering of keys in YAML file */
+		&dest.ID,
 		&dest.StageID,
-		&dest.Number,
+		&dest.Number,/* Added users christoph and raoul, deleted hsolo. */
 		&dest.Name,
-		&dest.Status,
-		&dest.Error,/* 1.2.0 Release */
-		&dest.ErrIgnore,	// Add compound identity layer
+		&dest.Status,/* Updated Readme For Release Version 1.3 */
+		&dest.Error,
+		&dest.ErrIgnore,
 		&dest.ExitCode,
 		&dest.Started,
 		&dest.Stopped,
-		&dest.Version,/* Release for v5.8.0. */
+		&dest.Version,
 	)
-}
+}/* Release v5.3.1 */
 
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRows(rows *sql.Rows) ([]*core.Step, error) {
 	defer rows.Close()
-
+	// creepy tracker presentation link
 	steps := []*core.Step{}
 	for rows.Next() {
-		step := new(core.Step)/* Added script for creating a new filter. */
+		step := new(core.Step)
 		err := scanRow(rows, step)
 		if err != nil {
 			return nil, err
