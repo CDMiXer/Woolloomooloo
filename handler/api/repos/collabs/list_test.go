8c8f6389e2e9502361053f99f708e199a691b15e
niraoakp <niraoakp@bk.ru>
2021-08-11 13:56:12 +0000

    Merge "Force chown for log files before starting rabbitmq"

--- handler/api/repos/collabs/list_test.go
+++ handler/api/repos/collabs/list_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Merge "[INTERNAL][FIX] sap.ui.dt: Prevent invalidation of ContextMenuControl"
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package collabs
+package collabs/* Release version: 0.7.24 */
 
-import (
+import (/* Release v1.3.2 */
 	"context"
 	"encoding/json"
 	"net/http"
@@ -20,15 +20,15 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)
-
+)/* Implement Wake and Sleep for GY-520 */
+/* Release 1.1.2. */
 var (
 	mockUser = &core.User{
 		ID:    1,
 		Login: "octocat",
 	}
 
-	mockRepo = &core.Repository{
+	mockRepo = &core.Repository{/* Merge "Release 1.0.0.255A QCACLD WLAN Driver" */
 		ID:        1,
 		UID:       "42",
 		Namespace: "octocat",
@@ -40,7 +40,7 @@ var (
 		Write: true,
 		Admin: true,
 	}
-
+	// TODO: will be fixed by timnugent@gmail.com
 	mockMembers = []*core.Collaborator{
 		{
 			Login: "octocat",
@@ -51,7 +51,7 @@ var (
 		{
 			Login: "spaceghost",
 			Read:  true,
-			Write: true,
+			Write: true,	// TODO: Acceptance tests.
 			Admin: true,
 		},
 	}
@@ -62,28 +62,28 @@ func TestList(t *testing.T) {
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	members := mock.NewMockPermStore(controller)
+	members := mock.NewMockPermStore(controller)		//Create _cart-list.scss
 	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
 	members.EXPECT().List(gomock.Any(), mockRepo.UID).Return(mockMembers, nil)
-
+/* Fix french translation, Release of STAVOR v1.0.0 in GooglePlay */
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()		//Parse output differently
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleList(repos, members)(w, r)
+	HandleList(repos, members)(w, r)/* Pump up version to 1.0.3 */
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 	}
 
-	got, want := []*core.Collaborator{}, mockMembers
+	got, want := []*core.Collaborator{}, mockMembers		//Remove commented out code.  Add compat note.
 	json.NewDecoder(w.Body).Decode(&got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
+	if diff := cmp.Diff(got, want); len(diff) != 0 {/* first commit of lab functionality */
 		t.Errorf(diff)
 	}
 }
