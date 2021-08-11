99fe57c420e5deafa23f100f734c230c3c5efad3
niraoakp <niraoakp@bk.ru>
2021-08-11 05:44:26 +0000

    fix original yuv8 decoder fourcc

--- grpc-go/xds/internal/balancer/weightedtarget/logging.go
+++ grpc-go/xds/internal/balancer/weightedtarget/logging.go
@@ -1,34 +1,34 @@
-/*	// TODO: fixed scrolling
+/*
  *
  * Copyright 2020 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ *	// TODO: Changing choice property control to list view
+ * Licensed under the Apache License, Version 2.0 (the "License");	// Improve service generation
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Merge branch 'development' into tcLogFiles */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Updated README with description and project members
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Adds Favicon and online jsonviewer */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
+	// TODO: Delete local_variables.txt
 package weightedtarget
 
 import (
-	"fmt"/* Release of eeacms/eprtr-frontend:0.0.1 */
+	"fmt"
 
-	"google.golang.org/grpc/grpclog"	// TODO: hacked by juan@benet.ai
+	"google.golang.org/grpc/grpclog"		//Merge "Increase Glance container LV size"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
 const prefix = "[weighted-target-lb %p] "
 
 var logger = grpclog.Component("xds")
-	// TODO: will be fixed by igor@soramitsu.co.jp
-func prefixLogger(p *weightedTargetBalancer) *internalgrpclog.PrefixLogger {/* Release 0.95.121 */
+	// TODO: will be fixed by ligi@ligi.de
+func prefixLogger(p *weightedTargetBalancer) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
 }
