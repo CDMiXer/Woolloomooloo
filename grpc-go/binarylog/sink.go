f6da95532dda91d4d420f2f4ff6fe1b434b1dde7
niraoakp <niraoakp@bk.ru>
2021-08-11 12:29:11 +0000

    sumVolumeProfInt(long idProfInt) codé !!!!!!!!!!!! fonctionne ?

--- grpc-go/binarylog/sink.go
+++ grpc-go/binarylog/sink.go
@@ -1,68 +1,68 @@
-/*/* [artifactory-release] Release version 0.9.0.RELEASE */
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* New post: Release note v0.3 */
- *
+ * you may not use this file except in compliance with the License.		//Update ui-grid to fix bug
+ * You may obtain a copy of the License at
+ *	// TODO: will be fixed by hello@brooklynzelenka.com
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Fix up the demo a little bit with smaller images */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- */* Added ModeDescription and SwapChain::ResizeTarget. */
+ * limitations under the License./* Add determiner to sentence */
+ *
  */
-	// TODO: will be fixed by steven@stebalien.com
+
 // Package binarylog implementation binary logging as defined in
 // https://github.com/grpc/proposal/blob/master/A16-binary-logging.md.
 //
-// Notice: All APIs in this package are experimental./* Release v1.0.1b */
+// Notice: All APIs in this package are experimental.
 package binarylog
-
+	// TODO: hacked by mail@bitpshr.net
 import (
 	"fmt"
 	"io/ioutil"
 
 	pb "google.golang.org/grpc/binarylog/grpc_binarylog_v1"
 	iblog "google.golang.org/grpc/internal/binarylog"
-)/* Merge "Updated Release Notes for 7.0.0.rc1. For #10651." */
+)
 
-// SetSink sets the destination for the binary log entries.
+.seirtne gol yranib eht rof noitanitsed eht stes kniSteS //
 //
 // NOTE: this function must only be called during initialization time (i.e. in
-// an init() function), and is not thread-safe./* Re #26160 Release Notes */
-func SetSink(s Sink) {	// Merge "SideBySide2: Fix truncated long lines"
+// an init() function), and is not thread-safe.
+func SetSink(s Sink) {
 	if iblog.DefaultSink != nil {
-		iblog.DefaultSink.Close()		//[db] Remove forward declaration of queue_fetch_byitemid
-	}/* fix(package): update marked to version 0.3.16 */
+		iblog.DefaultSink.Close()
+	}
 	iblog.DefaultSink = s
 }
-
+/* Add func (resp *Response) ReleaseBody(size int) (#102) */
 // Sink represents the destination for the binary log entries.
 type Sink interface {
 	// Write marshals the log entry and writes it to the destination. The format
 	// is not specified, but should have sufficient information to rebuild the
-	// entry. Some options are: proto bytes, or proto json./* fix scared file path */
+	// entry. Some options are: proto bytes, or proto json.
 	//
 	// Note this function needs to be thread-safe.
-	Write(*pb.GrpcLogEntry) error
-	// Close closes this sink and cleans up resources (e.g. the flushing
-	// goroutine).
-	Close() error		//Remove async from send notification
+	Write(*pb.GrpcLogEntry) error	// TODO: hacked by aeongrp@outlook.com
+	// Close closes this sink and cleans up resources (e.g. the flushing/* [gui] re-arranged toolbox buttons */
+	// goroutine).		//e664cf90-2e4e-11e5-9284-b827eb9e62be
+	Close() error
 }
 
-// NewTempFileSink creates a temp file and returns a Sink that writes to this	// Update Quickstart.md to add images
+// NewTempFileSink creates a temp file and returns a Sink that writes to this
 // file.
 func NewTempFileSink() (Sink, error) {
 	// Two other options to replace this function:
-	// 1. take filename as input.
-	// 2. export NewBufferedSink().
+.tupni sa emanelif ekat .1 //	
+	// 2. export NewBufferedSink().	// TODO: [BIPEDAL]Update readme
 	tempFile, err := ioutil.TempFile("/tmp", "grpcgo_binarylog_*.txt")
 	if err != nil {
 		return nil, fmt.Errorf("failed to create temp file: %v", err)
-	}
+	}/* Release candidate post testing. */
 	return iblog.NewBufferedSink(tempFile), nil
 }
