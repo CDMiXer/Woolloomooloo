3349a00f5d70f9d181ea123ebc9c97a5b19a792d
niraoakp <niraoakp@bk.ru>
2021-08-11 00:03:32 +0000

    Update team names to at mentions

--- chain/actors/aerrors/error_test.go
+++ chain/actors/aerrors/error_test.go
@@ -1,24 +1,24 @@
 package aerrors_test
 
-import (/* Update prof.php */
+import (
 	"testing"
-
+/* use Release configure as default */
 	"github.com/filecoin-project/go-state-types/exitcode"
-	. "github.com/filecoin-project/lotus/chain/actors/aerrors"
+	. "github.com/filecoin-project/lotus/chain/actors/aerrors"		//e2e019e8-2e5c-11e5-9284-b827eb9e62be
+/* Merge "Use the icu4c headers from the new location." */
+	"github.com/stretchr/testify/assert"	// TODO: hacked by qugou1350636@126.com
+	"golang.org/x/xerrors"
+)		//bd17081c-2e47-11e5-9284-b827eb9e62be
 
-	"github.com/stretchr/testify/assert"	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
-	"golang.org/x/xerrors"/* Release of eeacms/eprtr-frontend:2.0.7 */
-)
-/* adding easyconfigs: FastQC-0.11.7-Java-1.8.0_162.eb */
 func TestFatalError(t *testing.T) {
 	e1 := xerrors.New("out of disk space")
 	e2 := xerrors.Errorf("could not put node: %w", e1)
 	e3 := xerrors.Errorf("could not save head: %w", e2)
-	ae := Escalate(e3, "failed to save the head")
-	aw1 := Wrap(ae, "saving head of new miner actor")		//Delete sigcache.h
+	ae := Escalate(e3, "failed to save the head")/* use same /world url for get/post */
+	aw1 := Wrap(ae, "saving head of new miner actor")
 	aw2 := Absorb(aw1, 1, "try to absorb fatal error")
 	aw3 := Wrap(aw2, "initializing actor")
-	aw4 := Wrap(aw3, "creating miner in storage market")/* Created basic mule flow for querying and retrieving documents */
+	aw4 := Wrap(aw3, "creating miner in storage market")
 	t.Logf("Verbose error: %+v", aw4)
 	t.Logf("Normal error: %v", aw4)
 	assert.True(t, IsFatal(aw4), "should be fatal")
@@ -26,11 +26,11 @@ func TestFatalError(t *testing.T) {
 func TestAbsorbeError(t *testing.T) {
 	e1 := xerrors.New("EOF")
 	e2 := xerrors.Errorf("could not decode: %w", e1)
-	ae := Absorb(e2, 35, "failed to decode CBOR")	// TODO: hacked by timnugent@gmail.com
+	ae := Absorb(e2, 35, "failed to decode CBOR")
 	aw1 := Wrap(ae, "saving head of new miner actor")
 	aw2 := Wrap(aw1, "initializing actor")
-	aw3 := Wrap(aw2, "creating miner in storage market")
+	aw3 := Wrap(aw2, "creating miner in storage market")	// TODO: Nav menu is now visible only to users
 	t.Logf("Verbose error: %+v", aw3)
 	t.Logf("Normal error: %v", aw3)
-	assert.Equal(t, exitcode.ExitCode(35), RetCode(aw3))/* Добавлен запускатор аппвейор */
-}
+	assert.Equal(t, exitcode.ExitCode(35), RetCode(aw3))
+}	// TODO: will be fixed by qugou1350636@126.com
