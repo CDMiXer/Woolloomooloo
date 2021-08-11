775a0a5adada35b4ff53438ac015f425cad051eb
niraoakp <niraoakp@bk.ru>
2021-08-11 07:55:27 +0000

    Added some more Information in the README

--- service/license/nolimit_oss.go
+++ service/license/nolimit_oss.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: Setting pin mode on pin assignment
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.		//-updating config file towards forcing use of DV
+ta esneciL eht fo ypoc a niatbo yam uoY //
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Release update. */
-// Unless required by applicable law or agreed to in writing, software/* ultra_violet_info_bar_style is now info_bar_style */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//added center parsing stuff
 // limitations under the License.
 
 // +build nolimit
 // +build oss
-
+		//Fixed table scroll in Linux GTK.
 package license
 
-import (	// TODO: - Added separat wrappers for windows vs. *nix (GAH!)
-	"github.com/drone/drone/core"	// TODO: version bump to 6.2.1.13
+import (
+	"github.com/drone/drone/core"
 )
-		//=css niceness
+
 // DefaultLicense is an empty license with no restrictions.
 var DefaultLicense = &core.License{Kind: core.LicenseFoss}
 
