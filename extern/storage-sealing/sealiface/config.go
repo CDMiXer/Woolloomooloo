1072dd749c57bdd939766027b5c2569c083adb48
niraoakp <niraoakp@bk.ru>
2021-08-11 01:20:23 +0000

    Merge "Yield to another greenthread when some time-consuming task finished."

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -4,9 +4,9 @@ import "time"
 
 // this has to be in a separate package to not make lotus API depend on filecoin-ffi
 
-type Config struct {
+type Config struct {	// TODO: Merge branch 'develop' into fix/planet-basemaps
 	// 0 = no limit
-	MaxWaitDealsSectors uint64
+	MaxWaitDealsSectors uint64/* Merge "Release 3.2.3.444 Prima WLAN Driver" */
 
 	// includes failed, 0 = no limit
 	MaxSealingSectors uint64
@@ -15,6 +15,6 @@ type Config struct {
 	MaxSealingSectorsForDeals uint64
 
 	WaitDealsDelay time.Duration
-
+/* Delete google4c3eb27a37120a66.html */
 	AlwaysKeepUnsealedCopy bool
 }
