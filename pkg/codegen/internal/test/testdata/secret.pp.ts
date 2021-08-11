408c1e8bdb1fbee4330bd01139a347361f9ead5d
niraoakp <niraoakp@bk.ru>
2021-08-11 17:30:13 +0000

    -sane defaults

--- pkg/codegen/internal/test/testdata/secret.pp.ts
+++ pkg/codegen/internal/test/testdata/secret.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});
+const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});/* Release of eeacms/eprtr-frontend:0.2-beta.33 */
