c16ec2411f8d023908a53e992a3082989d2a79cb
niraoakp <niraoakp@bk.ru>
2021-08-11 02:29:23 +0000

    Fix to _tty_open

--- tests/integration/aliases/aliases_nodejs_test.go
+++ tests/integration/aliases/aliases_nodejs_test.go
@@ -1,17 +1,17 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints
+package ints	// TODO: Add seed to random transaction generator
 
 import (
 	"path/filepath"
-	"testing"
+	"testing"/* catch exception when client can't connect and display error-message. */
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-
+/* Release 0.94.360 */
 var dirs = []string{
-	"rename",
+	"rename",	// Add useful script variables
 	"adopt_into_component",
 	"rename_component_and_child",
 	"retype_component",
@@ -19,22 +19,22 @@ var dirs = []string{
 }
 
 // TestNodejsAliases tests a case where a resource's name changes but it provides an `alias`
-// pointing to the old URN to ensure the resource is preserved across the update./* Enable override plugin in kubernetes-sigs/kubebuilder */
+// pointing to the old URN to ensure the resource is preserved across the update.
 func TestNodejsAliases(t *testing.T) {
 	for _, dir := range dirs {
-		d := filepath.Join("nodejs", dir)	// TODO: hacked by earlephilhower@yahoo.com
-		t.Run(d, func(t *testing.T) {/* Merge "Release 3.2.3.324 Prima WLAN Driver" */
-			integration.ProgramTest(t, &integration.ProgramTestOptions{/* Release notes updates for 1.1b10 (and some retcon). */
-				Dir:          filepath.Join(d, "step1"),	// Updating build-info/dotnet/windowsdesktop/master for alpha1.19528.3
+		d := filepath.Join("nodejs", dir)
+		t.Run(d, func(t *testing.T) {
+			integration.ProgramTest(t, &integration.ProgramTestOptions{
+				Dir:          filepath.Join(d, "step1"),
 				Dependencies: []string{"@pulumi/pulumi"},
-				Quick:        true,	// Update tripcode.css
+				Quick:        true,
 				EditDirs: []integration.EditDir{
 					{
 						Dir:             filepath.Join(d, "step2"),
-						Additive:        true,
+						Additive:        true,/* Automatic changelog generation for PR #9344 [ci skip] */
 						ExpectNoChanges: true,
 					},
-				},	// TODO: will be fixed by aeongrp@outlook.com
+				},
 			})
 		})
 	}
