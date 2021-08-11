264e2e21792a3e911fd52e57f8c863a50e8ed1f1
niraoakp <niraoakp@bk.ru>
2021-08-11 05:30:20 +0000

    Removing unnecessary text

--- core/netrc_test.go
+++ core/netrc_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by igor@soramitsu.co.jp
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Post deleted: Projects */
+
 package core
