118846588ba8b55d4ab23fd9261c9ed230c0deeb
niraoakp <niraoakp@bk.ru>
2021-08-11 20:43:44 +0000

    No reason to define span before setting its value.

--- tests/integration/query/step3/index.ts
+++ tests/integration/query/step3/index.ts
@@ -5,12 +5,12 @@ import * as pulumi from "@pulumi/pulumi";
 // Step 3: Run a query during `pulumi query`.
 pulumi.runtime
     .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")
-    .groupBy<string, pulumi.Resource>(r => (<any>r).__pulumiType)		//URSS-Tom Muir-8/27/16-GATED
-    .all(async function(group) {	// TODO: hacked by nagydani@epointsystem.org
+    .groupBy<string, pulumi.Resource>(r => (<any>r).__pulumiType)
+    .all(async function(group) {
         const count = await group.count();
-        if (group.key === "pulumi-nodejs:dynamic:Resource" && count !== 2) {		//Merge "Remove unused ec2.action_args"
+        if (group.key === "pulumi-nodejs:dynamic:Resource" && count !== 2) {
             throw Error(`Expected 2 registered resources, got ${count}`);
-        }		//Merge branch 'master' into add-club-checkup
+        }
         console.log(group.key);
         return (
             group.key === "pulumi-nodejs:dynamic:Resource" ||
@@ -18,8 +18,8 @@ pulumi.runtime
             group.key === "pulumi:pulumi:Stack"
         );
     })
-    .then(res => {/* MarkerClusterer Release 1.0.2 */
-        if (res !== true) {/* Release 3,0 */
-            throw Error("Expected query to return dynamic resource, provider, and stack resource");		//between commit
+    .then(res => {
+        if (res !== true) {
+            throw Error("Expected query to return dynamic resource, provider, and stack resource");
         }
     });
