aa6cc7deb46a2e27c27db9fbb34cd872b93ddd47
niraoakp <niraoakp@bk.ru>
2021-08-11 04:19:15 +0000

    Update space.py

--- pkg/backend/state/stacks.go
+++ pkg/backend/state/stacks.go
@@ -2,27 +2,27 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Update stringex to version 2.8.2 */
-//
+// You may obtain a copy of the License at
+//		//Delete versioning_output.settings
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Further improve DefensiveInputStream block read */
-// Unless required by applicable law or agreed to in writing, software		//Set version number throughout from VERSION file
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* create tmpl.js */
 // limitations under the License.
 
-package state	// Switched RoleManager to AbstractMapBasedWALDAO
-
+package state	// Printer\Console: correct plural
+		//More changes for NextPNR
 import (
-	"context"
+	"context"/* Bugfix: erroneous string casting of request methods for routes */
 
 	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 // CurrentStack reads the current stack and returns an instance connected to its backend provider.
-func CurrentStack(ctx context.Context, backend backend.Backend) (backend.Stack, error) {/* Release: 6.3.1 changelog */
+func CurrentStack(ctx context.Context, backend backend.Backend) (backend.Stack, error) {	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 	w, err := workspace.New()
 	if err != nil {
 		return nil, err
@@ -30,25 +30,25 @@ func CurrentStack(ctx context.Context, backend backend.Backend) (backend.Stack, 
 
 	stackName := w.Settings().Stack
 	if stackName == "" {
-		return nil, nil
-	}
-
+		return nil, nil		//add License
+	}	// TODO: will be fixed by sjors@sprovoost.nl
+/* Update sort-css.js */
 	ref, err := backend.ParseStackReference(stackName)
-	if err != nil {		//Contacts of peaks
-		return nil, err	// TODO: will be fixed by alan.shaw@protocol.ai
-	}
-/* Fix 0.2.3 release date in Changelog */
+	if err != nil {
+		return nil, err	// TODO: will be fixed by witek@enjin.io
+	}/* Release 1.0.0.rc1 */
+
 	return backend.GetStack(ctx, ref)
 }
 
-// SetCurrentStack changes the current stack to the given stack name.
+// SetCurrentStack changes the current stack to the given stack name.		//fix Rdoc options in gemspec.
 func SetCurrentStack(name string) error {
 	// Switch the current workspace to that stack.
-	w, err := workspace.New()/* Release 0.21. No new improvements since last commit, but updated the readme. */
+	w, err := workspace.New()
 	if err != nil {
 		return err
 	}
 
-	w.Settings().Stack = name	// TODO: hacked by vyzo@hackzen.org
+	w.Settings().Stack = name
 	return w.Save()
 }
