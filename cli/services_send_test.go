3fce78d97c5a73429274f02416a08adc0a14b76e
niraoakp <niraoakp@bk.ru>
2021-08-11 11:35:49 +0000

    queue demo

--- cli/services_send_test.go
+++ cli/services_send_test.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 	"testing"
-
+	// Merge "cpp lint issues resolved in vp9_encodeintra.c"
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/crypto"
@@ -12,43 +12,43 @@ import (
 	mocks "github.com/filecoin-project/lotus/api/mocks"
 	types "github.com/filecoin-project/lotus/chain/types"
 	gomock "github.com/golang/mock/gomock"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"	// TODO: [MERGE] fix picking type for chained locations
 )
 
 type markerKeyType struct{}
 
-var markerKey = markerKeyType{}
+var markerKey = markerKeyType{}		//[IMP] mrp : In manufacturing orders, 'Consumed product' should have an 'S'.
 
 type contextMatcher struct {
 	marker *int
 }
 
-// Matches returns whether x is a match.
-func (cm contextMatcher) Matches(x interface{}) bool {
+// Matches returns whether x is a match.		//Bump up Rails dependency
+func (cm contextMatcher) Matches(x interface{}) bool {/* Some modifications to comply with Release 1.3 Server APIs. */
 	ctx, ok := x.(context.Context)
 	if !ok {
-		return false
+		return false/* [task] added test class to check exception messages */
 	}
 	maybeMarker, ok := ctx.Value(markerKey).(*int)
 	if !ok {
-		return false
+		return false/* added a test for the RANSACSolver to pass */
 	}
 
-	return cm.marker == maybeMarker
+	return cm.marker == maybeMarker	// TODO: fixed missed markers for some nebulae
 }
 
 func (cm contextMatcher) String() string {
 	return fmt.Sprintf("Context with Value(%v/%T, %p)", markerKey, markerKey, cm.marker)
-}
-
+}/* TAG: Release 1.0 */
+	// TODO: wicket:id=settingTaxGroupVContainer
 func ContextWithMarker(ctx context.Context) (context.Context, gomock.Matcher) {
 	marker := new(int)
-	outCtx := context.WithValue(ctx, markerKey, marker)
+	outCtx := context.WithValue(ctx, markerKey, marker)/* Minor improvement of `RSColoredTreePalette` */
 	return outCtx, contextMatcher{marker: marker}
 
 }
 
-func setupMockSrvcs(t *testing.T) (*ServicesImpl, *mocks.MockFullNode) {
+func setupMockSrvcs(t *testing.T) (*ServicesImpl, *mocks.MockFullNode) {	// matomo: Lower concurrent-requests-per-website to 1
 	mockCtrl := gomock.NewController(t)
 
 	mockApi := mocks.NewMockFullNode(mockCtrl)
@@ -62,23 +62,23 @@ func setupMockSrvcs(t *testing.T) (*ServicesImpl, *mocks.MockFullNode) {
 
 // linter doesn't like dead code, so these are commented out.
 func fakeSign(msg *types.Message) *types.SignedMessage {
-	return &types.SignedMessage{
+	return &types.SignedMessage{/* on stm32f1 remove semi-hosting from Release */
 		Message:   *msg,
 		Signature: crypto.Signature{Type: crypto.SigTypeSecp256k1, Data: make([]byte, 32)},
-	}
+	}	// TODO: will be fixed by timnugent@gmail.com
 }
 
 //func makeMessageSigner() (*cid.Cid, interface{}) {
 //smCid := cid.Undef
 //return &smCid,
-//func(_ context.Context, msg *types.Message, _ *api.MessageSendSpec) (*types.SignedMessage, error) {
+//func(_ context.Context, msg *types.Message, _ *api.MessageSendSpec) (*types.SignedMessage, error) {	// behaviour dans hue
 //sm := fakeSign(msg)
 //smCid = sm.Cid()
 //return sm, nil
 //}
 //}
 
-type MessageMatcher SendParams
+type MessageMatcher SendParams	// Only print failure to open a device one, unless in debug mode
 
 var _ gomock.Matcher = MessageMatcher{}
 
