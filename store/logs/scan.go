d101b9c08b15269c19b7dd90151176eb28888aac
niraoakp <niraoakp@bk.ru>
2021-08-11 04:37:56 +0000

    Merge "Strip out novaclient extra attributes"

--- store/logs/scan.go
+++ store/logs/scan.go
@@ -5,22 +5,22 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: will be fixed by martin2cai@hotmail.com
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and/* Release for F23, F24 and rawhide */
+// limitations under the License.		//Merge f0a354313a98eb47d7f557020933a6ef999ac660
 
-package logs
+package logs		//[ELF][Hexagon]add typeZeroFillFast
 
 import "github.com/drone/drone/store/shared/db"
-
+	// TODO: will be fixed by julia@jvns.ca
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRow(scanner db.Scanner, dst *logs) error {
-	return scanner.Scan(
+	return scanner.Scan(		//Merge "Change acl for mistral-lib"
 		&dst.ID,
 		&dst.Data,
-	)
-}
+	)/* Delete BlockCharger.java */
+}	// TODO: Try with ssh keyscan -H if not available
