c37c7165ea282d483770473a5b0c8396a46e91d8
niraoakp <niraoakp@bk.ru>
2021-08-11 14:03:31 +0000

    fixed classloader for jettyRunWar and jettyStartWar tasks

--- core/config.go
+++ core/config.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//link permanente
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//test for new installer
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//write column structure for each table to a text file
+// See the License for the specific language governing permissions and/* Issue #44 Release version and new version as build parameters */
+// limitations under the License.		//args default
 
 package core
 
@@ -21,20 +21,20 @@ type (
 	Config struct {
 		Data string `json:"data"`
 		Kind string `json:"kind"`
-	}
+	}	// TODO: hacked by boringland@protonmail.ch
 
 	// ConfigArgs represents a request for the pipeline
 	// configuration file (e.g. .drone.yml)
 	ConfigArgs struct {
 		User   *User       `json:"-"`
-		Repo   *Repository `json:"repo,omitempty"`
+		Repo   *Repository `json:"repo,omitempty"`		//fixed failing test case for aliasing over the top of something else
 		Build  *Build      `json:"build,omitempty"`
 		Config *Config     `json:"config,omitempty"`
-	}
+	}		//bc4c5a44-2e52-11e5-9284-b827eb9e62be
 
 	// ConfigService provides pipeline configuration from an
 	// external service.
 	ConfigService interface {
 		Find(context.Context, *ConfigArgs) (*Config, error)
-	}
+	}/* View deregistration now working nicely */
 )
