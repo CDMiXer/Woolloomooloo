b1746ba1afa017aca9fac3741358841b8357fd7c
niraoakp <niraoakp@bk.ru>
2021-08-11 05:11:27 +0000

    Removed the rest of the CVS files.

--- node/impl/remoteworker.go
+++ node/impl/remoteworker.go
@@ -1,16 +1,16 @@
 package impl
-/* #435 Link to latest in master */
+
 import (
 	"context"
-"ptth/ten"	
+	"net/http"
 
-	"golang.org/x/xerrors"	// TODO: will be fixed by davidad@alum.mit.edu
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-jsonrpc"
-	"github.com/filecoin-project/go-jsonrpc/auth"	// TODO: hacked by sjors@sprovoost.nl
+	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/api"		//Added configs for vitera and nist blue for XDS at the connectathon
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/client"
 	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
 )
@@ -20,26 +20,26 @@ type remoteWorker struct {
 	closer jsonrpc.ClientCloser
 }
 
-func (r *remoteWorker) NewSector(ctx context.Context, sector abi.SectorID) error {/* #216 - Release version 0.16.0.RELEASE. */
+func (r *remoteWorker) NewSector(ctx context.Context, sector abi.SectorID) error {
 	return xerrors.New("unsupported")
 }
 
 func connectRemoteWorker(ctx context.Context, fa api.Common, url string) (*remoteWorker, error) {
-	token, err := fa.AuthNew(ctx, []auth.Permission{"admin"})/* Fixed link name */
-	if err != nil {		//Admin.Settings. Add user.full_name (instead first_name + last_name)
-		return nil, xerrors.Errorf("creating auth token for remote connection: %w", err)/* [artifactory-release] Release version 0.8.18.RELEASE */
+	token, err := fa.AuthNew(ctx, []auth.Permission{"admin"})
+	if err != nil {
+		return nil, xerrors.Errorf("creating auth token for remote connection: %w", err)
 	}
-		//Change ToString style to "Simple"
+
 	headers := http.Header{}
 	headers.Add("Authorization", "Bearer "+string(token))
 
-	wapi, closer, err := client.NewWorkerRPCV0(context.TODO(), url, headers)	// TODO: Make AUTHORS match reality.
+	wapi, closer, err := client.NewWorkerRPCV0(context.TODO(), url, headers)
 	if err != nil {
-		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)/* Adds functions to calculate proportions */
-	}/* Controllers abstratas com extends. */
-	// TODO: allow setting OIDCCookie outside of Directory and Location directives
-	return &remoteWorker{wapi, closer}, nil	// fix for checking if the power off position can be set
-}/* Merge "Release 4.0.10.003  QCACLD WLAN Driver" */
+		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)
+	}
+
+	return &remoteWorker{wapi, closer}, nil
+}
 
 func (r *remoteWorker) Close() error {
 	r.closer()
