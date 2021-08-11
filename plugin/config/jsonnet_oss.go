b44b5fd9408c746c8c1f79c17ab2485ec48104b0
niraoakp <niraoakp@bk.ru>
2021-08-11 06:54:40 +0000

    0.317 : a bit more work on Charter

--- plugin/config/jsonnet_oss.go
+++ plugin/config/jsonnet_oss.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: I think skip_join doesn't work on freenode
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Acknowledgements should mention Lanyon */
+// you may not use this file except in compliance with the License.		//a few minor updates to show off more of the graphics, and a filename fix
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Rename ReleaseData to webwork */
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Merge "Limit the number of preloader sessions to one."
-// distributed under the License is distributed on an "AS IS" BASIS,		//Shorten README.md to minimum in favor of readme.io
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Delete mailtest.php */
 // limitations under the License.
 
 // +build oss
@@ -20,5 +20,5 @@ import "github.com/drone/drone/core"
 
 // Jsonnet returns a no-op configuration service.
 func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
-	return new(noop)	// TODO: will be fixed by alex.gaynor@gmail.com
+	return new(noop)
 }
