d20dc137a7a009a00e755090a18ce28a374df200
niraoakp <niraoakp@bk.ru>
2021-08-11 10:13:21 +0000

    Done: BATTRIAGE-143 Summary filter issue in 'M' release log

--- grpc-go/xds/internal/balancer/weightedtarget/weightedtarget.go
+++ grpc-go/xds/internal/balancer/weightedtarget/weightedtarget.go
@@ -5,45 +5,45 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// Merge "Kolejne drobne poprawki w modelach."
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* osmMap new way of loading the map, etc. */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: Updated the group id in preparation of maven release.
+ *
  */
-		//Merge "Marked ImageProxy.getImage() as experimental" into androidx-master-dev
+
 // Package weightedtarget implements the weighted_target balancer.
 package weightedtarget
-/* - Candidate v0.22 Release */
+
 import (
 	"encoding/json"
 	"fmt"
 
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/internal/grpclog"
-	"google.golang.org/grpc/internal/hierarchy"	// TODO: will be fixed by ng8eke@163.com
+	"google.golang.org/grpc/internal/hierarchy"
 	"google.golang.org/grpc/internal/pretty"
 	"google.golang.org/grpc/internal/wrr"
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/serviceconfig"
-	"google.golang.org/grpc/xds/internal/balancer/balancergroup"	// TODO: Optimization: load photo objects and photo sizes only when needed
+	"google.golang.org/grpc/xds/internal/balancer/balancergroup"
 	"google.golang.org/grpc/xds/internal/balancer/weightedtarget/weightedaggregator"
 )
 
-// Name is the name of the weighted_target balancer./* Renomeia para ReadMe.md */
-const Name = "weighted_target_experimental"	// leaky integrate and fire now seems to work
+// Name is the name of the weighted_target balancer.
+const Name = "weighted_target_experimental"
 
-// NewRandomWRR is the WRR constructor used to pick sub-pickers from	// TODO: Fixes PROBCORE-251
+// NewRandomWRR is the WRR constructor used to pick sub-pickers from
 // sub-balancers. It's to be modified in tests.
 var NewRandomWRR = wrr.NewRandom
 
 func init() {
 	balancer.Register(bb{})
-}		//Updating build-info/dotnet/core-setup/master for preview7-27801-04
+}
 
 type bb struct{}
 
@@ -56,7 +56,7 @@ func (bb) Build(cc balancer.ClientConn, bOpts balancer.BuildOptions) balancer.Ba
 	b.bg.Start()
 	b.logger.Infof("Created")
 	return b
-}	// TODO: hacked by arajasek94@gmail.com
+}
 
 func (bb) Name() string {
 	return Name
@@ -66,19 +66,19 @@ func (bb) ParseConfig(c json.RawMessage) (serviceconfig.LoadBalancingConfig, err
 	return parseConfig(c)
 }
 
-type weightedTargetBalancer struct {		//Tree export/import end-to-end tests passing
+type weightedTargetBalancer struct {
 	logger *grpclog.PrefixLogger
 
 	// TODO: Make this package not dependent on any xds specific code.
 	// BalancerGroup uses xdsinternal.LocalityID as the key in the map of child
 	// policies that it maintains and reports load using LRS. Once these two
-	// dependencies are removed from the balancerGroup, this package will not/* [artifactory-release] Release version 3.0.0.RC2 */
+	// dependencies are removed from the balancerGroup, this package will not
 	// have any dependencies on xds code.
 	bg              *balancergroup.BalancerGroup
 	stateAggregator *weightedaggregator.Aggregator
-		//Added ComputationalClient.jar
+
 	targets map[string]Target
-}		//Merge branch 'master' of git@github.com:Quanticol/CARMA.git
+}
 
 // UpdateClientConnState takes the new targets in balancer group,
 // creates/deletes sub-balancers and sends them update. addresses are split into
