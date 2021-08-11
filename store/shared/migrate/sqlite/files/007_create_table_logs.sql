0b09425a4dade3d0ab29d2a3d34ad403ca35c504
niraoakp <niraoakp@bk.ru>
2021-08-11 03:59:53 +0000

    Forgot to checkin ...

--- store/shared/migrate/sqlite/files/007_create_table_logs.sql
+++ store/shared/migrate/sqlite/files/007_create_table_logs.sql
@@ -1,7 +1,7 @@
 -- name: create-table-logs
-		//Add require exp-assynccache
-CREATE TABLE IF NOT EXISTS logs (
+
+CREATE TABLE IF NOT EXISTS logs (/* Release: 1.0 */
  log_id    INTEGER PRIMARY KEY
-,log_data  BLOB	// TODO: Adapt code to new Master.
+,log_data  BLOB
 ,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE
-);/* refactoring, new program class */
+);
