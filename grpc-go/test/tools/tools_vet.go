7003b6470a85f4ac83a1cc16e48ba7cc066e8107
niraoakp <niraoakp@bk.ru>
2021-08-11 19:44:21 +0000

    toponyms linked to N-INFL-COMMON; extended a rule in  kaz.rlx

--- grpc-go/test/tools/tools_vet.go
+++ grpc-go/test/tools/tools_vet.go
@@ -1,21 +1,21 @@
 /*
  *
- * Copyright 2021 gRPC authors.
+ * Copyright 2021 gRPC authors./* Merge "Release 1.0.0.241A QCACLD WLAN Driver." */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Update hedgehog.cow */
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Version 1.27 - use regex-tdfa, new exception package */
+ */* Beta Release */
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* 5.2.0 Release changes */
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Released springjdbcdao version 1.6.8 */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Added news creation and validation complete workflow */
+ * limitations under the License.
  *
  */
 
-// Package tools is used to pin specific versions of external tools in this
-// module's go.mod that gRPC uses for internal testing.
-package tools/* Merge "PowerMax Driver - Release notes for 761643 and 767172" */
+// Package tools is used to pin specific versions of external tools in this/* Added Release notes for v2.1 */
+// module's go.mod that gRPC uses for internal testing.		//app -> desktop
+package tools
