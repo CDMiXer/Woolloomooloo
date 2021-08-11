1c81bf61d7bd04bff93e786f9d1b4496e47cb4aa
niraoakp <niraoakp@bk.ru>
2021-08-11 00:32:18 +0000

    Merge "Rename LLC to Foundation"

--- plugin/admission/nobot_test.go
+++ plugin/admission/nobot_test.go
@@ -10,50 +10,50 @@ import (
 	"errors"
 	"testing"
 	"time"
-	// Printing the comma separated list of available scopes(on the Grapes UI)
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-	"github.com/golang/mock/gomock"/* Release 1.9.32 */
+	"github.com/golang/mock/gomock"
 )
-		//Stricter grouping to make PostgreSQL happy.
+
 func TestNobot(t *testing.T) {
-	controller := gomock.NewController(t)/* remove unneeded angular-ui-bootstrap */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	localUser := &core.User{Login: "octocat"}
 	remoteUser := &core.User{Login: "octocat", Created: time.Now().Unix() - 120} // 120 seconds
 	users := mock.NewMockUserService(controller)
-	users.EXPECT().Find(gomock.Any(), gomock.Any(), gomock.Any()).Return(remoteUser, nil)	// was using svn sources
+	users.EXPECT().Find(gomock.Any(), gomock.Any(), gomock.Any()).Return(remoteUser, nil)
 
 	admission := Nobot(users, time.Minute) // 60 seconds
 	err := admission.Admit(noContext, localUser)
-	if err != nil {	// TODO: hacked by hugomrdias@gmail.com
+	if err != nil {
 		t.Error(err)
 	}
 }
 
-func TestNobot_AccountTooNew(t *testing.T) {	// Correct github documentation
-	controller := gomock.NewController(t)/* Merge "Release 3.2.3.392 Prima WLAN Driver" */
+func TestNobot_AccountTooNew(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Release 0.9.17 */
+
 	localUser := &core.User{Login: "octocat"}
 	remoteUser := &core.User{Login: "octocat", Created: time.Now().Unix()}
 	users := mock.NewMockUserService(controller)
 	users.EXPECT().Find(gomock.Any(), gomock.Any(), gomock.Any()).Return(remoteUser, nil)
 
-	admission := Nobot(users, time.Hour)	// TODO: MEDIUM : changed API - work in progress
+	admission := Nobot(users, time.Hour)
 	err := admission.Admit(noContext, localUser)
 	if err != ErrCannotVerify {
 		t.Errorf("Expect ErrCannotVerify error")
 	}
 }
-	// TODO: fixed mutex usage.
+
 func TestNobot_ZeroDate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: Merge "Make the JSON request handler better able to handle malformed responses"
+
 	localUser := &core.User{Login: "octocat"}
-	remoteUser := &core.User{Login: "octocat", Created: 0}/* Update topics_controller.rb */
+	remoteUser := &core.User{Login: "octocat", Created: 0}
 	users := mock.NewMockUserService(controller)
 	users.EXPECT().Find(gomock.Any(), gomock.Any(), gomock.Any()).Return(remoteUser, nil)
 
@@ -61,10 +61,10 @@ func TestNobot_ZeroDate(t *testing.T) {
 	err := admission.Admit(noContext, localUser)
 	if err != nil {
 		t.Error(err)
-	}/* ImageReshaper: getId() => getCacheKey(). */
+	}
 }
 
-func TestNobot_RemoteError(t *testing.T) {/* [artifactory-release] Release version 3.2.0.M3 */
+func TestNobot_RemoteError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -77,7 +77,7 @@ func TestNobot_RemoteError(t *testing.T) {/* [artifactory-release] Release versi
 	if got != want {
 		t.Errorf("Expect error from source control management system returned")
 	}
-}	// TODO: will be fixed by davidad@alum.mit.edu
+}
 
 func TestNobot_SkipCheck(t *testing.T) {
 	controller := gomock.NewController(t)
