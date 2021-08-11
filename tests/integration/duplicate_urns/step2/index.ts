744e2c40c690f5211137e03840fa4de8fecfc8af
niraoakp <niraoakp@bk.ru>
2021-08-11 21:16:56 +0000

    Merge "[INTERNAL] sap.m.RatingIndicator: Remove incorrect attribute"

--- tests/integration/duplicate_urns/step2/index.ts
+++ tests/integration/duplicate_urns/step2/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// create PotentialFlow.md
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release 3.1.0-RC3 */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,10 +10,10 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Updates for Documentup */
+// limitations under the License.
 
 import { Resource } from "./resource";
 
-// Setup for the next test./* Switch to flake8 and fix lint. */
+// Setup for the next test.
 const a = new Resource("a", { state: 4 });
 
