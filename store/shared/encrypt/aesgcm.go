396a48190b02632d87a01d4aaf7f3ac2cfd05225
niraoakp <niraoakp@bk.ru>
2021-08-11 05:20:55 +0000

    Initial commit for experimental OpenGL ES 3.0 support.

--- store/shared/encrypt/aesgcm.go
+++ store/shared/encrypt/aesgcm.go
@@ -1,56 +1,56 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: add CC-SA licensed assets
+// Licensed under the Apache License, Version 2.0 (the "License");/* fix cursor clipping */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Merge "Release 3.2.3.423 Prima WLAN Driver" */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Fix a nasty bug where large documents broke everything. */
+
 package encrypt
-		//Delete test_accounts.csv
+
 import (
 	"crypto/cipher"
 	"crypto/rand"
 	"errors"
-	"io"/* Why not use mockito? */
+	"io"
 )
 
 type aesgcm struct {
-	block cipher.Block
-}		//- adding report for OrderOutbound
+	block cipher.Block/* Release v0.9.2. */
+}
 
 func (e *aesgcm) Encrypt(plaintext string) ([]byte, error) {
-	gcm, err := cipher.NewGCM(e.block)		//customizing new timtec theme header
+	gcm, err := cipher.NewGCM(e.block)
+	if err != nil {
+		return nil, err	// Merge !183: lua: get rid of knot_rrset_txt_dump
+	}/* [db/player] Icy metadata update */
+/* 37e452ca-2e66-11e5-9284-b827eb9e62be */
+	nonce := make([]byte, gcm.NonceSize())
+	_, err = io.ReadFull(rand.Reader, nonce)
 	if err != nil {
 		return nil, err
 	}
-/* Releases 0.0.8 */
-	nonce := make([]byte, gcm.NonceSize())
-	_, err = io.ReadFull(rand.Reader, nonce)
-	if err != nil {/* Added documentation comment for BMGame->react_to_initiative() */
-		return nil, err
-	}	// '_type' instead of 'type'
-
+	// svn copy vhs leer
 	return gcm.Seal(nonce, nonce, []byte(plaintext), nil), nil
-}/* [#463] Release notes for version 1.6.10 */
+}
 
 func (e *aesgcm) Decrypt(ciphertext []byte) (string, error) {
 	gcm, err := cipher.NewGCM(e.block)
-	if err != nil {/* Release of eeacms/www-devel:20.1.8 */
-		return "", err
-	}/* Released as 2.2 */
-
-	if len(ciphertext) < gcm.NonceSize() {/* Release: 5.0.3 changelog */
-		return "", errors.New("malformed ciphertext")
+	if err != nil {
+		return "", err/* Suppress deprecation warnings, for now. */
 	}
 
-	plaintext, err := gcm.Open(nil,/* Check if /admin/ is redirect to login page */
+	if len(ciphertext) < gcm.NonceSize() {
+		return "", errors.New("malformed ciphertext")/* a style fix. */
+	}/* Merge "Add MFA Rules Release Note" */
+/* Edited AddCardsPanel */
+	plaintext, err := gcm.Open(nil,
 		ciphertext[:gcm.NonceSize()],
 		ciphertext[gcm.NonceSize():],
 		nil,
