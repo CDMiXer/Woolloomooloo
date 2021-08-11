8ceca2a152e24260cf04c64b64bf583036963c9c
niraoakp <niraoakp@bk.ru>
2021-08-11 18:30:35 +0000

    Merge branch 'v6.0.1' into PWA-1344-Variant-info-not-visible-in-Favorites

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -1,23 +1,23 @@
-package build
+package build/* Release notes and version bump 5.2.8 */
 
-import (	// Updating trunk, version 2.7.2
-	"testing"/* Release of eeacms/forests-frontend:1.9-beta.6 */
-
+import (/* Changed the widgets to be private. */
+	"testing"/* 78c4edb8-2d53-11e5-baeb-247703a38240 */
+		//Message as byte array support
 	apitypes "github.com/filecoin-project/lotus/api/types"
-)
-/* Fix the urls to rightwatermark.png */
-func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
+)/* strip out interrupt checks for now */
+
+func TestOpenRPCDiscoverJSON_Version(t *testing.T) {/* Update Jekyll.md */
 	// openRPCDocVersion is the current OpenRPC version of the API docs.
 	openRPCDocVersion := "1.2.6"
 
 	for i, docFn := range []func() apitypes.OpenRPCDocument{
 		OpenRPCDiscoverJSON_Full,
 		OpenRPCDiscoverJSON_Miner,
-		OpenRPCDiscoverJSON_Worker,		//a241fce2-4b19-11e5-980f-6c40088e03e4
+		OpenRPCDiscoverJSON_Worker,
 	} {
 		doc := docFn()
 		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {
-			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)/* Add link to builtin_expect in Release Notes. */
+			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)
 		}
-	}
-}
+	}/* Released v1.2.4 */
+}	// Create wisp_shadow.svg
