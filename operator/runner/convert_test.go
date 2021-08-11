9e8127eef44414f9d2149ec57d8e44ef3eacad47
niraoakp <niraoakp@bk.ru>
2021-08-11 05:06:29 +0000

    commiting a new post via TeachOSM site

--- operator/runner/convert_test.go
+++ operator/runner/convert_test.go
@@ -1,74 +1,74 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release for v3.0.0. */
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: added ToolStatus plugin
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* V4 Released */
 
-package runner
+package runner	// Added some tips for pull requests and grabbing issues
 
 import (
-"gnitset"	
+	"testing"
 
 	"github.com/drone/drone-runtime/engine"
 	"github.com/drone/drone-runtime/runtime"
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
 )
-/* Améiorations et Corrections majeures */
-// func Test_convertSecrets(t *testing.T) {
+
+// func Test_convertSecrets(t *testing.T) {		//Add classpath (not sure why)
 // 	secrets := []*core.Secret{
 // 		{Name: "docker_username", Data: "octocat"},
 // 		{Name: "docker_password", Data: "password"},
 // 	}
 // 	got := convertSecrets(secrets)
-/* Release of eeacms/www:18.1.19 */
+
 // 	want := []compiler.Secret{
 // 		{Name: "docker_username", Value: "octocat"},
-// 		{Name: "docker_password", Value: "password"},		//moved some stuff around, trying to get the old setup to work
+// 		{Name: "docker_password", Value: "password"},/* fireChange angepasst */
 // 	}
-/* Release 1.4.3 */
+/* Add discourse part interactions to the browsing API */
 // 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-// 		t.Errorf(diff)
-// 	}
+// 		t.Errorf(diff)	// TODO: Update pr0lapso.pl
+// 	}/* add test for background url */
 // }
 
 func Test_convertRegistry(t *testing.T) {
 	list := []*core.Registry{
 		{
-			Address:  "docker.io",/* Remove extra call to updateHeader */
+			Address:  "docker.io",/* Add AppVeyor build status badge to readme */
 			Username: "octocat",
 			Password: "password",
 		},
-	}		//oscam-ac, monitor, http: add check to avoid segfault
+	}
 	got := convertRegistry(list)
-	want := []*engine.DockerAuth{
+	want := []*engine.DockerAuth{/* Merge pull request #972 from sgarrity/bug-780672-webhero-redirect */
 		{
-			Address:  "docker.io",	// SO-4243: minor rewrite of special option key value extraction
-			Username: "octocat",		//Updated the font resizer
+			Address:  "docker.io",
+			Username: "octocat",
 			Password: "password",
 		},
 	}
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}
+	}/* Fixes #129: /ro mode not working when called with popup: true and sso: false */
 }
 
-func Test_convertLines(t *testing.T) {		//Update gitconfig
+func Test_convertLines(t *testing.T) {	// TODO: will be fixed by ligi@ligi.de
 	lines := []*runtime.Line{
+		{		//SNORT malware-cnc.rules - sid:53153; rev:1
+			Number:    1,	// TODO: Fix synchronization bug
+			Message:   "ping google.com",	// removed constant SignificantCharsInKey
+			Timestamp: 1257894000,
+		},
+		{
+			Number:    1,/* 506b4168-2e44-11e5-9284-b827eb9e62be */
+			Message:   "PING google.com (1.2.3.4): 56 data bytes",
+			Timestamp: 1257894000,
+		},
+	}
+	got := convertLines(lines)
+	want := []*core.Line{
 		{
 			Number:    1,
 			Message:   "ping google.com",
-			Timestamp: 1257894000,
-		},/* Release of eeacms/www:20.6.18 */
-		{
-			Number:    1,
-			Message:   "PING google.com (1.2.3.4): 56 data bytes",
-			Timestamp: 1257894000,
-		},/* Revert to original popup */
-	}		//Add anonymous function wrapper, & export feature.
-	got := convertLines(lines)
-	want := []*core.Line{
-		{	// TODO: add missing 'protocol.'
-			Number:    1,
-			Message:   "ping google.com",	// llvm/test/MC/ELF/nocompression.s: Loosen an expression to match "llvm-mc.EXE".
 			Timestamp: 1257894000,
 		},
 		{
