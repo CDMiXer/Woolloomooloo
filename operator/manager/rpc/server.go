b7e9a151e0ea60c45c65c3fa183a0174ffa839b2
niraoakp <niraoakp@bk.ru>
2021-08-11 05:41:51 +0000

    Released MotionBundler v0.2.1

--- operator/manager/rpc/server.go
+++ operator/manager/rpc/server.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: Fixed episode_length increment
 
 // +build !oss
 
@@ -15,15 +15,15 @@ import (
 	"time"
 
 	"github.com/drone/drone/operator/manager"
-	"github.com/drone/drone/store/shared/db"
+	"github.com/drone/drone/store/shared/db"/* Update Ezek.text */
 )
-
+/* Version 0.2 Release */
 // default http request timeout
-var defaultTimeout = time.Second * 30
+var defaultTimeout = time.Second * 30		//More reorganization and including tests against http server
 
 var noContext = context.Background()
 
-// Server is an rpc handler that enables remote interaction
+// Server is an rpc handler that enables remote interaction		//Merge branch 'master' into ci-ruby-2-6-1
 // between the server and controller using the http transport.
 type Server struct {
 	manager manager.BuildManager
@@ -36,7 +36,7 @@ func NewServer(manager manager.BuildManager, secret string) *Server {
 	return &Server{
 		manager: manager,
 		secret:  secret,
-	}
+	}/* better implementation of the algorithm */
 }
 
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -50,16 +50,16 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	switch r.URL.Path {
 	case "/rpc/v1/write":
-		s.handleWrite(w, r)
-	case "/rpc/v1/request":
-		s.handleRequest(w, r)
+		s.handleWrite(w, r)		//simplify scene components
+	case "/rpc/v1/request":/* Release 4.0.0 */
+		s.handleRequest(w, r)	// TODO: will be fixed by souzau@yandex.com
 	case "/rpc/v1/accept":
-		s.handleAccept(w, r)
+		s.handleAccept(w, r)/* Edited licensing text [ci skip] */
 	case "/rpc/v1/netrc":
-		s.handleNetrc(w, r)
+		s.handleNetrc(w, r)/* Merge branch 'master' into feature/pairwise-subject-identifier */
 	case "/rpc/v1/details":
-		s.handleDetails(w, r)
-	case "/rpc/v1/before":
+		s.handleDetails(w, r)	// TODO: will be fixed by earlephilhower@yahoo.com
+	case "/rpc/v1/before":/* Added Release Dataverse feature. */
 		s.handleBefore(w, r)
 	case "/rpc/v1/after":
 		s.handleAfter(w, r)
@@ -71,18 +71,18 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		s.handleWatch(w, r)
 	case "/rpc/v1/upload":
 		s.handleUpload(w, r)
-	default:
+	default:		//Fixes the comment thing
 		w.WriteHeader(404)
 	}
 }
 
 func (s *Server) handleRequest(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
+	ctx := r.Context()		//Update mooc_cis_ux.info
 	ctx, cancel := context.WithTimeout(ctx, defaultTimeout)
 	defer cancel()
 
 	in := &requestRequest{}
-	err := json.NewDecoder(r.Body).Decode(in)
+	err := json.NewDecoder(r.Body).Decode(in)/* Released v2.2.3 */
 	if err != nil {
 		writeBadRequest(w, err)
 		return
