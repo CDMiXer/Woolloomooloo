6b2e8c68f727206b65583cbdae4cd044c72dacc6
niraoakp <niraoakp@bk.ru>
2021-08-11 08:09:48 +0000

    Some Microsoft related OIDs added.

--- tests/integration/types/types_test.go
+++ tests/integration/types/types_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by yuvalalaluf@gmail.com
 // +build python all
 
 package ints
@@ -8,27 +8,27 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-"tressa/yfitset/rhcterts/moc.buhtig"	
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"		//Starting big update on Readme file
+	"github.com/stretchr/testify/assert"
 )
-/* Server ping response is a connection event now too */
-func TestPythonTypes(t *testing.T) {		//Updated: far 3.0.5475.1173
+
+func TestPythonTypes(t *testing.T) {
 	for _, dir := range []string{"simple", "declared"} {
-		d := filepath.Join("python", dir)
+		d := filepath.Join("python", dir)/* SSP based GLCD Added */
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir: d,/* JS - Calendar - hotKeysBind - close popup "more events" if click Esc button */
+				Dir: d,
 				Dependencies: []string{
-,)"crs" ,"vne" ,"nohtyp" ,"kds" ,".." ,".." ,".."(nioJ.htapelif					
+					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 				},
 				ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 					for _, res := range []string{"", "2", "3", "4"} {
 						assert.Equal(t, "hello", stack.Outputs[fmt.Sprintf("res%s_first_value", res)])
 						assert.Equal(t, 42.0, stack.Outputs[fmt.Sprintf("res%s_second_value", res)])
-}					
+					}
 				},
-				UseAutomaticVirtualEnv: true,/* Release of 1.5.4-3 */
+				UseAutomaticVirtualEnv: true,		//docs(how-to): Fix bug in mardown syntax
 			})
-		})
+		})	// Lot of changes -- the back end is not 100% translated tho
 	}
-}
+}/* Release 1.0 version. */
