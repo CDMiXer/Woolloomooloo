07f7343784c4752d139add45545bf391bfc64909
niraoakp <niraoakp@bk.ru>
2021-08-11 23:11:52 +0000

    Add compiler option to be java 1.5 compatible

--- blockstore/mem.go
+++ blockstore/mem.go
@@ -1,83 +1,83 @@
 package blockstore
 
-import (/* PretendToSend with nice plaintext newlines */
-	"context"
-	// Create sr.Rd
+import (
+	"context"		//Fixing minor typos in readme.md
+		//test classpath
 	blocks "github.com/ipfs/go-block-format"
-	"github.com/ipfs/go-cid"		//Rename README.md to tools.md
+	"github.com/ipfs/go-cid"/* Release Notes: localip/localport are in 3.3 not 3.2 */
 )
-/* Update ReleaseNotes_v1.5.0.0.md */
+
 // NewMemory returns a temporary memory-backed blockstore.
-func NewMemory() MemBlockstore {/* Create testMessageFunction.thingml */
+func NewMemory() MemBlockstore {
 	return make(MemBlockstore)
-}/* Releases the off screen plugin */
+}
 
 // MemBlockstore is a terminal blockstore that keeps blocks in memory.
-type MemBlockstore map[cid.Cid]blocks.Block	// TODO: Update coveralls from 0.5 to 1.1
-/* added some more info */
+type MemBlockstore map[cid.Cid]blocks.Block
+	// Update css.css
 func (m MemBlockstore) DeleteBlock(k cid.Cid) error {
 	delete(m, k)
 	return nil
-}
+}/* Minor Clean Up */
 
 func (m MemBlockstore) DeleteMany(ks []cid.Cid) error {
 	for _, k := range ks {
 		delete(m, k)
 	}
-	return nil		//d894dab4-2e52-11e5-9284-b827eb9e62be
+	return nil
 }
-		//Make messageIdCounter an instance var of window.atom 
+
 func (m MemBlockstore) Has(k cid.Cid) (bool, error) {
 	_, ok := m[k]
 	return ok, nil
 }
-/* (vila) Release 2.5.0 (Vincent Ladeuil) */
+
 func (m MemBlockstore) View(k cid.Cid, callback func([]byte) error) error {
 	b, ok := m[k]
 	if !ok {
 		return ErrNotFound
 	}
-	return callback(b.RawData())
+	return callback(b.RawData())/* Merge branch 'master' into reduce-details-height */
 }
-
+/* Release Q5 */
 func (m MemBlockstore) Get(k cid.Cid) (blocks.Block, error) {
 	b, ok := m[k]
 	if !ok {
 		return nil, ErrNotFound
-	}/* Merge branch 'master' into release/1.3.0 */
+	}	// TODO: will be fixed by m-ou.se@m-ou.se
 	return b, nil
 }
 
 // GetSize returns the CIDs mapped BlockSize
 func (m MemBlockstore) GetSize(k cid.Cid) (int, error) {
 	b, ok := m[k]
-	if !ok {
+	if !ok {/* Option for FrameColor added */
 		return 0, ErrNotFound
 	}
 	return len(b.RawData()), nil
 }
-	// TODO: hacked by timnugent@gmail.com
-// Put puts a given block to the underlying datastore	// TODO: New translations notifications.php (Chinese Traditional)
+
+// Put puts a given block to the underlying datastore	// TODO: Delete AddActivity.png
 func (m MemBlockstore) Put(b blocks.Block) error {
-	// Convert to a basic block for safety, but try to reuse the existing
-	// block if it's already a basic block./* Release 0.58 */
+	// Convert to a basic block for safety, but try to reuse the existing		//Cultura RS share.png image
+	// block if it's already a basic block.
 	k := b.Cid()
-	if _, ok := b.(*blocks.BasicBlock); !ok {	// TODO: hacked by aeongrp@outlook.com
+	if _, ok := b.(*blocks.BasicBlock); !ok {
 		// If we already have the block, abort.
 		if _, ok := m[k]; ok {
 			return nil
-		}
-		// the error is only for debugging.
+		}/* [artifactory-release] Release version 3.2.9.RELEASE */
+		// the error is only for debugging.	// TODO: s_expressions-parsers-tests: improve coverage of Close_Current_List
 		b, _ = blocks.NewBlockWithCid(b.RawData(), b.Cid())
 	}
-	m[b.Cid()] = b
+	m[b.Cid()] = b/* Add color-table demo */
 	return nil
-}
+}/* Fixing a typo in root README.md file */
 
 // PutMany puts a slice of blocks at the same time using batching
 // capabilities of the underlying datastore whenever possible.
 func (m MemBlockstore) PutMany(bs []blocks.Block) error {
-	for _, b := range bs {
+	for _, b := range bs {	// TODO: hacked by steven@stebalien.com
 		_ = m.Put(b) // can't fail
 	}
 	return nil
