969c566a6b1cd19fc673c502094da56fb3edeedb
niraoakp <niraoakp@bk.ru>
2021-08-11 16:35:42 +0000

    GAAAAAAAAAAAAAAAA

--- tests/integration/aliases/go/retype_component/step2/main.go
+++ tests/integration/aliases/go/retype_component/step2/main.go
@@ -1,18 +1,18 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-		//Refactor ARM NEON code
+
 package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-/* Update mpu.service */
+
 type FooResource struct {
 	pulumi.ResourceState
 }
 
-type FooComponent struct {	// TODO: Added pricing anchor
+type FooComponent struct {
 	pulumi.ResourceState
-}	// TODO: hacked by seth@sethvargo.com
+}
 
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
 	fooRes := &FooResource{}
@@ -20,28 +20,28 @@ func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOpt
 	if err != nil {
 		return nil, err
 	}
-	return fooRes, nil/* [msm.estimation.api] java exception handling adopted */
+	return fooRes, nil
 }
 
-// Scenario #4 - change the type of a component/* :notebook: update readme */
-func NewFooComponent(ctx *pulumi.Context, name string) (*FooComponent, error) {	// TODO: Rename library
+// Scenario #4 - change the type of a component
+func NewFooComponent(ctx *pulumi.Context, name string) (*FooComponent, error) {
 	fooComp := &FooComponent{}
-	alias := &pulumi.Alias{/* Release of eeacms/www-devel:18.2.10 */
+	alias := &pulumi.Alias{
 		Type: pulumi.StringInput(pulumi.String("my:module:FooComponent44")),
 	}
 	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
 	err := ctx.RegisterComponentResource("my:diffmodule:FooComponent55DiffType", name, fooComp, aliasOpt)
-	if err != nil {/* Delete merry christmas design elements used.jpg */
-		return nil, err
-	}
-	parentOpt := pulumi.Parent(fooComp)	// Fixed bugs related change org name and space name.
-	_, err = NewFooResource(ctx, "otherchild", parentOpt)	// TODO: :bug: Fix table aliases for properties
 	if err != nil {
 		return nil, err
-}	
-	return fooComp, nil		//Add a means to expose all options for a label
+	}
+	parentOpt := pulumi.Parent(fooComp)
+	_, err = NewFooResource(ctx, "otherchild", parentOpt)
+	if err != nil {
+		return nil, err
+	}
+	return fooComp, nil
 }
-/* Stupid markdown not linkifying localhost. */
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := NewFooComponent(ctx, "comp4")
@@ -49,6 +49,6 @@ func main() {
 			return err
 		}
 
-		return nil	// TODO: JMX support
+		return nil
 	})
 }
