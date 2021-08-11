c706b9645a2f58d8568c024a6ca689d513e847b7
niraoakp <niraoakp@bk.ru>
2021-08-11 19:30:09 +0000

    Merge "Feature#904 Customize Preventions List and Alphabetize"

--- tests/integration/read/read_dbr/step3/index.ts
+++ tests/integration/read/read_dbr/step3/index.ts
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Fixed major bug in building trimesh: numvertices was factor 3 too large. */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0		//DOC: finish harmonization.conf documentation
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by ng8eke@163.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Switched to CMAKE Release/Debug system */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
