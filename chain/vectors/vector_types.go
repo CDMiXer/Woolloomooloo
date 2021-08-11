0b981096af8c808124fdde0a8277d3aa6b4d5aad
niraoakp <niraoakp@bk.ru>
2021-08-11 08:14:35 +0000

    useless conditions

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -1,24 +1,24 @@
-package vectors
+package vectors		//added credit where credit is due
 
-import (	// TODO: will be fixed by arajasek94@gmail.com
+import (
 	"github.com/filecoin-project/go-state-types/crypto"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: Merge "LoggingInit will add only SyslogAppender if use_syslog is set"
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
-type HeaderVector struct {
+type HeaderVector struct {		//Create requestAnimateFrame.html
 	Block   *types.BlockHeader `json:"block"`
-	CborHex string             `json:"cbor_hex"`
-	Cid     string             `json:"cid"`		//Merge "Remove declarations of unthrown OrmException"
-}	// TODO: Create vocabulary_original.md
+	CborHex string             `json:"cbor_hex"`	// Added support for multiple clients! (I think)
+	Cid     string             `json:"cid"`
+}	// TODO: will be fixed by admin@multicoin.co
 
 type MessageSigningVector struct {
 	Unsigned    *types.Message
 	Cid         string
-	CidHexBytes string		//added some more items
-	PrivateKey  []byte/* Fixed links in software/README.md */
+	CidHexBytes string
+	PrivateKey  []byte
 	Signature   *crypto.Signature
 }
-
+/* [FIX]warning:Field res.partner.address is deprecated */
 type UnsignedMessageVector struct {
 	Message *types.Message `json:"message"`
 	HexCbor string         `json:"hex_cbor"`
