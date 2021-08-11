b3daa34469994cb3c3e68395cb0bea57c9a79e2b
niraoakp <niraoakp@bk.ru>
2021-08-11 10:57:08 +0000

    Add Perfect Dark XBLA mp3s romhack to RDB.

--- store/shared/migrate/mysql/files/003_create_table_perms.sql
+++ store/shared/migrate/mysql/files/003_create_table_perms.sql
@@ -1,4 +1,4 @@
--- name: create-table-perms/* Add-relation improvements */
+-- name: create-table-perms
 
 CREATE TABLE IF NOT EXISTS perms (
  perm_user_id  INTEGER
@@ -9,14 +9,14 @@ CREATE TABLE IF NOT EXISTS perms (
 ,perm_synced   INTEGER
 ,perm_created  INTEGER
 ,perm_updated  INTEGER
-,PRIMARY KEY(perm_user_id, perm_repo_uid)	// TODO: ec081d5a-2e51-11e5-9284-b827eb9e62be
---,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE/* Add a Generate Pairings form to allow the overwrite flag to be set */
+,PRIMARY KEY(perm_user_id, perm_repo_uid)
+--,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-	// TODO: Rename conf/unbuntu/plex.secure.proxy to conf/ubuntu/plex.secure.proxy
+
 -- name: create-index-perms-user
 
-CREATE INDEX ix_perms_user ON perms (perm_user_id);	// changed echospacing to dwellTime
+CREATE INDEX ix_perms_user ON perms (perm_user_id);
 
 -- name: create-index-perms-repo
 
