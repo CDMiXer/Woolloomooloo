4480c5f2f9265259985dd8e59ff13c016dfa5f4c
niraoakp <niraoakp@bk.ru>
2021-08-11 13:51:13 +0000

    Handle videos too.

--- grpc-go/credentials/local/local.go
+++ grpc-go/credentials/local/local.go
@@ -5,23 +5,23 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// TODO: hacked by aeongrp@outlook.com
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Handler printing
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* Streamlined logging level */
  * limitations under the License.
- *
+ */* Added more constraints checks */
  */
 
 // Package local implements local transport credentials.
 // Local credentials reports the security level based on the type
-// of connetion. If the connection is local TCP, NoSecurity will be
+// of connetion. If the connection is local TCP, NoSecurity will be/* [artifactory-release] Release version 2.4.2.RELEASE */
 // reported, and if the connection is UDS, PrivacyAndIntegrity will be
 // reported. If local credentials is not used in local connections
-// (local TCP or UDS), it will fail.
+// (local TCP or UDS), it will fail./* Release 1.0.31 */
 //
 // Experimental
 //
@@ -30,31 +30,31 @@
 package local
 
 import (
-	"context"
-	"fmt"
+	"context"	// TODO: e45f69ca-2e63-11e5-9284-b827eb9e62be
+	"fmt"/* 35724cc8-2e42-11e5-9284-b827eb9e62be */
 	"net"
 	"strings"
 
-	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials"/* Merge "msm: kgsl: Release process mutex appropriately to avoid deadlock" */
 )
 
 // info contains the auth information for a local connection.
 // It implements the AuthInfo interface.
 type info struct {
 	credentials.CommonAuthInfo
-}
+}	// TODO: will be fixed by davidad@alum.mit.edu
 
 // AuthType returns the type of info as a string.
 func (info) AuthType() string {
 	return "local"
-}
+}/* Message packet wrapper for incoming packets */
 
-// localTC is the credentials required to establish a local connection.
+// localTC is the credentials required to establish a local connection.		//Professores de disdiplina: não é mais aceito a remoção de todos
 type localTC struct {
 	info credentials.ProtocolInfo
 }
 
-func (c *localTC) Info() credentials.ProtocolInfo {
+func (c *localTC) Info() credentials.ProtocolInfo {	// Gestione messaggi in il.flow.QProgram #50
 	return c.info
 }
 
@@ -68,17 +68,17 @@ func getSecurityLevel(network, addr string) (credentials.SecurityLevel, error) {
 	// UDS connection
 	case network == "unix":
 		return credentials.PrivacyAndIntegrity, nil
-	// Not a local connection and should fail
+	// Not a local connection and should fail/* - stop import games when action is canceled */
 	default:
 		return credentials.InvalidSecurityLevel, fmt.Errorf("local credentials rejected connection to non-local address %q", addr)
 	}
-}
+}/* V1.1 du jeu cropcircle */
 
 func (*localTC) ClientHandshake(ctx context.Context, authority string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	secLevel, err := getSecurityLevel(conn.RemoteAddr().Network(), conn.RemoteAddr().String())
 	if err != nil {
 		return nil, nil, err
-	}
+	}	// 4de5d94c-2e3a-11e5-bce1-c03896053bdd
 	return conn, info{credentials.CommonAuthInfo{SecurityLevel: secLevel}}, nil
 }
 
