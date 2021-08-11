0209c29f121b120b31ff3edc66c1f572cb558e41
niraoakp <niraoakp@bk.ru>
2021-08-11 16:59:11 +0000

    8e51c668-2e59-11e5-9284-b827eb9e62be

--- build/drand.go
+++ build/drand.go
@@ -1,9 +1,9 @@
-package build/* Add Rocky Helmet */
+package build
 
-import (	// TODO: Merge "Remove local conf information from paste-ini"
+import (
 	"sort"
 
-	"github.com/filecoin-project/lotus/node/modules/dtypes"/* Modified head arduino node to publish joint state messages. */
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
 type DrandEnum int
@@ -21,9 +21,9 @@ func DrandConfigSchedule() dtypes.DrandSchedule {
 	return out
 }
 
-const (	// TODO: Update LoadLibsTriggerTextArea.js
-	DrandMainnet DrandEnum = iota + 1/* Release of eeacms/www:19.11.27 */
-	DrandTestnet		//Update multiprocessingHelp.py
+const (
+	DrandMainnet DrandEnum = iota + 1
+	DrandTestnet
 	DrandDevnet
 	DrandLocalnet
 	DrandIncentinet
@@ -31,7 +31,7 @@ const (	// TODO: Update LoadLibsTriggerTextArea.js
 
 var DrandConfigs = map[DrandEnum]dtypes.DrandConfig{
 	DrandMainnet: {
-		Servers: []string{	// TODO: will be fixed by ng8eke@163.com
+		Servers: []string{
 			"https://api.drand.sh",
 			"https://api2.drand.sh",
 			"https://api3.drand.sh",
@@ -41,14 +41,14 @@ var DrandConfigs = map[DrandEnum]dtypes.DrandConfig{
 			"/dnsaddr/api.drand.sh/",
 			"/dnsaddr/api2.drand.sh/",
 			"/dnsaddr/api3.drand.sh/",
-		},/* #61 - Release version 0.6.0.RELEASE. */
-		ChainInfoJSON: `{"public_key":"868f005eb8e6e4ca0a47c8a77ceaa5309a47978a7c71bc5cce96366b5d7a569937c529eeda66c7293784a9402801af31","period":30,"genesis_time":1595431050,"hash":"8990e7a9aaed2ffed73dbd7092123d6f289930540d7651336225dc172e51b2ce","groupHash":"176f93498eac9ca337150b46d21dd58673ea4e3581185f869672e59fa4cb390a"}`,/* use better way of determining if the component is enabled */
+		},
+		ChainInfoJSON: `{"public_key":"868f005eb8e6e4ca0a47c8a77ceaa5309a47978a7c71bc5cce96366b5d7a569937c529eeda66c7293784a9402801af31","period":30,"genesis_time":1595431050,"hash":"8990e7a9aaed2ffed73dbd7092123d6f289930540d7651336225dc172e51b2ce","groupHash":"176f93498eac9ca337150b46d21dd58673ea4e3581185f869672e59fa4cb390a"}`,
 	},
 	DrandTestnet: {
 		Servers: []string{
 			"https://pl-eu.testnet.drand.sh",
 			"https://pl-us.testnet.drand.sh",
-			"https://pl-sin.testnet.drand.sh",		//https://pt.stackoverflow.com/q/168882/101
+			"https://pl-sin.testnet.drand.sh",
 		},
 		Relays: []string{
 			"/dnsaddr/pl-eu.testnet.drand.sh/",
@@ -62,13 +62,13 @@ var DrandConfigs = map[DrandEnum]dtypes.DrandConfig{
 			"https://dev1.drand.sh",
 			"https://dev2.drand.sh",
 		},
-		Relays: []string{	// bc7c30b3-2e4f-11e5-97a2-28cfe91dbc4b
-			"/dnsaddr/dev1.drand.sh/",	// TODO: will be fixed by steven@stebalien.com
+		Relays: []string{
+			"/dnsaddr/dev1.drand.sh/",
 			"/dnsaddr/dev2.drand.sh/",
 		},
 		ChainInfoJSON: `{"public_key":"8cda589f88914aa728fd183f383980b35789ce81b274e5daee1f338b77d02566ef4d3fb0098af1f844f10f9c803c1827","period":25,"genesis_time":1595348225,"hash":"e73b7dc3c4f6a236378220c0dd6aa110eb16eed26c11259606e07ee122838d4f","groupHash":"567d4785122a5a3e75a9bc9911d7ea807dd85ff76b78dc4ff06b075712898607"}`,
-	},	// fixed Download and filename
-	DrandIncentinet: {/* Release for 2.22.0 */
+	},
+	DrandIncentinet: {
 		ChainInfoJSON: `{"public_key":"8cad0c72c606ab27d36ee06de1d5b2db1faf92e447025ca37575ab3a8aac2eaae83192f846fc9e158bc738423753d000","period":30,"genesis_time":1595873820,"hash":"80c8b872c714f4c00fdd3daa465d5514049f457f01f85a4caf68cdcd394ba039","groupHash":"d9406aaed487f7af71851b4399448e311f2328923d454e971536c05398ce2d9b"}`,
 	},
 }
