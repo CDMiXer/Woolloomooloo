b164c19026c24801a2f720758bb0e1f360e0e08e
niraoakp <niraoakp@bk.ru>
2021-08-11 09:15:14 +0000

    bdb32ae4-2e47-11e5-9284-b827eb9e62be

--- cli/client_test.go
+++ cli/client_test.go
@@ -5,17 +5,17 @@ import (
 	"os"
 	"testing"
 	"time"
+	// Fix single quote bug
+	clitest "github.com/filecoin-project/lotus/cli/test"/* split and vimdiff and fix bugs */
+)	// TODO: will be fixed by ligi@ligi.de
 
-	clitest "github.com/filecoin-project/lotus/cli/test"
-)
-
-// TestClient does a basic test to exercise the client CLI	// TODO: hacked by seth@sethvargo.com
+// TestClient does a basic test to exercise the client CLI
 // commands
-func TestClient(t *testing.T) {/* Merge branch 'master' into thomas_semoss_dev6 */
+func TestClient(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
-	clitest.QuietMiningLogs()/* Release 0.48 */
-/* Release notes for 0.4 */
-	blocktime := 5 * time.Millisecond/* Serialize query execution during hupped query */
+	clitest.QuietMiningLogs()
+
+	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
 	clitest.RunClientTest(t, Commands, clientNode)
