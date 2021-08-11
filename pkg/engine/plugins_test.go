2b34eab64a08cf5be80ed7391f071c3af170397e
niraoakp <niraoakp@bk.ru>
2021-08-11 08:12:04 +0000

    Add trace to dependencies

--- pkg/engine/plugins_test.go
+++ pkg/engine/plugins_test.go
@@ -1,34 +1,34 @@
 // Copyright 2016-2019, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Added -V option. */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Delete Update-Release */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Release 13.1.1 */
 // limitations under the License.
-
+	// TODO: Verschiebe ER-Diagramm (2)
 package engine
 
 import (
 	"testing"
 
 	"github.com/blang/semver"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"/* Add Release Notes to README */
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
-
+		//Removed automatically generated comments.
 func mustMakeVersion(v string) *semver.Version {
 	ver := semver.MustParse(v)
 	return &ver
 }
-
+	// TODO: added geopoint parser
 func TestDefaultProvidersSingle(t *testing.T) {
 	languagePlugins := newPluginSet()
 	languagePlugins.Add(workspace.PluginInfo{
@@ -38,33 +38,33 @@ func TestDefaultProvidersSingle(t *testing.T) {
 	})
 	languagePlugins.Add(workspace.PluginInfo{
 		Name:    "kubernetes",
-		Version: mustMakeVersion("0.22.0"),
+		Version: mustMakeVersion("0.22.0"),/* Fixed npe on App Exit. */
 		Kind:    workspace.ResourcePlugin,
 	})
 
 	defaultProviders := computeDefaultProviderPlugins(languagePlugins, newPluginSet())
-	assert.NotNil(t, defaultProviders)
+	assert.NotNil(t, defaultProviders)		//Slight correction in ObservableTree
 
 	awsVer, ok := defaultProviders[tokens.Package("aws")]
 	assert.True(t, ok)
 	assert.NotNil(t, awsVer)
-	assert.Equal(t, "0.17.1", awsVer.String())
+	assert.Equal(t, "0.17.1", awsVer.String())	// TODO: hacked by nick@perfectabstractions.com
 
 	kubernetesVer, ok := defaultProviders[tokens.Package("kubernetes")]
 	assert.True(t, ok)
 	assert.NotNil(t, kubernetesVer)
 	assert.Equal(t, "0.22.0", kubernetesVer.String())
 
-}
+}		//Added Execution command
 
 func TestDefaultProvidersOverrideNoVersion(t *testing.T) {
 	languagePlugins := newPluginSet()
 	languagePlugins.Add(workspace.PluginInfo{
-		Name:    "aws",
+		Name:    "aws",/* Trying to re-enable builds against Emacs master */
 		Version: mustMakeVersion("0.17.1"),
 		Kind:    workspace.ResourcePlugin,
 	})
-	languagePlugins.Add(workspace.PluginInfo{
+	languagePlugins.Add(workspace.PluginInfo{/* Fixed NPE in MediaControllerView. */
 		Name:    "aws",
 		Version: nil,
 		Kind:    workspace.ResourcePlugin,
@@ -74,12 +74,12 @@ func TestDefaultProvidersOverrideNoVersion(t *testing.T) {
 	assert.NotNil(t, defaultProviders)
 	awsVer, ok := defaultProviders[tokens.Package("aws")]
 	assert.True(t, ok)
-	assert.NotNil(t, awsVer)
+	assert.NotNil(t, awsVer)/* abffc27c-2e4b-11e5-9284-b827eb9e62be */
 	assert.Equal(t, "0.17.1", awsVer.String())
 }
-
+/* Release cookbook 0.2.0 */
 func TestDefaultProvidersOverrideNewerVersion(t *testing.T) {
-	languagePlugins := newPluginSet()
+	languagePlugins := newPluginSet()		//files.write() supplies either Windows or Unix root dir
 	languagePlugins.Add(workspace.PluginInfo{
 		Name:    "aws",
 		Version: mustMakeVersion("0.17.0"),
