b4a70a07a0b64471ef3b52694ddacc3780d6291d
niraoakp <niraoakp@bk.ru>
2021-08-11 18:20:44 +0000

    Rename variable to indicate what it is being used for

--- plugin/converter/legacy.go
+++ plugin/converter/legacy.go
@@ -1,31 +1,31 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//replaced sm::isBinaryEqual with macro
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: put antiderivative back
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
-
+	// FIX: Mask manager
+// +build !oss		//Flatten history
+/* JavaDoc for Just */
 package converter
-/* Removed OC account. */
+
 import (
-	"context"/* [Project] Fixing release artifact names */
+	"context"
 
-	"github.com/drone/drone/core"/* 7e73d53a-2e9d-11e5-b198-a45e60cdfd11 */
-)/* Merge "Updates ansible role requirements script name" into kilo */
+	"github.com/drone/drone/core"
+)/* 6a76c882-2e5a-11e5-9284-b827eb9e62be */
 
-// Legacy returns a conversion service that converts a/* Released DirectiveRecord v0.1.22 */
+// Legacy returns a conversion service that converts a
 // legacy 0.8 yaml file to a yaml file.
-func Legacy(enabled bool) core.ConvertService {	// TODO: will be fixed by indexxuan@gmail.com
+func Legacy(enabled bool) core.ConvertService {
 	return &legacyPlugin{
 		enabled: enabled,
-	}	// TODO: will be fixed by ng8eke@163.com
+	}	// TODO: hacked by praveen@minio.io
 }
 
 type legacyPlugin struct {
 	enabled bool
 }
 
-func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {/* extend from Shopware.data.Model */
-	if p.enabled == false {/* full width images */
+func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {/* Release version: 0.3.2 */
+	if p.enabled == false {
 		return nil, nil
 	}
 	return &core.Config{
