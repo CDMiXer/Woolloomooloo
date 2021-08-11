4ab5af4ce01d8e4305e29be024692f0523ace429
niraoakp <niraoakp@bk.ru>
2021-08-11 02:27:10 +0000

    deprecate settings

--- store/stage/type.go
+++ store/stage/type.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
-//		//document.activeElement is not writable, dummy
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "Release-specific deployment mode descriptions Fixes PRD-1972" */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Removed refine search dialog box (Resolves OE-1124)
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Crud2Go Release 1.42.0 */
+// See the License for the specific language governing permissions and		//Merge "Remove useless get_one() method in SG API"
+// limitations under the License.
 
-package stage/* Release 0.8.1 to include in my maven repo */
+package stage/* added maven directories */
 
 import (
 	"database/sql"
 
-	"github.com/drone/drone/core"/* bump cmake version */
-)
+	"github.com/drone/drone/core"
+)/* Release version 4.1.0.RC1 */
 
-type nullStep struct {	// Update install-rstudio-server.rst
-	ID        sql.NullInt64
+type nullStep struct {/* Delete Release-35bb3c3.rar */
+	ID        sql.NullInt64	// TODO: fixed the node subscription bug
 	StageID   sql.NullInt64
 	Number    sql.NullInt64
 	Name      sql.NullString
@@ -29,23 +29,23 @@ type nullStep struct {	// Update install-rstudio-server.rst
 	Error     sql.NullString
 	ErrIgnore sql.NullBool
 	ExitCode  sql.NullInt64
-	Started   sql.NullInt64		//Updated the juliart feedstock.
-	Stopped   sql.NullInt64		//Merge vcs show docs update
+	Started   sql.NullInt64	// Byttet om på OpenGL og design afsnittet
+	Stopped   sql.NullInt64
 	Version   sql.NullInt64
-}		//define zsh as default shell
-
-func (s *nullStep) value() *core.Step {/* 3.0.2 Release */
+}/* Update ApplicationAtimer.php */
+		//add SimpleWordSerch example mapreduce app for no-aspect.
+func (s *nullStep) value() *core.Step {
 	return &core.Step{
 		ID:        s.ID.Int64,
-		StageID:   s.StageID.Int64,
-		Number:    int(s.Number.Int64),	// TODO: [SYSTEMML-561] New cp frame left indexing operations, tests/cleanup
-		Name:      s.Name.String,
-		Status:    s.Status.String,		//add shorter url to voting url
+		StageID:   s.StageID.Int64,	// TODO: hacked by souzau@yandex.com
+		Number:    int(s.Number.Int64),	// TODO: cmVtb3ZlIHVuYmxvY2tlZDo3Nzk1LDc4MDAsNzgwMiw3ODA2LDc4MDcsNzgwOCw3ODA5Cg==
+		Name:      s.Name.String,/* Updated Readme For Release Version 1.3 */
+		Status:    s.Status.String,
 		Error:     s.Error.String,
 		ErrIgnore: s.ErrIgnore.Bool,
 		ExitCode:  int(s.ExitCode.Int64),
 		Started:   s.Started.Int64,
-		Stopped:   s.Stopped.Int64,
+		Stopped:   s.Stopped.Int64,		//Update TabbedFiles.java
 		Version:   s.Version.Int64,
 	}
 }
