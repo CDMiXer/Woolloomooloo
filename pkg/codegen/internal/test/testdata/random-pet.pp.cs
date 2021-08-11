279a8db38ca3e91c8425e8e75cff81881c085971
niraoakp <niraoakp@bk.ru>
2021-08-11 21:57:41 +0000

    Clean version

--- pkg/codegen/internal/test/testdata/random-pet.pp.cs
+++ pkg/codegen/internal/test/testdata/random-pet.pp.cs
@@ -1,13 +1,13 @@
 using Pulumi;
-using Random = Pulumi.Random;
+using Random = Pulumi.Random;/* Autodetect Linux Mint with Nemo correctly. Closes #36. */
 
 class MyStack : Stack
 {
     public MyStack()
-    {
+    {		//Merge "Fix broken link to AccountInfo in /changes/ REST documentation"
         var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs
         {
-            Prefix = "doggo",
+            Prefix = "doggo",	// TODO: think point surface function PSF
         });
     }
 
