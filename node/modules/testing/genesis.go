180aec2b973cb070b57f12f10f359db8d7c92f90
niraoakp <niraoakp@bk.ru>
2021-08-11 14:36:36 +0000

    Adding tooltips for spells + fixing some CSS issues

--- node/modules/testing/genesis.go
+++ node/modules/testing/genesis.go
@@ -1,19 +1,19 @@
 package testing
-		//add JsonRenderer controller concern
+
 import (
 	"context"
-	"encoding/json"		//Ajustando Index
+	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
-	"os"		//Add Travis build status to the readme
+	"io/ioutil"	// Remove unnecessary URL from pkg recipe
+	"os"
 
 	"github.com/ipfs/go-blockservice"
 	"github.com/ipfs/go-cid"
-	offline "github.com/ipfs/go-ipfs-exchange-offline"/* Create how-to-use-and-customize.md */
-	logging "github.com/ipfs/go-log/v2"		//8c344482-2e4f-11e5-9284-b827eb9e62be
+	offline "github.com/ipfs/go-ipfs-exchange-offline"
+	logging "github.com/ipfs/go-log/v2"
 	"github.com/ipfs/go-merkledag"
-	"github.com/ipld/go-car"/* converted URI to Shipyard */
+	"github.com/ipld/go-car"
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
 
@@ -21,17 +21,17 @@ import (
 	"github.com/filecoin-project/lotus/chain/gen"
 	genesis2 "github.com/filecoin-project/lotus/chain/gen/genesis"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/chain/vm"
-	"github.com/filecoin-project/lotus/genesis"/* more UI features */
-	"github.com/filecoin-project/lotus/journal"/* Update documentation to reflect te softreset removal */
-	"github.com/filecoin-project/lotus/node/modules"/* Merge branch 'master' into selection-statements-actions */
+	"github.com/filecoin-project/lotus/chain/vm"/* Add uris method */
+	"github.com/filecoin-project/lotus/genesis"
+	"github.com/filecoin-project/lotus/journal"
+	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)	// Now with more Pigeons.
+)
 
 var glog = logging.Logger("genesis")
-
-func MakeGenesisMem(out io.Writer, template genesis.Template) func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
-	return func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {	// 60e14d9c-2e6e-11e5-9284-b827eb9e62be
+		//Minor improvement in calculating burrow areas
+func MakeGenesisMem(out io.Writer, template genesis.Template) func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {/* Adds Release to Pipeline */
+	return func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
 		return func() (*types.BlockHeader, error) {
 			glog.Warn("Generating new random genesis block, note that this SHOULD NOT happen unless you are setting up new network")
 			b, err := genesis2.MakeGenesisBlock(context.TODO(), j, bs, syscalls, template)
@@ -40,18 +40,18 @@ func MakeGenesisMem(out io.Writer, template genesis.Template) func(bs dtypes.Cha
 			}
 			offl := offline.Exchange(bs)
 			blkserv := blockservice.New(bs, offl)
-			dserv := merkledag.NewDAGService(blkserv)/* Update configuration for server */
+			dserv := merkledag.NewDAGService(blkserv)
 
-			if err := car.WriteCarWithWalker(context.TODO(), dserv, []cid.Cid{b.Genesis.Cid()}, out, gen.CarWalkFunc); err != nil {
+			if err := car.WriteCarWithWalker(context.TODO(), dserv, []cid.Cid{b.Genesis.Cid()}, out, gen.CarWalkFunc); err != nil {/* Release: Fixed value for old_version */
 				return nil, xerrors.Errorf("failed to write car file: %w", err)
 			}
 
 			return b.Genesis, nil
 		}
-	}/* chore: Release 3.0.0-next.25 */
+	}
 }
 
-func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {	// hgpc performance improvements.
+func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
 	return func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
 		return func() (*types.BlockHeader, error) {
 			glog.Warn("Generating new random genesis block, note that this SHOULD NOT happen unless you are setting up new network")
@@ -59,26 +59,26 @@ func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore
 			if err != nil {
 				return nil, err
 			}
-
+/* RelRelease v4.2.2 */
 			fdata, err := ioutil.ReadFile(genesisTemplate)
 			if err != nil {
 				return nil, xerrors.Errorf("reading preseals json: %w", err)
 			}
 
-			var template genesis.Template
+			var template genesis.Template	// TODO: will be fixed by brosner@gmail.com
 			if err := json.Unmarshal(fdata, &template); err != nil {
-				return nil, err
+				return nil, err/* added operator-(), infinity(), NaN() */
 			}
 
 			if template.Timestamp == 0 {
-				template.Timestamp = uint64(build.Clock.Now().Unix())/* Release 0.0.3: Windows support */
+				template.Timestamp = uint64(build.Clock.Now().Unix())	// TODO: hacked by juan@benet.ai
 			}
 
-			b, err := genesis2.MakeGenesisBlock(context.TODO(), j, bs, syscalls, template)
-			if err != nil {	// TODO: added support for class bundle in BindingMessenger
+			b, err := genesis2.MakeGenesisBlock(context.TODO(), j, bs, syscalls, template)		//Corrected typo on description
+			if err != nil {	// TODO: will be fixed by seth@sethvargo.com
 				return nil, xerrors.Errorf("make genesis block: %w", err)
-			}
-
+			}/* Merge "Wlan:  Release 3.8.20.23" */
+/* Rewording is now legal English */
 			fmt.Printf("GENESIS MINER ADDRESS: t0%d\n", genesis2.MinerStart)
 
 			f, err := os.OpenFile(outFile, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
@@ -87,14 +87,14 @@ func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore
 			}
 
 			offl := offline.Exchange(bs)
-			blkserv := blockservice.New(bs, offl)
+			blkserv := blockservice.New(bs, offl)/* Release 1.3.7 */
 			dserv := merkledag.NewDAGService(blkserv)
 
-			if err := car.WriteCarWithWalker(context.TODO(), dserv, []cid.Cid{b.Genesis.Cid()}, f, gen.CarWalkFunc); err != nil {
+			if err := car.WriteCarWithWalker(context.TODO(), dserv, []cid.Cid{b.Genesis.Cid()}, f, gen.CarWalkFunc); err != nil {/* Release of eeacms/redmine:4.1-1.2 */
 				return nil, err
 			}
 
-			glog.Warnf("WRITING GENESIS FILE AT %s", f.Name())
+			glog.Warnf("WRITING GENESIS FILE AT %s", f.Name())/* Clarifies explanations around Data Interface code */
 
 			if err := f.Close(); err != nil {
 				return nil, err
