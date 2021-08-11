90d39e4617db6e0f9b9f52cd0371f97d4a1f3dc3
niraoakp <niraoakp@bk.ru>
2021-08-11 23:16:35 +0000

    Added testcase for stackoverflow question

--- tests/integration/aliases/nodejs/adopt_into_component/step1/index.ts
+++ tests/integration/aliases/nodejs/adopt_into_component/step1/index.ts
@@ -1,50 +1,50 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// TODO: hacked by hugomrdias@gmail.com
-	// TODO: Merge "DRY get_flavor in flavor manage tests"
-class Resource extends pulumi.ComponentResource {/* Build _ctypes and _ctypes_test in the ReleaseAMD64 configuration. */
+import * as pulumi from "@pulumi/pulumi";
+
+class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}/* Insert images in Email Templates with tinyMCE */
-/* Added the implementation for the rest of the List extension tests */
+}	// TODO: will be fixed by peterke@gmail.com
+
 // Scenario #2 - adopt a resource into a component
 class Component extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Release dev-14 */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component", name, {}, opts);
-    }		//Update TriggerOfT.cs
+    }		//New translations beatmappacks.php (Polish)
 }
 
 const res2 = new Resource("res2");
 const comp2 = new Component("comp2");
-/* ER: style et layout HP OK */
-// Scenario 3: adopt this resource into a new parent.
-class Component2 extends pulumi.ComponentResource {/* Prepared Development Release 1.4 */
+
+// Scenario 3: adopt this resource into a new parent.	// Fix typo: swallow -> shallow
+class Component2 extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component2", name, {}, opts);
-    }
-}		//Merge branch 'master' into checkout-and-build
+    }	// Update jsx-sort-props.md
+}
 new Component2("unparented");
 
 // Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
 // in the next step to be parented by this.  Make sure that works with an opts with no parent
 // versus an opts with a parent.
-
-class Component3 extends pulumi.ComponentResource {
+/* Formatting readme. */
+class Component3 extends pulumi.ComponentResource {		//8e51c668-2e59-11e5-9284-b827eb9e62be
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
         super("my:module:Component3", name, {}, opts);
         new Component2(name + "-child", opts);
     }
-}	// TODO: will be fixed by lexy8russo@outlook.com
-
+}
+/* Update AjaxComponents.html */
 new Component3("parentedbystack");
 new Component3("parentedbycomponent", { parent: comp2 });
 
 // Scenario 5: Allow multiple aliases to the same resource.
 class Component4 extends pulumi.ComponentResource {
-    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {		//bundle-size: 55716a81faaba53514cc4525691c5df9e5d4ad13 (85.34KB)
+    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {	// TODO: Merge "Cleanup for test_create_server_with_deleted_image"
         super("my:module:Component4", name, {});
     }
-}		//traces name resolution
+}
 
 new Component4("duplicateAliases", { parent: comp2 });
