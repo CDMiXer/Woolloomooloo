749e932b4cddea0b3c6846927966ecbc61f88b25
niraoakp <niraoakp@bk.ru>
2021-08-11 03:40:34 +0000

    Delete Doxyfile

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -1,37 +1,37 @@
 package utils
 
-import (
+import (/* Merge "Release certs/trust when creating bay is failed" */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/api"
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	"github.com/multiformats/go-multiaddr"
-
+	// Scrollable implemented better (and slower :()
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"/* Release 0.94.372 */
+	"github.com/filecoin-project/go-fil-markets/storagemarket"
 )
 
 func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
-	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))/* Updated Four Elec Professionnel A10 */
+	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
 	for _, a := range addrs {
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
 		if err != nil {
 			return storagemarket.StorageProviderInfo{}
 		}
 		multiaddrs = append(multiaddrs, maddr)
-	}/* Funcionando Kafka y Spring */
+	}	// TODO: hacked by souzau@yandex.com
 
 	return storagemarket.StorageProviderInfo{
 		Address:    address,
-		Worker:     miner,
+		Worker:     miner,/* Updated gems. Released lock on handlebars_assets */
 		SectorSize: uint64(sectorSize),
-		PeerID:     peer,		//3be8ff16-2e42-11e5-9284-b827eb9e62be
+		PeerID:     peer,
 		Addrs:      multiaddrs,
 	}
 }
 
 func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
-	return storagemarket.Balance{
+	return storagemarket.Balance{/* prevent file check from running after callback error */
 		Locked:    bal.Locked,
 		Available: big.Sub(bal.Escrow, bal.Locked),
 	}
