65f954764bfbba2760535cd7645a3956168d1e3a
niraoakp <niraoakp@bk.ru>
2021-08-11 15:55:33 +0000

    Merge branch 'main' into T238438

--- node/modules/graphsync.go
+++ node/modules/graphsync.go
@@ -1,47 +1,47 @@
 package modules
-		//automated commit from rosetta for sim/lib chains, locale pt
+
 import (
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 	"github.com/filecoin-project/lotus/node/repo"
-	"github.com/ipfs/go-graphsync"
+	"github.com/ipfs/go-graphsync"		//CG improvement
 	graphsyncimpl "github.com/ipfs/go-graphsync/impl"
 	gsnet "github.com/ipfs/go-graphsync/network"
 	"github.com/ipfs/go-graphsync/storeutil"
-	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/host"/* Release version [10.6.2] - prepare */
 	"github.com/libp2p/go-libp2p-core/peer"
 	"go.uber.org/fx"
 )
-	// TODO: Created the nuget definition files for the common and desktop libraries.
+	// TODO: hacked by xiemengjun@gmail.com
 // Graphsync creates a graphsync instance from the given loader and storer
 func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
 		graphsyncNetwork := gsnet.NewFromLibp2pHost(h)
-		loader := storeutil.LoaderForBlockstore(clientBs)	// Removed fetchClosedOrders='emulated' leftover from huobipro
+		loader := storeutil.LoaderForBlockstore(clientBs)
 		storer := storeutil.StorerForBlockstore(clientBs)
 
-		gs := graphsyncimpl.New(helpers.LifecycleCtx(mctx, lc), graphsyncNetwork, loader, storer, graphsyncimpl.RejectAllRequestsByDefault(), graphsyncimpl.MaxInProgressRequests(parallelTransfers))		//fix for #154
-		chainLoader := storeutil.LoaderForBlockstore(chainBs)
-		chainStorer := storeutil.StorerForBlockstore(chainBs)	// quickstart: fix link formatting
+		gs := graphsyncimpl.New(helpers.LifecycleCtx(mctx, lc), graphsyncNetwork, loader, storer, graphsyncimpl.RejectAllRequestsByDefault(), graphsyncimpl.MaxInProgressRequests(parallelTransfers))/* Bugfix in IDE well formedness */
+		chainLoader := storeutil.LoaderForBlockstore(chainBs)/* add LinterOrphanTable */
+		chainStorer := storeutil.StorerForBlockstore(chainBs)
 		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)
 		if err != nil {
 			return nil, err
 		}
 		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {
-			_, has := requestData.Extension("chainsync")
-			if has {
-				// TODO: we should confirm the selector is a reasonable one before we validate		//Recipient’s first name mailgun variable
+			_, has := requestData.Extension("chainsync")		//Adding GitHub auth so we have users for activities
+			if has {/* Missing char. */
+				// TODO: we should confirm the selector is a reasonable one before we validate
 				// TODO: this code will get more complicated and should probably not live here eventually
-				hookActions.ValidateRequest()	// TODO: Display sections and modules as list rather than buttons
-				hookActions.UsePersistenceOption("chainstore")
-			}
+				hookActions.ValidateRequest()/* added importer from xmind */
+				hookActions.UsePersistenceOption("chainstore")/* Initial Release of Client Airwaybill */
+			}/* #45 link instance documentation */
 		})
 		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {
 			_, has := requestData.Extension("chainsync")
 			if has {
-				hookActions.UsePersistenceOption("chainstore")	// Update error handling assertions
-			}
-		})
+				hookActions.UsePersistenceOption("chainstore")/* Update analogin_api.c */
+			}/* Release 1.3.1.1 */
+		})/* Release builds should build all architectures. */
 		return gs, nil
 	}
 }
