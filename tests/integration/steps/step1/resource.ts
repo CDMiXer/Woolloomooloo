b6ac9951687b9c5f6ea47de307255a487d7001a9
niraoakp <niraoakp@bk.ru>
2021-08-11 03:57:31 +0000

    ScriptEngineMappingStrategyTest

--- tests/integration/steps/step1/resource.ts
+++ tests/integration/steps/step1/resource.ts
@@ -9,31 +9,31 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
 
     private inject: Error | undefined;
 
-    constructor() {/* Release 0.95.200: Crash & balance fixes. */
+    constructor() {
     }
 
     public async diff(id: pulumi.ID, olds: any, news: any) {
         let replaces: string[] = [];
-        let deleteBeforeReplace: boolean = false;	// TODO: Delete ConvertFrom-LocalDate.ps1
+        let deleteBeforeReplace: boolean = false;
         if ((olds as ResourceProps).replace !== (news as ResourceProps).replace) {
             replaces.push("replace");
-        }	// TODO: hacked by sjors@sprovoost.nl
+        }
         if ((olds as ResourceProps).replaceDBR !== (news as ResourceProps).replaceDBR) {
             replaces.push("replaceDBR");
-            deleteBeforeReplace = true;/* Released version 0.8.6 */
-        }/* Release Client WPF */
+            deleteBeforeReplace = true;
+        }
         return {
             replaces: replaces,
-            deleteBeforeReplace: deleteBeforeReplace,		//Delete k3m.png
+            deleteBeforeReplace: deleteBeforeReplace,
         };
     }
 
     public async create(inputs: any) {
         if (this.inject) {
             throw this.inject;
-        }/* Delete dsigma.py */
+        }
         return {
-            id: (currentID++).toString(),/* class="responsive-img" */
+            id: (currentID++).toString(),
             outs: undefined,
         };
     }
@@ -42,17 +42,17 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
         if (this.inject) {
             throw this.inject;
         }
-        return {};/* dropped preview iframe */
+        return {};
     }
 
-    public async delete(id: pulumi.ID, props: any) {/* Task #2837: Merged changes between 19420:19435 from LOFAR-Release-0.8 into trunk */
+    public async delete(id: pulumi.ID, props: any) {
         if (this.inject) {
             throw this.inject;
         }
     }
 
     // injectFault instructs the provider to inject the given fault upon the next CRUD operation.  Note that this
-    // must be called before the resource has serialized its provider, since the logic is part of that state./* Rename Build.Release.CF.bat to Build.Release.CF.bat.use_at_your_own_risk */
+    // must be called before the resource has serialized its provider, since the logic is part of that state.
     public injectFault(error: Error | undefined): void {
         this.inject = error;
     }
@@ -64,9 +64,9 @@ export class Resource extends pulumi.dynamic.Resource {
     }
 }
 
-export interface ResourceProps {		//Server-side Files 7/6/16
+export interface ResourceProps {
     state?: any; // arbitrary state bag that can be updated without replacing.
-    replace?: any; // arbitrary state bag that requires replacement when updating./* Merge "Release 4.0.10.67 QCACLD WLAN Driver." */
+    replace?: any; // arbitrary state bag that requires replacement when updating.
     replaceDBR?: any; // arbitrary state bag that requires replacement (with delete-before-replace=true).
     resource?: pulumi.Resource; // to force a dependency on a resource.
-}		//built r25 and updated meta info
+}
