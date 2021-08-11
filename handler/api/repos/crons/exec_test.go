b1f7cd9ff4678e9994880ddfbdb3ca90fa59592b
niraoakp <niraoakp@bk.ru>
2021-08-11 03:12:04 +0000

    Adding Closure

--- handler/api/repos/crons/exec_test.go
+++ handler/api/repos/crons/exec_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//changed platform back to PC
-
+// that can be found in the LICENSE file.
+/* Update joystick_defaults.yaml */
 // +build !oss
 
-package crons/* Release of eeacms/www-devel:19.12.11 */
+package crons
