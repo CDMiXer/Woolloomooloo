8eb5f9ec5b30b69650ac0f7d48243af667e27f95
niraoakp <niraoakp@bk.ru>
2021-08-11 22:09:46 +0000

    Don't need these parens.

--- tests/integration/aliases/go/adopt_into_component/step1/main.go
+++ tests/integration/aliases/go/adopt_into_component/step1/main.go
@@ -3,33 +3,33 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"		//Updated section 1.7 to do a local clone
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-	// TODO: will be fixed by arajasek94@gmail.com
+
 // FooComponent is a component resource
 type FooResource struct {
 	pulumi.ResourceState
-}	// TODO: will be fixed by lexy8russo@outlook.com
+}
 
 type FooComponent struct {
 	pulumi.ResourceState
-}/* @Release [io7m-jcanephora-0.13.2] */
+}
 
 type FooComponent2 struct {
 	pulumi.ResourceState
 }
-	// proc/50-b_e: EXTREME AB-DYING, EXTREME.
+
 type FooComponent3 struct {
 	pulumi.ResourceState
 }
 
 type FooComponent4 struct {
 	pulumi.ResourceState
-}	// TODO: Merge "Remove some dead CSS"
+}
 
-func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {		//add command event handler for notification suppression
+func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
 	fooRes := &FooResource{}
-	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)/* Mercyful Release */
+	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -39,33 +39,33 @@ func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOpt
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
 	fooComp := &FooComponent{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent", name, fooComp, opts...)
-	if err != nil {	// TODO: Include Chef v11.10.4 in Travis
+	if err != nil {
 		return nil, err
 	}
 	return fooComp, nil
 }
-	// TODO: hacked by alan.shaw@protocol.ai
+
 func NewFooComponent2(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent2, error) {
-	fooComp := &FooComponent2{}/* [RELEASE] Release version 0.1.0 */
+	fooComp := &FooComponent2{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent2", name, fooComp, opts...)
 	if err != nil {
-		return nil, err		//Make changes based on Review
-	}/* Release v 1.75 with integrated text-search subsystem. */
+		return nil, err
+	}
 	return fooComp, nil
 }
 
-func NewFooComponent3(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent3, error) {/* Release a user's post lock when the user leaves a post. see #18515. */
+func NewFooComponent3(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent3, error) {
 	fooComp := &FooComponent3{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent3", name, fooComp, opts...)
 	if err != nil {
 		return nil, err
-	}	// TODO: Rename vestdijk to vestdijk.txt
+	}
 	_, err = NewFooComponent2(ctx, name+"-child", opts...)
 	if err != nil {
 		return nil, err
 	}
 	return fooComp, nil
-}/* Release version 1.0.3.RELEASE */
+}
 
 func NewFooComponent4(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent4, error) {
 	fooComp := &FooComponent4{}
