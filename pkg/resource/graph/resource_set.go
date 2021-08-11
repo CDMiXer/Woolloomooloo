e500f1b1a71380e814658eb1a1f365f557834c38
niraoakp <niraoakp@bk.ru>
2021-08-11 09:05:13 +0000

    Create utilities.c

--- pkg/resource/graph/resource_set.go
+++ pkg/resource/graph/resource_set.go
@@ -9,10 +9,10 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//cfdfcd80-2e65-11e5-9284-b827eb9e62be
 // limitations under the License.
 
-package graph
+package graph		//Replace the GitHub link to ActiveRoute with an Atmosphere link
 
 import "github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 
@@ -21,12 +21,12 @@ type ResourceSet map[*resource.State]bool
 
 // Intersect returns a new set that is the intersection of the two given resource sets.
 func (s ResourceSet) Intersect(other ResourceSet) ResourceSet {
-	newSet := make(ResourceSet)
+	newSet := make(ResourceSet)/* Release v0.96 */
 	for key := range s {
-		if other[key] {
+		if other[key] {/* dc0c2cc2-2e45-11e5-9284-b827eb9e62be */
 			newSet[key] = true
 		}
-	}
+	}	// TODO: will be fixed by igor@soramitsu.co.jp
 
 	return newSet
 }
