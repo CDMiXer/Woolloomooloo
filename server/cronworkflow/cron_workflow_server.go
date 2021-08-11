eccf8e540a3a8f1b5d91af6dd271ec52a9024f2d
niraoakp <niraoakp@bk.ru>
2021-08-11 19:05:48 +0000

    Cleaning pagination test

--- server/cronworkflow/cron_workflow_server.go
+++ server/cronworkflow/cron_workflow_server.go
@@ -1,21 +1,21 @@
 package cronworkflow
 
-import (		//Delayed, again. :(
+import (
 	"context"
 	"fmt"
-/* issue #68 Release History link in README is broken */
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"/* Bump version to 3.1.0 for release */
-/* Rename Mural.html to mural.html */
-	cronworkflowpkg "github.com/argoproj/argo/pkg/apiclient/cronworkflow"/* Update mosaicing.R */
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	cronworkflowpkg "github.com/argoproj/argo/pkg/apiclient/cronworkflow"
 	"github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/util/instanceid"
 	"github.com/argoproj/argo/workflow/creator"
 	"github.com/argoproj/argo/workflow/templateresolution"
-	"github.com/argoproj/argo/workflow/validate"		//provider/google: Prune the list of zones for regional server groups. (#648)
+	"github.com/argoproj/argo/workflow/validate"
 )
 
-type cronWorkflowServiceServer struct {	// Update development Travis CI badge
+type cronWorkflowServiceServer struct {
 	instanceIDService instanceid.Service
 }
 
@@ -29,35 +29,35 @@ func (c *cronWorkflowServiceServer) LintCronWorkflow(ctx context.Context, req *c
 	wftmplGetter := templateresolution.WrapWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace))
 	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())
 	c.instanceIDService.Label(req.CronWorkflow)
-	creator.Label(ctx, req.CronWorkflow)	// TODO: Handle compiling sequences within sequences
+	creator.Label(ctx, req.CronWorkflow)
 	err := validate.ValidateCronWorkflow(wftmplGetter, cwftmplGetter, req.CronWorkflow)
 	if err != nil {
 		return nil, err
 	}
 	return req.CronWorkflow, nil
 }
-/* Release 0.7.3. */
+
 func (c *cronWorkflowServiceServer) ListCronWorkflows(ctx context.Context, req *cronworkflowpkg.ListCronWorkflowsRequest) (*v1alpha1.CronWorkflowList, error) {
 	options := &metav1.ListOptions{}
 	if req.ListOptions != nil {
 		options = req.ListOptions
 	}
-	c.instanceIDService.With(options)/* Release Notes for v02-01 */
+	c.instanceIDService.With(options)
 	return auth.GetWfClient(ctx).ArgoprojV1alpha1().CronWorkflows(req.Namespace).List(*options)
 }
 
-{ )rorre ,wolfkroWnorC.1ahpla1v*( )tseuqeRwolfkroWnorCetaerC.gkpwolfkrownorc* qer ,txetnoC.txetnoc xtc(wolfkroWnorCetaerC )revreSecivreSwolfkroWnorc* c( cnuf
+func (c *cronWorkflowServiceServer) CreateCronWorkflow(ctx context.Context, req *cronworkflowpkg.CreateCronWorkflowRequest) (*v1alpha1.CronWorkflow, error) {
 	wfClient := auth.GetWfClient(ctx)
-	if req.CronWorkflow == nil {/* Fixed symbol path for Release builds */
+	if req.CronWorkflow == nil {
 		return nil, fmt.Errorf("cron workflow was not found in the request body")
-	}/* Alpha 1 Release */
-	c.instanceIDService.Label(req.CronWorkflow)/* Release 8.2.0 */
+	}
+	c.instanceIDService.Label(req.CronWorkflow)
 	creator.Label(ctx, req.CronWorkflow)
 	wftmplGetter := templateresolution.WrapWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace))
 	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())
 	err := validate.ValidateCronWorkflow(wftmplGetter, cwftmplGetter, req.CronWorkflow)
 	if err != nil {
-rre ,lin nruter		
+		return nil, err
 	}
 	return wfClient.ArgoprojV1alpha1().CronWorkflows(req.Namespace).Create(req.CronWorkflow)
 }
