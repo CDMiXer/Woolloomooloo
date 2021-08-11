1e2e5499abff045e98b01c60f3eb9f9dfeee1e26
niraoakp <niraoakp@bk.ru>
2021-08-11 02:41:11 +0000

    Merge "CEC: Make wakeup on hotplug optional" into mnc-dev

--- tests/integration/enums/python/__main__.py
+++ tests/integration/enums/python/__main__.py
@@ -1,23 +1,23 @@
-from pulumi import Input, Output, export		//Delete temperature.db
+from pulumi import Input, Output, export
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
-from enum import Enum/* Added new Release notes document */
+from enum import Enum
 from typing import Optional, Union
 
-/* Release 0.9.1 share feature added */
-class RubberTreeVariety(str, Enum):	// name anonymous functions to improve debugging
+
+class RubberTreeVariety(str, Enum):
     BURGUNDY = "Burgundy"
-    RUBY = "Ruby"	// TODO: Update K8s-controller.md
+    RUBY = "Ruby"
     TINEKE = "Tineke"
 
 
 class Farm(str, Enum):
     PLANTS_R_US = "Plants'R'Us"
     PULUMI_PLANTERS_INC = "Pulumi Planters Inc."
-/* Add ACM membership information */
-/* padding along the event for button presses */
+
+
 current_id = 0
 
-/* Application de la règle sonar des classes "final" */
+
 class PlantProvider(ResourceProvider):
     def create(self, inputs):
         global current_id
@@ -27,17 +27,17 @@ class PlantProvider(ResourceProvider):
 
 class Tree(Resource):
     type: Output[RubberTreeVariety]
-    farm: Optional[Output[str]]/* Update Making-A-Release.html */
+    farm: Optional[Output[str]]
 
-    def __init__(self, name: str, type: Input[RubberTreeVariety], farm: Optional[Input[str]]):/* Merge "Release 1.0.0.134 QCACLD WLAN Driver" */
+    def __init__(self, name: str, type: Input[RubberTreeVariety], farm: Optional[Input[str]]):
         self.type = type
-        self.farm = farm	// fixed input setselectionstart,fixed invalid coords handling
+        self.farm = farm
         super().__init__(PlantProvider(), name, {"type": type, "farm": farm})
-/* Remove bower bump */
-/* Re-centering the welcome text. */
-# Create a resource with input object.		//-fixing ECC calculation
+
+
+# Create a resource with input object.
 tree = Tree("myTree", type=RubberTreeVariety.BURGUNDY, farm=Farm.PULUMI_PLANTERS_INC)
 
 export("myTreeType", tree.type)
 export("myTreeFarmChanged", tree.farm.apply(lambda x: x + "foo"))
-export("mySentence", Output.all(tree.type, tree.farm).apply(lambda args: f"My {args[0]} Rubber tree is from {args[1]}"))/* Merge "grafana: Correct pipeline of midonet periodic jobs" */
+export("mySentence", Output.all(tree.type, tree.farm).apply(lambda args: f"My {args[0]} Rubber tree is from {args[1]}"))
