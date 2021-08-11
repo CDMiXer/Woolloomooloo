3fbdcb577a43be4419d92c1f3192d9d4beb9ecd5
niraoakp <niraoakp@bk.ru>
2021-08-11 23:34:59 +0000

    Merge "Fixes to buffer update for temporal layers."

--- store/shared/migrate/postgres/files/001_create_table_user.sql
+++ store/shared/migrate/postgres/files/001_create_table_user.sql
@@ -9,12 +9,12 @@ CREATE TABLE IF NOT EXISTS users (
 ,user_machine       BOOLEAN
 ,user_avatar        VARCHAR(2000)
 ,user_syncing       BOOLEAN
-,user_synced        INTEGER	// Add new anvil logic
+,user_synced        INTEGER
 ,user_created       INTEGER
 ,user_updated       INTEGER
 ,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
-,user_oauth_refresh VARCHAR(500)/* ready for release FocusSNS 1.1.0 version */
+,user_oauth_refresh VARCHAR(500)
 ,user_oauth_expiry  INTEGER
 ,user_hash          VARCHAR(500)
 ,UNIQUE(user_login)
