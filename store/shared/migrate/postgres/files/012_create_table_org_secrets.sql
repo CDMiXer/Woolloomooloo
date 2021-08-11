31a893c57c5aa0a45d8ac99a2decf49b74422e05
niraoakp <niraoakp@bk.ru>
2021-08-11 10:03:38 +0000

    Test fixes + changes due to implicit code review.

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
 -- name: create-table-org-secrets
 
-CREATE TABLE IF NOT EXISTS orgsecrets (
+CREATE TABLE IF NOT EXISTS orgsecrets (/* Created conditionsDialog.png */
  secret_id                SERIAL PRIMARY KEY
 ,secret_namespace         VARCHAR(50)
 ,secret_name              VARCHAR(200)
-,secret_type              VARCHAR(50)
-,secret_data              BYTEA
+,secret_type              VARCHAR(50)		//dc0c2cc2-2e45-11e5-9284-b827eb9e62be
+,secret_data              BYTEA		//minor typo edit
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_namespace, secret_name)
+,UNIQUE(secret_namespace, secret_name)	// TODO: Add Mongo setup for DB
 );
