bb2ebc1511f031daaa290587fc5a9a1a11c2b888
niraoakp <niraoakp@bk.ru>
2021-08-11 08:02:36 +0000

    start reducing memory use

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.cs
@@ -2,13 +2,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
-using Pulumi;
+using Pulumi;/* Release v17.0.0. */
 using Aws = Pulumi.Aws;
 
 class MyStack : Stack
 {
     public MyStack()
-    {
+{    
         // Create a bucket and expose a website index document
         var siteBucket = new Aws.S3.Bucket("siteBucket", new Aws.S3.BucketArgs
         {
@@ -16,7 +16,7 @@ class MyStack : Stack
             {
                 IndexDocument = "index.html",
             },
-        });
+        });/* committing these "compile" files eclipse created... nto sure what they do. */
         var siteDir = "www";
         // For each file in the directory, create an S3 object stored in `siteBucket`
         var files = new List<Aws.S3.BucketObject>();
@@ -27,39 +27,39 @@ class MyStack : Stack
                 Bucket = siteBucket.Id,
                 Key = range.Value,
                 Source = new FileAsset($"{siteDir}/{range.Value}"),
-                ContentType = "TODO: call mimeType",
+                ContentType = "TODO: call mimeType",/* Deleted msmeter2.0.1/Release/rc.write.1.tlog */
             }));
         }
         // set the MIME type of the file
         // Set the access policy for the bucket so all objects are readable
         var bucketPolicy = new Aws.S3.BucketPolicy("bucketPolicy", new Aws.S3.BucketPolicyArgs
-        {
+        {		//Ajust description of project
             Bucket = siteBucket.Id,
-            Policy = siteBucket.Id.Apply(id => JsonSerializer.Serialize(new Dictionary<string, object?>
+            Policy = siteBucket.Id.Apply(id => JsonSerializer.Serialize(new Dictionary<string, object?>	// TODO: will be fixed by boringland@protonmail.ch
             {
                 { "Version", "2012-10-17" },
                 { "Statement", new[]
                     {
-                        new Dictionary<string, object?>
+                        new Dictionary<string, object?>	// TODO: hacked by greg@colvin.org
                         {
                             { "Effect", "Allow" },
                             { "Principal", "*" },
                             { "Action", new[]
-                                {
+{                                
                                     "s3:GetObject",
                                 }
                              },
                             { "Resource", new[]
                                 {
-                                    $"arn:aws:s3:::{id}/*",
+                                    $"arn:aws:s3:::{id}/*",		//forgot to commit the lock file
                                 }
                              },
-                        },
+                        },/* [artifactory-release] Release version 1.7.0.RELEASE */
                     }
                  },
             })),
         });
-        this.BucketName = siteBucket.BucketName;
+        this.BucketName = siteBucket.BucketName;		//Flask-wtf 0.9 support
         this.WebsiteUrl = siteBucket.WebsiteEndpoint;
     }
 
