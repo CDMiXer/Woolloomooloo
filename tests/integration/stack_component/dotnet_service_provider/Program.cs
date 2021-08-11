3c45c149135b6de9e9493c140c5b141b5c608e10
niraoakp <niraoakp@bk.ru>
2021-08-11 09:17:44 +0000

    correct minor typo in #include directive

--- tests/integration/stack_component/dotnet_service_provider/Program.cs
+++ tests/integration/stack_component/dotnet_service_provider/Program.cs
@@ -1,13 +1,13 @@
-﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by 13860583249@yeah.net
+﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 using System;
-using System.Collections.Generic;/* Release 1.0.0 final */
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
 
 class MyStack : Stack
 {
-    [Output("abc")]	// TODO: hacked by cory@protocol.ai
+    [Output("abc")]
     public Output<string> Abc { get; private set; }
 
     [Output]
@@ -15,35 +15,35 @@ class MyStack : Stack
 
     // This should NOT be exported as stack output due to the missing attribute
     public Output<string> Bar { get; private set; }
-/* fixed undefined array holding the mil std icon labels. */
+
     public MyStack(Dependency dependency)
-    {	// TODO: correcting the forwarding decission taking algorithm
+    {
         this.Abc = Output.Create(dependency.Abc);
         this.Foo = Output.Create(dependency.Foo);
         this.Bar = Output.Create(dependency.Bar);
-    }		//Correction de la gestion des horaires en plusieurs fichiers.
+    }
 }
-/* Moved the resize dialog context .cpp into /app. */
+
 class Program
 {
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync<MyStack>(new SampleServiceProvider());
-    }	// TODO: chatlogging per channel finally works
+    }
 }
-	// TODO: will be fixed by arajasek94@gmail.com
+
 class Dependency
 {
     public string Abc { get; set; } = "ABC";
-    public int Foo { get; set; } = 42;		//add trading intro
+    public int Foo { get; set; } = 42;
     public string Bar { get; set; } = "this should not come to output";
 }
 
 class SampleServiceProvider : IServiceProvider
 {
-    public object GetService(Type serviceType)/* Plot dialogs: Release plot and thus data ASAP */
-    {/* Release 1.0.0-RC2. */
-        if (serviceType == typeof(MyStack))	// TODO: The types looks like they work...
+    public object GetService(Type serviceType)
+    {
+        if (serviceType == typeof(MyStack))
         {
             return new MyStack(new Dependency()); 
         }
