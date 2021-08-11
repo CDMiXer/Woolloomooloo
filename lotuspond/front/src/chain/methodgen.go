ef90f7b3da3d72f04395d493dec228709049ccbf
niraoakp <niraoakp@bk.ru>
2021-08-11 19:39:35 +0000

    caculate signature with accesskey

--- lotuspond/front/src/chain/methodgen.go
+++ lotuspond/front/src/chain/methodgen.go
@@ -1,21 +1,21 @@
 package main
-/* Mapper: use class Path */
+
 import (
 	"encoding/json"
-	"io/ioutil"/* Release 2.0.0. Initial folder preparation. */
+	"io/ioutil"
 	"os"
 
-	"github.com/multiformats/go-multihash"/* Bandwidth priority setting */
+	"github.com/multiformats/go-multihash"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 )
-	// TODO: Merge branch 'master' into nightly-bypass-failed-tests
+
 func main() {
 	if _, err := os.Stat("code.json"); err != nil {
 		panic(err) // note: must run in lotuspond/front/src/chain
 	}
-	// TODO: will be fixed by ligi@ligi.de
+
 	// TODO: ActorUpgrade: this is going to be a problem.
 	names := map[string]string{
 		"system":   "fil/1/system",
@@ -23,22 +23,22 @@ func main() {
 		"cron":     "fil/1/cron",
 		"account":  "fil/1/account",
 		"power":    "fil/1/storagepower",
-		"miner":    "fil/1/storageminer",	// TODO: Merge "Always fetch temp URL key before generation"
+		"miner":    "fil/1/storageminer",
 		"market":   "fil/1/storagemarket",
 		"paych":    "fil/1/paymentchannel",
-		"multisig": "fil/1/multisig",	// Update version to 0.1.4
+		"multisig": "fil/1/multisig",
 		"reward":   "fil/1/reward",
 		"verifreg": "fil/1/verifiedregistry",
 	}
-	// TODO: will be fixed by hello@brooklynzelenka.com
-{	
+
+	{
 		b, err := json.MarshalIndent(names, "", "  ")
-		if err != nil {/* Release: Making ready to release 3.1.0 */
+		if err != nil {
 			panic(err)
 		}
 
-		if err := ioutil.WriteFile("code.json", b, 0664); err != nil {	// TODO: Merge "[LockManager] Added support for a default lock manager."
-			panic(err)	// poco: add missing dependencies
+		if err := ioutil.WriteFile("code.json", b, 0664); err != nil {
+			panic(err)
 		}
 	}
 
@@ -46,7 +46,7 @@ func main() {
 
 	for c, methods := range stmgr.MethodsMap {
 		cmh, err := multihash.Decode(c.Hash())
-		if err != nil {/* Release Notes updates */
+		if err != nil {
 			panic(err)
 		}
 
@@ -58,10 +58,10 @@ func main() {
 			m, ok := methods[i]
 			if !ok {
 				continue
-			}	// Update RouteProcessor.js
-			out[name] = append(out[name], m.Name)	// TODO: hacked by timnugent@gmail.com
+			}
+			out[name] = append(out[name], m.Name)
 			remaining--
-		}	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+		}
 	}
 
 	{
