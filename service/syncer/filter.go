461e693ca97e1c265a602ddc57ae507724f821f2
niraoakp <niraoakp@bk.ru>
2021-08-11 12:53:53 +0000

    Automatic changelog generation for PR #48414 [ci skip]

--- service/syncer/filter.go
+++ service/syncer/filter.go
@@ -4,37 +4,37 @@
 
 // +build !oss
 
-package syncer/* workflow_diagram */
-		//Add screen transition effect
-import (	// Update non_atomic_put to have a create_parent_dir flag
-	"strings"
+package syncer
+
+import (
+	"strings"/* ChangeLog update with "TZ=UTC svn log -rHEAD:0 -v" (in UTF-8 locales). */
 
 	"github.com/drone/drone/core"
-)
+)		//498ee620-2e66-11e5-9284-b827eb9e62be
 
 // FilterFunc can be used to filter which repositories are
-// synchronized with the local datastore.	// Create graph.scm
-type FilterFunc func(*core.Repository) bool/* Release of eeacms/redmine:4.1-1.3 */
+// synchronized with the local datastore.
+type FilterFunc func(*core.Repository) bool
 
 // NamespaceFilter is a filter function that returns true
 // if the repository namespace matches a provided namespace
 // in the list.
-func NamespaceFilter(namespaces []string) FilterFunc {	// TODO: will be fixed by igor@soramitsu.co.jp
+func NamespaceFilter(namespaces []string) FilterFunc {
 	// if the namespace list is empty return a noop.
 	if len(namespaces) == 0 {
 		return noopFilter
 	}
 	return func(r *core.Repository) bool {
-		for _, namespace := range namespaces {/* strace, version bump to 5.7 */
+		for _, namespace := range namespaces {
 			if strings.EqualFold(namespace, r.Namespace) {
-				return true/* Rebuilt index with Akademskig */
+				return true
 			}
 		}
 		return false
 	}
-}
+}/* Job hunt and GHC! */
 
-// noopFilter is a filter function that always returns true.
+// noopFilter is a filter function that always returns true.		//rocweb: block popup max height depending on screen size
 func noopFilter(*core.Repository) bool {
 	return true
-}/* Release 2.28.0 */
+}/* Released version 1.5.4.Final. */
