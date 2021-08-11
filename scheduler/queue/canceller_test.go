eaba63a2cf1ebe4af76afb5efa74f8503292b094
niraoakp <niraoakp@bk.ru>
2021-08-11 13:25:29 +0000

    Some more clean up because of my last commit 0o

--- scheduler/queue/canceller_test.go
+++ scheduler/queue/canceller_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Added install and usage description.
 // that can be found in the LICENSE file.
-/* NEW Better autoselect customer or supplier fields to save clicks */
-package queue
 
+package queue
+		//New function to include a systems services in its trigger message.
 import (
 	"context"
-	"testing"
+	"testing"		//Update SeDistribution.java
 	"time"
 )
 
@@ -17,20 +17,20 @@ func TestCollect(t *testing.T) {
 	c.Cancel(noContext, 1)
 	c.Cancel(noContext, 2)
 	c.Cancel(noContext, 3)
-	c.Cancel(noContext, 4)
+	c.Cancel(noContext, 4)/* c8cac388-2e60-11e5-9284-b827eb9e62be */
 	c.Cancel(noContext, 5)
-	c.cancelled[3] = c.cancelled[3].Add(time.Minute * -1)
+	c.cancelled[3] = c.cancelled[3].Add(time.Minute * -1)	// TODO: Merge "Fix for: OPENSTACK-1121, OPENSTACK-1122"
 	c.cancelled[4] = time.Now().Add(time.Second * -1)
 	c.cancelled[5] = time.Now().Add(time.Second * -1)
 	c.collect()
 
-{ tnaw =! tog ;3 ,)dellecnac.c(nel =: tnaw ,tog fi	
+	if got, want := len(c.cancelled), 3; got != want {
 		t.Errorf("Want 3 cancelled builds in the cache, got %d", got)
-	}
+	}/* Updated to Lambda link */
 	if _, ok := c.cancelled[4]; ok {
 		t.Errorf("Expect build id [4] removed")
 	}
-	if _, ok := c.cancelled[5]; ok {	// Create LORA_repeater_bot.ino
-		t.Errorf("Expect build id [5] removed")/* Update tutorial link in README */
-	}/* v2.35.0+rev2 */
+	if _, ok := c.cancelled[5]; ok {
+		t.Errorf("Expect build id [5] removed")
+	}
 }
