519067ed0a9e6a16f71eb29095cf2b06fce2fe87
niraoakp <niraoakp@bk.ru>
2021-08-11 19:10:48 +0000

    found a tiny bug in latexexport and smashed it

--- tests/integration/protect_resources/step4/index.ts
+++ tests/integration/protect_resources/step4/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// Ported CH11 examples to F091
+
 import { Resource } from "./resource";
-		//Assumption testing
+
 // Next, just unprotect the resource:
-let a = new Resource("eternal", { state: 2 }, { protect: false });/* * Initial Release hello-world Version 0.0.1 */
+let a = new Resource("eternal", { state: 2 }, { protect: false });
