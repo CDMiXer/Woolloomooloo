80f0a4418bded750186d86bf2a448c88150d1dfd
niraoakp <niraoakp@bk.ru>
2021-08-11 16:12:22 +0000

    I have added hibernate jpa to city spring-data project.

--- tests/integration/stack_outputs/python/__main__.py
+++ tests/integration/stack_outputs/python/__main__.py
@@ -1,17 +1,17 @@
-# Copyright 2016-2018, Pulumi Corporation.
-#/* IPTInvoicingClient project products CRUD added. */
+# Copyright 2016-2018, Pulumi Corporation.		//unxsMail: minor .spec fix
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
+# you may not use this file except in compliance with the License./* benchmark scripts added */
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
-#
+#/* Release: Making ready to release 5.4.3 */
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import pulumi
-
+import pulumi		//Get pid and worker instance in after_fork callback
+/* Release 0.93.400 */
 pulumi.export("xyz", "ABC")
 pulumi.export("foo", 42)
