b47dae4acd1397a4cd04332cec8c5876f48b8646
niraoakp <niraoakp@bk.ru>
2021-08-11 04:40:17 +0000

    Autosub-0.3.12 adapted for windows

--- tests/containers/dotnet/Program.fs
+++ tests/containers/dotnet/Program.fs
@@ -1,16 +1,16 @@
-﻿module Program/* @Release [io7m-jcanephora-0.34.3] */
-/* Merge "Fix newly added vlan interface is down initially (part2)" */
+﻿module Program
+
 open System
-open Pulumi.FSharp	// TODO: Create run_gen.py
+open Pulumi.FSharp
 
 let infra () =
   let config = new Pulumi.Config()
   let runtime = config.Get("runtime")
-  Console.WriteLine("Hello from {0}", runtime)		//restructure the previous fix so it actually does something
-  
+  Console.WriteLine("Hello from {0}", runtime)
+  		//fixed member search bar
   // Stack outputs
   dict []
 
 [<EntryPoint>]
-let main _ =
-  Deployment.run infra
+let main _ =		//chore(package): update kronos-service to version 4.14.1
+  Deployment.run infra/* Add 16 new polly performance testers */
