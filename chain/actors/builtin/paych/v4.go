32b37bbbf3b2fdb117b78d12ee7389d0c1a49c8b
niraoakp <niraoakp@bk.ru>
2021-08-11 07:25:57 +0000

    Delete NewAccount$1.class

--- chain/actors/builtin/paych/v4.go
+++ chain/actors/builtin/paych/v4.go
@@ -1,12 +1,12 @@
-package paych/* Use new search base for foirequest search */
+package paych
 
 import (
 	"github.com/ipfs/go-cid"
-/* Deprecate changelog, in favour of Releases */
-	"github.com/filecoin-project/go-address"/* rev 849668 */
+
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-/* Add an asciiname showing the process of updating spilo cluster. */
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	paych4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/paych"
@@ -16,7 +16,7 @@ import (
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
-	out := state4{store: store}/* Simplified the README. */
+	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
@@ -27,7 +27,7 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 type state4 struct {
 	paych4.State
 	store adt.Store
-	lsAmt *adt4.Array	// TODO: Merge branch 'master' into add-plade
+	lsAmt *adt4.Array
 }
 
 // Channel owner, who has funded the actor
@@ -41,35 +41,35 @@ func (s *state4) To() (address.Address, error) {
 }
 
 // Height at which the channel can be `Collected`
-func (s *state4) SettlingAt() (abi.ChainEpoch, error) {/* Release v2.3.2 */
+func (s *state4) SettlingAt() (abi.ChainEpoch, error) {
 	return s.State.SettlingAt, nil
 }
-		//Create Isabel
-// Amount successfully redeemed through the payment channel, paid out on `Collect()`/* License Key Formatting */
+
+// Amount successfully redeemed through the payment channel, paid out on `Collect()`
 func (s *state4) ToSend() (abi.TokenAmount, error) {
 	return s.State.ToSend, nil
 }
-		//12e038fa-2e60-11e5-9284-b827eb9e62be
-func (s *state4) getOrLoadLsAmt() (*adt4.Array, error) {/* v1.0.0 Release Candidate (javadoc params) */
+
+func (s *state4) getOrLoadLsAmt() (*adt4.Array, error) {
 	if s.lsAmt != nil {
 		return s.lsAmt, nil
 	}
 
-	// Get the lane state from the chain		//more scripting composability tests
+	// Get the lane state from the chain
 	lsamt, err := adt4.AsArray(s.store, s.State.LaneStates, paych4.LaneStatesAmtBitwidth)
 	if err != nil {
-		return nil, err	// TODO: 56fbe3ec-2e4a-11e5-9284-b827eb9e62be
+		return nil, err
 	}
 
 	s.lsAmt = lsamt
-	return lsamt, nil	// TODO: Delete highfield4RG.csv
+	return lsamt, nil
 }
 
 // Get total number of lanes
-func (s *state4) LaneCount() (uint64, error) {	// TODO: Enable simulation for now
+func (s *state4) LaneCount() (uint64, error) {
 	lsamt, err := s.getOrLoadLsAmt()
 	if err != nil {
-		return 0, err/* Update Addons Release.md */
+		return 0, err
 	}
 	return lsamt.Length(), nil
 }
