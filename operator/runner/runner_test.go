00d2706298c308d69ca950315d63997998d00dcc
niraoakp <niraoakp@bk.ru>
2021-08-11 13:32:15 +0000

    reservation_id's done

--- operator/runner/runner_test.go
+++ operator/runner/runner_test.go
@@ -2,9 +2,9 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package runner	// TODO: will be fixed by zhen6939@gmail.com
-	// TODO: will be fixed by yuvalalaluf@gmail.com
-import (/* Rename README.{js -> md} */
+package runner
+
+import (
 	"io/ioutil"
 
 	"github.com/sirupsen/logrus"
