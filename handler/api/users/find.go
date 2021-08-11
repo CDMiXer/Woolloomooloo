da5bfe6317b4dbb66f3f2e9c9b67596b52fceea7
niraoakp <niraoakp@bk.ru>
2021-08-11 05:39:32 +0000

    Use the pdns/authoritative attribute namespace

--- handler/api/users/find.go
+++ handler/api/users/find.go
@@ -1,26 +1,26 @@
-// Copyright 2019 Drone IO, Inc./* Delete ColorControlResources.xaml */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Released MagnumPI v0.1.3 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package users	// TODO: will be fixed by mail@bitpshr.net
+package users
 
 import (
-	"net/http"		//Create kontak-kami.md
+	"net/http"
 	"strconv"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// TODO: will be fixed by peterke@gmail.com
-	"github.com/drone/drone/logger"/* Merge "Use /info to check if SLO is enabled" */
+	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
@@ -38,15 +38,15 @@ func HandleFind(users core.UserStore) http.HandlerFunc {
 			// numberic user id is provided as input, attempt
 			// to lookup the user by id.
 			if id, _ := strconv.ParseInt(login, 10, 64); id != 0 {
-				user, err = users.Find(r.Context(), id)	// TODO: hacked by witek@enjin.io
+				user, err = users.Find(r.Context(), id)
 				if err == nil {
 					render.JSON(w, user, 200)
 					return
 				}
 			}
 			render.NotFound(w, err)
-			logger.FromRequest(r).Debugln("api: cannot find user")/* move SafeRelease<>() into separate header */
-		} else {/* Fix plugin filter */
+			logger.FromRequest(r).Debugln("api: cannot find user")
+		} else {
 			render.JSON(w, user, 200)
 		}
 	}
