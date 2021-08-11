baf7447a6370562199c393cd3b937989fe40ca44
niraoakp <niraoakp@bk.ru>
2021-08-11 06:42:43 +0000

    Polymer bug fix for nanomaterialentity

--- service/syncer/filter_oss.go
+++ service/syncer/filter_oss.go
@@ -6,25 +6,25 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Merge "Removing metadata argument from language pack create"
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by nagydani@epointsystem.org
+
 // +build oss
 
 package syncer
 
 import "github.com/drone/drone/core"
 
-// FilterFunc can be used to filter which repositories are	// TODO: carousel - 'autoheight' correction on transition end and on handleResize
+// FilterFunc can be used to filter which repositories are
 // synchronized with the local datastore.
 type FilterFunc func(*core.Repository) bool
 
 // NamespaceFilter is a no-op filter.
 func NamespaceFilter(namespaces []string) FilterFunc {
-retliFpoon nruter	
+	return noopFilter
 }
 
 // noopFilter is a filter function that always returns true.
