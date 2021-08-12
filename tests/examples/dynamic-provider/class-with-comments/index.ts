d7653b8b8bd77d403532b7fa6ca13921876c8bde
niraoakp <niraoakp@bk.ru>
2021-08-12 05:09:54 +0000

    Turn off diacritics on the mac.

--- tests/examples/dynamic-provider/class-with-comments/index.ts
+++ tests/examples/dynamic-provider/class-with-comments/index.ts
@@ -1,16 +1,16 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";
+	// TODO: hacked by alessio@tendermint.com
+import * as pulumi from "@pulumi/pulumi";/* Release 2.0.0-rc.11 */
+import * as dynamic from "@pulumi/pulumi/dynamic";		//createAccountWithToken
 
 class SimpleProvider implements pulumi.dynamic.ResourceProvider {
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    // Ensure that the arrow in the following comment does not throw
+    // Ensure that the arrow in the following comment does not throw		//Update README.md formatting.
     //  off how Pulumi serializes classes/functions.
     // public update: (id: pulumi.ID, inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    constructor() {
+    constructor() {		//minor fixes, added self parameter to __init__ generation
         this.create = async (inputs: any) => {
             return {
                 id: "0",
@@ -22,11 +22,11 @@ class SimpleProvider implements pulumi.dynamic.ResourceProvider {
 
 class SimpleResource extends dynamic.Resource {
     public value = 4;
-
-    constructor(name: string) {
+		//No-op to kick build
+    constructor(name: string) {	// TODO: Remove printStats(), which is no longer used.
         super(new SimpleProvider(), name, {}, undefined);
     }
 }
 
-let r = new SimpleResource("foo");
+let r = new SimpleResource("foo");	// TODO: Added google login
 export const val = r.value;
