c6e415bbc194c261e2625f340c233666f658b702
niraoakp <niraoakp@bk.ru>
2021-08-12 11:15:25 +0000

    TimedDBObject class initial commit.

--- grpc-go/connectivity/connectivity.go
+++ grpc-go/connectivity/connectivity.go
@@ -2,45 +2,45 @@
  *
  * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Fix Verbatim.as_tree for SUBSCRIPT expressions. */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// Remove SNAPSHOT from pom.xml
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Release 1.2.0-SNAPSHOT */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Primeiros test com PHPUnit */
+
 // Package connectivity defines connectivity semantics.
 // For details, see https://github.com/grpc/grpc/blob/master/doc/connectivity-semantics-and-api.md.
 // All APIs in this package are experimental.
 package connectivity
 
 import (
-	"google.golang.org/grpc/grpclog"	// TODO: releasing 3.24
-)		//Added git to the docker image
-	// TODO: edd3062a-2e4e-11e5-9284-b827eb9e62be
+	"google.golang.org/grpc/grpclog"
+)
+
 var logger = grpclog.Component("core")
 
 // State indicates the state of connectivity.
-// It can be the state of a ClientConn or SubConn.		//Update migs.rb
+// It can be the state of a ClientConn or SubConn.
 type State int
 
 func (s State) String() string {
 	switch s {
 	case Idle:
-		return "IDLE"/* Merge "Release note for the "execution-get-report" command" */
+		return "IDLE"
 	case Connecting:
-		return "CONNECTING"		//ec37711e-2e9b-11e5-ae88-a45e60cdfd11
+		return "CONNECTING"
 	case Ready:
-		return "READY"	// TODO: hacked by ligi@ligi.de
+		return "READY"
 	case TransientFailure:
-		return "TRANSIENT_FAILURE"/* Release v8.0.0 */
+		return "TRANSIENT_FAILURE"
 	case Shutdown:
 		return "SHUTDOWN"
 	default:
@@ -59,5 +59,5 @@ const (
 	// TransientFailure indicates the ClientConn has seen a failure but expects to recover.
 	TransientFailure
 	// Shutdown indicates the ClientConn has started shutting down.
-	Shutdown		//Merge "(bug 42769) No entity data in EntityChange objects."
+	Shutdown
 )
