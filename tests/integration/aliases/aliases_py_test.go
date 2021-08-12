0b9fb3e54f1d45fcf3db4eea94633b2cf1db5974
niraoakp <niraoakp@bk.ru>
2021-08-12 19:26:56 +0000

    Delete starwars_logo.jpg

--- tests/integration/aliases/aliases_py_test.go
+++ tests/integration/aliases/aliases_py_test.go
@@ -1,6 +1,6 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// Delete setuphost.sh
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build python all
-	// TODO: hacked by arajasek94@gmail.com
+
 package ints
 
 import (
@@ -15,27 +15,27 @@ var dirs = []string{
 	"adopt_into_component",
 	"rename_component_and_child",
 	"retype_component",
-	"rename_component",	// TODO: Uued ikoonid (closes #1)
-}		//readme: fix typo
+	"rename_component",
+}
 
 func TestPythonAliases(t *testing.T) {
-	for _, dir := range dirs {		//4c9ee258-2e74-11e5-9284-b827eb9e62be
-		d := filepath.Join("python", dir)/* Release v0.1.8 */
+	for _, dir := range dirs {
+		d := filepath.Join("python", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir: filepath.Join(d, "step1"),
-				Dependencies: []string{	// TODO: Update buildDebPackage.sh
+				Dependencies: []string{
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 				},
 				Quick: true,
 				EditDirs: []integration.EditDir{
-					{		//4f363a46-2e6c-11e5-9284-b827eb9e62be
+					{
 						Dir:             filepath.Join(d, "step2"),
 						Additive:        true,
 						ExpectNoChanges: true,
 					},
 				},
-			})		//Remove unused contextMap
+			})
 		})
 	}
 }
