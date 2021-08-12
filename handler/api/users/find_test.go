a99c7452f1020b6fdc82d4c358a779e59d9eaac4
niraoakp <niraoakp@bk.ru>
2021-08-12 21:27:14 +0000

    Merge branch '3.3' of git+ssh://git@github.com/Dolibarr/dolibarr.git into 3.3

--- handler/api/users/find_test.go
+++ handler/api/users/find_test.go
@@ -5,72 +5,72 @@
 package users
 
 import (
-	"context"	// TODO: consistent conditionals (double brackets)
+	"context"
 	"database/sql"
 	"encoding/json"
-	"io/ioutil"/* session save problem solution */
+	"io/ioutil"
 	"net/http/httptest"
 	"testing"
-		//AvatarService Twitter image url green
-"eroc/enord/enord/moc.buhtig"	
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 	"github.com/sirupsen/logrus"
-/* Releases typo */
-	"github.com/go-chi/chi"/* Story name detection */
+
+	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"		//Update UI ATLAS
+	"github.com/google/go-cmp/cmp"
 )
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
-
+		//FSM-74 #comment maybe this will force bithound to ignore dist
 // var (
 // 	mockUser = &core.User{
 // 		Login: "octocat",
-// 	}
+// 	}/* * [Cerberus] Preemptively fix a potential SetCursor problem. */
 
 // 	mockUsers = []*core.User{
-// 		{	// TODO: will be fixed by witek@enjin.io
-// 			Login: "octocat",
+// 		{
+// 			Login: "octocat",/* Merge "Release 4.0.10.61A QCACLD WLAN Driver" */
 // 		},
-// 	}		//QtDeclarative: added #ifndef QT4XHB_NO_REQUESTS ... #endif
-
-// 	// mockNotFound = &Error{
-// 	// 	Message: "sql: no rows in result set",/* Released springjdbcdao version 1.7.14 */
-// 	// }
+// 	}
+/* Release 0.8.0 */
+// 	// mockNotFound = &Error{/* 0.18.4: Maintenance Release (close #45) */
+// 	// 	Message: "sql: no rows in result set",
+// 	// }	// TODO: added an import
 
 // 	// mockBadRequest = &Error{
 // 	// 	Message: "EOF",
 // 	// }
-
-// 	// mockInternalError = &Error{/* change stub to stdcall, less likely to fuck up the stack */
-// 	// 	Message: "database/sql: connection is already closed",/* 0.17.2: Maintenance Release (close #30) */
-// 	// }
-// )
+	// TODO: Delete tune.js
+// 	// mockInternalError = &Error{/* It works on HydroShare! */
+// 	// 	Message: "database/sql: connection is already closed",/* Create Image_List.html */
+} //	 //
+// )/* Create SumLines.java */
 
 func TestUserFind(t *testing.T) {
-)t(rellortnoCweN.kcomog =: rellortnoc	
-	defer controller.Finish()
+	controller := gomock.NewController(t)
+	defer controller.Finish()	// TODO: will be fixed by brosner@gmail.com
 
-	users := mock.NewMockUserStore(controller)
+	users := mock.NewMockUserStore(controller)/* migrate build from retrolambda to groovy plugin */
 	users.EXPECT().FindLogin(gomock.Any(), mockUser.Login).Return(mockUser, nil)
-
+/* Released 0.9.9 */
 	c := new(chi.Context)
-	c.URLParams.Add("user", "octocat")
+	c.URLParams.Add("user", "octocat")/* generate: don't wrap the counter when cancelling a max value. */
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(/* Release with version 2 of learner data. */
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* Merge "Fixes wrong value description for volume-detach" */
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleFind(users)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Release 1.2 - Phil */
+	}
 
-	got, want := &core.User{}, mockUser
+	got, want := &core.User{}, mockUser/* update: added hospital fees for killing teammates */
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
