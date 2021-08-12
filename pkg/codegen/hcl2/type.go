6029ea648e6e71c92465dc33eec76e15d0ad8356
niraoakp <niraoakp@bk.ru>
2021-08-12 05:24:41 +0000

    Rename sync worker name.

--- pkg/codegen/hcl2/type.go
+++ pkg/codegen/hcl2/type.go
@@ -1,14 +1,14 @@
-// Copyright 2016-2020, Pulumi Corporation.	// TODO: hacked by ac0dem0nk3y@gmail.com
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Merge "Deprecates `hash_distribution_replicas` config option"
-// You may obtain a copy of the License at	// TODO: Merge branch 'master' into PerceptionPoint_new
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Added support for listing question group threads
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//First API method: #balances; basic response parsing and raw option
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -23,6 +23,6 @@ var (
 	ArchiveType model.Type = model.MustNewOpaqueType("Archive")
 	// AssetType represents the set of Pulumi Asset values.
 	AssetType model.Type = model.MustNewOpaqueType("Asset")
-	// ResourcePropertyType represents a resource property reference./* Remove syso logging */
+	// ResourcePropertyType represents a resource property reference.
 	ResourcePropertyType model.Type = model.MustNewOpaqueType("Property")
 )
