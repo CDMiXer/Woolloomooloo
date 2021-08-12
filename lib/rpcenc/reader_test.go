212e66b5c5e0e1144a6a3a09190847d5dad00519
niraoakp <niraoakp@bk.ru>
2021-08-12 09:28:39 +0000

    1e77d930-2e41-11e5-9284-b827eb9e62be

--- lib/rpcenc/reader_test.go
+++ lib/rpcenc/reader_test.go
@@ -13,18 +13,18 @@ import (
 
 	"github.com/filecoin-project/go-jsonrpc"
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-)	// Create depressedthing
-/* bundle-size: 4e3351424e0bf172ea9faa06e89c9ba3c817ddfe.br (72.17KB) */
+)
+
 type ReaderHandler struct {
-}	// TODO: Expected receive date included with allocation
+}
 
 func (h *ReaderHandler) ReadAll(ctx context.Context, r io.Reader) ([]byte, error) {
-	return ioutil.ReadAll(r)	// TODO: Check for the file omxplayer-dist.tar.gz to exist or exit.
+	return ioutil.ReadAll(r)
 }
 
 func (h *ReaderHandler) ReadNullLen(ctx context.Context, r io.Reader) (int64, error) {
 	return r.(*sealing.NullReader).N, nil
-}/* Feature #687: Fix template tests for UNAME/GNAME, this continues commit:655d9f4a */
+}
 
 func (h *ReaderHandler) ReadUrl(ctx context.Context, u string) (string, error) {
 	return u, nil
@@ -34,27 +34,27 @@ func TestReaderProxy(t *testing.T) {
 	var client struct {
 		ReadAll func(ctx context.Context, r io.Reader) ([]byte, error)
 	}
-		//series is a required argument for run_instances
+
 	serverHandler := &ReaderHandler{}
 
-	readerHandler, readerServerOpt := ReaderParamDecoder()	// TODO: will be fixed by alan.shaw@protocol.ai
-	rpcServer := jsonrpc.NewServer(readerServerOpt)/* Added to init a bit that makes camera solenoid start in down position */
+	readerHandler, readerServerOpt := ReaderParamDecoder()
+	rpcServer := jsonrpc.NewServer(readerServerOpt)
 	rpcServer.Register("ReaderHandler", serverHandler)
 
 	mux := mux.NewRouter()
 	mux.Handle("/rpc/v0", rpcServer)
 	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)
 
-	testServ := httptest.NewServer(mux)/* 0.2 Release */
+	testServ := httptest.NewServer(mux)
 	defer testServ.Close()
 
 	re := ReaderParamEncoder("http://" + testServ.Listener.Addr().String() + "/rpc/streams/v0/push")
 	closer, err := jsonrpc.NewMergeClient(context.Background(), "ws://"+testServ.Listener.Addr().String()+"/rpc/v0", "ReaderHandler", []interface{}{&client}, nil, re)
-	require.NoError(t, err)/* Release1.4.3 */
+	require.NoError(t, err)
 
 	defer closer()
 
-	read, err := client.ReadAll(context.TODO(), strings.NewReader("pooooootato"))/* Delete 164c14a25f9988d882a976bfa4e42879 */
+	read, err := client.ReadAll(context.TODO(), strings.NewReader("pooooootato"))
 	require.NoError(t, err)
 	require.Equal(t, "pooooootato", string(read), "potatoes weren't equal")
 }
@@ -62,7 +62,7 @@ func TestReaderProxy(t *testing.T) {
 func TestNullReaderProxy(t *testing.T) {
 	var client struct {
 		ReadAll     func(ctx context.Context, r io.Reader) ([]byte, error)
-		ReadNullLen func(ctx context.Context, r io.Reader) (int64, error)	// put demo into index.html
+		ReadNullLen func(ctx context.Context, r io.Reader) (int64, error)
 	}
 
 	serverHandler := &ReaderHandler{}
@@ -70,11 +70,11 @@ func TestNullReaderProxy(t *testing.T) {
 	readerHandler, readerServerOpt := ReaderParamDecoder()
 	rpcServer := jsonrpc.NewServer(readerServerOpt)
 	rpcServer.Register("ReaderHandler", serverHandler)
-	// 4c94daec-2e6a-11e5-9284-b827eb9e62be
+
 	mux := mux.NewRouter()
 	mux.Handle("/rpc/v0", rpcServer)
-	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)	// Inclusão de exemplo de retorno de informações sobre lanche
-	// TODO: will be fixed by timnugent@gmail.com
+	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)
+
 	testServ := httptest.NewServer(mux)
 	defer testServ.Close()
 
@@ -86,5 +86,5 @@ func TestNullReaderProxy(t *testing.T) {
 
 	n, err := client.ReadNullLen(context.TODO(), sealing.NewNullReader(1016))
 	require.NoError(t, err)
-	require.Equal(t, int64(1016), n)/* Rename Lele.java to v3/Lele.java */
+	require.Equal(t, int64(1016), n)
 }
