bcdd1d51dc982565a312040a9a3a1ee183482667
niraoakp <niraoakp@bk.ru>
2021-08-12 06:27:05 +0000

    Merge "Refactor core/comm tests"

--- tests/integration/steps/step4/index.ts
+++ tests/integration/steps/step4/index.ts
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Improved string reading code */
-import { Resource } from "./resource";/* Delete GRBL-Plotter/bin/Release/data/fonts directory */
+
+import { Resource } from "./resource";	// TODO: Checklist.java file is modified
 
 // Step4: Replace a resource (but this time, deleteBeforeReplace):
 // * Create 1 resource, a4, equivalent to the a3 in Step 3 (Same(a3, a4)).
 let a = new Resource("a", { state: 1, replace: 1 });
-// * Create 1 resource, c4, with a property different than the c3 in Step 3, requiring replacement; set	// TODO: zu früh gefreut, weiterer Fix
+// * Create 1 resource, c4, with a property different than the c3 in Step 3, requiring replacement; set
 //   deleteBeforeReplace to true (DeleteReplaced(c3), CreateReplacement(c4)).
 let c = new Resource("c", { state: 1, replaceDBR: 1, resource: a });
 // * Create 1 resource, e4, equivlaent to the e3 in Step 3 (Same(e3, e4)).
