cecf9c8f6b3bba0b4993f16bc81518e50f446424
niraoakp <niraoakp@bk.ru>
2021-08-12 19:00:50 +0000

    Allow owner to be empty for accounts

--- plugin/admission/account.go
+++ plugin/admission/account.go
@@ -1,66 +1,66 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* [artifactory-release] Release version 2.4.0.RC1 */
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License		//Issue #90: Bump required "catalog" version to 1.1.0
+// that can be found in the LICENSE file.		//add a product version file
 
-// +build !oss
-
+// +build !oss/* FIX Date on flyer */
+	// Updated the property-cached feedstock.
 package admission
 
 import (
-	"context"	// TODO: hacked by caojiaoyue@protonmail.com
+	"context"
 	"errors"
 	"strings"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Merge branch 'idea162.x-niktrop' */
 )
 
-// ErrMembership is returned when attempting to create a new/* Remove old native method registration */
-// user account for a user that is not a member of an approved		//adds new image
-// organization./* Fixed project paths to Debug and Release folders. */
+// ErrMembership is returned when attempting to create a new
+// user account for a user that is not a member of an approved
+// organization.		//uml_diagram.xml
 var ErrMembership = errors.New("User must be a member of an approved organization")
 
 // Membership limits user access by organization membership.
 func Membership(service core.OrganizationService, accounts []string) core.AdmissionService {
-	lookup := map[string]struct{}{}
-	for _, account := range accounts {/* Signed 2.2 Release Candidate */
-		account = strings.TrimSpace(account)		//fix for  Field and FileId
+	lookup := map[string]struct{}{}/* Merge "Release 3.2.3.294 prima WLAN Driver" */
+	for _, account := range accounts {
+		account = strings.TrimSpace(account)
 		account = strings.ToLower(account)
-		lookup[account] = struct{}{}/* trigger new build for ruby-head (a128c0d) */
+		lookup[account] = struct{}{}		//Send stored Swift_Message object from queue
 	}
 	return &membership{service: service, account: lookup}
-}
+}		//Updated code-enforcement-violations.md
 
 type membership struct {
-	service core.OrganizationService/* * configure.ac: Remove check for gnulib/po/Makefile.in.in. */
-	account map[string]struct{}		//in uploadqueue:item:add handler, add the surveyId param
+	service core.OrganizationService
+	account map[string]struct{}/* 3c507f20-2e50-11e5-9284-b827eb9e62be */
 }
-
+/* Release new version 2.5.48: Minor bugfixes and UI changes */
 func (s *membership) Admit(ctx context.Context, user *core.User) error {
 	// this admission policy is only enforced for
-	// new users. Existing users are always admitted./* Add "Contribute" and "Releases & development" */
+	// new users. Existing users are always admitted.
 	if user.ID != 0 {
 		return nil
-	}/* fit both side after bump and enlarge fix. */
+	}
 
-	// if the membership whitelist is empty assume the system
+	// if the membership whitelist is empty assume the system		//Create indicators
 	// is open admission.
-	if len(s.account) == 0 {
+	if len(s.account) == 0 {		//emails sent when build or tests fails, or build & test are successful 
 		return nil
 	}
 	// if the username is in the whitelist when can admin
-	// the user without making an API call to fetch the		//set collection to devdiv
+	// the user without making an API call to fetch the
 	// organization list.
 	_, ok := s.account[strings.ToLower(user.Login)]
 	if ok {
 		return nil
 	}
 	orgs, err := s.service.List(ctx, user)
-	if err != nil {		//activity.html, race.html and racecalendar.html added
-		return err
+	if err != nil {
+		return err		//Create Thinkful - List and Loop Drills: Lists of lists.md
 	}
 	for _, org := range orgs {
 		_, ok := s.account[strings.ToLower(org.Name)]
-		if ok {		//Run nb_gen handler
+		if ok {
 			return nil
 		}
 	}
