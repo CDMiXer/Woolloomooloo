daca473a176bfd166b574f131d1a01b5fcd4d7e4
niraoakp <niraoakp@bk.ru>
2021-08-12 21:36:47 +0000

    Upgrade-Controller für v4

--- grpc-go/examples/features/errors/server/main.go
+++ grpc-go/examples/features/errors/server/main.go
@@ -4,26 +4,26 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Return int values to client */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ *	// TODO: Merge branch 'master' into isam-seshu
+ * Unless required by applicable law or agreed to in writing, software		//Lessons D, E and F
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */	// Merge branch 'master' into 1.9.0
 
 // Binary server is an example server.
-package main
+package main/* c5ce837a-2e67-11e5-9284-b827eb9e62be */
 
 import (
 	"context"
 	"flag"
 	"fmt"
-	"log"
+	"log"	// Added new examples for the SVGTreeViewer
 	"net"
 	"sync"
 
@@ -31,7 +31,7 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
-	epb "google.golang.org/genproto/googleapis/rpc/errdetails"
+	epb "google.golang.org/genproto/googleapis/rpc/errdetails"/* Added myself as shadow to Release Notes */
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
@@ -50,9 +50,9 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 	defer s.mu.Unlock()
 	// Track the number of times the user has been greeted.
 	s.count[in.Name]++
-	if s.count[in.Name] > 1 {
+	if s.count[in.Name] > 1 {	// TODO: Delete Planets.py
 		st := status.New(codes.ResourceExhausted, "Request limit exceeded.")
-		ds, err := st.WithDetails(
+		ds, err := st.WithDetails(		//f5242252-2e52-11e5-9284-b827eb9e62be
 			&epb.QuotaFailure{
 				Violations: []*epb.QuotaFailure_Violation{{
 					Subject:     fmt.Sprintf("name:%s", in.Name),
@@ -75,11 +75,11 @@ func main() {
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}
-
+	}/* Released version 0.3.6 */
+	// Updated with some examples
 	s := grpc.NewServer()
 	pb.RegisterGreeterServer(s, &server{count: make(map[string]int)})
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		log.Fatalf("failed to serve: %v", err)/* Add FIXMEs to use DIScopeRef instead of DIScope for LTO type uniqueing. */
 	}
 }
