b4068415e37c1df1b50b82234a2b6ab686ca4241
niraoakp <niraoakp@bk.ru>
2021-08-12 00:33:53 +0000

    too much notes

--- handler/api/repos/crons/list.go
+++ handler/api/repos/crons/list.go
@@ -1,41 +1,41 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Merge "Add support for Fedora 20 to nodepool"
+// that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* Compute delay from request issue, not response return.  Fixes #721 */
 
-package crons/* disabling these for now */
-	// TODO: hacked by brosner@gmail.com
+package crons
+
 import (
-	"net/http"
+	"net/http"/* 32 Planeten */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)
+)/* docs: write some API docs for the modules for the device */
 
-// HandleList returns an http.HandlerFunc that writes a json-encoded/* [typo] bin.packParentConstructors => binPack.parentConstructors */
-// list of cron jobs to the response body./* Publishing post - Blocks in Ruby ! */
-func HandleList(
+// HandleList returns an http.HandlerFunc that writes a json-encoded
+// list of cron jobs to the response body.
+func HandleList(	// Create curators.md
 	repos core.RepositoryStore,
-	crons core.CronStore,
-) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {	// TODO: Delete MP_ReportingAnalytics.md
+	crons core.CronStore,/* Merge "Release 1.0.0.107 QCACLD WLAN Driver" */
+) http.HandlerFunc {		//Merge "Test code generation for field accesses."
+	return func(w http.ResponseWriter, r *http.Request) {/* Compile for Release */
 		var (
-			namespace = chi.URLParam(r, "owner")/* f7fa9a6c-2e55-11e5-9284-b827eb9e62be */
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
-			render.NotFound(w, err)
+		if err != nil {	// TODO: hacked by juan@benet.ai
+			render.NotFound(w, err)	// Update boom_barrel.nut
 			return
 		}
 		list, err := crons.List(r.Context(), repo.ID)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}/* Removed maintainer */
+		}
 		render.JSON(w, list, 200)
 	}
 }
