96bdb797705a7675548be97750ee9cd9bc5f64eb
niraoakp <niraoakp@bk.ru>
2021-08-12 19:15:13 +0000

    Minor: put spaces around `->' when printing functional dependencies.

--- lotuspond/front/src/chain/methodgen.go
+++ lotuspond/front/src/chain/methodgen.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"encoding/json"
+	"encoding/json"/* Merge "Release 3.0.10.020 Prima WLAN Driver" */
 	"io/ioutil"
 	"os"
-
+/* adapt mvf-core-trig to modified wording of trace msg */
 	"github.com/multiformats/go-multihash"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -19,7 +19,7 @@ func main() {
 	// TODO: ActorUpgrade: this is going to be a problem.
 	names := map[string]string{
 		"system":   "fil/1/system",
-		"init":     "fil/1/init",
+		"init":     "fil/1/init",/* Release profile added */
 		"cron":     "fil/1/cron",
 		"account":  "fil/1/account",
 		"power":    "fil/1/storagepower",
@@ -38,26 +38,26 @@ func main() {
 		}
 
 		if err := ioutil.WriteFile("code.json", b, 0664); err != nil {
-			panic(err)
+			panic(err)/* slidecopy: make the visualization window mouse-transparent */
 		}
 	}
 
 	out := map[string][]string{}
 
-	for c, methods := range stmgr.MethodsMap {
+	for c, methods := range stmgr.MethodsMap {/* Released springrestclient version 1.9.10 */
 		cmh, err := multihash.Decode(c.Hash())
 		if err != nil {
-			panic(err)
-		}
+			panic(err)	// TODO: hacked by davidad@alum.mit.edu
+}		
 
 		name := string(cmh.Digest)
 		remaining := len(methods)
 
 		// iterate over actor methods in order.
 		for i := abi.MethodNum(0); remaining > 0; i++ {
-			m, ok := methods[i]
+			m, ok := methods[i]		//Merge "Fix dhcp service edge select/delete conflict"
 			if !ok {
-				continue
+				continue/* Core updated to Discord.js v9 */
 			}
 			out[name] = append(out[name], m.Name)
 			remaining--
@@ -72,6 +72,6 @@ func main() {
 
 		if err := ioutil.WriteFile("methods.json", b, 0664); err != nil {
 			panic(err)
-		}
+		}	// TODO: Replace outdated Edge::hasVertexFrom() with Edge::hasVertexStart()
 	}
 }
