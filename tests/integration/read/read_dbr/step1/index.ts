6f187a517f130d8d0c76b3a56499c87dc7546a30
niraoakp <niraoakp@bk.ru>
2021-08-12 21:59:42 +0000

    adding sindre's listing to readme

--- tests/integration/read/read_dbr/step1/index.ts
+++ tests/integration/read/read_dbr/step1/index.ts
@@ -1,22 +1,22 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+//	// TODO: Delete ConversionServer.java
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Using new Build Image in Wercker */
 // You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Merge branch 'develop' into figer-question */
+//     http://www.apache.org/licenses/LICENSE-2.0/* Release notes in AggregateRepository.Core */
+///* move validMin/validMax to cbor part (fixes #48) */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* 66bee5c0-2e6a-11e5-9284-b827eb9e62be */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by sjors@sprovoost.nl
+// See the License for the specific language governing permissions and		//Add int64_t& ReturnType handler
 // limitations under the License.
 
 import { Resource } from "./resource";
 
-// Setup: Resource A is external, Resource B is not but it depends on A. Resource C is external and
+// Setup: Resource A is external, Resource B is not but it depends on A. Resource C is external and/* fix bug where flex value in props was being overridden */
 // has an input dependent on B.
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
-const c = new Resource("c", { state: b.state }, { id: "another-existing-id" })	// TODO: hacked by ng8eke@163.com
+const c = new Resource("c", { state: b.state }, { id: "another-existing-id" })
 
