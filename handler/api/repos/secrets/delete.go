409e22f478052761ff26302d49e6909ae9e1aa1e
niraoakp <niraoakp@bk.ru>
2021-08-12 06:19:31 +0000

    use method instead of function

--- handler/api/repos/secrets/delete.go
+++ handler/api/repos/secrets/delete.go
@@ -1,32 +1,32 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: indention de case true
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// Add MDHT error free Regression Test
+// +build !oss
 
 package secrets
-	// TODO: Translation to fr
-import (
-	"net/http"		//Update rayon to 0.7
 
-	"github.com/drone/drone/core"		//Fixed Java warnings in compiler.jx project.
+import (
+	"net/http"
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)/* use AddRef()/Release() for RefCounted */
+)
 
-// HandleDelete returns an http.HandlerFunc that processes http	// TODO: will be fixed by alan.shaw@protocol.ai
-// requests to delete the secret./* Release for v0.6.0. */
+// HandleDelete returns an http.HandlerFunc that processes http
+// requests to delete the secret.
 func HandleDelete(
 	repos core.RepositoryStore,
 	secrets core.SecretStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (/* Initial Release Notes */
-			namespace = chi.URLParam(r, "owner")/* Update ReleaseUpgrade.md */
+		var (
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			secret    = chi.URLParam(r, "secret")/* Add Sender::createFromLoopDns() function */
-		)/* using BuildPeriodRange in ltp.R */
+			secret    = chi.URLParam(r, "secret")
+		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
