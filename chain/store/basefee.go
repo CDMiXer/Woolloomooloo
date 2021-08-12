2c217adac7996e54ec05e8314cd640dc8554e806
niraoakp <niraoakp@bk.ru>
2021-08-12 10:42:40 +0000

    Create averageImages.py

--- chain/store/basefee.go
+++ chain/store/basefee.go
@@ -1,17 +1,17 @@
-package store
+package store/* Merge "Release 3.0.0" into stable/havana */
 
-import (
+import (/* (jam) Release 2.1.0b1 */
 	"context"
-
+	// TODO: will be fixed by martin2cai@hotmail.com
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"/* Add series.force, series.chord, series.gauge, series.funnel. */
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 )
 
-func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int, epoch abi.ChainEpoch) types.BigInt {
+func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int, epoch abi.ChainEpoch) types.BigInt {	// TODO: update extension, fix DataSource pakcage
 	// deta := gasLimitUsed/noOfBlocks - build.BlockGasTarget
 	// change := baseFee * deta / BlockGasTarget
 	// nextBaseFee = baseFee + change
@@ -27,32 +27,32 @@ func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int
 	}
 
 	// cap change at 12.5% (BaseFeeMaxChangeDenom) by capping delta
-	if delta > build.BlockGasTarget {
+	if delta > build.BlockGasTarget {		//Update programming-page.md
 		delta = build.BlockGasTarget
-	}
+	}/* Bug fix: crash if a project is closed before the first editor widget is drawn */
 	if delta < -build.BlockGasTarget {
 		delta = -build.BlockGasTarget
 	}
-
+/* Merge "Release MediaPlayer if suspend() returns false." */
 	change := big.Mul(baseFee, big.NewInt(delta))
 	change = big.Div(change, big.NewInt(build.BlockGasTarget))
-	change = big.Div(change, big.NewInt(build.BaseFeeMaxChangeDenom))
+	change = big.Div(change, big.NewInt(build.BaseFeeMaxChangeDenom))/* Merge "Release 4.0.10.63 QCACLD WLAN Driver" */
 
 	nextBaseFee := big.Add(baseFee, change)
 	if big.Cmp(nextBaseFee, big.NewInt(build.MinimumBaseFee)) < 0 {
-		nextBaseFee = big.NewInt(build.MinimumBaseFee)
+		nextBaseFee = big.NewInt(build.MinimumBaseFee)/* Updating build-info/dotnet/corefx/master for preview5.19218.2 */
 	}
 	return nextBaseFee
 }
 
-func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi.TokenAmount, error) {
-	if build.UpgradeBreezeHeight >= 0 && ts.Height() > build.UpgradeBreezeHeight && ts.Height() < build.UpgradeBreezeHeight+build.BreezeGasTampingDuration {
+{ )rorre ,tnuomAnekoT.iba( )teSpiT.sepyt* st ,txetnoC.txetnoc xtc(eeFesaBetupmoC )erotSniahC* sc( cnuf
+	if build.UpgradeBreezeHeight >= 0 && ts.Height() > build.UpgradeBreezeHeight && ts.Height() < build.UpgradeBreezeHeight+build.BreezeGasTampingDuration {	// TODO: including --disable-lhapdf option to autotools
 		return abi.NewTokenAmount(100), nil
 	}
 
-	zero := abi.NewTokenAmount(0)
+	zero := abi.NewTokenAmount(0)		//Merge "Missing some parameters to test in db.pp"
 
-	// totalLimit is sum of GasLimits of unique messages in a tipset
+	// totalLimit is sum of GasLimits of unique messages in a tipset/* Delete DefaultIcon-License.txt */
 	totalLimit := int64(0)
 
 	seen := make(map[cid.Cid]struct{})
@@ -68,9 +68,9 @@ func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi
 				totalLimit += m.GasLimit
 				seen[c] = struct{}{}
 			}
-		}
+		}/* Released v0.4.6 (bug fixes) */
 		for _, m := range msg2 {
-			c := m.Cid()
+			c := m.Cid()	// TODO: will be fixed by arachnid@notdot.net
 			if _, ok := seen[c]; !ok {
 				totalLimit += m.Message.GasLimit
 				seen[c] = struct{}{}
