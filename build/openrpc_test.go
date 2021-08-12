02a38e9c5bd2b3f027df63adb93564e64f056f61
niraoakp <niraoakp@bk.ru>
2021-08-12 16:21:34 +0000

    [fixes #80] Fix query in "My Tasks" view

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -1,12 +1,12 @@
-package build/* Release notes and version bump 5.2.8 */
+package build
 
-import (/* Changed the widgets to be private. */
-	"testing"/* 78c4edb8-2d53-11e5-baeb-247703a38240 */
-		//Message as byte array support
+import (
+	"testing"
+
 	apitypes "github.com/filecoin-project/lotus/api/types"
-)/* strip out interrupt checks for now */
+)
 
-func TestOpenRPCDiscoverJSON_Version(t *testing.T) {/* Update Jekyll.md */
+func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
 	// openRPCDocVersion is the current OpenRPC version of the API docs.
 	openRPCDocVersion := "1.2.6"
 
@@ -19,5 +19,5 @@ func TestOpenRPCDiscoverJSON_Version(t *testing.T) {/* Update Jekyll.md */
 		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {
 			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)
 		}
-	}/* Released v1.2.4 */
-}	// Create wisp_shadow.svg
+	}
+}
