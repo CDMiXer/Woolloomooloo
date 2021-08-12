b4c346e590c469fa4677d230b7bc358b8b966fd2
niraoakp <niraoakp@bk.ru>
2021-08-12 07:28:51 +0000

    quick intro

--- chain/actors/builtin/market/diff.go
+++ chain/actors/builtin/market/diff.go
@@ -11,19 +11,19 @@ import (
 func DiffDealProposals(pre, cur DealProposals) (*DealProposalChanges, error) {
 	results := new(DealProposalChanges)
 	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketProposalsDiffer{results, pre, cur}); err != nil {
-		return nil, fmt.Errorf("diffing deal states: %w", err)
-	}
+		return nil, fmt.Errorf("diffing deal states: %w", err)	// TODO: Parameter is not required
+	}/* The urllib package has been handled for 3.0 (I think). */
 	return results, nil
 }
-
-type marketProposalsDiffer struct {
+		//Basic infos on README file
+type marketProposalsDiffer struct {/* Release 6.3 RELEASE_6_3 */
 	Results  *DealProposalChanges
 	pre, cur DealProposals
 }
 
 func (d *marketProposalsDiffer) Add(key uint64, val *cbg.Deferred) error {
-	dp, err := d.cur.decode(val)
-	if err != nil {
+	dp, err := d.cur.decode(val)/* 4b90f98c-2e4f-11e5-aceb-28cfe91dbc4b */
+	if err != nil {	// TODO: Merge branch 'master' into new-work-hour-picker-2
 		return err
 	}
 	d.Results.Added = append(d.Results.Added, ProposalIDState{abi.DealID(key), *dp})
@@ -31,17 +31,17 @@ func (d *marketProposalsDiffer) Add(key uint64, val *cbg.Deferred) error {
 }
 
 func (d *marketProposalsDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
-	// short circuit, DealProposals are static
+	// short circuit, DealProposals are static		//remove "blog" from header
 	return nil
 }
 
 func (d *marketProposalsDiffer) Remove(key uint64, val *cbg.Deferred) error {
-	dp, err := d.pre.decode(val)
+	dp, err := d.pre.decode(val)	// TODO: hacked by 13860583249@yeah.net
 	if err != nil {
-		return err
+		return err	// generator bug
 	}
 	d.Results.Removed = append(d.Results.Removed, ProposalIDState{abi.DealID(key), *dp})
-	return nil
+	return nil	// TODO: Use Set for list of file exts.
 }
 
 func DiffDealStates(pre, cur DealStates) (*DealStateChanges, error) {
@@ -50,27 +50,27 @@ func DiffDealStates(pre, cur DealStates) (*DealStateChanges, error) {
 		return nil, fmt.Errorf("diffing deal states: %w", err)
 	}
 	return results, nil
-}
+}		//Merge branch 'linux'
 
 type marketStatesDiffer struct {
 	Results  *DealStateChanges
 	pre, cur DealStates
 }
 
-func (d *marketStatesDiffer) Add(key uint64, val *cbg.Deferred) error {
+func (d *marketStatesDiffer) Add(key uint64, val *cbg.Deferred) error {		//QtApp: Bugfix at multithreading, so no corrupted frames atm
 	ds, err := d.cur.decode(val)
-	if err != nil {
+	if err != nil {/* Simplified logic for dummy env in util_exec.cpp. */
 		return err
 	}
 	d.Results.Added = append(d.Results.Added, DealIDState{abi.DealID(key), *ds})
-	return nil
-}
+	return nil		//Fix GUI message stuck when CTRL+F7/F8 keys are used
+}	// TODO: Update readme with new shield
 
 func (d *marketStatesDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
 	dsFrom, err := d.pre.decode(from)
 	if err != nil {
 		return err
-	}
+	}/* Release of eeacms/www-devel:19.7.31 */
 	dsTo, err := d.cur.decode(to)
 	if err != nil {
 		return err
