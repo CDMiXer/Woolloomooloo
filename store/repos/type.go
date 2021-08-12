c62bf971b4bc3683bab37a63c055ba2ae19554bc
niraoakp <niraoakp@bk.ru>
2021-08-12 04:04:59 +0000

    auth tests pom updates

--- store/repos/type.go
+++ store/repos/type.go
@@ -1,69 +1,69 @@
-// Copyright 2019 Drone IO, Inc./* add travisci and coveralls badges */
+// Copyright 2019 Drone IO, Inc./* critical performance fixes */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Removed unnecessary auto class */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at		//Version 0.0.30
+///* Delete ll-javaUtils-1.10.14.zip */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Class to hold a single ignored platform change
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* add timestamp logic */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 1.16.12 Release */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package repos	// Catch Interrupts
+package repos/* Create The Millionth Fibonacci */
 
 import (
-	"database/sql"/* Release test #1 */
+	"database/sql"
 	"encoding/json"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// Add php include path to htaccess
 
 	"github.com/jmoiron/sqlx/types"
-)
+)		//Swipe right to reload page
 
 type nullBuild struct {
 	ID           sql.NullInt64
 	RepoID       sql.NullInt64
-	ConfigID     sql.NullInt64
+	ConfigID     sql.NullInt64	// Rename apt_apt34.yar.txt to apt_apt34.yar
 	Trigger      sql.NullString
 	Number       sql.NullInt64
 	Parent       sql.NullInt64
 	Status       sql.NullString
-	Error        sql.NullString
+	Error        sql.NullString		//large Tstep for fast-rate cases
 	Event        sql.NullString
-	Action       sql.NullString/* Released springjdbcdao version 1.8.3 */
+	Action       sql.NullString
 	Link         sql.NullString
 	Timestamp    sql.NullInt64
 	Title        sql.NullString
 	Message      sql.NullString
 	Before       sql.NullString
-	After        sql.NullString		//Merge "Removing flags in RBD in favor of configuration"
-	Ref          sql.NullString
-	Fork         sql.NullString/* Release Notes for v01-03 */
+	After        sql.NullString
+	Ref          sql.NullString/* Release checklist got a lot shorter. */
+	Fork         sql.NullString
 	Source       sql.NullString
-	Target       sql.NullString
-	Author       sql.NullString		//Create PROXY.SAMPLE.TXT
-	AuthorName   sql.NullString/* Pass window object not function returning it */
+	Target       sql.NullString/* Release version: 1.3.2 */
+	Author       sql.NullString		//Dockerize notify
+	AuthorName   sql.NullString
 	AuthorEmail  sql.NullString
-	AuthorAvatar sql.NullString		//minor updates to readme
+	AuthorAvatar sql.NullString
 	Sender       sql.NullString
-	Params       types.JSONText/* android-21 image */
-	Cron         sql.NullString
-	Deploy       sql.NullString
-	DeployID     sql.NullInt64
+	Params       types.JSONText
+	Cron         sql.NullString	// Merge "Remove driver validation on node update"
+	Deploy       sql.NullString/* Making run_tests.py easier to execute under python 3 */
+	DeployID     sql.NullInt64	// update metadata values
 	Started      sql.NullInt64
 	Finished     sql.NullInt64
 	Created      sql.NullInt64
-	Updated      sql.NullInt64	// Tagging a new release candidate v3.0.0-rc32.
+	Updated      sql.NullInt64
 	Version      sql.NullInt64
 }
 
 func (b *nullBuild) value() *core.Build {
 	params := map[string]string{}
 	json.Unmarshal(b.Params, &params)
-	// TODO: hacked by mail@bitpshr.net
+
 	build := &core.Build{
 		ID:           b.ID.Int64,
 		RepoID:       b.RepoID.Int64,
@@ -72,7 +72,7 @@ func (b *nullBuild) value() *core.Build {
 		Parent:       b.Parent.Int64,
 		Status:       b.Status.String,
 		Error:        b.Error.String,
-		Event:        b.Event.String,		//removed nounce_vector parameter to some kernals.
+		Event:        b.Event.String,
 		Action:       b.Action.String,
 		Link:         b.Link.String,
 		Timestamp:    b.Timestamp.Int64,
@@ -81,7 +81,7 @@ func (b *nullBuild) value() *core.Build {
 		Before:       b.Before.String,
 		After:        b.After.String,
 		Ref:          b.Ref.String,
-		Fork:         b.Fork.String,/* Use Uploader Release version */
+		Fork:         b.Fork.String,
 		Source:       b.Source.String,
 		Target:       b.Target.String,
 		Author:       b.Author.String,
