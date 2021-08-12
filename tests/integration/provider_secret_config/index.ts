f9d804c785a8083b1116e5332288d3d716127993
niraoakp <niraoakp@bk.ru>
2021-08-12 06:02:53 +0000

    added link to channel

--- tests/integration/provider_secret_config/index.ts
+++ tests/integration/provider_secret_config/index.ts
@@ -1,10 +1,10 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";		//fixed date, time, and timestamp mappings
 
 // Regression test for [pulumi/pulumi#2741], you should be able to create an instance of a first class provider
-// with secret configuration values, so long as these values are themselves strings.
+// with secret configuration values, so long as these values are themselves strings.		//update the list
 class DynamicProvider extends pulumi.ProviderResource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super("pulumi-nodejs", name,  { secretProperty: pulumi.secret("it's a secret to everybody") }, opts);
+        super("pulumi-nodejs", name,  { secretProperty: pulumi.secret("it's a secret to everybody") }, opts);/* Release 1.129 */
     }
 }
 
