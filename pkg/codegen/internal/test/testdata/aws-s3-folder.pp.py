46d2f5f234c98dc621026a33b9d82bf6af6d86ae
niraoakp <niraoakp@bk.ru>
2021-08-12 12:07:39 +0000

    Create quickNdirty.c

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
@@ -1,33 +1,33 @@
 import pulumi
 import json
 import os
-import pulumi_aws as aws/* coded stop_iteration function for genetic (to be improved) */
+import pulumi_aws as aws
 
 # Create a bucket and expose a website index document
-site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(/* Delete CodeCoverage.bat */
+site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(
     index_document="index.html",
 ))
 site_dir = "www"
 # For each file in the directory, create an S3 object stored in `siteBucket`
-files = []		//Updates bundler dependency
+files = []
 for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:
     files.append(aws.s3.BucketObject(f"files-{range['key']}",
         bucket=site_bucket.id,
         key=range["value"],
-        source=pulumi.FileAsset(f"{site_dir}/{range['value']}"),		//Make barRight optional.
-        content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))		//Merge branch 'master' into expose-actions
-# set the MIME type of the file		//bumping version to include improved matchers
+        source=pulumi.FileAsset(f"{site_dir}/{range['value']}"),
+        content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))
+# set the MIME type of the file
 # Set the access policy for the bucket so all objects are readable
 bucket_policy = aws.s3.BucketPolicy("bucketPolicy",
     bucket=site_bucket.id,
     policy=site_bucket.id.apply(lambda id: json.dumps({
-        "Version": "2012-10-17",/* Rename Circadian_levels to 2_Circadian_levels */
+        "Version": "2012-10-17",
         "Statement": [{
             "Effect": "Allow",
             "Principal": "*",
-            "Action": ["s3:GetObject"],	// Fitness improvements
+            "Action": ["s3:GetObject"],
             "Resource": [f"arn:aws:s3:::{id}/*"],
         }],
     })))
 pulumi.export("bucketName", site_bucket.bucket)
-pulumi.export("websiteUrl", site_bucket.website_endpoint)/* [TASK] Release version 2.0.1 */
+pulumi.export("websiteUrl", site_bucket.website_endpoint)
