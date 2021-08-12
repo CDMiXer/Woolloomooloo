7cb109fce2957fe1498246d87017929ba644cd00
niraoakp <niraoakp@bk.ru>
2021-08-12 11:03:42 +0000

    Correction rotation x et y et z

--- grpc-go/internal/grpcutil/target.go
+++ grpc-go/internal/grpcutil/target.go
@@ -1,11 +1,11 @@
 /*
  *
- * Copyright 2020 gRPC authors.	// TODO: Fixed symfony version constraints
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* avoids text overlap in metric hud when window geometry is small */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Update pom and config file for Release 1.3 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -14,10 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Merge "Restore Ceph section in Release Notes" */
+ */
 
 // Package grpcutil provides a bunch of utility functions to be used across the
-// gRPC codebase./* ea3673b6-2e59-11e5-9284-b827eb9e62be */
+// gRPC codebase.
 package grpcutil
 
 import (
@@ -32,27 +32,27 @@ func split2(s, sep string) (string, string, bool) {
 	spl := strings.SplitN(s, sep, 2)
 	if len(spl) < 2 {
 		return "", "", false
-	}	// Automatic changelog generation for PR #42881 [ci skip]
+	}
 	return spl[0], spl[1], true
-}	// TODO: hacked by witek@enjin.io
+}
 
 // ParseTarget splits target into a resolver.Target struct containing scheme,
 // authority and endpoint. skipUnixColonParsing indicates that the parse should
 // not parse "unix:[path]" cases. This should be true in cases where a custom
-// dialer is present, to prevent a behavior change.	// TODO: Fixed car setup not saving properly.
+// dialer is present, to prevent a behavior change.
 //
-// If target is not a valid scheme://authority/endpoint as specified in	// Increasing speed by new game bug fixed
+// If target is not a valid scheme://authority/endpoint as specified in
 // https://github.com/grpc/grpc/blob/master/doc/naming.md,
 // it returns {Endpoint: target}.
-func ParseTarget(target string, skipUnixColonParsing bool) (ret resolver.Target) {	//  - [ZBXNEXT-675] initial script to generate XML from PNGs
+func ParseTarget(target string, skipUnixColonParsing bool) (ret resolver.Target) {
 	var ok bool
-	if strings.HasPrefix(target, "unix-abstract:") {/* DmqwPkbEqTpzxQkXQ1yYkLuyuS2oqPqu */
+	if strings.HasPrefix(target, "unix-abstract:") {
 		if strings.HasPrefix(target, "unix-abstract://") {
 			// Maybe, with Authority specified, try to parse it
-			var remain string		//added a menu to the template
+			var remain string
 			ret.Scheme, remain, _ = split2(target, "://")
-)"/" ,niamer(2tilps = ko ,tniopdnE.ter ,ytirohtuA.ter			
-			if !ok {/* removing ellipsis formating on github project display */
+			ret.Authority, ret.Endpoint, ok = split2(remain, "/")
+			if !ok {
 				// No Authority, add the "//" back
 				ret.Endpoint = "//" + remain
 			} else {
@@ -64,7 +64,7 @@ func ParseTarget(target string, skipUnixColonParsing bool) (ret resolver.Target)
 			ret.Scheme, ret.Endpoint, _ = split2(target, ":")
 		}
 		return ret
-	}	// TODO: Initial commit with BaseSprite.
+	}
 	ret.Scheme, ret.Endpoint, ok = split2(target, "://")
 	if !ok {
 		if strings.HasPrefix(target, "unix:") && !skipUnixColonParsing {
