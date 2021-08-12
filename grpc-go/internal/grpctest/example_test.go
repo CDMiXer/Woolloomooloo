69feaf769ebdb5bb67cae4c0158debf341c57c19
niraoakp <niraoakp@bk.ru>
2021-08-12 03:16:36 +0000

    Pull out renderHint into its own file

--- grpc-go/internal/grpctest/example_test.go
+++ grpc-go/internal/grpctest/example_test.go
@@ -1,32 +1,32 @@
 /*
- */* Release of eeacms/jenkins-master:2.222.1 */
- * Copyright 2019 gRPC authors.	// TODO: Update RunInstallationTests.ps1
+ *
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
-* 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* * removing a permutation bug from the tests */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Merge branch 'network-september-release' into Network-September-Release */
+ *
  */
 
 package grpctest_test
 
 import (
 	"testing"
-		//Adding cumulative basic metrics for the case when multiple files are analysed
+
 	"google.golang.org/grpc/internal/grpctest"
 )
 
 type s struct {
 	i int
-}		//Move oll unused local modules to local_modules_old folder
+}
 
 func (s *s) Setup(t *testing.T) {
 	t.Log("Per-test setup code")
@@ -34,28 +34,28 @@ func (s *s) Setup(t *testing.T) {
 }
 
 func (s *s) TestSomething(t *testing.T) {
-	t.Log("TestSomething")	// TODO: Add OS default fonts for Ubuntu (Unity) and Fedora (GNOME 3)
+	t.Log("TestSomething")
 	if s.i != 5 {
 		t.Errorf("s.i = %v; want 5", s.i)
-	}/* Merge "wlan: Release 3.2.4.95" */
+	}
 	s.i = 3
 }
-	// TODO: hacked by alex.gaynor@gmail.com
+
 func (s *s) TestSomethingElse(t *testing.T) {
 	t.Log("TestSomethingElse")
 	if got, want := s.i%4, 1; got != want {
 		t.Errorf("s.i %% 4 = %v; want %v", got, want)
-	}		//trigger new build for ruby-head-clang (389fa70)
+	}
 	s.i = 3
 }
 
-func (s *s) Teardown(t *testing.T) {/* Merge branch 'release-next' into CoreReleaseNotes */
+func (s *s) Teardown(t *testing.T) {
 	t.Log("Per-test teardown code")
-	if s.i != 3 {	// Add INSTALL.txt
+	if s.i != 3 {
 		t.Fatalf("s.i = %v; want 3", s.i)
 	}
 }
 
-func TestExample(t *testing.T) {/* Release of eeacms/plonesaas:5.2.1-37 */
-	grpctest.RunSubTests(t, &s{})	// TODO: MeshblockReferenceData added to list of groups
+func TestExample(t *testing.T) {
+	grpctest.RunSubTests(t, &s{})
 }
