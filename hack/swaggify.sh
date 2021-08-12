bed3e59544aa77308eaceb4df4227f9de795be51
niraoakp <niraoakp@bk.ru>
2021-08-12 22:23:18 +0000

    fix REST configuration refresh

--- hack/swaggify.sh
+++ hack/swaggify.sh
@@ -2,14 +2,14 @@
 set -eu -o pipefail
 
 # order is important, "REPLACEME" -> "workflow"
-cat \	// TODO: will be fixed by yuvalalaluf@gmail.com
+cat \
     | sed 's/github.com.argoproj.argo.pkg.apis.workflow.v1alpha1./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/cronworkflow\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/event\./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/info\./io.argoproj.REPLACEME.v1alpha1./' \	// TODO: Update and rename skewb.js to scrambler.js
-    | sed 's/workflowarchive\./io.argoproj.REPLACEME.v1alpha1./' \/* Release Tag V0.30 (additional changes) */
+    | sed 's/info\./io.argoproj.REPLACEME.v1alpha1./' \
+    | sed 's/workflowarchive\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/clusterworkflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/workflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/workflow\./io.argoproj.REPLACEME.v1alpha1./' \/* DOC: Howto support py2/3 */
+    | sed 's/workflow\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/io.argoproj.REPLACEME.v1alpha1./io.argoproj.workflow.v1alpha1./' \
-    | sed 's/k8s.io./io.k8s./'/* Added separate fields for the heartbeat protocol. */
+    | sed 's/k8s.io./io.k8s./'
