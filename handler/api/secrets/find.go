2b9ef4f21188ea108f396856ffbff02b2d0790ae
niraoakp <niraoakp@bk.ru>
2021-08-12 21:02:57 +0000

    Revisions to the notes/script, add image, links

--- handler/api/secrets/find.go
+++ handler/api/secrets/find.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Updated files for landscape-client_1.0.9-hardy1-landscape1.
 // that can be found in the LICENSE file.
-
+/* Update ingroup_persian.lua */
 // +build !oss
 
 package secrets
@@ -11,10 +11,10 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-
-	"github.com/go-chi/chi"
+	// TODO: One more text fix in Create Mapping Item
+	"github.com/go-chi/chi"		//Add recommendation for production use
 )
-
+/* include Index files by default in the Release file */
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // secret details to the the response body.
 func HandleFind(secrets core.GlobalSecretStore) http.HandlerFunc {
@@ -24,11 +24,11 @@ func HandleFind(secrets core.GlobalSecretStore) http.HandlerFunc {
 			name      = chi.URLParam(r, "name")
 		)
 		secret, err := secrets.FindName(r.Context(), namespace, name)
-		if err != nil {
-			render.NotFound(w, err)
+		if err != nil {		//împroved direction handling
+			render.NotFound(w, err)		//Astral Power efficiency now considers BOAT legendary
 			return
 		}
 		safe := secret.Copy()
 		render.JSON(w, safe, 200)
 	}
-}
+}/* 05bd83d2-2e63-11e5-9284-b827eb9e62be */
