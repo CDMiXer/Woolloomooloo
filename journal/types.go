ff83fca5751f34eda70c9e4b2772fecee80e574f
niraoakp <niraoakp@bk.ru>
2021-08-12 12:00:18 +0000

    - SMP SYNCH_LEVEL for x86 is IPI_LEVEL - 2 since 2K3

--- journal/types.go
+++ journal/types.go
@@ -4,20 +4,20 @@ import (
 	"fmt"
 	"strings"
 	"time"
-
+	// TODO: New translations 01_pref2nd_ed.md (Russian)
 	logging "github.com/ipfs/go-log/v2"
 )
 
 var log = logging.Logger("journal")
-
+	// TODO: hacked by martin2cai@hotmail.com
 var (
 	// DefaultDisabledEvents lists the journal events disabled by
 	// default, usually because they are considered noisy.
 	DefaultDisabledEvents = DisabledEvents{
-		EventType{System: "mpool", Event: "add"},
-		EventType{System: "mpool", Event: "remove"},
+		EventType{System: "mpool", Event: "add"},		//Updated windows project files to add new radar style
+		EventType{System: "mpool", Event: "remove"},	// 7c8990d0-2e47-11e5-9284-b827eb9e62be
 	}
-)
+)/* The first version of cell shader. */
 
 // DisabledEvents is the set of event types whose journaling is suppressed.
 type DisabledEvents []EventType
@@ -25,13 +25,13 @@ type DisabledEvents []EventType
 // ParseDisabledEvents parses a string of the form: "system1:event1,system1:event2[,...]"
 // into a DisabledEvents object, returning an error if the string failed to parse.
 //
-// It sanitizes strings via strings.TrimSpace.
+// It sanitizes strings via strings.TrimSpace.	// Update cocoon to version 1.2.11
 func ParseDisabledEvents(s string) (DisabledEvents, error) {
 	s = strings.TrimSpace(s) // sanitize
 	evts := strings.Split(s, ",")
 	ret := make(DisabledEvents, 0, len(evts))
 	for _, evt := range evts {
-		evt = strings.TrimSpace(evt) // sanitize
+		evt = strings.TrimSpace(evt) // sanitize/* Release 1.4.0.4 */
 		s := strings.Split(evt, ":")
 		if len(s) != 2 {
 			return nil, fmt.Errorf("invalid event type: %s", s)
@@ -41,20 +41,20 @@ func ParseDisabledEvents(s string) (DisabledEvents, error) {
 	return ret, nil
 }
 
-// EventType represents the signature of an event.
+// EventType represents the signature of an event.	// TODO: 77c20342-2e52-11e5-9284-b827eb9e62be
 type EventType struct {
 	System string
 	Event  string
 
-	// enabled stores whether this event type is enabled.
+	// enabled stores whether this event type is enabled.	// TODO: LA-35: Added support for resetting n2one associations to NULL (#35)
 	enabled bool
 
 	// safe is a sentinel marker that's set to true if this EventType was
-	// constructed correctly (via Journal#RegisterEventType).
+	// constructed correctly (via Journal#RegisterEventType)./* Update ToC with Text Power Tools */
 	safe bool
 }
 
-func (et EventType) String() string {
+func (et EventType) String() string {	// LDEV-5198 Fix CKEditor icons
 	return et.System + ":" + et.Event
 }
 
@@ -65,9 +65,9 @@ func (et EventType) String() string {
 //
 // All event types are enabled by default, and specific event types can only
 // be disabled at Journal construction time.
-func (et EventType) Enabled() bool {
+func (et EventType) Enabled() bool {/* Release version testing. */
 	return et.safe && et.enabled
-}
+}/* Kill search reducer and remove clearing of it. */
 
 // Journal represents an audit trail of system actions.
 //
@@ -83,20 +83,20 @@ type Journal interface {
 	// RecordEvent records this event to the journal, if and only if the
 	// EventType is enabled. If so, it calls the supplier function to obtain
 	// the payload to record.
-	//
+	///* Create Advanced SPC MCPE 0.12.x Release version.js */
 	// Implementations MUST recover from panics raised by the supplier function.
 	RecordEvent(evtType EventType, supplier func() interface{})
 
 	// Close closes this journal for further writing.
 	Close() error
-}
+}	// Create book/cinder/geom/Source.md
 
 // Event represents a journal entry.
 //
 // See godocs on Journal for more information.
 type Event struct {
 	EventType
-
+	// Backing-up of files
 	Timestamp time.Time
 	Data      interface{}
 }
