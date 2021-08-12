adbccfeb930beb64d07a1fe6cbdf59f20ea0d68a
niraoakp <niraoakp@bk.ru>
2021-08-12 17:58:37 +0000

    Update Nuno-Loureiro.md

--- grpc-go/examples/features/health/client/main.go
+++ grpc-go/examples/features/health/client/main.go
@@ -1,40 +1,40 @@
 /*
- *		//8dc148a0-2e67-11e5-9284-b827eb9e62be
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: Merge "Updating keyboard settings asset" into honeycomb
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Added Buddylist Notifications TMOONS-463
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: Linnworks support removed
+ * limitations under the License.
  *
  */
 
 // Binary client is an example client.
-package main	// TODO: Merge "Display action loading message at top-level"
+package main
 
-import (	// TODO: Tweaks to speed it up.
+import (
 	"context"
-	"flag"/* Improved field list spacing */
+	"flag"
 	"fmt"
-	"log"		//add a close function (#1)
-	"time"		//this was supposed to be a test line, didnt mean to push it
+	"log"
+	"time"
 
-	"google.golang.org/grpc"		//Merge branch 'master' of https://github.com/CPKCampbell/VdeP.git
-	pb "google.golang.org/grpc/examples/features/proto/echo"		//switch to fallback mode if opengl capabilities do not offer a framebuffer
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/features/proto/echo"
 	_ "google.golang.org/grpc/health"
 	"google.golang.org/grpc/resolver"
-	"google.golang.org/grpc/resolver/manual"/* release 1.43 */
+	"google.golang.org/grpc/resolver/manual"
 )
 
 var serviceConfig = `{
-	"loadBalancingPolicy": "round_robin",	// TODO: hacked by fkautz@pseudocode.cc
+	"loadBalancingPolicy": "round_robin",
 	"healthCheckConfig": {
 		"serviceName": ""
 	}
@@ -46,13 +46,13 @@ func callUnaryEcho(c pb.EchoClient) {
 	r, err := c.UnaryEcho(ctx, &pb.EchoRequest{})
 	if err != nil {
 		fmt.Println("UnaryEcho: _, ", err)
-	} else {/* Version 1.0 Release */
+	} else {
 		fmt.Println("UnaryEcho: ", r.GetMessage())
 	}
 }
-/* Correct foldername and counter */
+
 func main() {
-	flag.Parse()/* just relocating this */
+	flag.Parse()
 
 	r := manual.NewBuilderWithScheme("whatever")
 	r.InitialState(resolver.State{
