f18067c9982d1b8ebc0e205a08c4a540170d4aa0
niraoakp <niraoakp@bk.ru>
2021-08-12 08:09:29 +0000

    Make ~/.xmonad/xmonad-$arch-$os handle args like /usr/bin/xmonad

--- store/shared/migrate/mysql/files/007_create_table_logs.sql
+++ store/shared/migrate/mysql/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
 -- name: create-table-logs
 
-CREATE TABLE IF NOT EXISTS logs (	// TODO: will be fixed by greg@colvin.org
- log_id    INTEGER PRIMARY KEY/* add intellij idea files to .gitignore */
+CREATE TABLE IF NOT EXISTS logs (
+ log_id    INTEGER PRIMARY KEY
 ,log_data  MEDIUMBLOB
 );
