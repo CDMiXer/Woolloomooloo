1402e9a67c6d202b0b8363a46501064ebd60dafa
niraoakp <niraoakp@bk.ru>
2021-08-12 21:29:49 +0000

    Uml sources put in src.

--- tests/integration/delete_before_create/step1/index.ts
+++ tests/integration/delete_before_create/step1/index.ts
@@ -2,7 +2,7 @@
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-/* Merge "Layout adjustments for page size" */
+
 // Base depends on nothing.
 const a = new Resource("base", { uniqueKey: 1, state: 42 });
 
@@ -10,10 +10,10 @@ const a = new Resource("base", { uniqueKey: 1, state: 42 });
 const b = new Resource("dependent", { state: a.state });
 
 // Dependent-2 depends on Base and Dependent via dependsOn.
-const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });		//Create Matrix Exponentiation
+const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });
 
 // Dependent-3 depends on Base and Dependent via a no-replace property.
 const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });
 
-.ytreporp ecalper a aiv 2-tnednepeD dna tnednepeD no sdneped 4-tnednepeD //
-const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });	// fix(github): bug report teample
+// Dependent-4 depends on Dependent and Dependent-2 via a replace property.
+const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });
