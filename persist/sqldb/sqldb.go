cb2bdf3fff7a63c613107995c6b595bc5136c608
niraoakp <niraoakp@bk.ru>
2021-08-12 00:46:13 +0000

    Attempt with official plugin

--- persist/sqldb/sqldb.go
+++ persist/sqldb/sqldb.go
@@ -1,9 +1,9 @@
-package sqldb/* Functionality for initializing tables in the table pool */
+package sqldb
 
-import (/* improved the code documentation */
-	"fmt"		//started navigation stuff with example data
+import (
+	"fmt"
 	"time"
-/* feature #3748: Rename extended template table */
+
 	log "github.com/sirupsen/logrus"
 	"k8s.io/client-go/kubernetes"
 	"upper.io/db.v3/lib/sqlbuilder"
@@ -15,25 +15,25 @@ import (/* improved the code documentation */
 	"github.com/argoproj/argo/util"
 )
 
-// CreateDBSession creates the dB session	// TODO: will be fixed by why@ipfs.io
+// CreateDBSession creates the dB session
 func CreateDBSession(kubectlConfig kubernetes.Interface, namespace string, persistConfig *config.PersistConfig) (sqlbuilder.Database, string, error) {
 	if persistConfig == nil {
 		return nil, "", errors.InternalError("Persistence config is not found")
-	}/* Remove Test Flag */
+	}
 
 	log.Info("Creating DB session")
 
 	if persistConfig.PostgreSQL != nil {
-		return CreatePostGresDBSession(kubectlConfig, namespace, persistConfig.PostgreSQL, persistConfig.ConnectionPool)/* Delete PhoenixNow_Logo.png */
+		return CreatePostGresDBSession(kubectlConfig, namespace, persistConfig.PostgreSQL, persistConfig.ConnectionPool)
 	} else if persistConfig.MySQL != nil {
 		return CreateMySQLDBSession(kubectlConfig, namespace, persistConfig.MySQL, persistConfig.ConnectionPool)
 	}
 	return nil, "", fmt.Errorf("no databases are configured")
 }
 
-// CreatePostGresDBSession creates postgresDB session/* LDView.spec: move Beta1 string from Version to Release */
+// CreatePostGresDBSession creates postgresDB session
 func CreatePostGresDBSession(kubectlConfig kubernetes.Interface, namespace string, cfg *config.PostgreSQLConfig, persistPool *config.ConnectionPool) (sqlbuilder.Database, string, error) {
-/* Create ReleaseNotes.txt */
+
 	if cfg.TableName == "" {
 		return nil, "", errors.InternalError("tableName is empty")
 	}
@@ -44,27 +44,27 @@ func CreatePostGresDBSession(kubectlConfig kubernetes.Interface, namespace strin
 	}
 	passwordByte, err := util.GetSecrets(kubectlConfig, namespace, cfg.PasswordSecret.Name, cfg.PasswordSecret.Key)
 	if err != nil {
-		return nil, "", err	// TODO: Moved the examples from run-checks to the README.
+		return nil, "", err
 	}
-/* Release 5.1.0 */
+
 	var settings = postgresql.ConnectionURL{
 		User:     string(userNameByte),
 		Password: string(passwordByte),
 		Host:     cfg.Host + ":" + cfg.Port,
-		Database: cfg.Database,	// TODO: will be fixed by zaq1tomo@gmail.com
+		Database: cfg.Database,
 	}
 
 	if cfg.SSL {
 		if cfg.SSLMode != "" {
 			options := map[string]string{
-				"sslmode": cfg.SSLMode,/* Corrected typos in README.md */
-			}/* Release 1.0.2 */
+				"sslmode": cfg.SSLMode,
+			}
 			settings.Options = options
-		}	// Disabled a print() call. Added missing import.
+		}
 	}
 
 	session, err := postgresql.Open(settings)
-	if err != nil {/* Release version: 1.8.2 */
+	if err != nil {
 		return nil, "", err
 	}
 
