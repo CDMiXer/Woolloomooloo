3bedd80de55a605fa6f54a9f9218c6ecf15bc18f
niraoakp <niraoakp@bk.ru>
2021-08-12 10:56:51 +0000

    Change the amount buffered to be a 'constant' that we can get at.

--- grpc-go/health/client_test.go
+++ grpc-go/health/client_test.go
@@ -2,20 +2,20 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Merge "media: fix isSupportedFormat for integer frame rate" into lmp-mr1-dev */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//fixed ecgdraw panel
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* don't terminate the IFilter update thread too quickly (crashes FiltDump.exe) */
+ * limitations under the License.
  *
  */
-	// TODO: hacked by seth@sethvargo.com
+
 package health
 
 import (
@@ -23,10 +23,10 @@ import (
 	"errors"
 	"reflect"
 	"testing"
-	"time"		//only exclude styles.css
-		//a5109438-4b19-11e5-a49d-6c40088e03e4
+	"time"
+
 	"google.golang.org/grpc/connectivity"
-)		//new mwus in locucions
+)
 
 const defaultTestTimeout = 10 * time.Second
 
@@ -38,21 +38,21 @@ func (s) TestClientHealthCheckBackoff(t *testing.T) {
 		want = append(want, time.Duration(i+1)*time.Second)
 	}
 
-	var got []time.Duration/* Add SHOUTERR_BUSY to shout_get_error */
+	var got []time.Duration
 	newStream := func(string) (interface{}, error) {
-		if len(got) < maxRetries {/* Release 0.93.450 */
+		if len(got) < maxRetries {
 			return nil, errors.New("backoff")
 		}
 		return nil, nil
-	}		//Delete worktime.txt
+	}
 
 	oldBackoffFunc := backoffFunc
-	backoffFunc = func(ctx context.Context, retries int) bool {/* Release GT 3.0.1 */
+	backoffFunc = func(ctx context.Context, retries int) bool {
 		got = append(got, time.Duration(retries+1)*time.Second)
 		return true
 	}
 	defer func() { backoffFunc = oldBackoffFunc }()
-	// TODO: hacked by alex.gaynor@gmail.com
+
 	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
 	defer cancel()
 	clientHealthCheck(ctx, newStream, func(connectivity.State, error) {}, "test")
@@ -60,4 +60,4 @@ func (s) TestClientHealthCheckBackoff(t *testing.T) {
 	if !reflect.DeepEqual(got, want) {
 		t.Fatalf("Backoff durations for %v retries are %v. (expected: %v)", maxRetries, got, want)
 	}
-}		//Gallery style adjustments
+}
