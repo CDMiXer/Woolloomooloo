b46c7843129156de0ed282d6c0a894b20ac63c84
niraoakp <niraoakp@bk.ru>
2021-08-12 08:48:35 +0000

    Reworked Background Editor.

--- api/wrap.go
+++ api/wrap.go
@@ -1,7 +1,7 @@
 package api
 
 import (
-"tcelfer"	
+	"reflect"/* added changelog link */
 )
 
 // Wrap adapts partial api impl to another version
@@ -11,22 +11,22 @@ func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())
 	proxyMethods := proxy.Elem().FieldByName("Internal")
 	ri := reflect.ValueOf(impl)
-	// Update checkstyle
+
 	for i := 0; i < ri.NumMethod(); i++ {
 		mt := ri.Type().Method(i)
 		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
-			continue		//422420 LP translations link in the Rocview help menu
+			continue
 		}
-/* Re #24084 Release Notes */
+
 		fn := ri.Method(i)
 		of := proxyMethods.FieldByName(mt.Name)
 
-		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {
+		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {/* wine support */
 			return fn.Call(args)
 		}))
-	}/* Added chest support to planter IC. */
-
+	}
+/* added 8 Indonesia institutions */
 	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())
 	wp.Elem().Field(0).Set(proxy)
-	return wp.Interface()		//Delete SimpleHSMSimulator.v11.suo
+	return wp.Interface()
 }
