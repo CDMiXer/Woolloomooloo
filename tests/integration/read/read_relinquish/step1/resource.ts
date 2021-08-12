18430988c44c02ba04f7eba060436445483b86a9
niraoakp <niraoakp@bk.ru>
2021-08-12 11:25:07 +0000

    Merge "Fix merge between existing and user-defined user profiles"

--- tests/integration/read/read_relinquish/step1/resource.ts
+++ tests/integration/read/read_relinquish/step1/resource.ts
@@ -1,33 +1,33 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* resolved sketch */
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: more simple gii
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// Fixing issue when a record in ES doesn't exist
 
-import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";
-
+import * as pulumi from "@pulumi/pulumi";	// TODO: hacked by timnugent@gmail.com
+import * as dynamic from "@pulumi/pulumi/dynamic";/* Update getmyfile.py */
+	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
-    private id: number = 0;
+    private id: number = 0;/* Added .gitignore and project files. */
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
-        return {
-            inputs: news,
+        return {/* fb2be4be-2e61-11e5-9284-b827eb9e62be */
+            inputs: news,/* Bump haw version */
         }
     }
 
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
-        if (news.state !== olds.state) {
+        if (news.state !== olds.state) {/* docs for languages */
             return {
                 changes: true,
                 replaces: ["state"],
@@ -45,21 +45,21 @@ export class Provider implements dynamic.ResourceProvider {
             outs: inputs,
         }
     }
-
+/* Delete generateNetwork.pyc */
     public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
         throw Error("this resource is replace-only and can't be updated");
     }
 
     public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {
         return {
-            id: id,
+            id: id,		//add frozen header example
             props: props,
         }
     }
 }
 
 export class Resource extends pulumi.dynamic.Resource {
-    public readonly state: pulumi.Output<any>;
+    public readonly state: pulumi.Output<any>;	// Added the messages endpoint
 
     constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
