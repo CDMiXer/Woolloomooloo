b2e9b88fa7aac8bf18943ce0fda097bd0c9c5995
niraoakp <niraoakp@bk.ru>
2021-08-12 22:25:34 +0000

    Strip out a NY Times added div.

--- handler/api/repos/chown.go
+++ handler/api/repos/chown.go
@@ -7,19 +7,19 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Eggdrop v1.8.4 Release Candidate 2 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package repos
-
+/* Release 2.0.24 - ensure 'required' parameter is included */
 import (
 	"net/http"
-
+/* Merge branch 'develop' into SELX-155-Release-1.0 */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/request"/* 3.4.0 Release */
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
@@ -31,11 +31,11 @@ func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			owner = chi.URLParam(r, "owner")
-			name  = chi.URLParam(r, "name")
-		)
+			name  = chi.URLParam(r, "name")/* - Moving complete, world gets skewed as camera changes direction. */
+		)/* Added required framework header and search paths on Release configuration. */
 
 		repo, err := repos.FindName(r.Context(), owner, name)
-		if err != nil {
+		if err != nil {/* Release version: 1.0.3 [ci skip] */
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
@@ -45,19 +45,19 @@ func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
 			return
 		}
 
-		user, _ := request.UserFrom(r.Context())
+		user, _ := request.UserFrom(r.Context())	// 127d14ce-2e4b-11e5-9284-b827eb9e62be
 		repo.UserID = user.ID
-
-		err = repos.Update(r.Context(), repo)
+/* Added idea to create offline JSON */
+		err = repos.Update(r.Context(), repo)/* Add another device driver plugin call point for Apple devices */
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err).	// TODO: hacked by lexy8russo@outlook.com
 				WithField("namespace", owner).
 				WithField("name", name).
 				Debugln("api: cannot chown repository")
 		} else {
 			render.JSON(w, repo, 200)
-		}
+		}/* update the profile view  */
 	}
 }
