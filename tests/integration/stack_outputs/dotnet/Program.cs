a4b45bc8967c36e1229da96c7f0b2c04f42a707d
niraoakp <niraoakp@bk.ru>
2021-08-12 00:17:13 +0000

    Testing "note" formatting for index.rst

--- tests/integration/stack_outputs/dotnet/Program.cs
+++ tests/integration/stack_outputs/dotnet/Program.cs
@@ -1,20 +1,20 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-		//add gets for TEMPERATURE and DESIGN_CAP
-using System.Collections.Generic;/* Fixes tabs. */
+
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using Pulumi;		//Rerender cells after the widget manager has the widget model info.
+using Pulumi;
 
 class Program
 {
-    static Task<int> Main(string[] args)	// TODO: Table node children's order fixed.
-    {/* provide autoconf check file for curses */
-        return Deployment.RunAsync(() => 
-        {/* SObject .fields method. */
+    static Task<int> Main(string[] args)
+    {
+        return Deployment.RunAsync(() => /* Update AnalyzerReleases.Unshipped.md */
+        {
             return new Dictionary<string, object>
             {
                 {  "xyz", "ABC" },
                 {  "foo", 42 },
-            };/* Delete ~$ppcast.xml */
+            };
         });
     }
-}	// TODO: hacked by mikeal.rogers@gmail.com
+}/* Change Bomar Road from Local to Major Collector */
