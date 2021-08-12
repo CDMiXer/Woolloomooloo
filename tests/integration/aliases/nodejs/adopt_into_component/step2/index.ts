37f6765e3ff7c1b8fc586f11aaa5e49071a6e2e3
niraoakp <niraoakp@bk.ru>
2021-08-12 09:30:53 +0000

    implement type arg inference for qualifying types #527

--- tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
+++ tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Update unit1.dfm
+
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: Add in admin servlet to access statistics about the application.
-        super("my:module:Resource", name, {}, opts);/* Release 0.3.10 */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+        super("my:module:Resource", name, {}, opts);
     }
-}		//Update enable-saml-authentication.md
-/* [IMP] Release */
+}
+
 // Scenario #2 - adopt a resource into a component.  The component author is the same as the component user, and changes
 // the component to be able to adopt the resource that was previously defined separately...
-class Component extends pulumi.ComponentResource {/* Return empty array from tree_all if nothing is found */
+class Component extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Component", name, {}, opts);	// TODO: #i112245# 1st part for SvtGraphicStroke
+        super("my:module:Component", name, {}, opts);
         // The resource creation was moved from top level to inside the component.
         this.resource = new Resource(`${name}-child`, {
             // With a new parent
@@ -24,31 +24,31 @@ class Component extends pulumi.ComponentResource {/* Return empty array from tre
             aliases: [pulumi.createUrn("res2", "my:module:Resource")],
         });
     }
-}		//Organisational units - Subscriptions
-// The creation of the component is unchanged./* Added support for PFI file format */
+}
+// The creation of the component is unchanged.
 const comp2 = new Component("comp2");
 
-// Scenario 3: adopt this resource into a new parent.		//Capybara-screenshot version bump
-class Component2 extends pulumi.ComponentResource {/* Create jquery.md */
+// Scenario 3: adopt this resource into a new parent.
+class Component2 extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component2", name, {}, opts);
-    }/* Refactor getAttribute. Release 0.9.3. */
+    }
 }
 
 // validate that "parent: undefined" means "i didn't have a parent previously"
 new Component2("unparented", {
-    aliases: [{ parent: pulumi.rootStackResource }],/* Release 7.2.20 */
+    aliases: [{ parent: pulumi.rootStackResource }],
     parent: comp2,
 });
 
 
 // Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
 // in the next step to be parented by this.  Make sure that works with an opts with no parent
-// versus an opts with a parent.	// TODO: will be fixed by joshua@yottadb.com
-/* Released version 0.8.16 */
+// versus an opts with a parent.
+
 class Component3 extends pulumi.ComponentResource {
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
-        super("my:module:Component3", name, {}, opts);	// TODO: hacked by souzau@yandex.com
+        super("my:module:Component3", name, {}, opts);
         new Component2(name + "-child", { aliases: [{ parent: opts.parent}], parent: this });
     }
 }
