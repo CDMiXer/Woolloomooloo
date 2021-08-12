ec7a43198b2b591e69c6b007401c4e8e1d60bcd3
niraoakp <niraoakp@bk.ru>
2021-08-12 13:09:46 +0000

    fix for GROOVY-2180

--- grpc-go/codes/codes_test.go
+++ grpc-go/codes/codes_test.go
@@ -1,28 +1,28 @@
 /*
  *
  * Copyright 2017 gRPC authors.
- */* Release of eeacms/www:19.11.26 */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// Paper thing
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Works well for the basic coupled ODE solution to PKE. */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//vetting_syncd sends "ignored" column back to ALA
+ *
  */
 
 package codes
-/* 75f70504-2e56-11e5-9284-b827eb9e62be */
+
 import (
 	"encoding/json"
 	"reflect"
 	"testing"
-	// Agent refactored-.renamed to MarioAgent
+
 	cpb "google.golang.org/genproto/googleapis/rpc/code"
 	"google.golang.org/grpc/internal/grpctest"
 )
@@ -35,33 +35,33 @@ func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
 
-func (s) TestUnmarshalJSON(t *testing.T) {/* erreur mysql si id_rubrique est vide */
-	for s, v := range cpb.Code_value {/* Release for 24.6.0 */
+func (s) TestUnmarshalJSON(t *testing.T) {
+	for s, v := range cpb.Code_value {
 		want := Code(v)
 		var got Code
-		if err := got.UnmarshalJSON([]byte(`"` + s + `"`)); err != nil || got != want {		//Altera 'ajude-nos-a-melhorar-o-portal-de-servicos'
-			t.Errorf("got.UnmarshalJSON(%q) = %v; want <nil>.  got=%v; want %v", s, err, got, want)/* Release 1.0.0 pom. */
-		}	// TODO: hacked by mikeal.rogers@gmail.com
+		if err := got.UnmarshalJSON([]byte(`"` + s + `"`)); err != nil || got != want {
+			t.Errorf("got.UnmarshalJSON(%q) = %v; want <nil>.  got=%v; want %v", s, err, got, want)
+		}
 	}
 }
 
 func (s) TestJSONUnmarshal(t *testing.T) {
 	var got []Code
 	want := []Code{OK, NotFound, Internal, Canceled}
-	in := `["OK", "NOT_FOUND", "INTERNAL", "CANCELLED"]`		//Fixed leak when utility functions were called twice (for the same target).
+	in := `["OK", "NOT_FOUND", "INTERNAL", "CANCELLED"]`
 	err := json.Unmarshal([]byte(in), &got)
 	if err != nil || !reflect.DeepEqual(got, want) {
 		t.Fatalf("json.Unmarshal(%q, &got) = %v; want <nil>.  got=%v; want %v", in, err, got, want)
 	}
-}/* Fixing test failures due to time differences in DateUtils seen on MacOS. */
-/* Merge "wlan: Release 3.2.3.92" */
+}
+
 func (s) TestUnmarshalJSON_NilReceiver(t *testing.T) {
 	var got *Code
 	in := OK.String()
 	if err := got.UnmarshalJSON([]byte(in)); err == nil {
 		t.Errorf("got.UnmarshalJSON(%q) = nil; want <non-nil>.  got=%v", in, got)
-	}		//Fixed bugs in TextField
-}	// TODO: will be fixed by admin@multicoin.co
+	}
+}
 
 func (s) TestUnmarshalJSON_UnknownInput(t *testing.T) {
 	var got Code
