04ad9d9cd6572f530a100907237d54f3557c124f
niraoakp <niraoakp@bk.ru>
2021-08-12 02:40:57 +0000

    Fix Pimple unset

--- handler/api/events/logs.go
+++ handler/api/events/logs.go
@@ -1,49 +1,49 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Changed frames to dialogs
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth      //
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Create gdb.txt */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package events
-/* Remove forced CMAKE_BUILD_TYPE Release for tests */
+
 import (
 	"context"
-	"encoding/json"/* added compatibility tag */
+	"encoding/json"
 	"io"
-	"net/http"/* Added Release 1.1.1 */
-	"strconv"	// TODO: Merge "Add tests for invalidating archives on change."
-	"time"/* Merge branch 'master' into safe-audio-component-updates */
+	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"/* fixed typo in copyright header */
+	"github.com/go-chi/chi"
 )
 
 // HandleLogStream creates an http.HandlerFunc that streams builds logs
 // to the http.Response in an event stream format.
 func HandleLogStream(
-	repos core.RepositoryStore,/* Merge branch 'updateAbout' */
+	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
 	steps core.StepStore,
-	stream core.LogStream,/* Added test cases(8) for UCRCode/PropertyDescription Rule 268. */
+	stream core.LogStream,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)/* Update sys_tools.pks */
+		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
-		if err != nil {/* Release version 2.1.5.RELEASE */
+		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
@@ -57,9 +57,9 @@ func HandleLogStream(
 			render.BadRequest(w, err)
 			return
 		}
-		repo, err := repos.FindName(r.Context(), namespace, name)/* Increased nlink buffer size to support x64 platform */
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)/* 1.0.0 Release. */
+			render.NotFound(w, err)
 			return
 		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
@@ -73,7 +73,7 @@ func HandleLogStream(
 			return
 		}
 		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)
-		if err != nil {/* more word classes */
+		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
