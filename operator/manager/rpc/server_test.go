4645b2af44129c5db7a836a891ecbcc4e16f9be7
niraoakp <niraoakp@bk.ru>
2021-08-12 02:00:40 +0000

    Add Manning NLP lecture link

--- operator/manager/rpc/server_test.go
+++ operator/manager/rpc/server_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by vyzo@hackzen.org
 // that can be found in the LICENSE file.
 
-sso! dliub+ //
-
+// +build !oss/* Fix issue 69. */
+/* fb126a84-2e52-11e5-9284-b827eb9e62be */
 package rpc
