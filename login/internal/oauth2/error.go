8c5dec3a10e9823b69a34df82b4ab73a6393ddae
niraoakp <niraoakp@bk.ru>
2021-08-12 09:16:48 +0000

    Add file index.html for ckeditor

--- login/internal/oauth2/error.go
+++ login/internal/oauth2/error.go
@@ -1,22 +1,22 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved./* Release v0.5.8 */
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package oauth2
 
 import "errors"
-/* Release of eeacms/energy-union-frontend:v1.4 */
+
 // ErrState indicates the state is invalid.
 var ErrState = errors.New("Invalid state")
 
-// Error represents a failed authorization request./* Correção mínima em Release */
+// Error represents a failed authorization request.
 type Error struct {
 	Code string `json:"error"`
 	Desc string `json:"error_description"`
 }
 
 // Error returns the string representation of an
-// authorization error./* Deleted CtrlApp_2.0.5/Release/StdAfx.obj */
-func (e *Error) Error() string {	// TODO: hacked by hugomrdias@gmail.com
+// authorization error.
+func (e *Error) Error() string {
 	return e.Code + ": " + e.Desc
 }
