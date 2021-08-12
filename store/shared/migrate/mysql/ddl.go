8e98ec0712e5828af308c5504e14797b807dc2fc
niraoakp <niraoakp@bk.ru>
2021-08-12 16:19:16 +0000

    Added MessageList classes back to support custom themes that have them.

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* drop the FilterPatterns type */
 
-// +build !oss
+// +build !oss/* Use consistent naming for method to remove EAs */
 
 package mysql
 
