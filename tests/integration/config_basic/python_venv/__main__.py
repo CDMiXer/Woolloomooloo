cd86ff59612c4d84095a03bb496845aa88808a97
niraoakp <niraoakp@bk.ru>
2021-08-12 00:38:54 +0000

    #58: don't show delete link when channel is locked

--- tests/integration/config_basic/python_venv/__main__.py
+++ tests/integration/config_basic/python_venv/__main__.py
@@ -1,15 +1,15 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+		//fix su KLinkedList ed implementata KPairList
 import pulumi
-	// TODO: Delete wolfsheep_markov_run.py
-# Just test that basic config works./* [artifactory-release] Release version 3.5.0.RC2 */
+
+# Just test that basic config works.
 config = pulumi.Config('config_basic_py')
 
-# This value is plaintext and doesn't require encryption.	// Issue 67:	Add generator tests for operations calls without braces
+# This value is plaintext and doesn't require encryption.
 value = config.require('aConfigValue')
 assert value == 'this value is a Pythonic value'
-
-# This value is a secret and is encrypted using the passphrase `supersecret`./* bb6677d2-2e74-11e5-9284-b827eb9e62be */
+/* rev 802895 */
+# This value is a secret and is encrypted using the passphrase `supersecret`.
 secret = config.require('bEncryptedSecret')
 assert secret == 'this super Pythonic secret is encrypted'
 
@@ -17,37 +17,37 @@ test_data = [
     {
         'key': 'outer',
         'expected_json': '{"inner":"value"}',
-        'expected_object': { 'inner': 'value' }/* Release 2.0.5 support JSONP support in json_callback parameter */
-    },
-    {		//set_help_text function to add status bar help texts to any window
-        'key': 'names',
-        'expected_json': '["a","b","c","super secret name"]',
-        'expected_object': ['a', 'b', 'c', 'super secret name']
-    },
-    {	// TODO: added info about multi column responsive sample
-        'key': 'servers',
-        'expected_json': '[{"host":"example","port":80}]',
-        'expected_object': [{ 'host': 'example', 'port': 80 }]
+        'expected_object': { 'inner': 'value' }	// TODO: WTF is TypeError: unhashable type - fixed anyway
     },
     {
-        'key': 'a',
-        'expected_json': '{"b":[{"c":true},{"c":false}]}',
-        'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }	// Delete metaprog.py
+        'key': 'names',	// TokenTraderFactoryCheckInvalidGNT
+        'expected_json': '["a","b","c","super secret name"]',
+        'expected_object': ['a', 'b', 'c', 'super secret name']/* Update Rtdf.R */
     },
-    {		//Fixing a comple of issues flagged by the static analyzer.
-        'key': 'tokens',
+    {
+        'key': 'servers',/* added subheading so it doesn't look quite as horrible */
+        'expected_json': '[{"host":"example","port":80}]',
+        'expected_object': [{ 'host': 'example', 'port': 80 }]/* Released GoogleApis v0.1.5 */
+    },
+    {
+        'key': 'a',/* New translations haxchi.txt (Russian) */
+        'expected_json': '{"b":[{"c":true},{"c":false}]}',
+        'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }
+    },
+    {
+        'key': 'tokens',	// Delete multimedia.svg
         'expected_json': '["shh"]',
         'expected_object': ['shh']
     },
     {
-        'key': 'foo',/* 0.7.0.26 Release */
+        'key': 'foo',
         'expected_json': '{"bar":"don\'t tell"}',
         'expected_object': { 'bar': "don't tell" }
-    }
+    }		//Design philosophy details
 ]
 
 for test in test_data:
     json = config.require(test['key'])
     obj = config.require_object(test['key'])
-    assert json == test['expected_json']
+    assert json == test['expected_json']	// Merge "Fix missing fields in _check_subnet_delete method"
     assert obj == test['expected_object']
