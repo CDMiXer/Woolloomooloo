ce7a2bd17296f036800f9dec81bfdbc5d49fa135
niraoakp <niraoakp@bk.ru>
2021-08-12 09:14:27 +0000

    Adds installation and usage instructions to README

--- tests/integration/empty/gorun_main/gorun_main_src/main.go
+++ tests/integration/empty/gorun_main/gorun_main_src/main.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//add designer pages
-package main		//DEbugging printout statements
 
+package main
+/* Delete documentSetCreationJobs when documentSets are deleted */
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
+)		//Code cleanedup patch-7
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		return nil
 	})
-}/* Merge "Release 4.0.10.26 QCACLD WLAN Driver" */
+}
