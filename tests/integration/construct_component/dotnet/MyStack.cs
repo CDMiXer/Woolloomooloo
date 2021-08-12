c5dc0224d7c739744df0c7d99d2af66d605b3772
niraoakp <niraoakp@bk.ru>
2021-08-12 11:53:21 +0000

    Merge "Update use of open() in object API"

--- tests/integration/construct_component/dotnet/MyStack.cs
+++ tests/integration/construct_component/dotnet/MyStack.cs
@@ -1,13 +1,13 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-		//Merge "msm: mdss: Add high_perf flag to run H/W at highest capability"
-using Pulumi;	// TODO: added default_setup_sp (still correct lsi prefix)
+
+using Pulumi;
 
 class MyStack : Stack
-{/* Release of eeacms/www:20.6.24 */
-    public MyStack()/* 0.9.10 Release. */
-    {		//Create README.md and updated installation ntes
+{
+    public MyStack()
+    {
         var componentA = new Component("a", new ComponentArgs { Echo = 42 });
-        var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });/* Set mergeinfo property when pushing merges. */
+        var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });
         var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });
     }
 }
