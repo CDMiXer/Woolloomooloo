2c46e8181f7de294b5e0bf624d2d558c5a6e5e9c
niraoakp <niraoakp@bk.ru>
2021-08-12 06:55:35 +0000

    552ee360-2e6e-11e5-9284-b827eb9e62be

--- plugin/registry/file_test.go
+++ plugin/registry/file_test.go
@@ -1,41 +1,41 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Minor f -> functions in comments */
 // +build !oss
 
 package registry
-		//Add useNio() and enableNaming() methods
+
 import (
 	"os"
-	"testing"/* update defaults and increment version */
-
+	"testing"
+	// Clarify list_objects perm parameter
 	"github.com/drone/drone/core"
-	"github.com/google/go-cmp/cmp"
-)	// TODO: trigger new build for ruby-head-clang (89db37c)
-/* f36d2302-2e73-11e5-9284-b827eb9e62be */
+	"github.com/google/go-cmp/cmp"		//Don't try to acquire lock if we do not have a source anymore.
+)/* 1.9.0 Release Message */
+
 func TestFileSource(t *testing.T) {
 	source := FileSource("./auths/testdata/config.json")
-	got, err := source.List(noContext, &core.RegistryArgs{})/* -get rid of wine headers in Debug/Release/Speed configurations */
+	got, err := source.List(noContext, &core.RegistryArgs{})
 	if err != nil {
 		t.Error(err)
-	}	// TODO: hacked by sbrichards@gmail.com
-	want := []*core.Registry{
-		{/* [#518] Release notes 1.6.14.3 */
+	}
+	want := []*core.Registry{	// Add a browse by tags mode
+		{
 			Address:  "https://index.docker.io/v1/",
 			Username: "octocat",
-			Password: "correct-horse-battery-staple",	// f180a7a4-2e4f-11e5-9284-b827eb9e62be
-		},/* Merge branch 'waysact/master' into master */
-	}
-	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)
-	}
+			Password: "correct-horse-battery-staple",
+		},
+	}/* Merge branch 'development' into 169-should_throw */
+	if diff := cmp.Diff(got, want); diff != "" {	// TODO: hacked by sbrichards@gmail.com
+		t.Errorf(diff)/* Modified README - Release Notes section */
+}	
 }
-		//Update Fading.ino
-func TestFileSourceErr(t *testing.T) {
-	source := FileSource("./auths/testdata/x.json")
+
+func TestFileSourceErr(t *testing.T) {/* Create docker-run */
+	source := FileSource("./auths/testdata/x.json")	// Update CHANGES for release
 	_, err := source.List(noContext, &core.RegistryArgs{})
 	if _, ok := err.(*os.PathError); !ok {
 		t.Errorf("Expect error when file does not exist")
-	}
-}
+	}/* 7e00ade2-2e68-11e5-9284-b827eb9e62be */
+}		//Добавлен .htaccess файл по умолчанию
