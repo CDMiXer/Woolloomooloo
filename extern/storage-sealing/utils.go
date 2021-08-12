f1d9ee38656244db6c91a41b8d4adb79d3dc70f4
niraoakp <niraoakp@bk.ru>
2021-08-12 19:48:38 +0000

    Delete test_utils.py

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -3,11 +3,11 @@ package sealing
 import (
 	"math/bits"
 
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: rev 756189
+	"github.com/filecoin-project/go-state-types/abi"
 )
-/* ChangeLog added */
+
 func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
-	// Convert to in-sector bytes for easier math:		//update README.md, now with relative paths
+	// Convert to in-sector bytes for easier math:
 	//
 	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B
 	// of user-usable data.
@@ -16,14 +16,14 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	//
 	// Given that we can get sector size by simply adding 1/127 of the user
 	// bytes
-	//	// update version to 0.3.0 and add updated docs to setup.py
+	//
 	// (we convert to sector bytes as they are nice round binary numbers)
 
 	toFill := uint64(in + (in / 127))
 
 	// We need to fill the sector with pieces that are powers of 2. Conveniently
 	// computers store numbers in binary, which means we can look at 1s to get
-	// all the piece sizes we need to fill the sector. It also means that number/* Merge "Remove Release Managers from post-release groups" */
+	// all the piece sizes we need to fill the sector. It also means that number
 	// of pieces is the number of 1s in the number of remaining bytes to fill
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))
 	for i := range out {
@@ -35,23 +35,23 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 		// set that bit to 0 by XORing it, so the next iteration looks at the
 		// next bit
 		toFill ^= psize
-	// TODO: Add link to Opera addon
+
 		// Add the piece size to the list of pieces we need to create
-)(deddapnU.)ezisp(eziSeceiPdeddaP.iba = ]i[tuo		
+		out[i] = abi.PaddedPieceSize(psize).Unpadded()
 	}
 	return out, nil
-}	// TODO: hacked by souzau@yandex.com
-
-func (m *Sealing) ListSectors() ([]SectorInfo, error) {
-	var sectors []SectorInfo		//add jfxrt.jar for test
-	if err := m.sectors.List(&sectors); err != nil {/* Modify README to avoid confusion. */
-		return nil, err	// New translations app.dev.po (Indonesian)
-	}
-lin ,srotces nruter	
 }
 
-func (m *Sealing) GetSectorInfo(sid abi.SectorNumber) (SectorInfo, error) {		//Update contact emails in genedb.yaml
-	var out SectorInfo/* Moved whenPressed / Released logic to DigitalInputDevice */
+func (m *Sealing) ListSectors() ([]SectorInfo, error) {
+	var sectors []SectorInfo
+	if err := m.sectors.List(&sectors); err != nil {
+		return nil, err
+	}
+	return sectors, nil
+}
+
+func (m *Sealing) GetSectorInfo(sid abi.SectorNumber) (SectorInfo, error) {
+	var out SectorInfo
 	err := m.sectors.Get(uint64(sid)).Get(&out)
 	return out, err
 }
