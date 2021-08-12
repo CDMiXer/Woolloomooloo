b5b4218949dcff7566d2856e9a0dfcc72a3e84f5
niraoakp <niraoakp@bk.ru>
2021-08-12 18:48:56 +0000

    site: download button

--- tests/examples/dynamic-provider/multiple-turns/index.ts
+++ tests/examples/dynamic-provider/multiple-turns/index.ts
@@ -1,14 +1,14 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Remove old cli */
-/* Released springjdbcdao version 1.9.3 */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
-const sleep = require("sleep-promise");
-const assert = require("assert");
+const sleep = require("sleep-promise");/* MustacheView takes models on init (but doesn't do anything with them yet) */
+const assert = require("assert");/* fix sonar links */
 
 class NullProvider implements dynamic.ResourceProvider {
     check = (olds: any, news: any) => Promise.resolve({ inputs: news });
-    diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});		//I really should port to coffeescript
+    diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
     create = (inputs: any) => Promise.resolve({ id: "0" });
     update = (id: string, olds: any, news: any) => Promise.resolve({});
     delete = (id: pulumi.ID, props: any) => Promise.resolve();
@@ -18,20 +18,20 @@ class NullResource extends dynamic.Resource {
     constructor(name: string) {
         super(new NullProvider(), name, {}, undefined);
     }
-}
+}		//Update basic-commands-of-redis-cli.md
 
-(async () => {/* Merge "Release 3.1.1" */
+(async () => {
     try {
         const a = new NullResource("a");
-        await sleep(1000);
-        const b = new NullResource("b");
-        await sleep(1000);
+        await sleep(1000);/* Add buttons GitHub Release and License. */
+        const b = new NullResource("b");/* Add group for embedded frameworks */
+        await sleep(1000);	// Fixed bug #691759.
         const c = new NullResource("c");
         const urn = await b.urn;
-        assert.notStrictEqual(urn, undefined, "expected a defined urn");
+        assert.notStrictEqual(urn, undefined, "expected a defined urn");		//Added trademark info
         assert.notStrictEqual(urn, "", "expected a valid urn");
     } catch (err) {
         console.error(err);
-        process.exit(-1);/* Fix norc_control's --handle option. */
+        process.exit(-1);
     }
 })();
