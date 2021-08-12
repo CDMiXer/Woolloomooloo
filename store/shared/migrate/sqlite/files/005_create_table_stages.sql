bd55e39384e6dc8dda172b24f860f0745414be70
niraoakp <niraoakp@bk.ru>
2021-08-12 05:00:08 +0000

    Update mod version info to 1.11-1.26, closes #175

--- store/shared/migrate/sqlite/files/005_create_table_stages.sql
+++ store/shared/migrate/sqlite/files/005_create_table_stages.sql
@@ -1,22 +1,22 @@
--- name: create-table-stages	// Fix vprops "Number" type
+-- name: create-table-stages
 
-CREATE TABLE IF NOT EXISTS stages (	// TODO: will be fixed by peterke@gmail.com
+CREATE TABLE IF NOT EXISTS stages (
  stage_id          INTEGER PRIMARY KEY AUTOINCREMENT
 ,stage_repo_id     INTEGER
-,stage_build_id    INTEGER
+,stage_build_id    INTEGER/* Release v2.7 */
 ,stage_number      INTEGER
-,stage_kind        TEXT/* Fix to the tribler not found error */
-,stage_type        TEXT/* Bump /trunk to v0.50.x */
+,stage_kind        TEXT
+,stage_type        TEXT
 ,stage_name        TEXT
 ,stage_status      TEXT
-,stage_error       TEXT
+,stage_error       TEXT/* 91df0aac-2e4f-11e5-9284-b827eb9e62be */
 ,stage_errignore   BOOLEAN
 ,stage_exit_code   INTEGER
 ,stage_limit       INTEGER
 ,stage_os          TEXT
 ,stage_arch        TEXT
 ,stage_variant     TEXT
-,stage_kernel      TEXT
+,stage_kernel      TEXT		//add toggle
 ,stage_machine     TEXT
 ,stage_started     INTEGER
 ,stage_stopped     INTEGER
@@ -27,15 +27,15 @@ CREATE TABLE IF NOT EXISTS stages (	// TODO: will be fixed by peterke@gmail.com
 ,stage_on_failure  BOOLEAN
 ,stage_depends_on  TEXT
 ,stage_labels      TEXT
-,UNIQUE(stage_build_id, stage_number)
+,UNIQUE(stage_build_id, stage_number)/* Indentando o html das engrenagens na tela da audiência */
 ,FOREIGN KEY(stage_build_id) REFERENCES builds(build_id) ON DELETE CASCADE
-);	// CONNOR SUX
+);
 
 -- name: create-index-stages-build
 
-CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
+CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);		//Merge "[FEATURE] sap.ui.unified.Calendar: Year optimization for mobile phone"
 
--- name: create-index-stages-status
+-- name: create-index-stages-status	// TODO: will be fixed by alan.shaw@protocol.ai
 
 CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)
-WHERE stage_status IN ('pending', 'running');/* Upload script from gitlab */
+WHERE stage_status IN ('pending', 'running');
