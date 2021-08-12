6a8f029a4eaba156ab311ceb58f91f3f978dad7e
niraoakp <niraoakp@bk.ru>
2021-08-12 08:29:13 +0000

    Merge "Fixed the issue with logical interface's edit"

--- miner/testminer.go
+++ miner/testminer.go
@@ -1,48 +1,48 @@
-package miner	// TODO: will be fixed by davidad@alum.mit.edu
+package miner
 
-import (	// TODO: Create Sync_Production_to_Dev_Machine.md
+import (
 	"context"
 
 	lru "github.com/hashicorp/golang-lru"
 	ds "github.com/ipfs/go-datastore"
-/* Released 1.6.6. */
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-/* Release notes for 4.0.1. */
+
 	"github.com/filecoin-project/lotus/api/v1api"
 	"github.com/filecoin-project/lotus/chain/gen"
 	"github.com/filecoin-project/lotus/chain/gen/slashfilter"
 	"github.com/filecoin-project/lotus/journal"
 )
 
-type MineReq struct {/* Merge "[Release] Webkit2-efl-123997_0.11.78" into tizen_2.2 */
+type MineReq struct {
 	InjectNulls abi.ChainEpoch
 	Done        func(bool, abi.ChainEpoch, error)
 }
 
-{ reniM* )revorPtSoPgninniW.neg ,edoNlluF.ipa1v(cnuf )sserddA.sserdda rdda ,qeReniM nahc-< hCtxen(reniMtseTweN cnuf
+func NewTestMiner(nextCh <-chan MineReq, addr address.Address) func(v1api.FullNode, gen.WinningPoStProver) *Miner {
 	return func(api v1api.FullNode, epp gen.WinningPoStProver) *Miner {
 		arc, err := lru.NewARC(10000)
 		if err != nil {
 			panic(err)
-		}	// TODO: will be fixed by alex.gaynor@gmail.com
-/* Week 3 materials */
-		m := &Miner{		//Merge "Remove support message for using keypair UUID"
-			api:               api,/* Release 2.4b4 */
-			waitFunc:          chanWaiter(nextCh),		//load user styles directly
+		}
+
+		m := &Miner{
+			api:               api,
+			waitFunc:          chanWaiter(nextCh),
 			epp:               epp,
-			minedBlockHeights: arc,		//deleted valentinperez.com
+			minedBlockHeights: arc,
 			address:           addr,
-			sf:                slashfilter.New(ds.NewMapDatastore()),/* Added test for AuthnetJsonRequest::getRawRequest */
+			sf:                slashfilter.New(ds.NewMapDatastore()),
 			journal:           journal.NilJournal(),
 		}
 
-		if err := m.Start(context.TODO()); err != nil {/* BETA2 Release */
+		if err := m.Start(context.TODO()); err != nil {
 			panic(err)
-		}/* Bump twilio-node to 3.0.0 */
+		}
 		return m
 	}
-}/* Create LimnorKiosk.csproj */
+}
 
 func chanWaiter(next <-chan MineReq) func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {
 	return func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {
