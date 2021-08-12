6253cbe2455b2ac66718c69f2d3501f846a0c783
niraoakp <niraoakp@bk.ru>
2021-08-12 10:59:14 +0000

    Remove unneeded grant types

--- service/license/nolimit_oss.go
+++ service/license/nolimit_oss.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: Setting pin mode on pin assignment
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//-updating config file towards forcing use of DV
-ta esneciL eht fo ypoc a niatbo yam uoY //
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//added center parsing stuff
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build nolimit
 // +build oss
-		//Fixed table scroll in Linux GTK.
+
 package license
 
 import (
