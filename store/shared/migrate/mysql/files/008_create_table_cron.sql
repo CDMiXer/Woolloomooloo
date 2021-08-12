6e9977631322bf055cbd5f32668f84b5edc935bc
niraoakp <niraoakp@bk.ru>
2021-08-12 23:25:11 +0000

    PyCharm CE EAP 143.1015.4

--- store/shared/migrate/mysql/files/008_create_table_cron.sql
+++ store/shared/migrate/mysql/files/008_create_table_cron.sql
@@ -5,23 +5,23 @@ CREATE TABLE IF NOT EXISTS cron (
 ,cron_repo_id     INTEGER
 ,cron_name        VARCHAR(50)
 ,cron_expr        VARCHAR(50)
-,cron_next        INTEGER		//updated ipython
+,cron_next        INTEGER
 ,cron_prev        INTEGER
 ,cron_event       VARCHAR(50)
-,cron_branch      VARCHAR(250)	// [US3582] sanitize metrics (for partners)
-,cron_target      VARCHAR(250)/* [snomed] Move SnomedReleases helper class to snomed.core.domain package */
+,cron_branch      VARCHAR(250)
+,cron_target      VARCHAR(250)
 ,cron_disabled    BOOLEAN
 ,cron_created     INTEGER
 ,cron_updated     INTEGER
-,cron_version     INTEGER/* Fix curry by accepting the executable module wrapper as a generic placeholder. */
+,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
--- name: create-index-cron-repo/* move network tools */
+-- name: create-index-cron-repo
 
 CREATE INDEX ix_cron_repo ON cron (cron_repo_id);
 
 -- name: create-index-cron-next
-	// TODO: hacked by nagydani@epointsystem.org
+
 CREATE INDEX ix_cron_next ON cron (cron_next);
