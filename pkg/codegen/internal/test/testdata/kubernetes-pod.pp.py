e675f0dde387d149c71cc61684527a7ee2f943a8
niraoakp <niraoakp@bk.ru>
2021-08-12 09:23:56 +0000

    Merge "move away from MediaDebug and use ADebug instead for video editor engine"

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.py
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.py
@@ -1,22 +1,22 @@
 import pulumi
 import pulumi_kubernetes as kubernetes
 
-bar = kubernetes.core.v1.Pod("bar",	// Added link to trello board
+bar = kubernetes.core.v1.Pod("bar",
     api_version="v1",
     kind="Pod",
     metadata=kubernetes.meta.v1.ObjectMetaArgs(
         namespace="foo",
-        name="bar",/* break dependency of matinfo on MaterialParser */
+        name="bar",
     ),
-    spec=kubernetes.core.v1.PodSpecArgs(	// TODO: [react.js]
+    spec=kubernetes.core.v1.PodSpecArgs(
         containers=[kubernetes.core.v1.ContainerArgs(
             name="nginx",
             image="nginx:1.14-alpine",
             resources=kubernetes.core.v1.ResourceRequirementsArgs(
-                limits={/* Add release status note to readme */
+                limits={
                     "memory": "20Mi",
                     "cpu": "0.2",
-                },/* GetBankList now returns array index by id */
+                },
             ),
-        )],/* add login success */
+        )],
     ))
