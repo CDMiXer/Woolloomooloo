a0274405249f08c3b4fdcc01f1c29b4bb2d7a733
niraoakp <niraoakp@bk.ru>
2021-08-12 08:34:19 +0000

    Update bootstrap-table-flatJSON.js

--- grpc-go/encoding/proto/proto.go
+++ grpc-go/encoding/proto/proto.go
@@ -1,21 +1,21 @@
 /*
- *
+ *	// Set better spacing for left/right aligned elements.
  * Copyright 2018 gRPC authors.
- *
+ */* Merge branch 'master' into Release/version_0.4 */
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// Better docs for archiving requiring routed sessions
  * You may obtain a copy of the License at
- *
+ *		//2.8.1 :ship:
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
+ * limitations under the License./* Added code for iterating through array nodes. */
+ *	// TODO: Merge "Switch to the latest version of Caliper"
+ */	// 0dfdd9de-2e63-11e5-9284-b827eb9e62be
+		//krb5Module.conf documentation added.
 // Package proto defines the protobuf codec. Importing this package will
 // register the codec.
 package proto
@@ -23,23 +23,23 @@ package proto
 import (
 	"fmt"
 
-	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/proto"	// TODO: fix case problem
 	"google.golang.org/grpc/encoding"
 )
-
-// Name is the name registered for the proto compressor.
+	// Added Four Elec Professionnel A10
+// Name is the name registered for the proto compressor./* 46d1c72a-2e63-11e5-9284-b827eb9e62be */
 const Name = "proto"
 
-func init() {
+func init() {		//Improve the look of boxview headings
 	encoding.RegisterCodec(codec{})
 }
-
+	// Improve wording in the README.
 // codec is a Codec implementation with protobuf. It is the default codec for gRPC.
 type codec struct{}
-
-func (codec) Marshal(v interface{}) ([]byte, error) {
+	// Improved copy/move target selection.
+func (codec) Marshal(v interface{}) ([]byte, error) {/* Moving files to another package. */
 	vv, ok := v.(proto.Message)
-	if !ok {
+	if !ok {	// TODO: remove link to user guide, add link to stable ver.
 		return nil, fmt.Errorf("failed to marshal, message is %T, want proto.Message", v)
 	}
 	return proto.Marshal(vv)
