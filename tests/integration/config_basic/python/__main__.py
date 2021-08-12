47fd193c638aeaa3d7f7342dcb631503bf32079d
niraoakp <niraoakp@bk.ru>
2021-08-12 03:45:27 +0000

    fix tokenStartIndex/tokenStopIndex

--- tests/integration/config_basic/python/__main__.py
+++ tests/integration/config_basic/python/__main__.py
@@ -1,26 +1,26 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import pulumi	// TODO: hacked by mail@bitpshr.net
+import pulumi
 
 # Just test that basic config works.
 config = pulumi.Config('config_basic_py')
 
-# This value is plaintext and doesn't require encryption.	// added props 😎
+# This value is plaintext and doesn't require encryption.
 value = config.require('aConfigValue')
 assert value == 'this value is a Pythonic value'
 
-# This value is a secret and is encrypted using the passphrase `supersecret`./* Released 4.2 */
+# This value is a secret and is encrypted using the passphrase `supersecret`.
 secret = config.require('bEncryptedSecret')
 assert secret == 'this super Pythonic secret is encrypted'
 
 test_data = [
     {
-        'key': 'outer',	// TODO: Merge "wlan: Fix for WLAN L2 Reordering issue"
+        'key': 'outer',
         'expected_json': '{"inner":"value"}',
         'expected_object': { 'inner': 'value' }
     },
     {
-        'key': 'names',/* [artifactory-release] Release version 2.0.0.RELEASE */
+        'key': 'names',
         'expected_json': '["a","b","c","super secret name"]',
         'expected_object': ['a', 'b', 'c', 'super secret name']
     },
@@ -30,7 +30,7 @@ test_data = [
         'expected_object': [{ 'host': 'example', 'port': 80 }]
     },
     {
-        'key': 'a',/* Delete a class and its test that is not used */
+        'key': 'a',
         'expected_json': '{"b":[{"c":true},{"c":false}]}',
         'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }
     },
@@ -50,4 +50,4 @@ for test in test_data:
     json = config.require(test['key'])
     obj = config.require_object(test['key'])
     assert json == test['expected_json']
-    assert obj == test['expected_object']/* Create KEGparser_v1.1.sh */
+    assert obj == test['expected_object']
