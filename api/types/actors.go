8c60fe5caa10ec10588d3f648cdf49667738c19c
niraoakp <niraoakp@bk.ru>
2021-08-13 07:02:29 +0000

    Delete Week 1 .docx

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
 package apitypes
 
-import "github.com/filecoin-project/go-state-types/network"
-
+import "github.com/filecoin-project/go-state-types/network"		//Added basic usage information to README
+/* Release version: 0.4.2 */
 type NetworkVersion = network.Version
