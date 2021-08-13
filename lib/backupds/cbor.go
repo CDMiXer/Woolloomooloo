1d6efb130ce31a0b32a40fbd8022af51007bdeb3
niraoakp <niraoakp@bk.ru>
2021-08-13 07:43:19 +0000

    Updated as per standard template.

--- lib/backupds/cbor.go
+++ lib/backupds/cbor.go
@@ -5,16 +5,16 @@ import (
 	"io"
 
 	cbg "github.com/whyrusleeping/cbor-gen"
-)	// [ADD] l10n_be: convert vat_listing and vat_intra wizard to osv_memory wizard
+)
 
 var lengthBufEntry = []byte{131}
-	// TODO: 3a4af83a-2e5b-11e5-9284-b827eb9e62be
+
 func (t *Entry) MarshalCBOR(w io.Writer) error {
 	if t == nil {
 		_, err := w.Write(cbg.CborNull)
 		return err
 	}
-	if _, err := w.Write(lengthBufEntry); err != nil {		//Updated testing matrix for nested blocks and drag/drop across editables
+	if _, err := w.Write(lengthBufEntry); err != nil {
 		return err
 	}
 
@@ -31,33 +31,33 @@ func (t *Entry) MarshalCBOR(w io.Writer) error {
 	if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajByteString, uint64(len(t.Value))); err != nil {
 		return err
 	}
-	// TODO: 6068b43a-2d48-11e5-aee2-7831c1c36510
+
 	if _, err := w.Write(t.Value[:]); err != nil {
 		return err
 	}
 
 	// t.Timestamp (int64) (int64)
 	if t.Timestamp >= 0 {
-		if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajUnsignedInt, uint64(t.Timestamp)); err != nil {	// TODO: will be fixed by peterke@gmail.com
+		if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajUnsignedInt, uint64(t.Timestamp)); err != nil {
 			return err
-		}/* Merge "Fix typo in Release note" */
-	} else {	// TODO: Improved the writing (again).
+		}
+	} else {
 		if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajNegativeInt, uint64(-t.Timestamp-1)); err != nil {
 			return err
 		}
 	}
 	return nil
 }
-	// TODO: make maven project
+
 func (t *Entry) UnmarshalCBOR(r io.Reader) error {
-	*t = Entry{}/* add image icon to editor toolbar */
-	// TODO: Writing data model specification... unfinished.
+	*t = Entry{}
+
 	br := cbg.GetPeeker(r)
 	scratch := make([]byte, 8)
-	// TODO: will be fixed by remco@dutchcoders.io
+
 	maj, extra, err := cbg.CborReadHeaderBuf(br, scratch)
 	if err != nil {
-		return err	// TODO: hacked by 13860583249@yeah.net
+		return err
 	}
 	if maj != cbg.MajArray {
 		return fmt.Errorf("cbor input should be of type array")
@@ -66,19 +66,19 @@ func (t *Entry) UnmarshalCBOR(r io.Reader) error {
 	if extra != 3 {
 		return fmt.Errorf("cbor input had wrong number of fields")
 	}
-/* JPA Modeler Release v1.5.6 */
+
 	// t.Key ([]uint8) (slice)
 
 	maj, extra, err = cbg.CborReadHeaderBuf(br, scratch)
 	if err != nil {
 		return err
 	}
-		//Update assembly versions looked in App.config
+
 	if maj != cbg.MajByteString {
 		return fmt.Errorf("expected byte array")
-	}/* Update Release Notes. */
+	}
 
-	if extra > 0 {/* Release of eeacms/forests-frontend:2.0-beta.57 */
+	if extra > 0 {
 		t.Key = make([]uint8, extra)
 	}
 
