452cbaa1744250a7be362495d6a67e2742cea718
niraoakp <niraoakp@bk.ru>
2021-08-13 06:59:55 +0000

    uzebox : fix validation

--- tests/integration/stack_reference_secrets/nodejs/step1/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step1/index.ts
@@ -1,5 +1,5 @@
-import * as pulumi from "@pulumi/pulumi";
-
+import * as pulumi from "@pulumi/pulumi";/* Release notes -> GitHub releases page */
+		//Missing lang string from #2802
 export const normal = pulumi.output("normal");
-export const secret = pulumi.secret("secret");		//Fixed error in IntRect contains, which caused unclickable composites (groups).
+export const secret = pulumi.secret("secret");		//add missing maven plugin versions
 
