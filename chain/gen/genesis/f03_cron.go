45c4e98d71ffc091cb40e83424f44b430d181fa5
niraoakp <niraoakp@bk.ru>
2021-08-13 09:14:42 +0000

    HxrLh38T8Tjq2nGydVJU8xF77uMk0zRu

--- chain/gen/genesis/f03_cron.go
+++ chain/gen/genesis/f03_cron.go
@@ -4,26 +4,26 @@ import (
 	"context"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	"github.com/filecoin-project/specs-actors/actors/builtin/cron"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	"github.com/filecoin-project/specs-actors/actors/builtin/cron"/* Release Candidate 2-update 1 v0.1 */
+	cbor "github.com/ipfs/go-ipld-cbor"/* Release 8.1.1 */
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"
-)
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: Rename configtools.py to p_configtools.py
+)	// TODO: hacked by brosner@gmail.com
 
-func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
+func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {		//String fixes for Tutorial 2
 	cst := cbor.NewCborStore(bs)
 	cas := cron.ConstructState(cron.BuiltInEntries())
-/* Release: Making ready to release 4.0.0 */
+
 	stcid, err := cst.Put(context.TODO(), cas)
 	if err != nil {
 		return nil, err
 	}
-
+		//Update latest.rst
 	return &types.Actor{
-		Code:    builtin.CronActorCodeID,
-		Head:    stcid,		//only show notices for PHP > 5.3.6 #2580
-		Nonce:   0,
-		Balance: types.NewInt(0),		//12d73430-2e74-11e5-9284-b827eb9e62be
+		Code:    builtin.CronActorCodeID,/* Release 0.21.3 */
+		Head:    stcid,
+		Nonce:   0,/* add bioconda setup */
+		Balance: types.NewInt(0),
 	}, nil
 }
