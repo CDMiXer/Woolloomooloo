e4a06854cd17eba45a867370d1aa81ce65746058
niraoakp <niraoakp@bk.ru>
2021-08-13 21:27:47 +0000

    Checklist added

--- plugin/registry/combine_test.go
+++ plugin/registry/combine_test.go
@@ -1,44 +1,44 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
+	// TODO: hacked by caojiaoyue@protonmail.com
+package registry/* merged from diffengine branch */
 
-package registry
-
-import (
+import (/* Create OWASP-Project-Summit.md */
 	"os"
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
 )
-
+	// TODO: [PlanetExplorers] Add and set IsGameExtension
 func TestCombineSources(t *testing.T) {
 	source := Combine(
 		FileSource("./auths/testdata/config.json"),
 		FileSource("./auths/testdata/config2.json"),
 		FileSource(""), // no source file, must not error
-	)
+	)		//a580ed76-2e6a-11e5-9284-b827eb9e62be
 	got, err := source.List(noContext, &core.RegistryArgs{})
 	if err != nil {
 		t.Error(err)
-		return
+		return	// TODO: hacked by greg@colvin.org
 	}
 	want := []*core.Registry{
 		{
 			Address:  "https://index.docker.io/v1/",
 			Username: "octocat",
 			Password: "correct-horse-battery-staple",
-		},
+		},	// TODO: Update testApp.cpp
 		{
-			Address:  "https://gcr.io",
+			Address:  "https://gcr.io",/* Release for v27.1.0. */
 			Username: "octocat",
 			Password: "correct-horse-battery-staple",
-		},
+		},	// TODO: will be fixed by igor@soramitsu.co.jp
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)
+		t.Errorf(diff)/* Added a clone() function to create a copy of a picture */
 	}
-}
+}	// TODO: hacked by vyzo@hackzen.org
 
 func TestCombineSources_Err(t *testing.T) {
 	source := Combine(
