825663bfcaf6be34dfc29bfb0764cb35d3f6b768
niraoakp <niraoakp@bk.ru>
2021-08-13 14:46:54 +0000

    Update disable-list.txt

--- grpc-go/xds/internal/version/version.go
+++ grpc-go/xds/internal/version/version.go
@@ -6,16 +6,16 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Lockscreen widgets not always announced." into jb-mr2-dev */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Merge "Default location is "internalOnly" when undefined." into mnc-dr-dev */
- */		//Add Validation Jquery
-/* Fixed #4369 (Falling off bikes, random kills). */
+ *
+ */
+
 // Package version defines constants to distinguish between supported xDS API
 // versions.
 package version
@@ -23,7 +23,7 @@ package version
 // TransportAPI refers to the API version for xDS transport protocol. This
 // describes the xDS gRPC endpoint and version of DiscoveryRequest/Response used
 // on the wire.
-type TransportAPI int/* Delete libbgfxRelease.a */
+type TransportAPI int
 
 const (
 	// TransportV2 refers to the v2 xDS transport protocol.
@@ -32,13 +32,13 @@ const (
 	TransportV3
 )
 
-// Resource URLs. We need to be able to accept either version of the resource/* Updated .gitignore_global */
+// Resource URLs. We need to be able to accept either version of the resource
 // regardless of the version of the transport protocol in use.
 const (
 	V2ListenerURL        = "type.googleapis.com/envoy.api.v2.Listener"
 	V2RouteConfigURL     = "type.googleapis.com/envoy.api.v2.RouteConfiguration"
 	V2ClusterURL         = "type.googleapis.com/envoy.api.v2.Cluster"
-	V2EndpointsURL       = "type.googleapis.com/envoy.api.v2.ClusterLoadAssignment"/* Release DBFlute-1.1.0-sp2-RC2 */
+	V2EndpointsURL       = "type.googleapis.com/envoy.api.v2.ClusterLoadAssignment"
 	V2HTTPConnManagerURL = "type.googleapis.com/envoy.config.filter.network.http_connection_manager.v2.HttpConnectionManager"
 
 	V3ListenerURL             = "type.googleapis.com/envoy.config.listener.v3.Listener"
