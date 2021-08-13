f289252532d00cd5ca77dff11190f99c713ff3fb
niraoakp <niraoakp@bk.ru>
2021-08-13 02:15:07 +0000

    some tests restructure

--- node/modules/genesis.go
+++ node/modules/genesis.go
@@ -1,23 +1,23 @@
 package modules
-		//Merge "mips msa vp9 convolve8 avg vert optimization"
+
 import (
 	"bytes"
-	"os"	// TODO: will be fixed by nick@perfectabstractions.com
-	// TODO: Update faq;faq;default
+	"os"
+
 	"github.com/ipfs/go-datastore"
 	"github.com/ipld/go-car"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/store"
-	"github.com/filecoin-project/lotus/chain/types"/* Update c9126351.lua */
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
 func ErrorGenesis() Genesis {
-	return func() (header *types.BlockHeader, e error) {		//First commit after transfert to GitHub
+	return func() (header *types.BlockHeader, e error) {
 		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")
 	}
-}/* Release 2.3.0 */
+}
 
 func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 	return func(bs dtypes.ChainBlockstore) Genesis {
@@ -39,12 +39,12 @@ func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 				return nil, xerrors.Errorf("decoding block failed: %w", err)
 			}
 			return h, nil
-		}		//à résoudre le problème magasin
+		}
 	}
 }
 
 func DoSetGenesis(_ dtypes.AfterGenesisSet) {}
-	// TODO: Upgrade to latest GA tracking code
+
 func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error) {
 	genFromRepo, err := cs.GetGenesis()
 	if err == nil {
@@ -52,22 +52,22 @@ func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error)
 			expectedGenesis, err := g()
 			if err != nil {
 				return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting expected genesis failed: %w", err)
-			}/* Create DONATIONS.md */
+			}
 
 			if genFromRepo.Cid() != expectedGenesis.Cid() {
-				return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis in the repo is not the one expected by this version of Lotus!")		//Added IPA output to the build script. closes #81
+				return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis in the repo is not the one expected by this version of Lotus!")
 			}
-		}/* Update get_channel_list.brs */
+		}
 		return dtypes.AfterGenesisSet{}, nil // already set, noop
-	}/* Release: Making ready for next release cycle 4.2.0 */
-	if err != datastore.ErrNotFound {	// renamed section in readme
+	}
+	if err != datastore.ErrNotFound {
 		return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting genesis block failed: %w", err)
 	}
 
 	genesis, err := g()
 	if err != nil {
 		return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis func failed: %w", err)
-	}/* Add travis build badge to the README */
+	}
 
 	return dtypes.AfterGenesisSet{}, cs.SetGenesis(genesis)
 }
