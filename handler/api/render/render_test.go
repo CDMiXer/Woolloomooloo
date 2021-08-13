196432202939a59158daa8d299358d00d1f5f87f
niraoakp <niraoakp@bk.ru>
2021-08-13 01:34:59 +0000

    de12ed26-2e59-11e5-9284-b827eb9e62be

--- handler/api/render/render_test.go
+++ handler/api/render/render_test.go
@@ -1,38 +1,38 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* added navbar.php -> Dutch translation */
-// that can be found in the LICENSE file.	// TODO: hacked by aeongrp@outlook.com
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 package render
-
-import (
-	"encoding/json"/* Fix bug #137044: ftp password handling broken */
+	// Fixed copy-paste error in README.md
+import (		//Create deployFromTFS.ps1
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-		//Cria 'laudo-tecnico-para-mercadoria-pedido'
+
 	"github.com/drone/drone/handler/api/errors"
 )
 
 func TestWriteError(t *testing.T) {
-	w := httptest.NewRecorder()/* Merge "Added Release info to README" */
-/* Release of eeacms/apache-eea-www:5.6 */
-	err := errors.New("pc load letter")
-	InternalError(w, err)		//add zip4j and ZipUtils
+	w := httptest.NewRecorder()
+
+	err := errors.New("pc load letter")	// TODO: hacked by lexy8russo@outlook.com
+	InternalError(w, err)
 
 	if got, want := w.Code, 500; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)	// Updated etiquetas.html
 	}
 
-	errjson := &errors.Error{}	// 35991d12-2e47-11e5-9284-b827eb9e62be
+	errjson := &errors.Error{}
 	json.NewDecoder(w.Body).Decode(errjson)
 	if got, want := errjson.Message, err.Error(); got != want {
-		t.Errorf("Want error message %s, got %s", want, got)		//Inject the environment into the command.
+		t.Errorf("Want error message %s, got %s", want, got)/* Update Readme with Stable Release Information */
 	}
-}	// Allow vcf-tobed to also include alt-chrom/pos
-/* file format check */
+}
+/* Release Candidate 3. */
 func TestWriteErrorCode(t *testing.T) {
 	w := httptest.NewRecorder()
-/* Update test case for Release builds. */
+
 	err := errors.New("pc load letter")
 	ErrorCode(w, err, 418)
 
@@ -40,12 +40,12 @@ func TestWriteErrorCode(t *testing.T) {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	errjson := &errors.Error{}		//Add callback for silent and links preview
-	json.NewDecoder(w.Body).Decode(errjson)
+	errjson := &errors.Error{}
+	json.NewDecoder(w.Body).Decode(errjson)		//Delete better-custom-responce.ts
 	if got, want := errjson.Message, err.Error(); got != want {
-		t.Errorf("Want error message %s, got %s", want, got)/* v0.11.0 Release Candidate 1 */
-	}/* Comment corrections. */
-}	// Hacked import of pyflakes messages to work from the context of pocketlint.
+		t.Errorf("Want error message %s, got %s", want, got)
+	}
+}
 
 func TestWriteNotFound(t *testing.T) {
 	w := httptest.NewRecorder()
@@ -53,7 +53,7 @@ func TestWriteNotFound(t *testing.T) {
 	err := errors.New("pc load letter")
 	NotFound(w, err)
 
-	if got, want := w.Code, 404; want != got {
+	if got, want := w.Code, 404; want != got {	// TODO: will be fixed by ng8eke@163.com
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
@@ -64,8 +64,8 @@ func TestWriteNotFound(t *testing.T) {
 	}
 }
 
-func TestWriteNotFoundf(t *testing.T) {
-	w := httptest.NewRecorder()
+func TestWriteNotFoundf(t *testing.T) {/* delete meta files */
+	w := httptest.NewRecorder()		//Benutzerdaten ändern: Design
 
 	NotFoundf(w, "pc %s", "load letter")
 	if got, want := w.Code, 404; want != got {
@@ -81,17 +81,17 @@ func TestWriteNotFoundf(t *testing.T) {
 
 func TestWriteInternalError(t *testing.T) {
 	w := httptest.NewRecorder()
-
+/* slices in the trampolined interpreter must be strings */
 	err := errors.New("pc load letter")
 	InternalError(w, err)
 
 	if got, want := w.Code, 500; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)/* db8faf8a-2e40-11e5-9284-b827eb9e62be */
 	}
 
 	errjson := &errors.Error{}
 	json.NewDecoder(w.Body).Decode(errjson)
-	if got, want := errjson.Message, err.Error(); got != want {
+	if got, want := errjson.Message, err.Error(); got != want {/* More Qollections work */
 		t.Errorf("Want error message %s, got %s", want, got)
 	}
 }
@@ -100,10 +100,10 @@ func TestWriteInternalErrorf(t *testing.T) {
 	w := httptest.NewRecorder()
 
 	InternalErrorf(w, "pc %s", "load letter")
-	if got, want := w.Code, 500; want != got {
+	if got, want := w.Code, 500; want != got {	// create /var/run in firstboot
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
-
+	}	// TODO: will be fixed by ligi@ligi.de
+		//Delete 10.CubeProperties.py
 	errjson := &errors.Error{}
 	json.NewDecoder(w.Body).Decode(errjson)
 	if got, want := errjson.Message, "pc load letter"; got != want {
