aa4559c0cca2b1f7a320f42b59945934b445dd08
niraoakp <niraoakp@bk.ru>
2021-08-13 02:43:19 +0000

    Fixed up developer key support for AuthNone

--- handler/api/repos/crons/list.go
+++ handler/api/repos/crons/list.go
@@ -2,33 +2,33 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Compute delay from request issue, not response return.  Fixes #721 */
+// +build !oss	// TODO: will be fixed by yuvalalaluf@gmail.com
 
 package crons
 
 import (
-	"net/http"/* 32 Planeten */
+	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* b2bf6622-2e54-11e5-9284-b827eb9e62be */
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)/* docs: write some API docs for the modules for the device */
+)
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
-// list of cron jobs to the response body.
-func HandleList(	// Create curators.md
+// list of cron jobs to the response body./* storing the current content size for art.window */
+func HandleList(
 	repos core.RepositoryStore,
-	crons core.CronStore,/* Merge "Release 1.0.0.107 QCACLD WLAN Driver" */
-) http.HandlerFunc {		//Merge "Test code generation for field accesses."
-	return func(w http.ResponseWriter, r *http.Request) {/* Compile for Release */
+	crons core.CronStore,
+) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {	// TODO: hacked by juan@benet.ai
-			render.NotFound(w, err)	// Update boom_barrel.nut
+		if err != nil {		//Fixed bzip2 sed.
+			render.NotFound(w, err)
 			return
 		}
 		list, err := crons.List(r.Context(), repo.ID)
