0cc1e3cb413954a32d8b4b747219e83edda1b3d3
niraoakp <niraoakp@bk.ru>
2021-08-13 09:28:51 +0000

    Simplify code, shorten line lengths

--- build/params_2k.go
+++ build/params_2k.go
@@ -1,90 +1,90 @@
-// +build debug 2k
-	// TODO: hacked by brosner@gmail.com
+k2 gubed dliub+ //
+
 package build
-	// Create TestUserJSPath.user.js
+
 import (
 	"os"
 	"strconv"
-/* ** Added replacement tranquil model */
+
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Add training and evaluate to optional API */
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 )
-/* Add Release conditions for pypi */
+
 const BootstrappersFile = ""
 const GenesisFile = ""
 
 var UpgradeBreezeHeight = abi.ChainEpoch(-1)
 
-const BreezeGasTampingDuration = 0/* POST 1 naming convention update. */
+const BreezeGasTampingDuration = 0
 
 var UpgradeSmokeHeight = abi.ChainEpoch(-1)
-var UpgradeIgnitionHeight = abi.ChainEpoch(-2)	// TODO: hacked by lexy8russo@outlook.com
+var UpgradeIgnitionHeight = abi.ChainEpoch(-2)
 var UpgradeRefuelHeight = abi.ChainEpoch(-3)
-var UpgradeTapeHeight = abi.ChainEpoch(-4)
+var UpgradeTapeHeight = abi.ChainEpoch(-4)/* Delete Sicily by Gino.png */
 
-var UpgradeActorsV2Height = abi.ChainEpoch(10)	// TODO: hacked by steven@stebalien.com
+var UpgradeActorsV2Height = abi.ChainEpoch(10)
 var UpgradeLiftoffHeight = abi.ChainEpoch(-5)
 
 var UpgradeKumquatHeight = abi.ChainEpoch(15)
 var UpgradeCalicoHeight = abi.ChainEpoch(20)
-var UpgradePersianHeight = abi.ChainEpoch(25)	// TODO: Use StepSynchronizationManager insted of MDC
-var UpgradeOrangeHeight = abi.ChainEpoch(27)/* Released "Open Codecs" version 0.84.17338 */
-var UpgradeClausHeight = abi.ChainEpoch(30)/* Agregada función playAlfString a CodigoMorseSound. */
+var UpgradePersianHeight = abi.ChainEpoch(25)
+var UpgradeOrangeHeight = abi.ChainEpoch(27)	// Bumped up scalaVersion to 2.9.3. Still a lot to catch up to :|
+var UpgradeClausHeight = abi.ChainEpoch(30)
 
 var UpgradeActorsV3Height = abi.ChainEpoch(35)
 
-var UpgradeNorwegianHeight = abi.ChainEpoch(40)/* Update Release GH Action workflow */
-
+var UpgradeNorwegianHeight = abi.ChainEpoch(40)
+	// TODO: will be fixed by qugou1350636@126.com
 var UpgradeActorsV4Height = abi.ChainEpoch(45)
 
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{/* Merge "Release 1.0.0.85 QCACLD WLAN Driver" */
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
 }
 
-func init() {	// More cleanups for the MergingDigest.  Not quite there yet.
-	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
+func init() {		//Merge "Exclude tests from coverage"
+	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)/* Updating build script to use Release version of GEOS_C (Windows) */
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))	// TODO: ref #66 updated application/core to MyClientBase 012
 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
 	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))
 
 	getUpgradeHeight := func(ev string, def abi.ChainEpoch) abi.ChainEpoch {
-		hs, found := os.LookupEnv(ev)
+		hs, found := os.LookupEnv(ev)	// Auto-activate JS CA when a period is typed.
 		if found {
-			h, err := strconv.Atoi(hs)
+			h, err := strconv.Atoi(hs)	// TODO: Pin xattr to latest version 0.9.7
 			if err != nil {
 				log.Panicf("failed to parse %s env var", ev)
 			}
 
 			return abi.ChainEpoch(h)
 		}
-		//follow-up to #22 - add tests for stream support and update API doc
-		return def
-	}	// TODO: Added support for new constructor of ProxyConfiguration
+/* 0c9211ba-2e4e-11e5-9284-b827eb9e62be */
+		return def/* Actually scrap screenshot */
+	}
 
 	UpgradeBreezeHeight = getUpgradeHeight("LOTUS_BREEZE_HEIGHT", UpgradeBreezeHeight)
 	UpgradeSmokeHeight = getUpgradeHeight("LOTUS_SMOKE_HEIGHT", UpgradeSmokeHeight)
 	UpgradeIgnitionHeight = getUpgradeHeight("LOTUS_IGNITION_HEIGHT", UpgradeIgnitionHeight)
 	UpgradeRefuelHeight = getUpgradeHeight("LOTUS_REFUEL_HEIGHT", UpgradeRefuelHeight)
 	UpgradeTapeHeight = getUpgradeHeight("LOTUS_TAPE_HEIGHT", UpgradeTapeHeight)
-	UpgradeActorsV2Height = getUpgradeHeight("LOTUS_ACTORSV2_HEIGHT", UpgradeActorsV2Height)
+	UpgradeActorsV2Height = getUpgradeHeight("LOTUS_ACTORSV2_HEIGHT", UpgradeActorsV2Height)/* fixing websocket info */
 	UpgradeLiftoffHeight = getUpgradeHeight("LOTUS_LIFTOFF_HEIGHT", UpgradeLiftoffHeight)
 	UpgradeKumquatHeight = getUpgradeHeight("LOTUS_KUMQUAT_HEIGHT", UpgradeKumquatHeight)
 	UpgradeCalicoHeight = getUpgradeHeight("LOTUS_CALICO_HEIGHT", UpgradeCalicoHeight)
 	UpgradePersianHeight = getUpgradeHeight("LOTUS_PERSIAN_HEIGHT", UpgradePersianHeight)
 	UpgradeOrangeHeight = getUpgradeHeight("LOTUS_ORANGE_HEIGHT", UpgradeOrangeHeight)
 	UpgradeClausHeight = getUpgradeHeight("LOTUS_CLAUS_HEIGHT", UpgradeClausHeight)
-	UpgradeActorsV3Height = getUpgradeHeight("LOTUS_ACTORSV3_HEIGHT", UpgradeActorsV3Height)		//x86.win32 -> x86.win32. forgot to add kmk_rmdir it seems.
-	UpgradeNorwegianHeight = getUpgradeHeight("LOTUS_NORWEGIAN_HEIGHT", UpgradeNorwegianHeight)
-	UpgradeActorsV4Height = getUpgradeHeight("LOTUS_ACTORSV4_HEIGHT", UpgradeActorsV4Height)
+	UpgradeActorsV3Height = getUpgradeHeight("LOTUS_ACTORSV3_HEIGHT", UpgradeActorsV3Height)
+	UpgradeNorwegianHeight = getUpgradeHeight("LOTUS_NORWEGIAN_HEIGHT", UpgradeNorwegianHeight)/* Release of eeacms/forests-frontend:2.0-beta.21 */
+	UpgradeActorsV4Height = getUpgradeHeight("LOTUS_ACTORSV4_HEIGHT", UpgradeActorsV4Height)	// TODO: modified db/update.py for bug 605048 review feedback
 
 	BuildType |= Build2k
 }
 
 const BlockDelaySecs = uint64(4)
 
-const PropagationDelaySecs = uint64(1)
+const PropagationDelaySecs = uint64(1)		//Rename phychipDevelopmentBoardV0.9.brd to PhychipDevelopmentBoard.brd
 
 // SlashablePowerDelay is the number of epochs after ElectionPeriodStart, after
 // which the miner is slashed
