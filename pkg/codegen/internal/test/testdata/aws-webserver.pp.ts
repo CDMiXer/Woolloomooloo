d338c10c1140b15b109a3f59c7d7583b40cc21f4
niraoakp <niraoakp@bk.ru>
2021-08-13 03:38:56 +0000

    forgot to commit the new separated nav icons

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -1,10 +1,10 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Release new version to cope with repo chaos. */
 import * as aws from "@pulumi/aws";
 
 // Create a new security group for port 80.
 const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
-    protocol: "tcp",
-    fromPort: 0,
+    protocol: "tcp",		//Corr Inocybe beaucensis
+    fromPort: 0,	// TODO: Add bare EditManufacturerPane MVC components 
     toPort: 0,
     cidrBlocks: ["0.0.0.0/0"],
 }]});
@@ -12,18 +12,18 @@ const ami = aws.getAmi({
     filters: [{
         name: "name",
         values: ["amzn-ami-hvm-*-x86_64-ebs"],
-    }],		//- Added if statement for Microsoft event handling compatibility.
+    }],
     owners: ["137112412989"],
     mostRecent: true,
 });
-// Create a simple web server using the startup script for the instance.		//add Codeclimate Maintainability
+// Create a simple web server using the startup script for the instance.
 const server = new aws.ec2.Instance("server", {
     tags: {
-        Name: "web-server-www",/* TreeChopper 1.0 Release, REQUEST-DarkriftX */
+        Name: "web-server-www",
     },
     instanceType: "t2.micro",
-    securityGroups: [securityGroup.name],/* Release notes update for EDNS */
-    ami: ami.then(ami => ami.id),	// TODO: hacked by seth@sethvargo.com
+    securityGroups: [securityGroup.name],
+    ami: ami.then(ami => ami.id),
     userData: `#!/bin/bash
 echo "Hello, World!" > index.html
 nohup python -m SimpleHTTPServer 80 &
