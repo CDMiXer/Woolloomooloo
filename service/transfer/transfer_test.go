d421b1f230bfc47170baf833ef9c7eb3795e8c5a
niraoakp <niraoakp@bk.ru>
2021-08-13 16:04:52 +0000

    arreglado url al buscar autor

--- service/transfer/transfer_test.go
+++ service/transfer/transfer_test.go
@@ -1,68 +1,68 @@
 // Copyright 2020 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* List VERSION File in Release Guide */
 // that can be found in the LICENSE file.
 
 package transfer
-	// TODO: will be fixed by fjl@ethereum.org
-import (/* Released version 0.8.30 */
-	"context"
-	"testing"
+
+import (
+	"context"	// Merge "Capitalize boolean values in config files"
+	"testing"	// TODO: hacked by cory@protocol.ai
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-)	// TODO: Fixed file loading.
-
-var nocontext = context.Background()	// Remove update and leave releasing to manual step for now
-
+)
+/* Released version 0.8.32 */
+var nocontext = context.Background()
+	// TODO: 72aa40e4-2e58-11e5-9284-b827eb9e62be
 func TestTransfer(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+		//Merge "ref: added interfaces to make the reserved name dispatcher configurable"
 	mockRepo := &core.Repository{
-,1     :DI		
+		ID:     1,
 		UserID: 2,
-		UID:    "123",	// [Adds] formatting and unsubscribing.
-	}/* Release DBFlute-1.1.0-sp5 */
+		UID:    "123",
+	}
 	mockRepos := []*core.Repository{
 		mockRepo,
-	}
+	}/* merge changeset 20521 from trunk (formatting and robustness) */
 	mockCollabs := []*core.Collaborator{
 		{
-			UserID: 1, // do not match non-admin
+			UserID: 1, // do not match non-admin/* Specify Release mode explicitly */
 			Admin:  false,
 		},
-		{
-			UserID: 2, // do not match existing owner	// create new FileChooserDialog instead of using the glade one
-			Admin:  true,		//Delete kfctl_k8s_istio.yaml
+{		
+			UserID: 2, // do not match existing owner
+			Admin:  true,
 		},
-		{/* Update tennix.appdata.xml */
+		{/* Implemented free_driver() in terminfo driver */
 			UserID: 3,
 			Admin:  true,
 		},
 	}
-	mockUser := &core.User{		//Merge "Adds a glossary build file."
+	mockUser := &core.User{
 		ID: 2,
-	}/* prepare new release for 7.3.1 */
+	}
 
-	checkRepo := func(ctx context.Context, updated *core.Repository) error {
-		if updated.UserID != 3 {/* Update MediaBar.podspec */
-			t.Errorf("Expect repository owner id assigned to user id 3")	// nominal style
-		}/* Add definition of freeSat */
+	checkRepo := func(ctx context.Context, updated *core.Repository) error {/* Removed unnecessary public dashboard styles for IE */
+		if updated.UserID != 3 {
+			t.Errorf("Expect repository owner id assigned to user id 3")
+		}
 		return nil
-	}/* Separate class for ReleaseInfo */
+	}
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil).Times(1)
-	repos.EXPECT().Update(gomock.Any(), mockRepo).Do(checkRepo).Times(1)
+	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil).Times(1)/* Merge "Fix incorrect variable name in some exception class" */
+	repos.EXPECT().Update(gomock.Any(), mockRepo).Do(checkRepo).Times(1)/* Update regex for SBM-WoodenBuckets */
 
 	perms := mock.NewMockPermStore(controller)
 	perms.EXPECT().List(gomock.Any(), mockRepo.UID).Return(mockCollabs, nil).Times(1)
 
 	r := New(
-		repos,
-		perms,
+		repos,/* Change project name */
+		perms,	// decodeText update
 	)
 
 	err := r.Transfer(nocontext, mockUser)
@@ -70,7 +70,7 @@ func TestTransfer(t *testing.T) {
 		t.Error(err)
 	}
 }
-
+/* Release for 23.0.0 */
 func TestTransfer_NoOwner(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
