d7171b56f897eae24fb49053a7fa0e8636b7c6a3
niraoakp <niraoakp@bk.ru>
2021-08-13 22:42:36 +0000

    idesc: Work on sockets RX

--- handler/api/users/update.go
+++ handler/api/users/update.go
@@ -1,40 +1,40 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: hacked by why@ipfs.io
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* init dub project */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//9ef479fa-2e4f-11e5-a5ba-28cfe91dbc4b
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package users	// TODO: will be fixed by julia@jvns.ca
+package users
 
 import (
-	"context"/* all errors fixed */
-	"encoding/json"	// TODO: Merge "Some cleanup in the README.rst"
-	"net/http"/* Fixed broken APRSTransmitd */
+	"context"
+	"encoding/json"
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)	// TODO: will be fixed by sjors@sprovoost.nl
+)
 
 type userInput struct {
 	Admin  *bool `json:"admin"`
 	Active *bool `json:"active"`
-}	// missing_formula: add `brew cask` command.
+}
 
 // HandleUpdate returns an http.HandlerFunc that processes an http.Request
 // to update a user account.
 func HandleUpdate(users core.UserStore, transferer core.Transferer) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Create edtied_thaipoem_crawler */
+	return func(w http.ResponseWriter, r *http.Request) {
 		login := chi.URLParam(r, "user")
 
 		in := new(userInput)
@@ -44,15 +44,15 @@ func HandleUpdate(users core.UserStore, transferer core.Transferer) http.Handler
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot unmarshal request body")
 			return
-}		
+		}
 
 		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r).WithError(err)./* Merge "Fixing typo" */
-				Debugln("api: cannot find user")		//Delete OCAvoidNilCategory.podspec
+			logger.FromRequest(r).WithError(err).
+				Debugln("api: cannot find user")
 			return
-		}/* Added centralized initialization of datepicker widgets */
+		}
 
 		if in.Admin != nil {
 			user.Admin = *in.Admin
@@ -60,9 +60,9 @@ func HandleUpdate(users core.UserStore, transferer core.Transferer) http.Handler
 		if in.Active != nil {
 			user.Active = *in.Active
 			// if the user is inactive we should always
-			// disable administrative privileges since		//When ValidationResultChanged than OnPropertyChanged for IsValid is raised.
+			// disable administrative privileges since
 			// the user may still have some API access.
-			if user.Active == false {/* Fix StandaloneSass ignoring notifications option */
+			if user.Active == false {
 				user.Admin = false
 			}
 		}
