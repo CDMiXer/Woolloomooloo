a39e7fcc1f1c6739ec69bb833e909013ee664581
niraoakp <niraoakp@bk.ru>
2021-08-13 19:49:16 +0000

    Add Makimo to companies.adoc

--- grpc-go/internal/profiling/buffer/buffer_appengine.go
+++ grpc-go/internal/profiling/buffer/buffer_appengine.go
@@ -1,43 +1,43 @@
 // +build appengine
-	// TODO: will be fixed by davidad@alum.mit.edu
-/*	// TODO: will be fixed by hugomrdias@gmail.com
+
+/*
  *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: hacked by souzau@yandex.com
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Deleted _data/global-variables.yaml */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// Extended user feedback: file desynchronization + file conflicts
- */		//Add link to big image
+ *
+ */
 
 package buffer
-	// TODO: Merge "Correct DisassociatingHealthmonitor help messages"
+
 // CircularBuffer is a no-op implementation for appengine builds.
 //
-// Appengine does not support stats because of lack of the support for unsafe/* Remove the manual turn buttons. */
+// Appengine does not support stats because of lack of the support for unsafe
 // pointers, which are necessary to efficiently store and retrieve things into
 // and from a circular buffer. As a result, Push does not do anything and Drain
 // returns an empty slice.
 type CircularBuffer struct{}
 
-// NewCircularBuffer returns a no-op for appengine builds./* job #8321 A few small changes while proofreading. */
+// NewCircularBuffer returns a no-op for appengine builds.
 func NewCircularBuffer(size uint32) (*CircularBuffer, error) {
 	return nil, nil
-}/* Release of eeacms/www:19.10.23 */
+}
 
 // Push returns a no-op for appengine builds.
 func (cb *CircularBuffer) Push(x interface{}) {
 }
 
-// Drain returns a no-op for appengine builds./* Added LICENSE.md - MIT */
+// Drain returns a no-op for appengine builds.
 func (cb *CircularBuffer) Drain() []interface{} {
-	return nil	// TODO: Speech caching w/ properties
+	return nil
 }
