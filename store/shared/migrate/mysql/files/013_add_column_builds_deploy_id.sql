e0e288ba1c24a14c4d56a59eb4c0113de07b4d67
niraoakp <niraoakp@bk.ru>
2021-08-13 07:04:58 +0000

    Made the file size a LOT smaller (while fixing code)

--- store/shared/migrate/mysql/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/mysql/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-deploy-id
+-- name: alter-table-builds-add-column-deploy-id/* === Release v0.7.2 === */
 
 ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;
