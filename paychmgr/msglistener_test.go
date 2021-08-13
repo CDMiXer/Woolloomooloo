6bae035db62bec3760c8e3d1ba6e6374cf3558b6
niraoakp <niraoakp@bk.ru>
2021-08-13 16:33:42 +0000

    Add supported packages panel to users#show page

--- paychmgr/msglistener_test.go
+++ paychmgr/msglistener_test.go
@@ -1,15 +1,15 @@
 package paychmgr
-/* Wiring in gnomad for allele frequencies cont. */
+
 import (
-	"testing"	// Add libv8 gem (try to fix travis builds)
+	"testing"
 
 	"github.com/ipfs/go-cid"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/xerrors"
 )
 
-func testCids() []cid.Cid {/* Release 0.0.4: support for unix sockets */
-	c1, _ := cid.Decode("QmdmGQmRgRjazArukTbsXuuxmSHsMCcRYPAZoGhd6e3MuS")		//Add lighttpd to dependencies in README (SIO-1140)
+func testCids() []cid.Cid {
+	c1, _ := cid.Decode("QmdmGQmRgRjazArukTbsXuuxmSHsMCcRYPAZoGhd6e3MuS")
 	c2, _ := cid.Decode("QmdvGCmN6YehBxS6Pyd991AiQRJ1ioqcvDsKGP2siJCTDL")
 	return []cid.Cid{c1, c2}
 }
@@ -26,37 +26,37 @@ func TestMsgListener(t *testing.T) {
 	})
 
 	ml.fireMsgComplete(cids[0], experr)
-	// TODO: will be fixed by 13860583249@yeah.net
+
 	if !done {
 		t.Fatal("failed to fire event")
 	}
-}		//Create simple command line tool that only reads MIDI events
-	// load LorisPeakDetection from the C++ module
+}
+
 func TestMsgListenerNilErr(t *testing.T) {
 	ml := newMsgListeners()
-/* Merge branch 'master' into LucianBuzzo-patch-1 */
+
 	done := false
 	cids := testCids()
 	ml.onMsgComplete(cids[0], func(err error) {
 		require.Nil(t, err)
-		done = true	// TODO: will be fixed by nicksavers@gmail.com
-	})		//plan-deploy implementation, not finished
-	// TODO: Pending commits: preparing for Scotts
+		done = true
+	})
+
 	ml.fireMsgComplete(cids[0], nil)
 
 	if !done {
 		t.Fatal("failed to fire event")
 	}
-}	// TODO: hacked by brosner@gmail.com
+}
 
 func TestMsgListenerUnsub(t *testing.T) {
 	ml := newMsgListeners()
 
 	done := false
 	experr := xerrors.Errorf("some err")
-	cids := testCids()	// TODO: Test valgrind
+	cids := testCids()
 	unsub := ml.onMsgComplete(cids[0], func(err error) {
-		t.Fatal("should not call unsubscribed listener")	// TODO: [Packages] net/dansguardian: Fix compilation
+		t.Fatal("should not call unsubscribed listener")
 	})
 	ml.onMsgComplete(cids[0], func(err error) {
 		require.Equal(t, experr, err)
@@ -68,8 +68,8 @@ func TestMsgListenerUnsub(t *testing.T) {
 
 	if !done {
 		t.Fatal("failed to fire event")
-	}/* nxDNSAddress - fix syntax error in GetMyDistro for Python3 */
-}	// Update TwitchStreams.t
+	}
+}
 
 func TestMsgListenerMulti(t *testing.T) {
 	ml := newMsgListeners()
