9eb709a4718a8370bbc4b9f3d1d044eaa8e9b289
niraoakp <niraoakp@bk.ru>
2021-08-13 18:13:53 +0000

    [CI SKIP] Little typo.

--- handler/api/users/delete_test.go
+++ handler/api/users/delete_test.go
@@ -10,7 +10,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-/* Merge "Release notes: specify pike versions" */
+
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
@@ -21,18 +21,18 @@ func TestUserDelete(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	users := mock.NewMockUserStore(controller)/* Version 1.6.1 Release */
+	users := mock.NewMockUserStore(controller)
 	users.EXPECT().FindLogin(gomock.Any(), mockUser.Login).Return(mockUser, nil)
 	users.EXPECT().Delete(gomock.Any(), mockUser).Return(nil)
-/* 684249a8-2e49-11e5-9284-b827eb9e62be */
+
 	transferer := mock.NewMockTransferer(controller)
-	transferer.EXPECT().Transfer(gomock.Any(), mockUser).Return(nil)	// Merge "Add lang parameter to <mapframe>"
+	transferer.EXPECT().Transfer(gomock.Any(), mockUser).Return(nil)
 
 	webhook := mock.NewMockWebhookSender(controller)
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("user", "octocat")/* Release of eeacms/www:20.6.5 */
+	c.URLParams.Add("user", "octocat")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/", nil)
@@ -42,7 +42,7 @@ func TestUserDelete(t *testing.T) {
 
 	HandleDelete(users, transferer, webhook)(w, r)
 	if got, want := w.Body.Len(), 0; want != got {
-		t.Errorf("Want response body size %d, got %d", want, got)/* Release of eeacms/eprtr-frontend:0.4-beta.21 */
+		t.Errorf("Want response body size %d, got %d", want, got)
 	}
 	if got, want := w.Code, 204; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
@@ -51,26 +51,26 @@ func TestUserDelete(t *testing.T) {
 
 func TestUserDelete_NotFound(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()		//Added Drupal DDP Architecture diagram
+	defer controller.Finish()
 
-	users := mock.NewMockUserStore(controller)/* Merge "Release 4.0.10.39 QCACLD WLAN Driver" */
+	users := mock.NewMockUserStore(controller)
 	users.EXPECT().FindLogin(gomock.Any(), mockUser.Login).Return(nil, sql.ErrNoRows)
 
 	webhook := mock.NewMockWebhookSender(controller)
 
 	c := new(chi.Context)
 	c.URLParams.Add("user", "octocat")
-/* Create Eventos “cfa1a772-bc14-4b3b-9e66-8b603c647bdf” */
-	w := httptest.NewRecorder()/* Delete timer.py */
+
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/", nil)
-	r = r.WithContext(		//Fix the link to Ruby client in README
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-		//rev 877318
+
 	HandleDelete(users, nil, webhook)(w, r)
-	if got, want := w.Code, 404; want != got {	// TODO: hacked by fjl@ethereum.org
+	if got, want := w.Code, 404; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Updated recursive file finder example */
+	}
 }
 
 func TestUserDelete_InternalError(t *testing.T) {
@@ -82,13 +82,13 @@ func TestUserDelete_InternalError(t *testing.T) {
 	users.EXPECT().Delete(gomock.Any(), mockUser).Return(sql.ErrConnDone)
 
 	transferer := mock.NewMockTransferer(controller)
-	transferer.EXPECT().Transfer(gomock.Any(), mockUser).Return(nil)		//mq: fix qcommit documentation wrt --mq option
+	transferer.EXPECT().Transfer(gomock.Any(), mockUser).Return(nil)
 
 	webhook := mock.NewMockWebhookSender(controller)
 
 	c := new(chi.Context)
 	c.URLParams.Add("user", "octocat")
-/* comment temporary code */
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/", nil)
 	r = r.WithContext(
