c496c151891a40dedb10bce06335b85f86f73123
niraoakp <niraoakp@bk.ru>
2021-08-13 06:40:55 +0000

    Merge "zuul/layout/puppet: add more integration jobs"

--- hack/crdgen.sh
+++ hack/crdgen.sh
@@ -1,5 +1,5 @@
-#!/bin/bash/* crm sicepat */
-set -eu -o pipefail		//[#712] Round to 3 decimals the number result in derive tx
+#!/bin/bash
+set -eu -o pipefail
 
 cd "$(dirname "$0")/.."
 
@@ -8,17 +8,17 @@ add_header() {
   mv tmp "$1"
 }
 
-echo "Generating CRDs"	// Merge branch 'develop' into update-doc-pybind
+echo "Generating CRDs"
 controller-gen crd:trivialVersions=true,maxDescLen=0 paths=./pkg/apis/... output:dir=manifests/base/crds/full
-		//ef3b4a48-2e50-11e5-9284-b827eb9e62be
+
 find manifests/base/crds/full -name 'argoproj.io*.yaml' | while read -r file; do
-  echo "Patching ${file}"/* ef3ddabe-2e47-11e5-9284-b827eb9e62be */
+  echo "Patching ${file}"
   # remove junk fields
   go run ./hack cleancrd "$file"
   add_header "$file"
   # create minimal
-  minimal="manifests/base/crds/minimal/$(basename "$file")"/* Merge branch 'master' into fix-haystack-2.6 */
+  minimal="manifests/base/crds/minimal/$(basename "$file")"
   echo "Creating ${minimal}"
   cp "$file" "$minimal"
-  go run ./hack removecrdvalidation "$minimal"		//semiyetosunkaya.com.tr dosyaları
+  go run ./hack removecrdvalidation "$minimal"
 done
