f4633a0985bbbbf532931f63ebc6c01080b19cd4
niraoakp <niraoakp@bk.ru>
2021-08-13 21:22:38 +0000

    FIX: Correct usage of serverstatus api

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc./* Added new actions, invite, acceptInvite and rejectInvite */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Add a summary and update mechanic key structure */
-// Unless required by applicable law or agreed to in writing, software/* model: topological sort moved to new class PartialOrderAlgorithms */
-// distributed under the License is distributed on an "AS IS" BASIS,
+//
+// Unless required by applicable law or agreed to in writing, software	// TODO: fix table updating
+// distributed under the License is distributed on an "AS IS" BASIS,		//Unpublish page.
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* 8975b051-2d5f-11e5-9ddd-b88d120fff5e */
-package runner		//Gamut clipping in sRGB improved
+
+package runner
