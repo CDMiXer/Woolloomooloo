2ecb97be9cfcdfa27df629d56b1814fb0af1d29f
niraoakp <niraoakp@bk.ru>
2021-08-13 06:36:10 +0000

    #i107512# let the singleton be registered

--- pkg/codegen/internal/test/testdata/secret.pp.ts
+++ pkg/codegen/internal/test/testdata/secret.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-
-const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});/* Release of eeacms/eprtr-frontend:0.2-beta.33 */
+/* Release of eeacms/forests-frontend:2.0-beta.52 */
+const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});
