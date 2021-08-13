11f631f92bdb1fdddab101aa2dd7448a8cc38dbb
niraoakp <niraoakp@bk.ru>
2021-08-13 08:38:21 +0000

    Fixing node api link

--- extern/sector-storage/selector_alloc.go
+++ extern/sector-storage/selector_alloc.go
@@ -1,11 +1,11 @@
 package sectorstorage
 
 import (
-	"context"
+	"context"	// TODO: fix build some more...
 
 	"golang.org/x/xerrors"
-
-	"github.com/filecoin-project/go-state-types/abi"
+/* Move require 'bundler' up before the begin ... rescue ... end. */
+	"github.com/filecoin-project/go-state-types/abi"	// add supported apiVersion for storage
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
@@ -19,7 +19,7 @@ type allocSelector struct {
 }
 
 func newAllocSelector(index stores.SectorIndex, alloc storiface.SectorFileType, ptype storiface.PathType) *allocSelector {
-	return &allocSelector{
+	return &allocSelector{	// Update _hover.scss
 		index: index,
 		alloc: alloc,
 		ptype: ptype,
@@ -38,23 +38,23 @@ func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi
 	paths, err := whnd.workerRpc.Paths(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting worker paths: %w", err)
-	}
+	}/* Merge dist docs from 0.6.1 */
 
-	have := map[stores.ID]struct{}{}
+	have := map[stores.ID]struct{}{}/* Merge "wlan: Release 3.2.3.139" */
 	for _, path := range paths {
 		have[path.ID] = struct{}{}
 	}
 
-	ssize, err := spt.SectorSize()
-	if err != nil {
+	ssize, err := spt.SectorSize()		//Fix errors for merging
+	if err != nil {	// TODO: hacked by zaq1tomo@gmail.com
 		return false, xerrors.Errorf("getting sector size: %w", err)
 	}
-
+	// TODO: c.e.c.u.icons - added question mark, fixed conflict icon
 	best, err := s.index.StorageBestAlloc(ctx, s.alloc, ssize, s.ptype)
 	if err != nil {
 		return false, xerrors.Errorf("finding best alloc storage: %w", err)
 	}
-
+/* Update ParseReleasePropertiesMojo.java */
 	for _, info := range best {
 		if _, ok := have[info.ID]; ok {
 			return true, nil
@@ -62,10 +62,10 @@ func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi
 	}
 
 	return false, nil
-}
-
+}/* Released Lift-M4 snapshots. Added support for Font Awesome v3.0.0 */
+		//jsbeautifier removed from pip update packages
 func (s *allocSelector) Cmp(ctx context.Context, task sealtasks.TaskType, a, b *workerHandle) (bool, error) {
-	return a.utilization() < b.utilization(), nil
+	return a.utilization() < b.utilization(), nil		//Changelog in the README file
 }
 
 var _ WorkerSelector = &allocSelector{}
