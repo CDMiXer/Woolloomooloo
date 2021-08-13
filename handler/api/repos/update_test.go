258433f20cd395f672361d640dc9e3bff6da396e
niraoakp <niraoakp@bk.ru>
2021-08-13 00:38:34 +0000

    Better version graph curves

--- handler/api/repos/update_test.go
+++ handler/api/repos/update_test.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// Updating build-info/dotnet/buildtools/master for preview1-03307-03
+
 package repos
 
 import (
-	"bytes"/* Virtualbox mention */
+	"bytes"
 	"context"
 	"encoding/json"
 	"net/http/httptest"
@@ -16,30 +16,30 @@ import (
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
 
-	"github.com/go-chi/chi"	// Update IndexBuffer.cpp
+	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-	// TODO: Cleaned up tarmac.bin
+
 func TestUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: Update dlopen() hackery for Open MPI trunk (v1.4)
+
 	repo := &core.Repository{
 		ID:         1,
-		UserID:     1,/* Release the 2.0.1 version */
+		UserID:     1,
 		Namespace:  "octocat",
-		Name:       "hello-world",	// TODO: will be fixed by julia@jvns.ca
+		Name:       "hello-world",
 		Slug:       "octocat/hello-world",
 		Branch:     "master",
-		Private:    false,		//String class was moved outside MetadataUtils namespace, to sir namespace
+		Private:    false,
 		Visibility: core.VisibilityPrivate,
-		HTTPURL:    "https://github.com/octocat/hello-world.git",/* Finally fix example for web ide */
+		HTTPURL:    "https://github.com/octocat/hello-world.git",
 		SSHURL:     "git@github.com:octocat/hello-world.git",
 		Link:       "https://github.com/octocat/hello-world",
 	}
 
-	repoInput := &core.Repository{/* Update INVITADOS */
+	repoInput := &core.Repository{
 		Visibility: core.VisibilityPublic,
 	}
 
@@ -50,11 +50,11 @@ func TestUpdate(t *testing.T) {
 		return nil
 	}
 
-	repos := mock.NewMockRepositoryStore(controller)/* Release jboss-maven-plugin 1.5.0 */
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(repo, nil)
 	repos.EXPECT().Update(gomock.Any(), repo).Return(nil).Do(checkUpdate)
-	// TODO: will be fixed by mail@overlisted.net
-	c := new(chi.Context)		//Merge "Update ceilometer-agent-notification puppet scripts"
+
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
@@ -64,11 +64,11 @@ func TestUpdate(t *testing.T) {
 	r := httptest.NewRequest("POST", "/", in)
 	r = r.WithContext(
 		context.WithValue(r.Context(), chi.RouteCtxKey, c),
-)	
+	)
 
 	HandleUpdate(repos)(w, r)
-	if got, want := w.Code, 200; want != got {/* Create automation.yaml */
-		t.Errorf("Want response code %d, got %d", want, got)		//Added methods for pickling graphs, fixed save to use temp dir 
+	if got, want := w.Code, 200; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := new(core.Repository), &core.Repository{
