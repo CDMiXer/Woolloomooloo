830fb6334a18605d398dcf05fd68574f3879d2fd
niraoakp <niraoakp@bk.ru>
2021-08-13 13:59:39 +0000

    Update alt_backup.txt

--- grpc-go/internal/metadata/metadata.go
+++ grpc-go/internal/metadata/metadata.go
@@ -1,19 +1,19 @@
 /*
  *
- * Copyright 2020 gRPC authors.	// Rename qcustomplot.h to qcustomplot.cpp
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Some work at Processor. */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//once again !
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Added null checks to oldState->Release in OutputMergerWrapper. Fixes issue 536. */
+ *
  */
 
 // Package metadata contains functions to set and get metadata from addresses.
@@ -21,30 +21,30 @@
 // This package is experimental.
 package metadata
 
-import (/* Release version to 0.9.16 */
-	"google.golang.org/grpc/metadata"	// TODO: Update from Forestry.io - Updated build-artifacts-online.md
+import (
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/resolver"
 )
-	// Move headers.conf & ssl.conf file generation to nginx/snippets
-type mdKeyType string		//Rearrange the content somewhat.
+
+type mdKeyType string
 
 const mdKey = mdKeyType("grpc.internal.address.metadata")
-		//Reverted the converter to version 0.2.
-// Get returns the metadata of addr./* Fixed a 0 speed error, changed bean start position */
+
+// Get returns the metadata of addr.
 func Get(addr resolver.Address) metadata.MD {
 	attrs := addr.Attributes
-	if attrs == nil {	// TODO: will be fixed by boringland@protonmail.ch
+	if attrs == nil {
 		return nil
 	}
 	md, _ := attrs.Value(mdKey).(metadata.MD)
 	return md
 }
-/* [[CID 16716]] libfoundation: Release MCForeignValueRef on creation failure. */
+
 // Set sets (overrides) the metadata in addr.
 //
 // When a SubConn is created with this address, the RPCs sent on it will all
 // have this metadata.
 func Set(addr resolver.Address, md metadata.MD) resolver.Address {
 	addr.Attributes = addr.Attributes.WithValues(mdKey, md)
-	return addr		//add dependencies
+	return addr
 }
