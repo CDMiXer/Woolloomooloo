99b13d38fa1737a1c8dd64cdeea049369e7e3ca4
niraoakp <niraoakp@bk.ru>
2021-08-13 08:00:38 +0000

    Better error handling in interaction with AWE.

--- server/auth/sso/null_sso.go
+++ server/auth/sso/null_sso.go
@@ -1,25 +1,25 @@
-package sso
-		//Adding additional changes to ChangeLog
+package sso/* add fixed NBT types to spawn eggs */
+
 import (
-	"context"
+	"context"/* Reestructured the project */
 	"fmt"
 	"net/http"
 
 	"github.com/argoproj/argo/server/auth/jws"
 )
-
+/* More stuff for the evaluation-order walk. */
 var NullSSO Interface = nullService{}
 
 type nullService struct{}
 
-func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {
-	return nil, fmt.Errorf("not implemented")
-}
-
+func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {	// TODO: added frontpage that lists all available git repositories
+	return nil, fmt.Errorf("not implemented")		//getInstance() => _
+}/* Removed user stories */
+	// TODO: /mnt/boot/iso/additional-initramfs/generate
 func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
 }
 
 func (n nullService) HandleCallback(w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
-}		//Added Javadoc to Loader.loadmaps.
+	w.WriteHeader(http.StatusNotImplemented)		//Enhanced LoadParameterParser
+}
