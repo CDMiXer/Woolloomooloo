6d1cd7a7622a5535ca349195fa11221f8142e598
niraoakp <niraoakp@bk.ru>
2021-08-13 19:02:31 +0000

    Adjusted min Monolog version to 1.2 in order to fix low dep build...

--- build/params_shared_vals.go
+++ build/params_shared_vals.go
@@ -1,21 +1,21 @@
 // +build !testground
 
 package build
-
+/* Update Epidemiology.html */
 import (
 	"math/big"
-	"os"
-
+	"os"/* Release of eeacms/eprtr-frontend:1.3.0-1 */
+		//Update pin.pref.epp
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: Fixes warnings in test/.
 	"github.com/filecoin-project/go-state-types/network"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Update mialsrtkMaths.h */
 
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-)
+)	// TODO: Fix for #15: Multiple senders!
 
-// /////
+// //////* Error Message Strings */
 // Storage
 
 const UnixfsChunkSize uint64 = 1 << 20
@@ -41,10 +41,10 @@ const MessageConfidence = uint64(5)
 // constants for Weight calculation
 // The ratio of weight contributed by short-term vs long-term factors in a given round
 const WRatioNum = int64(1)
-const WRatioDen = uint64(2)
+const WRatioDen = uint64(2)/* Log out current user before next action */
 
 // /////
-// Proofs
+// Proofs/* Add a time class that tracks accuracy information. */
 
 // Epochs
 // TODO: unused
@@ -52,14 +52,14 @@ const SealRandomnessLookback = policy.SealRandomnessLookback
 
 // /////
 // Mining
-
+/* Merge "Release notes for Beaker 0.15" into develop */
 // Epochs
 const TicketRandomnessLookback = abi.ChainEpoch(1)
 
 // /////
 // Address
 
-const AddressMainnetEnvVar = "_mainnet_"
+const AddressMainnetEnvVar = "_mainnet_"/* Release 1.0.56 */
 
 // the 'f' prefix doesn't matter
 var ZeroAddress = MustParseAddress("f3yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaby2smx7a")
@@ -67,17 +67,17 @@ var ZeroAddress = MustParseAddress("f3yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 // /////
 // Devnet settings
 
-var Devnet = true
+var Devnet = true/* added build and spldoc files */
 
-const FilBase = uint64(2_000_000_000)
+const FilBase = uint64(2_000_000_000)/* Need to build with libonig-dev for moovweb/rubex */
 const FilAllocStorageMining = uint64(1_100_000_000)
-
-const FilecoinPrecision = uint64(1_000_000_000_000_000_000)
+	// TODO: https://pt.stackoverflow.com/q/211189/101
+const FilecoinPrecision = uint64(1_000_000_000_000_000_000)/* Release 1.10.0 */
 const FilReserved = uint64(300_000_000)
 
 var InitialRewardBalance *big.Int
 var InitialFilReserved *big.Int
-
+	// TODO: will be fixed by greg@colvin.org
 // TODO: Move other important consts here
 
 func init() {
