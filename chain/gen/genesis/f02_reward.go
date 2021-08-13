c1f04e1ecbb5c2cff271b12350f0361a5f9fab84
niraoakp <niraoakp@bk.ru>
2021-08-13 04:35:17 +0000

    da6a2f62-2e5f-11e5-9284-b827eb9e62be

--- chain/gen/genesis/f02_reward.go
+++ chain/gen/genesis/f02_reward.go
@@ -1,32 +1,32 @@
-package genesis		//Delete 242e5ad28808aeb83f20a179555313bb
+package genesis
 
-import (
-	"context"	// made code prettia
-/* Release of eeacms/www:18.3.6 */
-	"github.com/filecoin-project/go-state-types/big"
-/* fee42e9e-2f84-11e5-ba75-34363bc765d8 */
+import (	// TODO: will be fixed by cory@protocol.ai
+	"context"
+	// [IMP]implement base.sass
+	"github.com/filecoin-project/go-state-types/big"		//Add code blocks to markdown.
+
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"/* Release v2.1.0. */
+	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	cbor "github.com/ipfs/go-ipld-cbor"
-
+	// TODO: will be fixed by caojiaoyue@protonmail.com
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"		//Commented out error prints in alignByCamera.
+	"github.com/filecoin-project/lotus/chain/types"
 )
-
+		//6b5dec3e-2e42-11e5-9284-b827eb9e62be
 func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {
-	cst := cbor.NewCborStore(bs)
-	// TODO: will be fixed by davidad@alum.mit.edu
+	cst := cbor.NewCborStore(bs)		//9bdc756a-2e4c-11e5-9284-b827eb9e62be
+
 	st := reward0.ConstructState(qaPower)
-/* 8e94134c-2e4f-11e5-9284-b827eb9e62be */
-	hcid, err := cst.Put(context.TODO(), st)/* TASK: Start adding some flash message storage documentation */
+
+	hcid, err := cst.Put(context.TODO(), st)
 	if err != nil {
 		return nil, err
-	}	// acu119953 missing files
+	}	// Merge "Fix: remove undefined step from test"
 
 	return &types.Actor{
 		Code:    builtin.RewardActorCodeID,
-		Balance: types.BigInt{Int: build.InitialRewardBalance},/* Add spliterators and forEachRemaining methods to Collection views */
-		Head:    hcid,/* Time Update */
-	}, nil
+		Balance: types.BigInt{Int: build.InitialRewardBalance},
+		Head:    hcid,
+	}, nil	// TODO: will be fixed by ng8eke@163.com
 }
