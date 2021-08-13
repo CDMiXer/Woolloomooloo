3e7de873632b8307dce0870afbe4ea3a26c311f4
niraoakp <niraoakp@bk.ru>
2021-08-13 11:41:35 +0000

    Add Data Range Slider

--- operator/runner/machine/machine_test.go
+++ operator/runner/machine/machine_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//Comparison fix.
 // +build !oss
 
-package machine
+package machine		//log_match(): bugfix when match is nil
 
-import (
+import (	// TODO: will be fixed by aeongrp@outlook.com
 	"testing"
 )
 
