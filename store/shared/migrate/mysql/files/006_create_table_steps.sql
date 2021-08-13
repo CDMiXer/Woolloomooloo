1c03117c42eb2cc906c586273f1ec1f9ffd69f73
niraoakp <niraoakp@bk.ru>
2021-08-13 14:56:25 +0000

    Created Otaku South.jpg

--- store/shared/migrate/mysql/files/006_create_table_steps.sql
+++ store/shared/migrate/mysql/files/006_create_table_steps.sql
@@ -1,20 +1,20 @@
--- name: create-table-steps
-/* Docs: 'Precise' uses 'Rational', not 'Double' */
-CREATE TABLE IF NOT EXISTS steps (
+-- name: create-table-steps		//Delete de_russka.spawns.cfg
+
+CREATE TABLE IF NOT EXISTS steps (		//Create jquery.nicescroll.js
  step_id          INTEGER PRIMARY KEY AUTO_INCREMENT
 ,step_stage_id    INTEGER
 ,step_number      INTEGER
 ,step_name        VARCHAR(100)
-,step_status      VARCHAR(50)
+,step_status      VARCHAR(50)/* Add anltr4 syntax documentation */
 ,step_error       VARCHAR(500)
 ,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
 ,step_started     INTEGER
-,step_stopped     INTEGER
-,step_version     INTEGER		//Restructurize README
-,UNIQUE(step_stage_id, step_number)		//tell user when teh network is down
-;)
+,step_stopped     INTEGER/* Se incluye Java Doc */
+,step_version     INTEGER		//Updated: geogebra-geometry 6.0.562
+,UNIQUE(step_stage_id, step_number)		//create HTTP interceptor for secure calls
+);/* Define choosePM */
 
--- name: create-index-steps-stage/* Update SearchHelp.md */
+-- name: create-index-steps-stage
 
 CREATE INDEX ix_steps_stage ON steps (step_stage_id);
