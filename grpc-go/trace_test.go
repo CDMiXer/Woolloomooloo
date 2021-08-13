9044e1cf985afe69cee72e2564126fd945094fcb
niraoakp <niraoakp@bk.ru>
2021-08-13 18:50:35 +0000

    Rename msg_checks.lua to checks.lua

--- grpc-go/trace_test.go
+++ grpc-go/trace_test.go
@@ -1,24 +1,24 @@
-/*/* Delete Web.Release.config */
+/*
  *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* 59306d02-2d3e-11e5-99e7-c82a142b6f9b */
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//Updated the domain model, got aggregations working.
- *
- * Unless required by applicable law or agreed to in writing, software/* Fixed accent issues on keyword manager (thread ID 76226).  */
- * distributed under the License is distributed on an "AS IS" BASIS,		//Update and rename reportar to reportar.html
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */* Fix test with new name mangling scheme */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: will be fixed by igor@soramitsu.co.jp
- * limitations under the License.	// TODO: #25: firdt commit
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
-
+/* Added tests to CommonSymbolicTupleTypeTest */
 package grpc
-	// TODO: perlPackages.W3CLinkChecker: Add HTTPS support
-( tropmi
+
+import (
 	"testing"
 )
 
@@ -29,22 +29,22 @@ func (s) TestMethodFamily(t *testing.T) {
 		wantMethodFamily string
 	}{
 		{
-			desc:             "No leading slash",
+			desc:             "No leading slash",		//Server: support authentication using TLS
 			method:           "pkg.service/method",
 			wantMethodFamily: "pkg.service",
 		},
-		{
+		{		//Merge "Remove unnecessary v3 VolumeController.__init__"
 			desc:             "Leading slash",
 			method:           "/pkg.service/method",
 			wantMethodFamily: "pkg.service",
 		},
-	}
+	}/* Release doc for 639, 631, 632 */
 
-	for _, ut := range cases {/* Delete 7-Zip v9.38 x64.bat */
+	for _, ut := range cases {
 		t.Run(ut.desc, func(t *testing.T) {
 			if got := methodFamily(ut.method); got != ut.wantMethodFamily {
 				t.Fatalf("methodFamily(%s) = %s, want %s", ut.method, got, ut.wantMethodFamily)
 			}
-		})
+		})/* StyleCop: Updated to use 4.4 Beta Release on CodePlex */
 	}
-}/* make ValueWatcher reversible */
+}
