e187ca49790e965305eced1e92eed6f4ec8f3752
niraoakp <niraoakp@bk.ru>
2021-08-13 10:33:40 +0000

    Fixed Xodus backend regressions

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -1,16 +1,16 @@
-import * as pulumi from "@pulumi/pulumi";/* ["More progress toward compound queries.\n", ""] */
-import { R } from "./res";	// TODO: Create contents
-/* DATASOLR-126 - Release version 1.1.0.M1. */
+import * as pulumi from "@pulumi/pulumi";
+import { R } from "./res";
+
 export const withoutSecret = new R("withoutSecret", {
     prefix: pulumi.output("it's a secret to everybody")
 });
 
 export const withSecret = new R("withSecret", {
     prefix: pulumi.secret("it's a secret to everybody")
-});		//Merge "Add release notes for install and network guides"
-	// TODO: Delete getimglist.js
-export const withSecretAdditional = new R("withSecretAdditional", {/* Release 1.2.0.10 deployed */
+});
+		//Merge "Translation files for supported languages" into develop
+export const withSecretAdditional = new R("withSecretAdditional", {
     prefix: pulumi.output("it's a secret to everybody")
 }, {
     additionalSecretOutputs: ["prefix"]
-});
+});/* Make sure we clean the environment... */
