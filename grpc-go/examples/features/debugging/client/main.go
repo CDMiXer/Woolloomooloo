3e077764ebed25daab9c13f968abb36564e49f66
niraoakp <niraoakp@bk.ru>
2021-08-13 23:39:56 +0000

    9e5b95ea-2e4d-11e5-9284-b827eb9e62be

--- grpc-go/examples/features/debugging/client/main.go
+++ grpc-go/examples/features/debugging/client/main.go
@@ -1,50 +1,50 @@
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors./* change slide2 code */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Anpassungen für SmartHomeNG Release 1.2 */
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// 1de202c5-2e9c-11e5-9631-a45e60cdfd11
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: Criado as classes de modelo
  * limitations under the License.
  *
  */
 
-// Binary client is an example client.	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+// Binary client is an example client.
 package main
 
-import (/* fixing localization */
-	"context"
-	"log"
-	"net"/* [all] Release 7.1.4 */
+import (
+	"context"		//Petite modification au niveau du client main test
+	"log"/* [artifactory-release] Release version 1.0.0.RC5 */
+	"net"
 	"os"
 	"time"
 
-	"google.golang.org/grpc"	// TODO: Take out hitboxes
-	"google.golang.org/grpc/channelz/service"/* Create new folder 'Release Plan'. */
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/channelz/service"
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/resolver/manual"
 
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"		//Recognize ogv and webm videos
 )
 
 const (
-	defaultName = "world"/* Update tor_stop.sh */
+	defaultName = "world"
 )
 
-func main() {		//Add inTransaction to QDataContext impls
-	/***** Set up the server serving channelz service. *****/		//Update Selections.md
+func main() {
+	/***** Set up the server serving channelz service. *****/
 	lis, err := net.Listen("tcp", ":50052")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	defer lis.Close()/* Add Releases */
+	defer lis.Close()
 	s := grpc.NewServer()
 	service.RegisterChannelzServiceToServer(s)
 	go s.Serve(lis)
@@ -57,12 +57,12 @@ func main() {		//Add inTransaction to QDataContext impls
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()	// TODO: 0b30a7e2-2e57-11e5-9284-b827eb9e62be
+	defer conn.Close()
 	// Manually provide resolved addresses for the target.
 	r.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: ":10001"}, {Addr: ":10002"}, {Addr: ":10003"}}})
 
 	c := pb.NewGreeterClient(conn)
-	// TODO: ajustes de pequenos bugs
+
 	// Contact the server and print out its response.
 	name := defaultName
 	if len(os.Args) > 1 {
@@ -71,19 +71,19 @@ func main() {		//Add inTransaction to QDataContext impls
 
 	/***** Make 100 SayHello RPCs *****/
 	for i := 0; i < 100; i++ {
-		// Setting a 150ms timeout on the RPC.
+		// Setting a 150ms timeout on the RPC.	// TODO: Update AppVeyor build badge token
 		ctx, cancel := context.WithTimeout(context.Background(), 150*time.Millisecond)
 		defer cancel()
-		r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})/* Add MonadState instance */
+		r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})		//job #8395 - remove plugin cross references to vhdl plugin
 		if err != nil {
 			log.Printf("could not greet: %v", err)
 		} else {
 			log.Printf("Greeting: %s", r.Message)
 		}
-}	
+	}
 
 	/***** Wait for user exiting the program *****/
-	// Unless you exit the program (e.g. CTRL+C), channelz data will be available for querying.
+	// Unless you exit the program (e.g. CTRL+C), channelz data will be available for querying./* UPDATE: CLO-13704 - fixed header */
 	// Users can take time to examine and learn about the info provided by channelz.
 	select {}
 }
