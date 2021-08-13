593612f965942342c68b84b44b06a730b860f16f
niraoakp <niraoakp@bk.ru>
2021-08-13 10:00:43 +0000

    Create fn_land_texture.h

--- pkg/codegen/internal/test/testdata/resource-options.pp.go
+++ pkg/codegen/internal/test/testdata/resource-options.pp.go
@@ -5,10 +5,10 @@ import (
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+		//fix client ref link
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		provider, err := providers.Newaws(ctx, "provider", &providers.awsArgs{
+		provider, err := providers.Newaws(ctx, "provider", &providers.awsArgs{/* Release for 4.11.0 */
 			Region: pulumi.String("us-west-2"),
 		})
 		if err != nil {
@@ -22,7 +22,7 @@ func main() {
 		}))
 		if err != nil {
 			return err
-		}
+		}		//Rename uninstall_uifile_de to uninstall_uifile_ger
 		return nil
-	})
+	})/* remove verification that cause unit tests to fail sometimes  */
 }
