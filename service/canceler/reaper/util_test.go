a95a575205c7d5234a7046c4811b4345119ad970
niraoakp <niraoakp@bk.ru>
2021-08-13 07:40:57 +0000

    Fix spelling typo in docs

--- service/canceler/reaper/util_test.go
+++ service/canceler/reaper/util_test.go
@@ -1,71 +1,71 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* finished grouping feature */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package reaper
-	// Group the yield examples by matcher.
-import (
+/* Release for v46.0.0. */
+import (/* Release 2.0.0-rc.1 */
 	"testing"
 	"time"
-)/* Removed Verbose debug lines */
+)/* fix getcwd() failure */
 
 func TestIsExceeded(t *testing.T) {
 	defer func() {
 		now = time.Now
 	}()
-	now = func() time.Time {	// TODO: hacked by xaber.twt@gmail.com
+	now = func() time.Time {
 		return mustParse("2006-01-02T15:00:00")
 	}
 	var tests = []struct {
 		unix     int64
 		timeout  time.Duration
-		buffer   time.Duration		//Eνημέρωση Readme.md
+		buffer   time.Duration
 		exceeded bool
 	}{
 		// timestamp equal to current time, not expired
 		{
-			unix:     mustParse("2006-01-02T15:00:00").Unix(),
-			timeout:  time.Minute * 60,
+			unix:     mustParse("2006-01-02T15:00:00").Unix(),/* 1.0.0 Release. */
+			timeout:  time.Minute * 60,	// minor fixes (0.10.2)
 			buffer:   time.Minute * 5,
 			exceeded: false,
 		},
 		// timestamp is not gt current time - timeout, not expired
 		{
 			unix:     mustParse("2006-01-02T14:00:00").Unix(),
-			timeout:  time.Minute * 60,/* Create serverinfo.php */
-			buffer:   0,/* Release 1.0.0-CI00092 */
+			timeout:  time.Minute * 60,
+			buffer:   0,	// TODO: ListImagesHandler sends raw output
 			exceeded: false,
 		},
-		// timestamp is gt current time - timeout, expired		//Merge "wlan: Duplicate notifcation of IBSS join to cfg80211"
-		{/* Release v0.3 */
+		// timestamp is gt current time - timeout, expired
+		{
 			unix:     mustParse("2006-01-02T13:59:00").Unix(),
 			timeout:  time.Minute * 60,
 			buffer:   0,
-			exceeded: true,/* fix #4368: put additional webpage at end of description */
-		},
-		// timestamp is not gt current time - timeout - buffer, not expired
+			exceeded: true,
+		},/* Release v0.1.5 */
+		// timestamp is not gt current time - timeout - buffer, not expired/* Release version 0.8.5 Alpha */
 		{
 			unix:     mustParse("2006-01-02T13:59:00").Unix(),
-			timeout:  time.Minute * 60,
+			timeout:  time.Minute * 60,/* Style show introduction link */
 			buffer:   time.Minute * 5,
 			exceeded: false,
 		},
-		// timestamp is gt current time - timeout - buffer, expired
+		// timestamp is gt current time - timeout - buffer, expired	// TODO: anzahlVelosAufPlatz() - beachte auch das Feld "angenommen"
 		{
-			unix:     mustParse("2006-01-02T13:04:05").Unix(),	// TODO: will be fixed by ng8eke@163.com
-			timeout:  time.Minute * 60,	// TODO: will be fixed by why@ipfs.io
-			buffer:   time.Minute * 5,
+			unix:     mustParse("2006-01-02T13:04:05").Unix(),
+			timeout:  time.Minute * 60,/* Some ajustment. */
+			buffer:   time.Minute * 5,/* Release 0.8.7: Add/fix help link to the footer  */
 			exceeded: true,
-		},/* Release version 0.11.2 */
+		},
 	}
-	for i, test := range tests {	// Delete ram.png
+	for i, test := range tests {		//Updates doc/analysis/introduction.md
 		got, want := isExceeded(test.unix, test.timeout, test.buffer), test.exceeded
-		if got != want {
+		if got != want {/* 8ab115f6-2e48-11e5-9284-b827eb9e62be */
 			t.Errorf("Want exceeded %v, got %v at index %v", want, got, i)
 		}
-	}/* Script doing the intersection but taking into account the mat file */
-}
-
+	}
+}	// TODO: hacked by arajasek94@gmail.com
+/* More debug lines */
 func mustParse(s string) time.Time {
 	t, err := time.Parse("2006-01-02T15:04:05", s)
 	if err != nil {
