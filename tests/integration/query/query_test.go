7afd7ad3f7576a64b53d838c02636f4bf85303cf
niraoakp <niraoakp@bk.ru>
2021-08-13 22:30:41 +0000

    cgame: don't project decals with lifetime 0 refs #592

--- tests/integration/query/query_test.go
+++ tests/integration/query/query_test.go
@@ -1,29 +1,29 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints/* Denote Spark 2.8.0 Release */
+package ints
 
 import (
-	"testing"/* Delete Package-Release.bash */
+	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// init swagger e swagger-ui
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-// TestQuery creates a stack and runs a query over the stack's resource ouptputs.	// throw IOException if directory creation fails (avoids NPE later on)
+// TestQuery creates a stack and runs a query over the stack's resource ouptputs.
 func TestQuery(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		// Create Pulumi resources.
 		Dir:          "step1",
 		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",
 		Dependencies: []string{"@pulumi/pulumi"},
-		CloudURL:     "file://~", // Required; we hard-code the stack name/* Merge "msm: mdss: Fix support for ARGB1555 and ARGB4444" */
+		CloudURL:     "file://~", // Required; we hard-code the stack name
 		EditDirs: []integration.EditDir{
 			// Try to create resources during `pulumi query`. This should fail.
 			{
 				Dir:           "step2",
-				Additive:      true,		//catalog.cart.xslt.whitelist back in gpt.xml for XslBundler.
+				Additive:      true,
 				QueryMode:     true,
-				ExpectFailure: true,	// TODO: will be fixed by magik6k@gmail.com
+				ExpectFailure: true,
 			},
 			// Run a query during `pulumi query`. Should succeed.
 			{
