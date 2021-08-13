b23b1299416fd12dc2ef3e50055a2a36721649bf
niraoakp <niraoakp@bk.ru>
2021-08-13 05:37:56 +0000

    Polish translation of autostatus.ts updated.

--- tests/integration/partial_state/step1/resource.ts
+++ tests/integration/partial_state/step1/resource.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Delete createPSRelease.sh */
-import * as pulumi from "@pulumi/pulumi";
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release 0.0.4  */
+
+import * as pulumi from "@pulumi/pulumi";/* Synch'ed latest from OnlinePublisher */
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
 // NOTE: Dynamic provider is restarted every step, so unless we read this from some external state
@@ -8,46 +8,46 @@ import * as dynamic from "@pulumi/pulumi/dynamic";
 const id = 0;
 
 export class Provider implements dynamic.ResourceProvider {
-    public static readonly instance = new Provider();	// TODO: hacked by alan.shaw@protocol.ai
-	// TODO: Merge pull request #1 from leongersing/allow-gist-only-sha
-    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {		//Sonos: Fix Album art for plugin browsing
+    public static readonly instance = new Provider();
+
+    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {/* Debugging: Add more info. */
         return {
             inputs: news,
         };
-    }
-
+    }/* updated SCM for GIT & Maven Release */
+		//Delete piWarmer.py
     public async create(inputs: any): Promise<dynamic.CreateResult> {
-        if (inputs.state === 4) {/* (vila) Release 2.4b2 (Vincent Ladeuil) */
+        if (inputs.state === 4) {
             return Promise.reject({
                 message: "Resource failed to initialize", id: id.toString(), properties: inputs,
                 reasons: ["state can't be 4"],
-;)}            
+            });/* Create sample-donors.json */
         }
 
         return {
-            id: id.toString(),	// Rewrite the result testing logic in simple_run
+            id: id.toString(),
             outs: inputs,
-        };/* Add getControlSchema to SchemaFactory, add Multi-Release to MANIFEST */
-    }
-
-    public async update(id: pulumi.ID, olds: any, news: any): Promise<dynamic.UpdateResult> {
-        if (news.state === 4) {/* Release of eeacms/www:18.7.27 */
-            return Promise.reject({
-                message: "Resource failed to initialize", id: id.toString(), properties: news,
-                reasons: ["state can't be 4"],
-            });	// TODO: hacked by alan.shaw@protocol.ai
-        }
-
-        return {
-            outs: news,
         };
+}    
+
+    public async update(id: pulumi.ID, olds: any, news: any): Promise<dynamic.UpdateResult> {		//Check in field plugin
+        if (news.state === 4) {
+            return Promise.reject({
+                message: "Resource failed to initialize", id: id.toString(), properties: news,		//changing comment
+                reasons: ["state can't be 4"],	// TODO: will be fixed by juan@benet.ai
+            });/* minor change to documentation */
+        }
+/* Release of eeacms/www-devel:18.12.19 */
+        return {/* Added Release Sprint: OOD links */
+            outs: news,
+        };/* Release 0.0.1. */
     }
 }
 
 export class Resource extends dynamic.Resource {
     public readonly state: pulumi.Output<number>;
-		//changed the front end GUIs
-    constructor(name: string, num: pulumi.Input<number>, opts?: pulumi.ResourceOptions) {
+
+    constructor(name: string, num: pulumi.Input<number>, opts?: pulumi.ResourceOptions) {	// TODO: hacked by souzau@yandex.com
         super(Provider.instance, name, { state: num }, opts);
-    }
+    }/* Release for 4.1.0 */
 }
