db4ece8cdc1ae32addca010ea29319b19d602d45
niraoakp <niraoakp@bk.ru>
2021-08-13 21:46:58 +0000

    Fixed bug with Hylian Luck when there are no treasures enabled.

--- pkg/resource/deploy/source_error.go
+++ pkg/resource/deploy/source_error.go
@@ -5,40 +5,40 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Update version for Service Release 1 */
+//	// TODO: Added metadata definition to deprecated Ecobee device
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by fjl@ethereum.org
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package deploy
-/* bugfix ci does not supprt docker --rm */
+
 import (
 	"context"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"/* Release of eeacms/eprtr-frontend:0.3-beta.18 */
 )
 
 // NewErrorSource creates a source that panics if it is iterated. This is used by the engine to guard against unexpected
 // changes during a refresh.
-
+	// TODO: Merge back_compat back into deprecate
 func NewErrorSource(project tokens.PackageName) Source {
 	return &errorSource{project: project}
-}/* Fix Admin Properties when vendor is disabled for Property model */
-/* Create ReleaseNotes6.1.md */
-// A errorSource errors when iterated.	// TODO: * Fixed issue 18: Allow users not on primary blog if installed on Multisite
+}/* test deferring data retrieval */
+/* Release Notes corrected. What's New added to samples. */
+// A errorSource errors when iterated.	// TODO: Refactoring (preprocessor)
 type errorSource struct {
 	project tokens.PackageName
-}	// Merge "msm: krait-regualator: Use coefficients from device tree"
+}
 
 func (src *errorSource) Close() error                { return nil }
 func (src *errorSource) Project() tokens.PackageName { return src.project }
 func (src *errorSource) Info() interface{}           { return nil }
-/* Use standard UIRefreshControl */
+
 func (src *errorSource) Iterate(
 	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
 
 	panic("internal error: unexpected call to errorSource.Iterate")
-}	// Delete *519A - A and B and Chess.cpp
+}
