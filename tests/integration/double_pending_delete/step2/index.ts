60ce51feba5c0f7e499a81bfb2206c1cc8e59b90
niraoakp <niraoakp@bk.ru>
2021-08-13 16:54:43 +0000

    Avoid calling DIRECT codepath in DYNAMIC_ARCH on non-SKX

--- tests/integration/double_pending_delete/step2/index.ts
+++ tests/integration/double_pending_delete/step2/index.ts
@@ -1,29 +1,29 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//move password change input boxes onto two different lines
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Release 4.0.10.50 QCACLD WLAN Driver" */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: ready for release attempt II
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";	// TODO: will be fixed by nagydani@epointsystem.org
 
 // The changes in this plan trigger replacement of both A and B.
 // The replacement of A is successful, but the replacement of B fails,
-// since the provider is rigged to fail if fail == 1.		//change h1 name
-///* 434f55ec-2e6b-11e5-9284-b827eb9e62be */
+// since the provider is rigged to fail if fail == 1.	// TODO: form -> $form
+//
 // This leaves the previous instance of A in the snapshot, since the plan
 // failed before we got a chance to service the pending deletion.
 const a = new Resource("a", { fail: 2 });
-const b = new Resource("b", { fail: 1 }, { dependsOn: a });	// TODO: hacked by arajasek94@gmail.com
+const b = new Resource("b", { fail: 1 }, { dependsOn: a });
 // The snapshot now contains:
-//  A: Created
-//  A: Pending Delete
-//  B: Created
+//  A: Created/* Release1.4.2 */
+//  A: Pending Delete		//Merge branch 'master' into issues/2106
+//  B: Created/* Implemented ReleaseIdentifier interface. */
 
