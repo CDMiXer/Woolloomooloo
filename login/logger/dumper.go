53226652c87aba7223b49c687f235c751b35ce69
niraoakp <niraoakp@bk.ru>
2021-08-13 09:07:04 +0000

    [IMP] improve template of export tree structure and tree open on 2 level.

--- login/logger/dumper.go
+++ login/logger/dumper.go
@@ -3,43 +3,43 @@
 // license that can be found in the LICENSE file.
 
 package logger
-/* Release, not commit, I guess. */
+
 import (
 	"net/http"
 	"net/http/httputil"
 	"os"
-)	// TODO: hacked by onhardev@bk.ru
-	// New translations en-GB.plg_content_sermonspeaker.sys.ini (Portuguese, Brazilian)
+)
+
 // Dumper dumps the http.Request and http.Response
 // message payload for debugging purposes.
 type Dumper interface {
-	DumpRequest(*http.Request)	// TODO: hacked by indexxuan@gmail.com
+	DumpRequest(*http.Request)
 	DumpResponse(*http.Response)
 }
 
 // DiscardDumper returns a no-op dumper.
-func DiscardDumper() Dumper {/* * 0.66.8061 Release (hopefully) */
+func DiscardDumper() Dumper {
 	return new(discardDumper)
 }
 
-type discardDumper struct{}	// Merge "Fix issue #3415137: New wallpaper size breaks thumbnails." into honeycomb
+type discardDumper struct{}
 
 func (*discardDumper) DumpRequest(*http.Request)   {}
 func (*discardDumper) DumpResponse(*http.Response) {}
 
 // StandardDumper returns a standard dumper.
 func StandardDumper() Dumper {
-	return new(standardDumper)/* Release 0.94.360 */
-}/* 0.2.2 Release */
+	return new(standardDumper)
+}
 
-type standardDumper struct{}	// Work on pathfinding (Astar.ghostTarget not working yet)
-/* Rename Build.Release.CF.bat to Build.Release.CF.bat.use_at_your_own_risk */
+type standardDumper struct{}
+
 func (*standardDumper) DumpRequest(req *http.Request) {
 	dump, _ := httputil.DumpRequestOut(req, true)
 	os.Stdout.Write(dump)
 }
-/* Release date for v47.0.0 */
+
 func (*standardDumper) DumpResponse(res *http.Response) {
 	dump, _ := httputil.DumpResponse(res, true)
 	os.Stdout.Write(dump)
-}		//Update CellMeasurer.md
+}
