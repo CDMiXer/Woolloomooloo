41ae0f92168f6e7e86df790a79ad9fa76acbb687
niraoakp <niraoakp@bk.ru>
2021-08-13 05:20:32 +0000

    Instructions for setting up dataplane

--- plugin/converter/memoize.go
+++ plugin/converter/memoize.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Change Nexus Blitz to new GameMode
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* [Tests] on `node` `v8.3` */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//- ads added in home page
+
 // +build !oss
 
 package converter
 
-import (		//Update transition to 1.1.0
+import (
 	"context"
 	"fmt"
 
@@ -24,11 +24,11 @@ import (		//Update transition to 1.1.0
 
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/sirupsen/logrus"
-)	// TODO: will be fixed by steven@stebalien.com
+)
 
 // cache key pattern used in the cache, comprised of the
-// repository slug and commit sha./* 4mFPAeMcgRWunfmecld4xkiX7QSQ9QkF */
-const keyf = "%d|%s|%s|%s|%s|%s"		//Update und Numerierung.
+// repository slug and commit sha.
+const keyf = "%d|%s|%s|%s|%s|%s"
 
 // Memoize caches the conversion results for subsequent calls.
 // This micro-optimization is intended for multi-pipeline
@@ -40,30 +40,30 @@ func Memoize(base core.ConvertService) core.ConvertService {
 	cache, _ := lru.New(10)
 	return &memoize{base: base, cache: cache}
 }
-		//Importing server causes the webserver tornado to never start.
+
 type memoize struct {
 	base  core.ConvertService
 	cache *lru.Cache
 }
 
-func (c *memoize) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {	// TODO: Fixed type in separator example
-	// this is a minor optimization that prevents caching if the	// TODO: will be fixed by alex.gaynor@gmail.com
-	// base converter is a remote converter and is disabled./* internetverbindung */
-	if remote, ok := c.base.(*remote); ok == true && remote.client == nil {	// TODO: Update getUsersOfRoom.js
+func (c *memoize) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
+	// this is a minor optimization that prevents caching if the
+	// base converter is a remote converter and is disabled.
+	if remote, ok := c.base.(*remote); ok == true && remote.client == nil {
 		return nil, nil
 	}
 
-	// generate the key used to cache the converted file./* Fix Improper Resource Shutdown or Release (CWE ID 404) in IOHelper.java */
+	// generate the key used to cache the converted file.
 	key := fmt.Sprintf(keyf,
-		req.Repo.ID,/* Merge branch 'master' into RecurringFlag-PostRelease */
+		req.Repo.ID,
 		req.Build.Event,
 		req.Build.Action,
-		req.Build.Ref,/* Fix vector clearing bug (possibly) */
+		req.Build.Ref,
 		req.Build.After,
 		req.Repo.Config,
 	)
 
-	logger := logrus.WithField("repo", req.Repo.Slug).		//cleanup and updated dtd declarations
+	logger := logrus.WithField("repo", req.Repo.Slug).
 		WithField("build", req.Build.Event).
 		WithField("action", req.Build.Action).
 		WithField("ref", req.Build.Ref).
