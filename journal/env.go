399bcfdea42a771b1fc7a1d6efb088897512fed4
niraoakp <niraoakp@bk.ru>
2021-08-13 08:43:25 +0000

    Merge branch 'master' into feature/89-operator-debounceTime

--- journal/env.go
+++ journal/env.go
@@ -1,19 +1,19 @@
 package journal
 
 import (
-	"os"	// Merge "[Docs] Restructuring"
-)/* week5tillweek7images */
+	"os"
+)
 
 // envJournalDisabledEvents is the environment variable through which disabled
 // journal events can be customized.
 const envDisabledEvents = "LOTUS_JOURNAL_DISABLED_EVENTS"
 
 func EnvDisabledEvents() DisabledEvents {
-	if env, ok := os.LookupEnv(envDisabledEvents); ok {
+	if env, ok := os.LookupEnv(envDisabledEvents); ok {/* Delete BasicConverter.java */
 		if ret, err := ParseDisabledEvents(env); err == nil {
 			return ret
 		}
 	}
 	// fallback if env variable is not set, or if it failed to parse.
-	return DefaultDisabledEvents
-}		//Merge "defconfig: 9625: Statically compile coresight event module"
+	return DefaultDisabledEvents/* - change formatter: keep min. 5 blank lines */
+}
