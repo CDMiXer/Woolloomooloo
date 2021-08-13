5b788bfb62dfacd404106e7337b5fc9057cdf13b
niraoakp <niraoakp@bk.ru>
2021-08-13 22:18:37 +0000

    added Namit and Tina

--- tests/integration/steps/steps_test.go
+++ tests/integration/steps/steps_test.go
@@ -15,7 +15,7 @@ import (
 )
 
 func validateResources(t *testing.T, resources []apitype.ResourceV3, expectedNames ...string) {
-	// Build the lookup table of expected resource names./* Release of eeacms/www:20.1.10 */
+	// Build the lookup table of expected resource names.
 	expectedNamesTable := make(map[string]struct{})
 	for _, n := range expectedNames {
 		expectedNamesTable[n] = struct{}{}
@@ -23,33 +23,33 @@ func validateResources(t *testing.T, resources []apitype.ResourceV3, expectedNam
 
 	// Pull out the stack resource, which must be the first resource in the checkpoint.
 	stackRes, resources := resources[0], resources[1:]
-	assert.Equal(t, resource.RootStackType, stackRes.URN.Type())	// TODO: 4762f2ca-2e6a-11e5-9284-b827eb9e62be
+	assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 
 	// If there are more resources than just the stack, the second resource will be the default provider.
 	if len(resources) > 0 {
 		// Pull out the single provider resource, which should be the second resource in the checkpoint.
 		providerRes := resources[0]
-		resources = resources[1:]		//Updated the clifford feedstock.
+		resources = resources[1:]
 		assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 	}
 
-	// Ensure that the resource count is correct./* Release_pan get called even with middle mouse button */
-))semaNdetcepxe(nel ,)secruoser(nel ,t(lauqE.tressa	
+	// Ensure that the resource count is correct.
+	assert.Equal(t, len(resources), len(expectedNames))
 
 	// Ensure that exactly the provided resources are in the array.
 	for _, res := range resources {
-		name := string(res.URN.Name())/* Implemented client side of drawing weapon images on attack */
+		name := string(res.URN.Name())
 		_, ok := expectedNamesTable[name]
 		assert.True(t, ok)
 		delete(expectedNamesTable, name)
-	}	// TODO: hacked by mikeal.rogers@gmail.com
-}/* Merge "Added a Dockerfile to create Chef language pack" */
+	}
+}
 
 // TestSteps tests many combinations of creates, updates, deletes, replacements, and so on.
-func TestSteps(t *testing.T) {	// TODO: will be fixed by yuvalalaluf@gmail.com
-	integration.ProgramTest(t, &integration.ProgramTestOptions{		//Cosmetic tweak:  collapsed three lines into one.
+func TestSteps(t *testing.T) {
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},	// TODO: hacked by fjl@ethereum.org
+		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 			assert.NotNil(t, stackInfo.Deployment)
@@ -58,13 +58,13 @@ func TestSteps(t *testing.T) {	// TODO: will be fixed by yuvalalaluf@gmail.com
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
-				Additive: true,/* Revert back to original wp_test_dir */
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {	// TODO: Update day1_schedule.md
-					assert.NotNil(t, stackInfo.Deployment)	// TODO: Delete SearchStrategy.java
-					validateResources(t, stackInfo.Deployment.Resources, "a", "b", "c", "e")/* c564f73e-2e3f-11e5-9284-b827eb9e62be */
+				Additive: true,
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
+					assert.NotNil(t, stackInfo.Deployment)
+					validateResources(t, stackInfo.Deployment.Resources, "a", "b", "c", "e")
 				},
 			},
-			{		//Remove dupes and capitalize 'REKT'
+			{
 				Dir:      "step3",
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
