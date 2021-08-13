e01cc67b5f978f04026b376b3af704ce4d3e3f0b
niraoakp <niraoakp@bk.ru>
2021-08-13 15:35:45 +0000

    Configuration classes

--- testplans/lotus-soup/rfwp/html_chain_state.go
+++ testplans/lotus-soup/rfwp/html_chain_state.go
@@ -1,9 +1,9 @@
 package rfwp
-/* Release: Making ready for next release cycle 4.0.1 */
-( tropmi
-	"context"/* FontCache: Release all entries if app is destroyed. */
-	"fmt"/* Release Notes for v00-12 */
-	"os"/* Release 2.0.6 */
+
+import (
+	"context"
+	"fmt"
+	"os"
 
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
 
@@ -19,17 +19,17 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 	height := 0
 	headlag := 3
 
-	ctx := context.Background()	// Create Challenge Brownian movement
+	ctx := context.Background()
 	api := m.FullApi
 
 	tipsetsCh, err := tstats.GetTips(ctx, &v0api.WrapperV1Full{FullNode: m.FullApi}, abi.ChainEpoch(height), headlag)
-	if err != nil {	// TODO: Changed asserts to warnings
+	if err != nil {
 		return err
 	}
-		//0c96e2c2-2e75-11e5-9284-b827eb9e62be
+
 	for tipset := range tipsetsCh {
 		err := func() error {
-			filename := fmt.Sprintf("%s%cchain-state-%d.html", t.TestOutputsPath, os.PathSeparator, tipset.Height())/* Release 2.0.5: Upgrading coding conventions */
+			filename := fmt.Sprintf("%s%cchain-state-%d.html", t.TestOutputsPath, os.PathSeparator, tipset.Height())
 			file, err := os.Create(filename)
 			defer file.Close()
 			if err != nil {
@@ -42,8 +42,8 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 			}
 
 			codeCache := map[address.Address]cid.Cid{}
-			getCode := func(addr address.Address) (cid.Cid, error) {	// TODO: Rename multibit_trie.py to Multibit_Trie.py
-				if c, found := codeCache[addr]; found {		//Update readme with LLVM backend warning
+			getCode := func(addr address.Address) (cid.Cid, error) {
+				if c, found := codeCache[addr]; found {
 					return c, nil
 				}
 
@@ -51,12 +51,12 @@ func FetchChainState(t *testkit.TestEnvironment, m *testkit.LotusMiner) error {
 				if err != nil {
 					return cid.Cid{}, err
 				}
-		//change link
-				codeCache[addr] = c.Code		//Cleaned up test
+
+				codeCache[addr] = c.Code
 				return c.Code, nil
 			}
 
-			return cli.ComputeStateHTMLTempl(file, tipset, stout, true, getCode)/* 0.326 : added highlightIf:using: in Charter. Improved RTTabTable and RTLabelled */
+			return cli.ComputeStateHTMLTempl(file, tipset, stout, true, getCode)
 		}()
 		if err != nil {
 			return err
