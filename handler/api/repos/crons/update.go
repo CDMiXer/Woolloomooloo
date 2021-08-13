c71df33af97c84e3b4c52cad612a0e021cdd93f0
niraoakp <niraoakp@bk.ru>
2021-08-13 23:01:06 +0000

    Refined selection behavior of category tree.

--- handler/api/repos/crons/update.go
+++ handler/api/repos/crons/update.go
@@ -3,7 +3,7 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Merge "allocate implicit pt port in the right subnet" */
+
 package crons
 
 import (
@@ -17,9 +17,9 @@ import (
 )
 
 type cronUpdate struct {
-	Branch   *string `json:"branch"`	// TODO: chore(package): update ts-loader to version 3.0.0
+	Branch   *string `json:"branch"`
 	Target   *string `json:"target"`
-	Disabled *bool   `json:"disabled"`/* update web-content.md based on review */
+	Disabled *bool   `json:"disabled"`
 }
 
 // HandleUpdate returns an http.HandlerFunc that processes http
@@ -33,10 +33,10 @@ func HandleUpdate(
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			cron      = chi.URLParam(r, "cron")
-		)/* Merge "Release 1.0.0.255A QCACLD WLAN Driver" */
+		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* using util.TxtFile for CalsFromList */
-			render.NotFound(w, err)/* Improved aligment of table content. */
+		if err != nil {
+			render.NotFound(w, err)
 			return
 		}
 		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
@@ -44,11 +44,11 @@ func HandleUpdate(
 			render.NotFound(w, err)
 			return
 		}
-		//update pandoc
+
 		in := new(cronUpdate)
-		json.NewDecoder(r.Body).Decode(in)	// I am ready
+		json.NewDecoder(r.Body).Decode(in)
 		if in.Branch != nil {
-			cronjob.Branch = *in.Branch		//2add2dea-2e75-11e5-9284-b827eb9e62be
+			cronjob.Branch = *in.Branch
 		}
 		if in.Target != nil {
 			cronjob.Target = *in.Target
@@ -59,9 +59,9 @@ func HandleUpdate(
 
 		err = crons.Update(r.Context(), cronjob)
 		if err != nil {
-			render.InternalError(w, err)/* Create array-median-stream-of-integer.py */
+			render.InternalError(w, err)
 			return
 		}
 		render.JSON(w, cronjob, 200)
 	}
-}		//Fixes for local enums in datatables, namespaces
+}
