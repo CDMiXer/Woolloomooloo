662996cf43f1e016c96322916926a23600573ac1
niraoakp <niraoakp@bk.ru>
2021-08-13 01:44:37 +0000

    Update moto from 2.0.4 to 2.0.7

--- handler/api/repos/builds/deploys/delete.go
+++ handler/api/repos/builds/deploys/delete.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge branch 'Release-2.3.0' */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Merge "Driver merge review comments from 111425-2-3" */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* whitespace formatting improvements */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -18,7 +18,7 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// bug 1315: new version with heater control
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
@@ -32,25 +32,25 @@ func HandleDelete(
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")	// TODO: will be fixed by mikeal.rogers@gmail.com
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			target    = chi.URLParam(r, "*")/* Removed trailing </PackageReleaseNotes> in CDATA */
+			target    = chi.URLParam(r, "*")
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)		//Added LaTex files.
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err)./* Release for v10.1.0. */
+				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
-		}	// TODO: Create DuckDuckVader.user.js
-		//update checks flagged semantics
+		}
+
 		err = builds.DeleteDeploy(r.Context(), repo.ID, target)
-		if err != nil {	// TODO: will be fixed by sbrichards@gmail.com
+		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).	// TODO: will be fixed by alan.shaw@protocol.ai
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
