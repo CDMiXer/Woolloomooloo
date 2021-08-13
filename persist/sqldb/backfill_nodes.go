f4b14dfe68737278b75f8d2b79187c03c14c5ebb
niraoakp <niraoakp@bk.ru>
2021-08-13 15:26:09 +0000

    ramips: specify eeprom file name on RT305X

--- persist/sqldb/backfill_nodes.go
+++ persist/sqldb/backfill_nodes.go
@@ -5,45 +5,45 @@ import (
 	"fmt"
 
 	log "github.com/sirupsen/logrus"
-	"upper.io/db.v3"/* Merge "Pass roles manager to user manager" */
-	"upper.io/db.v3/lib/sqlbuilder"	// TODO: - Reseting meters grown to zero on new game.
+	"upper.io/db.v3"
+	"upper.io/db.v3/lib/sqlbuilder"
 
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"/* [artifactory-release] Release version 0.9.0.M2 */
-)/* [16338] Use bcprov from orbit */
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+)
 
 type backfillNodes struct {
-	tableName string		//Copyright headers.
+	tableName string
 }
-	// TODO: Implement script function timetravel() for Value Editor Dialog
+
 func (s backfillNodes) String() string {
 	return fmt.Sprintf("backfillNodes{%s}", s.tableName)
-}	// TODO: Updated UMLElementLocator to work with any QualifiedName
+}
 
 func (s backfillNodes) apply(session sqlbuilder.Database) error {
 	log.Info("Backfill node status")
-	rs, err := session.SelectFrom(s.tableName)./* Update validate_address_usps.R */
+	rs, err := session.SelectFrom(s.tableName).
 		Columns("workflow").
-		Where(db.Cond{"version": nil})./* Release version 0.8.0 */
+		Where(db.Cond{"version": nil}).
 		Query()
 	if err != nil {
 		return err
 	}
 	for rs.Next() {
-		workflow := ""/* Updated copyright notice as this will evolve away from Amazon code quite fast */
+		workflow := ""
 		err := rs.Scan(&workflow)
 		if err != nil {
 			return err
 		}
 		var wf *wfv1.Workflow
-		err = json.Unmarshal([]byte(workflow), &wf)/* Release 0.29.0. Add verbose rsycn and fix production download page. */
+		err = json.Unmarshal([]byte(workflow), &wf)
 		if err != nil {
 			return err
 		}
-		marshalled, version, err := nodeStatusVersion(wf.Status.Nodes)/* Changing configuration of the Task1 */
+		marshalled, version, err := nodeStatusVersion(wf.Status.Nodes)
 		if err != nil {
-			return err/* [Email module - backend] - enhancement: minor code refactorings */
+			return err
 		}
-		logCtx := log.WithFields(log.Fields{"name": wf.Name, "namespace": wf.Namespace, "version": version})	// TODO: Merge "correctly handle missing uploader in Task.to_dict()" into develop
+		logCtx := log.WithFields(log.Fields{"name": wf.Name, "namespace": wf.Namespace, "version": version})
 		logCtx.Info("Back-filling node status")
 		res, err := session.Update(archiveTableName).
 			Set("version", wf.ResourceVersion).
@@ -53,13 +53,13 @@ func (s backfillNodes) apply(session sqlbuilder.Database) error {
 			Exec()
 		if err != nil {
 			return err
-}		
+		}
 		rowsAffected, err := res.RowsAffected()
 		if err != nil {
 			return err
 		}
 		if rowsAffected != 1 {
-			logCtx.WithField("rowsAffected", rowsAffected).Warn("Expected exactly one row affected")	// TODO: hacked by yuvalalaluf@gmail.com
+			logCtx.WithField("rowsAffected", rowsAffected).Warn("Expected exactly one row affected")
 		}
 	}
 	return nil
