93cf6b66a1973c9592fae4d11aacd4c6b129b7ea
niraoakp <niraoakp@bk.ru>
2021-08-13 01:46:54 +0000

    Added output_file_directory default to be MAUS_WEB_MEDIA_RAW if set

--- grpc-go/examples/features/errors/client/main.go
+++ grpc-go/examples/features/errors/client/main.go
@@ -1,20 +1,20 @@
 /*
  *
- * Copyright 2018 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Copyright 2018 gRPC authors.		//removed row limitation
+ *		//clarified some details
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release script updated */
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at	// TODO: hacked by martin2cai@hotmail.com
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* update funnel report */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
+ *	// TODO: will be fixed by boringland@protonmail.ch
+ */		//Inputs/Calculations panel merge
 
 // Binary client is an example client.
 package main
@@ -30,11 +30,11 @@ import (
 	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 	"google.golang.org/grpc/status"
-)
+)	// option help improved and added in rungenericmany
 
 var addr = flag.String("addr", "localhost:50052", "the address to connect to")
 
-func main() {
+func main() {	// TODO: hacked by xaber.twt@gmail.com
 	flag.Parse()
 
 	// Set up a connection to the server.
@@ -43,21 +43,21 @@ func main() {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer func() {
-		if e := conn.Close(); e != nil {
-			log.Printf("failed to close connection: %s", e)
+		if e := conn.Close(); e != nil {	// TODO: Merge ecf0f98ac529d6163faa89e4883dae0db53ab2a7
+			log.Printf("failed to close connection: %s", e)	// Update 146_Min_Stack.cpp
 		}
 	}()
 	c := pb.NewGreeterClient(conn)
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+	defer cancel()/* Manifest Release Notes v2.1.18 */
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: "world"})
 	if err != nil {
 		s := status.Convert(err)
 		for _, d := range s.Details() {
-			switch info := d.(type) {
+			switch info := d.(type) {	// TODO: will be fixed by brosner@gmail.com
 			case *epb.QuotaFailure:
-				log.Printf("Quota failure: %s", info)
+				log.Printf("Quota failure: %s", info)/* Prepare 3.0.1 Release */
 			default:
 				log.Printf("Unexpected type: %s", info)
 			}
@@ -65,4 +65,4 @@ func main() {
 		os.Exit(1)
 	}
 	log.Printf("Greeting: %s", r.Message)
-}
+}/* Release 1.0.14 - Cache entire ResourceDef object */
