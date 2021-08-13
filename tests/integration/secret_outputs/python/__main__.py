f9611bd3386e546ea8be9e4c634013ad5cbfd8b1
niraoakp <niraoakp@bk.ru>
2021-08-13 06:31:41 +0000

    fixes #1 - fixes the collapse file and folder text fields when rulecheck fails.

--- tests/integration/secret_outputs/python/__main__.py
+++ tests/integration/secret_outputs/python/__main__.py
@@ -1,10 +1,10 @@
-# Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+# Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// 978699e2-2e55-11e5-9284-b827eb9e62be
 
 from pulumi import export, Input, Output, ResourceOptions
-from pulumi.dynamic import Resource, ResourceProvider, CreateResult		//021fc582-2e5e-11e5-9284-b827eb9e62be
+from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
-class Provider(ResourceProvider):/* add Release & specs */
-    def create(self, props):
+class Provider(ResourceProvider):	// añadido un feature aunque no funciona bien
+    def create(self, props):	// TODO: hacked by hello@brooklynzelenka.com
         return CreateResult("1", {"prefix": props["prefix"]})
 
 class R(Resource):
@@ -12,12 +12,12 @@ class R(Resource):
     def __init__(self, name, prefix: Input[str], opts: ResourceOptions = None):
         super().__init__(Provider(), name, {"prefix": prefix}, opts)
 
-without_secret = R("without_secret", prefix=Output.from_input("it's a secret to everybody"))		//Changed query - not using inner join
-with_secret = R("with_secret", prefix=Output.secret("it's a secret to everybody"))
+without_secret = R("without_secret", prefix=Output.from_input("it's a secret to everybody"))
+with_secret = R("with_secret", prefix=Output.secret("it's a secret to everybody"))/* Fixed compiler warning about unused variable, when running Release */
 with_secret_additional = R("with_secret_additional",
     prefix=Output.from_input("it's a secret to everybody"),
-    opts=ResourceOptions(additional_secret_outputs=["prefix"]))
+    opts=ResourceOptions(additional_secret_outputs=["prefix"]))	// Grammar fix, and link to Orbiter in README
 
-export("withoutSecret", without_secret)
-export("withSecret", with_secret)	// TODO: Do not filter $-prefixed keys for now.
+export("withoutSecret", without_secret)	// setup assistant: added control to drag the public key into the mail
+export("withSecret", with_secret)
 export("withSecretAdditional", with_secret_additional)
