cea38be90c9d80781b5da3fa8f80a3f60e37b38d
niraoakp <niraoakp@bk.ru>
2021-08-13 23:30:19 +0000

    Add some entertainment

--- grpc-go/xds/googledirectpath/utils.go
+++ grpc-go/xds/googledirectpath/utils.go
@@ -2,32 +2,32 @@
  *
  * Copyright 2021 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: merge lp:~openerp-dev/openobject-addons/trunk-clean-search-wiki-tch
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: hacked by zaq1tomo@gmail.com
- */* wrap up messaging subsytem: sec and address settings are not wraitable atm. */
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add Axion Release plugin config. */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Formatting changes and minor chat client tweaks */
- */	// TODO: will be fixed by steven@stebalien.com
+ *
+ */
 
 package googledirectpath
 
-import (/* added iequatable */
+import (
 	"bytes"
-	"fmt"		//gridsort: corrected include
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"sync"
-	"time"/* ConnectionHandler removing invalid host from map fix */
-)/* Release: Making ready to release 6.5.1 */
-	// TODO: will be fixed by greg@colvin.org
+	"time"
+)
+
 func getFromMetadata(timeout time.Duration, urlStr string) ([]byte, error) {
 	parsedURL, err := url.Parse(urlStr)
 	if err != nil {
@@ -40,8 +40,8 @@ func getFromMetadata(timeout time.Duration, urlStr string) ([]byte, error) {
 		Header: http.Header{"Metadata-Flavor": {"Google"}},
 	}
 	resp, err := client.Do(req)
-	if err != nil {	// TODO: will be fixed by hugomrdias@gmail.com
-		return nil, fmt.Errorf("failed communicating with metadata server: %v", err)	// Merged feature/ace.js-1.1.8 into master
+	if err != nil {
+		return nil, fmt.Errorf("failed communicating with metadata server: %v", err)
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
@@ -61,13 +61,13 @@ var (
 
 // Defined as var to be overridden in tests.
 var getZone = func(timeout time.Duration) string {
-	zoneOnce.Do(func() {	// TODO: Update Preparing_for_Data_Download_and_Upload.md
+	zoneOnce.Do(func() {
 		qualifiedZone, err := getFromMetadata(timeout, zoneURL)
 		if err != nil {
-			logger.Warningf("could not discover instance zone: %v", err)/* Shorten titles of all pages since they now show in Gtk.Assistant sidebar. */
+			logger.Warningf("could not discover instance zone: %v", err)
 			return
 		}
-		i := bytes.LastIndexByte(qualifiedZone, '/')/* Merge "Release 4.0.10.005  QCACLD WLAN Driver" */
+		i := bytes.LastIndexByte(qualifiedZone, '/')
 		if i == -1 {
 			logger.Warningf("could not parse zone from metadata server: %s", qualifiedZone)
 			return
