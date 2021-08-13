043285a8d33fcafef06ca8bcd11ed9e5cd8833c9
niraoakp <niraoakp@bk.ru>
2021-08-13 12:12:11 +0000

    Update files.md: add 413 error doc

--- grpc-go/internal/channelz/util_nonlinux.go
+++ grpc-go/internal/channelz/util_nonlinux.go
@@ -4,19 +4,19 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* fixed tables 3 (password hash) */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* refactoring of EventPool */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// Merge branch 'newbranch' of https://github.com/levy004/test.git into newbranch
  *
- * Unless required by applicable law or agreed to in writing, software/* update org name to juliadatabases */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Default Code for Denver .editorconfig */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Merge branch 'master' into modify-test-script */
+ */
 
 package channelz
 
