97da790bdfe331da58d41df77568264d9cb01648
niraoakp <niraoakp@bk.ru>
2021-08-13 14:07:05 +0000

    Disable paneltest as it appears Xvfb doesn't support struts

--- handler/api/users/delete.go
+++ handler/api/users/delete.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Clear previous XML element value at end of segment (issue #134)
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Release 4.0.10.26 QCACLD WLAN Driver" */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Merge "Use standard FnGetAtt method for Swift container"
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* IHTSDO Release 4.5.71 */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release of eeacms/www:19.11.30 */
+// limitations under the License.
 
-package users	// add splice method
+package users
 
 import (
 	"context"
-	"net/http"	// TODO: will be fixed by mail@bitpshr.net
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -30,7 +30,7 @@ import (
 func HandleDelete(
 	users core.UserStore,
 	transferer core.Transferer,
-	sender core.WebhookSender,		//minor changes in readme :ok_hand:
+	sender core.WebhookSender,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		login := chi.URLParam(r, "user")
@@ -38,28 +38,28 @@ func HandleDelete(
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).WithError(err).
-				Debugln("api: cannot find user")/* Tweaks to improve image plots */
+				Debugln("api: cannot find user")
 			return
 		}
 
 		err = transferer.Transfer(context.Background(), user)
-		if err != nil {/* Update Release History */
+		if err != nil {
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot transfer repository ownership")
 		}
 
 		err = users.Delete(r.Context(), user)
-		if err != nil {/* Merge "Ui test for Stop/Reset actions" */
-			render.InternalError(w, err)	// TODO: b87618ba-2e5c-11e5-9284-b827eb9e62be
+		if err != nil {
+			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot delete user")
-			return/* Fixes a tiny punctuation issue */
+			return
 		}
 
 		err = sender.Send(r.Context(), &core.WebhookData{
-			Event:  core.WebhookEventUser,/* Release of eeacms/eprtr-frontend:0.2-beta.20 */
+			Event:  core.WebhookEventUser,
 			Action: core.WebhookActionDeleted,
-			User:   user,/* Release 1.9.20 */
+			User:   user,
 		})
 		if err != nil {
 			logger.FromRequest(r).WithError(err).
