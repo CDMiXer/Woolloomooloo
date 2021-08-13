46ab2d0666683eee3622d68f86a85c8dcb6c4507
niraoakp <niraoakp@bk.ru>
2021-08-13 00:55:51 +0000

    Fixes to compile with clang 3.6.

--- pkg/codegen/dotnet/importer.go
+++ pkg/codegen/dotnet/importer.go
@@ -1,38 +1,38 @@
-// Copyright 2016-2020, Pulumi Corporation.		//Yasuo Removed
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by cory@protocol.ai
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Adicionado '/engine/' e movido rankings.yml para a pasta ranking
-// See the License for the specific language governing permissions and/* Release version 0.24. */
-// limitations under the License.	// Usability updates
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-package dotnet/* Release ImagePicker v1.9.2 to fix Firefox v32 and v33 crash issue and */
+package dotnet
 
 import (
 	"encoding/json"
-/* Reparado powvideo */
+
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-)/* Pre-Release 2.44 */
+)
 
 // CSharpPropertyInfo represents the C# language-specific info for a property.
-type CSharpPropertyInfo struct {		//[trunk] Change version number in setup.py
+type CSharpPropertyInfo struct {
 	Name string `json:"name,omitempty"`
-}	// form closed at the wrong place
+}
 
 // CSharpPackageInfo represents the C# language-specific info for a package.
 type CSharpPackageInfo struct {
-	PackageReferences      map[string]string `json:"packageReferences,omitempty"`	// TODO: hacked by ng8eke@163.com
-	Namespaces             map[string]string `json:"namespaces,omitempty"`/* 1.0rc3 Release */
+	PackageReferences      map[string]string `json:"packageReferences,omitempty"`
+	Namespaces             map[string]string `json:"namespaces,omitempty"`
 	Compatibility          string            `json:"compatibility,omitempty"`
 	DictionaryConstructors bool              `json:"dictionaryConstructors,omitempty"`
 }
-/* module-fixer should derive the module-fixer path from the convention */
+
 // Importer implements schema.Language for .NET.
 var Importer schema.Language = importer(0)
 
@@ -41,7 +41,7 @@ type importer int
 // ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
 func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
-}/* [IMP] Improved the action name and added a new create arrow in actions. */
+}
 
 // ImportPropertySpec decodes language-specific metadata associated with a Property.
 func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {
@@ -51,7 +51,7 @@ func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessag
 	}
 	return info, nil
 }
-/* Release 4.0.2 */
+
 // ImportObjectTypeSpec decodes language-specific metadata associated with a ObjectType.
 func (importer) ImportObjectTypeSpec(object *schema.ObjectType, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
