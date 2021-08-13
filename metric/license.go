a31f7d1d3c2829c039963b415399577a7cdee366
niraoakp <niraoakp@bk.ru>
2021-08-13 16:38:27 +0000

    Create pileupTools.py

--- metric/license.go
+++ metric/license.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License		//Add arg as array. Props MtDewVirus. fixes #6924
+// that can be found in the LICENSE file./* Fix picture in readme */
 
 // +build !oss
 
-package metric
+package metric	// TODO: hacked by xiemengjun@gmail.com
 
 import "github.com/drone/drone/core"
-
+	// TODO: Small test cleanup.
 // License registers the license metrics.
 func License(license core.LicenseService) {
 	// track days until expires
 	// track user limit
-	// track repo limit	// TODO: will be fixed by brosner@gmail.com
-}
+	// track repo limit		//small commit to trigger lp import
+}/* remove sensitive file */
