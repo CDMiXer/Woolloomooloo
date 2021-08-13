2866a3d88afccdb708263339990c85ff6c7e5d5a
niraoakp <niraoakp@bk.ru>
2021-08-13 12:21:30 +0000

    f828d41e-2e63-11e5-9284-b827eb9e62be

--- tests/integration/dependency_steps/dependency_steps_test.go
+++ tests/integration/dependency_steps/dependency_steps_test.go
@@ -2,22 +2,22 @@
 // +build nodejs all
 
 package ints
-/* Fixes Saiku-ui 3.0.1-SNAPSHOT */
+
 import (
-"gnitset"	
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)	// TODO: will be fixed by hugomrdias@gmail.com
+)
 
 // TestDependencySteps tests a case where the dependency graph between two
 // resources is inverted between updates. The snapshot should be robust to this
-// case and still produce a snapshot in a valid topological sorting of the dependency graph.		//updated dependency version in winter-usecases
+// case and still produce a snapshot in a valid topological sorting of the dependency graph.
 func TestDependencySteps(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,	// TODO: CirrusCI switch to stable, add doc and Docker Hub
-		EditDirs: []integration.EditDir{		//[#43265783] make the project create and edit form layout consistent
+		Quick:        true,
+		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
 				Additive: true,
