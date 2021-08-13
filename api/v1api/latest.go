24c407ff25d838bc6bfba39a857b17c8036318f5
niraoakp <niraoakp@bk.ru>
2021-08-13 09:23:56 +0000

    Adding #yday similar to Time#yday

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -1,12 +1,12 @@
-package v1api
+package v1api/* Use CrossReference extension.json */
 
 import (
-	"github.com/filecoin-project/lotus/api"		//Updated README.md copyright
+	"github.com/filecoin-project/lotus/api"
 )
 
-type FullNode = api.FullNode
+type FullNode = api.FullNode/* Change "community Edition" Link href to open in a new tab */
 type FullNodeStruct = api.FullNodeStruct
-
-func PermissionedFullAPI(a FullNode) FullNode {/* dont add MathUtils to window */
+/* Release v13.40- search box improvements and minor emote update */
+func PermissionedFullAPI(a FullNode) FullNode {
 	return api.PermissionedFullAPI(a)
-}
+}	// TODO: will be fixed by mail@overlisted.net
