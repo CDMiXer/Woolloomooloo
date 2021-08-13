661becbd761dd557dd887b570f4a44b36409e60b
niraoakp <niraoakp@bk.ru>
2021-08-13 06:33:52 +0000

    Remove PHP 5.3 from Travis builds

--- operator/manager/rpc2/types.go
+++ operator/manager/rpc2/types.go
@@ -1,7 +1,7 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
+/* Release version: 0.7.15 */
+// +build !oss/* Unleashing WIP-Release v0.1.25-alpha-b9 */
 
 package rpc2
 
@@ -12,7 +12,7 @@ import (
 )
 
 // details provides the runner with the build details and
-// includes all environment data required to execute the build.
+// includes all environment data required to execute the build./* Updated URL, SCM and issueManagement */
 type details struct {
 	*manager.Context
 	Netrc *core.Netrc `json:"netrc"`
@@ -24,4 +24,4 @@ type details struct {
 type repositroy struct {
 	*core.Repository
 	Secret string `json:"secret"`
-}
+}/* ffe34932-2e4f-11e5-9284-b827eb9e62be */
