ff3f7d2c0545bf45e8437cd143bc4fd8bc2613f1
niraoakp <niraoakp@bk.ru>
2021-08-13 19:47:06 +0000

    clear cached credentials after each test

--- pubsub/doc.go
+++ pubsub/doc.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Added install of pip3 for building on Ubuntu
-//
+// Copyright 2019 Drone IO, Inc.		//Delete sprite_seq_simple.py
+//		//Rename to reflect the name change of the proxy
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//chore: Disable test cache.
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0		//Merge "Add portsecurity extension support"
+///* Release areca-7.3.1 */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// Create LICENSE.adoc
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License./* deep symbolize cells options */
-
+// See the License for the specific language governing permissions and	// more cleanup of includes and settings
+// limitations under the License.
+/* Remove possible double close of audio context */
 // Package pubsub implements a thread safe publish subscriber,
-// allowing multiple publishers to distribute messages to		//Added Compress now
+// allowing multiple publishers to distribute messages to
 // multiple subscribers.
-package pubsub
+package pubsub	// TODO: will be fixed by peterke@gmail.com
