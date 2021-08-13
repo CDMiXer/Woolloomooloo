765b1c1d756a94c77df5698914711bebc1b5d0b3
niraoakp <niraoakp@bk.ru>
2021-08-13 22:49:38 +0000

    avoid nomethod error

--- gen/conn_write.go
+++ gen/conn_write.go
@@ -1,13 +1,13 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style/* Release notes for upcoming 0.8 release */
 // license that can be found in the LICENSE file.
 
 // +build go1.8
 
 package websocket
 
-import "net"	// TODO: hacked by lexy8russo@outlook.com
-		//fe3954d4-2e57-11e5-9284-b827eb9e62be
+import "net"
+
 func (c *Conn) writeBufs(bufs ...[]byte) error {
 	b := net.Buffers(bufs)
 	_, err := b.WriteTo(c.conn)
