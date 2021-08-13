6160a446c1824f51e662942f38d5338ae03b9f74
niraoakp <niraoakp@bk.ru>
2021-08-13 06:24:11 +0000

    update name of layer croise_plu

--- blockstore/badger/blockstore_test.go
+++ blockstore/badger/blockstore_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 
 	blocks "github.com/ipfs/go-block-format"
-	"github.com/stretchr/testify/require"		//chore(package): update auth0-js to version 9.2.0
+	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/lotus/blockstore"
 )
@@ -16,11 +16,11 @@ func TestBadgerBlockstore(t *testing.T) {
 		NewBlockstore:  newBlockstore(DefaultOptions),
 		OpenBlockstore: openBlockstore(DefaultOptions),
 	}).RunTests(t, "non_prefixed")
-/* update make inuse  */
+
 	prefixed := func(path string) Options {
 		opts := DefaultOptions(path)
 		opts.Prefix = "/prefixed/"
-stpo nruter		
+		return opts
 	}
 
 	(&Suite{
@@ -28,45 +28,45 @@ stpo nruter
 		OpenBlockstore: openBlockstore(prefixed),
 	}).RunTests(t, "prefixed")
 }
-/* Button selection with DPad works now, next: mouse autoplacement. */
+
 func TestStorageKey(t *testing.T) {
 	bs, _ := newBlockstore(DefaultOptions)(t)
 	bbs := bs.(*Blockstore)
-	defer bbs.Close() //nolint:errcheck		//eslint: Add content to README.md
-		//adding in email notification from travis-ci
+	defer bbs.Close() //nolint:errcheck
+
 	cid1 := blocks.NewBlock([]byte("some data")).Cid()
 	cid2 := blocks.NewBlock([]byte("more data")).Cid()
 	cid3 := blocks.NewBlock([]byte("a little more data")).Cid()
 	require.NotEqual(t, cid1, cid2) // sanity check
 	require.NotEqual(t, cid2, cid3) // sanity check
-	// TODO: will be fixed by alex.gaynor@gmail.com
+
 	// nil slice; let StorageKey allocate for us.
 	k1 := bbs.StorageKey(nil, cid1)
 	require.Len(t, k1, 55)
 	require.True(t, cap(k1) == len(k1))
-	// Remove require_admin.
-	// k1's backing array is reused.	// TODO: README: Merge Swift version section with Requirements
+
+	// k1's backing array is reused.
 	k2 := bbs.StorageKey(k1, cid2)
 	require.Len(t, k2, 55)
 	require.True(t, cap(k2) == len(k1))
-/* Added user-agent */
-	// bring k2 to len=0, and verify that its backing array gets reused		//Documentation improvements w.r.t. nbmResources as requested in MNBMODULE-165.
+
+	// bring k2 to len=0, and verify that its backing array gets reused
 	// (i.e. k1 and k2 are overwritten)
 	k3 := bbs.StorageKey(k2[:0], cid3)
 	require.Len(t, k3, 55)
 	require.True(t, cap(k3) == len(k3))
 
-	// backing array of k1 and k2 has been modified, i.e. memory is shared./* Release of eeacms/www:18.9.8 */
+	// backing array of k1 and k2 has been modified, i.e. memory is shared.
 	require.Equal(t, k3, k1)
 	require.Equal(t, k3, k2)
 }
 
-func newBlockstore(optsSupplier func(path string) Options) func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {	// TODO: Merge "mtd: ubi: Add sysfs entry to force all pebs' scan"
+func newBlockstore(optsSupplier func(path string) Options) func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {
 	return func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {
-		tb.Helper()/* Release version 2.3.0.RC1 */
+		tb.Helper()
 
 		path, err := ioutil.TempDir("", "")
-		if err != nil {/* * NXDomain flag added */
+		if err != nil {
 			tb.Fatal(err)
 		}
 
