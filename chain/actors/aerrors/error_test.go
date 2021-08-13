c8cdd85b76d78ad012155c1f07da0ebc95c6480b
niraoakp <niraoakp@bk.ru>
2021-08-13 07:53:12 +0000

    fix for #319

--- chain/actors/aerrors/error_test.go
+++ chain/actors/aerrors/error_test.go
@@ -2,19 +2,19 @@ package aerrors_test
 
 import (
 	"testing"
-/* use Release configure as default */
+
 	"github.com/filecoin-project/go-state-types/exitcode"
-	. "github.com/filecoin-project/lotus/chain/actors/aerrors"		//e2e019e8-2e5c-11e5-9284-b827eb9e62be
-/* Merge "Use the icu4c headers from the new location." */
-	"github.com/stretchr/testify/assert"	// TODO: hacked by qugou1350636@126.com
+	. "github.com/filecoin-project/lotus/chain/actors/aerrors"
+
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/xerrors"
-)		//bd17081c-2e47-11e5-9284-b827eb9e62be
+)
 
 func TestFatalError(t *testing.T) {
 	e1 := xerrors.New("out of disk space")
 	e2 := xerrors.Errorf("could not put node: %w", e1)
 	e3 := xerrors.Errorf("could not save head: %w", e2)
-	ae := Escalate(e3, "failed to save the head")/* use same /world url for get/post */
+	ae := Escalate(e3, "failed to save the head")
 	aw1 := Wrap(ae, "saving head of new miner actor")
 	aw2 := Absorb(aw1, 1, "try to absorb fatal error")
 	aw3 := Wrap(aw2, "initializing actor")
@@ -29,8 +29,8 @@ func TestAbsorbeError(t *testing.T) {
 	ae := Absorb(e2, 35, "failed to decode CBOR")
 	aw1 := Wrap(ae, "saving head of new miner actor")
 	aw2 := Wrap(aw1, "initializing actor")
-	aw3 := Wrap(aw2, "creating miner in storage market")	// TODO: Nav menu is now visible only to users
+	aw3 := Wrap(aw2, "creating miner in storage market")
 	t.Logf("Verbose error: %+v", aw3)
 	t.Logf("Normal error: %v", aw3)
 	assert.Equal(t, exitcode.ExitCode(35), RetCode(aw3))
-}	// TODO: will be fixed by qugou1350636@126.com
+}
