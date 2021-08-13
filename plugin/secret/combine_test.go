60d64f0069c32085dbce14eef263e7348e5fd870
niraoakp <niraoakp@bk.ru>
2021-08-13 07:24:16 +0000

    Fix doxygen formatting

--- plugin/secret/combine_test.go
+++ plugin/secret/combine_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* 60c74e8a-5216-11e5-a43c-6c40088e03e4 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* removed libfontconfig */
 package secret
