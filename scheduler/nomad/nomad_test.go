95a38ea8b50754cb716014062249418ac7a5ccfb
niraoakp <niraoakp@bk.ru>
2021-08-14 16:21:10 +0000

    Php: Improved StringUtils generate random password method and tests

--- scheduler/nomad/nomad_test.go
+++ scheduler/nomad/nomad_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Update Advanced SPC Mod 0.14.x Release version */
 // +build !oss
 
 package nomad
