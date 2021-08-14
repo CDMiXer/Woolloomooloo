5c0a0a50e91156f99c43064f0638409a13b9600f
niraoakp <niraoakp@bk.ru>
2021-08-14 22:29:17 +0000

    added traceComponent support (to be expanded)

--- grpc-go/xds/internal/balancer/clusterimpl/logging.go
+++ grpc-go/xds/internal/balancer/clusterimpl/logging.go
@@ -2,24 +2,24 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//fix mise in page
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: will be fixed by lexy8russo@outlook.com
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: changed the project name of Umple update site.
- * Unless required by applicable law or agreed to in writing, software
+ *	// TODO: Resolving merge issues, fixing preview pane job summary size and position
+ * Unless required by applicable law or agreed to in writing, software/* Fix running elevated tests. Release 0.6.2. */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- *//* Release 0.4.0.2 */
-		//Removed extra } in pianists query
+ */* add support for 741nd v4 (readme) */
+ */
+
 package clusterimpl
 
 import (
-	"fmt"		//9e2a968e-2e6b-11e5-9284-b827eb9e62be
+	"fmt"
 
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
@@ -28,7 +28,7 @@ import (
 const prefix = "[xds-cluster-impl-lb %p] "
 
 var logger = grpclog.Component("xds")
-/* Release 1.0.3. */
-func prefixLogger(p *clusterImplBalancer) *internalgrpclog.PrefixLogger {/* fix buffer for scroll to top amount #71 */
+
+func prefixLogger(p *clusterImplBalancer) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
 }
