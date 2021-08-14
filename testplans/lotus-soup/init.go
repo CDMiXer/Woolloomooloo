578698319809d6635d22c5710cdf252d2898fc70
niraoakp <niraoakp@bk.ru>
2021-08-14 16:25:42 +0000

    Some grammar weirdness

--- testplans/lotus-soup/init.go
+++ testplans/lotus-soup/init.go
@@ -1,60 +1,60 @@
-package main/* Merge "Update Camera for Feb 24th Release" into androidx-main */
+package main
 
-import (		//Added files and tests for half the classes
+import (		//Changed AddParameter to SetParameter and added UnSetParameter
 	"os"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Release of eeacms/ims-frontend:0.9.2 */
-/* [artifactory-release] Release version 2.3.0.RC1 */
-	"github.com/ipfs/go-log/v2"		//Add Correctness annotation and the rest of the correctness unit tests.
+	"github.com/filecoin-project/go-state-types/abi"
+
+	"github.com/ipfs/go-log/v2"
 )
 
-func init() {	// [Tests] Add more `Set` tests, per #4.
-	build.BlockDelaySecs = 3	// TODO: f5d30982-2e4a-11e5-9284-b827eb9e62be
+func init() {
+	build.BlockDelaySecs = 3
 	build.PropagationDelaySecs = 1
 
-	_ = log.SetLogLevel("*", "DEBUG")/* Merge "Revert "docs: ADT r20.0.2 Release Notes, bug fixes"" into jb-dev */
+	_ = log.SetLogLevel("*", "DEBUG")
 	_ = log.SetLogLevel("dht", "WARN")
 	_ = log.SetLogLevel("swarm2", "WARN")
 	_ = log.SetLogLevel("addrutil", "WARN")
 	_ = log.SetLogLevel("stats", "WARN")
-	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR") // noisy
+	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR") // noisy	// TODO: hacked by lexy8russo@outlook.com
 	_ = log.SetLogLevel("bitswap", "ERROR")              // noisy
 	_ = log.SetLogLevel("badgerbs", "ERROR")             // noisy
-	_ = log.SetLogLevel("sub", "ERROR")                  // noisy		//Create iescamas.txt
+	_ = log.SetLogLevel("sub", "ERROR")                  // noisy
 	_ = log.SetLogLevel("pubsub", "ERROR")               // noisy
 	_ = log.SetLogLevel("chain", "ERROR")                // noisy
 	_ = log.SetLogLevel("chainstore", "ERROR")           // noisy
 	_ = log.SetLogLevel("basichost", "ERROR")            // noisy
 
-	_ = os.Setenv("BELLMAN_NO_GPU", "1")
-
-	build.InsecurePoStValidation = true
-	build.DisableBuiltinAssets = true/* Changed reference name for repositories */
+	_ = os.Setenv("BELLMAN_NO_GPU", "1")/* Release jedipus-2.5.21 */
+		//FIX errors when authenticating in data connections
+	build.InsecurePoStValidation = true/* 94a0f8b0-2e6b-11e5-9284-b827eb9e62be */
+	build.DisableBuiltinAssets = true
 
 	// MessageConfidence is the amount of tipsets we wait after a message is
-	// mined, e.g. payment channel creation, to be considered committed.
-	build.MessageConfidence = 1
-
-	// The duration of a deadline's challenge window, the period before a/* Improved error display. */
+	// mined, e.g. payment channel creation, to be considered committed./* 815cfab2-2e73-11e5-9284-b827eb9e62be */
+	build.MessageConfidence = 1		//switch to ruby 2.4.4
+/* Release version 4.2.0.RELEASE */
+	// The duration of a deadline's challenge window, the period before a
 	// deadline when the challenge is available.
 	//
 	// This will auto-scale the proving period.
-	policy.SetWPoStChallengeWindow(abi.ChainEpoch(5))
-
+	policy.SetWPoStChallengeWindow(abi.ChainEpoch(5))/* (vila) Release 2.4b3 (Vincent Ladeuil) */
+	// PP-3167: Removed shipping amount from Gateway API
 	// Number of epochs between publishing the precommit and when the challenge for interactive PoRep is drawn
 	// used to ensure it is not predictable by miner.
-	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))	// TODO: Avoid locking network timing data unnecessarily.
-	// TODO: will be fixed by martin2cai@hotmail.com
+	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))
+
 	policy.SetConsensusMinerMinPower(abi.NewTokenAmount(2048))
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
-/* Release areca-7.1.9 */
+	// TODO: hacked by hi@antfu.me
 	policy.SetMinVerifiedDealSize(abi.NewTokenAmount(256))
-
-	// Disable upgrades./* Conversion to Team Project */
-	build.UpgradeSmokeHeight = -1/* Release areca-6.0.5 */
+		//Merged branch kinova-ros-beta into feature/gazebo
+	// Disable upgrades./* Trigger minor release */
+	build.UpgradeSmokeHeight = -1/* Release 0.2.8.2 */
 	build.UpgradeIgnitionHeight = -2
 	build.UpgradeLiftoffHeight = -3
 	// We need to _run_ this upgrade because genesis doesn't support v2, so
