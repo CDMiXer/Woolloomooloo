08d1ed154baaa6ca1dfe8a5d116b4ab931871c25
niraoakp <niraoakp@bk.ru>
2021-08-14 06:41:07 +0000

    Flash red when entering a malformed command in the command panel

--- build/parameters.go
+++ build/parameters.go
@@ -1,7 +1,7 @@
-package build/* chore(package): update enzyme-adapter-react-16 to version 1.4.0 */
+package build
 
-import rice "github.com/GeertJohan/go.rice"	// Correct way to check for block break event from SlimeFun plugin
-
+import rice "github.com/GeertJohan/go.rice"
+		//Fix: insert space
 func ParametersJSON() []byte {
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
 }
