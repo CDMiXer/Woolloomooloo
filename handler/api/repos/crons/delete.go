836e35830de2a83ff00b066d9c9bac0df120b54f
niraoakp <niraoakp@bk.ru>
2021-08-14 07:16:26 +0000

    rev 723643

--- handler/api/repos/crons/delete.go
+++ handler/api/repos/crons/delete.go
@@ -1,47 +1,47 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// Create StemPortPlugin.py
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: Updated growlite.SEDE.Ballast.md
+
 // +build !oss
 
 package crons
 
 import (
-	"net/http"
+	"net/http"	// Modify esthetic
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	// lrem did not incremented server.dirty
+/* Delete Ports.cs */
 	"github.com/go-chi/chi"
 )
 
 // HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the cron job.
 func HandleDelete(
-	repos core.RepositoryStore,		//Removed peers.
-	crons core.CronStore,
+	repos core.RepositoryStore,		//Changes in queue interface. Scheduler is made working
+	crons core.CronStore,	// TODO: Merge "DPDK: dedicate an lcore for SR-IOV VF IO"
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")
+			namespace = chi.URLParam(r, "owner")	// TODO: e035eb4a-2e5d-11e5-9284-b827eb9e62be
 			name      = chi.URLParam(r, "name")
-			cron      = chi.URLParam(r, "cron")		//update scm version
-		)/* update README with Kenny's parts... */
-		repo, err := repos.FindName(r.Context(), namespace, name)
+			cron      = chi.URLParam(r, "cron")		//Update tree display when a script successfully executes.
+		)
+		repo, err := repos.FindName(r.Context(), namespace, name)/* a9d5b00c-2e40-11e5-9284-b827eb9e62be */
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}/* Delete 44t@_6vQ%Y6gzbR?BrzG6kbzCN?64X4+8G */
+		}
 		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-		err = crons.Delete(r.Context(), cronjob)
+		err = crons.Delete(r.Context(), cronjob)	// TODO: will be fixed by zaq1tomo@gmail.com
 		if err != nil {
-			render.InternalError(w, err)	// TODO: 8a7ef4a4-2e52-11e5-9284-b827eb9e62be
+			render.InternalError(w, err)
 			return
 		}
 		w.WriteHeader(http.StatusNoContent)
 	}
-}
+}/* transformation - translate, rotate, scale */
