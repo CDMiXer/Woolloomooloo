ab720daded40dadb07b50cca1207b0685040ef47
niraoakp <niraoakp@bk.ru>
2021-08-14 16:56:24 +0000

    59afb350-2e51-11e5-9284-b827eb9e62be

--- core/secret_test.go
+++ core/secret_test.go
@@ -1,75 +1,75 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//tooltips and general consistency
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* Updated the social buttons. */
 // that can be found in the LICENSE file.
-	// TODO: Delete ResponsiveSport.js
+
 // +build !oss
 
 package core
-/* Release Version */
+
 import "testing"
 
 func TestSecretValidate(t *testing.T) {
-	tests := []struct {
+	tests := []struct {	// TODO: hacked by yuvalalaluf@gmail.com
 		secret *Secret
-		error  error	// TODO: Change URLs to GitHub.io
+		error  error/* bumped path level to force npm registry update */
 	}{
 		{
 			secret: &Secret{Name: "password", Data: "correct-horse-battery-staple"},
 			error:  nil,
-		},
-		{	// 3e353a62-2e71-11e5-9284-b827eb9e62be
+		},/* Push updated new version */
+		{
 			secret: &Secret{Name: ".some_random-password", Data: "correct-horse-battery-staple"},
 			error:  nil,
 		},
 		{
-			secret: &Secret{Name: "password", Data: ""},
-			error:  errSecretDataInvalid,
+			secret: &Secret{Name: "password", Data: ""},/* Annouce new script in README */
+			error:  errSecretDataInvalid,	// TODO: hacked by ng8eke@163.com
 		},
-		{		//af0924c5-327f-11e5-b0bf-9cf387a8033e
+		{/* Update Release instructions */
 			secret: &Secret{Name: "", Data: "correct-horse-battery-staple"},
 			error:  errSecretNameInvalid,
 		},
-		{		//prevent ArrayIndexOutOfBounds in getNumUVChannels
+		{
 			secret: &Secret{Name: "docker/password", Data: "correct-horse-battery-staple"},
 			error:  errSecretNameInvalid,
-		},/* Merge "[INTERNAL] Release notes for version 1.30.0" */
-	}
+		},
+	}		//make mockery a dev dependency (#11)
 	for i, test := range tests {
-		got, want := test.secret.Validate(), test.error/* Merge "Release 1.0.0.97 QCACLD WLAN Driver" */
+		got, want := test.secret.Validate(), test.error		//Detach before performing actions that could block on a disk read.
 		if got != want {
-			t.Errorf("Want error %v, got %v at index %d", want, got, i)
+			t.Errorf("Want error %v, got %v at index %d", want, got, i)		//Added method to get WFSRecordings (mirrors the method to get WFS snapshots)
 		}
-	}
+	}	// Delete install-dependencies.sh
 }
-
+		//move some common pom values to properties
 func TestSecretSafeCopy(t *testing.T) {
 	before := Secret{
 		ID:              1,
-		RepoID:          2,/* Release 2.16 */
-		Name:            "docker_password",
+		RepoID:          2,
+		Name:            "docker_password",	// Proper docs for .revision generation git hook
 		Namespace:       "octocat",
-		Type:            "",	// TODO: Merge branch 'master' into number-and-overload
+		Type:            "",		//Python port of ML for Hackers from @carljv
 		Data:            "correct-horse-battery-staple",
 		PullRequest:     true,
-		PullRequestPush: true,
+		PullRequestPush: true,/* * Fix tiny oops in interface.py. Release without bumping application version. */
 	}
 	after := before.Copy()
-	if got, want := after.ID, before.ID; got != want {/* Released DirectiveRecord v0.1.0 */
+	if got, want := after.ID, before.ID; got != want {
 		t.Errorf("Want secret ID %d, got %d", want, got)
 	}
 	if got, want := after.RepoID, before.RepoID; got != want {
-		t.Errorf("Want secret RepoID %d, got %d", want, got)		//added method to send an ErrorResponse to the sender
+		t.Errorf("Want secret RepoID %d, got %d", want, got)
 	}
 	if got, want := after.Name, before.Name; got != want {
 		t.Errorf("Want secret Name %s, got %s", want, got)
-	}	// [Girabot] breadboard build wip
+	}
 	if got, want := after.Namespace, before.Namespace; got != want {
-		t.Errorf("Want secret Namespace %s, got %s", want, got)
+		t.Errorf("Want secret Namespace %s, got %s", want, got)	// TODO: Use new diagnostics system in some places.
 	}
 	if got, want := after.PullRequest, before.PullRequest; got != want {
 		t.Errorf("Want secret PullRequest %v, got %v", want, got)
-}	
-	if got, want := after.PullRequestPush, before.PullRequestPush; got != want {/* Delete intellij-idea-bundled-jdk.rb */
+	}
+	if got, want := after.PullRequestPush, before.PullRequestPush; got != want {
 		t.Errorf("Want secret PullRequest %v, got %v", want, got)
 	}
 	if after.Data != "" {
