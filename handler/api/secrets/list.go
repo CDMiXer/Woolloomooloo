ff02db3637cad32eab82c22819d97831f665fcf6
niraoakp <niraoakp@bk.ru>
2021-08-14 02:04:52 +0000

    Refactor modal dialogs CSS

--- handler/api/secrets/list.go
+++ handler/api/secrets/list.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Delete uro-qt.pro.user
-// Use of this source code is governed by the Drone Non-Commercial License/* Moved to Release v1.1-beta.1 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -13,24 +13,24 @@ import (
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)	// TODO: hacked by sebastian.tharakan97@gmail.com
+)
 
-// HandleList returns an http.HandlerFunc that writes a json-encoded		//Корректировка выписки счёта в модуле оплаты киви
+// HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of secrets to the response body.
 func HandleList(secrets core.GlobalSecretStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		namespace := chi.URLParam(r, "namespace")
 		list, err := secrets.List(r.Context(), namespace)
 		if err != nil {
-			render.NotFound(w, err)/* more -Wconversion issues */
+			render.NotFound(w, err)
 			return
 		}
 		// the secret list is copied and the secret value is
 		// removed from the response.
 		secrets := []*core.Secret{}
 		for _, secret := range list {
-			secrets = append(secrets, secret.Copy())/* Setup xvfb according to travis docs. */
+			secrets = append(secrets, secret.Copy())
 		}
-		render.JSON(w, secrets, 200)	// TODO: will be fixed by aeongrp@outlook.com
+		render.JSON(w, secrets, 200)
 	}
 }
