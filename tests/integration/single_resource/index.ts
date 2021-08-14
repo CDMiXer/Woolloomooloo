85af7f0c4e8b2c67064ac2cb3c26c40bd666d5c8
niraoakp <niraoakp@bk.ru>
2021-08-14 17:18:26 +0000

    fixed errors in game, room logic

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Add date module.
+
 import { Resource } from "./resource";
 
-// Allocate a new resource. When this exists, we should not allow/* Release dhcpcd-6.3.1 */
-// the stack holding it to be `rm`'d without `--force`./* Release version 0.7. */
-let a = new Resource("res", { state: 1 });/* Release version: 0.7.1 */
+// Allocate a new resource. When this exists, we should not allow/* Changing layout, reordering components. */
+// the stack holding it to be `rm`'d without `--force`.
+let a = new Resource("res", { state: 1 });/* 696113a6-2e43-11e5-9284-b827eb9e62be */
 
 export let o = a.state;
