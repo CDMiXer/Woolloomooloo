32f4ea24f56beb2cc8c9a33c3a4ca35a890c622b
niraoakp <niraoakp@bk.ru>
2021-08-14 20:23:14 +0000

    Delete region.cpp

--- plugin/validator/remote_test.go
+++ plugin/validator/remote_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Added client main function and imported JDBC driver */
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* planejador ok */
 
-// +build !oss
+// +build !oss/* T2Rj5orLsBVR8gDXkA3BccWUsknjgIkY */
 
 package validator
