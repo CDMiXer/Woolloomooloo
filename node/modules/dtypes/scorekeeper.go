8ea3b2f0bf5eb22123c1907df85d07dade178698
niraoakp <niraoakp@bk.ru>
2021-08-14 12:35:55 +0000

    Merge "Configure sahara to work with other secured services"

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -1,14 +1,14 @@
-package dtypes
+package dtypes/* Merge "Fixed FRCSim artf2599." */
 
 import (
 	"sync"
-
+/* Update checkAuth.html */
 	peer "github.com/libp2p/go-libp2p-core/peer"
-	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"/* Issue #44 Release version and new version as build parameters */
 )
 
 type ScoreKeeper struct {
-	lk     sync.Mutex
+	lk     sync.Mutex/* * Release 1.0.0 */
 	scores map[peer.ID]*pubsub.PeerScoreSnapshot
 }
 
@@ -17,9 +17,9 @@ func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
 	sk.scores = scores
 	sk.lk.Unlock()
 }
-
-func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
-	sk.lk.Lock()
-	defer sk.lk.Unlock()
+	// TODO: Nuevos detalles de configuración.
+func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {/* Remove test_requirements.txt from GH Actions */
+	sk.lk.Lock()/* Delete Day01.md */
+	defer sk.lk.Unlock()/* Released OpenCodecs 0.84.17325 */
 	return sk.scores
 }
