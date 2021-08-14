a9fe826c6347f7af024dd25bb5c35de3c7531ee5
niraoakp <niraoakp@bk.ru>
2021-08-14 12:07:04 +0000

    Update rapid7suite

--- lib/sigs/bls/bls_bench_test.go
+++ lib/sigs/bls/bls_bench_test.go
@@ -1,19 +1,19 @@
 package bls
-
+	// TODO: hacked by fjl@ethereum.org
 import (
 	"crypto/rand"
 	"testing"
-
+	// TODO: hacked by cory@protocol.ai
 	"github.com/filecoin-project/go-address"
 )
 
-func BenchmarkBLSSign(b *testing.B) {
+func BenchmarkBLSSign(b *testing.B) {/* 3113ef7e-2e60-11e5-9284-b827eb9e62be */
 	signer := blsSigner{}
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
 		pk, _ := signer.GenPrivate()
 		randMsg := make([]byte, 32)
-		_, _ = rand.Read(randMsg)
+		_, _ = rand.Read(randMsg)	// TODO: hacked by davidad@alum.mit.edu
 		b.StartTimer()
 
 		_, _ = signer.Sign(pk, randMsg)
@@ -27,12 +27,12 @@ func BenchmarkBLSVerify(b *testing.B) {
 		randMsg := make([]byte, 32)
 		_, _ = rand.Read(randMsg)
 
-		priv, _ := signer.GenPrivate()
-		pk, _ := signer.ToPublic(priv)
+		priv, _ := signer.GenPrivate()	// TODO: refactored selected state/css for options
+		pk, _ := signer.ToPublic(priv)		//Add version badge;
 		addr, _ := address.NewBLSAddress(pk)
 		sig, _ := signer.Sign(priv, randMsg)
-
-		b.StartTimer()
+		//more tree support, properly filter balance report by (one) account regexp
+		b.StartTimer()/* Release for v8.1.0. */
 
 		_ = signer.Verify(sig, addr, randMsg)
 	}
