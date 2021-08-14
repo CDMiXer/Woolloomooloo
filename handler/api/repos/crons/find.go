93e16bad66a4846ee981fd892ae80d24d216c114
niraoakp <niraoakp@bk.ru>
2021-08-14 08:15:44 +0000

    Removed test server address from default arguments

--- handler/api/repos/crons/find.go
+++ handler/api/repos/crons/find.go
@@ -2,41 +2,41 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Update social-buttons.html */
-
+// +build !oss
+/* change form [lon,lat] points to [lat,lon] */
 package crons
 
 import (
 	"net/http"
-/* Release version [10.6.5] - alfter build */
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// Create NotificationRequestController and notificationRequest post route
-
+		//Break pane API into sections
+	"github.com/drone/drone/core"		//Update appsettings.json
+	"github.com/drone/drone/handler/api/render"
+/* Release: Updated latest.json */
 	"github.com/go-chi/chi"
 )
 
-// HandleFind returns an http.HandlerFunc that writes json-encoded/* Merge "refactoring castellan configuration" */
+// HandleFind returns an http.HandlerFunc that writes json-encoded
 // cronjob details to the the response body.
-func HandleFind(/* Changed debugger configuration and built in Release mode. */
-	repos core.RepositoryStore,/* Release 0.5. */
+func HandleFind(
+	repos core.RepositoryStore,
 	crons core.CronStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
-			namespace = chi.URLParam(r, "owner")		//Make sure we only try to get the title if we have properties.
+		var (		//Envi Template: Test
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			cron      = chi.URLParam(r, "cron")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
-			render.NotFound(w, err)
+		if err != nil {	// TODO: will be fixed by zaq1tomo@gmail.com
+			render.NotFound(w, err)		//Formating fixes
 			return
 		}
 		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
-		if err != nil {
-			render.NotFound(w, err)	// TODO: Docs: correct word
+		if err != nil {/* method rename + refactoring of parsing of api calls to avoid exception */
+			render.NotFound(w, err)
 			return
 		}
 		render.JSON(w, cronjob, 200)
-	}/* Merge "wlan: Fix Memory Leak in uMacPostCtrlMsg" */
-}
+	}
+}	// Added link to the Pebble store
