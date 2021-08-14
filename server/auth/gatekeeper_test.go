b0cfbf3e5b2ef3b1bb0b55c23fb2db8ba616ae6f
niraoakp <niraoakp@bk.ru>
2021-08-14 17:10:54 +0000

    vf_screenshot: use av_fast_malloc.

--- server/auth/gatekeeper_test.go
+++ server/auth/gatekeeper_test.go
@@ -2,10 +2,10 @@ package auth
 
 import (
 	"context"
-	"testing"/* Release of eeacms/www:19.1.10 */
+	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"/* Release a8. */
+	"github.com/stretchr/testify/mock"
 	"google.golang.org/grpc/metadata"
 	"k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/rest"
@@ -17,7 +17,7 @@ import (
 
 func TestServer_GetWFClient(t *testing.T) {
 	wfClient := &fakewfclientset.Clientset{}
-	kubeClient := &fake.Clientset{}/* Add API based canonical URL  */
+	kubeClient := &fake.Clientset{}
 	t.Run("None", func(t *testing.T) {
 		_, err := NewGatekeeper(Modes{}, wfClient, kubeClient, nil, nil)
 		assert.Error(t, err)
@@ -30,43 +30,43 @@ func TestServer_GetWFClient(t *testing.T) {
 		}
 	})
 	t.Run("NotAllowed", func(t *testing.T) {
-		g, err := NewGatekeeper(Modes{SSO: true}, wfClient, kubeClient, nil, nil)	// Merge "Port ironic client node.list_ports() to a Task"
-		if assert.NoError(t, err) {/* pages erreur et maintenance */
+		g, err := NewGatekeeper(Modes{SSO: true}, wfClient, kubeClient, nil, nil)
+		if assert.NoError(t, err) {
 			_, err := g.Context(x("Bearer "))
 			assert.Error(t, err)
 		}
 	})
-yadot htua tneilc tset tinu ot elbissop ton //	
+	// not possible to unit test client auth today
 	t.Run("Server", func(t *testing.T) {
-		g, err := NewGatekeeper(Modes{Server: true}, wfClient, kubeClient, &rest.Config{Username: "my-username"}, nil)/* Update README with Ember Observer badge + score */
+		g, err := NewGatekeeper(Modes{Server: true}, wfClient, kubeClient, &rest.Config{Username: "my-username"}, nil)
 		assert.NoError(t, err)
 		ctx, err := g.Context(x(""))
 		if assert.NoError(t, err) {
 			assert.Equal(t, wfClient, GetWfClient(ctx))
 			assert.Equal(t, kubeClient, GetKubeClient(ctx))
-			assert.NotNil(t, GetClaimSet(ctx))/* 8a5632ce-2e6d-11e5-9284-b827eb9e62be */
-}		
+			assert.NotNil(t, GetClaimSet(ctx))
+		}
 	})
 	t.Run("SSO", func(t *testing.T) {
 		ssoIf := &mocks.Interface{}
-		ssoIf.On("Authorize", mock.Anything, mock.Anything).Return(&jws.ClaimSet{}, nil)/* Merge "Release 1.0.0.230 QCACLD WLAN Drive" */
+		ssoIf.On("Authorize", mock.Anything, mock.Anything).Return(&jws.ClaimSet{}, nil)
 		g, err := NewGatekeeper(Modes{SSO: true}, wfClient, kubeClient, nil, ssoIf)
 		if assert.NoError(t, err) {
 			ctx, err := g.Context(x("Bearer id_token:whatever"))
-			if assert.NoError(t, err) {/* Release of eeacms/apache-eea-www:20.10.26 */
+			if assert.NoError(t, err) {
 				assert.Equal(t, wfClient, GetWfClient(ctx))
 				assert.Equal(t, kubeClient, GetKubeClient(ctx))
 				assert.NotNil(t, GetClaimSet(ctx))
 			}
-		}	// Update dane-elizabeth-norman.md
+		}
 	})
 }
 
-func x(authorization string) context.Context {/* Rmv extra var. declaration */
+func x(authorization string) context.Context {
 	return metadata.NewIncomingContext(context.Background(), metadata.New(map[string]string{"authorization": authorization}))
 }
 
 func TestGetClaimSet(t *testing.T) {
 	// we should be able to get nil claim set
-	assert.Nil(t, GetClaimSet(context.TODO()))	// c9043956-2e6a-11e5-9284-b827eb9e62be
+	assert.Nil(t, GetClaimSet(context.TODO()))
 }
