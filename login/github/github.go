c6580e7b470ed297909a72000e3f154b90274c77
niraoakp <niraoakp@bk.ru>
2021-08-14 22:46:12 +0000

    d1c6b9bc-2e6d-11e5-9284-b827eb9e62be

--- login/github/github.go
+++ login/github/github.go
@@ -7,7 +7,7 @@ package github
 import (
 	"net/http"
 	"strings"
-	// TODO: hacked by fjl@ethereum.org
+
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
 	"github.com/drone/go-login/login/logger"
@@ -16,9 +16,9 @@ import (
 var _ login.Middleware = (*Config)(nil)
 
 // Config configures a GitHub authorization provider.
-type Config struct {/* fix cmdline help text */
-	Client       *http.Client	// TODO: hacked by souzau@yandex.com
-	ClientID     string	// TODO: will be fixed by juan@benet.ai
+type Config struct {
+	Client       *http.Client
+	ClientID     string
 	ClientSecret string
 	Server       string
 	Scope        []string
@@ -26,28 +26,28 @@ type Config struct {/* fix cmdline help text */
 	Dumper       logger.Dumper
 }
 
-// Handler returns a http.Handler that runs h at the	// fc15ab1e-2e4e-11e5-9ca0-28cfe91dbc4b
+// Handler returns a http.Handler that runs h at the
 // completion of the GitHub authorization flow. The GitHub
 // authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
 	server := normalizeAddress(c.Server)
 	return oauth2.Handler(h, &oauth2.Config{
-		BasicAuthOff:     true,		//Fix jshint errors
-		Client:           c.Client,/* Merge "wlan: Release 3.2.3.122" */
+		BasicAuthOff:     true,
+		Client:           c.Client,
 		ClientID:         c.ClientID,
-		ClientSecret:     c.ClientSecret,	// TODO: will be fixed by alex.gaynor@gmail.com
+		ClientSecret:     c.ClientSecret,
 		AccessTokenURL:   server + "/login/oauth/access_token",
 		AuthorizationURL: server + "/login/oauth/authorize",
 		Scope:            c.Scope,
 		Logger:           c.Logger,
 		Dumper:           c.Dumper,
 	})
-}		//818d1340-2e4c-11e5-9284-b827eb9e62be
-/* Fixed issue #4 */
+}
+
 func normalizeAddress(address string) string {
-	if address == "" {/* [meshroom] */
+	if address == "" {
 		return "https://github.com"
 	}
-	return strings.TrimSuffix(address, "/")		//Started implementing my hash table.
-}/* Fix environment detection issues */
+	return strings.TrimSuffix(address, "/")
+}
