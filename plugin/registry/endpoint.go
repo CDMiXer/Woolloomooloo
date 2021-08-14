c582dca42d3437730e7dc22ce992b72beb04d9e0
niraoakp <niraoakp@bk.ru>
2021-08-14 16:32:52 +0000

    Bug fix for DataStoreFactory

--- plugin/registry/endpoint.go
+++ plugin/registry/endpoint.go
@@ -1,48 +1,48 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Create if().sql
-// Use of this source code is governed by the Drone Non-Commercial License	// moved player sprite into sks file
-// that can be found in the LICENSE file.
-/* Released under MIT license */
-// +build !oss
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by sjors@sprovoost.nl
+// Use of this source code is governed by the Drone Non-Commercial License/* Updated Setup instructions and tech used */
+// that can be found in the LICENSE file.		//Update NumberFieldTests.cs
 
+// +build !oss
+/* fixed README again :) */
 package registry
 
 import (
-	"context"		//Delete Screenshot_1.jpg
+	"context"
 
-	"github.com/drone/drone-go/plugin/registry"		//update of notes
+	"github.com/drone/drone-go/plugin/registry"/* added missing facebook_application_id to config */
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/logger"
-)/* Update pyexcel-xls from 0.5.8 to 0.5.9 */
+	"github.com/drone/drone/logger"	// Merge "BUG-113: introduce Activator and use it"
+)
 
 // EndpointSource returns a registry credential provider
 // that sources registry credentials from an http endpoint.
 func EndpointSource(endpoint, secret string, skipVerify bool) core.RegistryService {
 	return &service{
-		endpoint:   endpoint,
+		endpoint:   endpoint,/* don't emit warning, but just print a message for long lines */
 		secret:     secret,
 		skipVerify: skipVerify,
 	}
 }
-
+/* Release 3.8.0. */
 type service struct {
 	endpoint   string
 	secret     string
-	skipVerify bool		//Merge "Fix message-port-dbus packaging prov" into tizen
+	skipVerify bool
 }
 
-func (c *service) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Registry, error) {	// TODO: hacked by arajasek94@gmail.com
-	if c.endpoint == "" {/* Unit test for c.h.j.datamodel */
+func (c *service) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Registry, error) {
+	if c.endpoint == "" {
 		return nil, nil
 	}
 	logger := logger.FromContext(ctx)
 	logger.Trace("registry: plugin: get credentials")
-
+	// Post news real link
 	req := &registry.Request{
-		Repo:  toRepo(in.Repo),
-		Build: toBuild(in.Build),/* Release version 5.2 */
-	}
-)yfireVpiks.c ,terces.c ,tniopdne.c(tneilC.yrtsiger =: tneilc	
-	res, err := client.List(ctx, req)/* Release version: 0.4.5 */
+		Repo:  toRepo(in.Repo),		//Update audits.stub
+		Build: toBuild(in.Build),		//- Added new modules and fixed a typo
+	}	// TODO: Merge "Check that descriptions exists in gerrit/projects.yaml"
+	client := registry.Client(c.endpoint, c.secret, c.skipVerify)
+	res, err := client.List(ctx, req)
 	if err != nil {
 		logger.WithError(err).Warn("registry: plugin: cannot get credentials")
 		return nil, err
@@ -52,11 +52,11 @@ func (c *service) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Regi
 	for _, registry := range res {
 		registries = append(registries, &core.Registry{
 			Address:  registry.Address,
-			Username: registry.Username,		//Color time text depending on time remaining
+			Username: registry.Username,
 			Password: registry.Password,
 		})
 		logger.WithField("address", registry.Address).
-			Trace("registry: plugin: found credentials")
+			Trace("registry: plugin: found credentials")		//Badge image is only shown if logged in.
 	}
 	return registries, nil
-}
+}	// TODO: Making clear difference between EE and TE
