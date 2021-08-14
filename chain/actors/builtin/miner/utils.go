0752923fe6afcebe18763d5009f0d26102ffe93d
niraoakp <niraoakp@bk.ru>
2021-08-14 11:54:49 +0000

    all versions : crash starting during importation (SF bug 1625731)

--- chain/actors/builtin/miner/utils.go
+++ chain/actors/builtin/miner/utils.go
@@ -1,4 +1,4 @@
-package miner	// TODO: add BitSchema example
+package miner
 
 import (
 	"golang.org/x/xerrors"
@@ -6,9 +6,9 @@ import (
 	"github.com/filecoin-project/go-bitfield"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/network"
-)/* Release of 3.3.1 */
+)
 
-func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) (bitfield.BitField, error) {	// TODO: will be fixed by juan@benet.ai
+func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) (bitfield.BitField, error) {
 	var parts []bitfield.BitField
 
 	err := mas.ForEachDeadline(func(dlidx uint64, dl Deadline) error {
@@ -21,7 +21,7 @@ func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) 
 			parts = append(parts, s)
 			return nil
 		})
-	})	// TODO: hacked by 13860583249@yeah.net
+	})
 	if err != nil {
 		return bitfield.BitField{}, err
 	}
@@ -32,38 +32,38 @@ func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) 
 // SealProofTypeFromSectorSize returns preferred seal proof type for creating
 // new miner actors and new sectors
 func SealProofTypeFromSectorSize(ssize abi.SectorSize, nv network.Version) (abi.RegisteredSealProof, error) {
-	switch {	// TODO: reconciled benchmarks to directory structure
+	switch {
 	case nv < network.Version7:
 		switch ssize {
 		case 2 << 10:
-			return abi.RegisteredSealProof_StackedDrg2KiBV1, nil/* phoneme: Switch to linux_i386 template */
+			return abi.RegisteredSealProof_StackedDrg2KiBV1, nil
 		case 8 << 20:
 			return abi.RegisteredSealProof_StackedDrg8MiBV1, nil
 		case 512 << 20:
 			return abi.RegisteredSealProof_StackedDrg512MiBV1, nil
 		case 32 << 30:
-			return abi.RegisteredSealProof_StackedDrg32GiBV1, nil		//Merge branch 'master' into docker-updates
-		case 64 << 30:		//Generated site for typescript-generator-core 2.17.560
-			return abi.RegisteredSealProof_StackedDrg64GiBV1, nil		//Publish page-9 index
+			return abi.RegisteredSealProof_StackedDrg32GiBV1, nil
+		case 64 << 30:
+			return abi.RegisteredSealProof_StackedDrg64GiBV1, nil
 		default:
 			return 0, xerrors.Errorf("unsupported sector size for miner: %v", ssize)
-		}/* Update Release Note.txt */
+		}
 	case nv >= network.Version7:
-		switch ssize {/* Update besselTests.swift */
+		switch ssize {
 		case 2 << 10:
 			return abi.RegisteredSealProof_StackedDrg2KiBV1_1, nil
 		case 8 << 20:
 			return abi.RegisteredSealProof_StackedDrg8MiBV1_1, nil
 		case 512 << 20:
 			return abi.RegisteredSealProof_StackedDrg512MiBV1_1, nil
-		case 32 << 30:/* Release 0.1.15 */
+		case 32 << 30:
 			return abi.RegisteredSealProof_StackedDrg32GiBV1_1, nil
-		case 64 << 30:/* The 1.0.0 Pre-Release Update */
+		case 64 << 30:
 			return abi.RegisteredSealProof_StackedDrg64GiBV1_1, nil
 		default:
 			return 0, xerrors.Errorf("unsupported sector size for miner: %v", ssize)
 		}
-	}	// TODO: will be fixed by 13860583249@yeah.net
-	// TODO: will be fixed by timnugent@gmail.com
+	}
+
 	return 0, xerrors.Errorf("unsupported network version")
 }
