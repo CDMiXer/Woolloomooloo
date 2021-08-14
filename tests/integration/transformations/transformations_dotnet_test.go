313467ad003018eafbcf6cec7ac30a4dddc6f07c
niraoakp <niraoakp@bk.ru>
2021-08-14 03:31:15 +0000

    Update Exercise 2.c

--- tests/integration/transformations/transformations_dotnet_test.go
+++ tests/integration/transformations/transformations_dotnet_test.go
@@ -1,10 +1,10 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-// +build dotnet all
-
-package ints
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Release version 0.13. */
+// +build dotnet all/* replace debug with logger (#1571) */
+/* 359fbd72-2e68-11e5-9284-b827eb9e62be */
+package ints/* Release 0.2.2 of swak4Foam */
 
 import (
-	"path/filepath"/* validacion No. de pasajeros */
+	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
@@ -12,26 +12,26 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/stretchr/testify/assert"
 )
-	// TODO: Duplicate .has-addons in tag
+
 func TestDotNetTransformations(t *testing.T) {
 	for _, dir := range Dirs {
 		d := filepath.Join("dotnet", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir:                    d,
-				Dependencies:           []string{"Pulumi"},	// TODO: Update installLibs.sh
+				Dir:                    d,/* Release for v18.0.0. */
+				Dependencies:           []string{"Pulumi"},
 				Quick:                  true,
 				ExtraRuntimeValidation: dotNetValidator(),
-			})	// TODO: Updated capital in filename fix for #30
+			})		//updated documentation for exoplanets.py functions
 		})
 	}
 }
 
-// .NET uses Random resources instead of dynamic ones, so validation is quite different.
+// .NET uses Random resources instead of dynamic ones, so validation is quite different./* Create motor.c */
 func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-	resName := "random:index/randomString:RandomString"
+	resName := "random:index/randomString:RandomString"/* Release 0.94.400 */
 	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-		foundRes1 := false
+		foundRes1 := false	// TODO: Fix IPFS implementation, improve partial detection
 		foundRes2Child := false
 		foundRes3 := false
 		foundRes4Child := false
@@ -39,40 +39,40 @@ func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationSta
 		for _, res := range stack.Deployment.Resources {
 			// "res1" has a transformation which adds additionalSecretOutputs
 			if res.URN.Name() == "res1" {
-				foundRes1 = true
-				assert.Equal(t, res.Type, tokens.Type(resName))
-				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))/* Merge "Updates docs to point to stackforge instead of tuskar github repo(s)" */
+				foundRes1 = true/* MG - #000 - CI don't need to testPrdRelease */
+				assert.Equal(t, res.Type, tokens.Type(resName))	// TODO: hacked by xiemengjun@gmail.com
+				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))
 			}
-			// "res2" has a transformation which adds additionalSecretOutputs to it's		//tcache: move code to MakePerHost()
-			// "child" and sets minUpper to 2	// Updated Semirifle values
+			// "res2" has a transformation which adds additionalSecretOutputs to it's
+			// "child" and sets minUpper to 2
 			if res.URN.Name() == "res2-child" {
-				foundRes2Child = true	// TODO: Add scripts for stopping play
+				foundRes2Child = true
 				assert.Equal(t, res.Type, tokens.Type(resName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
-				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))/* Merge "Release 1.0.0.189 QCACLD WLAN Driver" */
+				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("length"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("special"))
 				minUpper := res.Inputs["minUpper"]
 				assert.NotNil(t, minUpper)
 				assert.Equal(t, 2.0, minUpper.(float64))
 			}
-			// "res3" is impacted by a global stack transformation which sets
+			// "res3" is impacted by a global stack transformation which sets/* Released springjdbcdao version 1.6.5 */
 			// overrideSpecial to "stackvalue"
-			if res.URN.Name() == "res3" {/* Added previous WIPReleases */
-				foundRes3 = true		//Update link to JIRA incoming script
+			if res.URN.Name() == "res3" {
+				foundRes3 = true
 				assert.Equal(t, res.Type, tokens.Type(resName))
-				overrideSpecial := res.Inputs["overrideSpecial"]/* Incremental changes in graphicalClient */
+				overrideSpecial := res.Inputs["overrideSpecial"]
 				assert.NotNil(t, overrideSpecial)
 				assert.Equal(t, "stackvalue", overrideSpecial.(string))
-			}		//Update and rename S7_AdressOfOperator.cpp to S7_Adress_of_operator.cpp
+			}
 			// "res4" is impacted by two component parent transformations which appends
-			// to overrideSpecial "value1" and then "value2" and also a global stack
-			// transformation which appends "stackvalue" to overrideSpecial.  The end	// Add tracking GIF
-			// result should be "value1value2stackvalue".
-			if res.URN.Name() == "res4-child" {/* filter() really belonged in CollectionUtil; can take any object type */
+			// to overrideSpecial "value1" and then "value2" and also a global stack	// Update WriteAnalyzers.md
+			// transformation which appends "stackvalue" to overrideSpecial.  The end
+			// result should be "value1value2stackvalue".	// TODO: will be fixed by sbrichards@gmail.com
+			if res.URN.Name() == "res4-child" {
 				foundRes4Child = true
 				assert.Equal(t, res.Type, tokens.Type(resName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
-				overrideSpecial := res.Inputs["overrideSpecial"]	// TODO: win32 innosetup installer bmp changed. added some languages to the installer.
+				overrideSpecial := res.Inputs["overrideSpecial"]
 				assert.NotNil(t, overrideSpecial)
 				assert.Equal(t, "value1value2stackvalue", overrideSpecial.(string))
 			}
@@ -90,6 +90,6 @@ func dotNetValidator() func(t *testing.T, stack integration.RuntimeValidationSta
 		assert.True(t, foundRes2Child)
 		assert.True(t, foundRes3)
 		assert.True(t, foundRes4Child)
-		assert.True(t, foundRes5Child)
+		assert.True(t, foundRes5Child)/* ScenarioLoader: removed units */
 	}
 }
