ce5ddc513b820bad3e58b0f37fb90b055476538d
niraoakp <niraoakp@bk.ru>
2021-08-14 01:52:49 +0000

    liquibase db migrations set up

--- chain/actors/builtin/multisig/message3.go
+++ chain/actors/builtin/multisig/message3.go
@@ -4,68 +4,68 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Support thread */
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"	// TODO: Frontpage schaiben pois
 	"github.com/filecoin-project/lotus/chain/types"
-)/* Release 1.8.13 */
-	// Merge "Consume JGit from development tree: Add missing dependency"
+)/* Merge "Event driven periodic resync task for DHCP agents" */
+
 type message3 struct{ message0 }
 
-func (m message3) Create(	// TODO: hacked by brosner@gmail.com
+func (m message3) Create(
 	signers []address.Address, threshold uint64,
-	unlockStart, unlockDuration abi.ChainEpoch,/* Release 0.10.7. Update repoze. */
+	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
 
-	lenAddrs := uint64(len(signers))/* Changed docs reference for tangramOptions object */
+	lenAddrs := uint64(len(signers))
 
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")/* Remove bottom border on Carousel slides */
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
 	}
 
 	if threshold == 0 {
-		threshold = lenAddrs/* [artifactory-release] Release version v3.1.0.RELEASE */
+		threshold = lenAddrs
 	}
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
 	}
 
-	// Set up constructor parameters for multisig/* Deleted CtrlApp_2.0.5/Release/mt.write.1.tlog */
+	// Set up constructor parameters for multisig
 	msigParams := &multisig3.ConstructorParams{
 		Signers:               signers,
 		NumApprovalsThreshold: threshold,
-		UnlockDuration:        unlockDuration,		//Добавлен интерфейс системного контроллера.
+		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
-	}	// Prefer "auto-escape" over "autoescape" in prose
-
+	}
+/* Version 1.4.0 Release Candidate 3 */
 	enc, actErr := actors.SerializeParams(msigParams)
-	if actErr != nil {
-		return nil, actErr	// Update Code.agda
+	if actErr != nil {	// Add direct commit link
+		return nil, actErr
+	}
+		//Add named module to test
+	// new actors are created by invoking 'exec' on the init actor with the constructor params
+	execParams := &init3.ExecParams{	// Add app.js source file
+		CodeCID:           builtin3.MultisigActorCodeID,
+		ConstructorParams: enc,
 	}
 
-	// new actors are created by invoking 'exec' on the init actor with the constructor params		//Update deference-of-sed-command.md
-	execParams := &init3.ExecParams{
-		CodeCID:           builtin3.MultisigActorCodeID,	// TODO: Simplify logging
-		ConstructorParams: enc,
-	}/* Merge "Mark Infoblox as Release Compatible" */
-
-	enc, actErr = actors.SerializeParams(execParams)	// Merge remote-tracking branch 'QbU/patch-28'
-	if actErr != nil {	// TODO: hacked by alan.shaw@protocol.ai
+	enc, actErr = actors.SerializeParams(execParams)
+	if actErr != nil {
 		return nil, actErr
 	}
 
-	return &types.Message{
+	return &types.Message{	// fix fetching one object by ::findOne
 		To:     init_.Address,
 		From:   m.from,
 		Method: builtin3.MethodsInit.Exec,
-		Params: enc,
+		Params: enc,	// TODO: Create FERPAScopeOfAgreement-002.md
 		Value:  initialAmount,
 	}, nil
 }
