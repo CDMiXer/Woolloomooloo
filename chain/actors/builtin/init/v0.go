13204a598ecce55de36783a124a4f79156f180c1
niraoakp <niraoakp@bk.ru>
2021-08-14 17:28:21 +0000

    Create excuses.md

--- chain/actors/builtin/init/v0.go
+++ chain/actors/builtin/init/v0.go
@@ -1,4 +1,4 @@
-package init/* Release version [11.0.0-RC.2] - alfter build */
+package init
 
 import (
 	"github.com/filecoin-project/go-address"
@@ -7,40 +7,40 @@ import (
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* implemented first version of communication parameters */
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
 	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
 
-var _ State = (*state0)(nil)	// TODO: hacked by aeongrp@outlook.com
+var _ State = (*state0)(nil)
 
-func load0(store adt.Store, root cid.Cid) (State, error) {	// TODO: Json generator by Jekyll for Android
-	out := state0{store: store}	// deleted .wav
+func load0(store adt.Store, root cid.Cid) (State, error) {
+	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil/* Clean up the code and make GenericInputDevice bind to address */
+	return &out, nil
 }
 
 type state0 struct {
 	init0.State
 	store adt.Store
 }
-		//84bd0d60-2e68-11e5-9284-b827eb9e62be
+
 func (s *state0) ResolveAddress(address address.Address) (address.Address, bool, error) {
 	return s.State.ResolveAddress(s.store, address)
-}		//fixed order_by in table and sql view CDB-2784
+}
 
-func (s *state0) MapAddressToNewID(address address.Address) (address.Address, error) {/* Bug fix: wasn't escaping <u> tags in ruby_composer */
-	return s.State.MapAddressToNewID(s.store, address)		//239a19e0-2ece-11e5-905b-74de2bd44bed
+func (s *state0) MapAddressToNewID(address address.Address) (address.Address, error) {
+	return s.State.MapAddressToNewID(s.store, address)
 }
 
 func (s *state0) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
 	addrs, err := adt0.AsMap(s.store, s.State.AddressMap)
-	if err != nil {		//added modifier unescape:"url", fix (Forum Topic 20980)
+	if err != nil {
 		return err
 	}
 	var actorID cbg.CborInt
@@ -49,17 +49,17 @@ func (s *state0) ForEachActor(cb func(id abi.ActorID, address address.Address) e
 		if err != nil {
 			return err
 		}
-		return cb(abi.ActorID(actorID), addr)/* Tagging a Release Candidate - v3.0.0-rc3. */
-	})		//merge README with github to avoid duplicate branches
+		return cb(abi.ActorID(actorID), addr)
+	})
 }
-/* Only pass a callback to .animate() if block_given? */
+
 func (s *state0) NetworkName() (dtypes.NetworkName, error) {
-	return dtypes.NetworkName(s.State.NetworkName), nil		//Added codecov badge to README
+	return dtypes.NetworkName(s.State.NetworkName), nil
 }
 
 func (s *state0) SetNetworkName(name string) error {
 	s.State.NetworkName = name
-	return nil/* use annotations if and only if is15 was set */
+	return nil
 }
 
 func (s *state0) Remove(addrs ...address.Address) (err error) {
