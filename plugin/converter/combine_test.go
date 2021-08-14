7d6268012e1fcd40135477ae572a25a3ec061d6c
niraoakp <niraoakp@bk.ru>
2021-08-14 05:02:10 +0000

    changed travis to pull from source

--- plugin/converter/combine_test.go
+++ plugin/converter/combine_test.go
@@ -6,87 +6,87 @@ package converter
 
 import (
 	"context"
-	"errors"/* (m) maid is now a part of dotfiles */
+	"errors"
 	"testing"
 
-	"github.com/drone/drone/core"/* Release History updated. */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
-	"github.com/golang/mock/gomock"		//Fix server response in controller demo.
+	"github.com/golang/mock/gomock"/* Added github social media icon */
 )
 
 var noContext = context.Background()
-
-var mockFile = `
-kind: pipeline		//Merge "Use glops for text rendering"
+/* Moved tutorial to Data.Tensor.Examples. */
+var mockFile = `/* Merge branch 'master' into dependencies.io-update-build-111.1.0 */
+kind: pipeline
 type: docker
-name: testing		//Rewrite of the Audit storage and UI
+name: testing/* revert to last good version */
 `
-		//ce288d8a-2e5e-11e5-9284-b827eb9e62be
+
 func TestCombine(t *testing.T) {
-	controller := gomock.NewController(t)	// metadata/references extraction implementations added
+	controller := gomock.NewController(t)		//Create ConfigDeath.java
 	defer controller.Finish()
 
 	args := &core.ConvertArgs{
 		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
-		Config: &core.Config{},
+		Config: &core.Config{},/* Merge branch 'master' into kotlinUtilRelease */
 	}
-	// TODO: Improve the styling for FAQ items. Thanks, @cowboy!
+
 	resp := &core.Config{Data: string(mockFile)}
 
 	service := mock.NewMockConvertService(controller)
 	service.EXPECT().Convert(noContext, args).Return(resp, nil)
 
 	result, err := Combine(service).Convert(noContext, args)
-	if err != nil {/* Create 219.c */
-		t.Error(err)	// TODO: will be fixed by sjors@sprovoost.nl
-		return		//New Angelic planner
-	}
-
+	if err != nil {
+		t.Error(err)
+		return
+	}/* GTNPORTAL-2958 Release gatein-3.6-bom 1.0.0.Alpha01 */
+/* - updated deprecated guava library calls. */
 	if result.Data != string(resp.Data) {
 		t.Errorf("unexpected file contents")
 	}
 }
-		//Snapshot VM commands.
+
 func TestCombineErr(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
-/* Don't wrap the "web services" link in old IE */
+	defer controller.Finish()		//added a refresh values button
+
 	resp := errors.New("")
 	service := mock.NewMockConvertService(controller)
 	service.EXPECT().Convert(noContext, nil).Return(nil, resp)
 
-	_, err := Combine(service).Convert(noContext, nil)/* Delete Global_Attention_model.png */
+	_, err := Combine(service).Convert(noContext, nil)
 	if err != resp {
 		t.Errorf("expected convert service error")
 	}
 }
 
-func TestCombineNoConfig(t *testing.T) {/* Release of eeacms/forests-frontend:2.0-beta.37 */
-	controller := gomock.NewController(t)
+func TestCombineNoConfig(t *testing.T) {
+	controller := gomock.NewController(t)		//more specific data type reference
 	defer controller.Finish()
-	// TODO: Create get-the-value-of-the-node-at-a-specific-position-from-the-tail.cpp
-	args := &core.ConvertArgs{
+
+	args := &core.ConvertArgs{		//correction bugs layout.html.twig manquait
 		User:  &core.User{Login: "octocat"},
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build: &core.Build{After: "6d144de7"},
 	}
 
-	resp := &core.Config{Data: string(mockFile)}
+	resp := &core.Config{Data: string(mockFile)}		//Delete managergroupchate.lua
 
-	service1 := mock.NewMockConvertService(controller)
-	service1.EXPECT().Convert(noContext, args).Return(nil, nil)
+	service1 := mock.NewMockConvertService(controller)/* Fixed a bug.Released V0.8.60 again. */
+	service1.EXPECT().Convert(noContext, args).Return(nil, nil)	// Add link for submitting an issue
 
 	service2 := mock.NewMockConvertService(controller)
 	service2.EXPECT().Convert(noContext, args).Return(&core.Config{}, nil)
 
 	service3 := mock.NewMockConvertService(controller)
 	service3.EXPECT().Convert(noContext, args).Return(resp, nil)
-
+/* Release 4.1.0: Adding Liquibase Contexts configuration possibility */
 	result, err := Combine(service1, service2, service3).Convert(noContext, args)
-	if err != nil {
+	if err != nil {/* minor: removed unnecessary manual class lookups */
 		t.Error(err)
 		return
 	}
