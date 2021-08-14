d398fddb295219a93a475f84a058b36497030330
niraoakp <niraoakp@bk.ru>
2021-08-14 18:51:06 +0000

    Modified code for logout button

--- store/shared/migrate/postgres/files/007_create_table_logs.sql
+++ store/shared/migrate/postgres/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
 -- name: create-table-logs
-
+/* Release of eeacms/energy-union-frontend:1.7-beta.20 */
 CREATE TABLE IF NOT EXISTS logs (
  log_id    SERIAL PRIMARY KEY
-,log_data  BYTEA/* Delete NvFlexReleaseD3D_x64.lib */
-);
+,log_data  BYTEA
+);	// TODO: Update to experimental r13479
