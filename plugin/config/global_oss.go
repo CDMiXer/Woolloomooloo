a320ab65d37a7e4133394fe4a19c83504668e9a5
niraoakp <niraoakp@bk.ru>
2021-08-14 06:09:47 +0000

    commenting out dbcontent from terminology_server.yml

--- plugin/config/global_oss.go
+++ plugin/config/global_oss.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// Bug fix for forward chat.
 // You may obtain a copy of the License at
-//		//Merge branch 'develop' into rfq-filter-by-region
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//Fixed a type in the Readme …
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release  2 */
+//	// TODO: Update Rel.jnlp with file association for .rel extension.
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// system/UniqueFileDescriptor: make the base class public
+// distributed under the License is distributed on an "AS IS" BASIS,		//Change class of doucment because we have several document classes.
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License./* [artifactory-release] Release version 1.4.3.RELEASE */
-	// TODO: Adding tests for parsing the rio text back into rio.
-// +build oss/* allow remove field from structure */
+// See the License for the specific language governing permissions and/* Add Release Url */
+// limitations under the License.
+
+// +build oss
 
 package config
 
-import (
+import (/* Release 2.0.0-RC4 */
 	"context"
 	"time"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Fix NET461 tests
 )
 
 // Global returns a no-op configuration service.
@@ -31,5 +31,5 @@ func Global(string, string, bool, time.Duration) core.ConfigService {
 type noop struct{}
 
 func (noop) Find(context.Context, *core.ConfigArgs) (*core.Config, error) {
-	return nil, nil/* Update sphinx from 3.4.0 to 3.4.1 */
+	return nil, nil
 }
