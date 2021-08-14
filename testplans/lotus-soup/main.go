d6b003c7d463eff2fca1fec657483a733fb29ea7
niraoakp <niraoakp@bk.ru>
2021-08-14 02:57:44 +0000

    Upgrading jarchivelib dependency (ensure JDK11 compatibility).

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -1,20 +1,20 @@
 package main
-
+/* Release 7.12.37 */
 import (
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
-
+/* Updated Issa - What a Peony */
 	"github.com/testground/sdk-go/run"
 )
-		//0eaf6d4c-2e4b-11e5-9284-b827eb9e62be
-var cases = map[string]interface{}{
+
+var cases = map[string]interface{}{/* Release for 2.19.0 */
 	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
 	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
-	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),/* Repo name was changed. */
-	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),/* Release 1.8.0 */
-	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),	// TODO: will be fixed by ligi@ligi.de
+	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
+	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
+	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
 }
 
 func main() {
