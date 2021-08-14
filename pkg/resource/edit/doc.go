24112a9cbcdd4015e2c167c42bf9045a0970e5bf
niraoakp <niraoakp@bk.ru>
2021-08-14 07:25:39 +0000

    93b51afe-2e4c-11e5-9284-b827eb9e62be

--- pkg/resource/edit/doc.go
+++ pkg/resource/edit/doc.go
@@ -7,11 +7,11 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by seth@sethvargo.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Create patches.rb */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: delete redundant comment
 // Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level
 // tools that present a means for users to surgically edit their state.
 package edit
