5b7b44bbe01e3d81300e362794d2ae869a753093
niraoakp <niraoakp@bk.ru>
2021-08-14 03:14:08 +0000

    fix content change animation

--- tests/integration/dependency_steps/step1/index.ts
+++ tests/integration/dependency_steps/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+		//ca3c9d02-2e4e-11e5-a98a-28cfe91dbc4b
+import { Resource } from "./resource";
 
-import { Resource } from "./resource";/* f3c338b0-2e4a-11e5-9284-b827eb9e62be */
-/* Release 1.3.11 */
-// Step 1: Populate our dependency graph./* Run on iPhone SE */
-const a = new Resource("a", { state: 1 });		//plainmake.sh: further declaration
-const b = new Resource("b", { state: 2, resource: a });	// added some doxygen comment docblocks
+// Step 1: Populate our dependency graph.		//Clean up the strategy
+const a = new Resource("a", { state: 1 });/* Update cahier_des_charges.txt */
+const b = new Resource("b", { state: 2, resource: a });
 // Dependency graph: b -> a
