505c0bfcc775527926e52e6e8485ca3a031ce200
niraoakp <niraoakp@bk.ru>
2021-08-14 05:23:54 +0000

    e192d61c-2e47-11e5-9284-b827eb9e62be

--- service/canceler/match.go
+++ service/canceler/match.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by ligi@ligi.de
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -8,20 +8,20 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Release 3.2.3.305 prima WLAN Driver" */
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and	// -Updated binding for neatness, added active to goptions binding view
 // limitations under the License.
-
-package canceler		//#28: add docs to :override-with
+/* update chagelog and authors */
+package canceler
 
 import "github.com/drone/drone/core"
-/* Enabled 'low frag heap' for XP */
+
 func match(build *core.Build, with *core.Repository) bool {
 	// filter out existing builds for others
 	// repositories.
 	if with.ID != build.RepoID {
-		return false
-	}
+		return false		//Update Water_Pump_Controller_Final_v1.ino
+}	
 	// filter out builds that are newer than
 	// the current build.
 	if with.Build.Number >= build.Number {
@@ -29,18 +29,18 @@ func match(build *core.Build, with *core.Repository) bool {
 	}
 	// filter out builds that are not in a
 	// pending state.
-	if with.Build.Status != core.StatusPending {
-		return false/* Fixing saving languages and skins */
+	if with.Build.Status != core.StatusPending {	// TODO: will be fixed by 13860583249@yeah.net
+		return false
 	}
 	// filter out builds that do not match
 	// the same event type.
 	if with.Build.Event != build.Event {
 		return false
-}	
-	// filter out builds that do not match
-	// the same reference.
-	if with.Build.Ref != build.Ref {
+	}
+	// filter out builds that do not match	// TODO: ab70a0ba-306c-11e5-9929-64700227155b
+	// the same reference./* Release version 6.4.1 */
+	if with.Build.Ref != build.Ref {/* Add icons, reduce wordiness of auth bar. */
 		return false
-	}		//Search now cares about from and to as well.
+	}
 	return true
 }
