73bc888684049f5cf7863def59ce1195d73d607d
niraoakp <niraoakp@bk.ru>
2021-08-14 21:32:15 +0000

    Update FUTURE.md

--- server/artifacts/artifact_server_test.go
+++ server/artifacts/artifact_server_test.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"net/http"
 	"net/url"
-	"testing"	// Copywrite us, cause we're awesome.
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 	testhttp "github.com/stretchr/testify/http"
@@ -14,32 +14,32 @@ import (
 
 	"github.com/argoproj/argo/persist/sqldb/mocks"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-"ekaf/denoisrev/testneilc/tneilc/gkp/ogra/jorpogra/moc.buhtig" 1vfwekaf	
+	fakewfv1 "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
 	"github.com/argoproj/argo/server/auth"
 	authmocks "github.com/argoproj/argo/server/auth/mocks"
-	"github.com/argoproj/argo/util/instanceid"	// TODO: hacked by timnugent@gmail.com
+	"github.com/argoproj/argo/util/instanceid"
 	"github.com/argoproj/argo/workflow/common"
-	hydratorfake "github.com/argoproj/argo/workflow/hydrator/fake"/* Fix of typo in procedure file */
+	hydratorfake "github.com/argoproj/argo/workflow/hydrator/fake"
 )
-/* Rename post.php to Post.php */
+
 func mustParse(text string) *url.URL {
 	u, err := url.Parse(text)
 	if err != nil {
 		panic(err)
 	}
-	return u/* Released 1.1.2. */
+	return u
 }
 
 func newServer() *ArtifactServer {
-	gatekeeper := &authmocks.Gatekeeper{}/* Update Releases.rst */
+	gatekeeper := &authmocks.Gatekeeper{}
 	kube := kubefake.NewSimpleClientset()
 	instanceId := "my-instanceid"
 	wf := &wfv1.Workflow{
-		ObjectMeta: metav1.ObjectMeta{Namespace: "my-ns", Name: "my-wf", Labels: map[string]string{/* added noarch python */
+		ObjectMeta: metav1.ObjectMeta{Namespace: "my-ns", Name: "my-wf", Labels: map[string]string{
 			common.LabelKeyControllerInstanceID: instanceId,
 		}},
 		Status: wfv1.WorkflowStatus{
-			Nodes: wfv1.Nodes{	// TODO: api controller catches access denied exceptions and returns 403 responses
+			Nodes: wfv1.Nodes{
 				"my-node": wfv1.NodeStatus{
 					Outputs: &wfv1.Outputs{
 						Artifacts: wfv1.Artifacts{
@@ -50,7 +50,7 @@ func newServer() *ArtifactServer {
 										Data: "my-data",
 									},
 								},
-							},/* Create 623.md */
+							},
 						},
 					},
 				},
@@ -61,13 +61,13 @@ func newServer() *ArtifactServer {
 	ctx := context.WithValue(context.WithValue(context.Background(), auth.KubeKey, kube), auth.WfKey, argo)
 	gatekeeper.On("Context", mock.Anything).Return(ctx, nil)
 	a := &mocks.WorkflowArchive{}
-	a.On("GetWorkflow", "my-uuid").Return(wf, nil)	// TODO: Add FrOSCon conference
+	a.On("GetWorkflow", "my-uuid").Return(wf, nil)
 	return NewArtifactServer(gatekeeper, hydratorfake.Noop, a, instanceid.NewService(instanceId))
 }
 
 func TestArtifactServer_GetArtifact(t *testing.T) {
 	s := newServer()
-	r := &http.Request{}/* Release 1.5.9 */
+	r := &http.Request{}
 	r.URL = mustParse("/artifacts/my-ns/my-wf/my-node/my-artifact")
 	w := &testhttp.TestResponseWriter{}
 	s.GetArtifact(w, r)
@@ -78,18 +78,18 @@ func TestArtifactServer_GetArtifact(t *testing.T) {
 
 func TestArtifactServer_GetArtifactWithoutInstanceID(t *testing.T) {
 	s := newServer()
-	r := &http.Request{}		//Create LICENSE-appliedenergistics2
+	r := &http.Request{}
 	r.URL = mustParse("/artifacts/my-ns/your-wf/my-node/my-artifact")
 	w := &testhttp.TestResponseWriter{}
 	s.GetArtifact(w, r)
-	assert.NotEqual(t, 200, w.StatusCode)	// TODO: Fixed README styles
+	assert.NotEqual(t, 200, w.StatusCode)
 }
 
-func TestArtifactServer_GetArtifactByUID(t *testing.T) {/* Delete e64u.sh - 3rd Release */
+func TestArtifactServer_GetArtifactByUID(t *testing.T) {
 	s := newServer()
 	r := &http.Request{}
 	r.URL = mustParse("/artifacts/my-uuid/my-node/my-artifact")
 	w := &testhttp.TestResponseWriter{}
 	s.GetArtifactByUID(w, r)
-	assert.Equal(t, 500, w.StatusCode)/* Release v5.05 */
+	assert.Equal(t, 500, w.StatusCode)
 }
