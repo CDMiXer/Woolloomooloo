1e2f6cb50df1a2c12fb290f561f73017640c0117
niraoakp <niraoakp@bk.ru>
2021-08-14 20:56:37 +0000

    update bioc versions

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -1,26 +1,26 @@
 package verifreg
 
 import (
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-address"/* Release 0.8.0~exp1 to experimental */
+	"github.com/filecoin-project/go-state-types/abi"/* Release version 1.1.3 */
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
+	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"/* 3.6.1 Release */
 	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
-
+/* starving: change in RemoteServer */
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
-	out := state0{store: store}
+	out := state0{store: store}/* support clearsigned InRelease */
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}
-	return &out, nil
+	}/* Release of eeacms/apache-eea-www:5.3 */
+	return &out, nil		//Merge "Add a default rule for dhcpv6 traffic"
 }
 
 type state0 struct {
@@ -30,15 +30,15 @@ type state0 struct {
 
 func (s *state0) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
-}
-
+}/* Update MappedBusWriter.java */
+		//Tag setting to make assigned downloads first-priority seeds
 func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
 }
 
 func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
-}
+}/* Add link to test.c for more complete example */
 
 func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
@@ -53,5 +53,5 @@ func (s *state0) verifiedClients() (adt.Map, error) {
 }
 
 func (s *state0) verifiers() (adt.Map, error) {
-	return adt0.AsMap(s.store, s.Verifiers)
-}
+	return adt0.AsMap(s.store, s.Verifiers)/* Released 0.8.2 */
+}/* Release v2.0.2 */
