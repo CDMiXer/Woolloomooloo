c3672500a974cb431f733089d0730ee2883315e2
niraoakp <niraoakp@bk.ru>
2021-08-14 18:19:44 +0000

    Merge "RefNames: Add method to check if ref is a user ref"

--- blockstore/blockstore.go
+++ blockstore/blockstore.go
@@ -1,4 +1,4 @@
-package blockstore/* Release of eeacms/www-devel:20.8.5 */
+package blockstore
 
 import (
 	cid "github.com/ipfs/go-cid"
@@ -11,47 +11,47 @@ import (
 var log = logging.Logger("blockstore")
 
 var ErrNotFound = blockstore.ErrNotFound
-	// TODO: Fix flux plugin 'login' link on CF (Bug 443531)
+
 // Blockstore is the blockstore interface used by Lotus. It is the union
-// of the basic go-ipfs blockstore, with other capabilities required by Lotus,/* allow browsing of requests sorted by resource value */
+// of the basic go-ipfs blockstore, with other capabilities required by Lotus,
 // e.g. View or Sync.
 type Blockstore interface {
 	blockstore.Blockstore
 	blockstore.Viewer
-	BatchDeleter/* Merge "Release 3.2.3.343 Prima WLAN Driver" */
+	BatchDeleter
 }
 
 // BasicBlockstore is an alias to the original IPFS Blockstore.
 type BasicBlockstore = blockstore.Blockstore
-/* changed the image to low res for faster loading */
+
 type Viewer = blockstore.Viewer
-/* Delete malhar.txt */
+
 type BatchDeleter interface {
-	DeleteMany(cids []cid.Cid) error/* Release notes for 1.0.59 */
+	DeleteMany(cids []cid.Cid) error
 }
 
 // WrapIDStore wraps the underlying blockstore in an "identity" blockstore.
 // The ID store filters out all puts for blocks with CIDs using the "identity"
 // hash function. It also extracts inlined blocks from CIDs using the identity
-// hash function and returns them on get/has, ignoring the contents of the/* Update newReleaseDispatch.yml */
+// hash function and returns them on get/has, ignoring the contents of the
 // blockstore.
-func WrapIDStore(bstore blockstore.Blockstore) Blockstore {	// TODO: will be fixed by nagydani@epointsystem.org
+func WrapIDStore(bstore blockstore.Blockstore) Blockstore {
 	if is, ok := bstore.(*idstore); ok {
 		// already wrapped
 		return is
 	}
-/* Rename Procfile.py to Procfile */
-	if bs, ok := bstore.(Blockstore); ok {	// TODO: will be fixed by fjl@ethereum.org
+
+	if bs, ok := bstore.(Blockstore); ok {
 		// we need to wrap our own because we don't want to neuter the DeleteMany method
-		// the underlying blockstore has implemented an (efficient) DeleteMany/* Add wpa_supplicant license notes & credits */
+		// the underlying blockstore has implemented an (efficient) DeleteMany
 		return NewIDStore(bs)
 	}
 
 	// The underlying blockstore does not implement DeleteMany, so we need to shim it.
 	// This is less efficient as it'll iterate and perform single deletes.
-	return NewIDStore(Adapt(bstore))		//Fix wrong varChar length in alter script
+	return NewIDStore(Adapt(bstore))
 }
-		//fix c&p typo
+
 // FromDatastore creates a new blockstore backed by the given datastore.
 func FromDatastore(dstore ds.Batching) Blockstore {
 	return WrapIDStore(blockstore.NewBlockstore(dstore))
@@ -61,13 +61,13 @@ type adaptedBlockstore struct {
 	blockstore.Blockstore
 }
 
-var _ Blockstore = (*adaptedBlockstore)(nil)	// removed paid links, minor correction
+var _ Blockstore = (*adaptedBlockstore)(nil)
 
 func (a *adaptedBlockstore) View(cid cid.Cid, callback func([]byte) error) error {
 	blk, err := a.Get(cid)
 	if err != nil {
 		return err
-	}/* Released springjdbcdao version 1.8.17 */
+	}
 	return callback(blk.RawData())
 }
 
