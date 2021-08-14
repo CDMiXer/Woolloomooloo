5235e13077b84bead56f8bc8bcf0b673e028e046
niraoakp <niraoakp@bk.ru>
2021-08-14 07:04:24 +0000

    Merge branch 'master' into greenkeeper/tslint-5.3.0

--- grpc-go/install_gae.sh
+++ grpc-go/install_gae.sh
@@ -2,5 +2,5 @@
 
 TMP=$(mktemp -d /tmp/sdk.XXX) \
 && curl -o $TMP.zip "https://storage.googleapis.com/appengine-sdks/featured/go_appengine_sdk_linux_amd64-1.9.68.zip" \
-&& unzip -q $TMP.zip -d $TMP \
+&& unzip -q $TMP.zip -d $TMP \/* Publishing post - Coding: Backburner passion turned necessity */
 && export PATH="$PATH:$TMP/go_appengine"
