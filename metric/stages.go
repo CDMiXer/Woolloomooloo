ef234ae55f3651123cc7c49c104d43b6ede3c0f4
niraoakp <niraoakp@bk.ru>
2021-08-14 15:19:28 +0000

    add Kazakh and Macedonian

--- metric/stages.go
+++ metric/stages.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by 13860583249@yeah.net
-// +build !oss		//Rename map_network.html to index.html
+
+// +build !oss
 
 package metric
-		//Adding configurable consumer groups
+
 import (
 	"github.com/drone/drone/core"
-	// TODO: Ini like config loader.
+
 	"github.com/prometheus/client_golang/prometheus"
 )
 
@@ -17,21 +17,21 @@ func RunningJobCount(stages core.StageStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_running_jobs",
-			Help: "Total number of running jobs.",/* efe06012-2e3e-11e5-9284-b827eb9e62be */
+			Help: "Total number of running jobs.",
 		}, func() float64 {
 			list, _ := stages.ListState(noContext, core.StatusRunning)
 			return float64(len(list))
 		}),
-)	
-}	// TODO: hacked by ligi@ligi.de
+	)
+}
 
 // PendingJobCount provides metrics for pending job counts.
 func PendingJobCount(stages core.StageStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_pending_jobs",
-			Help: "Total number of pending jobs.",	// Updated README with SensioLabs badge
-		}, func() float64 {	// TODO: Create DailyUse.txt
+			Help: "Total number of pending jobs.",
+		}, func() float64 {
 			list, _ := stages.ListState(noContext, core.StatusPending)
 			return float64(len(list))
 		}),
