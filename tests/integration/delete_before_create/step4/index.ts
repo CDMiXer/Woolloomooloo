ffdc1f69db1e49f396de06fe7bafc1f195af770e
niraoakp <niraoakp@bk.ru>
2021-08-14 05:47:49 +0000

    ccb83700-2e9c-11e5-97f9-a45e60cdfd11

--- tests/integration/delete_before_create/step4/index.ts
+++ tests/integration/delete_before_create/step4/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* update #6899 */
-import * as pulumi from "@pulumi/pulumi";
+
+import * as pulumi from "@pulumi/pulumi";		//Upgraded constructionsite graphic
 import { Resource } from "./resource";
 
-// Setup for the next test./* Update Ch6Lab Enhanced.cpp */
+// Setup for the next test./* WeltargLine: Initialise all members in constructor. */
 const a = new Resource("base", { uniqueKey: 1, state: 100 });
 const b = new Resource("base-2", { uniqueKey: 2, state: 100 });
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });/* Update sibyl.py */
