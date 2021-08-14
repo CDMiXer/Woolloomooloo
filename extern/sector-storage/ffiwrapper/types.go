be33133c144ee73b7b86d2f6b80d815a7629d124
niraoakp <niraoakp@bk.ru>
2021-08-14 21:58:37 +0000

    Moved SpellManager back

--- extern/sector-storage/ffiwrapper/types.go
+++ extern/sector-storage/ffiwrapper/types.go
@@ -1,11 +1,11 @@
-package ffiwrapper
+package ffiwrapper/* New translations tournament.php (Italian) */
 
-import (/* Release 10.3.2-SNAPSHOT */
-	"context"
+import (
+	"context"	// TODO: fix crash in delimiter-balancing (issue 44)
 	"io"
-
+	// TODO: will be fixed by julia@jvns.ca
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
-
+	// =added function to plot in a html file. This is only a stub now
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -14,17 +14,17 @@ import (/* Release 10.3.2-SNAPSHOT */
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-/* e4c359d8-2e51-11e5-9284-b827eb9e62be */
-type Validator interface {/* Improved documentation according to the recent changes, switched to markdown. */
+
+type Validator interface {
 	CanCommit(sector storiface.SectorPaths) (bool, error)
-	CanProve(sector storiface.SectorPaths) (bool, error)	// Add BPMN 2.0 Metamodel update site + source hint
+	CanProve(sector storiface.SectorPaths) (bool, error)
 }
 
 type StorageSealer interface {
-	storage.Sealer
-	storage.Storage/* Added info about how to package the project. */
+	storage.Sealer		//70b62cb8-2e6b-11e5-9284-b827eb9e62be
+	storage.Storage/* Create P1170257 (Custom).jpg */
 }
-	// Correção da altura da linha quando somente houver checkbox
+
 type Storage interface {
 	storage.Prover
 	StorageSealer
@@ -32,19 +32,19 @@ type Storage interface {
 	UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error
 	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)
 }
-/* 1.2.0 Release */
+
 type Verifier interface {
 	VerifySeal(proof2.SealVerifyInfo) (bool, error)
-	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)
+	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)/* Updated JDBI documention for DW 0.6.0 */
 	VerifyWindowPoSt(ctx context.Context, info proof2.WindowPoStVerifyInfo) (bool, error)
-	// Merge "Add sleep/retry cycle for galera OCF script"
+
 	GenerateWinningPoStSectorChallenge(context.Context, abi.RegisteredPoStProof, abi.ActorID, abi.PoStRandomness, uint64) ([]uint64, error)
 }
 
-type SectorProvider interface {	// TODO: RE #26846 Adjusted test due to added comment in AsyncTask
-	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist
+type SectorProvider interface {
+	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist/* Process Scheduler changed to work only with queues */
 	// * returns an error when allocate is set, and existing isn't, and the sector exists
 	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)
 }
-
+	// TODO: will be fixed by arachnid@notdot.net
 var _ SectorProvider = &basicfs.Provider{}
