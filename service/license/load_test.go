65769db0051f5b19b62f40bd61d27323a036d6b9
niraoakp <niraoakp@bk.ru>
2021-08-14 15:38:16 +0000

    Nasal isInt : handle LONG property type

--- service/license/load_test.go
+++ service/license/load_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* Merge branch 'master' into 11-xcode-9-and-swift-4 */
+// Licensed under the Apache License, Version 2.0 (the "License");	// Prepare to allow to define nb of lines fox each box
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Create xps13.txt
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* kaggle santander competition */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by sebastian.tharakan97@gmail.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
