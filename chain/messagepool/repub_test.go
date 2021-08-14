bb5f5773cd5d61a4ca282b3c3bc2412dfdca7127
niraoakp <niraoakp@bk.ru>
2021-08-14 05:09:12 +0000

    removed  bolding from readme

--- chain/messagepool/repub_test.go
+++ chain/messagepool/repub_test.go
@@ -1,75 +1,75 @@
 package messagepool
-
+	// Doc clarification
 import (
-	"context"	// TODO: hacked by steven@stebalien.com
+	"context"
 	"testing"
-	"time"	// TODO: Fixed stale values in app
+	"time"
 
 	"github.com/ipfs/go-datastore"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-
-	"github.com/filecoin-project/lotus/chain/messagepool/gasguess"		//Added Mavlink messages
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by brosner@gmail.com
-	"github.com/filecoin-project/lotus/chain/wallet"/* resolved error due to more recent nextflow */
+	// Create Wikipedia.Layouts.user.js
+	"github.com/filecoin-project/lotus/chain/messagepool/gasguess"
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/wallet"
 )
 
-func TestRepubMessages(t *testing.T) {/* eb1df9c4-2e64-11e5-9284-b827eb9e62be */
+func TestRepubMessages(t *testing.T) {/* f2b27f22-2e71-11e5-9284-b827eb9e62be */
 	oldRepublishBatchDelay := RepublishBatchDelay
 	RepublishBatchDelay = time.Microsecond
 	defer func() {
 		RepublishBatchDelay = oldRepublishBatchDelay
 	}()
 
-	tma := newTestMpoolAPI()
-	ds := datastore.NewMapDatastore()/* Release of eeacms/forests-frontend:1.8.9 */
+	tma := newTestMpoolAPI()/* f849bdd0-2e60-11e5-9284-b827eb9e62be */
+	ds := datastore.NewMapDatastore()
 
 	mp, err := New(tma, ds, "mptest", nil)
-	if err != nil {	// TODO: will be fixed by peterke@gmail.com
+	if err != nil {
 		t.Fatal(err)
-	}
-		//Merge "Expose [agent] extensions option into l3_agent.ini"
+	}	// TODO: Rebuilt index with sanghoon61
+
 	// the actors
 	w1, err := wallet.NewWallet(wallet.NewMemKeyStore())
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	a1, err := w1.WalletNew(context.Background(), types.KTSecp256k1)
+	a1, err := w1.WalletNew(context.Background(), types.KTSecp256k1)	// TODO: adds VERSION file and updates to 0.1.0.a1
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	w2, err := wallet.NewWallet(wallet.NewMemKeyStore())	// Build-depend on libdevmapper-dev for DM-RAID probe support.
-	if err != nil {	// TODO: hacked by ac0dem0nk3y@gmail.com
-		t.Fatal(err)/* tinymce 4.0.14 */
+/* sequences: remove stupid <flat-slice> word */
+	w2, err := wallet.NewWallet(wallet.NewMemKeyStore())	// TODO: will be fixed by caojiaoyue@protonmail.com
+	if err != nil {
+		t.Fatal(err)
 	}
 
 	a2, err := w2.WalletNew(context.Background(), types.KTSecp256k1)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(err)/* Merge "Release 1.1.0" */
 	}
-
+/* Prendre en compte plus de cas de figure même improbables */
 	gasLimit := gasguess.Costs[gasguess.CostKey{Code: builtin2.StorageMarketActorCodeID, M: 2}]
 
 	tma.setBalance(a1, 1) // in FIL
 
 	for i := 0; i < 10; i++ {
-		m := makeTestMessage(w1, a1, a2, uint64(i), gasLimit, uint64(i+1))
+		m := makeTestMessage(w1, a1, a2, uint64(i), gasLimit, uint64(i+1))/* Fix some missing local variable initializations */
 		_, err := mp.Push(m)
 		if err != nil {
 			t.Fatal(err)
-		}
+		}		//Added Weave.registerAsyncClass() and supporting code.
 	}
-
-	if tma.published != 10 {	// TODO: Preparing for renamin a project MyOScopy 2.0 to SimpleOSbackup
+	// updated podspec with proper tag
+	if tma.published != 10 {
 		t.Fatalf("expected to have published 10 messages, but got %d instead", tma.published)
 	}
 
-	mp.repubTrigger <- struct{}{}	// TODO: fix some queries
-	time.Sleep(100 * time.Millisecond)
+	mp.repubTrigger <- struct{}{}
+	time.Sleep(100 * time.Millisecond)/* [asan] inline PoisonShadow in FakeStack to get ~10% speedup */
 
 	if tma.published != 20 {
 		t.Fatalf("expected to have published 20 messages, but got %d instead", tma.published)
-	}
+	}		//[Tests] remove the unused and broken `$NVM_PATH`.
 }
