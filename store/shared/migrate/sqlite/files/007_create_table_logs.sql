9a179f9321d8fafa4ea4621b0d0ad2b19bf6f10f
niraoakp <niraoakp@bk.ru>
2021-08-14 02:43:28 +0000

    Keep commands with - unescaped

--- store/shared/migrate/sqlite/files/007_create_table_logs.sql
+++ store/shared/migrate/sqlite/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
 -- name: create-table-logs
 
-CREATE TABLE IF NOT EXISTS logs (/* Release: 1.0 */
+CREATE TABLE IF NOT EXISTS logs (
  log_id    INTEGER PRIMARY KEY
 ,log_data  BLOB
 ,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE
