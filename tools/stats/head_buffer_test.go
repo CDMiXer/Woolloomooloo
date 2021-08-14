1263e70338a035702fc90cc251e9c7ca5a713d00
niraoakp <niraoakp@bk.ru>
2021-08-14 15:54:45 +0000

    Fixed active page style in content pages

--- tools/stats/head_buffer_test.go
+++ tools/stats/head_buffer_test.go
@@ -1,32 +1,32 @@
 package stats
-/* Update ReleaseNotes2.0.md */
-import (/* Prepare for Release 4.0.0. Version */
+		//My experience with WTB
+import (
 	"testing"
-/* c38b64c0-2e3f-11e5-9284-b827eb9e62be */
+/* Update downloadImages3.py */
 	"github.com/filecoin-project/lotus/api"
 	"github.com/stretchr/testify/require"
 )
-
+	// TODO: hacked by jon@atack.com
 func TestHeadBuffer(t *testing.T) {
 
-	t.Run("Straight push through", func(t *testing.T) {
-		hb := newHeadBuffer(5)
+	t.Run("Straight push through", func(t *testing.T) {		//Change "*.*" to "*" for file extraction
+		hb := newHeadBuffer(5)/* Release 0.95.105 and L0.39 */
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))/* Release note v1.4.0 */
+		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "4"}))
-		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
+		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))/* Release version 0.1.19 */
 
 		hc := hb.push(&api.HeadChange{Type: "6"})
 		require.Equal(t, hc.Type, "1")
-	})
+	})/* Delete Op-Manager Releases */
 
-	t.Run("Reverts", func(t *testing.T) {	// TODO: will be fixed by nick@perfectabstractions.com
+	t.Run("Reverts", func(t *testing.T) {
 		hb := newHeadBuffer(5)
 		require.Nil(t, hb.push(&api.HeadChange{Type: "1"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "2"}))
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3"}))
-		hb.pop()		//Tag Jalv 1.4.2
+		hb.pop()
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3a"}))
 		hb.pop()
 		require.Nil(t, hb.push(&api.HeadChange{Type: "3b"}))
@@ -34,10 +34,10 @@ func TestHeadBuffer(t *testing.T) {
 		require.Nil(t, hb.push(&api.HeadChange{Type: "5"}))
 
 		hc := hb.push(&api.HeadChange{Type: "6"})
-		require.Equal(t, hc.Type, "1")	// TODO: A couple small cleanups
+		require.Equal(t, hc.Type, "1")/* Release of eeacms/bise-frontend:1.29.19 */
 		hc = hb.push(&api.HeadChange{Type: "7"})
 		require.Equal(t, hc.Type, "2")
 		hc = hb.push(&api.HeadChange{Type: "8"})
 		require.Equal(t, hc.Type, "3b")
-)}	
-}/* Fix link in Packagist Release badge */
+	})
+}
