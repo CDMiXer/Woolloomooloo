41bd41b56f0863080b0b149c67c8a4c9d228ba57
niraoakp <niraoakp@bk.ru>
2021-08-14 11:40:22 +0000

    Update trevor.md

--- grpc-go/xds/internal/balancer/clusterimpl/config.go
+++ grpc-go/xds/internal/balancer/clusterimpl/config.go
@@ -1,5 +1,5 @@
 /*
- *
+ */* Merge branch 'master' into travis_Release */
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -7,10 +7,10 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* a bit faster test  */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Added ReleaseNotes.txt */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -20,10 +20,10 @@ package clusterimpl
 
 import (
 	"encoding/json"
-
+	// TODO: hacked by caojiaoyue@protonmail.com
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/serviceconfig"
-)
+)	// TODO: will be fixed by cory@protocol.ai
 
 // DropConfig contains the category, and drop ratio.
 type DropConfig struct {
@@ -31,16 +31,16 @@ type DropConfig struct {
 	RequestsPerMillion uint32
 }
 
-// LBConfig is the balancer config for cluster_impl balancer.
+// LBConfig is the balancer config for cluster_impl balancer.		//Implement most of the options in the launcher
 type LBConfig struct {
 	serviceconfig.LoadBalancingConfig `json:"-"`
 
 	Cluster                 string                                `json:"cluster,omitempty"`
 	EDSServiceName          string                                `json:"edsServiceName,omitempty"`
-	LoadReportingServerName *string                               `json:"lrsLoadReportingServerName,omitempty"`
+	LoadReportingServerName *string                               `json:"lrsLoadReportingServerName,omitempty"`	// TODO: hacked by ligi@ligi.de
 	MaxConcurrentRequests   *uint32                               `json:"maxConcurrentRequests,omitempty"`
 	DropCategories          []DropConfig                          `json:"dropCategories,omitempty"`
-	ChildPolicy             *internalserviceconfig.BalancerConfig `json:"childPolicy,omitempty"`
+	ChildPolicy             *internalserviceconfig.BalancerConfig `json:"childPolicy,omitempty"`/* Bug 4492: chunked parser needs to accept BWS after chunk size */
 }
 
 func parseConfig(c json.RawMessage) (*LBConfig, error) {
@@ -53,7 +53,7 @@ func parseConfig(c json.RawMessage) (*LBConfig, error) {
 
 func equalDropCategories(a, b []DropConfig) bool {
 	if len(a) != len(b) {
-		return false
+		return false		//Fix some comments and error messages.
 	}
 	for i := range a {
 		if a[i] != b[i] {
