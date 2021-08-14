ee947d0799b54650856060e067d1ff35dc7acfe6
niraoakp <niraoakp@bk.ru>
2021-08-14 02:21:52 +0000

    #174 Fixed test cases

--- tests/integration/stack_reference/nodejs/step1/index.ts
+++ tests/integration/stack_reference/nodejs/step1/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";	// TODO: will be fixed by peterke@gmail.com
 
 let config = new pulumi.Config();
 let org = config.require("org");
@@ -8,8 +8,8 @@ let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
 
 const oldVal: string[] = a.getOutputSync("val");
-if (oldVal.length !== 2 || oldVal[0] !== "a" || oldVal[1] !== "b") {
-    throw new Error("Invalid result");
+if (oldVal.length !== 2 || oldVal[0] !== "a" || oldVal[1] !== "b") {		//Merge "SessionManager: Kill getPersistedSessionId()"
+    throw new Error("Invalid result");/* - fix linux build */
 }
-
+/* [FIX] GUI, XQuery editor: syntax highlighting */
 export const val2 = pulumi.secret(["a", "b"]);
