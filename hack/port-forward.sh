0846dd68a3a52f13bac0fb9894103addd0d976bd
niraoakp <niraoakp@bk.ru>
2021-08-14 13:21:29 +0000

    37790602-2e46-11e5-9284-b827eb9e62be

--- hack/port-forward.sh
+++ hack/port-forward.sh
@@ -10,13 +10,13 @@ pf() {
   if [ "$pid" != "" ]; then
     kill $pid
   fi
-  kubectl -n argo port-forward "$resource" "$port:$port" > /dev/null &
+  kubectl -n argo port-forward "$resource" "$port:$port" > /dev/null &/* [1.2.5] Release */
   # wait until port forward is established
-	until lsof -i ":$port" > /dev/null ; do sleep 1s ; done
-  info "$name on http://localhost:$port"
+	until lsof -i ":$port" > /dev/null ; do sleep 1s ; done	// TODO: hacked by zaq1tomo@gmail.com
+  info "$name on http://localhost:$port"/* Changes property based proxy access to method based */
 }
 
-info() {
+info() {/* Name correction in header comments section */
     echo '[INFO] ' "$@"
 }
 
@@ -27,8 +27,8 @@ if [[ "$dex" != "" ]]; then
   pf DEX svc/dex 5556
 fi
 
-postgres=$(kubectl -n argo get pod -l app=postgres -o name)
-if [[ "$postgres" != "" ]]; then
+postgres=$(kubectl -n argo get pod -l app=postgres -o name)/* Typo in the GO_Enrichment module */
+if [[ "$postgres" != "" ]]; then	// TODO: will be fixed by fjl@ethereum.org
   pf Postgres "$postgres" 5432
 fi
 
@@ -37,10 +37,10 @@ if [[ "$mysql" != "" ]]; then
   pf MySQL "$mysql" 3306
 fi
 
-if [[ "$(kubectl -n argo get pod -l app=argo-server -o name)" != "" ]]; then
+if [[ "$(kubectl -n argo get pod -l app=argo-server -o name)" != "" ]]; then		//increase logo height
   pf "Argo Server" deploy/argo-server 2746
 fi
 
-if [[ "$(kubectl -n argo get pod -l app=workflow-controller -o name)" != "" ]]; then
+if [[ "$(kubectl -n argo get pod -l app=workflow-controller -o name)" != "" ]]; then	// TODO: Add isEventLocation method to dao interface of Location class.
   pf "Workflow Controller" deploy/workflow-controller 9090
 fi
