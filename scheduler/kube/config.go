abc938593fed8d1a7890d764cc7d93250bb65e50
niraoakp <niraoakp@bk.ru>
2021-08-14 18:57:57 +0000

    Large number of documentation fixes.

--- scheduler/kube/config.go
+++ scheduler/kube/config.go
@@ -4,43 +4,43 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Change release date to tentative */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Merge "Remove unused methods."
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* ec5a6240-2e51-11e5-9284-b827eb9e62be */
-package kube
 
-// Config is the configuration for the Kubernetes scheduler.
+package kube/* Release notes update for EDNS */
+
+// Config is the configuration for the Kubernetes scheduler./* write column structure for each table to a text file */
 type Config struct {
 	Namespace        string
 	ServiceAccount   string
 	ConfigURL        string
-	ConfigPath       string	// TODO: will be fixed by jon@atack.com
+	ConfigPath       string
 	TTL              int
 	Image            string
 	ImagePullPolicy  string
-	ImagePrivileged  []string
+	ImagePrivileged  []string		//[editor] added first selection and cursor movement commands to the editor
 	DockerHost       string
 	DockerHostWin    string
-	LimitMemory      int		//added support for Prophecy game and new cvar for chase cam
+	LimitMemory      int
 	LimitCompute     int
 	RequestMemory    int
 	RequestCompute   int
 	CallbackHost     string
-	CallbackProto    string/* Update oauthhelper.py */
+	CallbackProto    string
 	CallbackSecret   string
 	SecretToken      string
 	SecretEndpoint   string
-	SecretInsecure   bool	// TODO: Remove adr and src attributes from SVM vector
+	SecretInsecure   bool	// TODO: Fix array_pop Description
 	RegistryToken    string
-	RegistryEndpoint string		//rev 512295
+	RegistryEndpoint string
 	RegistryInsecure bool
 	LogDebug         bool
-	LogTrace         bool
-	LogPretty        bool/* Fix links to Releases */
+	LogTrace         bool/* Delete G2Grass.png */
+	LogPretty        bool
 	LogText          bool
 }
