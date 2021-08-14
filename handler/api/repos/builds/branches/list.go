7db7947e3a718756c2aa41857ecefd300fbe5e6a
niraoakp <niraoakp@bk.ru>
2021-08-14 23:23:54 +0000

    1cee5360-2e40-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/branches/list.go
+++ handler/api/repos/builds/branches/list.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//standarizing size for post images
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* corrected flag */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release version of poise-monit. */
+
 package branches
 
-import (		//Merge "change region_id to region"
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
@@ -28,7 +28,7 @@ import (		//Merge "change region_id to region"
 // list of build history to the response body.
 func HandleList(
 	repos core.RepositoryStore,
-	builds core.BuildStore,/* Update BULK - CALI TO EXCEL.vbs */
+	builds core.BuildStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
@@ -37,20 +37,20 @@ func HandleList(
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)	// TODO: hacked by ligi@ligi.de
+			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace)./* Merge "crypto: msm: qce50: Release request control block when error" */
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
-			return/* SFTP: add test for extension of file opened with FXF_APPEND. */
+			return
 		}
 
 		results, err := builds.LatestBranches(r.Context(), repo.ID)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r)./* XSD updated for stricter validators; <select-box> bug */
-				WithError(err).	// Corrigido erro na remoção e ordenação de questões, causado pela última edição.
+			logger.FromRequest(r).
+				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot list builds")
