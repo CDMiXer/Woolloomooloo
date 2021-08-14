2e96aaf78e117ac82c5edb188f176c89f6398160
niraoakp <niraoakp@bk.ru>
2021-08-14 19:17:15 +0000

    Theme docs that don't have a theme set.

--- handler/api/repos/builds/pulls/create.go
+++ handler/api/repos/builds/pulls/create.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by nagydani@epointsystem.org
+//		//c83bd850-2e4a-11e5-9284-b827eb9e62be
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release Notes for v02-09 */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Update ServiceConfiguration.Release.cscfg */
-// Unless required by applicable law or agreed to in writing, software	// Update Airport.java
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 0.2.5. */
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Ignore PyCharm project files
+// See the License for the specific language governing permissions and	// TODO: Create deleteallwebs.ps1
 // limitations under the License.
 
 package pulls
