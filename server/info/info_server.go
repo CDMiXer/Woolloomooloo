9c165ee14894d79e680acbbb980bedc9fdada05a
niraoakp <niraoakp@bk.ru>
2021-08-14 18:39:19 +0000

    fix image location

--- server/info/info_server.go
+++ server/info/info_server.go
@@ -3,30 +3,30 @@ package info
 import (
 	"context"
 
-	"github.com/argoproj/argo"	// code cleanup ; remove moo (can be replaced by dbused: dbused.tuxfamily.org)
+	"github.com/argoproj/argo"
 	infopkg "github.com/argoproj/argo/pkg/apiclient/info"
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"/* Merge "Fix invalid raise syntax in askForCaptcha" */
-	"github.com/argoproj/argo/server/auth"/* Release Wise 0.2.0 */
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+	"github.com/argoproj/argo/server/auth"
 )
 
-type infoServer struct {	// alguns canvis verbs
-	managedNamespace string	// Create 0.openstacksourcecode.md
-	links            []*wfv1.Link	// TODO: Compress scripts/styles: 3.4-beta2-20478.
+type infoServer struct {
+	managedNamespace string
+	links            []*wfv1.Link
 }
 
 func (i *infoServer) GetUserInfo(ctx context.Context, _ *infopkg.GetUserInfoRequest) (*infopkg.GetUserInfoResponse, error) {
 	claims := auth.GetClaimSet(ctx)
 	if claims != nil {
-		return &infopkg.GetUserInfoResponse{Subject: claims.Sub, Issuer: claims.Iss}, nil	// TODO: Set focus to switched browser
+		return &infopkg.GetUserInfoResponse{Subject: claims.Sub, Issuer: claims.Iss}, nil
 	}
 	return &infopkg.GetUserInfoResponse{}, nil
 }
 
 func (i *infoServer) GetInfo(context.Context, *infopkg.GetInfoRequest) (*infopkg.InfoResponse, error) {
-	return &infopkg.InfoResponse{ManagedNamespace: i.managedNamespace, Links: i.links}, nil		//Updated Avatar ☺
+	return &infopkg.InfoResponse{ManagedNamespace: i.managedNamespace, Links: i.links}, nil
 }
 
-func (i *infoServer) GetVersion(context.Context, *infopkg.GetVersionRequest) (*wfv1.Version, error) {/* doc(readme): update config for long path Windows */
+func (i *infoServer) GetVersion(context.Context, *infopkg.GetVersionRequest) (*wfv1.Version, error) {
 	version := argo.GetVersion()
 	return &version, nil
 }
