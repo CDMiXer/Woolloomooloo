d07c61a80d2e150a9f749fdad4ba943440e0959d
niraoakp <niraoakp@bk.ru>
2021-08-14 18:31:53 +0000

    Fixing delta mode issues - 2

--- plugin/secret/external_oss.go
+++ plugin/secret/external_oss.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by hugomrdias@gmail.com
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,7 +15,7 @@
 // +build oss
 
 package secret
-	// 19424384-2e59-11e5-9284-b827eb9e62be
+
 import (
 	"context"
 
@@ -25,7 +25,7 @@ import (
 // External returns a no-op registry secret provider.
 func External(string, string, bool) core.SecretService {
 	return new(noop)
-}/* a665b204-2e4b-11e5-9284-b827eb9e62be */
+}
 
 type noop struct{}
 
