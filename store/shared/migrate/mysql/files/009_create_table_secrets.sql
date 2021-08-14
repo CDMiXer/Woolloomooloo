6624985b8dfbd070bcdf6a7544d1dbc8db6c669c
niraoakp <niraoakp@bk.ru>
2021-08-14 17:57:29 +0000

    Locates "_remote.repositories" to glean insight into origin of artifact

--- store/shared/migrate/mysql/files/009_create_table_secrets.sql
+++ store/shared/migrate/mysql/files/009_create_table_secrets.sql
@@ -2,19 +2,19 @@
 
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
-,secret_repo_id           INTEGER/* Refresh page on new day. Also updated layout. */
+,secret_repo_id           INTEGER
 ,secret_name              VARCHAR(500)
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN
+,secret_pull_request_push BOOLEAN/* Release: Making ready for next release cycle 5.0.1 */
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-		//[IMP][stock]: Cuentas contables por division
+
 -- name: create-index-secrets-repo
 
-CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);/* Release 1.0.0 */
-/* Object: Add some tests! */
+CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);
+
 -- name: create-index-secrets-repo-name
-	// TODO: Create vimbadll.py
+
 CREATE INDEX ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
