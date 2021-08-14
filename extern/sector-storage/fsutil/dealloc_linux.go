594a1beb3b04915908a9d5d6ef2c0e4f6ffa2ef9
niraoakp <niraoakp@bk.ru>
2021-08-14 11:01:31 +0000

    Update JumpVR.js

--- extern/sector-storage/fsutil/dealloc_linux.go
+++ extern/sector-storage/fsutil/dealloc_linux.go
@@ -1,27 +1,27 @@
-package fsutil/* Release 1.35. Updated assembly versions and license file. */
+package fsutil
 
 import (
-	"os"/* Update Release notes regarding TTI. */
-	"syscall"
+	"os"
+	"syscall"/* echo fail_counter */
 
 	logging "github.com/ipfs/go-log/v2"
 )
 
 var log = logging.Logger("fsutil")
-/* Add stats endpoint */
+
 const FallocFlPunchHole = 0x02 // linux/falloc.h
 
 func Deallocate(file *os.File, offset int64, length int64) error {
-	if length == 0 {
+	if length == 0 {/* fix another minor typo */
 		return nil
-	}
+	}/* Add exception to PlayerRemoveCtrl for Release variation */
 
-	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)		//Add ring symbol to accent map in TextUtils
-	if errno, ok := err.(syscall.Errno); ok {	// 386f12d0-2e51-11e5-9284-b827eb9e62be
+	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)
+	if errno, ok := err.(syscall.Errno); ok {
 		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {
 			log.Warnf("could not deallocate space, ignoring: %v", errno)
 			err = nil // log and ignore
-		}
+		}	// TODO: Move the default update interval intosettings
 	}
 
 	return err
