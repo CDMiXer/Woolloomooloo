f42711ec1f278042050953c33247e6bc4686a1cb
niraoakp <niraoakp@bk.ru>
2021-08-14 10:34:59 +0000

    Created the tag for the 0.3.2 distribution.

--- handler/api/repos/builds/deploys/create_test.go
+++ handler/api/repos/builds/deploys/create_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Released MotionBundler v0.1.6 */
-// Use of this source code is governed by the Drone Non-Commercial License/* Release 2.1.0: All Liquibase settings are available via configuration */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package deploys
