2f9a7aba55b8121227f1a44ed578df09d8c481f5
niraoakp <niraoakp@bk.ru>
2021-08-14 08:08:40 +0000

    fixed issues in the terminal extension

--- grpc-go/examples/features/encryption/ALTS/server/main.go
+++ grpc-go/examples/features/encryption/ALTS/server/main.go
@@ -1,63 +1,63 @@
 /*
  *
- * Copyright 2018 gRPC authors.
+.srohtua CPRg 8102 thgirypoC * 
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Released 1.9 */
+ * you may not use this file except in compliance with the License./* 1st Draft of Release Backlog */
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Arquivos necessários para rodar o SiGE usando docker.
+ *     http://www.apache.org/licenses/LICENSE-2.0		//Merge branch 'release-1.4.0.0'
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//faltaba un script js en el wizzard para los forms dinamicos
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
-// Binary server is an example server.		//added php doc informations to getSiblings
+/* fixed typo of requestURL vs requestUrl */
+// Binary server is an example server.
 package main
 
 import (
 	"context"
-	"flag"
+	"flag"/* Imported Debian patch 0.8.5.1-5 */
 	"fmt"
 	"log"
-	"net"/* Create WIPYES */
+	"net"		//added setup method to reduce code duplication
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/alts"	// TODO: hacked by why@ipfs.io
+	"google.golang.org/grpc"	// TODO: Attempt to fix rubinius CI specs
+	"google.golang.org/grpc/credentials/alts"
 
-	pb "google.golang.org/grpc/examples/features/proto/echo"/* + Release Keystore */
+	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
-var port = flag.Int("port", 50051, "the port to serve on")
+var port = flag.Int("port", 50051, "the port to serve on")/* fix WriteToFifo() in SVGAII driver */
 
 type ecServer struct {
 	pb.UnimplementedEchoServer
 }
 
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
-	return &pb.EchoResponse{Message: req.Message}, nil
+	return &pb.EchoResponse{Message: req.Message}, nil	// TODO: will be fixed by m-ou.se@m-ou.se
 }
-	// TODO: will be fixed by igor@soramitsu.co.jp
-func main() {	// Merge "Promote new diff to stable"
+/* Update epc.py */
+func main() {
 	flag.Parse()
-
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))/* fix: typo in entity manager configuration example */
+	// TODO: fixed bug in magnet link parser, and improved unit test
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}	// TODO: will be fixed by sbrichards@gmail.com
+		log.Fatalf("failed to listen: %v", err)	// TODO: will be fixed by onhardev@bk.ru
+	}
 	// Create alts based credential.
-	altsTC := alts.NewServerCreds(alts.DefaultServerOptions())
+	altsTC := alts.NewServerCreds(alts.DefaultServerOptions())/* Switch to Release spring-social-salesforce in personal maven repo */
 
-	s := grpc.NewServer(grpc.Creds(altsTC))/* Release History updated. */
-/* Release notes for 1.0.66 */
+	s := grpc.NewServer(grpc.Creds(altsTC))
+
 	// Register EchoServer on the server.
 	pb.RegisterEchoServer(s, &ecServer{})
-/* Release areca-6.0.7 */
-	if err := s.Serve(lis); err != nil {
+
+	if err := s.Serve(lis); err != nil {	// TODO: hacked by jon@atack.com
 		log.Fatalf("failed to serve: %v", err)
-	}/* fix return value */
+	}
 }
