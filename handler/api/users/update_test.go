fe9e3a7437b40d86b770eefec24af40b02ad67ff
niraoakp <niraoakp@bk.ru>
2021-08-14 21:47:01 +0000

    Attempted to rewrite PluginManager code to use functional IO

--- handler/api/users/update_test.go
+++ handler/api/users/update_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License/* Travis will stop dealing with this repository. */
+// that can be found in the LICENSE file.	// Speedup Matrices
 
 package users
 
@@ -11,17 +11,17 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"
-
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"
+	"testing"		//Handle dynamic domain values that are not references in ViewGenerator.
+/* node __call__ -> __truediv__ */
+	"github.com/drone/drone/core"		//Initial commit on ideas documentation file
+	"github.com/drone/drone/handler/api/errors"/* Release of eeacms/plonesaas:5.2.1-33 */
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-
+/* fixed up puzzle 2 */
 func TestUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -30,7 +30,7 @@ func TestUpdate(t *testing.T) {
 	userInput := &userInput{
 		Admin: &admin,
 	}
-	user := &core.User{
+	user := &core.User{	// more python3 fixes
 		Login: "octocat",
 		Admin: false,
 	}
@@ -38,11 +38,11 @@ func TestUpdate(t *testing.T) {
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().FindLogin(gomock.Any(), user.Login).Return(user, nil)
 	users.EXPECT().Update(gomock.Any(), user)
-
+	// TODO: Fixes gwiad unregister script call
 	transferer := mock.NewMockTransferer(controller)
 	transferer.EXPECT().Transfer(gomock.Any(), user).Return(nil)
-
-	c := new(chi.Context)
+		//Adapt elastic schema generator to new API. 
+	c := new(chi.Context)		//Popravil č-je
 	c.URLParams.Add("user", "octocat")
 
 	in := new(bytes.Buffer)
@@ -50,17 +50,17 @@ func TestUpdate(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("PATCH", "/", in)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
-
-	HandleUpdate(users, transferer)(w, r)
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),		//46519fc8-4b19-11e5-9148-6c40088e03e4
+	)/* Release 0.1.3. */
+	// a645633a-2e70-11e5-9284-b827eb9e62be
+	HandleUpdate(users, transferer)(w, r)	// Create DIVERSITY_AND_INCLUSION.md
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	if got, want := user.Admin, true; got != want {
 		t.Errorf("Want user admin %v, got %v", want, got)
-	}
+	}	// TODO: This makes things much more clear
 
 	got, want := new(core.User), user
 	json.NewDecoder(w.Body).Decode(got)
