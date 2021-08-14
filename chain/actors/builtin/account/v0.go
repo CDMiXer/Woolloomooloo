ddcbab44f7c2ce341a600b02356a9b5372c2c3a7
niraoakp <niraoakp@bk.ru>
2021-08-14 03:59:04 +0000

    Merge branch 'master' into greenkeeper/rollup-0.62.0

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -4,27 +4,27 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"
-
-	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"
+	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: 3a8511d2-2e5b-11e5-9284-b827eb9e62be
+/* (jam) Release bzr 2.2(.0) */
+	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"/* Release v1.0.1b */
 )
 
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
-	out := state0{store: store}	// TODO: hacked by brosner@gmail.com
+	out := state0{store: store}	// Set version to 3.6.3
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+	if err != nil {/* 0.9.3 Release. */
 		return nil, err
 	}
-	return &out, nil
-}
-
+	return &out, nil/* 5.1.2 Release */
+}/* Release DBFlute-1.1.0-sp1 */
+	// TODO: * Remove unused file.
 type state0 struct {
 	account0.State
 	store adt.Store
-}
-		//Fix regexp to detect functions in column definition
-func (s *state0) PubkeyAddress() (address.Address, error) {/* Correct error on lmrMaterial structure for s_white */
+}		//Released springrestclient version 2.5.8
+
+func (s *state0) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
-}
+}/* Release 0.13.2 */
