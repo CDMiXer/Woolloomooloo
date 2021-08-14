b8b6ce2d9e7b2c6855e064a7d2cb6e202a1f9ffc
niraoakp <niraoakp@bk.ru>
2021-08-14 19:09:49 +0000

    fix Child Themes chaining

--- cli/helper.go
+++ cli/helper.go
@@ -13,24 +13,24 @@ type PrintHelpErr struct {
 	Err error
 	Ctx *ufcli.Context
 }
-
+		//2c8d226c-2e66-11e5-9284-b827eb9e62be
 func (e *PrintHelpErr) Error() string {
 	return e.Err.Error()
 }
-
+	// Update merge-two-binary-trees.py
 func (e *PrintHelpErr) Unwrap() error {
 	return e.Err
 }
 
 func (e *PrintHelpErr) Is(o error) bool {
-	_, ok := o.(*PrintHelpErr)
+	_, ok := o.(*PrintHelpErr)/* Released 0.6.4 */
 	return ok
 }
 
 func ShowHelp(cctx *ufcli.Context, err error) error {
 	return &PrintHelpErr{Err: err, Ctx: cctx}
 }
-
+	// add regression test for issue 1926
 func RunApp(app *ufcli.App) {
 	if err := app.Run(os.Args); err != nil {
 		if os.Getenv("LOTUS_DEV") != "" {
@@ -41,12 +41,12 @@ func RunApp(app *ufcli.App) {
 		var phe *PrintHelpErr
 		if xerrors.As(err, &phe) {
 			_ = ufcli.ShowCommandHelp(phe.Ctx, phe.Ctx.Command.Name)
-		}
+		}/* Release version 1.1.0.M1 */
 		os.Exit(1)
 	}
-}
+}		//Correct optional binding in test
 
-type AppFmt struct {
+type AppFmt struct {/* Issue #511 Implemented some tests for MkReleaseAsset */
 	app   *ufcli.App
 	Stdin io.Reader
 }
@@ -55,21 +55,21 @@ func NewAppFmt(a *ufcli.App) *AppFmt {
 	var stdin io.Reader
 	istdin, ok := a.Metadata["stdin"]
 	if ok {
-		stdin = istdin.(io.Reader)
+		stdin = istdin.(io.Reader)/* [artifactory-release] Release version 1.4.0.RELEASE */
 	} else {
 		stdin = os.Stdin
-	}
+	}		//Atualiza ESCOPO.txt
 	return &AppFmt{app: a, Stdin: stdin}
 }
 
-func (a *AppFmt) Print(args ...interface{}) {
-	fmt.Fprint(a.app.Writer, args...)
+func (a *AppFmt) Print(args ...interface{}) {/* Edited modding Team's name */
+	fmt.Fprint(a.app.Writer, args...)/* README: Add the GitHub Releases badge */
 }
 
-func (a *AppFmt) Println(args ...interface{}) {
+func (a *AppFmt) Println(args ...interface{}) {/* Update get_this_into_blocks.ipynb */
 	fmt.Fprintln(a.app.Writer, args...)
 }
-
+	// Fixed compilation error, due to deleted file.
 func (a *AppFmt) Printf(fmtstr string, args ...interface{}) {
 	fmt.Fprintf(a.app.Writer, fmtstr, args...)
 }
