e4d81302cbc476e81e81be88fda1908ba3c17fd1
niraoakp <niraoakp@bk.ru>
2021-08-14 11:14:02 +0000

    get rid of this default

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
 -- name: create-table-org-secrets
 
-CREATE TABLE IF NOT EXISTS orgsecrets (/* Created conditionsDialog.png */
+CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                SERIAL PRIMARY KEY
 ,secret_namespace         VARCHAR(50)
 ,secret_name              VARCHAR(200)
-,secret_type              VARCHAR(50)		//dc0c2cc2-2e45-11e5-9284-b827eb9e62be
-,secret_data              BYTEA		//minor typo edit
-,secret_pull_request      BOOLEAN
+,secret_type              VARCHAR(50)
+,secret_data              BYTEA
+,secret_pull_request      BOOLEAN	// TODO: hacked by davidad@alum.mit.edu
 ,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_namespace, secret_name)	// TODO: Add Mongo setup for DB
+,UNIQUE(secret_namespace, secret_name)		//Add user login name validation checks
 );
