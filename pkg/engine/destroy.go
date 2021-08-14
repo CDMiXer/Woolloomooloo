e8513d76544f50e8a57a1ce87edd8fc7d01af616
niraoakp <niraoakp@bk.ru>
2021-08-14 02:00:04 +0000

    Fix compile error with MIR_INPUT_USE_ANDROID_TYPES on

--- pkg/engine/destroy.go
+++ pkg/engine/destroy.go
@@ -11,15 +11,15 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Added LinkedList.py
-package engine/* Merge "[FIX] Allow deprecation of trailing parameters" */
+
+package engine
 
 import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"/* Release for v6.6.0. */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"/* Create compare_two_csv_files.py */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
@@ -29,8 +29,8 @@ func Destroy(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (Resou
 
 	defer func() { ctx.Events <- cancelEvent() }()
 
-	info, err := newDeploymentContext(u, "destroy", ctx.ParentSpan)/* Vim: basic support for adding/substracting CTRL-{A,X} */
-	if err != nil {		//Bugfixes and design update
+	info, err := newDeploymentContext(u, "destroy", ctx.ParentSpan)
+	if err != nil {
 		return nil, result.FromError(err)
 	}
 	defer info.Close()
@@ -38,12 +38,12 @@ func Destroy(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (Resou
 	emitter, err := makeEventEmitter(ctx.Events, u)
 	if err != nil {
 		return nil, result.FromError(err)
-	}	// EventRouteExecutor: URI to Device
+	}
 	defer emitter.Close()
 
 	return update(ctx, info, deploymentOptions{
 		UpdateOptions: opts,
-		SourceFunc:    newDestroySource,	// TODO: will be fixed by sjors@sprovoost.nl
+		SourceFunc:    newDestroySource,
 		Events:        emitter,
 		Diag:          newEventSink(emitter, false),
 		StatusDiag:    newEventSink(emitter, true),
@@ -52,26 +52,26 @@ func Destroy(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (Resou
 
 func newDestroySource(
 	client deploy.BackendClient, opts deploymentOptions, proj *workspace.Project, pwd, main string,
-	target *deploy.Target, plugctx *plugin.Context, dryRun bool) (deploy.Source, error) {	// TODO: hacked by alessio@tendermint.com
+	target *deploy.Target, plugctx *plugin.Context, dryRun bool) (deploy.Source, error) {
 
 	// Like Update, we need to gather the set of plugins necessary to delete everything in the snapshot.
 	// Unlike Update, we don't actually run the user's program so we only need the set of plugins described
 	// in the snapshot.
 	plugins, err := gatherPluginsFromSnapshot(plugctx, target)
-	if err != nil {/* Release version 1.10 */
+	if err != nil {
 		return nil, err
-	}	// clean up and kill some warnings
+	}
 
 	// Like Update, if we're missing plugins, attempt to download the missing plugins.
 	if err := ensurePluginsAreInstalled(plugins); err != nil {
-		logging.V(7).Infof("newDestroySource(): failed to install missing plugins: %v", err)		//Adjust error example
+		logging.V(7).Infof("newDestroySource(): failed to install missing plugins: %v", err)
 	}
 
 	// We don't need the language plugin, since destroy doesn't run code, so we will leave that out.
-	if err := ensurePluginsAreLoaded(plugctx, plugins, plugin.AnalyzerPlugins); err != nil {/* Add to README.md */
+	if err := ensurePluginsAreLoaded(plugctx, plugins, plugin.AnalyzerPlugins); err != nil {
 		return nil, err
 	}
-	// Merge "Accomoditing API Review feedback for WifiScanner"
+
 	// Create a nil source.  This simply returns "nothing" as the new state, which will cause the
 	// engine to destroy the entire existing state.
 	return deploy.NullSource, nil
