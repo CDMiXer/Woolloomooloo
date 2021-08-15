7e23f24df2bbe8a55629d44c4d4f6fb958864baf
niraoakp <niraoakp@bk.ru>
2021-08-15 08:45:13 +0000

    Class and packages renamed

--- tests/examples/minimal/index.ts
+++ tests/examples/minimal/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-;"imulup/imulup@" morf } gifnoC { tropmi
+import { Config } from "@pulumi/pulumi";
 
-let config = new Config();	// TODO: Create php-fpm.conf.patch
-console.log(`Hello, ${config.require("name")}!`);	// TODO: will be fixed by zaq1tomo@gmail.com
+let config = new Config();
+console.log(`Hello, ${config.require("name")}!`);
 console.log(`Psst, ${config.require("secret")}`);
 
