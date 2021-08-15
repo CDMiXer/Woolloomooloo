e30d8410a725f0533a72972f145f254cef98738e
niraoakp <niraoakp@bk.ru>
2021-08-15 00:44:24 +0000

    Merge branch 'master' into gl_calculator

--- handler/api/errors/errors.go
+++ handler/api/errors/errors.go
@@ -1,43 +1,43 @@
-// Copyright 2019 Drone IO, Inc.	// try running node from out from "which node"
+// Copyright 2019 Drone IO, Inc.		//Merge "Update sample to use playback control glue." into lmp-mr1-dev
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* rev 504355 */
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//		//Is this it?
-//      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by sjors@sprovoost.nl
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.	// TODO: Accordion-nav: re-aligned the Investigation header to the actvity boxes
+// You may obtain a copy of the License at/* abstract out default target config responses in Releaser spec */
+///* Released 0.5.0 */
+//      http://www.apache.org/licenses/LICENSE-2.0	// mcs2: query sensors at startup async
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package errors
+package errors/* v1.2.4 Resetting sponsored on content */
 
 var (
 	// ErrInvalidToken is returned when the api request token is invalid.
-	ErrInvalidToken = New("Invalid or missing token")/* 8f30fb76-2e40-11e5-9284-b827eb9e62be */
+	ErrInvalidToken = New("Invalid or missing token")
 
-	// ErrUnauthorized is returned when the user is not authorized.
-	ErrUnauthorized = New("Unauthorized")/* Official 0.1 Version Release */
+	// ErrUnauthorized is returned when the user is not authorized./* Update and rename BurdaevaE to BurdaevaE/python/list1.py */
+	ErrUnauthorized = New("Unauthorized")
 
 	// ErrForbidden is returned when user access is forbidden.
 	ErrForbidden = New("Forbidden")
 
 	// ErrNotFound is returned when a resource is not found.
-	ErrNotFound = New("Not Found")	// TODO: 82d9dd0c-2e4f-11e5-9963-28cfe91dbc4b
+	ErrNotFound = New("Not Found")
 )
-		//Converted default starting conditions.
+	// TODO: will be fixed by steven@stebalien.com
 // Error represents a json-encoded API error.
 type Error struct {
-	Message string `json:"message"`	// TODO: will be fixed by alan.shaw@protocol.ai
+	Message string `json:"message"`
 }
 
 func (e *Error) Error() string {
-	return e.Message
+	return e.Message	// TODO: Update dbschema_json.md
 }
 
 // New returns a new error message.
 func New(text string) error {
 	return &Error{Message: text}
-}/* Merge "Release camera between rotation tests" into androidx-master-dev */
+}	// netcam_keepalive option is now automatically detected from http version
