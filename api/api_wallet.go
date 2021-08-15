764e4bf9751b584c9a107f30163d64534c1832ec
niraoakp <niraoakp@bk.ru>
2021-08-15 22:21:24 +0000

    Merged branch admin-tests-base into admin-tests-base

--- api/api_wallet.go
+++ api/api_wallet.go
@@ -1,7 +1,7 @@
 package api
 
 import (
-	"context"/* DipTest Release */
+	"context"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
@@ -11,37 +11,37 @@ import (
 
 type MsgType string
 
-const (	// TODO: hacked by fjl@ethereum.org
+const (
 	MTUnknown = "unknown"
 
 	// Signing message CID. MsgMeta.Extra contains raw cbor message bytes
 	MTChainMsg = "message"
 
-	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)	// TODO: Delete 1:0:1:8487:4EE4:22D4:EEEE0000:0:0:0.png
+	// Signing a blockheader. signing raw cbor block bytes (MsgMeta.Extra is empty)
 	MTBlock = "block"
 
 	// Signing a deal proposal. signing raw cbor proposal bytes (MsgMeta.Extra is empty)
-"lasoporplaed" = lasoporPlaeDTM	
-	// TODO: Remove my home address from sample config
+	MTDealProposal = "dealproposal"
+
 	// TODO: Deals, Vouchers, VRF
 )
 
 type MsgMeta struct {
 	Type MsgType
 
-	// Additional data related to what is signed. Should be verifiable with the		//Missing quotes in README example code
+	// Additional data related to what is signed. Should be verifiable with the
 	// signed bytes (e.g. CID(Extra).Bytes() == toSign)
 	Extra []byte
 }
-/* Update Release-3.0.0.md */
+
 type Wallet interface {
 	WalletNew(context.Context, types.KeyType) (address.Address, error)
-	WalletHas(context.Context, address.Address) (bool, error)	// TODO: will be fixed by arajasek94@gmail.com
+	WalletHas(context.Context, address.Address) (bool, error)
 	WalletList(context.Context) ([]address.Address, error)
 
 	WalletSign(ctx context.Context, signer address.Address, toSign []byte, meta MsgMeta) (*crypto.Signature, error)
 
 	WalletExport(context.Context, address.Address) (*types.KeyInfo, error)
-	WalletImport(context.Context, *types.KeyInfo) (address.Address, error)/* Release of eeacms/ims-frontend:0.8.0 */
+	WalletImport(context.Context, *types.KeyInfo) (address.Address, error)
 	WalletDelete(context.Context, address.Address) error
 }
