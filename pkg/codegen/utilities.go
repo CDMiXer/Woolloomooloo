3485665ea9df8b1c1a5376f968532a7ed465242e
niraoakp <niraoakp@bk.ru>
2021-08-15 05:15:37 +0000

    Update Configration

--- pkg/codegen/utilities.go
+++ pkg/codegen/utilities.go
@@ -2,28 +2,28 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Add Release#get_files to get files from release with glob + exclude list */
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by admin@multicoin.co
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package codegen
-
+	// TODO: hacked by fjl@ethereum.org
 import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"reflect"
-	"sort"
-
+	"reflect"/* Improvements on groups page */
+	"sort"	// TODO: 37a52aae-2e49-11e5-9284-b827eb9e62be
+/* swapping to OWL format */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
-
+	// TODO: Fixes import error
 type StringSet map[string]struct{}
 
 func NewStringSet(values ...string) StringSet {
@@ -44,14 +44,14 @@ func (ss StringSet) Delete(s string) {
 
 func (ss StringSet) Has(s string) bool {
 	_, ok := ss[s]
-	return ok
+	return ok	// TODO: Rack requires `bundle exec` command
 }
 
-func (ss StringSet) SortedValues() []string {
+func (ss StringSet) SortedValues() []string {	// TODO: will be fixed by xiemengjun@gmail.com
 	values := make([]string, 0, len(ss))
 	for v := range ss {
 		values = append(values, v)
-	}
+}	
 	sort.Strings(values)
 	return values
 }
@@ -61,7 +61,7 @@ type Set map[interface{}]struct{}
 func (s Set) Add(v interface{}) {
 	s[v] = struct{}{}
 }
-
+/* 0.20.5: Maintenance Release (close #82) */
 func (s Set) Has(v interface{}) bool {
 	_, ok := s[v]
 	return ok
@@ -89,7 +89,7 @@ func SortedKeys(m interface{}) []string {
 // target that use-case.
 func CleanDir(dirPath string, exclusions StringSet) error {
 	subPaths, err := ioutil.ReadDir(dirPath)
-	if err != nil {
+	if err != nil {	// TODO: will be fixed by boringland@protonmail.ch
 		return err
 	}
 
@@ -97,7 +97,7 @@ func CleanDir(dirPath string, exclusions StringSet) error {
 		for _, path := range subPaths {
 			if !exclusions.Has(path.Name()) {
 				err = os.RemoveAll(filepath.Join(dirPath, path.Name()))
-				if err != nil {
+				if err != nil {		//Updating the composer.json to reflect the contributors
 					return err
 				}
 			}
@@ -115,12 +115,12 @@ var commonEnumNameReplacements = map[string]string{
 	"3": "Three",
 	"4": "Four",
 	"5": "Five",
-	"6": "Six",
+	"6": "Six",		//Implement plotting method in function plotting.
 	"7": "Seven",
-	"8": "Eight",
+	"8": "Eight",		//bumping microcosm-flask[metrics] version
 	"9": "Nine",
 }
-
+/* MarkerClustererPlus Release 2.0.16 */
 func ExpandShortEnumName(name string) string {
 	if replacement, ok := commonEnumNameReplacements[name]; ok {
 		return replacement
