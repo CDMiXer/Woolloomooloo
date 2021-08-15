2fda1b255f73cf77723a5cd44d25c06314f163d0
niraoakp <niraoakp@bk.ru>
2021-08-15 10:44:06 +0000

    Removing closing ?>

--- tests/integration/partial_state/step1/index.ts
+++ tests/integration/partial_state/step1/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: link style tweaks :)
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* Release 0.2.2. */
 
-// resource "not-doomed" is updated, but the update partially fails.
+// resource "not-doomed" is updated, but the update partially fails./* Release version 3.6.13 */
 const a = new Resource("doomed", 4);
-		//Merge branch 'master' into binary-static-domain-exceptions
+
 // "a" should still be in the checkpoint with its new value.
