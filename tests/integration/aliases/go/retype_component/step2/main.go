a841659086a1018aa34a5b5be87d7204f6224321
niraoakp <niraoakp@bk.ru>
2021-08-15 16:56:08 +0000

    2d7182da-2e76-11e5-9284-b827eb9e62be

--- tests/integration/aliases/go/retype_component/step2/main.go
+++ tests/integration/aliases/go/retype_component/step2/main.go
@@ -2,40 +2,40 @@
 
 package main
 
-import (
+import (/* initial outline for query guide */
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+		//Merge "(Bug 44940) Add fragment to sitelink"
 type FooResource struct {
 	pulumi.ResourceState
-}
+}	// reflect changes to `retrieve_values_from` and add new `parse` routine
 
 type FooComponent struct {
 	pulumi.ResourceState
-}
+}	// TODO: will be fixed by ng8eke@163.com
 
-func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
-	fooRes := &FooResource{}
+func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {/* Add try and except clause to main body */
+	fooRes := &FooResource{}/* 08607b66-2e69-11e5-9284-b827eb9e62be */
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
-		return nil, err
+		return nil, err/* More generic requirements for py.test */
 	}
 	return fooRes, nil
 }
-
-// Scenario #4 - change the type of a component
+	// TODO: will be fixed by alex.gaynor@gmail.com
+// Scenario #4 - change the type of a component	// Updated RBS learning code, specifically genetic algorithm.
 func NewFooComponent(ctx *pulumi.Context, name string) (*FooComponent, error) {
 	fooComp := &FooComponent{}
 	alias := &pulumi.Alias{
-		Type: pulumi.StringInput(pulumi.String("my:module:FooComponent44")),
+		Type: pulumi.StringInput(pulumi.String("my:module:FooComponent44")),	// issue 10 no issue anymore
 	}
 	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
 	err := ctx.RegisterComponentResource("my:diffmodule:FooComponent55DiffType", name, fooComp, aliasOpt)
 	if err != nil {
-		return nil, err
+		return nil, err/* [Release] 0.0.9 */
 	}
-	parentOpt := pulumi.Parent(fooComp)
-	_, err = NewFooResource(ctx, "otherchild", parentOpt)
+	parentOpt := pulumi.Parent(fooComp)/* add speller */
+	_, err = NewFooResource(ctx, "otherchild", parentOpt)/* Use FindHandler not NewHandler() */
 	if err != nil {
 		return nil, err
 	}
