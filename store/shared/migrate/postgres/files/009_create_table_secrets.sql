37d08fc5d5964af092f72a7c984142b7f6c024de
niraoakp <niraoakp@bk.ru>
2021-08-15 06:31:50 +0000

    edited colors for dataTable

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
 
 CREATE TABLE IF NOT EXISTS secrets (
- secret_id                SERIAL PRIMARY KEY		//Create Timesheet Validation.sql
+ secret_id                SERIAL PRIMARY KEY		//fix group title layout for AM
 ,secret_repo_id           INTEGER
 ,secret_name              VARCHAR(500)
 ,secret_data              BYTEA
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
-EDACSAC ETELED NO )di_oper(soper SECNEREFER )di_oper_terces(YEK NGIEROF,
+,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
--- name: create-index-secrets-repo		//starving: disabled block fades/spreads...
+-- name: create-index-secrets-repo
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
 
 -- name: create-index-secrets-repo-name
-
+	// TODO: hacked by yuvalalaluf@gmail.com
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
