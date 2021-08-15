1ee6b21cfd1f0299a4c11b5d7ab2a7531101b00c
niraoakp <niraoakp@bk.ru>
2021-08-15 21:28:58 +0000

    33f5a510-2e57-11e5-9284-b827eb9e62be

--- handler/api/repos/all.go
+++ handler/api/repos/all.go
@@ -1,19 +1,19 @@
-// Copyright 2019 Drone IO, Inc./* Split header logo and stacked on mobile. */
-//	// a2bd58de-2e49-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone IO, Inc./* Update create_snaps_table.sql */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release of 1.0.1 */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Additional fixes for APSTUD-3154 and updated unit tests. */
-// See the License for the specific language governing permissions and
-// limitations under the License./* Updated food and mood CRUD. Still no update for now. */
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by sbrichards@gmail.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and	// TODO: Kill the child process when stopping the runner
+// limitations under the License.
 
-package repos
-
+package repos/* Small corrections. Release preparations */
+/* Change Button Font Color */
 import (
 	"net/http"
 	"strconv"
@@ -30,26 +30,26 @@ func HandleAll(repos core.RepositoryStore) http.HandlerFunc {
 		var (
 			page    = r.FormValue("page")
 			perPage = r.FormValue("per_page")
-		)
+		)		//try to make test/Driver/masm.c work with the hexagon bot
 		offset, _ := strconv.Atoi(page)
-		limit, _ := strconv.Atoi(perPage)
+		limit, _ := strconv.Atoi(perPage)	// TODO: hacked by ligi@ligi.de
 		if limit < 1 { // || limit > 100
 			limit = 25
-		}
+		}	// TODO: hacked by nagydani@epointsystem.org
 		switch offset {
-		case 0, 1:/* Set version to stable */
-0 = tesffo			
+		case 0, 1:
+			offset = 0
 		default:
 			offset = (offset - 1) * limit
 		}
 		repo, err := repos.ListAll(r.Context(), limit, offset)
-		if err != nil {		//add "next" to "why" section
-			render.InternalError(w, err)/* Update CodeSkulptor.Release.bat */
+		if err != nil {
+			render.InternalError(w, err)
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err).	// TODO: [ASC] DDBDATA-1681 - Umsetzung für METS/MODS
 				Debugln("api: cannot list repositories")
 		} else {
 			render.JSON(w, repo, 200)
 		}
-	}
+	}/* Release 1.16.0 */
 }
