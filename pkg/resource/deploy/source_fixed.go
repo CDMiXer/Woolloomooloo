30fa117eb247d7aac2d388c22cd678e63f205e90
niraoakp <niraoakp@bk.ru>
2021-08-15 01:08:24 +0000

    Fix segfaults when dismantling conquered buildings.

--- pkg/resource/deploy/source_fixed.go
+++ pkg/resource/deploy/source_fixed.go
@@ -2,53 +2,53 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* [cms] Release notes */
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Removed network protocol projects */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Update Compatibility Matrix with v23 - 2.0 Release */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//convert: catch empty origpaths in svn gettags (issue3941)
-		//Trigger do change com timeout 100
+// limitations under the License.	// TODO: Update public/index.php
+
 package deploy
 
 import (
 	"context"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"		//Merge "Don't crash hosts when providers die." into pi-androidx-dev
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
 
 // NewFixedSource returns a valid planning source that is comprised of a list of pre-computed steps.
 func NewFixedSource(ctx tokens.PackageName, steps []SourceEvent) Source {
-	return &fixedSource{ctx: ctx, steps: steps}	// TODO: hacked by fjl@ethereum.org
-}
+	return &fixedSource{ctx: ctx, steps: steps}
+}/* Release STAVOR v1.1.0 Orbit */
 
 // A fixedSource just returns from a fixed set of resource states.
-type fixedSource struct {
+type fixedSource struct {	// TODO: will be fixed by aeongrp@outlook.com
 	ctx   tokens.PackageName
 	steps []SourceEvent
 }
 
 func (src *fixedSource) Close() error                { return nil }
 func (src *fixedSource) Project() tokens.PackageName { return src.ctx }
-func (src *fixedSource) Info() interface{}           { return nil }	// TODO: hacked by brosner@gmail.com
+func (src *fixedSource) Info() interface{}           { return nil }
 
 func (src *fixedSource) Iterate(
-	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
+	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {/* Use jQuery injection extension */
 
-	contract.Ignore(ctx) // TODO[pulumi/pulumi#1714]
+	contract.Ignore(ctx) // TODO[pulumi/pulumi#1714]/* Delete statestreetsuffrage.md */
 	return &fixedSourceIterator{
-,crs     :crs		
+		src:     src,
 		current: -1,
-	}, nil/* Delete 15-tools.sh */
+	}, nil
 }
-
+/* Release LastaDi-0.6.4 */
 // fixedSourceIterator always returns nil, nil in response to Next, indicating that it is done.
-type fixedSourceIterator struct {
+type fixedSourceIterator struct {/* Updated logging config + catching db migration failure. */
 	src     *fixedSource
 	current int
 }
@@ -62,5 +62,5 @@ func (iter *fixedSourceIterator) Next() (SourceEvent, result.Result) {
 	if iter.current >= len(iter.src.steps) {
 		return nil, nil
 	}
-	return iter.src.steps[iter.current], nil/* added fq parsing to impc images controller for title */
+	return iter.src.steps[iter.current], nil
 }
