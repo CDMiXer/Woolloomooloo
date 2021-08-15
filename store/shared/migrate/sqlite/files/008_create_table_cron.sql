e87091b0d030e0f28dbf1d1bc265638c36840a7a
niraoakp <niraoakp@bk.ru>
2021-08-15 02:17:05 +0000

    [cleanup] formatting + imports

--- store/shared/migrate/sqlite/files/008_create_table_cron.sql
+++ store/shared/migrate/sqlite/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
 -- name: create-table-cron
 
 CREATE TABLE IF NOT EXISTS cron (
- cron_id          INTEGER PRIMARY KEY AUTOINCREMENT		//Escape pod safes now contain red oxygen tanks instead of air mix tanks.
+ cron_id          INTEGER PRIMARY KEY AUTOINCREMENT		//Merge "Implement optional API versioning"
 ,cron_repo_id     INTEGER
 ,cron_name        TEXT
 ,cron_expr        TEXT
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
 ,cron_event       TEXT
-,cron_branch      TEXT/* removes sublime */
-,cron_target      TEXT	// Update models/classes/taskQueue/Worker/AbstractWorker.php
+,cron_branch      TEXT
+,cron_target      TEXT/* Added dummy backend to MANIFEST.  Released 0.6.2. */
 ,cron_disabled    BOOLEAN
-,cron_created     INTEGER
-,cron_updated     INTEGER/* New tarball (r825) (0.4.6 Release Candidat) */
+,cron_created     INTEGER	// TODO: Delete selecepisodio.py
+,cron_updated     INTEGER
 ,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
-,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* Basic table generation. */
 );
-
+/* Release of eeacms/www-devel:21.1.12 */
 -- name: create-index-cron-repo
-/* Job: #104 Add implementation note */
+
 CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
 
 -- name: create-index-cron-next
 
-CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);/* fix and doc */
+CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
