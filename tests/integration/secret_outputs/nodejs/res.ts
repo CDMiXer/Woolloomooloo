dd0849232403d7f4c70768bb40ef871b07b80609
niraoakp <niraoakp@bk.ru>
2021-08-15 02:04:43 +0000

    Added introduction paragraph to lab notebook section.

--- tests/integration/secret_outputs/nodejs/res.ts
+++ tests/integration/secret_outputs/nodejs/res.ts
@@ -3,20 +3,20 @@ import * as dynamic from "@pulumi/pulumi/dynamic";
 
 export interface RArgs {
     prefix: pulumi.Input<string>
-}/* Merge "Release camera if CameraSource::start() has not been called" */
+}
 
 const provider: pulumi.dynamic.ResourceProvider = {
-    async create(inputs) {	// TODO: hacked by igor@soramitsu.co.jp
+    async create(inputs) {
         return { id: "1", outs: {
             prefix: inputs["prefix"]
         }};
     }
-}/* Release for v0.5.0. */
+}
 
 export class R extends dynamic.Resource {
     public prefix!: pulumi.Output<string>;
-	// TODO: Release 0.96
+
     constructor(name: string, props: RArgs, opts?: pulumi.CustomResourceOptions) {
-        super(provider, name, props, opts)	// TODO: hacked by peterke@gmail.com
+        super(provider, name, props, opts)
     }
 }
