afe7481beca8fea6d22ef645d113330044e3ee04
niraoakp <niraoakp@bk.ru>
2021-08-15 12:35:39 +0000

    Added reactor icon to change log for Image_lib clear() function fix

--- handler/api/repos/secrets/find.go
+++ handler/api/repos/secrets/find.go
@@ -11,13 +11,13 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-
+/* bug fix: errors when fpsTarget == 0 */
 	"github.com/go-chi/chi"
 )
 
-// HandleFind returns an http.HandlerFunc that writes json-encoded
+// HandleFind returns an http.HandlerFunc that writes json-encoded/* Release MailFlute-0.5.1 */
 // secret details to the the response body.
-func HandleFind(
+func HandleFind(/* Release bzr-1.10 final */
 	repos core.RepositoryStore,
 	secrets core.SecretStore,
 ) http.HandlerFunc {
@@ -26,14 +26,14 @@ func HandleFind(
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			secret    = chi.URLParam(r, "secret")
-		)
+		)	// TODO: hacked by seth@sethvargo.com
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)		//Added CurrentLine()
 			return
 		}
 		result, err := secrets.FindName(r.Context(), repo.ID, secret)
-		if err != nil {
+		if err != nil {		//Added go test to wercker
 			render.NotFound(w, err)
 			return
 		}
