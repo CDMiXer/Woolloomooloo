75fb60573044149a4c9607ad39e84b552225d162
niraoakp <niraoakp@bk.ru>
2021-08-15 03:32:06 +0000

    Mislabeled disable share page

--- tests/integration/partial_state/step4/index.ts
+++ tests/integration/partial_state/step4/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* 30 secs is 600 ticks. Don't lie. */
+
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
 // resource "not-doomed" is updated, but the update partially fails.
-const a = new Resource("not-doomed", 4);/* Adding the Codeship badge */
+const a = new Resource("not-doomed", 4);
 
-// "a" should still be in the checkpoint with its new value.	// TODO: will be fixed by admin@multicoin.co
+// "a" should still be in the checkpoint with its new value.
