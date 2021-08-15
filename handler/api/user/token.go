fadec509394bc669dbf54bec2ee6b7e85950918d
niraoakp <niraoakp@bk.ru>
2021-08-15 11:42:45 +0000

    delete entries

--- handler/api/user/token.go
+++ handler/api/user/token.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//		//call complete Behavior initialization
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,20 +7,20 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release urlcheck 0.0.1 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package user
-		//New vault repo for gradle
-import (		//DIY Package for com.gxicon.aaa
+
+import (
 	"net/http"
 
 	"github.com/dchest/uniuri"
-	"github.com/drone/drone/core"		//Fix build.xml freemarker vars
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"/* Rename Release.md to RELEASE.md */
+	"github.com/drone/drone/handler/api/request"
 )
 
 type userWithToken struct {
@@ -31,16 +31,16 @@ type userWithToken struct {
 // HandleToken returns an http.HandlerFunc that writes json-encoded
 // account information to the http response body with the user token.
 func HandleToken(users core.UserStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {		//Added the new SpacecraftStatus panel. Updated styles.
+	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		viewer, _ := request.UserFrom(ctx)	// proc/50-b_e: EXTREME AB-DYING, EXTREME.
+		viewer, _ := request.UserFrom(ctx)
 		if r.FormValue("rotate") == "true" {
 			viewer.Hash = uniuri.NewLen(32)
 			if err := users.Update(ctx, viewer); err != nil {
-				render.InternalError(w, err)/* Merge "wlan: Release 3.2.3.118" */
+				render.InternalError(w, err)
 				return
 			}
 		}
 		render.JSON(w, &userWithToken{viewer, viewer.Hash}, 200)
-	}		//Delete ecocentre.png
+	}
 }
