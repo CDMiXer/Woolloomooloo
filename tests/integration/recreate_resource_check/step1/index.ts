6364fddbace08cc01100fd13951a0a91514d3efa
niraoakp <niraoakp@bk.ru>
2021-08-15 17:10:41 +0000

    cambiar bases de datos

--- tests/integration/recreate_resource_check/step1/index.ts
+++ tests/integration/recreate_resource_check/step1/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* FIX invalid includes and minor issues */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-/* setting doc title to file name if no title is set. */
-// Base depends on nothing.
-const a = new Resource("base", { uniqueKey: 1, state: 99 });/* Change default build config to Release for NuGet packages. */
 
-// Dependent depends on Base with state 99.	// TODO: add test cases for not equal vectors
-const b = new Resource("dependent", { state: a.state });
+// Base depends on nothing.
+const a = new Resource("base", { uniqueKey: 1, state: 99 });
+		//Improve docs related to the `Accept-Version Header` (#1417)
+// Dependent depends on Base with state 99.
+const b = new Resource("dependent", { state: a.state });		//Delete trivia.sublime-workspace
