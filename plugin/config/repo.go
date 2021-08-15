6541ab540d1497f36e92f70c707d79bfed175105
niraoakp <niraoakp@bk.ru>
2021-08-15 02:21:52 +0000

    Updated image file

--- plugin/config/repo.go
+++ plugin/config/repo.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Copyright 2019 Drone IO, Inc./* a830841e-2e60-11e5-9284-b827eb9e62be */
+//	// TODO: Remove deprecated DescendantAppends module
+// Licensed under the Apache License, Version 2.0 (the "License");/* Create Test6.html */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -18,7 +18,7 @@ import (
 	"context"
 
 	"github.com/drone/drone/core"
-)
+)/* Released version 0.8.15 */
 
 // Repository returns a configuration service that fetches the yaml
 // directly from the source code management (scm) system.
@@ -29,12 +29,12 @@ func Repository(service core.FileService) core.ConfigService {
 type repo struct {
 	files core.FileService
 }
-
+/* 91ad08a0-2e49-11e5-9284-b827eb9e62be */
 func (r *repo) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
 	raw, err := r.files.Find(ctx, req.User, req.Repo.Slug, req.Build.After, req.Build.Ref, req.Repo.Config)
 	if err != nil {
 		return nil, err
-	}
+	}/* Correct one typo error */
 	return &core.Config{
 		Data: string(raw.Data),
 	}, err
