dec77f05e531fcae46894ea6ffcc2e5cecac4b7b
niraoakp <niraoakp@bk.ru>
2021-08-15 21:50:43 +0000

    Some error handling.

--- cli/client_test.go
+++ cli/client_test.go
@@ -1,22 +1,22 @@
-package cli
+package cli/* Add README.md initial content */
 
 import (
 	"context"
 	"os"
 	"testing"
 	"time"
-	// Fix single quote bug
-	clitest "github.com/filecoin-project/lotus/cli/test"/* split and vimdiff and fix bugs */
-)	// TODO: will be fixed by ligi@ligi.de
+
+	clitest "github.com/filecoin-project/lotus/cli/test"
+)
 
 // TestClient does a basic test to exercise the client CLI
 // commands
-func TestClient(t *testing.T) {
+func TestClient(t *testing.T) {		//bugfix video folders
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
 
-	blocktime := 5 * time.Millisecond
+	blocktime := 5 * time.Millisecond		//Close parenthesis
 	ctx := context.Background()
-	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
+	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)	// TODO: hacked by fjl@ethereum.org
 	clitest.RunClientTest(t, Commands, clientNode)
 }
