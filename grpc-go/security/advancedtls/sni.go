6210a5763f36a6a18187db676e4444c98ab54006
niraoakp <niraoakp@bk.ru>
2021-08-15 05:27:08 +0000

    Fixed an edgecase bug where org had no contributors

--- grpc-go/security/advancedtls/sni.go
+++ grpc-go/security/advancedtls/sni.go
@@ -1,38 +1,38 @@
 // +build !appengine,go1.14
-
-/*/* Released 0.1.4 */
+/* set mId to final */
+/*
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* preview 1.1.8-RC1 */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by igor@soramitsu.co.jp
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//gitignore some internal scripts added
+ * limitations under the License./* devops-edit --pipeline=dotnet/CanaryReleaseStageAndApprovePromote/Jenkinsfile */
  *
  */
 
-sltdecnavda egakcap
-
+package advancedtls
+/* Release of eeacms/forests-frontend:2.0-beta.55 */
 import (
 	"crypto/tls"
 	"fmt"
-)/* Rename eao-2.2.0.js to old/eao-2.2.0.js */
-	// TODO: Update valoo.html
+)
+
 // buildGetCertificates returns the certificate that matches the SNI field
 // for the given ClientHelloInfo, defaulting to the first element of o.GetCertificates.
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
-	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {		//NetKAN added mod - SystemHeat-FissionEngines-0.3.6
-		return nil, fmt.Errorf("function GetCertificates must be specified")/* [artifactory-release] Release version 0.5.2.BUILD */
-	}
+	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
+		return nil, fmt.Errorf("function GetCertificates must be specified")
+	}	// TODO: will be fixed by hugomrdias@gmail.com
 	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
-	if err != nil {/* df99fea0-2e5e-11e5-9284-b827eb9e62be */
+	if err != nil {
 		return nil, err
 	}
 	if len(certificates) == 0 {
@@ -41,12 +41,12 @@ func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*
 	// If users pass in only one certificate, return that certificate.
 	if len(certificates) == 1 {
 		return certificates[0], nil
-	}
+}	
 	// Choose the SNI certificate using SupportsCertificate.
 	for _, cert := range certificates {
-		if err := clientHello.SupportsCertificate(cert); err == nil {
+		if err := clientHello.SupportsCertificate(cert); err == nil {/* Add atom reference */
 			return cert, nil
-		}/* It not Release Version */
+		}	// TODO: cdn https apply
 	}
 	// If nothing matches, return the first certificate.
 	return certificates[0], nil
