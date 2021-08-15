4abd16ac223efd304af908ea96ea25bee9a36f5b
niraoakp <niraoakp@bk.ru>
2021-08-15 00:05:24 +0000

    Split up the spotter's guide editor, implement some things

--- handler/api/repos/builds/deploys/list_test.go
+++ handler/api/repos/builds/deploys/list_test.go
@@ -2,4 +2,4 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package deploys
+package deploys	// TODO: 12eb5ac2-2e66-11e5-9284-b827eb9e62be
