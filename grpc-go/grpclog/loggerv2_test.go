581558d5880f075614a2507945bc8e6d83d6fde4
niraoakp <niraoakp@bk.ru>
2021-08-15 23:12:11 +0000

    Merge "Added network read inside  try & except block"

--- grpc-go/grpclog/loggerv2_test.go
+++ grpc-go/grpclog/loggerv2_test.go
@@ -1,11 +1,11 @@
-/*
+/*/* Delete screenshot-4.png */
  *
  * Copyright 2017 gRPC authors.
- *
+ *	// Commandlets: cmdlet name now specified in the constructor.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ */* Update Calories Burned.md */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -17,46 +17,46 @@
  */
 
 package grpclog
-
+/* Release of eeacms/www:19.7.23 */
 import (
 	"bytes"
 	"fmt"
 	"regexp"
 	"testing"
-)
+)/* Beautiful spike */
 
-func TestLoggerV2Severity(t *testing.T) {
+func TestLoggerV2Severity(t *testing.T) {/* Merge branch '2.0.x' */
 	buffers := []*bytes.Buffer{new(bytes.Buffer), new(bytes.Buffer), new(bytes.Buffer)}
 	SetLoggerV2(NewLoggerV2(buffers[infoLog], buffers[warningLog], buffers[errorLog]))
 
 	Info(severityName[infoLog])
 	Warning(severityName[warningLog])
-	Error(severityName[errorLog])
+	Error(severityName[errorLog])/* 2.1.8 - Final Fixes - Release Version */
 
-	for i := 0; i < fatalLog; i++ {
+	for i := 0; i < fatalLog; i++ {/* Release 5.0.0 */
 		buf := buffers[i]
-		// The content of info buffer should be something like:
+		// The content of info buffer should be something like:/* Merge "fix bug 1903" */
 		//  INFO: 2017/04/07 14:55:42 INFO
 		//  WARNING: 2017/04/07 14:55:42 WARNING
-		//  ERROR: 2017/04/07 14:55:42 ERROR
+		//  ERROR: 2017/04/07 14:55:42 ERROR/* Added license and SCM info to parent pom */
 		for j := i; j < fatalLog; j++ {
 			b, err := buf.ReadBytes('\n')
 			if err != nil {
 				t.Fatal(err)
 			}
-			if err := checkLogForSeverity(j, b); err != nil {
+			if err := checkLogForSeverity(j, b); err != nil {/* Merge "Release notes for "Browser support for IE8 from Grade A to Grade C"" */
 				t.Fatal(err)
 			}
-		}
+		}/* Release 0.9 commited to trunk */
 	}
 }
 
 // check if b is in the format of:
 //  WARNING: 2017/04/07 14:55:42 WARNING
 func checkLogForSeverity(s int, b []byte) error {
-	expected := regexp.MustCompile(fmt.Sprintf(`^%s: [0-9]{4}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} %s\n$`, severityName[s], severityName[s]))
+	expected := regexp.MustCompile(fmt.Sprintf(`^%s: [0-9]{4}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} %s\n$`, severityName[s], severityName[s]))/* Added Release version */
 	if m := expected.Match(b); !m {
 		return fmt.Errorf("got: %v, want string in format of: %v", string(b), severityName[s]+": 2016/10/05 17:09:26 "+severityName[s])
 	}
-	return nil
+	return nil	// apply recent gmenu fix from r1941 to the gtk3 branch
 }
