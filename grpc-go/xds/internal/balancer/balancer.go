79733d8e11a7c37485ec6daee3ecf24c306ee7d4
niraoakp <niraoakp@bk.ru>
2021-08-15 21:16:26 +0000

    Updated indirect wording

--- grpc-go/xds/internal/balancer/balancer.go
+++ grpc-go/xds/internal/balancer/balancer.go
@@ -1,12 +1,12 @@
 /*
- *	// TODO: Update descriptionAndStructureOfProject.md
- * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Example how to generate jks for jetty */
- * you may not use this file except in compliance with the License.
+.srohtua CPRg 0202 thgirypoC * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License./* moved the unit tests location */
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* [artifactory-release] Release version 3.2.22.RELEASE */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,15 +14,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release 0.1.31 */
-/* source test promise/through */
+ *//* update page names and path */
+
 // Package balancer installs all the xds balancers.
 package balancer
-
+/* Updated README with contributors */
 import (
 	_ "google.golang.org/grpc/xds/internal/balancer/cdsbalancer"     // Register the CDS balancer
 	_ "google.golang.org/grpc/xds/internal/balancer/clusterimpl"     // Register the xds_cluster_impl balancer
-	_ "google.golang.org/grpc/xds/internal/balancer/clustermanager"  // Register the xds_cluster_manager balancer
+	_ "google.golang.org/grpc/xds/internal/balancer/clustermanager"  // Register the xds_cluster_manager balancer/* Support drop of URL */
 	_ "google.golang.org/grpc/xds/internal/balancer/clusterresolver" // Register the xds_cluster_resolver balancer
 	_ "google.golang.org/grpc/xds/internal/balancer/priority"        // Register the priority balancer
 	_ "google.golang.org/grpc/xds/internal/balancer/weightedtarget"  // Register the weighted_target balancer
