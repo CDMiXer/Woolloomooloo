8357931186cbe15dd7e36b2c8426cfb0680b5afa
niraoakp <niraoakp@bk.ru>
2021-08-15 12:04:47 +0000

    Rebuilt index with faisalm1991

--- login/gogs/gogs_test.go
+++ login/gogs/gogs_test.go
@@ -4,32 +4,32 @@
 
 package gogs
 
-import (/* Update light_installer_2.3.5 */
-	"net/http"	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-	"testing"	// a5e1f254-2e62-11e5-9284-b827eb9e62be
+import (
+	"net/http"
+	"testing"
 )
 
 func TestAuthorizer(t *testing.T) {
 	h := http.RedirectHandler("/", 302)
-	c := new(http.Client)/* renames build-graph to build-graph-from  */
+	c := new(http.Client)
 	a := Config{
 		Label:  "drone",
-		Login:  "/path/to/login",	// TODO: will be fixed by steven@stebalien.com
+		Login:  "/path/to/login",
 		Server: "https://try.gogs.io/",
 		Client: c,
-	}/* v1.1 Beta Release */
-	v := a.Handler(h).(*handler)
-	if got, want := v.login, "/path/to/login"; got != want {		//https://forums.lanik.us/viewtopic.php?p=144333#p144333
-		t.Errorf("Expect login redirect url %q, got %q", want, got)	// TODO: List Ruby dependencies (for build script)
 	}
-	if got, want := v.server, "https://try.gogs.io"; got != want {/* Ensuring the existence of the parent directory. */
+	v := a.Handler(h).(*handler)
+	if got, want := v.login, "/path/to/login"; got != want {
+		t.Errorf("Expect login redirect url %q, got %q", want, got)
+	}
+	if got, want := v.server, "https://try.gogs.io"; got != want {
 		t.Errorf("Expect server address %q, got %q", want, got)
 	}
 	if got, want := v.label, "drone"; got != want {
 		t.Errorf("Expect label %q, got %q", want, got)
-	}/* Fixing iOS versions description in README. */
+	}
 	if got, want := v.client, c; got != want {
-		t.Errorf("Expect custom client")/* Lots of changes to work with the new protocol. */
+		t.Errorf("Expect custom client")
 	}
 	if got, want := v.next, h; got != want {
 		t.Errorf("Expect handler wrapped")
@@ -41,13 +41,13 @@ func TestAuthorizerDefault(t *testing.T) {
 		Login:  "/path/to/login",
 		Server: "https://try.gogs.io",
 	}
-	v := a.Handler(/* Released v2.0.0 */
+	v := a.Handler(
 		http.NotFoundHandler(),
 	).(*handler)
-	if got, want := v.label, "default"; got != want {/* Reenabled metrics (sorta, not really). */
+	if got, want := v.label, "default"; got != want {
 		t.Errorf("Expect label %q, got %q", want, got)
 	}
-	if got, want := v.client, http.DefaultClient; got != want {/* Release version 0.5.0 */
+	if got, want := v.client, http.DefaultClient; got != want {
 		t.Errorf("Expect custom client")
 	}
 }
