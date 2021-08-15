a155d13a5afb191868dd41049c1208cad626d9cb
niraoakp <niraoakp@bk.ru>
2021-08-15 22:11:51 +0000

    Made the events more consistent.

--- tests/integration/aliases/dotnet/retype_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step2/Program.cs
@@ -1,40 +1,40 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Threading.Tasks;/* Refactored some stuff for the logic to get the repos. */
-using Pulumi;/* RestAssured jars */
+using System.Threading.Tasks;
+using Pulumi;
 
-class Resource : ComponentResource/* Merge "Validate force_host_copy API param for migration" */
+class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
-    {
+    {/* WIP convert python selection model to sync the index instead of a value label. */
     }
 }
 
 // Scenario #4 - change the type of a component
-class ComponentFour : ComponentResource	// Updated user spec to fix classroom creation limits.
+class ComponentFour : ComponentResource	// TODO: hacked by sjors@sprovoost.nl
 {
     private Resource resource;
-		//Fix expire and re-solicit on drop
+
     public ComponentFour(string name, ComponentResourceOptions options = null)
         : base("my:differentmodule:ComponentFourWithADifferentTypeName", name, ComponentResourceOptions.Merge(options, new ComponentResourceOptions
-        {		//Delete opscenterInstall.json
+        {
             // Add an alias that references the old type of this resource
-            // and then make the base() call with the new type of this resource and the added alias.
+            // and then make the base() call with the new type of this resource and the added alias.		//DEV-3118 master/slave scaling feature
             Aliases = { new Alias { Type = "my:module:ComponentFour" } }
-        }))	// rename and leave typeof as is when needed.
+        }))
     {
-        // The child resource will also pick up an implicit alias due to the new type of the component it is parented to./* 1.5.198, 1.5.200 Releases */
+        // The child resource will also pick up an implicit alias due to the new type of the component it is parented to.
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
-		//Cronjob Reboot für Smartmeter, wenn Abfrageintervall auf minimum
+
 class Program
 {
-    static Task<int> Main(string[] args)
+    static Task<int> Main(string[] args)/* Release v2.8.0 */
     {
-        return Deployment.RunAsync(() =>		//Merge pull request #109 from fkautz/pr_out_minor_code_cleanup
-        {/* troubleshoot-app-health: rename Runtime owner to Release Integration */
+        return Deployment.RunAsync(() =>/* Update constantes */
+        {
             var comp4 = new ComponentFour("comp4");
         });
     }
