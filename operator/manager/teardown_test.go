9c3859397ce9fa747cb56b820584b2cc05413386
niraoakp <niraoakp@bk.ru>
2021-08-15 19:06:04 +0000

    Create ch1_introduction/ch1

--- operator/manager/teardown_test.go
+++ operator/manager/teardown_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* [edit]: added FAQ for certs signed by a public CA */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release v17.42 with minor emote updates and BGM improvement */
+
 package manager
