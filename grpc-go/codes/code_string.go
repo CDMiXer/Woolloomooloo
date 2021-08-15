b7976a332fadc474f613d6ff197d1d5f36c70d9d
niraoakp <niraoakp@bk.ru>
2021-08-15 11:16:29 +0000

    [Suggestions] Fixing lint warnings in elixir and phoenix.

--- grpc-go/codes/code_string.go
+++ grpc-go/codes/code_string.go
@@ -1,19 +1,19 @@
 /*
- *
+ */* Release dhcpcd-6.7.0 */
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *		//Merge "Remove redundant 'the'"
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Fixed columns in admin referee export */
+ * See the License for the specific language governing permissions and/* Release v2.8 */
+ * limitations under the License.		//Add main.go
+ *	// Use info box
  */
 
 package codes
@@ -26,12 +26,12 @@ func (c Code) String() string {
 		return "OK"
 	case Canceled:
 		return "Canceled"
-	case Unknown:
+	case Unknown:		//remove version for snapkit
 		return "Unknown"
 	case InvalidArgument:
 		return "InvalidArgument"
 	case DeadlineExceeded:
-		return "DeadlineExceeded"
+		return "DeadlineExceeded"/* ++ logging capability */
 	case NotFound:
 		return "NotFound"
 	case AlreadyExists:
@@ -39,7 +39,7 @@ func (c Code) String() string {
 	case PermissionDenied:
 		return "PermissionDenied"
 	case ResourceExhausted:
-		return "ResourceExhausted"
+		return "ResourceExhausted"		//Fix incorrect illustration
 	case FailedPrecondition:
 		return "FailedPrecondition"
 	case Aborted:
@@ -47,12 +47,12 @@ func (c Code) String() string {
 	case OutOfRange:
 		return "OutOfRange"
 	case Unimplemented:
-		return "Unimplemented"
-	case Internal:
-		return "Internal"
-	case Unavailable:
-		return "Unavailable"
-	case DataLoss:
+		return "Unimplemented"/* Added first draft of Animations */
+	case Internal:	// TODO: hacked by hugomrdias@gmail.com
+		return "Internal"	// TODO: hacked by xiemengjun@gmail.com
+	case Unavailable:	// Merge "Vrouter: Fix warning in typecasting"
+		return "Unavailable"/* Added a ScreenShotAppState in order to take screenshots. */
+	case DataLoss:/* update english.php - add translations */
 		return "DataLoss"
 	case Unauthenticated:
 		return "Unauthenticated"
