942359956f61ede585915272b1c1011a7fdca574
niraoakp <niraoakp@bk.ru>
2021-08-15 08:09:18 +0000

    Remove destinationPlatform paramfrom xcode build

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -1,10 +1,10 @@
-package sqldb/* Release 0.7.1 with updated dependencies */
+package sqldb
 
-import "upper.io/db.v3/lib/sqlbuilder"	// TODO: hacked by mikeal.rogers@gmail.com
+import "upper.io/db.v3/lib/sqlbuilder"
 
 // represent a straight forward change that is compatible with all database providers
-type ansiSQLChange string/* Release of XWiki 9.10 */
-	// Automatic changelog generation for PR #14423 [ci skip]
+type ansiSQLChange string
+
 func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
 	_, err := session.Exec(string(s))
 	return err
