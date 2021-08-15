7b8dd7ba805b4208f4364d0648562114430df2aa
niraoakp <niraoakp@bk.ru>
2021-08-15 05:41:35 +0000

    Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-960.

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -1,17 +1,17 @@
 package types
-
-import (		//Merge "msm: ipa: adding ftrace to ipa"
-	"errors"
+/* Release version [10.7.2] - alfter build */
+import (
+	"errors"		//Create new_task.tpl
 
 	"github.com/ipfs/go-cid"
 )
-/* Fix Mouse.ReleaseLeft */
+	// TODO: Affichage des propriétés.
 var ErrActorNotFound = errors.New("actor not found")
-
-type Actor struct {		//mobi: fix DrawPage() to not stop at hr
-	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
+/* wagon-ssh 2.7 -> 2.8. */
+type Actor struct {
+	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`./* Merge "Release 4.0.10.73 QCACLD WLAN Driver." */
 	Code    cid.Cid
-	Head    cid.Cid/* GitReleasePlugin - checks branch to be "master" */
+	Head    cid.Cid
 	Nonce   uint64
 	Balance BigInt
 }
