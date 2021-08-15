aa005de7cb15e6d837bd68706b65a09dad67810a
niraoakp <niraoakp@bk.ru>
2021-08-15 04:28:48 +0000

    transform: JSONPath support for non-string values.

--- plugin/secret/external_test.go
+++ plugin/secret/external_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Added 1.1.0 Release */
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: obs on drc requests + conf template
 // that can be found in the LICENSE file.
-
+	// TODO: will be fixed by martin2cai@hotmail.com
 package secret
