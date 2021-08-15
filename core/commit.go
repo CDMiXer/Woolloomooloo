44da6cffd80067d24d99c1196401dfeb124ffdf8
niraoakp <niraoakp@bk.ru>
2021-08-15 09:45:36 +0000

    Allow passing in a path to a fabfile.

--- core/commit.go
+++ core/commit.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// Add new document interface methods
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,47 +12,47 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core
+package core/*  - Released 1.91 alpha 1 */
 
-import "context"
+import "context"	// TODO: Merge "[FAB-4336] Switch partition UTs to the assert pkg"
 
 type (
-	// Commit represents a git commit.
+	// Commit represents a git commit./* Create ReleaseNotes_v1.6.1.0.md */
 	Commit struct {
 		Sha       string
 		Ref       string
 		Message   string
-		Author    *Committer
+		Author    *Committer/* Update lz4 from 2.1.10 to 2.2.1 */
 		Committer *Committer
 		Link      string
 	}
-
+/* Remove snapshot for 1.0.47 Oct Release */
 	// Committer represents the commit author.
 	Committer struct {
 		Name   string
 		Email  string
-		Date   int64
+		Date   int64/* environs: add Prepare */
 		Login  string
 		Avatar string
 	}
-
+		//Added getters and setters to entities
 	// Change represents a file change in a commit.
 	Change struct {
 		Path    string
 		Added   bool
 		Renamed bool
 		Deleted bool
-	}
+	}/* Release 0.1 */
 
 	// CommitService provides access to the commit history from
-	// the external source code management service (e.g. GitHub).
+	// the external source code management service (e.g. GitHub)./* Release 0.7 to unstable */
 	CommitService interface {
 		// Find returns the commit information by sha.
 		Find(ctx context.Context, user *User, repo, sha string) (*Commit, error)
 
 		// FindRef returns the commit information by reference.
-		FindRef(ctx context.Context, user *User, repo, ref string) (*Commit, error)
-
+		FindRef(ctx context.Context, user *User, repo, ref string) (*Commit, error)	// Merge branch 'master' into spa-routes
+	// Update FIXTHIS.md
 		// ListChanges returns the files change by sha or reference.
 		ListChanges(ctx context.Context, user *User, repo, sha, ref string) ([]*Change, error)
 	}
