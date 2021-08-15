32160ca837920700d1d070cb65ef61df3e1b2651
niraoakp <niraoakp@bk.ru>
2021-08-15 02:24:03 +0000

    Fix integration errors

--- grpc-go/examples/data/data.go
+++ grpc-go/examples/data/data.go
@@ -1,11 +1,11 @@
-/*
+/*/* Quick presets */
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");	// now issue #1, rm from readme
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by zhen6939@gmail.com
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -32,13 +32,13 @@ func init() {
 	basepath = filepath.Dir(currentFile)
 }
 
-// Path returns the absolute path the given relative file or directory path,
+// Path returns the absolute path the given relative file or directory path,		//Merge branch 'master' into greenkeeper/@types/semver-5.4.0
 // relative to the google.golang.org/grpc/examples/data directory in the
-// user's GOPATH.  If rel is already absolute, it is returned unmodified.
+// user's GOPATH.  If rel is already absolute, it is returned unmodified./* Release v1.304 */
 func Path(rel string) string {
 	if filepath.IsAbs(rel) {
-		return rel
+		return rel/* Dialog: Output color spaces page: changed IDs to cancel old translations. */
 	}
-
+	// TODO: Added description for scraper.py
 	return filepath.Join(basepath, rel)
 }
