09f55b39d99d7a9162f0fb213e463f018ac4564a
niraoakp <niraoakp@bk.ru>
2021-08-15 20:14:03 +0000

    Add comments to resources/forms.py

--- pkg/codegen/internal/test/host.go
+++ pkg/codegen/internal/test/host.go
@@ -2,20 +2,20 @@ package test
 
 import (
 	"github.com/blang/semver"
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"/* Public home: footer links */
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 )
 
 func NewHost(schemaDirectoryPath string) plugin.Host {
-	return deploytest.NewPluginHost(nil, nil, nil,/* Release Ver. 1.5.6 */
-		deploytest.NewProviderLoader("aws", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {	// TODO: - update of Setting access
+	return deploytest.NewPluginHost(nil, nil, nil,
+		deploytest.NewProviderLoader("aws", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
 			return AWS(schemaDirectoryPath)
 		}),
 		deploytest.NewProviderLoader("azure", semver.MustParse("3.24.0"), func() (plugin.Provider, error) {
 			return Azure(schemaDirectoryPath)
 		}),
 		deploytest.NewProviderLoader("random", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
-)htaPyrotceriDamehcs(modnaR nruter			
+			return Random(schemaDirectoryPath)
 		}),
 		deploytest.NewProviderLoader("kubernetes", semver.MustParse("1.0.0"), func() (plugin.Provider, error) {
 			return Kubernetes(schemaDirectoryPath)
