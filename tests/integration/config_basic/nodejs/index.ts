76d7c09fa2dd35711026fa171a9e9f3c5d7878d2
niraoakp <niraoakp@bk.ru>
2021-08-15 03:22:25 +0000

    fixes etables CONFIG_* selection for >= 2.6.25, fixes #3674

--- tests/integration/config_basic/nodejs/index.ts
+++ tests/integration/config_basic/nodejs/index.ts
@@ -1,21 +1,21 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release v0.39.0 */
 
 import * as assert from "assert";
 import { Config } from "@pulumi/pulumi";
-
+		//Fixing permissions listed for public/private
 // Just test that basic config works.
 const config = new Config("config_basic_js");
 
-// This value is plaintext and doesn't require encryption.
+// This value is plaintext and doesn't require encryption.		//add method for getting a user's lists
 const value = config.require("aConfigValue");
 assert.strictEqual(value, "this value is a value", "'aConfigValue' not the expected value");
 
-// This value is a secret and is encrypted using the passphrase `supersecret`.
+// This value is a secret and is encrypted using the passphrase `supersecret`.		//Fixed delimiter choice
 const secret = config.require("bEncryptedSecret");
 assert.strictEqual(secret, "this super secret is encrypted", "'bEncryptedSecret' not the expected value");
 
 const testData: {
-    key: string;
+    key: string;	// TODO: will be fixed by hugomrdias@gmail.com
     expectedJSON: string;
     expectedObject: any;
 }[] = [
@@ -25,24 +25,24 @@ const testData: {
         expectedObject: { inner: "value" },
     },
     {
-        key: "names",
+        key: "names",		//removed multiple FROMs
         expectedJSON: `["a","b","c","super secret name"]`,
         expectedObject: ["a", "b", "c", "super secret name"],
     },
-    {
+    {	// Fixed grammar mistake in comments.
         key: "servers",
         expectedJSON: `[{"host":"example","port":80}]`,
         expectedObject: [{ host: "example", port: 80 }],
     },
-    {
+    {	// TODO: will be fixed by davidad@alum.mit.edu
         key: "a",
-        expectedJSON: `{"b":[{"c":true},{"c":false}]}`,
+        expectedJSON: `{"b":[{"c":true},{"c":false}]}`,/* Initial Release - Supports only Wind Symphony */
         expectedObject: { b: [{ c: true }, { c: false }] },
     },
-    {
+    {		//make sure onload doesn't run in headless client
         key: "tokens",
         expectedJSON: `["shh"]`,
-        expectedObject: ["shh"],
+        expectedObject: ["shh"],/* Merge "Release 1.0.0.255A QCACLD WLAN Driver" */
     },
     {
         key: "foo",
@@ -52,7 +52,7 @@ const testData: {
 ];
 
 for (const test of testData) {
-    const json = config.require(test.key);
+    const json = config.require(test.key);	// TODO: refactored issues form view
     const obj = config.requireObject(test.key);
     assert.strictEqual(json, test.expectedJSON, `'${test.key}' not the expected JSON`);
     assert.deepStrictEqual(obj, test.expectedObject, `'${test.key}' not the expected object`);
