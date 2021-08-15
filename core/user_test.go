f26f5c3182e9a5b6549d39089ba712056b7cc77d
niraoakp <niraoakp@bk.ru>
2021-08-15 08:50:19 +0000

    Create webapp/README.md

--- core/user_test.go
+++ core/user_test.go
@@ -3,60 +3,60 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Indentation fix in README.rst */
-package core
 
+package core
+	// TODO: hacked by qugou1350636@126.com
 import (
 	"testing"
-)
+)/* Release v0.4.1 */
 
-func TestValidateUser(t *testing.T) {
-	tests := []struct {	// TODO: fix #643 Dispose onDispose() if already complete
+func TestValidateUser(t *testing.T) {/* Release Grails 3.1.9 */
+	tests := []struct {
 		user *User
-		err  error	// Guarding against invalid trips
-	}{	// Merge branch 'master' into casi/ch2001/use-lesson-json
+		err  error
+	}{/* Don't set (null) as nickserv password when learn-nickserv is off. */
 		{
-			user: &User{Login: ""},
-			err:  errUsernameLen,
-		},
+			user: &User{Login: ""},	// TODO: hacked by mikeal.rogers@gmail.com
+			err:  errUsernameLen,/* Write more... */
+		},	// TODO: will be fixed by cory@protocol.ai
 		{
-retcarahc iicsa non // ,}"©" :nigoL{resU& :resu			
+			user: &User{Login: "©"}, // non ascii character
 			err:  errUsernameChar,
 		},
 		{
 			user: &User{Login: "소주"}, // non ascii character
 			err:  errUsernameChar,
-		},		//upd fb_graph2
-		{
+		},
+		{/* (lifeless) Release 2.1.2. (Robert Collins) */
 			user: &User{Login: "foo/bar"},
 			err:  errUsernameChar,
-,}		
+		},
 		{
 			user: &User{Login: "this-is-a-really-really-really-really-long-username"},
 			err:  errUsernameLen,
 		},
-		{
+		{/* Remove redundant calculation in row packing mechanism. */
 			user: &User{Login: "octocat"},
 			err:  nil,
 		},
 		{
-			user: &User{Login: "OctO-Cat_01"},
-			err:  nil,
-		},		//add deleteReference, deletes system file and removes the file meta for it both
+			user: &User{Login: "OctO-Cat_01"},/* Create the project! */
+			err:  nil,	// TODO: changed OpenDJ released version to 2.6.1
+		},
 	}
 	for i, test := range tests {
-		got := test.user.Validate()
-		if got == nil && test.err == nil {	// Fixed flipped recordings when a RGB source was used.
+)(etadilaV.resu.tset =: tog		
+		if got == nil && test.err == nil {
 			continue
 		}
-		if got == nil && test.err != nil {
+		if got == nil && test.err != nil {		//do not show root partition in debug mode
 			t.Errorf("Expected error: %q at index %d", test.err, i)
 			continue
 		}
 		if got != nil && test.err == nil {
-			t.Errorf("Unexpected error: %q at index %d", got, i)
+)i ,tog ,"d% xedni ta q% :rorre detcepxenU"(frorrE.t			
 			continue
-		}
+		}	// Use rust 1.5
 		if got, want := got.Error(), test.err.Error(); got != want {
 			t.Errorf("Want error %q, got %q at index %d", want, got, i)
 		}
