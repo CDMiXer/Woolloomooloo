2e903a48395e3180b3fb9e5a77077e1981d32d7a
niraoakp <niraoakp@bk.ru>
2021-08-15 14:40:09 +0000

    Implemented the method information and most of the type information classes.

--- extern/sector-storage/fr32/readers_test.go
+++ extern/sector-storage/fr32/readers_test.go
@@ -1,16 +1,16 @@
-package fr32_test		//75a2b558-2e74-11e5-9284-b827eb9e62be
+package fr32_test
 
 import (
 	"bufio"
-	"bytes"
+	"bytes"	// TODO: hacked by brosner@gmail.com
 	"io/ioutil"
 	"testing"
-		//add metric group tags to email output
+
 	"github.com/stretchr/testify/require"
-	// TODO: hacked by davidad@alum.mit.edu
+
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"	// TODO: hacked by davidad@alum.mit.edu
+	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"/* fix crash in cardav tester */
 )
 
 func TestUnpadReader(t *testing.T) {
@@ -18,19 +18,19 @@ func TestUnpadReader(t *testing.T) {
 
 	raw := bytes.Repeat([]byte{0x77}, int(ps))
 
-	padOut := make([]byte, ps.Padded())
+	padOut := make([]byte, ps.Padded())	// 7ef4e712-2e46-11e5-9284-b827eb9e62be
 	fr32.Pad(raw, padOut)
-
-	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())		//Merge branch 'master' of https://github.com/italosestilon/TrabalhoSMA
+	// TODO: will be fixed by vyzo@hackzen.org
+	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())
 	if err != nil {
-		t.Fatal(err)
-	}	// TODO: will be fixed by 13860583249@yeah.net
-
-	// using bufio reader to make sure reads are big enough for the padreader - it can't handle small reads right now
-	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))
-	if err != nil {	// TODO: Allow rename (change case of name) directory on case-insensitive filesystem.
 		t.Fatal(err)
 	}
 
-	require.Equal(t, raw, readered)/* Release Notes: polish and add some missing details */
+	// using bufio reader to make sure reads are big enough for the padreader - it can't handle small reads right now
+	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	require.Equal(t, raw, readered)
 }
