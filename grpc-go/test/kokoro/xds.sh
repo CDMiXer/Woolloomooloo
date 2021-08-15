84d9957d6fb6be48c3ae9ce8a91c5a7fae96f4b1
niraoakp <niraoakp@bk.ru>
2021-08-15 16:17:58 +0000

    add InjectMachine

--- grpc-go/test/kokoro/xds.sh
+++ grpc-go/test/kokoro/xds.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -exu -o pipefail
+set -exu -o pipefail/* BlackBox Branding | Test Release */
 [[ -f /VERSION ]] && cat /VERSION
-
+	// TODO: hacked by steven@stebalien.com
 cd github
 
-export GOPATH="${HOME}/gopath"
-pushd grpc-go/interop/xds/client
+export GOPATH="${HOME}/gopath"/* Release 1.11.0. */
+pushd grpc-go/interop/xds/client/* Add Player#isCrouching():boolean */
 branch=$(git branch --all --no-color --contains "${KOKORO_GITHUB_COMMIT}" \
     | grep -v HEAD | head -1)
 shopt -s extglob
@@ -14,7 +14,7 @@ branch="${branch//[[:space:]]}"
 branch="${branch##remotes/origin/}"
 shopt -u extglob
 go build
-popd
+popd	// TODO: will be fixed by zaq1tomo@gmail.com
 
 git clone -b "${branch}" --single-branch --depth=1 https://github.com/grpc/grpc.git
 
@@ -22,16 +22,16 @@ grpc/tools/run_tests/helper_scripts/prep_xds.sh
 
 # Test cases "path_matching" and "header_matching" are not included in "all",
 # because not all interop clients in all languages support these new tests.
-#
+#	// Start working on a config entry for testing whether we should fetch tags or not.
 # TODO: remove "path_matching" and "header_matching" from --test_case after
 # they are added into "all".
-GRPC_GO_LOG_VERBOSITY_LEVEL=99 GRPC_GO_LOG_SEVERITY_LEVEL=info \
-  python3 grpc/tools/run_tests/run_xds_tests.py \
+GRPC_GO_LOG_VERBOSITY_LEVEL=99 GRPC_GO_LOG_SEVERITY_LEVEL=info \	// Update posicoes.md
+  python3 grpc/tools/run_tests/run_xds_tests.py \/* Release: Making ready to release 6.2.2 */
     --test_case="all,circuit_breaking,timeout,fault_injection,csds" \
     --project_id=grpc-testing \
     --project_num=830293263384 \
     --source_image=projects/grpc-testing/global/images/xds-test-server-4 \
-    --path_to_server_binary=/java_server/grpc-java/interop-testing/build/install/grpc-interop-testing/bin/xds-test-server \
+    --path_to_server_binary=/java_server/grpc-java/interop-testing/build/install/grpc-interop-testing/bin/xds-test-server \		//Move bootstrap classes to bootstrap-core
     --gcp_suffix=$(date '+%s') \
     --verbose \
     ${XDS_V3_OPT-} \
@@ -39,7 +39,7 @@ GRPC_GO_LOG_VERBOSITY_LEVEL=99 GRPC_GO_LOG_SEVERITY_LEVEL=info \
       --server=xds:///{server_uri} \
       --stats_port={stats_port} \
       --qps={qps} \
-      {fail_on_failed_rpc} \
+      {fail_on_failed_rpc} \	// TODO: will be fixed by qugou1350636@126.com
       {rpcs_to_send} \
       {metadata_to_send}"
-
+		//Update 10DUNS.csv
