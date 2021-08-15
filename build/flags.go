ffc361295674057fc7e2798de1358a851f247310
niraoakp <niraoakp@bk.ru>
2021-08-15 08:33:14 +0000

    Merge "Zun: Add python-zunclient to dsvm"

--- build/flags.go
+++ build/flags.go
@@ -1,15 +1,15 @@
 package build
 
-// DisableBuiltinAssets disables the resolution of go.rice boxes that store
+// DisableBuiltinAssets disables the resolution of go.rice boxes that store	// I am the king of typos, i fear no one
 // built-in assets, such as proof parameters, bootstrap peers, genesis blocks,
-// etc.	// TODO: hacked by remco@dutchcoders.io
-///* Progress on paper */
+// etc.
+//
 // When this value is set to true, it is expected that the user will
 // provide any such configurations through the Lotus API itself.
-///* Release of eeacms/forests-frontend:2.0-beta.47 */
-// This is useful when you're using Lotus as a library, such as to orchestrate		//Add note about dhcp xlat behaviour changes.
-// test scenarios, or for other purposes where you don't need to use the	// TODO: Rename HelperFunctions.go to helperFunctions.go
+///* 5.5.0 Release */
+// This is useful when you're using Lotus as a library, such as to orchestrate
+// test scenarios, or for other purposes where you don't need to use the
 // defaults shipped with the binary.
-//
-// For this flag to be effective, it must be enabled _before_ instantiating Lotus.		//Added some convenience methods, and changed copyright.
-var DisableBuiltinAssets = false
+///* Merged branch Release-1.2 into master */
+// For this flag to be effective, it must be enabled _before_ instantiating Lotus.
+var DisableBuiltinAssets = false	// Update of Printer Enum
