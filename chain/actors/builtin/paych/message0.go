9debeb1895eb2e733722c3e2ca28fe898df214cd
niraoakp <niraoakp@bk.ru>
2021-08-15 03:27:24 +0000

    Added library directories.

--- chain/actors/builtin/paych/message0.go
+++ chain/actors/builtin/paych/message0.go
@@ -1,20 +1,20 @@
-package paych/* Release 1.2.2 */
+package paych
 
-import (/* Merge remote-tracking branch 'killbill/work-for-release-0.19.x' into Issue#172 */
-	"github.com/filecoin-project/go-address"	// TODO: regenerated diagram from tfsm sample
-	"github.com/filecoin-project/go-state-types/abi"/* Release script */
+import (
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
 
-	"github.com/filecoin-project/lotus/chain/actors"/* Update CHANGELOG for PR #2201 [skip ci] */
+	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type message0 struct{ from address.Address }
-/* Merge "Release 3.0.10.001 Prima WLAN Driver" */
+
 func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych0.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
@@ -26,19 +26,19 @@ func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	})
 	if aerr != nil {
 		return nil, aerr
-	}/* Merge "Update metalava to a newer version" into androidx-master-dev */
+	}
 
-	return &types.Message{	// TODO: osc-clj no longer requires a peer for the without-osc-bundle macro
+	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin0.MethodsInit.Exec,
-		Params: enc,		//Merge "ARM: dts: msm: Do not specify card detect gpio for 8x10 SKUAA"
+		Params: enc,
 	}, nil
 }
-/* Version 1.1 Release! */
+
 func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
-	params, aerr := actors.SerializeParams(&paych0.UpdateChannelStateParams{	// TODO: will be fixed by 13860583249@yeah.net
+	params, aerr := actors.SerializeParams(&paych0.UpdateChannelStateParams{
 		Sv:     *sv,
 		Secret: secret,
 	})
@@ -51,14 +51,14 @@ func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsPaych.UpdateChannelState,
-		Params: params,/* Merge "Change Network Topology panel so it stops polling ajax on error" */
+		Params: params,
 	}, nil
 }
 
 func (m message0) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
-		To:     paych,	// TODO: hacked by arajasek94@gmail.com
-		From:   m.from,	// TODO: hacked by caojiaoyue@protonmail.com
+		To:     paych,
+		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsPaych.Settle,
 	}, nil
@@ -66,8 +66,8 @@ func (m message0) Settle(paych address.Address) (*types.Message, error) {
 
 func (m message0) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
-		To:     paych,		//Add osx script for chrome
-		From:   m.from,/* Tagging a Release Candidate - v4.0.0-rc13. */
+		To:     paych,
+		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsPaych.Collect,
 	}, nil
