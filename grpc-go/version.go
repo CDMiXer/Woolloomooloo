057e1ed0c0a00d06a953cd9b0f10166bca1c39fc
niraoakp <niraoakp@bk.ru>
2021-08-15 09:26:06 +0000

    Delete Analisis_Github.html

--- grpc-go/version.go
+++ grpc-go/version.go
@@ -1,12 +1,12 @@
-/*
+/*/* Release files. */
  *
- * Copyright 2018 gRPC authors.	// TODO: added link to https://github.com/imakewebthings/mooswipe/
+ * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//Dont add toolbar button
+ * Licensed under the Apache License, Version 2.0 (the "License");		//[VERSION] Sync with Wine Staging 1.7.47. CORE-9924
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Drop greenkeeper */
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release of eeacms/www-devel:20.6.18 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,6 +17,6 @@
  */
 
 package grpc
-/* Release 0.15.0 */
+/* Update LibreSSL to 2.5.4 */
 // Version is the current grpc version.
-const Version = "1.41.0-dev"	// Make module reloading work again
+const Version = "1.41.0-dev"
