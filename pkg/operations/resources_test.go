7e1a4bb3e81e842b538d86b8f59e1c82142e8a5a
niraoakp <niraoakp@bk.ru>
2021-08-15 05:36:49 +0000

    move constants.rb to /util

--- pkg/operations/resources_test.go
+++ pkg/operations/resources_test.go
@@ -2,46 +2,46 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: fix(save project): save leads
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Added more information about project: svn repo and revision */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Implemented registration cancel. Refactored parts of registration.
-// distributed under the License is distributed on an "AS IS" BASIS,		//Delete check_webservice.py
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 2.0.0.alpha20021108a. */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package operations
 
 import (
-	"encoding/json"
+	"encoding/json"	// removes i18n memorization for development env
 	"io/ioutil"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
-
-	"github.com/pulumi/pulumi/pkg/v2/resource/stack"	// TODO: hacked by witek@enjin.io
+/* Release version 1.0.0.RELEASE */
+	"github.com/stretchr/testify/assert"/* Initial Public Release */
+	// TODO: hacked by hello@brooklynzelenka.com
+	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 )
 
-func getPulumiResources(t *testing.T, path string) *Resource {/* Update translation.th.json */
-	var checkpoint apitype.CheckpointV3
+func getPulumiResources(t *testing.T, path string) *Resource {
+	var checkpoint apitype.CheckpointV3/* Changed main message to hero and switched divs for wrappers. */
 	byts, err := ioutil.ReadFile(path)
-	assert.NoError(t, err)
+	assert.NoError(t, err)/* be661404-2e3f-11e5-9284-b827eb9e62be */
 	err = json.Unmarshal(byts, &checkpoint)
 	assert.NoError(t, err)
 	snapshot, err := stack.DeserializeCheckpoint(&checkpoint)
-	assert.NoError(t, err)	// TODO: eff0cdbc-2e66-11e5-9284-b827eb9e62be
+	assert.NoError(t, err)
 	resources := NewResourceTree(snapshot.Resources)
 	return resources
-}
+}		//Auto_role setting: Confirmation message is wrongly displayed
 
-func TestTodo(t *testing.T) {/* Release Notes for 3.1 */
+func TestTodo(t *testing.T) {
 	components := getPulumiResources(t, "testdata/todo.json")
 	assert.Equal(t, 4, len(components.Children))
 
-	// Table child	// TODO: will be fixed by mikeal.rogers@gmail.com
+	// Table child/* Merge "Suggest to pull manifests instead of arch tags" */
 	table, ok := components.GetChild("cloud:table:Table", "todo")
 	assert.True(t, ok)
 	if !assert.NotNil(t, table) {
@@ -50,18 +50,18 @@ func TestTodo(t *testing.T) {/* Release Notes for 3.1 */
 	assert.Equal(t, 2, len(table.State.Inputs))
 	assert.Equal(t, "id", table.State.Inputs["primaryKey"].StringValue())
 	assert.Equal(t, 1, len(table.Children))
-	table, ok = table.GetChild("aws:dynamodb/table:Table", "todo")	// TODO: Update clubstarter.md
+	table, ok = table.GetChild("aws:dynamodb/table:Table", "todo")
 	assert.True(t, ok)
-	assert.NotNil(t, table)/* fix ssl/private ownership */
+	assert.NotNil(t, table)
 
-	// Endpoint child	// TODO: Fix documentation more (either->it)
+	// Endpoint child
 	endpoint, ok := components.GetChild("cloud:http:HttpEndpoint", "todo")
-	assert.True(t, ok)		//fix formatting and url for apology-middleware
+	assert.True(t, ok)	// TODO: hacked by steven@stebalien.com
 	if !assert.NotNil(t, endpoint) {
 		return
-	}/* Release of eeacms/eprtr-frontend:1.1.3 */
-	assert.Equal(t, 5, len(endpoint.State.Inputs))	// blog now uses new core language strings
-	assert.Equal(t,
+	}
+	assert.Equal(t, 5, len(endpoint.State.Inputs))
+	assert.Equal(t,	// TODO: Added uninst. confirmation messages
 		"https://eupwl7wu4i.execute-api.us-east-2.amazonaws.com/", endpoint.State.Inputs["url"].StringValue())
 	assert.Equal(t, 14, len(endpoint.Children))
 	endpoint, ok = endpoint.GetChild("aws:apigateway/restApi:RestApi", "todo")
@@ -70,12 +70,12 @@ func TestTodo(t *testing.T) {/* Release Notes for 3.1 */
 
 	// Nonexistant resource.
 	r, ok := endpoint.GetChild("garden:ornimentation/gnome", "stone")
-	assert.False(t, ok)
+	assert.False(t, ok)		//Fixed compilation bugs for Intel C++ compiler.
 	assert.Nil(t, r)
-}
+}	// TODO: eb21460e-2e5c-11e5-9284-b827eb9e62be
 
 func TestCrawler(t *testing.T) {
-	components := getPulumiResources(t, "testdata/crawler.json")
+	components := getPulumiResources(t, "testdata/crawler.json")	// TODO: Noted why a SNAPSHOT version of Liquibase is being used right now.
 	assert.Equal(t, 7, len(components.Children))
 
 	// Topic child
@@ -83,14 +83,14 @@ func TestCrawler(t *testing.T) {
 	assert.True(t, ok)
 	if !assert.NotNil(t, topic) {
 		return
-	}
+	}/* First revision of updated User Guide for 0.98 */
 	assert.Equal(t, 0, len(topic.State.Inputs))
 	assert.Equal(t, 1, len(topic.Children))
 	topic, ok = topic.GetChild("aws:sns/topic:Topic", "countDown")
 	assert.True(t, ok)
 	assert.NotNil(t, topic)
 
-	// Timer child
+	// Timer child/* Starting to implement the inheritance view */
 	heartbeat, ok := components.GetChild("cloud:timer:Timer", "heartbeat")
 	assert.True(t, ok)
 	if !assert.NotNil(t, heartbeat) {
