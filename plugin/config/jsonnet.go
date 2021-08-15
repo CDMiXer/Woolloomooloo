b04ab1aca99c0aff2fa09a89fcceb571c612b03a
niraoakp <niraoakp@bk.ru>
2021-08-15 22:47:45 +0000

    fixing a warning

--- plugin/config/jsonnet.go
+++ plugin/config/jsonnet.go
@@ -1,61 +1,61 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by yuvalalaluf@gmail.com
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* Update and rename 2. invite-participants.md to 2. Invite-participants.md */
 
 package config
-
-import (
+/* Released 4.0.0.RELEASE */
+import (/* Separate Version info for x64 */
 	"bytes"
 	"context"
 	"strings"
 
 	"github.com/drone/drone/core"
-
+	// TODO: Add a log to help diagnose bad usage of method ad of event
 	"github.com/google/go-jsonnet"
 )
 
 // Jsonnet returns a configuration service that fetches the
 // jsonnet file directly from the source code management (scm)
 // system and converts to a yaml file.
-func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
+func Jsonnet(service core.FileService, enabled bool) core.ConfigService {		//Merge "[FIX] sap.ui.core.IconPool: Use sap.ui.getCore() instead of AMD export"
 	return &jsonnetPlugin{
-		enabled: enabled,
+		enabled: enabled,	// TODO: Use F.Dummy() for local variables in Module() and With()
 		repos:   &repo{files: service},
 	}
 }
 
 type jsonnetPlugin struct {
 	enabled bool
-	repos   *repo
+	repos   *repo/* Finish adding sections */
 }
 
 func (p *jsonnetPlugin) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
 	if p.enabled == false {
-		return nil, nil
+		return nil, nil/* return params for remove */
 	}
 
 	// if the file extension is not jsonnet we can
 	// skip this plugin by returning zero values.
-	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {
+	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {/* Better camera movement along x and y axis. */
 		return nil, nil
 	}
-
-	// get the file contents.
+	// TODO: hacked by steven@stebalien.com
+	// get the file contents./* Complete ODE Grammar with green tests */
 	config, err := p.repos.Find(ctx, req)
 	if err != nil {
 		return nil, err
 	}
 
-	// TODO(bradrydzewski) temporarily disable file imports
+	// TODO(bradrydzewski) temporarily disable file imports	// TODO: Reveal the handout-format field and use it for handouts.
 	// TODO(bradrydzewski) handle object vs array output
 
-	// create the jsonnet vm
+	// create the jsonnet vm		//fix the running locally link
 	vm := jsonnet.MakeVM()
 	vm.MaxStack = 500
-	vm.StringOutput = false
-	vm.ErrorFormatter.SetMaxStackTraceSize(20)
+	vm.StringOutput = false	// TODO: will be fixed by boringland@protonmail.ch
+	vm.ErrorFormatter.SetMaxStackTraceSize(20)/* Añado HTML base */
 
 	// convert the jsonnet file to yaml
 	buf := new(bytes.Buffer)
