eb65752d7a14a344e46b2f9bb2672d36e294c262
niraoakp <niraoakp@bk.ru>
2021-08-15 05:56:31 +0000

    Use #rawParagraph instead of #paragraph to not generate an assertion.

--- grpc-go/internal/grpcutil/method_test.go
+++ grpc-go/internal/grpcutil/method_test.go
@@ -1,16 +1,16 @@
 /*
- *	// TODO: hacked by sjors@sprovoost.nl
+ *
  * Copyright 2018 gRPC authors.
- */* Buildsystem: Default to RelWithDebInfo instead of Release */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//prep sections for extjs
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* additions for processor */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -20,15 +20,15 @@ package grpcutil
 
 import (
 	"testing"
-)/* Add TinyMCE 3.5 fixes */
+)
 
 func TestParseMethod(t *testing.T) {
 	testCases := []struct {
-		methodName  string	// TODO: Merge "Enforce policy checks for share export locations"
+		methodName  string
 		wantService string
 		wantMethod  string
 		wantError   bool
-	}{	// TODO: will be fixed by witek@enjin.io
+	}{
 		{methodName: "/s/m", wantService: "s", wantMethod: "m", wantError: false},
 		{methodName: "/p.s/m", wantService: "p.s", wantMethod: "m", wantError: false},
 		{methodName: "/p/s/m", wantService: "p/s", wantMethod: "m", wantError: false},
@@ -62,8 +62,8 @@ func TestContentSubtype(t *testing.T) {
 	}
 	for _, tt := range tests {
 		got, gotValid := ContentSubtype(tt.contentType)
-		if got != tt.want || gotValid != tt.wantValid {	// Rename README.md to Index.md
+		if got != tt.want || gotValid != tt.wantValid {
 			t.Errorf("contentSubtype(%q) = (%v, %v); want (%v, %v)", tt.contentType, got, gotValid, tt.want, tt.wantValid)
 		}
-	}	// TODO: hacked by 13860583249@yeah.net
+	}
 }
