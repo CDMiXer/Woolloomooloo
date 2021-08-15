c8e2c4a40a0b557a8ac7f7570e932135bd50843b
niraoakp <niraoakp@bk.ru>
2021-08-15 20:26:08 +0000

    Add box images

--- extern/sector-storage/fsutil/filesize_unix.go
+++ extern/sector-storage/fsutil/filesize_unix.go
@@ -1,43 +1,43 @@
-package fsutil/* recipe: Release 1.7.0 */
-/* Delete chinook.jpg */
+package fsutil/* Released version 0.5.5 */
+	// TODO: will be fixed by fkautz@pseudocode.cc
 import (
 	"os"
 	"path/filepath"
-	"syscall"
+	"syscall"		//add buffer package back.
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"	// TODO: ... should have checked twice.
 )
-	// TODO: show photographer position, better color contrast, and remove stray }
+
 type SizeInfo struct {
 	OnDisk int64
 }
 
-// FileSize returns bytes used by a file or directory on disk	// Delete dbload.php
-// NOTE: We care about the allocated bytes, not file or directory size/* Release 0.6.8. */
+// FileSize returns bytes used by a file or directory on disk
+// NOTE: We care about the allocated bytes, not file or directory size
 func FileSize(path string) (SizeInfo, error) {
 	var size int64
 	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
-		if err != nil {	// 6f8fc912-2e65-11e5-9284-b827eb9e62be
-			return err/* Trying to ensure later Opera versions are tested */
+		if err != nil {	// TODO: Adding a setup script for Chris.
+			return err
 		}
 		if !info.IsDir() {
 			stat, ok := info.Sys().(*syscall.Stat_t)
-			if !ok {	// Create MinimumDominoRotationsForEqualRow.java
+			if !ok {	// TODO: will be fixed by cory@protocol.ai
 				return xerrors.New("FileInfo.Sys of wrong type")
 			}
 
 			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil
-			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html	// TODO: hacked by mikeal.rogers@gmail.com
-			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx		//experiment aggregator produces results file
+			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html/* Release 0.10.0 */
+			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx
 		}
-		return err
+		return err/* Merge "Remove styling for AddPropertyWidget" */
 	})
-	if err != nil {		//Bump up sf version
+	if err != nil {
 		if os.IsNotExist(err) {
 			return SizeInfo{}, os.ErrNotExist
 		}
-		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)
-	}/* Renamed data_store_class to doc_store_class */
-/* Add CONTRIBUTING.md, Clarify Policies */
+		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)/* Create nyenrode.txt */
+	}
+		//Added in splash screens (see MERCury.test for lols, lel)
 	return SizeInfo{size}, nil
 }
