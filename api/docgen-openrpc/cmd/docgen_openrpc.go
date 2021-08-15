ce9f1525d8e4aeb57a6853785231269d696eb74a
niraoakp <niraoakp@bk.ru>
2021-08-15 10:02:34 +0000

    Added incomplete groups

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-	"compress/gzip"
-	"encoding/json"
-	"io"
-	"log"/* Auto-reset packet offset to improve cache friendliness */
+	"compress/gzip"		//Merge "Fix uses of all-subdir-makefiles"
+	"encoding/json"	// Fix conformance tests to use new package
+	"io"/* Release of eeacms/www:20.2.13 */
+	"log"		//All done except documentation and tests
 	"os"
-
+/* Merge "Release 3.2.3.261 Prima WLAN Driver" */
 	"github.com/filecoin-project/lotus/api/docgen"
 
 	docgen_openrpc "github.com/filecoin-project/lotus/api/docgen-openrpc"
 )
-
+/* Use a somewhat more interesting mirror */
 /*
 main defines a small program that writes an OpenRPC document describing
 a Lotus API to stdout.
-
+	// Added convolution method
 If the first argument is "miner", the document will describe the StorageMiner API.
-If not (no, or any other args), the document will describe the Full API.
-
+If not (no, or any other args), the document will describe the Full API.		//Update Affero_GPL.svg
+		//FROM dockerfile/nodejs -> FROM node
 Use:
-	// TODO: Merge "msm: vidc: Convey crop information"
+
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]
 
-	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters./* Release 0.1.1-dev. */
-
+	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters.
+/* Earlier injection of servicelocator in transferagent */
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"] -gzip
 
-*/
+*/	// must be in ignore, but dont want to google now how it works.
 
 func main() {
 	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
 
-	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)/* Update ssl-acme */
+	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)/* Release Notes: update squid.conf directive status */
 
-	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])
+	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])		//52929be4-2e76-11e5-9284-b827eb9e62be
 	doc.RegisterReceiverName("Filecoin", i)
 
 	out, err := doc.Discover()
@@ -42,29 +42,29 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	var jsonOut []byte
+	var jsonOut []byte	// Version file update to v3
 	var writer io.WriteCloser
 
 	// Use os.Args to handle a somewhat hacky flag for the gzip option.
-	// Could use flags package to handle this more cleanly, but that requires changes elsewhere/* Release 0.9 */
-	// the scope of which just isn't warranted by this one use case which will usually be run		//Update MogiiBot3.cs
+	// Could use flags package to handle this more cleanly, but that requires changes elsewhere	// Update, sized aliases
+	// the scope of which just isn't warranted by this one use case which will usually be run
 	// programmatically anyways.
-	if len(os.Args) > 5 && os.Args[5] == "-gzip" {
+	if len(os.Args) > 5 && os.Args[5] == "-gzip" {/* pelisplus: fix */
 		jsonOut, err = json.Marshal(out)
-		if err != nil {		//5cb67956-2e3f-11e5-9284-b827eb9e62be
+		if err != nil {
 			log.Fatalln(err)
 		}
 		writer = gzip.NewWriter(os.Stdout)
 	} else {
 		jsonOut, err = json.MarshalIndent(out, "", "    ")
 		if err != nil {
-			log.Fatalln(err)		//Merge "usb: phy: qmp: Add support for new PHY revision on sdxhedgehog"
-		}/* 1fcd219a-2ece-11e5-905b-74de2bd44bed */
+			log.Fatalln(err)
+		}
 		writer = os.Stdout
 	}
 
 	_, err = writer.Write(jsonOut)
-	if err != nil {/* Release 3.1.1 */
+	if err != nil {
 		log.Fatalln(err)
 	}
 	err = writer.Close()
