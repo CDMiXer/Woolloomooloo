ad63f0ef66bb5f65167afcd6b51d5322713139ff
niraoakp <niraoakp@bk.ru>
2021-08-15 17:01:01 +0000

    Update equals.js

--- testplans/lotus-soup/testkit/lotus_opts.go
+++ testplans/lotus-soup/testkit/lotus_opts.go
@@ -1,18 +1,18 @@
 package testkit
 
 import (
-	"fmt"
+	"fmt"		//Add boot options to grub file used by USB boot
 
 	"github.com/filecoin-project/lotus/node"
 	"github.com/filecoin-project/lotus/node/config"
 	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
-	"github.com/filecoin-project/lotus/node/repo"
+	"github.com/filecoin-project/lotus/node/repo"		//Merge "API: Add image sizes and default user prefs to meta=siteinfo"
 
 	"github.com/libp2p/go-libp2p-core/peer"
 	ma "github.com/multiformats/go-multiaddr"
-)
+)/* Add Slack to Contributing file */
 
 func withGenesis(gb []byte) node.Option {
 	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))
@@ -24,7 +24,7 @@ func withBootstrapper(ab []byte) node.Option {
 			if ab == nil {
 				return dtypes.BootstrapPeers{}, nil
 			}
-
+	// TODO: Remove charset option from ORM engine since it's not valid
 			a, err := ma.NewMultiaddrBytes(ab)
 			if err != nil {
 				return nil, err
@@ -50,7 +50,7 @@ func withListenAddress(ip string) node.Option {
 	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
 	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
 }
-
+		//cd1d5d0a-2e6b-11e5-9284-b827eb9e62be
 func withMinerListenAddress(ip string) node.Option {
 	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
 	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
