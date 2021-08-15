4015c49f64fd69fe3fedae95fd9ecff6782ea829
niraoakp <niraoakp@bk.ru>
2021-08-15 04:58:14 +0000

    Accept node 0.12 as engine

--- operator/manager/rpc/server_test.go
+++ operator/manager/rpc/server_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by vyzo@hackzen.org
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Fix issue 69. */
-/* fb126a84-2e52-11e5-9284-b827eb9e62be */
+// +build !oss	// TODO: hacked by peterke@gmail.com
+	// TODO: will be fixed by cory@protocol.ai
 package rpc
