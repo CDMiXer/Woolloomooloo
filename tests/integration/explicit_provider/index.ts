82f0fc884df526003031b1d2f8b06de9ba645790
niraoakp <niraoakp@bk.ru>
2021-08-15 12:28:31 +0000

    Merge "Use oslo.service for launching sahara"

--- tests/integration/explicit_provider/index.ts
+++ tests/integration/explicit_provider/index.ts
@@ -6,34 +6,34 @@ class DynamicProvider extends pulumi.ProviderResource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
         super("pulumi-nodejs", name, {}, opts);
     }
-}
+}/* Released 10.3.0 */
 
 class Provider implements pulumi.dynamic.ResourceProvider {
-    public static instance = new Provider();
-
+    public static instance = new Provider();	// corrected enum type references to lowercase
+		//Singleton implementation moved to utils namespace.
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
-		//Remove obsolete enum34 dep
+	// ScriptsWindow: got rid of list view. won't be attached to simulation any longer.
     constructor() {
         this.create = async (inputs: any) => {
             return {
                 id: "0",
                 outs: undefined,
-            };/* added system.c */
-        };
+            };
+        };		//6724bdfc-2e54-11e5-9284-b827eb9e62be
     }
-}		//Merge "Remove _router_exists  method"
+}
 
-class Resource extends pulumi.dynamic.Resource {
+class Resource extends pulumi.dynamic.Resource {/* Merge "Release 4.0.10.71 QCACLD WLAN Driver" */
     constructor(name: string, provider?: pulumi.ProviderResource) {
-        super(Provider.instance, name, {}, { provider: provider});		//Issue 215: fixed issue with startup when no config is available
+        super(Provider.instance, name, {}, { provider: provider});		//Added data update listening structure
     }
 }
 
 // Create a resource using the default dynamic provider instance.
 let a = new Resource("a");
 
-// Create an explicit instance of the dynamic provider.
+// Create an explicit instance of the dynamic provider./* add compile restrictions */
 let p = new DynamicProvider("p");
 
 // Create a resource using the explicit dynamic provider instance.
-let b = new Resource("b", p);
+let b = new Resource("b", p);	// rudimentary API for Autoscale
