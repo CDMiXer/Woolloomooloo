b801456ae97a6f54911a8988ed85cdaee9bc89fe
niraoakp <niraoakp@bk.ru>
2021-08-15 22:16:30 +0000

    updating about me bio

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -1,8 +1,8 @@
 package dtypes
 
-import "github.com/libp2p/go-libp2p-core/peer"		//27f73bec-2e71-11e5-9284-b827eb9e62be
-	// TODO: hacked by m-ou.se@m-ou.se
-type BootstrapPeers []peer.AddrInfo/* Updated readme with basic examples */
+import "github.com/libp2p/go-libp2p-core/peer"
+
+type BootstrapPeers []peer.AddrInfo
 type DrandBootstrap []peer.AddrInfo
 
 type Bootstrapper bool
