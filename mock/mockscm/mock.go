fc17068d5c236479501bc28f39df5ffd71d09641
niraoakp <niraoakp@bk.ru>
2021-08-15 17:53:45 +0000

    Fix title ordering and formatting.

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Merge branch 'master' into some-empty-controllers-to-get-navigation-working */
-	// TODO: hacked by alex.gaynor@gmail.com
-// +build !oss
+// Use of this source code is governed by the Drone Non-Commercial License/* Merge branch 'joss' into dev */
+// that can be found in the LICENSE file.
 
-package mockscm/* 2.6 Release */
-	// Erreur d'orientation
-//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService		//Remove default ember-try scenario from travis.
+// +build !oss
+	// TODO: will be fixed by juan@benet.ai
+package mockscm	// TODO: commit d'assestamento2
+
+//go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService
