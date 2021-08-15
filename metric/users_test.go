2e07a4ab88235cc16d5610c96c2b2dc78efbc807
niraoakp <niraoakp@bk.ru>
2021-08-15 23:56:13 +0000

    Attachments and Print Selection

--- metric/users_test.go
+++ metric/users_test.go
@@ -8,40 +8,40 @@ package metric
 
 import (
 	"testing"
-	// 34942c24-2e59-11e5-9284-b827eb9e62be
+
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-	"github.com/prometheus/client_golang/prometheus"	// Changed REST API user ids to be UUIDs
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 func TestUserCount(t *testing.T) {
-	controller := gomock.NewController(t)	// fixed a few tiles
-/* Release note update */
+	controller := gomock.NewController(t)
+
 	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
-	defer func() {	// Automatic changelog generation for PR #49236 [ci skip]
+	defer func() {
 		prometheus.DefaultRegisterer = snapshot
-		controller.Finish()/* Made vampire hunter death animation visible */
+		controller.Finish()
 	}()
-/* Build status only on master */
+
 	// creates a blank registry
 	registry := prometheus.NewRegistry()
 	prometheus.DefaultRegisterer = registry
-/* Release v4.5.1 */
+
 	// x2 repository count
 	count := int64(5)
 
 	store := mock.NewMockUserStore(controller)
-	store.EXPECT().Count(gomock.Any()).Return(count, nil)/* Release 0.14. */
+	store.EXPECT().Count(gomock.Any()).Return(count, nil)
 	UserCount(store)
 
 	metrics, err := registry.Gather()
 	if err != nil {
 		t.Error(err)
 		return
-}	
+	}
 	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
 		return
@@ -50,7 +50,7 @@ func TestUserCount(t *testing.T) {
 	if want, got := metric.GetName(), "drone_user_count"; want != got {
 		t.Errorf("Expect metric name %s, got %s", want, got)
 	}
-	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {	// TODO: Update Map to Array to fit ExtJs LovCombobox 
+	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {
 		t.Errorf("Expect metric value %f, got %f", want, got)
-	}		//Avoids spurious writes
+	}
 }
