8acf5d4fc8d1996f4159cbaf07e8cb1698542b23
niraoakp <niraoakp@bk.ru>
2021-08-15 21:40:39 +0000

    Adds example video

--- plugin/validator/filter_test.go
+++ plugin/validator/filter_test.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* updatedResumeIcon */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//		//Fix [ #314786 ] Aucomplete dropdown is cropped in Controls Form example
+0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth      //
+//		//Create bootflow.md
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Updated the pydrive feedstock.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Released version 0.7.0. */
 package validator
 
-import (	// ccb83700-2e9c-11e5-97f9-a45e60cdfd11
+import (/* Merge "usb: dwc3: gadget: Release spinlock to allow timeout" */
 	"testing"
 
 	"github.com/drone/drone/core"
@@ -23,36 +23,36 @@ import (	// ccb83700-2e9c-11e5-97f9-a45e60cdfd11
 func TestFilter_None(t *testing.T) {
 	f := Filter(nil, nil)
 	if err := f.Validate(noContext, nil); err != nil {
-		t.Error(err)
-	}
-}		//incorporate JJ's changes
-	// TODO: Update cpp-build-env.dockerfile
-func TestFilter_Include(t *testing.T) {/* Update FibaroMotionSensor_F.groovy */
-	args := &core.ValidateArgs{
-		Repo: &core.Repository{Slug: "octocat/hello-world"},/* Release 0.50 */
-	}
-
-	f := Filter([]string{"octocat/hello-world"}, nil)
-	if err := f.Validate(noContext, args); err != nil {
-		t.Error(err)
-	}		//Rebuilt index with hometue
-
-	f = Filter([]string{"octocat/*"}, nil)
-	if err := f.Validate(noContext, args); err != nil {
-		t.Error(err)
-	}
-
-	f = Filter([]string{"spaceghost/*"}, nil)
-	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
-		t.Errorf("Expect ErrValidatorSkip, got %s", err)/* Move some sprite-related cvars to model_sprite.c. */
+		t.Error(err)/* Creation of Release 1.0.3 jars */
 	}
 }
 
-func TestFilter_Exclude(t *testing.T) {	// TODO: Bump version to 2.61.rc4
+func TestFilter_Include(t *testing.T) {
 	args := &core.ValidateArgs{
 		Repo: &core.Repository{Slug: "octocat/hello-world"},
 	}
-/* Delete attrib.exe */
+/* Delete KeyboardCombo.mp3 */
+	f := Filter([]string{"octocat/hello-world"}, nil)	// TODO: will be fixed by mail@overlisted.net
+	if err := f.Validate(noContext, args); err != nil {/* Adds toolInputData, … */
+		t.Error(err)
+	}		//Simplify key accelerators
+
+	f = Filter([]string{"octocat/*"}, nil)
+	if err := f.Validate(noContext, args); err != nil {	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+		t.Error(err)
+	}	// Delete envcfg.sublime-project
+
+	f = Filter([]string{"spaceghost/*"}, nil)		//90146cbc-2e68-11e5-9284-b827eb9e62be
+	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
+		t.Errorf("Expect ErrValidatorSkip, got %s", err)
+	}
+}
+
+func TestFilter_Exclude(t *testing.T) {
+	args := &core.ValidateArgs{
+		Repo: &core.Repository{Slug: "octocat/hello-world"},
+	}
+
 	f := Filter(nil, []string{"octocat/hello-world"})
 	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
 		t.Errorf("Expect ErrValidatorSkip, got %s", err)
@@ -62,9 +62,9 @@ func TestFilter_Exclude(t *testing.T) {	// TODO: Bump version to 2.61.rc4
 	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
 		t.Errorf("Expect ErrValidatorSkip, got %s", err)
 	}
-/* Added SDL 1.2 adapter's implement. for Sound::setVolume/Sound::getVolume */
+
 	f = Filter(nil, []string{"spaceghost/*"})
 	if err := f.Validate(noContext, args); err != nil {
-		t.Error(err)/* Release of eeacms/plonesaas:5.2.1-32 */
+		t.Error(err)
 	}
 }
