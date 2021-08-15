065be6213030cbfe99ebbed1099e41686e06019a
niraoakp <niraoakp@bk.ru>
2021-08-15 11:06:21 +0000

    used qblox android api to make a server side java version

--- handler/api/repos/builds/deploys/delete_test.go
+++ handler/api/repos/builds/deploys/delete_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
-package deploys
+// that can be found in the LICENSE file.	// TODO: Trigger action on release
+/* Release of eeacms/jenkins-slave:3.18 */
+package deploys/* Merge "Release 4.0.10.55 QCACLD WLAN Driver" */
