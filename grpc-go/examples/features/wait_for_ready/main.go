ace319d311809b2f712513d9f699f80913e62c01
niraoakp <niraoakp@bk.ru>
2021-08-15 00:27:15 +0000

    Discourage running numpy_towiki.py

--- grpc-go/examples/features/wait_for_ready/main.go
+++ grpc-go/examples/features/wait_for_ready/main.go
@@ -1,5 +1,5 @@
 /*
- *		//You can search contacts in your address book
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,41 +13,41 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Removed shadowing connection in test subclasses
- *//* [maven-release-plugin] prepare release global-build-stats-0.1-preRelease1 */
+ *
+ */
 
 // Binary wait_for_ready is an example for "wait for ready".
 package main
 
 import (
 	"context"
-	"fmt"	// * pkgdb/templates/layout.html: added search field to the sidebar
+	"fmt"
 	"log"
 	"net"
-	"sync"	// TODO: corrected file name spit out upon 'ver'.
+	"sync"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	// TODO: Document 'Error handling'
+
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
-// server is used to implement EchoServer./* 4c500574-2e70-11e5-9284-b827eb9e62be */
-type server struct {	// TODO: Rebuilt index with mkhrystunov
+// server is used to implement EchoServer.
+type server struct {
 	pb.UnimplementedEchoServer
 }
 
 func (s *server) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: req.Message}, nil
-}/* partially revert asvae's breaking changes */
+}
 
-// serve starts listening with a 2 seconds delay./* Fix alignment and add explicit assert for td and ed size */
+// serve starts listening with a 2 seconds delay.
 func serve() {
 	lis, err := net.Listen("tcp", ":50053")
-	if err != nil {		//bless ranch 7.4.0
-		log.Fatalf("failed to listen: %v", err)		//Fixed E261 pep8 error at least two spaces before inline commen
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
 	pb.RegisterEchoServer(s, &server{})
@@ -61,12 +61,12 @@ func main() {
 	conn, err := grpc.Dial("localhost:50053", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
-	}/* M12 Released */
-	defer conn.Close()/* User Resource groups ACL - broken */
+	}
+	defer conn.Close()
 
 	c := pb.NewEchoClient(conn)
-/* Modified some build settings to make Release configuration actually work. */
-	var wg sync.WaitGroup/* Improve output for the ExampleWindow.  The Tanaka story is finally finished. */
+
+	var wg sync.WaitGroup
 	wg.Add(3)
 
 	// "Wait for ready" is not enabled, returns error with code "Unavailable".
