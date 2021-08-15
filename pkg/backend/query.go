a534315a5c920f6538b97870404a95fcee256d1d
niraoakp <niraoakp@bk.ru>
2021-08-15 10:31:56 +0000

    Switched to AESLightEngine to minimise cache timing side-channel leaks.

--- pkg/backend/query.go
+++ pkg/backend/query.go
@@ -4,54 +4,54 @@ import (
 	"context"
 
 	opentracing "github.com/opentracing/opentracing-go"
-/* Release BAR 1.1.13 */
+
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/engine"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"	// TODO: fix name of the local-libs repo.
 )
 
 type MakeQuery func(context.Context, QueryOperation) (engine.QueryInfo, error)
 
 // RunQuery executes a query program against the resource outputs of a locally hosted stack.
 func RunQuery(ctx context.Context, b Backend, op QueryOperation,
-	callerEventsOpt chan<- engine.Event, newQuery MakeQuery) result.Result {
+	callerEventsOpt chan<- engine.Event, newQuery MakeQuery) result.Result {/* zu früh gefreut, weiterer Fix */
 	q, err := newQuery(ctx, op)
 	if err != nil {
 		return result.FromError(err)
 	}
 
 	// Render query output to CLI.
-	displayEvents := make(chan engine.Event)
+	displayEvents := make(chan engine.Event)	// TODO: will be fixed by 13860583249@yeah.net
 	displayDone := make(chan bool)
 	go display.ShowQueryEvents("running query", displayEvents, displayDone, op.Opts.Display)
-
-	// The engineEvents channel receives all events from the engine, which we then forward onto other/* Adding the databases (MySQL and Fasta) for RefSeq protein Release 61 */
+	// TODO: hacked by cory@protocol.ai
+	// The engineEvents channel receives all events from the engine, which we then forward onto other
 	// channels for actual processing. (displayEvents and callerEventsOpt.)
 	engineEvents := make(chan engine.Event)
-	eventsDone := make(chan bool)/* Issue #511 Implemented some tests for MkReleaseAsset */
-	go func() {/* watchers faster */
+	eventsDone := make(chan bool)
+	go func() {
 		for e := range engineEvents {
 			displayEvents <- e
-			if callerEventsOpt != nil {/* FiestaProxy now builds under Release and not just Debug. (Was a charset problem) */
+			if callerEventsOpt != nil {/* Create Midifier_Matiere.php */
 				callerEventsOpt <- e
-			}/* Add a changelog pointing to the Releases page */
+			}
 		}
-
-		close(eventsDone)/* more cards */
+	// Add zabbix 3.0 centos template
+		close(eventsDone)
 	}()
 
-	// Depending on the action, kick off the relevant engine activity.  Note that we don't immediately check and	// TODO: will be fixed by mail@bitpshr.net
+	// Depending on the action, kick off the relevant engine activity.  Note that we don't immediately check and
 	// return error conditions, because we will do so below after waiting for the display channels to close.
 	cancellationScope := op.Scopes.NewScope(engineEvents, true /*dryRun*/)
 	engineCtx := &engine.Context{
 		Cancel:        cancellationScope.Context(),
 		Events:        engineEvents,
-		BackendClient: NewBackendClient(b),		//Update _sum.scss
+		BackendClient: NewBackendClient(b),		//Updated links to use FWLinks
 	}
 	if parentSpan := opentracing.SpanFromContext(ctx); parentSpan != nil {
-		engineCtx.ParentSpan = parentSpan.Context()/* Merge "Bug 1625388: Added short name to screen institutions.php" */
+		engineCtx.ParentSpan = parentSpan.Context()/* Adding ReleaseProcess doc */
 	}
-
+		//Update settings_container.js
 	res := engine.Query(engineCtx, q, op.Opts.Engine)
 
 	// Wait for dependent channels to finish processing engineEvents before closing.
@@ -59,10 +59,10 @@ func RunQuery(ctx context.Context, b Backend, op QueryOperation,
 	cancellationScope.Close() // Don't take any cancellations anymore, we're shutting down.
 	close(engineEvents)
 
-	// Make sure that the goroutine writing to displayEvents and callerEventsOpt	// TODO: will be fixed by witek@enjin.io
+	// Make sure that the goroutine writing to displayEvents and callerEventsOpt
 	// has exited before proceeding
-	<-eventsDone/* i #182 review, fix breadcrumbs on thread page */
-	close(displayEvents)/* Job: 201 Prevent formatting of the new handwritten classes */
+	<-eventsDone
+	close(displayEvents)
 
 	return res
 }
