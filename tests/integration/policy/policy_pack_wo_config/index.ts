fd747434d4022e434a4e7534bc5aa65d93e20cda
niraoakp <niraoakp@bk.ru>
2021-08-15 23:19:18 +0000

    update the md

--- tests/integration/policy/policy_pack_wo_config/index.ts
+++ tests/integration/policy/policy_pack_wo_config/index.ts
@@ -1,22 +1,22 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-/* transplant: use ui out descriptor when calling util.system */
+
 import * as policy from "@pulumi/policy";
 
-const packName = process.env.TEST_POLICY_PACK;/* py_string.js : fix bug in string.split() */
-/* Extra instance for Outputable on 5-tuples */
+const packName = process.env.TEST_POLICY_PACK;
+
 if (!packName) {
-    console.log("no policy name provided");/* FIXED small GFX bug */
-    process.exit(-1);/* Merge "net: rmnet_data: Stop adding pad bytes for MAPv3 uplink packets" */
-/* Updated Changelog and Readme for 1.01 Release */
+    console.log("no policy name provided");
+    process.exit(-1);
+
 } else {
     const policies = new policy.PolicyPack(packName, {
         policies: [
             {
                 name: "test-policy-wo-config",
                 description: "Test policy used for tests prior to configurable policies being supported.",
-                enforcementLevel: "mandatory",/* Update ReleaseCandidate_2_ReleaseNotes.md */
+                enforcementLevel: "mandatory",
                 validateResource: (args, reportViolation) => {},
             },
         ],
-    });		//b0f8f050-2e4b-11e5-9284-b827eb9e62be
-}	// Merge "Omit "hatnotes" property from the lead response if no hatnotes are found"
+    });
+}
