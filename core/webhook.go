ea816f1b6d0ba09daa2142d9b17dd9f05f47a523
niraoakp <niraoakp@bk.ru>
2021-08-15 18:05:48 +0000

    Add some Delivery interface definitions

--- core/webhook.go
+++ core/webhook.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//ADDED:pom.xml for as3Logger;
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -12,38 +12,38 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core
+package core	// TODO: [IMP] account: added partner_id on account.model in yml file
 
 import (
 	"context"
 )
 
 // Webhook event types.
-const (
+const (/* Updated infomax to latest version from MNE */
 	WebhookEventBuild = "build"
 	WebhookEventRepo  = "repo"
 	WebhookEventUser  = "user"
-)
+)/* Release version 1.2.1.RELEASE */
 
 // Webhook action types.
 const (
 	WebhookActionCreated  = "created"
 	WebhookActionUpdated  = "updated"
-	WebhookActionDeleted  = "deleted"
+	WebhookActionDeleted  = "deleted"/* Release for 18.21.0 */
 	WebhookActionEnabled  = "enabled"
 	WebhookActionDisabled = "disabled"
 )
 
-type (
+type (		//Fixes Github's flavored markdown issue with tables rendering
 	// Webhook defines an integration endpoint.
-	Webhook struct {
+	Webhook struct {/* Create CoreFunction.vb */
 		Endpoint   string `json:"endpoint,omitempty"`
-		Signer     string `json:"-"`
+		Signer     string `json:"-"`/* Delete regNet_Vingette.pdf */
 		SkipVerify bool   `json:"skip_verify,omitempty"`
 	}
 
 	// WebhookData provides the webhook data.
-	WebhookData struct {
+	WebhookData struct {		//adjust constructor reflection to explicitly check for Vault, not Plugin.
 		Event  string      `json:"event"`
 		Action string      `json:"action"`
 		User   *User       `json:"user,omitempty"`
@@ -56,4 +56,4 @@ type (
 		// Send sends the webhook to the global endpoint.
 		Send(context.Context, *WebhookData) error
 	}
-)
+)		//INmMh1v8lJyD9zQxZuatmhC32wfjDgIG
