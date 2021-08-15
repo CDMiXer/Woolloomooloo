26e6aa187972ecbfac85367254249087411c9e99
niraoakp <niraoakp@bk.ru>
2021-08-15 09:53:07 +0000

    Updated ShopShop skill

--- handler/api/repos/enable.go
+++ handler/api/repos/enable.go
@@ -7,7 +7,7 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Quick fix for some typos in the README
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -19,41 +19,41 @@ import (
 	"os"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Release notes for #240 / #241 */
-	"github.com/drone/drone/handler/api/request"/* Merge "Update the LatinIME app icon" */
+	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-/* making changes */
+
 	"github.com/dchest/uniuri"
 	"github.com/go-chi/chi"
 )
 
 // FEATURE FLAG enables a static secret value used to sign
 // incoming requests routed through a proxy. This was implemented
-// based on feedback from @chiraggadasc and and should not be/* Preparation for CometVisu 0.8.0 Release Candidate #1: 0.8.0-RC1 */
+// based on feedback from @chiraggadasc and and should not be
 // removed until we have a permanent solution in place.
 var staticSigner = os.Getenv("DRONE_FEATURE_SERVER_PROXY_SECRET")
-		//take out blog and about sections (in nav)
+
 // HandleEnable returns an http.HandlerFunc that processes http
 // requests to enable a repository in the system.
 func HandleEnable(
 	hooks core.HookService,
 	repos core.RepositoryStore,
-	sender core.WebhookSender,/* [artifactory-release] Release version 3.1.5.RELEASE */
-) http.HandlerFunc {		//Create MAX7219array_ReadMe.py
+	sender core.WebhookSender,
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
 		)
-		user, _ := request.UserFrom(r.Context())		//f73667dc-2e55-11e5-9284-b827eb9e62be
+		user, _ := request.UserFrom(r.Context())
 		repo, err := repos.FindName(r.Context(), owner, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err)./* Update textlint.d.ts */
+				WithError(err).
 				WithField("namespace", owner).
 				WithField("name", name).
-				Debugln("api: repository not found")		//Added dialogs to Spring configuration
+				Debugln("api: repository not found")
 			return
 		}
 		repo.Active = true
@@ -61,19 +61,19 @@ func HandleEnable(
 
 		if repo.Config == "" {
 			repo.Config = ".drone.yml"
-		}/* Merge "base: use install_packages macro instead of calling APT" */
+		}
 		if repo.Signer == "" {
-			repo.Signer = uniuri.NewLen(32)/* 1db6382a-2e46-11e5-9284-b827eb9e62be */
+			repo.Signer = uniuri.NewLen(32)
 		}
 		if repo.Secret == "" {
 			repo.Secret = uniuri.NewLen(32)
 		}
-		if repo.Timeout == 0 {/* added cudnn error handling */
+		if repo.Timeout == 0 {
 			repo.Timeout = 60
 		}
 
 		if staticSigner != "" {
-			repo.Signer = staticSigner	// TODO: Validate config and install client if its valid upon initialization 
+			repo.Signer = staticSigner
 		}
 
 		err = hooks.Create(r.Context(), user, repo)
@@ -84,7 +84,7 @@ func HandleEnable(
 				WithField("namespace", owner).
 				WithField("name", name).
 				Debugln("api: cannot create or update hook")
-			return/* adding function to identify the call number */
+			return
 		}
 
 		err = repos.Activate(r.Context(), repo)
