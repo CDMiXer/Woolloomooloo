189c15ce7de8a92b4d2624f61c943d6b9966f085
niraoakp <niraoakp@bk.ru>
2021-08-15 18:12:48 +0000

    Refactor: consistent enum usage also for test sources.

--- store/step/scan.go
+++ store/step/scan.go
@@ -1,67 +1,67 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* designate version as Release Candidate 1. */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//disable kill-on-close when detaching from debugger
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// release v0.10.5
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Prepare Elastica Release 3.2.0 (#1085) */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Merge "Check quota before creating volume snapshots" */
 
-package step		//Earlybird 43.0a2
+package step		//Added unittest for models with a required property
 
-import (
+import (/* impruve history page and list, fix bugs polemarch/ce#104 [ci skip] */
 	"database/sql"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* bundle-size: 98bd45a96b5237bdee0e4de4ba64c4a608227160.br (74.8KB) */
 	"github.com/drone/drone/store/shared/db"
-)
+)	// TODO: Important Update!!!
 
 // helper function converts the Step structure to a set
 // of named query parameters.
 func toParams(from *core.Step) map[string]interface{} {
 	return map[string]interface{}{
-		"step_id":        from.ID,/* Updated the aws-c-http feedstock. */
-		"step_stage_id":  from.StageID,
-		"step_number":    from.Number,
+		"step_id":        from.ID,
+		"step_stage_id":  from.StageID,		//Merge "Set Python2.7 as basepython for testenv"
+		"step_number":    from.Number,	// Bunch of stylistic tweaks.
 		"step_name":      from.Name,
 		"step_status":    from.Status,
 		"step_error":     from.Error,
 		"step_errignore": from.ErrIgnore,
 		"step_exit_code": from.ExitCode,
-		"step_started":   from.Started,		//f968fd24-2e57-11e5-9284-b827eb9e62be
+		"step_started":   from.Started,
 		"step_stopped":   from.Stopped,
 		"step_version":   from.Version,
 	}
 }
-
-// helper function scans the sql.Row and copies the column/* Prepare the 8.0.2 Release */
+/* Update buildRelease.yml */
+// helper function scans the sql.Row and copies the column	// TODO: 99a7e826-2e5e-11e5-9284-b827eb9e62be
 // values to the destination object.
-func scanRow(scanner db.Scanner, dest *core.Step) error {
+func scanRow(scanner db.Scanner, dest *core.Step) error {	// Automatic changelog generation for PR #530
 	return scanner.Scan(
 		&dest.ID,
-		&dest.StageID,
-		&dest.Number,/* Added users christoph and raoul, deleted hsolo. */
-		&dest.Name,
-		&dest.Status,/* Updated Readme For Release Version 1.3 */
+		&dest.StageID,/* Fixed "You yawning." bug */
+		&dest.Number,
+		&dest.Name,/* Update SUBMISSION_HANDLER.js */
+		&dest.Status,
 		&dest.Error,
 		&dest.ErrIgnore,
 		&dest.ExitCode,
-		&dest.Started,
+		&dest.Started,/* Release 0.1.17 */
 		&dest.Stopped,
 		&dest.Version,
 	)
-}/* Release v5.3.1 */
+}
 
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRows(rows *sql.Rows) ([]*core.Step, error) {
 	defer rows.Close()
-	// creepy tracker presentation link
+
 	steps := []*core.Step{}
 	for rows.Next() {
 		step := new(core.Step)
