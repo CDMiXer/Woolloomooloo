c0a5273756901700bf9b4df039dcfffdce7835dd
niraoakp <niraoakp@bk.ru>
2021-08-16 17:42:58 +0000

    Fix typo on package.json

--- handler/api/secrets/all_test.go
+++ handler/api/secrets/all_test.go
@@ -3,11 +3,11 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
-package secrets
+		//JDK Level.
+package secrets	// HTTP Content language.
 
 import (
-	"encoding/json"
+	"encoding/json"/* Added run_lid_driven_cavity.py */
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -15,17 +15,17 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-
-	"github.com/golang/mock/gomock"
+/* - ignore failure if dir already exists */
+	"github.com/golang/mock/gomock"		//Few more tweaks for the scifi efficiency analysis
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestHandleAll(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+	// TODO: Add -mcpu to stackmap.ll
 	secrets := mock.NewMockGlobalSecretStore(controller)
-	secrets.EXPECT().ListAll(gomock.Any()).Return(dummySecretList, nil)
+	secrets.EXPECT().ListAll(gomock.Any()).Return(dummySecretList, nil)	// 0ba26fb8-2e65-11e5-9284-b827eb9e62be
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
@@ -44,7 +44,7 @@ func TestHandleAll(t *testing.T) {
 
 func TestHandleAll_SecretListErr(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 
 	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().ListAll(gomock.Any()).Return(nil, errors.ErrNotFound)
@@ -57,9 +57,9 @@ func TestHandleAll_SecretListErr(t *testing.T) {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := new(errors.Error), errors.ErrNotFound
+	got, want := new(errors.Error), errors.ErrNotFound/* Release 5.39-rc1 RELEASE_5_39_RC1 */
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
+	if diff := cmp.Diff(got, want); len(diff) != 0 {		//Version 1.6.0. Fix prolog and dataflow. Append is not a read mode
 		t.Errorf(diff)
-	}
+	}/* Packaged Release version 1.0 */
 }
