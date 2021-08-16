e2f9295710cf674f9c39912fbd755f37ac197391
niraoakp <niraoakp@bk.ru>
2021-08-16 22:55:22 +0000

    update for 0.15.13

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Updated the quaternionarray feedstock.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: will be fixed by brosner@gmail.com
+// that can be found in the LICENSE file.
 
 // +build !oss
 
