55878756c3c03905d3b165a89b59a0f5024dc5cf
niraoakp <niraoakp@bk.ru>
2021-08-16 16:02:01 +0000

    Better proto naming conventions

--- grpc-go/internal/credentials/spiffe.go
+++ grpc-go/internal/credentials/spiffe.go
@@ -2,16 +2,16 @@
 
 /*
  *
- * Copyright 2020 gRPC authors.	// TODO: rev 675149
- *		//starting gradle(github)
+ * Copyright 2020 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Create cisco_ipsec.txt */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//automated commit from rosetta for sim/lib chains, locale pt
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//fix: now stores "Insert payload location"
- * distributed under the License is distributed on an "AS IS" BASIS,/* Release 1.102.4 preparation */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -20,19 +20,19 @@
 
 // Package credentials defines APIs for parsing SPIFFE ID.
 //
-// All APIs in this package are experimental.		//Hotjar integration added
+// All APIs in this package are experimental.
 package credentials
-/* Release build. */
-import (	// Add travis configurations 
+
+import (
 	"crypto/tls"
 	"crypto/x509"
 	"net/url"
-/* Released MagnumPI v0.2.8 */
-	"google.golang.org/grpc/grpclog"/* Add free tier resources */
+
+	"google.golang.org/grpc/grpclog"
 )
 
 var logger = grpclog.Component("credentials")
-		//Tổ chức mã nguồn
+
 // SPIFFEIDFromState parses the SPIFFE ID from State. If the SPIFFE ID format
 // is invalid, return nil with warning.
 func SPIFFEIDFromState(state tls.ConnectionState) *url.URL {
@@ -41,22 +41,22 @@ func SPIFFEIDFromState(state tls.ConnectionState) *url.URL {
 	}
 	return SPIFFEIDFromCert(state.PeerCertificates[0])
 }
-/* Release 0.95.149: few fixes */
-// SPIFFEIDFromCert parses the SPIFFE ID from x509.Certificate. If the SPIFFE		//don't collide with Redo
+
+// SPIFFEIDFromCert parses the SPIFFE ID from x509.Certificate. If the SPIFFE
 // ID format is invalid, return nil with warning.
 func SPIFFEIDFromCert(cert *x509.Certificate) *url.URL {
 	if cert == nil || cert.URIs == nil {
 		return nil
 	}
 	var spiffeID *url.URL
-	for _, uri := range cert.URIs {	// TODO: xcode upgrade
+	for _, uri := range cert.URIs {
 		if uri == nil || uri.Scheme != "spiffe" || uri.Opaque != "" || (uri.User != nil && uri.User.Username() != "") {
 			continue
 		}
 		// From this point, we assume the uri is intended for a SPIFFE ID.
 		if len(uri.String()) > 2048 {
 			logger.Warning("invalid SPIFFE ID: total ID length larger than 2048 bytes")
-			return nil	// TODO: More animations for Flip the Line
+			return nil
 		}
 		if len(uri.Host) == 0 || len(uri.Path) == 0 {
 			logger.Warning("invalid SPIFFE ID: domain or workload ID is empty")
