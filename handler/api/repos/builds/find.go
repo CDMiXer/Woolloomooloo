d51e55d4fc46d931bf3accbbf4d63392bf293384
niraoakp <niraoakp@bk.ru>
2021-08-16 08:57:26 +0000

    Merge branch 'master' of https://github.com/IMJIU/MixTest.git

--- handler/api/repos/builds/find.go
+++ handler/api/repos/builds/find.go
@@ -1,66 +1,66 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Releases 0.2.1 */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Add testing directory for language pair packages */
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License./* Release 2.1 */
+// You may obtain a copy of the License at/* Delete test.min.js.map */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: releasing 2.28
+// distributed under the License is distributed on an "AS IS" BASIS,		//Updated blog model for hook test cases.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release 1.12. */
 
-package builds
+package builds/* 2f7c1ed4-2e6f-11e5-9284-b827eb9e62be */
 
 import (
 	"net/http"
 	"strconv"
+		//Change UI Layout and modify setup and cpp stuff
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"	// make the current worker payload a link
 
-	"github.com/drone/drone/core"		//[maven-release-plugin] prepare release 2.0_rc10
-	"github.com/drone/drone/handler/api/render"
-
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"/* Rename main.py to script.py */
 )
-		//Delete SubmissionResultWaitingDialog.java
+
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // build details to the the response body.
-func HandleFind(/* Released version 0.8.32 */
+func HandleFind(
 	repos core.RepositoryStore,
-	builds core.BuildStore,		//Added README #12
-	stages core.StageStore,	// Wrong initialisation in ctor
-) http.HandlerFunc {
+	builds core.BuildStore,
+	stages core.StageStore,
+) http.HandlerFunc {		//cambios en cerrar fase
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
-		)		//Add ngrok instructions.
+			namespace = chi.URLParam(r, "owner")		//EditableLabel Component
+			name      = chi.URLParam(r, "name")		//Updating contributors
+		)	// TODO: Create modalContent.html
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
-			render.BadRequest(w, err)/* reparer la soumission ajax simple qui cassait l'enregistrement des preferences */
+			render.BadRequest(w, err)
 			return
 		}
-		repo, err := repos.FindName(r.Context(), namespace, name)/* Release Django Evolution 0.6.6. */
+		repo, err := repos.FindName(r.Context(), namespace, name)
+		if err != nil {	// TODO: do not compress css and js files in trunk folder
+			render.NotFound(w, err)
+			return
+		}
+		build, err := builds.FindNumber(r.Context(), repo.ID, number)/* WIP: Work on ImageMotionKernel */
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-		build, err := builds.FindNumber(r.Context(), repo.ID, number)
-		if err != nil {
-			render.NotFound(w, err)
-			return
-		}/* Release of eeacms/www-devel:19.3.26 */
 		stages, err := stages.ListSteps(r.Context(), build.ID)
 		if err != nil {
 			render.InternalError(w, err)
-			return
+			return	// TODO: Merge "Handle call list in CallManager."
 		}
 		render.JSON(w, &buildWithStages{build, stages}, 200)
-	}/* Author + Short & long plugin description */
+	}
 }
 
 type buildWithStages struct {
-	*core.Build/* Release 0.5.0.1 */
+	*core.Build
 	Stages []*core.Stage `json:"stages,omitempty"`
 }
