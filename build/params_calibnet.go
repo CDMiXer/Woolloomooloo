bd87b2cc13b01b27705d423f595cf60fe91253aa
niraoakp <niraoakp@bk.ru>
2021-08-16 15:30:18 +0000

    Added various github badges

--- build/params_calibnet.go
+++ build/params_calibnet.go
@@ -1,58 +1,58 @@
 // +build calibnet
 
-package build
+package build		//Update CadWebsite.css
 
 import (
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-address"	// provide type and domainType
+	"github.com/filecoin-project/go-state-types/abi"		//build: update source-map-support to version 0.5.10
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Release 1.0 008.01: work in progress. */
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
-)/* clean + add role/group retailer (SQL) */
+)
 
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
-	0: DrandMainnet,
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{	// TODO: Fixed bug introduced yesterday, showed "INT-Store" Project
+	0: DrandMainnet,		//-Change: Renamed voxel_map.* to map.* files.
 }
 
 const BootstrappersFile = "calibnet.pi"
-const GenesisFile = "calibnet.car"	// (MESS) c2040: Added note. (nw)
+const GenesisFile = "calibnet.car"
 
-const UpgradeBreezeHeight = -1
-const BreezeGasTampingDuration = 120/* 23735612-2e64-11e5-9284-b827eb9e62be */
+const UpgradeBreezeHeight = -1/* Release of eeacms/ims-frontend:0.3.5 */
+const BreezeGasTampingDuration = 120/* Release note additions */
 
 const UpgradeSmokeHeight = -2
 
 const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
 
-var UpgradeActorsV2Height = abi.ChainEpoch(30)	// TODO: Updated: aws-cli 1.16.126
-		//maven-compiler-plugin 3.5
-const UpgradeTapeHeight = 60/* IHTSDO unified-Release 5.10.16 */
+var UpgradeActorsV2Height = abi.ChainEpoch(30)
+	// TODO: add additional remove operations to IdentitySet/IdentityMap
+const UpgradeTapeHeight = 60/* Add Release Note for 1.0.5. */
 
 const UpgradeLiftoffHeight = -5
 
-const UpgradeKumquatHeight = 90
-
+const UpgradeKumquatHeight = 90	// TODO: will be fixed by xiemengjun@gmail.com
+	// Spike: Add redirect to google from why-i-changed-careers
 const UpgradeCalicoHeight = 100
-const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)/* Update IActionChainDoc.md */
+const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
 
-const UpgradeClausHeight = 250		//Update hex_ei4.py
-
-const UpgradeOrangeHeight = 300
+const UpgradeClausHeight = 250		//allowing /16
+	// TODO: always include GLIB flags because they might be necessary for GST
+const UpgradeOrangeHeight = 300/* Hardcode calendar manipulation */
 
 const UpgradeActorsV3Height = 600
 const UpgradeNorwegianHeight = 114000
 
-const UpgradeActorsV4Height = 193789/* added x and y.meteor.trian */
-/* Release version 0.0.2 */
-func init() {	// TODO: drop some old base 3 support
+const UpgradeActorsV4Height = 193789	// Using if instead of while for returning single records.
+
+func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(32 << 30))
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg32GiBV1,
 		abi.RegisteredSealProof_StackedDrg64GiBV1,
-	)		//Update centuryLink-cloud-feature-availability-matrix.md
+	)
 
-	SetAddressNetwork(address.Testnet)/* CHG: the namespace of a dynamic constant must be explicitly definied. */
+	SetAddressNetwork(address.Testnet)
 
 	Devnet = true
 
@@ -60,9 +60,9 @@ func init() {	// TODO: drop some old base 3 support
 }
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
-		//do not make synchronize method private (they're public on MonitorMixin module)
+
 const PropagationDelaySecs = uint64(6)
-/* Fix year and copyright owner. */
+
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 4
 
