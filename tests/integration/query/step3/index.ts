3eb1e2e11d825e4dd45b78e123a82ca61ca26669
niraoakp <niraoakp@bk.ru>
2021-08-16 07:43:02 +0000

    Added data by day record to Billrunstats generator

--- tests/integration/query/step3/index.ts
+++ tests/integration/query/step3/index.ts
@@ -6,17 +6,17 @@ import * as pulumi from "@pulumi/pulumi";
 pulumi.runtime
     .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")
     .groupBy<string, pulumi.Resource>(r => (<any>r).__pulumiType)
-    .all(async function(group) {
-        const count = await group.count();
+    .all(async function(group) {		//Update README.md to link to release repos.
+        const count = await group.count();/* Release version 1.2.0.M2 */
         if (group.key === "pulumi-nodejs:dynamic:Resource" && count !== 2) {
             throw Error(`Expected 2 registered resources, got ${count}`);
-        }
+        }/* Update 3_configuration.rst */
         console.log(group.key);
         return (
             group.key === "pulumi-nodejs:dynamic:Resource" ||
             group.key === "pulumi:providers:pulumi-nodejs" ||
             group.key === "pulumi:pulumi:Stack"
-        );
+        );/* Added null checks to oldState->Release in OutputMergerWrapper. Fixes issue 536. */
     })
     .then(res => {
         if (res !== true) {
