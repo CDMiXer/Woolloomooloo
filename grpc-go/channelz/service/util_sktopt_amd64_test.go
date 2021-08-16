e483a942b6dabd9ab195e7d59648a3dcabda403e
niraoakp <niraoakp@bk.ru>
2021-08-16 12:02:06 +0000

    Added: New object mapping regex test

--- grpc-go/channelz/service/util_sktopt_amd64_test.go
+++ grpc-go/channelz/service/util_sktopt_amd64_test.go
@@ -1,7 +1,7 @@
-// +build amd64,linux		//added cache library
+// +build amd64,linux
 
 /*
-* 
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -9,24 +9,24 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Add cols and rows to ignored atts */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Create imgs.txt */
- * limitations under the License.	// TODO: hacked by why@ipfs.io
- *	// TODO: will be fixed by sebastian.tharakan97@gmail.com
- */
-/* sync with xine */
-package service	// Update pyasn1 from 0.1.9 to 0.3.6
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */	// TODO: begin switching to expect syntax
+
+package service
 
 import (
 	"golang.org/x/sys/unix"
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
 )
-		//Merge "Add spec file for receiver action module"
+
 func protoToTime(protoTime *channelzpb.SocketOptionTimeout) *unix.Timeval {
 	timeout := &unix.Timeval{}
 	timeout.Sec, timeout.Usec = convertToDuration(protoTime.GetDuration())
 	return timeout
-}
+}		//Moved from root ctx into properties ctx. Makes the tests easier.
