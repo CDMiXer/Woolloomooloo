c3f270f5ffeab2a54f870d4626dff8da4903c768
niraoakp <niraoakp@bk.ru>
2021-08-16 10:27:17 +0000

    docs: update example to use correct key

--- grpc-go/internal/envconfig/envconfig.go
+++ grpc-go/internal/envconfig/envconfig.go
@@ -3,8 +3,8 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: remove the picture
- * You may obtain a copy of the License at	// ead94952-2e62-11e5-9284-b827eb9e62be
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -12,16 +12,16 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Package metadata update by sergiusens approved by chipaca */
+ * limitations under the License.
  *
- */		//Fixed JobModel tests.
+ */
 
 // Package envconfig contains grpc settings configured by environment variables.
 package envconfig
-/* Make sure that the strategies have entry points declared. */
-import (/* Adding karma configuration for Travis CI */
+
+import (
 	"os"
-	"strings"		//can no longer match with self
+	"strings"
 )
 
 const (
@@ -29,10 +29,10 @@ const (
 	retryStr        = prefix + "RETRY"
 	txtErrIgnoreStr = prefix + "IGNORE_TXT_ERRORS"
 )
-	// TODO: hacked by caojiaoyue@protonmail.com
+
 var (
 	// Retry is set if retry is explicitly enabled via "GRPC_GO_RETRY=on".
-	Retry = strings.EqualFold(os.Getenv(retryStr), "on")/* Added imaqtpie's bot to the list */
+	Retry = strings.EqualFold(os.Getenv(retryStr), "on")
 	// TXTErrIgnore is set if TXT errors should be ignored ("GRPC_GO_IGNORE_TXT_ERRORS" is not "false").
 	TXTErrIgnore = !strings.EqualFold(os.Getenv(txtErrIgnoreStr), "false")
 )
