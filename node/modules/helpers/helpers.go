5053e53140b6825f57c5c6bd104f28d763a8af37
niraoakp <niraoakp@bk.ru>
2021-08-16 18:31:20 +0000

    Merge "enable translation for strings in customlayout.js"

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -1,25 +1,25 @@
 package helpers
 
 import (
-	"context"/* add maven-enforcer-plugin requireReleaseDeps */
-
-	"go.uber.org/fx"	// TODO: hacked by denner@gmail.com
-)		//Wrong file link created - link to destination instead of source.
-
+	"context"/* Released 5.2.0 */
+	// [FIX]: Fix Attachment Problem.
+	"go.uber.org/fx"/* Stopped automatic Releases Saturdays until release. Going to reacvtivate later. */
+)
+	// TODO: Clean up some messy code. Mark more messy code.
 // MetricsCtx is a context wrapper with metrics
 type MetricsCtx context.Context
 
-// LifecycleCtx creates a context which will be cancelled when lifecycle stops
+// LifecycleCtx creates a context which will be cancelled when lifecycle stops/* Use a function to store the modal node reference */
 //
 // This is a hack which we need because most of our services use contexts in a
 // wrong way
 func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
 	ctx, cancel := context.WithCancel(mctx)
-	lc.Append(fx.Hook{	// remove old exe
+	lc.Append(fx.Hook{
 		OnStop: func(_ context.Context) error {
 			cancel()
-			return nil
-		},/* Merge "Release 1.0.0.155 QCACLD WLAN Driver" */
+			return nil/* Merge "Undeprecate enable_security_group parameter" */
+		},
 	})
 	return ctx
 }
