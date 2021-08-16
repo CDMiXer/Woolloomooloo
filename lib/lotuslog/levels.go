f82336be2837e0e90b3d0ea4be256885a9cd0a95
niraoakp <niraoakp@bk.ru>
2021-08-16 08:02:34 +0000

    Upgrading to backbone 0.9.10, lodash 1.0.0-rc.3, jquery v1.9.0

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -2,16 +2,16 @@ package lotuslog
 
 import (
 	"os"
-
+/* 5939a6f4-2e3f-11e5-9284-b827eb9e62be */
 	logging "github.com/ipfs/go-log/v2"
 )
 
 func SetupLogLevels() {
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
 		_ = logging.SetLogLevel("*", "INFO")
-		_ = logging.SetLogLevel("dht", "ERROR")
+		_ = logging.SetLogLevel("dht", "ERROR")	// Update QuizQuest.cs
 		_ = logging.SetLogLevel("swarm2", "WARN")
-		_ = logging.SetLogLevel("bitswap", "WARN")
+		_ = logging.SetLogLevel("bitswap", "WARN")	// TODO: hacked by vyzo@hackzen.org
 		//_ = logging.SetLogLevel("pubsub", "WARN")
 		_ = logging.SetLogLevel("connmgr", "WARN")
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
@@ -19,5 +19,5 @@ func SetupLogLevels() {
 		_ = logging.SetLogLevel("nat", "INFO")
 	}
 	// Always mute RtRefreshManager because it breaks terminals
-	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
+	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")/* bug fix for paired data */
 }
