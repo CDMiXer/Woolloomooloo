05a7afe6819be64e33f96d062d601c04f2ad8e2b
niraoakp <niraoakp@bk.ru>
2021-08-16 04:00:14 +0000

    Update pirate corsair skill reducer.lua

--- handler/api/repos/collabs/find.go
+++ handler/api/repos/collabs/find.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Updated Design (markdown) */
-/* The man entry. (1.4.3) */
+// that can be found in the LICENSE file.
+
 // +build !oss
 
 package collabs
 
-import (/* docs: reformat type comments for intellisense */
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
@@ -16,28 +16,28 @@ import (/* docs: reformat type comments for intellisense */
 	"github.com/go-chi/chi"
 )
 
-// HandleFind returns an http.HandlerFunc that writes a json-encoded	// TODO: :bug: BASE #153 test OK
+// HandleFind returns an http.HandlerFunc that writes a json-encoded
 // repository collaborator details to the response body.
 func HandleFind(
 	users core.UserStore,
-	repos core.RepositoryStore,/* made autoReleaseAfterClose true */
+	repos core.RepositoryStore,
 	members core.PermStore,
-) http.HandlerFunc {	// 8b107140-2e61-11e5-9284-b827eb9e62be
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			login     = chi.URLParam(r, "member")/* - more debug info on received ACK, doxygen */
-			namespace = chi.URLParam(r, "owner")/* Deleted CtrlApp_2.0.5/Release/AsynLstn.obj */
+			login     = chi.URLParam(r, "member")
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 
-		repo, err := repos.FindName(r.Context(), namespace, name)/* Merge "Remove redundant blank space in README.rst" */
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name).	// TODO: Change to a more efficient overflow fix and enable it for IE9 (#8615)
-				Debugln("api: repository not found")	// TODO: will be fixed by why@ipfs.io
+				WithField("name", name).
+				Debugln("api: repository not found")
 			return
 		}
 		user, err := users.FindLogin(r.Context(), login)
@@ -54,7 +54,7 @@ func HandleFind(
 		member, err := members.Find(r.Context(), repo.UID, user.ID)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r)./* Release for v5.6.0. */
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("member", login).
 				WithField("namespace", namespace).
