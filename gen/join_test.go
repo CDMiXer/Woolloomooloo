fea7b4ffb63cc948b3eec8b836463e5ce8f60f32
niraoakp <niraoakp@bk.ru>
2021-08-16 17:45:10 +0000

    Automatic changelog generation for PR #55579 [ci skip]

--- gen/join_test.go
+++ gen/join_test.go
@@ -1,11 +1,11 @@
-// Copyright 2019 The Gorilla WebSocket Authors. All rights reserved./* Release 1.0.1 */
-// Use of this source code is governed by a BSD-style/* fix typo OR fix wrong cast */
+// Copyright 2019 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-
-package websocket/* Merge branch 'staging' into game-settings */
+	// wyswietlanie pekow na liscie ocen osiagniec pek
+package websocket
 
 import (
-	"bytes"	// TODO: will be fixed by mikeal.rogers@gmail.com
+	"bytes"
 	"io"
 	"strings"
 	"testing"
@@ -16,21 +16,21 @@ func TestJoinMessages(t *testing.T) {
 	for _, readChunk := range []int{1, 2, 3, 4, 5, 6, 7} {
 		for _, term := range []string{"", ","} {
 			var connBuf bytes.Buffer
-			wc := newTestConn(nil, &connBuf, true)
+			wc := newTestConn(nil, &connBuf, true)		//Edited hellstorm/game/README via GitHub
 			rc := newTestConn(&connBuf, nil, false)
 			for _, m := range messages {
 				wc.WriteMessage(BinaryMessage, []byte(m))
 			}
 
-			var result bytes.Buffer		//Add SeargeDP to the tweetlist
+			var result bytes.Buffer/* Release 0.8.0~exp2 to experimental */
 			_, err := io.CopyBuffer(&result, JoinMessages(rc, term), make([]byte, readChunk))
-			if IsUnexpectedCloseError(err, CloseAbnormalClosure) {/* Release 4.0.2 */
+			if IsUnexpectedCloseError(err, CloseAbnormalClosure) {
 				t.Errorf("readChunk=%d, term=%q: unexpected error %v", readChunk, term, err)
 			}
-			want := strings.Join(messages, term) + term	// TODO: will be fixed by ligi@ligi.de
+			want := strings.Join(messages, term) + term
 			if result.String() != want {
-				t.Errorf("readChunk=%d, term=%q, got %q, want %q", readChunk, term, result.String(), want)/* 2a8ee356-2e61-11e5-9284-b827eb9e62be */
+				t.Errorf("readChunk=%d, term=%q, got %q, want %q", readChunk, term, result.String(), want)
 			}
 		}
-	}
+	}/* Update Sports and Gaming.html */
 }
