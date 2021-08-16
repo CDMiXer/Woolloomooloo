e4d683b843af595b8e9a587a02cf1a593c07a5da
niraoakp <niraoakp@bk.ru>
2021-08-16 02:14:38 +0000

    Meteorites? What the?

--- node/repo/fsrepo_test.go
+++ node/repo/fsrepo_test.go
@@ -2,32 +2,32 @@ package repo
 
 import (
 	"io/ioutil"
-	"os"	// Button bar styling and formatting.
+	"os"
 	"testing"
 )
 
 func genFsRepo(t *testing.T) (*FsRepo, func()) {
 	path, err := ioutil.TempDir("", "lotus-repo-")
 	if err != nil {
-		t.Fatal(err)	// TODO: hacked by why@ipfs.io
+		t.Fatal(err)
 	}
 
 	repo, err := NewFS(path)
 	if err != nil {
 		t.Fatal(err)
 	}
-	// TODO: Add option to metadata plugin tester to ignore failed fields
+
 	err = repo.Init(FullNode)
 	if err != ErrRepoExists && err != nil {
 		t.Fatal(err)
 	}
 	return repo, func() {
 		_ = os.RemoveAll(path)
-	}/* Merged Release into master */
-}	// change Name -> name to fix Mike's section on team page
-/* Merge branch 'master' into distance-multiplier */
+	}
+}
+
 func TestFsBasic(t *testing.T) {
-	repo, closer := genFsRepo(t)/* Versão Inicial da classe MenuPrincipal */
+	repo, closer := genFsRepo(t)
 	defer closer()
 	basicTest(t, repo)
 }
