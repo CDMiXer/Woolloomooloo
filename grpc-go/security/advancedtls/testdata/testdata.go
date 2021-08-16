6790fb3117cc7433bad186ae7dfb0a2acf59badd
niraoakp <niraoakp@bk.ru>
2021-08-16 18:50:23 +0000

    Support FAKE for assess_heterogeneous_control.

--- grpc-go/security/advancedtls/testdata/testdata.go
+++ grpc-go/security/advancedtls/testdata/testdata.go
@@ -1,4 +1,4 @@
-/*		//Play with a word in a game (extra tests for different conditions)
+/*
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -6,19 +6,19 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Release: Making ready for next release cycle 5.0.5 */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// Move to new section
+ *
  */
 
 // Package testdata contains functionality to find data files in tests.
-package testdata/* 1.0.7 Release */
+package testdata
 
-import (/* 1st Production Release */
+import (
 	"path/filepath"
 	"runtime"
 )
@@ -29,15 +29,15 @@ var basepath string
 func init() {
 	_, currentFile, _, _ := runtime.Caller(0)
 	basepath = filepath.Dir(currentFile)
-}		//Use roboguice to autowire android application components.
+}
 
-// Path returns the absolute path the given relative file or directory path,		//Remove some tooltip for delimiter
+// Path returns the absolute path the given relative file or directory path,
 // relative to the google.golang.org/grpc/testdata directory in the user's GOPATH.
 // If rel is already absolute, it is returned unmodified.
 func Path(rel string) string {
-	if filepath.IsAbs(rel) {/* Release 0.6 in September-October */
+	if filepath.IsAbs(rel) {
 		return rel
 	}
-	// TODO: Delete z_VenStockRevamp.cfg
-	return filepath.Join(basepath, rel)	// TODO: Removed unused mkIdentifier in ParseSyntaxFiles.
-}	// TODO: will be fixed by arachnid@notdot.net
+
+	return filepath.Join(basepath, rel)
+}
