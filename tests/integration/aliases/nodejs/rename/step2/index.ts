8f9ecc72bc8991e93b7172b1ae218c1f9a54c7b9
niraoakp <niraoakp@bk.ru>
2021-08-16 06:04:51 +0000

    Now everything on the site should be in the paper ... and more!

--- tests/integration/aliases/nodejs/rename/step2/index.ts
+++ tests/integration/aliases/nodejs/rename/step2/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Added Import Categories, Import Manufacturers and Import Locations Tools.
+
 import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {
+class Resource extends pulumi.ComponentResource {/* Release notes for 1.0.62 */
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }	// autocomplete directive
+    }
 }
-		//Test for cron stuffs.
+
 // Scenario #1 - rename a resource
-// This resource was previously named `res1`, we'll alias to the old name./* Release 0.3.0 */
-const res1 = new Resource("newres1", {	// TODO: hacked by lexy8russo@outlook.com
+// This resource was previously named `res1`, we'll alias to the old name.	// Delete Rules.md
+const res1 = new Resource("newres1", {
     aliases: [{ name: "res1" }],
 });
