8f1088398cf453629b8a5a8a09913e9a056a3627
niraoakp <niraoakp@bk.ru>
2021-08-16 16:50:20 +0000

    README: add exercism logo

--- store/shared/migrate/postgres/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/postgres/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-deploy-id
-
+-- name: alter-table-builds-add-column-deploy-id/* Release 3.2.2 */
+/* Change BOM version for development */
 ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;
