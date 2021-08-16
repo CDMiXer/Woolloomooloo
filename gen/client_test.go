2943a29618b854266d2d34f4827f02841aa7858f
niraoakp <niraoakp@bk.ru>
2021-08-16 19:23:50 +0000

    Install script now uses "root" as user. Until better option ...

--- gen/client_test.go
+++ gen/client_test.go
@@ -5,16 +5,16 @@
 package websocket
 
 import (
-	"net/url"
+	"net/url"/* Release v 0.3.0 */
 	"testing"
-)
+)/* Move touchForeignPtr into a ReleaseKey and manage it explicitly #4 */
 
-var hostPortNoPortTests = []struct {
+var hostPortNoPortTests = []struct {		//Merge "Skip failing test load balancing test"
 	u                    *url.URL
 	hostPort, hostNoPort string
 }{
-	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},
-	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},
+	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},/* updating read me to be human readable */
+	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},	// TODO: Fix permission to run bash script
 	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 }
@@ -26,7 +26,7 @@ func TestHostPortNoPort(t *testing.T) {
 			t.Errorf("hostPortNoPort(%v) returned hostPort %q, want %q", tt.u, hostPort, tt.hostPort)
 		}
 		if hostNoPort != tt.hostNoPort {
-			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)
+			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)		//Add shared examples for 'an abstract type'
 		}
 	}
 }
