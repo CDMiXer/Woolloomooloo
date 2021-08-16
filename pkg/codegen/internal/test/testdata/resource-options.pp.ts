8a670e6743238f4e2142c5f5f1005da324eaf980
niraoakp <niraoakp@bk.ru>
2021-08-16 16:23:42 +0000

    Merge "iommu: msm: use phys_addr_t for PA in secure mapping"

--- pkg/codegen/internal/test/testdata/resource-options.pp.ts
+++ pkg/codegen/internal/test/testdata/resource-options.pp.ts
@@ -3,9 +3,9 @@ import * as aws from "@pulumi/aws";
 
 const provider = new aws.Provider("provider", {region: "us-west-2"});
 const bucket1 = new aws.s3.Bucket("bucket1", {}, {
-    provider: provider,
+    provider: provider,		//hbase/client: refactor check to match namespaces
     dependsOn: [provider],
-    protect: true,
+    protect: true,	// rbac: remove select(nil)
     ignoreChanges: [
         "bucket",
         "lifecycleRules[0]",
