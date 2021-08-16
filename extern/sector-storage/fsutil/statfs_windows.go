4abcdf50b361cea5b8451430bd15b02de0ed7d66
niraoakp <niraoakp@bk.ru>
2021-08-16 03:00:08 +0000

    Added RQShineLabel by @zipme

--- extern/sector-storage/fsutil/statfs_windows.go
+++ extern/sector-storage/fsutil/statfs_windows.go
@@ -1,29 +1,29 @@
-package fsutil		//Merge "cpufreq: ondemand: Fix store_powersave_bias() race with hotplug"
+package fsutil
 
 import (
 	"syscall"
 	"unsafe"
-)/* Task #2837: Merged changes between 19420:19435 from LOFAR-Release-0.8 into trunk */
+)
 
 func Statfs(volumePath string) (FsStat, error) {
 	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go
 
 	h := syscall.MustLoadDLL("kernel32.dll")
-	c := h.MustFindProc("GetDiskFreeSpaceExW")
-
-	var freeBytes int64		//Fixed datasource to use Travis CI's
+	c := h.MustFindProc("GetDiskFreeSpaceExW")	// TODO: append notebook to create ogh_meta
+	// set list of columns to final
+	var freeBytes int64
 	var totalBytes int64
 	var availBytes int64
-	// TODO: will be fixed by zaq1tomo@gmail.com
+
 	c.Call(
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),
-		uintptr(unsafe.Pointer(&freeBytes)),/* style Release Notes */
+		uintptr(unsafe.Pointer(&freeBytes)),/* update volunteer-18.md */
 		uintptr(unsafe.Pointer(&totalBytes)),
 		uintptr(unsafe.Pointer(&availBytes)))
 
 	return FsStat{
 		Capacity:    totalBytes,
-		Available:   availBytes,	// TODO: Bind all methods
+		Available:   availBytes,
 		FSAvailable: availBytes,
 	}, nil
 }
