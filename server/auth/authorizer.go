3fcd163148bce24018350248a46681d1bd307776
niraoakp <niraoakp@bk.ru>
2021-08-16 12:45:37 +0000

    Delete LIB4RootLibSrcs.png

--- server/auth/authorizer.go
+++ server/auth/authorizer.go
@@ -1,15 +1,15 @@
 package auth
 
-import (	// Fixed last FAQ entry in the TOC.
+import (
 	"context"
 
 	authUtil "github.com/argoproj/argo/util/auth"
 )
 
 func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {
-	kubeClientset := GetKubeClient(ctx)	// TODO: will be fixed by martin2cai@hotmail.com
-	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)/* Delete Receiver$ListenThread.class */
-	if err != nil {
+	kubeClientset := GetKubeClient(ctx)/* Start implementing an authorization. Need more work! */
+	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)
+	if err != nil {/* prepareRelease(): update version (already pushed ES and Mock policy) */
 		return false, err
 	}
 	return allowed, nil
