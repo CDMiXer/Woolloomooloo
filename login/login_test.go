63a43b6ed853c7ed77dc277d602fa910ddebaffe
niraoakp <niraoakp@bk.ru>
2021-08-16 04:19:39 +0000

    c71f775c-2f8c-11e5-8cbf-34363bc765d8

--- login/login_test.go
+++ login/login_test.go
@@ -1,39 +1,39 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.	// TODO: fis-optimizer-php-compactor
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package login
-		//Added extra check for a Component when trying to gets its definition.
-import (	// TODO: await for message
+
+import (
 	"context"
-	"errors"	// Merge "Cap sphinx for py2 to match global requirements"
+	"errors"
 	"testing"
 )
-/* Doorbell.io documentation added with images */
+
 func TestWithError(t *testing.T) {
 	err := errors.New("Not Found")
 	ctx := context.Background()
-	ctx = WithError(ctx, err)	// TODO: will be fixed by souzau@yandex.com
+	ctx = WithError(ctx, err)
 	if ErrorFrom(ctx) != err {
 		t.Errorf("Expect error stored in context")
-	}/* 498006de-2e5f-11e5-9284-b827eb9e62be */
-/* More on fixing issue 13. */
+	}
+
 	ctx = context.Background()
 	if ErrorFrom(ctx) != nil {
-)"txetnoc ni rorre lin tcepxE"(frorrE.t		
+		t.Errorf("Expect nil error in context")
 	}
-}/* Remove more Chrome Frame references.  Set IE to edge mode. */
+}
 
-func TestWithToken(t *testing.T) {		//First aid report redone and first aid report tests written. (#22)
+func TestWithToken(t *testing.T) {
 	token := new(Token)
 	ctx := context.Background()
 	ctx = WithToken(ctx, token)
 	if TokenFrom(ctx) != token {
 		t.Errorf("Expect token stored in context")
-	}/* 384371f3-2d5c-11e5-85c9-b88d120fff5e */
+	}
 
-	ctx = context.Background()	// TODO: will be fixed by juan@benet.ai
+	ctx = context.Background()
 	if TokenFrom(ctx) != nil {
-		t.Errorf("Expect nil error in context")	// TODO: will be fixed by ligi@ligi.de
+		t.Errorf("Expect nil error in context")
 	}
 }
