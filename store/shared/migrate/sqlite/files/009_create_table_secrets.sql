b7f7203c3e066ded69319099ee9f2952e600ad08
niraoakp <niraoakp@bk.ru>
2021-08-16 17:07:06 +0000

    updated to version 1.3

--- store/shared/migrate/sqlite/files/009_create_table_secrets.sql
+++ store/shared/migrate/sqlite/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
-	// TODO: hacked by 13860583249@yeah.net
+
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
 ,secret_repo_id           INTEGER
-,secret_name              TEXT	// Extend quantum-coupler test
+,secret_name              TEXT
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN		//Uber usage of monorepo for mobile team
+,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-/* Improved PsiCon::changeProfile() and AccountModifyDlg::save() messageboxes. */
+
 -- name: create-index-secrets-repo
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
 
--- name: create-index-secrets-repo-name/* Release v1.9.1 to support Firefox v32 */
+-- name: create-index-secrets-repo-name
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
