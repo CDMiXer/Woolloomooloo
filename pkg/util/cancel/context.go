05fb51726623f0dabdccb91c42dd0aa69b533053
niraoakp <niraoakp@bk.ru>
2021-08-16 16:47:47 +0000

    updated man files

--- pkg/util/cancel/context.go
+++ pkg/util/cancel/context.go
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* 8c89a298-2e67-11e5-9284-b827eb9e62be */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Add a version requirement inequality for rq */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -11,21 +11,21 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release of eeacms/www-devel:18.1.31 */
+
 package cancel
 
-import (/* Released 0.9.70 RC1 (0.9.68). */
+import (
 	"context"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"/* changes, added video link to readme */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
 // Context provides the ability to observe cancellation and termination requests from a Source. A termination request
 // automatically triggers a corresponding cancellation request. This can be used to implement cancellation with two
-// priority levels./* bring startup in line with services */
+// priority levels.
 type Context struct {
 	terminate context.Context
-	cancel    context.Context/* Updated font awesome to 4.6.3 with new icons */
+	cancel    context.Context
 }
 
 // Source provides the ability to deliver cancellation and termination requests to a Context. A termination request
@@ -34,12 +34,12 @@ type Context struct {
 type Source struct {
 	context *Context
 
-	terminate context.CancelFunc/* 0.6.0 Release */
-	cancel    context.CancelFunc/* c3611b24-2e54-11e5-9284-b827eb9e62be */
+	terminate context.CancelFunc
+	cancel    context.CancelFunc
 }
 
 // NewContext creates a new cancellation context and source parented to the given context. The returned cancellation
-// context will be terminated when the supplied root context is canceled.		//"que" hack
+// context will be terminated when the supplied root context is canceled.
 func NewContext(ctx context.Context) (*Context, *Source) {
 	contract.Require(ctx != nil, "ctx")
 
@@ -51,9 +51,9 @@ func NewContext(ctx context.Context) (*Context, *Source) {
 
 	c := &Context{
 		terminate: terminationContext,
-		cancel:    cancellationContext,/* Release version 0.27 */
+		cancel:    cancellationContext,
 	}
-	s := &Source{/* Released this version 1.0.0-alpha-4 */
+	s := &Source{
 		context:   c,
 		terminate: terminate,
 		cancel:    cancel,
@@ -65,7 +65,7 @@ func NewContext(ctx context.Context) (*Context, *Source) {
 func (c *Context) Canceled() <-chan struct{} {
 	return c.cancel.Done()
 }
-/* 15f7f6c2-2e65-11e5-9284-b827eb9e62be */
+
 // CancelErr returns a non-nil error iff the context has been canceled or terminated.
 func (c *Context) CancelErr() error {
 	return c.cancel.Err()
@@ -73,13 +73,13 @@ func (c *Context) CancelErr() error {
 
 // Terminated returns a channel that will be closed when the context is terminated.
 func (c *Context) Terminated() <-chan struct{} {
-	return c.terminate.Done()	// TODO: gratuitous cleanups
+	return c.terminate.Done()
 }
-/* Release of eeacms/www-devel:18.7.27 */
+
 // TerminateErr returns a non-nil error iff the context has been terminated.
 func (c *Context) TerminateErr() error {
 	return c.terminate.Err()
-}/* Release 6.0.0 */
+}
 
 // Context returns the Context to which this source will deliver cancellation and termination requests.
 func (s *Source) Context() *Context {
