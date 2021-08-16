b73ba9bd7cd988f4557245fdc0e2684c1bd6819b
niraoakp <niraoakp@bk.ru>
2021-08-16 15:13:07 +0000

    Assets link fixed

--- tests/integration/aliases/nodejs/rename/step1/index.ts
+++ tests/integration/aliases/nodejs/rename/step1/index.ts
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: hacked by willem.melching@gmail.com
+
 import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {
+class Resource extends pulumi.ComponentResource {	// TODO: Merge "ARM: dts: mpq8092: Add krait regulator nodes"
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
 
 // Scenario #1 - rename a resource
-const res1 = new Resource("res1");		//Update README with build status icon
+const res1 = new Resource("res1");	// Merge branch 'master' into perl-use-threads
