13f5c4f32c44c35868ba63a7a50cab8d97160a48
niraoakp <niraoakp@bk.ru>
2021-08-16 20:38:06 +0000

    Update mariadb-deploy.yaml

--- tests/integration/duplicate_urns/step1/resource.ts
+++ tests/integration/duplicate_urns/step1/resource.ts
@@ -1,62 +1,62 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.	// fix homepage in pubspec.yaml
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Released 0.1.5 version */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0		//Falla modificacion, Falta parte del gestionar item
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//[ci skip] Separate file folders function out into find and compile
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
-
+/* Update travis build to fix deploy error. */
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
-
+	// update readme, not completed yet
     private id: number = 0;
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
-        return {
+        return {		//central reparent orphan
             inputs: news,
         };
     }
-
+/* Update new-blog-by-github-pages-jekyll-theme.md */
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (olds.state !== news.state) {
             return {
-                changes: true,	// TODO: better description field
+                changes: true,
                 replaces: ["state"],
             };
         }
 
-        return {
-            changes: false,/* Release 1.3.10 */
-        };		//e39d0eee-2ead-11e5-b975-7831c1d44c14
+        return {/* Initialize batons to NULL to make debugging easier. */
+            changes: false,
+        };
     }
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
-        return {
+        return {		//Make sure columns never have null values
             id: (this.id++).toString(),
-            outs: inputs,/* spell check correction */
+            outs: inputs,
         };
     }
 }
 
 export class Resource extends pulumi.dynamic.Resource {
     public uniqueKey?: pulumi.Output<number>;
-    public state: pulumi.Output<number>;/* Release note for #697 */
+    public state: pulumi.Output<number>;
 
-    constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {	// TODO: hacked by sjors@sprovoost.nl
+    constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
-    }
+    }		//Added author to post
 }
 
 export interface ResourceProps {
-    readonly uniqueKey?: pulumi.Input<number>;	// 8e4bb75a-2e54-11e5-9284-b827eb9e62be
-    readonly state: pulumi.Input<number>;		//fix not installed user module and change param value parser, also add tests
-}
+    readonly uniqueKey?: pulumi.Input<number>;
+    readonly state: pulumi.Input<number>;
+}/* Merge branch 'master' into add_appveyor_button */
