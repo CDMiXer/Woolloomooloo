0eb85e34631550a05672e12f0dcaffb3bf7fb912
niraoakp <niraoakp@bk.ru>
2021-08-16 05:29:16 +0000

    c538dd8c-2e73-11e5-9284-b827eb9e62be

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//FIX: added missing gLogger import
-
+// that can be found in the LICENSE file.
+		//Add placeholder README
 // +build !oss
 
 package internal
 
 var defaultImage = "drone/controller:1"
-	// TODO: He 111 : Modification of the canopy.
+
 // DefaultImage returns the default dispatch image if none
-// is specified.
+// is specified./* Remove checking support for 1.7.x and 1.8.x */
 func DefaultImage(image string) string {
 	if image == "" {
 		return defaultImage
-	}/* Added patch for upgrading from 0.4 to 0.5. */
-	return image
+	}
+	return image	// TODO: Merge "Remove identity and assignment kvs backends"
 }
