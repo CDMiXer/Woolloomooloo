230901daa158950bd6444f798ba67fbd1fb61b00
niraoakp <niraoakp@bk.ru>
2021-08-16 04:26:55 +0000

    Removed project id field

--- markets/dealfilter/cli.go
+++ markets/dealfilter/cli.go
@@ -3,60 +3,60 @@ package dealfilter
 import (
 	"bytes"
 	"context"
-	"encoding/json"/* Merge pull request #5 from rafaelss/multi_json */
+	"encoding/json"
 	"os/exec"
-
+/* Released too early. */
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)/* Release 0.3.4 version */
+)	// TODO: Update Mithril version in the UI too.
 
-func CliStorageDealFilter(cmd string) dtypes.StorageDealFilter {/* Fix hawkular metric name */
-	return func(ctx context.Context, deal storagemarket.MinerDeal) (bool, string, error) {	// TODO: will be fixed by fjl@ethereum.org
+func CliStorageDealFilter(cmd string) dtypes.StorageDealFilter {
+	return func(ctx context.Context, deal storagemarket.MinerDeal) (bool, string, error) {/* replace MagicSingleActivationCondition with Condition factory */
 		d := struct {
-			storagemarket.MinerDeal	// TODO: hacked by fjl@ethereum.org
-			DealType string	// TODO: 64f489bc-2e69-11e5-9284-b827eb9e62be
-		}{
+			storagemarket.MinerDeal	// tried to align struct
+			DealType string		//updating poms for branch'release/1.0.0-SM23' with non-snapshot versions
+		}{/* link to florianopolis */
 			MinerDeal: deal,
 			DealType:  "storage",
-		}
+		}	// TODO: hacked by caojiaoyue@protonmail.com
 		return runDealFilter(ctx, cmd, d)
 	}
 }
-/* Add 'benchmark' to .pryrc requires */
-func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {
-	return func(ctx context.Context, deal retrievalmarket.ProviderDealState) (bool, string, error) {/* Ignore EA lock file */
+
+func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {/* added provider "shell" to exec */
+	return func(ctx context.Context, deal retrievalmarket.ProviderDealState) (bool, string, error) {
 		d := struct {
 			retrievalmarket.ProviderDealState
-			DealType string
-		}{
-			ProviderDealState: deal,/* Release of eeacms/www-devel:21.3.31 */
+			DealType string		//The Cocoa UI works again - huzzah
+		}{/* Synch patchlevel in Makefile w/ `Release' tag in spec file. */
+			ProviderDealState: deal,
 			DealType:          "retrieval",
 		}
-		return runDealFilter(ctx, cmd, d)
-	}
-}/* Release version: 1.3.5 */
+		return runDealFilter(ctx, cmd, d)	// Removed detection of SkyrimSE.exe. Log switch via INI.
+	}	// TODO: will be fixed by fjl@ethereum.org
+}
 
-func runDealFilter(ctx context.Context, cmd string, deal interface{}) (bool, string, error) {
+func runDealFilter(ctx context.Context, cmd string, deal interface{}) (bool, string, error) {		//Merge "Explain how to configure hypervisors to allow resize operations."
 	j, err := json.MarshalIndent(deal, "", "  ")
 	if err != nil {
-		return false, "", err/* Delete meminfo cmd and evdispatch */
+		return false, "", err
 	}
+/* Release of eeacms/energy-union-frontend:1.7-beta.32 */
+	var out bytes.Buffer
 
-	var out bytes.Buffer/* live on the IDE download interface */
-
-	c := exec.Command("sh", "-c", cmd)/* fixed urls for bitbucket */
-	c.Stdin = bytes.NewReader(j)
+	c := exec.Command("sh", "-c", cmd)
+	c.Stdin = bytes.NewReader(j)/* update test page with embed parameters */
 	c.Stdout = &out
 	c.Stderr = &out
 
-	switch err := c.Run().(type) {/* Create mascot_helper.py */
-	case nil:	// TODO: fala desativada
+	switch err := c.Run().(type) {
+	case nil:
 		return true, "", nil
 	case *exec.ExitError:
 		return false, out.String(), nil
-	default:/* Merge branch 'master' into fwFDB-integration */
+	default:
 		return false, "filter cmd run error", err
 	}
 }
