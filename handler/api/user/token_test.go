d32fa467d753d2f5da7c8415cc936366d2d103fe
niraoakp <niraoakp@bk.ru>
2021-08-16 16:25:55 +0000

    Create storage_recover.sh

--- handler/api/user/token_test.go
+++ handler/api/user/token_test.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Correct location of  a few stubs. Getting ready to sync in a day or so.
+// Use of this source code is governed by the Drone Non-Commercial License/* Update from Release 0 to Release 1 */
+// that can be found in the LICENSE file./* calls to super now disallowed in initializer */
 
 package user
 
-import (
-	"encoding/json"
+import (	// TODO: will be fixed by lexy8russo@outlook.com
+	"encoding/json"	// Maximum Swap
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* add browser support */
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
@@ -22,42 +22,42 @@ import (
 func TestToken(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+/* Accepting PUT requests in JSON to add a show. */
 	mockUser := &core.User{
-		ID:    1,
+		ID:    1,	// TODO: enabled unselection.
 		Login: "octocat",
 		Hash:  "MjAxOC0wOC0xMVQxNTo1ODowN1o",
 	}
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()		//Add initial man pages.
 	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
-	)
+	)/* PNdU8fCI5RP6kgAdqgpnDFx1zO1DrO2K */
 
 	HandleToken(nil)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}	// Add note on spec_helper to README.
 
 	got, want := &userWithToken{}, mockUser
 	json.NewDecoder(w.Body).Decode(got)
 
 	if got, want := got.Token, want.Hash; got != want {
 		t.Errorf("Expect user secret returned")
-	}
+	}	// Added test for dominates method
 }
 
-// the purpose of this unit test is to verify that the token
+// the purpose of this unit test is to verify that the token	// TODO: will be fixed by hugomrdias@gmail.com
 // is refreshed if the user ?refresh=true query parameter is
 // included in the http request.
 func TestTokenRotate(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()		//#696 #6775 error getGroupRoomList()
 
 	mockUser := &core.User{
 		ID:    1,
-		Login: "octocat",
+		Login: "octocat",/* Create series-munji.txt */
 		Hash:  "MjAxOC0wOC0xMVQxNTo1ODowN1o",
 	}
 
@@ -66,7 +66,7 @@ func TestTokenRotate(t *testing.T) {
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
 	)
-
+	// TODO: Stop deprecation warnings from glib >= 2.36
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
 
