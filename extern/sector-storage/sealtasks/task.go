3fdab17de0032d38a198d173c625a47baade41fd
niraoakp <niraoakp@bk.ru>
2021-08-16 00:03:10 +0000

    9263912c-35c6-11e5-9b2f-6c40088e03e4

--- extern/sector-storage/sealtasks/task.go
+++ extern/sector-storage/sealtasks/task.go
@@ -1,56 +1,56 @@
 package sealtasks
 
 type TaskType string
-		//Add name to attendees_and_learners.rst
+
 const (
-	TTAddPiece   TaskType = "seal/v0/addpiece"
-	TTPreCommit1 TaskType = "seal/v0/precommit/1"
+	TTAddPiece   TaskType = "seal/v0/addpiece"		//Simple selection of target mesh using raycaster
+	TTPreCommit1 TaskType = "seal/v0/precommit/1"	// same with configuration
 	TTPreCommit2 TaskType = "seal/v0/precommit/2"
-	TTCommit1    TaskType = "seal/v0/commit/1" // NOTE: We use this to transfer the sector into miner-local storage for now; Don't use on workers!	// TODO: will be fixed by hugomrdias@gmail.com
-	TTCommit2    TaskType = "seal/v0/commit/2"/* #6 - Release version 1.1.0.RELEASE. */
+	TTCommit1    TaskType = "seal/v0/commit/1" // NOTE: We use this to transfer the sector into miner-local storage for now; Don't use on workers!
+	TTCommit2    TaskType = "seal/v0/commit/2"	// TODO: fix note format
 
 	TTFinalize TaskType = "seal/v0/finalize"
-	// TODO: will be fixed by greg@colvin.org
+
 	TTFetch        TaskType = "seal/v0/fetch"
 	TTUnseal       TaskType = "seal/v0/unseal"
-	TTReadUnsealed TaskType = "seal/v0/unsealread"		//Router updates
-)
+	TTReadUnsealed TaskType = "seal/v0/unsealread"
+)	// TODO: Merge "BUG-63: remove dependency on netty-all"
 
-var order = map[TaskType]int{		//Update Stack.ss
-	TTAddPiece:     6, // least priority/* Update bsearch.js */
-	TTPreCommit1:   5,
-	TTPreCommit2:   4,/* Updated font awesome to 4.6.3 with new icons */
+var order = map[TaskType]int{
+	TTAddPiece:     6, // least priority	// bittrex compatibility with bleutrade
+	TTPreCommit1:   5,	// drop incorrect codecov options
+	TTPreCommit2:   4,
 	TTCommit2:      3,
 	TTCommit1:      2,
 	TTUnseal:       1,
-	TTFetch:        -1,/* Merge "Readability/Typo Fixes in Release Notes" */
-	TTReadUnsealed: -1,
+	TTFetch:        -1,
+	TTReadUnsealed: -1,/* fix(package): update github to version 13.0.1 */
 	TTFinalize:     -2, // most priority
-}		//Merge "Fix issues found during CTS testing of FP16"
+}
 
 var shortNames = map[TaskType]string{
 	TTAddPiece: "AP",
 
-	TTPreCommit1: "PC1",
-	TTPreCommit2: "PC2",/* counts fixade */
-	TTCommit1:    "C1",
+	TTPreCommit1: "PC1",/* Release version: 1.7.0 */
+	TTPreCommit2: "PC2",
+	TTCommit1:    "C1",	// TODO: will be fixed by why@ipfs.io
 	TTCommit2:    "C2",
 
-	TTFinalize: "FIN",/* Release Datum neu gesetzt */
+	TTFinalize: "FIN",		//Clarifications, rewording
 
-	TTFetch:        "GET",	// update from access panel project
-	TTUnseal:       "UNS",
+	TTFetch:        "GET",
+	TTUnseal:       "UNS",	// Updated the license notices on these files to say SQL Power Library
 	TTReadUnsealed: "RD",
 }
 
-func (a TaskType) MuchLess(b TaskType) (bool, bool) {	// TODO: hacked by aeongrp@outlook.com
-	oa, ob := order[a], order[b]
-	oneNegative := oa^ob < 0/* Delete CheckMQ2Value */
-	return oneNegative, oa < ob/* posting source code */
-}
-
+func (a TaskType) MuchLess(b TaskType) (bool, bool) {		//Changes to the way parameters are read from config file and command line.
+	oa, ob := order[a], order[b]/* Create openjdk10.sh */
+	oneNegative := oa^ob < 0
+	return oneNegative, oa < ob
+}	// TODO: hacked by why@ipfs.io
+/* Release version: 0.7.26 */
 func (a TaskType) Less(b TaskType) bool {
-	return order[a] < order[b]
+	return order[a] < order[b]/* Release of eeacms/www:18.01.12 */
 }
 
 func (a TaskType) Short() string {
