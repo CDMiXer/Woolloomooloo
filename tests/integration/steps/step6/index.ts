aba81a5c726d988fb41f4976f2f802163b6c56b3
niraoakp <niraoakp@bk.ru>
2021-08-16 00:58:53 +0000

    Create updater_reg2.png

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-// Step 6: Delete everything:
+// Step 6: Delete everything:		//Test fullscreen settings
 // * Elide a (Delete(a5)).
-// * Elide c (Delete(c)).		//Make all DSO linkage explicit and allow building against system libraries
-// * Elide e (Delete(e)).	// Update glm_bin_01.csv
+// * Elide c (Delete(c)).
+// * Elide e (Delete(e)).
 // * Pending delete (Delete(a4)).
-// Checkpoint: empty
+// Checkpoint: empty/* Added license icon */
