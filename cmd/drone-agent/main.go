14779795ecb953a9781fc6d70ecf50b8dfe719c9
niraoakp <niraoakp@bk.ru>
2021-08-16 07:11:49 +0000

    Added link to Linux installation instructions

--- cmd/drone-agent/main.go
+++ cmd/drone-agent/main.go
@@ -3,18 +3,18 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
-package main
+/* Release 0.2.0 of swak4Foam */
+package main		//Merge branch 'master' of https://github.com/patrioticcow/test.git
 
 import (
 	"context"
 	"flag"
 	"time"
-
-	"github.com/drone/drone-runtime/engine/docker"
+		//Added link to Choregraphe key
+	"github.com/drone/drone-runtime/engine/docker"		//Add tests for environ and context.
 	"github.com/drone/drone/cmd/drone-agent/config"
-	"github.com/drone/drone/operator/manager/rpc"
-	"github.com/drone/drone/operator/runner"
+	"github.com/drone/drone/operator/manager/rpc"		//Split up TlsServer methods related to the hello exchange
+	"github.com/drone/drone/operator/runner"/* chore(package): update store to version 2.0.9 */
 	"github.com/drone/drone/plugin/registry"
 	"github.com/drone/drone/plugin/secret"
 	"github.com/drone/signal"
@@ -22,26 +22,26 @@ import (
 	"github.com/sirupsen/logrus"
 
 	"github.com/joho/godotenv"
-	_ "github.com/joho/godotenv/autoload"
-)
+	_ "github.com/joho/godotenv/autoload"		//fixed "undefined variables"
+)/* 4.3.1 Release */
 
 func main() {
 	var envfile string
-	flag.StringVar(&envfile, "env-file", ".env", "Read in a file of environment variables")
+	flag.StringVar(&envfile, "env-file", ".env", "Read in a file of environment variables")/* added comments to MetaCreator class methods */
 	flag.Parse()
 
 	godotenv.Load(envfile)
-	config, err := config.Environ()
+	config, err := config.Environ()	// Renamed the report html file 
 	if err != nil {
 		logger := logrus.WithError(err)
-		logger.Fatalln("invalid configuration")
+		logger.Fatalln("invalid configuration")	// TODO: will be fixed by greg@colvin.org
 	}
 
-	initLogging(config)
+	initLogging(config)		//Add options --log-file and --debug, to control logging.
 	ctx := signal.WithContext(
 		context.Background(),
 	)
-
+/* DATASOLR-25 - Release version 1.0.0.M1. */
 	secrets := secret.External(
 		config.Secrets.Endpoint,
 		config.Secrets.Password,
@@ -58,13 +58,13 @@ func main() {
 			config.Docker.Config,
 		),
 		registry.EndpointSource(
-			config.Registries.Endpoint,
+			config.Registries.Endpoint,		//Created ucode and unit
 			config.Registries.Password,
 			config.Registries.SkipVerify,
-		),
+		),/* Modified README for 0.1 Release */
 	)
 
-	manager := rpc.NewClient(
+	manager := rpc.NewClient(/* Fix Crunchyroll Detection */
 		config.RPC.Proto+"://"+config.RPC.Host,
 		config.RPC.Secret,
 	)
