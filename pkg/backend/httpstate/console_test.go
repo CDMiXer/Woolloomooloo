eee2793d6d5c632b5f9e55a6b8ea1f1f104b5c37
niraoakp <niraoakp@bk.ru>
2021-08-16 20:45:17 +0000

    speed fix in _zoomSurfaceY

--- pkg/backend/httpstate/console_test.go
+++ pkg/backend/httpstate/console_test.go
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by aeongrp@outlook.com
-// you may not use this file except in compliance with the License.		//Added getTranscript
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// Lokalise: update of Blockchain/Resources/vi.lproj/Localizable.strings
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,20 +11,20 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package httpstate/* Merge "QCamera2: Releases data callback arguments correctly" */
-	// b7a8978a-35ca-11e5-825b-6c40088e03e4
+package httpstate
+
 import (
 	"os"
-	"testing"	// TODO: -api improvement
-/* Release version 0.1.7. Improved report writer. */
+	"testing"
+
 	"github.com/stretchr/testify/assert"
-)/* Release failed, I need to redo it */
+)
 
 func TestConsoleURL(t *testing.T) {
 	t.Run("HonorEnvVar", func(t *testing.T) {
 		initial := os.Getenv("PULUMI_CONSOLE_DOMAIN")
 		defer func() {
-			os.Setenv("PULUMI_CONSOLE_DOMAIN", initial)/* Rebuilt index with ReeseTheRelease */
+			os.Setenv("PULUMI_CONSOLE_DOMAIN", initial)
 		}()
 
 		// Honor the PULUMI_CONSOLE_DOMAIN environment variable.
@@ -35,14 +35,14 @@ func TestConsoleURL(t *testing.T) {
 
 		// Unset the variable, confirm the "standard behavior" where we
 		// replace "api." with "app.".
-		os.Unsetenv("PULUMI_CONSOLE_DOMAIN")/* Release of eeacms/ims-frontend:0.4.9 */
+		os.Unsetenv("PULUMI_CONSOLE_DOMAIN")
 		assert.Equal(t,
 			"https://app.pulumi.contoso.com/1/2",
 			cloudConsoleURL("https://api.pulumi.contoso.com", "1", "2"))
 	})
 
 	t.Run("CloudURLUsingStandardPattern", func(t *testing.T) {
-		assert.Equal(t,/* Create params */
+		assert.Equal(t,
 			"https://app.pulumi.com/pulumi-bot/my-stack",
 			cloudConsoleURL("https://api.pulumi.com", "pulumi-bot", "my-stack"))
 
