860785f13d7d4af668ff8681ec12413c2be7003b
niraoakp <niraoakp@bk.ru>
2021-08-16 20:21:31 +0000

    rename to better reflect purpose

--- tests/integration/large_resource/python/__main__.py
+++ tests/integration/large_resource/python/__main__.py
@@ -1,7 +1,7 @@
-import pulumi
-
-# Create a very long string (>4mb)
+import pulumi/* Release v0.9.5 */
+/* 5.3.6 Release */
+# Create a very long string (>4mb)	// TODO: #27 : Added beam chamber documentation.
 long_string = "a" * 5 * 1024 * 1025
 
 # Export the name of the bucket
-pulumi.export("long_string",  long_string)
+pulumi.export("long_string",  long_string)/* a81822ca-2e42-11e5-9284-b827eb9e62be */
