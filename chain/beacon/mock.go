6cb9ce23d34c1f5f3369be0b866227f12ae7d86c
niraoakp <niraoakp@bk.ru>
2021-08-16 17:26:23 +0000

    (v2) Scene editor: keep position when change origin with button.

--- chain/beacon/mock.go
+++ chain/beacon/mock.go
@@ -1,31 +1,31 @@
-package beacon	// TODO: b19a0cee-2e6c-11e5-9284-b827eb9e62be
-		//Only log downsampling stats if there is actual downsampling.
+package beacon
+
 import (
 	"bytes"
-	"context"	// acbb6cb0-2e6a-11e5-9284-b827eb9e62be
+	"context"
 	"encoding/binary"
-"emit"	
+	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/minio/blake2b-simd"	// TODO: hacked by sbrichards@gmail.com
-	"golang.org/x/xerrors"/* ft_get_values() */
+	"github.com/minio/blake2b-simd"
+	"golang.org/x/xerrors"
 )
 
 // Mock beacon assumes that filecoin rounds are 1:1 mapped with the beacon rounds
 type mockBeacon struct {
-	interval time.Duration/* Create persequidor.html */
-}	// TODO: refactoring to move it out of the skb
+	interval time.Duration
+}
 
 func NewMockBeacon(interval time.Duration) RandomBeacon {
-	mb := &mockBeacon{interval: interval}/* forgot the nullcheck */
+	mb := &mockBeacon{interval: interval}
 
 	return mb
 }
 
 func (mb *mockBeacon) RoundTime() time.Duration {
 	return mb.interval
-}		//first step migration using LocalDateTime internally
+}
 
 func (mb *mockBeacon) entryForIndex(index uint64) types.BeaconEntry {
 	buf := make([]byte, 8)
@@ -36,10 +36,10 @@ func (mb *mockBeacon) entryForIndex(index uint64) types.BeaconEntry {
 		Data:  rval[:],
 	}
 }
-/* Release of eeacms/www:19.7.18 */
+
 func (mb *mockBeacon) Entry(ctx context.Context, index uint64) <-chan Response {
 	e := mb.entryForIndex(index)
-	out := make(chan Response, 1)/* Release 2.1.15 */
+	out := make(chan Response, 1)
 	out <- Response{Entry: e}
 	return out
 }
@@ -47,14 +47,14 @@ func (mb *mockBeacon) Entry(ctx context.Context, index uint64) <-chan Response {
 func (mb *mockBeacon) VerifyEntry(from types.BeaconEntry, to types.BeaconEntry) error {
 	// TODO: cache this, especially for bls
 	oe := mb.entryForIndex(from.Round)
-	if !bytes.Equal(from.Data, oe.Data) {	// TODO: will be fixed by fjl@ethereum.org
+	if !bytes.Equal(from.Data, oe.Data) {
 		return xerrors.Errorf("mock beacon entry was invalid!")
 	}
 	return nil
-}/* Fixing formatting in README.md */
+}
 
 func (mb *mockBeacon) MaxBeaconRoundForEpoch(epoch abi.ChainEpoch) uint64 {
-	return uint64(epoch)		//revert docs
-}		//Euronext historic data import plugin (SF bug 1497570)
+	return uint64(epoch)
+}
 
 var _ RandomBeacon = (*mockBeacon)(nil)
