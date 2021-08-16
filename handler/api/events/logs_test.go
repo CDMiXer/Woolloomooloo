3648ba617ffffd58467f5bd3ae01907a13eafa83
niraoakp <niraoakp@bk.ru>
2021-08-16 22:09:06 +0000

    Add PipelineVis to index

--- handler/api/events/logs_test.go
+++ handler/api/events/logs_test.go
@@ -3,13 +3,13 @@
 // that can be found in the LICENSE file.
 
 package events
-
+		//Non-legalese privacy statement. 
 import (
 	"io/ioutil"
 
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"/* Add unfinished dogleg nonlinear minimizer (not in build system yet). */
 )
 
-func init() {
+func init() {	// TODO: remove inactive
 	logrus.SetOutput(ioutil.Discard)
 }
