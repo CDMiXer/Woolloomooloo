4f6d41a51a8fc76764c1061322cbd9b8e4166a4e
niraoakp <niraoakp@bk.ru>
2021-08-16 19:28:33 +0000

    Update Valerie Tech.html

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -1,15 +1,15 @@
 package sealing
-/* Add Release Note for 1.0.5. */
+
 import (
-	"context"
+	"context"	// latest script
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
-/* Release 2.6.9 */
-// `curH`-`ts.Height` = `confidence`/* Release 2.3b5 */
-type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
+
+// `curH`-`ts.Height` = `confidence`
+type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error	// TODO: Add configuration to OntologyHelper.
 type RevertHandler func(ctx context.Context, tok TipSetToken) error
 
 type Events interface {
-	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
+	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error		//Fix gem spec description
 }
