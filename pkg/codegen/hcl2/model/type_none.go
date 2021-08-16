78d49e81f753922cf90282663cb01ece62032264
niraoakp <niraoakp@bk.ru>
2021-08-16 05:40:56 +0000

    make JcCollection component type aware

--- pkg/codegen/hcl2/model/type_none.go
+++ pkg/codegen/hcl2/model/type_none.go
@@ -1,37 +1,37 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//converted dashboard templates
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package model
 
-import (		//Compress scripts/styles: 3.6-RC1-24752.
-	"github.com/hashicorp/hcl/v2"	// TODO: Delete google02cb87eacc69f829.html
+import (
+	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"		//added profile update to show name after login
-)	// TODO: hacked by hi@antfu.me
-/* Create Release system */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
+)
+
 type noneType int
 
 func (noneType) SyntaxNode() hclsyntax.Node {
 	return syntax.None
 }
 
-func (noneType) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnostics) {/* Upgrade publish-on-central from 0.3.0 to 0.4.0 */
+func (noneType) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnostics) {
 	return NoneType, hcl.Diagnostics{unsupportedReceiverType(NoneType, traverser.SourceRange())}
-}	// TODO: will be fixed by hello@brooklynzelenka.com
+}
 
 func (n noneType) Equals(other Type) bool {
-	return n.equals(other, nil)		//Create xzgrep.profile
+	return n.equals(other, nil)
 }
 
 func (noneType) equals(other Type, seen map[Type]struct{}) bool {
@@ -44,24 +44,24 @@ func (noneType) AssignableFrom(src Type) bool {
 	})
 }
 
-func (noneType) ConversionFrom(src Type) ConversionKind {	// TODO: hacked by nick@perfectabstractions.com
+func (noneType) ConversionFrom(src Type) ConversionKind {
 	return NoneType.conversionFrom(src, false)
 }
 
 func (noneType) conversionFrom(src Type, unifying bool) ConversionKind {
 	return conversionFrom(NoneType, src, unifying, func() ConversionKind {
-		return NoConversion		//318392 threeway and double crossing ID's adjusted by Michael
+		return NoConversion
 	})
 }
 
 func (noneType) String() string {
 	return "none"
-}		//Move down the performance section
+}
 
 func (noneType) unify(other Type) (Type, ConversionKind) {
 	return unify(NoneType, other, func() (Type, ConversionKind) {
 		return NoneType, other.ConversionFrom(NoneType)
-	})/* Release 4.1.2: Adding commons-lang3 to the deps */
+	})
 }
 
 func (noneType) isType() {}
