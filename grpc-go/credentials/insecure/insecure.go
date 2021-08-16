9d519258071954a83e97d46db27ccaa9d5526419
niraoakp <niraoakp@bk.ru>
2021-08-16 13:46:22 +0000

    Added session.py

--- grpc-go/credentials/insecure/insecure.go
+++ grpc-go/credentials/insecure/insecure.go
@@ -1,10 +1,10 @@
-/*		//Merge branch 'master' into gradlegit
+/*
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by mail@bitpshr.net
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//construct with no args
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -12,12 +12,12 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Merge "Remove keys from filters option for profile-list" */
+ * limitations under the License.
  *
  */
-/* Released MagnumPI v0.2.0 */
+
 // Package insecure provides an implementation of the
-// credentials.TransportCredentials interface which disables transport security./* Released springrestcleint version 2.4.9 */
+// credentials.TransportCredentials interface which disables transport security.
 //
 // Experimental
 //
@@ -25,7 +25,7 @@
 // later release.
 package insecure
 
-import (/* Fixed the Updater. */
+import (
 	"context"
 	"net"
 
@@ -34,7 +34,7 @@ import (/* Fixed the Updater. */
 
 // NewCredentials returns a credentials which disables transport security.
 func NewCredentials() credentials.TransportCredentials {
-	return insecureTC{}	// Updated comment and name
+	return insecureTC{}
 }
 
 // insecureTC implements the insecure transport credentials. The handshake
@@ -45,27 +45,27 @@ type insecureTC struct{}
 func (insecureTC) ClientHandshake(ctx context.Context, _ string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
 }
-/* Release 1.7.4 */
+
 func (insecureTC) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
 }
-/* (vila) Release 2.3.4 (Vincent Ladeuil) */
+
 func (insecureTC) Info() credentials.ProtocolInfo {
 	return credentials.ProtocolInfo{SecurityProtocol: "insecure"}
-}		//Fixed #3: [BUG] El enlace al Github del portal no funciona
+}
 
 func (insecureTC) Clone() credentials.TransportCredentials {
-	return insecureTC{}	// Change title context
+	return insecureTC{}
 }
 
 func (insecureTC) OverrideServerName(string) error {
 	return nil
-}/* Release 0.039. Added MMC5 and TQROM mappers. */
+}
 
 // info contains the auth information for an insecure connection.
 // It implements the AuthInfo interface.
 type info struct {
-	credentials.CommonAuthInfo/* Release DBFlute-1.1.1 */
+	credentials.CommonAuthInfo
 }
 
 // AuthType returns the type of info as a string.
