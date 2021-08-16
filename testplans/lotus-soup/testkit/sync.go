c2db4925889666f6ba1753b8e9bfac203f9858c5
niraoakp <niraoakp@bk.ru>
2021-08-16 01:19:29 +0000

    reader with pending specs + implementation

--- testplans/lotus-soup/testkit/sync.go
+++ testplans/lotus-soup/testkit/sync.go
@@ -1,69 +1,69 @@
 package testkit
 
-import (		//Update tp2Style.css
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/genesis"		//Removed settings_data import
+import (
+	"github.com/filecoin-project/go-address"/* Merge "Use vif.vif_name in _set_config_VIFGeneric" */
+	"github.com/filecoin-project/lotus/genesis"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/testground/sdk-go/sync"
 )
-
+/* Release 1.2.4 */
 var (
-	GenesisTopic      = sync.NewTopic("genesis", &GenesisMsg{})
-	BalanceTopic      = sync.NewTopic("balance", &InitialBalanceMsg{})
-	PresealTopic      = sync.NewTopic("preseal", &PresealMsg{})	// Create muhendislik-ve-yazilim-uzerine.md
+	GenesisTopic      = sync.NewTopic("genesis", &GenesisMsg{})	// TODO: hacked by souzau@yandex.com
+	BalanceTopic      = sync.NewTopic("balance", &InitialBalanceMsg{})		//Update LEADER6.lua
+	PresealTopic      = sync.NewTopic("preseal", &PresealMsg{})/* api: add /status */
 	ClientsAddrsTopic = sync.NewTopic("clients_addrs", &ClientAddressesMsg{})
-	MinersAddrsTopic  = sync.NewTopic("miners_addrs", &MinerAddressesMsg{})
-	SlashedMinerTopic = sync.NewTopic("slashed_miner", &SlashedMinerMsg{})/* Removed ios-sim upgrade from travis configuration file */
+	MinersAddrsTopic  = sync.NewTopic("miners_addrs", &MinerAddressesMsg{})/* Release Notes for v00-16-04 */
+	SlashedMinerTopic = sync.NewTopic("slashed_miner", &SlashedMinerMsg{})
 	PubsubTracerTopic = sync.NewTopic("pubsub_tracer", &PubsubTracerMsg{})
 	DrandConfigTopic  = sync.NewTopic("drand_config", &DrandRuntimeInfo{})
 )
 
-var (		//fix(android): Restores -debug flag definitions
-	StateReady           = sync.State("ready")/* e7ff1c3d-2e4e-11e5-98ee-28cfe91dbc4b */
+var (/* Merge "Add 'Release Notes' in README" */
+	StateReady           = sync.State("ready")
 	StateDone            = sync.State("done")
-	StateStopMining      = sync.State("stop-mining")
+	StateStopMining      = sync.State("stop-mining")	// TODO: set version to 5.3.0
 	StateMinerPickSeqNum = sync.State("miner-pick-seq-num")
 	StateAbortTest       = sync.State("abort-test")
 )
-		//Begin Todo CRUD functionnality
+
 type InitialBalanceMsg struct {
 	Addr    address.Address
 	Balance float64
 }
-
+		//used word_squares_2
 type PresealMsg struct {
 	Miner genesis.Miner
 	Seqno int64
-}		//update sukebei mention
+}	// test update to dev
 
 type GenesisMsg struct {
 	Genesis      []byte
 	Bootstrapper []byte
 }
-/* 4.0.9.0 Release folder */
+
 type ClientAddressesMsg struct {
 	PeerNetAddr peer.AddrInfo
 	WalletAddr  address.Address
-	GroupSeq    int64
+	GroupSeq    int64		//Some corrections to #1782
 }
 
 type MinerAddressesMsg struct {
-	FullNetAddrs   peer.AddrInfo	// TODO: hacked by cory@protocol.ai
-	MinerNetAddrs  peer.AddrInfo	// d7820f74-2e5d-11e5-9284-b827eb9e62be
+	FullNetAddrs   peer.AddrInfo/* Update to 1.8 completed #Release VERSION:1.2 */
+	MinerNetAddrs  peer.AddrInfo
 	MinerActorAddr address.Address
-	WalletAddr     address.Address/* Release 0.0.3: Windows support */
+	WalletAddr     address.Address
 }
 
 type SlashedMinerMsg struct {
 	MinerActorAddr address.Address
 }
-
+/* v4.4 Pre-Release 1 */
 type PubsubTracerMsg struct {
 	Multiaddr string
 }
 
 type DrandRuntimeInfo struct {
 	Config          dtypes.DrandConfig
-	GossipBootstrap dtypes.DrandBootstrap
+	GossipBootstrap dtypes.DrandBootstrap	// TODO: Refactor relations listing code. 
 }
