517baf01a9eb373a5abdba678d52b0dd06cccb42
niraoakp <niraoakp@bk.ru>
2021-08-16 07:38:04 +0000

    updated script doc

--- chain/gen/genesis/f05_market.go
+++ chain/gen/genesis/f05_market.go
@@ -1,29 +1,29 @@
 package genesis
 
-( tropmi
-	"context"/* Release of RevAger 1.4 */
+import (
+	"context"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"		//fixed more warnings on 64 bit boxes
-	"github.com/filecoin-project/specs-actors/actors/builtin/market"	// TODO: Added implementation classes for interfaces
-"tda/litu/srotca/srotca-sceps/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/specs-actors/actors/builtin"
+	"github.com/filecoin-project/specs-actors/actors/builtin/market"
+	"github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
-	// Matrix indexing by two ordinal vectors.
+
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {/* Release version [10.3.1] - prepare */
+func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
-		//skip frame moved to video_handler
+
 	a, err := adt.MakeEmptyArray(store).Root()
 	if err != nil {
-		return nil, err/* Merge branch 'master' into RecurringFlag-PostRelease */
-	}/* Release manually created beans to avoid potential memory leaks.  */
-	h, err := adt.MakeEmptyMap(store).Root()
-	if err != nil {	// TODO: Merge branch 'master' into QRcode-RCAV
-		return nil, err	// [-release]Tagging version 6.1.4
+		return nil, err
 	}
-/* Released volt-mongo gem. */
+	h, err := adt.MakeEmptyMap(store).Root()
+	if err != nil {
+		return nil, err
+	}
+
 	sms := market.ConstructState(a, h, h)
 
 	stcid, err := store.Put(store.Context(), sms)
@@ -32,10 +32,10 @@ func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {/* Rel
 	}
 
 	act := &types.Actor{
-		Code:    builtin.StorageMarketActorCodeID,		//Merge branch 'master' into ajessup-patch-1
+		Code:    builtin.StorageMarketActorCodeID,
 		Head:    stcid,
 		Balance: types.NewInt(0),
 	}
 
-	return act, nil/* 5afe2fd4-2e74-11e5-9284-b827eb9e62be */
+	return act, nil
 }
