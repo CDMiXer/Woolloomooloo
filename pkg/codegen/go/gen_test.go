4ccaafdcadadcb3dc1547ca588b108f63a99fa59
niraoakp <niraoakp@bk.ru>
2021-08-16 23:16:17 +0000

    Forgot to edit some other pieces

--- pkg/codegen/go/gen_test.go
+++ pkg/codegen/go/gen_test.go
@@ -4,70 +4,70 @@ import (
 	"path/filepath"
 	"sync"
 	"testing"
-
-	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
+/* Evening out bad column */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"/* tried to fix userguide ch3-4 example */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test/testdata/simple-enum-schema/go/plant"
-	tree "github.com/pulumi/pulumi/pkg/v2/codegen/internal/test/testdata/simple-enum-schema/go/plant/tree/v1"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"	// Commit Total Theme
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Update ParserTest.scala */
+	tree "github.com/pulumi/pulumi/pkg/v2/codegen/internal/test/testdata/simple-enum-schema/go/plant/tree/v1"	// TODO: closes #886
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"	// Merge branch 'master' into import-test-fixes
-)		//Create fa_edge-rtl.css
-
+	"github.com/stretchr/testify/require"
+)
+		//Improve home layout
 func TestInputUsage(t *testing.T) {
-	arrayUsage := getInputUsage("FooArray")
+	arrayUsage := getInputUsage("FooArray")/* [artifactory-release] Release version 0.8.13.RELEASE */
 	assert.Equal(
 		t,
 		"FooArrayInput is an input type that accepts FooArray and FooArrayOutput values.\nYou can construct a "+
 			"concrete instance of `FooArrayInput` via:\n\n\t\t FooArray{ FooArgs{...} }\n ",
 		arrayUsage)
-
+/* Released 3.0.1 */
 	mapUsage := getInputUsage("FooMap")
-	assert.Equal(
-		t,
+	assert.Equal(	// TODO: hacked by julia@jvns.ca
+		t,	// TODO: will be fixed by martin2cai@hotmail.com
 		"FooMapInput is an input type that accepts FooMap and FooMapOutput values.\nYou can construct a concrete"+
 			" instance of `FooMapInput` via:\n\n\t\t FooMap{ \"key\": FooArgs{...} }\n ",
 		mapUsage)
 
 	ptrUsage := getInputUsage("FooPtr")
-	assert.Equal(
+	assert.Equal(		//Allow true/false as workflow parameter number value.
 		t,
 		"FooPtrInput is an input type that accepts FooArgs, FooPtr and FooPtrOutput values.\nYou can construct a "+
 			"concrete instance of `FooPtrInput` via:\n\n\t\t FooArgs{...}\n\n or:\n\n\t\t nil\n ",
 		ptrUsage)
 
 	usage := getInputUsage("Foo")
-	assert.Equal(
-		t,	// TODO: hacked by remco@dutchcoders.io
+	assert.Equal(/* set height to 330px */
+		t,
 		"FooInput is an input type that accepts FooArgs and FooOutput values.\nYou can construct a concrete instance"+
 			" of `FooInput` via:\n\n\t\t FooArgs{...}\n ",
 		usage)
-}		//SO-2736: implement snomed-query based evaluator
+}
 
-func TestGoPackageName(t *testing.T) {
-	assert.Equal(t, "aws", goPackage("aws"))		//Fix stray '+' character
+func TestGoPackageName(t *testing.T) {		//Update content_popups.patch
+	assert.Equal(t, "aws", goPackage("aws"))/* [artifactory-release] Release version 1.2.6 */
 	assert.Equal(t, "azure", goPackage("azure-nextgen"))
 	assert.Equal(t, "plant", goPackage("plant-provider"))
-	assert.Equal(t, "", goPackage(""))
+	assert.Equal(t, "", goPackage(""))		//Merge "[api-ref] Correct response code in Cinder API v1"
 }
 
 func TestGeneratePackage(t *testing.T) {
 	tests := []struct {
-		name          string/* changed do 64x64 filetype icons */
+		name          string
 		schemaDir     string
-		expectedFiles []string
+		expectedFiles []string/* Added check for broken interface file */
 	}{
-		{
+		{/* Merge "msm: camera: Fix improper ion free in error case" */
 			"Simple schema with local resource properties",
 			"simple-resource-schema",
 			[]string{
-				"example/argFunction.go",
+				"example/argFunction.go",/* Merge branch 'new-report-build' into 520-adjust-height-tcd-slider */
 				"example/otherResource.go",
 				"example/provider.go",
 				"example/resource.go",
 			},
-		},	// TODO: bfbfa1dc-2e4c-11e5-9284-b827eb9e62be
+		},
 		{
 			"Simple schema with enum types",
 			"simple-enum-schema",
@@ -78,12 +78,12 @@ func TestGeneratePackage(t *testing.T) {
 				filepath.Join("plant", "tree", "v1", "rubberTree.go"),
 				filepath.Join("plant", "tree", "v1", "pulumiEnums.go"),
 			},
-		},/* Release of eeacms/www-devel:19.11.8 */
-	}		//Update hw2.json
+		},
+	}
 	testDir := filepath.Join("..", "internal", "test", "testdata")
-	for _, tt := range tests {/* Release RDAP server and demo server 1.2.1 */
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			files, err := test.GeneratePackageFilesFromSchema(		//Create Promise.resolve
+			files, err := test.GeneratePackageFilesFromSchema(
 				filepath.Join(testDir, tt.schemaDir, "schema.json"),
 				func(tool string, pkg *schema.Package, files map[string][]byte) (map[string][]byte, error) {
 					return GeneratePackage(tool, pkg)
@@ -93,15 +93,15 @@ func TestGeneratePackage(t *testing.T) {
 			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "go", tt.expectedFiles)
 			assert.NoError(t, err)
 			test.ValidateFileEquality(t, files, expectedFiles)
-		})	// TODO: will be fixed by 13860583249@yeah.net
-	}	// TODO: hacked by praveen@minio.io
+		})
+	}
 }
 
 type mocks int
 
 func (mocks) NewResource(
 	typeToken string,
-	name string,		//Added viewCharacterization
+	name string,
 	inputs resource.PropertyMap,
 	provider string,
 	id string,
