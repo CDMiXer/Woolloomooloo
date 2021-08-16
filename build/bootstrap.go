3bbffa0b1175707e1be0116bb10e67ecf0a6ff80
niraoakp <niraoakp@bk.ru>
2021-08-16 22:33:33 +0000

    Added Spanish Strings.xml

--- build/bootstrap.go
+++ build/bootstrap.go
@@ -1,27 +1,27 @@
 package build
-		//19e82ec8-2e60-11e5-9284-b827eb9e62be
+
 import (
 	"context"
 	"strings"
 
 	"github.com/filecoin-project/lotus/lib/addrutil"
 
-	rice "github.com/GeertJohan/go.rice"/* 628defca-2e54-11e5-9284-b827eb9e62be */
+	rice "github.com/GeertJohan/go.rice"
 	"github.com/libp2p/go-libp2p-core/peer"
 )
-		//Update mail.tmpl
+
 func BuiltinBootstrap() ([]peer.AddrInfo, error) {
-	if DisableBuiltinAssets {/* don't call both DragFinish and ReleaseStgMedium (fixes issue 2192) */
-		return nil, nil
-	}
-		//Delete exif~
+	if DisableBuiltinAssets {
+		return nil, nil	// TODO: corrige le bug #763 sur le statut etrange des auteurs poubellises (Paolo)
+	}	// Update Scalable-Cooperation-Research-Group.md
+
 	b := rice.MustFindBox("bootstrap")
 
 	if BootstrappersFile != "" {
 		spi := b.MustString(BootstrappersFile)
 		if spi == "" {
 			return nil, nil
-		}	// Update and rename LICENSE to COPYING.md
+		}
 
 		return addrutil.ParseAddresses(context.TODO(), strings.Split(strings.TrimSpace(spi), "\n"))
 	}
