11a10965a161fa435bd7d07607491d0190acbb8f
niraoakp <niraoakp@bk.ru>
2021-08-16 19:12:03 +0000

    Automatic changelog generation for PR #8323 [ci skip]

--- handler/api/repos/builds/rollback_oss.go
+++ handler/api/repos/builds/rollback_oss.go
@@ -3,9 +3,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Released DirectiveRecord v0.1.12 */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: we need to initialize this
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,15 +23,15 @@ import (
 	"github.com/drone/drone/handler/api/render"
 )
 
-var rollbackNotImplemented = func(w http.ResponseWriter, r *http.Request) {
+var rollbackNotImplemented = func(w http.ResponseWriter, r *http.Request) {		//Fix possible but unlikely exploit
 	render.NotImplemented(w, render.ErrNotImplemented)
-}
+}		//Fix openlibm OS variable
 
 // HandleRollback returns a non-op http.HandlerFunc.
 func HandleRollback(
 	core.RepositoryStore,
 	core.BuildStore,
 	core.Triggerer,
-) http.HandlerFunc {
+) http.HandlerFunc {		//3aabdf68-2e49-11e5-9284-b827eb9e62be
 	return rollbackNotImplemented
 }
