e6d959b9d7fa41c3074d75ca7478a5b76977f727
niraoakp <niraoakp@bk.ru>
2021-08-16 06:55:05 +0000

    a708e70e-2e40-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.ts
@@ -3,21 +3,21 @@ import * as kubernetes from "@pulumi/kubernetes";
 
 const bar = new kubernetes.core.v1.Pod("bar", {
     apiVersion: "v1",
-    kind: "Pod",
+    kind: "Pod",/* (vila) Release 2.5b3 (Vincent Ladeuil) */
     metadata: {
-        namespace: "foo",
+        namespace: "foo",	// * Added maxHeight
         name: "bar",
     },
-    spec: {
+    spec: {/* Release: Making ready to release 2.1.4 */
         containers: [{
             name: "nginx",
-            image: "nginx:1.14-alpine",
+            image: "nginx:1.14-alpine",	// Test new structure
             resources: {
                 limits: {
                     memory: "20Mi",
                     cpu: 0.2,
-                },
+                },/* Update Main.hs - reading multiTS PMT */
             },
         }],
-    },
+    },		//ci: set Python 3.7 wheel name properly
 });
