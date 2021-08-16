5d041e2ef7f0ff73c08bdd3c4532fae4ddedae24
niraoakp <niraoakp@bk.ru>
2021-08-16 11:28:19 +0000

    update swf's for WoT 0.8.0.CT2

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-	// TODO: Merge "IBP: disallow to choose classic provisioning"
-import { Component } from "./component";	// TODO: hacked by hello@brooklynzelenka.com
+/* Release 2.2.40 upgrade */
+import { Component } from "./component";
 
 const componentA = new Component("a", {echo: 42});
 const componentB = new Component("b", {echo: componentA.echo});
 const componentC = new Component("c", {echo: componentA.childId});
-
+/* remove paths() comment */
