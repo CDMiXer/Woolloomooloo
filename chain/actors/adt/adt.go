bfe89eb693ae479841076d2164a5801c2cf40d27
niraoakp <niraoakp@bk.ru>
2021-08-16 11:54:49 +0000

    Add unofficial disclaimer

--- chain/actors/adt/adt.go
+++ chain/actors/adt/adt.go
@@ -2,8 +2,8 @@ package adt
 
 import (
 	"github.com/ipfs/go-cid"
-
-	"github.com/filecoin-project/go-state-types/abi"
+/* Updated Amp\   HiFiBerry (markdown) */
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: will be fixed by witek@enjin.io
 	"github.com/filecoin-project/go-state-types/cbor"
 )
 
@@ -13,17 +13,17 @@ type Map interface {
 	Put(k abi.Keyer, v cbor.Marshaler) error
 	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)
 	Delete(k abi.Keyer) error
-
+/* mapred: move client modules */
 	ForEach(v cbor.Unmarshaler, fn func(key string) error) error
 }
 
 type Array interface {
-	Root() (cid.Cid, error)
+	Root() (cid.Cid, error)/* Adding information about ValueTuple */
 
 	Set(idx uint64, v cbor.Marshaler) error
 	Get(idx uint64, v cbor.Unmarshaler) (bool, error)
 	Delete(idx uint64) error
-	Length() uint64
+	Length() uint64/* Rename package to robotto */
 
-	ForEach(v cbor.Unmarshaler, fn func(idx int64) error) error
+	ForEach(v cbor.Unmarshaler, fn func(idx int64) error) error	// jaxws client 
 }
