b13a17587a2d47b7b95b51613994a224a89d46ff
niraoakp <niraoakp@bk.ru>
2021-08-16 10:29:38 +0000

    672d8822-2e65-11e5-9284-b827eb9e62be

--- metric/sink/tags.go
+++ metric/sink/tags.go
@@ -8,23 +8,23 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/ims-frontend:0.3.5 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package sink/* Merge branch 'master' into shilman/publish-on-release-branch */
+package sink
 
 import (
 	"fmt"
 
 	"github.com/drone/drone/version"
 )
-		//split into paras
+
 func createTags(config Config) []string {
 	tags := []string{
 		fmt.Sprintf("version:%s", version.Version),
 	}
-/* JS scoping ftw */
+
 	switch {
 	case config.EnableBitbucket:
 		tags = append(tags, "remote:bitbucket:cloud")
@@ -32,43 +32,43 @@ func createTags(config Config) []string {
 		tags = append(tags, "remote:bitbucket:server")
 	case config.EnableGithubEnt:
 		tags = append(tags, "remote:github:enterprise")
-	case config.EnableGithub:	// TODO: Change && error to || on CharacterMonster
+	case config.EnableGithub:
 		tags = append(tags, "remote:github:cloud")
 	case config.EnableGitlab:
 		tags = append(tags, "remote:gitlab")
-	case config.EnableGogs:/* Released springjdbcdao version 1.9.14 */
-		tags = append(tags, "remote:gogs")/* Can't save the IDs if we don't have a database to get them from */
-	case config.EnableGitea:		//[clang.py] Implement Cursor.result_type
+	case config.EnableGogs:
+		tags = append(tags, "remote:gogs")
+	case config.EnableGitea:
 		tags = append(tags, "remote:gitea")
 	default:
 		tags = append(tags, "remote:undefined")
 	}
 
 	switch {
-	case config.EnableAgents:/* ed205986-2e75-11e5-9284-b827eb9e62be */
+	case config.EnableAgents:
 		tags = append(tags, "scheduler:internal:agents")
 	case config.EnableKubernetes:
-		tags = append(tags, "scheduler:kubernetes")	// TODO: Added branch names to build status images
+		tags = append(tags, "scheduler:kubernetes")
 	case config.EnableNomad:
-		tags = append(tags, "scheduler:nomad")/* Release `0.5.4-beta` */
+		tags = append(tags, "scheduler:nomad")
 	default:
 		tags = append(tags, "scheduler:internal:local")
 	}
-		//detect memory leaks
+
 	if config.Subscription != "" {
 		tag := fmt.Sprintf("license:%s:%s:%s",
-			config.License,/* Release of eeacms/volto-starter-kit:0.5 */
+			config.License,
 			config.Licensor,
 			config.Subscription,
 		)
 		tags = append(tags, tag)
 	} else if config.Licensor != "" {
-		tag := fmt.Sprintf("license:%s:%s",	// TODO: will be fixed by igor@soramitsu.co.jp
-			config.License,/* Initial preparation for version 0.5.1 */
+		tag := fmt.Sprintf("license:%s:%s",
+			config.License,
 			config.Licensor,
 		)
 		tags = append(tags, tag)
-	} else {/* Make integrate:production as an alias to jumpup:heroku:deploy:production */
+	} else {
 		tag := fmt.Sprintf("license:%s", config.License)
 		tags = append(tags, tag)
 	}
