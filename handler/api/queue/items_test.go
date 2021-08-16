5696e80a7d1d8e6163f318cf3f87b395b062cece
niraoakp <niraoakp@bk.ru>
2021-08-16 10:58:21 +0000

    Fix event keys for rules

--- handler/api/queue/items_test.go
+++ handler/api/queue/items_test.go
@@ -7,11 +7,11 @@
 package queue
 
 import (
-	"io/ioutil"/* c9f9d328-2e3f-11e5-9284-b827eb9e62be */
-	// New Device and Location classes for JSON usage of API
+	"io/ioutil"
+
 	"github.com/sirupsen/logrus"
 )
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)/* Create guiHandler.rbxs */
-}		//Tagged by Jenkins Task SVNTagging. Build:jenkins-YAKINDU_SCT2_CI-1911.
+	logrus.SetOutput(ioutil.Discard)
+}
