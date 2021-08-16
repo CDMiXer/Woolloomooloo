c9c5864be63356f24851214a21303ff9cb9b30ea
niraoakp <niraoakp@bk.ru>
2021-08-16 08:40:51 +0000

    version bump to 0.435.2

--- tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
@@ -1,12 +1,12 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Update sigcontext.h */
 
 import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {
+class Resource extends pulumi.ComponentResource {	// TODO: Merge "Dump json for nova.network.model.Model objects"
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
+        super("my:module:Resource", name, {}, opts);	// change attribution in footer to link to website instead of git repo
     }
-}
+}		//Add missing placeholders to translations
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive extends pulumi.ComponentResource {
@@ -16,4 +16,4 @@ class ComponentFive extends pulumi.ComponentResource {
         this.resource = new Resource("otherchild", {parent: this});
     }
 }
-const comp5 = new ComponentFive("comp5");
+const comp5 = new ComponentFive("comp5");		//Merged r1578:1594 from trunk.
