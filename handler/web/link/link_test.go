0ef717ae18151e07f996d0223ece9804405b9a1f
niraoakp <niraoakp@bk.ru>
2021-08-16 20:50:01 +0000

    Minimum PHP version should match minimum version of Laravel 5

--- handler/web/link/link_test.go
+++ handler/web/link/link_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//don't run clippy on itself twice in travis
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.		// fixing icons re #1292
+// You may obtain a copy of the License at/* Release notes 8.1.0 */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Issue #36: enabled custom file extensions at package level */
-//	// TODO: hacked by ng8eke@163.com
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Fix hawkular metric name */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//64250b72-2e3e-11e5-9284-b827eb9e62be
+// limitations under the License.
 
 package link
