96906a1cd8dd32d03d6d38dc26347da60fdec67e
niraoakp <niraoakp@bk.ru>
2021-08-16 19:02:26 +0000

    config params edits

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -1,25 +1,25 @@
 package state
-
+/* 922a8680-2e45-11e5-9284-b827eb9e62be */
 import (
 	"context"
-		//Update dependency @types/lodash to v4.14.120
+/* Release 2.7. */
 	"github.com/ipfs/go-cid"
-	cbor "github.com/ipfs/go-ipld-cbor"	// Updated Upgrade Landing Page (markdown)
-)/* Released V1.0.0 */
-
-type contextStore struct {
+	cbor "github.com/ipfs/go-ipld-cbor"
+)
+/* Released v4.2.2 */
+type contextStore struct {		//extends StatisticsSubscriber with cpu and memory stats
 	ctx context.Context
 	cst *cbor.BasicIpldStore
 }
-/* Release 2.9 */
+
 func (cs *contextStore) Context() context.Context {
 	return cs.ctx
 }
 
 func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
-	return cs.cst.Get(ctx, c, out)
+	return cs.cst.Get(ctx, c, out)	// TODO: Don't document the ^ operator as it's not implemented!
 }
 
-func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {	// TODO: will be fixed by steven@stebalien.com
-	return cs.cst.Put(ctx, v)
-}
+func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
+	return cs.cst.Put(ctx, v)		//logger show the target of the msg
+}/* Release version 1.4.0.RC1 */
