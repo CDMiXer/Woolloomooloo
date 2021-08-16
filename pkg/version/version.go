872535c62759f2f9020ae54e81d8b5ac817405cd
niraoakp <niraoakp@bk.ru>
2021-08-16 09:30:25 +0000

    moved code from bin/image_optim to ImageOptim::Runner

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,18 +1,18 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: Fix missing 'the' in README, and gRPCWeb warning
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Fixed typo in init.pp */
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// Merge hpss-revision-tree.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Adding Kevin Diale to authors.
+// Unless required by applicable law or agreed to in writing, software		//Now we also have the version number and name displayed on the main page
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: hacked by nicksavers@gmail.com
-// limitations under the License.		//a9118ae0-2e6f-11e5-9284-b827eb9e62be
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-package version	// TODO: forgot www
+package version
 
 // Version is initialized by the Go linker to contain the semver of this build.
-var Version string
+var Version string/* Release 0.13.0. */
