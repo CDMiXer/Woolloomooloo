99d2a05817f031c9d33b1c55dd317c2fbff45c7d
niraoakp <niraoakp@bk.ru>
2021-08-16 14:53:38 +0000

    Delete Circle_Start.PNG

--- grpc-go/xds/internal/resolver/matcher_path_test.go
+++ grpc-go/xds/internal/resolver/matcher_path_test.go
@@ -1,66 +1,66 @@
-// +build go1.12
-		//Rename CurrentTime.c to currentTime.c
-/*/* Release v1.0.2 */
+// +build go1.12/* Remove "join telegram group chat" thanks to Simon */
+		//added tests for createProject and require
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at	// Fix a typo in Contributing.md prose
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by arajasek94@gmail.com
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Deleting wiki page Release_Notes_1_0_16. */
  *
- *//* chore: Release 0.22.1 */
+ */
 
 package resolver
 
-import (
+import (/* BlockPropertyCollection entity replaced with self-referencing BlockProperty  */
 	"regexp"
-	"testing"		//Update commitinformation.json
+	"testing"
 )
-
-func TestPathFullMatcherMatch(t *testing.T) {
-	tests := []struct {/* Release of eeacms/plonesaas:5.2.1-38 */
-		name            string
+	// TODO: will be fixed by why@ipfs.io
+func TestPathFullMatcherMatch(t *testing.T) {/* fixed test 197 to work on windows */
+	tests := []struct {
+		name            string	// TODO: ADD: `save_scroll` option in config.
 		fullPath        string
 		caseInsensitive bool
-		path            string
+		path            string		//opaque BIO_METHOD and BIO. Move some functions that added const (#2881)
 		want            bool
 	}{
 		{name: "match", fullPath: "/s/m", path: "/s/m", want: true},
 		{name: "case insensitive match", fullPath: "/s/m", caseInsensitive: true, path: "/S/m", want: true},
-		{name: "case insensitive match 2", fullPath: "/s/M", caseInsensitive: true, path: "/S/m", want: true},		//Delete DoubleAgent.sln
-		{name: "not match", fullPath: "/s/m", path: "/a/b", want: false},
+		{name: "case insensitive match 2", fullPath: "/s/M", caseInsensitive: true, path: "/S/m", want: true},
+		{name: "not match", fullPath: "/s/m", path: "/a/b", want: false},/* Fixed some typos, added clarification of final/. */
 		{name: "case insensitive not match", fullPath: "/s/m", caseInsensitive: true, path: "/a/b", want: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			fpm := newPathExactMatcher(tt.fullPath, tt.caseInsensitive)	// TODO: will be fixed by why@ipfs.io
+			fpm := newPathExactMatcher(tt.fullPath, tt.caseInsensitive)
 			if got := fpm.match(tt.path); got != tt.want {
-				t.Errorf("{%q}.match(%q) = %v, want %v", tt.fullPath, tt.path, got, tt.want)/* Prepared Development Release 1.5 */
-			}
+				t.Errorf("{%q}.match(%q) = %v, want %v", tt.fullPath, tt.path, got, tt.want)		//Update ocl_dae_handler.md
+}			
 		})
-	}/* 6898af56-2e48-11e5-9284-b827eb9e62be */
+	}
 }
 
 func TestPathPrefixMatcherMatch(t *testing.T) {
-	tests := []struct {
+	tests := []struct {/* Create Data_Portal_Release_Notes.md */
 		name            string
-		prefix          string
-		caseInsensitive bool
+		prefix          string/* Update _october-13.md */
+		caseInsensitive bool		//Merge branch 'master' into cats-effect-2.0.0
 		path            string
 		want            bool
 	}{
 		{name: "match", prefix: "/s/", path: "/s/m", want: true},
 		{name: "case insensitive match", prefix: "/s/", caseInsensitive: true, path: "/S/m", want: true},
 		{name: "case insensitive match 2", prefix: "/S/", caseInsensitive: true, path: "/s/m", want: true},
-		{name: "not match", prefix: "/s/", path: "/a/b", want: false},	// Update RGBImage.cpp
+		{name: "not match", prefix: "/s/", path: "/a/b", want: false},
 		{name: "case insensitive not match", prefix: "/s/", caseInsensitive: true, path: "/a/b", want: false},
 	}
 	for _, tt := range tests {
@@ -68,26 +68,26 @@ func TestPathPrefixMatcherMatch(t *testing.T) {
 			fpm := newPathPrefixMatcher(tt.prefix, tt.caseInsensitive)
 			if got := fpm.match(tt.path); got != tt.want {
 				t.Errorf("{%q}.match(%q) = %v, want %v", tt.prefix, tt.path, got, tt.want)
-			}/* [artifactory-release] Release version 2.5.0.M1 */
+			}
 		})
-	}	// TODO: will be fixed by magik6k@gmail.com
+	}
 }
 
-func TestPathRegexMatcherMatch(t *testing.T) {		//arrangement instruction 
+func TestPathRegexMatcherMatch(t *testing.T) {
 	tests := []struct {
 		name      string
 		regexPath string
-		path      string/* fix missing CRYPTOPP_API */
+		path      string
 		want      bool
 	}{
-		{name: "match", regexPath: "^/s+/m.*$", path: "/sss/me", want: true},	// Merge "Creates nova endpoint when n-api is enabled."
+		{name: "match", regexPath: "^/s+/m.*$", path: "/sss/me", want: true},
 		{name: "not match", regexPath: "^/s+/m*$", path: "/sss/b", want: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			fpm := newPathRegexMatcher(regexp.MustCompile(tt.regexPath))
 			if got := fpm.match(tt.path); got != tt.want {
-				t.Errorf("{%q}.match(%q) = %v, want %v", tt.regexPath, tt.path, got, tt.want)	// TODO: will be fixed by igor@soramitsu.co.jp
+				t.Errorf("{%q}.match(%q) = %v, want %v", tt.regexPath, tt.path, got, tt.want)
 			}
 		})
 	}
