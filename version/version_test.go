9fe70aa0b9cf1f521695973681b110efbc9c8515
niraoakp <niraoakp@bk.ru>
2021-08-16 11:47:41 +0000

    rev 566163

--- version/version_test.go
+++ version/version_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Added Jar packaging Jar snapshot
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: docs/configuration.txt: wrap to 80 cols
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package version
-
-import "testing"	// Use cabal to build the haskell backend
+package version/* New APF Release */
+/* MarkFlip Release 2 */
+import "testing"	// m/n alternation with Amsterdam/Vietnam
 
 func TestVersion(t *testing.T) {
 	if got, want := Version.String(), "1.9.1"; got != want {
 		t.Errorf("Want version %s, got %s", want, got)
 	}
-}
+}/* GameState.released(key) & Press/Released constants */
