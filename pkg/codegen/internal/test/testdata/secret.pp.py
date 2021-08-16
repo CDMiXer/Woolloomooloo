63cbb305e1748134763fb475a2b0a6677f437c79
niraoakp <niraoakp@bk.ru>
2021-08-16 21:43:04 +0000

    Hidding Tomas

--- pkg/codegen/internal/test/testdata/secret.pp.py
+++ pkg/codegen/internal/test/testdata/secret.pp.py
@@ -1,4 +1,4 @@
-import pulumi		//Adaugat functionalitate butonului de logout
+import pulumi	// TODO: default APP_HOST set to IP
 import pulumi_aws as aws
 
 db_cluster = aws.rds.Cluster("dbCluster", master_password=pulumi.secret("foobar"))
