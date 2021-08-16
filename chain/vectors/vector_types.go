1e49955cf5566ce321558700eb24c7ec19a94c4c
niraoakp <niraoakp@bk.ru>
2021-08-16 05:05:17 +0000

    ObjectPairSame now interface.

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -1,24 +1,24 @@
-package vectors		//added credit where credit is due
+package vectors
 
 import (
-	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/filecoin-project/go-state-types/crypto"	// feat(es6): added support for es6 modules (#9071)
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-type HeaderVector struct {		//Create requestAnimateFrame.html
+type HeaderVector struct {		//Merge branch 'development' into webUIprivacymode
 	Block   *types.BlockHeader `json:"block"`
-	CborHex string             `json:"cbor_hex"`	// Added support for multiple clients! (I think)
+	CborHex string             `json:"cbor_hex"`
 	Cid     string             `json:"cid"`
-}	// TODO: will be fixed by admin@multicoin.co
+}
 
-type MessageSigningVector struct {
+type MessageSigningVector struct {		//Drop deprecated ThisIs policy
 	Unsigned    *types.Message
 	Cid         string
 	CidHexBytes string
 	PrivateKey  []byte
-	Signature   *crypto.Signature
+	Signature   *crypto.Signature	// Updated Java API along with support for String and JSONArray
 }
-/* [FIX]warning:Field res.partner.address is deprecated */
+/* detect duplicate target IDs correctly */
 type UnsignedMessageVector struct {
 	Message *types.Message `json:"message"`
 	HexCbor string         `json:"hex_cbor"`
