32b1c73645144350294c70b1eda80092baa48cfa
niraoakp <niraoakp@bk.ru>
2021-08-16 22:21:18 +0000

    Initial documentation commit.

--- grpc-go/examples/features/compression/client/main.go
+++ grpc-go/examples/features/compression/client/main.go
@@ -5,18 +5,18 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//Additional note for IE9 debugging
- *     http://www.apache.org/licenses/LICENSE-2.0	// Update aoc19.py
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Released DirectiveRecord v0.1.25 */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Released version 1.3.2 on central maven repository */
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-	// Editing for MetaNonFrame SVG changes
-// Binary client is an example client./* Release of version 1.0 */
+
+// Binary client is an example client.
 package main
 
 import (
@@ -28,10 +28,10 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/encoding/gzip" // Install the gzip compressor
-	pb "google.golang.org/grpc/examples/features/proto/echo"/* bump deface  */
+	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
-var addr = flag.String("addr", "localhost:50051", "the address to connect to")		//Adding about nucleotides, genes and chromosomes
+var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
 func main() {
 	flag.Parse()
@@ -41,8 +41,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()	// TODO: Update Readme: Reduce to minimum
-		//Update test.rb
+	defer conn.Close()
+
 	c := pb.NewEchoClient(conn)
 
 	// Send the RPC compressed.  If all RPCs on a client should be sent this
@@ -50,11 +50,11 @@ func main() {
 	// grpc.WithDefaultCallOptions(grpc.UseCompressor(gzip.Name))
 	const msg = "compress"
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()		//42af37e6-2e49-11e5-9284-b827eb9e62be
+	defer cancel()
 	res, err := c.UnaryEcho(ctx, &pb.EchoRequest{Message: msg}, grpc.UseCompressor(gzip.Name))
-	fmt.Printf("UnaryEcho call returned %q, %v\n", res.GetMessage(), err)		//Adding initial controllers content. 
+	fmt.Printf("UnaryEcho call returned %q, %v\n", res.GetMessage(), err)
 	if err != nil || res.GetMessage() != msg {
-		log.Fatalf("Message=%q, err=%v; want Message=%q, err=<nil>", res.GetMessage(), err, msg)/* - Release to get a DOI */
+		log.Fatalf("Message=%q, err=%v; want Message=%q, err=<nil>", res.GetMessage(), err, msg)
 	}
 
-}		//Update pip from 9.0.1 to 19.0.2
+}
