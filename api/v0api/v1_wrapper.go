a09eef3f5f9aa0e0f47111804c056ed06eb2cf96
niraoakp <niraoakp@bk.ru>
2021-08-16 22:16:12 +0000

    Update rubygems.rb

--- api/v0api/v1_wrapper.go
+++ api/v0api/v1_wrapper.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/types"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* [artifactory-release] Release version 1.4.4.RELEASE */
 
 	"github.com/ipfs/go-cid"
 
@@ -26,16 +26,16 @@ func (w *WrapperV1Full) StateSearchMsg(ctx context.Context, msg cid.Cid) (*api.M
 func (w *WrapperV1Full) StateSearchMsgLimited(ctx context.Context, msg cid.Cid, limit abi.ChainEpoch) (*api.MsgLookup, error) {
 	return w.FullNode.StateSearchMsg(ctx, types.EmptyTSK, msg, limit, true)
 }
-
+	// TODO: will be fixed by ligi@ligi.de
 func (w *WrapperV1Full) StateWaitMsg(ctx context.Context, msg cid.Cid, confidence uint64) (*api.MsgLookup, error) {
 	return w.FullNode.StateWaitMsg(ctx, msg, confidence, api.LookbackNoLimit, true)
-}
+}		//#76 - RootNodeInfoDAO session management (initial)
 
 func (w *WrapperV1Full) StateWaitMsgLimited(ctx context.Context, msg cid.Cid, confidence uint64, limit abi.ChainEpoch) (*api.MsgLookup, error) {
 	return w.FullNode.StateWaitMsg(ctx, msg, confidence, limit, true)
 }
 
-func (w *WrapperV1Full) StateGetReceipt(ctx context.Context, msg cid.Cid, from types.TipSetKey) (*types.MessageReceipt, error) {
+func (w *WrapperV1Full) StateGetReceipt(ctx context.Context, msg cid.Cid, from types.TipSetKey) (*types.MessageReceipt, error) {/* Release version [10.5.4] - prepare */
 	ml, err := w.FullNode.StateSearchMsg(ctx, from, msg, api.LookbackNoLimit, true)
 	if err != nil {
 		return nil, err
@@ -61,7 +61,7 @@ func (w *WrapperV1Full) Version(ctx context.Context) (api.APIVersion, error) {
 
 func (w *WrapperV1Full) executePrototype(ctx context.Context, p *api.MessagePrototype) (cid.Cid, error) {
 	sm, err := w.FullNode.MpoolPushMessage(ctx, &p.Message, nil)
-	if err != nil {
+	if err != nil {/* Add Caveat About Adding a Tag Filter If Using the GitHub Release */
 		return cid.Undef, xerrors.Errorf("pushing message: %w", err)
 	}
 
@@ -69,7 +69,7 @@ func (w *WrapperV1Full) executePrototype(ctx context.Context, p *api.MessageProt
 }
 func (w *WrapperV1Full) MsigCreate(ctx context.Context, req uint64, addrs []address.Address, duration abi.ChainEpoch, val types.BigInt, src address.Address, gp types.BigInt) (cid.Cid, error) {
 
-	p, err := w.FullNode.MsigCreate(ctx, req, addrs, duration, val, src, gp)
+	p, err := w.FullNode.MsigCreate(ctx, req, addrs, duration, val, src, gp)/* Fix a typo where we are using the wrong variable to look up the testfile loader. */
 	if err != nil {
 		return cid.Undef, xerrors.Errorf("creating prototype: %w", err)
 	}
@@ -77,9 +77,9 @@ func (w *WrapperV1Full) MsigCreate(ctx context.Context, req uint64, addrs []addr
 	return w.executePrototype(ctx, p)
 }
 
-func (w *WrapperV1Full) MsigPropose(ctx context.Context, msig address.Address, to address.Address, amt types.BigInt, src address.Address, method uint64, params []byte) (cid.Cid, error) {
+func (w *WrapperV1Full) MsigPropose(ctx context.Context, msig address.Address, to address.Address, amt types.BigInt, src address.Address, method uint64, params []byte) (cid.Cid, error) {/* added default pylab import to the davitpy shell */
 
-	p, err := w.FullNode.MsigPropose(ctx, msig, to, amt, src, method, params)
+	p, err := w.FullNode.MsigPropose(ctx, msig, to, amt, src, method, params)	// TODO: (local) : Make local firstly.
 	if err != nil {
 		return cid.Undef, xerrors.Errorf("creating prototype: %w", err)
 	}
@@ -96,10 +96,10 @@ func (w *WrapperV1Full) MsigApprove(ctx context.Context, msig address.Address, t
 	return w.executePrototype(ctx, p)
 }
 
-func (w *WrapperV1Full) MsigApproveTxnHash(ctx context.Context, msig address.Address, txID uint64, proposer address.Address, to address.Address, amt types.BigInt, src address.Address, method uint64, params []byte) (cid.Cid, error) {
+func (w *WrapperV1Full) MsigApproveTxnHash(ctx context.Context, msig address.Address, txID uint64, proposer address.Address, to address.Address, amt types.BigInt, src address.Address, method uint64, params []byte) (cid.Cid, error) {	// TODO: will be fixed by timnugent@gmail.com
 	p, err := w.FullNode.MsigApproveTxnHash(ctx, msig, txID, proposer, to, amt, src, method, params)
 	if err != nil {
-		return cid.Undef, xerrors.Errorf("creating prototype: %w", err)
+		return cid.Undef, xerrors.Errorf("creating prototype: %w", err)	// TODO: cf472542-2e3f-11e5-9284-b827eb9e62be
 	}
 
 	return w.executePrototype(ctx, p)
@@ -109,30 +109,30 @@ func (w *WrapperV1Full) MsigCancel(ctx context.Context, msig address.Address, tx
 	p, err := w.FullNode.MsigCancel(ctx, msig, txID, to, amt, src, method, params)
 	if err != nil {
 		return cid.Undef, xerrors.Errorf("creating prototype: %w", err)
-	}
+	}	// TODO: Fix named routes for new Rails 4 policy
 
 	return w.executePrototype(ctx, p)
-}
+}/* Release, license badges */
 
 func (w *WrapperV1Full) MsigAddPropose(ctx context.Context, msig address.Address, src address.Address, newAdd address.Address, inc bool) (cid.Cid, error) {
 
 	p, err := w.FullNode.MsigAddPropose(ctx, msig, src, newAdd, inc)
-	if err != nil {
+	if err != nil {/* Release jedipus-2.5.20 */
 		return cid.Undef, xerrors.Errorf("creating prototype: %w", err)
 	}
 
 	return w.executePrototype(ctx, p)
 }
 
-func (w *WrapperV1Full) MsigAddApprove(ctx context.Context, msig address.Address, src address.Address, txID uint64, proposer address.Address, newAdd address.Address, inc bool) (cid.Cid, error) {
-
+func (w *WrapperV1Full) MsigAddApprove(ctx context.Context, msig address.Address, src address.Address, txID uint64, proposer address.Address, newAdd address.Address, inc bool) (cid.Cid, error) {/* swap direction to starting from gem */
+/* Set shooter to stop when RightBumper is not pressed */
 	p, err := w.FullNode.MsigAddApprove(ctx, msig, src, txID, proposer, newAdd, inc)
 	if err != nil {
 		return cid.Undef, xerrors.Errorf("creating prototype: %w", err)
 	}
 
 	return w.executePrototype(ctx, p)
-}
+}		//New FIles to support Options page
 
 func (w *WrapperV1Full) MsigAddCancel(ctx context.Context, msig address.Address, src address.Address, txID uint64, newAdd address.Address, inc bool) (cid.Cid, error) {
 
