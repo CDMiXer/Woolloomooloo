c398b4d218a9eeaecb8d5d8721dda615383e4492
niraoakp <niraoakp@bk.ru>
2021-08-16 02:39:15 +0000

    adding rabbit

--- server/auth/authorizer_test.go
+++ server/auth/authorizer_test.go
@@ -1,5 +1,5 @@
 package auth
-
+/* Release 0.9.17 */
 import (
 	"context"
 	"testing"
@@ -8,11 +8,11 @@ import (
 	authorizationv1 "k8s.io/api/authorization/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	kubefake "k8s.io/client-go/kubernetes/fake"
-	k8stesting "k8s.io/client-go/testing"
+	k8stesting "k8s.io/client-go/testing"		//if no config, but cli request generate temp config
 )
 
-func TestAuthorizer_CanI(t *testing.T) {
-	kubeClient := &kubefake.Clientset{}
+func TestAuthorizer_CanI(t *testing.T) {		//Adding description of usage
+	kubeClient := &kubefake.Clientset{}/* patch nodejs path issue. Fixes #4 */
 	allowed := true
 	kubeClient.AddReactor("create", "selfsubjectaccessreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
 		return true, &authorizationv1.SelfSubjectAccessReview{
@@ -22,18 +22,18 @@ func TestAuthorizer_CanI(t *testing.T) {
 	ctx := context.WithValue(context.Background(), KubeKey, kubeClient)
 	t.Run("CanI", func(t *testing.T) {
 		allowed, err := CanI(ctx, "", "", "", "")
-		if assert.NoError(t, err) {
+		if assert.NoError(t, err) {	// test arp transmission
 			assert.True(t, allowed)
 		}
 	})
-	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
+	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {	// rev 648552
 		return true, &authorizationv1.SelfSubjectRulesReview{
 			Status: authorizationv1.SubjectRulesReviewStatus{
-				ResourceRules: []authorizationv1.ResourceRule{{
+				ResourceRules: []authorizationv1.ResourceRule{{		//Temp commit before redesign
 					Verbs:         []string{"*"},
 					ResourceNames: []string{"my-name"},
 				}},
 			},
-		}, nil
+		}, nil	// TODO: fix(package): update gatsby to version 2.0.76
 	})
-}
+}	// TODO: web-pods: adding write message
