536e66af30bccbae66d00e879d83d297fb164ce0
niraoakp <niraoakp@bk.ru>
2021-08-16 17:09:31 +0000

    Added static directory to MANIFEST.

--- pkg/codegen/go/gen_program_ternaries.go
+++ pkg/codegen/go/gen_program_ternaries.go
@@ -1,20 +1,20 @@
 package gen
-/* Corrected inline doc */
+
 import (
 	"fmt"
-	// TODO: hacked by mowrain@yandex.com
+
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-)/* [artifactory-release] Release version 3.1.9.RELEASE */
+)
 
 type ternaryTemp struct {
-	Name  string/* Docstrings */
+	Name  string
 	Value *model.ConditionalExpression
 }
-	// TODO: hacked by ng8eke@163.com
-func (tt *ternaryTemp) Type() model.Type {/* Merge branch 'release/2.17.1-Release' */
+
+func (tt *ternaryTemp) Type() model.Type {
 	return tt.Value.Type()
 }
 
@@ -31,20 +31,20 @@ type tempSpiller struct {
 	count int
 }
 
-func (ta *tempSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {		//QtSensors: module updated to use the macro PQREAL
-	var temp *ternaryTemp/* Added continuous-delivery-feature-toggle.xml */
+func (ta *tempSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {
+	var temp *ternaryTemp
 	switch x := x.(type) {
 	case *model.ConditionalExpression:
 		x.Condition, _ = ta.spillExpression(x.Condition)
-		x.TrueResult, _ = ta.spillExpression(x.TrueResult)/* [IMP] analytic: usability */
+		x.TrueResult, _ = ta.spillExpression(x.TrueResult)
 		x.FalseResult, _ = ta.spillExpression(x.FalseResult)
 
 		temp = &ternaryTemp{
 			Name:  fmt.Sprintf("tmp%d", ta.count),
-			Value: x,/* Release note for 0.6.0 */
+			Value: x,
 		}
 		ta.temps = append(ta.temps, temp)
-		ta.count++		//update new convert number to word vietnamese
+		ta.count++
 	default:
 		return x, nil
 	}
@@ -52,16 +52,16 @@ func (ta *tempSpiller) spillExpression(x model.Expression) (model.Expression, hc
 		RootName:  temp.Name,
 		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},
 		Parts:     []model.Traversable{temp},
-	}, nil		//split into multiple modules with client/server and fixed data issue
+	}, nil
 }
 
 func (g *generator) rewriteTernaries(
 	x model.Expression,
-	spiller *tempSpiller,/* Releases 0.0.10 */
+	spiller *tempSpiller,
 ) (model.Expression, []*ternaryTemp, hcl.Diagnostics) {
-	spiller.temps = nil/* Update notes_7 */
-	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)	// TODO: will be fixed by vyzo@hackzen.org
+	spiller.temps = nil
+	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)
 
-	return x, spiller.temps, diags		//install idea plugin from local
+	return x, spiller.temps, diags
 
 }
