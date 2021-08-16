60447dbd2623c37db5e8fbe205d6a9e009948272
niraoakp <niraoakp@bk.ru>
2021-08-16 17:49:58 +0000

    Make sure interpolated string literals still work with newline escapes

--- handler/api/repos/crons/find_test.go
+++ handler/api/repos/crons/find_test.go
@@ -1,74 +1,74 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* fix -Wunused-variable warning in Release mode */
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* Proxmox 6 Release Key */
 
-package crons/* Fix link to ReleaseNotes.md */
+package crons	// Make winesteam compatible with configurable wine path
 
-import (/* Add merged commits to change log. */
-	"context"		//removed user dictioary menu item and unused resource entries
-	"encoding/json"	// TODO: Fixed command-line debugging on Mac OS X.
-	"net/http"	// TODO: hacked by alex.gaynor@gmail.com
-	"net/http/httptest"
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"/* Test Release RC8 */
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"
-
-	"github.com/go-chi/chi"/* claro_html_tab_bar : phpdoc + change arguments order */
-	"github.com/golang/mock/gomock"	// TODO: hacked by aeongrp@outlook.com
+	"github.com/drone/drone/mock"/* Released Code Injection Plugin */
+/* Add Snyk vulnerability badge */
+	"github.com/go-chi/chi"
+	"github.com/golang/mock/gomock"/* Rename registration.py to reg_projectManager.py */
 	"github.com/google/go-cmp/cmp"
-)
+)/* Optimize the Deferred object.[skip ci] */
 
 func TestHandleFind(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()	// Added `fail_if_no_header` to ChunkedUploadView
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
-
-	crons := mock.NewMockCronStore(controller)		//Renaming attribute name.
+/* Capitalised admin menu commands */
+	crons := mock.NewMockCronStore(controller)
 	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")	// TODO: will be fixed by igor@soramitsu.co.jp
-	c.URLParams.Add("cron", "nightly")
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("cron", "nightly")/* Update init.sls */
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()		//Create ext.php
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-		//Updates README. Makes zip file downloadable.
-	HandleFind(repos, crons).ServeHTTP(w, r)		//Create OwnExceptionTest.java
-	if got, want := w.Code, http.StatusOK; want != got {/* Release 3.2 060.01. */
+
+	HandleFind(repos, crons).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+	// List of related Ruby DOCX projects.
 	got, want := &core.Cron{}, dummyCron
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
+	if diff := cmp.Diff(got, want); len(diff) != 0 {	// TODO: Formattare stringhe output
 		t.Errorf(diff)
 	}
 }
 
-func TestHandleFind_RepoNotFound(t *testing.T) {
+func TestHandleFind_RepoNotFound(t *testing.T) {/* CHANGELOG.md: Mention #7, add commit references */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)		//Update react-native-aes.podspec
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)/* KeyMapper now uses HashMap instead of Hashtable internally - fixed test */
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")/* b79eff70-2e51-11e5-9284-b827eb9e62be */
-	c.URLParams.Add("cron", "nightly")		//Rename project to vendor.js
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("cron", "nightly")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)		//a91ae0f6-2e54-11e5-9284-b827eb9e62be
+	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
