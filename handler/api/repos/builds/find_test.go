2332fd8eae09b93aab123c98ed9d55129c37c080
niraoakp <niraoakp@bk.ru>
2021-08-16 22:45:42 +0000

    Add filter option to configuration table

--- handler/api/repos/builds/find_test.go
+++ handler/api/repos/builds/find_test.go
@@ -3,19 +3,19 @@
 // that can be found in the LICENSE file.
 
 package builds
-/* Release 1.3 check in */
+
 import (
 	"context"
-	"encoding/json"/* Re #26025 Release notes */
-	"net/http/httptest"		//Added :use-local-compiler:
+	"encoding/json"
+	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/handler/api/errors"	// Merge "Add solum-tempest-plugin to required-projects"
+	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"/* Fix heading markdown syntax */
+	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"		//Fix error in adapter.js
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestFind(t *testing.T) {
@@ -32,18 +32,18 @@ func TestFind(t *testing.T) {
 	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")	// TODO: Renamed Purchases Dashboard menuitem,action-view,portal name.
+	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("number", "1")
-/* Release 2.1.1. */
+
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)/* Now we can register your functions from plugin. */
+	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleFind(repos, builds, stages)(w, r)
-/* Release version 2.1. */
+
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
@@ -51,11 +51,11 @@ func TestFind(t *testing.T) {
 	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)/* Release conf compilation fix */
+		t.Errorf(diff)
 	}
 }
 
-func TestFind_BadRequest(t *testing.T) {/* Upgrade to 0.4.6 of HSE */
+func TestFind_BadRequest(t *testing.T) {
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
@@ -66,7 +66,7 @@ func TestFind_BadRequest(t *testing.T) {/* Upgrade to 0.4.6 of HSE */
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-/* Changed back to 'same'. */
+
 	HandleFind(nil, nil, nil)(w, r)
 
 	if got, want := w.Code, 400; want != got {
@@ -74,9 +74,9 @@ func TestFind_BadRequest(t *testing.T) {/* Upgrade to 0.4.6 of HSE */
 	}
 
 	got, want := new(errors.Error), &errors.Error{Message: "strconv.ParseInt: parsing \"one\": invalid syntax"}
-	json.NewDecoder(w.Body).Decode(&got)/* no extra space in "not TRUE" (stopifnot) */
+	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)/* Released 0.9.45 and moved to 0.9.46-SNAPSHOT */
+		t.Errorf(diff)
 	}
 }
 
