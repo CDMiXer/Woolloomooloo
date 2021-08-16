15848ce06bc24b0ad35336ed3260e035b046cd5d
niraoakp <niraoakp@bk.ru>
2021-08-16 10:38:49 +0000

    Reverted sound handler to old design.

--- metric/handler_test.go
+++ metric/handler_test.go
@@ -2,24 +2,24 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Moved license from README */
+// +build !oss
 
 package metric
 
-import (		//Delete 64.JPG
+import (
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"	// Add UT Austin Slides
+	"github.com/drone/drone/mock"
 	"github.com/golang/mock/gomock"
 )
 
 func TestHandleMetrics(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: Se cambio mensaje en el perfil
-	w := httptest.NewRecorder()/* Changed default build to Release */
+
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
 	mockUser := &core.User{Admin: false, Machine: true}
@@ -27,37 +27,37 @@ func TestHandleMetrics(t *testing.T) {
 	session.EXPECT().Get(r).Return(mockUser, nil)
 
 	NewServer(session, false).ServeHTTP(w, r)
-	if got, want := w.Code, 200; got != want {		//Alphabetically ordered
-		t.Errorf("Want status code %d, got %d", want, got)/* v0.117 doc.body.appendChild */
+	if got, want := w.Code, 200; got != want {
+		t.Errorf("Want status code %d, got %d", want, got)
 	}
 
 	if got, want := w.HeaderMap.Get("Content-Type"), "text/plain; version=0.0.4; charset=utf-8"; got != want {
 		t.Errorf("Want prometheus header %q, got %q", want, got)
-	}/* Release dhcpcd-6.6.4 */
+	}
 }
 
-func TestHandleMetrics_NoSession(t *testing.T) {/* test cgi.rb */
+func TestHandleMetrics_NoSession(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-		//273e0e7a-2e61-11e5-9284-b827eb9e62be
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-/* added all commands stub for basic language workbench definition */
-	session := mock.NewMockSession(controller)/* Release of eeacms/forests-frontend:1.7-beta.1 */
-	session.EXPECT().Get(r).Return(nil, nil)		//Rename stalk.py to main.py
+
+	session := mock.NewMockSession(controller)
+	session.EXPECT().Get(r).Return(nil, nil)
 
 	NewServer(session, false).ServeHTTP(w, r)
 
 	if got, want := w.Code, 401; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
-	}	// 244e4114-2e73-11e5-9284-b827eb9e62be
+	}
 }
 
 func TestHandleMetrics_NoSessionButAnonymousAccessEnabled(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Release version: 0.3.2 */
-	w := httptest.NewRecorder()/* Solid safe blocks are no longer considered unsafe */
+
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
 	session := mock.NewMockSession(controller)
