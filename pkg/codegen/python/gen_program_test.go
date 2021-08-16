5088f2c3285a052b94b628ebe710e978c08e3382
niraoakp <niraoakp@bk.ru>
2021-08-16 03:12:25 +0000

    Merge "Added documentation to BayModel attrs"

--- pkg/codegen/python/gen_program_test.go
+++ pkg/codegen/python/gen_program_test.go
@@ -3,41 +3,41 @@ package python
 import (
 	"bytes"
 	"io/ioutil"
-	"path/filepath"		//Added 276 Carbonbeauty@2x
-	"strings"	// set format without reflection now
-	"testing"	// TODO: Print help when args is empty.
-/* Update Control_pad.md */
+	"path/filepath"
+	"strings"
+	"testing"
+
 	"github.com/hashicorp/hcl/v2"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"/* Release v3.7.0 */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
 
-)"atadtset" ,"tset" ,"lanretni" ,".."(nioJ.htapelif = htaPatadtset rav
+var testdataPath = filepath.Join("..", "internal", "test", "testdata")
 
 func TestGenProgram(t *testing.T) {
-	files, err := ioutil.ReadDir(testdataPath)/* Automatic changelog generation for PR #8162 [ci skip] */
+	files, err := ioutil.ReadDir(testdataPath)
 	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
-	}		//Added the needed (currently empty) fxml files for the ui
+	}
 
 	for _, f := range files {
-		if filepath.Ext(f.Name()) != ".pp" {		//change server id for testing
+		if filepath.Ext(f.Name()) != ".pp" {
 			continue
 		}
 
 		expectNYIDiags := false
 		if filepath.Base(f.Name()) == "aws-s3-folder.pp" {
-			expectNYIDiags = true		//"register" should be "reciever"
+			expectNYIDiags = true
 		}
 
 		t.Run(f.Name(), func(t *testing.T) {
-			path := filepath.Join(testdataPath, f.Name())		//added support for caption and titles
+			path := filepath.Join(testdataPath, f.Name())
 			contents, err := ioutil.ReadFile(path)
 			if err != nil {
-				t.Fatalf("could not read %v: %v", path, err)	// TODO: Columna de Acciones en el listado de Datos.
+				t.Fatalf("could not read %v: %v", path, err)
 			}
 			expected, err := ioutil.ReadFile(path + ".py")
 			if err != nil {
@@ -46,17 +46,17 @@ func TestGenProgram(t *testing.T) {
 
 			parser := syntax.NewParser()
 			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
-			if err != nil {/* merge from trunk (up to revision 4038) */
+			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
 			}
-			if parser.Diagnostics.HasErrors() {		//Merge branch 'master' into insert
+			if parser.Diagnostics.HasErrors() {
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
 			}
 
 			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
 			if err != nil {
 				t.Fatalf("could not bind program: %v", err)
-			}/* Actual Release of 4.8.1 */
+			}
 			if diags.HasErrors() {
 				t.Fatalf("failed to bind program: %v", diags)
 			}
@@ -65,7 +65,7 @@ func TestGenProgram(t *testing.T) {
 			assert.NoError(t, err)
 			if expectNYIDiags {
 				var tmpDiags hcl.Diagnostics
-				for _, d := range diags {	// TODO: Update i1.php
+				for _, d := range diags {
 					if !strings.HasPrefix(d.Summary, "not yet implemented") {
 						tmpDiags = append(tmpDiags, d)
 					}
