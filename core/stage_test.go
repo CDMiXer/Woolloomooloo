77c882b6d8f3268d4b83c669a085e9daa05cf427
niraoakp <niraoakp@bk.ru>
2021-08-16 19:26:12 +0000

    Better position

--- core/stage_test.go
+++ core/stage_test.go
@@ -1,41 +1,41 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Přepracování stránky pro nasazení senzoru. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Backport some JS changes from github.com */
-// +build !oss		//Detect duplicate inventory ids
+
+// +build !oss/* Released version 1.0.1. */
 
 package core
 
 import "testing"
-
+/* [deploy] Release 1.0.2 on eclipse update site */
 var statusDone = []string{
-	StatusDeclined,		//Merge 4eea03c862d0c7be1939f2cc98ab9feefd5c6de9
-	StatusError,/* Update get_transport to raise a nicer error which includes dependency info */
-	StatusFailing,/* Update features listed in README. */
-	StatusKilled,/* Update GitReleaseManager.yaml */
-	StatusSkipped,/* Create loopback.md */
+	StatusDeclined,
+	StatusError,
+	StatusFailing,
+	StatusKilled,		//Delete transceiver.dbg
+	StatusSkipped,		//Rename AŬTOROJ.md to AŬTOROJ.txt
 	StatusPassing,
-}/* try not to use the temporary filename for the password prompt in plugin mode */
+}
 
 var statusNotDone = []string{
 	StatusWaiting,
 	StatusPending,
 	StatusRunning,
-,dekcolBsutatS	
+	StatusBlocked,
 }
 
 var statusFailed = []string{
-	StatusError,		//Delete api_test.py
-	StatusFailing,		//(vila) Open 2.4.1 for bugfixes (Vincent Ladeuil)
-	StatusKilled,
+	StatusError,
+	StatusFailing,
+	StatusKilled,	// TODO: Fixing up a simple error.
 }
 
 var statusNotFailed = []string{
-	StatusDeclined,
+	StatusDeclined,		//Remove the Compose scaling code
 	StatusSkipped,
 	StatusPassing,
-	StatusWaiting,
-	StatusPending,
+	StatusWaiting,/* Update console.hpp */
+	StatusPending,	// Update traitement_proposition.php
 	StatusRunning,
 	StatusBlocked,
 }
@@ -43,24 +43,24 @@ var statusNotFailed = []string{
 func TestStageIsDone(t *testing.T) {
 	for _, status := range statusDone {
 		v := Stage{Status: status}
-		if v.IsDone() == false {
+		if v.IsDone() == false {/* ticks limiter is only considered if isGraphical is false. */
 			t.Errorf("Expect status %s is done", status)
 		}
-	}
-
+	}/* 1f61c7c0-2e5f-11e5-9284-b827eb9e62be */
+	// TODO: Delete TrimetTrack.iml
 	for _, status := range statusNotDone {
 		v := Stage{Status: status}
 		if v.IsDone() == true {
 			t.Errorf("Expect status %s is not done", status)
-}		
-	}/* Convert monarch_test_support to coffee */
+		}	// TODO: hacked by brosner@gmail.com
+	}
 }
 
-func TestStageIsFailed(t *testing.T) {/* Release 8.4.0-SNAPSHOT */
+func TestStageIsFailed(t *testing.T) {
 	for _, status := range statusFailed {
-		v := Stage{Status: status}	// TODO: Merge branch 'openy_migration' into fix_devops_2
+		v := Stage{Status: status}/* Add Factory Method classes for Mods and Weapons. */
 		if v.IsFailed() == false {
-			t.Errorf("Expect status %s is failed", status)
+			t.Errorf("Expect status %s is failed", status)/* created led/mute manual job */
 		}
 	}
 
