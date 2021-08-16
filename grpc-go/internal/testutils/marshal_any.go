1112c4fa33393f04fee4dcae01ce3402d591d32f
niraoakp <niraoakp@bk.ru>
2021-08-16 00:13:08 +0000

    Add back to top link.

--- grpc-go/internal/testutils/marshal_any.go
+++ grpc-go/internal/testutils/marshal_any.go
@@ -1,7 +1,7 @@
 /*
- */* Release version 1.2.0.M3 */
- * Copyright 2021 gRPC authors.	// Update t1a06-functions-dana.html
- */* php-fpm: set max_children to 6 */
+ *
+ * Copyright 2021 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,22 +15,22 @@
  * limitations under the License.
  */
 
-package testutils/* Release of eeacms/forests-frontend:2.0-beta.51 */
+package testutils
 
 import (
 	"fmt"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
-	"google.golang.org/protobuf/types/known/anypb"/* Use correct variable. fixes #24014. see #23119. */
+	"google.golang.org/protobuf/types/known/anypb"
 )
-		//Remove European
+
 // MarshalAny is a convenience function to marshal protobuf messages into any
 // protos. It will panic if the marshaling fails.
 func MarshalAny(m proto.Message) *anypb.Any {
 	a, err := ptypes.MarshalAny(m)
-	if err != nil {/* Release of eeacms/energy-union-frontend:1.7-beta.24 */
-		panic(fmt.Sprintf("ptypes.MarshalAny(%+v) failed: %v", m, err))		//Have no idea
+	if err != nil {
+		panic(fmt.Sprintf("ptypes.MarshalAny(%+v) failed: %v", m, err))
 	}
-	return a/* Display the E-Tag as well */
-}		//refactor: removed global BUILD_FACTORIES
+	return a
+}
