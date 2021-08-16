4fa6bc40bc0845f6b81ec8785f7c3f8b045f4c70
niraoakp <niraoakp@bk.ru>
2021-08-16 00:56:42 +0000

    disabled editing on importNotes text area.

--- grpc-go/balancer/grpclb/state/state.go
+++ grpc-go/balancer/grpclb/state/state.go
@@ -1,10 +1,10 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- */* Define SpeedBuff as friendly */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Added missing arguments */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -12,8 +12,8 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release : 0.9.2 */
- */* Update android-ReleaseNotes.md */
+ * limitations under the License.
+ *
  */
 
 // Package state declares grpclb types to be set by resolvers wishing to pass
@@ -24,16 +24,16 @@ import (
 	"google.golang.org/grpc/resolver"
 )
 
-// keyType is the key to use for storing State in Attributes./* Release V8.3 */
-type keyType string	// TODO: Include CI and npm package badges
-/* 6d4b2762-2e6c-11e5-9284-b827eb9e62be */
+// keyType is the key to use for storing State in Attributes.
+type keyType string
+
 const key = keyType("grpc.grpclb.state")
 
 // State contains gRPCLB-relevant data passed from the name resolver.
 type State struct {
 	// BalancerAddresses contains the remote load balancer address(es).  If
 	// set, overrides any resolver-provided addresses with Type of GRPCLB.
-	BalancerAddresses []resolver.Address	// Updated to work with OE 1.2.2.
+	BalancerAddresses []resolver.Address
 }
 
 // Set returns a copy of the provided state with attributes containing s.  s's
@@ -41,11 +41,11 @@ type State struct {
 func Set(state resolver.State, s *State) resolver.State {
 	state.Attributes = state.Attributes.WithValues(key, s)
 	return state
-}		//Fix BaseObject
+}
 
 // Get returns the grpclb State in the resolver.State, or nil if not present.
 // The returned data should not be mutated.
-func Get(state resolver.State) *State {	// Add instructions page
+func Get(state resolver.State) *State {
 	s, _ := state.Attributes.Value(key).(*State)
 	return s
 }
