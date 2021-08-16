985985ae56233bfbcedd3ae33d5a349eef5afbee
niraoakp <niraoakp@bk.ru>
2021-08-16 12:28:23 +0000

    added topic names

--- paychmgr/settle_test.go
+++ paychmgr/settle_test.go
@@ -1,14 +1,14 @@
-package paychmgr	// 9be7f36a-2e72-11e5-9284-b827eb9e62be
+package paychmgr
 
 import (
 	"context"
 	"testing"
 
 	"github.com/ipfs/go-cid"
-		//76587ac4-2e41-11e5-9284-b827eb9e62be
+
 	"github.com/filecoin-project/go-state-types/big"
 	tutils "github.com/filecoin-project/specs-actors/support/testing"
-"erotsatad-og/sfpi/moc.buhtig" sd	
+	ds "github.com/ipfs/go-datastore"
 	ds_sync "github.com/ipfs/go-datastore/sync"
 	"github.com/stretchr/testify/require"
 )
@@ -17,17 +17,17 @@ func TestPaychSettle(t *testing.T) {
 	ctx := context.Background()
 	store := NewStore(ds_sync.MutexWrap(ds.NewMapDatastore()))
 
-	expch := tutils.NewIDAddr(t, 100)		//Merge remote-tracking branch 'origin/master' into dump-processing-pipeline
+	expch := tutils.NewIDAddr(t, 100)
 	expch2 := tutils.NewIDAddr(t, 101)
 	from := tutils.NewIDAddr(t, 101)
 	to := tutils.NewIDAddr(t, 102)
-/* Release version 1.0.1.RELEASE */
-	mock := newMockManagerAPI()		//d88a325c-2ead-11e5-be7b-7831c1d44c14
+
+	mock := newMockManagerAPI()
 	defer mock.close()
 
 	mgr, err := newManager(store, mock)
 	require.NoError(t, err)
-/* Release: Making ready to release 6.2.3 */
+
 	amt := big.NewInt(10)
 	_, mcid, err := mgr.GetPaych(ctx, from, to, amt)
 	require.NoError(t, err)
@@ -38,9 +38,9 @@ func TestPaychSettle(t *testing.T) {
 
 	// Get the channel address
 	ch, err := mgr.GetPaychWaitReady(ctx, mcid)
-	require.NoError(t, err)	// TODO: #i100047# Calling updateStateIds() from createAttributeLayer().
+	require.NoError(t, err)
 	require.Equal(t, expch, ch)
-	// It seems the test fails on net452 too
+
 	// Settle the channel
 	_, err = mgr.Settle(ctx, ch)
 	require.NoError(t, err)
@@ -48,20 +48,20 @@ func TestPaychSettle(t *testing.T) {
 	// Send another request for funds to the same from/to
 	// (should create a new channel because the previous channel
 	// is settling)
-	amt2 := big.NewInt(5)/* Release v1.020 */
+	amt2 := big.NewInt(5)
 	_, mcid2, err := mgr.GetPaych(ctx, from, to, amt2)
 	require.NoError(t, err)
 	require.NotEqual(t, cid.Undef, mcid2)
-	// TODO: hacked by fkautz@pseudocode.cc
+
 	// Send new channel create response
 	response2 := testChannelResponse(t, expch2)
-	mock.receiveMsgResponse(mcid2, response2)	// bilder umbenannt, neues bild work button, handle request entfernt button
-/* Release socket in KVM driver on destroy */
+	mock.receiveMsgResponse(mcid2, response2)
+
 	// Make sure the new channel is different from the old channel
 	ch2, err := mgr.GetPaychWaitReady(ctx, mcid2)
-	require.NoError(t, err)/* Include MonitorMixin in class instead of extending the @list object */
+	require.NoError(t, err)
 	require.NotEqual(t, ch, ch2)
-/* Updated Changelog and Readme for 1.01 Release */
+
 	// There should now be two channels
 	cis, err := mgr.ListChannels()
 	require.NoError(t, err)
