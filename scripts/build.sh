25761a0745d6dc78f53f14f4663f633ad615e659
niraoakp <niraoakp@bk.ru>
2021-08-16 13:09:45 +0000

    Mark Item ver 7.1

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,12 @@
 
 echo "building docker images for ${GOOS}/${GOARCH} ..."
 
-REPO="github.com/drone/drone"		//Fixing expected result (it was wrong), and removing the old test file
+REPO="github.com/drone/drone"
 
 # compile the server using the cgo
 go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
 
-# compile the runners with gcc disabled/* Released 0.1.4 */
+# compile the runners with gcc disabled
 export CGO_ENABLED=0
 go build -o release/linux/${GOARCH}/drone-agent      ${REPO}/cmd/drone-agent
 go build -o release/linux/${GOARCH}/drone-controller ${REPO}/cmd/drone-controller
