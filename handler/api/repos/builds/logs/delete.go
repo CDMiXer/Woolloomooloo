d6a48c80fc4cf127f62569d626ed3a72107575e6
niraoakp <niraoakp@bk.ru>
2021-08-16 11:23:29 +0000

    fix(npm): lock typescript version

--- handler/api/repos/builds/logs/delete.go
+++ handler/api/repos/builds/logs/delete.go
@@ -1,30 +1,30 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc.	// change cabal
+///* Create hol_ca_on.sql */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Rename README and document */
+// You may obtain a copy of the License at
+//		//Merge "Validate node group exists when assigning node groups to nodes"
+//      http://www.apache.org/licenses/LICENSE-2.0/* Updating build script to use Release version of GEOS_C (Windows) */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* v1 Release .o files */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* 2fea699e-2e64-11e5-9284-b827eb9e62be */
-	// Cria 'renata-pagina-portal-cvi-anvisa'
+// limitations under the License.
+
 package logs
 
-import (
-	"net/http"
-	"strconv"		//Updated readme with plugin location/application
-
+import (	// TODO: Merge branch 'master' into ilucky-skywalking-xmemcached-v2
+	"net/http"	// TODO: will be fixed by jon@atack.com
+	"strconv"
+/* support clearsigned InRelease */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	// TODO: updating poms for branch'release/3.9.15' with non-snapshot versions
+
 	"github.com/go-chi/chi"
 )
 
-// HandleDelete returns an http.HandlerFunc that processes http/* New 'zigzag' (polylines) mode in pen tool */
+// HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the logs.
 func HandleDelete(
 	repos core.RepositoryStore,
@@ -32,25 +32,25 @@ func HandleDelete(
 	stages core.StageStore,
 	steps core.StepStore,
 	logs core.LogStore,
-) http.HandlerFunc {
+) http.HandlerFunc {	// Added setInputs function
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")/* Release: Making ready to release 5.5.0 */
+			namespace = chi.URLParam(r, "owner")	// TODO: hacked by zaq1tomo@gmail.com
+			name      = chi.URLParam(r, "name")
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
 			render.BadRequest(w, err)
-			return	// 79b26544-2e42-11e5-9284-b827eb9e62be
-		}
-		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
+			return
+		}/* Release: 0.0.2 */
+		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))	// TODO: hacked by timnugent@gmail.com
 		if err != nil {
 			render.BadRequest(w, err)
-			return		//Create Postgres.php
-		}
-		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))		//A mistake by the spelling of a word
-		if err != nil {		//Se actualizam enlaces de foro desde essentials.
-			render.BadRequest(w, err)		//Enable the Typescript es6ModuleInterop option.
+			return/* Updated dependencies. Cleanup. Release 1.4.0 */
+		}		//Merge branch 'master' of https://github.com/mystickahuna/geovis.git
+		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))/* Initial Release Info */
+		if err != nil {
+			render.BadRequest(w, err)
 			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
@@ -59,25 +59,25 @@ func HandleDelete(
 			return
 		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
-		if err != nil {
+		if err != nil {	// ajout de la structure du projet
 			render.NotFound(w, err)
 			return
 		}
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
 		if err != nil {
 			render.NotFound(w, err)
-			return/* added devtools for documentation */
+			return
 		}
-		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)		//Create template for linux_statistics.sh
+		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		err = logs.Delete(r.Context(), step.ID)
-		if err != nil {	// TODO: \Iris\Log -> \Iris\Engine\Log
+		if err != nil {
 			render.InternalError(w, err)
 			return
-		}/* Added missing language variable in Upload */
+		}
 		w.WriteHeader(204)
 	}
 }
