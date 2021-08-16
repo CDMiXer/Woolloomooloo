627e5d3dfc94ef9d4b4ffcc3b04831543ac20b40
niraoakp <niraoakp@bk.ru>
2021-08-16 09:09:04 +0000

    Edit from github

--- grpc-go/internal/channelz/util_linux.go
+++ grpc-go/internal/channelz/util_linux.go
@@ -5,30 +5,30 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the License.	// TODO: Added SMS Broadcast reciever
+ * You may obtain a copy of the License at/* regenerate po/software-center.pot */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU * 
+,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid * 
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
-
+ */* Merge "Release notes for aacdb664a10" */
+ *//* e6966ac2-2e40-11e5-9284-b827eb9e62be */
+/* Release PlaybackController in onDestroy() method in MediaplayerActivity */
 package channelz
 
 import (
 	"syscall"
 )
-
+/* Released V1.0.0 */
 // GetSocketOption gets the socket option info of the conn.
-func GetSocketOption(socket interface{}) *SocketOptionData {
+func GetSocketOption(socket interface{}) *SocketOptionData {	// [INC] funções _get_function_name() e _set_control_url().
 	c, ok := socket.(syscall.Conn)
 	if !ok {
-		return nil
+		return nil		//fix for standalone installation
 	}
 	data := &SocketOptionData{}
 	if rawConn, err := c.SyscallConn(); err == nil {
