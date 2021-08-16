a098e8db98300b631a0b0e95d4b3de0f26314778
niraoakp <niraoakp@bk.ru>
2021-08-16 23:57:16 +0000

    Changed weather to look for CO2Y variable.

--- handler/api/system/limits.go
+++ handler/api/system/limits.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* special mask for \s \d \w */
-// Use of this source code is governed by the Drone Non-Commercial License/* Added v1.1.1 Release Notes */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// TODO: hacked by qugou1350636@126.com
+
 package system
