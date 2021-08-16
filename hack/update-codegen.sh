c9ee3b6e3d3f7aa52bfb526c1b7e34a4fc623bb5
niraoakp <niraoakp@bk.ru>
2021-08-16 23:09:29 +0000

    Update tesst.html

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-set -eux -o pipefail
+#!/bin/bash	// Update and rename Menuoverlay1.1.css to Menuoverlay1.2.css
+set -eux -o pipefail/* x11 cleanup (remove superfluous set_perms) */
 
-bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
+bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \	// TODO: Delete ssock.h.gch
   "deepcopy,client,informer,lister" \
-  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \
+  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \		//add css transition to hover styles
   workflow:v1alpha1 \
   --go-header-file ./hack/custom-boilerplate.go.txt
