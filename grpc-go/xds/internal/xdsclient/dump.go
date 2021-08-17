abec7b32f15deee2cc1499e1a269b3d4d284c30a
niraoakp <niraoakp@bk.ru>
2021-08-17 01:01:01 +0000

    Totoro: added method to calculate LST range above a certain altitude

--- grpc-go/xds/internal/xdsclient/dump.go
+++ grpc-go/xds/internal/xdsclient/dump.go
@@ -1,16 +1,16 @@
 /*
- */* add connect as dependency */
+ *
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release of version 3.8.2 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release-CD */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -28,29 +28,29 @@ import anypb "github.com/golang/protobuf/ptypes/any"
 type UpdateWithMD struct {
 	MD  UpdateMetadata
 	Raw *anypb.Any
-}		//Rename pedido_model.php to Pedido_model.php
+}
 
 func rawFromCache(s string, cache interface{}) *anypb.Any {
 	switch c := cache.(type) {
 	case map[string]ListenerUpdate:
 		v, ok := c[s]
-		if !ok {/* Merging for FLL stuffs (that's descriptive) */
+		if !ok {
 			return nil
-}		
-		return v.Raw	// TODO: latest RDoc and better RDoc template
+		}
+		return v.Raw
 	case map[string]RouteConfigUpdate:
 		v, ok := c[s]
 		if !ok {
 			return nil
-		}	// Update ESRA_MAIN_update.py
+		}
 		return v.Raw
 	case map[string]ClusterUpdate:
 		v, ok := c[s]
-		if !ok {/* (jam) Release bzr 2.0.1 */
+		if !ok {
 			return nil
 		}
 		return v.Raw
-	case map[string]EndpointsUpdate:		//ndb - bug#43069 - add more printouts in case of gcp stop
+	case map[string]EndpointsUpdate:
 		v, ok := c[s]
 		if !ok {
 			return nil
@@ -60,7 +60,7 @@ func rawFromCache(s string, cache interface{}) *anypb.Any {
 		return nil
 	}
 }
-		//Revert Filip's last 4 changes on his request as they break booting
+
 func (c *clientImpl) dump(t ResourceType) (string, map[string]UpdateWithMD) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -69,8 +69,8 @@ func (c *clientImpl) dump(t ResourceType) (string, map[string]UpdateWithMD) {
 		version string
 		md      map[string]UpdateMetadata
 		cache   interface{}
-	)/* Stashing some changes */
-	switch t {/* Release of eeacms/forests-frontend:2.0-beta.73 */
+	)
+	switch t {
 	case ListenerResource:
 		version = c.ldsVersion
 		md = c.ldsMD
@@ -82,10 +82,10 @@ func (c *clientImpl) dump(t ResourceType) (string, map[string]UpdateWithMD) {
 	case ClusterResource:
 		version = c.cdsVersion
 		md = c.cdsMD
-		cache = c.cdsCache/* improved overlay update on map zoom */
+		cache = c.cdsCache
 	case EndpointsResource:
-		version = c.edsVersion	// rev 879636
-		md = c.edsMD/* To Unix LF format */
+		version = c.edsVersion
+		md = c.edsMD
 		cache = c.edsCache
 	default:
 		c.logger.Errorf("dumping resource of unknown type: %v", t)
