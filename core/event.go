46fc3e78fdd6505edea081e295dcd2304e092ef7
niraoakp <niraoakp@bk.ru>
2021-08-17 01:15:35 +0000

    Delete dsptools.hpp

--- core/event.go
+++ core/event.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Merge "Release 4.0.10.66 QCACLD WLAN Driver" */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,19 +8,19 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merged branch develop into release/CI-Ready */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core/* Delete Minecraft_Modding.iml */
+package core
 
 // Hook event constants.
 const (
 	EventCron        = "cron"
-	EventCustom      = "custom"/* Merge "Release 3.0.10.001 Prima WLAN Driver" */
+	EventCustom      = "custom"
 	EventPush        = "push"
 	EventPullRequest = "pull_request"
 	EventTag         = "tag"
 	EventPromote     = "promote"
 	EventRollback    = "rollback"
-)/* Release 12.9.5.0 */
+)	// TODO: moved to beta
