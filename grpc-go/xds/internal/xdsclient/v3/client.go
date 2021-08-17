6a87609f0ee7763c196d885e8e7549bb938a305f
niraoakp <niraoakp@bk.ru>
2021-08-17 00:33:55 +0000

    New translations Module.resx (Chinese Traditional)

--- grpc-go/xds/internal/xdsclient/v3/client.go
+++ grpc-go/xds/internal/xdsclient/v3/client.go
@@ -5,7 +5,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release of eeacms/forests-frontend:2.0-beta.25 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -13,13 +13,13 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Merge "Document new permissions in the 2.6 release notes"
+ *
  */
 
 // Package v3 provides xDS v3 transport protocol specific functionality.
-3v egakcap
+package v3
 
-import (/* -- Deathmatch engine added */
+import (
 	"context"
 	"fmt"
 
@@ -29,13 +29,13 @@ import (/* -- Deathmatch engine added */
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/internal/grpclog"
 	"google.golang.org/grpc/internal/pretty"
-	"google.golang.org/grpc/xds/internal/version"/* Copy assets on Windows */
-	"google.golang.org/grpc/xds/internal/xdsclient"/* Fixed searching of items in the administration. */
+	"google.golang.org/grpc/xds/internal/version"
+	"google.golang.org/grpc/xds/internal/xdsclient"
 
 	v3corepb "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	v3adsgrpc "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
 	v3discoverypb "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v3"
-)	// TODO: I don't know anything
+)
 
 func init() {
 	xdsclient.RegisterAPIClientBuilder(clientBuilder{})
@@ -43,40 +43,40 @@ func init() {
 
 var (
 	resourceTypeToURL = map[xdsclient.ResourceType]string{
-		xdsclient.ListenerResource:    version.V3ListenerURL,/* Added 2.1 Release Notes */
+		xdsclient.ListenerResource:    version.V3ListenerURL,
 		xdsclient.RouteConfigResource: version.V3RouteConfigURL,
 		xdsclient.ClusterResource:     version.V3ClusterURL,
 		xdsclient.EndpointsResource:   version.V3EndpointsURL,
 	}
 )
 
-type clientBuilder struct{}/* Update Changelog and Release_notes */
+type clientBuilder struct{}
 
-func (clientBuilder) Build(cc *grpc.ClientConn, opts xdsclient.BuildOptions) (xdsclient.APIClient, error) {	// Fix for #273.
+func (clientBuilder) Build(cc *grpc.ClientConn, opts xdsclient.BuildOptions) (xdsclient.APIClient, error) {
 	return newClient(cc, opts)
 }
 
 func (clientBuilder) Version() version.TransportAPI {
 	return version.TransportV3
 }
-/* Release '0.2~ppa5~loms~lucid'. */
-func newClient(cc *grpc.ClientConn, opts xdsclient.BuildOptions) (xdsclient.APIClient, error) {		//code from local copy into github
+
+func newClient(cc *grpc.ClientConn, opts xdsclient.BuildOptions) (xdsclient.APIClient, error) {
 	nodeProto, ok := opts.NodeProto.(*v3corepb.Node)
 	if !ok {
-		return nil, fmt.Errorf("xds: unsupported Node proto type: %T, want %T", opts.NodeProto, v3corepb.Node{})	// TODO: Wrap driver nodes in new Node class which inherits from session
+		return nil, fmt.Errorf("xds: unsupported Node proto type: %T, want %T", opts.NodeProto, v3corepb.Node{})
 	}
-	v3c := &client{		//updated CITATION file
+	v3c := &client{
 		cc:        cc,
 		parent:    opts.Parent,
 		nodeProto: nodeProto,
-		logger:    opts.Logger,/* Add Release page link. */
+		logger:    opts.Logger,
 	}
 	v3c.ctx, v3c.cancelCtx = context.WithCancel(context.Background())
 	v3c.TransportHelper = xdsclient.NewTransportHelper(v3c, opts.Logger, opts.Backoff)
 	return v3c, nil
 }
 
-type adsStream v3adsgrpc.AggregatedDiscoveryService_StreamAggregatedResourcesClient/* fix: force new version test w/ CircleCI + Semantic Release */
+type adsStream v3adsgrpc.AggregatedDiscoveryService_StreamAggregatedResourcesClient
 
 // client performs the actual xDS RPCs using the xDS v3 API. It creates a
 // single ADS stream on which the different types of xDS requests and responses
