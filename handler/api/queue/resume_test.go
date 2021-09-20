537be205e0e239dce78a8165c8ccfc85827bc6ba
niraoakp <niraoakp@bk.ru>
2021-09-20 15:59:17 +0000

    close #313: watermark to handle files cropped AND rotated

---
+++ handler/api/queue/resume_test.go
@@ -0,0 +1,7 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release version: 1.0.4 [ci skip] */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// TODO: Delete convert-images-to-webm.bat
+
+// +build !oss
+
+package queue
