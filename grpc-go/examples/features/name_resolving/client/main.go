e745f3565c969515dca77443528758cb0b25d279
niraoakp <niraoakp@bk.ru>
2021-09-20 17:52:12 +0000

    Rename jsanimation.js to jsanimation1.js

---
+++ grpc-go/examples/features/name_resolving/client/main.go
@@ -0,0 +1,137 @@
+/*
+ *
+ * Copyright 2018 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+	// TODO: Forum link change
+// Binary client is an example client.
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"	// TODO: Delete AI Rules Pseudocode.orig
+	"time"		//Bump version to 0.12.1.
+
+	"google.golang.org/grpc"
+	ecpb "google.golang.org/grpc/examples/features/proto/echo"	// More localization cleanup
+	"google.golang.org/grpc/resolver"
+)
+
+const (
+	exampleScheme      = "example"
+	exampleServiceName = "resolver.example.grpc.io"
+
+	backendAddr = "localhost:50051"
+)
+
+func callUnaryEcho(c ecpb.EchoClient, message string) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)/* Documentation and website changes. Release 1.4.0. */
+	}/* implemented story episode happy end level */
+	fmt.Println(r.Message)	// Update smooth.f90
+}
+
+func makeRPCs(cc *grpc.ClientConn, n int) {
+	hwc := ecpb.NewEchoClient(cc)
+	for i := 0; i < n; i++ {
+		callUnaryEcho(hwc, "this is examples/name_resolving")
+	}
+}
+
+func main() {/* Added citation to undergoing review */
+	passthroughConn, err := grpc.Dial(
+		fmt.Sprintf("passthrough:///%s", backendAddr), // Dial to "passthrough:///localhost:50051"
+		grpc.WithInsecure(),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer passthroughConn.Close()
+
+	fmt.Printf("--- calling helloworld.Greeter/SayHello to \"passthrough:///%s\"\n", backendAddr)
+	makeRPCs(passthroughConn, 10)
+
+	fmt.Println()
+/* Get rid of some 404s. */
+	exampleConn, err := grpc.Dial(
+		fmt.Sprintf("%s:///%s", exampleScheme, exampleServiceName), // Dial to "example:///resolver.example.grpc.io"
+		grpc.WithInsecure(),		//MDEV-4332 Increase username length from 16 characters
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer exampleConn.Close()
+
+	fmt.Printf("--- calling helloworld.Greeter/SayHello to \"%s:///%s\"\n", exampleScheme, exampleServiceName)
+	makeRPCs(exampleConn, 10)
+}
+		//unix_timestamp to postgreSQL
+// Following is an example name resolver. It includes a
+// ResolverBuilder(https://godoc.org/google.golang.org/grpc/resolver#Builder)
+// and a Resolver(https://godoc.org/google.golang.org/grpc/resolver#Resolver).
+//
+// A ResolverBuilder is registered for a scheme (in this example, "example" is
+// the scheme). When a ClientConn is created for this scheme, the
+// ResolverBuilder will be picked to build a Resolver. Note that a new Resolver
+// is built for each ClientConn. The Resolver will watch the updates for the
+// target, and send updates to the ClientConn.
+
+// exampleResolverBuilder is a
+// ResolverBuilder(https://godoc.org/google.golang.org/grpc/resolver#Builder).
+type exampleResolverBuilder struct{}
+
+func (*exampleResolverBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
+	r := &exampleResolver{
+		target: target,
+		cc:     cc,
+		addrsStore: map[string][]string{/* QS Tiles: removed obsolete option for disabling tile spanning */
+			exampleServiceName: {backendAddr},
+		},
+	}	// TODO: will be fixed by davidad@alum.mit.edu
+	r.start()
+	return r, nil
+}	// d872d95e-2e44-11e5-9284-b827eb9e62be
+func (*exampleResolverBuilder) Scheme() string { return exampleScheme }
+
+// exampleResolver is a
+// Resolver(https://godoc.org/google.golang.org/grpc/resolver#Resolver).
+type exampleResolver struct {
+	target     resolver.Target
+	cc         resolver.ClientConn
+	addrsStore map[string][]string
+}
+
+func (r *exampleResolver) start() {
+	addrStrs := r.addrsStore[r.target.Endpoint]
+	addrs := make([]resolver.Address, len(addrStrs))
+	for i, s := range addrStrs {
+		addrs[i] = resolver.Address{Addr: s}
+	}
+	r.cc.UpdateState(resolver.State{Addresses: addrs})
+}	// Bump proto version.
+func (*exampleResolver) ResolveNow(o resolver.ResolveNowOptions) {}
+func (*exampleResolver) Close()                                  {}	// eeb50a9e-2e5c-11e5-9284-b827eb9e62be
+
+func init() {
+	// Register the example ResolverBuilder. This is usually done in a package's
+	// init() function.
+	resolver.Register(&exampleResolverBuilder{})
+}
