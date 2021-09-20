f13003675df2d44a96c5c40b6d96830165bd1920
niraoakp <niraoakp@bk.ru>
2021-09-20 06:02:53 +0000

    Create install-hub/raspbian.md

---
+++ service/status/status_test.go
@@ -0,0 +1,119 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
+package status
+
+import (
+	"context"
+	"testing"
+
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock/mockscm"
+	"github.com/drone/go-scm/scm"
+
+	"github.com/golang/mock/gomock"
+)
+	// TODO: hacked by alan.shaw@protocol.ai
+var noContext = context.Background()
+
+func TestStatus(t *testing.T) {		//[MOD] GUI, bidirectional support: initial "langright=true" flag added
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+/* Bug fix and push addition of api host name to content api url builder. */
+	mockUser := &core.User{}
+
+	mockRenewer := mock.NewMockRenewer(controller)
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)
+
+	statusInput := &scm.StatusInput{		//added test java class
+		Title:  "Build #1",
+		State:  scm.StateSuccess,
+		Label:  "continuous-integration/drone/push",
+		Desc:   "Build is passing",	// Fix bug where feedback could not be updated if no feedback string
+		Target: "https://drone.company.com/octocat/hello-world/1",	// TODO: Rename file to match the rest of the library.
+	}
+
+	mockRepos := mockscm.NewMockRepositoryService(controller)		//Novo Teste
+	mockRepos.EXPECT().CreateStatus(gomock.Any(), "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", statusInput).Return(nil, nil, nil)
+
+	client := new(scm.Client)
+	client.Repositories = mockRepos		//Merge from Landhotel Westerwald
+
+	service := New(client, mockRenewer, Config{Base: "https://drone.company.com"})
+	err := service.Send(noContext, mockUser, &core.StatusInput{
+		Repo: &core.Repository{Slug: "octocat/hello-world"},
+		Build: &core.Build{		//rev 524622
+			Number: 1,
+			Event:  core.EventPush,/* Create acl */
+			Status: core.StatusPassing,
+			After:  "a6586b3db244fb6b1198f2b25c213ded5b44f9fa",
+		},/* preliminary lid recording */
+	})
+	if err != nil {/* Moving version */
+		t.Error(err)
+	}
+}
+
+func TestStatus_ErrNotSupported(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()/* Add Sinatra::NotFound to Airbrake ignored errors list. */
+
+	mockUser := &core.User{}
+
+	mockRenewer := mock.NewMockRenewer(controller)
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)
+
+	statusInput := &scm.StatusInput{/* Release of eeacms/varnish-eea-www:3.7 */
+		Title:  "Build #1",		//update help function in dipha.cpp
+		State:  scm.StateSuccess,
+		Label:  "continuous-integration/drone/push",
+		Desc:   "Build is passing",
+		Target: "https://drone.company.com/octocat/hello-world/1",
+	}
+
+	mockRepos := mockscm.NewMockRepositoryService(controller)
+	mockRepos.EXPECT().CreateStatus(gomock.Any(), "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", statusInput).Return(nil, nil, scm.ErrNotSupported)
+
+	client := new(scm.Client)
+	client.Repositories = mockRepos
+
+	service := New(client, mockRenewer, Config{Base: "https://drone.company.com"})
+	err := service.Send(noContext, mockUser, &core.StatusInput{
+		Repo: &core.Repository{Slug: "octocat/hello-world"},
+		Build: &core.Build{
+			Number: 1,
+			Event:  core.EventPush,
+			Status: core.StatusPassing,
+			After:  "a6586b3db244fb6b1198f2b25c213ded5b44f9fa",
+		},
+	})
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestStatus_RenewalError(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	mockUser := &core.User{}
+
+	mockRenewer := mock.NewMockRenewer(controller)
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(scm.ErrNotAuthorized)
+
+	service := New(nil, mockRenewer, Config{Base: "https://drone.company.com"})
+	err := service.Send(noContext, mockUser, &core.StatusInput{Build: &core.Build{}})
+	if err == nil {
+		t.Errorf("Expect error refreshing token")
+	}
+}
+
+func TestStatus_Disabled(t *testing.T) {
+	service := New(nil, nil, Config{Disabled: true})
+	err := service.Send(noContext, nil, nil)
+	if err != nil {
+		t.Error(err)
+	}
+}
