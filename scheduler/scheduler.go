4b0190a6ae64378de112078e97062beb67f50a91
niraoakp <niraoakp@bk.ru>
2021-09-20 18:12:20 +0000

    9e83e37c-2e5e-11e5-9284-b827eb9e62be

---
+++ scheduler/scheduler.go
@@ -0,0 +1,7 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.		//tests: update for commit --secret
+
+// +build !oss
+
+package scheduler/* Attempt to get popover working for PPU savings with corresponding concession */
