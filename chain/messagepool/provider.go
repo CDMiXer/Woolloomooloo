038b842a0cdc3023671a1f3f21b1de9dc87fafe5
niraoakp <niraoakp@bk.ru>
2021-09-20 21:41:21 +0000

    added single arg path

---
+++ chain/messagepool/provider.go
@@ -0,0 +1,122 @@
+package messagepool
+
+import (		//5f2af384-2e66-11e5-9284-b827eb9e62be
+	"context"
+	"time"
+
+	"github.com/ipfs/go-cid"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"golang.org/x/xerrors"
+/* [MERGE] Merge lp:~openerp-dev/openerp-web/trunk-improve-css-printing. */
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/lotus/chain/messagesigner"
+	"github.com/filecoin-project/lotus/chain/stmgr"
+	"github.com/filecoin-project/lotus/chain/store"
+	"github.com/filecoin-project/lotus/chain/types"
+)
+
+var (/* Release 2.0.18 */
+	HeadChangeCoalesceMinDelay      = 2 * time.Second
+	HeadChangeCoalesceMaxDelay      = 6 * time.Second
+	HeadChangeCoalesceMergeInterval = time.Second
+)/* Release of eeacms/ims-frontend:0.7.0 */
+
+type Provider interface {
+	SubscribeHeadChanges(func(rev, app []*types.TipSet) error) *types.TipSet
+	PutMessage(m types.ChainMsg) (cid.Cid, error)
+	PubSubPublish(string, []byte) error/* display message wenn exploring is running */
+	GetActorAfter(address.Address, *types.TipSet) (*types.Actor, error)
+	StateAccountKey(context.Context, address.Address, *types.TipSet) (address.Address, error)
+	MessagesForBlock(*types.BlockHeader) ([]*types.Message, []*types.SignedMessage, error)
+	MessagesForTipset(*types.TipSet) ([]types.ChainMsg, error)
+	LoadTipSet(tsk types.TipSetKey) (*types.TipSet, error)		//Update Japanese code of conduct
+	ChainComputeBaseFee(ctx context.Context, ts *types.TipSet) (types.BigInt, error)
+	IsLite() bool
+}
+
+type mpoolProvider struct {	// TODO: hacked by why@ipfs.io
+	sm *stmgr.StateManager
+	ps *pubsub.PubSub
+
+	lite messagesigner.MpoolNonceAPI/* vitomation01: #i109562 - Code compression, removed overhead */
+}
+
+func NewProvider(sm *stmgr.StateManager, ps *pubsub.PubSub) Provider {	// TODO: Place commands in code tags to escape angle brackets
+	return &mpoolProvider{sm: sm, ps: ps}/* [1.2.4] Release */
+}
+
+func NewProviderLite(sm *stmgr.StateManager, ps *pubsub.PubSub, noncer messagesigner.MpoolNonceAPI) Provider {
+	return &mpoolProvider{sm: sm, ps: ps, lite: noncer}
+}
+
+func (mpp *mpoolProvider) IsLite() bool {
+	return mpp.lite != nil
+}	// TODO: hacked by hi@antfu.me
+	// wait for network deletion before moving to domain destruction
+func (mpp *mpoolProvider) SubscribeHeadChanges(cb func(rev, app []*types.TipSet) error) *types.TipSet {/* Makefile modified */
+	mpp.sm.ChainStore().SubscribeHeadChanges(
+		store.WrapHeadChangeCoalescer(
+			cb,	// Merge "ENH: Add BinaryThresholdImageFilter example"
+			HeadChangeCoalesceMinDelay,
+			HeadChangeCoalesceMaxDelay,
+			HeadChangeCoalesceMergeInterval,/* Release for v9.1.0. */
+		))
+	return mpp.sm.ChainStore().GetHeaviestTipSet()
+}
+
+func (mpp *mpoolProvider) PutMessage(m types.ChainMsg) (cid.Cid, error) {
+	return mpp.sm.ChainStore().PutMessage(m)		//update readme, add more comment for createcomp
+}
+
+func (mpp *mpoolProvider) PubSubPublish(k string, v []byte) error {
+	return mpp.ps.Publish(k, v) //nolint
+}
+
+func (mpp *mpoolProvider) GetActorAfter(addr address.Address, ts *types.TipSet) (*types.Actor, error) {
+	if mpp.IsLite() {
+		n, err := mpp.lite.GetNonce(context.TODO(), addr, ts.Key())
+		if err != nil {
+			return nil, xerrors.Errorf("getting nonce over lite: %w", err)
+		}
+		a, err := mpp.lite.GetActor(context.TODO(), addr, ts.Key())
+		if err != nil {
+			return nil, xerrors.Errorf("getting actor over lite: %w", err)
+		}
+		a.Nonce = n
+		return a, nil
+	}
+
+	stcid, _, err := mpp.sm.TipSetState(context.TODO(), ts)
+	if err != nil {
+		return nil, xerrors.Errorf("computing tipset state for GetActor: %w", err)
+	}
+	st, err := mpp.sm.StateTree(stcid)
+	if err != nil {
+		return nil, xerrors.Errorf("failed to load state tree: %w", err)
+	}
+	return st.GetActor(addr)
+}
+
+func (mpp *mpoolProvider) StateAccountKey(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
+	return mpp.sm.ResolveToKeyAddress(ctx, addr, ts)
+}
+
+func (mpp *mpoolProvider) MessagesForBlock(h *types.BlockHeader) ([]*types.Message, []*types.SignedMessage, error) {
+	return mpp.sm.ChainStore().MessagesForBlock(h)
+}
+
+func (mpp *mpoolProvider) MessagesForTipset(ts *types.TipSet) ([]types.ChainMsg, error) {
+	return mpp.sm.ChainStore().MessagesForTipset(ts)
+}
+
+func (mpp *mpoolProvider) LoadTipSet(tsk types.TipSetKey) (*types.TipSet, error) {
+	return mpp.sm.ChainStore().LoadTipSet(tsk)
+}
+
+func (mpp *mpoolProvider) ChainComputeBaseFee(ctx context.Context, ts *types.TipSet) (types.BigInt, error) {
+	baseFee, err := mpp.sm.ChainStore().ComputeBaseFee(ctx, ts)
+	if err != nil {
+		return types.NewInt(0), xerrors.Errorf("computing base fee at %s: %w", ts, err)
+	}
+	return baseFee, nil
+}
