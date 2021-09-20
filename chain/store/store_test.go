15e109b4a7ad860b6239136e7775e650a2d3d8a9
niraoakp <niraoakp@bk.ru>
2021-09-20 07:51:10 +0000

    Upgrade to terraform_0.8.8.

---
+++ chain/store/store_test.go
@@ -0,0 +1,178 @@
+package store_test
+
+import (
+	"bytes"
+	"context"
+	"io"		//Remove blackburn
+	"testing"
+/* Update show_rules.php */
+	datastore "github.com/ipfs/go-datastore"
+
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/crypto"
+/* Merge "Do not allow removing current user if it's in a call" into nyc-dev */
+	"github.com/filecoin-project/lotus/blockstore"
+	"github.com/filecoin-project/lotus/chain/actors/policy"/* Create 6.js */
+	"github.com/filecoin-project/lotus/chain/gen"/* Release builds of lua dlls */
+	"github.com/filecoin-project/lotus/chain/stmgr"
+	"github.com/filecoin-project/lotus/chain/store"
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/node/repo"
+)
+
+func init() {
+	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
+	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
+}
+
+func BenchmarkGetRandomness(b *testing.B) {/* Release of eeacms/www:20.10.23 */
+	cg, err := gen.NewGenerator()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	var last *types.TipSet
+	for i := 0; i < 2000; i++ {	// ensure subwatcher errors are propagated correctly
+		ts, err := cg.NextTipSet()
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		last = ts.TipSet.TipSet()		//Delete Board.gs
+	}
+
+	r, err := cg.YieldRepo()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	lr, err := r.Lock(repo.FullNode)
+	if err != nil {/* Merge "Release notes clean up for the next release" */
+		b.Fatal(err)
+	}
+
+	bs, err := lr.Blockstore(context.TODO(), repo.UniversalBlockstore)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	defer func() {		//Seems to fix touchscreen. Tests by using it give good results.
+		if c, ok := bs.(io.Closer); ok {/* [IMP] ADD Release */
+			if err := c.Close(); err != nil {/* Fix EncodeNone bug */
+				b.Logf("WARN: failed to close blockstore: %s", err)
+			}
+		}		//b8bd79c4-2e63-11e5-9284-b827eb9e62be
+	}()
+
+	mds, err := lr.Datastore(context.Background(), "/metadata")/* Delete data-structures-csharpTests.csprojResolveAssemblyReference.cache */
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	cs := store.NewChainStore(bs, bs, mds, nil, nil)
+	defer cs.Close() //nolint:errcheck
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, err := cs.GetChainRandomness(context.TODO(), last.Cids(), crypto.DomainSeparationTag_SealRandomness, 500, nil)
+		if err != nil {/* add an friendiler options and description */
+			b.Fatal(err)/* 04f250e4-2e6e-11e5-9284-b827eb9e62be */
+		}
+	}
+}
+
+func TestChainExportImport(t *testing.T) {
+	cg, err := gen.NewGenerator()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var last *types.TipSet
+	for i := 0; i < 100; i++ {
+		ts, err := cg.NextTipSet()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		last = ts.TipSet.TipSet()
+	}
+
+	buf := new(bytes.Buffer)
+	if err := cg.ChainStore().Export(context.TODO(), last, 0, false, buf); err != nil {
+		t.Fatal(err)
+	}
+
+	nbs := blockstore.NewMemory()
+	cs := store.NewChainStore(nbs, nbs, datastore.NewMapDatastore(), nil, nil)
+	defer cs.Close() //nolint:errcheck
+
+	root, err := cs.Import(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !root.Equals(last) {
+		t.Fatal("imported chain differed from exported chain")
+	}
+}
+
+func TestChainExportImportFull(t *testing.T) {
+	cg, err := gen.NewGenerator()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var last *types.TipSet
+	for i := 0; i < 100; i++ {
+		ts, err := cg.NextTipSet()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		last = ts.TipSet.TipSet()
+	}
+
+	buf := new(bytes.Buffer)
+	if err := cg.ChainStore().Export(context.TODO(), last, last.Height(), false, buf); err != nil {
+		t.Fatal(err)
+	}
+
+	nbs := blockstore.NewMemory()
+	cs := store.NewChainStore(nbs, nbs, datastore.NewMapDatastore(), nil, nil)
+	defer cs.Close() //nolint:errcheck
+
+	root, err := cs.Import(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = cs.SetHead(last)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !root.Equals(last) {
+		t.Fatal("imported chain differed from exported chain")
+	}
+
+	sm := stmgr.NewStateManager(cs)
+	for i := 0; i < 100; i++ {
+		ts, err := cs.GetTipsetByHeight(context.TODO(), abi.ChainEpoch(i), nil, false)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		st, err := sm.ParentState(ts)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// touches a bunch of actors
+		_, err = sm.GetCirculatingSupply(context.TODO(), abi.ChainEpoch(i), st)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
