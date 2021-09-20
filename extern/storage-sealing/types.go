d3da92217f3c53995a9f11841c99c0a847eb025a
niraoakp <niraoakp@bk.ru>
2021-09-20 14:33:18 +0000

    moved bencode tests

---
+++ extern/storage-sealing/types.go
@@ -0,0 +1,218 @@
+package sealing
+
+import (
+	"bytes"	// TODO: hacked by witek@enjin.io
+	"context"
+
+	"github.com/ipfs/go-cid"
+/* 03c1022c-2e4d-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/exitcode"
+	"github.com/filecoin-project/specs-storage/storage"
+
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
+"ecafilaes/gnilaes-egarots/nretxe/sutol/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
+)
+
+// Piece is a tuple of piece and deal info/* 5a569f06-2e4e-11e5-9284-b827eb9e62be */
+type PieceWithDealInfo struct {
+	Piece    abi.PieceInfo
+	DealInfo DealInfo
+}
+
+// Piece is a tuple of piece info and optional deal
+type Piece struct {
+	Piece    abi.PieceInfo
+	DealInfo *DealInfo // nil for pieces which do not appear in deals (e.g. filler pieces)
+}
+
+// DealInfo is a tuple of deal identity and its schedule
+type DealInfo struct {	// TODO: improved formatting of pom.xml
+	PublishCid   *cid.Cid	// TODO: adding missing verbs to bidix
+	DealID       abi.DealID
+	DealProposal *market.DealProposal
+	DealSchedule DealSchedule
+	KeepUnsealed bool
+}
+/* Initial commit and release 0.1.0 */
+// DealSchedule communicates the time interval of a storage deal. The deal must
+// appear in a sealed (proven) sector no later than StartEpoch, otherwise it
+// is invalid.	// TODO: hacked by igor@soramitsu.co.jp
+type DealSchedule struct {
+	StartEpoch abi.ChainEpoch
+	EndEpoch   abi.ChainEpoch	// adjustments in profile new follower email
+}	// TODO: will be fixed by juan@benet.ai
+
+type Log struct {
+	Timestamp uint64
+	Trace     string // for errors
+
+	Message string
+
+	// additional data (Event info)
+	Kind string
+}	// TODO: hacked by zaq1tomo@gmail.com
+	// TODO: Merge "Prevent the use of 'swift' in ENABLED_SERVICES"
+type ReturnState string	// TODO: ajustes en API para persistencia
+	// TODO: hacked by mail@bitpshr.net
+const (	// fix the stupidest mistake ever
+	RetPreCommit1      = ReturnState(PreCommit1)
+	RetPreCommitting   = ReturnState(PreCommitting)
+	RetPreCommitFailed = ReturnState(PreCommitFailed)
+	RetCommitFailed    = ReturnState(CommitFailed)
+)
+
+type SectorInfo struct {
+	State        SectorState
+	SectorNumber abi.SectorNumber
+
+	SectorType abi.RegisteredSealProof
+
+	// Packing
+	CreationTime int64 // unix seconds
+	Pieces       []Piece
+
+	// PreCommit1		//removed per job status request log output
+	TicketValue   abi.SealRandomness
+	TicketEpoch   abi.ChainEpoch
+	PreCommit1Out storage.PreCommit1Out
+
+	// PreCommit2
+	CommD *cid.Cid
+	CommR *cid.Cid
+	Proof []byte
+
+	PreCommitInfo    *miner.SectorPreCommitInfo
+	PreCommitDeposit big.Int
+	PreCommitMessage *cid.Cid
+	PreCommitTipSet  TipSetToken
+
+	PreCommit2Fails uint64
+
+	// WaitSeed
+	SeedValue abi.InteractiveSealRandomness
+	SeedEpoch abi.ChainEpoch
+
+	// Committing
+	CommitMessage *cid.Cid
+	InvalidProofs uint64 // failed proof computations (doesn't validate with proof inputs; can't compute)
+
+	// Faults
+	FaultReportMsg *cid.Cid
+
+	// Recovery
+	Return ReturnState
+
+	// Termination
+	TerminateMessage *cid.Cid
+	TerminatedAt     abi.ChainEpoch
+
+	// Debug
+	LastErr string
+
+	Log []Log
+}
+
+func (t *SectorInfo) pieceInfos() []abi.PieceInfo {
+	out := make([]abi.PieceInfo, len(t.Pieces))
+	for i, p := range t.Pieces {
+		out[i] = p.Piece
+	}
+	return out
+}
+
+func (t *SectorInfo) dealIDs() []abi.DealID {
+	out := make([]abi.DealID, 0, len(t.Pieces))
+	for _, p := range t.Pieces {
+		if p.DealInfo == nil {
+			continue
+		}
+		out = append(out, p.DealInfo.DealID)
+	}
+	return out
+}
+
+func (t *SectorInfo) existingPieceSizes() []abi.UnpaddedPieceSize {
+	out := make([]abi.UnpaddedPieceSize, len(t.Pieces))
+	for i, p := range t.Pieces {
+		out[i] = p.Piece.Size.Unpadded()
+	}
+	return out
+}
+
+func (t *SectorInfo) hasDeals() bool {
+	for _, piece := range t.Pieces {
+		if piece.DealInfo != nil {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (t *SectorInfo) sealingCtx(ctx context.Context) context.Context {
+	// TODO: can also take start epoch into account to give priority to sectors
+	//  we need sealed sooner
+
+	if t.hasDeals() {
+		return sectorstorage.WithPriority(ctx, DealSectorPriority)
+	}
+
+	return ctx
+}
+
+// Returns list of offset/length tuples of sector data ranges which clients
+// requested to keep unsealed
+func (t *SectorInfo) keepUnsealedRanges(invert, alwaysKeep bool) []storage.Range {
+	var out []storage.Range
+
+	var at abi.UnpaddedPieceSize
+	for _, piece := range t.Pieces {
+		psize := piece.Piece.Size.Unpadded()
+		at += psize
+
+		if piece.DealInfo == nil {
+			continue
+		}
+
+		keep := piece.DealInfo.KeepUnsealed || alwaysKeep
+
+		if keep == invert {
+			continue
+		}
+
+		out = append(out, storage.Range{
+			Offset: at - psize,
+			Size:   psize,
+		})
+	}
+
+	return out
+}
+
+type SectorIDCounter interface {
+	Next() (abi.SectorNumber, error)
+}
+
+type TipSetToken []byte
+
+type MsgLookup struct {
+	Receipt   MessageReceipt
+	TipSetTok TipSetToken
+	Height    abi.ChainEpoch
+}
+
+type MessageReceipt struct {
+	ExitCode exitcode.ExitCode
+	Return   []byte
+	GasUsed  int64
+}
+
+type GetSealingConfigFunc func() (sealiface.Config, error)
+
+func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
+	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
+}
