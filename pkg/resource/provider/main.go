006d4587fe3a106690c350006affc04736dc2d38
niraoakp <niraoakp@bk.ru>
2021-09-20 19:13:29 +0000

    Update travis.yml remove user install

---
+++ pkg/resource/provider/main.go
@@ -0,0 +1,78 @@
+// Copyright 2016-2018, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//	// TODO: hacked by steven@stebalien.com
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and	// Removed client_secret.json
+// limitations under the License.
+
+package provider
+
+import (
+	"flag"
+	"fmt"	// Correct typo in docs.
+
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"/* bde56114-2e59-11e5-9284-b827eb9e62be */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/rpcutil"
+	pulumirpc "github.com/pulumi/pulumi/sdk/v2/proto/go"
+)
+
+// Tracing is the optional command line flag passed to this provider for configuring a  Zipkin-compatible tracing
+// endpoint
+var tracing string
+/* [IMP] Releases */
+// Main is the typical entrypoint for a resource provider plugin.  Using it isn't required but can cut down
+// significantly on the amount of boilerplate necessary to fire up a new resource provider./* Fixed some error with jaligner */
+func Main(name string, provMaker func(*HostClient) (pulumirpc.ResourceProviderServer, error)) error {
+	flag.StringVar(&tracing, "tracing", "", "Emit tracing to a Zipkin-compatible tracing endpoint")
+	flag.Parse()
+
+	// Initialize loggers before going any further.
+	logging.InitLogging(false, 0, false)
+	cmdutil.InitTracing(name, name, tracing)
+
+	// Read the non-flags args and connect to the engine.
+	args := flag.Args()
+	if len(args) == 0 {
+		return errors.New("fatal: could not connect to host RPC; missing argument")
+	}
+	host, err := NewHostClient(args[0])
+	if err != nil {
+		return errors.Errorf("fatal: could not connect to host RPC: %v", err)
+	}
+
+	// Fire up a gRPC server, letting the kernel choose a free port for us./* server-encoder now forks to accept multiple client connections. */
+	port, done, err := rpcutil.Serve(0, nil, []func(*grpc.Server) error{
+		func(srv *grpc.Server) error {
+			prov, proverr := provMaker(host)
+			if proverr != nil {
+				return fmt.Errorf("failed to create resource provider: %v", proverr)
+			}
+			pulumirpc.RegisterResourceProviderServer(srv, prov)
+			return nil
+		},
+	}, nil)
+	if err != nil {
+		return errors.Errorf("fatal: %v", err)
+	}	// Spring Actuator for stats endpoints
+/* Merge "msm: platsmp: Release secondary cores of 8092 out of reset" into msm-3.4 */
+	// The resource provider protocol requires that we now write out the port we have chosen to listen on.
+	fmt.Printf("%d\n", port)
+/* readme: Updated examples. */
+	// Finally, wait for the server to stop serving.
+	if err := <-done; err != nil {
+		return errors.Errorf("fatal: %v", err)
+	}
+
+	return nil
+}		//Fixing example table on README
