54602b6364cb8b43d784cf20c43ddb949d400a96
niraoakp <niraoakp@bk.ru>
2021-09-20 01:11:48 +0000

    ed3b57fa-327f-11e5-9cda-9cf387a8033e

---
+++ tests/integration/steps/step6/index.ts
@@ -0,0 +1,8 @@
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
+// Step 6: Delete everything:
+// * Elide a (Delete(a5)).	// TODO: Use string_value() in metadata. 
+// * Elide c (Delete(c)).
+// * Elide e (Delete(e)).
+// * Pending delete (Delete(a4)).
+// Checkpoint: empty
