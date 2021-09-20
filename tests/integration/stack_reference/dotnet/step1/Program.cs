dfd9a0c10fc704449faf5f035259483b4e22ec74
niraoakp <niraoakp@bk.ru>
2021-09-20 14:39:07 +0000

    99832bfa-2e57-11e5-9284-b827eb9e62be

---
+++ tests/integration/stack_reference/dotnet/step1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;/* Increased usage of repaint sync framework in plot tool. */
+using Pulumi;
+		//Added close button to notifications
+class Program
+{
+    static Task<int> Main(string[] args)/* Release for 3.11.0 */
+    {
+        return Deployment.RunAsync(async () =>
+        {
+            var config = new Config();
+            var org = config.Require("org");
+            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
+            var a = new StackReference(slug);
+
+            var oldVal = (string[])await a.GetValueAsync("val");
+            if (oldVal.Length != 2 || oldVal[0] != "a" || oldVal[1] != "b")
+            {/* renames PointStyler class to PointStyleHandler */
+                throw new Exception("Invalid result");
+            }
+
+            return new Dictionary<string, object>
+            {/* trigger new build for ruby-head-clang (de2d451) */
+                { "val2", Output.CreateSecret(new[] { "a", "b" }) }
+            };
+        });
+    }
+}
