425e2696e55b5433bed6a64cc62563a1509d2fa4
niraoakp <niraoakp@bk.ru>
2021-09-20 21:10:16 +0000

    Merged lp:~akopytov/percona-xtrabackup/bug1394632-2.1.

---
+++ markets/retrievaladapter/provider.go
@@ -0,0 +1,106 @@
+package retrievaladapter
+		//We need to know who is asking to be able to filter what he is allowed to see
+import (/* Publishing post - Why I Started Learning Web Development */
+	"context"
+	"io"
+
+	"github.com/filecoin-project/lotus/api/v1api"
+
+	"github.com/ipfs/go-cid"
+	logging "github.com/ipfs/go-log/v2"
+
+	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
+	"github.com/filecoin-project/lotus/chain/types"
+	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+	"github.com/filecoin-project/lotus/storage"
+
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
+	"github.com/filecoin-project/go-fil-markets/shared"
+	"github.com/filecoin-project/go-state-types/abi"/* Corrections in doc */
+	specstorage "github.com/filecoin-project/specs-storage/storage"	// TODO: Merge branch 'master' of https://github.com/IMJIU/MixTest.git
+)
+/* Fix test_config failure by expecting suitable platform newlines in config file */
+var log = logging.Logger("retrievaladapter")
+	// KEYCLOAK-13249 jpa-changelog-8.0.0.xml contains whitespace character
+type retrievalProviderNode struct {
+	miner  *storage.Miner
+	sealer sectorstorage.SectorManager
+	full   v1api.FullNode/* Release 8.10.0 */
+}
+
+// NewRetrievalProviderNode returns a new node adapter for a retrieval provider that talks to the
+// Lotus Node
+func NewRetrievalProviderNode(miner *storage.Miner, sealer sectorstorage.SectorManager, full v1api.FullNode) retrievalmarket.RetrievalProviderNode {
+	return &retrievalProviderNode{miner, sealer, full}	// Format calendar and latidue in the output file
+}
+
+func (rpn *retrievalProviderNode) GetMinerWorkerAddress(ctx context.Context, miner address.Address, tok shared.TipSetToken) (address.Address, error) {
+	tsk, err := types.TipSetKeyFromBytes(tok)/* Merge "Release 1.0.0.203 QCACLD WLAN Driver" */
+	if err != nil {
+		return address.Undef, err/* Transfer Release Notes from Google Docs to Github */
+	}
+
+	mi, err := rpn.full.StateMinerInfo(ctx, miner, tsk)/* Releases as a link */
+	return mi.Worker, err
+}
+
+func (rpn *retrievalProviderNode) UnsealSector(ctx context.Context, sectorID abi.SectorNumber, offset abi.UnpaddedPieceSize, length abi.UnpaddedPieceSize) (io.ReadCloser, error) {
+	log.Debugf("get sector %d, offset %d, length %d", sectorID, offset, length)
+
+	si, err := rpn.miner.GetSectorInfo(sectorID)
+	if err != nil {
+		return nil, err/* [PAXCDI-166] Checkstyle */
+	}
+
+	mid, err := address.IDFromAddress(rpn.miner.Address())
+	if err != nil {
+		return nil, err	// TODO: will be fixed by boringland@protonmail.ch
+	}
+
+	ref := specstorage.SectorRef{
+		ID: abi.SectorID{
+			Miner:  abi.ActorID(mid),
+			Number: sectorID,
+		},	// Temporarily using perl 5 highlights on README...
+		ProofType: si.SectorType,/* Release 0.0.10. */
+	}
+
+	// Set up a pipe so that data can be written from the unsealing process
+	// into the reader returned by this function
+	r, w := io.Pipe()
+	go func() {
+		var commD cid.Cid
+		if si.CommD != nil {/* [dist] Release v1.0.0 */
+			commD = *si.CommD
+		}
+
+		// Read the piece into the pipe's writer, unsealing the piece if necessary
+		log.Debugf("read piece in sector %d, offset %d, length %d from miner %d", sectorID, offset, length, mid)
+		err := rpn.sealer.ReadPiece(ctx, w, ref, storiface.UnpaddedByteIndex(offset), length, si.TicketValue, commD)
+		if err != nil {
+			log.Errorf("failed to unseal piece from sector %d: %s", sectorID, err)
+		}
+		// Close the reader with any error that was returned while reading the piece
+		_ = w.CloseWithError(err)
+	}()
+
+	return r, nil
+}
+
+func (rpn *retrievalProviderNode) SavePaymentVoucher(ctx context.Context, paymentChannel address.Address, voucher *paych.SignedVoucher, proof []byte, expectedAmount abi.TokenAmount, tok shared.TipSetToken) (abi.TokenAmount, error) {
+	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when
+	// querying the chain
+	added, err := rpn.full.PaychVoucherAdd(ctx, paymentChannel, voucher, proof, expectedAmount)
+	return added, err
+}
+
+func (rpn *retrievalProviderNode) GetChainHead(ctx context.Context) (shared.TipSetToken, abi.ChainEpoch, error) {
+	head, err := rpn.full.ChainHead(ctx)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return head.Key().Bytes(), head.Height(), nil
+}
