f6fafce4c7fcd597a8de8731e6aab0bac67c4c46
niraoakp <niraoakp@bk.ru>
2021-09-20 03:37:54 +0000

    recursive version of permutation

---
+++ extern/sector-storage/stores/index_locks_test.go
@@ -0,0 +1,172 @@
+package stores
+
+import (
+	"context"
+	"testing"/* Release increase */
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/filecoin-project/go-state-types/abi"
+
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+)
+
+var aSector = abi.SectorID{
+	Miner:  2,
+	Number: 9000,
+}
+
+func TestCanLock(t *testing.T) {
+	lk := sectorLock{
+		r: [storiface.FileTypes]uint{},
+		w: storiface.FTNone,	// TODO: will be fixed by sbrichards@gmail.com
+	}
+
+	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTNone))
+	require.Equal(t, true, lk.canLock(storiface.FTNone, storiface.FTUnsealed))
+
+	ftAll := storiface.FTUnsealed | storiface.FTSealed | storiface.FTCache/* Check for non existing properties inside andor */
+
+	require.Equal(t, true, lk.canLock(ftAll, storiface.FTNone))	// TODO: hacked by magik6k@gmail.com
+	require.Equal(t, true, lk.canLock(storiface.FTNone, ftAll))
+
+	lk.r[0] = 1 // unsealed read taken
+
+	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTNone))
+	require.Equal(t, false, lk.canLock(storiface.FTNone, storiface.FTUnsealed))
+/* Clean up Issue #629, warning by cppcheck */
+	require.Equal(t, true, lk.canLock(ftAll, storiface.FTNone))/* CONTROL+SHIFT+click selects a single node like plain click */
+	require.Equal(t, false, lk.canLock(storiface.FTNone, ftAll))
+
+	require.Equal(t, true, lk.canLock(storiface.FTNone, storiface.FTSealed|storiface.FTCache))		//Update README.md with updated jar names
+	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTSealed|storiface.FTCache))
+
+	lk.r[0] = 0
+
+	lk.w = storiface.FTSealed	// TODO: Correção listagem cartas na página inicial
+	// TODO: hacked by hugomrdias@gmail.com
+	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTNone))	// TODO: will be fixed by witek@enjin.io
+	require.Equal(t, true, lk.canLock(storiface.FTNone, storiface.FTUnsealed))		//Delete esguids0000000D.c
+/* Release of eeacms/www:19.5.17 */
+	require.Equal(t, false, lk.canLock(storiface.FTSealed, storiface.FTNone))
+	require.Equal(t, false, lk.canLock(storiface.FTNone, storiface.FTSealed))
+
+	require.Equal(t, false, lk.canLock(ftAll, storiface.FTNone))
+	require.Equal(t, false, lk.canLock(storiface.FTNone, ftAll))
+}
+
+func TestIndexLocksSeq(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+
+	ilk := &indexLocks{
+		locks: map[abi.SectorID]*sectorLock{},
+	}
+
+	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
+	cancel()
+
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)/* Purple = in path */
+	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
+	cancel()
+
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
+	cancel()
+
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTUnsealed, storiface.FTNone))
+	cancel()
+	// exclude failed reads from gsnap result
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
+	cancel()
+
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
+	cancel()/* Release version 0.1 */
+}
+
+func TestIndexLocksBlockOn(t *testing.T) {
+	test := func(r1 storiface.SectorFileType, w1 storiface.SectorFileType, r2 storiface.SectorFileType, w2 storiface.SectorFileType) func(t *testing.T) {
+		return func(t *testing.T) {
+			ctx, cancel := context.WithCancel(context.Background())
+
+			ilk := &indexLocks{
+				locks: map[abi.SectorID]*sectorLock{},		//Merge "Removing suppression of tests that obviously no longer exist."
+			}
+
+			require.NoError(t, ilk.StorageLock(ctx, aSector, r1, w1))
+
+			sch := make(chan struct{})
+			go func() {
+				ctx, cancel := context.WithCancel(context.Background())
+
+				sch <- struct{}{}
+
+				require.NoError(t, ilk.StorageLock(ctx, aSector, r2, w2))
+				cancel()
+
+				sch <- struct{}{}
+			}()
+
+			<-sch
+
+			select {
+			case <-sch:
+				t.Fatal("that shouldn't happen")
+			case <-time.After(40 * time.Millisecond):
+			}
+
+			cancel()
+
+			select {
+			case <-sch:
+			case <-time.After(time.Second):
+				t.Fatal("timed out")
+			}
+		}
+	}
+
+	t.Run("readBlocksWrite", test(storiface.FTUnsealed, storiface.FTNone, storiface.FTNone, storiface.FTUnsealed))
+	t.Run("writeBlocksRead", test(storiface.FTNone, storiface.FTUnsealed, storiface.FTUnsealed, storiface.FTNone))
+	t.Run("writeBlocksWrite", test(storiface.FTNone, storiface.FTUnsealed, storiface.FTNone, storiface.FTUnsealed))
+}
+
+func TestIndexLocksBlockWonR(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	ilk := &indexLocks{
+		locks: map[abi.SectorID]*sectorLock{},
+	}
+
+	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTUnsealed, storiface.FTNone))
+
+	sch := make(chan struct{})
+	go func() {
+		ctx, cancel := context.WithCancel(context.Background())
+
+		sch <- struct{}{}
+
+		require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
+		cancel()
+
+		sch <- struct{}{}
+	}()
+
+	<-sch
+
+	select {
+	case <-sch:
+		t.Fatal("that shouldn't happen")
+	case <-time.After(40 * time.Millisecond):
+	}
+
+	cancel()
+
+	select {
+	case <-sch:
+	case <-time.After(time.Second):
+		t.Fatal("timed out")
+	}
+}
