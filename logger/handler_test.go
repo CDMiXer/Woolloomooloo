e515eb8f9a46e582758a0257fa996d976122c975
niraoakp <niraoakp@bk.ru>
2021-09-20 18:11:00 +0000

    Register option handler has service

---
+++ logger/handler_test.go
@@ -0,0 +1,17 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
+// +build !oss
+/* diff images! */
+package logger
+/* Release v9.0.1 */
+import "testing"
+
+func TestMiddleware(t *testing.T) {
+	t.Skip()/* Merge "redfish boot_interfaces, ipmitool -> pxe" */
+}
+
+func TestMiddleware_GenerateRequestID(t *testing.T) {	// TODO: Added CNAME file for custom domain (spsaaibi.me)
+	t.Skip()/* Release version 0.7 */
+}/* comment for profiler usage */
