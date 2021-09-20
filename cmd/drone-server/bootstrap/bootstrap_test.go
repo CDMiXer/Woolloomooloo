ccc47f61b5080a3e2b9b2c6b330c35cc8b8f0e2b
niraoakp <niraoakp@bk.ru>
2021-09-20 19:48:25 +0000

    Removed unec. wicket xml namespace from view.

---
+++ cmd/drone-server/bootstrap/bootstrap_test.go
@@ -0,0 +1,170 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
+package bootstrap
+
+import (
+	"context"	// TODO: upgraded processing\core.jar to 1.2.1
+	"database/sql"
+	"io/ioutil"
+	"testing"
+
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/mock"	// TODO: fix pyyaml install
+
+	"github.com/dchest/uniuri"
+	"github.com/golang/mock/gomock"/* Fix relative links in Release Notes */
+	"github.com/sirupsen/logrus"
+)
+
+var noContext = context.TODO()		//Clarify character limit
+
+func init() {
+	logrus.SetOutput(ioutil.Discard)
+}	// TODO: hacked by aeongrp@outlook.com
+/* Merge "Release wakelock after use" into honeycomb-mr2 */
+func TestBootstrap(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+/* Create silverstripe */
+	dummyUser := &core.User{
+		Login:   "octocat",
+		Machine: true,	// TODO: will be fixed by yuvalalaluf@gmail.com
+		Admin:   true,
+		Hash:    uniuri.NewLen(32),
+	}
+
+	store := mock.NewMockUserStore(controller)
+	store.EXPECT().FindLogin(gomock.Any(), dummyUser.Login).Return(nil, sql.ErrNoRows)
+	store.EXPECT().Create(gomock.Any(), dummyUser).Return(nil)/* Most recent DiceBoy Executable */
+
+	err := New(store).Bootstrap(noContext, dummyUser)
+	if err != nil {
+		t.Error(err)
+	}
+}		//7f7fa4e4-2e58-11e5-9284-b827eb9e62be
+
+func TestBootstrap_GenerateHash(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	dummyUser := &core.User{
+		Login:   "octocat",
+		Machine: false,/* Release 2.64 */
+		Admin:   true,
+		Hash:    "",
+	}
+
+	store := mock.NewMockUserStore(controller)
+	store.EXPECT().FindLogin(gomock.Any(), dummyUser.Login).Return(nil, sql.ErrNoRows)
+	store.EXPECT().Create(gomock.Any(), dummyUser).Return(nil)
+
+	err := New(store).Bootstrap(noContext, dummyUser)
+	if err != nil {
+		t.Error(err)
+	}
+	if got, want := len(dummyUser.Hash), 32; got != want {
+		t.Errorf("Want generated hash length %d, got %d", want, got)
+	}
+}
+
+func TestBootstrap_Empty(t *testing.T) {/* Fixed ArrayIndexOutOfBoundsException for out-of-bounds cells. */
+	controller := gomock.NewController(t)
+	defer controller.Finish()/* Update ReleaseNotes.rst */
+
+	dummyUser := &core.User{
+		Login: "",	// Remove bug where designated initializer was causing problems
+	}
+
+	store := mock.NewMockUserStore(controller)
+	err := New(store).Bootstrap(noContext, dummyUser)
+	if err != nil {
+		t.Error(err)/* Update 4.6 Release Notes */
+	}
+}
+/* Release 12.0.2 */
+func TestBootstrap_Exists_WithoutUpdates(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	dummyUser := &core.User{
+		Login:   "octocat",
+		Machine: true,
+		Admin:   true,
+		Hash:    uniuri.NewLen(32),
+	}
+
+	store := mock.NewMockUserStore(controller)
+	store.EXPECT().FindLogin(gomock.Any(), dummyUser.Login).Return(dummyUser, nil)
+	err := New(store).Bootstrap(noContext, dummyUser)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBootstrap_Exists_WithUpdates(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	dummyUser := &core.User{
+		Login:   "octocat",
+		Machine: true,
+		Admin:   true,
+		Hash:    uniuri.NewLen(32),
+	}
+	existingUser := &core.User{
+		Login:   "octocat",
+		Machine: false,
+		Admin:   false,
+		Hash:    uniuri.NewLen(32),
+	}
+
+	store := mock.NewMockUserStore(controller)
+	store.EXPECT().FindLogin(gomock.Any(), dummyUser.Login).Return(existingUser, nil)
+	store.EXPECT().Update(gomock.Any(), existingUser).Return(nil)
+	err := New(store).Bootstrap(noContext, dummyUser)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBootstrap_MissingTokenError(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	dummyUser := &core.User{
+		Login:   "octocat",
+		Machine: true,
+		Admin:   true,
+	}
+
+	store := mock.NewMockUserStore(controller)
+	store.EXPECT().FindLogin(gomock.Any(), dummyUser.Login).Return(nil, sql.ErrNoRows)
+
+	err := New(store).Bootstrap(noContext, dummyUser)
+	if err != errMissingToken {
+		t.Errorf("Expect missing token error")
+	}
+}
+
+func TestBootstrap_CreateError(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	dummyUser := &core.User{
+		Login:   "octocat",
+		Machine: true,
+		Admin:   true,
+		Hash:    uniuri.NewLen(32),
+	}
+
+	store := mock.NewMockUserStore(controller)
+	store.EXPECT().FindLogin(gomock.Any(), dummyUser.Login).Return(nil, sql.ErrNoRows)
+	store.EXPECT().Create(gomock.Any(), dummyUser).Return(sql.ErrConnDone)
+
+	err := New(store).Bootstrap(noContext, dummyUser)
+	if err != sql.ErrConnDone {
+		t.Errorf("Expect error creating user")
+	}
+}
