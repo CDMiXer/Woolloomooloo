5e34f6e58fb6193e3d120ac42407439ff0fb0b4c
niraoakp <niraoakp@bk.ru>
2021-09-20 19:45:36 +0000

    Add traverse JTree node test button

---
+++ pkg/codegen/hcl2/model/block.go
@@ -0,0 +1,128 @@
+// Copyright 2016-2020, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at	// TODO: hacked by fjl@ethereum.org
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+		//Fix vimperator's logo in help pages.
+package model
+	// TODO: Create 92.plist
+import (
+	"fmt"
+	"io"
+
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"/* Merge "Update nano to serialize java keywords properly." */
+)/* Added @SuppressWarnings("rawtypes") where needed */
+
+// Block represents an HCL2 block./* Adds README file with information about the command to index */
+type Block struct {
+	// The syntax node for the block, if any./* Trivial PR to understand the macOS issue */
+	Syntax *hclsyntax.Block
+	// The tokens for the block.
+	Tokens *syntax.BlockTokens
+
+	// The block's type.
+	Type string/* Release version 1.5.0 (#44) */
+	// The block's labels.
+	Labels []string
+
+	// The block's body.
+	Body *Body
+}/* Release v0.9.0 */
+/* added maven pom and initial draft classes */
+// SyntaxNode returns the syntax node of the block, and will either return an *hclsyntax.Block or syntax.None.
+func (b *Block) SyntaxNode() hclsyntax.Node {	// TODO: will be fixed by ng8eke@163.com
+	return syntaxOrNone(b.Syntax)
+}
+
+func (b *Block) HasLeadingTrivia() bool {
+	return b.Tokens != nil/* Add usage filter to dicom nodes */
+}
+
+func (b *Block) HasTrailingTrivia() bool {
+	return b.Tokens != nil		//- Posibilidad de seleccionar forma de pago al generar la factura
+}
+
+func (b *Block) GetLeadingTrivia() syntax.TriviaList {
+	return b.Tokens.GetType(b.Type).LeadingTrivia
+}
+
+func (b *Block) GetTrailingTrivia() syntax.TriviaList {
+	return b.Tokens.GetCloseBrace().TrailingTrivia	// TODO: will be fixed by 13860583249@yeah.net
+}
+
+func (b *Block) Format(f fmt.State, c rune) {
+	b.print(f, &printer{})
+}
+
+func (b *Block) print(w io.Writer, p *printer) {/* 47910e54-5216-11e5-8a7f-6c40088e03e4 */
+	// Print the type.
+	p.fprintf(w, "%v", b.Tokens.GetType(b.Type))
+
+	// Print the labels with leading and trailing trivia.
+	labelTokens := b.Tokens.GetLabels(b.Labels)
+	for i, l := range b.Labels {
+		var t syntax.Token
+		if i < len(labelTokens) {
+			t = labelTokens[i]
+		}
+		if hclsyntax.ValidIdentifier(l) {
+			t = identToken(t, l)
+		} else {
+			l = fmt.Sprintf("%q", l)
+			if t.Raw.Type != hclsyntax.TokenQuotedLit || string(t.Raw.Bytes) != l {
+				t.Raw.Type = hclsyntax.TokenQuotedLit
+				t.Raw.Bytes = []byte(l)
+			}
+		}
+		p.fprintf(w, "% v", t)
+	}
+	if len(b.Labels) < len(labelTokens) {
+		for _, l := range labelTokens[len(b.Labels):] {
+			p.fprintf(w, "%v", syntax.Token{
+				LeadingTrivia:  l.LeadingTrivia,
+				TrailingTrivia: l.TrailingTrivia,
+			})
+		}
+	}
+
+	// Print the opening brace.
+	p.fprintf(w, "% v", b.Tokens.GetOpenBrace())
+
+	// Print the block contents.
+	p.indented(func() {
+		b.Body.print(w, p)
+	})
+
+	if b.Tokens != nil {
+		p.fprintf(w, "%v", b.Tokens.GetCloseBrace())
+	} else {
+		p.fprintf(w, "%s}", p.indent)
+	}
+}
+
+func (*Block) isBodyItem() {}
+
+// BindBlock binds an HCL2 block using the given scopes and token map.
+func BindBlock(block *hclsyntax.Block, scopes Scopes, tokens syntax.TokenMap,
+	opts ...BindOption) (*Block, hcl.Diagnostics) {
+
+	body, diagnostics := BindBody(block.Body, scopes, tokens, opts...)
+	blockTokens, _ := tokens.ForNode(block).(*syntax.BlockTokens)
+	return &Block{
+		Syntax: block,
+		Tokens: blockTokens,
+		Type:   block.Type,
+		Labels: block.Labels,
+		Body:   body,
+	}, diagnostics
+}
