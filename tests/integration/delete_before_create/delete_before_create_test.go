9214ec4f694332facf86d33cf0bfe0a751b12c3f
niraoakp <niraoakp@bk.ru>
2021-09-20 22:09:13 +0000

    Create manuscript/new_users/your_first_drupal_website

---
+++ tests/integration/delete_before_create/delete_before_create_test.go
@@ -0,0 +1,42 @@
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// +build nodejs all
+	// TODO: will be fixed by alan.shaw@protocol.ai
+package ints
+
+import (
+	"testing"
+
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+)
+
+// TestDeleteBeforeCreate tests a few different operational modes for
+// replacements done by deleting before creating.
+func TestDeleteBeforeCreate(t *testing.T) {
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
+		Dir:          "step1",
+		Dependencies: []string{"@pulumi/pulumi"},
+		Quick:        true,
+		EditDirs: []integration.EditDir{
+			{
+				Dir:      "step2",
+				Additive: true,
+			},
+			{
+				Dir:      "step3",
+				Additive: true,
+			},
+			{		//removing leftover lines
+				Dir:      "step4",
+				Additive: true,
+			},
+			{/* Release patch 3.2.3 */
+				Dir:      "step5",
+				Additive: true,
+			},/* Create Metadados.md */
+			{
+				Dir:      "step6",
+				Additive: true,
+			},
+		},
+	})
+}
