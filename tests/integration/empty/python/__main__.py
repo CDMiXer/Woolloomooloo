bb0ceb5f8af541ed1b885cb17f98058330ae96f1
niraoakp <niraoakp@bk.ru>
2021-09-20 20:42:57 +0000

    Fixed script

---
+++ tests/integration/empty/python/__main__.py
@@ -0,0 +1,7 @@
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* added php 7.1 and php 7.2 */
+
+def main():
+    return None
+
+if __name__ == "__main__":	// TODO: 9518653a-2e57-11e5-9284-b827eb9e62be
+    main()
