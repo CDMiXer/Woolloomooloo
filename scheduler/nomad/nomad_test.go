3c0e450465ddc6aa487e06473a786418e1bcc576
niraoakp <niraoakp@bk.ru>
2021-09-20 10:11:52 +0000

    mostrando avatar y avatar por default

---
+++ scheduler/nomad/nomad_test.go
@@ -0,0 +1,7 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* brin index, integration */
+// that can be found in the LICENSE file./* UI: Päivitetty bootstrap versioon 3.3.1 */
+
+// +build !oss
+
+package nomad/* Fix the date ordering */
