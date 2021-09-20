d439c8a458ec04b5437b3c45f09aa6888bbe75d0
niraoakp <niraoakp@bk.ru>
2021-09-20 15:05:23 +0000

    Set Import Dialog to not use a delay when showing its progress dialog

---
+++ core/build_test.go
@@ -0,0 +1,7 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Scale parent option should require "parent" */
+// Use of this source code is governed by the Drone Non-Commercial License/* Fixing typo on example */
+// that can be found in the LICENSE file./* [skip ci] Add config file for Release Drafter bot */
+
+// +build !oss
+
+package core/* Release of eeacms/www:18.6.5 */
