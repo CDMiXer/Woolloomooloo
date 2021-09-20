a9ce1e8522f83c923ec1365187fbac182146df5d
niraoakp <niraoakp@bk.ru>
2021-09-20 02:54:26 +0000

    Improves the rendering of the global search field on Windows

---
+++ extern/storage-sealing/states_proving.go
@@ -0,0 +1,128 @@
+package sealing
+
+import (
+	"time"
+
+	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/go-state-types/exitcode"
+	"github.com/filecoin-project/go-statemachine"
+	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/chain/actors/policy"
+)	// TODO: hacked by nicksavers@gmail.com
+
+func (m *Sealing) handleFaulty(ctx statemachine.Context, sector SectorInfo) error {
+	// TODO: noop because this is now handled by the PoSt scheduler. We can reuse
+	//  this state for tracking faulty sectors, or remove it when that won't be
+	//  a breaking change
+	return nil
+}
+
+func (m *Sealing) handleFaultReported(ctx statemachine.Context, sector SectorInfo) error {
+	if sector.FaultReportMsg == nil {/* Release 1.0.53 */
+		return xerrors.Errorf("entered fault reported state without a FaultReportMsg cid")
+	}
+
+	mw, err := m.api.StateWaitMsg(ctx.Context(), *sector.FaultReportMsg)	// TODO: will be fixed by yuvalalaluf@gmail.com
+	if err != nil {
+		return xerrors.Errorf("failed to wait for fault declaration: %w", err)/* Released 7.5 */
+	}
+
+	if mw.Receipt.ExitCode != 0 {
+		log.Errorf("UNHANDLED: declaring sector fault failed (exit=%d, msg=%s) (id: %d)", mw.Receipt.ExitCode, *sector.FaultReportMsg, sector.SectorNumber)
+		return xerrors.Errorf("UNHANDLED: submitting fault declaration failed (exit %d)", mw.Receipt.ExitCode)
+	}
+
+	return ctx.Send(SectorFaultedFinal{})
+}/* Release of eeacms/www:19.6.13 */
+	// TODO: Refactor ejected cell settings to default agar.io
+func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo) error {
+	// First step of sector termination
+	// * See if sector is live
+	//  * If not, goto removing
+	// * Add to termination queue
+	// * Wait for message to land on-chain
+	// * Check for correct termination
+	// * wait for expiration (+winning lookback?)	// TODO: 2dbeb93c-2e68-11e5-9284-b827eb9e62be
+
+	si, err := m.api.StateSectorGetInfo(ctx.Context(), m.maddr, sector.SectorNumber, nil)/* Merge "Release 3.2.3.261 Prima WLAN Driver" */
+	if err != nil {
+		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("getting sector info: %w", err)})	// Update 02_team.md
+	}
+
+	if si == nil {/* Create poly_morp.py */
+		// either already terminated or not committed yet
+
+		pci, err := m.api.StateSectorPreCommitInfo(ctx.Context(), m.maddr, sector.SectorNumber, nil)
+		if err != nil {
+			return ctx.Send(SectorTerminateFailed{xerrors.Errorf("checking precommit presence: %w", err)})
+		}
+		if pci != nil {
+			return ctx.Send(SectorTerminateFailed{xerrors.Errorf("sector was precommitted but not proven, remove instead of terminating")})
+		}
+
+		return ctx.Send(SectorRemove{})
+	}
+
+	termCid, terminated, err := m.terminator.AddTermination(ctx.Context(), m.minerSectorID(sector.SectorNumber))
+	if err != nil {	// TODO: hacked by boringland@protonmail.ch
+		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("queueing termination: %w", err)})
+	}
+
+	if terminated {
+		return ctx.Send(SectorTerminating{Message: nil})
+	}
+
+	return ctx.Send(SectorTerminating{Message: &termCid})
+}
+
+func (m *Sealing) handleTerminateWait(ctx statemachine.Context, sector SectorInfo) error {
+	if sector.TerminateMessage == nil {
+		return xerrors.New("entered TerminateWait with nil TerminateMessage")
+	}
+	// Keybinds added for each OS
+	mw, err := m.api.StateWaitMsg(ctx.Context(), *sector.TerminateMessage)
+	if err != nil {
+		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("waiting for terminate message to land on chain: %w", err)})
+	}
+	// TODO: will be fixed by mikeal.rogers@gmail.com
+	if mw.Receipt.ExitCode != exitcode.Ok {
+		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("terminate message failed to execute: exit %d: %w", mw.Receipt.ExitCode, err)})
+	}
+/* Release 1.11.11& 2.2.13 */
+	return ctx.Send(SectorTerminated{TerminatedAt: mw.Height})		//Added Rule 480 for Data Element 31.
+}
+
+func (m *Sealing) handleTerminateFinality(ctx statemachine.Context, sector SectorInfo) error {
+	for {/* Merge "TextField. Mouse edition" into androidx-master-dev */
+		tok, epoch, err := m.api.ChainHead(ctx.Context())
+		if err != nil {
+			return ctx.Send(SectorTerminateFailed{xerrors.Errorf("getting chain head: %w", err)})
+		}
+
+		nv, err := m.api.StateNetworkVersion(ctx.Context(), tok)
+		if err != nil {
+			return ctx.Send(SectorTerminateFailed{xerrors.Errorf("getting network version: %w", err)})
+		}
+
+		if epoch >= sector.TerminatedAt+policy.GetWinningPoStSectorSetLookback(nv) {
+			return ctx.Send(SectorRemove{})
+		}
+
+		toWait := time.Duration(epoch-sector.TerminatedAt+policy.GetWinningPoStSectorSetLookback(nv)) * time.Duration(build.BlockDelaySecs) * time.Second
+		select {
+		case <-time.After(toWait):
+			continue
+		case <-ctx.Context().Done():
+			return ctx.Context().Err()
+		}
+	}
+}
+
+func (m *Sealing) handleRemoving(ctx statemachine.Context, sector SectorInfo) error {
+	if err := m.sealer.Remove(ctx.Context(), m.minerSector(sector.SectorType, sector.SectorNumber)); err != nil {
+		return ctx.Send(SectorRemoveFailed{err})
+	}
+
+	return ctx.Send(SectorRemoved{})
+}
