953d99c97debe89b59d33d148164871350f6e3be
niraoakp <niraoakp@bk.ru>
2021-09-20 23:15:30 +0000

    computing anova p-values

---
+++ operator/manager/teardown_test.go
@@ -0,0 +1,5 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Add thanks to Mathias to README
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// TODO: Merge branch 'master' into dima/revert-gulp
+
+package manager
