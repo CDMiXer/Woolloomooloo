757cbfacdb93678786819988400e6b099edb28e2
niraoakp <niraoakp@bk.ru>
2021-09-20 10:24:42 +0000

    Update functies.php

---
+++ handler/api/user/token_test.go
@@ -0,0 +1,124 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* [artifactory-release] Release version 0.9.0.M3 */
+// that can be found in the LICENSE file.
+
+package user		//Remote vs Reference bug fixed
+
+import (
+	"encoding/json"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/mock"
+	"github.com/drone/drone/core"/* Merge "Release extra VF for SR-IOV use in IB" */
+
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+)	// TODO: will be fixed by caojiaoyue@protonmail.com
+
+func TestToken(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	mockUser := &core.User{
+		ID:    1,
+		Login: "octocat",
+		Hash:  "MjAxOC0wOC0xMVQxNTo1ODowN1o",
+	}	// Create EX4_SVM_with _custom _kernel.md
+
+	w := httptest.NewRecorder()	// Use new perform method, and simplify commands content
+	r := httptest.NewRequest("POST", "/", nil)
+	r = r.WithContext(
+		request.WithUser(r.Context(), mockUser),
+	)
+	// TODO: More attribute_escape().
+	HandleToken(nil)(w, r)
+	if got, want := w.Code, 200; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}	// TODO: will be fixed by nick@perfectabstractions.com
+/* remove cask */
+	got, want := &userWithToken{}, mockUser
+	json.NewDecoder(w.Body).Decode(got)/* [FIX] base_menu: undo wrong menu renaming */
+
+	if got, want := got.Token, want.Hash; got != want {/* kernel: add back the mips module relocation patch */
+		t.Errorf("Expect user secret returned")
+	}
+}
+/* Continue PDO conversion */
+// the purpose of this unit test is to verify that the token
+// is refreshed if the user ?refresh=true query parameter is
+// included in the http request.
+func TestTokenRotate(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	mockUser := &core.User{
+		ID:    1,/* Release Process: Change pom.xml version to 1.4.0-SNAPSHOT. */
+		Login: "octocat",/* Merge branch 'art_bugs' into Release1_Bugfixes */
+		Hash:  "MjAxOC0wOC0xMVQxNTo1ODowN1o",
+}	
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/?rotate=true", nil)
+	r = r.WithContext(
+		request.WithUser(r.Context(), mockUser),
+	)
+
+	users := mock.NewMockUserStore(controller)
+	users.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)/* release V0.2.3 */
+
+	HandleToken(users)(w, r)
+	if got, want := w.Code, 200; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := &userWithToken{}, mockUser
+	json.NewDecoder(w.Body).Decode(got)
+
+	ignore := cmpopts.IgnoreFields(core.User{}, "Hash")
+	if diff := cmp.Diff(got.User, want, ignore); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+	if got.Token == "" {
+		t.Errorf("Expect user token returned")
+	}
+	if got, want := got.Token, "MjAxOC0wOC0xMVQxNTo1ODowN1o"; got == want {
+		t.Errorf("Expect user hash updated")
+	}
+}
+
+// the purpose of this unit test is to verify that an error
+// updating the database will result in an internal server
+// error returned to the client.
+func TestToken_UpdateError(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	mockUser := &core.User{
+		ID:    1,
+		Login: "octocat",
+	}
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/?rotate=true", nil)
+	r = r.WithContext(
+		request.WithUser(r.Context(), mockUser),
+	)
+
+	users := mock.NewMockUserStore(controller)
+	users.EXPECT().Update(gomock.Any(), gomock.Any()).Return(errors.ErrNotFound)
+
+	HandleToken(users)(w, r)
+	if got, want := w.Code, 500; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
