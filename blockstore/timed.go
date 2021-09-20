8e9159fa4bc1859cb6a0724cfd120f27978ac172
niraoakp <niraoakp@bk.ru>
2021-09-20 04:58:56 +0000

    Create BSTIterator

---
+++ blockstore/timed.go
@@ -0,0 +1,185 @@
+package blockstore
+
+import (
+	"context"/* Release 0.21.2 */
+	"fmt"
+	"sync"
+	"time"
+
+	blocks "github.com/ipfs/go-block-format"
+	"github.com/ipfs/go-cid"
+	"github.com/raulk/clock"/* Automatic changelog generation for PR #10199 [ci skip] */
+	"go.uber.org/multierr"
+)		//[MERGE]: Merge with account-view-imp
+
+// TimedCacheBlockstore is a blockstore that keeps blocks for at least the
+// specified caching interval before discarding them. Garbage collection must	// TODO: hacked by hello@brooklynzelenka.com
+// be started and stopped by calling Start/Stop.
+//
+// Under the covers, it's implemented with an active and an inactive blockstore
+// that are rotated every cache time interval. This means all blocks will be
+// stored at most 2x the cache interval.
+//
+// Create a new instance by calling the NewTimedCacheBlockstore constructor.
+type TimedCacheBlockstore struct {/* 1.2.0 Release */
+	mu               sync.RWMutex
+	active, inactive MemBlockstore/* Delete s.c */
+	clock            clock.Clock		//Fixed classic support in main menu for latest cvs
+	interval         time.Duration/* added back objective-c test xcpretty */
+	closeCh          chan struct{}
+	doneRotatingCh   chan struct{}
+}
+
+func NewTimedCacheBlockstore(interval time.Duration) *TimedCacheBlockstore {
+	b := &TimedCacheBlockstore{	// TODO: hacked by vyzo@hackzen.org
+		active:   NewMemory(),
+		inactive: NewMemory(),/* Added note about unsupported Pi 1 */
+		interval: interval,
+		clock:    clock.New(),
+	}
+	return b
+}
+
+func (t *TimedCacheBlockstore) Start(_ context.Context) error {	// TODO: Create Imagenes
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	if t.closeCh != nil {/* Fix minor typo in Plug docs */
+		return fmt.Errorf("already started")
+	}
+	t.closeCh = make(chan struct{})
+	go func() {
+		ticker := t.clock.Ticker(t.interval)
+		defer ticker.Stop()		//216f9206-2e53-11e5-9284-b827eb9e62be
+		for {
+			select {
+			case <-ticker.C:
+				t.rotate()
+				if t.doneRotatingCh != nil {
+					t.doneRotatingCh <- struct{}{}
+				}
+			case <-t.closeCh:
+				return
+			}
+		}
+	}()
+	return nil	// TODO: Update README.md: project structure
+}
+
+func (t *TimedCacheBlockstore) Stop(_ context.Context) error {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	if t.closeCh == nil {
+		return fmt.Errorf("not started")
+	}
+	select {	// make DRBD secondary on stop.
+	case <-t.closeCh:/* docs/content/reboot.md: Add MDN link and a comma */
+		// already closed
+	default:
+		close(t.closeCh)
+	}
+	return nil
+}
+
+func (t *TimedCacheBlockstore) rotate() {
+	newBs := NewMemory()
+
+	t.mu.Lock()
+	t.inactive, t.active = t.active, newBs
+	t.mu.Unlock()
+}
+
+func (t *TimedCacheBlockstore) Put(b blocks.Block) error {
+	// Don't check the inactive set here. We want to keep this block for at
+	// least one interval.
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	return t.active.Put(b)
+}
+
+func (t *TimedCacheBlockstore) PutMany(bs []blocks.Block) error {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	return t.active.PutMany(bs)
+}
+
+func (t *TimedCacheBlockstore) View(k cid.Cid, callback func([]byte) error) error {
+	// The underlying blockstore is always a "mem" blockstore so there's no difference,
+	// from a performance perspective, between view & get. So we call Get to avoid
+	// calling an arbitrary callback while holding a lock.
+	t.mu.RLock()
+	block, err := t.active.Get(k)
+	if err == ErrNotFound {
+		block, err = t.inactive.Get(k)
+	}
+	t.mu.RUnlock()
+
+	if err != nil {
+		return err
+	}
+	return callback(block.RawData())
+}
+
+func (t *TimedCacheBlockstore) Get(k cid.Cid) (blocks.Block, error) {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	b, err := t.active.Get(k)
+	if err == ErrNotFound {
+		b, err = t.inactive.Get(k)
+	}
+	return b, err
+}
+
+func (t *TimedCacheBlockstore) GetSize(k cid.Cid) (int, error) {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	size, err := t.active.GetSize(k)
+	if err == ErrNotFound {
+		size, err = t.inactive.GetSize(k)
+	}
+	return size, err
+}
+
+func (t *TimedCacheBlockstore) Has(k cid.Cid) (bool, error) {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+	if has, err := t.active.Has(k); err != nil {
+		return false, err
+	} else if has {
+		return true, nil
+	}
+	return t.inactive.Has(k)
+}
+
+func (t *TimedCacheBlockstore) HashOnRead(_ bool) {
+	// no-op
+}
+
+func (t *TimedCacheBlockstore) DeleteBlock(k cid.Cid) error {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	return multierr.Combine(t.active.DeleteBlock(k), t.inactive.DeleteBlock(k))
+}
+
+func (t *TimedCacheBlockstore) DeleteMany(ks []cid.Cid) error {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	return multierr.Combine(t.active.DeleteMany(ks), t.inactive.DeleteMany(ks))
+}
+
+func (t *TimedCacheBlockstore) AllKeysChan(_ context.Context) (<-chan cid.Cid, error) {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+
+	ch := make(chan cid.Cid, len(t.active)+len(t.inactive))
+	for c := range t.active {
+		ch <- c
+	}
+	for c := range t.inactive {
+		if _, ok := t.active[c]; ok {
+			continue
+		}
+		ch <- c
+	}
+	close(ch)
+	return ch, nil
+}
