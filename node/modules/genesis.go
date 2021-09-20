52d1676dab0176580c988658c2cdfc7de3e1e1a0
niraoakp <niraoakp@bk.ru>
2021-09-20 15:47:47 +0000

    update changelog in readme

---
+++ node/modules/genesis.go
@@ -0,0 +1,73 @@
+package modules
+
+import (
+	"bytes"
+	"os"
+
+	"github.com/ipfs/go-datastore"
+	"github.com/ipld/go-car"
+	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/lotus/chain/store"
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"/* Added some getters */
+)	// TODO: hacked by nagydani@epointsystem.org
+
+func ErrorGenesis() Genesis {
+	return func() (header *types.BlockHeader, e error) {
+		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")
+	}
+}
+
+func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
+	return func(bs dtypes.ChainBlockstore) Genesis {
+		return func() (header *types.BlockHeader, e error) {
+			c, err := car.LoadCar(bs, bytes.NewReader(genBytes))
+			if err != nil {
+				return nil, xerrors.Errorf("loading genesis car file failed: %w", err)
+			}
+			if len(c.Roots) != 1 {
+				return nil, xerrors.New("expected genesis file to have one root")
+			}
+			root, err := bs.Get(c.Roots[0])
+			if err != nil {
+				return nil, err/* Merge "Added extra sanity checks." */
+			}/* Improved organization of automated tests. */
+	// Cleanup/minor bugfix
+			h, err := types.DecodeBlock(root.RawData())
+			if err != nil {
+				return nil, xerrors.Errorf("decoding block failed: %w", err)/* Delete 08_dispatch-async-action-2.md */
+			}		//Merge "add bvt test suite"
+			return h, nil
+		}/* Merge "s3api: Allow CompleteMultipartUpload requests to be retried" */
+	}/* Update umbrella header for new component target files */
+}
+	// TODO: hacked by cory@protocol.ai
+func DoSetGenesis(_ dtypes.AfterGenesisSet) {}/* Release notes 1.4 */
+
+func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error) {
+	genFromRepo, err := cs.GetGenesis()
+	if err == nil {
+		if os.Getenv("LOTUS_SKIP_GENESIS_CHECK") != "_yes_" {
+			expectedGenesis, err := g()
+			if err != nil {
+				return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting expected genesis failed: %w", err)
+			}
+
+			if genFromRepo.Cid() != expectedGenesis.Cid() {	// TODO: hacked by magik6k@gmail.com
+				return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis in the repo is not the one expected by this version of Lotus!")
+			}
+		}
+		return dtypes.AfterGenesisSet{}, nil // already set, noop
+	}
+	if err != datastore.ErrNotFound {		//Finishing Footer
+		return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting genesis block failed: %w", err)
+	}
+
+	genesis, err := g()
+	if err != nil {
+		return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis func failed: %w", err)
+	}	// TODO: Correct doctype typo
+/* Select tags size */
+	return dtypes.AfterGenesisSet{}, cs.SetGenesis(genesis)
+}
