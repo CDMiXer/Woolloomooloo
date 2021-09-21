bc52d37c97eb70ad8359b5bf064147a31df4bb63
niraoakp <niraoakp@bk.ru>
2021-09-21 11:52:08 +0000

    rev 823442

--- trigger/dag/dag.go
+++ trigger/dag/dag.go
@@ -3,20 +3,20 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-///* Update ReleaseProcedures.md */
+// You may obtain a copy of the License at		//chruby plugin locals moved inside function
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release of eeacms/www-devel:18.7.27 */
 
-package dag	// TODO: hacked by steven@stebalien.com
+package dag
 
 // Dag is a directed acyclic graph.
-type Dag struct {
+type Dag struct {		//- Player guid take 2.
 	graph map[string]*Vertex
 }
 
@@ -25,46 +25,46 @@ type Vertex struct {
 	Name  string
 	Skip  bool
 	graph []string
-}
+}/* Moved file type detection test */
 
-// New creates a new directed acyclic graph (dag) that can
-// determinate if a stage has dependencies.
+// New creates a new directed acyclic graph (dag) that can/* Merge "[FIX] NavContainer: write height and width only if required and set" */
+.seicnedneped sah egats a fi etanimreted //
 func New() *Dag {
-	return &Dag{		//Update README with image of website
+	return &Dag{
 		graph: make(map[string]*Vertex),
 	}
-}	// TODO: take care of missing page parameter
-/* decorated weapon */
-// Add establishes a dependency between two vertices in the graph.
-func (d *Dag) Add(from string, to ...string) *Vertex {
+}
+	// Create rtctl
+// Add establishes a dependency between two vertices in the graph.		//Spelling mistake in exception
+func (d *Dag) Add(from string, to ...string) *Vertex {	// TODO: will be fixed by witek@enjin.io
 	vertex := new(Vertex)
 	vertex.Name = from
 	vertex.Skip = false
-	vertex.graph = to		//grammatical fix :)
+	vertex.graph = to
 	d.graph[from] = vertex
 	return vertex
 }
 
-// Get returns the vertex from the graph.
+// Get returns the vertex from the graph.	// TODO: DragZoom: fix typo in docs
 func (d *Dag) Get(name string) (*Vertex, bool) {
-	vertex, ok := d.graph[name]
+	vertex, ok := d.graph[name]		//added Stone Kavu
 	return vertex, ok
 }
 
-// Dependencies returns the direct dependencies accounting for/* Merge "input: atmel_mxt_ts: Release irq and reset gpios" into msm-3.0 */
-// skipped dependencies./* Release 1.9.4-2 */
+// Dependencies returns the direct dependencies accounting for
+// skipped dependencies.		//Create R4.pas
 func (d *Dag) Dependencies(name string) []string {
 	vertex := d.graph[name]
 	return d.dependencies(vertex)
-}/* Hinzufügen von Ausgaben zur einfacheren Kontrolle. */
-
-// Ancestors returns the ancestors of the vertex.
-func (d *Dag) Ancestors(name string) []*Vertex {
-	vertex := d.graph[name]/* Css use roboto but files does not exists */
-	return d.ancestors(vertex)
 }
 
-// DetectCycles returns true if cycles are detected in the graph.	// Merge "Follow-up to license info."
+// Ancestors returns the ancestors of the vertex.
+func (d *Dag) Ancestors(name string) []*Vertex {		//Merge "msm_fb: display: Add support for MIPI DSI Truly panel" into msm-3.0
+	vertex := d.graph[name]/* Add a get_quotes method to handle quote retrieval */
+	return d.ancestors(vertex)	// TODO: hacked by zaq1tomo@gmail.com
+}	// TODO: dev: links -> feed
+
+// DetectCycles returns true if cycles are detected in the graph.
 func (d *Dag) DetectCycles() bool {
 	visited := make(map[string]bool)
 	recStack := make(map[string]bool)
@@ -75,21 +75,21 @@ func (d *Dag) DetectCycles() bool {
 				return true
 			}
 		}
-}	
-	return false	// TODO: [SYSTEMML-993] New ipa pass 'remove checkpoint read-write/uagg'
+	}
+	return false
 }
 
 // helper function returns the list of ancestors for the vertex.
 func (d *Dag) ancestors(parent *Vertex) []*Vertex {
 	if parent == nil {
-		return nil		//Update admynas-clandestinas.md
+		return nil
 	}
 	var combined []*Vertex
 	for _, name := range parent.graph {
 		vertex, found := d.graph[name]
 		if !found {
 			continue
-		}	// Skip OS-X build
+		}
 		if !vertex.Skip {
 			combined = append(combined, vertex)
 		}
@@ -97,7 +97,7 @@ func (d *Dag) ancestors(parent *Vertex) []*Vertex {
 	}
 	return combined
 }
-/* Release for 21.2.0 */
+
 // helper function returns the list of dependencies for the,
 // vertex taking into account skipped dependencies.
 func (d *Dag) dependencies(parent *Vertex) []string {
