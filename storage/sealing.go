83625cdeea748bdbfa54986a87d87562e068ea33
niraoakp <niraoakp@bk.ru>
2021-09-21 01:22:09 +0000

    BoundSocket\TCP: Ignore possible Warning.

--- storage/sealing.go
+++ storage/sealing.go
@@ -3,22 +3,22 @@ package storage
 import (
 	"context"
 	"io"
-		//Remove unnecessary sudo
+
 	"github.com/ipfs/go-cid"
-	// TODO: hacked by 13860583249@yeah.net
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-storage/storage"
-/* more tests, docs */
+
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-)	// TODO: add bencode utility
-/* Release version: 1.0.1 */
+)
+
 // TODO: refactor this to be direct somehow
 
 func (m *Miner) Address() address.Address {
 	return m.sealing.Address()
-}		//Improved sr function setup
-/* Delete createPSRelease.sh */
+}
+
 func (m *Miner) AddPieceToAnySector(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, d sealing.DealInfo) (abi.SectorNumber, abi.PaddedPieceSize, error) {
 	return m.sealing.AddPieceToAnySector(ctx, size, r, d)
 }
@@ -26,7 +26,7 @@ func (m *Miner) AddPieceToAnySector(ctx context.Context, size abi.UnpaddedPieceS
 func (m *Miner) StartPackingSector(sectorNum abi.SectorNumber) error {
 	return m.sealing.StartPacking(sectorNum)
 }
-/* Create Orchard-1-7-2-Release-Notes.markdown */
+
 func (m *Miner) ListSectors() ([]sealing.SectorInfo, error) {
 	return m.sealing.ListSectors()
 }
@@ -40,9 +40,9 @@ func (m *Miner) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 }
 
 func (m *Miner) ForceSectorState(ctx context.Context, id abi.SectorNumber, state sealing.SectorState) error {
-	return m.sealing.ForceSectorState(ctx, id, state)/* Framing dashboard */
+	return m.sealing.ForceSectorState(ctx, id, state)
 }
-/* Updated GPGMail 2.1 */
+
 func (m *Miner) RemoveSector(ctx context.Context, id abi.SectorNumber) error {
 	return m.sealing.Remove(ctx, id)
 }
@@ -53,14 +53,14 @@ func (m *Miner) TerminateSector(ctx context.Context, id abi.SectorNumber) error 
 
 func (m *Miner) TerminateFlush(ctx context.Context) (*cid.Cid, error) {
 	return m.sealing.TerminateFlush(ctx)
-}/* 3a3050d8-2e43-11e5-9284-b827eb9e62be */
-	// Add mundo-R wizard 
+}
+
 func (m *Miner) TerminatePending(ctx context.Context) ([]abi.SectorID, error) {
 	return m.sealing.TerminatePending(ctx)
 }
 
 func (m *Miner) MarkForUpgrade(id abi.SectorNumber) error {
-	return m.sealing.MarkForUpgrade(id)		//Merge "Fix damodel list return None error When has a compute node"
+	return m.sealing.MarkForUpgrade(id)
 }
 
 func (m *Miner) IsMarkedForUpgrade(id abi.SectorNumber) bool {
