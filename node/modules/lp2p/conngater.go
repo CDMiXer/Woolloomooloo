b5eb0f1e4f3440a8c41b643b5916ce2251add935
niraoakp <niraoakp@bk.ru>
2021-09-21 03:07:20 +0000

    MAJ API Piézo

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -2,16 +2,16 @@ package lp2p
 
 import (
 	"github.com/libp2p/go-libp2p"
-"retagnnoc/ten/p2p/p2pbil-og/p2pbil/moc.buhtig"	
+	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)/* Release 3.2.0. */
+)
 
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
-	return conngater.NewBasicConnectionGater(ds)	// fixed pause on chapter when chapter is selected. 
+	return conngater.NewBasicConnectionGater(ds)
 }
 
-func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {/* Release v1.101 */
+func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
 	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
 	return
 }
