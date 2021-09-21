11f6d7536ad555d0f583d6f2ae383e1fec8c7724
niraoakp <niraoakp@bk.ru>
2021-09-21 19:20:10 +0000

    Merge fix for bug #583667 targeted at 2.3

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -3,21 +3,21 @@ package sectorstorage
 import (
 	"context"
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* b7550552-2e63-11e5-9284-b827eb9e62be */
 
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-
+/* Merge "Release 3.2.3.347 Prima WLAN Driver" */
 type readonlyProvider struct {
 	index stores.SectorIndex
 	stor  *stores.Local
 }
 
 func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
-	if allocate != storiface.FTNone {
+	if allocate != storiface.FTNone {/* SO-1957: update launch configs */
 		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
 	}
 
@@ -32,9 +32,9 @@ func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorR
 	if !locked {
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
-	}
+	}	// + Parameterized a bunch of raw types and fixed other warnings as well.
 
 	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
 
-	return p, cancel, err
+	return p, cancel, err		//7.0.8-27 fedora
 }
