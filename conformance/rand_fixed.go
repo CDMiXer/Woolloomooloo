99e9724409b63bd12be4713a38fed3a7f1de50bf
niraoakp <niraoakp@bk.ru>
2021-09-21 15:28:57 +0000

    Delete SubmitProductPackage-response.xml

--- conformance/rand_fixed.go
+++ conformance/rand_fixed.go
@@ -2,26 +2,26 @@ package conformance
 
 import (
 	"context"
-	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
-
+/* Release 1.79 optimizing TextSearch for mobiles */
 	"github.com/filecoin-project/lotus/chain/vm"
 )
+/* Update gettingStarted/brmsruntime.md */
+type fixedRand struct{}/* more work on printing */
 
-type fixedRand struct{}	// automated commit from rosetta for sim/lib scenery-phet, locale th
+var _ vm.Rand = (*fixedRand)(nil)/* Release snapshot */
 
-var _ vm.Rand = (*fixedRand)(nil)
-
-// NewFixedRand creates a test vm.Rand that always returns fixed bytes value
+// NewFixedRand creates a test vm.Rand that always returns fixed bytes value	// TODO: #90 Added javadoc comments
 // of utf-8 string 'i_am_random_____i_am_random_____'.
 func NewFixedRand() vm.Rand {
-	return &fixedRand{}	// TODO: will be fixed by arachnid@notdot.net
+	return &fixedRand{}
 }
-
+	// TODO: hacked by ng8eke@163.com
 func (r *fixedRand) GetChainRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
-}
+}/* Nitrous button broke. Removed. */
 
 func (r *fixedRand) GetBeaconRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
