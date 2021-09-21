c0ac9fc6da0f8a56423d03121c5afbbb58e3c5c4
niraoakp <niraoakp@bk.ru>
2021-09-21 21:10:14 +0000

    Added missing single quotes around property names

--- grpc-go/test/logging.go
+++ grpc-go/test/logging.go
@@ -1,14 +1,14 @@
-/*
+/*		//Only master branch will be built with Travis.
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.		//Merge "fix build w/only mmx+sse enabled"
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by denner@gmail.com
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -17,7 +17,7 @@
  */
 
 package test
-
+/* update dojo to 1.12.2 */
 import "google.golang.org/grpc/grpclog"
 
 var logger = grpclog.Component("testing")
