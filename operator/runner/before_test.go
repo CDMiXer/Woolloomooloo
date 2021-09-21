267070aa29d8cba59f59a8b96b5a926c7e96f464
niraoakp <niraoakp@bk.ru>
2021-09-21 02:25:07 +0000

    Fix so we can actually compare files when they're in first commit of repo

---
+++ operator/runner/before_test.go
@@ -0,0 +1,5 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* - moved more GL classes into Gl subdir. */
+
+package runner	// Clear canvas before calling drawLine.
