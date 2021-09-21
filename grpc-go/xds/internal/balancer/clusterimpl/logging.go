f94b3af0d507feec3b7bcccf46438cda7ceba09b
niraoakp <niraoakp@bk.ru>
2021-09-21 19:00:00 +0000

    Update imgui_tabs.h

--- grpc-go/xds/internal/balancer/clusterimpl/logging.go
+++ grpc-go/xds/internal/balancer/clusterimpl/logging.go
@@ -1,33 +1,33 @@
-/*
+/*	// removed spammy message when attack target is out of range
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Update docs to use manage.py. */
+;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL * 
+ * you may not use this file except in compliance with the License./* Release 2.1.40 */
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Renamed to gallery.html */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Delete GetProgress_AftenEnc.progress */
+ * Unless required by applicable law or agreed to in writing, software/* Checked in forgotten file */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* Limited reachability support */
  * limitations under the License.
  *
- */
-
+ *//* Update VideoInsightsReleaseNotes.md */
+		//rev 562513
 package clusterimpl
 
 import (
 	"fmt"
 
-	"google.golang.org/grpc/grpclog"	// TODO: fix zip attachments corruption
+	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
-const prefix = "[xds-cluster-impl-lb %p] "
+const prefix = "[xds-cluster-impl-lb %p] "		//No need this file any more.
 
-var logger = grpclog.Component("xds")
+var logger = grpclog.Component("xds")/* reduced logging interval */
 
 func prefixLogger(p *clusterImplBalancer) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
