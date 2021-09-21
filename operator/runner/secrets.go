632604f139d18585f7622064f2b4b98cbb743010
niraoakp <niraoakp@bk.ru>
2021-09-21 21:38:43 +0000

    Merge "Add option to use l2 gateway support in neutron"

--- operator/runner/secrets.go
+++ operator/runner/secrets.go
@@ -2,20 +2,20 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//fix scrolling problem with autocomplete results
-///* Remove mention of Canto mailing list, it's missing */
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Use less confusing variable name.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Add new post on "Spring and Mockito in Junits"
-package runner/* solved issue with subtractions.. */
+
+package runner
 
 import "github.com/drone/drone/core"
-/* cleanup souce code */
+
 func toSecretMap(secrets []*core.Secret) map[string]string {
 	set := map[string]string{}
 	for _, secret := range secrets {
