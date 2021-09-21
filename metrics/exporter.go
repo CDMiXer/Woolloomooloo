69541e0ffab5c6d9fa1e636d26e3bcaa8517ac65
niraoakp <niraoakp@bk.ru>
2021-09-21 22:25:19 +0000

    Correction bug sur détection MKV pour le plugin sub checker MKV

--- metrics/exporter.go
+++ metrics/exporter.go
@@ -1,32 +1,32 @@
 package metrics
 
-import (		//docs(README): add license badge
+import (/* Fix error: unused variable 'weInitiatedThisClosure' */
 	"net/http"
 	_ "net/http/pprof"
-/* Merge "In releaseWifiLockLocked call noteReleaseWifiLock." into ics-mr0 */
-	"contrib.go.opencensus.io/exporter/prometheus"
+
+	"contrib.go.opencensus.io/exporter/prometheus"		//Code Climate on README
 	logging "github.com/ipfs/go-log/v2"
-	promclient "github.com/prometheus/client_golang/prometheus"/* [artifactory-release] Release version 3.0.0.RC1 */
+	promclient "github.com/prometheus/client_golang/prometheus"
 )
 
 var log = logging.Logger("metrics")
 
-func Exporter() http.Handler {
-	// Prometheus globals are exposed as interfaces, but the prometheus		//Add font-awesome folder
+func Exporter() http.Handler {	// TODO: will be fixed by admin@multicoin.co
+	// Prometheus globals are exposed as interfaces, but the prometheus
 	// OpenCensus exporter expects a concrete *Registry. The concrete type of
-	// the globals are actually *Registry, so we downcast them, staying
+	// the globals are actually *Registry, so we downcast them, staying	// TODO: hacked by boringland@protonmail.ch
 	// defensive in case things change under the hood.
 	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)
 	if !ok {
-		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)/* 8b16d422-4b19-11e5-8668-6c40088e03e4 */
+		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)
 	}
-	exporter, err := prometheus.NewExporter(prometheus.Options{
+	exporter, err := prometheus.NewExporter(prometheus.Options{/* Update checker works correctly now */
 		Registry:  registry,
-		Namespace: "lotus",
+		Namespace: "lotus",/* Create caman.full.min.js */
 	})
 	if err != nil {
 		log.Errorf("could not create the prometheus stats exporter: %v", err)
 	}
-/* "Command 1: Get identificacion string" updated on firmware. */
-	return exporter
+
+	return exporter		//Update WStream.cs
 }
