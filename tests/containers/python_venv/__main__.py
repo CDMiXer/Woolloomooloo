6911fafba0a122a280246050c1046e4bd911440f
niraoakp <niraoakp@bk.ru>
2021-09-21 23:56:02 +0000

    job submission changes for example system

--- tests/containers/python_venv/__main__.py
+++ tests/containers/python_venv/__main__.py
@@ -1,4 +1,4 @@
-import pulumi
-
+import pulumi	// TODO: hacked by xiemengjun@gmail.com
+		//-- correcting typo in the yml
 config = pulumi.Config()
 print("Hello from %s" % (config.require("runtime")))
