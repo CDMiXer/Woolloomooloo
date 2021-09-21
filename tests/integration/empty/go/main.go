09b90fd8d7beb1039f0e42c0e406cf28cab2b88c
niraoakp <niraoakp@bk.ru>
2021-09-21 05:26:41 +0000

    Added a Terminal/Unix resource

---
+++ tests/integration/empty/go/main.go
@@ -0,0 +1,13 @@
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+/* New HPS URL's */
+package main
+
+import (
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+)
+
+func main() {/* Fixed Release config */
+	pulumi.Run(func(ctx *pulumi.Context) error {		//rev 505542
+		return nil
+	})
+}
