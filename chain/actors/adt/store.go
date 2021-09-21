b6794a99d2641cb010dbe694d7e3615c85306928
niraoakp <niraoakp@bk.ru>
2021-09-21 03:55:29 +0000

    Better setting on non-unity axis ratios for 2D fields

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -1,17 +1,17 @@
-package adt
-	// TODO: will be fixed by qugou1350636@126.com
+package adt	// TODO: will be fixed by why@ipfs.io
+
 import (
 	"context"
-/* Merge "Remove ssh tests diabling as #1074039 is fixed" */
+
 	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
-)	// TODO: hacked by fjl@ethereum.org
-/* Release version 1.8.0 */
-type Store interface {
-	Context() context.Context
-	cbor.IpldStore		//Update pythoncrypt.py
-}
+)
 
-func WrapStore(ctx context.Context, store cbor.IpldStore) Store {
-	return adt.WrapStore(ctx, store)/* Release version 1.0.0. */
+type Store interface {
+	Context() context.Context		//multiple TP bug
+	cbor.IpldStore
+}	// Update ARMLA.R
+	// TODO: will be fixed by steven@stebalien.com
+func WrapStore(ctx context.Context, store cbor.IpldStore) Store {	// routes: add sections
+	return adt.WrapStore(ctx, store)
 }
