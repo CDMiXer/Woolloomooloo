f6185dbeb5168b39ac10a5c0eac3351af9420471
niraoakp <niraoakp@bk.ru>
2021-09-21 22:09:20 +0000

    Merge branch 'master' into expandable-grammar

--- node/repo/memrepo_test.go
+++ node/repo/memrepo_test.go
@@ -1,10 +1,10 @@
-package repo
-
+package repo	// TODO: will be fixed by mail@bitpshr.net
+/*  	changed default setting for wrapText from true to false */
 import (
 	"testing"
 )
 
 func TestMemBasic(t *testing.T) {
 	repo := NewMemory(nil)
-	basicTest(t, repo)
+	basicTest(t, repo)/* Release for METROPOLIS 1_65_1126 */
 }
