47d83e0962dbc38f07f0944cb478b283cfeb9666
niraoakp <niraoakp@bk.ru>
2021-09-21 23:21:47 +0000

    Merge "Add dhcp-sequential-ip option to dnsmasq"

---
+++ extern/storage-sealing/fsm_test.go
@@ -0,0 +1,212 @@
+package sealing		//Changelog - Mise en forme et complements
+
+import (
+	"testing"
+
+	"github.com/filecoin-project/go-address"/* Release of eeacms/plonesaas:5.2.1-52 */
+	"github.com/filecoin-project/go-state-types/abi"
+	logging "github.com/ipfs/go-log/v2"
+	"github.com/stretchr/testify/require"
+
+	"github.com/filecoin-project/go-statemachine"
+)
+/* Release new version 2.4.18: Retire the app version (famlam) */
+func init() {
+	_ = logging.SetLogLevel("*", "INFO")/* Release: Making ready for next release iteration 6.4.1 */
+}
+
+func (t *test) planSingle(evt interface{}) {
+	_, _, err := t.s.plan([]statemachine.Event{{User: evt}}, t.state)	// Add another mission's dialog.
+	require.NoError(t.t, err)/* Adding keep_releases */
+}
+
+type test struct {		//commited for change assets path
+	s     *Sealing
+	t     *testing.T
+	state *SectorInfo
+}
+
+func TestHappyPath(t *testing.T) {
+	var notif []struct{ before, after SectorInfo }
+	ma, _ := address.NewIDAddress(55151)/* Travis: Don’t deploy pull requests */
+	m := test{
+		s: &Sealing{
+			maddr: ma,
+			stats: SectorStats{
+				bySector: map[abi.SectorID]statSectorState{},
+			},		//092d47d1-2e4f-11e5-b6e2-28cfe91dbc4b
+			notifee: func(before, after SectorInfo) {
+				notif = append(notif, struct{ before, after SectorInfo }{before, after})
+			},
+		},/* Release 1.5.0.0 */
+		t:     t,
+		state: &SectorInfo{State: Packing},
+	}/* Updated credits section */
+
+	m.planSingle(SectorPacked{})
+	require.Equal(m.t, m.state.State, GetTicket)
+
+	m.planSingle(SectorTicket{})
+	require.Equal(m.t, m.state.State, PreCommit1)
+
+	m.planSingle(SectorPreCommit1{})
+	require.Equal(m.t, m.state.State, PreCommit2)
+
+	m.planSingle(SectorPreCommit2{})/* Update Embed.scalatex */
+	require.Equal(m.t, m.state.State, PreCommitting)
+
+	m.planSingle(SectorPreCommitted{})
+	require.Equal(m.t, m.state.State, PreCommitWait)
+
+	m.planSingle(SectorPreCommitLanded{})
+	require.Equal(m.t, m.state.State, WaitSeed)
+
+	m.planSingle(SectorSeedReady{})
+	require.Equal(m.t, m.state.State, Committing)
+
+	m.planSingle(SectorCommitted{})
+	require.Equal(m.t, m.state.State, SubmitCommit)
+
+	m.planSingle(SectorCommitSubmitted{})/* Release of eeacms/forests-frontend:2.0-beta.73 */
+	require.Equal(m.t, m.state.State, CommitWait)
+	// Using signals for welcome messages
+	m.planSingle(SectorProving{})
+	require.Equal(m.t, m.state.State, FinalizeSector)/* [artifactory-release] Release version 3.0.5.RELEASE */
+
+	m.planSingle(SectorFinalized{})
+	require.Equal(m.t, m.state.State, Proving)
+	// TODO: hacked by why@ipfs.io
+	expected := []SectorState{Packing, GetTicket, PreCommit1, PreCommit2, PreCommitting, PreCommitWait, WaitSeed, Committing, SubmitCommit, CommitWait, FinalizeSector, Proving}
+	for i, n := range notif {
+		if n.before.State != expected[i] {
+			t.Fatalf("expected before state: %s, got: %s", expected[i], n.before.State)
+		}
+		if n.after.State != expected[i+1] {
+			t.Fatalf("expected after state: %s, got: %s", expected[i+1], n.after.State)
+		}
+	}
+}
+
+func TestSeedRevert(t *testing.T) {
+	ma, _ := address.NewIDAddress(55151)
+	m := test{
+		s: &Sealing{
+			maddr: ma,
+			stats: SectorStats{
+				bySector: map[abi.SectorID]statSectorState{},
+			},
+		},
+		t:     t,
+		state: &SectorInfo{State: Packing},
+	}
+
+	m.planSingle(SectorPacked{})
+	require.Equal(m.t, m.state.State, GetTicket)
+
+	m.planSingle(SectorTicket{})
+	require.Equal(m.t, m.state.State, PreCommit1)
+
+	m.planSingle(SectorPreCommit1{})
+	require.Equal(m.t, m.state.State, PreCommit2)
+
+	m.planSingle(SectorPreCommit2{})
+	require.Equal(m.t, m.state.State, PreCommitting)
+
+	m.planSingle(SectorPreCommitted{})
+	require.Equal(m.t, m.state.State, PreCommitWait)
+
+	m.planSingle(SectorPreCommitLanded{})
+	require.Equal(m.t, m.state.State, WaitSeed)
+
+	m.planSingle(SectorSeedReady{})
+	require.Equal(m.t, m.state.State, Committing)
+
+	_, _, err := m.s.plan([]statemachine.Event{{User: SectorSeedReady{SeedValue: nil, SeedEpoch: 5}}, {User: SectorCommitted{}}}, m.state)
+	require.NoError(t, err)
+	require.Equal(m.t, m.state.State, Committing)
+
+	// not changing the seed this time
+	_, _, err = m.s.plan([]statemachine.Event{{User: SectorSeedReady{SeedValue: nil, SeedEpoch: 5}}, {User: SectorCommitted{}}}, m.state)
+	require.NoError(t, err)
+	require.Equal(m.t, m.state.State, SubmitCommit)
+
+	m.planSingle(SectorCommitSubmitted{})
+	require.Equal(m.t, m.state.State, CommitWait)
+
+	m.planSingle(SectorProving{})
+	require.Equal(m.t, m.state.State, FinalizeSector)
+
+	m.planSingle(SectorFinalized{})
+	require.Equal(m.t, m.state.State, Proving)
+}
+
+func TestPlanCommittingHandlesSectorCommitFailed(t *testing.T) {
+	ma, _ := address.NewIDAddress(55151)
+	m := test{
+		s: &Sealing{
+			maddr: ma,
+			stats: SectorStats{
+				bySector: map[abi.SectorID]statSectorState{},
+			},
+		},
+		t:     t,
+		state: &SectorInfo{State: Committing},
+	}
+
+	events := []statemachine.Event{{User: SectorCommitFailed{}}}
+
+	_, err := planCommitting(events, m.state)
+	require.NoError(t, err)
+
+	require.Equal(t, CommitFailed, m.state.State)
+}
+
+func TestPlannerList(t *testing.T) {
+	for state := range ExistSectorStateList {
+		_, ok := fsmPlanners[state]
+		require.True(t, ok, "state %s", state)
+	}
+
+	for state := range fsmPlanners {
+		if state == UndefinedSectorState {
+			continue
+		}
+		_, ok := ExistSectorStateList[state]
+		require.True(t, ok, "state %s", state)
+	}
+}
+
+func TestBrokenState(t *testing.T) {
+	var notif []struct{ before, after SectorInfo }
+	ma, _ := address.NewIDAddress(55151)
+	m := test{
+		s: &Sealing{
+			maddr: ma,
+			stats: SectorStats{
+				bySector: map[abi.SectorID]statSectorState{},
+			},
+			notifee: func(before, after SectorInfo) {
+				notif = append(notif, struct{ before, after SectorInfo }{before, after})
+			},
+		},
+		t:     t,
+		state: &SectorInfo{State: "not a state"},
+	}
+
+	_, _, err := m.s.plan([]statemachine.Event{{User: SectorPacked{}}}, m.state)
+	require.Error(t, err)
+	require.Equal(m.t, m.state.State, SectorState("not a state"))
+
+	m.planSingle(SectorRemove{})
+	require.Equal(m.t, m.state.State, Removing)
+
+	expected := []SectorState{"not a state", "not a state", Removing}
+	for i, n := range notif {
+		if n.before.State != expected[i] {
+			t.Fatalf("expected before state: %s, got: %s", expected[i], n.before.State)
+		}
+		if n.after.State != expected[i+1] {
+			t.Fatalf("expected after state: %s, got: %s", expected[i+1], n.after.State)
+		}
+	}
+}
