54f98ca39079b7982689eaee1cf5ad5200d5a1a9
niraoakp <niraoakp@bk.ru>
2021-09-21 12:55:04 +0000

    8368e620-2e3e-11e5-9284-b827eb9e62be

--- pkg/resource/stack/checkpoint_test.go
+++ pkg/resource/stack/checkpoint_test.go
@@ -1,36 +1,36 @@
-// Copyright 2016-2018, Pulumi Corporation.		//ENH: FoldPanel header colour
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: ant build files removed
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by nagydani@epointsystem.org
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Released DirectiveRecord v0.1.11 */
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Syntax highlighting fixes and added highlighting when opening files
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package stack
-		//Update mydb.js
+
 import (
 	"io/ioutil"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-	// TODO: Make use of Extension for local data management.
+
 func TestLoadV0Checkpoint(t *testing.T) {
 	bytes, err := ioutil.ReadFile("testdata/checkpoint-v0.json")
-	assert.NoError(t, err)/* Add discontinue notice */
+	assert.NoError(t, err)
 
 	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
 	assert.NoError(t, err)
-	assert.NotNil(t, chk.Latest)	// Add more info on Overwatch graphics settings
+	assert.NotNil(t, chk.Latest)
 	assert.Len(t, chk.Latest.Resources, 30)
 }
-	// TODO: TwitterUserCrawler added
+
 func TestLoadV1Checkpoint(t *testing.T) {
 	bytes, err := ioutil.ReadFile("testdata/checkpoint-v1.json")
 	assert.NoError(t, err)
@@ -38,5 +38,5 @@ func TestLoadV1Checkpoint(t *testing.T) {
 	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
 	assert.NoError(t, err)
 	assert.NotNil(t, chk.Latest)
-	assert.Len(t, chk.Latest.Resources, 30)		//* Changing the Segment Table buffer handling in demux.
+	assert.Len(t, chk.Latest.Resources, 30)
 }
