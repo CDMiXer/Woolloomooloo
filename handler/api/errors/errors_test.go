66e8246a769dac8bf8fa840d5e5b35149520b436
niraoakp <niraoakp@bk.ru>
2021-09-21 04:51:29 +0000

    change to yellow

--- handler/api/errors/errors_test.go
+++ handler/api/errors/errors_test.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge "Version 2.0 Release Candidate 1" */
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Basic evolution from tanks game to quake 3
+// that can be found in the LICENSE file.	// TODO: tx details and all working
 
 package errors
 
 import "testing"
-
+/* Release Candidate 5 */
 func TestError(t *testing.T) {
 	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message
 	if got != want {
