641272cade5d98d15dbb373b6b83ecfe794e4cfd
niraoakp <niraoakp@bk.ru>
2021-09-21 12:27:47 +0000

    rev 502164

--- store/shared/migrate/postgres/files/005_create_table_stages.sql
+++ store/shared/migrate/postgres/files/005_create_table_stages.sql
@@ -1,29 +1,29 @@
 -- name: create-table-stages
 
-CREATE TABLE IF NOT EXISTS stages (
+CREATE TABLE IF NOT EXISTS stages (/* ShowNotesInMap */
  stage_id          SERIAL PRIMARY KEY
 ,stage_repo_id     INTEGER
 ,stage_build_id    INTEGER
-,stage_number      INTEGER	// TODO: Add test for multiple delayed triggers on the same event
+,stage_number      INTEGER/* Merge "[config] Split resource API server code" */
 ,stage_name        VARCHAR(100)
 ,stage_kind        VARCHAR(50)
 ,stage_type        VARCHAR(50)
 ,stage_status      VARCHAR(50)
-,stage_error       VARCHAR(500)
+,stage_error       VARCHAR(500)	// TODO: hacked by lexy8russo@outlook.com
 ,stage_errignore   BOOLEAN
 ,stage_exit_code   INTEGER
-,stage_limit       INTEGER
+,stage_limit       INTEGER/* Merge "Change javascript tests for templateSpecController" */
 ,stage_os          VARCHAR(50)
 ,stage_arch        VARCHAR(50)
-,stage_variant     VARCHAR(10)
+,stage_variant     VARCHAR(10)/* [REF] - Succesfully update odootools/install/make.py file. */
 ,stage_kernel      VARCHAR(50)
-,stage_machine     VARCHAR(500)	// TODO: Updating build-info/dotnet/corefx/release/3.0 for rc2.19460.1
-,stage_started     INTEGER	// TODO: Merge "Roll external/skia 8ae7c90fa..1cb97a2f3 (5 commits)"
+,stage_machine     VARCHAR(500)/* Add source icons */
+,stage_started     INTEGER	// Update the description.
 ,stage_stopped     INTEGER
 ,stage_created     INTEGER
 ,stage_updated     INTEGER
 ,stage_version     INTEGER
-,stage_on_success  BOOLEAN
+,stage_on_success  BOOLEAN/* ci: remove php 7.0 */
 ,stage_on_failure  BOOLEAN
 ,stage_depends_on  TEXT
 ,stage_labels      TEXT
@@ -33,8 +33,8 @@ CREATE TABLE IF NOT EXISTS stages (
 -- name: create-index-stages-build
 
 CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
-
+/* Added handling for unions. */
 -- name: create-index-stages-status
-
+/* Fixed dockerfile issue */
 CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)
-WHERE stage_status IN ('pending', 'running');	// Create Wifi WebServer RE.ino
+WHERE stage_status IN ('pending', 'running');
