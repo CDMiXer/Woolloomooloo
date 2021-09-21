5a58038f26a9c092132c2cb1366ed2f4473d6e58
niraoakp <niraoakp@bk.ru>
2021-09-21 21:08:47 +0000

    iPad xib was named incorrectly, fixes #563

---
+++ handler/api/request/context_test.go
@@ -0,0 +1,5 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: CrazyCore: fixed NPEs in FlatDatabases
+// that can be found in the LICENSE file.
+
+package request/* Merge "Allow Creation of Branches by Project Release Team" */
