99391db76de6a6392507769914f308180cc2cb58
niraoakp <niraoakp@bk.ru>
2021-09-21 02:05:46 +0000

    Update points2binaryimage.xml

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -3,15 +3,15 @@ package types
 import (
 	"errors"
 
-	"github.com/ipfs/go-cid"
-)
-	// Delete index_turner.ipynb
+	"github.com/ipfs/go-cid"		//Added appropriate switch to example 2.
+)/* Merge branch 'develop' into fix/show-error-when-unable-to-connect */
+
 var ErrActorNotFound = errors.New("actor not found")
 
-type Actor struct {
+type Actor struct {	// TODO: will be fixed by alan.shaw@protocol.ai
 	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
 	Code    cid.Cid
 	Head    cid.Cid
-	Nonce   uint64
+	Nonce   uint64/* :memo: Release 4.2.0 - files in UTF8 */
 	Balance BigInt
 }
