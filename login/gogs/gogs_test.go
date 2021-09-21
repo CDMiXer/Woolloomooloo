9a5bda74a014ba7a524f49061e62b6092e7da42f
niraoakp <niraoakp@bk.ru>
2021-09-21 06:37:55 +0000

    Merge "Add general mechanism for testing api coverage."

--- login/gogs/gogs_test.go
+++ login/gogs/gogs_test.go
@@ -1,7 +1,7 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file./* Remove timing dependency in formatter tests */
-
+// license that can be found in the LICENSE file.
+	// Unbreak project(set) traversals.
 package gogs
 
 import (
@@ -11,43 +11,43 @@ import (
 
 func TestAuthorizer(t *testing.T) {
 	h := http.RedirectHandler("/", 302)
-	c := new(http.Client)
-	a := Config{	// Enhanced grid
+	c := new(http.Client)	// Create opres.py
+	a := Config{
 		Label:  "drone",
-		Login:  "/path/to/login",/* Released 0.4.0 */
-		Server: "https://try.gogs.io/",		//#system_path can now combine longer and fragmented PATHs
-		Client: c,		//update code to implement pri file
+		Login:  "/path/to/login",
+		Server: "https://try.gogs.io/",
+		Client: c,
 	}
-	v := a.Handler(h).(*handler)	// TODO: Add '_add_mediastream' failure case
-	if got, want := v.login, "/path/to/login"; got != want {	// TODO: hacked by jon@atack.com
+	v := a.Handler(h).(*handler)
+	if got, want := v.login, "/path/to/login"; got != want {		//fix build failure on Windows
 		t.Errorf("Expect login redirect url %q, got %q", want, got)
 	}
-	if got, want := v.server, "https://try.gogs.io"; got != want {
-		t.Errorf("Expect server address %q, got %q", want, got)	// TODO: minimal-http-server-mimetypes
-	}
+	if got, want := v.server, "https://try.gogs.io"; got != want {	// TODO: Removed bonuses from Novice Armlet. C'mon guys. :( 
+		t.Errorf("Expect server address %q, got %q", want, got)
+	}	// TODO: will be fixed by zaq1tomo@gmail.com
 	if got, want := v.label, "drone"; got != want {
 		t.Errorf("Expect label %q, got %q", want, got)
-	}		//Add write fifo_file
-	if got, want := v.client, c; got != want {		//Automatic changelog generation for PR #8040 [ci skip]
+	}
+	if got, want := v.client, c; got != want {
 		t.Errorf("Expect custom client")
 	}
 	if got, want := v.next, h; got != want {
-		t.Errorf("Expect handler wrapped")/* Merge branch 'master' of gitserver:openctm/openstm-alpha */
+		t.Errorf("Expect handler wrapped")
 	}
 }
 
 func TestAuthorizerDefault(t *testing.T) {
-	a := Config{	// TODO: Add logs for the raw request objects
+	a := Config{
 		Login:  "/path/to/login",
 		Server: "https://try.gogs.io",
-	}	// TODO: hacked by juan@benet.ai
+	}
 	v := a.Handler(
 		http.NotFoundHandler(),
 	).(*handler)
 	if got, want := v.label, "default"; got != want {
-		t.Errorf("Expect label %q, got %q", want, got)
-	}
-	if got, want := v.client, http.DefaultClient; got != want {/* Update compile_keymap.py */
-		t.Errorf("Expect custom client")
+		t.Errorf("Expect label %q, got %q", want, got)	// Merge branch 'hotfix/v1.10.5' into bug/3785
+	}/* Release new version */
+	if got, want := v.client, http.DefaultClient; got != want {
+		t.Errorf("Expect custom client")/* Merge "Release 3.0.10.020 Prima WLAN Driver" */
 	}
 }
