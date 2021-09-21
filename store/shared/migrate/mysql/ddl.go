94e297ba1f2cec4888ddebe34cbfbc2382be74d6
niraoakp <niraoakp@bk.ru>
2021-09-21 17:00:43 +0000

    MSVC doesn't know the option /Wextra D:

---
+++ store/shared/migrate/mysql/ddl.go
@@ -0,0 +1,9 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: PEP8 and name corrections
+// Use of this source code is governed by the Drone Non-Commercial License	// 7c07a700-2e6f-11e5-9284-b827eb9e62be
+// that can be found in the LICENSE file.
+
+// +build !oss
+
+package mysql
+
+//go:generate togo ddl -package mysql -dialect mysql
