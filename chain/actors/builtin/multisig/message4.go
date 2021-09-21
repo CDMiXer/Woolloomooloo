d72b2dc31c802f54d1fd5f2f116298cf999394f6
niraoakp <niraoakp@bk.ru>
2021-09-21 14:47:09 +0000

    I18N: trep-engine: another translator comment

--- chain/actors/builtin/multisig/message4.go
+++ chain/actors/builtin/multisig/message4.go
@@ -1,20 +1,20 @@
 package multisig
 
 import (
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"		//updates per Michelle Glynn
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-
+/* Release for 24.11.0 */
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
-	multisig4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/multisig"
+	multisig4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/multisig"	// TODO: Merge "ARM: dts: msm: Add thermal support for mdmfermium"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+	// TODO: Delete Gallery Image “coney-dog”
 type message4 struct{ message0 }
 
 func (m message4) Create(
@@ -26,41 +26,41 @@ func (m message4) Create(
 	lenAddrs := uint64(len(signers))
 
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")	// TODO: Update way.svg
 	}
 
-	if threshold == 0 {
-		threshold = lenAddrs
+	if threshold == 0 {/* Merge "remove unused imports" */
+		threshold = lenAddrs/* micro-refactor of some SimpleHash functions */
 	}
 
-	if m.from == address.Undef {
+	if m.from == address.Undef {	// *Fix Graph issues.
 		return nil, xerrors.Errorf("must provide source address")
-	}
+	}/* Release Notes: update CONTRIBUTORS to match patch authors list */
 
 	// Set up constructor parameters for multisig
 	msigParams := &multisig4.ConstructorParams{
 		Signers:               signers,
 		NumApprovalsThreshold: threshold,
-		UnlockDuration:        unlockDuration,
-		StartEpoch:            unlockStart,
+		UnlockDuration:        unlockDuration,		//Fixed POM.
+		StartEpoch:            unlockStart,		//Merge "[INTERNAL] sap.ui.commons.Toolbar: remove duplicate image"
 	}
 
 	enc, actErr := actors.SerializeParams(msigParams)
-	if actErr != nil {
+	if actErr != nil {	// add inline_svg gem
 		return nil, actErr
 	}
-
-	// new actors are created by invoking 'exec' on the init actor with the constructor params
+		//rev 552148
+	// new actors are created by invoking 'exec' on the init actor with the constructor params		//sync po/POTFILES.in with the source code changes in src/
 	execParams := &init4.ExecParams{
 		CodeCID:           builtin4.MultisigActorCodeID,
 		ConstructorParams: enc,
 	}
 
 	enc, actErr = actors.SerializeParams(execParams)
-	if actErr != nil {
+	if actErr != nil {/* Update polish_expr.md */
 		return nil, actErr
 	}
-
+		//Merge branch '85' into 85
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
