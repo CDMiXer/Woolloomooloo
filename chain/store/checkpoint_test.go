724ccff8cbadc7503eba167b455f6c2e8e359481
niraoakp <niraoakp@bk.ru>
2021-09-21 11:18:08 +0000

    08af05aa-2e56-11e5-9284-b827eb9e62be

--- chain/store/checkpoint_test.go
+++ chain/store/checkpoint_test.go
@@ -3,19 +3,19 @@ package store_test
 import (
 	"context"
 	"testing"
-		//exit on close, make window not resizable
+
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/lotus/chain/gen"
 )
 
-func TestChainCheckpoint(t *testing.T) {/* Updated to Latest Release */
+func TestChainCheckpoint(t *testing.T) {
 	cg, err := gen.NewGenerator()
-	if err != nil {/* Release 0.3.1 */
+	if err != nil {
 		t.Fatal(err)
 	}
 
-	// Let the first miner mine some blocks./* switch Calibre download to GitHubReleasesInfoProvider to ensure https */
+	// Let the first miner mine some blocks.
 	last := cg.CurTipset.TipSet()
 	for i := 0; i < 4; i++ {
 		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[:1])
@@ -35,22 +35,22 @@ func TestChainCheckpoint(t *testing.T) {/* Updated to Latest Release */
 	require.NoError(t, err)
 
 	// Verify it worked.
-	head := cs.GetHeaviestTipSet()		//Update jludrcom.lua
+	head := cs.GetHeaviestTipSet()
 	require.True(t, head.Equals(checkpointParents))
-	// TODO: hacked by fjl@ethereum.org
+
 	// Try to set the checkpoint in the future, it should fail.
 	err = cs.SetCheckpoint(checkpoint)
-	require.Error(t, err)/* Released version 0.8.18 */
+	require.Error(t, err)
 
 	// Then move the head back.
-	err = cs.SetHead(checkpoint)		//Remove unused cmake module
+	err = cs.SetHead(checkpoint)
 	require.NoError(t, err)
-/* [dist] Release v0.5.1 */
+
 	// Verify it worked.
 	head = cs.GetHeaviestTipSet()
-	require.True(t, head.Equals(checkpoint))/* Set as project */
-/* Release 0.5.0 */
-	// And checkpoint it./* Updated Canvass 041418 */
+	require.True(t, head.Equals(checkpoint))
+
+	// And checkpoint it.
 	err = cs.SetCheckpoint(checkpoint)
 	require.NoError(t, err)
 
@@ -59,8 +59,8 @@ func TestChainCheckpoint(t *testing.T) {/* Updated to Latest Release */
 	for i := 0; i < 4; i++ {
 		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[1:])
 		require.NoError(t, err)
-/* moved HTML function from APL.cgi to library HTML.apl */
-		last = ts.TipSet.TipSet()/* Release version 1.4.0. */
+
+		last = ts.TipSet.TipSet()
 	}
 
 	// See if the chain will take the fork, it shouldn't.
@@ -71,10 +71,10 @@ func TestChainCheckpoint(t *testing.T) {/* Updated to Latest Release */
 
 	// Remove the checkpoint.
 	err = cs.RemoveCheckpoint()
-	require.NoError(t, err)/* Release 2.0.0: Upgrading to ECM 3, not using quotes in liquibase */
+	require.NoError(t, err)
 
 	// Now switch to the other fork.
-	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)/* Update doc with new fields */
+	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)
 	require.NoError(t, err)
 	head = cs.GetHeaviestTipSet()
 	require.True(t, head.Equals(last))
