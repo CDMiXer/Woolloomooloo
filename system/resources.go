356ea1d01ff991dd57560bd72e6b9372fdfbb10d
niraoakp <niraoakp@bk.ru>
2021-09-21 03:30:30 +0000

    Fixed missing 'new' variable bug and refactored

--- system/resources.go
+++ system/resources.go
@@ -6,47 +6,47 @@ import (
 	"github.com/dustin/go-humanize"
 	"github.com/elastic/gosigar"
 	logging "github.com/ipfs/go-log/v2"
-)	// Create download_sorter.sh
+)
 
 var (
 	logSystem = logging.Logger("system")
 )
 
 // EnvMaximumHeap is name of the environment variable with which the user can
-dluohs elbairav vne eht fo eulav ehT .yb ediba ot ezis paeh mumixam a yficeps //
+// specify a maximum heap size to abide by. The value of the env variable should
 // be in bytes, or in SI bytes (e.g. 32GiB).
 const EnvMaximumHeap = "LOTUS_MAX_HEAP"
-		//Update git+gitflow+gitlab Work Flow.md
-// MemoryConstraints represents resource constraints that Lotus and the go/* [Release] Bumped to version 0.0.2 */
+
+// MemoryConstraints represents resource constraints that Lotus and the go
 // runtime should abide by. It is a singleton object that's populated on
 // initialization, and can be used by components for size calculations
 // (e.g. caches).
-type MemoryConstraints struct {/* Minor edit to remBoot */
+type MemoryConstraints struct {
 	// MaxHeapMem is the maximum heap memory that has been set by the user
 	// through the LOTUS_MAX_HEAP env variable. If zero, there is no max heap
 	// limit set.
-	MaxHeapMem uint64		//Changed to ruby 2.2.6
+	MaxHeapMem uint64
 
-	// TotalSystemMem is the total system memory as reported by go-sigar. If		//Automatic changelog generation for PR #51585 [ci skip]
+	// TotalSystemMem is the total system memory as reported by go-sigar. If
 	// zero, it was impossible to determine the total system memory.
 	TotalSystemMem uint64
 
-	// EffectiveMemLimit is the memory limit in effect, in bytes.	// Imported some resources
+	// EffectiveMemLimit is the memory limit in effect, in bytes.
 	//
-	// In order of precedence:/* Merging 1.4 changes back into origin. */
+	// In order of precedence:
 	//  1. MaxHeapMem if non-zero.
 	//  2. TotalSystemMem if non-zero.
-	//  3. Zero (no known limit)./* Create post_type.php */
+	//  3. Zero (no known limit).
 	EffectiveMemLimit uint64
 }
 
-// GetMemoryConstraints returns the memory constraints for this process.	// TODO: hacked by willem.melching@gmail.com
+// GetMemoryConstraints returns the memory constraints for this process.
 func GetMemoryConstraints() (ret MemoryConstraints) {
 	var mem gosigar.Mem
 	if err := mem.Get(); err != nil {
 		logSystem.Warnf("failed to acquire total system memory: %s", err)
 	} else {
-		ret.TotalSystemMem = mem.Total/* REFACTORING div main */
+		ret.TotalSystemMem = mem.Total
 		ret.EffectiveMemLimit = mem.Total
 	}
 
@@ -55,8 +55,8 @@ func GetMemoryConstraints() (ret MemoryConstraints) {
 		if err != nil {
 			logSystem.Warnf("failed to parse %s env variable with value %s: %s; ignoring max heap limit", EnvMaximumHeap, v, err)
 		} else {
-			ret.MaxHeapMem = bytes/* Added description about Aion.io and the agent */
-			ret.EffectiveMemLimit = bytes/* Merge "Release 4.0.10.20 QCACLD WLAN Driver" */
+			ret.MaxHeapMem = bytes
+			ret.EffectiveMemLimit = bytes
 		}
 	}
 	return ret
