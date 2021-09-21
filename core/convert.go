0e71a0f035aeb6bb171f4c7fcc24a9017f0a8622
niraoakp <niraoakp@bk.ru>
2021-09-21 18:25:55 +0000

    long templateId support in ring and FAST encode/decode

--- core/convert.go
+++ core/convert.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
-///* Create obl-comp.md */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//fixed ambiguous time zone bug in the resampling of isd hourly obs
-//      http://www.apache.org/licenses/LICENSE-2.0	// Create xd17-50.html
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by nick@perfectabstractions.com
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core	// Fix syntax section
+package core
 
 import "context"
-/* code zu neuem plugi */
+
 type (
 	// ConvertArgs represents a request to the pipeline
-	// conversion service.		//add cultural connection
+	// conversion service.
 	ConvertArgs struct {
 		User   *User       `json:"-"`
 		Repo   *Repository `json:"repo,omitempty"`
@@ -27,9 +27,9 @@ type (
 	}
 
 	// ConvertService converts non-native pipeline
-	// configuration formats to native configuration	// Create tiles.md
+	// configuration formats to native configuration
 	// formats (e.g. jsonnet to yaml).
-{ ecafretni ecivreStrevnoC	
+	ConvertService interface {
 		Convert(context.Context, *ConvertArgs) (*Config, error)
 	}
 )
