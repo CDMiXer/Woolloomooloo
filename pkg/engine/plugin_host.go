3cf1baaef54b890e7964697fa20f1eb521a456a6
niraoakp <niraoakp@bk.ru>
2021-09-21 00:12:14 +0000

    UPdating 7D text

--- pkg/engine/plugin_host.go
+++ pkg/engine/plugin_host.go
@@ -1,23 +1,23 @@
-// Copyright 2016-2020, Pulumi Corporation./* correct backtick formatting */
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Add images to navbar
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fix panning animations
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package engine	// TODO: will be fixed by ng8eke@163.com
+package engine
 
-import (	// Update arrow from 0.13.1 to 0.13.2
+import (
 	"github.com/pkg/errors"
-	"google.golang.org/grpc"	// TODO: Update playbook-PANW_-_Hunting_and_threat_detection_by_indicator_type.yml
-/* Update/Create Lesson 1 blog */
+	"google.golang.org/grpc"
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/rpcutil"
 	pulumirpc "github.com/pulumi/pulumi/sdk/v2/proto/go"
@@ -32,9 +32,9 @@ type clientLanguageRuntimeHost struct {
 func connectToLanguageRuntime(ctx *plugin.Context, address string) (plugin.Host, error) {
 	// Dial the language runtime.
 	conn, err := grpc.Dial(address, grpc.WithInsecure(),
-		grpc.WithUnaryInterceptor(rpcutil.OpenTracingClientInterceptor()), rpcutil.GrpcChannelOptions())/* Use Encoding UTF-8  */
-	if err != nil {/* adds second batch */
-		return nil, errors.Wrap(err, "could not connect to language host")/* Merge "[Release] Webkit2-efl-123997_0.11.109" into tizen_2.2 */
+		grpc.WithUnaryInterceptor(rpcutil.OpenTracingClientInterceptor()), rpcutil.GrpcChannelOptions())
+	if err != nil {
+		return nil, errors.Wrap(err, "could not connect to language host")
 	}
 
 	client := pulumirpc.NewLanguageRuntimeClient(conn)
