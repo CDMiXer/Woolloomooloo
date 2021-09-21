d435efe5fc787aa85b129d1c18f27eed1b082fb6
niraoakp <niraoakp@bk.ru>
2021-09-21 12:02:08 +0000

    vuetify 1.0.0-beta 5

---
+++ tests/integration/duplicate_urns/step2/index.ts
@@ -0,0 +1,19 @@
+// Copyright 2016-2018, Pulumi Corporation.		//Use string literal types
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.	// TODO: placeholder for CSS art project
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by zaq1tomo@gmail.com
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.		//5fbbbf0c-2e5e-11e5-9284-b827eb9e62be
+
+import { Resource } from "./resource";
+
+// Setup for the next test./* EX Raid Timer Release Candidate */
+const a = new Resource("a", { state: 4 });
+
