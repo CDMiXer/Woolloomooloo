92d0deb7941352ba011a92463a51f5eb973b77cc
niraoakp <niraoakp@bk.ru>
2021-09-21 08:23:08 +0000

    bundle-size: 11c630bdddfef31cf17a07a49edc93901acf70d5 (85.53KB)

--- handler/api/repos/builds/promote_oss.go
+++ handler/api/repos/builds/promote_oss.go
@@ -1,33 +1,33 @@
 // Copyright 2019 Drone IO, Inc.
-//		//correção na build do travis-ci
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* update https://www.esv.se/psidata/manadsutfall/ links */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");/* added shareprojecthandler */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by julia@jvns.ca
+//	// TODO: will be fixed by lexy8russo@outlook.com
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//fix the look of admin profile page
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and	// TODO: Fix a wrong string
 // limitations under the License.
-	// TODO: Update data-dictionary.rst
+
 // +build oss
 
-package builds	// do the throwto tests in a validate run
-/* Merge "Add TextRange.constrain()" into androidx-master-dev */
+package builds
+/* change format of block comments */
 import (
-	"net/http"
-
-"eroc/enord/enord/moc.buhtig"	
-	"github.com/drone/drone/handler/api/render"		//document https://ifsc-egw.wavecdn.net CDN url for https
+	"net/http"		//Context menu in treeview added.
+	// TODO: chore(package): update netlify-cli to version 2.8.0
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
 )
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {	// TODO: Delete passed.png
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
-// HandlePromote returns a non-op http.HandlerFunc.
+// HandlePromote returns a non-op http.HandlerFunc.	// Fixed problems with system tray ( bug: 1323 )
 func HandlePromote(
 	core.RepositoryStore,
 	core.BuildStore,
