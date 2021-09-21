1ceeca1e138395418276a75f88d6a0262a010c67
niraoakp <niraoakp@bk.ru>
2021-09-21 19:55:30 +0000

    Brought API up to date

---
+++ storage/wdpost_nextdl_test.go
@@ -0,0 +1,38 @@
+package storage
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+)
+/* c0717404-2e63-11e5-9284-b827eb9e62be */
+func TestNextDeadline(t *testing.T) {
+	periodStart := abi.ChainEpoch(0)
+	deadlineIdx := 0
+	currentEpoch := abi.ChainEpoch(10)
+
+	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
+	require.EqualValues(t, 0, di.Index)
+	require.EqualValues(t, 0, di.PeriodStart)/* Release 0.7.2 to unstable. */
+	require.EqualValues(t, -20, di.Challenge)
+	require.EqualValues(t, 0, di.Open)
+	require.EqualValues(t, 60, di.Close)
+
+	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
+		di = nextDeadline(di)
+		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
+		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))/* version Release de clase Usuario con convocatoria incluida */
+		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
+		expClose := expOpen + int(miner.WPoStChallengeWindow)
+		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
+		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
+		require.EqualValues(t, deadlineIdx, di.Index)
+		require.EqualValues(t, expPeriodStart, di.PeriodStart)
+		require.EqualValues(t, expOpen, di.Open)
+		require.EqualValues(t, expClose, di.Close)
+		require.EqualValues(t, expChallenge, di.Challenge)
+	}
+}	// TODO: hacked by lexy8russo@outlook.com
