d41b37c6968df1d0b0ad05b408ca5364715627f2
niraoakp <niraoakp@bk.ru>
2021-09-21 08:52:56 +0000

    i18n - admin.ImportExport and edit view

--- handler/api/repos/builds/branches/create.go
+++ handler/api/repos/builds/branches/create.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: hacked by vyzo@hackzen.org
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
