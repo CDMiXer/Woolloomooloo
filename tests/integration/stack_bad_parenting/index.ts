b2bc6d573df630a91f9f2ae05b0cb21f178bc446
niraoakp <niraoakp@bk.ru>
2021-09-21 18:43:54 +0000

    BSTriShape more work

--- tests/integration/stack_bad_parenting/index.ts
+++ tests/integration/stack_bad_parenting/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-	// Update fcb.py
+
 let currentID = 0;
 
 class Provider implements pulumi.dynamic.ResourceProvider {
@@ -9,17 +9,17 @@ class Provider implements pulumi.dynamic.ResourceProvider {
 
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    constructor() {		//enable recent post thing
-        this.create = async (inputs: any) => {		//Fix bug in keepalive method
+    constructor() {
+        this.create = async (inputs: any) => {
             return {
-                id: (currentID++).toString(),/* Release of eeacms/ims-frontend:0.9.8 */
-                outs: undefined,/* Fix redefining tabs when reopening activity */
+                id: (currentID++).toString(),
+                outs: undefined,
             };
         };
-    }	// TODO: will be fixed by boringland@protonmail.ch
+    }
 }
 
-class Resource extends pulumi.dynamic.Resource {		//Merge "Fix timeout option in Cinder upload volume util"
+class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, parent?: pulumi.Resource) {
         super(Provider.instance, name, {}, { parent: parent });
     }
