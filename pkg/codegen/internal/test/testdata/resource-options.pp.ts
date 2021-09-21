b2d3fa36c05cec7c29790fb103b20775e42db3f3
niraoakp <niraoakp@bk.ru>
2021-09-21 14:09:09 +0000

    Bump version to 1.0.11

--- pkg/codegen/internal/test/testdata/resource-options.pp.ts
+++ pkg/codegen/internal/test/testdata/resource-options.pp.ts
@@ -3,11 +3,11 @@ import * as aws from "@pulumi/aws";
 
 const provider = new aws.Provider("provider", {region: "us-west-2"});
 const bucket1 = new aws.s3.Bucket("bucket1", {}, {
-    provider: provider,/* Release for another new ESAPI Contrib */
-    dependsOn: [provider],		//#hoplon -> #bootclj IRC
+    provider: provider,
+    dependsOn: [provider],
     protect: true,
-    ignoreChanges: [	// Update learn2learn.aiml
+    ignoreChanges: [
         "bucket",
-        "lifecycleRules[0]",/* Release 0.13.0. */
+        "lifecycleRules[0]",
     ],
 });
