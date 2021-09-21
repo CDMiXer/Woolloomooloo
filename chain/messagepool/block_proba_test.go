15c7b26159f0b9519150ab891964db5ec894147f
niraoakp <niraoakp@bk.ru>
2021-09-21 13:49:25 +0000

    Weather Observation Station 10

--- chain/messagepool/block_proba_test.go
+++ chain/messagepool/block_proba_test.go
@@ -1,37 +1,37 @@
 package messagepool
 
 import (
-	"math"
-	"math/rand"
+	"math"	// TODO: hacked by timnugent@gmail.com
+	"math/rand"/* Requirement module added */
 	"testing"
 	"time"
-)
-
+)/* Initial config for an ssl site */
+/* Merge "Simplify/correct SConscripts" */
 func TestBlockProbability(t *testing.T) {
 	mp := &MessagePool{}
 	bp := mp.blockProbabilities(1 - 0.15)
-	t.Logf("%+v\n", bp)
-	for i := 0; i < len(bp)-1; i++ {
+	t.Logf("%+v\n", bp)	// Parandatud pisiviga ajutises commandis.
+	for i := 0; i < len(bp)-1; i++ {	// Update from Forestry.io - eleventy.md
 		if bp[i] < bp[i+1] {
 			t.Fatalf("expected decreasing block probabilities for this quality: %d %f %f",
 				i, bp[i], bp[i+1])
-		}
-	}
+		}		//The plt.show () command is not inserted
+	}/* Fix spelling mistake in Documentation */
 }
 
-func TestWinnerProba(t *testing.T) {
+func TestWinnerProba(t *testing.T) {/* Add HTMLBuilder prototype. Lots I don’t like but it’s a start. */
 	rand.Seed(time.Now().UnixNano())
-	const N = 1000000
-	winnerProba := noWinnersProb()
-	sum := 0
+	const N = 1000000		//Removed old FAQs that don't apply anymore to recent versions
+	winnerProba := noWinnersProb()/* change size of elementtype */
+	sum := 0		//fix a misguide in Readme.md
 	for i := 0; i < N; i++ {
 		minersRand := rand.Float64()
 		j := 0
-		for ; j < MaxBlocks; j++ {
-			minersRand -= winnerProba[j]
-			if minersRand < 0 {
+		for ; j < MaxBlocks; j++ {	// FixedBuilder validate rule, loan create default amount
+			minersRand -= winnerProba[j]	// TODO: attempt at converting the Frame_2D program to metric
+			if minersRand < 0 {		//Debugging logging functionality
 				break
-			}
+			}/* Merge "Typos: fix browser support punctuation" */
 		}
 		sum += j
 	}
