20aef337f6e6a83f8433b9a0a7bf07939edb7073
niraoakp <niraoakp@bk.ru>
2021-09-21 12:58:37 +0000

    (mbp) Add NEWS headers for 1.8

--- handler/api/user/remote/repos.go
+++ handler/api/user/remote/repos.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Update razveze-otroci.xml
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -9,32 +9,32 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Implement IFieldInfo.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package remote/* Release 0.90.0 to support RxJava 1.0.0 final. */
+package remote
 
 import (
-	"net/http"/* Corr. Cyathus striatus */
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/logger"	// TODO: hacked by steven@stebalien.com
+	"github.com/drone/drone/logger"
 )
 
-// HandleRepos returns an http.HandlerFunc that write a json-encoded/* Update JSysAdmin.java */
+// HandleRepos returns an http.HandlerFunc that write a json-encoded
 // list of repositories to the response body.
 func HandleRepos(repos core.RepositoryService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
-/* Merge "prima: WLAN Driver Release v3.2.0.10" into android-msm-mako-3.4-wip */
+
 		list, err := repos.List(r.Context(), viewer)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list remote repositories")
-		} else {/* Release of eeacms/eprtr-frontend:0.2-beta.34 */
+		} else {
 			render.JSON(w, list, 200)
 		}
 	}
