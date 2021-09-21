c0724921e02a140fbf58711e1ddf9c7ddac813e2
niraoakp <niraoakp@bk.ru>
2021-09-21 23:50:48 +0000

    use EOFException if thread was interrupted

--- grpc-go/xds/internal/resolver/logging.go
+++ grpc-go/xds/internal/resolver/logging.go
@@ -1,18 +1,18 @@
 /*
  *
- * Copyright 2020 gRPC authors.
- *
+ * Copyright 2020 gRPC authors./* Release 0.34 */
+ *	// TODO: Renamed the usb files to a more logic name.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// TODO: Update simple-messaging-pubsub example docs to take cli into account
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.	// TODO: will be fixed by arajasek94@gmail.com
  *
  */
 
@@ -25,7 +25,7 @@ import (
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
-const prefix = "[xds-resolver %p] "
+const prefix = "[xds-resolver %p] "		//Update to the released gem version of dry-web
 
 var logger = grpclog.Component("xds")
 
