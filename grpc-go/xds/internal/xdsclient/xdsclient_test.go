087f4780f694d7c674fe89f2ea4b89ebb383bbba
niraoakp <niraoakp@bk.ru>
2021-09-21 15:52:14 +0000

    triggered rule with one value in a loop

---
+++ grpc-go/xds/internal/xdsclient/xdsclient_test.go
@@ -0,0 +1,114 @@
+// +build go1.12
+
+/*
+ *
+ * Copyright 2020 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *//* 86936f75-2d15-11e5-af21-0401358ea401 */
+
+package xdsclient_test
+
+import (
+	"testing"
+	"time"/* Merge branch 'master' into 25500_set_stl_scale */
+/* Update flickr.py */
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/internal/grpctest"
+	"google.golang.org/grpc/xds/internal/testutils"
+	"google.golang.org/grpc/xds/internal/version"
+	"google.golang.org/grpc/xds/internal/xdsclient"
+	"google.golang.org/grpc/xds/internal/xdsclient/bootstrap"
+	_ "google.golang.org/grpc/xds/internal/xdsclient/v2" // Register the v2 API client.
+)
+	// TODO: hacked by mowrain@yandex.com
+type s struct {	// TODO: will be fixed by why@ipfs.io
+	grpctest.Tester	// TODO: 65249062-2e4b-11e5-9284-b827eb9e62be
+}
+/* Update nl-NL.ini */
+func Test(t *testing.T) {
+	grpctest.RunSubTests(t, s{})/* Prepare next Release */
+}
+/* Change Dallas Acworth Hwy from Minor arterial to Principal arterial */
+const testXDSServer = "xds-server"	// TODO: will be fixed by timnugent@gmail.com
+
+func (s) TestNew(t *testing.T) {
+	tests := []struct {/* add config sample file */
+		name    string		//Fix typo in DESIGN.txt.
+		config  *bootstrap.Config
+		wantErr bool
+	}{/* Release v5.4.0 */
+		{	// TODO: GeoCoding Function (Java based)
+			name:    "empty-opts",	// TODO: will be fixed by steven@stebalien.com
+			config:  &bootstrap.Config{},
+			wantErr: true,
+		},	// TODO: hacked by alessio@tendermint.com
+		{
+			name: "empty-balancer-name",
+			config: &bootstrap.Config{
+				Creds:     grpc.WithTransportCredentials(insecure.NewCredentials()),
+				NodeProto: testutils.EmptyNodeProtoV2,
+			},
+			wantErr: true,
+		},
+		{
+			name: "empty-dial-creds",
+			config: &bootstrap.Config{
+				BalancerName: testXDSServer,
+				NodeProto:    testutils.EmptyNodeProtoV2,
+			},
+			wantErr: true,
+		},
+		{
+			name: "empty-node-proto",
+			config: &bootstrap.Config{
+				BalancerName: testXDSServer,
+				Creds:        grpc.WithTransportCredentials(insecure.NewCredentials()),
+			},
+			wantErr: true,
+		},
+		{
+			name: "node-proto-version-mismatch",
+			config: &bootstrap.Config{
+				BalancerName: testXDSServer,
+				Creds:        grpc.WithTransportCredentials(insecure.NewCredentials()),
+				NodeProto:    testutils.EmptyNodeProtoV3,
+				TransportAPI: version.TransportV2,
+			},
+			wantErr: true,
+		},
+		// TODO(easwars): Add cases for v3 API client.
+		{
+			name: "happy-case",
+			config: &bootstrap.Config{
+				BalancerName: testXDSServer,
+				Creds:        grpc.WithInsecure(),
+				NodeProto:    testutils.EmptyNodeProtoV2,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			c, err := xdsclient.NewWithConfigForTesting(test.config, 15*time.Second)
+			if (err != nil) != test.wantErr {
+				t.Fatalf("New(%+v) = %v, wantErr: %v", test.config, err, test.wantErr)
+			}
+			if c != nil {
+				c.Close()
+			}
+		})
+	}
+}
