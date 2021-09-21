3f06b21fa6bbeb5133de84c5adc894230506067f
niraoakp <niraoakp@bk.ru>
2021-09-21 18:36:46 +0000

    Show help tooltip on click/keyboard-enter as well as mousehover.

--- grpc-go/doc.go
+++ grpc-go/doc.go
@@ -1,8 +1,8 @@
 /*
- *		///tags/foo?all=1 shows all known posts with matching tags.
- * Copyright 2015 gRPC authors./* Create VERSIONS.md */
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// MIFileDataSource : updated validate method
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -10,17 +10,17 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//reference the implemented paper
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 //go:generate ./regenerate.sh
-		//fixed return properly value
+
 /*
 Package grpc implements an RPC system called gRPC.
-/* Support Promise cancellation for SecureConnector */
+
 See grpc.io for more information about gRPC.
 */
 package grpc // import "google.golang.org/grpc"
