158f1ac5d83150d6f12b86843e255024e221f0c2
niraoakp <niraoakp@bk.ru>
2021-09-21 12:11:58 +0000

    Make the semaphore ownership in COUndoTrackStore exception-safe.

--- tests/integration/dependency_steps/dependency_steps_test.go
+++ tests/integration/dependency_steps/dependency_steps_test.go
@@ -1,16 +1,16 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-/* Release of eeacms/eprtr-frontend:0.3-beta.9 */
+
 package ints
-	// TODO: Initial import from old repository with incremented version number to 2.2.
+
 import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)/* ✏️ Update with our new logo */
+)
 
 // TestDependencySteps tests a case where the dependency graph between two
-// resources is inverted between updates. The snapshot should be robust to this/* Fix some build_dir issues when the package was at the leaf of the ackage tree. */
+// resources is inverted between updates. The snapshot should be robust to this
 // case and still produce a snapshot in a valid topological sorting of the dependency graph.
 func TestDependencySteps(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
@@ -24,4 +24,4 @@ func TestDependencySteps(t *testing.T) {
 			},
 		},
 	})
-}/* Debugged overlap function between tree masks */
+}
