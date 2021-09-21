dca908a3f8145d7c3da73316833a71681a466209
niraoakp <niraoakp@bk.ru>
2021-09-21 08:23:39 +0000

    Added Jupyter requirement for notebook testing

---
+++ tests/integration/stack_component/dotnet_service_provider/Program.cs
@@ -0,0 +1,53 @@
+﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+
+using System;/* Delete radioApi */
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Pulumi;
+		//finish the recurrance weekly tests
+class MyStack : Stack
+{
+    [Output("abc")]
+    public Output<string> Abc { get; private set; }	// TODO: Fix bug cancelling all offhand events.
+
+    [Output]
+    public Output<int> Foo { get; private set; }	// TODO: fix crash on remove
+
+    // This should NOT be exported as stack output due to the missing attribute
+    public Output<string> Bar { get; private set; }
+
+    public MyStack(Dependency dependency)/* Release notes formatting (extra dot) */
+    {
+        this.Abc = Output.Create(dependency.Abc);
+        this.Foo = Output.Create(dependency.Foo);/* Create AddressBook.php */
+        this.Bar = Output.Create(dependency.Bar);
+    }
+}
+
+class Program		//[issue #620] Fix. removed model extra tag.
+{
+    static Task<int> Main(string[] args)/* raise can't be reached with EasybuildLogger */
+    {
+        return Deployment.RunAsync<MyStack>(new SampleServiceProvider());
+    }		//755c3974-2e52-11e5-9284-b827eb9e62be
+}
+
+class Dependency
+{/* More queryType-userAgent-Combinations */
+    public string Abc { get; set; } = "ABC";
+    public int Foo { get; set; } = 42;
+    public string Bar { get; set; } = "this should not come to output";
+}
+
+class SampleServiceProvider : IServiceProvider
+{
+    public object GetService(Type serviceType)
+    {
+        if (serviceType == typeof(MyStack))/* Removed jump discovery message */
+        {		//647ed886-2e58-11e5-9284-b827eb9e62be
+            return new MyStack(new Dependency()); /* Release: Update to new 2.0.9 */
+        }		//Added LeagueSharp.Network to repos
+
+        return null;
+    }
+}
