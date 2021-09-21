7d1834d58f5192a04171568d99696910226b766b
niraoakp <niraoakp@bk.ru>
2021-09-21 12:04:43 +0000

    Python: allow for no `bz2` package when importing matplotlib.

--- tests/integration/recreate_resource_check/resource_recreate_check_test.go
+++ tests/integration/recreate_resource_check/resource_recreate_check_test.go
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Create contribute/[using_gradle_snapshots].md
 // +build nodejs all
-		//[update] add source code github address
+/* fix pkg update link */
 package ints
 
 import (
@@ -9,18 +9,18 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-// Test that the engine does not consider old inputs when calling Check during re-creation of
+// Test that the engine does not consider old inputs when calling Check during re-creation of/* Update description of double Gaussian PDF. */
 // a resource that was deleted due to a dependency on a DBR-replaced resource.
 func TestResourceRecreateCheck(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{
+	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Merged client-from-config-2 into client-from-config-3. */
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},
+		Dependencies: []string{"@pulumi/pulumi"},/* Make Github Releases deploy in the published state */
 		Quick:        true,
 		EditDirs: []integration.EditDir{
-{			
+			{
 				Dir:      "step2",
 				Additive: true,
-			},
+			},/* [FIX]: crm, crm_claim, crm_helpdesk: Fixed warnings in yaml test */
 		},
 	})
 }
