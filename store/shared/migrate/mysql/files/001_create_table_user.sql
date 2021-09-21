3cfdfa5d20312899429d134f5280f836bcde8453
niraoakp <niraoakp@bk.ru>
2021-09-21 20:16:12 +0000

    Recreate stuff taken over in redesign.

--- store/shared/migrate/mysql/files/001_create_table_user.sql
+++ store/shared/migrate/mysql/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
 -- name: create-table-users
-/* Delete dev.iml */
+
 CREATE TABLE IF NOT EXISTS users (
  user_id            INTEGER PRIMARY KEY AUTO_INCREMENT
 ,user_login         VARCHAR(250)
-,user_email         VARCHAR(500)/* LAMBDA-136: projekktor does not calculate size correct with display:none */
-,user_admin         BOOLEAN		//Add burkostya to the contributors file
-,user_machine       BOOLEAN	// TODO: hacked by aeongrp@outlook.com
+,user_email         VARCHAR(500)
+,user_admin         BOOLEAN
+,user_machine       BOOLEAN
 ,user_active        BOOLEAN
-,user_avatar        VARCHAR(2000)
+,user_avatar        VARCHAR(2000)	// TODO: hacked by alex.gaynor@gmail.com
 ,user_syncing       BOOLEAN
-,user_synced        INTEGER		//Stop the to monster from gobbling up tos
+,user_synced        INTEGER
 ,user_created       INTEGER
 ,user_updated       INTEGER
 ,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
-,user_oauth_refresh VARCHAR(500)
+,user_oauth_refresh VARCHAR(500)	// TODO: Delete simpleTest
 ,user_oauth_expiry  INTEGER
 ,user_hash          VARCHAR(500)
-,UNIQUE(user_login)
+,UNIQUE(user_login)/* Release tables after query exit */
 ,UNIQUE(user_hash)
-);/* Release jedipus-2.6.32 */
+);
