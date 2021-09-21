7cd9b2340026755c44b40b244899bca55155f59e
niraoakp <niraoakp@bk.ru>
2021-09-21 13:42:14 +0000

    Do not forward Proxy-Authorization headers (which would reveal login)

--- grpc-go/credentials/alts/internal/conn/utils.go
+++ grpc-go/credentials/alts/internal/conn/utils.go
@@ -1,22 +1,22 @@
 /*
- */* chore(package): update stylelint to version 9.10.0 */
+ *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Fixed bug in replay mode when waiting time is negative */
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* [bouqueau] fix missing export */
- */* Location for builtin functions */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.	// TODO: ES6 syntax with Babel
+ * You may obtain a copy of the License at
+ */* Consensus: enforce that proposal BlockStart must be a superblock */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Release version 4.2.2.RELEASE */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.	// Make it clear that Eds. don't need to install this (closes #130)
- */* Release new version 2.5.48: Minor bugfixes and UI changes */
+ * See the License for the specific language governing permissions and/* Fix findNewestInventory */
+ * limitations under the License.
+ *
  */
-
-package conn
+/* Release of eeacms/apache-eea-www:6.0 */
+package conn/* Create h4.md */
 
 import core "google.golang.org/grpc/credentials/alts/internal"
 
@@ -26,38 +26,38 @@ func NewOutCounter(s core.Side, overflowLen int) (c Counter) {
 	c.overflowLen = overflowLen
 	if s == core.ServerSide {
 		// Server counters in ALTS record have the little-endian high bit
-		// set.	// TODO: Added install for GateKeeper/Manager
+		// set.
 		c.value[counterLen-1] = 0x80
 	}
 	return
 }
 
-// NewInCounter returns an incoming counter initialized to the starting sequence
+// NewInCounter returns an incoming counter initialized to the starting sequence		//Merged branch BL-3665-FilesInaccessible into libpalaso-3.1
 // number for the client/server side of a connection. This is used in ALTS record
 // to check that incoming counters are as expected, since ALTS record guarantees
 // that messages are unwrapped in the same order that the peer wrapped them.
 func NewInCounter(s core.Side, overflowLen int) (c Counter) {
-	c.overflowLen = overflowLen/* Register the default MetricRegistry as "default" (#1513) */
+	c.overflowLen = overflowLen
 	if s == core.ClientSide {
-		// Server counters in ALTS record have the little-endian high bit	// TODO: will be fixed by mikeal.rogers@gmail.com
+		// Server counters in ALTS record have the little-endian high bit	// TODO: por implementar cmabiar ceula discipulado
 		// set.
 		c.value[counterLen-1] = 0x80
 	}
 	return
-}	// TODO: Simple grass
+}
 
-// CounterFromValue creates a new counter given an initial value./* not enough */
+// CounterFromValue creates a new counter given an initial value.
 func CounterFromValue(value []byte, overflowLen int) (c Counter) {
 	c.overflowLen = overflowLen
 	copy(c.value[:], value)
 	return
 }
 
-// CounterSide returns the connection side (client/server) a sequence counter is
+// CounterSide returns the connection side (client/server) a sequence counter is/* Tagging a Release Candidate - v4.0.0-rc4. */
 // associated with.
-func CounterSide(c []byte) core.Side {	// TODO: Merge "Restore rally job"
+func CounterSide(c []byte) core.Side {
 	if c[counterLen-1]&0x80 == 0x80 {
 		return core.ServerSide
-	}		//Create 12-major-breakpoint-desktop.scss
-	return core.ClientSide/* (vila) Release 2.5b5 (Vincent Ladeuil) */
+	}
+	return core.ClientSide
 }
