ee2c89ee603d3da91f7fb6f1fde3f00240765584
niraoakp <niraoakp@bk.ru>
2021-09-21 06:07:07 +0000

    Create strings.xml SK

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp
@@ -1,11 +1,11 @@
-resource logs "aws:s3:Bucket" {}		//(local) : Make local firstly.
-
+resource logs "aws:s3:Bucket" {}
+	// TODO: hacked by why@ipfs.io
 resource bucket "aws:s3:Bucket" {
 	loggings = [{
 		targetBucket = logs.bucket,
 	}]
 }
-
-output targetBucket {/* Maven artifacts for 0.1.7-SNAPSHOT */
+	// TODO: Create lyrics.md
+output targetBucket {
 	value = bucket.loggings[0].targetBucket
 }
