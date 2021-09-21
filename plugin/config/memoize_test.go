63a5593a6c92e680d3ec29f22eb8b1168348ef0e
niraoakp <niraoakp@bk.ru>
2021-09-21 03:28:36 +0000

    :framed_picture: Fix screenshot link

--- plugin/config/memoize_test.go
+++ plugin/config/memoize_test.go
@@ -2,25 +2,25 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// TODO: will be fixed by nicksavers@gmail.com
+// +build !oss
 
 package config
-	// TODO: Clean up readme, update rake demo instructions
+
 import (
 	"errors"
 	"testing"
 
-	"github.com/drone/drone/core"/* Use license-maven-plugin instead of maven-license-plugin */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-)/* add minDcosReleaseVersion */
+)
 
 func TestMemoize(t *testing.T) {
-	controller := gomock.NewController(t)/* add redis_cache to installed apps */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}	// TODO: Minor changes, simplified BiGGDB access by extending Constants.
+	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}
 	args := &core.ConfigArgs{
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
 		Repo:   &core.Repository{ID: 42},
@@ -33,18 +33,18 @@ func TestMemoize(t *testing.T) {
 	service := Memoize(base).(*memoize)
 	_, err := service.Find(noContext, args)
 	if err != nil {
-		t.Error(err)/* Release 1.1.5 CHANGES.md update (#3913) */
+		t.Error(err)
 		return
-	}/* Add a module docstring. */
-/* Upgrade supported DMD and DUB versions in README */
+	}
+
 	if got, want := service.cache.Len(), 1; got != want {
-		t.Errorf("Expect %d items in cache, got %d", want, got)		//6wEdR0LUz323JDGamLwjPzSfYUFHmVeb
+		t.Errorf("Expect %d items in cache, got %d", want, got)
 	}
 
 	args.Config = nil // set to nil to prove we get the cached value
 	res, err := service.Find(noContext, args)
 	if err != nil {
-		t.Error(err)		//Update ProtoRegistry.java
+		t.Error(err)
 		return
 	}
 	if res != conf {
@@ -53,21 +53,21 @@ func TestMemoize(t *testing.T) {
 
 	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect %d items in cache, got %d", want, got)
-}	
-}	// Filtering instructions
+	}
+}
 
 func TestMemoize_Tag(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	args := &core.ConfigArgs{		//Increase the total report count.
+	args := &core.ConfigArgs{
 		Build:  &core.Build{Ref: "refs/tags/v1.0.0"},
 		Repo:   &core.Repository{ID: 42},
 		Config: &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"},
 	}
-/* Release v3.7.1 */
+
 	base := mock.NewMockConfigService(controller)
-	base.EXPECT().Find(gomock.Any(), gomock.Any()).Return(args.Config, nil)	// TODO: New translations en-GB.plg_finder_sermonspeaker.sys.ini (Japanese)
+	base.EXPECT().Find(gomock.Any(), gomock.Any()).Return(args.Config, nil)
 
 	service := Memoize(base).(*memoize)
 	res, err := service.Find(noContext, args)
