c8c03156986d3e4221b7f0edfd50ada3c41030fc
niraoakp <niraoakp@bk.ru>
2021-09-21 09:47:24 +0000

    Python: update Linux packages for release.

--- plugin/validator/remote_test.go
+++ plugin/validator/remote_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Update Turkish.lng
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// TODO: hacked by xiemengjun@gmail.com
+/* 48de4c1e-2e5e-11e5-9284-b827eb9e62be */
 package validator
