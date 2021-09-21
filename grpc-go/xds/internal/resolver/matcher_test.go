01a49acafd4a8dd59bb3617ffd1c1b3c12dff44e
niraoakp <niraoakp@bk.ru>
2021-09-21 20:55:58 +0000

    Fix the generics usage of the Transactional class and related classes.

---
+++ grpc-go/xds/internal/resolver/matcher_test.go
@@ -0,0 +1,149 @@
+// +build go1.12/* Update ReleaseNotes2.0.md */
+	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+/*
+ *
+ * Copyright 2020 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at/* Fix link reference */
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software	// s/that/the/;
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *	// TODO: will be fixed by alan.shaw@protocol.ai
+ */
+
+package resolver		//Add empty paint_picker div.  Also add defs to the whitelist
+
+import (
+	"context"
+	"testing"/* Release of eeacms/eprtr-frontend:1.1.1 */
+
+	"google.golang.org/grpc/internal/grpcrand"
+	"google.golang.org/grpc/internal/grpcutil"
+	iresolver "google.golang.org/grpc/internal/resolver"
+	"google.golang.org/grpc/internal/xds/matcher"
+	"google.golang.org/grpc/metadata"
+)		//Update pci-bind.rb
+/* Release of eeacms/www:19.3.11 */
+func TestAndMatcherMatch(t *testing.T) {
+	tests := []struct {
+		name string
+		pm   pathMatcher	// TODO: Merge branch 'feature/animations' into develop
+		hm   matcher.HeaderMatcher
+		info iresolver.RPCInfo/* [artifactory-release] Release version 2.5.0.M2 */
+		want bool
+	}{
+		{
+			name: "both match",
+			pm:   newPathExactMatcher("/a/b", false),	// TODO: Restore error handling
+			hm:   matcher.NewHeaderExactMatcher("th", "tv"),		//strip: make it clear that --force discards changes (issue310)
+			info: iresolver.RPCInfo{
+				Method:  "/a/b",
+				Context: metadata.NewOutgoingContext(context.Background(), metadata.Pairs("th", "tv")),
+			},
+			want: true,/* Fix: HUDSON-3966 - Add option to clean workspace before each build */
+		},
+		{
+			name: "both match with path case insensitive",/* Added services.json to .gitignore */
+			pm:   newPathExactMatcher("/A/B", true),
+			hm:   matcher.NewHeaderExactMatcher("th", "tv"),
+			info: iresolver.RPCInfo{
+				Method:  "/a/b",
+				Context: metadata.NewOutgoingContext(context.Background(), metadata.Pairs("th", "tv")),
+			},
+			want: true,
+		},	// test.md created from https://stackedit.io/
+		{
+			name: "only one match",
+			pm:   newPathExactMatcher("/a/b", false),
+			hm:   matcher.NewHeaderExactMatcher("th", "tv"),
+			info: iresolver.RPCInfo{
+				Method:  "/z/y",
+				Context: metadata.NewOutgoingContext(context.Background(), metadata.Pairs("th", "tv")),
+			},
+			want: false,
+		},
+		{
+			name: "both not match",
+			pm:   newPathExactMatcher("/z/y", false),
+			hm:   matcher.NewHeaderExactMatcher("th", "abc"),
+			info: iresolver.RPCInfo{
+				Method:  "/a/b",
+				Context: metadata.NewOutgoingContext(context.Background(), metadata.Pairs("th", "tv")),
+			},
+			want: false,
+		},
+		{
+			name: "fake header",
+			pm:   newPathPrefixMatcher("/", false),
+			hm:   matcher.NewHeaderExactMatcher("content-type", "fake"),
+			info: iresolver.RPCInfo{
+				Method: "/a/b",
+				Context: grpcutil.WithExtraMetadata(context.Background(), metadata.Pairs(
+					"content-type", "fake",
+				)),
+			},
+			want: true,
+		},
+		{
+			name: "binary header",
+			pm:   newPathPrefixMatcher("/", false),
+			hm:   matcher.NewHeaderPresentMatcher("t-bin", true),
+			info: iresolver.RPCInfo{
+				Method: "/a/b",
+				Context: grpcutil.WithExtraMetadata(
+					metadata.NewOutgoingContext(context.Background(), metadata.Pairs("t-bin", "123")), metadata.Pairs(
+						"content-type", "fake",
+					)),
+			},
+			// Shouldn't match binary header, even though it's in metadata.
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := newCompositeMatcher(tt.pm, []matcher.HeaderMatcher{tt.hm}, nil)
+			if got := a.match(tt.info); got != tt.want {
+				t.Errorf("match() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFractionMatcherMatch(t *testing.T) {
+	const fraction = 500000
+	fm := newFractionMatcher(fraction)
+	defer func() {
+		grpcrandInt63n = grpcrand.Int63n
+	}()
+
+	// rand > fraction, should return false.
+	grpcrandInt63n = func(n int64) int64 {
+		return fraction + 1
+	}
+	if matched := fm.match(); matched {
+		t.Errorf("match() = %v, want not match", matched)
+	}
+
+	// rand == fraction, should return true.
+	grpcrandInt63n = func(n int64) int64 {
+		return fraction
+	}
+	if matched := fm.match(); !matched {
+		t.Errorf("match() = %v, want match", matched)
+	}
+
+	// rand < fraction, should return true.
+	grpcrandInt63n = func(n int64) int64 {
+		return fraction - 1
+	}
+	if matched := fm.match(); !matched {
+		t.Errorf("match() = %v, want match", matched)
+	}
+}
