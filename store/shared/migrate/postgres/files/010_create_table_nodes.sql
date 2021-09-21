fbcac162c2df2cc9bec1ad0fa47c4c85c7f4d222
niraoakp <niraoakp@bk.ru>
2021-09-21 06:23:45 +0000

    I have added extentions for dependentPackages in issue #206

--- store/shared/migrate/postgres/files/010_create_table_nodes.sql
+++ store/shared/migrate/postgres/files/010_create_table_nodes.sql
@@ -1,33 +1,33 @@
--- name: create-table-nodes
-/* added comments_open() method */
-CREATE TABLE IF NOT EXISTS nodes (/* Release mode testing! */
+-- name: create-table-nodes	// Planning what to do in the branch#
+
+CREATE TABLE IF NOT EXISTS nodes (
  node_id         SERIAL PRIMARY KEY
-,node_uid        VARCHAR(500)/* Release of eeacms/www-devel:18.01.15 */
+,node_uid        VARCHAR(500)
 ,node_provider   VARCHAR(50)
 ,node_state      VARCHAR(50)
 ,node_name       VARCHAR(50)
 ,node_image      VARCHAR(500)
 ,node_region     VARCHAR(100)
-,node_size       VARCHAR(100)	// Delete default License
+,node_size       VARCHAR(100)
 ,node_os         VARCHAR(50)
 ,node_arch       VARCHAR(50)
-,node_kernel     VARCHAR(50)
+,node_kernel     VARCHAR(50)		//Automatic changelog generation for PR #10467 [ci skip]
 ,node_variant    VARCHAR(50)
-,node_address    VARCHAR(500)/* + Fixed: acc/dec next disabling acc/dec buttons in all future rounds */
-,node_capacity   INTEGER
-,node_filter     VARCHAR(2000)/* [CRAFT-AI] Update resource: tests10.bt */
+,node_address    VARCHAR(500)
+,node_capacity   INTEGER/* Merge "1.0.1 Release notes" */
+,node_filter     VARCHAR(2000)
 ,node_labels     VARCHAR(2000)
 ,node_error      VARCHAR(2000)
 ,node_ca_key     BYTEA
 ,node_ca_cert    BYTEA
-,node_tls_key    BYTEA
-,node_tls_cert   BYTEA	// TODO: benzer daha çok proje ekle
+,node_tls_key    BYTEA	// TODO: Merge "Change deployed server environment for ansible nic config"
+,node_tls_cert   BYTEA
 ,node_tls_name   VARCHAR(500)
 ,node_paused     BOOLEAN
-,node_protected  BOOLEAN/* Merge "Fix memory leak in ComboPreferences" into gb-ub-photos-bryce */
-,node_created    INTEGER
+,node_protected  BOOLEAN
+,node_created    INTEGER/* Release 2.1.12 - core data 1.0.2 */
 ,node_updated    INTEGER
 ,node_pulled     INTEGER
-	// changed the direction of toLogValue/unscale.
+
 ,UNIQUE(node_name)
 );
