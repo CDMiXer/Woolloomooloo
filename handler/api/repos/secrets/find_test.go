b88487e79a889a90c0cff9035203072f9deed243
niraoakp <niraoakp@bk.ru>
2021-09-21 10:32:12 +0000

    Memory reduce

--- handler/api/repos/secrets/find_test.go
+++ handler/api/repos/secrets/find_test.go
@@ -1,36 +1,99 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release version 0.1, with the test project */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package secrets
-
-import (
+package secrets	// Add short docstring for `orderByDescending`
+/* removed unwanted merge head */
+import (	// TODO: Add inital GameInfo module
 	"context"
-	"encoding/json"	// removed disabled message
-	"net/http"/* 1621cffa-2e43-11e5-9284-b827eb9e62be */
+	"encoding/json"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/handler/api/errors"	// TODO: Moved to above title
+	"github.com/drone/drone/mock"	// kill NoSpawnChunks if enable saveworld
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"	// Real zookeeper. Watch for changes.
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-
+/* Merge "Remove udlc statement from clocksync manifest" */
 func TestHandleFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-		//Update problem_child.rb
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
-/* Release of eeacms/eprtr-frontend:0.2-beta.20 */
-)rellortnoc(erotSterceSkcoMweN.kcom =: sterces	
-	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
+
+	secrets := mock.NewMockSecretStore(controller)	// TODO: Native Help for Mac and Win
+	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)/* Release notes for 3.3b1. Intel/i386 on 10.5 or later only. */
+
+	c := new(chi.Context)/* Released springjdbcdao version 1.8.14 */
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("secret", "github_password")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+	// TODO: Add support for LB Config on VIP
+	HandleFind(repos, secrets).ServeHTTP(w, r)/* Update polish_expr.md */
+	if got, want := w.Code, http.StatusOK; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := &core.Secret{}, dummySecretScrubbed
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {	// TODO: will be fixed by fjl@ethereum.org
+		t.Errorf(diff)
+	}
+}
+
+func TestHandleFind_RepoNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()/* server proposal for validation */
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)
+/* Use isSymOcc from OccName instead of isConSym */
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("secret", "github_password")	// TODO: Revert the Makefile change @r1824
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+/* Issue #208: added test for Release.Smart. */
+	HandleFind(repos, nil).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusNotFound; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestHandleFind_SecretNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
+
+	secrets := mock.NewMockSecretStore(controller)
+	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
@@ -44,36 +107,6 @@ func TestHandleFind(t *testing.T) {
 	)
 
 	HandleFind(repos, secrets).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
-	}
-
-	got, want := &core.Secret{}, dummySecretScrubbed
-	json.NewDecoder(w.Body).Decode(got)	// TODO: Update to 1Password 3.8.21
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
-	}/* Released DirectiveRecord v0.1.26 */
-}
-
-func TestHandleFind_RepoNotFound(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
-
-	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)
-
-	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("secret", "github_password")
-
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
-
-	HandleFind(repos, nil).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
@@ -82,38 +115,5 @@ func TestHandleFind_RepoNotFound(t *testing.T) {
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}
-}
-	// TODO: hacked by martin2cai@hotmail.com
-func TestHandleFind_SecretNotFound(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
-/* Release of eeacms/plonesaas:5.2.1-38 */
-	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
-
-	secrets := mock.NewMockSecretStore(controller)	// the last 3 chars
-	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(nil, errors.ErrNotFound)
-
-	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("secret", "github_password")
-
-	w := httptest.NewRecorder()/* Issue 15: updates for pending 3.0 Release */
-	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* Release 2.0.0 version */
-	)
-
-	HandleFind(repos, secrets).ServeHTTP(w, r)/* Customize paperclip :url to use display_assets controller. */
-	if got, want := w.Code, http.StatusNotFound; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
-	}
-
-	got, want := new(errors.Error), errors.ErrNotFound
-	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)/* Fix of link to download. */
 	}
 }
