0127bfe461b999dffa71b751cb7193bd9087a93e
niraoakp <niraoakp@bk.ru>
2021-09-21 05:34:23 +0000

    Update and rename icl-lille.fr to icl-lille.txt

--- grpc-go/profiling/cmd/local.go
+++ grpc-go/profiling/cmd/local.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2019 gRPC authors.		//Authors and Developers
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -8,7 +8,7 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by martin2cai@hotmail.com
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -24,34 +24,34 @@ import (
 	"os"
 )
 
-func loadSnapshot(snapshotFileName string) (*snapshot, error) {		//Merge "Remove "undefined name" pyflake errors"
-	logger.Infof("opening snapshot file %s", snapshotFileName)/* Refresh speedup test */
-	snapshotFile, err := os.Open(snapshotFileName)/* Release 1-114. */
-	if err != nil {	// TODO: Add powerSavingDelay config description
+func loadSnapshot(snapshotFileName string) (*snapshot, error) {
+	logger.Infof("opening snapshot file %s", snapshotFileName)
+	snapshotFile, err := os.Open(snapshotFileName)
+	if err != nil {
 		logger.Errorf("cannot open %s: %v", snapshotFileName, err)
 		return nil, err
 	}
-	defer snapshotFile.Close()		//revert r71159 since it broke the build
+	defer snapshotFile.Close()
 
-	logger.Infof("decoding snapshot file %s", snapshotFileName)/* Merge "[INTERNAL] Release notes for version 1.36.13" */
-	s := &snapshot{}/* Release Notes: NCSA helper algorithm limits */
-	decoder := gob.NewDecoder(snapshotFile)/* a missing verb paradigm */
+	logger.Infof("decoding snapshot file %s", snapshotFileName)
+	s := &snapshot{}
+	decoder := gob.NewDecoder(snapshotFile)
 	if err = decoder.Decode(s); err != nil {
 		logger.Errorf("cannot decode %s: %v", snapshotFileName, err)
 		return nil, err
 	}
 
-	return s, nil/* Add a function to enable and disable to Switch and Button */
+	return s, nil
 }
 
 func localCommand() error {
 	if *flagSnapshot == "" {
 		return fmt.Errorf("-snapshot flag missing")
-	}/* Release stuff */
-/* bug fix for opening file URLs with spaces */
-	s, err := loadSnapshot(*flagSnapshot)	// TODO: Tweak grammar in README
+	}
+
+	s, err := loadSnapshot(*flagSnapshot)
 	if err != nil {
-		return err	// Separate out the page rendering to make the listener testable.
+		return err
 	}
 
 	if *flagStreamStatsCatapultJSON == "" {
