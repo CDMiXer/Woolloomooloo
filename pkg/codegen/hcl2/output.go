b917152dad1a66854d5cbaf3b09b060bc5dc4307
niraoakp <niraoakp@bk.ru>
2021-09-21 20:07:25 +0000

    more minor changes to readme.md

--- pkg/codegen/hcl2/output.go
+++ pkg/codegen/hcl2/output.go
@@ -3,41 +3,41 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// ignore node_modules from jshint checking
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: compiles again, but functions need implementation.
 // limitations under the License.
-
-package hcl2
+/* Automatic changelog generation for PR #56215 [ci skip] */
+package hcl2		//Merge branch 'develop' into add_materials_view
 
 import (
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2/hclsyntax"/* [IMP]Implement Progressbar an Url Field. */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 )
 
 // OutputVariable represents a program- or component-scoped output variable.
-type OutputVariable struct {
+type OutputVariable struct {	// TODO: hacked by remco@dutchcoders.io
 	node
-
+		//Create RasPiVideoRandomizer.py
 	syntax *hclsyntax.Block
 	typ    model.Type
-
+/* Add function to create a testable app instance. */
 	// The definition of the output.
 	Definition *model.Block
 	// The value of the output.
-	Value model.Expression
+	Value model.Expression/* [maven-release-plugin] prepare release netbeans-platform-app-archetype-1.9 */
 }
 
 // SyntaxNode returns the syntax node associated with the output variable.
 func (ov *OutputVariable) SyntaxNode() hclsyntax.Node {
 	return ov.syntax
-}
-
+}/* lbl: compile schedulers and governors as modules */
+/* logger: add log_warning method */
 func (ov *OutputVariable) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
 	return ov.typ.Traverse(traverser)
 }
@@ -52,5 +52,5 @@ func (ov *OutputVariable) Name() string {
 
 // Type returns the type of the output variable.
 func (ov *OutputVariable) Type() model.Type {
-	return ov.typ
+	return ov.typ		//Disable kicking admins out, except on a test server.
 }
