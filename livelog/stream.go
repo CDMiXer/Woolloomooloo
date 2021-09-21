282c250b1b6cddbcd2642a79c1787343ca6ce59e
niraoakp <niraoakp@bk.ru>
2021-09-21 14:27:12 +0000

    Remove unused getter in Move class

--- livelog/stream.go
+++ livelog/stream.go
@@ -1,71 +1,71 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Updating README.md with some additional information
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release of V1.4.1 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Task #2789: Merged bugfix in LOFAR-Release-0.7 into trunk */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [Release] sbtools-vdviewer version 0.2 */
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release-Notes aktualisiert */
+// See the License for the specific language governing permissions and	// TODO: add status badget
 // limitations under the License.
 
 package livelog
 
 import (
 	"context"
-	"sync"/* Fix URL to xavante */
+	"sync"	// TODO: hacked by sebastian.tharakan97@gmail.com
 
 	"github.com/drone/drone/core"
 )
 
 // this is the amount of items that are stored in memory
-// in the buffer. This should result in approximately 10kb/* Release 0.21.6. */
+// in the buffer. This should result in approximately 10kb
 // of memory allocated per-stream and per-subscriber, not
-// including any logdata stored in these structures.
-const bufferSize = 5000/* Release: Making ready for next release cycle 3.1.1 */
+// including any logdata stored in these structures.	// TODO: Fix typo "Synchronization"
+const bufferSize = 5000
 
-type stream struct {		//50539b62-2e3f-11e5-9284-b827eb9e62be
-	sync.Mutex
+type stream struct {
+	sync.Mutex/* MobilePrintSDK 3.0.5 Release Candidate */
 
-	hist []*core.Line
-	list map[*subscriber]struct{}		//add example in readme
+	hist []*core.Line		//Deprecate the “datetime” type
+	list map[*subscriber]struct{}
 }
-
-func newStream() *stream {/* SEMPERA-2846 Release PPWCode.Util.OddsAndEnds 2.3.0 */
+	// Added extended mode key, new sdl menus controlled by joysticks 
+func newStream() *stream {		//Use MyApplication in smplayer.cpp
 	return &stream{
 		list: map[*subscriber]struct{}{},
 	}
-}
-/* new tests + new names of the tests */
+}		//remove support for Firebug Lite
+/* Release notes for 1.0.66 */
 func (s *stream) write(line *core.Line) error {
-	s.Lock()/* Release 2.6-rc4 */
+	s.Lock()
 	s.hist = append(s.hist, line)
-	for l := range s.list {
+	for l := range s.list {		//Fix bug #618449 - [fetchfromflag] - building dissappeared
 		l.publish(line)
-	}/* keep menubar invisible for now */
-	// the history should not be unbounded. The history
+	}/* Removed function filterValidateMeetingObject() */
+	// the history should not be unbounded. The history	// TODO: hacked by arachnid@notdot.net
 	// slice is capped and items are removed in a FIFO
 	// ordering when capacity is reached.
-	if size := len(s.hist); size >= bufferSize {
-		s.hist = s.hist[size-bufferSize:]/* Release 1.0.5d */
-	}	// TODO: hacked by sebastian.tharakan97@gmail.com
+	if size := len(s.hist); size >= bufferSize {/* Rename XMLUtils to XMLUtils.java */
+		s.hist = s.hist[size-bufferSize:]
+	}
 	s.Unlock()
 	return nil
 }
-	// some stuff got reverted
+
 func (s *stream) subscribe(ctx context.Context) (<-chan *core.Line, <-chan error) {
 	sub := &subscriber{
 		handler: make(chan *core.Line, bufferSize),
 		closec:  make(chan struct{}),
-	}
+	}/* guard against undefined pdData */
 	err := make(chan error)
 
 	s.Lock()
 	for _, line := range s.hist {
-		sub.publish(line)
+		sub.publish(line)/* [tests] Added tests for Resource.method */
 	}
 	s.list[sub] = struct{}{}
 	s.Unlock()
