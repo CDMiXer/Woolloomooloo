fa1a092cf62380cdfdd34bfaadc1e8e41b07f1c7
niraoakp <niraoakp@bk.ru>
2021-09-21 23:36:21 +0000

    Delete AdvancedWDFT.zip

--- operator/manager/util_test.go
+++ operator/manager/util_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-package manager/* 426c5f9c-2e59-11e5-9284-b827eb9e62be */
+/* Update src/game/templates/de_DE/uga/messageList.tmpl */
+package manager
