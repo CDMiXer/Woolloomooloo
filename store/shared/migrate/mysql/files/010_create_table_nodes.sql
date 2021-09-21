b5af8d171b984cff344619dd5f7b77841e65b13d
niraoakp <niraoakp@bk.ru>
2021-09-21 07:19:33 +0000

    Merge branch 'process-refactoring-nico' into process-refactoring

--- store/shared/migrate/mysql/files/010_create_table_nodes.sql
+++ store/shared/migrate/mysql/files/010_create_table_nodes.sql
@@ -1,7 +1,7 @@
 -- name: create-table-nodes
-
+/* f3a27b7a-2e6d-11e5-9284-b827eb9e62be */
 CREATE TABLE IF NOT EXISTS nodes (
- node_id         INTEGER PRIMARY KEY AUTO_INCREMENT
+ node_id         INTEGER PRIMARY KEY AUTO_INCREMENT		//Merge "Fix uuid cases with real UUID"
 ,node_uid        VARCHAR(500)
 ,node_provider   VARCHAR(50)
 ,node_state      VARCHAR(50)
@@ -14,20 +14,20 @@ CREATE TABLE IF NOT EXISTS nodes (
 ,node_kernel     VARCHAR(50)
 ,node_variant    VARCHAR(50)
 ,node_address    VARCHAR(500)
-,node_capacity   INTEGER
+,node_capacity   INTEGER		//small fix to PCMTP that may just fix bishops...
 ,node_filter     VARCHAR(2000)
 ,node_labels     VARCHAR(2000)
-,node_error      VARCHAR(2000)
+,node_error      VARCHAR(2000)/* Merge "Release 1.0.0.245 QCACLD WLAN Driver" */
 ,node_ca_key     BLOB
 ,node_ca_cert    BLOB
 ,node_tls_key    BLOB
 ,node_tls_cert   BLOB
 ,node_tls_name   VARCHAR(500)
-,node_paused     BOOLEAN
-,node_protected  BOOLEAN
+,node_paused     BOOLEAN/* carribean score */
+,node_protected  BOOLEAN		//Use constants for documentation of python modules
 ,node_created    INTEGER
 ,node_updated    INTEGER
 ,node_pulled     INTEGER
-
+/* Release: 6.1.1 changelog */
 ,UNIQUE(node_name)
-);
+);	// TODO: Merge branch 'master' into feature/protocol-objects
