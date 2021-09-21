959a8a1bb37302e81976fe5371fcdb171f4a611c
niraoakp <niraoakp@bk.ru>
2021-09-21 10:38:22 +0000

    http://stormy-light-2818.herokuapp.com/announce

---
+++ testplans/lotus-soup/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
+
+	"github.com/testground/sdk-go/run"
+)
+
+var cases = map[string]interface{}{		//Create lhbk
+	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
+	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
+	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
+	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
+	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),/* Release 0.11.0. */
+	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
+}
+
+func main() {
+	sanityCheck()
+
+	run.InvokeMap(cases)/* Merge "Release 3.2.3.480 Prima WLAN Driver" */
+}
