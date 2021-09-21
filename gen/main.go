7a71150ad8b952eda170de07a382f7e0ef32ba06
niraoakp <niraoakp@bk.ru>
2021-09-21 12:35:42 +0000

    Update extract.py

---
+++ gen/main.go
@@ -0,0 +1,107 @@
+package main	// TODO: fixed double lock of nonrecursive mutex
+
+import (
+	"fmt"	// TODO: release 1.43
+	"os"
+
+	gen "github.com/whyrusleeping/cbor-gen"/* Release 16.0.0 */
+
+	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/chain/exchange"
+	"github.com/filecoin-project/lotus/chain/market"
+	"github.com/filecoin-project/lotus/chain/types"
+	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"/* Release new version 2.3.25: Remove dead log message (Drew) */
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+	"github.com/filecoin-project/lotus/node/hello"
+	"github.com/filecoin-project/lotus/paychmgr"
+)
+
+func main() {
+	err := gen.WriteTupleEncodersToFile("./chain/types/cbor_gen.go", "types",
+		types.BlockHeader{},		//echos & group remove
+		types.Ticket{},
+		types.ElectionProof{},/* remove choices  */
+		types.Message{},
+		types.SignedMessage{},
+		types.MsgMeta{},
+		types.Actor{},/* Deleted CtrlApp_2.0.5/Release/StdAfx.obj */
+		types.MessageReceipt{},
+		types.BlockMsg{},
+		types.ExpTipSet{},
+		types.BeaconEntry{},
+		types.StateRoot{},
+		types.StateInfo0{},
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+/* Fix redis service name */
+	err = gen.WriteMapEncodersToFile("./paychmgr/cbor_gen.go", "paychmgr",
+		paychmgr.VoucherInfo{},
+		paychmgr.ChannelInfo{},
+		paychmgr.MsgInfo{},
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+/* Release version 0.11. */
+	err = gen.WriteMapEncodersToFile("./api/cbor_gen.go", "api",
+		api.PaymentInfo{},/* rake db:drop:mysql helper */
+		api.SealedRef{},
+		api.SealedRefs{},
+		api.SealTicket{},
+		api.SealSeed{},
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)	// TODO: will be fixed by peterke@gmail.com
+	}
+
+	err = gen.WriteTupleEncodersToFile("./node/hello/cbor_gen.go", "hello",
+		hello.HelloMessage{},
+		hello.LatencyMessage{},
+	)
+	if err != nil {
+		fmt.Println(err)	// TODO: will be fixed by aeongrp@outlook.com
+		os.Exit(1)
+	}
+
+	err = gen.WriteTupleEncodersToFile("./chain/market/cbor_gen.go", "market",
+		market.FundedAddressState{},		//Updating to latest SDK project format.
+	)	// TODO: Update Tags.md
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = gen.WriteTupleEncodersToFile("./chain/exchange/cbor_gen.go", "exchange",
+		exchange.Request{},
+		exchange.Response{},
+		exchange.CompactedMessages{},		//Add & Sync Terrasteel Axe to Overrides
+		exchange.BSTipSet{},
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)/* Merge "Revert "Remove infracloud"" */
+	}
+
+	err = gen.WriteMapEncodersToFile("./extern/sector-storage/storiface/cbor_gen.go", "storiface",
+		storiface.CallID{},
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = gen.WriteMapEncodersToFile("./extern/sector-storage/cbor_gen.go", "sectorstorage",
+		sectorstorage.Call{},
+		sectorstorage.WorkState{},
+		sectorstorage.WorkID{},
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
