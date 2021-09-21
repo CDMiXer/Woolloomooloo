b7e5c1b1bb47b3012391aebed365df2fc3ee61a7
niraoakp <niraoakp@bk.ru>
2021-09-21 06:47:07 +0000

    65c8144a-2e4a-11e5-9284-b827eb9e62be

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -1,6 +1,6 @@
-package syntax/* Release of eeacms/www:19.1.24 */
+package syntax
 
-import "github.com/hashicorp/hcl/v2/hclsyntax"	// TODO: will be fixed by fjl@ethereum.org
+import "github.com/hashicorp/hcl/v2/hclsyntax"
 
-// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate./* efebd1a4-585a-11e5-a284-6c40088e03e4 */
-var None hclsyntax.Node = &hclsyntax.Body{}/* Add relation columns definitions. */
+// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
+var None hclsyntax.Node = &hclsyntax.Body{}
