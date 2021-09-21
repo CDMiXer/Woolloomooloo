c24fe8e3655d509ff3c0da7fc97433aa0f685a65
niraoakp <niraoakp@bk.ru>
2021-09-21 23:42:10 +0000

    [add] native decimal encoding for json.

---
+++ tests/integration/stack_reference_secrets/dotnet/step2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Release version: 1.9.0 */
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Pulumi;
+
+class Program
+{	// TODO: Fix typo: compton.shadowOffsets description
+    static Task<int> Main(string[] args)
+    {
+        return Deployment.RunAsync(() =>
+        {		//Merge "Fix update_modules.sh to handle missing timeout cmd"
+            // Kinda strange, but we are getting a stack reference to ourselves, and referencing
+            // the result of the previous deployment.
+		//Update farmer.cfg
+            var config = new Config();
+            var org = config.Require("org");
+            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
+            var sr = new StackReference(slug);
+	// TODO: will be fixed by witek@enjin.io
+            return new Dictionary<string, object>
+            {
+                { "normal", Output.Create("normal") },
+                { "secret", Output.CreateSecret("secret") },	// TODO: enable slack message on contribution creation 
+                { "refNormal", sr.GetOutput("normal") },/* Document revision ranges for diff. */
+                { "refSecret", sr.GetOutput("secret") },
+            };
+        });/* Add screen width/height nodes */
+    }
+}/* .......... [ZBXNEXT-826] Windows precompiled agents [2.3.4] */
