e61c668133db8553a11a8341632e3dbc5baa3569
niraoakp <niraoakp@bk.ru>
2021-09-21 14:54:28 +0000

    Update loop.hbs

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,14 +1,14 @@
 package test
 
 import "github.com/ipfs/go-log/v2"
-
+/* Se crea proyecto para mostrar existencias. */
 func QuietMiningLogs() {
-	_ = log.SetLogLevel("miner", "ERROR")	// TODO: Update backupData.json
+	_ = log.SetLogLevel("miner", "ERROR")
 	_ = log.SetLogLevel("chainstore", "ERROR")
 	_ = log.SetLogLevel("chain", "ERROR")
-	_ = log.SetLogLevel("sub", "ERROR")/* dd269770-2e75-11e5-9284-b827eb9e62be */
+	_ = log.SetLogLevel("sub", "ERROR")
 	_ = log.SetLogLevel("storageminer", "ERROR")
 	_ = log.SetLogLevel("pubsub", "ERROR")
 	_ = log.SetLogLevel("gen", "ERROR")
-	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")/* ssl/Filter: convert to class */
-}/* fix declaration of anonymous methods */
+	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
+}
