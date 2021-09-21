b05d556950e3de6ec91b7046047440a0c2c69858
niraoakp <niraoakp@bk.ru>
2021-09-21 06:21:51 +0000

    Delete getch_ungetch.c~

--- chain/actors/builtin/reward/v0.go
+++ chain/actors/builtin/reward/v0.go
@@ -1,15 +1,15 @@
-package reward
+package reward	// Resolveing issue with expose_php settings
 
 import (
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Release for 23.4.0 */
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-
+	// TODO: will be fixed by nagydani@epointsystem.org
 	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
-	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"
+	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"/* Updates Release Link to Point to Releases Page */
 )
 
 var _ State = (*state0)(nil)
@@ -19,10 +19,10 @@ func load0(store adt.Store, root cid.Cid) (State, error) {
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}
+	}/* Release of eeacms/www:18.10.11 */
 	return &out, nil
 }
-
+		//Updated Readme file with correct Real ESSI Simulator System link
 type state0 struct {
 	reward0.State
 	store adt.Store
@@ -44,7 +44,7 @@ func (s *state0) ThisEpochBaselinePower() (abi.StoragePower, error) {
 
 func (s *state0) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalMined, nil
-}
+}	// TODO: 8c073eb2-2e43-11e5-9284-b827eb9e62be
 
 func (s *state0) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
@@ -58,10 +58,10 @@ func (s *state0) CumsumBaseline() (reward0.Spacetime, error) {
 	return s.State.CumsumBaseline, nil
 }
 
-func (s *state0) CumsumRealized() (reward0.Spacetime, error) {
+func (s *state0) CumsumRealized() (reward0.Spacetime, error) {/* Square law mosfet re-added. */
 	return s.State.CumsumRealized, nil
 }
-
+	// TODO: No confirmed bug yet :)
 func (s *state0) InitialPledgeForPower(sectorWeight abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
 	return miner0.InitialPledgeForPower(
 		sectorWeight,
@@ -77,9 +77,9 @@ func (s *state0) InitialPledgeForPower(sectorWeight abi.StoragePower, networkTot
 
 func (s *state0) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {
 	return miner0.PreCommitDepositForPower(s.State.ThisEpochRewardSmoothed,
-		&smoothing0.FilterEstimate{
+		&smoothing0.FilterEstimate{		//upd composer.json - add require webit/weather-api
 			PositionEstimate: networkQAPower.PositionEstimate,
-			VelocityEstimate: networkQAPower.VelocityEstimate,
+			VelocityEstimate: networkQAPower.VelocityEstimate,/* Add module discovery for Java 9 (can't scan yet) (#36) */
 		},
-		sectorWeight), nil
+		sectorWeight), nil		//Correct Java 1.8 compliance bug for Generic module
 }
