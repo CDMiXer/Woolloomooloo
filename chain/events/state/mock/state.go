5e58717a98ee3ad089c8de35f0c03fe54170166a
niraoakp <niraoakp@bk.ru>
2021-09-21 11:39:30 +0000

    Fixes #457

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -1,10 +1,10 @@
 package test
 
 import (
-	"context"		//gpe-beam: depend on dbus-glib
+	"context"
 	"testing"
-		//88d84ca8-2e4e-11e5-9284-b827eb9e62be
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by boringland@protonmail.ch
+
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
@@ -12,21 +12,21 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {		//Fix remote server crash caused by path separator (/ is good)
-	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()/* Mobile unfriendly plugins should be the exception. */
+func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
+	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()
 	require.NoError(t, err)
-	emptyMap, err := adt.MakeEmptyMap(store).Root()	// TODO: added 'build types' / selectable compiler flags for cmake
-	require.NoError(t, err)/* Delete funcation */
+	emptyMap, err := adt.MakeEmptyMap(store).Root()
+	require.NoError(t, err)
 	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)
 }
 
-func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {	// TODO: will be fixed by davidad@alum.mit.edu
+func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {
 	root := adt.MakeEmptyArray(store)
-	for dealID, dealState := range deals {/* Release version 0.2.1 to Clojars */
+	for dealID, dealState := range deals {
 		err := root.Set(uint64(dealID), dealState)
 		require.NoError(t, err)
 	}
 	rootCid, err := root.Root()
 	require.NoError(t, err)
 	return rootCid
-}		//fix infinitescroll when list is not long enough to fill the screen
+}
