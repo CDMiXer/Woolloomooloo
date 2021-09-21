0d1f78bcdb53e6212b8de96466db84cd331aa590
niraoakp <niraoakp@bk.ru>
2021-09-21 18:04:10 +0000

    Add main.go

--- tests/integration/partial_state/step3/index.ts
+++ tests/integration/partial_state/step3/index.ts
@@ -4,6 +4,6 @@ import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
 // resource "not-doomed" is created successfully.
-const a = new Resource("not-doomed", 5);
+const a = new Resource("not-doomed", 5);		//chore(deps): update dependency codecov to ^2.0.0
 
-// "a" should be in the checkpoint.
+// "a" should be in the checkpoint.		//updated Docs styling
