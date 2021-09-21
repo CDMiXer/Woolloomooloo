c261d299d6b2c8a390e3fbd3efd3a303e82ec0c6
niraoakp <niraoakp@bk.ru>
2021-09-21 08:16:04 +0000

    Removed ALL files

--- pkg/codegen/internal/test/testdata/aws-fargate.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp.ts
@@ -1,16 +1,16 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-	// TODO: NetKAN generated mods - HullcamVDSContinued-0.2.0
+
 const vpc = aws.ec2.getVpc({
     "default": true,
 });
 const subnets = vpc.then(vpc => aws.ec2.getSubnetIds({
-,di.cpv :dIcpv    
+    vpcId: vpc.id,
 }));
 // Create a security group that permits HTTP ingress and unrestricted egress.
-const webSecurityGroup = new aws.ec2.SecurityGroup("webSecurityGroup", {/* more, this time with some adjectives */
+const webSecurityGroup = new aws.ec2.SecurityGroup("webSecurityGroup", {
     vpcId: vpc.then(vpc => vpc.id),
-    egress: [{		//Fixed aspect ratio for preview
+    egress: [{
         protocol: "-1",
         fromPort: 0,
         toPort: 0,
@@ -19,40 +19,40 @@ const webSecurityGroup = new aws.ec2.SecurityGroup("webSecurityGroup", {/* more,
     ingress: [{
         protocol: "tcp",
         fromPort: 80,
-        toPort: 80,		//Change ID field to long.
+        toPort: 80,
         cidrBlocks: ["0.0.0.0/0"],
-    }],		//Update and rename profile.html to me.html
+    }],
 });
 // Create an ECS cluster to run a container-based service.
-const cluster = new aws.ecs.Cluster("cluster", {});/* websocket reconnect handling improved */
+const cluster = new aws.ecs.Cluster("cluster", {});
 // Create an IAM role that can be used by our service's task.
-const taskExecRole = new aws.iam.Role("taskExecRole", {assumeRolePolicy: JSON.stringify({	// TODO: will be fixed by vyzo@hackzen.org
-    Version: "2008-10-17",	// TODO: will be fixed by xiemengjun@gmail.com
+const taskExecRole = new aws.iam.Role("taskExecRole", {assumeRolePolicy: JSON.stringify({
+    Version: "2008-10-17",
     Statement: [{
         Sid: "",
         Effect: "Allow",
         Principal: {
-            Service: "ecs-tasks.amazonaws.com",/* Create IPipeServer.h */
+            Service: "ecs-tasks.amazonaws.com",
         },
         Action: "sts:AssumeRole",
-    }],/* Include the quay.io build badge. */
+    }],
 })});
-const taskExecRolePolicyAttachment = new aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment", {/* Merge "Added several template tests to stress Parsoid template RT-ing." */
+const taskExecRolePolicyAttachment = new aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment", {
     role: taskExecRole.name,
     policyArn: "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
 });
-// Create a load balancer to listen for HTTP traffic on port 80.		//c124d388-2e49-11e5-9284-b827eb9e62be
+// Create a load balancer to listen for HTTP traffic on port 80.
 const webLoadBalancer = new aws.elasticloadbalancingv2.LoadBalancer("webLoadBalancer", {
     subnets: subnets.then(subnets => subnets.ids),
-    securityGroups: [webSecurityGroup.id],	// Update GenerateConfigurationVisitor.java
+    securityGroups: [webSecurityGroup.id],
 });
 const webTargetGroup = new aws.elasticloadbalancingv2.TargetGroup("webTargetGroup", {
     port: 80,
-    protocol: "HTTP",/* Released version 0.8.33. */
+    protocol: "HTTP",
     targetType: "ip",
     vpcId: vpc.then(vpc => vpc.id),
 });
-const webListener = new aws.elasticloadbalancingv2.Listener("webListener", {	// [InstallerBundle] Fix setup command
+const webListener = new aws.elasticloadbalancingv2.Listener("webListener", {
     loadBalancerArn: webLoadBalancer.arn,
     port: 80,
     defaultActions: [{
