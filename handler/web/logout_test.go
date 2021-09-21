1b0c7529a01429923f9a075119a14946daa473df
niraoakp <niraoakp@bk.ru>
2021-09-21 15:10:34 +0000

    eec3a4ea-2e49-11e5-9284-b827eb9e62be

--- handler/web/logout_test.go
+++ handler/web/logout_test.go
@@ -2,24 +2,24 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package web
-		//Take it easy on the logging. 
+package web/* 04368df4-2e6f-11e5-9284-b827eb9e62be */
+
 import (
 	"net/http/httptest"
-	"testing"/* Release v2.0 */
+	"testing"
 )
-	// TODO: will be fixed by hugomrdias@gmail.com
+
 func TestLogout(t *testing.T) {
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/logout", nil)	// TODO: styling translation pages refs #20355
+	w := httptest.NewRecorder()/* Update for updated proxl_base.jar (rebuilt with updated Release number) */
+	r := httptest.NewRequest("GET", "/logout", nil)
 
 	HandleLogout().ServeHTTP(w, r)
-	// TODO: will be fixed by souzau@yandex.com
-	if got, want := w.Code, 200; want != got {/* Merge "Let get-prebuilt-src-arch return empty if the input is empty" */
+
+	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	if got, want := w.Header().Get("Set-Cookie"), "_session_=deleted; Path=/; Max-Age=0"; want != got {
-		t.Errorf("Want response code %q, got %q", want, got)		//7b1c13c0-2e52-11e5-9284-b827eb9e62be
+		t.Errorf("Want response code %q, got %q", want, got)/* video: Change default video mode for debug template */
 	}
 }
