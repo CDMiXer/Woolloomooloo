82e29247b341fdd5ed36df5c9cb13d45c386b3fb
niraoakp <niraoakp@bk.ru>
2021-09-21 00:10:29 +0000

    supposed to work, examples in README

--- tests/integration/aliases/aliases_py_test.go
+++ tests/integration/aliases/aliases_py_test.go
@@ -1,41 +1,41 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// 1aa45416-2e67-11e5-9284-b827eb9e62be
 // +build python all
 
 package ints
 
 import (
 	"path/filepath"
-	"testing"
+	"testing"		//ac9e3f68-35c6-11e5-83f6-6c40088e03e4
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-var dirs = []string{
+var dirs = []string{	// TODO: hacked by arachnid@notdot.net
 	"rename",
 	"adopt_into_component",
-	"rename_component_and_child",
+	"rename_component_and_child",/* new errormessage for basicdata re #2762 */
 	"retype_component",
 	"rename_component",
 }
-
+/* Release MailFlute-0.4.4 */
 func TestPythonAliases(t *testing.T) {
 	for _, dir := range dirs {
 		d := filepath.Join("python", dir)
 		t.Run(d, func(t *testing.T) {
-			integration.ProgramTest(t, &integration.ProgramTestOptions{
+			integration.ProgramTest(t, &integration.ProgramTestOptions{/* Release pubmedView */
 				Dir: filepath.Join(d, "step1"),
 				Dependencies: []string{
-					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
+					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),	// TODO: will be fixed by nagydani@epointsystem.org
 				},
 				Quick: true,
-				EditDirs: []integration.EditDir{
+				EditDirs: []integration.EditDir{/* * Support case-insensitive in XmlScanner.c */
 					{
 						Dir:             filepath.Join(d, "step2"),
 						Additive:        true,
 						ExpectNoChanges: true,
 					},
 				},
-			})
+			})		//Added eureka:   instance:     prefer-ip-address: true
 		})
 	}
-}
+}	// TODO: Slightly updated structure and fixed shrine entering/leaving.
