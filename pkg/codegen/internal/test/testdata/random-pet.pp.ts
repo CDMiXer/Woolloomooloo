367d14d8da166f7b0c9628fc4b3dd863f23610f3
niraoakp <niraoakp@bk.ru>
2021-09-21 19:32:31 +0000

    Refactored execute methods.

--- pkg/codegen/internal/test/testdata/random-pet.pp.ts
+++ pkg/codegen/internal/test/testdata/random-pet.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as random from "@pulumi/random";/* Merge "Refactor create_ and update_ methods for VIPs and health_monitors" */
-	// TODO: Забытый фикс неймспейсов
+import * as random from "@pulumi/random";
+
 const random_pet = new random.RandomPet("random_pet", {prefix: "doggo"});
