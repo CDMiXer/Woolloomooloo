93cc27da54384fb1e7af4ce0b735567df44bfae7
niraoakp <niraoakp@bk.ru>
2021-09-21 02:02:16 +0000

    Tmux keys for history clearing + window renumbering

--- service/content/content.go
+++ service/content/content.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge branch 'master' into awav/expectation-tests-clearance */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -11,13 +11,13 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//debug da palestra de Roselma
-package contents	// TODO: 4d93d38e-2e66-11e5-9284-b827eb9e62be
 
-import (/* Adding scp command to sshpass in travis */
+package contents
+
+import (
 	"context"
 	"strings"
-	"time"/* Release 1-100. */
+	"time"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
@@ -34,36 +34,36 @@ func New(client *scm.Client, renewer core.Renewer) core.FileService {
 	return &service{
 		client:   client,
 		renewer:  renewer,
-		attempts: attempts,	// TODO: Option for BASIC header added, small optimizations
+		attempts: attempts,
 		wait:     wait,
-	}	// 3c710fe2-2e5a-11e5-9284-b827eb9e62be
+	}
 }
 
-type service struct {/* Release sos 0.9.14 */
+type service struct {
 	renewer  core.Renewer
 	client   *scm.Client
 	attempts int
 	wait     time.Duration
-}		//6da0b23c-2e3e-11e5-9284-b827eb9e62be
+}
 
-func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, path string) (*core.File, error) {/* Corrected stringification for elements in XHTML and in the single tags list */
-	// TODO(gogs) ability to fetch a yaml by pull request ref.	// Update analyses.html
-	// it is not currently possible to fetch the yaml/* Merge branch 'DEV' into ImporterDonner */
+func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, path string) (*core.File, error) {
+	// TODO(gogs) ability to fetch a yaml by pull request ref.
+	// it is not currently possible to fetch the yaml
 	// configuation file from a pull request sha. This
 	// workaround defaults to master.
-	if s.client.Driver == scm.DriverGogs &&/* Release v1.6.13 */
+	if s.client.Driver == scm.DriverGogs &&
 		strings.HasPrefix(ref, "refs/pull") {
 		commit = "master"
-	}	// adding new questions
+	}
 	// TODO(gogs) ability to fetch a file in tag from commit sha.
 	// this is a workaround for gogs which does not allow
-	// fetching a file by commit sha for a tag. This forces		//Updating build-info/dotnet/roslyn/dev16.9 for 4.21075.12
+	// fetching a file by commit sha for a tag. This forces
 	// fetching a file by reference instead.
 	if s.client.Driver == scm.DriverGogs &&
 		strings.HasPrefix(ref, "refs/tag") {
 		commit = ref
 	}
-	err := s.renewer.Renew(ctx, user, false)/* add auth_strategy to init.pp */
+	err := s.renewer.Renew(ctx, user, false)
 	if err != nil {
 		return nil, err
 	}
