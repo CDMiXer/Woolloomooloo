91e1adb00cddb3b798e9d7284341a24b58126e8f
niraoakp <niraoakp@bk.ru>
2021-09-21 22:14:38 +0000

    Initial specs for Debian

--- plugin/registry/external_test.go
+++ plugin/registry/external_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file.	// TODO: hacked by steven@stebalien.com
+/* Merge "Add 7 missed meters in Resource Usage Panel." */
 // +build !oss
 
-package registry	// TODO: will be fixed by nagydani@epointsystem.org
+package registry		//readme + cli prompt copy update
