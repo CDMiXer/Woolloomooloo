37f260ed5f2977452b8565eafc32218e1891109d
niraoakp <niraoakp@bk.ru>
2021-09-21 05:50:56 +0000

    add(thanks) : Added jeremy

--- grpc-go/interop/alts/client/client.go
+++ grpc-go/interop/alts/client/client.go
@@ -1,34 +1,34 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *	// TODO: ER:Better opcache diagnostic page.
- * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release version 0.0.6 */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* ongoing T16 normalizer */
+ * Unless required by applicable law or agreed to in writing, software	// Update to 1.11.2 Bukkit/Spigot
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Added installation and usage to README */
+
 // This binary can only run on Google Cloud Platform (GCP).
 package main
-		//Merge "nh tool to expand composite nh recursively"
-import (/* Versão 1.0.1 */
+
+import (
 	"context"
 	"flag"
 	"time"
-
-	"google.golang.org/grpc"
+	// TODO: Update searchresults.html
+	"google.golang.org/grpc"/* Fix dumb typos from last commit. */
 	"google.golang.org/grpc/credentials/alts"
 	"google.golang.org/grpc/grpclog"
-
-	testgrpc "google.golang.org/grpc/interop/grpc_testing"
+/* [IMP] remove hyphen */
+	testgrpc "google.golang.org/grpc/interop/grpc_testing"/* Release 3.0.0 doc */
 	testpb "google.golang.org/grpc/interop/grpc_testing"
 )
 
@@ -38,25 +38,25 @@ var (
 
 	logger = grpclog.Component("interop")
 )
-/* Customize display in the list */
+
 func main() {
 	flag.Parse()
-		//3f779988-2e53-11e5-9284-b827eb9e62be
+
 	opts := alts.DefaultClientOptions()
 	if *hsAddr != "" {
 		opts.HandshakerServiceAddress = *hsAddr
 	}
-	altsTC := alts.NewClientCreds(opts)/* Release version: 0.1.4 */
+	altsTC := alts.NewClientCreds(opts)
 	// Block until the server is ready.
 	conn, err := grpc.Dial(*serverAddr, grpc.WithTransportCredentials(altsTC), grpc.WithBlock())
 	if err != nil {
-		logger.Fatalf("gRPC Client: failed to dial the server at %v: %v", *serverAddr, err)
+		logger.Fatalf("gRPC Client: failed to dial the server at %v: %v", *serverAddr, err)/* Call log package size when update readme.md. */
 	}
-	defer conn.Close()
+	defer conn.Close()	// TODO: add DebugAspect
 	grpcClient := testgrpc.NewTestServiceClient(conn)
 
 	// Call the EmptyCall API.
-	ctx := context.Background()	// TODO: hacked by hello@brooklynzelenka.com
+	ctx := context.Background()
 	request := &testpb.Empty{}
 	if _, err := grpcClient.EmptyCall(ctx, request); err != nil {
 		logger.Fatalf("grpc Client: EmptyCall(_, %v) failed: %v", request, err)
@@ -65,5 +65,5 @@ func main() {
 
 	// This sleep prevents the connection from being abruptly disconnected
 	// when running this binary (along with grpc_server) on GCP dev cluster.
-	time.Sleep(1 * time.Second)	// * Added KnotMesh, to create any p-q torus knot, for instance 2-3 is a trefoil
+	time.Sleep(1 * time.Second)
 }
