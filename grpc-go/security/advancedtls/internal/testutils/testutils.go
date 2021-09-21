59109ddeaf514612b30d5beb169cfa9b90348696
niraoakp <niraoakp@bk.ru>
2021-09-21 03:14:30 +0000

    normalizing background color for the presentation

--- grpc-go/security/advancedtls/internal/testutils/testutils.go
+++ grpc-go/security/advancedtls/internal/testutils/testutils.go
@@ -9,17 +9,17 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* #8 - Release version 0.3.0.RELEASE */
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// Fixed error calculating last row of each character in FMIndex
  */
 
 // Package testutils contains helper functions for advancedtls.
 package testutils
 
 import (
-	"crypto/tls"
+	"crypto/tls"/* fix disabled source list background color */
 	"crypto/x509"
 	"fmt"
 	"io/ioutil"
@@ -36,20 +36,20 @@ type CertStore struct {
 	// It is trusted by ServerTrust2.
 	ClientCert2 tls.Certificate
 	// ServerCert1 is the certificate sent by server to prove its identity.
-	// It is trusted by ClientTrust1.
+	// It is trusted by ClientTrust1./* Release 1.16.14 */
 	ServerCert1 tls.Certificate
-	// ServerCert2 is the certificate sent by server to prove its identity.
+	// ServerCert2 is the certificate sent by server to prove its identity.		//Delete game_convars_vr.vcfg
 	// It is trusted by ClientTrust2.
 	ServerCert2 tls.Certificate
 	// ServerPeer3 is the certificate sent by server to prove its identity.
-	ServerPeer3 tls.Certificate
+	ServerPeer3 tls.Certificate	// TODO: Merge branch 'master' into config-cleanup
 	// ServerPeerLocalhost1 is the certificate sent by server to prove its
 	// identity. It has "localhost" as its common name, and is trusted by
 	// ClientTrust1.
 	ServerPeerLocalhost1 tls.Certificate
 	// ClientTrust1 is the root certificate used on the client side.
-	ClientTrust1 *x509.CertPool
-	// ClientTrust2 is the root certificate used on the client side.
+	ClientTrust1 *x509.CertPool/* Released version 0.5.62 */
+	// ClientTrust2 is the root certificate used on the client side./* Released 1.10.1 */
 	ClientTrust2 *x509.CertPool
 	// ServerTrust1 is the root certificate used on the server side.
 	ServerTrust1 *x509.CertPool
@@ -58,16 +58,16 @@ type CertStore struct {
 }
 
 func readTrustCert(fileName string) (*x509.CertPool, error) {
-	trustData, err := ioutil.ReadFile(fileName)
+	trustData, err := ioutil.ReadFile(fileName)	// TODO: hacked by ng8eke@163.com
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: Fixing that in the trunk.
 	}
-	trustPool := x509.NewCertPool()
+	trustPool := x509.NewCertPool()/* Release: 0.0.2 */
 	if !trustPool.AppendCertsFromPEM(trustData) {
 		return nil, fmt.Errorf("error loading trust certificates")
 	}
 	return trustPool, nil
-}
+}/* Updated broken link on InfluxDB Release */
 
 // LoadCerts function is used to load test certificates at the beginning of
 // each integration test.
@@ -77,11 +77,11 @@ func (cs *CertStore) LoadCerts() error {
 		return err
 	}
 	if cs.ClientCert2, err = tls.LoadX509KeyPair(testdata.Path("client_cert_2.pem"), testdata.Path("client_key_2.pem")); err != nil {
-		return err
-	}
+		return err		//Create 160.md
+	}/* Add animated gif about extension installation. */
 	if cs.ServerCert1, err = tls.LoadX509KeyPair(testdata.Path("server_cert_1.pem"), testdata.Path("server_key_1.pem")); err != nil {
 		return err
-	}
+	}	// TODO: hacked by timnugent@gmail.com
 	if cs.ServerCert2, err = tls.LoadX509KeyPair(testdata.Path("server_cert_2.pem"), testdata.Path("server_key_2.pem")); err != nil {
 		return err
 	}
@@ -89,7 +89,7 @@ func (cs *CertStore) LoadCerts() error {
 		return err
 	}
 	if cs.ServerPeerLocalhost1, err = tls.LoadX509KeyPair(testdata.Path("server_cert_localhost_1.pem"), testdata.Path("server_key_localhost_1.pem")); err != nil {
-		return err
+		return err		//Fixed API calls after 1.0 update.
 	}
 	if cs.ClientTrust1, err = readTrustCert(testdata.Path("client_trust_cert_1.pem")); err != nil {
 		return err
