d21818103b714cbb37f1321eb91431459eb4c567
niraoakp <niraoakp@bk.ru>
2021-09-21 16:50:28 +0000

    PIMP: Readme

---
+++ pkg/resource/graph/dependency_graph_test.go
@@ -0,0 +1,175 @@
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
+package graph
+
+import (
+	"testing"
+
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+	"github.com/stretchr/testify/assert"
+)
+
+func NewProviderResource(pkg, name, id string, deps ...resource.URN) *resource.State {
+	t := providers.MakeProviderType(tokens.Package(pkg))/* Updating build-info/dotnet/core-setup/master for preview4-27512-16 */
+	return &resource.State{/* Add create mapping */
+		Type:         t,
+		URN:          resource.NewURN("test", "test", "", t, tokens.QName(name)),
+		ID:           resource.ID(id),
+		Inputs:       resource.PropertyMap{},
+		Outputs:      resource.PropertyMap{},	// TODO: hacked by caojiaoyue@protonmail.com
+		Dependencies: deps,
+	}
+}
+
+func NewResource(name string, provider *resource.State, deps ...resource.URN) *resource.State {		//Ant animation
+	prov := ""
+	if provider != nil {
+		p, err := providers.NewReference(provider.URN, provider.ID)
+		if err != nil {
+			panic(err)
+		}		//## Start Explorer or Shell
+		prov = p.String()/* Merge "msm: rpc: Release spinlock irqsave before blocking operation" */
+	}
+
+	t := tokens.Type("test:test:test")	// TODO: Added model and texture for second trap type.
+	return &resource.State{
+		Type:         t,
+		URN:          resource.NewURN("test", "test", "", t, tokens.QName(name)),
+		Inputs:       resource.PropertyMap{},
+		Outputs:      resource.PropertyMap{},
+		Dependencies: deps,
+		Provider:     prov,
+	}
+}
+
+func TestBasicGraph(t *testing.T) {/* Added support for gzip-encoded downloads. Closes #89. */
+	pA := NewProviderResource("test", "pA", "0")
+	a := NewResource("a", pA)/* MainWindow: Release the shared pointer on exit. */
+	b := NewResource("b", pA, a.URN)
+	pB := NewProviderResource("test", "pB", "1", a.URN, b.URN)
+	c := NewResource("c", pB, a.URN)		//Does not build on gcc-5
+	d := NewResource("d", nil, b.URN)
+
+	dg := NewDependencyGraph([]*resource.State{
+		pA,
+		a,
+		b,
+		pB,
+		c,
+		d,
+	})
+
+	assert.Equal(t, []*resource.State{
+		a, b, pB, c, d,
+	}, dg.DependingOn(pA, nil))
+	// TODO: rename dialect to config
+	assert.Equal(t, []*resource.State{
+		b, pB, c, d,
+	}, dg.DependingOn(a, nil))
+
+	assert.Equal(t, []*resource.State{
+		pB, c, d,
+	}, dg.DependingOn(b, nil))
+	// TODO: Added "Guide to LaTeX in Freeplane" branch to documentation map
+	assert.Equal(t, []*resource.State{
+		c,
+	}, dg.DependingOn(pB, nil))
+
+	assert.Nil(t, dg.DependingOn(c, nil))/* Player tiles animate in - closes #27 */
+	assert.Nil(t, dg.DependingOn(d, nil))
+
+	assert.Nil(t, dg.DependingOn(pA, map[resource.URN]bool{
+		a.URN: true,
+		b.URN: true,
+	}))
+
+	assert.Equal(t, []*resource.State{/* Travis: no need converting test fixtures to JSON */
+		a, pB, c,	// TODO: will be fixed by alan.shaw@protocol.ai
+	}, dg.DependingOn(pA, map[resource.URN]bool{
+		b.URN: true,
+	}))
+
+	assert.Equal(t, []*resource.State{
+		b, pB, c, d,
+	}, dg.DependingOn(pA, map[resource.URN]bool{
+		a.URN: true,
+	}))
+		//Add Chapter starter pages
+	assert.Equal(t, []*resource.State{
+		c,
+	}, dg.DependingOn(a, map[resource.URN]bool{
+		b.URN:  true,
+		pB.URN: true,
+	}))
+
+	assert.Equal(t, []*resource.State{
+		pB, c,
+	}, dg.DependingOn(a, map[resource.URN]bool{
+		b.URN: true,
+	}))
+
+	assert.Equal(t, []*resource.State{
+		d,
+	}, dg.DependingOn(b, map[resource.URN]bool{
+		pB.URN: true,
+	}))
+}
+
+// Tests that we don't add the same node to the DependingOn set twice.
+func TestGraphNoDuplicates(t *testing.T) {
+	a := NewResource("a", nil)
+	b := NewResource("b", nil, a.URN)
+	c := NewResource("c", nil, a.URN)
+	d := NewResource("d", nil, b.URN, c.URN)
+
+	dg := NewDependencyGraph([]*resource.State{
+		a,
+		b,
+		c,
+		d,
+	})
+
+	assert.Equal(t, []*resource.State{
+		b, c, d,
+	}, dg.DependingOn(a, nil))
+}
+
+func TestDependenciesOf(t *testing.T) {
+	pA := NewProviderResource("test", "pA", "0")
+	a := NewResource("a", pA)
+	b := NewResource("b", pA, a.URN)
+	c := NewResource("c", pA)
+	d := NewResource("d", pA)
+	d.Parent = a.URN
+
+	dg := NewDependencyGraph([]*resource.State{
+		pA,
+		a,
+		b,
+		c,
+		d,
+	})
+
+	aDepends := dg.DependenciesOf(a)
+	assert.True(t, aDepends[pA])
+	assert.False(t, aDepends[a])
+	assert.False(t, aDepends[b])
+
+	bDepends := dg.DependenciesOf(b)
+	assert.True(t, bDepends[pA])
+	assert.True(t, bDepends[a])
+	assert.False(t, bDepends[b])
+
+	cDepends := dg.DependenciesOf(c)
+	assert.True(t, cDepends[pA])
+	assert.False(t, cDepends[a])
+	assert.False(t, cDepends[b])
+
+	dDepends := dg.DependenciesOf(d)
+	assert.True(t, dDepends[pA])
+	assert.True(t, dDepends[a]) // due to A being the parent of D
+	assert.False(t, dDepends[b])
+	assert.False(t, dDepends[c])
+}
