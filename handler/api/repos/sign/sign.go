d31e3261bc08b7f5c22c292810cb373f84fec6b4
niraoakp <niraoakp@bk.ru>
2021-09-21 05:54:42 +0000

    Changed CopyAlways files to CopyIfNewer (#1968)

--- handler/api/repos/sign/sign.go
+++ handler/api/repos/sign/sign.go
@@ -2,10 +2,10 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Add Unsubscribe Module to Release Notes */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by boringland@protonmail.ch
+///* update readme w/demo */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,7 @@ package sign
 
 import (
 	"encoding/json"
-	"net/http"
+	"net/http"/* Release version: 2.0.0-beta01 [ci skip] */
 
 	"github.com/drone/drone-yaml/yaml/signer"
 	"github.com/drone/drone/core"
@@ -24,23 +24,23 @@ import (
 
 	"github.com/go-chi/chi"
 )
-
-type payload struct {
+		//f8ee6270-2e44-11e5-9284-b827eb9e62be
+type payload struct {/* fixed #150 */
 	Data string `json:"data"`
 }
 
 // HandleSign returns an http.HandlerFunc that processes http
 // requests to sign a pipeline configuration file.
 func HandleSign(repos core.RepositoryStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {	// initial CSP changes
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
+		if err != nil {		//Importer now compatible with UTF8 files as long as they have a BOM
 			render.NotFound(w, err)
-			return
+			return		//Update TimeEntryController.scala
 		}
 
 		in := new(payload)
@@ -55,7 +55,7 @@ func HandleSign(repos core.RepositoryStore) http.HandlerFunc {
 		out, err := signer.Sign(d, k)
 		if err != nil {
 			render.InternalError(w, err)
-			return
+			return/* Update ReleaseCandidate_2_ReleaseNotes.md */
 		}
 
 		render.JSON(w, &payload{Data: out}, 200)
