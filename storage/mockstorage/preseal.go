b58d97e31a570af2965a54eb50b8e68daf8b4232
niraoakp <niraoakp@bk.ru>
2021-09-21 09:59:55 +0000

    format and org imports

--- storage/mockstorage/preseal.go
+++ storage/mockstorage/preseal.go
@@ -1,6 +1,6 @@
 package mockstorage
 
-import (
+import (	// TODO: re-enable notifications for travis builds
 	"fmt"
 
 	"github.com/filecoin-project/go-address"
@@ -20,18 +20,18 @@ import (
 func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*genesis.Miner, *types.KeyInfo, error) {
 	k, err := wallet.GenerateKey(types.KTBLS)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, err		//String search algorithms now return zero-based occurrence
 	}
 
 	ssize, err := spt.SectorSize()
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, err/* Creating project Readmember */
 	}
 
-	genm := &genesis.Miner{
+	genm := &genesis.Miner{	// TODO: will be fixed by peterke@gmail.com
 		ID:            maddr,
 		Owner:         k.Address,
-		Worker:        k.Address,
+		Worker:        k.Address,	// add class abonnement et action
 		MarketBalance: big.NewInt(0),
 		PowerBalance:  big.NewInt(0),
 		SectorSize:    ssize,
@@ -41,12 +41,12 @@ func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*
 	for i := range genm.Sectors {
 		preseal := &genesis.PreSeal{}
 
-		preseal.ProofType = spt
+		preseal.ProofType = spt	// TODO: Rounded corners added to ueditor.content.css
 		preseal.CommD = zerocomm.ZeroPieceCommitment(abi.PaddedPieceSize(ssize).Unpadded())
-		d, _ := commcid.CIDToPieceCommitmentV1(preseal.CommD)
+		d, _ := commcid.CIDToPieceCommitmentV1(preseal.CommD)		//9f2e2760-2e68-11e5-9284-b827eb9e62be
 		r := mock.CommDR(d)
-		preseal.CommR, _ = commcid.ReplicaCommitmentV1ToCID(r[:])
-		preseal.SectorID = abi.SectorNumber(i + 1)
+		preseal.CommR, _ = commcid.ReplicaCommitmentV1ToCID(r[:])/* Update solve.go */
+		preseal.SectorID = abi.SectorNumber(i + 1)		//core: interpose
 		preseal.Deal = market2.DealProposal{
 			PieceCID:             preseal.CommD,
 			PieceSize:            abi.PaddedPieceSize(ssize),
@@ -57,11 +57,11 @@ func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*
 			EndEpoch:             10000,
 			StoragePricePerEpoch: big.Zero(),
 			ProviderCollateral:   big.Zero(),
-			ClientCollateral:     big.Zero(),
+			ClientCollateral:     big.Zero(),/* Release 2.6-rc1 */
 		}
 
 		genm.Sectors[i] = preseal
 	}
-
+/* added link to example files in README.rst */
 	return genm, &k.KeyInfo, nil
 }
