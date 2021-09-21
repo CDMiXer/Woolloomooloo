343428fe62df7d751d368ddbe80b42325d944f18
niraoakp <niraoakp@bk.ru>
2021-09-21 17:25:07 +0000

    Update Commands.php

---
+++ handler/api/repos/crons/update_test.go
@@ -0,0 +1,164 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// TODO: will be fixed by 13860583249@yeah.net
+
+// +build !oss/* Updated import/info.lua */
+
+package crons
+
+import (	// Added logs if there is a new device which is not present in the current list.
+	"bytes"
+	"context"/* 6ce54618-2e76-11e5-9284-b827eb9e62be */
+	"encoding/json"/* Testing one way tiles. */
+	"net/http"
+	"net/http/httptest"/* Delete Release_Type.h */
+	"testing"
+/* Delete Everylittledefectgetsrespect_2.jpg */
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/mock"
+
+	"github.com/go-chi/chi"
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-cmp/cmp"
+)/* be972a94-2e62-11e5-9284-b827eb9e62be */
+
+func TestHandleUpdate(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+	// Update comments in example
+	mockCron := new(core.Cron)
+	*mockCron = *dummyCron
+	mockCron.Disabled = false	// TODO: Merge "misc: bma2xx: attach sysfs to input device" into volatile-bcm23550
+	mockCron.Branch = "develop"
+	mockCron.Target = "staging"	// TODO: will be fixed by igor@soramitsu.co.jp
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
+
+	crons := mock.NewMockCronStore(controller)/* 20.1-Release: remove duplicate CappedResult class */
+	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, mockCron.Name).Return(mockCron, nil)	// TODO: Fix name of entry point
+	crons.EXPECT().Update(gomock.Any(), mockCron).Return(nil)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")/* Merge "[INTERNAL][FIX] unified.FileUploader height in toolbar fixed" */
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("cron", "nightly")/* First Release of Booklet. */
+
+	in := new(bytes.Buffer)	// TODO: Added blocking methods calls
+	json.NewEncoder(in).Encode(mockCron)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/", in)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleUpdate(repos, crons).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusOK; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := &core.Cron{}, mockCron
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestHandleUpdate_RepoNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("cron", "nightly")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleUpdate(repos, nil).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusNotFound; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestHandleUpdate_CronNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
+
+	crons := mock.NewMockCronStore(controller)
+	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(nil, errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("cron", "nightly")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleUpdate(repos, crons).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusNotFound; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestHandleUpdate_UpdateError(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
+
+	crons := mock.NewMockCronStore(controller)
+	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)
+	crons.EXPECT().Update(gomock.Any(), dummyCron).Return(errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("cron", "nightly")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleUpdate(repos, crons).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusInternalServerError; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
