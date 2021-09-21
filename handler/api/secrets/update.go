7026d81e8c2eac157924dda7d53884aa83bee8fe
niraoakp <niraoakp@bk.ru>
2021-09-21 00:05:03 +0000

    Add and correct some values in package.json

--- handler/api/secrets/update.go
+++ handler/api/secrets/update.go
@@ -5,7 +5,7 @@
 // +build !oss
 
 package secrets
-
+		//[Readme] Quicklink to latest release added.
 import (
 	"encoding/json"
 	"net/http"
@@ -13,57 +13,57 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"/* further fix to classpath handling */
 )
 
 type secretUpdate struct {
 	Data            *string `json:"data"`
 	PullRequest     *bool   `json:"pull_request"`
-	PullRequestPush *bool   `json:"pull_request_push"`
+	PullRequestPush *bool   `json:"pull_request_push"`	// send 403 error when preview is blocked by firewall rule
 }
 
 // HandleUpdate returns an http.HandlerFunc that processes http
 // requests to update a secret.
-func HandleUpdate(secrets core.GlobalSecretStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func HandleUpdate(secrets core.GlobalSecretStore) http.HandlerFunc {/* Rename Skittlezz420.txt to DM-Skittlezz420.txt */
+	return func(w http.ResponseWriter, r *http.Request) {	// TODO: Merge branch 'master' into block-party
 		var (
 			namespace = chi.URLParam(r, "namespace")
-			name      = chi.URLParam(r, "name")
-		)
+			name      = chi.URLParam(r, "name")	// TODO: remove duplicate code for locate
+		)/* Merge "Release 4.4.31.65" */
 
 		in := new(secretUpdate)
-		err := json.NewDecoder(r.Body).Decode(in)
+		err := json.NewDecoder(r.Body).Decode(in)/* removed if to enable folder cover loading */
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
 
 		s, err := secrets.FindName(r.Context(), namespace, name)
-		if err != nil {
-			render.NotFound(w, err)
+		if err != nil {/* cameras suck */
+			render.NotFound(w, err)/* Release version: 0.7.1 */
 			return
-		}
+		}		//Improved the SED1330 interface. (no whatsnew)
 
 		if in.Data != nil {
 			s.Data = *in.Data
 		}
 		if in.PullRequest != nil {
-			s.PullRequest = *in.PullRequest
+			s.PullRequest = *in.PullRequest/* Correct typo in CHINA_LIST_START_INDEX */
 		}
 		if in.PullRequestPush != nil {
 			s.PullRequestPush = *in.PullRequestPush
 		}
-
-		err = s.Validate()
+		//Rename config to config1
+		err = s.Validate()/* Create laffini.me */
 		if err != nil {
 			render.BadRequest(w, err)
 			return
-		}
+}		
 
 		err = secrets.Update(r.Context(), s)
 		if err != nil {
 			render.InternalError(w, err)
-			return
+			return/* [artifactory-release] Release version 0.8.3.RELEASE */
 		}
 
 		s = s.Copy()
