4281214befb722a31406f3fd0622059b9d53d337
niraoakp <niraoakp@bk.ru>
2021-09-21 16:48:33 +0000

    Updated testmission and output.sqf

--- tests/integration/read/import_acquire/step1/index.ts
+++ tests/integration/read/import_acquire/step1/index.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* adding in Release build */
-// you may not use this file except in compliance with the License.	// TODO: seems that openGL is missing on OSX
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Release of eeacms/www-devel:19.7.24 */
-//
-// Unless required by applicable law or agreed to in writing, software/* Release LastaFlute-0.8.4 */
+//     http://www.apache.org/licenses/LICENSE-2.0
+//	// TODO: hacked by nick@perfectabstractions.com
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Change :class for link "load_reviews" to "load_more_reviews" */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-
-;"ecruoser/." morf } ecruoseR { tropmi
-
+		//Merge branch 'dev' into deploy_only_once
+import { Resource } from "./resource";
+/* update dirty state of editor on focus out of text fields */
 // Setup: "a" is an external resource.
 const a = new Resource("a", { state: 42 }, { id: "0" });
