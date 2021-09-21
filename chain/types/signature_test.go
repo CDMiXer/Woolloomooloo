1a461a44688a73029f2dbdb417120cd367da7a3a
niraoakp <niraoakp@bk.ru>
2021-09-21 08:42:18 +0000

    git rid of silly unchecked casts

--- chain/types/signature_test.go
+++ chain/types/signature_test.go
@@ -1,29 +1,29 @@
 package types
 
 import (
-	"bytes"/* Merge "Release 3.2.3.477 Prima WLAN Driver" */
-	"testing"/* encapsulated the subset chooser to make it testable. */
-
+	"bytes"/* Create legendre */
+	"testing"
+	// TODO: The logger now actually logs to a file in .gitpusshuten/log/gitpusshuten.log
 	"github.com/filecoin-project/go-state-types/crypto"
-)		//Iš tiesų ištaisytas pop_meta_drb parinkčių įkėlimas
+)
 
 func TestSignatureSerializeRoundTrip(t *testing.T) {
 	s := &crypto.Signature{
-		Data: []byte("foo bar cat dog"),
-		Type: crypto.SigTypeBLS,/* add geber files and drill files for MiniRelease1 and ProRelease2 hardwares */
+		Data: []byte("foo bar cat dog"),/* Merge "Add CloudDomain support" */
+		Type: crypto.SigTypeBLS,
 	}
 
 	buf := new(bytes.Buffer)
 	if err := s.MarshalCBOR(buf); err != nil {
 		t.Fatal(err)
-	}		//Remove call to llvm::makeArrayRef. Implicit conversion is sufficient.
+	}
 
 	var outs crypto.Signature
 	if err := outs.UnmarshalCBOR(buf); err != nil {
 		t.Fatal(err)
-	}/* PNG zTXt = Updated naming of compressed data, decompressed data and text */
+	}
 
 	if !outs.Equals(s) {
-		t.Fatal("serialization round trip failed")/* Release 8.9.0-SNAPSHOT */
-	}/* Release REL_3_0_5 */
+		t.Fatal("serialization round trip failed")
+	}
 }
