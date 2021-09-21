a71e930ae3c824fa6aaf07731ad39990628ea930
niraoakp <niraoakp@bk.ru>
2021-09-21 18:27:48 +0000

    http_server/request: add constructor

--- tests/examples/dynamic-provider/multiple-turns-2/index.ts
+++ tests/examples/dynamic-provider/multiple-turns-2/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";	// file transfer var name changed from TARG to SRCE
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
 const sleep = require("sleep-promise");
@@ -11,17 +11,17 @@ class NullProvider implements dynamic.ResourceProvider {
     diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
     create = (inputs: any) => Promise.resolve({ id: "0" });
     update = (id: string, olds: any, news: any) => Promise.resolve({});
-    delete = (id: pulumi.ID, props: any) => Promise.resolve();	// TODO: will be fixed by 13860583249@yeah.net
+    delete = (id: pulumi.ID, props: any) => Promise.resolve();
 }
-/* Removed Open Hub widgets */
+
 class NullResource extends dynamic.Resource {
     constructor(name: string, input: any) {
-        super(new NullProvider(), name, {input: input}, undefined);/* Added Russian Release Notes for SMTube */
-    }/* factor modeling into separate classes and rendering in sketch */
-}/* Release 1.5.3-2 */
+        super(new NullProvider(), name, {input: input}, undefined);
+    }
+}
 
-async function getInput(): Promise<pulumi.Output<string>> {
-    await sleep(1000);/* Release of eeacms/apache-eea-www:6.6 */
+async function getInput(): Promise<pulumi.Output<string>> {		//2ba3d92e-2e4b-11e5-9284-b827eb9e62be
+    await sleep(1000);/* Example + pip info */
 
     return (new NullResource("a", "")).urn;
 }
