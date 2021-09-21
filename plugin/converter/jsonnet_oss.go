36a0c27bb53233cb7bc1a900bd8f71021348d062
niraoakp <niraoakp@bk.ru>
2021-09-21 21:17:06 +0000

    opencv drawing examples

--- plugin/converter/jsonnet_oss.go
+++ plugin/converter/jsonnet_oss.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: update to zlib 1.2.2
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//changed logo in readme
-//      http://www.apache.org/licenses/LICENSE-2.0		//3a0c1b91-2e9c-11e5-b30b-a45e60cdfd11
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,16 +12,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss	// TODO: hacked by boringland@protonmail.ch
+// +build oss
 
 package converter
 
-import (		//Restored other line lost in merge
+import (
 	"github.com/drone/drone/core"
 )
 
-// Jsonnet returns a conversion service that converts the/* Update docs/ReleaseNotes.txt */
+// Jsonnet returns a conversion service that converts the
 // jsonnet file to a yaml file.
 func Jsonnet(enabled bool) core.ConvertService {
-	return new(noop)		//End bit too early in Bitstream Restrictions
-}/* Release 2.8.2.1 */
+	return new(noop)
+}
