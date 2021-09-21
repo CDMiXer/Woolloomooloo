f0796b27ed04e154df5f5383b65bcba5b6d14e27
niraoakp <niraoakp@bk.ru>
2021-09-21 08:19:39 +0000

    Automatic changelog generation #4058 [ci skip]

--- logger/logger.go
+++ logger/logger.go
@@ -1,34 +1,34 @@
-// Copyright 2019 Drone IO, Inc.		//fixed untracked files
+// Copyright 2019 Drone IO, Inc.
 // Copyright 2016 The containerd Authors.
-//		//Node v6.9.4
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Imported Debian patch 0.4.0-1 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge "Release locks when action is cancelled" */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Add getitem ellipsis tests.
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 23.2.0 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package logger
-/* Merge branch 'develop' into bug/widget/titles */
+
 import (
 	"context"
 	"net/http"
 
 	"github.com/sirupsen/logrus"
-)/* Release of eeacms/plonesaas:5.2.4-9 */
+)
 
-type loggerKey struct{}	// delete top_apps folder
+type loggerKey struct{}
 
-// L is an alias for the the standard logger./* Release of eeacms/eprtr-frontend:0.4-beta.11 */
+// L is an alias for the the standard logger.
 var L = logrus.NewEntry(logrus.StandardLogger())
 
 // WithContext returns a new context with the provided logger. Use in
-// combination with logger.WithField(s) for great effect./* TYPO3 CMS 6 Release (v1.0.0) */
+// combination with logger.WithField(s) for great effect.
 func WithContext(ctx context.Context, logger *logrus.Entry) context.Context {
 	return context.WithValue(ctx, loggerKey{}, logger)
 }
@@ -38,13 +38,13 @@ func WithContext(ctx context.Context, logger *logrus.Entry) context.Context {
 func FromContext(ctx context.Context) *logrus.Entry {
 	logger := ctx.Value(loggerKey{})
 	if logger == nil {
-		return L/* Release 2.6.7 */
+		return L
 	}
 	return logger.(*logrus.Entry)
-}/* Add curl install to trusty. */
-/* Release 0.33.2 */
+}
+
 // FromRequest retrieves the current logger from the request. If no
 // logger is available, the default logger is returned.
-func FromRequest(r *http.Request) *logrus.Entry {/* Release for 18.15.0 */
+func FromRequest(r *http.Request) *logrus.Entry {
 	return FromContext(r.Context())
 }
