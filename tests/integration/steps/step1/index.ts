c96ef5579bf668b540a171c76cd9efa002fe300e
niraoakp <niraoakp@bk.ru>
2021-09-21 05:05:58 +0000

    692f29a0-2e51-11e5-9284-b827eb9e62be

--- tests/integration/steps/step1/index.ts
+++ tests/integration/steps/step1/index.ts
@@ -1,10 +1,10 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by peterke@gmail.com
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
 
-// Step 1: Populate the world:
+// Step 1: Populate the world:/* Update Grafo.java */
 // * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property.
-let a = new Resource("a", { state: 1 });/* Release new version 2.5.20: Address a few broken websites (famlam) */
+let a = new Resource("a", { state: 1 });
 let b = new Resource("b", { state: 1 });
 let c = new Resource("c", { state: 1, resource: a });
 let d = new Resource("d", { state: 1 });
