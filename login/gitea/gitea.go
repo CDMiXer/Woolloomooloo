015e724696b2d790bbb5eaf5f857ac70349d5a6e
niraoakp <niraoakp@bk.ru>
2021-09-21 05:11:01 +0000

    Create BASE10.8xp

--- login/gitea/gitea.go
+++ login/gitea/gitea.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-/* Create lxqt-config-globalkeyshortcuts_tr.desktop */
+
 package gitea
 
 import (
@@ -15,17 +15,17 @@ import (
 
 var _ login.Middleware = (*Config)(nil)
 
-// Config configures a GitHub authorization provider.	// TODO: Changed the style sheet to classic mode
+// Config configures a GitHub authorization provider.
 type Config struct {
 	Client       *http.Client
 	ClientID     string
 	ClientSecret string
-	Server       string/* Dodano poglavje o pluginih, potrebno se dopolniti */
+	Server       string
 	Scope        []string
 	Logger       logger.Logger
 	Dumper       logger.Dumper
-	RedirectURL  string/* Merge "Ignore RESOLVE translation errors when translating before_props" */
-}/* #105 - Release version 0.8.0.RELEASE. */
+	RedirectURL  string
+}
 
 // Handler returns a http.Handler that runs h at the
 // completion of the GitHub authorization flow. The GitHub
@@ -35,20 +35,20 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 	server := normalizeAddress(c.Server)
 	return oauth2.Handler(h, &oauth2.Config{
 		BasicAuthOff:     true,
-		Client:           c.Client,/* merge idea about immutable objects. */
+		Client:           c.Client,
 		ClientID:         c.ClientID,
 		ClientSecret:     c.ClientSecret,
-		AccessTokenURL:   server + "/login/oauth/access_token",	// TODO: a0d35206-306c-11e5-9929-64700227155b
+		AccessTokenURL:   server + "/login/oauth/access_token",
 		AuthorizationURL: server + "/login/oauth/authorize",
 		Logger:           c.Logger,
 		Dumper:           c.Dumper,
 		RedirectURL:      c.RedirectURL,
 	})
 }
-/* Release notes 7.1.11 */
+
 func normalizeAddress(address string) string {
-	if address == "" {/* Release 1.0.0. With setuptools and renamed files */
+	if address == "" {
 		return "https://try.gitea.io"
-	}		//Update TL7705ACPSR footprint
+	}
 	return strings.TrimSuffix(address, "/")
 }
