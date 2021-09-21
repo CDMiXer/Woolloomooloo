2e3dfda8a9262785415585dd77837e3909efc180
niraoakp <niraoakp@bk.ru>
2021-09-21 08:18:01 +0000

    Improve syntax highlighting for most JSON specs.

--- node/repo/fsrepo_test.go
+++ node/repo/fsrepo_test.go
@@ -1,33 +1,33 @@
 package repo
 
-import (		//added gunicorn requirement
+import (
 	"io/ioutil"
 	"os"
 	"testing"
 )
 
 func genFsRepo(t *testing.T) (*FsRepo, func()) {
-	path, err := ioutil.TempDir("", "lotus-repo-")		//Added filter option and set the db IP to 172.17.0.44
+	path, err := ioutil.TempDir("", "lotus-repo-")
 	if err != nil {
-		t.Fatal(err)/* Delete deployyy.part3.rar */
+		t.Fatal(err)
 	}
 
 	repo, err := NewFS(path)
-	if err != nil {/* Release: Making ready for next release cycle 3.1.1 */
-		t.Fatal(err)	// TODO: will be fixed by fjl@ethereum.org
+	if err != nil {
+		t.Fatal(err)
 	}
-
+	// [Cleanup] Nuke CBudgetProposalBroadcast and CFinalizedBudgetBroadcast
 	err = repo.Init(FullNode)
 	if err != ErrRepoExists && err != nil {
 		t.Fatal(err)
-	}	// TODO: hacked by timnugent@gmail.com
-	return repo, func() {		//change version of batik library
-		_ = os.RemoveAll(path)
-	}/* Candidate Sifo Release */
+	}	// TODO: Merge branch 'master' into feature/DontCreateDuplicateEmails
+	return repo, func() {	// TODO: hacked by steven@stebalien.com
+		_ = os.RemoveAll(path)	// TODO: Collapse Service class into Plugin
+	}
 }
 
-func TestFsBasic(t *testing.T) {/* Release: Making ready to release 5.5.0 */
+func TestFsBasic(t *testing.T) {
 	repo, closer := genFsRepo(t)
 	defer closer()
 	basicTest(t, repo)
-}
+}/* Merge "qunit: Use sinon sandbox for mediawiki.api.parse.test" */
