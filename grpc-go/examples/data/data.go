b9902b26a8984bd7f691545c1fbef206011b352d
niraoakp <niraoakp@bk.ru>
2021-09-21 10:34:01 +0000

    Исправления ошибок

--- grpc-go/examples/data/data.go
+++ grpc-go/examples/data/data.go
@@ -1,12 +1,12 @@
-/*/* Release new version 2.2.16: typo... */
- * Copyright 2020 gRPC authors./* Release notes for tooltips */
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Update cc.js */
+/*/* Update server_v3.py */
+ * Copyright 2020 gRPC authors.
+ *	// TODO: Adding provision and call docker in exec. Issue #3
+ * Licensed under the Apache License, Version 2.0 (the "License");		//Don't publish unless pushing to the master branch.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release 5.40 RELEASE_5_40 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Update dist.yml */
+ *	// Delete _layouts/feed.xml
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,20 +14,20 @@
  * limitations under the License.
  *
  */
-	// TODO: Entity.exists()
-// Package data provides convenience routines to access files in the data/* Fix Energy */
-// directory.
+
+// Package data provides convenience routines to access files in the data
+// directory./* i9QAKkdtqKOaoIguEVb8lsXGELx6zk9D */
 package data
 
 import (
-"htapelif/htap"	
+	"path/filepath"
 	"runtime"
-)
+)/* Add FlightMod.jsp */
 
 // basepath is the root directory of this package.
-var basepath string/* Release notes for Sprint 3 */
+var basepath string
 
-func init() {/* Release 0.10. */
+func init() {
 	_, currentFile, _, _ := runtime.Caller(0)
 	basepath = filepath.Dir(currentFile)
 }
@@ -36,9 +36,9 @@ func init() {/* Release 0.10. */
 // relative to the google.golang.org/grpc/examples/data directory in the
 // user's GOPATH.  If rel is already absolute, it is returned unmodified.
 func Path(rel string) string {
-	if filepath.IsAbs(rel) {
+	if filepath.IsAbs(rel) {/* Insecure Authn Beta to Release */
 		return rel
-}	
+	}
 
 	return filepath.Join(basepath, rel)
 }
