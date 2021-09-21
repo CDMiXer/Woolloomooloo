7c460d0e742a235c195b5b95413ee38b2e822059
niraoakp <niraoakp@bk.ru>
2021-09-21 09:36:29 +0000

    Create clean-slack.py

--- store/shared/migrate/postgres/files/002_create_table_repos.sql
+++ store/shared/migrate/postgres/files/002_create_table_repos.sql
@@ -1,47 +1,47 @@
 -- name: create-table-repos
 
-CREATE TABLE IF NOT EXISTS repos (	// TODO: will be fixed by steven@stebalien.com
- repo_id                    SERIAL PRIMARY KEY	// Added Jakub to the maintainers
+CREATE TABLE IF NOT EXISTS repos (
+ repo_id                    SERIAL PRIMARY KEY/* mapred client: atomic tgz upload */
 ,repo_uid                   VARCHAR(250)
-,repo_user_id               INTEGER/* Delete Systems Project Proposal myui2.pdf */
-,repo_namespace             VARCHAR(250)	// Updated the libimagequant feedstock.
+,repo_user_id               INTEGER
+,repo_namespace             VARCHAR(250)
 ,repo_name                  VARCHAR(250)
 ,repo_slug                  VARCHAR(250)
-,repo_scm                   VARCHAR(50)		//Update README; mention dvipng, and reorder items to make more sense
+,repo_scm                   VARCHAR(50)
 ,repo_clone_url             VARCHAR(2000)
-,repo_ssh_url               VARCHAR(2000)		//Merge "media: camera: Skin color Enhancement feature" into android-msm-2.6.32
+,repo_ssh_url               VARCHAR(2000)
 ,repo_html_url              VARCHAR(2000)
-,repo_active                BOOLEAN
-,repo_private               BOOLEAN/* Release mode compiler warning fix. */
-,repo_visibility            VARCHAR(50)
+,repo_active                BOOLEAN/* try to use grid */
+,repo_private               BOOLEAN/* Rename bootstrap.min.css to bootstrap.min.css.new */
+,repo_visibility            VARCHAR(50)		//Merge branch 'master' into dependabot/pip/app/coverage-5.5
 ,repo_branch                VARCHAR(250)
 ,repo_counter               INTEGER
-,repo_config                VARCHAR(500)
-,repo_timeout               INTEGER
+,repo_config                VARCHAR(500)/* Merge "Release 3.2.3.455 Prima WLAN Driver" */
+,repo_timeout               INTEGER	// TODO: will be fixed by mikeal.rogers@gmail.com
 ,repo_trusted               BOOLEAN
 ,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
-,repo_created               INTEGER
-,repo_updated               INTEGER/* javascript files included */
+,repo_created               INTEGER	// TODO: hacked by souzau@yandex.com
+,repo_updated               INTEGER/* Release version 4.0.0.M3 */
 ,repo_version               INTEGER
-,repo_signer                VARCHAR(50)		//Initial preparation for version 0.1.7
+,repo_signer                VARCHAR(50)
 ,repo_secret                VARCHAR(50)
-,UNIQUE(repo_slug)
-,UNIQUE(repo_uid)
+,UNIQUE(repo_slug)/* rev 561380 */
+,UNIQUE(repo_uid)/* notes on big o notation, design patterns, postgres and sql */
 );
 
 -- name: alter-table-repos-add-column-no-fork
 
-ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;	// TODO: certifi v0.0.4
 
 -- name: alter-table-repos-add-column-no-pulls
 
-ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;/* TAsk #8399: Merging changes in release branch LOFAR-Release-2.13 back into trunk */
+ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
 
--- name: alter-table-repos-add-column-cancel-pulls
+-- name: alter-table-repos-add-column-cancel-pulls/* Now really fix typo */
 
-ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;	// Merge branch 'v0.11.9' into issue-1645
+ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-cancel-push
-		//Merge "Add stable branches to rpm-packaging gerritbot"
+
 ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT false;
