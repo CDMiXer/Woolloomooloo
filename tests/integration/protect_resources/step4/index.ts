af401edbbe32fd47c57139481a0bf23b1ed3a706
niraoakp <niraoakp@bk.ru>
2021-09-21 01:27:26 +0000

    Update manutencao_produtos_button_alterar_action.prg

--- tests/integration/protect_resources/step4/index.ts
+++ tests/integration/protect_resources/step4/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Merge "Fix a permissions probem in ConnectivityManager"
+	// Added core liquibase support
 import { Resource } from "./resource";
 
-// Next, just unprotect the resource:
-let a = new Resource("eternal", { state: 2 }, { protect: false });/* setup async fallbacks for google analytics */
+// Next, just unprotect the resource:/* Release 20060711a. */
+let a = new Resource("eternal", { state: 2 }, { protect: false });
