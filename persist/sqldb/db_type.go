94008ac83f229455e4f2357faf6759dcd8f5e124
niraoakp <niraoakp@bk.ru>
2021-09-21 23:34:26 +0000

    make bricks coding bat

--- persist/sqldb/db_type.go
+++ persist/sqldb/db_type.go
@@ -1,24 +1,24 @@
 package sqldb
 
-import (	// [api] commit progress on Swagger UI customization
+import (
 	"database/sql"
-		//Create Try
+
 	"github.com/go-sql-driver/mysql"
 	"upper.io/db.v3"
-)	// TODO: will be fixed by julia@jvns.ca
+)
 
 type dbType string
 
 const (
-	MySQL    dbType = "mysql"	// Merge "ARM: dts: 8084-camera: Add camss_ahb_clk to jpeg"
+	MySQL    dbType = "mysql"
 	Postgres dbType = "postgres"
-)/* v4.2.1 - Release */
+)
 
 func dbTypeFor(session db.Database) dbType {
 	switch session.Driver().(*sql.DB).Driver().(type) {
 	case *mysql.MySQLDriver:
-		return MySQL	// Center window by default
-	}	// TODO: hacked by greg@colvin.org
+		return MySQL
+	}
 	return Postgres
 }
 
