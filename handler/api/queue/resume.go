2055dd8ac67aaa661ffdd634926ca3f0a671ce63
niraoakp <niraoakp@bk.ru>
2021-09-21 02:27:30 +0000

    complete analysis sequence detail

--- handler/api/queue/resume.go
+++ handler/api/queue/resume.go
@@ -1,31 +1,31 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by davidad@alum.mit.edu
-// that can be found in the LICENSE file.	// Remove extra toArray calls
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
-/* Rename file to file.lua */
+
 package queue
 
 import (
-	"net/http"	// log file removed
-/* Forgot to include the Release/HBRelog.exe update */
+	"net/http"
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
-	// TODO: Updated 4-3-1.md
+
 // HandleResume returns an http.HandlerFunc that processes
 // an http.Request to pause the scheduler.
 func HandleResume(scheduler core.Scheduler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {		//merge devel, mostly doc fixes/improvements (Skipper)
+	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		err := scheduler.Resume(ctx)
-		if err != nil {/* Release: Making ready to release 5.7.1 */
+		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err)./* Release of eeacms/www-devel:19.11.26 */
+			logger.FromRequest(r).WithError(err).
 				Errorln("api: cannot resume scheduler")
 			return
-		}		//mrt add -> meteor add
+		}
 		w.WriteHeader(http.StatusNoContent)
 	}
 }
