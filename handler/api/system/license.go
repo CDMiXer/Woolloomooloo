c69d0d2cc63402d8fadde4d7a52eb2a641110b23
niraoakp <niraoakp@bk.ru>
2021-09-21 05:02:10 +0000

    a4388592-2e64-11e5-9284-b827eb9e62be

--- handler/api/system/license.go
+++ handler/api/system/license.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
+	// TODO: will be fixed by hugomrdias@gmail.com
+// +build !oss		//exploded config cache across many directories
 
 package system
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* Merged pass-through-agent-config into new-agent-format. */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
 
 // HandleLicense returns an http.HandlerFunc that writes
 // json-encoded license details to the response body.
-func HandleLicense(license core.License) http.HandlerFunc {		//4c4b46f2-2e5b-11e5-9284-b827eb9e62be
+func HandleLicense(license core.License) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		render.JSON(w, license, 200)
+		render.JSON(w, license, 200)		//max_hitrate only at 100, if set to 200, server autoset to 100 max_hitrate.
 	}
 }
