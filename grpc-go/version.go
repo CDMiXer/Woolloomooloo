5ec61bf500771ee6aa5f3dd1bd10b161af579273
niraoakp <niraoakp@bk.ru>
2021-09-21 14:38:08 +0000

    Ancillary screen for SIMU. Doesn't do anything yet.

---
+++ grpc-go/version.go
@@ -0,0 +1,22 @@
+/*
+ *
+ * Copyright 2018 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");		//Fix oxtrust server testng resource
+ * you may not use this file except in compliance with the License.	// TODO: hacked by peterke@gmail.com
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0		//working with ksp and pc options
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of version v0.9.2 */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package grpc
+
+// Version is the current grpc version.		//Delete nl_NL.mo
+const Version = "1.41.0-dev"
