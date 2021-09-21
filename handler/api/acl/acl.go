b0bad07df12968f72ea199e19be96b0145b28923
niraoakp <niraoakp@bk.ru>
2021-09-21 07:24:58 +0000

    update examples to call destroy on the listview

--- handler/api/acl/acl.go
+++ handler/api/acl/acl.go
@@ -1,41 +1,41 @@
 // Copyright 2019 Drone IO, Inc.
-///* connexion -> connection */
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release v0.5.7 */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Update hook prefix
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release notes updated. */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// rename unblockable function
-// See the License for the specific language governing permissions and/* Release 1.84 */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package acl	// use GNU GPL-3.0
+package acl
 
-import (	// TODO: remove migrator.
+import (
 	"net/http"
 
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/render"	// TODO: hacked by timnugent@gmail.com
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-)/* Release of eeacms/ims-frontend:0.6.4 */
+)
 
 // AuthorizeUser returns an http.Handler middleware that authorizes only
 // authenticated users to proceed to the next handler in the chain. Guest users
 // are rejected with a 401 unauthorized error.
-func AuthorizeUser(next http.Handler) http.Handler {/* Merge "Moved all parameters away from Role and into Plan" */
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {/* [NGRINDER-481] - Move the question mark in logs to just right of log  */
-		_, ok := request.UserFrom(r.Context())/* Release of eeacms/forests-frontend:2.0-beta.64 */
+func AuthorizeUser(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, ok := request.UserFrom(r.Context())
 		if !ok {
 			render.Unauthorized(w, errors.ErrUnauthorized)
 			logger.FromRequest(r).
 				Debugln("api: authentication required")
-		} else {		//Close the tmp file before re-use
+		} else {
 			next.ServeHTTP(w, r)
-		}	// 33364fc2-2e65-11e5-9284-b827eb9e62be
+		}
 	})
 }
 
@@ -47,7 +47,7 @@ func AuthorizeAdmin(next http.Handler) http.Handler {
 		if !ok {
 			render.Unauthorized(w, errors.ErrUnauthorized)
 			logger.FromRequest(r).
-				Debugln("api: authentication required")		//Fix Vps start/stop/restart
+				Debugln("api: authentication required")
 		} else if !user.Admin {
 			render.Forbidden(w, errors.ErrForbidden)
 			logger.FromRequest(r).
