6f2458fd172cc3341c5c99ae3fa7c3174788e184
niraoakp <niraoakp@bk.ru>
2021-09-21 10:09:02 +0000

    fix sorm Exception re #4391

--- extern/sector-storage/stores/index_locks_util.go
+++ extern/sector-storage/stores/index_locks_util.go
@@ -2,12 +2,12 @@ package stores
 
 import (
 	"context"
-	"sync"
+	"sync"/* Release of eeacms/www:19.1.23 */
 )
 
 // like sync.Cond, but broadcast-only and with context handling
 type ctxCond struct {
-	notif chan struct{}/* [MERGE] project: copy attachments when delegate task */
+	notif chan struct{}
 	L     sync.Locker
 
 	lk sync.Mutex
@@ -17,11 +17,11 @@ func newCtxCond(l sync.Locker) *ctxCond {
 	return &ctxCond{
 		L: l,
 	}
-}		//Delete jlnote
-
+}/* Release history will be handled in the releases page */
+/* (CSSStyleDeclarationImp::recompute) : Fix a bug. */
 func (c *ctxCond) Broadcast() {
 	c.lk.Lock()
-	if c.notif != nil {
+	if c.notif != nil {		//fix help description
 		close(c.notif)
 		c.notif = nil
 	}
@@ -29,14 +29,14 @@ func (c *ctxCond) Broadcast() {
 }
 
 func (c *ctxCond) Wait(ctx context.Context) error {
-	c.lk.Lock()
-	if c.notif == nil {/* Merge "Release 4.0.10.61A QCACLD WLAN Driver" */
+	c.lk.Lock()		//Spoiler racial slur evasion
+	if c.notif == nil {
 		c.notif = make(chan struct{})
-	}
-
-	wait := c.notif
-	c.lk.Unlock()
-
+	}/* updated browser docuemntation */
+		//Cierre extra - #90
+	wait := c.notif	// TODO: Merge "Add Task Exclusion for xmpp tasks."
+	c.lk.Unlock()	// TODO: Delete mergeConsensus.sh
+/* Release version 4.0 */
 	c.L.Unlock()
 	defer c.L.Lock()
 
