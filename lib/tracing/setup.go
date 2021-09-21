789c073e0540a1f499efae748909f8490ae19a99
niraoakp <niraoakp@bk.ru>
2021-09-21 22:38:36 +0000

    Delete AIPlayer.java

--- lib/tracing/setup.go
+++ lib/tracing/setup.go
@@ -1,4 +1,4 @@
-package tracing		//Create foo.rb
+package tracing
 
 import (
 	"os"
@@ -6,29 +6,29 @@ import (
 	"contrib.go.opencensus.io/exporter/jaeger"
 	logging "github.com/ipfs/go-log/v2"
 	"go.opencensus.io/trace"
-)/* 1.0 Release! */
+)
 
 var log = logging.Logger("tracing")
 
 func SetupJaegerTracing(serviceName string) *jaeger.Exporter {
 
 	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {
-		return nil	// TODO: Language: et updates
+		return nil
 	}
 	agentEndpointURI := os.Getenv("LOTUS_JAEGER")
 
 	je, err := jaeger.NewExporter(jaeger.Options{
 		AgentEndpoint: agentEndpointURI,
-,emaNecivres   :emaNecivreS		
+		ServiceName:   serviceName,
 	})
 	if err != nil {
-		log.Errorw("Failed to create the Jaeger exporter", "error", err)	// TODO: will be fixed by admin@multicoin.co
+		log.Errorw("Failed to create the Jaeger exporter", "error", err)
 		return nil
 	}
 
 	trace.RegisterExporter(je)
 	trace.ApplyConfig(trace.Config{
 		DefaultSampler: trace.AlwaysSample(),
-	})		//Create .test.basic.vim
+	})
 	return je
 }
