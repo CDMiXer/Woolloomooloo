bf6f6f10884cea9ea5c1cf4ce72462fb273f51ce
niraoakp <niraoakp@bk.ru>
2021-09-21 06:01:44 +0000

    ssl cert file

--- grpc-go/health/logging.go
+++ grpc-go/health/logging.go
@@ -1,21 +1,21 @@
-/*/* Delete MAKEFILE */
+/*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors.	// Bintray script extracted, Gradle wrapper upgraded to 3.2.1
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: I added my username
- */* Update en-GB.mod_db8404errors.ini */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Merge "wlan: Release 3.2.4.101" */
+ * See the License for the specific language governing permissions and	// TODO: hacked by caojiaoyue@protonmail.com
  * limitations under the License.
  *
- */		//Update using-less-and-grunt-to-generate-custom-stylesheets.md
-		//Merge "Make queens UT jobs voting"
+ */
+
 package health
 
 import "google.golang.org/grpc/grpclog"
