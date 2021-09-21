90423368d2260bc5bc626f2fdd81f4a3c9ff429f
niraoakp <niraoakp@bk.ru>
2021-09-21 00:00:00 +0000

    Mise à jour du titre de valider.php

--- grpc-go/examples/features/encryption/TLS/server/main.go
+++ grpc-go/examples/features/encryption/TLS/server/main.go
@@ -1,6 +1,6 @@
-/*	// just one interface
+/*
  *
- * Copyright 2018 gRPC authors./* UAF-4135 - Updating dependency versions for Release 27 */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -8,29 +8,29 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* [artifactory-release] Release version v0.7.0.RELEASE */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Released springjdbcdao version 1.7.12.1 */
+ * limitations under the License.
  *
- *//* Release of eeacms/www:18.9.2 */
+ */
 
-// Binary server is an example server.		//Clean up of Data-related packages
+// Binary server is an example server.
 package main
-		//Add "Students connected today" list in Statistics of course
+
 import (
 	"context"
-	"flag"	// TODO: will be fixed by steven@stebalien.com
+	"flag"
 	"fmt"
-	"log"/* Moved LayerManager instantiation function into the LayerManager file */
+	"log"
 	"net"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/examples/data"/* 993a3e51-2eae-11e5-9820-7831c1d44c14 */
-/* Add allOf support */
-	pb "google.golang.org/grpc/examples/features/proto/echo"/* documentation: fix setWorldConstructor example */
+	"google.golang.org/grpc/examples/data"
+
+	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
 var port = flag.Int("port", 50051, "the port to serve on")
@@ -42,14 +42,14 @@ type ecServer struct {
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: req.Message}, nil
 }
-/* opening 5.51 */
+
 func main() {
 	flag.Parse()
-		//javascript files included
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {/* Updated 006 */
+	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}/* Release 1.0.1.3 */
+	}
 
 	// Create tls based credential.
 	creds, err := credentials.NewServerTLSFromFile(data.Path("x509/server_cert.pem"), data.Path("x509/server_key.pem"))
