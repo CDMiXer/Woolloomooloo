f8652253adec9fde33462860566ec0d064060463
niraoakp <niraoakp@bk.ru>
2021-09-21 00:48:20 +0000

    Удвой слова

--- core/trigger_test.go
+++ core/trigger_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Fixing up euca_conf --check to provide better checks and better messages.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Merge "const correctness, validPixel test." */
 // +build !oss
 
-package core	// TODO: 42a683ba-2e5a-11e5-9284-b827eb9e62be
+package core
