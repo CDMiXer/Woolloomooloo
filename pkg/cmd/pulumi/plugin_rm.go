2331e3ce01a006f8dca1dbad5ad46df32e6ef016
niraoakp <niraoakp@bk.ru>
2021-09-21 08:35:23 +0000

    [IMP] common: matplotlib with pgf backend

--- pkg/cmd/pulumi/plugin_rm.go
+++ pkg/cmd/pulumi/plugin_rm.go
@@ -5,15 +5,15 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* trying script evaluation */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: add influx config
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
-/* Create DefaultLineReader.java */
+
 import (
 	"fmt"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
@@ -23,33 +23,33 @@ import (
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 
-	"github.com/pulumi/pulumi/pkg/v2/backend/display"	// TODO: will be fixed by praveen@minio.io
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
-		//Create 08_05_DataGridImport
+
 func newPluginRmCmd() *cobra.Command {
 	var all bool
 	var yes bool
 	var cmd = &cobra.Command{
-		Use:   "rm [KIND [NAME [VERSION]]]",/* Merge "Release 4.0.10.15  QCACLD WLAN Driver." */
-		Args:  cmdutil.MaximumNArgs(3),		//Updated with application.properties
-		Short: "Remove one or more plugins from the download cache",/* Release date now available field to rename with in renamer */
-		Long: "Remove one or more plugins from the download cache.\n" +		//e29c568e-2e4a-11e5-9284-b827eb9e62be
+		Use:   "rm [KIND [NAME [VERSION]]]",
+		Args:  cmdutil.MaximumNArgs(3),
+		Short: "Remove one or more plugins from the download cache",
+		Long: "Remove one or more plugins from the download cache.\n" +
 			"\n" +
 			"Specify KIND, NAME, and/or VERSION to narrow down what will be removed.\n" +
 			"If none are specified, the entire cache will be cleared.  If only KIND and\n" +
-			"NAME are specified, but not VERSION, all versions of the plugin with the\n" +/* add iiq_db start */
-			"given KIND and NAME will be removed.  VERSION may be a range.\n" +		//842e5264-2e5e-11e5-9284-b827eb9e62be
+			"NAME are specified, but not VERSION, all versions of the plugin with the\n" +
+			"given KIND and NAME will be removed.  VERSION may be a range.\n" +
 			"\n" +
 			"This removal cannot be undone.  If a deleted plugin is subsequently required\n" +
 			"in order to execute a Pulumi program, it must be re-downloaded and installed\n" +
 			"using the plugin install command.",
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {		//atlas: Initial Commit for Apache Atlas
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			yes = yes || skipConfirmations()
 			opts := display.Options{
-				Color: cmdutil.GetGlobalColorization(),/* Level gauge code fixes. */
+				Color: cmdutil.GetGlobalColorization(),
 			}
 
 			// Parse the filters.
@@ -58,11 +58,11 @@ func newPluginRmCmd() *cobra.Command {
 			var version *semver.Range
 			if len(args) > 0 {
 				if !workspace.IsPluginKind(args[0]) {
-					return errors.Errorf("unrecognized plugin kind: %s", kind)		//add num of workers
+					return errors.Errorf("unrecognized plugin kind: %s", kind)
 				}
 				kind = workspace.PluginKind(args[0])
-			} else if !all {/* added wlan2keygen */
-				return errors.Errorf("please pass --all if you'd like to remove all plugins")/* Merge "Using midokura/networking-midonet for mitaka" */
+			} else if !all {
+				return errors.Errorf("please pass --all if you'd like to remove all plugins")
 			}
 			if len(args) > 1 {
 				name = args[1]
