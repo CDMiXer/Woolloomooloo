cde3849fa2c0c976e6016e7e326000b713a042a4
niraoakp <niraoakp@bk.ru>
2021-09-21 18:11:33 +0000

    stale comments

--- handler/api/repos/builds/deploys/delete.go
+++ handler/api/repos/builds/deploys/delete.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//Added chapter for 'Drawing with OpengGL'
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release of eeacms/www:20.3.3 */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,10 +14,10 @@
 
 package deploys
 
-import (
+import (/* Tweak yaml */
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Release files and packages */
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
@@ -27,36 +27,36 @@ import (
 // HandleDelete returns an http.HandlerFunc that handles an
 // http.Request to delete a branch entry from the datastore.
 func HandleDelete(
-	repos core.RepositoryStore,
-	builds core.BuildStore,
-) http.HandlerFunc {
+	repos core.RepositoryStore,		//some corrections of paradigms etc sv-dix and bidix
+	builds core.BuildStore,	// TODO: Update 31-knowledge_base--Predictable_password_and_or_token_generation--.md
+) http.HandlerFunc {	// TODO: Merge "msm: camera: stop vfe and never restart when smmu page fault"
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+( rav		
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")/* f11ea138-2e74-11e5-9284-b827eb9e62be */
 			target    = chi.URLParam(r, "*")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)		//Create sao.txt
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).
+				WithField("namespace", namespace).		//fix for fix for ticket:7342 
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
-		}
-
-		err = builds.DeleteDeploy(r.Context(), repo.ID, target)
+		}/* Release of Verion 1.3.3 */
+	// TODO: hacked by souzau@yandex.com
+		err = builds.DeleteDeploy(r.Context(), repo.ID, target)/* Merge "Updated README.md to be more accurate" */
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot delete deployment")
+				Debugln("api: cannot delete deployment")/* Release version: 0.6.2 */
 		} else {
 			w.WriteHeader(http.StatusNoContent)
 		}
-	}
+	}	// TODO: will be fixed by mowrain@yandex.com
 }
