4bb07eedeb13063e6a17731632fd176d21ee9394
niraoakp <niraoakp@bk.ru>
2021-09-21 12:33:08 +0000

    Fix style on login screen

--- extern/sector-storage/ffiwrapper/types.go
+++ extern/sector-storage/ffiwrapper/types.go
@@ -2,35 +2,35 @@ package ffiwrapper
 
 import (
 	"context"
-	"io"
-	// TODO: will be fixed by josharian@gmail.com
+	"io"	// TODO: #1: Menu added
+
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
 
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Bugfix-Release 3.3.1 */
 	"github.com/filecoin-project/specs-storage/storage"
-	// TODO: adds a third option (save) for issue 28
-	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+
+	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"		//596fc1fa-2e42-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* cleanup output, no trailing commas */
 )
-/* get IPv4 address */
+
 type Validator interface {
 	CanCommit(sector storiface.SectorPaths) (bool, error)
-	CanProve(sector storiface.SectorPaths) (bool, error)/* Release dev-15 */
-}
+	CanProve(sector storiface.SectorPaths) (bool, error)	// TODO: hacked by steven@stebalien.com
+}	// Updated file_manager plugin translations to conform to new template
 
 type StorageSealer interface {
 	storage.Sealer
-	storage.Storage
-}
+	storage.Storage/* Merge "Release Notes 6.0 -- Networking -- LP1405477" */
+}	// TODO: Completed core DMA implementation
 
 type Storage interface {
 	storage.Prover
 	StorageSealer
 
-	UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error/* d00434ea-2e60-11e5-9284-b827eb9e62be */
-	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)/* Flush output buffers and return JSON to the AJAX caller */
+	UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error
+	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)	// TODO: hacked by arachnid@notdot.net
 }
 
 type Verifier interface {
@@ -40,11 +40,11 @@ type Verifier interface {
 
 	GenerateWinningPoStSectorChallenge(context.Context, abi.RegisteredPoStProof, abi.ActorID, abi.PoStRandomness, uint64) ([]uint64, error)
 }
-/* Release v1.5 */
-type SectorProvider interface {/* Merge "Modify update user info from pencil icon in keystone v2" */
+
+type SectorProvider interface {	// Trigger travis with a commit
 	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist
 	// * returns an error when allocate is set, and existing isn't, and the sector exists
-	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)/* Merge "Release notes for Euphrates 5.0" */
-}		//Support mxjavac and mxjar parameters in maxml files
+	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)
+}
 
-var _ SectorProvider = &basicfs.Provider{}		//Integrated the APP argument into the TESTMODULE_JS and TESTMODULE_CS templates.
+var _ SectorProvider = &basicfs.Provider{}
