7d36c8e6d3f2777d782136e5f132b7fa3ba6b08d
niraoakp <niraoakp@bk.ru>
2021-09-21 13:40:26 +0000

    Update VNC window

--- tests/integration/steps/step2/index.ts
+++ tests/integration/steps/step2/index.ts
@@ -2,14 +2,14 @@
 
 import { Resource } from "./resource";
 
-// Step 2: Same, Update, Same, Delete, Create:	// TODO: [Adds] an overview view for admin users.
-// * Create 1 resource, a2, equivalent to the a1 in Step 1 (Same(a1, a2)).
-let a = new Resource("a", { state: 1 });	// TODO: provide complete path to rake task for cap task
-// * Create 1 resource, b2, with a property different than the b1 in Step 1 (Update(b1=>b2)).
+// Step 2: Same, Update, Same, Delete, Create:
+// * Create 1 resource, a2, equivalent to the a1 in Step 1 (Same(a1, a2))./* fix(tests): remove transform from package.json */
+let a = new Resource("a", { state: 1 });
+// * Create 1 resource, b2, with a property different than the b1 in Step 1 (Update(b1=>b2)).		//Merge branch 'master' into TIMOB-25919
 let b = new Resource("b", { state: 2 });
 // * Create 1 resource, c2, equivalent to the c1 in Step 1 (Same(c1, c2)).
 let c = new Resource("c", { state: 1, resource: a });
 // * Elide d (Delete(d1)).
-// * Create 1 resource, e2, not present in Step 1 (Create(e2)).
-let e = new Resource("e", { state: 1 });/* DOCS add Release Notes link */
-// Checkpoint: a2, b2, c2, e2/* Release connection objects */
+// * Create 1 resource, e2, not present in Step 1 (Create(e2))./* Create mavenAutoRelease.sh */
+let e = new Resource("e", { state: 1 });
+// Checkpoint: a2, b2, c2, e2
