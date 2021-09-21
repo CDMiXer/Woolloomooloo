f4574c207a22c9ffd2cddec53cd8303c95b39fc0
niraoakp <niraoakp@bk.ru>
2021-09-21 19:27:26 +0000

    Explain weird spree override

--- handler/api/repos/builds/deploys/create_test.go
+++ handler/api/repos/builds/deploys/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
-package deploys/* Add space to recursive children */
+// that can be found in the LICENSE file.	// TODO: ADD: added suport of captcha in DSS
+	// modify druid configuration
+package deploys
