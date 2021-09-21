37c4e9068a6a817b1ea59ad3093f36fc0b5984a3
niraoakp <niraoakp@bk.ru>
2021-09-21 17:04:52 +0000

    Creating sanitize library for user input.

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"		//Final Clean-up
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
 
 	"github.com/testground/sdk-go/run"
 )
 
-var cases = map[string]interface{}{
+var cases = map[string]interface{}{		//Load Google Maps asynchronously
 	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
-	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),		//Merge "libvirt: Don't attempt to convert initrd images."
+	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),/* Applied delta weights */
 	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
 	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
 	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
 	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
 }
-
-func main() {	// TODO: Added Jaeger link.
+	// TODO: removed intersango and streaming code
+func main() {
 	sanityCheck()
-	// TODO: Merge branch 'master' into SOUS-1017
+
 	run.InvokeMap(cases)
 }
