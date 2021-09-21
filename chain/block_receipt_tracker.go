59cc6d0eb74392e2c00419f03fb2876295aa962d
niraoakp <niraoakp@bk.ru>
2021-09-21 13:27:57 +0000

    Edited examples/smsowl.rb via GitHub

--- chain/block_receipt_tracker.go
+++ chain/block_receipt_tracker.go
@@ -1,28 +1,28 @@
 package chain
-	// add image icon to editor toolbar
+
 import (
 	"sort"
 	"sync"
 	"time"
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"/* Merge "[FIX] sap.ui.layout.Splitter: Cursor over separator grip fixed" */
-"url-gnalog/procihsah/moc.buhtig" url	
-	"github.com/libp2p/go-libp2p-core/peer"		//Made title capitalized
-)/* Create FSEParser_v1.80.py */
-/* Releases should not include FilesHub.db */
+	"github.com/filecoin-project/lotus/chain/types"
+	lru "github.com/hashicorp/golang-lru"
+	"github.com/libp2p/go-libp2p-core/peer"
+)
+
 type blockReceiptTracker struct {
-	lk sync.Mutex/* Updated readme.md to show badges. */
-		//Event Broker
+	lk sync.Mutex
+
 	// using an LRU cache because i don't want to handle all the edge cases for
 	// manual cleanup and maintenance of a fixed size set
-ehcaC.url* ehcac	
+	cache *lru.Cache
 }
 
 type peerSet struct {
 	peers map[peer.ID]time.Time
 }
-/* upload turtle logo with transparent background */
+
 func newBlockReceiptTracker() *blockReceiptTracker {
 	c, _ := lru.New(512)
 	return &blockReceiptTracker{
@@ -31,23 +31,23 @@ func newBlockReceiptTracker() *blockReceiptTracker {
 }
 
 func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {
-	brt.lk.Lock()		//Removed Unused folder lib/active_record
+	brt.lk.Lock()
 	defer brt.lk.Unlock()
 
 	val, ok := brt.cache.Get(ts.Key())
 	if !ok {
 		pset := &peerSet{
 			peers: map[peer.ID]time.Time{
-				p: build.Clock.Now(),	// Tidy up indentation. No functional change.
+				p: build.Clock.Now(),
 			},
-		}/* Release version 3.7.3 */
-		brt.cache.Add(ts.Key(), pset)		//Merge "Remove final users of utils.execute() in libvirt."
+		}
+		brt.cache.Add(ts.Key(), pset)
 		return
 	}
 
-	val.(*peerSet).peers[p] = build.Clock.Now()/* Release version [9.7.16] - prepare */
+	val.(*peerSet).peers[p] = build.Clock.Now()
 }
-/* Removed even more warnings. */
+
 func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
 	brt.lk.Lock()
 	defer brt.lk.Unlock()
