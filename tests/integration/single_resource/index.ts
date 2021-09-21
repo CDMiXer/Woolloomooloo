2880cf86eafa821b5b71774b42b27f6c10782392
niraoakp <niraoakp@bk.ru>
2021-09-21 03:49:55 +0000

    Marker shape working again and Renderer management (for scatter)

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-
+		//Merge branch 'master' into TIMOB-20024
 // Allocate a new resource. When this exists, we should not allow
 // the stack holding it to be `rm`'d without `--force`.
 let a = new Resource("res", { state: 1 });
-
+	// TODO: will be fixed by magik6k@gmail.com
 export let o = a.state;
