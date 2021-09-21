4c8575e222c6577b4f24c275d0804114cacc8d96
niraoakp <niraoakp@bk.ru>
2021-09-21 11:06:14 +0000

    LDEV-5095 Initial implementation of learner complection chart

--- grpc-go/internal/channelz/util_nonlinux.go
+++ grpc-go/internal/channelz/util_nonlinux.go
@@ -5,20 +5,20 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// Create nf.js
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* added leave balance to staff leave requests view */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release information */
- * See the License for the specific language governing permissions and	// TODO: Minor: Update project properties for embedded Pentaho DI 4.4.0.
- * limitations under the License.		//Merge "Revert "[workaround] Set region for packtack jobs explicitely""
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
-	// TODO: hacked by steven@stebalien.com
-package channelz		//nombre actualizado
+
+package channelz
 
 // GetSocketOption gets the socket option info of the conn.
 func GetSocketOption(c interface{}) *SocketOptionData {
