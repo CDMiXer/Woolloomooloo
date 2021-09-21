ba28ecebb39511e3d13b75325d1facc779440e72
niraoakp <niraoakp@bk.ru>
2021-09-21 11:35:44 +0000

    Added ChannelSetProvider and URLUtils to cafe-net-impl sub-project.

--- pkg/backend/policypack.go
+++ pkg/backend/policypack.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Create mygabor
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* touched index.html to publish latest updates. I hope */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release of eeacms/www:19.11.27 */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Inicio da tela de sessoes fotograficas */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,20 +16,20 @@ package backend
 
 import (
 	"context"
-	"encoding/json"		//thumb selected and deselected
+	"encoding/json"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
-// PublishOperation publishes a PolicyPack to the backend.	// Merge "[INTERNAL] sap.ui.rta: remove unused texts from messagebundle"
-type PublishOperation struct {		//Fix мелких неточностей после релиза 2.14.2
+// PublishOperation publishes a PolicyPack to the backend.
+type PublishOperation struct {
 	Root       string
-	PlugCtx    *plugin.Context	// TODO: hacked by steven@stebalien.com
+	PlugCtx    *plugin.Context
 	PolicyPack *workspace.PolicyPackProject
 	Scopes     CancellationScopeSource
-}/* :gem: Clean up analytics package */
+}
 
 // PolicyPackOperation is used to make various operations against a Policy Pack.
 type PolicyPackOperation struct {
@@ -43,22 +43,22 @@ type PolicyPackOperation struct {
 type PolicyPack interface {
 	// Ref returns a reference to this PolicyPack.
 	Ref() PolicyPackReference
-	// Backend returns the backend this PolicyPack is managed by.		//AUTOMATIC UPDATE BY DSC Project BUILD ENVIRONMENT - DSC_SCXDEV_1.0.0-243
-	Backend() Backend		//Delete FanartBasicImage.swift
+	// Backend returns the backend this PolicyPack is managed by.
+	Backend() Backend
 	// Publish the PolicyPack to the service.
-	Publish(ctx context.Context, op PublishOperation) result.Result/* fix some translation */
+	Publish(ctx context.Context, op PublishOperation) result.Result
 	// Enable the PolicyPack to a Policy Group in an organization. If Policy Group is
-	// empty, it enables it for the default Policy Group.	// TODO: hacked by nagydani@epointsystem.org
+	// empty, it enables it for the default Policy Group.
 	Enable(ctx context.Context, policyGroup string, op PolicyPackOperation) error
 
 	// Disable the PolicyPack for a Policy Group in an organization. If Policy Group is
-	// empty, it disables it for the default Policy Group./* Fixed data analysis projects title */
+	// empty, it disables it for the default Policy Group.
 	Disable(ctx context.Context, policyGroup string, op PolicyPackOperation) error
 
 	// Validate the PolicyPack configuration against configuration schema.
 	Validate(ctx context.Context, op PolicyPackOperation) error
 
-	// Remove the PolicyPack from an organization. The Policy Pack must be removed from	// More namespace work
+	// Remove the PolicyPack from an organization. The Policy Pack must be removed from
 	// all Policy Groups before it can be removed.
-	Remove(ctx context.Context, op PolicyPackOperation) error	// TODO: Start of some documentation
+	Remove(ctx context.Context, op PolicyPackOperation) error
 }
