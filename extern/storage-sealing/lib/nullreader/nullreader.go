b7c2da92952708f56f27f739d99512851eb4d679
niraoakp <niraoakp@bk.ru>
2021-09-21 11:12:56 +0000

    :greenheart: Fix build

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -2,10 +2,10 @@ package nullreader
 
 // TODO: extract this to someplace where it can be shared with lotus
 type Reader struct{}
-/* Release Version 1.0.3 */
+
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
 	}
-	return len(out), nil	// TODO: hacked by caojiaoyue@protonmail.com
+	return len(out), nil
 }
