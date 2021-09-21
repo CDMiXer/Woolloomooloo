6019f75ccc6739c9fdc0b79789b1d22e04e2af44
niraoakp <niraoakp@bk.ru>
2021-09-21 10:43:03 +0000

    Added comment for copying hints across layers

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,20 +1,20 @@
 package modules
-	// TODO: 5ae1d880-2f86-11e5-bec2-34363bc765d8
-import (
-	"go.uber.org/fx"
-/* Fix SimSubstRule1. Unfortunately it makes one of the other tests take forever */
+
+import (	// Merge branch 'master' into feature/emoji-custom
+	"go.uber.org/fx"		//Log restarting due to FFmpeg errors
+
 	"github.com/filecoin-project/lotus/chain/stmgr"
-	"github.com/filecoin-project/lotus/chain/store"
+	"github.com/filecoin-project/lotus/chain/store"/* Delete some more code */
 )
 
-func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {/* Java files and resources for the lock screen */
+func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
 	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
 	if err != nil {
 		return nil, err
-}	
-	lc.Append(fx.Hook{/* Release 2.2.0 */
-		OnStart: sm.Start,		//Create floatRange.py
+	}
+	lc.Append(fx.Hook{
+		OnStart: sm.Start,
 		OnStop:  sm.Stop,
-	})
+	})		//Merge "Disable Ceilometer API by default on undercloud"
 	return sm, nil
-}/* Updated for v2 */
+}
