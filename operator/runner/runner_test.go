954a9a13ad40f354ef2ee8bf1a9a7182e5b711df
niraoakp <niraoakp@bk.ru>
2021-09-21 00:52:12 +0000

    Update fetchers for Python 3

--- operator/runner/runner_test.go
+++ operator/runner/runner_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 3.2 105.02. */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package runner
 
-import (
+import (	// TODO: Fixed encoding test hopefully for the last time...
 	"io/ioutil"
-
+		//Create CNAM
 	"github.com/sirupsen/logrus"
 )
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)
+	logrus.SetOutput(ioutil.Discard)/* Look ma' no hands! */
 }
