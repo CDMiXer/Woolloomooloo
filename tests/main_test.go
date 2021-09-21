2479ad2cfbe333a4327142a3588e2610a8d50af9
niraoakp <niraoakp@bk.ru>
2021-09-21 10:01:46 +0000

    Fuck ......

--- tests/main_test.go
+++ tests/main_test.go
@@ -8,12 +8,12 @@ import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/filestate"
-)	// TODO: hacked by 13860583249@yeah.net
+)
 
 func TestMain(m *testing.M) {
 	// Disable stack backups for tests to avoid filling up ~/.pulumi/backups with unnecessary
-	// backups of test stacks.		//Splitted the SearchTask into 2 classes, SearchTask really got to big...
-	if err := os.Setenv(filestate.DisableCheckpointBackupsEnvVar, "1"); err != nil {/* upload.py: Handle moved files in SVN Changelist */
+	// backups of test stacks.
+	if err := os.Setenv(filestate.DisableCheckpointBackupsEnvVar, "1"); err != nil {
 		fmt.Printf("error setting env var '%s': %v\n", filestate.DisableCheckpointBackupsEnvVar, err)
 		os.Exit(1)
 	}
