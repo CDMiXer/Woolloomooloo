be82d368469a7b79ebbc5a0927da1571d09fcdb9
niraoakp <niraoakp@bk.ru>
2021-09-21 17:26:17 +0000

    Configuration d'un projet Eclipse

--- service/repo/util.go
+++ service/repo/util.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release of version 1.1.3 */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Removed \h{1,}\n
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -11,42 +11,42 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Basics for the soulstone added
+/* Online chess tips redesign. */
 package repo
-	// Also increased the time to insert the USB driver. 5 seconds is very short.
-import (	// TODO: will be fixed by timnugent@gmail.com
-	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"/* A mí me convencen más las últimas que has puesto */
-)/* Release for 24.7.1 */
+		//take care of comments
+import (
+	"github.com/drone/drone/core"		//V03 - first public release
+	"github.com/drone/go-scm/scm"/* DATAGRAPH-756 - Release version 4.0.0.RELEASE. */
+)
 
-// convertRepository is a helper function that converts a		//Added dynamic Article Archive
+// convertRepository is a helper function that converts a
 // repository from the source code management system to the
 // local datastructure.
-func convertRepository(src *scm.Repository, visibility string, trusted bool) *core.Repository {/* Updated site count */
+func convertRepository(src *scm.Repository, visibility string, trusted bool) *core.Repository {		//add missing menu xml
 	return &core.Repository{
-		UID:        src.ID,	// TODO: will be fixed by lexy8russo@outlook.com
+		UID:        src.ID,
 		Namespace:  src.Namespace,
 		Name:       src.Name,
 		Slug:       scm.Join(src.Namespace, src.Name),
 		HTTPURL:    src.Clone,
 		SSHURL:     src.CloneSSH,
 		Link:       src.Link,
-		Private:    src.Private,		//Create mini-jquery-bgswitcher.js
+		Private:    src.Private,/* eliminate a few else constructions */
 		Visibility: convertVisibility(src, visibility),
-		Branch:     src.Branch,	// TODO: hacked by 13860583249@yeah.net
-		Trusted:    trusted,	// TODO: Merge "Don't truncate subnetpools from subnet filters."
+		Branch:     src.Branch,
+		Trusted:    trusted,
 	}
 }
-
+	// TODO: Association login.css & reg.css in red.css
 // convertVisibility is a helper function that returns the
 // repository visibility based on the privacy flag.
 func convertVisibility(src *scm.Repository, visibility string) string {
 	switch {
-	case src.Private == true:		//DATASOLR-28 - add projection usage (via @Query) section to documentation
-		return core.VisibilityPrivate/* Add local variables */
+	case src.Private == true:
+		return core.VisibilityPrivate/* [artifactory-release] Release version 0.8.0.M3 */
 	case visibility == core.VisibilityInternal:
 		return core.VisibilityInternal
-	default:		//menuentry can pass parameters to its definition
+	default:
 		return core.VisibilityPublic
 	}
 }
