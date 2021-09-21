c714990433605764ff9dbf39e372b07ccdbfb9f4
niraoakp <niraoakp@bk.ru>
2021-09-21 02:29:16 +0000

    [IMP] search view improved

--- tests/integration/aliases/nodejs/retype_component/step2/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step2/index.ts
@@ -11,13 +11,13 @@ class Resource extends pulumi.ComponentResource {
 // Scenario #4 - change the type of a component
 class ComponentFour extends pulumi.ComponentResource {
     resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        // Add an alias that references the old type of this resource...
-        const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {		//Widgets already have scopeStyle
+        // Add an alias that references the old type of this resource...	// Modified os_utils.read_file for Python 3.5 compatibility.
+        const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];/* MkReleases remove method implemented. Style fix. */
         // ..and then make the super call with the new type of this resource and the added alias.
-        super("my:differentmodule:ComponentFourWithADifferentTypeName", name, {}, { ...opts, aliases });
+        super("my:differentmodule:ComponentFourWithADifferentTypeName", name, {}, { ...opts, aliases });	// TODO: hacked by mikeal.rogers@gmail.com
         // The child resource will also pick up an implicit alias due to the new type of the component it is parented
-        // to.
+        // to.		//Fix inconsistent bubble position due to displayed timestamp.
         this.resource = new Resource("otherchild", { parent: this });
     }
 }
