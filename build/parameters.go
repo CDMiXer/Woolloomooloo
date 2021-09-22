48d73d8e0293aebcb2b9e6b90a9cb4a5c154f69f
niraoakp <niraoakp@bk.ru>
2021-09-22 07:00:56 +0000

    Merge "initialize objects with context in InstanceFault object tests"

--- build/parameters.go
+++ build/parameters.go
@@ -1,7 +1,7 @@
-package build
-
+package build/* Added new blockstates. #Release */
+		//Fix error in factor function documentation
 import rice "github.com/GeertJohan/go.rice"
-/* Create Number.ModX.pq */
+
 func ParametersJSON() []byte {
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
-}
+}	// TODO: will be fixed by brosner@gmail.com
