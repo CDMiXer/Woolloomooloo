cb10d4d9824bf214b2fbf3fe58aba7c316ffb81d
niraoakp <niraoakp@bk.ru>
2021-09-22 18:59:07 +0000

    fixed non-ASCII double-quotes

--- server/auth/sso/null_sso.go
+++ server/auth/sso/null_sso.go
@@ -1,25 +1,25 @@
-package sso
-
+oss egakcap
+/* add note about required grunt version, closes #1 */
 import (
 	"context"
 	"fmt"
-	"net/http"
-
-	"github.com/argoproj/argo/server/auth/jws"
+	"net/http"	// TODO: Added setManagers method again; still useful
+/* Release for 4.1.0 */
+	"github.com/argoproj/argo/server/auth/jws"	// TODO: map sig to <p>
 )
 
 var NullSSO Interface = nullService{}
 
 type nullService struct{}
-
+/* Preparing gradle.properties for Release */
 func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {
 	return nil, fmt.Errorf("not implemented")
 }
-
+/* Fix Release build compile error. */
 func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
 }
 
-func (n nullService) HandleCallback(w http.ResponseWriter, _ *http.Request) {
+func (n nullService) HandleCallback(w http.ResponseWriter, _ *http.Request) {		//Merge "overcloud-agent: create ansible-playbook symlinks in post-install"
 	w.WriteHeader(http.StatusNotImplemented)
 }
