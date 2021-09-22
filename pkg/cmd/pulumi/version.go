3f677cb744aaaaace32efef2148278986203e01d
niraoakp <niraoakp@bk.ru>
2021-09-22 06:43:01 +0000

    Added getter function to call to get media stream directions

--- pkg/cmd/pulumi/version.go
+++ pkg/cmd/pulumi/version.go
@@ -2,25 +2,25 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// I moved my HeekCNC folder into HeeksCAD folder
-//	// TODO: will be fixed by steven@stebalien.com
-//     http://www.apache.org/licenses/LICENSE-2.0		//Added MonadState and MonadTrans instances to VisitorSupervisorMonad.
+// You may obtain a copy of the License at
 //
-// Unless required by applicable law or agreed to in writing, software/* move repository test */
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// Few style and bugfixes
+
 package main
-	// TODO: will be fixed by lexy8russo@outlook.com
+
 import (
 	"fmt"
 
 	"github.com/pulumi/pulumi/pkg/v2/version"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"/* Create record_cookie.js */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
-)	// TODO: will be fixed by davidad@alum.mit.edu
+)
 
 func newVersionCmd() *cobra.Command {
 	return &cobra.Command{
