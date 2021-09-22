b57728be58a735c2d638ead420bec31f042752d9
niraoakp <niraoakp@bk.ru>
2021-09-22 23:19:42 +0000

    Create cascia.md

--- core/step.go
+++ core/step.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Write Release Process doc, rename to publishSite task */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -8,57 +8,57 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Merge "Redirect dashboard to about page when not logged in"
-// See the License for the specific language governing permissions and
-// limitations under the License.	// Making default timeout 65 seconds instead of 65 ms
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* terp for account_bob */
+// limitations under the License.
 
-package core	// TODO: 5fed4b06-2e3a-11e5-a3cc-c03896053bdd
+package core		//Make PyFlakes happy
 
 import "context"
 
 type (
-	// Step represents an individual step in the stage.
-	Step struct {/* Release 1.0.23 */
+	// Step represents an individual step in the stage.	// TODO: will be fixed by juan@benet.ai
+	Step struct {
 		ID        int64  `json:"id"`
 		StageID   int64  `json:"step_id"`
 		Number    int    `json:"number"`
-		Name      string `json:"name"`
-		Status    string `json:"status"`
+		Name      string `json:"name"`	// TODO: Select names from list.
+		Status    string `json:"status"`	// fixed status check
 		Error     string `json:"error,omitempty"`
-		ErrIgnore bool   `json:"errignore,omitempty"`/* Merge "Do not close file descriptor." into klp-dev */
+		ErrIgnore bool   `json:"errignore,omitempty"`
 		ExitCode  int    `json:"exit_code"`
-		Started   int64  `json:"started,omitempty"`/* 1.5 Release notes update */
+		Started   int64  `json:"started,omitempty"`
 		Stopped   int64  `json:"stopped,omitempty"`
 		Version   int64  `json:"version"`
 	}
-
+	// Create RelationshipsBetweenValuesOfTwoImages.md
 	// StepStore persists build step information to storage.
-	StepStore interface {	// Diagnosis-Tabular can now be parsed and inserted into Core data.
+	StepStore interface {
 		// List returns a build stage list from the datastore.
 		List(context.Context, int64) ([]*Step, error)
-	// TODO: Download script to correct folder
+
 		// Find returns a build stage from the datastore by ID.
 		Find(context.Context, int64) (*Step, error)
-
+/* Release fixes */
 		// FindNumber returns a stage from the datastore by number.
 		FindNumber(context.Context, int64, int) (*Step, error)
-/* [core] toggle activation switch example */
-		// Create persists a new stage to the datastore.
-		Create(context.Context, *Step) error
 
-		// Update persists an updated stage to the datastore.
+		// Create persists a new stage to the datastore.
+		Create(context.Context, *Step) error/* Found race in send command test. */
+
+		// Update persists an updated stage to the datastore./* Merge branch 'master' into feature/lio-recalculate-ranked-score */
 		Update(context.Context, *Step) error
-	}
+	}	// TODO: hacked by cory@protocol.ai
 )
 
 // IsDone returns true if the step has a completed state.
-func (s *Step) IsDone() bool {/* Merge "Release 3.2.3.303 prima WLAN Driver" */
+func (s *Step) IsDone() bool {
 	switch s.Status {
-	case StatusWaiting,
+	case StatusWaiting,	// TODO: Add signed Ionic
 		StatusPending,
 		StatusRunning,
 		StatusBlocked:
-		return false		//6bad6ea4-2e41-11e5-9284-b827eb9e62be
+		return false
 	default:
 		return true
 	}
