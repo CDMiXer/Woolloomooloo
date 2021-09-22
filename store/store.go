69d7fd05405573e83be956d5391c5b484b39672f
niraoakp <niraoakp@bk.ru>
2021-09-22 09:29:33 +0000

    Updated apache passenger (markdown)

--- store/store.go
+++ store/store.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Update PublicBeta_ReleaseNotes.md */
-// you may not use this file except in compliance with the License./* remove warning from direnv (will work with the rest of ruby managers)  */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* Release v4.1.11 [ci skip] */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// 5974a42a-2e62-11e5-9284-b827eb9e62be
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Use stroke only if different from fill
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Merge "Make sure str->str map types have string, string keys and values"
+// limitations under the License.
 
 package store
