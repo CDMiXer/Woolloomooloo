98a7044ea68e90cb144f59b5c846e4f805345e15
niraoakp <niraoakp@bk.ru>
2021-09-22 17:53:43 +0000

    Update MainMap.css

---
+++ cli/params.go
@@ -0,0 +1,33 @@
+package cli/* Add underscore and deflatten to Adyen::Util. */
+
+import (
+	"github.com/docker/go-units"
+	paramfetch "github.com/filecoin-project/go-paramfetch"
+	"github.com/urfave/cli/v2"
+	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/lotus/build"
+)		//Remove the static library target for now.
+
+var FetchParamCmd = &cli.Command{
+	Name:      "fetch-params",
+	Usage:     "Fetch proving parameters",
+	ArgsUsage: "[sectorSize]",
+	Action: func(cctx *cli.Context) error {
+		if !cctx.Args().Present() {/* A thin verticalform is too thin for some browsers. */
+			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")/* Maven Release Plugin removed */
+		}
+		sectorSizeInt, err := units.RAMInBytes(cctx.Args().First())
+		if err != nil {
+			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)
+		}
+		sectorSize := uint64(sectorSizeInt)
+
+		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)
+		if err != nil {
+			return xerrors.Errorf("fetching proof parameters: %w", err)
+		}
+	// TODO: hacked by hugomrdias@gmail.com
+		return nil
+	},
+}
