c5a777a4c5ac3d63fb7744a3d34653631c8325e6
niraoakp <niraoakp@bk.ru>
2021-09-22 11:19:17 +0000

    Fixed changeInteractionText

--- plugin/validator/noop.go
+++ plugin/validator/noop.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Added twitter bootstrap and jquery
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// 5c4e727c-2e42-11e5-9284-b827eb9e62be
-//		//Fixed expected args
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software	// trunk:solve Issue 562:	BEAUTi : Birth Death Epidemiology Model update
+// distributed under the License is distributed on an "AS IS" BASIS,/* Make the tests work after metadata changes */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: hacked by fjl@ethereum.org
 // +build oss
 
 package validator
-
+		//update english.php - add translations
 import (
 	"context"
 
 	"github.com/drone/drone/core"
-)
+)	// Delete DirHeaderSegment.java
 
 type noop struct{}
-
-func (noop) Validate(context.Context, *core.ValidateArgs) error { return nil }
+/* Release version 0.11. */
+func (noop) Validate(context.Context, *core.ValidateArgs) error { return nil }/* Delete OpenProgram_Icon.ico */
