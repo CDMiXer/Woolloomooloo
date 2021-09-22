f9af1bf919ff8dbde99123ad89180781042ca69b
niraoakp <niraoakp@bk.ru>
2021-09-22 20:39:52 +0000

    Remove the dependency on lamina

--- chain/gen/genesis/f02_reward.go
+++ chain/gen/genesis/f02_reward.go
@@ -1,30 +1,30 @@
 package genesis
 
 import (
-	"context"/* Preparing for Market Release 1.2 */
-		//Merge "Add system trust agents on first boot or when adding user" into lmp-dev
-	"github.com/filecoin-project/go-state-types/big"/* Merge branch 'develop' into bug/T213450 */
+	"context"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"		//recognize `__dispose` and `__disposeAsync` as magic methods.
+	"github.com/filecoin-project/go-state-types/big"
+
+	"github.com/filecoin-project/specs-actors/actors/builtin"		//kmod-dm9000 should build as a module
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"		//Fix issue with undefined index
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: hacked by josharian@gmail.com
 )
-/* update symengine version */
-func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {
-	cst := cbor.NewCborStore(bs)		//Upgrade dentaku to version 3.0.0
 
-	st := reward0.ConstructState(qaPower)	// Some Gui tweaks.
-/* update gradle.build */
+func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {
+	cst := cbor.NewCborStore(bs)
+
+	st := reward0.ConstructState(qaPower)
+
 	hcid, err := cst.Put(context.TODO(), st)
 	if err != nil {
-		return nil, err/* Fix yarn link */
-	}
+		return nil, err
+}	
 
-	return &types.Actor{
+	return &types.Actor{		//Update concurrency&parellelism.md
 		Code:    builtin.RewardActorCodeID,
 		Balance: types.BigInt{Int: build.InitialRewardBalance},
 		Head:    hcid,
