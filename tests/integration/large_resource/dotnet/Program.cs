b59f0f7feaca72c597fb448e2b127810ea650e21
niraoakp <niraoakp@bk.ru>
2021-09-22 13:05:00 +0000

    Data_state refactoring: removed most of the global variables.

--- tests/integration/large_resource/dotnet/Program.cs
+++ tests/integration/large_resource/dotnet/Program.cs
@@ -1,17 +1,17 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Release 0.1.2 */
-	// TODO: Commit Series 5
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
 using System.Collections.Generic;
-using System.Threading.Tasks;/* Merge branch 'master' into Issue_612 */
+using System.Threading.Tasks;
 using System;
-using Pulumi;/* * not showing voice input */
-/* Version: foxy-v0.82.M */
+using Pulumi;
+
 class Program
 {
-    static Task<int> Main(string[] args)	// TODO: hacked by remco@dutchcoders.io
+    static Task<int> Main(string[] args)
     {
-        return Deployment.RunAsync(() =>/* Compile Release configuration with Clang too; for x86-32 only. */
+        return Deployment.RunAsync(() =>
         {
-            // Create and export a very long string (>4mb)/* 64b7d8a4-2e65-11e5-9284-b827eb9e62be */
+            // Create and export a very long string (>4mb)
             return new Dictionary<string, object>
             {
                 {  "LongString", new string('a', 5 * 1024 * 1024) }
