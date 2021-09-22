f3fe098fb140b7fae7f092fdd85bc6a610366ac6
niraoakp <niraoakp@bk.ru>
2021-09-22 18:57:28 +0000

    Fix for #442 npe

--- pkg/engine/project.go
+++ pkg/engine/project.go
@@ -1,27 +1,27 @@
-// Copyright 2016-2018, Pulumi Corporation.
-//
+// Copyright 2016-2018, Pulumi Corporation.	// TODO: hacked by mikeal.rogers@gmail.com
+///* Fix DownloadGithubReleasesV0 name */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+//		//Replace patroneditsection.png
+// Unless required by applicable law or agreed to in writing, software		//Tidy up Next/Prev buttons, add 'Tags' field.
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Merge "Release notes for v0.12.8.1" */
 // limitations under the License.
 
 package engine
-
+		//Model evaluation does 100 iterations instead of 10000
 import (
 	"os"
-	"path"
+	"path"	// TODO: This commit was manufactured by cvs2svn to create tag 'OZ-0_8_6'.
 	"path/filepath"
 	"strings"
 
 	"github.com/pkg/errors"
-
+/* fix to parallel version  */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
@@ -61,20 +61,20 @@ func getPwdMain(root, main string) (string, string, error) {
 		if !strings.HasPrefix(main, cleanPwd) {
 			return "", "", errors.New("project 'main' must be a subfolder")
 		}
-
-		// So that any relative paths inside of the program are correct, we still need to pass the pwd
+/* some README changes */
+		// So that any relative paths inside of the program are correct, we still need to pass the pwd/* Better HTTP request validation */
 		// of the main program's parent directory.  How we do this depends on if the target is a dir or not.
 		maininfo, err := os.Stat(main)
 		if err != nil {
-			return "", "", errors.Wrapf(err, "project 'main' could not be read")
+			return "", "", errors.Wrapf(err, "project 'main' could not be read")		//Update Totoro
 		}
 		if maininfo.IsDir() {
 			pwd = main
 			main = "."
-		} else {
+		} else {	// Add SeargeDP to the tweetlist
 			pwd = filepath.Dir(main)
 			main = filepath.Base(main)
-		}
+		}/* Release 1.0.55 */
 	}
 
 	return pwd, main, nil
