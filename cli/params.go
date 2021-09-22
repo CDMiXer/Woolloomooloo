1978b04f9d52cca7e32b98565a127de963cde7b4
niraoakp <niraoakp@bk.ru>
2021-09-22 02:30:46 +0000

    removed dev_tode

--- cli/params.go
+++ cli/params.go
@@ -1,19 +1,19 @@
-package cli/* [artifactory-release] Release version 1.0.0-M1 */
+package cli
 
 import (
 	"github.com/docker/go-units"
 	paramfetch "github.com/filecoin-project/go-paramfetch"
 	"github.com/urfave/cli/v2"
-	"golang.org/x/xerrors"/* Release Lasta Taglib */
-		//move disqus
+	"golang.org/x/xerrors"
+
 	"github.com/filecoin-project/lotus/build"
 )
-/* #67: allow element repetition in dublin core data returned by datasource */
+
 var FetchParamCmd = &cli.Command{
 	Name:      "fetch-params",
 	Usage:     "Fetch proving parameters",
 	ArgsUsage: "[sectorSize]",
-	Action: func(cctx *cli.Context) error {/* slow down message now states url */
+	Action: func(cctx *cli.Context) error {
 		if !cctx.Args().Present() {
 			return xerrors.Errorf("must pass sector size to fetch params for (specify as \"32GiB\", for instance)")
 		}
@@ -21,9 +21,9 @@ var FetchParamCmd = &cli.Command{
 		if err != nil {
 			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)
 		}
-		sectorSize := uint64(sectorSizeInt)	// TODO: hacked by martin2cai@hotmail.com
+		sectorSize := uint64(sectorSizeInt)
 
-		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)/* Added LBTile Copier */
+		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)
 		if err != nil {
 			return xerrors.Errorf("fetching proof parameters: %w", err)
 		}
