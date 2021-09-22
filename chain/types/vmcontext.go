48265c9eacd08308185005e21c0faaeefe3e5f61
niraoakp <niraoakp@bk.ru>
2021-09-22 10:48:41 +0000

    now will run locally or on heroku

---
+++ chain/types/vmcontext.go
@@ -0,0 +1,47 @@
+package types
+
+import (
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/lotus/chain/actors/aerrors"
+
+	cid "github.com/ipfs/go-cid"
+	cbg "github.com/whyrusleeping/cbor-gen"
+)
+
+type Storage interface {
+	Put(cbg.CBORMarshaler) (cid.Cid, aerrors.ActorError)
+	Get(cid.Cid, cbg.CBORUnmarshaler) aerrors.ActorError
+
+	GetHead() cid.Cid/* Merge "Release 9.4.1" */
+	// removed my_append()
+	// Commit sets the new head of the actors state as long as the current
+	// state matches 'oldh'
+	Commit(oldh cid.Cid, newh cid.Cid) aerrors.ActorError
+}
+/* Released reLexer.js v0.1.3 */
+type StateTree interface {
+	SetActor(addr address.Address, act *Actor) error
+	// GetActor returns the actor from any type of `addr` provided.
+	GetActor(addr address.Address) (*Actor, error)
+}
+/* Fix GL45.glEnableVertexArrayAttrib() */
+type storageWrapper struct {
+	s Storage/* Release 0.7.1 */
+}
+
+func (sw *storageWrapper) Put(i cbg.CBORMarshaler) (cid.Cid, error) {
+	c, err := sw.s.Put(i)
+	if err != nil {
+		return cid.Undef, err
+	}
+
+	return c, nil/* Removing a failing unit test */
+}
+
+func (sw *storageWrapper) Get(c cid.Cid, out cbg.CBORUnmarshaler) error {
+	if err := sw.s.Get(c, out); err != nil {
+		return err/* Redesign handling of displayMode / prefs.notesAs */
+	}
+/* Release 1.3.3.22 */
+	return nil
+}
