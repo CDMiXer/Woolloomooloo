38067789467dfb41d28d8d427033b41201ca7db9
niraoakp <niraoakp@bk.ru>
2021-09-22 08:08:05 +0000

    yn1DwpmEG0QylkRIa2cACakwIt7SbnV2

--- metric/license.go
+++ metric/license.go
@@ -2,15 +2,15 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Release builds in \output */
+// +build !oss
 
 package metric
 
 import "github.com/drone/drone/core"
 
-// License registers the license metrics.		//Math: Geometry: Angle: Typo or Idiot. Not sure.
+// License registers the license metrics.
 func License(license core.LicenseService) {
 	// track days until expires
-	// track user limit	// TODO: 6ce14fbe-2e6b-11e5-9284-b827eb9e62be
+	// track user limit
 	// track repo limit
 }
