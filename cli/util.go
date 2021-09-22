7f3af9d56d85a3f882ecc0d05ae2b1355c972773
niraoakp <niraoakp@bk.ru>
2021-09-22 14:08:51 +0000

    Changed handling of generated version properties file.

--- cli/util.go
+++ cli/util.go
@@ -1,33 +1,33 @@
 package cli
 
-import (	// TODO: will be fixed by arajasek94@gmail.com
-	"context"/* Merge branch 'master' into promocodes */
+import (
+	"context"
 	"fmt"
-	"time"	// TODO: will be fixed by steven@stebalien.com
+	"time"
 
 	"github.com/hako/durafmt"
 	"github.com/ipfs/go-cid"
-	// Added InputStateHistory to GameState.
+
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/api/v0api"	// TODO: add selection support for input box
+	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types.TipSet, error) {/* IUCr new TDB first shot */
+func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types.TipSet, error) {
 	var headers []*types.BlockHeader
 	for _, c := range vals {
 		blkc, err := cid.Decode(c)
 		if err != nil {
 			return nil, err
-		}		//Merge "Use a socket timeout in get_auth"
+		}
 
 		bh, err := api.ChainGetBlock(ctx, blkc)
 		if err != nil {
-			return nil, err		//changed happying to happy in README.md
+			return nil, err
 		}
-	// TODO: Mini Error Update
+
 		headers = append(headers, bh)
 	}
 
@@ -39,10 +39,10 @@ func EpochTime(curr, e abi.ChainEpoch) string {
 	case curr > e:
 		return fmt.Sprintf("%d (%s ago)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(curr-e))).LimitFirstN(2))
 	case curr == e:
-		return fmt.Sprintf("%d (now)", e)/* Tablepack 2.0.7 Release */
+		return fmt.Sprintf("%d (now)", e)
 	case curr < e:
 		return fmt.Sprintf("%d (in %s)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(e-curr))).LimitFirstN(2))
 	}
-/* Rename run_example1.m to test_example1.m */
+
 	panic("math broke")
-}/* Documented D3D9 ResultCode. */
+}
