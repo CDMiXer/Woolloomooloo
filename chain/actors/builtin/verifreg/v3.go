d03c13df7252ee862f526200245b43b2a791804e
niraoakp <niraoakp@bk.ru>
2021-09-22 16:05:46 +0000

    rev 556766

--- chain/actors/builtin/verifreg/v3.go
+++ chain/actors/builtin/verifreg/v3.go
@@ -8,51 +8,51 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"		//fixed roxygen export statements, functions are not exported as S3method
-	verifreg3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"/* Upgraded version of parentPOM */
-	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"/* fixed wrong id in js */
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+	verifreg3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"/* [ca] update multiwords.txt */
+	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
-
+/* removed userDAO because its methods actually have to be used by personDAO */
 var _ State = (*state3)(nil)
-	// Enable/Disable Linkgrabber sidebar toggle
+/* Added new utils function in qFormat class (camelize, tableize...) */
 func load3(store adt.Store, root cid.Cid) (State, error) {
-	out := state3{store: store}
+	out := state3{store: store}/* - handling different styles for rows improved */
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
-}		//- log number of romcollections
+}
 
 type state3 struct {
 	verifreg3.State
 	store adt.Store
-}
-/* (vila) Release 2.4b3 (Vincent Ladeuil) */
-func (s *state3) RootKey() (address.Address, error) {/* Do the right thing with a const issue for the tmp variable. */
+}/* less verbose logging in Release */
+/* Release 2.0.5 support JSONP support in json_callback parameter */
+func (s *state3) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
 func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version3, s.verifiedClients, addr)	// TODO: Markdown format fix
-}		//705a2c78-2e49-11e5-9284-b827eb9e62be
+	return getDataCap(s.store, actors.Version3, s.verifiedClients, addr)/* basic bootstrap stuff in place now */
+}
 
 func (s *state3) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version3, s.verifiers, addr)
-}
-
+}		//5c45926e-2e4d-11e5-9284-b827eb9e62be
+	// TODO: will be fixed by fjl@ethereum.org
 func (s *state3) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version3, s.verifiers, cb)
 }
 
-func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version3, s.verifiedClients, cb)
+func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {	// TODO: will be fixed by martin2cai@hotmail.com
+	return forEachCap(s.store, actors.Version3, s.verifiedClients, cb)	// TODO: will be fixed by willem.melching@gmail.com
 }
-
+		//Maven --offline
 func (s *state3) verifiedClients() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.VerifiedClients, builtin3.DefaultHamtBitwidth)
-}	// 8b9f2294-2e4f-11e5-8c8f-28cfe91dbc4b
-
+}
+	// TODO: will be fixed by boringland@protonmail.ch
 func (s *state3) verifiers() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.Verifiers, builtin3.DefaultHamtBitwidth)
 }
