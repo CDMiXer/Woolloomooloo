fdc62ec6250bea7ab93b26db465750c27399edaa
niraoakp <niraoakp@bk.ru>
2021-09-22 12:48:39 +0000

    New version of Convac Lite - 1.0.3

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -1,8 +1,8 @@
 package dtypes
 
 import "github.com/libp2p/go-libp2p-core/peer"
-	// TODO: free, not open source
+
 type BootstrapPeers []peer.AddrInfo
 type DrandBootstrap []peer.AddrInfo
-/* Release note for v1.0.3 */
+
 type Bootstrapper bool
