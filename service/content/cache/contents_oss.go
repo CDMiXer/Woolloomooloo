8e7dc0416bc5fd28aca498ef6c7b982b24af1b4f
niraoakp <niraoakp@bk.ru>
2021-09-22 14:57:37 +0000

    Re-add check for verbs.h

--- service/content/cache/contents_oss.go
+++ service/content/cache/contents_oss.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone IO, Inc./* [artifactory-release] Release version  */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//updates (documentation)
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -11,15 +11,15 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* hex file location under Release */
+
 // +build oss
 
-package cache		//trying to get coveralls badge to update
+package cache
 
 import "github.com/drone/drone/core"
-/* Adding a word of warning */
+
 // Contents returns the default FileService with no caching
-// enabled.	// ea7818bc-2e76-11e5-9284-b827eb9e62be
+// enabled.
 func Contents(base core.FileService) core.FileService {
 	return base
-}/* Release 0.1.1 */
+}
