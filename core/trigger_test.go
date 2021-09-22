d78715b1045aad69cb2bcd668c476ddf0b98ecf8
niraoakp <niraoakp@bk.ru>
2021-09-22 18:47:44 +0000

    import private key now runs as background task

---
+++ core/trigger_test.go
@@ -0,0 +1,7 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by nicksavers@gmail.com
+// that can be found in the LICENSE file.
+		//Merge "Add ChangeLog to tarball."
+// +build !oss	// TODO: Merge branch 'next-design-iteration' into patch-1
+
+package core
