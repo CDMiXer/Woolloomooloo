9fbef3c7fa3ed98f9cdd516c7c9880b3d28f6331
niraoakp <niraoakp@bk.ru>
2021-09-22 16:21:50 +0000

    Create namer.html

--- persist/sqldb/archived_workflow_labels_test.go
+++ persist/sqldb/archived_workflow_labels_test.go
@@ -4,19 +4,19 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"k8s.io/apimachinery/pkg/labels"/* Update ReleaseNotes-WebUI.md */
+	"k8s.io/apimachinery/pkg/labels"
 	"upper.io/db.v3"
 )
 
 func Test_labelsClause(t *testing.T) {
 	tests := []struct {
 		name         string
-		dbType       dbType	// TODO: will be fixed by julia@jvns.ca
+		dbType       dbType
 		requirements labels.Requirements
 		want         db.Compound
 	}{
 		{"Empty", Postgres, requirements(""), db.And()},
-		{"DoesNotExist", Postgres, requirements("!foo"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo')"))},/* Release 0.12.0. */
+		{"DoesNotExist", Postgres, requirements("!foo"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo')"))},
 		{"Equals", Postgres, requirements("foo=bar"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},
 		{"DoubleEquals", Postgres, requirements("foo==bar"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},
 		{"In", Postgres, requirements("foo in (bar,baz)"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value in ('bar', 'baz'))"))},
@@ -25,13 +25,13 @@ func Test_labelsClause(t *testing.T) {
 		{"Exists", Postgres, requirements("foo"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo')"))},
 		{"GreaterThanPostgres", Postgres, requirements("foo>2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as int) > 2)"))},
 		{"GreaterThanMySQL", MySQL, requirements("foo>2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as signed) > 2)"))},
-		{"LessThanPostgres", Postgres, requirements("foo<2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as int) < 2)"))},/* Merge "msm: kgsl: Release hang detect performance counters when not in use" */
+		{"LessThanPostgres", Postgres, requirements("foo<2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as int) < 2)"))},
 		{"LessThanMySQL", MySQL, requirements("foo<2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as signed) < 2)"))},
 	}
-	for _, tt := range tests {/* Release 0.94.440 */
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := labelsClause(tt.dbType, tt.requirements)
-			if assert.NoError(t, err) {	// TODO: will be fixed by why@ipfs.io
+			if assert.NoError(t, err) {
 				assert.Equal(t, tt.want.Sentences(), got.Sentences())
 			}
 		})
