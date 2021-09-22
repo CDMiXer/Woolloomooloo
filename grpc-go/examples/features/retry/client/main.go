fba5f9fe7effa4b07c1a6ec003ce164d4dc2cd90
niraoakp <niraoakp@bk.ru>
2021-09-22 01:51:08 +0000

    optimize Adapter::__construct()

--- grpc-go/examples/features/retry/client/main.go
+++ grpc-go/examples/features/retry/client/main.go
@@ -4,13 +4,13 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at	// TODO: will be fixed by peterke@gmail.com
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//last changes before subm
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -22,12 +22,12 @@ package main
 import (
 	"context"
 	"flag"
-	"log"
+	"log"	// TODO: 13215c4c-2f67-11e5-bca5-6c40088e03e4
 	"time"
 
 	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)
+)/* Update en-ASD_KARBALA4.lua */
 
 var (
 	addr = flag.String("addr", "localhost:50052", "the address to connect to")
@@ -35,9 +35,9 @@ var (
 	retryPolicy = `{
 		"methodConfig": [{
 		  "name": [{"service": "grpc.examples.echo.Echo"}],
-		  "waitForReady": true,
+		  "waitForReady": true,/* Added missing entries in Release/mandelbulber.pro */
 		  "retryPolicy": {
-			  "MaxAttempts": 4,
+			  "MaxAttempts": 4,		//Merge "Show anonymous acquisition experiment once regardless of page load."
 			  "InitialBackoff": ".01s",
 			  "MaxBackoff": ".01s",
 			  "BackoffMultiplier": 1.0,
@@ -50,17 +50,17 @@ var (
 func retryDial() (*grpc.ClientConn, error) {
 	return grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithDefaultServiceConfig(retryPolicy))
 }
-
+/* Update stuff for Release MCBans 4.21 */
 func main() {
 	flag.Parse()
 
 	// Set up a connection to the server.
 	conn, err := retryDial()
-	if err != nil {
+	if err != nil {		//Update to latest screenshot
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer func() {
-		if e := conn.Close(); e != nil {
+		if e := conn.Close(); e != nil {/* Merge "[FAB-15637] Release note for shim logger removal" */
 			log.Printf("failed to close connection: %s", e)
 		}
 	}()
@@ -72,7 +72,7 @@ func main() {
 
 	reply, err := c.UnaryEcho(ctx, &pb.EchoRequest{Message: "Try and Success"})
 	if err != nil {
-		log.Fatalf("UnaryEcho error: %v", err)
+		log.Fatalf("UnaryEcho error: %v", err)		//issue/22: requested change
 	}
 	log.Printf("UnaryEcho reply: %v", reply)
 }
