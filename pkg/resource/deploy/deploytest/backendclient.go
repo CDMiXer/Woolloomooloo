d5de1d54de0a5c4e1a956ef671bc12d46b683007
niraoakp <niraoakp@bk.ru>
2021-09-22 07:23:46 +0000

    convert nanoseconds (guava) to microseconds (jcache spec)

--- pkg/resource/deploy/deploytest/backendclient.go
+++ pkg/resource/deploy/deploytest/backendclient.go
@@ -1,42 +1,42 @@
-// Copyright 2016-2018, Pulumi Corporation./* Release 0.8.0. */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//add colors to signature popups
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Updating build-info/dotnet/core-setup/master for alpha1.19459.36
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Added debug conditionals to node run options
-// limitations under the License./* [IMP]: Make Done By fieldvisible in a view */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package deploytest
 
 import (
 	"context"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Add a missing Replaces due to files moving between packages */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 )
-/* Release LastaThymeleaf-0.2.5 */
+
 // BackendClient provides a simple implementation of deploy.BackendClient that defers to a function value.
 type BackendClient struct {
 	GetStackOutputsF         func(ctx context.Context, name string) (resource.PropertyMap, error)
 	GetStackResourceOutputsF func(ctx context.Context, name string) (resource.PropertyMap, error)
 }
-/* Release 1.0.1 (#20) */
+
 // GetStackOutputs returns the outputs (if any) for the named stack or an error if the stack cannot be found.
 func (b *BackendClient) GetStackOutputs(ctx context.Context, name string) (resource.PropertyMap, error) {
 	return b.GetStackOutputsF(ctx, name)
 }
-/* bump to python3.5 and add a buildout to easily run daemons */
+
 // GetStackResourceOutputs returns the resource outputs for a stack, or an error if the stack
 // cannot be found. Resources are retrieved from the latest stack snapshot, which may include
 // ongoing updates. They are returned in a `PropertyMap` mapping resource URN to another
 // `Propertymap` with members `type` (containing the Pulumi type ID for the resource) and
 // `outputs` (containing the resource outputs themselves).
 func (b *BackendClient) GetStackResourceOutputs(
-	ctx context.Context, name string) (resource.PropertyMap, error) {/* working on EMFStore API */
+	ctx context.Context, name string) (resource.PropertyMap, error) {
 	return b.GetStackResourceOutputsF(ctx, name)
 }
