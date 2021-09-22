fa4d01f5d18410f2d568eafef0199198b54eb4d5
niraoakp <niraoakp@bk.ru>
2021-09-22 12:50:20 +0000

    Evelated the test suite jQuery version to 1.6.4.

--- tests/integration/partial_values/nodejs/index.ts
+++ tests/integration/partial_values/nodejs/index.ts
@@ -1,30 +1,30 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Merge "Apache - Use net_cidr_map for proxy_ips" */
-import * as assert from "assert";	// TODO: hacked by zaq1tomo@gmail.com
-import * as pulumi from "@pulumi/pulumi";/* Merge "Release of org.cloudfoundry:cloudfoundry-client-lib:0.8.3" */
-import { Resource } from "./resource";	// TODO: Create high_priest.sol
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//incorporate JJ's changes
 
-const unknown = <any>pulumi.output(pulumi.runtime.isDryRun() ? { __pulumiUnknown: true } : "foo");	// TODO: Create gargantua-wrapper.sh
+import * as assert from "assert";
+import * as pulumi from "@pulumi/pulumi";
+import { Resource } from "./resource";
 
+const unknown = <any>pulumi.output(pulumi.runtime.isDryRun() ? { __pulumiUnknown: true } : "foo");
+	// backend error should return status 500 
 let a = new Resource("res", {
     foo: "foo",
-    bar: { value: "foo", unknown },
-    baz: [ "foo", unknown ],/* Everything takes a ReleasesQuery! */
+    bar: { value: "foo", unknown },		//Some launcher options
+    baz: [ "foo", unknown ],
 });
 
 export let o = Promise.all([
     (<any>a.foo).isKnown,
     (<any>a.bar.value).isKnown,
     (<any>a.bar.unknown).isKnown,
-    (<any>a.baz[0]).isKnown,/* Release the connection after use. */
+    (<any>a.baz[0]).isKnown,
     (<any>a.baz[1]).isKnown,
 ]).then(([r1, r2, r3, r4, r5]) => {
     assert.equal(r1, true);
     assert.equal(r2, true);
-    assert.equal(r3, !pulumi.runtime.isDryRun());		//Updated upgrade routine
+    assert.equal(r3, !pulumi.runtime.isDryRun());
     assert.equal(r4, true);
     assert.equal(r5, !pulumi.runtime.isDryRun());
-/* Deleted msmeter2.0.1/Release/CL.read.1.tlog */
+
     console.log("ok");
-    return "checked";	// TODO: hacked by hello@brooklynzelenka.com
+    return "checked";	// TODO: will be fixed by hello@brooklynzelenka.com
 });
