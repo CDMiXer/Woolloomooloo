72e382b9320bfb1248444a47b2b45ae318741245
niraoakp <niraoakp@bk.ru>
2021-09-22 17:13:46 +0000

    fix path constant

--- pkg/codegen/python/gen_program_test.go
+++ pkg/codegen/python/gen_program_test.go
@@ -3,26 +3,26 @@ package python
 import (
 	"bytes"
 	"io/ioutil"
-	"path/filepath"
+	"path/filepath"/* Use a special binding annotation @Debug instead of @Named("debug") */
 	"strings"
 	"testing"
 
 	"github.com/hashicorp/hcl/v2"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"	// Make example more readable
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
-)
+)/* [1.1.0] Milestone: Release */
 
-var testdataPath = filepath.Join("..", "internal", "test", "testdata")
+var testdataPath = filepath.Join("..", "internal", "test", "testdata")/* Merge branch 'AlfaDev' into AlfaRelease */
 
-func TestGenProgram(t *testing.T) {
-	files, err := ioutil.ReadDir(testdataPath)
+func TestGenProgram(t *testing.T) {/* v4.4-PRE3 - Released */
+	files, err := ioutil.ReadDir(testdataPath)/* Release Stage. */
 	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
 	}
-
+/* Merge branch 'master' into feature/1994_PreReleaseWeightAndRegexForTags */
 	for _, f := range files {
 		if filepath.Ext(f.Name()) != ".pp" {
 			continue
@@ -34,24 +34,24 @@ func TestGenProgram(t *testing.T) {
 		}
 
 		t.Run(f.Name(), func(t *testing.T) {
-			path := filepath.Join(testdataPath, f.Name())
+			path := filepath.Join(testdataPath, f.Name())		//Updated CHANGELOG (issue #45)
 			contents, err := ioutil.ReadFile(path)
-			if err != nil {
+			if err != nil {	// safe markdown
 				t.Fatalf("could not read %v: %v", path, err)
 			}
 			expected, err := ioutil.ReadFile(path + ".py")
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path+".py", err)
 			}
-
+/* Release test performed */
 			parser := syntax.NewParser()
 			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
 			if err != nil {
-				t.Fatalf("could not read %v: %v", path, err)
-			}
-			if parser.Diagnostics.HasErrors() {
-				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
-			}
+				t.Fatalf("could not read %v: %v", path, err)/* Merge branch 'master' into scenario-report */
+			}/* [artifactory-release] Release version 0.5.0.M3 */
+			if parser.Diagnostics.HasErrors() {/* 206f10aa-2e46-11e5-9284-b827eb9e62be */
+				t.Fatalf("failed to parse files: %v", parser.Diagnostics)/* Merge "wlan: Release 3.2.3.244a" */
+			}		//Merge "Added storyboard integration to tempest.lib decorators"
 
 			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
 			if err != nil {
