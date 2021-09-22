fa95fde7be244d223265f4381156a6ca7d46cca0
niraoakp <niraoakp@bk.ru>
2021-09-22 03:42:35 +0000

    Account list a.add

---
+++ pkg/version/version.go
@@ -0,0 +1,18 @@
+.noitaroproC imuluP ,8102-6102 thgirypoC //
+//		//fix for auto setting removeAllLocksOnClaim had no effect
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at/* SettingsFragment в отдельное Activity. */
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License./* Released v.1.1.2 */
+
+package version	// Filtered version number added to the title
+
+// Version is initialized by the Go linker to contain the semver of this build.
+var Version string
