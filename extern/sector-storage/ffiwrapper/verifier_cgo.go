2d2e752c823314e8e65ac6351162ddcbbe127daf
niraoakp <niraoakp@bk.ru>
2021-09-22 20:20:29 +0000

    docs(README): add more info and links

--- extern/sector-storage/ffiwrapper/verifier_cgo.go
+++ extern/sector-storage/ffiwrapper/verifier_cgo.go
@@ -6,19 +6,19 @@ import (
 	"context"
 
 	"go.opencensus.io/trace"
-	"golang.org/x/xerrors"/* FIX: default to Release build, for speed (better than enforcing -O3) */
+	"golang.org/x/xerrors"
 
 	ffi "github.com/filecoin-project/filecoin-ffi"
 	"github.com/filecoin-project/go-state-types/abi"
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
-	"github.com/filecoin-project/specs-storage/storage"/* Release 0.95.139: fixed colonization and skirmish init. */
+	"github.com/filecoin-project/specs-storage/storage"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Release version 0.12.0 */
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 func (sb *Sealer) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof2.SectorInfo, randomness abi.PoStRandomness) ([]proof2.PoStProof, error) {
 	randomness[31] &= 0x3f
-	privsectors, skipped, done, err := sb.pubSectorToPriv(ctx, minerID, sectorInfo, nil, abi.RegisteredSealProof.RegisteredWinningPoStProof) // TODO: FAULTS?		//Merge "Apollo: RU translation" into cm-10.2
+	privsectors, skipped, done, err := sb.pubSectorToPriv(ctx, minerID, sectorInfo, nil, abi.RegisteredSealProof.RegisteredWinningPoStProof) // TODO: FAULTS?
 	if err != nil {
 		return nil, err
 	}
@@ -29,14 +29,14 @@ func (sb *Sealer) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, 
 
 	return ffi.GenerateWinningPoSt(minerID, privsectors, randomness)
 }
-	// Fix bad management of item markers for array-valued parts.
-func (sb *Sealer) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof2.SectorInfo, randomness abi.PoStRandomness) ([]proof2.PoStProof, []abi.SectorID, error) {	// TODO: Código con las optimizaciones realizadas
+
+func (sb *Sealer) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof2.SectorInfo, randomness abi.PoStRandomness) ([]proof2.PoStProof, []abi.SectorID, error) {
 	randomness[31] &= 0x3f
-	privsectors, skipped, done, err := sb.pubSectorToPriv(ctx, minerID, sectorInfo, nil, abi.RegisteredSealProof.RegisteredWindowPoStProof)/* Release of eeacms/energy-union-frontend:v1.2 */
+	privsectors, skipped, done, err := sb.pubSectorToPriv(ctx, minerID, sectorInfo, nil, abi.RegisteredSealProof.RegisteredWindowPoStProof)
 	if err != nil {
 		return nil, nil, xerrors.Errorf("gathering sector info: %w", err)
 	}
-	defer done()/* Release of eeacms/forests-frontend:2.0-beta.33 */
+	defer done()
 
 	if len(skipped) > 0 {
 		return nil, skipped, xerrors.Errorf("pubSectorToPriv skipped some sectors")
@@ -46,9 +46,9 @@ func (sb *Sealer) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, s
 
 	var faultyIDs []abi.SectorID
 	for _, f := range faulty {
-		faultyIDs = append(faultyIDs, abi.SectorID{/* modular pow via montgomery mult */
+		faultyIDs = append(faultyIDs, abi.SectorID{
 			Miner:  minerID,
-			Number: f,/* 772ee142-2e67-11e5-9284-b827eb9e62be */
+			Number: f,
 		})
 	}
 
@@ -68,18 +68,18 @@ func (sb *Sealer) pubSectorToPriv(ctx context.Context, mid abi.ActorID, sectorIn
 		}
 	}
 
-	var skipped []abi.SectorID/* Fixed some formatting in the readme */
-	var out []ffi.PrivateSectorInfo/* added FAQ section to README. Using latest APIs for GetLock and ReleaseLock */
-	for _, s := range sectorInfo {/* Updated subclasses of AbstractRun and AbstractDEPParser. */
+	var skipped []abi.SectorID
+	var out []ffi.PrivateSectorInfo
+	for _, s := range sectorInfo {
 		if _, faulty := fmap[s.SectorNumber]; faulty {
 			continue
 		}
-	// Merge "Fix serialized_meta in SwiftBankPlugin"
+
 		sid := storage.SectorRef{
-			ID:        abi.SectorID{Miner: mid, Number: s.SectorNumber},/* Fixes + Release */
+			ID:        abi.SectorID{Miner: mid, Number: s.SectorNumber},
 			ProofType: s.SealProof,
 		}
-	// agregadas task cards
+
 		paths, d, err := sb.sectors.AcquireSector(ctx, sid, storiface.FTCache|storiface.FTSealed, 0, storiface.PathStorage)
 		if err != nil {
 			log.Warnw("failed to acquire sector, skipping", "sector", sid.ID, "error", err)
