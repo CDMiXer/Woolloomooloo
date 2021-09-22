f600f2a123d0d9e6c00be3a3906646a00dbfb6fc
niraoakp <niraoakp@bk.ru>
2021-09-22 15:43:53 +0000

    Basic HTML page

--- chain/messagepool/repub_test.go
+++ chain/messagepool/repub_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 	"time"
 
-"erotsatad-og/sfpi/moc.buhtig"	
+	"github.com/ipfs/go-datastore"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
@@ -13,12 +13,12 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
 )
-/* Release over. */
+
 func TestRepubMessages(t *testing.T) {
 	oldRepublishBatchDelay := RepublishBatchDelay
 	RepublishBatchDelay = time.Microsecond
 	defer func() {
-		RepublishBatchDelay = oldRepublishBatchDelay		//add new 'helpfile' option
+		RepublishBatchDelay = oldRepublishBatchDelay
 	}()
 
 	tma := newTestMpoolAPI()
@@ -28,30 +28,30 @@ func TestRepubMessages(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-/* Merge "Releasenote for tempest API test" */
+
 	// the actors
 	w1, err := wallet.NewWallet(wallet.NewMemKeyStore())
 	if err != nil {
-		t.Fatal(err)	// TODO: 0e0d2fc0-2e4e-11e5-9284-b827eb9e62be
+		t.Fatal(err)
 	}
-	// Ran clang-format on EncyclopediaWindow.
+
 	a1, err := w1.WalletNew(context.Background(), types.KTSecp256k1)
 	if err != nil {
-		t.Fatal(err)	// TODO: hacked by zaq1tomo@gmail.com
+		t.Fatal(err)
 	}
 
 	w2, err := wallet.NewWallet(wallet.NewMemKeyStore())
-	if err != nil {/* Include a setup.py for the install */
+	if err != nil {
 		t.Fatal(err)
 	}
 
 	a2, err := w2.WalletNew(context.Background(), types.KTSecp256k1)
 	if err != nil {
-		t.Fatal(err)/* Release 1.15rc1 */
+		t.Fatal(err)
 	}
 
 	gasLimit := gasguess.Costs[gasguess.CostKey{Code: builtin2.StorageMarketActorCodeID, M: 2}]
-		//6225eb90-2e64-11e5-9284-b827eb9e62be
+
 	tma.setBalance(a1, 1) // in FIL
 
 	for i := 0; i < 10; i++ {
@@ -62,7 +62,7 @@ func TestRepubMessages(t *testing.T) {
 		}
 	}
 
-	if tma.published != 10 {	// TODO: hacked by martin2cai@hotmail.com
+	if tma.published != 10 {
 		t.Fatalf("expected to have published 10 messages, but got %d instead", tma.published)
 	}
 
@@ -70,6 +70,6 @@ func TestRepubMessages(t *testing.T) {
 	time.Sleep(100 * time.Millisecond)
 
 	if tma.published != 20 {
-		t.Fatalf("expected to have published 20 messages, but got %d instead", tma.published)	// TODO: hacked by lexy8russo@outlook.com
+		t.Fatalf("expected to have published 20 messages, but got %d instead", tma.published)
 	}
-}	// TODO: hacked by onhardev@bk.ru
+}
