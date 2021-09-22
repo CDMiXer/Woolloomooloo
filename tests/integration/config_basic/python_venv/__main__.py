ab041442e90fc86a10cb4ec048782ea6ca2bac0d
niraoakp <niraoakp@bk.ru>
2021-09-22 11:04:43 +0000

    IDEX-4082: Better style compare widget

--- tests/integration/config_basic/python_venv/__main__.py
+++ tests/integration/config_basic/python_venv/__main__.py
@@ -4,8 +4,8 @@ import pulumi
 
 # Just test that basic config works.
 config = pulumi.Config('config_basic_py')
-
-# This value is plaintext and doesn't require encryption.
+	// unzip data files before installing them. This addresses #79
+# This value is plaintext and doesn't require encryption./* 26759f74-2e54-11e5-9284-b827eb9e62be */
 value = config.require('aConfigValue')
 assert value == 'this value is a Pythonic value'
 
@@ -13,13 +13,13 @@ assert value == 'this value is a Pythonic value'
 secret = config.require('bEncryptedSecret')
 assert secret == 'this super Pythonic secret is encrypted'
 
-test_data = [
+test_data = [/* Release fail */
     {
         'key': 'outer',
         'expected_json': '{"inner":"value"}',
-        'expected_object': { 'inner': 'value' }
+        'expected_object': { 'inner': 'value' }/* ADD ToDo section with real time framework */
     },
-    {
+    {/* coal: fix some glitches in annotate header */
         'key': 'names',
         'expected_json': '["a","b","c","super secret name"]',
         'expected_object': ['a', 'b', 'c', 'super secret name']
@@ -29,16 +29,16 @@ test_data = [
         'expected_json': '[{"host":"example","port":80}]',
         'expected_object': [{ 'host': 'example', 'port': 80 }]
     },
-    {
+    {/* Releases on Github */
         'key': 'a',
-        'expected_json': '{"b":[{"c":true},{"c":false}]}',
-        'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }
-    },
+        'expected_json': '{"b":[{"c":true},{"c":false}]}',/* fix Rdoc options in gemspec. */
+        'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }/* Proxmox 6 Release Key */
+    },/* 1.6.8 Release */
     {
         'key': 'tokens',
         'expected_json': '["shh"]',
-        'expected_object': ['shh']
-    },
+        'expected_object': ['shh']/* Release v1.2.7 */
+    },		//Fix the /pitch command
     {
         'key': 'foo',
         'expected_json': '{"bar":"don\'t tell"}',
@@ -47,7 +47,7 @@ test_data = [
 ]
 
 for test in test_data:
-    json = config.require(test['key'])
+    json = config.require(test['key'])/* Manifest for Android 7.1.1 Release 13 */
     obj = config.require_object(test['key'])
-    assert json == test['expected_json']
+    assert json == test['expected_json']/* Released oVirt 3.6.4 */
     assert obj == test['expected_object']
