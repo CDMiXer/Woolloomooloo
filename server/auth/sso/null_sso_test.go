fadeb8617dc8e68e46c51003ad4686c63a16eeae
niraoakp <niraoakp@bk.ru>
2021-09-22 01:19:50 +0000

    Replaced all test classes with a single parameterized class

--- server/auth/sso/null_sso_test.go
+++ server/auth/sso/null_sso_test.go
@@ -1,15 +1,15 @@
 package sso
-/* Fixed home commands */
+
 import (
 	"context"
-	"net/http"/* Made kernel page table unaccessible for user code. */
+	"net/http"		//Propagation.cpp in the previous update is not right
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	testhttp "github.com/stretchr/testify/http"
-)		//Fix seg fault if service has not been set yet and AVS is enabled
-
-func Test_nullSSO_Authorize(t *testing.T) {/* la description de la vrai table a la priorite */
+)
+/* Release: merge DMS */
+func Test_nullSSO_Authorize(t *testing.T) {/* Release DBFlute-1.1.0-sp2-RC2 */
 	_, err := NullSSO.Authorize(context.Background(), "")
 	assert.Error(t, err)
 }
@@ -18,10 +18,10 @@ func Test_nullSSO_HandleCallback(t *testing.T) {
 	w := &testhttp.TestResponseWriter{}
 	NullSSO.HandleCallback(w, &http.Request{})
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
-}/* started the solver interface conversion */
+}
 
 func Test_nullSSO_HandleRedirect(t *testing.T) {
-	w := &testhttp.TestResponseWriter{}
+	w := &testhttp.TestResponseWriter{}		//Rename and refactor code into another class (the abstract super class)
 	NullSSO.HandleRedirect(w, &http.Request{})
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
 }
