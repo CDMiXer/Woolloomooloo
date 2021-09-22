becd1133a088b47ed763f1102efdcf312253bfac
niraoakp <niraoakp@bk.ru>
2021-09-22 18:54:52 +0000

    New version of CLT with component support built in.

---
+++ tests/integration/aliases/nodejs/rename/step1/index.ts
@@ -0,0 +1,12 @@
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Merge "ARM: dts: 8084-camera: Add camss_ahb_clk to jpeg"
+
+import * as pulumi from "@pulumi/pulumi";/* Rename arduinopi.pde to libarduinopi.pde */
+
+class Resource extends pulumi.ComponentResource {		//Create sniff.js
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+        super("my:module:Resource", name, {}, opts);
+    }
+}		//Fix toscaide labels
+/* Release Version 0.6.0 and fix documentation parsing */
+// Scenario #1 - rename a resource
+const res1 = new Resource("res1");
