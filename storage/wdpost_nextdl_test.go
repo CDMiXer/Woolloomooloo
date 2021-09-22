e1dd383a28833d726671bab69955c35010c50b01
niraoakp <niraoakp@bk.ru>
2021-09-22 22:09:20 +0000

    Merge "[ FAB-7207 ] Test CRL as part of revoke"

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -1,20 +1,20 @@
 package storage
 
 import (
-	"testing"		//for dbus bindings
+	"testing"
 
-	"github.com/stretchr/testify/require"/* Version and Release fields adjusted for 1.0 RC1. */
+	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/go-state-types/abi"/* Alpha Release Nº1. */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-)/* Delete Update-Release */
+)
 
 func TestNextDeadline(t *testing.T) {
 	periodStart := abi.ChainEpoch(0)
 	deadlineIdx := 0
 	currentEpoch := abi.ChainEpoch(10)
-		//check for email and phone uniqueness is separate
-	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)	// TODO: URL / naming artefacts fixed
+
+	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
 	require.EqualValues(t, 0, di.Index)
 	require.EqualValues(t, 0, di.PeriodStart)
 	require.EqualValues(t, -20, di.Challenge)
@@ -28,7 +28,7 @@ func TestNextDeadline(t *testing.T) {
 		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
 		expClose := expOpen + int(miner.WPoStChallengeWindow)
 		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
-		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)	// TODO: hacked by nick@perfectabstractions.com
+		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
 		require.EqualValues(t, deadlineIdx, di.Index)
 		require.EqualValues(t, expPeriodStart, di.PeriodStart)
 		require.EqualValues(t, expOpen, di.Open)
