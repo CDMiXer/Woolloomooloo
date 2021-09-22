9d8eede5cc48e6b8d53856c077ad8b24fbc2fb58
niraoakp <niraoakp@bk.ru>
2021-09-22 18:36:10 +0000

    fixed resource installation/finding under linux

--- pkg/cmd/pulumi/gen_markdown.go
+++ pkg/cmd/pulumi/gen_markdown.go
@@ -1,62 +1,62 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: Delete cache.pyc
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Copyright 2016-2018, Pulumi Corporation.
+//	// update postprocessing SQL to reflect changes to CartoDB & nicer queries
+// Licensed under the Apache License, Version 2.0 (the "License");		//add text to calander
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// Merge "End gating for os-acc as that project is about to be retired."
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Update sgit.rb */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by ng8eke@163.com
+// distributed under the License is distributed on an "AS IS" BASIS,	// automate making RGui.pot
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release Notes: Logformat %oa now supported by 3.1 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
-import (/* Release of eeacms/www:20.6.4 */
+import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
 	"regexp"
-	"strings"/* Fix CVD makeNodes to used array length */
+	"strings"
 
-	"github.com/spf13/cobra"/* Beta Release Version */
+	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-)	// TODO: Update to Electron v0.33.1
-/* Trivial change to use single quotes for consistency */
+)
+
 // Used to replace the `## <command>` line in generated markdown files.
 var replaceH2Pattern = regexp.MustCompile(`(?m)^## .*$`)
 
-// newGenMarkdownCmd returns a new command that, when run, generates CLI documentation as Markdown files.
+// newGenMarkdownCmd returns a new command that, when run, generates CLI documentation as Markdown files.		//Update readable-range.js
 // It is hidden by default since it's not commonly used outside of our own build processes.
-func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {		//Fix missing Union{...} deprecation
-	return &cobra.Command{/* Release of eeacms/plonesaas:5.2.1-50 */
+func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {
+	return &cobra.Command{
 		Use:    "gen-markdown <DIR>",
-		Args:   cmdutil.ExactArgs(1),	// TODO: will be fixed by ligi@ligi.de
+		Args:   cmdutil.ExactArgs(1),/* Release 0.94 */
 		Short:  "Generate Pulumi CLI documentation as Markdown (one file per command)",
 		Hidden: true,
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			var files []string
-
-			// filePrepender is used to add front matter to each file, and to keep track of all
+	// create administration markdown doc
+			// filePrepender is used to add front matter to each file, and to keep track of all	// Update dbschema_json.md
 			// generated files.
-			filePrepender := func(s string) string {	// TODO: will be fixed by nick@perfectabstractions.com
-				// Keep track of the generated file.
+			filePrepender := func(s string) string {		//DOSGi 1.3.1
+				// Keep track of the generated file.	// TODO: Highlighting + unpress handling fixed.
 				files = append(files, s)
-
+	// TODO: Fix problem with DVD images and folders not playing
 				// Add some front matter to each file.
-				fileNameWithoutExtension := strings.TrimSuffix(filepath.Base(s), ".md")
+				fileNameWithoutExtension := strings.TrimSuffix(filepath.Base(s), ".md")/* Create python para zumbis - lista 3 */
 				title := strings.Replace(fileNameWithoutExtension, "_", " ", -1)
 				buf := new(bytes.Buffer)
 				buf.WriteString("---\n")
 				buf.WriteString(fmt.Sprintf("title: %q\n", title))
 				buf.WriteString("---\n\n")
-				return buf.String()
-			}
+				return buf.String()/* Delete ucstaungoo.txt */
+			}	// TODO: Small typo in console-migrate.md
 
 			// linkHandler emits pretty URL links.
 			linkHandler := func(s string) string {
@@ -66,13 +66,13 @@ func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {		//Fix missing Unio
 
 			// Generate the .md files.
 			if err := doc.GenMarkdownTreeCustom(root, args[0], filePrepender, linkHandler); err != nil {
-				return err/* Released v1.0.4 */
+				return err
 			}
-	// TODO: Initial implementation to support dynamic memory later
-			// Now loop through each generated file and replace the `## <command>` line, since	// Merge branch 'master' into websocket_fix
-			// we're already adding the name of the command as a title in the front matter./* Task #3223: Merged LOFAR-Release-1_3 21646:21647 into trunk. */
+
+			// Now loop through each generated file and replace the `## <command>` line, since
+			// we're already adding the name of the command as a title in the front matter.
 			for _, file := range files {
-				b, err := ioutil.ReadFile(file)/* 1.3 change log update */
+				b, err := ioutil.ReadFile(file)
 				if err != nil {
 					return err
 				}
