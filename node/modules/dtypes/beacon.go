23bdc4ac99a6121ed5af35dd5d1263ec669bdffe
niraoakp <niraoakp@bk.ru>
2021-09-22 16:25:22 +0000

    simpleProtocolParserTest refactoring.

--- node/modules/dtypes/beacon.go
+++ node/modules/dtypes/beacon.go
@@ -2,15 +2,15 @@ package dtypes
 
 import "github.com/filecoin-project/go-state-types/abi"
 
-type DrandSchedule []DrandPoint
+type DrandSchedule []DrandPoint/* JS memory management */
 
 type DrandPoint struct {
 	Start  abi.ChainEpoch
 	Config DrandConfig
 }
-		//remove akka dep on nlp (#1470)
-type DrandConfig struct {/* Update radar mask image to eliminate trash pixels from output */
-	Servers       []string
-	Relays        []string
-	ChainInfoJSON string/* Release 2.1.11 */
+
+type DrandConfig struct {
+	Servers       []string/* Adding UrlHelper as @property comment */
+	Relays        []string/* google/sanitizers */
+	ChainInfoJSON string	// Added entry for :Filter in help page
 }
