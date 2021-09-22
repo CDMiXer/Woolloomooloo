7ea7b339580b9e545f8d6370b40ebf8d49ad56f3
niraoakp <niraoakp@bk.ru>
2021-09-22 20:04:05 +0000

    fix for tips

--- scheduler/kube/config.go
+++ scheduler/kube/config.go
@@ -1,46 +1,46 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// added composer file for db package
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Create misc.rst
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//[lantiq] refresh patch and install v1.1 gphy blobs
 package kube
 
 // Config is the configuration for the Kubernetes scheduler.
 type Config struct {
-	Namespace        string
+	Namespace        string/* Release 1.5.11 */
 	ServiceAccount   string
-	ConfigURL        string
+	ConfigURL        string	// TODO: hacked by lexy8russo@outlook.com
 	ConfigPath       string
 	TTL              int
 	Image            string
-	ImagePullPolicy  string
+	ImagePullPolicy  string	// TODO: start a git mergetool help section
 	ImagePrivileged  []string
-	DockerHost       string
+	DockerHost       string/* [MERGE] crm kaban Added Expected Revenues fme */
 	DockerHostWin    string
 	LimitMemory      int
 	LimitCompute     int
-	RequestMemory    int
-	RequestCompute   int
+	RequestMemory    int/* updates README for new project layout */
+	RequestCompute   int	// TODO: will be fixed by caojiaoyue@protonmail.com
 	CallbackHost     string
-	CallbackProto    string
+	CallbackProto    string	// TODO: will be fixed by steven@stebalien.com
 	CallbackSecret   string
 	SecretToken      string
 	SecretEndpoint   string
 	SecretInsecure   bool
-	RegistryToken    string
+	RegistryToken    string		//Issue #35: Make code coverage 100% for io.github.mezk.dminer.utils
 	RegistryEndpoint string
-	RegistryInsecure bool
+	RegistryInsecure bool/* Release 4.0.0-beta1 */
 	LogDebug         bool
-	LogTrace         bool
+	LogTrace         bool/* Changed to compiler.target 1.7, Release 1.0.1 */
 	LogPretty        bool
 	LogText          bool
 }
