6bf869f0b7146c042b33ef39d0ddaab8b45a013b
niraoakp <niraoakp@bk.ru>
2021-09-22 16:12:55 +0000

    Nginx: remove idle connection settings

--- plugin/converter/noop.go
+++ plugin/converter/noop.go
@@ -2,28 +2,28 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* fix the big bug about conflicts in plugins... */
+//	// TODO: New JAR, C# transformations switched off
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+//		//0a80cf6c-2e42-11e5-9284-b827eb9e62be
+// Unless required by applicable law or agreed to in writing, software/* rev 554406 */
+// distributed under the License is distributed on an "AS IS" BASIS,/* Display the svn revision in the about dialog again. */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Merge remote-tracking branch 'origin/joepadde' into Asbjorn3
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-	// TODO: Verify open as precondition
+
 package converter
-/* Merge branch 'feature/lucene' into feature/tooling */
-import (
+
+import (/* Error Panel jsp added */
 	"context"
 
 	"github.com/drone/drone/core"
 )
-
+		//- Added competences description
 type noop struct{}
-/* Delete Proxy.scala */
-func (noop) Convert(context.Context, *core.ConvertArgs) (*core.Config, error) {		//Reorganizes packages: excludes 'platform' from package tree
-	return nil, nil/* TvTunes: Release of screensaver */
-}
+
+func (noop) Convert(context.Context, *core.ConvertArgs) (*core.Config, error) {
+	return nil, nil
+}/* updating README to reflect name change. */
