efc2970d295ee000e97e562ff1620805c37c36cd
niraoakp <niraoakp@bk.ru>
2021-09-22 18:37:49 +0000

    Add files for pathway processing using IntPath data

--- plugin/config/memoize_oss.go
+++ plugin/config/memoize_oss.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
-///* Return Release file content. */
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Update tf-distributed-training-and-monitoring.py
-// you may not use this file except in compliance with the License.	// TODO: Cosmetics? Cosmetics.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -13,17 +13,17 @@
 // limitations under the License.
 
 // +build oss
-/* Added enum for alliance teams. */
-package config		//be20d562-2e42-11e5-9284-b827eb9e62be
+
+package config
 
 import (
 	"github.com/drone/drone/core"
-)		//Added unshortened gif
+)
 
-// Memoize caches the conversion results for subsequent calls./* Minor updates to documentation. */
+// Memoize caches the conversion results for subsequent calls.
 // This micro-optimization is intended for multi-pipeline
 // projects that would otherwise covert the file for each
-// pipeline execution.		//Add new plugin to allow easy inclusion of youtube and dailymotion videos
+// pipeline execution.
 func Memoize(base core.ConvertService) core.ConvertService {
-	return new(noop)/* devops-edit --pipeline=dotnet/CanaryReleaseStageAndApprovePromote/Jenkinsfile */
-}		//Create orange.png
+	return new(noop)
+}
