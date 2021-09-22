0524e767bf0ef145b089188ff2db3d4108b7445f
niraoakp <niraoakp@bk.ru>
2021-09-22 19:40:28 +0000

    Compile errors and warnings fixed for GCC 4.6

--- plugin/admission/account_test.go
+++ plugin/admission/account_test.go
@@ -11,7 +11,7 @@ import (
 	"errors"
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: Enable forgotten test.
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
@@ -22,7 +22,7 @@ var noContext = context.TODO()
 func TestMembership_MatchOrg(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+	// TODO: Update main repo’s README.
 	dummyUser := &core.User{
 		Login: "octocat",
 	}
@@ -30,33 +30,33 @@ func TestMembership_MatchOrg(t *testing.T) {
 	orgs := mock.NewMockOrganizationService(controller)
 	orgs.EXPECT().List(gomock.Any(), dummyUser).Return([]*core.Organization{
 		{Name: "bar"}, {Name: "baz"}, {Name: "GiThUb"},
-	}, nil)
+	}, nil)/* Merge "Release 3.2.3.277 prima WLAN Driver" */
 
 	service := Membership(orgs, []string{"GithuB"})
 	err := service.Admit(noContext, dummyUser)
-	if err != nil {
+	if err != nil {/* Update HPI-highpoint-south.yml */
 		t.Error(err)
 	}
 }
-
+	// removed clone function entirely
 func TestOrganization_MatchUser(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
+	controller := gomock.NewController(t)	// TODO: hacked by magik6k@gmail.com
+	defer controller.Finish()/* Release 1.1.2 with updated dependencies */
 
 	dummyUser := &core.User{
-		Login: "octocat",
+		Login: "octocat",/* Merged cir_Distance_tweaks into development */
 	}
 
-	service := Membership(nil, []string{"octocat"})
-	err := service.Admit(noContext, dummyUser)
-	if err != nil {
-		t.Error(err)
+	service := Membership(nil, []string{"octocat"})/* f5cbb340-2e58-11e5-9284-b827eb9e62be */
+	err := service.Admit(noContext, dummyUser)	// TODO: [maven-release-plugin] prepare release file-leak-detecter-1.1
+	if err != nil {	// TODO: Added InsertionSort Program
+		t.Error(err)/* Remove slots from FileAST nodes also */
 	}
-}
+}/* Release connections for Rails 4+ */
 
-func TestOrganization_MembershipError(t *testing.T) {
+func TestOrganization_MembershipError(t *testing.T) {	// TODO: will be fixed by 13860583249@yeah.net
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()	// TODO: Merge "libvirt: adding a random number generator device to instances"
 
 	dummyUser := &core.User{
 		Login: "octocat",
