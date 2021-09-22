e80f8b7b1f5e6889bea8c20785426d1e81b2e4a0
niraoakp <niraoakp@bk.ru>
2021-09-22 05:31:25 +0000

    stock items

--- tests/integration/custom_timeouts/python/success/__main__.py
+++ tests/integration/custom_timeouts/python/success/__main__.py
@@ -1,25 +1,25 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Created Progress Dialog for Refresh button
-/* update templates from "store..." to "$..." */
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
 from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions
-/* Started new Release 0.7.7-SNAPSHOT */
+
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
-		//removed prints.
+
 # Attempt to create a resource with a CustomTimeout
 res1 = Resource1("res1",
     opts=ResourceOptions(custom_timeouts=CustomTimeouts(create='30m'))
-)/* add Jruby support */
+)
 
-# Also use the previous workaround method, which we should not regress upon		//Reorg text
+# Also use the previous workaround method, which we should not regress upon
 res2 = Resource1("res2",
     opts=ResourceOptions(custom_timeouts={'create': '15m', 'delete': '15m'})
 )
 
-res3 = Resource1("res3",		//merge fix for avoiding cache issues in AggTableTestCase
+res3 = Resource1("res3",
     opts=ResourceOptions(custom_timeouts=CustomTimeouts(update='30m'))
-)/* added comments and fixed one if statement to be more accurate */
-	// TODO: Ignorign Pods
+)
+
 res4 = Resource1("res4",
-    opts=ResourceOptions(custom_timeouts=CustomTimeouts(delete='30m'))/* Merge "diag: Release wakeup sources properly" */
-)/* Merge "Avoid calling repopulateParamSelectWidget multiple times on setup" */
+    opts=ResourceOptions(custom_timeouts=CustomTimeouts(delete='30m'))
+)
