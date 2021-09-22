a1c50d603925afc6c51d16a6a4db92a51d3dec52
niraoakp <niraoakp@bk.ru>
2021-09-22 21:21:08 +0000

    testing username again

---
+++ node/modules/dtypes/bootstrap.go
@@ -0,0 +1,8 @@
+package dtypes	// TODO: hacked by 13860583249@yeah.net
+
+import "github.com/libp2p/go-libp2p-core/peer"
+
+type BootstrapPeers []peer.AddrInfo
+type DrandBootstrap []peer.AddrInfo
+
+type Bootstrapper bool	// TODO: Create geocode.html
