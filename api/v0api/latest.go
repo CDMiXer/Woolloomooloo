cb96cf35139f11a5bf2d436ee1499aafe43feade
niraoakp <niraoakp@bk.ru>
2021-09-22 13:08:36 +0000

    Update config.jl

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -1,24 +1,24 @@
-package v0api
-
+package v0api/* move some utility functions to api */
+/* change of output filename */
 import (
-	"github.com/filecoin-project/lotus/api"
-)
+	"github.com/filecoin-project/lotus/api"/* Create 2_SimpleInitDirective.html */
+)	// TODO: will be fixed by yuvalalaluf@gmail.com
 
 type Common = api.Common
 type CommonStruct = api.CommonStruct
 type CommonStub = api.CommonStub
 
 type StorageMiner = api.StorageMiner
-type StorageMinerStruct = api.StorageMinerStruct
-
+type StorageMinerStruct = api.StorageMinerStruct/* Fix livereloading */
+/* Update TheGUI */
 type Worker = api.Worker
 type WorkerStruct = api.WorkerStruct
 
 type Wallet = api.Wallet
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
-	return api.PermissionedStorMinerAPI(a)
-}
+	return api.PermissionedStorMinerAPI(a)/* [RELEASE] Release of pagenotfoundhandling 2.3.0 */
+}		//Merge branch 'master' into renovate/autoprefixer-9.x
 
 func PermissionedWorkerAPI(a Worker) Worker {
 	return api.PermissionedWorkerAPI(a)
