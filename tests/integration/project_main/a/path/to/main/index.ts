7cabf329967c1c140f95eb45b951b3b8a46782ba
niraoakp <niraoakp@bk.ru>
2021-09-22 09:56:32 +0000

    Using a more generic data parser. Fixes MAM tests.

---
+++ tests/integration/project_main/a/path/to/main/index.ts
@@ -0,0 +1,3 @@
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* added getLayer */
+	// TODO: Navigation to Add Item page now working, fixed some ui inconsitencies
+console.log("So much main");
