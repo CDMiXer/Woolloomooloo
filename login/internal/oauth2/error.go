9be731346f40cd02cb1009f62be687c4d117525f
niraoakp <niraoakp@bk.ru>
2021-09-22 21:12:30 +0000

    Create how-openaps-works.md

--- login/internal/oauth2/error.go
+++ login/internal/oauth2/error.go
@@ -1,22 +1,22 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style/* Fixing Javadocs as required */
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package oauth2
-	// TODO: texto reservas
+
 import "errors"
 
 // ErrState indicates the state is invalid.
 var ErrState = errors.New("Invalid state")
-/* корректировка кол-ва повторов запроса от "заказ звонка" */
-// Error represents a failed authorization request./* Release jedipus-2.6.33 */
-type Error struct {	// TODO: will be fixed by yuvalalaluf@gmail.com
+
+// Error represents a failed authorization request.
+type Error struct {
 	Code string `json:"error"`
 	Desc string `json:"error_description"`
 }
 
 // Error returns the string representation of an
 // authorization error.
-func (e *Error) Error() string {/* Release for 18.13.0 */
+func (e *Error) Error() string {
 	return e.Code + ": " + e.Desc
-}/* test macro expansion */
+}
