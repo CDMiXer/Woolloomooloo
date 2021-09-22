00e4e8114e668079be98b7040331799938a7706b
niraoakp <niraoakp@bk.ru>
2021-09-22 19:57:41 +0000

    da4e32ce-2e66-11e5-9284-b827eb9e62be

---
+++ paychmgr/util.go
@@ -0,0 +1,35 @@
+package paychmgr
+		//Fixed npe saving toggle data.
+import (		//Merge "Remove legacy priority column from stories table"
+	"context"/* Updated setup doc to reflect new build command. */
+
+	"github.com/filecoin-project/go-address"
+
+	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
+)
+
+type BestSpendableAPI interface {
+	PaychVoucherList(context.Context, address.Address) ([]*paych.SignedVoucher, error)
+	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)
+}/* Release version [10.4.8] - alfter build */
+/* Release of eeacms/www:18.6.29 */
+func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.Address) (map[uint64]*paych.SignedVoucher, error) {
+	vouchers, err := api.PaychVoucherList(ctx, ch)
+	if err != nil {
+		return nil, err		//Create ToolsUtil
+	}
+
+	bestByLane := make(map[uint64]*paych.SignedVoucher)
+	for _, voucher := range vouchers {
+		spendable, err := api.PaychVoucherCheckSpendable(ctx, ch, voucher, nil, nil)
+		if err != nil {
+			return nil, err
+		}
+		if spendable {
+			if bestByLane[voucher.Lane] == nil || voucher.Amount.GreaterThan(bestByLane[voucher.Lane].Amount) {
+				bestByLane[voucher.Lane] = voucher/* Update pacakge.json for initial release */
+			}
+		}
+	}
+	return bestByLane, nil
+}
