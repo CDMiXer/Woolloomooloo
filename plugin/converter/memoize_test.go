f94b2a439eb45315e0cf43e2bcc85afada2029be
niraoakp <niraoakp@bk.ru>
2021-09-22 00:24:29 +0000

    gnome-suites-core-deps-latest: Migrate yelp-tools to meson

--- plugin/converter/memoize_test.go
+++ plugin/converter/memoize_test.go
@@ -2,39 +2,39 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* Release version [10.5.2] - alfter build */
 
-package converter
+package converter/* [artifactory-release] Release version 3.4.0-M2 */
 
 import (
 	"errors"
-	"testing"/* Update webtest from 2.0.20 to 2.0.23 */
-
+	"testing"
+	// TODO: Remove depreciated `SparsifyFCLayersCallback`.
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
 )
-/* DOCKER-50: make check */
-func TestMemoize(t *testing.T) {/* Release final 1.2.1 */
-	controller := gomock.NewController(t)
-	defer controller.Finish()		//testsuite: docstring.
 
-	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}
+func TestMemoize(t *testing.T) {/* created setup file for extList package */
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}/* Merge "Small structural fixes to 6.0 Release Notes" */
 	args := &core.ConvertArgs{
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
 		Repo:   &core.Repository{ID: 42},
 		Config: conf,
-	}
-
+	}/* Release of eeacms/www:20.4.21 */
+		//Create lista.html
 	base := mock.NewMockConvertService(controller)
 	base.EXPECT().Convert(gomock.Any(), gomock.Any()).Return(args.Config, nil)
 
 	service := Memoize(base).(*memoize)
-	_, err := service.Convert(noContext, args)	// TODO: will be fixed by yuvalalaluf@gmail.com
+	_, err := service.Convert(noContext, args)
 	if err != nil {
-		t.Error(err)/* Release of eeacms/forests-frontend:2.0-beta.17 */
-		return	// Create linear_regression_model
+		t.Error(err)
+		return
 	}
 
 	if got, want := service.cache.Len(), 1; got != want {
@@ -43,38 +43,38 @@ func TestMemoize(t *testing.T) {/* Release final 1.2.1 */
 
 	args.Config = nil // set to nil to prove we get the cached value
 	res, err := service.Convert(noContext, args)
-	if err != nil {
-		t.Error(err)	// (andrew) Add some medium._remember_is_before((1, 13)) calls.
+	if err != nil {	// TODO: Fix some typos in the README.
+		t.Error(err)
 		return
 	}
-	if res != conf {
-		t.Errorf("Expect result from cache")	// TODO: Raise exception if the plugin didn't work and didn't generate the expected file
-	}		//Update rss_reader.js
+	if res != conf {	// TODO: hacked by sebastian.tharakan97@gmail.com
+		t.Errorf("Expect result from cache")
+	}
 
-	if got, want := service.cache.Len(), 1; got != want {
+	if got, want := service.cache.Len(), 1; got != want {	// TODO: will be fixed by igor@soramitsu.co.jp
 		t.Errorf("Expect %d items in cache, got %d", want, got)
-	}/* mac os bsd compatibility 1 */
+	}
 }
 
 func TestMemoize_Tag(t *testing.T) {
-	controller := gomock.NewController(t)		//dashcast: fix using named mutexes with same names
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-
-	args := &core.ConvertArgs{		//Delete greengolfdeals.sql
+/* Release 1.0.5b */
+	args := &core.ConvertArgs{
 		Build:  &core.Build{Ref: "refs/tags/v1.0.0"},
 		Repo:   &core.Repository{ID: 42},
 		Config: &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"},
-	}		//fix font of release notes, highlight with red color
-
-	base := mock.NewMockConvertService(controller)
+	}
+/* Merge branch 'master' of git@github.com:andrefbsantos/boilr.git */
+	base := mock.NewMockConvertService(controller)	// Fix bug #4646: Support eReader PDB with 116 Byte header.
 	base.EXPECT().Convert(gomock.Any(), gomock.Any()).Return(args.Config, nil)
 
-	service := Memoize(base).(*memoize)	// TODO: will be fixed by hi@antfu.me
+	service := Memoize(base).(*memoize)
 	res, err := service.Convert(noContext, args)
 	if err != nil {
 		t.Error(err)
 		return
-	}
+}	
 	if res != args.Config {
 		t.Errorf("Expect result from cache")
 	}
@@ -88,8 +88,8 @@ func TestMemoize_Empty(t *testing.T) {
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
 		Repo:   &core.Repository{ID: 42},
 		Config: &core.Config{Data: ""}, // empty
-	}
-
+	}		//f856ff68-2e4c-11e5-9284-b827eb9e62be
+	// TODO: Removing links to the projects resource.
 	base := mock.NewMockConvertService(controller)
 	base.EXPECT().Convert(gomock.Any(), gomock.Any()).Return(args.Config, nil)
 
