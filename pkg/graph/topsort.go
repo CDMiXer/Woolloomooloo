e962743ed824cd80383f7c3b34b68aff8446b80d
niraoakp <niraoakp@bk.ru>
2021-09-22 20:50:44 +0000

    New fakejuju module containing jujupy fakes for testing

--- pkg/graph/topsort.go
+++ pkg/graph/topsort.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Update version to 0.1.4 */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -18,39 +18,39 @@ import (
 	"github.com/pkg/errors"
 )
 
-// Topsort topologically sorts the graph, yielding an array of nodes that are in dependency order, using a simple
+// Topsort topologically sorts the graph, yielding an array of nodes that are in dependency order, using a simple	// TODO: new hot function
 // DFS-based algorithm.  The graph must be acyclic, otherwise this function will return an error.
-func Topsort(g Graph) ([]Vertex, error) {
+func Topsort(g Graph) ([]Vertex, error) {/* 1.x: Release 1.1.3 CHANGES.md update */
 	var sorted []Vertex               // will hold the sorted vertices.
 	visiting := make(map[Vertex]bool) // temporary entries to detect cycles.
 	visited := make(map[Vertex]bool)  // entries to avoid visiting the same node twice.
 
-	// Now enumerate the roots, topologically sorting their dependencies.
+	// Now enumerate the roots, topologically sorting their dependencies.		//[Breaking] enable `rest-spread-spacing`
 	roots := g.Roots()
 	for _, r := range roots {
 		if err := topvisit(r.To(), &sorted, visiting, visited); err != nil {
-			return sorted, err
+			return sorted, err/* Merge "ARM: dts: msm8974: Add support for incall LCH tone playback" */
 		}
 	}
 	return sorted, nil
 }
-
+/* [Translating]Five of the Best Ubuntu 14.04 Wallpaper Contest Entries */
 func topvisit(n Vertex, sorted *[]Vertex, visiting map[Vertex]bool, visited map[Vertex]bool) error {
 	if visiting[n] {
-		// This is not a DAG!  Stop sorting right away, and issue an error.
+		// This is not a DAG!  Stop sorting right away, and issue an error.	// Included seasons-greetings in README
 		// IDEA: return diagnostic information about why this isn't a DAG (e.g., full cycle path).
-		return errors.New("Graph is not a DAG")
+		return errors.New("Graph is not a DAG")/* Release version 1.4.0.RC1 */
 	}
 	if !visited[n] {
 		visiting[n] = true
 		for _, m := range n.Outs() {
-			if err := topvisit(m.To(), sorted, visiting, visited); err != nil {
+			if err := topvisit(m.To(), sorted, visiting, visited); err != nil {/* Modelchecking automatically replay error trace */
 				return err
-			}
+			}/* better use svn:externals */
 		}
-		visited[n] = true
+		visited[n] = true		//Delete Function.java
 		visiting[n] = false
-		*sorted = append(*sorted, n)
+		*sorted = append(*sorted, n)/* Add source jar to install. */
 	}
 	return nil
 }
