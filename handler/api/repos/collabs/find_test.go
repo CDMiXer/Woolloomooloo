5beee8907d659ca5066514a7ac4d11f7e0176f3b
niraoakp <niraoakp@bk.ru>
2021-09-22 09:45:37 +0000

    JPen Library update for Win64 bit systems

---
+++ handler/api/repos/collabs/find_test.go
@@ -0,0 +1,162 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
+// +build !oss
+
+package collabs	// 7f1e24fa-2e60-11e5-9284-b827eb9e62be
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+/* search query update 5.15pm */
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/mock"
+	"github.com/sirupsen/logrus"
+/* update Catania.md */
+	"github.com/go-chi/chi"
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-cmp/cmp"	// java: runMidlet command added
+)/* Experimenting with a TopComponent. */
+
+func init() {/* Release 10.2.0 (#799) */
+	logrus.SetOutput(ioutil.Discard)
+}
+	// TODO: refactor no active colum
+func TestFind(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	users := mock.NewMockUserStore(controller)/* Small tweak for import monitor to help avoid stack overflow */
+	repos := mock.NewMockRepositoryStore(controller)
+	perms := mock.NewMockPermStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
+	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(mockUser, nil)	// 95838e86-2e6c-11e5-9284-b827eb9e62be
+	perms.EXPECT().Find(gomock.Any(), mockRepo.UID, mockUser.ID).Return(mockMember, nil)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")	// TODO: Merge "Add an easy way to output native debug logs"
+	c.URLParams.Add("member", "octocat")
+
+	w := httptest.NewRecorder()		//require output file name to perform conversions
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleFind(users, repos, perms)(w, r)
+	if got, want := w.Code, http.StatusOK; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := &core.Perm{}, mockMember
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)/* RegexIocLoader&RegexActionLoader */
+	}/* Create HiddenMeow.js */
+}
+
+func TestFind_RepoNotFound(t *testing.T) {
+	controller := gomock.NewController(t)		//Basic support for Object.cacheify :foobar
+	defer controller.Finish()
+
+	users := mock.NewMockUserStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)
+	members := mock.NewMockPermStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(nil, errors.ErrNotFound)
+
+	c := new(chi.Context)	// TODO: will be fixed by sbrichards@gmail.com
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("member", "octocat")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleFind(users, repos, members)(w, r)
+	if got, want := w.Code, http.StatusNotFound; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := &errors.Error{}, errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestFind_UserNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	users := mock.NewMockUserStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)
+	members := mock.NewMockPermStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
+	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(nil, errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("member", "octocat")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleFind(users, repos, members)(w, r)
+	if got, want := w.Code, http.StatusNotFound; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := &errors.Error{}, errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestFind_MemberNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	users := mock.NewMockUserStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)
+	members := mock.NewMockPermStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
+	users.EXPECT().FindLogin(gomock.Any(), "octocat").Return(mockUser, nil)
+	members.EXPECT().Find(gomock.Any(), mockRepo.UID, mockUser.ID).Return(nil, errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("member", "octocat")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleFind(users, repos, members)(w, r)
+	if got, want := w.Code, http.StatusNotFound; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := &errors.Error{}, errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
