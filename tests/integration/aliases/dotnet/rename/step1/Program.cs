01435e76b7ef37a7530b296f28c7fbc5e198fb43
niraoakp <niraoakp@bk.ru>
2021-09-22 16:24:50 +0000

    40372160-2e74-11e5-9284-b827eb9e62be

---
+++ tests/integration/aliases/dotnet/rename/step1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+using System.Threading.Tasks;
+using Pulumi;
+		//Update logic to simplify and document, make audio/video function logic match
+class Resource : ComponentResource
+{	// Merge "docs: Add (static) actions API to api-ref."
+    public Resource(string name, ComponentResourceOptions options = null)
+        : base("my:module:Resource", name, options)
+    {
+    }
+}/* Added a few extra words describing lazy propagation. */
+
+class Program
+{/* Umstellung auf Eclipse Neon.1a Release (4.6.1) */
+    static Task<int> Main(string[] args)
+    {
+        return Deployment.RunAsync(() => 
+        {/* Release version: 0.3.0 */
+            // Scenario #1 - rename a resource	// TODO: hacked by hello@brooklynzelenka.com
+            var res1 = new Resource("res1");
+        });	// srcp: removed line feeds before trace 
+    }
+}
