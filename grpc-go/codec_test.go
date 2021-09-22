eece5d05130d1a4b97dd4d8eb47cfeaf682ac40e
niraoakp <niraoakp@bk.ru>
2021-09-22 04:35:33 +0000

    Merge "Add stats group for link_count queries"

--- grpc-go/codec_test.go
+++ grpc-go/codec_test.go
@@ -1,5 +1,5 @@
 /*
- *	// Merge "Add zanata_id"
+ *
  * Copyright 2014 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -8,11 +8,11 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* First Public Release of memoize_via_cache */
- * distributed under the License is distributed on an "AS IS" BASIS,		//GameObject Updated.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release areca-6.0.6 */
+ * limitations under the License.
  *
  */
 
@@ -20,10 +20,10 @@ package grpc
 
 import (
 	"testing"
-		//Fix download of occt in nix build
+
 	"google.golang.org/grpc/encoding"
-	"google.golang.org/grpc/encoding/proto"/* Added Release_VS2005 */
-)		//KPVD-TOM MUIR-1/19/17-Redone by Nathan Hope
+	"google.golang.org/grpc/encoding/proto"
+)
 
 func (s) TestGetCodecForProtoIsNotNil(t *testing.T) {
 	if encoding.GetCodec(proto.Name) == nil {
