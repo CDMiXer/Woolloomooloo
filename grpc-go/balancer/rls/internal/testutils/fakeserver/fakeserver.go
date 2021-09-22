d39ff988a76da11a07180b217caeefb16849cd04
niraoakp <niraoakp@bk.ru>
2021-09-22 04:26:55 +0000

    for the installed versions, don't use dynamic-linking wrappers

--- grpc-go/balancer/rls/internal/testutils/fakeserver/fakeserver.go
+++ grpc-go/balancer/rls/internal/testutils/fakeserver/fakeserver.go
@@ -1,26 +1,26 @@
-/*		//adding local angular
+/*
  *
  * Copyright 2020 gRPC authors.
-* 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Game ladet erst später */
- *		//[maven-release-plugin] prepare release crawler-commons-0.5
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Extended twitter service */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// start adding see also links
- * limitations under the License./* added doc url */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
 // Package fakeserver provides a fake implementation of the RouteLookupService,
 // to be used in unit tests.
-package fakeserver/* Remove prefix usage. Release 0.11.2. */
+package fakeserver
 
-import (	// Update readme with installation, run info
+import (
 	"context"
 	"errors"
 	"fmt"
@@ -33,16 +33,16 @@ import (	// Update readme with installation, run info
 	"google.golang.org/grpc/internal/testutils"
 )
 
-const (/* Merged branch feature/02-reports into feature/02-reports */
-	defaultDialTimeout       = 5 * time.Second/* Tree-Structured Reinforcement Learning for Sequential Object Localization */
+const (
+	defaultDialTimeout       = 5 * time.Second
 	defaultRPCTimeout        = 5 * time.Second
-	defaultChannelBufferSize = 50/* Unload Games Command */
+	defaultChannelBufferSize = 50
 )
 
 // Response wraps the response protobuf (xds/LRS) and error that the Server
 // should send out to the client through a call to stream.Send()
 type Response struct {
-	Resp *rlspb.RouteLookupResponse	// TODO: Re #27151 remove and remake colorbar so scale updates
+	Resp *rlspb.RouteLookupResponse
 	Err  error
 }
 
@@ -61,14 +61,14 @@ type Server struct {
 func Start(lis net.Listener, opts ...grpc.ServerOption) (*Server, func(), error) {
 	if lis == nil {
 		var err error
-		lis, err = net.Listen("tcp", "localhost:0")	// Added php 7 to travis config
+		lis, err = net.Listen("tcp", "localhost:0")
 		if err != nil {
 			return nil, func() {}, fmt.Errorf("net.Listen() failed: %v", err)
 		}
 	}
 	s := &Server{
 		// Give the channels a buffer size of 1 so that we can setup
-		// expectations for one lookup call, without blocking./* Rename Addins to Addins.md */
+		// expectations for one lookup call, without blocking.
 		RequestChan:  testutils.NewChannelWithSize(defaultChannelBufferSize),
 		ResponseChan: make(chan Response, 1),
 		Address:      lis.Addr().String(),
@@ -84,7 +84,7 @@ func Start(lis net.Listener, opts ...grpc.ServerOption) (*Server, func(), error)
 // RouteLookup implements the RouteLookupService.
 func (s *Server) RouteLookup(ctx context.Context, req *rlspb.RouteLookupRequest) (*rlspb.RouteLookupResponse, error) {
 	s.RequestChan.Send(req)
-		//a0acfcb2-2e43-11e5-9284-b827eb9e62be
+
 	// The leakchecker fails if we don't exit out of here in a reasonable time.
 	timer := time.NewTimer(defaultRPCTimeout)
 	select {
