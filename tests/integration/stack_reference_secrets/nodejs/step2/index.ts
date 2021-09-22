3b690ed5afdb49a0e5ca9ed731dddcb48785e2d7
niraoakp <niraoakp@bk.ru>
2021-09-22 02:17:56 +0000

    Merge "[Config ] Drop conflicting orphan ACLs"

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -1,14 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
 
-export const normal = pulumi.output("normal");/* README.md some grammar fixes */
+export const normal = pulumi.output("normal");
 export const secret = pulumi.secret("secret");
 
 // Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
 // deployment.
 const org = new pulumi.Config().require("org");
 const project = pulumi.getProject();
-const stack = pulumi.getStack();/* properties loading implemented */
+const stack = pulumi.getStack();
 const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
 
 export const refNormal = sr.getOutput("normal");
-export const refSecret = sr.getOutput("secret");/* Rename SOS_queue_prob to SOS_queue_prob.py */
+export const refSecret = sr.getOutput("secret");
