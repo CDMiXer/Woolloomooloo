eead9cd6f20d6cc31fc110815592312b0c235638
niraoakp <niraoakp@bk.ru>
2021-09-22 01:56:18 +0000

    Hide timecheck-thread from webinterface status page.

--- plugin/converter/remote_oss.go
+++ plugin/converter/remote_oss.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release the GIL in all File calls */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Document public API more */
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Made changes to the UI
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release for 2.16.0 */
-// See the License for the specific language governing permissions and/* Update Release Log v1.3 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss	// Libedit: fix a bug (affects only multi parts per packages) after moving an item.
-		//Elegant code and inelegant code.  Switches and Callbacks
-package converter
+// +build oss		//some more safety patches (thanks to Thibaut!)
+
+package converter/* put path to liblinear in a constant */
 
 import (
 	"time"
 
 	"github.com/drone/drone/core"
 )
-
+		//Added internals.png
 // Remote returns a conversion service that converts the
 // configuration file using a remote http service.
 func Remote(endpoint, signer, extension string, skipVerify bool, timeout time.Duration) core.ConvertService {
