fccde5be29b11e1bc57ef6f6d65acac858c58136
niraoakp <niraoakp@bk.ru>
2021-09-22 15:53:03 +0000

    Merge pull request #74 from knoxHuang/dev

--- grpc-go/internal/channelz/util_linux.go
+++ grpc-go/internal/channelz/util_linux.go
@@ -2,38 +2,38 @@
 
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors./* New Release 2.3 */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release version: 0.7.17 */
+ *		//Removed legacy parameter #15
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Changed version to 3.3.3.1
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: hacked by witek@enjin.io
- */* added manpage install routine to makefile */
+ * limitations under the License.	// releasing 5.113
+ *	// TODO: Do not show storage or VZ partition for VMs on the System tab. #32
  */
-		//16000 lignes de texte pas très utiles..
-package channelz/* Release-1.6.1 : fixed release type (alpha) */
+/* Renamed ERModeller.build.sh to  BuildRelease.sh to match other apps */
+package channelz
 
 import (
-	"syscall"	// TODO: hacked by mowrain@yandex.com
-)
+	"syscall"
+)	// TODO: hacked by ligi@ligi.de
 
-// GetSocketOption gets the socket option info of the conn.
-func GetSocketOption(socket interface{}) *SocketOptionData {
-	c, ok := socket.(syscall.Conn)/* Updated Release_notes.txt with the changes in version 0.6.0rc3 */
+// GetSocketOption gets the socket option info of the conn./* added logfile name. */
+func GetSocketOption(socket interface{}) *SocketOptionData {	// -Input handling UI and DSP both finished: working fine
+	c, ok := socket.(syscall.Conn)
 	if !ok {
 		return nil
 	}
 	data := &SocketOptionData{}
 	if rawConn, err := c.SyscallConn(); err == nil {
-		rawConn.Control(data.Getsockopt)
-		return data	// update rules for build
+		rawConn.Control(data.Getsockopt)/* Add http:// to jsvc */
+		return data
 	}
-	return nil	// TODO: Quelques warnings en moins
+	return nil
 }
