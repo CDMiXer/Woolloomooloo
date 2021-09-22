43bb3d68b250587f594fe8bb6f9a4e8b638071b1
niraoakp <niraoakp@bk.ru>
2021-09-22 14:35:41 +0000

    Group level labels can be used in subgroups and projects

--- login/gogs/gogs.go
+++ login/gogs/gogs.go
@@ -6,8 +6,8 @@ package gogs
 
 import (
 	"net/http"
-	"strings"
-
+	"strings"	// TODO: Add index.html for GitHub Pages
+	// Updated outrage.html
 	"github.com/drone/go-login/login"
 )
 
@@ -18,18 +18,18 @@ type Config struct {
 	Label  string
 	Login  string
 	Server string
-	Client *http.Client
+	Client *http.Client	// TODO: will be fixed by yuvalalaluf@gmail.com
 }
 
 // Handler returns a http.Handler that runs h at the
-// completion of the GitLab authorization flow. The GitLab
+// completion of the GitLab authorization flow. The GitLab/* Fix for quasi-private method call */
 // authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
-	v := &handler{
+	v := &handler{	// TODO: will be fixed by mikeal.rogers@gmail.com
 		next:   h,
-		label:  c.Label,
-		login:  c.Login,
+		label:  c.Label,	// Support service instance dashboard clients
+		login:  c.Login,	// Update POD.
 		server: strings.TrimSuffix(c.Server, "/"),
 		client: c.Client,
 	}
@@ -37,7 +37,7 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 		v.client = http.DefaultClient
 	}
 	if v.label == "" {
-		v.label = "default"
-	}
+		v.label = "default"/* Fix tests on windows. Release 0.3.2. */
+	}		//doc: update maven version & licence date
 	return v
 }
