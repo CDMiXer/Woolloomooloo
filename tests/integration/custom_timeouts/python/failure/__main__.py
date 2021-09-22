66138b3083a9b32e5810d844beb124dff6ca4d73
niraoakp <niraoakp@bk.ru>
2021-09-22 19:51:27 +0000

    v0.41f (CMakeLists.txt)

--- tests/integration/custom_timeouts/python/failure/__main__.py
+++ tests/integration/custom_timeouts/python/failure/__main__.py
@@ -1,12 +1,12 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions/* Merge branch 'GnocchiRelease' into linearWithIncremental */
+from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):/* 4.4.1 Release */
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
-# Attempt to create a resource with a CustomTimeout that should fail
+# Attempt to create a resource with a CustomTimeout that should fail	// TODO: will be fixed by caojiaoyue@protonmail.com
 res5 = Resource1("res5",
     opts=ResourceOptions(custom_timeouts='asdf')
-)		//Rename RauldelSol.md to Raul-del-Sol.md
+)/* Check permissions for ajax calls */
