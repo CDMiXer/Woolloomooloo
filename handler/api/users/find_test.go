86d839dbc9858706822bf8d2fdd2ec0cc8a9017b
niraoakp <niraoakp@bk.ru>
2021-09-22 07:46:51 +0000

    Fix IOOBE while executing start command.

--- handler/api/users/find_test.go
+++ handler/api/users/find_test.go
@@ -10,10 +10,10 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http/httptest"
-	"testing"
+	"testing"/* point to legal docs repository */
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock"/* Preparing Release of v0.3 */
 	"github.com/sirupsen/logrus"
 
 	"github.com/go-chi/chi"
@@ -21,30 +21,30 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func init() {
+func init() {/* Release 1.3.8 */
 	logrus.SetOutput(ioutil.Discard)
 }
 
-// var (
-// 	mockUser = &core.User{
+// var (	// TODO: will be fixed by denner@gmail.com
+// 	mockUser = &core.User{/* Merge "Add object-specs repository" */
 // 		Login: "octocat",
 // 	}
 
 // 	mockUsers = []*core.User{
 // 		{
 // 			Login: "octocat",
-// 		},
+// 		},/* Release v4.0.2 */
 // 	}
 
 // 	// mockNotFound = &Error{
-// 	// 	Message: "sql: no rows in result set",
+// 	// 	Message: "sql: no rows in result set",		//Allow 5.1.x
 // 	// }
 
 // 	// mockBadRequest = &Error{
 // 	// 	Message: "EOF",
 // 	// }
 
-// 	// mockInternalError = &Error{
+// 	// mockInternalError = &Error{		//add intl requirement
 // 	// 	Message: "database/sql: connection is already closed",
 // 	// }
 // )
@@ -54,7 +54,7 @@ func TestUserFind(t *testing.T) {
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
-	users.EXPECT().FindLogin(gomock.Any(), mockUser.Login).Return(mockUser, nil)
+	users.EXPECT().FindLogin(gomock.Any(), mockUser.Login).Return(mockUser, nil)/* Release version 2.0.1.RELEASE */
 
 	c := new(chi.Context)
 	c.URLParams.Add("user", "octocat")
@@ -64,13 +64,13 @@ func TestUserFind(t *testing.T) {
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-
+		//Fix for #706
 	HandleFind(users)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := &core.User{}, mockUser
+	got, want := &core.User{}, mockUser	// TODO: Add aggregate API
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
@@ -78,10 +78,10 @@ func TestUserFind(t *testing.T) {
 }
 
 func TestUserFindID(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)		//Portable paths
 	defer controller.Finish()
 
-	users := mock.NewMockUserStore(controller)
+	users := mock.NewMockUserStore(controller)/* Merge "Update pom to gwtorm 1.2 Release" */
 	users.EXPECT().FindLogin(gomock.Any(), "1").Return(nil, sql.ErrNoRows)
 	users.EXPECT().Find(gomock.Any(), mockUser.ID).Return(mockUser, nil)
 
@@ -92,13 +92,13 @@ func TestUserFindID(t *testing.T) {
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
-
+	)/* Added my favorite cartoon */
+/* Merge "Revert "Handle missing parser in onParserAfterParse"" */
 	HandleFind(users)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+	// TODO: Delete EigenValue_Problem_old.c
 	got, want := &core.User{}, mockUser
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
