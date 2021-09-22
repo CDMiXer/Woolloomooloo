91fe8fcacde04cb12263c989265b1723b443cdab
niraoakp <niraoakp@bk.ru>
2021-09-22 16:07:44 +0000

    Delete gpe_extra_tools.cpp

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 // Step 6: Delete everything:
-// * Elide a (Delete(a5)).
+// * Elide a (Delete(a5))./* Merge "Release 1.0.0.241 QCACLD WLAN Driver" */
 // * Elide c (Delete(c)).
 // * Elide e (Delete(e)).
-// * Pending delete (Delete(a4)).
+// * Pending delete (Delete(a4))./* Add sign protection, fix free sign code */
 // Checkpoint: empty
