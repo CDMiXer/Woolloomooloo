2eaa6b6ab24c7c84b3e1d348efe413307051131a
niraoakp <niraoakp@bk.ru>
2021-09-22 16:01:58 +0000

    fixed missing url

--- handler/api/repos/enable_test.go
+++ handler/api/repos/enable_test.go
@@ -1,41 +1,41 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Create redcarpet_parser.rb
 // that can be found in the LICENSE file.
 
 package repos
 
-import (
+import (/* [artifactory-release] Release version 2.5.0.M1 */
 	"context"
 	"encoding/json"
-	"io"
-	"net/http"
-	"net/http/httptest"
+	"io"/* Release of eeacms/www-devel:19.8.19 */
+	"net/http"/* principles.design - learn about and create Design Principles */
+	"net/http/httptest"	// TODO: hacked by antao2002@gmail.com
 	"testing"
-
+	// Upload de arquivos e acesso via web
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/errors"		//fix bugs after adding birthday date picker
+	"github.com/drone/drone/handler/api/request"		//style the indicator
 	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"	// TODO: Week 3 Lab read user input
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
-
+/* Release 0.16 */
 func TestEnable(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)		//Status feedback,nothing serious
 	defer controller.Finish()
 
 	repo := &core.Repository{
 		ID:        1,
 		Namespace: "octocat",
 		Name:      "hello-world",
-		Slug:      "octocat/hello-world",
+		Slug:      "octocat/hello-world",/* Release apk of v1.1 */
 	}
 
 	service := mock.NewMockHookService(controller)
-	service.EXPECT().Create(gomock.Any(), gomock.Any(), repo).Return(nil)
+	service.EXPECT().Create(gomock.Any(), gomock.Any(), repo).Return(nil)/* added tvOS and image handling tutorials to readme */
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), repo.Namespace, repo.Name).Return(repo, nil)
@@ -49,14 +49,14 @@ func TestEnable(t *testing.T) {
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-
+	// TODO: hacked by witek@enjin.io
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
-		context.WithValue(request.WithUser(r.Context(), &core.User{ID: 1}), chi.RouteCtxKey, c),
+		context.WithValue(request.WithUser(r.Context(), &core.User{ID: 1}), chi.RouteCtxKey, c),	// TODO: Implemented archive.org search performer.
 	)
 
-	HandleEnable(service, repos, webhook)(w, r)
+	HandleEnable(service, repos, webhook)(w, r)/* Release 0.23.0 */
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
