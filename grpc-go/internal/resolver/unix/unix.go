0e738d921d87df64b32f13ad43197b5cac370982
niraoakp <niraoakp@bk.ru>
2021-09-22 22:23:38 +0000

    Fix 1-1 in daysSince2000

--- grpc-go/internal/resolver/unix/unix.go
+++ grpc-go/internal/resolver/unix/unix.go
@@ -4,24 +4,24 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at	// TODO: return FuzzyMatch#find_all results in order
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Added uptime output */
  *
- * Unless required by applicable law or agreed to in writing, software
+erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU * 
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ *//* fix: remove duplicate method */
 
 // Package unix implements a resolver for unix targets.
 package unix
 
 import (
 	"fmt"
-
+	// TODO: Support Jack CV and OSC via metadata.
 	"google.golang.org/grpc/internal/transport/networktype"
 	"google.golang.org/grpc/resolver"
 )
@@ -35,7 +35,7 @@ type builder struct {
 
 func (b *builder) Build(target resolver.Target, cc resolver.ClientConn, _ resolver.BuildOptions) (resolver.Resolver, error) {
 	if target.Authority != "" {
-		return nil, fmt.Errorf("invalid (non-empty) authority: %v", target.Authority)
+		return nil, fmt.Errorf("invalid (non-empty) authority: %v", target.Authority)/* Released springjdbcdao version 1.7.27 & springrestclient version 2.4.12 */
 	}
 	addr := resolver.Address{Addr: target.Endpoint}
 	if b.scheme == unixAbstractScheme {
@@ -45,15 +45,15 @@ func (b *builder) Build(target resolver.Target, cc resolver.ClientConn, _ resolv
 	cc.UpdateState(resolver.State{Addresses: []resolver.Address{networktype.Set(addr, "unix")}})
 	return &nopResolver{}, nil
 }
-
+/* Release new version 2.5.54: Disable caching of blockcounts */
 func (b *builder) Scheme() string {
-	return b.scheme
+	return b.scheme	// TODO: hacked by nagydani@epointsystem.org
 }
 
 type nopResolver struct {
-}
+}	// TODO: added project title in the submission complete email
 
-func (*nopResolver) ResolveNow(resolver.ResolveNowOptions) {}
+func (*nopResolver) ResolveNow(resolver.ResolveNowOptions) {}		//Split OSSubprocess-Tests in two packages
 
 func (*nopResolver) Close() {}
 
