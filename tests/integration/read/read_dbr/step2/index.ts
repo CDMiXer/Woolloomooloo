a80ec0404a1e4ab320290286c9969a30fbe5b091
niraoakp <niraoakp@bk.ru>
2021-09-22 17:06:53 +0000

    #238: use xs:all in 'activiy' complexType instaad of xs:sequence

--- tests/integration/read/read_dbr/step2/index.ts
+++ tests/integration/read/read_dbr/step2/index.ts
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release version 0.0.6 */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -12,15 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";	// TODO: hacked by earlephilhower@yahoo.com
-	// TODO: hacked by ng8eke@163.com
+import { Resource } from "./resource";
+
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
 
-// B must be replaced, but it is a DBR replacement.	// TODO: hacked by ng8eke@163.com
+// B must be replaced, but it is a DBR replacement.
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 2)});
 
 // C depends on B, so it gets re-read. Before the read, it is removed from the
-// snapshot due to the deletion of B./* Implement browsing options */
+// snapshot due to the deletion of B.
 const c = new Resource("c", { state: b.state }, { id: "another-existing-id" })
 
 // The engine generates:
