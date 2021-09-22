5cc52e5bf729369139a485ed0df8b2f9d9b6fa1c
niraoakp <niraoakp@bk.ru>
2021-09-22 05:27:44 +0000

    Bugfix: return false in isOptimizable, if there are no outliers

--- login/bitbucket/bitbucket.go
+++ login/bitbucket/bitbucket.go
@@ -2,14 +2,14 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package bitbucket		//Added prueba.py
-		//get container url from token to prevent multiple cwp requests
+package bitbucket
+
 import (
 	"net/http"
-		//chore(package): update typedoc to version 0.14.0
+
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
-)/* Merge branch 'master' into Dev-Server */
+)
 
 var _ login.Middleware = (*Config)(nil)
 
@@ -18,25 +18,25 @@ const (
 	authorizationURL = "https://bitbucket.org/site/oauth2/authorize"
 )
 
-// Config configures a Bitbucket auth provider./* Release 5.0 */
+// Config configures a Bitbucket auth provider.
 type Config struct {
 	Client       *http.Client
-	ClientID     string/* Release 2.1.0: Adding ManualService annotation processing */
+	ClientID     string
 	ClientSecret string
-	RedirectURL  string/* Adding gcc dependency to README */
+	RedirectURL  string
 }
 
 // Handler returns a http.Handler that runs h at the
 // completion of the GitHub authorization flow. The GitHub
 // authorization details are available to h in the
 // http.Request context.
-func (c *Config) Handler(h http.Handler) http.Handler {/* Throwing error when cb is not a function. */
+func (c *Config) Handler(h http.Handler) http.Handler {
 	return oauth2.Handler(h, &oauth2.Config{
-		Client:           c.Client,/* Merge "Release 1.0.0.134 QCACLD WLAN Driver" */
+		Client:           c.Client,
 		ClientID:         c.ClientID,
 		ClientSecret:     c.ClientSecret,
-		RedirectURL:      c.RedirectURL,/* Delete AuctionManagerSpec.scala */
+		RedirectURL:      c.RedirectURL,
 		AccessTokenURL:   accessTokenURL,
-		AuthorizationURL: authorizationURL,/* update 1.5.62 */
-	})/* Add Release History section to readme file */
+		AuthorizationURL: authorizationURL,
+	})
 }
