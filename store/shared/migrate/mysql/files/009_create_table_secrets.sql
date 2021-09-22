e6617fc48eaf87f4927682fab4cba7f4db34363f
niraoakp <niraoakp@bk.ru>
2021-09-22 06:17:36 +0000

    Add manager event listener example

--- store/shared/migrate/mysql/files/009_create_table_secrets.sql
+++ store/shared/migrate/mysql/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
--- name: create-table-secrets/* Alarms almost finished. Now creating tests. */
+-- name: create-table-secrets
 
-CREATE TABLE IF NOT EXISTS secrets (
+CREATE TABLE IF NOT EXISTS secrets (	// note daemon-runner.
  secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
-,secret_repo_id           INTEGER/* item grouping in inventory */
-,secret_name              VARCHAR(500)
-,secret_data              BLOB/* Remove no more used constant */
+,secret_repo_id           INTEGER
+,secret_name              VARCHAR(500)		//Update 02_challenge-defi.md
+,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);/* Release 0.1.2 - updated debian package info */
+);
 
 -- name: create-index-secrets-repo
 
 CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);
 
 -- name: create-index-secrets-repo-name
-	// data-retrieval
-CREATE INDEX ix_secret_repo_name ON secrets (secret_repo_id, secret_name);/* Update 13.Arrays */
+/* Release version: 0.7.5 */
+CREATE INDEX ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
