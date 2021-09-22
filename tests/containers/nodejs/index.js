bc63e4f957a2769b65fe19be4e94e727ec25528f
niraoakp <niraoakp@bk.ru>
2021-09-22 11:45:10 +0000

    Update the .gitignore to avoid conflicts with IDE files

--- tests/containers/nodejs/index.js
+++ tests/containers/nodejs/index.js
@@ -1,4 +1,4 @@
-"use strict";
-const pulumi = require("@pulumi/pulumi");
+"use strict";	// TODO: Update Delaunay.hx
+const pulumi = require("@pulumi/pulumi");/* Merge branch 'release/0.9.28' into develop */
 const config = new pulumi.Config();
 console.log("Hello from", config.require("runtime"));
