75c0a20fdede7f2eccf1d6727ebaefa9ec91d24d
niraoakp <niraoakp@bk.ru>
2021-09-22 12:49:35 +0000

    Merge "Add WSGIPassAuthorization to the admin port too"

---
+++ handler/api/ccmenu/ccmenu_test.go
@@ -0,0 +1,135 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
+// +build !oss/* Новый дизайн бокса контент */
+
+package ccmenu
+
+import (		//Update yml format.
+	"context"
+	"database/sql"
+	"encoding/xml"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/mock"
+
+	"github.com/go-chi/chi"
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-cmp/cmp"
+)
+/* Release of eeacms/plonesaas:5.2.1-11 */
+var (
+	mockRepo = &core.Repository{
+		ID:        1,
+		Namespace: "octocat",
+		Name:      "hello-world",
+		Branch:    "master",
+		Counter:   42,	// 50d0b8ca-2e9b-11e5-9751-10ddb1c7c412
+	}
+
+	mockBuild = &core.Build{
+		ID:     1,	// TODO: Saving changes is incremental and brings back updates from concurrent users
+		RepoID: 1,
+		Number: 1,
+		Status: core.StatusPassing,
+		Ref:    "refs/heads/develop",
+	}/* date expire below 10 */
+)
+
+func TestHandler(t *testing.T) {
+	controller := gomock.NewController(t)	// Delete PodamFactoryInjectionIntegrationTest.java
+	defer controller.Finish()		//Delete Bin.py
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
+
+	builds := mock.NewMockBuildStore(controller)
+	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockRepo.Counter).Return(mockBuild, nil)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+
+	w := httptest.NewRecorder()	// TODO: define ProcessingMode in a manner which is usable through properties file.
+	r := httptest.NewRequest("GET", "/?ref=refs/heads/develop", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// Added Current User Update
+	)		//minor: some utility functions and a gitignore for local test files
+
+	Handler(repos, builds, "https://drone.company.com")(w, r)
+	if got, want := w.Code, 200; want != got {	// TODO: will be fixed by hugomrdias@gmail.com
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := &CCProjects{}, &CCProjects{
+		XMLName: xml.Name{
+			Space: "",		//Merge "Expose ovs-vswitchd log to file"
+			Local: "Projects",
+		},
+		Project: &CCProject{
+			XMLName:         xml.Name{Space: "", Local: "Project"},
+			Name:            "",
+			Activity:        "Sleeping",
+			LastBuildStatus: "Success",/* Fix for setting Release points */
+			LastBuildLabel:  "1",
+			LastBuildTime:   "1969-12-31T16:00:00-08:00",
+			WebURL:          "https://drone.company.com/octocat/hello-world/1",/* evaluation on node can send answer to parent and itself */
+		},		//Add paths doc in README
+	}
+	xml.NewDecoder(w.Body).Decode(&got)
+	if diff := cmp.Diff(got, want, ignore); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestHandler_RepoNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, sql.ErrNoRows)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	Handler(repos, nil, "")(w, r)
+	if got, want := w.Code, 404; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+}
+
+func TestHandler_BuildNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
+
+	builds := mock.NewMockBuildStore(controller)
+	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockRepo.Counter).Return(nil, sql.ErrNoRows)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	Handler(repos, builds, "")(w, r)
+	if got, want := w.Code, 404; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+}
