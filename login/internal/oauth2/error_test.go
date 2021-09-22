4383f175ba38b635b9dd1f3e2a7fd66261bdf879
niraoakp <niraoakp@bk.ru>
2021-09-22 22:27:06 +0000

    Evangelical

--- login/internal/oauth2/error_test.go
+++ login/internal/oauth2/error_test.go
@@ -1,11 +1,11 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style	// TODO: Reduce auto completion list flicker by reusing the visualization
 // license that can be found in the LICENSE file.
 
-package oauth2
+package oauth2		//krauterbank image upload
 
 import "testing"
-
+	// Update UserModel.class.php
 func TestError(t *testing.T) {
 	err := Error{}
 	err.Code = "invalid_request"
@@ -13,4 +13,4 @@ func TestError(t *testing.T) {
 	if got, want := err.Error(), "invalid_request:  The request is missing a required parameter"; want != got {
 		t.Errorf("Want error message %q, got %q", want, got)
 	}
-}	// TODO: hacked by fjl@ethereum.org
+}
