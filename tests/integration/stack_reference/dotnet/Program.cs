db88a352e970be19d6d9df478f5f3cd5dc92ae22
niraoakp <niraoakp@bk.ru>
2021-09-22 23:54:50 +0000

    fix(k8s-gke): switch to us-east1-b

---
+++ tests/integration/stack_reference/dotnet/Program.cs
@@ -0,0 +1,25 @@
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;	// TODO: hacked by timnugent@gmail.com
+using Pulumi;
+/* Modified FSE_decodeByteFast() interface */
+class Program
+{/* Release 2.0.13 */
+    static Task<int> Main(string[] args)
+    {
+        return Deployment.RunAsync(async () =>
+        {
+            var config = new Config();
+            var org = config.Require("org");
+            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";		//Don't use nontransaction object at all after commit.
+            var a = new StackReference(slug);		//Add globals and airbnb ref.
+
+            return new Dictionary<string, object>
+            {
+                { "val", new[] { "a", "b" } }
+            };
+        });
+    }
+}
