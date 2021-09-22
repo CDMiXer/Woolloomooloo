b889141478c551d7960ddee31b8660f5fe05989d
niraoakp <niraoakp@bk.ru>
2021-09-22 03:52:13 +0000

    Redirecionamento de paginas ao navegar apos login e logout

--- tests/integration/recreate_resource_check/step1/index.ts
+++ tests/integration/recreate_resource_check/step1/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";
-
+import { Resource } from "./resource";	// TODO: hacked by nick@perfectabstractions.com
+/* percentile stats */
 // Base depends on nothing.
-const a = new Resource("base", { uniqueKey: 1, state: 99 });		//New version of FlatOn - 1.0.4
+const a = new Resource("base", { uniqueKey: 1, state: 99 });
 
-// Dependent depends on Base with state 99.	// bump version-sync to 0.5
+// Dependent depends on Base with state 99.
 const b = new Resource("dependent", { state: a.state });
