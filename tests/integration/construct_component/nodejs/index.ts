0a21c1f5775d53fe9813dd9a975cbd2021469486
niraoakp <niraoakp@bk.ru>
2021-09-22 12:06:57 +0000

    PosReader commented readChar

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-/* Rewrite README to indicate that this is a fork. */
+
 import { Component } from "./component";
 
 const componentA = new Component("a", {echo: 42});
