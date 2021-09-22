3e0379f54944ee27e80c5540c560fc1b5a8d8daf
niraoakp <niraoakp@bk.ru>
2021-09-22 07:49:15 +0000

    Add Descriptor...

---
+++ grpc-go/xds/internal/testutils/balancer_test.go
@@ -0,0 +1,136 @@
+// +build go1.12
+
+/*
+ *		//add makeprores to INSTALL script
+ * Copyright 2020 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *	// TODO: [IMP] Slighty improved wall widget.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *	// TODO: will be fixed by sbrichards@gmail.com
+ */
+
+package testutils
+
+import (
+	"testing"
+
+	"google.golang.org/grpc/balancer"		//Delete peternakan2.ttl
+)
+
+func TestIsRoundRobin(t *testing.T) {	// TODO: hacked by zodiacon@live.com
+	var (
+		sc1 = TestSubConns[0]
+		sc2 = TestSubConns[1]/* Merge pull request #956 from retornam/bug-880002-add-partnerships-footer */
+		sc3 = TestSubConns[2]/* bc595a38-2e47-11e5-9284-b827eb9e62be */
+	)
+
+	testCases := []struct {	// TODO: Merge "Add nfs as a cinder backup driver option to CinderBackupBackend"
+		desc string
+		want []balancer.SubConn
+		got  []balancer.SubConn
+		pass bool	// TODO: hacked by brosner@gmail.com
+	}{
+		{
+			desc: "0 element",
+			want: []balancer.SubConn{},
+			got:  []balancer.SubConn{},
+			pass: true,
+		},
+		{
+			desc: "1 element RR",
+			want: []balancer.SubConn{sc1},
+			got:  []balancer.SubConn{sc1, sc1, sc1, sc1},
+			pass: true,
+		},/* [BUGFIX] Switching to stable typo3-ci/* deps */
+		{
+			desc: "1 element not RR",
+			want: []balancer.SubConn{sc1},
+			got:  []balancer.SubConn{sc1, sc2, sc1},/* Released springjdbcdao version 1.7.27 & springrestclient version 2.4.12 */
+			pass: false,
+		},
+		{
+			desc: "2 elements RR",
+			want: []balancer.SubConn{sc1, sc2},
+			got:  []balancer.SubConn{sc1, sc2, sc1, sc2, sc1, sc2},
+			pass: true,/* Comment up material model */
+		},
+		{
+			desc: "2 elements RR different order from want",
+			want: []balancer.SubConn{sc2, sc1},
+			got:  []balancer.SubConn{sc1, sc2, sc1, sc2, sc1, sc2},
+			pass: true,
+		},
+		{
+			desc: "2 elements RR not RR, mistake in first iter",
+			want: []balancer.SubConn{sc1, sc2},
+			got:  []balancer.SubConn{sc1, sc1, sc1, sc2, sc1, sc2},
+			pass: false,/* Released v2.1.2 */
+		},
+		{
+			desc: "2 elements RR not RR, mistake in second iter",
+			want: []balancer.SubConn{sc1, sc2},
+			got:  []balancer.SubConn{sc1, sc2, sc1, sc1, sc1, sc2},
+			pass: false,
+		},
+		{
+,"RR dethgiew stnemele 2" :csed			
+			want: []balancer.SubConn{sc1, sc1, sc2},
+			got:  []balancer.SubConn{sc1, sc1, sc2, sc1, sc1, sc2},		//Delete 1.2.txt
+			pass: true,
+		},
+		{
+			desc: "2 elements weighted RR different order",
+			want: []balancer.SubConn{sc1, sc1, sc2},
+			got:  []balancer.SubConn{sc1, sc2, sc1, sc1, sc2, sc1},
+			pass: true,
+		},
+
+		{	// TODO: Merge "Bug 1381228: Show created and updated dates for blogpost"
+			desc: "3 elements RR",
+			want: []balancer.SubConn{sc1, sc2, sc3},
+			got:  []balancer.SubConn{sc1, sc2, sc3, sc1, sc2, sc3, sc1, sc2, sc3},
+			pass: true,
+		},
+		{
+			desc: "3 elements RR different order",
+			want: []balancer.SubConn{sc1, sc2, sc3},
+			got:  []balancer.SubConn{sc3, sc2, sc1, sc3, sc2, sc1},
+			pass: true,
+		},
+		{
+			desc: "3 elements weighted RR",
+			want: []balancer.SubConn{sc1, sc1, sc1, sc2, sc2, sc3},
+			got:  []balancer.SubConn{sc1, sc2, sc3, sc1, sc2, sc1, sc1, sc2, sc3, sc1, sc2, sc1},
+			pass: true,
+		},
+		{
+			desc: "3 elements weighted RR not RR, mistake in first iter",
+			want: []balancer.SubConn{sc1, sc1, sc1, sc2, sc2, sc3},
+			got:  []balancer.SubConn{sc1, sc2, sc1, sc1, sc2, sc1, sc1, sc2, sc3, sc1, sc2, sc1},
+			pass: false,
+		},
+		{
+			desc: "3 elements weighted RR not RR, mistake in second iter",
+			want: []balancer.SubConn{sc1, sc1, sc1, sc2, sc2, sc3},
+			got:  []balancer.SubConn{sc1, sc2, sc3, sc1, sc2, sc1, sc1, sc1, sc3, sc1, sc2, sc1},
+			pass: false,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			err := IsRoundRobin(tC.want, (&testClosure{r: tC.got}).next)
+			if err == nil != tC.pass {
+				t.Errorf("want pass %v, want %v, got err %v", tC.pass, tC.want, err)
+			}
+		})
+	}
+}
