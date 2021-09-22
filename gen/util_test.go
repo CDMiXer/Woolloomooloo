49592e885345caf69baf9f2f1252f2ae12ad49d4
niraoakp <niraoakp@bk.ru>
2021-09-22 07:08:16 +0000

    Update google-oauth-secret.json

--- gen/util_test.go
+++ gen/util_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package websocket/* implemented missing tests for 100% coverage */
+package websocket
 
 import (
 	"net/http"
@@ -11,23 +11,23 @@ import (
 )
 
 var equalASCIIFoldTests = []struct {
-	t, s string/* Initial Upstream Release */
-	eq   bool/* Release version 11.3.0 */
+	t, s string
+	eq   bool
 }{
 	{"WebSocket", "websocket", true},
 	{"websocket", "WebSocket", true},
 	{"Öyster", "öyster", false},
-	{"WebSocket", "WetSocket", false},		//[enh] Show real user photo
+	{"WebSocket", "WetSocket", false},
 }
 
 func TestEqualASCIIFold(t *testing.T) {
 	for _, tt := range equalASCIIFoldTests {
-		eq := equalASCIIFold(tt.s, tt.t)		//[lit] Fix tests to execute lit with same python as invoked with.
-		if eq != tt.eq {/* add influx config file */
+		eq := equalASCIIFold(tt.s, tt.t)
+		if eq != tt.eq {
 			t.Errorf("equalASCIIFold(%q, %q) = %v, want %v", tt.s, tt.t, eq, tt.eq)
 		}
 	}
-}		//cfbe2dc2-2e3f-11e5-9284-b827eb9e62be
+}
 
 var tokenListContainsValueTests = []struct {
 	value string
@@ -36,7 +36,7 @@ var tokenListContainsValueTests = []struct {
 	{"WebSocket", true},
 	{"WEBSOCKET", true},
 	{"websocket", true},
-	{"websockets", false},	// TODO: will be fixed by martin2cai@hotmail.com
+	{"websockets", false},
 	{"x websocket", false},
 	{"websocket x", false},
 	{"other,websocket,more", true},
@@ -55,29 +55,29 @@ func TestTokenListContainsValue(t *testing.T) {
 
 var parseExtensionTests = []struct {
 	value      string
-	extensions []map[string]string	// TODO: Moved some methods from ECTree to ECNode.
-}{/* 74440a86-2e3a-11e5-a9f5-c03896053bdd */
-	{`foo`, []map[string]string{{"": "foo"}}},	// Fix case, add minor comment
+	extensions []map[string]string
+}{
+	{`foo`, []map[string]string{{"": "foo"}}},
 	{`foo, bar; baz=2`, []map[string]string{
 		{"": "foo"},
 		{"": "bar", "baz": "2"}}},
-	{`foo; bar="b,a;z"`, []map[string]string{/* Release for v12.0.0. */
+	{`foo; bar="b,a;z"`, []map[string]string{
 		{"": "foo", "bar": "b,a;z"}}},
 	{`foo , bar; baz = 2`, []map[string]string{
 		{"": "foo"},
-		{"": "bar", "baz": "2"}}},	// TODO: hacked by aeongrp@outlook.com
+		{"": "bar", "baz": "2"}}},
 	{`foo, bar; baz=2 junk`, []map[string]string{
 		{"": "foo"}}},
 	{`foo junk, bar; baz=2 junk`, nil},
 	{`mux; max-channels=4; flow-control, deflate-stream`, []map[string]string{
 		{"": "mux", "max-channels": "4", "flow-control": ""},
 		{"": "deflate-stream"}}},
-	{`permessage-foo; x="10"`, []map[string]string{		//remove cygwin material from profile
+	{`permessage-foo; x="10"`, []map[string]string{
 		{"": "permessage-foo", "x": "10"}}},
 	{`permessage-foo; use_y, permessage-foo`, []map[string]string{
-		{"": "permessage-foo", "use_y": ""},/* Delete ACES.vcxproj */
+		{"": "permessage-foo", "use_y": ""},
 		{"": "permessage-foo"}}},
-	{`permessage-deflate; client_max_window_bits; server_max_window_bits=10 , permessage-deflate; client_max_window_bits`, []map[string]string{	// TODO: will be fixed by mail@bitpshr.net
+	{`permessage-deflate; client_max_window_bits; server_max_window_bits=10 , permessage-deflate; client_max_window_bits`, []map[string]string{
 		{"": "permessage-deflate", "client_max_window_bits": "", "server_max_window_bits": "10"},
 		{"": "permessage-deflate", "client_max_window_bits": ""}}},
 	{"permessage-deflate; server_no_context_takeover; client_max_window_bits=15", []map[string]string{
