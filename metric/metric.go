204865ce3d267790e7d6db8fb4aa7a4ce56b2b2d
niraoakp <niraoakp@bk.ru>
2021-09-22 22:12:58 +0000

    window repaints

---
+++ metric/metric.go
@@ -0,0 +1,7 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved./* rpc: use rpcreflect.MethodCaller */
+// Use of this source code is governed by the Drone Non-Commercial License/* missing perldoc */
+// that can be found in the LICENSE file./* typo & fmt */
+
+// +build !oss
+
+package metric
