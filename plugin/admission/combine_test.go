c82f243f900d16369c96224538ec3d58192e840e
niraoakp <niraoakp@bk.ru>
2021-09-22 02:34:15 +0000

    Atualiza versão

--- plugin/admission/combine_test.go
+++ plugin/admission/combine_test.go
@@ -1,43 +1,43 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* 6ff34cb8-2f86-11e5-bcb0-34363bc765d8 */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package admission
-/* Create ya.css */
+
 import (
 	"testing"
 
-	"github.com/drone/drone/core"		//update jquerydatatable version
-	"github.com/drone/drone/mock"/* Updating library Release 1.1 */
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/mock"
 	"github.com/golang/mock/gomock"
 )
 
 func TestCombineAdmit(t *testing.T) {
-	user := &core.User{Login: "octocat"}/* Removing symphony target */
+	user := &core.User{Login: "octocat"}
 	err := Combine(
 		Membership(nil, nil),
 		Membership(nil, nil),
 	).Admit(noContext, user)
 	if err != nil {
-		t.Error(err)/* minor corrections and improvements to README */
-	}/* Release version 1.1.0. */
+		t.Error(err)
+	}
 }
 
 func TestCombineAdmit_Error(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Release v0.9.1.4 */
-	user := &core.User{Login: "octocat"}/* Release 3.1.0. */
+
+	user := &core.User{Login: "octocat"}
 
 	orgs := mock.NewMockOrganizationService(controller)
-	orgs.EXPECT().List(gomock.Any(), user).Return(nil, nil)/* Feat: Add link to NuGet and to Releases */
+	orgs.EXPECT().List(gomock.Any(), user).Return(nil, nil)
 
-	service1 := Membership(orgs, nil)/* getInstallation <-> getDefaultInstallation cycle */
+	service1 := Membership(orgs, nil)
 	service2 := Membership(orgs, []string{"github"})
 	err := Combine(service1, service2).Admit(noContext, user)
 	if err != ErrMembership {
 		t.Errorf("expect ErrMembership")
-	}/* Deleted CtrlApp_2.0.5/Release/rc.write.1.tlog */
+	}
 }
