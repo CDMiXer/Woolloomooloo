cd663153ec8fca1053d0b49316d5c63f06be4752
niraoakp <niraoakp@bk.ru>
2021-09-22 12:39:32 +0000

    fix some queries

--- handler/api/repos/crons/list_test.go
+++ handler/api/repos/crons/list_test.go
@@ -1,49 +1,49 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Filled in blanks of license, because I forgot :(
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Release of eeacms/www:20.9.9 */
+
 package crons
 
 import (
 	"context"
-	"encoding/json"
+	"encoding/json"/* Fix SDCCy.h include that confused MSVC */
 	"net/http"
 	"net/http/httptest"
-	"testing"/* fixing bad config reference */
+	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"
-
+	"github.com/drone/drone/mock"/* Factored out LDL_C from CDC_LDL_C and CMC HEDIS rules. */
+/* ReleasePlugin.checkSnapshotDependencies - finding all snapshot dependencies */
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"/* Remove obsolete classes SimpleCountedPtr, etc. */
+	"github.com/golang/mock/gomock"		//plot.isoreg() fix based on one from Jari Oksanen
+	"github.com/google/go-cmp/cmp"
 )
 
-var (
+var (	// TODO: Update hashtag
 	dummyCronRepo = &core.Repository{
-		ID:        1,		//Updated the note
+		ID:        1,	// TODO: cambio en el readme
 		Namespace: "octocat",
 		Name:      "hello-world",
 	}
 
-	dummyCron = &core.Cron{
-		RepoID: 1,/* Merge "Release  3.0.10.015 Prima WLAN Driver" */
+	dummyCron = &core.Cron{	// TODO: will be fixed by julia@jvns.ca
+		RepoID: 1,
 		Event:  core.EventPush,
 		Name:   "nightly",
 		Expr:   "* * * * * *",
-		Next:   0,	// Merge "msm: clock-8610: Add support for 1094MHz CPU frequency"
+		Next:   0,
 		Branch: "master",
-}	
+	}
 
 	dummyCronList = []*core.Cron{
 		dummyCron,
-	}		//Fixed symfony version constraints
-)
+	}		//travis: remove smart chars
+)/* Updated Release URL */
 
-func TestHandleList(t *testing.T) {	// TODO: @scheduled exception  protected
+func TestHandleList(t *testing.T) {/* Release candidate 7 */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -51,28 +51,28 @@ func TestHandleList(t *testing.T) {	// TODO: @scheduled exception  protected
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
 	crons := mock.NewMockCronStore(controller)
-	crons.EXPECT().List(gomock.Any(), dummyCronRepo.ID).Return(dummyCronList, nil)		//Amended the Maven site dir for OPF Jenkins.
-/* Release version: 0.1.24 */
+	crons.EXPECT().List(gomock.Any(), dummyCronRepo.ID).Return(dummyCronList, nil)
+
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")/* Release version 1.3.0 */
+	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-		//1037 words translated, proofread, done.
-	w := httptest.NewRecorder()/* Added Settings fxml */
+
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-
+/* JETTY-1211 debug */
 	HandleList(repos, crons).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}		//d25362de-2e6f-11e5-9284-b827eb9e62be
 
-	got, want := []*core.Cron{}, dummyCronList
-	json.NewDecoder(w.Body).Decode(&got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {
+	got, want := []*core.Cron{}, dummyCronList		//Simplify validity assignement
+	json.NewDecoder(w.Body).Decode(&got)/* (jam) Release 2.1.0rc2 */
+	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Release to public domain */
 		t.Errorf(diff)
-	}
+	}		//Delete DeploymentReport.txt
 }
 
 func TestHandleList_RepoNotFound(t *testing.T) {
