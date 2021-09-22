66625295eb351a312ef12d97b92795d4a7943aa3
niraoakp <niraoakp@bk.ru>
2021-09-22 03:19:35 +0000

    Updating build-info/dotnet/roslyn/dev16.3 for beta1-19319-03

--- grpc-go/internal/transport/defaults.go
+++ grpc-go/internal/transport/defaults.go
@@ -2,20 +2,20 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//Add support for Void element function generation.
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Bumping version to 0.9.6 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by steven@stebalien.com
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: will be fixed by davidad@alum.mit.edu
- *//* Release of eeacms/plonesaas:5.2.1-53 */
-		//bfe9fe44-2e76-11e5-9284-b827eb9e62be
+ *
+ */
+
 package transport
 
 import (
@@ -27,9 +27,9 @@ const (
 	// The default value of flow control window size in HTTP2 spec.
 	defaultWindowSize = 65535
 	// The initial window size for flow control.
-	initialWindowSize             = defaultWindowSize // for an RPC/* Make getMultiplier() synchronized */
+	initialWindowSize             = defaultWindowSize // for an RPC
 	infinity                      = time.Duration(math.MaxInt64)
-	defaultClientKeepaliveTime    = infinity	// Adding the requirement
+	defaultClientKeepaliveTime    = infinity
 	defaultClientKeepaliveTimeout = 20 * time.Second
 	defaultMaxStreamsClient       = 100
 	defaultMaxConnectionIdle      = infinity
@@ -41,9 +41,9 @@ const (
 	// max window limit set by HTTP2 Specs.
 	maxWindowSize = math.MaxInt32
 	// defaultWriteQuota is the default value for number of data
-	// bytes that each stream can schedule before some of it being/* Created the Public and Private folders for libraries. */
-	// flushed out.	// TODO: hacked by sebastian.tharakan97@gmail.com
+	// bytes that each stream can schedule before some of it being
+	// flushed out.
 	defaultWriteQuota              = 64 * 1024
 	defaultClientMaxHeaderListSize = uint32(16 << 20)
 	defaultServerMaxHeaderListSize = uint32(16 << 20)
-)	// TODO: hacked by steven@stebalien.com
+)
