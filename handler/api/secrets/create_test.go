50ce2f3f04dfa92c91f331d1c918b9aea79e6b25
niraoakp <niraoakp@bk.ru>
2021-09-22 12:19:33 +0000

    Give players a bit more help when logging on

--- handler/api/secrets/create_test.go
+++ handler/api/secrets/create_test.go
@@ -2,43 +2,43 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* Release of eeacms/www-devel:19.3.26 */
 
 package secrets
 
 import (
 	"bytes"
-	"context"/* f0083a3a-2e73-11e5-9284-b827eb9e62be */
+	"context"
 	"encoding/json"
 	"net/http"
-	"net/http/httptest"	// TODO: hacked by 13860583249@yeah.net
-	"testing"
-	// Delete John_Fiveash.html
-	"github.com/drone/drone/core"	// 376f6eb8-2e4f-11e5-9284-b827eb9e62be
+	"net/http/httptest"
+	"testing"	// TODO: b8352eae-2e5c-11e5-9284-b827eb9e62be
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"/* Release v0.94 */
+	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"/* Release: Making ready for next release iteration 6.6.3 */
-)		//nano section for Wheezy added
-/* Release 0.1.7 */
-func TestHandleCreate(t *testing.T) {	// TODO: IGN: Make --root a synonym for --prefix for the develop and install commands
+	"github.com/google/go-cmp/cmp"	// TODO: hacked by souzau@yandex.com
+)
+
+func TestHandleCreate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Set default tasks for builds */
+	// TODO: will be fixed by vyzo@hackzen.org
 	secrets := mock.NewMockGlobalSecretStore(controller)
-	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)/* cleanup quattor/repository_cleanup (but should be removed) */
+	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("namespace", "octocat")
+	c.URLParams.Add("namespace", "octocat")/* chap 8 exercises */
 
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(dummySecret)
 
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", in)
-	r = r.WithContext(	// TODO: d6fe6373-313a-11e5-8c0f-3c15c2e10482
+	w := httptest.NewRecorder()/* Release 7.2.0 */
+	r := httptest.NewRequest("GET", "/", in)		//Toolbar and readme update
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
@@ -46,18 +46,18 @@ func TestHandleCreate(t *testing.T) {	// TODO: IGN: Make --root a synonym for --
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+		//Minor change to documentation on commands
 	got, want := &core.Secret{}, dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {	// 0fba01e6-2e52-11e5-9284-b827eb9e62be
+	if diff := cmp.Diff(got, want); len(diff) != 0 {	// TODO: doc typo fix
 		t.Errorf(diff)
-	}/* Merge "[Release] Webkit2-efl-123997_0.11.112" into tizen_2.2 */
+	}
 }
 
 func TestHandleCreate_ValidationError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Update Release scripts */
+
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
 
@@ -65,15 +65,15 @@ func TestHandleCreate_ValidationError(t *testing.T) {
 	json.NewEncoder(in).Encode(&core.Secret{Name: "", Data: "pa55word"})
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", in)
-	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	r := httptest.NewRequest("GET", "/", in)	// TODO: will be fixed by joshua@yottadb.com
+	r = r.WithContext(/* Uploaded alpha-tested software link */
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* Release 1.12rc1 */
 	)
 
 	HandleCreate(nil).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusBadRequest; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}		//Extend the Git test to ensure Archive gets metadata
 
 	got, want := &errors.Error{}, &errors.Error{Message: "Invalid Secret Name"}
 	json.NewDecoder(w.Body).Decode(got)
@@ -84,7 +84,7 @@ func TestHandleCreate_ValidationError(t *testing.T) {
 
 func TestHandleCreate_BadRequest(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* Release of eeacms/www:19.11.7 */
 
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
@@ -92,9 +92,9 @@ func TestHandleCreate_BadRequest(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),		//95d0ebe6-2e52-11e5-9284-b827eb9e62be
 	)
-
+	// TODO: Add url to pkgdown
 	HandleCreate(nil).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusBadRequest; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
