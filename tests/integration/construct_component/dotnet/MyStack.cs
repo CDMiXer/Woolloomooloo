c038df7eb259e019a74cadbb74252fab7f16837b
niraoakp <niraoakp@bk.ru>
2021-09-22 18:21:37 +0000

    integrated winstone server to build

--- tests/integration/construct_component/dotnet/MyStack.cs
+++ tests/integration/construct_component/dotnet/MyStack.cs
@@ -1,13 +1,13 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-using Pulumi;/* Add comment to windows doc */
+using Pulumi;
 
 class MyStack : Stack
 {
-    public MyStack()/* Probably better */
+    public MyStack()/* Release 2.0.0 README */
     {
-        var componentA = new Component("a", new ComponentArgs { Echo = 42 });	// TODO: hacked by ligi@ligi.de
-        var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });	// TODO: will be fixed by igor@soramitsu.co.jp
-        var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });/* Trying new iteration procedure.... will this work I wonder? */
+        var componentA = new Component("a", new ComponentArgs { Echo = 42 });
+        var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });
+        var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });
     }
-}/* Create textMe.py */
+}
