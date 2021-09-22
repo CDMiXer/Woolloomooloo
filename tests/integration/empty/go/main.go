5898093bb608c2d2d4559a94ec46be32f0369b19
niraoakp <niraoakp@bk.ru>
2021-09-22 07:10:04 +0000

    Initial module docstring

--- tests/integration/empty/go/main.go
+++ tests/integration/empty/go/main.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-package main/* Release Name = Yak */
+package main
 
-import (	// Rebuilt index with Phunmbi
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"	// TODO: will be fixed by mikeal.rogers@gmail.com
-)/* Released springjdbcdao version 1.8.19 */
+import (
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+)
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		return nil
-	})		//Ran 2to3 on cking_suite script and updated shebang to python3.
+	})
 }
