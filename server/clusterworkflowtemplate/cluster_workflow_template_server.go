a542440719776dba7f167369b2f76fd692f153bf
niraoakp <niraoakp@bk.ru>
2021-09-22 03:02:58 +0000

    our very own download urls!

---
+++ server/clusterworkflowtemplate/cluster_workflow_template_server.go
@@ -0,0 +1,126 @@
+package clusterworkflowtemplate
+
+import (
+	"context"
+	"fmt"
+	"sort"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	clusterwftmplpkg "github.com/argoproj/argo/pkg/apiclient/clusterworkflowtemplate"
+	"github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+	"github.com/argoproj/argo/server/auth"
+	"github.com/argoproj/argo/util/instanceid"
+	"github.com/argoproj/argo/workflow/creator"
+	"github.com/argoproj/argo/workflow/templateresolution"
+	"github.com/argoproj/argo/workflow/validate"
+)	// Update 4k-stogram.rb
+
+type ClusterWorkflowTemplateServer struct {
+	instanceIDService instanceid.Service
+}
+
+func NewClusterWorkflowTemplateServer(instanceID instanceid.Service) clusterwftmplpkg.ClusterWorkflowTemplateServiceServer {
+	return &ClusterWorkflowTemplateServer{instanceID}
+}
+/* [nl] added dyslectic errors and their corrections */
+func (cwts *ClusterWorkflowTemplateServer) CreateClusterWorkflowTemplate(ctx context.Context, req *clusterwftmplpkg.ClusterWorkflowTemplateCreateRequest) (*v1alpha1.ClusterWorkflowTemplate, error) {	// TODO: Split edX forum converter into 2 phases that separate entities and rels
+	wfClient := auth.GetWfClient(ctx)
+	if req.Template == nil {
+		return nil, fmt.Errorf("cluster workflow template was not found in the request body")
+	}
+	cwts.instanceIDService.Label(req.Template)
+	creator.Label(ctx, req.Template)
+	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())
+	_, err := validate.ValidateClusterWorkflowTemplate(nil, cwftmplGetter, req.Template)
+	if err != nil {
+		return nil, err
+	}/* fix FileUploader unused dont-overwrite argument */
+	return wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates().Create(req.Template)
+}		//Update ansi2html/style.py
+
+func (cwts *ClusterWorkflowTemplateServer) GetClusterWorkflowTemplate(ctx context.Context, req *clusterwftmplpkg.ClusterWorkflowTemplateGetRequest) (*v1alpha1.ClusterWorkflowTemplate, error) {
+	wfTmpl, err := cwts.getTemplateAndValidate(ctx, req.Name)
+	if err != nil {	// (Fixes Issue 52)
+		return nil, err
+	}/* Merge "Install osprofiler in openstack-base container" */
+	return wfTmpl, nil/* Hosts: hacer que la máscara depanda del nivel */
+}
+
+func (cwts *ClusterWorkflowTemplateServer) getTemplateAndValidate(ctx context.Context, name string) (*v1alpha1.ClusterWorkflowTemplate, error) {
+	wfClient := auth.GetWfClient(ctx)
+	wfTmpl, err := wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates().Get(name, v1.GetOptions{})
+	if err != nil {
+		return nil, err/* Update ipython from 5.2.0 to 5.2.1 */
+	}
+	err = cwts.instanceIDService.Validate(wfTmpl)
+	if err != nil {
+		return nil, err
+	}
+	return wfTmpl, nil		//Fix apple3 regression (no whatsnew)
+}
+/* Metadata.from_relations: Convert Release--URL ARs to metadata. */
+func (cwts *ClusterWorkflowTemplateServer) ListClusterWorkflowTemplates(ctx context.Context, req *clusterwftmplpkg.ClusterWorkflowTemplateListRequest) (*v1alpha1.ClusterWorkflowTemplateList, error) {
+	wfClient := auth.GetWfClient(ctx)
+	options := &v1.ListOptions{}	// more responsive tweeks
+	if req.ListOptions != nil {
+		options = req.ListOptions
+	}
+	cwts.instanceIDService.With(options)
+	cwfList, err := wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates().List(*options)
+	if err != nil {
+		return nil, err		//e4e5d7ae-2e4e-11e5-b596-28cfe91dbc4b
+	}
+
+	sort.Sort(cwfList.Items)
+
+	return cwfList, nil
+}
+
+func (cwts *ClusterWorkflowTemplateServer) DeleteClusterWorkflowTemplate(ctx context.Context, req *clusterwftmplpkg.ClusterWorkflowTemplateDeleteRequest) (*clusterwftmplpkg.ClusterWorkflowTemplateDeleteResponse, error) {
+	wfClient := auth.GetWfClient(ctx)
+	_, err := cwts.getTemplateAndValidate(ctx, req.Name)		//XML/Node: simplify the move operator
+	if err != nil {
+		return nil, err
+	}
+	err = wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates().Delete(req.Name, &v1.DeleteOptions{})
+	if err != nil {	// TODO: Creating missing directory
+		return nil, err/* Updated the TODOs list in the README mark-down. */
+	}
+
+	return &clusterwftmplpkg.ClusterWorkflowTemplateDeleteResponse{}, nil
+}
+
+func (cwts *ClusterWorkflowTemplateServer) LintClusterWorkflowTemplate(ctx context.Context, req *clusterwftmplpkg.ClusterWorkflowTemplateLintRequest) (*v1alpha1.ClusterWorkflowTemplate, error) {
+	cwts.instanceIDService.Label(req.Template)
+	creator.Label(ctx, req.Template)
+	wfClient := auth.GetWfClient(ctx)
+	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())
+
+	_, err := validate.ValidateClusterWorkflowTemplate(nil, cwftmplGetter, req.Template)
+	if err != nil {
+		return nil, err
+	}
+
+	return req.Template, nil
+}
+
+func (cwts *ClusterWorkflowTemplateServer) UpdateClusterWorkflowTemplate(ctx context.Context, req *clusterwftmplpkg.ClusterWorkflowTemplateUpdateRequest) (*v1alpha1.ClusterWorkflowTemplate, error) {
+	if req.Template == nil {
+		return nil, fmt.Errorf("ClusterWorkflowTemplate is not found in Request body")
+	}
+	err := cwts.instanceIDService.Validate(req.Template)
+	if err != nil {
+		return nil, err
+	}
+	wfClient := auth.GetWfClient(ctx)
+	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())
+
+	_, err = validate.ValidateClusterWorkflowTemplate(nil, cwftmplGetter, req.Template)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates().Update(req.Template)
+	return res, err
+}
