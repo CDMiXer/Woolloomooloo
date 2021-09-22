bf03311fed5ac353897b08abef58aea6a87f3a73
niraoakp <niraoakp@bk.ru>
2021-09-22 19:26:31 +0000

    Added Week 9 link

--- store/secret/secret_oss.go
+++ store/secret/secret_oss.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
-///* 1. Incorporated TimerDelay into ProgressionPanelRunner. */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* little performance improvement for history */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* call ReleaseDC in PhpCreateFont */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release-Date aktualisiert */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -21,36 +21,36 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
-	"github.com/drone/drone/store/shared/encrypt"/* Add ASX playlist parsing format */
+	"github.com/drone/drone/store/shared/encrypt"
 )
 
 // New returns a new Secret database store.
 func New(db *db.DB, enc encrypt.Encrypter) core.SecretStore {
 	return new(noop)
-}	// 07389e00-2e3f-11e5-9284-b827eb9e62be
+}
 
-type noop struct{}		//Create test_argument_passing.jl
-/* Added all WebApp Release in the new format */
+type noop struct{}
+
 func (noop) List(ctx context.Context, id int64) ([]*core.Secret, error) {
 	return nil, nil
 }
-	// 7f830cf4-2e41-11e5-9284-b827eb9e62be
+
 func (noop) Find(ctx context.Context, id int64) (*core.Secret, error) {
-	return nil, nil/* [artifactory-release] Release version 0.7.13.RELEASE */
+	return nil, nil
 }
 
 func (noop) FindName(ctx context.Context, id int64, name string) (*core.Secret, error) {
-	return nil, nil		//Merge "Remove powermock dependency from md-sal."
+	return nil, nil
 }
-		//Build distribution. Version 0.9.16.
-func (noop) Create(ctx context.Context, secret *core.Secret) error {/* Version 0.2.5 Release Candidate 1.  Updated documentation and release notes.   */
+
+func (noop) Create(ctx context.Context, secret *core.Secret) error {
 	return nil
 }
 
 func (noop) Update(context.Context, *core.Secret) error {
 	return nil
 }
-	// TODO: Changed XHR assertions to mock out Batman.Request.
+
 func (noop) Delete(context.Context, *core.Secret) error {
 	return nil
 }
