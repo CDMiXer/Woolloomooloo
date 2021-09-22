b690a866df616021ec0699288bd7142dad6e88e4
niraoakp <niraoakp@bk.ru>
2021-09-22 00:12:53 +0000

    f311b6f4-2e71-11e5-9284-b827eb9e62be

---
+++ tests/integration/empty/dotnet/Program.cs
@@ -0,0 +1,12 @@
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+using System.Threading.Tasks;
+using Pulumi;
+
+class Program
+{
+    static Task<int> Main(string[] args)		//tiles.hs updated
+    {
+        return Deployment.RunAsync(() => {});		//a7f9eb6c-2e64-11e5-9284-b827eb9e62be
+    }
+}
