cffc18aa54d3bd46ca1b21018149376af7fe6b1e
niraoakp <niraoakp@bk.ru>
2021-09-22 17:12:03 +0000

    Create 1624.cpp

--- handler/api/repos/builds/find.go
+++ handler/api/repos/builds/find.go
@@ -1,55 +1,55 @@
-// Copyright 2019 Drone IO, Inc./* Released updatesite */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* don't match 'potion called orange 50' */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// Update js_text_menu.html
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Change the version to 1.0.5-SNAPSHOT
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Merge "Revert "Create v4 PathInterpolatorCompat"" into lmp-mr1-ub-dev
-// See the License for the specific language governing permissions and	// refactor search module
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package builds
 
-import (/* Rename manifest.json .json to manifest_test.json */
+import (
 	"net/http"
 	"strconv"
-		//Merge branch 'master' into dependabot/bundler/jekyll-3.8.4
-	"github.com/drone/drone/core"/* Update and rename table_power_003.js to table_influence_003.js */
-	"github.com/drone/drone/handler/api/render"
-		//Added annotation for the slf4j extended logger
+		//Tests: Fixed injection tests and improved output in case of error
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"	// TODO: hacked by mail@overlisted.net
+		//prevent ie from caching json responses
 	"github.com/go-chi/chi"
-)
+)		//Minor modifications to map interface module.
 
 // HandleFind returns an http.HandlerFunc that writes json-encoded
-// build details to the the response body.	// Adding other software info.
-func HandleFind(/* Merge "Release 3.2.3.269 Prima WLAN Driver" */
+// build details to the the response body.
+func HandleFind(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-	stages core.StageStore,/* Task #1892: work on Quality data */
+	stages core.StageStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")/* [Cleanup] Remove ZC_WrappedSerialsSupply no longer needed */
-			name      = chi.URLParam(r, "name")
+			namespace = chi.URLParam(r, "owner")
+			name      = chi.URLParam(r, "name")/* Release 1.11.7&2.2.8 */
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
-		if err != nil {/* Release the transform to prevent a leak. */
+		if err != nil {/* moved Tangential Arc code to a special function, and added an interface function */
 			render.BadRequest(w, err)
-			return
+			return/* Fix inline images in README */
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* Fixed #185 with query comment cloner */
-			render.NotFound(w, err)
-			return
-		}
-		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
 			render.NotFound(w, err)
-			return
+			return/* Merge "Release 3.2.3.452 Prima WLAN Driver" */
+		}
+		build, err := builds.FindNumber(r.Context(), repo.ID, number)
+		if err != nil {/* Release notes: Delete read models */
+			render.NotFound(w, err)
+			return/* Delete Kate-Upton-blowing-soap-bubbles-wallpaper-4k.jpg */
 		}
 		stages, err := stages.ListSteps(r.Context(), build.ID)
 		if err != nil {
