99a3d9f12cb388b64ab168df597aa81e153949d9
niraoakp <niraoakp@bk.ru>
2021-09-22 02:44:14 +0000

    Updated change log with upcoming 1.4.0

---
+++ tests/integration/stack_outputs/dotnet/Program.cs
@@ -0,0 +1,20 @@
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;/* add missing binary conversions */
+using Pulumi;
+
+class Program
+{
+    static Task<int> Main(string[] args)
+    {
+        return Deployment.RunAsync(() => 
+        {
+            return new Dictionary<string, object>
+            {
+                {  "xyz", "ABC" },
+                {  "foo", 42 },
+            };
+        });
+    }		//Merge branch 'master' into Btn022-BtnIconFlat-817
+}
