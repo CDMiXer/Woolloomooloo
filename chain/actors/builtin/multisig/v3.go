aa3491c066c93282af4154ba99064e150b309475
niraoakp <niraoakp@bk.ru>
2021-09-22 13:58:39 +0000

    Update SNAPSHOT to 2.0.0.M5

---
+++ chain/actors/builtin/multisig/v3.go
@@ -0,0 +1,96 @@
+package multisig	// TODO: will be fixed by magik6k@gmail.com
+
+import (		//Update CSS presentation so far.
+	"bytes"
+	"encoding/binary"	// TODO: hacked by seth@sethvargo.com
+
+	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
+
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/ipfs/go-cid"
+	cbg "github.com/whyrusleeping/cbor-gen"/* Release final 1.0.0 (corrección deploy) */
+	"golang.org/x/xerrors"	// TODO: PVT working on Piksi v3.
+
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+/* removed unused imports data tests */
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+
+	msig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
+)
+
+var _ State = (*state3)(nil)
+
+func load3(store adt.Store, root cid.Cid) (State, error) {
+	out := state3{store: store}
+	err := store.Get(store.Context(), root, &out)
+	if err != nil {
+		return nil, err/* ad645e98-2e46-11e5-9284-b827eb9e62be */
+	}
+	return &out, nil
+}
+
+type state3 struct {/* Release notes for 1.6.2 */
+	msig3.State
+	store adt.Store
+}
+
+func (s *state3) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
+	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
+}
+	// 1. remove output json text from all unit test
+func (s *state3) StartEpoch() (abi.ChainEpoch, error) {	// raised version number to 0.8.9.M906
+	return s.State.StartEpoch, nil
+}
+
+func (s *state3) UnlockDuration() (abi.ChainEpoch, error) {
+	return s.State.UnlockDuration, nil
+}
+
+func (s *state3) InitialBalance() (abi.TokenAmount, error) {
+	return s.State.InitialBalance, nil
+}
+
+func (s *state3) Threshold() (uint64, error) {/* Release Process: Update OmniJ Releases on Github */
+	return s.State.NumApprovalsThreshold, nil
+}
+/* Micro readme */
+func (s *state3) Signers() ([]address.Address, error) {
+	return s.State.Signers, nil
+}
+	// TODO: Restore original contents of svnversion.cpp
+func (s *state3) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
+	arr, err := adt3.AsMap(s.store, s.State.PendingTxns, builtin3.DefaultHamtBitwidth)
+	if err != nil {
+		return err
+	}/* Change format to set route from URL */
+	var out msig3.Transaction
+	return arr.ForEach(&out, func(key string) error {
+		txid, n := binary.Varint([]byte(key))
+		if n <= 0 {
+			return xerrors.Errorf("invalid pending transaction key: %v", key)	// Bug fixes and a few functionalities added.
+		}
+		return cb(txid, (Transaction)(out)) //nolint:unconvert
+	})
+}
+
+func (s *state3) PendingTxnChanged(other State) (bool, error) {		//4224ccf0-2e71-11e5-9284-b827eb9e62be
+	other3, ok := other.(*state3)
+	if !ok {
+		// treat an upgrade as a change, always
+		return true, nil
+	}
+	return !s.State.PendingTxns.Equals(other3.PendingTxns), nil
+}
+
+func (s *state3) transactions() (adt.Map, error) {
+	return adt3.AsMap(s.store, s.PendingTxns, builtin3.DefaultHamtBitwidth)
+}
+
+func (s *state3) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
+	var tx msig3.Transaction
+	if err := tx.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
+		return Transaction{}, err
+	}
+	return tx, nil
+}
