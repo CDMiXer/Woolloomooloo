cee025e01ab3be40123f019e505e16725e7f4f94
niraoakp <niraoakp@bk.ru>
2021-09-22 09:52:53 +0000

    Merge branch 'dev' into feat/dynamic-font-loading

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// Update i18n-js to version 3.2.1
+
 // +build !oss
 
-package scheduler		//Reindent, always remove decimal point, even if there's no thousand separator
+package scheduler
