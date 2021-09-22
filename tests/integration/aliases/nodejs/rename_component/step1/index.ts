a09e3549f9c313e939fb410ae743843c02ab9471
niraoakp <niraoakp@bk.ru>
2021-09-22 06:55:39 +0000

    add Grammar>>#startRule

--- tests/integration/aliases/nodejs/rename_component/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step1/index.ts
@@ -2,17 +2,17 @@
 
 import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {/* Release npm package from travis */
+class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }/* Locale for studyperiods in viewstudent */
+    }
 }
 
-// Scenario #3 - rename a component (and all it's children)/* Fix regressions from 0.3.0. Add render RST and render Jinja2. Release 0.4.0. */
+// Scenario #3 - rename a component (and all it's children)
 class ComponentThree extends pulumi.ComponentResource {
-    resource1: Resource;		//Added Persian (fa) (فارسی)
-    resource2: Resource;/* Release areca-5.5.7 */
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// Delete rsz_1rsz_3prof-page.jpg
+    resource1: Resource;
+    resource2: Resource;
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentThree", name, {}, opts);
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
