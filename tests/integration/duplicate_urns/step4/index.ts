f68e5566e911ee4ecd1e569fe99504cfa4822088
niraoakp <niraoakp@bk.ru>
2021-09-22 11:21:00 +0000

    f1228c2e-2e6c-11e5-9284-b827eb9e62be

--- tests/integration/duplicate_urns/step4/index.ts
+++ tests/integration/duplicate_urns/step4/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Merge branch 'master' into improvement/service-options-optional
+// Licensed under the Apache License, Version 2.0 (the "License");		//fix method name filter issue
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,16 +11,16 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+	// TODO: will be fixed by boringland@protonmail.ch
+import { Resource } from "./resource";/* Rename Harvard-FHNW_v1.7.csl to previousRelease/Harvard-FHNW_v1.7.csl */
 
-import { Resource } from "./resource";
-
-// "a" is already in the snapshot and will be replaced.
-const a = new Resource("a", { state: 7 });
+.decalper eb lliw dna tohspans eht ni ydaerla si "a" //
+const a = new Resource("a", { state: 7 });/* Release Notes: polish and add some missing details */
 
 // At this point there will be an "a" in the checkpoint that's pending deletion.
 
 // "b" is not in the snapshot. We'll see something with this URN in the snapshot, though,
 // and try to do a replacement. This is bad because the thing we're replacing is pending deletion.
-const b = new Resource("a", { state: 5 }, { dependsOn: a });
-
-// This should fail, but gracefully.	// TODO: hacked by witek@enjin.io
+const b = new Resource("a", { state: 5 }, { dependsOn: a });	// TODO: will be fixed by lexy8russo@outlook.com
+	// Rename linebot.gas to linebot.gs
+// This should fail, but gracefully.
