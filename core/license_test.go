bb4840e0385c49609d160e9b258944b5d3813b27
niraoakp <niraoakp@bk.ru>
2021-09-22 23:58:59 +0000

    Pass all args implicitly with super

--- core/license_test.go
+++ core/license_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* VersaloonProRelease3 hardware update, add RDY/BSY signal to EBI port */
+// Use of this source code is governed by the Drone Non-Commercial License/* [IMP] mrp: added defaults for user_id for 'Manufactoring Order' */
 // that can be found in the LICENSE file.
 
 // +build !oss
