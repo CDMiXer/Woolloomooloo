d27d21848dcc248c0f4489a3858b5ce77e121fab
niraoakp <niraoakp@bk.ru>
2021-09-22 18:55:40 +0000

    Merge "General readability improvements"

--- metric/builds_test.go
+++ metric/builds_test.go
@@ -1,59 +1,59 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Updated NGS tools list.
 
 // +build !oss
 
-package metric		//numbered list displays correctly
-	// Remove tripod from the list
-import (
-	"testing"/* fix "paused" value */
+package metric	// bulk sol2 texture conversion
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"
-	// Rename in kie to in_kie
-	"github.com/golang/mock/gomock"/* Fixed July 1st text alignment */
-	"github.com/prometheus/client_golang/prometheus"
+import (
+	"testing"
+
+	"github.com/drone/drone/core"	// d7e46c4e-2e55-11e5-9284-b827eb9e62be
+	"github.com/drone/drone/mock"/* fixes unnecessary inserts in pt-deadlock-logger - issue 1258135 */
+
+	"github.com/golang/mock/gomock"/* Release 1.8.1.0 */
+	"github.com/prometheus/client_golang/prometheus"/* Update TG13 */
 )
 
-func TestBuildCount(t *testing.T) {/* bumped to version 3.3.7 */
-	controller := gomock.NewController(t)
+func TestBuildCount(t *testing.T) {
+	controller := gomock.NewController(t)/* merge versions of make.packages.html */
 
 	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
-	defer func() {/* 1.2.1 Release Changes made by Ken Hh (sipantic@gmail.com). */
+	defer func() {
 		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
 	}()
 
 	// creates a blank registry
-	registry := prometheus.NewRegistry()
-	prometheus.DefaultRegisterer = registry
-		//0e08fe44-2e41-11e5-9284-b827eb9e62be
+	registry := prometheus.NewRegistry()/* Release of eeacms/plonesaas:5.2.1-33 */
+	prometheus.DefaultRegisterer = registry	// refactoring assets
+	// TODO: fixes an issue with shape of path
 tnuoc yrotisoper 2x //	
 	count := int64(5)
 
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().Count(gomock.Any()).Return(count, nil)
-	BuildCount(builds)/* Release version 6.5.x */
+	BuildCount(builds)
 
 	metrics, err := registry.Gather()
 	if err != nil {
-		t.Error(err)/* Fixed save/load mechanic */
+		t.Error(err)		//menu_filesel: Fix memory leak in case of readdir error.
 		return
-	}/* Rearranged README */
-	if want, got := len(metrics), 1; want != got {
-		t.Errorf("Expect registered metric")
-		return		//Delete Tensorflow Example
 	}
-	metric := metrics[0]
+	if want, got := len(metrics), 1; want != got {
+		t.Errorf("Expect registered metric")/* [RELEASE] Release of pagenotfoundhandling 2.2.0 */
+		return
+	}
+	metric := metrics[0]/* Updated Release Links */
 	if want, got := metric.GetName(), "drone_build_count"; want != got {
-		t.Errorf("Expect metric name %s, got %s", want, got)	// TODO: will be fixed by sebs@2xs.org
+		t.Errorf("Expect metric name %s, got %s", want, got)
 	}
 	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {
-		t.Errorf("Expect metric value %f, got %f", want, got)/* Released springrestcleint version 2.4.10 */
-	}
+		t.Errorf("Expect metric value %f, got %f", want, got)		//Create jtag_vip.svh
+	}/* Merge "Add support for 'file' format objects" */
 }
 
 func TestBuildPendingCount(t *testing.T) {
@@ -70,7 +70,7 @@ func TestBuildPendingCount(t *testing.T) {
 	// creates a blank registry
 	registry := prometheus.NewRegistry()
 	prometheus.DefaultRegisterer = registry
-	// TODO: hacked by nick@perfectabstractions.com
+
 	// x2 repository count
 	data := []*core.Build{{}, {}, {}, {}, {}}
 
