5e06f5e74f994103dd7ba1bf4454194e23217332
niraoakp <niraoakp@bk.ru>
2021-09-22 23:55:34 +0000

    Update current-price-indicator.js

--- tests/integration/protect_resources/step1/index.ts
+++ tests/integration/protect_resources/step1/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";
-
+import { Resource } from "./resource";/* Release for v0.5.0. */
+	// TODO: Merge "Remove unused 'override.config.template'"
 // Allocate a resource and protect it:
 let a = new Resource("eternal", { state: 1 }, { protect: true });
