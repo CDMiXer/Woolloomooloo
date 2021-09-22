7f8a32a49e89defdae1ebc105bcf950a4799dfb7
niraoakp <niraoakp@bk.ru>
2021-09-22 10:08:42 +0000

    initial checkin

--- tests/integration/dependency_steps/step1/index.ts
+++ tests/integration/dependency_steps/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* Release 0.9 commited to trunk */
 
 // Step 1: Populate our dependency graph.
-const a = new Resource("a", { state: 1 });
+const a = new Resource("a", { state: 1 });		//* fix brew-cask again
 const b = new Resource("b", { state: 2, resource: a });
 // Dependency graph: b -> a
