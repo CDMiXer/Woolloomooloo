76d41d553b55cddcb90b92b24788b2ed62241bce
niraoakp <niraoakp@bk.ru>
2021-09-22 03:09:01 +0000

    a5d996a0-2e5c-11e5-9284-b827eb9e62be

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -2,29 +2,29 @@ package full
 
 import (
 	"testing"
-/* Merge "Release 1.0.0.89 QCACLD WLAN Driver" */
+
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-)		//Fix: Write canonincal path instead of object hash.
+)
 
-func TestMedian(t *testing.T) {/* c742d5da-2e44-11e5-9284-b827eb9e62be */
-	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
+func TestMedian(t *testing.T) {
+	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{/* :sparkles: ajout CVComponent */
 		{big.NewInt(5), build.BlockGasTarget},
 	}, 1))
-		//New version of WPstart - 1.2.1
+	// TODO: will be fixed by steven@stebalien.com
 	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
-		{big.NewInt(10), build.BlockGasTarget},/* disabled buffer overflow checks for Release build */
+		{big.NewInt(10), build.BlockGasTarget},
 	}, 1))
 
-	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
-		{big.NewInt(10), build.BlockGasTarget / 2},	// TODO: Merge "SettingsProvider: dup-suppress from cache."
-		{big.NewInt(20), build.BlockGasTarget / 2},/* [releng] Release v6.16.2 */
-	}, 1))
+	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{/* Release areca-5.5.4 */
+		{big.NewInt(10), build.BlockGasTarget / 2},		//packages/updatedd: use new service functions
+		{big.NewInt(20), build.BlockGasTarget / 2},
+	}, 1))	// Update to Chromium version 25.0.1364.45.
 
 	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
@@ -32,7 +32,7 @@ func TestMedian(t *testing.T) {/* c742d5da-2e44-11e5-9284-b827eb9e62be */
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 1))
 
-	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
+	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{		//Merge "Add ColorMatrix Intrinsic." into jb-mr1-dev
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
