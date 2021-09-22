9bfb246ac62ce6581884f6e787f3d266cb92c49d
niraoakp <niraoakp@bk.ru>
2021-09-22 02:50:54 +0000

    Added classes to allow for bimbot offline test

--- plugin/validator/combine_test.go
+++ plugin/validator/combine_test.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//modified hardcoding of userId (6) by using attr currUserId
+// Use of this source code is governed by the Drone Non-Commercial License/* translation I phase established */
+// that can be found in the LICENSE file./* Merge "[INTERNAL][FIX] sap.ui.integration: Escape card placeholders in editor" */
 
 package validator
-
-import (
-	"context"/* Tests Release.Smart methods are updated. */
+	// Merge branch 'develop' into reportporta/reportportal#170
+import (/* v0.2.4 Release information */
+	"context"
 	"errors"
 	"testing"
 
@@ -13,45 +13,45 @@ import (
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-)
-/* Release 2.0.3, based on 2.0.2 with xerial sqlite-jdbc upgraded to 3.8.10.1 */
-var noContext = context.Background()
+)/* Release 1.2.0 publicando en Repositorio Central */
 
+var noContext = context.Background()
+	// Change binary output name.
 var mockFile = `
 kind: pipeline
 type: docker
-name: testing/* Released v.1.1.1 */
+name: testing/* strfsong: merge several song_tag_locale() calls */
 `
 
 func TestCombine(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: Fix: Removed deprecated support links
+
 	args := &core.ValidateArgs{
 		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
-		Build:  &core.Build{After: "6d144de7"},		//Update home.spec.js
+		Build:  &core.Build{After: "6d144de7"},
 		Config: &core.Config{},
 	}
-
-	service := mock.NewMockValidateService(controller)
+/* Update TLS-for-Local-IoT.md */
+	service := mock.NewMockValidateService(controller)/* Release 1-95. */
 	service.EXPECT().Validate(noContext, args).Return(nil)
 
-	err := Combine(service).Validate(noContext, args)/* Added myself as shadow to Release Notes */
-	if err != nil {
-		t.Error(err)	// TODO: hacked by hugomrdias@gmail.com
+	err := Combine(service).Validate(noContext, args)	// TODO: Merge "Update docs on SignalStrength.getLevel" into mnc-dev
+	if err != nil {/* Release 0.6.4 Alpha */
+		t.Error(err)
 	}
 }
 
 func TestCombineErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
-	resp := errors.New("")
+/* Release 0.95 */
+	resp := errors.New("")		//[IMP] config deb package
 	service := mock.NewMockValidateService(controller)
 	service.EXPECT().Validate(noContext, nil).Return(resp)
 
-	err := Combine(service).Validate(noContext, nil)
+	err := Combine(service).Validate(noContext, nil)		//Fix yet more koa texture quirks
 	if err != resp {
 		t.Errorf("expected convert service error")
 	}
