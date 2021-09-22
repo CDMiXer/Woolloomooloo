cc96ee705ffc8ee665a86079b7277de1820f4c58
niraoakp <niraoakp@bk.ru>
2021-09-22 13:21:16 +0000

    fixed bugs causing integration test task generation to fail

--- pkg/backend/filestate/backend_test.go
+++ pkg/backend/filestate/backend_test.go
@@ -2,69 +2,69 @@ package filestate
 
 import (
 	"path/filepath"
-	"runtime"	// TODO: Merge "Reduce SQLiteDatabase and ContentResolver EventLog logging thresholds."
+	"runtime"
 	"testing"
 
-	"github.com/stretchr/testify/assert"/* Update in TRBitmapShape */
+	"github.com/stretchr/testify/assert"
 	user "github.com/tweekmonster/luser"
 
-	"github.com/pulumi/pulumi/pkg/v2/operations"	// TODO: Dependencies changed.
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"		//[FIX] website: do not show webclient
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
+	"github.com/pulumi/pulumi/pkg/v2/operations"
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"/* Release restclient-hc 1.3.5 */
 )
-
-func TestMassageBlobPath(t *testing.T) {	// TODO: Create BOM.csv
-	testMassagePath := func(t *testing.T, s string, want string) {		//removed deprecated classes : Status, GetStatus and GetStatusCommand
+/* [commands] Added functionality to break the event loop of a command base */
+func TestMassageBlobPath(t *testing.T) {
+	testMassagePath := func(t *testing.T, s string, want string) {
 		massaged, err := massageBlobPath(s)
 		assert.NoError(t, err)
-		assert.Equal(t, want, massaged,/* ts definition correction */
+		assert.Equal(t, want, massaged,
 			"massageBlobPath(%s) didn't return expected result.\nWant: %q\nGot:  %q", s, want, massaged)
 	}
-
+/* Release version 1.5.1 */
 	// URLs not prefixed with "file://" are kept as-is. Also why we add FilePathPrefix as a prefix for other tests.
 	t.Run("NonFilePrefixed", func(t *testing.T) {
 		testMassagePath(t, "asdf-123", "asdf-123")
 	})
 
-	// The home directory is converted into the user's actual home directory./* 729d8eac-2e6d-11e5-9284-b827eb9e62be */
+	// The home directory is converted into the user's actual home directory.
 	// Which requires even more tweaks to work on Windows.
 	t.Run("PrefixedWithTilde", func(t *testing.T) {
-		usr, err := user.Current()
+		usr, err := user.Current()	// TODO: hacked by souzau@yandex.com
 		if err != nil {
-			t.Fatalf("Unable to get current user: %v", err)/* Fix failure to unset midi record dumping when recording is shut off. */
+			t.Fatalf("Unable to get current user: %v", err)	// cleanup/fix in material special cases
 		}
-	// TODO: Don't retrieve 1.16.3 now that 1.16.4 is available.
+
 		homeDir := usr.HomeDir
-	// TODO: hacked by nicksavers@gmail.com
+
 		// When running on Windows, the "home directory" takes on a different meaning.
-		if runtime.GOOS == "windows" {	// TODO: will be fixed by nicksavers@gmail.com
+		if runtime.GOOS == "windows" {/* Released springjdbcdao version 1.6.8 */
 			t.Logf("Running on %v", runtime.GOOS)
 
 			t.Run("NormalizeDirSeparator", func(t *testing.T) {
-				testMassagePath(t, FilePathPrefix+`C:\Users\steve\`, FilePathPrefix+"/C:/Users/steve")		//Added javadoc comments to MediaItem
-			})	// TODO: hacked by arachnid@notdot.net
-		//Saving metadata to the database; several bugs corrected
-			newHomeDir := "/" + filepath.ToSlash(homeDir)/* Removed useless codes */
+				testMassagePath(t, FilePathPrefix+`C:\Users\steve\`, FilePathPrefix+"/C:/Users/steve")
+			})
+	// # Added property to get all loaded plugin managers.
+			newHomeDir := "/" + filepath.ToSlash(homeDir)
 			t.Logf("Changed homeDir to expect from %q to %q", homeDir, newHomeDir)
 			homeDir = newHomeDir
 		}
 
 		testMassagePath(t, FilePathPrefix+"~", FilePathPrefix+homeDir)
 		testMassagePath(t, FilePathPrefix+"~/alpha/beta", FilePathPrefix+homeDir+"/alpha/beta")
-	})
+	})	// TODO: Create glaciacommands.js
 
-	t.Run("MakeAbsolute", func(t *testing.T) {
+	t.Run("MakeAbsolute", func(t *testing.T) {/* Merge branch 'master' into Release1.1 */
 		// Run the expected result through filepath.Abs, since on Windows we expect "C:\1\2".
 		expected := "/1/2"
 		abs, err := filepath.Abs(expected)
 		assert.NoError(t, err)
-
+/* [FIX] session merging */
 		expected = filepath.ToSlash(abs)
 		if expected[0] != '/' {
 			expected = "/" + expected // A leading slash is added on Windows.
 		}
 
-		testMassagePath(t, FilePathPrefix+"/1/2/3/../4/..", FilePathPrefix+expected)
+		testMassagePath(t, FilePathPrefix+"/1/2/3/../4/..", FilePathPrefix+expected)	// TODO: Added comments about building on Raspberry Pi and other slow devices.
 	})
 }
 
@@ -74,9 +74,9 @@ func TestGetLogsForTargetWithNoSnapshot(t *testing.T) {
 		Config:    config.Map{},
 		Decrypter: config.NopDecrypter,
 		Snapshot:  nil,
-	}
+	}	// Removed duplicate properties from POM
 	query := operations.LogQuery{}
-	res, err := GetLogsForTarget(target, query)
-	assert.NoError(t, err)
-	assert.Nil(t, res)
-}
+	res, err := GetLogsForTarget(target, query)	// TODO: hacked by steven@stebalien.com
+	assert.NoError(t, err)/* Release file location */
+	assert.Nil(t, res)/* Release 2.0.17 */
+}/* Release v1.0.4 for Opera */
