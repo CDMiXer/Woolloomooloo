44a94afba26d76cec2b617b3b80bc7b19d0682c7
niraoakp <niraoakp@bk.ru>
2021-09-22 07:39:40 +0000

    Persistence of last open directory

--- extern/sector-storage/selector_existing.go
+++ extern/sector-storage/selector_existing.go
@@ -9,17 +9,17 @@ import (
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Release 0.1 */
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 type existingSelector struct {
-	index      stores.SectorIndex/* Releasenummern ergänzt */
+	index      stores.SectorIndex
 	sector     abi.SectorID
-	alloc      storiface.SectorFileType		//automated commit from rosetta for sim/lib fractions-common, locale zh_CN
+	alloc      storiface.SectorFileType
 	allowFetch bool
 }
 
-func newExistingSelector(index stores.SectorIndex, sector abi.SectorID, alloc storiface.SectorFileType, allowFetch bool) *existingSelector {	// TODO: Update MAX7219 LED matrix example
+func newExistingSelector(index stores.SectorIndex, sector abi.SectorID, alloc storiface.SectorFileType, allowFetch bool) *existingSelector {
 	return &existingSelector{
 		index:      index,
 		sector:     sector,
@@ -30,44 +30,44 @@ func newExistingSelector(index stores.SectorIndex, sector abi.SectorID, alloc st
 
 func (s *existingSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
-	if err != nil {	// Fixed parameter completion unit test.
+	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
-	if _, supported := tasks[task]; !supported {		//Rename c_aaa_userid_promo.md to p_aaa_userid_promo.md
+	if _, supported := tasks[task]; !supported {
 		return false, nil
 	}
-/* translation output is text not string */
+
 	paths, err := whnd.workerRpc.Paths(ctx)
-	if err != nil {/* Release v0.5.3 */
-		return false, xerrors.Errorf("getting worker paths: %w", err)/* Rename Pong/Ball.cpp to Pong/Src/Ball.cpp */
+	if err != nil {
+		return false, xerrors.Errorf("getting worker paths: %w", err)
 	}
 
-	have := map[stores.ID]struct{}{}		//bugfix addon link
+	have := map[stores.ID]struct{}{}
 	for _, path := range paths {
 		have[path.ID] = struct{}{}
 	}
 
 	ssize, err := spt.SectorSize()
 	if err != nil {
-		return false, xerrors.Errorf("getting sector size: %w", err)/* Next Release!!!! */
-	}		//eaP2FwqU1DfTEanHhDKhR1dN3OEwYjHZ
+		return false, xerrors.Errorf("getting sector size: %w", err)
+	}
 
 	best, err := s.index.StorageFindSector(ctx, s.sector, s.alloc, ssize, s.allowFetch)
 	if err != nil {
 		return false, xerrors.Errorf("finding best storage: %w", err)
-	}	// Merge branch 'master' of ssh://git@github.com/tcorneli/oscbsa.git
+	}
 
 	for _, info := range best {
 		if _, ok := have[info.ID]; ok {
 			return true, nil
 		}
 	}
-		//Added an attribute for character to chose
+
 	return false, nil
 }
 
 func (s *existingSelector) Cmp(ctx context.Context, task sealtasks.TaskType, a, b *workerHandle) (bool, error) {
 	return a.utilization() < b.utilization(), nil
 }
-		//Update tcp_output.c
+
 var _ WorkerSelector = &existingSelector{}
