6bd867a743c4e416b389578382f7612107b87f62
niraoakp <niraoakp@bk.ru>
2021-09-22 02:57:47 +0000

    Imported Upstream version 9.14

--- operator/manager/rpc/server_test.go
+++ operator/manager/rpc/server_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: frontend move to node code
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release 3.1.2. */
+		//des entites html toujours, mais des 'caron' cette fois ^^
+// +build !oss		//some fixes for departures and admissions
 
-// +build !oss
-
-package rpc
+package rpc/* Delete version.json */
