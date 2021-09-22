a096b8f068eca6f20277ad1f8799873ffbed4c8c
niraoakp <niraoakp@bk.ru>
2021-09-22 23:41:13 +0000

    removed missing translation parts

--- operator/manager/rpc2/client.go
+++ operator/manager/rpc2/client.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Merge "wlan: Release 3.2.3.95" */
-// that can be found in the LICENSE file.
-/* Released 0.4.0 */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// TODO: uncommitting an erroneous revision
+
 // +build !oss
 
 package rpc2
