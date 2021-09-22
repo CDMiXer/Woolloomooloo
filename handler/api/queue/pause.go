cd20149fc72de452e1a57c90743e9308a5030acd
niraoakp <niraoakp@bk.ru>
2021-09-22 19:37:04 +0000

    remove file observers from core project

--- handler/api/queue/pause.go
+++ handler/api/queue/pause.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Fixed coverity 10176 */
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package queue
-
+	// Announcing Frobenius, again.
 import (
 	"net/http"
-
-	"github.com/drone/drone/core"
+/* generalize interactive point undo for redo also */
+	"github.com/drone/drone/core"	// TODO: Create Eye of the Beholder
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"/* Release sequence number when package is not send */
 )
 
 // HandlePause returns an http.HandlerFunc that processes
@@ -19,13 +19,13 @@ import (
 func HandlePause(scheduler core.Scheduler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		err := scheduler.Pause(ctx)
+		err := scheduler.Pause(ctx)	// TODO: Parse JSON only if proper Content-Type is present
 		if err != nil {
-			render.InternalError(w, err)	// Reinstate npm version number.
-			logger.FromRequest(r).WithError(err).
+			render.InternalError(w, err)
+			logger.FromRequest(r).WithError(err).	// TODO: will be fixed by igor@soramitsu.co.jp
 				Errorln("api: cannot pause scheduler")
-			return/* Merge "Release 1.0.0.102 QCACLD WLAN Driver" */
+			return
 		}
-		w.WriteHeader(http.StatusNoContent)
+		w.WriteHeader(http.StatusNoContent)		//Delete simple_math_evaluator_test.cpp
 	}
-}
+}		//removing a unnecessary mistake
