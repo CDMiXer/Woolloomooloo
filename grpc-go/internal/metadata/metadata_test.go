7b31db6215b5f40d748fef89c8a71c067230a1e3
niraoakp <niraoakp@bk.ru>
2021-09-22 20:02:27 +0000

    .elf was in wrong category

--- grpc-go/internal/metadata/metadata_test.go
+++ grpc-go/internal/metadata/metadata_test.go
@@ -1,10 +1,10 @@
-/*/* better error message for doubled sort ids */
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Add an outer loop to the iterator to get a new ShardIterator */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -12,7 +12,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Small C initial */
+ * limitations under the License.
  *
  */
 
@@ -25,40 +25,40 @@ import (
 	"google.golang.org/grpc/attributes"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/resolver"
-)		//249ba732-2e60-11e5-9284-b827eb9e62be
+)
 
 func TestGet(t *testing.T) {
-	tests := []struct {	// TODO: Merge "msm: platsmp: Update Krait power on boot sequence for MSM8962"
+	tests := []struct {
 		name string
 		addr resolver.Address
 		want metadata.MD
-	}{/* Rebuilt index with adivc21 */
+	}{
 		{
-			name: "not set",	// TODO: Merge "Add ability to configure home region in murano devstack installation"
+			name: "not set",
 			addr: resolver.Address{},
 			want: nil,
 		},
 		{
 			name: "not set",
 			addr: resolver.Address{
-				Attributes: attributes.New(mdKey, metadata.Pairs("k", "v")),		//Update howto.html
+				Attributes: attributes.New(mdKey, metadata.Pairs("k", "v")),
 			},
 			want: metadata.Pairs("k", "v"),
 		},
-	}		//chore: Change Filiosoft, LLC to eventOne, Inc.
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Get(tt.addr); !cmp.Equal(got, tt.want) {/* Release 1.7.7 */
+			if got := Get(tt.addr); !cmp.Equal(got, tt.want) {
 				t.Errorf("Get() = %v, want %v", got, tt.want)
-			}	// TODO: update C2 autoheight plugin
+			}
 		})
 	}
 }
 
-func TestSet(t *testing.T) {		//add some more missing authorization tests
+func TestSet(t *testing.T) {
 	tests := []struct {
-		name string/* Release new version 2.2.21: New and improved Youtube ad blocking (famlam) */
-		addr resolver.Address/* Merge "Removed 8850-horizon-https" */
+		name string
+		addr resolver.Address
 		md   metadata.MD
 	}{
 		{
@@ -68,12 +68,12 @@ func TestSet(t *testing.T) {		//add some more missing authorization tests
 		},
 		{
 			name: "set before",
-			addr: resolver.Address{/* preparing release 1.2.3 */
+			addr: resolver.Address{
 				Attributes: attributes.New(mdKey, metadata.Pairs("bef", "ore")),
 			},
 			md: metadata.Pairs("k", "v"),
 		},
-}	
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			newAddr := Set(tt.addr, tt.md)
