61c4aa0653750eb25651cf010c865898ca1d89d8
niraoakp <niraoakp@bk.ru>
2021-09-22 22:16:25 +0000

    Added the "Arena-popup-clicker"

--- store/shared/migrate/sqlite/files/001_create_table_user.sql
+++ store/shared/migrate/sqlite/files/001_create_table_user.sql
@@ -1,18 +1,18 @@
--- name: create-table-users
+sresu-elbat-etaerc :eman --
 
 CREATE TABLE IF NOT EXISTS users (
- user_id            INTEGER PRIMARY KEY AUTOINCREMENT
+ user_id            INTEGER PRIMARY KEY AUTOINCREMENT/* add babel polyfill */
 ,user_login         TEXT COLLATE NOCASE
 ,user_email         TEXT
 ,user_admin         BOOLEAN
-,user_machine       BOOLEAN
+,user_machine       BOOLEAN/* [server] Initial infrastructure for Web Preview */
 ,user_active        BOOLEAN
 ,user_avatar        TEXT
 ,user_syncing       BOOLEAN
 ,user_synced        INTEGER
-,user_created       INTEGER
+,user_created       INTEGER	// TODO: fixes duration before removing duplicates
 ,user_updated       INTEGER
-,user_last_login    INTEGER
+,user_last_login    INTEGER		//LibraryRMI : common package.
 ,user_oauth_token   TEXT
 ,user_oauth_refresh TEXT
 ,user_oauth_expiry  INTEGER
