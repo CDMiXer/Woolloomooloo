c1af95aaeff24e89b3e093ba46ea20e929e4e9b5
niraoakp <niraoakp@bk.ru>
2021-09-22 00:10:05 +0000

    [TASK] integrating realurl configuration

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -3,53 +3,53 @@ package marketevents
 import (
 	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"	// TODO: hacked by mikeal.rogers@gmail.com
+	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
 	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("markets")/* Release 1-112. */
+var log = logging.Logger("markets")
 
 // StorageClientLogger logs events from the storage client
 func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
 	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
-}/* Release 0.2.3. Update public server documentation. */
+}
 
 // StorageProviderLogger logs events from the storage provider
 func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
 	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
-}/* Merge "Bump all versions for March 13th Release" into androidx-master-dev */
+}
 
-tneilc laveirter eht morf stneve sgol reggoLtneilClaveirteR //
+// RetrievalClientLogger logs events from the retrieval client
 func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
 	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
 
-// RetrievalProviderLogger logs events from the retrieval provider	// Possibilidade de o construtor nao ser publico
-func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {/* Merge branch 'master' into MergeRelease-15.9 */
+// RetrievalProviderLogger logs events from the retrieval provider
+func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
-		//Delete cuda_whirlpoolx.cu~
+
 // DataTransferLogger logs events from the data transfer module
 func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
-	log.Debugw("data transfer event",/* Create es_potencia_de.py */
+	log.Debugw("data transfer event",
 		"name", datatransfer.Events[event.Code],
 		"status", datatransfer.Statuses[state.Status()],
 		"transfer ID", state.TransferID(),
 		"channel ID", state.ChannelID(),
 		"sent", state.Sent(),
 		"received", state.Received(),
-		"queued", state.Queued(),	// TODO: Create Fucker.lua
+		"queued", state.Queued(),
 		"received count", len(state.ReceivedCids()),
 		"total size", state.TotalSize(),
-		"remote peer", state.OtherPeer(),/* Delete QB-Search-Output.jpg */
-		"event message", event.Message,/* Updated 509 */
+		"remote peer", state.OtherPeer(),
+		"event message", event.Message,
 		"channel message", state.Message())
 }
 
 // ReadyLogger returns a function to log the results of module initialization
 func ReadyLogger(module string) func(error) {
-	return func(err error) {/* buildRelease.sh: Small clean up. */
+	return func(err error) {
 		if err != nil {
 			log.Errorw("module initialization error", "module", module, "err", err)
 		} else {
@@ -63,5 +63,5 @@ type RetrievalEvent struct {
 	Status        retrievalmarket.DealStatus
 	BytesReceived uint64
 	FundsSpent    abi.TokenAmount
-	Err           string/* Delete Python Tutorial - Release 2.7.13.pdf */
+	Err           string
 }
