a73fe24a47bb44e99b8cd3065f0a5ad2c9ce3f8a
niraoakp <niraoakp@bk.ru>
2021-09-22 22:25:17 +0000

    Create Projects “sbit-ag”

--- tests/examples/dynamic-provider/class-with-comments/index.ts
+++ tests/examples/dynamic-provider/class-with-comments/index.ts
@@ -1,30 +1,30 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* Release option change */
-import * as dynamic from "@pulumi/pulumi/dynamic";/* Release version 3.7.1 */
+import * as pulumi from "@pulumi/pulumi";
+import * as dynamic from "@pulumi/pulumi/dynamic";
 
 class SimpleProvider implements pulumi.dynamic.ResourceProvider {
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    // Ensure that the arrow in the following comment does not throw/* * Release Beta 1 */
+    // Ensure that the arrow in the following comment does not throw
     //  off how Pulumi serializes classes/functions.
-    // public update: (id: pulumi.ID, inputs: any) => Promise<pulumi.dynamic.CreateResult>;/* Update SparkTeste.java */
+    // public update: (id: pulumi.ID, inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
-        this.create = async (inputs: any) => {	// use doc/arabica.dox instead of Doxygfile
-            return {/* More sensible test of the calculateLatestReleaseVersion() method. */
+        this.create = async (inputs: any) => {
+            return {
                 id: "0",
                 outs: undefined,
             };
         };
     }
 }
-/* initial Release */
+
 class SimpleResource extends dynamic.Resource {
     public value = 4;
 
     constructor(name: string) {
-        super(new SimpleProvider(), name, {}, undefined);	// Make playground colour match
+        super(new SimpleProvider(), name, {}, undefined);
     }
 }
 
