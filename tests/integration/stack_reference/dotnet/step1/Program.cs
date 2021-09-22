ce2f5f05859add21616b5e8b99283a79a6d715b8
niraoakp <niraoakp@bk.ru>
2021-09-22 19:55:12 +0000

    Log data set parameters, skip DWOF with k=1, better messages.

--- tests/integration/stack_reference/dotnet/step1/Program.cs
+++ tests/integration/stack_reference/dotnet/step1/Program.cs
@@ -1,27 +1,27 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System;		//added parameters section to readme
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
-/* Delete jquery-2.2.1.js */
-class Program/* Added _ as a valid character for gtm_auth */
+
+class Program
 {
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(async () =>
         {
             var config = new Config();
-            var org = config.Require("org");	// TODO: Delete Mini_Project_Naive_Bayes-Answers.ipynb
+            var org = config.Require("org");
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
             var a = new StackReference(slug);
 
-            var oldVal = (string[])await a.GetValueAsync("val");	// TODO: will be fixed by davidad@alum.mit.edu
+            var oldVal = (string[])await a.GetValueAsync("val");
             if (oldVal.Length != 2 || oldVal[0] != "a" || oldVal[1] != "b")
-{            
+            {
                 throw new Exception("Invalid result");
-}            
-	// TODO: Merge branch 'master' into viewer-changes-new
+            }
+
             return new Dictionary<string, object>
             {
                 { "val2", Output.CreateSecret(new[] { "a", "b" }) }
