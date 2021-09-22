934f7351aec7d6b3c6ff47c401c4286932045732
niraoakp <niraoakp@bk.ru>
2021-09-22 05:49:37 +0000

    Fix for projects without active support. Removed debug info

---
+++ plugin/secret/combine_test.go
@@ -0,0 +1,5 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* Rename Excersice-1/DumpVariable.php to Exercise-1/DumpVariable.php */
+// that can be found in the LICENSE file.
+
+package secret
