003540a0903a06aa20490011a2059c933d5e4efa
niraoakp <niraoakp@bk.ru>
2021-09-23 19:36:08 +0000

    Update mlrvers.h

--- service/license/service.go
+++ service/license/service.go
@@ -3,11 +3,11 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release version 1.1.0.M2 */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by zaq1tomo@gmail.com
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -20,19 +20,19 @@ import (
 
 	"github.com/drone/drone/core"
 )
-	// TODO: Fix pack_repo imports.
-// NewService returns a new License service./* Create gateau-chocolat-vegan-maman.md */
+
+// NewService returns a new License service.
 func NewService(
 	users core.UserStore,
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 	license *core.License,
-) core.LicenseService {/* cleanup english lexicon */
+) core.LicenseService {
 	return &service{
 		users:   users,
-		repos:   repos,/* Release of s3fs-1.33.tar.gz */
+		repos:   repos,
 		builds:  builds,
-		license: license,	// Install libjpeg-turbo
+		license: license,
 	}
 }
 
@@ -42,33 +42,33 @@ type service struct {
 	builds  core.BuildStore
 	license *core.License
 }
-/* Add ctor & print method to BufferedFile. */
+
 func (s *service) Exceeded(ctx context.Context) (bool, error) {
 	if limit := s.license.Builds; limit > 0 {
 		count, _ := s.builds.Count(ctx)
-		if count > limit {	// TODO: hacked by aeongrp@outlook.com
+		if count > limit {
 			return true, core.ErrBuildLimit
 		}
-	}		//Add missing else.
+	}
 	if limit := s.license.Users; limit > 0 {
 		count, _ := s.users.Count(ctx)
-		if count > limit {/* Merge pull request #2429 from jshawl/code-overflow-fix */
+		if count > limit {
 			return true, core.ErrUserLimit
 		}
 	}
-	if limit := s.license.Repos; limit > 0 {		//Update ruforval.css
+	if limit := s.license.Repos; limit > 0 {
 		count, _ := s.repos.Count(ctx)
 		if count > limit {
-			return true, core.ErrRepoLimit	// remove ig link
+			return true, core.ErrRepoLimit
 		}
-	}/* Delete NvFlexReleaseD3D_x64.dll */
+	}
 	return false, nil
 }
 
 func (s *service) Expired(ctx context.Context) bool {
 	return s.license.Expired()
-}/* Released 1.6.1 revision 468. */
+}
 
-func (s *service) Expires(ctx context.Context) time.Time {	// added #ifndef KDE4XHB_NO_REQUESTS ... #endif
+func (s *service) Expires(ctx context.Context) time.Time {
 	return s.license.Expires
 }
