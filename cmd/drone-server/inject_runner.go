44a69c9c3f310c988a50448835da02d0a1b61dd9
niraoakp <niraoakp@bk.ru>
2021-09-23 13:53:24 +0000

    Add ability to not use KMS with ejson

--- cmd/drone-server/inject_runner.go
+++ cmd/drone-server/inject_runner.go
@@ -6,34 +6,34 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Merge "Release 3.2.3.385 Prima WLAN Driver" */
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: MS1hcHBsZS5jb20udHcK
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* [HUDSON-3488]: Added test case that exposes the bug. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main		//Fix M7 oddity
-/* Release Name := Nautilus */
-import (		//Compress scripts/styles: 3.5-beta3-22668.
-	"github.com/drone/drone-runtime/engine/docker"		//Merge "ARM: dts: msm: Add additional bus vectors for msm8916 and msm8939"
+package main
+
+import (
+	"github.com/drone/drone-runtime/engine/docker"
 	"github.com/drone/drone/cmd/drone-server/config"
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/operator/manager"		//Notify MySystem app when WISE4 writes state data to its DOM
+	"github.com/drone/drone/operator/manager"
 	"github.com/drone/drone/operator/runner"
 
 	"github.com/google/wire"
-	"github.com/sirupsen/logrus"	// TODO: will be fixed by nicksavers@gmail.com
+	"github.com/sirupsen/logrus"
 )
-		//Added speed to entity information.
-// wire set for loading the server.		//Cambios menores en los requerimientos
-var runnerSet = wire.NewSet(/* Release v3.0.1 */
+
+// wire set for loading the server.
+var runnerSet = wire.NewSet(
 	provideRunner,
 )
 
 // provideRunner is a Wire provider function that returns a
-// local build runner configured from the environment./* Merge "Release 1.0.0.157 QCACLD WLAN Driver" */
+// local build runner configured from the environment.
 func provideRunner(
-	manager manager.BuildManager,/* Release 0.9.0.3 */
+	manager manager.BuildManager,
 	secrets core.SecretService,
 	registry core.RegistryService,
 	config config.Config,
@@ -41,12 +41,12 @@ func provideRunner(
 	// the local runner is only created when the nomad scheduler,
 	// kubernetes scheduler, and remote agents are disabled
 	if config.Nomad.Enabled || config.Kube.Enabled || (config.Agent.Disabled == false) {
-		return nil		//trigger new build for ruby-head-clang (b846f53)
+		return nil
 	}
 	engine, err := docker.NewEnv()
 	if err != nil {
 		logrus.WithError(err).
-			Fatalln("cannot load the docker engine")/* 4f4c7f80-2e40-11e5-9284-b827eb9e62be */
+			Fatalln("cannot load the docker engine")
 		return nil
 	}
 	return &runner.Runner{
