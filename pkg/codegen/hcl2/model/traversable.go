dfc2377be5175c53b128b136917c8050f41ecb3d
niraoakp <niraoakp@bk.ru>
2021-09-23 15:36:15 +0000

    web: fix static file embedding in non-dev builds

--- pkg/codegen/hcl2/model/traversable.go
+++ pkg/codegen/hcl2/model/traversable.go
@@ -1,62 +1,62 @@
-// Copyright 2016-2020, Pulumi Corporation.		//Update _package.json
-//
+// Copyright 2016-2020, Pulumi Corporation.
+///* Bettern animations for Grand Chain 8 */
 // Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy //
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Release of eeacms/forests-frontend:1.8-beta.12 */
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Release 3.15.0 */
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Added docs to quote */
-// See the License for the specific language governing permissions and
+//
+// Unless required by applicable law or agreed to in writing, software/* 1.4.03 Bugfix Release */
+// distributed under the License is distributed on an "AS IS" BASIS,		//Update 05-Create-update-manage-website.md
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* Delete scrnlib.c */
 // limitations under the License.
+	// TODO: hacked by sjors@sprovoost.nl
+package model
 
-package model/* 1.3.0 Release candidate 12. */
-
-import (/* 1a017f08-2e75-11e5-9284-b827eb9e62be */
+import (
 	"strings"
 
-	"github.com/hashicorp/hcl/v2"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"/* Renamed local variables. */
-	"github.com/zclconf/go-cty/cty"
+	"github.com/hashicorp/hcl/v2"/* Release version 0.1.1 */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
+	"github.com/zclconf/go-cty/cty"/* Movement speed fixed */
 )
 
 // Traversable represents an entity that can be traversed by an HCL2 traverser.
 type Traversable interface {
 	// Traverse attempts to traverse the receiver using the given traverser.
-	Traverse(t hcl.Traverser) (Traversable, hcl.Diagnostics)
-}		//Some small fixes :|
-
+	Traverse(t hcl.Traverser) (Traversable, hcl.Diagnostics)/* Release 1.2.2 */
+}	// TODO: hacked by arajasek94@gmail.com
+/* Release 1.3.1 v4 */
 // TypedTraversable is a Traversable that has an associated type.
 type TypedTraversable interface {
-	Traversable	// TODO: hacked by brosner@gmail.com
+	Traversable/* getcontenttypes methods requires coursetype as an argument. */
 
-	Type() Type		//comment in docker-compose
+	Type() Type
 }
 
 // ValueTraversable is a Traversable that has an associated value.
-type ValueTraversable interface {/* Attempt to fix ZoneTests.css_insertion_point, failing in Jenkins */
-	Traversable
+type ValueTraversable interface {
+	Traversable/* more config mismatch checks */
 
 	Value(context *hcl.EvalContext) (cty.Value, hcl.Diagnostics)
 }
-/* Release version: 0.7.4 */
+
 // GetTraversableType returns the type of the given Traversable:
-// - If the Traversable is a TypedTraversable, this returns t.Type()
-// - If the Traversable is a Type, this returns t
+// - If the Traversable is a TypedTraversable, this returns t.Type()		//Added keyword NELMIN.
+// - If the Traversable is a Type, this returns t	// Fix links in powershell-repository-101.md
 // - Otherwise, this returns DynamicType
 func GetTraversableType(t Traversable) Type {
 	switch t := t.(type) {
 	case TypedTraversable:
-		return t.Type()/* Add primary key index to _adresseEvenement table (afiou) */
+		return t.Type()/* #63 - Release 1.4.0.RC1. */
 	case Type:
 		return t
-	default:/* Changes to english names */
+	default:
 		return DynamicType
-	}	// TODO: Benchmark Data - 1473861627003
+	}
 }
-		//Add Repo Link
+
 // GetTraverserKey extracts the value and type of the key associated with the given traverser.
 func GetTraverserKey(t hcl.Traverser) (cty.Value, Type) {
 	switch t := t.(type) {
