6a43993ac32ac73b8847b85efe9764d9175fd838
niraoakp <niraoakp@bk.ru>
2021-09-23 16:40:47 +0000

    X# DataMember.Value (get only, set coming soon)

--- tests/integration/large_resource/dotnet/Program.cs
+++ tests/integration/large_resource/dotnet/Program.cs
@@ -3,14 +3,14 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System;
-using Pulumi;
-
+using Pulumi;/* Release on 16/4/17 */
+		//Merge "Run full multinode tests against new dib images"
 class Program
-{
+{	// TODO: improve main navigation screenreader behaviour
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
-        {
+        {/* fixed lint issues */
             // Create and export a very long string (>4mb)
             return new Dictionary<string, object>
             {
