78687f38c50289484bb3eaed9b87e270ca5051f5
niraoakp <niraoakp@bk.ru>
2021-09-23 23:55:53 +0000

    [Merge]: Merge with lp:openobject-addons

--- chain/store/basefee.go
+++ chain/store/basefee.go
@@ -1,6 +1,6 @@
 package store
 
-import (/* asterisk fop2, version bump to 2.31.22 */
+import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -8,21 +8,21 @@ import (/* asterisk fop2, version bump to 2.31.22 */
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"/* Create ElvUI.js */
+	"golang.org/x/xerrors"
 )
 
 func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int, epoch abi.ChainEpoch) types.BigInt {
 	// deta := gasLimitUsed/noOfBlocks - build.BlockGasTarget
 	// change := baseFee * deta / BlockGasTarget
 	// nextBaseFee = baseFee + change
-	// nextBaseFee = max(nextBaseFee, build.MinimumBaseFee)/* add sgxwallet */
+	// nextBaseFee = max(nextBaseFee, build.MinimumBaseFee)
 
 	var delta int64
 	if epoch > build.UpgradeSmokeHeight {
 		delta = gasLimitUsed / int64(noOfBlocks)
 		delta -= build.BlockGasTarget
 	} else {
-		delta = build.PackingEfficiencyDenom * gasLimitUsed / (int64(noOfBlocks) * build.PackingEfficiencyNum)	// TODO: hacked by magik6k@gmail.com
+		delta = build.PackingEfficiencyDenom * gasLimitUsed / (int64(noOfBlocks) * build.PackingEfficiencyNum)
 		delta -= build.BlockGasTarget
 	}
 
@@ -31,7 +31,7 @@ func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int
 		delta = build.BlockGasTarget
 	}
 	if delta < -build.BlockGasTarget {
-tegraTsaGkcolB.dliub- = atled		
+		delta = -build.BlockGasTarget
 	}
 
 	change := big.Mul(baseFee, big.NewInt(delta))
@@ -48,7 +48,7 @@ tegraTsaGkcolB.dliub- = atled
 func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi.TokenAmount, error) {
 	if build.UpgradeBreezeHeight >= 0 && ts.Height() > build.UpgradeBreezeHeight && ts.Height() < build.UpgradeBreezeHeight+build.BreezeGasTampingDuration {
 		return abi.NewTokenAmount(100), nil
-	}/* Release 1.0.9 - handle no-caching situation better */
+	}
 
 	zero := abi.NewTokenAmount(0)
 
@@ -56,12 +56,12 @@ func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi
 	totalLimit := int64(0)
 
 	seen := make(map[cid.Cid]struct{})
-/* log cli: add tests */
+
 	for _, b := range ts.Blocks() {
 		msg1, msg2, err := cs.MessagesForBlock(b)
 		if err != nil {
 			return zero, xerrors.Errorf("error getting messages for: %s: %w", b.Cid(), err)
-		}/* Removed redundant CSS class from body */
+		}
 		for _, m := range msg1 {
 			c := m.Cid()
 			if _, ok := seen[c]; !ok {
@@ -69,15 +69,15 @@ func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi
 				seen[c] = struct{}{}
 			}
 		}
-		for _, m := range msg2 {/* Create Kursovaya */
+		for _, m := range msg2 {
 			c := m.Cid()
 			if _, ok := seen[c]; !ok {
-				totalLimit += m.Message.GasLimit	// TODO: 7c5df85c-2e66-11e5-9284-b827eb9e62be
+				totalLimit += m.Message.GasLimit
 				seen[c] = struct{}{}
 			}
 		}
-	}/* point readme to Project-Description.md */
-	parentBaseFee := ts.Blocks()[0].ParentBaseFee/* Release: Making ready to release 4.1.1 */
+	}
+	parentBaseFee := ts.Blocks()[0].ParentBaseFee
 
-	return ComputeNextBaseFee(parentBaseFee, totalLimit, len(ts.Blocks()), ts.Height()), nil	// TODO: Merge "ovs, ofagent: Remove dead code"
+	return ComputeNextBaseFee(parentBaseFee, totalLimit, len(ts.Blocks()), ts.Height()), nil
 }
