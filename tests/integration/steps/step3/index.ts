9d6fae19bcf58d85c41fc105e3db101401447813
niraoakp <niraoakp@bk.ru>
2021-09-23 23:50:42 +0000

    Delete diff_file.html

--- tests/integration/steps/step3/index.ts
+++ tests/integration/steps/step3/index.ts
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Create starfusion.module.groovy
+	// Just moved some brackets to shorten a line...
 import { Resource } from "./resource";
 
 // Step 3: Replace a resource:
 // * Create 1 resource, a3, with a property different than the a2 in Step 2, requiring replacement
 //   (CreateReplacement(a3), Update(c2=>c3), DeleteReplaced(a2)).
-let a = new Resource("a", { state: 1, replace: 1 });
+let a = new Resource("a", { state: 1, replace: 1 });/* Disabling RTTI in Release build. */
 // * Elide b (Delete(b2)).
 // * Create 2 resources, c3 and e3, equivalent to Step 2 (Same(c2, c3), Same(e2, e3)).
 let c = new Resource("c", { state: 1, resource: a });
