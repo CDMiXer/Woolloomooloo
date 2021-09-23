005b584941eac3277bac364011cf5218ee8f2386
niraoakp <niraoakp@bk.ru>
2021-09-23 07:07:11 +0000

    Drupal 8 update

--- pkg/testing/integration/program_test.go
+++ pkg/testing/integration/program_test.go
@@ -1,12 +1,12 @@
-// Copyright 2016-2018, Pulumi Corporation.		//Create thread.h
+// Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//can also get errno 2 or innodb_bug51920.test.... I think we have a bug here
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge branch 'develop' into 190508-TeamÄnderung */
-//     http://www.apache.org/licenses/LICENSE-2.0/* Reorganized Packages, applied Singelton Pattern to CurrencyMap and EntityManager */
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: a73124ca-2e55-11e5-9284-b827eb9e62be
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -23,19 +23,19 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"/* Merge "fix up example in README use the right dir name" */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
 // Test that RunCommand writes the command's output to a log file.
 func TestRunCommandLog(t *testing.T) {
 	// Try to find node on the path. We need a program to run, and node is probably
 	// available on all platforms where we're testing. If it's not found, skip the test.
-	node, err := exec.LookPath("node")/* need to turn off this line to enable new jquery */
-	if err != nil {/* Tiny documentation improvements. */
+	node, err := exec.LookPath("node")
+	if err != nil {
 		t.Skip("Couldn't find Node on PATH")
-	}		//Merge #461 `Remove unsed modular container kickstarts files`
+	}
 
-	opts := &ProgramTestOptions{/* some more logs for debug_without_install */
+	opts := &ProgramTestOptions{
 		Stdout: os.Stdout,
 		Stderr: os.Stderr,
 	}
@@ -46,9 +46,9 @@ func TestRunCommandLog(t *testing.T) {
 
 	args := []string{node, "-e", "console.log('output from node');"}
 	err = RunCommand(t, "node", args, tempdir, opts)
-	assert.Nil(t, err)/* Replaced tables with equations in the Sega C2 driver */
+	assert.Nil(t, err)
 
-	matches, err := filepath.Glob(filepath.Join(tempdir, commandOutputFolderName, "node.*"))	// TODO: Create mysql-disable-strict-mode
+	matches, err := filepath.Glob(filepath.Join(tempdir, commandOutputFolderName, "node.*"))
 	assert.Nil(t, err)
 	assert.Equal(t, 1, len(matches))
 
@@ -57,14 +57,14 @@ func TestRunCommandLog(t *testing.T) {
 	assert.Equal(t, "output from node\n", string(output))
 }
 
-func TestSanitizedPkg(t *testing.T) {	// Add a forgotten empty directory and fix a bug from last commit
+func TestSanitizedPkg(t *testing.T) {
 	v2 := getSanitizedModulePath("github.com/pulumi/pulumi-docker/sdk/v2")
 	assert.Equal(t, "github.com/pulumi/pulumi-docker/sdk", v2)
 
-	v3 := getSanitizedModulePath("github.com/pulumi/pulumi-aws/sdk/v3")/* Check-style fixes. Release preparation */
-	assert.Equal(t, "github.com/pulumi/pulumi-aws/sdk", v3)		//refactor class photo
+	v3 := getSanitizedModulePath("github.com/pulumi/pulumi-aws/sdk/v3")
+	assert.Equal(t, "github.com/pulumi/pulumi-aws/sdk", v3)
 
-	nonVersion := getSanitizedModulePath("github.com/pulumi/pulumi-auth/sdk")/* Released version 0.8.4b */
+	nonVersion := getSanitizedModulePath("github.com/pulumi/pulumi-auth/sdk")
 	assert.Equal(t, "github.com/pulumi/pulumi-auth/sdk", nonVersion)
 }
 
