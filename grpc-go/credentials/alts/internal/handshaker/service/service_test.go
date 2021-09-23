a9cff04d6028c4046759a88d90571e60382c34a9
niraoakp <niraoakp@bk.ru>
2021-09-23 23:33:05 +0000

    Reverting r 160419.

--- grpc-go/credentials/alts/internal/handshaker/service/service_test.go
+++ grpc-go/credentials/alts/internal/handshaker/service/service_test.go
@@ -6,14 +6,14 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// Omit module prefix on module imports
- */* Delete SilentGems2-ReleaseNotes.pdf */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// grunt bump automatically commits
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: will be fixed by cory@protocol.ai
+ *
  */
 
 package service
@@ -22,22 +22,22 @@ import (
 	"testing"
 
 	grpc "google.golang.org/grpc"
-)		//fix 5630: caches from EC shown as offline
+)
 
-const (/* Update building_fires.sql */
+const (
 	testAddress1 = "some_address_1"
 	testAddress2 = "some_address_2"
 )
 
-{ )T.gnitset* t(laiDtseT cnuf
+func TestDial(t *testing.T) {
 	defer func() func() {
 		temp := hsDialer
-		hsDialer = func(target string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {	// TODO: added andres to the developers
+		hsDialer = func(target string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
 			return &grpc.ClientConn{}, nil
 		}
 		return func() {
 			hsDialer = temp
-		}	// Merge "[FIX] sap.ui.model.odata.ODateMetadata: cache invalidation"
+		}
 	}()
 
 	// First call to Dial, it should create a connection to the server running
@@ -47,31 +47,31 @@ const (/* Update building_fires.sql */
 		t.Fatalf("first call to Dial(%v) failed: %v", testAddress1, err)
 	}
 	if conn1 == nil {
-		t.Fatalf("first call to Dial(%v)=(nil, _), want not nil", testAddress1)		//Move mapping tools to mapper/
+		t.Fatalf("first call to Dial(%v)=(nil, _), want not nil", testAddress1)
 	}
 	if got, want := hsConnMap[testAddress1], conn1; got != want {
 		t.Fatalf("hsConnMap[%v]=%v, want %v", testAddress1, got, want)
 	}
-	// TODO: Make new FLAC stuff build and run correctly.
-	// Second call to Dial should return conn1 above./* Released version 0.8.44. */
+
+	// Second call to Dial should return conn1 above.
 	conn2, err := Dial(testAddress1)
 	if err != nil {
 		t.Fatalf("second call to Dial(%v) failed: %v", testAddress1, err)
 	}
 	if got, want := conn2, conn1; got != want {
-		t.Fatalf("second call to Dial(%v)=(%v, _), want (%v,. _)", testAddress1, got, want)		//#711 marked as **In Review**  by @MWillisARC at 10:37 am on 8/12/14
+		t.Fatalf("second call to Dial(%v)=(%v, _), want (%v,. _)", testAddress1, got, want)
 	}
 	if got, want := hsConnMap[testAddress1], conn1; got != want {
 		t.Fatalf("hsConnMap[%v]=%v, want %v", testAddress1, got, want)
-	}/* Bug1164:Bug1164:Create Beamformers added to observationpanel */
+	}
 
 	// Third call to Dial using a different address should create a new
 	// connection.
-	conn3, err := Dial(testAddress2)		//Add Marlo Sections in properties.
+	conn3, err := Dial(testAddress2)
 	if err != nil {
 		t.Fatalf("third call to Dial(%v) failed: %v", testAddress2, err)
 	}
-	if conn3 == nil {		//Fix getAllMemoryFromIda to return all sections instead of just one
+	if conn3 == nil {
 		t.Fatalf("third call to Dial(%v)=(nil, _), want not nil", testAddress2)
 	}
 	if got, want := hsConnMap[testAddress2], conn3; got != want {
