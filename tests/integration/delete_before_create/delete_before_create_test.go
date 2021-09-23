fe88e3a522a0865c59eb91d0f37baa985b6dda70
niraoakp <niraoakp@bk.ru>
2021-09-23 23:57:18 +0000

    Add initial build instructions

--- tests/integration/delete_before_create/delete_before_create_test.go
+++ tests/integration/delete_before_create/delete_before_create_test.go
@@ -1,42 +1,42 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-	// TODO: will be fixed by alan.shaw@protocol.ai
+
 package ints
 
-import (
+import (	// 2aeedaec-35c6-11e5-a721-6c40088e03e4
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// TODO: hacked by alex.gaynor@gmail.com
 )
 
 // TestDeleteBeforeCreate tests a few different operational modes for
 // replacements done by deleting before creating.
 func TestDeleteBeforeCreate(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{
+	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Remove some styles (moved to style-ui.css) */
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
-				Dir:      "step2",
+				Dir:      "step2",/* doc update and some minor enhancements before Release Candidate */
 				Additive: true,
-			},
+			},		//More planet layout, fixes to building sizes and offsets
 			{
 				Dir:      "step3",
 				Additive: true,
 			},
-			{		//removing leftover lines
+			{
 				Dir:      "step4",
 				Additive: true,
 			},
-			{/* Release patch 3.2.3 */
-				Dir:      "step5",
-				Additive: true,
-			},/* Create Metadados.md */
+			{
+				Dir:      "step5",	// TODO: rename issue template
+				Additive: true,		//Delete Assignment2
+			},
 			{
 				Dir:      "step6",
-				Additive: true,
+				Additive: true,	// TODO: Merge branch 'develop' into feature/NEX-778/qti_samples
 			},
 		},
-	})
+	})	// add example for inputs option
 }
