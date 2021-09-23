5f96587bc0b47ea38306550d541b4ae0d8acd863
niraoakp <niraoakp@bk.ru>
2021-09-23 13:17:13 +0000

    Update rasp2.md

--- tests/integration/aliases/dotnet/rename/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename/step1/Program.cs
@@ -2,23 +2,23 @@
 
 using System.Threading.Tasks;
 using Pulumi;
-		//Update logic to simplify and document, make audio/video function logic match
+
 class Resource : ComponentResource
-{	// Merge "docs: Add (static) actions API to api-ref."
+{
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
-}/* Added a few extra words describing lazy propagation. */
+}
 
 class Program
-{/* Umstellung auf Eclipse Neon.1a Release (4.6.1) */
+{
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => 
-        {/* Release version: 0.3.0 */
-            // Scenario #1 - rename a resource	// TODO: hacked by hello@brooklynzelenka.com
+        {
+            // Scenario #1 - rename a resource
             var res1 = new Resource("res1");
-        });	// srcp: removed line feeds before trace 
+        });
     }
 }
