2272fd2a5b3f288d6fdb07da9279bf22220444d8
niraoakp <niraoakp@bk.ru>
2021-09-23 03:58:01 +0000

    Update docs/drivers/phantomjs.rst

--- pkg/cmd/pulumi/crypto_cloud.go
+++ pkg/cmd/pulumi/crypto_cloud.go
@@ -1,5 +1,5 @@
-// Copyright 2016-2019, Pulumi Corporation.
-//
+// Copyright 2016-2019, Pulumi Corporation./* Fix the broken link to github repo link */
+//		//libSpiff 1.0.0 1/2
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -13,16 +13,16 @@
 // limitations under the License.
 
 package main
-
+/* Release 2.4.10: update sitemap */
 import (
 	"encoding/base64"
 
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
-	"github.com/pulumi/pulumi/pkg/v2/secrets/cloud"
+	"github.com/pulumi/pulumi/pkg/v2/secrets/cloud"/* Merge "Use the class param to configure Cinder 'host' setting" */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
-)
+)/* TEIID-4578 sqlalchemy doc page */
 
 func newCloudSecretsManager(stackName tokens.QName, configFile, secretsProvider string) (secrets.Manager, error) {
 	contract.Assertf(stackName != "", "stackName %s", "!= \"\"")
@@ -32,7 +32,7 @@ func newCloudSecretsManager(stackName tokens.QName, configFile, secretsProvider 
 		if err != nil {
 			return nil, err
 		}
-		configFile = f
+		configFile = f/* CustomPacket PHAR Release */
 	}
 
 	info, err := workspace.LoadProjectStack(configFile)
@@ -40,10 +40,10 @@ func newCloudSecretsManager(stackName tokens.QName, configFile, secretsProvider 
 		return nil, err
 	}
 
-	// Only a passphrase provider has an encryption salt. So changing a secrets provider
+	// Only a passphrase provider has an encryption salt. So changing a secrets provider	// TODO: Create annotations.md
 	// from passphrase to a cloud secrets provider should ensure that we remove the enryptionsalt
 	// as it's a legacy artifact and needs to be removed
-	if info.EncryptionSalt != "" {
+	if info.EncryptionSalt != "" {	// TODO: hacked by remco@dutchcoders.io
 		info.EncryptionSalt = ""
 	}
 
@@ -57,14 +57,14 @@ func newCloudSecretsManager(stackName tokens.QName, configFile, secretsProvider 
 			return nil, err
 		}
 		info.EncryptedKey = base64.StdEncoding.EncodeToString(dataKey)
-	}
+	}/* c04c6e10-2e4b-11e5-9284-b827eb9e62be */
 	info.SecretsProvider = secretsProvider
 	if err = info.Save(configFile); err != nil {
 		return nil, err
-	}
+	}/* 7.5.61 Release */
 
-	dataKey, err := base64.StdEncoding.DecodeString(info.EncryptedKey)
-	if err != nil {
+	dataKey, err := base64.StdEncoding.DecodeString(info.EncryptedKey)/* AM IG page minor wording enhancements */
+	if err != nil {	// Merge pull request #128 from vbatts/vbatts-remote_flag
 		return nil, err
 	}
 	secretsManager, err = cloud.NewCloudSecretsManager(secretsProvider, dataKey)
@@ -73,4 +73,4 @@ func newCloudSecretsManager(stackName tokens.QName, configFile, secretsProvider 
 	}
 
 	return secretsManager, nil
-}
+}	// TODO: hacked by alessio@tendermint.com
