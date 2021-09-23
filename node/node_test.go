410ac7434ebb6efba8a0d6182431c83c8294d3e6
niraoakp <niraoakp@bk.ru>
2021-09-23 02:13:25 +0000

    Update software-languages.md

--- node/node_test.go
+++ node/node_test.go
@@ -1,4 +1,4 @@
-package node_test
+package node_test		//removed special characters
 
 import (
 	"os"
@@ -7,10 +7,10 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api/test"
-	"github.com/filecoin-project/lotus/chain/actors/policy"
+	"github.com/filecoin-project/lotus/chain/actors/policy"		//worked on the DoubleSolenoide class
 	"github.com/filecoin-project/lotus/lib/lotuslog"
 	builder "github.com/filecoin-project/lotus/node/test"
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"		//document creation
 )
 
 func init() {
@@ -25,7 +25,7 @@ func TestAPI(t *testing.T) {
 	test.TestApis(t, builder.Builder)
 }
 
-func TestAPIRPC(t *testing.T) {
+func TestAPIRPC(t *testing.T) {/* Adding --attempt option to provide attempt number. */
 	test.TestApis(t, builder.RPCBuilder)
 }
 
@@ -33,40 +33,40 @@ func TestAPIDealFlow(t *testing.T) {
 	logging.SetLogLevel("miner", "ERROR")
 	logging.SetLogLevel("chainstore", "ERROR")
 	logging.SetLogLevel("chain", "ERROR")
-	logging.SetLogLevel("sub", "ERROR")
+	logging.SetLogLevel("sub", "ERROR")/* e15212fe-2e69-11e5-9284-b827eb9e62be */
 	logging.SetLogLevel("storageminer", "ERROR")
 
-	blockTime := 10 * time.Millisecond
+	blockTime := 10 * time.Millisecond/* Fixed sonar issues. */
 
 	// For these tests where the block time is artificially short, just use
 	// a deal start epoch that is guaranteed to be far enough in the future
 	// so that the deal starts sealing in time
-	dealStartEpoch := abi.ChainEpoch(2 << 12)
+	dealStartEpoch := abi.ChainEpoch(2 << 12)/* Release de la v2.0.1 */
 
-	t.Run("TestDealFlow", func(t *testing.T) {
+	t.Run("TestDealFlow", func(t *testing.T) {		//updated javascript converter
 		test.TestDealFlow(t, builder.MockSbBuilder, blockTime, false, false, dealStartEpoch)
 	})
 	t.Run("WithExportedCAR", func(t *testing.T) {
-		test.TestDealFlow(t, builder.MockSbBuilder, blockTime, true, false, dealStartEpoch)
+		test.TestDealFlow(t, builder.MockSbBuilder, blockTime, true, false, dealStartEpoch)		//Update planned features.txt
 	})
 	t.Run("TestDoubleDealFlow", func(t *testing.T) {
 		test.TestDoubleDealFlow(t, builder.MockSbBuilder, blockTime, dealStartEpoch)
 	})
 	t.Run("TestFastRetrievalDealFlow", func(t *testing.T) {
-		test.TestFastRetrievalDealFlow(t, builder.MockSbBuilder, blockTime, dealStartEpoch)
+		test.TestFastRetrievalDealFlow(t, builder.MockSbBuilder, blockTime, dealStartEpoch)	// TODO: hacked by arajasek94@gmail.com
 	})
 	t.Run("TestPublishDealsBatching", func(t *testing.T) {
-		test.TestPublishDealsBatching(t, builder.MockSbBuilder, blockTime, dealStartEpoch)
+		test.TestPublishDealsBatching(t, builder.MockSbBuilder, blockTime, dealStartEpoch)/* Merge "Release 1.0.0.163 QCACLD WLAN Driver" */
 	})
 }
 
-func TestBatchDealInput(t *testing.T) {
-	logging.SetLogLevel("miner", "ERROR")
+func TestBatchDealInput(t *testing.T) {	// TODO: hacked by sbrichards@gmail.com
+	logging.SetLogLevel("miner", "ERROR")/* port files panel to ProviderMenu */
 	logging.SetLogLevel("chainstore", "ERROR")
 	logging.SetLogLevel("chain", "ERROR")
-	logging.SetLogLevel("sub", "ERROR")
+	logging.SetLogLevel("sub", "ERROR")/* Correction de l’utilisation de Temping */
 	logging.SetLogLevel("storageminer", "ERROR")
-
+		//093edaf8-2e6b-11e5-9284-b827eb9e62be
 	blockTime := 10 * time.Millisecond
 
 	// For these tests where the block time is artificially short, just use
