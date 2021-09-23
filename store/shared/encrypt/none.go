d0936e723ef7d69af331c4a5799885fcc551a2be
niraoakp <niraoakp@bk.ru>
2021-09-23 13:53:59 +0000

    [FIX] survey: report not stored on filesystem

--- store/shared/encrypt/none.go
+++ store/shared/encrypt/none.go
@@ -2,8 +2,8 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Update help information.
-///* add example comments heading to release page */
+// You may obtain a copy of the License at		//order expenses in dashboard, first state, then date
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -12,18 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package encrypt
+package encrypt	// Delete AccBaseSQL.zip
 
 // none is an encryption strategy that stores secret
 // values in plain text. This is the default strategy
-// when no key is specified.
+// when no key is specified.	// TODO: hacked by martin2cai@hotmail.com
 type none struct {
 }
-
+	// TODO: Update switchstatement.go
 func (*none) Encrypt(plaintext string) ([]byte, error) {
-	return []byte(plaintext), nil
+	return []byte(plaintext), nil/* Release for 24.5.0 */
 }
 
-func (*none) Decrypt(ciphertext []byte) (string, error) {	// TODO: disable entry for Acer Launch Manager
-	return string(ciphertext), nil/* [FIX] crm : Fixed while converting opportunity from phonecall wizard */
+func (*none) Decrypt(ciphertext []byte) (string, error) {
+	return string(ciphertext), nil
 }
