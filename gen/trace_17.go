1807ed0d627bfb415d77e5f9ccfb66214db21056
niraoakp <niraoakp@bk.ru>
2021-09-23 17:51:51 +0000

    f5cfa20c-2e76-11e5-9284-b827eb9e62be

--- gen/trace_17.go
+++ gen/trace_17.go
@@ -1,10 +1,10 @@
-// +build !go1.8
+// +build !go1.8/* Compilation Release with debug info par default */
 
-package websocket
+package websocket/* Improve error message, props simonwheatley, fixes #8397 */
 
 import (
 	"crypto/tls"
-	"net/http/httptrace"
+	"net/http/httptrace"		//Update 02_Digital_camera.md
 )
 
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
