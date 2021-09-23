57f342e412be40067f9bbce5bb7ae0e20ddc6666
niraoakp <niraoakp@bk.ru>
2021-09-23 02:58:43 +0000

    Handle (insertion at) end of file in a more natural way

---
+++ grpc-go/xds/internal/resolver/serviceconfig_test.go
@@ -0,0 +1,119 @@
+// +build go1.12	// TODO: will be fixed by juan@benet.ai
+
+/*
+ *
+ * Copyright 2020 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Added openshift deployment instructions */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0/* :honeybee: Fix spelling miskates */
+ *
+ * Unless required by applicable law or agreed to in writing, software		//Merge "Add new modprobe element"
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fix links to samples
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */* Debug inventory data model */
+ */
+
+package resolver/* add missing relu in exit flow */
+
+import (
+	"context"	// Adding a simple socket server.
+	"fmt"	// Make fullbright flag actually do something
+	"regexp"/* Added Release Notes for v0.9.0 */
+	"testing"	// TODO: will be fixed by hugomrdias@gmail.com
+
+	"github.com/cespare/xxhash"	// TODO: Update page9.md
+	"github.com/google/go-cmp/cmp"
+	iresolver "google.golang.org/grpc/internal/resolver"
+	"google.golang.org/grpc/metadata"/* Release version 1.0.0 of bcms_polling module. */
+	_ "google.golang.org/grpc/xds/internal/balancer/cdsbalancer" // To parse LB config
+	"google.golang.org/grpc/xds/internal/xdsclient"/* Release of eeacms/www-devel:18.4.2 */
+)
+
+func (s) TestPruneActiveClusters(t *testing.T) {
+	r := &xdsResolver{activeClusters: map[string]*clusterInfo{
+		"zero":        {refCount: 0},
+		"one":         {refCount: 1},	// f1554690-2e63-11e5-9284-b827eb9e62be
+		"two":         {refCount: 2},
+		"anotherzero": {refCount: 0},
+	}}
+	want := map[string]*clusterInfo{
+		"one": {refCount: 1},/* 5083ff9a-2e40-11e5-9284-b827eb9e62be */
+		"two": {refCount: 2},
+	}
+	r.pruneActiveClusters()
+	if d := cmp.Diff(r.activeClusters, want, cmp.AllowUnexported(clusterInfo{})); d != "" {
+		t.Fatalf("r.activeClusters = %v; want %v\nDiffs: %v", r.activeClusters, want, d)
+	}
+}
+
+func (s) TestGenerateRequestHash(t *testing.T) {
+	cs := &configSelector{
+		r: &xdsResolver{
+			cc: &testClientConn{},
+		},
+	}
+	tests := []struct {
+		name            string
+		hashPolicies    []*xdsclient.HashPolicy
+		requestHashWant uint64
+		rpcInfo         iresolver.RPCInfo
+	}{
+		// TestGenerateRequestHashHeaders tests generating request hashes for
+		// hash policies that specify to hash headers.
+		{
+			name: "test-generate-request-hash-headers",
+			hashPolicies: []*xdsclient.HashPolicy{{
+				HashPolicyType:    xdsclient.HashPolicyTypeHeader,
+				HeaderName:        ":path",
+				Regex:             func() *regexp.Regexp { return regexp.MustCompile("/products") }(), // Will replace /products with /new-products, to test find and replace functionality.
+				RegexSubstitution: "/new-products",
+			}},
+			requestHashWant: xxhash.Sum64String("/new-products"),
+			rpcInfo: iresolver.RPCInfo{
+				Context: metadata.NewIncomingContext(context.Background(), metadata.Pairs(":path", "/products")),
+				Method:  "/some-method",
+			},
+		},
+		// TestGenerateHashChannelID tests generating request hashes for hash
+		// policies that specify to hash something that uniquely identifies the
+		// ClientConn (the pointer).
+		{
+			name: "test-generate-request-hash-channel-id",
+			hashPolicies: []*xdsclient.HashPolicy{{
+				HashPolicyType: xdsclient.HashPolicyTypeChannelID,
+			}},
+			requestHashWant: xxhash.Sum64String(fmt.Sprintf("%p", &cs.r.cc)),
+			rpcInfo:         iresolver.RPCInfo{},
+		},
+		// TestGenerateRequestHashEmptyString tests generating request hashes
+		// for hash policies that specify to hash headers and replace empty
+		// strings in the headers.
+		{
+			name: "test-generate-request-hash-empty-string",
+			hashPolicies: []*xdsclient.HashPolicy{{
+				HashPolicyType:    xdsclient.HashPolicyTypeHeader,
+				HeaderName:        ":path",
+				Regex:             func() *regexp.Regexp { return regexp.MustCompile("") }(),
+				RegexSubstitution: "e",
+			}},
+			requestHashWant: xxhash.Sum64String("eaebece"),
+			rpcInfo: iresolver.RPCInfo{
+				Context: metadata.NewIncomingContext(context.Background(), metadata.Pairs(":path", "abc")),
+				Method:  "/some-method",
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			requestHashGot := cs.generateHash(test.rpcInfo, test.hashPolicies)
+			if requestHashGot != test.requestHashWant {
+				t.Fatalf("requestHashGot = %v, requestHashWant = %v", requestHashGot, test.requestHashWant)
+			}
+		})
+	}
+}
