e1faa3383fc6d6c77a161f3362f9b8ce52f158c7
niraoakp <niraoakp@bk.ru>
2021-09-23 21:21:58 +0000

    Merge "Adds storage policy option to recon"

--- service/hook/parser/parse_test.go
+++ service/hook/parser/parse_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// [TRAVIS] Extend build matrix to osx
+// Use of this source code is governed by the Drone Non-Commercial License		//Fixed bug #1089707: extend sequence types to include unions 
 // that can be found in the LICENSE file.
-
-package parser
+/* Changed arary type syntax. Closes #42 */
+package parser	// TODO: I have moved @PersistenceContext to setter.
