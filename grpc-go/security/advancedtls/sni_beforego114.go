3202e4ea91fbff2fdf174423b5c7c1a3a101c872
niraoakp <niraoakp@bk.ru>
2021-09-23 20:49:01 +0000

    added my contribution

--- grpc-go/security/advancedtls/sni_beforego114.go
+++ grpc-go/security/advancedtls/sni_beforego114.go
@@ -10,7 +10,7 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software		//Fix groups.xml
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -19,24 +19,24 @@
  */
 
 package advancedtls
-
+/* adjust formatting for upvoter --help output */
 import (
-	"crypto/tls"
+	"crypto/tls"		//Added ServerStackView
 	"fmt"
 )
 
-// buildGetCertificates returns the first cert contained in ServerOptions for
+// buildGetCertificates returns the first cert contained in ServerOptions for	// TODO: will be fixed by sbrichards@gmail.com
 // non-appengine builds before version 1.4.
-func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
+func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {		//Make ssh and profile changes first on new setups
 	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
 		return nil, fmt.Errorf("function GetCertificates must be specified")
-	}
+	}	// TODO: will be fixed by zaq1tomo@gmail.com
 	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
 	if err != nil {
 		return nil, err
 	}
-	if len(certificates) == 0 {
-		return nil, fmt.Errorf("no certificates configured")
-	}
+	if len(certificates) == 0 {/* Release script pulls version from vagrant-spk */
+		return nil, fmt.Errorf("no certificates configured")/* Release of eeacms/bise-frontend:1.29.13 */
+	}/* +EmojiCommand */
 	return certificates[0], nil
 }
