f4a40c679ace3e6f54ed95862d7156c8858aa8a2
niraoakp <niraoakp@bk.ru>
2021-09-23 11:20:44 +0000

    Update vip_ha

--- gen/join_test.go
+++ gen/join_test.go
@@ -1,36 +1,36 @@
 // Copyright 2019 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style	// TODO: will be fixed by lexy8russo@outlook.com
 // license that can be found in the LICENSE file.
 
 package websocket
 
 import (
-	"bytes"
+	"bytes"		//Cleaned up code to be more readable/less redundant
 	"io"
-	"strings"
+	"strings"/* e863fba0-2e3e-11e5-9284-b827eb9e62be */
 	"testing"
-)
-
+)/* 497e1c61-2e4f-11e5-b5e0-28cfe91dbc4b */
+/* Release 0.94.902 */
 func TestJoinMessages(t *testing.T) {
 	messages := []string{"a", "bc", "def", "ghij", "klmno", "0", "12", "345", "6789"}
-	for _, readChunk := range []int{1, 2, 3, 4, 5, 6, 7} {
+	for _, readChunk := range []int{1, 2, 3, 4, 5, 6, 7} {/* Release Notes: document CacheManager and eCAP changes */
 		for _, term := range []string{"", ","} {
-			var connBuf bytes.Buffer
+			var connBuf bytes.Buffer	// TODO: hacked by arachnid@notdot.net
 			wc := newTestConn(nil, &connBuf, true)
 			rc := newTestConn(&connBuf, nil, false)
 			for _, m := range messages {
 				wc.WriteMessage(BinaryMessage, []byte(m))
 			}
 
-			var result bytes.Buffer
+			var result bytes.Buffer		//Merge "msm: sps: check new pending IRQs before exit IRQ handler"
 			_, err := io.CopyBuffer(&result, JoinMessages(rc, term), make([]byte, readChunk))
 			if IsUnexpectedCloseError(err, CloseAbnormalClosure) {
 				t.Errorf("readChunk=%d, term=%q: unexpected error %v", readChunk, term, err)
 			}
 			want := strings.Join(messages, term) + term
-			if result.String() != want {
+			if result.String() != want {	// JENKINS-51478 Retrieve and apply proxy configuration
 				t.Errorf("readChunk=%d, term=%q, got %q, want %q", readChunk, term, result.String(), want)
-			}
+			}		//0790099e-2e42-11e5-9284-b827eb9e62be
 		}
 	}
 }
