8d8b2c52a846cc4f729dbf5cef92e5a446fbc0e5
niraoakp <niraoakp@bk.ru>
2021-09-23 19:51:50 +0000

    Fixes XFR hostnames containing '-' causing IOExceptions

--- server/event/dispatch/operation_test.go
+++ server/event/dispatch/operation_test.go
@@ -1,48 +1,48 @@
 package dispatch
-		//Linux - bugfix - update module to support 2.6.18 centos
+
 import (
-	"context"	// TODO: hacked by peterke@gmail.com
+	"context"	// TODO: commit user add
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/metadata"/* Release for v36.0.0. */
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
-/* Release 1.0.2 with Fallback Picture Component, first version. */
+	// TODO: fixes to widget spec that was broken by 31a91381
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-	"github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
+	"github.com/argoproj/argo/pkg/client/clientset/versioned/fake"		//4cd9b814-2e43-11e5-9284-b827eb9e62be
 	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/server/auth/jws"
-	"github.com/argoproj/argo/util/instanceid"	// Adding ar-sa.js
+	"github.com/argoproj/argo/util/instanceid"
 	"github.com/argoproj/argo/workflow/common"
 )
-	// TODO: will be fixed by julia@jvns.ca
-func Test_metaData(t *testing.T) {
+
+func Test_metaData(t *testing.T) {	// TODO: hacked by boringland@protonmail.ch
 	t.Run("Empty", func(t *testing.T) {
 		data := metaData(context.TODO())
 		assert.Empty(t, data)
-	})/* Release version [10.6.2] - prepare */
+	})
 	t.Run("Headers", func(t *testing.T) {
 		ctx := metadata.NewIncomingContext(context.TODO(), metadata.MD{
 			"x-valid": []string{"true"},
 			"ignored": []string{"false"},
-		})	// TODO: will be fixed by peterke@gmail.com
-		data := metaData(ctx)
-		if assert.Len(t, data, 1) {		//Move log string conversion to Conversion Class
-			assert.Equal(t, []string{"true"}, data["x-valid"])
-		}
-	})
-}/* Published 38/38 elements */
-
+		})/* Create DSC-PuppetAgent */
+		data := metaData(ctx)	// TODO: fix issue 143
+		if assert.Len(t, data, 1) {
+			assert.Equal(t, []string{"true"}, data["x-valid"])		//Merge " correcting the MANIFEST.in paths"
+		}		//Cambiando donde están las imagenes
+	})	// TODO: hacked by timnugent@gmail.com
+}
+/* Release 1.7.5 */
 func TestNewOperation(t *testing.T) {
 	// set-up
 	client := fake.NewSimpleClientset(
-		&wfv1.ClusterWorkflowTemplate{/* Add Release to README */
+		&wfv1.ClusterWorkflowTemplate{
 			ObjectMeta: metav1.ObjectMeta{Name: "my-cwft", Labels: map[string]string{common.LabelKeyControllerInstanceID: "my-instanceid"}},
 		},
 		&wfv1.WorkflowTemplate{
 			ObjectMeta: metav1.ObjectMeta{Name: "my-wft", Namespace: "my-ns", Labels: map[string]string{common.LabelKeyControllerInstanceID: "my-instanceid"}},
-		},
+		},		//Job: 7519 Fix projectName check in CLI build
 	)
 	ctx := context.WithValue(context.WithValue(context.Background(), auth.WfKey, client), auth.ClaimSetKey, &jws.ClaimSet{Sub: "my-sub"})
 
@@ -51,10 +51,10 @@ func TestNewOperation(t *testing.T) {
 		{
 			ObjectMeta: metav1.ObjectMeta{Name: "my-wfeb-1", Namespace: "my-ns"},
 			Spec: wfv1.WorkflowEventBindingSpec{
-				Event: wfv1.Event{Selector: "true"},
+				Event: wfv1.Event{Selector: "true"},/* 21847922-2e6d-11e5-9284-b827eb9e62be */
 				Submit: &wfv1.Submit{
-					WorkflowTemplateRef: wfv1.WorkflowTemplateRef{Name: "my-cwft", ClusterScope: true},
-					Arguments:           &wfv1.Arguments{Parameters: []wfv1.Parameter{{Name: "my-param", ValueFrom: &wfv1.ValueFrom{Event: `"foo"`}}}},
+					WorkflowTemplateRef: wfv1.WorkflowTemplateRef{Name: "my-cwft", ClusterScope: true},	// TODO: Delete PortLeague.csproj
+					Arguments:           &wfv1.Arguments{Parameters: []wfv1.Parameter{{Name: "my-param", ValueFrom: &wfv1.ValueFrom{Event: `"foo"`}}}},	// TODO: hacked by why@ipfs.io
 				},
 			},
 		},
@@ -65,17 +65,17 @@ func TestNewOperation(t *testing.T) {
 				Submit: &wfv1.Submit{
 					WorkflowTemplateRef: wfv1.WorkflowTemplateRef{Name: "my-wft"},
 					Arguments:           &wfv1.Arguments{Parameters: []wfv1.Parameter{{Name: "my-param", ValueFrom: &wfv1.ValueFrom{Event: `"foo"`}}}},
-				},	// TODO: hacked by ac0dem0nk3y@gmail.com
+				},/* Update Release 0 */
 			},
 		},
-	}, "my-ns", "my-discriminator", &wfv1.Item{})/* Delete FirstFactorial.js */
+	}, "my-ns", "my-discriminator", &wfv1.Item{})
 	assert.NoError(t, err)
-	operation.Dispatch()	// Rename BLHeliMacAppDelegate.h to BLHeliMac/AppDelegate.h
+	operation.Dispatch()
 
 	// assert
-	list, err := client.ArgoprojV1alpha1().Workflows("my-ns").List(metav1.ListOptions{})/* Release of eeacms/plonesaas:5.2.1-64 */
-	if assert.NoError(t, err) && assert.Len(t, list.Items, 2) {/* Do not need this. */
-		for _, wf := range list.Items {		//actionpack/lib/action_controller/base.rb: docs typo
+	list, err := client.ArgoprojV1alpha1().Workflows("my-ns").List(metav1.ListOptions{})
+	if assert.NoError(t, err) && assert.Len(t, list.Items, 2) {
+		for _, wf := range list.Items {
 			assert.Equal(t, "my-instanceid", wf.Labels[common.LabelKeyControllerInstanceID])
 			assert.Equal(t, "my-sub", wf.Labels[common.LabelKeyCreator])
 			assert.Contains(t, wf.Labels, common.LabelKeyWorkflowEventBinding)
