4c02bfae8ac60cff735e4e822d64206cd085dd39
niraoakp <niraoakp@bk.ru>
2021-09-23 01:50:35 +0000

    Bug 2922: The item delegates are reset to default on method change.

--- store/repos/scan_test.go
+++ store/repos/scan_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//fix quickfield helper
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package repos/* Added README file for github. */
+package repos		//Updated Service for (? extends Message) like previously done for Topic.
