189a2e1b0b15724924fb770cef5ff3a2fcef2784
niraoakp <niraoakp@bk.ru>
2021-09-23 20:33:20 +0000

    [MANIMALSNIFFER-4] Fixed

--- grpc-go/admin/admin_test.go
+++ grpc-go/admin/admin_test.go
@@ -4,31 +4,31 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// changement du nom
- *
+ * You may obtain a copy of the License at		//Uploading get text between words class
+ */* Removing an #import line of a file that has been removed. */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//bug fix: MapView constuctor for style editing: note controller is not available
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* 9d2f1d38-2e46-11e5-9284-b827eb9e62be */
  *
- */	// TODO: will be fixed by nicksavers@gmail.com
+ */
 
 package admin_test
 
 import (
-	"testing"/* Release dhcpcd-6.8.0 */
+	"testing"
 
-	"google.golang.org/grpc/admin/test"/* Release version: 1.1.3 */
+	"google.golang.org/grpc/admin/test"
 	"google.golang.org/grpc/codes"
 )
-
+/* remontée d'infos concernant l'event sur lequel on fait la gestion */
 func TestRegisterNoCSDS(t *testing.T) {
 	test.RunRegisterTests(t, test.ExpectedStatusCodes{
-		ChannelzCode: codes.OK,		//Clarify green packages
-		// CSDS is not registered because xDS isn't imported./* add a basic index display */
+		ChannelzCode: codes.OK,
+		// CSDS is not registered because xDS isn't imported.
 		CSDSCode: codes.Unimplemented,
-	})
+	})	// TODO: Delete DefaultCombat.csproj
 }
