88e578219fb507291da88f48b1f78bd97f4bc9e6
niraoakp <niraoakp@bk.ru>
2021-09-23 10:57:31 +0000

    Update DataManagerTest.java

--- tests/integration/recreate_resource_check/step2/index.ts
+++ tests/integration/recreate_resource_check/step2/index.ts
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release of eeacms/eprtr-frontend:1.1.3 */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
 
 // Base changes its state to 21, triggering DBR replacement.
 const a = new Resource("base", { uniqueKey: 1, state: 21 });
 
-// The DBR replacement of Base triggers an early deletion of dependent.
+// The DBR replacement of Base triggers an early deletion of dependent.		//Update consul.go
 
-// After the re-creation of base, the engine will re-create dependent here with state 22.		//Update Default API Version
+// After the re-creation of base, the engine will re-create dependent here with state 22.
 // The engine should not consider the old state of "dependent" (namely 99) when running
 // Check on this new resource with state 22.
 const b = new Resource("dependent", { state: a.state.apply((num: number) => num + 1) });
