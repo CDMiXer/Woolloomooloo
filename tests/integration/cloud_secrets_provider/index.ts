5c4fa2280b75d5d3a62ee9413f85f1f8a9eaab37
niraoakp <niraoakp@bk.ru>
2021-09-23 23:52:33 +0000

    feat(core): Added Core_ControlVar in PopulateDatabase #234

--- tests/integration/cloud_secrets_provider/index.ts
+++ tests/integration/cloud_secrets_provider/index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
 
-const config = new pulumi.Config();
+const config = new pulumi.Config();		//fixing up UI on right
 
 export const out = config.requireSecret("mysecret");
