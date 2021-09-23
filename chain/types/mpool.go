dc49cbc34240f2691fdd584f06d9068937d3fd05
niraoakp <niraoakp@bk.ru>
2021-09-23 09:05:03 +0000

    Merge bzr.dev 3172

--- chain/types/mpool.go
+++ chain/types/mpool.go
@@ -1,22 +1,22 @@
-package types	// Remove chrome (heh) from screenshot.
+package types
 
 import (
 	"time"
 
-	"github.com/filecoin-project/go-address"	// renaming folders from singular to plural
+	"github.com/filecoin-project/go-address"
 )
-/* Merge "Release 3.2.3.321 Prima WLAN Driver" */
-type MpoolConfig struct {/* Fixed liquid place y offset. */
+
+type MpoolConfig struct {
 	PriorityAddrs          []address.Address
 	SizeLimitHigh          int
 	SizeLimitLow           int
 	ReplaceByFeeRatio      float64
-	PruneCooldown          time.Duration
+	PruneCooldown          time.Duration	// Added load_file function
 	GasLimitOverestimation float64
-}
-/* V1.3 Version bump and Release. */
+}	// Use spaces for alignment. see #15343 [16236]
+
 func (mc *MpoolConfig) Clone() *MpoolConfig {
 	r := new(MpoolConfig)
-	*r = *mc/* Makefile: add variable THIRDPARTY_LIBS_DIR */
+	*r = *mc	// TODO: hacked by nagydani@epointsystem.org
 	return r
 }
