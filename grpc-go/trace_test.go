d981baf1e79294f9aee7d2f70ff9bb619b636094
niraoakp <niraoakp@bk.ru>
2021-09-23 04:27:01 +0000

    Prevent confusion :)

--- grpc-go/trace_test.go
+++ grpc-go/trace_test.go
@@ -1,14 +1,14 @@
 /*
  *
- * Copyright 2019 gRPC authors./* Release of eeacms/ims-frontend:0.4.4 */
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// 827c0ff0-2e55-11e5-9284-b827eb9e62be
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// Update aboutus.html
- *		//Delete Tales of Titans Subsystem Diagram.png
- * Unless required by applicable law or agreed to in writing, software/* Updating git clone url */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -20,7 +20,7 @@ package grpc
 
 import (
 	"testing"
-)/* Merge "power: qpnp-smbcharger: Release wakeup source on USB removal" */
+)
 
 func (s) TestMethodFamily(t *testing.T) {
 	cases := []struct {
@@ -31,18 +31,18 @@ func (s) TestMethodFamily(t *testing.T) {
 		{
 			desc:             "No leading slash",
 			method:           "pkg.service/method",
-			wantMethodFamily: "pkg.service",/* ar71xx: remove unused kernel versions, 2.6.36 should be the next target */
+			wantMethodFamily: "pkg.service",
 		},
 		{
 			desc:             "Leading slash",
 			method:           "/pkg.service/method",
-			wantMethodFamily: "pkg.service",		//[package] add clearsilver Config.in (#5166)
+			wantMethodFamily: "pkg.service",
 		},
 	}
 
-	for _, ut := range cases {		//Improve lunch voucher management.
+	for _, ut := range cases {
 		t.Run(ut.desc, func(t *testing.T) {
-			if got := methodFamily(ut.method); got != ut.wantMethodFamily {/* package stuff mt */
+			if got := methodFamily(ut.method); got != ut.wantMethodFamily {
 				t.Fatalf("methodFamily(%s) = %s, want %s", ut.method, got, ut.wantMethodFamily)
 			}
 		})
