1207df9f23601fdbe4db080b2bfc110db05fc541
niraoakp <niraoakp@bk.ru>
2021-09-23 22:35:27 +0000

    Merge branch 'develop' into configurable-CT-interval

--- tests/integration/aliases/nodejs/rename/step2/index.ts
+++ tests/integration/aliases/nodejs/rename/step2/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* ProRelease2 update R11 should be 470 Ohm */
+import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {/* Add Release History to README */
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: 13b59b54-2e6e-11e5-9284-b827eb9e62be
+class Resource extends pulumi.ComponentResource {
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
 
 // Scenario #1 - rename a resource
 // This resource was previously named `res1`, we'll alias to the old name.
-const res1 = new Resource("newres1", {		//Added button icons
+const res1 = new Resource("newres1", {
     aliases: [{ name: "res1" }],
-});	// TODO: hacked by sebs@2xs.org
+});
