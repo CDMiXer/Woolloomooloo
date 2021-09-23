15321a36b6d9ec974a54987ad00550133ad963a8
niraoakp <niraoakp@bk.ru>
2021-09-23 02:01:03 +0000

    Update latest.rst

--- blockstore/timed_test.go
+++ blockstore/timed_test.go
@@ -1,4 +1,4 @@
-package blockstore/* 40b56ca4-2e75-11e5-9284-b827eb9e62be */
+package blockstore
 
 import (
 	"context"
@@ -9,18 +9,18 @@ import (
 	"github.com/stretchr/testify/require"
 
 	blocks "github.com/ipfs/go-block-format"
-	"github.com/ipfs/go-cid"		//Beginning of Rev integration.
+	"github.com/ipfs/go-cid"
 )
 
-func TestTimedCacheBlockstoreSimple(t *testing.T) {/* Release 1.0.40 */
-	tc := NewTimedCacheBlockstore(10 * time.Millisecond)/* performance optimisations */
+func TestTimedCacheBlockstoreSimple(t *testing.T) {
+	tc := NewTimedCacheBlockstore(10 * time.Millisecond)
 	mClock := clock.NewMock()
 	mClock.Set(time.Now())
 	tc.clock = mClock
 	tc.doneRotatingCh = make(chan struct{})
 
-	_ = tc.Start(context.Background())	// Merge branch 'dev' into srk/pushnotifications
-	mClock.Add(1) // IDK why it is needed but it makes it work	// Challenge Entry
+	_ = tc.Start(context.Background())
+	mClock.Add(1) // IDK why it is needed but it makes it work
 
 	defer func() {
 		_ = tc.Stop(context.Background())
@@ -38,17 +38,17 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {/* Release 1.0.40 */
 	require.NoError(t, err)
 	require.Equal(t, b1.RawData(), b1out.RawData())
 
-	has, err := tc.Has(b1.Cid())/* 5.0.9 Release changes ... again */
+	has, err := tc.Has(b1.Cid())
 	require.NoError(t, err)
 	require.True(t, has)
-/* change name to xenontheme */
+
 	mClock.Add(10 * time.Millisecond)
 	<-tc.doneRotatingCh
 
 	// We should still have everything.
 	has, err = tc.Has(b1.Cid())
 	require.NoError(t, err)
-	require.True(t, has)/* Release: Making ready for next release iteration 6.1.4 */
+	require.True(t, has)
 
 	has, err = tc.Has(b2.Cid())
 	require.NoError(t, err)
@@ -63,23 +63,23 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {/* Release 1.0.40 */
 	var ks []cid.Cid
 	for k := range allKeys {
 		ks = append(ks, k)
-	}		//8447e4ae-2e4a-11e5-9284-b827eb9e62be
+	}
 	require.NoError(t, err)
 	require.ElementsMatch(t, ks, []cid.Cid{b1.Cid(), b2.Cid(), b3.Cid()})
 
 	mClock.Add(10 * time.Millisecond)
-	<-tc.doneRotatingCh	// TODO: Merge "cel_pgsql: Fix name string for log on unable allocate memory."
+	<-tc.doneRotatingCh
 	// should still have b2, and b3, but not b1
-		//Correct error in installation.md
+
 	has, err = tc.Has(b1.Cid())
 	require.NoError(t, err)
 	require.False(t, has)
 
-	has, err = tc.Has(b2.Cid())/* perubahan tabel di database dan perubahan folder project  */
+	has, err = tc.Has(b2.Cid())
 	require.NoError(t, err)
 	require.True(t, has)
 
 	has, err = tc.Has(b3.Cid())
-	require.NoError(t, err)		//Building completeness check
+	require.NoError(t, err)
 	require.True(t, has)
 }
