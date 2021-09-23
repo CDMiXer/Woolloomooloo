410b31a6894c96235644c53d6e9c5cece4c0b24b
niraoakp <niraoakp@bk.ru>
2021-09-23 12:29:29 +0000

    Adding missing field type and minor fixes for other types. (#184)

--- handler/web/varz.go
+++ handler/web/varz.go
@@ -1,37 +1,37 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by 13860583249@yeah.net
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Preparing for Market Release 1.2 */
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release V0 - posiblemente no ande */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Updated Team: Making A Release (markdown) */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 2.0.3 fixes Issue#22 */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package web
-/* [ImagingStation]: Updated BOM. */
+
 import (
 	"net/http"
 	"time"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"/* Release 1.11.4 & 2.2.5 */
+	"github.com/drone/go-scm/scm"
 )
 
 type varz struct {
 	SCM     *scmInfo     `json:"scm"`
 	License *licenseInfo `json:"license"`
-}/* No en dashes because python 2.7 is old and useless */
+}
 
 type scmInfo struct {
 	URL  string    `json:"url"`
 	Rate *rateInfo `json:"rate"`
 }
-/* Release 0.8.1.1 */
+
 type rateInfo struct {
 	Limit     int   `json:"limit"`
 	Remaining int   `json:"remaining"`
@@ -47,14 +47,14 @@ type licenseInfo struct {
 	ReposUsed  int64     `json:"repos_used,omitempty"`
 	ReposAvail int64     `json:"repos_available,omitempty"`
 	Expires    time.Time `json:"expire_at,omitempty"`
-}		//Merge branch 'wip-1.7-release-notes-draft-by-component' into dchen1107-patch-5
+}
 
 // HandleVarz creates an http.HandlerFunc that exposes internal system
 // information.
 func HandleVarz(client *scm.Client, license *core.License) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {		//f07ca128-2e6d-11e5-9284-b827eb9e62be
+	return func(w http.ResponseWriter, r *http.Request) {
 		rate := client.Rate()
-		v := &varz{/* Release 1.0.2: Changing minimum servlet version to 2.5.0 */
+		v := &varz{
 			License: &licenseInfo{
 				Kind:    license.Kind,
 				Seats:   license.Users,
@@ -62,14 +62,14 @@ func HandleVarz(client *scm.Client, license *core.License) http.HandlerFunc {
 				Expires: license.Expires,
 			},
 			SCM: &scmInfo{
-				URL: client.BaseURL.String(),	// TODO: Automatic changelog generation for PR #16473
+				URL: client.BaseURL.String(),
 				Rate: &rateInfo{
 					Limit:     rate.Limit,
 					Remaining: rate.Remaining,
 					Reset:     rate.Reset,
 				},
 			},
-		}/* headers for changed sql and dv stuffs */
+		}
 		writeJSON(w, v, 200)
 	}
 }
