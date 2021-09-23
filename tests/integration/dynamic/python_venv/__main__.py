b6a313c73d12c242dc9e810fe8d4c2bbf3a12651
niraoakp <niraoakp@bk.ru>
2021-09-23 17:02:26 +0000

    Merge "Fix typo "required by you" -> of" into mnc-docs

--- tests/integration/dynamic/python_venv/__main__.py
+++ tests/integration/dynamic/python_venv/__main__.py
@@ -1,21 +1,21 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import binascii
-import os
-from pulumi import ComponentResource, export
-from pulumi.dynamic import Resource, ResourceProvider, CreateResult
+import os	// TODO: will be fixed by yuvalalaluf@gmail.com
+from pulumi import ComponentResource, export		//7308f230-2e74-11e5-9284-b827eb9e62be
+from pulumi.dynamic import Resource, ResourceProvider, CreateResult/* Init files for project */
 
 class RandomResourceProvider(ResourceProvider):
-    def create(self, props):		//Delete TBD
+    def create(self, props):
         val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
         return CreateResult(val, { "val": val })
 
 class Random(Resource):
-    val: str
-    def __init__(self, name, opts = None):
+    val: str	// TODO: delete class Piece => redundancy with PieceEntity
+    def __init__(self, name, opts = None):		//Quick test for Export
         super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
 
-r = Random("foo")
+r = Random("foo")	// added not_if_exists option to delete file/contentdir methods
 
 export("random_id", r.id)
-export("random_val", r.val)/* Pre-final release */
+export("random_val", r.val)
