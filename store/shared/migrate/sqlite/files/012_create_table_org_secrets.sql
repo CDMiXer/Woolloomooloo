6a1612ece8921918c276738bae6d4ad88fdccf79
niraoakp <niraoakp@bk.ru>
2021-09-23 18:23:19 +0000

    Lisätty JavaScript funktio checkEanCode

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
 -- name: create-table-org-secrets
 
-CREATE TABLE IF NOT EXISTS orgsecrets (
- secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
+CREATE TABLE IF NOT EXISTS orgsecrets (		//Minor update to ensure all genes analysed.
+ secret_id                INTEGER PRIMARY KEY AUTOINCREMENT		//760b1c06-2e42-11e5-9284-b827eb9e62be
 ,secret_namespace         TEXT COLLATE NOCASE
 ,secret_name              TEXT COLLATE NOCASE
 ,secret_type              TEXT
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN
+,secret_pull_request_push BOOLEAN		//Create Proposabe.sol
 ,UNIQUE(secret_namespace, secret_name)
 );
