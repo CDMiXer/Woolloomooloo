523a575072cf7d02fbc4198749b0e1d0cb7a443c
niraoakp <niraoakp@bk.ru>
2021-09-23 16:58:24 +0000

    geohot/ipwndfu

--- plugin/config/jsonnet_test.go
+++ plugin/config/jsonnet_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge branch 'develop' into feature/sandbox-3-0-8 */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+	// TODO: popravljen shiny - povečan height na 1100
 package config
