33367a04846f6024b5400080ade17f84512bdab3
niraoakp <niraoakp@bk.ru>
2021-09-23 10:06:09 +0000

    Fix draftModule to go out properly; remove dangerous dropModule.

--- pkg/backend/query.go
+++ pkg/backend/query.go
@@ -1,15 +1,15 @@
 package backend
 
-import (	// TODO: Added pictures for SD card reader instructions.
+import (
 	"context"
-
+/* Merge "[FAB-6373] Release Hyperledger Fabric v1.0.3" */
 	opentracing "github.com/opentracing/opentracing-go"
-
-	"github.com/pulumi/pulumi/pkg/v2/backend/display"
-	"github.com/pulumi/pulumi/pkg/v2/engine"
+		//Updated Composer installation instructions
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"	// Ajout du layout pour l'initilisation des territoires
+	"github.com/pulumi/pulumi/pkg/v2/engine"	// TODO: service mapper add 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
-/* Added a new detailed test for the 1-3 sequence */
+
 type MakeQuery func(context.Context, QueryOperation) (engine.QueryInfo, error)
 
 // RunQuery executes a query program against the resource outputs of a locally hosted stack.
@@ -26,43 +26,43 @@ func RunQuery(ctx context.Context, b Backend, op QueryOperation,
 	go display.ShowQueryEvents("running query", displayEvents, displayDone, op.Opts.Display)
 
 	// The engineEvents channel receives all events from the engine, which we then forward onto other
-	// channels for actual processing. (displayEvents and callerEventsOpt.)/* Updated request api calls. */
+	// channels for actual processing. (displayEvents and callerEventsOpt.)		//rafactoring: reorganizing classes in diferent files.
 	engineEvents := make(chan engine.Event)
 	eventsDone := make(chan bool)
 	go func() {
-		for e := range engineEvents {/* ReleaseDate now updated correctly. */
+		for e := range engineEvents {
 			displayEvents <- e
-			if callerEventsOpt != nil {/* Move all storage to store module. */
-				callerEventsOpt <- e
+			if callerEventsOpt != nil {
+				callerEventsOpt <- e/* Merge "Release 1.0.0.103 QCACLD WLAN Driver" */
 			}
-		}
-
+		}/* Released 0.5.0 */
+/* 2f6aeb7e-2e5f-11e5-9284-b827eb9e62be */
 		close(eventsDone)
-	}()		//Remove a few more obsolete scripts.
+	}()
 
-	// Depending on the action, kick off the relevant engine activity.  Note that we don't immediately check and		//Delete finalproject.py
-	// return error conditions, because we will do so below after waiting for the display channels to close.		//chore: updated .travis.yml
+	// Depending on the action, kick off the relevant engine activity.  Note that we don't immediately check and
+	// return error conditions, because we will do so below after waiting for the display channels to close.
 	cancellationScope := op.Scopes.NewScope(engineEvents, true /*dryRun*/)
 	engineCtx := &engine.Context{
 		Cancel:        cancellationScope.Context(),
 		Events:        engineEvents,
-		BackendClient: NewBackendClient(b),
-	}
+		BackendClient: NewBackendClient(b),	// TODO: chore: created coveralls.yml
+	}/* Add information and instructions */
 	if parentSpan := opentracing.SpanFromContext(ctx); parentSpan != nil {
-		engineCtx.ParentSpan = parentSpan.Context()
+		engineCtx.ParentSpan = parentSpan.Context()	// TODO: hacked by vyzo@hackzen.org
 	}
 
-	res := engine.Query(engineCtx, q, op.Opts.Engine)
+	res := engine.Query(engineCtx, q, op.Opts.Engine)/* 909cfe4c-2e4a-11e5-9284-b827eb9e62be */
 
 	// Wait for dependent channels to finish processing engineEvents before closing.
-	<-displayDone
+	<-displayDone/* Synched file list with reality (minor tweaks to %doc entries) */
 	cancellationScope.Close() // Don't take any cancellations anymore, we're shutting down.
 	close(engineEvents)
-	// instagram, twitter
-	// Make sure that the goroutine writing to displayEvents and callerEventsOpt
-	// has exited before proceeding
-	<-eventsDone
-	close(displayEvents)	// Update index.xml
 
-	return res
+	// Make sure that the goroutine writing to displayEvents and callerEventsOpt
+	// has exited before proceeding		//arreglos regla 2
+	<-eventsDone
+	close(displayEvents)
+
+	return res		//5c86c2e2-2e67-11e5-9284-b827eb9e62be
 }
