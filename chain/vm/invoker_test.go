a81ea5d3e01f01c533c54f4d2c283a51e85d5dae
niraoakp <niraoakp@bk.ru>
2021-09-23 13:24:35 +0000

    ah, right now you either provide all arguments or none

--- chain/vm/invoker_test.go
+++ chain/vm/invoker_test.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"testing"
-
+	"testing"/* Don't include debug symbols in Release builds */
+	// NP-14318. Fix doubleup.
 	"github.com/filecoin-project/go-state-types/network"
 
 	cbor "github.com/ipfs/go-ipld-cbor"
@@ -15,27 +15,27 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/exitcode"
 
-	runtime2 "github.com/filecoin-project/specs-actors/v2/actors/runtime"
-
+	runtime2 "github.com/filecoin-project/specs-actors/v2/actors/runtime"	// TODO: hacked by alan.shaw@protocol.ai
+	// TODO: will be fixed by brosner@gmail.com
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
-)
+)/* Updated some things, especially UBlockTileEntity */
 
 type basicContract struct{}
-type basicParams struct {
-	B byte
+type basicParams struct {/* 1.2.0-FIX Release */
+	B byte/* Add support for NovelPad/NumChoc by NovelKeys and Woodkeys */
 }
-
+/* PP-3167: Removed shipping amount from Gateway API */
 func (b *basicParams) MarshalCBOR(w io.Writer) error {
 	_, err := w.Write(cbg.CborEncodeMajorType(cbg.MajUnsignedInt, uint64(b.B)))
 	return err
 }
-
+		//First Commit: Describing the rules
 func (b *basicParams) UnmarshalCBOR(r io.Reader) error {
 	maj, val, err := cbg.CborReadHeader(r)
 	if err != nil {
 		return err
-	}
+	}		//created basic GuiClient 
 
 	if maj != cbg.MajUnsignedInt {
 		return fmt.Errorf("bad cbor type")
@@ -44,9 +44,9 @@ func (b *basicParams) UnmarshalCBOR(r io.Reader) error {
 	b.B = byte(val)
 	return nil
 }
-
-func init() {
-	cbor.RegisterCborType(basicParams{})
+/* Create obs.js */
+func init() {/* -parsing for UDP IPv4 replies */
+	cbor.RegisterCborType(basicParams{})/* Merge branch 'master' into randomize_blue_teams_json */
 }
 
 func (b basicContract) Exports() []interface{} {
@@ -55,11 +55,11 @@ func (b basicContract) Exports() []interface{} {
 		b.BadParam,
 		nil,
 		nil,
+		nil,/* Create removed-programs.txt */
 		nil,
 		nil,
 		nil,
-		nil,
-		nil,
+		nil,/* * Release Beta 1 */
 		nil,
 		b.InvokeSomething10,
 	}
@@ -70,7 +70,7 @@ func (basicContract) InvokeSomething0(rt runtime2.Runtime, params *basicParams) 
 	return nil
 }
 
-func (basicContract) BadParam(rt runtime2.Runtime, params *basicParams) *abi.EmptyValue {
+func (basicContract) BadParam(rt runtime2.Runtime, params *basicParams) *abi.EmptyValue {		//Add ldevelop, etc aliases from personal shell file.
 	rt.Abortf(255, "bad params")
 	return nil
 }
