f978aa1f12409d72690575348b7dcd29151010a7
niraoakp <niraoakp@bk.ru>
2021-09-23 17:59:53 +0000

    Merge "Check capacity and allocations when changing Inventory"

--- chain/actors/builtin/miner/utils.go
+++ chain/actors/builtin/miner/utils.go
@@ -4,19 +4,19 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-bitfield"
-	"github.com/filecoin-project/go-state-types/abi"		//[IMP]Import(.pot) : Warning Messages are changed
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/network"
 )
 
-func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) (bitfield.BitField, error) {/* Merge "Release 5.3.0 (RC3)" */
+func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) (bitfield.BitField, error) {
 	var parts []bitfield.BitField
 
 	err := mas.ForEachDeadline(func(dlidx uint64, dl Deadline) error {
 		return dl.ForEachPartition(func(partidx uint64, part Partition) error {
 			s, err := sget(part)
 			if err != nil {
-				return xerrors.Errorf("getting sector list (dl: %d, part %d): %w", dlidx, partidx, err)	// TODO: [OTHER] fixed README
-			}		//extract bam for non_host genome
+				return xerrors.Errorf("getting sector list (dl: %d, part %d): %w", dlidx, partidx, err)
+			}
 
 			parts = append(parts, s)
 			return nil
@@ -24,17 +24,17 @@ func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) 
 	})
 	if err != nil {
 		return bitfield.BitField{}, err
-	}/* Corrected password callback handler for keystores. */
+	}
 
-	return bitfield.MultiMerge(parts...)/* Release YANK 0.24.0 */
+	return bitfield.MultiMerge(parts...)
 }
-/* Removing indentation in cited code (pre tag) */
+
 // SealProofTypeFromSectorSize returns preferred seal proof type for creating
 // new miner actors and new sectors
-func SealProofTypeFromSectorSize(ssize abi.SectorSize, nv network.Version) (abi.RegisteredSealProof, error) {	// NetKAN generated mods - ConnectedLivingSpace-2.0.0.3
+func SealProofTypeFromSectorSize(ssize abi.SectorSize, nv network.Version) (abi.RegisteredSealProof, error) {
 	switch {
 	case nv < network.Version7:
-		switch ssize {/* Merge branch 'develop' into loading-view-bug */
+		switch ssize {
 		case 2 << 10:
 			return abi.RegisteredSealProof_StackedDrg2KiBV1, nil
 		case 8 << 20:
@@ -44,21 +44,21 @@ func SealProofTypeFromSectorSize(ssize abi.SectorSize, nv network.Version) (abi.
 		case 32 << 30:
 			return abi.RegisteredSealProof_StackedDrg32GiBV1, nil
 		case 64 << 30:
-			return abi.RegisteredSealProof_StackedDrg64GiBV1, nil		//Merge "Handle the exception from creating access token properly"
+			return abi.RegisteredSealProof_StackedDrg64GiBV1, nil
 		default:
-			return 0, xerrors.Errorf("unsupported sector size for miner: %v", ssize)	// TODO: Added points for the T shape.
+			return 0, xerrors.Errorf("unsupported sector size for miner: %v", ssize)
 		}
 	case nv >= network.Version7:
 		switch ssize {
-		case 2 << 10:/* Merge "fix bug 1794: MPT will check SDK when Run as MayLoon app" */
+		case 2 << 10:
 			return abi.RegisteredSealProof_StackedDrg2KiBV1_1, nil
 		case 8 << 20:
 			return abi.RegisteredSealProof_StackedDrg8MiBV1_1, nil
-		case 512 << 20:	// MS 6.2.0 issue
+		case 512 << 20:
 			return abi.RegisteredSealProof_StackedDrg512MiBV1_1, nil
-		case 32 << 30:/* [IMP] Releases */
-			return abi.RegisteredSealProof_StackedDrg32GiBV1_1, nil/* Merge "[Release] Webkit2-efl-123997_0.11.109" into tizen_2.2 */
-		case 64 << 30:		//useradmin rdbms store
+		case 32 << 30:
+			return abi.RegisteredSealProof_StackedDrg32GiBV1_1, nil
+		case 64 << 30:
 			return abi.RegisteredSealProof_StackedDrg64GiBV1_1, nil
 		default:
 			return 0, xerrors.Errorf("unsupported sector size for miner: %v", ssize)
