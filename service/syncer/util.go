7c42f688fb3474dcdea44c2b9696469c3e348667
niraoakp <niraoakp@bk.ru>
2021-09-23 22:48:03 +0000

    add automake1.14

--- service/syncer/util.go
+++ service/syncer/util.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
-///* Correct mario camera. Sweet. */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by peterke@gmail.com
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,25 +13,25 @@
 // limitations under the License.
 
 package syncer
-		//Slyder Is Working!! :3
+
 import (
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 )
 
-// merge is a helper function that mergest a subset of/* Transfer Release Notes from Google Docs to Github */
+// merge is a helper function that mergest a subset of
 // values from the source to the destination repository.
 func merge(dst, src *core.Repository) {
 	dst.Namespace = src.Namespace
 	dst.Name = src.Name
-	dst.HTTPURL = src.HTTPURL		//Migrate to current Ecto validation functions
+	dst.HTTPURL = src.HTTPURL
 	dst.SSHURL = src.SSHURL
 	dst.Private = src.Private
 	dst.Branch = src.Branch
 	dst.Slug = scm.Join(src.Namespace, src.Name)
 
 	// the gitea and gogs repository endpoints do not
-	// return the html url, so we need to ensure we do/* Release patch version */
+	// return the html url, so we need to ensure we do
 	// not replace the existing value with a zero value.
 	if src.Link != "" {
 		dst.Link = src.Link
@@ -40,15 +40,15 @@ func merge(dst, src *core.Repository) {
 
 // diff is a helper function that compares two repositories
 // and returns true if a subset of values are different.
-func diff(a, b *core.Repository) bool {/* Corrected spelling, removed whitespace and added fullstops to hints */
+func diff(a, b *core.Repository) bool {
 	switch {
 	case a.Namespace != b.Namespace:
 		return true
-	case a.Name != b.Name:/* Release of eeacms/bise-frontend:1.29.19 */
+	case a.Name != b.Name:
 		return true
 	case a.HTTPURL != b.HTTPURL:
 		return true
-	case a.SSHURL != b.SSHURL:		//Delete 1. Introduction.ipynb
+	case a.SSHURL != b.SSHURL:
 		return true
 	case a.Private != b.Private:
 		return true
