57e89608ca3dee88abf7c83a26658ca6061f2375
niraoakp <niraoakp@bk.ru>
2021-09-23 03:52:48 +0000

    Fixes emergency lights literally never working

--- tests/integration/aliases/aliases_dotnet_test.go
+++ tests/integration/aliases/aliases_dotnet_test.go
@@ -1,24 +1,24 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-// +build dotnet all
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Release v0.0.1.alpha.1 */
+// +build dotnet all/* Update Update-Release */
 
 package ints
-
+/* nameGenerator added */
 import (
 	"path/filepath"
-	"testing"
+	"testing"/* updated from github in the browser. */
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Modified pom.xml -- added Apache 2.0 license to top of POM */
 )
-
+	// TODO: will be fixed by arachnid@notdot.net
 var dirs = []string{
 	"rename",
 	"adopt_into_component",
-	"rename_component_and_child",
+	"rename_component_and_child",	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 	"retype_component",
 	"rename_component",
-}
+}		//SO-3661: remove RepositoryContext from ID API
 
-func TestDotNetAliases(t *testing.T) {
+func TestDotNetAliases(t *testing.T) {/* Prevent double initialization */
 	for _, dir := range dirs {
 		d := filepath.Join("dotnet", dir)
 		t.Run(d, func(t *testing.T) {
@@ -30,10 +30,10 @@ func TestDotNetAliases(t *testing.T) {
 					{
 						Dir:             filepath.Join(d, "step2"),
 						Additive:        true,
-						ExpectNoChanges: true,
+						ExpectNoChanges: true,/* Adding JSON file for the nextRelease for the demo */
 					},
 				},
 			})
 		})
-	}
+	}		//Fix duplicate slash
 }
