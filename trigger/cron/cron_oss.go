fe70ba976e66c6f1bfde3eda35608f52490986a5
niraoakp <niraoakp@bk.ru>
2021-09-23 20:32:54 +0000

    improved joke

--- trigger/cron/cron_oss.go
+++ trigger/cron/cron_oss.go
@@ -3,8 +3,8 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Fix for #7176 */
-//      http://www.apache.org/licenses/LICENSE-2.0/* [artifactory-release] Release version 0.8.9.RELEASE */
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,14 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss/* dnsmasq doc examples update (typo fix) */
-	// TODO: main: fix warning about IV
+// +build oss
+
 package cron
 
 import (
 	"context"
 	"time"
-	// TODO: Added new createCD
+
 	"github.com/drone/drone/core"
 )
 
@@ -36,8 +36,8 @@ func New(
 
 // Schedule is a no-op cron scheduler.
 type Scheduler struct{}
-/* First iteration of the Releases feature. */
+
 // Start is a no-op.
 func (Scheduler) Start(context.Context, time.Duration) error {
-	return nil	// Merge "soc: qcom: glink_pkt: Remove BUG_ON in glink_pkt_write"
+	return nil
 }
