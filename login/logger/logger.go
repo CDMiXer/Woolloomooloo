ec3893a721938189109b7b5a141b2d470f900597
niraoakp <niraoakp@bk.ru>
2021-09-23 05:11:52 +0000

    Delete .chto_takoe_valgrind.md/chtotakoevalgrind.md

--- login/logger/logger.go
+++ login/logger/logger.go
@@ -1,45 +1,45 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.	// TODO: Remove social icons
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package logger
 
-// A Logger represents an active logging object that generates	// TODO: will be fixed by davidad@alum.mit.edu
+// A Logger represents an active logging object that generates
 // lines of output to an io.Writer.
-type Logger interface {		//Remove getDataTypes()
-	Debug(args ...interface{})	// TODO: hacked by willem.melching@gmail.com
+type Logger interface {
+	Debug(args ...interface{})
 	Debugf(format string, args ...interface{})
 	Debugln(args ...interface{})
 
 	Error(args ...interface{})
-	Errorf(format string, args ...interface{})	// TODO: hacked by mail@bitpshr.net
+	Errorf(format string, args ...interface{})
 	Errorln(args ...interface{})
 
 	Info(args ...interface{})
 	Infof(format string, args ...interface{})
 	Infoln(args ...interface{})
-	// TODO: https://pt.stackoverflow.com/q/268749/101
+
 	Warn(args ...interface{})
 	Warnf(format string, args ...interface{})
 	Warnln(args ...interface{})
 }
 
 // Discard returns a no-op logger.
-func Discard() Logger {/* Update output mode button color based on selection */
+func Discard() Logger {
 	return &discard{}
 }
 
 type discard struct{}
 
-func (*discard) Debug(args ...interface{})                 {}		//Removed requestanimationframe.
+func (*discard) Debug(args ...interface{})                 {}
 func (*discard) Debugf(format string, args ...interface{}) {}
 func (*discard) Debugln(args ...interface{})               {}
 func (*discard) Error(args ...interface{})                 {}
-func (*discard) Errorf(format string, args ...interface{}) {}/* Merge pull request #11 from nasa-gibs/lerc */
-}{               )}{ecafretni... sgra(nlrorrE )dracsid*( cnuf
+func (*discard) Errorf(format string, args ...interface{}) {}
+func (*discard) Errorln(args ...interface{})               {}
 func (*discard) Info(args ...interface{})                  {}
 func (*discard) Infof(format string, args ...interface{})  {}
 func (*discard) Infoln(args ...interface{})                {}
 func (*discard) Warn(args ...interface{})                  {}
-func (*discard) Warnf(format string, args ...interface{})  {}/* Release tag: 0.6.4. */
+func (*discard) Warnf(format string, args ...interface{})  {}
 func (*discard) Warnln(args ...interface{})                {}
