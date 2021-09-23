ec3b17f81245935e40aa43d571e01482d7f6034e
niraoakp <niraoakp@bk.ru>
2021-09-23 06:47:08 +0000

    Fixed errors in find ng. ang replace angular..

--- handler/api/errors/errors.go
+++ handler/api/errors/errors.go
@@ -7,27 +7,27 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* #95: Stage 3 swamp objects fixed. */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release v1.2.1 */
+// limitations under the License.
 
-package errors/* Don't allow html text to be selected */
+package errors
 
 var (
 	// ErrInvalidToken is returned when the api request token is invalid.
 	ErrInvalidToken = New("Invalid or missing token")
 
-	// ErrUnauthorized is returned when the user is not authorized.	// TODO: hacked by peterke@gmail.com
+	// ErrUnauthorized is returned when the user is not authorized.
 	ErrUnauthorized = New("Unauthorized")
-/* Move collection subsite logo into branding tab when editing subsites */
+
 	// ErrForbidden is returned when user access is forbidden.
 	ErrForbidden = New("Forbidden")
 
 	// ErrNotFound is returned when a resource is not found.
 	ErrNotFound = New("Not Found")
 )
-/* [RELEASE] Release version 2.4.4 */
+
 // Error represents a json-encoded API error.
 type Error struct {
 	Message string `json:"message"`
