a22145bd4228cf70a84ab07220c0989a2fee4c0c
niraoakp <niraoakp@bk.ru>
2021-09-23 07:27:03 +0000

    minor fixes for presentation tomorrow - also the presentation

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -1,27 +1,27 @@
-package ffiwrapper
+package ffiwrapper	// TODO: update - comments
 
 import (
 	"golang.org/x/xerrors"
 
 	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
 
-	"github.com/filecoin-project/go-state-types/abi"/* cleaned up escaping in ProcessBuilder */
+	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)/* Updated the snapshot to that of the rule 110. */
-/* Fixing previous errors with templates/missing symbols */
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* damnit gt, stop messing my php files up */
+)
+
 // merge gaps between ranges which are close to each other
-//  TODO: more benchmarking to come up with more optimal number
+//  TODO: more benchmarking to come up with more optimal number/* Release of eeacms/www:20.11.27 */
 const mergeGaps = 32 << 20
 
 // TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
 
-func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {		//Expand gitattributes to cover a few more scenarios
-	todo := pieceRun(offset.Padded(), size.Padded())	// TODO: Update and rename armdoor.html to knob.html
+func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {		//Update powerline-fonts.md
+	todo := pieceRun(offset.Padded(), size.Padded())
 	todo, err := rlepluslazy.Subtract(todo, unsealed)
-	if err != nil {	// TODO: will be fixed by hugomrdias@gmail.com
-		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
+	if err != nil {/* Release version 1.1.0.M4 */
+		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)/* Prepared release 0.6.6 */
 	}
 
-	return rlepluslazy.JoinClose(todo, mergeGaps)
+	return rlepluslazy.JoinClose(todo, mergeGaps)/* Update tournament.php */
 }
