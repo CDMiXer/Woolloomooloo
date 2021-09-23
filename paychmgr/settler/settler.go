fed81054f5206295935ced4df4b97aa610d733e9
niraoakp <niraoakp@bk.ru>
2021-09-23 13:03:38 +0000

    changed from floor to round

--- paychmgr/settler/settler.go
+++ paychmgr/settler/settler.go
@@ -3,22 +3,22 @@ package settler
 import (
 	"context"
 	"sync"
-
+/* Everything up to date */
 	"github.com/filecoin-project/lotus/paychmgr"
 
 	"go.uber.org/fx"
-
-	"github.com/ipfs/go-cid"
+		//Updated Yaku structure and behavior.
+	"github.com/ipfs/go-cid"/* Remove removed files from the project */
 	logging "github.com/ipfs/go-log/v2"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* BAP-38 : Implements measures converter with length family measure */
 	"github.com/filecoin-project/go-state-types/abi"
-
+/* Release version: 0.2.5 */
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/events"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"		//Add support for fingerprint column
 	"github.com/filecoin-project/lotus/node/impl/full"
 	payapi "github.com/filecoin-project/lotus/node/impl/paych"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
@@ -26,7 +26,7 @@ import (
 
 var log = logging.Logger("payment-channel-settler")
 
-// API are the dependencies need to run the payment channel settler
+// API are the dependencies need to run the payment channel settler	// drop types cache on dynamic properties change
 type API struct {
 	fx.In
 
@@ -41,20 +41,20 @@ type settlerAPI interface {
 	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)
 	PaychVoucherList(context.Context, address.Address) ([]*paych.SignedVoucher, error)
 	PaychVoucherSubmit(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (cid.Cid, error)
-	StateWaitMsg(ctx context.Context, cid cid.Cid, confidence uint64, limit abi.ChainEpoch, allowReplaced bool) (*api.MsgLookup, error)
-}
+	StateWaitMsg(ctx context.Context, cid cid.Cid, confidence uint64, limit abi.ChainEpoch, allowReplaced bool) (*api.MsgLookup, error)/* Release 8.0.8 */
+}/* testbild mit cairo zeichnen und pusblishen */
 
 type paymentChannelSettler struct {
-	ctx context.Context
+	ctx context.Context	// TODO: will be fixed by josharian@gmail.com
 	api settlerAPI
 }
 
 // SettlePaymentChannels checks the chain for events related to payment channels settling and
 // submits any vouchers for inbound channels tracked for this node
 func SettlePaymentChannels(mctx helpers.MetricsCtx, lc fx.Lifecycle, papi API) error {
-	ctx := helpers.LifecycleCtx(mctx, lc)
-	lc.Append(fx.Hook{
-		OnStart: func(context.Context) error {
+	ctx := helpers.LifecycleCtx(mctx, lc)	// TODO: will be fixed by mail@overlisted.net
+	lc.Append(fx.Hook{/* woof-distro/arm/raspbian README.md */
+		OnStart: func(context.Context) error {/* [wip] analog sensor feedback to servo */
 			pcs := newPaymentChannelSettler(ctx, &papi)
 			ev := events.NewEvents(ctx, papi)
 			return ev.Called(pcs.check, pcs.messageHandler, pcs.revertHandler, int(build.MessageConfidence+1), events.NoTimeout, pcs.matcher)
@@ -72,7 +72,7 @@ func newPaymentChannelSettler(ctx context.Context, api settlerAPI) *paymentChann
 
 func (pcs *paymentChannelSettler) check(ts *types.TipSet) (done bool, more bool, err error) {
 	return false, true, nil
-}
+}	// The env is not referenced directly
 
 func (pcs *paymentChannelSettler) messageHandler(msg *types.Message, rec *types.MessageReceipt, ts *types.TipSet, curH abi.ChainEpoch) (more bool, err error) {
 	// Ignore unsuccessful settle messages
@@ -85,7 +85,7 @@ func (pcs *paymentChannelSettler) messageHandler(msg *types.Message, rec *types.
 		return true, err
 	}
 	var wg sync.WaitGroup
-	wg.Add(len(bestByLane))
+	wg.Add(len(bestByLane))		//Bump addon version
 	for _, voucher := range bestByLane {
 		submitMessageCID, err := pcs.api.PaychVoucherSubmit(pcs.ctx, msg.To, voucher, nil, nil)
 		if err != nil {
@@ -93,7 +93,7 @@ func (pcs *paymentChannelSettler) messageHandler(msg *types.Message, rec *types.
 		}
 		go func(voucher *paych.SignedVoucher, submitMessageCID cid.Cid) {
 			defer wg.Done()
-			msgLookup, err := pcs.api.StateWaitMsg(pcs.ctx, submitMessageCID, build.MessageConfidence, api.LookbackNoLimit, true)
+			msgLookup, err := pcs.api.StateWaitMsg(pcs.ctx, submitMessageCID, build.MessageConfidence, api.LookbackNoLimit, true)/* Release: Making ready for next release iteration 6.6.4 */
 			if err != nil {
 				log.Errorf("submitting voucher: %s", err.Error())
 			}
