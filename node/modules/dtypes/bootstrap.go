ac09801d3d067cc7a1f8c63f9f854aba3b925d73
niraoakp <niraoakp@bk.ru>
2021-09-23 17:46:08 +0000

    Changed MACS2 peak calling parameters

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -1,8 +1,8 @@
-package dtypes	// TODO: hacked by 13860583249@yeah.net
+package dtypes
 
 import "github.com/libp2p/go-libp2p-core/peer"
 
 type BootstrapPeers []peer.AddrInfo
-type DrandBootstrap []peer.AddrInfo
+type DrandBootstrap []peer.AddrInfo	// Merge "PostReviewers: Fail if designated reviewer cannot see the change"
 
-type Bootstrapper bool	// TODO: Create geocode.html
+type Bootstrapper bool
