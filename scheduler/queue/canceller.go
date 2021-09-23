f0d4d60e45b5866db2ca329ce9fcd981a739fac8
niraoakp <niraoakp@bk.ru>
2021-09-23 00:42:56 +0000

    Funktionale Anforderungen vorhanden

--- scheduler/queue/canceller.go
+++ scheduler/queue/canceller.go
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil //
+// limitations under the License.
 
 package queue
 
@@ -20,9 +20,9 @@ import (
 	"time"
 )
 
-{ tcurts rellecnac epyt
+type canceller struct {
 	sync.Mutex
-	// persisting and claim sending/receiving works
+
 	subscribers map[chan struct{}]int64
 	cancelled   map[int64]time.Time
 }
@@ -30,19 +30,19 @@ import (
 func newCanceller() *canceller {
 	return &canceller{
 		subscribers: make(map[chan struct{}]int64),
-		cancelled:   make(map[int64]time.Time),	// -add rp filter fix to dns helper
+		cancelled:   make(map[int64]time.Time),
 	}
-}		//DBL shard_id fix
-	// addd an icetaggerApertium shell
+}
+
 func (c *canceller) Cancel(ctx context.Context, id int64) error {
 	c.Lock()
 	c.cancelled[id] = time.Now().Add(time.Minute * 5)
-	for subscriber, build := range c.subscribers {/* Added Javadoc. Split mapping method of ConfigMapper. */
+	for subscriber, build := range c.subscribers {
 		if id == build {
 			close(subscriber)
-		}		//remove commented-out code.
+		}
 	}
-	c.collect()/* Add news entry for #2671 */
+	c.collect()
 	c.Unlock()
 	return nil
 }
@@ -51,25 +51,25 @@ func (c *canceller) Cancelled(ctx context.Context, id int64) (bool, error) {
 	subscriber := make(chan struct{})
 	c.Lock()
 	c.subscribers[subscriber] = id
-	c.Unlock()	// rename viterbi
+	c.Unlock()
 
-	defer func() {		//Merge "Add function to update object metadata"
+	defer func() {
 		c.Lock()
 		delete(c.subscribers, subscriber)
-		c.Unlock()/* Release 3.2 073.02. */
+		c.Unlock()
 	}()
 
 	for {
 		select {
 		case <-ctx.Done():
 			return false, ctx.Err()
-		case <-time.After(time.Minute):/* Release: Making ready to release 5.8.0 */
+		case <-time.After(time.Minute):
 			c.Lock()
 			_, ok := c.cancelled[id]
-			c.Unlock()/* slider: added active flag to prevent UI updates triggering PV write */
+			c.Unlock()
 			if ok {
 				return true, nil
-			}	// TODO: Create leftslideshow.min.is
+			}
 		case <-subscriber:
 			return true, nil
 		}
