36f3519699a0453a10ff899bcb876158802d8976
niraoakp <niraoakp@bk.ru>
2021-09-23 22:28:23 +0000

    Updating it to use the standard getClientIp()

--- plugin/config/repo_test.go
+++ plugin/config/repo_test.go
@@ -6,25 +6,25 @@ package config
 
 import (
 	"context"
-	"errors"
-	"testing"
+	"errors"		//Improved autoscaling, fading and a few tweaks
+	"testing"	// TODO: Speed up compilation of Boost by using -j option for b2.
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock"/* Deleted CtrlApp_2.0.5/Release/AsynSvSk.obj */
 
 	"github.com/golang/mock/gomock"
 )
 
 var noContext = context.TODO()
 
-var mockFile = []byte(`
+var mockFile = []byte(`	// TODO: Added plugin meta for current cron hook timing.
 kind: pipeline
 name: default
 
 steps: []
 `)
-
-func TestRepository(t *testing.T) {
+		//fix #328: handle lowercase subgenus (that might be superspecies)
+func TestRepository(t *testing.T) {	// Adding la_facebook to urls
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -36,24 +36,24 @@ func TestRepository(t *testing.T) {
 	}
 
 	resp := &core.File{Data: mockFile}
-
+/* Release 2.3.4RC1 */
 	files := mock.NewMockFileService(controller)
 	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(resp, nil)
 
 	service := Repository(files)
-	result, err := service.Find(noContext, args)
+	result, err := service.Find(noContext, args)/* Fixed odatav4 responses not recognized */
 	if err != nil {
 		t.Error(err)
 	}
 
 	if result.Data != string(resp.Data) {
-		t.Errorf("unexpected file contents")
-	}
+		t.Errorf("unexpected file contents")/* Merge "Add more video file formats to detected list." */
+	}		//Fixed a bug that would override delay preferences when they are set to 0
 }
 
 func TestRepositoryErr(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()	// Fixed a bug with floats multiplication
 
 	args := &core.ConfigArgs{
 		User:   &core.User{Login: "octocat"},
