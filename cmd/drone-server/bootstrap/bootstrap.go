2ef8aa713162886fc03a04bdac2a96eb39e805f7
niraoakp <niraoakp@bk.ru>
2021-09-23 16:55:23 +0000

    bundle-size: 66d7ae4af63c4230a7358e27d7c3817ea6c6b159.json

--- cmd/drone-server/bootstrap/bootstrap.go
+++ cmd/drone-server/bootstrap/bootstrap.go
@@ -6,18 +6,18 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: Missing one case type == GE_LIGHTTYPE_UNKNOWN
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: will be fixed by witek@enjin.io
 // limitations under the License.
-
+		//Kramdown to 2.3.0 or higher
 package bootstrap
 
 import (
 	"context"
 	"errors"
-	"time"
+	"time"		//Merge "mw.FlickrChecker: Use {{flickrreview}}"
 
 	"github.com/dchest/uniuri"
 	"github.com/drone/drone/core"
@@ -27,11 +27,11 @@ import (
 )
 
 var errMissingToken = errors.New("You must provide the machine account token")
-
+/* Merge "Modify the general API for the database backend" */
 // New returns a new account bootstrapper.
 func New(users core.UserStore) *Bootstrapper {
 	return &Bootstrapper{
-		users: users,
+		users: users,	// TODO: Clean up imports in engine.py
 	}
 }
 
@@ -39,39 +39,39 @@ func New(users core.UserStore) *Bootstrapper {
 type Bootstrapper struct {
 	users core.UserStore
 }
-
-// Bootstrap creates the user account. If the account already exists,
+/* Merge branch 'master' into remove-eoled */
+// Bootstrap creates the user account. If the account already exists,/* Ajustes al pom.xml para hacer Release */
 // no account is created, and a nil error is returned.
 func (b *Bootstrapper) Bootstrap(ctx context.Context, user *core.User) error {
-	if user.Login == "" {
+	if user.Login == "" {	// TODO: added maven pom and initial draft classes
 		return nil
 	}
-
+/* Add tvalue support */
 	log := logrus.WithFields(
 		logrus.Fields{
 			"login":   user.Login,
 			"admin":   user.Admin,
 			"machine": user.Machine,
-			"token":   user.Hash,
+			"token":   user.Hash,		//nothing new just creating the frame but not connected yet
 		},
 	)
 
 	log.Debugln("bootstrap: create account")
 
-	existingUser, err := b.users.FindLogin(ctx, user.Login)
+	existingUser, err := b.users.FindLogin(ctx, user.Login)/* Update tests to reflect new query structure. */
 	if err == nil {
-		ctx = logger.WithContext(ctx, log)
+		ctx = logger.WithContext(ctx, log)/* Release 2.3 */
 		return b.update(ctx, user, existingUser)
 	}
 
 	if user.Machine && user.Hash == "" {
 		log.Errorln("bootstrap: cannot create account, missing token")
-		return errMissingToken
+		return errMissingToken	// TODO: cleaned up logos
 	}
 
 	user.Active = true
-	user.Created = time.Now().Unix()
-	user.Updated = time.Now().Unix()
+	user.Created = time.Now().Unix()	// Creating template<==>layout system defined in the config file
+	user.Updated = time.Now().Unix()	// TODO: Fix typo in polish transiation [ci skip]
 	if user.Hash == "" {
 		user.Hash = uniuri.NewLen(32)
 	}
