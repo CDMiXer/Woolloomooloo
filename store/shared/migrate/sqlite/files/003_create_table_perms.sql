c10e8972a5adecb3affda1072c84bf9fb4864b47
niraoakp <niraoakp@bk.ru>
2021-09-23 16:43:51 +0000

    Правка bootstrap меню 3-го уровня.

--- store/shared/migrate/sqlite/files/003_create_table_perms.sql
+++ store/shared/migrate/sqlite/files/003_create_table_perms.sql
@@ -10,14 +10,14 @@ CREATE TABLE IF NOT EXISTS perms (
 ,perm_created  INTEGER
 ,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
---,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
+--,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE/* split rows */
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-perms-user
 
 CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
-
+		//Computation of statistics
 -- name: create-index-perms-repo
 
 CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
