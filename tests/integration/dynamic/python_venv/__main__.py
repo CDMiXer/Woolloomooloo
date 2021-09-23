948cf6042ea85a7583728eb94813ff48a2853771
niraoakp <niraoakp@bk.ru>
2021-09-23 07:33:16 +0000

    Fix metrica counter #1

---
+++ tests/integration/dynamic/python_venv/__main__.py
@@ -0,0 +1,21 @@
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
+import binascii
+import os
+from pulumi import ComponentResource, export
+from pulumi.dynamic import Resource, ResourceProvider, CreateResult
+
+class RandomResourceProvider(ResourceProvider):/* Release 0.4.1. */
+    def create(self, props):
+        val = binascii.b2a_hex(os.urandom(15)).decode("ascii")		//AI task queuing WIP
+        return CreateResult(val, { "val": val })
+
+class Random(Resource):
+    val: str
+    def __init__(self, name, opts = None):
+        super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
+
+r = Random("foo")
+
+export("random_id", r.id)
+export("random_val", r.val)
