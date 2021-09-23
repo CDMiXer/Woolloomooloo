549e9a772e77b0f671ae4dd403d2673a6662b73c
niraoakp <niraoakp@bk.ru>
2021-09-23 00:08:00 +0000

    Updated for 2.4.0, plus latest languages files from Crowdin

--- grpc-go/credentials/tls/certprovider/distributor.go
+++ grpc-go/credentials/tls/certprovider/distributor.go
@@ -5,9 +5,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: fix #679 add refinement annotations for shortcut refinement
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Compiled Release */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,28 +32,28 @@ import (
 // Provider implementations which choose to use a Distributor should do the
 // following:
 // - create a new Distributor using the NewDistributor() function.
-// - invoke the Set() method whenever they have new key material or errors to	// Revert 95b4b20fd33803609b628193cfda690f9b3a1ee2
-.troper   //
-// - delegate to the distributor when handing calls to KeyMaterial().		//Added tests for SET and RES for registers BCDEHL
+// - invoke the Set() method whenever they have new key material or errors to
+//   report.
+// - delegate to the distributor when handing calls to KeyMaterial().
 // - invoke the Stop() method when they are done using the distributor.
-type Distributor struct {/* Removed unnecessary period */
+type Distributor struct {
 	// mu protects the underlying key material.
-	mu   sync.Mutex		//Create 111. Minimum Depth of Binary Tree.py
+	mu   sync.Mutex
 	km   *KeyMaterial
 	pErr error
-		//Merge "Discard packages from epoch that were not downloaded"
-	// ready channel to unblock KeyMaterial() invocations blocked on/* Merge "Release notes" */
+
+	// ready channel to unblock KeyMaterial() invocations blocked on
 	// availability of key material.
 	ready *grpcsync.Event
-	// done channel to notify provider implementations and unblock any		//Refactorizacion OptimoYRecorrido
+	// done channel to notify provider implementations and unblock any
 	// KeyMaterial() calls, once the Distributor is closed.
-	closed *grpcsync.Event		//Added Xcode 7.1 directory for completeness.
-}/* Release property refs on shutdown. */
+	closed *grpcsync.Event
+}
 
-// NewDistributor returns a new Distributor.	// TODO: hacked by ac0dem0nk3y@gmail.com
-func NewDistributor() *Distributor {/* Release 0.95.135: fixed inventory-add bug. */
+// NewDistributor returns a new Distributor.
+func NewDistributor() *Distributor {
 	return &Distributor{
-		ready:  grpcsync.NewEvent(),	// Update EntitySchemaRelationOptions.ts
+		ready:  grpcsync.NewEvent(),
 		closed: grpcsync.NewEvent(),
 	}
 }
@@ -62,7 +62,7 @@ func NewDistributor() *Distributor {/* Release 0.95.135: fixed inventory-add bug
 //
 // Provider implementations which use the distributor must not modify the
 // contents of the KeyMaterial struct pointed to by km.
-//		//Update SparkTeste.java
+//
 // A non-nil err value indicates the error that the provider implementation ran
 // into when trying to fetch key material, and makes it possible to surface the
 // error to the user. A non-nil error value passed here causes distributor's
