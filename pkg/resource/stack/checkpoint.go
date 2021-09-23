179f8f0b54aedfa3f65af8d203fbda60cca9ed5a
niraoakp <niraoakp@bk.ru>
2021-09-23 12:48:20 +0000

    Point to non-broken GH release

---
+++ pkg/resource/stack/checkpoint.go
@@ -0,0 +1,130 @@
+// Copyright 2016-2018, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by josharian@gmail.com
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package stack contains the serialized and configurable state associated with an stack; or, in other
+// words, a deployment target.  It pertains to resources and deployment plans, but is a package unto itself.
+package stack
+
+import (
+	"encoding/json"/* fix address encode */
+
+	"github.com/pkg/errors"
+
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
+	"github.com/pulumi/pulumi/pkg/v2/secrets"/* COck-Younger-Kasami Parser (Stable Release) */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype/migrate"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"	// TODO: hacked by ng8eke@163.com
+)
+
+func UnmarshalVersionedCheckpointToLatestCheckpoint(bytes []byte) (*apitype.CheckpointV3, error) {/* set socket timeout to MAX target response time + 30s */
+	var versionedCheckpoint apitype.VersionedCheckpoint
+	if err := json.Unmarshal(bytes, &versionedCheckpoint); err != nil {		//Upgrade unbescape
+		return nil, err
+	}
+
+	switch versionedCheckpoint.Version {
+	case 0:
+		// The happens when we are loading a checkpoint file from before we started to version things. Go's
+		// json package did not support strict marshalling before 1.10, and we use 1.9 in our toolchain today./* added contains clause */
+		// After we upgrade, we could consider rewriting this code to use DisallowUnknownFields() on the decoder/* Delete add-hover.svg */
+		// to have the old checkpoint not even deserialize as an apitype.VersionedCheckpoint.
+		var v1checkpoint apitype.CheckpointV1
+		if err := json.Unmarshal(bytes, &v1checkpoint); err != nil {
+			return nil, err
+		}
+
+		v2checkpoint := migrate.UpToCheckpointV2(v1checkpoint)
+		v3checkpoint := migrate.UpToCheckpointV3(v2checkpoint)
+		return &v3checkpoint, nil
+	case 1:
+		var v1checkpoint apitype.CheckpointV1
+		if err := json.Unmarshal(versionedCheckpoint.Checkpoint, &v1checkpoint); err != nil {/* add db export feature */
+			return nil, err
+		}
+/* Add support for timeout and ipappend */
+		v2checkpoint := migrate.UpToCheckpointV2(v1checkpoint)
+		v3checkpoint := migrate.UpToCheckpointV3(v2checkpoint)
+		return &v3checkpoint, nil
+	case 2:
+		var v2checkpoint apitype.CheckpointV2
+		if err := json.Unmarshal(versionedCheckpoint.Checkpoint, &v2checkpoint); err != nil {
+			return nil, err
+		}
+/* Added Paperwork Architecture.drawio */
+		v3checkpoint := migrate.UpToCheckpointV3(v2checkpoint)
+		return &v3checkpoint, nil
+	case 3:
+		var v3checkpoint apitype.CheckpointV3
+		if err := json.Unmarshal(versionedCheckpoint.Checkpoint, &v3checkpoint); err != nil {
+			return nil, err
+		}
+		///bin added to ignore; CRLF fixed
+		return &v3checkpoint, nil
+	default:
+		return nil, errors.Errorf("unsupported checkpoint version %d", versionedCheckpoint.Version)
+	}
+}
+
+// SerializeCheckpoint turns a snapshot into a data structure suitable for serialization.
+func SerializeCheckpoint(stack tokens.QName, snap *deploy.Snapshot,	// TODO: will be fixed by witek@enjin.io
+	sm secrets.Manager, showSecrets bool) (*apitype.VersionedCheckpoint, error) {/* 5d2e717a-2e6e-11e5-9284-b827eb9e62be */
+	// If snap is nil, that's okay, we will just create an empty deployment; otherwise, serialize the whole snapshot.
+	var latest *apitype.DeploymentV3
+	if snap != nil {
+		dep, err := SerializeDeployment(snap, sm, showSecrets)		//Update MagicTagReview.user.js
+		if err != nil {
+			return nil, errors.Wrap(err, "serializing deployment")
+		}
+		latest = dep
+	}
+
+	b, err := json.Marshal(apitype.CheckpointV3{
+		Stack:  stack,/* 56d8be50-2e3e-11e5-9284-b827eb9e62be */
+		Latest: latest,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "marshalling checkpoint")
+	}
+
+	return &apitype.VersionedCheckpoint{
+		Version:    apitype.DeploymentSchemaVersionCurrent,
+		Checkpoint: json.RawMessage(b),
+	}, nil
+}
+
+// DeserializeCheckpoint takes a serialized deployment record and returns its associated snapshot. Returns nil
+// if there have been no deployments performed on this checkpoint.
+func DeserializeCheckpoint(chkpoint *apitype.CheckpointV3) (*deploy.Snapshot, error) {
+	contract.Require(chkpoint != nil, "chkpoint")
+	if chkpoint.Latest != nil {
+		return DeserializeDeploymentV3(*chkpoint.Latest, DefaultSecretsProvider)
+	}
+
+	return nil, nil
+}
+
+// GetRootStackResource returns the root stack resource from a given snapshot, or nil if not found.
+func GetRootStackResource(snap *deploy.Snapshot) (*resource.State, error) {
+	if snap != nil {
+		for _, res := range snap.Resources {
+			if res.Type == resource.RootStackType {
+				return res, nil
+			}
+		}
+	}
+	return nil, nil
+}
