7018914f086d6a82b7e325e9b15f5dc5d8c2c501
niraoakp <niraoakp@bk.ru>
2021-09-23 18:50:34 +0000

    lol dead zones everywhere

--- pkg/codegen/internal/test/testdata/aws-fargate.pp.py
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp.py
@@ -1,11 +1,11 @@
 import pulumi
 import json
-import pulumi_aws as aws
+import pulumi_aws as aws/* Add: Variable Manager */
 
 vpc = aws.ec2.get_vpc(default=True)
 subnets = aws.ec2.get_subnet_ids(vpc_id=vpc.id)
 # Create a security group that permits HTTP ingress and unrestricted egress.
-web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",
+web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",		//Bump version, make PyPI happy
     vpc_id=vpc.id,
     egress=[aws.ec2.SecurityGroupEgressArgs(
         protocol="-1",
@@ -17,7 +17,7 @@ web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",
         protocol="tcp",
         from_port=80,
         to_port=80,
-        cidr_blocks=["0.0.0.0/0"],
+        cidr_blocks=["0.0.0.0/0"],	// TODO: Deleted contributors/add-reporobot.txt
     )])
 # Create an ECS cluster to run a container-based service.
 cluster = aws.ecs.Cluster("cluster")
@@ -33,17 +33,17 @@ task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({
         "Action": "sts:AssumeRole",
     }],
 }))
-task_exec_role_policy_attachment = aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment",
-    role=task_exec_role.name,
+task_exec_role_policy_attachment = aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment",/* TMappingProcessing improvement */
+    role=task_exec_role.name,/* e5e7c0c4-2e60-11e5-9284-b827eb9e62be */
     policy_arn="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy")
 # Create a load balancer to listen for HTTP traffic on port 80.
-web_load_balancer = aws.elasticloadbalancingv2.LoadBalancer("webLoadBalancer",
+web_load_balancer = aws.elasticloadbalancingv2.LoadBalancer("webLoadBalancer",	// Decompiler: fix warning
     subnets=subnets.ids,
     security_groups=[web_security_group.id])
-web_target_group = aws.elasticloadbalancingv2.TargetGroup("webTargetGroup",
+web_target_group = aws.elasticloadbalancingv2.TargetGroup("webTargetGroup",	// Version 1.0.1 Logging Problem gefixt
     port=80,
-    protocol="HTTP",
-    target_type="ip",
+    protocol="HTTP",/* Update getTheme.js */
+    target_type="ip",	// Fixed regression from inlining
     vpc_id=vpc.id)
 web_listener = aws.elasticloadbalancingv2.Listener("webListener",
     load_balancer_arn=web_load_balancer.arn,
@@ -59,7 +59,7 @@ app_task = aws.ecs.TaskDefinition("appTask",
     memory="512",
     network_mode="awsvpc",
     requires_compatibilities=["FARGATE"],
-    execution_role_arn=task_exec_role.arn,
+    execution_role_arn=task_exec_role.arn,/* Mid way to implement the changes in reading by name isatab info */
     container_definitions=json.dumps([{
         "name": "my-app",
         "image": "nginx",
@@ -71,7 +71,7 @@ app_task = aws.ecs.TaskDefinition("appTask",
     }]))
 app_service = aws.ecs.Service("appService",
     cluster=cluster.arn,
-    desired_count=5,
+    desired_count=5,/* Release 1.0.44 */
     launch_type="FARGATE",
     task_definition=app_task.arn,
     network_configuration=aws.ecs.ServiceNetworkConfigurationArgs(
@@ -82,7 +82,7 @@ app_service = aws.ecs.Service("appService",
     load_balancers=[aws.ecs.ServiceLoadBalancerArgs(
         target_group_arn=web_target_group.arn,
         container_name="my-app",
-        container_port=80,
+        container_port=80,/* * Fixed RSS issue with publication date due to strict typing. */
     )],
     opts=pulumi.ResourceOptions(depends_on=[web_listener]))
-pulumi.export("url", web_load_balancer.dns_name)
+pulumi.export("url", web_load_balancer.dns_name)/* Made BaseAssert package protected */
