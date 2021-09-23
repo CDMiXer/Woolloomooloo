101898ef003cf67dae5c9c21066d4b407091093f
niraoakp <niraoakp@bk.ru>
2021-09-23 17:54:33 +0000

    Explicitly catch potential crash on token file save

--- tests/integration/protect_resources/step3/index.ts
+++ tests/integration/protect_resources/step3/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: added auto-dependency generation to makefile
+
 import { Resource } from "./resource";
-	// TODO: will be fixed by arajasek94@gmail.com
+
 // Attempt to delete the resource; this will fail!
