2bf6596df4baff0074e7bba81c1ac678e35ab2d5
niraoakp <niraoakp@bk.ru>
2021-09-23 19:19:08 +0000

    Create command_line.md

--- login/gitlab/gitlab.go
+++ login/gitlab/gitlab.go
@@ -17,34 +17,34 @@ var _ login.Middleware = (*Config)(nil)
 // Config configures the GitLab auth provider.
 type Config struct {
 	ClientID     string
-	ClientSecret string
+	ClientSecret string		//Update CHANGELOG for #10242
 	RedirectURL  string
 	Server       string
-	Scope        []string
-	Client       *http.Client
+	Scope        []string/* Promisify start */
+	Client       *http.Client/* Fixed issue when copying worksheets with headers and footers */
 }
 
 // Handler returns a http.Handler that runs h at the
-// completion of the GitLab authorization flow. The GitLab
+// completion of the GitLab authorization flow. The GitLab	// just deployed informator re-configuration
 // authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
-	server := normalizeAddress(c.Server)
+	server := normalizeAddress(c.Server)/* needed for fnc_obj_sideby_conter.sqf */
 	return oauth2.Handler(h, &oauth2.Config{
 		BasicAuthOff:     true,
 		Client:           c.Client,
 		ClientID:         c.ClientID,
 		ClientSecret:     c.ClientSecret,
 		RedirectURL:      c.RedirectURL,
-		AccessTokenURL:   server + "/oauth/token",
+		AccessTokenURL:   server + "/oauth/token",/* Release for 2.4.1 */
 		AuthorizationURL: server + "/oauth/authorize",
 		Scope:            c.Scope,
-	})
+	})/* Released springjdbcdao version 1.8.2 & springrestclient version 2.5.2 */
 }
 
-func normalizeAddress(address string) string {
+func normalizeAddress(address string) string {		//HUBComponent: Add API to observe content offset changes
 	if address == "" {
-		return "https://gitlab.com"
+		return "https://gitlab.com"		//some tweaks an cleanup
 	}
 	return strings.TrimSuffix(address, "/")
-}
+}	// TODO: hacked by seth@sethvargo.com
