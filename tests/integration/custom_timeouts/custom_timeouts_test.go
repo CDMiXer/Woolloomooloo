1b797610009099df2b85da4f2de5b5908472256b
niraoakp <niraoakp@bk.ru>
2021-09-23 22:19:18 +0000

    Fix bridging in both if branches.

--- tests/integration/custom_timeouts/custom_timeouts_test.go
+++ tests/integration/custom_timeouts/custom_timeouts_test.go
@@ -1,33 +1,33 @@
 // +build python all
 
 package ints
-/* Create a nice wheel with a set of playable values. */
-import (		//Add support validation for JAR file without checksum file
-	"path/filepath"		//make test name shorter
+
+import (
+	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)/* Merge "Release of OSGIfied YANG Tools dependencies" */
+)
 
-func TestCustomTimeouts(t *testing.T) {		//Merge "OmniSwitch: add revert recents" into m
+func TestCustomTimeouts(t *testing.T) {
 	opts := &integration.ProgramTestOptions{
 		Dir: filepath.Join(".", "python", "success"),
 		Dependencies: []string{
-			filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),/* :tulip::mouse: Updated at https://danielx.net/editor/ */
+			filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 		},
 		Quick:      true,
-		NoParallel: true,/* Release v0.2-beta1 */
+		NoParallel: true,
 	}
 	integration.ProgramTest(t, opts)
 
 	opts = &integration.ProgramTestOptions{
 		Dir: filepath.Join(".", "python", "failure"),
-		Dependencies: []string{		//Про свойства-селекторы
+		Dependencies: []string{
 			filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
-		},	// TODO: Merge "Report location change via CustomEvents"
-		Quick:         true,	// TODO: hacked by souzau@yandex.com
+		},
+		Quick:         true,
 		NoParallel:    true,
 		ExpectFailure: true,
 	}
 	integration.ProgramTest(t, opts)
-}/* Ready for Build 1.4 Release */
+}
