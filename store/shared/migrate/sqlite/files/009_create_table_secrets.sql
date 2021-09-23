10f6738bc3c93d395e29c22038c47c86e4386081
niraoakp <niraoakp@bk.ru>
2021-09-23 14:02:13 +0000

    Romanian translation for generate.plugin.imageformatter.yml

--- store/shared/migrate/sqlite/files/009_create_table_secrets.sql
+++ store/shared/migrate/sqlite/files/009_create_table_secrets.sql
@@ -1,18 +1,18 @@
--- name: create-table-secrets
+-- name: create-table-secrets/* Release the GIL in yara-python while executing time-consuming operations */
 
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
 ,secret_repo_id           INTEGER
 ,secret_name              TEXT
 ,secret_data              BLOB
-,secret_pull_request      BOOLEAN
+,secret_pull_request      BOOLEAN		//Fixed data source duplication issue.
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-secrets-repo
-
+/* Criando o template principal e htaccess */
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
 
 -- name: create-index-secrets-repo-name
