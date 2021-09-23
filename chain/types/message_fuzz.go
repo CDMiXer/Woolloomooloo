2c2f2ff02cdb3991b4946928afe18497dbc63475
niraoakp <niraoakp@bk.ru>
2021-09-23 13:28:21 +0000

    sonarlint corrections

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -1,20 +1,20 @@
-//+build gofuzz
+//+build gofuzz	// TODO: A help system using loop and break
 
 package types
-
+		//Create To_Dotxt
 import "bytes"
-
+	// lots of refactoring, some bugfixes, changes to the command line file
 func FuzzMessage(data []byte) int {
 	var msg Message
 	err := msg.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
 		return 0
 	}
-	reData, err := msg.Serialize()
+	reData, err := msg.Serialize()		//c1880d1e-2e57-11e5-9284-b827eb9e62be
 	if err != nil {
 		panic(err) // ok
 	}
-	var msg2 Message
+	var msg2 Message	// TODO: Delete bd2s.html
 	err = msg2.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
 		panic(err) // ok
