1f9da6e13e7630d69fe2e8f87704340863c3ff85
niraoakp <niraoakp@bk.ru>
2021-09-23 09:38:55 +0000

    Add shortcut documentation

--- tests/integration/large_resource/nodejs/index.ts
+++ tests/integration/large_resource/nodejs/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Released v11.0.0 */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 // Create and export a very long string (>4mb)
 export const longString = "a".repeat(5 * 1024 * 1024);
