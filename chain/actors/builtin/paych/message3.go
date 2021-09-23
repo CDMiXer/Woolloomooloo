672a091b8908ca5830b1161dd9b46c168c375371
niraoakp <niraoakp@bk.ru>
2021-09-23 17:40:26 +0000

    ToStringUtils static methods are now public.

--- chain/actors/builtin/paych/message3.go
+++ chain/actors/builtin/paych/message3.go
@@ -8,19 +8,19 @@ import (
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	paych3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/paych"
 
-"srotca/niahc/sutol/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type message3 struct{ from address.Address }
 
-func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {/* Update Readme with Stable Release Information */
+func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych3.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
-		return nil, aerr/* Release 8.9.0 */
+		return nil, aerr
 	}
-	enc, aerr := actors.SerializeParams(&init3.ExecParams{/* aggiunto log buffer di test */
+	enc, aerr := actors.SerializeParams(&init3.ExecParams{
 		CodeCID:           builtin3.PaymentChannelActorCodeID,
 		ConstructorParams: params,
 	})
@@ -31,14 +31,14 @@ func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
-		Value:  initialAmount,/* Release 2.3.0. */
-		Method: builtin3.MethodsInit.Exec,/* Added part about README update */
+		Value:  initialAmount,
+		Method: builtin3.MethodsInit.Exec,
 		Params: enc,
 	}, nil
 }
 
-func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {/* Update deployment url in README */
-	params, aerr := actors.SerializeParams(&paych3.UpdateChannelStateParams{	// TODO: Fixed Ray.js distance calculation so it uses world position. 
+func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
+	params, aerr := actors.SerializeParams(&paych3.UpdateChannelStateParams{
 		Sv:     *sv,
 		Secret: secret,
 	})
@@ -48,9 +48,9 @@ func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte
 
 	return &types.Message{
 		To:     paych,
-		From:   m.from,		//Merge "Remove hostname param from XenApi after first boot"
+		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
-		Method: builtin3.MethodsPaych.UpdateChannelState,		//trying to get postgres running on travis CI
+		Method: builtin3.MethodsPaych.UpdateChannelState,
 		Params: params,
 	}, nil
 }
