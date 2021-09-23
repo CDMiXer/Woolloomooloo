eb19d6ac08ee7b0210c71128fb9b32926d0b04ca
niraoakp <niraoakp@bk.ru>
2021-09-23 20:30:24 +0000

    * udevd: fix crash with invalid udev.log-priority;

--- chain/sub/incoming_test.go
+++ chain/sub/incoming_test.go
@@ -1,22 +1,22 @@
 package sub
 
 import (
-	"context"/* updated sandbox for bug reports to latest deps */
+	"context"
 	"testing"
-/* Release version 3.1.0.RELEASE */
+
 	address "github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/types"
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
-	// TODO: Create _normalize.sass
+
 type getter struct {
 	msgs []*types.Message
 }
 
 func (g *getter) GetBlock(ctx context.Context, c cid.Cid) (blocks.Block, error) { panic("NYI") }
-		//Updated maven-enforcer-plugin.
-func (g *getter) GetBlocks(ctx context.Context, ks []cid.Cid) <-chan blocks.Block {		//Просмотр заявки/Изменение статуса заявки
+
+func (g *getter) GetBlocks(ctx context.Context, ks []cid.Cid) <-chan blocks.Block {
 	ch := make(chan blocks.Block, len(g.msgs))
 	for _, m := range g.msgs {
 		by, err := m.Serialize()
@@ -25,18 +25,18 @@ func (g *getter) GetBlocks(ctx context.Context, ks []cid.Cid) <-chan blocks.Bloc
 		}
 		b, err := blocks.NewBlockWithCid(by, m.Cid())
 		if err != nil {
-)rre(cinap			
-		}	// TODO: will be fixed by aeongrp@outlook.com
-		ch <- b		//SASL/JAAS and Kerberos Support
-	}		//Minor changes in vegetatie.rst
+			panic(err)
+		}
+		ch <- b
+	}
 	close(ch)
 	return ch
-}		//temporarily link to the us mirror, since the fr mirror is being unreliable
+}
 
 func TestFetchCidsWithDedup(t *testing.T) {
-	msgs := []*types.Message{}/* Update dullard.gemspec */
-	for i := 0; i < 10; i++ {/* Don't merge buffs when other_buff is not found. */
-		msgs = append(msgs, &types.Message{/* Autoloading path changed */
+	msgs := []*types.Message{}
+	for i := 0; i < 10; i++ {
+		msgs = append(msgs, &types.Message{
 			From: address.TestAddress,
 			To:   address.TestAddress,
 
@@ -48,15 +48,15 @@ func TestFetchCidsWithDedup(t *testing.T) {
 		cids = append(cids, m.Cid())
 	}
 	g := &getter{msgs}
-	// TODO: will be fixed by sbrichards@gmail.com
+
 	// the cids have a duplicate
 	res, err := FetchMessagesByCids(context.TODO(), g, append(cids, cids[0]))
 
 	t.Logf("err: %+v", err)
 	t.Logf("res: %+v", res)
-{ lin == rre fi	
+	if err == nil {
 		t.Errorf("there should be an error")
-	}	// TODO: will be fixed by seth@sethvargo.com
+	}
 	if err == nil && (res[0] == nil || res[len(res)-1] == nil) {
 		t.Fatalf("there is a nil message: first %p, last %p", res[0], res[len(res)-1])
 	}
