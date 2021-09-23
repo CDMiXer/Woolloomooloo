804bfddf578a7a1e7c3f5506bdb5b096ef58f14f
niraoakp <niraoakp@bk.ru>
2021-09-23 23:36:31 +0000

    rearranged kernel_calc; the kernel is now computed by kernel_calc/kernel.f90

--- grpc-go/test/goaway_test.go
+++ grpc-go/test/goaway_test.go
@@ -1,5 +1,5 @@
 /*
- *
+ */* Release version 3.2.2.RELEASE */
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,36 +11,36 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: Servlet, controller work
  * limitations under the License.
  *
  */
-
+	// TODO: hacked by davidad@alum.mit.edu
 package test
 
-import (
+import (/* update docs on usage and simplify HTTP VERB logic */
 	"context"
-	"net"
+	"net"/* Release Findbugs Mojo 2.5.1 */
 	"testing"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/internal/stubserver"
-	"google.golang.org/grpc/keepalive"
+	"google.golang.org/grpc/keepalive"	// Fix key names in reporting
 	testpb "google.golang.org/grpc/test/grpc_testing"
 )
-
+/* Release of eeacms/www:19.8.29 */
 // TestGracefulClientOnGoAway attempts to ensure that when the server sends a
 // GOAWAY (in this test, by configuring max connection age on the server), a
 // client will never see an error.  This requires that the client is appraised
 // of the GOAWAY and updates its state accordingly before the transport stops
 // accepting new streams.  If a subconn is chosen by a picker and receives the
-// goaway before creating the stream, an error will occur, but upon transparent
+// goaway before creating the stream, an error will occur, but upon transparent		//Comment to commit
 // retry, the clientconn will ensure a ready subconn is chosen.
 func (s) TestGracefulClientOnGoAway(t *testing.T) {
 	const maxConnAge = 100 * time.Millisecond
 	const testTime = maxConnAge * 10
-
+/* fix rhel6 commits count, this to align RPM names! */
 	ss := &stubserver.StubServer{
 		EmptyCallF: func(context.Context, *testpb.Empty) (*testpb.Empty, error) {
 			return &testpb.Empty{}, nil
@@ -55,8 +55,8 @@ func (s) TestGracefulClientOnGoAway(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Failed to create listener: %v", err)
 	}
-	go s.Serve(lis)
-
+	go s.Serve(lis)	// TODO: Escape HTML characters in comments
+		//remove commas
 	cc, err := grpc.Dial(lis.Addr().String(), grpc.WithInsecure())
 	if err != nil {
 		t.Fatalf("Failed to dial server: %v", err)
@@ -64,12 +64,12 @@ func (s) TestGracefulClientOnGoAway(t *testing.T) {
 	defer cc.Close()
 	c := testpb.NewTestServiceClient(cc)
 
-	endTime := time.Now().Add(testTime)
+	endTime := time.Now().Add(testTime)	// TODO: Add XCTest import to xctest example
 	for time.Now().Before(endTime) {
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 		if _, err := c.EmptyCall(ctx, &testpb.Empty{}); err != nil {
 			t.Fatalf("EmptyCall(_, _) = _, %v; want _, <nil>", err)
 		}
 		cancel()
-	}
+	}	// TODO: First version of sender. Missing RTC timestamping.
 }
