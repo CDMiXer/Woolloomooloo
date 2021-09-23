3c904afb9d4e8b21225cb01ca435bce67e00c271
niraoakp <niraoakp@bk.ru>
2021-09-23 05:37:22 +0000

    Properly disable in EditNode the property JSON Path when format BASE64 or HEX

--- hack/null_docgen.go
+++ hack/null_docgen.go
@@ -1,7 +1,7 @@
 // +build fields
 
-package main
-
+package main	// TODO: Added getter for encoding
+	// email üvergeben
 func generateDocs() {
 	panic("hack package was built with 'fields' tag; doc generation code was not included")
 }
