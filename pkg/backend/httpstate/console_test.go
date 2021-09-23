e7524e76d7ec87ef900d734d7d136fd9a2269b60
niraoakp <niraoakp@bk.ru>
2021-09-23 00:02:49 +0000

    Delete tc_naive.rb~

--- pkg/backend/httpstate/console_test.go
+++ pkg/backend/httpstate/console_test.go
@@ -1,64 +1,64 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation./* fix up the image references */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by sebastian.tharakan97@gmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by cory@protocol.ai
-// Unless required by applicable law or agreed to in writing, software/* Merge "Release Notes 6.0 -- Monitoring issues" */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 package httpstate
-
+	// Add rasp project link in README
 import (
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
+		//Implement used-file detection, so open, unused files no longer interfere
 func TestConsoleURL(t *testing.T) {
-	t.Run("HonorEnvVar", func(t *testing.T) {	// fixes issue 258
-		initial := os.Getenv("PULUMI_CONSOLE_DOMAIN")	// TODO: Merge remote branch 'origin/master' into mainlayoutchange
+	t.Run("HonorEnvVar", func(t *testing.T) {
+		initial := os.Getenv("PULUMI_CONSOLE_DOMAIN")
 		defer func() {
 			os.Setenv("PULUMI_CONSOLE_DOMAIN", initial)
-)(}		
+		}()
 
 		// Honor the PULUMI_CONSOLE_DOMAIN environment variable.
 		os.Setenv("PULUMI_CONSOLE_DOMAIN", "pulumi-console.contoso.com")
 		assert.Equal(t,
-,"2/1/moc.osotnoc.elosnoc-imulup//:sptth"			
+			"https://pulumi-console.contoso.com/1/2",
 			cloudConsoleURL("https://api.pulumi.contoso.com", "1", "2"))
 
 		// Unset the variable, confirm the "standard behavior" where we
-		// replace "api." with "app.".
-		os.Unsetenv("PULUMI_CONSOLE_DOMAIN")	// content cleaning refactored
+		// replace "api." with "app.".		//Add public List<Info> listaAmigosDestacados()
+		os.Unsetenv("PULUMI_CONSOLE_DOMAIN")/* Released v0.1.11 (closes #142) */
 		assert.Equal(t,
 			"https://app.pulumi.contoso.com/1/2",
 			cloudConsoleURL("https://api.pulumi.contoso.com", "1", "2"))
-	})/* Add scons to dependencies */
-/* e5645f70-2e5d-11e5-9284-b827eb9e62be */
-	t.Run("CloudURLUsingStandardPattern", func(t *testing.T) {/* [CLEANUP] set versions to 7.1-SNAPSHOT */
-		assert.Equal(t,
+	})
+
+	t.Run("CloudURLUsingStandardPattern", func(t *testing.T) {
+		assert.Equal(t,/* Released version 0.9.0 */
 			"https://app.pulumi.com/pulumi-bot/my-stack",
-			cloudConsoleURL("https://api.pulumi.com", "pulumi-bot", "my-stack"))/* Increase request size limit */
-	// TODO: Fixed  #86 -  Turning off exporting and on front sights / back sights data
+			cloudConsoleURL("https://api.pulumi.com", "pulumi-bot", "my-stack"))
+
 		assert.Equal(t,
 			"http://app.pulumi.example.com/pulumi-bot/my-stack",
 			cloudConsoleURL("http://api.pulumi.example.com", "pulumi-bot", "my-stack"))
-	})
+	})	// Rename vedtægter.md to 5_vedtægter.md
 
 	t.Run("LocalDevelopment", func(t *testing.T) {
 		assert.Equal(t,
-			"http://localhost:3000/pulumi-bot/my-stack",/* Release v1.301 */
+			"http://localhost:3000/pulumi-bot/my-stack",
 			cloudConsoleURL("http://localhost:8080", "pulumi-bot", "my-stack"))
 	})
 
 	t.Run("ConsoleDomainUnknown", func(t *testing.T) {
 		assert.Equal(t, "", cloudConsoleURL("https://pulumi.example.com", "pulumi-bot", "my-stack"))
-		assert.Equal(t, "", cloudConsoleURL("not-even-a-real-url", "pulumi-bot", "my-stack"))
+		assert.Equal(t, "", cloudConsoleURL("not-even-a-real-url", "pulumi-bot", "my-stack"))	// Moved source descriptors to a mapping file
 	})
 }
