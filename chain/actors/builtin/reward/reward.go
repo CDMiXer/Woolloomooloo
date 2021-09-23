fbf5b6f2f7f2a012084bb8579546fea8c67adb0d
niraoakp <niraoakp@bk.ru>
2021-09-23 11:52:49 +0000

    Separate out the various index mechanisms for the expanded content tables.

--- chain/actors/builtin/reward/reward.go
+++ chain/actors/builtin/reward/reward.go
@@ -1,6 +1,6 @@
 package reward
 
-import (
+import (/* Append function call for determinant */
 	"github.com/filecoin-project/go-state-types/abi"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	"github.com/ipfs/go-cid"
@@ -11,43 +11,43 @@ import (
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+/* -updated documentation */
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* Added support for large graphs */
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"	// 99ec982e-2e4d-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/lotus/chain/types"
-)
-
+)	// Merge "Validate uuid parameters strictly for create volume API"
+	// google drive load credential
 func init() {
 
 	builtin.RegisterActorState(builtin0.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
 	})
 
-	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load2(store, root)
-	})
+	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {		//Add note about BBB pin map
+		return load2(store, root)	// TODO: will be fixed by aeongrp@outlook.com
+	})		//Merge "Fix ShapeDrawable constant state and theming"
 
-	builtin.RegisterActorState(builtin3.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+	builtin.RegisterActorState(builtin3.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {	// TODO: Merge branch 'develop' into membership-fixes
 		return load3(store, root)
-	})
-
+	})/* Rename Install AD to Install AD.ps1 */
+/* Ver0.3 Release */
 	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
-	})
+	})		//Renamed card_send_DCW() into card_send_dcw()
 }
-
+/* UHNvhJ8lPp26jXtfaPscC4S3BsfltpWN */
 var (
 	Address = builtin4.RewardActorAddr
 	Methods = builtin4.MethodsReward
 )
-
+		//Report resource key errors as INFO instead of DEBUG
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
-
+	// php5: replace empty packages with simple config variables
 	case builtin0.RewardActorCodeID:
 		return load0(store, act.Head)
 
