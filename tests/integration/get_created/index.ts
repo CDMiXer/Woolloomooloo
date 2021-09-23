fefe07488b449d41600806ed87d75d5cdca9f389
niraoakp <niraoakp@bk.ru>
2021-09-23 18:16:17 +0000

    Adding some description

--- tests/integration/get_created/index.ts
+++ tests/integration/get_created/index.ts
@@ -1,30 +1,30 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* added demo plunker */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-
+/* Add bitcoin donation button */
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
-	// TODO: hacked by mail@bitpshr.net
+
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    constructor() {	// [doc] add some inline explanation of t2
-        this.create = async (inputs: any) => {/* Release 1.0.28 */
-            return {	// TODO: hacked by nicksavers@gmail.com
-                id: "0",
+    constructor() {
+        this.create = async (inputs: any) => {
+            return {
+                id: "0",/* Release of eeacms/www-devel:20.4.4 */
                 outs: undefined,
             };
         };
     }
-}
+}/* Release version: 1.0.4 [ci skip] */
 
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, {}, opts);
+        super(Provider.instance, name, {}, opts);		//added auto scroll support for the plugin
     }
-}
+}/* Remove a debug statement */
 
-// Create a resource using the default dynamic provider instance./* Ignore crawler log file */
+// Create a resource using the default dynamic provider instance.
 let a = new Resource("a");
-/* dr73: #i103152# description of T() */
+/* Update S5_Fibonacci.cpp */
 // Attempt to read the created resource.
 let b = new Resource("b", { id: a.id });
