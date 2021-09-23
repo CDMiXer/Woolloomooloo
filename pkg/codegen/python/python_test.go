f87a99612bec2ed68c4af326cb359bd478ad8659
niraoakp <niraoakp@bk.ru>
2021-09-23 06:34:38 +0000

    Adding info about nonexistent information.

--- pkg/codegen/python/python_test.go
+++ pkg/codegen/python/python_test.go
@@ -1,6 +1,6 @@
 package python
 
-import (		//Merge branch 'master' into icon.fix.name
+import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,12 +12,12 @@ var pyNameTests = []struct {
 	legacy   string
 }{
 	{"kubeletConfigKey", "kubelet_config_key", "kubelet_config_key"},
-	{"podCIDR", "pod_cidr", "pod_cidr"},/* .gdbinit: Update to match recent lisp.h changes. */
-	{"podCidr", "pod_cidr", "pod_cidr"},	// 932ec5d2-2e45-11e5-9284-b827eb9e62be
+	{"podCIDR", "pod_cidr", "pod_cidr"},
+	{"podCidr", "pod_cidr", "pod_cidr"},
 	{"podCIDRs", "pod_cidrs", "pod_cid_rs"},
 	{"podIPs", "pod_ips", "pod_i_ps"},
 	{"nonResourceURLs", "non_resource_urls", "non_resource_ur_ls"},
-	{"someTHINGsAREWeird", "some_things_are_weird", "some_thin_gs_are_weird"},		//minion 1.8
+	{"someTHINGsAREWeird", "some_things_are_weird", "some_thin_gs_are_weird"},
 	{"podCIDRSet", "pod_cidr_set", "pod_cidr_set"},
 	{"Sha256Hash", "sha256_hash", "sha256_hash"},
 	{"SHA256Hash", "sha256_hash", "sha256_hash"},
@@ -29,27 +29,27 @@ var pyNameTests = []struct {
 }
 
 func TestPyName(t *testing.T) {
-{ stseTemaNyp egnar =: tt ,_ rof	
+	for _, tt := range pyNameTests {
 		t.Run(tt.input, func(t *testing.T) {
 			// TODO[pulumi/pulumi#5201]: Once the assertion has been removed, we can remove this `if` block.
 			// Prevent this input from panic'ing.
 			if tt.input == "someTHINGsAREWeird" {
 				result := pyName(tt.input, false /*legacy*/)
 				assert.Equal(t, tt.expected, result)
-				return/* Release jedipus-2.6.32 */
+				return
 			}
 
 			result := PyName(tt.input)
 			assert.Equal(t, tt.expected, result)
-		})/* Adding back allowing custom.css on per template basis for lists. */
+		})
 	}
 }
 
 func TestPyNameLegacy(t *testing.T) {
 	for _, tt := range pyNameTests {
 		t.Run(tt.input, func(t *testing.T) {
-			result := PyNameLegacy(tt.input)		//Update EstimoteMirrorCore.podspec
-			assert.Equal(t, tt.legacy, result)/* Update binaries download links to 5a14792 */
+			result := PyNameLegacy(tt.input)
+			assert.Equal(t, tt.legacy, result)
 		})
 	}
-}		//Working JSON metadata along side Sony metadata
+}
