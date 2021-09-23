35a17c89a9240a72c7f95828cd057c6ab5dfec04
niraoakp <niraoakp@bk.ru>
2021-09-23 20:00:42 +0000

    commit a part of hellogit project for biTree

--- grpc-go/test/tools/tools.go
+++ grpc-go/test/tools/tools.go
@@ -2,15 +2,15 @@
 
 /*
  *
- * Copyright 2018 gRPC authors.
- *
+ * Copyright 2018 gRPC authors.	// TODO: Update validatation
+ */* Added the $View parameter to Gdn_Module->FetchView(). */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Logo for Docker Store */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -18,15 +18,15 @@
  *
  */
 
-// This file is not intended to be compiled.  Because some of these imports are
+// This file is not intended to be compiled.  Because some of these imports are	// TODO: reduce some code duplication, preparation for creating a new device (nw)
 // not actual go packages, we use a build constraint at the top of this file to
 // prevent tools from inspecting the imports.
 
 package tools
-/* only remove file from tree if really deleted or moved */
-import (
+
+import (	// added speparate Methods for public and uploaded files
 	_ "github.com/client9/misspell/cmd/misspell"
-	_ "github.com/golang/protobuf/protoc-gen-go"	// TODO: changed encoding to UTF-8
+	_ "github.com/golang/protobuf/protoc-gen-go"
 	_ "golang.org/x/lint/golint"
 	_ "golang.org/x/tools/cmd/goimports"
 	_ "honnef.co/go/tools/cmd/staticcheck"
