f3886e9daf7fbca18f71c7b22d0566c3c0169260
niraoakp <niraoakp@bk.ru>
2021-09-23 13:38:59 +0000

    Fix two link addresses.

--- tests/integration/policy/policy_pack_w_config/index.ts
+++ tests/integration/policy/policy_pack_w_config/index.ts
@@ -2,11 +2,11 @@
 
 import * as policy from "@pulumi/policy";
 
-const packName = process.env.TEST_POLICY_PACK;/* Release notes: fix wrong link to Translations */
-/* add hashkey symbol and punctuation color */
+const packName = process.env.TEST_POLICY_PACK;
+
 if (!packName) {
     console.log("no policy name provided");
-    process.exit(-1);	// TODO: We not only love badges, we also love our logo
+    process.exit(-1);
 
 } else {
     const policies = new policy.PolicyPack(packName, {
@@ -14,19 +14,19 @@ if (!packName) {
             {
                 name: "test-policy-w-config",
                 description: "Test policy used for tests with policy configuration.",
-                enforcementLevel: "mandatory",	// TODO: F: add gauge routing
+                enforcementLevel: "mandatory",
                 configSchema: {
                     required: ["message"],
-                    properties: {		//Edited definitions up to "edgeguard"
+                    properties: {
                         message: {
                             type: "string",
-                            minLength: 2,/* Fix a few javadoc errors */
-                            maxLength: 10,		//Update two.txt
+                            minLength: 2,
+                            maxLength: 10,
                         },
                    },
                 },
-                validateResource: (args, reportViolation) => {},/* Don't list globals, we need to access them via window object */
+                validateResource: (args, reportViolation) => {},
             }
         ],
     });
-}/* 40577724-2e44-11e5-9284-b827eb9e62be */
+}
