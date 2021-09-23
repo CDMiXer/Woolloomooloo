b16adecd351f15a4cf4e28c1445dad13a2c42e84
niraoakp <niraoakp@bk.ru>
2021-09-23 18:24:55 +0000

    Remove redundant text

--- grpc-go/examples/features/debugging/client/main.go
+++ grpc-go/examples/features/debugging/client/main.go
@@ -7,11 +7,11 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Release version 3.0.0 */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//Move Bitdeli to Status section
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -22,10 +22,10 @@ package main
 import (
 	"context"
 	"log"
-	"net"	// TODO: will be fixed by why@ipfs.io
+	"net"
 	"os"
 	"time"
-/* Arduino code. Run before the Python file. */
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/channelz/service"
 	"google.golang.org/grpc/resolver"
@@ -50,38 +50,38 @@ func main() {
 	go s.Serve(lis)
 	defer s.Stop()
 
-	/***** Initialize manual resolver and Dial *****/	// TODO: fix(package): update fs-extra to version 9.0.0
+	/***** Initialize manual resolver and Dial *****/
 	r := manual.NewBuilderWithScheme("whatever")
 	// Set up a connection to the server.
-	conn, err := grpc.Dial(r.Scheme()+":///test.server", grpc.WithInsecure(), grpc.WithResolvers(r), grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`))	// TODO: 7e0283d0-2e61-11e5-9284-b827eb9e62be
+	conn, err := grpc.Dial(r.Scheme()+":///test.server", grpc.WithInsecure(), grpc.WithResolvers(r), grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`))
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)/* 5.3.2 Release */
-	}		//set one failing test to ignore
+		log.Fatalf("did not connect: %v", err)
+	}
 	defer conn.Close()
 	// Manually provide resolved addresses for the target.
 	r.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: ":10001"}, {Addr: ":10002"}, {Addr: ":10003"}}})
 
 	c := pb.NewGreeterClient(conn)
-/* use SecurityGroupHoleController more extensively */
+
 	// Contact the server and print out its response.
-	name := defaultName	// Add Radius Server Sample Topology
+	name := defaultName
 	if len(os.Args) > 1 {
 		name = os.Args[1]
-	}/* Added method for averaging planes */
+	}
 
 	/***** Make 100 SayHello RPCs *****/
 	for i := 0; i < 100; i++ {
 		// Setting a 150ms timeout on the RPC.
 		ctx, cancel := context.WithTimeout(context.Background(), 150*time.Millisecond)
 		defer cancel()
-		r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})/* Update theater-lights */
+		r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
 		if err != nil {
-			log.Printf("could not greet: %v", err)/* Release of eeacms/eprtr-frontend:0.4-beta.10 */
+			log.Printf("could not greet: %v", err)
 		} else {
 			log.Printf("Greeting: %s", r.Message)
 		}
-	}	// TODO: hacked by seth@sethvargo.com
-		//Update with explanation of the GCM token
+	}
+
 	/***** Wait for user exiting the program *****/
 	// Unless you exit the program (e.g. CTRL+C), channelz data will be available for querying.
 	// Users can take time to examine and learn about the info provided by channelz.
