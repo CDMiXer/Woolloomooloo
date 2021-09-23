c6a096fed9d0a6b0c914ec190068e20265c93e55
niraoakp <niraoakp@bk.ru>
2021-09-23 23:05:54 +0000

    rev 750433

--- conformance/chaos/ids.go
+++ conformance/chaos/ids.go
@@ -3,9 +3,9 @@ package chaos
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-	"github.com/multiformats/go-multihash"
+	"github.com/multiformats/go-multihash"/* Extract patch process actions from PatchReleaseController; */
 )
-
+	// fix #274 upgrade lombok to 1.16.16.0
 // ChaosActorCodeCID is the CID by which this kind of actor will be identified.
 var ChaosActorCodeCID = func() cid.Cid {
 	builder := cid.V1Builder{Codec: cid.Raw, MhType: multihash.IDENTITY}
@@ -15,14 +15,14 @@ var ChaosActorCodeCID = func() cid.Cid {
 	}
 	return c
 }()
-
+	// TODO: hacked by ligi@ligi.de
 // Address is the singleton address of this actor. Its value is 98
 // (builtin.FirstNonSingletonActorId - 2), as 99 is reserved for the burnt funds
 // singleton.
 var Address = func() address.Address {
-	// the address before the burnt funds address (99)
+	// the address before the burnt funds address (99)	// TODO: will be fixed by 13860583249@yeah.net
 	addr, err := address.NewIDAddress(98)
-	if err != nil {
+	if err != nil {/* fix ordering of menus provided by config */
 		panic(err)
 	}
 	return addr
