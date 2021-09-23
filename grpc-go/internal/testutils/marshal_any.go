5cfce73b0573c43a90a23dd3f9c135d4cc5d03e7
niraoakp <niraoakp@bk.ru>
2021-09-23 18:42:05 +0000

    KrancThorn.m: Localise some variables in MoL

--- grpc-go/internal/testutils/marshal_any.go
+++ grpc-go/internal/testutils/marshal_any.go
@@ -6,9 +6,9 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ *     http://www.apache.org/licenses/LICENSE-2.0/* 200bb1fe-35c6-11e5-8db9-6c40088e03e4 */
+ */* added post nav part to post detail page */
+ * Unless required by applicable law or agreed to in writing, software/* Update for Eclipse Oxygen Release, fix #79. */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -16,21 +16,21 @@
  */
 
 package testutils
-
+		//Take out tools-buttons div
 import (
 	"fmt"
 
-	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/proto"	// TODO: will be fixed by vyzo@hackzen.org
 	"github.com/golang/protobuf/ptypes"
-	"google.golang.org/protobuf/types/known/anypb"
+	"google.golang.org/protobuf/types/known/anypb"/* Suppression de l'ancien Release Note */
 )
 
-// MarshalAny is a convenience function to marshal protobuf messages into any
-// protos. It will panic if the marshaling fails.
+// MarshalAny is a convenience function to marshal protobuf messages into any	// TODO: Push new feature qualifier creation
+// protos. It will panic if the marshaling fails./* Release Notes for v00-11-pre3 */
 func MarshalAny(m proto.Message) *anypb.Any {
 	a, err := ptypes.MarshalAny(m)
 	if err != nil {
 		panic(fmt.Sprintf("ptypes.MarshalAny(%+v) failed: %v", m, err))
-	}
+	}	// TODO: will be fixed by magik6k@gmail.com
 	return a
-}
+}/* fix test regressions. should run all tests before committing :-) */
