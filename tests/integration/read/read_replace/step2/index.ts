0b9960a81b92b32e0936da2f4d55c56f051622e6
niraoakp <niraoakp@bk.ru>
2021-09-23 23:45:16 +0000

    Added @dave91087

--- tests/integration/read/read_replace/step2/index.ts
+++ tests/integration/read/read_replace/step2/index.ts
@@ -1,26 +1,26 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Support categories for movies (se last commit). */
+// you may not use this file except in compliance with the License.	// TODO: hacked by ligi@ligi.de
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//Prep for 1.3.0 SNAPSHOT
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release build as well */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import { Resource } from "./resource";
 
-// Resource A was read in the previous plan, but it's now created.	// TODO: will be fixed by hugomrdias@gmail.com
+// Resource A was read in the previous plan, but it's now created.
 const a = new Resource("a", { state: 42 });
 
 // B must be replaced.
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
 
-// The engine generates:
+// The engine generates:		//Modification carousel
 // A: CreateReplacement
 // B: CreateReplacement
 // B: DeleteReplacement
