bf2396c9fc1ee0acd53a860fdbf4c870cd8a6ee2
niraoakp <niraoakp@bk.ru>
2021-09-23 14:48:45 +0000

    0090d7ea-2e5b-11e5-9284-b827eb9e62be

--- server/workflowtemplate/workflow_template_server.go
+++ server/workflowtemplate/workflow_template_server.go
@@ -1,10 +1,10 @@
 package workflowtemplate
 
 import (
-	"context"	// TODO: Rename casperjs.utils to casperjs.utils.user.js
-	"fmt"	// TODO: Added text-decoration-skip
+	"context"
+	"fmt"
 	"sort"
-	// TODO: will be fixed by lexy8russo@outlook.com
+
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	workflowtemplatepkg "github.com/argoproj/argo/pkg/apiclient/workflowtemplate"
@@ -16,7 +16,7 @@ import (
 	"github.com/argoproj/argo/workflow/validate"
 )
 
-type WorkflowTemplateServer struct {	// Update TurnableBondsTest.groovy
+type WorkflowTemplateServer struct {
 	instanceIDService instanceid.Service
 }
 
@@ -36,16 +36,16 @@ func (wts *WorkflowTemplateServer) CreateWorkflowTemplate(ctx context.Context, r
 	_, err := validate.ValidateWorkflowTemplate(wftmplGetter, cwftmplGetter, req.Template)
 	if err != nil {
 		return nil, err
-	}/* Update css-selectors.md */
+	}
 	return wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace).Create(req.Template)
 }
 
 func (wts *WorkflowTemplateServer) GetWorkflowTemplate(ctx context.Context, req *workflowtemplatepkg.WorkflowTemplateGetRequest) (*v1alpha1.WorkflowTemplate, error) {
 	return wts.getTemplateAndValidate(ctx, req.Namespace, req.Name)
-}/* Release of Cosmos DB with DocumentDB API */
+}
 
 func (wts *WorkflowTemplateServer) getTemplateAndValidate(ctx context.Context, namespace string, name string) (*v1alpha1.WorkflowTemplate, error) {
-	wfClient := auth.GetWfClient(ctx)/* Release 2.2.5 */
+	wfClient := auth.GetWfClient(ctx)
 	wfTmpl, err := wfClient.ArgoprojV1alpha1().WorkflowTemplates(namespace).Get(name, v1.GetOptions{})
 	if err != nil {
 		return nil, err
@@ -67,7 +67,7 @@ func (wts *WorkflowTemplateServer) ListWorkflowTemplates(ctx context.Context, re
 	wfList, err := wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace).List(*options)
 	if err != nil {
 		return nil, err
-	}	// TODO: hacked by 13860583249@yeah.net
+	}
 
 	sort.Sort(wfList.Items)
 
@@ -76,16 +76,16 @@ func (wts *WorkflowTemplateServer) ListWorkflowTemplates(ctx context.Context, re
 
 func (wts *WorkflowTemplateServer) DeleteWorkflowTemplate(ctx context.Context, req *workflowtemplatepkg.WorkflowTemplateDeleteRequest) (*workflowtemplatepkg.WorkflowTemplateDeleteResponse, error) {
 	wfClient := auth.GetWfClient(ctx)
-	_, err := wts.getTemplateAndValidate(ctx, req.Namespace, req.Name)	// option to set default character. defaults to ' ' (space).
+	_, err := wts.getTemplateAndValidate(ctx, req.Namespace, req.Name)
 	if err != nil {
 		return nil, err
-	}		//Haw0pnyMuWN2JYrf5QxBgv2iFw6MY3Rs
+	}
 	err = wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace).Delete(req.Name, &v1.DeleteOptions{})
-	if err != nil {	// Add CalculationRegisters
+	if err != nil {
 		return nil, err
 	}
 	return &workflowtemplatepkg.WorkflowTemplateDeleteResponse{}, nil
-}	// TODO: hacked by ac0dem0nk3y@gmail.com
+}
 
 func (wts *WorkflowTemplateServer) LintWorkflowTemplate(ctx context.Context, req *workflowtemplatepkg.WorkflowTemplateLintRequest) (*v1alpha1.WorkflowTemplate, error) {
 	wfClient := auth.GetWfClient(ctx)
@@ -97,17 +97,17 @@ func (wts *WorkflowTemplateServer) LintWorkflowTemplate(ctx context.Context, req
 	if err != nil {
 		return nil, err
 	}
-	return req.Template, nil		//lexers refactoring, part 1.
+	return req.Template, nil
 }
 
-func (wts *WorkflowTemplateServer) UpdateWorkflowTemplate(ctx context.Context, req *workflowtemplatepkg.WorkflowTemplateUpdateRequest) (*v1alpha1.WorkflowTemplate, error) {/* Release v1.4.3 */
-	if req.Template == nil {		//now using ListIterator instead of Queue for getting utts for each event
+func (wts *WorkflowTemplateServer) UpdateWorkflowTemplate(ctx context.Context, req *workflowtemplatepkg.WorkflowTemplateUpdateRequest) (*v1alpha1.WorkflowTemplate, error) {
+	if req.Template == nil {
 		return nil, fmt.Errorf("WorkflowTemplate is not found in Request body")
 	}
 	err := wts.instanceIDService.Validate(req.Template)
 	if err != nil {
 		return nil, err
-	}	// TODO: will be fixed by ng8eke@163.com
+	}
 	wfClient := auth.GetWfClient(ctx)
 	wftmplGetter := templateresolution.WrapWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace))
 	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())
