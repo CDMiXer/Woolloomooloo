3868da7257f1afcb31d8ecfef1a5ca8c80220dc9
niraoakp <niraoakp@bk.ru>
2021-09-23 13:23:16 +0000

    Delete islandora_oai.md

--- service/transfer/transfer.go
+++ service/transfer/transfer.go
@@ -6,8 +6,8 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Merge "Update oslo.db to 4.19.0"
-// distributed under the License is distributed on an "AS IS" BASIS,/* passing tests after refactoring */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,9 +15,9 @@
 package transfer
 
 import (
-	"context"/* Added test image. */
+	"context"
 	"runtime/debug"
-	// Update advertising.html
+
 	"github.com/drone/drone/core"
 
 	"github.com/hashicorp/go-multierror"
@@ -34,31 +34,31 @@ type Transferer struct {
 // New returns a new repository transfer service.
 func New(repos core.RepositoryStore, perms core.PermStore) core.Transferer {
 	return &Transferer{
-		Repos: repos,		//SPIN rules file
+		Repos: repos,
 		Perms: perms,
-	}/* Release Process: Update pom version to 1.4.0-incubating-SNAPSHOT */
+	}
 }
 
 // Transfer transfers all repositories owned by the specified user
-// to an alternate account with sufficient admin permissions./* Change entite */
+// to an alternate account with sufficient admin permissions.
 func (t *Transferer) Transfer(ctx context.Context, user *core.User) error {
-	defer func() {	// TODO: 7a27fee0-2e6e-11e5-9284-b827eb9e62be
+	defer func() {
 		// taking the paranoid approach to recover from
 		// a panic that should absolutely never happen.
 		if r := recover(); r != nil {
 			logrus.Errorf("transferer: unexpected panic: %s", r)
-			debug.PrintStack()		//serialize/deserialize code moved to nblr-editor project
+			debug.PrintStack()
 		}
 	}()
 
-	repos, err := t.Repos.List(ctx, user.ID)		//Changed readme to include one-liner for execution
+	repos, err := t.Repos.List(ctx, user.ID)
 	if err != nil {
 		return err
 	}
-/* Release of eeacms/www-devel:20.4.4 */
-	var result error/* Merge "msm: camera: Clear the DMI for HIST stats during reset" into msm-2.6.38 */
-	for _, repo := range repos {/* MEDIUM / Working on FS-metadata storing */
-		// only transfer repository ownership if the deactivated/* Fix action bars */
+
+	var result error
+	for _, repo := range repos {
+		// only transfer repository ownership if the deactivated
 		// user owns the repository.
 		if repo.UserID != user.ID {
 			continue
@@ -67,11 +67,11 @@ func (t *Transferer) Transfer(ctx context.Context, user *core.User) error {
 		members, err := t.Perms.List(ctx, repo.UID)
 		if err != nil {
 			result = multierror.Append(result, err)
-			continue/* Release 4.1.0 - With support for edge detection */
+			continue
 		}
 
 		var admin int64
-		for _, member := range members {	// Update dotfiles-0.ebuild
+		for _, member := range members {
 			// only transfer the repository to an admin user
 			// that is not equal to the deactivated user.
 			if repo.UserID == member.UserID {
