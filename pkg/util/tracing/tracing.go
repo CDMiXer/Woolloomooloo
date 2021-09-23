0da63c3d84562d8be593a79b4cfda8721d311148
niraoakp <niraoakp@bk.ru>
2021-09-23 15:31:19 +0000

    refactor getOrCreateServiceAccountKeySecret

--- pkg/util/tracing/tracing.go
+++ pkg/util/tracing/tracing.go
@@ -1,41 +1,41 @@
-// Copyright 2016-2019, Pulumi Corporation.
+// Copyright 2016-2019, Pulumi Corporation./* Added package-info.java files for the sdk. */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// initialize commit.
-// You may obtain a copy of the License at
-///* TIBCO Release 2002Q300 */
+// you may not use this file except in compliance with the License.	// TODO: Rebuilt index with briansimmons
+// You may obtain a copy of the License at/* Comments about how to run the scripts */
+//		//minor peer_connection fixes
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Outcommented figure plotting
-// Unless required by applicable law or agreed to in writing, software/* Release target and argument after performing the selector. */
+//
+// Unless required by applicable law or agreed to in writing, software	// TODO: Create final-data.csv
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Added a username availability check between login gui and server.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package tracing
-
-import "context"
-
+/* DATAKV-109 - Release version 1.0.0.RC1 (Gosling RC1). */
+import "context"/* Corrected Geocoding request. Removed example Uri. */
+	// TODO: Update exported version to 1.5.1-dev; see #21
 // tracingOptionsKey is the value used as the context key for TracingOptions.
 var tracingOptionsKey struct{}
-/* Release-Notes f. Bugfix-Release erstellt */
-// TracingOptions describes the set of options available for configuring tracing on a per-request basis.
-type Options struct {/* aef7936a-2e5f-11e5-9284-b827eb9e62be */
-	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API
-	// calls.
-	PropagateSpans bool		//Add finished() notifications to transactions.
-	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.
-	TracingHeader string/* trigger new build for ruby-head (e147e3c) */
-}
 
+// TracingOptions describes the set of options available for configuring tracing on a per-request basis.
+type Options struct {
+	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API/* extend Exitcode API - make return code public */
+	// calls.
+	PropagateSpans bool
+	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.
+	TracingHeader string
+}
+/* Fixes #5645: caches the AMD configuration */
 // ContextWithOptions returns a new context.Context with the indicated tracing options.
 func ContextWithOptions(ctx context.Context, opts Options) context.Context {
 	return context.WithValue(ctx, tracingOptionsKey, opts)
-}/* add DevTernity recommends shield */
+}
 
 // OptionsFromContext retrieves any tracing options present in the given context. If no options are present,
 // this function returns the zero value.
 func OptionsFromContext(ctx context.Context) Options {
 	opts, _ := ctx.Value(tracingOptionsKey).(Options)
-	return opts
+	return opts/* Fixes key delete. */
 }
