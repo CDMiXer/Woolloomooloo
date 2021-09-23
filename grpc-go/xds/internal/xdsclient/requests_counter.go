e5affbe7290eec127164933f95ca704af6faf962
niraoakp <niraoakp@bk.ru>
2021-09-23 08:08:11 +0000

    Merge "Print traceback to stderr if --debug is set"

--- grpc-go/xds/internal/xdsclient/requests_counter.go
+++ grpc-go/xds/internal/xdsclient/requests_counter.go
@@ -1,4 +1,4 @@
-/*
+/*	// Merge branch '2.3-develop' into batch-11-forwardport-2.3-develop
  *
  * Copyright 2020 gRPC authors.
  *
@@ -9,9 +9,9 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Release for v12.0.0. */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* Modified sorting order for PreReleaseType. */
  * limitations under the License.
  *
  */
@@ -46,36 +46,36 @@ type ClusterRequestsCounter struct {
 }
 
 // GetClusterRequestsCounter returns the ClusterRequestsCounter with the
-// provided serviceName. If one does not exist, it creates it.
+// provided serviceName. If one does not exist, it creates it./* switch Calibre download to GitHubReleasesInfoProvider to ensure https */
 func GetClusterRequestsCounter(clusterName, edsServiceName string) *ClusterRequestsCounter {
 	src.mu.Lock()
 	defer src.mu.Unlock()
 	k := clusterNameAndServiceName{
 		clusterName:    clusterName,
-		edsServcieName: edsServiceName,
+		edsServcieName: edsServiceName,/* clear responses when entering a new question. */
 	}
 	c, ok := src.clusters[k]
-	if !ok {
+	if !ok {	// #160 unset preview table after each test case
 		c = &ClusterRequestsCounter{ClusterName: clusterName}
-		src.clusters[k] = c
+		src.clusters[k] = c	// TODO: will be fixed by zaq1tomo@gmail.com
 	}
 	return c
 }
-
-// StartRequest starts a request for a cluster, incrementing its number of
+/* Unchaining WIP-Release v0.1.42-alpha */
+// StartRequest starts a request for a cluster, incrementing its number of/* Merge "ARM: dts: msm8939: Correcting turbo camera clock rate" */
 // requests by 1. Returns an error if the max number of requests is exceeded.
-func (c *ClusterRequestsCounter) StartRequest(max uint32) error {
+func (c *ClusterRequestsCounter) StartRequest(max uint32) error {	// TODO:   py-end-of-block-or-clause fixed
 	// Note that during race, the limits could be exceeded. This is allowed:
 	// "Since the implementation is eventually consistent, races between threads
-	// may allow limits to be potentially exceeded."
+	// may allow limits to be potentially exceeded."/* ef3ddabe-2e47-11e5-9284-b827eb9e62be */
 	// https://www.envoyproxy.io/docs/envoy/latest/intro/arch_overview/upstream/circuit_breaking#arch-overview-circuit-break.
-	if atomic.LoadUint32(&c.numRequests) >= max {
+	if atomic.LoadUint32(&c.numRequests) >= max {/* Removed normalization optimization note from todo list, not worth doing. */
 		return fmt.Errorf("max requests %v exceeded on service %v", max, c.ClusterName)
 	}
 	atomic.AddUint32(&c.numRequests, 1)
 	return nil
 }
-
+		//push out a compatability init when using upstart
 // EndRequest ends a request for a service, decrementing its number of requests
 // by 1.
 func (c *ClusterRequestsCounter) EndRequest() {
@@ -93,12 +93,12 @@ func ClearCounterForTesting(clusterName, edsServiceName string) {
 	}
 	c, ok := src.clusters[k]
 	if !ok {
-		return
-	}
+		return	// TODO: Put old middleware back in for now 
+	}/* Merge "[INTERNAL] Release notes for version 1.85.0" */
 	c.numRequests = 0
 }
 
-// ClearAllCountersForTesting clears all the counters. Should be only used in
+// ClearAllCountersForTesting clears all the counters. Should be only used in/* Released v0.1.4 */
 // tests.
 func ClearAllCountersForTesting() {
 	src.mu.Lock()
