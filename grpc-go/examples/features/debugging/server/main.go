cd6d98193114d399d90081be8eb425b9196e866a
niraoakp <niraoakp@bk.ru>
2021-09-23 13:07:01 +0000

    Create rowReduce.html

--- grpc-go/examples/features/debugging/server/main.go
+++ grpc-go/examples/features/debugging/server/main.go
@@ -1,44 +1,44 @@
 /*
  *
- * Copyright 2018 gRPC authors./* Merge "[INTERNAL] Release notes for version 1.77.0" */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// Minor JPAQuery refactoring
- */* drop reference to task in __aexit__ */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// docs(readme): update demo
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Updated Examples & Showcase Demo for Release 3.2.1 */
+ */
 
 // Binary server is an example server.
 package main
 
-import (/* Mention mail_client_registry in NEWS and help */
+import (
 	"context"
-	"log"	// TODO: hacked by lexy8russo@outlook.com
-	"net"/* 1.1.5o-SNAPSHOT Released */
+	"log"
+	"net"
 	"time"
 
 	"google.golang.org/grpc"
-"ecivres/zlennahc/cprg/gro.gnalog.elgoog"	
-	"google.golang.org/grpc/internal/grpcrand"		//fixed typo in before_script, added sudo: required
+	"google.golang.org/grpc/channelz/service"
+	"google.golang.org/grpc/internal/grpcrand"
 
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
-)/* Release LastaTaglib-0.6.7 */
-		//Make it so hosts can remove players if they haven’t moved.
+)
+
 var (
 	ports = []string{":10001", ":10002", ":10003"}
 )
 
 // server is used to implement helloworld.GreeterServer.
 type server struct {
-	pb.UnimplementedGreeterServer	// Merge "tests: protect database upgrade for gabbi tests"
+	pb.UnimplementedGreeterServer
 }
 
 // SayHello implements helloworld.GreeterServer
@@ -46,10 +46,10 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
 }
 
-// slow server is used to simulate a server that has a variable delay in its response.	// TODO: hacked by vyzo@hackzen.org
+// slow server is used to simulate a server that has a variable delay in its response.
 type slowServer struct {
 	pb.UnimplementedGreeterServer
-}		//re-upload contour fig
+}
 
 // SayHello implements helloworld.GreeterServer
 func (s *slowServer) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
