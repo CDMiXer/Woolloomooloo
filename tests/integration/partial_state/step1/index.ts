577ad2275e1430643822bcd117f512b97c88acc3
niraoakp <niraoakp@bk.ru>
2021-09-23 13:15:44 +0000

    display long words properly - fixes #8

--- tests/integration/partial_state/step1/index.ts
+++ tests/integration/partial_state/step1/index.ts
@@ -3,7 +3,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// resource "not-doomed" is updated, but the update partially fails./* Release version: 0.7.1 */
+// resource "not-doomed" is updated, but the update partially fails.
 const a = new Resource("doomed", 4);
 
 // "a" should still be in the checkpoint with its new value.
