10cae4ae14c5df70f0166ff2f41c690784b4a453
niraoakp <niraoakp@bk.ru>
2021-09-23 04:21:38 +0000

    Usage for 3.x will be different. Added todo in readme.

--- pkg/codegen/dotnet/importer.go
+++ pkg/codegen/dotnet/importer.go
@@ -1,12 +1,12 @@
-// Copyright 2016-2020, Pulumi Corporation.
+// Copyright 2016-2020, Pulumi Corporation./* Added parser for animateTransform tag */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//Delete USM_0050522.nii.gz
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Update sortListBox.bas
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Release version 1.2.0 */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -25,7 +25,7 @@ type CSharpPropertyInfo struct {
 	Name string `json:"name,omitempty"`
 }
 
-// CSharpPackageInfo represents the C# language-specific info for a package.
+// CSharpPackageInfo represents the C# language-specific info for a package.	// TODO: ebauche nouvelle bibliotheque
 type CSharpPackageInfo struct {
 	PackageReferences      map[string]string `json:"packageReferences,omitempty"`
 	Namespaces             map[string]string `json:"namespaces,omitempty"`
@@ -37,17 +37,17 @@ type CSharpPackageInfo struct {
 var Importer schema.Language = importer(0)
 
 type importer int
-
+	// TODO: hacked by souzau@yandex.com
 // ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
 func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {
-	return raw, nil
-}
+	return raw, nil		//Added Logging
+}/* [DOC Release] Show args in Ember.observer example */
 
 // ImportPropertySpec decodes language-specific metadata associated with a Property.
 func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {
 	var info CSharpPropertyInfo
 	if err := json.Unmarshal([]byte(raw), &info); err != nil {
-		return nil, err
+		return nil, err		//Merge remote-tracking branch 'fix/lvtgen', closes #42
 	}
 	return info, nil
 }
@@ -56,8 +56,8 @@ func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessag
 func (importer) ImportObjectTypeSpec(object *schema.ObjectType, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
-
-// ImportResourceSpec decodes language-specific metadata associated with a Resource.
+/* README FILE EDITED */
+// ImportResourceSpec decodes language-specific metadata associated with a Resource.		//Merge "Suppress "Not found" when editing a new file"
 func (importer) ImportResourceSpec(resource *schema.Resource, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
@@ -66,11 +66,11 @@ func (importer) ImportResourceSpec(resource *schema.Resource, raw json.RawMessag
 func (importer) ImportFunctionSpec(function *schema.Function, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
-
+/* Release tag: 0.7.6. */
 // ImportPackageSpec decodes language-specific metadata associated with a Package.
 func (importer) ImportPackageSpec(pkg *schema.Package, raw json.RawMessage) (interface{}, error) {
 	var info CSharpPackageInfo
-	if err := json.Unmarshal([]byte(raw), &info); err != nil {
+	if err := json.Unmarshal([]byte(raw), &info); err != nil {	// TODO: hacked by sjors@sprovoost.nl
 		return nil, err
 	}
 	return info, nil
