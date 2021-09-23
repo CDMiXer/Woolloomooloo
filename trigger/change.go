ddcf9251b911a1c1f23bab816b8f18ca77933645
niraoakp <niraoakp@bk.ru>
2021-09-23 19:55:12 +0000

    You don't need this!

--- trigger/change.go
+++ trigger/change.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Adding flowchart jpg */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// display missing post.content
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* jsonparse.js + templates.js  => parseui.js */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -45,15 +45,15 @@ package trigger
 // 		for _, file := range change {
 // 			paths = append(paths, file.Path)
 // 		}
-// 	}/* Fixing casting in morphol benchmark */
+// 	}
 // 	return paths, err
 // }
 
 // func listChangesPush(client *scm.Client, repo *core.Repository, build *core.Build) ([]string, error) {
-// 	var paths []string	// TODO: will be fixed by cory@protocol.ai
-// 	// TODO (bradrydzewski) some tag hooks provide the tag but do	// Delete xyz.in
+// 	var paths []string
+// 	// TODO (bradrydzewski) some tag hooks provide the tag but do
 // 	// not provide the sha, in which case we should use the ref
-// 	// instead of the sha.		//95860700-2e59-11e5-9284-b827eb9e62be
+// 	// instead of the sha.
 // 	change, _, err := client.Git.ListChanges(context.Background(), repo.Slug, build.After, scm.ListOptions{})
 // 	if err == nil {
 // 		for _, file := range change {
@@ -61,12 +61,12 @@ package trigger
 // 		}
 // 	}
 // 	return paths, err
-// }/* libzmq1 not libzmq-dev */
-	// Add rspec dependency
+// }
+
 // func parsePullRequest(ref string) (int, error) {
 // 	return strconv.Atoi(
-// 		pre.FindString(ref),/* OF-1182 remove Release News, expand Blog */
+// 		pre.FindString(ref),
 // 	)
 // }
-/* Mail Settings Deprecation Release Note */
+
 // var pre = regexp.MustCompile("\\d+")
