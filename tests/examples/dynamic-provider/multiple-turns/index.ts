12b11db669ae1bb77c3ff53518ca1195d5178a02
niraoakp <niraoakp@bk.ru>
2021-09-23 21:14:04 +0000

    (Fixes issue 1625)

--- tests/examples/dynamic-provider/multiple-turns/index.ts
+++ tests/examples/dynamic-provider/multiple-turns/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";/* Release version 4.0.0.12. */
-/* Release Tag V0.30 (additional changes) */
+import * as dynamic from "@pulumi/pulumi/dynamic";/* Updated manual.html */
+
 const sleep = require("sleep-promise");
-const assert = require("assert");/* [artifactory-release] Release version 1.0.4.RELEASE */
-		//Update SysML metamodel and add tests for TUID calculation.
+const assert = require("assert");
+		//Fis GlobalUnit composed Name
 class NullProvider implements dynamic.ResourceProvider {
     check = (olds: any, news: any) => Promise.resolve({ inputs: news });
     diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
     create = (inputs: any) => Promise.resolve({ id: "0" });
     update = (id: string, olds: any, news: any) => Promise.resolve({});
     delete = (id: pulumi.ID, props: any) => Promise.resolve();
-}
-/* NEW Add none/all selection into list of files for FTP browser module */
+}/* Changes to english names */
+
 class NullResource extends dynamic.Resource {
     constructor(name: string) {
         super(new NullProvider(), name, {}, undefined);
@@ -31,7 +31,7 @@ class NullResource extends dynamic.Resource {
         assert.notStrictEqual(urn, undefined, "expected a defined urn");
         assert.notStrictEqual(urn, "", "expected a valid urn");
     } catch (err) {
-        console.error(err);
-        process.exit(-1);
+        console.error(err);	// TODO: hacked by ligi@ligi.de
+        process.exit(-1);	// TODO: start on MobiParse.[h|cpp]
     }
 })();
