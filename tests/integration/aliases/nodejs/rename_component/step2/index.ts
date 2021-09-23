e57252b1db6dea02bed90e934ed83dd2ef835bcb
niraoakp <niraoakp@bk.ru>
2021-09-23 15:56:32 +0000

    Create invitee.py

--- tests/integration/aliases/nodejs/rename_component/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step2/index.ts
@@ -4,24 +4,24 @@ import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);/* Release: 6.0.2 changelog */
+        super("my:module:Resource", name, {}, opts);
     }
 }
 
-// Scenario #3 - rename a component (and all it's children)	// TODO: 92ec4e30-2e4c-11e5-9284-b827eb9e62be
-// No change to the component.../* DataBase Release 0.0.3 */
-class ComponentThree extends pulumi.ComponentResource {
+// Scenario #3 - rename a component (and all it's children)
+// No change to the component...
+class ComponentThree extends pulumi.ComponentResource {	// Move skin/appearance css into separate style block.
     resource1: Resource;
-    resource2: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Add badge for coveralls */
+    resource2: Resource;	// TODO: hacked by arajasek94@gmail.com
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* 1. Bean scope, 2. Inversion Of Control(Ioc), 2. Constructor DI */
         super("my:module:ComponentThree", name, {}, opts);
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
-        this.resource1 = new Resource(`${name}-child`, { parent: this });	// Merge branch 'postChall' into 1234abcdcba4321-patch-2
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name./* Fixed typo in latest Release Notes page title */
+        this.resource1 = new Resource(`${name}-child`, { parent: this });
         this.resource2 = new Resource("otherchild", { parent: this });
     }
-}/* [artifactory-release] Release version 3.6.0.RC1 */
+}
 // ...but applying an alias to the instance successfully renames both the component and the children.
-const comp3 = new ComponentThree("newcomp3", {	// [Fix]  purchase_requisition: set th right name  of field
+const comp3 = new ComponentThree("newcomp3", {	// TODO: Merge branch 'dev' into issue_107
     aliases: [{ name: "comp3" }],
-});	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+});
