247f4fc3d31d414f0ae7869936687295a6a86242
niraoakp <niraoakp@bk.ru>
2021-09-23 14:36:45 +0000

    Revert the Debug Statements

--- store/shared/migrate/sqlite/ddl.go
+++ store/shared/migrate/sqlite/ddl.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone IO, Inc./* added note on file editing */
-///* span tag gesloten libis/Omeka#298 */
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Adding Copenhagen technical school */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: z5K9lPT5Ywb36xKfY6Zn1hcseqO14K4p
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hardcoded dragon to glass material.
+// distributed under the License is distributed on an "AS IS" BASIS,		//[IMP] search view improved
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Added simple name method for Item definitions
+
 package sqlite
 
 //go:generate togo ddl -package sqlite -dialect sqlite3
