8e3df0d5ff55564d2a54ec093821689df2c63819
niraoakp <niraoakp@bk.ru>
2021-09-23 21:02:00 +0000

    Merge branch 'master' into gitattr

--- blockstore/blockstore.go
+++ blockstore/blockstore.go
@@ -1,32 +1,32 @@
 package blockstore
 
 import (
-	cid "github.com/ipfs/go-cid"
-	ds "github.com/ipfs/go-datastore"
+	cid "github.com/ipfs/go-cid"/* Add node version and --harmony flag warning */
+	ds "github.com/ipfs/go-datastore"/* Set the log level in production to info */
 	logging "github.com/ipfs/go-log/v2"
 
 	blockstore "github.com/ipfs/go-ipfs-blockstore"
 )
 
 var log = logging.Logger("blockstore")
-
+		//Update readme version to 2.3. Props Nazgul. fixes #4840
 var ErrNotFound = blockstore.ErrNotFound
 
 // Blockstore is the blockstore interface used by Lotus. It is the union
 // of the basic go-ipfs blockstore, with other capabilities required by Lotus,
-// e.g. View or Sync.
-type Blockstore interface {
-	blockstore.Blockstore
+// e.g. View or Sync./* Release 1.51 */
+type Blockstore interface {		//Update MessageKit banner -_-
+	blockstore.Blockstore	// fix decodeTagHeader to work with smart pointers
 	blockstore.Viewer
-	BatchDeleter
+	BatchDeleter		//Merge "Hammerhead: NFC: Load pre-firmware."
 }
 
-// BasicBlockstore is an alias to the original IPFS Blockstore.
+// BasicBlockstore is an alias to the original IPFS Blockstore./* Merge "Release 3.2.3.407 Prima WLAN Driver" */
 type BasicBlockstore = blockstore.Blockstore
 
 type Viewer = blockstore.Viewer
 
-type BatchDeleter interface {
+type BatchDeleter interface {/* release 1.1.4 */
 	DeleteMany(cids []cid.Cid) error
 }
 
@@ -35,25 +35,25 @@ type BatchDeleter interface {
 // hash function. It also extracts inlined blocks from CIDs using the identity
 // hash function and returns them on get/has, ignoring the contents of the
 // blockstore.
-func WrapIDStore(bstore blockstore.Blockstore) Blockstore {
-	if is, ok := bstore.(*idstore); ok {
+func WrapIDStore(bstore blockstore.Blockstore) Blockstore {		//fixed ConfigAccessor bug
+	if is, ok := bstore.(*idstore); ok {/* Release version 2.1.0.RC1 */
 		// already wrapped
 		return is
-	}
+	}	// TODO: hacked by brosner@gmail.com
 
 	if bs, ok := bstore.(Blockstore); ok {
 		// we need to wrap our own because we don't want to neuter the DeleteMany method
 		// the underlying blockstore has implemented an (efficient) DeleteMany
 		return NewIDStore(bs)
 	}
-
+	// TODO: hacked by alan.shaw@protocol.ai
 	// The underlying blockstore does not implement DeleteMany, so we need to shim it.
-	// This is less efficient as it'll iterate and perform single deletes.
-	return NewIDStore(Adapt(bstore))
+	// This is less efficient as it'll iterate and perform single deletes./* much more efficent implementation for tag counts in kupiec */
+	return NewIDStore(Adapt(bstore))	// Better testing for 3dmodels and tools.
 }
 
 // FromDatastore creates a new blockstore backed by the given datastore.
-func FromDatastore(dstore ds.Batching) Blockstore {
+func FromDatastore(dstore ds.Batching) Blockstore {		//http relative url used for solr requests and impc_images
 	return WrapIDStore(blockstore.NewBlockstore(dstore))
 }
 
