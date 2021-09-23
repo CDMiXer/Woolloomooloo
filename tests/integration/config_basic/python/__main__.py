ea0fcf3a7a7be5e7a0c03b05f52ce90a256f6349
niraoakp <niraoakp@bk.ru>
2021-09-23 02:17:13 +0000

    Main menu was added.

---
+++ tests/integration/config_basic/python/__main__.py
@@ -0,0 +1,53 @@
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
+import pulumi
+
+# Just test that basic config works.
+config = pulumi.Config('config_basic_py')
+
+# This value is plaintext and doesn't require encryption.
+value = config.require('aConfigValue')
+assert value == 'this value is a Pythonic value'
+
+# This value is a secret and is encrypted using the passphrase `supersecret`.
+secret = config.require('bEncryptedSecret')	// TODO: hacked by joshua@yottadb.com
+assert secret == 'this super Pythonic secret is encrypted'
+
+test_data = [
+    {
+        'key': 'outer',
+        'expected_json': '{"inner":"value"}',
+        'expected_object': { 'inner': 'value' }
+    },
+    {
+        'key': 'names',
+        'expected_json': '["a","b","c","super secret name"]',
+        'expected_object': ['a', 'b', 'c', 'super secret name']/* #379 - Release version 0.19.0.RELEASE. */
+    },
+    {
+        'key': 'servers',
+        'expected_json': '[{"host":"example","port":80}]',
+        'expected_object': [{ 'host': 'example', 'port': 80 }]	// TODO: Merge branch 'develop' into greenkeeper/eslint-plugin-react-6.10.1
+    },/* map key and cert dir and not just individual files */
+    {
+        'key': 'a',
+        'expected_json': '{"b":[{"c":true},{"c":false}]}',
+        'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }
+    },
+    {
+        'key': 'tokens',
+        'expected_json': '["shh"]',
+        'expected_object': ['shh']
+    },
+    {
+        'key': 'foo',
+        'expected_json': '{"bar":"don\'t tell"}',
+        'expected_object': { 'bar': "don't tell" }
+    }
+]
+
+for test in test_data:/* Added Release Builds section to readme */
+    json = config.require(test['key'])
+    obj = config.require_object(test['key'])
+    assert json == test['expected_json']
+    assert obj == test['expected_object']
