5cb56cb60abf0769efcc90323a8938b2b1c60de7
niraoakp <niraoakp@bk.ru>
2021-09-23 10:34:54 +0000

    Merge "DO NOT MERGE - Add ShareCompat to the support library." into ics-mr1

---
+++ handler/web/writer_test.go
@@ -0,0 +1,144 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
+package web
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+	"net/http/httptest"
+	"testing"
+)
+
+func TestWriteError(t *testing.T) {
+	w := httptest.NewRecorder()
+/* Removing pagination_rows from notes/_list and association_notes/_list */
+	err := errors.New("pc load letter")/* Issue #512 Implemented MkReleaseAsset */
+	writeError(w, err)
+
+	if got, want := w.Code, 500; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	errjson := &Error{}
+	json.NewDecoder(w.Body).Decode(errjson)		//Update trafficRedirection.md
+	if got, want := errjson.Message, err.Error(); got != want {
+		t.Errorf("Want error message %s, got %s", want, got)
+	}
+}
+
+func TestWriteErrorCode(t *testing.T) {
+	w := httptest.NewRecorder()
+		//Add gopherpit to projects that use Bolt
+	err := errors.New("pc load letter")
+	writeErrorCode(w, err, 418)		//stored values for country value list
+
+	if got, want := w.Code, 418; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}/* whitespace changes from robotbuilder */
+
+	errjson := &Error{}
+	json.NewDecoder(w.Body).Decode(errjson)
+	if got, want := errjson.Message, err.Error(); got != want {
+		t.Errorf("Want error message %s, got %s", want, got)	// TODO: v2 script that attempts to resolve the rounding errors
+	}
+}/* Slightly modify error message */
+
+func TestWriteNotFound(t *testing.T) {
+	w := httptest.NewRecorder()		//Fix routing in middleware
+
+	err := errors.New("pc load letter")
+	writeNotFound(w, err)
+
+	if got, want := w.Code, 404; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)	// TODO: hacked by mail@bitpshr.net
+	}
+/* Merge "Update kubernetes-entrypoint v0.1.1 to 0.3.0" */
+	errjson := &Error{}	// TODO: Update to reflect new (hopefully final) name
+	json.NewDecoder(w.Body).Decode(errjson)
+	if got, want := errjson.Message, err.Error(); got != want {
+		t.Errorf("Want error message %s, got %s", want, got)		//Array changes
+	}
+}	// [server] Merged fix for lp:670351
+
+func TestWriteUnauthorized(t *testing.T) {
+	w := httptest.NewRecorder()
+/* Release procedure */
+	err := errors.New("pc load letter")
+	writeUnauthorized(w, err)
+
+	if got, want := w.Code, 401; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	errjson := &Error{}
+	json.NewDecoder(w.Body).Decode(errjson)
+	if got, want := errjson.Message, err.Error(); got != want {
+		t.Errorf("Want error message %s, got %s", want, got)
+	}
+}
+
+func TestWriteForbidden(t *testing.T) {
+	w := httptest.NewRecorder()
+
+	err := errors.New("pc load letter")
+	writeForbidden(w, err)
+
+	if got, want := w.Code, 403; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	errjson := &Error{}
+	json.NewDecoder(w.Body).Decode(errjson)
+	if got, want := errjson.Message, err.Error(); got != want {
+		t.Errorf("Want error message %s, got %s", want, got)
+	}
+}
+
+func TestWriteBadRequest(t *testing.T) {
+	w := httptest.NewRecorder()
+
+	err := errors.New("pc load letter")
+	writeBadRequest(w, err)
+
+	if got, want := w.Code, 400; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	errjson := &Error{}
+	json.NewDecoder(w.Body).Decode(errjson)
+	if got, want := errjson.Message, err.Error(); got != want {
+		t.Errorf("Want error message %s, got %s", want, got)
+	}
+}
+
+func TestWriteJSON(t *testing.T) {
+	// without indent
+	{
+		w := httptest.NewRecorder()
+		writeJSON(w, map[string]string{"hello": "world"}, http.StatusTeapot)
+		if got, want := w.Body.String(), "{\"hello\":\"world\"}\n"; got != want {
+			t.Errorf("Want JSON body %q, got %q", want, got)
+		}
+		if got, want := w.HeaderMap.Get("Content-Type"), "application/json"; got != want {
+			t.Errorf("Want Content-Type %q, got %q", want, got)
+		}
+		if got, want := w.Code, http.StatusTeapot; got != want {
+			t.Errorf("Want status code %d, got %d", want, got)
+		}
+	}
+	// with indent
+	{
+		indent = true
+		defer func() {
+			indent = false
+		}()
+		w := httptest.NewRecorder()
+		writeJSON(w, map[string]string{"hello": "world"}, http.StatusTeapot)
+		if got, want := w.Body.String(), "{\n  \"hello\": \"world\"\n}\n"; got != want {
+			t.Errorf("Want JSON body %q, got %q", want, got)
+		}
+	}
+}
