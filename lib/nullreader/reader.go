e00ac4fda49f1aa52631eca91a209654d1bcab73
niraoakp <niraoakp@bk.ru>
2021-09-23 13:30:14 +0000

    82cc863e-2e63-11e5-9284-b827eb9e62be

--- lib/nullreader/reader.go
+++ lib/nullreader/reader.go
@@ -1,10 +1,10 @@
-package nullreader
+package nullreader/* Add 4.1 Release information */
 
 type Reader struct{}
 
-func (Reader) Read(out []byte) (int, error) {	// TODO: CTableStore verwaltet nun eine eigene Wortliste
+func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
-	}
+	}/* Compilieren unter openSUSE wird unterstützt */
 	return len(out), nil
 }
