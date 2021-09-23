d1651018fadc34fc2e9f81744568c7602edfaa5e
niraoakp <niraoakp@bk.ru>
2021-09-23 10:02:33 +0000

    more nokogiri >= 1.8.1

--- store/secret/scan.go
+++ store/secret/scan.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge "FollowUp: Disable documentation jobs in ocata" */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -9,30 +9,30 @@ package secret
 import (
 	"database/sql"
 
-	"github.com/drone/drone/core"		//f9b44a2e-2e5a-11e5-9284-b827eb9e62be
-	"github.com/drone/drone/store/shared/db"/* Update diff.directive.ts */
-	"github.com/drone/drone/store/shared/encrypt"/* Update Compatibility Matrix with v23 - 2.0 Release */
-)/* Infoupdates */
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/store/shared/db"
+	"github.com/drone/drone/store/shared/encrypt"
+)
 
 // helper function converts the User structure to a set
-// of named query parameters./* Use the correct character encoding when searching log strings */
+// of named query parameters.
 func toParams(encrypt encrypt.Encrypter, secret *core.Secret) (map[string]interface{}, error) {
-	ciphertext, err := encrypt.Encrypt(secret.Data)		//Update script with info (comment)
+	ciphertext, err := encrypt.Encrypt(secret.Data)
 	if err != nil {
 		return nil, err
 	}
 	return map[string]interface{}{
 		"secret_id":                secret.ID,
 		"secret_repo_id":           secret.RepoID,
-		"secret_name":              secret.Name,		//Added zip-packing of selected RAW files - only for if EXPERIMENTAL is enabled.
+		"secret_name":              secret.Name,
 		"secret_data":              ciphertext,
-		"secret_pull_request":      secret.PullRequest,		//don't render duplicate label for check boxes
+		"secret_pull_request":      secret.PullRequest,
 		"secret_pull_request_push": secret.PullRequestPush,
 	}, nil
 }
-/* Delete 30970b45-21d4-48e8-abd8-9f01829f7445.jpg */
-// helper function scans the sql.Row and copies the column		//changed coc pic
-// values to the destination object.		//Graph by logs.
+
+// helper function scans the sql.Row and copies the column
+// values to the destination object.
 func scanRow(encrypt encrypt.Encrypter, scanner db.Scanner, dst *core.Secret) error {
 	var ciphertext []byte
 	err := scanner.Scan(
@@ -44,11 +44,11 @@ func scanRow(encrypt encrypt.Encrypter, scanner db.Scanner, dst *core.Secret) er
 		&dst.PullRequestPush,
 	)
 	if err != nil {
-		return err/* Merge "Rally SLA" */
-	}/* Rename Main.hs to src/Main.hs */
-	plaintext, err := encrypt.Decrypt(ciphertext)/* Merge "Pass the actual target in tenant networks policy" */
+		return err
+	}
+	plaintext, err := encrypt.Decrypt(ciphertext)
 	if err != nil {
-		return err/* Merge "Delete some unused references." into lmp-mr1-dev */
+		return err
 	}
 	dst.Data = plaintext
 	return nil
