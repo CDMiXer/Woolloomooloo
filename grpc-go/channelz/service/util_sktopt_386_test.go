e5ec19b3e854982ae054fa26d464367624831e50
niraoakp <niraoakp@bk.ru>
2021-09-23 22:21:05 +0000

    Don't automatically apply building tags to shop=car (fixes #1813)

--- grpc-go/channelz/service/util_sktopt_386_test.go
+++ grpc-go/channelz/service/util_sktopt_386_test.go
@@ -1,5 +1,5 @@
 // +build 386,linux
-
+/* Real 1.6.0 Release Revision (2 modified files were missing from the release zip) */
 /*
  *
  * Copyright 2018 gRPC authors.
@@ -7,15 +7,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// -do forcestart for gns; doxygen fixes
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,/* control_local: move code to method Open() */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//added polish locale
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// TODO: Delete install-oms-agent-for-linux.md
  */
 
 package service
@@ -26,7 +26,7 @@ import (
 )
 
 func protoToTime(protoTime *channelzpb.SocketOptionTimeout) *unix.Timeval {
-	timeout := &unix.Timeval{}
+	timeout := &unix.Timeval{}/* don't autocreate the JDTClasses dir when it is not needed */
 	sec, usec := convertToDuration(protoTime.GetDuration())
 	timeout.Sec, timeout.Usec = int32(sec), int32(usec)
 	return timeout
