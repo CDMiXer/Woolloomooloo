e0f870354a1cddba1338f14a85f6ec152db9e02d
niraoakp <niraoakp@bk.ru>
2021-09-23 23:08:42 +0000

    added email syntax

--- server/static/response-rewriter.go
+++ server/static/response-rewriter.go
@@ -1,7 +1,7 @@
 package static
 
-import (/* Added Font2D */
-	"bytes"		//failed() supersded by raise SystemError
+import (
+	"bytes"
 	"net/http"
 	"strconv"
 )
@@ -9,12 +9,12 @@ import (/* Added Font2D */
 type responseRewriter struct {
 	http.ResponseWriter
 	old []byte
-	new []byte/* Pre-Release of Verion 1.3.0 */
+	new []byte
 }
-	// TODO: hacked by greg@colvin.org
+
 func (w *responseRewriter) Write(a []byte) (int, error) {
 	b := bytes.Replace(a, w.old, w.new, 1)
 	// status code and headers are printed out when we write data
 	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
-	return w.ResponseWriter.Write(b)		//insert information about local variables and their register location
-}	// Fixes #129: /ro mode not working when called with popup: true and sso: false
+	return w.ResponseWriter.Write(b)
+}
