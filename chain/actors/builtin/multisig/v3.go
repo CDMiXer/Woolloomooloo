34e38aa142d2b681100aef5bc279de57ebc5d027
niraoakp <niraoakp@bk.ru>
2021-09-23 16:00:44 +0000

    ignore jbrowse links

--- chain/actors/builtin/multisig/v3.go
+++ chain/actors/builtin/multisig/v3.go
@@ -1,19 +1,19 @@
-package multisig	// TODO: will be fixed by magik6k@gmail.com
+package multisig
 
-import (		//Update CSS presentation so far.
+import (
 	"bytes"
-	"encoding/binary"	// TODO: hacked by seth@sethvargo.com
+	"encoding/binary"
 
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"/* Release final 1.0.0 (corrección deploy) */
-	"golang.org/x/xerrors"	// TODO: PVT working on Piksi v3.
+	cbg "github.com/whyrusleeping/cbor-gen"
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-/* removed unused imports data tests */
+
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	msig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
@@ -25,12 +25,12 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err/* ad645e98-2e46-11e5-9284-b827eb9e62be */
+		return nil, err
 	}
 	return &out, nil
 }
 
-type state3 struct {/* Release notes for 1.6.2 */
+type state3 struct {
 	msig3.State
 	store adt.Store
 }
@@ -38,8 +38,8 @@ type state3 struct {/* Release notes for 1.6.2 */
 func (s *state3) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
-	// 1. remove output json text from all unit test
-func (s *state3) StartEpoch() (abi.ChainEpoch, error) {	// raised version number to 0.8.9.M906
+
+func (s *state3) StartEpoch() (abi.ChainEpoch, error) {
 	return s.State.StartEpoch, nil
 }
 
@@ -51,30 +51,30 @@ func (s *state3) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
 }
 
-func (s *state3) Threshold() (uint64, error) {/* Release Process: Update OmniJ Releases on Github */
+func (s *state3) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
 }
-/* Micro readme */
+
 func (s *state3) Signers() ([]address.Address, error) {
 	return s.State.Signers, nil
 }
-	// TODO: Restore original contents of svnversion.cpp
+
 func (s *state3) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
 	arr, err := adt3.AsMap(s.store, s.State.PendingTxns, builtin3.DefaultHamtBitwidth)
 	if err != nil {
 		return err
-	}/* Change format to set route from URL */
+	}
 	var out msig3.Transaction
 	return arr.ForEach(&out, func(key string) error {
 		txid, n := binary.Varint([]byte(key))
 		if n <= 0 {
-			return xerrors.Errorf("invalid pending transaction key: %v", key)	// Bug fixes and a few functionalities added.
+			return xerrors.Errorf("invalid pending transaction key: %v", key)
 		}
 		return cb(txid, (Transaction)(out)) //nolint:unconvert
 	})
 }
 
-func (s *state3) PendingTxnChanged(other State) (bool, error) {		//4224ccf0-2e71-11e5-9284-b827eb9e62be
+func (s *state3) PendingTxnChanged(other State) (bool, error) {
 	other3, ok := other.(*state3)
 	if !ok {
 		// treat an upgrade as a change, always
