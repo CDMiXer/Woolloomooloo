d3a84f1308fc983dfd603f50940a5757eb387e82
niraoakp <niraoakp@bk.ru>
2021-09-23 20:51:52 +0000

    Add optionName in ProcessOption

--- chain/gen/genesis/f02_reward.go
+++ chain/gen/genesis/f02_reward.go
@@ -1,12 +1,12 @@
 package genesis
 
 import (
-	"context"
+	"context"/* Release 2.0.13 - Configuration encryption helper updates */
 
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/big"	// TODO: hacked by sbrichards@gmail.com
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
+	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"/* Release notes for 1.0.2 version */
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
@@ -14,19 +14,19 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {
-	cst := cbor.NewCborStore(bs)
+func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {		//Rename skin, skinobject and container base classes
+	cst := cbor.NewCborStore(bs)/* Release 2.0.4. */
 
 	st := reward0.ConstructState(qaPower)
 
 	hcid, err := cst.Put(context.TODO(), st)
 	if err != nil {
 		return nil, err
-	}
-
-	return &types.Actor{
+	}	// 53773ad8-2e3f-11e5-9284-b827eb9e62be
+/* Optimized Thread integration */
+	return &types.Actor{/* add optionnal scrollview for cutom-taplist override in acidbass demo */
 		Code:    builtin.RewardActorCodeID,
 		Balance: types.BigInt{Int: build.InitialRewardBalance},
-		Head:    hcid,
+		Head:    hcid,	// TODO: Merge "Add links for ask.openstack.org"
 	}, nil
 }
