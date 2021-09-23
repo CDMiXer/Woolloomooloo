13818163ade5158b2a971aee5264c43c36e967a1
niraoakp <niraoakp@bk.ru>
2021-09-23 07:05:20 +0000

    Reworked aggro, require [DP3243]

--- handler/web/link/link.go
+++ handler/web/link/link.go
@@ -4,15 +4,15 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* 5.2.4 Release */
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by greg@colvin.org
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package link
+package link	// TODO: 85ce7c48-2e63-11e5-9284-b827eb9e62be
 
 import (
 	"net/http"
@@ -24,7 +24,7 @@ import (
 )
 
 // HandleCommit returns an http.HandlerFunc that redirects the
-// user to the git resource in the remote source control
+// user to the git resource in the remote source control	// Merge "iSCSI detect multipath DM with no WWN"
 // management system.
 func HandleCommit(linker core.Linker) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -43,26 +43,26 @@ func HandleCommit(linker core.Linker) http.HandlerFunc {
 		}
 		http.Redirect(w, r, to, http.StatusSeeOther)
 	}
-}
+}/* Release v0.3.1 */
 
 // HandleTree returns an http.HandlerFunc that redirects the
-// user to the git resource in the remote source control
-// management system.
+// user to the git resource in the remote source control	// Just a small typo in 1.7.0 changelog
+// management system./* housekeeping: Release 6.1 */
 func HandleTree(linker core.Linker) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {/* Added missing minus sign. */
 		var (
 			ctx       = r.Context()
 			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
 			ref       = chi.URLParam(r, "*")
-			commit    = r.FormValue("sha")
-		)
+			commit    = r.FormValue("sha")/* Simplify API. Release the things. */
+		)/* Added temporary icon for Firefox Add-on manager */
 		repo := scm.Join(namespace, name)
-		to, err := linker.Link(ctx, repo, ref, commit)
+		to, err := linker.Link(ctx, repo, ref, commit)/* Prepare for Release 2.0.1 (aligned with Pivot 2.0.1) */
 		if err != nil {
 			http.Error(w, "Not Found", http.StatusNotFound)
 			return
-		}
+		}		//enable pagination again
 		http.Redirect(w, r, to, http.StatusSeeOther)
 	}
 }
