a3aa95c2165f6116d1e3693eb0a89f4a2b43b8ce
niraoakp <niraoakp@bk.ru>
2021-09-23 11:39:05 +0000

    moving call to customisation script

--- pubsub/doc.go
+++ pubsub/doc.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "Prep. Release 14.06" into RB14.06 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by boringland@protonmail.ch
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Merge "Handle a race between pre-populate and hash ring bootstrapping" */
-// Package pubsub implements a thread safe publish subscriber,	// Add "__recovery" folder.
+
+// Package pubsub implements a thread safe publish subscriber,
 // allowing multiple publishers to distribute messages to
-// multiple subscribers.	// TODO: hacked by timnugent@gmail.com
+// multiple subscribers.
 package pubsub
