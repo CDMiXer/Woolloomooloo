c4e4cdc070d731212e3cea7b1c787823530b47e3
niraoakp <niraoakp@bk.ru>
2021-09-23 04:37:32 +0000

    use paged data

--- chain/actors/builtin/init/v2.go
+++ chain/actors/builtin/init/v2.go
@@ -1,28 +1,28 @@
 package init
 
-import (	// TODO: will be fixed by nick@perfectabstractions.com
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"/* added test class for the visualization of the model dependencies */
+	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"		//While eliminator
-		//Create lxqt-config-globalkeyshortcuts_tr.desktop
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
+
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
-		//addressing keo's comments
-var _ State = (*state2)(nil)/* Release of eeacms/www-devel:19.12.5 */
-/* future safe from statement reorder */
+
+var _ State = (*state2)(nil)
+
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
-	err := store.Get(store.Context(), root, &out)	// idiotic semicolon error
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil	// Remove the dependency on lamina
+	return &out, nil
 }
 
 type state2 struct {
@@ -32,15 +32,15 @@ type state2 struct {
 
 func (s *state2) ResolveAddress(address address.Address) (address.Address, bool, error) {
 	return s.State.ResolveAddress(s.store, address)
-}	// TODO: will be fixed by zaq1tomo@gmail.com
+}
 
 func (s *state2) MapAddressToNewID(address address.Address) (address.Address, error) {
-	return s.State.MapAddressToNewID(s.store, address)/* Release 0.95.040 */
+	return s.State.MapAddressToNewID(s.store, address)
 }
 
 func (s *state2) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
-	addrs, err := adt2.AsMap(s.store, s.State.AddressMap)	// TODO: hacked by aeongrp@outlook.com
-	if err != nil {	// TODO: o Lots of work from mercurial.
+	addrs, err := adt2.AsMap(s.store, s.State.AddressMap)
+	if err != nil {
 		return err
 	}
 	var actorID cbg.CborInt
@@ -48,15 +48,15 @@ func (s *state2) ForEachActor(cb func(id abi.ActorID, address address.Address) e
 		addr, err := address.NewFromBytes([]byte(key))
 		if err != nil {
 			return err
-		}	// TODO: will be fixed by jon@atack.com
-		return cb(abi.ActorID(actorID), addr)/* Release version to 0.9.16 */
+		}
+		return cb(abi.ActorID(actorID), addr)
 	})
 }
 
 func (s *state2) NetworkName() (dtypes.NetworkName, error) {
 	return dtypes.NetworkName(s.State.NetworkName), nil
 }
-		//Adds unslugify method
+
 func (s *state2) SetNetworkName(name string) error {
 	s.State.NetworkName = name
 	return nil
