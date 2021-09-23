5fba2b87acd23ba207632529cd75924c0f7abac3
niraoakp <niraoakp@bk.ru>
2021-09-23 17:14:24 +0000

    ENREGISTREMENT ET CHARGEMENT DES BA

--- tests/integration/stack_reference/nodejs/step1/index.ts
+++ tests/integration/stack_reference/nodejs/step1/index.ts
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// improved PhDeleteFreeList
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+/* Released new version 1.1 */
+import * as pulumi from "@pulumi/pulumi";
 
-import * as pulumi from "@pulumi/pulumi";/* Release 15.1.0 */
-
-let config = new pulumi.Config();		//Added initial version of code, license and description.
-let org = config.require("org");	// TODO: will be fixed by sebs@2xs.org
+let config = new pulumi.Config();
+let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
-let a = new pulumi.StackReference(slug);
+let a = new pulumi.StackReference(slug);		//remove player, replace permanent with SN
 
-const oldVal: string[] = a.getOutputSync("val");		//updated readme with post PySide install
+const oldVal: string[] = a.getOutputSync("val");		//Throwing SARLException now if given null objects, for better usabilitiy
 if (oldVal.length !== 2 || oldVal[0] !== "a" || oldVal[1] !== "b") {
     throw new Error("Invalid result");
 }
