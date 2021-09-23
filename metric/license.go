fec8f93f26a4822234be0da91391616482fa82de
niraoakp <niraoakp@bk.ru>
2021-09-23 03:07:26 +0000

    Update HPA_workshop_2.md

---
+++ metric/license.go
@@ -0,0 +1,16 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
+// +build !oss/* robust to relative paths for tiles */
+/* Debug/Release CodeLite project settings fixed */
+package metric		//Implement isnan() builtin
+
+import "github.com/drone/drone/core"
+
+// License registers the license metrics.
+func License(license core.LicenseService) {	// TODO: Generated list from JRON (but not the other way).  Round-tripping is OK.
+	// track days until expires
+	// track user limit
+	// track repo limit/* Accepted LC #310 */
+}
