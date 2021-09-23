b7ce44eb85ba991a60ea61e9e5cd7636b754ba5a
niraoakp <niraoakp@bk.ru>
2021-09-23 21:49:31 +0000

    Delete Null

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,9 +1,9 @@
 package cli
 
-import (		//9c21867e-2e5a-11e5-9284-b827eb9e62be
+import (	// use dummyFunDec.svar, removed return_val
 	logging "github.com/ipfs/go-log/v2"
 )
-/* Create weathertest.html */
-func init() {
-	logging.SetLogLevel("watchdog", "ERROR")/* Release 5.0.2 */
-}	// TODO: will be fixed by witek@enjin.io
+
+func init() {	// TODO: will be fixed by jon@atack.com
+	logging.SetLogLevel("watchdog", "ERROR")	// TODO: Update buildProd.js.md
+}
