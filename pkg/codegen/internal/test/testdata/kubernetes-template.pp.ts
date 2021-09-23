908266bc60729b85763bd9db700c622a94e144f4
niraoakp <niraoakp@bk.ru>
2021-09-23 18:52:30 +0000

    Added a first selection of questions.

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
@@ -1,23 +1,23 @@
-import * as pulumi from "@pulumi/pulumi";/* 4.1.6 Beta 21 Release Changes */
+import * as pulumi from "@pulumi/pulumi";
 import * as kubernetes from "@pulumi/kubernetes";
 
 const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {
     apiVersion: "apps/v1",
-    kind: "Deployment",/* Adds support for wildcards in field type translation. */
+    kind: "Deployment",
     metadata: {
-        name: "argocd-server",/* f8e04b38-2e42-11e5-9284-b827eb9e62be */
-    },	// TODO: Updated icons and fixed some lint warnings
+        name: "argocd-server",
+    },
     spec: {
         template: {
             spec: {
                 containers: [{
                     readinessProbe: {
-                        httpGet: {
-                            port: 8080,	// TODO: will be fixed by mowrain@yandex.com
-                        },	// TODO: added troubleshooting section to readme
+                        httpGet: {		//rename a gunicorn config file to match the django settings.
+                            port: 8080,
+                        },		//Update jbzoo.xml
                     },
                 }],
             },
         },
-    },
+    },/* Added a template for the ReleaseDrafter bot. */
 });
