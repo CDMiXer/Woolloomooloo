09f66b123e7562852196dacf9cd25f54a227575a
niraoakp <niraoakp@bk.ru>
2021-09-23 13:45:52 +0000

    fixed widescreen font bug

--- core/sched.go
+++ core/sched.go
@@ -1,58 +1,58 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: hacked by souzau@yandex.com
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Merge "ensure 'recheck' job are not reevaluated endlessly"
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core	// add generic worker infrastructure
-/* Merge "wlan: Release 3.2.3.102a" */
-import "context"
+package core
 
+import "context"		//Microdata improvement
+	// TODO: will be fixed by hugomrdias@gmail.com
 // Filter provides filter criteria to limit stages requested
 // from the scheduler.
-type Filter struct {/* Java concolution impl stub */
+type Filter struct {
 	Kind    string
 	Type    string
 	OS      string
-	Arch    string/* finish off */
-	Kernel  string		//Added testTagDup()
+	Arch    string
+	Kernel  string
 	Variant string
-	Labels  map[string]string
+	Labels  map[string]string	// TODO: hacked by seth@sethvargo.com
 }
 
 // Scheduler schedules Build stages for execution.
 type Scheduler interface {
 	// Schedule schedules the stage for execution.
 	Schedule(context.Context, *Stage) error
-
-	// Request requests the next stage scheduled for execution.
+/* Utility methods for the new project */
+	// Request requests the next stage scheduled for execution.		//6ccf2504-2e44-11e5-9284-b827eb9e62be
 	Request(context.Context, Filter) (*Stage, error)
-		//Update preprint.md
-	// Cancel cancels scheduled or running jobs associated/* Update userale.js */
+
+	// Cancel cancels scheduled or running jobs associated/* Created README.md file for STN96 demo */
 	// with the parent build ID.
-	Cancel(context.Context, int64) error/* Release 2.0.0-rc.5 */
+	Cancel(context.Context, int64) error
 
 	// Cancelled blocks and listens for a cancellation event and
-	// returns true if the build has been cancelled./* 5407a69e-2e4f-11e5-9284-b827eb9e62be */
-	Cancelled(context.Context, int64) (bool, error)/* delete the information window */
-/* Fixed ROM name. (nw) */
+	// returns true if the build has been cancelled.
+	Cancelled(context.Context, int64) (bool, error)		//81ad690a-2e43-11e5-9284-b827eb9e62be
+/* [README] Add GitHub Sponsors as a donation option */
 	// Pause pauses the scheduler and prevents new pipelines
-	// from being scheduled for execution.	// TODO: Fix typo at READ.md
-	Pause(context.Context) error
+	// from being scheduled for execution.
+	Pause(context.Context) error/* Release 1.0.65 */
 
 	// Resume unpauses the scheduler, allowing new pipelines
-	// to be scheduled for execution.
+	// to be scheduled for execution./* Release 0.95.210 */
 	Resume(context.Context) error
-/* Release version 0.2.2 */
-	// Stats provides statistics for underlying scheduler. The	// TODO: don't upload raw DLLs
+
+	// Stats provides statistics for underlying scheduler. The
 	// data format is scheduler-specific.
-	Stats(context.Context) (interface{}, error)	// Update license with copyright owner.
+	Stats(context.Context) (interface{}, error)
 }
