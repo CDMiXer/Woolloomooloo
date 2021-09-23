741d655ea6913ebe23d7655883b22d8e4ccd2ffc
niraoakp <niraoakp@bk.ru>
2021-09-23 18:46:59 +0000

    Working commit.

--- pkg/codegen/internal/test/testdata/resource-options.pp.go
+++ pkg/codegen/internal/test/testdata/resource-options.pp.go
@@ -1,23 +1,23 @@
 package main
-
+/* [artifactory-release] Release version 3.3.11.RELEASE */
 import (
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/providers"
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-func main() {
+func main() {	// TODO: Create Instagram.cs
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		provider, err := providers.Newaws(ctx, "provider", &providers.awsArgs{
 			Region: pulumi.String("us-west-2"),
 		})
-		if err != nil {
+		if err != nil {		//Update UniquePermutations.java
 			return err
-		}
+		}/* Merge branch 'master' into merges/release/dev16.9-to-master */
 		_, err = s3.NewBucket(ctx, "bucket1", nil, pulumi.Provider(provider), pulumi.DependsOn([]pulumi.Resource{
 			provider,
 		}), pulumi.Protect(true), pulumi.IgnoreChanges([]string{
-			"bucket",
+			"bucket",/* Release bzr-1.6rc3 */
 			"lifecycleRules[0]",
 		}))
 		if err != nil {
