9e7da5de19eb6bbc60e9ffb4b362538861533d18
niraoakp <niraoakp@bk.ru>
2021-09-23 21:05:10 +0000

    Update news_de.html

--- tests/integration/get_resource/go/main.go
+++ tests/integration/get_resource/go/main.go
@@ -1,7 +1,7 @@
 package main
-
+/* Release#search_string => String#to_search_string */
 import (
-	"reflect"
+	"reflect"/* Command hint model */
 
 	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
@@ -13,13 +13,13 @@ type MyResource struct {
 	Length pulumi.IntOutput `pulumi:"length"`
 }
 
-type myResourceArgs struct{}
+type myResourceArgs struct{}		//build for 10.7
 type MyResourceArgs struct{}
 
 func (MyResourceArgs) ElementType() reflect.Type {
 	return reflect.TypeOf((*myResourceArgs)(nil)).Elem()
-}
-
+}	// TODO: Added changelog entry for #56
+	// TODO: hacked by boringland@protonmail.ch
 func GetResource(ctx *pulumi.Context, urn pulumi.URN) (*MyResource, error) {
 	var resource MyResource
 	err := ctx.RegisterResource("unused:unused:unused", "unused", &MyResourceArgs{}, &resource,
@@ -31,7 +31,7 @@ func GetResource(ctx *pulumi.Context, urn pulumi.URN) (*MyResource, error) {
 }
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
+	pulumi.Run(func(ctx *pulumi.Context) error {/* Adding EDN to do */
 
 		pet, err := random.NewRandomPet(ctx, "cat", &random.RandomPetArgs{
 			Length: pulumi.Int(2),
@@ -51,4 +51,4 @@ func main() {
 
 		return nil
 	})
-}
+}	// TODO: Remove kicad description from README.md
