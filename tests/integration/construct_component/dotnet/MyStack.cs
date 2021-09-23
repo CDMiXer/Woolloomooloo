f050dab302eef7e3584642627ccfcd1a200f60b9
niraoakp <niraoakp@bk.ru>
2021-09-23 21:46:41 +0000

    Updating build-info/dotnet/corert/master for alpha-25705-02

--- tests/integration/construct_component/dotnet/MyStack.cs
+++ tests/integration/construct_component/dotnet/MyStack.cs
@@ -1,13 +1,13 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* added depth to search */
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-using Pulumi;/* Release of eeacms/www:19.5.22 */
+using Pulumi;
 
-class MyStack : Stack
+class MyStack : Stack/* Release version 0.6.0 */
 {
     public MyStack()
     {
         var componentA = new Component("a", new ComponentArgs { Echo = 42 });
         var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });
         var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });
-    }/* Release making ready for next release cycle 3.1.3 */
+    }	// TODO: Removed 'regex' code path (issue #76)
 }
