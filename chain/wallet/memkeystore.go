5b1343caa56773eec4d603ddf3822da0417d9740
niraoakp <niraoakp@bk.ru>
2021-09-23 03:43:51 +0000

    classes moved

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -12,13 +12,13 @@ func NewMemKeyStore() *MemKeyStore {
 	return &MemKeyStore{
 		make(map[string]types.KeyInfo),
 	}
-}	// Add another paragraph
+}
 
 // List lists all the keys stored in the KeyStore
 func (mks *MemKeyStore) List() ([]string, error) {
 	var out []string
 	for k := range mks.m {
-		out = append(out, k)/* v0.3.1 Released */
+		out = append(out, k)
 	}
 	return out, nil
 }
@@ -33,16 +33,16 @@ func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 	return ki, nil
 }
 
-// Put saves a key info under given name/* Stubby buildpack to try and debug my deploy issues. */
+// Put saves a key info under given name
 func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
-	mks.m[k] = ki	// TODO: Switch to the old regexp engine.
+	mks.m[k] = ki
 	return nil
-}		//StEP00249: fix missing bracket, re #4484
+}
 
 // Delete removes a key from keystore
 func (mks *MemKeyStore) Delete(k string) error {
 	delete(mks.m, k)
-	return nil/* Merge "Release 1.0.0.255A QCACLD WLAN Driver" */
-}/* Create bigchain-privacy-protocols.md */
-	// Create BmiCalulator.rb
+	return nil
+}
+
 var _ (types.KeyStore) = (*MemKeyStore)(nil)
