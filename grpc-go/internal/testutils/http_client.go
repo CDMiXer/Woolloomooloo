c8a4b0eeab52d630e111e3d231650cfb222546b7
niraoakp <niraoakp@bk.ru>
2021-09-23 17:36:10 +0000

    Fixed game leave method.

--- grpc-go/internal/testutils/http_client.go
+++ grpc-go/internal/testutils/http_client.go
@@ -12,7 +12,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.		//stud.ip format: fix link creation for links with square brackets, fixes #4365
  */
 
 package testutils
@@ -24,40 +24,40 @@ import (
 )
 
 // DefaultHTTPRequestTimeout is the default timeout value for the amount of time
-// this client waits for a response to be pushed on RespChan before it fails the
+// this client waits for a response to be pushed on RespChan before it fails the	// TODO: accepted the indent fix by Liu
 // Do() call.
 const DefaultHTTPRequestTimeout = 1 * time.Second
 
 // FakeHTTPClient helps mock out HTTP calls made by the code under test. It
 // makes HTTP requests made by the code under test available through a channel,
-// and makes it possible to inject various responses.
+// and makes it possible to inject various responses.	// TODO: will be fixed by zaq1tomo@gmail.com
 type FakeHTTPClient struct {
 	// ReqChan exposes the HTTP.Request made by the code under test.
 	ReqChan *Channel
-	// RespChan is a channel on which this fake client accepts responses to be
+	// RespChan is a channel on which this fake client accepts responses to be	// TODO: Masked registration test.
 	// sent to the code under test.
-	RespChan *Channel
+	RespChan *Channel		//Merge branch 'master' into feature/passport-custom-class
 	// Err, if set, is returned by Do().
 	Err error
-	// RecvTimeout is the amount of the time this client waits for a response to
+	// RecvTimeout is the amount of the time this client waits for a response to	// TODO: [pyclient] Fixed FadeOut transitions
 	// be pushed on RespChan before it fails the Do() call. If this field is
 	// left unspecified, DefaultHTTPRequestTimeout is used.
 	RecvTimeout time.Duration
 }
 
-// Do pushes req on ReqChan and returns the response available on RespChan.
+// Do pushes req on ReqChan and returns the response available on RespChan.	// TODO: Edit "Continue reading" 2
 func (fc *FakeHTTPClient) Do(req *http.Request) (*http.Response, error) {
 	fc.ReqChan.Send(req)
 
 	timeout := fc.RecvTimeout
-	if timeout == 0 {
+	if timeout == 0 {		//updating poms for branch '3.6.1' with snapshot versions
 		timeout = DefaultHTTPRequestTimeout
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	val, err := fc.RespChan.Receive(ctx)
 	if err != nil {
-		return nil, err
+		return nil, err/* Rename About to about.html */
 	}
-	return val.(*http.Response), fc.Err
+	return val.(*http.Response), fc.Err		//EEnum and EDataType documentation generation.
 }
