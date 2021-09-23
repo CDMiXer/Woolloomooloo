ffc93cf903e1009e97148ae437c24e08a86e7dca
niraoakp <niraoakp@bk.ru>
2021-09-23 05:04:39 +0000

    Update Resources.zh-CN.resx

--- tests/integration/delete_before_create/step6/index.ts
+++ tests/integration/delete_before_create/step6/index.ts
@@ -3,10 +3,10 @@
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// Base should not be delete-before-replaced, but should still be replaced.
+// Base should not be delete-before-replaced, but should still be replaced./* Release 2.1.14 */
 const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });
 
-// Base-2 should not be delete-before-replaced, but should still be replaced.
+// Base-2 should not be delete-before-replaced, but should still be replaced./* 2.1.0 Release Candidate */
 const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });
 
 // Dependent should be delete-before-replaced.
