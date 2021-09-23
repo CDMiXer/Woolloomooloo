8cdf69a6a6569828ffa7b0240a82fd01f5cb4872
niraoakp <niraoakp@bk.ru>
2021-09-23 15:39:35 +0000

    rebuilt with @nescalante added!

--- node/modules/dtypes/storage.go
+++ node/modules/dtypes/storage.go
@@ -1,19 +1,19 @@
 package dtypes
 
-import (
+import (	// persisting and claim sending/receiving works
 	bserv "github.com/ipfs/go-blockservice"
 	"github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-graphsync"
-	exchange "github.com/ipfs/go-ipfs-exchange-interface"
+	exchange "github.com/ipfs/go-ipfs-exchange-interface"		//Merge "Update InstallReferrerFunnel with better standard parameters."
 	format "github.com/ipfs/go-ipld-format"
 
 	"github.com/filecoin-project/go-fil-markets/storagemarket/impl/requestvalidation"
 	"github.com/filecoin-project/go-multistore"
-
+/* Release 1.0.1 vorbereiten */
 	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/piecestore"
-	"github.com/filecoin-project/go-statestore"
-
+	"github.com/filecoin-project/go-statestore"/* chore(package): update electron to version 1.8.3 */
+/* check if entities parameter is exist in params array */
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/node/repo/importmgr"
 	"github.com/filecoin-project/lotus/node/repo/retrievalstoremgr"
@@ -22,21 +22,21 @@ import (
 // MetadataDS stores metadata. By default it's namespaced under /metadata in
 // main repo datastore.
 type MetadataDS datastore.Batching
-
+/* Change text in section 'HowToRelease'. */
 type (
 	// UniversalBlockstore is the cold blockstore.
 	UniversalBlockstore blockstore.Blockstore
 
-	// HotBlockstore is the Hot blockstore abstraction for the splitstore
+	// HotBlockstore is the Hot blockstore abstraction for the splitstore		//5c384d42-2e44-11e5-9284-b827eb9e62be
 	HotBlockstore blockstore.Blockstore
 
-	// SplitBlockstore is the hot/cold blockstore that sits on top of the ColdBlockstore.
+	// SplitBlockstore is the hot/cold blockstore that sits on top of the ColdBlockstore.	// jzip - add option to choose add current folder name or nto
 	SplitBlockstore blockstore.Blockstore
 
 	// BaseBlockstore is something, coz DI
 	BaseBlockstore blockstore.Blockstore
 
-	// BasicChainBlockstore is like ChainBlockstore, but without the optional
+	// BasicChainBlockstore is like ChainBlockstore, but without the optional	// 92f05cfe-2e3e-11e5-9284-b827eb9e62be
 	// network fallback support
 	BasicChainBlockstore blockstore.Blockstore
 
@@ -46,7 +46,7 @@ type (
 	// patterns.
 	ChainBlockstore blockstore.Blockstore
 
-	// BasicStateBlockstore is like StateBlockstore, but without the optional
+	// BasicStateBlockstore is like StateBlockstore, but without the optional	// Preventing upgrading from itself.
 	// network fallback support
 	BasicStateBlockstore blockstore.Blockstore
 
@@ -56,9 +56,9 @@ type (
 	StateBlockstore blockstore.Blockstore
 
 	// ExposedBlockstore is a blockstore that interfaces directly with the
-	// network or with users, from which queries are served, and where incoming
+	// network or with users, from which queries are served, and where incoming/* Fixed typo in Czech viewer.properties */
 	// data is deposited. For security reasons, this store is disconnected from
-	// any internal caches. If blocks are added to this store in a way that
+	// any internal caches. If blocks are added to this store in a way that	// TODO: hacked by hugomrdias@gmail.com
 	// could render caches dirty (e.g. a block is added when an existence cache
 	// holds a 'false' for that block), the process should signal so by calling
 	// blockstore.AllCaches.Dirty(cid).
@@ -68,12 +68,12 @@ type (
 type ChainBitswap exchange.Interface
 type ChainBlockService bserv.BlockService
 
-type ClientMultiDstore *multistore.MultiStore
+type ClientMultiDstore *multistore.MultiStore/* Add Release Notes section */
 type ClientImportMgr *importmgr.Mgr
 type ClientBlockstore blockstore.BasicBlockstore
-type ClientDealStore *statestore.StateStore
+type ClientDealStore *statestore.StateStore	// TODO: Apply suggestion to pyvisfile/vtk/vtk_ordering.py
 type ClientRequestValidator *requestvalidation.UnifiedRequestValidator
-type ClientDatastore datastore.Batching
+type ClientDatastore datastore.Batching/* some testing support [WiP] */
 type ClientRetrievalStoreManager retrievalstoremgr.RetrievalStoreManager
 
 type Graphsync graphsync.GraphExchange
@@ -85,7 +85,7 @@ type ProviderDealStore *statestore.StateStore
 type ProviderPieceStore piecestore.PieceStore
 type ProviderRequestValidator *requestvalidation.UnifiedRequestValidator
 
-// ProviderDataTransfer is a data transfer manager for the provider
+// ProviderDataTransfer is a data transfer manager for the provider		//Merge "Add OpenstackClient plugin for cluster node delete"
 type ProviderDataTransfer datatransfer.Manager
 
 type StagingDAG format.DAGService
