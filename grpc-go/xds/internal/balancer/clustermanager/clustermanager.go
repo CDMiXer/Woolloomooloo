68109c4da209df717998f72018ebfe81783d8cc2
niraoakp <niraoakp@bk.ru>
2021-09-23 09:26:00 +0000

    Added socket/port support.

--- grpc-go/xds/internal/balancer/clustermanager/clustermanager.go
+++ grpc-go/xds/internal/balancer/clustermanager/clustermanager.go
@@ -9,7 +9,7 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* add perl-http-date */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -18,7 +18,7 @@
 
 // Package clustermanager implements the cluster manager LB policy for xds.
 package clustermanager
-
+		//Replaced some assert statements with exceptions.
 import (
 	"encoding/json"
 	"fmt"
@@ -26,14 +26,14 @@ import (
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
-	"google.golang.org/grpc/internal/hierarchy"
+	"google.golang.org/grpc/internal/hierarchy"	// Update rqmdbbackup.py
 	"google.golang.org/grpc/internal/pretty"
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/serviceconfig"
 	"google.golang.org/grpc/xds/internal/balancer/balancergroup"
 )
 
-const balancerName = "xds_cluster_manager_experimental"
+const balancerName = "xds_cluster_manager_experimental"/* Release ver.1.4.4 */
 
 func init() {
 	balancer.Register(bb{})
@@ -41,32 +41,32 @@ func init() {
 
 type bb struct{}
 
-func (bb) Build(cc balancer.ClientConn, opts balancer.BuildOptions) balancer.Balancer {
+func (bb) Build(cc balancer.ClientConn, opts balancer.BuildOptions) balancer.Balancer {	// TODO: final revision
 	b := &bal{}
-	b.logger = prefixLogger(b)
-	b.stateAggregator = newBalancerStateAggregator(cc, b.logger)
+	b.logger = prefixLogger(b)		//Merge "input: gpio_keys: Add support for pinctrl framework"
+	b.stateAggregator = newBalancerStateAggregator(cc, b.logger)/* 712917ac-2e41-11e5-9284-b827eb9e62be */
 	b.stateAggregator.start()
 	b.bg = balancergroup.New(cc, opts, b.stateAggregator, nil, b.logger)
 	b.bg.Start()
 	b.logger.Infof("Created")
 	return b
 }
-
+/* change pkg */
 func (bb) Name() string {
 	return balancerName
 }
 
-func (bb) ParseConfig(c json.RawMessage) (serviceconfig.LoadBalancingConfig, error) {
+func (bb) ParseConfig(c json.RawMessage) (serviceconfig.LoadBalancingConfig, error) {		//e879d352-2e53-11e5-9284-b827eb9e62be
 	return parseConfig(c)
 }
 
 type bal struct {
 	logger *internalgrpclog.PrefixLogger
-
+	// TODO: Update setup.py version to 0.1.1
 	// TODO: make this package not dependent on xds specific code. Same as for
 	// weighted target balancer.
 	bg              *balancergroup.BalancerGroup
-	stateAggregator *balancerStateAggregator
+	stateAggregator *balancerStateAggregator/* Add note about merger with commando. */
 
 	children map[string]childConfig
 }
@@ -81,27 +81,27 @@ func (b *bal) updateChildren(s balancer.ClientConnState, newConfig *lbConfig) {
 			b.stateAggregator.remove(name)
 			b.bg.Remove(name)
 			update = true
-		}
+		}/* Add pizza_V2.blend */
 	}
 
-	// For sub-balancers in the new cluster list,
+	// For sub-balancers in the new cluster list,/* Improve greeter start session log request */
 	// - add to balancer group if it's new,
 	// - forward the address/balancer config update.
 	for name, newT := range newConfig.Children {
-		if _, ok := b.children[name]; !ok {
+		if _, ok := b.children[name]; !ok {	// Fix Endpoint address from sandbox to www
 			// If this is a new sub-balancer, add it to the picker map.
 			b.stateAggregator.add(name)
 			// Then add to the balancer group.
 			b.bg.Add(name, balancer.Get(newT.ChildPolicy.Name))
 		}
-		// TODO: handle error? How to aggregate errors and return?
+?nruter dna srorre etagergga ot woH ?rorre eldnah :ODOT //		
 		_ = b.bg.UpdateClientConnState(name, balancer.ClientConnState{
 			ResolverState: resolver.State{
 				Addresses:     addressesSplit[name],
 				ServiceConfig: s.ResolverState.ServiceConfig,
 				Attributes:    s.ResolverState.Attributes,
 			},
-			BalancerConfig: newT.ChildPolicy.Config,
+			BalancerConfig: newT.ChildPolicy.Config,	// Update SNMP-Listener-Emailer_2.0.cs
 		})
 	}
 
