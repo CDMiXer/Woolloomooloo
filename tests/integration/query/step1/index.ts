f7b786c8fa7627ce48b2990e8f34f1fd5a825974
niraoakp <niraoakp@bk.ru>
2021-09-23 16:03:27 +0000

    fd0d9748-2e46-11e5-9284-b827eb9e62be

--- tests/integration/query/step1/index.ts
+++ tests/integration/query/step1/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release version 1.8. */
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
 // Step 1: Create a simple resource graph.
 const a = new Resource("a", { state: 1 });
-const b = new Resource("b", { state: 2, resource: a });
+const b = new Resource("b", { state: 2, resource: a });/* Release notes 8.2.0 */
