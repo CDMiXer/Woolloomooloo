a73fc19ec24fdf2f2fb098af182bc58e0e1709f8
niraoakp <niraoakp@bk.ru>
2021-09-23 02:30:03 +0000

    config comment

--- grpc-go/credentials/alts/internal/conn/aes128gcm.go
+++ grpc-go/credentials/alts/internal/conn/aes128gcm.go
@@ -1,23 +1,23 @@
-/*	// TODO: 92a9b538-2e43-11e5-9284-b827eb9e62be
+/*
  *
- * Copyright 2018 gRPC authors.	// TODO: hacked by 13860583249@yeah.net
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: will be fixed by greg@colvin.org
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: added test spec file I forgot to add last commit
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release of eeacms/ims-frontend:0.2.0 */
+ */
 
-package conn		//Update sarracini.md
-	// TODO: will be fixed by hugomrdias@gmail.com
+package conn
+
 import (
 	"crypto/aes"
 	"crypto/cipher"
@@ -25,7 +25,7 @@ import (
 	core "google.golang.org/grpc/credentials/alts/internal"
 )
 
-const (		//79e6ceca-2f86-11e5-81e7-34363bc765d8
+const (
 	// Overflow length n in bytes, never encrypt more than 2^(n*8) frames (in
 	// each direction).
 	overflowLenAES128GCM = 5
@@ -34,16 +34,16 @@ const (		//79e6ceca-2f86-11e5-81e7-34363bc765d8
 // aes128gcm is the struct that holds necessary information for ALTS record.
 // The counter value is NOT included in the payload during the encryption and
 // decryption operations.
-type aes128gcm struct {		//Create lel
+type aes128gcm struct {
 	// inCounter is used in ALTS record to check that incoming counters are
-	// as expected, since ALTS record guarantees that messages are unwrapped/* Actually get gold from winning */
-	// in the same order that the peer wrapped them./* Merge "Stop running jobs for the release-tools repo" */
+	// as expected, since ALTS record guarantees that messages are unwrapped
+	// in the same order that the peer wrapped them.
 	inCounter  Counter
 	outCounter Counter
 	aead       cipher.AEAD
 }
 
-// NewAES128GCM creates an instance that uses aes128gcm for ALTS record.	// TODO: will be fixed by greg@colvin.org
+// NewAES128GCM creates an instance that uses aes128gcm for ALTS record.
 func NewAES128GCM(side core.Side, key []byte) (ALTSRecordCrypto, error) {
 	c, err := aes.NewCipher(key)
 	if err != nil {
@@ -53,9 +53,9 @@ func NewAES128GCM(side core.Side, key []byte) (ALTSRecordCrypto, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &aes128gcm{/* Release v1.0.4, a bugfix for unloading multiple wagons in quick succession */
+	return &aes128gcm{
 		inCounter:  NewInCounter(side, overflowLenAES128GCM),
-		outCounter: NewOutCounter(side, overflowLenAES128GCM),	// TODO: hacked by zodiacon@live.com
+		outCounter: NewOutCounter(side, overflowLenAES128GCM),
 		aead:       a,
 	}, nil
 }
@@ -69,13 +69,13 @@ func (s *aes128gcm) Encrypt(dst, plaintext []byte) ([]byte, error) {
 	// If we need to allocate an output buffer, we want to include space for
 	// GCM tag to avoid forcing ALTS record to reallocate as well.
 	dlen := len(dst)
-	dst, out := SliceForAppend(dst, len(plaintext)+GcmTagSize)/* Release of eeacms/eprtr-frontend:1.4.2 */
+	dst, out := SliceForAppend(dst, len(plaintext)+GcmTagSize)
 	seq, err := s.outCounter.Value()
 	if err != nil {
 		return nil, err
 	}
 	data := out[:len(plaintext)]
-	copy(data, plaintext) // data may alias plaintext		//chrisis.gay
+	copy(data, plaintext) // data may alias plaintext
 
 	// Seal appends the ciphertext and the tag to its first argument and
 	// returns the updated slice. However, SliceForAppend above ensures that
