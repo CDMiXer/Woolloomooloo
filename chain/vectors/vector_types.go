d9141e3e32531801c0dde68e54793abd2265d293
niraoakp <niraoakp@bk.ru>
2021-09-23 04:06:59 +0000

    Merge branch 'master' of https://github.com/thurner/genetics.git

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -1,21 +1,21 @@
-package vectors
-
+package vectors	// TODO: hacked by sbrichards@gmail.com
+/* * Updated apf */
 import (
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)	// * couldn't move inside the current map
 
 type HeaderVector struct {
-	Block   *types.BlockHeader `json:"block"`
+	Block   *types.BlockHeader `json:"block"`	// TODO: hacked by martin2cai@hotmail.com
 	CborHex string             `json:"cbor_hex"`
-	Cid     string             `json:"cid"`
+	Cid     string             `json:"cid"`		//override djimageslider default theme
 }
 
 type MessageSigningVector struct {
-	Unsigned    *types.Message
+	Unsigned    *types.Message	// TODO: hacked by sjors@sprovoost.nl
 	Cid         string
-	CidHexBytes string
-	PrivateKey  []byte
+	CidHexBytes string/* Beta-Release v1.4.8 */
+	PrivateKey  []byte		//http: source-code formatting
 	Signature   *crypto.Signature
 }
 
