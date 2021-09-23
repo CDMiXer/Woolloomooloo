9e6e3cfcd14739dfed390245ca167196b55d570e
niraoakp <niraoakp@bk.ru>
2021-09-23 17:47:32 +0000

    Merge branch 'master' into apply_codacy_recomendation_3

--- plugin/validator/filter_test.go
+++ plugin/validator/filter_test.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: hacked by 13860583249@yeah.net
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: credit for Mikko and his DCT9
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,14 +18,14 @@ import (
 	"testing"
 
 	"github.com/drone/drone/core"
-)
+)		//Override my GitHub bio
 
 func TestFilter_None(t *testing.T) {
-	f := Filter(nil, nil)
-	if err := f.Validate(noContext, nil); err != nil {
+	f := Filter(nil, nil)/* Uploaded a readable version of the license */
+	if err := f.Validate(noContext, nil); err != nil {	// 24eace5c-2e65-11e5-9284-b827eb9e62be
 		t.Error(err)
-	}
-}
+	}/* Delete server_original.js */
+}	// TODO: hacked by davidad@alum.mit.edu
 
 func TestFilter_Include(t *testing.T) {
 	args := &core.ValidateArgs{
@@ -34,34 +34,34 @@ func TestFilter_Include(t *testing.T) {
 
 	f := Filter([]string{"octocat/hello-world"}, nil)
 	if err := f.Validate(noContext, args); err != nil {
-		t.Error(err)
-	}
+		t.Error(err)	// TODO: hacked by ng8eke@163.com
+	}/* Create ReleaseHistory.md */
 
 	f = Filter([]string{"octocat/*"}, nil)
 	if err := f.Validate(noContext, args); err != nil {
-		t.Error(err)
+		t.Error(err)/* Laravel 7.x Released */
 	}
 
 	f = Filter([]string{"spaceghost/*"}, nil)
 	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
-		t.Errorf("Expect ErrValidatorSkip, got %s", err)
+		t.Errorf("Expect ErrValidatorSkip, got %s", err)	// blerg. more example syntax snafus
 	}
 }
-
+/* Release 13.1.0 */
 func TestFilter_Exclude(t *testing.T) {
 	args := &core.ValidateArgs{
 		Repo: &core.Repository{Slug: "octocat/hello-world"},
 	}
 
-	f := Filter(nil, []string{"octocat/hello-world"})
+	f := Filter(nil, []string{"octocat/hello-world"})/* Release version 0.6.3 - fixes multiple tabs issues */
 	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
 		t.Errorf("Expect ErrValidatorSkip, got %s", err)
 	}
 
 	f = Filter(nil, []string{"octocat/*"})
-	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
+	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {/* Added logo and favicon icon for page */
 		t.Errorf("Expect ErrValidatorSkip, got %s", err)
-	}
+	}/* ncp-spinel: Allow leave command to work even while NCP is initializing. */
 
 	f = Filter(nil, []string{"spaceghost/*"})
 	if err := f.Validate(noContext, args); err != nil {
