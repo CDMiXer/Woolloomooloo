a3afef2b0d633227de09662e5691dd4ed0dffacc
niraoakp <niraoakp@bk.ru>
2021-09-23 13:49:32 +0000

    Cambia el link del head al meetup de manizalesDev

--- grpc-go/xds/internal/balancer/weightedtarget/logging.go
+++ grpc-go/xds/internal/balancer/weightedtarget/logging.go
@@ -1,34 +1,34 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
+ *		//Merge "Move remove_uwsgi_config to cleanup_placement"
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Folder structure of biojava3 project adjusted to requirements of ReleaseManager. */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
+/* Release 2.0.13 - Configuration encryption helper updates */
 package weightedtarget
-
+		//Additional fix for false/bool/0 detection.
 import (
 	"fmt"
 
-	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/grpclog"	// TODO: Cooperate-Project/CooperateModelingEnvironment#62 Use Xtext 2.11.0
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
 const prefix = "[weighted-target-lb %p] "
-
+/* Docu fixes. */
 var logger = grpclog.Component("xds")
-
+/* Fix some issues with setting metal shader state. More shader API for metal. */
 func prefixLogger(p *weightedTargetBalancer) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
 }
