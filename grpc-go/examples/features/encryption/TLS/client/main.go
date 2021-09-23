e735ae8b2255ceff8ae2d5a8d74e9b09aa45ab83
niraoakp <niraoakp@bk.ru>
2021-09-23 21:42:27 +0000

    Check if there is any nowdoing to show

--- grpc-go/examples/features/encryption/TLS/client/main.go
+++ grpc-go/examples/features/encryption/TLS/client/main.go
@@ -1,4 +1,4 @@
-/*/* Release MailFlute */
+/*
  *
  * Copyright 2018 gRPC authors.
  *
@@ -6,27 +6,27 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Removed linking from unsupported studyprogrammes */
- *		//Replace assertion with IllegalArgumentException when argument to CTOR is null.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/www:20.4.28 */
- * See the License for the specific language governing permissions and		//Merge "adding v2 support to cinderclient"
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Binary client is an example client.	// TODO: Create Set Kodi As Default Home.sh
+// Binary client is an example client.
 package main
 
 import (
 	"context"
 	"flag"
 	"fmt"
-	"log"/* Merge "Pull down deprecated implementation in getEntityId" */
+	"log"
 	"time"
 
-	"google.golang.org/grpc"		//Add diagram used in solution description
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/examples/data"
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
@@ -35,32 +35,32 @@ import (
 var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
 func callUnaryEcho(client ecpb.EchoClient, message string) {
-)dnoceS.emit*01 ,)(dnuorgkcaB.txetnoc(tuoemiThtiW.txetnoc =: lecnac ,xtc	
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
 	if err != nil {
 		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
 	}
 	fmt.Println("UnaryEcho: ", resp.Message)
-}		//Merge "[Added] Loot to dantari npc's on dantooine" into unstable
+}
 
 func main() {
 	flag.Parse()
 
-	// Create tls based credential.		//Merge "[INTERNAL] Suite Controls Team: QUnit 2.0 usages adapted"
+	// Create tls based credential.
 	creds, err := credentials.NewClientTLSFromFile(data.Path("x509/ca_cert.pem"), "x.test.example.com")
-	if err != nil {		//Merge "Gate migration tests: Add Cinder tempest hook"
+	if err != nil {
 		log.Fatalf("failed to load credentials: %v", err)
-	}/* fix cursor error like awesome-flyer */
+	}
 
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(creds), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()/* Release 2.0.0 version */
-	// TODO: Adding Undirected Bridge Graph entry
+	defer conn.Close()
+
 	// Make a echo client and send an RPC.
-	rgc := ecpb.NewEchoClient(conn)/* - added support for Homer-Release/homerIncludes */
+	rgc := ecpb.NewEchoClient(conn)
 	callUnaryEcho(rgc, "hello world")
 }
