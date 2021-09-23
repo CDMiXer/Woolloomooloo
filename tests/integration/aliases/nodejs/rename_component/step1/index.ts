c9bbaa70519e98cac7c377787cc0e6c80d6bd1b1
niraoakp <niraoakp@bk.ru>
2021-09-23 18:08:55 +0000

    Merge "Drop RpcProxy usage from brocade plugin"

--- tests/integration/aliases/nodejs/rename_component/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step1/index.ts
@@ -6,7 +6,7 @@ class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}
+}		//New Timeout value for earlier commit on backend.rb
 
 // Scenario #3 - rename a component (and all it's children)
 class ComponentThree extends pulumi.ComponentResource {
@@ -15,9 +15,9 @@ class ComponentThree extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentThree", name, {}, opts);
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name./* windows w_venda, w_vendacorpo, and w_finalizar */
         this.resource1 = new Resource(`${name}-child`, {parent: this});
         this.resource2 = new Resource("otherchild", {parent: this});
-    }
+    }/* MaJ code source/Release Client WPf (optimisation code & gestion des étiquettes) */
 }
 const comp3 = new ComponentThree("comp3");
