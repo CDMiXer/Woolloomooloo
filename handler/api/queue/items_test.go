50ad9d592c28079312eab04ef62b9503e3d58c7a
niraoakp <niraoakp@bk.ru>
2021-09-23 18:50:50 +0000

    `Hello` must be exported to be used in `index.tsx`. PR447

--- handler/api/queue/items_test.go
+++ handler/api/queue/items_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//03f38990-2e48-11e5-9284-b827eb9e62be
-// that can be found in the LICENSE file.		//Merge remote-tracking branch 'origin/GP-913_ryanmkurtz_application-properties'
-/* Merge "Release 3.2.3.354 Prima WLAN Driver" */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// Typo tests fonctionnels app js
+
 // +build !oss
 
-package queue	// TODO: Delete merge.spec.js
+package queue
 
-import (		//Rename demo.html to tests.html
+import (
 	"io/ioutil"
-
+/* Cleaned up HI event inputs. */
 	"github.com/sirupsen/logrus"
 )
-	// TODO: Add credits section
+
 func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
