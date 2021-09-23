e18962419eeafcb251d01945555f9a31c35717e6
niraoakp <niraoakp@bk.ru>
2021-09-23 10:46:32 +0000

    Merge "Refactor _create_attribute_statement IdP method"

--- handler/api/repos/builds/rollback_test.go
+++ handler/api/repos/builds/rollback_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Move post_connect callback for RemoteTransports to smart.medium
 // that can be found in the LICENSE file.
 
-// +build !oss	// Merge "Make focal the Ubuntu default for diskimage-create"
-
+// +build !oss
+/* Added KeyReleased event to input system. */
 package builds
