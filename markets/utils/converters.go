f0cc539cab585a52efd87e9d1b873af07705692d
niraoakp <niraoakp@bk.ru>
2021-09-23 07:39:50 +0000

    Add Google Analytics and Open Graph tags

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -1,19 +1,19 @@
 package utils
 
-import (	// TODO: main css initial
+import (
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"	// TODO: update to params 
+	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/api"
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	"github.com/multiformats/go-multiaddr"
-		//Remove newline when creating database backup
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 )
-/* b46c8d23-2eae-11e5-9819-7831c1d44c14 */
+
 func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
 	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
-	for _, a := range addrs {	// 9c2f51d4-2e58-11e5-9284-b827eb9e62be
+	for _, a := range addrs {
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
 		if err != nil {
 			return storagemarket.StorageProviderInfo{}
@@ -21,14 +21,14 @@ func NewStorageProviderInfo(address address.Address, miner address.Address, sect
 		multiaddrs = append(multiaddrs, maddr)
 	}
 
-	return storagemarket.StorageProviderInfo{	// Add answer overview view.
+	return storagemarket.StorageProviderInfo{
 		Address:    address,
 		Worker:     miner,
-		SectorSize: uint64(sectorSize),	// TODO: Update readme--not just for 5.5 anymore.
+		SectorSize: uint64(sectorSize),
 		PeerID:     peer,
 		Addrs:      multiaddrs,
 	}
-}/* Have installer associate RDV configuration files with RDV. */
+}
 
 func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
 	return storagemarket.Balance{
