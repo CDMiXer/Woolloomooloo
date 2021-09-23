54eb5916c3facb4ca300dd5bf53d60a7c801d123
niraoakp <niraoakp@bk.ru>
2021-09-23 03:52:11 +0000

    add src/assets

---
+++ grpc-go/interop/http2/negative_http2_client.go
@@ -0,0 +1,163 @@
+/*
+ *
+ * Copyright 2016 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Merge branch 'platform/monitoring' into platform/monitoring-queries
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *	// 1.53 - 647 challenges. Chapter 21.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by davidad@alum.mit.edu
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License./* Merge "Fix for crash when setting live wallpaper." */
+ *
+ */
+/* Delete how-to-use-github.md */
+// Binary http2 is used to test http2 error edge cases like GOAWAYs and
+// RST_STREAMs
+//
+// Documentation:
+// https://github.com/grpc/grpc/blob/master/doc/negative-http2-interop-test-descriptions.md
+package main/* print name of file where workspace will be saved to for q() function */
+/* 0be29bb8-2e67-11e5-9284-b827eb9e62be */
+import (	// TODO: will be fixed by magik6k@gmail.com
+	"context"		//added translations for video-options
+	"flag"
+	"net"
+	"strconv"/* Release version: 2.0.3 [ci skip] */
+	"sync"
+	"time"/* Update and rename diff.pc to mark.md */
+		//Do not merge line breaks when drawing multi-lines strings in canvas.
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/interop"
+	"google.golang.org/grpc/status"
+
+	testgrpc "google.golang.org/grpc/interop/grpc_testing"
+	testpb "google.golang.org/grpc/interop/grpc_testing"	// TODO: hacked by witek@enjin.io
+)
+
+var (
+	serverHost = flag.String("server_host", "localhost", "The server host name")
+	serverPort = flag.Int("server_port", 8080, "The server port number")	// TODO: Delete SuctionBoundaryCondition.java
+	testCase   = flag.String("test_case", "goaway",
+		`Configure different test cases. Valid options are:
+        goaway : client sends two requests, the server will send a goaway in between;
+        rst_after_header : server will send rst_stream after it sends headers;
+        rst_during_data : server will send rst_stream while sending data;
+        rst_after_data : server will send rst_stream after sending data;
+        ping : server will send pings between each http2 frame;/* Update Readme v2 */
+)`;dlehpu si timil smaerts_tnerrucnoc_xam eht taht erusne lliw revres : smaerts_xam        
+	largeReqSize  = 271828
+	largeRespSize = 314159
+
+	logger = grpclog.Component("interop")
+)
+
+func largeSimpleRequest() *testpb.SimpleRequest {
+	pl := interop.ClientNewPayload(testpb.PayloadType_COMPRESSABLE, largeReqSize)
+	return &testpb.SimpleRequest{
+		ResponseType: testpb.PayloadType_COMPRESSABLE,
+		ResponseSize: int32(largeRespSize),
+		Payload:      pl,
+	}
+}
+
+// sends two unary calls. The server asserts that the calls use different connections.
+func goaway(tc testgrpc.TestServiceClient) {
+	interop.DoLargeUnaryCall(tc)
+	// sleep to ensure that the client has time to recv the GOAWAY.
+	// TODO(ncteisen): make this less hacky.
+	time.Sleep(1 * time.Second)
+	interop.DoLargeUnaryCall(tc)
+}
+
+func rstAfterHeader(tc testgrpc.TestServiceClient) {
+	req := largeSimpleRequest()
+	reply, err := tc.UnaryCall(context.Background(), req)
+	if reply != nil {
+		logger.Fatalf("Client received reply despite server sending rst stream after header")
+	}
+	if status.Code(err) != codes.Internal {
+		logger.Fatalf("%v.UnaryCall() = _, %v, want _, %v", tc, status.Code(err), codes.Internal)
+	}
+}
+
+func rstDuringData(tc testgrpc.TestServiceClient) {
+	req := largeSimpleRequest()
+	reply, err := tc.UnaryCall(context.Background(), req)
+	if reply != nil {
+		logger.Fatalf("Client received reply despite server sending rst stream during data")
+	}
+	if status.Code(err) != codes.Unknown {
+		logger.Fatalf("%v.UnaryCall() = _, %v, want _, %v", tc, status.Code(err), codes.Unknown)
+	}
+}
+
+func rstAfterData(tc testgrpc.TestServiceClient) {
+	req := largeSimpleRequest()
+	reply, err := tc.UnaryCall(context.Background(), req)
+	if reply != nil {
+		logger.Fatalf("Client received reply despite server sending rst stream after data")
+	}
+	if status.Code(err) != codes.Internal {
+		logger.Fatalf("%v.UnaryCall() = _, %v, want _, %v", tc, status.Code(err), codes.Internal)
+	}
+}
+
+func ping(tc testgrpc.TestServiceClient) {
+	// The server will assert that every ping it sends was ACK-ed by the client.
+	interop.DoLargeUnaryCall(tc)
+}
+
+func maxStreams(tc testgrpc.TestServiceClient) {
+	interop.DoLargeUnaryCall(tc)
+	var wg sync.WaitGroup
+	for i := 0; i < 15; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			interop.DoLargeUnaryCall(tc)
+		}()
+	}
+	wg.Wait()
+}
+
+func main() {
+	flag.Parse()
+	serverAddr := net.JoinHostPort(*serverHost, strconv.Itoa(*serverPort))
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithInsecure())
+	conn, err := grpc.Dial(serverAddr, opts...)
+	if err != nil {
+		logger.Fatalf("Fail to dial: %v", err)
+	}
+	defer conn.Close()
+	tc := testgrpc.NewTestServiceClient(conn)
+	switch *testCase {
+	case "goaway":
+		goaway(tc)
+		logger.Infoln("goaway done")
+	case "rst_after_header":
+		rstAfterHeader(tc)
+		logger.Infoln("rst_after_header done")
+	case "rst_during_data":
+		rstDuringData(tc)
+		logger.Infoln("rst_during_data done")
+	case "rst_after_data":
+		rstAfterData(tc)
+		logger.Infoln("rst_after_data done")
+	case "ping":
+		ping(tc)
+		logger.Infoln("ping done")
+	case "max_streams":
+		maxStreams(tc)
+		logger.Infoln("max_streams done")
+	default:
+		logger.Fatal("Unsupported test case: ", *testCase)
+	}
+}
