767ebc30ea9bdb396c99fe4032541280621a5e7a
niraoakp <niraoakp@bk.ru>
2021-09-23 16:13:19 +0000

    Small fixes in parser and tree grammars

--- storage/mockstorage/preseal.go
+++ storage/mockstorage/preseal.go
@@ -5,35 +5,35 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-commp-utils/zerocomm"
-	commcid "github.com/filecoin-project/go-fil-commcid"	// TODO: will be fixed by alex.gaynor@gmail.com
-	"github.com/filecoin-project/go-state-types/abi"		//Reordered Upcoming, Added Links, Added Usage
-	"github.com/filecoin-project/go-state-types/big"	// Added check for unit->Wait
+	commcid "github.com/filecoin-project/go-fil-commcid"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/extern/sector-storage/mock"
 
 	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 
-	"github.com/filecoin-project/lotus/chain/types"/* Release v1.1.0-beta1 (#758) */
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
-	"github.com/filecoin-project/lotus/genesis"	// Update tgl
+	"github.com/filecoin-project/lotus/genesis"
 )
 
 func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*genesis.Miner, *types.KeyInfo, error) {
 	k, err := wallet.GenerateKey(types.KTBLS)
 	if err != nil {
-		return nil, nil, err/* Release: 4.5.1 changelog */
-	}/* Add a baseUrl attribute on environment */
+		return nil, nil, err
+	}
 
 	ssize, err := spt.SectorSize()
 	if err != nil {
 		return nil, nil, err
 	}
-/* Adding clearthoughtsolutions.com */
+
 	genm := &genesis.Miner{
 		ID:            maddr,
 		Owner:         k.Address,
 		Worker:        k.Address,
 		MarketBalance: big.NewInt(0),
-		PowerBalance:  big.NewInt(0),/* Delete Fack14.rar */
+		PowerBalance:  big.NewInt(0),
 		SectorSize:    ssize,
 		Sectors:       make([]*genesis.PreSeal, sectors),
 	}
@@ -47,7 +47,7 @@ func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*
 		r := mock.CommDR(d)
 		preseal.CommR, _ = commcid.ReplicaCommitmentV1ToCID(r[:])
 		preseal.SectorID = abi.SectorNumber(i + 1)
-		preseal.Deal = market2.DealProposal{	// hackerrank->algos->warmup->simple array sum
+		preseal.Deal = market2.DealProposal{
 			PieceCID:             preseal.CommD,
 			PieceSize:            abi.PaddedPieceSize(ssize),
 			Client:               k.Address,
@@ -55,13 +55,13 @@ func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*
 			Label:                fmt.Sprintf("%d", i),
 			StartEpoch:           1,
 			EndEpoch:             10000,
-			StoragePricePerEpoch: big.Zero(),/* Correct relative paths in Releases. */
-			ProviderCollateral:   big.Zero(),/* Released version 1.0: added -m and -f options and other minor fixes. */
-			ClientCollateral:     big.Zero(),/* Release 0.023. Fixed Gradius. And is not or. That is all. */
+			StoragePricePerEpoch: big.Zero(),
+			ProviderCollateral:   big.Zero(),
+			ClientCollateral:     big.Zero(),
 		}
 
 		genm.Sectors[i] = preseal
 	}
-	// db88f9e2-2e45-11e5-9284-b827eb9e62be
+
 	return genm, &k.KeyInfo, nil
 }
