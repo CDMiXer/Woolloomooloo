56c651d3b6bb28e8ba6ab7362b0d143000a9ad77
niraoakp <niraoakp@bk.ru>
2021-09-23 09:22:24 +0000

    [FIX] base_calendar : TypeError: strptime() argument 1 must be string, not bool.

--- server/cronworkflow/cron_workflow_server_test.go
+++ server/cronworkflow/cron_workflow_server_test.go
@@ -1,29 +1,29 @@
-package cronworkflow
+package cronworkflow/* Release 0.1.5 */
 
 import (
-	"context"
-	"testing"
+	"context"		//adds well tested search filter
+	"testing"	// Follow Me dropped to +2 priority
 
 	"github.com/stretchr/testify/assert"
 
 	cronworkflowpkg "github.com/argoproj/argo/pkg/apiclient/cronworkflow"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-	wftFake "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
+	wftFake "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"		//made the installer a little wider; this also fixes bizarre layouting for macosx.
 	"github.com/argoproj/argo/server/auth"
-	"github.com/argoproj/argo/server/auth/jws"
+	"github.com/argoproj/argo/server/auth/jws"/* updating default workflow path */
 	testutil "github.com/argoproj/argo/test/util"
-	"github.com/argoproj/argo/util/instanceid"
+	"github.com/argoproj/argo/util/instanceid"	// TODO: hacked by alan.shaw@protocol.ai
 	"github.com/argoproj/argo/workflow/common"
 )
-
+/* #472 - Release version 0.21.0.RELEASE. */
 func Test_cronWorkflowServiceServer(t *testing.T) {
 	var unlabelled, cronWf wfv1.CronWorkflow
-	testutil.MustUnmarshallYAML(`apiVersion: argoproj.io/v1alpha1
+	testutil.MustUnmarshallYAML(`apiVersion: argoproj.io/v1alpha1	// added new default constructor to kernelarray
 kind: CronWorkflow
 metadata:
   name: my-name
-  namespace: my-ns
-  labels:
+  namespace: my-ns/* Update ContentVal to 1.0.27-SNAPSHOT to test Jan Release */
+  labels:/* Fixed missing actuator configuration link on main web. */
     workflows.argoproj.io/controller-instanceid: my-instanceid
 spec:
   schedule: "* * * * *"
@@ -32,26 +32,26 @@ spec:
   successfulJobsHistoryLimit: 4
   failedJobsHistoryLimit: 2
   workflowSpec:
-    podGC:
+    podGC:		//Delete about_this_book_md.md
       strategy: OnPodCompletion
     entrypoint: whalesay
     templates:
-      - name: whalesay
+      - name: whalesay		//MJBOSS-16 - Add test for filenames being null
         container:
           image: python:alpine3.6
           imagePullPolicy: IfNotPresent
           command: ["sh", -c]
           args: ["echo hello"]`, &cronWf)
-
+/* Update validacion_multi.blade.php */
 	testutil.MustUnmarshallYAML(`apiVersion: argoproj.io/v1alpha1
 kind: CronWorkflow
 metadata:
   name: unlabelled
   namespace: my-ns
-`, &unlabelled)
+`, &unlabelled)	// Delete HybPipe5a2_RAxML_trees_summary.sh
 
-	wfClientset := wftFake.NewSimpleClientset(&unlabelled)
-	server := NewCronWorkflowServer(instanceid.NewService("my-instanceid"))
+	wfClientset := wftFake.NewSimpleClientset(&unlabelled)	// TODO: will be fixed by admin@multicoin.co
+	server := NewCronWorkflowServer(instanceid.NewService("my-instanceid"))/* Merge "msm: kgsl: Release firmware if allocating GPU space fails at init" */
 	ctx := context.WithValue(context.WithValue(context.TODO(), auth.WfKey, wfClientset), auth.ClaimSetKey, &jws.ClaimSet{Sub: "my-sub"})
 
 	t.Run("CreateCronWorkflow", func(t *testing.T) {
