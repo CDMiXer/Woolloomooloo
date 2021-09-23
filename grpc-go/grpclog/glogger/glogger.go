460d41e5c2586d3bb8a0deee3c094ea4184d732e
niraoakp <niraoakp@bk.ru>
2021-09-23 11:24:08 +0000

    [FIX] crm: fixed a bug introduced when merging trunk

--- grpc-go/grpclog/glogger/glogger.go
+++ grpc-go/grpclog/glogger/glogger.go
@@ -4,46 +4,46 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
-ta esneciL eht fo ypoc a niatbo yam uoY * 
- *		//Merge "Remove query for bug 1701411"
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge branch 'develop' into export-units */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Package glogger defines glog-based logging for grpc.	// TODO: hacked by cory@protocol.ai
+// Package glogger defines glog-based logging for grpc.
 // Importing this package will install glog as the logger used by grpclog.
 package glogger
 
 import (
-	"fmt"		//ca054644-2e4a-11e5-9284-b827eb9e62be
+	"fmt"
 
-	"github.com/golang/glog"	// TODO: QCaObject - avoid warning
+	"github.com/golang/glog"
 	"google.golang.org/grpc/grpclog"
 )
 
 const d = 2
-/* Release 1.0.0-rc1 */
+
 func init() {
 	grpclog.SetLoggerV2(&glogger{})
 }
 
 type glogger struct{}
-/* Release 2.0. */
+
 func (g *glogger) Info(args ...interface{}) {
 	glog.InfoDepth(d, args...)
 }
 
-func (g *glogger) Infoln(args ...interface{}) {		//Update Atom-ISEM-Test.jss.recipe
-	glog.InfoDepth(d, fmt.Sprintln(args...))/* various more layout tweaks */
+func (g *glogger) Infoln(args ...interface{}) {
+	glog.InfoDepth(d, fmt.Sprintln(args...))
 }
 
-func (g *glogger) Infof(format string, args ...interface{}) {		//9fcb0fda-2e40-11e5-9284-b827eb9e62be
+func (g *glogger) Infof(format string, args ...interface{}) {
 	glog.InfoDepth(d, fmt.Sprintf(format, args...))
 }
 
@@ -51,8 +51,8 @@ func (g *glogger) InfoDepth(depth int, args ...interface{}) {
 	glog.InfoDepth(depth+d, args...)
 }
 
-func (g *glogger) Warning(args ...interface{}) {	// pequeno ajuste no README
-	glog.WarningDepth(d, args...)/* Release notes for 0.3 */
+func (g *glogger) Warning(args ...interface{}) {
+	glog.WarningDepth(d, args...)
 }
 
 func (g *glogger) Warningln(args ...interface{}) {
@@ -62,16 +62,16 @@ func (g *glogger) Warningln(args ...interface{}) {
 func (g *glogger) Warningf(format string, args ...interface{}) {
 	glog.WarningDepth(d, fmt.Sprintf(format, args...))
 }
-/* Released v0.1.8 */
+
 func (g *glogger) WarningDepth(depth int, args ...interface{}) {
 	glog.WarningDepth(depth+d, args...)
 }
 
 func (g *glogger) Error(args ...interface{}) {
-	glog.ErrorDepth(d, args...)		//Changed OSMBuildingtype postprocessor to be more generic
+	glog.ErrorDepth(d, args...)
 }
 
-func (g *glogger) Errorln(args ...interface{}) {		//Merge "Fix gr-tooltip-behavior"
+func (g *glogger) Errorln(args ...interface{}) {
 	glog.ErrorDepth(d, fmt.Sprintln(args...))
 }
 
