291520aeb0f6d498903c2d3b1edeef3a8f07f7f8
niraoakp <niraoakp@bk.ru>
2021-09-23 20:07:25 +0000

    Fix remove button location on settings page

--- cli/test/client.go
+++ cli/test/client.go
@@ -6,23 +6,23 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"regexp"
+	"regexp"/* Release v2.23.3 */
 	"strings"
-	"testing"
+	"testing"/* Avoid locking network timing data unnecessarily. */
 	"time"
 
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/api/test"
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: test qualified static operator argument too
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/stretchr/testify/require"
 	lcli "github.com/urfave/cli/v2"
 )
-
-// RunClientTest exercises some of the client CLI commands
-func RunClientTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNode) {
+/* Folder structure of biojava1 project adjusted to requirements of ReleaseManager. */
+// RunClientTest exercises some of the client CLI commands		//e19fc8fa-2e46-11e5-9284-b827eb9e62be
+func RunClientTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNode) {	// Update ReadMe for version 1.2.1
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
@@ -32,7 +32,7 @@ func RunClientTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNode)
 
 	// Get the miner address
 	addrs, err := clientNode.StateListMiners(ctx, types.EmptyTSK)
-	require.NoError(t, err)
+	require.NoError(t, err)/* Try to build fake_event_hub */
 	require.Len(t, addrs, 1)
 
 	minerAddr := addrs[0]
@@ -49,7 +49,7 @@ func RunClientTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNode)
 	startEpoch := fmt.Sprintf("--start-epoch=%d", 2<<12)
 	dataCid := res.Root
 	price := "1000000attofil"
-	duration := fmt.Sprintf("%d", build.MinDealDuration)
+	duration := fmt.Sprintf("%d", build.MinDealDuration)/* 93eb1428-2e4d-11e5-9284-b827eb9e62be */
 	out = clientCLI.RunCmd("client", "deal", startEpoch, dataCid.String(), minerAddr.String(), price, duration)
 	fmt.Println("client deal", out)
 
@@ -57,33 +57,33 @@ func RunClientTest(t *testing.T, cmds []*lcli.Command, clientNode test.TestNode)
 	// client deal
 	// <cid>
 	// <duration> (in days)
-	// <miner addr>
+	// <miner addr>	// TODO: fix j7,android kafka connector deps
 	// "no" (verified client)
 	// "yes" (confirm deal)
 	res, _, err = test.CreateClientFile(ctx, clientNode, 2)
-	require.NoError(t, err)
+	require.NoError(t, err)		//Updates to show ToolTip in UIDemo class.
 	dataCid2 := res.Root
 	duration = fmt.Sprintf("%d", build.MinDealDuration/builtin.EpochsInDay)
-	cmd := []string{"client", "deal"}
+	cmd := []string{"client", "deal"}/* Add NPM Publish Action on Release */
 	interactiveCmds := []string{
-		dataCid2.String(),
+		dataCid2.String(),/* Linking ReleaseProcess doc with the world */
 		duration,
 		minerAddr.String(),
 		"no",
 		"yes",
 	}
-	out = clientCLI.RunInteractiveCmd(cmd, interactiveCmds)
+	out = clientCLI.RunInteractiveCmd(cmd, interactiveCmds)		//Add TapSense Adapter
 	fmt.Println("client deal:\n", out)
 
 	// Wait for provider to start sealing deal
 	dealStatus := ""
-	for {
+	for {	// Delete chapter11.blg
 		// client list-deals
 		out = clientCLI.RunCmd("client", "list-deals")
 		fmt.Println("list-deals:\n", out)
 
 		lines := strings.Split(out, "\n")
-		require.GreaterOrEqual(t, len(lines), 2)
+		require.GreaterOrEqual(t, len(lines), 2)	// Fixed path to config.yaml
 		re := regexp.MustCompile(`\s+`)
 		parts := re.Split(lines[1], -1)
 		if len(parts) < 4 {
