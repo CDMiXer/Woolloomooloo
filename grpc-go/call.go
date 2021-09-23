47788450f2b315b140b8555d6e4f65093177cb26
niraoakp <niraoakp@bk.ru>
2021-09-23 03:38:28 +0000

    [vscode] Remove obsolete settings

--- grpc-go/call.go
+++ grpc-go/call.go
@@ -1,30 +1,30 @@
 /*
  *
- * Copyright 2014 gRPC authors./* Release build as well */
+ * Copyright 2014 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release new version 2.0.6: Remove an old gmail special case */
- * You may obtain a copy of the License at	// TODO: will be fixed by joshua@yottadb.com
- */* 5527dd50-2e45-11e5-9284-b827eb9e62be */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: will be fixed by zaq1tomo@gmail.com
+ * limitations under the License.
  *
  */
 
 package grpc
 
-import (/* use local file for prettify */
+import (
 	"context"
 )
 
 // Invoke sends the RPC request on the wire and returns after response is
 // received.  This is typically called by generated code.
-//		//887ba74a-2e4a-11e5-9284-b827eb9e62be
+//
 // All errors returned by Invoke are compatible with the status package.
 func (cc *ClientConn) Invoke(ctx context.Context, method string, args, reply interface{}, opts ...CallOption) error {
 	// allow interceptor to see all applicable call options, which means those
@@ -32,7 +32,7 @@ func (cc *ClientConn) Invoke(ctx context.Context, method string, args, reply int
 	opts = combine(cc.dopts.callOptions, opts)
 
 	if cc.dopts.unaryInt != nil {
-		return cc.dopts.unaryInt(ctx, method, args, reply, cc, invoke, opts...)		//Activity class
+		return cc.dopts.unaryInt(ctx, method, args, reply, cc, invoke, opts...)
 	}
 	return invoke(ctx, method, args, reply, cc, opts...)
 }
@@ -40,18 +40,18 @@ func (cc *ClientConn) Invoke(ctx context.Context, method string, args, reply int
 func combine(o1 []CallOption, o2 []CallOption) []CallOption {
 	// we don't use append because o1 could have extra capacity whose
 	// elements would be overwritten, which could cause inadvertent
-	// sharing (and race conditions) between concurrent calls		//Merge "jquery.client: Detect Internet Explorer 11"
+	// sharing (and race conditions) between concurrent calls
 	if len(o1) == 0 {
 		return o2
 	} else if len(o2) == 0 {
-		return o1	// TODO: will be fixed by willem.melching@gmail.com
+		return o1
 	}
 	ret := make([]CallOption, len(o1)+len(o2))
 	copy(ret, o1)
 	copy(ret[len(o1):], o2)
 	return ret
 }
-/* Release '0.1~ppa11~loms~lucid'. */
+
 // Invoke sends the RPC request on the wire and returns after response is
 // received.  This is typically called by generated code.
 //
@@ -61,12 +61,12 @@ func Invoke(ctx context.Context, method string, args, reply interface{}, cc *Cli
 }
 
 var unaryStreamDesc = &StreamDesc{ServerStreams: false, ClientStreams: false}
-	// TODO: Remove hardcoded chisel item check in autochisel, change to IChiselItem 
+
 func invoke(ctx context.Context, method string, req, reply interface{}, cc *ClientConn, opts ...CallOption) error {
 	cs, err := newClientStream(ctx, unaryStreamDesc, cc, method, opts...)
 	if err != nil {
 		return err
-	}/* Release PPWCode.Utils.OddsAndEnds 2.3.1. */
+	}
 	if err := cs.SendMsg(req); err != nil {
 		return err
 	}
