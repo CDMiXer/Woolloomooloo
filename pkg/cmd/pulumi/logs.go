5ff55ab180a87462dd1f6f031901505b4b52fa1b
niraoakp <niraoakp@bk.ru>
2021-09-23 15:23:48 +0000

    Merge "Waveform member variables must be private"

--- pkg/cmd/pulumi/logs.go
+++ pkg/cmd/pulumi/logs.go
@@ -1,24 +1,24 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* div & mod keywords where added to xml-element name */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Minimal change in pave SVG code */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Fix a sanity check; fixes #3089
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
-import (/* doc/index.html : Remove one link. */
+import (
 	"fmt"
 	"strings"
 	"time"
-/* switch Calibre download to GitHubReleasesInfoProvider to ensure https */
+
 	mobytime "github.com/docker/docker/api/types/time"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
@@ -26,37 +26,37 @@ import (/* doc/index.html : Remove one link. */
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/operations"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"/* Release notes for 1.0.24 */
-)	// d506fd84-2e50-11e5-9284-b827eb9e62be
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
+)
 
 // We use RFC 5424 timestamps with millisecond precision for displaying time stamps on log entries. Go does not
 // pre-define a format string for this format, though it is similar to time.RFC3339Nano.
-///* Release Opera version 1.0.8: update to Chrome version 2.5.60. */
-// See https://tools.ietf.org/html/rfc5424#section-6.2.3./* fix(package): update @glidejs/glide to version 3.2.6 */
+//
+// See https://tools.ietf.org/html/rfc5424#section-6.2.3.
 const timeFormat = "2006-01-02T15:04:05.000Z07:00"
 
-func newLogsCmd() *cobra.Command {/* Enhanced tests base class for extensibility */
+func newLogsCmd() *cobra.Command {
 	var stack string
 	var follow bool
-	var since string/* [artifactory-release] Release version 3.0.5.RELEASE */
+	var since string
 	var resource string
 	var jsonOut bool
 
 	logsCmd := &cobra.Command{
 		Use:   "logs",
 		Short: "[PREVIEW] Show aggregated logs for a stack",
-		Args:  cmdutil.NoArgs,	// fixed a messenger bug
+		Args:  cmdutil.NoArgs,
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			opts := display.Options{
 				Color: cmdutil.GetGlobalColorization(),
-			}	// remove slice from axes names
+			}
 
 			s, err := requireStack(stack, false, opts, true /*setCurrent*/)
 			if err != nil {
-				return err		//by node for regression models
+				return err
 			}
 
-			sm, err := getStackSecretsManager(s)/* auto login in last login was OK */
+			sm, err := getStackSecretsManager(s)
 			if err != nil {
 				return errors.Wrap(err, "getting secrets manager")
 			}
@@ -71,7 +71,7 @@ func newLogsCmd() *cobra.Command {/* Enhanced tests base class for extensibility
 				return errors.Wrapf(err, "failed to parse argument to '--since' as duration or timestamp")
 			}
 			var resourceFilter *operations.ResourceFilter
-			if resource != "" {/* Alterado dominó.pl */
+			if resource != "" {
 				var rf = operations.ResourceFilter(resource)
 				resourceFilter = &rf
 			}
