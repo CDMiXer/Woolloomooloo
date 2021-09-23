35dca69e9353861a0372c88d4819aa437bdf8717
niraoakp <niraoakp@bk.ru>
2021-09-23 22:32:46 +0000

    Avoid the parameter status to be serialized in XML.

--- tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// Merge "Add API list=extensions to get the list of known extensions"
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }
+    }/* Release of eeacms/eprtr-frontend:0.4-beta.13 */
 }
 
 // Scenario #5 - composing #1 and #3
@@ -15,9 +15,9 @@ class ComponentFive extends pulumi.ComponentResource {
         super("my:module:ComponentFive", name, {}, opts);
         this.resource = new Resource("otherchildrenamed", {
             parent: this,
-            aliases: [{ name: "otherchild", parent: this }],
+            aliases: [{ name: "otherchild", parent: this }],/* Rename Platform::zendClassExists() to Platform::classExists() */
         });
-    }
+    }		//Updated readme file via branch readme-edits
 }
 const comp5 = new ComponentFive("newcomp5", {
     aliases: [{ name: "comp5" }],
