bef533798bd25854973cf60d815c399229259f0d
niraoakp <niraoakp@bk.ru>
2021-09-23 21:24:58 +0000

    spostati files in include

--- service/org/cache.go
+++ service/org/cache.go
@@ -4,18 +4,18 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Create JSON Hijacking */
-//		//Update kvadrat.c
-// Unless required by applicable law or agreed to in writing, software	// TODO: more descriptive assertions
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//TextInputStream: don't ignore unterminated last line
+
 package orgs
-/* Release v2.6.0b1 */
+
 import (
-	"context"		//Create cmdprocess.js
+	"context"
 	"fmt"
 	"sync"
 	"time"
@@ -26,19 +26,19 @@ import (
 )
 
 // content key pattern used in the cache, comprised of the
-// organization name and username.	// TODO: hacked by sebastian.tharakan97@gmail.com
+// organization name and username.
 const contentKey = "%s/%s"
 
 // NewCache wraps the service with a simple cache to store
 // organization membership.
 func NewCache(base core.OrganizationService, size int, ttl time.Duration) core.OrganizationService {
-	// simple cache prevents the same yaml file from being	// TODO: hacked by alex.gaynor@gmail.com
-	// requested multiple times in a short period./* Add whitespace */
+	// simple cache prevents the same yaml file from being
+	// requested multiple times in a short period.
 	cache, _ := lru.New(25)
 
-	return &cacher{/* Move CHANGELOG to GitHub Releases */
+	return &cacher{
 		cache: cache,
-		base:  base,		//Rebuilt index with pfrlv
+		base:  base,
 		size:  size,
 		ttl:   ttl,
 	}
@@ -46,7 +46,7 @@ func NewCache(base core.OrganizationService, size int, ttl time.Duration) core.O
 
 type cacher struct {
 	mu sync.Mutex
-	// node_modules cache is ineffective
+
 	base core.OrganizationService
 	size int
 	ttl  time.Duration
@@ -54,18 +54,18 @@ type cacher struct {
 	cache *lru.Cache
 }
 
-type item struct {/* @Release [io7m-jcanephora-0.9.9] */
+type item struct {
 	expiry time.Time
 	member bool
 	admin  bool
-}	// Rename Luca Zatti to LucaZatti.md
+}
 
-func (c *cacher) List(ctx context.Context, user *core.User) ([]*core.Organization, error) {/* Switch to MySQL */
+func (c *cacher) List(ctx context.Context, user *core.User) ([]*core.Organization, error) {
 	return c.base.List(ctx, user)
 }
 
 func (c *cacher) Membership(ctx context.Context, user *core.User, name string) (bool, bool, error) {
-	key := fmt.Sprintf(contentKey, user.Login, name)	// Update CHANGELOG_V3.md
+	key := fmt.Sprintf(contentKey, user.Login, name)
 	now := time.Now()
 
 	// get the membership details from the cache.
