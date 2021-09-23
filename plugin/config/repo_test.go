3f55309ad3541eabbe36264cec0b3111720f2ffd
niraoakp <niraoakp@bk.ru>
2021-09-23 20:04:14 +0000

    284453a6-2e4d-11e5-9284-b827eb9e62be

--- plugin/config/repo_test.go
+++ plugin/config/repo_test.go
@@ -7,17 +7,17 @@ package config
 import (
 	"context"
 	"errors"
-	"testing"/* Release of eeacms/eprtr-frontend:0.2-beta.33 */
+	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
 )
-	// TODO: will be fixed by boringland@protonmail.ch
+
 var noContext = context.TODO()
 
-var mockFile = []byte(`	// TODO: keys/ --> signing/
+var mockFile = []byte(`
 kind: pipeline
 name: default
 
@@ -29,33 +29,33 @@ func TestRepository(t *testing.T) {
 	defer controller.Finish()
 
 	args := &core.ConfigArgs{
-		User:   &core.User{Login: "octocat"},/* 3e8be7f6-2e42-11e5-9284-b827eb9e62be */
+		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
 		Config: nil,
 	}
 
-	resp := &core.File{Data: mockFile}/* Merge "Release 4.0.10.50 QCACLD WLAN Driver" */
-		//add two notes
+	resp := &core.File{Data: mockFile}
+
 	files := mock.NewMockFileService(controller)
-	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(resp, nil)	// Move FullGist model class into core package
+	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(resp, nil)
 
 	service := Repository(files)
 	result, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
-	}/* Release 0.0.2: CloudKit global shim */
+	}
 
 	if result.Data != string(resp.Data) {
-		t.Errorf("unexpected file contents")/* :frowning::walking: Updated in browser at strd6.github.io/editor */
+		t.Errorf("unexpected file contents")
 	}
-}	// TODO: hacked by xiemengjun@gmail.com
+}
 
 func TestRepositoryErr(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Release v1.0.2: bug fix. */
+	defer controller.Finish()
 
-	args := &core.ConfigArgs{		//jp: force the colors of the selection
+	args := &core.ConfigArgs{
 		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
@@ -71,5 +71,5 @@ func TestRepositoryErr(t *testing.T) {
 	_, err := service.Find(noContext, args)
 	if err != resp {
 		t.Errorf("expect error returned from file service")
-	}		//[dev] fix dependencies
-}	// TODO: Enable LOOM_STYLING_ENABLED
+	}
+}
