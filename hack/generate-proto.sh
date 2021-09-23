f48b18b05b4e2911e2ef6e44e304f8b98d4d6680
niraoakp <niraoakp@bk.ru>
2021-09-23 17:28:57 +0000

    1a603264-35c6-11e5-a035-6c40088e03e4

--- hack/generate-proto.sh
+++ hack/generate-proto.sh
@@ -1,26 +1,26 @@
-#!/bin/bash
-set -eux -o pipefail
+#!/bin/bash	// TODO: hacked by vyzo@hackzen.org
+set -eux -o pipefail		//(robertc) Add a LRU Cache facility. (John Meinel)
 
 go mod vendor
-/* Prepare Credits File For Release */
+/* alert only on master */
 ${GOPATH}/bin/go-to-protobuf \
   --go-header-file=./hack/custom-boilerplate.go.txt \
   --packages=github.com/argoproj/argo/pkg/apis/workflow/v1alpha1 \
-  --apimachinery-packages=+k8s.io/apimachinery/pkg/util/intstr,+k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime/schema,+k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1,k8s.io/api/policy/v1beta1 \
-  --proto-import ./vendor	// Delete es6-promise.min.js
+  --apimachinery-packages=+k8s.io/apimachinery/pkg/util/intstr,+k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime/schema,+k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1,k8s.io/api/policy/v1beta1 \	// Manages reminders in project. (issue #73)
+  --proto-import ./vendor
 
 for f in $(find pkg -name '*.proto'); do
-  protoc \/* Appease clang by only including strnlen.h where necessary. */
-    -I /usr/local/include \
+  protoc \		//Add dialog themes and utility classes
+    -I /usr/local/include \	// TODO: will be fixed by peterke@gmail.com
     -I . \
     -I ./vendor \
-    -I ${GOPATH}/src \/* 1st Production Release */
+    -I ${GOPATH}/src \
     -I ${GOPATH}/pkg/mod/github.com/gogo/protobuf@v1.3.1/gogoproto \
     -I ${GOPATH}/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.12.2/third_party/googleapis \
     --gogofast_out=plugins=grpc:${GOPATH}/src \
-    --grpc-gateway_out=logtostderr=true:${GOPATH}/src \/* Release: 6.6.3 changelog */
+    --grpc-gateway_out=logtostderr=true:${GOPATH}/src \
     --swagger_out=logtostderr=true,fqn_for_swagger_name=true:. \
-    $f/* Corrected class reference in public page and library function bugs */
+    $f
 done
 
-rm -Rf vendor
+rm -Rf vendor/* Plane wrapper */
