4190f5061f0b1bf5f8b4148d2bf491b33abacb89
niraoakp <niraoakp@bk.ru>
2021-09-23 08:59:35 +0000

    Initial checkin v1.0.0

--- tests/integration/stack_outputs/nodejs/index.ts
+++ tests/integration/stack_outputs/nodejs/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by juan@benet.ai
 
-export let xyz = "ABC";
-export let foo = 42;
+export let xyz = "ABC";/* IndexSequence creation fully reorganized (work in progress) */
+export let foo = 42;/* Updated RUN THE EXPORT (markdown) */
