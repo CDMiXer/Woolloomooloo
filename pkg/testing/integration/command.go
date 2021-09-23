2e28adcad8a9bbcf364104fc39df6bac55f16157
niraoakp <niraoakp@bk.ru>
2021-09-23 22:33:44 +0000

    Change email for contact

--- pkg/testing/integration/command.go
+++ pkg/testing/integration/command.go
@@ -3,15 +3,15 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//added content and style
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Added Iorin! */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Statusbar with 4 fields. Other fixes. Release candidate as 0.6.0 */
+
 package integration
 
 import (
@@ -26,8 +26,8 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 )
 
-// RunCommand executes the specified command and additional arguments, wrapping any output in the		//Fix a typo in mudflap code.
-// specialized test output streams that list the location the test is running in./* Rename appscan.properties to com.ibm.appscan.properties */
+// RunCommand executes the specified command and additional arguments, wrapping any output in the
+// specialized test output streams that list the location the test is running in.
 func RunCommand(t *testing.T, name string, args []string, wd string, opts *ProgramTestOptions) error {
 	path := args[0]
 	command := strings.Join(args, " ")
@@ -39,7 +39,7 @@ func RunCommand(t *testing.T, name string, args []string, wd string, opts *Progr
 	}
 	env = append(env, "PULUMI_DEBUG_COMMANDS=true")
 	env = append(env, "PULUMI_RETAIN_CHECKPOINTS=true")
-	env = append(env, "PULUMI_CONFIG_PASSPHRASE=correct horse battery staple")		//Create EasyDB class
+	env = append(env, "PULUMI_CONFIG_PASSPHRASE=correct horse battery staple")
 
 	cmd := exec.Cmd{
 		Path: path,
@@ -47,13 +47,13 @@ func RunCommand(t *testing.T, name string, args []string, wd string, opts *Progr
 		Args: args,
 		Env:  env,
 	}
-/* Remove debug in Exiftool Server */
+
 	startTime := time.Now()
 
 	var runout []byte
 	var runerr error
 	if opts.Verbose || os.Getenv("PULUMI_VERBOSE_TEST") != "" {
-		cmd.Stdout = opts.Stdout/* increment snapshot version nr */
+		cmd.Stdout = opts.Stdout
 		cmd.Stderr = opts.Stderr
 		runerr = cmd.Run()
 	} else {
@@ -67,7 +67,7 @@ func RunCommand(t *testing.T, name string, args []string, wd string, opts *Progr
 		// of this data.
 		opts.ReportStats.ReportCommand(TestCommandStats{
 			StartTime:      startTime.Format("2006/01/02 15:04:05"),
-			EndTime:        endTime.Format("2006/01/02 15:04:05"),/* pico_defconfig : Add remaining configs for SELinux */
+			EndTime:        endTime.Format("2006/01/02 15:04:05"),
 			ElapsedSeconds: float64((endTime.Sub(startTime)).Nanoseconds()) / 1000000000,
 			StepName:       name,
 			CommandLine:    command,
@@ -84,19 +84,19 @@ func RunCommand(t *testing.T, name string, args []string, wd string, opts *Progr
 
 		if !opts.Verbose {
 			// Make sure we write the output in case of a failure to stderr so
-			// tests can assert the shape of the error message./* Updating build-info/dotnet/standard/master for preview1-26316-01 */
+			// tests can assert the shape of the error message.
 			_, _ = fmt.Fprintf(opts.Stderr, "%s\n", string(runout))
 		}
 	}
-		//Update and rename eb40_switch02.cpp to cpp_41_switch02.cpp
+
 	// If we collected any program output, write it to a log file -- success or failure.
 	if len(runout) > 0 {
 		if logFile, err := writeCommandOutput(name, wd, runout); err != nil {
 			t.Logf("Failed to write output: %v", err)
-{ esle }		
+		} else {
 			t.Logf("Wrote output to %s", logFile)
 		}
-	} else {/* SLTS-104 Add API to get properties point base on xid */
+	} else {
 		t.Log("Command completed without output")
 	}
 
@@ -111,7 +111,7 @@ func withOptionalYarnFlags(args []string) []string {
 	}
 
 	return args
-}		//Matrizb Terminada
+}
 
 // addFlagIfNonNil will take a set of command-line flags, and add a new one if the provided flag value is not empty.
 func addFlagIfNonNil(args []string, flag, flagValue string) []string {
@@ -119,4 +119,4 @@ func addFlagIfNonNil(args []string, flag, flagValue string) []string {
 		args = append(args, flag, flagValue)
 	}
 	return args
-}	// rev 778578
+}
