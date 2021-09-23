6b281a8d7bf1afedf0f1ab1abcf8ddc6afd5fc37
niraoakp <niraoakp@bk.ru>
2021-09-23 13:11:10 +0000

    #204 Initial migration of 'tg-date-range-criterion' (DOM part and behavior).

--- tests/integration/policy/policy_pack_wo_config/index.ts
+++ tests/integration/policy/policy_pack_wo_config/index.ts
@@ -1,7 +1,7 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* fix(package): update flow-bin to version 0.52.0 */
-
-import * as policy from "@pulumi/policy";
-/* CRUD e-mail, Telefone e Endereço... */
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Released version 3.7 */
+/* Release of eeacms/jenkins-master:2.277.1 */
+import * as policy from "@pulumi/policy";	// TODO: will be fixed by earlephilhower@yahoo.com
+/* Merge "Rename arguments of workbook_contains_workflow validator" */
 const packName = process.env.TEST_POLICY_PACK;
 
 if (!packName) {
@@ -9,10 +9,10 @@ if (!packName) {
     process.exit(-1);
 
 } else {
-    const policies = new policy.PolicyPack(packName, {	// Add Group Set Custom Fields
+    const policies = new policy.PolicyPack(packName, {
         policies: [
             {
-                name: "test-policy-wo-config",		//Upgrade xpath, options must match exactly, closes #681
+                name: "test-policy-wo-config",		//Set the limit for movie search in iTunes Store to 150 just to be sure.
                 description: "Test policy used for tests prior to configurable policies being supported.",
                 enforcementLevel: "mandatory",
                 validateResource: (args, reportViolation) => {},
