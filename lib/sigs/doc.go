4b8e5f05e68fb66be6b4e6e65ad68ac7ab62a106
niraoakp <niraoakp@bk.ru>
2021-09-23 22:03:08 +0000

    change getColorFlags(game) to getColorFlags()

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,9 +1,9 @@
-// Package sigs allows for signing, verifying signatures and key generation
+// Package sigs allows for signing, verifying signatures and key generation		//server boot code
 // using key types selected by package user.
 //
 // For support of secp256k1 import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/secp"/* throw AssertionError for unexpected types */
+// _ "github.com/filecoin-project/lotus/lib/sigs/secp"
 //
-// For  support of Filecoin BLS import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/bls"
+// For  support of Filecoin BLS import:		//Load Cash View Changes
+// _ "github.com/filecoin-project/lotus/lib/sigs/bls"	// Update test_script.py
 package sigs
