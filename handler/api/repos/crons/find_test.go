65e9807a40d4bcf7b764963efb164679dc65e8f5
niraoakp <niraoakp@bk.ru>
2021-09-23 19:28:07 +0000

    Norwegian in installer

--- handler/api/repos/crons/find_test.go
+++ handler/api/repos/crons/find_test.go
@@ -2,24 +2,24 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss		//renaming GNUNET_CRYPTO_get_host_identity to GNUNET_CRYPTO_get_peer_identity
+// +build !oss
 
-package crons	// TODO: Update nitro.app.src
-/* ndb - reduce MCP_BUG53205 a little more */
+package crons
+
 import (
 	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"/* Release v4.1.4 [ci skip] */
+	"testing"
 
-	"github.com/drone/drone/core"		//got the screen correctly split in half
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"	// First information on Google Cloud Services
+	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"	// TODO: hacked by mail@bitpshr.net
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestHandleFind(t *testing.T) {
@@ -29,23 +29,23 @@ func TestHandleFind(t *testing.T) {
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
-	crons := mock.NewMockCronStore(controller)/* Release sos 0.9.14 */
+	crons := mock.NewMockCronStore(controller)
 	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)
-	// Some other distributions work now.
-	c := new(chi.Context)/* Changed version to 141217, this commit is Release Candidate 1 */
-	c.URLParams.Add("owner", "octocat")	// TODO: will be fixed by timnugent@gmail.com
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("cron", "nightly")
 
-	w := httptest.NewRecorder()/* Fixing transfer/transaction button link visibility on account summary page */
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(	// Add name key
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleFind(repos, crons).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* rev 618782 */
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := &core.Cron{}, dummyCron
@@ -55,7 +55,7 @@ func TestHandleFind(t *testing.T) {
 	}
 }
 
-func TestHandleFind_RepoNotFound(t *testing.T) {	// TODO: will be fixed by why@ipfs.io
+func TestHandleFind_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -74,7 +74,7 @@ func TestHandleFind_RepoNotFound(t *testing.T) {	// TODO: will be fixed by why@i
 	)
 
 	HandleFind(repos, nil).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusNotFound; want != got {	// TODO: will be fixed by igor@soramitsu.co.jp
+	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
