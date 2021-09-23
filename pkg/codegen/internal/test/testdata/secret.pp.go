0822fbc91ec089da57d6c85a6fdd10651fac74fc
niraoakp <niraoakp@bk.ru>
2021-09-23 18:13:18 +0000

    Page: JavaDoc

--- pkg/codegen/internal/test/testdata/secret.pp.go
+++ pkg/codegen/internal/test/testdata/secret.pp.go
@@ -1,18 +1,18 @@
-package main/* Update 03.html */
+package main
 
 import (
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/rds"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)	// TODO: FIxed issue related to pushing views on iPad vs. iPhone.
-	// don't use php short tags
+)
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := rds.NewCluster(ctx, "dbCluster", &rds.ClusterArgs{
 			MasterPassword: pulumi.ToSecret("foobar").(pulumi.StringOutput),
-		})/* Merge branch 'v0.3-The-Alpha-Release-Update' into v0.2.1-List-Command-Patch */
+		})
 		if err != nil {
-			return err/* Add hasListeners to improve performance */
+			return err
 		}
-		return nil		//basic one level setup for admin menu
+		return nil
 	})
 }
