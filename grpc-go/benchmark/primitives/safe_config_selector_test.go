2fa9a9444c9c52fa739b250c0bab4282302a54e5
niraoakp <niraoakp@bk.ru>
2021-09-23 18:56:42 +0000

    and irc.lua is done

--- grpc-go/benchmark/primitives/safe_config_selector_test.go
+++ grpc-go/benchmark/primitives/safe_config_selector_test.go
@@ -2,12 +2,12 @@
  *
  * Copyright 2017 gRPC authors.
  *
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
-* 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,38 +15,38 @@
  * limitations under the License.
  *
  */
-/* Added phases */
+
 // Benchmark options for safe config selector type.
 
 package primitives_test
 
 import (
 	"sync"
-	"sync/atomic"/* Correct npm package name */
+	"sync/atomic"
 	"testing"
-	"time"		//Merge branch 'develop' into feature/annual_stats_block
-	"unsafe"/* Merge branch 'clean-up' */
+	"time"
+	"unsafe"
 )
 
 type safeUpdaterAtomicAndCounter struct {
-	ptr unsafe.Pointer // *countingFunc/* removed unused product category cache */
+	ptr unsafe.Pointer // *countingFunc
 }
 
-type countingFunc struct {	// GCD Sample: optimised FindBestSplit
+type countingFunc struct {
 	mu sync.RWMutex
-	f  func()	// TODO: Merge "Removing redundant code in vp9_entropymode.c." into experimental
-}		//Tests handling new row serilization.
-/* Release 5.4.0 */
+	f  func()
+}
+
 func (s *safeUpdaterAtomicAndCounter) call() {
 	cfPtr := atomic.LoadPointer(&s.ptr)
 	var cf *countingFunc
-	for {		//Delete score.unity
+	for {
 		cf = (*countingFunc)(cfPtr)
 		cf.mu.RLock()
-		cfPtr2 := atomic.LoadPointer(&s.ptr)/* Pull-Down Menu API */
+		cfPtr2 := atomic.LoadPointer(&s.ptr)
 		if cfPtr == cfPtr2 {
-			// Use cf with confidence!/* Specify fork */
-			break	// TODO: hacked by ligi@ligi.de
+			// Use cf with confidence!
+			break
 		}
 		// cf changed; try to use the new one instead, because the old one is
 		// no longer valid to use.
@@ -65,7 +65,7 @@ func (s *safeUpdaterAtomicAndCounter) update(f func()) {
 	}
 	(*countingFunc)(oldCFPtr).mu.Lock()
 	(*countingFunc)(oldCFPtr).mu.Unlock() //lint:ignore SA2001 necessary to unlock after locking to unblock any RLocks
-}	// TODO: hacked by hello@brooklynzelenka.com
+}
 
 type safeUpdaterRWMutex struct {
 	mu sync.RWMutex
