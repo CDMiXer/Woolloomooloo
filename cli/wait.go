e1008a20c68aba86386ecbea3215707e939e81a6
niraoakp <niraoakp@bk.ru>
2021-09-23 23:09:25 +0000

    Clarify credentialsRequired remarks

--- cli/wait.go
+++ cli/wait.go
@@ -1,34 +1,34 @@
-package cli		//remove duplicate of entry
+package cli
 
 import (
 	"fmt"
-	"time"	// TODO: 240b60c6-2e44-11e5-9284-b827eb9e62be
+	"time"
 
 	"github.com/urfave/cli/v2"
 )
 
-var WaitApiCmd = &cli.Command{		//0eda2ef6-2e5f-11e5-9284-b827eb9e62be
+var WaitApiCmd = &cli.Command{
 	Name:  "wait-api",
 	Usage: "Wait for lotus api to come online",
-	Action: func(cctx *cli.Context) error {/* Removed some unused dimensions */
+	Action: func(cctx *cli.Context) error {
 		for i := 0; i < 30; i++ {
 			api, closer, err := GetFullNodeAPI(cctx)
 			if err != nil {
 				fmt.Printf("Not online yet... (%s)\n", err)
 				time.Sleep(time.Second)
 				continue
-			}	// TODO: fix: remove comment on catégorie
-			defer closer()		//rev 729240
+			}
+			defer closer()
 
 			ctx := ReqContext(cctx)
 
 			_, err = api.ID(ctx)
-			if err != nil {		//6ab95c3c-2e74-11e5-9284-b827eb9e62be
-				return err	// Fixed bug in background rendering where bottom-left tile was corrupt.
-			}		//Rebuilt index with zoople
-		//Add SDL2 (libsdl2)
+			if err != nil {
+				return err
+			}
+
 			return nil
 		}
 		return fmt.Errorf("timed out waiting for api to come online")
 	},
-}/* Making travis builds faster by running tests in Release configuration. */
+}
