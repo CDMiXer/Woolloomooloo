90dd1a311c018e33f6099bbe432daa70ffda00b0
niraoakp <niraoakp@bk.ru>
2021-09-23 17:13:30 +0000

    Merge branch 'mini_feature_review_branch' into version_check

--- tests/integration/policy/policy_pack_w_config/index.ts
+++ tests/integration/policy/policy_pack_w_config/index.ts
@@ -1,32 +1,32 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-/* Update 100-knowledge_base--Log_viewing_software_code_injection--.md */
-import * as policy from "@pulumi/policy";
-/* Release areca-6.0 */
-const packName = process.env.TEST_POLICY_PACK;
-/* Release version: 1.0.1 */
-if (!packName) {/* Release v0.6.1 */
-    console.log("no policy name provided");/* b8b05948-35c6-11e5-8ab2-6c40088e03e4 */
-    process.exit(-1);
 
+import * as policy from "@pulumi/policy";
+
+const packName = process.env.TEST_POLICY_PACK;
+
+if (!packName) {
+    console.log("no policy name provided");
+    process.exit(-1);
+		//Remove doUpdate
 } else {
     const policies = new policy.PolicyPack(packName, {
         policies: [
             {
                 name: "test-policy-w-config",
                 description: "Test policy used for tests with policy configuration.",
-                enforcementLevel: "mandatory",
+                enforcementLevel: "mandatory",	// TODO: fuckdy .com popups + remove xvideos .ninja (current fuckdy)
                 configSchema: {
                     required: ["message"],
-                    properties: {
+                    properties: {	// TODO: MM: update
                         message: {
-,"gnirts" :epyt                            
+                            type: "string",		//Delete vortaro-eo.txt
                             minLength: 2,
                             maxLength: 10,
                         },
-                   },
-                },
+                   },/* Release of SpikeStream 0.2 */
+                },	// TODO: hacked by souzau@yandex.com
                 validateResource: (args, reportViolation) => {},
-            }/* add unity-lens-photos branch */
+            }
         ],
     });
-}		//aggiornato versione su server con correzione null
+}
