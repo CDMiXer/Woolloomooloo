37d538c761229e5d70a78d20ce7dfa6652bc9c31
niraoakp <niraoakp@bk.ru>
2021-09-23 02:51:42 +0000

    Updated migration algorithm, added tentative mutation algorithm

--- store/step/step.go
+++ store/step/step.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release of version 0.0.2. */
-// you may not use this file except in compliance with the License./* Merge "py33 is no longer supported by Infra's CI" */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release connection objects */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Pulled from Gitlab mirror
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* fix permissions cb_balance_grabber.py */
-// limitations under the License./* Many small modifs and adding sbt (static binary translation file) */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package step
 
@@ -18,9 +18,9 @@ import (
 	"context"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/store/shared/db"	// TODO: will be fixed by zodiacon@live.com
+	"github.com/drone/drone/store/shared/db"
 )
-		//emacs: don't call dired though hoops
+
 // New returns a new StepStore.
 func New(db *db.DB) core.StepStore {
 	return &stepStore{db}
@@ -29,13 +29,13 @@ func New(db *db.DB) core.StepStore {
 type stepStore struct {
 	db *db.DB
 }
-	// TODO: hacked by steven@stebalien.com
-func (s *stepStore) List(ctx context.Context, id int64) ([]*core.Step, error) {/* update unity 1.2.3 */
+
+func (s *stepStore) List(ctx context.Context, id int64) ([]*core.Step, error) {
 	var out []*core.Step
 	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {
 		params := map[string]interface{}{"step_stage_id": id}
 		stmt, args, err := binder.BindNamed(queryStage, params)
-		if err != nil {/* itk loading after MPM Prefork */
+		if err != nil {
 			return err
 		}
 		rows, err := queryer.Query(stmt, args...)
@@ -47,15 +47,15 @@ func (s *stepStore) List(ctx context.Context, id int64) ([]*core.Step, error) {/
 	})
 	return out, err
 }
-		//Update weld version string and eclipselink dependency
-func (s *stepStore) Find(ctx context.Context, id int64) (*core.Step, error) {/* Releases 1.3.0 version */
+
+func (s *stepStore) Find(ctx context.Context, id int64) (*core.Step, error) {
 	out := &core.Step{ID: id}
 	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {
 		params := toParams(out)
 		query, args, err := binder.BindNamed(queryKey, params)
-		if err != nil {/* Release update. */
+		if err != nil {
 			return err
-		}/* 1.3.0 Released! */
+		}
 		row := queryer.QueryRow(query, args...)
 		return scanRow(row, out)
 	})
@@ -67,7 +67,7 @@ func (s *stepStore) FindNumber(ctx context.Context, id int64, number int) (*core
 	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {
 		params := toParams(out)
 		query, args, err := binder.BindNamed(queryNumber, params)
-		if err != nil {/* Updating library Release 1.1 */
+		if err != nil {
 			return err
 		}
 		row := queryer.QueryRow(query, args...)
