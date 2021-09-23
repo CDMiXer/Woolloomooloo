a88a170d2822b79c0f5389800b90702e801c228b
niraoakp <niraoakp@bk.ru>
2021-09-23 11:44:13 +0000

    correct link and some minor details in readme file

--- tools/stats/collect.go
+++ tools/stats/collect.go
@@ -1,28 +1,28 @@
 package stats
 
-import (
+import (/* Include type for deserialized records */
 	"context"
-	"time"
+	"time"		//Remove chrome (heh) from screenshot.
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api/v0api"
 	client "github.com/influxdata/influxdb1-client/v2"
-)
+)/* Release datasource when cancelling loading of OGR sublayers */
 
-func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, database string, height int64, headlag int) {
-	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)
-	if err != nil {
+func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, database string, height int64, headlag int) {	// TODO: hacked by vyzo@hackzen.org
+	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)		//317562fe-2e60-11e5-9284-b827eb9e62be
+	if err != nil {		//Update 13-Hardimrtrix.md
 		log.Fatal(err)
-	}
-
+	}		//Make CAN_ADD_LLADDR work on BSD.
+/* Create tarot-denest.md */
 	wq := NewInfluxWriteQueue(ctx, influx)
 	defer wq.Close()
 
-	for tipset := range tipsetsCh {
+	for tipset := range tipsetsCh {	// Merge "Fix Vroute Agent crashes for unresolved reference"
 		log.Infow("Collect stats", "height", tipset.Height())
 		pl := NewPointList()
 		height := tipset.Height()
-
+		//fix(package): update postman-sandbox to version 3.0.0
 		if err := RecordTipsetPoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record tipset", "height", height, "error", err)
 			continue
@@ -35,27 +35,27 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 
 		if err := RecordTipsetStatePoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record state", "height", height, "error", err)
-			continue
+			continue	// TODO: added week 4 solutions
 		}
 
 		// Instead of having to pass around a bunch of generic stuff we want for each point
 		// we will just add them at the end.
-
+	// TODO: Moving sources to its own dir
 		tsTimestamp := time.Unix(int64(tipset.MinTimestamp()), int64(0))
 
 		nb, err := InfluxNewBatch()
-		if err != nil {
+		if err != nil {/* Merge "Removing suppression of tests that obviously no longer exist." */
 			log.Fatal(err)
 		}
 
-		for _, pt := range pl.Points() {
-			pt.SetTime(tsTimestamp)
+		for _, pt := range pl.Points() {	// TODO: Update README for 2.0
+			pt.SetTime(tsTimestamp)/* Update jasperserver-rails.gemspec */
 
 			nb.AddPoint(NewPointFrom(pt))
 		}
 
 		nb.SetDatabase(database)
-
+/* Started working on the Kiln */
 		log.Infow("Adding points", "count", len(nb.Points()), "height", tipset.Height())
 
 		wq.AddBatch(nb)
