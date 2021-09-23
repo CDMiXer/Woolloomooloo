016ed25b3da4ce8e89a8f3a4712c3e5c638a162e
niraoakp <niraoakp@bk.ru>
2021-09-23 21:06:31 +0000

    Did some refactorings and comment updates in ContinuousCircleLineSegmentTest.

--- handler/api/repos/crons/exec.go
+++ handler/api/repos/crons/exec.go
@@ -1,32 +1,32 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Merge branch 'idea173.x-pr/393' */
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package crons
 
-import (/* improved XML utilities */
+import (
 	"context"
 	"fmt"
 	"net/http"
-
+/* 5ys4V9foF5eM0pKAc50lQmu2P4bb67Ok */
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"	// TODO: f3471722-2e5d-11e5-9284-b827eb9e62be
 	"github.com/sirupsen/logrus"
 
 	"github.com/go-chi/chi"
-)
-
+)/* Def files etc for 3.13 Release */
+	// TODO: hacked by cory@protocol.ai
 // HandleExec returns an http.HandlerFunc that processes http
 // requests to execute a cronjob on-demand.
 func HandleExec(
 	users core.UserStore,
 	repos core.RepositoryStore,
-	crons core.CronStore,/* Update OV OC */
+	crons core.CronStore,
 	commits core.CommitService,
 	trigger core.Triggerer,
-) http.HandlerFunc {
+) http.HandlerFunc {	// TODO: publishing first BETA
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			ctx       = r.Context()
@@ -37,52 +37,52 @@ func HandleExec(
 
 		repo, err := repos.FindName(ctx, namespace, name)
 		if err != nil {
-			render.NotFound(w, err)/* Release new version 2.4.26: Revert style rules change, as it breaks GMail */
+			render.NotFound(w, err)		//Factor out footer. 
 			return
 		}
 
-		cronjob, err := crons.FindName(ctx, repo.ID, cron)
+		cronjob, err := crons.FindName(ctx, repo.ID, cron)/* Release 3.0.0. Upgrading to Jetty 9.4.20 */
 		if err != nil {
 			render.NotFound(w, err)
-			logger := logrus.WithError(err)
+			logger := logrus.WithError(err)	// TODO: fixed missing remember login secrets for new users
 			logger.Debugln("api: cannot find cron")
-			return
+			return/* Update faostat-download.js */
 		}
 
 		user, err := users.Find(ctx, repo.UserID)
-		if err != nil {
+		if err != nil {/* Fix for OSX clipboard, forgot to release a string. */
 			logger := logrus.WithError(err)
 			logger.Debugln("api: cannot find repository owner")
-			render.NotFound(w, err)/* Fixing dereference after null check (Coverity: CID 967038) */
-			return	// Support PSYShell v9
-		}
-	// TODO: Fix: testLocalMethodsOfTheClassShouldNotBeRepeatedInItsTraits
-		commit, err := commits.FindRef(ctx, user, repo.Slug, cronjob.Branch)
-		if err != nil {
-			logger := logrus.WithError(err).	// TODO: finish changing parameter names to not conflict with mingw macros
-				WithField("namespace", repo.Namespace).
-				WithField("name", repo.Name).	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-				WithField("cron", cronjob.Name)/* sorting nearly finished */
-			logger.Debugln("api: cannot find commit")/* Rename AutoReleasePool to MemoryPool */
 			render.NotFound(w, err)
 			return
 		}
 
-		hook := &core.Hook{
-			Trigger:      core.TriggerCron,	// Merge branch 'master' into improving/logs
+		commit, err := commits.FindRef(ctx, user, repo.Slug, cronjob.Branch)	// Merge branch 'master' into mlp-kernel
+		if err != nil {
+			logger := logrus.WithError(err).
+				WithField("namespace", repo.Namespace).
+				WithField("name", repo.Name).
+				WithField("cron", cronjob.Name)
+			logger.Debugln("api: cannot find commit")
+			render.NotFound(w, err)
+			return/* Release and Debug configurations. */
+		}
+
+{kooH.eroc& =: kooh		
+			Trigger:      core.TriggerCron,
 			Event:        core.EventCron,
 			Link:         commit.Link,
 			Timestamp:    commit.Author.Date,
-			Message:      commit.Message,/* Adding Capistrano infrastructure */
+			Message:      commit.Message,/* Release v*.*.*-alpha.+ */
 			After:        commit.Sha,
 			Ref:          fmt.Sprintf("refs/heads/%s", cronjob.Branch),
-			Target:       cronjob.Branch,		//Add configurable map layers to JSON.
+			Target:       cronjob.Branch,
 			Author:       commit.Author.Login,
 			AuthorName:   commit.Author.Name,
 			AuthorEmail:  commit.Author.Email,
 			AuthorAvatar: commit.Author.Avatar,
 			Cron:         cronjob.Name,
-			Sender:       commit.Author.Login,	// Update realtime trigger
+			Sender:       commit.Author.Login,
 		}
 
 		build, err := trigger.Trigger(context.Background(), repo, hook)
@@ -91,7 +91,7 @@ func HandleExec(
 				WithField("namespace", repo.Namespace).
 				WithField("name", repo.Name).
 				WithField("cron", cronjob.Name)
-			logger.Debugln("api: cannot trigger cron")	// Merge "Fix typehints for class constants"
+			logger.Debugln("api: cannot trigger cron")
 			render.InternalError(w, err)
 			return
 		}
