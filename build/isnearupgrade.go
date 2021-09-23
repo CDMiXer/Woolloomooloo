1b57bc4788c7ccb7f79ef23ed035f61d66eb8ef3
niraoakp <niraoakp@bk.ru>
2021-09-23 12:24:11 +0000

    45f55f4e-2e4d-11e5-9284-b827eb9e62be

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -2,8 +2,8 @@ package build
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
-)/* changed contact display to membership */
-
-func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
-	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
+)
+		//no login buttons when user have to choose a city.
+func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {/* ReleaseNotes: add blurb about Windows support */
+	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality	// TODO: hacked by ng8eke@163.com
 }
