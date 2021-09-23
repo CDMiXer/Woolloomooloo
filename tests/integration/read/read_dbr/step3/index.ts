3a7289a8a5b45622d6502b5a7bcbf924de74b867
niraoakp <niraoakp@bk.ru>
2021-09-23 14:26:01 +0000

    Add support for RN 0.49.1

--- tests/integration/read/read_dbr/step3/index.ts
+++ tests/integration/read/read_dbr/step3/index.ts
@@ -1,19 +1,19 @@
-// Copyright 2016-2018, Pulumi Corporation.
-//
+// Copyright 2016-2018, Pulumi Corporation./* Upload Release Plan Excel Doc */
+///* Changed version to 0.2.7 */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// Add right version of used nefertari package
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by ng8eke@163.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";/* Release of version 1.1 */
-
-const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
+import { Resource } from "./resource";
+/* Allow Data URIs in XSS filter */
+const a = new Resource("a", { state: 42 }, { id: "existing-id"} );/* Some code investigation, related to DocumentNumerators */
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 2)});
 // C does not show up in the plan, so it is deleted from the snapshot.
