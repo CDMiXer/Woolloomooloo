34519cbc9eb91a4310c8eb7bdc296cd737b9ad49
niraoakp <niraoakp@bk.ru>
2021-09-23 19:15:45 +0000

    93748e58-2e42-11e5-9284-b827eb9e62be

--- chain/events/state/fastapi.go
+++ chain/events/state/fastapi.go
@@ -1,27 +1,27 @@
 package state
-
+/* replaced emma with jacoco */
 import (
-	"context"/* Release of eeacms/www-devel:19.1.22 */
+	"context"/* Merge "Release 1.0.0.76 QCACLD WLAN Driver" */
 
-	"github.com/filecoin-project/go-address"	// Added gh list_keys and gh create_keys to manage ssh keys
-/* Create Prova.java */
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/go-address"
+
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by sbrichards@gmail.com
 )
 
-type FastChainApiAPI interface {	// TODO: will be fixed by steven@stebalien.com
-	ChainAPI	// Fixing ACMP logging to use correct status conversion
+type FastChainApiAPI interface {		//Improvements on FastaManipulatorServer
+	ChainAPI	// TODO: hacked by alex.gaynor@gmail.com
 
-	ChainGetTipSet(context.Context, types.TipSetKey) (*types.TipSet, error)/* d924fcce-2e5d-11e5-9284-b827eb9e62be */
+	ChainGetTipSet(context.Context, types.TipSetKey) (*types.TipSet, error)
 }
 
-type fastAPI struct {
+type fastAPI struct {		//Improve message error
 	FastChainApiAPI
 }
 
 func WrapFastAPI(api FastChainApiAPI) ChainAPI {
-	return &fastAPI{	// TODO: Merge "[INTERNAL] sap.ui.layout.Form: AddFormField handler adjusted"
+	return &fastAPI{/* Merge "Release 1.0.0.95 QCACLD WLAN Driver" */
 		api,
-	}	// TODO: hacked by steven@stebalien.com
+	}
 }
 
 func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
