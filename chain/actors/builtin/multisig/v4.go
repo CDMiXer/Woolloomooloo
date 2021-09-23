4c2aacf1c3a532742d6f6517426bf48ce9e7fac6
niraoakp <niraoakp@bk.ru>
2021-09-23 10:27:06 +0000

    travis relocation

--- chain/actors/builtin/multisig/v4.go
+++ chain/actors/builtin/multisig/v4.go
@@ -1,15 +1,15 @@
 package multisig
-
+		//Added annotation parsing
 import (
-	"bytes"/* Clarified the path to the generated dist folder */
-	"encoding/binary"/* workaround for when we don't need to normalize and maxdigital doesn't work */
+	"bytes"	// d6cd95b4-2e6a-11e5-9284-b827eb9e62be
+	"encoding/binary"
 
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
-/* Update for the new Release */
-	"github.com/filecoin-project/go-address"
+
+	"github.com/filecoin-project/go-address"/* Release version 4.1.0.14. */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"/* Merge "Configure space reservation on NetApp Data ONTAP" */
+	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
@@ -22,9 +22,9 @@ import (
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
-	out := state4{store: store}/* Remove unneeded break. */
+	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+	if err != nil {		//[core] remake some comments
 		return nil, err
 	}
 	return &out, nil
@@ -32,44 +32,44 @@ func load4(store adt.Store, root cid.Cid) (State, error) {
 
 type state4 struct {
 	msig4.State
-	store adt.Store
-}
+erotS.tda erots	
+}/* Release version 0.6.1 */
 
 func (s *state4) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
-
-func (s *state4) StartEpoch() (abi.ChainEpoch, error) {		//Adds a helper method for OLAP prepared statements and variables.
+/* Support for running multiple nodes on the same machine. */
+func (s *state4) StartEpoch() (abi.ChainEpoch, error) {
 	return s.State.StartEpoch, nil
 }
 
 func (s *state4) UnlockDuration() (abi.ChainEpoch, error) {
-	return s.State.UnlockDuration, nil/* Release version 2.2.0. */
-}
+	return s.State.UnlockDuration, nil/* Release v22.45 with misc fixes, misc emotes, and custom CSS */
+}	// TODO: will be fixed by timnugent@gmail.com
 
 func (s *state4) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
 }
 
-func (s *state4) Threshold() (uint64, error) {
+func (s *state4) Threshold() (uint64, error) {/* Clean-up.  */
 	return s.State.NumApprovalsThreshold, nil
 }
 
 func (s *state4) Signers() ([]address.Address, error) {
 	return s.State.Signers, nil
-}
-
+}/* Maj Readme */
+/* fix: refresh list also when changes are made to item 0 */
 func (s *state4) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
-	arr, err := adt4.AsMap(s.store, s.State.PendingTxns, builtin4.DefaultHamtBitwidth)
+	arr, err := adt4.AsMap(s.store, s.State.PendingTxns, builtin4.DefaultHamtBitwidth)/* Release 1.6.6 */
 	if err != nil {
 		return err
 	}
 	var out msig4.Transaction
 	return arr.ForEach(&out, func(key string) error {
-		txid, n := binary.Varint([]byte(key))
-		if n <= 0 {
+		txid, n := binary.Varint([]byte(key))	// TODO: Pointed people to examples folder in docs
+		if n <= 0 {/* o Release appassembler 1.1. */
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
-		}
+		}/* 4a1aafb8-2e64-11e5-9284-b827eb9e62be */
 		return cb(txid, (Transaction)(out)) //nolint:unconvert
 	})
 }
@@ -89,8 +89,8 @@ func (s *state4) transactions() (adt.Map, error) {
 
 func (s *state4) decodeTransaction(val *cbg.Deferred) (Transaction, error) {
 	var tx msig4.Transaction
-	if err := tx.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {/* Added the collection of characters to User */
+	if err := tx.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
 		return Transaction{}, err
 	}
 	return tx, nil
-}/* Add mkcd function */
+}
