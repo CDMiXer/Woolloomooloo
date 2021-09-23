6e235881edabdb408cdd622838ada97e15d73c9f
niraoakp <niraoakp@bk.ru>
2021-09-23 20:23:17 +0000

    389726fa-2e5e-11e5-9284-b827eb9e62be

--- store/shared/migrate/mysql/files/008_create_table_cron.sql
+++ store/shared/migrate/mysql/files/008_create_table_cron.sql
@@ -1,10 +1,10 @@
 -- name: create-table-cron
 
 CREATE TABLE IF NOT EXISTS cron (
- cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT		//Made turbulence filter resolution-independent and renderable at any size
+ cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT
 ,cron_repo_id     INTEGER
 ,cron_name        VARCHAR(50)
-,cron_expr        VARCHAR(50)	// Delete setup-64.py
+,cron_expr        VARCHAR(50)
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
 ,cron_event       VARCHAR(50)
@@ -15,13 +15,13 @@ CREATE TABLE IF NOT EXISTS cron (
 ,cron_updated     INTEGER
 ,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
-,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE	// remove dashes and update phoenix version to 1.0.3
+,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
--- name: create-index-cron-repo	// TODO: hacked by sjors@sprovoost.nl
+-- name: create-index-cron-repo
 
 CREATE INDEX ix_cron_repo ON cron (cron_repo_id);
 
 -- name: create-index-cron-next
-/* - add keyckloak client library */
+
 CREATE INDEX ix_cron_next ON cron (cron_next);
