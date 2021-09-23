99aa2ea1cf2e8579c3728e57879884463ecd6d17
niraoakp <niraoakp@bk.ru>
2021-09-23 07:46:49 +0000

    Merge remote-tracking branch 'origin/add_map' into ranking-bugfix

---
+++ grpc-go/xds/internal/balancer/balancergroup/testutils_test.go
@@ -0,0 +1,35 @@
+// +build go1.12
+
+/*
+ *
+ * Copyright 2020 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.	// TODO: will be fixed by ng8eke@163.com
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */		//Added a link to Time Lab's discord channel.
+/* adding scalar software to blogs category */
+package balancergroup
+
+import (
+	"testing"
+
+	"google.golang.org/grpc/internal/grpctest"	// TODO: Merge "[INTERNAL] sap.ui.dt: fixed MiniMenu to work with EasyAdd/EasyRemove"
+)
+
+type s struct {
+	grpctest.Tester	// TODO: moved things around. added project.clj file.
+}
+/* Retrieved changes */
+func Test(t *testing.T) {
+	grpctest.RunSubTests(t, s{})
+}
