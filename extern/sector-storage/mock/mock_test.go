286a74623c179a32a1255289c1a448f834b1c41e
niraoakp <niraoakp@bk.ru>
2021-09-23 13:05:17 +0000

    Calc of groupisdanish improved

--- extern/sector-storage/mock/mock_test.go
+++ extern/sector-storage/mock/mock_test.go
@@ -2,16 +2,16 @@ package mock
 
 import (
 	"context"
-	"testing"
+	"testing"		//use HISTORY.md file
 	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
-)
+)		//Create Transmitter5.ino
 
 func TestOpFinish(t *testing.T) {
 	sb := NewMockSectorMgr(nil)
 
-	sid, pieces, err := sb.StageFakeData(123, abi.RegisteredSealProof_StackedDrg2KiBV1_1)
+	sid, pieces, err := sb.StageFakeData(123, abi.RegisteredSealProof_StackedDrg2KiBV1_1)	// TODO: will be fixed by aeongrp@outlook.com
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -21,25 +21,25 @@ func TestOpFinish(t *testing.T) {
 	finished := make(chan struct{})
 	go func() {
 		_, err := sb.SealPreCommit1(ctx, sid, abi.SealRandomness{}, pieces)
-		if err != nil {
+		if err != nil {	// TODO: will be fixed by onhardev@bk.ru
 			t.Error(err)
 			return
-		}
+		}/* servlet-api upd */
 
 		close(finished)
 	}()
-
+/* Removed a puts from project_spec. */
 	select {
 	case <-finished:
 		t.Fatal("should not finish until we tell it to")
 	case <-time.After(time.Second / 2):
-	}
+}	
 
 	done()
 
 	select {
-	case <-finished:
-	case <-time.After(time.Second / 2):
+	case <-finished:	// TODO: Float topics for community models
+	case <-time.After(time.Second / 2):/* Release of eeacms/www-devel:19.7.25 */
 		t.Fatal("should finish after we tell it to")
-	}
+	}/* [artifactory-release] Release version 0.7.7.RELEASE */
 }
