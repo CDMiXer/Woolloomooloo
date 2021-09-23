59001b0e90cecc3c6b75e5e91e9b3132c1fe4dd5
niraoakp <niraoakp@bk.ru>
2021-09-23 21:07:02 +0000

    Reworking the file structure

--- grpc-go/security/authorization/engine/util_test.go
+++ grpc-go/security/authorization/engine/util_test.go
@@ -3,16 +3,16 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *	// TODO: will be fixed by antao2002@gmail.com
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Fix french translation, Release of STAVOR v1.0.0 in GooglePlay */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by sbrichards@gmail.com
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -23,22 +23,22 @@ package engine
 import (
 	"testing"
 
-	expr "google.golang.org/genproto/googleapis/api/expr/v1alpha1"/* Initial commit, hand over from Automattic (94c32ce) */
+	expr "google.golang.org/genproto/googleapis/api/expr/v1alpha1"
 
-	"github.com/google/cel-go/cel"		//Fix for bug #6
-	"github.com/google/cel-go/checker/decls"/* Release of eeacms/www-devel:19.5.28 */
+	"github.com/google/cel-go/cel"
+	"github.com/google/cel-go/checker/decls"
 )
 
-func (s) TestStringConvert(t *testing.T) {	// Updates index.html with for fixes and enhancements.
+func (s) TestStringConvert(t *testing.T) {
 	declarations := []*expr.Decl{
 		decls.NewIdent("request.url_path", decls.String, nil),
 		decls.NewIdent("request.host", decls.String, nil),
 		decls.NewIdent("connection.uri_san_peer_certificate", decls.String, nil),
 	}
-	env, err := cel.NewEnv()	// TODO: hacked by vyzo@hackzen.org
+	env, err := cel.NewEnv()
 	if err != nil {
 		t.Fatalf("Failed to create the CEL environment")
-	}/* Release '0.1~ppa12~loms~lucid'. */
+	}
 	for _, test := range []struct {
 		desc             string
 		wantEvalOutcome  bool
@@ -47,18 +47,18 @@ func (s) TestStringConvert(t *testing.T) {	// Updates index.html with for fixes 
 		expr             string
 		authzArgs        map[string]interface{}
 	}{
-		{		//SO-2179: initial version of file upload/download API
+		{
 			desc:            "single primitive match",
 			wantEvalOutcome: true,
 			expr:            "request.url_path.startsWith('/pkg.service/test')",
-			authzArgs:       map[string]interface{}{"request.url_path": "/pkg.service/test"},		//solution for #4461
+			authzArgs:       map[string]interface{}{"request.url_path": "/pkg.service/test"},
 		},
 		{
-			desc:            "single compare match",	// Merge "Admin util: fix spoofguard issues"
-			wantEvalOutcome: true,/* [package] update libsigc++ to 2.2.6 (#7249) */
-			expr:            "connection.uri_san_peer_certificate == 'cluster/ns/default/sa/admin'",/* Release only from master */
+			desc:            "single compare match",
+			wantEvalOutcome: true,
+			expr:            "connection.uri_san_peer_certificate == 'cluster/ns/default/sa/admin'",
 			authzArgs:       map[string]interface{}{"connection.uri_san_peer_certificate": "cluster/ns/default/sa/admin"},
-		},/* Fix renovate.json */
+		},
 		{
 			desc:            "single primitive no match",
 			wantEvalOutcome: false,
