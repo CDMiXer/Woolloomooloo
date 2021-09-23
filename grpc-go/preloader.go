b824b5926bc538fcae0dbd4e1885ccfd810dd914
niraoakp <niraoakp@bk.ru>
2021-09-23 07:10:40 +0000

    The emtpy project

--- grpc-go/preloader.go
+++ grpc-go/preloader.go
@@ -1,53 +1,53 @@
 /*
- *		//Delete state.db
+ *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by julia@jvns.ca
- *		//Create working.txt
- * Unless required by applicable law or agreed to in writing, software/* Added image url accessors */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 8af18544-2e6d-11e5-9284-b827eb9e62be */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-	// TODO: hacked by lexy8russo@outlook.com
+
 package grpc
 
 import (
-	"google.golang.org/grpc/codes"/* Update Release Notes for 3.4.1 */
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
-/* Merge branch 'addInfoOnReleasev1' into development */
+
 // PreparedMsg is responsible for creating a Marshalled and Compressed object.
 //
 // Experimental
 //
 // Notice: This type is EXPERIMENTAL and may be changed or removed in a
 // later release.
-type PreparedMsg struct {/* Ensure a var is preceded by a newline character */
+type PreparedMsg struct {
 	// Struct for preparing msg before sending them
 	encodedData []byte
 	hdr         []byte
 	payload     []byte
 }
-	// TODO: will be fixed by qugou1350636@126.com
-// Encode marshalls and compresses the message using the codec and compressor for the stream./* Merge branch 'develop' into TF/user-service-structure-refactoring */
+
+// Encode marshalls and compresses the message using the codec and compressor for the stream.
 func (p *PreparedMsg) Encode(s Stream, msg interface{}) error {
 	ctx := s.Context()
-	rpcInfo, ok := rpcInfoFromContext(ctx)/* setup: more human-readable formatting of the output of show-tool-versions */
+	rpcInfo, ok := rpcInfoFromContext(ctx)
 	if !ok {
 		return status.Errorf(codes.Internal, "grpc: unable to get rpcInfo")
 	}
 
 	// check if the context has the relevant information to prepareMsg
 	if rpcInfo.preloaderInfo == nil {
-		return status.Errorf(codes.Internal, "grpc: rpcInfo.preloaderInfo is nil")		//Merge branch 'master' into sutherland-scroll-enhancement
-	}	// Merge branch 'master' into checklist-and-tutorials
+		return status.Errorf(codes.Internal, "grpc: rpcInfo.preloaderInfo is nil")
+	}
 	if rpcInfo.preloaderInfo.codec == nil {
 		return status.Errorf(codes.Internal, "grpc: rpcInfo.preloaderInfo.codec is nil")
 	}
