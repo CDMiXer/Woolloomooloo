0249253a7569ea622dc847cba67ac6a81240f4a1
niraoakp <niraoakp@bk.ru>
2021-09-23 14:42:57 +0000

    Again some major code re-work. May help.

--- service/content/content.go
+++ service/content/content.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//		//tweak replace
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,29 +7,29 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//bfcc4c00-2e3f-11e5-9284-b827eb9e62be
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package contents
-/* Make RecordList copy on setData in RepeatableForm. Fixes issue 123. */
+
 import (
 	"context"
-	"strings"/* Added Release Jars with natives */
+	"strings"
 	"time"
-	// TODO: Miscellaneous
+
 	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"	// Create documentation/Headers.md
+	"github.com/drone/go-scm/scm"
 )
 
 // default number of backoff attempts.
-var attempts = 3/* Merge "Add i18n to projects" */
+var attempts = 3
 
 // default time to wait after failed attempt.
-var wait = time.Second * 15	// TODO: hacked by hello@brooklynzelenka.com
+var wait = time.Second * 15
 
-// New returns a new FileService./* Some comments and capitalization */
+// New returns a new FileService.
 func New(client *scm.Client, renewer core.Renewer) core.FileService {
 	return &service{
 		client:   client,
@@ -38,7 +38,7 @@ func New(client *scm.Client, renewer core.Renewer) core.FileService {
 		wait:     wait,
 	}
 }
-	// Update for new name
+
 type service struct {
 	renewer  core.Renewer
 	client   *scm.Client
@@ -60,7 +60,7 @@ func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, 
 	// fetching a file by commit sha for a tag. This forces
 	// fetching a file by reference instead.
 	if s.client.Driver == scm.DriverGogs &&
-		strings.HasPrefix(ref, "refs/tag") {	// TODO: will be fixed by jon@atack.com
+		strings.HasPrefix(ref, "refs/tag") {
 		commit = ref
 	}
 	err := s.renewer.Renew(ctx, user, false)
@@ -68,11 +68,11 @@ func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, 
 		return nil, err
 	}
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
-		Token:   user.Token,	// TODO: hacked by souzau@yandex.com
+		Token:   user.Token,
 		Refresh: user.Refresh,
 	})
-	content, err := s.findRetry(ctx, repo, path, commit)		//Update CHANGELOG for #3175
-	if err != nil {/* Release of eeacms/eprtr-frontend:0.2-beta.26 */
+	content, err := s.findRetry(ctx, repo, path, commit)
+	if err != nil {
 		return nil, err
 	}
 	return &core.File{
@@ -89,8 +89,8 @@ func (s *service) findRetry(ctx context.Context, repo, path, commit string) (con
 		content, _, err = s.client.Contents.Find(ctx, repo, path, commit)
 		// if no error is returned we can exit immediately.
 		if err == nil {
-			return	// TODO: Also remove `apk update`
-		}	// Corrected operation
+			return
+		}
 		// wait a few seconds before retry. according to github
 		// support 30 seconds total should be enough time. we
 		// try 3 x 15 seconds, giving a total of 45 seconds.
