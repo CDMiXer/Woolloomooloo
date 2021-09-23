62bcc93cee628d68e93f971fca4089873773fb34
niraoakp <niraoakp@bk.ru>
2021-09-23 15:08:30 +0000

    WorkflowTemplate documents and data fixtures updated #70

--- handler/api/repos/builds/branches/create.go
+++ handler/api/repos/builds/branches/create.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release 0.1.10. */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: add database.
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Merge "Pull docker images from global registry"
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//2c9641ee-2e52-11e5-9284-b827eb9e62be
 
-package branches/* Release policy added */
+package branches
