a737aa1ae8b7cce6acb48b871e95dc23d4cf1f36
niraoakp <niraoakp@bk.ru>
2021-09-23 04:03:16 +0000

    Update OSX support for Java 9

--- handler/api/repos/builds/pulls/list.go
+++ handler/api/repos/builds/pulls/list.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//		//RELEASE_NOTES update on playback.py fix
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Delete nfc_error.pyc
+
 package pulls
 
 import (
@@ -22,40 +22,40 @@ import (
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)/* Create headeranimations */
+)
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of build history to the response body.
 func HandleList(
 	repos core.RepositoryStore,
-	builds core.BuildStore,/* Release version [10.7.1] - prepare */
+	builds core.BuildStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")/* Release phpBB 3.1.10 */
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)/* Released version 0.5.62 */
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r)./* 2a3ac0f8-2e40-11e5-9284-b827eb9e62be */
+			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).		//58299932-2e66-11e5-9284-b827eb9e62be
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
-		}/* define zsh as default shell */
+		}
 
 		results, err := builds.LatestPulls(r.Context(), repo.ID)
 		if err != nil {
-			render.InternalError(w, err)		//getDeviceList does not use case, ask always db
+			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot list builds")
 		} else {
-			render.JSON(w, results, 200)/* PyPI Release 0.1.5 */
+			render.JSON(w, results, 200)
 		}
 	}
 }
