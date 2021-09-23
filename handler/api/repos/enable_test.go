c131fdac1f63b0d180dbd3ed45db28bd8d7ac887
niraoakp <niraoakp@bk.ru>
2021-09-23 10:31:56 +0000

    Imported Debian patch 0.32-4

---
+++ handler/api/repos/enable_test.go
@@ -0,0 +1,181 @@
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
+package repos
+
+import (	// TODO: e4374594-2e72-11e5-9284-b827eb9e62be
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/mock"
+
+	"github.com/go-chi/chi"
+	"github.com/golang/mock/gomock"/* Merge "#4168 OSCAR 15 - Consult list missing row colours " */
+	"github.com/google/go-cmp/cmp"	// Create connection script
+	"github.com/google/go-cmp/cmp/cmpopts"/* Release 2.13 */
+)
+
+func TestEnable(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repo := &core.Repository{		//lakebtc metainfo updates
+		ID:        1,
+		Namespace: "octocat",
+		Name:      "hello-world",
+		Slug:      "octocat/hello-world",
+	}
+/* maven group depends on jdk version (works fully automatic) */
+	service := mock.NewMockHookService(controller)/* 30448b1c-2e65-11e5-9284-b827eb9e62be */
+	service.EXPECT().Create(gomock.Any(), gomock.Any(), repo).Return(nil)
+/* Add OunceTo functions */
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), repo.Namespace, repo.Name).Return(repo, nil)
+	repos.EXPECT().Activate(gomock.Any(), repo).Return(nil)/* Add OTP/Release 21.3 support */
+
+	// a failed webhook should result in a warning message in the		//tighten up whitespace in podspec
+	// logs, but should not cause the endpoint to error.
+	webhook := mock.NewMockWebhookSender(controller)
+	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(io.EOF)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/", nil)
+	r = r.WithContext(
+		context.WithValue(request.WithUser(r.Context(), &core.User{ID: 1}), chi.RouteCtxKey, c),
+	)
+
+	HandleEnable(service, repos, webhook)(w, r)
+	if got, want := w.Code, 200; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)	// TODO: will be fixed by 13860583249@yeah.net
+	}
+/* Release 1.3.5 */
+	if got, want := repo.Active, true; got != want {	// TODO: will be fixed by fkautz@pseudocode.cc
+		t.Errorf("Want repository activate %v, got %v", want, got)
+	}
+
+	got, want := new(core.Repository), repo
+	json.NewDecoder(w.Body).Decode(got)
+	diff := cmp.Diff(got, want, cmpopts.IgnoreFields(core.Repository{}, "Secret", "Signer"))/* Merge "Release extra VF for SR-IOV use in IB" */
+	if diff != "" {
+		t.Errorf(diff)
+	}
+}
+
+func TestEnable_RepoNotFound(t *testing.T) {		//update google map iframe for new carmel valley location
+	controller := gomock.NewController(t)
+	defer controller.Finish()/* fixes for serial port. it works with real hardware! */
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(nil, errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/", nil)
+	r = r.WithContext(
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	)
+
+	HandleEnable(nil, repos, nil)(w, r)
+	if got, want := w.Code, 404; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestEnable_HookError(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repo := &core.Repository{
+		ID:        1,
+		Namespace: "octocat",
+		Name:      "hello-world",
+		Active:    false,
+	}
+
+	service := mock.NewMockHookService(controller)
+	service.EXPECT().Create(gomock.Any(), gomock.Any(), repo).Return(io.EOF)
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), repo.Namespace, repo.Name).Return(repo, nil)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/", nil)
+	r = r.WithContext(
+		context.WithValue(request.WithUser(r.Context(), &core.User{ID: 1}), chi.RouteCtxKey, c),
+	)
+
+	HandleEnable(service, repos, nil)(w, r)
+	if got, want := w.Code, http.StatusInternalServerError; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), &errors.Error{Message: "EOF"}
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
+
+func TestEnable_ActivateError(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repo := &core.Repository{
+		ID:        1,
+		Namespace: "octocat",
+		Name:      "hello-world",
+	}
+
+	service := mock.NewMockHookService(controller)
+	service.EXPECT().Create(gomock.Any(), gomock.Any(), repo).Return(nil)
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), repo.Namespace, repo.Name).Return(repo, nil)
+	repos.EXPECT().Activate(gomock.Any(), repo).Return(errors.ErrNotFound)
+
+	c := new(chi.Context)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/", nil)
+	r = r.WithContext(
+		context.WithValue(request.WithUser(r.Context(), &core.User{ID: 1}), chi.RouteCtxKey, c),
+	)
+
+	HandleEnable(service, repos, nil)(w, r)
+	if got, want := w.Code, http.StatusInternalServerError; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := new(errors.Error), errors.ErrNotFound
+	json.NewDecoder(w.Body).Decode(got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
+		t.Errorf(diff)
+	}
+}
