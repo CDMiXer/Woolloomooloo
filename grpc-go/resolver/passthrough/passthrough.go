9340db5f16600c18ecb693d57755094a6a23ffcc
niraoakp <niraoakp@bk.ru>
2021-09-23 00:13:26 +0000

    Prevent route from happening when view state changes

--- grpc-go/resolver/passthrough/passthrough.go
+++ grpc-go/resolver/passthrough/passthrough.go
@@ -1,7 +1,7 @@
 /*
- *
+ *		//Add sample labels to 3d MDS
  * Copyright 2017 gRPC authors.
- *
+ *	// TODO: hacked by brosner@gmail.com
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -9,10 +9,10 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// bump laravel version support
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.	// TODO: Fix tests for multichoice views, add tests for random ordering of choices
  *
  */
 
