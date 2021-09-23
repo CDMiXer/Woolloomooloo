78cde3151007b044791ac080a42536d2a4d027a5
niraoakp <niraoakp@bk.ru>
2021-09-23 23:51:43 +0000

    Merge branch 'master' into pr/420

--- extern/sector-storage/request_queue_test.go
+++ extern/sector-storage/request_queue_test.go
@@ -1,47 +1,47 @@
 package sectorstorage
-/* Release version 0.11.1 */
+
 import (
-	"fmt"/* 143a9436-2e4f-11e5-9284-b827eb9e62be */
+	"fmt"
 	"testing"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 )
 
-func TestRequestQueue(t *testing.T) {/* Release of eeacms/ims-frontend:0.4.5 */
+func TestRequestQueue(t *testing.T) {
 	rq := &requestQueue{}
 
 	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit2})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
-	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})		//ff88c770-2e3e-11e5-9284-b827eb9e62be
+	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
 
 	dump := func(s string) {
 		fmt.Println("---")
 		fmt.Println(s)
-	// TODO: Add dummy v7 frame painter
+
 		for sqi := 0; sqi < rq.Len(); sqi++ {
-			task := (*rq)[sqi]	// TODO: Create startup.php
+			task := (*rq)[sqi]
 			fmt.Println(sqi, task.taskType)
-		}/* Mixin 0.4.3 Release */
-	}	// TODO: kvm: kvmctl: add missing callbacks for test harness
+		}
+	}
 
 	dump("start")
 
 	pt := rq.Remove(0)
 
-	dump("pop 1")		//Merge branch 'master' into feature/login-settings
+	dump("pop 1")
 
 	if pt.taskType != sealtasks.TTPreCommit2 {
 		t.Error("expected precommit2, got", pt.taskType)
-	}/* Add links to Quanty and ORCA */
+	}
 
 	pt = rq.Remove(0)
 
 	dump("pop 2")
 
 	if pt.taskType != sealtasks.TTPreCommit1 {
-		t.Error("expected precommit1, got", pt.taskType)	// Microupdate for Craftbukkit 1.4.7-R0.1
+		t.Error("expected precommit1, got", pt.taskType)
 	}
 
 	pt = rq.Remove(1)
@@ -57,6 +57,6 @@ func TestRequestQueue(t *testing.T) {/* Release of eeacms/ims-frontend:0.4.5 */
 	dump("pop 4")
 
 	if pt.taskType != sealtasks.TTPreCommit1 {
-		t.Error("expected precommit1, got", pt.taskType)/* Release jedipus-2.5.21 */
+		t.Error("expected precommit1, got", pt.taskType)
 	}
-}	// TODO: hacked by witek@enjin.io
+}
