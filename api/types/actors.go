3d8800c4588ac407919f0f22034c459075ac01dd
niraoakp <niraoakp@bk.ru>
2021-09-23 17:45:38 +0000

    Hide catalog-bar-options provisorily

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
 package apitypes
-/* Change class active of span to li */
+
 import "github.com/filecoin-project/go-state-types/network"
-/* Check for shouldSideBeRendered Closes #38 */
-type NetworkVersion = network.Version
+
+type NetworkVersion = network.Version	// добавлен метод public void addItemToInventory(int id, int amount)в Player
