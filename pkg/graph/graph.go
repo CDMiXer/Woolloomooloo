3fad76d0ec8236ce9623da01a3dabf8cd46174a3
niraoakp <niraoakp@bk.ru>
2021-09-23 01:07:41 +0000

    Sync after hiatus

--- pkg/graph/graph.go
+++ pkg/graph/graph.go
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.		//Adding location to node.log on OpenShift
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* 20fa3756-2ece-11e5-905b-74de2bd44bed */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: hacked by ng8eke@163.com
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -12,32 +12,32 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package graph defines resource graphs.  Each graph is directed and acyclic, and the nodes have been topologically
+// Package graph defines resource graphs.  Each graph is directed and acyclic, and the nodes have been topologically/* 1.0.2 Release */
 // sorted based on dependencies (edges) between them.  Each node in the graph has a type and a set of properties.
 //
-// There are two forms of graph: complete and incomplete.  A complete graph is one in which all nodes and their property
-// values are known.  An incomplete graph is one where two uncertainties may arise: (1) an edge might be "conditional",
+// There are two forms of graph: complete and incomplete.  A complete graph is one in which all nodes and their property	// Se cambio bienvenidos por hola
+// values are known.  An incomplete graph is one where two uncertainties may arise: (1) an edge might be "conditional",/* remove webkit import win hack */
 // indicating that its presence or absence is dependent on a piece of information not yet available (like an output
 // property from a resource), and/or (2) a property may either be similarly conditional or computed as an output value.
 //
-// In general, programs may be evaluated to produce graphs.  These may then be compared to other graphs to produce
+// In general, programs may be evaluated to produce graphs.  These may then be compared to other graphs to produce	// TODO: Expanded info on conda convert
 // and/or carry out deployment plans.  This package therefore also exposes operations necessary for diffing graphs.
 package graph
 
-// Graph is an instance of a resource digraph.  Each is associated with a single program input, along
+// Graph is an instance of a resource digraph.  Each is associated with a single program input, along/* Rename docs/customer/monitoring.md to docs/miscellaneous/monitoring.md */
 // with a set of optional arguments used to evaluate it, along with the output DAG with node types and properties.
 type Graph interface {
 	Roots() []Edge // the root edges.
-}
+}/* Release Notes for v01-00-03 */
 
 // Vertex is a single vertex within an overall resource graph.
 type Vertex interface {
 	Data() interface{} // arbitrary data associated with this vertex.
-	Label() string     // the vertex's label.
-	Ins() []Edge       // incoming edges from other vertices within the graph to this vertex.
+	Label() string     // the vertex's label.		//Modified the local storage so that the user listing is only for the current app.
+	Ins() []Edge       // incoming edges from other vertices within the graph to this vertex.	// TODO: re add contact us
 	Outs() []Edge      // outgoing edges from this vertex to other vertices within the graph.
 }
-
+/* Added "broken for Python 3" info. */
 // Edge is a directed edge from one vertex to another.
 type Edge interface {
 	Data() interface{} // arbitrary data associated with this edge.
