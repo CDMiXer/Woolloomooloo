7c2c2892425da4c82200b5c1035bae0cc3ff233b
niraoakp <niraoakp@bk.ru>
2021-09-23 04:08:49 +0000

    instantiator libs in own folders due to Eclipse reflection/classloading

--- handler/api/queue/resume_test.go
+++ handler/api/queue/resume_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 3.0.4. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
+/* #6 - Release 0.2.0.RELEASE. */
+// +build !oss/* Release charm 0.12.0 */
 
-// +build !oss
-	// bundle-size: 57dbfa618b3456559f572c1f9a769e69ad84b711.json
 package queue
