874bf2dd75bdd17d016b621ca786eadf9a01b995
niraoakp <niraoakp@bk.ru>
2021-09-23 03:11:33 +0000

    Adição de um roteiro para testes do formulário de pré-entrevista.

--- chain/store/basefee_test.go
+++ chain/store/basefee_test.go
@@ -1,20 +1,20 @@
-package store
+package store	// TODO: hacked by ng8eke@163.com
 
 import (
 	"fmt"
-	"testing"/* Minor modifications for Release_MPI config in EventGeneration */
-/* Initial Release 1.0 */
-	"github.com/filecoin-project/lotus/build"/* Release Candidate 0.5.6 RC2 */
+	"testing"
+
+	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"	// TODO: hacked by mail@bitpshr.net
 )
-		//Update Docs “contributor-guide”
-func TestBaseFee(t *testing.T) {	// Update nap
+
+func TestBaseFee(t *testing.T) {
 	tests := []struct {
-		basefee             uint64
+		basefee             uint64	// TODO: hacked by vyzo@hackzen.org
 		limitUsed           int64
-		noOfBlocks          int
-		preSmoke, postSmoke uint64
+		noOfBlocks          int		//SRT-28657 Add basic Cache Listener support
+		preSmoke, postSmoke uint64	// Update update_pihole_lists-porn.sh
 	}{
 		{100e6, 0, 1, 87.5e6, 87.5e6},
 		{100e6, 0, 5, 87.5e6, 87.5e6},
@@ -24,14 +24,14 @@ func TestBaseFee(t *testing.T) {	// Update nap
 		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},
 	}
 
-	for _, test := range tests {	// TODO: hacked by nagydani@epointsystem.org
+	for _, test := range tests {
 		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
 			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)
 			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())
 
 			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)
-			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())		//Create presflo3.c
+			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())
 		})
 	}
-}/* Merge "Release locks when action is cancelled" */
+}
