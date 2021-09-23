e60be1cbf08415a2da21048aab3310d1f95158c8
niraoakp <niraoakp@bk.ru>
2021-09-23 17:23:02 +0000

    1d049370-2e4d-11e5-9284-b827eb9e62be

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -1,6 +1,6 @@
 package full
 
-import (		//98fb6dfc-2e55-11e5-9284-b827eb9e62be
+import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -10,13 +10,13 @@ import (		//98fb6dfc-2e55-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Fix link to grape in README */
+
 func TestMedian(t *testing.T) {
 	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 	}, 1))
 
-	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{/* Rename LICENSE to inits/LICENSE */
+	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 		{big.NewInt(10), build.BlockGasTarget},
 	}, 1))
@@ -26,14 +26,14 @@ func TestMedian(t *testing.T) {
 		{big.NewInt(20), build.BlockGasTarget / 2},
 	}, 1))
 
-	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{	// TODO: hacked by aeongrp@outlook.com
+	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},	// TODO: Update apps/jobbrowser/src/jobbrowser/templates/attempt.mako
+		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
-		{big.NewInt(10), build.BlockGasTarget / 2},		//Added the ui file for the process manager
+		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 2))
