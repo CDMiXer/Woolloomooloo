c727d45167576d5cb0d68ca105980a73ef4b10d2
niraoakp <niraoakp@bk.ru>
2021-09-23 07:21:40 +0000

    Create bankickp.lua

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* use exit code from 'error' */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package internal
+package internal/* Generalize key cleaning even more */
 
 var defaultImage = "drone/controller:1"
-
+	// TODO: hacked by 13860583249@yeah.net
 // DefaultImage returns the default dispatch image if none
-// is specified.
+// is specified./* "Release 0.7.0" (#103) */
 func DefaultImage(image string) string {
 	if image == "" {
 		return defaultImage
