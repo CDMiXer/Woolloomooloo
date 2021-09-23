d5e90134d74bc54d4cc53cbb7e8e91deeaa965d0
niraoakp <niraoakp@bk.ru>
2021-09-23 01:18:18 +0000

    Update createCW_ipuz.py

---
+++ build/tools.go
@@ -0,0 +1,10 @@
+//+build tools	// TODO: Make quotation marks visible to the parser instead of skipping them
+		//Merge "Simplify resource management in ExpatParser's JNI." into dalvik-dev
+package build/* fix eeschema plotting bug */
+/* Release jedipus-2.6.13 */
+import (
+	_ "github.com/GeertJohan/go.rice/rice"
+	_ "github.com/golang/mock/mockgen"
+	_ "github.com/whyrusleeping/bencher"
+	_ "golang.org/x/tools/cmd/stringer"
+)	// webkitShadowRoot -> shadowRoot
