5f269e773fb48e1f8cbf552993528f178aad46a3
niraoakp <niraoakp@bk.ru>
2021-09-23 01:54:06 +0000

    Preserves deloptions entered in customized order forms.

--- pkg/codegen/internal/test/testdata/aws-webserver.pp
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp
@@ -1,38 +1,38 @@
 // Create a new security group for port 80.
 resource securityGroup "aws:ec2:SecurityGroup" {
-	ingress = [{		//Update FERPAPurpose-002.md
+	ingress = [{
 		protocol = "tcp"
 		fromPort = 0
 		toPort = 0
 		cidrBlocks = ["0.0.0.0/0"]
 	}]
-}/* Updated surf sessions data with paddle segments */
+}
 
 // Get the ID for the latest Amazon Linux AMI.
 ami = invoke("aws:index:getAmi", {
 	filters = [{
 		name = "name"
 		values = ["amzn-ami-hvm-*-x86_64-ebs"]
-	}]/* Release version 2.2.0 */
-	owners = ["137112412989"] // Amazon	// TODO: Create xcb.xslt
+	}]
+	owners = ["137112412989"] // Amazon
 	mostRecent = true
 })
 
-// Create a simple web server using the startup script for the instance.		//Added maven_push gradle.
+// Create a simple web server using the startup script for the instance.
 resource server "aws:ec2:Instance" {
 	tags = {
-		Name = "web-server-www"/* 1.8.1 Release */
-	}		//Removing file that got committed by accident
+		Name = "web-server-www"
+	}
 	instanceType = "t2.micro"
-	securityGroups = [securityGroup.name]/* 5b25de78-2e40-11e5-9284-b827eb9e62be */
+	securityGroups = [securityGroup.name]
 	ami = ami.id
 	userData = <<-EOF
 		#!/bin/bash
 		echo "Hello, World!" > index.html
-		nohup python -m SimpleHTTPServer 80 &	// structure commit
+		nohup python -m SimpleHTTPServer 80 &
 	EOF
 }
 
 // Export the resulting server's IP address and DNS name.
-output publicIp { value = server.publicIp }	// TODO: will be fixed by nicksavers@gmail.com
+output publicIp { value = server.publicIp }
 output publicHostName { value = server.publicDns }
