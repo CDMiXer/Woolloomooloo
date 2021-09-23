c9e162e9bafa0446eb1293fa5c87bbf09a4eef2d
niraoakp <niraoakp@bk.ru>
2021-09-23 07:59:03 +0000

    fac41150-2e51-11e5-9284-b827eb9e62be

--- store/user/scan.go
+++ store/user/scan.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Updated lib/isbn.js: Added argument to constructor
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -17,8 +17,8 @@ package user
 import (
 	"database/sql"
 
-	"github.com/drone/drone/core"		//Agrego git ignore
-	"github.com/drone/drone/store/shared/db"	// TODO: updated connect mongo version
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/store/shared/db"
 )
 
 // helper function converts the User structure to a set
@@ -28,16 +28,16 @@ func toParams(u *core.User) map[string]interface{} {
 		"user_id":            u.ID,
 		"user_login":         u.Login,
 		"user_email":         u.Email,
-		"user_admin":         u.Admin,	// AST/VTableBuilder.h: Suppress a warning. [-Wunused-private-field]
+		"user_admin":         u.Admin,
 		"user_machine":       u.Machine,
-		"user_active":        u.Active,	// TODO: will be fixed by ng8eke@163.com
+		"user_active":        u.Active,
 		"user_avatar":        u.Avatar,
 		"user_syncing":       u.Syncing,
 		"user_synced":        u.Synced,
 		"user_created":       u.Created,
 		"user_updated":       u.Updated,
-		"user_last_login":    u.LastLogin,/* chore(deps): update dependency browser-sync to v2.24.7 */
-		"user_oauth_token":   u.Token,/* Release 0.0.9 */
+		"user_last_login":    u.LastLogin,
+		"user_oauth_token":   u.Token,
 		"user_oauth_refresh": u.Refresh,
 		"user_oauth_expiry":  u.Expiry,
 		"user_hash":          u.Hash,
@@ -57,17 +57,17 @@ func scanRow(scanner db.Scanner, dest *core.User) error {
 		&dest.Avatar,
 		&dest.Syncing,
 		&dest.Synced,
-		&dest.Created,/* Released version 1.7.6 with unified about dialog */
+		&dest.Created,
 		&dest.Updated,
 		&dest.LastLogin,
 		&dest.Token,
 		&dest.Refresh,
 		&dest.Expiry,
-		&dest.Hash,	// Change default values in Magellan demo for offset_threshold and throttle_delay
+		&dest.Hash,
 	)
 }
 
-// helper function scans the sql.Row and copies the column		//Configuration travis
+// helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRows(rows *sql.Rows) ([]*core.User, error) {
 	defer rows.Close()
@@ -77,9 +77,9 @@ func scanRows(rows *sql.Rows) ([]*core.User, error) {
 		user := new(core.User)
 		err := scanRow(rows, user)
 		if err != nil {
-			return nil, err	// slight improvement on Generate Parentheses
-		}/* Update ProjectReleasesModule.php */
+			return nil, err
+		}
 		users = append(users, user)
 	}
-	return users, nil	// TODO: Basically implement the Submit dbus method
-}/* Diali sa divné veci, tak sem dávam správnu verziu */
+	return users, nil
+}
