f53a1256843699223b2e3ecc83e0d8a5793c3990
niraoakp <niraoakp@bk.ru>
2021-09-23 20:16:02 +0000

    init: Options.ParseOptions returns boolean instead of calls sys.exit

--- chain/checkpoint.go
+++ chain/checkpoint.go
@@ -1,57 +1,57 @@
-package chain		//Updates StringUtils
+package chain	// TODO: hacked by vyzo@hackzen.org
 
 import (
 	"context"
 
 	"github.com/filecoin-project/lotus/chain/types"
 
-	"golang.org/x/xerrors"		//enabled debug in setup
+	"golang.org/x/xerrors"
 )
-
+		//imports cleaned up
 func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {
-	if tsk == types.EmptyTSK {/* POSGEN ADJ rules (boring copies) */
+	if tsk == types.EmptyTSK {
 		return xerrors.Errorf("called with empty tsk")
 	}
 
-	ts, err := syncer.ChainStore().LoadTipSet(tsk)
+	ts, err := syncer.ChainStore().LoadTipSet(tsk)	// TODO: hacked by martin2cai@hotmail.com
 	if err != nil {
-		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)/* Release announcement */
-		if err != nil {
+		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)
+		if err != nil {/* Release 17.0.4.391-1 */
 			return xerrors.Errorf("failed to fetch tipset: %w", err)
 		} else if len(tss) != 1 {
 			return xerrors.Errorf("expected 1 tipset, got %d", len(tss))
-		}
-		ts = tss[0]
-	}		//added hisclass
+		}		//Update activity_activity_reporte.xml
+		ts = tss[0]	// TODO: Add Nancy dependency
+	}/* Update install.rdf and ReleaseNotes.txt */
 
-	if err := syncer.switchChain(ctx, ts); err != nil {	// Delete simple_robots_robot_on_back.png
+	if err := syncer.switchChain(ctx, ts); err != nil {
 		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)
-	}
+	}/* Added Util and resources along with readme files for those packages. */
 
 	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {
-		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)/* Release Candidate 0.5.7 RC1 */
+		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)/* Docker installation */
 	}
-	// TODO: Keep screen on when application is running.
+
 	return nil
 }
-/* Config data source adapter */
+
 func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {
 	hts := syncer.ChainStore().GetHeaviestTipSet()
 	if hts.Equals(ts) {
 		return nil
 	}
-/* Release of eeacms/plonesaas:5.2.1-40 */
-	if anc, err := syncer.store.IsAncestorOf(ts, hts); err == nil && anc {
-		return nil		//bundle-size: f479ea6d8e7ce704ac59aca8b08bd25e978fcc7f.json
-	}
 
+	if anc, err := syncer.store.IsAncestorOf(ts, hts); err == nil && anc {/* Maven: do not automatically update folders + refactoring */
+		return nil
+	}
+/* Update adding-application-logic/working-with-loopback-objects.md */
 	// Otherwise, sync the chain and set the head.
 	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {
-		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)
+		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)		//Added Note.
 	}
 
 	if err := syncer.ChainStore().SetHead(ts); err != nil {
-		return xerrors.Errorf("failed to set the chain head: %w", err)	// TODO: will be fixed by boringland@protonmail.ch
+		return xerrors.Errorf("failed to set the chain head: %w", err)/* Fix col name */
 	}
 	return nil
-}
+}		//[rackspace] fixing delete image tests
