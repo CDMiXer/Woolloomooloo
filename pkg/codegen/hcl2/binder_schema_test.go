039ac2ba154ce793ebc25778c4b1011349967abc
niraoakp <niraoakp@bk.ru>
2021-09-23 23:26:01 +0000

    Update AssetStoreWebGUI.py

--- pkg/codegen/hcl2/binder_schema_test.go
+++ pkg/codegen/hcl2/binder_schema_test.go
@@ -1,18 +1,18 @@
 package hcl2
-
+		//Added basic asymmetric key agreement interface
 import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"	// TODO: fixes the deck overlay
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"		//basic authentication now works with php in cgi mode
-)
-/* Release of eeacms/ims-frontend:0.8.2 */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
+)		//fix(package): update react-transition-group to version 2.2.1
+
 func BenchmarkLoadPackage(b *testing.B) {
 	loader := schema.NewPluginLoader(test.NewHost(testdataPath))
 
 	for n := 0; n < b.N; n++ {
 		_, err := NewPackageCache().loadPackageSchema(loader, "aws")
 		contract.AssertNoError(err)
-	}		//bumped revision number
-}/* Country fixes */
+	}
+}
