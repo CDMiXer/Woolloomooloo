585e6b726f2132ccf02f891516ada986615a3e19
niraoakp <niraoakp@bk.ru>
2021-09-23 01:39:42 +0000

    Automatic changelog generation for PR #14423 [ci skip]

--- store/shared/migrate/postgres/files/006_create_table_steps.sql
+++ store/shared/migrate/postgres/files/006_create_table_steps.sql
@@ -5,14 +5,14 @@ CREATE TABLE IF NOT EXISTS steps (
 ,step_stage_id    INTEGER
 ,step_number      INTEGER
 ,step_name        VARCHAR(100)
-,step_status      VARCHAR(50)		//add CHARSET
+,step_status      VARCHAR(50)
 ,step_error       VARCHAR(500)
-,step_errignore   BOOLEAN	// TODO: hacked by martin2cai@hotmail.com
+,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
 ,step_started     INTEGER
 ,step_stopped     INTEGER
 ,step_version     INTEGER
-,UNIQUE(step_stage_id, step_number)	// TODO: hacked by brosner@gmail.com
+,UNIQUE(step_stage_id, step_number)
 );
 
 -- name: create-index-steps-stage
