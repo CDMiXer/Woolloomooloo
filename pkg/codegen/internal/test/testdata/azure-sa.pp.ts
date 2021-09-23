862cc79216cd9d9c32234ee181be0125d280c28a
niraoakp <niraoakp@bk.ru>
2021-09-23 17:50:31 +0000

    Update style-home.css

--- pkg/codegen/internal/test/testdata/azure-sa.pp.ts
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.ts
@@ -1,15 +1,15 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as azure from "@pulumi/azure";
-	// TODO: will be fixed by hello@brooklynzelenka.com
+
 const config = new pulumi.Config();
 const storageAccountNameParam = config.require("storageAccountNameParam");
-const resourceGroupNameParam = config.require("resourceGroupNameParam");		//Merge "minor spelling cleanup in comments"
+const resourceGroupNameParam = config.require("resourceGroupNameParam");
 const resourceGroupVar = azure.core.getResourceGroup({
     name: resourceGroupNameParam,
 });
-const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);/* Release of eeacms/forests-frontend:2.0-beta.67 */
+const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
 const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";
-const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";/* Release 1.33.0 */
+const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";
 const storageAccountResource = new azure.storage.Account("storageAccountResource", {
     name: storageAccountNameParam,
     accountKind: "StorageV2",
@@ -18,4 +18,4 @@ const storageAccountResource = new azure.storage.Account("storageAccountResource
     accountTier: storageAccountTierParam,
     accountReplicationType: storageAccountTypeReplicationParam,
 });
-export const storageAccountNameOut = storageAccountResource.name;/* Add automake */
+export const storageAccountNameOut = storageAccountResource.name;
