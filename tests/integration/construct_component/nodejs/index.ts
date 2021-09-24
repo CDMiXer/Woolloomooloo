d868bbb2cb1064119381715ddc06040cb8e8ac90
niraoakp <niraoakp@bk.ru>
2021-09-24 08:40:04 +0000

    chore: update dependency typescript to v3.1.4

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 import { Component } from "./component";
-		//Prepare for FreeBSD port
+
 const componentA = new Component("a", {echo: 42});
-const componentB = new Component("b", {echo: componentA.echo});
+const componentB = new Component("b", {echo: componentA.echo});	// TODO: Refactor: move stuff around into a more logical order.
 const componentC = new Component("c", {echo: componentA.childId});
 
