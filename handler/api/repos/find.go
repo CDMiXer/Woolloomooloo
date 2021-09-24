3836a0f5cec9c4ec07b0a44712856a74153518b5
niraoakp <niraoakp@bk.ru>
2021-09-24 04:14:32 +0000

    f0d6036a-2e60-11e5-9284-b827eb9e62be

--- handler/api/repos/find.go
+++ handler/api/repos/find.go
@@ -3,32 +3,32 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge "Release notes: deprecate kubernetes" */
-//      http://www.apache.org/licenses/LICENSE-2.0/* Bash script to create a apache2 self signed certificat file for SSL */
 //
-// Unless required by applicable law or agreed to in writing, software/* Kernel config update automatic using olddefconfig */
-// distributed under the License is distributed on an "AS IS" BASIS,/* a8b9a8fa-2e4e-11e5-9284-b827eb9e62be */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//reads and writes happen in O(logn) time
+// limitations under the License.
 
-package repos/* Create Readme-template */
+package repos
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/handler/api/render"/* Rapidgator: fixed bug #47 */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
-)	// TODO: Create BoNeSi install script
-/* Merge "Move product description to index.rst from Release Notes" */
+)
+
 // HandleFind returns an http.HandlerFunc that writes the
 // json-encoded repository details to the response body.
 func HandleFind() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		repo, _ := request.RepoFrom(ctx)/* Create bashrc.colour */
-		perm, _ := request.PermFrom(ctx)/* Update deployment manifests */
+		repo, _ := request.RepoFrom(ctx)
+		perm, _ := request.PermFrom(ctx)
 		repo.Perms = perm
 		render.JSON(w, repo, 200)
-	}	// TODO: hacked by brosner@gmail.com
+	}
 }
