b0fef3358aa4deb5d79e8fa2437f7249f0baa94b
niraoakp <niraoakp@bk.ru>
2021-09-24 15:46:54 +0000

    improve error message from standard file.show

--- tests/integration/read/read_replace/read_dbr_test.go
+++ tests/integration/read/read_replace/read_dbr_test.go
@@ -1,30 +1,30 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all/* Release of Prestashop Module V1.0.4 */
+// +build nodejs all
 
-package ints	// TODO: Final tweaks for the night
-
+package ints
+	// TODO: generic pop and length function for ziplist encoding
 import (
 	"testing"
-/* changed arguments from byte to int because of bytes being signed in java */
+/* Merge "Move some opts into nova.utils" */
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)/* Release of eeacms/eprtr-frontend:0.4-beta.27 */
+)
 
 // Test that the engine handles the replacement of an external resource with a
 // owned once gracefully.
 func TestReadReplace(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{	// TODO: will be fixed by martin2cai@hotmail.com
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
+			{		//replaced comment with review
+				Dir:      "step2",		//Merge "Correct server test"
+				Additive: true,
+			},		//fix errors after merge of patricks code
 			{
-				Dir:      "step2",
+				Dir:      "step3",
 				Additive: true,
 			},
-			{/* Release used objects when trying to connect an already connected WMI namespace */
-				Dir:      "step3",
-				Additive: true,	// TODO: will be fixed by alessio@tendermint.com
-			},
 		},
-	})	// TODO: documented coordinators
+	})/* Task 3 Pre-Release Material */
 }
