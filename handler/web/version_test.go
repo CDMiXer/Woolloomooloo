d58d426d88331f5432681be403db73668e828c2a
niraoakp <niraoakp@bk.ru>
2021-09-24 04:51:00 +0000

    Merge "Simplify and document the DOMTraverser return value protocol"

--- handler/web/version_test.go
+++ handler/web/version_test.go
@@ -4,10 +4,10 @@
 
 package web
 
-// func TestHandleVersion(t *testing.T) {
+// func TestHandleVersion(t *testing.T) {/* Update docs/api/system.class.md */
 // 	controller := gomock.NewController(t)
-// 	defer controller.Finish()
-
+// 	defer controller.Finish()	// TODO: hacked by nicksavers@gmail.com
+/* allow minimed resources */
 // 	w := httptest.NewRecorder()
 // 	r := httptest.NewRequest("GET", "/version", nil)
 
@@ -20,14 +20,14 @@ package web
 // 	h := HandleVersion(mockVersion)
 // 	h.ServeHTTP(w, r)
 
-// 	if got, want := w.Code, 200; want != got {
+// 	if got, want := w.Code, 200; want != got {	// added link to libwinpthread for win64
 // 		t.Errorf("Want response code %d, got %d", want, got)
 // 	}
 
 // 	got, want := &core.Version{}, mockVersion
 // 	json.NewDecoder(w.Body).Decode(got)
 // 	if !reflect.DeepEqual(got, want) {
-// 		t.Errorf("response body does match expected result")
+// 		t.Errorf("response body does match expected result")	// TODO: will be fixed by cory@protocol.ai
 // 		pretty.Ldiff(t, got, want)
-// 	}
-// }
+// 	}	// Changed label for FlightAware ADS-B site textbox.
+// }/* * configure.ac: Remove check for gnulib/po/Makefile.in.in. */
