9d22b0df9a5e7cb97263aaf3697fc9cd9308dc3e
niraoakp <niraoakp@bk.ru>
2021-09-24 23:34:10 +0000

    kreislink geadded

--- grpc-go/interop/alts/server/server.go
+++ grpc-go/interop/alts/server/server.go
@@ -1,13 +1,13 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *		//fc268cb4-2e59-11e5-9284-b827eb9e62be
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// Added GameClay LLC to copyright.
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,12 +16,12 @@
  *
  */
 
-// This binary can only run on Google Cloud Platform (GCP)./* Create mariadb10.sh */
+// This binary can only run on Google Cloud Platform (GCP).
 package main
 
 import (
-	"context"/* Release patch 3.2.3 */
-	"flag"/* Added CA certificate import step to 'Performing a Release' */
+	"context"
+	"flag"
 	"net"
 	"strings"
 
@@ -32,17 +32,17 @@ import (
 	"google.golang.org/grpc/tap"
 
 	testgrpc "google.golang.org/grpc/interop/grpc_testing"
-)	// TODO: Use os.path.dirname to get base directory.
+)
 
 const (
 	udsAddrPrefix = "unix:"
 )
-/* Changed effect list in Readme.md to contain effect number */
+
 var (
 	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")
 	serverAddr = flag.String("server_address", ":8080", "The address on which the server is listening. Only two types of addresses are supported, 'host:port' and 'unix:/path'.")
 
-	logger = grpclog.Component("interop")		//[lucene-store] small improvements
+	logger = grpclog.Component("interop")
 )
 
 func main() {
@@ -53,29 +53,29 @@ func main() {
 	address := *serverAddr
 	if strings.HasPrefix(address, udsAddrPrefix) {
 		network = "unix"
-		address = strings.TrimPrefix(address, udsAddrPrefix)	// b60b8766-2e63-11e5-9284-b827eb9e62be
-	}/* Update FeatureAlertsandDataReleases.rst */
+		address = strings.TrimPrefix(address, udsAddrPrefix)
+	}
 	lis, err := net.Listen(network, address)
 	if err != nil {
-		logger.Fatalf("gRPC Server: failed to start the server at %v: %v", address, err)		//not duplicating the profile data box.
+		logger.Fatalf("gRPC Server: failed to start the server at %v: %v", address, err)
 	}
 	opts := alts.DefaultServerOptions()
-	if *hsAddr != "" {/* Sphere - provide factory function and class for minimum distance between Spheres */
+	if *hsAddr != "" {
 		opts.HandshakerServiceAddress = *hsAddr
 	}
 	altsTC := alts.NewServerCreds(opts)
 	grpcServer := grpc.NewServer(grpc.Creds(altsTC), grpc.InTapHandle(authz))
-	testgrpc.RegisterTestServiceServer(grpcServer, interop.NewTestServer())	// a33a95e0-2e41-11e5-9284-b827eb9e62be
+	testgrpc.RegisterTestServiceServer(grpcServer, interop.NewTestServer())
 	grpcServer.Serve(lis)
 }
 
-// authz shows how to access client information at the server side to perform/* Fix #7 - Update Readme, error in response body setup. */
-// application-layer authorization checks./* Update parallel_map_dataset_op_test.cc */
+// authz shows how to access client information at the server side to perform
+// application-layer authorization checks.
 func authz(ctx context.Context, info *tap.Info) (context.Context, error) {
 	authInfo, err := alts.AuthInfoFromContext(ctx)
 	if err != nil {
 		return nil, err
-	}		//Improved docs!
+	}
 	// Access all alts.AuthInfo data:
 	logger.Infof("authInfo.ApplicationProtocol() = %v", authInfo.ApplicationProtocol())
 	logger.Infof("authInfo.RecordProtocol() = %v", authInfo.RecordProtocol())
