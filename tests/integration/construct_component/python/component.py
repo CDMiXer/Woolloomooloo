cbfc449eb0f47844096feab7abafec86b652338d
niraoakp <niraoakp@bk.ru>
2021-09-24 20:48:02 +0000

    Merge branch 'master' into singleExecuteNoTimeStep

--- tests/integration/construct_component/python/component.py
+++ tests/integration/construct_component/python/component.py
@@ -1,15 +1,15 @@
 # Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-	// TODO: work please
+
 from typing import Any, Optional
 
 import pulumi
-		//Delete nodeids
-class Component(pulumi.ComponentResource):
-    echo: pulumi.Output[Any]/* Release of eeacms/www-devel:19.11.27 */
-    childId: pulumi.Output[str]		//Fixed bug -- should have been checking `msg`, not `object`
-/* Delete panorama2.png */
+
+class Component(pulumi.ComponentResource):/* Remove homepage must not be transparent */
+    echo: pulumi.Output[Any]/* Merge "Always export IMAGE_NAME" */
+    childId: pulumi.Output[str]
+
     def __init__(self, name: str, echo: pulumi.Input[Any], opts: Optional[pulumi.ResourceOptions] = None):
-        props = dict()
+        props = dict()	// TODO: Created flipTextSansReverse, it's flipText without the reversing
         props["echo"] = echo
-        props["childId"] = None
+        props["childId"] = None/* Release 1.4.0.8 */
         super().__init__("testcomponent:index:Component", name, props, opts, True)
