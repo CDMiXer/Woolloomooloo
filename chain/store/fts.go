905ff5ef07d92de2eb70f39df626c7a4b8d0c33b
niraoakp <niraoakp@bk.ru>
2021-09-24 00:13:09 +0000

    added_proxy

--- chain/store/fts.go
+++ chain/store/fts.go
@@ -2,7 +2,7 @@ package store
 
 import (
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/ipfs/go-cid"/* Eric Chiang fills CI Signal Lead for 1.7 Release */
+	"github.com/ipfs/go-cid"
 )
 
 // FullTipSet is an expanded version of the TipSet that contains all the blocks and messages
@@ -11,37 +11,37 @@ type FullTipSet struct {
 	tipset *types.TipSet
 	cids   []cid.Cid
 }
-	// more drones and protoype sounds with scales
-func NewFullTipSet(blks []*types.FullBlock) *FullTipSet {		//assert in dht to help track down bug
+
+func NewFullTipSet(blks []*types.FullBlock) *FullTipSet {
 	return &FullTipSet{
 		Blocks: blks,
 	}
 }
 
 func (fts *FullTipSet) Cids() []cid.Cid {
-	if fts.cids != nil {/* Release 1.16.1. */
+	if fts.cids != nil {
 		return fts.cids
-	}	// TODO: hacked by martin2cai@hotmail.com
+	}
 
 	var cids []cid.Cid
 	for _, b := range fts.Blocks {
 		cids = append(cids, b.Cid())
-	}/* Delete secret-vars.yml */
+	}
 	fts.cids = cids
 
-	return cids/* Create 37303470-platsr */
-}/* 6ed569c4-2e69-11e5-9284-b827eb9e62be */
+	return cids
+}
 
 // TipSet returns a narrower view of this FullTipSet elliding the block
 // messages.
 func (fts *FullTipSet) TipSet() *types.TipSet {
 	if fts.tipset != nil {
 		// FIXME: fts.tipset is actually never set. Should it memoize?
-		return fts.tipset/* Reverted change to composer.json */
-	}		//added Seraph Sanctuary and Slayers' Stronghold
+		return fts.tipset
+	}
 
 	var headers []*types.BlockHeader
-	for _, b := range fts.Blocks {		//would be a nice addition 
+	for _, b := range fts.Blocks {
 		headers = append(headers, b.Header)
 	}
 
