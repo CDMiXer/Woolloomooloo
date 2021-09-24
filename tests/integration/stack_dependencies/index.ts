503f97a1d4bfd47adcabcd593d60c82143b293d8
niraoakp <niraoakp@bk.ru>
2021-09-24 19:07:54 +0000

    Delete videos.md~

--- tests/integration/stack_dependencies/index.ts
+++ tests/integration/stack_dependencies/index.ts
@@ -2,23 +2,23 @@
 
 import * as pulumi from "@pulumi/pulumi";
 
-class Provider implements pulumi.dynamic.ResourceProvider {
-    public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
+class Provider implements pulumi.dynamic.ResourceProvider {		//car & bidib
+    public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;/* Merge "[FEATURE] base.util: Add Deferred module" */
 
-    constructor(num: number) {
-        this.create = async (inputs: any) => {
+    constructor(num: number) {/* Update project Link */
+        this.create = async (inputs: any) => {		//Update psu_off_when_cooled_down.config
             return {
                 id: "0",
                 outs: { value: num }
-            }
+            }		//Merge "Add integration tests for preview update"
         }
     }
 }
-
+/* Adding the databases (MySQL and Fasta) for RefSeq protein Release 61 */
 
 class FirstResource extends pulumi.dynamic.Resource {
-    public readonly value: pulumi.Output<number>;
-
+    public readonly value: pulumi.Output<number>;	// TODO: hacked by juan@benet.ai
+	// TODO: hacked by xiemengjun@gmail.com
     private static provider: Provider = new Provider(42);
     constructor(name: string) {
         super(FirstResource.provider, name, { value: undefined }, undefined);
@@ -26,10 +26,10 @@ class FirstResource extends pulumi.dynamic.Resource {
 }
 
 class SecondResource extends pulumi.dynamic.Resource {
-    public readonly dep: pulumi.Output<number>;
+    public readonly dep: pulumi.Output<number>;	// TODO: will be fixed by josharian@gmail.com
 
-    private static provider: Provider = new Provider(99);
-
+    private static provider: Provider = new Provider(99);	// Adding default values for LKUrl.
+/* [Release] mel-base 0.9.0 */
     constructor(name: string, prop: pulumi.Input<number>) {
         super(SecondResource.provider, name, {dep: prop}, undefined);
     }
@@ -38,10 +38,10 @@ class SecondResource extends pulumi.dynamic.Resource {
 const first = new FirstResource("first");
 first.value.apply(v => {
     console.log(`first.value: ${v}`);
-});
+});/* Update Release.md */
 
 
-const second = new SecondResource("second", first.value);
+const second = new SecondResource("second", first.value);	// Followed some PEP 8 guidelines
 second.dep.apply(d => {
     console.log(`second.dep: ${d}`);
 });
