3847e5833d6d21c04d4c9f070d975e20a2a9c91c
niraoakp <niraoakp@bk.ru>
2021-09-24 07:34:36 +0000

    Delete remote.d.ts

--- tests/integration/double_pending_delete/double_pending_delete_test.go
+++ tests/integration/double_pending_delete/double_pending_delete_test.go
@@ -1,61 +1,33 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints
+package ints	// TODO: Extracted a standalone document type
 
 import (
 	"testing"
-
+		//scitran/freesurfer-recon-all:0.3.1_6.0.1
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/stretchr/testify/assert"	// TODO: remove spaces, use tabs
-)
-	// TODO: d671aba2-2e40-11e5-9284-b827eb9e62be
+	"github.com/stretchr/testify/assert"
+)/* Merge "Support configuration of custom fluentd outputs" */
+
 // Test that the engine tolerates two deletions of the same URN in the same plan.
 func TestDoublePendingDelete(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{	// TODO: hacked by vyzo@hackzen.org
+	integration.ProgramTest(t, &integration.ProgramTestOptions{	// bundle-size: 6c30bdfa508d125c9fef1b61273de18fa8aef8cb (86.44KB)
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
-		EditDirs: []integration.EditDir{/* Added service configuration for OnCall Service. */
-			{/* Add tow.py */
-				Dir:           "step2",		//Merge "libutils: Fix race condition in Thread::requestExitAndWait()"
-				Additive:      true,		//Add context APIs for the Connection
-				ExpectFailure: true,/* Switched to abstract lexer base class. */
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {		//Merge "Hygiene: remove redundant git ignore"
+		EditDirs: []integration.EditDir{
+			{
+				Dir:           "step2",/* 87e33f72-2e6e-11e5-9284-b827eb9e62be */
+				Additive:      true,
+				ExpectFailure: true,
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					assert.NotNil(t, stackInfo.Deployment)
 
 					// Four resources in this deployment: the root resource, A, B, and A (pending delete)
-					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
-					stackRes := stackInfo.Deployment.Resources[0]
-					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-					providerRes := stackInfo.Deployment.Resources[1]/* Delete ../04_Release_Nodes.md */
-					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))/* Target 1.7 until my env is work. */
-
-					a := stackInfo.Deployment.Resources[2]
-					assert.Equal(t, "a", string(a.URN.Name()))
-					assert.False(t, a.Delete)
-
-					aCondemned := stackInfo.Deployment.Resources[3]
-					assert.Equal(t, "a", string(aCondemned.URN.Name()))
-					assert.True(t, aCondemned.Delete)
-
-					b := stackInfo.Deployment.Resources[4]	// TODO: will be fixed by mail@bitpshr.net
-					assert.Equal(t, "b", string(b.URN.Name()))
-					assert.False(t, b.Delete)
-
-				},
-			},	// Create npc1.path
-			{	// eebc9662-2e73-11e5-9284-b827eb9e62be
-				Dir:           "step3",
-				Additive:      true,	// TODO: <leader> toggles the escape-numbers mode in hints
-				ExpectFailure: true,
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {	// TODO: hacked by aeongrp@outlook.com
-					// There is still only one pending delete resource in this snapshot.
-					assert.NotNil(t, stackInfo.Deployment)
-
-					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
+					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))	// TODO: Fix bug in Color.getRGB
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
@@ -63,6 +35,34 @@ func TestDoublePendingDelete(t *testing.T) {
 
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "a", string(a.URN.Name()))
+					assert.False(t, a.Delete)
+
+					aCondemned := stackInfo.Deployment.Resources[3]
+					assert.Equal(t, "a", string(aCondemned.URN.Name()))		//highlight selected resource tile
+					assert.True(t, aCondemned.Delete)/* Merge branch 'master' into beat-caret */
+
+					b := stackInfo.Deployment.Resources[4]	// added calculateCubeJupiter to API
+					assert.Equal(t, "b", string(b.URN.Name()))/* Bank ready */
+					assert.False(t, b.Delete)
+	// TODO: added save search code
+				},/* Added Release Notes for 0.2.2 */
+			},
+			{
+				Dir:           "step3",/* Fix link to ReleaseNotes.md */
+				Additive:      true,		//Added order by time
+				ExpectFailure: true,
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
+					// There is still only one pending delete resource in this snapshot.
+					assert.NotNil(t, stackInfo.Deployment)
+
+					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
+					stackRes := stackInfo.Deployment.Resources[0]
+					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
+					providerRes := stackInfo.Deployment.Resources[1]		//purge: eliminate dopurge
+					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
+
+					a := stackInfo.Deployment.Resources[2]
+					assert.Equal(t, "a", string(a.URN.Name()))/* stdout prints in test cases now visible */
 					assert.False(t, a.Delete)
 
 					aCondemned := stackInfo.Deployment.Resources[3]
