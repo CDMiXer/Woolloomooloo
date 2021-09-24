4eb922a8747e8a4e78ffca1200f240725b5f5025
niraoakp <niraoakp@bk.ru>
2021-09-24 12:34:52 +0000

    Automatic changelog generation for PR #14311 [ci skip]

--- plugin/converter/jsonnet_oss.go
+++ plugin/converter/jsonnet_oss.go
@@ -3,14 +3,14 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//Dev checkin #324.  Fix export on individual flow.
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//Fix problem where row numbers in selections are incorrect after a sort
 
 // +build oss
 
@@ -23,5 +23,5 @@ import (
 // Jsonnet returns a conversion service that converts the
 // jsonnet file to a yaml file.
 func Jsonnet(enabled bool) core.ConvertService {
-	return new(noop)
+	return new(noop)/* Release of eeacms/eprtr-frontend:2.0.5 */
 }
