d9d139fabb59caca6c2bb78eec0624e02e789921
niraoakp <niraoakp@bk.ru>
2021-09-24 09:27:27 +0000

    Creating the Maven project.

--- handler/api/repos/secrets/list.go
+++ handler/api/repos/secrets/list.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Simplify less for the LoadingThrobber
-// that can be found in the LICENSE file./* Added figures for slides. */
-		//Corrected bad class reference in "Adding own commands"
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
 // +build !oss
 
 package secrets
@@ -10,31 +10,31 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// Fix missing method
+	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
-// list of secrets to the response body./* prevent mob mounting through blocks */
-func HandleList(	// TODO: hacked by joshua@yottadb.com
+// list of secrets to the response body.
+func HandleList(
 	repos core.RepositoryStore,
-	secrets core.SecretStore,	// TODO: Update asdf.js
+	secrets core.SecretStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* fixed keycodes and button, color selection */
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")	// Update easy-autoupdating-site.md
-		)/* Release of eeacms/www:19.3.18 */
-		repo, err := repos.FindName(r.Context(), namespace, name)/* david in the house */
+			name      = chi.URLParam(r, "name")
+		)
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-		list, err := secrets.List(r.Context(), repo.ID)		//readFrom, writeTo and getBytes for ByteArrayQueue
+		list, err := secrets.List(r.Context(), repo.ID)
 		if err != nil {
 			render.NotFound(w, err)
-			return/* Create FacturaWebReleaseNotes.md */
+			return
 		}
 		// the secret list is copied and the secret value is
 		// removed from the response.
