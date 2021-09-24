298f470e76ec4c0ddc58c6d2763c9b7f38f81c01
niraoakp <niraoakp@bk.ru>
2021-09-24 13:50:35 +0000

    c7b79588-2e70-11e5-9284-b827eb9e62be

--- chain/actors/builtin/verifreg/v4.go
+++ chain/actors/builtin/verifreg/v4.go
@@ -1,9 +1,9 @@
 package verifreg
 
-import (/* Add linuxbrew to readme */
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"/* Add support to Django 1.11 */
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
@@ -11,15 +11,15 @@ import (/* Add linuxbrew to readme */
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	verifreg4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/verifreg"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
-)/* fix padding container */
+)
 
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {	// TODO: Update Readme - frontend images
-		return nil, err	// #add string service and json and mysql helper to html
+	if err != nil {
+		return nil, err
 	}
 	return &out, nil
 }
@@ -30,24 +30,24 @@ type state4 struct {
 }
 
 func (s *state4) RootKey() (address.Address, error) {
-	return s.State.RootKey, nil	// TODO: rev 879289
+	return s.State.RootKey, nil
 }
 
 func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiedClients, addr)
-}		//add implementation for compare_to_remote_version
+}
 
 func (s *state4) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiers, addr)
 }
 
-func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* Release of eeacms/plonesaas:5.2.1-52 */
+func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version4, s.verifiers, cb)
-}/* Release v3.1.5 */
-/* Merge "Use settings to persist sticky widget." into jb-mr1-lockscreen-dev */
-func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {	// TODO: Added example picture
+}
+
+func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version4, s.verifiedClients, cb)
-}/* Mudança geral */
+}
 
 func (s *state4) verifiedClients() (adt.Map, error) {
 	return adt4.AsMap(s.store, s.VerifiedClients, builtin4.DefaultHamtBitwidth)
@@ -55,4 +55,4 @@ func (s *state4) verifiedClients() (adt.Map, error) {
 
 func (s *state4) verifiers() (adt.Map, error) {
 	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)
-}/* releasing 1.58 */
+}
