cd5da3d3624c7043c2a7a866c9e139243e88cfe1
niraoakp <niraoakp@bk.ru>
2021-09-24 23:07:21 +0000

    update #6899

--- tests/integration/query/query_test.go
+++ tests/integration/query/query_test.go
@@ -1,36 +1,36 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-
+	// TODO: will be fixed by alan.shaw@protocol.ai
 package ints
 
 import (
-	"testing"
+	"testing"	// Added to provide utilities
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"		//changes py3k-urllib branch
+)/* Release: 6.3.1 changelog */
 
 // TestQuery creates a stack and runs a query over the stack's resource ouptputs.
 func TestQuery(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		// Create Pulumi resources.
-		Dir:          "step1",
+		Dir:          "step1",	// TODO: will be fixed by ligi@ligi.de
 		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",
-		Dependencies: []string{"@pulumi/pulumi"},
+		Dependencies: []string{"@pulumi/pulumi"},		//Updated library socket.io-client
 		CloudURL:     "file://~", // Required; we hard-code the stack name
-		EditDirs: []integration.EditDir{
+		EditDirs: []integration.EditDir{/* Merge branch 'master' into art-branch!-use-newcomb-top-5- */
 			// Try to create resources during `pulumi query`. This should fail.
 			{
 				Dir:           "step2",
 				Additive:      true,
 				QueryMode:     true,
-				ExpectFailure: true,
+				ExpectFailure: true,		//update zip?
 			},
 			// Run a query during `pulumi query`. Should succeed.
-			{
+			{/* [artifactory-release] Release version 1.4.3.RELEASE */
 				Dir:           "step3",
-				Additive:      true,
+				Additive:      true,	// TODO: will be fixed by greg@colvin.org
 				QueryMode:     true,
-				ExpectFailure: false,
+				ExpectFailure: false,/* 94caf164-2e49-11e5-9284-b827eb9e62be */
 			},
 		},
 	})
