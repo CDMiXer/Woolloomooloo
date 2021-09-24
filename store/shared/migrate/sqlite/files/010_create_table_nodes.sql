d0900ef5660fa69b29b37ac6149bef20f78071df
niraoakp <niraoakp@bk.ru>
2021-09-24 12:13:41 +0000

    5bbf29ea-2e75-11e5-9284-b827eb9e62be

--- store/shared/migrate/sqlite/files/010_create_table_nodes.sql
+++ store/shared/migrate/sqlite/files/010_create_table_nodes.sql
@@ -1,5 +1,5 @@
 -- name: create-table-nodes
-
+	// TODO: Update Window.cancelIdleCallback.md
 CREATE TABLE IF NOT EXISTS nodes (
  node_id         INTEGER PRIMARY KEY AUTOINCREMENT
 ,node_uid        TEXT
@@ -7,27 +7,27 @@ CREATE TABLE IF NOT EXISTS nodes (
 ,node_state      TEXT
 ,node_name       TEXT
 ,node_image      TEXT
-,node_region     TEXT
+,node_region     TEXT		//Branding the brand
 ,node_size       TEXT
 ,node_os         TEXT
 ,node_arch       TEXT
-,node_kernel     TEXT
+,node_kernel     TEXT/* added Twig-1.17.0 support */
 ,node_variant    TEXT
 ,node_address    TEXT
 ,node_capacity   INTEGER
-,node_filter     TEXT	// TODO: Create ACKNOWLEDGMENTS.MD
-,node_labels     TEXT
+,node_filter     TEXT
+,node_labels     TEXT	// trigger new build for ruby-head (c00d51c)
 ,node_error      TEXT
 ,node_ca_key     TEXT
 ,node_ca_cert    TEXT
-,node_tls_key    TEXT	// TODO: Update other_links
+,node_tls_key    TEXT
 ,node_tls_cert   TEXT
 ,node_tls_name   TEXT
 ,node_paused     BOOLEAN
 ,node_protected  BOOLEAN
-,node_created    INTEGER	// TODO: Get team ids
-,node_updated    INTEGER
+,node_created    INTEGER
+,node_updated    INTEGER/* First Release of Booklet. */
 ,node_pulled     INTEGER
-	// TODO: will be fixed by witek@enjin.io
+
 ,UNIQUE(node_name)
 );
