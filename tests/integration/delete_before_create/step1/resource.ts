0b725bae85fabc214ddf1e6c611ce8f64e3a887a
niraoakp <niraoakp@bk.ru>
2021-09-24 20:37:48 +0000

    ScriptEngineMappingStrategyTest

--- tests/integration/delete_before_create/step1/resource.ts
+++ tests/integration/delete_before_create/step1/resource.ts
@@ -1,39 +1,39 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// Updated to V2
+
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 import uuidv4 = require("uuid/v4");
 
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
-		//Update cld.sh
-    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {	// TODO: [FIX] Removed tabs instead of spaces from css.
+
+    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
             inputs: news,
-        };		//Updated the tvb-data feedstock.
-    }
-/* (v2) Texture packer: fix NPE. */
-    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
-        if (olds.state !== news.state) {/* Added to Dev and Social/Fun/Cool */
-            return {/* Release of eeacms/jenkins-master:2.249.2 */
-                changes: true,
-                replaces: ["state"],/* Deleted msmeter2.0.1/Release/rc.command.1.tlog */
-                deleteBeforeReplace: news.noDBR ? false : true,
-            };
-        }
-/* Added a summary of what this application is in the README */
-        if (olds.noReplace !== news.noReplace) {
-            return {
-                changes: true,
-            }/* * gem rake tasks now show up inside a rails app with rake -T. */
-        }
-
-        return {/* Create CheckColor */
-            changes: false,	// TODO: Fix display bug in waste widget
         };
     }
 
-    public async create(inputs: any): Promise<dynamic.CreateResult> {	// TODO: Removed un-needed dependencies and spec bump
+    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
+        if (olds.state !== news.state) {
+            return {
+                changes: true,
+                replaces: ["state"],
+                deleteBeforeReplace: news.noDBR ? false : true,
+            };
+        }
+
+        if (olds.noReplace !== news.noReplace) {
+            return {
+                changes: true,
+            }
+        }
+
+        return {
+            changes: false,
+        };
+    }
+
+    public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
             id: uuidv4(),
             outs: inputs,
@@ -41,7 +41,7 @@ export class Provider implements dynamic.ResourceProvider {
     }
 }
 
-export class Resource extends pulumi.dynamic.Resource {	// TODO: Delete monsters.png
+export class Resource extends pulumi.dynamic.Resource {
     public uniqueKey?: pulumi.Output<number>;
     public state: pulumi.Output<number>;
     public noReplace?: pulumi.Output<number>;
@@ -50,9 +50,9 @@ export class Resource extends pulumi.dynamic.Resource {	// TODO: Delete monsters
         super(Provider.instance, name, props, opts);
     }
 }
-/* Rename url.md to url.html */
+
 export interface ResourceProps {
-    readonly uniqueKey?: pulumi.Input<number>;/* Add url to list introduction */
+    readonly uniqueKey?: pulumi.Input<number>;
     readonly state: pulumi.Input<number>;
     readonly noReplace?: pulumi.Input<number>;
     readonly noDBR?: pulumi.Input<boolean>;
