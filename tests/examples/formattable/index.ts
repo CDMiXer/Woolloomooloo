bae773e5d148c03f7fbda4e1181a5287a524fe75
niraoakp <niraoakp@bk.ru>
2021-09-24 09:41:56 +0000

    README: I meant a dissembler, not a compiler

--- tests/examples/formattable/index.ts
+++ tests/examples/formattable/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by nick@perfectabstractions.com
 
-console.log("Here's some format goo: %s, %v, %q, %d!");
-console.error("Here's some format goo: %s, %v, %q, %d!");
+console.log("Here's some format goo: %s, %v, %q, %d!");		//add EdgesHelper.class
+console.error("Here's some format goo: %s, %v, %q, %d!");	// Update KeyboardHandler.podspec
