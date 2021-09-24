7deed7c0cea60f059e8cafc13e7a2326510527f9
niraoakp <niraoakp@bk.ru>
2021-09-24 04:18:22 +0000

    updates peer dependencies to remove warnings

--- plugin/secret/combine_test.go
+++ plugin/secret/combine_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Rename Excersice-1/DumpVariable.php to Exercise-1/DumpVariable.php */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package secret
