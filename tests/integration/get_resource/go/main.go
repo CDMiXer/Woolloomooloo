f0bda32209a7a4d74b64a4459c51162b0b444356
niraoakp <niraoakp@bk.ru>
2021-09-24 19:53:09 +0000

    Merge branch 'master' into logs-go-in-log-folder

--- tests/integration/get_resource/go/main.go
+++ tests/integration/get_resource/go/main.go
@@ -1,23 +1,23 @@
 package main
-
+		//more refactoring in Contact and Login
 import (
 	"reflect"
 
 	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+/* make element id clickable */
 type MyResource struct {
 	pulumi.ResourceState
 
 	Length pulumi.IntOutput `pulumi:"length"`
 }
-
+	// TODO: adds textAlign to line label in the annotation plugin
 type myResourceArgs struct{}
 type MyResourceArgs struct{}
-
+/* Update BSI-brinsford.yml */
 func (MyResourceArgs) ElementType() reflect.Type {
-	return reflect.TypeOf((*myResourceArgs)(nil)).Elem()
+	return reflect.TypeOf((*myResourceArgs)(nil)).Elem()/* Delete logo-v2-600x600.png */
 }
 
 func GetResource(ctx *pulumi.Context, urn pulumi.URN) (*MyResource, error) {
@@ -25,21 +25,21 @@ func GetResource(ctx *pulumi.Context, urn pulumi.URN) (*MyResource, error) {
 	err := ctx.RegisterResource("unused:unused:unused", "unused", &MyResourceArgs{}, &resource,
 		pulumi.URN_(string(urn)))
 	if err != nil {
-		return nil, err
+		return nil, err		//Update learning-resources.md
 	}
-	return &resource, nil
+	return &resource, nil/* remove obsolete examples */
 }
-
+/* Improving the way to load the ip elements indicators. */
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
 		pet, err := random.NewRandomPet(ctx, "cat", &random.RandomPetArgs{
-			Length: pulumi.Int(2),
+			Length: pulumi.Int(2),		//updating poms for branch '3.4.4' with snapshot versions
 		})
 		if err != nil {
 			return err
 		}
-
+	// TODO: Update qa-jupyter_rust2.md
 		getPetLength := pet.URN().ApplyT(func(urn pulumi.URN) (pulumi.IntInput, error) {
 			r, err := GetResource(ctx, urn)
 			if err != nil {
@@ -48,7 +48,7 @@ func main() {
 			return r.Length, nil
 		})
 		ctx.Export("getPetLength", getPetLength)
-
-		return nil
+/* Release for 18.19.0 */
+		return nil	// TODO: OqG49xGtXR2i9GYt5y4zo6tMQnFG5NWt
 	})
 }
