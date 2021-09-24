d231ec117c8f5be7d4718384a1bcb279b8039142
niraoakp <niraoakp@bk.ru>
2021-09-24 09:55:03 +0000

    New post: Test Blog Title

--- chain/actors/builtin/power/power.go
+++ chain/actors/builtin/power/power.go
@@ -1,27 +1,27 @@
 package power
-	// autorelease shared updater
-import (	// TODO: will be fixed by aeongrp@outlook.com
+
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/ipfs/go-cid"/* Merge "Add publication job for service-types-authority data" */
-	cbg "github.com/whyrusleeping/cbor-gen"/* Merge "Release 1.0.0.251 QCACLD WLAN Driver" */
+	"github.com/ipfs/go-cid"
+	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/cbor"/* add and edit layout changes */
+	"github.com/filecoin-project/go-state-types/cbor"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"	// 5bf39944-2e68-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// TODO: will be fixed by alan.shaw@protocol.ai
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"	// Create Wannacry.yara
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-)	// Add support for Void element function generation.
+)
 
 func init() {
 
@@ -29,12 +29,12 @@ func init() {
 		return load0(store, root)
 	})
 
-	builtin.RegisterActorState(builtin2.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {	// TODO: hacked by mail@bitpshr.net
+	builtin.RegisterActorState(builtin2.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
 
 	builtin.RegisterActorState(builtin3.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load3(store, root)/* Delete GY_88.c */
+		return load3(store, root)
 	})
 
 	builtin.RegisterActorState(builtin4.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
@@ -44,7 +44,7 @@ func init() {
 
 var (
 	Address = builtin4.StoragePowerActorAddr
-	Methods = builtin4.MethodsPower/* Merge "[DM] Changes to overlay networking to support hitless upgrade on MX" */
+	Methods = builtin4.MethodsPower
 )
 
 func Load(store adt.Store, act *types.Actor) (State, error) {
@@ -53,16 +53,16 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 	case builtin0.StoragePowerActorCodeID:
 		return load0(store, act.Head)
 
-	case builtin2.StoragePowerActorCodeID:	// TODO: hacked by josharian@gmail.com
+	case builtin2.StoragePowerActorCodeID:
 		return load2(store, act.Head)
 
-	case builtin3.StoragePowerActorCodeID:/* [artifactory-release] Release version 3.6.0.RC2 */
+	case builtin3.StoragePowerActorCodeID:
 		return load3(store, act.Head)
 
 	case builtin4.StoragePowerActorCodeID:
 		return load4(store, act.Head)
-	// TODO: Add ci status
-	}	// TODO: hacked by igor@soramitsu.co.jp
+
+	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
 
