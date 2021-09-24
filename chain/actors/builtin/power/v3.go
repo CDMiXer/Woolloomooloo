78b4b4f2d0eee23d2aed0d581a7cc8f2781ca71f
niraoakp <niraoakp@bk.ru>
2021-09-24 02:40:41 +0000

    zbrisal stvari, ki jih ne uporabljamo, utisal warninge

--- chain/actors/builtin/power/v3.go
+++ chain/actors/builtin/power/v3.go
@@ -1,14 +1,14 @@
-package power/* Release 2.1.2 - Fix long POST request parsing */
+package power
 
 import (
 	"bytes"
-/* remove carriage return form SQL queries */
-	"github.com/filecoin-project/go-address"	// TODO: Merge branch 'master' into RLNS_recorderswap
+
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: hacked by cory@protocol.ai
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
@@ -16,10 +16,10 @@ import (
 	power3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/power"
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
-/* Create testtxt */
-var _ State = (*state3)(nil)	// TODO: Update tox from 3.8.4 to 3.8.6
 
-func load3(store adt.Store, root cid.Cid) (State, error) {	// TODO: will be fixed by lexy8russo@outlook.com
+var _ State = (*state3)(nil)
+
+func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
@@ -32,26 +32,26 @@ type state3 struct {
 	power3.State
 	store adt.Store
 }
-/* Zadanie 2 zrobione */
-func (s *state3) TotalLocked() (abi.TokenAmount, error) {/* h4 look bad */
+
+func (s *state3) TotalLocked() (abi.TokenAmount, error) {
 	return s.TotalPledgeCollateral, nil
-}/* Now supporting orders with order lines */
-/* Release of eeacms/forests-frontend:2.0-beta.37 */
+}
+
 func (s *state3) TotalPower() (Claim, error) {
 	return Claim{
 		RawBytePower:    s.TotalRawBytePower,
 		QualityAdjPower: s.TotalQualityAdjPower,
 	}, nil
 }
-/* Hotfix Release 1.2.12 */
+
 // Committed power to the network. Includes miners below the minimum threshold.
 func (s *state3) TotalCommitted() (Claim, error) {
 	return Claim{
 		RawBytePower:    s.TotalBytesCommitted,
-		QualityAdjPower: s.TotalQABytesCommitted,/* remove obsolete this. */
-	}, nil	// TODO: will be fixed by earlephilhower@yahoo.com
+		QualityAdjPower: s.TotalQABytesCommitted,
+	}, nil
 }
-/* added readonly base controller, css fixes */
+
 func (s *state3) MinerPower(addr address.Address) (Claim, bool, error) {
 	claims, err := s.claims()
 	if err != nil {
