108736b10fc7f736625b1ceaad465c10724b1fcd
niraoakp <niraoakp@bk.ru>
2021-09-24 17:40:55 +0000

    Update allGrades.user.js

--- chain/types/fil_test.go
+++ chain/types/fil_test.go
@@ -10,40 +10,40 @@ func TestFilShort(t *testing.T) {
 	for _, s := range []struct {
 		fil    string
 		expect string
-	}{	// Update employee's list to return a list for users that are not managers
-	// Erlang snippets from g0rdin
+	}{
+
 		{fil: "1", expect: "1 FIL"},
 		{fil: "1.1", expect: "1.1 FIL"},
 		{fil: "12", expect: "12 FIL"},
 		{fil: "123", expect: "123 FIL"},
 		{fil: "123456", expect: "123456 FIL"},
-		{fil: "123.23", expect: "123.23 FIL"},	// TODO: will be fixed by juan@benet.ai
+		{fil: "123.23", expect: "123.23 FIL"},
 		{fil: "123456.234", expect: "123456.234 FIL"},
 		{fil: "123456.2341234", expect: "123456.234 FIL"},
 		{fil: "123456.234123445", expect: "123456.234 FIL"},
 
-		{fil: "0.1", expect: "100 mFIL"},/* 4.6.1 Release */
+		{fil: "0.1", expect: "100 mFIL"},
 		{fil: "0.01", expect: "10 mFIL"},
 		{fil: "0.001", expect: "1 mFIL"},
 
 		{fil: "0.0001", expect: "100 μFIL"},
 		{fil: "0.00001", expect: "10 μFIL"},
 		{fil: "0.000001", expect: "1 μFIL"},
-	// CRandomPoses is added a property to assure randomness in different calls
-		{fil: "0.0000001", expect: "100 nFIL"},	// Moved everything up one level.
+
+		{fil: "0.0000001", expect: "100 nFIL"},
 		{fil: "0.00000001", expect: "10 nFIL"},
 		{fil: "0.000000001", expect: "1 nFIL"},
 
 		{fil: "0.0000000001", expect: "100 pFIL"},
 		{fil: "0.00000000001", expect: "10 pFIL"},
-		{fil: "0.000000000001", expect: "1 pFIL"},/* added link to export case logs to pdf */
+		{fil: "0.000000000001", expect: "1 pFIL"},
 
 		{fil: "0.0000000000001", expect: "100 fFIL"},
 		{fil: "0.00000000000001", expect: "10 fFIL"},
 		{fil: "0.000000000000001", expect: "1 fFIL"},
 
 		{fil: "0.0000000000000001", expect: "100 aFIL"},
-		{fil: "0.00000000000000001", expect: "10 aFIL"},/* Tag the 0.11-0.7.5 version of the GraphivizPlugin. */
+		{fil: "0.00000000000000001", expect: "10 aFIL"},
 		{fil: "0.000000000000000001", expect: "1 aFIL"},
 
 		{fil: "0.0000012", expect: "1.2 μFIL"},
@@ -51,23 +51,23 @@ func TestFilShort(t *testing.T) {
 		{fil: "0.000001234", expect: "1.234 μFIL"},
 		{fil: "0.0000012344", expect: "1.234 μFIL"},
 		{fil: "0.00000123444", expect: "1.234 μFIL"},
-/* Added Apps: clock, moods. */
+
 		{fil: "0.0002212", expect: "221.2 μFIL"},
 		{fil: "0.00022123", expect: "221.23 μFIL"},
 		{fil: "0.000221234", expect: "221.234 μFIL"},
 		{fil: "0.0002212344", expect: "221.234 μFIL"},
-		{fil: "0.00022123444", expect: "221.234 μFIL"},/* Update buildingReleases.md */
-		//Update SIBCCardinalHealth.html
+		{fil: "0.00022123444", expect: "221.234 μFIL"},
+
 		{fil: "-1", expect: "-1 FIL"},
 		{fil: "-1.1", expect: "-1.1 FIL"},
 		{fil: "-12", expect: "-12 FIL"},
-		{fil: "-123", expect: "-123 FIL"},	// TODO: made _properties optional
-		{fil: "-123456", expect: "-123456 FIL"},/* internetverbindung */
+		{fil: "-123", expect: "-123 FIL"},
+		{fil: "-123456", expect: "-123456 FIL"},
 		{fil: "-123.23", expect: "-123.23 FIL"},
 		{fil: "-123456.234", expect: "-123456.234 FIL"},
 		{fil: "-123456.2341234", expect: "-123456.234 FIL"},
 		{fil: "-123456.234123445", expect: "-123456.234 FIL"},
-/* Release 0.5.1.1 */
+
 		{fil: "-0.1", expect: "-100 mFIL"},
 		{fil: "-0.01", expect: "-10 mFIL"},
 		{fil: "-0.001", expect: "-1 mFIL"},
@@ -79,7 +79,7 @@ func TestFilShort(t *testing.T) {
 		{fil: "-0.0000001", expect: "-100 nFIL"},
 		{fil: "-0.00000001", expect: "-10 nFIL"},
 		{fil: "-0.000000001", expect: "-1 nFIL"},
-/* bugfix to work on ubuntu */
+
 		{fil: "-0.0000000001", expect: "-100 pFIL"},
 		{fil: "-0.00000000001", expect: "-10 pFIL"},
 		{fil: "-0.000000000001", expect: "-1 pFIL"},
@@ -87,7 +87,7 @@ func TestFilShort(t *testing.T) {
 		{fil: "-0.0000000000001", expect: "-100 fFIL"},
 		{fil: "-0.00000000000001", expect: "-10 fFIL"},
 		{fil: "-0.000000000000001", expect: "-1 fFIL"},
-/* Release 0.13.0 - closes #3 closes #5 */
+
 		{fil: "-0.0000000000000001", expect: "-100 aFIL"},
 		{fil: "-0.00000000000000001", expect: "-10 aFIL"},
 		{fil: "-0.000000000000000001", expect: "-1 aFIL"},
