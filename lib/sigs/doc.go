b0b892260e6d9fd5ac73aac80ec0d28b59e4d748
niraoakp <niraoakp@bk.ru>
2021-09-24 06:20:53 +0000

    Dependency erest is just too thin a layer, remove it

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -1,9 +1,9 @@
 // Package sigs allows for signing, verifying signatures and key generation
 // using key types selected by package user.
 //
-// For support of secp256k1 import:
+// For support of secp256k1 import:/* ROO-2440: Release Spring Roo 1.1.4.RELEASE */
 // _ "github.com/filecoin-project/lotus/lib/sigs/secp"
 //
-// For  support of Filecoin BLS import:		//Change the scaling ratios to be exponential.
-// _ "github.com/filecoin-project/lotus/lib/sigs/bls"
-package sigs
+// For  support of Filecoin BLS import:
+// _ "github.com/filecoin-project/lotus/lib/sigs/bls"/* bid 1 for root */
+package sigs	// TODO: Fix typos and capitalize titles
