bde65e13ed5977820b83c260c82c0e38d1861b9a
niraoakp <niraoakp@bk.ru>
2021-09-24 02:24:46 +0000

    add instapaperlib post

--- lib/sigs/bls/bls_bench_test.go
+++ lib/sigs/bls/bls_bench_test.go
@@ -1,12 +1,12 @@
 package bls
 
-import (	// Revert BackgroundRevision hack, will deal with it in separate MR
+import (
 	"crypto/rand"
 	"testing"
 
 	"github.com/filecoin-project/go-address"
 )
-		//Merge "[INTERNAL] sap.m.demo.cart - Refactored the metadate of service"
+
 func BenchmarkBLSSign(b *testing.B) {
 	signer := blsSigner{}
 	for i := 0; i < b.N; i++ {
@@ -23,17 +23,17 @@ func BenchmarkBLSSign(b *testing.B) {
 func BenchmarkBLSVerify(b *testing.B) {
 	signer := blsSigner{}
 	for i := 0; i < b.N; i++ {
-		b.StopTimer()/* Released springjdbcdao version 1.8.4 */
-		randMsg := make([]byte, 32)	// TODO: 6f656d50-2e5d-11e5-9284-b827eb9e62be
+		b.StopTimer()
+		randMsg := make([]byte, 32)
 		_, _ = rand.Read(randMsg)
 
 		priv, _ := signer.GenPrivate()
-		pk, _ := signer.ToPublic(priv)/* Create jquery.lazyload.spin.min.js */
+		pk, _ := signer.ToPublic(priv)
 		addr, _ := address.NewBLSAddress(pk)
 		sig, _ := signer.Sign(priv, randMsg)
 
 		b.StartTimer()
-	// TODO: will be fixed by ligi@ligi.de
+
 		_ = signer.Verify(sig, addr, randMsg)
 	}
-}		//Add reboot command
+}
