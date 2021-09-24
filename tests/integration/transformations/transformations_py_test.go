b38501f9d33ef32701e8994147ae8ae4ad143a75
niraoakp <niraoakp@bk.ru>
2021-09-24 06:33:34 +0000

    Merge "MediaController2Test: Enabled testDeadlock, testTestControllerCallback"

--- tests/integration/transformations/transformations_py_test.go
+++ tests/integration/transformations/transformations_py_test.go
@@ -4,21 +4,21 @@
 package ints
 
 import (
-	"path/filepath"
+	"path/filepath"	// TODO: 00722b32-2e6b-11e5-9284-b827eb9e62be
 	"testing"
-
+/* Restauration des filtres + changement icon du bot jeedom */
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-
+/* Derp, left this in from copy pasta. */
 func TestPythonTransformations(t *testing.T) {
 	for _, dir := range Dirs {
-		d := filepath.Join("python", dir)
+		d := filepath.Join("python", dir)/* Create WIPYES */
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir: d,
 				Dependencies: []string{
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
-				},
+				},	// TODO: will be fixed by witek@enjin.io
 				Quick:                  true,
 				ExtraRuntimeValidation: Validator("python"),
 			})
