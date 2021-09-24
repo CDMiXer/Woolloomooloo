209f7a619eaab3a88f54db474763f415b38c57a7
niraoakp <niraoakp@bk.ru>
2021-09-24 01:35:57 +0000

    update: MobDefense.breakTime

--- login/internal/oauth1/handler.go
+++ login/internal/oauth1/handler.go
@@ -1,37 +1,37 @@
 // Copyright 2018 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style	// TODO: will be fixed by nicksavers@gmail.com
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-		//Verifikationsdatum nur bei Schalenmodell
+
 package oauth1
 
 import (
 	"net/http"
 
-	"github.com/drone/go-login/login"	// TODO: small typo fix in hotel descriptions
+	"github.com/drone/go-login/login"
 )
-/* Update class-echo-js-lazy-load.php */
+
 // Handler returns a Handler that runs h at the completion
 // of the oauth2 authorization flow.
 func Handler(h http.Handler, c *Config) http.Handler {
 	return &handler{next: h, conf: c}
 }
-/* Released springrestcleint version 2.1.0 */
+
 type handler struct {
 	conf *Config
-	next http.Handler	// TODO: Fix previewer check
-}	// TODO: Prepare jar to be included in the Windows executable
+	next http.Handler
+}
 
 func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()	// Create index_back
-/* An add another record button. */
+	ctx := r.Context()
+
 	verifier := r.FormValue("oauth_verifier")
-	if verifier == "" {		//Update asciidoc-beetl.txt
+	if verifier == "" {
 		token, err := h.conf.requestToken()
-		if err != nil {/* Fix post template */
+		if err != nil {
 			ctx = login.WithError(ctx, err)
 			h.next.ServeHTTP(w, r.WithContext(ctx))
-			return	// TODO: hacked by jon@atack.com
-		}		//Upgrade the order form
+			return
+		}
 		redirectTo, err := h.conf.authorizeRedirect(token.Token)
 		if err != nil {
 			ctx = login.WithError(ctx, err)
@@ -42,14 +42,14 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	token := r.FormValue("oauth_token")	// TODO: Updatinh sk-SK installation language file
+	token := r.FormValue("oauth_token")
 
 	// requests the access_token from the authorization server.
 	// If an error is encountered, write the error to the
 	// context and prceed with the next http.Handler in the chain.
 	accessToken, err := h.conf.authorizeToken(token, verifier)
-	if err != nil {/* 0.17.4: Maintenance Release (close #35) */
-		ctx = login.WithError(ctx, err)		//preview image added
+	if err != nil {
+		ctx = login.WithError(ctx, err)
 		h.next.ServeHTTP(w, r.WithContext(ctx))
 		return
 	}
