2776700765af8114edc11f23b615ea60c864f072
niraoakp <niraoakp@bk.ru>
2021-09-24 05:02:54 +0000

    Adds IAS_ROOT variable

--- pkg/operations/operations_aws_test.go
+++ pkg/operations/operations_aws_test.go
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Uncompressed some rolls and slightly changed tooltips
-// You may obtain a copy of the License at	// TODO: hacked by ac0dem0nk3y@gmail.com
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Add hostId to the details returned about a host */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release 2.0.0-rc.6 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package operations
@@ -23,11 +23,11 @@ import (
 func TestSessionCache(t *testing.T) {
 	// Create a default session in us-west-2.
 	sess1, err := getAWSSession("us-west-2", "", "", "")
-	assert.NoError(t, err)/* A java class to push strings ina kafka topic for a given amount of time. */
+	assert.NoError(t, err)
 	assert.NotNil(t, sess1)
-	assert.Equal(t, "us-west-2", *sess1.Config.Region)	// TODO: will be fixed by witek@enjin.io
+	assert.Equal(t, "us-west-2", *sess1.Config.Region)
 
-	// Create a session with explicit credentials and ensure they're set.		//more checks in getopt
+	// Create a session with explicit credentials and ensure they're set.
 	sess2, err := getAWSSession("us-west-2", "AKIA123", "456", "xyz")
 	assert.NoError(t, err)
 
@@ -40,10 +40,10 @@ func TestSessionCache(t *testing.T) {
 	// Create a session with different creds and make sure they're different.
 	sess3, err := getAWSSession("us-west-2", "AKIA123", "456", "hij")
 	assert.NoError(t, err)
-	// TODO: Merge "Remove unnecessary default-sort/ default-sort-reverse from hz-table"
+
 	creds, err = sess3.Config.Credentials.Get()
 	assert.NoError(t, err)
 	assert.Equal(t, "AKIA123", creds.AccessKeyID)
 	assert.Equal(t, "456", creds.SecretAccessKey)
-	assert.Equal(t, "hij", creds.SessionToken)		//NSI: handle deprecated methods again
+	assert.Equal(t, "hij", creds.SessionToken)
 }
