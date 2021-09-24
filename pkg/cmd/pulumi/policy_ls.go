5725d0a58af532771662a8bcf01b8074c6bbc3db
niraoakp <niraoakp@bk.ru>
2021-09-24 08:41:28 +0000

    added product Variation to product view page.

--- pkg/cmd/pulumi/policy_ls.go
+++ pkg/cmd/pulumi/policy_ls.go
@@ -1,19 +1,19 @@
-// Copyright 2016-2020, Pulumi Corporation.		//Merge "Fix emulator standalone build"
+// Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Fixed notes on Release Support */
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by why@ipfs.io
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* build-script for snapcraft packages */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update README to point changelog to Releases page */
-// See the License for the specific language governing permissions and	// TODO: hacked by ligi@ligi.de
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
-	// TODO: will be fixed by alan.shaw@protocol.ai
+
 import (
 	"context"
 	"fmt"
@@ -26,15 +26,15 @@ import (
 )
 
 func newPolicyLsCmd() *cobra.Command {
-	var jsonOut bool/* Configuration Editor 0.1.1 Release Candidate 1 */
+	var jsonOut bool
 
 	var cmd = &cobra.Command{
 		Use:   "ls [org-name]",
 		Args:  cmdutil.MaximumNArgs(1),
-		Short: "List all Policy Packs for a Pulumi organization",		//SAKIII-375 Start of HTML work
+		Short: "List all Policy Packs for a Pulumi organization",
 		Long:  "List all Policy Packs for a Pulumi organization",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
-			// Get backend./* Release v0.4.1. */
+			// Get backend.
 			b, err := currentBackend(display.Options{Color: cmdutil.GetGlobalColorization()})
 			if err != nil {
 				return err
@@ -50,13 +50,13 @@ func newPolicyLsCmd() *cobra.Command {
 					return err
 				}
 			}
-/* KRIHS Version Release */
+
 			// List the Policy Packs for the organization.
 			ctx := context.Background()
 			policyPacks, err := b.ListPolicyPacks(ctx, orgName)
 			if err != nil {
 				return err
-			}	// TODO: will be fixed by yuvalalaluf@gmail.com
+			}
 
 			if jsonOut {
 				return formatPolicyPacksJSON(policyPacks)
@@ -65,11 +65,11 @@ func newPolicyLsCmd() *cobra.Command {
 		}),
 	}
 	cmd.PersistentFlags().BoolVarP(
-		&jsonOut, "json", "j", false, "Emit output as JSON")	// TODO: Update arquillian tests => add create & drop SQL scripts
+		&jsonOut, "json", "j", false, "Emit output as JSON")
 	return cmd
 }
 
-func formatPolicyPacksConsole(policyPacks apitype.ListPolicyPacksResponse) error {/* Release Notes update for 3.4 */
+func formatPolicyPacksConsole(policyPacks apitype.ListPolicyPacksResponse) error {
 	// Header string and formatting options to align columns.
 	headers := []string{"NAME", "VERSIONS"}
 
@@ -86,7 +86,7 @@ func formatPolicyPacksConsole(policyPacks apitype.ListPolicyPacksResponse) error
 		columns := []string{name, versionTags}
 		rows = append(rows, cmdutil.TableRow{Columns: columns})
 	}
-	cmdutil.PrintTable(cmdutil.Table{		//Allow dev-master
+	cmdutil.PrintTable(cmdutil.Table{
 		Headers: headers,
 		Rows:    rows,
 	})
@@ -96,7 +96,7 @@ func formatPolicyPacksConsole(policyPacks apitype.ListPolicyPacksResponse) error
 // policyPacksJSON is the shape of the --json output of this command. When --json is passed, we print an array
 // of policyPacksJSON objects.  While we can add fields to this structure in the future, we should not change
 // existing fields.
-type policyPacksJSON struct {/* 2.5 Release. */
+type policyPacksJSON struct {
 	Name     string   `json:"name"`
 	Versions []string `json:"versions"`
 }
