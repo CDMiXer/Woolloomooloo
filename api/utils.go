169c2c59a93251018eb6b05830c3617a2a5ade00
niraoakp <niraoakp@bk.ru>
2021-09-24 20:22:44 +0000

    Fixed problem with To-Many relations retrieved as a single object

--- api/utils.go
+++ api/utils.go
@@ -1,7 +1,7 @@
 package api
-	// TODO: hacked by 13860583249@yeah.net
-import (/* [docs] Recommend nbstripout */
-	"context"/* Release 0.2.6 with special thanks to @aledovsky and @douglasjarquin */
+
+import (
+	"context"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
@@ -9,20 +9,20 @@ import (/* [docs] Recommend nbstripout */
 
 type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
 
-type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)/* [artifactory-release] Release version 1.5.0.RC1 */
+type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)
 
 type Signable interface {
-	Sign(context.Context, SignFunc) error	// multi-dim arrays not yet supported with ForeignWrappers
-}	// Add link to Javadoc in README
+	Sign(context.Context, SignFunc) error
+}
 
 func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {
-	for _, s := range signable {		//Merge "Unmute emergency calls when they connect." into klp-dev
+	for _, s := range signable {
 		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {
 			return signer(ctx, addr, b)
 		})
 		if err != nil {
 			return err
 		}
-	}/* Angular JS 1 generator Release v2.5 Beta */
+	}
 	return nil
 }
