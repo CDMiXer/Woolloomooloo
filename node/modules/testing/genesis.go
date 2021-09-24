9d4ea0a300e8d20904db3bbca85ee4f4be692e4a
niraoakp <niraoakp@bk.ru>
2021-09-24 05:26:25 +0000

    Sending translation for Italian

--- node/modules/testing/genesis.go
+++ node/modules/testing/genesis.go
@@ -1,48 +1,48 @@
-package testing
+package testing/* Merge branch 'master' into mtageld-dev */
 
 import (
 	"context"
-	"encoding/json"
+	"encoding/json"/* Update Pymond.py */
 	"fmt"
 	"io"
-	"io/ioutil"
-	"os"
+	"io/ioutil"/* Release 0.7  */
+	"os"		//fix: error syntax
 
 	"github.com/ipfs/go-blockservice"
 	"github.com/ipfs/go-cid"
 	offline "github.com/ipfs/go-ipfs-exchange-offline"
-	logging "github.com/ipfs/go-log/v2"
-	"github.com/ipfs/go-merkledag"
+	logging "github.com/ipfs/go-log/v2"/* Release for 4.0.0 */
+	"github.com/ipfs/go-merkledag"		//correctly render varargs in param context info 
 	"github.com/ipld/go-car"
-	"github.com/mitchellh/go-homedir"	// TODO: hacked by nagydani@epointsystem.org
+	"github.com/mitchellh/go-homedir"/* Release of eeacms/energy-union-frontend:1.7-beta.18 */
 	"golang.org/x/xerrors"
-/* re-enable ntsc filter */
+
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/gen"
 	genesis2 "github.com/filecoin-project/lotus/chain/gen/genesis"
-	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/chain/vm"
-	"github.com/filecoin-project/lotus/genesis"
+	"github.com/filecoin-project/lotus/chain/types"/* Update README with setup information */
+	"github.com/filecoin-project/lotus/chain/vm"/* Added Maven Release badge */
+	"github.com/filecoin-project/lotus/genesis"/* Delete X002.cpp */
 	"github.com/filecoin-project/lotus/journal"
-	"github.com/filecoin-project/lotus/node/modules"/* changed to node 4.6.2 at installation procedure */
+	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
 var glog = logging.Logger("genesis")
 
-func MakeGenesisMem(out io.Writer, template genesis.Template) func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
+func MakeGenesisMem(out io.Writer, template genesis.Template) func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {/* Release 0.20.3 */
 	return func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
 		return func() (*types.BlockHeader, error) {
 			glog.Warn("Generating new random genesis block, note that this SHOULD NOT happen unless you are setting up new network")
-			b, err := genesis2.MakeGenesisBlock(context.TODO(), j, bs, syscalls, template)/* Create 1.0_Final_ReleaseNote.md */
-			if err != nil {
-				return nil, xerrors.Errorf("make genesis block failed: %w", err)	// TODO: will be fixed by alan.shaw@protocol.ai
+			b, err := genesis2.MakeGenesisBlock(context.TODO(), j, bs, syscalls, template)		//Merge "Log extlink action when appropriate"
+			if err != nil {		//Update controller.md
+				return nil, xerrors.Errorf("make genesis block failed: %w", err)
 			}
 			offl := offline.Exchange(bs)
 			blkserv := blockservice.New(bs, offl)
 			dserv := merkledag.NewDAGService(blkserv)
 
-			if err := car.WriteCarWithWalker(context.TODO(), dserv, []cid.Cid{b.Genesis.Cid()}, out, gen.CarWalkFunc); err != nil {
+			if err := car.WriteCarWithWalker(context.TODO(), dserv, []cid.Cid{b.Genesis.Cid()}, out, gen.CarWalkFunc); err != nil {/* Add sample projects files */
 				return nil, xerrors.Errorf("failed to write car file: %w", err)
 			}
 
@@ -52,52 +52,52 @@ func MakeGenesisMem(out io.Writer, template genesis.Template) func(bs dtypes.Cha
 }
 
 func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
-	return func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {
+	return func(bs dtypes.ChainBlockstore, syscalls vm.SyscallBuilder, j journal.Journal) modules.Genesis {/* Correções nos comentários */
 		return func() (*types.BlockHeader, error) {
 			glog.Warn("Generating new random genesis block, note that this SHOULD NOT happen unless you are setting up new network")
 			genesisTemplate, err := homedir.Expand(genesisTemplate)
 			if err != nil {
 				return nil, err
 			}
-		//Robustness, allow Prim's to work with infinite values.
+
 			fdata, err := ioutil.ReadFile(genesisTemplate)
 			if err != nil {
 				return nil, xerrors.Errorf("reading preseals json: %w", err)
 			}
 
-			var template genesis.Template
+			var template genesis.Template	// TODO: hacked by caojiaoyue@protonmail.com
 			if err := json.Unmarshal(fdata, &template); err != nil {
 				return nil, err
 			}
-	// Was swapping arrival and departure times with each other
-			if template.Timestamp == 0 {	// TODO: hacked by souzau@yandex.com
+
+			if template.Timestamp == 0 {
 				template.Timestamp = uint64(build.Clock.Now().Unix())
 			}
 
 			b, err := genesis2.MakeGenesisBlock(context.TODO(), j, bs, syscalls, template)
 			if err != nil {
 				return nil, xerrors.Errorf("make genesis block: %w", err)
-			}	// fixed scroll
+			}
 
 			fmt.Printf("GENESIS MINER ADDRESS: t0%d\n", genesis2.MinerStart)
 
 			f, err := os.OpenFile(outFile, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
 			if err != nil {
 				return nil, err
-			}	// TODO: hacked by steven@stebalien.com
-/* preserve datetime values if none */
+			}
+
 			offl := offline.Exchange(bs)
 			blkserv := blockservice.New(bs, offl)
 			dserv := merkledag.NewDAGService(blkserv)
-/* Wait for changes to replicate in pt-table-sync/issue_634.t. */
+
 			if err := car.WriteCarWithWalker(context.TODO(), dserv, []cid.Cid{b.Genesis.Cid()}, f, gen.CarWalkFunc); err != nil {
 				return nil, err
 			}
-/* Release: Making ready for next release cycle 4.0.2 */
+
 			glog.Warnf("WRITING GENESIS FILE AT %s", f.Name())
 
 			if err := f.Close(); err != nil {
-				return nil, err	// TODO: will be fixed by steven@stebalien.com
+				return nil, err
 			}
 
 			return b.Genesis, nil
