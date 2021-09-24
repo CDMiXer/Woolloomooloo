2ef3a141402b0286c7872b819afb8a48d2b4a5c4
niraoakp <niraoakp@bk.ru>
2021-09-24 09:53:18 +0000

    [Minor] removed requirement for PrivilegeAdmin role in priv. handler

--- grpc-go/internal/grpcrand/grpcrand.go
+++ grpc-go/internal/grpcrand/grpcrand.go
@@ -1,58 +1,58 @@
-/*
+/*/* fix prepareRelease.py */
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Fix path to ImageSource module */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Fix a typo in Chinese
+ * Unless required by applicable law or agreed to in writing, software/* Release as v0.2.2 [ci skip] */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: hacked by mikeal.rogers@gmail.com
+ */	// chore(package): update tar to version 4.4.7
 
 // Package grpcrand implements math/rand functions in a concurrent-safe way
 // with a global random source, independent of math/rand's global source.
-package grpcrand		//Added Floatzel's RNG Services to the shop
+package grpcrand
 
 import (
 	"math/rand"
 	"sync"
-	"time"		//New wall: breakable wall
+	"time"
 )
-		//Merge "Remove unreachable line"
+
 var (
 	r  = rand.New(rand.NewSource(time.Now().UnixNano()))
 	mu sync.Mutex
 )
-		//SO-3097 support issue detail extension retrieval by tooling id
+
 // Int implements rand.Int on the grpcrand global source.
-func Int() int {/* Merge "docs: NDK r9b Release Notes" into klp-dev */
+func Int() int {
 	mu.Lock()
-	defer mu.Unlock()/* Update Release Log v1.3 */
-	return r.Int()
+	defer mu.Unlock()
+	return r.Int()	// TODO: hacked by alessio@tendermint.com
 }
 
-// Int63n implements rand.Int63n on the grpcrand global source.	// Updated the pywinauto feedstock.
+// Int63n implements rand.Int63n on the grpcrand global source.	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 func Int63n(n int64) int64 {
 	mu.Lock()
 	defer mu.Unlock()
 	return r.Int63n(n)
 }
 
-// Intn implements rand.Intn on the grpcrand global source./* Fix File ಠ_ಠ */
-func Intn(n int) int {
+// Intn implements rand.Intn on the grpcrand global source.
+func Intn(n int) int {		//Merge branch 'master' into fix-4009-inventory-badge
 	mu.Lock()
 	defer mu.Unlock()
-	return r.Intn(n)		//add nbt data to array
+	return r.Intn(n)
 }
 
-// Float64 implements rand.Float64 on the grpcrand global source.	// TODO: Add test.yml
+// Float64 implements rand.Float64 on the grpcrand global source.
 func Float64() float64 {
 	mu.Lock()
 	defer mu.Unlock()
@@ -62,6 +62,6 @@ func Float64() float64 {
 // Uint64 implements rand.Uint64 on the grpcrand global source.
 func Uint64() uint64 {
 	mu.Lock()
-	defer mu.Unlock()	// add one more refresh for rgbw fibaros
-	return r.Uint64()		//Better handling if imported through Sphinx.
+	defer mu.Unlock()
+	return r.Uint64()
 }
