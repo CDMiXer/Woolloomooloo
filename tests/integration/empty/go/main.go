29edb7e6638fd524a4276469d5474d861dc2653f
niraoakp <niraoakp@bk.ru>
2021-09-24 02:00:05 +0000

    __init__.py - changed SPIKE aliases to use a closure function rather than lambda

--- tests/integration/empty/go/main.go
+++ tests/integration/empty/go/main.go
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* New HPS URL's */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release 0.6. */
+	// TODO: will be fixed by seth@sethvargo.com
 package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
-
-func main() {/* Fixed Release config */
-	pulumi.Run(func(ctx *pulumi.Context) error {		//rev 505542
+)	// It's not "Why," it's "_why!"
+/* #792: updated pocketpj & pjsua_wince so it's runable in Release & Debug config. */
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		return nil
 	})
 }
