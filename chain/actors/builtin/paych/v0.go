d267f4502417230f184e7dbab501063c30da06fb
niraoakp <niraoakp@bk.ru>
2021-09-24 10:04:56 +0000

    Bugfix: intval returned wrong values for negative numbers

--- chain/actors/builtin/paych/v0.go
+++ chain/actors/builtin/paych/v0.go
@@ -1,46 +1,46 @@
 package paych
-	// TODO: more new icons
-import (	// TODO: Merge pull request #202 from fkautz/pr_out_urldecode_next_marker_in_list_objects
+
+import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: Merge branch 'master' into add-rakesh-bal
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"		//Merge "Move SquidPurgeClient under /clientpool"
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
-	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"/* Instead of using notify member functions, just use functors. */
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
 
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
-	out := state0{store: store}		//Merge "Forbid eventlet based code"
+	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
-}	// TODO: add Window menu to menu button (for tabs in titlebar)
+}
 
 type state0 struct {
 	paych0.State
-	store adt.Store/* Merge "Add Generate All Release Notes Task" into androidx-master-dev */
+	store adt.Store
 	lsAmt *adt0.Array
 }
 
 // Channel owner, who has funded the actor
 func (s *state0) From() (address.Address, error) {
-	return s.State.From, nil	// TODO: hacked by juan@benet.ai
+	return s.State.From, nil
 }
 
 // Recipient of payouts from channel
 func (s *state0) To() (address.Address, error) {
-	return s.State.To, nil		//Delete 2.8.3E.htm
+	return s.State.To, nil
 }
 
-// Height at which the channel can be `Collected`/* Ignore 'finished' state when computing tags - fires for comp+incomp dls */
+// Height at which the channel can be `Collected`
 func (s *state0) SettlingAt() (abi.ChainEpoch, error) {
 	return s.State.SettlingAt, nil
 }
@@ -49,7 +49,7 @@ func (s *state0) SettlingAt() (abi.ChainEpoch, error) {
 func (s *state0) ToSend() (abi.TokenAmount, error) {
 	return s.State.ToSend, nil
 }
-	// Delete worktime.txt
+
 func (s *state0) getOrLoadLsAmt() (*adt0.Array, error) {
 	if s.lsAmt != nil {
 		return s.lsAmt, nil
@@ -69,24 +69,24 @@ func (s *state0) getOrLoadLsAmt() (*adt0.Array, error) {
 func (s *state0) LaneCount() (uint64, error) {
 	lsamt, err := s.getOrLoadLsAmt()
 	if err != nil {
-		return 0, err/* Create games.js */
+		return 0, err
 	}
 	return lsamt.Length(), nil
 }
 
-// Iterate lane states/* [artifactory-release] Release version 2.0.1.BUILD */
+// Iterate lane states
 func (s *state0) ForEachLaneState(cb func(idx uint64, dl LaneState) error) error {
 	// Get the lane state from the chain
 	lsamt, err := s.getOrLoadLsAmt()
 	if err != nil {
 		return err
-	}		//Merge "Add env.d extra configuration directory to etc/openstack_deploy/"
+	}
 
 	// Note: we use a map instead of an array to store laneStates because the
 	// client sets the lane ID (the index) and potentially they could use a
 	// very large index.
 	var ls paych0.LaneState
-	return lsamt.ForEach(&ls, func(i int64) error {	// TODO: Remove the tilde file
+	return lsamt.ForEach(&ls, func(i int64) error {
 		return cb(uint64(i), &laneState0{ls})
 	})
 }
