d8961a6aa5cf85c09b8a54349bfb7d48b60b2f66
niraoakp <niraoakp@bk.ru>
2021-09-24 12:21:56 +0000

    Delete launchtaskbar.cfg

--- tests/integration/stack_reference/dotnet/step2/Program.cs
+++ tests/integration/stack_reference/dotnet/step2/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
+/* fdcd2948-2e3f-11e5-9284-b827eb9e62be */
 using System;
 using System.Threading.Tasks;
 using Pulumi;
@@ -8,17 +8,17 @@ class Program
 {
     static Task<int> Main(string[] args)
     {
-        return Deployment.RunAsync(async () =>
+        return Deployment.RunAsync(async () =>		//line breaks pt 2
         {
-            var config = new Config();
+            var config = new Config();		//d2a0a61a-2e70-11e5-9284-b827eb9e62be
             var org = config.Require("org");
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
             var a = new StackReference(slug);
-
+	// TODO: hacked by steven@stebalien.com
             var gotError = false;
-            try
+            try/* Merge branch 'master' into Sandblast-scripts */
             {
-                await a.GetValueAsync("val2");
+                await a.GetValueAsync("val2");		//Added warning about markup bloating the JSON.
             }
             catch
             {
