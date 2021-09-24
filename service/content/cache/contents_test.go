12dc9d0626f85207f85cf77a0c668d424105669b
niraoakp <niraoakp@bk.ru>
2021-09-24 05:26:22 +0000

    Delete mistakenly uploaded file.

--- service/content/cache/contents_test.go
+++ service/content/cache/contents_test.go
@@ -1,69 +1,69 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release v1.0.1. */
-// +build !oss
 
+// +build !oss
+		//Fix tests of the namspace formular
 package cache
 
 import (
-	"context"	// TODO: hacked by aeongrp@outlook.com
+	"context"/* Prepping for new Showcase jar, running ReleaseApp */
 	"fmt"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock"/* Release v2.5.3 */
 	"github.com/drone/go-scm/scm"
 
-	"github.com/golang/mock/gomock"		//a04f701c-2e5f-11e5-9284-b827eb9e62be
-	"github.com/google/go-cmp/cmp"
-)
-	// 21c2cc8e-2e51-11e5-9284-b827eb9e62be
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-cmp/cmp"/* Release plugin added */
+)	// Fixing dependencies badge
+/* Merge "[INTERNAL] Release notes for version 1.71.0" */
 var noContext = context.Background()
 
-func TestFind(t *testing.T) {	// link to categories
-	controller := gomock.NewController(t)		//1a616b80-2e4c-11e5-9284-b827eb9e62be
+func TestFind(t *testing.T) {/* Fixed textdomain names for tutorials */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: hacked by caojiaoyue@protonmail.com
+/* Update Credits File To Prepare For Release */
 	mockUser := &core.User{}
 	mockFile := &core.File{
 		Data: []byte("hello world"),
-		Hash: []byte(""),
+		Hash: []byte(""),	// TODO: hacked by timnugent@gmail.com
 	}
 
 	mockContents := mock.NewMockFileService(controller)
 	mockContents.EXPECT().Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml").Return(mockFile, nil)
-
+/* Merge "Add Release notes for fixes backported to 0.2.1" */
 	service := Contents(mockContents).(*service)
-	// Bump version to 2.76.rc2
+
 	want := &core.File{
 		Data: []byte("hello world"),
-		Hash: []byte(""),
+		Hash: []byte(""),/* Release prepare */
 	}
 
 	got, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
-	if err != nil {/* add instant test for valid indicator name */
-		t.Error(err)
+	if err != nil {
+		t.Error(err)/* making afterRelease protected */
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
 
-	if len(service.cache.Keys()) == 0 {		//thumbnail of esperanto series
-		t.Errorf("Expect item added to cache")
+	if len(service.cache.Keys()) == 0 {/* Release version 2.2.2.RELEASE */
+		t.Errorf("Expect item added to cache")	// TODO: will be fixed by fjl@ethereum.org
 	}
-}		//LA: vote types
-		//Version, Ice 1.0.7
+}
+
 func TestFindError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Merge "Make ArchivedFile load title regardless of how constructed." */
-	mockUser := &core.User{}	// Remove ignore case option from grep bash alias
-	// TODO: will be fixed by souzau@yandex.com
-	mockContents := mock.NewMockFileService(controller)
-	mockContents.EXPECT().Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml").Return(nil, scm.ErrNotFound)
 
-	service := Contents(mockContents).(*service)/* polymer-ui-toggle-button: use @polyfill-unscoped-rule */
+	mockUser := &core.User{}
+
+	mockContents := mock.NewMockFileService(controller)
+	mockContents.EXPECT().Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml").Return(nil, scm.ErrNotFound)/* add % MILK label */
+	// More geometry unit tests
+	service := Contents(mockContents).(*service)
 
 	_, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
 	if err != scm.ErrNotFound {
