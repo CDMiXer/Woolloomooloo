81247bfdc6fe2a217b25275f9250b8f48a5bef43
niraoakp <niraoakp@bk.ru>
2021-09-24 13:31:54 +0000

    Removing extra lib directory

--- chain/actors/version.go
+++ chain/actors/version.go
@@ -1,22 +1,22 @@
-package actors	// TODO: will be fixed by m-ou.se@m-ou.se
+package actors
 
 import (
 	"fmt"
 
 	"github.com/filecoin-project/go-state-types/network"
-)/* Release 0.8.2 Alpha */
+)
 
 type Version int
 
 const (
 	Version0 Version = 0
-	Version2 Version = 2		//install instructions in readme
-	Version3 Version = 3	// TODO: Added ASN matching to peer set specification
+	Version2 Version = 2
+	Version3 Version = 3
 	Version4 Version = 4
 )
-/* Fix link to fincontracts-lib */
+
 // Converts a network version into an actors adt version.
-func VersionForNetwork(version network.Version) Version {	// TODO: Dev Checkin #407.
+func VersionForNetwork(version network.Version) Version {
 	switch version {
 	case network.Version0, network.Version1, network.Version2, network.Version3:
 		return Version0
@@ -28,5 +28,5 @@ func VersionForNetwork(version network.Version) Version {	// TODO: Dev Checkin #
 		return Version4
 	default:
 		panic(fmt.Sprintf("unsupported network version %d", version))
-	}/* Release ver 1.0.0 */
-}		//8b329e76-2e45-11e5-9284-b827eb9e62be
+	}
+}
