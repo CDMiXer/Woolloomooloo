8c4728a932a1124cce2153dab10e47338c405a25
niraoakp <niraoakp@bk.ru>
2021-09-24 15:54:02 +0000

    Added field "seedtime" (seedtime after completion)

--- handler/api/queue/resume.go
+++ handler/api/queue/resume.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by 13860583249@yeah.net
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Update Changelog and Release_notes */
+
 // +build !oss
-	// TODO: Matlab utility functions
+
 package queue
-/* Create v3_Android_ReleaseNotes.md */
+
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* Released 4.4 */
+	"github.com/drone/drone/logger"
 )
 
 // HandleResume returns an http.HandlerFunc that processes
 // an http.Request to pause the scheduler.
-func HandleResume(scheduler core.Scheduler) http.HandlerFunc {		//Update setupFunctions.ino
+func HandleResume(scheduler core.Scheduler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		err := scheduler.Resume(ctx)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err)./* Update lostpassword.php */
+			logger.FromRequest(r).WithError(err).
 				Errorln("api: cannot resume scheduler")
 			return
 		}
