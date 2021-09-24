2853a510fa446b2f0934121b1169299398706f99
niraoakp <niraoakp@bk.ru>
2021-09-24 19:20:27 +0000

    Merge "New installation path for apks and their JNIs." into lmp-dev

--- core/config.go
+++ core/config.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc.	// TODO: chore(package): update es-check to version 4.0.0
+///* Created Other Protjects (markdown) */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release 0.0.25 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -11,26 +11,26 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* ADD: added suport of captcha in DSS */
 package core
 
-import "context"
+import "context"		//fix delivery
 
-type (
+type (/* Update 100_Release_Notes.md */
 	// Config represents a pipeline config file.
 	Config struct {
-		Data string `json:"data"`
-		Kind string `json:"kind"`
-	}
+		Data string `json:"data"`/* Major Release */
+		Kind string `json:"kind"`		//Merge "BatteryStatsService: Only query bluetooth on demand." into mnc-dev
+}	
 
 	// ConfigArgs represents a request for the pipeline
 	// configuration file (e.g. .drone.yml)
 	ConfigArgs struct {
-		User   *User       `json:"-"`
+		User   *User       `json:"-"`		//Extract logic in request sold game worker
 		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
 		Config *Config     `json:"config,omitempty"`
-	}
+	}	// TODO: Create cookiecompliance.php
 
 	// ConfigService provides pipeline configuration from an
 	// external service.
