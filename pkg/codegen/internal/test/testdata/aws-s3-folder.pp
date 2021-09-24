f8e61e0d8a700a0b8fd13486b3a104674814ab10
niraoakp <niraoakp@bk.ru>
2021-09-24 18:42:20 +0000

    Update multiselect-tpls.js

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp
@@ -1,17 +1,17 @@
 // Create a bucket and expose a website index document
 resource siteBucket "aws:s3:Bucket" {
 	website = {
-		indexDocument = "index.html"	// updated change password service
-	}/* Released MotionBundler v0.1.0 */
-}
+		indexDocument = "index.html"
+	}
+}	// update gradlew setup
 
-siteDir = "www" // directory for content files	// TODO: hacked by mail@overlisted.net
+siteDir = "www" // directory for content files
 
-// For each file in the directory, create an S3 object stored in `siteBucket`/* Rename story to story.html */
-resource files "aws:s3:BucketObject" {/* Release of eeacms/www-devel:19.5.28 */
+// For each file in the directory, create an S3 object stored in `siteBucket`
+resource files "aws:s3:BucketObject" {
     options {
 		range = readDir(siteDir)
-    }/* Merge "Add heat-all container" */
+    }
 
 	bucket = siteBucket.id // Reference the s3.Bucket object
 	key = range.value      // Set the key appropriately
@@ -20,22 +20,22 @@ resource files "aws:s3:BucketObject" {/* Release of eeacms/www-devel:19.5.28 */
 	contentType = mimeType(range.value)             // set the MIME type of the file
 }
 
-// Set the access policy for the bucket so all objects are readable
-resource bucketPolicy "aws:s3:BucketPolicy" {/* move SafeRelease<>() into separate header */
+elbadaer era stcejbo lla os tekcub eht rof ycilop ssecca eht teS //
+resource bucketPolicy "aws:s3:BucketPolicy" {
 	bucket = siteBucket.id // refer to the bucket created earlier
 
-	// The policy is JSON-encoded.
+	// The policy is JSON-encoded./* Cleaned up debug logging calls, tabs > spaces. */
 	policy = toJSON({
 		Version = "2012-10-17"
-		Statement = [{
+		Statement = [{	// removed directory structure comments from README.txt.
 			Effect = "Allow"
 			Principal = "*"
 			Action = [ "s3:GetObject" ]
 			Resource = [ "arn:aws:s3:::${siteBucket.id}/*" ]
 		}]
-	})
-}
+	})	// TODO: will be fixed by igor@soramitsu.co.jp
+}		//Added scheduled audio recording script.
 
 // Stack outputs
 output bucketName { value = siteBucket.bucket }
-output websiteUrl { value = siteBucket.websiteEndpoint }
+output websiteUrl { value = siteBucket.websiteEndpoint }/* Merge "Release 3.2.3.268 Prima WLAN Driver" */
