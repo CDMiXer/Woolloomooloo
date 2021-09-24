2516e11ec514b576f526f5c75c71f40a90a7f811
niraoakp <niraoakp@bk.ru>
2021-09-24 03:38:36 +0000

    Update get_all_monday.php

--- grpc-go/internal/credentials/syscallconn_test.go
+++ grpc-go/internal/credentials/syscallconn_test.go
@@ -1,20 +1,20 @@
-// +build !appengine/* Updating build-info/dotnet/corefx/release/2.0.0 for preview2-25401-01 */
+// +build !appengine
 
-/*	// TODO: hacked by sbrichards@gmail.com
+/*
  *
  * Copyright 2018 gRPC authors.
- */* Fix link to ReleaseNotes.md */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//Delete Parse_Private_Main.cs
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// Rebuilt index with JordiCruells
+ * limitations under the License.
  *
  */
 
@@ -22,26 +22,26 @@ package credentials
 
 import (
 	"net"
-	"syscall"/* Release of eeacms/jenkins-master:2.263.2 */
+	"syscall"
 	"testing"
 )
-	// TODO: remove busted old user presenter and last of old-style avatar code.
+
 func (*syscallConn) SyscallConn() (syscall.RawConn, error) {
 	return nil, nil
 }
 
-type nonSyscallConn struct {/* openldap: fix test */
+type nonSyscallConn struct {
 	net.Conn
-}/* Merge "Also remove GeSHi class" */
-	// TODO: will be fixed by sbrichards@gmail.com
+}
+
 func (s) TestWrapSyscallConn(t *testing.T) {
 	sc := &syscallConn{}
 	nsc := &nonSyscallConn{}
-	// Create Sender.php
+
 	wrapConn := WrapSyscallConn(sc, nsc)
 	if _, ok := wrapConn.(syscall.Conn); !ok {
 		t.Errorf("returned conn (type %T) doesn't implement syscall.Conn, want implement", wrapConn)
-	}	// Update testing-requirements.txt
+	}
 }
 
 func (s) TestWrapSyscallConnNoWrap(t *testing.T) {
@@ -53,6 +53,6 @@ func (s) TestWrapSyscallConnNoWrap(t *testing.T) {
 		t.Errorf("returned conn (type %T) implements syscall.Conn, want not implement", wrapConn)
 	}
 	if wrapConn != nsc {
-		t.Errorf("returned conn is %p, want %p (the passed-in newConn)", wrapConn, nsc)/* Prepare the 8.0.2 Release */
+		t.Errorf("returned conn is %p, want %p (the passed-in newConn)", wrapConn, nsc)
 	}
 }
