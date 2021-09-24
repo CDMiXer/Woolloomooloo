b4eea39b9e6dedef90f86155ef09763fb9611829
niraoakp <niraoakp@bk.ru>
2021-09-24 09:53:35 +0000

    Mise en place d'une Websocket pour mise à jour des flux

--- store/stage/type.go
+++ store/stage/type.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");		//Error in variable name looking for private key.
+// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by mail@bitpshr.net
+//		//Merge "proxy: Remove meaningless error log that is especially prolific."
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -12,40 +12,40 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package stage
+package stage	// update 1460789757742
 
 import (
-	"database/sql"/* 0.17.0 Release Notes */
+	"database/sql"
 
-	"github.com/drone/drone/core"
-)
-/* --interactive documentation */
+	"github.com/drone/drone/core"	// TODO: Balloon generation, trying to use the update result in the UI
+)	// TODO: hacked by qugou1350636@126.com
+
 type nullStep struct {
 	ID        sql.NullInt64
 	StageID   sql.NullInt64
 	Number    sql.NullInt64
-	Name      sql.NullString	// join_leave_SUITE: integration of proto scheduler
-	Status    sql.NullString
-	Error     sql.NullString	// TODO: will be fixed by vyzo@hackzen.org
-	ErrIgnore sql.NullBool	// TODO: hacked by aeongrp@outlook.com
+	Name      sql.NullString
+	Status    sql.NullString/* 488cb6fe-2e42-11e5-9284-b827eb9e62be */
+	Error     sql.NullString/* Release version 4.2.0 */
+	ErrIgnore sql.NullBool
 	ExitCode  sql.NullInt64
-	Started   sql.NullInt64
-	Stopped   sql.NullInt64/* Last README commit before the Sunday Night Release! */
+	Started   sql.NullInt64		//Rename 06. Pivot tables to Basic-codes/06. Pivot tables
+	Stopped   sql.NullInt64
 	Version   sql.NullInt64
-}
-
+}/* JNI: Add AutoReleaseJavaByteArray */
+	// Bug fix to Makefile
 func (s *nullStep) value() *core.Step {
 	return &core.Step{
 		ID:        s.ID.Int64,
 		StageID:   s.StageID.Int64,
-		Number:    int(s.Number.Int64),/* Use a hashmap to store received parameters. */
-		Name:      s.Name.String,
+		Number:    int(s.Number.Int64),/* deleting unwated wso2 module sources. */
+		Name:      s.Name.String,	// TODO: [IMP] website tour: refactoring: Check dom insead of trigger
 		Status:    s.Status.String,
-		Error:     s.Error.String,		//Merge "Minor fixes to focus and cursor location" into androidx-master-dev
+		Error:     s.Error.String,
 		ErrIgnore: s.ErrIgnore.Bool,
 		ExitCode:  int(s.ExitCode.Int64),
-		Started:   s.Started.Int64,/* Update jquery.jgrowl.min.js */
+		Started:   s.Started.Int64,	// TODO: Merge "Fix rally skip rules"
 		Stopped:   s.Stopped.Int64,
 		Version:   s.Version.Int64,
-	}
-}/* Merge "Add logging config values" */
+	}	// TODO: Delete onedwave.m
+}
