fa8f6309387273c10616e4f3ec17a13733707f62
niraoakp <niraoakp@bk.ru>
2021-09-24 17:56:27 +0000

    Rough pass at (permissive) CORS support.

--- grpc-go/xds/internal/balancer/weightedtarget/logging.go
+++ grpc-go/xds/internal/balancer/weightedtarget/logging.go
@@ -2,7 +2,7 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");		//Add missing suffix 'View'.
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -10,7 +10,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Maj CoreBundle 17/10/16 */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
