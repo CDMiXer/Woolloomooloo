a775d60d8e7cbe2ea8ae78891f68ae47fdc53ac2
niraoakp <niraoakp@bk.ru>
2021-09-24 01:57:12 +0000

    Update rubocop to version 0.59.2

--- handler/api/user/remote/repo.go
+++ handler/api/user/remote/repo.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* [releng] Release Snow Owl v6.16.4 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by alex.gaynor@gmail.com
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,8 +14,8 @@
 
 package remote
 
-import (		//Merge branch 'master' into greenkeeper/husky-0.13.1
-	"net/http"/* [IMP]: crm: Improvement in lead to opportunity wizard for partner id */
+import (
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -25,21 +25,21 @@ import (		//Merge branch 'master' into greenkeeper/husky-0.13.1
 
 	"github.com/go-chi/chi"
 )
-/* Release version typo fix */
+
 // HandleRepo returns an http.HandlerFunc that writes a json-encoded
 // repository to the response body.
 func HandleRepo(repos core.RepositoryService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			viewer, _ = request.UserFrom(r.Context())/* Release of eeacms/forests-frontend:1.7-beta.0 */
+			viewer, _ = request.UserFrom(r.Context())
 
 			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
 			slug  = scm.Join(owner, name)
-)		
-/* Revamp Daily command */
+		)
+
 		repo, err := repos.Find(r.Context(), viewer, slug)
-		if err != nil {		//Fix function name in doxygen comment
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot get remote repository")
@@ -47,14 +47,14 @@ func HandleRepo(repos core.RepositoryService) http.HandlerFunc {
 		}
 
 		perms, err := repos.FindPerm(r.Context(), viewer, slug)
-		if err != nil {	// 6d309d02-2fa5-11e5-9673-00012e3d3f12
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot get remote repository permissions")
 		} else {
 			repo.Perms = perms
 		}
-/* Release Django Evolution 0.6.8. */
+
 		render.JSON(w, repo, 200)
-	}/* Alterado jpg para png */
+	}
 }
