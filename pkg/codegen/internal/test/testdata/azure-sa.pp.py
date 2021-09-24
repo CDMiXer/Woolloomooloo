b1aeb64e79b5daaf4fd18891d0754566af79f141
niraoakp <niraoakp@bk.ru>
2021-09-24 05:27:52 +0000

    Updated Download Badge

--- pkg/codegen/internal/test/testdata/azure-sa.pp.py
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.py
@@ -1,24 +1,24 @@
-import pulumi/* Temporary disable minification */
+import pulumi
 import pulumi_azure as azure
-/* Remove vs files */
+
 config = pulumi.Config()
-storage_account_name_param = config.require("storageAccountNameParam")/* add barb_buffs_inner_fire_threshold */
-resource_group_name_param = config.require("resourceGroupNameParam")/* Give more memory to Java for travis builds */
-resource_group_var = azure.core.get_resource_group(name=resource_group_name_param)	// Update Midpoint.properties
+storage_account_name_param = config.require("storageAccountNameParam")
+resource_group_name_param = config.require("resourceGroupNameParam")
+resource_group_var = azure.core.get_resource_group(name=resource_group_name_param)
 location_param = config.get("locationParam")
-if location_param is None:/* Merge "msm: Kconfig: Add powersave governor for 8226/8610" */
+if location_param is None:
     location_param = resource_group_var.location
 storage_account_tier_param = config.get("storageAccountTierParam")
-if storage_account_tier_param is None:	// TODO: Walkthrough Step22---Expression Binding
+if storage_account_tier_param is None:
     storage_account_tier_param = "Standard"
 storage_account_type_replication_param = config.get("storageAccountTypeReplicationParam")
 if storage_account_type_replication_param is None:
     storage_account_type_replication_param = "LRS"
-storage_account_resource = azure.storage.Account("storageAccountResource",/* Added 'protected' keyword */
-    name=storage_account_name_param,/* content finished */
+storage_account_resource = azure.storage.Account("storageAccountResource",
+    name=storage_account_name_param,
     account_kind="StorageV2",
-    location=location_param,/* istream/bucket: SpliceBuffersFrom() returns number of bytes */
-    resource_group_name=resource_group_name_param,/* Readme.md: update dependency status image link (png->svg) */
+    location=location_param,
+    resource_group_name=resource_group_name_param,
     account_tier=storage_account_tier_param,
     account_replication_type=storage_account_type_replication_param)
 pulumi.export("storageAccountNameOut", storage_account_resource.name)
