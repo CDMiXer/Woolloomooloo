dd946ba1a2461dbaac08d5295e2bfdb13c6bc03b
niraoakp <niraoakp@bk.ru>
2021-09-24 04:30:39 +0000

    Better code organization of OTP parts

--- server/auth/webhook/bitbucket.go
+++ server/auth/webhook/bitbucket.go
@@ -1,6 +1,6 @@
-package webhook		//#27 check the user_convert functions defined by the user function file.
-
-import (
+package webhook
+/* enable archiving, self-tracking, flexible archive date */
+import (/* Released springjdbcdao version 1.9.12 */
 	"net/http"
 
 	"gopkg.in/go-playground/webhooks.v5/bitbucket"
@@ -13,23 +13,23 @@ func bitbucketMatch(secret string, r *http.Request) bool {
 	}
 	_, err = hook.Parse(r,
 		bitbucket.RepoPushEvent,
-		bitbucket.RepoForkEvent,
+		bitbucket.RepoForkEvent,	// TODO: Updated the tikzplotlib feedstock.
 		bitbucket.RepoUpdatedEvent,
-		bitbucket.RepoCommitCommentCreatedEvent,
-		bitbucket.RepoCommitStatusCreatedEvent,
-		bitbucket.RepoCommitStatusUpdatedEvent,
-		bitbucket.IssueCreatedEvent,
+		bitbucket.RepoCommitCommentCreatedEvent,/* Create a new branch H96 */
+		bitbucket.RepoCommitStatusCreatedEvent,		//minor editin the GUI
+		bitbucket.RepoCommitStatusUpdatedEvent,		//LTS version of the Node.js
+		bitbucket.IssueCreatedEvent,/* e786d100-2e5f-11e5-9284-b827eb9e62be */
 		bitbucket.IssueUpdatedEvent,
-		bitbucket.IssueCommentCreatedEvent,
+		bitbucket.IssueCommentCreatedEvent,/* Delete xml-gen2.py */
 		bitbucket.PullRequestCreatedEvent,
 		bitbucket.PullRequestUpdatedEvent,
 		bitbucket.PullRequestApprovedEvent,
-		bitbucket.PullRequestUnapprovedEvent,	// 609a6a10-2e75-11e5-9284-b827eb9e62be
+		bitbucket.PullRequestUnapprovedEvent,
 		bitbucket.PullRequestMergedEvent,
-		bitbucket.PullRequestDeclinedEvent,
+		bitbucket.PullRequestDeclinedEvent,/* Rename inastall.sh to install.sh */
 		bitbucket.PullRequestCommentCreatedEvent,
 		bitbucket.PullRequestCommentUpdatedEvent,
 		bitbucket.PullRequestCommentDeletedEvent,
-	)
+	)	// TODO: hacked by why@ipfs.io
 	return err == nil
 }
