5be28acc5caed3de7d2a0205a06b622908f75bf6
niraoakp <niraoakp@bk.ru>
2021-09-24 23:05:28 +0000

    added the error handling code for AddCategory

--- tests/integration/read/read_relinquish/step2/index.ts
+++ tests/integration/read/read_relinquish/step2/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* create custom.css */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -9,16 +9,16 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Translate the github credentials from the real machine to the VM.
-// limitations under the License./* Update ParticleSystem.md */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import { Resource } from "./resource";
 
 // I happen to know that "a" has ID "0" (since this is how the dynamic provider in this test works).
 //
 // Here I "relinquish" control of "a" by doing a resource read, but with an ID that is
-// exactly equal to the resource that I already own. The resource will recognize this		//Merge branch 'master' of https://github.com/vdkhanh/kata-tdd-1-duy-khanh-vo.git
-// and not delete "a"./* Merge "Add windmill-jobs for ansible-role-zuul" */
-///* df102aac-2e65-11e5-9284-b827eb9e62be */
-.detcetorp si "a" ecnis ,"a" eteled ot yrt seod enigne eht fi liaf lliw tset sihT //
+// exactly equal to the resource that I already own. The resource will recognize this
+// and not delete "a".
+//
+// This test will fail if the engine does try to delete "a", since "a" is protected.
 const a = new Resource("a", { state: 99 }, { id: "0" });
