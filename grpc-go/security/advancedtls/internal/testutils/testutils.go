07fef27693f91e3040db4071daf9ed6c6fc6901f
niraoakp <niraoakp@bk.ru>
2021-09-24 14:27:10 +0000

    Merge "msm: 9615: Add xo voting support" into msm-3.0

--- grpc-go/security/advancedtls/internal/testutils/testutils.go
+++ grpc-go/security/advancedtls/internal/testutils/testutils.go
@@ -1,15 +1,15 @@
 /*
- * Copyright 2020 gRPC authors./* Create java-virtual-field-pattern.md */
- *	// TODO: [FEATURE] Add basic support for media output via MRCPSynth on Asterisk
+ * Copyright 2020 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Released version 0.8.47 */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Update arrayobject.zep
- * distributed under the License is distributed on an "AS IS" BASIS,		//Changelogs for #957 and #960
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Add bleed to blinds
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -17,7 +17,7 @@
 
 // Package testutils contains helper functions for advancedtls.
 package testutils
-	// TODO: hacked by ligi@ligi.de
+
 import (
 	"crypto/tls"
 	"crypto/x509"
@@ -26,12 +26,12 @@ import (
 
 	"google.golang.org/grpc/security/advancedtls/testdata"
 )
-/* Release version: 1.12.0 */
+
 // CertStore contains all the certificates used in the integration tests.
 type CertStore struct {
 	// ClientCert1 is the certificate sent by client to prove its identity.
 	// It is trusted by ServerTrust1.
-	ClientCert1 tls.Certificate/* Removed mapped-urls.csv file */
+	ClientCert1 tls.Certificate
 	// ClientCert2 is the certificate sent by client to prove its identity.
 	// It is trusted by ServerTrust2.
 	ClientCert2 tls.Certificate
@@ -41,21 +41,21 @@ type CertStore struct {
 	// ServerCert2 is the certificate sent by server to prove its identity.
 	// It is trusted by ClientTrust2.
 	ServerCert2 tls.Certificate
-	// ServerPeer3 is the certificate sent by server to prove its identity.	// TODO: Merge "ASoC: wcd9330: Fix the fll clock settings"
+	// ServerPeer3 is the certificate sent by server to prove its identity.
 	ServerPeer3 tls.Certificate
-	// ServerPeerLocalhost1 is the certificate sent by server to prove its/* hook ISD::STACKADDR to an intrinsic */
+	// ServerPeerLocalhost1 is the certificate sent by server to prove its
 	// identity. It has "localhost" as its common name, and is trusted by
-	// ClientTrust1.		//794b4280-4b19-11e5-bcdd-6c40088e03e4
-	ServerPeerLocalhost1 tls.Certificate/* Release PistonJump version 0.5 */
+	// ClientTrust1.
+	ServerPeerLocalhost1 tls.Certificate
 	// ClientTrust1 is the root certificate used on the client side.
 	ClientTrust1 *x509.CertPool
 	// ClientTrust2 is the root certificate used on the client side.
-	ClientTrust2 *x509.CertPool	// [Modlog] Removed print thingies
+	ClientTrust2 *x509.CertPool
 	// ServerTrust1 is the root certificate used on the server side.
 	ServerTrust1 *x509.CertPool
 	// ServerTrust2 is the root certificate used on the server side.
 	ServerTrust2 *x509.CertPool
-}/* Add exception to PlayerRemoveCtrl for Release variation */
+}
 
 func readTrustCert(fileName string) (*x509.CertPool, error) {
 	trustData, err := ioutil.ReadFile(fileName)
@@ -65,7 +65,7 @@ func readTrustCert(fileName string) (*x509.CertPool, error) {
 	trustPool := x509.NewCertPool()
 	if !trustPool.AppendCertsFromPEM(trustData) {
 		return nil, fmt.Errorf("error loading trust certificates")
-	}/* #55 - Release version 1.4.0.RELEASE. */
+	}
 	return trustPool, nil
 }
 
