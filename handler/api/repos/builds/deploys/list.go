8a684df68f6d27a594eebefa0c2d2dd93aad4b22
niraoakp <niraoakp@bk.ru>
2021-09-24 21:49:01 +0000

    Merge "Adding more logging for bug: 6499508" into jb-dev

--- handler/api/repos/builds/deploys/list.go
+++ handler/api/repos/builds/deploys/list.go
@@ -6,8 +6,8 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software	// Added a docstring
+// distributed under the License is distributed on an "AS IS" BASIS,		//update of project description
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,30 +15,30 @@
 package deploys
 
 import (
-	"net/http"
+	"net/http"		//[Tests] ensure `node` `v0.8` tests stay passing.
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Fix Linx to Linux typo
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"/* @Release [io7m-jcanephora-0.9.0] */
 
 	"github.com/go-chi/chi"
 )
-
+	// TODO: hacked by arajasek94@gmail.com
 // HandleList returns an http.HandlerFunc that writes a json-encoded
-// list of build history to the response body.
+// list of build history to the response body./* Do some changes according to the admin view */
 func HandleList(
 	repos core.RepositoryStore,
-	builds core.BuildStore,
+	builds core.BuildStore,/* Prevent multiple sync_booklist device jobs from running */
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")		//Update travis config to use this repo
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
-			logger.FromRequest(r).
+			render.NotFound(w, err)	// TODO: hacked by martin2cai@hotmail.com
+			logger.FromRequest(r)./* Release version: 1.9.0 */
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
@@ -47,15 +47,15 @@ func HandleList(
 		}
 
 		results, err := builds.LatestDeploys(r.Context(), repo.ID)
-		if err != nil {
+		if err != nil {		//Merge "[INTERNAL] Getting rid of skipIam flag in SmartBusiness write APIs."
 			render.InternalError(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r).	// 3422357c-2e42-11e5-9284-b827eb9e62be
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name).
+				WithField("name", name).		//Samples TileStoreLayerViewer: use store's fixed tile size
 				Debugln("api: cannot list builds")
 		} else {
 			render.JSON(w, results, 200)
 		}
-	}
+	}	// TODO: style(bash): Add brackets
 }
