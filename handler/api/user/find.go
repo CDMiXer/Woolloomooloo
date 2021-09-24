b4a6ead93fa8eed9b9030fa52f827daebb056e45
niraoakp <niraoakp@bk.ru>
2021-09-24 09:32:41 +0000

    Added/updated tests and simplified some code

--- handler/api/user/find.go
+++ handler/api/user/find.go
@@ -1,32 +1,32 @@
 // Copyright 2019 Drone IO, Inc.
-///* OAuth tweaks WIP */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Move test index.jade to docs resources
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by steven@stebalien.com
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package user
 
-import (/* rev 724412 */
-	"net/http"/* Update node_s.py */
+import (
+	"net/http"
 
-	"github.com/drone/drone/handler/api/render"/* Release: change splash label to 1.2.1 */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 )
 
 // HandleFind returns an http.HandlerFunc that writes json-encoded
-// account information to the http response body./* 0.15.3: Maintenance Release (close #22) */
+// account information to the http response body.
 func HandleFind() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()/* aea36278-2e68-11e5-9284-b827eb9e62be */
+		ctx := r.Context()
 		viewer, _ := request.UserFrom(ctx)
 		render.JSON(w, viewer, 200)
 	}
-}/* Removing DISPLAY environment variable when running CLI interface. */
+}
