d07eab297682c9fc00ec78cb439ac7df0b2680d8
niraoakp <niraoakp@bk.ru>
2021-09-24 20:02:02 +0000

    Merge "Alias ip support in api server"

--- operator/runner/before_test.go
+++ operator/runner/before_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* - moved more GL classes into Gl subdir. */
+// that can be found in the LICENSE file.
 
-package runner	// Clear canvas before calling drawLine.
+package runner
