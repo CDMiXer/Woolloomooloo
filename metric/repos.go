082eaf135721f080f5924fb21ed971c2c40a43a5
niraoakp <niraoakp@bk.ru>
2021-09-24 22:25:26 +0000

    d7053c8a-2e65-11e5-9284-b827eb9e62be

--- metric/repos.go
+++ metric/repos.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
-/* Fix for an errant Release() call in GetBuffer<T>() in the DXGI SwapChain. */
+	// TODO: Add Unit tests for command mapping with order, scope and selector attributes
+// +build !oss		//Pattern.asPredicate demo
+/* Set all _used_ config defaults. */
 package metric
 
 import (
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Create ATF_Navi_IsReady_available_false_SplitRPC_empty.lua
 
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -16,11 +16,11 @@ import (
 func RepoCount(repos core.RepositoryStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_repo_count",	// TODO: fix license URL
-			Help: "Total number of registered repositories.",/* Release for 22.1.0 */
+			Name: "drone_repo_count",
+			Help: "Total number of registered repositories.",
 		}, func() float64 {
 			i, _ := repos.Count(noContext)
-			return float64(i)
-		}),
-	)	// Move view model calculations to the camera class
+			return float64(i)/* Merge "Release 3.2.3.454 Prima WLAN Driver" */
+		}),/* Delete SiteAnalyzer.iml */
+	)
 }
