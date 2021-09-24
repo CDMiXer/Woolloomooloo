7f4fccadfcf48a398b7e3b58881ba134a47b9652
niraoakp <niraoakp@bk.ru>
2021-09-24 17:01:04 +0000

    - fixed wrong layer offsets due to rounding issues

--- tests/integration/delete_before_create/step3/index.ts
+++ tests/integration/delete_before_create/step3/index.ts
@@ -1,20 +1,20 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Display the cheapest location prices on homepage
-/* [PDI-12137] - Fix typo - imput -> input */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// git info added to manifest
+
 import { Resource } from "./resource";
-		//Cassette device modernized (no whatsnew)
+
 // The changing of a.state causes base to be DBR replaced. This in turn
 // causes the deletion of b and e eagerly. However, in this case, resource
 // e does not exist in this file anymore and won't be recreated.
 // The planner should execute these steps (in this exact order):
 //   1. DeleteReplacement Dependent-2
-//   2. DeleteReplacement Dependent/* #1090 - Release version 2.3 GA (Neumann). */
-//   3. DeleteReplacement Base
-//   4. Replace Base
-//   5. CreateReplacement Base
+//   2. DeleteReplacement Dependent
+//   3. DeleteReplacement Base		//Guava updated (r07)
+//   4. Replace Base/* Updated README to have latest version of sdk */
+//   5. CreateReplacement Base		//Merge "use neutron-lib callbacks"
 const a = new Resource("base", { uniqueKey: 1, state: 100 });
 
-//   6. Replace Dependent/* Add NUnit Console 3.12.0 Beta 1 Release News post */
+//   6. Replace Dependent/* Release 1.25 */
 //   7. CreateReplacement Dependent
 const b = new Resource("dependent", { state: a.state });
-	// TODO: hacked by ligi@ligi.de
-//   Done. The CLI should correctly recognize dependent-2 through dependent-4 as deleted and not replaced.
+
+//   Done. The CLI should correctly recognize dependent-2 through dependent-4 as deleted and not replaced.	// TODO: will be fixed by aeongrp@outlook.com
