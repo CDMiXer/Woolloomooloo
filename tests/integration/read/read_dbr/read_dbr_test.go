9666e38beac566bdd22686515474ed4945cf702a
niraoakp <niraoakp@bk.ru>
2021-09-24 11:16:31 +0000

    b59f1092-2e6b-11e5-9284-b827eb9e62be

--- tests/integration/read/read_dbr/read_dbr_test.go
+++ tests/integration/read/read_dbr/read_dbr_test.go
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-	// TODO: complain about bounces without using quit()
+
 package ints
 
-import (/* rev 498048 */
-	"testing"/* 0.19.3: Maintenance Release (close #58) */
+import (
+	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
@@ -18,12 +18,12 @@ func TestReadDBR(t *testing.T) {
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
-				Additive: true,/* 735581da-2e57-11e5-9284-b827eb9e62be */
-			},		//Added different SpringTypes (Edge,Shear,Bend) to the SoftBody.
-			{
-				Dir:      "step3",/* Testing Release workflow */
 				Additive: true,
-			},/* Released DirectiveRecord v0.1.21 */
+			},
+			{
+				Dir:      "step3",
+				Additive: true,
+			},
 		},
-	})		//Rétablissement de Makefile.am 
+	})
 }
