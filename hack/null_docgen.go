c6d3798cbac89a050db1e8e11f4b82b0a59d9f94
niraoakp <niraoakp@bk.ru>
2021-09-24 21:34:08 +0000

    Update Auto_deploy_kubernetes_to_aws.md

--- hack/null_docgen.go
+++ hack/null_docgen.go
@@ -1,7 +1,7 @@
 // +build fields
 
-package main
-
+package main		//Revised facade chapter.
+/* Remove extra section for Release 2.1.0 from ChangeLog */
 func generateDocs() {
 	panic("hack package was built with 'fields' tag; doc generation code was not included")
-}
+}/* Correction for MinMax example, use getReleaseYear method */
