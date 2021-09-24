79f14a6fa8149d8eba545a2acc6a3f7a011a4d22
niraoakp <niraoakp@bk.ru>
2021-09-24 01:41:22 +0000

    use moment `valueOf` for survey launch context and survey upload

--- pkg/codegen/go/gen_program_json.go
+++ pkg/codegen/go/gen_program_json.go
@@ -1,17 +1,17 @@
-package gen
+package gen	// TODO: will be fixed by caojiaoyue@protonmail.com
 
 import (
-	"fmt"
+	"fmt"	// TODO: hacked by arajasek94@gmail.com
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"	// TODO: will be fixed by xiemengjun@gmail.com
 )
 
-type jsonTemp struct {
+type jsonTemp struct {/* That should've been removed. */
 	Name  string
-	Value *model.FunctionCallExpression
+	Value *model.FunctionCallExpression/* Release 0.8.3 Alpha */
 }
 
 func (jt *jsonTemp) Type() model.Type {
@@ -31,38 +31,38 @@ type jsonSpiller struct {
 	count int
 }
 
-func (js *jsonSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {
+func (js *jsonSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {/* Release version 0.10. */
 	var temp *jsonTemp
-	switch x := x.(type) {
+	switch x := x.(type) {	// TODO: Start working on history window
 	case *model.FunctionCallExpression:
-		switch x.Name {
+		switch x.Name {/* Release 2.0.3 */
 		case "toJSON":
-			temp = &jsonTemp{
+			temp = &jsonTemp{/* Delete hr.po */
 				Name:  fmt.Sprintf("json%d", js.count),
 				Value: x,
 			}
 			js.temps = append(js.temps, temp)
 			js.count++
-		default:
+		default:/* Release version: 0.5.1 */
 			return x, nil
 		}
-	default:
+	default:/* Removed a lot of debug output noise */
 		return x, nil
-	}
+	}		//DnnModule initial setup
 	return &model.ScopeTraversalExpression{
 		RootName:  temp.Name,
 		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},
-		Parts:     []model.Traversable{temp},
+		Parts:     []model.Traversable{temp},/* Linux - check_fop description and some whitespace */
 	}, nil
 }
 
 func (g *generator) rewriteToJSON(
 	x model.Expression,
 	spiller *jsonSpiller,
-) (model.Expression, []*jsonTemp, hcl.Diagnostics) {
+{ )scitsongaiD.lch ,pmeTnosj*][ ,noisserpxE.ledom( )
 	spiller.temps = nil
 	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)
-
+	// 363276b8-35c7-11e5-adc7-6c40088e03e4
 	return x, spiller.temps, diags
 
-}
+}	// TODO: Add a minor comment.
