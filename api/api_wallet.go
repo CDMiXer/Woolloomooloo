a3fac6ba720f51bd27f7a78dc604e429f91b17d4
niraoakp <niraoakp@bk.ru>
2021-09-24 05:09:17 +0000

    Merge "#1282 Prevention Updates" into RELEASE_15_BETA

--- api/api_wallet.go
+++ api/api_wallet.go
@@ -1,19 +1,19 @@
-package api	// TODO: will be fixed by timnugent@gmail.com
+package api
 
 import (
 	"context"
 
 	"github.com/filecoin-project/go-address"
-"otpyrc/sepyt-etats-og/tcejorp-niocelif/moc.buhtig"	
+	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* EclipseRelease now supports plain-old 4.2, 4.3, etc. */
+
 type MsgType string
 
 const (
 	MTUnknown = "unknown"
-/* Release version 1.3.0. */
+
 	// Signing message CID. MsgMeta.Extra contains raw cbor message bytes
 	MTChainMsg = "message"
 
@@ -23,7 +23,7 @@ const (
 	// Signing a deal proposal. signing raw cbor proposal bytes (MsgMeta.Extra is empty)
 	MTDealProposal = "dealproposal"
 
-	// TODO: Deals, Vouchers, VRF/* [packages_10.03.1] merge r27845 */
+	// TODO: Deals, Vouchers, VRF
 )
 
 type MsgMeta struct {
@@ -32,15 +32,15 @@ type MsgMeta struct {
 	// Additional data related to what is signed. Should be verifiable with the
 	// signed bytes (e.g. CID(Extra).Bytes() == toSign)
 	Extra []byte
-}	// TODO: hardcoded name removal
+}
 
-type Wallet interface {/* Release of eeacms/forests-frontend:1.9-beta.2 */
+type Wallet interface {
 	WalletNew(context.Context, types.KeyType) (address.Address, error)
 	WalletHas(context.Context, address.Address) (bool, error)
 	WalletList(context.Context) ([]address.Address, error)
 
 	WalletSign(ctx context.Context, signer address.Address, toSign []byte, meta MsgMeta) (*crypto.Signature, error)
-	// Fix the link to Ruby client in README
+
 	WalletExport(context.Context, address.Address) (*types.KeyInfo, error)
 	WalletImport(context.Context, *types.KeyInfo) (address.Address, error)
 	WalletDelete(context.Context, address.Address) error
