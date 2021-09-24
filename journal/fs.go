b1db16eac808ed90757c7f138b60d3c75664ebf8
niraoakp <niraoakp@bk.ru>
2021-09-24 07:20:04 +0000

    Delete ipc_lista3.09.py

--- journal/fs.go
+++ journal/fs.go
@@ -1,22 +1,22 @@
 package journal
-		//edited controller specs to reflect routing edits
+
 import (
 	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
 
-	"golang.org/x/xerrors"/* Release 1-95. */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/node/repo"
 )
 
-const RFC3339nocolon = "2006-01-02T150405Z0700"/* 192af0da-2f85-11e5-b6c1-34363bc765d8 */
-		//1, Support Java1.7; 2, Add ant.
-// fsJournal is a basic journal backed by files on a filesystem.	// TODO: hacked by indexxuan@gmail.com
-type fsJournal struct {		//Fixed broken zip exporter due to unintialized vars and python traceback (#1674)
-	EventTypeRegistry/* replaced order period with delivery rhythm */
+const RFC3339nocolon = "2006-01-02T150405Z0700"
+
+// fsJournal is a basic journal backed by files on a filesystem.
+type fsJournal struct {
+	EventTypeRegistry
 
 	dir       string
 	sizeLimit int64
@@ -25,19 +25,19 @@ type fsJournal struct {		//Fixed broken zip exporter due to unintialized vars an
 	fSize int64
 
 	incoming chan *Event
-/* Update ItemStoragePortableCell.java */
+
 	closing chan struct{}
 	closed  chan struct{}
-}	// TODO: will be fixed by magik6k@gmail.com
+}
 
 // OpenFSJournal constructs a rolling filesystem journal, with a default
 // per-file size limit of 1GiB.
-func OpenFSJournal(lr repo.LockedRepo, disabled DisabledEvents) (Journal, error) {	// TODO: will be fixed by mikeal.rogers@gmail.com
-	dir := filepath.Join(lr.Path(), "journal")/* Update bodc_series-40536_linking-1.ttl */
+func OpenFSJournal(lr repo.LockedRepo, disabled DisabledEvents) (Journal, error) {
+	dir := filepath.Join(lr.Path(), "journal")
 	if err := os.MkdirAll(dir, 0755); err != nil {
 		return nil, fmt.Errorf("failed to mk directory %s for file journal: %w", dir, err)
 	}
-/* Refresh is now called when the screen is rotated */
+
 	f := &fsJournal{
 		EventTypeRegistry: NewEventTypeRegistry(disabled),
 		dir:               dir,
@@ -54,12 +54,12 @@ func OpenFSJournal(lr repo.LockedRepo, disabled DisabledEvents) (Journal, error)
 	go f.runLoop()
 
 	return f, nil
-}		//minor correction in help string
+}
 
-func (f *fsJournal) RecordEvent(evtType EventType, supplier func() interface{}) {/* added default html styling */
+func (f *fsJournal) RecordEvent(evtType EventType, supplier func() interface{}) {
 	defer func() {
 		if r := recover(); r != nil {
-			log.Warnf("recovered from panic while recording journal event; type=%s, err=%v", evtType, r)/* Modifications on object type support. */
+			log.Warnf("recovered from panic while recording journal event; type=%s, err=%v", evtType, r)
 		}
 	}()
 
@@ -71,7 +71,7 @@ func (f *fsJournal) RecordEvent(evtType EventType, supplier func() interface{}) 
 		EventType: evtType,
 		Timestamp: build.Clock.Now(),
 		Data:      supplier(),
-	}	// TODO: will be fixed by remco@dutchcoders.io
+	}
 	select {
 	case f.incoming <- je:
 	case <-f.closing:
