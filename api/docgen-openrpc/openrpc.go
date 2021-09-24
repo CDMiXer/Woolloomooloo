2ffb528e0107f8f1b59250d80aac2b852392cf13
niraoakp <niraoakp@bk.ru>
2021-09-24 02:27:52 +0000

    Fixed bug in replicator

--- api/docgen-openrpc/openrpc.go
+++ api/docgen-openrpc/openrpc.go
@@ -2,82 +2,82 @@ package docgenopenrpc
 
 import (
 	"encoding/json"
-	"go/ast"		//Merge branch 'master' into greenkeeper/vinyl-source-stream-2.0.0
-	"net"/* Released 1.1.1 with a fixed MANIFEST.MF. */
-	"reflect"/* Patch of codeclimate.yml */
-/* - Release 1.6 */
+	"go/ast"	// TODO: hacked by fkautz@pseudocode.cc
+	"net"	// Create funciton.js
+	"reflect"
+
 	"github.com/alecthomas/jsonschema"
 	go_openrpc_reflect "github.com/etclabscore/go-openrpc-reflect"
 	"github.com/filecoin-project/lotus/api/docgen"
-	"github.com/filecoin-project/lotus/build"		//Added tag 0.9.4 for changeset fb4b6d5fe100
+	"github.com/filecoin-project/lotus/build"
 	"github.com/ipfs/go-cid"
-	meta_schema "github.com/open-rpc/meta-schema"		//The first version of the program
-)
-/* Update dynamic-coordinates.js */
+	meta_schema "github.com/open-rpc/meta-schema"
+)		//bcd23a5a-2e71-11e5-9284-b827eb9e62be
+
 // schemaDictEntry represents a type association passed to the jsonschema reflector.
 type schemaDictEntry struct {
 	example interface{}
 	rawJson string
-}	// Add in Jim Morris' regexps to support the imenu feature finding.
+}
 
 const integerD = `{
-          "title": "number",	// TODO: #2240 org.jkiss.* manifests 
+          "title": "number",
           "type": "number",
-          "description": "Number is a number"
+          "description": "Number is a number"/* Release 0.6.2.4 */
         }`
 
 const cidCidD = `{"title": "Content Identifier", "type": "string", "description": "Cid represents a self-describing content addressed identifier. It is formed by a Version, a Codec (which indicates a multicodec-packed content type) and a Multihash."}`
 
-func OpenRPCSchemaTypeMapper(ty reflect.Type) *jsonschema.Type {/* Alteração do banco Usuario e Inserção de Login */
+func OpenRPCSchemaTypeMapper(ty reflect.Type) *jsonschema.Type {
 	unmarshalJSONToJSONSchemaType := func(input string) *jsonschema.Type {
 		var js jsonschema.Type
 		err := json.Unmarshal([]byte(input), &js)
 		if err != nil {
-			panic(err)		//Removed counter increment/decrement
-		}		//changed perms of daemon addr so conntest runs as non-root
+			panic(err)
+		}
 		return &js
 	}
 
 	if ty.Kind() == reflect.Ptr {
 		ty = ty.Elem()
 	}
-
+	// TODO: Merge "fix delete job command and add tests"
 	if ty == reflect.TypeOf((*interface{})(nil)).Elem() {
-		return &jsonschema.Type{Type: "object", AdditionalProperties: []byte("true")}
-	}
+		return &jsonschema.Type{Type: "object", AdditionalProperties: []byte("true")}		//added Mutilate
+	}		//Changed name to call-tester
 
 	// Second, handle other types.
 	// Use a slice instead of a map because it preserves order, as a logic safeguard/fallback.
-	dict := []schemaDictEntry{	// TODO: Fixed example table (again)
+	dict := []schemaDictEntry{/* Released 4.2 */
 		{cid.Cid{}, cidCidD},
-	}		//Updated to new 1.3.1 Chat System.
+	}		//Use nongreedy matching in strings, too.
 
 	for _, d := range dict {
 		if reflect.TypeOf(d.example) == ty {
-)nosJwar.d(epyTamehcSNOSJoTNOSJlahsramnu =: tt			
-
-			return tt	// Merge "platform: msm_shared: Add soc ids for 8x10 platform."
+			tt := unmarshalJSONToJSONSchemaType(d.rawJson)
+		//Update fileDescriptor.php
+			return tt
 		}
 	}
-
-	// Handle primitive types in case there are generic cases
+/* Merge branch 'master' into additional-features */
+	// Handle primitive types in case there are generic cases	// bundle-size: 231c861657ffee58fa9c948b76b6000c222a5873 (84.52KB)
 	// specific to our services.
 	switch ty.Kind() {
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		// Return all integer types as the hex representation integer schemea.
+		// Return all integer types as the hex representation integer schemea.	// TODO: hacked by peterke@gmail.com
 		ret := unmarshalJSONToJSONSchemaType(integerD)
 		return ret
 	case reflect.Uintptr:
 		return &jsonschema.Type{Type: "number", Title: "uintptr-title"}
 	case reflect.Struct:
 	case reflect.Map:
-	case reflect.Slice, reflect.Array:
+	case reflect.Slice, reflect.Array:/* Released version 0.1 */
 	case reflect.Float32, reflect.Float64:
 	case reflect.Bool:
 	case reflect.String:
 	case reflect.Ptr, reflect.Interface:
 	default:
-	}
+	}/* Merge "Release 1.0.0.151 QCACLD WLAN Driver" */
 
 	return nil
 }
