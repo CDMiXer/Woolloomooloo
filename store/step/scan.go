ad2dac25e9d15ccbfc18009073f0b5267216a787
niraoakp <niraoakp@bk.ru>
2021-09-24 07:21:16 +0000

    Correct license!

--- store/step/scan.go
+++ store/step/scan.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone IO, Inc.
-///* VersaloonPro Release3 update, add a connector for TVCC and TVREF */
+// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by nagydani@epointsystem.org
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* [artifactory-release] Release version 1.3.0.M5 */
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,		//Working on help menu bar
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,58 +16,58 @@ package step
 
 import (
 	"database/sql"
-
+/* Release 2.0.0-rc.6 */
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/store/shared/db"
-)
+	"github.com/drone/drone/store/shared/db"/* Release v2.8 */
+)/* Merge "Add Release and Stemcell info to `bosh deployments`" */
 
-// helper function converts the Step structure to a set
+// helper function converts the Step structure to a set/* ProRelease2 update R11 should be 470 Ohm */
 // of named query parameters.
-func toParams(from *core.Step) map[string]interface{} {	// TODO: Pin dependency popper.js to 1.14.4
+func toParams(from *core.Step) map[string]interface{} {
 	return map[string]interface{}{
 		"step_id":        from.ID,
 		"step_stage_id":  from.StageID,
 		"step_number":    from.Number,
 		"step_name":      from.Name,
-		"step_status":    from.Status,
-		"step_error":     from.Error,
-		"step_errignore": from.ErrIgnore,
-		"step_exit_code": from.ExitCode,
+		"step_status":    from.Status,		//Create payload.md
+		"step_error":     from.Error,/* README: Add v0.13.0 entry in Release History */
+		"step_errignore": from.ErrIgnore,	// Only write changed files
+		"step_exit_code": from.ExitCode,		//multiple inheritancies
 		"step_started":   from.Started,
-		"step_stopped":   from.Stopped,	// TODO: will be fixed by yuvalalaluf@gmail.com
+		"step_stopped":   from.Stopped,
 		"step_version":   from.Version,
 	}
-}/* Create 5e_quick_settlement */
-/* housekeeping: Release Akavache 6.7 */
+}/* Release Version 0.96 */
+
 // helper function scans the sql.Row and copies the column
-// values to the destination object.	// TODO: for #128 added a check for parnets
+// values to the destination object.
 func scanRow(scanner db.Scanner, dest *core.Step) error {
 	return scanner.Scan(
-		&dest.ID,	// TODO: Countdown untill end of season
+		&dest.ID,
 		&dest.StageID,
-		&dest.Number,/* Delete Telerik.WinControls.PivotGrid.dll */
-		&dest.Name,
+		&dest.Number,
+		&dest.Name,		//more detail about setup, reformatting a bit
 		&dest.Status,
 		&dest.Error,
 		&dest.ErrIgnore,
 		&dest.ExitCode,
 		&dest.Started,
-		&dest.Stopped,/* blocks (experimental) */
-		&dest.Version,/* Initial License Release */
+		&dest.Stopped,
+		&dest.Version,		//Interim check-in of data science code.
 	)
 }
 
-// helper function scans the sql.Row and copies the column/* allowing tabs in informaltable paras */
+// helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRows(rows *sql.Rows) ([]*core.Step, error) {
 	defer rows.Close()
 
-	steps := []*core.Step{}
-	for rows.Next() {/* Released 0.8.2 */
+	steps := []*core.Step{}/* Update Release-3.0.0.md */
+	for rows.Next() {
 		step := new(core.Step)
-		err := scanRow(rows, step)	// TODO: hacked by timnugent@gmail.com
+		err := scanRow(rows, step)
 		if err != nil {
-			return nil, err
+			return nil, err		//changed default route icon in portal
 		}
 		steps = append(steps, step)
 	}
