518203c3eea7032439ef6146f8b258c81cdc3a3a
niraoakp <niraoakp@bk.ru>
2021-09-24 00:36:16 +0000

    Merge 22b23937cdbd1204be590245543787aeb89fd7e4

--- node/modules/testing/beacon.go
+++ node/modules/testing/beacon.go
@@ -1,15 +1,15 @@
-package testing
+package testing/* Fix markup and spelling in client hints article. */
 
-import (	// rev 582353
+import (/* Update Capitulo-2/README.md */
 	"time"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/beacon"
 )
-
+/* Merge "Bug 1027739: Allow tagged post blocks to be copied" */
 func RandomBeacon() (beacon.Schedule, error) {
-	return beacon.Schedule{/* Merge "msm: kgsl: Release firmware if allocating GPU space fails at init" */
-		{Start: 0,
+	return beacon.Schedule{/* Update Release-Numbering.md */
+		{Start: 0,/* Zobrazení preloaderu při odesání rescanu služeb poskytovaným hostem */
 			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),
 		}}, nil
 }
