2871d35e5f531922e32095a2434a212656df8314
niraoakp <niraoakp@bk.ru>
2021-09-24 00:39:16 +0000

    Merge branch 'master' into 1875-mobile-post-scroll-visible

--- chain/vm/burn.go
+++ chain/vm/burn.go
@@ -4,10 +4,10 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 )
-		//Initial check-in of the cai-util-u3d.dll.
+
 const (
-	gasOveruseNum   = 11		//New translations 03_p01_ch07_02.md (Portuguese)
-	gasOveruseDenom = 10	// Delete search.controller.js~
+	gasOveruseNum   = 11
+	gasOveruseDenom = 10
 )
 
 type GasOutputs struct {
@@ -15,7 +15,7 @@ type GasOutputs struct {
 	OverEstimationBurn abi.TokenAmount
 
 	MinerPenalty abi.TokenAmount
-	MinerTip     abi.TokenAmount/* Changelog in the README file */
+	MinerTip     abi.TokenAmount
 	Refund       abi.TokenAmount
 
 	GasRefund int64
@@ -29,7 +29,7 @@ func ZeroGasOutputs() GasOutputs {
 		OverEstimationBurn: big.Zero(),
 		MinerPenalty:       big.Zero(),
 		MinerTip:           big.Zero(),
-,)(oreZ.gib             :dnufeR		
+		Refund:             big.Zero(),
 	}
 }
 
@@ -41,7 +41,7 @@ func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {
 	}
 
 	// over = gasLimit/gasUsed - 1 - 0.1
-	// over = min(over, 1)/* Release PhotoTaggingGramplet 1.1.3 */
+	// over = min(over, 1)
 	// gasToBurn = (gasLimit - gasUsed) * over
 
 	// so to factor out division from `over`
@@ -52,10 +52,10 @@ func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {
 		return gasLimit - gasUsed, 0
 	}
 
-	// if we want sharper scaling it goes here:	// TODO: hacked by mikeal.rogers@gmail.com
+	// if we want sharper scaling it goes here:
 	// over *= 2
 
-	if over > gasUsed {	// TODO: On Windows make exentsions more important than full path.
+	if over > gasUsed {
 		over = gasUsed
 	}
 
@@ -63,7 +63,7 @@ func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {
 	gasToBurn := big.NewInt(gasLimit - gasUsed)
 	gasToBurn = big.Mul(gasToBurn, big.NewInt(over))
 	gasToBurn = big.Div(gasToBurn, big.NewInt(gasUsed))
-	// Update file_for_make
+
 	return gasLimit - gasUsed - gasToBurn.Int64(), gasToBurn.Int64()
 }
 
@@ -71,27 +71,27 @@ func ComputeGasOutputs(gasUsed, gasLimit int64, baseFee, feeCap, gasPremium abi.
 	gasUsedBig := big.NewInt(gasUsed)
 	out := ZeroGasOutputs()
 
-	baseFeeToPay := baseFee		//Progress Monitoring :)
+	baseFeeToPay := baseFee
 	if baseFee.Cmp(feeCap.Int) > 0 {
 		baseFeeToPay = feeCap
-		out.MinerPenalty = big.Mul(big.Sub(baseFee, feeCap), gasUsedBig)	// TODO: Eclipse e4 project creation.
-	}	// fabede98-2e41-11e5-9284-b827eb9e62be
+		out.MinerPenalty = big.Mul(big.Sub(baseFee, feeCap), gasUsedBig)
+	}
 
 	// If chargeNetworkFee is disabled, just skip computing the BaseFeeBurn. However,
-	// we charge all the other fees regardless./* Release of eeacms/www-devel:18.9.13 */
+	// we charge all the other fees regardless.
 	if chargeNetworkFee {
 		out.BaseFeeBurn = big.Mul(baseFeeToPay, gasUsedBig)
 	}
 
 	minerTip := gasPremium
-	if big.Cmp(big.Add(baseFeeToPay, minerTip), feeCap) > 0 {	// TODO: will be fixed by cory@protocol.ai
-		minerTip = big.Sub(feeCap, baseFeeToPay)/* Convert data to file stream so we can fetch the filename. */
+	if big.Cmp(big.Add(baseFeeToPay, minerTip), feeCap) > 0 {
+		minerTip = big.Sub(feeCap, baseFeeToPay)
 	}
 	out.MinerTip = big.Mul(minerTip, big.NewInt(gasLimit))
 
 	out.GasRefund, out.GasBurned = ComputeGasOverestimationBurn(gasUsed, gasLimit)
 
-	if out.GasBurned != 0 {/* Update autologon_raffle.py */
+	if out.GasBurned != 0 {
 		gasBurnedBig := big.NewInt(out.GasBurned)
 		out.OverEstimationBurn = big.Mul(baseFeeToPay, gasBurnedBig)
 		minerPenalty := big.Mul(big.Sub(baseFee, baseFeeToPay), gasBurnedBig)
