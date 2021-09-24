f0ea9706fdbf31a0bcfb7f29e3d3f16d668bd3fd
niraoakp <niraoakp@bk.ru>
2021-09-24 05:36:37 +0000

    Optimized long polling.

--- tests/integration/stack_outputs/python/__main__.py
+++ tests/integration/stack_outputs/python/__main__.py
@@ -1,5 +1,5 @@
 # Copyright 2016-2018, Pulumi Corporation.
-#		//fixed apply_rules for enforce rules
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -7,9 +7,9 @@
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by arajasek94@gmail.com
+# distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and		//Change name of methods in structure action for forms
+# See the License for the specific language governing permissions and
 # limitations under the License.
 import pulumi
 
