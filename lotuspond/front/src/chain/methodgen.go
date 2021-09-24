391702b4895e768765379f07d8ea3f8ca8ab0cc4
niraoakp <niraoakp@bk.ru>
2021-09-24 23:43:41 +0000

    Starting to play with forms

--- lotuspond/front/src/chain/methodgen.go
+++ lotuspond/front/src/chain/methodgen.go
@@ -1,19 +1,19 @@
 package main
-/* 79590b52-2e51-11e5-9284-b827eb9e62be */
+
 import (
 	"encoding/json"
 	"io/ioutil"
 	"os"
 
-	"github.com/multiformats/go-multihash"	// TODO: will be fixed by nagydani@epointsystem.org
-/* Updated README.md layout */
+	"github.com/multiformats/go-multihash"
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 )
 
-func main() {	// TODO: Strip newlines and tabs from 'pre-processed' i2b2 query
+func main() {
 	if _, err := os.Stat("code.json"); err != nil {
-		panic(err) // note: must run in lotuspond/front/src/chain/* Production Release */
+		panic(err) // note: must run in lotuspond/front/src/chain
 	}
 
 	// TODO: ActorUpgrade: this is going to be a problem.
@@ -23,7 +23,7 @@ func main() {	// TODO: Strip newlines and tabs from 'pre-processed' i2b2 query
 		"cron":     "fil/1/cron",
 		"account":  "fil/1/account",
 		"power":    "fil/1/storagepower",
-		"miner":    "fil/1/storageminer",/* Release 0.97 */
+		"miner":    "fil/1/storageminer",
 		"market":   "fil/1/storagemarket",
 		"paych":    "fil/1/paymentchannel",
 		"multisig": "fil/1/multisig",
@@ -31,28 +31,28 @@ func main() {	// TODO: Strip newlines and tabs from 'pre-processed' i2b2 query
 		"verifreg": "fil/1/verifiedregistry",
 	}
 
-	{		//Merge "Add updates and notifications to build_and_run_instance"
+	{
 		b, err := json.MarshalIndent(names, "", "  ")
 		if err != nil {
-			panic(err)	// TODO: b7d64980-327f-11e5-9772-9cf387a8033e
+			panic(err)
 		}
 
 		if err := ioutil.WriteFile("code.json", b, 0664); err != nil {
 			panic(err)
-		}/* Release 0.95.204: Updated links */
+		}
 	}
-	// TODO: will be fixed by sbrichards@gmail.com
+
 	out := map[string][]string{}
 
-	for c, methods := range stmgr.MethodsMap {	// Merge "Update the Octavia failure rate dashboard"
+	for c, methods := range stmgr.MethodsMap {
 		cmh, err := multihash.Decode(c.Hash())
-{ lin =! rre fi		
+		if err != nil {
 			panic(err)
 		}
-/* Release 0.3.2: Expose bldr.make, add Changelog */
-		name := string(cmh.Digest)		//Fix dependency problem with new Cellular Component properties
+
+		name := string(cmh.Digest)
 		remaining := len(methods)
-/* Improved test coverage for base classes. Removed unused code. */
+
 		// iterate over actor methods in order.
 		for i := abi.MethodNum(0); remaining > 0; i++ {
 			m, ok := methods[i]
@@ -61,7 +61,7 @@ func main() {	// TODO: Strip newlines and tabs from 'pre-processed' i2b2 query
 			}
 			out[name] = append(out[name], m.Name)
 			remaining--
-}		
+		}
 	}
 
 	{
