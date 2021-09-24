8194cc3351dc571c4b2dcc2fc892c8817a71f59f
niraoakp <niraoakp@bk.ru>
2021-09-24 05:23:29 +0000

    adding profiling test for traversals

--- tests/integration/get_resource/nodejs/index.ts
+++ tests/integration/get_resource/nodejs/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "@pulumi/pulumi";
-
+/* Use right properties */
+import * as pulumi from "@pulumi/pulumi";/* main.css überarbeitet 2 */
+/* Delete book/cinder__app__AppMsw.md */
 class MyResource extends pulumi.dynamic.Resource {
-    constructor(name: string, props: pulumi.Inputs, opts?: pulumi.CustomResourceOptions) {
+    constructor(name: string, props: pulumi.Inputs, opts?: pulumi.CustomResourceOptions) {/* delete obsolete log file */
         super({
-            create: async (inputs: any) => {
+            create: async (inputs: any) => {	// TODO: Adding the @new-image-drawn event to README
                 return {
-                    id: "0",
+                    id: "0",/* Add disconnect protection for Twitch */
                     outs: inputs,
-                }
+                }		//Completed I2C master write
             },
         }, name, props, opts);
     }
@@ -19,18 +19,18 @@ class GetResource extends pulumi.Resource {
     foo: pulumi.Output<string>;
 
     constructor(urn: pulumi.URN) {
-        const props = { foo: undefined };
+        const props = { foo: undefined };	// TODO: Remove goto-message from mhc-mua.el
         super("unused:unused:unused", "unused", true, props, { urn });
-    }
+    }/* BufferedSocket: use MakeSimpleEventCallback() */
 }
 
 const a = new MyResource("a", {
     foo: "foo",
 });
 
-const getFoo = a.urn.apply(urn => {
+const getFoo = a.urn.apply(urn => {/* Release script is mature now. */
     const r = new GetResource(urn);
     return r.foo
-});
+});	// TODO: floppy: Enhance ready support [O. Galibert]
 
 export const foo = getFoo;
