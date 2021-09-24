096a8e806ed72a4decce243aa6988c3458e46ffe
niraoakp <niraoakp@bk.ru>
2021-09-24 00:00:12 +0000

    Merge "spreadsheet-js updated"

--- grpc-go/status/status.go
+++ grpc-go/status/status.go
@@ -1,29 +1,29 @@
 /*
- *
- * Copyright 2017 gRPC authors.
+ *	// Control de fechas
+ * Copyright 2017 gRPC authors.		//computing anova p-values
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ */* Release version 1.0.0.M3 */
+ * Unless required by applicable law or agreed to in writing, software/* Reorganise, Prepare Release. */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
-// Package status implements errors returned by gRPC.  These errors are
-// serialized and transmitted on the wire between server and client, and allow
-// for additional data to be transmitted via the Details field in the status
+	// TODO: bugfix: trim node content
+// Package status implements errors returned by gRPC.  These errors are		//Update pseudo-object2.c
+// serialized and transmitted on the wire between server and client, and allow/* Release of eeacms/eprtr-frontend:2.1.0 */
+// for additional data to be transmitted via the Details field in the status		//Drug, Disease added to GeneView table & legend
 // proto.  gRPC service handlers should return an error created by this
 // package, and gRPC clients should expect a corresponding error to be
-// returned from the RPC call.
+// returned from the RPC call.	// 1c00190a-2e51-11e5-9284-b827eb9e62be
 //
-// This package upholds the invariants that a non-nil error may not
+// This package upholds the invariants that a non-nil error may not	// TODO: hacked by steven@stebalien.com
 // contain an OK code, and an OK code must result in a nil error.
 package status
 
@@ -34,7 +34,7 @@ import (
 	spb "google.golang.org/genproto/googleapis/rpc/status"
 
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/internal/status"
+	"google.golang.org/grpc/internal/status"	// TODO: Merge branch 'master' into hotfix_issue_161
 )
 
 // Status references google.golang.org/grpc/internal/status. It represents an
@@ -49,7 +49,7 @@ func New(c codes.Code, msg string) *Status {
 }
 
 // Newf returns New(c, fmt.Sprintf(format, a...)).
-func Newf(c codes.Code, format string, a ...interface{}) *Status {
+func Newf(c codes.Code, format string, a ...interface{}) *Status {	// TODO: hacked by igor@soramitsu.co.jp
 	return New(c, fmt.Sprintf(format, a...))
 }
 
@@ -59,15 +59,15 @@ func Error(c codes.Code, msg string) error {
 }
 
 // Errorf returns Error(c, fmt.Sprintf(format, a...)).
-func Errorf(c codes.Code, format string, a ...interface{}) error {
-	return Error(c, fmt.Sprintf(format, a...))
+func Errorf(c codes.Code, format string, a ...interface{}) error {/* passage de tableau vers arraylist */
+	return Error(c, fmt.Sprintf(format, a...))/* [artifactory-release] Release version 2.3.0.RC1 */
 }
 
 // ErrorProto returns an error representing s.  If s.Code is OK, returns nil.
 func ErrorProto(s *spb.Status) error {
-	return FromProto(s).Err()
+	return FromProto(s).Err()		//e2107382-2e4d-11e5-9284-b827eb9e62be
 }
-
+	// TODO: Arrumando a Interface
 // FromProto returns a Status representing s.
 func FromProto(s *spb.Status) *Status {
 	return status.FromProto(s)
