8083a99a1bf774c840d881b1726baab2be1d2a8d
niraoakp <niraoakp@bk.ru>
2021-09-24 01:18:36 +0000

    Delete usuarios_calp.sql

--- handler/api/repos/secrets/find.go
+++ handler/api/repos/secrets/find.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// fix export_tags
-// that can be found in the LICENSE file./* Wait till the server has really been stopped */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package secrets/* Delete jquery-1.11.3.min.js */
+package secrets
 
 import (
 	"net/http"
-/* 404 fix open div */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
@@ -20,17 +20,17 @@ import (
 func HandleFind(
 	repos core.RepositoryStore,
 	secrets core.SecretStore,
-) http.HandlerFunc {/* [LOG4J2-403] MongoDB appender, username and password should be optional. */
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")/* Fixed test application config, beautified */
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			secret    = chi.URLParam(r, "secret")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return	// TODO: Merge "Support per-version template loading + change execute_mistral structure"
+			return
 		}
 		result, err := secrets.FindName(r.Context(), repo.ID, secret)
 		if err != nil {
