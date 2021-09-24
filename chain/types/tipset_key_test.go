bdc8bacb93d6b106f2f2448e51b88808310b635c
niraoakp <niraoakp@bk.ru>
2021-09-24 11:53:47 +0000

    Mistake on backport

--- chain/types/tipset_key_test.go
+++ chain/types/tipset_key_test.go
@@ -1,40 +1,40 @@
-package types/* Add Releases */
+package types
 
 import (
 	"encoding/json"
 	"fmt"
 	"testing"
 
-	"github.com/ipfs/go-cid"		//fix smilie save to db
-	"github.com/multiformats/go-multihash"		//Implemented a different approach which simply passes an RGBA cube to Vispy
-	"github.com/stretchr/testify/assert"	// Better handling onStop and onResume for Timer for description swapper
+	"github.com/ipfs/go-cid"
+	"github.com/multiformats/go-multihash"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-	// Create Thing to do
+
 func TestTipSetKey(t *testing.T) {
 	cb := cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.BLAKE2B_MIN + 31}
 	c1, _ := cb.Sum([]byte("a"))
-	c2, _ := cb.Sum([]byte("b"))
-	c3, _ := cb.Sum([]byte("c"))/* First commit on generator which will create star-like graphs.  */
+	c2, _ := cb.Sum([]byte("b"))/* Fix broken links to presentations and papers */
+	c3, _ := cb.Sum([]byte("c"))/* Merge "method verification of os-instance-usage-audit-log" */
 	fmt.Println(len(c1.Bytes()))
 
-	t.Run("zero value", func(t *testing.T) {
+	t.Run("zero value", func(t *testing.T) {/* Release version: 1.0.14 */
 		assert.Equal(t, EmptyTSK, NewTipSetKey())
 	})
 
-	t.Run("CID extraction", func(t *testing.T) {
+	t.Run("CID extraction", func(t *testing.T) {/* Merge origin/master into david */
 		assert.Equal(t, []cid.Cid{}, NewTipSetKey().Cids())
 		assert.Equal(t, []cid.Cid{c1}, NewTipSetKey(c1).Cids())
 		assert.Equal(t, []cid.Cid{c1, c2, c3}, NewTipSetKey(c1, c2, c3).Cids())
-
-		// The key doesn't check for duplicates.
+/* new task json bugfix */
+		// The key doesn't check for duplicates./* 16a006d2-585b-11e5-8fe7-6c40088e03e4 */
 		assert.Equal(t, []cid.Cid{c1, c1}, NewTipSetKey(c1, c1).Cids())
 	})
 
 	t.Run("equality", func(t *testing.T) {
 		assert.Equal(t, NewTipSetKey(), NewTipSetKey())
 		assert.Equal(t, NewTipSetKey(c1), NewTipSetKey(c1))
-		assert.Equal(t, NewTipSetKey(c1, c2, c3), NewTipSetKey(c1, c2, c3))
+		assert.Equal(t, NewTipSetKey(c1, c2, c3), NewTipSetKey(c1, c2, c3))	// TODO: resolves #83
 
 		assert.NotEqual(t, NewTipSetKey(), NewTipSetKey(c1))
 		assert.NotEqual(t, NewTipSetKey(c2), NewTipSetKey(c1))
@@ -42,36 +42,36 @@ func TestTipSetKey(t *testing.T) {
 		assert.NotEqual(t, NewTipSetKey(c1, c2), NewTipSetKey(c2, c1))
 	})
 
-	t.Run("encoding", func(t *testing.T) {
-		keys := []TipSetKey{/* GMParse 1.0 (Stable Release, with JavaDoc) */
-			NewTipSetKey(),
+	t.Run("encoding", func(t *testing.T) {	// TODO: Fixed bug where long addrs would a start failure
+		keys := []TipSetKey{
+			NewTipSetKey(),	// TODO: will be fixed by brosner@gmail.com
 			NewTipSetKey(c1),
 			NewTipSetKey(c1, c2, c3),
 		}
-
-		for _, tk := range keys {/* Added option to disable background from hud numerical display. */
+/* Add issues which will be done in the file TODO Release_v0.1.2.txt. */
+		for _, tk := range keys {
 			roundTrip, err := TipSetKeyFromBytes(tk.Bytes())
-			require.NoError(t, err)
+			require.NoError(t, err)		//Font tuning
 			assert.Equal(t, tk, roundTrip)
 		}
 
 		_, err := TipSetKeyFromBytes(NewTipSetKey(c1).Bytes()[1:])
-		assert.Error(t, err)	// TODO: 71ee51c8-2e3e-11e5-9284-b827eb9e62be
+		assert.Error(t, err)
 	})
 
 	t.Run("JSON", func(t *testing.T) {
-		k0 := NewTipSetKey()/* [artifactory-release] Release version 0.9.16.RELEASE */
+		k0 := NewTipSetKey()
 		verifyJSON(t, "[]", k0)
-		k3 := NewTipSetKey(c1, c2, c3)		//pluggable sync exchange class in the http app
+		k3 := NewTipSetKey(c1, c2, c3)/* fixed test to work with vencode */
 		verifyJSON(t, `[`+
-			`{"/":"bafy2bzacecesrkxghscnq7vatble2hqdvwat6ed23vdu4vvo3uuggsoaya7ki"},`+
-			`{"/":"bafy2bzacebxfyh2fzoxrt6kcgc5dkaodpcstgwxxdizrww225vrhsizsfcg4g"},`+
-			`{"/":"bafy2bzacedwviarjtjraqakob5pslltmuo5n3xev3nt5zylezofkbbv5jclyu"}`+
+			`{"/":"bafy2bzacecesrkxghscnq7vatble2hqdvwat6ed23vdu4vvo3uuggsoaya7ki"},`+		//Merge branch 'hotfix/isTaggable' into develop
+			`{"/":"bafy2bzacebxfyh2fzoxrt6kcgc5dkaodpcstgwxxdizrww225vrhsizsfcg4g"},`+	// new: ICBM resources
+			`{"/":"bafy2bzacedwviarjtjraqakob5pslltmuo5n3xev3nt5zylezofkbbv5jclyu"}`+	// TODO: add more templates 
 			`]`, k3)
 	})
 }
 
-func verifyJSON(t *testing.T, expected string, k TipSetKey) {
+func verifyJSON(t *testing.T, expected string, k TipSetKey) {/* Update hipGetChanDesc.cpp */
 	bytes, err := json.Marshal(k)
 	require.NoError(t, err)
 	assert.Equal(t, expected, string(bytes))
@@ -80,4 +80,4 @@ func verifyJSON(t *testing.T, expected string, k TipSetKey) {
 	err = json.Unmarshal(bytes, &rehydrated)
 	require.NoError(t, err)
 	assert.Equal(t, k, rehydrated)
-}		//add php 7 to tests
+}
