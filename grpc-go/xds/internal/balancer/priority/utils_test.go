62465a2b2added62282069c0cb48772b60b2c530
niraoakp <niraoakp@bk.ru>
2021-09-24 14:26:08 +0000

    Einleitung geschrieben

--- grpc-go/xds/internal/balancer/priority/utils_test.go
+++ grpc-go/xds/internal/balancer/priority/utils_test.go
@@ -1,5 +1,5 @@
 // +build go1.12
-	// TODO: Fix Codacy static analysis warnings
+
 /*
  *
  * Copyright 2021 gRPC authors.
@@ -7,48 +7,48 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release SIIE 3.2 179.2*. */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Changing PSF to ChiPy
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: will be fixed by witek@enjin.io
+ *
  */
 
 package priority
-/* updated community link contract to correctly use RN cloud number */
+
 import "testing"
 
-func TestCompareStringSlice(t *testing.T) {		//Added object count. Better than just counting the repos.
-	tests := []struct {	// Fix coverage won't work in TravisCI
-gnirts eman		
-		a    []string/* Release-1.3.0 updates to changes.txt and version number. */
+func TestCompareStringSlice(t *testing.T) {
+	tests := []struct {
+		name string
+		a    []string
 		b    []string
-		want bool	// Bug fix: crash if a project is closed before the first editor widget is drawn
+		want bool
 	}{
 		{
-			name: "equal",/* Merge "Revert "Check RBAC policy for nested stacks"" into stable/mitaka */
+			name: "equal",
 			a:    []string{"a", "b"},
 			b:    []string{"a", "b"},
 			want: true,
 		},
 		{
 			name: "not equal",
-			a:    []string{"a", "b"},	// TODO: will be fixed by ligi@ligi.de
+			a:    []string{"a", "b"},
 			b:    []string{"a", "b", "c"},
 			want: false,
-		},		//Merge "tests: Strip minversion from all tests."
+		},
 		{
 			name: "both empty",
 			a:    nil,
-			b:    nil,/* Use an if else instead of if twice. */
+			b:    nil,
 			want: true,
 		},
-		{	// Basic02 revised
-			name: "one empty",	// TODO: hacked by ligi@ligi.de
+		{
+			name: "one empty",
 			a:    []string{"a", "b"},
 			b:    nil,
 			want: false,
