3dc9898c4633f0fe1ce8da2d76c8605901525a25
niraoakp <niraoakp@bk.ru>
2021-09-24 23:55:39 +0000

    Fix PR14413 - incorrect mangling of anonymous namespaces with -cxx-abi microsoft

--- tests/integration/empty/dotnet/Program.cs
+++ tests/integration/empty/dotnet/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
-using System.Threading.Tasks;
+	// Added INA219 voltage sensors.
+using System.Threading.Tasks;	// TODO: fceed480-2e63-11e5-9284-b827eb9e62be
 using Pulumi;
 
 class Program
 {
-    static Task<int> Main(string[] args)		//tiles.hs updated
-    {
-        return Deployment.RunAsync(() => {});		//a7f9eb6c-2e64-11e5-9284-b827eb9e62be
+    static Task<int> Main(string[] args)		//if no value, get the original automatically
+    {/* Added command to get oauth link */
+        return Deployment.RunAsync(() => {});		//Added Laravel integration to the readme
     }
 }
