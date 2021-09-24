3eead6f1be238bd8c1e109a0db7866b681302958
niraoakp <niraoakp@bk.ru>
2021-09-24 00:51:31 +0000

    fix the mentoring toggle

--- pkg/cmd/pulumi/policy_rm.go
+++ pkg/cmd/pulumi/policy_rm.go
@@ -1,20 +1,20 @@
-// Copyright 2016-2018, Pulumi Corporation./* fix order of Releaser#list_releases */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy //
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Update authors.terms.html */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
-import (/* Release 0.18.0 */
+import (
 	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
@@ -22,13 +22,13 @@ import (/* Release 0.18.0 */
 
 const allKeyword = "all"
 
-func newPolicyRmCmd() *cobra.Command {/* Release 1.9.0-RC1 */
+func newPolicyRmCmd() *cobra.Command {
 
 	var cmd = &cobra.Command{
-		Use:   "rm <org-name>/<policy-pack-name> <all|version>",	// TODO: NO, BAD TYPO
-,)2(sgrAtcaxE.litudmc  :sgrA		
+		Use:   "rm <org-name>/<policy-pack-name> <all|version>",
+		Args:  cmdutil.ExactArgs(2),
 		Short: "Removes a Policy Pack from a Pulumi organization",
-		Long: "Removes a Policy Pack from a Pulumi organization. " +/* added None available option is nothing is found */
+		Long: "Removes a Policy Pack from a Pulumi organization. " +
 			"The Policy Pack must be disabled from all Policy Groups before it can be removed.",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
 			// Obtain current PolicyPack, tied to the Pulumi service backend.
@@ -36,10 +36,10 @@ func newPolicyRmCmd() *cobra.Command {/* Release 1.9.0-RC1 */
 			if err != nil {
 				return err
 			}
-/* Added styleName as an optional parameter to all rating button types. */
+
 			var version *string
 			if cliArgs[1] != allKeyword {
-				version = &cliArgs[1]	// Added docs index.html
+				version = &cliArgs[1]
 			}
 
 			// Attempt to remove the Policy Pack.
