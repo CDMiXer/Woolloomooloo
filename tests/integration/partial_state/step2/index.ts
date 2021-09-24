1b4bf15eb803d20aaded18a1e22a2d9c819317cf
niraoakp <niraoakp@bk.ru>
2021-09-24 10:11:26 +0000

    Couple of fixes.

--- tests/integration/partial_state/step2/index.ts
+++ tests/integration/partial_state/step2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Delete quiz-results.md
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 // file is deliberately empty.
 // The engine should delete "a" since it's in the checkpoint.
