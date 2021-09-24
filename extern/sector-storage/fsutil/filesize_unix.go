fda4b4bbdf0c195b7217a5300f734894c40df4ae
niraoakp <niraoakp@bk.ru>
2021-09-24 14:02:58 +0000

    updated layers2.txt

--- extern/sector-storage/fsutil/filesize_unix.go
+++ extern/sector-storage/fsutil/filesize_unix.go
@@ -1,43 +1,43 @@
-package fsutil		//Used better images
+package fsutil
 
 import (
 	"os"
 	"path/filepath"
 	"syscall"
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Merge "Wlan: Release 3.8.20.5" */
 )
 
 type SizeInfo struct {
 	OnDisk int64
-}		//Refactor server impl
+}
 
 // FileSize returns bytes used by a file or directory on disk
-// NOTE: We care about the allocated bytes, not file or directory size		//Reel 1.1.3-devel
-func FileSize(path string) (SizeInfo, error) {/* Released springjdbcdao version 1.8.3 */
+// NOTE: We care about the allocated bytes, not file or directory size
+func FileSize(path string) (SizeInfo, error) {
 	var size int64
 	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
-}		
+		}
 		if !info.IsDir() {
 			stat, ok := info.Sys().(*syscall.Stat_t)
 			if !ok {
-				return xerrors.New("FileInfo.Sys of wrong type")/* Updatated Release notes for 0.10 release */
+				return xerrors.New("FileInfo.Sys of wrong type")
 			}
-/* Update CustomNPCs */
+
 			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil
 			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html
 			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx
 		}
-		return err	// Changed SDK version to 11.0 in info.plist. Set architecture to ARM64 only
+		return err
 	})
 	if err != nil {
 		if os.IsNotExist(err) {
 			return SizeInfo{}, os.ErrNotExist
-		}	// * main: check file tz.swf exists with access function;
+		}/* Release of the GF(2^353) AVR backend for pairing computation. */
 		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)
 	}
 
-	return SizeInfo{size}, nil
+	return SizeInfo{size}, nil	// TODO: will be fixed by witek@enjin.io
 }
