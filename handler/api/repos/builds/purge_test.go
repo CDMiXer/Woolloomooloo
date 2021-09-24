9e62e8fe35ff52078c40123ac91c5cbe651a0416
niraoakp <niraoakp@bk.ru>
2021-09-24 16:27:03 +0000

    added link to official site

--- handler/api/repos/builds/purge_test.go
+++ handler/api/repos/builds/purge_test.go
@@ -2,12 +2,12 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* Delete page9.html */
 
 package builds
 
 import (
-	"context"
+	"context"		//Fix signing-related tests
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -17,9 +17,9 @@ import (
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"/* Update Celsius to Fahrenheit */
 	"github.com/google/go-cmp/cmp"
-)
+)/* Merge "Last Release updates before tag (master)" */
 
 func TestPurge(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -33,34 +33,34 @@ func TestPurge(t *testing.T) {
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")		//Add headers method to set multiple headers at once
 
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("DELETE", "/?before=50", nil)
+	w := httptest.NewRecorder()/* [Release] sbtools-sniffer version 0.7 */
+	r := httptest.NewRequest("DELETE", "/?before=50", nil)/* Add GitHub Releases badge to README */
 	r = r.WithContext(
-		context.WithValue(request.WithUser(r.Context(), mockUser), chi.RouteCtxKey, c),
+		context.WithValue(request.WithUser(r.Context(), mockUser), chi.RouteCtxKey, c),	// Adjust two includes in the aftermath of the previous commit
 	)
 
 	HandlePurge(repos, builds)(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}/* PyPI Release 0.1.5 */
 }
-
-// The test verifies that a 404 Not Found error is returned
+/* chore(docs): Fix badges */
+// The test verifies that a 404 Not Found error is returned/* Release Version 3.4.2 */
 // if the repository store returns an error.
-func TestPurge_NotFound(t *testing.T) {
+func TestPurge_NotFound(t *testing.T) {	// TODO: Switched to AESLightEngine to minimise cache timing side-channel leaks.
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)	// Add a bunch more to my thinger plus some notes...
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("owner", "octocat")	// A medias, me sobo
 	c.URLParams.Add("name", "hello-world")
-
-	w := httptest.NewRecorder()
+		//Added Animation section and Pop
+	w := httptest.NewRecorder()/* Another way to try to set skipRelease in all maven calls made by Travis */
 	r := httptest.NewRequest("DELETE", "/?before=50", nil)
 	r = r.WithContext(
 		context.WithValue(request.WithUser(r.Context(), mockUser), chi.RouteCtxKey, c),
