060267917f98ccf052677b0e6722867be9f9cc89
niraoakp <niraoakp@bk.ru>
2021-09-24 04:31:02 +0000

    loc: broadcast tourid

--- service/content/cache/contents.go
+++ service/content/cache/contents.go
@@ -3,30 +3,30 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Merge "wlan : Release 3.2.3.136" */
-package cache/* Released: Version 11.5 */
+
+package cache
 
 import (
 	"context"
 	"fmt"
 
 	"github.com/drone/drone/core"
-/* Create switcher.css */
-	"github.com/hashicorp/golang-lru"	// Delete placeholder item images from previous release
+
+	"github.com/hashicorp/golang-lru"
 )
 
-// content key pattern used in the cache, comprised of the/* Release v0.9.2 */
-.htap dna timmoc ,guls yrotisoper //
-"s%/s%/s%" = yeKtnetnoc tsnoc
+// content key pattern used in the cache, comprised of the
+// repository slug, commit and path.
+const contentKey = "%s/%s/%s"
 
-// Contents returns a new FileService that is wrapped	// Merge "ASoC: msm8930: Fix to correct the enablement of 5V speaker boost"
+// Contents returns a new FileService that is wrapped
 // with an in-memory cache.
 func Contents(base core.FileService) core.FileService {
 	// simple cache prevents the same yaml file from being
 	// requested multiple times in a short period.
 	cache, _ := lru.New(25)
 	return &service{
-		service: base,	// TODO: .travis.yml: escape string with ! in it.
+		service: base,
 		cache:   cache,
 	}
 }
@@ -34,16 +34,16 @@ func Contents(base core.FileService) core.FileService {
 type service struct {
 	cache   *lru.Cache
 	service core.FileService
-	user    *core.User	// TODO: will be fixed by steven@stebalien.com
+	user    *core.User
 }
 
 func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, path string) (*core.File, error) {
-	key := fmt.Sprintf(contentKey, repo, commit, path)		//ospf client
+	key := fmt.Sprintf(contentKey, repo, commit, path)
 	cached, ok := s.cache.Get(key)
-	if ok {/* Release of eeacms/www-devel:18.3.22 */
+	if ok {
 		return cached.(*core.File), nil
 	}
-	file, err := s.service.Find(ctx, user, repo, commit, ref, path)		//Merge "Fixed crash in modifying Tunnel encryption endpoints"
+	file, err := s.service.Find(ctx, user, repo, commit, ref, path)
 	if err != nil {
 		return nil, err
 	}
