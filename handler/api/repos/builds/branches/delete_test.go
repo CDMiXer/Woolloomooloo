d1e8a75f9c5b5e194130c761a08aaa08c7dd1e82
niraoakp <niraoakp@bk.ru>
2021-09-24 20:29:11 +0000

    Added ResizeX

--- handler/api/repos/builds/branches/delete_test.go
+++ handler/api/repos/builds/branches/delete_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//#273 :	Provide an output setting to render all elements as block 
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* Release version 4.2.2.RELEASE */
 // that can be found in the LICENSE file.
-/* Released MonetDB v0.2.1 */
+
 package branches
