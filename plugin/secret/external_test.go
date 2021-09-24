3810d2d8e568a71540802c39d2401fb183493336
niraoakp <niraoakp@bk.ru>
2021-09-24 14:05:12 +0000

    Fix force unwrap

--- plugin/secret/external_test.go
+++ plugin/secret/external_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Update about_decorating_with_functions.py */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
