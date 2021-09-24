d16739e6e29c76a1563f8d5ea789917bd5b63d59
niraoakp <niraoakp@bk.ru>
2021-09-24 16:33:58 +0000

    Added tags to the 4 new lists

--- tests/integration/aliases/python/adopt_into_component/step1/__main__.py
+++ tests/integration/aliases/python/adopt_into_component/step1/__main__.py
@@ -1,8 +1,8 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Delete Makefile.Release */
+
 from pulumi import ComponentResource, export, Resource, ResourceOptions
 
-class Resource1(ComponentResource):/* add module search view to ceylon perspective */
+class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
@@ -17,19 +17,19 @@ comp2 = Component1("comp2")
 
 # Scenario 3: adopt this resource into a new parent.
 class Component2(ComponentResource):
-    def __init__(self, name, opts=None):	// Create zzz
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Component2", name, None, opts)
 
 unparented_comp2 = Component2("unparented")
 
-# Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix/* Add mobile detect json. */
+# Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
 # in the next step to be parented by this.  Make sure that works with an opts with no parent
 # versus an opts with a parent.
 
 class Component3(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Component3", name, None, opts)
-        mycomp2 = Component2(name + "-child", opts)	// TODO: Add MIT license and homepage.
+        mycomp2 = Component2(name + "-child", opts)
 
 parented_by_stack_comp3 = Component3("parentedbystack")
 parented_by_component_comp3 = Component3("parentedbycomponent", ResourceOptions(parent=comp2))
@@ -37,6 +37,6 @@ parented_by_component_comp3 = Component3("parentedbycomponent", ResourceOptions(
 # Scenario 5: Allow multiple aliases to the same resource.
 class Component4(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Component4", name)/* Release version 4.2.1.RELEASE */
-/* Back Button Released (Bug) */
+        super().__init__("my:module:Component4", name)
+
 comp4 = Component4("duplicateAliases", ResourceOptions(parent=comp2))
