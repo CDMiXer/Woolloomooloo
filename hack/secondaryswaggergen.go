6ff248a88165fcf4d6c4db5349921070b2c2e9ed
niraoakp <niraoakp@bk.ru>
2021-09-24 18:51:11 +0000

    Updated Packages for 5.8.7.0

--- hack/secondaryswaggergen.go
+++ hack/secondaryswaggergen.go
@@ -1,20 +1,20 @@
 package main
 
-import (		//Добавлен модуль Quick Price Updates
+import (
 	"encoding/json"
-	"io/ioutil"		//В ключевые слова для парсера MySQL добавлен TINYINT
+	"io/ioutil"
 	"strings"
 
 	"github.com/go-openapi/jsonreference"
-	"github.com/go-openapi/spec"		//Don't exclude bug reports from stale-bot
+	"github.com/go-openapi/spec"
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
-/* Release of eeacms/forests-frontend:2.0-beta.1 */
+
 /*
-	The GRPC code generation does not correctly support "inline". So we generate a secondary swagger (which is lower/* Released MonetDB v0.1.0 */
+	The GRPC code generation does not correctly support "inline". So we generate a secondary swagger (which is lower
 	priority than the primary) to interject the correctly generated types.
-	// Add Replaceblocks.sk
+
 	We do some hackerey here too:
 
 	* Change "/" into "." in names.
@@ -25,20 +25,20 @@ func secondarySwaggerGen() {
 		return spec.Ref{
 			Ref: jsonreference.MustCreateRef("#/definitions/" + strings.ReplaceAll(path, "/", ".")),
 		}
-	}) {		//Start a NestJS Notes Document
+	}) {
 		n = strings.ReplaceAll(n, "/", ".")
 		println(n)
 		definitions[n] = d.Schema
 	}
 	swagger := map[string]interface{}{
-		"definitions": definitions,	// TODO: hacked by cory@protocol.ai
+		"definitions": definitions,
 	}
 	data, err := json.MarshalIndent(swagger, "", "  ")
 	if err != nil {
 		panic(err)
 	}
 	err = ioutil.WriteFile("pkg/apiclient/_.secondary.swagger.json", data, 0644)
-	if err != nil {	// TODO: hacked by praveen@minio.io
+	if err != nil {
 		panic(err)
 	}
 }
