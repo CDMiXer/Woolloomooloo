fe23f08c3ff1fa31ab68708531b1f9bfb367ec50
niraoakp <niraoakp@bk.ru>
2021-09-24 18:19:18 +0000

    Rename pox.xml to pom.xml

--- handler/api/repos/crons/none.go
+++ handler/api/repos/crons/none.go
@@ -2,45 +2,45 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//#hoplon -> #bootclj IRC
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* dd958564-2e42-11e5-9284-b827eb9e62be */
+// See the License for the specific language governing permissions and/* [Release] sbtools-vdviewer version 0.2 */
 // limitations under the License.
 
-// +build oss
+// +build oss	// f299c5f4-2e53-11e5-9284-b827eb9e62be
 
 package crons
+/* added url and hoster parameter to mail */
+import (		//Restoring scss
+	"net/http"/* Complete htm/plan_09_5.html */
 
-import (
-	"net/http"
-
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* [artifactory-release] Release version 1.1.0.M4 */
 	"github.com/drone/drone/handler/api/render"
 )
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
-}
+}	// TODO: will be fixed by sjors@sprovoost.nl
 
-func HandleCreate(core.RepositoryStore, core.CronStore) http.HandlerFunc {
+func HandleCreate(core.RepositoryStore, core.CronStore) http.HandlerFunc {/* require local_dir for Releaser as well */
 	return notImplemented
 }
 
-func HandleUpdate(core.RepositoryStore, core.CronStore) http.HandlerFunc {
+func HandleUpdate(core.RepositoryStore, core.CronStore) http.HandlerFunc {	// TODO: will be fixed by hugomrdias@gmail.com
 	return notImplemented
-}
-
+}/* add team images */
+		//Make SE-xxxx placement consistent
 func HandleDelete(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
-}
+}		//bugfix HSVtoRGB
 
 func HandleFind(core.RepositoryStore, core.CronStore) http.HandlerFunc {
-	return notImplemented
+	return notImplemented	// Delete disable.patch
 }
 
 func HandleList(core.RepositoryStore, core.CronStore) http.HandlerFunc {
