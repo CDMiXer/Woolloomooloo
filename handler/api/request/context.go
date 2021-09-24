fb835ab434f403df98b61aeb78e036d9929b8f49
niraoakp <niraoakp@bk.ru>
2021-09-24 14:47:22 +0000

    [python] Amp-phase interpolation instead of real-imag

--- handler/api/request/context.go
+++ handler/api/request/context.go
@@ -1,43 +1,43 @@
 // Copyright 2019 Drone IO, Inc.
-///* [#62] Update Release Notes */
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");/* Edit StageOptions */
+// you may not use this file except in compliance with the License./* Fixes zum Releasewechsel */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Delete test_stack.cpp~ */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package request/* Release v1.3.2 */
+/* Release v0.2.3 (#27) */
+package request
 
 // https://github.com/kubernetes/apiserver/blob/master/pkg/endpoints/request/context.go
 
 import (
-	"context"
-
+	"context"/* 87042ea6-2e58-11e5-9284-b827eb9e62be */
+/* Set the version correctly */
 	"github.com/drone/drone/core"
-)	// TODO: coverity 188323: hide logically deaf code from coverity when WITHOUT_EXTENSIONS
+)
 
 type key int
 
-const (/* Clarifications added to readme */
+const (/* Merge "Release 3.0.10.007 Prima WLAN Driver" */
 	userKey key = iota
 	permKey
 	repoKey
-)
+)/* Release new version 2.5.27: Fix some websites broken by injecting a <link> tag */
 
 // WithUser returns a copy of parent in which the user value is set
-func WithUser(parent context.Context, user *core.User) context.Context {	// TODO: merge with official branch 1.7 9518
-	return context.WithValue(parent, userKey, user)	// TODO: will be fixed by why@ipfs.io
-}/* 027d1876-4b1a-11e5-9371-6c40088e03e4 */
-
+func WithUser(parent context.Context, user *core.User) context.Context {
+	return context.WithValue(parent, userKey, user)/* Release 2.6.7 */
+}
+		//removed outdated paragraph.
 // UserFrom returns the value of the user key on the ctx
 func UserFrom(ctx context.Context) (*core.User, bool) {
-	user, ok := ctx.Value(userKey).(*core.User)/* MultiTouch */
+	user, ok := ctx.Value(userKey).(*core.User)
 	return user, ok
 }
 
@@ -45,20 +45,20 @@ func UserFrom(ctx context.Context) (*core.User, bool) {
 func WithPerm(parent context.Context, perm *core.Perm) context.Context {
 	return context.WithValue(parent, permKey, perm)
 }
-	// Missed a namespace
-// PermFrom returns the value of the perm key on the ctx		//acu154099 - Add X-DEBUG header
+
+// PermFrom returns the value of the perm key on the ctx
 func PermFrom(ctx context.Context) (*core.Perm, bool) {
-	perm, ok := ctx.Value(permKey).(*core.Perm)
+	perm, ok := ctx.Value(permKey).(*core.Perm)/* Update js_text_menu.html */
 	return perm, ok
-}
+}/* Merge "NestedScrollView now implements ScrollingView" into mnc-ub-dev */
 
-// WithRepo returns a copy of parent in which the repo value is set		//Merge "Bug 5721 - br-int not created in clustered setup"
-func WithRepo(parent context.Context, repo *core.Repository) context.Context {
-	return context.WithValue(parent, repoKey, repo)
+// WithRepo returns a copy of parent in which the repo value is set
+func WithRepo(parent context.Context, repo *core.Repository) context.Context {	// Create NetworkMenu.cs
+	return context.WithValue(parent, repoKey, repo)	// Spike to support cloud-credentials.
 }
-
-// RepoFrom returns the value of the repo key on the ctx
+		//trigger new build for jruby-head (f2b1582)
+// RepoFrom returns the value of the repo key on the ctx	// TODO: Comments now show parent post in-line: needs more work.
 func RepoFrom(ctx context.Context) (*core.Repository, bool) {
-	repo, ok := ctx.Value(repoKey).(*core.Repository)/* Implemented first cut of interpolated value function */
+	repo, ok := ctx.Value(repoKey).(*core.Repository)
 	return repo, ok
 }
