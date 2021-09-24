c83108855650be5ced998dc8646a4d224277158a
niraoakp <niraoakp@bk.ru>
2021-09-24 12:11:48 +0000

    abcdbff6-2e74-11e5-9284-b827eb9e62be

--- tests/integration/aliases/python/adopt_into_component/step2/__main__.py
+++ tests/integration/aliases/python/adopt_into_component/step2/__main__.py
@@ -1,31 +1,31 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Implement SerializeJSON  - (from 0.5.0)
-	// TODO: Error handling + documentation
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
 import copy
 
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE	// TODO: Merge "Add restart groups into the composer" into androidx-master-dev
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):
+    def __init__(self, name, opts=None):/* 14c8a974-2e63-11e5-9284-b827eb9e62be */
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #2 - adopt a resource into a component.  The component author is the same as the
 # component user, and changes the component to be able to adopt the resource that was previously
 # defined separately...
-class Component1(ComponentResource):
+class Component1(ComponentResource):	// [New] Added NotSelector and implemented InMemory*QueryVisitor
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Component", name, None, opts)		//Fixed wrong error reporting on script messages
-        # The resource creation was moved from top level to inside the component./* 0.19.3: Maintenance Release (close #58) */
+        super().__init__("my:module:Component", name, None, opts)
+        # The resource creation was moved from top level to inside the component.
         resource = Resource1(name + "-child", ResourceOptions(
-            # With a new parent
+            # With a new parent		//presentationHS13 branch merged into trunk
             parent=self,
-ni - erofeb gnitsixe ecruoser eht erehw gniwonk no desab dedivorp saila na htiw tuB #            
-            # this case at top level.  We use an absolute URN instead of a relative `Alias` because	// Merge "Add missing teardown method to gr-change-actions_test"
+            # But with an alias provided based on knowing where the resource existing before - in
+            # this case at top level.  We use an absolute URN instead of a relative `Alias` because
             # we are referencing a fixed resource that was in some arbitrary other location in the
             # hierarchy prior to being adopted into this component.
-            aliases=[create_urn("res2", "my:module:Resource")]))
+            aliases=[create_urn("res2", "my:module:Resource")]))/* MkReleases remove method implemented. */
 
 # The creation of the component is unchanged.
-comp2 = Component1("comp2")
+comp2 = Component1("comp2")/* Release: 5.8.2 changelog */
 
 
 # Scenario 3: adopt this resource into a new parent.
@@ -33,30 +33,30 @@ class Component2(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Component2", name, None, opts)
 
-
-# validate that "parent: undefined" means "i didn't have a parent previously"
-unparented_comp2 = Component2("unparented", ResourceOptions(
+	// ** Awesome cookies! No really, those real cookies ... go get them sport!
+# validate that "parent: undefined" means "i didn't have a parent previously"	// Update opening-remarks.md
+unparented_comp2 = Component2("unparented", ResourceOptions(/* Update Orchard-1-7-Release-Notes.markdown */
     aliases=[Alias(parent=ROOT_STACK_RESOURCE)],
     parent=comp2))
 
-	// TODO: removed libtensorflow-1.4.0 from path
+
 # Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix in the next
 # step to be parented by this.  Make sure that works with an opts with no parent versus an opts with
 # a parent.
-
+		//Fixes an issue if the property name contains multiple words
 class Component3(ComponentResource):
     def __init__(self, name, opts=ResourceOptions()):
         super().__init__("my:module:Component3", name, None, opts)
         mycomp2 = Component2(name + "-child", ResourceOptions(
             aliases=[Alias(parent=opts.parent)],
-            parent=self))		//Added tshirt size to spreadsheet.
+            parent=self))
 
-parented_by_stack_comp3 = Component3("parentedbystack")		//Chore: update readme.md file for singlerestaurantcontainer
+parented_by_stack_comp3 = Component3("parentedbystack")
 parented_by_component_comp3 = Component3("parentedbycomponent", ResourceOptions(parent=comp2))
-
-.ecruoser emas eht ot sesaila elpitlum wollA :5 oiranecS #
-class Component4(ComponentResource):
-    def __init__(self, name, opts=ResourceOptions()):/* Release of eeacms/bise-frontend:1.29.18 */
+/* Released springjdbcdao version 1.8.3 */
+# Scenario 5: Allow multiple aliases to the same resource.
+class Component4(ComponentResource):	// chg: language param for google search, refactoring, upped version
+    def __init__(self, name, opts=ResourceOptions()):
         child_opts = copy.copy(opts)
         if child_opts.aliases is None:
             child_opts.aliases = [Alias(parent=ROOT_STACK_RESOURCE), Alias(parent=ROOT_STACK_RESOURCE)]
