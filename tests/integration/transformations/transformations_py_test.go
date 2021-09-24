d3c4d490468831ef2a7de7d0bf764f19b681ed51
niraoakp <niraoakp@bk.ru>
2021-09-24 20:41:52 +0000

    fix the constraint order

--- tests/integration/transformations/transformations_py_test.go
+++ tests/integration/transformations/transformations_py_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.		//Delete links-k2
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build python all
 
 package ints
@@ -12,16 +12,16 @@ import (
 
 func TestPythonTransformations(t *testing.T) {
 	for _, dir := range Dirs {
-		d := filepath.Join("python", dir)/* Teach llvm-readobj to print human friendly description of reserved sections. */
-		t.Run(d, func(t *testing.T) {/* Release version 1.2.1 */
+		d := filepath.Join("python", dir)
+		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir: d,/* [version] again, github actions reacted only Release keyword */
-				Dependencies: []string{	// add flake check before coverage 
+				Dir: d,
+				Dependencies: []string{
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
-				},/* -Fix some issues with Current Iteration / Current Release. */
+				},
 				Quick:                  true,
-				ExtraRuntimeValidation: Validator("python"),	// TODO: Delete insercion-provincias.sql
+				ExtraRuntimeValidation: Validator("python"),
 			})
-		})	// TODO: Start splitting out non-UI base code to project below DomUI
+		})
 	}
 }
