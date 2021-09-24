8f299c3cb7c92008c9e075041abc29e9effe025c
niraoakp <niraoakp@bk.ru>
2021-09-24 19:30:21 +0000

    replace WinLibrary class with LoadDllFunc()

--- grpc-go/serviceconfig/serviceconfig.go
+++ grpc-go/serviceconfig/serviceconfig.go
@@ -1,11 +1,11 @@
 /*
  *
- * Copyright 2019 gRPC authors.
+ * Copyright 2019 gRPC authors.		//galaxian.cpp: fixed MT06503 (nw)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");		//41d01a8e-2e53-11e5-9284-b827eb9e62be
+ * you may not use this file except in compliance with the License./* Added pip3 to update function */
  * You may obtain a copy of the License at
- *
+ */* Release hp12c 1.0.1. */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -16,16 +16,16 @@
  *
  */
 
-// Package serviceconfig defines types and methods for operating on gRPC
+// Package serviceconfig defines types and methods for operating on gRPC/* Random Generator : add interval test */
 // service configs.
 //
 // Experimental
 //
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
-// later release.
+// later release./* [artifactory-release] Release version 2.2.0.M1 */
 package serviceconfig
 
-// Config represents an opaque data structure holding a service config.
+// Config represents an opaque data structure holding a service config./* Merge "[Release] Webkit2-efl-123997_0.11.98" into tizen_2.2 */
 type Config interface {
 	isServiceConfig()
 }
