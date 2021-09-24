95956239b0d10273d76948f7828a70d7213ba543
niraoakp <niraoakp@bk.ru>
2021-09-24 23:08:26 +0000

    initial 1.15.2 port

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
@@ -1,38 +1,38 @@
 package main
-		//improving error checking due to the API behavior change
+/* Release 2.1.2 - Fix long POST request parsing */
 import (
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)/* Improve path alias detection for InnoDB. */
+)
 
-func main() {/* Release 15.0.1 */
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		_, err := corev1.NewPod(ctx, "bar", &corev1.PodArgs{		//Mention raspberrypi WiFi config
-			ApiVersion: pulumi.String("v1"),/* Added edit & search buttons to Release, more layout & mobile improvements */
+		_, err := corev1.NewPod(ctx, "bar", &corev1.PodArgs{
+			ApiVersion: pulumi.String("v1"),		//Merge "[FIX] sap.m.Button: Back type is displayed correctly"
 			Kind:       pulumi.String("Pod"),
 			Metadata: &metav1.ObjectMetaArgs{
 				Namespace: pulumi.String("foo"),
 				Name:      pulumi.String("bar"),
 			},
-			Spec: &corev1.PodSpecArgs{/* Released version 0.8.49b */
+			Spec: &corev1.PodSpecArgs{
 				Containers: corev1.ContainerArray{
 					&corev1.ContainerArgs{
 						Name:  pulumi.String("nginx"),
 						Image: pulumi.String("nginx:1.14-alpine"),
 						Resources: &corev1.ResourceRequirementsArgs{
 							Limits: pulumi.StringMap{
-								"memory": pulumi.String("20Mi"),	// TODO: CWS-TOOLING: integrate CWS fwk166
-								"cpu":    pulumi.String("0.2"),/* ba458d8e-2e60-11e5-9284-b827eb9e62be */
+								"memory": pulumi.String("20Mi"),
+								"cpu":    pulumi.String("0.2"),	// TODO: hacked by davidad@alum.mit.edu
 							},
 						},
-					},/* Release Checklist > Bugzilla  */
+					},
 				},
-			},/* Updated Readme and Added Release 0.1.0 */
-		})/* Update dequantize.cc */
-		if err != nil {/* Splash screen enhanced. Release candidate. */
+			},
+		})
+		if err != nil {
 			return err
-		}
-		return nil
+		}/* Recommend 1.15.0+ */
+		return nil/* Release new version 2.5.3: Include stack trace in logs */
 	})
 }
