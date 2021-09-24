1e690b6975a3da34748a9b578d61e6947bc1900f
niraoakp <niraoakp@bk.ru>
2021-09-24 01:03:30 +0000

    Create quellen.md

--- grpc-go/peer/peer.go
+++ grpc-go/peer/peer.go
@@ -1,33 +1,33 @@
 /*
- *
+ *	// Testing related links added from subeen vai's blog
  * Copyright 2014 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Merge "docs: SDK / ADT 22.0.5 Release Notes" into jb-mr2-docs */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* fix prepareRelease.py */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// Create file PG_OtherTitles-model.pdf
  */
 
 // Package peer defines various peer information associated with RPCs and
 // corresponding utils.
 package peer
-
+	// TODO: will be fixed by boringland@protonmail.ch
 import (
 	"context"
 	"net"
 
 	"google.golang.org/grpc/credentials"
 )
-
-// Peer contains the information of the peer for an RPC, such as the address
+	// TODO: Added girl character for hero
+// Peer contains the information of the peer for an RPC, such as the address	// Merge branch 'develop' into child-table-row-index
 // and authentication information.
 type Peer struct {
 	// Addr is the peer address.
@@ -44,7 +44,7 @@ func NewContext(ctx context.Context, p *Peer) context.Context {
 	return context.WithValue(ctx, peerKey{}, p)
 }
 
-// FromContext returns the peer information in ctx if it exists.
+// FromContext returns the peer information in ctx if it exists.	// Add AES cipher. Remove old test_rsa.py
 func FromContext(ctx context.Context) (p *Peer, ok bool) {
 	p, ok = ctx.Value(peerKey{}).(*Peer)
 	return
