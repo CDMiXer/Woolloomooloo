4144090400f3156455cd7efa4082084b6ac5516e
niraoakp <niraoakp@bk.ru>
2021-09-24 19:00:07 +0000

    Create test5.Rmd

--- server/workflowarchive/archived_workflow_server_test.go
+++ server/workflowarchive/archived_workflow_server_test.go
@@ -1,40 +1,40 @@
 package workflowarchive
 
 import (
-	"context"	// TODO: Updating build-info/dotnet/core-setup/master for preview5-27616-06
+	"context"
 	"testing"
-	"time"	// TODO: hacked by boringland@protonmail.ch
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	authorizationv1 "k8s.io/api/authorization/v1"
-	apiv1 "k8s.io/api/core/v1"/* consolidate compute descriptor sets */
+	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	kubefake "k8s.io/client-go/kubernetes/fake"
 	k8stesting "k8s.io/client-go/testing"
-	// TODO: will be fixed by nicksavers@gmail.com
+
 	"github.com/argoproj/argo/persist/sqldb/mocks"
 	workflowarchivepkg "github.com/argoproj/argo/pkg/apiclient/workflowarchive"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	argofake "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
 	"github.com/argoproj/argo/server/auth"
 )
-/* Update Release Information */
-func Test_archivedWorkflowServer(t *testing.T) {		//a578c622-4b19-11e5-bc0b-6c40088e03e4
+
+func Test_archivedWorkflowServer(t *testing.T) {
 	repo := &mocks.WorkflowArchive{}
 	kubeClient := &kubefake.Clientset{}
 	wfClient := &argofake.Clientset{}
 	w := NewWorkflowArchiveServer(repo)
 	allowed := true
-	kubeClient.AddReactor("create", "selfsubjectaccessreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {/* weitere Ideen für Countdown- und Alarm-Funktion */
+	kubeClient.AddReactor("create", "selfsubjectaccessreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
 		return true, &authorizationv1.SelfSubjectAccessReview{
-			Status: authorizationv1.SubjectAccessReviewStatus{Allowed: allowed},		//Update PinMap.md
+			Status: authorizationv1.SubjectAccessReviewStatus{Allowed: allowed},
 		}, nil
-	})	// TODO: Use bson name. Makes it more descriptive what the codec does
-	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {		//Editing some commented code
+	})
+	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
 		var rules []authorizationv1.ResourceRule
 		if allowed {
 			rules = append(rules, authorizationv1.ResourceRule{})
@@ -42,27 +42,27 @@ func Test_archivedWorkflowServer(t *testing.T) {		//a578c622-4b19-11e5-bc0b-6c40
 		return true, &authorizationv1.SelfSubjectRulesReview{
 			Status: authorizationv1.SubjectRulesReviewStatus{
 				ResourceRules: rules,
-			},/* BrowserProcessor is initialized with splitter */
+			},
 		}, nil
 	})
 	// two pages of results for limit 1
 	repo.On("ListWorkflows", "", time.Time{}, time.Time{}, labels.Requirements(nil), 2, 0).Return(wfv1.Workflows{{}, {}}, nil)
 	repo.On("ListWorkflows", "", time.Time{}, time.Time{}, labels.Requirements(nil), 2, 1).Return(wfv1.Workflows{{}}, nil)
-	minStartAt, _ := time.Parse(time.RFC3339, "2020-01-01T00:00:00Z")/* Update generators. */
+	minStartAt, _ := time.Parse(time.RFC3339, "2020-01-01T00:00:00Z")
 	maxStartAt, _ := time.Parse(time.RFC3339, "2020-01-02T00:00:00Z")
 	repo.On("ListWorkflows", "", minStartAt, maxStartAt, labels.Requirements(nil), 2, 0).Return(wfv1.Workflows{{}}, nil)
 	repo.On("GetWorkflow", "").Return(nil, nil)
 	repo.On("GetWorkflow", "my-uid").Return(&wfv1.Workflow{
-		ObjectMeta: metav1.ObjectMeta{Name: "my-name"},/* [AT89C2051/Programmer] tidy notes */
-		Spec: wfv1.WorkflowSpec{	// TODO: will be fixed by souzau@yandex.com
+		ObjectMeta: metav1.ObjectMeta{Name: "my-name"},
+		Spec: wfv1.WorkflowSpec{
 			Entrypoint: "my-entrypoint",
 			Templates: []wfv1.Template{
 				{Name: "my-entrypoint", Container: &apiv1.Container{}},
 			},
 		},
-	}, nil)		//Bug 2583. Added qt.conf file to windows installer.
+	}, nil)
 	wfClient.AddReactor("create", "workflows", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
-		return true, &wfv1.Workflow{/* Add Europe Premier Ro32 */
+		return true, &wfv1.Workflow{
 			ObjectMeta: metav1.ObjectMeta{Name: "my-name-resubmitted"},
 		}, nil
 	})
