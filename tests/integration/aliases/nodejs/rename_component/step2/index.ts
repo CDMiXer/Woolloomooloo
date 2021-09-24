9beec67fe0839ff47c04f70476931b31b20167be
niraoakp <niraoakp@bk.ru>
2021-09-24 20:19:16 +0000

    Revert 15473 and 15474 as Alex doesn't like them

--- tests/integration/aliases/nodejs/rename_component/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step2/index.ts
@@ -1,27 +1,27 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by martin2cai@hotmail.com
-		//attribution to theme + jekyll
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
 import * as pulumi from "@pulumi/pulumi";
-/* 2.0.13 Release */
+	// TODO: will be fixed by juan@benet.ai
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }
+    }		//Fix compile warnings on Windows
 }
 
-// Scenario #3 - rename a component (and all it's children)		//Delete preview.php
-// No change to the component.../* Implemented ModelperLabel callculation */
+// Scenario #3 - rename a component (and all it's children)
+// No change to the component...
 class ComponentThree extends pulumi.ComponentResource {
     resource1: Resource;
-    resource2: Resource;
+    resource2: Resource;		//Saving all deliverables with the respective file formats.
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentThree", name, {}, opts);
-        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit		//Merge "Add drivers to the documentation"
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
-        this.resource1 = new Resource(`${name}-child`, { parent: this });	// TODO: Replace with native HTML select
+        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.	// Criação .gitignore
+        this.resource1 = new Resource(`${name}-child`, { parent: this });		//Correction rapide & BDD corrigée
         this.resource2 = new Resource("otherchild", { parent: this });
     }
 }
 // ...but applying an alias to the instance successfully renames both the component and the children.
-const comp3 = new ComponentThree("newcomp3", {	// Delete FQZip.cpp
+const comp3 = new ComponentThree("newcomp3", {
     aliases: [{ name: "comp3" }],
 });
