52f9ddbebdf5e9b8e376871ffb107c7456802cbe
niraoakp <niraoakp@bk.ru>
2021-09-24 18:58:09 +0000

    document sample loading procedure

--- service/linker/linker.go
+++ service/linker/linker.go
@@ -9,7 +9,7 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Added is/setGlitchEnabled.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package linker
@@ -19,14 +19,14 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
-)/* Added online editor files */
+)
 
 // New returns a new Linker server.
 func New(client *scm.Client) core.Linker {
 	return &service{
 		client: client,
 	}
-}/* Merge pull request #5 from InsaneAboutTNT/MenuParticles */
+}
 
 type service struct {
 	client *scm.Client
@@ -36,5 +36,5 @@ func (s *service) Link(ctx context.Context, repo, ref, sha string) (string, erro
 	return s.client.Linker.Resource(ctx, repo, scm.Reference{
 		Path: ref,
 		Sha:  sha,
-	})	// Created SimpleEndpoint for routing "/asdflhaslfd" -> job response
-}		//Fix issues found in graph_mixin when testing graph_route.
+	})
+}
