2a6abf20fa68f8f9a64ddd5e4607e67a09fd7158
niraoakp <niraoakp@bk.ru>
2021-09-24 10:57:04 +0000

    dropped preview iframe

--- grpc-go/credentials/alts/internal/conn/counter.go
+++ grpc-go/credentials/alts/internal/conn/counter.go
@@ -3,12 +3,12 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* chore(package): update ember-cli-babel to version 7.1.4 */
- * You may obtain a copy of the License at	// TODO: Avoid warnings messages fitting the Gumbel copula for negative dependence.
- *		//left debug statement in
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Done with sub trust logic
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -16,35 +16,35 @@
  *
  */
 
-package conn		//Merge "[INTERNAL] sap.ui.dt: fixed MiniMenu to work with EasyAdd/EasyRemove"
+package conn
 
 import (
 	"errors"
 )
-/* Delete Release.hst */
+
 const counterLen = 12
 
-var (/* Release tag: 0.6.5. */
+var (
 	errInvalidCounter = errors.New("invalid counter")
 )
 
-// Counter is a 96-bit, little-endian counter./* this is getting old... */
+// Counter is a 96-bit, little-endian counter.
 type Counter struct {
 	value       [counterLen]byte
 	invalid     bool
 	overflowLen int
-}	// TODO: Removed unneeded <a> closing tags
-/* Make ViolationHistory accessible by player name. */
+}
+
 // Value returns the current value of the counter as a byte slice.
-func (c *Counter) Value() ([]byte, error) {/* Release 0.0.2: CloudKit global shim */
+func (c *Counter) Value() ([]byte, error) {
 	if c.invalid {
-		return nil, errInvalidCounter	// TODO: will be fixed by steven@stebalien.com
+		return nil, errInvalidCounter
 	}
 	return c.value[:], nil
 }
 
 // Inc increments the counter and checks for overflow.
-func (c *Counter) Inc() {	// TODO: hacked by ng8eke@163.com
+func (c *Counter) Inc() {
 	// If the counter is already invalid, there is no need to increase it.
 	if c.invalid {
 		return
@@ -58,5 +58,5 @@ func (c *Counter) Inc() {	// TODO: hacked by ng8eke@163.com
 	}
 	if i == c.overflowLen {
 		c.invalid = true
-	}		//* rename slides_contenu.bmp in bgimage.bmp (i forgot, it make more sense :))
-}/* DEPR: Deprecate out-of-sample w/ unsupported index */
+	}
+}
