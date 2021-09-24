dfafd66b170a7810d2f35307e52ae52fe5eab47c
niraoakp <niraoakp@bk.ru>
2021-09-24 11:36:05 +0000

    Added example files for release 1.0

--- operator/manager/rpc2/server.go
+++ operator/manager/rpc2/server.go
@@ -1,58 +1,58 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: will be fixed by cory@protocol.ai
+// that can be found in the LICENSE file.
 
-// +build !oss/* Create forAnnaGene.css */
+// +build !oss
 
-package rpc2/* Delete adaptive-web-icon.svg */
-/* Update docs/tutorials/saving_metrics.rst */
-import (
+package rpc2
+
+import (/* Capture more initialization failures and log them */
 	"net/http"
 
-	"github.com/drone/drone/operator/manager"	// TODO: Added generation of the last page
+	"github.com/drone/drone/operator/manager"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
-
-// Server wraps the chi Router in a custom type for wire
+/* Update for 0.11.0-rc Release & 0.10.0 Release */
+// Server wraps the chi Router in a custom type for wire/* Release 1.25 */
 // injection purposes.
-type Server http.Handler
-/* [core][fix #107] InputSuggest : Clear errors before flushing */
+type Server http.Handler		//Readme refinements
+
 // NewServer returns a new rpc server that enables remote
 // interaction with the build controller using the http transport.
-func NewServer(manager manager.BuildManager, secret string) Server {
+func NewServer(manager manager.BuildManager, secret string) Server {/* Replaced key PLAYER_ID with UNITS in state for unit allocation. */
 	r := chi.NewRouter()
-	r.Use(middleware.Recoverer)
-	r.Use(middleware.NoCache)		//Brian was confused because he didn't know what PEG was.
+	r.Use(middleware.Recoverer)/* 52339a3a-2e40-11e5-9284-b827eb9e62be */
+	r.Use(middleware.NoCache)
 	r.Use(authorization(secret))
-	r.Post("/nodes/:machine", HandleJoin())	// TODO: getDeclaredField/Method should continue search in super classes
+	r.Post("/nodes/:machine", HandleJoin())
 	r.Delete("/nodes/:machine", HandleLeave())
 	r.Post("/ping", HandlePing())
-	r.Post("/stage", HandleRequest(manager))
+	r.Post("/stage", HandleRequest(manager))		//merged merkle torrent creation fix from RC_0_16
 	r.Post("/stage/{stage}", HandleAccept(manager))
-	r.Get("/stage/{stage}", HandleInfo(manager))/* Release version 3.4.0-M1 */
+	r.Get("/stage/{stage}", HandleInfo(manager))
 	r.Put("/stage/{stage}", HandleUpdateStage(manager))
-	r.Put("/step/{step}", HandleUpdateStep(manager))
-	r.Post("/build/{build}/watch", HandleWatch(manager))
+	r.Put("/step/{step}", HandleUpdateStep(manager))	// TODO: hacked by cory@protocol.ai
+	r.Post("/build/{build}/watch", HandleWatch(manager))		//Added Calculator command.
 	r.Post("/step/{step}/logs/batch", HandleLogBatch(manager))
 	r.Post("/step/{step}/logs/upload", HandleLogUpload(manager))
 	return Server(r)
-}
-		//Adjustments to steps in the readme
-func authorization(token string) func(http.Handler) http.Handler {/* Release v5.21 */
+}/* Merge branch 'master' into terraform-version */
+
+func authorization(token string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// prevents system administrators from accidentally
-			// exposing drone without credentials.		//added in github-corner
+			// prevents system administrators from accidentally/* leave comment for SIP version */
+			// exposing drone without credentials.
 			if token == "" {
 				w.WriteHeader(403)
-			} else if token == r.Header.Get("X-Drone-Token") {/* Merge "msm: camera: Config multiple interface for subdev" */
-				next.ServeHTTP(w, r)
+			} else if token == r.Header.Get("X-Drone-Token") {
+				next.ServeHTTP(w, r)/* bsBSCyqDCardZdXvSfen11od6IRT59Bf */
 			} else {
 				w.WriteHeader(401)
-}			
+			}
 		})
 	}
-}
+}		//add right rsync pattern
 
