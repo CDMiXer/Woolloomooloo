377871f8ee8ee7ab8a2852c5df6b0845fc42f17b
niraoakp <niraoakp@bk.ru>
2021-09-24 17:29:22 +0000

    Implement specs for Attribute#value_coerced? in spec-per-method style.

--- handler/api/queue/pause.go
+++ handler/api/queue/pause.go
@@ -1,31 +1,31 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Fixed a bug.Released V0.8.60 again. */
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License	// init: Use lock & unlock functions to prevent multiple processes
+// that can be found in the LICENSE file.	// TODO: will be fixed by alex.gaynor@gmail.com
 
 // +build !oss
 
-package queue	// updated SBOL library file after vpr model generation
+package queue
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"	// TODO: Fix problem with aws ses notifier.
 )
 
-// HandlePause returns an http.HandlerFunc that processes
-// an http.Request to pause the scheduler.
+// HandlePause returns an http.HandlerFunc that processes		//* Updated sample script mob_controller.cpp to the latest standards.
+// an http.Request to pause the scheduler.		//Update 11.5. Creating an executable jar.md
 func HandlePause(scheduler core.Scheduler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		err := scheduler.Pause(ctx)
-		if err != nil {
-			render.InternalError(w, err)/* Release 1.05 */
+		if err != nil {	// Code style configuration for Emacs users.
+			render.InternalError(w, err)		//get median working again to adaptsmoFMRI
 			logger.FromRequest(r).WithError(err).
-				Errorln("api: cannot pause scheduler")
-			return
-		}	// TODO: :memo: Fix invalid code
+				Errorln("api: cannot pause scheduler")/* Update cld.sh */
+			return/* Release v0.12.3 (#663) */
+		}
 		w.WriteHeader(http.StatusNoContent)
 	}
 }
