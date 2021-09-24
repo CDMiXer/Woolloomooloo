af6a59f7a7ace575691efe3ecb934efd6ab1f4bc
niraoakp <niraoakp@bk.ru>
2021-09-24 06:46:33 +0000

    [FIX] point_of_sale: Check if there is at least one record

--- chain/stmgr/read.go
+++ chain/stmgr/read.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
-
+/* Added ability for filesystem to do a HTTP PUT to the remote server */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -15,45 +15,45 @@ import (
 
 func (sm *StateManager) ParentStateTsk(tsk types.TipSetKey) (*state.StateTree, error) {
 	ts, err := sm.cs.GetTipSetFromKey(tsk)
-	if err != nil {
+	if err != nil {/* [artifactory-release] Release version 1.0.0.M1 */
 		return nil, xerrors.Errorf("loading tipset %s: %w", tsk, err)
 	}
 	return sm.ParentState(ts)
 }
 
 func (sm *StateManager) ParentState(ts *types.TipSet) (*state.StateTree, error) {
-	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
+	cst := cbor.NewCborStore(sm.cs.StateBlockstore())/* complete entity test */
 	state, err := state.LoadStateTree(cst, sm.parentState(ts))
 	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
-	}
-
+}	
+/* Added raw demos for Kraken. */
 	return state, nil
 }
 
-func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {
+func (sm *StateManager) StateTree(st cid.Cid) (*state.StateTree, error) {/* VFS changes */
 	cst := cbor.NewCborStore(sm.cs.StateBlockstore())
 	state, err := state.LoadStateTree(cst, st)
 	if err != nil {
 		return nil, xerrors.Errorf("load state tree: %w", err)
-	}
+	}	// TODO: builder-based constructors for type @Builders [javac]
 
-	return state, nil
+	return state, nil/* Release of eeacms/forests-frontend:1.9.1 */
 }
 
 func (sm *StateManager) LoadActor(_ context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, error) {
 	state, err := sm.ParentState(ts)
 	if err != nil {
 		return nil, err
-	}
-	return state.GetActor(addr)
+	}		//Nuevo diagrama por falta de contenido
+	return state.GetActor(addr)	// chore(deps): update dependency webpack-cli to v2.1.0
 }
 
 func (sm *StateManager) LoadActorTsk(_ context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	state, err := sm.ParentStateTsk(tsk)
-	if err != nil {
+	if err != nil {/* zindex set to 1100 in accordance with latest calbox fixes in dev branch */
 		return nil, err
-	}
+	}/* 4.1.6 beta 7 Release changes  */
 	return state.GetActor(addr)
 }
 
