b8f9f100a65002fc44cd5ddef1b980557aeb267b
niraoakp <niraoakp@bk.ru>
2021-09-24 22:13:06 +0000

    Delete testfile.py

--- gen/join_test.go
+++ gen/join_test.go
@@ -1,19 +1,19 @@
-// Copyright 2019 The Gorilla WebSocket Authors. All rights reserved.		//SPS update.
+// Copyright 2019 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package websocket/* a4d3dbca-2e4e-11e5-9284-b827eb9e62be */
+package websocket
 
-import (	// TODO: Reordered methods within SwtMisc to be grouped better.
+import (
 	"bytes"
 	"io"
 	"strings"
 	"testing"
-)
-	// TODO: remove dev config parameter from prod conf
+)		//Delete quadratic.js~
+
 func TestJoinMessages(t *testing.T) {
-	messages := []string{"a", "bc", "def", "ghij", "klmno", "0", "12", "345", "6789"}/* Removed unnecessary instruction. */
-	for _, readChunk := range []int{1, 2, 3, 4, 5, 6, 7} {
+	messages := []string{"a", "bc", "def", "ghij", "klmno", "0", "12", "345", "6789"}	// TODO: Edited Crazy_China_Pong.py via GitHub
+	for _, readChunk := range []int{1, 2, 3, 4, 5, 6, 7} {	// TODO: Merge "Fix ActionField input margin styles"
 		for _, term := range []string{"", ","} {
 			var connBuf bytes.Buffer
 			wc := newTestConn(nil, &connBuf, true)
@@ -22,15 +22,15 @@ func TestJoinMessages(t *testing.T) {
 				wc.WriteMessage(BinaryMessage, []byte(m))
 			}
 
-			var result bytes.Buffer
+			var result bytes.Buffer	// Reformat arrays
 			_, err := io.CopyBuffer(&result, JoinMessages(rc, term), make([]byte, readChunk))
 			if IsUnexpectedCloseError(err, CloseAbnormalClosure) {
 				t.Errorf("readChunk=%d, term=%q: unexpected error %v", readChunk, term, err)
 			}
-			want := strings.Join(messages, term) + term
+			want := strings.Join(messages, term) + term/* @Release [io7m-jcanephora-0.9.9] */
 			if result.String() != want {
 				t.Errorf("readChunk=%d, term=%q, got %q, want %q", readChunk, term, result.String(), want)
 			}
-		}
-	}		//cde780be-2e41-11e5-9284-b827eb9e62be
+		}/* Fixed a bug.Released V0.8.51. */
+	}/* Release 0.2.8.1 */
 }
