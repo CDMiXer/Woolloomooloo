93c9b49b56551f0c634f2108ea6bebb7d063e9a0
niraoakp <niraoakp@bk.ru>
2021-09-24 06:52:46 +0000

    initial re-work on Data access for allowing UI interaction

--- handler/api/system/license.go
+++ handler/api/system/license.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* bitfinex2 parseOrderStatus edits */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
-// +build !oss
+// that can be found in the LICENSE file./* Release v5.30 */
+/* BrowserBot v0.3 Release */
+// +build !oss/* Release plugin configuration added */
 
 package system
-
+		//Create Получить файлы юзера
 import (
-	"net/http"		//Merge branch 'develop' into fix/mail-cleanup-text
+	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Merge "Release 3.0.10.022 Prima WLAN Driver" */
 	"github.com/drone/drone/handler/api/render"
-)
+)/* Gartner MQ Press Release */
 
 // HandleLicense returns an http.HandlerFunc that writes
 // json-encoded license details to the response body.
 func HandleLicense(license core.License) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		render.JSON(w, license, 200)	// Added sftp resource
+		render.JSON(w, license, 200)
 	}
 }
