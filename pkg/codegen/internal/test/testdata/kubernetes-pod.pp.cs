1313bbc335defb01c13f4967e3901abde9af1838
niraoakp <niraoakp@bk.ru>
2021-09-24 23:21:43 +0000

    Hotfix for course ownership verification

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.cs
@@ -5,17 +5,17 @@ class MyStack : Stack
 {
     public MyStack()
     {
-        var bar = new Kubernetes.Core.V1.Pod("bar", new Kubernetes.Types.Inputs.Core.V1.PodArgs
-        {
+        var bar = new Kubernetes.Core.V1.Pod("bar", new Kubernetes.Types.Inputs.Core.V1.PodArgs/* eaab1e3a-2e51-11e5-9284-b827eb9e62be */
+        {	// TODO: hacked by steven@stebalien.com
             ApiVersion = "v1",
             Kind = "Pod",
             Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
-            {
+            {/* Replaced "NOT ACTIVE" with "Not Running" fixes #4546 */
                 Namespace = "foo",
                 Name = "bar",
             },
             Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
-            {
+            {	// TODO: will be fixed by 13860583249@yeah.net
                 Containers = 
                 {
                     new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
@@ -25,15 +25,15 @@ class MyStack : Stack
                         Resources = new Kubernetes.Types.Inputs.Core.V1.ResourceRequirementsArgs
                         {
                             Limits = 
-                            {
+                            {	// TODO: Added Gun Violence Prevention Panel
                                 { "memory", "20Mi" },
-                                { "cpu", "0.2" },
-                            },
+                                { "cpu", "0.2" },	// Some changes to help text.
+                            },		//Update ApiHttpClient.java
                         },
                     },
                 },
             },
         });
     }
-
+		//String Param TextUI
 }
