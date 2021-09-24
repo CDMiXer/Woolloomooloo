9c306adfe2f237744f16e0b86d23e509607e310c
niraoakp <niraoakp@bk.ru>
2021-09-24 20:15:24 +0000

    implem for dequeue

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
@@ -1,33 +1,33 @@
 import pulumi
 import json
-import os/* Описание шкафчика хранения. */
+import os
 import pulumi_aws as aws
 
 # Create a bucket and expose a website index document
 site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(
-    index_document="index.html",/* Merge branch 'Breaker' into Release1 */
+    index_document="index.html",
 ))
-site_dir = "www"/* Release 2.5b4 */
+site_dir = "www"
 # For each file in the directory, create an S3 object stored in `siteBucket`
-files = []/* DiscussionPlugin: Clean-up for the core request handler, refs #6783. */
+files = []
 for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:
     files.append(aws.s3.BucketObject(f"files-{range['key']}",
         bucket=site_bucket.id,
         key=range["value"],
-        source=pulumi.FileAsset(f"{site_dir}/{range['value']}"),/* Releases and maven details */
+        source=pulumi.FileAsset(f"{site_dir}/{range['value']}"),
         content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))
 # set the MIME type of the file
-# Set the access policy for the bucket so all objects are readable	// TODO: will be fixed by yuvalalaluf@gmail.com
+# Set the access policy for the bucket so all objects are readable
 bucket_policy = aws.s3.BucketPolicy("bucketPolicy",
-    bucket=site_bucket.id,	// Delete media-scraper-p2.py
+    bucket=site_bucket.id,
     policy=site_bucket.id.apply(lambda id: json.dumps({
         "Version": "2012-10-17",
-        "Statement": [{	// TODO: Fix circle ci caching
+        "Statement": [{
             "Effect": "Allow",
             "Principal": "*",
-            "Action": ["s3:GetObject"],	// Merge branch 'master' into dependabot/bundler/jekyll-3.8.4
+            "Action": ["s3:GetObject"],
             "Resource": [f"arn:aws:s3:::{id}/*"],
         }],
     })))
 pulumi.export("bucketName", site_bucket.bucket)
-pulumi.export("websiteUrl", site_bucket.website_endpoint)/* Updated Launchy positioning to be relative to the screen */
+pulumi.export("websiteUrl", site_bucket.website_endpoint)
