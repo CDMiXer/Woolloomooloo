c66e3a4b926165c8770d912b5f6adf7d70282ae7
niraoakp <niraoakp@bk.ru>
2021-09-24 03:54:45 +0000

    [IMP] mail: You have one unread message

--- handler/api/repos/crons/list.go
+++ handler/api/repos/crons/list.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//duplicate createTraverser methods removed
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package crons
-
+		//removed persistence/jpa dependency, we do not need this here
 import (
 	"net/http"
 
@@ -13,29 +13,29 @@ import (
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)/* Release: Making ready for next release iteration 5.9.1 */
-/* add cc-sa and bok styles */
+)
+
 // HandleList returns an http.HandlerFunc that writes a json-encoded
-// list of cron jobs to the response body.
+// list of cron jobs to the response body.	// TODO: On screen keyboard
 func HandleList(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,	// src/ogg.c : Fix log message.
 	crons core.CronStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {		//Prepare for 0.11 release.
-		var (/* Released springjdbcdao version 1.9.16 */
+	return func(w http.ResponseWriter, r *http.Request) {
+		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")	// TODO: will be fixed by vyzo@hackzen.org
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
-			render.NotFound(w, err)/* Release version 2.3.0.RELEASE */
+		if err != nil {	// TODO: 1a017f08-2e75-11e5-9284-b827eb9e62be
+			render.NotFound(w, err)
 			return
 		}
-		list, err := crons.List(r.Context(), repo.ID)/* Merge "Set priority for havana channel" */
+		list, err := crons.List(r.Context(), repo.ID)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}	// Rename database.sample.yml to database.yml
+		}
 		render.JSON(w, list, 200)
-	}
-}		//Create general.html
+	}/* Release of eeacms/forests-frontend:2.0-beta.41 */
+}
