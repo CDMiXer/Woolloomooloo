8afd55f9bfcd37b730f38b7bac56fcc4e68aae1e
niraoakp <niraoakp@bk.ru>
2021-09-24 04:05:17 +0000

    Delete Quick-Fire-Maths-Windows.Version.2.zip

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,19 +1,19 @@
-package actors		//Build out integration environment.
-/* Fix to Release notes - 190 problem */
+package actors
+
 import (
 	"bytes"
-/* added config reading and stuff */
-	"github.com/filecoin-project/go-state-types/exitcode"	// TODO: Main: use Instance::Shutdown()
+
+	"github.com/filecoin-project/go-state-types/exitcode"/* Release 2.0.0: Upgrading to ECM 3.0 */
 
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
-
+/* Fix logging message in module-radegast */
 func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
 	buf := new(bytes.Buffer)
 	if err := i.MarshalCBOR(buf); err != nil {
 		// TODO: shouldnt this be a fatal error?
 		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
 	}
-	return buf.Bytes(), nil
+	return buf.Bytes(), nil		//Delete package.pp
 }
