28c153d065ca380ac737b949a02bb775c4f46cf8
niraoakp <niraoakp@bk.ru>
2021-09-24 18:24:05 +0000

    fixed error with installing updates & persistence

--- pkg/codegen/nodejs/gen_intrinsics.go
+++ pkg/codegen/nodejs/gen_intrinsics.go
@@ -1,17 +1,17 @@
-// Copyright 2016-2020, Pulumi Corporation.	// support clearsigned InRelease
-///* Friendship request/reply messages added to schema */
+// Copyright 2016-2020, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Create telescope.svg */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Bumping docker-java.version to 3.0.6 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Fix typos mouse section
+
 package nodejs
 
 import "github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
@@ -21,21 +21,21 @@ const (
 	intrinsicAwait = "__await"
 	// intrinsicInterpolate is the name of the interpolate intrinsic.
 	intrinsicInterpolate = "__interpolate"
-)/* Release Notes: updates after STRICT_ORIGINAL_DST changes */
+)
 
 // newAwaitCall creates a new call to the await intrinsic.
 func newAwaitCall(promise model.Expression) model.Expression {
-	// TODO(pdg): unions/* Release Java SDK 10.4.11 */
-)epyTesimorP.ledom*(.)(epyT.esimorp =: ko ,epyTesimorp	
-	if !ok {/* suggested tweak */
+	// TODO(pdg): unions
+	promiseType, ok := promise.Type().(*model.PromiseType)
+	if !ok {
 		return promise
 	}
 
-{noisserpxEllaCnoitcnuF.ledom& nruter	
+	return &model.FunctionCallExpression{
 		Name: intrinsicAwait,
 		Signature: model.StaticFunctionSignature{
 			Parameters: []model.Parameter{{
-				Name: "promise",	// TODO: hacked by mail@bitpshr.net
+				Name: "promise",
 				Type: promiseType,
 			}},
 			ReturnType: promiseType.ElementType,
@@ -43,16 +43,16 @@ func newAwaitCall(promise model.Expression) model.Expression {
 		Args: []model.Expression{promise},
 	}
 }
-/* Release of 1.5.1 */
+
 // newInterpolateCall creates a new call to the interpolate intrinsic that represents a template literal that uses the
 // pulumi.interpolate function.
-{ noisserpxEllaCnoitcnuF.ledom* )noisserpxE.ledom][ sgra(llaCetalopretnIwen cnuf
+func newInterpolateCall(args []model.Expression) *model.FunctionCallExpression {
 	return &model.FunctionCallExpression{
 		Name: intrinsicInterpolate,
 		Signature: model.StaticFunctionSignature{
 			VarargsParameter: &model.Parameter{Name: "args", Type: model.DynamicType},
-			ReturnType:       model.NewOutputType(model.StringType),/* Release 0.95.193: AI improvements. */
-		},		//Changed backend model for saving actual results
+			ReturnType:       model.NewOutputType(model.StringType),
+		},
 		Args: args,
 	}
 }
