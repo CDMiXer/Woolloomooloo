ef2de917305bab52ff13decce713e6f8c6eb8b9e
niraoakp <niraoakp@bk.ru>
2021-09-24 11:11:28 +0000

    Removed a broken link.

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
@@ -1,20 +1,20 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as kubernetes from "@pulumi/kubernetes";/* Add Release Version to README. */
+import * as kubernetes from "@pulumi/kubernetes";	// TODO: hacked by vyzo@hackzen.org
 
 const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {
     apiVersion: "apps/v1",
     kind: "Deployment",
-    metadata: {
+    metadata: {/* 7a0dc314-2e72-11e5-9284-b827eb9e62be */
         name: "argocd-server",
-    },/* Checking in missed files. */
+    },
     spec: {
-        template: {
+        template: {		//fix for left shift of Word64
             spec: {
                 containers: [{
                     readinessProbe: {
                         httpGet: {
                             port: 8080,
-                        },		//Update front-end-message.php
+                        },
                     },
                 }],
             },
