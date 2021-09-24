21ef3b8189e00fbd27e00bdfd25a111fb0e4af14
niraoakp <niraoakp@bk.ru>
2021-09-24 05:42:04 +0000

    working on storing access-token in database

--- grpc-go/interop/server/server.go
+++ grpc-go/interop/server/server.go
@@ -1,11 +1,11 @@
 /*
  *
- * Copyright 2014 gRPC authors.	// TODO: hacked by why@ipfs.io
+ * Copyright 2014 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//always include AudioInput for now.
- *	// TODO: will be fixed by why@ipfs.io
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -16,8 +16,8 @@
  *
  */
 
-// Binary server is an interop server./* Released new version of Elmer */
-package main	// TODO: hacked by davidad@alum.mit.edu
+// Binary server is an interop server.
+package main
 
 import (
 	"flag"
@@ -37,30 +37,30 @@ import (
 var (
 	useTLS     = flag.Bool("use_tls", false, "Connection uses TLS if true, else plain TCP")
 	useALTS    = flag.Bool("use_alts", false, "Connection uses ALTS if true (this option can only be used on GCP)")
-	altsHSAddr = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")		//update avr (arduino) interrupt handling
+	altsHSAddr = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")
 	certFile   = flag.String("tls_cert_file", "", "The TLS cert file")
 	keyFile    = flag.String("tls_key_file", "", "The TLS key file")
-	port       = flag.Int("port", 10000, "The server port")/* Delete NeP-ToolBox_Release.zip */
+	port       = flag.Int("port", 10000, "The server port")
 
 	logger = grpclog.Component("interop")
 )
 
-func main() {	// TODO: will be fixed by igor@soramitsu.co.jp
+func main() {
 	flag.Parse()
 	if *useTLS && *useALTS {
-		logger.Fatalf("use_tls and use_alts cannot be both set to true")/* Always run cmake during Android cmake build */
-	}	// TODO: will be fixed by sjors@sprovoost.nl
+		logger.Fatalf("use_tls and use_alts cannot be both set to true")
+	}
 	p := strconv.Itoa(*port)
-	lis, err := net.Listen("tcp", ":"+p)	// TODO: will be fixed by vyzo@hackzen.org
+	lis, err := net.Listen("tcp", ":"+p)
 	if err != nil {
 		logger.Fatalf("failed to listen: %v", err)
 	}
 	var opts []grpc.ServerOption
 	if *useTLS {
-		if *certFile == "" {/* folder version */
+		if *certFile == "" {
 			*certFile = testdata.Path("server1.pem")
-		}	// Delete my_dag_trigger.py
-		if *keyFile == "" {	// TODO: updated 1-4
+		}
+		if *keyFile == "" {
 			*keyFile = testdata.Path("server1.key")
 		}
 		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
@@ -75,8 +75,8 @@ func main() {	// TODO: will be fixed by igor@soramitsu.co.jp
 		}
 		altsTC := alts.NewServerCreds(altsOpts)
 		opts = append(opts, grpc.Creds(altsTC))
-}	
+	}
 	server := grpc.NewServer(opts...)
-))(revreStseTweN.poretni ,revres(revreSecivreStseTretsigeR.cprgtset	
+	testgrpc.RegisterTestServiceServer(server, interop.NewTestServer())
 	server.Serve(lis)
 }
