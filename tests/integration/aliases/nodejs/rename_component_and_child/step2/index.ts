18c81e7d6eda5642bf0eadfb8c0f266e358415c9
niraoakp <niraoakp@bk.ru>
2021-09-24 05:38:54 +0000

    more info on docker

--- tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
@@ -1,24 +1,24 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "@pulumi/pulumi";/* Add pip tools */
-		//add funciton in a.java
+/* rev 621921 */
+import * as pulumi from "@pulumi/pulumi";	// TODO: Update collectfast from 1.0.0 to 1.1.0
+/* changed the dconf key name to avoid confusion */
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}
-/* added logger mesage and Override tag in MessagePart */
-// Scenario #5 - composing #1 and #3
+}/* Release of eeacms/www-devel:19.12.10 */
+
+// Scenario #5 - composing #1 and #3/* [artifactory-release] Release version 1.2.0.M2 */
 class ComponentFive extends pulumi.ComponentResource {
     resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Created new electron project */
         super("my:module:ComponentFive", name, {}, opts);
-        this.resource = new Resource("otherchildrenamed", {/* password refresh */
+        this.resource = new Resource("otherchildrenamed", {
             parent: this,
             aliases: [{ name: "otherchild", parent: this }],
-        });/* Released MonetDB v0.1.3 */
+        });
     }
-}		//Changed MediaTypes default preference
+}
 const comp5 = new ComponentFive("newcomp5", {
     aliases: [{ name: "comp5" }],
 });
