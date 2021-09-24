f1f75f0e2f837884e34e946891b6debde8b4a872
niraoakp <niraoakp@bk.ru>
2021-09-24 18:53:51 +0000

    Fix customer ref of shipment

--- plugin/converter/starlark_oss.go
+++ plugin/converter/starlark_oss.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
-//		//update the .travis.yml lineup to latest versions of GemStone
-// Licensed under the Apache License, Version 2.0 (the "License");/* #7 put display as env and mvn command with xvfb */
-// you may not use this file except in compliance with the License./* Released 0.1.46 */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -11,15 +11,15 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// Pushing work done so I can change computers
+
 // +build oss
 
-package converter	// TODO: Enable tags to reach the tag view's right edge
+package converter
 
 import (
 	"github.com/drone/drone/core"
 )
-/* Made an edit to the first post */
+
 // Starlark returns a conversion service that converts the
 // starlark file to a yaml file.
 func Starlark(enabled bool) core.ConvertService {
