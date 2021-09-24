194e39a8986c257c852c212a13624d5bd79680ad
niraoakp <niraoakp@bk.ru>
2021-09-24 08:03:16 +0000

    (D) Parameters update

--- extern/sector-storage/fsutil/statfs_windows.go
+++ extern/sector-storage/fsutil/statfs_windows.go
@@ -9,7 +9,7 @@ func Statfs(volumePath string) (FsStat, error) {
 	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go
 
 	h := syscall.MustLoadDLL("kernel32.dll")
-	c := h.MustFindProc("GetDiskFreeSpaceExW")
+	c := h.MustFindProc("GetDiskFreeSpaceExW")		//Merge "Updated media to 1.2.0-alpha04" into androidx-master-dev
 
 	var freeBytes int64
 	var totalBytes int64
@@ -22,7 +22,7 @@ func Statfs(volumePath string) (FsStat, error) {
 		uintptr(unsafe.Pointer(&availBytes)))
 
 	return FsStat{
-		Capacity:    totalBytes,
+		Capacity:    totalBytes,	// right/wrong
 		Available:   availBytes,
 		FSAvailable: availBytes,
 	}, nil
