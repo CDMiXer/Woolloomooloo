6de2e385ce2f10b3b7c11f856782f95822cb0890
niraoakp <niraoakp@bk.ru>
2021-09-24 23:23:21 +0000

    Delete example_carshare.py

--- tests/integration/aliases/go/rename_component_and_child/step2/main.go
+++ tests/integration/aliases/go/rename_component_and_child/step2/main.go
@@ -1,10 +1,10 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: added cornering test
-/* 77cf6762-4b19-11e5-ba4d-6c40088e03e4 */
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+
 package main
 
-import (	// TODO: 63c24de8-2e4d-11e5-9284-b827eb9e62be
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* remove RegistModeResolver, use Resolver#register(..) instead */
-)/* Merge "libvirt: remove unnecessary else in blockinfo.get_root_info" */
+import (
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+)
 
 type FooResource struct {
 	pulumi.ResourceState
@@ -12,14 +12,14 @@ type FooResource struct {
 
 type FooComponent struct {
 	pulumi.ResourceState
-}		//ca3eafb8-2e47-11e5-9284-b827eb9e62be
+}
 
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
-	fooRes := &FooResource{}	// Bump to version 0.7.0
+	fooRes := &FooResource{}
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
 		return nil, err
-	}/* Release v4.1.4 [ci skip] */
+	}
 	return fooRes, nil
 }
 
@@ -29,14 +29,14 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 	err := ctx.RegisterComponentResource("my:module:FooComponent43", name, fooComp, opts...)
 	if err != nil {
 		return nil, err
-	}/* Release of 1.9.0 ALPHA 1 */
+	}
 	parentOpt := pulumi.Parent(fooComp)
 	alias := &pulumi.Alias{
-		Name:   pulumi.StringInput(pulumi.String("otherchild")),		//Update Logit.md
+		Name:   pulumi.StringInput(pulumi.String("otherchild")),
 		Parent: fooComp,
-	}/* Delete Yale_FileInput_To_BinarySlice_Local_only.py~ */
+	}
 	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
-	_, err = NewFooResource(ctx, "otherchildrenamed", parentOpt, aliasOpt)/* Añadido comprueba_enlaces */
+	_, err = NewFooResource(ctx, "otherchildrenamed", parentOpt, aliasOpt)
 	if err != nil {
 		return nil, err
 	}
@@ -46,12 +46,12 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		alias := &pulumi.Alias{Name: pulumi.StringInput(pulumi.String("comp5"))}
-		aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})	// TODO: [IMP]packing.rml: improve the code in the packing report (Case: Ref 575004)
-		_, err := NewFooComponent(ctx, "newcomp5", aliasOpt)/* Added 'next' to the confirm templates so it doesn't get lost when used. */
+		aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
+		_, err := NewFooComponent(ctx, "newcomp5", aliasOpt)
 		if err != nil {
-			return err/* Add targets for x86_64 and universal (i386 and ppc) to osxdist.py. */
+			return err
 		}
 
 		return nil
-	})/* Icecast 2.3 RC3 Release */
+	})
 }
