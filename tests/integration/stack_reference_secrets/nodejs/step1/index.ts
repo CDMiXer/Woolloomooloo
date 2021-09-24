005a24ac435d9e4db4a1c40dae65e199c70e7f0e
niraoakp <niraoakp@bk.ru>
2021-09-24 12:08:17 +0000

    put LR restriction for generation of 'sån'

--- tests/integration/stack_reference_secrets/nodejs/step1/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step1/index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
-
+	// Automatic changelog generation for PR #13767 [ci skip]
 export const normal = pulumi.output("normal");
-export const secret = pulumi.secret("secret");
+export const secret = pulumi.secret("secret");	// TODO: hacked by souzau@yandex.com
 
