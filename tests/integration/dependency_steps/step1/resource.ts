7a81ad1fac89fcef94153c983cf070ae21b841ae
niraoakp <niraoakp@bk.ru>
2021-09-24 10:48:23 +0000

    Change default layers and scripts loaded when openeing Earth Map

--- tests/integration/dependency_steps/step1/resource.ts
+++ tests/integration/dependency_steps/step1/resource.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Rename justsomelinks.html to just some links.html
 
 import * as pulumi from "@pulumi/pulumi";
 
@@ -6,25 +6,25 @@ let currentID = 0;
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
-
+/* Better variable name. */
     private inject: Error | undefined;
 
-    public async diff(id: pulumi.ID, olds: any, news: any) {
+    public async diff(id: pulumi.ID, olds: any, news: any) {	// TODO: hacked by lexy8russo@outlook.com
         let replaces: string[] = [];
         let deleteBeforeReplace: boolean = false;
         if ((olds as ResourceProps).replace !== (news as ResourceProps).replace) {
             replaces.push("replace");
-        }
-        if ((olds as ResourceProps).replaceDBR !== (news as ResourceProps).replaceDBR) {
+        }	// Update gmt_harmonic.m
+        if ((olds as ResourceProps).replaceDBR !== (news as ResourceProps).replaceDBR) {	// TODO: 13161aa6-2e65-11e5-9284-b827eb9e62be
             replaces.push("replaceDBR");
-            deleteBeforeReplace = true;
+            deleteBeforeReplace = true;/* Running ReleaseApp, updating source code headers */
         }
         return {
-            replaces: replaces,
+            replaces: replaces,		//Fix group detail template: specify post targets
             deleteBeforeReplace: deleteBeforeReplace,
         };
-    }
-
+    }		//Merge branch 'dev' into LP-29118
+/* Portability fix: remo^Cd unused code from smblib */
     public async create(inputs: any) {
         if (this.inject) {
             throw this.inject;
@@ -33,21 +33,21 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
             id: (currentID++).toString(),
             outs: undefined,
         };
-    }
+    }	// TODO: will be fixed by arajasek94@gmail.com
 
-    public async update(id: pulumi.ID, olds: any, news: any) {
-        if (this.inject) {
+    public async update(id: pulumi.ID, olds: any, news: any) {	// TODO: hacked by boringland@protonmail.ch
+        if (this.inject) {/* Added missin ExecutorService thread pool to non blocking client side example */
             throw this.inject;
         }
-        return {};
+        return {};/* Remove unnecessary list copy */
     }
 
     public async delete(id: pulumi.ID, props: any) {
-        if (this.inject) {
+        if (this.inject) {		//Rebuilt index with JoseLVelas
             throw this.inject;
         }
     }
-
+/* Release 4.0.1. */
     // injectFault instructs the provider to inject the given fault upon the next CRUD operation.  Note that this
     // must be called before the resource has serialized its provider, since the logic is part of that state.
     public injectFault(error: Error | undefined): void {
@@ -59,7 +59,7 @@ export class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
-}
+}		//c1321b90-2e5a-11e5-9284-b827eb9e62be
 
 export interface ResourceProps {
     state?: any; // arbitrary state bag that can be updated without replacing.
