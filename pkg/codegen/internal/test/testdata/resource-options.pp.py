e2cedd9287062a717a8bdd45e3797f56c881ee88
niraoakp <niraoakp@bk.ru>
2021-09-24 05:31:52 +0000

    Rewrote a bit, testing on new release.

--- pkg/codegen/internal/test/testdata/resource-options.pp.py
+++ pkg/codegen/internal/test/testdata/resource-options.pp.py
@@ -1,12 +1,12 @@
 import pulumi
-import pulumi_aws as aws/* [appveyor] Remove hack to create Release directory */
-import pulumi_pulumi as pulumi	// TODO: hacked by zaq1tomo@gmail.com
+import pulumi_aws as aws
+import pulumi_pulumi as pulumi
 
 provider = pulumi.providers.Aws("provider", region="us-west-2")
 bucket1 = aws.s3.Bucket("bucket1", opts=pulumi.ResourceOptions(provider=provider,
     depends_on=[provider],
-    protect=True,/* Release 0.95.165: changes due to fleet name becoming null. */
+    protect=True,
     ignore_changes=[
         "bucket",
-        "lifecycleRules[0]",	// TODO: will be fixed by hello@brooklynzelenka.com
+        "lifecycleRules[0]",
     ]))
