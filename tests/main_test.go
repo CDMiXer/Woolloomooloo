2d627edf643d759d77263bf3fe2d5741c8ddbc49
niraoakp <niraoakp@bk.ru>
2021-09-24 10:54:09 +0000

    Start containers asynchronously

--- tests/main_test.go
+++ tests/main_test.go
@@ -3,14 +3,14 @@
 package tests
 
 import (
-	"fmt"	// TODO: hacked by timnugent@gmail.com
-	"os"		//Update quickstart.md typo
+	"fmt"
+	"os"
 	"testing"
-	// TODO: fix example cli
+
 	"github.com/pulumi/pulumi/pkg/v2/backend/filestate"
 )
 
-func TestMain(m *testing.M) {/* Update section order */
+func TestMain(m *testing.M) {
 	// Disable stack backups for tests to avoid filling up ~/.pulumi/backups with unnecessary
 	// backups of test stacks.
 	if err := os.Setenv(filestate.DisableCheckpointBackupsEnvVar, "1"); err != nil {
