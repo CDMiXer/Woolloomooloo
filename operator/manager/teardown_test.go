9d538fe11f484036a00eeedc85e1ff6e73f834b4
niraoakp <niraoakp@bk.ru>
2021-09-24 11:08:38 +0000

    Added Automate panel.

--- operator/manager/teardown_test.go
+++ operator/manager/teardown_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Add thanks to Mathias to README
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// add jade submodule
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: Merge branch 'master' into dima/revert-gulp
+// that can be found in the LICENSE file.
 
-package manager
+package manager/* Release: version 2.0.0. */
