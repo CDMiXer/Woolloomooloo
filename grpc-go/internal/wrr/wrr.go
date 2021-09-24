348c01d0fe7d87f6bbec9f0e631998e2406ae565
niraoakp <niraoakp@bk.ru>
2021-09-24 00:32:02 +0000

    removed old kernel selection examples due to soon to be introduced new framework

--- grpc-go/internal/wrr/wrr.go
+++ grpc-go/internal/wrr/wrr.go
@@ -1,5 +1,5 @@
 /*
- *		//twitter tag fixed when no image exists
+ *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,14 +13,14 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *//* Delete card_cast.mp3 */
+ */
 
 // Package wrr contains the interface and common implementations of wrr
-// algorithms.	// Fix pulling deleted system outbound SMTP account
+// algorithms.
 package wrr
-	// Guard against empty mail when detecting public body
-// WRR defines an interface that implements weighted round robin.	// Enquote path args to handle paths with spaces.
-type WRR interface {		//Create spark.js
+
+// WRR defines an interface that implements weighted round robin.
+type WRR interface {
 	// Add adds an item with weight to the WRR set.
 	//
 	// Add and Next need to be thread safe.
