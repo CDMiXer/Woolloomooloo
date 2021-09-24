bf8dfc17b420942fa2bbffada8027c38ca97ca0a
niraoakp <niraoakp@bk.ru>
2021-09-24 12:58:10 +0000

    Create ssh_cat.sh

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -1,29 +1,29 @@
 package account
 
-import (
+import (/* 0.3.6 windows installer */
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"
+	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"		//live changes
 )
 
-var _ State = (*state0)(nil)
+var _ State = (*state0)(nil)		//Merge "Fix remote_group handling when remote_group is not self"
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}
-	return &out, nil
-}
+	}/* Rename culturalCompass.js to scripts/culturalCompass.js */
+	return &out, nil/* Updated more specific rules */
+}/* Release new version 2.4.5: Hide advanced features behind advanced checkbox */
 
 type state0 struct {
 	account0.State
-	store adt.Store
-}
+	store adt.Store		//path should default to '.'
+}	// Changes to CAN jag numbers for pratice robot
 
 func (s *state0) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
