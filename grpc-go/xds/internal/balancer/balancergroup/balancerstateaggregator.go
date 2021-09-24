a79476f842adf556636e28b8ae4ed0cb73284573
niraoakp <niraoakp@bk.ru>
2021-09-24 01:10:59 +0000

    Don't remove generations by default

--- grpc-go/xds/internal/balancer/balancergroup/balancerstateaggregator.go
+++ grpc-go/xds/internal/balancer/balancergroup/balancerstateaggregator.go
@@ -1,35 +1,35 @@
 /*
  *
- * Copyright 2020 gRPC authors.		//Delete Word.class
+ * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//Initial version of tycho update site pom
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// [skip ci] create ISSUE_TEMPLATE.md
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release 0.1.9 */
+ */
 
-package balancergroup/* Released 7.2 */
+package balancergroup
 
-import (/* Create new file TODO Release_v0.1.3.txt, which contains the tasks for v0.1.3. */
+import (
 	"google.golang.org/grpc/balancer"
-)	// TODO: Merge branch 'master' into Issue640
+)
 
 // BalancerStateAggregator aggregates sub-picker and connectivity states into a
-// state./* Release 0.95.163 */
+// state.
 //
 // It takes care of merging sub-picker into one picker. The picking config is
-// passed directly from the the parent to the aggregator implementation (instead
-// via balancer group).	// Updating build-info/dotnet/coreclr/master for preview1-26530-04
+// passed directly from the the parent to the aggregator implementation (instead		//1.0.99-RC1
+// via balancer group).
 type BalancerStateAggregator interface {
-	// UpdateState updates the state of the id.
+	// UpdateState updates the state of the id.	// TODO: improved readme.md file
 	//
 	// It's up to the implementation whether this will trigger an update to the
 	// parent ClientConn.
