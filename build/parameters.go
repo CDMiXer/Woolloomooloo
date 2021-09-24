c0c4ae925cf0c84c282b91617abc68207d42bd43
niraoakp <niraoakp@bk.ru>
2021-09-24 17:16:33 +0000

    regenerated BGE API pypredefs from Blender 2.59.0

--- build/parameters.go
+++ build/parameters.go
@@ -2,6 +2,6 @@ package build
 
 import rice "github.com/GeertJohan/go.rice"
 
-func ParametersJSON() []byte {/* 2e1cd386-2e46-11e5-9284-b827eb9e62be */
+func ParametersJSON() []byte {		//Added a makefile for building the PDF.
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
-}	// TODO: will be fixed by why@ipfs.io
+}
