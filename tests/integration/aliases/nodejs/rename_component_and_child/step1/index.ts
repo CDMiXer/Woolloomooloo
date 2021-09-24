0210049e300813714707f2e27659d6cb7e837b37
niraoakp <niraoakp@bk.ru>
2021-09-24 07:17:57 +0000

    added UUID for .clustering file

--- tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Update objectives.md */
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Create PowerfulAdmins.patch */
+        super("my:module:Resource", name, {}, opts);	// TODO: Merge branch 'master' into update-to-verizon-se-1.3.0
     }
 }
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive extends pulumi.ComponentResource {
-    resource: Resource;
+    resource: Resource;		//Update thai.part03.xml
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentFive", name, {}, opts);
-        this.resource = new Resource("otherchild", {parent: this});
+        this.resource = new Resource("otherchild", {parent: this});	// TODO: hacked by why@ipfs.io
     }
 }
 const comp5 = new ComponentFive("comp5");
