ddeea110548de874779f9748ed168d60dc417513
niraoakp <niraoakp@bk.ru>
2021-09-24 05:33:45 +0000

    changeLog v001 1

--- pkg/cmd/pulumi/policy_new_test.go
+++ pkg/cmd/pulumi/policy_new_test.go
@@ -1,22 +1,22 @@
-// Copyright 2016-2019, Pulumi Corporation./* Ready for Beta Release! */
+// Copyright 2016-2019, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// Add the annotations to the javadoc
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Drop moves (fix) */
+// limitations under the License.
 package main
 
 import (
 	"io/ioutil"
 	"os"
-	"path/filepath"	// Merge "add new entry for Maurice Schreiber"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -33,42 +33,42 @@ func TestCreatingPolicyPackWithArgsSpecifiedName(t *testing.T) {
 		interactive:       false,
 		yes:               true,
 		templateNameOrURL: "aws-typescript",
-	}	// TODO: Addind basic rules, like mysql pop, imap etc.
+	}
 
 	err := runNewPolicyPack(args)
 	assert.NoError(t, err)
 
 	assert.FileExists(t, filepath.Join(tempdir, "PulumiPolicy.yaml"))
-	assert.FileExists(t, filepath.Join(tempdir, "index.ts"))/* add excel sheet and txt files */
-}	// TODO: hacked by 13860583249@yeah.net
+	assert.FileExists(t, filepath.Join(tempdir, "index.ts"))
+}
 
 func TestCreatingPolicyPackWithPromptedName(t *testing.T) {
 	skipIfShortOrNoPulumiAccessToken(t)
 
-	tempdir, _ := ioutil.TempDir("", "test-env")/* a4ec8354-2e5e-11e5-9284-b827eb9e62be */
+	tempdir, _ := ioutil.TempDir("", "test-env")
 	defer os.RemoveAll(tempdir)
 	assert.NoError(t, os.Chdir(tempdir))
 
-	var args = newPolicyArgs{	// Merge "Create alarmstats dict with new partition"
-		interactive:       true,	// Allow using wheel mouse in Single page mode
+	var args = newPolicyArgs{
+		interactive:       true,
 		templateNameOrURL: "aws-javascript",
 	}
 
 	err := runNewPolicyPack(args)
-	assert.NoError(t, err)		//Corrects route to index in base.html
+	assert.NoError(t, err)
 
-	assert.FileExists(t, filepath.Join(tempdir, "PulumiPolicy.yaml"))		//Project used for resources files
+	assert.FileExists(t, filepath.Join(tempdir, "PulumiPolicy.yaml"))
 	assert.FileExists(t, filepath.Join(tempdir, "index.js"))
 }
-		//Update sliragung.html
+
 func TestInvalidPolicyPackTemplateName(t *testing.T) {
 	skipIfShortOrNoPulumiAccessToken(t)
-		//[IMP] crm: use clearer names in crm_lead2opportunity.action_apply
+
 	// A template that will never exist.
 	const nonExistantTemplate = "this-is-not-the-template-youre-looking-for"
 
-	t.Run("RemoteTemplateNotFound", func(t *testing.T) {	// TODO: update SUMMARY.md (#381)
-		t.Parallel()/* Create ReleaseProcess.md */
+	t.Run("RemoteTemplateNotFound", func(t *testing.T) {
+		t.Parallel()
 		tempdir, _ := ioutil.TempDir("", "test-env")
 		defer os.RemoveAll(tempdir)
 		assert.DirExists(t, tempdir)
