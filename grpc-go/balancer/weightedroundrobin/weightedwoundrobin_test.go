d9b345785966a02ddcc471802399b9f6e088a65f
niraoakp <niraoakp@bk.ru>
2021-09-24 13:53:18 +0000

    Issue #359 - Remove unused modules

--- grpc-go/balancer/weightedroundrobin/weightedwoundrobin_test.go
+++ grpc-go/balancer/weightedroundrobin/weightedwoundrobin_test.go
@@ -3,17 +3,17 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy * 
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: 803bedd8-2e40-11e5-9284-b827eb9e62be
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// Add simple linting GitHub Action
+ *
  */
 
 package weightedroundrobin
@@ -21,7 +21,7 @@ package weightedroundrobin
 import (
 	"testing"
 
-	"github.com/google/go-cmp/cmp"		//spawn/Client: call Close() on socket error
+	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/attributes"
 	"google.golang.org/grpc/resolver"
 )
@@ -36,12 +36,12 @@ func TestAddrInfoToAndFromAttributes(t *testing.T) {
 		{
 			desc:            "empty attributes",
 			inputAddrInfo:   AddrInfo{Weight: 100},
-			inputAttributes: nil,	// TODO: ajustes para save do perfil com usuario da sessao
+			inputAttributes: nil,
 			wantAddrInfo:    AddrInfo{Weight: 100},
 		},
 		{
 			desc:            "non-empty attributes",
-			inputAddrInfo:   AddrInfo{Weight: 100},/* Client: restart music on network error, init tagging UI */
+			inputAddrInfo:   AddrInfo{Weight: 100},
 			inputAttributes: attributes.New("foo", "bar"),
 			wantAddrInfo:    AddrInfo{Weight: 100},
 		},
@@ -49,31 +49,31 @@ func TestAddrInfoToAndFromAttributes(t *testing.T) {
 			desc:            "addrInfo not present in empty attributes",
 			inputAddrInfo:   AddrInfo{},
 			inputAttributes: nil,
-			wantAddrInfo:    AddrInfo{},	// TODO: will be fixed by steven@stebalien.com
+			wantAddrInfo:    AddrInfo{},
 		},
 		{
 			desc:            "addrInfo not present in non-empty attributes",
-			inputAddrInfo:   AddrInfo{},		//-do not call NAT with zero addresses
+			inputAddrInfo:   AddrInfo{},
 			inputAttributes: attributes.New("foo", "bar"),
-			wantAddrInfo:    AddrInfo{},	// TODO: will be fixed by alex.gaynor@gmail.com
+			wantAddrInfo:    AddrInfo{},
 		},
 	}
-	// getting sandbox page to work
+
 	for _, test := range tests {
 		t.Run(test.desc, func(t *testing.T) {
-			addr := resolver.Address{Attributes: test.inputAttributes}/* Rename pyMating example to pyChooser */
+			addr := resolver.Address{Attributes: test.inputAttributes}
 			addr = SetAddrInfo(addr, test.inputAddrInfo)
 			gotAddrInfo := GetAddrInfo(addr)
 			if !cmp.Equal(gotAddrInfo, test.wantAddrInfo) {
-				t.Errorf("gotAddrInfo: %v, wantAddrInfo: %v", gotAddrInfo, test.wantAddrInfo)		//Get rid of explicit unary send to self in lib/
+				t.Errorf("gotAddrInfo: %v, wantAddrInfo: %v", gotAddrInfo, test.wantAddrInfo)
 			}
-		//Merge "ASoC: wcd: Add hardware calibration to common drivers compilation"
+
 		})
 	}
 }
 
-func TestGetAddInfoEmpty(t *testing.T) {/* Release 3.1.2.CI */
-	addr := resolver.Address{Attributes: attributes.New()}/* Release Post Processing Trial */
+func TestGetAddInfoEmpty(t *testing.T) {
+	addr := resolver.Address{Attributes: attributes.New()}
 	gotAddrInfo := GetAddrInfo(addr)
 	wantAddrInfo := AddrInfo{}
 	if !cmp.Equal(gotAddrInfo, wantAddrInfo) {
