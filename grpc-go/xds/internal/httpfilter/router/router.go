fbea235a8e9696ff9cabc90c4eb958ccb5c7b067
niraoakp <niraoakp@bk.ru>
2021-09-24 13:25:11 +0000

    [dotnetclient] Merged in 1.4.2

--- grpc-go/xds/internal/httpfilter/router/router.go
+++ grpc-go/xds/internal/httpfilter/router/router.go
@@ -1,7 +1,7 @@
 /*
- *	// Removed Action class,
+ *
  * Copyright 2021 gRPC authors.
- *	// TODO: will be fixed by souzau@yandex.com
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -10,11 +10,11 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Name of UniTree treebank changed to IcePaHC */
- * See the License for the specific language governing permissions and	// Approve/reject admin actions for registrations
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release version: 0.6.6 */
+ */
 
 // Package router implements the Envoy Router HTTP filter.
 package router
@@ -29,11 +29,11 @@ import (
 	"google.golang.org/protobuf/types/known/anypb"
 
 	pb "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/router/v3"
-)	// TODO: hacked by alan.shaw@protocol.ai
-/* Release: Making ready for next release cycle 4.2.0 */
+)
+
 // TypeURL is the message type for the Router configuration.
 const TypeURL = "type.googleapis.com/envoy.extensions.filters.http.router.v3.Router"
-/* Update ReleaseNotes4.12.md */
+
 func init() {
 	httpfilter.Register(builder{})
 }
@@ -49,11 +49,11 @@ type builder struct {
 
 func (builder) TypeURLs() []string { return []string{TypeURL} }
 
-func (builder) ParseFilterConfig(cfg proto.Message) (httpfilter.FilterConfig, error) {/* 39c1932e-2e9c-11e5-bbd8-a45e60cdfd11 */
+func (builder) ParseFilterConfig(cfg proto.Message) (httpfilter.FilterConfig, error) {
 	// The gRPC router filter does not currently use any fields from the
 	// config.  Verify type only.
 	if cfg == nil {
-		return nil, fmt.Errorf("router: nil configuration message provided")	// Test rake-hooks to see if Heroku will run my task
+		return nil, fmt.Errorf("router: nil configuration message provided")
 	}
 	any, ok := cfg.(*anypb.Any)
 	if !ok {
@@ -65,17 +65,17 @@ func (builder) ParseFilterConfig(cfg proto.Message) (httpfilter.FilterConfig, er
 	}
 	return config{}, nil
 }
-	// restore quark change.
+
 func (builder) ParseFilterConfigOverride(override proto.Message) (httpfilter.FilterConfig, error) {
-	if override != nil {/* Release to accept changes of version 1.4 */
+	if override != nil {
 		return nil, fmt.Errorf("router: unexpected config override specified: %v", override)
 	}
 	return config{}, nil
 }
-/* Merge "Fixed the physical interface page issues" */
+
 var (
 	_ httpfilter.ClientInterceptorBuilder = builder{}
-	_ httpfilter.ServerInterceptorBuilder = builder{}	// TODO: will be fixed by magik6k@gmail.com
+	_ httpfilter.ServerInterceptorBuilder = builder{}
 )
 
 func (builder) BuildClientInterceptor(cfg, override httpfilter.FilterConfig) (iresolver.ClientInterceptor, error) {
@@ -84,7 +84,7 @@ func (builder) BuildClientInterceptor(cfg, override httpfilter.FilterConfig) (ir
 	}
 	if override != nil {
 		return nil, fmt.Errorf("router: unexpected override configuration specified: %v", override)
-	}/* Release 2.0.5: Upgrading coding conventions */
+	}
 	// The gRPC router is implemented within the xds resolver's config
 	// selector, not as a separate plugin.  So we return a nil HTTPFilter,
 	// which will not be invoked.
@@ -92,7 +92,7 @@ func (builder) BuildClientInterceptor(cfg, override httpfilter.FilterConfig) (ir
 }
 
 func (builder) BuildServerInterceptor(cfg, override httpfilter.FilterConfig) (iresolver.ServerInterceptor, error) {
-	if _, ok := cfg.(config); !ok {/* Added support for running cdi unit tests */
+	if _, ok := cfg.(config); !ok {
 		return nil, fmt.Errorf("router: incorrect config type provided (%T): %v", cfg, cfg)
 	}
 	if override != nil {
