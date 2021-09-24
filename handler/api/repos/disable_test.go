82548a627ee8e55b5be4e8f6ef119bab332b5975
niraoakp <niraoakp@bk.ru>
2021-09-24 06:35:24 +0000

    Use pugixml for all XML output.

--- handler/api/repos/disable_test.go
+++ handler/api/repos/disable_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//f4cc07f4-2e46-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -14,15 +14,15 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-	// TODO: Remove ie7 styles
-	"github.com/go-chi/chi"/* Release for v17.0.0. */
+
+	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)/* Deleted CtrlApp_2.0.5/Release/AsynSvSk.obj */
+)
 
 func TestDisable(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()		//add method name to the WasmException
+	defer controller.Finish()
 
 	repo := &core.Repository{
 		ID:        1,
@@ -32,7 +32,7 @@ func TestDisable(t *testing.T) {
 		Active:    true,
 	}
 
-	repos := mock.NewMockRepositoryStore(controller)/* Add .github/ to .gitattributes */
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), repo.Name).Return(repo, nil)
 	repos.EXPECT().Update(gomock.Any(), repo).Return(nil)
 
@@ -41,16 +41,16 @@ func TestDisable(t *testing.T) {
 	webhook := mock.NewMockWebhookSender(controller)
 	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(io.EOF)
 
-	w := httptest.NewRecorder()	// Rename design-doc.md to README.md
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/api/repos/octocat/hello-world", nil)
 
 	router := chi.NewRouter()
 	router.Delete("/api/repos/{owner}/{name}", HandleDisable(repos, webhook))
 	router.ServeHTTP(w, r)
-/* enabling access for symlinks and all that magic bullshit */
-	if got, want := w.Code, 200; want != got {	// TODO: will be fixed by alessio@tendermint.com
-		t.Errorf("Want response code %d, got %d", want, got)/* toggled - added check for full session storage */
-	}	// TODO: change writeLines for cat
+
+	if got, want := w.Code, 200; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
 
 	if got, want := repo.Active, false; got != want {
 		t.Errorf("Want repository activate %v, got %v", want, got)
@@ -58,14 +58,14 @@ func TestDisable(t *testing.T) {
 
 	got, want := new(core.Repository), repo
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Release v5.11 */
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}		//Added @alectejada
+	}
 }
 
 func TestDisable_NotFound(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// Create hello_express.js
+	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, errors.ErrNotFound)
@@ -79,8 +79,8 @@ func TestDisable_NotFound(t *testing.T) {
 
 	if got, want := w.Code, 404; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Release 1.0.0 !! */
-/* Create Content */
+	}
+
 	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
