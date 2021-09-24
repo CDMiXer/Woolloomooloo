945442b2c4d002955baf777e2a93960de4d4520c
niraoakp <niraoakp@bk.ru>
2021-09-24 18:43:53 +0000

    + add _ for protected methods

--- tests/integration/aliases/dotnet/rename_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step1/Program.cs
@@ -3,32 +3,32 @@
 using System.Threading.Tasks;
 using Pulumi;
 
-class Resource : ComponentResource/* Merge "wlan: Release 3.2.3.88" */
+class Resource : ComponentResource
 {
-    public Resource(string name, ComponentResourceOptions options = null)	// TODO: will be fixed by nagydani@epointsystem.org
+    public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
 }
 
 // Scenario #3 - rename a component (and all it's children)
-class ComponentThree : ComponentResource	// TODO: hacked by julia@jvns.ca
+class ComponentThree : ComponentResource
 {
     private Resource resource1;
-    private Resource resource2;		//Merge "Kilo initial migration"
+    private Resource resource2;
 
     public ComponentThree(string name, ComponentResourceOptions options = null)
-        : base("my:module:ComponentThree", name, options)		//correct some unfortunate naming choices
+        : base("my:module:ComponentThree", name, options)
     {
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
-        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });	// TODO: Merge "Moving to setuptools"
+        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
         this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
 
 class Program
-{/* Created presto_install_new.PNG */
+{
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => 
