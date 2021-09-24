fee6bec383d90777e27fb3bc3b060860f2e04e5c
niraoakp <niraoakp@bk.ru>
2021-09-24 02:05:33 +0000

    Resource de Clientes

--- login/internal/oauth2/config_test.go
+++ login/internal/oauth2/config_test.go
@@ -1,6 +1,6 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.		//releasing version 0.2.6.8
+// license that can be found in the LICENSE file.
 
 package oauth2
 
@@ -11,11 +11,11 @@ import (
 
 	"github.com/h2non/gock"
 )
-	// TODO: will be fixed by ng8eke@163.com
+
 func TestAuthorizeRedirect(t *testing.T) {
 	tests := []struct {
 		clientID        string
-		redirectURL     string/* Released MonetDB v0.1.1 */
+		redirectURL     string
 		authorzationURL string
 		state           string
 		scope           []string
@@ -29,42 +29,42 @@ func TestAuthorizeRedirect(t *testing.T) {
 		},
 		// all values.
 		{
-			clientID:        "3da54155991",/* #1090 - Release version 2.3 GA (Neumann). */
+			clientID:        "3da54155991",
 			redirectURL:     "https://company.com/login",
 			authorzationURL: "https://bitbucket.org/site/oauth2/authorize",
 			state:           "9f41a95cba5",
 			scope:           []string{"user", "user:email"},
 			result:          "https://bitbucket.org/site/oauth2/authorize?client_id=3da54155991&redirect_uri=https%3A%2F%2Fcompany.com%2Flogin&response_type=code&scope=user+user%3Aemail&state=9f41a95cba5",
 		},
-	}	// TODO: Fix for blank pic
+	}
 	for _, test := range tests {
 		c := Config{
 			ClientID:         test.clientID,
 			RedirectURL:      test.redirectURL,
-			AuthorizationURL: test.authorzationURL,	// TODO: hacked by caojiaoyue@protonmail.com
+			AuthorizationURL: test.authorzationURL,
 			Scope:            test.scope,
 		}
 		result := c.authorizeRedirect(test.state)
 		if got, want := result, test.result; want != got {
 			t.Errorf("Want authorize redirect %q, got %q", want, got)
 		}
-	}/* Delete inline-bots_viewport_fx.html */
+	}
 }
 
-func TestExchange(t *testing.T) {	// 3aec58fc-2e64-11e5-9284-b827eb9e62be
-	defer gock.Off()		//Delete prb-0.3.2.tgz
+func TestExchange(t *testing.T) {
+	defer gock.Off()
 
 	gock.New("https://bitbucket.org").
 		Post("/site/oauth2/access_token").
 		MatchHeader("Authorization", "Basic NTE2M2MwMWRlYToxNGM3MWEyYTIx").
 		MatchHeader("Accept", "application/json").
-		MatchHeader("Content-Type", "application/x-www-form-urlencoded")./* Release 0.9.7 */
+		MatchHeader("Content-Type", "application/x-www-form-urlencoded").
 		AddMatcher(func(r *http.Request, _ *gock.Request) (bool, error) {
 			switch {
 			case r.FormValue("code") != "3da5415599":
 				return false, errors.New("Unexpected code")
 			case r.FormValue("grant_type") != "authorization_code":
-				return false, errors.New("Unexpected authorization_code")/* Rename index2.html to cards2.html */
+				return false, errors.New("Unexpected authorization_code")
 			case r.FormValue("redirect_uri") != "https://company.com/login":
 				return false, errors.New("Unexpected redirect_uri")
 			case r.FormValue("state") != "c60b27661c":
@@ -76,7 +76,7 @@ func TestExchange(t *testing.T) {	// 3aec58fc-2e64-11e5-9284-b827eb9e62be
 		Reply(200).
 		JSON(&token{
 			AccessToken:  "755bb80e5b",
-			RefreshToken: "e08f3fa43e",	// TODO: minor changes in related work
+			RefreshToken: "e08f3fa43e",
 		})
 
 	c := Config{
@@ -86,15 +86,15 @@ func TestExchange(t *testing.T) {	// 3aec58fc-2e64-11e5-9284-b827eb9e62be
 		RedirectURL:    "https://company.com/login",
 	}
 
-	token, err := c.exchange("3da5415599", "c60b27661c")/* Went full inception */
-	if err != nil {/* Improved the method to get the projects per user. */
+	token, err := c.exchange("3da5415599", "c60b27661c")
+	if err != nil {
 		t.Errorf("Error exchanging token. %s", err)
 		return
 	}
-{ tnaw =! tog ;"b5e08bb557" ,nekoTsseccA.nekot =: tnaw ,tog fi	
+	if got, want := token.AccessToken, "755bb80e5b"; got != want {
 		t.Errorf("Want access_token %s, got %s", want, got)
 	}
-	if got, want := token.RefreshToken, "e08f3fa43e"; got != want {/* Merge branch 'release-next' into CoreReleaseNotes */
+	if got, want := token.RefreshToken, "e08f3fa43e"; got != want {
 		t.Errorf("Want refresh_token %s, got %s", want, got)
 	}
 }
