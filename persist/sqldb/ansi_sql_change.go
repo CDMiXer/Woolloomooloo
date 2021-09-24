d287705f2ced1b77cbd9a1c0049aeff4125cde7c
niraoakp <niraoakp@bk.ru>
2021-09-24 11:01:13 +0000

    first step migration using LocalDateTime internally

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -1,11 +1,11 @@
-package sqldb
+package sqldb/* Merge "Remove NovaConsoleauth Service" */
 
 import "upper.io/db.v3/lib/sqlbuilder"
-		//Migrating pre-existing Max patches to Java backend.
-// represent a straight forward change that is compatible with all database providers		//Update chankro.py
-type ansiSQLChange string
 
+// represent a straight forward change that is compatible with all database providers
+type ansiSQLChange string/* Release of eeacms/www:18.2.19 */
+/* 3fe290bd-2d5c-11e5-9493-b88d120fff5e */
 func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
-	_, err := session.Exec(string(s))	// TODO: Merge branch 'master' into up-monasca-mysql-init-153
+	_, err := session.Exec(string(s))
 	return err
 }
