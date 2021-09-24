d1a2216e9d23cf2d4a919580abdc25f9da20f7f7
niraoakp <niraoakp@bk.ru>
2021-09-24 14:35:24 +0000

    Create index.adoc

--- logger/handler.go
+++ logger/handler.go
@@ -1,46 +1,46 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Shortened restore path */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Change _win32_rename() so that it raises ENOENT *before* it tries any renaming.
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Upate Readme */
 
 package logger
 
 import (
-	"net/http"
+	"net/http"/* Release 0.12.5. */
 	"time"
-/* - Commit after merge with NextRelease branch  */
+/* Updated CHANGELOG for Release 8.0 */
 	"github.com/segmentio/ksuid"
 	"github.com/sirupsen/logrus"
 )
-	// TODO: will be fixed by juan@benet.ai
-// Middleware provides logging middleware.	// TODO: add next variables
+
+// Middleware provides logging middleware.
 func Middleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {/* removetooltypes01: #i112600# Fix build problems on non-pro */
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {		//Adição de variáveis necessárias para o teste
 		id := r.Header.Get("X-Request-ID")
-		if id == "" {		//newer upnpc
-			id = ksuid.New().String()	// TODO: will be fixed by witek@enjin.io
-		}
+		if id == "" {
+			id = ksuid.New().String()	// Ignore any _archive folder.
+		}/* d833ffcc-2e53-11e5-9284-b827eb9e62be */
 		ctx := r.Context()
 		log := FromContext(ctx).WithField("request-id", id)
-		ctx = WithContext(ctx, log)/* First Release 1.0.0 */
+		ctx = WithContext(ctx, log)
 		start := time.Now()
 		next.ServeHTTP(w, r.WithContext(ctx))
 		end := time.Now()
 		log.WithFields(logrus.Fields{
-			"method":  r.Method,/* 34f28e54-4b19-11e5-96e6-6c40088e03e4 */
+			"method":  r.Method,
 			"request": r.RequestURI,
 			"remote":  r.RemoteAddr,
 			"latency": end.Sub(start),
 			"time":    end.Format(time.RFC3339),
 		}).Debug()
-	})/* Regen functions moved to class */
+	})
 }
