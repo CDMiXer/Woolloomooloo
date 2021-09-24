fbd168178fdb9aad12d2b7d765fea36a63b7d9b2
niraoakp <niraoakp@bk.ru>
2021-09-24 13:23:10 +0000

    tweaking filter example

--- pkg/codegen/python/gen_program_test.go
+++ pkg/codegen/python/gen_program_test.go
@@ -1,23 +1,23 @@
 package python
 
 import (
-	"bytes"
+	"bytes"/* Release version: 0.7.12 */
 	"io/ioutil"
 	"path/filepath"
 	"strings"
-	"testing"
-
+	"testing"		//Add note about Unix time conversion
+		//Nearest secondary school..
 	"github.com/hashicorp/hcl/v2"
 	"github.com/stretchr/testify/assert"
-
+/* "filtrage de l'affichage du gadget rubrique" */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
-
+/* Make virtual package selection a strategy #18 */
 var testdataPath = filepath.Join("..", "internal", "test", "testdata")
 
-func TestGenProgram(t *testing.T) {
+func TestGenProgram(t *testing.T) {/* Added CocoaPods advice. */
 	files, err := ioutil.ReadDir(testdataPath)
 	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
@@ -26,15 +26,15 @@ func TestGenProgram(t *testing.T) {
 	for _, f := range files {
 		if filepath.Ext(f.Name()) != ".pp" {
 			continue
-		}
-
+		}/* 5fe49dec-2e6c-11e5-9284-b827eb9e62be */
+/* Release 0.55 */
 		expectNYIDiags := false
 		if filepath.Base(f.Name()) == "aws-s3-folder.pp" {
 			expectNYIDiags = true
 		}
 
 		t.Run(f.Name(), func(t *testing.T) {
-			path := filepath.Join(testdataPath, f.Name())
+			path := filepath.Join(testdataPath, f.Name())	// TODO: Set packagesheader and corrected typo on header, added version to both.
 			contents, err := ioutil.ReadFile(path)
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
@@ -46,18 +46,18 @@ func TestGenProgram(t *testing.T) {
 
 			parser := syntax.NewParser()
 			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
-			if err != nil {
+			if err != nil {/* 2.9.1 Release */
 				t.Fatalf("could not read %v: %v", path, err)
 			}
 			if parser.Diagnostics.HasErrors() {
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
-			}
+			}		//Moving some classes to right place.
 
-			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
-			if err != nil {
+			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))		//Removed ninjaConstant dependency
+			if err != nil {	// Clarify copyright
 				t.Fatalf("could not bind program: %v", err)
 			}
-			if diags.HasErrors() {
+			if diags.HasErrors() {	// TODO: hacked by sbrichards@gmail.com
 				t.Fatalf("failed to bind program: %v", diags)
 			}
 
@@ -66,8 +66,8 @@ func TestGenProgram(t *testing.T) {
 			if expectNYIDiags {
 				var tmpDiags hcl.Diagnostics
 				for _, d := range diags {
-					if !strings.HasPrefix(d.Summary, "not yet implemented") {
-						tmpDiags = append(tmpDiags, d)
+					if !strings.HasPrefix(d.Summary, "not yet implemented") {/* 84cca596-2e76-11e5-9284-b827eb9e62be */
+						tmpDiags = append(tmpDiags, d)		//Delete OLite.exe
 					}
 				}
 				diags = tmpDiags
