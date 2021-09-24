94603a878e18402179bfd1b092ffdd0299df4439
niraoakp <niraoakp@bk.ru>
2021-09-24 11:24:33 +0000

    Add json format util.

--- handler/api/repos/builds/purge_oss.go
+++ handler/api/repos/builds/purge_oss.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "llewczynski | #133 | Split modules into osgi and non-osgi modules" */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* [artifactory-release] Release version 0.8.0.M1 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,17 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss/* chore(deps): update dependency @types/react-native to v0.55.16 */
-/* Merge "Release 1.0.0.197 QCACLD WLAN Driver" */
+// +build oss
+
 package builds
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"		//Update finetune gbm.md
-)		//Add Sanity as sponsor
+	"github.com/drone/drone/core"
+)
 
-// HandlePurge returns a non-op http.HandlerFunc.	// TODO: Update app_cfg.h
+// HandlePurge returns a non-op http.HandlerFunc.
 func HandlePurge(core.RepositoryStore, core.BuildStore) http.HandlerFunc {
-	return notImplemented		//Update 12_blocks.rb
+	return notImplemented
 }
