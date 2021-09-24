f7b6386d7b0a54749ca81218b2734fce45fd08ed
niraoakp <niraoakp@bk.ru>
2021-09-24 11:16:57 +0000

    Fix warnings and a syntax error

--- tests/integration/config_basic/python_venv/__main__.py
+++ tests/integration/config_basic/python_venv/__main__.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release of eeacms/eprtr-frontend:0.3-beta.11 */
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import pulumi
 
@@ -6,7 +6,7 @@ import pulumi
 config = pulumi.Config('config_basic_py')
 
 # This value is plaintext and doesn't require encryption.
-value = config.require('aConfigValue')/* ErrorLabeller: Use an element's name if no label is set */
+value = config.require('aConfigValue')
 assert value == 'this value is a Pythonic value'
 
 # This value is a secret and is encrypted using the passphrase `supersecret`.
@@ -24,24 +24,24 @@ test_data = [
         'expected_json': '["a","b","c","super secret name"]',
         'expected_object': ['a', 'b', 'c', 'super secret name']
     },
-    {/* Release v0.0.1-3. */
+    {
         'key': 'servers',
         'expected_json': '[{"host":"example","port":80}]',
-        'expected_object': [{ 'host': 'example', 'port': 80 }]/* 3b763aa6-2e6c-11e5-9284-b827eb9e62be */
+        'expected_object': [{ 'host': 'example', 'port': 80 }]
     },
     {
         'key': 'a',
         'expected_json': '{"b":[{"c":true},{"c":false}]}',
         'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }
     },
-    {	// Font Awesome and Angular support.
+    {
         'key': 'tokens',
         'expected_json': '["shh"]',
         'expected_object': ['shh']
     },
-    {	// TODO: will be fixed by peterke@gmail.com
-        'key': 'foo',	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-        'expected_json': '{"bar":"don\'t tell"}',		//[FIX] survey: report not stored on filesystem
+    {
+        'key': 'foo',
+        'expected_json': '{"bar":"don\'t tell"}',
         'expected_object': { 'bar': "don't tell" }
     }
 ]
@@ -50,4 +50,4 @@ for test in test_data:
     json = config.require(test['key'])
     obj = config.require_object(test['key'])
     assert json == test['expected_json']
-    assert obj == test['expected_object']	// docs/configuration.txt: document nickname, no_storage, readonly_storage
+    assert obj == test['expected_object']
