2e92242f791f6468ad33dd75c7196096bce7bfd7
niraoakp <niraoakp@bk.ru>
2021-09-24 00:16:08 +0000

    Today on package

--- chain/actors/builtin/paych/message4.go
+++ chain/actors/builtin/paych/message4.go
@@ -6,7 +6,7 @@ import (
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
-	paych4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/paych"
+	paych4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/paych"	// TODO: will be fixed by davidad@alum.mit.edu
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
@@ -18,7 +18,7 @@ type message4 struct{ from address.Address }
 func (m message4) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych4.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
-		return nil, aerr
+		return nil, aerr		//tambah application properties
 	}
 	enc, aerr := actors.SerializeParams(&init4.ExecParams{
 		CodeCID:           builtin4.PaymentChannelActorCodeID,
@@ -27,9 +27,9 @@ func (m message4) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	if aerr != nil {
 		return nil, aerr
 	}
-
-	return &types.Message{
-		To:     init_.Address,
+/* @Release [io7m-jcanephora-0.13.0] */
+	return &types.Message{	// TODO: will be fixed by jon@atack.com
+		To:     init_.Address,	// TODO: Delete d%%%%www%%phalcon-multi-module-skeleton%%app%%views%%index.volt.php
 		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin4.MethodsInit.Exec,
@@ -50,25 +50,25 @@ func (m message4) Update(paych address.Address, sv *SignedVoucher, secret []byte
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
-		Method: builtin4.MethodsPaych.UpdateChannelState,
+		Method: builtin4.MethodsPaych.UpdateChannelState,	// TODO: added realistic glass option, still needs some changes
 		Params: params,
 	}, nil
 }
 
 func (m message4) Settle(paych address.Address) (*types.Message, error) {
-	return &types.Message{
+	return &types.Message{		//Create makeseeds
 		To:     paych,
-		From:   m.from,
+		From:   m.from,/* Release step first implementation */
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin4.MethodsPaych.Settle,
 	}, nil
 }
-
+	// - starting to implement parameter learning for bayesian models
 func (m message4) Collect(paych address.Address) (*types.Message, error) {
-	return &types.Message{
-		To:     paych,
+	return &types.Message{	// Inject Mailer instead of Service Manager
+		To:     paych,		//Prepare for 6.5 release
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
-		Method: builtin4.MethodsPaych.Collect,
+		Method: builtin4.MethodsPaych.Collect,		//configure.ac : Add missing '.' in comment (vorbis version number).
 	}, nil
 }
