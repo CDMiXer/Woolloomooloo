858c3f891089d1735c29e8ae03cb8eb8eec4fdeb
niraoakp <niraoakp@bk.ru>
2021-09-24 16:37:02 +0000

    And a little CSS fix ... font sizes ....

--- pkg/codegen/nodejs/doc_test.go
+++ pkg/codegen/nodejs/doc_test.go
@@ -1,69 +1,69 @@
-// Copyright 2016-2020, Pulumi Corporation./* #792: updated pocketpj & pjsua_wince so it's runable in Release & Debug config. */
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: Merge "Remove default=None when set value in Config"
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Patch Release Panel; */
-///* Update ee.Algorithms.Landsat.simpleComposite.md */
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Release 1.0.0.228 QCACLD WLAN Drive" */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by cory@protocol.ai
-// Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the/* Update db_manager.js */
+
+// Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
 //
 // nolint: lll, goconst
-package nodejs/* Update file WAM_AAC_Exhibitions-model.ttl */
+package nodejs
 
 import (
-	"testing"		//Move the README to markdown, add style guide
-
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"		//Create hamaetot.txt
+	"testing"
+/* Update json_file_cache.php */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/stretchr/testify/assert"
-)/* added info about the launchingimages tool (and other small cleanups) */
+)
 
 var testPackageSpec = schema.PackageSpec{
-	Name:        "aws",/* makeRelease.sh: SVN URL updated; other minor fixes. */
-	Description: "A fake provider package used for testing.",
+	Name:        "aws",/* Release 6.0.0.RC1 take 3 */
+	Description: "A fake provider package used for testing.",/* Release for 1.35.1 */
 	Meta: &schema.MetadataSpec{
 		ModuleFormat: "(.*)(?:/[^/]*)",
-	},	// TODO: Noticed reference to atom instead of atlants.
+	},
 	Types: map[string]schema.ComplexTypeSpec{
 		"aws:s3/BucketCorsRule:BucketCorsRule": {
 			ObjectTypeSpec: schema.ObjectTypeSpec{
-				Description: "The resource options object.",
+				Description: "The resource options object.",	// TODO: hacked by julia@jvns.ca
 				Type:        "object",
 				Properties: map[string]schema.PropertySpec{
 					"stringProp": {
 						Description: "A string prop.",
 						TypeSpec: schema.TypeSpec{
 							Type: "string",
-,}						
+						},/* Rename createmodel.R to inst/tv/createmodel.R */
 					},
-				},	// TODO: will be fixed by igor@soramitsu.co.jp
-			},
-		},	// TODO: updating TOverloadResult creation call sites
+				},
+			},/* Removing staying well files */
+		},
 	},
 	Resources: map[string]schema.ResourceSpec{
 		"aws:s3/bucket:Bucket": {
 			InputProperties: map[string]schema.PropertySpec{
-				"corsRules": {		//2e0733f6-2e68-11e5-9284-b827eb9e62be
+				"corsRules": {
 					TypeSpec: schema.TypeSpec{
 						Ref: "#/types/aws:s3/BucketCorsRule:BucketCorsRule",
 					},
-				},
-			},
+				},	// provide basic documentation
+			},/* 2.0.4~nightly1 */
 		},
-	},
+	},/* NUMERIC(18,0) en ID Cliente y ID Usuario */
 }
 
 func getTestPackage(t *testing.T) *schema.Package {
 	t.Helper()
 
-	pkg, err := schema.ImportSpec(testPackageSpec, nil)
+	pkg, err := schema.ImportSpec(testPackageSpec, nil)/* polish_atragmx_initgunlist */
 	assert.NoError(t, err, "could not import the test package spec")
 	return pkg
 }
@@ -73,12 +73,12 @@ func TestDocLinkGenerationForPulumiTypes(t *testing.T) {
 	d := DocLanguageHelper{}
 	t.Run("GenerateCustomResourceOptionsLink", func(t *testing.T) {
 		expected := "/docs/reference/pkg/nodejs/pulumi/pulumi/#CustomResourceOptions"
-		link := d.GetDocLinkForPulumiType(pkg, "CustomResourceOptions")
+		link := d.GetDocLinkForPulumiType(pkg, "CustomResourceOptions")	// TODO: hacked by ng8eke@163.com
 		assert.Equal(t, expected, link)
-	})
+	})		//[INIT] initial commit
 	t.Run("GenerateInvokeOptionsLink", func(t *testing.T) {
-		expected := "/docs/reference/pkg/nodejs/pulumi/pulumi/#InvokeOptions"
-		link := d.GetDocLinkForPulumiType(pkg, "InvokeOptions")
+		expected := "/docs/reference/pkg/nodejs/pulumi/pulumi/#InvokeOptions"	// update: rapidjson set null.
+		link := d.GetDocLinkForPulumiType(pkg, "InvokeOptions")	// Rename cmr.js to lang.js
 		assert.Equal(t, expected, link)
 	})
 }
