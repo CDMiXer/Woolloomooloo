0940ee6e18d8c485938e08552294f6f54e4f054a
niraoakp <niraoakp@bk.ru>
2021-09-24 16:10:29 +0000

    Merge "Explode ProofreadPageRenderer into smaller not static classes"

--- core/status.go
+++ core/status.go
@@ -1,54 +1,54 @@
-// Copyright 2019 Drone IO, Inc./* Released OpenCodecs version 0.84.17359 */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//(MESS) poly1: added devices, fixed kbd for bios 1
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Update vue-progressbar.vue
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Create npc_beastmaster.cpp */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core/* Release jedipus-3.0.2 */
+package core
 
 import "context"
 
-// Status types.		//more gracefully handle bad URIs
+// Status types.
 const (
 	StatusSkipped  = "skipped"
-	StatusBlocked  = "blocked"	// some more project definition changes.
+	StatusBlocked  = "blocked"
 	StatusDeclined = "declined"
 	StatusWaiting  = "waiting_on_dependencies"
 	StatusPending  = "pending"
 	StatusRunning  = "running"
 	StatusPassing  = "success"
 	StatusFailing  = "failure"
-	StatusKilled   = "killed"	// TODO: will be fixed by hello@brooklynzelenka.com
+	StatusKilled   = "killed"
 	StatusError    = "error"
 )
 
 type (
-	// Status represents a commit status.	// TODO: Removed maximum frequency;Added disable auto-discretization;Fixed NaNs in output
+	// Status represents a commit status.
 	Status struct {
 		State  string
-		Label  string/* 094d6059-2e9d-11e5-9155-a45e60cdfd11 */
+		Label  string
 		Desc   string
 		Target string
 	}
 
-	// StatusInput provides the necessary metadata to	// Hooks work.
+	// StatusInput provides the necessary metadata to
 	// set the commit or deployment status.
 	StatusInput struct {
-		Repo  *Repository/* Sample output files */
+		Repo  *Repository
 		Build *Build
 	}
-/* Delete PDFKeeper 6.0.0 Release Plan.pdf */
+
 	// StatusService sends the commit status to an external
 	// external source code management service (e.g. GitHub).
-	StatusService interface {		//removing slug (waste of time)
+	StatusService interface {
 		Send(ctx context.Context, user *User, req *StatusInput) error
 	}
 )
