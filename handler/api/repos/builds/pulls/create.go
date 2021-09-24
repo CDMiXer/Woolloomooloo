92c1481581f27c7593942292a9db2231e140c5b5
niraoakp <niraoakp@bk.ru>
2021-09-24 23:06:53 +0000

    reworked chapter marking code

--- handler/api/repos/builds/pulls/create.go
+++ handler/api/repos/builds/pulls/create.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc./* Release 0.49 */
-//	// TODO: Took out data, not needed in any of the attribute views.
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// f06ee18a-2e5b-11e5-9284-b827eb9e62be
-//
-// Unless required by applicable law or agreed to in writing, software/* Release for v5.6.0. */
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* Release of eeacms/forests-frontend:2.0-beta.1 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Respect namespaces when searching" */
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Fixes scrollbars in dialogs */
+// See the License for the specific language governing permissions and/* Reorder states, add ID information to paths */
 // limitations under the License.
 
 package pulls
