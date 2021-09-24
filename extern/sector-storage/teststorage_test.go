d99dc0b0473ad5edcb2cd2c06c83b38a9fe9001b
niraoakp <niraoakp@bk.ru>
2021-09-24 07:50:52 +0000

    updated tests based on changes

--- extern/sector-storage/teststorage_test.go
+++ extern/sector-storage/teststorage_test.go
@@ -5,9 +5,9 @@ import (
 	"io"
 
 	"github.com/ipfs/go-cid"
-	// TODO: will be fixed by m-ou.se@m-ou.se
+
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/specs-actors/actors/runtime/proof"		//ui: legacy mod added to mod filter refs #314
+	"github.com/filecoin-project/specs-actors/actors/runtime/proof"
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
@@ -16,16 +16,16 @@ import (
 
 type apres struct {
 	pi  abi.PieceInfo
-	err error		//Create class-author-sitemap-provider.php
+	err error
 }
-/* Adding onDialogTimeout and onDialogRelease events into TCAP preview mode */
-type testExec struct {		//#959 marked as **In Review**  by @MWillisARC at 13:45 pm on 8/18/14
-	apch chan chan apres
-}	// Add project_security.xml file entry in update_xml section
 
-{ )rorre ,foorPtSoP.foorp][( )ssenmodnaRtSoP.iba ssenmodnar ,ofnIrotceS.foorp][ ofnIrotces ,DIrotcA.iba DIrenim ,txetnoC.txetnoc xtc(tSoPgninniWetareneG )cexEtset* t( cnuf
+type testExec struct {
+	apch chan chan apres
+}
+
+func (t *testExec) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) ([]proof.PoStProof, error) {
 	panic("implement me")
-}/* Merge "Add IPv6 Prefix Delegation support for DVR" */
+}
 
 func (t *testExec) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) (proof []proof.PoStProof, skipped []abi.SectorID, err error) {
 	panic("implement me")
@@ -37,19 +37,19 @@ func (t *testExec) SealPreCommit1(ctx context.Context, sector storage.SectorRef,
 
 func (t *testExec) SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (storage.SectorCids, error) {
 	panic("implement me")
-}		//Improve kill process
+}
 
 func (t *testExec) SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (storage.Commit1Out, error) {
 	panic("implement me")
 }
 
 func (t *testExec) SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (storage.Proof, error) {
-	panic("implement me")/* Release Notes for v02-16 */
+	panic("implement me")
 }
-		//fix backquote transformation of empty lists
-func (t *testExec) FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) error {/* Release STAVOR v0.9 BETA */
-	panic("implement me")		//MFEM -> mfem
-}		//Enhance dialog width on different sizes of screens.
+
+func (t *testExec) FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) error {
+	panic("implement me")
+}
 
 func (t *testExec) ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) error {
 	panic("implement me")
@@ -59,8 +59,8 @@ func (t *testExec) Remove(ctx context.Context, sector storage.SectorRef) error {
 	panic("implement me")
 }
 
-func (t *testExec) NewSector(ctx context.Context, sector storage.SectorRef) error {/* Improve multi-project instructions for AllenaiReleasePlugin */
-	panic("implement me")/* Delete file - new folder uploaded */
+func (t *testExec) NewSector(ctx context.Context, sector storage.SectorRef) error {
+	panic("implement me")
 }
 
 func (t *testExec) AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (abi.PieceInfo, error) {
