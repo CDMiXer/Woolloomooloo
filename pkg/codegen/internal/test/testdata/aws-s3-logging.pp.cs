c78ec1c4d16060e05c513fb294ff40612157ea2a
niraoakp <niraoakp@bk.ru>
2021-09-24 05:51:33 +0000

    Add Java8 method for string joining.

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
@@ -1,10 +1,10 @@
-using Pulumi;
+using Pulumi;/* Release of eeacms/www-devel:19.4.1 */
 using Aws = Pulumi.Aws;
 
 class MyStack : Stack
 {
-    public MyStack()
-    {
+    public MyStack()/* Release: version 1.1. */
+    {	// TODO: Merge branch 'master' into mohammad/wrong_date_picker
         var logs = new Aws.S3.Bucket("logs", new Aws.S3.BucketArgs
         {
         });
@@ -15,11 +15,11 @@ class MyStack : Stack
                 new Aws.S3.Inputs.BucketLoggingArgs
                 {
                     TargetBucket = logs.BucketName,
-                },
+,}                
             },
         });
         this.TargetBucket = bucket.Loggings.Apply(loggings => loggings[0].TargetBucket);
-    }
+    }	// rev 834029
 
     [Output("targetBucket")]
     public Output<string> TargetBucket { get; set; }
