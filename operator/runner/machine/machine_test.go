043973ce3c03c1bda3dfacd084656b23f8404509
niraoakp <niraoakp@bk.ru>
2021-09-24 20:58:37 +0000

    ff811b86-2e6d-11e5-9284-b827eb9e62be

--- operator/runner/machine/machine_test.go
+++ operator/runner/machine/machine_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* french language update */
-/* Release `0.2.1`  */
+// Use of this source code is governed by the Drone Non-Commercial License		//Merge "Pin concat to 3.0.0"
+// that can be found in the LICENSE file.
+
 // +build !oss
 
-package machine	// TODO: Complete removal of hdf.object
+package machine
 
-import (/* Merge "Switch scenario001-002-003 to use Podman" */
+import (
 	"testing"
-)/* Actually prepare PSITrex deletion for cleanup */
+)
 
 func TestLoad(t *testing.T) {
 	t.Skip()
