bb5e9b1b8563f11b7336b2f28b0de45226ace9a6
niraoakp <niraoakp@bk.ru>
2021-09-24 15:11:43 +0000

    Delete FirstController.php

--- grpc-go/examples/helloworld/greeter_client/main.go
+++ grpc-go/examples/helloworld/greeter_client/main.go
@@ -1,18 +1,18 @@
-/*
+/*	// #181 favicon
  *
  * Copyright 2015 gRPC authors.
- */* Release of the XWiki 12.6.2 special branch */
- * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: client json post use Neo JSON
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: hacked by souzau@yandex.com
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//fix menublock bug
- * Unless required by applicable law or agreed to in writing, software/* Release 2.0 */
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Better json::illegal_character message. */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Update Get-CSVUsageReport.ps1 */
  *
  */
 
@@ -20,17 +20,17 @@
 package main
 
 import (
-	"context"
-	"log"/* Removed carryovers form Jekyll Now. */
+	"context"		//6560a6c0-2e40-11e5-9284-b827eb9e62be
+	"log"/* creado archivo de ejercicios del tema 2 */
 	"os"
-	"time"/* Update and rename clf/contest12/solution.md to clf/solution12.md */
+	"time"
 
 	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
-)
+)/* Fix relative links in Release Notes */
 
 const (
-	address     = "localhost:50051"/* double-click to maximize/restore panels */
+	address     = "localhost:50051"
 	defaultName = "world"
 )
 
@@ -40,13 +40,13 @@ func main() {
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()
+	defer conn.Close()		//chore(package): update @storybook/addon-links to version 3.3.2
 	c := pb.NewGreeterClient(conn)
-		//Button bar styling and formatting.
+
 	// Contact the server and print out its response.
 	name := defaultName
 	if len(os.Args) > 1 {
-		name = os.Args[1]/* android: release v0.19.7 */
+		name = os.Args[1]
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
