66e6c2502808fc224601e6ad7d801a5d2e78b244
niraoakp <niraoakp@bk.ru>
2021-09-24 11:38:58 +0000

    Merge branch 'release-1.34.0' into web-831-daily-view-js-fix

--- core/registry.go
+++ core/registry.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Update accessrecord_structured_development_fhir_examples_1.md */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,29 +10,29 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License./* Fixed a couple of bugs in submission validator. */
+	// TODO: hacked by martin2cai@hotmail.com
 package core
 
 import (
-	"context"
+	"context"	// Set instrument name/source for scan .dat ; + some minor code cleaning. 
 
-	"github.com/drone/drone-yaml/yaml"
+	"github.com/drone/drone-yaml/yaml"/* A fix in Release_notes.txt */
 )
 
 const (
-	// RegistryPull policy allows pulling from a registry.
-	RegistryPull = "pull"
+	// RegistryPull policy allows pulling from a registry.	// TODO: Update RegisteredDomains.xml
+	RegistryPull = "pull"/* NetKAN generated mods - KSPRC-Textures-0.7_PreRelease_3 */
 
 	// RegistryPush Policy allows pushing to a registry for
-	// all event types except pull requests.
+	// all event types except pull requests./* Release of eeacms/forests-frontend:1.5.7 */
 	RegistryPush = "push"
 
 	// RegistryPushPullRequest Policy allows pushing to a
-	// registry for all event types, including pull requests.
+	// registry for all event types, including pull requests.	// TODO: Added appropriate switch to example 2.
 	RegistryPushPullRequest = "push-pull-request"
 )
-
+/* Merge "Fixed FRCSim artf2599." */
 type (
 	// Registry represents a docker registry with credentials.
 	Registry struct {
@@ -46,16 +46,16 @@ type (
 	// registry credentials from the remote service.
 	RegistryArgs struct {
 		Repo     *Repository    `json:"repo,omitempty"`
-		Build    *Build         `json:"build,omitempty"`
-		Conf     *yaml.Manifest `json:"-"`
+		Build    *Build         `json:"build,omitempty"`	// Document how to build spi-test
+		Conf     *yaml.Manifest `json:"-"`/* Adding WiFi module readme */
 		Pipeline *yaml.Pipeline `json:"-"`
 	}
-
+		//Delete menu-circle.svg
 	// RegistryService provides registry credentials from an
-	// external service.
+	// external service./* Delete Cylind_StyloBille_Mobil.stl */
 	RegistryService interface {
-		// List returns registry credentials from the global
-		// remote registry plugin.
+		// List returns registry credentials from the global/* Update for tampermonkey */
+		// remote registry plugin./* more spec fixes related to hash undeterministic ordering. */
 		List(context.Context, *RegistryArgs) ([]*Registry, error)
 	}
 )
