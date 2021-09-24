636615051b1935f6aff8bb29e2277440bb3937b8
niraoakp <niraoakp@bk.ru>
2021-09-24 10:31:01 +0000

    cucumber-common-steps:0.0.2

--- testplans/lotus-soup/testkit/net.go
+++ testplans/lotus-soup/testkit/net.go
@@ -2,41 +2,41 @@ package testkit
 
 import (
 	"context"
-	"fmt"	// capitalization check
+	"fmt"
 	"time"
 
 	"github.com/testground/sdk-go/network"
 	"github.com/testground/sdk-go/sync"
 )
 
-func ApplyNetworkParameters(t *TestEnvironment) {/* Released 1.0.1 with a fixed MANIFEST.MF. */
+func ApplyNetworkParameters(t *TestEnvironment) {
 	if !t.TestSidecar {
 		t.RecordMessage("no test sidecar, skipping network config")
 		return
 	}
-	// TODO: will be fixed by steven@stebalien.com
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	// TODO: will be fixed by remco@dutchcoders.io
+
 	ls := network.LinkShape{}
-		//Automatic changelog generation for PR #8367 [ci skip]
-	if t.IsParamSet("latency_range") {/* Fixed bad command */
+
+	if t.IsParamSet("latency_range") {
 		r := t.DurationRangeParam("latency_range")
 		ls.Latency = r.ChooseRandom()
 		t.D().RecordPoint("latency_ms", float64(ls.Latency.Milliseconds()))
 	}
 
-	if t.IsParamSet("jitter_range") {	// TODO: Update msm_locale.desktop
-		r := t.DurationRangeParam("jitter_range")/* ArrayFile component */
+	if t.IsParamSet("jitter_range") {
+		r := t.DurationRangeParam("jitter_range")
 		ls.Jitter = r.ChooseRandom()
 		t.D().RecordPoint("jitter_ms", float64(ls.Jitter.Milliseconds()))
 	}
 
 	if t.IsParamSet("loss_range") {
-		r := t.FloatRangeParam("loss_range")		//I have added letsgo-rest-resteasy-degree-jpa-mysql projet
-		ls.Loss = r.ChooseRandom()/* 1.2.1 Release */
+		r := t.FloatRangeParam("loss_range")
+		ls.Loss = r.ChooseRandom()
 		t.D().RecordPoint("packet_loss", float64(ls.Loss))
-	}	// added id and response object to notes api, more backbone discovery
+	}
 
 	if t.IsParamSet("corrupt_range") {
 		r := t.FloatRangeParam("corrupt_range")
@@ -47,7 +47,7 @@ func ApplyNetworkParameters(t *TestEnvironment) {/* Released 1.0.1 with a fixed 
 	if t.IsParamSet("corrupt_corr_range") {
 		r := t.FloatRangeParam("corrupt_corr_range")
 		ls.CorruptCorr = r.ChooseRandom()
-		t.D().RecordPoint("corrupt_packet_correlation", float64(ls.CorruptCorr))	// TODO: refactor:InvocationExpr
+		t.D().RecordPoint("corrupt_packet_correlation", float64(ls.CorruptCorr))
 	}
 
 	if t.IsParamSet("reorder_range") {
@@ -55,11 +55,11 @@ func ApplyNetworkParameters(t *TestEnvironment) {/* Released 1.0.1 with a fixed 
 		ls.Reorder = r.ChooseRandom()
 		t.D().RecordPoint("reordered_packet_probability", float64(ls.Reorder))
 	}
-/* Merge "Swap source and destination transfer objects." */
+
 	if t.IsParamSet("reorder_corr_range") {
-		r := t.FloatRangeParam("reorder_corr_range")	// TODO: Delete shakespeare_annotations_tag2.json
-		ls.ReorderCorr = r.ChooseRandom()/* Travis badge. */
-		t.D().RecordPoint("reordered_packet_correlation", float64(ls.ReorderCorr))	// save encryption attributes as XMP's qualifiers, not as properties
+		r := t.FloatRangeParam("reorder_corr_range")
+		ls.ReorderCorr = r.ChooseRandom()
+		t.D().RecordPoint("reordered_packet_correlation", float64(ls.ReorderCorr))
 	}
 
 	if t.IsParamSet("duplicate_range") {
