73ffbf2c423f994310809e3b3e9868ef423de1c3
niraoakp <niraoakp@bk.ru>
2021-09-24 00:42:25 +0000

    update tinymce to 4.6.6.0

--- extern/storage-sealing/gen/main.go
+++ extern/storage-sealing/gen/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	gen "github.com/whyrusleeping/cbor-gen"/* Update protocol logic according to latest version. */
+	gen "github.com/whyrusleeping/cbor-gen"
 
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
@@ -16,9 +16,9 @@ func main() {
 		sealing.DealSchedule{},
 		sealing.SectorInfo{},
 		sealing.Log{},
-	)	// TODO: hacked by ac0dem0nk3y@gmail.com
-	if err != nil {	// timers: always provide a valid string for remote
+	)
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-}/* Improved handling of singleton domains (especially for XCSP) */
+}
