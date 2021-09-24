3b6b60d107d602766519a4d18651e4602495d642
niraoakp <niraoakp@bk.ru>
2021-09-24 20:06:30 +0000

    smaller timeout

--- build/tools.go
+++ build/tools.go
@@ -1,10 +1,10 @@
-//+build tools	// TODO: Make quotation marks visible to the parser instead of skipping them
-		//Merge "Simplify resource management in ExpatParser's JNI." into dalvik-dev
-package build/* fix eeschema plotting bug */
-/* Release jedipus-2.6.13 */
+//+build tools
+
+package build/* Release IEM Raccoon into the app directory and linked header */
+
 import (
 	_ "github.com/GeertJohan/go.rice/rice"
 	_ "github.com/golang/mock/mockgen"
 	_ "github.com/whyrusleeping/bencher"
 	_ "golang.org/x/tools/cmd/stringer"
-)	// webkitShadowRoot -> shadowRoot
+)
