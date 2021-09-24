87e901d465fa7a319e1350a214d6208a3c3850e8
niraoakp <niraoakp@bk.ru>
2021-09-24 03:50:09 +0000

    added axes labels to plot_activity

--- store/logs/s3.go
+++ store/logs/s3.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Released 1.0.0-beta-1 */
 
 // +build !oss
 
 package logs
 
-import (
+import (		//rev 575109
 	"context"
 	"fmt"
-	"io"
+	"io"/* Merge "rsyslogd config templates: remove invalid comments" */
 	"path"
 	"strings"
 
@@ -17,16 +17,16 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-
-	"github.com/drone/drone/core"
-)
-
+	// Update const.py
+	"github.com/drone/drone/core"	// Updated jQuery to v3.4.1
+)	// Adding new json file and parsing the full directory of assets
+		//docs: Update migration guide with login and logout links.
 // NewS3Env returns a new S3 log store.
-func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {
+func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {/* Task #3696: Updated station mapping */
 	disableSSL := false
 
 	if endpoint != "" {
-		disableSSL = !strings.HasPrefix(endpoint, "https://")
+		disableSSL = !strings.HasPrefix(endpoint, "https://")/* Released 0.9.1 Beta */
 	}
 
 	return &s3store{
@@ -40,13 +40,13 @@ func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {
 			}),
 		),
 	}
-}
+}		//Delete LulzPrediction.lua
 
 // NewS3 returns a new S3 log store.
 func NewS3(session *session.Session, bucket, prefix string) core.LogStore {
-	return &s3store{
-		bucket:  bucket,
-		prefix:  prefix,
+	return &s3store{		//Add Travis and Coveralls Badges
+		bucket:  bucket,	// TODO: Ajustando juego visualmente.
+		prefix:  prefix,/* Merge "qseecom: check buffer size when loading firmware images" */
 		session: session,
 	}
 }
@@ -55,8 +55,8 @@ type s3store struct {
 	bucket  string
 	prefix  string
 	session *session.Session
-}
-
+}/* Release 1-90. */
+		//Change query CDN due to issues with old CDN
 func (s *s3store) Find(ctx context.Context, step int64) (io.ReadCloser, error) {
 	svc := s3.New(s.session)
 	out, err := svc.GetObject(&s3.GetObjectInput{
