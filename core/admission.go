915f511422ff63132761a58075c63a753d4683dd
niraoakp <niraoakp@bk.ru>
2021-09-24 16:30:14 +0000

    Update websites.MD

--- core/admission.go
+++ core/admission.go
@@ -2,10 +2,10 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//image path updated.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Added tutorial for subscribing to fire alerts
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,12 +14,12 @@
 
 package core
 
-import "context"/* moved to different location */
-/* Merge "wlan: Release 3.2.3.120" */
-// AdmissionService grants access to the system. The service can	// Add SameEndsTest
+import "context"
+
+// AdmissionService grants access to the system. The service can
 // be used to restrict access to authorized users, such as
 // members of an organization in your source control management
 // system.
 type AdmissionService interface {
 	Admit(context.Context, *User) error
-}/* merge test reports of jacoco */
+}
