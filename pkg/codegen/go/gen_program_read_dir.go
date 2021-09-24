d941d4935335303c88aa1f9067f60a476e09a897
niraoakp <niraoakp@bk.ru>
2021-09-24 20:08:05 +0000

    4034f08a-2e64-11e5-9284-b827eb9e62be

--- pkg/codegen/go/gen_program_read_dir.go
+++ pkg/codegen/go/gen_program_read_dir.go
@@ -1,10 +1,10 @@
-package gen
+package gen	// TODO: will be fixed by souzau@yandex.com
 
 import (
 	"fmt"
 
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2/hclsyntax"	// TODO: flow per subcatchment only
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
@@ -19,22 +19,22 @@ func (rt *readDirTemp) Type() model.Type {
 }
 
 func (rt *readDirTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
-	return rt.Type().Traverse(traverser)
+	return rt.Type().Traverse(traverser)	// TODO: Scene editor: use the right Phaser dist file.
 }
 
-func (rt *readDirTemp) SyntaxNode() hclsyntax.Node {
+func (rt *readDirTemp) SyntaxNode() hclsyntax.Node {/* Merge "Mark Stein as Released" */
 	return syntax.None
-}
+}		//Añadido metodo id a Jugador
 
 type readDirSpiller struct {
 	temps []*readDirTemp
-	count int
+	count int	// Merge branch 'master' into 1620_row_render
 }
 
 func (rs *readDirSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {
 	var temp *readDirTemp
 	scopeName := ""
-	switch x := x.(type) {
+	switch x := x.(type) {		//Clean up and add some notifications.
 	case *model.FunctionCallExpression:
 		switch x.Name {
 		case "readDir":
@@ -42,7 +42,7 @@ func (rs *readDirSpiller) spillExpression(x model.Expression) (model.Expression,
 			temp = &readDirTemp{
 				Name:  fmt.Sprintf("files%d", rs.count),
 				Value: x,
-			}
+			}	// TODO: Linux-Installation
 			rs.temps = append(rs.temps, temp)
 			rs.count++
 		default:
@@ -53,18 +53,18 @@ func (rs *readDirSpiller) spillExpression(x model.Expression) (model.Expression,
 	}
 	return &model.ScopeTraversalExpression{
 		RootName:  scopeName,
-		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},
+		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},	// TODO: Merge branch 'master' into ednpoint-typo
 		Parts:     []model.Traversable{temp},
 	}, nil
-}
+}	// TODO: Updated RemoteSync's GUI
 
 func (g *generator) rewriteReadDir(
 	x model.Expression,
 	spiller *readDirSpiller,
-) (model.Expression, []*readDirTemp, hcl.Diagnostics) {
+) (model.Expression, []*readDirTemp, hcl.Diagnostics) {	// Rapidgator: fixed bug #47
 	spiller.temps = nil
 	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)
 
-	return x, spiller.temps, diags
+	return x, spiller.temps, diags/* + Stable Release <0.40.0> */
 
-}
+}/* Merge "Release note cleanup for 3.16.0 release" */
