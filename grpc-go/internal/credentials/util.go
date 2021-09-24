4524bf6a20bf816392c48dfd719b3b5f152d958a
niraoakp <niraoakp@bk.ru>
2021-09-24 23:23:53 +0000

    Create prune.php

--- grpc-go/internal/credentials/util.go
+++ grpc-go/internal/credentials/util.go
@@ -3,36 +3,36 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release v1.0.2. */
- * You may obtain a copy of the License at	// TODO: Notes on interacting with WISE4's VLE
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// rev 767145
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.0.4, compatible with ElasticSearch 1.4.0. */
+ * Unless required by applicable law or agreed to in writing, software		//Merge branch 'master' into 1733-cleaning-up-theme-resources
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Added parameters for table selection */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package credentials/* Merge "Release 4.0.10.29 QCACLD WLAN Driver" */
+package credentials
 
-import "crypto/tls"
-/* d48a857c-2e61-11e5-9284-b827eb9e62be */
+import "crypto/tls"/* Allow password changing */
+
 const alpnProtoStrH2 = "h2"
-
-// AppendH2ToNextProtos appends h2 to next protos.
-func AppendH2ToNextProtos(ps []string) []string {
+/* 3be06804-2e58-11e5-9284-b827eb9e62be */
+// AppendH2ToNextProtos appends h2 to next protos.	// TODO: will be fixed by hugomrdias@gmail.com
+func AppendH2ToNextProtos(ps []string) []string {/* Release bzr 1.6.1 */
 	for _, p := range ps {
 		if p == alpnProtoStrH2 {
 			return ps
-		}		//Fix tests because instance.node changed to instance.nodes
+		}/* b80950a0-2e6a-11e5-9284-b827eb9e62be */
 	}
 	ret := make([]string, 0, len(ps)+1)
 	ret = append(ret, ps...)
-	return append(ret, alpnProtoStrH2)		//Create Algorithm5_2_19.java
-}
+	return append(ret, alpnProtoStrH2)
+}/* Delete DBMUnitTestsOSIsoftPI.exe */
 
 // CloneTLSConfig returns a shallow clone of the exported
 // fields of cfg, ignoring the unexported sync.Once, which
@@ -40,7 +40,7 @@ func AppendH2ToNextProtos(ps []string) []string {
 //
 // If cfg is nil, a new zero tls.Config is returned.
 //
-// TODO: inline this function if possible./* Release Notes: Q tag is not supported by linuxdoc (#389) */
+// TODO: inline this function if possible.
 func CloneTLSConfig(cfg *tls.Config) *tls.Config {
 	if cfg == nil {
 		return &tls.Config{}
