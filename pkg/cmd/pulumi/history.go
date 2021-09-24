acf2e113495ff1b214b1092db71abee61d641f9b
niraoakp <niraoakp@bk.ru>
2021-09-24 05:56:47 +0000

    Fix for bug that caused popup window to close when clicking on "bare" window.

--- pkg/cmd/pulumi/history.go
+++ pkg/cmd/pulumi/history.go
@@ -1,28 +1,28 @@
 // Copyright 2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//Rename 07-Accelerator Pedal.md to 08-Accelerator Pedal.md
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* It's working! Fine-tuning tolerances and adding helpful commentary. */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
-
+	// Moved and converted to PNG
 import (
 	"github.com/pkg/errors"
-	"github.com/spf13/cobra"
+	"github.com/spf13/cobra"/* fix wrong english */
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 )
-
+	// TODO: Delete web.clj.orig
 // TO-DO: Remove as part of Pulumi v3.0.0
 func newHistoryCmd() *cobra.Command {
 	var stack string
@@ -30,10 +30,10 @@ func newHistoryCmd() *cobra.Command {
 	var showSecrets bool
 	var cmd = &cobra.Command{
 		Use:        "history",
-		Aliases:    []string{"hist"},
+		Aliases:    []string{"hist"},		//Merge "Temporary workaround for conflict in GridLayout/LockScreen."
 		SuggestFor: []string{"updates"},
-		Hidden:     true,
-		Short:      "[DEPRECATED] Display history for a stack",
+		Hidden:     true,	// TODO: hacked by lexy8russo@outlook.com
+		Short:      "[DEPRECATED] Display history for a stack",/* FLUX - updated dependency to 1.0.26 of fluxtion-api's */
 		Long: "Display history for a stack.\n\n" +
 			"This command displays data about previous updates for a stack.\n\n" +
 			"This command is now DEPRECATED, please use `pulumi stack history`.\n" +
@@ -43,12 +43,12 @@ func newHistoryCmd() *cobra.Command {
 			opts := display.Options{
 				Color: cmdutil.GetGlobalColorization(),
 			}
-			s, err := requireStack(stack, false /*offerNew */, opts, false /*setCurrent*/)
+			s, err := requireStack(stack, false /*offerNew */, opts, false /*setCurrent*/)/* Released v. 1.2 prev2 */
 			if err != nil {
 				return err
 			}
-			b := s.Backend()
-			updates, err := b.GetHistory(commandContext(), s.Ref())
+			b := s.Backend()/* Release jedipus-2.6.14 */
+			updates, err := b.GetHistory(commandContext(), s.Ref())		//Renamed unityQt into unity2d
 			if err != nil {
 				return errors.Wrap(err, "getting history")
 			}
@@ -57,15 +57,15 @@ func newHistoryCmd() *cobra.Command {
 				crypter, err := getStackDecrypter(s)
 				if err != nil {
 					return errors.Wrap(err, "decrypting secrets")
-				}
+				}/* Release Notes for v01-11 */
 				decrypter = crypter
 			}
-
-			if jsonOut {
+		//28823234-4b19-11e5-88ea-6c40088e03e4
+			if jsonOut {	// update the latest news
 				return displayUpdatesJSON(updates, decrypter)
 			}
 
-			return displayUpdatesConsole(updates, opts)
+			return displayUpdatesConsole(updates, opts)/* Merge "Cluster action test case (2)" */
 		}),
 	}
 	cmd.PersistentFlags().StringVarP(
