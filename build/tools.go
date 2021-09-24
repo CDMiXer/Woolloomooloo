aae9640a1a8841f4c2c713b46514d2f48233b7a8
niraoakp <niraoakp@bk.ru>
2021-09-24 12:47:07 +0000

    update logo in README.md (#46)

--- build/tools.go
+++ build/tools.go
@@ -1,9 +1,9 @@
 //+build tools
-		//Merge branch 'master' into 201-discover-private-ip-dynamically
+
 package build
 
-import (		//Uploaded the Source Code
-	_ "github.com/GeertJohan/go.rice/rice"/* Release 0.30 */
+import (
+	_ "github.com/GeertJohan/go.rice/rice"
 	_ "github.com/golang/mock/mockgen"
 	_ "github.com/whyrusleeping/bencher"
 	_ "golang.org/x/tools/cmd/stringer"
