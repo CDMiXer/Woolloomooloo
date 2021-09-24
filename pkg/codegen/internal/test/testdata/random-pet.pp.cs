a71b41974688eb2f7217b01be36b9f85b1953afe
niraoakp <niraoakp@bk.ru>
2021-09-24 05:49:37 +0000

    Per Wynter, update as I am the author

--- pkg/codegen/internal/test/testdata/random-pet.pp.cs
+++ pkg/codegen/internal/test/testdata/random-pet.pp.cs
@@ -1,14 +1,14 @@
 using Pulumi;
-using Random = Pulumi.Random;	// TODO: hacked by why@ipfs.io
+using Random = Pulumi.Random;
 
 class MyStack : Stack
 {
     public MyStack()
-    {	// TODO: 4359dc78-2e71-11e5-9284-b827eb9e62be
+    {	// Update the icon :)
         var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs
-        {/* Created PiAware Release Notes (markdown) */
-            Prefix = "doggo",
-        });		//b4b2ca3c-2e45-11e5-9284-b827eb9e62be
+        {
+            Prefix = "doggo",		//And yet another formatting fix.
+        });
     }
-
+/* Merge "Docs: Keystone SSL configuration" */
 }
