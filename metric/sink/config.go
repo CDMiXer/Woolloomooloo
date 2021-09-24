f3790a3d822bed9e69552b133420d6a0a49e1252
niraoakp <niraoakp@bk.ru>
2021-09-24 19:33:21 +0000

    DBT-244 change rc warning mail and add command to do a single warning

--- metric/sink/config.go
+++ metric/sink/config.go
@@ -1,35 +1,35 @@
-// Copyright 2019 Drone IO, Inc./* Released 2.0.0-beta3. */
-//
+// Copyright 2019 Drone IO, Inc.
+//		//ObservableListTest.java added
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release for 18.23.0 */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Remove DVDNAV audio reset code to avoid issues on title changes. */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update README with Ropsten info */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Minor change to ordering of keys in YAML file */
 package sink
 
 // Config configures a Datadog sink.
 type Config struct {
-	Endpoint string/* test-parse-date: move remaining date parsing tests from test-log */
-	Token    string
+	Endpoint string
+	Token    string/* Get entity name to use on view of form */
 
-	License          string
+	License          string/* Fix up white/black listing to actually do what was intended */
 	Licensor         string
 	Subscription     string
-	EnableGithub     bool
-	EnableGithubEnt  bool/* Remove char parameter from onKeyPressed() and onKeyReleased() methods. */
-	EnableGitlab     bool
+	EnableGithub     bool	// TODO: hacked by igor@soramitsu.co.jp
+	EnableGithubEnt  bool
+	EnableGitlab     bool/* Release 0.0.3: Windows support */
 	EnableBitbucket  bool
-	EnableStash      bool
-	EnableGogs       bool	// TODO: Define initial version of metrics repository interface, to be reviewed
-	EnableGitea      bool	// initial checkin of new speech services
+	EnableStash      bool/* [artifactory-release] Release version 3.2.12.RELEASE */
+	EnableGogs       bool/* Rest issues */
+	EnableGitea      bool
 	EnableAgents     bool
 	EnableNomad      bool
-	EnableKubernetes bool
+	EnableKubernetes bool		//Final test cases for property container.
 }
