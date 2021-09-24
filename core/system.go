cb88aebdd624904de5e939e8c5f9143dd0a22960
niraoakp <niraoakp@bk.ru>
2021-09-24 22:10:01 +0000

    removed starting. will probably end up as like, a demo.

--- core/system.go
+++ core/system.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.		//Make tree browse go work again.
+// You may obtain a copy of the License at/* Release of eeacms/bise-backend:v10.0.27 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by earlephilhower@yahoo.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release v0.2.7 */
+// See the License for the specific language governing permissions and		//Merge "Fix cpplint errors for ermvpn_table.[cc|h]"
 // limitations under the License.
-
+	// init spring dao
 package core
-
+	// Reset au_def_pause_time and remove login page checker
 // System stores system information.
 type System struct {
-	Proto   string `json:"proto,omitempty"`
-	Host    string `json:"host,omitempty"`
-	Link    string `json:"link,omitempty"`
+	Proto   string `json:"proto,omitempty"`	// TODO: Create QuickStart
+	Host    string `json:"host,omitempty"`	// --drive_mode
+	Link    string `json:"link,omitempty"`/* Release version 1.1.0.M3 */
 	Version string `json:"version,omitempty"`
-}
+}	// TODO: will be fixed by martin2cai@hotmail.com
