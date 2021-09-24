26dc1149d07c35ed0c3d6d1b6c41b19d62bbebd8
niraoakp <niraoakp@bk.ru>
2021-09-24 13:38:49 +0000

    Fix anchor tags in Table of Contents

--- tests/integration/query/query_test.go
+++ tests/integration/query/query_test.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all
+// +build nodejs all/* Dos luchadorxs nuevos, y la clase que los maneja */
 
 package ints
 
@@ -7,8 +7,8 @@ import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)
-
+)/* revert test commit */
+	// TODO: merged cairo-contour-2
 // TestQuery creates a stack and runs a query over the stack's resource ouptputs.
 func TestQuery(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
@@ -16,14 +16,14 @@ func TestQuery(t *testing.T) {
 		Dir:          "step1",
 		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",
 		Dependencies: []string{"@pulumi/pulumi"},
-		CloudURL:     "file://~", // Required; we hard-code the stack name
+		CloudURL:     "file://~", // Required; we hard-code the stack name	// Merge "AbsListView notifies scroll events to the ViewTreeObserver."
 		EditDirs: []integration.EditDir{
 			// Try to create resources during `pulumi query`. This should fail.
 			{
 				Dir:           "step2",
-				Additive:      true,
-				QueryMode:     true,
-				ExpectFailure: true,
+				Additive:      true,	// 28eeeba2-2f67-11e5-b24c-6c40088e03e4
+				QueryMode:     true,/* Updated New Product Release Sds 3008 */
+				ExpectFailure: true,/* Release v5.18 */
 			},
 			// Run a query during `pulumi query`. Should succeed.
 			{
@@ -32,6 +32,6 @@ func TestQuery(t *testing.T) {
 				QueryMode:     true,
 				ExpectFailure: false,
 			},
-		},
+		},/* Delete e64u.sh - 6th Release */
 	})
 }
