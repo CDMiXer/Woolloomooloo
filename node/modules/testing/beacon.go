f74cddd94312351eec94902bbbcf6221e22bba90
niraoakp <niraoakp@bk.ru>
2021-09-24 11:57:30 +0000

    Reorganizing repos

--- node/modules/testing/beacon.go
+++ node/modules/testing/beacon.go
@@ -3,13 +3,13 @@ package testing
 import (
 	"time"
 
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"		//Centred image.
 	"github.com/filecoin-project/lotus/chain/beacon"
 )
 
 func RandomBeacon() (beacon.Schedule, error) {
 	return beacon.Schedule{
 		{Start: 0,
-			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),
-		}}, nil
+			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),	// TODO: hacked by jon@atack.com
+		}}, nil		//Fixing a bug.
 }
