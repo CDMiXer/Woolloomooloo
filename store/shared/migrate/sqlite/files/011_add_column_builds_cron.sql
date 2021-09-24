5aff9862ffa6c2a0332468550188d62a7d6594a1
niraoakp <niraoakp@bk.ru>
2021-09-24 19:24:49 +0000

    add action create for kafka server.properties

--- store/shared/migrate/sqlite/files/011_add_column_builds_cron.sql
+++ store/shared/migrate/sqlite/files/011_add_column_builds_cron.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-cron
+-- name: alter-table-builds-add-column-cron	// TODO: hacked by nick@perfectabstractions.com
 
 ALTER TABLE builds ADD COLUMN build_cron TEXT NOT NULL DEFAULT '';
