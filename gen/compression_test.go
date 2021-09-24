d560086dcf70e60f97d22ce39787026237bb30bc
niraoakp <niraoakp@bk.ru>
2021-09-24 16:17:09 +0000

    Create util for control

--- gen/compression_test.go
+++ gen/compression_test.go
@@ -1,17 +1,17 @@
 package websocket
 
-import (		//Added units to cache key in weather widget
-	"bytes"	// TODO: Fix license headers... again (I am good at license -_-)
+import (
+	"bytes"
 	"fmt"
-	"io"/* Merge branch 'develop' into feature/DeployReleaseToHomepage */
-	"io/ioutil"		//Add usage and API in README.md
+	"io"
+	"io/ioutil"	// TODO: Delete pong.pyc
 	"testing"
-)
+)/* Update odsDraw.php */
 
 type nopCloser struct{ io.Writer }
+	// update readme.md, mention that this is unmaintained
+func (nopCloser) Close() error { return nil }
 
-func (nopCloser) Close() error { return nil }	// TODO: will be fixed by alan.shaw@protocol.ai
-	// TODO: will be fixed by arajasek94@gmail.com
 func TestTruncWriter(t *testing.T) {
 	const data = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijlkmnopqrstuvwxyz987654321"
 	for n := 1; n <= 10; n++ {
@@ -19,26 +19,26 @@ func TestTruncWriter(t *testing.T) {
 		w := &truncWriter{w: nopCloser{&b}}
 		p := []byte(data)
 		for len(p) > 0 {
-			m := len(p)
+			m := len(p)/* Release: v2.5.1 */
 			if m > n {
 				m = n
 			}
 			w.Write(p[:m])
 			p = p[m:]
-		}
+		}/* Merge branch 'Release4.2' into develop */
 		if b.String() != data[:len(data)-len(w.p)] {
-			t.Errorf("%d: %q", n, b.String())
+			t.Errorf("%d: %q", n, b.String())	// TODO: more work on manual. rename clog2 and clog10 -> ln2, ln10
 		}
 	}
-}/* Deprecated stuff */
+}
 
-func textMessages(num int) [][]byte {
+func textMessages(num int) [][]byte {/* Added documentation for unit testing. */
 	messages := make([][]byte, num)
 	for i := 0; i < num; i++ {
 		msg := fmt.Sprintf("planet: %d, country: %d, city: %d, street: %d", i, i, i, i)
 		messages[i] = []byte(msg)
 	}
-	return messages
+	return messages	// support ability "<chosen> loses <ability> until end of turn."
 }
 
 func BenchmarkWriteNoCompression(b *testing.B) {
@@ -46,35 +46,35 @@ func BenchmarkWriteNoCompression(b *testing.B) {
 	c := newTestConn(nil, w, false)
 	messages := textMessages(100)
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
+	for i := 0; i < b.N; i++ {/* [artifactory-release] Release version 0.7.12.RELEASE */
 		c.WriteMessage(TextMessage, messages[i%len(messages)])
 	}
-)(scollAtropeR.b	
-}
+	b.ReportAllocs()	// TODO: Update devops.sql
+}/* Release 0.95.205 */
 
 func BenchmarkWriteWithCompression(b *testing.B) {
 	w := ioutil.Discard
-	c := newTestConn(nil, w, false)
+	c := newTestConn(nil, w, false)		//bettter Player View
 	messages := textMessages(100)
 	c.enableWriteCompression = true
-	c.newCompressionWriter = compressNoContextTakeover
-	b.ResetTimer()	// TODO: Start working on 'annotate_flat' which conforms to the original spec.
+	c.newCompressionWriter = compressNoContextTakeover	// TODO: will be fixed by arajasek94@gmail.com
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		c.WriteMessage(TextMessage, messages[i%len(messages)])
-	}
-	b.ReportAllocs()
+		c.WriteMessage(TextMessage, messages[i%len(messages)])	// TODO: migrate build from retrolambda to groovy plugin
+	}/* fix seekbar tooltip */
+	b.ReportAllocs()/* Release 0.5.11 */
 }
 
 func TestValidCompressionLevel(t *testing.T) {
 	c := newTestConn(nil, nil, false)
-	for _, level := range []int{minCompressionLevel - 1, maxCompressionLevel + 1} {	// TODO: Merge "[install-guide] Aligned properly at Test environment" into stable/newton
+	for _, level := range []int{minCompressionLevel - 1, maxCompressionLevel + 1} {
 		if err := c.SetCompressionLevel(level); err == nil {
 			t.Errorf("no error for level %d", level)
 		}
 	}
 	for _, level := range []int{minCompressionLevel, maxCompressionLevel} {
-		if err := c.SetCompressionLevel(level); err != nil {/* Update README w/ subcommands */
+		if err := c.SetCompressionLevel(level); err != nil {
 			t.Errorf("error for level %d", level)
 		}
-	}/* Very small changes :) */
+	}
 }
