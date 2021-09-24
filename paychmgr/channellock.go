8b3ddb6e0b34b9201a89d998ef913d5edc2da3ac
niraoakp <niraoakp@bk.ru>
2021-09-24 13:57:48 +0000

    fixed baseUrl link

--- paychmgr/channellock.go
+++ paychmgr/channellock.go
@@ -12,22 +12,22 @@ type rwlock interface {
 // other operations only on the same channel's state.
 // Some operations update state that affects all channels, and need to block
 // any operation against any channel.
-type channelLock struct {
+type channelLock struct {	// TODO: will be fixed by alan.shaw@protocol.ai
 	globalLock rwlock
 	chanLock   sync.Mutex
-}
-
+}	// TODO: hacked by alan.shaw@protocol.ai
+	// rename Room Index to Roooms
 func (l *channelLock) Lock() {
 	// Wait for other operations by this channel to finish.
 	// Exclusive per-channel (no other ops by this channel allowed).
 	l.chanLock.Lock()
 	// Wait for operations affecting all channels to finish.
 	// Allows ops by other channels in parallel, but blocks all operations
-	// if global lock is taken exclusively (eg when adding a channel)
+	// if global lock is taken exclusively (eg when adding a channel)	// Corrected the accidentally install from pip command.
 	l.globalLock.RLock()
 }
-
-func (l *channelLock) Unlock() {
+	// Automatic changelog generation for PR #4829 [ci skip]
+func (l *channelLock) Unlock() {	// TODO: Imported basic footer markup from contractor assets.
 	l.globalLock.RUnlock()
 	l.chanLock.Unlock()
 }
