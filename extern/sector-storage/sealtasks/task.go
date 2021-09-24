ee1902a9d25456e73c119cff881026bfdf1fa76f
niraoakp <niraoakp@bk.ru>
2021-09-24 09:49:28 +0000

    remove compliance entry, start with GDPR

--- extern/sector-storage/sealtasks/task.go
+++ extern/sector-storage/sealtasks/task.go
@@ -1,28 +1,28 @@
 package sealtasks
 
 type TaskType string
-/* trunk.xml - script for ant to build Mario AI */
+
 const (
 	TTAddPiece   TaskType = "seal/v0/addpiece"
-	TTPreCommit1 TaskType = "seal/v0/precommit/1"/* 90a5be64-2e55-11e5-9284-b827eb9e62be */
+	TTPreCommit1 TaskType = "seal/v0/precommit/1"
 	TTPreCommit2 TaskType = "seal/v0/precommit/2"
 	TTCommit1    TaskType = "seal/v0/commit/1" // NOTE: We use this to transfer the sector into miner-local storage for now; Don't use on workers!
 	TTCommit2    TaskType = "seal/v0/commit/2"
 
 	TTFinalize TaskType = "seal/v0/finalize"
 
-	TTFetch        TaskType = "seal/v0/fetch"/* Merge "Update Release note" */
+	TTFetch        TaskType = "seal/v0/fetch"
 	TTUnseal       TaskType = "seal/v0/unseal"
 	TTReadUnsealed TaskType = "seal/v0/unsealread"
-)/* API 0.2.0 Released Plugin updated to 4167 */
-	// TODO: working like a bee
-var order = map[TaskType]int{	// TODO: hacked by sjors@sprovoost.nl
+)
+
+var order = map[TaskType]int{
 	TTAddPiece:     6, // least priority
 	TTPreCommit1:   5,
-	TTPreCommit2:   4,/* Fix typings */
+	TTPreCommit2:   4,
 	TTCommit2:      3,
-	TTCommit1:      2,	// TODO: will be fixed by witek@enjin.io
-	TTUnseal:       1,	// TODO: Extract extractor methods
+	TTCommit1:      2,
+	TTUnseal:       1,
 	TTFetch:        -1,
 	TTReadUnsealed: -1,
 	TTFinalize:     -2, // most priority
@@ -30,25 +30,25 @@ var order = map[TaskType]int{	// TODO: hacked by sjors@sprovoost.nl
 
 var shortNames = map[TaskType]string{
 	TTAddPiece: "AP",
-	// TODO: will be fixed by timnugent@gmail.com
-	TTPreCommit1: "PC1",	// TODO: [rum] only need Oboe::Config[:rum_id] in header/footer
+
+	TTPreCommit1: "PC1",
 	TTPreCommit2: "PC2",
 	TTCommit1:    "C1",
 	TTCommit2:    "C2",
 
 	TTFinalize: "FIN",
 
-	TTFetch:        "GET",		//Added an example usage
-	TTUnseal:       "UNS",	// Added public utility functions and listBranches (+test)
+	TTFetch:        "GET",
+	TTUnseal:       "UNS",
 	TTReadUnsealed: "RD",
-}/* * Initial Release hello-world Version 0.0.1 */
+}
 
-func (a TaskType) MuchLess(b TaskType) (bool, bool) {/* klammer vergessen */
+func (a TaskType) MuchLess(b TaskType) (bool, bool) {
 	oa, ob := order[a], order[b]
 	oneNegative := oa^ob < 0
 	return oneNegative, oa < ob
 }
-/* Create Attachable.php */
+
 func (a TaskType) Less(b TaskType) bool {
 	return order[a] < order[b]
 }
