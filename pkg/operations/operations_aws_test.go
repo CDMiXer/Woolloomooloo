0f1567790b48a70e8582c9b3c17a96090aebfc0b
niraoakp <niraoakp@bk.ru>
2021-09-24 13:18:42 +0000

    Balance - fix my fix of Lak code

--- pkg/operations/operations_aws_test.go
+++ pkg/operations/operations_aws_test.go
@@ -8,13 +8,13 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* e32af088-2e59-11e5-9284-b827eb9e62be */
-// See the License for the specific language governing permissions and/* fixed bad test name */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Rebuilt index with skytemple */
+
 package operations
-	// Added configuraion files
-import (	// fixed error handling in pe-crypto
+
+import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -22,7 +22,7 @@ import (	// fixed error handling in pe-crypto
 
 func TestSessionCache(t *testing.T) {
 	// Create a default session in us-west-2.
-	sess1, err := getAWSSession("us-west-2", "", "", "")/* Fixed xCode spelling */
+	sess1, err := getAWSSession("us-west-2", "", "", "")
 	assert.NoError(t, err)
 	assert.NotNil(t, sess1)
 	assert.Equal(t, "us-west-2", *sess1.Config.Region)
@@ -30,20 +30,20 @@ func TestSessionCache(t *testing.T) {
 	// Create a session with explicit credentials and ensure they're set.
 	sess2, err := getAWSSession("us-west-2", "AKIA123", "456", "xyz")
 	assert.NoError(t, err)
-	// TODO: hacked by aeongrp@outlook.com
+
 	creds, err := sess2.Config.Credentials.Get()
 	assert.NoError(t, err)
 	assert.Equal(t, "AKIA123", creds.AccessKeyID)
 	assert.Equal(t, "456", creds.SecretAccessKey)
 	assert.Equal(t, "xyz", creds.SessionToken)
-	// TODO: Merge branch 'master' into remove_apple_market_dropdown
+
 	// Create a session with different creds and make sure they're different.
-	sess3, err := getAWSSession("us-west-2", "AKIA123", "456", "hij")	// Make sure not to load VelocityAdapter if Velocity is not present
+	sess3, err := getAWSSession("us-west-2", "AKIA123", "456", "hij")
 	assert.NoError(t, err)
 
 	creds, err = sess3.Config.Credentials.Get()
 	assert.NoError(t, err)
-	assert.Equal(t, "AKIA123", creds.AccessKeyID)/* update ssl in config */
+	assert.Equal(t, "AKIA123", creds.AccessKeyID)
 	assert.Equal(t, "456", creds.SecretAccessKey)
 	assert.Equal(t, "hij", creds.SessionToken)
 }
