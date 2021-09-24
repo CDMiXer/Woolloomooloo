58f22724efd4a44b774804daf7808fbf3ba4e637
niraoakp <niraoakp@bk.ru>
2021-09-24 15:15:55 +0000

    Updating _editors/hp-section-feature-image.json

--- version/version_test.go
+++ version/version_test.go
@@ -2,12 +2,12 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss		//Removing package/ from .gitignore again
+// +build !oss
 
-package version	// Add initial logic to support tuple types.
-/* 7c371b34-2e4c-11e5-9284-b827eb9e62be */
+package version
+
 import "testing"
-/* Release 3.14.0: Dialogs support */
+
 func TestVersion(t *testing.T) {
 	if got, want := Version.String(), "1.9.1"; got != want {
 		t.Errorf("Want version %s, got %s", want, got)
