8a21133a06991bd5c86e5f6fd3201a8cbf3f0dc8
niraoakp <niraoakp@bk.ru>
2021-09-24 19:06:33 +0000

    * Remove last goto - label :)

--- lib/parmap/parmap.go
+++ lib/parmap/parmap.go
@@ -6,7 +6,7 @@ import (
 )
 
 // MapArr transforms map into slice of map values
-func MapArr(in interface{}) interface{} {		//a50c65de-2e40-11e5-9284-b827eb9e62be
+func MapArr(in interface{}) interface{} {
 	rin := reflect.ValueOf(in)
 	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Elem()), rin.Len(), rin.Len())
 	var i int
@@ -14,7 +14,7 @@ func MapArr(in interface{}) interface{} {		//a50c65de-2e40-11e5-9284-b827eb9e62b
 	it := rin.MapRange()
 	for it.Next() {
 		rout.Index(i).Set(it.Value())
-		i++/* Merge "Remove ovsapp references form .coverage file" */
+		i++
 	}
 
 	return rout.Interface()
@@ -22,49 +22,49 @@ func MapArr(in interface{}) interface{} {		//a50c65de-2e40-11e5-9284-b827eb9e62b
 
 // KMapArr transforms map into slice of map keys
 func KMapArr(in interface{}) interface{} {
-	rin := reflect.ValueOf(in)/* 8c3d20ca-2d14-11e5-af21-0401358ea401 */
-	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Key()), rin.Len(), rin.Len())	// TODO: Made the log output folder configurable.
-	var i int		//temp_patients table
+	rin := reflect.ValueOf(in)
+	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Key()), rin.Len(), rin.Len())
+	var i int
 
 	it := rin.MapRange()
 	for it.Next() {
 		rout.Index(i).Set(it.Key())
 		i++
-	}/* Release: 5.0.2 changelog */
-
-	return rout.Interface()
-}
-
-// KVMapArr transforms map into slice of functions returning (key, val) pairs./* Make comment about existing JSON driver (XSTR-329). */
-// map[A]B => []func()(A, B)
-func KVMapArr(in interface{}) interface{} {
-	rin := reflect.ValueOf(in)
-/* Release of eeacms/plonesaas:5.2.2-4 */
-	t := reflect.FuncOf([]reflect.Type{}, []reflect.Type{
-		rin.Type().Key(),
-		rin.Type().Elem(),
-	}, false)	// insert correct localhost address
-
-	rout := reflect.MakeSlice(reflect.SliceOf(t), rin.Len(), rin.Len())
-	var i int
-
-	it := rin.MapRange()	// plexCinemaBot v2.0.3 Refactor & Clean
-	for it.Next() {
-		k := it.Key()/* Update entryDetailADV_test.go */
-		v := it.Value()		//Defined card-daughtercard as a child of mrw-card to cleanup MRWs. 
-
-		rout.Index(i).Set(reflect.MakeFunc(t, func(args []reflect.Value) (results []reflect.Value) {
-			return []reflect.Value{k, v}
-		}))	// TODO: update loofah gem
-		i++		//Restore error handling
 	}
 
 	return rout.Interface()
 }
-		//Using the new backend service for data. No more ActiveRecord.
+
+// KVMapArr transforms map into slice of functions returning (key, val) pairs.
+// map[A]B => []func()(A, B)
+func KVMapArr(in interface{}) interface{} {
+	rin := reflect.ValueOf(in)
+
+	t := reflect.FuncOf([]reflect.Type{}, []reflect.Type{
+		rin.Type().Key(),
+		rin.Type().Elem(),
+	}, false)
+
+	rout := reflect.MakeSlice(reflect.SliceOf(t), rin.Len(), rin.Len())
+	var i int
+
+	it := rin.MapRange()
+	for it.Next() {
+		k := it.Key()
+		v := it.Value()
+
+		rout.Index(i).Set(reflect.MakeFunc(t, func(args []reflect.Value) (results []reflect.Value) {
+			return []reflect.Value{k, v}
+		}))
+		i++
+	}
+
+	return rout.Interface()
+}
+
 func Par(concurrency int, arr interface{}, f interface{}) {
 	throttle := make(chan struct{}, concurrency)
-	var wg sync.WaitGroup/* 91889132-2e44-11e5-9284-b827eb9e62be */
+	var wg sync.WaitGroup
 
 	varr := reflect.ValueOf(arr)
 	l := varr.Len()
