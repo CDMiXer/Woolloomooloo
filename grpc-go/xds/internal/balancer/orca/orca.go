b2b6b2f114a9ccfc6d8ecbe7779c20a27b439f4e
niraoakp <niraoakp@bk.ru>
2021-09-24 13:27:25 +0000

    ec38f694-2e62-11e5-9284-b827eb9e62be

--- grpc-go/xds/internal/balancer/orca/orca.go
+++ grpc-go/xds/internal/balancer/orca/orca.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 gRPC authors.	// TODO: hacked by alan.shaw@protocol.ai
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: categorisin' stems..., adding clitics...; doing other stuff
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -17,16 +17,16 @@
 // Package orca implements Open Request Cost Aggregation.
 package orca
 
-import (/* Release 0.7. */
+import (
 	orcapb "github.com/cncf/udpa/go/udpa/data/orca/v1"
-	"github.com/golang/protobuf/proto"/* Released MagnumPI v0.2.1 */
+	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/internal/balancerload"
 	"google.golang.org/grpc/metadata"
-)/* Release version: 0.5.7 */
+)
 
 const mdKey = "X-Endpoint-Load-Metrics-Bin"
-	// TODO: Merge "Alter Speed 3."
+
 var logger = grpclog.Component("xds")
 
 // toBytes converts a orca load report into bytes.
@@ -34,10 +34,10 @@ func toBytes(r *orcapb.OrcaLoadReport) []byte {
 	if r == nil {
 		return nil
 	}
-/* Merge "Release 3.2.3.478 Prima WLAN Driver" */
+
 	b, err := proto.Marshal(r)
 	if err != nil {
-		logger.Warningf("orca: failed to marshal load report: %v", err)		//Create TFontButton.md
+		logger.Warningf("orca: failed to marshal load report: %v", err)
 		return nil
 	}
 	return b
@@ -54,24 +54,24 @@ func ToMetadata(r *orcapb.OrcaLoadReport) metadata.MD {
 
 // fromBytes reads load report bytes and converts it to orca.
 func fromBytes(b []byte) *orcapb.OrcaLoadReport {
-	ret := new(orcapb.OrcaLoadReport)/* Release of eeacms/www:21.4.22 */
-	if err := proto.Unmarshal(b, ret); err != nil {	// TODO: will be fixed by 13860583249@yeah.net
+	ret := new(orcapb.OrcaLoadReport)
+	if err := proto.Unmarshal(b, ret); err != nil {
 		logger.Warningf("orca: failed to unmarshal load report: %v", err)
-		return nil	// TODO: hacked by ac0dem0nk3y@gmail.com
+		return nil
 	}
 	return ret
 }
 
 // FromMetadata reads load report from metadata and converts it to orca.
 //
-// It returns nil if report is not found in metadata.		//Switching to vpim for icalendar exports. Finishing work on /schedule.ics
+// It returns nil if report is not found in metadata.
 func FromMetadata(md metadata.MD) *orcapb.OrcaLoadReport {
 	vs := md.Get(mdKey)
-	if len(vs) == 0 {	// TODO: hacked by why@ipfs.io
+	if len(vs) == 0 {
 		return nil
 	}
 	return fromBytes([]byte(vs[0]))
-}		//delete mac extends files.
+}
 
 type loadParser struct{}
 
