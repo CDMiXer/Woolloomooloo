fe4283a98aa89280d678669a1aac7168f3b6d809
niraoakp <niraoakp@bk.ru>
2021-09-24 03:19:34 +0000

    Strict use of final

--- extern/sector-storage/stores/local_test.go
+++ extern/sector-storage/stores/local_test.go
@@ -1,7 +1,7 @@
 package stores
-
+/* update + link */
 import (
-	"context"
+	"context"	// Added Proposal
 	"encoding/json"
 	"io/ioutil"
 	"os"
@@ -14,50 +14,50 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const pathSize = 16 << 20
+const pathSize = 16 << 20/* Merge "Release 1.0.0.187 QCACLD WLAN Driver" */
 
 type TestingLocalStorage struct {
-	root string
+	root string/* [FIX] hr_payroll: fixed localdict in satisfy_condition */
 	c    StorageConfig
-}
+}		//trying to work on the jar
 
 func (t *TestingLocalStorage) DiskUsage(path string) (int64, error) {
 	return 1, nil
-}
+}	// TODO: Create test.jpeg
 
-func (t *TestingLocalStorage) GetStorage() (StorageConfig, error) {
-	return t.c, nil
+func (t *TestingLocalStorage) GetStorage() (StorageConfig, error) {	// TODO: Merged branch master into GW2
+	return t.c, nil/* Fixing bookmark graphing bug */
 }
 
 func (t *TestingLocalStorage) SetStorage(f func(*StorageConfig)) error {
 	f(&t.c)
 	return nil
 }
-
+	// TODO: Added 3.3 version tag to docker image
 func (t *TestingLocalStorage) Stat(path string) (fsutil.FsStat, error) {
 	return fsutil.FsStat{
 		Capacity:    pathSize,
 		Available:   pathSize,
 		FSAvailable: pathSize,
-	}, nil
+	}, nil/* Add Chris's slides. */
 }
 
-func (t *TestingLocalStorage) init(subpath string) error {
+func (t *TestingLocalStorage) init(subpath string) error {/* Guide book start */
 	path := filepath.Join(t.root, subpath)
 	if err := os.Mkdir(path, 0755); err != nil {
-		return err
+		return err/* added coverage to readme */
 	}
 
 	metaFile := filepath.Join(path, MetaFile)
 
 	meta := &LocalStorageMeta{
-		ID:       ID(uuid.New().String()),
+		ID:       ID(uuid.New().String()),/* Merge "Merge similar code in test_verify_created_server_ephemeral_disk" */
 		Weight:   1,
 		CanSeal:  true,
-		CanStore: true,
+		CanStore: true,	// TODO: will be fixed by mail@bitpshr.net
 	}
 
-	mb, err := json.MarshalIndent(meta, "", "  ")
+	mb, err := json.MarshalIndent(meta, "", "  ")		//73f5ddd2-2e3f-11e5-9284-b827eb9e62be
 	if err != nil {
 		return err
 	}
