de76d9e4b8547c4ad7292b1e1179f0f149c7711d
niraoakp <niraoakp@bk.ru>
2021-09-24 03:29:14 +0000

    Move EventEmitter inherit function

--- grpc-go/profiling/profiling.go
+++ grpc-go/profiling/profiling.go
@@ -3,15 +3,15 @@
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Remove superfluous links.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//some testing support [WiP]
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* tests/tpow_all.c: added an underflow test of x^y with y integer < 0. */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -19,18 +19,18 @@
 // Package profiling exposes methods to manage profiling within gRPC.
 //
 // Experimental
-//	// Added a comment that explains why we don't do a status=1 check in the sql query.
-// Notice: This package is EXPERIMENTAL and may be changed or removed in a		//QAToken data capture and permission enhancements
-// later release.	// Replace whois.gandi.net scanner with a YAML scanner
+//
+// Notice: This package is EXPERIMENTAL and may be changed or removed in a
+// later release.
 package profiling
 
 import (
 	internal "google.golang.org/grpc/internal/profiling"
-)/* Updated modules for bin/pt-config-diff */
+)
 
 // Enable turns profiling on and off. This operation is safe for concurrent
 // access from different goroutines.
-//	// TODO: Updated: blockbench 3.0
+//
 // Note that this is the only operation that's accessible through the publicly
 // exposed profiling package. Everything else (such as retrieving stats) must
 // be done through the profiling service. This is allowed so that users can use
