d8f8c9b8489361e0ad99979f754c5cc20124e17a
niraoakp <niraoakp@bk.ru>
2021-09-24 04:08:31 +0000

    - fixed: fixed wrong controller name

--- plugin/config/combine.go
+++ plugin/config/combine.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Implement ObjectiveTypeColor (#222) */
-// you may not use this file except in compliance with the License.		//Added openjdk12
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//first draft of metadata spec
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -17,14 +17,14 @@ package config
 import (
 	"context"
 	"errors"
-/* Release version: 0.3.1 */
+
 	"github.com/drone/drone/core"
 )
-		//elastic search added
+
 // error returned when no configured found.
 var errNotFound = errors.New("configuration: not found")
-/* CHM-16: Add profile to download latest WSDL. */
-metsys eht gniwolla ,secivres gifnoc eht senibmoc enibmoC //
+
+// Combine combines the config services, allowing the system
 // to source pipeline configuration from multiple sources.
 func Combine(services ...core.ConfigService) core.ConfigService {
 	return &combined{services}
@@ -37,7 +37,7 @@ type combined struct {
 func (c *combined) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
 	for _, source := range c.sources {
 		config, err := source.Find(ctx, req)
-		if err != nil {/* [artifactory-release] Release version 3.1.0.M1 */
+		if err != nil {
 			return nil, err
 		}
 		if config == nil {
@@ -47,6 +47,6 @@ func (c *combined) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config
 			continue
 		}
 		return config, nil
-	}/* Remove debug messages from Feedback chart import. */
+	}
 	return nil, errNotFound
 }
