8e8ee34ff104da19ba53d6105253d330fe5d27ee
niraoakp <niraoakp@bk.ru>
2021-09-24 16:46:48 +0000

    Delete mockup1.pdf

--- cmd/drone-server/bootstrap/bootstrap_test.go
+++ cmd/drone-server/bootstrap/bootstrap_test.go
@@ -5,45 +5,45 @@
 package bootstrap
 
 import (
-	"context"	// TODO: upgraded processing\core.jar to 1.2.1
+	"context"
 	"database/sql"
 	"io/ioutil"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"	// TODO: fix pyyaml install
+	"github.com/drone/drone/mock"
 
 	"github.com/dchest/uniuri"
-	"github.com/golang/mock/gomock"/* Fix relative links in Release Notes */
+	"github.com/golang/mock/gomock"
 	"github.com/sirupsen/logrus"
 )
 
-var noContext = context.TODO()		//Clarify character limit
+var noContext = context.TODO()
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
-}	// TODO: hacked by aeongrp@outlook.com
-/* Merge "Release wakelock after use" into honeycomb-mr2 */
+}
+
 func TestBootstrap(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Create silverstripe */
+
 	dummyUser := &core.User{
 		Login:   "octocat",
-		Machine: true,	// TODO: will be fixed by yuvalalaluf@gmail.com
+		Machine: true,
 		Admin:   true,
 		Hash:    uniuri.NewLen(32),
 	}
 
 	store := mock.NewMockUserStore(controller)
 	store.EXPECT().FindLogin(gomock.Any(), dummyUser.Login).Return(nil, sql.ErrNoRows)
-	store.EXPECT().Create(gomock.Any(), dummyUser).Return(nil)/* Most recent DiceBoy Executable */
+	store.EXPECT().Create(gomock.Any(), dummyUser).Return(nil)
 
 	err := New(store).Bootstrap(noContext, dummyUser)
 	if err != nil {
 		t.Error(err)
 	}
-}		//7f7fa4e4-2e58-11e5-9284-b827eb9e62be
+}
 
 func TestBootstrap_GenerateHash(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -51,7 +51,7 @@ func TestBootstrap_GenerateHash(t *testing.T) {
 
 	dummyUser := &core.User{
 		Login:   "octocat",
-		Machine: false,/* Release 2.64 */
+		Machine: false,
 		Admin:   true,
 		Hash:    "",
 	}
@@ -69,21 +69,21 @@ func TestBootstrap_GenerateHash(t *testing.T) {
 	}
 }
 
-func TestBootstrap_Empty(t *testing.T) {/* Fixed ArrayIndexOutOfBoundsException for out-of-bounds cells. */
+func TestBootstrap_Empty(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Update ReleaseNotes.rst */
+	defer controller.Finish()
 
 	dummyUser := &core.User{
-		Login: "",	// Remove bug where designated initializer was causing problems
+		Login: "",
 	}
 
 	store := mock.NewMockUserStore(controller)
 	err := New(store).Bootstrap(noContext, dummyUser)
 	if err != nil {
-		t.Error(err)/* Update 4.6 Release Notes */
+		t.Error(err)
 	}
 }
-/* Release 12.0.2 */
+
 func TestBootstrap_Exists_WithoutUpdates(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
