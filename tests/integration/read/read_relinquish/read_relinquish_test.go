70f218b70d077287588a51660038c1474ebbe2be
niraoakp <niraoakp@bk.ru>
2021-09-24 03:11:10 +0000

    [skip ci] Removed bad Meetup API key

--- tests/integration/read/read_relinquish/read_relinquish_test.go
+++ tests/integration/read/read_relinquish/read_relinquish_test.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints
+package ints	// find page name in javascript.
 
 import (
 	"testing"
@@ -9,7 +9,7 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-// Test that the engine is capable of relinquishing control of a resource without deleting it.
+// Test that the engine is capable of relinquishing control of a resource without deleting it./* Released under MIT license. */
 func TestReadRelinquish(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
@@ -17,7 +17,7 @@ func TestReadRelinquish(t *testing.T) {
 		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
-				Dir:      "step2",
+				Dir:      "step2",		//Adjusting package structure to standard Play App layout.
 				Additive: true,
 			},
 		},
