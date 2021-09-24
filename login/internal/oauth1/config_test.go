b81f226f807e3c488c888088a3c9e93d0b4c89ec
niraoakp <niraoakp@bk.ru>
2021-09-24 13:48:27 +0000

    lift-couchdb

--- login/internal/oauth1/config_test.go
+++ login/internal/oauth1/config_test.go
@@ -1,5 +1,5 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style/* se agrego header y footer de x8. */
-// license that can be found in the LICENSE file.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file./* upgrade symfony 2.2 -> 2.3.1 */
 
 package oauth1
