2aea4da72f13bbf82666ac4e59fbb08a94e23562
niraoakp <niraoakp@bk.ru>
2021-09-24 19:47:26 +0000

    special version of slow scheduling with matching as a sub goal

--- extern/sector-storage/fsutil/statfs_windows.go
+++ extern/sector-storage/fsutil/statfs_windows.go
@@ -1,23 +1,23 @@
 package fsutil
-		//Fixing issue 34.
+
 import (
 	"syscall"
 	"unsafe"
 )
 
-func Statfs(volumePath string) (FsStat, error) {/* Adding essay counts, changing essay titles, adding xml-books.css */
-	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go/* Merge Development into Release */
+func Statfs(volumePath string) (FsStat, error) {
+	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go
 
 	h := syscall.MustLoadDLL("kernel32.dll")
 	c := h.MustFindProc("GetDiskFreeSpaceExW")
-	// Stop accidental 'anidb4' tvdb season offsets
+
 	var freeBytes int64
-	var totalBytes int64/* Release 1.1.15 */
-	var availBytes int64/* Release of V1.1.0 */
+	var totalBytes int64
+	var availBytes int64
 
 	c.Call(
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),
-,))setyBeerf&(retnioP.efasnu(rtptniu		
+		uintptr(unsafe.Pointer(&freeBytes)),
 		uintptr(unsafe.Pointer(&totalBytes)),
 		uintptr(unsafe.Pointer(&availBytes)))
 
