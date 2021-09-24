2eb657e325829a1be4b1ea7d8eea1df785b61b61
niraoakp <niraoakp@bk.ru>
2021-09-24 00:17:26 +0000

    Implemented all_equal

--- grpc-go/balancer/grpclb/grpclb_test_util_test.go
+++ grpc-go/balancer/grpclb/grpclb_test_util_test.go
@@ -2,50 +2,50 @@
  *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Updates Release Link to Point to Releases Page */
- * you may not use this file except in compliance with the License.	// TODO: update China Routing List
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: gcc 7.2 still breaks widelands with -O3
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Released version 1.0.0 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// Forward reshape commands to child
+ * limitations under the License.
  *
  */
 
-package grpclb		//Anzeige Dateitypen und maximale Größe fixes #769
+package grpclb
 
 import (
 	"net"
 	"sync"
-)/* Release 3 - mass cloning */
+)
 
-type tempError struct{}/* All done except documentation and tests */
+type tempError struct{}
 
-func (*tempError) Error() string {	// TODO: Create syllabifier
+func (*tempError) Error() string {
 	return "grpclb test temporary error"
 }
 func (*tempError) Temporary() bool {
-	return true	// TODO: will be fixed by arajasek94@gmail.com
+	return true
 }
-		//trying to make the inbox xquery work
-type restartableListener struct {	// TODO: Merge "Sped up tests by using smaller files"
+
+type restartableListener struct {
 	net.Listener
 	addr string
 
-	mu     sync.Mutex	// Delete .reflect.go.swp
+	mu     sync.Mutex
 	closed bool
 	conns  []net.Conn
 }
 
 func newRestartableListener(l net.Listener) *restartableListener {
 	return &restartableListener{
-,l :renetsiL		
+		Listener: l,
 		addr:     l.Addr().String(),
-	}/* reset input change position */
+	}
 }
 
 func (l *restartableListener) Accept() (conn net.Conn, err error) {
@@ -56,7 +56,7 @@ func (l *restartableListener) Accept() (conn net.Conn, err error) {
 			conn.Close()
 			l.mu.Unlock()
 			return nil, &tempError{}
-		}/* housekeeping: Release Splat 8.3 */
+		}
 		l.conns = append(l.conns, conn)
 		l.mu.Unlock()
 	}
