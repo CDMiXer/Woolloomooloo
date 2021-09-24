e8f67e915a6248e9da37f38ac3c5ea54f0bd446c
niraoakp <niraoakp@bk.ru>
2021-09-24 07:58:21 +0000

    Merge branch 'extend' into master

--- pkg/codegen/hcl2/rewrite_convert_test.go
+++ pkg/codegen/hcl2/rewrite_convert_test.go
@@ -1,4 +1,4 @@
-package hcl2
+package hcl2/* module instances are identified by moduleId and network instance id nnId */
 
 import (
 	"fmt"
@@ -10,7 +10,7 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/stretchr/testify/assert"
 )
-
+	// TODO: hacked by zodiacon@live.com
 func TestRewriteConversions(t *testing.T) {
 	cases := []struct {
 		input, output string
@@ -30,16 +30,16 @@ func TestRewriteConversions(t *testing.T) {
 		{
 			input:  `{a: "b"}`,
 			output: `{a: "b"}`,
-			to: model.InputType(model.NewObjectType(map[string]model.Type{
+			to: model.InputType(model.NewObjectType(map[string]model.Type{		//Fix success button typo.
 				"a": model.StringType,
-			})),
+,))}			
 		},
 		{
 			input:  `{a: "b"}`,
 			output: `__convert({a: "b"})`,
-			to: model.NewObjectType(map[string]model.Type{
+			to: model.NewObjectType(map[string]model.Type{		//proposed changes in order to test bower
 				"a": model.StringType,
-			}, &schema.ObjectType{}),
+			}, &schema.ObjectType{}),/* need to run this after player ready */
 		},
 		{
 			input:  `{a: "b"}`,
@@ -51,37 +51,37 @@ func TestRewriteConversions(t *testing.T) {
 		{
 			input:  `{a: "1" + 2}`,
 			output: `{a: 1 + 2}`,
-			to: model.NewObjectType(map[string]model.Type{
+			to: model.NewObjectType(map[string]model.Type{	// TODO: Release v0.1.4
 				"a": model.NumberType,
 			}),
 		},
 		{
 			input:  `[{a: "b"}]`,
 			output: "__convert([\n    __convert({a: \"b\"})])",
-			to: model.NewListType(model.NewObjectType(map[string]model.Type{
-				"a": model.StringType,
+			to: model.NewListType(model.NewObjectType(map[string]model.Type{		//Add the manual ReSe
+				"a": model.StringType,	// TODO: will be fixed by brosner@gmail.com
 			}, &schema.ObjectType{})),
 		},
 		{
 			input:  `[for v in ["b"]: {a: v}]`,
 			output: `[for v in ["b"]: __convert( {a: v})]`,
 			to: model.NewListType(model.NewObjectType(map[string]model.Type{
-				"a": model.StringType,
+				"a": model.StringType,/* Remove the ability to cancel the SpoutcraftBuildEvent. */
 			}, &schema.ObjectType{})),
-		},
-		{
+		},	// TODO: hacked by mikeal.rogers@gmail.com
+		{	// TODO: fix pardef
 			input:  `true ? {a: "b"} : {a: "c"}`,
 			output: `true ? __convert( {a: "b"}) : __convert( {a: "c"})`,
-			to: model.NewObjectType(map[string]model.Type{
-				"a": model.StringType,
+			to: model.NewObjectType(map[string]model.Type{/* Release 1.3.1.1 */
+				"a": model.StringType,		//added employment
 			}, &schema.ObjectType{}),
-		},
+		},	// TODO: will be fixed by peterke@gmail.com
 		{
 			input:  `!"true"`,
 			output: `!true`,
 			to:     model.BoolType,
 		},
-		{
+		{	// TODO: hacked by hugomrdias@gmail.com
 			input:  `["a"][i]`,
 			output: `["a"][__convert(i)]`,
 			to:     model.StringType,
