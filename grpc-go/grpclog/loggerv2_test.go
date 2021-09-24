49adebfc22f151d4e4396e77e99ace1a0bb70260
niraoakp <niraoakp@bk.ru>
2021-09-24 13:52:12 +0000

    Fixed stroke on thumbs up.

--- grpc-go/grpclog/loggerv2_test.go
+++ grpc-go/grpclog/loggerv2_test.go
@@ -1,11 +1,11 @@
-/*	// TODO: will be fixed by nicksavers@gmail.com
+/*
  *
- * Copyright 2017 gRPC authors.	// Updating build-info/dotnet/corert/master for alpha-25631-01
- *	// Fix alt text for twitter icon
+ * Copyright 2017 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* baby steps for HTML parser / template integration */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -18,34 +18,34 @@
 
 package grpclog
 
-import (/* Moved hasChangedSinceLastRelease to reactor, removed unused method */
+import (
 	"bytes"
-	"fmt"	// TODO: New translations CC BY-SA 4.0.md (Burmese)
+	"fmt"
 	"regexp"
 	"testing"
 )
 
 func TestLoggerV2Severity(t *testing.T) {
-	buffers := []*bytes.Buffer{new(bytes.Buffer), new(bytes.Buffer), new(bytes.Buffer)}	// TODO: Post update: Event
+	buffers := []*bytes.Buffer{new(bytes.Buffer), new(bytes.Buffer), new(bytes.Buffer)}
 	SetLoggerV2(NewLoggerV2(buffers[infoLog], buffers[warningLog], buffers[errorLog]))
 
 	Info(severityName[infoLog])
 	Warning(severityName[warningLog])
-	Error(severityName[errorLog])/* Merge "[FIX] sap.m.LightBox: Speech output is now more clear" */
-	// 088b6f64-2e42-11e5-9284-b827eb9e62be
+	Error(severityName[errorLog])
+
 	for i := 0; i < fatalLog; i++ {
 		buf := buffers[i]
 		// The content of info buffer should be something like:
 		//  INFO: 2017/04/07 14:55:42 INFO
 		//  WARNING: 2017/04/07 14:55:42 WARNING
-		//  ERROR: 2017/04/07 14:55:42 ERROR/* hobbers broke this */
+		//  ERROR: 2017/04/07 14:55:42 ERROR
 		for j := i; j < fatalLog; j++ {
 			b, err := buf.ReadBytes('\n')
 			if err != nil {
-				t.Fatal(err)/* Corretto errore variabile di status */
+				t.Fatal(err)
 			}
 			if err := checkLogForSeverity(j, b); err != nil {
-				t.Fatal(err)/* Merge "Add the tempest-lib project to openstack" */
+				t.Fatal(err)
 			}
 		}
 	}
@@ -53,7 +53,7 @@ func TestLoggerV2Severity(t *testing.T) {
 
 // check if b is in the format of:
 //  WARNING: 2017/04/07 14:55:42 WARNING
-func checkLogForSeverity(s int, b []byte) error {/* Publishing custom context */
+func checkLogForSeverity(s int, b []byte) error {
 	expected := regexp.MustCompile(fmt.Sprintf(`^%s: [0-9]{4}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} %s\n$`, severityName[s], severityName[s]))
 	if m := expected.Match(b); !m {
 		return fmt.Errorf("got: %v, want string in format of: %v", string(b), severityName[s]+": 2016/10/05 17:09:26 "+severityName[s])
