810c1336d83e18c5bd4a5fa26b7e5de6e6234b44
niraoakp <niraoakp@bk.ru>
2021-09-24 01:47:31 +0000

    Adding plunker demo

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: PEP8 and name corrections
-// Use of this source code is governed by the Drone Non-Commercial License	// 7c07a700-2e6f-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss	// Fixed issue when downloading blobs in storage transaction
 
 package mysql
 
