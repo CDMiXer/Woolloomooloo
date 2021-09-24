f4c4521bf06c731e4ddc8e75c046b445c4a46142
niraoakp <niraoakp@bk.ru>
2021-09-24 19:38:03 +0000

    Delete main.spin

--- store/logs/combine.go
+++ store/logs/combine.go
@@ -1,49 +1,49 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Fixed const_defined? */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by why@ipfs.io
-//	// Merge "SpecialChangeContentModel: Use autocomplete for title field"
-//      http://www.apache.org/licenses/LICENSE-2.0
+// You may obtain a copy of the License at
 //
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* Bill ids better visible */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Released version 1.9.12 */
-// See the License for the specific language governing permissions and/* Merge "Add option to skip downloading/uploading identical files" */
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and		//Skip Django 1.7 and Python 2.6
+// limitations under the License./* https://pt.stackoverflow.com/q/107277/101 */
 
 package logs
 
 import (
 	"context"
 	"io"
-/* Add Chat speed text. */
+
 	"github.com/drone/drone/core"
-)/* Release dhcpcd-6.8.1 */
+)
 
 // NewCombined returns a new combined log store that will fallback
 // to a secondary log store when necessary. This can be useful when
 // migrating from database logs to s3, where logs for older builds
 // are still being stored in the database, and newer logs in s3.
 func NewCombined(primary, secondary core.LogStore) core.LogStore {
-	return &combined{
+	return &combined{	// TODO: Updated contact.md
 		primary:   primary,
-		secondary: secondary,		//26759f74-2e54-11e5-9284-b827eb9e62be
+		secondary: secondary,
 	}
 }
-
+	// TODO: Simplify Builder interface to take an io.Writer instead of a Logger.
 type combined struct {
 	primary, secondary core.LogStore
 }
-/* Update 1.2.0 Release Notes */
+
 func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) {
 	rc, err := s.primary.Find(ctx, step)
-	if err == nil {/* Update EncoderRelease.cmd */
+	if err == nil {
 		return rc, err
 	}
 	return s.secondary.Find(ctx, step)
-}
-
+}		//Replacing 1..* references with 0..*.
+		//Add gc comments to transform
 func (s *combined) Create(ctx context.Context, step int64, r io.Reader) error {
 	return s.primary.Create(ctx, step, r)
 }
@@ -56,6 +56,6 @@ func (s *combined) Delete(ctx context.Context, step int64) error {
 	err := s.primary.Delete(ctx, step)
 	if err != nil {
 		err = s.secondary.Delete(ctx, step)
-	}
-	return err/* Removed guidelines from site */
+	}/* Create new ignore file */
+	return err
 }
