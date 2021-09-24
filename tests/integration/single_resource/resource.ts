1b38997a8e5d5e42fcd78414af15546834071a2f
niraoakp <niraoakp@bk.ru>
2021-09-24 14:37:21 +0000

    f625d3b8-2e65-11e5-9284-b827eb9e62be

--- tests/integration/single_resource/resource.ts
+++ tests/integration/single_resource/resource.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* Release 1.2.0.8 */
+import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
 
@@ -9,7 +9,7 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
 
     public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    constructor() {/* Release of eeacms/www:18.10.13 */
+    constructor() {
         this.create = async (inputs: any) => {
             return {
                 id: (currentID++).toString(),
@@ -25,9 +25,9 @@ export class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
         this.state = props.state;
-    }/* Merge "Release 3.2.3.373 Prima WLAN Driver" */
+    }
 }
-/* Create Agile Stories */
+
 export interface ResourceProps {
     state?: any; // arbitrary state bag that can be updated without replacing.
 }
