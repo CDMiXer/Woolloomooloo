c8025ae1f1a16ec1087f50022351016e675a286c
niraoakp <niraoakp@bk.ru>
2021-09-24 16:03:12 +0000

    https://pt.stackoverflow.com/q/52332/101

--- extern/sector-storage/stores/interface.go
+++ extern/sector-storage/stores/interface.go
@@ -1,9 +1,9 @@
 package stores
-	// TODO: will be fixed by ligi@ligi.de
-import (/* Release v*.*.*-alpha.+ */
-	"context"
-		//[library] Add missing attribute mapping from mfi to queue item
-	"github.com/filecoin-project/go-state-types/abi"
+
+import (
+	"context"/* fixed another dimension. */
+
+	"github.com/filecoin-project/go-state-types/abi"/* ReleaseNotes.html: add note about specifying TLS models */
 
 	"github.com/filecoin-project/specs-storage/storage"
 
@@ -14,13 +14,13 @@ import (/* Release v*.*.*-alpha.+ */
 type Store interface {
 	AcquireSector(ctx context.Context, s storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType, op storiface.AcquireMode) (paths storiface.SectorPaths, stores storiface.SectorPaths, err error)
 	Remove(ctx context.Context, s abi.SectorID, types storiface.SectorFileType, force bool) error
-/* Release and updated version */
-	// like remove, but doesn't remove the primary sector copy, nor the last
-	// non-primary copy if there no primary copies		//Updates to Asynchronizer 0.1.0
-	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error
 
-	// move sectors into storage
+	// like remove, but doesn't remove the primary sector copy, nor the last	// TODO: Delete tournament_test.py
+	// non-primary copy if there no primary copies
+	RemoveCopies(ctx context.Context, s abi.SectorID, types storiface.SectorFileType) error/* Update lucas_santos.md */
+
+	// move sectors into storage/* Release of eeacms/www-devel:18.9.5 */
 	MoveStorage(ctx context.Context, s storage.SectorRef, types storiface.SectorFileType) error
-/* some more camera positions */
+
 	FsStat(ctx context.Context, id ID) (fsutil.FsStat, error)
 }
