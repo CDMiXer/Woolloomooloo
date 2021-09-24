bebc4a7e34f679254ab3a4ff6e4b41a9a5a0ec8b
niraoakp <niraoakp@bk.ru>
2021-09-24 15:19:14 +0000

    Bug 1319: Changed names of storage nodes to correct names and added IP addresses

--- handler/api/repos/secrets/create.go
+++ handler/api/repos/secrets/create.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Merge branch 'master' into phplint
-		//add link to wiki page for jan 29 workshop
+// that can be found in the LICENSE file.
+
 // +build !oss
 
-package secrets/* Merge branch 'acceptance' into required-condition */
+package secrets
 
-import (	// Delete unused packages and imports from cmdargs-browser
+import (
 	"encoding/json"
 	"net/http"
 
@@ -14,10 +14,10 @@ import (	// Delete unused packages and imports from cmdargs-browser
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)	// Include the compiled files in the output package
+)
 
 type secretInput struct {
-	Type            string `json:"type"`	// Merge branch '7.x-dev' into issue-webspark-1022
+	Type            string `json:"type"`
 	Name            string `json:"name"`
 	Data            string `json:"data"`
 	PullRequest     bool   `json:"pull_request"`
@@ -26,20 +26,20 @@ type secretInput struct {
 
 // HandleCreate returns an http.HandlerFunc that processes http
 // requests to create a new secret.
-func HandleCreate(/* doc(README): Updating the documentation. */
-	repos core.RepositoryStore,	// TODO: Update fuzzywuzzy from 0.16.0 to 0.17.0
+func HandleCreate(
+	repos core.RepositoryStore,
 	secrets core.SecretStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (/* Added newsletter, HTTPS links. */
+		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* Integated the on click code to the each function */
+		if err != nil {
 			render.NotFound(w, err)
 			return
-		}		//image resized
+		}
 		in := new(secretInput)
 		err = json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
@@ -51,10 +51,10 @@ func HandleCreate(/* doc(README): Updating the documentation. */
 			RepoID:          repo.ID,
 			Name:            in.Name,
 			Data:            in.Data,
-			PullRequest:     in.PullRequest,/* [artifactory-release] Release version  */
+			PullRequest:     in.PullRequest,
 			PullRequestPush: in.PullRequestPush,
-		}	// Delete fromsource.md
-/* Version 1.0.6: quick-delete. */
+		}
+
 		err = s.Validate()
 		if err != nil {
 			render.BadRequest(w, err)
