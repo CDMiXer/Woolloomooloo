e1d6cd4600665add57ce60a8eb45e58ebcaf0ab0
niraoakp <niraoakp@bk.ru>
2021-09-24 00:10:23 +0000

    good riddance strange  `require './looper'` bug

--- plugin/validator/noop.go
+++ plugin/validator/noop.go
@@ -4,24 +4,24 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// Fix wrapLinks call
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by ng8eke@163.com
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss/* 44447952-2e50-11e5-9284-b827eb9e62be */
+// +build oss
 
 package validator
 
 import (
 	"context"
-	// TODO: hacked by arajasek94@gmail.com
+
 	"github.com/drone/drone/core"
 )
 
 type noop struct{}
-		//Delete fluxo.jpg
+
 func (noop) Validate(context.Context, *core.ValidateArgs) error { return nil }
