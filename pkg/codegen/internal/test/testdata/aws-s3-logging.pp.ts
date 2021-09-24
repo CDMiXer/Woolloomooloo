b6a2ef0e3d48fe88db733744fdc7ea970b9b7bb3
niraoakp <niraoakp@bk.ru>
2021-09-24 15:52:32 +0000

    changed createFolder

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
@@ -1,8 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";	// TODO: Minor docstring fixes
-
+import * as aws from "@pulumi/aws";
+		//also display status of computer
 const logs = new aws.s3.Bucket("logs", {});
 const bucket = new aws.s3.Bucket("bucket", {loggings: [{
     targetBucket: logs.bucket,
 }]});
-export const targetBucket = bucket.loggings[0].targetBucket;
+export const targetBucket = bucket.loggings[0].targetBucket;		//hotfix 500 error
