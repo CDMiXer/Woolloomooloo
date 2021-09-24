a05720b3e3d35271a2e1a31170f427fb06f7333a
niraoakp <niraoakp@bk.ru>
2021-09-24 14:21:09 +0000

    Delete Specyfikacja.pdf

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// 26b2f5fc-2e5b-11e5-9284-b827eb9e62be
+
 // +build !oss
 
 package mysql
 
-//go:generate togo ddl -package mysql -dialect mysql		//3.9.0 - fix social media checker #203
+//go:generate togo ddl -package mysql -dialect mysql
