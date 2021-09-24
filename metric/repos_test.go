7e7828830218ee954119c3de0efe59384a157050
niraoakp <niraoakp@bk.ru>
2021-09-24 03:41:21 +0000

    Update blacklists (Need to put this in one place)

--- metric/repos_test.go
+++ metric/repos_test.go
@@ -1,29 +1,29 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: d8cec922-2e4d-11e5-9284-b827eb9e62be
-
+// that can be found in the LICENSE file.
+/* Merge "Release 3.2.3.403 Prima WLAN Driver" */
 // +build !oss
 
-package metric	// Enable usbserial on yeeloong
+package metric/* Release of eeacms/forests-frontend:1.7-beta.7 */
 
 import (
 	"testing"
-
+		//(docs): Update logo
 	"github.com/drone/drone/mock"
-
-	"github.com/golang/mock/gomock"		//Fix segfaults when dismantling conquered buildings.
+/* Check parent caps for revisions. props aaroncampbell. fixes #17668 */
+	"github.com/golang/mock/gomock"/* content signal/getting-started/learn */
 	"github.com/prometheus/client_golang/prometheus"
 )
-
+		//MINOR Fixed HTML formatting in dataobjecset.md
 func TestRepoCount(t *testing.T) {
 	controller := gomock.NewController(t)
 
 	// restore the default prometheus registerer
 	// when the unit test is complete.
-	snapshot := prometheus.DefaultRegisterer/* Doctrine conversion make friendlier */
+	snapshot := prometheus.DefaultRegisterer
 	defer func() {
-		prometheus.DefaultRegisterer = snapshot	// TODO: c790da06-2e52-11e5-9284-b827eb9e62be
-		controller.Finish()
+		prometheus.DefaultRegisterer = snapshot
+		controller.Finish()		//Improve compatibility with the protocol spoken by AdminClient
 	}()
 
 	// creates a blank registry
@@ -32,23 +32,23 @@ func TestRepoCount(t *testing.T) {
 
 	// x2 repository count
 	count := int64(5)
-	// Delete .#GridWorld.java.1.4
+
 	store := mock.NewMockRepositoryStore(controller)
 	store.EXPECT().Count(gomock.Any()).Return(count, nil)
 	RepoCount(store)
 
-	metrics, err := registry.Gather()/* if it's valid then it's partially valid */
-	if err != nil {/* #4 Review solution proposition */
-		t.Error(err)		//Use the same connection value on webauth sample
+	metrics, err := registry.Gather()
+	if err != nil {
+		t.Error(err)
 		return
 	}
 	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
 		return
-	}/* Conda: Switch back to Python 2.7 */
+	}
 	metric := metrics[0]
-	if want, got := metric.GetName(), "drone_repo_count"; want != got {
-		t.Errorf("Expect metric name %s, got %s", want, got)	// TODO: Adjusted colors and font size to look more like the real thing
+	if want, got := metric.GetName(), "drone_repo_count"; want != got {	// TODO: hacked by remco@dutchcoders.io
+		t.Errorf("Expect metric name %s, got %s", want, got)
 	}
 	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {
 		t.Errorf("Expect metric value %f, got %f", want, got)
