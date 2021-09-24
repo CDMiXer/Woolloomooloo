45c02e36f7bb9d616ad59839588dfe0301e3b622
niraoakp <niraoakp@bk.ru>
2021-09-24 11:55:34 +0000

    Update version constraints on Jane Street packages (#6710)

--- tests/integration/aliases/nodejs/retype_component/step1/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step1/index.ts
@@ -1,19 +1,19 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release 4.1.0 */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {
+class Resource extends pulumi.ComponentResource {	// Some code and documentation fixes
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
+        super("my:module:Resource", name, {}, opts);/* Update dossiermgt css */
     }
-}/* Release v0.0.1beta4. */
-
+}
+		//Merge branch 'develop' into issue/8482-privacy-settings-crash
 // Scenario #4 - change the type of a component
-class ComponentFour extends pulumi.ComponentResource {		//Borrado logico de maestros
-    resource: Resource;	// TODO: hacked by seth@sethvargo.com
+class ComponentFour extends pulumi.ComponentResource {
+    resource: Resource;/* Feedback from Homebrew */
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFour", name, {}, opts);/* Trying to add language selection remembering */
+        super("my:module:ComponentFour", name, {}, opts);
         this.resource = new Resource("otherchild", {parent: this});
-    }/* Completed eq; added tests */
+    }		//added blueprint command
 }
 const comp4 = new ComponentFour("comp4");
