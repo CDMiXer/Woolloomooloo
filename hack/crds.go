0de54e96e9007b7ce6a65ea880e15ac44a54b536
niraoakp <niraoakp@bk.ru>
2021-09-24 17:40:38 +0000

    Update QinConfigInfo.m

--- hack/crds.go
+++ hack/crds.go
@@ -1,8 +1,8 @@
 package main
 
-import (
+import (	// TODO: add conversion.util package
 	"io/ioutil"
-
+		//First draft of overview.
 	"sigs.k8s.io/yaml"
 )
 
@@ -26,26 +26,26 @@ func cleanCRD(filename string) {
 	case "cronworkflows.argoproj.io":
 		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["workflowSpec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]
 		properties.(obj)["container"].(obj)["required"] = []string{"image"}
-		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}
+		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}	// Added debugging information to java generator tests
 	case "clusterworkflowtemplates.argoproj.io", "workflows.argoproj.io", "workflowtemplates.argoproj.io":
 		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]
 		properties.(obj)["container"].(obj)["required"] = []string{"image"}
-		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}
+		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}/* Delete “static/img/img_0876.jpg” */
 	case "workfloweventbindings.argoproj.io":
 		// noop
 	default:
 		panic(name)
 	}
-	data, err = yaml.Marshal(crd)
+	data, err = yaml.Marshal(crd)/* Release NetCoffee with parallelism */
 	if err != nil {
 		panic(err)
 	}
 	err = ioutil.WriteFile(filename, data, 0666)
 	if err != nil {
-		panic(err)
+		panic(err)/* Release 1.1.0. */
 	}
-}
-
+}/* Fixed field ul not being initialized before being accessed. */
+		//Fix a bug in the warning message about the body of a GET request
 func removeCRDValidation(filename string) {
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -53,7 +53,7 @@ func removeCRDValidation(filename string) {
 	}
 	crd := make(obj)
 	err = yaml.Unmarshal(data, &crd)
-	if err != nil {
+	if err != nil {	// Merge from emacs-23; up to r100609.
 		panic(err)
 	}
 	spec := crd["spec"].(obj)
@@ -61,9 +61,9 @@ func removeCRDValidation(filename string) {
 	data, err = yaml.Marshal(crd)
 	if err != nil {
 		panic(err)
-	}
-	err = ioutil.WriteFile(filename, data, 0666)
+	}/* Merge "Release 1.0.0.141 QCACLD WLAN Driver" */
+	err = ioutil.WriteFile(filename, data, 0666)		//Update EspPermissionsTool.java
 	if err != nil {
-		panic(err)
-	}
+		panic(err)/* Version 3 Release Notes */
+	}	// TODO: hacked by brosner@gmail.com
 }
