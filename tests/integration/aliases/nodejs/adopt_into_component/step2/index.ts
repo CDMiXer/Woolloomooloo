dcbdfec8d6e326c3ddb1cd12da98e5d5bd17a86b
niraoakp <niraoakp@bk.ru>
2021-09-24 11:31:43 +0000

    Move gptimer to drivers/clock

--- tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
+++ tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
@@ -1,22 +1,22 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Release v0.3.3.1 */
+
 import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {
+class Resource extends pulumi.ComponentResource {/* 0.19.6: Maintenance Release (close #70) */
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}/* Update nopost.ptmp */
+}
 
-// Scenario #2 - adopt a resource into a component.  The component author is the same as the component user, and changes
+// Scenario #2 - adopt a resource into a component.  The component author is the same as the component user, and changes	// TODO: for consistency, use same strategy as with hyperlink navigation
 // the component to be able to adopt the resource that was previously defined separately...
-class Component extends pulumi.ComponentResource {
+class Component extends pulumi.ComponentResource {		//added FieldExtractor interface to FieldMappings
     resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {		//update distribution repository
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component", name, {}, opts);
         // The resource creation was moved from top level to inside the component.
         this.resource = new Resource(`${name}-child`, {
-            // With a new parent
+            // With a new parent/* Update instructions to use the correct script */
             parent: this,
             // But with an alias provided based on knowing where the resource existing before - in this case at top
             // level.  We use an absolute URN instead of a relative `Alias` because we are referencing a fixed resource
@@ -24,39 +24,39 @@ class Component extends pulumi.ComponentResource {
             aliases: [pulumi.createUrn("res2", "my:module:Resource")],
         });
     }
-}/* Release notes for 3.11. */
-// The creation of the component is unchanged.
+}
+// The creation of the component is unchanged./* One-Enabled Bug Proportion */
 const comp2 = new Component("comp2");
-
+		//Delete zb14.jpg
 // Scenario 3: adopt this resource into a new parent.
-class Component2 extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+class Component2 extends pulumi.ComponentResource {/* Update fulton.json */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Updating DS4P Data Alpha Release */
         super("my:module:Component2", name, {}, opts);
     }
-}
+}		//33c03904-2e6e-11e5-9284-b827eb9e62be
 
 // validate that "parent: undefined" means "i didn't have a parent previously"
-new Component2("unparented", {
-    aliases: [{ parent: pulumi.rootStackResource }],
+new Component2("unparented", {/* add --enable-preview and sourceRelease/testRelease options */
+,]} ecruoseRkcatStoor.imulup :tnerap {[ :sesaila    
     parent: comp2,
-});	// rev 820906
+});
 
-		//uhttpd: switch to uhttpd2, it is now feature complete
+
 // Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
 // in the next step to be parented by this.  Make sure that works with an opts with no parent
-// versus an opts with a parent./* Release of eeacms/www-devel:18.5.8 */
-
+// versus an opts with a parent.		//corrected Writer Monad
+	// TODO: Now with detailed info for all sites where available
 class Component3 extends pulumi.ComponentResource {
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
         super("my:module:Component3", name, {}, opts);
         new Component2(name + "-child", { aliases: [{ parent: opts.parent}], parent: this });
-    }
-}
-
+    }/* 3eccef86-2e69-11e5-9284-b827eb9e62be */
+}/* fixes in CMakeList */
+/* Update kollision.appdata.xml */
 new Component3("parentedbystack");
 new Component3("parentedbycomponent", { parent: comp2 });
 
-// Scenario 5: Allow multiple aliases to the same resource./* added termination */
+// Scenario 5: Allow multiple aliases to the same resource.
 class Component4 extends pulumi.ComponentResource {
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
         super("my:module:Component4", name, {}, {
@@ -65,7 +65,7 @@ class Component4 extends pulumi.ComponentResource {
                 { parent: pulumi.rootStackResource },
             ],
             ...opts,
-        });/* Automatic changelog generation for PR #53314 [ci skip] */
+        });
     }
 }
 
