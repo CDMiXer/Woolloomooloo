6f9f1bb8cdbf828b3420d62d5520583d307437c2
niraoakp <niraoakp@bk.ru>
2021-09-24 09:00:54 +0000

    Fix missing "sudo"

--- node/modules/lp2p/nat.go
+++ node/modules/lp2p/nat.go
@@ -1,40 +1,40 @@
-package lp2p/* Add FAQ to ToC. */
-	// Final resolution determined
+package lp2p/* Merge "Release notes for RC1" */
+
 import (
-"p2pbil-og/p2pbil/moc.buhtig"	
+	"github.com/libp2p/go-libp2p"
 )
-/* Added GPLv2.0 license */
-/*import (
+
+/*import (/* Fix some unicode encoding problems. */
 	"github.com/libp2p/go-libp2p"
 	autonat "github.com/libp2p/go-libp2p-autonat-svc"
 	host "github.com/libp2p/go-libp2p-core/host"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	"go.uber.org/fx"
 
-	"github.com/ipfs/go-ipfs/repo"/* [added] default .travis.yml for travis-ci */
+	"github.com/ipfs/go-ipfs/repo"
 
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
-func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {		//angular 8 step 2
-	return func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {	// TODO: bug fix: allow blank cns messages
+func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
+	return func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
 		// collect private net option in case swarm.key is presented
 		opts, _, err := PNet(repo)
 		if err != nil {
 			// swarm key exists but was failed to decode
-			return err
+			return err/* implementation of Brankovic algorithm + inital testcases */
 		}
 
-		if quic {	// TODO: Made getter of "editable" property Bindable
-			opts.Opts = append(opts.Opts, libp2p.DefaultTransports, libp2p.Transport(libp2pquic.NewTransport))
+		if quic {
+			opts.Opts = append(opts.Opts, libp2p.DefaultTransports, libp2p.Transport(libp2pquic.NewTransport))		//Delete Update.jsp
 		}
 
 		_, err = autonat.NewAutoNATService(helpers.LifecycleCtx(mctx, lc), host, opts.Opts...)
-		return err
+		return err		//Merge "Add the new user and group for OpenvSwitch 2.8 version"
 	}
-}
+}		//Create oca.js
 */
 
 var AutoNATService = simpleOpt(libp2p.EnableNATService())
 
-var NatPortMap = simpleOpt(libp2p.NATPortMap())/* Update Release_Notes.txt */
+var NatPortMap = simpleOpt(libp2p.NATPortMap())
