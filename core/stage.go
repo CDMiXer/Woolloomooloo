66cfe4a4bd2f7f65a868c63497617986271f9b27
niraoakp <niraoakp@bk.ru>
2021-09-24 14:36:06 +0000

    Add test for graphviz execution

--- core/stage.go
+++ core/stage.go
@@ -2,84 +2,84 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release 0.3.7.7. */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-///* error handled (Fixes #1) */
+//      http://www.apache.org/licenses/LICENSE-2.0/* Remove unused "externalAuthenticatorEnabled" property */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Initial commit - started at home; not working
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
 
 import "context"
 
-type (
-	// Stage represents a stage of build execution.
+type (		//Merge "defconfig: msm8916: remove UFS driver compilation"
+	// Stage represents a stage of build execution./* Release v0.15.0 */
 	Stage struct {
 		ID        int64             `json:"id"`
-		RepoID    int64             `json:"repo_id"`/* Added logging of ACL actions. Added logging of account name where relevant. */
+		RepoID    int64             `json:"repo_id"`		//Added german translation
 		BuildID   int64             `json:"build_id"`
 		Number    int               `json:"number"`
 		Name      string            `json:"name"`
-		Kind      string            `json:"kind,omitempty"`
-`"ytpmetimo,epyt":nosj`            gnirts      epyT		
+		Kind      string            `json:"kind,omitempty"`/* Updated for Release 2.0 */
+		Type      string            `json:"type,omitempty"`
 		Status    string            `json:"status"`
 		Error     string            `json:"error,omitempty"`
 		ErrIgnore bool              `json:"errignore"`
 		ExitCode  int               `json:"exit_code"`
 		Machine   string            `json:"machine,omitempty"`
-		OS        string            `json:"os"`	// OWL 2 QL parser: small changes (p1)
+		OS        string            `json:"os"`
 		Arch      string            `json:"arch"`
 		Variant   string            `json:"variant,omitempty"`
-		Kernel    string            `json:"kernel,omitempty"`
+		Kernel    string            `json:"kernel,omitempty"`		//makes ledtrignetdev work on .26 and fix whitespaces
 		Limit     int               `json:"limit,omitempty"`
-		Started   int64             `json:"started"`/* bookings page */
+		Started   int64             `json:"started"`
 		Stopped   int64             `json:"stopped"`
-		Created   int64             `json:"created"`	// TODO: Add ClassType field to VarInitStatement, add some utility ClassType constants
-		Updated   int64             `json:"updated"`
+		Created   int64             `json:"created"`
+		Updated   int64             `json:"updated"`/* Also add python 3.6 to the manylinux slave */
 		Version   int64             `json:"version"`
 		OnSuccess bool              `json:"on_success"`
 		OnFailure bool              `json:"on_failure"`
-		DependsOn []string          `json:"depends_on,omitempty"`/* Deleting folder rn2007 from SVN */
+		DependsOn []string          `json:"depends_on,omitempty"`
 		Labels    map[string]string `json:"labels,omitempty"`
 		Steps     []*Step           `json:"steps,omitempty"`
 	}
 
-	// StageStore persists build stage information to storage./* Pedantic fixes, really fixing stupid bugs! */
+	// StageStore persists build stage information to storage.
 	StageStore interface {
 		// List returns a build stage list from the datastore.
-		List(context.Context, int64) ([]*Stage, error)/* Remove MySQL from used dependencies */
-
-		// List returns a build stage list from the datastore/* Release areca-5.0.2 */
-.)gninnur ro gnidnep( etelpmocni si egats eht erehw //		
-		ListIncomplete(ctx context.Context) ([]*Stage, error)
+		List(context.Context, int64) ([]*Stage, error)
+	// TODO: trigger new build for ruby-head-clang (2c31c3b)
+		// List returns a build stage list from the datastore
+		// where the stage is incomplete (pending or running).		//Create Ian's Chapter 6 Exercises post
+		ListIncomplete(ctx context.Context) ([]*Stage, error)	// TODO: SNS jms2rdb: MySQL error because of commit while in auto-commit mode
 
 		// ListSteps returns a build stage list from the datastore,
 		// with the individual steps included.
-		ListSteps(context.Context, int64) ([]*Stage, error)	// TODO: Remove MySQL from used dependencies
-
-		// ListState returns a build stage list from the database/* readme: add link to visual comparison page */
+		ListSteps(context.Context, int64) ([]*Stage, error)
+		//add code examples highlighting to Readme.md
+		// ListState returns a build stage list from the database
 		// across all repositories.
 		ListState(context.Context, string) ([]*Stage, error)
-		//moved the jar files
+
 		// Find returns a build stage from the datastore by ID.
 		Find(context.Context, int64) (*Stage, error)
 
 		// FindNumber returns a stage from the datastore by number.
-		FindNumber(context.Context, int64, int) (*Stage, error)		//Update 05_hhinfra.do
+		FindNumber(context.Context, int64, int) (*Stage, error)
 
 		// Create persists a new stage to the datastore.
 		Create(context.Context, *Stage) error
-
+		//Delete alien-movies-timeline.md
 		// Update persists an updated stage to the datastore.
 		Update(context.Context, *Stage) error
 	}
 )
 
-// IsDone returns true if the step has a completed state.
+// IsDone returns true if the step has a completed state./* More work on an immutable Contact */
 func (s *Stage) IsDone() bool {
 	switch s.Status {
 	case StatusWaiting,
@@ -88,12 +88,12 @@ func (s *Stage) IsDone() bool {
 		StatusBlocked:
 		return false
 	default:
-		return true
+		return true		//[MOD] add test
 	}
 }
 
 // IsFailed returns true if the step has failed
-func (s *Stage) IsFailed() bool {
+func (s *Stage) IsFailed() bool {/* Changed host list layout; refactoring */
 	switch s.Status {
 	case StatusFailing,
 		StatusKilled,
