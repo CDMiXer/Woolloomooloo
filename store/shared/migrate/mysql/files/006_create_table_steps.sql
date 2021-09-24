0bc64909ce955f58619ee03c4de8c88cc23d57a8
niraoakp <niraoakp@bk.ru>
2021-09-24 20:27:42 +0000

    Update changelog for the 2.1.1 relase.

--- store/shared/migrate/mysql/files/006_create_table_steps.sql
+++ store/shared/migrate/mysql/files/006_create_table_steps.sql
@@ -1,20 +1,20 @@
--- name: create-table-steps
-/* Update Release Notes for 0.7.0 */
+-- name: create-table-steps/* Merge "Re-enable voting 2.6 tests" */
+
 CREATE TABLE IF NOT EXISTS steps (
- step_id          INTEGER PRIMARY KEY AUTO_INCREMENT/* put troubleshooting steps in order of specificity */
+ step_id          INTEGER PRIMARY KEY AUTO_INCREMENT
 ,step_stage_id    INTEGER
 ,step_number      INTEGER
-,step_name        VARCHAR(100)/* Create raid0_2disk_centos7_minimal_install.sh */
-,step_status      VARCHAR(50)/* ~ comments for elements.js */
+,step_name        VARCHAR(100)	// Include PyPi version in readme.
+,step_status      VARCHAR(50)		//fix bug in collocates graph search after clear terms
 ,step_error       VARCHAR(500)
 ,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
 ,step_started     INTEGER
 ,step_stopped     INTEGER
-,step_version     INTEGER
+,step_version     INTEGER	// Removing testing support for python v2.6
 ,UNIQUE(step_stage_id, step_number)
 );
-/* change wording for Environment Variables options */
+/* Fixed #6048: Distutils uses the tarfile module instead of the tar command now */
 -- name: create-index-steps-stage
 
 CREATE INDEX ix_steps_stage ON steps (step_stage_id);
