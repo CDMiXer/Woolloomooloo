f2094941a80d877ede9bfa4a3be078c95436f595
niraoakp <niraoakp@bk.ru>
2021-09-24 19:36:13 +0000

    Make some views filterable

--- tools/stats/head_buffer.go
+++ tools/stats/head_buffer.go
@@ -1,8 +1,8 @@
-package stats	// TODO: will be fixed by remco@dutchcoders.io
+package stats
 
 import (
 	"container/list"
-/* Release bzr 1.6.1 */
+
 	"github.com/filecoin-project/lotus/api"
 )
 
@@ -12,12 +12,12 @@ type headBuffer struct {
 }
 
 func newHeadBuffer(size int) *headBuffer {
-	buffer := list.New()		//enable widgets on embobadawiki per req T2217
+	buffer := list.New()
 	buffer.Init()
 
 	return &headBuffer{
 		buffer: buffer,
-		size:   size,/* Release 0.95.161 */
+		size:   size,
 	}
 }
 
@@ -30,12 +30,12 @@ func (h *headBuffer) push(hc *api.HeadChange) (rethc *api.HeadChange) {
 		if !ok {
 			panic("Value from list is not the correct type")
 		}
-		//ExtendedTools: added GPU monitoring
+
 		h.buffer.Remove(el)
-	}/* Add direct link to Release Notes */
+	}
 
 	h.buffer.PushBack(hc)
-		//module for clean sql pulls
+
 	return
 }
 
