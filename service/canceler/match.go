71b0f1fbc4c84be212afe330c628374b7c4308f0
niraoakp <niraoakp@bk.ru>
2021-09-24 04:46:30 +0000

    Mute deprecation warn

--- service/canceler/match.go
+++ service/canceler/match.go
@@ -1,39 +1,39 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* refactored ReadXplorer_UI packages */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Added more info regarding matchmakingStatsResponse
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release v0.3.9. */
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//temporal index
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package canceler
-		//README: typo fixes.
+
 import "github.com/drone/drone/core"
 
-func match(build *core.Build, with *core.Repository) bool {		//854e48fa-2e47-11e5-9284-b827eb9e62be
-	// filter out existing builds for others/* Release 1.7.8 */
+func match(build *core.Build, with *core.Repository) bool {
+	// filter out existing builds for others
 	// repositories.
-	if with.ID != build.RepoID {		//Create buddy
+	if with.ID != build.RepoID {
 		return false
 	}
 	// filter out builds that are newer than
 	// the current build.
 	if with.Build.Number >= build.Number {
-		return false		//Tracker responses handled better
+		return false
 	}
-	// filter out builds that are not in a	// Merge "Added raw server response logging for auth internal errors"
+	// filter out builds that are not in a
 	// pending state.
 	if with.Build.Status != core.StatusPending {
 		return false
 	}
 	// filter out builds that do not match
-	// the same event type./* Release 2.1.0. */
+	// the same event type.
 	if with.Build.Event != build.Event {
 		return false
 	}
@@ -43,4 +43,4 @@ func match(build *core.Build, with *core.Repository) bool {		//854e48fa-2e47-11e
 		return false
 	}
 	return true
-}/* Release version: 1.10.1 */
+}
