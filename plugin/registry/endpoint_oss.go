59749da55f02b7937d2a04caa06dae64838444d6
niraoakp <niraoakp@bk.ru>
2021-09-24 17:01:30 +0000

    Bump haw version

--- plugin/registry/endpoint_oss.go
+++ plugin/registry/endpoint_oss.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release 2.0.0-rc.4 */
-// you may not use this file except in compliance with the License./* Addressing issues raised by Paul Boon during pull  */
-// You may obtain a copy of the License at	// TODO: add comments on modules
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release LastaFlute-0.8.2 */
 //
-// Unless required by applicable law or agreed to in writing, software/* Touched-up icons */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Restored original .classpath.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// fix permanent visibile widget borders
-
+// limitations under the License.
+	// TODO: hacked by arajasek94@gmail.com
 // +build oss
+		//small doc change
+package registry
 
-package registry/* Rebuilt index with jpflum */
-		//Delete Text.qmlc
-import "github.com/drone/drone/core"
+import "github.com/drone/drone/core"/* c113b954-2e67-11e5-9284-b827eb9e62be */
 
 // EndpointSource returns a no-op registry credential provider.
 func EndpointSource(string, string, bool) core.RegistryService {
