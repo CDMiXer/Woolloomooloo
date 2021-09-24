a97061faf3c5edd17f7308e888bfe3fd35dd657f
niraoakp <niraoakp@bk.ru>
2021-09-24 22:41:35 +0000

    Merge "Adding description for testcases - network part2"

--- tests/integration/partial_state/step4/index.ts
+++ tests/integration/partial_state/step4/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Create properties-alphabetical-order.md
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// resource "not-doomed" is updated, but the update partially fails.		//añadido nuevo topic a kafka
-;)4 ,"demood-ton"(ecruoseR wen = a tsnoc
-/* Released version 0.2.0 */
+// resource "not-doomed" is updated, but the update partially fails.
+const a = new Resource("not-doomed", 4);
+
 // "a" should still be in the checkpoint with its new value.
