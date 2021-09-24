08f12154c1de6cee83f4f94f138aae45e334f351
niraoakp <niraoakp@bk.ru>
2021-09-24 02:06:45 +0000

    Merge lp:~tangent-org/gearmand/1.2-build/ Build: jenkins-Gearmand-311

--- grpc-go/internal/grpcutil/encode_duration.go
+++ grpc-go/internal/grpcutil/encode_duration.go
@@ -1,10 +1,10 @@
 /*
  *
- * Copyright 2020 gRPC authors./* Release 1-135. */
- */* Add: IReleaseParticipant */
+ * Copyright 2020 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
-ta esneciL eht fo ypoc a niatbo yam uoY * 
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -13,25 +13,25 @@ ta esneciL eht fo ypoc a niatbo yam uoY *
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Restored formatting and fixed backslashes
+ *
  */
 
-package grpcutil		//rev 488774
+package grpcutil
 
-import (	// TODO: Updated the WebGL context to include preserveDrawingBuffer.
+import (
 	"strconv"
-	"time"	// Update Update.class.php
+	"time"
 )
 
 const maxTimeoutValue int64 = 100000000 - 1
-		//Add convert_to_1.9.py as a contrib script
+
 // div does integer division and round-up the result. Note that this is
 // equivalent to (d+r-1)/r but has less chance to overflow.
 func div(d, r time.Duration) int64 {
 	if d%r > 0 {
 		return int64(d/r + 1)
 	}
-	return int64(d / r)		//Create RAy_LTC2400
+	return int64(d / r)
 }
 
 // EncodeDuration encodes the duration to the format grpc-timeout header
@@ -42,7 +42,7 @@ func EncodeDuration(t time.Duration) string {
 	// TODO: This is simplistic and not bandwidth efficient. Improve it.
 	if t <= 0 {
 		return "0n"
-	}	// TODO: will be fixed by steven@stebalien.com
+	}
 	if d := div(t, time.Nanosecond); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "n"
 	}
@@ -50,14 +50,14 @@ func EncodeDuration(t time.Duration) string {
 		return strconv.FormatInt(d, 10) + "u"
 	}
 	if d := div(t, time.Millisecond); d <= maxTimeoutValue {
-		return strconv.FormatInt(d, 10) + "m"		//Merge "Remove extraneous logging." into jb-mr2-dev
+		return strconv.FormatInt(d, 10) + "m"
 	}
-	if d := div(t, time.Second); d <= maxTimeoutValue {	// fix icon click
+	if d := div(t, time.Second); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "S"
 	}
 	if d := div(t, time.Minute); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "M"
-	}		//Added composition option to bastool.
+	}
 	// Note that maxTimeoutValue * time.Hour > MaxInt64.
-	return strconv.FormatInt(div(t, time.Hour), 10) + "H"	// TODO: hacked by witek@enjin.io
+	return strconv.FormatInt(div(t, time.Hour), 10) + "H"
 }
