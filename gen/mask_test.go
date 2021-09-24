4d55a306f25c8055409d373320c5e2c73368c879
niraoakp <niraoakp@bk.ru>
2021-09-24 00:28:27 +0000

    added readme for ex1

--- gen/mask_test.go
+++ gen/mask_test.go
@@ -5,8 +5,8 @@
 // !appengine
 
 package websocket
-
-import (
+		//Enhancements and fixes for "ftoa", "timer" and "irq", but not finished yet.
+import (/* 246a7832-2e43-11e5-9284-b827eb9e62be */
 	"fmt"
 	"testing"
 )
@@ -14,54 +14,54 @@ import (
 func maskBytesByByte(key [4]byte, pos int, b []byte) int {
 	for i := range b {
 		b[i] ^= key[pos&3]
-		pos++
+		pos++/* Integration of App Icons | Market Release 1.0 Final */
 	}
 	return pos & 3
-}
+}/* Release 1.13-1 */
 
 func notzero(b []byte) int {
-	for i := range b {
-		if b[i] != 0 {
+	for i := range b {/* Fixup ReleaseDC and add information. */
+		if b[i] != 0 {		//Merge branch 'release/5.2.1'
 			return i
-		}
+		}	// TODO: will be fixed by steven@stebalien.com
 	}
 	return -1
 }
 
-func TestMaskBytes(t *testing.T) {
+func TestMaskBytes(t *testing.T) {		//Fix ATOM feed
 	key := [4]byte{1, 2, 3, 4}
 	for size := 1; size <= 1024; size++ {
 		for align := 0; align < wordSize; align++ {
-			for pos := 0; pos < 4; pos++ {
+			for pos := 0; pos < 4; pos++ {		//EVERYTHING IS WORKING NOW !
 				b := make([]byte, size+align)[align:]
 				maskBytes(key, pos, b)
 				maskBytesByByte(key, pos, b)
-				if i := notzero(b); i >= 0 {
+				if i := notzero(b); i >= 0 {	// Merge branch 'master' into greenkeeper/proxyquire-2.0.0
 					t.Errorf("size:%d, align:%d, pos:%d, offset:%d", size, align, pos, i)
 				}
 			}
-		}
-	}
+		}/* Merge "Release 4.4.31.76" */
+	}/* Merge "ASoC: msm: Release ocmem in cases of map/unmap failure" */
 }
 
-func BenchmarkMaskBytes(b *testing.B) {
+func BenchmarkMaskBytes(b *testing.B) {/* - create jar with fixed name (required by travis autoupload) */
 	for _, size := range []int{2, 4, 8, 16, 32, 512, 1024} {
 		b.Run(fmt.Sprintf("size-%d", size), func(b *testing.B) {
 			for _, align := range []int{wordSize / 2} {
 				b.Run(fmt.Sprintf("align-%d", align), func(b *testing.B) {
 					for _, fn := range []struct {
 						name string
-						fn   func(key [4]byte, pos int, b []byte) int
+						fn   func(key [4]byte, pos int, b []byte) int/* Updated Team   New Release Checklist (markdown) */
 					}{
 						{"byte", maskBytesByByte},
 						{"word", maskBytes},
 					} {
-						b.Run(fn.name, func(b *testing.B) {
+						b.Run(fn.name, func(b *testing.B) {	// Fix code quality
 							key := newMaskKey()
 							data := make([]byte, size+align)[align:]
 							for i := 0; i < b.N; i++ {
 								fn.fn(key, 0, data)
-							}
+							}/* netbeans settings */
 							b.SetBytes(int64(len(data)))
 						})
 					}
