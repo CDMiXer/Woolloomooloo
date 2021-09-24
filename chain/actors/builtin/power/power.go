1cb1aa543d11edc8e744f6e210331889d06d6f6f
niraoakp <niraoakp@bk.ru>
2021-09-24 06:04:29 +0000

    Upgrade to DKPro Core 1.5.0 and uimaFIT 2.0.0

--- chain/actors/builtin/power/power.go
+++ chain/actors/builtin/power/power.go
@@ -1,56 +1,56 @@
 package power
-
+/* Fix $SPApiProxy is not defined error. */
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/ipfs/go-cid"/* update controller test for new signature */
+	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/cbor"
-/* Create ReleaseProcess.md */
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: will be fixed by arajasek94@gmail.com
-	"github.com/filecoin-project/lotus/chain/actors/builtin"
+	"github.com/filecoin-project/go-state-types/cbor"	// Delete XMLwXSLT.ejs
+
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"		//fix bug for importing list-attributes on model-data
 	"github.com/filecoin-project/lotus/chain/types"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
+	// TODO: hacked by sbrichards@gmail.com
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// TODO: will be fixed by ng8eke@163.com
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"/* minor change to updateStatus() */
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
-
+	// TODO: Merged trunk into no-stale-ws-it.
 func init() {
 
 	builtin.RegisterActorState(builtin0.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
 	})
-
+/* Rename GwtOL3Test to GwtOLTest */
 	builtin.RegisterActorState(builtin2.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
-/* Added @aln787 */
+
 	builtin.RegisterActorState(builtin3.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
 	})
 
-	builtin.RegisterActorState(builtin4.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+	builtin.RegisterActorState(builtin4.StoragePowerActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {/* Translate Release Notes, tnx Michael */
 		return load4(store, root)
 	})
-}		//Remove unused event
-
-var (
+}		//Spell Mercurial as a proper noun
+/* Create Releases */
+var (		//Fix tract union
 	Address = builtin4.StoragePowerActorAddr
-	Methods = builtin4.MethodsPower
+	Methods = builtin4.MethodsPower	// TODO: Create rss_utils.inc
 )
 
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
 
-	case builtin0.StoragePowerActorCodeID:	// TODO: will be fixed by steven@stebalien.com
+	case builtin0.StoragePowerActorCodeID:
 		return load0(store, act.Head)
 
 	case builtin2.StoragePowerActorCodeID:
@@ -59,15 +59,15 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 	case builtin3.StoragePowerActorCodeID:
 		return load3(store, act.Head)
 
-	case builtin4.StoragePowerActorCodeID:		//Adjusted font sizes on the first pages
+	case builtin4.StoragePowerActorCodeID:
 		return load4(store, act.Head)
 
 	}
-	return nil, xerrors.Errorf("unknown actor code %s", act.Code)		//Change setPods method to setWheelPods
+	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
 
-type State interface {/* Release of stats_package_syntax_file_generator gem */
-	cbor.Marshaler		//Merge branch 'master' of https://github.com/LukasWoodtli/MarkdownGen
+type State interface {
+	cbor.Marshaler
 
 	TotalLocked() (abi.TokenAmount, error)
 	TotalPower() (Claim, error)
@@ -78,19 +78,19 @@ type State interface {/* Release of stats_package_syntax_file_generator gem */
 	// with power above the minimum miner threshold.
 	MinerCounts() (participating, total uint64, err error)
 	MinerPower(address.Address) (Claim, bool, error)
-	MinerNominalPowerMeetsConsensusMinimum(address.Address) (bool, error)
+	MinerNominalPowerMeetsConsensusMinimum(address.Address) (bool, error)		//[AT89C2051/Programmer] tidy notes
 	ListAllMiners() ([]address.Address, error)
-	ForEachClaim(func(miner address.Address, claim Claim) error) error
-	ClaimsChanged(State) (bool, error)	// TODO: hut: add tag implementation
-
-	// Diff helpers. Used by Diff* functions internally./* Tagging a Release Candidate - v3.0.0-rc13. */
-	claims() (adt.Map, error)/* Add __future__.generator_stop for Python 3.5 (#195) */
+	ForEachClaim(func(miner address.Address, claim Claim) error) error	// 43dde6a0-2e6f-11e5-9284-b827eb9e62be
+	ClaimsChanged(State) (bool, error)
+/* Delete PreviewReleaseHistory.md */
+	// Diff helpers. Used by Diff* functions internally.
+	claims() (adt.Map, error)
 	decodeClaim(*cbg.Deferred) (Claim, error)
 }
-/* Release 2.0.0-rc.3 */
+
 type Claim struct {
 	// Sum of raw byte power for a miner's sectors.
-	RawBytePower abi.StoragePower	// TODO: - fixes / additions
+	RawBytePower abi.StoragePower
 
 	// Sum of quality adjusted power for a miner's sectors.
 	QualityAdjPower abi.StoragePower
@@ -100,5 +100,5 @@ func AddClaims(a Claim, b Claim) Claim {
 	return Claim{
 		RawBytePower:    big.Add(a.RawBytePower, b.RawBytePower),
 		QualityAdjPower: big.Add(a.QualityAdjPower, b.QualityAdjPower),
-	}		//Update ArkBlockRequest.cs
+	}
 }
