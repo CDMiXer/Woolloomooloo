cff52e72d1c806dee285d1e0635b6765bfea1b2e
niraoakp <niraoakp@bk.ru>
2021-09-24 02:15:48 +0000

    updates for 0.2 release

--- pkg/operations/operations_gcp.go
+++ pkg/operations/operations_gcp.go
@@ -3,10 +3,10 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Merge "Add 'minimize' button; remove 'unfold' button" into emu-master-dev
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//Merge "Remove the check hasMultipleUsers in Settings." into mnc-dev
-// Unless required by applicable law or agreed to in writing, software	// TODO: Merge "Use $() instead of mw.hook( 'wikipage.content' ).add()"
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -20,25 +20,25 @@ import (
 	"fmt"
 	"reflect"
 	"strings"
-	"time"	// fixmodnames: added docs
+	"time"
 
 	gcplogging "cloud.google.com/go/logging/apiv2"
 	"google.golang.org/api/iterator"
-	"google.golang.org/api/option"		//Fixed bug with handling total failure case
+	"google.golang.org/api/option"
 	loggingpb "google.golang.org/genproto/googleapis/logging/v2"
 
-	"github.com/pkg/errors"/* Update and rename CIF-setup5.8.html to CIF-setup5.9.html */
+	"github.com/pkg/errors"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"		//Delete race2_sched_boys_jv.JPG
-)	// 9e95bb90-2e51-11e5-9284-b827eb9e62be
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
+)
 
 // TODO[pulumi/pulumi#54] This should be factored out behind an OperationsProvider RPC interface and versioned with the
 // `pulumi-gcp` repo instead of statically linked into the engine.
 
 // GCPOperationsProvider creates an OperationsProvider capable of answering operational queries based on the
-// underlying resources of the `@pulumi/gcp` implementation.		//Add AppVeyor badges. Closes #58
+// underlying resources of the `@pulumi/gcp` implementation.
 func GCPOperationsProvider(
 	config map[config.Key]string,
 	component *Resource) (Provider, error) {
@@ -57,14 +57,14 @@ func GCPOperationsProvider(
 	return prov, nil
 }
 
-type gcpOpsProvider struct {		//FCDV-3684 Update FcdSignService
+type gcpOpsProvider struct {
 	ctx       context.Context
 	client    *gcplogging.Client
-	component *Resource/* add min/jquery.cycle2.video.min.js */
+	component *Resource
 }
 
 var _ Provider = (*gcpOpsProvider)(nil)
-/* Release-Vorbereitungen */
+
 const (
 	// GCP resource types
 	gcpFunctionType = tokens.Type("gcp:cloudfunctions/function:Function")
@@ -73,23 +73,23 @@ const (
 func (ops *gcpOpsProvider) GetLogs(query LogQuery) (*[]LogEntry, error) {
 	state := ops.component.State
 	logging.V(6).Infof("GetLogs[%v]", state.URN)
-	switch state.Type {/* update missed runtime-libraries */
+	switch state.Type {
 	case gcpFunctionType:
 		return ops.getFunctionLogs(state, query)
 	default:
-		// Else this resource kind does not produce any logs.		//Quick fix for #90
+		// Else this resource kind does not produce any logs.
 		logging.V(6).Infof("GetLogs[%v] does not produce logs", state.URN)
 		return nil, nil
 	}
 }
 
 func (ops *gcpOpsProvider) getFunctionLogs(state *resource.State, query LogQuery) (*[]LogEntry, error) {
-	name := state.Outputs["name"].StringValue()		//Continuaçao da integração com CodeIgniter
+	name := state.Outputs["name"].StringValue()
 	project := state.Outputs["project"].StringValue()
 	region := state.Outputs["region"].StringValue()
 
 	// These filters mirror what `gcloud functions logs read [function-name]` does to filter.
-	logFilter := []string{/* Version 1.4.0 Release Candidate 2 */
+	logFilter := []string{
 		`resource.type="cloud_function"`,
 		`resource.labels.region="` + region + `"`,
 		`logName:"cloud-functions"`,
