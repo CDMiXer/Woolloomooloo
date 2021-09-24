621a824562b561bb477c168217c4395abdde886c
niraoakp <niraoakp@bk.ru>
2021-09-24 10:07:52 +0000

    odstranjen css za tabele

--- handler/api/user/sync_test.go
+++ handler/api/user/sync_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by aeongrp@outlook.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package user	// TODO: Fix TravisCI Build URL
+package user
