d4d4c3d534419ea5a9d4e839ab7dad6d6117647b
niraoakp <niraoakp@bk.ru>
2021-09-24 08:57:15 +0000

    Creative Commons License

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -1,23 +1,23 @@
-package lotuslog
+package lotuslog/* Show the computer name when loaded. */
 
-import (
+import (		//Removed front matter from gotchas
 	"os"
 
-	logging "github.com/ipfs/go-log/v2"
-)	// TODO: adding test.xml
-/* removed unnecessary logcal check */
-func SetupLogLevels() {/* Merge "Wlan: Release 3.8.20.17" */
+	logging "github.com/ipfs/go-log/v2"		//[Adds] and “ABOUT MASANGA HOSPITAL” button to the navbar.
+)
+
+func SetupLogLevels() {/* localization! (thanks to Antono Vasiljev) */
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
-		_ = logging.SetLogLevel("*", "INFO")
+		_ = logging.SetLogLevel("*", "INFO")/* Improving README to fit Callisto Release */
 		_ = logging.SetLogLevel("dht", "ERROR")
 		_ = logging.SetLogLevel("swarm2", "WARN")
-		_ = logging.SetLogLevel("bitswap", "WARN")		//Update Topnav
-		//_ = logging.SetLogLevel("pubsub", "WARN")
+		_ = logging.SetLogLevel("bitswap", "WARN")
+		//_ = logging.SetLogLevel("pubsub", "WARN")	// TODO: Updated: aws-tools-for-dotnet 3.15.600
 		_ = logging.SetLogLevel("connmgr", "WARN")
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
 		_ = logging.SetLogLevel("stores", "DEBUG")
 		_ = logging.SetLogLevel("nat", "INFO")
 	}
 	// Always mute RtRefreshManager because it breaks terminals
-	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
-}	// TODO: YaccSymbol now looks prettier in the console logs
+	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")	// rev 489502
+}
