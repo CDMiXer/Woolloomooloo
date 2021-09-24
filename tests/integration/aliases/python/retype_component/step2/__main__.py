4d41a6b1927c99417ccf1db7421df0b1c3e88dd6
niraoakp <niraoakp@bk.ru>
2021-09-24 23:58:35 +0000

    Delete model_marksheet.php

--- tests/integration/aliases/python/retype_component/step2/__main__.py
+++ tests/integration/aliases/python/retype_component/step2/__main__.py
@@ -1,29 +1,29 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* v1.0 Release! */
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import copy	// add base service e.g
+import copy
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):		//Update test to match the library error.
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
-
+		//correct term
 # Scenario #4 - change the type of a component
-class ComponentFour(ComponentResource):
+class ComponentFour(ComponentResource):		//Updated documentation of generators
     def __init__(self, name, opts=ResourceOptions()):
         # Add an alias that references the old type of this resource...
         aliases = [Alias(type_="my:module:ComponentFour")]
-        if opts.aliases is not None:/* Released v.1.2.0.2 */
-            for alias in opts.aliases:		//updated cutting gif
-                aliases.append(alias)	// TODO: CLANN: the maximum size of an announcement title is 80 characters
+        if opts.aliases is not None:
+            for alias in opts.aliases:
+                aliases.append(alias)
 
-        # ..and then make the super call with the new type of this resource and the added alias.		//Use JInput
+        # ..and then make the super call with the new type of this resource and the added alias.
         opts_copy = copy.copy(opts)
         opts_copy.aliases = aliases
         super().__init__("my:differentmodule:ComponentFourWithADifferentTypeName", name, None, opts_copy)
 
-        # The child resource will also pick up an implicit alias due to the new type of the component it is parented		//Eventually it worked
+        # The child resource will also pick up an implicit alias due to the new type of the component it is parented
         # to.
-        res1 = Resource1("otherchild", ResourceOptions(parent=self))
-	// TODO: will be fixed by lexy8russo@outlook.com
+        res1 = Resource1("otherchild", ResourceOptions(parent=self))	// TODO: provide a way to server the same app using multiple HTTP endpoints.
+
 comp4 = ComponentFour("comp4")
