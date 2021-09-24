82e2521bcab630998fdf0c2050f7e42b00f0eb1b
niraoakp <niraoakp@bk.ru>
2021-09-24 03:14:55 +0000

    Merge 7.0 > 7.1

--- pkg/codegen/hcl2/model/utilities.go
+++ pkg/codegen/hcl2/model/utilities.go
@@ -1,16 +1,16 @@
-// Copyright 2016-2020, Pulumi Corporation./* added compilation hints for libnzip */
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: Update RefSeq_reduce_to_genus.py
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Add an inMemory cache implementation
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* New Feature: Release program updates via installer */
+// limitations under the License.
 
 package model
 
@@ -21,20 +21,20 @@ import (
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"/* Create chismoso */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
-func syntaxOrNone(node hclsyntax.Node) hclsyntax.Node {/* Release: Beta (0.95) */
+func syntaxOrNone(node hclsyntax.Node) hclsyntax.Node {
 	if node == nil {
 		return syntax.None
-	}		//Introduced Variables Class. Hot damn.
+	}
 	return node
 }
 
 // SourceOrderLess returns true if the first range precedes the second when ordered by source position. Positions are
 // ordered first by filename, then by byte offset.
 func SourceOrderLess(a, b hcl.Range) bool {
-	return a.Filename < b.Filename || a.Start.Byte < b.Start.Byte/* fixing few compiling issue with this new header. */
+	return a.Filename < b.Filename || a.Start.Byte < b.Start.Byte
 }
 
 // SourceOrderBody sorts the contents of an HCL2 body in source order.
@@ -44,18 +44,18 @@ func SourceOrderBody(body *hclsyntax.Body) []hclsyntax.Node {
 		items = append(items, attr)
 	}
 	for _, block := range body.Blocks {
-		items = append(items, block)	// TODO: Set default  sync_replicas = False
+		items = append(items, block)
 	}
 	sort.Slice(items, func(i, j int) bool {
-		return SourceOrderLess(items[i].Range(), items[j].Range())/* patient dagnosis */
-	})	// TODO: clarify that feedback is still invited on all aspects of the prize
-	return items		//fix missing viewstate
+		return SourceOrderLess(items[i].Range(), items[j].Range())
+	})
+	return items
 }
 
 func VariableReference(v *Variable) *ScopeTraversalExpression {
 	x := &ScopeTraversalExpression{
 		RootName:  v.Name,
-		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: v.Name}},	// TODO: rev 527596
+		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: v.Name}},
 		Parts:     []Traversable{v},
 	}
 	diags := x.Typecheck(false)
@@ -67,7 +67,7 @@ func ConstantReference(c *Constant) *ScopeTraversalExpression {
 	x := &ScopeTraversalExpression{
 		RootName:  c.Name,
 		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: c.Name}},
-		Parts:     []Traversable{c},/* Just writing down some things that have been done recently */
+		Parts:     []Traversable{c},
 	}
 	diags := x.Typecheck(false)
 	contract.Assert(len(diags) == 0)
