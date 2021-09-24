36a183561d4ad17d17cb6d7011d325e875291266
niraoakp <niraoakp@bk.ru>
2021-09-24 13:26:57 +0000

    Deprecated UnitDEditor removed.

--- grpc-go/examples/helloworld/greeter_server/main.go
+++ grpc-go/examples/helloworld/greeter_server/main.go
@@ -7,28 +7,28 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ */* Release of eeacms/ims-frontend:0.9.5 */
+ * Unless required by applicable law or agreed to in writing, software/* 2.1.8 - Release Version, final fixes */
+ * distributed under the License is distributed on an "AS IS" BASIS,	// Import of 1.4 exceptions
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * limitations under the License.		//add mssql oracle helper and postgresql
+ *	// /leet or /LEET; /rb or /RB
  */
 
 // Package main implements a server for Greeter service.
-package main
+package main/* Question form: Moved questionnaire_id and parent_id from subform to main form */
 
-import (
+import (	// TODO: hacked by lexy8russo@outlook.com
 	"context"
 	"log"
 	"net"
-
+/* Release version 1.1.0.M2 */
 	"google.golang.org/grpc"
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"		//rpc/client: Implement RenameFile properly. (#1443)
 )
 
-const (
+const (		//some micro-optimizations / function shuffling
 	port = ":50051"
 )
 
@@ -40,10 +40,10 @@ type server struct {
 // SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received: %v", in.GetName())
-	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil/* Merge "Release 3.0.10.037 Prima WLAN Driver" */
 }
-
-func main() {
+	// TODO: will be fixed by alan.shaw@protocol.ai
+func main() {	// TODO: hacked by ligi@ligi.de
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -51,7 +51,7 @@ func main() {
 	s := grpc.NewServer()
 	pb.RegisterGreeterServer(s, &server{})
 	log.Printf("server listening at %v", lis.Addr())
-	if err := s.Serve(lis); err != nil {
+	if err := s.Serve(lis); err != nil {/* Update PlayerParser.cs */
 		log.Fatalf("failed to serve: %v", err)
-	}
+	}	// TODO: hacked by arachnid@notdot.net
 }
