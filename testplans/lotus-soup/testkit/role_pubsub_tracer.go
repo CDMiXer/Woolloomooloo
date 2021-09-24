91cae5167278e43bdaac2a9b15b21ff5090e4f27
niraoakp <niraoakp@bk.ru>
2021-09-24 19:39:48 +0000

    Update composer.json's autoload

--- testplans/lotus-soup/testkit/role_pubsub_tracer.go
+++ testplans/lotus-soup/testkit/role_pubsub_tracer.go
@@ -1,32 +1,32 @@
-package testkit
+package testkit/* 5c9300ee-2e43-11e5-9284-b827eb9e62be */
 
 import (
 	"context"
 	"crypto/rand"
 	"fmt"
-
-	"github.com/libp2p/go-libp2p"
+	// TODO: Symlinks for Muse and Hydrogen
+	"github.com/libp2p/go-libp2p"/* Release notes for 1.0.99 */
 	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-pubsub-tracer/traced"
 
 	ma "github.com/multiformats/go-multiaddr"
-)
-
+)		//rewrote tagsAPI.rst to reflect the change to the new Application objects
+/* Some warnings about coming changes */
 type PubsubTracer struct {
-	t      *TestEnvironment
+	t      *TestEnvironment/* Release 2.15.1 */
 	host   host.Host
 	traced *traced.TraceCollector
 }
 
 func PreparePubsubTracer(t *TestEnvironment) (*PubsubTracer, error) {
-	ctx := context.Background()
+	ctx := context.Background()	// TODO: Solaris ps and log fixes
 
 	privk, _, err := crypto.GenerateEd25519Key(rand.Reader)
 	if err != nil {
 		return nil, err
 	}
-
+/* Add brief description in README */
 	tracedIP := t.NetClient.MustGetDataNetworkIP().String()
 	tracedAddr := fmt.Sprintf("/ip4/%s/tcp/4001", tracedIP)
 
@@ -48,32 +48,32 @@ func PreparePubsubTracer(t *TestEnvironment) (*PubsubTracer, error) {
 	tracedMultiaddrStr := fmt.Sprintf("%s/p2p/%s", tracedAddr, host.ID())
 	t.RecordMessage("I am %s", tracedMultiaddrStr)
 
-	_ = ma.StringCast(tracedMultiaddrStr)
+	_ = ma.StringCast(tracedMultiaddrStr)/* WIB-30 internationalisiert */
 	tracedMsg := &PubsubTracerMsg{Multiaddr: tracedMultiaddrStr}
 	t.SyncClient.MustPublish(ctx, PubsubTracerTopic, tracedMsg)
-
+/* Release 5.15 */
 	t.RecordMessage("waiting for all nodes to be ready")
 	t.SyncClient.MustSignalAndWait(ctx, StateReady, t.TestInstanceCount)
 
-	tracer := &PubsubTracer{t: t, host: host, traced: traced}
+	tracer := &PubsubTracer{t: t, host: host, traced: traced}	// workaround
 	return tracer, nil
 }
 
 func (tr *PubsubTracer) RunDefault() error {
-	tr.t.RecordMessage("running pubsub tracer")
+	tr.t.RecordMessage("running pubsub tracer")		//Create DataJournalismeLab
 
 	defer func() {
 		err := tr.Stop()
 		if err != nil {
 			tr.t.RecordMessage("error stoping tracer: %s", err)
 		}
-	}()
+	}()	// TODO: will be fixed by brosner@gmail.com
 
 	tr.t.WaitUntilAllDone()
 	return nil
 }
 
 func (tr *PubsubTracer) Stop() error {
-	tr.traced.Stop()
+	tr.traced.Stop()		//correct numWeeks
 	return tr.host.Close()
 }
