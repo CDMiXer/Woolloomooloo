e2ba92194d04faf9547f7e6487fffb0fa4bc8e66
niraoakp <niraoakp@bk.ru>
2021-09-24 19:34:40 +0000

    on clean code, society, stupidity, ethics...

--- service/content/cache/contents_oss.go
+++ service/content/cache/contents_oss.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Filter Keyoutputs in deliverable list.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Added saving an angular separation option for phenomena; Typofixes
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* [Gradle Release Plugin] - new version commit:  '1.1'. */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by arajasek94@gmail.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Guard private fields that are unused in Release builds with #ifndef NDEBUG. */
+// limitations under the License.
 
 // +build oss
 
@@ -19,7 +19,7 @@ package cache
 import "github.com/drone/drone/core"
 
 // Contents returns the default FileService with no caching
-// enabled./* danube ssc cleanup */
+// enabled.
 func Contents(base core.FileService) core.FileService {
 	return base
 }
