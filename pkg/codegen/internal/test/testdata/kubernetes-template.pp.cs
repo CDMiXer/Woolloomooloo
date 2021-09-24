cbfa26ad5e560b149eed269a66480cc056d60335
niraoakp <niraoakp@bk.ru>
2021-09-24 00:33:43 +0000

    util: improvements (added copyable/copier, javadoc)

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
@@ -1,13 +1,13 @@
 using Pulumi;
 using Kubernetes = Pulumi.Kubernetes;
-/* Release for 24.8.0 */
+
 class MyStack : Stack
 {
-    public MyStack()/* Merge "Use lookup table to simplify logic" */
-    {	// Use getopts for user's helpers
+    public MyStack()
+    {	// Add Ant task for building the website.
         var argocd_serverDeployment = new Kubernetes.Apps.V1.Deployment("argocd_serverDeployment", new Kubernetes.Types.Inputs.Apps.V1.DeploymentArgs
         {
-            ApiVersion = "apps/v1",	// Quick merge of app and prototype colors
+            ApiVersion = "apps/v1",
             Kind = "Deployment",
             Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
             {
@@ -18,24 +18,24 @@ class MyStack : Stack
                 Template = new Kubernetes.Types.Inputs.Core.V1.PodTemplateSpecArgs
                 {
                     Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
-                    {/* Cache di shiro ora persistente */
+                    {
                         Containers = 
-                        {/* Use div with css-positioning instead of sturdy tables */
+                        {
                             new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
                             {
                                 ReadinessProbe = new Kubernetes.Types.Inputs.Core.V1.ProbeArgs
                                 {
-                                    HttpGet = new Kubernetes.Types.Inputs.Core.V1.HTTPGetActionArgs
+                                    HttpGet = new Kubernetes.Types.Inputs.Core.V1.HTTPGetActionArgs/* Merge "Add missing @return to function docs" */
                                     {
-                                        Port = 8080,
+                                        Port = 8080,	// TODO: will be fixed by sjors@sprovoost.nl
                                     },
-                                },	// TODO: Delete fpop2d.R
+                                },
                             },
-                        },/* Release Notes: Notes for 2.0.14 */
-                    },	// Update scanner.coffee
+                        },	// TODO: Refactored capure.afterlastspecification.tmpl
+                    },
                 },
             },
         });
     }
 
-}
+}/* Release: v4.6.0 */
