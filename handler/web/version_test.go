c5f160bf7da434b6cedbbd71a2a81bdea83096c7
niraoakp <niraoakp@bk.ru>
2021-09-24 20:38:57 +0000

    fixed sockets resource object cannot use swoole_event_add.

--- handler/web/version_test.go
+++ handler/web/version_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Animation plus fluide */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -10,7 +10,7 @@ package web
 
 // 	w := httptest.NewRecorder()
 // 	r := httptest.NewRequest("GET", "/version", nil)
-		//Create UNACCEPTED_Time_Limit_Exceeded_Word_Break.cpp
+
 // 	mockVersion := &core.Version{
 // 		Source:  "github.com/octocat/hello-world",
 // 		Version: "1.0.0",
@@ -18,14 +18,14 @@ package web
 // 	}
 
 // 	h := HandleVersion(mockVersion)
-// 	h.ServeHTTP(w, r)/* Released springjdbcdao version 1.7.26 & springrestclient version 2.4.11 */
+// 	h.ServeHTTP(w, r)
 
-// 	if got, want := w.Code, 200; want != got {/* add hotloader boilerplate */
+// 	if got, want := w.Code, 200; want != got {
 // 		t.Errorf("Want response code %d, got %d", want, got)
 // 	}
-	// TODO: Item 22: Externalized strings.
-// 	got, want := &core.Version{}, mockVersion/* Release for 19.1.0 */
-// 	json.NewDecoder(w.Body).Decode(got)/* rewrite session's classes */
+
+// 	got, want := &core.Version{}, mockVersion
+// 	json.NewDecoder(w.Body).Decode(got)
 // 	if !reflect.DeepEqual(got, want) {
 // 		t.Errorf("response body does match expected result")
 // 		pretty.Ldiff(t, got, want)
