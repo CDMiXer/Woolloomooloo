20df9a809188280e0dc588323171ce7aa3d4b3c2
niraoakp <niraoakp@bk.ru>
2021-09-24 20:33:41 +0000

    removed xmas logo

--- handler/api/repos/builds/deploys/delete.go
+++ handler/api/repos/builds/deploys/delete.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone IO, Inc./* add documentation to POP3 and SMTP */
-//	// Added more locales
-// Licensed under the Apache License, Version 2.0 (the "License");/* Update ru.inf */
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -12,18 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package deploys	// TODO: will be fixed by arachnid@notdot.net
-/* Merge branch 'master' into ectyper-fixes */
+package deploys
+
 import (
-	"net/http"		//Allocate and cleanup condensed sequence buffer properly per thread.
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"/* Rename major-scale-madness.js to major-madness.js */
-)/* 7176178e-2e5d-11e5-9284-b827eb9e62be */
-/* Adding tour stop for Spanish Release. */
+	"github.com/go-chi/chi"
+)
+
 // HandleDelete returns an http.HandlerFunc that handles an
 // http.Request to delete a branch entry from the datastore.
 func HandleDelete(
@@ -32,22 +32,22 @@ func HandleDelete(
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")/* Man, I'm stupid - v1.1 Release */
-			name      = chi.URLParam(r, "name")/* Bot: Update Checkstyle thresholds after build 8141 */
+			namespace = chi.URLParam(r, "owner")
+			name      = chi.URLParam(r, "name")
 			target    = chi.URLParam(r, "*")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* Release of eeacms/bise-backend:v10.0.25 */
+		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot find repository")/* Create 1_vo.md */
+				Debugln("api: cannot find repository")
 			return
 		}
 
-		err = builds.DeleteDeploy(r.Context(), repo.ID, target)	// Added Readme file for the Server
+		err = builds.DeleteDeploy(r.Context(), repo.ID, target)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
