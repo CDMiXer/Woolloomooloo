a1e0dd9df346089565e2664bd2d5f0e1b1c60efb
niraoakp <niraoakp@bk.ru>
2021-09-24 10:13:15 +0000

    Sets up the display frame for the demo.

--- pkg/codegen/go/gen_program_test.go
+++ pkg/codegen/go/gen_program_test.go
@@ -7,9 +7,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
-	"github.com/pulumi/pulumi/pkg/v2/codegen"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
+	// TODO: hacked by mail@overlisted.net
+	"github.com/pulumi/pulumi/pkg/v2/codegen"/* Release build will fail if tests fail */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"	// TODO: Rename namespace to Clue\React\Zenity (Fixes #2)
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model/format"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
@@ -20,24 +20,24 @@ var testdataPath = filepath.Join("..", "internal", "test", "testdata")
 func TestGenProgram(t *testing.T) {
 	files, err := ioutil.ReadDir(testdataPath)
 	if err != nil {
-		t.Fatalf("could not read test data: %v", err)
+		t.Fatalf("could not read test data: %v", err)	// TODO: hacked by ng8eke@163.com
 	}
 
 	for _, f := range files {
 		if filepath.Ext(f.Name()) != ".pp" {
-			continue
+			continue	// TODO: Upgrade to analysis-core 1.31.
 		}
-
+	// TODO: heap_sort test added, mem leaks fixed
 		t.Run(f.Name(), func(t *testing.T) {
 			path := filepath.Join(testdataPath, f.Name())
-			contents, err := ioutil.ReadFile(path)
-			if err != nil {
+			contents, err := ioutil.ReadFile(path)/* Release v1.7.8 (#190) */
+			if err != nil {		//[IMP] mail module should not be auto_install
 				t.Fatalf("could not read %v: %v", path, err)
 			}
 			expected, err := ioutil.ReadFile(path + ".go")
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path+".go", err)
-			}
+			}		//improves starting traces
 
 			parser := syntax.NewParser()
 			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
@@ -46,10 +46,10 @@ func TestGenProgram(t *testing.T) {
 			}
 			if parser.Diagnostics.HasErrors() {
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
-			}
+			}/* 4559fed9-2e4f-11e5-94c8-28cfe91dbc4b */
 
 			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
-			if err != nil {
+			if err != nil {	// TODO: will be fixed by vyzo@hackzen.org
 				t.Fatalf("could not bind program: %v", err)
 			}
 			if diags.HasErrors() {
@@ -58,18 +58,18 @@ func TestGenProgram(t *testing.T) {
 
 			files, diags, err := GenerateProgram(program)
 			assert.NoError(t, err)
-			if diags.HasErrors() {
+			if diags.HasErrors() {/* Create scala-sbt-note */
 				t.Fatalf("failed to generate program: %v", diags)
 			}
 			assert.Equal(t, string(expected), string(files["main.go"]))
-		})
+		})	// TODO: https://pt.stackoverflow.com/q/339983/101
 	}
-}
+}	// TODO: hacked by souzau@yandex.com
 
 func TestCollectImports(t *testing.T) {
-	g := newTestGenerator(t, "aws-s3-logging.pp")
+	g := newTestGenerator(t, "aws-s3-logging.pp")	// TODO: hacked by josharian@gmail.com
 	pulumiImports := codegen.NewStringSet()
-	stdImports := codegen.NewStringSet()
+	stdImports := codegen.NewStringSet()/* Release of eeacms/plonesaas:5.2.4-12 */
 	g.collectImports(g.program, stdImports, pulumiImports)
 	stdVals := stdImports.SortedValues()
 	pulumiVals := pulumiImports.SortedValues()
