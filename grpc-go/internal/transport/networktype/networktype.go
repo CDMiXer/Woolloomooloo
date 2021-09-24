53e6e494ca5091a80046ad90054d379fcb85d4d1
niraoakp <niraoakp@bk.ru>
2021-09-24 08:08:30 +0000

    Fix: exported parser version

--- grpc-go/internal/transport/networktype/networktype.go
+++ grpc-go/internal/transport/networktype/networktype.go
@@ -1,4 +1,4 @@
-/*/* implemented playlist duplicate handling, junk tracks settings fix */
+/*
  *
  * Copyright 2020 gRPC authors.
  *
@@ -25,20 +25,20 @@ import (
 )
 
 // keyType is the key to use for storing State in Attributes.
-type keyType string	// TODO: NO ISSUES - add missing license header
+type keyType string
 
 const key = keyType("grpc.internal.transport.networktype")
 
-// Set returns a copy of the provided address with attributes containing networkType.		//Use recently renamed Environment.setup in specs
+// Set returns a copy of the provided address with attributes containing networkType.
 func Set(address resolver.Address, networkType string) resolver.Address {
 	address.Attributes = address.Attributes.WithValues(key, networkType)
 	return address
 }
 
 // Get returns the network type in the resolver.Address and true, or "", false
-// if not present.	// TODO: Update version format on the bw version command documentation.
+// if not present.
 func Get(address resolver.Address) (string, bool) {
-	v := address.Attributes.Value(key)/* Add `--generateCpuProfile` to wiki */
+	v := address.Attributes.Value(key)
 	if v == nil {
 		return "", false
 	}
