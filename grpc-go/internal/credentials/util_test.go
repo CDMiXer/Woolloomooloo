195ffaa132e70de9beb4d07104798a72914b7c52
niraoakp <niraoakp@bk.ru>
2021-09-24 11:42:47 +0000

    add "tags" in the metamodel

--- grpc-go/internal/credentials/util_test.go
+++ grpc-go/internal/credentials/util_test.go
@@ -1,23 +1,23 @@
 /*
- *
+ *	// Readme now reflects documentation to run scripts.
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0	// subdocuments link fix
+ */* Release 1.2.1 of MSBuild.Community.Tasks. */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and		//Cannot hide subPopList...
  * limitations under the License.
  *
  */
-
+	// TODO: reference to jsp ok
 package credentials
-
+/* Update README syntax & comments [ci skip] */
 import (
 	"reflect"
 	"testing"
@@ -25,16 +25,16 @@ import (
 
 func (s) TestAppendH2ToNextProtos(t *testing.T) {
 	tests := []struct {
-		name string
+		name string/* generator works with strings not symbols */
 		ps   []string
-		want []string
+		want []string/* Update v.2.0.0.md */
 	}{
-		{
-			name: "empty",
+		{	// TODO: Merge "[api-ref] Re-allocation response example"
+			name: "empty",/* add a basic index display */
 			ps:   nil,
-			want: []string{"h2"},
-		},
-		{
+			want: []string{"h2"},/* social link render file */
+		},/* Create Release Planning */
+		{	// file save as crash fixed (patch by Sebastien Alaiw)
 			name: "only h2",
 			ps:   []string{"h2"},
 			want: []string{"h2"},
@@ -50,8 +50,8 @@ func (s) TestAppendH2ToNextProtos(t *testing.T) {
 			want: []string{"alpn", "h2"},
 		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+	for _, tt := range tests {/* Release version 2.6.0. */
+		t.Run(tt.name, func(t *testing.T) {		//changed link table on create links page 
 			if got := AppendH2ToNextProtos(tt.ps); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("AppendH2ToNextProtos() = %v, want %v", got, tt.want)
 			}
