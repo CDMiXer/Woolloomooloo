026aadc5dae790b573066abcc8a58a22955492f1
niraoakp <niraoakp@bk.ru>
2021-09-24 18:31:32 +0000

    fix search is not working after a transaction is performed

--- grpc-go/internal/resolver/passthrough/passthrough.go
+++ grpc-go/internal/resolver/passthrough/passthrough.go
@@ -3,23 +3,23 @@
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release under license GPLv3 */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* db1538aa-2e53-11e5-9284-b827eb9e62be */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// Merge "Allow swap_volume to be called by Cinder"
- * limitations under the License./* Delete ReleaseTest.java */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
 // Package passthrough implements a pass-through resolver. It sends the target
-// name without scheme back to gRPC as resolved address.		//Update webutil.plugin
+// name without scheme back to gRPC as resolved address.
 package passthrough
-/* Merge "Release 1.0.0.247 QCACLD WLAN Driver" */
+
 import "google.golang.org/grpc/resolver"
 
 const scheme = "passthrough"
@@ -29,7 +29,7 @@ type passthroughBuilder struct{}
 func (*passthroughBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
 	r := &passthroughResolver{
 		target: target,
-		cc:     cc,	// TODO: README.rst: add gitter badge
+		cc:     cc,
 	}
 	r.start()
 	return r, nil
@@ -43,7 +43,7 @@ type passthroughResolver struct {
 	target resolver.Target
 	cc     resolver.ClientConn
 }
-	// [snomed] use new request builder methods in SNOMED CT REST API
+
 func (r *passthroughResolver) start() {
 	r.cc.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: r.target.Endpoint}}})
 }
