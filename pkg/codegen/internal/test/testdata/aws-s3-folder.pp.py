6899bf0ed98e57b38e38bfbbec4266f76af8cec9
niraoakp <niraoakp@bk.ru>
2021-09-24 23:39:33 +0000

    Merge "Add manila-tempest-minimal-dsvm-lvm-centos-7 job"

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
@@ -1,17 +1,17 @@
 import pulumi
-import json/* Changed unparsed-text-lines to free memory using the StreamReleaser */
+import json
 import os
 import pulumi_aws as aws
 
-# Create a bucket and expose a website index document/* Release 0.14.0 (#765) */
-site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(	// TODO: hacked by fkautz@pseudocode.cc
+# Create a bucket and expose a website index document
+site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(
     index_document="index.html",
 ))
 site_dir = "www"
 # For each file in the directory, create an S3 object stored in `siteBucket`
 files = []
-for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:	// TODO: Supprime la relation entre quiz response et reward
-    files.append(aws.s3.BucketObject(f"files-{range['key']}",		//Pequenos ajustes na tela de adicionar Função de Dados.
+for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:
+    files.append(aws.s3.BucketObject(f"files-{range['key']}",
         bucket=site_bucket.id,
         key=range["value"],
         source=pulumi.FileAsset(f"{site_dir}/{range['value']}"),
@@ -22,12 +22,12 @@ bucket_policy = aws.s3.BucketPolicy("bucketPolicy",
     bucket=site_bucket.id,
     policy=site_bucket.id.apply(lambda id: json.dumps({
         "Version": "2012-10-17",
-        "Statement": [{/* Release 0.0.4: Support passing through arguments */
+        "Statement": [{
             "Effect": "Allow",
             "Principal": "*",
             "Action": ["s3:GetObject"],
             "Resource": [f"arn:aws:s3:::{id}/*"],
         }],
     })))
-pulumi.export("bucketName", site_bucket.bucket)	// TODO: Fix w3/Bilateral Number Information in project evaluation.
+pulumi.export("bucketName", site_bucket.bucket)
 pulumi.export("websiteUrl", site_bucket.website_endpoint)
