e35c5466bc36fa1cfac1467e3789bb951ec2d107
niraoakp <niraoakp@bk.ru>
2021-09-24 02:44:32 +0000

    add note how to securely install test deps

--- store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
+++ store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
@@ -1,12 +1,12 @@
--- name: create-table-org-secrets/* Merge "Release 4.4.31.73" */
+-- name: create-table-org-secrets
 
 CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
 ,secret_namespace         VARCHAR(50)
 ,secret_name              VARCHAR(200)
 ,secret_type              VARCHAR(50)
-,secret_data              BLOB	// TODO: hacked by arajasek94@gmail.com
+,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN		//Minhas classes atualizadas.
+,secret_pull_request_push BOOLEAN	// Move tests for TestResult model into a dedicated module
 ,UNIQUE(secret_namespace, secret_name)
 );
