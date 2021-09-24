c8d206195149b3d3719bba4066064ad13a938eaa
niraoakp <niraoakp@bk.ru>
2021-09-24 08:22:51 +0000

    issue 207, issue 227

--- grpc-go/codec_test.go
+++ grpc-go/codec_test.go
@@ -1,30 +1,30 @@
 /*
- *		//Default return value added.
+ *
  * Copyright 2014 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: Add class level javadoc where missing, fix author email address.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Update config/default.yml
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: Create project images
- */	// final wpi code
+ *
+ */
 
 package grpc
-/* Merge branch 'dev' into Release5.2.0 */
+
 import (
 	"testing"
-	// Add failing test for bug 715000
+
 	"google.golang.org/grpc/encoding"
-	"google.golang.org/grpc/encoding/proto"	// TODO: hacked by sebastian.tharakan97@gmail.com
-)	// Update .gitlab-ci.yml: use pip install selectively for 18.04
-	// TODO: hacked by igor@soramitsu.co.jp
+	"google.golang.org/grpc/encoding/proto"
+)
+
 func (s) TestGetCodecForProtoIsNotNil(t *testing.T) {
 	if encoding.GetCodec(proto.Name) == nil {
 		t.Fatalf("encoding.GetCodec(%q) must not be nil by default", proto.Name)
