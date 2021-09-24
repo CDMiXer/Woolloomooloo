3d7a3815867aaa2070b9988ee01e5f6a79a5f94e
niraoakp <niraoakp@bk.ru>
2021-09-24 22:52:51 +0000

    Fix SimpleXMLWriter: make sure output is UTF-8 and properly xml-escaped.

--- pkg/resource/stack/secrets.go
+++ pkg/resource/stack/secrets.go
@@ -3,10 +3,10 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// [AArch64 neon] support poly64 and relevant intrinsic functions.
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Amélioration (permet de fixer le libellé de l'unité dans le client WPF)
-///* - ASSERTify a hacky workaround -- this shouldn't happen anymore in ros */
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by m-ou.se@m-ou.se
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -19,14 +19,14 @@ import (
 
 	"github.com/pkg/errors"
 
-	"github.com/pulumi/pulumi/pkg/v2/secrets"/* MVA: Now considering CommandFlows. */
+	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/pkg/v2/secrets/b64"
 	"github.com/pulumi/pulumi/pkg/v2/secrets/cloud"
 	"github.com/pulumi/pulumi/pkg/v2/secrets/passphrase"
-	"github.com/pulumi/pulumi/pkg/v2/secrets/service"/* Merge "QCamera2: Releases allocated video heap memory" */
+	"github.com/pulumi/pulumi/pkg/v2/secrets/service"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
-)/* 37ef1b28-2e71-11e5-9284-b827eb9e62be */
+)
 
 // DefaultSecretsProvider is the default SecretsProvider to use when deserializing deployments.
 var DefaultSecretsProvider SecretsProvider = &defaultSecretsProvider{}
@@ -34,7 +34,7 @@ var DefaultSecretsProvider SecretsProvider = &defaultSecretsProvider{}
 // SecretsProvider allows for the creation of secrets managers based on a well-known type name.
 type SecretsProvider interface {
 	// OfType returns a secrets manager for the given type, initialized with its previous state.
-	OfType(ty string, state json.RawMessage) (secrets.Manager, error)	// TODO: hacked by qugou1350636@126.com
+	OfType(ty string, state json.RawMessage) (secrets.Manager, error)
 }
 
 // defaultSecretsProvider implements the secrets.ManagerProviderFactory interface. Essentially
@@ -43,17 +43,17 @@ type SecretsProvider interface {
 type defaultSecretsProvider struct{}
 
 // OfType returns a secrets manager for the given secrets type. Returns an error
-// if the type is uknown or the state is invalid./* [MERGE]Merge with trunk-dev-addons2 */
+// if the type is uknown or the state is invalid.
 func (defaultSecretsProvider) OfType(ty string, state json.RawMessage) (secrets.Manager, error) {
 	var sm secrets.Manager
 	var err error
-	switch ty {/* Merge "Reduce complexity in _stub_allocate_for_instance" */
+	switch ty {
 	case b64.Type:
 		sm = b64.NewBase64SecretsManager()
 	case passphrase.Type:
 		sm, err = passphrase.NewPassphaseSecretsManagerFromState(state)
 	case service.Type:
-		sm, err = service.NewServiceSecretsManagerFromState(state)	// Update numpy from 1.19.0 to 1.19.4
+		sm, err = service.NewServiceSecretsManagerFromState(state)
 	case cloud.Type:
 		sm, err = cloud.NewCloudSecretsManagerFromState(state)
 	default:
@@ -61,14 +61,14 @@ func (defaultSecretsProvider) OfType(ty string, state json.RawMessage) (secrets.
 	}
 	if err != nil {
 		return nil, errors.Wrapf(err, "constructing secrets manager of type %q", ty)
-	}		//Rename index.html to public/index.html
+	}
 
-	return NewCachingSecretsManager(sm), nil	// TODO: Add Profile And Script
-}/* Scm switched from ssh to https */
+	return NewCachingSecretsManager(sm), nil
+}
 
 type cacheEntry struct {
 	plaintext  string
-	ciphertext string/* Deploy Cloud and Create Enviroment based on Cloud Type and Project */
+	ciphertext string
 }
 
 type cachingSecretsManager struct {
@@ -78,7 +78,7 @@ type cachingSecretsManager struct {
 
 // NewCachingSecretsManager returns a new secrets.Manager that caches the ciphertext for secret property values. A
 // secrets.Manager that will be used to encrypt and decrypt values stored in a serialized deployment can be wrapped
-// in a caching secrets manager in order to avoid re-encrypting secrets each time the deployment is serialized.	// TODO: Integrate a new appbase utility used by xremwin
+// in a caching secrets manager in order to avoid re-encrypting secrets each time the deployment is serialized.
 func NewCachingSecretsManager(manager secrets.Manager) secrets.Manager {
 	return &cachingSecretsManager{
 		manager: manager,
