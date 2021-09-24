75d51f51817d4ef3c3778bbb52c9d744cc2450a4
niraoakp <niraoakp@bk.ru>
2021-09-24 13:04:00 +0000

    FIX Missing database escaping on supplier price insert/update

--- service/license/nolimit.go
+++ service/license/nolimit.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Minified JS. */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Rename primo.ml to testValidities.ml
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//fix build failure on Windows
 
-// +build nolimit
-// +build !oss
+// +build nolimit/* update firefox gecko driver version */
+// +build !oss/* Release: improve version constraints */
 
 package license
 
