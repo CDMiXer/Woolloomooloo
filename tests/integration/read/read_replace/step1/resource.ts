d723796c54998dc4c9fde860b3ebe12f2e20fc09
niraoakp <niraoakp@bk.ru>
2021-09-24 18:26:27 +0000

    smart page filter

--- tests/integration/read/read_replace/step1/resource.ts
+++ tests/integration/read/read_replace/step1/resource.ts
@@ -1,30 +1,30 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Sub: Update ReleaseNotes.txt for 3.5-rc1 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// FIX #17 No se eliminaban materias asociadas a unidad
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Adding timeout */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Adding media part 4.
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";	// fix raw count props when sparse
-/* Released 1.3.0 */
+import * as dynamic from "@pulumi/pulumi/dynamic";
+
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
-		//Restore phppaser 1.3 support
-    private id: number = 0;		//Added some more Information in the README
+
+    private id: number = 0;
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
             inputs: news,
         }
-    }/* fixed CMakeLists.txt compiler options and set Release as default */
+    }
 
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (news.state !== olds.state) {
@@ -32,7 +32,7 @@ export class Provider implements dynamic.ResourceProvider {
                 changes: true,
                 replaces: ["state"],
             };
-        }/* Release of eeacms/jenkins-slave-dind:17.12-3.21 */
+        }
 
         return {
             changes: false,
@@ -40,14 +40,14 @@ export class Provider implements dynamic.ResourceProvider {
     }
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
-        return {/* Release: Making ready for next release iteration 5.4.2 */
-            id: (this.id++).toString(),/* Have a project frame.  */
-            outs: inputs,	// TODO: Rename zouwu to zouwu.md
+        return {
+            id: (this.id++).toString(),
+            outs: inputs,
         }
-}    
+    }
 
-    public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {/* Release version [11.0.0] - prepare */
-        throw Error("this resource is replace-only and can't be updated");/* Merge "Fix cache partition support" */
+    public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
+        throw Error("this resource is replace-only and can't be updated");
     }
 
     public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {
