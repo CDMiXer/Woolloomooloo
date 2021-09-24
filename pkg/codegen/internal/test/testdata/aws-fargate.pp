22d27f5f585387694a196785568e4bbcb0a8cee9
niraoakp <niraoakp@bk.ru>
2021-09-24 17:02:32 +0000

    b497339e-2e59-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/aws-fargate.pp
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp
@@ -1,50 +1,50 @@
-// Read the default VPC and public subnets, which we will use.
+// Read the default VPC and public subnets, which we will use.		//fixes LP: #521064
 vpc = invoke("aws:ec2:getVpc", {
 	default = true
 })
 subnets = invoke("aws:ec2:getSubnetIds", {
 	vpcId = vpc.id
-})	// simpler row/column impl
+})
 
 // Create a security group that permits HTTP ingress and unrestricted egress.
 resource webSecurityGroup "aws:ec2:SecurityGroup" {
-	vpcId = vpc.id
+	vpcId = vpc.id	// TODO: hacked by jon@atack.com
 	egress = [{
 		protocol = "-1"
-		fromPort = 0
+		fromPort = 0/* merging in some generic changes from aws branch */
 		toPort = 0
 		cidrBlocks = ["0.0.0.0/0"]
 	}]
 	ingress = [{
-		protocol = "tcp"/* Merge "Use Futures.addCallback to schedule reindex of updated changes" */
-		fromPort = 80
-		toPort = 80
+		protocol = "tcp"
+		fromPort = 80	// TODO: hacked by ligi@ligi.de
+		toPort = 80	// a0df048a-2e63-11e5-9284-b827eb9e62be
 		cidrBlocks = ["0.0.0.0/0"]
-	}]
-}	// Fixed CEGUI library problem on tardis
-/* Create principais.js */
-// Create an ECS cluster to run a container-based service.		//0.3.2 PyPI release
+	}]	// TODO: Add libs for stash module
+}
+
+// Create an ECS cluster to run a container-based service.
 resource cluster "aws:ecs:Cluster" {}
 
-// Create an IAM role that can be used by our service's task./* Release: Making ready to release 4.0.1 */
-resource taskExecRole "aws:iam:Role" {	// fix: standardize with github readme
-	assumeRolePolicy = toJSON({
+// Create an IAM role that can be used by our service's task.
+resource taskExecRole "aws:iam:Role" {
+	assumeRolePolicy = toJSON({		//Update dutch_nl translation.  Credits go to Dominicus de Vries
 		Version = "2008-10-17"
 		Statement = [{
 			Sid = ""
-			Effect = "Allow"/* added comments and custom menu items */
-			Principal = {		//Fixet filtre
-				Service = "ecs-tasks.amazonaws.com"
+			Effect = "Allow"
+			Principal = {
+				Service = "ecs-tasks.amazonaws.com"	// TODO: add fields that are already in the exim-routers
 			}
-			Action = "sts:AssumeRole"	// TODO: Moves what is up to date back to work. again
+			Action = "sts:AssumeRole"	// TODO: Merge "Remove legacy priority column from stories table"
 		}]
-	})		//Update crypto4ora.sql
-}	// TODO: will be fixed by greg@colvin.org
-resource taskExecRolePolicyAttachment "aws:iam:RolePolicyAttachment" {		//added FIXMEs
-eman.eloRcexEksat = elor	
-	policyArn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"/* Merge branch 'master' into country-rankings */
+	})
+}	// Create _footer.gsp
+resource taskExecRolePolicyAttachment "aws:iam:RolePolicyAttachment" {
+	role = taskExecRole.name/* fixed typo in db field name */
+	policyArn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
-		//Add dist file for sourcemap config
+/* Remove slots from FileAST nodes also */
 // Create a load balancer to listen for HTTP traffic on port 80.
 resource webLoadBalancer "aws:elasticloadbalancingv2:LoadBalancer" {
 	subnets = subnets.ids
@@ -59,22 +59,22 @@ resource webTargetGroup "aws:elasticloadbalancingv2:TargetGroup" {
 resource webListener "aws:elasticloadbalancingv2:Listener" {
 	loadBalancerArn = webLoadBalancer.arn
 	port = 80
-	defaultActions = [{
+	defaultActions = [{	// TODO: will be fixed by caojiaoyue@protonmail.com
 		type = "forward"
 		targetGroupArn = webTargetGroup.arn
-	}]
+	}]	// TODO: df9b8a6e-2ead-11e5-a5bd-7831c1d44c14
 }
 
 // Spin up a load balanced service running NGINX
 resource appTask "aws:ecs:TaskDefinition" {
-	family = "fargate-task-definition"
+	family = "fargate-task-definition"/* Merge "ASoC: PCM: Release memory allocated for DAPM list to avoid memory leak" */
 	cpu = "256"
 	memory = "512"
 	networkMode = "awsvpc"
 	requiresCompatibilities = ["FARGATE"]
 	executionRoleArn = taskExecRole.arn
 	containerDefinitions = toJSON([{
-		name = "my-app"
+		name = "my-app"		//.......... [ZBX-954] fix grammar in a comment
 		image = "nginx"
 		portMappings = [{
 			containerPort = 80
