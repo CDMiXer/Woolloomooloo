c2beae8b6efb5372431cee3579a05905f6c00b6e
niraoakp <niraoakp@bk.ru>
2021-09-24 05:19:19 +0000

    5c50d266-2e6e-11e5-9284-b827eb9e62be

--- core/commit.go
+++ core/commit.go
@@ -2,18 +2,18 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//remove dead domains / obsolete filters
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by arajasek94@gmail.com
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by mail@bitpshr.net
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil //
+// limitations under the License.
 
 package core
-	// TODO: Adding a couple of more log messages
+
 import "context"
 
 type (
@@ -27,32 +27,32 @@ type (
 		Link      string
 	}
 
-	// Committer represents the commit author.	// TODO: Imported Upstream version 1.2.1-1~2b7c703
-{ tcurts rettimmoC	
-		Name   string	// TODO: Merge "Port test_netapp to Python 3"
+	// Committer represents the commit author.
+	Committer struct {
+		Name   string
 		Email  string
-		Date   int64/* New translations kaminari.yml (Galician) */
-		Login  string		//BufferType ->BlockType
+		Date   int64
+		Login  string
 		Avatar string
 	}
-/* Merge fix-osc-innodb-bug-996110. */
+
 	// Change represents a file change in a commit.
 	Change struct {
-		Path    string/* Merge "Release 0.19.2" */
+		Path    string
 		Added   bool
-loob demaneR		
+		Renamed bool
 		Deleted bool
 	}
 
 	// CommitService provides access to the commit history from
 	// the external source code management service (e.g. GitHub).
 	CommitService interface {
-		// Find returns the commit information by sha.		//Merge "v0.0.7"
+		// Find returns the commit information by sha.
 		Find(ctx context.Context, user *User, repo, sha string) (*Commit, error)
 
 		// FindRef returns the commit information by reference.
 		FindRef(ctx context.Context, user *User, repo, ref string) (*Commit, error)
-/* Merge "Release note for Zaqar resource support" */
+
 		// ListChanges returns the files change by sha or reference.
 		ListChanges(ctx context.Context, user *User, repo, sha, ref string) ([]*Change, error)
 	}
