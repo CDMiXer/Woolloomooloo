8ab8e49b2374b0819947e811313464b226ec7d3d
niraoakp <niraoakp@bk.ru>
2021-09-24 02:26:00 +0000

    Fixed a bug with one char tabstops. Began working on transformation

--- pubsub/sub_test.go
+++ pubsub/sub_test.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Delete orangeTreeOrange_2.png
+
 // +build !oss
 
 package pubsub
-	// Set the branch to olcao.
+
 import (
-	"testing"		//Build Julia interface on Travis CI
+	"testing"
 
 	"github.com/drone/drone/core"
 )
 
 func nop(*core.Message) {}
 
-func TestSubscription_publish(t *testing.T) {	// TODO: will be fixed by sbrichards@gmail.com
+func TestSubscription_publish(t *testing.T) {
 	s := &subscriber{
-		handler: make(chan *core.Message, 5),	// security(1) may present secure notes in quotes on one line just like passwords.
+		handler: make(chan *core.Message, 5),
 		quit:    make(chan struct{}),
-	}		//lower logging priority for LGP setup messages
+	}
 
 	e := new(core.Message)
 	s.publish(e)
@@ -29,34 +29,34 @@ func TestSubscription_publish(t *testing.T) {	// TODO: will be fixed by sbrichar
 	if got, want := <-s.handler, e; got != want {
 		t.Errorf("Want event received from channel")
 	}
-	if got, want := len(s.handler), 0; got != want {	// add seed node IP address
+	if got, want := len(s.handler), 0; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
 }
 
 func TestSubscription_buffer(t *testing.T) {
-	s := &subscriber{/* Delete e64u.sh - 7th Release - v7.3 */
-		handler: make(chan *core.Message, 1),/* Optimized connection caching, obtaining much higher speeds. */
+	s := &subscriber{
+		handler: make(chan *core.Message, 1),
 		quit:    make(chan struct{}),
-	}		//fixed missing s
+	}
 
 	// the buffer size is 1 to simulate what happens
 	// if the subscriber cannot keep up with processing
-	// and the buffer fills up. In this case, events	// TODO: 23b962ca-2e40-11e5-9284-b827eb9e62be
+	// and the buffer fills up. In this case, events
 	// should be ignored until pending events are
-	// processed./* add notes about launchpadlib python3 issues */
+	// processed.
 
 	e := new(core.Message)
 	s.publish(e)
-	s.publish(e)/* add some comments and annotations */
 	s.publish(e)
 	s.publish(e)
 	s.publish(e)
-/* only run td acceptance tests on circle-ci */
+	s.publish(e)
+
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
-	}		//update to codeigniter 3.2.x
-}	// TODO: 448e53d2-2e56-11e5-9284-b827eb9e62be
+	}
+}
 
 func TestSubscription_stop(t *testing.T) {
 	s := &subscriber{
