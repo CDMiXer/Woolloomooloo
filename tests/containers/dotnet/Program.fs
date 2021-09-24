a1151bdc8b1fec5d3d86a4f72d1dcb91a9001891
niraoakp <niraoakp@bk.ru>
2021-09-24 09:34:31 +0000

    500 per 10min is less than 10 per 10sec... RITO PLZ

--- tests/containers/dotnet/Program.fs
+++ tests/containers/dotnet/Program.fs
@@ -1,16 +1,16 @@
-margorP eludom﻿
+﻿module Program		//aa693b82-2e41-11e5-9284-b827eb9e62be
 
-open System
+open System	// TODO: hacked by lexy8russo@outlook.com
 open Pulumi.FSharp
 
 let infra () =
   let config = new Pulumi.Config()
-  let runtime = config.Get("runtime")
+  let runtime = config.Get("runtime")	// TODO: Removing jquery dependency from harness.js
   Console.WriteLine("Hello from {0}", runtime)
-  	// Cleaned up test config and added unit tests for plain passworded client.
+  
   // Stack outputs
   dict []
 
 [<EntryPoint>]
-let main _ =
+let main _ =/* Release version: 1.9.0 */
   Deployment.run infra
