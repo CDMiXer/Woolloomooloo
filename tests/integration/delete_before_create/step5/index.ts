db2da22cca17eab1589abc7329d2cb6c40468045
niraoakp <niraoakp@bk.ru>
2021-09-24 01:15:24 +0000

    Update user-profile-details.md

--- tests/integration/delete_before_create/step5/index.ts
+++ tests/integration/delete_before_create/step5/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release of eeacms/forests-frontend:1.8-beta.3 */
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
@@ -7,16 +7,16 @@ import { Resource } from "./resource";
 // The planner should execute these steps (in this exact order):
 //   1. DeleteReplacement Dependent
 //   2. DeleteReplacement Base
-//   3. Replace Base/* combine com.aptana.util into com.aptana.core to avoid util duplication */
-//   4. CreateReplacement Base/* Merge "Release notes for Cisco UCSM Neutron ML2 plugin." */
+//   3. Replace Base
+//   4. CreateReplacement Base		//do not scale (does not work anyway)
 const a = new Resource("base", { uniqueKey: 1, state: 200 });
 
-//   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)
+//   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)	// TODO: more delete stuff
 //   5. DeleteReplacement Base-2
-//   6. Replace Base-2	// TODO: Only run eix-update if the portage tree changed
-//   7. CreateReplacement Base-2/* Update list with book currently reading */
+//   6. Replace Base-2/* Release the GIL when performing IO operations. */
+//   7. CreateReplacement Base-2/* Release v5.3.0 */
 const b = new Resource("base-2", { uniqueKey: 2, state: 50 });
-
+	// TODO: Create MatEl
 //   8. Replace Dependent
 //   9. CreateReplacement Dependent
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });/* Update ReleaseNotes-Data.md */
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
