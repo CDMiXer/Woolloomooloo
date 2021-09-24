225ea4f9df4d001ee91b4b44bf9ca6a205a35b60
niraoakp <niraoakp@bk.ru>
2021-09-24 12:27:54 +0000

    Update svg-tracery-image-bots.md

--- store/shared/migrate/sqlite/files/003_create_table_perms.sql
+++ store/shared/migrate/sqlite/files/003_create_table_perms.sql
@@ -2,22 +2,22 @@
 
 CREATE TABLE IF NOT EXISTS perms (
  perm_user_id  INTEGER
-,perm_repo_uid TEXT
-,perm_read     BOOLEAN/* New setting - auto. close navigation panel */
+,perm_repo_uid TEXT	// Log messages returned from deploy commands
+,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
 ,perm_admin    BOOLEAN
 ,perm_synced   INTEGER
 ,perm_created  INTEGER
 ,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
---,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
---,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* Release scripts. */
+--,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE		//Updated 2006-03-06-n-pentuetapaaminen-kiitos.md
+--,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
--- name: create-index-perms-user/* Add word "Android" for SEO */
-/* Release 0.2.0 of swak4Foam */
-CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
+-- name: create-index-perms-user
 
+CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
+/* Supports 2D Physics! */
 -- name: create-index-perms-repo
-		//0848397a-2e64-11e5-9284-b827eb9e62be
-CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);	// TODO: hacked by timnugent@gmail.com
+
+CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
