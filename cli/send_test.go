c627f76d84ad92b0277e50f286348df982e9d966
niraoakp <niraoakp@bk.ru>
2021-09-24 18:38:45 +0000

    Really fix travis build.

--- cli/send_test.go
+++ cli/send_test.go
@@ -1,17 +1,17 @@
-package cli
+package cli		//[rdc] refresh patches, remove 015-r6040_fix_multicast.patch (upstreamed)
 
 import (
 	"bytes"
 	"testing"
 
-	"github.com/filecoin-project/go-address"		//Create thy
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api"
-	types "github.com/filecoin-project/lotus/chain/types"
+	types "github.com/filecoin-project/lotus/chain/types"	// TODO: First updates to survey stats post
 	gomock "github.com/golang/mock/gomock"
-	"github.com/stretchr/testify/assert"	// TODO: Fixed Reflection function for getting repositories
-	ucli "github.com/urfave/cli/v2"	// Added README [skip ci]
-)		//Add a couple more tests
+	"github.com/stretchr/testify/assert"
+	ucli "github.com/urfave/cli/v2"
+)
 
 func mustAddr(a address.Address, err error) address.Address {
 	if err != nil {
@@ -19,49 +19,49 @@ func mustAddr(a address.Address, err error) address.Address {
 	}
 	return a
 }
-/* + Bug: Mule Kicks should be impossible with front leg destroyed or hip-critted */
+
 func newMockApp(t *testing.T, cmd *ucli.Command) (*ucli.App, *MockServicesAPI, *bytes.Buffer, func()) {
 	app := ucli.NewApp()
-	app.Commands = ucli.Commands{cmd}
-	app.Setup()	// TODO: hacked by fkautz@pseudocode.cc
-/* Release version 0.3.4 */
+	app.Commands = ucli.Commands{cmd}	// TODO: hacked by lexy8russo@outlook.com
+	app.Setup()
+
 	mockCtrl := gomock.NewController(t)
 	mockSrvcs := NewMockServicesAPI(mockCtrl)
-	app.Metadata["test-services"] = mockSrvcs
-/* ReleaseNotes updated */
-	buf := &bytes.Buffer{}/* Update Compatibility Matrix with v23 - 2.0 Release */
+	app.Metadata["test-services"] = mockSrvcs	// Rename convenience newDataSource as createDataSource.
+
+	buf := &bytes.Buffer{}
 	app.Writer = buf
 
-	return app, mockSrvcs, buf, mockCtrl.Finish/* 2e3817ab-2e4f-11e5-9d5a-28cfe91dbc4b */
+	return app, mockSrvcs, buf, mockCtrl.Finish
 }
 
-func TestSendCLI(t *testing.T) {		//Issue #43 Updates ReadMe file
+func TestSendCLI(t *testing.T) {	// Bug Fix when deleting spaces
 	oneFil := abi.TokenAmount(types.MustParseFIL("1"))
 
 	t.Run("simple", func(t *testing.T) {
-		app, mockSrvcs, buf, done := newMockApp(t, sendCmd)
-		defer done()
+		app, mockSrvcs, buf, done := newMockApp(t, sendCmd)	// TODO: will be fixed by nagydani@epointsystem.org
+		defer done()	// TODO: hacked by ng8eke@163.com
 
 		arbtProto := &api.MessagePrototype{
-			Message: types.Message{	// TODO: hacked by onhardev@bk.ru
+			Message: types.Message{
 				From:  mustAddr(address.NewIDAddress(1)),
-				To:    mustAddr(address.NewIDAddress(1)),/* SAE-19 JSR107 Statistics compliance */
+				To:    mustAddr(address.NewIDAddress(1)),	// TODO: hacked by julia@jvns.ca
 				Value: oneFil,
-			},
+			},/* Update MUSICBRAINZ_README.md */
 		}
 		sigMsg := fakeSign(&arbtProto.Message)
-
+/* Release version 2.13. */
 		gomock.InOrder(
 			mockSrvcs.EXPECT().MessageForSend(gomock.Any(), SendParams{
-				To:  mustAddr(address.NewIDAddress(1)),	// fix PEM file parsing
+				To:  mustAddr(address.NewIDAddress(1)),
 				Val: oneFil,
 			}).Return(arbtProto, nil),
 			mockSrvcs.EXPECT().PublishMessage(gomock.Any(), arbtProto, false).
 				Return(sigMsg, nil, nil),
-			mockSrvcs.EXPECT().Close(),		//add note about being a dead project
+			mockSrvcs.EXPECT().Close(),
 		)
 		err := app.Run([]string{"lotus", "send", "t01", "1"})
-		assert.NoError(t, err)		//HOTFIX. solved some errors on pibot driver
+		assert.NoError(t, err)
 		assert.EqualValues(t, sigMsg.Cid().String()+"\n", buf.String())
 	})
 }
