d40810eb860bb670d4563f12f38ab7715b5b6a74
niraoakp <niraoakp@bk.ru>
2021-09-24 13:10:46 +0000

    Delete Door.png

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
@@ -1,35 +1,35 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* (GH-495) Update GitReleaseManager reference from 0.8.0 to 0.9.0 */
 import * as aws from "@pulumi/aws";
 import * from "fs";
 
 // Create a bucket and expose a website index document
 const siteBucket = new aws.s3.Bucket("siteBucket", {website: {
-    indexDocument: "index.html",/* Merge "Update version in finalize-installation-rdo" */
+    indexDocument: "index.html",
 }});
 const siteDir = "www";
-// For each file in the directory, create an S3 object stored in `siteBucket`
-const files: aws.s3.BucketObject[];
+// For each file in the directory, create an S3 object stored in `siteBucket`	// TODO: Prepare version 1.4.2
+const files: aws.s3.BucketObject[];	// da5c82c2-2e64-11e5-9284-b827eb9e62be
 for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {
     files.push(new aws.s3.BucketObject(`files-${range.key}`, {
-        bucket: siteBucket.id,
+        bucket: siteBucket.id,/* Merge "Rename ActivityCompat23.java to ActivityCompatApi23.java" */
         key: range.value,
         source: new pulumi.asset.FileAsset(`${siteDir}/${range.value}`),
-        contentType: (() => throw new Error("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))(),		//Modify ^X attribute parsing to handle polyself with gender change
-    }));		//docs(limits): simple rate limits documentation (#126)
+        contentType: (() => throw new Error("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))(),
+    }));
 }
-// set the MIME type of the file/* Merge "Move _capture_stdout to a common place" */
+// set the MIME type of the file
 // Set the access policy for the bucket so all objects are readable
-const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
+const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {	// TODO: Updated TODO-List.
     bucket: siteBucket.id,
     policy: siteBucket.id.apply(id => JSON.stringify({
-        Version: "2012-10-17",/* chore(package): rollup@1.15.4 */
+        Version: "2012-10-17",
         Statement: [{
             Effect: "Allow",
-            Principal: "*",
+            Principal: "*",/* Add test that params (dynamic segments) are passed */
             Action: ["s3:GetObject"],
             Resource: [`arn:aws:s3:::${id}/*`],
         }],
     })),
-});	// Disabled temp copy of old episodes
-export const bucketName = siteBucket.bucket;	// TODO: will be fixed by steven@stebalien.com
+});
+export const bucketName = siteBucket.bucket;
 export const websiteUrl = siteBucket.websiteEndpoint;
