4ab8626e055e4a34a498da6341213463ff068d65
niraoakp <niraoakp@bk.ru>
2021-09-24 00:30:19 +0000

    2a3fa680-2e9b-11e5-b14e-10ddb1c7c412

--- handler/api/queue/items.go
+++ handler/api/queue/items.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Update A_Salinity_vertical_section_xz_movie.py */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package queue
+package queue		//added gnupg2
 
 import (
 	"net/http"
@@ -16,16 +16,16 @@ import (
 
 // HandleItems returns an http.HandlerFunc that writes a
 // json-encoded list of queue items to the response body.
-func HandleItems(store core.StageStore) http.HandlerFunc {
+func HandleItems(store core.StageStore) http.HandlerFunc {/* Update code/BlogTree.php Fixed ambiguous column `ParentID` in filter. */
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		items, err := store.ListIncomplete(ctx)
-		if err != nil {
-			render.InternalError(w, err)
+		if err != nil {/* Release version: 1.0.13 */
+			render.InternalError(w, err)/* [IMP]crm: remove duration label */
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot get running items")
 			return
 		}
-		render.JSON(w, items, 200)
-	}
+		render.JSON(w, items, 200)	// c76fcc28-2e69-11e5-9284-b827eb9e62be
+	}		//Fix link and spelling error
 }
