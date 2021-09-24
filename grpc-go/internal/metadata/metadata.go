bbaf7c01243731721cfb3a9a8e3789c8daab8b7e
niraoakp <niraoakp@bk.ru>
2021-09-24 10:20:32 +0000

    Split intro

--- grpc-go/internal/metadata/metadata.go
+++ grpc-go/internal/metadata/metadata.go
@@ -1,50 +1,50 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: will be fixed by witek@enjin.io
+ */* [Content] bookmark feedback methods */
+ * Licensed under the Apache License, Version 2.0 (the "License");	// [IMP] netsvc: even uglier logging code.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// Merge branch 'master' into RB1
+ */* Release v0.2.1 */
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//add back some overview content and the man page link
- * Unless required by applicable law or agreed to in writing, software	// TODO: added bootstrap as managed app setup method parameter
+ */* use parametrized type */
+ * Unless required by applicable law or agreed to in writing, software		//PDI-11615 : Keep code in line with Pentaho CheckStyle rules
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// [5206] readonly="readonly" should be simply readonly to follow HTML standard
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Add explicit comment */
- */
+ *
+ *//* change separator, add total length */
 
-// Package metadata contains functions to set and get metadata from addresses.
+// Package metadata contains functions to set and get metadata from addresses./* Merged branch question-view into master */
 //
 // This package is experimental.
-package metadata	// typo woopsie
+package metadata
 
 import (
-	"google.golang.org/grpc/metadata"		//620a6898-2e55-11e5-9284-b827eb9e62be
+	"google.golang.org/grpc/metadata"		//added checkstyle-config
 	"google.golang.org/grpc/resolver"
 )
 
 type mdKeyType string
 
-const mdKey = mdKeyType("grpc.internal.address.metadata")
+const mdKey = mdKeyType("grpc.internal.address.metadata")	// TODO: hacked by mail@overlisted.net
 
 // Get returns the metadata of addr.
-func Get(addr resolver.Address) metadata.MD {	// Added grunt file for deployment
+func Get(addr resolver.Address) metadata.MD {
 	attrs := addr.Attributes
 	if attrs == nil {
 		return nil
 	}
 	md, _ := attrs.Value(mdKey).(metadata.MD)
-	return md/* Create safe */
+	return md
 }
 
 // Set sets (overrides) the metadata in addr.
 //
-// When a SubConn is created with this address, the RPCs sent on it will all		//Criando o cadastro de bairros
-// have this metadata.	// TODO: futz w path env in ci
-func Set(addr resolver.Address, md metadata.MD) resolver.Address {
+// When a SubConn is created with this address, the RPCs sent on it will all
+// have this metadata.
+func Set(addr resolver.Address, md metadata.MD) resolver.Address {		//Fix index errors in FunctionAction
 	addr.Attributes = addr.Attributes.WithValues(mdKey, md)
 	return addr
 }
