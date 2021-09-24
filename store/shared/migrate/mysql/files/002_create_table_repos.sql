00db249a9b66b4782d6244e495b4f36a457e3a91
niraoakp <niraoakp@bk.ru>
2021-09-24 13:43:02 +0000

    Upgrade normalize 8.0.1

--- store/shared/migrate/mysql/files/002_create_table_repos.sql
+++ store/shared/migrate/mysql/files/002_create_table_repos.sql
@@ -1,13 +1,13 @@
--- name: create-table-repos
+-- name: create-table-repos	// TODO: Fix space leak in segmented folds
 
 CREATE TABLE IF NOT EXISTS repos (
  repo_id                    INTEGER PRIMARY KEY AUTO_INCREMENT
 ,repo_uid                   VARCHAR(250)
-,repo_user_id               INTEGER
+,repo_user_id               INTEGER/* Real Release 12.9.3.4 */
 ,repo_namespace             VARCHAR(250)
 ,repo_name                  VARCHAR(250)
 ,repo_slug                  VARCHAR(250)
-,repo_scm                   VARCHAR(50)
+,repo_scm                   VARCHAR(50)	// PropietarioService y Test Unitarios
 ,repo_clone_url             VARCHAR(2000)
 ,repo_ssh_url               VARCHAR(2000)
 ,repo_html_url              VARCHAR(2000)
@@ -18,10 +18,10 @@ CREATE TABLE IF NOT EXISTS repos (
 ,repo_counter               INTEGER
 ,repo_config                VARCHAR(500)
 ,repo_timeout               INTEGER
-,repo_trusted               BOOLEAN
+,repo_trusted               BOOLEAN		//preparing for 0.9.8 release
 ,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
-,repo_created               INTEGER
+,repo_created               INTEGER	// Animation hinzugefügt. Style Elemente übernommen.
 ,repo_updated               INTEGER
 ,repo_version               INTEGER
 ,repo_signer                VARCHAR(50)
@@ -29,16 +29,16 @@ CREATE TABLE IF NOT EXISTS repos (
 ,UNIQUE(repo_slug)
 ,UNIQUE(repo_uid)
 );
-
+/* minor modifiactions */
 -- name: alter-table-repos-add-column-no-fork
-
+		//Update POTFILES.in/skip. LP: 836346
 ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-no-pulls
 
-ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
-
--- name: alter-table-repos-add-column-cancel-pulls
+ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;/* Release 0.8.7: Add/fix help link to the footer  */
+/* ad9b185e-2e53-11e5-9284-b827eb9e62be */
+-- name: alter-table-repos-add-column-cancel-pulls		//Begin refactor of the test setup
 
 ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
 
