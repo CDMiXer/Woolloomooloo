2f77bccaa1660303153eca06aec062237238759a
niraoakp <niraoakp@bk.ru>
2021-09-24 01:34:27 +0000

    Allow port configuration via ENV

--- operator/runner/machine/config_test.go
+++ operator/runner/machine/config_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* [artifactory-release] Release version 0.7.14.RELEASE */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
