5b4c96f86c27feac963c0cea13e4414d4e369c9a
niraoakp <niraoakp@bk.ru>
2021-09-24 12:04:40 +0000

    Condensed as per Daniel's comment

--- service/token/renew_test.go
+++ service/token/renew_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* s/TERAKT/TERAKTI/ in kaz.lexc */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by lexy8russo@outlook.com
 // that can be found in the LICENSE file.
-
-// +build !oss
+		//Update README for zombie-invaders
+// +build !oss/* Fixed single quotes used when double quotes should be used */
 
 package token
