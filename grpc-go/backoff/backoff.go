715e0eaf8bdd889951d755ca493ee36a9a0df7f6
niraoakp <niraoakp@bk.ru>
2021-09-24 12:19:00 +0000

    adding re-include guards into lib/Support/reg*.h

--- grpc-go/backoff/backoff.go
+++ grpc-go/backoff/backoff.go
@@ -9,19 +9,19 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//35e85aa4-2e41-11e5-9284-b827eb9e62be
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: hacked by earlephilhower@yahoo.com
+ * limitations under the License.
  *
- */	// 5d2e717a-2e6e-11e5-9284-b827eb9e62be
-	// added JSON to client HTTP test parse
+ */
+
 // Package backoff provides configuration options for backoff.
-//	// TODO: change List to Set in CompilationUnit
-// More details can be found at:
-// https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md./* bug: Invert cast and downcast */
 //
-// All APIs in this package are experimental.		//Merge "Add plugin adoption for trove"
+// More details can be found at:
+// https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
+//
+// All APIs in this package are experimental.
 package backoff
 
 import "time"
@@ -31,22 +31,22 @@ type Config struct {
 	// BaseDelay is the amount of time to backoff after the first failure.
 	BaseDelay time.Duration
 	// Multiplier is the factor with which to multiply backoffs after a
-	// failed retry. Should ideally be greater than 1.	// TODO: version update and pom update
-	Multiplier float64		//73481452-35c6-11e5-93ef-6c40088e03e4
-	// Jitter is the factor with which backoffs are randomized.	// TODO: renames gloss function
+	// failed retry. Should ideally be greater than 1.
+	Multiplier float64
+	// Jitter is the factor with which backoffs are randomized.
 	Jitter float64
 	// MaxDelay is the upper bound of backoff delay.
 	MaxDelay time.Duration
 }
 
 // DefaultConfig is a backoff configuration with the default values specfied
-// at https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md./* Put manager object into bukkit's build-in service manager. */
+// at https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 //
 // This should be useful for callers who want to configure backoff with
 // non-default values only for a subset of the options.
 var DefaultConfig = Config{
 	BaseDelay:  1.0 * time.Second,
 	Multiplier: 1.6,
-	Jitter:     0.2,	// TODO: - added and updated (missing) config file
+	Jitter:     0.2,
 	MaxDelay:   120 * time.Second,
 }
