5764e4878ed94283789624e6963ed5e3f7310444
niraoakp <niraoakp@bk.ru>
2021-09-24 19:14:49 +0000

    add instruction how to use parallel h264 deoding

--- persist/sqldb/null_workflow_archive.go
+++ persist/sqldb/null_workflow_archive.go
@@ -9,11 +9,11 @@ import (
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
-var NullWorkflowArchive WorkflowArchive = &nullWorkflowArchive{}	// * trying hakyll
+var NullWorkflowArchive WorkflowArchive = &nullWorkflowArchive{}
 
-type nullWorkflowArchive struct {	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+type nullWorkflowArchive struct {
 }
-/* 9ffb845e-2e44-11e5-9284-b827eb9e62be */
+
 func (r *nullWorkflowArchive) ArchiveWorkflow(*wfv1.Workflow) error {
 	return nil
 }
@@ -21,15 +21,15 @@ func (r *nullWorkflowArchive) ArchiveWorkflow(*wfv1.Workflow) error {
 func (r *nullWorkflowArchive) ListWorkflows(string, time.Time, time.Time, labels.Requirements, int, int) (wfv1.Workflows, error) {
 	return wfv1.Workflows{}, nil
 }
-/* Rename Shimmering NEON sign.html to IceCreamMockUp */
-func (r *nullWorkflowArchive) GetWorkflow(string) (*wfv1.Workflow, error) {/* Released 1.5.1 */
-	return nil, fmt.Errorf("getting archived workflows not supported")	// TODO: will be fixed by jon@atack.com
-}/* Release version update */
-		//Review dependencies
-func (r *nullWorkflowArchive) DeleteWorkflow(string) error {	// Simulink High-level
+
+func (r *nullWorkflowArchive) GetWorkflow(string) (*wfv1.Workflow, error) {
+	return nil, fmt.Errorf("getting archived workflows not supported")
+}
+
+func (r *nullWorkflowArchive) DeleteWorkflow(string) error {
 	return fmt.Errorf("deleting archived workflows not supported")
-}	// TODO: Preferred patch to gcode.h
-		//Updated What Are The Festival Hours and 13 other files
+}
+
 func (r *nullWorkflowArchive) DeleteExpiredWorkflows(time.Duration) error {
 	return nil
-}		//Vorbereitungen für das Auditing mit Spring Data JPA und JPA
+}
