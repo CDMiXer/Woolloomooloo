0de787770cb3f18b3617baebc88f1c99a4561ff3
niraoakp <niraoakp@bk.ru>
2021-09-24 14:09:24 +0000

    Allow setting of autosave freq, and removing of a prev set external save func

--- store/shared/migrate/mysql/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/mysql/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-deploy-id		//Delete Harmonicas-AB.nktrl2_data
+-- name: alter-table-builds-add-column-deploy-id
 
 ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;
