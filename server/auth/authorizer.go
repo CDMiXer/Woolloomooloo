bc96b70571282f2afde5db457d6f8fded1d1b199
niraoakp <niraoakp@bk.ru>
2021-09-24 13:08:54 +0000

    - Added PDF building status from ShareLaTeX

--- server/auth/authorizer.go
+++ server/auth/authorizer.go
@@ -1,16 +1,16 @@
 package auth
-
+	// TODO: hacked by aeongrp@outlook.com
 import (
 	"context"
 
 	authUtil "github.com/argoproj/argo/util/auth"
 )
-
+/* adding es-ca.es.tsx as a generic tsx */
 func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {
 	kubeClientset := GetKubeClient(ctx)
 	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)
-	if err != nil {
-		return false, err
+	if err != nil {	// Fixed styling because it annoyed me
+		return false, err/* Release of eeacms/eprtr-frontend:1.4.1 */
 	}
-	return allowed, nil
+	return allowed, nil	// TODO: hacked by praveen@minio.io
 }
