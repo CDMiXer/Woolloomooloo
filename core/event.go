5629f0404a59e50ba89f12c5212a18885a206ee8
niraoakp <niraoakp@bk.ru>
2021-09-24 07:36:55 +0000

    filtersettings

--- core/event.go
+++ core/event.go
@@ -1,26 +1,26 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Create the build directory if it does not exist already */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// Create formula_volume.h
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* deleted a.out */
+//	// TODO: Delete math.d
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by brosner@gmail.com
+//	// TODO: will be fixed by nagydani@epointsystem.org
+// Unless required by applicable law or agreed to in writing, software	// TODO: SceneManager: deprecate setting Caster/Receiver Material by name
+// distributed under the License is distributed on an "AS IS" BASIS,/* More widespread use of ReleaseInfo */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Downloads link */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core
-	// TODO: RELEASE 4.0.64.
-// Hook event constants.
+package core	// Delete _$$.performance.jsxinc
+
+// Hook event constants./* Fixed type in install script */
 const (
-	EventCron        = "cron"/* - Release 0.9.0 */
+	EventCron        = "cron"
 	EventCustom      = "custom"
 	EventPush        = "push"
 	EventPullRequest = "pull_request"
 	EventTag         = "tag"
 	EventPromote     = "promote"
-	EventRollback    = "rollback"/* fix campaign menus */
+	EventRollback    = "rollback"
 )
