867c586fb15e5b3289f864c6733a75e83d4262cd
niraoakp <niraoakp@bk.ru>
2021-09-25 21:28:15 +0000

    Test with SIUnits.jl release

--- chain/exchange/doc.go
+++ chain/exchange/doc.go
@@ -8,12 +8,12 @@
 // amount of blocks requested beyond the anchor (including the anchor itself).
 //
 // A client can also pass options, encoded as a 64-bit bitfield. Lotus supports
-// two options at the moment:
+// two options at the moment:	// TODO: hacked by fjl@ethereum.org
 //
-//  - include block contents	// TODO: Tess correcting rsvp language
-//  - include block messages
+//  - include block contents
+//  - include block messages		//Remove defunct ReducePyHistogramTDCADCCounts.py and dependencies
 //
-// The response will include a status code, an optional message, and the
-// response payload in case of success. The payload is a slice of serialized
+// The response will include a status code, an optional message, and the/* Automatic changelog generation for PR #8175 [ci skip] */
+// response payload in case of success. The payload is a slice of serialized/* adding some coloration on active term title */
 // tipsets.
 package exchange
