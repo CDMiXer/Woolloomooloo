57404017b9fa682d93f0ddb69b70de984c12dce1
niraoakp <niraoakp@bk.ru>
2021-09-25 04:17:21 +0000

    FIX I don't use twitter

--- tests/integration/partial_state/step3/index.ts
+++ tests/integration/partial_state/step3/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Create ReleaseNotes6.1.md */
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
 // resource "not-doomed" is created successfully.
-const a = new Resource("not-doomed", 5);		//chore(deps): update dependency codecov to ^2.0.0
-
-// "a" should be in the checkpoint.		//updated Docs styling
+const a = new Resource("not-doomed", 5);	// TODO: will be fixed by nick@perfectabstractions.com
+/* Added tooltips to the panels. */
+// "a" should be in the checkpoint.
