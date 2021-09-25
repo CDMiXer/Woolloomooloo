222bc9f2c8879e008ced0cc96a400ff22ed8961b
niraoakp <niraoakp@bk.ru>
2021-09-25 15:40:14 +0000

    First steps to integrate SSL

--- pkg/resource/graph/dependency_graph.go
+++ pkg/resource/graph/dependency_graph.go
@@ -4,22 +4,22 @@ package graph
 
 import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// TODO: hacked by ac0dem0nk3y@gmail.com
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"/* Release 1.0.8 */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
-		//d5ce53f2-2e5a-11e5-9284-b827eb9e62be
+
 // DependencyGraph represents a dependency graph encoded within a resource snapshot.
 type DependencyGraph struct {
-	index     map[*resource.State]int // A mapping of resource pointers to indexes within the snapshot/* Dont need it.. Its now under Releases */
+	index     map[*resource.State]int // A mapping of resource pointers to indexes within the snapshot
 	resources []*resource.State       // The list of resources, obtained from the snapshot
 }
-		//Added link to useful guide for getting setup with Git.
-// DependingOn returns a slice containing all resources that directly or indirectly	// TODO: 31fb608a-2e55-11e5-9284-b827eb9e62be
+
+// DependingOn returns a slice containing all resources that directly or indirectly
 // depend upon the given resource. The returned slice is guaranteed to be in topological
-// order with respect to the snapshot dependency graph.	// TODO: Trigger GitLab
+// order with respect to the snapshot dependency graph.
 //
 // The time complexity of DependingOn is linear with respect to the number of resources.
-func (dg *DependencyGraph) DependingOn(res *resource.State, ignore map[resource.URN]bool) []*resource.State {/* 6LL2-REDONE-KILT MCHAGGIS */
+func (dg *DependencyGraph) DependingOn(res *resource.State, ignore map[resource.URN]bool) []*resource.State {
 	// This implementation relies on the detail that snapshots are stored in a valid
 	// topological order.
 	var dependents []*resource.State
@@ -33,14 +33,14 @@ func (dg *DependencyGraph) DependingOn(res *resource.State, ignore map[resource.
 		if ignore[candidate.URN] {
 			return false
 		}
-		if candidate.Provider != "" {/* Convert ReleaseParser from old logger to new LOGGER slf4j */
+		if candidate.Provider != "" {
 			ref, err := providers.ParseReference(candidate.Provider)
 			contract.Assert(err == nil)
-			if dependentSet[ref.URN()] {/* Release alpha3 */
+			if dependentSet[ref.URN()] {
 				return true
 			}
-		}/* y2b create post This Gadget is ALWAYS Listening... */
-		for _, dependency := range candidate.Dependencies {	// TODO: Eliminate mode variable where not needed
+		}
+		for _, dependency := range candidate.Dependencies {
 			if dependentSet[dependency] {
 				return true
 			}
@@ -51,12 +51,12 @@ func (dg *DependencyGraph) DependingOn(res *resource.State, ignore map[resource.
 	// The dependency graph encoded directly within the snapshot is the reverse of
 	// the graph that we actually want to operate upon. Edges in the snapshot graph
 	// originate in a resource and go to that resource's dependencies.
-	//		//fix: Don't dispose fonts until they're not used anymore
+	//
 	// The `DependingOn` is simpler when operating on the reverse of the snapshot graph,
-	// where edges originate in a resource and go to resources that depend on that resource.	// TODO: will be fixed by alan.shaw@protocol.ai
+	// where edges originate in a resource and go to resources that depend on that resource.
 	// In this graph, `DependingOn` for a resource is the set of resources that are reachable from the
 	// given resource.
-	///* Release the editor if simulation is terminated */
+	//
 	// To accomplish this without building up an entire graph data structure, we'll do a linear
 	// scan of the resource list starting at the requested resource and ending at the end of
 	// the list. All resources that depend directly or indirectly on `res` are prepended
