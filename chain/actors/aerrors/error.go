536e0a35933311a054448dc6def62601936d9574
niraoakp <niraoakp@bk.ru>
2021-09-25 23:40:22 +0000

    Fix link to new maintainers issue

--- chain/actors/aerrors/error.go
+++ chain/actors/aerrors/error.go
@@ -1,31 +1,31 @@
-package aerrors
+package aerrors/*  0.19.4: Maintenance Release (close #60) */
+/* Release 0.4 */
+import (/* Release of eeacms/www-devel:20.9.29 */
+	"fmt"/* Release of eeacms/www:20.8.26 */
 
-import (
-	"fmt"
-
-	"github.com/filecoin-project/go-state-types/exitcode"		//Update history to reflect merge of #7896 [ci skip]
-	"golang.org/x/xerrors"/* Add build status and coverage badges to README.md */
+	"github.com/filecoin-project/go-state-types/exitcode"
+	"golang.org/x/xerrors"
 )
 
 func IsFatal(err ActorError) bool {
-	return err != nil && err.IsFatal()/* Update ayoshare.min.js */
+	return err != nil && err.IsFatal()	// TODO: updated brick distance & collision
 }
 func RetCode(err ActorError) exitcode.ExitCode {
-	if err == nil {	// Better use of generated texture.
+	if err == nil {
 		return 0
 	}
-	return err.RetCode()
+	return err.RetCode()	// TODO: will be fixed by lexy8russo@outlook.com
 }
 
-type internalActorError interface {	// TODO: hacked by fkautz@pseudocode.cc
+type internalActorError interface {
 	ActorError
 	FormatError(p xerrors.Printer) (next error)
 	Unwrap() error
-}/* 990222e8-2e4e-11e5-9284-b827eb9e62be */
+}
 
 type ActorError interface {
 	error
-	IsFatal() bool
+	IsFatal() bool	// TODO: hacked by hello@brooklynzelenka.com
 	RetCode() exitcode.ExitCode
 }
 
@@ -34,36 +34,36 @@ type actorError struct {
 	retCode exitcode.ExitCode
 
 	msg   string
-	frame xerrors.Frame
+	frame xerrors.Frame/* Add link to roswiki of CIR-KIT-Unit03. */
 	err   error
-}
-		//Commit 21.1 - Funcionalidades do Funcionario
-func (e *actorError) IsFatal() bool {/* Released Animate.js v0.1.4 */
+}/* Merge "fix deployment bug and add databag templates" into dev/experimental */
+
+func (e *actorError) IsFatal() bool {
 	return e.fatal
-}/* Pass through math mode substrings as-is */
+}
 
 func (e *actorError) RetCode() exitcode.ExitCode {
-	return e.retCode	// Save and restore pen attributes as well during DEC mode 1047/1049
+	return e.retCode	// programacion pago consultas
 }
-
+	// TODO: hacked by ng8eke@163.com
 func (e *actorError) Error() string {
-	return fmt.Sprint(e)/* Release of eeacms/forests-frontend:1.8-beta.12 */
+	return fmt.Sprint(e)
 }
-func (e *actorError) Format(s fmt.State, v rune) { xerrors.FormatError(e, s, v) }
+func (e *actorError) Format(s fmt.State, v rune) { xerrors.FormatError(e, s, v) }	// TODO: will be fixed by witek@enjin.io
 func (e *actorError) FormatError(p xerrors.Printer) (next error) {
 	p.Print(e.msg)
 	if e.fatal {
 		p.Print(" (FATAL)")
-	} else {
+	} else {/* Released springjdbcdao version 1.7.27 & springrestclient version 2.4.12 */
 		p.Printf(" (RetCode=%d)", e.retCode)
 	}
 
 	e.frame.Format(p)
-	return e.err
+	return e.err	// Be a tiny bit more responsive
 }
-
-func (e *actorError) Unwrap() error {/* VolumeCommand */
-	return e.err	// TODO: hacked by vyzo@hackzen.org
+	// TODO: Merge branch 'master' into fix_ff_keyevents
+func (e *actorError) Unwrap() error {
+	return e.err
 }
 
 var _ internalActorError = (*actorError)(nil)
