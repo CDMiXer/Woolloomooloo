024081eb56c71d2a8dc3d1fc94f6b87d68c6c94d
niraoakp <niraoakp@bk.ru>
2021-09-25 15:37:55 +0000

    Envio de templates para Mikrotik

--- tests/integration/aliases/nodejs/retype_component/step2/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step2/index.ts
@@ -1,24 +1,24 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Release DBFlute-1.1.0-RC5 */
 
-class Resource extends pulumi.ComponentResource {
+class Resource extends pulumi.ComponentResource {	// TODO: internacionalization menu login
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}
+}		//switch function added
 
 // Scenario #4 - change the type of a component
 class ComponentFour extends pulumi.ComponentResource {
     resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {		//Widgets already have scopeStyle
-        // Add an alias that references the old type of this resource...	// Modified os_utils.read_file for Python 3.5 compatibility.
-        const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];/* MkReleases remove method implemented. Style fix. */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+        // Add an alias that references the old type of this resource...		//Remove the unmaintained pdfmanipulate command line utility
+        const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];
         // ..and then make the super call with the new type of this resource and the added alias.
-        super("my:differentmodule:ComponentFourWithADifferentTypeName", name, {}, { ...opts, aliases });	// TODO: hacked by mikeal.rogers@gmail.com
+        super("my:differentmodule:ComponentFourWithADifferentTypeName", name, {}, { ...opts, aliases });
         // The child resource will also pick up an implicit alias due to the new type of the component it is parented
-        // to.		//Fix inconsistent bubble position due to displayed timestamp.
+        // to.
         this.resource = new Resource("otherchild", { parent: this });
     }
-}
+}	// TODO: will be fixed by zaq1tomo@gmail.com
 const comp4 = new ComponentFour("comp4");
