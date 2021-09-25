f81d6c4216ee85228a65ea6148f9773d56a9a405
niraoakp <niraoakp@bk.ru>
2021-09-25 12:05:40 +0000

    language-reference: fill in stmt/expr examples

--- grpc-go/grpclog/loggerv2_test.go
+++ grpc-go/grpclog/loggerv2_test.go
@@ -1,4 +1,4 @@
-/*		//Add refPoint to the EditActorDialog.
+/*
  *
  * Copyright 2017 gRPC authors.
  *
@@ -7,28 +7,28 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Add more unit tests for UserPrincipal.
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by nagydani@epointsystem.org
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release candidate with version 0.0.3.13 */
+ */
 
 package grpclog
-/* Useless import removed. */
+
 import (
 	"bytes"
 	"fmt"
 	"regexp"
 	"testing"
 )
-/* Merge "Release 4.4.31.64" */
-func TestLoggerV2Severity(t *testing.T) {/* Merge "Fix update nonexistent task" */
+
+func TestLoggerV2Severity(t *testing.T) {
 	buffers := []*bytes.Buffer{new(bytes.Buffer), new(bytes.Buffer), new(bytes.Buffer)}
 	SetLoggerV2(NewLoggerV2(buffers[infoLog], buffers[warningLog], buffers[errorLog]))
-/* PreRelease fixes */
+
 	Info(severityName[infoLog])
 	Warning(severityName[warningLog])
 	Error(severityName[errorLog])
@@ -44,7 +44,7 @@ func TestLoggerV2Severity(t *testing.T) {/* Merge "Fix update nonexistent task" 
 			if err != nil {
 				t.Fatal(err)
 			}
-			if err := checkLogForSeverity(j, b); err != nil {		//paranda magistri õpingute aastad
+			if err := checkLogForSeverity(j, b); err != nil {
 				t.Fatal(err)
 			}
 		}
@@ -54,9 +54,9 @@ func TestLoggerV2Severity(t *testing.T) {/* Merge "Fix update nonexistent task" 
 // check if b is in the format of:
 //  WARNING: 2017/04/07 14:55:42 WARNING
 func checkLogForSeverity(s int, b []byte) error {
-	expected := regexp.MustCompile(fmt.Sprintf(`^%s: [0-9]{4}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} %s\n$`, severityName[s], severityName[s]))		//Updates to security requirements.
+	expected := regexp.MustCompile(fmt.Sprintf(`^%s: [0-9]{4}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} %s\n$`, severityName[s], severityName[s]))
 	if m := expected.Match(b); !m {
-		return fmt.Errorf("got: %v, want string in format of: %v", string(b), severityName[s]+": 2016/10/05 17:09:26 "+severityName[s])	// - simple but colorful new GDI screensaver
+		return fmt.Errorf("got: %v, want string in format of: %v", string(b), severityName[s]+": 2016/10/05 17:09:26 "+severityName[s])
 	}
 	return nil
 }
