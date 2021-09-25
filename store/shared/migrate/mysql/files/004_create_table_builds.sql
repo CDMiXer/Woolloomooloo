e99670c1fc5eca9e0345269499561c70bbe5c2a4
niraoakp <niraoakp@bk.ru>
2021-09-25 09:40:17 +0000

    Added commands to configure more settings for existing sites.

--- store/shared/migrate/mysql/files/004_create_table_builds.sql
+++ store/shared/migrate/mysql/files/004_create_table_builds.sql
@@ -1,7 +1,7 @@
 -- name: create-table-builds
 
 CREATE TABLE IF NOT EXISTS builds (
- build_id            INTEGER PRIMARY KEY AUTO_INCREMENT
+ build_id            INTEGER PRIMARY KEY AUTO_INCREMENT	// TODO: will be fixed by 13860583249@yeah.net
 ,build_repo_id       INTEGER
 ,build_config_id     INTEGER
 ,build_trigger       VARCHAR(250)
@@ -12,8 +12,8 @@ CREATE TABLE IF NOT EXISTS builds (
 ,build_event         VARCHAR(50)
 ,build_action        VARCHAR(50)
 ,build_link          VARCHAR(1000)
-,build_timestamp     INTEGER
-,build_title         VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+,build_timestamp     INTEGER	// TODO: add base for surveys local triggered component
+,build_title         VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci/* 33f5ed0a-2e74-11e5-9284-b827eb9e62be */
 ,build_message       VARCHAR(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ,build_before        VARCHAR(50)
 ,build_after         VARCHAR(50)
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS builds (
 ,build_deploy        VARCHAR(500)
 ,build_params        VARCHAR(2000)
 ,build_started       INTEGER
-,build_finished      INTEGER
+,build_finished      INTEGER		//Moved Correlators to gdsc.core
 ,build_created       INTEGER
 ,build_updated       INTEGER
 ,build_version       INTEGER
@@ -40,15 +40,15 @@ CREATE TABLE IF NOT EXISTS builds (
 -- name: create-index-builds-repo
 
 CREATE INDEX ix_build_repo ON builds (build_repo_id);
-
+/* Release of eeacms/www:20.1.10 */
 -- name: create-index-builds-author
 
 CREATE INDEX ix_build_author ON builds (build_author);
-
+		//:bow::crying_cat_face: Updated in browser at strd6.github.io/editor
 -- name: create-index-builds-sender
 
 CREATE INDEX ix_build_sender ON builds (build_sender);
 
 -- name: create-index-builds-ref
-
+	// Two old versions
 CREATE INDEX ix_build_ref ON builds (build_repo_id, build_ref);
