1ca41481c6d8aa4e102195a2b31ec84ca817e81f
niraoakp <niraoakp@bk.ru>
2021-09-25 19:52:05 +0000

    Update main_raw.cpp

--- tests/integration/empty/gorun_main/gorun_main_src/main.go
+++ tests/integration/empty/gorun_main/gorun_main_src/main.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// TODO: Create cambiaip.cmd
 package main
-	// TODO: hacked by hugomrdias@gmail.com
+
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-func main() {/* Rename the class to match file name */
-	pulumi.Run(func(ctx *pulumi.Context) error {/* Release of jQAssistant 1.6.0 */
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		return nil
 	})
 }
