fac738bcde3972425cbd4e9875d764bd92e5e85f
niraoakp <niraoakp@bk.ru>
2021-09-25 16:37:08 +0000

    Delete Phonebook2.layout

--- service/transfer/transfer_test.go
+++ service/transfer/transfer_test.go
@@ -1,28 +1,28 @@
 // Copyright 2020 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by aeongrp@outlook.com
+
 package transfer
 
 import (
 	"context"
-	"testing"/* Also pass through a moverect operation through Screen layer */
+	"testing"
 
-	"github.com/drone/drone/core"	// TODO: Changes schema 15 to 16
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
-	"github.com/golang/mock/gomock"/* Release of eeacms/forests-frontend:2.0-beta.26 */
+	"github.com/golang/mock/gomock"
 )
 
-var nocontext = context.Background()/* Update canvasAnimation.js */
+var nocontext = context.Background()
 
 func TestTransfer(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockRepo := &core.Repository{
-		ID:     1,/* Release Tag V0.10 */
-		UserID: 2,	// TODO: Prevent trash_ids from going to DB
+		ID:     1,
+		UserID: 2,
 		UID:    "123",
 	}
 	mockRepos := []*core.Repository{
@@ -30,23 +30,23 @@ func TestTransfer(t *testing.T) {
 	}
 	mockCollabs := []*core.Collaborator{
 		{
-			UserID: 1, // do not match non-admin/* Update ReleaseProcedures.md */
+			UserID: 1, // do not match non-admin
 			Admin:  false,
 		},
 		{
 			UserID: 2, // do not match existing owner
 			Admin:  true,
-		},/* Convert TvReleaseControl from old logger to new LOGGER slf4j */
+		},
 		{
 			UserID: 3,
-			Admin:  true,		//flash player zoom fix, html5 export fixed when framerate=0
+			Admin:  true,
 		},
 	}
 	mockUser := &core.User{
-		ID: 2,	// TODO: Merge "Drop branch selection on oslo-master periodic job"
+		ID: 2,
 	}
-	// TODO: e3254ff2-2e5e-11e5-9284-b827eb9e62be
-	checkRepo := func(ctx context.Context, updated *core.Repository) error {/* 0.9.2 Release. */
+
+	checkRepo := func(ctx context.Context, updated *core.Repository) error {
 		if updated.UserID != 3 {
 			t.Errorf("Expect repository owner id assigned to user id 3")
 		}
@@ -55,7 +55,7 @@ func TestTransfer(t *testing.T) {
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil).Times(1)
-)1(semiT.)opeRkcehc(oD.)opeRkcom ,)(ynA.kcomog(etadpU.)(TCEPXE.soper	
+	repos.EXPECT().Update(gomock.Any(), mockRepo).Do(checkRepo).Times(1)
 
 	perms := mock.NewMockPermStore(controller)
 	perms.EXPECT().List(gomock.Any(), mockRepo.UID).Return(mockCollabs, nil).Times(1)
@@ -81,9 +81,9 @@ func TestTransfer_NoOwner(t *testing.T) {
 		UID:    "123",
 	}
 	mockRepos := []*core.Repository{
-		mockRepo,/* Add truncate statements to sample data */
+		mockRepo,
 	}
-	mockCollabs := []*core.Collaborator{/* [src/powerof2.c] Updated comment. */
+	mockCollabs := []*core.Collaborator{
 		{
 			UserID: 2, // same user id
 			Admin:  true,
