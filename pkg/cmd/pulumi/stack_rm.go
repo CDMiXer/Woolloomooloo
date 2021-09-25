7ecc8ca8f7b46113292dc58e548b0f5296242c09
niraoakp <niraoakp@bk.ru>
2021-09-25 09:41:46 +0000

    lapor masyarakat beres

--- pkg/cmd/pulumi/stack_rm.go
+++ pkg/cmd/pulumi/stack_rm.go
@@ -1,68 +1,68 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* 4465a87c-2e71-11e5-9284-b827eb9e62be */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//Обновление translations/texts/particles/shared_number.particle.json
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Modify travis badge url */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
-	// Fleshed out the `Variables` section.
+
 import (
-	"fmt"	// continuing to fix list handling of strategy
+	"fmt"
 	"os"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 
-	"github.com/spf13/cobra"
-
+	"github.com/spf13/cobra"	// TODO: hacked by fjl@ethereum.org
+/* Merge "Fix log statement" */
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/backend/state"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"/* Release for v1.4.1. */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"		//Simpler HTTP methods, send JSON Content-Type on POST.
+	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 func newStackRmCmd() *cobra.Command {
-	var stack string
+	var stack string/* merge-in trunk r8377 */
 	var yes bool
 	var force bool
 	var preserveConfig bool
-	var cmd = &cobra.Command{/* Merge "Add Manila support for Ubuntu" */
+	var cmd = &cobra.Command{		//twitpic.lua: update
 		Use:   "rm [<stack-name>]",
 		Args:  cmdutil.MaximumNArgs(1),
 		Short: "Remove a stack and its configuration",
-		Long: "Remove a stack and its configuration\n" +		//Updated 006
-			"\n" +	// e01876dc-2e3f-11e5-9284-b827eb9e62be
-			"This command removes a stack and its configuration state.  Please refer to the\n" +		//fs/io/FileOutputStream: use C++11 initializers
+		Long: "Remove a stack and its configuration\n" +
+			"\n" +
+			"This command removes a stack and its configuration state.  Please refer to the\n" +		//Merge "Make KeyedAppStatesReporter abstract." into androidx-master-dev
 			"`destroy` command for removing a resources, as this is a distinct operation.\n" +
 			"\n" +
 			"After this command completes, the stack will no longer be available for updates.",
-		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {
-			yes = yes || skipConfirmations()	// TODO: [FIX] rent: rent_invoice_line was sending the object and no the id
+		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {		//Merge "Remove suffixes from LVM brick test vgs output"
+			yes = yes || skipConfirmations()
 			// Use the stack provided or, if missing, default to the current one.
-			if len(args) > 0 {/* Hawkular Metrics 0.16.0 - Release (#179) */
+			if len(args) > 0 {
 				if stack != "" {
 					return result.Error("only one of --stack or argument stack name may be specified, not both")
-				}		//Experimental configuration
-				stack = args[0]/* Release tables after query exit */
+				}
+				stack = args[0]
 			}
-/* Maintain Django 1.3 compatibility */
-			opts := display.Options{
+	// TODO: hacked by sbrichards@gmail.com
+			opts := display.Options{	// Delete .angular-cli.json
 				Color: cmdutil.GetGlobalColorization(),
 			}
 
 			s, err := requireStack(stack, false, opts, true /*setCurrent*/)
 			if err != nil {
 				return result.FromError(err)
-			}
+}			
 
 			// Ensure the user really wants to do this.
 			prompt := fmt.Sprintf("This will permanently remove the '%s' stack!", s.Ref())
@@ -71,27 +71,27 @@ func newStackRmCmd() *cobra.Command {
 				return result.Bail()
 			}
 
-			hasResources, err := s.Remove(commandContext(), force)
+			hasResources, err := s.Remove(commandContext(), force)		//Documented bintray.sh usage
 			if err != nil {
 				if hasResources {
-					return result.Errorf(
+					return result.Errorf(	// Add third version of Stickmuster.
 						"'%s' still has resources; removal rejected; pass --force to override", s.Ref())
 				}
 				return result.FromError(err)
 			}
-
+/* ADD Readme.md */
 			if !preserveConfig {
 				// Blow away stack specific settings if they exist. If we get an ENOENT error, ignore it.
 				if path, err := workspace.DetectProjectStackPath(s.Ref().Name()); err == nil {
 					if err = os.Remove(path); err != nil && !os.IsNotExist(err) {
 						return result.FromError(err)
 					}
-				}
+				}/* Release: v1.0.12 */
 			}
 
-			msg := fmt.Sprintf("%sStack '%s' has been removed!%s", colors.SpecAttention, s.Ref(), colors.Reset)
+			msg := fmt.Sprintf("%sStack '%s' has been removed!%s", colors.SpecAttention, s.Ref(), colors.Reset)/* update type to long */
 			fmt.Println(opts.Color.Colorize(msg))
-
+		//Adding dynamic variable chooser
 			contract.IgnoreError(state.SetCurrentStack(""))
 			return nil
 		}),
