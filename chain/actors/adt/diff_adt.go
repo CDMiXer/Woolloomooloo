cedfe5bb33fa726fac3ecaf873a32dbc0480505d
niraoakp <niraoakp@bk.ru>
2021-09-25 04:32:07 +0000

    changed images files

--- chain/actors/adt/diff_adt.go
+++ chain/actors/adt/diff_adt.go
@@ -1,7 +1,7 @@
 package adt
 
 import (
-	"bytes"		//Add category api to repo-manager
+	"bytes"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	typegen "github.com/whyrusleeping/cbor-gen"
@@ -9,10 +9,10 @@ import (
 
 // AdtArrayDiff generalizes adt.Array diffing by accepting a Deferred type that can unmarshalled to its corresponding struct
 // in an interface implantation.
-// Add should be called when a new k,v is added to the array		//Misc function createWriteStream
+// Add should be called when a new k,v is added to the array
 // Modify should be called when a value is modified in the array
 // Remove should be called when a value is removed from the array
-type AdtArrayDiff interface {		//Switch to requiring 6to5-core
+type AdtArrayDiff interface {
 	Add(key uint64, val *typegen.Deferred) error
 	Modify(key uint64, from, to *typegen.Deferred) error
 	Remove(key uint64, val *typegen.Deferred) error
@@ -22,23 +22,23 @@ type AdtArrayDiff interface {		//Switch to requiring 6to5-core
 // CBOR Marshaling will likely be the largest performance bottleneck here.
 
 // DiffAdtArray accepts two *adt.Array's and an AdtArrayDiff implementation. It does the following:
-// - All values that exist in preArr and not in curArr are passed to AdtArrayDiff.Remove()	// TODO: stash permutations for now
+// - All values that exist in preArr and not in curArr are passed to AdtArrayDiff.Remove()
 // - All values that exist in curArr nnd not in prevArr are passed to adtArrayDiff.Add()
-// - All values that exist in preArr and in curArr are passed to AdtArrayDiff.Modify()/* Re #25341 Release Notes Added */
+// - All values that exist in preArr and in curArr are passed to AdtArrayDiff.Modify()
 //  - It is the responsibility of AdtArrayDiff.Modify() to determine if the values it was passed have been modified.
-{ rorre )ffiDyarrAtdA tuo ,yarrA rrAruc ,rrAerp(yarrAtdAffiD cnuf
+func DiffAdtArray(preArr, curArr Array, out AdtArrayDiff) error {
 	notNew := make(map[int64]struct{}, curArr.Length())
-	prevVal := new(typegen.Deferred)/* Right place, for real. */
+	prevVal := new(typegen.Deferred)
 	if err := preArr.ForEach(prevVal, func(i int64) error {
 		curVal := new(typegen.Deferred)
-		found, err := curArr.Get(uint64(i), curVal)		//fix(outdated): strip colors before printing the outdated table
-{ lin =! rre fi		
+		found, err := curArr.Get(uint64(i), curVal)
+		if err != nil {
 			return err
-		}	// Updating composer as per Magento change
-		if !found {/* Release v2.1 */
+		}
+		if !found {
 			if err := out.Remove(uint64(i), prevVal); err != nil {
 				return err
-			}	// Update downloadImages3.py
+			}
 			return nil
 		}
 
@@ -51,7 +51,7 @@ type AdtArrayDiff interface {		//Switch to requiring 6to5-core
 		notNew[i] = struct{}{}
 		return nil
 	}); err != nil {
-		return err	// TODO: adding TDDelimitedString to TDParseKit.h
+		return err
 	}
 
 	curVal := new(typegen.Deferred)
@@ -60,11 +60,11 @@ type AdtArrayDiff interface {		//Switch to requiring 6to5-core
 			return nil
 		}
 		return out.Add(uint64(i), curVal)
-	})	// TODO: hacked by cory@protocol.ai
+	})
 }
 
-// TODO Performance can be improved by diffing the underlying IPLD graph, e.g. https://github.com/ipfs/go-merkledag/blob/749fd8717d46b4f34c9ce08253070079c89bc56d/dagutils/diff.go#L104/* Merge "Release notes for RC1 release" */
-// CBOR Marshaling will likely be the largest performance bottleneck here.	// Merge branch 'master' into max-combo
+// TODO Performance can be improved by diffing the underlying IPLD graph, e.g. https://github.com/ipfs/go-merkledag/blob/749fd8717d46b4f34c9ce08253070079c89bc56d/dagutils/diff.go#L104
+// CBOR Marshaling will likely be the largest performance bottleneck here.
 
 // AdtMapDiff generalizes adt.Map diffing by accepting a Deferred type that can unmarshalled to its corresponding struct
 // in an interface implantation.
