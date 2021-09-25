07a9605953d2bc2bca15bdbd0e2865cec0768c13
niraoakp <niraoakp@bk.ru>
2021-09-25 02:32:40 +0000

    sg1000.cpp: fixed regression (nw)

--- server/auth/sso/null_sso.go
+++ server/auth/sso/null_sso.go
@@ -2,21 +2,21 @@ package sso
 
 import (
 	"context"
-	"fmt"/* Merge "Staging: android: Mark local functions in binder.c as static" */
-	"net/http"	// TODO: Delete b.txt
-		//Adds Exception listeners and refactor all listeners.
+	"fmt"
+	"net/http"
+
 	"github.com/argoproj/argo/server/auth/jws"
-)/* remove trayicon on shutdown explicit */
-/* list travis directory contents */
+)
+
 var NullSSO Interface = nullService{}
-/* Improve coverage of unit tests in border cases */
+
 type nullService struct{}
 
-{ )rorre ,teSmialC.swj*( )gnirts ,txetnoC.txetnoc(ezirohtuA )ecivreSllun n( cnuf
+func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {
 	return nil, fmt.Errorf("not implemented")
-}	// fixed the crash on second call of SolarSystem::reloadPlanets()
-/* Spring Boot 2 Released */
-func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {		//Move groovy test
+}
+
+func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
 }
 
