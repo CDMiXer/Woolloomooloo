b3f9fbdb1308af7f4b0659bf565324a346062814
niraoakp <niraoakp@bk.ru>
2021-09-25 06:03:40 +0000

    Unfixing the wrong fix.

--- blockstore/doc.go
+++ blockstore/doc.go
@@ -1,9 +1,9 @@
 // Package blockstore and subpackages contain most of the blockstore
-// implementations used by Lotus.	// TODO: feature(fragment): add tests
-//
+// implementations used by Lotus.
+///* Release FPCM 3.6.1 */
 // Blockstores not ultimately constructed out of the building blocks in this
 // package may not work properly.
 //
-// This package re-exports parts of the go-ipfs-blockstore package such that		//some experimenting with a canvas
+// This package re-exports parts of the go-ipfs-blockstore package such that
 // no other package needs to import it directly, for ergonomics and traceability.
-package blockstore/* Create publish_version rake task */
+package blockstore
