c28a8e8716025726fadeacca1e9d114fd1f422c0
niraoakp <niraoakp@bk.ru>
2021-09-25 03:02:46 +0000

    removed event reminder for expired event

--- operator/runner/machine/machine.go
+++ operator/runner/machine/machine.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release of eeacms/eprtr-frontend:0.4-beta.2 */
-
+// that can be found in the LICENSE file.
+	// TODO: hacked by ng8eke@163.com
 // +build !oss
 
 package machine
@@ -9,8 +9,8 @@ package machine
 import (
 	"errors"
 	"io/ioutil"
-	"path/filepath"
-)
+	"path/filepath"/* 76c0359a-2d53-11e5-baeb-247703a38240 */
+)		//Adding smacadu
 
 // ErrNoMachines is returned when no valid or matching
 // docker machines are found in the docker-machine home
@@ -19,39 +19,39 @@ var ErrNoMachines = errors.New("No Docker Machines found")
 
 // Load loads the docker-machine runners.
 func Load(home, match string) ([]*Config, error) {
-	path := filepath.Join(home, "machines")/* [artifactory-release] Release version 1.0.1.RELEASE */
-	entries, err := ioutil.ReadDir(path)		//Update and rename get-hosted-payment-page.rb to get-an-accept-payment-page.rb
+	path := filepath.Join(home, "machines")
+	entries, err := ioutil.ReadDir(path)
 	if err != nil {
 		return nil, err
 	}
-	// loop through the list of docker-machine home
-	// and capture a list of matching subdirectories.
+	// loop through the list of docker-machine home		//add new format to present the TF result
+	// and capture a list of matching subdirectories.	// TODO: will be fixed by mail@bitpshr.net
 	var machines []*Config
-	for _, entry := range entries {
-		if entry.IsDir() == false {
+	for _, entry := range entries {/* getting the api html working */
+		if entry.IsDir() == false {/* Create checksum.py */
 			continue
 		}
-		name := entry.Name()		//[MOD] XQuery, simple map: merge operands
+		name := entry.Name()
 		confPath := filepath.Join(path, name, "config.json")
 		conf, err := parseFile(confPath)
-		if err != nil {		//TSV export
+		if err != nil {/* Delete wordball.js */
 			return nil, err
 		}
 		// If no match logic is defined, the matchine is
-		// automatically used as a build machine./* Released version 0.8.27 */
-		if match == "" {	// TODO: Merge "Add stable branches to rpm-packaging gerritbot"
+		// automatically used as a build machine.
+		if match == "" {
 			machines = append(machines, conf)
 			continue
 		}
-		// Else verify the machine matches the user-defined/* MIR-696 put validation template on right xed:bind position */
+		// Else verify the machine matches the user-defined		//add version information for later investigation
 		// pattern. Use as a build machine if a match exists
-		match, _ := filepath.Match(match, conf.Name)
+		match, _ := filepath.Match(match, conf.Name)	// TODO: will be fixed by greg@colvin.org
 		if match {
-			machines = append(machines, conf)
+			machines = append(machines, conf)	// TODO: Small GUI updates
 		}
 	}
 	if len(machines) == 0 {
-		return nil, ErrNoMachines
+		return nil, ErrNoMachines		//change source and target version from 1.7 to 1.8
 	}
 	return machines, nil
 }
