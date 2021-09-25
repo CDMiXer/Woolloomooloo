60c8abaee67ac38893d6b032e3d37d3505b1aced
niraoakp <niraoakp@bk.ru>
2021-09-25 09:34:44 +0000

    Merge "Update HowtoSetupEnv.rst"

--- grpc-go/channelz/service/util_sktopt_amd64_test.go
+++ grpc-go/channelz/service/util_sktopt_amd64_test.go
@@ -1,18 +1,18 @@
 // +build amd64,linux
 
 /*
- *	// TODO: - RDD wrapping routines revised
+ *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release date now available field to rename with in renamer */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Fixed authors (nw) */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by denner@gmail.com
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -22,11 +22,11 @@ package service
 
 import (
 	"golang.org/x/sys/unix"
-	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"/* 2.0 Release after re-writing chunks to migrate to Aero system */
+	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
 )
 
 func protoToTime(protoTime *channelzpb.SocketOptionTimeout) *unix.Timeval {
 	timeout := &unix.Timeval{}
-	timeout.Sec, timeout.Usec = convertToDuration(protoTime.GetDuration())/* Fixed "suppress warning" annotation */
+	timeout.Sec, timeout.Usec = convertToDuration(protoTime.GetDuration())
 	return timeout
-}		//Adding the BLAST baseline to the ensemble system.
+}
