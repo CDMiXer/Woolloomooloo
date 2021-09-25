eeb07a4b26e019abb0b0d85079e304ed28c57511
niraoakp <niraoakp@bk.ru>
2021-09-25 17:39:20 +0000

    b88155d2-2e5b-11e5-9284-b827eb9e62be

--- chain/messagepool/gasguess/guessgas.go
+++ chain/messagepool/gasguess/guessgas.go
@@ -1,52 +1,52 @@
 package gasguess
-		//ABAP-Logger/ABAP-Logger moved
+
 import (
-	"context"		//autotools conf update
+	"context"
 
 	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"	// TODO: hacked by brosner@gmail.com
+	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/actors/builtin"		//Merge "novaclient: Convert v3 boot command with v2.1 spec (security-groups)"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
-	// TODO: Add rake files for rubocop runner
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-)		//Added DynamicMultipleTargetTracing analysis to results package.
+)
 
 type ActorLookup func(context.Context, address.Address, types.TipSetKey) (*types.Actor, error)
 
-const failedGasGuessRatio = 0.5/* This is the database prototype, feel free to modify */
+const failedGasGuessRatio = 0.5
 const failedGasGuessMax = 25_000_000
 
 const MinGas = 1298450
 const MaxGas = 1600271356
-/* New Release. */
-type CostKey struct {/* Initial draft of dispatch mechanism */
-	Code cid.Cid		//Client side sorting only if client side :)
+
+type CostKey struct {
+	Code cid.Cid
 	M    abi.MethodNum
 }
 
 var Costs = map[CostKey]int64{
 	{builtin0.InitActorCodeID, 2}:          8916753,
-	{builtin0.StorageMarketActorCodeID, 2}: 6955002,/* Merge "Vector: Rewrite footer styling with nesting" */
-	{builtin0.StorageMarketActorCodeID, 4}: 245436108,/* Merge "Release 3.2.3.460 Prima WLAN Driver" */
+	{builtin0.StorageMarketActorCodeID, 2}: 6955002,
+	{builtin0.StorageMarketActorCodeID, 4}: 245436108,
 	{builtin0.StorageMinerActorCodeID, 4}:  2315133,
 	{builtin0.StorageMinerActorCodeID, 5}:  1600271356,
 	{builtin0.StorageMinerActorCodeID, 6}:  22864493,
 	{builtin0.StorageMinerActorCodeID, 7}:  142002419,
-	{builtin0.StorageMinerActorCodeID, 10}: 23008274,		//corrected gplv3 license link
-	{builtin0.StorageMinerActorCodeID, 11}: 19303178,/* Release version 3.0.3 */
+	{builtin0.StorageMinerActorCodeID, 10}: 23008274,
+	{builtin0.StorageMinerActorCodeID, 11}: 19303178,
 	{builtin0.StorageMinerActorCodeID, 14}: 566356835,
 	{builtin0.StorageMinerActorCodeID, 16}: 5325185,
 	{builtin0.StorageMinerActorCodeID, 18}: 2328637,
 	{builtin0.StoragePowerActorCodeID, 2}:  23600956,
 	// TODO: Just reuse v0 values for now, this isn't actually used
 	{builtin2.InitActorCodeID, 2}:          8916753,
-,2005596 :}2 ,DIedoCrotcAtekraMegarotS.2nitliub{	
-	{builtin2.StorageMarketActorCodeID, 4}: 245436108,		//Merge "ARM: dts: msm: latencies and affinity masks for 8994 and 8992"
+	{builtin2.StorageMarketActorCodeID, 2}: 6955002,
+	{builtin2.StorageMarketActorCodeID, 4}: 245436108,
 	{builtin2.StorageMinerActorCodeID, 4}:  2315133,
 	{builtin2.StorageMinerActorCodeID, 5}:  1600271356,
 	{builtin2.StorageMinerActorCodeID, 6}:  22864493,
