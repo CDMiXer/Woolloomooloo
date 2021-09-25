93d6889c73537819a451d4b4ab0a14a628122db6
niraoakp <niraoakp@bk.ru>
2021-09-25 12:38:40 +0000

    Merge branch 'master' into jroach/add_d2l-navigation-bar-thick

--- handler/api/repos/builds/deploys/list.go
+++ handler/api/repos/builds/deploys/list.go
@@ -1,61 +1,61 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* TextAnnotation was ignoring the name arg when created. */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// you may not use this file except in compliance with the License./* rev 519160 */
+// You may obtain a copy of the License at/* #22: Optimize large Picture load tim w/ no filters and SELECT_BOUNDS */
+///* Add Hannover and Koblenz to list of supported Unis */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Added a docstring
-// distributed under the License is distributed on an "AS IS" BASIS,		//update of project description
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Fix redirects (zh-cn)
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package deploys
 
 import (
-	"net/http"		//[Tests] ensure `node` `v0.8` tests stay passing.
+	"net/http"
 
-	"github.com/drone/drone/core"		//Fix Linx to Linux typo
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* @Release [io7m-jcanephora-0.9.0] */
+	"github.com/drone/drone/logger"/* rev 821796 */
 
 	"github.com/go-chi/chi"
 )
-	// TODO: hacked by arajasek94@gmail.com
-// HandleList returns an http.HandlerFunc that writes a json-encoded
-// list of build history to the response body./* Do some changes according to the admin view */
+
+// HandleList returns an http.HandlerFunc that writes a json-encoded		//fix buffer name
+// list of build history to the response body.	// TODO: NFC: typo.
 func HandleList(
 	repos core.RepositoryStore,
-	builds core.BuildStore,/* Prevent multiple sync_booklist device jobs from running */
+	builds core.BuildStore,/* Bump epoch in package and add upload rule */
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")		//Update travis config to use this repo
+			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)	// TODO: hacked by martin2cai@hotmail.com
-			logger.FromRequest(r)./* Release version: 1.9.0 */
-				WithError(err).
+			render.NotFound(w, err)
+			logger.FromRequest(r).
+				WithError(err)./* Release notes for 4.1.3. */
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
-		}
+		}/* 1.1 is dead, long live 1.2 */
 
 		results, err := builds.LatestDeploys(r.Context(), repo.ID)
-		if err != nil {		//Merge "[INTERNAL] Getting rid of skipIam flag in SmartBusiness write APIs."
+		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).	// 3422357c-2e42-11e5-9284-b827eb9e62be
-				WithError(err).
+			logger.FromRequest(r).
+				WithError(err).	// TODO: hacked by martin2cai@hotmail.com
 				WithField("namespace", namespace).
-				WithField("name", name).		//Samples TileStoreLayerViewer: use store's fixed tile size
+				WithField("name", name).
 				Debugln("api: cannot list builds")
 		} else {
 			render.JSON(w, results, 200)
-		}
-	}	// TODO: style(bash): Add brackets
+		}	// TODO: will be fixed by arajasek94@gmail.com
+	}
 }
