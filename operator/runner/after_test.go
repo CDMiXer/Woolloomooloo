d89a11add1e228aaa82ec057448db6e2cfe4f2e7
niraoakp <niraoakp@bk.ru>
2021-09-25 08:34:52 +0000

    Made a little more documentation progress.

--- operator/runner/after_test.go
+++ operator/runner/after_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Create send.pv.lua */
-// that can be found in the LICENSE file.		//Whoops, switched order of file matching before, do it the right way now
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
-package runner
+package runner/* SB-671: testUpdateMetadataOnDeleteReleaseVersionDirectory fixed */
