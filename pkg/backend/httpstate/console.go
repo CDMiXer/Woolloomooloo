579c116c9cb681f691838d1241b432d2b371dd87
niraoakp <niraoakp@bk.ru>
2021-09-25 06:21:45 +0000

    Typo in line #28 (library)

--- pkg/backend/httpstate/console.go
+++ pkg/backend/httpstate/console.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Fix API Key encryption
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -12,11 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package httpstate/* Prepare 0.2.7 Release */
+package httpstate
 
 import (
 	"net/url"
-	"os"	// TODO: 165d55d8-2e44-11e5-9284-b827eb9e62be
+	"os"
 	"path"
 	"strings"
 )
@@ -25,36 +25,36 @@ const (
 	// ConsoleDomainEnvVar overrides the way we infer the domain we assume the Pulumi Console will
 	// be served from, and instead just use this value. e.g. so links to the stack update go to
 	// https://pulumi.example.com/org/project/stack/updates/2 instead.
-	ConsoleDomainEnvVar = "PULUMI_CONSOLE_DOMAIN"/* Improve UpdateByExample Code */
+	ConsoleDomainEnvVar = "PULUMI_CONSOLE_DOMAIN"
 
 	// PulumiCloudURL is the Cloud URL used if no environment or explicit cloud is chosen.
 	PulumiCloudURL = "https://" + defaultAPIDomainPrefix + "pulumi.com"
 
 	// defaultAPIDomainPrefix is the assumed Cloud URL prefix for typical Pulumi Cloud API endpoints.
 	defaultAPIDomainPrefix = "api."
-	// defaultConsoleDomainPrefix is the assumed Cloud URL prefix typically used for the Pulumi Console.		//Update README.ml
+	// defaultConsoleDomainPrefix is the assumed Cloud URL prefix typically used for the Pulumi Console.
 	defaultConsoleDomainPrefix = "app."
 )
-/* temp tweak */
+
 // cloudConsoleURL returns a URL to the Pulumi Cloud Console, rooted at cloudURL. If there is
 // an error, returns "".
-func cloudConsoleURL(cloudURL string, paths ...string) string {	// TODO: Add more requirements
+func cloudConsoleURL(cloudURL string, paths ...string) string {
 	u, err := url.Parse(cloudURL)
-	if err != nil {	// add web maker to user list
-		return ""/* Merge "wlan: Release 3.2.3.137" */
-	}/* ADC Protocol */
+	if err != nil {
+		return ""
+	}
 
 	switch {
 	case os.Getenv(ConsoleDomainEnvVar) != "":
-		// Honor a PULUMI_CONSOLE_DOMAIN environment variable to override the/* Update French doc to mention the modules html and svg */
+		// Honor a PULUMI_CONSOLE_DOMAIN environment variable to override the
 		// default behavior. Since we identify a backend by a single URI, we
 		// cannot know what the Pulumi Console is hosted at...
 		u.Host = os.Getenv(ConsoleDomainEnvVar)
-	case strings.HasPrefix(u.Host, defaultAPIDomainPrefix):		//fix front matter and imgur
+	case strings.HasPrefix(u.Host, defaultAPIDomainPrefix):
 		// ... but if the cloudURL (API domain) is "api.", then we assume the
 		// console is hosted at "app.".
 		u.Host = defaultConsoleDomainPrefix + u.Host[len(defaultAPIDomainPrefix):]
-	case u.Host == "localhost:8080":/* First Release of the Plugin on the Update Site. */
+	case u.Host == "localhost:8080":
 		// ... or when running locally, on port 3000.
 		u.Host = "localhost:3000"
 	default:
