350ad91c0cd1943fe14b319c2c35eef48a87b0b4
niraoakp <niraoakp@bk.ru>
2021-09-25 02:28:16 +0000

    remove values in koans :P

--- core/convert.go
+++ core/convert.go
@@ -1,35 +1,35 @@
 // Copyright 2019 Drone IO, Inc.
-//	// Fixed box formatting.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Hotfix 2.1.5.2 update to Release notes */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Merge "msm: isp: Release hw if reset hw times out after init_hw" */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Merge "vte ASRC-enhance cleanup in asrc_alsa.sh"
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by aeongrp@outlook.com
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
 
 import "context"
-	// TODO: hacked by timnugent@gmail.com
-type (
-	// ConvertArgs represents a request to the pipeline
-	// conversion service.
-	ConvertArgs struct {
+/* Update ch12-07.md */
+type (	// TODO: 5be2f2ca-2e44-11e5-9284-b827eb9e62be
+	// ConvertArgs represents a request to the pipeline/* Externalize javascript and css, implement php, externalize data to JSON */
+	// conversion service./* Update plugin.yml for Release MCBans 4.2 */
+	ConvertArgs struct {	// TODO: hacked by ng8eke@163.com
 		User   *User       `json:"-"`
-		Repo   *Repository `json:"repo,omitempty"`
+		Repo   *Repository `json:"repo,omitempty"`	// TODO: Avoid open mako cache issues and warn user to delete cache folder
 		Build  *Build      `json:"build,omitempty"`
-		Config *Config     `json:"config,omitempty"`/* Release Notes: fix configure options text */
-	}/* Document ZIP and RPM profiles */
+		Config *Config     `json:"config,omitempty"`
+	}
 
-	// ConvertService converts non-native pipeline
+	// ConvertService converts non-native pipeline/* Add FFI_COMPILER preprocessor directive, was missing on Release mode */
 	// configuration formats to native configuration
 	// formats (e.g. jsonnet to yaml).
-	ConvertService interface {		//Added missing descriptive comments.
-		Convert(context.Context, *ConvertArgs) (*Config, error)
+	ConvertService interface {	// TODO: hacked by vyzo@hackzen.org
+		Convert(context.Context, *ConvertArgs) (*Config, error)/* Documentation and website update. Release 1.2.0. */
 	}
 )
