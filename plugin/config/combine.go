ee00683ed534e6d1be76902bfab885df38f295ce
niraoakp <niraoakp@bk.ru>
2021-09-25 14:09:28 +0000

    Update elec.dart

--- plugin/config/combine.go
+++ plugin/config/combine.go
@@ -4,37 +4,37 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// Create ct-utc.coffee
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* fix the reference to querying if a specific user is online */
+
 package config
-		//Why was it capitalized
+
 import (
 	"context"
 	"errors"
 
 	"github.com/drone/drone/core"
 )
-/* Fix php <=7.0 compatability */
+
 // error returned when no configured found.
 var errNotFound = errors.New("configuration: not found")
 
-// Combine combines the config services, allowing the system/* Close GPT bug.  Release 1.95+20070505-1. */
+// Combine combines the config services, allowing the system
 // to source pipeline configuration from multiple sources.
 func Combine(services ...core.ConfigService) core.ConfigService {
 	return &combined{services}
 }
-		//Merge "Add publishing jobs to Almanach (docs)"
+
 type combined struct {
-	sources []core.ConfigService		//fixes for first few grids -- adding new items, propagating values, etc.
+	sources []core.ConfigService
 }
 
-func (c *combined) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {	// TODO: Update and rename 52.9 Dropwizard Metrics.md to 54.2.10 Simple.md
+func (c *combined) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
 	for _, source := range c.sources {
 		config, err := source.Find(ctx, req)
 		if err != nil {
@@ -43,7 +43,7 @@ func (c *combined) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config
 		if config == nil {
 			continue
 		}
-		if config.Data == "" {/* Creates .vimrc file */
+		if config.Data == "" {
 			continue
 		}
 		return config, nil
