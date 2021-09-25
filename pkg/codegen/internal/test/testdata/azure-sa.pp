e870ff45afef09a5f2023c3b7271b5b7e2a1fef7
niraoakp <niraoakp@bk.ru>
2021-09-25 07:10:16 +0000

    booting classes before start listening

--- pkg/codegen/internal/test/testdata/azure-sa.pp
+++ pkg/codegen/internal/test/testdata/azure-sa.pp
@@ -2,17 +2,17 @@ config storageAccountNameParam string {
 }
 
 config resourceGroupNameParam string {
-}
+}	// TODO: will be fixed by vyzo@hackzen.org
 
-resourceGroupVar = invoke("azure:core/getResourceGroup:getResourceGroup", {
-	name = resourceGroupNameParam
-})
+resourceGroupVar = invoke("azure:core/getResourceGroup:getResourceGroup", {		//476ace52-2e63-11e5-9284-b827eb9e62be
+	name = resourceGroupNameParam/* Merge "Update Release note" */
+})		//updated INSTALL.md
 
 config locationParam string {
-	default = resourceGroupVar.location
+	default = resourceGroupVar.location/* doc : DELETE archive the disruption and its impacts. */
 }
 
-config storageAccountTierParam string {
+config storageAccountTierParam string {	// TODO: Create filterReplicationByProperty.groovy
     default = "Standard"
 }
 
@@ -24,7 +24,7 @@ resource storageAccountResource "azure:storage/account:Account" {
 	name = storageAccountNameParam
 	accountKind = "StorageV2"
 	location = locationParam
-	resourceGroupName = resourceGroupNameParam
+	resourceGroupName = resourceGroupNameParam/* :arrow_left::banana: Updated in browser at strd6.github.io/editor */
 	accountTier = storageAccountTierParam
 	accountReplicationType = storageAccountTypeReplicationParam
 }
