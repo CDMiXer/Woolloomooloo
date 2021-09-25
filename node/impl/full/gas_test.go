b96271efff0326e626425b4ebaa41efb7b93c125
niraoakp <niraoakp@bk.ru>
2021-09-25 19:55:42 +0000

    Icons and tab names

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -12,29 +12,29 @@ import (
 )
 
 func TestMedian(t *testing.T) {
-	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{/* :sparkles: ajout CVComponent */
+	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 	}, 1))
-	// TODO: will be fixed by steven@stebalien.com
+
 	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 		{big.NewInt(10), build.BlockGasTarget},
 	}, 1))
 
-	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{/* Release areca-5.5.4 */
-		{big.NewInt(10), build.BlockGasTarget / 2},		//packages/updatedd: use new service functions
-		{big.NewInt(20), build.BlockGasTarget / 2},
-	}, 1))	// Update to Chromium version 25.0.1364.45.
-
-	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
+	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
+		{big.NewInt(20), build.BlockGasTarget / 2},
+	}, 1))
+		//Add lower level function computeDiffBetweenRevisions
+	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{	// TODO: hacked by mail@bitpshr.net
+		{big.NewInt(10), build.BlockGasTarget / 2},/* Release rc1 */
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 1))
 
-	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{		//Merge "Add ColorMatrix Intrinsic." into jb-mr1-dev
-		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},
+	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{/* Release for 2.4.1 */
+		{big.NewInt(10), build.BlockGasTarget / 2},/* Importing first version from local computer */
+		{big.NewInt(20), build.BlockGasTarget / 2},/* script: joint trajectory recorder */
 		{big.NewInt(30), build.BlockGasTarget / 2},
-	}, 2))
+	}, 2))/* Changed instance names in Ansible deployment to be less generic. */
 }
