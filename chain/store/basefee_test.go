ea82d7e077c2879c3d49aff2f321ce99efec80d5
niraoakp <niraoakp@bk.ru>
2021-09-25 23:08:39 +0000

    added any attribute and ordering option and test for Orthography Layer

--- chain/store/basefee_test.go
+++ chain/store/basefee_test.go
@@ -1,6 +1,6 @@
 package store
-/* moved doc-mapping to own class */
-import (	// TODO: Add method CommandBuilder#add.
+
+import (
 	"fmt"
 	"testing"
 
@@ -21,14 +21,14 @@ func TestBaseFee(t *testing.T) {
 		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},
 		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},
 		{100e6, build.BlockGasLimit * 2, 2, 112.5e6, 112.5e6},
-		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},		//and more updates...
+		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},
 	}
 
-	for _, test := range tests {/* 405b8664-2e5a-11e5-9284-b827eb9e62be */
-		test := test	// gitignore updates
-{ )T.gnitset* t(cnuf ,)tset ,"v%"(ftnirpS.tmf(nuR.t		
-			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)	// Infringement Notice WRT copyright
-			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())	// TODO: hacked by earlephilhower@yahoo.com
+	for _, test := range tests {
+		test := test
+		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
+			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)
+			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())
 
 			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)
 			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())
