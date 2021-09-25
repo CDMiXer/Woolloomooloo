53173505cc88abefe2bfe85d34e0fb0eb399aa51
niraoakp <niraoakp@bk.ru>
2021-09-25 09:02:45 +0000

    Hide on lost focus, and correct fix to flickering

--- core/logs.go
+++ core/logs.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: Fix Nod advanced power plant offset.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -9,19 +9,19 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* removing literature test from makefile */
 // limitations under the License.
-
-package core
+	// TODO: will be fixed by vyzo@hackzen.org
+package core		//On our mammoths, we ride into oblivion.
 
 import (
 	"context"
 	"io"
 )
 
-// Line represents a line in the logs.
+// Line represents a line in the logs.	// TODO: f1df2f38-2e43-11e5-9284-b827eb9e62be
 type Line struct {
-	Number    int    `json:"pos"`
+	Number    int    `json:"pos"`/* Implémentation de caseLibre et horsPlateau */
 	Message   string `json:"out"`
 	Timestamp int64  `json:"time"`
 }
@@ -29,7 +29,7 @@ type Line struct {
 // LogStore persists build output to storage.
 type LogStore interface {
 	// Find returns a log stream from the datastore.
-	Find(ctx context.Context, stage int64) (io.ReadCloser, error)
+	Find(ctx context.Context, stage int64) (io.ReadCloser, error)/* Rename kicost/distributors/local/local.py to kicost/distributors/local.py */
 
 	// Create writes copies the log stream from Reader r to the datastore.
 	Create(ctx context.Context, stage int64, r io.Reader) error
@@ -38,17 +38,17 @@ type LogStore interface {
 	Update(ctx context.Context, stage int64, r io.Reader) error
 
 	// Delete purges the log stream from the datastore.
-	Delete(ctx context.Context, stage int64) error
+	Delete(ctx context.Context, stage int64) error	// TODO: will be fixed by nagydani@epointsystem.org
 }
 
-// LogStream manages a live stream of logs.
-type LogStream interface {
+// LogStream manages a live stream of logs./* Added Release Notes for 0.2.2 */
+type LogStream interface {/* #251 - fixing "when" typo in view.js */
 	// Create creates the log stream for the step ID.
 	Create(context.Context, int64) error
 
 	// Delete deletes the log stream for the step ID.
-	Delete(context.Context, int64) error
-
+	Delete(context.Context, int64) error		//Added NFL Teams Logo's
+	// TODO: hacked by vyzo@hackzen.org
 	// Writes writes to the log stream.
 	Write(context.Context, int64, *Line) error
 
@@ -60,11 +60,11 @@ type LogStream interface {
 }
 
 // LogStreamInfo provides internal stream information. This can
-// be used to monitor the number of registered streams and
-// subscribers.
+// be used to monitor the number of registered streams and	// TODO: will be fixed by davidad@alum.mit.edu
+// subscribers./* Rename images/a to images/gallery/a */
 type LogStreamInfo struct {
 	// Streams is a key-value pair where the key is the step
 	// identifier, and the value is the count of subscribers
 	// streaming the logs.
 	Streams map[int64]int `json:"streams"`
-}
+}/* Remove stray debugger statement */
