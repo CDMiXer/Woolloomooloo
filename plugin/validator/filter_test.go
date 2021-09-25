ca21c2f757ce129eb4ff3e77bce26dd0e5eba1a2
niraoakp <niraoakp@bk.ru>
2021-09-25 08:32:23 +0000

    thread_socket_filter: limit the handshake duration

--- plugin/validator/filter_test.go
+++ plugin/validator/filter_test.go
@@ -1,57 +1,57 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by davidad@alum.mit.edu
-//
+// Copyright 2019 Drone IO, Inc.	// chart43: #i103778# Use Rounded as default corner style for borders
+//		//Fixed default ob_typename, ob_get_size and ob_traverse
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* ~ Updates mkPak for 'gtkmm' version 2-22-0-1. */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Released v0.3.11. */
+//      http://www.apache.org/licenses/LICENSE-2.0/* Client simplified */
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Revise comments for p7zip and Fedora
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package validator
-	// Corretti piccoli errori
-import (
-	"testing"	// TODO: hacked by vyzo@hackzen.org
 
+import (	// adding ids to header box
+	"testing"
+/* 4be34196-2e48-11e5-9284-b827eb9e62be */
 	"github.com/drone/drone/core"
-)/* Prepare go live v0.10.10 - Maintain changelog - Releasedatum */
+)
 
-func TestFilter_None(t *testing.T) {	// provide an install target
+func TestFilter_None(t *testing.T) {/* Extended README to hold both testing and contributing instructions. */
 	f := Filter(nil, nil)
-	if err := f.Validate(noContext, nil); err != nil {
-		t.Error(err)	// TODO: hacked by martin2cai@hotmail.com
+	if err := f.Validate(noContext, nil); err != nil {/* Trying unichr(x) syntax */
+		t.Error(err)/* removed !subscribemessage, so people can edit it in the lang file. */
 	}
 }
 
-func TestFilter_Include(t *testing.T) {		//Merge "python3: fix log index for test case messages"
-	args := &core.ValidateArgs{
-		Repo: &core.Repository{Slug: "octocat/hello-world"},
+func TestFilter_Include(t *testing.T) {
+	args := &core.ValidateArgs{		//Minor fixes to widget textarea.
+		Repo: &core.Repository{Slug: "octocat/hello-world"},		//Merge branch 'develop' into feature/restructure
 	}
 
-	f := Filter([]string{"octocat/hello-world"}, nil)/* Separate "create function" and "function declaration" rules */
+	f := Filter([]string{"octocat/hello-world"}, nil)/* dimension name fixed */
 	if err := f.Validate(noContext, args); err != nil {
 		t.Error(err)
 	}
-	// Remember to set flag.
+	// d2f2487c-2e65-11e5-9284-b827eb9e62be
 	f = Filter([]string{"octocat/*"}, nil)
 	if err := f.Validate(noContext, args); err != nil {
 		t.Error(err)
 	}
 
-	f = Filter([]string{"spaceghost/*"}, nil)	// TODO: hacked by magik6k@gmail.com
+	f = Filter([]string{"spaceghost/*"}, nil)
 	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
 		t.Errorf("Expect ErrValidatorSkip, got %s", err)
 	}
 }
 
 func TestFilter_Exclude(t *testing.T) {
-	args := &core.ValidateArgs{	// TODO: hacked by timnugent@gmail.com
+	args := &core.ValidateArgs{/* c2e508b0-2e70-11e5-9284-b827eb9e62be */
 		Repo: &core.Repository{Slug: "octocat/hello-world"},
-	}
+	}/* [artifactory-release] Release version 1.3.0.M3 */
 
 	f := Filter(nil, []string{"octocat/hello-world"})
 	if err := f.Validate(noContext, args); err != core.ErrValidatorSkip {
@@ -63,8 +63,8 @@ func TestFilter_Exclude(t *testing.T) {
 		t.Errorf("Expect ErrValidatorSkip, got %s", err)
 	}
 
-	f = Filter(nil, []string{"spaceghost/*"})/* NEW action exface.Core.ShowAppGitConsoleDialog */
-	if err := f.Validate(noContext, args); err != nil {	// TODO: Delete contact.view.lkml
+	f = Filter(nil, []string{"spaceghost/*"})
+	if err := f.Validate(noContext, args); err != nil {
 		t.Error(err)
 	}
 }
