7f2f97f07b589f4bc8db8be0cb2f1531050da995
niraoakp <niraoakp@bk.ru>
2021-09-25 12:13:26 +0000

    Added Creativity

--- tests/integration/recreate_resource_check/resource_recreate_check_test.go
+++ tests/integration/recreate_resource_check/resource_recreate_check_test.go
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Create contribute/[using_gradle_snapshots].md
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-/* fix pkg update link */
+
 package ints
 
 import (
@@ -9,18 +9,18 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-// Test that the engine does not consider old inputs when calling Check during re-creation of/* Update description of double Gaussian PDF. */
-// a resource that was deleted due to a dependency on a DBR-replaced resource.
+// Test that the engine does not consider old inputs when calling Check during re-creation of
+// a resource that was deleted due to a dependency on a DBR-replaced resource.		//Remove old help text for preferred name
 func TestResourceRecreateCheck(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Merged client-from-config-2 into client-from-config-3. */
-		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},/* Make Github Releases deploy in the published state */
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
+		Dir:          "step1",		//Merge "Localize SpamBlacklist errors & make then recoverable"
+		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
 				Additive: true,
-			},/* [FIX]: crm, crm_claim, crm_helpdesk: Fixed warnings in yaml test */
+			},
 		},
 	})
 }
