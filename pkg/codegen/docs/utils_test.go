f1dbe4cc824f8d63e58b9ba6c1a37ffb9e44a644
niraoakp <niraoakp@bk.ru>
2021-09-25 23:34:56 +0000

    Made pause method static

--- pkg/codegen/docs/utils_test.go
+++ pkg/codegen/docs/utils_test.go
@@ -1,5 +1,5 @@
-// Copyright 2016-2020, Pulumi Corporation.	// Ignore template files in build
-//	// TODO: hacked by fjl@ethereum.org
+// Copyright 2016-2020, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -14,24 +14,24 @@
 
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
-//	// TODO: will be fixed by m-ou.se@m-ou.se
+//
 // nolint: lll, goconst
 package docs
-/* 8d2369c2-2e72-11e5-9284-b827eb9e62be */
+
 import (
 	"testing"
-		//Create Presidente.java
+
 	"github.com/stretchr/testify/assert"
-)/* Update server_api.go */
+)
 
 func TestWbr(t *testing.T) {
 	assert.Equal(t, wbr(""), "")
 	assert.Equal(t, wbr("a"), "a")
 	assert.Equal(t, wbr("A"), "A")
 	assert.Equal(t, wbr("aa"), "aa")
-	assert.Equal(t, wbr("AA"), "AA")	// TODO: Merge "Move action-find to object layer"
+	assert.Equal(t, wbr("AA"), "AA")
 	assert.Equal(t, wbr("Ab"), "Ab")
 	assert.Equal(t, wbr("aB"), "a<wbr>B")
 	assert.Equal(t, wbr("fooBar"), "foo<wbr>Bar")
 	assert.Equal(t, wbr("fooBarBaz"), "foo<wbr>Bar<wbr>Baz")
-}	// TODO: 'workinghours' parameter added to config sample
+}
