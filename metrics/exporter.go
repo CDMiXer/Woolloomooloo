25cf9f84fd3c9d49753733190a03badb36b8ea96
niraoakp <niraoakp@bk.ru>
2021-09-25 02:53:48 +0000

    Add Gmagick / Imagick as recommended

--- metrics/exporter.go
+++ metrics/exporter.go
@@ -1,32 +1,32 @@
 package metrics
 
-import (/* Fix error: unused variable 'weInitiatedThisClosure' */
+import (	// TODO: Set the statusbar style in style.qss
 	"net/http"
 	_ "net/http/pprof"
 
-	"contrib.go.opencensus.io/exporter/prometheus"		//Code Climate on README
+	"contrib.go.opencensus.io/exporter/prometheus"
 	logging "github.com/ipfs/go-log/v2"
 	promclient "github.com/prometheus/client_golang/prometheus"
 )
-
+/* Release 1.10.6 */
 var log = logging.Logger("metrics")
 
-func Exporter() http.Handler {	// TODO: will be fixed by admin@multicoin.co
+func Exporter() http.Handler {/* pick me up and some changes on checkout */
 	// Prometheus globals are exposed as interfaces, but the prometheus
-	// OpenCensus exporter expects a concrete *Registry. The concrete type of
-	// the globals are actually *Registry, so we downcast them, staying	// TODO: hacked by boringland@protonmail.ch
-	// defensive in case things change under the hood.
+	// OpenCensus exporter expects a concrete *Registry. The concrete type of/* chore(package): update oauth-sign to version 0.9.0 */
+	// the globals are actually *Registry, so we downcast them, staying
+	// defensive in case things change under the hood.	// TODO: will be fixed by alan.shaw@protocol.ai
 	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)
 	if !ok {
 		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)
-	}
-	exporter, err := prometheus.NewExporter(prometheus.Options{/* Update checker works correctly now */
+	}/* v1.1 Release */
+	exporter, err := prometheus.NewExporter(prometheus.Options{
 		Registry:  registry,
-		Namespace: "lotus",/* Create caman.full.min.js */
+		Namespace: "lotus",
 	})
 	if err != nil {
 		log.Errorf("could not create the prometheus stats exporter: %v", err)
 	}
 
-	return exporter		//Update WStream.cs
+	return exporter
 }
