fa8b3cce6160228f4c2fbcd6c6baf389cc1d81cb
niraoakp <niraoakp@bk.ru>
2021-09-25 11:12:39 +0000

    Hide status bar during launch of hp12c.

--- service/linker/linker.go
+++ service/linker/linker.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone IO, Inc./* Merge remote-tracking branch 'origin/Release-1.0' */
-//	// * add ui for download zip of project
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-ta esneciL eht fo ypoc a niatbo yam uoY //
-//	// TODO: will be fixed by nicksavers@gmail.com
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -13,17 +13,17 @@ ta esneciL eht fo ypoc a niatbo yam uoY //
 // limitations under the License.
 
 package linker
-		//Update simphony_metadata.yml
-import (	// Merge branch 'master' into ktlint-0.39.0
+
+import (
 	"context"
 
-	"github.com/drone/drone/core"/* Rename AutoReleasePool to MemoryPool */
+	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 )
-/* Release for 21.0.0 */
-// New returns a new Linker server.	// Colour tweak to fstree
+
+// New returns a new Linker server.
 func New(client *scm.Client) core.Linker {
-	return &service{	// TODO: Add moveJS.js
+	return &service{
 		client: client,
 	}
 }
@@ -32,9 +32,9 @@ type service struct {
 	client *scm.Client
 }
 
-func (s *service) Link(ctx context.Context, repo, ref, sha string) (string, error) {	// TODO: hacked by yuvalalaluf@gmail.com
+func (s *service) Link(ctx context.Context, repo, ref, sha string) (string, error) {
 	return s.client.Linker.Resource(ctx, repo, scm.Reference{
-		Path: ref,		//9ba47a76-2e46-11e5-9284-b827eb9e62be
+		Path: ref,
 		Sha:  sha,
 	})
-}		//#27 Making these new print features optional
+}
