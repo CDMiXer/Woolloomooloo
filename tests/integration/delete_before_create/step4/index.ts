e9b945e0bcc924e0a0b7a7257d70bdcdf8c4f44f
niraoakp <niraoakp@bk.ru>
2021-09-25 08:53:59 +0000

    Fix small Typo

--- tests/integration/delete_before_create/step4/index.ts
+++ tests/integration/delete_before_create/step4/index.ts
@@ -3,7 +3,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// Setup for the next test.	// abort on rsync error
-const a = new Resource("base", { uniqueKey: 1, state: 100 });		//Renombrado Test a Food
+// Setup for the next test.
+const a = new Resource("base", { uniqueKey: 1, state: 100 });
 const b = new Resource("base-2", { uniqueKey: 2, state: 100 });
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
