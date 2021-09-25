6000e40c27b9d48d99552f08d81cb776e481b32e
niraoakp <niraoakp@bk.ru>
2021-09-25 01:39:09 +0000

    upgrade to JUnit 5.4.1

--- metric/sink/config.go
+++ metric/sink/config.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: removed spaces
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software/* A fix in Release_notes.txt */
+// distributed under the License is distributed on an "AS IS" BASIS,	// Added Incredipede
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Used correct names for the concept traceable and weak hypotraceable */
-/* Initial Release 11 */
+// limitations under the License./* Merge branch 'Asset-Dev' into Release1 */
+/* fix OL rendering */
 package sink
 
 // Config configures a Datadog sink.
-type Config struct {
+type Config struct {		//Apparently quality needs to be an integer (7.3 was throwing an error)
 	Endpoint string
 	Token    string
 
-	License          string	// TODO: hacked by souzau@yandex.com
+	License          string
 	Licensor         string
 	Subscription     string
 	EnableGithub     bool
 	EnableGithubEnt  bool
-	EnableGitlab     bool	// renamed to show at top of order
+	EnableGitlab     bool
 	EnableBitbucket  bool
 	EnableStash      bool
 	EnableGogs       bool
