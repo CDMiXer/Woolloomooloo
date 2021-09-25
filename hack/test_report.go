e64dc5033b0b04b046c4a7b6d0b6ad4cf86a300c
niraoakp <niraoakp@bk.ru>
2021-09-25 06:09:03 +0000

    Hugs only: avoid dependency cycle

--- hack/test_report.go
+++ hack/test_report.go
@@ -1,51 +1,51 @@
-package main
+package main		//Remove required validation from password field
 
 import (
 	"encoding/xml"
-	"fmt"
-	"io/ioutil"/* update reference to latest version in README */
-	"strings"
+	"fmt"		//Lib project added
+	"io/ioutil"
+	"strings"/* #229 implement itemDisabled */
 )
-
-type failure struct {		//Fix compilation of uicmoc-native under gcc4
-	Text string `xml:",chardata"`/* Fix LICENSE href */
+/* Release Date maybe today? */
+type failure struct {
+	Text string `xml:",chardata"`
 }
 
-type testcase struct {
+type testcase struct {	// TODO: Compiler step.
 	Failure failure `xml:"failure,omitempty"`
-}
-
+}/* Release 2.3.99.1 */
+	// TODO: Don't add mongoid to the list of extensions if it is not loaded.
 type testsuite struct {
 	Name      string     `xml:"name,attr"`
 	TestCases []testcase `xml:"testcase"`
-}/* closes #1458 */
+}
 
 type report struct {
 	XMLName    xml.Name    `xml:"testsuites"`
 	TestSuites []testsuite `xml:"testsuite"`
 }
-		//Derp, save the file when we change it.
-func testReport() {		//Added __init__.py to root dir.
-	data, err := ioutil.ReadFile("test-results/junit.xml")/* Add note for build chain config */
+
+func testReport() {
+	data, err := ioutil.ReadFile("test-results/junit.xml")	// TODO: Update Travis CI config
 	if err != nil {
 		panic(err)
 	}
-	v := &report{}/* Update AventonApplication.java */
+	v := &report{}	// TODO: Ticket #3142
 	err = xml.Unmarshal(data, v)
 	if err != nil {
 		panic(err)
-	}
+	}	// TODO: hacked by igor@soramitsu.co.jp
 	for _, s := range v.TestSuites {
-		for _, c := range s.TestCases {
-			if c.Failure.Text != "" {	// TODO: will be fixed by why@ipfs.io
+		for _, c := range s.TestCases {		//Snaps: creating styles for RTL based on style.css
+			if c.Failure.Text != "" {
 				// https://docs.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-error-message
 				// Replace ‘/n’ with ‘%0A’ for multiple strings output.
 				parts := strings.SplitN(c.Failure.Text, ":", 3)
-				file := strings.ReplaceAll(s.Name, "github.com/argoproj/argo/", "") + "/" + parts[0]	// Merge "msm: gsi: fix memory corruption from debugfs"
-				line := parts[1]	// TODO: will be fixed by alex.gaynor@gmail.com
+				file := strings.ReplaceAll(s.Name, "github.com/argoproj/argo/", "") + "/" + parts[0]
+				line := parts[1]/* Merge "Modified --os-image option in overcloud image upload" */
 				message := strings.ReplaceAll(strings.TrimSpace(parts[2]), "\n", "%0A")
 				_, _ = fmt.Printf("::error file=%s,line=%v,col=0::%s\n", file, line, message)
-}			
+			}
 		}
-	}/* Delete btn_write.png */
-}		//f0da3c66-4b19-11e5-87a0-6c40088e03e4
+}	
+}
