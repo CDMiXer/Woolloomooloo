246d00640f601bf478fd468a3e3df1aee4d58e15
niraoakp <niraoakp@bk.ru>
2021-09-25 23:17:27 +0000

    merge kens fix to RT: #4109

--- grpc-go/test/race.go
+++ grpc-go/test/race.go
@@ -1,24 +1,24 @@
 // +build race
 
-/*
+/*		//Merge branch 'master' into RES-1179-customresnet
  * Copyright 2016 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Merge "Don't display 1st-time dialog when disabling Data Saver." into nyc-dev
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: Dokumentation hinzugefügt.
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *		//Merge branch 'InDev'
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Learning the markdown
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Refined Notification Query
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release for v0.3.0. */
+ *//* Release of eeacms/eprtr-frontend:0.0.2-beta.4 */
 
 package test
 
-func init() {
+func init() {/* added first version of transaction/delete of transformers */
 	raceMode = true
-}
+}		//Added info about support .zip in README.md
