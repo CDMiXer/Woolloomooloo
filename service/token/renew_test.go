f2195893a495e683c986368d815483c27f03890f
niraoakp <niraoakp@bk.ru>
2021-09-25 06:07:11 +0000

    Committed to OBS: supportutils-plugin-iprint-1.0-2

--- service/token/renew_test.go
+++ service/token/renew_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by lexy8russo@outlook.com
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Update README for zombie-invaders
-// +build !oss/* Fixed single quotes used when double quotes should be used */
+
+// +build !oss/* Update description meta tag to match body */
 
 package token
