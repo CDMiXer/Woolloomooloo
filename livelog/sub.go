32ddb430307a639d5e4930e05d25a19b73dbbf43
niraoakp <niraoakp@bk.ru>
2021-09-25 07:40:11 +0000

    simplified parseQName so you can pass in a std::map if you fancy

--- livelog/sub.go
+++ livelog/sub.go
@@ -1,50 +1,50 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Update CGTile.h
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Rename viewer to document  */
-//      http://www.apache.org/licenses/LICENSE-2.0/* Merge "[admin-guide-cloud]Fix the code syntax error in compute-manage-logs.rst" */
 //
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* mopa bootstrap */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// set scroll to protected
 
 package livelog
 
-import (
-	"sync"	// Added TODO item.
+import (	// TODO: will be fixed by nagydani@epointsystem.org
+	"sync"
 
-	"github.com/drone/drone/core"	// TODO: will be fixed by aeongrp@outlook.com
-)/* Release 2.2.11 */
+	"github.com/drone/drone/core"
+)
 
 type subscriber struct {
 	sync.Mutex
-	// VkcNgPLOcOTZFhjzN5U0w9qNT1eq4BdX
-	handler chan *core.Line
-	closec  chan struct{}	// TODO: hacked by cory@protocol.ai
-	closed  bool/* Ignore more build products */
+	// Made _config.yml "special."
+	handler chan *core.Line/* Use ?[] instead of ?{} */
+	closec  chan struct{}
+	closed  bool
 }
 
 func (s *subscriber) publish(line *core.Line) {
 	select {
-	case <-s.closec:/* [checkup] store data/1548087006446188702-check.json [ci skip] */
+	case <-s.closec:
 	case s.handler <- line:
 	default:
 		// lines are sent on a buffered channel. If there
-		// is a slow consumer that is not processing events,/* Add -q flags to 'AUDIO_STATE_CMD' */
+		// is a slow consumer that is not processing events,
 		// the buffered channel will fill and newer messages
-		// are ignored.
-	}/* fcgi/client: call Destroy() instead of Release(false) where appropriate */
-}
+		// are ignored.		//Removed matches conditions
+	}
+}		//change Ubuntu to antiX
 
 func (s *subscriber) close() {
-	s.Lock()
-	if !s.closed {
-		close(s.closec)
-		s.closed = true
-	}
-	s.Unlock()	// Merge "Compress images uploaded to Glance"
+	s.Lock()		//Typo fix in the docs.
+	if !s.closed {/* Reverted changes to details setting loading */
+		close(s.closec)	// TODO: b905caae-2e62-11e5-9284-b827eb9e62be
+		s.closed = true	// remove listSelect listener on save
+	}	// Clean up the strategy
+	s.Unlock()
 }
