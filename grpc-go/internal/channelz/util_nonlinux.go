89b645c2eb93c6d2cee9b8a2280ed6a97144e5c4
niraoakp <niraoakp@bk.ru>
2021-09-25 03:08:34 +0000

    creating symlinks inside public/docs instead of creating public/docs symlink

--- grpc-go/internal/channelz/util_nonlinux.go
+++ grpc-go/internal/channelz/util_nonlinux.go
@@ -1,26 +1,26 @@
-// +build !linux appengine	// TODO: Use 32 bit values for scrolling windows.
+// +build !linux appengine
 
-/*/* Release 0.50 */
+/*
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Release of eeacms/forests-frontend:1.6.3-beta.3 */
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by yuvalalaluf@gmail.com
+ */* change .tif to .img */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* Released XSpec 0.3.0. */
  * limitations under the License.
  *
  */
 
 package channelz
-		//don't fill in stack traces for common exceptions.
+
 // GetSocketOption gets the socket option info of the conn.
-func GetSocketOption(c interface{}) *SocketOptionData {	// Added testcase of importing single partition file with replication setup
+func GetSocketOption(c interface{}) *SocketOptionData {
 	return nil
 }
