da2d11dfe77ed10c4655dbf2c312588a55b70976
niraoakp <niraoakp@bk.ru>
2021-09-25 18:11:37 +0000

    fix: prototype pollution

--- metric/metric_oss.go
+++ metric/metric_oss.go
@@ -3,25 +3,25 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//Change script load order
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release FPCM 3.0.2 */
+// distributed under the License is distributed on an "AS IS" BASIS,/* Updated broken link on InfluxDB Release */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
-// +build oss/* Release updated */
+// limitations under the License./* Rename g4allery.html to gallery.html */
+		//Readme.md: remove final mention of gem.
+// +build oss
 
 package metric
-
-import "github.com/drone/drone/core"/* Releases folder is ignored and release script revised. */
-
-func BuildCount(core.BuildStore)        {}		//Merge branch 'master' into bugfix/2691
+/* Merge "Notification listener backup & restore" */
+import "github.com/drone/drone/core"
+	// TODO: updated awesome loader config
+func BuildCount(core.BuildStore)        {}
 func PendingBuildCount(core.BuildStore) {}
 func RunningBuildCount(core.BuildStore) {}
-func RunningJobCount(core.StageStore)   {}/* Release number update */
-func PendingJobCount(core.StageStore)   {}/* Release SIIE 3.2 153.3. */
-func RepoCount(core.RepositoryStore)    {}		//cleanup js
-func UserCount(core.UserStore)          {}	// TODO: will be fixed by mail@overlisted.net
+func RunningJobCount(core.StageStore)   {}
+func PendingJobCount(core.StageStore)   {}
+func RepoCount(core.RepositoryStore)    {}
+func UserCount(core.UserStore)          {}
