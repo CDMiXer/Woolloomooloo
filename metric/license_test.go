cfcbc6cebebce106c50bb3d2b93f10b0f36007b7
niraoakp <niraoakp@bk.ru>
2021-09-25 18:08:40 +0000

    Clean up support.function regex

--- metric/license_test.go
+++ metric/license_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by ac0dem0nk3y@gmail.com
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Update user_facts.view.lkml */
+// +build !oss
 
 package metric
