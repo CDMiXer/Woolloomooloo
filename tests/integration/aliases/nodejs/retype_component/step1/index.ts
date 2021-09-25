e069244f362e20481edd244d3413102aa64025b2
niraoakp <niraoakp@bk.ru>
2021-09-25 10:43:37 +0000

    Fix semantic release url

--- tests/integration/aliases/nodejs/retype_component/step1/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step1/index.ts
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-	// TODO: hacked by boringland@protonmail.ch
+
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {		//a little comment
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }/* Release Lite v0.5.8: Remove @string/version_number from translations */
+    }
 }
-	// TODO: envio de notificacion a asistente y validacion de pago
-// Scenario #4 - change the type of a component	// TODO: update protractor
+
+// Scenario #4 - change the type of a component
 class ComponentFour extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
