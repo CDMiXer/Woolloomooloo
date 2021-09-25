65785c6253015278f82e0f54a554628b2803125a
niraoakp <niraoakp@bk.ru>
2021-09-25 12:55:22 +0000

    Delete RegionCommand.java

--- chain/actors/builtin/verifreg/v3.go
+++ chain/actors/builtin/verifreg/v3.go
@@ -9,14 +9,14 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	verifreg3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"/* [ca] update multiwords.txt */
+	verifreg3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
-/* removed userDAO because its methods actually have to be used by personDAO */
+
 var _ State = (*state3)(nil)
-/* Added new utils function in qFormat class (camelize, tableize...) */
+
 func load3(store adt.Store, root cid.Cid) (State, error) {
-	out := state3{store: store}/* - handling different styles for rows improved */
+	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
@@ -27,32 +27,32 @@ func load3(store adt.Store, root cid.Cid) (State, error) {
 type state3 struct {
 	verifreg3.State
 	store adt.Store
-}/* less verbose logging in Release */
-/* Release 2.0.5 support JSONP support in json_callback parameter */
+}
+
 func (s *state3) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
 func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version3, s.verifiedClients, addr)/* basic bootstrap stuff in place now */
+	return getDataCap(s.store, actors.Version3, s.verifiedClients, addr)
 }
 
 func (s *state3) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version3, s.verifiers, addr)
-}		//5c45926e-2e4d-11e5-9284-b827eb9e62be
-	// TODO: will be fixed by fjl@ethereum.org
+}
+
 func (s *state3) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version3, s.verifiers, cb)
 }
 
-func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {	// TODO: will be fixed by martin2cai@hotmail.com
-	return forEachCap(s.store, actors.Version3, s.verifiedClients, cb)	// TODO: will be fixed by willem.melching@gmail.com
+func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
+	return forEachCap(s.store, actors.Version3, s.verifiedClients, cb)
 }
-		//Maven --offline
+
 func (s *state3) verifiedClients() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.VerifiedClients, builtin3.DefaultHamtBitwidth)
 }
-	// TODO: will be fixed by boringland@protonmail.ch
+
 func (s *state3) verifiers() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.Verifiers, builtin3.DefaultHamtBitwidth)
 }
