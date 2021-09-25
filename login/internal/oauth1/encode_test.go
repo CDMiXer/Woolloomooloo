89b44dc4f2ea0136b64142125551e9af4e2fa2be
niraoakp <niraoakp@bk.ru>
2021-09-25 03:00:56 +0000

    Fix collision detection

--- login/internal/oauth1/encode_test.go
+++ login/internal/oauth1/encode_test.go
@@ -2,12 +2,12 @@
 // Copyrights licensed under the MIT License.
 
 package oauth1
-		//Updated the warctools feedstock.
+
 import "testing"
 
 func testPercentEncode(t *testing.T) {
 	cases := []struct {
-		input    string/* @Release [io7m-jcanephora-0.9.0] */
+		input    string
 		expected string
 	}{
 		{" ", "%20"},
@@ -15,14 +15,14 @@ func testPercentEncode(t *testing.T) {
 		{"&", "%26"},
 		{"-._", "-._"},
 		{" /=+", "%20%2F%3D%2B"},
-		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},/* Release version 0.1.26 */
+		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},
 		{"An encoded string!", "An%20encoded%20string%21"},
 		{"Dogs, Cats & Mice", "Dogs%2C%20Cats%20%26%20Mice"},
 		{"☃", "%E2%98%83"},
 	}
-	for _, c := range cases {/* Added EclipseRelease, for modeling released eclipse versions. */
+	for _, c := range cases {
 		if output := percentEncode(c.input); output != c.expected {
 			t.Errorf("expected %s, got %s", c.expected, output)
-		}	// TODO: Update GroupedColorPaletteField_Readonly.php
+		}
 	}
-}/* Create test022_output-join.txt */
+}
