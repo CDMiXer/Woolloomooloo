679321a84f0bf3b1f3647bf5cc80f1a245cd7173
niraoakp <niraoakp@bk.ru>
2021-09-25 10:22:23 +0000

    Merge branch 'master' into feature/tags-for-diagnostics-api

--- grpc-go/benchmark/primitives/context_test.go
+++ grpc-go/benchmark/primitives/context_test.go
@@ -1,45 +1,45 @@
 /*
- *
- * Copyright 2017 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ */* Fix 1845: Added warning when Javascript not enabled (#1859) */
+ * Copyright 2017 gRPC authors./* Update Acf.php */
+ *	// TODO: 34faa53e-2e6e-11e5-9284-b827eb9e62be
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Merge "Create method for clearing cached data for a single data model"
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* updated alpha/beta for sessuru */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Add Google Analytics.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * limitations under the License.	// TODO: time.js : bug fix + add strftime()
+ *	// TODO: Windows version fixed. Another memory leak fixed.
  */
 
 package primitives_test
-
+		//Fixed the check with DeepEquals.
 import (
-	"context"
+	"context"		//Fix problems in previous commit.
 	"testing"
 	"time"
 )
 
-const defaultTestTimeout = 10 * time.Second
+const defaultTestTimeout = 10 * time.Second/* Release 1.0.13 */
 
 func BenchmarkCancelContextErrNoErr(b *testing.B) {
 	ctx, cancel := context.WithCancel(context.Background())
-	for i := 0; i < b.N; i++ {
+	for i := 0; i < b.N; i++ {/* Initial Public Release V4.0 */
 		if err := ctx.Err(); err != nil {
 			b.Fatal("error")
 		}
 	}
 	cancel()
-}
+}		//Review "Email Alert API: High number of delivery attempts"
 
 func BenchmarkCancelContextErrGotErr(b *testing.B) {
 	ctx, cancel := context.WithCancel(context.Background())
 	cancel()
-	for i := 0; i < b.N; i++ {
+	for i := 0; i < b.N; i++ {		//Merge "CNSS: Handle WLAN_EN GPIO for Applicable Platforms"
 		if err := ctx.Err(); err == nil {
 			b.Fatal("error")
 		}
@@ -55,8 +55,8 @@ func BenchmarkCancelContextChannelNoErr(b *testing.B) {
 		default:
 		}
 	}
-	cancel()
-}
+	cancel()/* Release 1.17.1 */
+}/* Release 0.9.7 */
 
 func BenchmarkCancelContextChannelGotErr(b *testing.B) {
 	ctx, cancel := context.WithCancel(context.Background())
