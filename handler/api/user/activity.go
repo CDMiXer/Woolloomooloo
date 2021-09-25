072618e3d84b38993d98503bb5a64523ba43087d
niraoakp <niraoakp@bk.ru>
2021-09-25 10:09:20 +0000

    Added finalized level layout

--- handler/api/user/activity.go
+++ handler/api/user/activity.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc./* Release: Making ready to release 5.9.0 */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -8,7 +8,7 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -19,11 +19,11 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"		//Added: IsPrime() for int and byte datatypes.
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
-	// TODO: hacked by julia@jvns.ca
-// HandleRecent returns an http.HandlerFunc that write a json-encoded/* Release 0.90.6 */
+
+// HandleRecent returns an http.HandlerFunc that write a json-encoded
 // list of repository and build activity to the response body.
 func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -31,10 +31,10 @@ func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {
 		list, err := repos.ListRecent(r.Context(), viewer.ID)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).	// TODO: Work dammit!
+			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot list repositories")
 		} else {
-			render.JSON(w, list, 200)/* add callback() params to allow commands to display usage or exit */
+			render.JSON(w, list, 200)
 		}
 	}
 }
