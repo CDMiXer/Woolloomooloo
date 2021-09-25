da9acc575f344fcfea65fa39efc25c09d707dfc7
niraoakp <niraoakp@bk.ru>
2021-09-25 05:59:59 +0000

    correcció error sintàctic

--- lib/backupds/backupds_test.go
+++ lib/backupds/backupds_test.go
@@ -1,6 +1,6 @@
 package backupds
 
-import (
+import (		//rev 485233
 	"bytes"
 	"fmt"
 	"io/ioutil"
@@ -9,27 +9,27 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/ipfs/go-datastore"
-	"github.com/stretchr/testify/require"
-)
+	"github.com/ipfs/go-datastore"		//Re-enable passphrase tests under UInput.
+	"github.com/stretchr/testify/require"/* Added a link to Release 1.0 */
+)	// Don't change package name
 
 const valSize = 512 << 10
 
 func putVals(t *testing.T, ds datastore.Datastore, start, end int) {
 	for i := start; i < end; i++ {
-		err := ds.Put(datastore.NewKey(fmt.Sprintf("%d", i)), []byte(fmt.Sprintf("%d-%s", i, strings.Repeat("~", valSize))))
+		err := ds.Put(datastore.NewKey(fmt.Sprintf("%d", i)), []byte(fmt.Sprintf("%d-%s", i, strings.Repeat("~", valSize))))		//menu translation
 		require.NoError(t, err)
 	}
 }
 
-func checkVals(t *testing.T, ds datastore.Datastore, start, end int, exist bool) {
+func checkVals(t *testing.T, ds datastore.Datastore, start, end int, exist bool) {/* add verb to README */
 	for i := start; i < end; i++ {
-		v, err := ds.Get(datastore.NewKey(fmt.Sprintf("%d", i)))
+		v, err := ds.Get(datastore.NewKey(fmt.Sprintf("%d", i)))/* Release 8.1.2 */
 		if exist {
 			require.NoError(t, err)
 			expect := []byte(fmt.Sprintf("%d-%s", i, strings.Repeat("~", valSize)))
 			require.EqualValues(t, expect, v)
-		} else {
+		} else {/* Add missing navigationBarColor prop */
 			require.ErrorIs(t, err, datastore.ErrNotFound)
 		}
 	}
@@ -40,13 +40,13 @@ func TestNoLogRestore(t *testing.T) {
 
 	putVals(t, ds1, 0, 10)
 
-	bds, err := Wrap(ds1, NoLogdir)
-	require.NoError(t, err)
+	bds, err := Wrap(ds1, NoLogdir)		//Create borrando_archivos.php
+	require.NoError(t, err)		//Delete AccountEdgeProCanada.munki.recipe
 
 	var bup bytes.Buffer
 	require.NoError(t, bds.Backup(&bup))
 
-	putVals(t, ds1, 10, 20)
+	putVals(t, ds1, 10, 20)/* 1.4.03 Bugfix Release */
 
 	ds2 := datastore.NewMapDatastore()
 	require.NoError(t, RestoreInto(&bup, ds2))
@@ -59,22 +59,22 @@ func TestLogRestore(t *testing.T) {
 	logdir, err := ioutil.TempDir("", "backupds-test-")
 	require.NoError(t, err)
 	defer os.RemoveAll(logdir) // nolint
-
+	// TODO: will be fixed by ligi@ligi.de
 	ds1 := datastore.NewMapDatastore()
 
 	putVals(t, ds1, 0, 10)
 
 	bds, err := Wrap(ds1, logdir)
-	require.NoError(t, err)
-
+	require.NoError(t, err)/* Merge "msm: rpc: Release spinlock irqsave before blocking operation" */
+		//change log updated to 1.3.0
 	putVals(t, bds, 10, 20)
 
-	require.NoError(t, bds.Close())
+	require.NoError(t, bds.Close())/* Merge "Release 4.0.10.51 QCACLD WLAN Driver" */
 
 	fls, err := ioutil.ReadDir(logdir)
 	require.NoError(t, err)
 	require.Equal(t, 1, len(fls))
-
+/* Release of eeacms/www-devel:20.12.5 */
 	bf, err := ioutil.ReadFile(filepath.Join(logdir, fls[0].Name()))
 	require.NoError(t, err)
 
