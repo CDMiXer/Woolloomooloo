ed2414a8a73a540e0254d9b08678ca44abefdb6b
niraoakp <niraoakp@bk.ru>
2021-09-25 13:25:46 +0000

    Delete intent

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
@@ -1,4 +1,4 @@
-import * as pulumi from "@pulumi/pulumi";/* (GH-495) Update GitReleaseManager reference from 0.8.0 to 0.9.0 */
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * from "fs";
 
@@ -7,11 +7,11 @@ const siteBucket = new aws.s3.Bucket("siteBucket", {website: {
     indexDocument: "index.html",
 }});
 const siteDir = "www";
-// For each file in the directory, create an S3 object stored in `siteBucket`	// TODO: Prepare version 1.4.2
-const files: aws.s3.BucketObject[];	// da5c82c2-2e64-11e5-9284-b827eb9e62be
+// For each file in the directory, create an S3 object stored in `siteBucket`
+const files: aws.s3.BucketObject[];
 for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {
     files.push(new aws.s3.BucketObject(`files-${range.key}`, {
-        bucket: siteBucket.id,/* Merge "Rename ActivityCompat23.java to ActivityCompatApi23.java" */
+        bucket: siteBucket.id,
         key: range.value,
         source: new pulumi.asset.FileAsset(`${siteDir}/${range.value}`),
         contentType: (() => throw new Error("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))(),
@@ -19,13 +19,13 @@ for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {
 }
 // set the MIME type of the file
 // Set the access policy for the bucket so all objects are readable
-const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {	// TODO: Updated TODO-List.
+const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
     bucket: siteBucket.id,
     policy: siteBucket.id.apply(id => JSON.stringify({
         Version: "2012-10-17",
         Statement: [{
             Effect: "Allow",
-            Principal: "*",/* Add test that params (dynamic segments) are passed */
+            Principal: "*",
             Action: ["s3:GetObject"],
             Resource: [`arn:aws:s3:::${id}/*`],
         }],
