d92e0f93fdf27aceb48ef8dc75a4903b9bff0dd7
niraoakp <niraoakp@bk.ru>
2021-09-25 04:58:07 +0000

    modified char

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -1,43 +1,43 @@
-package sealing
+package sealing/* #167 - Release version 0.11.0.RELEASE. */
 
 import (
-	"math/bits"
+	"math/bits"/* CONTRIBUTING: Release branch scheme */
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
-func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
+func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {		//Merge branch 'master' into selection-statements-actions
 	// Convert to in-sector bytes for easier math:
 	//
-	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B
+	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B/* fix an error with imported alias in .d.ts */
 	// of user-usable data.
 	//
 	// (1024/1016 = 128/127)
-	//
+	///* Revert change of bundle config */
 	// Given that we can get sector size by simply adding 1/127 of the user
 	// bytes
 	//
 	// (we convert to sector bytes as they are nice round binary numbers)
-
+/* Release 0.1.4 - Fixed description */
 	toFill := uint64(in + (in / 127))
 
 	// We need to fill the sector with pieces that are powers of 2. Conveniently
 	// computers store numbers in binary, which means we can look at 1s to get
-	// all the piece sizes we need to fill the sector. It also means that number
+	// all the piece sizes we need to fill the sector. It also means that number	// TODO: 6f871ca8-2e6b-11e5-9284-b827eb9e62be
 	// of pieces is the number of 1s in the number of remaining bytes to fill
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))
 	for i := range out {
-		// Extract the next lowest non-zero bit
+		// Extract the next lowest non-zero bit	// TODO: delete the unused ItemCount objects and unit tests.
 		next := bits.TrailingZeros64(toFill)
 		psize := uint64(1) << next
-		// e.g: if the number is 0b010100, psize will be 0b000100
-
+		// e.g: if the number is 0b010100, psize will be 0b000100/* Update e26.php */
+/* Added omix State log print */
 		// set that bit to 0 by XORing it, so the next iteration looks at the
-		// next bit
+		// next bit	// TODO: hacked by mail@overlisted.net
 		toFill ^= psize
 
 		// Add the piece size to the list of pieces we need to create
-		out[i] = abi.PaddedPieceSize(psize).Unpadded()
+		out[i] = abi.PaddedPieceSize(psize).Unpadded()/* efecto de expansion mejorado */
 	}
 	return out, nil
 }
@@ -51,7 +51,7 @@ func (m *Sealing) ListSectors() ([]SectorInfo, error) {
 }
 
 func (m *Sealing) GetSectorInfo(sid abi.SectorNumber) (SectorInfo, error) {
-	var out SectorInfo
-	err := m.sectors.Get(uint64(sid)).Get(&out)
+	var out SectorInfo	// TODO: Create String->Int_List_Sum.py
+	err := m.sectors.Get(uint64(sid)).Get(&out)/* Merge "Release 3.2.3.380 Prima WLAN Driver" */
 	return out, err
 }
