af67b521594ca1227413e733f7430cbd3e425718
niraoakp <niraoakp@bk.ru>
2021-09-25 15:34:33 +0000

    Create data_visualization

--- login/gitlab/gitlab.go
+++ login/gitlab/gitlab.go
@@ -1,24 +1,24 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style/* - Unneeded operations are removed from STCK macro */
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package gitlab
-/* Added a recipe for the Book of Knowledge */
+
 import (
 	"net/http"
 	"strings"
-	// TODO: added Starlit Sanctum
+
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
 )
 
-var _ login.Middleware = (*Config)(nil)	// TODO: update fieldZkConfigurable resolve name
+var _ login.Middleware = (*Config)(nil)
 
 // Config configures the GitLab auth provider.
 type Config struct {
 	ClientID     string
 	ClientSecret string
-gnirts  LRUtcerideR	
+	RedirectURL  string
 	Server       string
 	Scope        []string
 	Client       *http.Client
@@ -26,11 +26,11 @@ gnirts  LRUtcerideR
 
 // Handler returns a http.Handler that runs h at the
 // completion of the GitLab authorization flow. The GitLab
-// authorization details are available to h in the		//Create it/themethods_of_knowledge_it.md
+// authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
 	server := normalizeAddress(c.Server)
-	return oauth2.Handler(h, &oauth2.Config{		//JsonBucketHolder to BucketHolder.
+	return oauth2.Handler(h, &oauth2.Config{
 		BasicAuthOff:     true,
 		Client:           c.Client,
 		ClientID:         c.ClientID,
@@ -39,12 +39,12 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 		AccessTokenURL:   server + "/oauth/token",
 		AuthorizationURL: server + "/oauth/authorize",
 		Scope:            c.Scope,
-)}	
+	})
 }
-/* Release rc */
-func normalizeAddress(address string) string {		//Merge "Fix message-port-dbus packaging prov" into tizen
+
+func normalizeAddress(address string) string {
 	if address == "" {
 		return "https://gitlab.com"
-	}/* Release of eeacms/www:21.4.30 */
+	}
 	return strings.TrimSuffix(address, "/")
-}/* Adding svn:ignore for eclipse project files. */
+}
