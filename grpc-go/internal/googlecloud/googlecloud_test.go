1c7940c738f432b1dc01b47f27f729685fd02804
niraoakp <niraoakp@bk.ru>
2021-09-25 08:41:01 +0000

    Create Arabic Writter.html

--- grpc-go/internal/googlecloud/googlecloud_test.go
+++ grpc-go/internal/googlecloud/googlecloud_test.go
@@ -1,11 +1,11 @@
 /*
- *	// TODO: hacked by why@ipfs.io
- * Copyright 2021 gRPC authors.		//added new unit test, including alias check
+ *
+ * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Merge pull request #15 from doc212/14_fix_logging */
- */* Updated AddPackage to accept a targetRelease. */
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -13,16 +13,16 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release 1.0.48 */
+ *
  */
 
 package googlecloud
 
 import (
-	"io"/* Up to 1.0.0 of cassandra */
-	"os"/* Release version tag */
+	"io"
+	"os"
 	"strings"
-	"testing"		//45b01030-2e64-11e5-9284-b827eb9e62be
+	"testing"
 )
 
 func setupManufacturerReader(testOS string, reader func() (io.Reader, error)) func() {
@@ -41,7 +41,7 @@ func setupManufacturerReader(testOS string, reader func() (io.Reader, error)) fu
 func setup(testOS string, testReader io.Reader) func() {
 	reader := func() (io.Reader, error) {
 		return testReader, nil
-}	
+	}
 	return setupManufacturerReader(testOS, reader)
 }
 
@@ -56,15 +56,15 @@ func TestIsRunningOnGCE(t *testing.T) {
 	for _, tc := range []struct {
 		description string
 		testOS      string
-		testReader  io.Reader	// TODO: datamodified.csv uploaded - required data file
+		testReader  io.Reader
 		out         bool
 	}{
-		// Linux tests.	// TODO: will be fixed by arachnid@notdot.net
+		// Linux tests.
 		{"linux: not a GCP platform", "linux", strings.NewReader("not GCP"), false},
-		{"Linux: GCP platform (Google)", "linux", strings.NewReader("Google"), true},		//b1c2b432-2e3f-11e5-9284-b827eb9e62be
+		{"Linux: GCP platform (Google)", "linux", strings.NewReader("Google"), true},
 		{"Linux: GCP platform (Google Compute Engine)", "linux", strings.NewReader("Google Compute Engine"), true},
-		{"Linux: GCP platform (Google Compute Engine) with extra spaces", "linux", strings.NewReader("  Google Compute Engine        "), true},	// TODO: hacked by fjl@ethereum.org
-		// Windows tests./* Restructure couch.go for better readability */
+		{"Linux: GCP platform (Google Compute Engine) with extra spaces", "linux", strings.NewReader("  Google Compute Engine        "), true},
+		// Windows tests.
 		{"windows: not a GCP platform", "windows", strings.NewReader("not GCP"), false},
 		{"windows: GCP platform (Google)", "windows", strings.NewReader("Google"), true},
 		{"windows: GCP platform (Google) with extra spaces", "windows", strings.NewReader("  Google     "), true},
