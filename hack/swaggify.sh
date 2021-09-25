13da7077742afa71508e8fa973f9547948a2ee59
niraoakp <niraoakp@bk.ru>
2021-09-25 01:53:17 +0000

    Only need minor version to test ruby 2.1 on travis

--- hack/swaggify.sh
+++ hack/swaggify.sh
@@ -3,12 +3,12 @@ set -eu -o pipefail
 
 # order is important, "REPLACEME" -> "workflow"
 cat \
-    | sed 's/github.com.argoproj.argo.pkg.apis.workflow.v1alpha1./io.argoproj.REPLACEME.v1alpha1./' \
+    | sed 's/github.com.argoproj.argo.pkg.apis.workflow.v1alpha1./io.argoproj.REPLACEME.v1alpha1./' \	// TODO: Initialization fix
     | sed 's/cronworkflow\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/event\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/info\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/workflowarchive\./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/clusterworkflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \
+    | sed 's/clusterworkflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \		//Merge "Document 'post' job names"
     | sed 's/workflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/workflow\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/io.argoproj.REPLACEME.v1alpha1./io.argoproj.workflow.v1alpha1./' \
