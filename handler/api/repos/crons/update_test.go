eec822f7859349277033adfdfd088cd5a0d0f7d5
niraoakp <niraoakp@bk.ru>
2021-09-25 13:44:19 +0000

    #111, fix some broken links

--- handler/api/repos/crons/update_test.go
+++ handler/api/repos/crons/update_test.go
@@ -1,69 +1,69 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: will be fixed by 13860583249@yeah.net
+// Use of this source code is governed by the Drone Non-Commercial License/* Add very untested factorial and combo functions */
+// that can be found in the LICENSE file.
 
-// +build !oss/* Updated import/info.lua */
+// +build !oss	// Action steps undefined?
 
 package crons
 
-import (	// Added logs if there is a new device which is not present in the current list.
+import (
 	"bytes"
-	"context"/* 6ce54618-2e76-11e5-9284-b827eb9e62be */
-	"encoding/json"/* Testing one way tiles. */
+	"context"
+	"encoding/json"
 	"net/http"
-	"net/http/httptest"/* Delete Release_Type.h */
-	"testing"
-/* Delete Everylittledefectgetsrespect_2.jpg */
+	"net/http/httptest"/* Release 5.4-rc3 */
+	"testing"/* interface for square representation */
+		//Debian APT instructions in INSTALL
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-
-	"github.com/go-chi/chi"
+/* alien.complex vocabulary implementing support for C99 complex numbers */
+	"github.com/go-chi/chi"	// TODO: hacked by steven@stebalien.com
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
-)/* be972a94-2e62-11e5-9284-b827eb9e62be */
+	"github.com/google/go-cmp/cmp"		//Add tweet ID to a blog post
+)
 
 func TestHandleUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// Update comments in example
+
 	mockCron := new(core.Cron)
 	*mockCron = *dummyCron
-	mockCron.Disabled = false	// TODO: Merge "misc: bma2xx: attach sysfs to input device" into volatile-bcm23550
+	mockCron.Disabled = false	// TODO: Split out win32 specific code so that it can be tested on all platforms.
 	mockCron.Branch = "develop"
-	mockCron.Target = "staging"	// TODO: will be fixed by igor@soramitsu.co.jp
+	mockCron.Target = "staging"
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
-	crons := mock.NewMockCronStore(controller)/* 20.1-Release: remove duplicate CappedResult class */
-	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, mockCron.Name).Return(mockCron, nil)	// TODO: Fix name of entry point
+	crons := mock.NewMockCronStore(controller)
+	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, mockCron.Name).Return(mockCron, nil)
 	crons.EXPECT().Update(gomock.Any(), mockCron).Return(nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")/* Merge "[INTERNAL][FIX] unified.FileUploader height in toolbar fixed" */
+	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("cron", "nightly")/* First Release of Booklet. */
+	c.URLParams.Add("cron", "nightly")
 
-	in := new(bytes.Buffer)	// TODO: Added blocking methods calls
+	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(mockCron)
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/", in)
-	r = r.WithContext(
+	r := httptest.NewRequest("POST", "/", in)		//Update dwifslpreproc
+	r = r.WithContext(	// TODO: change variables names
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-
+		//Minor Z80 multiplication improvement
 	HandleUpdate(repos, crons).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {
+	if got, want := w.Code, http.StatusOK; want != got {	// added $.afui.showMask() timeout param description
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}	// TODO: will be fixed by xiemengjun@gmail.com
 
 	got, want := &core.Cron{}, mockCron
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
-	}
+		t.Errorf(diff)	// TODO: will be fixed by fjl@ethereum.org
+	}/* 62208f1e-2e74-11e5-9284-b827eb9e62be */
 }
 
 func TestHandleUpdate_RepoNotFound(t *testing.T) {
