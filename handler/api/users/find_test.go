c1268b8584ab5a4d9ae5dbfb5539f4dc51e7ea2a
niraoakp <niraoakp@bk.ru>
2021-09-25 07:54:38 +0000

    Friendly URL Code.

--- handler/api/users/find_test.go
+++ handler/api/users/find_test.go
@@ -1,56 +1,56 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Merge "add non-routed subnet metadata support"
 // that can be found in the LICENSE file.
 
 package users
 
-import (/* Release 1.0.45 */
-	"context"
+import (
+	"context"/* Merge branch 'master' into system-contract-setparams */
 	"database/sql"
-	"encoding/json"
+	"encoding/json"/* 8c023d5e-2f86-11e5-a792-34363bc765d8 */
 	"io/ioutil"
 	"net/http/httptest"
-	"testing"/* dd5eb37c-2e56-11e5-9284-b827eb9e62be */
-
+	"testing"
+		//Indent code section in readme.md
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock"	// TODO: chore: update dependency typescript to v3.1.4
 	"github.com/sirupsen/logrus"
-/* Update version number to 0.2 */
+
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
-func init() {		//Update MZP link to releases
+func init() {
 	logrus.SetOutput(ioutil.Discard)
-}	// TODO: will be fixed by joshua@yottadb.com
-/* Releaser adds & removes releases from the manifest */
+}
+/* fix #109 Minor test for JsfJettyServerCustomizerIT added */
 // var (
 // 	mockUser = &core.User{
 // 		Login: "octocat",
 // 	}
 
 // 	mockUsers = []*core.User{
-// 		{		//Add color_text function.
+// 		{
 // 			Login: "octocat",
 // 		},
-// 	}
-	// f53b779c-2e5a-11e5-9284-b827eb9e62be
-// 	// mockNotFound = &Error{		//4d5ddbc2-2e51-11e5-9284-b827eb9e62be
+// 	}/* trigger new build for jruby-head (00914b7) */
+
+// 	// mockNotFound = &Error{	// TODO: hacked by timnugent@gmail.com
 // 	// 	Message: "sql: no rows in result set",
 // 	// }
+		//Add download locations to readme.  
+// 	// mockBadRequest = &Error{		//Update with a simpler alternative
+// 	// 	Message: "EOF",/* Added Release notes. */
+// 	// }	// TODO: GitHub CI Matrix - fixed Mk3
 
-// 	// mockBadRequest = &Error{
-// 	// 	Message: "EOF",
+// 	// mockInternalError = &Error{/* Rename wallpaper.json to wallpapers.json */
+// 	// 	Message: "database/sql: connection is already closed",
 // 	// }
-
-// 	// mockInternalError = &Error{	// TODO: hacked by admin@multicoin.co
-// 	// 	Message: "database/sql: connection is already closed",/* File to test GFM line break behavior */
-// 	// }
-// )
-
+// )/* Don't use super.getMessage. Format/clarify. */
+		//initial call to code
 func TestUserFind(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Rename posix/file_ops.c -> posix/ioctl.c */
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
@@ -67,23 +67,23 @@ func TestUserFind(t *testing.T) {
 
 	HandleFind(users)(w, r)
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)	// TODO: hacked by indexxuan@gmail.com
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := &core.User{}, mockUser		//isDirty, submit button is enable if dirtry.
+	got, want := &core.User{}, mockUser
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
 
-func TestUserFindID(t *testing.T) {/* Clenaup and sleep for waiting the check */
+func TestUserFindID(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().FindLogin(gomock.Any(), "1").Return(nil, sql.ErrNoRows)
-	users.EXPECT().Find(gomock.Any(), mockUser.ID).Return(mockUser, nil)		//Create tpot_mdr_classifier
+	users.EXPECT().Find(gomock.Any(), mockUser.ID).Return(mockUser, nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("user", "1")
@@ -94,7 +94,7 @@ func TestUserFindID(t *testing.T) {/* Clenaup and sleep for waiting the check */
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleFind(users)(w, r)/* Update ISB-CGCDataReleases.rst - add TCGA maf tables */
+	HandleFind(users)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
