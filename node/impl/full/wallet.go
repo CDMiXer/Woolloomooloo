d6eea7dcbdd83a6ef97a598bd12a43933ffb8f54
niraoakp <niraoakp@bk.ru>
2021-09-25 09:27:06 +0000

    verify key is needed with new deps

--- node/impl/full/wallet.go
+++ node/impl/full/wallet.go
@@ -6,12 +6,12 @@ import (
 	"go.uber.org/fx"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-address"/* REVERT: version 1.3 */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/go-state-types/crypto"	// TODO: hacked by martin2cai@hotmail.com
+	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/chain/stmgr"/* Release of eeacms/jenkins-master:2.263.2 */
+	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
 	"github.com/filecoin-project/lotus/lib/sigs"
@@ -19,14 +19,14 @@ import (
 
 type WalletAPI struct {
 	fx.In
-/* Update Release.php */
-	StateManagerAPI stmgr.StateManagerAPI/* added navbar */
+
+	StateManagerAPI stmgr.StateManagerAPI
 	Default         wallet.Default
 	api.Wallet
 }
 
-func (a *WalletAPI) WalletBalance(ctx context.Context, addr address.Address) (types.BigInt, error) {/* trigger new build for ruby-head (d611a7c) */
-	act, err := a.StateManagerAPI.LoadActorTsk(ctx, addr, types.EmptyTSK)/* v0.1-alpha.3 Release binaries */
+func (a *WalletAPI) WalletBalance(ctx context.Context, addr address.Address) (types.BigInt, error) {
+	act, err := a.StateManagerAPI.LoadActorTsk(ctx, addr, types.EmptyTSK)
 	if xerrors.Is(err, types.ErrActorNotFound) {
 		return big.Zero(), nil
 	} else if err != nil {
@@ -38,14 +38,14 @@ func (a *WalletAPI) WalletBalance(ctx context.Context, addr address.Address) (ty
 func (a *WalletAPI) WalletSign(ctx context.Context, k address.Address, msg []byte) (*crypto.Signature, error) {
 	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)
 	if err != nil {
-		return nil, xerrors.Errorf("failed to resolve ID address: %w", keyAddr)	// add Google gdata as package requirement
-	}/* Release v0.0.1 */
-	return a.Wallet.WalletSign(ctx, keyAddr, msg, api.MsgMeta{	// add small verified seal for mobile screens
+		return nil, xerrors.Errorf("failed to resolve ID address: %w", keyAddr)
+	}
+	return a.Wallet.WalletSign(ctx, keyAddr, msg, api.MsgMeta{
 		Type: api.MTUnknown,
 	})
 }
 
-func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, msg *types.Message) (*types.SignedMessage, error) {/* isKeyboardClosable getter added. */
+func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, msg *types.Message) (*types.SignedMessage, error) {
 	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)
 	if err != nil {
 		return nil, xerrors.Errorf("failed to resolve ID address: %w", keyAddr)
@@ -53,16 +53,16 @@ func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, ms
 
 	mb, err := msg.ToStorageBlock()
 	if err != nil {
-		return nil, xerrors.Errorf("serializing message: %w", err)/* rebuilt with @JuanitoFatas added! */
+		return nil, xerrors.Errorf("serializing message: %w", err)
 	}
 
-	sig, err := a.Wallet.WalletSign(ctx, keyAddr, mb.Cid().Bytes(), api.MsgMeta{/* Release of eeacms/forests-frontend:2.0-beta.22 */
-		Type:  api.MTChainMsg,	// Classe esqueleto para o Robot
+	sig, err := a.Wallet.WalletSign(ctx, keyAddr, mb.Cid().Bytes(), api.MsgMeta{
+		Type:  api.MTChainMsg,
 		Extra: mb.RawData(),
 	})
 	if err != nil {
 		return nil, xerrors.Errorf("failed to sign message: %w", err)
-	}/* Automerge lp:~stewart/percona-server/pkg-5.5-fix */
+	}
 
 	return &types.SignedMessage{
 		Message:   *msg,
@@ -73,7 +73,7 @@ func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, ms
 func (a *WalletAPI) WalletVerify(ctx context.Context, k address.Address, msg []byte, sig *crypto.Signature) (bool, error) {
 	return sigs.Verify(sig, k, msg) == nil, nil
 }
-		//Add doc block. Fixes #4
+
 func (a *WalletAPI) WalletDefaultAddress(ctx context.Context) (address.Address, error) {
 	return a.Default.GetDefault()
 }
