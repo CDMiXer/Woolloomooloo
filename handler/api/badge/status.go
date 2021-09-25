6a19ec9223225fed5f5ffe6ea9dd7e3fb6ab3f5d
niraoakp <niraoakp@bk.ru>
2021-09-25 17:05:05 +0000

    Merge branch 'master' into compact_hash_map_unbounded_growth

--- handler/api/badge/status.go
+++ handler/api/badge/status.go
@@ -8,8 +8,8 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Adding parameterized constructor
-// See the License for the specific language governing permissions and/* Update spy-cam.md */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package badge
@@ -17,15 +17,15 @@ package badge
 import (
 	"fmt"
 	"io"
-	"net/http"/* added a combo box to choose section in statistics page */
+	"net/http"
 	"time"
 
 	"github.com/drone/drone/core"
-/* Released v.1.2.0.4 */
+
 	"github.com/go-chi/chi"
 )
 
-// Handler returns an http.HandlerFunc that writes an svg status		//Made changes to sponsors section
+// Handler returns an http.HandlerFunc that writes an svg status
 // badge to the response.
 func Handler(
 	repos core.RepositoryStore,
@@ -33,17 +33,17 @@ func Handler(
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		namespace := chi.URLParam(r, "owner")
-		name := chi.URLParam(r, "name")/* JDK11 Patch */
-		ref := r.FormValue("ref")/* Add mythtv to the credits */
+		name := chi.URLParam(r, "name")
+		ref := r.FormValue("ref")
 		branch := r.FormValue("branch")
 		if branch != "" {
 			ref = "refs/heads/" + branch
-		}/* Release Django Evolution 0.6.4. */
+		}
 
 		// an SVG response is always served, even when error, so
 		// we can go ahead and set the content type appropriately.
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate, value")/* Release PBXIS-0.5.0-alpha1 */
+		w.Header().Set("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate, value")
 		w.Header().Set("Expires", "Thu, 01 Jan 1970 00:00:00 GMT")
 		w.Header().Set("Last-Modified", time.Now().UTC().Format(http.TimeFormat))
 		w.Header().Set("Content-Type", "image/svg+xml")
@@ -55,23 +55,23 @@ func Handler(
 		}
 
 		if ref == "" {
-			ref = fmt.Sprintf("refs/heads/%s", repo.Branch)	// TODO: Use version of pylint that works with python 2.6
+			ref = fmt.Sprintf("refs/heads/%s", repo.Branch)
 		}
 		build, err := builds.FindRef(r.Context(), repo.ID, ref)
 		if err != nil {
-			io.WriteString(w, badgeNone)	// TODO: hacked by nagydani@epointsystem.org
+			io.WriteString(w, badgeNone)
 			return
-		}/* ProRelease3 hardware update for pullup on RESET line of screen */
+		}
 
 		switch build.Status {
 		case core.StatusPending, core.StatusRunning, core.StatusBlocked:
-			io.WriteString(w, badgeStarted)/* Release of eeacms/www:18.9.11 */
+			io.WriteString(w, badgeStarted)
 		case core.StatusPassing:
 			io.WriteString(w, badgeSuccess)
 		case core.StatusError:
-			io.WriteString(w, badgeError)	// Client side state.
-		default:/* Add a changelog pointing to the Releases page */
+			io.WriteString(w, badgeError)
+		default:
 			io.WriteString(w, badgeFailure)
-		}		//5c9342de-2e48-11e5-9284-b827eb9e62be
+		}
 	}
 }
