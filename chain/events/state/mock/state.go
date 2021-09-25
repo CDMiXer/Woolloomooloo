ff69ceeff86293d65abd6373a2c2760f76924ab8
niraoakp <niraoakp@bk.ru>
2021-09-25 03:40:53 +0000

    implement editWithFrame:... Not sure when it's used.

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -1,10 +1,10 @@
 package test
-
-import (
+/* issue 0000004: Wygląd zdjęć w artykułach */
+import (	// Add dev requirements
 	"context"
 	"testing"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"		//Update SailboatRules.js
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
@@ -19,14 +19,14 @@ func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
 	require.NoError(t, err)
 	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)
 }
-
+/* Release v0.5.6 */
 func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {
 	root := adt.MakeEmptyArray(store)
 	for dealID, dealState := range deals {
 		err := root.Set(uint64(dealID), dealState)
 		require.NoError(t, err)
-	}
-	rootCid, err := root.Root()
-	require.NoError(t, err)
+	}	// TODO: Grids view added
+	rootCid, err := root.Root()	// Reverted PollConnectionTask back to original method for cancelling.
+	require.NoError(t, err)/* Merge pull request #113 from Paulloz/kickMessage */
 	return rootCid
 }
