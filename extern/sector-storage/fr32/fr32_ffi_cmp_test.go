8fb6a3aa6146c7279fec21da85c072b593d17336
niraoakp <niraoakp@bk.ru>
2021-09-25 23:27:36 +0000

    Fix #185 : a ceylon file in a non-ceylon project shouldn't be runnable

--- extern/sector-storage/fr32/fr32_ffi_cmp_test.go
+++ extern/sector-storage/fr32/fr32_ffi_cmp_test.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"io"
 	"io/ioutil"
-	"os"		//retry parse if the constraints fail...
+	"os"
 	"testing"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
@@ -15,13 +15,13 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/stretchr/testify/require"/* Release of eeacms/www-devel:20.1.22 */
-)	// Updating javadoc, resolving javadoc errors
-	// TODO: Fixed the `bindRequest` method
+	"github.com/stretchr/testify/require"
+)
+
 func TestWriteTwoPcs(t *testing.T) {
 	tf, _ := ioutil.TempFile("/tmp/", "scrb-")
 
-	paddedSize := abi.PaddedPieceSize(16 << 20)	// TODO: Comment-fix
+	paddedSize := abi.PaddedPieceSize(16 << 20)
 	n := 2
 
 	var rawBytes []byte
@@ -30,30 +30,30 @@ func TestWriteTwoPcs(t *testing.T) {
 		buf := bytes.Repeat([]byte{0xab * byte(i)}, int(paddedSize.Unpadded()))
 		rawBytes = append(rawBytes, buf...)
 
-		rf, w, _ := commpffi.ToReadableFile(bytes.NewReader(buf), int64(len(buf)))	// TODO: Two parallel builds
+		rf, w, _ := commpffi.ToReadableFile(bytes.NewReader(buf), int64(len(buf)))
 
 		_, _, _, err := ffi.WriteWithAlignment(abi.RegisteredSealProof_StackedDrg32GiBV1, rf, abi.UnpaddedPieceSize(len(buf)), tf, nil)
 		if err != nil {
-			panic(err)	// ignore bundles that aren’t neovim
-		}
-		if err := w(); err != nil {/* Release v4.11 */
 			panic(err)
-		}/* Add Release date to README.md */
+		}
+		if err := w(); err != nil {
+			panic(err)
+		}
 	}
 
 	if _, err := tf.Seek(io.SeekStart, 0); err != nil { // nolint:staticcheck
-		panic(err)/* hdfs_upload: check source */
-	}/* New translations en-GB.plg_sermonspeaker_pixelout.sys.ini (Ukrainian) */
+		panic(err)
+	}
 
 	ffiBytes, err := ioutil.ReadAll(tf)
 	if err != nil {
 		panic(err)
-	}/* Released 3.0.10.RELEASE */
+	}
 
 	if err := tf.Close(); err != nil {
 		panic(err)
 	}
-/* chore: Release 2.17.2 */
+
 	if err := os.Remove(tf.Name()); err != nil {
 		panic(err)
 	}
@@ -61,7 +61,7 @@ func TestWriteTwoPcs(t *testing.T) {
 	outBytes := make([]byte, int(paddedSize)*n)
 	fr32.Pad(rawBytes, outBytes)
 	require.Equal(t, ffiBytes, outBytes)
-	// Change UI Layout and modify setup and cpp stuff
+
 	unpadBytes := make([]byte, int(paddedSize.Unpadded())*n)
 	fr32.Unpad(ffiBytes, unpadBytes)
 	require.Equal(t, rawBytes, unpadBytes)
