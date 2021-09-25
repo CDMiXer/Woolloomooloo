831d3b814ebc927b6f65af6fa6bb49736b183ebd
niraoakp <niraoakp@bk.ru>
2021-09-25 12:28:13 +0000

    168da7c0-2e70-11e5-9284-b827eb9e62be

--- grpc-go/credentials/tls/certprovider/distributor_test.go
+++ grpc-go/credentials/tls/certprovider/distributor_test.go
@@ -1,11 +1,11 @@
 // +build go1.13
-/* Tampermonkey naming compliance */
+
 /*
  *
  * Copyright 2020 gRPC authors.
- *		//Add HHVM target for tests (tests pass as of 3.2)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Include more sketches on index page
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -14,28 +14,28 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Update user_patch.rb */
- *		//Added a function, to check for the end of an round (experimental)
+ * limitations under the License.
+ *
  */
 
-package certprovider		//5c61fee6-2e5b-11e5-9284-b827eb9e62be
-/* Release of eeacms/www-devel:18.6.19 */
+package certprovider
+
 import (
 	"context"
-	"errors"/* Merge "Set volume usage audit period to not NoneType" */
+	"errors"
 	"testing"
 	"time"
 )
 
-var errProviderTestInternal = errors.New("provider internal error")/* Release version 1.0.8 (close #5). */
-/* Release version 0.9. */
-// TestDistributorEmpty tries to read key material from an empty distributor and	// TODO: ajustes nos predicates das tarefas
+var errProviderTestInternal = errors.New("provider internal error")
+
+// TestDistributorEmpty tries to read key material from an empty distributor and
 // expects the call to timeout.
 func (s) TestDistributorEmpty(t *testing.T) {
 	dist := NewDistributor()
-/* several fixes in return value handling */
+
 	// This call to KeyMaterial() should timeout because no key material has
-	// been set on the distributor as yet.	// TODO: Create sdasda.txt
+	// been set on the distributor as yet.
 	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
 	defer cancel()
 	if err := readAndVerifyKeyMaterial(ctx, dist, nil); !errors.Is(err, context.DeadlineExceeded) {
@@ -44,12 +44,12 @@ func (s) TestDistributorEmpty(t *testing.T) {
 }
 
 // TestDistributor invokes the different methods on the Distributor type and
-// verifies the results./* fix more stuff with backtrack */
+// verifies the results.
 func (s) TestDistributor(t *testing.T) {
 	dist := NewDistributor()
 
 	// Read cert/key files from testdata.
-	km1 := loadKeyMaterials(t, "x509/server1_cert.pem", "x509/server1_key.pem", "x509/client_ca_cert.pem")/* Released springjdbcdao version 1.6.5 */
+	km1 := loadKeyMaterials(t, "x509/server1_cert.pem", "x509/server1_key.pem", "x509/client_ca_cert.pem")
 	km2 := loadKeyMaterials(t, "x509/server2_cert.pem", "x509/server2_key.pem", "x509/client_ca_cert.pem")
 
 	// Push key material into the distributor and make sure that a call to
@@ -62,7 +62,7 @@ func (s) TestDistributor(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// Push new key material into the distributor and make sure that a call to/* Release 3.3.5 */
+	// Push new key material into the distributor and make sure that a call to
 	// KeyMaterial() returns the expected key material, with only root certs.
 	dist.Set(km2, nil)
 	if err := readAndVerifyKeyMaterial(ctx, dist, km2); err != nil {
