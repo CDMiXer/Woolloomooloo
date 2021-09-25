8d4d076c67816a28d1b1635963b620dc320cdafd
niraoakp <niraoakp@bk.ru>
2021-09-25 09:06:05 +0000

    fix #9: attributes outside rng:data

--- store/shared/migrate/sqlite/files/011_add_column_builds_cron.sql
+++ store/shared/migrate/sqlite/files/011_add_column_builds_cron.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-cron		//fix(package): remove yarn.lock
-	// TODO: added (preprocessing) processors for complex data objects
+-- name: alter-table-builds-add-column-cron
+
 ALTER TABLE builds ADD COLUMN build_cron TEXT NOT NULL DEFAULT '';
