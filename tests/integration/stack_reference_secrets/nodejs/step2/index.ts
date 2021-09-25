fae65c6fff8138414ddc36b1b4627ebdadff8b74
niraoakp <niraoakp@bk.ru>
2021-09-25 17:43:06 +0000

    Create NormalizeVector.java

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -1,14 +1,14 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* use parsers database */
 
-export const normal = pulumi.output("normal");/* update chat demo */
+export const normal = pulumi.output("normal");	// TODO: hacked by hugomrdias@gmail.com
 export const secret = pulumi.secret("secret");
-		//Fix generation of CHM name for release candidates.
+
 // Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
 // deployment.
 const org = new pulumi.Config().require("org");
 const project = pulumi.getProject();
 const stack = pulumi.getStack();
-const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
-		//merged [19626] to UOS 2.1
-export const refNormal = sr.getOutput("normal");
-export const refSecret = sr.getOutput("secret");	// TODO: will be fixed by remco@dutchcoders.io
+const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);		//Change logging for TestResult initialization
+/* Removed the need for profiles */
+export const refNormal = sr.getOutput("normal");/* Merge "wlan: Release 3.2.3.89" */
+export const refSecret = sr.getOutput("secret");
