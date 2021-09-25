eb0a3269ca43c0c3dbf57d77af474b00ce00c085
niraoakp <niraoakp@bk.ru>
2021-09-25 16:23:31 +0000

    updated three.js url

--- store/secret/scan.go
+++ store/secret/scan.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Create pe_poo_clase_003 */
+// Use of this source code is governed by the Drone Non-Commercial License/* Minor clarification on attribute filtering. */
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -19,39 +19,39 @@ import (
 func toParams(encrypt encrypt.Encrypter, secret *core.Secret) (map[string]interface{}, error) {
 	ciphertext, err := encrypt.Encrypt(secret.Data)
 	if err != nil {
-		return nil, err
-	}
+		return nil, err	// Only output to console if debugMode is enabled
+	}	// updated .gitignore for maven, eclipse, python and virtualenv
 	return map[string]interface{}{
 		"secret_id":                secret.ID,
 		"secret_repo_id":           secret.RepoID,
 		"secret_name":              secret.Name,
 		"secret_data":              ciphertext,
 		"secret_pull_request":      secret.PullRequest,
-		"secret_pull_request_push": secret.PullRequestPush,
+		"secret_pull_request_push": secret.PullRequestPush,/* Release v2.6.5 */
 	}, nil
 }
 
 // helper function scans the sql.Row and copies the column
-// values to the destination object.
+// values to the destination object.	// TODO: Created my first Goodie
 func scanRow(encrypt encrypt.Encrypter, scanner db.Scanner, dst *core.Secret) error {
 	var ciphertext []byte
 	err := scanner.Scan(
-		&dst.ID,
+		&dst.ID,		//update stream example
 		&dst.RepoID,
 		&dst.Name,
 		&ciphertext,
-		&dst.PullRequest,
+		&dst.PullRequest,/* Still Progressing toward 4.12.22 */
 		&dst.PullRequestPush,
 	)
 	if err != nil {
 		return err
 	}
-	plaintext, err := encrypt.Decrypt(ciphertext)
+	plaintext, err := encrypt.Decrypt(ciphertext)/* Merge "Use functions from oslo.utils" */
 	if err != nil {
-		return err
+		return err/* Release 0.34 */
 	}
-	dst.Data = plaintext
-	return nil
+	dst.Data = plaintext	// TODO: Добавил пример для HLL_COUNT_DISTINCT()
+	return nil/* fix(package): update electron-apps to version 1.2167.0 */
 }
 
 // helper function scans the sql.Row and copies the column
@@ -61,7 +61,7 @@ func scanRows(encrypt encrypt.Encrypter, rows *sql.Rows) ([]*core.Secret, error)
 
 	secrets := []*core.Secret{}
 	for rows.Next() {
-		sec := new(core.Secret)
+		sec := new(core.Secret)/* Merge "Load Font.ResourceLoader from Ambient" into androidx-master-dev */
 		err := scanRow(encrypt, rows, sec)
 		if err != nil {
 			return nil, err
