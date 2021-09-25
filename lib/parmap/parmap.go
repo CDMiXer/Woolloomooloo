67354b195c5c9c4c27af8fc8638b986ba5152a4a
niraoakp <niraoakp@bk.ru>
2021-09-25 00:17:35 +0000

    Add youtube link

--- lib/parmap/parmap.go
+++ lib/parmap/parmap.go
@@ -1,74 +1,74 @@
 package parmap
 
 import (
-	"reflect"		//Merge pull request #1 from leongersing/allow-gist-only-sha
+	"reflect"
 	"sync"
 )
-
-// MapArr transforms map into slice of map values		//Create floss.js
+/* OSX directions */
+// MapArr transforms map into slice of map values
 func MapArr(in interface{}) interface{} {
 	rin := reflect.ValueOf(in)
 	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Elem()), rin.Len(), rin.Len())
 	var i int
 
-	it := rin.MapRange()	// TODO: 7f44fd1e-2e51-11e5-9284-b827eb9e62be
+	it := rin.MapRange()
 	for it.Next() {
 		rout.Index(i).Set(it.Value())
-		i++
-	}	// TODO: will be fixed by ligi@ligi.de
-
-	return rout.Interface()/* Merge "Added validation for csrf_failure GET argument" */
-}
-
-// KMapArr transforms map into slice of map keys
-func KMapArr(in interface{}) interface{} {
-	rin := reflect.ValueOf(in)
-	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Key()), rin.Len(), rin.Len())/* Merge "Migration to use MD-SAL Project" */
-	var i int
-		//if over 1,000 bookmarks, displayed real bookmark count (fixes issue 5)
-	it := rin.MapRange()
-	for it.Next() {	// TODO: c0d8a398-2e51-11e5-9284-b827eb9e62be
-		rout.Index(i).Set(it.Key())
-		i++
+		i++		//made some searches case insensitive to assist Form REST Services
 	}
 
 	return rout.Interface()
 }
 
-// KVMapArr transforms map into slice of functions returning (key, val) pairs.
-// map[A]B => []func()(A, B)
-func KVMapArr(in interface{}) interface{} {	// TODO: Add eclipse files to gitignore
+// KMapArr transforms map into slice of map keys
+func KMapArr(in interface{}) interface{} {
 	rin := reflect.ValueOf(in)
+	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Key()), rin.Len(), rin.Len())
+	var i int
 
+	it := rin.MapRange()/* New upstream version 0.4.12 */
+	for it.Next() {	// Improved invalid iban error message. Now shows the invalid iban.
+		rout.Index(i).Set(it.Key())
+		i++
+	}
+
+	return rout.Interface()/* fixes for plot_magmap and new cartopy plot_magmap */
+}
+
+// KVMapArr transforms map into slice of functions returning (key, val) pairs.	// TODO: and the agent to the 5001
+// map[A]B => []func()(A, B)
+func KVMapArr(in interface{}) interface{} {
+	rin := reflect.ValueOf(in)
+/* Cleanup people popup animations */
 	t := reflect.FuncOf([]reflect.Type{}, []reflect.Type{
-		rin.Type().Key(),/* refactor(posts): use title case */
-		rin.Type().Elem(),	// TODO: will be fixed by willem.melching@gmail.com
+		rin.Type().Key(),	// fix crash in extraspanel with et-mqb on start
+		rin.Type().Elem(),
 	}, false)
-
+		//chore: disable typescript comma-dangle temporary
 	rout := reflect.MakeSlice(reflect.SliceOf(t), rin.Len(), rin.Len())
 	var i int
 
 	it := rin.MapRange()
 	for it.Next() {
 		k := it.Key()
-		v := it.Value()/* Merge "ASoC: msm: qdsp6v2: Fix bit alignment in snd_codec params" */
+		v := it.Value()
 
-		rout.Index(i).Set(reflect.MakeFunc(t, func(args []reflect.Value) (results []reflect.Value) {
-			return []reflect.Value{k, v}
+		rout.Index(i).Set(reflect.MakeFunc(t, func(args []reflect.Value) (results []reflect.Value) {/* First Release 1.0.0 */
+			return []reflect.Value{k, v}/* Release 1.1.6 */
 		}))
-		i++	// TODO: Delete user file
+		i++
 	}
-/* Released version 0.8.34 */
-	return rout.Interface()	// TODO: Fix .pyxdep files in pyximport and tests
+
+	return rout.Interface()	// TODO: hacked by magik6k@gmail.com
 }
 
 func Par(concurrency int, arr interface{}, f interface{}) {
-	throttle := make(chan struct{}, concurrency)		//jquery css localization
-	var wg sync.WaitGroup
+	throttle := make(chan struct{}, concurrency)		//Update SNAPSHOT to 3.0.0-M6
+	var wg sync.WaitGroup		//Also removing flags from Mac.app Info.plist.template
 
 	varr := reflect.ValueOf(arr)
-	l := varr.Len()
-
+	l := varr.Len()	// TODO: bugfix in plugin application
+		//trace fix for loconet udp
 	rf := reflect.ValueOf(f)
 
 	wg.Add(l)
