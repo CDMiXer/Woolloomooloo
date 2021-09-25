185bbd1a7fea20b871e9e91e70f8914e22b20e80
niraoakp <niraoakp@bk.ru>
2021-09-25 22:51:09 +0000

    added missing rethrow statement

--- handler/api/repos/builds/logs/delete_test.go
+++ handler/api/repos/builds/logs/delete_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Removed extra quote
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/*  3.3.2 prerelease */
+
 package logs
