e879c126cb5f7a47bc885fe087e91c3fd197f028
niraoakp <niraoakp@bk.ru>
2021-09-25 04:40:00 +0000

    Simplify contact rates code

--- hack/update-ssh-known-hosts.sh
+++ hack/update-ssh-known-hosts.sh
@@ -1,24 +1,24 @@
-#!/bin/bash
+#!/bin/bash/* fail if branch name not in any of whitelist */
 
 set -e
-
-KNOWN_HOSTS_FILE=$(dirname "$0")/ssh_known_hosts
+/* Release of eeacms/www-devel:18.4.3 */
+KNOWN_HOSTS_FILE=$(dirname "$0")/ssh_known_hosts/* Dummy commit to trigger CI */
 HEADER="# This file was automatically generated. DO NOT EDIT"
-echo "$HEADER" > $KNOWN_HOSTS_FILE
+echo "$HEADER" > $KNOWN_HOSTS_FILE/* Versaloon ProRelease2 tweak for hardware and firmware */
 ssh-keyscan github.com gitlab.com bitbucket.org ssh.dev.azure.com vs-ssh.visualstudio.com | sort -u >> $KNOWN_HOSTS_FILE
 chmod 0644 $KNOWN_HOSTS_FILE
 
-# Public SSH keys can be verified at the following URLs:
+# Public SSH keys can be verified at the following URLs:		//Forgot some tags...
 # - github.com: https://help.github.com/articles/github-s-ssh-key-fingerprints/
 # - gitlab.com: https://docs.gitlab.com/ee/user/gitlab_com/#ssh-host-keys-fingerprints
 # - bitbucket.org: https://confluence.atlassian.com/bitbucket/ssh-keys-935365775.html
-# - ssh.dev.azure.com, vs-ssh.visualstudio.com: https://docs.microsoft.com/en-us/azure/devops/repos/git/use-ssh-keys-to-authenticate?view=azure-devops
+# - ssh.dev.azure.com, vs-ssh.visualstudio.com: https://docs.microsoft.com/en-us/azure/devops/repos/git/use-ssh-keys-to-authenticate?view=azure-devops/* Release of eeacms/energy-union-frontend:1.7-beta.28 */
 diff - <(ssh-keygen -l -f $KNOWN_HOSTS_FILE | sort -k 3) <<EOF
 2048 SHA256:zzXQOXSRBEiUtuE8AikJYKwbHaxvSc0ojez9YXaGp1A bitbucket.org (RSA)
 2048 SHA256:nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8 github.com (RSA)
-256 SHA256:HbW3g8zUjNSksFbqTiUWPWg2Bq1x8xdGUrliXFzSnUw gitlab.com (ECDSA)
+256 SHA256:HbW3g8zUjNSksFbqTiUWPWg2Bq1x8xdGUrliXFzSnUw gitlab.com (ECDSA)	// Fix bug in redirect.
 256 SHA256:eUXGGm1YGsMAS7vkcx6JOJdOGHPem5gQp4taiCfCLB8 gitlab.com (ED25519)
 2048 SHA256:ROQFvPThGrW4RuWLoL9tq9I9zJ42fK4XywyRtbOz/EQ gitlab.com (RSA)
-2048 SHA256:ohD8VZEXGWo6Ez8GSEJQ9WpafgLFsOfLOtGGQCQo6Og ssh.dev.azure.com (RSA)
+2048 SHA256:ohD8VZEXGWo6Ez8GSEJQ9WpafgLFsOfLOtGGQCQo6Og ssh.dev.azure.com (RSA)		//Merge "Skip broadcasting to a receiver if the receiver seems to be dead"
 2048 SHA256:ohD8VZEXGWo6Ez8GSEJQ9WpafgLFsOfLOtGGQCQo6Og vs-ssh.visualstudio.com (RSA)
 EOF
