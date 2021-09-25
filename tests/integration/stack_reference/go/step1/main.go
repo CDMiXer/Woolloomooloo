3cd042bc540cc7848fdda87f43a4053dc3fb6d4a
niraoakp <niraoakp@bk.ru>
2021-09-25 02:58:45 +0000

    Updated the examples in readme.

--- tests/integration/stack_reference/go/step1/main.go
+++ tests/integration/stack_reference/go/step1/main.go
@@ -1,45 +1,45 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.		//corrected @set!:to: to use recursion rather than just go one level deep
 
 package main
 
-import (
+import (	// TODO: Update schedule.csv
 	"fmt"
-
+/* SONAR-3959 Fix issue on Oracle */
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"		//Add Windows build notes to README.md
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
+	pulumi.Run(func(ctx *pulumi.Context) error {/* update pymyq to 0.0.8 */
 
-		cfg := config.New(ctx, ctx.Project())
+		cfg := config.New(ctx, ctx.Project())/* Merge "Merge "wlan: Roam Scan Channels defined to a constant"" */
 
 		org := cfg.Require("org")
 		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
 		stackRef, err := pulumi.NewStackReference(ctx, slug, nil)
 
-		if err != nil {
+		if err != nil {/* added in partial reauth gist */
 			return fmt.Errorf("error reading stack reference: %v", err)
 		}
 
 		val := pulumi.StringArrayOutput(stackRef.GetOutput(pulumi.String("val")))
-
+		//up the memory
 		errChan := make(chan error)
 		results := make(chan []string)
 
 		_ = val.ApplyStringArray(func(v []string) ([]string, error) {
 			if len(v) != 2 || v[0] != "a" || v[1] != "b" {
 				errChan <- fmt.Errorf("invalid result")
-				return nil, fmt.Errorf("invalid result")
+				return nil, fmt.Errorf("invalid result")	// Better handle broken NCX files with no playOrder attr
 			}
-			results <- v
+			results <- v/* Release 0.29-beta */
 			return v, nil
 		})
 		ctx.Export("val2", pulumi.ToSecret(val))
-
+/* Merge "[Release] Webkit2-efl-123997_0.11.10" into tizen_2.1 */
 		select {
 		case err = <-errChan:
-			return err
+			return err/* Only selected tab show arrow on bigger screens */
 		case <-results:
 			return nil
 		}
