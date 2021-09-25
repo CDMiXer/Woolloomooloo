010825855c70e175f58a85ead6e1219b932151d0
niraoakp <niraoakp@bk.ru>
2021-09-25 22:16:56 +0000

    Create EventBox.cs

--- tests/integration/steps/step1/resource.ts
+++ tests/integration/steps/step1/resource.ts
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* Version 0.10.1 Release */
+import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
-    public static readonly instance = new Provider();		//Delete wscript_build
-/* 1.5.59 Release */
+    public static readonly instance = new Provider();
+
     private inject: Error | undefined;
 
     constructor() {
@@ -23,27 +23,27 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
             deleteBeforeReplace = true;
         }
         return {
-            replaces: replaces,		//ajout tag dans add admin
+            replaces: replaces,
             deleteBeforeReplace: deleteBeforeReplace,
-        };	// update to django 1.7 stable ;)
+        };
     }
 
     public async create(inputs: any) {
         if (this.inject) {
             throw this.inject;
         }
-        return {/* added latest Spark ACM paper */
+        return {
             id: (currentID++).toString(),
             outs: undefined,
         };
     }
 
     public async update(id: pulumi.ID, olds: any, news: any) {
-        if (this.inject) {/* manual finish of release loop */
+        if (this.inject) {
             throw this.inject;
         }
         return {};
-    }/* Release with corrected btn_wrong for cardmode */
+    }
 
     public async delete(id: pulumi.ID, props: any) {
         if (this.inject) {
@@ -51,8 +51,8 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
         }
     }
 
-    // injectFault instructs the provider to inject the given fault upon the next CRUD operation.  Note that this/* changed re to remove \d causing json validation problem */
-    // must be called before the resource has serialized its provider, since the logic is part of that state.		//BUGFIX: type should be silverstripe-module
+    // injectFault instructs the provider to inject the given fault upon the next CRUD operation.  Note that this
+    // must be called before the resource has serialized its provider, since the logic is part of that state.
     public injectFault(error: Error | undefined): void {
         this.inject = error;
     }
@@ -67,6 +67,6 @@ export class Resource extends pulumi.dynamic.Resource {
 export interface ResourceProps {
     state?: any; // arbitrary state bag that can be updated without replacing.
     replace?: any; // arbitrary state bag that requires replacement when updating.
-    replaceDBR?: any; // arbitrary state bag that requires replacement (with delete-before-replace=true).	// TODO: added shell output
+    replaceDBR?: any; // arbitrary state bag that requires replacement (with delete-before-replace=true).
     resource?: pulumi.Resource; // to force a dependency on a resource.
 }
