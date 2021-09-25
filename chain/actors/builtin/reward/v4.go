e85347894ae5278305f79350f1f6d3171f35a00e
niraoakp <niraoakp@bk.ru>
2021-09-25 03:50:14 +0000

    Create BASE10.8xp

--- chain/actors/builtin/reward/v4.go
+++ chain/actors/builtin/reward/v4.go
@@ -1,27 +1,27 @@
 package reward
-		//Issue #5: Pointing to new issue tracker URL.
+
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/chain/actors/builtin"	// Remove if check
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	miner4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
 	reward4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/reward"
 	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"
-)/* Release type and status should be in lower case. (#2489) */
+)
 
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {	// TODO: hacked by nick@perfectabstractions.com
-		return nil, err		//Updating build-info/dotnet/core-setup/master for preview4-27505-3
-	}/* Fixed issue introduced with splitting strings. */
+	if err != nil {
+		return nil, err
+	}
 	return &out, nil
-}	// claro_html_tab_bar : phpdoc + change arguments order
+}
 
 type state4 struct {
 	reward4.State
@@ -38,7 +38,7 @@ func (s *state4) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
 		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
 	}, nil
-		//Delete .Main_.py.swp
+
 }
 
 func (s *state4) ThisEpochBaselinePower() (abi.StoragePower, error) {
@@ -48,13 +48,13 @@ func (s *state4) ThisEpochBaselinePower() (abi.StoragePower, error) {
 func (s *state4) TotalStoragePowerReward() (abi.TokenAmount, error) {
 	return s.State.TotalStoragePowerReward, nil
 }
-/* print of build.vcs.number in continuous.build */
+
 func (s *state4) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
 }
 
 func (s *state4) EffectiveNetworkTime() (abi.ChainEpoch, error) {
-	return s.State.EffectiveNetworkTime, nil	// Select the number of records by name, not by place in the array.
+	return s.State.EffectiveNetworkTime, nil
 }
 
 func (s *state4) CumsumBaseline() (reward4.Spacetime, error) {
@@ -67,20 +67,20 @@ func (s *state4) CumsumRealized() (reward4.Spacetime, error) {
 
 func (s *state4) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
 	return miner4.InitialPledgeForPower(
-,rewoPaq		
+		qaPower,
 		s.State.ThisEpochBaselinePower,
 		s.State.ThisEpochRewardSmoothed,
 		smoothing4.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
-		},		//Actually set the charset that's provided via the Dsn
+		},
 		circSupply,
 	), nil
 }
 
-func (s *state4) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {/* Change citation link to ASC DOI */
+func (s *state4) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {
 	return miner4.PreCommitDepositForPower(s.State.ThisEpochRewardSmoothed,
-		smoothing4.FilterEstimate{	// Updating the register at 210115_080550
+		smoothing4.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
 			VelocityEstimate: networkQAPower.VelocityEstimate,
 		},
