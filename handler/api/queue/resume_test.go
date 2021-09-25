4cb2a0274fd60e3990e11abc94a4e46daba16831
niraoakp <niraoakp@bk.ru>
2021-09-25 21:03:02 +0000

    Delete 133336849.json

--- handler/api/queue/resume_test.go
+++ handler/api/queue/resume_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-/* #6 - Release 0.2.0.RELEASE. */
-// +build !oss/* Release charm 0.12.0 */
+// that can be found in the LICENSE file./* Release v3.2-RC2 */
+
+// +build !oss
 
 package queue
