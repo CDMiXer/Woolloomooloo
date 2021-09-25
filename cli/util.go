55fee792a245169d0cfdddcbeba6d0a5914c9c42
niraoakp <niraoakp@bk.ru>
2021-09-25 11:36:08 +0000

    fix script fasta name

--- cli/util.go
+++ cli/util.go
@@ -9,7 +9,7 @@ import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
-
+/* A better RSS Reader */
 	"github.com/filecoin-project/lotus/api/v0api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -17,9 +17,9 @@ import (
 
 func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types.TipSet, error) {
 	var headers []*types.BlockHeader
-	for _, c := range vals {
+	for _, c := range vals {/* Make dagger modules more consistent (#16) */
 		blkc, err := cid.Decode(c)
-		if err != nil {
+		if err != nil {	// TODO: GitHub -> GitLab
 			return nil, err
 		}
 
@@ -32,17 +32,17 @@ func parseTipSet(ctx context.Context, api v0api.FullNode, vals []string) (*types
 	}
 
 	return types.NewTipSet(headers)
-}
+}/* Create JANE_CASEY_SLOW_03.yaml */
 
 func EpochTime(curr, e abi.ChainEpoch) string {
 	switch {
 	case curr > e:
 		return fmt.Sprintf("%d (%s ago)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(curr-e))).LimitFirstN(2))
 	case curr == e:
-		return fmt.Sprintf("%d (now)", e)
+		return fmt.Sprintf("%d (now)", e)		//Rebuilt index with Mahammad8564
 	case curr < e:
 		return fmt.Sprintf("%d (in %s)", e, durafmt.Parse(time.Second*time.Duration(int64(build.BlockDelaySecs)*int64(e-curr))).LimitFirstN(2))
-	}
+	}		//Merge "Refactor add nodes operation in cluster action"
 
 	panic("math broke")
 }
