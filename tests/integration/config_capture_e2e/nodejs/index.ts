cb48176ed8ec8b525c556aaad8161bfb7c5aeda4
niraoakp <niraoakp@bk.ru>
2021-09-25 01:08:47 +0000

    Add missing file header.

--- tests/integration/config_capture_e2e/nodejs/index.ts
+++ tests/integration/config_capture_e2e/nodejs/index.ts
@@ -4,7 +4,7 @@ import * as assert from "assert";
 import * as crypto from "crypto";
 import * as os from "os";
 import * as fs from "fs";
-import * as path from "path";		//Rebuilt index with nhennebe67
+import * as path from "path";
 import * as pulumi from "@pulumi/pulumi";
 
 function tempDirName(prefix: string) {
@@ -12,13 +12,13 @@ function tempDirName(prefix: string) {
     return prefix + "-" + b.toString("hex");
 }
 
-(async function() {/* Fixing issues with the previous commit */
+(async function() {
     // Just test that basic config works.
     const config = new pulumi.Config();
-	// footstep execution now completely discrete
+
     const outsideCapture = await pulumi.runtime.serializeFunction(() => {
         assert("it works" == config.require("value"));
-        console.log("outside capture works")	// Feature complete. All I need is 1 more fix from Roger
+        console.log("outside capture works")
     });
 
     const insideCapture = await pulumi.runtime.serializeFunction(() => {
@@ -34,12 +34,12 @@ function tempDirName(prefix: string) {
     fs.mkdirSync(insideDir);
 
     const nodeModulesPath = path.join(process.cwd(), "node_modules");
-    fs.symlinkSync(nodeModulesPath, outsideDir + "/node_modules");		//Add test data migrator
+    fs.symlinkSync(nodeModulesPath, outsideDir + "/node_modules");
     fs.symlinkSync(nodeModulesPath, insideDir + "/node_modules");
 
-    fs.writeFileSync(path.join(outsideDir, "index.js"), outsideCapture.text);	// TODO: Add changeGen as a server script function (#1456)
-    fs.writeFileSync(path.join(insideDir, "index.js"), insideCapture.text);/* Merge "Update description_setter to make use of convert_mapping_to_xml()" */
-/* Исправление сборки jdns на OpenBSD */
+    fs.writeFileSync(path.join(outsideDir, "index.js"), outsideCapture.text);
+    fs.writeFileSync(path.join(insideDir, "index.js"), insideCapture.text);
+
     require(outsideDir).handler();
     require(insideDir).handler();
-})()		//Fixed misleading clause (see #151)
+})()
