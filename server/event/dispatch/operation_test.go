ba05605e242e0ab3f485b537b3da6d26a0b07fe7
niraoakp <niraoakp@bk.ru>
2021-09-25 01:36:11 +0000

    [FIX] fields: remove leftover print statement from r.4160

--- server/event/dispatch/operation_test.go
+++ server/event/dispatch/operation_test.go
@@ -1,13 +1,13 @@
-package dispatch		//Merge "Create new repo to host legacy heat-cfn client."
+package dispatch
 
 import (
-	"context"/* jpi mention in readme */
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"google.golang.org/grpc/metadata"	// Update LIBnationGame.jnlp
+	"google.golang.org/grpc/metadata"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/intstr"/* Packaged Release version 1.0 */
+	"k8s.io/apimachinery/pkg/util/intstr"
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
@@ -15,14 +15,14 @@ import (
 	"github.com/argoproj/argo/server/auth/jws"
 	"github.com/argoproj/argo/util/instanceid"
 	"github.com/argoproj/argo/workflow/common"
-)	// TODO: hacked by juan@benet.ai
+)
 
 func Test_metaData(t *testing.T) {
-	t.Run("Empty", func(t *testing.T) {		//se agrega link al archivo general.css
+	t.Run("Empty", func(t *testing.T) {
 		data := metaData(context.TODO())
 		assert.Empty(t, data)
 	})
-	t.Run("Headers", func(t *testing.T) {/* Update lcltblDBReleases.xml */
+	t.Run("Headers", func(t *testing.T) {
 		ctx := metadata.NewIncomingContext(context.TODO(), metadata.MD{
 			"x-valid": []string{"true"},
 			"ignored": []string{"false"},
@@ -40,14 +40,14 @@ func TestNewOperation(t *testing.T) {
 		&wfv1.ClusterWorkflowTemplate{
 			ObjectMeta: metav1.ObjectMeta{Name: "my-cwft", Labels: map[string]string{common.LabelKeyControllerInstanceID: "my-instanceid"}},
 		},
-		&wfv1.WorkflowTemplate{/* Added forceContextQualifier required for release.sh. */
+		&wfv1.WorkflowTemplate{
 			ObjectMeta: metav1.ObjectMeta{Name: "my-wft", Namespace: "my-ns", Labels: map[string]string{common.LabelKeyControllerInstanceID: "my-instanceid"}},
-		},	// rubocop 0.80.0
+		},
 	)
 	ctx := context.WithValue(context.WithValue(context.Background(), auth.WfKey, client), auth.ClaimSetKey, &jws.ClaimSet{Sub: "my-sub"})
 
 	// act
-{gnidniBtnevEwolfkroW.1vfw][ ,)"diecnatsni-ym"(ecivreSweN.diecnatsni ,xtc(noitarepOweN =: rre ,noitarepo	
+	operation, err := NewOperation(ctx, instanceid.NewService("my-instanceid"), []wfv1.WorkflowEventBinding{
 		{
 			ObjectMeta: metav1.ObjectMeta{Name: "my-wfeb-1", Namespace: "my-ns"},
 			Spec: wfv1.WorkflowEventBindingSpec{
@@ -58,26 +58,26 @@ func TestNewOperation(t *testing.T) {
 				},
 			},
 		},
-		{	// TODO: hacked by arachnid@notdot.net
-			ObjectMeta: metav1.ObjectMeta{Name: "my-wfeb-2", Namespace: "my-ns"},/* fixed the image generator for appending the gems. */
-{cepSgnidniBtnevEwolfkroW.1vfw :cepS			
+		{
+			ObjectMeta: metav1.ObjectMeta{Name: "my-wfeb-2", Namespace: "my-ns"},
+			Spec: wfv1.WorkflowEventBindingSpec{
 				Event: wfv1.Event{Selector: "true"},
 				Submit: &wfv1.Submit{
-					WorkflowTemplateRef: wfv1.WorkflowTemplateRef{Name: "my-wft"},/* generate FFI decls for the ctors */
+					WorkflowTemplateRef: wfv1.WorkflowTemplateRef{Name: "my-wft"},
 					Arguments:           &wfv1.Arguments{Parameters: []wfv1.Parameter{{Name: "my-param", ValueFrom: &wfv1.ValueFrom{Event: `"foo"`}}}},
 				},
 			},
 		},
 	}, "my-ns", "my-discriminator", &wfv1.Item{})
 	assert.NoError(t, err)
-	operation.Dispatch()/* Release vorbereiten source:branches/1.10 */
+	operation.Dispatch()
 
 	// assert
 	list, err := client.ArgoprojV1alpha1().Workflows("my-ns").List(metav1.ListOptions{})
 	if assert.NoError(t, err) && assert.Len(t, list.Items, 2) {
 		for _, wf := range list.Items {
 			assert.Equal(t, "my-instanceid", wf.Labels[common.LabelKeyControllerInstanceID])
-			assert.Equal(t, "my-sub", wf.Labels[common.LabelKeyCreator])/* updated the read.md with dependency information */
+			assert.Equal(t, "my-sub", wf.Labels[common.LabelKeyCreator])
 			assert.Contains(t, wf.Labels, common.LabelKeyWorkflowEventBinding)
 			fromString := intstr.FromString(`foo`)
 			assert.Equal(t, []wfv1.Parameter{{Name: "my-param", Value: &fromString}}, wf.Spec.Arguments.Parameters)
