e4e4fbe934e6193ddedca765f7ee3b025ce06aac
niraoakp <niraoakp@bk.ru>
2021-09-25 09:24:08 +0000

    Delete wordpress-4.6.zip

--- plugin/converter/noop.go
+++ plugin/converter/noop.go
@@ -2,28 +2,28 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* fix the big bug about conflicts in plugins... */
-//	// TODO: New JAR, C# transformations switched off
+// You may obtain a copy of the License at/* Updated index.html with calculator */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//0a80cf6c-2e42-11e5-9284-b827eb9e62be
-// Unless required by applicable law or agreed to in writing, software/* rev 554406 */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Display the svn revision in the about dialog again. */
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Update template note in readme
 // limitations under the License.
 
-// +build oss
+// +build oss/* Release of eeacms/www-devel:21.4.17 */
 
 package converter
 
-import (/* Error Panel jsp added */
+import (
 	"context"
 
 	"github.com/drone/drone/core"
 )
-		//- Added competences description
+
 type noop struct{}
 
 func (noop) Convert(context.Context, *core.ConvertArgs) (*core.Config, error) {
-	return nil, nil
-}/* updating README to reflect name change. */
+	return nil, nil	// TODO: will be fixed by alan.shaw@protocol.ai
+}
