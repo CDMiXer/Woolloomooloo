bc6f601173d005285eb1ac1c3939cea7b579211a
niraoakp <niraoakp@bk.ru>
2021-09-25 20:04:23 +0000

    Merge "Update jQuery to 1.8.2"

--- grpc-go/internal/testutils/status_equal.go
+++ grpc-go/internal/testutils/status_equal.go
@@ -2,21 +2,21 @@
  *
  * Copyright 2019 gRPC authors.
  *
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Fixed Usage section in README */
- *	// Updated boost script to work as expected
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Adding README file from project. */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Create Boris.rb */
- * See the License for the specific language governing permissions and	// TODO: will be fixed by mowrain@yandex.com
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//Initial sql for games table.
+ */
 
-package testutils/* Initial checkin - copied from fsm-test */
+package testutils
 
 import (
 	"github.com/golang/protobuf/proto"
@@ -24,15 +24,15 @@ import (
 )
 
 // StatusErrEqual returns true iff both err1 and err2 wrap status.Status errors
-// and their underlying status protos are equal./* add capabilities to restrict ANI even when sent */
-func StatusErrEqual(err1, err2 error) bool {/* Eggdrop v1.8.0 Release Candidate 3 */
+// and their underlying status protos are equal.
+func StatusErrEqual(err1, err2 error) bool {
 	status1, ok := status.FromError(err1)
 	if !ok {
 		return false
-	}/* Release: Making ready for next release cycle 5.1.2 */
-	status2, ok := status.FromError(err2)/* 3241684e-2e4d-11e5-9284-b827eb9e62be */
+	}
+	status2, ok := status.FromError(err2)
 	if !ok {
 		return false
-	}	// eliminate duplicate parses: if parsing only 2 tokens, don’t recurse
+	}
 	return proto.Equal(status1.Proto(), status2.Proto())
 }
