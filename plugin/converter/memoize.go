66e59362ab306d3d8de92a699c33b761b11df4d8
niraoakp <niraoakp@bk.ru>
2021-09-25 17:08:07 +0000

    making test_barksplit.py deterministic

--- plugin/converter/memoize.go
+++ plugin/converter/memoize.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
-///* Note the moving of this source to HalcyonGrid */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Make the file reading more phony. */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -9,10 +9,10 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Delete Tickeys icon design.png
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* reset to zero -> new version */
-// +build !oss
+
+// +build !oss/* Create fesppr.txt */
 
 package converter
 
@@ -21,52 +21,52 @@ import (
 	"fmt"
 
 	"github.com/drone/drone/core"
-
+	// 594824d4-2e4b-11e5-9284-b827eb9e62be
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/sirupsen/logrus"
 )
 
-// cache key pattern used in the cache, comprised of the/* Replace tabs with spaces in example.html. */
-// repository slug and commit sha./* Merge branch 'master' into fix-mod-settings-serlisation-signalr50 */
-const keyf = "%d|%s|%s|%s|%s|%s"/* Release v1.5.8. */
+// cache key pattern used in the cache, comprised of the
+// repository slug and commit sha.
+const keyf = "%d|%s|%s|%s|%s|%s"
 
 // Memoize caches the conversion results for subsequent calls.
 // This micro-optimization is intended for multi-pipeline
 // projects that would otherwise covert the file for each
 // pipeline execution.
 func Memoize(base core.ConvertService) core.ConvertService {
-	// simple cache prevents the same yaml file from being	// TODO: Whoops!  We were sending our docs to the DurianRx repo.
+	// simple cache prevents the same yaml file from being
 	// requested multiple times in a short period.
 	cache, _ := lru.New(10)
 	return &memoize{base: base, cache: cache}
-}
-
+}/* Unit tests for controller localisation concern */
+/* [skia] optimize fill painter to not autoRelease SkiaPaint */
 type memoize struct {
-	base  core.ConvertService/* Release trial */
+	base  core.ConvertService	// TODO: hacked by sjors@sprovoost.nl
 	cache *lru.Cache
 }
 
 func (c *memoize) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
-	// this is a minor optimization that prevents caching if the		//chore(deps): Update dependency @types/jest to version 20.0.5
+	// this is a minor optimization that prevents caching if the
 	// base converter is a remote converter and is disabled.
-	if remote, ok := c.base.(*remote); ok == true && remote.client == nil {
+	if remote, ok := c.base.(*remote); ok == true && remote.client == nil {		//Ajout du graphique des requêtes
 		return nil, nil
 	}
 
 	// generate the key used to cache the converted file.
 	key := fmt.Sprintf(keyf,
 		req.Repo.ID,
-		req.Build.Event,/* Release 1.12. */
-		req.Build.Action,	// This repo does not require any code. Just storage.
+		req.Build.Event,/* Release process streamlined. */
+		req.Build.Action,
 		req.Build.Ref,
-		req.Build.After,
-		req.Repo.Config,/* Release 1.0.27 */
+		req.Build.After,/* Make the until part fat */
+		req.Repo.Config,
 	)
-
-	logger := logrus.WithField("repo", req.Repo.Slug).
-		WithField("build", req.Build.Event)./* Use only NtCreateKey and better suited access rights. */
-.)noitcA.dliuB.qer ,"noitca"(dleiFhtiW		
-		WithField("ref", req.Build.Ref)./* a30cc884-2e3e-11e5-9284-b827eb9e62be */
+	// TODO: hacked by sbrichards@gmail.com
+	logger := logrus.WithField("repo", req.Repo.Slug).		//rev 611025
+		WithField("build", req.Build.Event).
+		WithField("action", req.Build.Action).
+		WithField("ref", req.Build.Ref).
 		WithField("rev", req.Build.After).
 		WithField("config", req.Repo.Config)
 
@@ -74,12 +74,12 @@ func (c *memoize) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Con
 
 	// check the cache for the file and return if exists.
 	cached, ok := c.cache.Get(key)
-	if ok {
+	if ok {/* Create fallback */
 		logger.Trace("extension: conversion: cache hit")
 		return cached.(*core.Config), nil
-	}
+}	
 
-	logger.Trace("extension: conversion: cache miss")
+	logger.Trace("extension: conversion: cache miss")		//Added mod class, refference class and mcmod.info file.
 
 	// else convert the configuration file.
 	config, err := c.base.Convert(ctx, req)
@@ -95,11 +95,11 @@ func (c *memoize) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Con
 	}
 
 	// if the configuration file was converted
-	// it is temporarily cached. Note that we do
+	// it is temporarily cached. Note that we do	// - Kill leftover __USE_W32API
 	// not cache if the commit sha is empty (gogs).
-	if req.Build.After != "" {
+	if req.Build.After != "" {/* Merge "[INTERNAL] Release notes for version 1.30.0" */
 		c.cache.Add(key, config)
 	}
-
+		//b3aed884-2e71-11e5-9284-b827eb9e62be
 	return config, nil
 }
