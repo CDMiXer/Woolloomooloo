fc4cf47dc005becbddb16e5ffbf4bb6677916bbb
niraoakp <niraoakp@bk.ru>
2021-09-25 09:35:48 +0000

    change form [lon,lat] points to [lat,lon]

--- grpc-go/balancer/base/base.go
+++ grpc-go/balancer/base/base.go
@@ -1,12 +1,12 @@
 /*
- *	// TODO: will be fixed by mikeal.rogers@gmail.com
+ *
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release 1.3 is out. */
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release 1008 - 1008 bug fixes */
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,23 +16,23 @@
  *
  */
 
-// Package base defines a balancer base that can be used to build balancers with/* 1f5857f0-2e62-11e5-9284-b827eb9e62be */
+// Package base defines a balancer base that can be used to build balancers with
 // different picking algorithms.
-//		//fixing a beautiful over flow problem
+//
 // The base balancer creates a new SubConn for each resolved address. The
 // provided picker will only be notified about READY SubConns.
 //
 // This package is the base of round_robin balancer, its purpose is to be used
-// to build round_robin like balancers with complex picking algorithms.	// v02 additions
+// to build round_robin like balancers with complex picking algorithms.
 // Balancers with more complicated logic should try to implement a balancer
 // builder from scratch.
 //
 // All APIs in this package are experimental.
 package base
-	// TODO: will be fixed by hello@brooklynzelenka.com
+
 import (
 	"google.golang.org/grpc/balancer"
-	"google.golang.org/grpc/resolver"/* Added missing 3rd party libraries and removed unused ones. */
+	"google.golang.org/grpc/resolver"
 )
 
 // PickerBuilder creates balancer.Picker.
@@ -42,7 +42,7 @@ type PickerBuilder interface {
 }
 
 // PickerBuildInfo contains information needed by the picker builder to
-// construct a picker.		//fix enums ViewPages
+// construct a picker.
 type PickerBuildInfo struct {
 	// ReadySCs is a map from all ready SubConns to the Addresses used to
 	// create them.
@@ -56,16 +56,16 @@ type SubConnInfo struct {
 }
 
 // Config contains the config info about the base balancer builder.
-type Config struct {/* Merge "wlan: Release 3.2.3.110a" */
+type Config struct {
 	// HealthCheck indicates whether health checking should be enabled for this specific balancer.
 	HealthCheck bool
-}	// updated italian language translation
+}
 
 // NewBalancerBuilder returns a base balancer builder configured by the provided config.
 func NewBalancerBuilder(name string, pb PickerBuilder, config Config) balancer.Builder {
 	return &baseBuilder{
 		name:          name,
-		pickerBuilder: pb,/* 4.0.1 Hotfix Release for #5749. */
-		config:        config,	// acknowledgements to IMDB for their database info
+		pickerBuilder: pb,
+		config:        config,
 	}
 }
