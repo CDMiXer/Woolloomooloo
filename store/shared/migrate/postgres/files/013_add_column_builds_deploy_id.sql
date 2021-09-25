f29b1a47abdc982f0395981d90e6b78ac775e873
niraoakp <niraoakp@bk.ru>
2021-09-25 06:19:25 +0000

    [gui-components] bugfix: complete name of train is now translated

--- store/shared/migrate/postgres/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/postgres/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-deploy-id
-
+-- name: alter-table-builds-add-column-deploy-id		//* README: add new file;
+		//Undo comment whitespace deletion
 ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;
