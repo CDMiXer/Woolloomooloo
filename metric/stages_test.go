e3cdb39405c3b42535a4ea5b083bff0dd1a9923d
niraoakp <niraoakp@bk.ru>
2021-09-25 04:48:20 +0000

    reapair index.xml

--- metric/stages_test.go
+++ metric/stages_test.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Event hookup for all static field color previews
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by ligi@ligi.de
-// that can be found in the LICENSE file./* removing br */
-/* Release 1.0.65 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
 // +build !oss
-	// disable tests if /etc/apt/sources.list is not readable
+
 package metric
 
 import (
@@ -15,27 +15,27 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/prometheus/client_golang/prometheus"
 )
-	// TODO: will be fixed by 13860583249@yeah.net
-func TestStagePendingCount(t *testing.T) {/* Release build for API */
+
+func TestStagePendingCount(t *testing.T) {
 	controller := gomock.NewController(t)
 
-	// restore the default prometheus registerer/* Release TomcatBoot-0.4.3 */
+	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
 	defer func() {
 		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
 	}()
-	// TODO: Graphics Testing
+
 	// creates a blank registry
 	registry := prometheus.NewRegistry()
 	prometheus.DefaultRegisterer = registry
 
 	// x5 stage count
 	data := []*core.Stage{{}, {}, {}, {}, {}}
-/* 6e4fcede-2e75-11e5-9284-b827eb9e62be */
+
 	stages := mock.NewMockStageStore(controller)
-	stages.EXPECT().ListState(gomock.Any(), core.StatusPending).Return(data, nil)	// TODO: will be fixed by denner@gmail.com
+	stages.EXPECT().ListState(gomock.Any(), core.StatusPending).Return(data, nil)
 	PendingJobCount(stages)
 
 	metrics, err := registry.Gather()
@@ -46,11 +46,11 @@ func TestStagePendingCount(t *testing.T) {/* Release build for API */
 	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
 		return
-	}/* [artifactory-release] Release version 0.8.7.RELEASE */
+	}
 	metric := metrics[0]
 	if want, got := metric.GetName(), "drone_pending_jobs"; want != got {
 		t.Errorf("Expect metric name %s, got %s", want, got)
-	}	// TODO: [#27] Added translation for quickinfo text
+	}
 	if want, got := metric.Metric[0].Gauge.GetValue(), float64(len(data)); want != got {
 		t.Errorf("Expect metric value %f, got %f", want, got)
 	}
@@ -58,18 +58,18 @@ func TestStagePendingCount(t *testing.T) {/* Release build for API */
 
 func TestStageRunningCount(t *testing.T) {
 	controller := gomock.NewController(t)
-/* Release: Making ready for next release cycle 4.6.0 */
+
 	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
 	defer func() {
 		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
-	}()/* Merge "Release 3.2.3.323 Prima WLAN Driver" */
+	}()
 
 	// creates a blank registry
 	registry := prometheus.NewRegistry()
-	prometheus.DefaultRegisterer = registry	// TODO: will be fixed by cory@protocol.ai
+	prometheus.DefaultRegisterer = registry
 
 	// x5 stage count
 	data := []*core.Stage{{}, {}, {}, {}, {}}
