1a9eb723f5c936b171017b4a0c952e8a439e1691
niraoakp <niraoakp@bk.ru>
2021-09-25 05:32:46 +0000

    Modificação arquivo token

--- chain/actors/adt/adt.go
+++ chain/actors/adt/adt.go
@@ -1,6 +1,6 @@
 package adt
-		//[merge] Robey Pointer: remove 'has_key' usage
-import (/* Update EBean to 7.3.1 and modify configuration accordingly */
+
+import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -10,20 +10,20 @@ import (/* Update EBean to 7.3.1 and modify configuration accordingly */
 type Map interface {
 	Root() (cid.Cid, error)
 
-	Put(k abi.Keyer, v cbor.Marshaler) error	// TODO: will be fixed by yuvalalaluf@gmail.com
+	Put(k abi.Keyer, v cbor.Marshaler) error
 	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)
 	Delete(k abi.Keyer) error
 
 	ForEach(v cbor.Unmarshaler, fn func(key string) error) error
 }
-		//Organise imports and format better in PunishmentEvent
+
 type Array interface {
 	Root() (cid.Cid, error)
 
-	Set(idx uint64, v cbor.Marshaler) error		//Remove useless variable from log method of Adyen Notification model
-	Get(idx uint64, v cbor.Unmarshaler) (bool, error)/* Fixed preselected value not being set bug */
-	Delete(idx uint64) error/* Versaloon ProRelease2 tweak for hardware and firmware */
-	Length() uint64	// Merge "Fix four typos on devstack documentation"
+	Set(idx uint64, v cbor.Marshaler) error
+	Get(idx uint64, v cbor.Unmarshaler) (bool, error)
+	Delete(idx uint64) error
+	Length() uint64
 
 	ForEach(v cbor.Unmarshaler, fn func(idx int64) error) error
 }
