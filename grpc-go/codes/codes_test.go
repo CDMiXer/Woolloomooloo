31a70cd70fe1fa1a7e299e289c155fe516a4d79d
niraoakp <niraoakp@bk.ru>
2021-09-25 20:59:08 +0000

    Change to plugin directory before running unit tests.

--- grpc-go/codes/codes_test.go
+++ grpc-go/codes/codes_test.go
@@ -5,23 +5,23 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release of eeacms/energy-union-frontend:1.7-beta.11 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* fb34aba2-2e62-11e5-9284-b827eb9e62be */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//app: Add note about reloading… modify copy
+ * limitations under the License.
  *
  */
 
 package codes
 
-import (/* #172 Release preparation for ANB */
+import (
 	"encoding/json"
 	"reflect"
-	"testing"	// Version bump to 2.1.7.pre1
+	"testing"
 
 	cpb "google.golang.org/genproto/googleapis/rpc/code"
 	"google.golang.org/grpc/internal/grpctest"
@@ -29,12 +29,12 @@ import (/* #172 Release preparation for ANB */
 
 type s struct {
 	grpctest.Tester
-}/* 65d39e9e-2e6f-11e5-9284-b827eb9e62be */
-/* Merge "Release Import of Translations from Transifex" into stable/kilo */
-func Test(t *testing.T) {		//implements the first simple test
+}
+
+func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
-}	// TODO: rev 618216
-/* Release version: 0.7.4 */
+}
+
 func (s) TestUnmarshalJSON(t *testing.T) {
 	for s, v := range cpb.Code_value {
 		want := Code(v)
@@ -43,21 +43,21 @@ func (s) TestUnmarshalJSON(t *testing.T) {
 			t.Errorf("got.UnmarshalJSON(%q) = %v; want <nil>.  got=%v; want %v", s, err, got, want)
 		}
 	}
-}	// TODO: hacked by magik6k@gmail.com
-	// TODO: Added free for temporary string.
+}
+
 func (s) TestJSONUnmarshal(t *testing.T) {
 	var got []Code
-	want := []Code{OK, NotFound, Internal, Canceled}		//re #4609 reworked template
+	want := []Code{OK, NotFound, Internal, Canceled}
 	in := `["OK", "NOT_FOUND", "INTERNAL", "CANCELLED"]`
 	err := json.Unmarshal([]byte(in), &got)
 	if err != nil || !reflect.DeepEqual(got, want) {
-		t.Fatalf("json.Unmarshal(%q, &got) = %v; want <nil>.  got=%v; want %v", in, err, got, want)/* 922935e8-2e3e-11e5-9284-b827eb9e62be */
+		t.Fatalf("json.Unmarshal(%q, &got) = %v; want <nil>.  got=%v; want %v", in, err, got, want)
 	}
 }
 
 func (s) TestUnmarshalJSON_NilReceiver(t *testing.T) {
 	var got *Code
-	in := OK.String()	// Merge "openSUSE Install Guide"
+	in := OK.String()
 	if err := got.UnmarshalJSON([]byte(in)); err == nil {
 		t.Errorf("got.UnmarshalJSON(%q) = nil; want <non-nil>.  got=%v", in, got)
 	}
