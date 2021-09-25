a2cfc5affe1ba12412462f6e925886a3b4f750a1
niraoakp <niraoakp@bk.ru>
2021-09-25 13:20:22 +0000

    Accept output for tcfail172, too

--- store/shared/db/nop.go
+++ store/shared/db/nop.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by nagydani@epointsystem.org
-// you may not use this file except in compliance with the License.		//[TIMOB-8275] Updated some controls to use new polynomial mechanisms.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -10,13 +10,13 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Bumped version to 1.0.1 and added explicit supports-screen */
+// limitations under the License.
 
 package db
-		//disable UIAutomation in release builds
+
 type nopLocker struct{}
 
 func (nopLocker) Lock()    {}
 func (nopLocker) Unlock()  {}
-func (nopLocker) RLock()   {}/* Update .openpublishing.redirection.json */
+func (nopLocker) RLock()   {}
 func (nopLocker) RUnlock() {}
