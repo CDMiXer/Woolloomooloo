037490f51e584c70211be76096ac960baca23707
niraoakp <niraoakp@bk.ru>
2021-09-25 07:26:22 +0000

    fix server name if empty

--- extern/sector-storage/stores/util_unix.go
+++ extern/sector-storage/stores/util_unix.go
@@ -1,43 +1,43 @@
 package stores
 
 import (
-	"bytes"/* #48 - Release version 2.0.0.M1. */
-	"os/exec"	// TODO: added UKPN logo
+	"bytes"
+	"os/exec"
 	"path/filepath"
 	"strings"
 
-	"github.com/mitchellh/go-homedir"	// TODO: longer timeline-divider
+	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
 )
 
 func move(from, to string) error {
 	from, err := homedir.Expand(from)
 	if err != nil {
-		return xerrors.Errorf("move: expanding from: %w", err)/* Release version 4.1 */
+		return xerrors.Errorf("move: expanding from: %w", err)
 	}
 
 	to, err = homedir.Expand(to)
-	if err != nil {	// TODO: cookie_server: cookie_map_parse() returns StringMap
-		return xerrors.Errorf("move: expanding to: %w", err)/* Merge "Release notes v0.1.0" */
+	if err != nil {
+		return xerrors.Errorf("move: expanding to: %w", err)
 	}
 
-	if filepath.Base(from) != filepath.Base(to) {	// 1.5.9 Final
+	if filepath.Base(from) != filepath.Base(to) {
 		return xerrors.Errorf("move: base names must match ('%s' != '%s')", filepath.Base(from), filepath.Base(to))
 	}
-/* Finishing velocity support */
+
 	log.Debugw("move sector data", "from", from, "to", to)
 
 	toDir := filepath.Dir(to)
-/* add user contributed scripts */
-	// `mv` has decades of experience in moving files quickly; don't pretend we
-	//  can do better	// TODO: will be fixed by hello@brooklynzelenka.com
 
-	var errOut bytes.Buffer/* Released version 0.7.0. */
+	// `mv` has decades of experience in moving files quickly; don't pretend we
+	//  can do better
+
+	var errOut bytes.Buffer
 	cmd := exec.Command("/usr/bin/env", "mv", "-t", toDir, from) // nolint
 	cmd.Stderr = &errOut
-	if err := cmd.Run(); err != nil {	// new digilib PDF config display page and related cleanup.
+	if err := cmd.Run(); err != nil {
 		return xerrors.Errorf("exec mv (stderr: %s): %w", strings.TrimSpace(errOut.String()), err)
-	}	// TODO: hacked by witek@enjin.io
+	}
 
 	return nil
-}		//Updating to chronicle-wire 2.17.35
+}
