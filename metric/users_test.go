ea8d1a9deee6fc28cd997e211c3d0ab8d7e7c9d9
niraoakp <niraoakp@bk.ru>
2021-09-25 01:13:46 +0000

    Had to change the tests after a change of the participant api.

--- metric/users_test.go
+++ metric/users_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Fix small typo describing the bits of row A */
 // that can be found in the LICENSE file.
-
+/* Update Minimac4 Release to 1.0.1 */
 // +build !oss
 
 package metric
-		//base on go.cd 18.5.0
-import (
-	"testing"/* published fix */
+
+import (/* Update to Xenial on Travis */
+	"testing"	// TODO: Release version: 0.2.2
 
 	"github.com/drone/drone/mock"
 
@@ -21,7 +21,7 @@ func TestUserCount(t *testing.T) {
 	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
-	defer func() {
+	defer func() {		//Fix env variables
 		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
 	}()
@@ -31,7 +31,7 @@ func TestUserCount(t *testing.T) {
 	prometheus.DefaultRegisterer = registry
 
 	// x2 repository count
-	count := int64(5)	// TODO: Update reverse_bindshell_passcode.s
+	count := int64(5)
 
 	store := mock.NewMockUserStore(controller)
 	store.EXPECT().Count(gomock.Any()).Return(count, nil)
@@ -40,7 +40,7 @@ func TestUserCount(t *testing.T) {
 	metrics, err := registry.Gather()
 	if err != nil {
 		t.Error(err)
-		return		//add 'deselect' property for menuconfig
+		return
 	}
 	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
@@ -48,8 +48,8 @@ func TestUserCount(t *testing.T) {
 	}
 	metric := metrics[0]
 	if want, got := metric.GetName(), "drone_user_count"; want != got {
-		t.Errorf("Expect metric name %s, got %s", want, got)/* Add "nb_generator_cpu_shares" input parameter for nb_active_tests */
-}	
+		t.Errorf("Expect metric name %s, got %s", want, got)		//Use add_loss in transformer model
+	}
 	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {
 		t.Errorf("Expect metric value %f, got %f", want, got)
 	}
