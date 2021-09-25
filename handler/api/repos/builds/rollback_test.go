034fcb45cd6132a166408aff015283969a95a1d2
niraoakp <niraoakp@bk.ru>
2021-09-25 09:07:47 +0000

    - Import setup from setuptools if available

--- handler/api/repos/builds/rollback_test.go
+++ handler/api/repos/builds/rollback_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Move post_connect callback for RemoteTransports to smart.medium
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// TODO: Update with new func and change parameter of object
 
-// +build !oss
-/* Added KeyReleased event to input system. */
-package builds
+// +build !oss/* update readme according to last changes */
+	// Version 30 Julio AM
+package builds/* copied from older document for formatting */
