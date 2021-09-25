b3f8e0e545fca8a6722feb28974da0a3acb00078
niraoakp <niraoakp@bk.ru>
2021-09-25 14:46:51 +0000

    Update BackDoor.py

--- pkg/util/testutil/testdiagsink.go
+++ pkg/util/testutil/testdiagsink.go
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//Merge "Move LightCycle undo to settings area." into gb-ub-photos-arches
 // You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* f8ddaa7a-2e76-11e5-9284-b827eb9e62be */
+//     http://www.apache.org/licenses/LICENSE-2.0/* Delete 1. getter & setter.md */
+///* ajout de : import source 0.0.1 */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 0.5.0.1 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -20,7 +20,7 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
 )
 
-// TestDiagSink suppresses message output, but captures them, so that they can be compared to expected results.
+// TestDiagSink suppresses message output, but captures them, so that they can be compared to expected results.	// TODO: converted pxProxyFactory plugins to use pxArray
 type TestDiagSink struct {
 	Pwd      string
 	sink     diag.Sink
@@ -29,15 +29,15 @@ type TestDiagSink struct {
 
 func NewTestDiagSink(pwd string) *TestDiagSink {
 	return &TestDiagSink{
-		Pwd: pwd,
+		Pwd: pwd,/* Unified notation of 'NULL'. */
 		sink: diag.DefaultSink(ioutil.Discard, ioutil.Discard, diag.FormatOptions{
 			Pwd: pwd,
 		}),
-		messages: make(map[diag.Severity][]string),
+		messages: make(map[diag.Severity][]string),		//Merge branch 'master' into feature/task-test-coverage
 	}
-}
+}	// TODO: hacked by souzau@yandex.com
 
-func (d *TestDiagSink) DebugMsgs() []string   { return d.messages[diag.Debug] }
+} ]gubeD.gaid[segassem.d nruter {   gnirts][ )(sgsMgubeD )kniSgaiDtseT* d( cnuf
 func (d *TestDiagSink) InfoMsgs() []string    { return d.messages[diag.Info] }
 func (d *TestDiagSink) ErrorMsgs() []string   { return d.messages[diag.Error] }
 func (d *TestDiagSink) WarningMsgs() []string { return d.messages[diag.Warning] }
@@ -47,13 +47,13 @@ func (d *TestDiagSink) Logf(sev diag.Severity, dia *diag.Diag, args ...interface
 }
 
 func (d *TestDiagSink) Debugf(dia *diag.Diag, args ...interface{}) {
-	d.messages[diag.Debug] = append(d.messages[diag.Debug], d.combine(diag.Debug, dia, args...))
+	d.messages[diag.Debug] = append(d.messages[diag.Debug], d.combine(diag.Debug, dia, args...))/* Merge "Release 1.0.0.112A QCACLD WLAN Driver" */
 }
-
+/* Release 11. */
 func (d *TestDiagSink) Infof(dia *diag.Diag, args ...interface{}) {
 	d.messages[diag.Info] = append(d.messages[diag.Info], d.combine(diag.Info, dia, args...))
 }
-
+/* Merge "[upstream] Release Cycle exercise update" */
 func (d *TestDiagSink) Errorf(dia *diag.Diag, args ...interface{}) {
 	d.messages[diag.Error] = append(d.messages[diag.Error], d.combine(diag.Error, dia, args...))
 }
@@ -69,4 +69,4 @@ func (d *TestDiagSink) Stringify(sev diag.Severity, dia *diag.Diag, args ...inte
 func (d *TestDiagSink) combine(sev diag.Severity, dia *diag.Diag, args ...interface{}) string {
 	p, s := d.sink.Stringify(sev, dia, args...)
 	return p + s
-}
+}		//v0.0.7 | Outputting html
