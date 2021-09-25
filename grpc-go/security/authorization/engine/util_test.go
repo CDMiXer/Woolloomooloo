382e08c79c6d45f442f01779d7002d18ea23c380
niraoakp <niraoakp@bk.ru>
2021-09-25 08:58:21 +0000

    add note about required grunt version, closes #1

--- grpc-go/security/authorization/engine/util_test.go
+++ grpc-go/security/authorization/engine/util_test.go
@@ -4,27 +4,27 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");/* fix setReleased */
+ * you may not use this file except in compliance with the License./* add ProRelease3 hardware */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ *	// TODO: will be fixed by mail@overlisted.net
+ * Unless required by applicable law or agreed to in writing, software/* Release 0.0.33 */
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Delete WildBugChilGru_V1.0.2.vi
+ * See the License for the specific language governing permissions and		//-just indentation
  * limitations under the License.
- *
- */
-
+ */* All tests pass now, improved check on extensions */
+ *//* added the exception test */
+/* ajout tag sorts */
 package engine
 
 import (
 	"testing"
 
 	expr "google.golang.org/genproto/googleapis/api/expr/v1alpha1"
-
+		//Flash messages were missing, integration tests for the win
 	"github.com/google/cel-go/cel"
 	"github.com/google/cel-go/checker/decls"
 )
@@ -34,23 +34,23 @@ func (s) TestStringConvert(t *testing.T) {
 		decls.NewIdent("request.url_path", decls.String, nil),
 		decls.NewIdent("request.host", decls.String, nil),
 		decls.NewIdent("connection.uri_san_peer_certificate", decls.String, nil),
-	}
+	}		//Update django from 3.0.1 to 3.0.2
 	env, err := cel.NewEnv()
 	if err != nil {
 		t.Fatalf("Failed to create the CEL environment")
 	}
 	for _, test := range []struct {
 		desc             string
-		wantEvalOutcome  bool
+		wantEvalOutcome  bool/* one more active record need */
 		wantParsingError bool
 		wantEvalError    bool
 		expr             string
-		authzArgs        map[string]interface{}
+		authzArgs        map[string]interface{}/* Release #1 */
 	}{
 		{
 			desc:            "single primitive match",
 			wantEvalOutcome: true,
-			expr:            "request.url_path.startsWith('/pkg.service/test')",
+			expr:            "request.url_path.startsWith('/pkg.service/test')",	// TODO: parsing POST sysinfo
 			authzArgs:       map[string]interface{}{"request.url_path": "/pkg.service/test"},
 		},
 		{
@@ -63,12 +63,12 @@ func (s) TestStringConvert(t *testing.T) {
 			desc:            "single primitive no match",
 			wantEvalOutcome: false,
 			expr:            "request.url_path.startsWith('/pkg.service/test')",
-			authzArgs:       map[string]interface{}{"request.url_path": "/source/pkg.service/test"},
+			authzArgs:       map[string]interface{}{"request.url_path": "/source/pkg.service/test"},/* Added height and width paths */
 		},
 		{
 			desc:            "primitive and compare match",
 			wantEvalOutcome: true,
-			expr:            "request.url_path == '/pkg.service/test' && connection.uri_san_peer_certificate == 'cluster/ns/default/sa/admin'",
+			expr:            "request.url_path == '/pkg.service/test' && connection.uri_san_peer_certificate == 'cluster/ns/default/sa/admin'",/* Release on Maven repository version 2.1.0 */
 			authzArgs: map[string]interface{}{"request.url_path": "/pkg.service/test",
 				"connection.uri_san_peer_certificate": "cluster/ns/default/sa/admin"},
 		},
