fc9cf00d9be9819c78f737caf681a5a9fe8d39b8
niraoakp <niraoakp@bk.ru>
2021-09-25 12:09:30 +0000

    Implement zip operator

--- pkg/resource/deploy/providers/provider_test.go
+++ pkg/resource/deploy/providers/provider_test.go
@@ -1,30 +1,30 @@
-package providers
+package providers/* #i10000# clean up files with zero byte size */
 
 import (
 	"testing"
 
-	"github.com/blang/semver"	// TODO: hacked by nick@perfectabstractions.com
+	"github.com/blang/semver"		//Update linter-majors
 	"github.com/stretchr/testify/assert"
-/* - added: support for adjustable SIP server port */
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-)	// TODO: hacked by igor@soramitsu.co.jp
-	// TODO: will be fixed by arachnid@notdot.net
+)
+
 func TestProviderRequestNameNil(t *testing.T) {
-	req := NewProviderRequest(nil, "pkg")	// TODO: will be fixed by joshua@yottadb.com
+	req := NewProviderRequest(nil, "pkg")
 	assert.Equal(t, tokens.QName("default"), req.Name())
 	assert.Equal(t, "pkg", req.String())
-}
+}		//Delete DVDad.jpg
 
-func TestProviderRequestNameNoPre(t *testing.T) {/* Now saves crop in cache. */
-	ver := semver.MustParse("0.18.1")	// TODO: Combine logger
+func TestProviderRequestNameNoPre(t *testing.T) {
+	ver := semver.MustParse("0.18.1")
 	req := NewProviderRequest(&ver, "pkg")
-	assert.Equal(t, "default_0_18_1", req.Name().String())/* Release script updated. */
-	assert.Equal(t, "pkg-0.18.1", req.String())/* Infrastructure for Preconditions and FirstReleaseFlag check  */
+	assert.Equal(t, "default_0_18_1", req.Name().String())
+	assert.Equal(t, "pkg-0.18.1", req.String())
 }
 
 func TestProviderRequestNameDev(t *testing.T) {
-	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")
+	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")/* Create jquery.realAutoComplete.js */
 	req := NewProviderRequest(&ver, "pkg")
-	assert.Equal(t, "default_0_17_7_dev_1555435978_gb7030aa4_dirty", req.Name().String())	// TODO: will be fixed by lexy8russo@outlook.com
-	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())
-}	// Turn off the msbuild engine.
+	assert.Equal(t, "default_0_17_7_dev_1555435978_gb7030aa4_dirty", req.Name().String())
+	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())/* Add changelog.md to build ignore */
+}
