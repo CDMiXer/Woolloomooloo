3a9b64c3f8c9a966bdec910a4b571731d93e54b0
niraoakp <niraoakp@bk.ru>
2021-09-25 19:51:12 +0000

    Added my own information into readme

--- plugin/webhook/config.go
+++ plugin/webhook/config.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.		//[MIN] Minor rewritings triggered by PMD
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -8,16 +8,16 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//[WRITE] Sync with Wine Staging 1.9.16. CORE-11866
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package webhook
-		//add selectedDate option!
+
 import "github.com/drone/drone/core"
-	// inizio gestione processi
-// Config provides the webhook configuration.	// TODO: Continent grid added
-type Config struct {		//Initial commit of nxOMSSyslog.py
+
+// Config provides the webhook configuration.
+type Config struct {
 	Events   []string
 	Endpoint []string
 	Secret   string
