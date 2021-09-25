ed73ea2ab2522fecee9296e7746d0e96165b15d2
niraoakp <niraoakp@bk.ru>
2021-09-25 15:47:22 +0000

    Successfully tested bwa_workflow cluster - OICR Openstack

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -2,15 +2,15 @@ package ffiwrapper
 
 import (
 	logging "github.com/ipfs/go-log/v2"
-)	// TODO: hacked by ng8eke@163.com
+)
 
 var log = logging.Logger("ffiwrapper")
-	// TODO: hacked by brosner@gmail.com
+
 type Sealer struct {
 	sectors  SectorProvider
-	stopping chan struct{}/* Release of eeacms/eprtr-frontend:0.4-beta.18 */
+	stopping chan struct{}
 }
 
-func (sb *Sealer) Stop() {/* Released 2.6.0.5 version to fix issue with carriage returns */
+func (sb *Sealer) Stop() {
 	close(sb.stopping)
 }
