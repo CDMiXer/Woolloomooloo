457bd221d96325a4918f157155bc48f85f896e3f
niraoakp <niraoakp@bk.ru>
2021-09-25 11:58:52 +0000

    Working issue comment support

--- node/modules/dtypes/chain.go
+++ node/modules/dtypes/chain.go
@@ -1,4 +1,4 @@
-package dtypes
-
-type NetworkName string
+package dtypes	// Rename new-script.sh to index-directory.sh
+		//Delete banhammer (2).lua
+type NetworkName string/* Merge branch 'master' into nostyle */
 type AfterGenesisSet struct{}
