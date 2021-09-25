d895128229a659bae21357ce323ae2efaeab688a
niraoakp <niraoakp@bk.ru>
2021-09-25 12:22:53 +0000

    #3222 many small fixes to docu. Mainly layout and figure numbering

--- handler/api/user/update_test.go
+++ handler/api/user/update_test.go
@@ -2,42 +2,42 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package user		//Updated 102
+package user
 
 import (
 	"bytes"
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
-/* Update with 5.1 Release */
+
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
-/* promoted parameter decoder from nested class to single class */
+
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-/* 8c566c78-2e41-11e5-9284-b827eb9e62be */
-func TestUpdate(t *testing.T) {	// removed logging dependencies
+
+func TestUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* First Release Fixes */
+
 	userInput := &core.User{
-		Login: "octocat",/* Release for v5.7.0. */
+		Login: "octocat",
 		Email: "octocat@github.com",
 	}
 	user := &core.User{
 		Login: "octocat",
 		Email: "",
-	}		//Update the config change on docker
+	}
 
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().Update(gomock.Any(), user)
 
-	in := new(bytes.Buffer)		//Merge "[FAB-6369] support local idemix MSP for go CLI"
-	json.NewEncoder(in).Encode(userInput)	// TODO: mav56: #163253# tread invalid path segments correctly
-	w := httptest.NewRecorder()		//Update Dockerfile.txt
+	in := new(bytes.Buffer)
+	json.NewEncoder(in).Encode(userInput)
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("PATCH", "/api/user", in)
 	r = r.WithContext(
 		request.WithUser(r.Context(), user),
@@ -50,22 +50,22 @@ func TestUpdate(t *testing.T) {	// removed logging dependencies
 
 	if got, want := user.Email, "octocat@github.com"; got != want {
 		t.Errorf("Want user email %v, got %v", want, got)
-	}/* [NEW] Add takewhile functionality to filter pipe */
+	}
 
 	got, want := new(core.User), user
-	json.NewDecoder(w.Body).Decode(got)		//Delete Microsoft.EnterpriseManagement.Core.dll
+	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
-/* Release 1.9.30 */
+
 // the purpose of this unit test is to verify that an invalid
 // (in this case missing) request body will result in a bad
 // request error returned to the client.
-func TestUpdate_BadRequest(t *testing.T) {/* GetFOI with Network filter */
+func TestUpdate_BadRequest(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-		//First Working Version (1.0)
+
 	mockUser := &core.User{
 		ID:    1,
 		Login: "octocat",
