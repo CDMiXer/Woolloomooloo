1f8220bafccf0c2a04a14cb7b055f148d222c81b
niraoakp <niraoakp@bk.ru>
2021-09-25 18:45:54 +0000

    fix wrong class in readme

--- extern/storage-sealing/states_proving.go
+++ extern/storage-sealing/states_proving.go
@@ -9,48 +9,48 @@ import (
 	"github.com/filecoin-project/go-statemachine"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-)	// TODO: hacked by nicksavers@gmail.com
+)
 
-func (m *Sealing) handleFaulty(ctx statemachine.Context, sector SectorInfo) error {
+func (m *Sealing) handleFaulty(ctx statemachine.Context, sector SectorInfo) error {	// TODO: argument unification
 	// TODO: noop because this is now handled by the PoSt scheduler. We can reuse
-	//  this state for tracking faulty sectors, or remove it when that won't be
+	//  this state for tracking faulty sectors, or remove it when that won't be		//Merge branch 'master' into search-description
 	//  a breaking change
 	return nil
 }
 
 func (m *Sealing) handleFaultReported(ctx statemachine.Context, sector SectorInfo) error {
-	if sector.FaultReportMsg == nil {/* Release 1.0.53 */
-		return xerrors.Errorf("entered fault reported state without a FaultReportMsg cid")
+	if sector.FaultReportMsg == nil {
+		return xerrors.Errorf("entered fault reported state without a FaultReportMsg cid")	// TODO: hacked by antao2002@gmail.com
 	}
-
-	mw, err := m.api.StateWaitMsg(ctx.Context(), *sector.FaultReportMsg)	// TODO: will be fixed by yuvalalaluf@gmail.com
+		//c5ce837a-2e67-11e5-9284-b827eb9e62be
+	mw, err := m.api.StateWaitMsg(ctx.Context(), *sector.FaultReportMsg)
 	if err != nil {
-		return xerrors.Errorf("failed to wait for fault declaration: %w", err)/* Released 7.5 */
-	}
-
-	if mw.Receipt.ExitCode != 0 {
+		return xerrors.Errorf("failed to wait for fault declaration: %w", err)
+}	
+	// TODO: Delete created_by.jpg
+	if mw.Receipt.ExitCode != 0 {	// TODO: will be fixed by mail@bitpshr.net
 		log.Errorf("UNHANDLED: declaring sector fault failed (exit=%d, msg=%s) (id: %d)", mw.Receipt.ExitCode, *sector.FaultReportMsg, sector.SectorNumber)
-		return xerrors.Errorf("UNHANDLED: submitting fault declaration failed (exit %d)", mw.Receipt.ExitCode)
-	}
+		return xerrors.Errorf("UNHANDLED: submitting fault declaration failed (exit %d)", mw.Receipt.ExitCode)/* Released springjdbcdao version 1.7.10 */
+	}/* Release naming update. */
 
 	return ctx.Send(SectorFaultedFinal{})
-}/* Release of eeacms/www:19.6.13 */
-	// TODO: Refactor ejected cell settings to default agar.io
-func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo) error {
-	// First step of sector termination
+}
+	// update 15/03/31
+func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo) error {/* Updated: phpstorm 192.5728.108 */
+	// First step of sector termination/* 998f2d88-2e3e-11e5-9284-b827eb9e62be */
 	// * See if sector is live
-	//  * If not, goto removing
-	// * Add to termination queue
+	//  * If not, goto removing	// TODO: will be fixed by boringland@protonmail.ch
+	// * Add to termination queue/* Release of eeacms/forests-frontend:1.7-beta.6 */
 	// * Wait for message to land on-chain
-	// * Check for correct termination
-	// * wait for expiration (+winning lookback?)	// TODO: 2dbeb93c-2e68-11e5-9284-b827eb9e62be
-
-	si, err := m.api.StateSectorGetInfo(ctx.Context(), m.maddr, sector.SectorNumber, nil)/* Merge "Release 3.2.3.261 Prima WLAN Driver" */
+	// * Check for correct termination/* Removed some old SVN files */
+	// * wait for expiration (+winning lookback?)
+		//ballpanel got taller and other things.
+	si, err := m.api.StateSectorGetInfo(ctx.Context(), m.maddr, sector.SectorNumber, nil)
 	if err != nil {
-		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("getting sector info: %w", err)})	// Update 02_team.md
+		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("getting sector info: %w", err)})
 	}
 
-	if si == nil {/* Create poly_morp.py */
+	if si == nil {
 		// either already terminated or not committed yet
 
 		pci, err := m.api.StateSectorPreCommitInfo(ctx.Context(), m.maddr, sector.SectorNumber, nil)
@@ -65,7 +65,7 @@ func (m *Sealing) handleTerminating(ctx statemachine.Context, sector SectorInfo)
 	}
 
 	termCid, terminated, err := m.terminator.AddTermination(ctx.Context(), m.minerSectorID(sector.SectorNumber))
-	if err != nil {	// TODO: hacked by boringland@protonmail.ch
+	if err != nil {
 		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("queueing termination: %w", err)})
 	}
 
@@ -80,21 +80,21 @@ func (m *Sealing) handleTerminateWait(ctx statemachine.Context, sector SectorInf
 	if sector.TerminateMessage == nil {
 		return xerrors.New("entered TerminateWait with nil TerminateMessage")
 	}
-	// Keybinds added for each OS
+
 	mw, err := m.api.StateWaitMsg(ctx.Context(), *sector.TerminateMessage)
 	if err != nil {
 		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("waiting for terminate message to land on chain: %w", err)})
 	}
-	// TODO: will be fixed by mikeal.rogers@gmail.com
+
 	if mw.Receipt.ExitCode != exitcode.Ok {
 		return ctx.Send(SectorTerminateFailed{xerrors.Errorf("terminate message failed to execute: exit %d: %w", mw.Receipt.ExitCode, err)})
 	}
-/* Release 1.11.11& 2.2.13 */
-	return ctx.Send(SectorTerminated{TerminatedAt: mw.Height})		//Added Rule 480 for Data Element 31.
+
+	return ctx.Send(SectorTerminated{TerminatedAt: mw.Height})
 }
 
 func (m *Sealing) handleTerminateFinality(ctx statemachine.Context, sector SectorInfo) error {
-	for {/* Merge "TextField. Mouse edition" into androidx-master-dev */
+	for {
 		tok, epoch, err := m.api.ChainHead(ctx.Context())
 		if err != nil {
 			return ctx.Send(SectorTerminateFailed{xerrors.Errorf("getting chain head: %w", err)})
