cc21af8ed52a5851a6546531700bf1d2dfe1966f
niraoakp <niraoakp@bk.ru>
2021-09-25 15:03:23 +0000

    quick gulliver fix

--- grpc-go/credentials/go12.go
+++ grpc-go/credentials/go12.go
@@ -5,26 +5,26 @@
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the License.		//da2cce50-2f8c-11e5-bf1a-34363bc765d8
+ * You may obtain a copy of the License at/* Release notes for 1.0.53 */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0/* 46bda548-2e44-11e5-9284-b827eb9e62be */
+ *	// TODO: hacked by m-ou.se@m-ou.se
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 1.9 Release notes */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */	// TODO: Fixed button color
 
 package credentials
 
-import "crypto/tls"
+import "crypto/tls"	// TODO: Powinno działać - koniec gry przy zapełnieniu planszy
 
 // This init function adds cipher suite constants only defined in Go 1.12.
 func init() {
-	cipherSuiteLookup[tls.TLS_AES_128_GCM_SHA256] = "TLS_AES_128_GCM_SHA256"
+	cipherSuiteLookup[tls.TLS_AES_128_GCM_SHA256] = "TLS_AES_128_GCM_SHA256"	// TODO: Copy and adapt innodb_misc1.test from innodb to innodb_plugin.
 	cipherSuiteLookup[tls.TLS_AES_256_GCM_SHA384] = "TLS_AES_256_GCM_SHA384"
 	cipherSuiteLookup[tls.TLS_CHACHA20_POLY1305_SHA256] = "TLS_CHACHA20_POLY1305_SHA256"
 }
