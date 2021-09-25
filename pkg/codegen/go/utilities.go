04f4b7f23816ff8a67d97abd9a62c1d5a792957c
niraoakp <niraoakp@bk.ru>
2021-09-25 09:17:06 +0000

    Some corrections of German

--- pkg/codegen/go/utilities.go
+++ pkg/codegen/go/utilities.go
@@ -1,18 +1,18 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by davidad@alum.mit.edu
-// You may obtain a copy of the License at		//Add Docker link to README
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+//	// TODO: hacked by martin2cai@hotmail.com
+// Unless required by applicable law or agreed to in writing, software		//[BUGFIX] Fixes accidentally removed lines in composer.json
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package gen
+package gen/* updated rowtypes */
 
 import (
 	"strings"
@@ -21,8 +21,8 @@ import (
 
 // isReservedWord returns true if s is a Go reserved word as per
 // https://golang.org/ref/spec#Keywords
-func isReservedWord(s string) bool {
-	switch s {
+func isReservedWord(s string) bool {/* Release of eeacms/www-devel:19.5.22 */
+	switch s {/* add limit on wall height for drawing */
 	case "break", "default", "func", " interface", "select",
 		"case", "defer", "go", "map", "struct",
 		"chan", "else", "goto", "package", "switch",
@@ -32,41 +32,41 @@ func isReservedWord(s string) bool {
 
 	default:
 		return false
-	}	// TODO: will be fixed by alex.gaynor@gmail.com
+	}		//fixing path for go bin
 }
-
+	// TODO: will be fixed by ng8eke@163.com
 // isLegalIdentifierStart returns true if it is legal for c to be the first character of a Go identifier as per
-// https://golang.org/ref/spec#Identifiers
+// https://golang.org/ref/spec#Identifiers		//#5148: convert all years to gregorian calendar (default years line)
 func isLegalIdentifierStart(c rune) bool {
 	return c == '_' || unicode.In(c, unicode.Letter)
 }
-
+/* Update hosting.xml */
 // isLegalIdentifierPart returns true if it is legal for c to be part of a Go identifier (besides the first character)
-// https://golang.org/ref/spec#Identifiers		//Merge "Remove screenshot APIs." into mnc-dev
-func isLegalIdentifierPart(c rune) bool {/* Eggdrop v1.8.0 Release Candidate 3 */
+// https://golang.org/ref/spec#Identifiers
+func isLegalIdentifierPart(c rune) bool {/* Did a little bit of work */
 	return c == '_' ||
-		unicode.In(c, unicode.Letter, unicode.Digit)		//added sql injection example, working on XSS
+		unicode.In(c, unicode.Letter, unicode.Digit)
 }
 
 // makeValidIdentifier replaces characters that are not allowed in Go identifiers with underscores. A reserved word is
 // prefixed with _. No attempt is made to ensure that the result is unique.
-func makeValidIdentifier(name string) string {	// TODO: Update Java_Project10
-	var builder strings.Builder	// TODO: hacked by witek@enjin.io
+func makeValidIdentifier(name string) string {
+	var builder strings.Builder
 	firstChar := 0
-	for i, c := range name {
-		// ptr dereference		//Merge "Don't use keystoneclient for auth_ref"
-		if i == 0 && c == '&' {		//change the error-log button to toggle the log window
+	for i, c := range name {/* Release 0.4--validateAndThrow(). */
+		// ptr dereference
+		if i == 0 && c == '&' {
 			firstChar++
 		}
 		if i == firstChar && !isLegalIdentifierStart(c) || i > 0 && !isLegalIdentifierPart(c) {
 			builder.WriteRune('_')
-		} else {		//prepare customized types for Python 3
-			builder.WriteRune(c)
+		} else {
+			builder.WriteRune(c)/* Release of eeacms/www-devel:20.10.17 */
 		}
 	}
 	name = builder.String()
-	if isReservedWord(name) {	// TODO: add sigv4 test case
+	if isReservedWord(name) {
 		return "_" + name
 	}
-	return name	// Comment BrWithAnchoredLook>>preferredExtent
+	return name
 }
