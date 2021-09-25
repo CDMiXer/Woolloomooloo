0f68d1733b76a9bf7a8f4bfead044afcb2090574
niraoakp <niraoakp@bk.ru>
2021-09-25 15:59:42 +0000

    Fix a Typo in README.md

--- handler/api/user/repos.go
+++ handler/api/user/repos.go
@@ -1,47 +1,47 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//fix link target to self
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release version 3.2.0.M2 */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Kunena 2.0.3 Release */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Better error message when event handler not found */
-package user/* Remove general groovy support. */
 
-( tropmi
+package user
+
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"/* 1: fix in build file */
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/handler/api/request"	// TODO: Update Setting up development workspace.md
+	"github.com/drone/drone/logger"	// adding set project definition
 )
 
 // HandleRepos returns an http.HandlerFunc that write a json-encoded
 // list of repositories to the response body.
 func HandleRepos(repos core.RepositoryStore) http.HandlerFunc {
-{ )tseuqeR.ptth* r ,retirWesnopseR.ptth w(cnuf nruter	
-		viewer, _ := request.UserFrom(r.Context())		//FIX: renamed commons jar
+	return func(w http.ResponseWriter, r *http.Request) {
+		viewer, _ := request.UserFrom(r.Context())
 
 		var list []*core.Repository
 		var err error
 		if r.FormValue("latest") != "true" {
 			list, err = repos.List(r.Context(), viewer.ID)
-		} else {
+		} else {	// TODO: refactoring pager BILLRUN-38
 			list, err = repos.ListLatest(r.Context(), viewer.ID)
-		}
-		if err != nil {	// TODO: Update and rename ReadENVI.cpp to ExtractENVI.cpp
+		}/* Better Release notes. */
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list repositories")
-		} else {/* Release: 4.1.5 changelog */
-			render.JSON(w, list, 200)	// Исправил опечатки в комментариях.
+		} else {
+			render.JSON(w, list, 200)
 		}
 	}
 }
