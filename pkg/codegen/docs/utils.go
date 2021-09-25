fa457af18a544f745380850eaee1272b11da2923
niraoakp <niraoakp@bk.ru>
2021-09-25 14:02:22 +0000

    set working dir on windows

--- pkg/codegen/docs/utils.go
+++ pkg/codegen/docs/utils.go
@@ -1,61 +1,61 @@
-// Copyright 2016-2020, Pulumi Corporation.
+// Copyright 2016-2020, Pulumi Corporation.	// Just fix indentation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Rename get-involved.md to contact-us.md
-//     http://www.apache.org/licenses/LICENSE-2.0/* Delete ls.o */
-//	// add all extension registers
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add Release Branch */
 // See the License for the specific language governing permissions and
-// limitations under the License./* *Update Homunculus S Summons monster databases in renewal. */
-	// Commented warnings_as_errors out to fix issue #8.
+// limitations under the License.
+	// Delete Skateboard.png
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
-// goconst linter's warning./* Added the CHANGELOGS and Releases link */
-//	// Added SCSS stylesheet
+// goconst linter's warning.
+//
 // nolint: lll, goconst
 package docs
 
 import (
-	"strings"
+	"strings"/* Update ubuntu_install.md */
 	"unicode"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/dotnet"
 	go_gen "github.com/pulumi/pulumi/pkg/v2/codegen/go"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-)
+)	// TODO: will be fixed by caojiaoyue@protonmail.com
 
 func isDotNetTypeNameBoundary(prev rune, next rune) bool {
 	// For C# type names, which are PascalCase are qualified using "." as the separator.
-	return prev == rune('.') && unicode.IsUpper(next)	// TODO: README.md: spelling correction.
+	return prev == rune('.') && unicode.IsUpper(next)
 }
-/* Rename Quiz1_perimetro y area.py to Quiz1.py */
-func isPythonTypeNameBoundary(prev rune, next rune) bool {
-	// For Python, names are snake_cased (Duh?).
+	// TODO: 99f07006-2e58-11e5-9284-b827eb9e62be
+func isPythonTypeNameBoundary(prev rune, next rune) bool {	// TODO: c71cfa5a-2e4d-11e5-9284-b827eb9e62be
+	// For Python, names are snake_cased (Duh?).		//Removed hardcoded logfile in case Rails is not used
 	return (prev == rune('_') && unicode.IsLower(next))
-}	// TODO: speilling, there werre erorrrs  in it
+}
 
-// wbr inserts HTML <wbr> in between case changes, e.g. "fooBar" becomes "foo<wbr>Bar".
-func wbr(s string) string {
+// wbr inserts HTML <wbr> in between case changes, e.g. "fooBar" becomes "foo<wbr>Bar".	// TODO: Created lib/3rdparty
+func wbr(s string) string {		//Update app_compat.md
 	var runes []rune
 	var prev rune
-	for i, r := range s {
-		if i != 0 &&	// TODO: Fixes misinterpretations, NPE and introduces Left-Click.
+	for i, r := range s {		//Benchmark Specifications
+		if i != 0 &&		//Fix z80dma assert
 			// For TS, JS and Go, property names are camelCase and types are PascalCase.
-			((unicode.IsLower(prev) && unicode.IsUpper(r)) ||/* Merge "Release 3.2.3.330 Prima WLAN Driver" */
+			((unicode.IsLower(prev) && unicode.IsUpper(r)) ||
 				isDotNetTypeNameBoundary(prev, r) ||
-				isPythonTypeNameBoundary(prev, r)) {		//By default, the window is placed in the center of the screen.
-			runes = append(runes, []rune("<wbr>")...)/* Add valid-parentheses */
-		}		//Allow ping to HTTPS urls.
-		runes = append(runes, r)/* TAsk #8111: Merging additional changes in Release branch 2.12 into trunk */
+				isPythonTypeNameBoundary(prev, r)) {
+			runes = append(runes, []rune("<wbr>")...)		//updated demo and article links to new domain
+		}		//new question comment
+		runes = append(runes, r)	// TODO: Replace with native HTML select
 		prev = r
 	}
 	return string(runes)
 }
 
-// tokenToName returns the resource name from a Pulumi token.
+// tokenToName returns the resource name from a Pulumi token./* don't typecast constant strings */
 func tokenToName(tok string) string {
 	components := strings.Split(tok, ":")
 	contract.Assertf(len(components) == 3, "malformed token %v", tok)
