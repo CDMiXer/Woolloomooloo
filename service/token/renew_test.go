d7308cac3c7008ffdf51581bc76f40cd04066169
niraoakp <niraoakp@bk.ru>
2021-09-25 14:17:53 +0000

    Made Shape and ShapeRecord public, and readme newline fix

--- service/token/renew_test.go
+++ service/token/renew_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Allow the parameterise option to be turned off from the CLI
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release 0.8.0~exp3 */
-// +build !oss/* fix a minor typo in javadoc for variables */
-		//Fixed the fix for highlighting (typo)
+
+// +build !oss
+
 package token
