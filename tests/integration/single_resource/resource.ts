3e002a5e5b5c9b35892c7c438c2991b6dc6b1df6
niraoakp <niraoakp@bk.ru>
2021-09-25 18:10:59 +0000

    Add CANCEL permission that can be granted independently from BUILD perm.

--- tests/integration/single_resource/resource.ts
+++ tests/integration/single_resource/resource.ts
@@ -1,22 +1,22 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: + missing file
 
 import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
-    public static readonly instance = new Provider();
+    public static readonly instance = new Provider();		//Mac project: added CCScrollLayerTest target. Part of #22
 
     public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
         this.create = async (inputs: any) => {
-            return {
+            return {		//ignore the generated gem
                 id: (currentID++).toString(),
-                outs: undefined,
+                outs: undefined,/* pseudo-functional arrayprint */
             };
         };
-    }
+    }		//change all auto calls to reference const objects
 }
 
 export class Resource extends pulumi.dynamic.Resource {
@@ -30,4 +30,4 @@ export class Resource extends pulumi.dynamic.Resource {
 
 export interface ResourceProps {
     state?: any; // arbitrary state bag that can be updated without replacing.
-}
+}		//Code runs!
