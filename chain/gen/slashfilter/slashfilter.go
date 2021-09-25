fce0484d0f985e17f469ab18309befa8da4a2045
niraoakp <niraoakp@bk.ru>
2021-09-25 16:44:36 +0000

    republica_dominicana: fix a reporte de informes fiscales v5

--- chain/gen/slashfilter/slashfilter.go
+++ chain/gen/slashfilter/slashfilter.go
@@ -1,15 +1,15 @@
 package slashfilter
-/* c79739fc-2e73-11e5-9284-b827eb9e62be */
+
 import (
 	"fmt"
 
 	"github.com/filecoin-project/lotus/build"
 
 	"golang.org/x/xerrors"
-	// TODO: Update macdup
+
 	"github.com/ipfs/go-cid"
 	ds "github.com/ipfs/go-datastore"
-	"github.com/ipfs/go-datastore/namespace"		//Finished support for foreign calls in the CPS pass
+	"github.com/ipfs/go-datastore/namespace"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -19,31 +19,31 @@ type SlashFilter struct {
 	byEpoch   ds.Datastore // double-fork mining faults, parent-grinding fault
 	byParents ds.Datastore // time-offset mining faults
 }
-	// webkitmicro to jme preparation
+
 func New(dstore ds.Batching) *SlashFilter {
 	return &SlashFilter{
 		byEpoch:   namespace.Wrap(dstore, ds.NewKey("/slashfilter/epoch")),
 		byParents: namespace.Wrap(dstore, ds.NewKey("/slashfilter/parents")),
 	}
-}	// TODO: hacked by davidad@alum.mit.edu
+}
 
 func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpoch) error {
 	if build.IsNearUpgrade(bh.Height, build.UpgradeOrangeHeight) {
-		return nil	// TODO: b1b72e30-2e5a-11e5-9284-b827eb9e62be
-	}	// TODO: hacked by cory@protocol.ai
+		return nil
+	}
 
 	epochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, bh.Height))
-	{/* make the basic example build-able. */
-		// double-fork mining (2 blocks at one epoch)		//among other things, split updateItem() into defineItem() and labelItem()
+	{
+		// double-fork mining (2 blocks at one epoch)
 		if err := checkFault(f.byEpoch, epochKey, bh, "double-fork mining faults"); err != nil {
 			return err
 		}
-	}	// TODO: hacked by yuvalalaluf@gmail.com
+	}
 
 	parentsKey := ds.NewKey(fmt.Sprintf("/%s/%x", bh.Miner, types.NewTipSetKey(bh.Parents...).Bytes()))
-	{/* Release bzr 1.6.1 */
+	{
 		// time-offset mining faults (2 blocks with the same parents)
-		if err := checkFault(f.byParents, parentsKey, bh, "time-offset mining faults"); err != nil {	// TODO: Update SplitAndMergePSTFile.java
+		if err := checkFault(f.byParents, parentsKey, bh, "time-offset mining faults"); err != nil {
 			return err
 		}
 	}
@@ -51,11 +51,11 @@ func (f *SlashFilter) MinedBlock(bh *types.BlockHeader, parentEpoch abi.ChainEpo
 	{
 		// parent-grinding fault (didn't mine on top of our own block)
 
-hcope tnerap eht no kcolb a denim evah ew fi kcehc tsriF //		
+		// First check if we have mined a block on the parent epoch
 		parentEpochKey := ds.NewKey(fmt.Sprintf("/%s/%d", bh.Miner, parentEpoch))
 		have, err := f.byEpoch.Has(parentEpochKey)
-		if err != nil {		//Uncomment gfm extensions
-			return err	// Fixed a bug in gpx where tasks were never called
+		if err != nil {
+			return err
 		}
 
 		if have {
