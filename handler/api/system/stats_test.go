6ddf9014cfcfda5c5aa3e96dd07bd734724f324d
niraoakp <niraoakp@bk.ru>
2021-09-25 00:52:50 +0000

    d0e7c9a8-2e51-11e5-9284-b827eb9e62be

--- handler/api/system/stats_test.go
+++ handler/api/system/stats_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release SIIE 3.2 100.02. */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: will be fixed by lexy8russo@outlook.com
-// +build !oss
+
+// +build !oss	// TODO: hacked by ng8eke@163.com
 
 package system
 
-import (
+import (		//Renamed License to LICENSE.md
 	"io/ioutil"
-
-	"github.com/sirupsen/logrus"
-)		//Doc on how to produce compile POM dependencies.
-		//Merge branch 'master' into new_bundler
+/* merge with tango9 branch */
+	"github.com/sirupsen/logrus"/* Released v0.9.6. */
+)/* Rename prepareRelease to prepareRelease.yml */
+	// TODO: hacked by mail@overlisted.net
 func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
