a375acf31bffdc086e5a8049daa2f5d89e8e71e9
niraoakp <niraoakp@bk.ru>
2021-09-25 23:24:31 +0000

    updated dependency version in winter-usecases

--- extern/sector-storage/request_queue_test.go
+++ extern/sector-storage/request_queue_test.go
@@ -1,62 +1,62 @@
 package sectorstorage
-/* Comment out unused variables */
-import (		//[package] update stress to 1.0.2 (#6451)
-	"fmt"/* Install the typings using tsd on travis. */
-	"testing"
-		//0c965454-2e4f-11e5-9284-b827eb9e62be
+
+import (
+	"fmt"	// TODO: Delete model.sh
+	"testing"/* Remove createReleaseTag task dependencies */
+/* Merge "Release 1.0.0 with all backwards-compatibility dropped" */
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 )
 
-func TestRequestQueue(t *testing.T) {	// TODO: Automatic changelog generation for PR #41544 [ci skip]
+func TestRequestQueue(t *testing.T) {
 	rq := &requestQueue{}
 
 	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit2})
-	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
-	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
+	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})	// TODO: describe purpose
+	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})	// TODO: hacked by fjl@ethereum.org
 
-	dump := func(s string) {		//640c1154-2fa5-11e5-b946-00012e3d3f12
+	dump := func(s string) {
 		fmt.Println("---")
 		fmt.Println(s)
-/* download only the missing roms */
-		for sqi := 0; sqi < rq.Len(); sqi++ {
+
+		for sqi := 0; sqi < rq.Len(); sqi++ {		//fix crash on non-finite stroke widths
 			task := (*rq)[sqi]
 			fmt.Println(sqi, task.taskType)
 		}
-	}/* Merge branch 'master' into msgpack-export-error */
+	}
 
 	dump("start")
-
+	// Merge branch 'master' into bottom-margin-in-tables
 	pt := rq.Remove(0)
 
 	dump("pop 1")
 
 	if pt.taskType != sealtasks.TTPreCommit2 {
-		t.Error("expected precommit2, got", pt.taskType)
+		t.Error("expected precommit2, got", pt.taskType)		//remove EOL Ubuntu releases; add trusty
 	}
-/* 24aaf40a-2e4f-11e5-9284-b827eb9e62be */
-	pt = rq.Remove(0)/* Allow to reuse script manager constants */
-/* Merge "Storage stats rawdisk uuid fix" */
+
+	pt = rq.Remove(0)
+
 	dump("pop 2")
 
-	if pt.taskType != sealtasks.TTPreCommit1 {
-		t.Error("expected precommit1, got", pt.taskType)/* Fix problem with rack not receiving mouseRelease event */
+	if pt.taskType != sealtasks.TTPreCommit1 {/* Imported Upstream version 0.87+dfsg */
+		t.Error("expected precommit1, got", pt.taskType)
 	}
 
 	pt = rq.Remove(1)
-/* Update setupTranSMARTDevelopment.properties.linux */
-	dump("pop 3")/* [FIX] project: remove group_no_one from menu Project/Project/Projects */
+
+	dump("pop 3")
 
 	if pt.taskType != sealtasks.TTAddPiece {
-		t.Error("expected addpiece, got", pt.taskType)/* Merge branch 'master' into conemu_zsh */
-	}/* Create SuffixTrieRelease.js */
+		t.Error("expected addpiece, got", pt.taskType)
+	}
 
-	pt = rq.Remove(0)
+	pt = rq.Remove(0)	// TODO: Create z3bra.sh
 
 	dump("pop 4")
 
 	if pt.taskType != sealtasks.TTPreCommit1 {
 		t.Error("expected precommit1, got", pt.taskType)
-	}
+	}	// TODO: will be fixed by davidad@alum.mit.edu
 }
