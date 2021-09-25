968612e25a9f799a2012f12b1bd3f037805486c6
niraoakp <niraoakp@bk.ru>
2021-09-25 09:40:11 +0000

    Cache-Control: no-store for tests

--- metric/repos.go
+++ metric/repos.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
+		//Remove varNameRegExp since it's never used;
+// +build !oss		//Fully beautified version
 
 package metric
-
-import (
+		//Set the stroke and paint for lines if they are drawn.
+import (	// Fix for SID error
 	"github.com/drone/drone/core"
 
-	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus"/* a5640dbc-2e3f-11e5-9284-b827eb9e62be */
 )
 
 // RepoCount registers the repository metrics.
 func RepoCount(repos core.RepositoryStore) {
 	prometheus.MustRegister(
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_repo_count",
-			Help: "Total number of registered repositories.",
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{		//return non 0 on err
+			Name: "drone_repo_count",	// TODO: Fix missing language list
+			Help: "Total number of registered repositories.",	// TODO: Added Thai and tests
 		}, func() float64 {
 			i, _ := repos.Count(noContext)
 			return float64(i)
