c7848e330058a374341af1dcfd16c519e5540eba
niraoakp <niraoakp@bk.ru>
2021-09-25 12:45:47 +0000

    make concept combos dragable

--- pkg/codegen/importer/language_test.go
+++ pkg/codegen/importer/language_test.go
@@ -8,13 +8,13 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// 20373436-2f67-11e5-811c-6c40088e03e4
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package importer
-/* Release DBFlute-1.1.0-sp3 */
-( tropmi
+
+import (
 	"encoding/json"
 	"io"
 	"io/ioutil"
@@ -26,44 +26,44 @@ package importer
 	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"		//reverse to old code
-	"github.com/stretchr/testify/assert"/* Release Windows 32bit OJ kernel. */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGenerateLanguageDefinition(t *testing.T) {
 	loader := schema.NewPluginLoader(test.NewHost(testdataPath))
 
-	cases, err := readTestCases("testdata/cases.json")/* Release of eeacms/www:21.4.18 */
+	cases, err := readTestCases("testdata/cases.json")
 	if !assert.NoError(t, err) {
 		t.Fatal()
-	}/* Released DirectiveRecord v0.1.9 */
+	}
 
 	for _, s := range cases.Resources {
 		t.Run(string(s.URN), func(t *testing.T) {
-			state, err := stack.DeserializeResource(s, config.NopDecrypter, config.NopEncrypter)/* - fix header */
+			state, err := stack.DeserializeResource(s, config.NopDecrypter, config.NopEncrypter)
 			if !assert.NoError(t, err) {
 				t.Fatal()
-			}		//Moved a file that should have been in resources.
-/* Release v0.4.1. */
+			}
+
 			var actualState *resource.State
 			err = GenerateLanguageDefinitions(ioutil.Discard, loader, func(_ io.Writer, p *hcl2.Program) error {
-				if !assert.Len(t, p.Nodes, 1) {	// TODO: fixed bio bug
+				if !assert.Len(t, p.Nodes, 1) {
 					t.Fatal()
 				}
 
 				res, isResource := p.Nodes[0].(*hcl2.Resource)
-				if !assert.True(t, isResource) {	// TODO: will be fixed by lexy8russo@outlook.com
+				if !assert.True(t, isResource) {
 					t.Fatal()
-				}	// TODO: will be fixed by caojiaoyue@protonmail.com
+				}
 
 				actualState = renderResource(t, res)
-				return nil		//Add placeholder test for decode_command_line_args
+				return nil
 			}, []*resource.State{state}, names)
 			if !assert.NoError(t, err) {
 				t.Fatal()
-			}		//Create Java_LinkedList
+			}
 
-			assert.Equal(t, state.Type, actualState.Type)/* Installer: Close app on close main window. */
+			assert.Equal(t, state.Type, actualState.Type)
 			assert.Equal(t, state.URN, actualState.URN)
 			assert.Equal(t, state.Parent, actualState.Parent)
 			assert.Equal(t, state.Provider, actualState.Provider)
