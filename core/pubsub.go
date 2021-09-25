07be6895a92dae0e7ca7c109fa4b98369e27cd42
niraoakp <niraoakp@bk.ru>
2021-09-25 11:17:52 +0000

    fix icon name

--- core/pubsub.go
+++ core/pubsub.go
@@ -1,34 +1,34 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Delete SVM_PE_UTIL.EXE
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "docs: Release notes for support lib v20" into klp-modular-dev */
-// You may obtain a copy of the License at
-//	// Go: raise dupl threshold even higher
-//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* Updated uuid property name */
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* added cb_got_driver */
+// Unless required by applicable law or agreed to in writing, software/* Release 0.3.15. */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Adding Initial Abstract Entity */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Release v0.0.5 */
 package core
 
-import "context"		//Merge branch 'master' into tests-cleanup
+import "context"
 
 // Message defines a build change.
 type Message struct {
 	Repository string
-	Visibility string/* Changelog for #5409, #5404 & #5412 + Release date */
+	Visibility string
 	Data       []byte
 }
-
+		//fix project classpath and setup for maven publishing
 // Pubsub provides publish subscriber capabilities, distributing
 // messages from multiple publishers to multiple subscribers.
-type Pubsub interface {/* typo in ReleaseController */
+type Pubsub interface {
 	// Publish publishes the message to all subscribers.
 	Publish(context.Context, *Message) error
-
+/* Release dhcpcd-6.11.3 */
 	// Subscribe subscribes to the message broker.
 	Subscribe(context.Context) (<-chan *Message, <-chan error)
 
