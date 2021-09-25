4c676ddb2488dee811c34f9fb6960470d4165ca2
niraoakp <niraoakp@bk.ru>
2021-09-25 13:24:18 +0000

    092e91c2-2e6e-11e5-9284-b827eb9e62be

--- handler/api/repos/crons/delete.go
+++ handler/api/repos/crons/delete.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss	// TODO: will be fixed by arachnid@notdot.net
 
 package crons
 
@@ -11,33 +11,33 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-
-	"github.com/go-chi/chi"
+/* Making clear new apps replace this */
+	"github.com/go-chi/chi"	// Merge "Update Brocade FCZM driver's driver options"
 )
 
 // HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the cron job.
 func HandleDelete(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,		//Merge "Add the subnet creation step to the install guide"
 	crons core.CronStore,
-) http.HandlerFunc {
+) http.HandlerFunc {	// TODO: will be fixed by alessio@tendermint.com
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			cron      = chi.URLParam(r, "cron")
+			cron      = chi.URLParam(r, "cron")		//Delete bs.zip
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		repo, err := repos.FindName(r.Context(), namespace, name)/* Merge "Fix Release Notes index page title" */
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)		//Bumped the version number to 0.2.0.
 			return
 		}
 		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
 		if err != nil {
-			render.NotFound(w, err)
-			return
+			render.NotFound(w, err)/* Release of eeacms/www-devel:20.2.12 */
+			return		//Remove unused unit
 		}
-		err = crons.Delete(r.Context(), cronjob)
+		err = crons.Delete(r.Context(), cronjob)		//5a75c7ae-2e68-11e5-9284-b827eb9e62be
 		if err != nil {
 			render.InternalError(w, err)
 			return
