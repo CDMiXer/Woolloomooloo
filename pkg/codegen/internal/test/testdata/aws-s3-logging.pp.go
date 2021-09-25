117f075e4c66e03fc5661ae60463a0315518ff9d
niraoakp <niraoakp@bk.ru>
2021-09-25 15:53:17 +0000

    Merge branch 'master' into controlsCreditsHud

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
@@ -3,25 +3,25 @@ package main
 import (
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
+)	// TODO: will be fixed by jon@atack.com
 
-func main() {
+func main() {/* Create projects pages + Add keywords style */
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		logs, err := s3.NewBucket(ctx, "logs", nil)
-		if err != nil {
-			return err
+		if err != nil {	// TODO: Merge "ID: 3611758 Implementation of option to display all versions of lab"
+			return err	// TODO: will be fixed by caojiaoyue@protonmail.com
 		}
-		bucket, err := s3.NewBucket(ctx, "bucket", &s3.BucketArgs{
+		bucket, err := s3.NewBucket(ctx, "bucket", &s3.BucketArgs{/* Added sketch example */
 			Loggings: s3.BucketLoggingArray{
 				&s3.BucketLoggingArgs{
 					TargetBucket: logs.Bucket,
-				},
+				},/* Changelog for v3 */
 			},
 		})
-		if err != nil {
+		if err != nil {	// TODO: hacked by martin2cai@hotmail.com
 			return err
 		}
-		ctx.Export("targetBucket", bucket.Loggings.ApplyT(func(loggings []s3.BucketLogging) (string, error) {
+		ctx.Export("targetBucket", bucket.Loggings.ApplyT(func(loggings []s3.BucketLogging) (string, error) {/* Update target definitions following the KNIME 3.6 Release */
 			return loggings[0].TargetBucket, nil
 		}).(pulumi.StringOutput))
 		return nil
