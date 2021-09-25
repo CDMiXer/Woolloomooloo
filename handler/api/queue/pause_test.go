7a9308909aa91a0637d8c70f9e3c3dd5790952f5
niraoakp <niraoakp@bk.ru>
2021-09-25 15:14:14 +0000

    prepare version 2.59

--- handler/api/queue/pause_test.go
+++ handler/api/queue/pause_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Removed hashbang because the script will never work on a sensible OS.
 // that can be found in the LICENSE file.
-
+		//CLI method to modify all transactions in search results.
 // +build !oss
 
-package queue
+package queue		//Non blocking error when load/save resolved
