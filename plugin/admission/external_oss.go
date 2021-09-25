3a58279437324de616f18e063a2f8d2e92e558be
niraoakp <niraoakp@bk.ru>
2021-09-25 13:35:08 +0000

    Remove 3 useless files

--- plugin/admission/external_oss.go
+++ plugin/admission/external_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* links plus description */
-// You may obtain a copy of the License at	// TODO: will be fixed by mail@bitpshr.net
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Regenerate schema */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Merge branch '1.0.0' into 1372-improve-sql-loader */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [TDA7297Kit] revise schematic */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// Terminado ejercicio 1
+
 // +build oss
 
 package admission
 
-import "github.com/drone/drone/core"/* Release v0.2.0 summary */
+import "github.com/drone/drone/core"
 
-// External is a no-op admission controller		//Changed the date format.
+// External is a no-op admission controller
 func External(string, string, bool) core.AdmissionService {
 	return new(noop)
 }
