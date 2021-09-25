1ffa55a00d40b0cbd13f6e7f04f719d08d103111
niraoakp <niraoakp@bk.ru>
2021-09-25 17:54:24 +0000

    Add time to error info

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 // Step 6: Delete everything:
-// * Elide a (Delete(a5)).	// TODO: Use string_value() in metadata. 
+// * Elide a (Delete(a5)).
 // * Elide c (Delete(c)).
 // * Elide e (Delete(e)).
 // * Pending delete (Delete(a4)).
