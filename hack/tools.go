22bc44e47300b174ab9cdd5fa5108b3eace7a73a
niraoakp <niraoakp@bk.ru>
2021-09-25 03:30:29 +0000

    Update Safety Pharmacology Analysis.R

--- hack/tools.go
+++ hack/tools.go
@@ -1,12 +1,12 @@
-// +build tools
+// +build tools/* remove jdt core update site from setup */
 
 // This package contains code generation utilities
-// This package imports things required by build scripts, to force `go mod` to see them as dependencies	// Correct INFO=4 condition
-package tools		//Fix error with unreachable state of host
-/* Release version 0.1.21 */
+// This package imports things required by build scripts, to force `go mod` to see them as dependencies
+package tools/* 5c9300ee-2e43-11e5-9284-b827eb9e62be */
+
 import (
 	_ "bou.ke/staticfiles"
-	_ "github.com/go-swagger/go-swagger/cmd/swagger"/* Release of eeacms/www-devel:20.4.22 */
+	_ "github.com/go-swagger/go-swagger/cmd/swagger"/* Merge "Cleans nullable=True in db model column declaration" */
 	_ "github.com/gogo/protobuf/gogoproto"
 	_ "github.com/gogo/protobuf/protoc-gen-gogo"
 	_ "github.com/gogo/protobuf/protoc-gen-gogofast"
@@ -15,7 +15,7 @@ import (
 	_ "github.com/jstemmer/go-junit-report"
 	_ "github.com/mattn/goreman"
 	_ "golang.org/x/tools/cmd/goimports"
-	_ "k8s.io/code-generator"
+	_ "k8s.io/code-generator"		//e1b48908-2e45-11e5-9284-b827eb9e62be
 	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
 	_ "k8s.io/code-generator/cmd/defaulter-gen"
@@ -24,7 +24,7 @@ import (
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
 	_ "k8s.io/code-generator/cmd/openapi-gen"
-	_ "k8s.io/code-generator/cmd/register-gen"	// TODO: data source initialization inprovements
+	_ "k8s.io/code-generator/cmd/register-gen"	// TODO: will be fixed by caojiaoyue@protonmail.com
 	_ "k8s.io/code-generator/cmd/set-gen"
 	_ "k8s.io/kube-openapi/cmd/openapi-gen"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
