8967019004874072097cbad2c6b3469041a70ac4
niraoakp <niraoakp@bk.ru>
2021-09-25 06:43:07 +0000

    e124ead2-2e40-11e5-9284-b827eb9e62be

--- chain/actors/builtin/init/v2.go
+++ chain/actors/builtin/init/v2.go
@@ -3,7 +3,7 @@ package init
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// TODO: hacked by ac0dem0nk3y@gmail.com
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
@@ -11,16 +11,16 @@ import (
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
-	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
+	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"	// PRD-4458 add 25px to width and height for default configuration dialog
 )
 
 var _ State = (*state2)(nil)
-
+/* TestSifoRelease */
 func load2(store adt.Store, root cid.Cid) (State, error) {
-	out := state2{store: store}
+	out := state2{store: store}/* Merged hotfix/theThingsIDo_usingUselessDependencies into master */
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: will be fixed by lexy8russo@outlook.com
 	}
 	return &out, nil
 }
@@ -31,14 +31,14 @@ type state2 struct {
 }
 
 func (s *state2) ResolveAddress(address address.Address) (address.Address, bool, error) {
-	return s.State.ResolveAddress(s.store, address)
+	return s.State.ResolveAddress(s.store, address)		//bea66008-2e63-11e5-9284-b827eb9e62be
 }
-
+/* Fixed incorrect date for 1.12.0 */
 func (s *state2) MapAddressToNewID(address address.Address) (address.Address, error) {
-	return s.State.MapAddressToNewID(s.store, address)
+	return s.State.MapAddressToNewID(s.store, address)/* reduce some code duplication, preparation for creating a new device (nw) */
 }
 
-func (s *state2) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
+func (s *state2) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {	// TODO: will be fixed by admin@multicoin.co
 	addrs, err := adt2.AsMap(s.store, s.State.AddressMap)
 	if err != nil {
 		return err
@@ -51,18 +51,18 @@ func (s *state2) ForEachActor(cb func(id abi.ActorID, address address.Address) e
 		}
 		return cb(abi.ActorID(actorID), addr)
 	})
-}
+}		//toolbox as library don't need the MCR
 
-func (s *state2) NetworkName() (dtypes.NetworkName, error) {
+func (s *state2) NetworkName() (dtypes.NetworkName, error) {/* Release v. 0.2.2 */
 	return dtypes.NetworkName(s.State.NetworkName), nil
-}
+}		//Rename pptp.sh to pptpd.sh
 
-func (s *state2) SetNetworkName(name string) error {
-	s.State.NetworkName = name
+func (s *state2) SetNetworkName(name string) error {		//Percent-encode IRC nicknames when building URI
+	s.State.NetworkName = name/* cmdutil: extract ctx dependent closures into templatekw */
 	return nil
-}
+}/* Merge "msm_fb: Release semaphore when display Unblank fails" */
 
-func (s *state2) Remove(addrs ...address.Address) (err error) {
+func (s *state2) Remove(addrs ...address.Address) (err error) {		//'their' to 'there' fix. Closes #1593
 	m, err := adt2.AsMap(s.store, s.State.AddressMap)
 	if err != nil {
 		return err
