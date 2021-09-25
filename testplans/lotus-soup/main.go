620de973094f4911bbc5bc5d828b6c4f69a9fd72
niraoakp <niraoakp@bk.ru>
2021-09-25 18:24:07 +0000

    Added path in cookie to correct location

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -8,17 +8,17 @@ import (
 	"github.com/testground/sdk-go/run"
 )
 
-var cases = map[string]interface{}{		//Create lhbk
+var cases = map[string]interface{}{
 	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
 	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
 	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
-	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),/* Release 0.11.0. */
+	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
 	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
 }
 
 func main() {
 	sanityCheck()
 
-	run.InvokeMap(cases)/* Merge "Release 3.2.3.480 Prima WLAN Driver" */
+	run.InvokeMap(cases)
 }
