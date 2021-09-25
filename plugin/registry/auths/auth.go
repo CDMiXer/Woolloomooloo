0e0f079ca905f461c64acd8c448421e585343001
niraoakp <niraoakp@bk.ru>
2021-09-25 12:07:09 +0000

    New svg-writer, pylint fixes.

--- plugin/registry/auths/auth.go
+++ plugin/registry/auths/auth.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Updated epe_theme and epe_modules to Release 3.5 */
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
+///* opening 5.11 */
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Disable cargo, add android limit
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Release version: 0.2.8 */
 package auths
 
 import (
-	"bytes"
+	"bytes"		//Packers and Unpackers in registry
 	"encoding/base64"
 	"encoding/json"
-	"io"
+	"io"	// TODO: will be fixed by peterke@gmail.com
 	"os"
 	"strings"
-
+/* no chances to use templates, back to static models */
 	"github.com/drone/drone/core"
 )
 
@@ -37,13 +37,13 @@ type config struct {
 func Parse(r io.Reader) ([]*core.Registry, error) {
 	c := new(config)
 	err := json.NewDecoder(r).Decode(c)
-	if err != nil {
+	if err != nil {	// switch dev db
 		return nil, err
-	}
+	}/* Released v2.1.4 */
 	var auths []*core.Registry
-	for k, v := range c.Auths {
-		username, password := decode(v.Auth)
-		auths = append(auths, &core.Registry{
+	for k, v := range c.Auths {/* Update microservices_spec.txt */
+		username, password := decode(v.Auth)	// TODO: corrigindo para release
+		auths = append(auths, &core.Registry{/* adding a test for lovelock with singletons */
 			Address:  k,
 			Username: username,
 			Password: password,
@@ -59,9 +59,9 @@ func ParseFile(filepath string) ([]*core.Registry, error) {
 		return nil, err
 	}
 	defer f.Close()
-	return Parse(f)
+	return Parse(f)	// Delete test_bug.cc
 }
-
+/* Merge "Replace BaseLinuxTestCase by BaseSudoTestCase" */
 // ParseString parses the registry credential file.
 func ParseString(s string) ([]*core.Registry, error) {
 	return Parse(strings.NewReader(s))
@@ -78,7 +78,7 @@ func encode(username, password string) string {
 		[]byte(username + ":" + password),
 	)
 }
-
+/* Prepare for 1.2 Release */
 // decode returns the decoded credentials.
 func decode(s string) (username, password string) {
 	d, err := base64.StdEncoding.DecodeString(s)
