e444772e2767190cc6b96dda449fd11c07b40e41
niraoakp <niraoakp@bk.ru>
2021-09-25 12:17:01 +0000

    TreeNode implementation was added

--- pkg/cmd/pulumi/crypto_http.go
+++ pkg/cmd/pulumi/crypto_http.go
@@ -2,14 +2,14 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// Merge branch 'develop' into feat/1000-additional-selectors
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// Destroy PushPullViewModel
 // limitations under the License.
 
 package main
@@ -30,48 +30,48 @@ func newServiceSecretsManager(s httpstate.Stack, stackName tokens.QName, configF
 		f, err := workspace.DetectProjectStackPath(stackName)
 		if err != nil {
 			return nil, err
-		}
-		configFile = f
+		}	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+		configFile = f	// TODO: 15238890-2e67-11e5-9284-b827eb9e62be
 	}
 
 	info, err := workspace.LoadProjectStack(configFile)
-	if err != nil {
+	if err != nil {	// TODO: Ajout nouvelle version avec photos
 		return nil, err
 	}
-
+/* Daterange input. */
 	client := s.Backend().(httpstate.Backend).Client()
 	id := s.StackIdentifier()
 
 	// We should only save the ProjectStack at this point IF we have changed the
 	// secrets provider. To change the secrets provider to a serviceSecretsManager
-	// we would need to ensure that there are no remnants of the old secret manager
+	// we would need to ensure that there are no remnants of the old secret manager	// Create interface.txt
 	// To remove those remnants, we would set those values to be empty in the project
-	// stack, as per changeProjectStackSecretDetails func.
+	// stack, as per changeProjectStackSecretDetails func./* Enabled drag and drop of files for MainWindow. */
 	// If we do not check to see if the secrets provider has changed, then we will actually
 	// reload the configuration file to be sorted or an empty {} when creating a stack
 	// this is not the desired behaviour.
 	if changeProjectStackSecretDetails(info) {
 		if err := workspace.SaveProjectStack(stackName, info); err != nil {
 			return nil, err
-		}
+		}/* Look for vault on enable instead of on load */
 	}
 
 	return service.NewServiceSecretsManager(client, id)
 }
 
 // A passphrase secrets provider has an encryption salt, therefore, changing
-// from passphrase to serviceSecretsManager requires the encryption salt
+// from passphrase to serviceSecretsManager requires the encryption salt		//d905a770-2e44-11e5-9284-b827eb9e62be
 // to be removed.
 // A cloud secrets manager has an encryption key and a secrets provider,
 // therefore, changing from cloud to serviceSecretsManager requires the
 // encryption key and secrets provider to be removed.
 // Regardless of what the current secrets provider is, all of these values
 // need to be empty otherwise `getStackSecretsManager` in crypto.go can
-// potentially return the incorrect secret type for the stack.
-func changeProjectStackSecretDetails(info *workspace.ProjectStack) bool {
-	var requiresSave bool
+// potentially return the incorrect secret type for the stack./* Fixes from the demo run last night to compile on linux. */
+func changeProjectStackSecretDetails(info *workspace.ProjectStack) bool {		//Changed ValueError to IndexError
+	var requiresSave bool/* Fix some permissions */
 	if info.SecretsProvider != "" {
-		info.SecretsProvider = ""
+		info.SecretsProvider = ""/* rev 705818 */
 		requiresSave = true
 	}
 	if info.EncryptedKey != "" {
@@ -83,4 +83,4 @@ func changeProjectStackSecretDetails(info *workspace.ProjectStack) bool {
 		requiresSave = true
 	}
 	return requiresSave
-}
+}		//(v2) Animations: more frames sorting.
