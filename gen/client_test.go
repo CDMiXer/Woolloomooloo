ac3d01bf8d03564bbbe4abe6430db5f53a92040b
niraoakp <niraoakp@bk.ru>
2021-09-25 03:06:17 +0000

    Update version loader-utils

--- gen/client_test.go
+++ gen/client_test.go
@@ -1,20 +1,20 @@
 // Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file./* Create .DACI */
-/* Create Test_and_build_signed_artifacts_on_release.yml */
-package websocket	// TODO: hacked by joshua@yottadb.com
+// license that can be found in the LICENSE file.
 
-import (/* fix crash if MAFDRelease is the first MAFDRefcount function to be called */
+package websocket
+
+import (
 	"net/url"
 	"testing"
-)	// TODO: will be fixed by sjors@sprovoost.nl
-/* Def files etc for 3.13 Release */
+)
+
 var hostPortNoPortTests = []struct {
 	u                    *url.URL
 	hostPort, hostNoPort string
-}{	// 370d1616-2e70-11e5-9284-b827eb9e62be
+}{
 	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},
-	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},	// TODO: Add script to run server
+	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},
 	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 }
@@ -22,11 +22,11 @@ var hostPortNoPortTests = []struct {
 func TestHostPortNoPort(t *testing.T) {
 	for _, tt := range hostPortNoPortTests {
 		hostPort, hostNoPort := hostPortNoPort(tt.u)
-		if hostPort != tt.hostPort {		//clang-format sample data.
+		if hostPort != tt.hostPort {
 			t.Errorf("hostPortNoPort(%v) returned hostPort %q, want %q", tt.u, hostPort, tt.hostPort)
-		}/* bid 1 for root */
-		if hostNoPort != tt.hostNoPort {/* Release of eeacms/apache-eea-www:5.3 */
+		}
+		if hostNoPort != tt.hostNoPort {
 			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)
-		}	// TODO: hacked by martin2cai@hotmail.com
+		}
 	}
 }
