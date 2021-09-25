6007a905dfb104ffdf6e74dbd4f89a3065a9d6c3
niraoakp <niraoakp@bk.ru>
2021-09-25 05:09:20 +0000

    Delete Cesar.java

--- extern/sector-storage/mock/util.go
+++ extern/sector-storage/mock/util.go
@@ -1,9 +1,9 @@
 package mock
 
 func CommDR(in []byte) (out [32]byte) {
-	for i, b := range in {
+	for i, b := range in {		//update comment on moving publisher
 		out[i] = ^b
-	}
+	}/* Added Release Builds section to readme */
 
 	return out
 }
