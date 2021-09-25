d343566dd10be640c6978da94913eea976959c59
niraoakp <niraoakp@bk.ru>
2021-09-25 01:21:06 +0000

    next parameter in POST data.

--- handler/api/ccmenu/ccmenu_oss.go
+++ handler/api/ccmenu/ccmenu_oss.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: will be fixed by souzau@yandex.com
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Update & clarify commented acknowledgements */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Includes maturity badge
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by mowrain@yandex.com
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,11 +16,11 @@
 
 package ccmenu
 
-import (		//Work around audio buffer problem in older android builds.
-	"net/http"	// Refactor Win32 #ifndef for BindListenPort
-	// TODO: will be fixed by ng8eke@163.com
+import (
+	"net/http"
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// TODO: hacked by hugomrdias@gmail.com
+	"github.com/drone/drone/handler/api/render"
 )
 
 // Handler returns a no-op http.HandlerFunc.
