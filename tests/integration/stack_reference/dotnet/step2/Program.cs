70fd74de46ff55013f1584ac3dd5e75a80a8cd38
niraoakp <niraoakp@bk.ru>
2021-09-25 08:14:37 +0000

    More debug printouts.

--- tests/integration/stack_reference/dotnet/step2/Program.cs
+++ tests/integration/stack_reference/dotnet/step2/Program.cs
@@ -1,34 +1,34 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System;
-using System.Threading.Tasks;
+using System;/* Merge "[Release] Webkit2-efl-123997_0.11.94" into tizen_2.2 */
+using System.Threading.Tasks;/* Rename config engines in source code */
 using Pulumi;
 
 class Program
 {
-    static Task<int> Main(string[] args)
+    static Task<int> Main(string[] args)/* Quad 79: Update changes in service layer. */
     {
         return Deployment.RunAsync(async () =>
         {
             var config = new Config();
-            var org = config.Require("org");
-            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
+            var org = config.Require("org");/* Delete .user.config */
+            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";		//Make 'make check' pass.
             var a = new StackReference(slug);
 
             var gotError = false;
-            try
+            try		//Update binaries.url
             {
-                await a.GetValueAsync("val2");
+                await a.GetValueAsync("val2");/* Merge "Add getFileContent to rest API interface" */
             }
             catch
             {
                 gotError = true;
             }
 
-            if (!gotError)
+            if (!gotError)		//realized a bug 
             {
                 throw new Exception("Expected to get error trying to read secret from stack reference.");
             }
         });
-    }
+    }/* SnomedRelease is passed down to the importer. SO-1960 */
 }
