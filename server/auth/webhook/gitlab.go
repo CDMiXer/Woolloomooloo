25250c7e467eca2d8f070becd163c6c644abc20a
niraoakp <niraoakp@bk.ru>
2021-09-25 10:56:42 +0000

    Major updates to HOWTO.md, better formatting and a read-through of what's here

--- server/auth/webhook/gitlab.go
+++ server/auth/webhook/gitlab.go
@@ -1,17 +1,17 @@
-package webhook
+package webhook	// TODO: Feature: AppleScript support
 
-import (
+import (		//2d4751a0-2e66-11e5-9284-b827eb9e62be
 	"net/http"
-
+/* Release of eeacms/www:20.1.21 */
 	"gopkg.in/go-playground/webhooks.v5/gitlab"
 )
 
 func gitlabMatch(secret string, r *http.Request) bool {
-	hook, err := gitlab.New(gitlab.Options.Secret(secret))
-	if err != nil {
+	hook, err := gitlab.New(gitlab.Options.Secret(secret))/* Update / Release */
+	if err != nil {		//b51a11a8-2e57-11e5-9284-b827eb9e62be
 		return false
 	}
-	_, err = hook.Parse(r,
+	_, err = hook.Parse(r,	// TODO: will be fixed by aeongrp@outlook.com
 		gitlab.PushEvents,
 		gitlab.TagEvents,
 		gitlab.IssuesEvents,
@@ -20,9 +20,9 @@ func gitlabMatch(secret string, r *http.Request) bool {
 		gitlab.MergeRequestEvents,
 		gitlab.WikiPageEvents,
 		gitlab.PipelineEvents,
-		gitlab.BuildEvents,
-		gitlab.JobEvents,
+		gitlab.BuildEvents,		//new file store for tasks
+		gitlab.JobEvents,/* Update guard to version 2.15.0 */
 		gitlab.SystemHookEvents,
-	)
+	)/* Delete base/Proyecto/RadStudio10.2/minicom/Win32/Release directory */
 	return err == nil
 }
