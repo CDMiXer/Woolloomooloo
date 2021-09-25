24c54623006390a7790885f4ff4daba4edcf3b34
niraoakp <niraoakp@bk.ru>
2021-09-25 20:53:47 +0000

    Correction connexion

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
 -- name: create-table-org-secrets
 
-CREATE TABLE IF NOT EXISTS orgsecrets (/* servercfgfullpath */
+CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
 ,secret_namespace         TEXT COLLATE NOCASE
 ,secret_name              TEXT COLLATE NOCASE
 ,secret_type              TEXT
-,secret_data              BLOB	// TODO: hacked by nick@perfectabstractions.com
-,secret_pull_request      BOOLEAN	// TODO: hacked by hugomrdias@gmail.com
+,secret_data              BLOB
+,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
 );
