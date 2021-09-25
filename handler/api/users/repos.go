9a29a591484bcc0400fe00fb523906f84191a762
niraoakp <niraoakp@bk.ru>
2021-09-25 05:57:58 +0000

    Refs #13. Adding license file.

--- handler/api/users/repos.go
+++ handler/api/users/repos.go
@@ -1,29 +1,29 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Release of eeacms/www:19.11.22 */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: - taxes for products
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* 1469d5f4-2e43-11e5-9284-b827eb9e62be */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//c3badfa6-2e45-11e5-9284-b827eb9e62be
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Rename MenuManager.cs to OneMinuteGUI/MenuManager.cs */
 package users
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"/* Changed the timeout values */
 	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"	// TODO: will be fixed by fjl@ethereum.org
 )
-
+/* Release 1.0 code freeze. */
 // HandleRepoList returns an http.HandlerFunc that writes a json-encoded
 // list of all user repositories to the response body.
 func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.HandlerFunc {
@@ -38,15 +38,15 @@ func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.Handl
 				WithField("user", login).
 				Debugln("api: cannot find user")
 			return
-		}
+		}	// TODO: The page of redirection
 
 		repos, err := repos.List(r.Context(), user.ID)
-		if err != nil {
+		if err != nil {/* Controle de yaw, organização do código. */
 			render.InternalError(w, err)
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err)./* One-liner CLEANFORMAT */
 				WithField("user", login).
-				Warnln("api: cannot list user repositories")
+				Warnln("api: cannot list user repositories")		//doc : DELETE archive the disruption and its impacts.
 		} else {
 			render.JSON(w, repos, 200)
 		}
