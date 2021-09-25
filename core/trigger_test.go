7c0d422b99bf1b36be80e26d4f798fe0eb41fbea
niraoakp <niraoakp@bk.ru>
2021-09-25 00:17:55 +0000

    Replace available languages

--- core/trigger_test.go
+++ core/trigger_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by nicksavers@gmail.com
+// Use of this source code is governed by the Drone Non-Commercial License/* test fail on error */
 // that can be found in the LICENSE file.
-		//Merge "Add ChangeLog to tarball."
-// +build !oss	// TODO: Merge branch 'next-design-iteration' into patch-1
+	// TODO: will be fixed by why@ipfs.io
+// +build !oss
 
-package core
+package core	// TODO: hacked by witek@enjin.io
