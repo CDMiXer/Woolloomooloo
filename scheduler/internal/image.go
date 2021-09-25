8d34d5a01362c54fb5ed3109cdd45b26b35c10f8
niraoakp <niraoakp@bk.ru>
2021-09-25 21:51:37 +0000

    Inserção de select list e life ao personagem

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -1,18 +1,18 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* use exit code from 'error' */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss		//Delete ecoli.fa
 
-package internal/* Generalize key cleaning even more */
+package internal
 
 var defaultImage = "drone/controller:1"
-	// TODO: hacked by 13860583249@yeah.net
-// DefaultImage returns the default dispatch image if none
-// is specified./* "Release 0.7.0" (#103) */
+/* increaded build number */
+// DefaultImage returns the default dispatch image if none	// TODO: Added $EXTRA_PADDING_IN_MBS
+// is specified.
 func DefaultImage(image string) string {
 	if image == "" {
 		return defaultImage
-	}
+	}/* Released springjdbcdao version 1.9.11 */
 	return image
 }
