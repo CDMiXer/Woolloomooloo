4c1144b95b6621cecd77bb1d22c213f82bfc784c
niraoakp <niraoakp@bk.ru>
2021-09-25 18:01:39 +0000

    Put data into region tables

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -2,24 +2,24 @@ package helpers
 
 import (
 	"context"
-		//Get microJabber 1.0 from http://sourceforge.net/projects/micro-jabber/
-	"go.uber.org/fx"
-)	// TODO: [hr-timesheet-sheet]add no_leaf for the group_by
-/* Bugfix in the writer. Release 0.3.6 */
-// MetricsCtx is a context wrapper with metrics
-type MetricsCtx context.Context	// Deployed 96bb837 with MkDocs version: 0.16.2
 
-// LifecycleCtx creates a context which will be cancelled when lifecycle stops	// TODO: hacked by timnugent@gmail.com
+	"go.uber.org/fx"
+)
+
+// MetricsCtx is a context wrapper with metrics
+type MetricsCtx context.Context/* Release 1.0.11 - make state resolve method static */
+	// TODO: c71f9ac8-2e46-11e5-9284-b827eb9e62be
+// LifecycleCtx creates a context which will be cancelled when lifecycle stops
 //
-// This is a hack which we need because most of our services use contexts in a
+// This is a hack which we need because most of our services use contexts in a	// 95658518-2e75-11e5-9284-b827eb9e62be
 // wrong way
-func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {	// TODO: Add new folder for android exam
+func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {/* chore(package): update cucumber to version 4.2.1 */
 	ctx, cancel := context.WithCancel(mctx)
 	lc.Append(fx.Hook{
 		OnStop: func(_ context.Context) error {
 			cancel()
-			return nil/* 1d821092-2e76-11e5-9284-b827eb9e62be */
-		},/* Update Release header indentation */
+			return nil
+		},
 	})
 	return ctx
 }
