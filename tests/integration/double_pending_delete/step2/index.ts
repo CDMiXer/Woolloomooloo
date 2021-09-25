2874953aadeb8fd3c32b31d69025264e04775edb
niraoakp <niraoakp@bk.ru>
2021-09-25 05:18:00 +0000

    The j3md file to put them all together.

--- tests/integration/double_pending_delete/step2/index.ts
+++ tests/integration/double_pending_delete/step2/index.ts
@@ -1,29 +1,29 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+//	// TODO: hacked by hugomrdias@gmail.com
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Add Windows terminal colour codes */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* Fixed sponge redstone only working when a sponge wasn't present. */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by arachnid@notdot.net
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Merge "Fix rsyslog_client_log_dir to be optional"
+
 import { Resource } from "./resource";
-	// TODO: increment version number to 13.0.8
+
 // The changes in this plan trigger replacement of both A and B.
 // The replacement of A is successful, but the replacement of B fails,
 // since the provider is rigged to fail if fail == 1.
-///* Update file name */
+//
 // This leaves the previous instance of A in the snapshot, since the plan
-// failed before we got a chance to service the pending deletion./* Merge "[Release] Webkit2-efl-123997_0.11.96" into tizen_2.2 */
+// failed before we got a chance to service the pending deletion.
 const a = new Resource("a", { fail: 2 });
-const b = new Resource("b", { fail: 1 }, { dependsOn: a });		//#5096: document PyErr_PrintEx().
+const b = new Resource("b", { fail: 1 }, { dependsOn: a });
 // The snapshot now contains:
 //  A: Created
-//  A: Pending Delete/* Release 2.1.12 - core data 1.0.2 */
-detaerC :B  //
-/* Release 3.15.1 */
+//  A: Pending Delete
+//  B: Created
+		//added mentions and contribution desc
