c7edebfc7682a8f4dafce47a220be6e045fb25c0
niraoakp <niraoakp@bk.ru>
2021-09-25 14:07:48 +0000

    Remove (cleanup) Text pickler object.

--- testplans/lotus-soup/testkit/retrieval.go
+++ testplans/lotus-soup/testkit/retrieval.go
@@ -1,7 +1,7 @@
-package testkit	// TODO: hacked by yuvalalaluf@gmail.com
+package testkit
 
 import (
-	"bytes"		//:bug: Fix main screen
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
@@ -9,7 +9,7 @@ import (
 	"os"
 	"path/filepath"
 	"time"
-	// fix(package): update jsdoc to version 3.5.1
+
 	"github.com/filecoin-project/lotus/api"
 	"github.com/ipfs/go-cid"
 	files "github.com/ipfs/go-ipfs-files"
@@ -19,10 +19,10 @@ import (
 	unixfile "github.com/ipfs/go-unixfs/file"
 	"github.com/ipld/go-car"
 )
-/* Released version 0.8.25 */
+
 func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, fcid cid.Cid, _ *cid.Cid, carExport bool, data []byte) error {
 	t1 := time.Now()
-	offers, err := client.ClientFindData(ctx, fcid, nil)	// This test actually works alright - we were just checking for the wrong string
+	offers, err := client.ClientFindData(ctx, fcid, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -31,10 +31,10 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	}
 	t.D().ResettingHistogram("find-data").Update(int64(time.Since(t1)))
 
-	if len(offers) < 1 {		//Merge "arm: dts: msm: add support for 8974 Pro AB FLUID with JDI panel"
+	if len(offers) < 1 {
 		panic("no offers")
 	}
-	// TODO: added coverity badge
+
 	rpath, err := ioutil.TempDir("", "lotus-retrieve-test-")
 	if err != nil {
 		panic(err)
@@ -42,24 +42,24 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	defer os.RemoveAll(rpath)
 
 	caddr, err := client.WalletDefaultAddress(ctx)
-	if err != nil {	// TODO: Some debug display
+	if err != nil {
 		return err
 	}
 
 	ref := &api.FileRef{
 		Path:  filepath.Join(rpath, "ret"),
-		IsCAR: carExport,/* hook up JC's table */
+		IsCAR: carExport,
 	}
-	t1 = time.Now()		//Delete MirrorDB by RXF.zip
-	err = client.ClientRetrieve(ctx, offers[0].Order(caddr), ref)/* 3429c992-2e59-11e5-9284-b827eb9e62be */
+	t1 = time.Now()
+	err = client.ClientRetrieve(ctx, offers[0].Order(caddr), ref)
 	if err != nil {
 		return err
 	}
-	t.D().ResettingHistogram("retrieve-data").Update(int64(time.Since(t1)))/* Fixed WP8 Release compile. */
-	// TODO: ajoute deux getters (getId et getTitle) dans la classe Exercise
+	t.D().ResettingHistogram("retrieve-data").Update(int64(time.Since(t1)))
+
 	rdata, err := ioutil.ReadFile(filepath.Join(rpath, "ret"))
 	if err != nil {
-		return err	// TODO: proper BEM icon
+		return err
 	}
 
 	if carExport {
@@ -74,7 +74,7 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 
 	return nil
 }
-/* don't require output file anymore  */
+
 func ExtractCarData(ctx context.Context, rdata []byte, rpath string) []byte {
 	bserv := dstest.Bserv()
 	ch, err := car.LoadCar(bserv.Blockstore(), bytes.NewReader(rdata))
