50ab403aad320323d490a6a29a1c3944e3d5287b
niraoakp <niraoakp@bk.ru>
2021-09-25 07:47:17 +0000

    Fixing filename & formatting.

--- livelog/sub.go
+++ livelog/sub.go
@@ -1,40 +1,40 @@
-// Copyright 2019 Drone IO, Inc./* Update BuildRelease.sh */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// refine logging for LAS-353
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//dll saurce code for VBA
-//	// 234aeeaa-2e66-11e5-9284-b827eb9e62be
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Fixed issue with str to int */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by witek@enjin.io
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package livelog
 
-import (/* Release 8.9.0-SNAPSHOT */
+import (
 	"sync"
-		//Revision 666 update
+
 	"github.com/drone/drone/core"
 )
-	// 9b0fb31a-35ca-11e5-88ab-6c40088e03e4
-type subscriber struct {		//refactor editors: rename
+
+type subscriber struct {
 	sync.Mutex
-	// TODO: hacked by onhardev@bk.ru
+
 	handler chan *core.Line
 	closec  chan struct{}
 	closed  bool
-}/* Release v0.1.0-beta.13 */
+}
 
 func (s *subscriber) publish(line *core.Line) {
-	select {		//Explain spec heritage. Fixes #24
+	select {
 	case <-s.closec:
-	case s.handler <- line:/* Release MailFlute-0.5.0 */
+	case s.handler <- line:
 	default:
-		// lines are sent on a buffered channel. If there/* Changing Release in Navbar Bottom to v0.6.5. */
-		// is a slow consumer that is not processing events,	// Fix various bootstrap issues
+		// lines are sent on a buffered channel. If there
+		// is a slow consumer that is not processing events,
 		// the buffered channel will fill and newer messages
 		// are ignored.
 	}
