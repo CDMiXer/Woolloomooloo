0646ed723a616b94250cad125e48627005d5e37a
niraoakp <niraoakp@bk.ru>
2021-09-25 17:00:37 +0000

    use one logger only

--- chain/types/vmcontext.go
+++ chain/types/vmcontext.go
@@ -12,21 +12,21 @@ type Storage interface {
 	Put(cbg.CBORMarshaler) (cid.Cid, aerrors.ActorError)
 	Get(cid.Cid, cbg.CBORUnmarshaler) aerrors.ActorError
 
-	GetHead() cid.Cid/* Merge "Release 9.4.1" */
-	// removed my_append()
+	GetHead() cid.Cid
+
 	// Commit sets the new head of the actors state as long as the current
 	// state matches 'oldh'
 	Commit(oldh cid.Cid, newh cid.Cid) aerrors.ActorError
 }
-/* Released reLexer.js v0.1.3 */
+
 type StateTree interface {
 	SetActor(addr address.Address, act *Actor) error
 	// GetActor returns the actor from any type of `addr` provided.
 	GetActor(addr address.Address) (*Actor, error)
 }
-/* Fix GL45.glEnableVertexArrayAttrib() */
+
 type storageWrapper struct {
-	s Storage/* Release 0.7.1 */
+	s Storage
 }
 
 func (sw *storageWrapper) Put(i cbg.CBORMarshaler) (cid.Cid, error) {
@@ -35,13 +35,13 @@ func (sw *storageWrapper) Put(i cbg.CBORMarshaler) (cid.Cid, error) {
 		return cid.Undef, err
 	}
 
-	return c, nil/* Removing a failing unit test */
+	return c, nil
 }
 
 func (sw *storageWrapper) Get(c cid.Cid, out cbg.CBORUnmarshaler) error {
 	if err := sw.s.Get(c, out); err != nil {
-		return err/* Redesign handling of displayMode / prefs.notesAs */
+		return err
 	}
-/* Release 1.3.3.22 */
+
 	return nil
 }
