b0e3a8f777dec7bc4b8bc364fe607eba462152a6
niraoakp <niraoakp@bk.ru>
2021-09-25 22:07:58 +0000

    caching to merge

--- tests/integration/read/read_dbr/step1/index.ts
+++ tests/integration/read/read_dbr/step1/index.ts
@@ -5,7 +5,7 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Upreved for Release Candidate 2. */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,7 @@ import { Resource } from "./resource";
 
 // Setup: Resource A is external, Resource B is not but it depends on A. Resource C is external and
 // has an input dependent on B.
-const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
+const a = new Resource("a", { state: 42 }, { id: "existing-id"} );	// TODO: cleanStringArray function
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
 const c = new Resource("c", { state: b.state }, { id: "another-existing-id" })
 
