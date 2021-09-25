a4b61b285673e795438181f6f73501eb8f00a1d2
niraoakp <niraoakp@bk.ru>
2021-09-25 18:47:17 +0000

    Updated the button for 5.6

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -13,22 +13,22 @@ var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
 
 func Security(enabled, preferTLS bool) interface{} {
 	if !enabled {
-		return func() (opts Libp2pOpts) {		//Fill out documentation in Monadic
+		return func() (opts Libp2pOpts) {
 			// TODO: shouldn't this be Errorf to guarantee visibility?
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
 		You will not be able to connect to any nodes configured to use encrypted connections`)
 			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
 			return opts
 		}
-}	
+	}
 	return func() (opts Libp2pOpts) {
 		if preferTLS {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
 		} else {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
 		}
-		return opts	// TODO: hacked by cory@protocol.ai
-	}/* Update recruit page */
+		return opts
+	}
 }
 
 func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {
