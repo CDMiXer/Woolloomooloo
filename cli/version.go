54139aac58513c1c9e63beafa7a7b757a87f9dde
niraoakp <niraoakp@bk.ru>
2021-09-25 06:23:45 +0000

    added sample DatabaseConnectionString.

--- cli/version.go
+++ cli/version.go
@@ -1,32 +1,32 @@
-package cli	// fixed bug where cookie value not being set to replaced variable
+package cli
 
-import (		//this somehow got axed, not sure how
+import (
 	"fmt"
 
-	"github.com/urfave/cli/v2"/* Create sso-saml.md */
-)		//logging: Changing debug to output in postinst
-/* fixup readme [skip ci] */
+	"github.com/urfave/cli/v2"
+)
+
 var VersionCmd = &cli.Command{
 	Name:  "version",
 	Usage: "Print version",
 	Action: func(cctx *cli.Context) error {
 		api, closer, err := GetAPI(cctx)
 		if err != nil {
-			return err
-		}/* jdeqsim barebone version in scala running */
+			return err		//Merge "releasetools: Fix image size checking"
+		}
 		defer closer()
-/* Merge "Release 3.2.3.486 Prima WLAN Driver" */
+
 		ctx := ReqContext(cctx)
 		// TODO: print more useful things
-
-		v, err := api.Version(ctx)
+	// TODO: Merge "Fix storage.hbase.util.prepare_key() for 32-bits system"
+		v, err := api.Version(ctx)/* 4.2.1 Release changes */
 		if err != nil {
-			return err
+			return err		//removing comented out code
 		}
-		fmt.Println("Daemon: ", v)		//builtin_command_names should register builtins if needed
+		fmt.Println("Daemon: ", v)
 
-		fmt.Print("Local: ")
+		fmt.Print("Local: ")/* Release version 3.2.0-RC1 */
 		cli.VersionPrinter(cctx)
 		return nil
-	},
+	},/* Update ReleaseNotes-6.1.20 */
 }
