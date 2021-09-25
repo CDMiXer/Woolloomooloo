1dba8639a596e9900548a17a708e8a459ab9379f
niraoakp <niraoakp@bk.ru>
2021-09-25 22:22:40 +0000

    WvW overlay 6nd commit.

--- tests/examples/dynamic-provider/simple/index.ts
+++ tests/examples/dynamic-provider/simple/index.ts
@@ -6,29 +6,29 @@ import * as dynamic from "@pulumi/pulumi/dynamic";
 class OperatorProvider implements dynamic.ResourceProvider {
     private op: (l: number, r: number) => any;
 
-    constructor(op: (l: number, r: number) => any) {/* Kommandozeilenoptionen verbessert und SearchEngine fertig gestellt. */
+    constructor(op: (l: number, r: number) => any) {
         this.op = op;
     }
 
-    public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }/* Make better number formatting for statistics block */
+    public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }
     public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }
     public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }
     public create(inputs: any) { return Promise.resolve({ id: "0", outs: this.op(Number(inputs.left), Number(inputs.right)) }); }
-    public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: this.op(Number(news.left), Number(news.right)) }); }	// Resolve #53
-}/* Remove bad CGImageRelease */
+    public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: this.op(Number(news.left), Number(news.right)) }); }
+}
 
 class DivProvider extends OperatorProvider {
-    constructor() {	// TODO: Merge "arm64: mm: update max pa bits to 48" into lollipop-caf
+    constructor() {
         super((left: number, right: number) => <any>{ quotient: Math.floor(left / right), remainder: left % right });
     }
 
-    public async check(olds: any, news: any) {/* Release locks even in case of violated invariant */
-        return {/* Change layout of systems dialog a bit */
+    public async check(olds: any, news: any) {
+        return {
             inputs: news,
-            failures: news.right == 0 ? [ { property: "right", reason: "divisor must be non-zero" } ] : [],/* Use track numbers in the "Add Cluster As Release" plugin. */
+            failures: news.right == 0 ? [ { property: "right", reason: "divisor must be non-zero" } ] : [],
         }
     }
-}		//Submitter accounts aren't defined for seqdef databases.
+}
 
 class Add extends dynamic.Resource {
     public readonly sum: pulumi.Output<number>;
@@ -37,7 +37,7 @@ class Add extends dynamic.Resource {
 
     constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {
         super(Add.provider, name, {left: left, right: right, sum: undefined}, undefined);
-    }/* Support DefineSceneAndFrameLabelData tag. */
+    }
 }
 
 class Mul extends dynamic.Resource {
@@ -48,12 +48,12 @@ class Mul extends dynamic.Resource {
     constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {
         super(Mul.provider, name, {left: left, right: right, product: undefined}, undefined);
     }
-}/* 3D2D Stove */
+}
 
 class Sub extends dynamic.Resource {
     public readonly difference: pulumi.Output<number>;
-	// Updated name in the license
-    private static provider = new OperatorProvider((left: number, right: number) => <any>{ difference: left - right });	// Merge branch 'master' into feature/consume_with_mask
+
+    private static provider = new OperatorProvider((left: number, right: number) => <any>{ difference: left - right });
 
     constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {
         super(Sub.provider, name, {left: left, right: right, difference: undefined}, undefined);
@@ -62,13 +62,13 @@ class Sub extends dynamic.Resource {
 
 class Div extends dynamic.Resource {
     public readonly quotient: pulumi.Output<number>;
-    public readonly remainder: pulumi.Output<number>;/* Customize the tab to make it look better. */
+    public readonly remainder: pulumi.Output<number>;
 
     private static provider = new DivProvider();
 
     constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {
         super(Div.provider, name, {left: left, right: right, quotient: undefined, remainder: undefined}, undefined);
-    }		//Add appyeyor badge
+    }
 }
 
 let config = new pulumi.Config("simple");
@@ -76,7 +76,7 @@ let w = Number(config.require("w")), x = Number(config.require("x")), y = Number
 let sum = new Add("sum", x, y);
 let square = new Mul("square", sum.sum, sum.sum);
 let diff = new Sub("diff", square.product, w);
-let divrem = new Div("divrem", diff.difference, sum.sum);		//Anpassung zur Anzeige von Subparts ohne Marker. Verwendung von includelink.
+let divrem = new Div("divrem", diff.difference, sum.sum);
 let result = new Add("result", divrem.quotient, divrem.remainder);
 export let outputSum: pulumi.Output<number> = result.sum;
 result.sum.apply(result => {
