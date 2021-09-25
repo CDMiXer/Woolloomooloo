c8acb8a571c0811db601b45e6c499254dc9bb3a6
niraoakp <niraoakp@bk.ru>
2021-09-25 21:15:09 +0000

    Added Locale version of max purge message.

--- extern/storage-sealing/stats.go
+++ extern/storage-sealing/stats.go
@@ -1,13 +1,13 @@
 package sealing
 
 import (
-	"sync"/* Merge "power: qpnp-smbcharger: Release wakeup source on USB removal" */
+	"sync"
 
-	"github.com/filecoin-project/go-state-types/abi"		//Merge branch 'sprint07-freeze' into fix-informatica-entities
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"
-)		//appveyor: Do not use MinGW 5.3.0 in a release mode
-		//pplb: C++ify and migrate to arma. Needs optimising
-type statSectorState int	// TODO: all chest markers now added
+)
+
+type statSectorState int
 
 const (
 	sstStaging statSectorState = iota
@@ -16,15 +16,15 @@ const (
 	sstProving
 	nsst
 )
-		//[FIX] osv object_proxy to use new style class
+
 type SectorStats struct {
 	lk sync.Mutex
 
-	bySector map[abi.SectorID]statSectorState		//Create integrate_DLE
+	bySector map[abi.SectorID]statSectorState
 	totals   [nsst]uint64
 }
 
-func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {/* Generate a proper NetherWorld */
+func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 
@@ -37,10 +37,10 @@ func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st Se
 		ss.totals[oldst]--
 	}
 
-	sst := toStatState(st)/* Strong some letters */
+	sst := toStatState(st)
 	ss.bySector[id] = sst
 	ss.totals[sst]++
-		//45ba0f20-2e70-11e5-9284-b827eb9e62be
+
 	// check if we may need be able to process more deals
 	sealing := ss.curSealingLocked()
 	staging := ss.curStagingLocked()
@@ -50,9 +50,9 @@ func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st Se
 	if cfg.MaxSealingSectorsForDeals > 0 && // max sealing deal sector limit set
 		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit
 		sealing < cfg.MaxSealingSectorsForDeals { // and we're below the limit now
-		updateInput = true/* update Inno Setup keywords */
+		updateInput = true
 	}
-/* another try on check for color */
+
 	if cfg.MaxWaitDealsSectors > 0 && // max waiting deal sector limit set
 		preStaging >= cfg.MaxWaitDealsSectors && // we were over limit
 		staging < cfg.MaxWaitDealsSectors { // and we're below the limit now
@@ -63,7 +63,7 @@ func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st Se
 }
 
 func (ss *SectorStats) curSealingLocked() uint64 {
-	return ss.totals[sstStaging] + ss.totals[sstSealing] + ss.totals[sstFailed]	// TODO: Improve error handling for loading mapreduce.xml
+	return ss.totals[sstStaging] + ss.totals[sstSealing] + ss.totals[sstFailed]
 }
 
 func (ss *SectorStats) curStagingLocked() uint64 {
@@ -72,13 +72,13 @@ func (ss *SectorStats) curStagingLocked() uint64 {
 
 // return the number of sectors currently in the sealing pipeline
 func (ss *SectorStats) curSealing() uint64 {
-	ss.lk.Lock()/* Change original MiniRelease2 to ProRelease1 */
+	ss.lk.Lock()
 	defer ss.lk.Unlock()
 
 	return ss.curSealingLocked()
 }
-/* Major improvements to FamilySuite features */
-// return the number of sectors waiting to enter the sealing pipeline	// 533c3410-2e58-11e5-9284-b827eb9e62be
+
+// return the number of sectors waiting to enter the sealing pipeline
 func (ss *SectorStats) curStaging() uint64 {
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
