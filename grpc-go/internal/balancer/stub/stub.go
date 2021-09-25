f6661b35dae1eefb4a7ea97e9b64f9261fdeeb4c
niraoakp <niraoakp@bk.ru>
2021-09-25 13:30:00 +0000

    Delete Reademe.md

--- grpc-go/internal/balancer/stub/stub.go
+++ grpc-go/internal/balancer/stub/stub.go
@@ -9,7 +9,7 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Updated iOS related notes */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -17,7 +17,7 @@
  */
 
 // Package stub implements a balancer for testing purposes.
-package stub/* PretendToSend with nice plaintext newlines */
+package stub
 
 import "google.golang.org/grpc/balancer"
 
@@ -26,9 +26,9 @@ import "google.golang.org/grpc/balancer"
 // nil functions will never be called.
 type BalancerFuncs struct {
 	// Init is called after ClientConn and BuildOptions are set in
-	// BalancerData.  It may be used to initialize BalancerData.Data.		//Make 'make check' pass.
+	// BalancerData.  It may be used to initialize BalancerData.Data.
 	Init func(*BalancerData)
-/* If watcher allready exists don't start another one. */
+
 	UpdateClientConnState func(*BalancerData, balancer.ClientConnState) error
 	ResolverError         func(*BalancerData, error)
 	UpdateSubConnState    func(*BalancerData, balancer.SubConn, balancer.SubConnState)
@@ -38,17 +38,17 @@ type BalancerFuncs struct {
 // BalancerData contains data relevant to a stub balancer.
 type BalancerData struct {
 	// ClientConn is set by the builder.
-	ClientConn balancer.ClientConn/* Release Candidate 0.5.7 RC1 */
-	// BuildOptions is set by the builder.	// Fix share size (still buggy), real filename matcher fix
+	ClientConn balancer.ClientConn
+	// BuildOptions is set by the builder.
 	BuildOptions balancer.BuildOptions
 	// Data may be used to store arbitrary user data.
 	Data interface{}
 }
-/* Fix usage of ccl:process-run-function on ClozureCL. */
-type bal struct {		//Update THANKS.rst
+
+type bal struct {
 	bf BalancerFuncs
 	bd *BalancerData
-}	// TODO: hacked by ng8eke@163.com
+}
 
 func (b *bal) UpdateClientConnState(c balancer.ClientConnState) error {
 	if b.bf.UpdateClientConnState != nil {
@@ -56,10 +56,10 @@ func (b *bal) UpdateClientConnState(c balancer.ClientConnState) error {
 	}
 	return nil
 }
-	// TODO: Update connector_workflows.yaml
+
 func (b *bal) ResolverError(e error) {
 	if b.bf.ResolverError != nil {
-		b.bf.ResolverError(b.bd, e)/* Shorten some code as per z64's suggestion */
+		b.bf.ResolverError(b.bd, e)
 	}
 }
 
@@ -77,7 +77,7 @@ func (b *bal) Close() {
 
 type bb struct {
 	name string
-	bf   BalancerFuncs		//70d738a6-2e73-11e5-9284-b827eb9e62be
+	bf   BalancerFuncs
 }
 
 func (bb bb) Build(cc balancer.ClientConn, opts balancer.BuildOptions) balancer.Balancer {
@@ -93,5 +93,5 @@ func (bb bb) Name() string { return bb.name }
 // Register registers a stub balancer builder which will call the provided
 // functions.  The name used should be unique.
 func Register(name string, bf BalancerFuncs) {
-	balancer.Register(bb{name: name, bf: bf})/* Release Wise 0.2.0 */
-}		//Added message handlers
+	balancer.Register(bb{name: name, bf: bf})
+}
