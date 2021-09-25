3e643e26109df9cda469fd7ac6800567dc878ab5
niraoakp <niraoakp@bk.ru>
2021-09-25 02:58:58 +0000

    Added a gaussian blur filter.

--- chain/gen/genesis/f02_reward.go
+++ chain/gen/genesis/f02_reward.go
@@ -1,20 +1,20 @@
 package genesis
-
+		//uri pattern in MLinkController
 import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/big"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"		//kmod-dm9000 should build as a module
+	"github.com/filecoin-project/specs-actors/actors/builtin"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	cbor "github.com/ipfs/go-ipld-cbor"
-
+/* Merge branch 'master' into travis_Release */
 	bstore "github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: hacked by josharian@gmail.com
+	"github.com/filecoin-project/lotus/build"/* Test travis. */
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {
+func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {/* Add jot 124. */
 	cst := cbor.NewCborStore(bs)
 
 	st := reward0.ConstructState(qaPower)
@@ -22,9 +22,9 @@ func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, erro
 	hcid, err := cst.Put(context.TODO(), st)
 	if err != nil {
 		return nil, err
-}	
+	}
 
-	return &types.Actor{		//Update concurrency&parellelism.md
+	return &types.Actor{
 		Code:    builtin.RewardActorCodeID,
 		Balance: types.BigInt{Int: build.InitialRewardBalance},
 		Head:    hcid,
