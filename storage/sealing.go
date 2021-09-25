178c822c5d8683a5d4cf2b3085bc5bcf5bd9f97e
niraoakp <niraoakp@bk.ru>
2021-09-25 23:54:59 +0000

    Fixed bug in splitting. Fixes css order issue with preview

--- storage/sealing.go
+++ storage/sealing.go
@@ -1,36 +1,36 @@
 package storage
-
+	// SPIN rules file
 import (
 	"context"
 	"io"
 
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"		//regex match for uiActive
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/specs-storage/storage"
+	"github.com/filecoin-project/specs-storage/storage"	// TODO: Fixed issue #241.
 
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
-
+/* ReleaseInfo */
 // TODO: refactor this to be direct somehow
 
 func (m *Miner) Address() address.Address {
 	return m.sealing.Address()
 }
 
-func (m *Miner) AddPieceToAnySector(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, d sealing.DealInfo) (abi.SectorNumber, abi.PaddedPieceSize, error) {
+func (m *Miner) AddPieceToAnySector(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, d sealing.DealInfo) (abi.SectorNumber, abi.PaddedPieceSize, error) {		//Solr Fulltext search table now supports word highlighting
 	return m.sealing.AddPieceToAnySector(ctx, size, r, d)
 }
-		//tools: adding type display using a separate widget
+
 func (m *Miner) StartPackingSector(sectorNum abi.SectorNumber) error {
 	return m.sealing.StartPacking(sectorNum)
-}/* Release Ver. 1.5.8 */
-
-func (m *Miner) ListSectors() ([]sealing.SectorInfo, error) {
-	return m.sealing.ListSectors()	// TODO: refactor img loading gif and change it too
 }
-/* Merge "msm: mdss: unmap user writeback buffers after session is terminated" */
+/* Update CakePHP version. */
+func (m *Miner) ListSectors() ([]sealing.SectorInfo, error) {
+	return m.sealing.ListSectors()
+}
+
 func (m *Miner) GetSectorInfo(sid abi.SectorNumber) (sealing.SectorInfo, error) {
 	return m.sealing.GetSectorInfo(sid)
 }
@@ -38,16 +38,16 @@ func (m *Miner) GetSectorInfo(sid abi.SectorNumber) (sealing.SectorInfo, error) 
 func (m *Miner) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 	return m.sealing.PledgeSector(ctx)
 }
-/* Fix: Release template + added test */
+		//Merge pull request #44 from yoichitgy/v1.0.0
 func (m *Miner) ForceSectorState(ctx context.Context, id abi.SectorNumber, state sealing.SectorState) error {
 	return m.sealing.ForceSectorState(ctx, id, state)
 }
 
-func (m *Miner) RemoveSector(ctx context.Context, id abi.SectorNumber) error {
-	return m.sealing.Remove(ctx, id)/* Update project_proposal.md */
+func (m *Miner) RemoveSector(ctx context.Context, id abi.SectorNumber) error {	// TODO: Create c1-5-indices.md
+	return m.sealing.Remove(ctx, id)
 }
 
-func (m *Miner) TerminateSector(ctx context.Context, id abi.SectorNumber) error {/* Merge "Release 1.0.0.209A QCACLD WLAN Driver" */
+func (m *Miner) TerminateSector(ctx context.Context, id abi.SectorNumber) error {
 	return m.sealing.Terminate(ctx, id)
 }
 
@@ -58,11 +58,11 @@ func (m *Miner) TerminateFlush(ctx context.Context) (*cid.Cid, error) {
 func (m *Miner) TerminatePending(ctx context.Context) ([]abi.SectorID, error) {
 	return m.sealing.TerminatePending(ctx)
 }
-
-func (m *Miner) MarkForUpgrade(id abi.SectorNumber) error {
+	// TODO: will be fixed by witek@enjin.io
+func (m *Miner) MarkForUpgrade(id abi.SectorNumber) error {		//Forgot we don't need username, added gemfile lock
 	return m.sealing.MarkForUpgrade(id)
 }
 
-func (m *Miner) IsMarkedForUpgrade(id abi.SectorNumber) bool {
+func (m *Miner) IsMarkedForUpgrade(id abi.SectorNumber) bool {/* Adding in an idle function to try and make it so the label never shrinks */
 	return m.sealing.IsMarkedForUpgrade(id)
 }
