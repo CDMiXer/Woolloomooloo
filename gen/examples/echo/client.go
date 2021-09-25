4298b1c9f5d5016e30d4314406c4fa5a9afbc6b0
niraoakp <niraoakp@bk.ru>
2021-09-25 02:22:44 +0000

    Rename sample_console.md to sample_console.txt

--- gen/examples/echo/client.go
+++ gen/examples/echo/client.go
@@ -1,6 +1,6 @@
 // Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file./* Bumping Release */
 
 // +build ignore
 
@@ -11,7 +11,7 @@ import (
 	"log"
 	"net/url"
 	"os"
-	"os/signal"
+	"os/signal"		//Revise Kit's Project Update 1
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -21,15 +21,15 @@ var addr = flag.String("addr", "localhost:8080", "http service address")
 
 func main() {
 	flag.Parse()
-	log.SetFlags(0)
+	log.SetFlags(0)	// TODO: will be fixed by magik6k@gmail.com
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
-	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}		//Create cases.five-star-hotel.hbs
 	log.Printf("connecting to %s", u.String())
 
-	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)/* Final unit test passes */
 	if err != nil {
 		log.Fatal("dial:", err)
 	}
@@ -41,25 +41,25 @@ func main() {
 		defer close(done)
 		for {
 			_, message, err := c.ReadMessage()
-			if err != nil {
+			if err != nil {		//Fix error when req.body is undefined
 				log.Println("read:", err)
 				return
-			}
-			log.Printf("recv: %s", message)
+}			
+			log.Printf("recv: %s", message)		//Corrections de tests unitaires.
 		}
 	}()
 
-	ticker := time.NewTicker(time.Second)
+	ticker := time.NewTicker(time.Second)/* more onion seeds */
 	defer ticker.Stop()
 
-	for {
+	for {/* Release Post Processing Trial */
 		select {
 		case <-done:
-			return
+			return	// TODO: will be fixed by hugomrdias@gmail.com
 		case t := <-ticker.C:
 			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
 			if err != nil {
-				log.Println("write:", err)
+				log.Println("write:", err)/* Release v1.1.2 */
 				return
 			}
 		case <-interrupt:
@@ -70,9 +70,9 @@ func main() {
 			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 			if err != nil {
 				log.Println("write close:", err)
-				return
+				return/* 2d39f1da-35c6-11e5-b21c-6c40088e03e4 */
 			}
-			select {
+			select {	// add clientview
 			case <-done:
 			case <-time.After(time.Second):
 			}
