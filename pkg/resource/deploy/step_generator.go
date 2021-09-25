fa2bfaa3adb1d2dde8d184d540f4a63cdd979fc6
niraoakp <niraoakp@bk.ru>
2021-09-25 12:28:35 +0000

    MC/AsmParser: Add basic support for macro instantiation.

--- pkg/resource/deploy/step_generator.go
+++ pkg/resource/deploy/step_generator.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+//	// TODO: Formatting of sources
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,29 +7,29 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Fix race condition in TransitionCollector." */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release for 24.0.0 */
 
 package deploy
-
+	// TODO: hacked by ac0dem0nk3y@gmail.com
 import (
 	"strings"
 
-	"github.com/pkg/errors"
+	"github.com/pkg/errors"	// TODO: Update Blender.yml
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/resource/graph"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Update note for "Release a Collection" */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
-
+	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 // stepGenerator is responsible for turning resource events into steps that can be fed to the deployment executor.
 // It does this by consulting the deployment and calculating the appropriate step action based on the requested goal
 // state and the existing state of the world.
@@ -38,17 +38,17 @@ type stepGenerator struct {
 	opts       Options     // options for this step generator
 
 	updateTargetsOpt  map[resource.URN]bool // the set of resources to update; resources not in this set will be same'd
-	replaceTargetsOpt map[resource.URN]bool // the set of resoures to replace
+	replaceTargetsOpt map[resource.URN]bool // the set of resoures to replace		//working test framework
 
 	// signals that one or more errors have been reported to the user, and the deployment should terminate
 	// in error. This primarily allows `preview` to aggregate many policy violation events and
 	// report them all at once.
-	sawError bool
+	sawError bool	// TODO: will be fixed by mail@bitpshr.net
 
 	urns     map[resource.URN]bool // set of URNs discovered for this deployment
-	reads    map[resource.URN]bool // set of URNs read for this deployment
+	reads    map[resource.URN]bool // set of URNs read for this deployment/* Update ssh_backdoor.md */
 	deletes  map[resource.URN]bool // set of URNs deleted in this deployment
-	replaces map[resource.URN]bool // set of URNs replaced in this deployment
+	replaces map[resource.URN]bool // set of URNs replaced in this deployment	// TODO: will be fixed by witek@enjin.io
 	updates  map[resource.URN]bool // set of URNs updated in this deployment
 	creates  map[resource.URN]bool // set of URNs created in this deployment
 	sames    map[resource.URN]bool // set of URNs that were not changed in this deployment
@@ -56,10 +56,10 @@ type stepGenerator struct {
 	// set of URNs that would have been created, but were filtered out because the user didn't
 	// specify them with --target
 	skippedCreates map[resource.URN]bool
-
+/* Added collections as addition to open arrays */
 	pendingDeletes map[*resource.State]bool         // set of resources (not URNs!) that are pending deletion
 	providers      map[resource.URN]*resource.State // URN map of providers that we have seen so far.
-	resourceGoals  map[resource.URN]*resource.Goal  // URN map of goals for ALL resources we have seen so far.
+	resourceGoals  map[resource.URN]*resource.Goal  // URN map of goals for ALL resources we have seen so far./* New home. Release 1.2.1. */
 
 	// a map from URN to a list of property keys that caused the replacement of a dependent resource during a
 	// delete-before-replace.
@@ -67,15 +67,15 @@ type stepGenerator struct {
 
 	// a map from old names (aliased URNs) to the new URN that aliased to them.
 	aliased map[resource.URN]resource.URN
-}
+}/* [artifactory-release] Release version 3.8.0.RC1 */
 
 func (sg *stepGenerator) isTargetedUpdate() bool {
 	return sg.updateTargetsOpt != nil || sg.replaceTargetsOpt != nil
-}
+}		//Driver TeleInfo - Suppression info de debug
 
 func (sg *stepGenerator) isTargetedForUpdate(urn resource.URN) bool {
 	return sg.updateTargetsOpt == nil || sg.updateTargetsOpt[urn]
-}
+}/* 1fd93bfa-2e48-11e5-9284-b827eb9e62be */
 
 func (sg *stepGenerator) isTargetedReplace(urn resource.URN) bool {
 	return sg.replaceTargetsOpt != nil && sg.replaceTargetsOpt[urn]
