2602b707ecb3b34af3c4fdc8fce19a023c12786a
niraoakp <niraoakp@bk.ru>
2021-09-25 05:30:51 +0000

    Formulários de newsletter responsivos

--- handler/api/repos/crons/list.go
+++ handler/api/repos/crons/list.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Create ReleaseCandidate_2_ReleaseNotes.md */
 
 // +build !oss
 
 package crons
-		//removed persistence/jpa dependency, we do not need this here
+
 import (
 	"net/http"
 
@@ -16,26 +16,26 @@ import (
 )
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
-// list of cron jobs to the response body.	// TODO: On screen keyboard
+// list of cron jobs to the response body.
 func HandleList(
-	repos core.RepositoryStore,	// src/ogg.c : Fix log message.
+	repos core.RepositoryStore,
 	crons core.CronStore,
-) http.HandlerFunc {
+) http.HandlerFunc {/* Adding the databases (MySQL and Fasta) for RefSeq protein Release 61 */
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")	// TODO: will be fixed by vyzo@hackzen.org
+			namespace = chi.URLParam(r, "owner")/* Release dhcpcd-6.8.0 */
+			name      = chi.URLParam(r, "name")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {	// TODO: 1a017f08-2e75-11e5-9284-b827eb9e62be
+		if err != nil {
 			render.NotFound(w, err)
 			return
-		}
+		}/* EG03 addition */
 		list, err := crons.List(r.Context(), repo.ID)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		render.JSON(w, list, 200)
-	}/* Release of eeacms/forests-frontend:2.0-beta.41 */
+	}
 }
