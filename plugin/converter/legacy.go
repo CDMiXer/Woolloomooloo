d9a5a1e88733978cd0194346ccb71f3e9e00808a
niraoakp <niraoakp@bk.ru>
2021-09-25 10:58:25 +0000

    Use Forge update checker

--- plugin/converter/legacy.go
+++ plugin/converter/legacy.go
@@ -1,34 +1,34 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Add extra sanity check
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss	// Update calendar settings
 
 package converter
 
 import (
-	"context"	// 5e045696-2e5a-11e5-9284-b827eb9e62be
+	"context"
 
 	"github.com/drone/drone/core"
-)
+)/* Improving the daemon tests and fixtures. */
 
 // Legacy returns a conversion service that converts a
-// legacy 0.8 yaml file to a yaml file./* Release v12.37 */
-func Legacy(enabled bool) core.ConvertService {
-	return &legacyPlugin{	// Update Russian.ts
+// legacy 0.8 yaml file to a yaml file.
+func Legacy(enabled bool) core.ConvertService {	// TODO: Escape " char from JSON description for now
+	return &legacyPlugin{
 		enabled: enabled,
-	}		//Create Day_ch
-}/* Eclipse generated .gitignore files */
+	}
+}
 
-type legacyPlugin struct {/* Fix Release History spacing */
+type legacyPlugin struct {
 	enabled bool
 }
 
 func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
-	if p.enabled == false {/* Merge "Wlan: Release 3.8.20.3" */
+	if p.enabled == false {/* Fix the Release manifest stuff to actually work correctly. */
 		return nil, nil
 	}
-	return &core.Config{
+	return &core.Config{		//Increase registerUIAScript timeout to 30 secs for slow VMs.
 		Data: req.Config.Data,
 	}, nil
-}
+}/* New Release. */
