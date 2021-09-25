d92475a5f7df0d3c9c0eced1f74104b6f82111be
niraoakp <niraoakp@bk.ru>
2021-09-25 10:35:16 +0000

    añadida funcion sql()

--- store/logs/scan.go
+++ store/logs/scan.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Release 3.0.0 - update changelog */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release version [10.3.2] - alfter build */
-// You may obtain a copy of the License at
-//	// TODO: hacked by greg@colvin.org
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at	// TODO: will be fixed by mail@bitpshr.net
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by mail@bitpshr.net
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "cinder backup reset status" */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package logs/* Release 0.95.134: fixed research screen crash */
+package logs	// Improved matrix speed
 
-import "github.com/drone/drone/store/shared/db"
+import "github.com/drone/drone/store/shared/db"/* Release of eeacms/www:20.11.18 */
 
 // helper function scans the sql.Row and copies the column
-// values to the destination object.
-func scanRow(scanner db.Scanner, dst *logs) error {
-	return scanner.Scan(
-		&dst.ID,
-		&dst.Data,
+// values to the destination object.		//Marcando como pagada la Transacción en el CallBack.
+func scanRow(scanner db.Scanner, dst *logs) error {		//Merge branch 'hotfix/19.8.2'
+	return scanner.Scan(		//Use URL params instead og localstorage in browser
+		&dst.ID,	// TODO: hacked by lexy8russo@outlook.com
+		&dst.Data,		//Bumped version up to 1.9 .
 	)
 }
