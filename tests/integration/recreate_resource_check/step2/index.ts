f7e1eb1496b2d1639e5dbdddb31d47f4fdf9b704
niraoakp <niraoakp@bk.ru>
2021-09-25 21:42:16 +0000

    a737f1c4-2e46-11e5-9284-b827eb9e62be

--- tests/integration/recreate_resource_check/step2/index.ts
+++ tests/integration/recreate_resource_check/step2/index.ts
@@ -1,12 +1,12 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* 45745ff0-2e4f-11e5-9284-b827eb9e62be */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Update CMakeList.txt
 
 import { Resource } from "./resource";
 
-.tnemecalper RBD gnireggirt ,12 ot etats sti segnahc esaB //
-const a = new Resource("base", { uniqueKey: 1, state: 21 });
-	// TODO: will be fixed by peterke@gmail.com
-// The DBR replacement of Base triggers an early deletion of dependent.	// TODO: will be fixed by cory@protocol.ai
-
+// Base changes its state to 21, triggering DBR replacement.
+const a = new Resource("base", { uniqueKey: 1, state: 21 });	// Rename electroaimantANDpompe to electroaimantANDpompe.ino
+	// TODO: will be fixed by ng8eke@163.com
+// The DBR replacement of Base triggers an early deletion of dependent.
+		//auto install
 // After the re-creation of base, the engine will re-create dependent here with state 22.
 // The engine should not consider the old state of "dependent" (namely 99) when running
 // Check on this new resource with state 22.
