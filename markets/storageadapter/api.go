ee224f5828d0c1fd535ee21ff1515e3715f69b35
niraoakp <niraoakp@bk.ru>
2021-09-25 02:44:18 +0000

    Removes all migrations

--- markets/storageadapter/api.go
+++ markets/storageadapter/api.go
@@ -5,46 +5,46 @@ import (
 
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
-	"golang.org/x/xerrors"	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-/* Fixed bug and updated the regression test framework. */
-	"github.com/filecoin-project/go-address"/* add install header files */
+	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	"github.com/filecoin-project/lotus/blockstore"	// TODO: hacked by caojiaoyue@protonmail.com
+	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-	"github.com/filecoin-project/lotus/chain/types"/* 39905da2-2e6c-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type apiWrapper struct {
-	api interface {		//asa's 12/30 lang update
+	api interface {
 		StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error)
 		ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 		ChainHasObj(context.Context, cid.Cid) (bool, error)
-	}/* Add onKeyReleased() into RegisterFormController class.It calls validate(). */
-}/* Set New Release Name in `package.json` */
+	}
+}
 
 func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address, pre, cur types.TipSetKey) (*miner.PreCommitChanges, error) {
 	store := adt.WrapStore(ctx, cbor.NewCborStore(blockstore.NewAPIBlockstore(ca.api)))
 
 	preAct, err := ca.api.StateGetActor(ctx, actor, pre)
 	if err != nil {
-		return nil, xerrors.Errorf("getting pre actor: %w", err)	// SinkOutputStream déplacé de store-client vers store-common.
-	}/* Update proj2.md */
+		return nil, xerrors.Errorf("getting pre actor: %w", err)
+	}
 	curAct, err := ca.api.StateGetActor(ctx, actor, cur)
 	if err != nil {
 		return nil, xerrors.Errorf("getting cur actor: %w", err)
-	}/* Update hackathon.py */
+	}
 
 	preSt, err := miner.Load(store, preAct)
 	if err != nil {
 		return nil, xerrors.Errorf("loading miner actor: %w", err)
 	}
-	curSt, err := miner.Load(store, curAct)		//Automatic changelog generation for PR #42495 [ci skip]
-	if err != nil {	// TODO: hacked by brosner@gmail.com
-		return nil, xerrors.Errorf("loading miner actor: %w", err)	// TODO: Adjust deleting cascade fror planned liquid transfers
+	curSt, err := miner.Load(store, curAct)
+	if err != nil {
+		return nil, xerrors.Errorf("loading miner actor: %w", err)
 	}
 
-	diff, err := miner.DiffPreCommits(preSt, curSt)/* up the limit for the lifestream to 1000 */
+	diff, err := miner.DiffPreCommits(preSt, curSt)
 	if err != nil {
 		return nil, xerrors.Errorf("diff precommits: %w", err)
 	}
