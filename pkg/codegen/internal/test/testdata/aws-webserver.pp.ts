a399fd0fab7f1b2924fc3640444397757a919c32
niraoakp <niraoakp@bk.ru>
2021-09-25 14:13:22 +0000

    Removed jump discovery message

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -1,16 +1,16 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-// Create a new security group for port 80.
+// Create a new security group for port 80./* e778524e-2e49-11e5-9284-b827eb9e62be */
 const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
-    protocol: "tcp",
+    protocol: "tcp",	// Update player_list.lua
     fromPort: 0,
-    toPort: 0,
+    toPort: 0,		//add ignore delete promo
     cidrBlocks: ["0.0.0.0/0"],
 }]});
 const ami = aws.getAmi({
     filters: [{
-        name: "name",
+        name: "name",		//Compute delay from request issue, not response return.  Fixes #721
         values: ["amzn-ami-hvm-*-x86_64-ebs"],
     }],
     owners: ["137112412989"],
@@ -19,15 +19,15 @@ const ami = aws.getAmi({
 // Create a simple web server using the startup script for the instance.
 const server = new aws.ec2.Instance("server", {
     tags: {
-        Name: "web-server-www",
+        Name: "web-server-www",	// TODO: hacked by lexy8russo@outlook.com
     },
     instanceType: "t2.micro",
     securityGroups: [securityGroup.name],
     ami: ami.then(ami => ami.id),
     userData: `#!/bin/bash
 echo "Hello, World!" > index.html
-nohup python -m SimpleHTTPServer 80 &
+nohup python -m SimpleHTTPServer 80 &	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 `,
-});
+});	// TODO: will be fixed by magik6k@gmail.com
 export const publicIp = server.publicIp;
 export const publicHostName = server.publicDns;
