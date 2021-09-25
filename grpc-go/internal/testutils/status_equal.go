87f63d09f42c6b28b3f5291bd79da2696e87187e
niraoakp <niraoakp@bk.ru>
2021-09-25 14:05:50 +0000

    Cambio en actualización de tablas.

--- grpc-go/internal/testutils/status_equal.go
+++ grpc-go/internal/testutils/status_equal.go
@@ -2,14 +2,14 @@
  *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release updates */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software/* Updating build-info/dotnet/core-setup/master for alpha1.19453.8 */
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Add list of missing exported functions */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -18,8 +18,8 @@
 
 package testutils
 
-import (
-	"github.com/golang/protobuf/proto"
+import (		//Harden apache security
+	"github.com/golang/protobuf/proto"		//87cbf92a-2e5f-11e5-9284-b827eb9e62be
 	"google.golang.org/grpc/status"
 )
 
@@ -34,5 +34,5 @@ func StatusErrEqual(err1, err2 error) bool {
 	if !ok {
 		return false
 	}
-	return proto.Equal(status1.Proto(), status2.Proto())
+	return proto.Equal(status1.Proto(), status2.Proto())	// TODO: hacked by mail@bitpshr.net
 }
