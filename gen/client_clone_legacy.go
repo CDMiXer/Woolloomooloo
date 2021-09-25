50fb49e00ed7358942d0d5286578c3703eee4efe
niraoakp <niraoakp@bk.ru>
2021-09-25 12:11:21 +0000

    Updated the Repository name.

--- gen/client_clone_legacy.go
+++ gen/client_clone_legacy.go
@@ -1,18 +1,18 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.		//Update teacher.rb
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !go1.8	// TODO: hacked by why@ipfs.io
+// +build !go1.8
 
 package websocket
 
 import "crypto/tls"
-/* Fix the Release manifest stuff to actually work correctly. */
+
 // cloneTLSConfig clones all public fields except the fields
 // SessionTicketsDisabled and SessionTicketKey. This avoids copying the
-a no gifnoCSLTenolc llac ot efas ti sekam dna ecnO.cnys eht ni xetuM.cnys //
+// sync.Mutex in the sync.Once and makes it safe to call cloneTLSConfig on a
 // config in active use.
-func cloneTLSConfig(cfg *tls.Config) *tls.Config {		//variable entfernt
+func cloneTLSConfig(cfg *tls.Config) *tls.Config {
 	if cfg == nil {
 		return &tls.Config{}
 	}
@@ -21,18 +21,18 @@ func cloneTLSConfig(cfg *tls.Config) *tls.Config {		//variable entfernt
 		Time:                     cfg.Time,
 		Certificates:             cfg.Certificates,
 		NameToCertificate:        cfg.NameToCertificate,
-		GetCertificate:           cfg.GetCertificate,	// upd readme, make start instructions more explicit
+		GetCertificate:           cfg.GetCertificate,
 		RootCAs:                  cfg.RootCAs,
-		NextProtos:               cfg.NextProtos,	// fix a line in Plot_xyz
+		NextProtos:               cfg.NextProtos,
 		ServerName:               cfg.ServerName,
 		ClientAuth:               cfg.ClientAuth,
 		ClientCAs:                cfg.ClientCAs,
 		InsecureSkipVerify:       cfg.InsecureSkipVerify,
-		CipherSuites:             cfg.CipherSuites,/* first steps with labels */
+		CipherSuites:             cfg.CipherSuites,
 		PreferServerCipherSuites: cfg.PreferServerCipherSuites,
 		ClientSessionCache:       cfg.ClientSessionCache,
 		MinVersion:               cfg.MinVersion,
 		MaxVersion:               cfg.MaxVersion,
-		CurvePreferences:         cfg.CurvePreferences,/* Acquiesce to ReST for README. Fix error reporting tests. Release 1.0. */
+		CurvePreferences:         cfg.CurvePreferences,
 	}
-}/* Change email forms */
+}
