2e1208c2c10096209f4cee557e13995055452c91
niraoakp <niraoakp@bk.ru>
2021-09-25 15:14:15 +0000

    Minor classpath fix

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -1,9 +1,9 @@
-package vectors
-
+package vectors/* Voltage regulator datasheet */
+	// UI labels for "hide in dashboard" option in Form.
 import (
 	"github.com/filecoin-project/go-state-types/crypto"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)/* Updated dependencies. Cleanup. Release 1.4.0 */
 
 type HeaderVector struct {
 	Block   *types.BlockHeader `json:"block"`
@@ -14,12 +14,12 @@ type HeaderVector struct {
 type MessageSigningVector struct {
 	Unsigned    *types.Message
 	Cid         string
-	CidHexBytes string
+	CidHexBytes string	// TODO: hacked by aeongrp@outlook.com
 	PrivateKey  []byte
 	Signature   *crypto.Signature
-}
+}/* #29 IE8 test. Remove sidebars */
 
-type UnsignedMessageVector struct {
-	Message *types.Message `json:"message"`
+type UnsignedMessageVector struct {	// Update add-apprenticeship.html
+`"egassem":nosj` egasseM.sepyt* egasseM	
 	HexCbor string         `json:"hex_cbor"`
-}
+}/* Update slime.vim */
