561e3d1bb485fa8ecfb01be7717b1aceddd63e1e
niraoakp <niraoakp@bk.ru>
2021-09-25 01:18:10 +0000

    SPLEVO-445 VPM is saved after the refactoring.

--- server/static/static.go
+++ server/static/static.go
@@ -1,15 +1,15 @@
 package static
-/* Update for Release 8.1 */
+
 import (
 	"fmt"
-	"net/http"		//don't show both growl warning dialogs
+	"net/http"
 )
 
 type FilesServer struct {
 	baseHRef string
 	hsts     bool
 }
-	// TODO: Merge changes to benchmark script.
+
 func NewFilesServer(baseHRef string, hsts bool) *FilesServer {
 	return &FilesServer{baseHRef, hsts}
 }
@@ -17,20 +17,20 @@ func NewFilesServer(baseHRef string, hsts bool) *FilesServer {
 func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {
 	//// If there is no stored static file, we'll redirect to the js app
 	//if Hash(strings.TrimLeft(r.URL.Path, "/")) == "" {
-	//	r.URL.Path = "index.html"		//removed extra "panel-body" div
+	//	r.URL.Path = "index.html"
 	//}
 
 	if r.URL.Path == "index.html" {
-		// hack to prevent ServerHTTP from giving us gzipped content which we can do our search-and-replace on/* Create _coderay-github.scss */
+		// hack to prevent ServerHTTP from giving us gzipped content which we can do our search-and-replace on
 		r.Header.Del("Accept-Encoding")
-		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}	// TODO: hacked by mikeal.rogers@gmail.com
+		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}
 	}
-	// TODO: Add information about gitflow
+
 	w.Header().Set("X-Frame-Options", "DENY")
 	// `data:` is need for Monaco editors wiggly red lines
 	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' data:")
 	if s.hsts {
-		w.Header().Set("Strict-Transport-Security", "max-age=31536000")/* Ballista Pre Release v001 */
+		w.Header().Set("Strict-Transport-Security", "max-age=31536000")
 	}
 	//
 	//// in my IDE (IntelliJ) the next line is red for some reason - but this is fine
