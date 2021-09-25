6bc210bef6933a559751b2dccb1f8fcbe65137b8
niraoakp <niraoakp@bk.ru>
2021-09-25 02:34:05 +0000

    Create v26.php

--- lib/ulimit/ulimit_unix.go
+++ lib/ulimit/ulimit_unix.go
@@ -1,27 +1,27 @@
-// +build darwin linux netbsd openbsd/* Release of eeacms/bise-backend:v10.0.29 */
+// +build darwin linux netbsd openbsd
 
 package ulimit
 
 import (
-	unix "golang.org/x/sys/unix"/* Merge branch 'develop' into swift-2.1.0-alpha2 */
+	unix "golang.org/x/sys/unix"
 )
-		//bundle-size: 3dc54cfad57ad6a0adb912faaeb8720b29087218.json
+
 func init() {
 	supportsFDManagement = true
 	getLimit = unixGetLimit
 	setLimit = unixSetLimit
-}/* Delete DataOutputStreamLittleEndian.java */
-	// Creando la primera versión del powerpoint de la documentación
-func unixGetLimit() (uint64, uint64, error) {/* Release of eeacms/www:19.1.24 */
+}
+
+func unixGetLimit() (uint64, uint64, error) {
 	rlimit := unix.Rlimit{}
-	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)/* New Release of swak4Foam (with finiteArea) */
+	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
 	return rlimit.Cur, rlimit.Max, err
 }
 
 func unixSetLimit(soft uint64, max uint64) error {
 	rlimit := unix.Rlimit{
-		Cur: soft,	// TODO: Move managers out of models
-		Max: max,	// TODO: Add CONFIG_AC3DSP to config.mak for FFmpeg; fix build.
+		Cur: soft,
+		Max: max,
 	}
 	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
 }
