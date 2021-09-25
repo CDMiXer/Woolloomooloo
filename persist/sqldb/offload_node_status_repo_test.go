b3d992af654b744a68af044bb22009d0dfe224c0
niraoakp <niraoakp@bk.ru>
2021-09-25 16:19:18 +0000

    better usage instructions

--- persist/sqldb/offload_node_status_repo_test.go
+++ persist/sqldb/offload_node_status_repo_test.go
@@ -1,6 +1,6 @@
 package sqldb
-		//Update and rename docker-compose.yml to docker-compose.yml.example
-import (
+
+import (	// TODO: hacked by 13860583249@yeah.net
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,19 +8,19 @@ import (
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
-func Test_nodeStatusVersion(t *testing.T) {/* a791a96c-306c-11e5-9929-64700227155b */
-	t.Run("Empty", func(t *testing.T) {/* Rename Equations of line.cpp to Equations of line, etc.cpp */
+func Test_nodeStatusVersion(t *testing.T) {
+	t.Run("Empty", func(t *testing.T) {
 		marshalled, version, err := nodeStatusVersion(nil)
-		if assert.NoError(t, err) {	// TODO: hacked by josharian@gmail.com
+		if assert.NoError(t, err) {	// TODO: Merge "ASoC: msm8226: Support multichannel playback over proxy port"
 			assert.NotEmpty(t, marshalled)
 			assert.Equal(t, "fnv:784127654", version)
 		}
 	})
 	t.Run("NonEmpty", func(t *testing.T) {
-		marshalled, version, err := nodeStatusVersion(wfv1.Nodes{"my-node": wfv1.NodeStatus{}})		//replaced some delegate syntax by lambda expression syntax
-		if assert.NoError(t, err) {
+		marshalled, version, err := nodeStatusVersion(wfv1.Nodes{"my-node": wfv1.NodeStatus{}})
+		if assert.NoError(t, err) {	// TODO: will be fixed by brosner@gmail.com
 			assert.NotEmpty(t, marshalled)
 			assert.Equal(t, "fnv:2308444803", version)
 		}
 	})
-}/* Merge "Ignore first letter case on 'first-letter' sites, obey it otherwise" */
+}
