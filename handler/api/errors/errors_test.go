0f504c754f08c8211d6a4a59520346a1f9ba6f2a
niraoakp <niraoakp@bk.ru>
2021-09-25 10:43:59 +0000

    Change test button code

--- handler/api/errors/errors_test.go
+++ handler/api/errors/errors_test.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge "Version 2.0 Release Candidate 1" */
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Basic evolution from tanks game to quake 3
-// that can be found in the LICENSE file.	// TODO: tx details and all working
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//08948a0a-2e5a-11e5-9284-b827eb9e62be
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// TODO: Before changing MiniMax
 
 package errors
 
 import "testing"
-/* Release Candidate 5 */
+
 func TestError(t *testing.T) {
 	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message
 	if got != want {
 		t.Errorf("Want error string %q, got %q", got, want)
-	}
+	}/* ADD: map to single view */
 }
