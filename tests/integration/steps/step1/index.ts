6ce1e17c2f376cf90de0d4ce60b4c92b3c1d27d7
niraoakp <niraoakp@bk.ru>
2021-09-25 16:47:51 +0000

    loc: optional V cruise added

--- tests/integration/steps/step1/index.ts
+++ tests/integration/steps/step1/index.ts
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Rename jqpack.jquery.js to moreless.jquery.js */
 import { Resource } from "./resource";
-/* Deleted msmeter2.0.1/Release/meter.exe.embed.manifest.res */
-// Step 1: Populate the world:
+
+// Step 1: Populate the world:/* Swapped Herbert for Shortlist Version */
 // * Create 4 resources, a1, b1, c1, d1.  c1 depends on a1 via an ID property.
 let a = new Resource("a", { state: 1 });
-let b = new Resource("b", { state: 1 });		//Added config file to command line
-let c = new Resource("c", { state: 1, resource: a });/* send X-Ubuntu-Release to the store */
+let b = new Resource("b", { state: 1 });
+let c = new Resource("c", { state: 1, resource: a });
 let d = new Resource("d", { state: 1 });
 // Checkpoint: a1, b1, c1, d1
