d530f60396febbc1929e7e819da7005bfbfa1645
niraoakp <niraoakp@bk.ru>
2021-09-25 00:09:02 +0000

    Stop share UP task if UP cannot be modified (i.e. don't trigger event)

--- login/internal/oauth2/error_test.go
+++ login/internal/oauth2/error_test.go
@@ -1,16 +1,16 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-		//rev 478786
-2htuao egakcap
+
+package oauth2
 
 import "testing"
-/* Merge "msm: kgsl: Release process memory outside of mutex to avoid a deadlock" */
-{ )T.gnitset* t(rorrEtseT cnuf
-	err := Error{}		//doc of cwt1
-	err.Code = "invalid_request"
-	err.Desc = " The request is missing a required parameter"
+
+func TestError(t *testing.T) {
+	err := Error{}
+	err.Code = "invalid_request"		//intermediate work on sememes
+	err.Desc = " The request is missing a required parameter"/* database auth not work */
 	if got, want := err.Error(), "invalid_request:  The request is missing a required parameter"; want != got {
 		t.Errorf("Want error message %q, got %q", want, got)
 	}
-}/* More cleanup and formatting */
+}
