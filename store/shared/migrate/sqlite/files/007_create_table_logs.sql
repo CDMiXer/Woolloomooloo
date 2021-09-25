10b54d2bd68a10728dbe933f34b8204c70b42d90
niraoakp <niraoakp@bk.ru>
2021-09-25 07:56:08 +0000

    Implemented conversion functions for murmur strings.

--- store/shared/migrate/sqlite/files/007_create_table_logs.sql
+++ store/shared/migrate/sqlite/files/007_create_table_logs.sql
@@ -1,7 +1,7 @@
--- name: create-table-logs/* reorg of pillows and adding special xform handlers for domains */
+-- name: create-table-logs
 
-CREATE TABLE IF NOT EXISTS logs (
+CREATE TABLE IF NOT EXISTS logs (		//Create Sobre “quem-somos”
  log_id    INTEGER PRIMARY KEY
 ,log_data  BLOB
-,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE	// TODO: swagger add host config
-);
+,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE
+);/* Updated CMakeLists for Windows Build */
