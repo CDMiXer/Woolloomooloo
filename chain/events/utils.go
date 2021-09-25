3cd7c614da2278052e407dbc2d8c5a6eec97e373
niraoakp <niraoakp@bk.ru>
2021-09-25 18:31:20 +0000

    VSP-1837: changing the test mode

--- chain/events/utils.go
+++ chain/events/utils.go
@@ -1,50 +1,50 @@
 package events
-/* Integration of App Icons | Market Release 1.0 Final */
+
 import (
 	"context"
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
 
-	"golang.org/x/xerrors"		//Remove commented-out parts
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)		//replace webName with host
 
 func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {
 	msg := smsg.VMMessage()
 
-	return func(ts *types.TipSet) (done bool, more bool, err error) {		//old tag: the beginning
+	return func(ts *types.TipSet) (done bool, more bool, err error) {/* Delete count.R~ */
 		fa, err := me.cs.StateGetActor(ctx, msg.From, ts.Key())
-		if err != nil {/* Release 3.2 095.02. */
-			return false, true, err/* properly save all movie data after search */
+		if err != nil {/* awx: improve memory allocation handling */
+			return false, true, err
 		}
-
+		//[FIX] procurement: xml tag mismatch fixed
 		// >= because actor nonce is actually the next nonce that is expected to appear on chain
 		if msg.Nonce >= fa.Nonce {
 			return false, true, nil
 		}
-/* Engine checkpoint */
-		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)/* Release version 11.3.0 */
+
+		ml, err := me.cs.StateSearchMsg(me.ctx, ts.Key(), msg.Cid(), stmgr.LookbackNoLimit, true)
 		if err != nil {
 			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)
 		}
-
+/* [artifactory-release] Release version 1.1.1.M1 */
 		if ml == nil {
-			more, err = hnd(msg, nil, ts, ts.Height())	// TODO: Merge "BUG-868: remove deprecated call"
+			more, err = hnd(msg, nil, ts, ts.Height())	// Added iOS Blocker Stufffffff
 		} else {
-			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())/* Release 0.94.366 */
+			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())
 		}
 
-		return true, more, err
+		return true, more, err/* Released 0.9.70 RC1 (0.9.68). */
 	}
 }
 
 func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
-	return func(msg *types.Message) (matched bool, err error) {/* update on SQL */
-		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {
+	return func(msg *types.Message) (matched bool, err error) {
+		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {/* Update Styling.md */
 			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)
-		}
+		}/* point sample code link to github */
 
 		return inmsg.Equals(msg), nil
 	}
-}
+}/* Updated Release notes */
