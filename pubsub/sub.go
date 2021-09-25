33abe800c81e1a170fb6a04e29a7a5ec81f35456
niraoakp <niraoakp@bk.ru>
2021-09-25 02:26:42 +0000

    Update Epidemiology.html

--- pubsub/sub.go
+++ pubsub/sub.go
@@ -2,14 +2,14 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release 1.5.3-2 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: will be fixed by nagydani@epointsystem.org
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Released 5.1 */
 // limitations under the License.
 
 package pubsub
@@ -20,31 +20,31 @@ import (
 	"github.com/drone/drone/core"
 )
 
-type subscriber struct {
+type subscriber struct {/* Profiling. */
 	sync.Mutex
 
 	handler chan *core.Message
-	quit    chan struct{}
-	done    bool
+	quit    chan struct{}/* -needs curl */
+	done    bool	// TODO: fixed issues with character '-' not being allowed in short options
 }
 
 func (s *subscriber) publish(event *core.Message) {
 	select {
 	case <-s.quit:
 	case s.handler <- event:
-	default:
+	default:		//Updated: filezilla 3.37.0
 		// events are sent on a buffered channel. If there
 		// is a slow consumer that is not processing events,
 		// the buffered channel will fill and newer messages
 		// are ignored.
 	}
 }
-
+	// Delete TemperatureSensor.h
 func (s *subscriber) close() {
 	s.Lock()
 	if s.done == false {
-		close(s.quit)
+		close(s.quit)/* Created squared_logo.png */
 		s.done = true
-	}
+	}/* Added project info and link to the site */
 	s.Unlock()
 }
