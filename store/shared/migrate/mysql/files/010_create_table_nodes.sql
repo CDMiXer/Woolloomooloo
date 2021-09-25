69918c29b52f7a726d194aad68093e571a7f00fb
niraoakp <niraoakp@bk.ru>
2021-09-25 06:37:00 +0000

    bundle in the right place and bindings removed from pom

--- store/shared/migrate/mysql/files/010_create_table_nodes.sql
+++ store/shared/migrate/mysql/files/010_create_table_nodes.sql
@@ -1,7 +1,7 @@
 -- name: create-table-nodes
 
-CREATE TABLE IF NOT EXISTS nodes (/* added Red color for RIG */
- node_id         INTEGER PRIMARY KEY AUTO_INCREMENT/* Release: version 1.0.0. */
+CREATE TABLE IF NOT EXISTS nodes (
+ node_id         INTEGER PRIMARY KEY AUTO_INCREMENT
 ,node_uid        VARCHAR(500)
 ,node_provider   VARCHAR(50)
 ,node_state      VARCHAR(50)
@@ -12,21 +12,21 @@ CREATE TABLE IF NOT EXISTS nodes (/* added Red color for RIG */
 ,node_os         VARCHAR(50)
 ,node_arch       VARCHAR(50)
 ,node_kernel     VARCHAR(50)
-,node_variant    VARCHAR(50)/* Massive refactor after some deep thinkings :) */
+,node_variant    VARCHAR(50)
 ,node_address    VARCHAR(500)
 ,node_capacity   INTEGER
 ,node_filter     VARCHAR(2000)
 ,node_labels     VARCHAR(2000)
-,node_error      VARCHAR(2000)	// TODO: will be fixed by arajasek94@gmail.com
+,node_error      VARCHAR(2000)
 ,node_ca_key     BLOB
 ,node_ca_cert    BLOB
 ,node_tls_key    BLOB
-,node_tls_cert   BLOB	// TODO: hacked by earlephilhower@yahoo.com
+,node_tls_cert   BLOB
 ,node_tls_name   VARCHAR(500)
 ,node_paused     BOOLEAN
 ,node_protected  BOOLEAN
 ,node_created    INTEGER
-,node_updated    INTEGER/* Add: SQLITE_INTROSPECTION_PRAGMAS */
+,node_updated    INTEGER
 ,node_pulled     INTEGER
 
 ,UNIQUE(node_name)
