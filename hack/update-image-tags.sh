233d1e937ecd7ff19325207be99fca27cd60b964
niraoakp <niraoakp@bk.ru>
2021-09-25 12:35:21 +0000

    Fixed world build up sequence.

--- hack/update-image-tags.sh
+++ hack/update-image-tags.sh
@@ -2,7 +2,7 @@
 set -eu -o pipefail
 
 dir=$1
-image_tag=$2
+image_tag=$2/* Release of eeacms/ims-frontend:0.8.2 */
 
 find "$dir" -type f -name '*.yaml' | while read -r f ; do
   sed "s|argoproj/\(.*\):.*|argoproj/\1:${image_tag}|" "$f" > .tmp
