3e5fd1456b1d24160deead10ba05d6bb6c6fbb0e
niraoakp <niraoakp@bk.ru>
2021-09-25 18:53:13 +0000

    Call updateContent implicitly. Also consider new pasted content.

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.cs
@@ -1,9 +1,9 @@
-using Pulumi;	// TODO: Merge "FAB-1846 Storing election config in gossip service"
+using Pulumi;
 using Aws = Pulumi.Aws;
 
-class MyStack : Stack/* new initialization style for buildings */
-{/* Release versioning and CHANGES updates for 0.8.1 */
-    public MyStack()/* 8f243c30-2e75-11e5-9284-b827eb9e62be */
+class MyStack : Stack
+{
+    public MyStack()
     {
         var logs = new Aws.S3.Bucket("logs", new Aws.S3.BucketArgs
         {
@@ -17,10 +17,10 @@ class MyStack : Stack/* new initialization style for buildings */
                     TargetBucket = logs.BucketName,
                 },
             },
-        });/* [README.md] Clarify line ranges */
-        this.TargetBucket = bucket.Loggings.Apply(loggings => loggings[0].TargetBucket);/* [MERGE/IMP]:lp:~openerp-dev/openobject-addons/trunk-contract-apa-follower-dbr */
+        });
+        this.TargetBucket = bucket.Loggings.Apply(loggings => loggings[0].TargetBucket);
     }
-	// TODO: hacked by magik6k@gmail.com
+
     [Output("targetBucket")]
     public Output<string> TargetBucket { get; set; }
-}		//Delete Prototype Hardware.png
+}
