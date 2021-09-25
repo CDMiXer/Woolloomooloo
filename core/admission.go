028b8dd56dd27e26a9b785d3ed2f28bb531db236
niraoakp <niraoakp@bk.ru>
2021-09-25 14:14:50 +0000

    youtube support

--- core/admission.go
+++ core/admission.go
@@ -2,12 +2,12 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* Slightly more descriptive (prescriptive) error */
+//	// TODO: Add yaml test for good measure
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+//	// TODO: hacked by caojiaoyue@protonmail.com
+// Unless required by applicable law or agreed to in writing, software/* Added cache for google images */
+// distributed under the License is distributed on an "AS IS" BASIS,/* Removed <~z~> bug as this now works */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
