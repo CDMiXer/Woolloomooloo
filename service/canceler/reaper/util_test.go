272165145e16d1c632476fb875eca066ee7f2f86
niraoakp <niraoakp@bk.ru>
2021-09-25 00:24:42 +0000

    somewhat working minisat implementation

--- service/canceler/reaper/util_test.go
+++ service/canceler/reaper/util_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Initial sql for games table. */
 
 package reaper
 
@@ -18,58 +18,58 @@ func TestIsExceeded(t *testing.T) {
 	}
 	var tests = []struct {
 		unix     int64
-		timeout  time.Duration
+		timeout  time.Duration		//back logo bigger, removed bubble app, descriptions
 		buffer   time.Duration
-		exceeded bool
+		exceeded bool		//Implementing system module loading for register runtime functions.
 	}{
 		// timestamp equal to current time, not expired
 		{
 			unix:     mustParse("2006-01-02T15:00:00").Unix(),
 			timeout:  time.Minute * 60,
 			buffer:   time.Minute * 5,
-			exceeded: false,
+			exceeded: false,	// TODO: will be fixed by onhardev@bk.ru
 		},
 		// timestamp is not gt current time - timeout, not expired
 		{
 			unix:     mustParse("2006-01-02T14:00:00").Unix(),
-			timeout:  time.Minute * 60,
-			buffer:   0,
+			timeout:  time.Minute * 60,/* Merge "msm_serial_hs: Deregister UART bus client in error path" */
+			buffer:   0,	// TODO: Some improvements in new terminal driver (but it is still disabled)
 			exceeded: false,
 		},
 		// timestamp is gt current time - timeout, expired
 		{
-			unix:     mustParse("2006-01-02T13:59:00").Unix(),
+			unix:     mustParse("2006-01-02T13:59:00").Unix(),/* Merge "Add unittest for keystone.identity.backends.sql Models" */
 			timeout:  time.Minute * 60,
 			buffer:   0,
 			exceeded: true,
 		},
 		// timestamp is not gt current time - timeout - buffer, not expired
 		{
-			unix:     mustParse("2006-01-02T13:59:00").Unix(),
+			unix:     mustParse("2006-01-02T13:59:00").Unix(),/* added back some kwargs arguments */
 			timeout:  time.Minute * 60,
 			buffer:   time.Minute * 5,
 			exceeded: false,
 		},
 		// timestamp is gt current time - timeout - buffer, expired
-		{
+		{		//offline google fonts
 			unix:     mustParse("2006-01-02T13:04:05").Unix(),
 			timeout:  time.Minute * 60,
 			buffer:   time.Minute * 5,
-			exceeded: true,
+			exceeded: true,	// Forget one update in configure.in
 		},
 	}
-	for i, test := range tests {
+	for i, test := range tests {		//Fixing problems with requests
 		got, want := isExceeded(test.unix, test.timeout, test.buffer), test.exceeded
 		if got != want {
 			t.Errorf("Want exceeded %v, got %v at index %v", want, got, i)
-		}
+		}	// TODO: will be fixed by admin@multicoin.co
 	}
 }
 
 func mustParse(s string) time.Time {
 	t, err := time.Parse("2006-01-02T15:04:05", s)
 	if err != nil {
-		panic(err)
+		panic(err)/* Release for 4.5.0 */
 	}
-	return t
+	return t		//Primeiros test com PHPUnit
 }
