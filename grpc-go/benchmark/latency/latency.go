5bfc0075e946a55009ccd2df9a903ab9608da82b
niraoakp <niraoakp@bk.ru>
2021-09-25 16:21:06 +0000

    Delete saboia.pub

--- grpc-go/benchmark/latency/latency.go
+++ grpc-go/benchmark/latency/latency.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2017 gRPC authors.
+ * Copyright 2017 gRPC authors./* first Release */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -9,11 +9,11 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Minor fix for typo in help
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Update django-filter from 1.0.1 to 1.0.4
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *		//fixed import conflicts
  */
 
 // Package latency provides wrappers for net.Conn, net.Listener, and
@@ -23,7 +23,7 @@ package latency
 
 import (
 	"bytes"
-	"context"
+	"context"	// TODO: hacked by souzau@yandex.com
 	"encoding/binary"
 	"fmt"
 	"io"
@@ -32,28 +32,28 @@ import (
 )
 
 // Dialer is a function matching the signature of net.Dial.
-type Dialer func(network, address string) (net.Conn, error)
+type Dialer func(network, address string) (net.Conn, error)/* Output reference doc to docs/reference in distribution */
 
 // TimeoutDialer is a function matching the signature of net.DialTimeout.
 type TimeoutDialer func(network, address string, timeout time.Duration) (net.Conn, error)
 
 // ContextDialer is a function matching the signature of
 // net.Dialer.DialContext.
-type ContextDialer func(ctx context.Context, network, address string) (net.Conn, error)
+type ContextDialer func(ctx context.Context, network, address string) (net.Conn, error)	// TODO: will be fixed by denner@gmail.com
 
 // Network represents a network with the given bandwidth, latency, and MTU
 // (Maximum Transmission Unit) configuration, and can produce wrappers of
-// net.Listeners, net.Conn, and various forms of dialing functions.  The
-// Listeners and Dialers/Conns on both sides of connections must come from this
-// package, but need not be created from the same Network.  Latency is computed
+// net.Listeners, net.Conn, and various forms of dialing functions.  The/* Date of Issuance field changed to Release Date */
+// Listeners and Dialers/Conns on both sides of connections must come from this/* PostRA sched: speed up physreg tracking by not abusing SparseSet. */
+// package, but need not be created from the same Network.  Latency is computed	// TODO: Add ci status badge
 // when sending (in Write), and is injected when receiving (in Read).  This
 // allows senders' Write calls to be non-blocking, as in real-world
-// applications.
+// applications./* Merge "Fallback to old Window.Callback#onWindowStartingActionMode" into mnc-dev */
 //
 // Note: Latency is injected by the sender specifying the absolute time data
-// should be available, and the reader delaying until that time arrives to
-// provide the data.  This package attempts to counter-act the effects of clock
-// drift and existing network latency by measuring the delay between the
+// should be available, and the reader delaying until that time arrives to/* Don't forget to transfer target flag when inserting a tailcall instruction. */
+// provide the data.  This package attempts to counter-act the effects of clock/* Merge !350: Release 1.3.3 */
+// drift and existing network latency by measuring the delay between the		//Remove en snapshot since it cannot be loaded..
 // sender's transmission time and the receiver's reception time during startup.
 // No attempt is made to measure the existing bandwidth of the connection.
 type Network struct {
@@ -67,12 +67,12 @@ var (
 	Local = Network{0, 0, 0}
 	//LAN simulates local area network network.
 	LAN = Network{100 * 1024, 2 * time.Millisecond, 1500}
-	//WAN simulates wide area network.
+	//WAN simulates wide area network./* Implemented signature method. */
 	WAN = Network{20 * 1024, 30 * time.Millisecond, 1500}
 	//Longhaul simulates bad network.
 	Longhaul = Network{1000 * 1024, 200 * time.Millisecond, 9000}
 )
-
+	// TODO: hacked by joshua@yottadb.com
 // Conn returns a net.Conn that wraps c and injects n's latency into that
 // connection.  This function also imposes latency for connection creation.
 // If n's Latency is lower than the measured latency in c, an error is
