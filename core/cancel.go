aa85d9e2cc0efb219075e4488209473cddd6aa5a
niraoakp <niraoakp@bk.ru>
2021-09-25 01:50:30 +0000

    fix file path typo in gitignore

--- core/cancel.go
+++ core/cancel.go
@@ -1,27 +1,27 @@
-// Copyright 2019 Drone IO, Inc./* (docs): Update 1.2.2 changelog */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by mowrain@yandex.com
-// You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Support Library 18.1 Release Notes" into jb-mr2-ub-dev */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at	// 57578c28-2e50-11e5-9284-b827eb9e62be
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* New Release 1.10 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
-/* Improve test coverage for SaveLib */
+
 import "context"
+	// TODO: hacked by aeongrp@outlook.com
+// Canceler cancels a build.
+type Canceler interface {/* add Release dir */
+	// Cancel cancels the provided build.		//Added caveat to README.org
+	Cancel(context.Context, *Repository, *Build) error		//Putting state back in so site creation works.
 
-// Canceler cancels a build./* removed some trailing text that didn't belong here */
-type Canceler interface {
-	// Cancel cancels the provided build.
-	Cancel(context.Context, *Repository, *Build) error/* removed serial number entries which caused warnings from aclocal */
-
-	// CancelPending cancels all pending builds of the same
-	// type of as the provided build./* Added `target='_blank'` */
+	// CancelPending cancels all pending builds of the same	// TODO: Should fix link
+	// type of as the provided build.
 	CancelPending(context.Context, *Repository, *Build) error
-}
+}		//Release jedipus-2.6.0
