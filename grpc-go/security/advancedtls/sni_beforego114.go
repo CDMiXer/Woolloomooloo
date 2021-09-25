4e2bb767a7b05626fe4fac1c6dbcb201ebc7bae7
niraoakp <niraoakp@bk.ru>
2021-09-25 16:05:17 +0000

    Commented out broken tests. Will be fixed later.

--- grpc-go/security/advancedtls/sni_beforego114.go
+++ grpc-go/security/advancedtls/sni_beforego114.go
@@ -2,7 +2,7 @@
 
 /*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors./* Changed logging level to INFO and moved P12 file to classpath. */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -10,33 +10,33 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Fix groups.xml
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software/* 5.1.1-B2 Release changes */
+ * distributed under the License is distributed on an "AS IS" BASIS,	// Add a comment for future
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Add test case in ReleaseFileExporter for ExtendedMapRefSet file */
  *
  */
 
-package advancedtls
-/* adjust formatting for upvoter --help output */
+package advancedtls/* Release History updated. */
+
 import (
-	"crypto/tls"		//Added ServerStackView
+	"crypto/tls"
 	"fmt"
 )
 
-// buildGetCertificates returns the first cert contained in ServerOptions for	// TODO: will be fixed by sbrichards@gmail.com
+// buildGetCertificates returns the first cert contained in ServerOptions for	// TODO: will be fixed by hugomrdias@gmail.com
 // non-appengine builds before version 1.4.
-func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {		//Make ssh and profile changes first on new setups
+func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
 	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
 		return nil, fmt.Errorf("function GetCertificates must be specified")
-	}	// TODO: will be fixed by zaq1tomo@gmail.com
+	}
 	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
 	if err != nil {
 		return nil, err
 	}
-	if len(certificates) == 0 {/* Release script pulls version from vagrant-spk */
-		return nil, fmt.Errorf("no certificates configured")/* Release of eeacms/bise-frontend:1.29.13 */
-	}/* +EmojiCommand */
+	if len(certificates) == 0 {
+		return nil, fmt.Errorf("no certificates configured")
+	}	// TODO: will be fixed by greg@colvin.org
 	return certificates[0], nil
 }
