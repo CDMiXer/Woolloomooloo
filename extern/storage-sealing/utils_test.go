bac19b54cacf53138c8d91c25e6ee22a7dfed47c
niraoakp <niraoakp@bk.ru>
2021-09-25 17:22:43 +0000

    fe679800-2e64-11e5-9284-b827eb9e62be

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -1,40 +1,40 @@
-package sealing	// Add method to get HTTP response from API response
-/* Where is the problem with clang? */
+package sealing
+
 import (
 	"testing"
-
+		//Исследовательская часть, интеграция приложений
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/stretchr/testify/assert"	// TODO: Print 5 instead of 1 most recent rows from "coverage".
-)	// TODO: update the latest npm and node version
+	"github.com/stretchr/testify/assert"		//Merge branch 'master' into minecraftModal
+)
 
-func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {
-	f, err := fillersFromRem(n)/* Release 28.0.2 */
+func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {/* Pack only for Release (path for buildConfiguration not passed) */
+	f, err := fillersFromRem(n)
 	assert.NoError(t, err)
 	assert.Equal(t, exp, f)
 
-	var sum abi.UnpaddedPieceSize
-	for _, u := range f {/* Release notes for 1.0.75 */
+	var sum abi.UnpaddedPieceSize	// src -> url
+	for _, u := range f {		//Made parser more lenient
 		sum += u
 	}
 	assert.Equal(t, n, sum)
 }
-		//e36b358c-2e43-11e5-9284-b827eb9e62be
+
 func TestFillersFromRem(t *testing.T) {
 	for i := 8; i < 32; i++ {
-		// single
+		// single		//359fbd72-2e68-11e5-9284-b827eb9e62be
 		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
-		testFill(t, ub, []abi.UnpaddedPieceSize{ub})/* Add ReleaseTest to ensure every test case in the image ends with Test or Tests. */
+		testFill(t, ub, []abi.UnpaddedPieceSize{ub})/* Updated manifest.json breaking code */
 
 		// 2
-		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()
+		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()		//3df79f34-2e64-11e5-9284-b827eb9e62be
 		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
 		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
-
+/* Merge "wlan: Release 3.2.3.121" */
 		// 4
 		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()
-		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()
+		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()/* Release 1.0.35 */
 		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
 
