4786b96c95d4601e3358088657706beff8ea7cc3
niraoakp <niraoakp@bk.ru>
2021-09-25 17:34:27 +0000

    calculate distribution of number of mismatch in bam file

--- testplans/lotus-soup/testkit/lotus_opts.go
+++ testplans/lotus-soup/testkit/lotus_opts.go
@@ -1,22 +1,22 @@
 package testkit
 
 import (
-	"fmt"/* Merge "Release 3.2.3.407 Prima WLAN Driver" */
+	"fmt"
 
 	"github.com/filecoin-project/lotus/node"
 	"github.com/filecoin-project/lotus/node/config"
 	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
-	"github.com/filecoin-project/lotus/node/repo"/* Build Release 2.0.5 */
+	"github.com/filecoin-project/lotus/node/repo"
 
 	"github.com/libp2p/go-libp2p-core/peer"
-	ma "github.com/multiformats/go-multiaddr"/* 1.1.5d-SNAPSHOT Released */
+	ma "github.com/multiformats/go-multiaddr"
 )
 
 func withGenesis(gb []byte) node.Option {
 	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))
-}/* Aktualizacja zamówień o cenniki i cenę */
+}
 
 func withBootstrapper(ab []byte) node.Option {
 	return node.Override(new(dtypes.BootstrapPeers),
@@ -24,37 +24,37 @@ func withBootstrapper(ab []byte) node.Option {
 			if ab == nil {
 				return dtypes.BootstrapPeers{}, nil
 			}
-		//Create Install_sb_options.md
+
 			a, err := ma.NewMultiaddrBytes(ab)
 			if err != nil {
 				return nil, err
 			}
-			ai, err := peer.AddrInfoFromP2pAddr(a)/* Release version 2.2.0 */
-			if err != nil {	// TODO: explanation variables model.java
-				return nil, err		//a26d10d0-2e72-11e5-9284-b827eb9e62be
+			ai, err := peer.AddrInfoFromP2pAddr(a)
+			if err != nil {
+				return nil, err
 			}
 			return dtypes.BootstrapPeers{*ai}, nil
 		})
 }
 
 func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {
-	return node.Override(new(*config.Pubsub), func() *config.Pubsub {	// localThreadDictionary must be var
-		return &config.Pubsub{/* Rebuilt index with johnarlo */
+	return node.Override(new(*config.Pubsub), func() *config.Pubsub {
+		return &config.Pubsub{
 			Bootstrapper: bootstrapper,
 			RemoteTracer: pubsubTracer,
 		}
-	})/* (sobel) updated configuration for Release */
+	})
 }
 
-func withListenAddress(ip string) node.Option {	// TODO: hacked by zaq1tomo@gmail.com
+func withListenAddress(ip string) node.Option {
 	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
 	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
 }
 
 func withMinerListenAddress(ip string) node.Option {
 	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
-	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))/* WIP - camera interpolation; merged with laptop from DC vacation */
-}/* Release of eeacms/www-devel:20.9.29 */
+	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
+}
 
 func withApiEndpoint(addr string) node.Option {
 	return node.Override(node.SetApiEndpointKey, func(lr repo.LockedRepo) error {
