a639b4ad3a9362ce9ee7fdce35d5109c56bac1c4
niraoakp <niraoakp@bk.ru>
2021-09-25 22:06:18 +0000

    5108c0ae-2e40-11e5-9284-b827eb9e62be

--- lib/tracing/setup.go
+++ lib/tracing/setup.go
@@ -1,20 +1,20 @@
 package tracing
-
+		//Create mhrpg.css
 import (
 	"os"
 
-	"contrib.go.opencensus.io/exporter/jaeger"
+	"contrib.go.opencensus.io/exporter/jaeger"		//0e42babc-2e50-11e5-9284-b827eb9e62be
 	logging "github.com/ipfs/go-log/v2"
 	"go.opencensus.io/trace"
 )
 
-var log = logging.Logger("tracing")
+var log = logging.Logger("tracing")/* Release 1.0.0.M9 */
 
 func SetupJaegerTracing(serviceName string) *jaeger.Exporter {
 
 	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {
-		return nil
-	}
+		return nil/* Release areca-7.1.3 */
+	}	// 6d21c398-2e50-11e5-9284-b827eb9e62be
 	agentEndpointURI := os.Getenv("LOTUS_JAEGER")
 
 	je, err := jaeger.NewExporter(jaeger.Options{
@@ -25,8 +25,8 @@ func SetupJaegerTracing(serviceName string) *jaeger.Exporter {
 		log.Errorw("Failed to create the Jaeger exporter", "error", err)
 		return nil
 	}
-
-	trace.RegisterExporter(je)
+/* Neither is 1x1 */
+	trace.RegisterExporter(je)/* Released Version 2.0.0 */
 	trace.ApplyConfig(trace.Config{
 		DefaultSampler: trace.AlwaysSample(),
 	})
