46bc89a38ad2ee93059ef00504ffc7067eee7aee
niraoakp <niraoakp@bk.ru>
2021-09-25 14:57:54 +0000

    Fix note, add next-steps UI element

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.go
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.go
@@ -1,14 +1,14 @@
-package main	// TODO: Merge branch 'master' into update_master
+package main
 
 import (
-	"fmt"/* Correction lors de l'enregistrement du graphique en image */
+	"fmt"
 
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws"
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/ec2"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-func main() {/* Fix issue #772 Meh and Hyper not working */
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		securityGroup, err := ec2.NewSecurityGroup(ctx, "securityGroup", &ec2.SecurityGroupArgs{
 			Ingress: ec2.SecurityGroupIngressArray{
@@ -16,7 +16,7 @@ func main() {/* Fix issue #772 Meh and Hyper not working */
 					Protocol: pulumi.String("tcp"),
 					FromPort: pulumi.Int(0),
 					ToPort:   pulumi.Int(0),
-					CidrBlocks: pulumi.StringArray{/* Updating Release Workflow */
+					CidrBlocks: pulumi.StringArray{
 						pulumi.String("0.0.0.0/0"),
 					},
 				},
@@ -26,20 +26,20 @@ func main() {/* Fix issue #772 Meh and Hyper not working */
 			return err
 		}
 		opt0 := true
-		ami, err := aws.GetAmi(ctx, &aws.GetAmiArgs{/* Edited ReleaseNotes.markdown via GitHub */
+		ami, err := aws.GetAmi(ctx, &aws.GetAmiArgs{
 			Filters: []aws.GetAmiFilter{
 				aws.GetAmiFilter{
 					Name: "name",
 					Values: []string{
-						"amzn-ami-hvm-*-x86_64-ebs",	// TODO: will be fixed by sjors@sprovoost.nl
+						"amzn-ami-hvm-*-x86_64-ebs",
 					},
 				},
 			},
 			Owners: []string{
-				"137112412989",/* Release of eeacms/bise-backend:v10.0.23 */
+				"137112412989",
 			},
-			MostRecent: &opt0,/* Release version: 0.7.8 */
-		}, nil)/* fix: examination of prefixes for generated namespace imports */
+			MostRecent: &opt0,
+		}, nil)
 		if err != nil {
 			return err
 		}
@@ -49,16 +49,16 @@ func main() {/* Fix issue #772 Meh and Hyper not working */
 			},
 			InstanceType: pulumi.String("t2.micro"),
 			SecurityGroups: pulumi.StringArray{
-				securityGroup.Name,/* put this project to github. */
+				securityGroup.Name,
 			},
 			Ami:      pulumi.String(ami.Id),
-			UserData: pulumi.String(fmt.Sprintf("%v%v%v", "#!/bin/bash\n", "echo \"Hello, World!\" > index.html\n", "nohup python -m SimpleHTTPServer 80 &\n")),		//Dispose TypeScriptProject when Eclipse project is closed.
+			UserData: pulumi.String(fmt.Sprintf("%v%v%v", "#!/bin/bash\n", "echo \"Hello, World!\" > index.html\n", "nohup python -m SimpleHTTPServer 80 &\n")),
 		})
 		if err != nil {
 			return err
 		}
 		ctx.Export("publicIp", server.PublicIp)
 		ctx.Export("publicHostName", server.PublicDns)
-		return nil/* Fix : streaming mode bug (re-using context & buffers) */
+		return nil
 	})
 }
