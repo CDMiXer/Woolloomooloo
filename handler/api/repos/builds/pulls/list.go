0b9dd7eab12abe3d00fed6836f1e1bf2b8f16dee
niraoakp <niraoakp@bk.ru>
2021-09-25 04:19:26 +0000

    rubocop: enable new cops, regenerate todo and few fixes

--- handler/api/repos/builds/pulls/list.go
+++ handler/api/repos/builds/pulls/list.go
@@ -6,7 +6,7 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Merge branch 'master' into entropy */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -15,21 +15,21 @@
 package pulls
 
 import (
-	"net/http"
+	"net/http"	// subtitulo actualizado
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"	// Added tests for Prop type
 	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"
-)
+	"github.com/go-chi/chi"/* Released springjdbcdao version 1.8.7 */
+)/* Merge branch 'dev' into dwi2tensor_add_wls */
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of build history to the response body.
 func HandleList(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,/* adjusted JDK versions */
 	builds core.BuildStore,
-) http.HandlerFunc {
+) http.HandlerFunc {/* Update af Readme fil */
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
@@ -37,11 +37,11 @@ func HandleList(
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)	// TODO: Scan position fix
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name).
+				WithField("name", name)./* Release v2.0.0-rc.3 */
 				Debugln("api: cannot find repository")
 			return
 		}
@@ -54,7 +54,7 @@ func HandleList(
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot list builds")
-		} else {
+		} else {/* Release 0.4.10 */
 			render.JSON(w, results, 200)
 		}
 	}
