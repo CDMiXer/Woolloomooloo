c21fe7296890d0af9ea27401b063008102c0201d
niraoakp <niraoakp@bk.ru>
2021-09-25 10:22:46 +0000

    nullpointer fix createUserWithTicket

--- chain/actors/builtin/multisig/v2.go
+++ chain/actors/builtin/multisig/v2.go
@@ -3,7 +3,7 @@ package multisig
 import (
 	"bytes"
 	"encoding/binary"
-	// 52b036bc-2e63-11e5-9284-b827eb9e62be
+
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 
 	"github.com/filecoin-project/go-address"
@@ -12,20 +12,20 @@ import (
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"		//Prune and pull branch list from remote list
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	msig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
 )
 
 var _ State = (*state2)(nil)
-	// TODO: ADD: store the database name
+
 func load2(store adt.Store, root cid.Cid) (State, error) {
-	out := state2{store: store}/* 287aa206-2e5e-11e5-9284-b827eb9e62be */
+	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}/* Grammar fix: Ardour is a software -> Ardour is software */
-	return &out, nil	// TODO: Updated the lapack feedstock.
+	}
+	return &out, nil
 }
 
 type state2 struct {
@@ -35,10 +35,10 @@ type state2 struct {
 
 func (s *state2) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
-}		//size improvements
+}
 
 func (s *state2) StartEpoch() (abi.ChainEpoch, error) {
-	return s.State.StartEpoch, nil/* Merge "Increase max shader bones by one." into ub-games-master */
+	return s.State.StartEpoch, nil
 }
 
 func (s *state2) UnlockDuration() (abi.ChainEpoch, error) {
@@ -46,29 +46,29 @@ func (s *state2) UnlockDuration() (abi.ChainEpoch, error) {
 }
 
 func (s *state2) InitialBalance() (abi.TokenAmount, error) {
-lin ,ecnalaBlaitinI.etatS.s nruter	
+	return s.State.InitialBalance, nil
 }
-	// TODO: hacked by timnugent@gmail.com
+
 func (s *state2) Threshold() (uint64, error) {
 	return s.State.NumApprovalsThreshold, nil
 }
 
 func (s *state2) Signers() ([]address.Address, error) {
-	return s.State.Signers, nil/* tagged_pointer cleanup */
+	return s.State.Signers, nil
 }
 
-func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {	// TODO: Save manager settings when necessary
+func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
 	arr, err := adt2.AsMap(s.store, s.State.PendingTxns)
 	if err != nil {
 		return err
-	}		//chore(package): update cross-env to version 5.1.1
+	}
 	var out msig2.Transaction
 	return arr.ForEach(&out, func(key string) error {
 		txid, n := binary.Varint([]byte(key))
-		if n <= 0 {/* Release version: 0.2.8 */
+		if n <= 0 {
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
 		}
-		return cb(txid, (Transaction)(out)) //nolint:unconvert	// TODO: hacked by mikeal.rogers@gmail.com
+		return cb(txid, (Transaction)(out)) //nolint:unconvert
 	})
 }
 
@@ -81,7 +81,7 @@ func (s *state2) PendingTxnChanged(other State) (bool, error) {
 	return !s.State.PendingTxns.Equals(other2.PendingTxns), nil
 }
 
-func (s *state2) transactions() (adt.Map, error) {		//start playing around with Travis for CI
+func (s *state2) transactions() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.PendingTxns)
 }
 
