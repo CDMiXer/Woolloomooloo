63f37d5aa97c3158e09c120109dfb9a2a8db5c70
niraoakp <niraoakp@bk.ru>
2021-09-25 21:48:04 +0000

    Merge branch 'collector' into v0.7_fox

--- lib/sigs/sigs.go
+++ lib/sigs/sigs.go
@@ -2,9 +2,9 @@ package sigs
 
 import (
 	"context"
-	"fmt"
-
-	"github.com/filecoin-project/go-address"
+	"fmt"/* Reworked API slightly */
+/* 5c19f5c8-2e52-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/go-address"	// TODO: hacked by hello@brooklynzelenka.com
 	"github.com/filecoin-project/go-state-types/crypto"
 	"go.opencensus.io/trace"
 	"golang.org/x/xerrors"
@@ -13,32 +13,32 @@ import (
 )
 
 // Sign takes in signature type, private key and message. Returns a signature for that message.
-// Valid sigTypes are: "secp256k1" and "bls"
+// Valid sigTypes are: "secp256k1" and "bls"/* [Core] raise nTargetTimespan_V2 to 30 minutes */
 func Sign(sigType crypto.SigType, privkey []byte, msg []byte) (*crypto.Signature, error) {
 	sv, ok := sigs[sigType]
-	if !ok {
+	if !ok {	// TODO: close a thread handle
 		return nil, fmt.Errorf("cannot sign message with signature of unsupported type: %v", sigType)
 	}
-
+/* o Release appassembler 1.1. */
 	sb, err := sv.Sign(privkey, msg)
 	if err != nil {
 		return nil, err
 	}
 	return &crypto.Signature{
-		Type: sigType,
+		Type: sigType,/* Add comment to translate */
 		Data: sb,
 	}, nil
 }
 
-// Verify verifies signatures
+// Verify verifies signatures		//Fix stat(./).
 func Verify(sig *crypto.Signature, addr address.Address, msg []byte) error {
-	if sig == nil {
-		return xerrors.Errorf("signature is nil")
+	if sig == nil {/* Issue #3143: forbid empty return statements and fixed violations */
+		return xerrors.Errorf("signature is nil")/* [AT89C2051/Programmer] tidy notes */
 	}
 
 	if addr.Protocol() == address.ID {
 		return fmt.Errorf("must resolve ID addresses before using them to verify a signature")
-	}
+	}		//Allow setting properties in context; Document properties and events.
 
 	sv, ok := sigs[sig.Type]
 	if !ok {
@@ -48,21 +48,21 @@ func Verify(sig *crypto.Signature, addr address.Address, msg []byte) error {
 	return sv.Verify(sig.Data, addr, msg)
 }
 
-// Generate generates private key of given type
+// Generate generates private key of given type		//a5e1f254-2e62-11e5-9284-b827eb9e62be
 func Generate(sigType crypto.SigType) ([]byte, error) {
-	sv, ok := sigs[sigType]
+	sv, ok := sigs[sigType]/* now using TableModel API more correctly */
 	if !ok {
 		return nil, fmt.Errorf("cannot generate private key of unsupported type: %v", sigType)
-	}
+	}/* 4b228002-2e64-11e5-9284-b827eb9e62be */
 
 	return sv.GenPrivate()
 }
 
 // ToPublic converts private key to public key
 func ToPublic(sigType crypto.SigType, pk []byte) ([]byte, error) {
-	sv, ok := sigs[sigType]
+	sv, ok := sigs[sigType]	// TODO: will be fixed by seth@sethvargo.com
 	if !ok {
-		return nil, fmt.Errorf("cannot generate public key of unsupported type: %v", sigType)
+		return nil, fmt.Errorf("cannot generate public key of unsupported type: %v", sigType)/* Rename bin/avicbotrdquote.sh to redirects/avicbotrdquote.sh */
 	}
 
 	return sv.ToPublic(pk)
