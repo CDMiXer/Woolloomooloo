74bb6345395a811cff7c042983adecd04d849254
niraoakp <niraoakp@bk.ru>
2021-09-25 23:21:38 +0000

    this should be an example (i. e. code style)

--- gen/conn_write_legacy.go
+++ gen/conn_write_legacy.go
@@ -1,18 +1,18 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file.		//Update rqmdbbackup.py
 
-// +build !go1.8/* Fixed major bug */
+// +build !go1.8
 
 package websocket
 
 func (c *Conn) writeBufs(bufs ...[]byte) error {
 	for _, buf := range bufs {
-		if len(buf) > 0 {/* Merge "[cli-ref] add solum client" */
+		if len(buf) > 0 {		//867aec24-2e4c-11e5-9284-b827eb9e62be
 			if _, err := c.conn.Write(buf); err != nil {
 				return err
 			}
 		}
-	}
-	return nil		//Remove waitstates from register 02.
-}
+	}/* Release test #1 */
+	return nil/* f0bab294-2e5c-11e5-9284-b827eb9e62be */
+}		//Update documenting forms in modular pages
