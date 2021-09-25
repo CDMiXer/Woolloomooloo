ddb97197455591e180320ccb1e7c7f20fc4d895c
niraoakp <niraoakp@bk.ru>
2021-09-25 12:24:14 +0000

    Update main_gantry.ino

--- plugin/converter/noop.go
+++ plugin/converter/noop.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: hacked by remco@dutchcoders.io
-// Licensed under the Apache License, Version 2.0 (the "License");/* correctly handling callback errors */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Release 1.06 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Comment Formatted */
+
 // +build oss
 
 package converter
 
-import (		//nebula level00 spicy bacon ipsum and eclipse project stuff
+import (
 	"context"
-	// TODO: [afu_bodenprofilstandorte_nabodat_pub] queue angepasst
-	"github.com/drone/drone/core"		//Delete install-webserver.sh
+
+	"github.com/drone/drone/core"
 )
 
 type noop struct{}
