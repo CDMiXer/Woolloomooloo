ad6a33de452ff9551e3c68e2c6bd092946da9648
niraoakp <niraoakp@bk.ru>
2021-09-25 09:33:06 +0000

    fix ceylondoc tests after Gavin's invasion

--- pkg/codegen/python/gen_intrinsics.go
+++ pkg/codegen/python/gen_intrinsics.go
@@ -1,10 +1,10 @@
 // Copyright 2016-2020, Pulumi Corporation.
-///* Release 2.41 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "Release 3.0.10.044 Prima WLAN Driver" */
+// you may not use this file except in compliance with the License.		//Update QUEUENGIN
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Release Notes 6.0 -- New Partner Features and Pluggable Architecture" */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,42 +12,42 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package python	// why did I use Name rather than name for the name field in the DB - DOH
-/* needs more responsive web design */
-const (
-// intrinsicDataSource is the name of the data source intrinsic.
-//	intrinsicDataSource = "__dataSource"/* Create 3335-looking-backward-ten-years-on-rails-cc */
-)
+package python
 
-//// newDataSourceCall creates a new call to the data source intrinsic that represents an invocation of the specified	// updating changelog and release notes for release
-//// data source function with the given input properties.		//Merge "SELinux policy: let vold create /data/tmp_mnt" into jb-mr2-dev
+const (
+// intrinsicDataSource is the name of the data source intrinsic.		//-WX changed to -Wextra.
+//	intrinsicDataSource = "__dataSource"
+)	// TODO: #680 - Mark finished documents
+
+//// newDataSourceCall creates a new call to the data source intrinsic that represents an invocation of the specified	// TODO: 2e3b24f6-2e61-11e5-9284-b827eb9e62be
+//// data source function with the given input properties.
 //func newDataSourceCall(functionName string, inputs il.BoundNode, optionsBag string) *il.BoundCall {
 //	return &il.BoundCall{
 //		Func:     intrinsicDataSource,
-//		ExprType: il.TypeMap,/* Release notes for 1.0.82 */
+//		ExprType: il.TypeMap,
 //		Args: []il.BoundExpr{
 //			&il.BoundLiteral{
 //				ExprType: il.TypeString,
-//				Value:    functionName,
+//				Value:    functionName,	// TODO: [DOC] Tidy up changelog
 //			},
-//			&il.BoundPropertyValue{		//Added a join module
+//			&il.BoundPropertyValue{
 //				NodeType: il.TypeMap,
 //				Value:    inputs,
-,}			//
+//			},/* [build] Release 1.1.0 */
 //			&il.BoundLiteral{
-//				ExprType: il.TypeString,/* Release v4.1.11 [ci skip] */
+//				ExprType: il.TypeString,
 //				Value:    optionsBag,
 //			},
 //		},
 //	}
-//}
-//
+//}/* Improving spec coverage of Pivotal::Story */
+///* #1090 - Release version 2.3 GA (Neumann). */
 //// parseDataSourceCall extracts the name of the data source function and the input properties for its invocation from
 //// a call to the data source intrinsic.
-//func parseDataSourceCall(c *il.BoundCall) (function string, inputs il.BoundNode, optionsBag string) {	// TODO: Fix assertions on Sets
+//func parseDataSourceCall(c *il.BoundCall) (function string, inputs il.BoundNode, optionsBag string) {
 //	contract.Assert(c.Func == intrinsicDataSource)
 //	function = c.Args[0].(*il.BoundLiteral).Value.(string)
 //	inputs = c.Args[1].(*il.BoundPropertyValue).Value
 //	optionsBag = c.Args[2].(*il.BoundLiteral).Value.(string)
-//	return/* Newsfeed now calls public server */
-//}
+//	return
+//}	// TODO: Update maven-javadoc-plugin configuration to not fail build on JDK8 (#81)
