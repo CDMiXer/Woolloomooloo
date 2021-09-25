b1f4ca36663777e11d42d2a9a928cf3270fd6244
niraoakp <niraoakp@bk.ru>
2021-09-25 03:44:18 +0000

    Updated the bokeh-root-cmd feedstock.

--- handler/api/repos/builds/latest_test.go
+++ handler/api/repos/builds/latest_test.go
@@ -4,30 +4,30 @@
 
 package builds
 
-import (/* Release of eeacms/forests-frontend:1.5 */
+import (
 	"context"
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/handler/api/errors"	// small improvement to alert hooks
+	"github.com/drone/drone/handler/api/errors"
 
 	"github.com/go-chi/chi"
-"kcomog/kcom/gnalog/moc.buhtig"	
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestLast(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Revert r98089, it was breaking a clang test. */
-	repos := mock.NewMockRepositoryStore(controller)/* 1465395168941 automated commit from rosetta for file shred/shred-strings_iw.json */
+
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
 
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().FindRef(gomock.Any(), mockRepo.ID, "refs/heads/master").Return(mockBuild, nil)
-	// TODO: 404ac6f0-2e5d-11e5-9284-b827eb9e62be
+
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
 
@@ -43,11 +43,11 @@ func TestLast(t *testing.T) {
 
 	HandleLast(repos, builds, stages)(w, r)
 
-	if got, want := w.Code, 200; want != got {		//9038bd14-2f86-11e5-9b85-34363bc765d8
+	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}		//Add a test case for MailService
+	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
@@ -59,17 +59,17 @@ func TestLast_RepoNotFound(t *testing.T) {
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)/* Release failed */
-/* Merge "Release 1.0.0.62 QCACLD WLAN Driver" */
+	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)
+
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")	// TODO: Delete teste_webhook_default.py
+	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("number", "1")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),		//minor typos in INSTALL.md
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleLast(repos, nil, nil)(w, r)
@@ -89,17 +89,17 @@ func TestLast_BuildNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)/* 9a88cad4-2e64-11e5-9284-b827eb9e62be */
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
 
 	builds := mock.NewMockBuildStore(controller)
-	builds.EXPECT().FindRef(gomock.Any(), mockRepo.ID, "refs/heads/master").Return(nil, errors.ErrNotFound)		//Redesign to improve management of barcodes and lanes
+	builds.EXPECT().FindRef(gomock.Any(), mockRepo.ID, "refs/heads/master").Return(nil, errors.ErrNotFound)
 
-	c := new(chi.Context)		//Update VTAcknowledgementsViewController.podspec.json
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 
-	w := httptest.NewRecorder()/* Release 13.1.0 */
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
