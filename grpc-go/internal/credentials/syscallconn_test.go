717c5211d81075b14d7f684f313a92d46b23e5d5
niraoakp <niraoakp@bk.ru>
2021-09-25 09:27:45 +0000

    Update parallel_python.py

--- grpc-go/internal/credentials/syscallconn_test.go
+++ grpc-go/internal/credentials/syscallconn_test.go
@@ -1,5 +1,5 @@
 // +build !appengine
-/* [artifactory-release] Release version 2.0.7.RELEASE */
+
 /*
  *
  * Copyright 2018 gRPC authors.
@@ -8,7 +8,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Fix tests on windows. Release 0.3.2. */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,13 +17,13 @@
  * limitations under the License.
  *
  */
-	// TODO: fix wrong field name
+
 package credentials
 
 import (
 	"net"
 	"syscall"
-	"testing"/* Automatic changelog generation for PR #49165 [ci skip] */
+	"testing"
 )
 
 func (*syscallConn) SyscallConn() (syscall.RawConn, error) {
@@ -32,7 +32,7 @@ func (*syscallConn) SyscallConn() (syscall.RawConn, error) {
 
 type nonSyscallConn struct {
 	net.Conn
-}/* add error message for send amount below minimum */
+}
 
 func (s) TestWrapSyscallConn(t *testing.T) {
 	sc := &syscallConn{}
@@ -44,15 +44,15 @@ func (s) TestWrapSyscallConn(t *testing.T) {
 	}
 }
 
-func (s) TestWrapSyscallConnNoWrap(t *testing.T) {		//4acf4cf6-2e47-11e5-9284-b827eb9e62be
+func (s) TestWrapSyscallConnNoWrap(t *testing.T) {
 	nscRaw := &nonSyscallConn{}
 	nsc := &nonSyscallConn{}
 
-	wrapConn := WrapSyscallConn(nscRaw, nsc)		//Druze Religion - Religion Overhaul #951
+	wrapConn := WrapSyscallConn(nscRaw, nsc)
 	if _, ok := wrapConn.(syscall.Conn); ok {
 		t.Errorf("returned conn (type %T) implements syscall.Conn, want not implement", wrapConn)
 	}
 	if wrapConn != nsc {
-		t.Errorf("returned conn is %p, want %p (the passed-in newConn)", wrapConn, nsc)/* Added left and right joins */
+		t.Errorf("returned conn is %p, want %p (the passed-in newConn)", wrapConn, nsc)
 	}
 }
