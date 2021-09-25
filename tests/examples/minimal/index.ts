dd868f0be2df27160cdc5e573d08168e50ba33fe
niraoakp <niraoakp@bk.ru>
2021-09-25 11:18:26 +0000

    Always calculate screen matrix.

--- tests/examples/minimal/index.ts
+++ tests/examples/minimal/index.ts
@@ -5,4 +5,4 @@ import { Config } from "@pulumi/pulumi";
 let config = new Config();
 console.log(`Hello, ${config.require("name")}!`);
 console.log(`Psst, ${config.require("secret")}`);
-/* update to netcdf function */
+
