7489396f18f866318f8f6351d9135fc2be0c1c85
niraoakp <niraoakp@bk.ru>
2021-09-25 22:01:00 +0000

    Updating build-info/dotnet/corert/master for alpha-25715-01

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Tagging a Release Candidate - v4.0.0-rc9. */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Release 2.2.0 */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Updated AnalyzerBeans version */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
