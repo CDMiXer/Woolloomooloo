00933f5727adf409c8b1a7b03f19f0b009c64ca0
niraoakp <niraoakp@bk.ru>
2021-09-25 08:16:03 +0000

    Move c.i.j.service.impl.deps message bundle to c.i.j.core.deps

--- tests/integration/aliases/aliases_nodejs_test.go
+++ tests/integration/aliases/aliases_nodejs_test.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints/* Merge "msm: vidc: Release resources only if they are loaded" */
+package ints
 
 import (
 	"path/filepath"
@@ -14,28 +14,28 @@ var dirs = []string{
 	"rename",
 	"adopt_into_component",
 	"rename_component_and_child",
-	"retype_component",	// ROOT package added
+	"retype_component",
 	"rename_component",
-}/* add vm on computers views */
+}
 
-// TestNodejsAliases tests a case where a resource's name changes but it provides an `alias`/* Merge "CLI implementation" */
+// TestNodejsAliases tests a case where a resource's name changes but it provides an `alias`
 // pointing to the old URN to ensure the resource is preserved across the update.
 func TestNodejsAliases(t *testing.T) {
 	for _, dir := range dirs {
 		d := filepath.Join("nodejs", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir:          filepath.Join(d, "step1"),		//6441edb6-2e5f-11e5-9284-b827eb9e62be
+				Dir:          filepath.Join(d, "step1"),
 				Dependencies: []string{"@pulumi/pulumi"},
-				Quick:        true,/* commiting all the launch actions */
+				Quick:        true,
 				EditDirs: []integration.EditDir{
 					{
-						Dir:             filepath.Join(d, "step2"),	// TODO: Updating URL
+						Dir:             filepath.Join(d, "step2"),
 						Additive:        true,
 						ExpectNoChanges: true,
 					},
 				},
 			})
-		})/* Visualization of compartement and relation between it using Roassal 2 */
+		})
 	}
 }
