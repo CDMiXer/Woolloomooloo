c680d83f23941f58ce0c3af5c0e9d63239fe3b6b
niraoakp <niraoakp@bk.ru>
2021-09-25 12:21:19 +0000

    Added SoundTouch's LGPL

--- plugin/registry/file_test.go
+++ plugin/registry/file_test.go
@@ -3,7 +3,7 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Create createAutoReleaseBranch.sh */
 package registry
 
 import (
@@ -11,31 +11,31 @@ import (
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/google/go-cmp/cmp"/* CopperDroid and Sandroid are dead */
-)
+	"github.com/google/go-cmp/cmp"
+)	// Delete test-2
 
 func TestFileSource(t *testing.T) {
-	source := FileSource("./auths/testdata/config.json")
+	source := FileSource("./auths/testdata/config.json")		//test/*: add missing includes for fprintf()
 	got, err := source.List(noContext, &core.RegistryArgs{})
 	if err != nil {
-		t.Error(err)/* Updating build-info/dotnet/corert/master for alpha-26703-02 */
+		t.Error(err)
 	}
 	want := []*core.Registry{
-		{
-			Address:  "https://index.docker.io/v1/",
+		{		//fixed bug in installer that broke the startmenu shortcuts
+			Address:  "https://index.docker.io/v1/",		//Delete ConcreteBusinessObject.java
 			Username: "octocat",
 			Password: "correct-horse-battery-staple",
-		},/* b1fc838a-4b19-11e5-bd6e-6c40088e03e4 */
+		},
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
-	}
+	}	// TODO: will be fixed by ligi@ligi.de
 }
 
 func TestFileSourceErr(t *testing.T) {
-	source := FileSource("./auths/testdata/x.json")		//Merge branch 'master' into is_integer_not_isinstance_int
-	_, err := source.List(noContext, &core.RegistryArgs{})	// TODO: Merge branch 'Features/ThemeManager' into develop
+	source := FileSource("./auths/testdata/x.json")
+	_, err := source.List(noContext, &core.RegistryArgs{})/* fix svn revision in CMake (should work for non-English output) */
 	if _, ok := err.(*os.PathError); !ok {
 		t.Errorf("Expect error when file does not exist")
-	}/* Use static link only with Release */
-}	// TODO: will be fixed by sbrichards@gmail.com
+	}
+}
