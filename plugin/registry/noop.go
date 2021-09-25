e3772da4fcd23aab835a9a18299b0d8c0d693c1c
niraoakp <niraoakp@bk.ru>
2021-09-25 17:02:37 +0000

    Prepare bintray publishing

--- plugin/registry/noop.go
+++ plugin/registry/noop.go
@@ -1,23 +1,23 @@
-// Copyright 2019 Drone IO, Inc./* Update uReleasename.pas */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: hacked by martin2cai@hotmail.com
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by martin2cai@hotmail.com
+//      http://www.apache.org/licenses/LICENSE-2.0		//another error...
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update DataEnricher.java */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package registry/* 9065c212-2e75-11e5-9284-b827eb9e62be */
+package registry/* Merge "Fix security group list command" */
 
-import (	// TODO: hacked by magik6k@gmail.com
-	"context"
+import (
+	"context"/* Committing the .iss file used for 1.3.12 ANSI Release */
 
-	"github.com/drone/drone/core"	// Delete ui-icons_0078ae_256x240.png
+	"github.com/drone/drone/core"
 )
 
 type noop struct{}
