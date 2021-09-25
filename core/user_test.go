be38180732456be3dc47c90be088d724524ecaf5
niraoakp <niraoakp@bk.ru>
2021-09-25 16:40:48 +0000

    Saved a Panamax template jd_demo.pmx

--- core/user_test.go
+++ core/user_test.go
@@ -2,63 +2,63 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss		//Delete communityPage.import.css.map
+// +build !oss
 
 package core
 
-import (/* 77eaf412-2e4a-11e5-9284-b827eb9e62be */
+import (	// TODO: even more pics
 	"testing"
-)/* [Docs] Add label PropType to Toggle for documentation */
-/* Remove getrange and checkrange from __functions */
+)
+
 func TestValidateUser(t *testing.T) {
 	tests := []struct {
-		user *User/* Release notes for 1.0.93 */
-		err  error
-{}	
-		{
+		user *User
+		err  error/* Adding Release Build script for Windows  */
+	}{
+		{/* Merge branch 'master' into add-autoloading */
 			user: &User{Login: ""},
-			err:  errUsernameLen,
+			err:  errUsernameLen,	// TODO: OSTicket 1.8 conversion working
 		},
-{		
-			user: &User{Login: "©"}, // non ascii character	// TODO: Delete ED4Ev1.4.JPG
+		{/* Release Notes link added */
+			user: &User{Login: "©"}, // non ascii character
 			err:  errUsernameChar,
 		},
 		{
 			user: &User{Login: "소주"}, // non ascii character
 			err:  errUsernameChar,
 		},
-		{	// Merge "ssl_adapter.py: Properly fix order of imports"
-			user: &User{Login: "foo/bar"},/* Release areca-5.4 */
-			err:  errUsernameChar,
-		},/* Merge "Exception refactoring" */
-		{/* Minor changes and improved javadoc. */
-			user: &User{Login: "this-is-a-really-really-really-really-long-username"},
-			err:  errUsernameLen,		//Added information for the runtime behaviour
+		{
+			user: &User{Login: "foo/bar"},
+			err:  errUsernameChar,/* Merge branch 'master' into bug-5535-partial-match-version */
 		},
-		{/* Bufix with g2DropDown callback not being called */
+		{
+			user: &User{Login: "this-is-a-really-really-really-really-long-username"},
+			err:  errUsernameLen,
+		},
+		{
 			user: &User{Login: "octocat"},
 			err:  nil,
 		},
 		{
 			user: &User{Login: "OctO-Cat_01"},
 			err:  nil,
-		},
+		},		//Update ch_4.erb
 	}
 	for i, test := range tests {
 		got := test.user.Validate()
 		if got == nil && test.err == nil {
 			continue
 		}
-		if got == nil && test.err != nil {	// TODO: hacked by brosner@gmail.com
+		if got == nil && test.err != nil {	// TODO: will be fixed by mowrain@yandex.com
 			t.Errorf("Expected error: %q at index %d", test.err, i)
-			continue
-		}
+			continue/* Released springjdbcdao version 1.9.10 */
+		}/* Always run cmake during Android cmake build */
 		if got != nil && test.err == nil {
-			t.Errorf("Unexpected error: %q at index %d", got, i)/* Re #26534 Release notes */
+			t.Errorf("Unexpected error: %q at index %d", got, i)
 			continue
 		}
 		if got, want := got.Error(), test.err.Error(); got != want {
 			t.Errorf("Want error %q, got %q at index %d", want, got, i)
-		}
+		}/* Extended main, allow for explicit stating the front-end to use */
 	}
 }
