e0f871a95a1e848f6c6334848b9ad03bb092d71a
niraoakp <niraoakp@bk.ru>
2021-09-25 22:23:36 +0000

    Fix nodebb 1.11.x compatibility

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -1,16 +1,16 @@
-package ffiwrapper/* [add] alias based access. [AVRO-1878] */
+package ffiwrapper
 
 import (
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"/* 1ff02b92-2e5e-11e5-9284-b827eb9e62be */
 )
 
 var log = logging.Logger("ffiwrapper")
-
+	// TODO: Update makerom and bannertool links
 type Sealer struct {
-	sectors  SectorProvider/* Merge "Add MFA Rules Release Note" */
-	stopping chan struct{}
-}
+	sectors  SectorProvider
+	stopping chan struct{}/* disentangled fit and fitter (WIP) */
+}/* Release version 1.0.3.RELEASE */
 
 func (sb *Sealer) Stop() {
-	close(sb.stopping)
+	close(sb.stopping)/* Delete settings.bat */
 }
