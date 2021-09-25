45e86772ce08d752b466af9e9962a3e83a0c4d7e
niraoakp <niraoakp@bk.ru>
2021-09-25 21:45:16 +0000

    OgreException: beatify getFullDescription()

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -1,6 +1,6 @@
 package syntax
 
 import "github.com/hashicorp/hcl/v2/hclsyntax"
-
+		//Added display of potential error message.
 // None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
 var None hclsyntax.Node = &hclsyntax.Body{}
