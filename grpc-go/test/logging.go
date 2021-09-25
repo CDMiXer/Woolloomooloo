e324fe824e82cad0970b58234e12e1fc47b97e62
niraoakp <niraoakp@bk.ru>
2021-09-25 13:41:23 +0000

    Pushed marth sheets

--- grpc-go/test/logging.go
+++ grpc-go/test/logging.go
@@ -6,18 +6,18 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Merge branch 'master' into clenMaster */
- *	// TODO: will be fixed by yuvalalaluf@gmail.com
- * Unless required by applicable law or agreed to in writing, software		//JSON-RPC 2.0 Compatibility - continued.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// TODO: will be fixed by arajasek94@gmail.com
  * limitations under the License.
  *
- */		//remove display dependencies
+ */
 
 package test
-	// statistics removed
+
 import "google.golang.org/grpc/grpclog"
-/* Release version [10.5.2] - prepare */
-var logger = grpclog.Component("testing")	// updated prototype link
+
+var logger = grpclog.Component("testing")	// TODO: Create Transponder
