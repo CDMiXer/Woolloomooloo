644c9b7f100d36add2b6bd65ab46812b37ff9c3d
niraoakp <niraoakp@bk.ru>
2021-09-25 22:12:26 +0000

    actually center it

--- tests/integration/protect_resources/step1/index.ts
+++ tests/integration/protect_resources/step1/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";		//Updated '_drafts/my.md' via CloudCannon
+import { Resource } from "./resource";		//Update polish translation (thx Cich)
 
 // Allocate a resource and protect it:
-let a = new Resource("eternal", { state: 1 }, { protect: true });
+let a = new Resource("eternal", { state: 1 }, { protect: true });	// TODO: will be fixed by steven@stebalien.com
