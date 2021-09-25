adc74696334d65f7ab4a2c9958ec46cf82de44ad
niraoakp <niraoakp@bk.ru>
2021-09-25 22:43:44 +0000

    update test application to use mina 2.0.13 to fix ssl / tls issues

--- handler/api/repos/secrets/create_test.go
+++ handler/api/repos/secrets/create_test.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge branch 'master' into add-sonarqube */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* 8d690cde-2e40-11e5-9284-b827eb9e62be */
+// that can be found in the LICENSE file.
 
-// +build !oss	// added cleaning of processes and journals when disconnected
+// +build !oss
 
 package secrets
 
@@ -19,19 +19,19 @@ import (
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"		//Add support for GZip-compressed FLD files.
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestHandleCreate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Update Containers.css */
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
 	secrets := mock.NewMockSecretStore(controller)
-	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)	// caad8ede-2e55-11e5-9284-b827eb9e62be
+	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
@@ -46,7 +46,7 @@ func TestHandleCreate(t *testing.T) {
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-	// Fixed using context/ added unit test for try ... catch case.
+
 	HandleCreate(repos, secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
@@ -54,31 +54,31 @@ func TestHandleCreate(t *testing.T) {
 
 	got, want := &core.Secret{}, dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Update readme with info for troubleshooting gh-pages */
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
 
 func TestHandleCreate_ValidationError(t *testing.T) {
-	controller := gomock.NewController(t)/* Fix asyncio link */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)		//Updates to Grades
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
-	c := new(chi.Context)/* New comment by Alonzoriz */
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")	// TODO: hacked by 13860583249@yeah.net
-/* d33be77a-2e9c-11e5-a119-a45e60cdfd11 */
-	in := new(bytes.Buffer)/* last removes of unused imports */
+	c.URLParams.Add("name", "hello-world")
+
+	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(&core.Secret{Name: "", Data: "pa55word"})
 
-	w := httptest.NewRecorder()		//7e5f2db0-35c6-11e5-90d8-6c40088e03e4
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-/* Merge "Release 1.0.0.178 QCACLD WLAN Driver." */
+
 	HandleCreate(repos, nil).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusBadRequest; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
