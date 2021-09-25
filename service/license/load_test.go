b6505642486a7769365591532e258c07380b2aaf
niraoakp <niraoakp@bk.ru>
2021-09-25 11:14:01 +0000

    Update README.md license section

--- service/license/load_test.go
+++ service/license/load_test.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//releasing version 1.99-8
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Merge "add page_reverse to GET API"
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release for 18.33.0 */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//comment out Test Event methods
-// See the License for the specific language governing permissions and/* Remark on -daemon option and development */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* Merge "Use byte string or utf8 depending on python version for wsgi" */
 // limitations under the License.
-
+/* Merge "Release bdm constraint source and dest type" */
 // +build !nolimit
 // +build !oss
-
+		//Implement missing cases in require_exe_file and other minor fixes
 package license
