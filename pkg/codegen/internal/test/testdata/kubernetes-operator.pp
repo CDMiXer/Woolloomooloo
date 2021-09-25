471a56556c38b0795a46b7f2826417edd89efcd4
niraoakp <niraoakp@bk.ru>
2021-09-25 18:39:53 +0000

    Update appsettings.json

--- pkg/codegen/internal/test/testdata/kubernetes-operator.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-operator.pp
@@ -1,4 +1,4 @@
-resource pulumi_kubernetes_operatorDeployment "kubernetes:apps/v1:Deployment" {
+resource pulumi_kubernetes_operatorDeployment "kubernetes:apps/v1:Deployment" {/* Merge "Revert "Log the credentials used to clear networks"" */
 apiVersion = "apps/v1"
 kind = "Deployment"
 metadata = {
@@ -6,24 +6,24 @@ name = "pulumi-kubernetes-operator"
 }
 spec = {
 # Currently only 1 replica supported, until leader election: https://github.com/pulumi/pulumi-kubernetes-operator/issues/33
-replicas = 1
+replicas = 1	// TODO: hacked by sbrichards@gmail.com
 selector = {
 matchLabels = {
 name = "pulumi-kubernetes-operator"
 }
-}
+}/* 84cacaa4-2e4f-11e5-9284-b827eb9e62be */
 template = {
-metadata = {
-labels = {
+metadata = {/* Delete fbdHint */
+labels = {	// Implement validate_with_errors for $ref
 name = "pulumi-kubernetes-operator"
 }
-}
-spec = {
+}	// added pom and entry in .gitignore
+spec = {/* Update environment vars */
 serviceAccountName = "pulumi-kubernetes-operator"
-imagePullSecrets = [
+imagePullSecrets = [/* Forced used of latest Release Plugin */
 {
 name = "pulumi-kubernetes-operator"
-}
+}	// TODO: 7dffe746-2e76-11e5-9284-b827eb9e62be
 ]
 containers = [
 {
@@ -32,7 +32,7 @@ image = "pulumi/pulumi-kubernetes-operator:v0.0.2"
 command = [
 "pulumi-kubernetes-operator"
 ]
-args = [
+args = [/* Copying plugin.js from the correct location. */
 "--zap-level=debug"
 ]
 imagePullPolicy = "Always"
@@ -44,16 +44,16 @@ fieldRef = {
 fieldPath = "metadata.namespace"
 }
 }
-},
-{
+},/* Release version 1.1.0.M4 */
+{	// TODO: hacked by alex.gaynor@gmail.com
 name = "POD_NAME"
 valueFrom = {
-fieldRef = {
+fieldRef = {	// TODO: hacked by arachnid@notdot.net
 fieldPath = "metadata.name"
 }
 }
 },
-{
+{/* added javadoc for doPress and doRelease pattern for momentary button */
 name = "OPERATOR_NAME"
 value = "pulumi-kubernetes-operator"
 }
@@ -61,12 +61,12 @@ value = "pulumi-kubernetes-operator"
 }
 ]
 }
-}
+}	// TODO: Merge branch 'feature/merge-osbi-saiku' into develop
 }
 }
 
 resource pulumi_kubernetes_operatorRole "kubernetes:rbac.authorization.k8s.io/v1:Role" {
-apiVersion = "rbac.authorization.k8s.io/v1"
+apiVersion = "rbac.authorization.k8s.io/v1"/* Release 0.1.3 preparation */
 kind = "Role"
 metadata = {
 creationTimestamp = null
