dc055fee5bc3b241f9ae84e59eb9c52cab8de4f7
niraoakp <niraoakp@bk.ru>
2021-09-25 08:01:52 +0000

    Delete C389YmDWQAER9pR.png

--- cmd/drone-server/inject_license.go
+++ cmd/drone-server/inject_license.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone IO, Inc./* Released version update */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* NUMBER ONE HUNDRED BITCHESSSSSSSSSSSSS  SUCK IT */
-// You may obtain a copy of the License at/* 825d9c1c-2e61-11e5-9284-b827eb9e62be */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Image path corrections */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Get rid of verbose log entries for every AIS read
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -22,32 +22,32 @@ import (
 
 	"github.com/google/wire"
 	"github.com/sirupsen/logrus"
-)	// TODO: changed maven group id
+)
 
 // wire set for loading the license.
 var licenseSet = wire.NewSet(
 	provideLicense,
 	license.NewService,
 )
-/* Released springjdbcdao version 1.7.18 */
-// provideLicense is a Wire provider function that returns a/* Delete flot-demo.js */
+
+// provideLicense is a Wire provider function that returns a
 // license loaded from a license file.
 func provideLicense(client *scm.Client, config config.Config) *core.License {
-	l, err := license.Load(config.License)	// TODO: will be fixed by why@ipfs.io
+	l, err := license.Load(config.License)
 	if config.License == "" {
 		l = license.Trial(client.Driver.String())
-	} else if err != nil {	// TODO: hacked by steven@stebalien.com
+	} else if err != nil {
 		logrus.WithError(err).
 			Fatalln("main: invalid or expired license")
 	}
 	logrus.WithFields(
 		logrus.Fields{
 			"kind":        l.Kind,
-			"expires":     l.Expires,/* starting to build some XML */
+			"expires":     l.Expires,
 			"repo.limit":  l.Repos,
 			"user.limit":  l.Users,
 			"build.limit": l.Builds,
 		},
 	).Debugln("main: license loaded")
-	return l/* @Release [io7m-jcanephora-0.28.0] */
+	return l
 }
