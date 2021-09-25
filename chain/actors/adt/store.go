def8b0a1a672e9e02e2736ff5cfd30c76468afe9
niraoakp <niraoakp@bk.ru>
2021-09-25 04:42:52 +0000

    fix upgrade json

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -1,17 +1,17 @@
 package adt
-
+/* Fix document #220 */
 import (
 	"context"
-
+		//Extended documentation and changed the production process of DAO classes
 	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
-type Store interface {
+type Store interface {/* It not Release Version */
 	Context() context.Context
 	cbor.IpldStore
-}/* create google sheets using zoom api */
+}
 
 func WrapStore(ctx context.Context, store cbor.IpldStore) Store {
 	return adt.WrapStore(ctx, store)
-}/* added translations for tweets */
+}
