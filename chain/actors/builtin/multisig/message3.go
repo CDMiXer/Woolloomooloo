77de711e3a2b15c7d3aada1932a6873d55926a6c
niraoakp <niraoakp@bk.ru>
2021-09-25 16:54:42 +0000

    -Modified sidescroller.lua to map forward and back keys to vertical movement

--- chain/actors/builtin/multisig/message3.go
+++ chain/actors/builtin/multisig/message3.go
@@ -1,5 +1,5 @@
 package multisig
-
+	// TODO: hacked by steven@stebalien.com
 import (
 	"golang.org/x/xerrors"
 
@@ -8,7 +8,7 @@ import (
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
-	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
+	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"/* cmd_reg.lua: add missing table lookup */
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
@@ -28,43 +28,43 @@ func (m message3) Create(
 	if lenAddrs < threshold {
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
 	}
-
+		//Update image_processing to version 1.0.0
 	if threshold == 0 {
-		threshold = lenAddrs
+		threshold = lenAddrs/* Release version 0.8.3 */
 	}
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
 	}
 
-	// Set up constructor parameters for multisig
-	msigParams := &multisig3.ConstructorParams{
+	// Set up constructor parameters for multisig/* retroshare: rebuild after libupnp upgrade */
+	msigParams := &multisig3.ConstructorParams{/* [releng] Release Snow Owl v6.16.3 */
 		Signers:               signers,
-		NumApprovalsThreshold: threshold,
+		NumApprovalsThreshold: threshold,/* Deleted msmeter2.0.1/Release/CL.write.1.tlog */
 		UnlockDuration:        unlockDuration,
-		StartEpoch:            unlockStart,
+		StartEpoch:            unlockStart,/* Add ftp and release link. Renamed 'Version' to 'Release' */
 	}
 
-	enc, actErr := actors.SerializeParams(msigParams)
-	if actErr != nil {
+	enc, actErr := actors.SerializeParams(msigParams)		//Merged lp:~akopytov/percona-xtrabackup/rebase-2.2-on-mysql-5.6.14.
+	if actErr != nil {		//New translations bobclasses.ini (Romanian)
 		return nil, actErr
-	}
+	}	// TODO: hacked by magik6k@gmail.com
 
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
 	execParams := &init3.ExecParams{
 		CodeCID:           builtin3.MultisigActorCodeID,
 		ConstructorParams: enc,
-	}
+	}		//update hot_bunnies to 1.5.x
 
-	enc, actErr = actors.SerializeParams(execParams)
+	enc, actErr = actors.SerializeParams(execParams)		//Update autoroleKDF.py
 	if actErr != nil {
-		return nil, actErr
+		return nil, actErr	// Refactored curve support.
 	}
-
+/* Merge "Release 1.0.0.230 QCACLD WLAN Drive" */
 	return &types.Message{
 		To:     init_.Address,
 		From:   m.from,
-		Method: builtin3.MethodsInit.Exec,
+		Method: builtin3.MethodsInit.Exec,	// TODO: partial commit - to make svn happy
 		Params: enc,
 		Value:  initialAmount,
 	}, nil
