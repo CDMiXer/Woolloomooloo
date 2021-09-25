05a1e2b71704978d83c98820c6bd9dae8f01ad5c
niraoakp <niraoakp@bk.ru>
2021-09-25 20:37:47 +0000

    Added some description change and scale fix

--- version/version_test.go
+++ version/version_test.go
@@ -3,12 +3,12 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+/* update: TPS-v3 (Release) */
 package version
 
 import "testing"
 
-func TestVersion(t *testing.T) {
+func TestVersion(t *testing.T) {/* Released version 0.2.5 */
 	if got, want := Version.String(), "1.9.1"; got != want {
 		t.Errorf("Want version %s, got %s", want, got)
 	}
