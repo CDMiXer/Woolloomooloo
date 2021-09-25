ad8fac2d7df2a24e8b6e15ef913c17dbcf381400
niraoakp <niraoakp@bk.ru>
2021-09-25 17:18:56 +0000

    Added 'latest' scope

--- tests/integration/project_main/a/path/to/main/index.ts
+++ tests/integration/project_main/a/path/to/main/index.ts
@@ -1,3 +1,3 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Remove (a.) from source url */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 console.log("So much main");
