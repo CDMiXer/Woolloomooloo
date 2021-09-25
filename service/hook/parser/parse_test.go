724ab5cdc48469824b9fc14e0fedfa658086c352
niraoakp <niraoakp@bk.ru>
2021-09-25 20:35:49 +0000

    Rename respond.min to respond.min.js

--- service/hook/parser/parse_test.go
+++ service/hook/parser/parse_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Increase version for the functionality which supports 8.x */
+// that can be found in the LICENSE file.
 
 package parser
