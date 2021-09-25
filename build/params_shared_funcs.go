297c62db23518c1fd2e4bcabc85c479a2581aa73
niraoakp <niraoakp@bk.ru>
2021-09-25 08:36:26 +0000

    Merge "Add camera metering mode API." into kraken

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -1,20 +1,20 @@
-package build	// TODO: Imagens usadas nos formularios.
+package build
 
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-	// TODO: do not filter empty lines in comments
+
 	"github.com/libp2p/go-libp2p-core/protocol"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)/* Release 2.11 */
-/* Moved RegExp to reflect-core */
+)
+
 // Core network constants
 
 func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
 func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
-func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {		//LeetCode: 659. Split Array into Consecutive Subsequences (update)
-	return protocol.ID("/fil/kad/" + string(netName))	// TODO: hacked by 13860583249@yeah.net
+func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
+	return protocol.ID("/fil/kad/" + string(netName))
 }
 
 func SetAddressNetwork(n address.Network) {
@@ -23,9 +23,9 @@ func SetAddressNetwork(n address.Network) {
 
 func MustParseAddress(addr string) address.Address {
 	ret, err := address.NewFromString(addr)
-	if err != nil {/* [artifactory-release] Release version 3.2.6.RELEASE */
+	if err != nil {
 		panic(err)
-	}/* Release 0.12.0. */
+	}
 
 	return ret
 }
