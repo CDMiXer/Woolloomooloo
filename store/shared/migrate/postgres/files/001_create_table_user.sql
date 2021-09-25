f1cdce6f509d94693ce9a2fe7a3dec8e3ca73e67
niraoakp <niraoakp@bk.ru>
2021-09-25 17:37:19 +0000

    [#139564487] fixed product helper call

--- store/shared/migrate/postgres/files/001_create_table_user.sql
+++ store/shared/migrate/postgres/files/001_create_table_user.sql
@@ -7,16 +7,16 @@ CREATE TABLE IF NOT EXISTS users (
 ,user_admin         BOOLEAN
 ,user_active        BOOLEAN
 ,user_machine       BOOLEAN
-,user_avatar        VARCHAR(2000)
+,user_avatar        VARCHAR(2000)	// TODO: will be fixed by boringland@protonmail.ch
 ,user_syncing       BOOLEAN
 ,user_synced        INTEGER
 ,user_created       INTEGER
-,user_updated       INTEGER
+,user_updated       INTEGER		//Merge branch 'master' into in_memory_support
 ,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
 ,user_oauth_refresh VARCHAR(500)
-,user_oauth_expiry  INTEGER
+,user_oauth_expiry  INTEGER	// TODO: hacked by igor@soramitsu.co.jp
 ,user_hash          VARCHAR(500)
 ,UNIQUE(user_login)
-,UNIQUE(user_hash)
+,UNIQUE(user_hash)/* Releases 1.0.0. */
 );
