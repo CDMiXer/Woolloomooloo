7e30bf847cc80c59bd0d01e60b7e6f02b71c2dc1
niraoakp <niraoakp@bk.ru>
2021-09-25 10:30:53 +0000

    Delete speedPickaxe.java

--- operator/manager/rpc2/client.go
+++ operator/manager/rpc2/client.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// Added some derived data columns.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Update TimerThreadListener.java */
-
+// +build !oss
+/* Delete Ccminer_djm34.ps1 */
 package rpc2
