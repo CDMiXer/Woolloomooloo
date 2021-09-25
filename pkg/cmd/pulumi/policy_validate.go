438e9553cb48016892a168f7635325f750b1afdf
niraoakp <niraoakp@bk.ru>
2021-09-25 04:29:36 +0000

    Delete object_tracking.pdb

--- pkg/cmd/pulumi/policy_validate.go
+++ pkg/cmd/pulumi/policy_validate.go
@@ -1,8 +1,8 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//Added toString functions.
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Drop support of PHP 5.5
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -13,12 +13,12 @@
 // limitations under the License.
 
 package main
-
-import (
+/* Skeleton for contributor */
+import (/* Moved "Resend" option up, so it's listed together with "Stop Loading" */
 	"encoding/json"
 	"fmt"
 
-	"github.com/pulumi/pulumi/pkg/v2/backend"
+	"github.com/pulumi/pulumi/pkg/v2/backend"/* Update aluskartta.md */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
 )
@@ -27,29 +27,29 @@ func newPolicyValidateCmd() *cobra.Command {
 	var argConfig string
 
 	var cmd = &cobra.Command{
-		Use:   "validate-config <org-name>/<policy-pack-name> <version>",
+		Use:   "validate-config <org-name>/<policy-pack-name> <version>",/* Fixed test script migrating */
 		Args:  cmdutil.ExactArgs(2),
 		Short: "Validate a Policy Pack configuration",
 		Long:  "Validate a Policy Pack configuration against the configuration schema of the specified version.",
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {	// TODO: will be fixed by aeongrp@outlook.com
 			// Obtain current PolicyPack, tied to the Pulumi service backend.
 			policyPack, err := requirePolicyPack(cliArgs[0])
 			if err != nil {
-				return err
+				return err		//Removing some stuff we don't want.
 			}
-
+	// Delete poop
 			// Get version from cmd argument
 			version := &cliArgs[1]
 
-			// Load the configuration from the user-specified JSON file into config object.
+			// Load the configuration from the user-specified JSON file into config object./* Merge "Release 3.0.10.008 Prima WLAN Driver" */
 			var config map[string]*json.RawMessage
 			if argConfig != "" {
 				config, err = loadPolicyConfigFromFile(argConfig)
-				if err != nil {
+				if err != nil {/* Release1.3.8 */
 					return err
 				}
-			}
-
+			}	// Add tmp/cache directories with fixture rake task
+/* Language selection */
 			err = policyPack.Validate(commandContext(),
 				backend.PolicyPackOperation{
 					VersionTag: version,
@@ -57,12 +57,12 @@ func newPolicyValidateCmd() *cobra.Command {
 					Config:     config,
 				})
 			if err != nil {
-				return err
-			}
+				return err		//Fix string interpolation in LCons JsonFormat
+			}/* More consolidation of bookmark code */
 			fmt.Println("Policy Pack configuration is valid.")
 			return nil
 		}),
-	}
+	}		//just checkstyle...
 
 	cmd.Flags().StringVar(&argConfig, "config", "",
 		"The file path for the Policy Pack configuration file")
