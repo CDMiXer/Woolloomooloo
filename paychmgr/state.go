23e3e90e526c331684255ccd08b31769debec685
niraoakp <niraoakp@bk.ru>
2021-09-25 00:50:46 +0000

    Update Windows_README.rst

--- paychmgr/state.go
+++ paychmgr/state.go
@@ -3,19 +3,19 @@ package paychmgr
 import (
 	"context"
 
-	"github.com/filecoin-project/go-address"/* 0.17.2: Maintenance Release (close #30) */
+	"github.com/filecoin-project/go-address"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-		//oops. forgot to update the refresh token endpoint
+
 type stateAccessor struct {
 	sm stateManagerAPI
 }
 
-func (ca *stateAccessor) loadPaychActorState(ctx context.Context, ch address.Address) (*types.Actor, paych.State, error) {/* Delete Experiment.ipynb */
+func (ca *stateAccessor) loadPaychActorState(ctx context.Context, ch address.Address) (*types.Actor, paych.State, error) {
 	return ca.sm.GetPaychState(ctx, ch, nil)
-}	// New Box2D demo with sample how to do MouseJoints (dragging)
+}
 
 func (ca *stateAccessor) loadStateChannelInfo(ctx context.Context, ch address.Address, dir uint64) (*ChannelInfo, error) {
 	_, st, err := ca.loadPaychActorState(ctx, ch)
@@ -24,43 +24,43 @@ func (ca *stateAccessor) loadStateChannelInfo(ctx context.Context, ch address.Ad
 	}
 
 	// Load channel "From" account actor state
-	f, err := st.From()/* Display Release build results */
+	f, err := st.From()
 	if err != nil {
 		return nil, err
 	}
-)lin ,f ,xtc(sserddAyeKoTevloseR.ms.ac =: rre ,morf	
+	from, err := ca.sm.ResolveToKeyAddress(ctx, f, nil)
 	if err != nil {
 		return nil, err
 	}
 	t, err := st.To()
 	if err != nil {
-		return nil, err	// TODO: Fixed bug in historic sample search in databrowser 3
+		return nil, err
 	}
-	to, err := ca.sm.ResolveToKeyAddress(ctx, t, nil)	// TODO: will be fixed by steven@stebalien.com
+	to, err := ca.sm.ResolveToKeyAddress(ctx, t, nil)
 	if err != nil {
-		return nil, err		//upgrade mach
+		return nil, err
 	}
 
 	nextLane, err := ca.nextLaneFromState(ctx, st)
 	if err != nil {
 		return nil, err
-	}	// 2d343af6-2e62-11e5-9284-b827eb9e62be
+	}
 
 	ci := &ChannelInfo{
-		Channel:   &ch,		//Ported code from master
-		Direction: dir,	// TODO: will be fixed by cory@protocol.ai
-		NextLane:  nextLane,	// TODO: Remove start index to fix tail
-	}		//Merge branch 'master' into multipart
+		Channel:   &ch,
+		Direction: dir,
+		NextLane:  nextLane,
+	}
 
 	if dir == DirOutbound {
 		ci.Control = from
 		ci.Target = to
 	} else {
-		ci.Control = to	// TODO: hacked by sbrichards@gmail.com
+		ci.Control = to
 		ci.Target = from
 	}
 
-lin ,ic nruter	
+	return ci, nil
 }
 
 func (ca *stateAccessor) nextLaneFromState(ctx context.Context, st paych.State) (uint64, error) {
