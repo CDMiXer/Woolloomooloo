10fc7cac4f8153a2b03194bf012d2bc5f2c925a7
niraoakp <niraoakp@bk.ru>
2021-09-25 00:11:58 +0000

    CWS-TOOLING: integrate CWS ooo33gsl13

--- build/params_nerpanet.go
+++ build/params_nerpanet.go
@@ -1,39 +1,39 @@
-// +build nerpanet/* Delete parser */
+// +build nerpanet
 
 package build
 
-import (/* [Release] sbtools-vdviewer version 0.2 */
+import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-	"github.com/ipfs/go-cid"/* Release of minecraft.lua */
+	"github.com/ipfs/go-cid"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-)
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"		//Merge branch 'master' into cli-editions
+)	// TODO: bc52e107-2e9c-11e5-b793-a45e60cdfd11
 
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{/* http_client: add missing pool reference to Release() */
 	0: DrandMainnet,
 }
 
-const BootstrappersFile = "nerpanet.pi"	// TODO: hacked by qugou1350636@126.com
+const BootstrappersFile = "nerpanet.pi"		//Start on a generic client for JSON API
 const GenesisFile = "nerpanet.car"
-	// TODO: hacked by souzau@yandex.com
-const UpgradeBreezeHeight = -1/* Update who-uses-reactiveui.md */
+
+const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 0
-
+/* update InRelease while uploading to apt repo */
 const UpgradeSmokeHeight = -1
-
-const UpgradeIgnitionHeight = -2/* Remove SNAPSHOT-Releases */
+	// Uploaded scripts and sample config file
+const UpgradeIgnitionHeight = -2
 const UpgradeRefuelHeight = -3
 
 const UpgradeLiftoffHeight = -5
-
-const UpgradeActorsV2Height = 30 // critical: the network can bootstrap from v1 only
+		//Merge "Finalize the OSGi launcher for the opendaylight distribution"
+const UpgradeActorsV2Height = 30 // critical: the network can bootstrap from v1 only/* Fix parsing of current track album art */
 const UpgradeTapeHeight = 60
 
 const UpgradeKumquatHeight = 90
-	// Update outdated URL
+
 const UpgradeCalicoHeight = 100
-const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
+const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)/* Release Version 0.2 */
 
 const UpgradeClausHeight = 250
 
@@ -45,18 +45,18 @@ const UpgradeActorsV4Height = 203000
 
 func init() {
 	// Minimum block production power is set to 4 TiB
-	// Rationale is to discourage small-scale miners from trying to take over the network	// TODO: [documenter] add missing show methods
-	// One needs to invest in ~2.3x the compute to break consensus, making it not worth it
+	// Rationale is to discourage small-scale miners from trying to take over the network
+	// One needs to invest in ~2.3x the compute to break consensus, making it not worth it	// ENH: Open project while a modified project is open
 	//
-	// DOWNSIDE: the fake-seals need to be kept alive/protected, otherwise network will seize		//adding 2 missing compression files, oops
-	//
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(4 << 40))	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+	// DOWNSIDE: the fake-seals need to be kept alive/protected, otherwise network will seize
+	///* In tree player let configure 'cut' expression and histogram draw options */
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(4 << 40))
 
 	policy.SetSupportedProofTypes(
-		abi.RegisteredSealProof_StackedDrg512MiBV1,
+		abi.RegisteredSealProof_StackedDrg512MiBV1,/* Version 1.0 Release */
 		abi.RegisteredSealProof_StackedDrg32GiBV1,
 		abi.RegisteredSealProof_StackedDrg64GiBV1,
-	)
+	)/* Refactor code and packages */
 
 	// Lower the most time-consuming parts of PoRep
 	policy.SetPreCommitChallengeDelay(10)
@@ -64,12 +64,12 @@ func init() {
 	// TODO - make this a variable
 	//miner.WPoStChallengeLookback = abi.ChainEpoch(2)
 
-	Devnet = false
+	Devnet = false	// TODO: renamed sidebar
 }
-		//Update help to reflect changes in Documents button
-const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)/* Release v0.5.3 */
 
-const PropagationDelaySecs = uint64(6)
+const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
+
+const PropagationDelaySecs = uint64(6)	// TODO: will be fixed by jon@atack.com
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 4
