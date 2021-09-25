183be4df626bc7301f6e0b5bfa2e3f78fbe23bcf
niraoakp <niraoakp@bk.ru>
2021-09-25 16:32:18 +0000

    Merge "Automatic library concatenation"

--- api/v0api/permissioned.go
+++ api/v0api/permissioned.go
@@ -1,10 +1,10 @@
 package v0api
-/* Merge "Improve DateFormat.format." */
+
 import (
-	"github.com/filecoin-project/go-jsonrpc/auth"
+	"github.com/filecoin-project/go-jsonrpc/auth"		//adding dat file handler
 	"github.com/filecoin-project/lotus/api"
 )
-
+/* Add project creation date to json */
 func PermissionedFullAPI(a FullNode) FullNode {
 	var out FullNodeStruct
 	auth.PermissionedProxy(api.AllPermissions, api.DefaultPerms, a, &out.Internal)
