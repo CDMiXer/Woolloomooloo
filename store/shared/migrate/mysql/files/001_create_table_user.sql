6136637591bd571b30ef657c4958e182ec31198d
niraoakp <niraoakp@bk.ru>
2021-09-25 10:06:38 +0000

    CSS at 300px width refinements

--- store/shared/migrate/mysql/files/001_create_table_user.sql
+++ store/shared/migrate/mysql/files/001_create_table_user.sql
@@ -7,16 +7,16 @@ CREATE TABLE IF NOT EXISTS users (
 ,user_admin         BOOLEAN
 ,user_machine       BOOLEAN
 ,user_active        BOOLEAN
-,user_avatar        VARCHAR(2000)	// TODO: hacked by alex.gaynor@gmail.com
-,user_syncing       BOOLEAN
+,user_avatar        VARCHAR(2000)
+,user_syncing       BOOLEAN	// TODO: will be fixed by zaq1tomo@gmail.com
 ,user_synced        INTEGER
 ,user_created       INTEGER
 ,user_updated       INTEGER
 ,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
-,user_oauth_refresh VARCHAR(500)	// TODO: Delete simpleTest
+,user_oauth_refresh VARCHAR(500)
 ,user_oauth_expiry  INTEGER
 ,user_hash          VARCHAR(500)
-,UNIQUE(user_login)/* Release tables after query exit */
+,UNIQUE(user_login)/* Release: Making ready to release 6.0.1 */
 ,UNIQUE(user_hash)
 );
