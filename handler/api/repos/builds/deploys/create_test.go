606ae1e75c212a34a9b57bbfe9f8a9455a157da7
niraoakp <niraoakp@bk.ru>
2021-09-25 03:35:41 +0000

    Adding statistics translations for other objects

--- handler/api/repos/builds/deploys/create_test.go
+++ handler/api/repos/builds/deploys/create_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Add controller generator spec
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: ADD: added suport of captcha in DSS
-	// modify druid configuration
+// that can be found in the LICENSE file.
+
 package deploys
