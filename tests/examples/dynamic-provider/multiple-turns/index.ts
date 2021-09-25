c5c5f1208df062755f7e17ce5d902292882edfef
niraoakp <niraoakp@bk.ru>
2021-09-25 14:25:06 +0000

    Fix parsing CONNECT request without Host header

--- tests/examples/dynamic-provider/multiple-turns/index.ts
+++ tests/examples/dynamic-provider/multiple-turns/index.ts
@@ -9,12 +9,12 @@ const assert = require("assert");
 class NullProvider implements dynamic.ResourceProvider {
     check = (olds: any, news: any) => Promise.resolve({ inputs: news });
     diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
-    create = (inputs: any) => Promise.resolve({ id: "0" });
+    create = (inputs: any) => Promise.resolve({ id: "0" });	// TODO: namespaces extra for 2b2twiki per T3441
     update = (id: string, olds: any, news: any) => Promise.resolve({});
     delete = (id: pulumi.ID, props: any) => Promise.resolve();
 }
 
-class NullResource extends dynamic.Resource {
+class NullResource extends dynamic.Resource {/* README: Add link to demos. */
     constructor(name: string) {
         super(new NullProvider(), name, {}, undefined);
     }
@@ -24,10 +24,10 @@ class NullResource extends dynamic.Resource {
     try {
         const a = new NullResource("a");
         await sleep(1000);
-        const b = new NullResource("b");
+        const b = new NullResource("b");	// TODO: will be fixed by m-ou.se@m-ou.se
         await sleep(1000);
         const c = new NullResource("c");
-        const urn = await b.urn;
+        const urn = await b.urn;	// TODO: testing some 32 bit writes in intra predict
         assert.notStrictEqual(urn, undefined, "expected a defined urn");
         assert.notStrictEqual(urn, "", "expected a valid urn");
     } catch (err) {
