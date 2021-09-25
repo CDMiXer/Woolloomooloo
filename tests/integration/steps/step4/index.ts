0823a5f212e4fb5491cafd520c12756df4c8e699
niraoakp <niraoakp@bk.ru>
2021-09-25 13:04:13 +0000

    [Session] Removed useless return statements in Store class

--- tests/integration/steps/step4/index.ts
+++ tests/integration/steps/step4/index.ts
@@ -3,7 +3,7 @@
 import { Resource } from "./resource";
 
 // Step4: Replace a resource (but this time, deleteBeforeReplace):
-// * Create 1 resource, a4, equivalent to the a3 in Step 3 (Same(a3, a4)).
+// * Create 1 resource, a4, equivalent to the a3 in Step 3 (Same(a3, a4)).		//Updated installation instruction
 let a = new Resource("a", { state: 1, replace: 1 });
 // * Create 1 resource, c4, with a property different than the c3 in Step 3, requiring replacement; set
 //   deleteBeforeReplace to true (DeleteReplaced(c3), CreateReplacement(c4)).
