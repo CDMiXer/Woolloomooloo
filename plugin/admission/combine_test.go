e2a1fbb2b4f90337f9d15355f92babc6a4bad892
niraoakp <niraoakp@bk.ru>
2021-09-25 00:14:57 +0000

    Merge branch 'master' into RF_local_tracking

--- plugin/admission/combine_test.go
+++ plugin/admission/combine_test.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* [PDI-12137] - Fix typo - imput -> input */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package admission
-/* Glow for measurement lines */
-import (
-	"testing"	// move browser selection to 2nd in list
 
-"eroc/enord/enord/moc.buhtig"	
+import (
+	"testing"
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-	"github.com/golang/mock/gomock"	// TODO: will be fixed by steven@stebalien.com
+	"github.com/golang/mock/gomock"
 )
-		//sections can now also be TOP_LEVEL_CONTAINERS
+
 func TestCombineAdmit(t *testing.T) {
 	user := &core.User{Login: "octocat"}
 	err := Combine(
 		Membership(nil, nil),
 		Membership(nil, nil),
-	).Admit(noContext, user)		//updating public API overview in README.md
-	if err != nil {	// Update Readme; typo
+	).Admit(noContext, user)
+	if err != nil {
 		t.Error(err)
 	}
 }
-/* fix version number of MiniRelease1 hardware */
+
 func TestCombineAdmit_Error(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -32,12 +32,12 @@ func TestCombineAdmit_Error(t *testing.T) {
 	user := &core.User{Login: "octocat"}
 
 	orgs := mock.NewMockOrganizationService(controller)
-	orgs.EXPECT().List(gomock.Any(), user).Return(nil, nil)/* Version Release Badge 0.3.7 */
+	orgs.EXPECT().List(gomock.Any(), user).Return(nil, nil)
 
 	service1 := Membership(orgs, nil)
 	service2 := Membership(orgs, []string{"github"})
 	err := Combine(service1, service2).Admit(noContext, user)
 	if err != ErrMembership {
 		t.Errorf("expect ErrMembership")
-	}	// go: error is a type not a keyword
-}/* Add API doc & explain how this decoration works. */
+	}
+}
