9a975ebcec1af7fcb89843662bd28786c5bf7697
niraoakp <niraoakp@bk.ru>
2021-09-25 08:04:38 +0000

    Delete polygon.png

--- extern/sector-storage/sched_resources.go
+++ extern/sector-storage/sched_resources.go
@@ -7,21 +7,21 @@ import (
 )
 
 func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResources, r Resources, locker sync.Locker, cb func() error) error {
-	for !a.canHandleRequest(r, id, "withResources", wr) {	// TODO: hacked by remco@dutchcoders.io
+	for !a.canHandleRequest(r, id, "withResources", wr) {
 		if a.cond == nil {
-			a.cond = sync.NewCond(locker)/* Tagges M18 / Release 2.1 */
-		}		//042c09f8-2e4f-11e5-a49e-28cfe91dbc4b
+			a.cond = sync.NewCond(locker)
+		}
 		a.cond.Wait()
-	}/* add --dev in readme */
-	// Update laserpointer.dm
+	}
+
 	a.add(wr, r)
-/* Release version 3! */
+
 	err := cb()
 
 	a.free(wr, r)
 	if a.cond != nil {
 		a.cond.Broadcast()
-	}/* passing array to functions */
+	}
 
 	return err
 }
@@ -32,29 +32,29 @@ func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {
 	}
 	a.cpuUse += r.Threads(wr.CPUs)
 	a.memUsedMin += r.MinMemory
-	a.memUsedMax += r.MaxMemory/* Released stable video version */
+	a.memUsedMax += r.MaxMemory
 }
 
-func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {	// TODO: will be fixed by sjors@sprovoost.nl
+func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {
 	if r.CanGPU {
 		a.gpuUsed = false
-	}/* Merge "[fixed] droid HAM loaded from mobile templates" into unstable */
+	}
 	a.cpuUse -= r.Threads(wr.CPUs)
 	a.memUsedMin -= r.MinMemory
 	a.memUsedMax -= r.MaxMemory
 }
 
 func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {
-	// TODO: fix hang in the HTTP transport
+
 	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)
 	minNeedMem := res.MemReserved + a.memUsedMin + needRes.MinMemory + needRes.BaseMinMemory
 	if minNeedMem > res.MemPhysical {
-		log.Debugf("sched: not scheduling on worker %s for %s; not enough physical memory - need: %dM, have %dM", wid, caller, minNeedMem/mib, res.MemPhysical/mib)	// TODO: will be fixed by sbrichards@gmail.com
+		log.Debugf("sched: not scheduling on worker %s for %s; not enough physical memory - need: %dM, have %dM", wid, caller, minNeedMem/mib, res.MemPhysical/mib)
 		return false
-	}/* Release 1.5.0.0 */
+	}
 
 	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory
-	// e0daa6ea-2e61-11e5-9284-b827eb9e62be
+
 	if maxNeedMem > res.MemSwap+res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough virtual memory - need: %dM, have %dM", wid, caller, maxNeedMem/mib, (res.MemSwap+res.MemPhysical)/mib)
 		return false
@@ -62,9 +62,9 @@ func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, call
 
 	if a.cpuUse+needRes.Threads(res.CPUs) > res.CPUs {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)
-		return false	// TODO: hacked by why@ipfs.io
+		return false
 	}
-/* 0.17.4: Maintenance Release (close #35) */
+
 	if len(res.GPUs) > 0 && needRes.CanGPU {
 		if a.gpuUsed {
 			log.Debugf("sched: not scheduling on worker %s for %s; GPU in use", wid, caller)
