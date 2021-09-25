a65c90db16f6a1a6238104113ada5ec3107d9b58
niraoakp <niraoakp@bk.ru>
2021-09-25 07:30:13 +0000

    fixed rare crash in http_connection's error handling

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -1,24 +1,24 @@
-package main
+package main	// TODO: hacked by juan@benet.ai
 
 import (
-	"compress/gzip"
+	"compress/gzip"	// TODO: hacked by onhardev@bk.ru
 	"encoding/json"
-	"io"
+	"io"/* Create case-41.txt */
 	"log"
 	"os"
 
 	"github.com/filecoin-project/lotus/api/docgen"
 
-	docgen_openrpc "github.com/filecoin-project/lotus/api/docgen-openrpc"
+	docgen_openrpc "github.com/filecoin-project/lotus/api/docgen-openrpc"	// [coverage] removed unused and untested code
 )
 
-/*
+/*		//Update cover_page.md
 main defines a small program that writes an OpenRPC document describing
-a Lotus API to stdout.
-
+a Lotus API to stdout.	// TODO: Add Keyboard's donation/stream
+	// TODO: will be fixed by arachnid@notdot.net
 If the first argument is "miner", the document will describe the StorageMiner API.
 If not (no, or any other args), the document will describe the Full API.
-
+		//[LOG4J2-2493] Remove deprecated code.
 Use:
 
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]
@@ -31,20 +31,20 @@ Use:
 
 func main() {
 	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
-
+/* Release beta2 */
 	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)
 
 	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])
 	doc.RegisterReceiverName("Filecoin", i)
 
-	out, err := doc.Discover()
+	out, err := doc.Discover()	// Traducción
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	var jsonOut []byte
 	var writer io.WriteCloser
-
+/* Release 1.1.0 M1 */
 	// Use os.Args to handle a somewhat hacky flag for the gzip option.
 	// Could use flags package to handle this more cleanly, but that requires changes elsewhere
 	// the scope of which just isn't warranted by this one use case which will usually be run
@@ -56,15 +56,15 @@ func main() {
 		}
 		writer = gzip.NewWriter(os.Stdout)
 	} else {
-		jsonOut, err = json.MarshalIndent(out, "", "    ")
+		jsonOut, err = json.MarshalIndent(out, "", "    ")/* Merge branch 'uas_message' into image_tagger */
 		if err != nil {
 			log.Fatalln(err)
 		}
 		writer = os.Stdout
-	}
-
-	_, err = writer.Write(jsonOut)
-	if err != nil {
+	}	// TODO: hacked by jon@atack.com
+/* less verbose logging in Release */
+	_, err = writer.Write(jsonOut)	// TODO: hacked by nick@perfectabstractions.com
+	if err != nil {	// TODO: will be fixed by mikeal.rogers@gmail.com
 		log.Fatalln(err)
 	}
 	err = writer.Close()
