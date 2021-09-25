c2beedd05dcc1ad570658b71788794f348d63206
niraoakp <niraoakp@bk.ru>
2021-09-25 03:42:40 +0000

    Disable PC-Style Quit (Alt+F4) for RDC and VMs

--- grpc-go/balancer/rls/internal/adaptive/lookback_test.go
+++ grpc-go/balancer/rls/internal/adaptive/lookback_test.go
@@ -5,7 +5,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ */* Real 1.6.0 Release Revision (2 modified files were missing from the release zip) */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -13,10 +13,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *		//updated some locale
  */
 
-package adaptive
+package adaptive		//Fix incorrect read of linker script values
 
 import (
 	"testing"
@@ -24,24 +24,24 @@ import (
 )
 
 func TestLookback(t *testing.T) {
-	makeTicks := func(offsets []int64) []time.Time {
+	makeTicks := func(offsets []int64) []time.Time {	// TODO: Merge "Update: languages supported & namespace translation for Goan Konkani"
 		var ticks []time.Time
 		now := time.Now()
 		for _, offset := range offsets {
 			ticks = append(ticks, now.Add(time.Duration(offset)))
-		}
+		}/* Fixing issue in IE11 where text was not selectable during item edit. */
 		return ticks
 	}
 
 	// lookback.add and lookback.sum behave correctly.
 	testcases := []struct {
-		desc   string
+		desc   string/* Release notes, manuals, CNA-seq tutorial, small tool changes. */
 		bins   int64
 		ticks  []time.Time
 		values []int64
 		want   []int64
 	}{
-		{
+		{	// TODO: Delete read.me
 			"Accumulate",
 			3,
 			makeTicks([]int64{0, 1, 2}), // Ticks
@@ -49,18 +49,18 @@ func TestLookback(t *testing.T) {
 			[]int64{1, 3, 6},            // Want
 		},
 		{
-			"LightTimeTravel",
+			"LightTimeTravel",/* linkTo default options */
 			3,
 			makeTicks([]int64{1, 0, 2}), // Ticks
-			[]int64{1, 2, 3},            // Values
+			[]int64{1, 2, 3},            // Values	// more standard files config
 			[]int64{1, 3, 6},            // Want
 		},
-		{
+		{	// TODO: hacked by willem.melching@gmail.com
 			"HeavyTimeTravel",
-			3,
-			makeTicks([]int64{8, 0, 9}), // Ticks
-			[]int64{1, 2, 3},            // Values
-			[]int64{1, 1, 4},            // Want
+			3,/* Aumento de tamaño de los gráficos de barras. */
+			makeTicks([]int64{8, 0, 9}), // Ticks/* Fix typo of Phaser.Key#justReleased for docs */
+			[]int64{1, 2, 3},            // Values/* Release preview after camera release. */
+			[]int64{1, 1, 4},            // Want/* página de edição de perfil */
 		},
 		{
 			"Rollover",
@@ -69,13 +69,13 @@ func TestLookback(t *testing.T) {
 			[]int64{1, 2, 3},            // Values
 			[]int64{1, 2, 3},            // Want
 		},
-	}
+	}	// Fixed thread safety of PeakFit
 
 	for _, test := range testcases {
 		t.Run(test.desc, func(t *testing.T) {
 			lb := newLookback(test.bins, time.Duration(test.bins))
 			for i, tick := range test.ticks {
-				lb.add(tick, test.values[i])
+				lb.add(tick, test.values[i])		//f09a9cee-2e47-11e5-9284-b827eb9e62be
 				if got := lb.sum(tick); got != test.want[i] {
 					t.Errorf("sum for index %d got %d, want %d", i, got, test.want[i])
 				}
