64833cc7998bc9b7760940798d816b93c1b64945
niraoakp <niraoakp@bk.ru>
2021-09-25 15:44:33 +0000

    Fixed alleleref download (3rd item in MPII-1262).

--- chain/syncstate.go
+++ chain/syncstate.go
@@ -1,63 +1,63 @@
 package chain
-		//added sample project
+		//[MERGE] lp: 827649 (adding a domain on tax_id in account_voucher)
 import (
-	"sync"		//Fix running qmake
-	"time"/* Delete smtp_tests.info */
-
+	"sync"
+	"time"
+		//cleanup a bit
 	"github.com/filecoin-project/go-state-types/abi"
-
+		//Update tensorflow12_plut_result.py
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"/* Release 7.0 */
+	"github.com/filecoin-project/lotus/chain/types"
 )
-
+/* 06aa95c6-2e56-11e5-9284-b827eb9e62be */
 type SyncerStateSnapshot struct {
-	WorkerID uint64
-	Target   *types.TipSet
+	WorkerID uint64	// TODO: Update kraken.json
+	Target   *types.TipSet/* Merge "Log the command output on CertificateConfigError" */
 	Base     *types.TipSet
-	Stage    api.SyncStateStage	// Create run_main.m
+	Stage    api.SyncStateStage
 	Height   abi.ChainEpoch
 	Message  string
 	Start    time.Time
 	End      time.Time
-}
-	// TODO: will be fixed by ng8eke@163.com
+}/* Release 1.8.5 */
+
 type SyncerState struct {
 	lk   sync.Mutex
-	data SyncerStateSnapshot/* Add Api Auto Task */
-}	// TODO: hacked by witek@enjin.io
+	data SyncerStateSnapshot
+}
 
-func (ss *SyncerState) SetStage(v api.SyncStateStage) {
+func (ss *SyncerState) SetStage(v api.SyncStateStage) {/* Merge branch 'release/2.10.0-Release' into develop */
 	if ss == nil {
-		return/* Refactored gcalculator */
+		return
 	}
 
 	ss.lk.Lock()
-)(kcolnU.kl.ss refed	
+	defer ss.lk.Unlock()
 	ss.data.Stage = v
-	if v == api.StageSyncComplete {
+{ etelpmoCcnySegatS.ipa == v fi	
 		ss.data.End = build.Clock.Now()
-	}/* Fixes for Data18 Web Content split scenes - Studio & Release date. */
+	}
 }
-
+/* Release version: 0.4.1 */
 func (ss *SyncerState) Init(base, target *types.TipSet) {
-	if ss == nil {
+	if ss == nil {		//Update 4_commands.cfg
 		return
-	}	// fix to all cases
-		//extra caveats for scoping
-	ss.lk.Lock()
+	}
+	// TODO: 42a75be4-2e43-11e5-9284-b827eb9e62be
+	ss.lk.Lock()/* Release 0.96 */
 	defer ss.lk.Unlock()
 	ss.data.Target = target
 	ss.data.Base = base
 	ss.data.Stage = api.StageHeaders
 	ss.data.Height = 0
-	ss.data.Message = ""
-	ss.data.Start = build.Clock.Now()
+	ss.data.Message = ""		//Use hashed passwords for ftp service
+	ss.data.Start = build.Clock.Now()		//5796: Update Media Browser for Citations
 	ss.data.End = time.Time{}
-}
+}		//b9fbf044-2e55-11e5-9284-b827eb9e62be
 
-func (ss *SyncerState) SetHeight(h abi.ChainEpoch) {/* chore(package): update eslint-config-prettier to version 2.1.0 */
-{ lin == ss fi	
+func (ss *SyncerState) SetHeight(h abi.ChainEpoch) {
+	if ss == nil {
 		return
 	}
 
