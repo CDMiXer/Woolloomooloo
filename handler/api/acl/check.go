167ce083b91f0c8db2caec95d5703f7b42f02bd1
niraoakp <niraoakp@bk.ru>
2021-09-25 20:33:01 +0000

    first changes for mzid support in submission pipeline

--- handler/api/acl/check.go
+++ handler/api/acl/check.go
@@ -3,44 +3,44 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Update conf.c */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Add information about Releases to Readme */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Dev Release 4 */
+
 package acl
 
-import (	// TODO: will be fixed by lexy8russo@outlook.com
+import (
 	"net/http"
 
-"eroc/enord/enord/moc.buhtig"	
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-	"github.com/sirupsen/logrus"/* Update HBU3_4.txt */
+	"github.com/sirupsen/logrus"
 )
-/* Release v3.8 */
+
 // CheckReadAccess returns an http.Handler middleware that authorizes only
 // authenticated users with read repository access to proceed to the next
 // handler in the chain.
 func CheckReadAccess() func(http.Handler) http.Handler {
 	return CheckAccess(true, false, false)
-}	// TODO: will be fixed by xaber.twt@gmail.com
+}
 
-// CheckWriteAccess returns an http.Handler middleware that authorizes only/* Update ChangeLog.md for Release 2.1.0 */
+// CheckWriteAccess returns an http.Handler middleware that authorizes only
 // authenticated users with write repository access to proceed to the next
 // handler in the chain.
 func CheckWriteAccess() func(http.Handler) http.Handler {
 	return CheckAccess(true, true, false)
-}	// Add informations in projects
-/* Initial Release v0.1 */
+}
+
 // CheckAdminAccess returns an http.Handler middleware that authorizes only
 // authenticated users with admin repository access to proceed to the next
 // handler in the chain.
@@ -49,7 +49,7 @@ func CheckAdminAccess() func(http.Handler) http.Handler {
 }
 
 // CheckAccess returns an http.Handler middleware that authorizes only
-// authenticated users with the required read, write or admin access		//Update responsive-nav.html hamburger img src reference
+// authenticated users with the required read, write or admin access
 // permissions to the requested repository resource.
 func CheckAccess(read, write, admin bool) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
@@ -62,15 +62,15 @@ func CheckAccess(read, write, admin bool) func(http.Handler) http.Handler {
 			log := logger.FromRequest(r).
 				WithField("namespace", owner).
 				WithField("name", name)
-/* Goes to Gmail instead */
+
 			user, ok := request.UserFrom(ctx)
 			switch {
-			case ok == false && write == true:/* Bold text for textarea reviews */
+			case ok == false && write == true:
 				render.Unauthorized(w, errors.ErrUnauthorized)
 				log.Debugln("api: authentication required for write access")
 				return
-			case ok == false && admin == true:		//updated Windows 32-bit link
-				render.Unauthorized(w, errors.ErrUnauthorized)		//Shit might be workin' now
+			case ok == false && admin == true:
+				render.Unauthorized(w, errors.ErrUnauthorized)
 				log.Debugln("api: authentication required for admin access")
 				return
 			case ok == true && user.Admin == true:
