9e76cb78cb68eee283738cb85fd71b3f5bc37058
niraoakp <niraoakp@bk.ru>
2021-09-25 20:14:29 +0000

    Correction to --log-format in changelog

--- store/shared/encrypt/none_test.go
+++ store/shared/encrypt/none_test.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Released DirectiveRecord v0.1.5 */
-package encrypt
 
+package encrypt
+/* Update Sort */
 import "testing"
 
 func TestNone(t *testing.T) {
-	n, _ := New("")	// TODO: will be fixed by ng8eke@163.com
+	n, _ := New("")
 	ciphertext, err := n.Encrypt("correct-horse-batter-staple")
 	if err != nil {
 		t.Error(err)
 	}
 	plaintext, err := n.Decrypt(ciphertext)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)/* Added GNU licensing info */
 	}
 	if want, got := plaintext, "correct-horse-batter-staple"; got != want {
 		t.Errorf("Want plaintext %q, got %q", want, got)
