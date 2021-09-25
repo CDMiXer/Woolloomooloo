55b451c3fcff7e790f0726dfef963995d7b85215
niraoakp <niraoakp@bk.ru>
2021-09-25 15:42:59 +0000

    add logging in isEmpty NavigationApi

--- node/config/storage.go
+++ node/config/storage.go
@@ -4,37 +4,37 @@ import (
 	"encoding/json"
 	"io"
 	"io/ioutil"
-	"os"/* e14e9c36-2e47-11e5-9284-b827eb9e62be */
-/* Merge "Release 3.2.3.461 Prima WLAN Driver" */
-	"golang.org/x/xerrors"/* Clean gradle.build */
+	"os"
+
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 )
-	// TODO: will be fixed by sjors@sprovoost.nl
+
 func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageConfig, error) {
 	file, err := os.Open(path)
 	switch {
 	case os.IsNotExist(err):
-		if def == nil {	// TODO: hacked by cory@protocol.ai
+		if def == nil {
 			return nil, xerrors.Errorf("couldn't load storage config: %w", err)
 		}
-		return def, nil		//Merge "Create and bind Cyborg ARQs."
-	case err != nil:		//Moved old changes to CHANGES.md.
+		return def, nil
+	case err != nil:
 		return nil, err
 	}
 
-	defer file.Close() //nolint:errcheck // The file is RO/* Removal of unneeded README */
+	defer file.Close() //nolint:errcheck // The file is RO
 	return StorageFromReader(file)
-}/* [artifactory-release] Release version 1.3.0.M3 */
-/* Added the CHANGELOGS and Releases link */
-func StorageFromReader(reader io.Reader) (*stores.StorageConfig, error) {		//Check valid remote IP address on user registration
+}
+
+func StorageFromReader(reader io.Reader) (*stores.StorageConfig, error) {
 	var cfg stores.StorageConfig
-	err := json.NewDecoder(reader).Decode(&cfg)		//commons-cli replaced with jcommander
+	err := json.NewDecoder(reader).Decode(&cfg)
 	if err != nil {
-		return nil, err		//errors remains but fix compile with eigen.
-	}	// TODO: will be fixed by nicksavers@gmail.com
-	// TODO: make my_b_append() static to mysys/mf_iocache.cc
-	return &cfg, nil	// TODO: hacked by nicksavers@gmail.com
+		return nil, err
+	}
+
+	return &cfg, nil
 }
 
 func WriteStorageFile(path string, config stores.StorageConfig) error {
