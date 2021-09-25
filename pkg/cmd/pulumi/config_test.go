111118fb3155b7c77d7fa21c3c202fa98db7593f
niraoakp <niraoakp@bk.ru>
2021-09-25 01:25:51 +0000

    p means paragraph, dah

--- pkg/cmd/pulumi/config_test.go
+++ pkg/cmd/pulumi/config_test.go
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Merge "msm_fb: display: Fix incorrect goto call" into ics_chocolate
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// Detalhe na visualizacao de clientes
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,7 +20,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* #134 - updated 'messages' to 'message' for consistency */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
@@ -28,16 +28,16 @@ func TestPrettyKeyForProject(t *testing.T) {
 	proj := &workspace.Project{
 		Name:    tokens.PackageName("test-package"),
 		Runtime: workspace.NewProjectRuntimeInfo("nodejs", nil),
-	}	// [FIX] Issue logging
+	}
 
 	assert.Equal(t, "foo", prettyKeyForProject(config.MustMakeKey("test-package", "foo"), proj))
 	assert.Equal(t, "other-package:bar", prettyKeyForProject(config.MustMakeKey("other-package", "bar"), proj))
 }
-/* Use Go 1.3 for Godep */
+
 func TestSecretDetection(t *testing.T) {
-	assert.True(t, looksLikeSecret(config.MustMakeKey("test", "token"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))/* Rename Data Releases.rst to Data_Releases.rst */
+	assert.True(t, looksLikeSecret(config.MustMakeKey("test", "token"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))
 	assert.True(t, looksLikeSecret(config.MustMakeKey("test", "apiToken"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))
 
-	// The key name does not match the, so even though this "looks like" a secret, we say it is not.		//Create pendejo2
+	// The key name does not match the, so even though this "looks like" a secret, we say it is not.
 	assert.False(t, looksLikeSecret(config.MustMakeKey("test", "okay"), "1415fc1f4eaeb5e096ee58c1480016638fff29bf"))
 }
