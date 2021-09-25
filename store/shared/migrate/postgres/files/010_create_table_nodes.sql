4d567a38e2556f3bbc6e07c0b22fb02620594c40
niraoakp <niraoakp@bk.ru>
2021-09-25 11:30:56 +0000

    Add troubleshooting for missed custom certs

--- store/shared/migrate/postgres/files/010_create_table_nodes.sql
+++ store/shared/migrate/postgres/files/010_create_table_nodes.sql
@@ -1,4 +1,4 @@
--- name: create-table-nodes	// Planning what to do in the branch#
+-- name: create-table-nodes
 
 CREATE TABLE IF NOT EXISTS nodes (
  node_id         SERIAL PRIMARY KEY
@@ -11,21 +11,21 @@ CREATE TABLE IF NOT EXISTS nodes (
 ,node_size       VARCHAR(100)
 ,node_os         VARCHAR(50)
 ,node_arch       VARCHAR(50)
-,node_kernel     VARCHAR(50)		//Automatic changelog generation for PR #10467 [ci skip]
+,node_kernel     VARCHAR(50)
 ,node_variant    VARCHAR(50)
 ,node_address    VARCHAR(500)
-,node_capacity   INTEGER/* Merge "1.0.1 Release notes" */
+,node_capacity   INTEGER
 ,node_filter     VARCHAR(2000)
 ,node_labels     VARCHAR(2000)
 ,node_error      VARCHAR(2000)
 ,node_ca_key     BYTEA
 ,node_ca_cert    BYTEA
-,node_tls_key    BYTEA	// TODO: Merge "Change deployed server environment for ansible nic config"
+,node_tls_key    BYTEA
 ,node_tls_cert   BYTEA
 ,node_tls_name   VARCHAR(500)
 ,node_paused     BOOLEAN
 ,node_protected  BOOLEAN
-,node_created    INTEGER/* Release 2.1.12 - core data 1.0.2 */
+,node_created    INTEGER
 ,node_updated    INTEGER
 ,node_pulled     INTEGER
 
