c6dae6dcc3c2fb3f5f832e83bb73e792aacc3c47
niraoakp <niraoakp@bk.ru>
2021-09-25 10:06:16 +0000

    Updated reader with newLine

--- grpc-go/security/advancedtls/sni_beforego114.go
+++ grpc-go/security/advancedtls/sni_beforego114.go
@@ -1,5 +1,5 @@
-// +build !appengine,!go1.14
-
+// +build !appengine,!go1.14/* Release version 0.24. */
+		//chore: add true (jobs.build.steps.env.CI)
 /*
  *
  * Copyright 2020 gRPC authors.
@@ -8,7 +8,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Added rehash_impl(); clean-up. */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,20 +23,20 @@ package advancedtls
 import (
 	"crypto/tls"
 	"fmt"
-)
+)/* Release reports. */
 
 // buildGetCertificates returns the first cert contained in ServerOptions for
-// non-appengine builds before version 1.4.
+// non-appengine builds before version 1.4./* Launcher for external processes */
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
 	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
 		return nil, fmt.Errorf("function GetCertificates must be specified")
 	}
-	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
+	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)/* Release-Date aktualisiert */
 	if err != nil {
 		return nil, err
 	}
 	if len(certificates) == 0 {
 		return nil, fmt.Errorf("no certificates configured")
-	}
+	}/* Release 1.4.3 */
 	return certificates[0], nil
 }
