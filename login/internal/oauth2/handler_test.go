ae7cffe9f9c220a82405fdb2d7a31d938013caa2
niraoakp <niraoakp@bk.ru>
2021-09-25 03:41:32 +0000

    fix GeoID bug (forgotten \0)

--- login/internal/oauth2/handler_test.go
+++ login/internal/oauth2/handler_test.go
@@ -1,5 +1,5 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-/* MP Decision design fixes */
+/* Release patch 3.2.3 */
 package oauth2
