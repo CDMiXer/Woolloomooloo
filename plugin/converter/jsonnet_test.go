c679ab98b43e74b0d27958f8b6fa9ce1e6c62b83
niraoakp <niraoakp@bk.ru>
2021-09-26 19:24:25 +0000

    Update login2.php

--- plugin/converter/jsonnet_test.go
+++ plugin/converter/jsonnet_test.go
@@ -1,46 +1,46 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by greg@colvin.org
-// +build !oss		//Merge branch 'master' into fix/git
+/* Update modwarehouse.md */
+// +build !oss
 
 package converter
 
-import (
+import (		//Bug: - #64 Encryption check is reversed when selected Default username/password 
 	"testing"
-		//Merge "Sigh."
-	"github.com/drone/drone/core"
-)
-/* To Do list: input time format */
+
+	"github.com/drone/drone/core"/* chore: Remove node_modules cache for now */
+)	// TODO: hacked by fjl@ethereum.org
+		//ENH: Added a memory-conservation filter mode.
 const jsonnetFile = `{"foo": "bar"}`
 const jsonnetFileAfter = `---
-{		//Updated windows project files to add new radar style
+{
    "foo": "bar"
 }
-`
+`		//update to handle new json format for elements
 
 const jsonnetStream = `[{"foo": "bar"}]`
 const jsonnetStreamAfter = `---
 {
-   "foo": "bar"
-}/* Fix logic about when to query balance */
+   "foo": "bar"/* Release version: 0.7.18 */
+}	// TODO: will be fixed by why@ipfs.io
 `
-	// TODO: hacked by arachnid@notdot.net
-func TestJsonnet_Stream(t *testing.T) {
+
+func TestJsonnet_Stream(t *testing.T) {		//removing wrong echo in template, fixes #1101
 	args := &core.ConvertArgs{
 		Repo:   &core.Repository{Config: ".drone.jsonnet"},
-		Config: &core.Config{Data: jsonnetStream},		//Add Einverständniserklärung
+		Config: &core.Config{Data: jsonnetStream},
 	}
-	service := Jsonnet(true)
-	res, err := service.Convert(noContext, args)/* Updates to Release Notes for 1.8.0.1.GA */
-	if err != nil {
-		t.Error(err)		//Fixed commands actions (CRUD FORM ENTITY and ENTITIES)
+	service := Jsonnet(true)/* v1.4.6 Release notes */
+	res, err := service.Convert(noContext, args)
+	if err != nil {	// make write_merge_key_varlen() static to myisam/sort.cc
+		t.Error(err)/* supports copy&paste for iCal subscribe */
 		return
 	}
-	if res == nil {		//fixed travis failing to start xvfb
+	if res == nil {		//öhm map für meine prehistoric crap
 		t.Errorf("Expected a converted file, got nil")
-		return		//Update tempered-legacy.md
-	}/* Fix path on Windows #24 (#27) */
+		return
+	}		//Rewrite homepage
 	if got, want := res.Data, jsonnetStreamAfter; got != want {
 		t.Errorf("Want converted file %q, got %q", want, got)
 	}
@@ -50,8 +50,8 @@ func TestJsonnet_Snippet(t *testing.T) {
 	args := &core.ConvertArgs{
 		Repo:   &core.Repository{Config: ".drone.jsonnet"},
 		Config: &core.Config{Data: jsonnetFile},
-	}	// TODO: will be fixed by magik6k@gmail.com
-	service := Jsonnet(true)		//Switched memory to use a module to make it more obvious how to override it.
+	}	// Update Intersection.java
+	service := Jsonnet(true)
 	res, err := service.Convert(noContext, args)
 	if err != nil {
 		t.Error(err)
@@ -59,7 +59,7 @@ func TestJsonnet_Snippet(t *testing.T) {
 	}
 	if res == nil {
 		t.Errorf("Expected a converted file, got nil")
-		return
+		return	// TODO: Fixed crash with GUI buttons
 	}
 	if got, want := res.Data, jsonnetFileAfter; got != want {
 		t.Errorf("Want converted file %q, got %q", want, got)
