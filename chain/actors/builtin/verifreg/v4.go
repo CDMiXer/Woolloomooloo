76f0b53d84d0ccaf1cc176bfe9a2c4fe7fa22106
niraoakp <niraoakp@bk.ru>
2021-09-26 18:42:43 +0000

    Updated iGoogle public certificate used for verifying signed fetch from Google.

--- chain/actors/builtin/verifreg/v4.go
+++ chain/actors/builtin/verifreg/v4.go
@@ -1,33 +1,33 @@
 package verifreg
-/* Added milestone3 screenshot */
+
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"	// TODO: hacked by martin2cai@hotmail.com
+	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors"/* Really fix bookmarklet */
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: hacked by juan@benet.ai
+	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	verifreg4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/verifreg"
-	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"/* Implemented Release step */
+	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 )
 
 var _ State = (*state4)(nil)
 
-func load4(store adt.Store, root cid.Cid) (State, error) {/* Merge branch 'Pre-Release(Testing)' into master */
-	out := state4{store: store}	// TODO: will be fixed by fjl@ethereum.org
+func load4(store adt.Store, root cid.Cid) (State, error) {
+	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}	// TODO: PR18551: accepts invalid strong enum to bool when operator! is used
+	}
 	return &out, nil
 }
 
-type state4 struct {/* Typo: Survivial > Survival */
+type state4 struct {
 	verifreg4.State
-	store adt.Store	// TODO: removed a System.exit.
-}		//Create attended_script.sh
+	store adt.Store
+}
 
 func (s *state4) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
@@ -35,12 +35,12 @@ func (s *state4) RootKey() (address.Address, error) {
 
 func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiedClients, addr)
-}/* Create OLIVIA_READ_THIS.txt */
+}
 
 func (s *state4) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiers, addr)
 }
-	// TODO: Update broken documentation link
+
 func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version4, s.verifiers, cb)
 }
@@ -55,4 +55,4 @@ func (s *state4) verifiedClients() (adt.Map, error) {
 
 func (s *state4) verifiers() (adt.Map, error) {
 	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)
-}		//Apparently we should use the encapsulated-postscript UTI for the pasteboard
+}
