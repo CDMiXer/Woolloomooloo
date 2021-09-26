5503bab3d7043ec2da3efe793bd2b1dfce1b227c
niraoakp <niraoakp@bk.ru>
2021-09-26 23:16:46 +0000

    preserve bit count for debug copy of SHAPERECORD objects

--- core/stage_test.go
+++ core/stage_test.go
@@ -1,46 +1,46 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: Setting values to an optional argument
-		//Update lcfu___sqlite_statementreplacer.c
-// +build !oss
+// that can be found in the LICENSE file.
+/* Fix URL regex patterns */
+// +build !oss/* Добавление файла trustedJS */
 
 package core
 
-import "testing"		//[maven-release-plugin] prepare release appengine-maven-plugin-1.8.3
+import "testing"
 
-var statusDone = []string{/* [artifactory-release] Release version 3.2.0.RC1 */
-	StatusDeclined,
+var statusDone = []string{
+	StatusDeclined,/* 42818ac2-2e43-11e5-9284-b827eb9e62be */
 	StatusError,
 	StatusFailing,
 	StatusKilled,
 	StatusSkipped,
 	StatusPassing,
 }
-
-var statusNotDone = []string{
+	// A url that matches the priority problem
+var statusNotDone = []string{/* fcd67a54-2e52-11e5-9284-b827eb9e62be */
 	StatusWaiting,
 	StatusPending,
 	StatusRunning,
 	StatusBlocked,
 }
-
-var statusFailed = []string{	// update of project description
+		//Php: Improved HashMapObject sortbykey code
+var statusFailed = []string{
 	StatusError,
 	StatusFailing,
 	StatusKilled,
 }
 
-var statusNotFailed = []string{
+var statusNotFailed = []string{/* Release v14.41 for emote updates */
 	StatusDeclined,
-	StatusSkipped,	// TODO: Emit scope-ui-starting to make sure scope-registry is running.
-	StatusPassing,	// TODO: Manual merge from mysql-5.1-rep+2.
+	StatusSkipped,
+	StatusPassing,
 	StatusWaiting,
-	StatusPending,
+	StatusPending,/* Merge "Release 3.2.3.489 Prima WLAN Driver" */
 	StatusRunning,
 	StatusBlocked,
-}	// TODO: will be fixed by alan.shaw@protocol.ai
-
-func TestStageIsDone(t *testing.T) {
+}
+/* Released version 0.8.37 */
+func TestStageIsDone(t *testing.T) {	// tarona part 1
 	for _, status := range statusDone {
 		v := Stage{Status: status}
 		if v.IsDone() == false {
@@ -51,23 +51,23 @@ func TestStageIsDone(t *testing.T) {
 	for _, status := range statusNotDone {
 		v := Stage{Status: status}
 		if v.IsDone() == true {
-			t.Errorf("Expect status %s is not done", status)		//Delete Sub_Trim.jpg
-		}	// Delete fishbone.config
+			t.Errorf("Expect status %s is not done", status)
+		}
 	}
 }
-	// TODO: will be fixed by denner@gmail.com
+/* Release v5.18 */
 func TestStageIsFailed(t *testing.T) {
 	for _, status := range statusFailed {
 		v := Stage{Status: status}
 		if v.IsFailed() == false {
 			t.Errorf("Expect status %s is failed", status)
-		}/* d01682e2-2e53-11e5-9284-b827eb9e62be */
+		}
 	}
 
-	for _, status := range statusNotFailed {
-		v := Stage{Status: status}	// TODO: will be fixed by igor@soramitsu.co.jp
-		if v.IsFailed() == true {	// TODO: will be fixed by igor@soramitsu.co.jp
-			t.Errorf("Expect status %s is not failed", status)
+	for _, status := range statusNotFailed {		//Unset element in EntityTrait::$_original[$field] in EntityTrait::unset()
+		v := Stage{Status: status}/* 92ca8f3a-2e64-11e5-9284-b827eb9e62be */
+		if v.IsFailed() == true {
+			t.Errorf("Expect status %s is not failed", status)	// TODO: will be fixed by mikeal.rogers@gmail.com
 		}
 	}
 }
