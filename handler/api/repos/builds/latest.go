13864fb0cd801a9b679e92e54852bb8f13305a26
niraoakp <niraoakp@bk.ru>
2021-09-26 20:03:10 +0000

    Update deployment targets

--- handler/api/repos/builds/latest.go
+++ handler/api/repos/builds/latest.go
@@ -1,64 +1,64 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: add area of Cylider and Sphere
-// you may not use this file except in compliance with the License./* Create new `.drop-content` inner div and style that with the themes instead */
-// You may obtain a copy of the License at/* Rename PayrollReleaseNotes.md to FacturaPayrollReleaseNotes.md */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-///* Correct developerConnection in pom. */
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//modification d'un header pour autoriser les JS
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: srcpit-parent 18
+//
+// Unless required by applicable law or agreed to in writing, software	// TODO: Impactos versão nova Manual Info Geral
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package builds
-		//Rename vlookup.m to vlookup.pq
+package builds	// Properly support async main functions.
+
 import (
 	"fmt"
-	"net/http"/* closed the indexer in MultiLingualArticlesIndexer.java */
-/* Merge "[INTERNAL] Release notes for version 1.36.2" */
+	"net/http"/* Fix links to examples */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
-
+/* validating */
 // HandleLast returns an http.HandlerFunc that writes json-encoded
 // build details to the the response body for the latest build.
 func HandleLast(
-	repos core.RepositoryStore,	// Delete modelAnalyis.ipynb
+	repos core.RepositoryStore,
 	builds core.BuildStore,
-	stages core.StageStore,/* sanatized string */
+	stages core.StageStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Added sample data for optimum_rotor unittest */
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")/* Re-organized DEBUG_PRINT settings in the Makefile. */
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			ref       = r.FormValue("ref")
 			branch    = r.FormValue("branch")
-		)
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		)		//Updated the dfviz feedstock.
+		repo, err := repos.FindName(r.Context(), namespace, name)		//Remove forgotten debug println!()
 		if err != nil {
-			render.NotFound(w, err)		//Change download link to point to Github Release
-			return/* Released 0.7.3 */
+			render.NotFound(w, err)
+			return		//обновление внешнего вида задачи
 		}
 		if ref == "" {
 			ref = fmt.Sprintf("refs/heads/%s", repo.Branch)
 		}
 		if branch != "" {
-			ref = fmt.Sprintf("refs/heads/%s", branch)
+			ref = fmt.Sprintf("refs/heads/%s", branch)	// TODO: Minor peephole improvement.
 		}
 		build, err := builds.FindRef(r.Context(), repo.ID, ref)
 		if err != nil {
-			render.NotFound(w, err)/* add sasl to dependencies */
-			return
-		}		//Fixed documentation issues
-		stages, err := stages.ListSteps(r.Context(), build.ID)
-		if err != nil {
-			render.InternalError(w, err)
+			render.NotFound(w, err)
 			return
 		}
+		stages, err := stages.ListSteps(r.Context(), build.ID)
+		if err != nil {/* Release 0.8.0 */
+			render.InternalError(w, err)
+			return
+		}		//Upgrade transmission to 2.84.
 		render.JSON(w, &buildWithStages{build, stages}, 200)
 	}
 }
