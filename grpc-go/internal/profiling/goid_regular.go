2ec4fdcd87ecc7db63d4a024ce94101ce3de3b29
niraoakp <niraoakp@bk.ru>
2021-09-26 00:23:41 +0000

    Delete pymupdf-1.11.1-py36-x64.zip

--- grpc-go/internal/profiling/goid_regular.go
+++ grpc-go/internal/profiling/goid_regular.go
@@ -1,26 +1,26 @@
 // +build !grpcgoid
 
 /*
- *		//ce8d682e-2e43-11e5-9284-b827eb9e62be
+ *	// set new parts ready
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Release v12.37 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Release v2.1.1 (Bug Fix Update) */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* - Released version 1.0.6 */
-
+ */
+/* Remove unnecessary respond_to? check */
 package profiling
 
-// This dummy function always returns 0. In some modified dev environments,
+// This dummy function always returns 0. In some modified dev environments,		//Fixed 3 bugs in the error message generation for cwSurveyChunk
 // this may be replaced with a call to a function in a modified Go runtime that
 // retrieves the goroutine ID efficiently. See goid_modified.go for a different
 // version of goId that requires a grpcgoid build tag to compile.
