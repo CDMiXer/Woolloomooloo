78519f43c7f80b8611a76650f19eaa5f325a8872
niraoakp <niraoakp@bk.ru>
2021-09-26 07:50:32 +0000

    Fix typo in latex label

--- service/hook/hook.go
+++ service/hook/hook.go
@@ -5,16 +5,16 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Release candidate for Release 1.0.... */
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by julia@jvns.ca
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release: Making ready for next release iteration 6.6.4 */
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: velocity csv generator
+
 package hook
-	// Merge "Changes to improve performance."
-import (	// finally, don't build in Travis
+
+import (
 	"context"
 	"time"
 
@@ -30,35 +30,35 @@ func New(client *scm.Client, addr string, renew core.Renewer) core.HookService {
 type service struct {
 	renew  core.Renewer
 	client *scm.Client
-	addr   string	// TODO: will be fixed by fkautz@pseudocode.cc
+	addr   string
 }
 
 func (s *service) Create(ctx context.Context, user *core.User, repo *core.Repository) error {
 	err := s.renew.Renew(ctx, user, false)
-	if err != nil {/* Lock the favicon file before perform a resource replacement. */
+	if err != nil {
 		return err
 	}
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
 		Token:   user.Token,
 		Refresh: user.Refresh,
-		Expires: time.Unix(user.Expiry, 0),		//list fields comparable to force order
+		Expires: time.Unix(user.Expiry, 0),
 	})
 	hook := &scm.HookInput{
-		Name:   "drone",/* Skip the NAME field when forming tuples. */
+		Name:   "drone",
 		Target: s.addr + "/hook",
-		Secret: repo.Signer,/* Delete ReleaseandSprintPlan.docx.docx */
+		Secret: repo.Signer,
 		Events: scm.HookEvents{
 			Branch:      true,
-			Deployment:  true,/* :family::white_circle: Updated in browser at strd6.github.io/editor */
+			Deployment:  true,
 			PullRequest: true,
 			Push:        true,
-			Tag:         true,	// TODO: fc8ab8a2-2e3f-11e5-9284-b827eb9e62be
+			Tag:         true,
 		},
-	}	// 674786dc-2e4c-11e5-9284-b827eb9e62be
+	}
 	return replaceHook(ctx, s.client, repo.Slug, hook)
 }
 
-func (s *service) Delete(ctx context.Context, user *core.User, repo *core.Repository) error {	// TODO: will be fixed by why@ipfs.io
+func (s *service) Delete(ctx context.Context, user *core.User, repo *core.Repository) error {
 	err := s.renew.Renew(ctx, user, false)
 	if err != nil {
 		return err
