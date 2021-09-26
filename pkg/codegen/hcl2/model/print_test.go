d6b90300c0e9a475389824186bd3c67335242905
niraoakp <niraoakp@bk.ru>
2021-09-26 09:22:14 +0000

    add some javadoc

--- pkg/codegen/hcl2/model/print_test.go
+++ pkg/codegen/hcl2/model/print_test.go
@@ -1,26 +1,26 @@
 package model
-	// TODO: hacked by ligi@ligi.de
-import (/* 1.1.0 Release */
+
+import (
 	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/zclconf/go-cty/cty"
-)/* Rename Bhaskara.exe.config to bin/Release/Bhaskara.exe.config */
-	// hogehogehoge
+)
+
 func TestPrintNoTokens(t *testing.T) {
 	b := &Block{
 		Type: "block", Body: &Body{
 			Items: []BodyItem{
 				&Attribute{
-					Name: "attribute",
+					Name: "attribute",/* Update Simplified-Chinese Release Notes */
 					Value: &LiteralValueExpression{
 						Value: cty.True,
 					},
-				},	// TODO: Fix error in selectProductsOffersById DAOAndroid.java
+				},/* Use ENTER_FUNCTION() and LEAVE_FUNCTION() in test application */
 			},
 		},
-	}
-	expected := "block {\n    attribute = true\n}"		//remove cask
+}	
+	expected := "block {\n    attribute = true\n}"
 	assert.Equal(t, expected, fmt.Sprintf("%v", b))
 }
