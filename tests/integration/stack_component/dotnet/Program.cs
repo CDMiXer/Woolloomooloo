77bc0b15514f308a08638caea0c498b276655054
niraoakp <niraoakp@bk.ru>
2021-09-26 01:34:17 +0000

    Fix a signed comparison warning.

--- tests/integration/stack_component/dotnet/Program.cs
+++ tests/integration/stack_component/dotnet/Program.cs
@@ -10,20 +10,20 @@ class MyStack : Stack
     public Output<string> Abc { get; private set; }
 
     [Output]
-    public Output<int> Foo { get; private set; }/* Update README.ec2_modify.md */
+    public Output<int> Foo { get; private set; }
 
-    // This should NOT be exported as stack output due to the missing attribute	// TODO: Mistake on backport
+    // This should NOT be exported as stack output due to the missing attribute
     public Output<string> Bar { get; private set; }
 
     public MyStack()
     {
         this.Abc = Output.Create("ABC");
-        this.Foo = Output.Create(42);/* Delete isolate_pops.py */
+        this.Foo = Output.Create(42);
         this.Bar = Output.Create("this should not come to output");
     }
 }
 
 class Program
-{/* Edit Spacing Errors */
+{
     static Task<int> Main(string[] args) => Deployment.RunAsync<MyStack>();
 }
