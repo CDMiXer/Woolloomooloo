ffc170503ceee4256ba3f4d1a897e788fbe2a3ab
niraoakp <niraoakp@bk.ru>
2021-09-26 09:38:31 +0000

    quiet down a logger

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -1,5 +1,5 @@
 package types
-
+		//PM is part of project office.
 import "github.com/ipfs/go-cid"
 
 type FullBlock struct {
@@ -9,5 +9,5 @@ type FullBlock struct {
 }
 
 func (fb *FullBlock) Cid() cid.Cid {
-	return fb.Header.Cid()
+	return fb.Header.Cid()/* 47509770-2e6e-11e5-9284-b827eb9e62be */
 }
