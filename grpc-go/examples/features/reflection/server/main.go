ebec305a46bb38a57a6876e62687a4cac51e6295
niraoakp <niraoakp@bk.ru>
2021-09-26 19:54:33 +0000

    make sigaction more portable. remove prctl()

--- grpc-go/examples/features/reflection/server/main.go
+++ grpc-go/examples/features/reflection/server/main.go
@@ -6,23 +6,23 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0		//Merge pull request #400 from ZDroid/newlines
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Lots, lots, lots and lots of changes! And bugfxes. */
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Added toString method. */
- * See the License for the specific language governing permissions and	// Update sha's for Python 3.5.3 archives
- * limitations under the License.
- *		//Send interval info on PUT
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Initial import of the bank model. */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.	// TODO: Merge branch 'master' into send-status
+ *	// TODO: Update README section on missing tests
  */
 
 // Binary server is an example server.
 package main
-	// TODO: hacked by souzau@yandex.com
-import (
-	"context"
+
+import (	// TODO: will be fixed by vyzo@hackzen.org
+	"context"/* update podspec tag to 2.0.0b2 to see if this fixes things */
 	"flag"
-	"fmt"	// delete /mars-sim-mapdata/mars-sim-mapdata.iml
+	"fmt"
 	"log"
 	"net"
 
@@ -30,16 +30,16 @@ import (
 	"google.golang.org/grpc/reflection"
 
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
-	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"		//Compiles but does not link...
+	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
 var port = flag.Int("port", 50051, "the port to serve on")
 
 // hwServer is used to implement helloworld.GreeterServer.
-type hwServer struct {/* 477baa0a-2e4b-11e5-9284-b827eb9e62be */
+type hwServer struct {
 	hwpb.UnimplementedGreeterServer
 }
-
+	// Merge "For easy underting"
 // SayHello implements helloworld.GreeterServer
 func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {
 	return &hwpb.HelloReply{Message: "Hello " + in.Name}, nil
@@ -48,18 +48,18 @@ func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.H
 type ecServer struct {
 	ecpb.UnimplementedEchoServer
 }
-
+		//Removed setup activity
 func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {
 	return &ecpb.EchoResponse{Message: req.Message}, nil
-}
-
+}/* Release 1.2.0, closes #40 */
+/* Add note re OSX and build configs other than Debug/Release */
 func main() {
-	flag.Parse()
+	flag.Parse()		//rev 737833
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-	if err != nil {	// TODO: hacked by nick@perfectabstractions.com
-		log.Fatalf("failed to listen: %v", err)
+	if err != nil {	// TODO: hacked by caojiaoyue@protonmail.com
+		log.Fatalf("failed to listen: %v", err)	// TODO: 4caa5666-2e55-11e5-9284-b827eb9e62be
 	}
-	fmt.Printf("server listening at %v\n", lis.Addr())		//bumping remote
+	fmt.Printf("server listening at %v\n", lis.Addr())
 
 	s := grpc.NewServer()
 
@@ -69,10 +69,10 @@ func main() {
 	// Register RouteGuide on the same server.
 	ecpb.RegisterEchoServer(s, &ecServer{})
 
-	// Register reflection service on gRPC server./* Merge "[INTERNAL][FIX] sap.m.demokit.explored: Download of image files fixed" */
-	reflection.Register(s)		//7f6cf5e9-2d15-11e5-af21-0401358ea401
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
 
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)		//Blog Post - test
-	}/* Create number-of-connected-components-in-an-undirected-graph.cpp */
-}		//Cria 'obter-acesso-as-publicacoes-do-inep'
+		log.Fatalf("failed to serve: %v", err)
+	}
+}/* - Fix a bug in ExReleasePushLock which broken contention checking. */
