f48bdc70067ecee892fa605cac6088cf20344b19
niraoakp <niraoakp@bk.ru>
2021-09-26 16:40:40 +0000

    Improved GUI names

--- hack/pull-build-images.sh
+++ hack/pull-build-images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eu -o pipefail
 
-grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done
+grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done	// 4fa52c06-2e5f-11e5-9284-b827eb9e62be
