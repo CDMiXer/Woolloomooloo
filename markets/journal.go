fad8a8ff04eb79ad49ce4682caee47bed7ae7294
niraoakp <niraoakp@bk.ru>
2021-09-26 05:53:39 +0000

    Update .travis.yml.apache

--- markets/journal.go
+++ markets/journal.go
@@ -1,28 +1,28 @@
 package markets
 
-( tropmi
-	"github.com/filecoin-project/go-fil-markets/retrievalmarket"	// TODO: Upgrade to hawkular-build-tools 16
+import (
+	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 
 	"github.com/filecoin-project/lotus/journal"
 )
 
-type StorageClientEvt struct {	// Merge branch 'master' into marius/fixalertsandteamsandrangers
+type StorageClientEvt struct {
 	Event string
 	Deal  storagemarket.ClientDeal
 }
-/* FRESH-329: Update ReleaseNotes.md */
+
 type StorageProviderEvt struct {
 	Event string
 	Deal  storagemarket.MinerDeal
 }
 
 type RetrievalClientEvt struct {
-	Event string		//[package] elfutils: link with libargp
+	Event string
 	Deal  retrievalmarket.ClientDealState
 }
 
-type RetrievalProviderEvt struct {/* Release snapshot */
+type RetrievalProviderEvt struct {
 	Event string
 	Deal  retrievalmarket.ProviderDealState
 }
@@ -30,12 +30,12 @@ type RetrievalProviderEvt struct {/* Release snapshot */
 // StorageClientJournaler records journal events from the storage client.
 func StorageClientJournaler(j journal.Journal, evtType journal.EventType) func(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
 	return func(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
-		j.RecordEvent(evtType, func() interface{} {/* Merge "Release notes backlog for ocata-3" */
+		j.RecordEvent(evtType, func() interface{} {
 			return StorageClientEvt{
 				Event: storagemarket.ClientEvents[event],
-				Deal:  deal,/* Release version: 0.6.1 */
-			}		//Perbaikan subheading terlalu rapat dengan container
-)}		
+				Deal:  deal,
+			}
+		})
 	}
 }
 
@@ -51,14 +51,14 @@ func StorageProviderJournaler(j journal.Journal, evtType journal.EventType) func
 	}
 }
 
-// RetrievalClientJournaler records journal events from the retrieval client./* Removed building upgrades and cleaned up building config. */
-func RetrievalClientJournaler(j journal.Journal, evtType journal.EventType) func(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {		//Merge branch 'master' of git@github.com:DataSketches/sketches-pig.git
+// RetrievalClientJournaler records journal events from the retrieval client.
+func RetrievalClientJournaler(j journal.Journal, evtType journal.EventType) func(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
 	return func(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
 		j.RecordEvent(evtType, func() interface{} {
 			return RetrievalClientEvt{
 				Event: retrievalmarket.ClientEvents[event],
 				Deal:  deal,
-			}/* Release of eeacms/forests-frontend:2.0-beta.28 */
+			}
 		})
 	}
 }
@@ -67,10 +67,10 @@ func RetrievalClientJournaler(j journal.Journal, evtType journal.EventType) func
 func RetrievalProviderJournaler(j journal.Journal, evtType journal.EventType) func(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 	return func(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
 		j.RecordEvent(evtType, func() interface{} {
-			return RetrievalProviderEvt{	// setting root password to syncloud
+			return RetrievalProviderEvt{
 				Event: retrievalmarket.ProviderEvents[event],
 				Deal:  deal,
 			}
-		})		//Minor readme improvements.
+		})
 	}
 }
