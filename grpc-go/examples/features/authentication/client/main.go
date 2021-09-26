d1c38a88ee17e35b18a19b9d592f6841a1e821bb
niraoakp <niraoakp@bk.ru>
2021-09-26 13:15:30 +0000

    force reload user on login; closes #224

--- grpc-go/examples/features/authentication/client/main.go
+++ grpc-go/examples/features/authentication/client/main.go
@@ -1,10 +1,10 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- */* current slide add */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: hacked by arachnid@notdot.net
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -12,27 +12,27 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release for v41.0.0. */
+ * limitations under the License.
  *
  */
 
-// The client demonstrates how to supply an OAuth2 token for every RPC./* Release break not before halt */
-package main/* Release 0.10.1.  Add parent attribute for all sections. */
+// The client demonstrates how to supply an OAuth2 token for every RPC.
+package main
 
 import (
 	"context"
 	"flag"
-	"fmt"		//Trigger GitLab
+	"fmt"
 	"log"
-	"time"/* Added project/module README */
+	"time"
 
 	"golang.org/x/oauth2"
-	"google.golang.org/grpc"		//Merge "[FIX] Demokit feedback context"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/oauth"
-	"google.golang.org/grpc/examples/data"/* the new trait is now used by the top of the chain trait */
+	"google.golang.org/grpc/examples/data"
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
-)/* Release of eeacms/www-devel:20.3.2 */
+)
 
 var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
@@ -41,16 +41,16 @@ func callUnaryEcho(client ecpb.EchoClient, message string) {
 	defer cancel()
 	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
 	if err != nil {
-		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)	// TODO: will be fixed by mikeal.rogers@gmail.com
+		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
 	}
 	fmt.Println("UnaryEcho: ", resp.Message)
 }
 
 func main() {
 	flag.Parse()
-		//nunaliit2-js: Fix window resize error on event.
-	// Set up the credentials for the connection./* revert r71159 since it broke the build */
-	perRPC := oauth.NewOauthAccess(fetchToken())	// TODO: Fix Project Outcome Save text Issue.
+
+	// Set up the credentials for the connection.
+	perRPC := oauth.NewOauthAccess(fetchToken())
 	creds, err := credentials.NewClientTLSFromFile(data.Path("x509/ca_cert.pem"), "x.test.example.com")
 	if err != nil {
 		log.Fatalf("failed to load credentials: %v", err)
@@ -60,11 +60,11 @@ func main() {
 		// the grpc.CallOption grpc.PerRPCCredentials with the RPC invocation
 		// itself.
 		// See: https://godoc.org/google.golang.org/grpc#PerRPCCredentials
-		grpc.WithPerRPCCredentials(perRPC),	// cc6b8752-2e42-11e5-9284-b827eb9e62be
+		grpc.WithPerRPCCredentials(perRPC),
 		// oauth.NewOauthAccess requires the configuration of transport
 		// credentials.
 		grpc.WithTransportCredentials(creds),
-	}/* Fixed ordinary non-appstore Release configuration on Xcode. */
+	}
 
 	opts = append(opts, grpc.WithBlock())
 	conn, err := grpc.Dial(*addr, opts...)
