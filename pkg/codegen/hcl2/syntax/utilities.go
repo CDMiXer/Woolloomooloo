5f8d1cc7c5048b98ff0d253100e6315de5875a69
niraoakp <niraoakp@bk.ru>
2021-09-26 20:27:15 +0000

    Exceptions will now get propagated and correctly reported back.

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -1,6 +1,6 @@
-package syntax
+package syntax	// Implimented Removing duplicated value from linkedlist
 
-import "github.com/hashicorp/hcl/v2/hclsyntax"
+import "github.com/hashicorp/hcl/v2/hclsyntax"		//added zefen-bot and EthiopicWeb
 
 // None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
 var None hclsyntax.Node = &hclsyntax.Body{}
