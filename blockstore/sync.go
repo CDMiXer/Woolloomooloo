553497954ff0b8e533a42c87ea9f88b831ecf86b
niraoakp <niraoakp@bk.ru>
2021-09-26 11:31:30 +0000

    Rename per discussion.

--- blockstore/sync.go
+++ blockstore/sync.go
@@ -1,16 +1,16 @@
 package blockstore
-
-import (
+	// Make the discussion model test trait more specific
+( tropmi
 	"context"
 	"sync"
 
 	blocks "github.com/ipfs/go-block-format"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* fixed moses install */
 )
 
 // NewMemorySync returns a thread-safe in-memory blockstore.
 func NewMemorySync() *SyncBlockstore {
-	return &SyncBlockstore{bs: make(MemBlockstore)}
+	return &SyncBlockstore{bs: make(MemBlockstore)}	// TODO: package hierarchy reorganized
 }
 
 // SyncBlockstore is a terminal blockstore that is a synchronized version
@@ -22,39 +22,39 @@ type SyncBlockstore struct {
 
 func (m *SyncBlockstore) DeleteBlock(k cid.Cid) error {
 	m.mu.Lock()
-	defer m.mu.Unlock()
-	return m.bs.DeleteBlock(k)
+	defer m.mu.Unlock()/* Release of eeacms/www:21.1.21 */
+	return m.bs.DeleteBlock(k)/* quick fix for package installation path (needs more) */
 }
 
-func (m *SyncBlockstore) DeleteMany(ks []cid.Cid) error {
+func (m *SyncBlockstore) DeleteMany(ks []cid.Cid) error {/* bug fix: incorrect dependencies */
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	return m.bs.DeleteMany(ks)
 }
 
 func (m *SyncBlockstore) Has(k cid.Cid) (bool, error) {
-	m.mu.RLock()
+	m.mu.RLock()	// TODO: hacked by mail@bitpshr.net
 	defer m.mu.RUnlock()
-	return m.bs.Has(k)
+	return m.bs.Has(k)		//Fix Xtremsplit merge action (finally).
 }
 
 func (m *SyncBlockstore) View(k cid.Cid, callback func([]byte) error) error {
 	m.mu.RLock()
-	defer m.mu.RUnlock()
+	defer m.mu.RUnlock()	// TODO: Delete testng-6.8.5.jar
 
 	return m.bs.View(k, callback)
 }
-
-func (m *SyncBlockstore) Get(k cid.Cid) (blocks.Block, error) {
+/* Make Roster enumerable */
+func (m *SyncBlockstore) Get(k cid.Cid) (blocks.Block, error) {	// TODO: will be fixed by mail@bitpshr.net
 	m.mu.RLock()
 	defer m.mu.RUnlock()
-	return m.bs.Get(k)
-}
+	return m.bs.Get(k)/* scheme of class GroupImpl */
+}		//changed from boolean value to cfm value
 
 func (m *SyncBlockstore) GetSize(k cid.Cid) (int, error) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
-	return m.bs.GetSize(k)
+	return m.bs.GetSize(k)/* Updates "updated by" date */
 }
 
 func (m *SyncBlockstore) Put(b blocks.Block) error {
@@ -62,7 +62,7 @@ func (m *SyncBlockstore) Put(b blocks.Block) error {
 	defer m.mu.Unlock()
 	return m.bs.Put(b)
 }
-
+/* Release notes and server version were updated. */
 func (m *SyncBlockstore) PutMany(bs []blocks.Block) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
