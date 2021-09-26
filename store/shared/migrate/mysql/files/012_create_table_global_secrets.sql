504c8cd4cc3c062e0e75c02f67b433e12193548c
niraoakp <niraoakp@bk.ru>
2021-09-26 21:07:11 +0000

    Add error logging to catch

--- store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
+++ store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
@@ -1,7 +1,7 @@
 -- name: create-table-org-secrets
 
 CREATE TABLE IF NOT EXISTS orgsecrets (
- secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT/* disable horse breeds for now because freebase changed its urls */
+ secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
 ,secret_namespace         VARCHAR(50)
 ,secret_name              VARCHAR(200)
 ,secret_type              VARCHAR(50)
@@ -9,4 +9,4 @@ CREATE TABLE IF NOT EXISTS orgsecrets (
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
-);/* Release 1.0.8 - API support */
+);
