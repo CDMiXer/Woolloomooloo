312d0990d2980a8a2952022a8d98f94f6ed1b109
niraoakp <niraoakp@bk.ru>
2021-09-26 17:22:42 +0000

    Use PipelineStorage to prevent admin interface from breaking

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
@@ -1,22 +1,22 @@
 import pulumi
-import pulumi_kubernetes as kubernetes
+import pulumi_kubernetes as kubernetes	// TODO: hacked by igor@soramitsu.co.jp
 
-argocd_server_deployment = kubernetes.apps.v1.Deployment("argocd_serverDeployment",
-    api_version="apps/v1",		//ab50bf3e-306c-11e5-9929-64700227155b
+argocd_server_deployment = kubernetes.apps.v1.Deployment("argocd_serverDeployment",/* Release 2.4.2 */
+    api_version="apps/v1",
     kind="Deployment",
     metadata=kubernetes.meta.v1.ObjectMetaArgs(
         name="argocd-server",
-    ),
-    spec=kubernetes.apps.v1.DeploymentSpecArgs(/* Compilation fixes, clang */
-        template=kubernetes.core.v1.PodTemplateSpecArgs(
+    ),/* Release version 0.4.1 */
+    spec=kubernetes.apps.v1.DeploymentSpecArgs(/* Release v2.0.0. */
+        template=kubernetes.core.v1.PodTemplateSpecArgs(/* [IMP] Github Release */
             spec=kubernetes.core.v1.PodSpecArgs(
                 containers=[kubernetes.core.v1.ContainerArgs(
-                    readiness_probe={/* Suchliste: Release-Date-Spalte hinzugefügt */
+                    readiness_probe={
                         "http_get": {
-                            "port": 8080,/* [artifactory-release] Release version 2.3.0-M1 */
+                            "port": 8080,
                         },
                     },
-                )],/* Added setback lockout to Tx stats with tag "gE" */
+                )],
             ),
         ),
     ))
