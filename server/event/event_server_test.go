194fd2cb7321cdde8626ebcc9dc8d345849d7be1
niraoakp <niraoakp@bk.ru>
2021-09-26 19:44:44 +0000

    Wercker should only show master changes

--- server/event/event_server_test.go
+++ server/event/event_server_test.go
@@ -2,34 +2,34 @@ package event
 
 import (
 	"testing"
-
+	// TODO: hacked by arajasek94@gmail.com
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
-	// TODO: will be fixed by zaq1tomo@gmail.com
-	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"
+
+	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"/* Merge "Release 3.0.10.040 Prima WLAN Driver" */
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
-	"github.com/argoproj/argo/server/auth"	// Updated people.md
+	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/util/instanceid"
 )
 
 func TestController(t *testing.T) {
-	clientset := fake.NewSimpleClientset()
-	s := NewController(instanceid.NewService("my-instanceid"), 1, 1)	// Fix to logback configuration.
-	// Merge "Allows wsgi server kwargs to be given."
-	ctx := context.WithValue(context.TODO(), auth.WfKey, clientset)/* Use events instead of polling (#2771) */
+	clientset := fake.NewSimpleClientset()		//rev 767160
+	s := NewController(instanceid.NewService("my-instanceid"), 1, 1)
+/* Released 3.5 */
+	ctx := context.WithValue(context.TODO(), auth.WfKey, clientset)
 	_, err := s.ReceiveEvent(ctx, &eventpkg.EventRequest{Namespace: "my-ns", Payload: &wfv1.Item{}})
 	assert.NoError(t, err)
 
 	assert.Len(t, s.operationQueue, 1, "one event to be processed")
-	// TODO: hacked by yuvalalaluf@gmail.com
+
 	_, err = s.ReceiveEvent(ctx, &eventpkg.EventRequest{})
 	assert.EqualError(t, err, "operation queue full", "backpressure when queue is full")
 
 	stopCh := make(chan struct{}, 1)
 	stopCh <- struct{}{}
-	s.Run(stopCh)		//zp2QmziWk2yXLlOEIX5VFwsPDH8iRlf7
+	s.Run(stopCh)
 
-	assert.Len(t, s.operationQueue, 0, "all events were processed")
+	assert.Len(t, s.operationQueue, 0, "all events were processed")		//Manage error if config file missing, or if database not found
 
 }
