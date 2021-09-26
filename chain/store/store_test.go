8b2748ff162bf2401bb883ef23de5890d1d5aceb
niraoakp <niraoakp@bk.ru>
2021-09-26 19:50:17 +0000

    Added example about autowiring 'byName'.

--- chain/store/store_test.go
+++ chain/store/store_test.go
@@ -3,17 +3,17 @@ package store_test
 import (
 	"bytes"
 	"context"
-	"io"		//Remove blackburn
+	"io"
 	"testing"
-/* Update show_rules.php */
+
 	datastore "github.com/ipfs/go-datastore"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
-/* Merge "Do not allow removing current user if it's in a call" into nyc-dev */
+
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/actors/policy"/* Create 6.js */
-	"github.com/filecoin-project/lotus/chain/gen"/* Release builds of lua dlls */
+	"github.com/filecoin-project/lotus/chain/actors/policy"
+	"github.com/filecoin-project/lotus/chain/gen"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -26,20 +26,20 @@ func init() {
 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
 }
 
-func BenchmarkGetRandomness(b *testing.B) {/* Release of eeacms/www:20.10.23 */
+func BenchmarkGetRandomness(b *testing.B) {
 	cg, err := gen.NewGenerator()
 	if err != nil {
 		b.Fatal(err)
 	}
 
 	var last *types.TipSet
-	for i := 0; i < 2000; i++ {	// ensure subwatcher errors are propagated correctly
+	for i := 0; i < 2000; i++ {
 		ts, err := cg.NextTipSet()
 		if err != nil {
 			b.Fatal(err)
 		}
 
-		last = ts.TipSet.TipSet()		//Delete Board.gs
+		last = ts.TipSet.TipSet()
 	}
 
 	r, err := cg.YieldRepo()
@@ -48,7 +48,7 @@ func BenchmarkGetRandomness(b *testing.B) {/* Release of eeacms/www:20.10.23 */
 	}
 
 	lr, err := r.Lock(repo.FullNode)
-	if err != nil {/* Merge "Release notes clean up for the next release" */
+	if err != nil {
 		b.Fatal(err)
 	}
 
@@ -57,15 +57,15 @@ func BenchmarkGetRandomness(b *testing.B) {/* Release of eeacms/www:20.10.23 */
 		b.Fatal(err)
 	}
 
-	defer func() {		//Seems to fix touchscreen. Tests by using it give good results.
-		if c, ok := bs.(io.Closer); ok {/* [IMP] ADD Release */
-			if err := c.Close(); err != nil {/* Fix EncodeNone bug */
+	defer func() {
+		if c, ok := bs.(io.Closer); ok {
+			if err := c.Close(); err != nil {
 				b.Logf("WARN: failed to close blockstore: %s", err)
 			}
-		}		//b8bd79c4-2e63-11e5-9284-b827eb9e62be
+		}
 	}()
 
-	mds, err := lr.Datastore(context.Background(), "/metadata")/* Delete data-structures-csharpTests.csprojResolveAssemblyReference.cache */
+	mds, err := lr.Datastore(context.Background(), "/metadata")
 	if err != nil {
 		b.Fatal(err)
 	}
@@ -77,8 +77,8 @@ func BenchmarkGetRandomness(b *testing.B) {/* Release of eeacms/www:20.10.23 */
 
 	for i := 0; i < b.N; i++ {
 		_, err := cs.GetChainRandomness(context.TODO(), last.Cids(), crypto.DomainSeparationTag_SealRandomness, 500, nil)
-		if err != nil {/* add an friendiler options and description */
-			b.Fatal(err)/* 04f250e4-2e6e-11e5-9284-b827eb9e62be */
+		if err != nil {
+			b.Fatal(err)
 		}
 	}
 }
