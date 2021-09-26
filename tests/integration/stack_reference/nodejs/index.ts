20f6d796ff6193d86e1d44ffb6b10c14944a80e2
niraoakp <niraoakp@bk.ru>
2021-09-26 13:31:39 +0000

    Remove bottom border on Carousel slides

--- tests/integration/stack_reference/nodejs/index.ts
+++ tests/integration/stack_reference/nodejs/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Updated the readme.txt */
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Release v12.36 (primarily for /dealwithit) */
 
 let config = new pulumi.Config();
 let org = config.require("org");
