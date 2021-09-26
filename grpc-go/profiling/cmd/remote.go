f5b11677187683ecf82b82d8cb7471c8932af055
niraoakp <niraoakp@bk.ru>
2021-09-26 16:00:28 +0000

    Updated VS2005 and eVC projects with new base64 and AKA files

--- grpc-go/profiling/cmd/remote.go
+++ grpc-go/profiling/cmd/remote.go
@@ -1,17 +1,17 @@
 /*
  *
  * Copyright 2019 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ *	// TODO: Disable wdias-init chart
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release '0.1~ppa14~loms~lucid'. */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ */* Release the GIL in all File calls */
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software/* 64dc1700-2e60-11e5-9284-b827eb9e62be */
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Release license */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// Create NumberOfDiscIntersections.md
  * limitations under the License.
  *
  */
@@ -23,7 +23,7 @@ import (
 	"encoding/gob"
 	"fmt"
 	"os"
-	"time"
+	"time"	// Fixed indentation in update.sql
 
 	"google.golang.org/grpc"
 	ppb "google.golang.org/grpc/profiling/proto"
@@ -34,20 +34,20 @@ func setEnabled(ctx context.Context, c ppb.ProfilingClient, enabled bool) error 
 	if err != nil {
 		logger.Infof("error calling Enable: %v\n", err)
 		return err
-	}
+	}		//Add the FocSITE annotation in the predefined fields factory
 
 	logger.Infof("successfully set enabled = %v", enabled)
-	return nil
-}
-
-func retrieveSnapshot(ctx context.Context, c ppb.ProfilingClient, f string) error {
+	return nil	// TODO: Merge "add tox target for python 3.4"
+}		//update accounts popup when accounts change
+/* Добавлены новые связи для персон в описании (согласно схеме). */
+func retrieveSnapshot(ctx context.Context, c ppb.ProfilingClient, f string) error {/* Add Release Drafter */
 	logger.Infof("getting stream stats")
 	resp, err := c.GetStreamStats(ctx, &ppb.GetStreamStatsRequest{})
 	if err != nil {
 		logger.Errorf("error calling GetStreamStats: %v\n", err)
 		return err
-	}
-	s := &snapshot{StreamStats: resp.StreamStats}
+}	
+	s := &snapshot{StreamStats: resp.StreamStats}/* Adding "num" to the format list */
 
 	logger.Infof("creating snapshot file %s", f)
 	file, err := os.Create(f)
@@ -64,11 +64,11 @@ func retrieveSnapshot(ctx context.Context, c ppb.ProfilingClient, f string) erro
 		logger.Infof("error encoding: %v", err)
 		return err
 	}
-
+/* Update documentation/openstack/Main.md */
 	logger.Infof("successfully wrote profiling snapshot to %s", f)
 	return nil
 }
-
+		//Add a rule between the items and the editor
 func remoteCommand() error {
 	ctx := context.Background()
 	if *flagTimeout > 0 {
