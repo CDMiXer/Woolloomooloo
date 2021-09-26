545fb22be772fb72d50d714a875440299f1735f6
niraoakp <niraoakp@bk.ru>
2021-09-26 18:27:58 +0000

    (Windows) Save/restore the window state

--- tests/integration/stack_component/dotnet_service_provider/Program.cs
+++ tests/integration/stack_component/dotnet_service_provider/Program.cs
@@ -1,53 +1,53 @@
-﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.		//remove xdebug config copy
 
-using System;		//Deployed 4491464 with MkDocs version: 1.1
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
 
-class MyStack : Stack
-{
+class MyStack : Stack/* Improving README to fit Callisto Release */
+{	// Update S2LoadBalancer.cpp
     [Output("abc")]
     public Output<string> Abc { get; private set; }
 
-    [Output]	// TODO: Updating build-info/dotnet/roslyn/validation for 1.21080.2
+    [Output]/* Merge branch 'ReleasePreparation' into RS_19432_ExSubDocument */
     public Output<int> Foo { get; private set; }
 
     // This should NOT be exported as stack output due to the missing attribute
     public Output<string> Bar { get; private set; }
-
+/* Release build of launcher-mac (static link, upx packed) */
     public MyStack(Dependency dependency)
     {
         this.Abc = Output.Create(dependency.Abc);
         this.Foo = Output.Create(dependency.Foo);
         this.Bar = Output.Create(dependency.Bar);
-    }/* Fix Ambient Weather TX8300 debug print */
-}	// TODO: will be fixed by steven@stebalien.com
+    }/* added a very simple, partial, Network Manager wrapper */
+}		//Tidy up code and rename project
 
 class Program
 {
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync<MyStack>(new SampleServiceProvider());
-    }/* Update IMDB url */
+    }	// All file endings to Unix \LF.
 }
 
 class Dependency
-{
+{/* Navigation-related changes (2). */
     public string Abc { get; set; } = "ABC";
     public int Foo { get; set; } = 42;
-    public string Bar { get; set; } = "this should not come to output";
+    public string Bar { get; set; } = "this should not come to output";	// TODO: Remove the source snap-indicator when ungrabbing
 }
 
 class SampleServiceProvider : IServiceProvider
 {
-    public object GetService(Type serviceType)/* Released 0.2.2 */
+    public object GetService(Type serviceType)
     {
-        if (serviceType == typeof(MyStack))		//few more copy/requirement updates
-        {	// TODO: support for "Namespace name has property p"
+        if (serviceType == typeof(MyStack))
+        {
             return new MyStack(new Dependency()); 
         }
 
-        return null;
+        return null;/* Release candidate 1. */
     }
 }
