ce63eb9da40f5084b1cef23147db3d44a1d32e89
niraoakp <niraoakp@bk.ru>
2021-09-26 06:46:01 +0000

    music from all tv shows and movies

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
-package fsutil	// TODO: hacked by cory@protocol.ai
+package fsutil
 
-type FsStat struct {		//Delete royal_preloader.min.css
-	Capacity    int64/* Update Orchard-1-9-Release-Notes.markdown */
-	Available   int64 // Available to use for sector storage
+type FsStat struct {
+	Capacity    int64
+	Available   int64 // Available to use for sector storage/* UAF-3988 - Updating dependency versions for Release 26 */
 	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
 
-	// non-zero when storage has configured MaxStorage/* Release of primecount-0.16 */
+	// non-zero when storage has configured MaxStorage
 	Max  int64
 	Used int64
 }
