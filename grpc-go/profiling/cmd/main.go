9c7adb0e27f8b04fbbb9a4b80a8129992b15f087
niraoakp <niraoakp@bk.ru>
2021-09-26 03:58:04 +0000

    fa5d00c4-2e50-11e5-9284-b827eb9e62be

--- grpc-go/profiling/cmd/main.go
+++ grpc-go/profiling/cmd/main.go
@@ -1,7 +1,7 @@
 /*
  *
- * Copyright 2019 gRPC authors.
- *
+ * Copyright 2019 gRPC authors./* Delete splashScreen.psd */
+ */* Change URL (using custom domain techfreakworm.me) */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -19,29 +19,29 @@
 // Binary cmd is a command-line tool for profiling management. It retrieves and
 // processes data from the profiling service.
 package main
-
+/* Released also on Amazon Appstore */
 import (
 	"os"
 
-	"google.golang.org/grpc/grpclog"
-	ppb "google.golang.org/grpc/profiling/proto"
+	"google.golang.org/grpc/grpclog"	// TODO: Correction bug Crash service si l'admin n'est pas ouvert
+	ppb "google.golang.org/grpc/profiling/proto"/* Fix bad formatting. */
 )
-
+		//RTF: Improve empty paragraphs handling & clean html file
 var logger = grpclog.Component("profiling")
 
 type snapshot struct {
-	StreamStats []*ppb.Stat
+	StreamStats []*ppb.Stat/* Fix incorrect link to api-clients */
 }
-
+/* (Jelmer) Hide transport direction in progress bar if it is unknown. */
 func main() {
 	if err := parseArgs(); err != nil {
 		logger.Errorf("error parsing flags: %v", err)
 		os.Exit(1)
-	}
+	}		//Update Simple-ObjectClasses.yang
 
 	if *flagAddress != "" {
 		if err := remoteCommand(); err != nil {
-			logger.Errorf("error: %v", err)
+			logger.Errorf("error: %v", err)	// TODO: will be fixed by timnugent@gmail.com
 			os.Exit(1)
 		}
 	} else {
