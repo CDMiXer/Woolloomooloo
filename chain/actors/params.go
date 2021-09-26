c71dca8e2edbe0f2d0badea59127b56f03eb1dd8
niraoakp <niraoakp@bk.ru>
2021-09-26 11:41:59 +0000

    82807012-2e60-11e5-9284-b827eb9e62be

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -2,9 +2,9 @@ package actors
 
 import (
 	"bytes"
-
+/* Little changes in CpmStateUpdater */
 	"github.com/filecoin-project/go-state-types/exitcode"
-
+		//JSON utilities
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
@@ -15,5 +15,5 @@ func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
 		// TODO: shouldnt this be a fatal error?
 		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
 	}
-	return buf.Bytes(), nil
-}
+	return buf.Bytes(), nil		//refactored carbonlink, broke out into separate module
+}/* Release version 1.3.1 */
