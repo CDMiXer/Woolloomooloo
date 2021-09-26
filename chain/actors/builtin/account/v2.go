aba83695c5c97ea4a1e027d88dbb7c3078b75ee4
niraoakp <niraoakp@bk.ru>
2021-09-26 11:21:59 +0000

    Merge "ListTags: Include timestamp in tag info"

--- chain/actors/builtin/account/v2.go
+++ chain/actors/builtin/account/v2.go
@@ -1,30 +1,30 @@
 package account
-
+	// TODO: hacked by remco@dutchcoders.io
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-
+		//Algumas atualizações.
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-/* dbd2c0ee-2e46-11e5-9284-b827eb9e62be */
+
 	account2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/account"
 )
-	// TODO: will be fixed by hugomrdias@gmail.com
-var _ State = (*state2)(nil)	// bleutrade fetchMyTrades renamed to fetchOrderTrades
+
+var _ State = (*state2)(nil)	// TODO: update for creating pdf with arial font
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}		//Restmock should be RestMock
-	return &out, nil	// TODO: - Added new 'Auth' controller include
+	}
+	return &out, nil
 }
 
 type state2 struct {
-	account2.State	// Add additional points.
+	account2.State
 	store adt.Store
 }
 
-func (s *state2) PubkeyAddress() (address.Address, error) {/* 1.5.0 Release */
+func (s *state2) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
 }
