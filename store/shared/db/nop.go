92f8a3763000d407dc3a4756f1149ebcb22c971c
niraoakp <niraoakp@bk.ru>
2021-09-26 11:23:20 +0000

    Update release notes for the version 2.1

--- store/shared/db/nop.go
+++ store/shared/db/nop.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by boringland@protonmail.ch
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Merge maria-5.3-mwl248 -> 5.5 = maria-5.5-mwl248. */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//remove examples as submodules.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fix redefinition warning
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package db
 
-type nopLocker struct{}		//Merge "Fix back key handling for playback controls." into lmp-mr1-dev
-
-func (nopLocker) Lock()    {}/* Release of eeacms/jenkins-slave-dind:19.03-3.25 */
+type nopLocker struct{}
+	// TODO: Update sidebar.hbs
+func (nopLocker) Lock()    {}	// TODO: CJS / Browserify
 func (nopLocker) Unlock()  {}
 func (nopLocker) RLock()   {}
 func (nopLocker) RUnlock() {}
