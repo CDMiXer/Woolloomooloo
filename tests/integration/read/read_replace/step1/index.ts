36a70d1671c8dcbefe011e3194e9732e1a881639
niraoakp <niraoakp@bk.ru>
2021-09-26 12:32:29 +0000

    Export c.a.e.js.contentassist package

--- tests/integration/read/read_replace/step1/index.ts
+++ tests/integration/read/read_replace/step1/index.ts
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+//	// TODO: will be fixed by nagydani@epointsystem.org
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Add a few fixes and tweaks to splay map and a test for the remove issue
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Release 1.0.0.M4 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,6 +15,6 @@
 import { Resource } from "./resource";
 
 // Setup: Resource A is external, Resource B is not.
-const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
-const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
+const a = new Resource("a", { state: 42 }, { id: "existing-id"} );	// TODO: Fixed some namespace bugs.
+const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});/* [artifactory-release] Release version 3.1.0.RC1 */
 
