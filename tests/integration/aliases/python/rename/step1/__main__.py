9921bce285c2385624464db63e5ff9f7e9a2784d
niraoakp <niraoakp@bk.ru>
2021-09-26 20:38:02 +0000

    add travis-ci build status badge

--- tests/integration/aliases/python/rename/step1/__main__.py
+++ tests/integration/aliases/python/rename/step1/__main__.py
@@ -1,10 +1,10 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Added a templateRoot option to the Engine. Also added tests. */
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)/* [FIX] calendar-picker (phpboost) */
+        super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #1 - rename a resource
 res1 = Resource1("res1")
