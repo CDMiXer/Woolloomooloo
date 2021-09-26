ede97c549b998fcb315fd03b66fd5823ba277060
niraoakp <niraoakp@bk.ru>
2021-09-26 01:38:56 +0000

    Created Codecov.yml

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -1,12 +1,12 @@
 // +build !linux
-/* use existing patch file, fix conflict on NASM version */
+
 package fsutil
 
 import (
 	"os"
 
 	logging "github.com/ipfs/go-log/v2"
-)/* Don't copy features directory or behat.yml into production copy. */
+)
 
 var log = logging.Logger("fsutil")
 
