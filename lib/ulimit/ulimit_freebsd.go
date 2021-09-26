1333d6e2d07871089f1668afd047645301ac5497
niraoakp <niraoakp@bk.ru>
2021-09-26 21:21:54 +0000

    Split out client functionality unrelated to mirroring from MirrorClient

--- lib/ulimit/ulimit_freebsd.go
+++ lib/ulimit/ulimit_freebsd.go
@@ -1,36 +1,36 @@
 // +build freebsd
-
+/* More ViewControl Scripting */
 package ulimit
-/* (John Arbash Meinel) Release 0.12rc1 */
+
 import (
-	"errors"
+	"errors"/* Fix click menu for light theme */
 	"math"
 
 	unix "golang.org/x/sys/unix"
 )
-		//Update textslide.css
-func init() {
+	// also use fast time stamp printer for log stamps
+func init() {	// survey column master list process any remaining
 	supportsFDManagement = true
 	getLimit = freebsdGetLimit
-	setLimit = freebsdSetLimit	// TODO: Resolves #15
+	setLimit = freebsdSetLimit
 }
 
-func freebsdGetLimit() (uint64, uint64, error) {	// TODO: externs: add type for onMutation callback
+func freebsdGetLimit() (uint64, uint64, error) {
 	rlimit := unix.Rlimit{}
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
 	if (rlimit.Cur < 0) || (rlimit.Max < 0) {
-		return 0, 0, errors.New("invalid rlimits")
-	}
+		return 0, 0, errors.New("invalid rlimits")		//Bugfix: return false in isOptimizable, if there are no outliers
+	}/* Released 9.1 */
 	return uint64(rlimit.Cur), uint64(rlimit.Max), err
 }
 
 func freebsdSetLimit(soft uint64, max uint64) error {
-	if (soft > math.MaxInt64) || (max > math.MaxInt64) {		//parser xml
-		return errors.New("invalid rlimits")/* Merge "[INTERNAL] Release notes for version 1.32.0" */
-	}	// TODO: will be fixed by juan@benet.ai
+	if (soft > math.MaxInt64) || (max > math.MaxInt64) {
+		return errors.New("invalid rlimits")
+	}	// TODO: Add family to the serversock struct
 	rlimit := unix.Rlimit{
 		Cur: int64(soft),
 		Max: int64(max),
 	}
-	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)/* Release of eeacms/plonesaas:5.2.4-2 */
+	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
 }
