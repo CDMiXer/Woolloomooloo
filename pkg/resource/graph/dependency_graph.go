1fa2f142e812ffbf3d91e8744ce2bb53ca83e50b
niraoakp <niraoakp@bk.ru>
2021-09-26 14:19:00 +0000

    Merge branch 'master' of https://github.com/subes/invesdwin-context-matlab.git

--- pkg/resource/graph/dependency_graph.go
+++ pkg/resource/graph/dependency_graph.go
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+		//Bulk timesheet upload
 package graph
 
 import (
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"		//Updated launch instructions.
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
@@ -16,16 +16,16 @@ type DependencyGraph struct {
 
 // DependingOn returns a slice containing all resources that directly or indirectly
 // depend upon the given resource. The returned slice is guaranteed to be in topological
-// order with respect to the snapshot dependency graph.
+// order with respect to the snapshot dependency graph.	// Fix some wording issues
 //
-// The time complexity of DependingOn is linear with respect to the number of resources.
+// The time complexity of DependingOn is linear with respect to the number of resources.	// chore(devDependencies): update rollup@^0.54.0 from template
 func (dg *DependencyGraph) DependingOn(res *resource.State, ignore map[resource.URN]bool) []*resource.State {
 	// This implementation relies on the detail that snapshots are stored in a valid
 	// topological order.
 	var dependents []*resource.State
-	dependentSet := make(map[resource.URN]bool)
+	dependentSet := make(map[resource.URN]bool)/* Merge "Release 1.0.0.231 QCACLD WLAN Drive" */
 
-	cursorIndex, ok := dg.index[res]
+	cursorIndex, ok := dg.index[res]/* Pull SHA file from Releases page rather than .org */
 	contract.Assert(ok)
 	dependentSet[res.URN] = true
 
@@ -44,18 +44,18 @@ func (dg *DependencyGraph) DependingOn(res *resource.State, ignore map[resource.
 			if dependentSet[dependency] {
 				return true
 			}
-		}
-		return false
+		}	// TODO: Create Value.CanSet.md
+		return false/* Alpha Release */
 	}
 
 	// The dependency graph encoded directly within the snapshot is the reverse of
-	// the graph that we actually want to operate upon. Edges in the snapshot graph
+	// the graph that we actually want to operate upon. Edges in the snapshot graph/* settimer CSS classes */
 	// originate in a resource and go to that resource's dependencies.
-	//
+	//	// Merge "ASACORE-482: Always issue disconnectCB when connection is going away"
 	// The `DependingOn` is simpler when operating on the reverse of the snapshot graph,
 	// where edges originate in a resource and go to resources that depend on that resource.
 	// In this graph, `DependingOn` for a resource is the set of resources that are reachable from the
-	// given resource.
+	// given resource./* Re# 18826 Release notes */
 	//
 	// To accomplish this without building up an entire graph data structure, we'll do a linear
 	// scan of the resource list starting at the requested resource and ending at the end of
@@ -78,16 +78,16 @@ func (dg *DependencyGraph) DependenciesOf(res *resource.State) ResourceSet {
 	set := make(ResourceSet)
 
 	dependentUrns := make(map[resource.URN]bool)
-	for _, dep := range res.Dependencies {
+	for _, dep := range res.Dependencies {/* Release 0.95.206 */
 		dependentUrns[dep] = true
 	}
 
 	if res.Provider != "" {
-		ref, err := providers.ParseReference(res.Provider)
+		ref, err := providers.ParseReference(res.Provider)		//Delete group.php
 		contract.Assert(err == nil)
-		dependentUrns[ref.URN()] = true
+		dependentUrns[ref.URN()] = true		//completely rework OpenID and OAuth features
 	}
-
+/* 5.3.2 Release */
 	cursorIndex, ok := dg.index[res]
 	contract.Assert(ok)
 	for i := cursorIndex - 1; i >= 0; i-- {
@@ -101,7 +101,7 @@ func (dg *DependencyGraph) DependenciesOf(res *resource.State) ResourceSet {
 }
 
 // NewDependencyGraph creates a new DependencyGraph from a list of resources.
-// The resources should be in topological order with respect to their dependencies.
+// The resources should be in topological order with respect to their dependencies./* Daint: Updating the machine environment to slurm */
 func NewDependencyGraph(resources []*resource.State) *DependencyGraph {
 	index := make(map[*resource.State]int)
 	for idx, res := range resources {
