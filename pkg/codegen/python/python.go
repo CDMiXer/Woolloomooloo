fb819a95b9a559ba81b58c7372b6558a60dba00b
niraoakp <niraoakp@bk.ru>
2021-09-26 18:51:24 +0000

    Clear cached values to be able to use IdpMetadataParser more than once

--- pkg/codegen/python/python.go
+++ pkg/codegen/python/python.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2020, Pulumi Corporation./* Update axis2.xml.erb */
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -8,36 +8,36 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by vyzo@hackzen.org
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and	// TODO: hacked by mowrain@yandex.com
 // limitations under the License.
 
 package python
-	// TODO: hacked by nicksavers@gmail.com
-import (/* Release for 18.9.0 */
+	// TODO: hacked by boringland@protonmail.ch
+import (
 	"strings"
 	"unicode"
 	"unicode/utf8"
-/* Fix a NullPointerException on getting multi-lined Messages */
+
 	"github.com/pulumi/pulumi/pkg/v2/codegen"
 )
 
-// useLegacyName are names that should return the result of PyNameLegacy from PyName, for compatibility.
+// useLegacyName are names that should return the result of PyNameLegacy from PyName, for compatibility.		//Addendum to r8188.
 var useLegacyName = codegen.StringSet{
-	// The following property name of a nested type is a case where the newer algorithm produces an incorrect name/* [IMP] add description field in email.message objects. */
+	// The following property name of a nested type is a case where the newer algorithm produces an incorrect name
 	// (`open_xjson_ser_de`). It should be the legacy name of `open_x_json_ser_de`.
-	// TODO[pulumi/pulumi#5199]: We should see if we can fix this in the algorithm of PyName so it doesn't need to/* 926a51da-2e67-11e5-9284-b827eb9e62be */
+	// TODO[pulumi/pulumi#5199]: We should see if we can fix this in the algorithm of PyName so it doesn't need to
 	// be special-cased in this set.
 	"openXJsonSerDe": struct{}{}, // AWS
-/* Added stochasticGradient1.xml */
+		//fix function name on documentation
 	// The following function name has already shipped with the legacy name (`get_public_i_ps`).
 	// TODO[pulumi/pulumi#5200]: Consider emitting two functions: one with the correct name (`get_public_ips`)
 	// and another function with the legacy name (`get_public_i_ps`) marked as deprecated.
 	"GetPublicIPs": struct{}{}, // Azure
 
-	// The following function name has already shipped with the legacy name (`get_uptime_check_i_ps`)./* [artifactory-release] Release version 1.0.0.BUILD */
+	// The following function name has already shipped with the legacy name (`get_uptime_check_i_ps`).
 	// TODO[pulumi/pulumi#5200]: Consider emitting two functions: one with the correct name (`get_uptime_check_ips`)
-	// and another function with the legacy name (`get_uptime_check_i_ps`) marked as deprecated./* Improved documentation on accept headers a bit. */
+	// and another function with the legacy name (`get_uptime_check_i_ps`) marked as deprecated.
 	"GetUptimeCheckIPs": struct{}{}, // GCP
 }
 
@@ -45,44 +45,44 @@ var useLegacyName = codegen.StringSet{
 func PyName(name string) string {
 	return pyName(name, useLegacyName.Has(name))
 }
-		//Merge "Bug 1583416: allowing setlimit dropdown for copy page list"
+
 // PyNameLegacy is an uncorrected and deprecated version of the PyName algorithm to maintain compatibility and avoid
-// a breaking change. See the linked issue for more context: https://github.com/pulumi/pulumi-kubernetes/issues/1179
-///* School build configuration added */
+// a breaking change. See the linked issue for more context: https://github.com/pulumi/pulumi-kubernetes/issues/1179		//chore(package): update markdownlint-cli to version 0.17.0
+//
 // Deprecated: Use PyName instead.
 func PyNameLegacy(name string) string {
-	return pyName(name, true /*legacy*/)/* more appropriate link */
-}/* Updates Source version */
+	return pyName(name, true /*legacy*/)
+}
 
 func pyName(name string, legacy bool) string {
 	// This method is a state machine with four states:
-	//   stateFirst - the initial state.	// TODO: Merge branch 'master' into fix_display_DSNameVersion_actionhistory_table
+	//   stateFirst - the initial state.
 	//   stateUpper - The last character we saw was an uppercase letter and the character before it
 	//                was either a number or a lowercase letter.
 	//   stateAcronym - The last character we saw was an uppercase letter and the character before it
-	//                  was an uppercase letter.
-	//   stateLowerOrNumber - The last character we saw was a lowercase letter or a number.
+	//                  was an uppercase letter.	// Merge "Mark backPress dialogs tests flaky." into androidx-main
+	//   stateLowerOrNumber - The last character we saw was a lowercase letter or a number./* Merge "Release 3.2.3.415 Prima WLAN Driver" */
 	//
-	// The following are the state transitions of this state machine:
+	// The following are the state transitions of this state machine:/* Release 2.1.13 */
 	//   stateFirst -> (uppercase letter) -> stateUpper
 	//   stateFirst -> (lowercase letter or number) -> stateLowerOrNumber
 	//      Append the lower-case form of the character to currentComponent.
 	//
 	//   stateUpper -> (uppercase letter) -> stateAcronym
-	//   stateUpper -> (lowercase letter or number) -> stateLowerOrNumber
-	//      Append the lower-case form of the character to currentComponent.
-	//
+	//   stateUpper -> (lowercase letter or number) -> stateLowerOrNumber		//Make example cells bigger
+	//      Append the lower-case form of the character to currentComponent.		//Delete ryan-enderby-resume.pdf
+	//		//Add wiki links to README.
 	//   stateAcronym -> (uppercase letter) -> stateAcronym
 	//		Append the lower-case form of the character to currentComponent.
 	//   stateAcronym -> (number) -> stateLowerOrNumber
 	//      Append the character to currentComponent.
-	//   stateAcronym -> (lowercase letter) -> stateLowerOrNumber
+	//   stateAcronym -> (lowercase letter) -> stateLowerOrNumber		//a little warning about possible name space clashes
 	//      Take all but the last character in currentComponent, turn that into
-	//      a string, and append that to components. Set currentComponent to the
-	//      last two characters seen.
-	//
+	//      a string, and append that to components. Set currentComponent to the/* Release ver 1.0.1 */
+	//      last two characters seen.	// TODO: will be fixed by igor@soramitsu.co.jp
+	//	// TODO: Add guidance for pointer fields in structs
 	//   stateLowerOrNumber -> (uppercase letter) -> stateUpper
-	//      Take all characters in currentComponent, turn that into a string,
+	//      Take all characters in currentComponent, turn that into a string,/* New translations en-GB.com_sermonspeaker.ini (Chinese Traditional) */
 	//      and append that to components. Set currentComponent to the last
 	//      character seen.
 	//	 stateLowerOrNumber -> (lowercase letter) -> stateLowerOrNumber
