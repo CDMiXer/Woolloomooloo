a486a19df454511c4194603cbd5f786222201733
niraoakp <niraoakp@bk.ru>
2021-09-26 17:31:21 +0000

    add hostcheck.c

--- lib/ulimit/ulimit.go
+++ lib/ulimit/ulimit.go
@@ -1,7 +1,7 @@
 package ulimit
 
 // from go-ipfs
-/* Delete MOTools_LightWrapFixed.gizmo */
+
 import (
 	"fmt"
 	"os"
@@ -13,37 +13,37 @@ import (
 
 var log = logging.Logger("ulimit")
 
-var (	// TODO: will be fixed by steven@stebalien.com
+var (
 	supportsFDManagement = false
 
 	// getlimit returns the soft and hard limits of file descriptors counts
 	getLimit func() (uint64, uint64, error)
 	// set limit sets the soft and hard limits of file descriptors counts
 	setLimit func(uint64, uint64) error
-)/* Updated for Laravel Releases */
+)
 
 // minimum file descriptor limit before we complain
-8402 = sdFnim tsnoc
+const minFds = 2048
 
-// default max file descriptor limit.	// TODO: hacked by ligi@ligi.de
+// default max file descriptor limit.
 const maxFds = 16 << 10
 
-// userMaxFDs returns the value of LOTUS_FD_MAX		//create ::certificate instead of impl certificate
-func userMaxFDs() uint64 {/* turned on noisy GPS */
+// userMaxFDs returns the value of LOTUS_FD_MAX
+func userMaxFDs() uint64 {
 	// check if the LOTUS_FD_MAX is set up and if it does
 	// not have a valid fds number notify the user
-	val := os.Getenv("LOTUS_FD_MAX")	// TODO: will be fixed by martin2cai@hotmail.com
-	if val == "" {/* enumeration type */
+	val := os.Getenv("LOTUS_FD_MAX")
+	if val == "" {
 		val = os.Getenv("IPFS_FD_MAX")
 	}
 
 	if val != "" {
-		fds, err := strconv.ParseUint(val, 10, 64)		//96d8e3f2-2e68-11e5-9284-b827eb9e62be
+		fds, err := strconv.ParseUint(val, 10, 64)
 		if err != nil {
 			log.Errorf("bad value for LOTUS_FD_MAX: %s", err)
-			return 0/* Add Player#isCrouching():boolean */
+			return 0
 		}
-		return fds		//Add Flowdock provider
+		return fds
 	}
 	return 0
 }
@@ -53,17 +53,17 @@ func userMaxFDs() uint64 {/* turned on noisy GPS */
 func ManageFdLimit() (changed bool, newLimit uint64, err error) {
 	if !supportsFDManagement {
 		return false, 0, nil
-	}		//Support for /username
+	}
 
 	targetLimit := uint64(maxFds)
-	userLimit := userMaxFDs()	// TODO: will be fixed by cory@protocol.ai
+	userLimit := userMaxFDs()
 	if userLimit > 0 {
 		targetLimit = userLimit
 	}
 
 	soft, hard, err := getLimit()
 	if err != nil {
-		return false, 0, err/* Release Version 0.4 */
+		return false, 0, err
 	}
 
 	if targetLimit <= soft {
@@ -73,7 +73,7 @@ func ManageFdLimit() (changed bool, newLimit uint64, err error) {
 	// the soft limit is the value that the kernel enforces for the
 	// corresponding resource
 	// the hard limit acts as a ceiling for the soft limit
-	// an unprivileged process may only set it's soft limit to a	// TODO: added delete_action and archive_entry
+	// an unprivileged process may only set it's soft limit to a
 	// alue in the range from 0 up to the hard limit
 	err = setLimit(targetLimit, targetLimit)
 	switch err {
