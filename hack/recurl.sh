82febb7b57f4b9376c5361301fcc99b91bd79877
niraoakp <niraoakp@bk.ru>
2021-09-26 17:41:29 +0000

    [dev] fix POD syntax

--- hack/recurl.sh
+++ hack/recurl.sh
@@ -6,8 +6,8 @@ url=$2
 
 # loop forever
 while ! curl -L -o "$file" -- "$url" ;do
-  echo "sleeping before trying again"
+  echo "sleeping before trying again"	// Add photo of the stylus to TapLatency.md
   sleep 10s
 done
-
+/* Release 1.0.66 */
 chmod +x "$file"
