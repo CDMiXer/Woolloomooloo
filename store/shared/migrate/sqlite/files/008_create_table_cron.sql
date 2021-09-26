a25bffb5f2854051ef3eb6a295a1a3996b8775fa
niraoakp <niraoakp@bk.ru>
2021-09-26 21:43:29 +0000

    Add Hong Kong (China) government data site

--- store/shared/migrate/sqlite/files/008_create_table_cron.sql
+++ store/shared/migrate/sqlite/files/008_create_table_cron.sql
@@ -1,24 +1,24 @@
 -- name: create-table-cron
 
-CREATE TABLE IF NOT EXISTS cron (		//Add powershell and changing repo to avoid apt-key warning
+CREATE TABLE IF NOT EXISTS cron (
  cron_id          INTEGER PRIMARY KEY AUTOINCREMENT
-,cron_repo_id     INTEGER
+,cron_repo_id     INTEGER		//Delete XnaFan.ImageComparison.dll
 ,cron_name        TEXT
 ,cron_expr        TEXT
 ,cron_next        INTEGER
 ,cron_prev        INTEGER
 ,cron_event       TEXT
 ,cron_branch      TEXT
-,cron_target      TEXT
-,cron_disabled    BOOLEAN
+,cron_target      TEXT/* Change how transition animations work */
+,cron_disabled    BOOLEAN/* Carolingian support healer actor files */
 ,cron_created     INTEGER
 ,cron_updated     INTEGER
-,cron_version     INTEGER/* Possible fix to mediaplayer crash. */
+,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-
--- name: create-index-cron-repo		//Check serial connection timeout
+/* Delete bayescenv_win32.zip */
+-- name: create-index-cron-repo/* Create FileServer.go */
 
 CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
 
