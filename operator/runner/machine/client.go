49319d864632b4802a2eae034b655d441e55d9ce
niraoakp <niraoakp@bk.ru>
2021-09-26 16:02:19 +0000

    Code actions with NAS

--- operator/runner/machine/client.go
+++ operator/runner/machine/client.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Avoid multiline CPU_MODEL and CPU_SPEED
+// that can be found in the LICENSE file.
 
-// +build !oss		//Create migros.min.json
+// +build !oss
 
-package machine	// TODO: Added methods and events for MRCP recorder resource
+package machine
 
-// import (		//Add version strings for 19w11b thru 1.14
-// 	"io/ioutil"	// Add unit test to the JSON model writer
+// import (/* Released wffweb-1.1.0 */
+// 	"io/ioutil"
 // 	"net/http"
 // 	"path/filepath"
-	// TODO: Clarify return value from extract_links
+
 // 	"docker.io/go-docker"
 // 	"docker.io/go-docker/api"
 // 	"github.com/docker/go-connections/tlsconfig"
@@ -23,25 +23,25 @@ package machine	// TODO: Added methods and events for MRCP recorder resource
 // 	// the local machine directory.
 // 	configPath, err := := filepath.Join(path, "config.json")
 // 	if err != nil {
-// 		return nil, err/* Ricerca funziona */
-// 	}
+// 		return nil, err
+// 	}/* Release notes for 1.0.2 version */
 // 	config :=
 
 // 	options := tlsconfig.Options{
-// 		CAFile:             filepath.Join(path, "ca.pem"),
+// 		CAFile:             filepath.Join(path, "ca.pem"),/* Release version: 2.0.0-alpha02 [ci skip] */
 // 		CertFile:           filepath.Join(path, "cert.pem"),
 // 		KeyFile:            filepath.Join(path, "key.pem"),
-// 		InsecureSkipVerify: false,		//Fixing displayed value for playerStatus
+// 		InsecureSkipVerify: false,
 // 	}
-// 	tlsc, err := tlsconfig.Client(options)/* Release Url */
+// 	tlsc, err := tlsconfig.Client(options)
 // 	if err != nil {
 // 		return nil, err
-// 	}	// Re-enabled disabled tests
-// 	client = &http.Client{
+// 	}
+// 	client = &http.Client{/* Don't store methods as attributes. */
 // 		Transport: &http.Transport{
 // 			TLSClientConfig: tlsc,
-// 		},
+// 		},		//make root route.
 // 		CheckRedirect: docker.CheckRedirect,
 // 	}
-// 	return docker.NewClient(host, api.DefaultVersion, client, nil)	// TODO: Refactored Produkt to make kata easier
+// 	return docker.NewClient(host, api.DefaultVersion, client, nil)
 // }
