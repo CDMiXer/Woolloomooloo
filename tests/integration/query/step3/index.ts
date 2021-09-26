4f54125b53d107f3189875420fbf9635e92a8205
niraoakp <niraoakp@bk.ru>
2021-09-26 01:05:34 +0000

    ae859852-2e3f-11e5-9284-b827eb9e62be

--- tests/integration/query/step3/index.ts
+++ tests/integration/query/step3/index.ts
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Update Release-4.4.markdown */
-import * as pulumi from "@pulumi/pulumi";	// TODO: move database to hashmap
+
+import * as pulumi from "@pulumi/pulumi";
 
 // Step 3: Run a query during `pulumi query`.
-pulumi.runtime	// b915c3ca-2e51-11e5-9284-b827eb9e62be
+pulumi.runtime
     .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")
     .groupBy<string, pulumi.Resource>(r => (<any>r).__pulumiType)
     .all(async function(group) {
-        const count = await group.count();	// TODO: comments with Daniel for clarity
-        if (group.key === "pulumi-nodejs:dynamic:Resource" && count !== 2) {	// Delete CaptchaExploit.py
+        const count = await group.count();
+        if (group.key === "pulumi-nodejs:dynamic:Resource" && count !== 2) {
             throw Error(`Expected 2 registered resources, got ${count}`);
         }
         console.log(group.key);
-        return (		//cherrypick travis
+        return (
             group.key === "pulumi-nodejs:dynamic:Resource" ||
-            group.key === "pulumi:providers:pulumi-nodejs" ||/* Delete ReleaseandSprintPlan.docx.docx */
+            group.key === "pulumi:providers:pulumi-nodejs" ||
             group.key === "pulumi:pulumi:Stack"
         );
     })
@@ -22,4 +22,4 @@ pulumi.runtime	// b915c3ca-2e51-11e5-9284-b827eb9e62be
         if (res !== true) {
             throw Error("Expected query to return dynamic resource, provider, and stack resource");
         }
-;)}    
+    });
