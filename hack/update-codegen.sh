08887b11780426c5a70667a1c58c51a33dd249fa
niraoakp <niraoakp@bk.ru>
2021-09-26 21:20:21 +0000

    Corrected link to scripts/readme.md

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bash/* Released reLexer.js v0.1.1 */
 set -eux -o pipefail
 
-bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \/* nav_msg: Add comment to explain how update_bit_sync works. */
+bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
   "deepcopy,client,informer,lister" \
   github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \
   workflow:v1alpha1 \
