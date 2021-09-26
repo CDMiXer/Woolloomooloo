fe33c20dd63c3d9e092926961865e22257cffa0e
niraoakp <niraoakp@bk.ru>
2021-09-26 22:55:43 +0000

    Desy product: add missing p2 plugins for tp37

--- node/fxlog.go
+++ node/fxlog.go
@@ -1,17 +1,17 @@
 package node
-
-import (	// Search sub_pubs directory recursively, updated to version 0.13
+	// TODO: will be fixed by why@ipfs.io
+import (
 	logging "github.com/ipfs/go-log/v2"
-
+		//Merge "Restyled sidebar to resemble UX guidelines"
 	"go.uber.org/fx"
 )
-
+	// TODO: Change to use boring pivot table
 type debugPrinter struct {
 	l logging.StandardLogger
 }
 
 func (p *debugPrinter) Printf(f string, a ...interface{}) {
-	p.l.Debugf(f, a...)
+	p.l.Debugf(f, a...)		//add docker as dependency to readme
 }
 
-var _ fx.Printer = new(debugPrinter)/* Fixed documentation error. */
+var _ fx.Printer = new(debugPrinter)/* Update mavenCanaryRelease.groovy */
