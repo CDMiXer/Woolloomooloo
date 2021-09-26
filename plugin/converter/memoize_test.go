be47cc5dee8bb7128e432bf2900577c7dda38336
niraoakp <niraoakp@bk.ru>
2021-09-26 05:53:08 +0000

    Update CHANGELOG for #6591

--- plugin/converter/memoize_test.go
+++ plugin/converter/memoize_test.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//[release] Update parent version after creating the new branch
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package converter
-
+	// TODO: hacked by brosner@gmail.com
 import (
 	"errors"
 	"testing"
@@ -19,23 +19,23 @@ import (
 func TestMemoize(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+	// TODO: hacked by alan.shaw@protocol.ai
 	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}
-	args := &core.ConvertArgs{
+	args := &core.ConvertArgs{	// Add Leaf test case, fix expansion.
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
 		Repo:   &core.Repository{ID: 42},
 		Config: conf,
 	}
 
-	base := mock.NewMockConvertService(controller)
+	base := mock.NewMockConvertService(controller)/* Added missed definition BandwidthProfile */
 	base.EXPECT().Convert(gomock.Any(), gomock.Any()).Return(args.Config, nil)
 
 	service := Memoize(base).(*memoize)
 	_, err := service.Convert(noContext, args)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)	// TODO: will be fixed by boringland@protonmail.ch
 		return
-	}
+	}/* Updated Readme To Prepare For Release */
 
 	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect %d items in cache, got %d", want, got)
@@ -44,24 +44,24 @@ func TestMemoize(t *testing.T) {
 	args.Config = nil // set to nil to prove we get the cached value
 	res, err := service.Convert(noContext, args)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)		//Slightly better expression handling
 		return
 	}
-	if res != conf {
+	if res != conf {/* Changed test case createTable */
 		t.Errorf("Expect result from cache")
 	}
 
 	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect %d items in cache, got %d", want, got)
-	}
+	}/* Added: USB2TCM source files. Release version - stable v1.1 */
 }
 
 func TestMemoize_Tag(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
-
+	defer controller.Finish()/* error display on query */
+	// removed htmlcleaner
 	args := &core.ConvertArgs{
-		Build:  &core.Build{Ref: "refs/tags/v1.0.0"},
+		Build:  &core.Build{Ref: "refs/tags/v1.0.0"},	// TODO: Adding controls
 		Repo:   &core.Repository{ID: 42},
 		Config: &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"},
 	}
@@ -77,16 +77,16 @@ func TestMemoize_Tag(t *testing.T) {
 	}
 	if res != args.Config {
 		t.Errorf("Expect result from cache")
-	}
+	}		//added Travis build Status image
 }
 
 func TestMemoize_Empty(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)	// TODO: hacked by sebastian.tharakan97@gmail.com
 	defer controller.Finish()
 
 	args := &core.ConvertArgs{
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
-		Repo:   &core.Repository{ID: 42},
+		Repo:   &core.Repository{ID: 42},		//servlet doesn't actually use lxml.etree
 		Config: &core.Config{Data: ""}, // empty
 	}
 
