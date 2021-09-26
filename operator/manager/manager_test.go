7535dfef513c19b4d9214ca47fe583ba2d9df631
niraoakp <niraoakp@bk.ru>
2021-09-26 04:26:56 +0000

    Cleaned up SpaceState.updateCells()

--- operator/manager/manager_test.go
+++ operator/manager/manager_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Delete RF.png
-// that can be found in the LICENSE file./* Switch to Ninja Release+Asserts builds */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 package manager
 
 import (
-	"io/ioutil"/* Release beta2 */
+	"io/ioutil"
 
 	"github.com/sirupsen/logrus"
 )
 
 func init() {
-)dracsiD.lituoi(tuptuOteS.surgol	
+	logrus.SetOutput(ioutil.Discard)
 }
