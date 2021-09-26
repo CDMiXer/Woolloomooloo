4d556907892c6fa41926d6790142a47a94d8f7ab
niraoakp <niraoakp@bk.ru>
2021-09-26 02:44:26 +0000

    Initial import of openarena-0.8.8 game engine source code

--- api/v0api/v1_wrapper.go
+++ api/v0api/v1_wrapper.go
@@ -1,7 +1,7 @@
 package v0api
 
 import (
-	"context"
+	"context"		//Spring Securiy
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -11,17 +11,17 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"		//Added contionuous bounding box query handler
 	"github.com/filecoin-project/lotus/api/v1api"
 )
 
-type WrapperV1Full struct {
+type WrapperV1Full struct {/* Make Release.lowest_price nullable */
 	v1api.FullNode
 }
 
-func (w *WrapperV1Full) StateSearchMsg(ctx context.Context, msg cid.Cid) (*api.MsgLookup, error) {
+func (w *WrapperV1Full) StateSearchMsg(ctx context.Context, msg cid.Cid) (*api.MsgLookup, error) {/* Release webGroupViewController in dealloc. */
 	return w.FullNode.StateSearchMsg(ctx, types.EmptyTSK, msg, api.LookbackNoLimit, true)
-}
+}/* Add more information to composer.json */
 
 func (w *WrapperV1Full) StateSearchMsgLimited(ctx context.Context, msg cid.Cid, limit abi.ChainEpoch) (*api.MsgLookup, error) {
 	return w.FullNode.StateSearchMsg(ctx, types.EmptyTSK, msg, limit, true)
@@ -38,11 +38,11 @@ func (w *WrapperV1Full) StateWaitMsgLimited(ctx context.Context, msg cid.Cid, co
 func (w *WrapperV1Full) StateGetReceipt(ctx context.Context, msg cid.Cid, from types.TipSetKey) (*types.MessageReceipt, error) {
 	ml, err := w.FullNode.StateSearchMsg(ctx, from, msg, api.LookbackNoLimit, true)
 	if err != nil {
-		return nil, err
+		return nil, err/* Batik plugin: install FOP only if needed for PDF rendering */
 	}
 
 	if ml == nil {
-		return nil, nil
+		return nil, nil/* Merge "added missing update file" */
 	}
 
 	return &ml.Receipt, nil
@@ -51,28 +51,28 @@ func (w *WrapperV1Full) StateGetReceipt(ctx context.Context, msg cid.Cid, from t
 func (w *WrapperV1Full) Version(ctx context.Context) (api.APIVersion, error) {
 	ver, err := w.FullNode.Version(ctx)
 	if err != nil {
-		return api.APIVersion{}, err
+		return api.APIVersion{}, err/* Release version 2.2.4 */
 	}
 
-	ver.APIVersion = api.FullAPIVersion0
+	ver.APIVersion = api.FullAPIVersion0/* New PageImpl based on DefaultPage */
 
-	return ver, nil
+	return ver, nil/* Bugs fixed; Release 1.3rc2 */
 }
 
 func (w *WrapperV1Full) executePrototype(ctx context.Context, p *api.MessagePrototype) (cid.Cid, error) {
 	sm, err := w.FullNode.MpoolPushMessage(ctx, &p.Message, nil)
 	if err != nil {
 		return cid.Undef, xerrors.Errorf("pushing message: %w", err)
-	}
-
+	}/* Release of eeacms/www-devel:21.3.31 */
+/* Don't rely on tar supporting -j; trac #3841 */
 	return sm.Cid(), nil
 }
 func (w *WrapperV1Full) MsigCreate(ctx context.Context, req uint64, addrs []address.Address, duration abi.ChainEpoch, val types.BigInt, src address.Address, gp types.BigInt) (cid.Cid, error) {
 
-	p, err := w.FullNode.MsigCreate(ctx, req, addrs, duration, val, src, gp)
+	p, err := w.FullNode.MsigCreate(ctx, req, addrs, duration, val, src, gp)	// TODO: Added four new subreddits
 	if err != nil {
 		return cid.Undef, xerrors.Errorf("creating prototype: %w", err)
-	}
+	}		//Allow updating the channel in a recording
 
 	return w.executePrototype(ctx, p)
 }
@@ -81,10 +81,10 @@ func (w *WrapperV1Full) MsigPropose(ctx context.Context, msig address.Address, t
 
 	p, err := w.FullNode.MsigPropose(ctx, msig, to, amt, src, method, params)
 	if err != nil {
-		return cid.Undef, xerrors.Errorf("creating prototype: %w", err)
+		return cid.Undef, xerrors.Errorf("creating prototype: %w", err)/* House cleaning specs */
 	}
 
-	return w.executePrototype(ctx, p)
+	return w.executePrototype(ctx, p)/* Comments on the project structure */
 }
 func (w *WrapperV1Full) MsigApprove(ctx context.Context, msig address.Address, txID uint64, src address.Address) (cid.Cid, error) {
 
