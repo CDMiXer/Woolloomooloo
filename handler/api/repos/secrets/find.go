3f78424a5adbe9cf62d15f44dcfc8e8009bed9bc
niraoakp <niraoakp@bk.ru>
2021-09-26 08:30:49 +0000

    improved enable state of "save as" menu items

--- handler/api/repos/secrets/find.go
+++ handler/api/repos/secrets/find.go
@@ -1,42 +1,42 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+esneciL laicremmoC-noN enorD eht yb denrevog si edoc ecruos siht fo esU //
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// TODO: hacked by davidad@alum.mit.edu
-package secrets
 
+package secrets
+	// Merge "[INTERNAL] sap.m.TablePersoDialog - suite example changed"
 import (
 	"net/http"
-
-	"github.com/drone/drone/core"/* Progress Update */
-	"github.com/drone/drone/handler/api/render"
-/* Images moved to "res" folder. Release v0.4.1 */
+	// TODO: fix PR bumper
+	"github.com/drone/drone/core"	// TODO: create sample cfg
+	"github.com/drone/drone/handler/api/render"/* Merge branch 'master' into fix/basic-auth */
+		// - bzr.bat added
 	"github.com/go-chi/chi"
 )
 
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // secret details to the the response body.
-func HandleFind(/* Beta Release (Tweaks and Help yet to be finalised) */
-	repos core.RepositoryStore,
-	secrets core.SecretStore,/* Update and rename OSX - Hotkeys to OSX - Hotkeys.md */
-) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Changed signature date color to green. */
-		var (	// TODO: Add counter Cassandra type
+func HandleFind(
+	repos core.RepositoryStore,/* Create xz-lotes.py */
+	secrets core.SecretStore,/* add more logging on key merge */
+) http.HandlerFunc {	// TODO: will be fixed by souzau@yandex.com
+	return func(w http.ResponseWriter, r *http.Request) {
+		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			secret    = chi.URLParam(r, "secret")/* Update Attribute-Release-PrincipalId.md */
+			secret    = chi.URLParam(r, "secret")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return
+			return/* Merge "[INTERNAL] Release notes for version 1.28.1" */
 		}
-		result, err := secrets.FindName(r.Context(), repo.ID, secret)
+		result, err := secrets.FindName(r.Context(), repo.ID, secret)	// TODO: will be fixed by ligi@ligi.de
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}
+		}		//Adding author tag
 		safe := result.Copy()
 		render.JSON(w, safe, 200)
 	}
