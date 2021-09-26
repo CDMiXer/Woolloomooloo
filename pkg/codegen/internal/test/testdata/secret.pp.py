5057f6fd4eb3fd4f94cbd2a3bdd40d49144734e1
niraoakp <niraoakp@bk.ru>
2021-09-26 01:35:35 +0000

    Add IModalSettings.appendTo propert

--- pkg/codegen/internal/test/testdata/secret.pp.py
+++ pkg/codegen/internal/test/testdata/secret.pp.py
@@ -1,4 +1,4 @@
-import pulumi/* a8cb12c2-2d5f-11e5-a4c8-b88d120fff5e */
-import pulumi_aws as aws		//working on the read me file.
+import pulumi
+import pulumi_aws as aws
 
 db_cluster = aws.rds.Cluster("dbCluster", master_password=pulumi.secret("foobar"))
