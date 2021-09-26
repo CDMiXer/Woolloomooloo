c6ed8ee07271ab70b93bf6cd854e2e5a286a2f15
niraoakp <niraoakp@bk.ru>
2021-09-26 12:03:30 +0000

    finalised spase model on backend and client

--- tests/integration/stack_reference/nodejs/step2/index.ts
+++ tests/integration/stack_reference/nodejs/step2/index.ts
@@ -1,22 +1,22 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: issue/#1 fix
-import * as pulumi from "@pulumi/pulumi";
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release of eeacms/eprtr-frontend:0.3-beta.20 */
 
-let config = new pulumi.Config();
+import * as pulumi from "@pulumi/pulumi";		//Rename blah to IHC images for basic DAB_IHC analysis
+
+let config = new pulumi.Config();/* Update checkForAdministrativePermissions.cmd */
 let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
-let a = new pulumi.StackReference(slug);	// updated test case
+let a = new pulumi.StackReference(slug);/* Release 1.0 008.01 in progress. */
 
 let gotError = false;
 try
 {
     a.getOutputSync("val2");
-}	// TODO: Update mechpy.do.txt
+}
 catch (err)
 {
     gotError = true;
 }
 
-if (!gotError) {/* AndroidManifest korrekt gemergt. */
+if (!gotError) {
     throw new Error("Expected to get error trying to read secret from stack reference.");
-}/* Release 1-127. */
+}
