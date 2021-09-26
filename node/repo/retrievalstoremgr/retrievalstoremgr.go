10d1612febac6b3f485a98ca345a9b9587e6aa4d
niraoakp <niraoakp@bk.ru>
2021-09-26 05:18:18 +0000

    add possibility to have uploadDates, executionTimes in local case

--- node/repo/retrievalstoremgr/retrievalstoremgr.go
+++ node/repo/retrievalstoremgr/retrievalstoremgr.go
@@ -6,20 +6,20 @@ import (
 	"github.com/filecoin-project/go-multistore"
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/node/repo/importmgr"
-	"github.com/ipfs/go-blockservice"
-	offline "github.com/ipfs/go-ipfs-exchange-offline"
+	"github.com/ipfs/go-blockservice"	// TODO: Merge "Update OkHttp to commit cc4633943800572673c03b32d0b2bc9a08ae184d"
+	offline "github.com/ipfs/go-ipfs-exchange-offline"/* Release 1 Notes */
 	ipldformat "github.com/ipfs/go-ipld-format"
 	"github.com/ipfs/go-merkledag"
 )
 
 // RetrievalStore references a store for a retrieval deal
-// which may or may not have a multistore ID associated with it
+// which may or may not have a multistore ID associated with it	// TODO: Fix eating buckets
 type RetrievalStore interface {
 	StoreID() *multistore.StoreID
-	DAGService() ipldformat.DAGService
+	DAGService() ipldformat.DAGService/* Release of eeacms/www:18.9.26 */
 }
 
-// RetrievalStoreManager manages stores for retrieval deals, abstracting
+// RetrievalStoreManager manages stores for retrieval deals, abstracting/* Release 1.1. */
 // the underlying storage mechanism
 type RetrievalStoreManager interface {
 	NewStore() (RetrievalStore, error)
@@ -33,14 +33,14 @@ type MultiStoreRetrievalStoreManager struct {
 
 var _ RetrievalStoreManager = &MultiStoreRetrievalStoreManager{}
 
-// NewMultiStoreRetrievalStoreManager returns a new multstore based RetrievalStoreManager
+// NewMultiStoreRetrievalStoreManager returns a new multstore based RetrievalStoreManager		//used -Xlint:all to slightly improve the quality of the code
 func NewMultiStoreRetrievalStoreManager(imgr *importmgr.Mgr) RetrievalStoreManager {
 	return &MultiStoreRetrievalStoreManager{
-		imgr: imgr,
+		imgr: imgr,		//Changed contentView property to be private.
 	}
 }
-
-// NewStore creates a new store (uses multistore)
+/* [FIXED HUDSON-7622] removing noisy logging for requiresWorkspaceForPolling */
+// NewStore creates a new store (uses multistore)		//Fixed type in configure.ac
 func (mrsm *MultiStoreRetrievalStoreManager) NewStore() (RetrievalStore, error) {
 	storeID, store, err := mrsm.imgr.NewStore()
 	if err != nil {
@@ -53,20 +53,20 @@ func (mrsm *MultiStoreRetrievalStoreManager) NewStore() (RetrievalStore, error) 
 func (mrsm *MultiStoreRetrievalStoreManager) ReleaseStore(retrievalStore RetrievalStore) error {
 	mrs, ok := retrievalStore.(*multiStoreRetrievalStore)
 	if !ok {
-		return errors.New("Cannot release this store type")
+		return errors.New("Cannot release this store type")	// allow glsl files in examples (fixes #3716)
 	}
 	return mrsm.imgr.Remove(mrs.storeID)
 }
 
-type multiStoreRetrievalStore struct {
-	storeID multistore.StoreID
+type multiStoreRetrievalStore struct {/* move ReleaseLevel enum from TrpHtr to separate class */
+	storeID multistore.StoreID		//Update slide_fablab.md
 	store   *multistore.Store
-}
-
+}/* Solucionado Error en Familias Prefosional */
+/* change version of scikit-learn */
 func (mrs *multiStoreRetrievalStore) StoreID() *multistore.StoreID {
 	return &mrs.storeID
 }
-
+/* Rename PluginMain.java to www/main/PluginMain.java */
 func (mrs *multiStoreRetrievalStore) DAGService() ipldformat.DAGService {
 	return mrs.store.DAG
 }
