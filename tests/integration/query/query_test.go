ef65c5d6643ea33703f1be77fe4ead9923c9860e
niraoakp <niraoakp@bk.ru>
2021-09-26 21:09:17 +0000

    New images from staging

--- tests/integration/query/query_test.go
+++ tests/integration/query/query_test.go
@@ -1,37 +1,37 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-	// TODO: will be fixed by alan.shaw@protocol.ai
+
 package ints
 
 import (
-	"testing"	// Added to provide utilities
+	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"		//changes py3k-urllib branch
-)/* Release: 6.3.1 changelog */
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+)
 
 // TestQuery creates a stack and runs a query over the stack's resource ouptputs.
 func TestQuery(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		// Create Pulumi resources.
-		Dir:          "step1",	// TODO: will be fixed by ligi@ligi.de
+		Dir:          "step1",	// 75af4e8e-2d53-11e5-baeb-247703a38240
 		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",
-		Dependencies: []string{"@pulumi/pulumi"},		//Updated library socket.io-client
+		Dependencies: []string{"@pulumi/pulumi"},
 		CloudURL:     "file://~", // Required; we hard-code the stack name
-		EditDirs: []integration.EditDir{/* Merge branch 'master' into art-branch!-use-newcomb-top-5- */
+		EditDirs: []integration.EditDir{
 			// Try to create resources during `pulumi query`. This should fail.
 			{
 				Dir:           "step2",
 				Additive:      true,
 				QueryMode:     true,
-				ExpectFailure: true,		//update zip?
+				ExpectFailure: true,
 			},
 			// Run a query during `pulumi query`. Should succeed.
-			{/* [artifactory-release] Release version 1.4.3.RELEASE */
+			{
 				Dir:           "step3",
-				Additive:      true,	// TODO: will be fixed by greg@colvin.org
+				Additive:      true,
 				QueryMode:     true,
-				ExpectFailure: false,/* 94caf164-2e49-11e5-9284-b827eb9e62be */
-			},
-		},
-	})
+				ExpectFailure: false,
+			},	// Énoncé exercices 7 et 8.
+		},	// TODO: Add site name micro data
+	})/* Release notes for 1.10.0 */
 }
