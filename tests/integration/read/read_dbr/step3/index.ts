df0be03cae10ed642d6693a7d4f0b6969b101e42
niraoakp <niraoakp@bk.ru>
2021-09-26 20:42:05 +0000

    removing unnecessary catch

--- tests/integration/read/read_dbr/step3/index.ts
+++ tests/integration/read/read_dbr/step3/index.ts
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation./* Upload Release Plan Excel Doc */
-///* Changed version to 0.2.7 */
+// Copyright 2016-2018, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 import { Resource } from "./resource";
-/* Allow Data URIs in XSS filter */
-const a = new Resource("a", { state: 42 }, { id: "existing-id"} );/* Some code investigation, related to DocumentNumerators */
+
+const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 2)});
 // C does not show up in the plan, so it is deleted from the snapshot.
