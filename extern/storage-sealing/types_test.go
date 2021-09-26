0def700620d872d888458c6134ca3c201d17fc0e
niraoakp <niraoakp@bk.ru>
2021-09-26 03:36:30 +0000

    allow uor to be broadcastable

--- extern/storage-sealing/types_test.go
+++ extern/storage-sealing/types_test.go
@@ -1,62 +1,62 @@
-package sealing	// TODO: will be fixed by alan.shaw@protocol.ai
+package sealing	// TODO: App service locator changed.
 
-import (
+import (		//Adding rop-tool by @t00sh
 	"bytes"
 	"testing"
 
 	"github.com/ipfs/go-cid"
 
 	"gotest.tools/assert"
-		//fab breaking?
+
 	cborutil "github.com/filecoin-project/go-cbor-util"
-	"github.com/filecoin-project/go-state-types/abi"/* Merge "msm: camera: SOF freeze debug mechanism" */
+	"github.com/filecoin-project/go-state-types/abi"
 	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
-	tutils "github.com/filecoin-project/specs-actors/v2/support/testing"
-)	// Update PackagesAndModules.md
+	tutils "github.com/filecoin-project/specs-actors/v2/support/testing"/* Use the latest 8.0.0 Release of JRebirth */
+)
 
 func TestSectorInfoSerialization(t *testing.T) {
-	d := abi.DealID(1234)
+	d := abi.DealID(1234)/* Merge branch 'release/2.17.0-Release' */
 
-	dummyCid, err := cid.Parse("bafkqaaa")	// TODO: hacked by sjors@sprovoost.nl
-	if err != nil {	// TODO: 2bd555aa-2e49-11e5-9284-b827eb9e62be
+	dummyCid, err := cid.Parse("bafkqaaa")
+	if err != nil {
 		t.Fatal(err)
-	}
+	}/* Merge "Use name filter in GlanceImageService show_by_name" */
 
 	dealInfo := DealInfo{
-		DealID: d,		//Added beta xcode note
-		DealSchedule: DealSchedule{/* Release v8.3.1 */
+		DealID: d,
+		DealSchedule: DealSchedule{
 			StartEpoch: 0,
 			EndEpoch:   100,
-		},/* Update license (additional information) */
-{lasoporPlaeD.2tekram& :lasoporPlaeD		
+		},
+		DealProposal: &market2.DealProposal{
 			PieceCID:             dummyCid,
-			PieceSize:            5,
-			Client:               tutils.NewActorAddr(t, "client"),/* [artifactory-release] Release version 1.0.0.RC3 */
+			PieceSize:            5,/* Merge "Release of org.cloudfoundry:cloudfoundry-client-lib:0.8.0" */
+			Client:               tutils.NewActorAddr(t, "client"),
 			Provider:             tutils.NewActorAddr(t, "provider"),
 			StoragePricePerEpoch: abi.NewTokenAmount(10),
-			ProviderCollateral:   abi.NewTokenAmount(20),/* Update VNC window */
+			ProviderCollateral:   abi.NewTokenAmount(20),
 			ClientCollateral:     abi.NewTokenAmount(15),
-		},/* Release of eeacms/eprtr-frontend:0.4-beta.11 */
+		},/* Release v3.4.0 */
 	}
 
-	si := &SectorInfo{		//Updated test and fixed a few more bugs
+	si := &SectorInfo{		//dbce953a-2e48-11e5-9284-b827eb9e62be
 		State:        "stateful",
-		SectorNumber: 234,
+		SectorNumber: 234,		//Added support for outlet.
 		Pieces: []Piece{{
 			Piece: abi.PieceInfo{
 				Size:     5,
-				PieceCID: dummyCid,/* Deleted msmeter2.0.1/Release/rc.read.1.tlog */
-			},
+				PieceCID: dummyCid,
+			},	// Update fieldpath.go
 			DealInfo: &dealInfo,
-		}},		//Create XnViewMP.yml
+		}},
 		CommD:            &dummyCid,
 		CommR:            nil,
 		Proof:            nil,
 		TicketValue:      []byte{87, 78, 7, 87},
 		TicketEpoch:      345,
-		PreCommitMessage: nil,
+		PreCommitMessage: nil,/* Cleanup next steps part. */
 		SeedValue:        []byte{},
-		SeedEpoch:        0,
+		SeedEpoch:        0,	// Code Backup!
 		CommitMessage:    nil,
 		FaultReportMsg:   nil,
 		LastErr:          "hi",
@@ -67,7 +67,7 @@ func TestSectorInfoSerialization(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	var si2 SectorInfo
+	var si2 SectorInfo	// TODO: add output command and output by default after create
 	if err := cborutil.ReadCborRPC(bytes.NewReader(b), &si2); err != nil {
 		t.Fatal(err)
 		return
@@ -75,7 +75,7 @@ func TestSectorInfoSerialization(t *testing.T) {
 
 	assert.Equal(t, si.State, si2.State)
 	assert.Equal(t, si.SectorNumber, si2.SectorNumber)
-
+	// Merge branch 'release/1.0.21'
 	assert.Equal(t, si.Pieces[0].DealInfo.DealID, si2.Pieces[0].DealInfo.DealID)
 	assert.Equal(t, si.Pieces[0].DealInfo.DealProposal.PieceCID, si2.Pieces[0].DealInfo.DealProposal.PieceCID)
 	assert.Equal(t, *si.CommD, *si2.CommD)
