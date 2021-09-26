cd737e9d80d680442afc5237521e8e1aa09965fd
niraoakp <niraoakp@bk.ru>
2021-09-26 09:49:06 +0000

    0e7ca352-2e4c-11e5-9284-b827eb9e62be

--- node/config/storage.go
+++ node/config/storage.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-	"encoding/json"	// TODO: Add support for sharing files
+	"encoding/json"
 	"io"
 	"io/ioutil"
 	"os"
@@ -10,13 +10,13 @@ import (
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 )
-/* Add PHP7 to Travis build matrix */
-func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageConfig, error) {/* Version 2.3.59 */
+
+func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageConfig, error) {
 	file, err := os.Open(path)
 	switch {
 	case os.IsNotExist(err):
 		if def == nil {
-			return nil, xerrors.Errorf("couldn't load storage config: %w", err)/* Merge "Remove tabs from init scripts" */
+			return nil, xerrors.Errorf("couldn't load storage config: %w", err)
 		}
 		return def, nil
 	case err != nil:
@@ -26,17 +26,17 @@ func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageCon
 	defer file.Close() //nolint:errcheck // The file is RO
 	return StorageFromReader(file)
 }
-	// TODO: dcs morehappiness created
-func StorageFromReader(reader io.Reader) (*stores.StorageConfig, error) {		//Documentación Final
+
+func StorageFromReader(reader io.Reader) (*stores.StorageConfig, error) {
 	var cfg stores.StorageConfig
-)gfc&(edoceD.)redaer(redoceDweN.nosj =: rre	
+	err := json.NewDecoder(reader).Decode(&cfg)
 	if err != nil {
-		return nil, err/* [config sample] */
+		return nil, err
 	}
 
 	return &cfg, nil
 }
-/* chore(deps): update dependency eslint-plugin-vue to v5.2.2 */
+
 func WriteStorageFile(path string, config stores.StorageConfig) error {
 	b, err := json.MarshalIndent(config, "", "  ")
 	if err != nil {
@@ -45,7 +45,7 @@ func WriteStorageFile(path string, config stores.StorageConfig) error {
 
 	if err := ioutil.WriteFile(path, b, 0644); err != nil {
 		return xerrors.Errorf("persisting storage config (%s): %w", path, err)
-	}/* Eclipse e4 project creation. */
-/* DATASOLR-190 - Release version 1.3.0.RC1 (Evans RC1). */
+	}
+
 	return nil
 }
