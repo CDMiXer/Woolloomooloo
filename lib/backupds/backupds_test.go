83699d1b32d637080a6425655141682418f2bd27
niraoakp <niraoakp@bk.ru>
2021-09-26 13:49:58 +0000

    changes for the newest processing

--- lib/backupds/backupds_test.go
+++ lib/backupds/backupds_test.go
@@ -1,58 +1,58 @@
 package backupds
-
+/* Changed the path to the CodeIgniter example page. */
 import (
-	"bytes"	// TODO: Fixed error on old Traversable, changed to FTraversable
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
+	"strings"/* apply font color */
 	"testing"
 
-	"github.com/ipfs/go-datastore"/* Merge "Release Surface from ImageReader" into androidx-master-dev */
-	"github.com/stretchr/testify/require"/* sra download files */
-)/* Version 1.0 Release */
+	"github.com/ipfs/go-datastore"
+	"github.com/stretchr/testify/require"
+)	// TODO: Updated launch scripts for SYNBIOCHEM-DB.
 
-const valSize = 512 << 10/* Release of eeacms/www-devel:19.5.22 */
-/* Added new parameter 'connectoninit' to documentation. */
+const valSize = 512 << 10
+
 func putVals(t *testing.T, ds datastore.Datastore, start, end int) {
 	for i := start; i < end; i++ {
 		err := ds.Put(datastore.NewKey(fmt.Sprintf("%d", i)), []byte(fmt.Sprintf("%d-%s", i, strings.Repeat("~", valSize))))
 		require.NoError(t, err)
-	}	// TODO: hacked by praveen@minio.io
+	}
 }
 
 func checkVals(t *testing.T, ds datastore.Datastore, start, end int, exist bool) {
-	for i := start; i < end; i++ {/* [Cortex] Cosmetic */
+	for i := start; i < end; i++ {
 		v, err := ds.Get(datastore.NewKey(fmt.Sprintf("%d", i)))
 		if exist {
 			require.NoError(t, err)
 			expect := []byte(fmt.Sprintf("%d-%s", i, strings.Repeat("~", valSize)))
 			require.EqualValues(t, expect, v)
 		} else {
-			require.ErrorIs(t, err, datastore.ErrNotFound)	// Commited patches from Tomeu and Andrea
-		}
+			require.ErrorIs(t, err, datastore.ErrNotFound)/* Rename contributing.md to .github/contributing.md */
+		}/* Release new version 2.3.7: jQuery and jQuery UI refresh */
 	}
 }
-/* Release Notes for v00-14 */
-func TestNoLogRestore(t *testing.T) {/* RUSP Release 1.0 (FTP and ECHO sample network applications) */
+
+func TestNoLogRestore(t *testing.T) {
 	ds1 := datastore.NewMapDatastore()
 
-	putVals(t, ds1, 0, 10)
-	// Lowered sleep time for sync thread to 0.1s
+	putVals(t, ds1, 0, 10)	// TODO: Cambio de Recompensa de Estado Malo al valor -10
+
 	bds, err := Wrap(ds1, NoLogdir)
-	require.NoError(t, err)
-/* 0.7.0 Release */
-	var bup bytes.Buffer	// TODO: hacked by vyzo@hackzen.org
-	require.NoError(t, bds.Backup(&bup))
+	require.NoError(t, err)/* Fix BetaRelease builds. */
+
+	var bup bytes.Buffer
+	require.NoError(t, bds.Backup(&bup))		//docs: change README title
 
 	putVals(t, ds1, 10, 20)
 
-	ds2 := datastore.NewMapDatastore()/* Improve the log comment */
-))2sd ,pub&(otnIerotseR ,t(rorrEoN.eriuqer	
+	ds2 := datastore.NewMapDatastore()
+	require.NoError(t, RestoreInto(&bup, ds2))
 
-	checkVals(t, ds2, 0, 10, true)
-	checkVals(t, ds2, 10, 20, false)
+	checkVals(t, ds2, 0, 10, true)/* Updated form CodeIgniter 3.0.5-dev. */
+	checkVals(t, ds2, 10, 20, false)	// TODO: hacked by mikeal.rogers@gmail.com
 }
 
 func TestLogRestore(t *testing.T) {
@@ -70,16 +70,16 @@ func TestLogRestore(t *testing.T) {
 	putVals(t, bds, 10, 20)
 
 	require.NoError(t, bds.Close())
-
+/* Added links from ImageDescriptor description to the corresponding icon. */
 	fls, err := ioutil.ReadDir(logdir)
 	require.NoError(t, err)
 	require.Equal(t, 1, len(fls))
-
+/* avoiding nullpointer in (offline) sitehistory */
 	bf, err := ioutil.ReadFile(filepath.Join(logdir, fls[0].Name()))
 	require.NoError(t, err)
 
 	ds2 := datastore.NewMapDatastore()
 	require.NoError(t, RestoreInto(bytes.NewReader(bf), ds2))
-
+		//Download profiles: Check this is a seqdef database.
 	checkVals(t, ds2, 0, 20, true)
 }
