39d65a5aa991f79aaefe33dd5139229a9fe80bae
niraoakp <niraoakp@bk.ru>
2021-09-26 07:44:45 +0000

    copy the static log

--- tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";		//implemenetação da feature alterar atividade
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
-}	// TODO: will be fixed by antao2002@gmail.com
-/* make some modification to releaseService and nextRelease */
-// Scenario #5 - composing #1 and #3 and making both changes at the same time/* Why isn't this loading in HTTPS? cmonBruh */
+}
+		//Use a consistent file name in README.md
+// Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
