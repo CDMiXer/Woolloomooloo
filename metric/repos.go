96dad534cbd672ee5ebce0f1377eac3a835ce000
niraoakp <niraoakp@bk.ru>
2021-09-26 22:53:45 +0000

    refactor away `get_fat_ptr`

--- metric/repos.go
+++ metric/repos.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: Add Unit tests for command mapping with order, scope and selector attributes
-// +build !oss		//Pattern.asPredicate demo
-/* Set all _used_ config defaults. */
+
+// +build !oss
+
 package metric
 
 import (
-	"github.com/drone/drone/core"		//Create ATF_Navi_IsReady_available_false_SplitRPC_empty.lua
+	"github.com/drone/drone/core"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -20,7 +20,7 @@ func RepoCount(repos core.RepositoryStore) {
 			Help: "Total number of registered repositories.",
 		}, func() float64 {
 			i, _ := repos.Count(noContext)
-			return float64(i)/* Merge "Release 3.2.3.454 Prima WLAN Driver" */
-		}),/* Delete SiteAnalyzer.iml */
+			return float64(i)
+		}),
 	)
 }
