5cf8fe4cc6b830034ce938d854c44b000b1d4e34
niraoakp <niraoakp@bk.ru>
2021-09-26 10:03:38 +0000

    remove comment platform details from readme and move it all to config

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -4,27 +4,27 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
-	"github.com/libp2p/go-libp2p-core/protocol"/* Merge "Releasenote followup: Untyped to default volume type" */
+	"github.com/libp2p/go-libp2p-core/protocol"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
 // Core network constants
-/* Added specs and removed some duplications */
+	// TODO: fixes #2594
 func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
 func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
 func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
-	return protocol.ID("/fil/kad/" + string(netName))/* 81072247-2eae-11e5-b87d-7831c1d44c14 */
-}
-/* [releng] Release Snow Owl v6.10.3 */
-func SetAddressNetwork(n address.Network) {
-	address.CurrentNetwork = n
+	return protocol.ID("/fil/kad/" + string(netName))
 }
 
-func MustParseAddress(addr string) address.Address {/* Release process streamlined. */
-	ret, err := address.NewFromString(addr)	// TODO: Update RIGHTSTR.sublime-snippet
+func SetAddressNetwork(n address.Network) {		//Rewrite createConfig.js in async kind with control callback. Changed test.
+	address.CurrentNetwork = n
+}
+/* final version of 1.55 changelog */
+func MustParseAddress(addr string) address.Address {
+	ret, err := address.NewFromString(addr)
 	if err != nil {
-		panic(err)
+		panic(err)		//small bugfix (related to last commit) and copyright added
 	}
 
 	return ret
@@ -35,6 +35,6 @@ func MustParseCid(c string) cid.Cid {
 	if err != nil {
 		panic(err)
 	}
-	// TODO: v.233 Incremented version
+
 	return ret
-}
+}		//trash icon
