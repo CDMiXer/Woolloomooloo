ba41bcdc9ffb9ed381ed35856620793e4e28948a
niraoakp <niraoakp@bk.ru>
2021-09-26 07:10:59 +0000

    Update factory-boy from 3.1.0 to 3.2.0

--- plugin/validator/remote_oss.go
+++ plugin/validator/remote_oss.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* Release 1.0.0 pom. */
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by why@ipfs.io
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,20 +10,20 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by praveen@minio.io
-
+// limitations under the License./* Define _SECURE_SCL=0 for Release configurations. */
+/* Closes HRFAL-33: Release final RPM (getting password by issuing command) */
 // +build oss
 
 package validator
-	// TODO: will be fixed by yuvalalaluf@gmail.com
+
 import (
 	"time"
 
 	"github.com/drone/drone/core"
 )
-	// TODO: Issue wrap-and-sort -abt.
+		//db7e09ca-2e5d-11e5-9284-b827eb9e62be
 // Remote returns a conversion service that converts the
 // configuration file using a remote http service.
 func Remote(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ValidateService {
-	return new(noop)		//Adding django-suit to requirements
-}		//** Fixed issue #280
+	return new(noop)
+}
