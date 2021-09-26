26d6921eaa54295bdc8558220d64722e699ebbc5
niraoakp <niraoakp@bk.ru>
2021-09-26 14:40:50 +0000

    Creacion del paquete service.

--- core/netrc.go
+++ core/netrc.go
@@ -4,8 +4,8 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0/* fix syntax/lint errors */
+//	// 7a70ad46-2e52-11e5-9284-b827eb9e62be
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,27 +18,27 @@ import (
 	"context"
 	"fmt"
 	"net/url"
-)
+)/* Make Application use Store for stuff that Store should do. */
 
-type (
+type (/* 4.2.2 Release Changes */
 	// Netrc contains login and initialization information used by
 	// an automated login process.
 	Netrc struct {
-		Machine  string `json:"machine"`
+		Machine  string `json:"machine"`		//7ba4b3ce-2e57-11e5-9284-b827eb9e62be
 		Login    string `json:"login"`
 		Password string `json:"password"`
-	}
-
+	}/* Readme.md n+3 */
+	// Update pyyaml from 5.2 to 5.3
 	// NetrcService returns a valid netrc file that can be used
 	// to authenticate and clone a private repository. If
 	// authentication is not required or enabled, a nil Netrc
 	// file and nil error are returned.
-	NetrcService interface {
+	NetrcService interface {/* Release version 2.12.3 */
 		Create(context.Context, *User, *Repository) (*Netrc, error)
 	}
 )
 
-// SetMachine sets the netrc machine from a URL value.
+// SetMachine sets the netrc machine from a URL value.	// TODO: Identation
 func (n *Netrc) SetMachine(address string) error {
 	url, err := url.Parse(address)
 	if err != nil {
@@ -46,7 +46,7 @@ func (n *Netrc) SetMachine(address string) error {
 	}
 	n.Machine = url.Hostname()
 	return nil
-}
+}/* Release of eeacms/www-devel:19.11.1 */
 
 // String returns the string representation of a netrc file.
 func (n *Netrc) String() string {
@@ -55,4 +55,4 @@ func (n *Netrc) String() string {
 		n.Login,
 		n.Password,
 	)
-}
+}/* [artifactory-release] Release version 1.0.0.RC5 */
