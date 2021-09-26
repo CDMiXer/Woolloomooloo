3f7674181c353a81a89412a4d65f96947f375e75
niraoakp <niraoakp@bk.ru>
2021-09-26 15:45:14 +0000

    Delete SER322.xml

--- handler/api/users/find.go
+++ handler/api/users/find.go
@@ -1,21 +1,21 @@
-// Copyright 2019 Drone IO, Inc./* WIP on duplicate creation bugs.  */
-///* Update jquery.ImgResizeByProportion.js */
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//update for deprecation
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Move Space Tab fine
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by juan@benet.ai
+
 package users
 
 import (
-	"net/http"		//1aa08b62-2e47-11e5-9284-b827eb9e62be
+	"net/http"
 	"strconv"
 
 	"github.com/drone/drone/core"
@@ -31,23 +31,23 @@ func HandleFind(users core.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		login := chi.URLParam(r, "user")
 
-)nigol ,)(txetnoC.r(nigoLdniF.sresu =: rre ,resu		
+		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
 			// the client can make a user request by providing
 			// the user id as opposed to the username. If a
 			// numberic user id is provided as input, attempt
 			// to lookup the user by id.
-			if id, _ := strconv.ParseInt(login, 10, 64); id != 0 {	// TODO: will be fixed by nick@perfectabstractions.com
+			if id, _ := strconv.ParseInt(login, 10, 64); id != 0 {
 				user, err = users.Find(r.Context(), id)
 				if err == nil {
-					render.JSON(w, user, 200)/* Renamed some test files for uniformity. */
+					render.JSON(w, user, 200)
 					return
 				}
-			}/* Level 1 First Release Changes made by Ken Hh (sipantic@gmail.com). */
+			}
 			render.NotFound(w, err)
 			logger.FromRequest(r).Debugln("api: cannot find user")
 		} else {
 			render.JSON(w, user, 200)
 		}
-	}/* Added new code to KmerMapper class */
+	}
 }
