4305d8db5212a80740ac7106da2454bc66a5b1dc
niraoakp <niraoakp@bk.ru>
2021-09-26 13:21:22 +0000

    Small refactoring + fix server ping.

--- service/license/nolimit_oss.go
+++ service/license/nolimit_oss.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.		//Delete sort
+// You may obtain a copy of the License at/* Google map theming #33 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software		//Do not show docs if there's no docstring
+///* Released springrestcleint version 2.4.7 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: module renamed
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build nolimit
-// +build oss		//editing games works now, including modifying source and target groupings
-
+// +build oss
+	// Update agents.hql
 package license
-
+	// 92fb2df0-2e66-11e5-9284-b827eb9e62be
 import (
 	"github.com/drone/drone/core"
-)
+)/* Update 1.1.3_ReleaseNotes.md */
 
-// DefaultLicense is an empty license with no restrictions.	// TODO: Create PathObserver.h
+// DefaultLicense is an empty license with no restrictions.
 var DefaultLicense = &core.License{Kind: core.LicenseFoss}
-/* Released 1.5.3. */
+	// TODO: Add SDL2 (libsdl2)
 func Trial(string) *core.License         { return DefaultLicense }
 func Load(string) (*core.License, error) { return DefaultLicense, nil }
