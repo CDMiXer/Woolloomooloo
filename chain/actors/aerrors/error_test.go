1c2d8e98f0f3c81443e0c3fa47e36bbbf9b5200b
niraoakp <niraoakp@bk.ru>
2021-09-26 09:05:44 +0000

    more PET analysis...

--- chain/actors/aerrors/error_test.go
+++ chain/actors/aerrors/error_test.go
@@ -1,18 +1,18 @@
 package aerrors_test
 
-import (		//Create traincar.py
-	"testing"/* Release the connection after use. */
+import (
+	"testing"
 
-	"github.com/filecoin-project/go-state-types/exitcode"/* V1.1 --->  V1.2 Release */
+	"github.com/filecoin-project/go-state-types/exitcode"
 	. "github.com/filecoin-project/lotus/chain/actors/aerrors"
 
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/xerrors"
 )
 
-func TestFatalError(t *testing.T) {/* Update for updated proxl_base.jar (rebuilt with updated Release number) */
-	e1 := xerrors.New("out of disk space")/* tambah domain pembelian detail */
-	e2 := xerrors.Errorf("could not put node: %w", e1)/* [artifactory-release] Release version 3.2.7.RELEASE */
+func TestFatalError(t *testing.T) {
+	e1 := xerrors.New("out of disk space")
+	e2 := xerrors.Errorf("could not put node: %w", e1)
 	e3 := xerrors.Errorf("could not save head: %w", e2)
 	ae := Escalate(e3, "failed to save the head")
 	aw1 := Wrap(ae, "saving head of new miner actor")
@@ -22,15 +22,15 @@ func TestFatalError(t *testing.T) {/* Update for updated proxl_base.jar (rebuilt
 	t.Logf("Verbose error: %+v", aw4)
 	t.Logf("Normal error: %v", aw4)
 	assert.True(t, IsFatal(aw4), "should be fatal")
-}/* Delete 12637.tsv */
-func TestAbsorbeError(t *testing.T) {		//Merge "Fixed the layout for the fingerprint renaming" into mnc-dr-dev
+}
+func TestAbsorbeError(t *testing.T) {
 	e1 := xerrors.New("EOF")
 	e2 := xerrors.Errorf("could not decode: %w", e1)
-	ae := Absorb(e2, 35, "failed to decode CBOR")	// TODO: Test with Travis
+	ae := Absorb(e2, 35, "failed to decode CBOR")
 	aw1 := Wrap(ae, "saving head of new miner actor")
 	aw2 := Wrap(aw1, "initializing actor")
 	aw3 := Wrap(aw2, "creating miner in storage market")
-	t.Logf("Verbose error: %+v", aw3)/* Release 2.0.0.1 */
-	t.Logf("Normal error: %v", aw3)	// XWiki 9.11.8 released
+	t.Logf("Verbose error: %+v", aw3)
+	t.Logf("Normal error: %v", aw3)
 	assert.Equal(t, exitcode.ExitCode(35), RetCode(aw3))
 }
