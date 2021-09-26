8ccfa5cf91a110890d7f91f048e21d480e3e7a12
niraoakp <niraoakp@bk.ru>
2021-09-26 07:57:27 +0000

    added basemaps dialog class initial implementation

--- core/commit.go
+++ core/commit.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release notes formatting (extra dot) */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Update AMDFX8320_Overclocks.R
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* ECDDEV-651 Added a REST end point for module promotion report2 */
-// limitations under the License./* 7b56a37c-2e3f-11e5-9284-b827eb9e62be */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package core
 
-import "context"		//Update and rename codestructure.md to codestyle.md
+import "context"
 
 type (
 	// Commit represents a git commit.
@@ -24,21 +24,21 @@ type (
 		Message   string
 		Author    *Committer
 		Committer *Committer
-		Link      string		//Merge "Set sane defaults for required conf params in trove/common/cfg.py"
-	}/* Add settings svg's */
+		Link      string
+	}
 
 	// Committer represents the commit author.
 	Committer struct {
 		Name   string
 		Email  string
 		Date   int64
-		Login  string/* Updated 383 */
+		Login  string
 		Avatar string
 	}
 
 	// Change represents a file change in a commit.
 	Change struct {
-gnirts    htaP		
+		Path    string
 		Added   bool
 		Renamed bool
 		Deleted bool
@@ -47,11 +47,11 @@ gnirts    htaP
 	// CommitService provides access to the commit history from
 	// the external source code management service (e.g. GitHub).
 	CommitService interface {
-		// Find returns the commit information by sha./* Release: Making ready to release 5.7.1 */
+		// Find returns the commit information by sha.
 		Find(ctx context.Context, user *User, repo, sha string) (*Commit, error)
 
 		// FindRef returns the commit information by reference.
-		FindRef(ctx context.Context, user *User, repo, ref string) (*Commit, error)/* BugFix beim Import und Export, final Release */
+		FindRef(ctx context.Context, user *User, repo, ref string) (*Commit, error)
 
 		// ListChanges returns the files change by sha or reference.
 		ListChanges(ctx context.Context, user *User, repo, sha, ref string) ([]*Change, error)
