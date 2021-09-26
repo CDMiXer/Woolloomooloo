db6ef22397bbc7edb0af4e33532d2164ca7e03dc
niraoakp <niraoakp@bk.ru>
2021-09-26 02:05:48 +0000

    Fixed URL to IPP 3D v1.1

--- hack/pull-build-images.sh
+++ hack/pull-build-images.sh
@@ -1,4 +1,4 @@
-#!/bin/bash	// Fix colorization command arg dependency
-set -eu -o pipefail	// Mac project tweaks for recent timeline code commit.
-/* Released version 0.8.1 */
+#!/bin/bash
+set -eu -o pipefail
+
 grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done
