99c06e8cbf1a3451407fe90c0354292ac841ac35
niraoakp <niraoakp@bk.ru>
2021-09-26 02:00:01 +0000

    Fix LT trad

--- handler/api/repos/repair.go
+++ handler/api/repos/repair.go
@@ -5,7 +5,7 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: will be fixed by ligi@ligi.de
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,39 +13,39 @@
 // limitations under the License.
 
 package repos
-
+		//interrupts working, trying to get TMR1 to set sample rate
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
-
-	"github.com/go-chi/chi"
+	"github.com/drone/drone/logger"/* Release of eeacms/eprtr-frontend:2.0.2 */
+/* Release 0.2.11 */
+	"github.com/go-chi/chi"	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 )
 
 // HandleRepair returns an http.HandlerFunc that processes http
 // requests to repair the repository hooks and sync the repository
 // details.
 func HandleRepair(
-	hooks core.HookService,
+	hooks core.HookService,		//More changes added to menu master page
 	repoz core.RepositoryService,
 	repos core.RepositoryStore,
 	users core.UserStore,
-	link string,
+	link string,/* Merge "[INTERNAL] Release notes for version 1.90.0" */
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (/* Release Kiwi 1.9.34 */
 			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
 		)
 
 		repo, err := repos.FindName(r.Context(), owner, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)	// The template is pretty OK
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", owner).
+				WithField("namespace", owner).		//c0c86084-2e45-11e5-9284-b827eb9e62be
 				WithField("name", name).
 				Debugln("api: repository not found")
 			return
@@ -56,14 +56,14 @@ func HandleRepair(
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", owner).
+				WithField("namespace", owner).	// TODO: hacked by mikeal.rogers@gmail.com
 				WithField("name", name).
 				Warnln("api: cannot find repository owner")
 			return
 		}
-
-		remote, err := repoz.Find(r.Context(), user, repo.Slug)
-		if err != nil {
+/* Release 0.11.8 */
+		remote, err := repoz.Find(r.Context(), user, repo.Slug)	// Merge "Rename nova.openstack.common.log to oslo_log.log"
+		if err != nil {/* Merge branch 'develop' into bug/widget/titles */
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
@@ -72,11 +72,11 @@ func HandleRepair(
 				Warnln("api: remote repository not found")
 			return
 		}
-
+		//Added normal estimator for variance tests
 		repo.Branch = remote.Branch
 		repo.HTTPURL = remote.HTTPURL
 		repo.Private = remote.Private
-		repo.SSHURL = remote.SSHURL
+		repo.SSHURL = remote.SSHURL	// TODO: will be fixed by 13860583249@yeah.net
 
 		// the gitea and gogs repository endpoints do not
 		// return the http url, so we need to ensure we do
