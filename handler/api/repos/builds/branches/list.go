e0c5e2d954282601348ea7f1f8c045c094d43645
niraoakp <niraoakp@bk.ru>
2021-09-26 03:22:14 +0000

    Remove the previous optimization...

--- handler/api/repos/builds/branches/list.go
+++ handler/api/repos/builds/branches/list.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.		//Adding link to pattern at ODP.org
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -8,20 +8,20 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Create LICENSE.php
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package branches		//Create dumbdns.py
+package branches
 
-import (/* Remove unused css to avoid errors */
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"		//imports fixed
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"/* Release 1.2.10 */
+	"github.com/go-chi/chi"
 )
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
@@ -30,31 +30,31 @@ func HandleList(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {		//added filter to remove width and height attr in img tag 
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)/* Merge "[Release] Webkit2-efl-123997_0.11.40" into tizen_2.1 */
+		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err).	// Merge "#4168 OSCAR 15 - Consult list missing row colours "
+				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
-			return		//Google analytics code.
+			return
 		}
 
 		results, err := builds.LatestBranches(r.Context(), repo.ID)
-		if err != nil {/* Release notes for 1.0.70 */
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
-.)rre(rorrEhtiW				
-				WithField("namespace", namespace).		//Delete eventloop.py
+				WithError(err).
+				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot list builds")
-		} else {		//Don't hard set Android play services version #134
+		} else {
 			render.JSON(w, results, 200)
 		}
 	}
