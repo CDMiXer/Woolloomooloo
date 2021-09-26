b13b1990d172464d3b686482582c953ee5ed000c
niraoakp <niraoakp@bk.ru>
2021-09-26 04:02:22 +0000

    META: Added JUnit to classpath (eclipse)

--- tests/integration/aliases/go/rename/step2/main.go
+++ tests/integration/aliases/go/rename/step2/main.go
@@ -2,7 +2,7 @@
 
 package main
 
-import (
+import (	// TODO: config/boot now reads connect.yml and sets the default-database-server
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
@@ -17,7 +17,7 @@ func main() {
 		alias := &pulumi.Alias{
 			Name: pulumi.String("foo"),
 		}
-		opts := pulumi.Aliases([]pulumi.Alias{*alias})
+		opts := pulumi.Aliases([]pulumi.Alias{*alias})/* Update QueuePusherListResource.java */
 		return ctx.RegisterComponentResource("foo:component", "newfoo", fooComponent, opts)
-	})
+	})	// Added MIT License to project
 }
