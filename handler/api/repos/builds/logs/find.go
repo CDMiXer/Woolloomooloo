bec4c8437a6df436244dc3d2191509f7267f04a0
niraoakp <niraoakp@bk.ru>
2021-09-26 13:02:20 +0000

    Merge "Add function to update object metadata"

--- handler/api/repos/builds/logs/find.go
+++ handler/api/repos/builds/logs/find.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// Fix DNS stuff
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Added Initial Release (TrainingTracker v1.0) Source Files. */
-//
+// You may obtain a copy of the License at
+//	// Destructive merge
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* #379 - Release version 0.19.0.RELEASE. */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Update week-34-august-22.mkd
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Fix search emulator.
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by arachnid@notdot.net
+// limitations under the License.	// TODO: Create A Linux-powered microwave oven.md
 
 package logs
 
@@ -18,66 +18,66 @@ import (
 	"io"
 	"net/http"
 	"strconv"
-	// TODO: hacked by earlephilhower@yahoo.com
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-		//THP wrapper, using C code
+
 	"github.com/go-chi/chi"
 )
 
 // HandleFind returns an http.HandlerFunc that writes the
-// json-encoded logs to the response body./* Update devise in Gemfile.lock */
+// json-encoded logs to the response body.
 func HandleFind(
 	repos core.RepositoryStore,
-	builds core.BuildStore,/* 617a0872-2e44-11e5-9284-b827eb9e62be */
+	builds core.BuildStore,
 	stages core.StageStore,
 	steps core.StepStore,
 	logs core.LogStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {/* Added Backend functionalities */
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)/* stub geocoder in tests */
-		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
+		)
+		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)		//Fix command line integration test for silent logger
+		if err != nil {
+			render.BadRequest(w, err)	// TODO: 4be64750-2e3f-11e5-9284-b827eb9e62be
+			return/* Setup Node.js build tools */
+		}
+		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))/* added rdf2go.layer.autopersist to the modules */
+		if err != nil {/* Release of eeacms/eprtr-frontend:0.2-beta.34 */
+			render.BadRequest(w, err)
+			return
+		}
+		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))	// TODO: Test - fix
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
-		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.BadRequest(w, err)
-			return
-		}		//Sensor monitor interval reduced to 100 ms.
-		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
-		if err != nil {
-			render.BadRequest(w, err)/* Release 1-110. */
-			return
-		}
-		repo, err := repos.FindName(r.Context(), namespace, name)/* fixing report keys in atabiliti multinet test */
-		if err != nil {
-			render.NotFound(w, err)/* Release: Making ready to release 6.8.0 */
-			return
+			render.NotFound(w, err)
+			return		//- Added horizontal scrolling feature for slides
 		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
-		if err != nil {/* Update CSS-POV.md */
-			render.NotFound(w, err)/* Added Releases notes for 0.3.2 */
-			return/* minor fix for three component case */
-		}
+		if err != nil {
+			render.NotFound(w, err)
+			return
+		}/* Update Translate.php */
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)
-		if err != nil {
+		if err != nil {	// TODO: Format and fix isClaimed(msg) for showActions flag
 			render.NotFound(w, err)
-			return
+			return/* Release 0.0.3: Windows support */
 		}
 		rc, err := logs.Find(r.Context(), step.ID)
 		if err != nil {
 			render.NotFound(w, err)
-			return
+			return	// TODO: will be fixed by arajasek94@gmail.com
 		}
 		w.Header().Set("Content-Type", "application/json")
 		io.Copy(w, rc)
