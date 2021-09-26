51a5c1a719b388269c89fb614cf45b7014ced17e
niraoakp <niraoakp@bk.ru>
2021-09-26 20:38:22 +0000

    #scm-ver 0.24.0-SNAPSHOT

--- gen/examples/chat/main.go
+++ gen/examples/chat/main.go
@@ -1,32 +1,32 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-/* first bit of refactoring */
+
 package main
 
 import (
 	"flag"
 	"log"
-	"net/http"/* Create validations.php */
+	"net/http"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
 
-func serveHome(w http.ResponseWriter, r *http.Request) {/* Changed OTH Regensburg to Techbase */
+func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
-	if r.URL.Path != "/" {		//Delete Table
-		http.Error(w, "Not found", http.StatusNotFound)	// TODO: hacked by joshua@yottadb.com
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
 		return
-	}	// TODO: Fix Admin Properties when vendor is disabled for Property model
+	}
 	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)		//Remove Willow references
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
-	}	// TODO: will be fixed by magik6k@gmail.com
+	}
 	http.ServeFile(w, r, "home.html")
-}		//added check for nil
+}
 
 func main() {
-	flag.Parse()	// Merge "Rework clientmanager"
+	flag.Parse()
 	hub := newHub()
 	go hub.run()
 	http.HandleFunc("/", serveHome)
