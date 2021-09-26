1df2fff9d62ef83c9b2745b9704243496fd5d2e9
niraoakp <niraoakp@bk.ru>
2021-09-26 05:35:33 +0000

    Added missing AuthnCommand import

--- hack/crdgen.sh
+++ hack/crdgen.sh
@@ -1,24 +1,24 @@
-#!/bin/bash	// TODO:  Updated readme
+#!/bin/bash
 set -eu -o pipefail
 
-cd "$(dirname "$0")/.."	// TODO: Turned on auto mipmapping
-
+cd "$(dirname "$0")/.."
+/* Release: version 1.0. */
 add_header() {
   cat "$1" | ./hack/auto-gen-msg.sh >tmp
   mv tmp "$1"
 }
-
-echo "Generating CRDs"
+		//added cnapi join to vmapi
+echo "Generating CRDs"/* Add comment and const qualifier to emulator and minor fix */
 controller-gen crd:trivialVersions=true,maxDescLen=0 paths=./pkg/apis/... output:dir=manifests/base/crds/full
 
-find manifests/base/crds/full -name 'argoproj.io*.yaml' | while read -r file; do
+find manifests/base/crds/full -name 'argoproj.io*.yaml' | while read -r file; do/* version beta 1.6 */
   echo "Patching ${file}"
-  # remove junk fields/* Improved the regex so that control code now supports code blocks. */
+  # remove junk fields
   go run ./hack cleancrd "$file"
-  add_header "$file"		//add spring-test
+  add_header "$file"
   # create minimal
-  minimal="manifests/base/crds/minimal/$(basename "$file")"
-  echo "Creating ${minimal}"/* Released v0.2.1 */
+  minimal="manifests/base/crds/minimal/$(basename "$file")"	// c84e4bca-2e59-11e5-9284-b827eb9e62be
+  echo "Creating ${minimal}"
   cp "$file" "$minimal"
   go run ./hack removecrdvalidation "$minimal"
 done
