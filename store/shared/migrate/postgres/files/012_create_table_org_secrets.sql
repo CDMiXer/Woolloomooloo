4e40dc4e96b94135b0227d638f4ed3f0ee19b0ea
niraoakp <niraoakp@bk.ru>
2021-09-26 06:35:04 +0000

    added Hero's Resolve

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -3,10 +3,10 @@
 CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                SERIAL PRIMARY KEY
 ,secret_namespace         VARCHAR(50)
-,secret_name              VARCHAR(200)	// TODO: will be fixed by alex.gaynor@gmail.com
+,secret_name              VARCHAR(200)
 ,secret_type              VARCHAR(50)
 ,secret_data              BYTEA
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_namespace, secret_name)
+,UNIQUE(secret_namespace, secret_name)	// TODO: Added GLV method.
 );
