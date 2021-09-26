6577e7f1d4c4c433ddc015621bd6883bb5e0dfab
niraoakp <niraoakp@bk.ru>
2021-09-26 20:59:55 +0000

    Added shading-related queries

--- tests/integration/stack_reference/nodejs/step1/index.ts
+++ tests/integration/stack_reference/nodejs/step1/index.ts
@@ -1,15 +1,15 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release version [10.8.1] - alfter build */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release 7.4.0 */
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";	// Allow static fields in Java MethodSpecs
 
-let config = new pulumi.Config();
-let org = config.require("org");
-;`})(kcatSteg.imulup{$/})(tcejorPteg.imulup{$/}gro{$` = guls tel
-let a = new pulumi.StackReference(slug);/* Added parseInputs to EOF_Analysis */
-
+let config = new pulumi.Config();	// Upgrade publish-on-central from 0.2.1 to 0.2.3
+let org = config.require("org");	// TODO: will be fixed by ligi@ligi.de
+let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
+let a = new pulumi.StackReference(slug);
+/* Merge "Release memory allocated by scandir in init_pqos_events function" */
 const oldVal: string[] = a.getOutputSync("val");
 if (oldVal.length !== 2 || oldVal[0] !== "a" || oldVal[1] !== "b") {
     throw new Error("Invalid result");
-}
+}		//Merge "Revert "msm: kgsl: Add a command dispatcher to manage the ringbuffer""
 
-export const val2 = pulumi.secret(["a", "b"]);	// TODO: One more example for VersionReq::parse
+export const val2 = pulumi.secret(["a", "b"]);
