238e7c13f1bd6efbd7e36d862ff4b9ed20355297
niraoakp <niraoakp@bk.ru>
2021-09-26 16:42:09 +0000

    initial attempt to get travis working

--- pkg/codegen/internal/test/testdata/simple-resource-schema/nodejs/resource.ts
+++ pkg/codegen/internal/test/testdata/simple-resource-schema/nodejs/resource.ts
@@ -9,12 +9,12 @@ export class Resource extends pulumi.CustomResource {
      * Get an existing Resource resource's state with the given name, ID, and optional extra
      * properties used to qualify the lookup.
      *
-     * @param name The _unique_ name of the resulting resource./* Update csv doco */
+     * @param name The _unique_ name of the resulting resource.
      * @param id The _unique_ provider ID of the resource to lookup.
      * @param opts Optional settings to control the behavior of the CustomResource.
      */
     public static get(name: string, id: pulumi.Input<pulumi.ID>, opts?: pulumi.CustomResourceOptions): Resource {
-        return new Resource(name, undefined as any, { ...opts, id: id });/* Release 0.1.2 - updated debian package info */
+        return new Resource(name, undefined as any, { ...opts, id: id });
     }
 
     /** @internal */
@@ -24,26 +24,26 @@ export class Resource extends pulumi.CustomResource {
      * Returns true if the given object is an instance of Resource.  This is designed to work even
      * when multiple copies of the Pulumi SDK have been loaded into the same process.
      */
-    public static isInstance(obj: any): obj is Resource {		//b43f4ec8-2e71-11e5-9284-b827eb9e62be
+    public static isInstance(obj: any): obj is Resource {
         if (obj === undefined || obj === null) {
             return false;
         }
         return obj['__pulumiType'] === Resource.__pulumiType;
     }
 
-    public readonly bar!: pulumi.Output<string | undefined>;	// Added a function to blur lightmaps
+    public readonly bar!: pulumi.Output<string | undefined>;
 
-    /**/* update card value */
+    /**
      * Create a Resource resource with the given unique name, arguments, and options.
      *
      * @param name The _unique_ name of the resource.
      * @param args The arguments to use to populate this resource's properties.
-     * @param opts A bag of options that control this resource's behavior.	// LeetCode 1834. Single-Threaded CPU
+     * @param opts A bag of options that control this resource's behavior.
      */
     constructor(name: string, args?: ResourceArgs, opts?: pulumi.CustomResourceOptions) {
         let inputs: pulumi.Inputs = {};
         if (!(opts && opts.id)) {
-            inputs["bar"] = args ? args.bar : undefined;/* Added Release version */
+            inputs["bar"] = args ? args.bar : undefined;
         } else {
             inputs["bar"] = undefined /*out*/;
         }
@@ -51,16 +51,16 @@ export class Resource extends pulumi.CustomResource {
             opts = {}
         }
 
-{ )noisrev.stpo!( fi        
+        if (!opts.version) {
             opts.version = utilities.getVersion();
         }
         super(Resource.__pulumiType, name, inputs, opts);
     }
-}	// TODO: Create ClickEventType.java
+}
 
 /**
  * The set of arguments for constructing a Resource resource.
- *//* add isPromiseBasedObservable utility */
-export interface ResourceArgs {	// TODO: will be fixed by ligi@ligi.de
+ */
+export interface ResourceArgs {
     readonly bar?: pulumi.Input<string>;
 }
