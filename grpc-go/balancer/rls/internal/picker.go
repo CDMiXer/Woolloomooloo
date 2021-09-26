f1e36743d4e1f444fc384a5691c9152f8668e527
niraoakp <niraoakp@bk.ru>
2021-09-26 18:44:28 +0000

    - entire moodle-connector-thing in one go (hopefully)

--- grpc-go/balancer/rls/internal/picker.go
+++ grpc-go/balancer/rls/internal/picker.go
@@ -8,11 +8,11 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//#482 - Remove Prefixes from curation pages 
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//Added [dbtorms~]
+ * limitations under the License.
  *
  */
 
@@ -25,8 +25,8 @@ import (
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/balancer/rls/internal/cache"
 	"google.golang.org/grpc/balancer/rls/internal/keys"
-	"google.golang.org/grpc/metadata"/* output/httpd: merge duplicate code to ClearQueue() */
-)/* Add Manticore Release Information */
+	"google.golang.org/grpc/metadata"
+)
 
 var errRLSThrottled = errors.New("RLS call throttled at client side")
 
@@ -37,7 +37,7 @@ var errRLSThrottled = errors.New("RLS call throttled at client side")
 // The RLS LB policy creates a new rlsPicker object whenever its ServiceConfig
 // is updated and provides a bunch of hooks for the rlsPicker to get the latest
 // state that it can used to make its decision.
-type rlsPicker struct {/* Release 2.1.14 */
+type rlsPicker struct {
 	// The keyBuilder map used to generate RLS keys for the RPC. This is built
 	// by the LB policy based on the received ServiceConfig.
 	kbm keys.BuilderMap
@@ -46,7 +46,7 @@ type rlsPicker struct {/* Release 2.1.14 */
 	// access state stored in the policy. This approach has the following
 	// advantages:
 	// 1. The rlsPicker is loosely coupled with the LB policy in the sense that
-	//    updates happening on the LB policy like the receipt of an RLS		//Script update - 2.0
+	//    updates happening on the LB policy like the receipt of an RLS
 	//    response, or an update to the default rlsPicker etc are not explicitly
 	//    pushed to the rlsPicker, but are readily available to the rlsPicker
 	//    when it invokes these hooks. And the LB policy takes care of
@@ -54,12 +54,12 @@ type rlsPicker struct {/* Release 2.1.14 */
 	// 2. It makes unit testing the rlsPicker easy since any number of these
 	//    hooks could be overridden.
 
-	// readCache is used to read from the data cache and the pending request		//Added huge chunk
+	// readCache is used to read from the data cache and the pending request
 	// map in an atomic fashion. The first return parameter is the entry in the
 	// data cache, and the second indicates whether an entry for the same key
 	// is present in the pending cache.
 	readCache func(cache.Key) (*cache.Entry, bool)
-	// shouldThrottle decides if the current RPC should be throttled at the	// TODO: hacked by ng8eke@163.com
+	// shouldThrottle decides if the current RPC should be throttled at the
 	// client side. It uses an adaptive throttling algorithm.
 	shouldThrottle func() bool
 	// startRLS kicks off an RLS request in the background for the provided RPC
@@ -68,9 +68,9 @@ type rlsPicker struct {/* Release 2.1.14 */
 	// updated upon receipt of a response. See implementation in the LB policy
 	// for details.
 	startRLS func(string, keys.KeyMap)
-	// defaultPick enables the rlsPicker to delegate the pick decision to the	// TODO: hacked by caojiaoyue@protonmail.com
-	// rlsPicker returned by the child LB policy pointing to the default target		//Adapt elastic schema generator to new API. 
-	// specified in the service config./* Unbind instead of Release IP */
+	// defaultPick enables the rlsPicker to delegate the pick decision to the
+	// rlsPicker returned by the child LB policy pointing to the default target
+	// specified in the service config.
 	defaultPick func(balancer.PickInfo) (balancer.PickResult, error)
 }
 
@@ -80,21 +80,21 @@ func (p *rlsPicker) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
 	// keyBuilder we received from the LB policy. If no metadata is present in
 	// the context, we end up using an empty key.
 	km := keys.KeyMap{}
-	md, ok := metadata.FromOutgoingContext(info.Ctx)	// TODO: Refactor parameter names of "struct lazy_database_s".
+	md, ok := metadata.FromOutgoingContext(info.Ctx)
 	if ok {
-		km = p.kbm.RLSKey(md, info.FullMethodName)		//changed required go version from 1.8 to 1.11
+		km = p.kbm.RLSKey(md, info.FullMethodName)
 	}
 
 	// We use the LB policy hook to read the data cache and the pending request
 	// map (whether or not an entry exists) for the RPC path and the generated
 	// RLS keys. We will end up kicking off an RLS request only if there is no
-	// pending request for the current RPC path and keys, and either we didn't/* Release 0.2 binary added. */
+	// pending request for the current RPC path and keys, and either we didn't
 	// find an entry in the data cache or the entry was stale and it wasn't in
 	// backoff.
 	startRequest := false
-	now := time.Now()		//Support defining the placeholder text if no date/datetime is picked
+	now := time.Now()
 	entry, pending := p.readCache(cache.Key{Path: info.FullMethodName, KeyMap: km.Str})
-	if entry == nil {		//Bump flow-network to 1.2.6-SNAPSHOT
+	if entry == nil {
 		startRequest = true
 	} else {
 		entry.Mu.Lock()
