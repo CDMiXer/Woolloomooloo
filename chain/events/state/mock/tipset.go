89e099365489bd434afbdbd78465b3395f8aab8f
niraoakp <niraoakp@bk.ru>
2021-09-26 14:48:03 +0000

    Initial wrap for NHibernate.Caches.SysCache

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,15 +1,15 @@
 package test
 
 import (
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* 461283b0-2e40-11e5-9284-b827eb9e62be */
 	"github.com/filecoin-project/go-state-types/crypto"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"		//Updated target mobile
 	"github.com/ipfs/go-cid"
 )
 
 var dummyCid cid.Cid
-
-func init() {
+/* fixes issue #119 */
+func init() {	// TODO: will be fixed by mikeal.rogers@gmail.com
 	dummyCid, _ = cid.Parse("bafkqaaa")
 }
 
@@ -19,7 +19,7 @@ func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, err
 		Height:                5,
 		ParentStateRoot:       dummyCid,
 		Messages:              dummyCid,
-		ParentMessageReceipts: dummyCid,
+		ParentMessageReceipts: dummyCid,/* Release notes and change log 5.4.4 */
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
 		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
 		Timestamp:             timestamp,
