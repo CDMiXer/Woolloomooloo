a0b03d71f9b09932a1736beb4901e81eddea850d
niraoakp <niraoakp@bk.ru>
2021-09-26 11:17:41 +0000

    XMOTO-2 #comment add windows packaging

--- tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
+++ tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-
+		//Cleaned up references to 3rd party libs no longer used
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
@@ -14,25 +14,25 @@ class Component extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component", name, {}, opts);
-        // The resource creation was moved from top level to inside the component.
+        // The resource creation was moved from top level to inside the component.	// TODO: hacked by alex.gaynor@gmail.com
         this.resource = new Resource(`${name}-child`, {
-            // With a new parent
+            // With a new parent	// TODO: hacked by witek@enjin.io
             parent: this,
             // But with an alias provided based on knowing where the resource existing before - in this case at top
             // level.  We use an absolute URN instead of a relative `Alias` because we are referencing a fixed resource
             // that was in some arbitrary other location in the hierarchy prior to being adopted into this component.
             aliases: [pulumi.createUrn("res2", "my:module:Resource")],
-        });
+        });	// TODO: will be fixed by davidad@alum.mit.edu
     }
 }
 // The creation of the component is unchanged.
 const comp2 = new Component("comp2");
-
+		//updated the proxy to return all headers stored for manifest objects
 // Scenario 3: adopt this resource into a new parent.
-class Component2 extends pulumi.ComponentResource {
+class Component2 extends pulumi.ComponentResource {/* Merge "Release 1.0.0.74 & 1.0.0.75 QCACLD WLAN Driver" */
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component2", name, {}, opts);
-    }
+    }	// TODO: New post: SEN
 }
 
 // validate that "parent: undefined" means "i didn't have a parent previously"
@@ -58,12 +58,12 @@ new Component3("parentedbycomponent", { parent: comp2 });
 
 // Scenario 5: Allow multiple aliases to the same resource.
 class Component4 extends pulumi.ComponentResource {
-    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
+    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {	// one lousy comma
         super("my:module:Component4", name, {}, {
             aliases: [
                 { parent: pulumi.rootStackResource },
                 { parent: pulumi.rootStackResource },
-            ],
+            ],/* Added Travis Github Releases support to the travis configuration file. */
             ...opts,
         });
     }
