4c75c7db8388152206179d31222a42a52b7c44d4
niraoakp <niraoakp@bk.ru>
2021-09-26 13:44:22 +0000

    c7f1a50a-2e63-11e5-9284-b827eb9e62be

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eu -o pipefail
-
+	// TODO: Use less registers if div/rem with longs and divisor is power of two
 for m in $*; do
   MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
   MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')
