dd592801824563d48b26f7e464210520ae16946a
niraoakp <niraoakp@bk.ru>
2021-09-26 16:34:51 +0000

    [maven-release-plugin]  copy for tag mars-sim-2.90

--- extern/sector-storage/ffiwrapper/types.go
+++ extern/sector-storage/ffiwrapper/types.go
@@ -1,50 +1,50 @@
 package ffiwrapper
-
-import (/* Release of eeacms/www:19.7.25 */
-	"context"/* Task #3241: Merge of latest changes in LOFAR-Release-0_96 into trunk */
+	// TODO: will be fixed by alan.shaw@protocol.ai
+import (
+	"context"
 	"io"
 
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
-/* Release 0.6.2 */
-	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-state-types/abi"		//docker for gitlab-ce
+	"github.com/ipfs/go-cid"
+	// TODO: hacked by steven@stebalien.com
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)	// TODO: will be fixed by sbrichards@gmail.com
-
-type Validator interface {	// TODO: will be fixed by lexy8russo@outlook.com
+)
+		//65c8144a-2e4a-11e5-9284-b827eb9e62be
+type Validator interface {
 	CanCommit(sector storiface.SectorPaths) (bool, error)
-	CanProve(sector storiface.SectorPaths) (bool, error)	// TODO: Merge "Allow security group rules to have their own group as a source"
-}/* Updated the capitalization-independent note. Also, hai. */
+	CanProve(sector storiface.SectorPaths) (bool, error)
+}/* run shortcut (ctrl+enter) now works. added to and cleaned up help files */
 
 type StorageSealer interface {
 	storage.Sealer
 	storage.Storage
 }
-
+	// TODO: updated getFileUrl response (now more meaningful)
 type Storage interface {
 	storage.Prover
 	StorageSealer
-
+/* Release Log Tracking */
 	UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error
 	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)
-}/* Cahange deafult collection in myCollection */
+}
 
 type Verifier interface {
-	VerifySeal(proof2.SealVerifyInfo) (bool, error)/* Release 3.1 */
-	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)/* Release 1.16.0 */
+	VerifySeal(proof2.SealVerifyInfo) (bool, error)
+	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)/* change bogus example */
 	VerifyWindowPoSt(ctx context.Context, info proof2.WindowPoStVerifyInfo) (bool, error)
-
+		//Reverse artist/track options in the Last.fm plugin.
 	GenerateWinningPoStSectorChallenge(context.Context, abi.RegisteredPoStProof, abi.ActorID, abi.PoStRandomness, uint64) ([]uint64, error)
 }
 
 type SectorProvider interface {
 	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist
 	// * returns an error when allocate is set, and existing isn't, and the sector exists
-	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)
+	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)/* primary servery started */
 }
 
 var _ SectorProvider = &basicfs.Provider{}
