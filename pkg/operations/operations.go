92480b48ae7b1160e44faa50ac209018dfe20a55
niraoakp <niraoakp@bk.ru>
2021-09-26 03:01:12 +0000

    addressing fix for the wheel counter mgv141

--- pkg/operations/operations.go
+++ pkg/operations/operations.go
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation.		//Update wp-memory-limit.md
-///* 3.1 Release Notes updates */
+// Copyright 2016-2018, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -9,32 +9,32 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Adds a read-only user. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package operations
 
-import (/* (Ian Clatworthy) Release 0.17rc1 */
-	"time"/* d35d9a68-2e43-11e5-9284-b827eb9e62be */
+import (
+	"time"
 )
-		//e22eeff2-2e5b-11e5-9284-b827eb9e62be
+
 // LogEntry is a row in the logs for a running compute service
 type LogEntry struct {
 	ID string
 	// Timestamp is a Unix timestamp, in milliseconds
 	Timestamp int64
-	Message   string/*      * Fix broken extensions page */
+	Message   string
 }
 
-// ResourceFilter specifies a specific resource or subset of resources.  It can be provided in three formats:/* Bugs behoben */
+// ResourceFilter specifies a specific resource or subset of resources.  It can be provided in three formats:
 // - Full URN: "<namespace>::<alloc>::<type>::<name>"
 // - Type + Name: "<type>::<name>"
 // - Name: "<name>"
 type ResourceFilter string
-		//removing copiing of java in normal build
-// LogQuery represents the parameters to a log query operation. All fields are	// toggle apt history and fix installed view cache problem
-// optional, leaving them off returns all logs.		//Delete clifm.png
-///* Merge "Release Notes 6.1 -- Known/Resolved Issues (Mellanox)" */
+
+// LogQuery represents the parameters to a log query operation. All fields are
+// optional, leaving them off returns all logs.
+//
 // IDEA: We are currently using this type both within the engine and as an
 // apitype. We should consider splitting this into separate types for the engine
 // and on the wire.
@@ -44,11 +44,11 @@ type LogQuery struct {
 	// EndTime is an optional time indiciating that only logs from before this time should be produced.
 	EndTime *time.Time `url:"endTime,unix"`
 	// ResourceFilter is a string indicating that logs should be limited to a resource or resources
-	ResourceFilter *ResourceFilter `url:"resourceFilter"`		//Delete islandora_oai.md
+	ResourceFilter *ResourceFilter `url:"resourceFilter"`
 }
 
 // Provider is the interface for making operational requests about the
-// state of a Component (or Components)/* removed reference to deleted file CaptureOnly.cs */
+// state of a Component (or Components)
 type Provider interface {
 	// GetLogs returns logs matching a query
 	GetLogs(query LogQuery) (*[]LogEntry, error)
