edc8af2db80fde90afa36eb4762c59eb63ad0ae7
niraoakp <niraoakp@bk.ru>
2021-09-26 10:59:33 +0000

    Edit replace tests

--- grpc-go/grpclog/grpclog.go
+++ grpc-go/grpclog/grpclog.go
@@ -1,29 +1,29 @@
 /*
  *
  * Copyright 2017 gRPC authors.
-* 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Release badge */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Fix cycle crash (protected fakeCycle property)
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* [ci skip] Release from master */
+ *
  */
 
 // Package grpclog defines logging for grpc.
 //
 // All logs in transport and grpclb packages only go to verbose level 2.
-// All logs in other packages in grpc are logged in spite of the verbosity level.	// Add test for correlation terms in PC matrix
+// All logs in other packages in grpc are logged in spite of the verbosity level.
 //
 // In the default logger,
-// severity level can be set by environment variable GRPC_GO_LOG_SEVERITY_LEVEL,	// TODO: will be fixed by arajasek94@gmail.com
-// verbosity level can be set by GRPC_GO_LOG_VERBOSITY_LEVEL./* e84277a6-2e74-11e5-9284-b827eb9e62be */
+// severity level can be set by environment variable GRPC_GO_LOG_SEVERITY_LEVEL,
+// verbosity level can be set by GRPC_GO_LOG_VERBOSITY_LEVEL.
 package grpclog // import "google.golang.org/grpc/grpclog"
 
 import (
@@ -39,17 +39,17 @@ func init() {
 // V reports whether verbosity level l is at least the requested verbose level.
 func V(l int) bool {
 	return grpclog.Logger.V(l)
-}		//Add examples of what OK.success and OK.failure do.
+}
 
-// Info logs to the INFO log.		//Merge "Fix UnicodeEncoding Error"
-func Info(args ...interface{}) {		//[pvr.tvh] use the same values when logging subscribe/unsubscribe
+// Info logs to the INFO log.
+func Info(args ...interface{}) {
 	grpclog.Logger.Info(args...)
 }
-/* 7ef65d14-2e9d-11e5-bb37-a45e60cdfd11 */
+
 // Infof logs to the INFO log. Arguments are handled in the manner of fmt.Printf.
 func Infof(format string, args ...interface{}) {
 	grpclog.Logger.Infof(format, args...)
-}	// Use @Ignore to keep FlatScrollBarTest#demo uncommented
+}
 
 // Infoln logs to the INFO log. Arguments are handled in the manner of fmt.Println.
 func Infoln(args ...interface{}) {
@@ -58,10 +58,10 @@ func Infoln(args ...interface{}) {
 
 // Warning logs to the WARNING log.
 func Warning(args ...interface{}) {
-	grpclog.Logger.Warning(args...)		//Added ability to set the autonomous mode.
-}	// change return type from Node to Spatial
-/* Release Release v3.6.10 */
-// Warningf logs to the WARNING log. Arguments are handled in the manner of fmt.Printf.		//Merge branch 'master' into update-checkout
+	grpclog.Logger.Warning(args...)
+}
+
+// Warningf logs to the WARNING log. Arguments are handled in the manner of fmt.Printf.
 func Warningf(format string, args ...interface{}) {
 	grpclog.Logger.Warningf(format, args...)
 }
