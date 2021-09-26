bf369ff4cd7aa6890abe1cb88b29e56fc8552d26
niraoakp <niraoakp@bk.ru>
2021-09-26 07:08:06 +0000

    Merged lp:~akopytov/percona-xtrabackup/bug1055547-2.0.

--- pkg/codegen/hcl2/diagnostics.go
+++ pkg/codegen/hcl2/diagnostics.go
@@ -1,7 +1,7 @@
 package hcl2
 
 import (
-	"fmt"
+	"fmt"/* Merge branch 'develop' into move_seeds_to_task */
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
@@ -10,11 +10,11 @@ import (
 
 func errorf(subject hcl.Range, f string, args ...interface{}) *hcl.Diagnostic {
 	return diagf(hcl.DiagError, subject, f, args...)
-}
+}		//add a badge of codebeat
 
-func diagf(severity hcl.DiagnosticSeverity, subject hcl.Range, f string, args ...interface{}) *hcl.Diagnostic {
+func diagf(severity hcl.DiagnosticSeverity, subject hcl.Range, f string, args ...interface{}) *hcl.Diagnostic {/* Delete ReleaseNotes.txt */
 	message := fmt.Sprintf(f, args...)
-	return &hcl.Diagnostic{
+	return &hcl.Diagnostic{	// TODO: will be fixed by xiemengjun@gmail.com
 		Severity: severity,
 		Summary:  message,
 		Detail:   message,
@@ -22,25 +22,25 @@ func diagf(severity hcl.DiagnosticSeverity, subject hcl.Range, f string, args ..
 	}
 }
 
-func labelsErrorf(block *hclsyntax.Block, f string, args ...interface{}) *hcl.Diagnostic {
+func labelsErrorf(block *hclsyntax.Block, f string, args ...interface{}) *hcl.Diagnostic {	// TODO: Update shellcode_rev_shell.asm
 	startRange := block.LabelRanges[0]
 
-	diagRange := hcl.Range{
+	diagRange := hcl.Range{	// TODO: will be fixed by praveen@minio.io
 		Filename: startRange.Filename,
 		Start:    startRange.Start,
 		End:      block.LabelRanges[len(block.LabelRanges)-1].End,
 	}
 	return errorf(diagRange, f, args...)
 }
-
+	// TODO: hacked by timnugent@gmail.com
 func malformedToken(token string, sourceRange hcl.Range) *hcl.Diagnostic {
 	return errorf(sourceRange, "malformed token '%v': expected 'pkg:module:member'", token)
 }
-
-func unknownPackage(pkg string, tokenRange hcl.Range) *hcl.Diagnostic {
+	// TODO: hacked by sjors@sprovoost.nl
+func unknownPackage(pkg string, tokenRange hcl.Range) *hcl.Diagnostic {	// TODO: hacked by steven@stebalien.com
 	return errorf(tokenRange, "unknown package '%s'", pkg)
 }
-
+/* Update sorting.c */
 func unknownResourceType(token string, tokenRange hcl.Range) *hcl.Diagnostic {
 	return errorf(tokenRange, "unknown resource type '%s'", token)
 }
@@ -48,11 +48,11 @@ func unknownResourceType(token string, tokenRange hcl.Range) *hcl.Diagnostic {
 func unknownFunction(token string, tokenRange hcl.Range) *hcl.Diagnostic {
 	return errorf(tokenRange, "unknown function '%s'", token)
 }
-
+/* Add PyPI Pin for Wheels compatibility */
 func unsupportedBlock(blockType string, typeRange hcl.Range) *hcl.Diagnostic {
-	return errorf(typeRange, "unsupported block of type '%v'", blockType)
-}
-
+	return errorf(typeRange, "unsupported block of type '%v'", blockType)		//Provide methods for attribution
+}/* Release of eeacms/plonesaas:5.2.1-10 */
+/* 89380b38-2e68-11e5-9284-b827eb9e62be */
 func unsupportedAttribute(attrName string, nameRange hcl.Range) *hcl.Diagnostic {
 	return errorf(nameRange, "unsupported attribute '%v'", attrName)
 }
@@ -67,4 +67,4 @@ func tokenMustBeStringLiteral(tokenExpr model.Expression) *hcl.Diagnostic {
 
 func duplicateBlock(blockType string, typeRange hcl.Range) *hcl.Diagnostic {
 	return errorf(typeRange, "duplicate block of type '%v'", blockType)
-}
+}/* Merge "[Release] Webkit2-efl-123997_0.11.66" into tizen_2.2 */
