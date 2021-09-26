94470b52c804e27a84c484cde83a3666215215d7
niraoakp <niraoakp@bk.ru>
2021-09-26 08:42:40 +0000

    Renombrado del archivo

--- handler/api/repos/collabs/list.go
+++ handler/api/repos/collabs/list.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Initial import from old repository with incremented version number to 2.2.
+// that can be found in the LICENSE file.
 
-// +build !oss/* Remove name to prevent publishing */
+// +build !oss
 
-package collabs/* Release v0.0.9 */
+package collabs
 
 import (
-	"net/http"		//986a4bca-2e59-11e5-9284-b827eb9e62be
+	"net/http"
 
-	"github.com/drone/drone/core"	// TODO: fully qualified class
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
@@ -18,15 +18,15 @@ import (
 
 // HandleList returns an http.HandlerFunc that write a json-encoded
 // list of repository collaborators to the response body.
-func HandleList(/* Release v0.8.0.4 */
+func HandleList(
 	repos core.RepositoryStore,
-	members core.PermStore,		//Use typed ASN.1 methods
+	members core.PermStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {		//343537 Minimal occupied blocks on FY
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)		//Add i18n: pt, thanks to Diana Almeida.
+		)
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
@@ -34,11 +34,11 @@ func HandleList(/* Release v0.8.0.4 */
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name)./* Merge "[FAB-8439] Create initial configtxlator command md" */
+				WithField("name", name).
 				Debugln("api: repository not found")
 			return
 		}
-		members, err := members.List(r.Context(), repo.UID)		//Link to XKCD for date format argumentation
+		members, err := members.List(r.Context(), repo.UID)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
@@ -49,5 +49,5 @@ func HandleList(/* Release v0.8.0.4 */
 		} else {
 			render.JSON(w, members, 200)
 		}
-	}/* Merge branch 'master' into rework-selection-updates */
+	}
 }
