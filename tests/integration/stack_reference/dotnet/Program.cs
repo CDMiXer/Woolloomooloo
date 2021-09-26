62dcebab65514bae6d9682f46933ade33f037a27
niraoakp <niraoakp@bk.ru>
2021-09-26 04:00:48 +0000

    btool: Fix multiply included test file header

--- tests/integration/stack_reference/dotnet/Program.cs
+++ tests/integration/stack_reference/dotnet/Program.cs
@@ -2,19 +2,19 @@
 
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;	// TODO: hacked by timnugent@gmail.com
+using System.Threading.Tasks;
 using Pulumi;
-/* Modified FSE_decodeByteFast() interface */
+
 class Program
-{/* Release 2.0.13 */
+{
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(async () =>
         {
             var config = new Config();
             var org = config.Require("org");
-            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";		//Don't use nontransaction object at all after commit.
-            var a = new StackReference(slug);		//Add globals and airbnb ref.
+            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
+            var a = new StackReference(slug);
 
             return new Dictionary<string, object>
             {
