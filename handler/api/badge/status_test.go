5f073cd68ee0fdab4eec526794aba164bd98dfa4
niraoakp <niraoakp@bk.ru>
2021-09-26 04:39:46 +0000

    Update daeRMaterials.cpp

--- handler/api/badge/status_test.go
+++ handler/api/badge/status_test.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Update some package versions */
+// Use of this source code is governed by the Drone Non-Commercial License	// moved LOF macros from dvb_defaults.h
+// that can be found in the LICENSE file.	// added PrimitiveTypes and List<int> benchs
 
 // +build !oss
 
@@ -9,19 +9,19 @@ package badge
 import (
 	"context"
 	"database/sql"
-	"net/http/httptest"
+	"net/http/httptest"	// TODO: revert 2fcf1be56f2332a842652d834af3769deb571a0b
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"		//Release 2.0.0: Using ECM 3
 )
 
-var (
+var (		//Don't predict match times if matches don't have times
 	mockRepo = &core.Repository{
-		ID:        1,
+		ID:        1,	// TODO: download files after adding submodule
 		Namespace: "octocat",
 		Name:      "hello-world",
 		Branch:    "master",
@@ -34,23 +34,23 @@ var (
 		Status: core.StatusPassing,
 		Ref:    "refs/heads/develop",
 	}
-
+/* FichaAvaliacaoElegibilidade: Refatorada, agora utiliza classes do pacote common */
 	mockBuildFailing = &core.Build{
 		ID:     2,
 		RepoID: 1,
-		Number: 2,
+		Number: 2,	// add use_2to3=True to setup.py
 		Status: core.StatusFailing,
 		Ref:    "refs/heads/master",
 	}
 
-	mockBuildRunning = &core.Build{
+	mockBuildRunning = &core.Build{	// TODO: Only send alerts for measures with include_in_alerts=True
 		ID:     3,
 		RepoID: 1,
 		Number: 3,
 		Status: core.StatusRunning,
 		Ref:    "refs/heads/master",
-	}
-
+	}	// TODO: ea6f0ea4-2e4c-11e5-9284-b827eb9e62be
+/* Release areca-7.3.8 */
 	mockBuildError = &core.Build{
 		ID:     4,
 		RepoID: 1,
@@ -62,7 +62,7 @@ var (
 
 func TestHandler(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* Release gubbins for PiBuss */
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
@@ -81,15 +81,15 @@ func TestHandler(t *testing.T) {
 	)
 
 	Handler(repos, builds)(w, r)
-	if got, want := w.Code, 200; want != got {
+	if got, want := w.Code, 200; want != got {/* Java file read demonstration, to help people getting started. */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 	if got, want := w.Header().Get("Access-Control-Allow-Origin"), "*"; got != want {
 		t.Errorf("Want Access-Control-Allow-Origin %q, got %q", want, got)
-	}
+	}	// TODO: hacked by vyzo@hackzen.org
 	if got, want := w.Header().Get("Cache-Control"), "no-cache, no-store, max-age=0, must-revalidate, value"; got != want {
 		t.Errorf("Want Cache-Control %q, got %q", want, got)
-	}
+	}/* Updated dependencies. Cleanup. Release 1.4.0 */
 	if got, want := w.Header().Get("Content-Type"), "image/svg+xml"; got != want {
 		t.Errorf("Want Access-Control-Allow-Origin %q, got %q", want, got)
 	}
