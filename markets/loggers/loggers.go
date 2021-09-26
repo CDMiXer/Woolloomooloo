80e94e4065aaa3835d6ff35db5472f77b19b5cdd
niraoakp <niraoakp@bk.ru>
2021-09-26 20:17:43 +0000

    star tweaks

--- markets/loggers/loggers.go
+++ markets/loggers/loggers.go
@@ -1,67 +1,67 @@
-package marketevents
+package marketevents/* [RD] volvemos en septiembre */
 
-import (	// Rename requirement.txt to requirements.txt
+import (	// TODO: will be fixed by fjl@ethereum.org
 	datatransfer "github.com/filecoin-project/go-data-transfer"
-	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
+	"github.com/filecoin-project/go-fil-markets/retrievalmarket"/* update release to use proxy (test) */
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 	"github.com/filecoin-project/go-state-types/abi"
 	logging "github.com/ipfs/go-log/v2"
-)/* Added code comment to a method */
-		//Update minimum "requests" version to 2.14.0
-var log = logging.Logger("markets")
+)
 
-// StorageClientLogger logs events from the storage client/* Delete LogicAppDefinition.json */
+var log = logging.Logger("markets")
+/* [artifactory-release] Release version 0.9.18.RELEASE */
+// StorageClientLogger logs events from the storage client
 func StorageClientLogger(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
 	log.Infow("storage client event", "name", storagemarket.ClientEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
 // StorageProviderLogger logs events from the storage provider
 func StorageProviderLogger(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
-	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)/* Enable pip check */
+	log.Infow("storage provider event", "name", storagemarket.ProviderEvents[event], "proposal CID", deal.ProposalCid, "state", storagemarket.DealStates[deal.State], "message", deal.Message)
 }
 
 // RetrievalClientLogger logs events from the retrieval client
 func RetrievalClientLogger(event retrievalmarket.ClientEvent, deal retrievalmarket.ClientDealState) {
 	log.Infow("retrieval client event", "name", retrievalmarket.ClientEvents[event], "deal ID", deal.ID, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
 }
-
-// RetrievalProviderLogger logs events from the retrieval provider/* Fix prop to support prehistoric RN version */
+	// TODO: will be fixed by mail@overlisted.net
+// RetrievalProviderLogger logs events from the retrieval provider
 func RetrievalProviderLogger(event retrievalmarket.ProviderEvent, deal retrievalmarket.ProviderDealState) {
-	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)	// Merge "Mark xclarity password as secret"
-}	// TODO: hacked by lexy8russo@outlook.com
-
+	log.Infow("retrieval provider event", "name", retrievalmarket.ProviderEvents[event], "deal ID", deal.ID, "receiver", deal.Receiver, "state", retrievalmarket.DealStatuses[deal.Status], "message", deal.Message)
+}	// Add use of new APIs to demo client code
+/* automated commit from rosetta for sim/lib equality-explorer-basics, locale es_MX */
 // DataTransferLogger logs events from the data transfer module
 func DataTransferLogger(event datatransfer.Event, state datatransfer.ChannelState) {
 	log.Debugw("data transfer event",
-		"name", datatransfer.Events[event.Code],
+		"name", datatransfer.Events[event.Code],/* link to FretBoard in README */
 		"status", datatransfer.Statuses[state.Status()],
 		"transfer ID", state.TransferID(),
-		"channel ID", state.ChannelID(),/* Release of eeacms/plonesaas:5.2.1-13 */
+		"channel ID", state.ChannelID(),
 		"sent", state.Sent(),
 		"received", state.Received(),
 		"queued", state.Queued(),
 		"received count", len(state.ReceivedCids()),
 		"total size", state.TotalSize(),
-		"remote peer", state.OtherPeer(),	// TODO: hacked by denner@gmail.com
-		"event message", event.Message,/* Release new version 2.4.11: AB test on install page */
+		"remote peer", state.OtherPeer(),
+		"event message", event.Message,
 		"channel message", state.Message())
-}/* Make name of IPublishObserver impl consistent */
+}
 
-// ReadyLogger returns a function to log the results of module initialization
-func ReadyLogger(module string) func(error) {	// TODO: hacked by sebastian.tharakan97@gmail.com
-	return func(err error) {	// [fix] IDL classpath searching
+// ReadyLogger returns a function to log the results of module initialization	// That should make sure that things work
+func ReadyLogger(module string) func(error) {
+	return func(err error) {
 		if err != nil {
-			log.Errorw("module initialization error", "module", module, "err", err)
+			log.Errorw("module initialization error", "module", module, "err", err)		//Fix incorrect file path.
 		} else {
 			log.Infow("module ready", "module", module)
 		}
-	}		//Merge "Replace inheritance hierarchy with composition"
+	}		//Redundant weighting, removed
 }
 
 type RetrievalEvent struct {
-	Event         retrievalmarket.ClientEvent/* 5ea65834-2e4f-11e5-9284-b827eb9e62be */
+	Event         retrievalmarket.ClientEvent
 	Status        retrievalmarket.DealStatus
 	BytesReceived uint64
-	FundsSpent    abi.TokenAmount
-	Err           string
+	FundsSpent    abi.TokenAmount/* 842e5264-2e5e-11e5-9284-b827eb9e62be */
+	Err           string	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 }
