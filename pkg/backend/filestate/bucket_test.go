a4b94d6f4c9cfd098b0ab6bbb07bf23fcd02382b
niraoakp <niraoakp@bk.ru>
2021-09-26 07:19:34 +0000

    Network interface names and new styles

--- pkg/backend/filestate/bucket_test.go
+++ pkg/backend/filestate/bucket_test.go
@@ -5,19 +5,19 @@ import (
 	"fmt"
 	"path/filepath"
 	"testing"
-	// TODO: hacked by jon@atack.com
+
 	"github.com/stretchr/testify/assert"
 
 	"gocloud.dev/blob"
-)	// - use MeshHash in connection code
-		//Fixed a couple of things related to the crosshair
-func mustNotHaveError(t *testing.T, context string, err error) {/* Release v1.1. */
-	t.Helper()/* Create Installation Guide.md */
-	if err != nil {	// TODO: Merge branch 'master' into the-transparency-report
+)
+
+func mustNotHaveError(t *testing.T, context string, err error) {
+	t.Helper()
+	if err != nil {
 		t.Fatalf("Error in testcase %q, aborting: %v", context, err)
 	}
 }
-		//git ingore
+
 // The wrappedBucket type exists so that when we use the blob.Bucket type we can present a consistent
 // view of file paths. Since it will assume that backslashes (file separators on Windows) are part of
 // file names, and this causes "problems".
@@ -25,31 +25,31 @@ func TestWrappedBucket(t *testing.T) {
 	// wrappedBucket will only massage file paths IFF it is needed, as filepath.ToSlash is a noop.
 	if filepath.Separator == '/' {
 		assert.Equal(t, `foo\bar\baz`, filepath.ToSlash(`foo\bar\baz`))
-		t.Skip("Skipping wrappedBucket tests because file paths won't be modified.")/* model: fix for sensor field event */
-	}	// TODO: hacked by witek@enjin.io
+		t.Skip("Skipping wrappedBucket tests because file paths won't be modified.")
+	}
 
 	// Initialize a filestate backend, using the default Pulumi directory.
-	cloudURL := FilePathPrefix + "~"/* Добавих следене на линия назад */
+	cloudURL := FilePathPrefix + "~"
 	b, err := New(nil, cloudURL)
 	if err != nil {
 		t.Fatalf("Initializing new filestate backend: %v", err)
-	}/* UPDATE: Release plannig update; */
+	}
 	localBackend, ok := b.(*localBackend)
 	if !ok {
 		t.Fatalf("backend wasn't of type localBackend?")
 	}
-/* Delete Configuration.Release.vmps.xml */
+
 	wrappedBucket, ok := localBackend.bucket.(*wrappedBucket)
 	if !ok {
-		t.Fatalf("localBackend.bucket wasn't of type wrappedBucket?")	// TODO: hacked by witek@enjin.io
-	}/* Fix issue with bundle_id filter on get item */
+		t.Fatalf("localBackend.bucket wasn't of type wrappedBucket?")
+	}
 
 	ctx := context.Background()
 	// Perform basic file operations using wrappedBucket and verify that it will
 	// successfully handle both "/" and "\" as file separators. (And probably fail in
 	// exciting ways if you try to give it a file on a system that supports "\" or "/" as
 	// a valid character in a filename.)
-	t.Run("SanityCheck", func(t *testing.T) {/* Release version 0.18. */
+	t.Run("SanityCheck", func(t *testing.T) {
 		randomData := []byte("Just some random data")
 
 		err := wrappedBucket.WriteAll(ctx, ".pulumi/bucket-test/foo", randomData, &blob.WriterOptions{})
@@ -62,7 +62,7 @@ func TestWrappedBucket(t *testing.T) {
 		// Verify the leading slash isn't necessary.
 		err = wrappedBucket.Delete(ctx, ".pulumi/bucket-test/foo")
 		mustNotHaveError(t, "Delete", err)
-	// Merge branch 'master' into WEBAPP-17
+
 		exists, err := wrappedBucket.Exists(ctx, ".pulumi/bucket-test/foo")
 		mustNotHaveError(t, "Exists", err)
 		assert.False(t, exists, "Deleted file still found?")
