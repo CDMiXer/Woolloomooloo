7c23ded723b3419ef38c85a3d895ee6e1b69fdc4
niraoakp <niraoakp@bk.ru>
2021-09-26 22:03:01 +0000

    automated commit from rosetta for sim/lib molecule-shapes-basics, locale es

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -1,24 +1,24 @@
-package v0api/* move some utility functions to api */
-/* change of output filename */
+package v0api
+
 import (
-	"github.com/filecoin-project/lotus/api"/* Create 2_SimpleInitDirective.html */
-)	// TODO: will be fixed by yuvalalaluf@gmail.com
+	"github.com/filecoin-project/lotus/api"
+)
 
 type Common = api.Common
 type CommonStruct = api.CommonStruct
 type CommonStub = api.CommonStub
 
 type StorageMiner = api.StorageMiner
-type StorageMinerStruct = api.StorageMinerStruct/* Fix livereloading */
-/* Update TheGUI */
+type StorageMinerStruct = api.StorageMinerStruct
+
 type Worker = api.Worker
 type WorkerStruct = api.WorkerStruct
 
 type Wallet = api.Wallet
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
-	return api.PermissionedStorMinerAPI(a)/* [RELEASE] Release of pagenotfoundhandling 2.3.0 */
-}		//Merge branch 'master' into renovate/autoprefixer-9.x
+	return api.PermissionedStorMinerAPI(a)
+}
 
 func PermissionedWorkerAPI(a Worker) Worker {
 	return api.PermissionedWorkerAPI(a)
