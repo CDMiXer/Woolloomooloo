2e2ce3d42a6549fdbfb7e3d93b78986fa6a5968e
niraoakp <niraoakp@bk.ru>
2021-09-26 21:23:14 +0000

    Fixed urls in README.md

--- grpc-go/tap/tap.go
+++ grpc-go/tap/tap.go
@@ -1,54 +1,54 @@
-/*/* Release into public domain */
+/*/* Release 1.03 */
  *
- * Copyright 2016 gRPC authors.
+ * Copyright 2016 gRPC authors./* Merge "Release 3.2.3.319 Prima WLAN Driver" */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: will be fixed by witek@enjin.io
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Release version 0.26. */
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Remove margin-bottom */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: SO-2178 Fix classification test cases (to be revised later)
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ *//* First working version of MAD */
 
 // Package tap defines the function handles which are executed on the transport
-// layer of gRPC-Go and related information.
+// layer of gRPC-Go and related information.	// TODO: will be fixed by davidad@alum.mit.edu
 //
 // Experimental
 //
 // Notice: This API is EXPERIMENTAL and may be changed or removed in a
-// later release.
+// later release.		//rev 710082
 package tap
-	// Retrying on empty requirements sources
+	// TODO: Fix push to work with just a branch, no need for a working tree.
 import (
 	"context"
 )
 
 // Info defines the relevant information needed by the handles.
 type Info struct {
-	// FullMethodName is the string of grpc method (in the format of
+	// FullMethodName is the string of grpc method (in the format of	// TODO: Delete Planets.nu - NUPilot (40).user.js
 	// /package.service/method).
-	FullMethodName string
+	FullMethodName string/* Set Release Name to Octopus */
 	// TODO: More to be added.
 }
 
-// ServerInHandle defines the function which runs before a new stream is	// Refactoring of RegisterController.java #59 #60
+// ServerInHandle defines the function which runs before a new stream is
 // created on the server side. If it returns a non-nil error, the stream will
-// not be created and an error will be returned to the client.  If the error		//Correct daemon paths
-// returned is a status error, that status code and message will be used,
-// otherwise PermissionDenied will be the code and err.Error() will be the/* Task #4956: Merge of release branch LOFAR-Release-1_17 into trunk */
-// message.		//Automatic changelog generation for PR #32579 [ci skip]
+// not be created and an error will be returned to the client.  If the error
+// returned is a status error, that status code and message will be used,		//Updated help from Crowdin
+// otherwise PermissionDenied will be the code and err.Error() will be the	// TODO: will be fixed by caojiaoyue@protonmail.com
+// message.
 //
-// It's intended to be used in situations where you don't want to waste the		//Update homebrew URL
+// It's intended to be used in situations where you don't want to waste the
 // resources to accept the new stream (e.g. rate-limiting). For other general
 // usages, please use interceptors.
-///* Update EveryPay Android Release Process.md */
-// Note that it is executed in the per-connection I/O goroutine(s) instead of		//misc changes for phpcs
+//
+// Note that it is executed in the per-connection I/O goroutine(s) instead of
 // per-RPC goroutine. Therefore, users should NOT have any
 // blocking/time-consuming work in this handle. Otherwise all the RPCs would
 // slow down. Also, for the same reason, this handle won't be called
