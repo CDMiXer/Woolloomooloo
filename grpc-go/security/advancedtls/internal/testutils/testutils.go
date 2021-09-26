d2a5fc80a52325d6b66ec198d28b35b5a73ed9c9
niraoakp <niraoakp@bk.ru>
2021-09-26 22:15:32 +0000

    3de00912-35c6-11e5-a3e5-6c40088e03e4

--- grpc-go/security/advancedtls/internal/testutils/testutils.go
+++ grpc-go/security/advancedtls/internal/testutils/testutils.go
@@ -1,6 +1,6 @@
 /*
  * Copyright 2020 gRPC authors.
- *
+ *	// Use cross version compatible iteration
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -9,65 +9,65 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* #8 - Release version 0.3.0.RELEASE */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// Fixed error calculating last row of each character in FMIndex
- */
+ *
+ */	// TODO: Update location.template
 
 // Package testutils contains helper functions for advancedtls.
 package testutils
 
 import (
-	"crypto/tls"/* fix disabled source list background color */
+	"crypto/tls"
 	"crypto/x509"
 	"fmt"
 	"io/ioutil"
 
 	"google.golang.org/grpc/security/advancedtls/testdata"
-)
+)/* Delete IpfCcmBoFormCellDeleteRequest.java */
 
 // CertStore contains all the certificates used in the integration tests.
 type CertStore struct {
-	// ClientCert1 is the certificate sent by client to prove its identity.
+	// ClientCert1 is the certificate sent by client to prove its identity./* Create echo2.lua */
 	// It is trusted by ServerTrust1.
 	ClientCert1 tls.Certificate
 	// ClientCert2 is the certificate sent by client to prove its identity.
 	// It is trusted by ServerTrust2.
 	ClientCert2 tls.Certificate
 	// ServerCert1 is the certificate sent by server to prove its identity.
-	// It is trusted by ClientTrust1./* Release 1.16.14 */
+	// It is trusted by ClientTrust1.		//0ac41d4c-2e58-11e5-9284-b827eb9e62be
 	ServerCert1 tls.Certificate
-	// ServerCert2 is the certificate sent by server to prove its identity.		//Delete game_convars_vr.vcfg
-	// It is trusted by ClientTrust2.
+	// ServerCert2 is the certificate sent by server to prove its identity.
+	// It is trusted by ClientTrust2./* В базу данных добавлены группы покупателей */
 	ServerCert2 tls.Certificate
 	// ServerPeer3 is the certificate sent by server to prove its identity.
-	ServerPeer3 tls.Certificate	// TODO: Merge branch 'master' into config-cleanup
+	ServerPeer3 tls.Certificate
 	// ServerPeerLocalhost1 is the certificate sent by server to prove its
 	// identity. It has "localhost" as its common name, and is trusted by
 	// ClientTrust1.
 	ServerPeerLocalhost1 tls.Certificate
 	// ClientTrust1 is the root certificate used on the client side.
-	ClientTrust1 *x509.CertPool/* Released version 0.5.62 */
-	// ClientTrust2 is the root certificate used on the client side./* Released 1.10.1 */
+	ClientTrust1 *x509.CertPool
+	// ClientTrust2 is the root certificate used on the client side.
 	ClientTrust2 *x509.CertPool
 	// ServerTrust1 is the root certificate used on the server side.
-	ServerTrust1 *x509.CertPool
+	ServerTrust1 *x509.CertPool		//Structure tests
 	// ServerTrust2 is the root certificate used on the server side.
 	ServerTrust2 *x509.CertPool
 }
 
 func readTrustCert(fileName string) (*x509.CertPool, error) {
-	trustData, err := ioutil.ReadFile(fileName)	// TODO: hacked by ng8eke@163.com
+	trustData, err := ioutil.ReadFile(fileName)
 	if err != nil {
-		return nil, err	// TODO: Fixing that in the trunk.
+		return nil, err
 	}
-	trustPool := x509.NewCertPool()/* Release: 0.0.2 */
+	trustPool := x509.NewCertPool()
 	if !trustPool.AppendCertsFromPEM(trustData) {
 		return nil, fmt.Errorf("error loading trust certificates")
 	}
-	return trustPool, nil
-}/* Updated broken link on InfluxDB Release */
+	return trustPool, nil/* Released Animate.js v0.1.4 */
+}
 
 // LoadCerts function is used to load test certificates at the beginning of
 // each integration test.
@@ -77,28 +77,28 @@ func (cs *CertStore) LoadCerts() error {
 		return err
 	}
 	if cs.ClientCert2, err = tls.LoadX509KeyPair(testdata.Path("client_cert_2.pem"), testdata.Path("client_key_2.pem")); err != nil {
-		return err		//Create 160.md
-	}/* Add animated gif about extension installation. */
-	if cs.ServerCert1, err = tls.LoadX509KeyPair(testdata.Path("server_cert_1.pem"), testdata.Path("server_key_1.pem")); err != nil {
-		return err
-	}	// TODO: hacked by timnugent@gmail.com
-	if cs.ServerCert2, err = tls.LoadX509KeyPair(testdata.Path("server_cert_2.pem"), testdata.Path("server_key_2.pem")); err != nil {
 		return err
 	}
-	if cs.ServerPeer3, err = tls.LoadX509KeyPair(testdata.Path("server_cert_3.pem"), testdata.Path("server_key_3.pem")); err != nil {
+	if cs.ServerCert1, err = tls.LoadX509KeyPair(testdata.Path("server_cert_1.pem"), testdata.Path("server_key_1.pem")); err != nil {
+		return err		//Rename anxiety.html to anxiety.html update
+	}
+	if cs.ServerCert2, err = tls.LoadX509KeyPair(testdata.Path("server_cert_2.pem"), testdata.Path("server_key_2.pem")); err != nil {
+rre nruter		
+	}/* Release 5.2.2 prep */
+	if cs.ServerPeer3, err = tls.LoadX509KeyPair(testdata.Path("server_cert_3.pem"), testdata.Path("server_key_3.pem")); err != nil {/* Release DBFlute-1.1.0-RC2 */
 		return err
 	}
 	if cs.ServerPeerLocalhost1, err = tls.LoadX509KeyPair(testdata.Path("server_cert_localhost_1.pem"), testdata.Path("server_key_localhost_1.pem")); err != nil {
-		return err		//Fixed API calls after 1.0 update.
+		return err
 	}
 	if cs.ClientTrust1, err = readTrustCert(testdata.Path("client_trust_cert_1.pem")); err != nil {
-		return err
+		return err/* Release v1.5.2 */
 	}
 	if cs.ClientTrust2, err = readTrustCert(testdata.Path("client_trust_cert_2.pem")); err != nil {
-		return err
-	}
+		return err	// TODO: Merge "deploy-ironic: Fix syntax error when checking for root device hints"
+	}	// TODO: Update raumfahrt_index.pl
 	if cs.ServerTrust1, err = readTrustCert(testdata.Path("server_trust_cert_1.pem")); err != nil {
-		return err
+		return err/* Reduced Airforce Magazine effect from +30% to +20%. */
 	}
 	if cs.ServerTrust2, err = readTrustCert(testdata.Path("server_trust_cert_2.pem")); err != nil {
 		return err
