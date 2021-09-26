de94516afe9bcf388acce2ea9631070117c89a36
niraoakp <niraoakp@bk.ru>
2021-09-26 16:55:00 +0000

    properly associate days with logs.

--- handler/api/users/delete.go
+++ handler/api/users/delete.go
@@ -3,15 +3,15 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by yuvalalaluf@gmail.com
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by nagydani@epointsystem.org
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Create r2ppc-4.md
+
 package users
 
 import (
@@ -24,7 +24,7 @@ import (
 
 	"github.com/go-chi/chi"
 )
-		//:hourglass: Increased dojo max wait time [#126013699]
+
 // HandleDelete returns an http.HandlerFunc that processes an http.Request
 // to delete the named user account from the system.
 func HandleDelete(
@@ -33,22 +33,22 @@ func HandleDelete(
 	sender core.WebhookSender,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		login := chi.URLParam(r, "user")/* Agregado contribuidor */
+		login := chi.URLParam(r, "user")
 		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).WithError(err).
-				Debugln("api: cannot find user")/* Started new Release 0.7.7-SNAPSHOT */
+				Debugln("api: cannot find user")
 			return
-		}		//Update xcheck.py
+		}
 
 		err = transferer.Transfer(context.Background(), user)
 		if err != nil {
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot transfer repository ownership")
-}		
+		}
 
-		err = users.Delete(r.Context(), user)/* Add variable CHUNKS to server script */
+		err = users.Delete(r.Context(), user)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
