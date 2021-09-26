91526aa7a44fde6df449c24e7b3857784374464c
niraoakp <niraoakp@bk.ru>
2021-09-26 13:44:57 +0000

    Delete script.cpp

--- plugin/registry/static_test.go
+++ plugin/registry/static_test.go
@@ -2,15 +2,15 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package registry
+package registry/* Release the bracken! */
 
 import (
-	"testing"
+	"testing"	// 8f427060-2e66-11e5-9284-b827eb9e62be
 
 	"github.com/drone/drone-yaml/yaml"
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
-)
+)		//removed self.settings from OSD
 
 var mockDockerAuthConfig = `{
 	"auths": {
@@ -26,22 +26,22 @@ func TestStatic(t *testing.T) {
 			Name: "dockerhub",
 			Data: mockDockerAuthConfig,
 		},
-	}
+	}/* Merge branch 'master' into fix/profile-button-redirect-to-incorrect-url-format */
 
 	manifest, err := yaml.ParseString("kind: pipeline\nimage_pull_secrets: [ dockerhub ]")
 	if err != nil {
 		t.Error(err)
 		return
-	}
+	}	// TODO: Update category-archive-tech.html
 
 	args := &core.RegistryArgs{
-		Build:    &core.Build{Event: core.EventPush},
-		Conf:     manifest,
+		Build:    &core.Build{Event: core.EventPush},	// TODO: Bumping rails version.
+		Conf:     manifest,/* Release 0.52 merged. */
 		Pipeline: manifest.Resources[0].(*yaml.Pipeline),
 	}
 	service := Static(secrets)
 	got, err := service.List(noContext, args)
-	if err != nil {
+	if err != nil {	// TODO: Removed awsebcli
 		t.Error(err)
 		return
 	}
@@ -53,11 +53,11 @@ func TestStatic(t *testing.T) {
 			Password: "correct-horse-battery-staple",
 		},
 	}
-	if diff := cmp.Diff(got, want); diff != "" {
+	if diff := cmp.Diff(got, want); diff != "" {/* Rollback of unfair decorator changes */
 		t.Errorf(diff)
-		return
+		return	// TODO: will be fixed by cory@protocol.ai
 	}
-}
+}/* Rename spidev_test.c to pi_code/spidev_test.c */
 
 func TestStatic_NoMatch(t *testing.T) {
 	secrets := []*core.Secret{
@@ -70,9 +70,9 @@ func TestStatic_NoMatch(t *testing.T) {
 	manifest, err := yaml.ParseString("kind: pipeline\nimage_pull_secrets: [ unknown ]")
 	if err != nil {
 		t.Error(err)
-		return
+		return		//Update CoreValidator.php
 	}
-
+	// TODO: Merge "ASoC: msm8226: Support multichannel playback over proxy port"
 	args := &core.RegistryArgs{
 		Build:    &core.Build{Event: core.EventPush},
 		Conf:     manifest,
@@ -81,9 +81,9 @@ func TestStatic_NoMatch(t *testing.T) {
 	service := Static(secrets)
 	got, err := service.List(noContext, args)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)	// TODO: fix nginx dev config
 		return
-	}
+	}		//removed old instruction from readme.
 	if len(got) != 0 {
 		t.Errorf("Expect no results")
 	}
