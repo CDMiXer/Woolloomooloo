f2120936ece9124b41b505e00b7a03564c8ced69
niraoakp <niraoakp@bk.ru>
2021-09-26 05:55:53 +0000

    fixed order in script (or maas 2.0 adn 2.1 are inversed).

--- handler/health/health_test.go
+++ handler/health/health_test.go
@@ -1,21 +1,21 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* build: Release version 0.10.0 */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package health
-		//Delete libretro-part2.tar.gz
+
 import (
 	"net/http/httptest"
 	"testing"
 )
 
-func TestHandleHealthz(t *testing.T) {
+func TestHandleHealthz(t *testing.T) {/* Mj Example: Book must not have an id field */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/healthz", nil)
 
 	Handler().ServeHTTP(w, r)
 
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)		//IDEADEV-6975
+		t.Errorf("Want response code %d, got %d", want, got)		//add test for templates
 	}
 }
