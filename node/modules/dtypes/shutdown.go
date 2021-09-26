0276ef0d93809bbce4f35d08fa4e6901bdc57d37
niraoakp <niraoakp@bk.ru>
2021-09-26 17:07:27 +0000

    restructured page

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
-package dtypes
-
-// ShutdownChan is a channel to which you send a value if you intend to shut/* Release version 1.5.0 */
-// down the daemon (or miner), including the node and RPC server.
+package dtypes/* Release 2.4.1 */
+/* Update README: Links and installation info */
+// ShutdownChan is a channel to which you send a value if you intend to shut
+// down the daemon (or miner), including the node and RPC server./* Release 1.3.2.0 */
 type ShutdownChan chan struct{}
