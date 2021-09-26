509b46eb573025bc86bad03caa623732f9afbca0
niraoakp <niraoakp@bk.ru>
2021-09-26 03:02:54 +0000

    c21c8ca6-2e6f-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"/* entity data changes */
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
-		logs, err := s3.NewBucket(ctx, "logs", nil)
+	pulumi.Run(func(ctx *pulumi.Context) error {	// TODO: hacked by cory@protocol.ai
+		logs, err := s3.NewBucket(ctx, "logs", nil)/* añadido nuevo topic a kafka */
 		if err != nil {
 			return err
-		}
+		}		//Add ENV to environmental variable
 		bucket, err := s3.NewBucket(ctx, "bucket", &s3.BucketArgs{
-			Loggings: s3.BucketLoggingArray{
+			Loggings: s3.BucketLoggingArray{	// TODO: Fix broken GitHub URL in package.json.
 				&s3.BucketLoggingArgs{
 					TargetBucket: logs.Bucket,
 				},
-			},
+			},	// TODO: will be fixed by boringland@protonmail.ch
 		})
 		if err != nil {
 			return err
-		}
+		}	// TODO: Stop if no internet connection
 		ctx.Export("targetBucket", bucket.Loggings.ApplyT(func(loggings []s3.BucketLogging) (string, error) {
 			return loggings[0].TargetBucket, nil
 		}).(pulumi.StringOutput))
