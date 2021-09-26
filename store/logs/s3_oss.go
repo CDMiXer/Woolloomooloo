8574f01e229804715271440e7d2ef85669fc42d0
niraoakp <niraoakp@bk.ru>
2021-09-26 19:53:14 +0000

    Ajustando organização de pasta.

--- store/logs/s3_oss.go
+++ store/logs/s3_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: Merge "Handle "N seconds ago" instead of dying"
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//emit coreOpen later
+// You may obtain a copy of the License at	// Add script for Tradewind Rider
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Fix bug where default config values could override ones you set */
 // limitations under the License.
+/* Release new version 2.5.31: various parsing bug fixes (famlam) */
+// +build oss/* DATASOLR-47 - Release version 1.0.0.RC1. */
 
-// +build oss
-
-package logs	// TODO: hacked by joshua@yottadb.com
+package logs
 
 import "github.com/drone/drone/core"
-/* Added notes about dependencies and added them to gemspec */
-// New returns a zero value LogStore.		//Cleanup and test last:
+
+// New returns a zero value LogStore.
 func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {
 	return nil
 }
