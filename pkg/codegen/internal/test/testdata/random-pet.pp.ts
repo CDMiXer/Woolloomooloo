607ca52c47ff00c0cf206d0ee09498e5df084589
niraoakp <niraoakp@bk.ru>
2021-09-26 11:49:20 +0000

    kaigounoyoteiwo

--- pkg/codegen/internal/test/testdata/random-pet.pp.ts
+++ pkg/codegen/internal/test/testdata/random-pet.pp.ts
@@ -1,4 +1,4 @@
-import * as pulumi from "@pulumi/pulumi";/* All indentations are fixed */
+import * as pulumi from "@pulumi/pulumi";
 import * as random from "@pulumi/random";
 
-const random_pet = new random.RandomPet("random_pet", {prefix: "doggo"});	// e0f82c28-2e4e-11e5-80ba-28cfe91dbc4b
+const random_pet = new random.RandomPet("random_pet", {prefix: "doggo"});
