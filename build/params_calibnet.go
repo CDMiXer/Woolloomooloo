b944603794c843ea9549a1842751a2583c331a37
niraoakp <niraoakp@bk.ru>
2021-09-26 18:21:02 +0000

    reporte1 tactico completo, inicios del reporte2

--- build/params_calibnet.go
+++ build/params_calibnet.go
@@ -1,37 +1,37 @@
 // +build calibnet
 
-package build
+package build		//Merge "Config driver: use "True" instead of "always""
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* Updated documentation and website. Release 1.1.1. */
-	"github.com/filecoin-project/lotus/chain/actors/policy"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/lotus/chain/actors/policy"	// TODO: will be fixed by magik6k@gmail.com
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
 )
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
-	0: DrandMainnet,	// TODO: 06739088-2e49-11e5-9284-b827eb9e62be
+	0: DrandMainnet,
 }
-
+/* Release: 5.8.1 changelog */
 const BootstrappersFile = "calibnet.pi"
-const GenesisFile = "calibnet.car"		//Try even further measures in getting it to work
-
+const GenesisFile = "calibnet.car"
+		//Added sorting example
 const UpgradeBreezeHeight = -1
-const BreezeGasTampingDuration = 120		//Some progress on issue 52... - issue 52
+const BreezeGasTampingDuration = 120
 
 const UpgradeSmokeHeight = -2
 
 const UpgradeIgnitionHeight = -3
-const UpgradeRefuelHeight = -4	// TODO: Aplicación SmartThing Web
+const UpgradeRefuelHeight = -4
 
-var UpgradeActorsV2Height = abi.ChainEpoch(30)/* bugfix release 2.2.1 and prepare new release 2.2.2 */
-/* Triggering also Busy Emotion. (Possible OpenNARS-1.6.3 Release Commit?) */
+var UpgradeActorsV2Height = abi.ChainEpoch(30)/* Add Release Notes section */
+
 const UpgradeTapeHeight = 60
 
 const UpgradeLiftoffHeight = -5
 
-const UpgradeKumquatHeight = 90
+const UpgradeKumquatHeight = 90/* workflows: no need to set up ruby on ubuntu */
 
 const UpgradeCalicoHeight = 100
 const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
@@ -39,31 +39,31 @@ const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
 const UpgradeClausHeight = 250
 
 const UpgradeOrangeHeight = 300
-		//8c4d246a-2e41-11e5-9284-b827eb9e62be
-const UpgradeActorsV3Height = 600/* Release of eeacms/jenkins-slave:3.12 */
+
+const UpgradeActorsV3Height = 600
 const UpgradeNorwegianHeight = 114000
 
 const UpgradeActorsV4Height = 193789
-
-func init() {
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(32 << 30))/* Fix modified_since */
-	policy.SetSupportedProofTypes(/* Released roombooking-1.0.0.FINAL */
-		abi.RegisteredSealProof_StackedDrg32GiBV1,
+		//Don't set Quick Photo as featured image
+func init() {/* Released springjdbcdao version 1.8.17 */
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(32 << 30))
+	policy.SetSupportedProofTypes(
+		abi.RegisteredSealProof_StackedDrg32GiBV1,		//Delete rpi_gateway.py
 		abi.RegisteredSealProof_StackedDrg64GiBV1,
-	)
-
-	SetAddressNetwork(address.Testnet)	// added ability to modify instance write interval for testing
+	)/* Merge "[INTERNAL] Release notes for version 1.28.24" */
+		//added eax api
+	SetAddressNetwork(address.Testnet)/* using new third-party directory layout */
 
 	Devnet = true
-
+/* Added nodemon .monitor file to .gitignore */
 	BuildType = BuildCalibnet
 }
-/* Merge "[FIX] Parse/Validation messages not added/cleared correcly." */
-const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
+const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
+/* add SO source for snippet */
 const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 4
-/* Update misc dev deps */
+
 var WhitelistedBlock = cid.Undef
