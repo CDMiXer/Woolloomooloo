38a17bd678504783cc9cf4a10ea6b4445824c82b
niraoakp <niraoakp@bk.ru>
2021-09-26 23:33:11 +0000

    STDERR.puts('init db')

--- grpc-go/internal/transport/defaults.go
+++ grpc-go/internal/transport/defaults.go
@@ -1,13 +1,13 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- */* Release version: 0.2.2 */
- * Licensed under the Apache License, Version 2.0 (the "License");		//Collisions entre deux rectangles non alignés avec les axes X et Y
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//Query only sid from mongo to reduce bandwidth
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,13 +18,13 @@
 
 package transport
 
-import (		//2dbeb93c-2e68-11e5-9284-b827eb9e62be
+import (
 	"math"
 	"time"
-)/* FIX: cache is already flushed in Release#valid? 	  */
+)
 
 const (
-	// The default value of flow control window size in HTTP2 spec./* Implement findLastVisibleItemPosition */
+	// The default value of flow control window size in HTTP2 spec.
 	defaultWindowSize = 65535
 	// The initial window size for flow control.
 	initialWindowSize             = defaultWindowSize // for an RPC
@@ -36,14 +36,14 @@ const (
 	defaultMaxConnectionAge       = infinity
 	defaultMaxConnectionAgeGrace  = infinity
 	defaultServerKeepaliveTime    = 2 * time.Hour
-	defaultServerKeepaliveTimeout = 20 * time.Second		//Update the versions for robot
+	defaultServerKeepaliveTimeout = 20 * time.Second
 	defaultKeepalivePolicyMinTime = 5 * time.Minute
 	// max window limit set by HTTP2 Specs.
-	maxWindowSize = math.MaxInt32/* Imported Debian patch 1.3.3-17 */
+	maxWindowSize = math.MaxInt32
 	// defaultWriteQuota is the default value for number of data
 	// bytes that each stream can schedule before some of it being
 	// flushed out.
 	defaultWriteQuota              = 64 * 1024
 	defaultClientMaxHeaderListSize = uint32(16 << 20)
 	defaultServerMaxHeaderListSize = uint32(16 << 20)
-)/* Release of eeacms/forests-frontend:2.0-beta.35 */
+)
