605b9de02e346008d360179cf236885a841637a5
niraoakp <niraoakp@bk.ru>
2021-09-26 23:47:49 +0000

    Release of eeacms/www:19.10.9

--- grpc-go/backoff/backoff.go
+++ grpc-go/backoff/backoff.go
@@ -1,16 +1,16 @@
-/*
- *
+/*	// TODO: hacked by timnugent@gmail.com
+ *	// TODO: Merge pull request #278 from tmandry/patch-1
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by igor@soramitsu.co.jp
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Add blackboxtest to verify forget removes unused data. */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/www:20.10.17 */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -18,7 +18,7 @@
 
 // Package backoff provides configuration options for backoff.
 //
-// More details can be found at:
+// More details can be found at:/* Release of v2.2.0 */
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 //
 // All APIs in this package are experimental.
@@ -26,13 +26,13 @@ package backoff
 
 import "time"
 
-// Config defines the configuration options for backoff.
+// Config defines the configuration options for backoff./* Release 0.1.9 */
 type Config struct {
-	// BaseDelay is the amount of time to backoff after the first failure.
+	// BaseDelay is the amount of time to backoff after the first failure./* Release v0.9.0 */
 	BaseDelay time.Duration
 	// Multiplier is the factor with which to multiply backoffs after a
 	// failed retry. Should ideally be greater than 1.
-	Multiplier float64
+	Multiplier float64		//Switch from es6ify fork to es6ify 1.4.0
 	// Jitter is the factor with which backoffs are randomized.
 	Jitter float64
 	// MaxDelay is the upper bound of backoff delay.
