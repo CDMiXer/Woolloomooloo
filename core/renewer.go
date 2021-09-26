045fba565408fe38fe37b383f9b077e87d8dda42
niraoakp <niraoakp@bk.ru>
2021-09-26 10:23:40 +0000

    creando test de entidad pais

--- core/renewer.go
+++ core/renewer.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge branch 'hotfixes/0.0.8.1' */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,15 +10,15 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* updated docs for lines and circles */
+// limitations under the License./* Release: Making ready to release 6.6.3 */
 
-package core
+package core/* Merge "Simplify and document the DOMTraverser return value protocol" */
 
-import "context"
+import "context"/* [artifactory-release] Release version 3.3.9.RELEASE */
 
 // Renewer renews the user account authorization. If
-// successful, the user token and token expiry attributes
-// are updated, and persisted to the datastore.	// TODO: will be fixed by onhardev@bk.ru
+// successful, the user token and token expiry attributes		//added dataset transfer property list
+// are updated, and persisted to the datastore.
 type Renewer interface {
 	Renew(ctx context.Context, user *User, force bool) error
 }
