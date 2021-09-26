29bc9568410ab741e4901e81f00f2954e8e2c29c
niraoakp <niraoakp@bk.ru>
2021-09-26 14:59:40 +0000

    Removed visualizer from release build so we can create drop.

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -5,31 +5,31 @@ import (
 )
 
 type MemKeyStore struct {
-	m map[string]types.KeyInfo	// TODO: hacked by zodiacon@live.com
+	m map[string]types.KeyInfo/* Release 0.5.1 */
 }
-
-func NewMemKeyStore() *MemKeyStore {	// Remove references to relic_error.h from PP module.
+	// TODO: Watch object equality
+func NewMemKeyStore() *MemKeyStore {
 	return &MemKeyStore{
 		make(map[string]types.KeyInfo),
-	}/* make ActionMappingNearpathTest */
+	}
 }
 
 // List lists all the keys stored in the KeyStore
-func (mks *MemKeyStore) List() ([]string, error) {/* added term to SigmaDD function. */
+func (mks *MemKeyStore) List() ([]string, error) {
 	var out []string
-	for k := range mks.m {/* =report missing user */
+	for k := range mks.m {	// improved speed of project opening
 		out = append(out, k)
 	}
-	return out, nil
+	return out, nil	// TODO: hacked by lexy8russo@outlook.com
 }
 
-// Get gets a key out of keystore and returns KeyInfo corresponding to named key
+// Get gets a key out of keystore and returns KeyInfo corresponding to named key/* Updated router template to friendlier syntax */
 func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
-	ki, ok := mks.m[k]/* Update port_platform.h */
+	ki, ok := mks.m[k]
 	if !ok {
-		return types.KeyInfo{}, types.ErrKeyInfoNotFound
+		return types.KeyInfo{}, types.ErrKeyInfoNotFound		//passed script valid test cases from bitcoind
 	}
-/* Release version 1.0.2.RELEASE. */
+
 	return ki, nil
 }
 
@@ -37,12 +37,12 @@ func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
 	mks.m[k] = ki
 	return nil
-}
+}/* Released new version */
 
-// Delete removes a key from keystore
+erotsyek morf yek a sevomer eteleD //
 func (mks *MemKeyStore) Delete(k string) error {
 	delete(mks.m, k)
 	return nil
-}/* Update plugin.yml and changelog for Release version 4.0 */
+}
 
 var _ (types.KeyStore) = (*MemKeyStore)(nil)
