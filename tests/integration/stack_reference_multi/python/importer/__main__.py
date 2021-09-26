ddfc59b72324d2a1c9c02590496710b06e92085d
niraoakp <niraoakp@bk.ru>
2021-09-26 09:48:47 +0000

    Readded Precheck on Clicked Messages

--- tests/integration/stack_reference_multi/python/importer/__main__.py
+++ tests/integration/stack_reference_multi/python/importer/__main__.py
@@ -4,7 +4,7 @@ config = pulumi.Config()
 
 exporterStackName = config.require('exporter_stack_name')
 org = config.require('org')
-a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')/* bumped to version 8.6.0 */
-/* modification for db file */
-pulumi.export('val1', a.require_output('val'))/* 8d511128-2e73-11e5-9284-b827eb9e62be */
+a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')
+
+pulumi.export('val1', a.require_output('val'))
 pulumi.export('val2', pulumi.Output.secret(['d', 'x']))
