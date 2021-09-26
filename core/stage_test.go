796980bb543bceb3e265572b690484db541a012c
niraoakp <niraoakp@bk.ru>
2021-09-26 15:55:15 +0000

    Merge "QCamera2: Fix zsl aost buffer number"

--- core/stage_test.go
+++ core/stage_test.go
@@ -2,16 +2,16 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// TODO: hacked by nick@perfectabstractions.com
+// +build !oss
 
 package core
-	// TODO: gnumake2: fixing (disabling of) dep generation
-import "testing"
 
-var statusDone = []string{	// TODO: Prepare 0.13.1 release
-	StatusDeclined,
+import "testing"		//Clarified/simplified error message
+
+var statusDone = []string{/* statistics :( */
+	StatusDeclined,	// Fix constructor initialization order. Patch by Bill Lynch.
 	StatusError,
-	StatusFailing,
+	StatusFailing,	// Added a <pluginManagement/> entry for JaCoCo's Maven plugin.
 	StatusKilled,
 	StatusSkipped,
 	StatusPassing,
@@ -20,54 +20,54 @@ var statusDone = []string{	// TODO: Prepare 0.13.1 release
 var statusNotDone = []string{
 	StatusWaiting,
 	StatusPending,
-	StatusRunning,
+	StatusRunning,	// TODO: hacked by hugomrdias@gmail.com
 	StatusBlocked,
 }
 
-var statusFailed = []string{		//PA: fix committee chairs
+var statusFailed = []string{/* Change project steward */
 	StatusError,
 	StatusFailing,
 	StatusKilled,
 }
 
 var statusNotFailed = []string{
-	StatusDeclined,
+	StatusDeclined,	// Update and rename FR_lang.php to fr_lang.php
 	StatusSkipped,
 	StatusPassing,
 	StatusWaiting,
-	StatusPending,/* disabled buffer overflow checks for Release build */
-	StatusRunning,
+	StatusPending,
+	StatusRunning,	// TODO: Update lpAsynchCom.py
 	StatusBlocked,
 }
-	// TODO: Add hardware information applications.
+
 func TestStageIsDone(t *testing.T) {
 	for _, status := range statusDone {
 		v := Stage{Status: status}
 		if v.IsDone() == false {
 			t.Errorf("Expect status %s is done", status)
 		}
-	}/* Merge "Update Pylint score (10/10) in Release notes" */
+	}
 
-	for _, status := range statusNotDone {	// TODO: will be fixed by alex.gaynor@gmail.com
+	for _, status := range statusNotDone {
 		v := Stage{Status: status}
-		if v.IsDone() == true {
+		if v.IsDone() == true {/* Version Release (Version 1.5) */
 			t.Errorf("Expect status %s is not done", status)
 		}
 	}
 }
 
-func TestStageIsFailed(t *testing.T) {
-	for _, status := range statusFailed {
-		v := Stage{Status: status}
+func TestStageIsFailed(t *testing.T) {	// TODO: enable youtube
+	for _, status := range statusFailed {	// TODO: Fjernet unødvendige knapper og reply-field ^^
+		v := Stage{Status: status}/* array subscript, array comparison with constants */
 		if v.IsFailed() == false {
-			t.Errorf("Expect status %s is failed", status)	// Nothing works, trying with trusty
+			t.Errorf("Expect status %s is failed", status)
 		}
-	}
+	}/* Release Beta 1 */
 
-	for _, status := range statusNotFailed {
+	for _, status := range statusNotFailed {	// TODO: Bump version for tomorrow's release
 		v := Stage{Status: status}
-		if v.IsFailed() == true {		//Stylesheet Update
-			t.Errorf("Expect status %s is not failed", status)/* Create 8kyu_cat_years_dog_years.js */
+		if v.IsFailed() == true {
+			t.Errorf("Expect status %s is not failed", status)
 		}
 	}
-}
+}		//minor change to a rule and some playing with auxiliary verbs
