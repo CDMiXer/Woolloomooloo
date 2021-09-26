cbcf85256495efed5d60e25c8ded8b2a9e27e311
niraoakp <niraoakp@bk.ru>
2021-09-26 13:04:02 +0000

    Fix for Bug#78706 (21947042), fixed StringRegressionTest.testBug11629()

--- handler/api/repos/crons/list_test.go
+++ handler/api/repos/crons/list_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Final fix for bug#36773: Moved patch in 5.1 from rpl to bug team tree. */
 
 // +build !oss
 
@@ -8,44 +8,44 @@ package crons
 
 import (
 	"context"
-	"encoding/json"/* Fix SDCCy.h include that confused MSVC */
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"/* Factored out LDL_C from CDC_LDL_C and CMC HEDIS rules. */
-/* ReleasePlugin.checkSnapshotDependencies - finding all snapshot dependencies */
-	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"		//plot.isoreg() fix based on one from Jari Oksanen
+	"github.com/drone/drone/mock"
+
+	"github.com/go-chi/chi"/* Release: 0.95.006 */
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
-var (	// TODO: Update hashtag
-	dummyCronRepo = &core.Repository{
-		ID:        1,	// TODO: cambio en el readme
+var (
+	dummyCronRepo = &core.Repository{	// TODO: teste bianka
+		ID:        1,
 		Namespace: "octocat",
-		Name:      "hello-world",
+		Name:      "hello-world",/* Update ReleaseNotes-6.1.18 */
 	}
 
-	dummyCron = &core.Cron{	// TODO: will be fixed by julia@jvns.ca
+	dummyCron = &core.Cron{
 		RepoID: 1,
-		Event:  core.EventPush,
+		Event:  core.EventPush,/* Delete SVBRelease.zip */
 		Name:   "nightly",
 		Expr:   "* * * * * *",
-		Next:   0,
-		Branch: "master",
+		Next:   0,/* New tutorial for argonaut-codecs */
+		Branch: "master",		//make compatible with composer >=2.0.5
 	}
 
 	dummyCronList = []*core.Cron{
-		dummyCron,
-	}		//travis: remove smart chars
-)/* Updated Release URL */
+		dummyCron,/* Merge branch 'develop' into devop/update-greenkeeper */
+	}
+)
 
-func TestHandleList(t *testing.T) {/* Release candidate 7 */
+func TestHandleList(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()/* ipf: Fix #1360 [O. Galibert] */
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
@@ -58,38 +58,38 @@ func TestHandleList(t *testing.T) {/* Release candidate 7 */
 	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)/* Update/Create QbQIVV59Uu3mRPlhCsw_img_5.jpg */
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-/* JETTY-1211 debug */
-	HandleList(repos, crons).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
-	}		//d25362de-2e6f-11e5-9284-b827eb9e62be
 
-	got, want := []*core.Cron{}, dummyCronList		//Simplify validity assignement
-	json.NewDecoder(w.Body).Decode(&got)/* (jam) Release 2.1.0rc2 */
-	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Release to public domain */
+	HandleList(repos, crons).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusOK; want != got {		//Fixed reference to getter function
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := []*core.Cron{}, dummyCronList
+	json.NewDecoder(w.Body).Decode(&got)
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}		//Delete DeploymentReport.txt
+	}
 }
 
 func TestHandleList_RepoNotFound(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)		//Le vaisseau mére il prend cher
 	defer controller.Finish()
-
-	repos := mock.NewMockRepositoryStore(controller)
+/* fix typo: regular -ire verbs conjugation for 3rd person plural */
+	repos := mock.NewMockRepositoryStore(controller)/* Move ReleaseChecklist into the developer guide */
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")	// Make Binary the parent of ObjectFile and update children to new interface.
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),		//fix missing removal of dynamic toolbar
 	)
 
 	HandleList(repos, nil).ServeHTTP(w, r)
