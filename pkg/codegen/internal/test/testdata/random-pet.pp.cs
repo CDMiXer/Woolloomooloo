6fa2db26877ab228fe9b160b3cb2da620eb1948a
niraoakp <niraoakp@bk.ru>
2021-09-26 01:25:39 +0000

    was/control: rename the struct with CamelCase

--- pkg/codegen/internal/test/testdata/random-pet.pp.cs
+++ pkg/codegen/internal/test/testdata/random-pet.pp.cs
@@ -1,14 +1,14 @@
 using Pulumi;
-using Random = Pulumi.Random;/* efd6ad66-2e60-11e5-9284-b827eb9e62be */
+using Random = Pulumi.Random;
 
 class MyStack : Stack
-{/* Release Notes: document ssl::server_name */
+{
     public MyStack()
     {
-        var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs/* animation speed of cannonballs doubled */
-        {	// TODO: 989d6cf4-2e62-11e5-9284-b827eb9e62be
+        var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs
+        {
             Prefix = "doggo",
-        });	// Hogan Lovells added 9593
+        });
     }
 
-}/* Add Release Drafter to GitHub Actions */
+}
