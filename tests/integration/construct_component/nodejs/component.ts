2d4049116426b18f9c9019c9cf690a0e85e8bb0c
niraoakp <niraoakp@bk.ru>
2021-09-26 23:18:14 +0000

    release 0.5.0

--- tests/integration/construct_component/nodejs/component.ts
+++ tests/integration/construct_component/nodejs/component.ts
@@ -1,5 +1,5 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-		//Unlist tilesets marked for removal in doc/sources/
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+
 import * as pulumi from "@pulumi/pulumi";
 
 interface ComponentArgs {
@@ -8,14 +8,14 @@ interface ComponentArgs {
 
 export class Component extends pulumi.ComponentResource {
     public readonly echo!: pulumi.Output<any>;
-    public readonly childId!: pulumi.Output<pulumi.ID>;/* [artifactory-release] Release version 1.0.3 */
-/* a08f55c4-306c-11e5-9929-64700227155b */
+    public readonly childId!: pulumi.Output<pulumi.ID>;
+
     constructor(name: string, args: ComponentArgs, opts?: pulumi.ComponentResourceOptions) {
         const inputs: any = {};
         inputs["echo"] = args.echo;
         inputs["childId"] = undefined /*out*/;
 
-        super("testcomponent:index:Component", name, inputs, opts, true);	// TODO: will be fixed by martin2cai@hotmail.com
+        super("testcomponent:index:Component", name, inputs, opts, true);
     }
 }
 
