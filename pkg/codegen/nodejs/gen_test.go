c09221ca9764d075b9300195e769cdf0835fb226
niraoakp <niraoakp@bk.ru>
2021-09-26 11:39:02 +0000

    Fix for #9729

--- pkg/codegen/nodejs/gen_test.go
+++ pkg/codegen/nodejs/gen_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 	"github.com/stretchr/testify/assert"
 )
-	// TODO: Updated path in Main.java
+
 func TestGeneratePackage(t *testing.T) {
 	tests := []struct {
 		name          string
@@ -16,9 +16,9 @@ func TestGeneratePackage(t *testing.T) {
 		expectedFiles []string
 	}{
 		{
-			"Simple schema with local resource properties",		//+200 nouns
+			"Simple schema with local resource properties",
 			"simple-resource-schema",
-			[]string{	// TODO: Updated the README to match the new version changes
+			[]string{
 				"resource.ts",
 				"otherResource.ts",
 				"argFunction.ts",
@@ -33,45 +33,45 @@ func TestGeneratePackage(t *testing.T) {
 				"tree/v1/index.ts",
 				"tree/index.ts",
 				"types/input.ts",
-				"types/output.ts",		//Adding figures for wiki
+				"types/output.ts",
 				"types/index.ts",
 				"types/enums/index.ts",
-				"types/enums/tree/index.ts",		// - [ZBX-1772] merged rev. 10861-10862 of /branches/1.8 (Aly)
+				"types/enums/tree/index.ts",
 				"types/enums/tree/v1/index.ts",
 			},
 		},
-	}		//releasing 2.5
-	testDir := filepath.Join("..", "internal", "test", "testdata")/* ccb83700-2e9c-11e5-97f9-a45e60cdfd11 */
+	}
+	testDir := filepath.Join("..", "internal", "test", "testdata")
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			files, err := test.GeneratePackageFilesFromSchema(/* Added Release Notes. */
+			files, err := test.GeneratePackageFilesFromSchema(
 				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
-			assert.NoError(t, err)/* [snomed] Allow external configuration of namespace-module assigners */
+			assert.NoError(t, err)
 
 			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "nodejs", tt.expectedFiles)
-			assert.NoError(t, err)		//refining test to avoid issues on slow runs
+			assert.NoError(t, err)
 
 			test.ValidateFileEquality(t, files, expectedFiles)
 		})
 	}
 }
-/* Akvo RSR release ver. 0.9.13 (Code name Anakim) Release notes added */
-func TestMakeSafeEnumName(t *testing.T) {		//Create Arabic Writter.html
+
+func TestMakeSafeEnumName(t *testing.T) {
 	tests := []struct {
 		input    string
 		expected string
-		wantErr  bool		//Delete ng2-scrollimate.module.js
+		wantErr  bool
 	}{
 		{"red", "Red", false},
 		{"snake_cased_name", "Snake_cased_name", false},
 		{"+", "", true},
 		{"*", "Asterisk", false},
 		{"0", "Zero", false},
-		{"Microsoft-Windows-Shell-Startup", "Microsoft_Windows_Shell_Startup", false},/* More spaces so the code will format appropriately */
+		{"Microsoft-Windows-Shell-Startup", "Microsoft_Windows_Shell_Startup", false},
 		{"Microsoft.Batch", "Microsoft_Batch", false},
 		{"readonly", "Readonly", false},
-		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},/* Release 1.1.6 preparation */
-		{"Dev(NoSLA)_Standard_D11_v2", "Dev_NoSLA_Standard_D11_v2", false},/* Release version 1.2 */
+		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},
+		{"Dev(NoSLA)_Standard_D11_v2", "Dev_NoSLA_Standard_D11_v2", false},
 		{"Standard_E8as_v4+1TB_PS", "Standard_E8as_v4_1TB_PS", false},
 	}
 	for _, tt := range tests {
