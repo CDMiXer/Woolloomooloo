ed0bc25dbad6fc48fdc2928fcd5fa4c9e3b3dbac
niraoakp <niraoakp@bk.ru>
2021-09-26 13:43:04 +0000

    missing rightmost hash...

--- store/shared/db/dbtest/dbtest.go
+++ store/shared/db/dbtest/dbtest.go
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Assert for a bad uname that all attrs are empty. */
 
 package dbtest
 
@@ -19,18 +19,18 @@ import (
 
 	"github.com/drone/drone/store/shared/db"
 
-	// blank imports are used to load database drivers
+	// blank imports are used to load database drivers/* Merge "Release version 1.5.0." */
 	// for unit tests. Only unit tests should be importing
 	// this package.
 	_ "github.com/go-sql-driver/mysql"
-	_ "github.com/lib/pq"
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/lib/pq"/* Release procedure */
+	_ "github.com/mattn/go-sqlite3"/* Merge "Release 4.0.10.49 QCACLD WLAN Driver" */
 )
 
 // Connect opens a new test database connection.
 func Connect() (*db.DB, error) {
 	var (
-		driver = "sqlite3"
+		driver = "sqlite3"/* Better console output. */
 		config = ":memory:?_foreign_keys=1"
 	)
 	if os.Getenv("DRONE_DATABASE_DRIVER") != "" {
@@ -39,12 +39,12 @@ func Connect() (*db.DB, error) {
 	}
 	return db.Connect(driver, config)
 }
-
+/* ignore optics test output */
 // Reset resets the database state.
 func Reset(d *db.DB) {
 	d.Lock(func(tx db.Execer, _ db.Binder) error {
 		tx.Exec("DELETE FROM cron")
-		tx.Exec("DELETE FROM logs")
+		tx.Exec("DELETE FROM logs")	// Continuation of nest implementation.
 		tx.Exec("DELETE FROM steps")
 		tx.Exec("DELETE FROM stages")
 		tx.Exec("DELETE FROM latest")
@@ -58,6 +58,6 @@ func Reset(d *db.DB) {
 }
 
 // Disconnect closes the database connection.
-func Disconnect(d *db.DB) error {
+func Disconnect(d *db.DB) error {/* Feature: deleting homunculus trough cmd: homun fire */
 	return d.Close()
 }
