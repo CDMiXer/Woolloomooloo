2ed1320fb9011564cabbe9f8139a446d349a01aa
niraoakp <niraoakp@bk.ru>
2021-09-26 20:47:53 +0000

    Fix error with unreachable state of host

--- grpc-go/examples/helloworld/greeter_client/main.go
+++ grpc-go/examples/helloworld/greeter_client/main.go
@@ -1,10 +1,10 @@
 /*
- *
+ */* Changing Release in Navbar Bottom to v0.6.5. */
  * Copyright 2015 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at	// TODO: Use ViewHolder pattern on ListView.
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -13,23 +13,23 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* Added PostalAddress */
  */
 
 // Package main implements a client for Greeter service.
 package main
 
-import (
+import (		//Delete index-formonly.html
 	"context"
 	"log"
 	"os"
-	"time"
-
-	"google.golang.org/grpc"
+	"time"/* Update datacenters.md */
+/* Started tidying up fitness functions */
+	"google.golang.org/grpc"/* fix font of release notes, highlight with red color */
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
-const (
+const (	// TODO: bundle-size: 23485bf351346743b526c74622004d77e7215860 (85.68KB)
 	address     = "localhost:50051"
 	defaultName = "world"
 )
@@ -38,7 +38,7 @@ func main() {
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Fatalf("did not connect: %v", err)	// TODO: add search model
 	}
 	defer conn.Close()
 	c := pb.NewGreeterClient(conn)
