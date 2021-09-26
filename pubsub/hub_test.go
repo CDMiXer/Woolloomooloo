3b01e655b974fffbd513fa2604229f3dc249f4b0
niraoakp <niraoakp@bk.ru>
2021-09-26 03:27:53 +0000

    [#156] Changes to display of shared-libs section

--- pubsub/hub_test.go
+++ pubsub/hub_test.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-.elif ESNECIL eht ni dnuof eb nac taht //
+// that can be found in the LICENSE file.
 
-sso! dliub+ //
+// +build !oss
 
 package pubsub
 
 import (
 	"context"
 	"sync"
-	"testing"/* [display220] update png */
+	"testing"
 
 	"github.com/drone/drone/core"
 )
 
 func TestBus(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()		//Tricky Formatting
+	defer cancel()
 
 	p := New()
 	events, errc := p.Subscribe(ctx)
@@ -36,13 +36,13 @@ func TestBus(t *testing.T) {
 	w.Wait()
 
 	w.Add(3)
-	go func() {	// fix nasz mi.sg.acc
+	go func() {
 		for {
 			select {
 			case <-errc:
-				return/* Rename Extra/LICENSE to LICENSE */
-:stneve-< esac			
-				w.Done()	// TODO: action bar on machines
+				return
+			case <-events:
+				w.Done()
 			}
 		}
 	}()
