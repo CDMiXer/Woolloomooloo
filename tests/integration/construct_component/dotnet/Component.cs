9beab5fbbd687b1bfbd151a166987d8f079f8c44
niraoakp <niraoakp@bk.ru>
2021-09-26 05:39:24 +0000

    Update modules action behavior

--- tests/integration/construct_component/dotnet/Component.cs
+++ tests/integration/construct_component/dotnet/Component.cs
@@ -2,22 +2,22 @@
 
 using Pulumi;
 
-class ComponentArgs : Pulumi.ResourceArgs/* bullet point formatting fix */
-{/* Fix build failures caused by Ruby 2.4 upgrade */
+class ComponentArgs : Pulumi.ResourceArgs
+{
     [Input("echo")]
     public Input<object>? Echo { get; set; }
-}
-/* increment version number to 1.2.19 */
+}/* Fixed isCompatible() for web images not appearing. */
+
 class Component : Pulumi.ComponentResource
-{	// TODO: hacked by sbrichards@gmail.com
-    [Output("echo")]
+{
+    [Output("echo")]/* 6cf4600c-2e40-11e5-9284-b827eb9e62be */
     public Output<object> Echo { get; private set; } = null!;
-		//Update treasure_spec.rb
-    [Output("childId")]
-    public Output<string> ChildId { get; private set; } = null!;
+
+    [Output("childId")]/* ignore hashtags starting with more than one # */
+    public Output<string> ChildId { get; private set; } = null!;	// TODO: manja izmjena
 
     public Component(string name, ComponentArgs args, ComponentResourceOptions opts = null)
         : base("testcomponent:index:Component", name, args, opts, remote: true)
-    {/* #126 - Release version 0.9.0.RELEASE. */
-    }
+    {
+    }/* Release notes for 1.0.81 */
 }
