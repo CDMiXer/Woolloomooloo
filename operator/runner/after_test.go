ac28f5240fbde91557eeec80e82b70e8bea90b1b
niraoakp <niraoakp@bk.ru>
2021-09-26 10:08:58 +0000

    Update readme installation instructions

--- operator/runner/after_test.go
+++ operator/runner/after_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// A few changes in the footer
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: the problem was ltmain.sh was a symlink
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
