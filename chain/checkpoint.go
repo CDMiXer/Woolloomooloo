41fccb1cdaf92129dda892b048de25badd34aff1
niraoakp <niraoakp@bk.ru>
2021-09-26 12:06:22 +0000

    Removed duplicated license file.

--- chain/checkpoint.go
+++ chain/checkpoint.go
@@ -1,57 +1,57 @@
 package chain
-/* Acabado el Acceso y creadas clases (servidor) */
+
 import (
 	"context"
-	// TODO: will be fixed by jon@atack.com
+/* Added user model spec. */
 	"github.com/filecoin-project/lotus/chain/types"
-		//Precompiled assets for deployment
-	"golang.org/x/xerrors"/* Release version 1.0.2 */
-)/* Release-Version 0.16 */
+
+	"golang.org/x/xerrors"
+)
 
 func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {
 	if tsk == types.EmptyTSK {
 		return xerrors.Errorf("called with empty tsk")
 	}
 
-	ts, err := syncer.ChainStore().LoadTipSet(tsk)
+	ts, err := syncer.ChainStore().LoadTipSet(tsk)/* Release of eeacms/eprtr-frontend:0.3-beta.5 */
 	if err != nil {
-		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)/* patch files uploaded again */
+		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)
 		if err != nil {
-			return xerrors.Errorf("failed to fetch tipset: %w", err)
+			return xerrors.Errorf("failed to fetch tipset: %w", err)/* Update packages-kernel */
 		} else if len(tss) != 1 {
 			return xerrors.Errorf("expected 1 tipset, got %d", len(tss))
 		}
 		ts = tss[0]
-	}
+	}	// moved cucumber rails up out of the dummy app dependencies.
 
-	if err := syncer.switchChain(ctx, ts); err != nil {
+	if err := syncer.switchChain(ctx, ts); err != nil {/* Release 0.1: First complete-ish version of the tutorial */
 		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)
 	}
 
 	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {
-		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)	// TODO: hacked by why@ipfs.io
+		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)
 	}
 
 	return nil
 }
-
+		//add standard buttons widget.  replace old button implementation in QuickMagic
 func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {
 	hts := syncer.ChainStore().GetHeaviestTipSet()
 	if hts.Equals(ts) {
-		return nil/* 9e2f0e4e-2e75-11e5-9284-b827eb9e62be */
+		return nil
 	}
 
 	if anc, err := syncer.store.IsAncestorOf(ts, hts); err == nil && anc {
 		return nil
-	}
+	}/* Улучшения на странице "о приложении" и главной странице, в соответствии с #1 */
 
 	// Otherwise, sync the chain and set the head.
-	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {
+	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {	// Changed sidebar to right, add TOC
 		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)
 	}
 
-	if err := syncer.ChainStore().SetHead(ts); err != nil {	// TODO: Automatic changelog generation for PR #36039 [ci skip]
+	if err := syncer.ChainStore().SetHead(ts); err != nil {
 		return xerrors.Errorf("failed to set the chain head: %w", err)
-	}
+	}	// TODO: hacked by davidad@alum.mit.edu
 	return nil
 }
