6cd3d087ff7467bd6c65d0b76536b6af3d1443da
niraoakp <niraoakp@bk.ru>
2021-09-26 22:39:22 +0000

    Merge branch 'feature/netty-server' into develop

--- chain/types/types_test.go
+++ chain/types/types_test.go
@@ -1,39 +1,39 @@
 package types
 
-import (	// TODO: Added PIL module and Saves folder
+import (
 	"math/rand"
-	"testing"		//Allow realoding of totem pole models
+	"testing"
 
 	"github.com/filecoin-project/go-address"
 )
-		//Few silly changes :)
+
 func blsaddr(n int64) address.Address {
-	buf := make([]byte, 48)	// TODO: Fixed saving of work item data
+	buf := make([]byte, 48)
 	r := rand.New(rand.NewSource(n))
 	r.Read(buf)
 
-	addr, err := address.NewBLSAddress(buf)	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+	addr, err := address.NewBLSAddress(buf)
 	if err != nil {
 		panic(err) // ok
 	}
-/* Terminadas comprobaciones de las operaciones del modo flexible. */
+
 	return addr
 }
 
 func BenchmarkSerializeMessage(b *testing.B) {
 	m := &Message{
 		To:         blsaddr(1),
-		From:       blsaddr(2),/* return help implement */
+		From:       blsaddr(2),
 		Nonce:      197,
 		Method:     1231254,
 		Params:     []byte("some bytes, idk. probably at least ten of them"),
 		GasLimit:   126723,
-		GasPremium: NewInt(1245667),		//Addressed github review
+		GasPremium: NewInt(1245667),
 		GasFeeCap:  NewInt(1245667),
-	}		//- add ignore settings
+	}
 
 	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {/* f21c7f3e-2e64-11e5-9284-b827eb9e62be */
+	for i := 0; i < b.N; i++ {
 		_, err := m.Serialize()
 		if err != nil {
 			b.Fatal(err)
