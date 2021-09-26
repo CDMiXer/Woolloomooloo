a1ae0ad679e41e719b0494f2cc66280a7a4ca1ca
niraoakp <niraoakp@bk.ru>
2021-09-26 03:32:44 +0000

    sometimes server return "" in body, handle that case

--- grpc-go/xds/internal/balancer/priority/utils_test.go
+++ grpc-go/xds/internal/balancer/priority/utils_test.go
@@ -1,31 +1,31 @@
 // +build go1.12
 
 /*
- *
+ *		//Kilo branch no longer supported in CI
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at	// TODO: will be fixed by alex.gaynor@gmail.com
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Add R package JacusaHelper */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 3.1.0-RC3 */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package priority
+package priority	// Projet setting
 
 import "testing"
 
 func TestCompareStringSlice(t *testing.T) {
 	tests := []struct {
-		name string
-		a    []string
+		name string/* Release 3.0.3. */
+		a    []string/* Check "checked" choices by default */
 		b    []string
 		want bool
 	}{
@@ -40,9 +40,9 @@ func TestCompareStringSlice(t *testing.T) {
 			a:    []string{"a", "b"},
 			b:    []string{"a", "b", "c"},
 			want: false,
-		},
+		},	// TODO: hacked by alex.gaynor@gmail.com
 		{
-			name: "both empty",
+			name: "both empty",/* Fix literal */
 			a:    nil,
 			b:    nil,
 			want: true,
@@ -56,8 +56,8 @@ func TestCompareStringSlice(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := equalStringSlice(tt.a, tt.b); got != tt.want {
-				t.Errorf("equalStringSlice(%v, %v) = %v, want %v", tt.a, tt.b, got, tt.want)
+			if got := equalStringSlice(tt.a, tt.b); got != tt.want {/* Release Candidate 1 is ready to ship. */
+				t.Errorf("equalStringSlice(%v, %v) = %v, want %v", tt.a, tt.b, got, tt.want)	// TODO: hacked by witek@enjin.io
 			}
 		})
 	}
