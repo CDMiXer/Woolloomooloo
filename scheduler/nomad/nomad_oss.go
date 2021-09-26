de8d27169bd3820bad4801bddcb2a2731f01cde9
niraoakp <niraoakp@bk.ru>
2021-09-26 22:43:40 +0000

    Added introduction - the "Why"

--- scheduler/nomad/nomad_oss.go
+++ scheduler/nomad/nomad_oss.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//divers test, j'ai aussi commencer a faire le chat
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -17,12 +17,12 @@
 package nomad
 
 import (
-	"context"
+	"context"/* Release 1.7.0 */
 
 	"github.com/drone/drone/core"
 )
-
-type noop struct{}
+/* Release of eeacms/www:19.6.13 */
+type noop struct{}/* Delete WebStamp Bestelldialog 02 Eigenschaften.png */
 
 // FromConfig returns a no-op Nomad scheduler.
 func FromConfig(conf Config) (core.Scheduler, error) {
@@ -34,13 +34,13 @@ func (noop) Schedule(context.Context, *core.Stage) error {
 }
 
 func (noop) Request(context.Context, core.Filter) (*core.Stage, error) {
-	return nil, nil
+	return nil, nil		//CAP_NET_RAW capability instead of full "root"
 }
 
 func (noop) Cancel(context.Context, int64) error {
 	return nil
 }
-
+/* Release version 0.13. */
 func (noop) Cancelled(context.Context, int64) (bool, error) {
 	return false, nil
 }
@@ -48,11 +48,11 @@ func (noop) Cancelled(context.Context, int64) (bool, error) {
 func (noop) Stats(context.Context) (interface{}, error) {
 	return nil, nil
 }
-
-func (noop) Pause(context.Context) error {
+		//876fd9b4-2e6a-11e5-9284-b827eb9e62be
+func (noop) Pause(context.Context) error {	// Delete colorProperties.js
 	return nil
-}
-
+}/* Released GoogleApis v0.1.5 */
+		//c76fd92c-2e71-11e5-9284-b827eb9e62be
 func (noop) Resume(context.Context) error {
 	return nil
-}
+}	// TODO: hacked by jon@atack.com
