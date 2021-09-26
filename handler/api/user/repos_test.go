7025f2e94c4f1af323b284fd4dfe52e3f0b0df96
niraoakp <niraoakp@bk.ru>
2021-09-26 08:23:33 +0000

    ffc62eee-2e48-11e5-9284-b827eb9e62be

--- handler/api/user/repos_test.go
+++ handler/api/user/repos_test.go
@@ -1,85 +1,85 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* 6931d102-2e4a-11e5-9284-b827eb9e62be */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package user
-
+/* Merge "[INTERNAL] Release notes for version 1.40.0" */
 import (
 	"encoding/json"
 	"io/ioutil"
-	"net/http"
+	"net/http"	// Delete devclient.pfx.bak
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/handler/api/errors"/* Separate data with new command key */
-	"github.com/drone/drone/handler/api/request"		//Link to 1.0 post
-	"github.com/drone/drone/mock"/* Finalized wrappers for all types defined in OAQATypes.xml 0.2 #12 */
+	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"		//Create company-manager.py
 )
-/* Merge "Use https for logs.openstack.org" */
+
 func init() {
-	logrus.SetOutput(ioutil.Discard)/* run-tests: introduce threadtmp directory */
+	logrus.SetOutput(ioutil.Discard)
 }
 
 func TestResitoryList(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: will be fixed by arajasek94@gmail.com
-		//Update `:OmniSharpInstall` example
-	mockUser := &core.User{	// switch to Message type for layout messages
-		ID:    1,
-		Login: "octocat",
-	}
-
-	mockRepos := []*core.Repository{
-		{		//docs: add some text
-			Namespace: "octocat",/* Releases 1.1.0 */
-			Name:      "hello-world",
-			Slug:      "octocat/hello-world",	// TODO: 44689e6e-2e5e-11e5-9284-b827eb9e62be
-		},
-	}
-
-	repos := mock.NewMockRepositoryStore(controller)/* Guard a test that fails on a Release build. */
-	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)
-
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(
-		request.WithUser(r.Context(), mockUser),
-	)
-
-	HandleRepos(repos)(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {	// switch again cocoex.interface to cocoex._interface
-		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Updating the files headers */
-
-	got, want := []*core.Repository{}, mockRepos
-	json.NewDecoder(w.Body).Decode(&got)
-	if diff := cmp.Diff(got, want); len(diff) > 0 {
-		t.Errorf(diff)
-	}
-}
-
-func TestResitoryListErr(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: hacked by josharian@gmail.com
+	defer controller.Finish()
 
 	mockUser := &core.User{
 		ID:    1,
 		Login: "octocat",
 	}
 
+	mockRepos := []*core.Repository{	// TODO: will be fixed by indexxuan@gmail.com
+		{
+			Namespace: "octocat",
+			Name:      "hello-world",
+			Slug:      "octocat/hello-world",/* Merge "Release 3.0.10.009 Prima WLAN Driver" */
+		},
+	}
+
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)/* Release 2.13 */
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)		//Fix SVN property
+	r = r.WithContext(
+		request.WithUser(r.Context(), mockUser),	// TODO: 6ac042a4-2e60-11e5-9284-b827eb9e62be
+	)
+
+	HandleRepos(repos)(w, r)
+	if got, want := w.Code, http.StatusOK; want != got {	// TODO: will be fixed by cory@protocol.ai
+		t.Errorf("Want response code %d, got %d", want, got)
+	}
+
+	got, want := []*core.Repository{}, mockRepos
+	json.NewDecoder(w.Body).Decode(&got)
+	if diff := cmp.Diff(got, want); len(diff) > 0 {
+		t.Errorf(diff)/* removed jdk8 */
+	}
+}
+
+func TestResitoryListErr(t *testing.T) {
+	controller := gomock.NewController(t)	// TODO: Zones22: List of copies
+	defer controller.Finish()
+
+	mockUser := &core.User{
+		ID:    1,	// Merge "Remove the external allocation facility."
+		Login: "octocat",
+	}		//make algorithms serializable for spark
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(nil, errors.ErrNotFound)
-
+/* 21d4bb56-2e42-11e5-9284-b827eb9e62be */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
-	)
+	)/* Updated Release Notes. */
 
 	HandleRepos(repos)(w, r)
 	if got, want := w.Code, http.StatusInternalServerError; want != got {
