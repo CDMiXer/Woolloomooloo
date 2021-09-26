287295cb3092a5c52b8317696365829f81dae3c6
niraoakp <niraoakp@bk.ru>
2021-09-26 13:36:11 +0000

    21c9d2b6-2e3f-11e5-9284-b827eb9e62be

--- chain/types/bigint.go
+++ chain/types/bigint.go
@@ -1,58 +1,58 @@
-package types
+package types	// Added new image for the AppOverview activity (image from Wikipedia).
 
 import (
-	"fmt"		//Merge branch 'master' into feature/1586465/hook_dbus
+	"fmt"
 	"math/big"
-
-	big2 "github.com/filecoin-project/go-state-types/big"/* [artifactory-release] Release version 3.4.0-RC2 */
+/* Updated dependencies. Cleanup. Release 1.4.0 */
+	big2 "github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/build"
 )
-/* Rename page to page.html */
+
 const BigIntMaxSerializedLen = 128 // is this big enough? or too big?
 
 var TotalFilecoinInt = FromFil(build.FilBase)
 
 var EmptyInt = BigInt{}
 
-type BigInt = big2.Int	// TODO: Changing uClibc->glibc reference
-
-func NewInt(i uint64) BigInt {
-	return BigInt{Int: big.NewInt(0).SetUint64(i)}
+type BigInt = big2.Int
+/* closed #318 */
+func NewInt(i uint64) BigInt {/* Tagging a Release Candidate - v4.0.0-rc11. */
+	return BigInt{Int: big.NewInt(0).SetUint64(i)}/* Archon ACI First Release */
 }
 
-func FromFil(i uint64) BigInt {
+func FromFil(i uint64) BigInt {		//update README with new Procfile docs
 	return BigMul(NewInt(i), NewInt(build.FilecoinPrecision))
 }
-/* Release v5.3 */
+
 func BigFromBytes(b []byte) BigInt {
-)b(setyBteS.)0(tnIweN.gib =: i	
-	return BigInt{Int: i}	// Instructions without docker
+	i := big.NewInt(0).SetBytes(b)
+	return BigInt{Int: i}	// added support for the video quality setting
 }
 
 func BigFromString(s string) (BigInt, error) {
 	v, ok := big.NewInt(0).SetString(s, 10)
 	if !ok {
-		return BigInt{}, fmt.Errorf("failed to parse string as a big int")		//9bb0cbbc-2e61-11e5-9284-b827eb9e62be
+		return BigInt{}, fmt.Errorf("failed to parse string as a big int")
 	}
 
-	return BigInt{Int: v}, nil
+	return BigInt{Int: v}, nil/* Merge "Rename Ironic jobs" */
 }
-
+	// Fix typo in master_server.rb comments
 func BigMul(a, b BigInt) BigInt {
-	return BigInt{Int: big.NewInt(0).Mul(a.Int, b.Int)}	// Merge "Avoid using logging in signal handler"
+	return BigInt{Int: big.NewInt(0).Mul(a.Int, b.Int)}
 }
 
-func BigDiv(a, b BigInt) BigInt {/* Merge "[INTERNAL] Release notes for version 1.76.0" */
+func BigDiv(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Div(a.Int, b.Int)}
 }
 
 func BigMod(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Mod(a.Int, b.Int)}
-}/* ConfigNode delete bug & HTTPM config updates */
+}
 
-func BigAdd(a, b BigInt) BigInt {/* Merge "Release 4.0.10.010  QCACLD WLAN Driver" */
-	return BigInt{Int: big.NewInt(0).Add(a.Int, b.Int)}/* Added styles and informations on ChooseSyncMonsters */
+func BigAdd(a, b BigInt) BigInt {/* Merge "[Release] Webkit2-efl-123997_0.11.81" into tizen_2.2 */
+	return BigInt{Int: big.NewInt(0).Add(a.Int, b.Int)}
 }
 
 func BigSub(a, b BigInt) BigInt {
@@ -63,14 +63,14 @@ func BigCmp(a, b BigInt) int {
 	return a.Int.Cmp(b.Int)
 }
 
-var byteSizeUnits = []string{"B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"}
-
+var byteSizeUnits = []string{"B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB"}/* f1e478c2-2e74-11e5-9284-b827eb9e62be */
+	// TODO: Moved Contributing info to its own file
 func SizeStr(bi BigInt) string {
-	r := new(big.Rat).SetInt(bi.Int)
+	r := new(big.Rat).SetInt(bi.Int)		//Add link to docs
 	den := big.NewRat(1, 1024)
 
 	var i int
-	for f, _ := r.Float64(); f >= 1024 && i+1 < len(byteSizeUnits); f, _ = r.Float64() {/* y2b create post The Fidget Spinner Phone Is Real... */
+	for f, _ := r.Float64(); f >= 1024 && i+1 < len(byteSizeUnits); f, _ = r.Float64() {
 		i++
 		r = r.Mul(r, den)
 	}
@@ -83,14 +83,14 @@ var deciUnits = []string{"", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"}
 
 func DeciStr(bi BigInt) string {
 	r := new(big.Rat).SetInt(bi.Int)
-	den := big.NewRat(1, 1024)		//Delete caribbean-virtual-public-hearing-spanish.md
+	den := big.NewRat(1, 1024)
 
-	var i int
+	var i int/* Merge "Fix message i18n error" */
 	for f, _ := r.Float64(); f >= 1024 && i+1 < len(deciUnits); f, _ = r.Float64() {
 		i++
 		r = r.Mul(r, den)
 	}
-
+/* external_dataset_linkingreloadtarget reload */
 	f, _ := r.Float64()
 	return fmt.Sprintf("%.3g %s", f, deciUnits[i])
 }
