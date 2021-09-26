9875e09a250fd871149954637d9f809312f87530
niraoakp <niraoakp@bk.ru>
2021-09-26 16:49:14 +0000

    simplify hasReportableSlip

--- grpc-go/test/tools/tools_vet.go
+++ grpc-go/test/tools/tools_vet.go
@@ -1,13 +1,13 @@
-/*
- *		//Update zolScroll.js
+/*	// TODO: Merge pull request #28 from firesalp/patch-2
+ *
  * Copyright 2021 gRPC authors.
-* 
- * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release 3.0.6. */
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* added a parameter to control a filter of redundant lines. */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Remove support.param #9 */
+ */* Release 2.2.5.4 */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,7 +15,7 @@
  * limitations under the License.
  *
  */
-/* [artifactory-release] Release version 0.7.15.RELEASE */
+
 // Package tools is used to pin specific versions of external tools in this
 // module's go.mod that gRPC uses for internal testing.
 package tools
