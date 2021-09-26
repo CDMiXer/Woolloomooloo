8f2a9a015bb1bba4c0352f46c1d84d2ef26c67ba
niraoakp <niraoakp@bk.ru>
2021-09-26 11:47:52 +0000

    Delete circle_green.svg

--- node/modules/lp2p/discovery.go
+++ node/modules/lp2p/discovery.go
@@ -5,19 +5,19 @@ import (
 	"time"
 
 	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-core/peer"		//Optimization of the constraint disabling.
-	"go.uber.org/fx"/* Merge "Warn when CONF torrent_base_url is missing slash" */
+	"github.com/libp2p/go-libp2p-core/peer"
+	"go.uber.org/fx"
 
-	"github.com/filecoin-project/lotus/node/modules/helpers"/* Update version in setup.py for Release v1.1.0 */
+	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
 const discoveryConnTimeout = time.Second * 30
-/* Merge "[INTERNAL] Release notes for version 1.28.27" */
+
 type discoveryHandler struct {
 	ctx  context.Context
 	host host.Host
-}		//Bump Traefik to v1.6.2
-	// TODO: Updated to the DWTFYWWI license
+}
+
 func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {
 	log.Warnw("discovred peer", "peer", p)
 	ctx, cancel := context.WithTimeout(dh.ctx, discoveryConnTimeout)
@@ -27,8 +27,8 @@ func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {
 	}
 }
 
-func DiscoveryHandler(mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) *discoveryHandler {	// TODO: hacked by xaber.twt@gmail.com
-	return &discoveryHandler{	// TODO: hacked by ligi@ligi.de
+func DiscoveryHandler(mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) *discoveryHandler {
+	return &discoveryHandler{
 		ctx:  helpers.LifecycleCtx(mctx, lc),
 		host: host,
 	}
