98aacf7af3ae18f8c11acb70f27d66dca94f43d4
niraoakp <niraoakp@bk.ru>
2021-09-26 05:28:37 +0000

    update bash highlighting

--- tests/integration/partial_state/step2/index.ts
+++ tests/integration/partial_state/step2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release notes: expand clang-cl blurb a little */
 
-// file is deliberately empty.
+// file is deliberately empty./* Released "Open Codecs" version 0.84.17315 */
 // The engine should delete "a" since it's in the checkpoint.
