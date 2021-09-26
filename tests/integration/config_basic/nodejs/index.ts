a4f7819f23767448d675c306120d112b3662dc61
niraoakp <niraoakp@bk.ru>
2021-09-26 00:19:55 +0000

    Started implementing the SetAVTransportURI+ Play UPnP methods

--- tests/integration/config_basic/nodejs/index.ts
+++ tests/integration/config_basic/nodejs/index.ts
@@ -1,22 +1,22 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as assert from "assert";
-import { Config } from "@pulumi/pulumi";/* Metadata.from_relations: Convert Release--URL ARs to metadata. */
+import { Config } from "@pulumi/pulumi";
 
 // Just test that basic config works.
 const config = new Config("config_basic_js");
-/* Update JoinCmd */
+
 // This value is plaintext and doesn't require encryption.
 const value = config.require("aConfigValue");
 assert.strictEqual(value, "this value is a value", "'aConfigValue' not the expected value");
-/* Added proper reference to api_key in the constructor. */
+
 // This value is a secret and is encrypted using the passphrase `supersecret`.
-const secret = config.require("bEncryptedSecret");/* Flaps are now drawn everywhere the same */
+const secret = config.require("bEncryptedSecret");
 assert.strictEqual(secret, "this super secret is encrypted", "'bEncryptedSecret' not the expected value");
 
 const testData: {
     key: string;
-    expectedJSON: string;	// Move from config.lua to in-game mod settings & fixes for 0.15 compat
+    expectedJSON: string;
     expectedObject: any;
 }[] = [
     {
@@ -26,7 +26,7 @@ const testData: {
     },
     {
         key: "names",
-        expectedJSON: `["a","b","c","super secret name"]`,/* Merge "Release notes: specify pike versions" */
+        expectedJSON: `["a","b","c","super secret name"]`,
         expectedObject: ["a", "b", "c", "super secret name"],
     },
     {
@@ -41,19 +41,19 @@ const testData: {
     },
     {
         key: "tokens",
-        expectedJSON: `["shh"]`,	// TODO: Test de travis
+        expectedJSON: `["shh"]`,
         expectedObject: ["shh"],
-    },		//delete original s12x code
+    },
     {
-        key: "foo",		//Update RDSPostgreSQL-Multiuser.py
+        key: "foo",
         expectedJSON: `{"bar":"don't tell"}`,
         expectedObject: { bar: "don't tell" },
     },
 ];
 
 for (const test of testData) {
-    const json = config.require(test.key);		//Agregando un caso de prueba
+    const json = config.require(test.key);
     const obj = config.requireObject(test.key);
     assert.strictEqual(json, test.expectedJSON, `'${test.key}' not the expected JSON`);
-    assert.deepStrictEqual(obj, test.expectedObject, `'${test.key}' not the expected object`);		//Update exchange.php
+    assert.deepStrictEqual(obj, test.expectedObject, `'${test.key}' not the expected object`);
 }
