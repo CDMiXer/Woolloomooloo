b1250d799efa3a5ff6febf421aeebc3aab09f28a
niraoakp <niraoakp@bk.ru>
2021-09-26 02:21:34 +0000

    Fixed enum validation bugs.

--- handler/api/queue/items.go
+++ handler/api/queue/items.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* better versions select focus styles */
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Release areca-7.2.14 */
 package queue
 
 import (
-	"net/http"
+	"net/http"/* fix SerialSocketServer when run without GUI */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -18,14 +18,14 @@ import (
 // json-encoded list of queue items to the response body.
 func HandleItems(store core.StageStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		items, err := store.ListIncomplete(ctx)
+		ctx := r.Context()/* Delete Fighter_sword.json */
+		items, err := store.ListIncomplete(ctx)	// TODO: fixing various issues Nate had: ready queue simply must be purged on reconnect
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Warnln("api: cannot get running items")
+				Warnln("api: cannot get running items")/* $164-Created a edit user page, added an edit action column in user-page. */
 			return
-		}
+		}		//auto generate map when player move
 		render.JSON(w, items, 200)
 	}
 }
