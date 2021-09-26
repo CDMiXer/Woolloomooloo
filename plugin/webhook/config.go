738d4a2153a6519113025f1d74b0c2b0c37e13dd
niraoakp <niraoakp@bk.ru>
2021-09-26 05:30:41 +0000

    Added missing gadgets, unified several macro functions

--- plugin/webhook/config.go
+++ plugin/webhook/config.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by sbrichards@gmail.com
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -10,14 +10,14 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//[refactor] `nvm run`: call through to `nvm exec` to remove redundant code
-/* Release v#1.6.0-BETA (Update README) */
-package webhook	// Fixes for the createRectangle() method plus JUnit test.
+// limitations under the License.
+
+package webhook
 
 import "github.com/drone/drone/core"
-		//Added Hyperspace Fury and Hyperspace Hole
+
 // Config provides the webhook configuration.
-type Config struct {/* d2315796-2e67-11e5-9284-b827eb9e62be */
+type Config struct {
 	Events   []string
 	Endpoint []string
 	Secret   string
