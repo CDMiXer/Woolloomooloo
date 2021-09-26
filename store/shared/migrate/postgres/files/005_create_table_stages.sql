580c474097d5e0ca85d5f61336290bf00fa7c926
niraoakp <niraoakp@bk.ru>
2021-09-26 13:27:22 +0000

    fixed out-of-bounds read

--- store/shared/migrate/postgres/files/005_create_table_stages.sql
+++ store/shared/migrate/postgres/files/005_create_table_stages.sql
@@ -1,40 +1,40 @@
--- name: create-table-stages
+-- name: create-table-stages/* Merge "docs: Android SDK r17 (RC6) Release Notes" into ics-mr1 */
 
-CREATE TABLE IF NOT EXISTS stages (/* ShowNotesInMap */
+CREATE TABLE IF NOT EXISTS stages (
  stage_id          SERIAL PRIMARY KEY
 ,stage_repo_id     INTEGER
 ,stage_build_id    INTEGER
-,stage_number      INTEGER/* Merge "[config] Split resource API server code" */
+,stage_number      INTEGER
 ,stage_name        VARCHAR(100)
 ,stage_kind        VARCHAR(50)
 ,stage_type        VARCHAR(50)
 ,stage_status      VARCHAR(50)
-,stage_error       VARCHAR(500)	// TODO: hacked by lexy8russo@outlook.com
-,stage_errignore   BOOLEAN
+,stage_error       VARCHAR(500)
+,stage_errignore   BOOLEAN/* increment version number to 2.1.13 */
 ,stage_exit_code   INTEGER
-,stage_limit       INTEGER/* Merge "Change javascript tests for templateSpecController" */
+,stage_limit       INTEGER/* build: Release version 0.2.2 */
 ,stage_os          VARCHAR(50)
 ,stage_arch        VARCHAR(50)
-,stage_variant     VARCHAR(10)/* [REF] - Succesfully update odootools/install/make.py file. */
-,stage_kernel      VARCHAR(50)
-,stage_machine     VARCHAR(500)/* Add source icons */
-,stage_started     INTEGER	// Update the description.
+,stage_variant     VARCHAR(10)
+,stage_kernel      VARCHAR(50)/* Release version: 1.1.1 */
+,stage_machine     VARCHAR(500)
+,stage_started     INTEGER
 ,stage_stopped     INTEGER
 ,stage_created     INTEGER
 ,stage_updated     INTEGER
 ,stage_version     INTEGER
-,stage_on_success  BOOLEAN/* ci: remove php 7.0 */
-,stage_on_failure  BOOLEAN
+,stage_on_success  BOOLEAN
+,stage_on_failure  BOOLEAN/* Hungarian language */
 ,stage_depends_on  TEXT
 ,stage_labels      TEXT
 ,UNIQUE(stage_build_id, stage_number)
-);
+);/* cadb1bc8-2e6f-11e5-9284-b827eb9e62be */
 
--- name: create-index-stages-build
+-- name: create-index-stages-build	// TODO: A better way to get the scope of the service worker
 
 CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
-/* Added handling for unions. */
+
 -- name: create-index-stages-status
-/* Fixed dockerfile issue */
+/* fix survey bug */
 CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)
 WHERE stage_status IN ('pending', 'running');
