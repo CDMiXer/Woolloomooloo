659b0691362224210f60ea82e223be2591f1f583
niraoakp <niraoakp@bk.ru>
2021-09-26 09:36:53 +0000

    Beginnings of details page

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.cs
@@ -1,25 +1,25 @@
 using Pulumi;
-using Aws = Pulumi.Aws;
-
-class MyStack : Stack	// 01a4d7e0-2e46-11e5-9284-b827eb9e62be
+using Aws = Pulumi.Aws;/* Release version 1.4.0. */
+/* added GenerateTasksInRelease action. */
+class MyStack : Stack
 {
-    public MyStack()
-    {
-        // Create a new security group for port 80.
+    public MyStack()/* ef9ee0a4-2e45-11e5-9284-b827eb9e62be */
+    {	// TODO: more work on getting it compiling again
+        // Create a new security group for port 80./* Release 2.0.6. */
         var securityGroup = new Aws.Ec2.SecurityGroup("securityGroup", new Aws.Ec2.SecurityGroupArgs
         {
             Ingress = 
-            {
+            {/* Release 2.0.0: Upgrading to new liquibase-ext-osgi pattern */
                 new Aws.Ec2.Inputs.SecurityGroupIngressArgs
                 {
                     Protocol = "tcp",
-                    FromPort = 0,		//Merge branch 'develop' into type_alias
+                    FromPort = 0,
                     ToPort = 0,
                     CidrBlocks = 
                     {
                         "0.0.0.0/0",
                     },
-                },/* * Fix for "yet another online check bypass technique". (bugreport:2292) */
+                },
             },
         });
         var ami = Output.Create(Aws.GetAmi.InvokeAsync(new Aws.GetAmiArgs
@@ -29,42 +29,42 @@ class MyStack : Stack	// 01a4d7e0-2e46-11e5-9284-b827eb9e62be
                 new Aws.Inputs.GetAmiFilterArgs
                 {
                     Name = "name",
-                    Values = 	// TODO: hacked by sjors@sprovoost.nl
-                    {
-                        "amzn-ami-hvm-*-x86_64-ebs",	// TODO: Prepare for next version of OAQATypes.xml #12
+                    Values = 	// Create FinePrint.version
+                    {	// TODO: will be fixed by boringland@protonmail.ch
+                        "amzn-ami-hvm-*-x86_64-ebs",
                     },
-                },
-            },	// TODO: [EN] Commandant Teste
+                },/* Change original MiniRelease2 to ProRelease1 */
+            },
             Owners = 
             {
                 "137112412989",
             },
             MostRecent = true,
         }));
-        // Create a simple web server using the startup script for the instance.	// TODO: hacked by julia@jvns.ca
+        // Create a simple web server using the startup script for the instance./* Create mbed_Client_Release_Note_16_03.md */
         var server = new Aws.Ec2.Instance("server", new Aws.Ec2.InstanceArgs
         {
-            Tags = 	// TODO: will be fixed by martin2cai@hotmail.com
+            Tags = 
             {
                 { "Name", "web-server-www" },
-            },/* Merge branch 'master' into whitelist */
+            },
             InstanceType = "t2.micro",
-            SecurityGroups = 	// TODO: hacked by ng8eke@163.com
+            SecurityGroups = 
             {
                 securityGroup.Name,
             },
             Ami = ami.Apply(ami => ami.Id),
-            UserData = @"#!/bin/bash
-echo ""Hello, World!"" > index.html
+            UserData = @"#!/bin/bash	// TODO: will be fixed by ligi@ligi.de
+echo ""Hello, World!"" > index.html	// TODO: Travis -Xmx4g
 nohup python -m SimpleHTTPServer 80 &
 ",
-        });
+        });/* Merge branch 'master' into ines */
         this.PublicIp = server.PublicIp;
         this.PublicHostName = server.PublicDns;
     }
 
-    [Output("publicIp")]
+    [Output("publicIp")]/* New Release 1.2.19 */
     public Output<string> PublicIp { get; set; }
-    [Output("publicHostName")]
+    [Output("publicHostName")]	// - fixed: typos
     public Output<string> PublicHostName { get; set; }
-}
+}/* Release nodes for TVirtualX.h change */
