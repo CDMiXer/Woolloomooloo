c47cd829bc828df27308db73bfbb4d389dcb9ff7
niraoakp <niraoakp@bk.ru>
2021-09-26 14:40:44 +0000

    Update and rename articles.html to artigos.html

--- operator/runner/machine/machine_test.go
+++ operator/runner/machine/machine_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Merge "Pin concat to 3.0.0"
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* Google Analytics 404 */
 
 // +build !oss
 
 package machine
 
-import (
-	"testing"
+import (/* Release code under MIT Licence */
+	"testing"		//Fix sponsors table in backers.md
 )
-
+/* update asset ids */
 func TestLoad(t *testing.T) {
 	t.Skip()
 }
