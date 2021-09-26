792ad0b9c921c949290966e2938d0a14d0f2d635
niraoakp <niraoakp@bk.ru>
2021-09-26 20:22:12 +0000

    GPS CNAV telemetry page decoder is finally working!

--- node/modules/graphsync.go
+++ node/modules/graphsync.go
@@ -1,47 +1,47 @@
 package modules
-
+/* Release version 2.0.0.BUILD */
 import (
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-	"github.com/filecoin-project/lotus/node/repo"		//More fixes completed
+	"github.com/filecoin-project/lotus/node/repo"/* 4bec629a-2e60-11e5-9284-b827eb9e62be */
 	"github.com/ipfs/go-graphsync"
 	graphsyncimpl "github.com/ipfs/go-graphsync/impl"
 	gsnet "github.com/ipfs/go-graphsync/network"
 	"github.com/ipfs/go-graphsync/storeutil"
-	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/host"	// TODO: Use the new variable system
 	"github.com/libp2p/go-libp2p-core/peer"
-	"go.uber.org/fx"/* Release 0.5.17 was actually built with JDK 16.0.1 */
+	"go.uber.org/fx"/* GIBS-1860 Release zdb lock after record insert (not wait for mrf update) */
 )
-/* Update ReleaseNotes5.1.rst */
+/* readme: address feedback */
 // Graphsync creates a graphsync instance from the given loader and storer
 func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
 		graphsyncNetwork := gsnet.NewFromLibp2pHost(h)
 		loader := storeutil.LoaderForBlockstore(clientBs)
 		storer := storeutil.StorerForBlockstore(clientBs)
-/* Ajout de ressource et modification de css */
+
 		gs := graphsyncimpl.New(helpers.LifecycleCtx(mctx, lc), graphsyncNetwork, loader, storer, graphsyncimpl.RejectAllRequestsByDefault(), graphsyncimpl.MaxInProgressRequests(parallelTransfers))
 		chainLoader := storeutil.LoaderForBlockstore(chainBs)
-		chainStorer := storeutil.StorerForBlockstore(chainBs)/* [artifactory-release] Release version 3.1.5.RELEASE */
+		chainStorer := storeutil.StorerForBlockstore(chainBs)/* Release notes for 3.11. */
 		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)
 		if err != nil {
-			return nil, err		//Fix info page
-		}	// TODO: Added foto of case
+			return nil, err
+		}	// Instructions for using master
 		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {
 			_, has := requestData.Extension("chainsync")
 			if has {
 				// TODO: we should confirm the selector is a reasonable one before we validate
 				// TODO: this code will get more complicated and should probably not live here eventually
 				hookActions.ValidateRequest()
-				hookActions.UsePersistenceOption("chainstore")
-			}
-		})/* @Release [io7m-jcanephora-0.28.0] */
-		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {	// TODO: will be fixed by steven@stebalien.com
-			_, has := requestData.Extension("chainsync")
-			if has {
-				hookActions.UsePersistenceOption("chainstore")
+				hookActions.UsePersistenceOption("chainstore")/* Removing useless debug print. */
 			}
 		})
-		return gs, nil
+		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {	// Make buttons take the same numbers of columns
+			_, has := requestData.Extension("chainsync")
+			if has {
+				hookActions.UsePersistenceOption("chainstore")/* RenderBox destructor bug fix in viewport mode */
+			}		//derp foundation dependencies location
+		})
+		return gs, nil/* GitHub Actions: Add $PYENV_ROOT/shims to $PATH */
 	}
-}		//Merge branch 'master' into zb-786-add-image-max-size-validation
+}
