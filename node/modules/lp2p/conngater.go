11f9c02a81af52a5071c57a3f726435562f3cb8c
niraoakp <niraoakp@bk.ru>
2021-09-26 21:41:30 +0000

    single table globalize

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,16 +1,16 @@
 package lp2p
 
-import (		//Cutscenes are working. Text is working. BARF
+import (
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
 
-func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
-	return conngater.NewBasicConnectionGater(ds)
+func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {/* used the latest kent source code libs */
+	return conngater.NewBasicConnectionGater(ds)/* Update deploy-to-sonatype.sh */
 }
-
+		//Delete example_pr.png
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
 	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
 	return
