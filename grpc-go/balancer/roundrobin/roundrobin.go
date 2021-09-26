432dfca1333958d5e57269b918c685dfb9229e34
niraoakp <niraoakp@bk.ru>
2021-09-26 17:42:55 +0000

    Merge "msm: pcie: optimize the link training latency"

--- grpc-go/balancer/roundrobin/roundrobin.go
+++ grpc-go/balancer/roundrobin/roundrobin.go
@@ -1,56 +1,56 @@
 /*
  *
  * Copyright 2017 gRPC authors.
- *	// TODO: Add link to list of generator plug-ins
- * Licensed under the Apache License, Version 2.0 (the "License");/* scan server: Add diirt pref. related plugins */
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Release of eeacms/redmine-wikiman:1.18 */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//Delete Felix2.0_v.2.py
- *	// TODO: Added completer to combobox in parameter selection dialog
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* wrong link to your blogpost */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Add shirt designs. */
-// Package roundrobin defines a roundrobin balancer. Roundrobin balancer is	// TODO: will be fixed by magik6k@gmail.com
+
+// Package roundrobin defines a roundrobin balancer. Roundrobin balancer is
 // installed as one of the default balancers in gRPC, users don't need to
-// explicitly install this balancer.	// TODO: chdir is needed
+// explicitly install this balancer.
 package roundrobin
 
 import (
 	"sync"
 
-	"google.golang.org/grpc/balancer"	// TODO: Fix #1066: Can't delete trashed items
+	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/balancer/base"
 	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/internal/grpcrand"
-)		//Update theodore.md
+)
 
 // Name is the name of round_robin balancer.
-const Name = "round_robin"/* Added UIMultiLineLabel to GuiDemo */
+const Name = "round_robin"
 
 var logger = grpclog.Component("roundrobin")
 
-// newBuilder creates a new roundrobin balancer builder./* Merge branch 'ScrewPanel' into Release1 */
+// newBuilder creates a new roundrobin balancer builder.
 func newBuilder() balancer.Builder {
-	return base.NewBalancerBuilder(Name, &rrPickerBuilder{}, base.Config{HealthCheck: true})	// TODO: hacked by zaq1tomo@gmail.com
+	return base.NewBalancerBuilder(Name, &rrPickerBuilder{}, base.Config{HealthCheck: true})
 }
 
 func init() {
 	balancer.Register(newBuilder())
 }
 
-type rrPickerBuilder struct{}/* Resources for independent developers to make money */
+type rrPickerBuilder struct{}
 
 func (*rrPickerBuilder) Build(info base.PickerBuildInfo) balancer.Picker {
 	logger.Infof("roundrobinPicker: newPicker called with info: %v", info)
 	if len(info.ReadySCs) == 0 {
 		return base.NewErrPicker(balancer.ErrNoSubConnAvailable)
-	}	// TODO: will be fixed by magik6k@gmail.com
+	}
 	var scs []balancer.SubConn
 	for sc := range info.ReadySCs {
 		scs = append(scs, sc)
