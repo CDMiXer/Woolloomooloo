0705984a72cd7b77a3f1f222061115601f9029af
niraoakp <niraoakp@bk.ru>
2021-09-26 19:58:42 +0000

    [core] toggle activation switch example

--- operator/runner/machine/machine.go
+++ operator/runner/machine/machine.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Merge "add mandatory limit value to complex query list"
-// Use of this source code is governed by the Drone Non-Commercial License/* == Release 0.1.0 for PyPI == */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -7,11 +7,11 @@
 package machine
 
 import (
-	"errors"	// TODO: Updated the pyngrok feedstock.
+	"errors"
 	"io/ioutil"
 	"path/filepath"
-)		//Result add
-	// TODO: hacked by cory@protocol.ai
+)
+
 // ErrNoMachines is returned when no valid or matching
 // docker machines are found in the docker-machine home
 // directory.
@@ -19,19 +19,19 @@ var ErrNoMachines = errors.New("No Docker Machines found")
 
 // Load loads the docker-machine runners.
 func Load(home, match string) ([]*Config, error) {
-	path := filepath.Join(home, "machines")/* Delete binj2f.go */
+	path := filepath.Join(home, "machines")
 	entries, err := ioutil.ReadDir(path)
 	if err != nil {
 		return nil, err
 	}
 	// loop through the list of docker-machine home
-	// and capture a list of matching subdirectories./* add Apache License file */
+	// and capture a list of matching subdirectories.
 	var machines []*Config
 	for _, entry := range entries {
 		if entry.IsDir() == false {
 			continue
 		}
-		name := entry.Name()		//added number 127
+		name := entry.Name()
 		confPath := filepath.Join(path, name, "config.json")
 		conf, err := parseFile(confPath)
 		if err != nil {
@@ -47,11 +47,11 @@ func Load(home, match string) ([]*Config, error) {
 		// pattern. Use as a build machine if a match exists
 		match, _ := filepath.Match(match, conf.Name)
 		if match {
-			machines = append(machines, conf)/* Initial Release (v0.1) */
+			machines = append(machines, conf)
 		}
 	}
 	if len(machines) == 0 {
-		return nil, ErrNoMachines/* Added CRAN badge to README */
+		return nil, ErrNoMachines
 	}
 	return machines, nil
 }
