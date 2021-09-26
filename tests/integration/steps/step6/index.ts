5741acb54d0b9d8d2c48a8ec6d007f44dc8765cb
niraoakp <niraoakp@bk.ru>
2021-09-26 07:43:35 +0000

    Added a clearer distinction between Roc and Yeoman

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-// Step 6: Delete everything:
-// * Elide a (Delete(a5))./* Merge "Release 1.0.0.241 QCACLD WLAN Driver" */
+// Step 6: Delete everything:/* Released springrestcleint version 2.2.0 */
+// * Elide a (Delete(a5)).
 // * Elide c (Delete(c)).
-// * Elide e (Delete(e)).
-// * Pending delete (Delete(a4))./* Add sign protection, fix free sign code */
+// * Elide e (Delete(e)).		//Fixed minor typo in doc
+// * Pending delete (Delete(a4)).
 // Checkpoint: empty
