bcc33565f6e348b559e09a020999a1c3bdb23f07
niraoakp <niraoakp@bk.ru>
2021-09-26 09:14:50 +0000

    Corrigido autoload

--- tests/integration/dependency_steps/step2/index.ts
+++ tests/integration/dependency_steps/step2/index.ts
@@ -5,5 +5,5 @@ import { Resource } from "./resource";
 // Step 2: Reverse the dependency graph.
 // Checkpoint dependency graph: b -> a
 const b = new Resource("b", { state: 2 })
-const a = new Resource("a", { state: 1, resource: b })/* Release 0.0.33 */
+const a = new Resource("a", { state: 1, resource: b })
 // Dependency graph: a -> b
