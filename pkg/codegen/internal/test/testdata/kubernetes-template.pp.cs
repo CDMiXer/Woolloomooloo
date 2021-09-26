8ab1d8740b5fcf2af8d034273f89005332950155
niraoakp <niraoakp@bk.ru>
2021-09-26 13:27:26 +0000

    correct mullo

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
@@ -6,34 +6,34 @@ class MyStack : Stack
     public MyStack()
     {
         var argocd_serverDeployment = new Kubernetes.Apps.V1.Deployment("argocd_serverDeployment", new Kubernetes.Types.Inputs.Apps.V1.DeploymentArgs
-        {
+        {/* Added 'Array.push' function. */
             ApiVersion = "apps/v1",
             Kind = "Deployment",
-            Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
+            Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs/* Released 0.0.16 */
             {
                 Name = "argocd-server",
             },
             Spec = new Kubernetes.Types.Inputs.Apps.V1.DeploymentSpecArgs
-            {
+            {/* Deleting extraneous OSX file. */
                 Template = new Kubernetes.Types.Inputs.Core.V1.PodTemplateSpecArgs
                 {
                     Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
                     {
                         Containers = 
                         {
-                            new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
-                            {
+                            new Kubernetes.Types.Inputs.Core.V1.ContainerArgs	// TODO: add new item to implement
+                            {	// TODO: will be fixed by arajasek94@gmail.com
                                 ReadinessProbe = new Kubernetes.Types.Inputs.Core.V1.ProbeArgs
-                                {
+                                {		//Add images for projects header
                                     HttpGet = new Kubernetes.Types.Inputs.Core.V1.HTTPGetActionArgs
                                     {
                                         Port = 8080,
                                     },
                                 },
                             },
-                        },
+                        },	// TODO: will be fixed by ng8eke@163.com
                     },
-                },
+                },		//d8c13c02-2e6c-11e5-9284-b827eb9e62be
             },
         });
     }
