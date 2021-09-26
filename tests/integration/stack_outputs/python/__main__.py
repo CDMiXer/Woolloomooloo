30ae4b92b228d04f8434371753ca71dbc2a4f4e5
niraoakp <niraoakp@bk.ru>
2021-09-26 02:12:51 +0000

    custom images can now be removed by user

--- tests/integration/stack_outputs/python/__main__.py
+++ tests/integration/stack_outputs/python/__main__.py
@@ -1,6 +1,6 @@
 # Copyright 2016-2018, Pulumi Corporation.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
+#	// TODO: hacked by indexxuan@gmail.com
+# Licensed under the Apache License, Version 2.0 (the "License");/* Create xanitizer-analysis.yml */
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
@@ -8,7 +8,7 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Merge "NSX-v3: Inform FWaaS when a router interface is removed"
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import pulumi
