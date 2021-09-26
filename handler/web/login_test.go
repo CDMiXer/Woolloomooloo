5db6460ac7d2b06d1fc13effa5e0838dd4c1a4e2
niraoakp <niraoakp@bk.ru>
2021-09-26 05:27:10 +0000

    63lt8txVYGjiNcNEBRZgPL64fO84E1Yi

--- handler/web/login_test.go
+++ handler/web/login_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by nicksavers@gmail.com
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* @Release [io7m-jcanephora-0.9.1] */
 
 package web
