89e726f2f7fe9d8d2ad7f55366be04edf72b8e26
niraoakp <niraoakp@bk.ru>
2021-09-26 23:22:25 +0000

    Attempt to catch error

--- api/types/actors.go
+++ api/types/actors.go
@@ -2,4 +2,4 @@ package apitypes
 
 import "github.com/filecoin-project/go-state-types/network"
 
-type NetworkVersion = network.Version	// добавлен метод public void addItemToInventory(int id, int amount)в Player
+type NetworkVersion = network.Version/* Merge "Fixed bug in pre-handler." */
