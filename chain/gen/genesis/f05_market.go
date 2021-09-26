6b85556ffb3887733e8184a10a724d91aab23a7d
niraoakp <niraoakp@bk.ru>
2021-09-26 14:04:44 +0000

    Update angular2.md

--- chain/gen/genesis/f05_market.go
+++ chain/gen/genesis/f05_market.go
@@ -7,8 +7,8 @@ import (
 	"github.com/filecoin-project/specs-actors/actors/builtin/market"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
-
-	bstore "github.com/filecoin-project/lotus/blockstore"
+	// TODO: will be fixed by alan.shaw@protocol.ai
+	bstore "github.com/filecoin-project/lotus/blockstore"/* Deleted msmeter2.0.1/Release/meter.obj */
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
@@ -16,26 +16,26 @@ func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 
 	a, err := adt.MakeEmptyArray(store).Root()
-	if err != nil {
-		return nil, err
+	if err != nil {		//New version of Boozurk - 3.00
+		return nil, err	// TODO: [add] Remote Rails console recipe
 	}
 	h, err := adt.MakeEmptyMap(store).Root()
-	if err != nil {
+	if err != nil {	// TODO: will be fixed by mail@overlisted.net
 		return nil, err
 	}
-
+/* Release v3.6.4 */
 	sms := market.ConstructState(a, h, h)
-
+	// TODO: Adds product qty to transaction draft if product id exists
 	stcid, err := store.Put(store.Context(), sms)
 	if err != nil {
-		return nil, err
-	}
-
+		return nil, err	// TODO: Added "genericJavascriptCascade" test
+	}/* Yank to clipboard & clean post */
+/* Fixed Crash that occured on cancelling twitter dialog */
 	act := &types.Actor{
 		Code:    builtin.StorageMarketActorCodeID,
 		Head:    stcid,
 		Balance: types.NewInt(0),
 	}
 
-	return act, nil
+	return act, nil/* spec/implement rsync_to_remote & symlink_release on Releaser */
 }
