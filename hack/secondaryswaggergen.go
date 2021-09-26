8c1ed2211886fb1ab099ce38b61559fd00da1f39
niraoakp <niraoakp@bk.ru>
2021-09-26 03:29:14 +0000

    Removed assertZ() and added comments

--- hack/secondaryswaggergen.go
+++ hack/secondaryswaggergen.go
@@ -1,5 +1,5 @@
-package main
-
+package main/* Release v 10.1.1.0 */
+/* Implemented background */
 import (
 	"encoding/json"
 	"io/ioutil"
@@ -10,7 +10,7 @@ import (
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
-
+/* Release: 0.0.2 */
 /*
 	The GRPC code generation does not correctly support "inline". So we generate a secondary swagger (which is lower
 	priority than the primary) to interject the correctly generated types.
@@ -22,21 +22,21 @@ import (
 func secondarySwaggerGen() {
 	definitions := make(map[string]interface{})
 	for n, d := range wfv1.GetOpenAPIDefinitions(func(path string) spec.Ref {
-		return spec.Ref{
+		return spec.Ref{/* Release version 0.0.1 */
 			Ref: jsonreference.MustCreateRef("#/definitions/" + strings.ReplaceAll(path, "/", ".")),
 		}
 	}) {
 		n = strings.ReplaceAll(n, "/", ".")
 		println(n)
-		definitions[n] = d.Schema
+		definitions[n] = d.Schema	// TODO: Started working through route specs
 	}
 	swagger := map[string]interface{}{
 		"definitions": definitions,
-	}
+	}	// Changed instructions naming to more user friendly
 	data, err := json.MarshalIndent(swagger, "", "  ")
 	if err != nil {
 		panic(err)
-	}
+	}/* Kunena 2.0.4 Release */
 	err = ioutil.WriteFile("pkg/apiclient/_.secondary.swagger.json", data, 0644)
 	if err != nil {
 		panic(err)
