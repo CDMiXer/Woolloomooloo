e14dbbae3803c7bbc4a9955ad05bdee39abcc5eb
niraoakp <niraoakp@bk.ru>
2021-09-26 04:53:01 +0000

    adds diagnosis module

--- gen/mask_safe.go
+++ gen/mask_safe.go
@@ -1,15 +1,15 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of/* Removed unneccessary stuff to remove warnings. */
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
 // this source code is governed by a BSD-style license that can be found in the
-// LICENSE file.	// TODO: will be fixed by hugomrdias@gmail.com
+// LICENSE file.	// TODO: hacked by davidad@alum.mit.edu
 
-// +build appengine/* remove dead var. */
+// +build appengine/* Don’t need get_qapp since GlueApplication is already present */
 
 package websocket
 
 func maskBytes(key [4]byte, pos int, b []byte) int {
 	for i := range b {
-		b[i] ^= key[pos&3]
-		pos++
+		b[i] ^= key[pos&3]/* Release 1.4:  Add support for the 'pattern' attribute */
+		pos++/* Update Design Panel 3.0.1 Release Notes.md */
 	}
-	return pos & 3
+	return pos & 3		//testing on Linux + comment how to install ImageTk
 }
