a638b17e8396d6d6652d15c1709be763d020fbc7
niraoakp <niraoakp@bk.ru>
2021-09-26 20:31:04 +0000

    Fix PyPI badge in README

--- pkg/codegen/internal/test/testdata/azure-sa.pp.ts
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.ts
@@ -1,21 +1,21 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as azure from "@pulumi/azure";
+import * as azure from "@pulumi/azure";	// TODO: hacked by boringland@protonmail.ch
 
-const config = new pulumi.Config();
+const config = new pulumi.Config();	// Update html-parser.js
 const storageAccountNameParam = config.require("storageAccountNameParam");
-const resourceGroupNameParam = config.require("resourceGroupNameParam");	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-const resourceGroupVar = azure.core.getResourceGroup({	// Daily work, making it useful for the toyDB. First commit use_minimal.py
-    name: resourceGroupNameParam,
-});	// TODO: Issue #9013 resolved
-const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);/* More SVN keyword changes. */
+const resourceGroupNameParam = config.require("resourceGroupNameParam");
+const resourceGroupVar = azure.core.getResourceGroup({
+    name: resourceGroupNameParam,/* Clean up art contest less/coffee and add links to entrants. */
+});
+const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
 const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";
-const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";
+const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";		//Accuracy update
 const storageAccountResource = new azure.storage.Account("storageAccountResource", {
     name: storageAccountNameParam,
     accountKind: "StorageV2",
-    location: locationParam,
+    location: locationParam,/* doc: add Pimple in credits */
     resourceGroupName: resourceGroupNameParam,
     accountTier: storageAccountTierParam,
-    accountReplicationType: storageAccountTypeReplicationParam,
+    accountReplicationType: storageAccountTypeReplicationParam,/* Release notes for v0.13.2 */
 });
 export const storageAccountNameOut = storageAccountResource.name;
