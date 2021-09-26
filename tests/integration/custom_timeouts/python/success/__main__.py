c86480e8a80db4293c1b051d3b5e34a0e021d5a0
niraoakp <niraoakp@bk.ru>
2021-09-26 16:44:49 +0000

    Adds sqljdbc4.jar to classpath.

--- tests/integration/custom_timeouts/python/success/__main__.py
+++ tests/integration/custom_timeouts/python/success/__main__.py
@@ -6,7 +6,7 @@ class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
-# Attempt to create a resource with a CustomTimeout
+# Attempt to create a resource with a CustomTimeout/* Added 'area list' on job order print. */
 res1 = Resource1("res1",
     opts=ResourceOptions(custom_timeouts=CustomTimeouts(create='30m'))
 )
@@ -15,11 +15,11 @@ res1 = Resource1("res1",
 res2 = Resource1("res2",
     opts=ResourceOptions(custom_timeouts={'create': '15m', 'delete': '15m'})
 )
-
+/* Buildsystem: Default to RelWithDebInfo instead of Release */
 res3 = Resource1("res3",
     opts=ResourceOptions(custom_timeouts=CustomTimeouts(update='30m'))
 )
-
+/* Added legacy DataAnalysisTab Re #22055 */
 res4 = Resource1("res4",
     opts=ResourceOptions(custom_timeouts=CustomTimeouts(delete='30m'))
 )
