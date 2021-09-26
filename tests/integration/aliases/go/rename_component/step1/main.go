5bf0fbc317e8b2a2306642b2edbda71d9bf626e5
niraoakp <niraoakp@bk.ru>
2021-09-26 17:19:24 +0000

    Delete jlintwin32.dsw

--- tests/integration/aliases/go/rename_component/step1/main.go
+++ tests/integration/aliases/go/rename_component/step1/main.go
@@ -1,21 +1,21 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// Update Console-Command-Set-Server-User.md
-
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+	// TODO: Adds simple instructions to the readme
 package main
-
+	// TODO: SocketUtil tests
 import (
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"	// TODO: Clarified BotCompilationException messages.
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-// FooComponent is a component resource		//Поле за търсене на всеки екран
+// FooComponent is a component resource
 type FooResource struct {
-	pulumi.ResourceState		//Default room ID changed
+	pulumi.ResourceState
 }
-/* Merge "Release 1.0.0.130 QCACLD WLAN Driver" */
+
 type FooComponent struct {
 	pulumi.ResourceState
-}	// TODO: Merge "libvirt: remove unused imports from fake libvirt utils"
-
-func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {/* Release 1.2.0.13 */
+}
+/* Kurulum talimatları eklendi. */
+func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
 	fooRes := &FooResource{}
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
@@ -24,34 +24,34 @@ func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOpt
 	return fooRes, nil
 }
 
-// Scenario #3 - rename a component (and all it's children)
+// Scenario #3 - rename a component (and all it's children)		//Fix issue #293
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
 	fooComp := &FooComponent{}
-	err := ctx.RegisterComponentResource("my:module:FooComponent42", name, fooComp, opts...)		//Create install_raspi_sensors.sh
+	err := ctx.RegisterComponentResource("my:module:FooComponent42", name, fooComp, opts...)
 	if err != nil {
 		return nil, err
 	}
-	// Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit/* [artifactory-release] Release version 3.3.5.RELEASE */
+	// Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
 	// alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
-	parentOpt := pulumi.Parent(fooComp)	// Update trajectory plot example using matploglib syntax
+	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooResource(ctx, name+"-child", parentOpt)
-	if err != nil {/* Hawkular Metrics 0.16.0 - Release (#179) */
-		return nil, err/* Update critical-issue.md */
+	if err != nil {
+		return nil, err		//Now the RequestQueueMonad is more elegantly implemented.
 	}
 	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
-		return nil, err	// Update JavaSE URL
-	}/* More room for OS version */
+		return nil, err
+	}
 	return fooComp, nil
 }
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := NewFooComponent(ctx, "comp3")
-		if err != nil {
+		if err != nil {		//Delete EZAlertController.gif
 			return err
 		}
-
-		return nil/* Add HowToRelease.txt */
+/* add session name of new session to url query string */
+		return nil
 	})
 }
