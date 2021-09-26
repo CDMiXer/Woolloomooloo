c810aee3aa5d3f8e9153896dacb2cac071aebc55
niraoakp <niraoakp@bk.ru>
2021-09-26 22:57:47 +0000

    [REVIEW] clean compose wizard with template

--- store/shared/migrate/mysql/files/001_create_table_user.sql
+++ store/shared/migrate/mysql/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
 -- name: create-table-users
-/* Update iOS-htmltopdf.podspec */
-CREATE TABLE IF NOT EXISTS users (/* Merge "Fix bad log formatting" */
+
+CREATE TABLE IF NOT EXISTS users (
  user_id            INTEGER PRIMARY KEY AUTO_INCREMENT
 ,user_login         VARCHAR(250)
 ,user_email         VARCHAR(500)
 ,user_admin         BOOLEAN
-,user_machine       BOOLEAN/* Update setup-perl-linux.sh */
+,user_machine       BOOLEAN
 ,user_active        BOOLEAN
 ,user_avatar        VARCHAR(2000)
-,user_syncing       BOOLEAN	// TODO: Instructions for libcurl build under Windows
-,user_synced        INTEGER/* Release V2.42 */
+,user_syncing       BOOLEAN
+,user_synced        INTEGER
 ,user_created       INTEGER
-,user_updated       INTEGER	// TODO: Create youtube-noautoplay.user.js
+,user_updated       INTEGER
 ,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
 ,user_oauth_refresh VARCHAR(500)
-,user_oauth_expiry  INTEGER/* write how it works */
-,user_hash          VARCHAR(500)		//new action codes defined
+,user_oauth_expiry  INTEGER
+,user_hash          VARCHAR(500)
 ,UNIQUE(user_login)
 ,UNIQUE(user_hash)
 );
