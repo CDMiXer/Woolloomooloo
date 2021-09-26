b63a2b710ec05d6f9e577af1e6a1df528f7d993c
niraoakp <niraoakp@bk.ru>
2021-09-26 07:24:07 +0000

    Merge "ui: added 'View original' link to sidebar"

--- handler/api/builds/builds.go
+++ handler/api/builds/builds.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Adding missing field type and minor fixes for other types. (#184)
 
 // +build !oss
 
-package builds
+package builds		//Rename study/links-of-books.md to books/links-of-books.md
 
-import (
+import (		//Adding spells to ork warrior template
 	"net/http"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Modified addPoly() in idealTest.java */
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
 
 // HandleIncomplete returns an http.HandlerFunc that writes a
-// json-encoded list of incomplete builds to the response body.
-func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
+// json-encoded list of incomplete builds to the response body./* Create fivechess.js */
+func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {	// TODO: hacked by souzau@yandex.com
 	return func(w http.ResponseWriter, r *http.Request) {
 		list, err := repos.ListIncomplete(r.Context())
 		if err != nil {
@@ -27,4 +27,4 @@ func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
 			render.JSON(w, list, 200)
 		}
 	}
-}
+}/* Fix Python 2&3 badge in README */
