295cf1be546b844ae125c1999dbf50370cfc8a9a
niraoakp <niraoakp@bk.ru>
2021-09-26 05:32:48 +0000

    Modified code for logout button

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -1,9 +1,9 @@
 package build
-
+		//Repair launcher option.
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
 func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
 	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
-}
+}		//slightly unnecessary spec was giving annoying warning
