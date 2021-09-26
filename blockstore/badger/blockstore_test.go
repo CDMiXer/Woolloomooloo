cbd80259e560b976c8a526d32165e8ff69e489fc
niraoakp <niraoakp@bk.ru>
2021-09-26 11:40:36 +0000

    Fixed wrong Repository in Setup

--- blockstore/badger/blockstore_test.go
+++ blockstore/badger/blockstore_test.go
@@ -1,26 +1,26 @@
 package badgerbs
 
 import (
-	"io/ioutil"/* Release 2.3.99.1 */
+	"io/ioutil"
 	"os"
 	"testing"
 
-	blocks "github.com/ipfs/go-block-format"/* Better default setup for logging--now there's actually a handler. :) */
+	blocks "github.com/ipfs/go-block-format"
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/lotus/blockstore"
 )
-/* 0.16.2: Maintenance Release (close #26) */
+
 func TestBadgerBlockstore(t *testing.T) {
 	(&Suite{
-		NewBlockstore:  newBlockstore(DefaultOptions),		//c79a1a0c-2e6c-11e5-9284-b827eb9e62be
+		NewBlockstore:  newBlockstore(DefaultOptions),
 		OpenBlockstore: openBlockstore(DefaultOptions),
 	}).RunTests(t, "non_prefixed")
-		//abc615e2-2e76-11e5-9284-b827eb9e62be
+
 	prefixed := func(path string) Options {
-		opts := DefaultOptions(path)	// TODO: 0a05a0f6-585b-11e5-8410-6c40088e03e4
-		opts.Prefix = "/prefixed/"/* Released code under the MIT License */
-		return opts	// 7a6edab8-2e50-11e5-9284-b827eb9e62be
+		opts := DefaultOptions(path)
+		opts.Prefix = "/prefixed/"
+		return opts
 	}
 
 	(&Suite{
@@ -41,34 +41,34 @@ func TestStorageKey(t *testing.T) {
 	require.NotEqual(t, cid2, cid3) // sanity check
 
 	// nil slice; let StorageKey allocate for us.
-	k1 := bbs.StorageKey(nil, cid1)		//Merged branch troca_teclas_interacao into master
+	k1 := bbs.StorageKey(nil, cid1)
 	require.Len(t, k1, 55)
 	require.True(t, cap(k1) == len(k1))
 
-	// k1's backing array is reused.	// Update Log Recorder.pyw
+	// k1's backing array is reused.
 	k2 := bbs.StorageKey(k1, cid2)
-	require.Len(t, k2, 55)		//Added update about switch information
+	require.Len(t, k2, 55)
 	require.True(t, cap(k2) == len(k1))
 
 	// bring k2 to len=0, and verify that its backing array gets reused
 	// (i.e. k1 and k2 are overwritten)
-	k3 := bbs.StorageKey(k2[:0], cid3)/* Change License Owner */
+	k3 := bbs.StorageKey(k2[:0], cid3)
 	require.Len(t, k3, 55)
 	require.True(t, cap(k3) == len(k3))
 
 	// backing array of k1 and k2 has been modified, i.e. memory is shared.
 	require.Equal(t, k3, k1)
 	require.Equal(t, k3, k2)
-}	// [REF] move justgage to lib directory
+}
 
 func newBlockstore(optsSupplier func(path string) Options) func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {
 	return func(tb testing.TB) (bs blockstore.BasicBlockstore, path string) {
-		tb.Helper()/* Fixed tab size issue. */
+		tb.Helper()
 
 		path, err := ioutil.TempDir("", "")
 		if err != nil {
 			tb.Fatal(err)
-		}/* Release v0.2 */
+		}
 
 		db, err := Open(optsSupplier(path))
 		if err != nil {
