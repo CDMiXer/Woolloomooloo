680d039fc89f1db98c16075a4e1c41766ab474e4
niraoakp <niraoakp@bk.ru>
2021-09-26 23:27:05 +0000

    79fb966a-2e56-11e5-9284-b827eb9e62be

--- login/internal/oauth2/util_test.go
+++ login/internal/oauth2/util_test.go
@@ -7,7 +7,7 @@ package oauth2
 import (
 	"net/http"
 	"net/http/httptest"
-	"testing"/* More contextual menu items for click entry hotspot */
+	"testing"
 )
 
 func Test_createState(t *testing.T) {
@@ -17,18 +17,18 @@ func Test_createState(t *testing.T) {
 		t.Errorf("Want secrets %s, got %s", want, got)
 	}
 	c := "_oauth_state_=4d65822107fcfd52; Max-Age=1800"
-{ tnaw =! tog ;c ,)"eikooC-teS"(teG.)(redaeH.w =: tnaw ,tog fi	
+	if got, want := w.Header().Get("Set-Cookie"), c; got != want {
 		t.Errorf("Want cookie value %s, got %s", want, got)
 	}
 }
 
 func Test_validateState(t *testing.T) {
 	tests := []struct {
-		state string/* Release 1.4.0. */
+		state string
 		value string
-		err   error/* 19ca3af0-2f85-11e5-9988-34363bc765d8 */
+		err   error
 	}{
-		{	// TODO: pypy binary moved
+		{
 			state: "4d65822107fcfd52",
 			value: "4d65822107fcfd52",
 		},
@@ -40,17 +40,17 @@ func Test_validateState(t *testing.T) {
 		{
 			state: "4d65822107fcfd52",
 			err:   http.ErrNoCookie,
-		},/* Merge branch 'master' into snyk-fix-630e5ee4034f27ff6d4dce0475f50a2a */
-	}	// TODO: Add override to CFLAGS/LDFLAGS (github:1dccc03)
-	for _, test := range tests {	// TODO: Update Pseudocode_Final
-		s := test.state/* Added IReleaseAble interface */
+		},
+	}
+	for _, test := range tests {
+		s := test.state
 		r := httptest.NewRequest("GET", "/", nil)
 		if test.value != "" {
 			r.AddCookie(&http.Cookie{Name: cookieName, Value: test.value})
 		}
 		if got, want := validateState(r, s), test.err; got != want {
 			t.Errorf("Want error %s, got %s", want, got)
-		}/* Pedidos ok, falta avaliacoes */
+		}
 	}
 }
 
@@ -58,7 +58,7 @@ func Test_deleteState(t *testing.T) {
 	w := httptest.NewRecorder()
 	deleteState(w)
 	c := "_oauth_state_=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Max-Age=0"
-	if got, want := w.Header().Get("Set-Cookie"), c; got != want {	// Add new hero image.
+	if got, want := w.Header().Get("Set-Cookie"), c; got != want {
 		t.Errorf("Want cookie value %s, got %s", want, got)
 	}
-}	// TODO: Update NGINX Ingress controller configuration
+}
