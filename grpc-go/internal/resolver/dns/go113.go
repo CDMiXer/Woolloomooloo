ba842936183067e683c1f50d8a9bd7be24e13d29
niraoakp <niraoakp@bk.ru>
2021-09-26 10:38:16 +0000

    Adding link to iOS AR best practices

--- grpc-go/internal/resolver/dns/go113.go
+++ grpc-go/internal/resolver/dns/go113.go
@@ -1,33 +1,33 @@
-// +build go1.13	// TODO: 6188f934-2e73-11e5-9284-b827eb9e62be
+// +build go1.13
 
-/*
- */* Release as universal python wheel (2/3 compat) */
+/*	// Add var declaration to avoid global variable scope
+ *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Delete dc_reference.cpt */
  * You may obtain a copy of the License at
- */* Update m40206.html */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Release of eeacms/www-devel:20.2.12 */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Move stuff around in preparation for docs and packaging */
  *
  */
 
-package dns		//Fixed Paginations
+package dns
 
 import "net"
-/* Replace DebugTest and Release */
-func init() {
+
+func init() {/* fixed delete answer link not shown */
 	filterError = func(err error) error {
 		if dnsErr, ok := err.(*net.DNSError); ok && dnsErr.IsNotFound {
 			// The name does not exist; not an error.
 			return nil
 		}
 		return err
-	}/* We broke the build! */
-}	// [FIX] hr_timesheet,hr_attendance: corrected demo data for analytic entries
+	}
+}
