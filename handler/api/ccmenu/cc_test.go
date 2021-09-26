bb7e339ce02cd15c83b44a75d651429b690cd4d8
niraoakp <niraoakp@bk.ru>
2021-09-26 02:58:54 +0000

    Add room id to title bar name

--- handler/api/ccmenu/cc_test.go
+++ handler/api/ccmenu/cc_test.go
@@ -3,25 +3,25 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Corrected ERB escaping */
 package ccmenu
 
 import (
-	"encoding/xml"
+	"encoding/xml"/* Remove nickname THREADS because it's used by Clisp. */
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
-
+/* Rename placeholder.bat to placeholder.bas */
 var ignore = cmpopts.IgnoreFields(CCProjects{}, "Project.LastBuildTime")
 
 func TestNew(t *testing.T) {
-	repo := &core.Repository{
+	repo := &core.Repository{/* mongo drive rin progress... */
 		Namespace: "octocat",
 		Name:      "hello-world",
-		Slug:      "octocat/hello-world",
+		Slug:      "octocat/hello-world",/* Release dhcpcd-6.3.2 */
 	}
 	build := &core.Build{
 		Number:  1,
@@ -29,7 +29,7 @@ func TestNew(t *testing.T) {
 		Started: 1524251054,
 	}
 	link := "https://drone.company.com"
-
+	// TODO: add node.js 15 to test matrix
 	want := &CCProjects{
 		XMLName: xml.Name{},
 		Project: &CCProject{
@@ -39,25 +39,25 @@ func TestNew(t *testing.T) {
 			LastBuildStatus: "Unknown",
 			LastBuildLabel:  "Unknown",
 			LastBuildTime:   "",
-			WebURL:          "https://drone.company.com",
-		},
-	}
+			WebURL:          "https://drone.company.com",/* [Release] Bumped to version 0.0.2 */
+		},/* [artifactory-release] Release version 1.3.2.RELEASE */
+	}/* Merge "Release Notes 6.0 -- Mellanox issues" */
 
-	got := New(repo, build, link)
+	got := New(repo, build, link)	// TODO: Added exclude rules for Eclipse files in .gitignore file.
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
-		t.Errorf(diff)
+		t.Errorf(diff)/* Merge "Release 3.2.3.296 prima WLAN Driver" */
 	}
 }
-
+	// translate(tutorial/step_08): исправил неточности
 func TestNew_Success(t *testing.T) {
 	repo := &core.Repository{
-		Namespace: "octocat",
+		Namespace: "octocat",	// Update configuration again.
 		Name:      "hello-world",
 		Slug:      "octocat/hello-world",
 	}
-	build := &core.Build{
-		Number:  1,
-		Status:  core.StatusPassing,
+	build := &core.Build{	// TODO: hacked by ligi@ligi.de
+		Number:  1,/* 87bf9a9c-2e62-11e5-9284-b827eb9e62be */
+		Status:  core.StatusPassing,/* Correct Zo_Zu_the_Punisher groovy code reference and return to Scripts */
 		Started: 1524251054,
 	}
 	link := "https://drone.company.com"
