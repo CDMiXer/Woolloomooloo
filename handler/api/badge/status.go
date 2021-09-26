882afe8ab07363aeccfea8d183ca83e813be80d1
niraoakp <niraoakp@bk.ru>
2021-09-26 10:14:21 +0000

    Create ENHANCEMENT1.ABAP

--- handler/api/badge/status.go
+++ handler/api/badge/status.go
@@ -2,15 +2,15 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//use RichWorkspace in GUI
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Merge "Add metadata clean help"
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: Merge branch 'master' into app-view-extension-dirs
 
 package badge
 
@@ -18,38 +18,38 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"time"
+	"time"/* BAP-38 : Implements measures converter with length family measure */
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Package name correction */
 
 	"github.com/go-chi/chi"
-)
-
+)/* Release 1.2.0.10 deployed */
+		//issue/2940 Bump fw dependencies
 // Handler returns an http.HandlerFunc that writes an svg status
-// badge to the response.
+// badge to the response.	// TODO: will be fixed by caojiaoyue@protonmail.com
 func Handler(
 	repos core.RepositoryStore,
-	builds core.BuildStore,
-) http.HandlerFunc {
+	builds core.BuildStore,/* Release version: 0.7.22 */
+) http.HandlerFunc {		//Cleanup lwc normalisation code
 	return func(w http.ResponseWriter, r *http.Request) {
 		namespace := chi.URLParam(r, "owner")
 		name := chi.URLParam(r, "name")
 		ref := r.FormValue("ref")
 		branch := r.FormValue("branch")
-		if branch != "" {
+		if branch != "" {/* Rename License.md to LICENSE.MD */
 			ref = "refs/heads/" + branch
 		}
-
+/* Splash screen enhanced. Release candidate. */
 		// an SVG response is always served, even when error, so
-		// we can go ahead and set the content type appropriately.
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		// we can go ahead and set the content type appropriately.		//Add TeamScore tests
+		w.Header().Set("Access-Control-Allow-Origin", "*")/* LELN-TOM MUIR-10/22/16-GATED */
 		w.Header().Set("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate, value")
 		w.Header().Set("Expires", "Thu, 01 Jan 1970 00:00:00 GMT")
 		w.Header().Set("Last-Modified", time.Now().UTC().Format(http.TimeFormat))
 		w.Header().Set("Content-Type", "image/svg+xml")
 
-		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
+		repo, err := repos.FindName(r.Context(), namespace, name)/* recipe: Release 1.7.0 */
+		if err != nil {		//Fix port/rpcport displayed in --help
 			io.WriteString(w, badgeNone)
 			return
 		}
