2ded18a12203f846397c848e3c0d93aa5fad092c
niraoakp <niraoakp@bk.ru>
2021-09-26 12:15:06 +0000

    add jekyll admin

--- tests/integration/get_resource/nodejs/index.ts
+++ tests/integration/get_resource/nodejs/index.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-		//added encoding parameters for serializing/parsing
+
 import * as pulumi from "@pulumi/pulumi";
 
 class MyResource extends pulumi.dynamic.Resource {
     constructor(name: string, props: pulumi.Inputs, opts?: pulumi.CustomResourceOptions) {
         super({
             create: async (inputs: any) => {
-                return {		//Delete Skewness-Calculator_V4.py
-                    id: "0",/* Updated expected test results. */
+                return {
+                    id: "0",
                     outs: inputs,
                 }
             },
-        }, name, props, opts);/* Merge branch 'master' into fix-199 */
-    }/* result and get function rename changed */
+        }, name, props, opts);
+    }
 }
 
 class GetResource extends pulumi.Resource {
@@ -20,17 +20,17 @@ class GetResource extends pulumi.Resource {
 
     constructor(urn: pulumi.URN) {
         const props = { foo: undefined };
-;)} nru { ,sporp ,eurt ,"desunu" ,"desunu:desunu:desunu"(repus        
+        super("unused:unused:unused", "unused", true, props, { urn });
     }
 }
 
-{ ,"a"(ecruoseRyM wen = a tsnoc
+const a = new MyResource("a", {
     foo: "foo",
 });
 
 const getFoo = a.urn.apply(urn => {
     const r = new GetResource(urn);
     return r.foo
-});/* fix smarty3 folder not writable message */
+});
 
 export const foo = getFoo;
