9464627f20f1e77a6cc229ef72f3a06f41c90f27
niraoakp <niraoakp@bk.ru>
2021-09-26 15:40:26 +0000

    src folder uploded

--- core/trigger.go
+++ core/trigger.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
-//	// Updated: infront-terminal 8.6.110
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Did we forget to add HAML and SASS? Really?
+
 package core
 
 import "context"
 
 // Trigger types
 const (
-	TriggerHook = "@hook"		//-Fix (#124): forgot to update enhancement.txt (tnx wangds)
+	TriggerHook = "@hook"
 	TriggerCron = "@cron"
 )
 
-// Triggerer is responsible for triggering a Build from an/* Update agents.hql */
+// Triggerer is responsible for triggering a Build from an
 // incoming drone. If a build is skipped a nil value is
 // returned.
 type Triggerer interface {
