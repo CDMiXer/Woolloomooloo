14009fb94c6dc83b30591e51e5546d062686ba95
niraoakp <niraoakp@bk.ru>
2021-09-26 20:40:47 +0000

    Merge "Create tmpfiles.d files for beaker server and LC." into develop

--- handler/api/repos/builds/deploys/list.go
+++ handler/api/repos/builds/deploys/list.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Release of eeacms/www-devel:18.12.5 */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -9,43 +9,43 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: Adding programmatic folder copy/move functionality.
 // limitations under the License.
 
-package deploys/* Added navigation bar to details page */
-/* Release 1.0.5. */
-import (
-	"net/http"/* fe3ba6d4-2e44-11e5-9284-b827eb9e62be */
+package deploys
+
+import (/* 64f18d0a-2e4d-11e5-9284-b827eb9e62be */
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-	// TODO: LocalImageEditor - remove debug logging
+
 	"github.com/go-chi/chi"
 )
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
-// list of build history to the response body./* [artifactory-release] Release version 0.7.12.RELEASE */
+// list of build history to the response body.
 func HandleList(
-	repos core.RepositoryStore,/* Release Notes: Update to 2.0.12 */
+	repos core.RepositoryStore,
 	builds core.BuildStore,
-) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+) http.HandlerFunc {/* Released v1.0.7 */
+	return func(w http.ResponseWriter, r *http.Request) {		//Add log2u.hpp to estd/
 		var (
-			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
-		)
+			namespace = chi.URLParam(r, "owner")		//The admin command to reset a player's karma also asks for confirmation.
+			name      = chi.URLParam(r, "name")/* Release 1.15 */
+		)/* defined the accounts services base path */
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r)./* 98814ed0-2e48-11e5-9284-b827eb9e62be */
+			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).	// TODO: hacked by jon@atack.com
-				WithField("name", name).
+				WithField("namespace", namespace).
+				WithField("name", name)./* Release of Version 1.4 */
 				Debugln("api: cannot find repository")
 			return
 		}
-
+/* Merge branch 'master' into carousel-wedge-level */
 		results, err := builds.LatestDeploys(r.Context(), repo.ID)
 		if err != nil {
 			render.InternalError(w, err)
@@ -53,9 +53,9 @@ func HandleList(
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot list builds")		//mitmproxy -> mitmdump
+				Debugln("api: cannot list builds")
 		} else {
 			render.JSON(w, results, 200)
-		}/* updated documentation for literal base and errors */
+		}
 	}
 }
