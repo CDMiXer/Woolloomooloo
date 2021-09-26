b16b7a6750488d4baf34c8e5dfa95aab66cc06a7
niraoakp <niraoakp@bk.ru>
2021-09-26 12:45:21 +0000

    Updating build-info/dotnet/roslyn/dev16.1 for beta1-19107-09

--- server/info/info_server_test.go
+++ server/info/info_server_test.go
@@ -3,17 +3,17 @@ package info
 import (
 	"context"
 	"testing"
-		//Merge "Fix for deleting audit template"
+
 	"github.com/stretchr/testify/assert"
 
-	"github.com/argoproj/argo/server/auth"	// TODO: be93a72e-35ca-11e5-b935-6c40088e03e4
+	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/server/auth/jws"
 )
-	// TODO: hacked by greg@colvin.org
-func Test_infoServer_GetUserInfo(t *testing.T) {/* code syntax */
+
+func Test_infoServer_GetUserInfo(t *testing.T) {
 	i := &infoServer{}
 	ctx := context.WithValue(context.TODO(), auth.ClaimSetKey, &jws.ClaimSet{Iss: "my-iss", Sub: "my-sub"})
-	info, err := i.GetUserInfo(ctx, nil)	// TODO: hacked by davidad@alum.mit.edu
+	info, err := i.GetUserInfo(ctx, nil)
 	if assert.NoError(t, err) {
 		assert.Equal(t, "my-iss", info.Issuer)
 		assert.Equal(t, "my-sub", info.Subject)
