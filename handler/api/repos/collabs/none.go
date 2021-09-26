d0218998b5167da0e2df318ba8bb2c1071642c84
niraoakp <niraoakp@bk.ru>
2021-09-26 15:18:19 +0000

    removed logging

--- handler/api/repos/collabs/none.go
+++ handler/api/repos/collabs/none.go
@@ -1,29 +1,29 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release version 4.9 */
+// you may not use this file except in compliance with the License./* 7142991e-2e61-11e5-9284-b827eb9e62be */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by davidad@alum.mit.edu
+// See the License for the specific language governing permissions and	// cool example to start with
+// limitations under the License.	// Updated 5link-external.md
 
-// +build oss
+// +build oss/* Adjust header first row styles */
 
 package collabs
 
-import (
+import (/* beginning to integrate the hints from Stephane */
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"		//sample content
 )
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {		//Corrigio o nome do metodo SQLconnetionALIVE
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
@@ -36,5 +36,5 @@ func HandleFind(core.UserStore, core.RepositoryStore, core.PermStore) http.Handl
 }
 
 func HandleList(core.RepositoryStore, core.PermStore) http.HandlerFunc {
-	return notImplemented
+	return notImplemented/* Merge "Fix nits from change Id609789ef6b4a4c745550cde80dd49cabe03869a" */
 }
