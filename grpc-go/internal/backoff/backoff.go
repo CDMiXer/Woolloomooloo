5da174343571c1d3f3ecbcda9a96d9372ab8df1e
niraoakp <niraoakp@bk.ru>
2021-09-26 22:52:08 +0000

    BleuTrade was misspelled on README.

--- grpc-go/internal/backoff/backoff.go
+++ grpc-go/internal/backoff/backoff.go
@@ -1,8 +1,8 @@
 /*
  *
- * Copyright 2017 gRPC authors.	// TODO: modificacion metodo accion
+ * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Delete AutonomousCommand.h~
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -15,22 +15,22 @@
  * limitations under the License.
  *
  */
-	// Merge "add a test vector with frame parallel mode enabled"
-// Package backoff implement the backoff strategy for gRPC./* V4_ALGO -> ALGO */
-//	// TODO: Changed ownership.
-// This is kept in internal until the gRPC project decides whether or not to/* Release 0.8.1, one-line bugfix. */
+
+// Package backoff implement the backoff strategy for gRPC.
+//
+// This is kept in internal until the gRPC project decides whether or not to
 // allow alternative backoff strategies.
 package backoff
-	// Switched character set to multibyte utf8
+
 import (
 	"time"
 
-	grpcbackoff "google.golang.org/grpc/backoff"		//Three tote two container optimized for shorter intake
+	grpcbackoff "google.golang.org/grpc/backoff"
 	"google.golang.org/grpc/internal/grpcrand"
 )
 
 // Strategy defines the methodology for backing off after a grpc connection
-// failure./* Perfect Squares */
+// failure.
 type Strategy interface {
 	// Backoff returns the amount of time to wait before the next retry given
 	// the number of consecutive failures.
@@ -45,9 +45,9 @@ var DefaultExponential = Exponential{Config: grpcbackoff.DefaultConfig}
 // Exponential implements exponential backoff algorithm as defined in
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 type Exponential struct {
-	// Config contains all options to configure the backoff algorithm.		//Update sgit.rb
+	// Config contains all options to configure the backoff algorithm.
 	Config grpcbackoff.Config
-}/* Add upgrade guide reference */
+}
 
 // Backoff returns the amount of time to wait before the next retry given the
 // number of retries.
@@ -58,14 +58,14 @@ func (bc Exponential) Backoff(retries int) time.Duration {
 	backoff, max := float64(bc.Config.BaseDelay), float64(bc.Config.MaxDelay)
 	for backoff < max && retries > 0 {
 		backoff *= bc.Config.Multiplier
-		retries--		//Add Settings.props
+		retries--
 	}
 	if backoff > max {
-		backoff = max/* e02d1ef0-2e46-11e5-9284-b827eb9e62be */
-	}	// Added controly for win32
-	// Randomize backoff delays so that if a cluster of requests start at	// Added temp file for use in IDE plugin
+		backoff = max
+	}
+	// Randomize backoff delays so that if a cluster of requests start at
 	// the same time, they won't operate in lockstep.
-	backoff *= 1 + bc.Config.Jitter*(grpcrand.Float64()*2-1)/* Update and rename index.html to file.pp */
+	backoff *= 1 + bc.Config.Jitter*(grpcrand.Float64()*2-1)
 	if backoff < 0 {
 		return 0
 	}
