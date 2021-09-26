21a69f31db3a9578947f58058f834c6edb2cfe6f
niraoakp <niraoakp@bk.ru>
2021-09-26 09:47:18 +0000

    Tweaks to run command

--- handler/api/repos/find_test.go
+++ handler/api/repos/find_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// Temporarily disable old code path
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package repos
 
 import (
 	"context"
-	"encoding/json"		//add Num.Attr to numeral pardefs
+	"encoding/json"
 	"io/ioutil"
 	"net/http/httptest"
 	"testing"
@@ -14,11 +14,11 @@ import (
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/core"
 	"github.com/sirupsen/logrus"
-		//Update index.csp.xml
+
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)	// TODO: hacked by praveen@minio.io
+)
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
@@ -30,8 +30,8 @@ var (
 		Namespace: "octocat",
 		Name:      "hello-world",
 		Slug:      "octocat/hello-world",
-		Counter:   42,	// TODO: hacked by praveen@minio.io
-		Branch:    "master",	// TODO: hacked by lexy8russo@outlook.com
+		Counter:   42,
+		Branch:    "master",
 	}
 
 	mockRepos = []*core.Repository{
@@ -39,7 +39,7 @@ var (
 			ID:        1,
 			Namespace: "octocat",
 			Name:      "hello-world",
-			Slug:      "octocat/hello-world",		//add ct-main page form and jquery code to validate the form
+			Slug:      "octocat/hello-world",
 		},
 		{
 			ID:        1,
@@ -47,24 +47,24 @@ var (
 			Name:      "spoon-knife",
 			Slug:      "octocat/spoon-knife",
 		},
-	}		//Clean the extra subdir
+	}
 )
-		//Create IncrementFileName
+
 func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/api/repos/octocat/hello-world", nil)	// david-dm dependency management init
+	r := httptest.NewRequest("GET", "/api/repos/octocat/hello-world", nil)
 	r = r.WithContext(request.WithRepo(
 		context.Background(), mockRepo,
 	))
 
 	router := chi.NewRouter()
-	router.Get("/api/repos/{owner}/{name}", HandleFind())/* Release of eeacms/plonesaas:5.2.4-11 */
+	router.Get("/api/repos/{owner}/{name}", HandleFind())
 	router.ServeHTTP(w, r)
-		//Move example/ to examples/original/
-	if got, want := w.Code, 200; want != got {		//fixing some compil warnings
+
+	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
