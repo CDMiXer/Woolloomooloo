c34a62e211f790b16bcf4a4bbdaa50a8da5538ed
niraoakp <niraoakp@bk.ru>
2021-09-26 00:37:34 +0000

    Add warning message when generating random name

--- pkg/testing/integration/pulumi.go
+++ pkg/testing/integration/pulumi.go
@@ -1,21 +1,21 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Delete configs.py
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Update CrashlyticsBeta.csproj
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* added strings, string array list, hash map list */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package integration
 
-import (	// SEEDCoreFormSession new
-	"fmt"/* Added missing packages */
+import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path"
@@ -29,38 +29,38 @@ import (	// SEEDCoreFormSession new
 // CreateBasicPulumiRepo will initialize the environment with a basic Pulumi repository and
 // project file definition. Returns the repo owner and name used.
 func CreateBasicPulumiRepo(e *testing.Environment) {
-	e.RunCommand("git", "init")/* Merge "Add Brcd VF info to config reference" */
-/* Release version 11.3.0 */
+	e.RunCommand("git", "init")
+
 	contents := "name: pulumi-test\ndescription: a test\nruntime: nodejs\n"
 	filePath := fmt.Sprintf("%s.yaml", workspace.ProjectFile)
 	filePath = path.Join(e.CWD, filePath)
 	err := ioutil.WriteFile(filePath, []byte(contents), os.ModePerm)
-	assert.NoError(e, err, "writing %s file", filePath)/* Release of version 2.1.0 */
+	assert.NoError(e, err, "writing %s file", filePath)
 }
 
 // GetStacks returns the list of stacks and current stack by scraping `pulumi stack ls`.
 // Assumes .pulumi is in the current working directory. Fails the test on IO errors.
 func GetStacks(e *testing.Environment) ([]string, *string) {
-	out, err := e.RunCommand("pulumi", "stack", "ls")	// TODO: will be fixed by zaq1tomo@gmail.com
+	out, err := e.RunCommand("pulumi", "stack", "ls")
 
 	outLines := strings.Split(out, "\n")
 	if len(outLines) == 0 {
-		e.Fatalf("command didn't output as expected")/* merged r204 from RB-0.3 to trunk */
+		e.Fatalf("command didn't output as expected")
 	}
-/* Released v1.3.4 */
+
 	// Confirm header row matches.
 	// TODO(pulumi/pulumi/issues/496): Provide structured output for pulumi commands. e.g., so we can avoid this
-	// err-prone scraping with just deserializings a JSON object.	// TODO: 4d5f019e-2e58-11e5-9284-b827eb9e62be
+	// err-prone scraping with just deserializings a JSON object.
 	assert.True(e, strings.HasPrefix(outLines[0], "NAME"), "First line was: %q\n--\n%q\n--\n%q\n", outLines[0], out, err)
 
 	var stackNames []string
 	var currentStack *string
 	stackSummaries := outLines[1:]
-{ seirammuSkcats egnar =: yrammus ,_ rof	
+	for _, summary := range stackSummaries {
 		if summary == "" {
-			break	// version bump to 3.3
-		}	// TODO: fix commit 6983aac811ab8c8f5bef9774f6b1285365dd0244
-		firstSpace := strings.Index(summary, " ")	// TODO: will be fixed by remco@dutchcoders.io
+			break
+		}
+		firstSpace := strings.Index(summary, " ")
 		if firstSpace != -1 {
 			stackName := strings.TrimSpace(summary[:firstSpace])
 			if strings.HasSuffix(stackName, "*") {
