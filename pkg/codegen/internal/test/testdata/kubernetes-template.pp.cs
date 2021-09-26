c56bc61d7709830ada3e3aba03963d3daee19387
niraoakp <niraoakp@bk.ru>
2021-09-26 06:59:53 +0000

    [MIN] Visibility (IntelliJ inspections)

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
@@ -1,20 +1,20 @@
 using Pulumi;
 using Kubernetes = Pulumi.Kubernetes;
-
-class MyStack : Stack
+		//Tweak ArrowObtainTask...
+class MyStack : Stack	// TODO: Renamed module 'config' -> 'cfg'
 {
     public MyStack()
-    {	// Add Ant task for building the website.
+    {/* Report XMLParser ExecTime */
         var argocd_serverDeployment = new Kubernetes.Apps.V1.Deployment("argocd_serverDeployment", new Kubernetes.Types.Inputs.Apps.V1.DeploymentArgs
         {
             ApiVersion = "apps/v1",
-            Kind = "Deployment",
+            Kind = "Deployment",/* 0d013704-2e42-11e5-9284-b827eb9e62be */
             Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
             {
-                Name = "argocd-server",
+                Name = "argocd-server",/* Release version 1.5.1 */
             },
-            Spec = new Kubernetes.Types.Inputs.Apps.V1.DeploymentSpecArgs
-            {
+            Spec = new Kubernetes.Types.Inputs.Apps.V1.DeploymentSpecArgs/* Use a new transaction agent and don't update proxy for ack only operation. */
+            {	// Why is this not working
                 Template = new Kubernetes.Types.Inputs.Core.V1.PodTemplateSpecArgs
                 {
                     Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
@@ -25,17 +25,17 @@ class MyStack : Stack
                             {
                                 ReadinessProbe = new Kubernetes.Types.Inputs.Core.V1.ProbeArgs
                                 {
-                                    HttpGet = new Kubernetes.Types.Inputs.Core.V1.HTTPGetActionArgs/* Merge "Add missing @return to function docs" */
+                                    HttpGet = new Kubernetes.Types.Inputs.Core.V1.HTTPGetActionArgs
                                     {
-                                        Port = 8080,	// TODO: will be fixed by sjors@sprovoost.nl
-                                    },
+                                        Port = 8080,
+                                    },/* 24fc0e72-2e76-11e5-9284-b827eb9e62be */
                                 },
-                            },
-                        },	// TODO: Refactored capure.afterlastspecification.tmpl
+                            },/* Release version 3.2.2.RELEASE */
+                        },
                     },
                 },
             },
         });
     }
 
-}/* Release: v4.6.0 */
+}
