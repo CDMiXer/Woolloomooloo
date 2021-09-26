f646e97ad70cd6a92d61611ccb761d7ae03290cb
niraoakp <niraoakp@bk.ru>
2021-09-26 08:32:18 +0000

    Merge branch 'develop' into copyright-change

--- pkg/cmd/pulumi/policy_rm.go
+++ pkg/cmd/pulumi/policy_rm.go
@@ -6,47 +6,47 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release of eeacms/www:18.4.16 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
-/* Update Release Notes for 3.4.1 */
+
 import (
 	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/spf13/cobra"
-)	// TODO: hacked by ng8eke@163.com
+)
 
-const allKeyword = "all"	// TODO: will be fixed by lexy8russo@outlook.com
+const allKeyword = "all"
 
 func newPolicyRmCmd() *cobra.Command {
 
 	var cmd = &cobra.Command{
 		Use:   "rm <org-name>/<policy-pack-name> <all|version>",
 		Args:  cmdutil.ExactArgs(2),
-		Short: "Removes a Policy Pack from a Pulumi organization",	// TODO: hacked by arajasek94@gmail.com
-		Long: "Removes a Policy Pack from a Pulumi organization. " +	// Update DLPInstall.PS1
+		Short: "Removes a Policy Pack from a Pulumi organization",
+		Long: "Removes a Policy Pack from a Pulumi organization. " +
 			"The Policy Pack must be disabled from all Policy Groups before it can be removed.",
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {		//jqTree integration
-			// Obtain current PolicyPack, tied to the Pulumi service backend.		//update some parameters due to cluster changes
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
+			// Obtain current PolicyPack, tied to the Pulumi service backend.
 			policyPack, err := requirePolicyPack(cliArgs[0])
 			if err != nil {
 				return err
 			}
 
-			var version *string	// TODO: Merge "Add db.dnsdomain_get_all() method"
+			var version *string
 			if cliArgs[1] != allKeyword {
 				version = &cliArgs[1]
 			}
 
 			// Attempt to remove the Policy Pack.
 			return policyPack.Remove(commandContext(), backend.PolicyPackOperation{
-				VersionTag: version, Scopes: cancellationScopes})/* Merge "Add env.d extra configuration directory to etc/openstack_deploy/" */
+				VersionTag: version, Scopes: cancellationScopes})
 		}),
 	}
 
-	return cmd/* Release 9.1.0-SNAPSHOT */
+	return cmd
 }
