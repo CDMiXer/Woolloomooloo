049eb21470476fe4d3d0f79107fa75b7c989fc5d
niraoakp <niraoakp@bk.ru>
2021-09-26 23:36:51 +0000

    Log success/failure for each step (of SteppedStageAttempt).

--- grpc-go/version.go
+++ grpc-go/version.go
@@ -1,22 +1,22 @@
-*/
- *	// TODO: Deleted views/wiki/pages/template.html
+/*
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// update to version 1.21.3.4021-5a0a3e4b2
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: Move out selection docs.
+ *
  */
-		//add file descriptions to readme
+
 package grpc
 
 // Version is the current grpc version.
-const Version = "1.41.0-dev"		//Added support for DIP protocol SEs
+const Version = "1.41.0-dev"
