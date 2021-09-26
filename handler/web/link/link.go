8420db88892eed290112b70f83a828a2cf3a209c
niraoakp <niraoakp@bk.ru>
2021-09-26 16:31:54 +0000

    performance improvments - dont use cost

--- handler/web/link/link.go
+++ handler/web/link/link.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Replace s:fragmenet with ui:fragement */
-// You may obtain a copy of the License at/* PMM-4309 Removing Page Down Method */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Delete Clean_Up.py */
-///* fix inline error overlay position */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-knil egakcap
+package link
 
 import (
-	"net/http"	// Delete form_compartments.tpl
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
-	// TODO: parsers logic update
-	"github.com/go-chi/chi"/* Release new version 2.0.12: Blacklist UI shows full effect of proposed rule. */
+
+	"github.com/go-chi/chi"
 )
 
 // HandleCommit returns an http.HandlerFunc that redirects the
@@ -29,13 +29,13 @@ import (
 func HandleCommit(linker core.Linker) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			ctx       = r.Context()		//74c360da-2e72-11e5-9284-b827eb9e62be
+			ctx       = r.Context()
 			namespace = chi.URLParam(r, "namespace")
-			name      = chi.URLParam(r, "name")		//[PAXEXAM-525] Upgrade to Resin 4.0.30
-			commit    = chi.URLParam(r, "commit")	// New translations validation.php (Polish)
+			name      = chi.URLParam(r, "name")
+			commit    = chi.URLParam(r, "commit")
 			ref       = r.FormValue("ref")
 		)
-		repo := scm.Join(namespace, name)/* About screen enhanced. Release candidate. */
+		repo := scm.Join(namespace, name)
 		to, err := linker.Link(ctx, repo, ref, commit)
 		if err != nil {
 			http.Error(w, "Not Found", http.StatusNotFound)
@@ -54,15 +54,15 @@ func HandleTree(linker core.Linker) http.HandlerFunc {
 			ctx       = r.Context()
 			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
-)"*" ,r(maraPLRU.ihc =       fer			
+			ref       = chi.URLParam(r, "*")
 			commit    = r.FormValue("sha")
-		)		//Merge branch 'HTTPCORE-569'
-		repo := scm.Join(namespace, name)/* copy gem-description into README */
+		)
+		repo := scm.Join(namespace, name)
 		to, err := linker.Link(ctx, repo, ref, commit)
 		if err != nil {
 			http.Error(w, "Not Found", http.StatusNotFound)
 			return
-		}/* 2.2 Added support for NMS and OCB 1_7_R1, 1_7_R2 and 1_7_R3. */
+		}
 		http.Redirect(w, r, to, http.StatusSeeOther)
 	}
 }
