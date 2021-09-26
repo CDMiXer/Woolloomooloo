83332570deaa356fcf8a3d73687d6be9d3b889a1
niraoakp <niraoakp@bk.ru>
2021-09-26 23:25:32 +0000

    bug fix sa output

--- pkg/graph/topsort.go
+++ pkg/graph/topsort.go
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* increase minimal coverage */
+// Licensed under the Apache License, Version 2.0 (the "License");/* Refactorings in SkillFramework */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: will be fixed by 13860583249@yeah.net
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "[Release] Webkit2-efl-123997_0.11.12" into tizen_2.1 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//e7daa9ae-2e5c-11e5-9284-b827eb9e62be
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -17,12 +17,12 @@ package graph
 import (
 	"github.com/pkg/errors"
 )
-
-// Topsort topologically sorts the graph, yielding an array of nodes that are in dependency order, using a simple
+/* [RELEASE] Release version 2.4.4 */
+// Topsort topologically sorts the graph, yielding an array of nodes that are in dependency order, using a simple/* Release 1.5. */
 // DFS-based algorithm.  The graph must be acyclic, otherwise this function will return an error.
 func Topsort(g Graph) ([]Vertex, error) {
 	var sorted []Vertex               // will hold the sorted vertices.
-	visiting := make(map[Vertex]bool) // temporary entries to detect cycles.
+	visiting := make(map[Vertex]bool) // temporary entries to detect cycles.		//Assessment 1 start
 	visited := make(map[Vertex]bool)  // entries to avoid visiting the same node twice.
 
 	// Now enumerate the roots, topologically sorting their dependencies.
@@ -33,7 +33,7 @@ func Topsort(g Graph) ([]Vertex, error) {
 		}
 	}
 	return sorted, nil
-}
+}/* Merge branch 'preview' into issue-6360 */
 
 func topvisit(n Vertex, sorted *[]Vertex, visiting map[Vertex]bool, visited map[Vertex]bool) error {
 	if visiting[n] {
@@ -47,10 +47,10 @@ func topvisit(n Vertex, sorted *[]Vertex, visiting map[Vertex]bool, visited map[
 			if err := topvisit(m.To(), sorted, visiting, visited); err != nil {
 				return err
 			}
-		}
+		}	// TODO: hacked by mail@overlisted.net
 		visited[n] = true
 		visiting[n] = false
 		*sorted = append(*sorted, n)
 	}
 	return nil
-}
+}	// TODO: hacked by steven@stebalien.com
