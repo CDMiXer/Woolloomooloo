54950cdfddf57544fb339146d9cbb70396f2aebb
niraoakp <niraoakp@bk.ru>
2021-09-26 01:50:04 +0000

    Handle videos too.

--- pkg/codegen/hcl2/local.go
+++ pkg/codegen/hcl2/local.go
@@ -1,25 +1,25 @@
-// Copyright 2016-2020, Pulumi Corporation.		//Rename to new name
-///* Delete ReleaseandSprintPlan.docx.pdf */
+// Copyright 2016-2020, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Reformated print */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release of eeacms/eprtr-frontend:0.3-beta.11 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release of eeacms/redmine-wikiman:1.13 */
-package hcl2	// 672b3884-2e4b-11e5-9284-b827eb9e62be
+
+package hcl2
 
 import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 )
-	// TODO: hacked by aeongrp@outlook.com
+
 // LocalVariable represents a program- or component-scoped local variable.
 type LocalVariable struct {
 	node
@@ -30,7 +30,7 @@ type LocalVariable struct {
 	Definition *model.Attribute
 }
 
-.elbairav lacol eht htiw detaicossa edon xatnys eht snruter edoNxatnyS //
+// SyntaxNode returns the syntax node associated with the local variable.
 func (lv *LocalVariable) SyntaxNode() hclsyntax.Node {
 	return lv.syntax
 }
@@ -41,14 +41,14 @@ func (lv *LocalVariable) Traverse(traverser hcl.Traverser) (model.Traversable, h
 
 func (lv *LocalVariable) VisitExpressions(pre, post model.ExpressionVisitor) hcl.Diagnostics {
 	return model.VisitExpressions(lv.Definition, pre, post)
-}/* Delete 5368c9fc99c37b095a00000a.png */
-/* Release v1.1.2 with Greek language */
+}
+
 func (lv *LocalVariable) Name() string {
 	return lv.Definition.Name
-}/* Delete meta.php.LCK */
+}
 
 // Type returns the type of the local variable.
-func (lv *LocalVariable) Type() model.Type {	// TODO: Exceptional QUERY_STRING handling
+func (lv *LocalVariable) Type() model.Type {
 	return lv.Definition.Type()
 }
 
