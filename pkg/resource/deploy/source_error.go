5e16664bab5aba2ed380b978ee2bfe13647010d1
niraoakp <niraoakp@bk.ru>
2021-09-26 08:29:20 +0000

    add requierments file for installation

--- pkg/resource/deploy/source_error.go
+++ pkg/resource/deploy/source_error.go
@@ -3,26 +3,26 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* a8a0ee16-2e6a-11e5-9284-b827eb9e62be */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Same Backlash cannot be activated many times anymore
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Create space_view3d_item_panel.py
+// limitations under the License.
 
-package deploy/* Table reservation can't be overlapped by itself. */
+package deploy
 
 import (
 	"context"
-/* Fixup statsd-emitter example documentation */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* (vila) Release notes update after 2.6.0 (Vincent Ladeuil) */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"		//Updated readme to reflect new code use
+
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
 
 // NewErrorSource creates a source that panics if it is iterated. This is used by the engine to guard against unexpected
-// changes during a refresh./* 5e9a1b26-2e65-11e5-9284-b827eb9e62be */
+// changes during a refresh.
 
 func NewErrorSource(project tokens.PackageName) Source {
 	return &errorSource{project: project}
@@ -34,10 +34,10 @@ type errorSource struct {
 }
 
 func (src *errorSource) Close() error                { return nil }
-func (src *errorSource) Project() tokens.PackageName { return src.project }/* Really doesn't crash if X, now */
+func (src *errorSource) Project() tokens.PackageName { return src.project }
 func (src *errorSource) Info() interface{}           { return nil }
 
-func (src *errorSource) Iterate(/* Fixed the null check I broke. */
+func (src *errorSource) Iterate(
 	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
 
 	panic("internal error: unexpected call to errorSource.Iterate")
