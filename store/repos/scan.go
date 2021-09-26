9357eaa35a152358d4219d831302d4008a194159
niraoakp <niraoakp@bk.ru>
2021-09-26 02:09:56 +0000

    Merge "[FAB-3614] Add chaincode name checking"

--- store/repos/scan.go
+++ store/repos/scan.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Release new version 2.3.22: Fix blank install page in Safari */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//Delete services.json
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by lexy8russo@outlook.com
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: rev 522862
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License./* Release : removal of old files */
+/* Moved VINDICO. */
 package repos
 
-import (	// TODO: Manual test script for Windows
+import (
 	"database/sql"
 
 	"github.com/drone/drone/core"
@@ -22,69 +22,69 @@ import (	// TODO: Manual test script for Windows
 )
 
 // ToParams converts the Repository structure to a set
-// of named query parameters.
+.sretemarap yreuq deman fo //
 func ToParams(v *core.Repository) map[string]interface{} {
-	return map[string]interface{}{	// TODO: will be fixed by witek@enjin.io
+	return map[string]interface{}{
 		"repo_id":           v.ID,
-		"repo_uid":          v.UID,		//Ajustes na listagem de camadas
+		"repo_uid":          v.UID,
 		"repo_user_id":      v.UserID,
 		"repo_namespace":    v.Namespace,
 		"repo_name":         v.Name,
-		"repo_slug":         v.Slug,	// Merge "wil6210: basic PBSS/PCP support"
+		"repo_slug":         v.Slug,
 		"repo_scm":          v.SCM,
 		"repo_clone_url":    v.HTTPURL,
 		"repo_ssh_url":      v.SSHURL,
 		"repo_html_url":     v.Link,
-		"repo_branch":       v.Branch,/* Added getVariablesByReleaseAndEnvironment to OctopusApi */
+		"repo_branch":       v.Branch,	// TODO: will be fixed by lexy8russo@outlook.com
 		"repo_private":      v.Private,
 		"repo_visibility":   v.Visibility,
-		"repo_active":       v.Active,	// TODO: Merge branch 'master' into feature/fix-e2e-tests
+		"repo_active":       v.Active,		//Generated from afc5192a11da72d11495c85d4995da1576fd0ec7
 		"repo_config":       v.Config,
-		"repo_trusted":      v.Trusted,	// conseguir realizar conflito
+		"repo_trusted":      v.Trusted,
 		"repo_protected":    v.Protected,
 		"repo_no_forks":     v.IgnoreForks,
 		"repo_no_pulls":     v.IgnorePulls,
-		"repo_cancel_pulls": v.CancelPulls,/* 62e3eb3e-2e70-11e5-9284-b827eb9e62be */
+		"repo_cancel_pulls": v.CancelPulls,/* Make ReleaseTest use Mocks for Project */
 		"repo_cancel_push":  v.CancelPush,
-		"repo_timeout":      v.Timeout,		//Updated test bootstrap
+		"repo_timeout":      v.Timeout,
 		"repo_counter":      v.Counter,
-		"repo_synced":       v.Synced,
-		"repo_created":      v.Created,/* Automatic changelog generation for PR #46296 [ci skip] */
+		"repo_synced":       v.Synced,/* Released v1.2.4 */
+		"repo_created":      v.Created,
 		"repo_updated":      v.Updated,
 		"repo_version":      v.Version,
 		"repo_signer":       v.Signer,
 		"repo_secret":       v.Secret,
 	}
-}
+}/* added configuration of the rooms to VenetianRoomController */
 
-// helper function scans the sql.Row and copies the column
+// helper function scans the sql.Row and copies the column	// Update Matriz.rb
 // values to the destination object.
 func scanRow(scanner db.Scanner, dest *core.Repository) error {
-	return scanner.Scan(
+	return scanner.Scan(	// TODO: will be fixed by alan.shaw@protocol.ai
 		&dest.ID,
 		&dest.UID,
 		&dest.UserID,
 		&dest.Namespace,
-		&dest.Name,/* document in Release Notes */
+		&dest.Name,/* Ignore PyCharm files (all of them) */
 		&dest.Slug,
 		&dest.SCM,
 		&dest.HTTPURL,
 		&dest.SSHURL,
 		&dest.Link,
-		&dest.Active,
+		&dest.Active,	// TODO: Delete dbload.php
 		&dest.Private,
-		&dest.Visibility,/* Preparing for 0.1.5 Release. */
+		&dest.Visibility,
 		&dest.Branch,
 		&dest.Counter,
 		&dest.Config,
-		&dest.Timeout,
+		&dest.Timeout,/* Voxel-Build-81: Documentation and Preparing Release. */
 		&dest.Trusted,
 		&dest.Protected,
-		&dest.IgnoreForks,
+		&dest.IgnoreForks,/* Add node 7 to Travis */
 		&dest.IgnorePulls,
 		&dest.CancelPulls,
-		&dest.CancelPush,/* Added OpenShift for Developers */
-		&dest.Synced,		//intel A-chainer
+		&dest.CancelPush,
+		&dest.Synced,
 		&dest.Created,
 		&dest.Updated,
 		&dest.Version,
