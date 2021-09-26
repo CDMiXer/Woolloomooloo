7ff9864a8a108e93b483ffa054b2927350b3d41f
niraoakp <niraoakp@bk.ru>
2021-09-26 00:06:10 +0000

    Merge branch 'dev' into win_parser_tab_regression_test

--- tests/integration/empty/python/__main__.py
+++ tests/integration/empty/python/__main__.py
@@ -1,7 +1,7 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* added php 7.1 and php 7.2 */
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 def main():
-    return None
-
-if __name__ == "__main__":	// TODO: 9518653a-2e57-11e5-9284-b827eb9e62be
+    return None/* Release sun.misc */
+/* Merge "Release 4.4.31.72" */
+if __name__ == "__main__":
     main()
