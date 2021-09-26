104930fbf4faadd827c0e27d15a3b0ebcbffe41a
niraoakp <niraoakp@bk.ru>
2021-09-26 23:44:19 +0000

    Update from Forestry.io - _drafts/_quartiers/point-e.md

--- tests/containers/dotnet/Program.fs
+++ tests/containers/dotnet/Program.fs
@@ -1,16 +1,16 @@
-﻿module Program		//Update and rename sqlmonitor.conf to asq.conf
+﻿module Program
 
 open System
 open Pulumi.FSharp
 
-let infra () =	// TODO: will be fixed by hugomrdias@gmail.com
-  let config = new Pulumi.Config()/* Merge "Release 3.2.3.407 Prima WLAN Driver" */
+let infra () =
+  let config = new Pulumi.Config()
   let runtime = config.Get("runtime")
   Console.WriteLine("Hello from {0}", runtime)
   
   // Stack outputs
   dict []
-		//module  Users: add general password random
+
 [<EntryPoint>]
-let main _ =/* [artifactory-release] Release version 0.7.15.RELEASE */
-  Deployment.run infra	// TODO: will be fixed by alan.shaw@protocol.ai
+let main _ =
+  Deployment.run infra
