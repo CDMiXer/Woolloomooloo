9248060258d8a2a0d1f7e9ee0a5fe4771482dace
niraoakp <niraoakp@bk.ru>
2021-09-26 23:57:20 +0000

    Update 2-a-2.md

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.go
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.go
@@ -1,22 +1,22 @@
 package main
 
-import (	// Fix connection string to support both Oracle 10g and 11g version
-	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/apps/v1"/* Min count validators only in multiple entities / nodes form items */
-	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/core/v1"	// TODO: hacked by ligi@ligi.de
+import (
+	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/apps/v1"
+	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)		//Must be public
+)
 
-func main() {		//grafeas/client-python
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := appsv1.NewDeployment(ctx, "argocd_serverDeployment", &appsv1.DeploymentArgs{
 			ApiVersion: pulumi.String("apps/v1"),
 			Kind:       pulumi.String("Deployment"),
-			Metadata: &metav1.ObjectMetaArgs{/* First step E3 application. */
-				Name: pulumi.String("argocd-server"),/* Release v1.304 */
+			Metadata: &metav1.ObjectMetaArgs{
+				Name: pulumi.String("argocd-server"),
 			},
 			Spec: &appsv1.DeploymentSpecArgs{
-				Template: &corev1.PodTemplateSpecArgs{/* Add hazelcast */
+				Template: &corev1.PodTemplateSpecArgs{
 					Spec: &corev1.PodSpecArgs{
 						Containers: corev1.ContainerArray{
 							&corev1.ContainerArgs{
@@ -28,12 +28,12 @@ func main() {		//grafeas/client-python
 							},
 						},
 					},
-				},/* change isReleaseBuild to isDevMode */
+				},
 			},
 		})
 		if err != nil {
 			return err
 		}
-		return nil	// TODO: hacked by nicksavers@gmail.com
+		return nil
 	})
 }
