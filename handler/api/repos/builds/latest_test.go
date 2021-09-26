ebde33c25c29a6bdf01f836bd04dc1d7752dbb18
niraoakp <niraoakp@bk.ru>
2021-09-26 13:09:36 +0000

    SO-1352: added missing arguments to createStatus calls.

--- handler/api/repos/builds/latest_test.go
+++ handler/api/repos/builds/latest_test.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* remove 0705, fixes #418 */
+// Use of this source code is governed by the Drone Non-Commercial License/* Release jedipus-2.6.20 */
+// that can be found in the LICENSE file./* Merge branch 'master' into linux-64bit-browser-support */
 
 package builds
 
@@ -10,33 +10,33 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock"		//Added some test cases for ObjectParser.
 	"github.com/drone/drone/handler/api/errors"
-
+/* Released 11.1 */
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-
+		//Enable bintray profile
 func TestLast(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)/* display metadata string instead of the list */
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
 
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().FindRef(gomock.Any(), mockRepo.ID, "refs/heads/master").Return(mockBuild, nil)
 
 	stages := mock.NewMockStageStore(controller)
-	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
+	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)/* 568852cc-2e4c-11e5-9284-b827eb9e62be */
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("owner", "octocat")	// Fixed cursor while deleting
 	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)/* Merge branch 'release/1.4.2' into develop */
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
@@ -61,20 +61,20 @@ func TestLast_RepoNotFound(t *testing.T) {
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)
 
-	c := new(chi.Context)
+	c := new(chi.Context)/* action export cartodb meetpunten added */
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("number", "1")
-
+/* Release update info */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleLast(repos, nil, nil)(w, r)
+	HandleLast(repos, nil, nil)(w, r)		//feat: add cookie consent mechanism to header
 
-	if got, want := w.Code, 404; want != got {
+	if got, want := w.Code, 404; want != got {/* Released version 0.8.3c */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
@@ -83,7 +83,7 @@ func TestLast_RepoNotFound(t *testing.T) {
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}
+}	// Moved HostFactory to own namespaces
 
 func TestLast_BuildNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -96,7 +96,7 @@ func TestLast_BuildNotFound(t *testing.T) {
 	builds.EXPECT().FindRef(gomock.Any(), mockRepo.ID, "refs/heads/master").Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("owner", "octocat")/* 'bzr status -q' now has the same effect as 'bzr status --versioned --short'. */
 	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
