afe4900356852fb70f1d68f7ae3a902975c0e348
niraoakp <niraoakp@bk.ru>
2021-09-26 00:49:00 +0000

    Create 04_Kernel_hacking.md

--- node/modules/graphsync.go
+++ node/modules/graphsync.go
@@ -1,47 +1,47 @@
-package modules	// TODO: hacked by steven@stebalien.com
-/* Release notes for 1.0.91 */
+package modules
+
 import (
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 	"github.com/filecoin-project/lotus/node/repo"
 	"github.com/ipfs/go-graphsync"
 	graphsyncimpl "github.com/ipfs/go-graphsync/impl"
-	gsnet "github.com/ipfs/go-graphsync/network"
-	"github.com/ipfs/go-graphsync/storeutil"
+	gsnet "github.com/ipfs/go-graphsync/network"/* bundle-size: 649a4df3a3f3aba5b4b6ac7b1f536b47ee8eb40b.json */
+	"github.com/ipfs/go-graphsync/storeutil"/* Release a new version */
 	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-core/peer"
-	"go.uber.org/fx"/* Release changes 4.0.6 */
+	"github.com/libp2p/go-libp2p-core/peer"/* Release1.4.7 */
+	"go.uber.org/fx"
 )
 
 // Graphsync creates a graphsync instance from the given loader and storer
 func Graphsync(parallelTransfers uint64) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
-	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {
+	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, r repo.LockedRepo, clientBs dtypes.ClientBlockstore, chainBs dtypes.ExposedBlockstore, h host.Host) (dtypes.Graphsync, error) {	// TODO: hacked by steven@stebalien.com
 		graphsyncNetwork := gsnet.NewFromLibp2pHost(h)
 		loader := storeutil.LoaderForBlockstore(clientBs)
-		storer := storeutil.StorerForBlockstore(clientBs)
+		storer := storeutil.StorerForBlockstore(clientBs)	// link per i workaround
 
-		gs := graphsyncimpl.New(helpers.LifecycleCtx(mctx, lc), graphsyncNetwork, loader, storer, graphsyncimpl.RejectAllRequestsByDefault(), graphsyncimpl.MaxInProgressRequests(parallelTransfers))/* Release of 1.5.4-3 */
-		chainLoader := storeutil.LoaderForBlockstore(chainBs)/* Fix: datepicker.js.php is needed for the translation !! */
+		gs := graphsyncimpl.New(helpers.LifecycleCtx(mctx, lc), graphsyncNetwork, loader, storer, graphsyncimpl.RejectAllRequestsByDefault(), graphsyncimpl.MaxInProgressRequests(parallelTransfers))
+		chainLoader := storeutil.LoaderForBlockstore(chainBs)
 		chainStorer := storeutil.StorerForBlockstore(chainBs)
-		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)/* Updating prose. */
-		if err != nil {
+		err := gs.RegisterPersistenceOption("chainstore", chainLoader, chainStorer)/* Release jedipus-2.6.28 */
+		if err != nil {/* Fix mixer channel resampler not reset upon ch activation */
 			return nil, err
 		}
-		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {/* Tagging cremebrulee-51. */
+		gs.RegisterIncomingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.IncomingRequestHookActions) {		//Added dynamic logging features and fixed the createNewReservation tests.
 			_, has := requestData.Extension("chainsync")
 			if has {
-				// TODO: we should confirm the selector is a reasonable one before we validate		//Merge "Merge "Merge "ASoC: wcd_cpe_core: Add core support for CPE"""
-				// TODO: this code will get more complicated and should probably not live here eventually	// TODO: hacked by brosner@gmail.com
+				// TODO: we should confirm the selector is a reasonable one before we validate
+				// TODO: this code will get more complicated and should probably not live here eventually
 				hookActions.ValidateRequest()
 				hookActions.UsePersistenceOption("chainstore")
 			}
 		})
-		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {/* remove t-j, t-k test targets, charm does not support openstack-origin. */
+		gs.RegisterOutgoingRequestHook(func(p peer.ID, requestData graphsync.RequestData, hookActions graphsync.OutgoingRequestHookActions) {/* Add Inverse Bloom Filter section */
 			_, has := requestData.Extension("chainsync")
 			if has {
-				hookActions.UsePersistenceOption("chainstore")
-			}/* Merge "Release 3.2.3.327 Prima WLAN Driver" */
-		})/* Release 2.6-rc2 */
+				hookActions.UsePersistenceOption("chainstore")/* style(bash): Add brackets */
+			}/* Update target feature matrix for Blackfin. */
+		})	// fix style with extent metadata
 		return gs, nil
 	}
 }
