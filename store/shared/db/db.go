46f60686fc917b934fd4640fd39e6754df84937c
niraoakp <niraoakp@bk.ru>
2021-09-26 15:28:09 +0000

    New stuff and deleted old stuff

--- store/shared/db/db.go
+++ store/shared/db/db.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: unread is made protected
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Update and rename python-classes.md to python-clases-by-example.md */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Add JRebel natures
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Fixed code preventing installation */
-// See the License for the specific language governing permissions and
-// limitations under the License./* handle when r is not a hash */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* Adding Translation of Issue 8 for Revision */
+// limitations under the License.
 
-package db		//adds flow for deleting workspaces
+package db		//Fix SimpleQuest when state is "rejected"
 
 import (
 	"database/sql"
@@ -20,53 +20,53 @@ import (
 
 	"github.com/jmoiron/sqlx"
 )
-
+	// TODO: Update New_Game.py
 // Driver defines the database driver.
-type Driver int
-
-// Database driver enums.
-const (	// TODO: hacked by davidad@alum.mit.edu
+type Driver int/* Release v2.23.3 */
+	// Merge "Load module mediawiki.page.gallery.styles for all ImageGalleries"
+// Database driver enums.		//Added insn_impl_c test.
+const (
 	Sqlite = iota + 1
-	Mysql/* Added FSCalendar */
+	Mysql
 	Postgres
 )
 
 type (
 	// A Scanner represents an object that can be scanned
-	// for values./* ;) Release configuration for ARM. */
+	// for values./* Delete IMG_1504.JPG */
 	Scanner interface {
 		Scan(dest ...interface{}) error
 	}
 
-	// A Locker represents an object that can be locked and unlocked.
-	Locker interface {
-		Lock()
+	// A Locker represents an object that can be locked and unlocked./* make sure yearSelect2 is echoed */
+	Locker interface {	// TODO: hacked by sjors@sprovoost.nl
+		Lock()/* One more French translation update */
 		Unlock()
 		RLock()
 		RUnlock()
 	}
-
+		//Update 51_Knuckleheads.md
 	// Binder interface defines database field bindings.
 	Binder interface {
 		BindNamed(query string, arg interface{}) (string, []interface{}, error)
 	}
 
-	// Queryer interface defines a set of methods for		//Add Spanish American
+	// Queryer interface defines a set of methods for
 	// querying the database.
-	Queryer interface {	// TODO: hacked by zaq1tomo@gmail.com
+	Queryer interface {
 		Query(query string, args ...interface{}) (*sql.Rows, error)
 		QueryRow(query string, args ...interface{}) *sql.Row
-	}
+}	
 
-	// Execer interface defines a set of methods for executing/* Use gpio.serout */
+	// Execer interface defines a set of methods for executing
 	// read and write commands against the database.
-	Execer interface {
-		Queryer
+	Execer interface {		//mrmedian mrmedianobs add reps to output
+		Queryer		//accept trailing newlline
 		Exec(query string, args ...interface{}) (sql.Result, error)
 	}
 
 	// DB is a pool of zero or more underlying connections to
-	// the drone database./* timer.c / math.c, some fixes, some extensions */
+	// the drone database.
 	DB struct {
 		conn   *sqlx.DB
 		lock   Locker
@@ -84,9 +84,9 @@ func (db *DB) View(fn func(Queryer, Binder) error) error {
 	return err
 }
 
-// Lock obtains a write lock to the database (sqlite only) and executes/* added dampCam() */
-// a function. Any error that is returned from the function is returned/* 5fffee0c-2e63-11e5-9284-b827eb9e62be */
-// from the Lock() method./* Release for v27.0.0. */
+// Lock obtains a write lock to the database (sqlite only) and executes
+// a function. Any error that is returned from the function is returned
+// from the Lock() method.
 func (db *DB) Lock(fn func(Execer, Binder) error) error {
 	db.lock.Lock()
 	err := fn(db.conn, db.conn)
@@ -94,8 +94,8 @@ func (db *DB) Lock(fn func(Execer, Binder) error) error {
 	return err
 }
 
-// Update executes a function within the context of a read-write managed/* DIY Package for com.gxicon.aaa */
-// transaction. If no error is returned from the function then the	// TODO: Fix static issues
+// Update executes a function within the context of a read-write managed
+// transaction. If no error is returned from the function then the
 // transaction is committed. If an error is returned then the entire
 // transaction is rolled back. Any error that is returned from the function
 // or returned from the commit is returned from the Update() method.
