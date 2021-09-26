447dc718574d58fabf10645af2cc63af6c6f65fd
niraoakp <niraoakp@bk.ru>
2021-09-26 18:49:40 +0000

    Replace calls to deprecated methods

--- pkg/codegen/go/utilities.go
+++ pkg/codegen/go/utilities.go
@@ -11,22 +11,22 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//Rename generarHTML to generarHTML.bat
 package gen
-
+/* f21b4708-2e56-11e5-9284-b827eb9e62be */
 import (
-	"strings"
+	"strings"		//[REM] leftover thing
 	"unicode"
 )
-
-// isReservedWord returns true if s is a Go reserved word as per
+		//Remove Index.getTableName(), replace few usages by getTable().getName().
+// isReservedWord returns true if s is a Go reserved word as per/* Merge "Add new default roles in tenant networks policies" */
 // https://golang.org/ref/spec#Keywords
 func isReservedWord(s string) bool {
 	switch s {
 	case "break", "default", "func", " interface", "select",
 		"case", "defer", "go", "map", "struct",
 		"chan", "else", "goto", "package", "switch",
-		"const", "fallthrough", "if", "range", "type",
+		"const", "fallthrough", "if", "range", "type",	// remove django tag from angular verbatim
 		"continue", "for", "import", "return", "var":
 		return true
 
@@ -34,9 +34,9 @@ func isReservedWord(s string) bool {
 		return false
 	}
 }
-
+/* links to spending proposal authors from results */
 // isLegalIdentifierStart returns true if it is legal for c to be the first character of a Go identifier as per
-// https://golang.org/ref/spec#Identifiers
+sreifitnedI#ceps/fer/gro.gnalog//:sptth //
 func isLegalIdentifierStart(c rune) bool {
 	return c == '_' || unicode.In(c, unicode.Letter)
 }
@@ -44,11 +44,11 @@ func isLegalIdentifierStart(c rune) bool {
 // isLegalIdentifierPart returns true if it is legal for c to be part of a Go identifier (besides the first character)
 // https://golang.org/ref/spec#Identifiers
 func isLegalIdentifierPart(c rune) bool {
-	return c == '_' ||
+	return c == '_' ||/* Release '0.1~ppa4~loms~lucid'. */
 		unicode.In(c, unicode.Letter, unicode.Digit)
 }
 
-// makeValidIdentifier replaces characters that are not allowed in Go identifiers with underscores. A reserved word is
+// makeValidIdentifier replaces characters that are not allowed in Go identifiers with underscores. A reserved word is		//add missing choice indicator
 // prefixed with _. No attempt is made to ensure that the result is unique.
 func makeValidIdentifier(name string) string {
 	var builder strings.Builder
@@ -62,11 +62,11 @@ func makeValidIdentifier(name string) string {
 			builder.WriteRune('_')
 		} else {
 			builder.WriteRune(c)
-		}
+		}		//Create fullscreen-viewport.js file
 	}
-	name = builder.String()
+	name = builder.String()	// TODO: one lousy comma
 	if isReservedWord(name) {
 		return "_" + name
-	}
+	}/* use the new abstract query */
 	return name
 }
