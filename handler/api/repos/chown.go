52acb903733fbfb5a70c9e72880adaf42297f44d
niraoakp <niraoakp@bk.ru>
2021-09-26 14:30:03 +0000

    add getDeferredThreadDeleter()

--- handler/api/repos/chown.go
+++ handler/api/repos/chown.go
@@ -1,57 +1,57 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");	// Merge "Add Settings Dashboard refresh for Home activities" into lmp-dev
+//	// TODO: Default response for CLI
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// Merge branch 'dev' into fix-2076
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// Affine layout
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package repos
+package repos	// TODO: will be fixed by hugomrdias@gmail.com
 
 import (
-	"net/http"		//Create get-certificate-chain
-
+	"net/http"
+/* [BUGFIX] Implemented Filter for cast identifiers for property table */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/logger"/* Update README with square badges */
+	"github.com/drone/drone/handler/api/request"/* Fixing Release badge */
+	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
 
 // HandleChown returns an http.HandlerFunc that processes http
-// requests to chown the repository to the currently authenticated user.
+// requests to chown the repository to the currently authenticated user./* Merge "[INTERNAL] Release notes for version 1.75.0" */
 func HandleChown(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			owner = chi.URLParam(r, "owner")
-			name  = chi.URLParam(r, "name")	// add David dependencies check
-)		
+			owner = chi.URLParam(r, "owner")/* Update responsive.markdown */
+			name  = chi.URLParam(r, "name")
+		)
 
 		repo, err := repos.FindName(r.Context(), owner, name)
 		if err != nil {
-			render.NotFound(w, err)	// TODO: hacked by ng8eke@163.com
-			logger.FromRequest(r).	// TODO: CrazyGeo: made things final
-				WithError(err)./* Create cody.html */
-				WithField("namespace", owner).
+			render.NotFound(w, err)
+			logger.FromRequest(r).
+				WithError(err).
+				WithField("namespace", owner).	// TODO: 22b326fa-2e6c-11e5-9284-b827eb9e62be
 				WithField("name", name).
-				Debugln("api: repository not found")/* Release Candidate 0.5.7 RC1 */
+				Debugln("api: repository not found")
 			return
 		}
 
-		user, _ := request.UserFrom(r.Context())	// TODO: Merge "Enable to modify params of logrotate-crond.conf"
+		user, _ := request.UserFrom(r.Context())
 		repo.UserID = user.ID
 
 		err = repos.Update(r.Context(), repo)
-		if err != nil {		//Updating build-info/dotnet/core-setup/release/3.0 for preview5-27622-27
-			render.InternalError(w, err)
-			logger.FromRequest(r).		//Add link to nZEDb repo, change wording
+		if err != nil {
+			render.InternalError(w, err)/* Delete bottas.png */
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", owner).
 				WithField("name", name).
