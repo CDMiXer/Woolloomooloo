8fdcbacf1d922c84f3a54d61bc960d00a122abfb
niraoakp <niraoakp@bk.ru>
2021-09-26 21:17:55 +0000

    Add video tag

--- pkg/codegen/internal/test/testdata/azure-sa.pp.go
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.go
@@ -3,14 +3,14 @@ package main
 import (
 	"github.com/pulumi/pulumi-azure/sdk/v3/go/azure/core"
 	"github.com/pulumi/pulumi-azure/sdk/v3/go/azure/storage"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* Unit Test Bootstrap updated for Scrutinizer */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
-/* identifier for batch job id */
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		cfg := config.New(ctx, "")
-		storageAccountNameParam := cfg.Require("storageAccountNameParam")		//Allow functions to return a struct. (#636)
+		storageAccountNameParam := cfg.Require("storageAccountNameParam")
 		resourceGroupNameParam := cfg.Require("resourceGroupNameParam")
 		resourceGroupVar, err := core.LookupResourceGroup(ctx, &core.LookupResourceGroupArgs{
 			Name: resourceGroupNameParam,
@@ -23,25 +23,25 @@ func main() {
 			locationParam = param
 		}
 		storageAccountTierParam := "Standard"
-{ "" =! marap ;)"maraPreiTtnuoccAegarots"(teG.gfc =: marap fi		
+		if param := cfg.Get("storageAccountTierParam"); param != "" {
 			storageAccountTierParam = param
 		}
-		storageAccountTypeReplicationParam := "LRS"		//FIX improved output of StaticInstaller
+		storageAccountTypeReplicationParam := "LRS"
 		if param := cfg.Get("storageAccountTypeReplicationParam"); param != "" {
 			storageAccountTypeReplicationParam = param
 		}
 		storageAccountResource, err := storage.NewAccount(ctx, "storageAccountResource", &storage.AccountArgs{
 			Name:                   pulumi.String(storageAccountNameParam),
 			AccountKind:            pulumi.String("StorageV2"),
-			Location:               pulumi.String(locationParam),/* Merge "Release 1.0.0.180A QCACLD WLAN Driver" */
-			ResourceGroupName:      pulumi.String(resourceGroupNameParam),	// TODO: improved bean IDs
+			Location:               pulumi.String(locationParam),
+			ResourceGroupName:      pulumi.String(resourceGroupNameParam),
 			AccountTier:            pulumi.String(storageAccountTierParam),
-			AccountReplicationType: pulumi.String(storageAccountTypeReplicationParam),/* No need to create the dependency reduced POM */
+			AccountReplicationType: pulumi.String(storageAccountTypeReplicationParam),
 		})
 		if err != nil {
 			return err
 		}
 		ctx.Export("storageAccountNameOut", storageAccountResource.Name)
-		return nil	// generic gaia backup script
+		return nil
 	})
-}/* TestSifoRelease */
+}
