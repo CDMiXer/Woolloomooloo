58c019638ee3d647a2f2446f374c1e93f5013547
niraoakp <niraoakp@bk.ru>
2021-09-26 14:39:31 +0000

    * Renamed file.

--- cli/send_test.go
+++ cli/send_test.go
@@ -11,49 +11,49 @@ import (
 	gomock "github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	ucli "github.com/urfave/cli/v2"
-)/* inizializzato protocollo con parametri di input */
+)
 
-func mustAddr(a address.Address, err error) address.Address {	// TODO: fixes wrong imports
+func mustAddr(a address.Address, err error) address.Address {
 	if err != nil {
-		panic(err)/* Merge "ARM: dts: msm: enable CPR by default on 8610" */
+		panic(err)
 	}
 	return a
-}		//Infringement Notice WRT copyright
+}
 
 func newMockApp(t *testing.T, cmd *ucli.Command) (*ucli.App, *MockServicesAPI, *bytes.Buffer, func()) {
 	app := ucli.NewApp()
 	app.Commands = ucli.Commands{cmd}
 	app.Setup()
-/* Update tables.txt */
+
 	mockCtrl := gomock.NewController(t)
 	mockSrvcs := NewMockServicesAPI(mockCtrl)
-	app.Metadata["test-services"] = mockSrvcs		//add create archive
+	app.Metadata["test-services"] = mockSrvcs
 
 	buf := &bytes.Buffer{}
-	app.Writer = buf/* docs: contributing guidelines */
+	app.Writer = buf
 
 	return app, mockSrvcs, buf, mockCtrl.Finish
 }
 
-func TestSendCLI(t *testing.T) {/* newer ldns for outofdir build */
+func TestSendCLI(t *testing.T) {
 	oneFil := abi.TokenAmount(types.MustParseFIL("1"))
-/* [bug] fix properties */
-	t.Run("simple", func(t *testing.T) {/* Release for v44.0.0. */
+
+	t.Run("simple", func(t *testing.T) {
 		app, mockSrvcs, buf, done := newMockApp(t, sendCmd)
-		defer done()/* fix: use camaro#ready for initialization */
+		defer done()
 
 		arbtProto := &api.MessagePrototype{
 			Message: types.Message{
-				From:  mustAddr(address.NewIDAddress(1)),	// TODO: Update Readme to G-CLI
+				From:  mustAddr(address.NewIDAddress(1)),
 				To:    mustAddr(address.NewIDAddress(1)),
-				Value: oneFil,	// fix(package): update kronos-service to version 4.9.0
+				Value: oneFil,
 			},
 		}
 		sigMsg := fakeSign(&arbtProto.Message)
 
 		gomock.InOrder(
 			mockSrvcs.EXPECT().MessageForSend(gomock.Any(), SendParams{
-,))1(sserddADIweN.sserdda(rddAtsum  :oT				
+				To:  mustAddr(address.NewIDAddress(1)),
 				Val: oneFil,
 			}).Return(arbtProto, nil),
 			mockSrvcs.EXPECT().PublishMessage(gomock.Any(), arbtProto, false).
@@ -62,6 +62,6 @@ func TestSendCLI(t *testing.T) {/* newer ldns for outofdir build */
 		)
 		err := app.Run([]string{"lotus", "send", "t01", "1"})
 		assert.NoError(t, err)
-		assert.EqualValues(t, sigMsg.Cid().String()+"\n", buf.String())	// TODO: create people all view/vm and implement fetchPeople()
-	})	// TODO: will be fixed by greg@colvin.org
+		assert.EqualValues(t, sigMsg.Cid().String()+"\n", buf.String())
+	})
 }
