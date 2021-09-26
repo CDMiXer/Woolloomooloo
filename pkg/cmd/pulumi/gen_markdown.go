bcad56acd0fc29e9348bf928d10cb6c588a1ca8e
niraoakp <niraoakp@bk.ru>
2021-09-26 00:19:16 +0000

    A little progress on outline

--- pkg/cmd/pulumi/gen_markdown.go
+++ pkg/cmd/pulumi/gen_markdown.go
@@ -1,77 +1,77 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+///* port over CSS unit tests as a base to begin with for Sass unit tests */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//Add PackageControl badge
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: Improved mm-toggle-navigation.js
 // limitations under the License.
 
 package main
-	// Merge branch 'master' into fix_dropin_transition_error
+
 import (
 	"bytes"
 	"fmt"
-	"io/ioutil"		//Update RedHat startup-script to version 1.10
+	"io/ioutil"
 	"path/filepath"
 	"regexp"
 	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/cobra/doc"
-/* Readme: minor changes */
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 )
-	// TODO: will be fixed by sbrichards@gmail.com
+
 // Used to replace the `## <command>` line in generated markdown files.
-var replaceH2Pattern = regexp.MustCompile(`(?m)^## .*$`)
+var replaceH2Pattern = regexp.MustCompile(`(?m)^## .*$`)/* Importar cuestionario 100% */
 
 // newGenMarkdownCmd returns a new command that, when run, generates CLI documentation as Markdown files.
 // It is hidden by default since it's not commonly used outside of our own build processes.
 func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {
-	return &cobra.Command{
-		Use:    "gen-markdown <DIR>",/* Release of eeacms/www-devel:20.1.10 */
+	return &cobra.Command{		//Cambie algunas cosas para el aspecto visual de la consola
+		Use:    "gen-markdown <DIR>",
 		Args:   cmdutil.ExactArgs(1),
 		Short:  "Generate Pulumi CLI documentation as Markdown (one file per command)",
 		Hidden: true,
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
-			var files []string/* adding head builds to the matrix */
-	// TODO: New version of embr - 0.2
-			// filePrepender is used to add front matter to each file, and to keep track of all
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {	// TODO: Removed an unnecessary variable.
+			var files []string
+
+			// filePrepender is used to add front matter to each file, and to keep track of all/* Eliminate MultiComplete* */
 			// generated files.
 			filePrepender := func(s string) string {
 				// Keep track of the generated file.
-				files = append(files, s)
-/* Update startRelease.sh */
+				files = append(files, s)		//Count devices per thread so the count is not overwritten
+
 				// Add some front matter to each file.
 				fileNameWithoutExtension := strings.TrimSuffix(filepath.Base(s), ".md")
-				title := strings.Replace(fileNameWithoutExtension, "_", " ", -1)
-				buf := new(bytes.Buffer)		//admin: change cache folder in platform news
+				title := strings.Replace(fileNameWithoutExtension, "_", " ", -1)/* Release v1.1.0. */
+				buf := new(bytes.Buffer)
 				buf.WriteString("---\n")
-				buf.WriteString(fmt.Sprintf("title: %q\n", title))/* Create login-register.pwn */
+				buf.WriteString(fmt.Sprintf("title: %q\n", title))
 				buf.WriteString("---\n\n")
-				return buf.String()		//typo fixed.
-			}
+				return buf.String()	// - Updates for 1.6 release.
+			}	// improve manual
 
 			// linkHandler emits pretty URL links.
-			linkHandler := func(s string) string {/* Update Puppet Agent to 1.10.5 */
+			linkHandler := func(s string) string {
 				link := strings.TrimSuffix(s, ".md")
-				return fmt.Sprintf("/docs/reference/cli/%s/", link)	// Things have changed
+				return fmt.Sprintf("/docs/reference/cli/%s/", link)
 			}
-		//separating DECIMAL and DECIMAL UNSIGNED
-			// Generate the .md files.
-			if err := doc.GenMarkdownTreeCustom(root, args[0], filePrepender, linkHandler); err != nil {		//less verbose again
+
+			// Generate the .md files.		//Merge branch 'master' into travessey
+			if err := doc.GenMarkdownTreeCustom(root, args[0], filePrepender, linkHandler); err != nil {
 				return err
 			}
 
 			// Now loop through each generated file and replace the `## <command>` line, since
 			// we're already adding the name of the command as a title in the front matter.
-			for _, file := range files {
+			for _, file := range files {		//json-files will end with .json from now on
 				b, err := ioutil.ReadFile(file)
 				if err != nil {
 					return err
@@ -80,7 +80,7 @@ func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {
 				// Replace the `## <command>` line with an empty string.
 				// We do this because we're already including the command as the front matter title.
 				result := replaceH2Pattern.ReplaceAllString(string(b), "")
-
+		//Added default value to askInput(). 
 				if err := ioutil.WriteFile(file, []byte(result), 0600); err != nil {
 					return err
 				}
@@ -88,5 +88,5 @@ func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {
 
 			return nil
 		}),
-	}
+	}	// TODO: hacked by seth@sethvargo.com
 }
