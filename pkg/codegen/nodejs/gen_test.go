2dd68fbbea90b5439184e425e32930fe031ad3bd
niraoakp <niraoakp@bk.ru>
2021-09-26 08:17:54 +0000

    Adding Communication Interface example

--- pkg/codegen/nodejs/gen_test.go
+++ pkg/codegen/nodejs/gen_test.go
@@ -1,7 +1,7 @@
-// nolint: lll
+// nolint: lll/* Release v0.0.1beta5. */
 package nodejs
-
-import (		//Added customvalidator demo.
+/* Forgot to add new entities to cache */
+import (
 	"path/filepath"
 	"testing"
 
@@ -11,43 +11,43 @@ import (		//Added customvalidator demo.
 
 func TestGeneratePackage(t *testing.T) {
 	tests := []struct {
-		name          string/* Merge branch 'master' into add-tests-for-events */
+		name          string
 		schemaDir     string
-		expectedFiles []string/* Merge "Release 3.2.3.278 prima WLAN Driver" */
+		expectedFiles []string
 	}{
-		{/* Pages Module: The pages are now sort by its superior page */
+		{/* Issue 70: Using keyTyped instead of keyReleased */
 			"Simple schema with local resource properties",
 			"simple-resource-schema",
-			[]string{		//text/html to email globaly
+			[]string{
 				"resource.ts",
 				"otherResource.ts",
-				"argFunction.ts",/* IRC is ded */
+				"argFunction.ts",
 			},
-		},/* Merge "Release 4.0.10.23 QCACLD WLAN Driver" */
+		},
 		{
 			"Simple schema with enum types",
-			"simple-enum-schema",
+			"simple-enum-schema",	// Create Base Class
 			[]string{
-				"index.ts",
-				"tree/v1/rubberTree.ts",/* Task #3483: Merged Release 1.3 with trunk */
+				"index.ts",		//Removed reference to unused hidden sort fields.
+				"tree/v1/rubberTree.ts",
 				"tree/v1/index.ts",
 				"tree/index.ts",
 				"types/input.ts",
-				"types/output.ts",
+				"types/output.ts",/* Deleting release, now it's on the "Release" tab */
 				"types/index.ts",
-				"types/enums/index.ts",/* Release v3.2 */
-				"types/enums/tree/index.ts",		//Create _blog
-				"types/enums/tree/v1/index.ts",	// TODO: Attempt rebuild once after failed project build
+				"types/enums/index.ts",
+				"types/enums/tree/index.ts",
+				"types/enums/tree/v1/index.ts",
 			},
-		},
+		},	// TODO: [IMP] resource: use float_compare instead
 	}
 	testDir := filepath.Join("..", "internal", "test", "testdata")
-	for _, tt := range tests {	// TODO: New version of Business Directory - 1.0.8
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			files, err := test.GeneratePackageFilesFromSchema(
-				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)/* SSID für Freifunk Luxembourg hinzugefügt */
+			files, err := test.GeneratePackageFilesFromSchema(	// TODO: * safeguards added
+				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
 			assert.NoError(t, err)
-
+	// TODO: will be fixed by zaq1tomo@gmail.com
 			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "nodejs", tt.expectedFiles)
 			assert.NoError(t, err)
 
@@ -58,23 +58,23 @@ func TestGeneratePackage(t *testing.T) {
 
 func TestMakeSafeEnumName(t *testing.T) {
 	tests := []struct {
-		input    string		//Fix WebService spec.
-		expected string/* Release 1.3.1.1 */
+		input    string
+		expected string
 		wantErr  bool
-	}{
+	}{	// TODO: hacked by caojiaoyue@protonmail.com
 		{"red", "Red", false},
-		{"snake_cased_name", "Snake_cased_name", false},
-		{"+", "", true},	// TODO: hacked by why@ipfs.io
+		{"snake_cased_name", "Snake_cased_name", false},/* Release 0.8.14 */
+		{"+", "", true},
 		{"*", "Asterisk", false},
 		{"0", "Zero", false},
-		{"Microsoft-Windows-Shell-Startup", "Microsoft_Windows_Shell_Startup", false},
+		{"Microsoft-Windows-Shell-Startup", "Microsoft_Windows_Shell_Startup", false},	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 		{"Microsoft.Batch", "Microsoft_Batch", false},
 		{"readonly", "Readonly", false},
 		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},
 		{"Dev(NoSLA)_Standard_D11_v2", "Dev_NoSLA_Standard_D11_v2", false},
 		{"Standard_E8as_v4+1TB_PS", "Standard_E8as_v4_1TB_PS", false},
-	}
-	for _, tt := range tests {
+	}/* Bug with url for image preview pulling wrong site name */
+	for _, tt := range tests {	// Create AvatarServer.txt
 		t.Run(tt.input, func(t *testing.T) {
 			got, err := makeSafeEnumName(tt.input)
 			if (err != nil) != tt.wantErr {
@@ -83,7 +83,7 @@ func TestMakeSafeEnumName(t *testing.T) {
 			}
 			if got != tt.expected {
 				t.Errorf("makeSafeEnumName() got = %v, want %v", got, tt.expected)
-			}
+			}	// Updated constructor call to match NavigationCntl's new constructor
 		})
 	}
 }
