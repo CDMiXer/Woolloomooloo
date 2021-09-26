8c5d298276ad14f1902af5bfcfa49ce44c3bb780
niraoakp <niraoakp@bk.ru>
2021-09-26 02:55:55 +0000

    Delete application_pt.properties

--- plugin/converter/legacy_oss.go
+++ plugin/converter/legacy_oss.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//fix: Cast String without calling `.toString()`
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: Add current project files
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Change transferCash to makeOnlineTransaction
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,/* feat: better parameter tuning */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
-package converter/* Release v0.24.2 */
+package converter
 
 import (
-	"github.com/drone/drone/core"
-)	// TODO: tried to implement constructor that takes a Class as parameter
-
+	"github.com/drone/drone/core"	// TODO: will be fixed by lexy8russo@outlook.com
+)
+	// TODO: retain instead of strong since we're not ARC yet
 // Legacy returns a conversion service that converts the
 // legacy 0.8 file to a yaml file.
 func Legacy(enabled bool) core.ConvertService {
-	return new(noop)	// Merge branch 'master' into doug-fix-legacy-gamelift
+	return new(noop)
 }
