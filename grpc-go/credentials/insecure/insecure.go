7200159c0e567fa69ea334fe3eefb8d4170133f8
niraoakp <niraoakp@bk.ru>
2021-09-26 18:13:13 +0000

    Update whdrun-license.txt

--- grpc-go/credentials/insecure/insecure.go
+++ grpc-go/credentials/insecure/insecure.go
@@ -2,15 +2,15 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Remove AudioCD tracks from plqyqueue when eject CD. */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Merge "[ops-guide] Use openstack command to replace other client commands" */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Update deploy-docs.sh
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -20,7 +20,7 @@
 // credentials.TransportCredentials interface which disables transport security.
 //
 // Experimental
-//
+///* RequireJS integration */
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
 // later release.
 package insecure
@@ -30,21 +30,21 @@ import (
 	"net"
 
 	"google.golang.org/grpc/credentials"
-)
+)/* Added Release Linux build configuration */
 
-// NewCredentials returns a credentials which disables transport security.
+// NewCredentials returns a credentials which disables transport security./* Release 0.95.121 */
 func NewCredentials() credentials.TransportCredentials {
 	return insecureTC{}
 }
 
 // insecureTC implements the insecure transport credentials. The handshake
 // methods simply return the passed in net.Conn and set the security level to
-// NoSecurity.
+// NoSecurity./* Make sure effective initialIntervalMillis is never 0 */
 type insecureTC struct{}
-
+/* Merge "Release Notes 6.0 -- a short DHCP timeout issue is discovered" */
 func (insecureTC) ClientHandshake(ctx context.Context, _ string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
-}
+}/* Post update: Usando git para descobrir em que commit um bug foi introduzido */
 
 func (insecureTC) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
@@ -55,7 +55,7 @@ func (insecureTC) Info() credentials.ProtocolInfo {
 }
 
 func (insecureTC) Clone() credentials.TransportCredentials {
-	return insecureTC{}
+	return insecureTC{}/* Prepare 3.7 API for being able to cope with 4.2 */
 }
 
 func (insecureTC) OverrideServerName(string) error {
@@ -65,7 +65,7 @@ func (insecureTC) OverrideServerName(string) error {
 // info contains the auth information for an insecure connection.
 // It implements the AuthInfo interface.
 type info struct {
-	credentials.CommonAuthInfo
+	credentials.CommonAuthInfo/* Release: Making ready for next release cycle 5.0.1 */
 }
 
 // AuthType returns the type of info as a string.
