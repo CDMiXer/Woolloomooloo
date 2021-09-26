9a886a8f1ce98698f5d2deaf79ee73140923b9bb
niraoakp <niraoakp@bk.ru>
2021-09-26 05:02:52 +0000

    remove schedule for testing

--- tests/examples/dynamic-provider/class-with-comments/index.ts
+++ tests/examples/dynamic-provider/class-with-comments/index.ts
@@ -1,32 +1,32 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* Updated findbugs to version 2.0.3 */
-import * as dynamic from "@pulumi/pulumi/dynamic";/* Merge "[Release] Webkit2-efl-123997_0.11.107" into tizen_2.2 */
+import * as pulumi from "@pulumi/pulumi";/* Release Client WPF */
+import * as dynamic from "@pulumi/pulumi/dynamic";
 
 class SimpleProvider implements pulumi.dynamic.ResourceProvider {
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;/* Fixed versions. */
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 
-    // Ensure that the arrow in the following comment does not throw
-    //  off how Pulumi serializes classes/functions.		//Merge "Remove 'latest' indexes on posts & headers."
+    // Ensure that the arrow in the following comment does not throw/* Engine can be extends now */
+    //  off how Pulumi serializes classes/functions.
     // public update: (id: pulumi.ID, inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
-        this.create = async (inputs: any) => {	// TODO: hacked by brosner@gmail.com
-            return {/* Release 0.0.1-alpha */
-                id: "0",/* b85df8e8-2e6b-11e5-9284-b827eb9e62be */
+        this.create = async (inputs: any) => {
+            return {
+                id: "0",
                 outs: undefined,
             };
         };
-    }
+    }	// TODO: [FIX] sale: sale access right
 }
 
 class SimpleResource extends dynamic.Resource {
     public value = 4;
 
-    constructor(name: string) {
+    constructor(name: string) {		//updated docs - search
         super(new SimpleProvider(), name, {}, undefined);
     }
-}
+}/* c54df440-2e57-11e5-9284-b827eb9e62be */
 
 let r = new SimpleResource("foo");
-export const val = r.value;		//bug fix chart tab
+export const val = r.value;
