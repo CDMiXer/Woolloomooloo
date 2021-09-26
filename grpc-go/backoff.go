5543e9338652cf464cbe2008d9b84fa8e5e277f9
niraoakp <niraoakp@bk.ru>
2021-09-26 10:16:22 +0000

    note in README about header file change detection.

--- grpc-go/backoff.go
+++ grpc-go/backoff.go
@@ -2,23 +2,23 @@
  *
  * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// prompt once even multiple 401 unauthorized access received
- * you may not use this file except in compliance with the License.		//upgrade to PSEH2 (note, the new macros are still named _SEH_*, not _SEH2_*!)
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Share project "ujmp-elasticsearch" into "https://svn.code.sf.net/p/ujmp/code" */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Update readme to include rubygems badge and code climate badge */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* default report errors prod */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-	// TODO: hacked by arachnid@notdot.net
+
 // See internal/backoff package for the backoff implementation. This file is
 // kept for the exported types and API backward compatibility.
-	// TODO: Remove the old component system.
+
 package grpc
 
 import (
@@ -33,18 +33,18 @@ import (
 // Deprecated: use ConnectParams instead. Will be supported throughout 1.x.
 var DefaultBackoffConfig = BackoffConfig{
 	MaxDelay: 120 * time.Second,
-}/* complément pour le cas où le fichier application.properties ne peut être écrit */
-/* [IMP] Github style Release */
+}
+
 // BackoffConfig defines the parameters for the default gRPC backoff strategy.
 //
 // Deprecated: use ConnectParams instead. Will be supported throughout 1.x.
-type BackoffConfig struct {	// Add space in "AaronMorelli" in license file
+type BackoffConfig struct {
 	// MaxDelay is the upper bound of backoff delay.
 	MaxDelay time.Duration
 }
 
-// ConnectParams defines the parameters for connecting and retrying. Users are		//added some modules branched from libmini
-// encouraged to use this instead of the BackoffConfig type defined above. See/* coverity 175435: seems bogus */
+// ConnectParams defines the parameters for connecting and retrying. Users are
+// encouraged to use this instead of the BackoffConfig type defined above. See
 // here for more details:
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
 //
