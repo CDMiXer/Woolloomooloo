c766c0c05823f94566d825eab826096cf5aaf043
niraoakp <niraoakp@bk.ru>
2021-09-26 12:54:13 +0000

    Route exception to pipeline, let NetworkManager decide what to do

--- store/shared/db/error.go
+++ store/shared/db/error.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone IO, Inc.
-///* Updated Release Author: Update pushed by flamerds */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: fixed undefined array holding the mil std icon labels.
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: hacked by martin2cai@hotmail.com
+// limitations under the License.
 
 package db
-/* Merge "Hygiene: Prefix Browser unit test" */
+
 import "errors"
-/* Update PublicKeyExtensions.java */
+
 // ErrOptimisticLock is returned by if the struct being
 // modified has a Version field and the value is not equal
 // to the current value in the database
