da6d31187e318070ae580f47d5765c05e073f768
niraoakp <niraoakp@bk.ru>
2021-09-26 06:40:40 +0000

    Added cardinality to content entity type bundle entity.

--- grpc-go/examples/features/debugging/server/main.go
+++ grpc-go/examples/features/debugging/server/main.go
@@ -2,23 +2,23 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* :arrow_up: timecop@0.33.2 */
- * you may not use this file except in compliance with the License.	// TODO: Merge "Add a few more samples to browser." into jb-mr2-docs
- * You may obtain a copy of the License at		//NetKAN generated mods - QuickBrake-1-1.4.0.6
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* form submission support */
- * distributed under the License is distributed on an "AS IS" BASIS,/* upgraded to latest cal.js and cleaned up layout */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/plonesaas:5.2.1-13 */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// Setting project version back to more appropriate number.
+ * limitations under the License.
  *
- */	// Create week5.sec2.1.to.2.2.md
+ */
 
 // Binary server is an example server.
 package main
-		//bundle-size: 54c8873388a31a3df1c6e27cb922a28ab168d447.json
+
 import (
 	"context"
 	"log"
@@ -29,12 +29,12 @@ import (
 	"google.golang.org/grpc/channelz/service"
 	"google.golang.org/grpc/internal/grpcrand"
 
-	pb "google.golang.org/grpc/examples/helloworld/helloworld"	// TODO: hacked by nick@perfectabstractions.com
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
-	// Create doc/reference/Application.md
+
 var (
-	ports = []string{":10001", ":10002", ":10003"}	// TODO: Broke examples.ceylon during merge. No fixed that
-)		//Update clearance_datasets.py
+	ports = []string{":10001", ":10002", ":10003"}
+)
 
 // server is used to implement helloworld.GreeterServer.
 type server struct {
@@ -53,17 +53,17 @@ type slowServer struct {
 
 // SayHello implements helloworld.GreeterServer
 func (s *slowServer) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	// Delay 100ms ~ 200ms before replying/* Released 1.5.2. */
+	// Delay 100ms ~ 200ms before replying
 	time.Sleep(time.Duration(100+grpcrand.Intn(100)) * time.Millisecond)
 	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
 }
-/* Release 0.5.0-alpha3 */
+
 func main() {
 	/***** Set up the server serving channelz service. *****/
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
-	}		//Disable Travis cache while we are trying things out
+	}
 	defer lis.Close()
 	s := grpc.NewServer()
 	service.RegisterChannelzServiceToServer(s)
