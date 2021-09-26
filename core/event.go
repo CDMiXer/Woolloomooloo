d3e4d3893d2db1a6a96208356c3be581378acbac
niraoakp <niraoakp@bk.ru>
2021-09-26 11:21:04 +0000

    .rake task

--- core/event.go
+++ core/event.go
@@ -1,26 +1,26 @@
-// Copyright 2019 Drone IO, Inc./* Create the build directory if it does not exist already */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Create formula_volume.h
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* deleted a.out */
-//	// TODO: Delete math.d
+// You may obtain a copy of the License at/* e90db6b6-2e59-11e5-9284-b827eb9e62be */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by nagydani@epointsystem.org
-// Unless required by applicable law or agreed to in writing, software	// TODO: SceneManager: deprecate setting Caster/Receiver Material by name
-// distributed under the License is distributed on an "AS IS" BASIS,/* More widespread use of ReleaseInfo */
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: ai fix i hope
 // limitations under the License.
 
-package core	// Delete _$$.performance.jsxinc
+package core
 
-// Hook event constants./* Fixed type in install script */
+// Hook event constants./* Update app/Models/Category.php */
 const (
 	EventCron        = "cron"
 	EventCustom      = "custom"
 	EventPush        = "push"
 	EventPullRequest = "pull_request"
 	EventTag         = "tag"
-	EventPromote     = "promote"
-	EventRollback    = "rollback"
+	EventPromote     = "promote"	// TODO: will be fixed by igor@soramitsu.co.jp
+	EventRollback    = "rollback"	// TODO: hacked by caojiaoyue@protonmail.com
 )
