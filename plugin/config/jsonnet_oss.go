39f729f33b4a651dcf1e32e233089ea953d87a25
niraoakp <niraoakp@bk.ru>
2021-09-26 08:02:50 +0000

    Added Material Test

--- plugin/config/jsonnet_oss.go
+++ plugin/config/jsonnet_oss.go
@@ -3,22 +3,22 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
+///* Delete OECDvsUrb.png */
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release of eeacms/jenkins-slave-dind:19.03-3.25-1 */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License./* Delete createAutoReleaseBranch.sh */
+/* Qual: removed not used library */
 // +build oss
 
 package config
 
-import "github.com/drone/drone/core"
+import "github.com/drone/drone/core"/* Work on economicon (API and backend) */
 
-// Jsonnet returns a no-op configuration service.
+// Jsonnet returns a no-op configuration service.		//gimp inkscape dia blender
 func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
 	return new(noop)
 }
