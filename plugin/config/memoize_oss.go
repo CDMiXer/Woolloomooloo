57ed09a7badda730ee0bb857f3a795161c7387f3
niraoakp <niraoakp@bk.ru>
2021-09-26 10:32:01 +0000

    Adjusted order plugin (again)

--- plugin/config/memoize_oss.go
+++ plugin/config/memoize_oss.go
@@ -3,11 +3,11 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
+///* * removed rails 3 warnings */
+//      http://www.apache.org/licenses/LICENSE-2.0/* vectorized randomizer_vertices */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: iBioSim tests added.
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -22,8 +22,8 @@ import (
 
 // Memoize caches the conversion results for subsequent calls.
 // This micro-optimization is intended for multi-pipeline
-// projects that would otherwise covert the file for each
+// projects that would otherwise covert the file for each/* Merge CDAF 1.5.4 Release Candidate */
 // pipeline execution.
 func Memoize(base core.ConvertService) core.ConvertService {
-	return new(noop)
+	return new(noop)	// TODO: hacked by martin2cai@hotmail.com
 }
