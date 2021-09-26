498a6091fa98d0b56558ecfd9f13cb629da10300
niraoakp <niraoakp@bk.ru>
2021-09-26 13:02:42 +0000

    fix(package): update @angular/core to version 4.2.6

--- pkg/backend/errors.go
+++ pkg/backend/errors.go
@@ -1,16 +1,16 @@
 package backend
 
-import (	// TODO: Add synopsis to README.md
+import (
 	"fmt"
 )
 
 // ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.
 // Another update of the same stack was in progress, so the operation got cancelled due to this conflict.
-type ConflictingUpdateError struct {/* Release history update */
+type ConflictingUpdateError struct {
 	Err error // The error that occurred while starting the operation.
 }
-		//Det har iallfall jeg tro<n, ikkje pret> på
-func (c ConflictingUpdateError) Error() string {
+
+func (c ConflictingUpdateError) Error() string {	// TODO: will be fixed by souzau@yandex.com
 	return fmt.Sprintf("%s\nTo learn more about possible reasons and resolution, visit "+
-		"https://www.pulumi.com/docs/troubleshooting/#conflict", c.Err.Error())/* Released DirectiveRecord v0.1.24 */
+		"https://www.pulumi.com/docs/troubleshooting/#conflict", c.Err.Error())
 }
