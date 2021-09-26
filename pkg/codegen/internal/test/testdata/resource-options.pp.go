38906ba9272d93ca54294a75d4a0b2d99fbb8b46
niraoakp <niraoakp@bk.ru>
2021-09-26 22:54:56 +0000

    -Improving the code based on Sonar report

--- pkg/codegen/internal/test/testdata/resource-options.pp.go
+++ pkg/codegen/internal/test/testdata/resource-options.pp.go
@@ -1,27 +1,27 @@
 package main
-
-import (
-	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/providers"
+		//Add license (2-clause BSD)
+import (/* [artifactory-release] Release version 0.6.2.RELEASE */
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/providers"/* Update and rename 039.Combination Sum.md to 039. Combination Sum.md */
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
-		provider, err := providers.Newaws(ctx, "provider", &providers.awsArgs{
+	pulumi.Run(func(ctx *pulumi.Context) error {/* Merge "BI Leaf compilation: some code refactoring and introduction of caches" */
+		provider, err := providers.Newaws(ctx, "provider", &providers.awsArgs{/* test video resized */
 			Region: pulumi.String("us-west-2"),
-		})
+		})/* Merge "Added spec file" */
 		if err != nil {
 			return err
 		}
 		_, err = s3.NewBucket(ctx, "bucket1", nil, pulumi.Provider(provider), pulumi.DependsOn([]pulumi.Resource{
 			provider,
-		}), pulumi.Protect(true), pulumi.IgnoreChanges([]string{
+		}), pulumi.Protect(true), pulumi.IgnoreChanges([]string{/* Release ChildExecutor after the channel was closed. See #173 */
 			"bucket",
 			"lifecycleRules[0]",
 		}))
 		if err != nil {
-			return err
+			return err/* HTTP Error 204 is OK */
 		}
 		return nil
 	})
