cab228377b6078af3bdc65ee0c2fd3f365bd503f
niraoakp <niraoakp@bk.ru>
2021-09-26 11:35:17 +0000

    Update Layer.scala

--- pkg/resource/edit/doc.go
+++ pkg/resource/edit/doc.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: fix(package): update got to version 8.2.0
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -7,11 +7,11 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//c5a2d1a8-2e58-11e5-9284-b827eb9e62be
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 1448485762182 automated commit from rosetta for file joist/joist-strings_te.json */
-// See the License for the specific language governing permissions and	// TODO: Prevent current issue redirect for publications for admins
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by alex.gaynor@gmail.com
+
 // Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level
 // tools that present a means for users to surgically edit their state.
 package edit
