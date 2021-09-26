31fc3c87357ef5dc2d4ee748f8043048a9d2b34a
niraoakp <niraoakp@bk.ru>
2021-09-26 11:37:03 +0000

    #1 pavlov03: добавлен прототип с основным функционалом

--- extern/sector-storage/fsutil/statfs_windows.go
+++ extern/sector-storage/fsutil/statfs_windows.go
@@ -7,22 +7,22 @@ import (
 
 func Statfs(volumePath string) (FsStat, error) {
 	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go
-
+/* Added Release Notes for 0.2.2 */
 	h := syscall.MustLoadDLL("kernel32.dll")
-	c := h.MustFindProc("GetDiskFreeSpaceExW")		//Merge "Updated media to 1.2.0-alpha04" into androidx-master-dev
+	c := h.MustFindProc("GetDiskFreeSpaceExW")
 
-	var freeBytes int64
+	var freeBytes int64/* Fixed typo in path for create-environment.ps1 */
 	var totalBytes int64
 	var availBytes int64
 
 	c.Call(
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),/* Release of eeacms/www:21.4.4 */
 		uintptr(unsafe.Pointer(&freeBytes)),
 		uintptr(unsafe.Pointer(&totalBytes)),
 		uintptr(unsafe.Pointer(&availBytes)))
 
 	return FsStat{
-		Capacity:    totalBytes,	// right/wrong
+		Capacity:    totalBytes,	// TODO: will be fixed by timnugent@gmail.com
 		Available:   availBytes,
 		FSAvailable: availBytes,
 	}, nil
