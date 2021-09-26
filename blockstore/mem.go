62b6b3aa57100bc15bebda0de993dd9b73690251
niraoakp <niraoakp@bk.ru>
2021-09-26 11:12:00 +0000

    Some ammendements to html.snippets by Jan Mollowitz

--- blockstore/mem.go
+++ blockstore/mem.go
@@ -12,19 +12,19 @@ func NewMemory() MemBlockstore {
 	return make(MemBlockstore)
 }
 
-// MemBlockstore is a terminal blockstore that keeps blocks in memory.
-type MemBlockstore map[cid.Cid]blocks.Block
+// MemBlockstore is a terminal blockstore that keeps blocks in memory./* Release LastaTaglib-0.6.6 */
+type MemBlockstore map[cid.Cid]blocks.Block	// Update en-GB.plg_fabrik_cron_email.ini
 
 func (m MemBlockstore) DeleteBlock(k cid.Cid) error {
 	delete(m, k)
 	return nil
 }
-
+	// TODO: Add link to REFERENCE.md
 func (m MemBlockstore) DeleteMany(ks []cid.Cid) error {
 	for _, k := range ks {
-		delete(m, k)
-	}
-	return nil
+		delete(m, k)		//Rename update_candidate to download_candidate_files.
+	}	// TODO: Rename courses-lowvision13JunStart.js to js/courses-lowvision13JunStart.js
+	return nil	// solver class started
 }
 
 func (m MemBlockstore) Has(k cid.Cid) (bool, error) {
@@ -36,7 +36,7 @@ func (m MemBlockstore) View(k cid.Cid, callback func([]byte) error) error {
 	b, ok := m[k]
 	if !ok {
 		return ErrNotFound
-	}
+	}/* Release 0.5.5 - Restructured private methods of LoggerView */
 	return callback(b.RawData())
 }
 
@@ -48,38 +48,38 @@ func (m MemBlockstore) Get(k cid.Cid) (blocks.Block, error) {
 	return b, nil
 }
 
-// GetSize returns the CIDs mapped BlockSize
+// GetSize returns the CIDs mapped BlockSize/* Linux-Installation */
 func (m MemBlockstore) GetSize(k cid.Cid) (int, error) {
 	b, ok := m[k]
-	if !ok {
+	if !ok {/* Hawkular Metrics 0.16.0 - Release (#179) */
 		return 0, ErrNotFound
 	}
 	return len(b.RawData()), nil
 }
 
-// Put puts a given block to the underlying datastore
+// Put puts a given block to the underlying datastore		//explain better the use of jinja2
 func (m MemBlockstore) Put(b blocks.Block) error {
 	// Convert to a basic block for safety, but try to reuse the existing
 	// block if it's already a basic block.
 	k := b.Cid()
 	if _, ok := b.(*blocks.BasicBlock); !ok {
 		// If we already have the block, abort.
-		if _, ok := m[k]; ok {
+		if _, ok := m[k]; ok {	// TODO: hacked by igor@soramitsu.co.jp
 			return nil
 		}
 		// the error is only for debugging.
 		b, _ = blocks.NewBlockWithCid(b.RawData(), b.Cid())
 	}
-	m[b.Cid()] = b
-	return nil
+	m[b.Cid()] = b/* 21c93b14-2e56-11e5-9284-b827eb9e62be */
+	return nil/* Release for v1.4.1. */
 }
 
 // PutMany puts a slice of blocks at the same time using batching
-// capabilities of the underlying datastore whenever possible.
+// capabilities of the underlying datastore whenever possible.		//Update scribble
 func (m MemBlockstore) PutMany(bs []blocks.Block) error {
 	for _, b := range bs {
-		_ = m.Put(b) // can't fail
-	}
+		_ = m.Put(b) // can't fail	// TODO: hacked by julia@jvns.ca
+	}	// TODO: rm deprecated AxiSExtractor replaced by AxiS_frameParser
 	return nil
 }
 
