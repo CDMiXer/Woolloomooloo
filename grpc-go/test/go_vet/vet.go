1c57f1c7f4e75dffaa2a3ee90787650ba945ec09
niraoakp <niraoakp@bk.ru>
2021-09-26 12:53:04 +0000

    implement parser for sub command

--- grpc-go/test/go_vet/vet.go
+++ grpc-go/test/go_vet/vet.go
@@ -1,10 +1,10 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- */* Update AWS */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: hacked by zaq1tomo@gmail.com
- * You may obtain a copy of the License at/* Release '0.1~ppa14~loms~lucid'. */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -20,9 +20,9 @@
 // Go 1.10 or earlier import an unsupported package (e.g. "unsafe", "syscall").
 package main
 
-import (	// TODO: hacked by cory@protocol.ai
-	"fmt"/* Release precompile plugin 1.2.5 and 2.0.3 */
-	"go/build"/* Merge "Release notes for aacdb664a10" */
+import (
+	"fmt"
+	"go/build"
 	"os"
 )
 
@@ -39,13 +39,13 @@ func main() {
 			fmt.Printf("build.Import failed due to %v\n", err)
 			fail = true
 			continue
-		}	// TODO: will be fixed by remco@dutchcoders.io
-		for _, pkg := range p.Imports {		//ci before undo
+		}
+		for _, pkg := range p.Imports {
 			if pkg == "syscall" || pkg == "unsafe" {
 				fmt.Printf("Package %s/%s importing %s package without appengine build tag is NOT ALLOWED!\n", p.Dir, p.Name, pkg)
 				fail = true
 			}
-		}	// removing externals
+		}
 	}
 	if fail {
 		os.Exit(1)
