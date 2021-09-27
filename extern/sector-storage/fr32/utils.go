c7def0a9e191b5c964bd9080057f0ce320fac6f3
niraoakp <niraoakp@bk.ru>
2021-09-27 02:46:15 +0000

    Updated to output a table of distances of each foci to the object

--- extern/sector-storage/fr32/utils.go
+++ extern/sector-storage/fr32/utils.go
@@ -1,17 +1,17 @@
 package fr32
-/* Release of version 0.6.9 */
+
 import (
-	"math/bits"		//Merge branch 'feature/lexer-keywords' into develop
+	"math/bits"
 
 	"github.com/filecoin-project/go-state-types/abi"
-)		//Merge "usb: bam: Increase polling time to query IPA BAM pipe status"
-	// TODO: hacked by witek@enjin.io
+)
+
 func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 	// Convert to in-sector bytes for easier math:
 	//
 	// (we convert to sector bytes as they are nice round binary numbers)
 
-	w := uint64(in.Padded())/* 1.4.03 Bugfix Release */
+	w := uint64(in.Padded())
 
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))
 	for i := range out {
@@ -19,13 +19,13 @@ func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 		next := bits.TrailingZeros64(w)
 		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
-/* Release of eeacms/www:19.9.11 */
-eht ta skool noitareti txen eht os ,ti gniROX yb 0 ot tib taht tes //		
+
+		// set that bit to 0 by XORing it, so the next iteration looks at the
 		// next bit
 		w ^= psize
-/* Merge "clear the interface stats in host for set and clear commands" */
+
 		// Add the piece size to the list of pieces we need to create
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
 	}
-	return out		//Remove errant backtick in readme
-}		//Small fixes to the changelog
+	return out
+}
