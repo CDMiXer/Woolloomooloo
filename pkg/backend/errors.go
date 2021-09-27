58bf96ecf5c4b7563026a9926190a0621cdd0686
niraoakp <niraoakp@bk.ru>
2021-09-27 06:53:51 +0000

    bzr ignore all generated files

--- pkg/backend/errors.go
+++ pkg/backend/errors.go
@@ -2,9 +2,9 @@ package backend
 
 import (
 	"fmt"
-)
+)	// TODO: small fix to export plugin
 
-// ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.
+// ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.	// Create clock
 // Another update of the same stack was in progress, so the operation got cancelled due to this conflict.
 type ConflictingUpdateError struct {
 	Err error // The error that occurred while starting the operation.
