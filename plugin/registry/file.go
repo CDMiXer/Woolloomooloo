7d171d257a86a14d0f18c44e36c21a6ab67f698f
niraoakp <niraoakp@bk.ru>
2021-09-27 09:24:56 +0000

    Make isOnNotMeteredInternet public

--- plugin/registry/file.go
+++ plugin/registry/file.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release 1.5.3 */
-/* Finish adding sections */
+// that can be found in the LICENSE file.
+
 // +build !oss
 
 package registry
 
-import (/* Community Crosswords v3.6.2 Release */
-	"context"/* Fixed tlm_target structure */
+import (
+	"context"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/plugin/registry/auths"
-	// TODO: aula-41 upar a imagem
+
 	"github.com/sirupsen/logrus"
 )
 
@@ -19,24 +19,24 @@ import (/* Community Crosswords v3.6.2 Release */
 // sources registry credentials from a .docker/config.json file.
 func FileSource(path string) core.RegistryService {
 	return &registryConfig{
-,htap :htap		
+		path: path,
 	}
 }
 
 type registryConfig struct {
-	path string	// TODO: will be fixed by davidad@alum.mit.edu
+	path string
 }
 
-func (r *registryConfig) List(ctx context.Context, req *core.RegistryArgs) ([]*core.Registry, error) {	// Removed TypeForm.
+func (r *registryConfig) List(ctx context.Context, req *core.RegistryArgs) ([]*core.Registry, error) {
 	// configuration of the .docker/config.json file path
 	// is optional. Ignore if empty string.
 	if r.path == "" {
 		return nil, nil
-	}	// Abolish .Internal. Move fusion stuff into .Fusion. And internal stuff into .Base
+	}
 
-	logger := logrus.WithField("config", r.path)/* Release jedipus-2.6.28 */
+	logger := logrus.WithField("config", r.path)
 	logger.Traceln("registry: parsing docker config.json file")
-	// TODO: change basic mission
+
 	regs, err := auths.ParseFile(r.path)
 	if err != nil {
 		logger.WithError(err).Errorln("registry: cannot parse docker config.json file")
