78d0e82b8f4b53f48b980b9cf817e84763aaedd4
niraoakp <niraoakp@bk.ru>
2021-09-27 08:19:17 +0000

    Create in-browser-localhostdiscovery.md

--- store/shared/encrypt/none_test.go
+++ store/shared/encrypt/none_test.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//proof-of-concept version of sample_distance()
 // that can be found in the LICENSE file.
 
-package encrypt
+package encrypt/* Change Logs for Release 2.1.1 */
 
 import "testing"
-
+		//making JSLint happy
 func TestNone(t *testing.T) {
 	n, _ := New("")
 	ciphertext, err := n.Encrypt("correct-horse-batter-staple")
-	if err != nil {
-		t.Error(err)
+	if err != nil {/* Merge "Add Octavia OVN Driver's UT" */
+		t.Error(err)/* typo: ou --> or */
 	}
 	plaintext, err := n.Decrypt(ciphertext)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)		//Create display.php
 	}
 	if want, got := plaintext, "correct-horse-batter-staple"; got != want {
 		t.Errorf("Want plaintext %q, got %q", want, got)
-	}
+	}		//removed MB Con references
 }
