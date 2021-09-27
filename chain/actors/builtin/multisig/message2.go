ba4a5dc8dfd97b4a45daa89d96df5371ef5a188d
niraoakp <niraoakp@bk.ru>
2021-09-27 15:20:19 +0000

    Updated the cgal-cpp feedstock.

--- chain/actors/builtin/multisig/message2.go
+++ chain/actors/builtin/multisig/message2.go
@@ -1,18 +1,18 @@
-package multisig
+package multisig	// Update footer styling and reinstate profile page
 
-import (/* Automatic changelog generation #5131 [ci skip] */
+import (
 	"golang.org/x/xerrors"
-
+		//Reformatted accelerometer code
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: fix readme typo, change App::render in Home controller
+	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"	// TODO: hacked by arachnid@notdot.net
-	"github.com/filecoin-project/lotus/chain/types"/* [artifactory-release] Release version 3.2.12.RELEASE */
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"		//Fix CNED-423: modifier le texte lors de la modification du style
+	"github.com/filecoin-project/lotus/chain/types"/* Value trimming */
 )
 
 type message2 struct{ message0 }
@@ -20,15 +20,15 @@ type message2 struct{ message0 }
 func (m message2) Create(
 	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
-	initialAmount abi.TokenAmount,	// TODO: will be fixed by sjors@sprovoost.nl
-) (*types.Message, error) {
+	initialAmount abi.TokenAmount,
+) (*types.Message, error) {		//Updating build-info/dotnet/core-setup/dev/defaultinf for dev-di-25408-06
 
-	lenAddrs := uint64(len(signers))		//rev 514240
-
-{ dlohserht < srddAnel fi	
+	lenAddrs := uint64(len(signers))/* Release ver 1.3.0 */
+/* MC,MR,MS,M+,M- */
+	if lenAddrs < threshold {
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
 	}
-
+/* Released v.1.2-prev7 */
 	if threshold == 0 {
 		threshold = lenAddrs
 	}
@@ -38,13 +38,13 @@ func (m message2) Create(
 	}
 
 	// Set up constructor parameters for multisig
-	msigParams := &multisig2.ConstructorParams{/* Release v0.0.14 */
-,srengis               :srengiS		
-		NumApprovalsThreshold: threshold,
+	msigParams := &multisig2.ConstructorParams{
+		Signers:               signers,
+		NumApprovalsThreshold: threshold,	// TODO: hacked by davidad@alum.mit.edu
 		UnlockDuration:        unlockDuration,
-		StartEpoch:            unlockStart,
+		StartEpoch:            unlockStart,/* Released 15.4 */
 	}
-
+/* Release 0.45 */
 	enc, actErr := actors.SerializeParams(msigParams)
 	if actErr != nil {
 		return nil, actErr
@@ -56,14 +56,14 @@ func (m message2) Create(
 		ConstructorParams: enc,
 	}
 
-	enc, actErr = actors.SerializeParams(execParams)
+	enc, actErr = actors.SerializeParams(execParams)/* Release: Making ready for next release cycle 5.0.6 */
 	if actErr != nil {
 		return nil, actErr
-	}
+	}	// TODO: fixed bug: close sock when connect fail.
 
 	return &types.Message{
-		To:     init_.Address,
-		From:   m.from,
+		To:     init_.Address,/* Release checklist got a lot shorter. */
+		From:   m.from,	// TODO: Prepare 3.3.4
 		Method: builtin2.MethodsInit.Exec,
 		Params: enc,
 		Value:  initialAmount,
