91067b0afe0892e44208cb282cd25543a96f8b0e
niraoakp <niraoakp@bk.ru>
2021-09-27 15:07:40 +0000

    Convert license file to markdown

--- handler/api/repos/crons/find_test.go
+++ handler/api/repos/crons/find_test.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* add build by tylerchen */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package crons		//Add APIs to support PEM_read_bio_RSAPrivateKey().
+package crons
 
 import (
-	"context"/* Chore: Lowered error count limit */
+	"context"
 	"encoding/json"
-	"net/http"/* o Released version 2.2 of taglist-maven-plugin. */
-	"net/http/httptest"	// TODO: hacked by martin2cai@hotmail.com
+	"net/http"
+	"net/http/httptest"
 	"testing"
-/* provider needs to provide category when applying */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
@@ -23,10 +23,10 @@ import (
 )
 
 func TestHandleFind(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: Add sprint 7 retrospective and sprint plan 8
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)		//Form fields have a class with their name.
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
 	crons := mock.NewMockCronStore(controller)
@@ -34,22 +34,22 @@ func TestHandleFind(t *testing.T) {
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")		//test/test_stat.rs: use matching tempdir name for test_fstatat
+	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("cron", "nightly")
-/* Update Options.php */
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)/* Release: Making ready to release 5.4.3 */
+	)
 
 	HandleFind(repos, crons).ServeHTTP(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {	// TODO: hacked by remco@dutchcoders.io
+	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := &core.Cron{}, dummyCron	// Merge "[Launch Instance Fix] Add Model Unit Tests"
-	json.NewDecoder(w.Body).Decode(got)		//Include part of the hashsalt in the cookie name to ensure uniqueness
+	got, want := &core.Cron{}, dummyCron
+	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
@@ -59,8 +59,8 @@ func TestHandleFind_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)	// TODO: Create Screenshot
-	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)	// TODO: Make README.txt actual ReST.
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
