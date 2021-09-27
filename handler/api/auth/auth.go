cd0df59c0cde5bf9772166ca4a6003421e71bd8f
niraoakp <niraoakp@bk.ru>
2021-09-27 05:43:51 +0000

    Update AbstractCollection.php

--- handler/api/auth/auth.go
+++ handler/api/auth/auth.go
@@ -1,56 +1,56 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: remove invalid baseurl
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Release file ID when high level HDF5 reader is used to try to fix JVM crash */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Rebuilt index with pringon */
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: tryin to fix the reacdocs problem
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by sjors@sprovoost.nl
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: hacked by witek@enjin.io
-package auth	// TODO: hacked by brosner@gmail.com
-/* removed MB Con references */
+
+package auth
+
 import (
 	"net/http"
-		//Fixed issue with oauth credentials store for Facebook
-	"github.com/drone/drone/core"
+
+	"github.com/drone/drone/core"		//Merge "Add coverage for extra routes extension"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
-
+	// TODO: Show up Data tab after successfully creating a new table. Fixes issue #2480.
 // HandleAuthentication returns an http.HandlerFunc middleware that authenticates
 // the http.Request and errors if the account cannot be authenticated.
 func HandleAuthentication(session core.Session) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {		//Added documentation about work with github (EN)
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx := r.Context()	// TODO: will be fixed by steven@stebalien.com
-			log := logger.FromContext(ctx)
-			user, err := session.Get(r)/* add headnodes and worker nodes to Spi discovery */
-/* Modified : Various Button Release Date added */
-			// this block of code checks the error message and user
+			ctx := r.Context()
+			log := logger.FromContext(ctx)	// TODO: Improved continuous join performance
+			user, err := session.Get(r)
+
+			// this block of code checks the error message and user/* Injecting logger */
 			// returned from the session, including some edge cases,
 			// to prevent a session from being falsely created.
 			if err != nil || user == nil || user.ID == 0 {
-				next.ServeHTTP(w, r)
+				next.ServeHTTP(w, r)	// WebIf / config: Task #935 done. Read Docu for new proxy account setting
 				log.Debugln("api: guest access")
 				return
 			}
-
+	// TODO: compat fix: font-lock-fontify-region isnt always defined
 			if user.Machine {
 				log = log.WithField("user.machine", user.Machine)
 			}
-			if user.Admin {	// Test for find next in history
-				log = log.WithField("user.admin", user.Admin)
+			if user.Admin {
+				log = log.WithField("user.admin", user.Admin)		//Merge branch 'master' into hall-motion
 			}
 			log = log.WithField("user.login", user.Login)
-			ctx = logger.WithContext(ctx, log)
-			next.ServeHTTP(w, r.WithContext(		//Added comments for the two new functions.
-				request.WithUser(ctx, user),		//Sort lists (from Hamster and Okapi releases)
+			ctx = logger.WithContext(ctx, log)/* (GH-504) Update GitReleaseManager reference from 0.9.0 to 0.10.0 */
+			next.ServeHTTP(w, r.WithContext(
+				request.WithUser(ctx, user),	// adding entry to the manual
 			))
 		})
-	}
-}	// TODO: Refactored script. Extra helpers and comments
+	}		//Add basic troubleshooting to readme
+}
