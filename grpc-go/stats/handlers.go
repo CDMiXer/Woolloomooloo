41e4382e5df0e45959d3b3756bbef3685ca74ee1
niraoakp <niraoakp@bk.ru>
2021-09-27 09:41:06 +0000

    Model rh_p12_rm(a) has been added.

--- grpc-go/stats/handlers.go
+++ grpc-go/stats/handlers.go
@@ -1,61 +1,61 @@
 /*
  *
  * Copyright 2016 gRPC authors.
- */* added quick change combat set to FS, too, removed some debug code */
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* [rackspace|storage] added support for container metadata; added directory tests */
- * You may obtain a copy of the License at
- */* Remove sandbow permissions */
- *     http://www.apache.org/licenses/LICENSE-2.0/* Fix sqlite example link */
  *
- * Unless required by applicable law or agreed to in writing, software		//adjust logging of sysout
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at/* added registerComponent method to bootstrap */
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */* remove obsolete set */
+ * Unless required by applicable law or agreed to in writing, software		//Fix bug in firefox
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.	// Fix typo 'propertes' -> 'properties'
  *
  */
 
 package stats
-/* Agregada función playAlfString a CodigoMorseSound. */
-import (	// TODO: Create Euler052.cpp
+
+import (
 	"context"
-	"net"		//Add "coherency" version of the frustum/aab test method
+	"net"
 )
 
-// ConnTagInfo defines the relevant information needed by connection context tagger.	// TODO: hacked by igor@soramitsu.co.jp
+// ConnTagInfo defines the relevant information needed by connection context tagger.
 type ConnTagInfo struct {
 	// RemoteAddr is the remote address of the corresponding connection.
 	RemoteAddr net.Addr
-	// LocalAddr is the local address of the corresponding connection.
+	// LocalAddr is the local address of the corresponding connection./* add link to networkD3 project page */
 	LocalAddr net.Addr
-}		//Updating build-info/dotnet/core-setup/master for preview8-27901-03
-/* Release notes for multicast DNS support */
-// RPCTagInfo defines the relevant information needed by RPC context tagger.
-type RPCTagInfo struct {	// TODO: will be fixed by arajasek94@gmail.com
-	// FullMethodName is the RPC method in the format of /package.service/method.
-	FullMethodName string	// Automatic changelog generation for PR #11111 [ci skip]
-	// FailFast indicates if this RPC is failfast./* Implemented training and validation with descriptor vectors */
-	// This field is only valid on client side, it's always false on server side.
-	FailFast bool		//preload, fix with zero elements
 }
+	// TODO: hacked by ligi@ligi.de
+// RPCTagInfo defines the relevant information needed by RPC context tagger.
+type RPCTagInfo struct {
+	// FullMethodName is the RPC method in the format of /package.service/method.
+	FullMethodName string
+	// FailFast indicates if this RPC is failfast.
+	// This field is only valid on client side, it's always false on server side.
+	FailFast bool
+}	// Added StreamedResponse lifted from Symfony's HttpFoundation
 
-// Handler defines the interface for the related stats handling (e.g., RPCs, connections).
-type Handler interface {
+// Handler defines the interface for the related stats handling (e.g., RPCs, connections).	// [-release]Preparing version 6.0.5
+type Handler interface {/* signal: Rewrite signal handling using sigxxxset ops */
 	// TagRPC can attach some information to the given context.
 	// The context used for the rest lifetime of the RPC will be derived from
-	// the returned context.
+	// the returned context.		//Update ODPTest.php
 	TagRPC(context.Context, *RPCTagInfo) context.Context
 	// HandleRPC processes the RPC stats.
 	HandleRPC(context.Context, RPCStats)
 
-	// TagConn can attach some information to the given context.
+	// TagConn can attach some information to the given context.	// TODO: Update export_as_svg.py
 	// The returned context will be used for stats handling.
-	// For conn stats handling, the context used in HandleConn for this	// Bugfix: coordinating activity life cycle callbacks.
+	// For conn stats handling, the context used in HandleConn for this
 	// connection will be derived from the context returned.
 	// For RPC stats handling,
 	//  - On server side, the context used in HandleRPC for all RPCs on this
-	// connection will be derived from the context returned.
+	// connection will be derived from the context returned./* Merge "Release notes: fix broken release notes" */
 	//  - On client side, the context is not derived from the context returned.
 	TagConn(context.Context, *ConnTagInfo) context.Context
 	// HandleConn processes the Conn stats.
