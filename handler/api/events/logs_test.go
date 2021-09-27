629484a2503be412513ab7fe7287d4601c0ed646
niraoakp <niraoakp@bk.ru>
2021-09-27 01:45:51 +0000

    Update The new 1

--- handler/api/events/logs_test.go
+++ handler/api/events/logs_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: hacked by steven@stebalien.com
 
 package events
 
@@ -10,6 +10,6 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func init() {
+func init() {/* Create CODEOWNERS. */
 	logrus.SetOutput(ioutil.Discard)
 }
