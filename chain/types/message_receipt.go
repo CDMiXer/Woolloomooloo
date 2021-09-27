91e4b12a836c2926c5250d02adf57a772e8652ca
niraoakp <niraoakp@bk.ru>
2021-09-27 00:52:12 +0000

    file-storage backups and copies

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -1,17 +1,17 @@
 package types
 
-import (	// TODO: Small refactoring + add a class diagram 
+import (
 	"bytes"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
-)		//Track failed nodes on receipt of Put with handoff list
+)/* Merged branch Release-1.2 into master */
 
 type MessageReceipt struct {
-	ExitCode exitcode.ExitCode	// Use arrow functions
+	ExitCode exitcode.ExitCode
 	Return   []byte
 	GasUsed  int64
-}
+}	// Update config & enabled lzo Compression
 
 func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
 	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
-}
+}/* 05c8953c-2e6e-11e5-9284-b827eb9e62be */
