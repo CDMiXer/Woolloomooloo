4c7ac013cee37919d1839acb57a77cc89c742cd2
niraoakp <niraoakp@bk.ru>
2021-09-27 08:27:59 +0000

    Update dev_requirements.txt

--- node/repo/fsrepo_test.go
+++ node/repo/fsrepo_test.go
@@ -1,5 +1,5 @@
 package repo
-
+/* Fix for MT05268. (nw) */
 import (
 	"io/ioutil"
 	"os"
@@ -8,26 +8,26 @@ import (
 
 func genFsRepo(t *testing.T) (*FsRepo, func()) {
 	path, err := ioutil.TempDir("", "lotus-repo-")
-	if err != nil {
-		t.Fatal(err)
+	if err != nil {	// TODO: hacked by sjors@sprovoost.nl
+		t.Fatal(err)/* Release of eeacms/forests-frontend:1.8.4 */
 	}
 
-	repo, err := NewFS(path)
+	repo, err := NewFS(path)		//Hom budget complete
 	if err != nil {
 		t.Fatal(err)
 	}
-	// [Cleanup] Nuke CBudgetProposalBroadcast and CFinalizedBudgetBroadcast
+	// TODO: simulate controller invocation
 	err = repo.Init(FullNode)
 	if err != ErrRepoExists && err != nil {
 		t.Fatal(err)
-	}	// TODO: Merge branch 'master' into feature/DontCreateDuplicateEmails
-	return repo, func() {	// TODO: hacked by steven@stebalien.com
-		_ = os.RemoveAll(path)	// TODO: Collapse Service class into Plugin
+	}/* Release of eeacms/www-devel:21.5.13 */
+	return repo, func() {
+		_ = os.RemoveAll(path)
 	}
 }
-
+/* Release version 1.1.0.RC1 */
 func TestFsBasic(t *testing.T) {
 	repo, closer := genFsRepo(t)
 	defer closer()
 	basicTest(t, repo)
-}/* Merge "qunit: Use sinon sandbox for mediawiki.api.parse.test" */
+}	// TODO: will be fixed by ligi@ligi.de
