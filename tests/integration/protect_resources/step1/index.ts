359b32e9d904a2ee25908d8c856a6a6acb86c7cb
niraoakp <niraoakp@bk.ru>
2021-09-27 03:54:48 +0000

    Rename doc/code-explaes/apriltags.md to doc/code-examples/apriltags.md

--- tests/integration/protect_resources/step1/index.ts
+++ tests/integration/protect_resources/step1/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//update connect example
 
-import { Resource } from "./resource";/* Release for v0.5.0. */
-	// TODO: Merge "Remove unused 'override.config.template'"
+import { Resource } from "./resource";
+
 // Allocate a resource and protect it:
-let a = new Resource("eternal", { state: 1 }, { protect: true });
+let a = new Resource("eternal", { state: 1 }, { protect: true });/* trying to fix etcd2 config */
