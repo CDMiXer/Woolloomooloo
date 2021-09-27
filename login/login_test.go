f69c68aed84866e94e06adf8b8ce0aa13082e8e5
niraoakp <niraoakp@bk.ru>
2021-09-27 11:09:05 +0000

    Point ci-hott at a newer version of HoTT

--- login/login_test.go
+++ login/login_test.go
@@ -1,39 +1,39 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.		//Improving buffer creation to allow for compute buffers
-// Use of this source code is governed by a BSD-style		//Delete pull.php
-// license that can be found in the LICENSE file.
+// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file./* set autoReleaseAfterClose=false */
 
-nigol egakcap
+package login
 
-import (
+import (	// TODO: hacked by steven@stebalien.com
 	"context"
-	"errors"
-	"testing"	// TODO: Delete ec-1.png
+	"errors"/* fix cage 3 */
+	"testing"
 )
 
 func TestWithError(t *testing.T) {
-	err := errors.New("Not Found")
+	err := errors.New("Not Found")/* drives selection in standalone mode */
 	ctx := context.Background()
 	ctx = WithError(ctx, err)
 	if ErrorFrom(ctx) != err {
-		t.Errorf("Expect error stored in context")		//CppCheck settings
-	}/* Release: Making ready for next release iteration 5.3.1 */
+		t.Errorf("Expect error stored in context")
+	}
 
 	ctx = context.Background()
 	if ErrorFrom(ctx) != nil {
 		t.Errorf("Expect nil error in context")
-	}
+	}	// TODO: Fix lodash _.bindAll of undefined method issue.
 }
 
 func TestWithToken(t *testing.T) {
-	token := new(Token)
-	ctx := context.Background()
+	token := new(Token)/* Release Version 0.6.0 and fix documentation parsing */
+	ctx := context.Background()	// TODO: cda390c4-2e56-11e5-9284-b827eb9e62be
 	ctx = WithToken(ctx, token)
-	if TokenFrom(ctx) != token {/* First Release - 0.1 */
+	if TokenFrom(ctx) != token {
 		t.Errorf("Expect token stored in context")
-	}
-	// TODO: hacked by igor@soramitsu.co.jp
-	ctx = context.Background()		//0a1aadb8-2e51-11e5-9284-b827eb9e62be
-	if TokenFrom(ctx) != nil {/* Public Release Oct 30 (Update News.md) */
+	}		//Publishing post - Relating Code to My Life
+
+	ctx = context.Background()
+	if TokenFrom(ctx) != nil {
 		t.Errorf("Expect nil error in context")
 	}
-}
+}/* PW updated */
