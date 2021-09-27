6ca6532ccf9ca693b01fe85c67131588a2677dfa
niraoakp <niraoakp@bk.ru>
2021-09-27 02:03:15 +0000

    Merge branch 'master' into fix-reading-urls

--- store/shared/migrate/sqlite/ddl.go
+++ store/shared/migrate/sqlite/ddl.go
@@ -2,16 +2,16 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Released v.1.2.0.2 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release version 4.2.0.RELEASE */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//update review by authenticated user ok
 package sqlite
 
 //go:generate togo ddl -package sqlite -dialect sqlite3
