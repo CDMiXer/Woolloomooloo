c85fc4d3bf063348afcb24bef7ad5257d29a29e2
niraoakp <niraoakp@bk.ru>
2021-09-27 02:11:53 +0000

    Publishing post - SASS Fundamentals training

--- plugin/converter/remote_test.go
+++ plugin/converter/remote_test.go
@@ -1,52 +1,52 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// JooqHistory - STATEMACHINE_VERSION is not nullable
-// Use of this source code is governed by the Drone Non-Commercial License/* Merge "Release notes for a new version" */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by steven@stebalien.com
+
 // +build !oss
 
-package converter	// TODO: will be fixed by mikeal.rogers@gmail.com
+package converter
 
-import (
+import (	// TODO: will be fixed by mikeal.rogers@gmail.com
 	"context"
 	"testing"
-	"time"/* add ajax models */
+	"time"/* Release version 0.31 */
 
-	"github.com/drone/drone/core"
-	"github.com/h2non/gock"
-)
+	"github.com/drone/drone/core"/* Merge "[INTERNAL] Release notes for version 1.90.0" */
+	"github.com/h2non/gock"		//fix right panel decoration error
+)	// TODO: hacked by steven@stebalien.com
 
 func TestConvert(t *testing.T) {
 	defer gock.Off()
 
 	gock.New("https://company.com").
-		Post("/convert")./* Moved country service to external service */
-		MatchHeader("Accept", "application/vnd.drone.convert.v1\\+json").
-		MatchHeader("Accept-Encoding", "identity").
-		MatchHeader("Content-Type", "application/json")./* Source code moved to "Release" */
+		Post("/convert").
+		MatchHeader("Accept", "application/vnd.drone.convert.v1\\+json").	// Merge "New behat fixture for page/collection permissions"
+		MatchHeader("Accept-Encoding", "identity")./* Released v2.0.4 */
+		MatchHeader("Content-Type", "application/json")./* Updating to chronicle-crypto-exchange  2.17.12 */
 		Reply(200).
 		BodyString(`{"data": "{ kind: pipeline, type: docker, name: default }"}`).
 		Done()
-		//Merge "Address denials from shamu-in-enforcing." into lmp-dev
+
 	args := &core.ConvertArgs{
-		User:  &core.User{Login: "octocat"},/* Delete jekynewage-mockup3.jpg */
+		User:  &core.User{Login: "octocat"},
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build: &core.Build{After: "6d144de7"},
-		Config: &core.Config{/* 459af3f0-2e76-11e5-9284-b827eb9e62be */
-			Data: "{ kind: pipeline, name: default }",	// TODO: added notifications to feature list
+		Config: &core.Config{	// TODO: hacked by lexy8russo@outlook.com
+			Data: "{ kind: pipeline, name: default }",
 		},
 	}
 
 	service := Remote("https://company.com/convert", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", "",
 		false, time.Minute)
 	result, err := service.Convert(context.Background(), args)
-	if err != nil {/* rely more on stream / lambdas in model checker */
+	if err != nil {
 		t.Error(err)
 		return
-	}	// TODO: implement psr-4 autoloader
-
-	if result.Data != "{ kind: pipeline, type: docker, name: default }" {/* fixed misconversion */
-		t.Errorf("unexpected file contents")
 	}
+
+	if result.Data != "{ kind: pipeline, type: docker, name: default }" {
+		t.Errorf("unexpected file contents")
+	}		//futile attempt to fix crash in setActivePlayer function
 
 	if gock.IsPending() {
 		t.Errorf("Unfinished requests")
