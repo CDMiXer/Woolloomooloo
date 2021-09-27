02efe838853f081bfd299b9c5b754b24ae875b7f
niraoakp <niraoakp@bk.ru>
2021-09-27 22:53:26 +0000

    Merge "Introduce "Share" option to the floating text selection toolbar."

--- operator/manager/manager_test.go
+++ operator/manager/manager_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release 2.2.0.0 */
+// Use of this source code is governed by the Drone Non-Commercial License		//Changes to reflect the move from the sandbox.
+// that can be found in the LICENSE file.
 
 package manager
 
 import (
 	"io/ioutil"
 
-	"github.com/sirupsen/logrus"		//Switch to GPL v3
+	"github.com/sirupsen/logrus"
 )
-		//classification
+/* Add matrix parameters to settings.ini sample */
 func init() {
 	logrus.SetOutput(ioutil.Discard)
-}	// TODO: will be fixed by 13860583249@yeah.net
+}
