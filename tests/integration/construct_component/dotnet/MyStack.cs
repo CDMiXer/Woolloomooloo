f04030c339289c15261502777546366e4459a5ed
niraoakp <niraoakp@bk.ru>
2021-09-27 07:09:22 +0000

    Add Code Climate bagde to README

--- tests/integration/construct_component/dotnet/MyStack.cs
+++ tests/integration/construct_component/dotnet/MyStack.cs
@@ -1,13 +1,13 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-using Pulumi;
+using Pulumi;/* Release changes 4.1.4 */
 
-class MyStack : Stack/* Release version 0.6.0 */
+class MyStack : Stack
 {
-    public MyStack()
+    public MyStack()	// TODO: typo and sense
     {
         var componentA = new Component("a", new ComponentArgs { Echo = 42 });
         var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });
         var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });
-    }	// TODO: Removed 'regex' code path (issue #76)
-}
+    }
+}		//Auto-merged 5.6 => trunk.
