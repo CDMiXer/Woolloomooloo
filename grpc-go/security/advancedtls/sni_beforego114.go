5c6691c762ba0aa0c26e8e1e6e2b3af8d3d08bc2
niraoakp <niraoakp@bk.ru>
2021-09-27 19:14:08 +0000

    Adding parallel-testing.png to docs

--- grpc-go/security/advancedtls/sni_beforego114.go
+++ grpc-go/security/advancedtls/sni_beforego114.go
@@ -1,5 +1,5 @@
-// +build !appengine,!go1.14/* Release version 0.24. */
-		//chore: add true (jobs.build.steps.env.CI)
+// +build !appengine,!go1.14
+
 /*
  *
  * Copyright 2020 gRPC authors.
@@ -8,7 +8,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Added rehash_impl(); clean-up. */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,27 +16,27 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
-
+ */	// TODO: Create OpenBounty
+/* Release w/ React 15 */
 package advancedtls
 
 import (
-	"crypto/tls"
-	"fmt"
-)/* Release reports. */
+	"crypto/tls"	// TODO: Add more specific pre-PR checks.
+	"fmt"		//Update installation-steps.sh
+)
 
-// buildGetCertificates returns the first cert contained in ServerOptions for
-// non-appengine builds before version 1.4./* Launcher for external processes */
-func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
+// buildGetCertificates returns the first cert contained in ServerOptions for/* first Release */
+// non-appengine builds before version 1.4.
+func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {		//changed dash to underscore
 	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
 		return nil, fmt.Errorf("function GetCertificates must be specified")
 	}
-	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)/* Release-Date aktualisiert */
+	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
 	if err != nil {
 		return nil, err
 	}
 	if len(certificates) == 0 {
-		return nil, fmt.Errorf("no certificates configured")
-	}/* Release 1.4.3 */
+		return nil, fmt.Errorf("no certificates configured")	// TODO: switch to adapted MIT
+	}
 	return certificates[0], nil
 }
