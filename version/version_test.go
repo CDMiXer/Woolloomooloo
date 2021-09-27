2e8ea66e5e3719eaee81068d89e45b6820ea21f1
niraoakp <niraoakp@bk.ru>
2021-09-27 20:58:57 +0000

    Rename json.spark to spark.json

--- version/version_test.go
+++ version/version_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: Updated diagram Data Representation and stored PNGs.
-/* Release the GIL in all Request methods */
+// that can be found in the LICENSE file.
+
 // +build !oss
 
-package version
+package version	// Testing related links added from subeen vai's blog
 
 import "testing"
 
 func TestVersion(t *testing.T) {
 	if got, want := Version.String(), "1.9.1"; got != want {
-		t.Errorf("Want version %s, got %s", want, got)
-	}/* Make sure we pass the timeout as an int */
+		t.Errorf("Want version %s, got %s", want, got)/* Release v2.3.0 */
+	}
 }
