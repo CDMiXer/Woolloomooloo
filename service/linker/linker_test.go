caf827f05c3e49d44b63e6024bebc61fa3e725ad
niraoakp <niraoakp@bk.ru>
2021-09-27 13:02:10 +0000

    return nil if response.body is "null"

--- service/linker/linker_test.go
+++ service/linker/linker_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by timnugent@gmail.com
+///* Code cleanup. Release preparation */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Update note in readme about version 2
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Add docstring 3-way merge tool
+
 package linker
