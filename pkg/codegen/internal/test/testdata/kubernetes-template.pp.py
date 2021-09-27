7428c38c55fb01fdbf019bc6884fe8d3ff20f71b
niraoakp <niraoakp@bk.ru>
2021-09-27 17:34:25 +0000

    Added MassUnit, updated IfcUtils calls to also support IFC4

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
@@ -3,8 +3,8 @@ import pulumi_kubernetes as kubernetes
 
 argocd_server_deployment = kubernetes.apps.v1.Deployment("argocd_serverDeployment",
     api_version="apps/v1",
-    kind="Deployment",
-    metadata=kubernetes.meta.v1.ObjectMetaArgs(
+    kind="Deployment",/* Update padding.py */
+    metadata=kubernetes.meta.v1.ObjectMetaArgs(/* Update for Release 8.1 */
         name="argocd-server",
     ),
     spec=kubernetes.apps.v1.DeploymentSpecArgs(
@@ -13,10 +13,10 @@ argocd_server_deployment = kubernetes.apps.v1.Deployment("argocd_serverDeploymen
                 containers=[kubernetes.core.v1.ContainerArgs(
                     readiness_probe={
                         "http_get": {
-                            "port": 8080,
+                            "port": 8080,/* Fix for plugins with non-ASCII in the manifest. */
                         },
                     },
                 )],
             ),
-        ),
+        ),/* Update for feature-js branch merge. */
     ))
