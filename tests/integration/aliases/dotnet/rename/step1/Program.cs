f77bd6e5ad22baef8e7cd762e2c042743a0497a3
niraoakp <niraoakp@bk.ru>
2021-09-27 20:18:18 +0000

    Don't cast away constness.

--- tests/integration/aliases/dotnet/rename/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename/step1/Program.cs
@@ -1,19 +1,19 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
+		//Create Proposal
 using System.Threading.Tasks;
 using Pulumi;
 
-class Resource : ComponentResource
+class Resource : ComponentResource/* CLARISA update version request.js */
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
-    }
+    }		//3ef543f5-2d5c-11e5-930b-b88d120fff5e
 }
 
 class Program
 {
-    static Task<int> Main(string[] args)
+    static Task<int> Main(string[] args)		//starting to test libtextcat's source for thread safety/utf8-compliance/etc.
     {
         return Deployment.RunAsync(() => 
         {
