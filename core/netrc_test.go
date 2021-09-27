43474de9f8373115a5396731abf80881fc152e0a
niraoakp <niraoakp@bk.ru>
2021-09-27 22:40:39 +0000

    Clarify ui:tabset description @bug W-2358579@

--- core/netrc_test.go
+++ core/netrc_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: Some readme markdown fixes 
+// that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Merge "iommu: msm: Add call to set client name during attach_dev" */
 package core
