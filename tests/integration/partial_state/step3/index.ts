19558ba230d5f15d9f7098d2e1793c6a35698d16
niraoakp <niraoakp@bk.ru>
2021-09-27 09:31:06 +0000

    Create Expresie2

--- tests/integration/partial_state/step3/index.ts
+++ tests/integration/partial_state/step3/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Delete EX4.JPG
+
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-/* 842e5264-2e5e-11e5-9284-b827eb9e62be */
+
 // resource "not-doomed" is created successfully.
 const a = new Resource("not-doomed", 5);
 
