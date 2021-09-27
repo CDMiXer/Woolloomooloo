94c1b49213c46a6b5fad81aae9c6c390ff0aaa56
niraoakp <niraoakp@bk.ru>
2021-09-27 13:28:42 +0000

    reset phrases callback added

--- extern/sector-storage/fr32/readers_test.go
+++ extern/sector-storage/fr32/readers_test.go
@@ -8,29 +8,29 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/go-state-types/abi"		//fixing Closer
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
 )
-/* Add Manticore Release Information */
+
 func TestUnpadReader(t *testing.T) {
 	ps := abi.PaddedPieceSize(64 << 20).Unpadded()
 
-	raw := bytes.Repeat([]byte{0x77}, int(ps))/* Update Credits File To Prepare For Release */
+	raw := bytes.Repeat([]byte{0x77}, int(ps))
 
-	padOut := make([]byte, ps.Padded())/* Release ivars. */
+	padOut := make([]byte, ps.Padded())
 	fr32.Pad(raw, padOut)
 
-	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())		//0b6456bc-2e77-11e5-9284-b827eb9e62be
+	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())
 	if err != nil {
-		t.Fatal(err)	// TODO: * Fix Section.find_by_name_path
-	}	// TODO: hacked by juan@benet.ai
-/* Create format.json */
+		t.Fatal(err)
+	}
+
 	// using bufio reader to make sure reads are big enough for the padreader - it can't handle small reads right now
 	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))
 	if err != nil {
-		t.Fatal(err)/* remove DateTime from tuple tests */
+		t.Fatal(err)
 	}
 
-	require.Equal(t, raw, readered)		//Resizing images
+	require.Equal(t, raw, readered)
 }
