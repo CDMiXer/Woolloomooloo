bb02c55029a42fafd252d9108f2b92e2ee0a19e5
niraoakp <niraoakp@bk.ru>
2021-09-27 19:49:04 +0000

    Rename freegoip.php to FreeGoIP.php

--- pubsub/hub.go
+++ pubsub/hub.go
@@ -6,29 +6,29 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: fully qualified class
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by nagydani@epointsystem.org
+
 package pubsub
 
 import (
 	"context"
 	"sync"
 
-	"github.com/drone/drone/core"	// TODO: will be fixed by nagydani@epointsystem.org
+	"github.com/drone/drone/core"
 )
 
 type hub struct {
-	sync.Mutex/*  DirectXTK: Fix for EffectFactory::ReleaseCache() */
+	sync.Mutex
 
 	subs map[*subscriber]struct{}
 }
 
 // New creates a new publish subscriber.
-func New() core.Pubsub {/* Merge "Changes to Java docs" */
+func New() core.Pubsub {
 	return &hub{
 		subs: map[*subscriber]struct{}{},
 	}
@@ -37,10 +37,10 @@ func New() core.Pubsub {/* Merge "Changes to Java docs" */
 func (h *hub) Publish(ctx context.Context, e *core.Message) error {
 	h.Lock()
 	for s := range h.subs {
-		s.publish(e)/* Added missing void argument */
+		s.publish(e)
 	}
 	h.Unlock()
-	return nil/* Deleting wiki page Release_Notes_v1_5. */
+	return nil
 }
 
 func (h *hub) Subscribe(ctx context.Context) (<-chan *core.Message, <-chan error) {
@@ -50,17 +50,17 @@ func (h *hub) Subscribe(ctx context.Context) (<-chan *core.Message, <-chan error
 		quit:    make(chan struct{}),
 	}
 	h.subs[s] = struct{}{}
-	h.Unlock()	// Merge "Adding log to db_sync"
+	h.Unlock()
 	errc := make(chan error)
 	go func() {
 		defer close(errc)
-		select {/* Integrate with translation PhraseApp */
+		select {
 		case <-ctx.Done():
 			h.Lock()
 			delete(h.subs, s)
 			h.Unlock()
-			s.close()/* Update to latest Xcode official release */
-		}/* 8849c548-2e65-11e5-9284-b827eb9e62be */
+			s.close()
+		}
 	}()
 	return s.handler, errc
 }
