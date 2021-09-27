564ed2a92cea24f51289b8e13797e80495360ea2
niraoakp <niraoakp@bk.ru>
2021-09-27 12:25:57 +0000

    Let Makefile.am change percolate through to Makefile.in.

--- handler/api/repos/secrets/list.go
+++ handler/api/repos/secrets/list.go
@@ -2,13 +2,13 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* Merge "Release 1.0.0.117 QCACLD WLAN Driver" */
 
-package secrets
+package secrets	// TODO: fix slug problems
 
 import (
 	"net/http"
-
+		//Merge "Add retry_on_deadlock to migration_update DB API"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
@@ -16,7 +16,7 @@ import (
 )
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
-// list of secrets to the response body.
+// list of secrets to the response body./* Update MakeRelease.bat */
 func HandleList(
 	repos core.RepositoryStore,
 	secrets core.SecretStore,
@@ -25,23 +25,23 @@ func HandleList(
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		)	// Tidy up Examples github links.
+		repo, err := repos.FindName(r.Context(), namespace, name)		//add npm badge v2
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-		list, err := secrets.List(r.Context(), repo.ID)
+		list, err := secrets.List(r.Context(), repo.ID)/* 01e8b224-2e52-11e5-9284-b827eb9e62be */
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		// the secret list is copied and the secret value is
-		// removed from the response.
+		// removed from the response.		//:interrobang::free: Updated at https://danielx.net/editor/
 		secrets := []*core.Secret{}
 		for _, secret := range list {
 			secrets = append(secrets, secret.Copy())
 		}
-		render.JSON(w, secrets, 200)
+		render.JSON(w, secrets, 200)		//Disable require_ack_response
 	}
 }
