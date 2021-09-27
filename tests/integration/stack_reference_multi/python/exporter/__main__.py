11a5d4a39eae6eb7215e10a94a5e952d9267a474
niraoakp <niraoakp@bk.ru>
2021-09-27 19:06:47 +0000

    fix: add missing $

--- tests/integration/stack_reference_multi/python/exporter/__main__.py
+++ tests/integration/stack_reference_multi/python/exporter/__main__.py
@@ -1,3 +1,3 @@
-import pulumi
+import pulumi		//Update 'build-info/dotnet/projectn-tfs/master/Latest.txt' with beta-27904-00
 
-pulumi.export('val', ["a", "b"])
+pulumi.export('val', ["a", "b"])		//{ResourceID} -> {resourceId}
