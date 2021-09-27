a2678fae7a5b6d461c918c63a63b5c741745ceaa
niraoakp <niraoakp@bk.ru>
2021-09-27 12:35:05 +0000

    docs: note correct minimum GHC version in dev docs

--- handler/web/logout_test.go
+++ handler/web/logout_test.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//damn Markdown :D
+
 package web
 
-import (		//Commmented failing test. This test is not implemented and fails automatically.
-	"net/http/httptest"	// TODO: Merge "Merge with neutron master branch changes"
+import (
+	"net/http/httptest"
 	"testing"
 )
 
@@ -19,7 +19,7 @@ func TestLogout(t *testing.T) {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	if got, want := w.Header().Get("Set-Cookie"), "_session_=deleted; Path=/; Max-Age=0"; want != got {		//release 0.8.2.
+	if got, want := w.Header().Get("Set-Cookie"), "_session_=deleted; Path=/; Max-Age=0"; want != got {
 		t.Errorf("Want response code %q, got %q", want, got)
 	}
 }
