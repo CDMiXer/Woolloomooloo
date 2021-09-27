741a0f0b6850223918035dec9a494eb74e66d949
niraoakp <niraoakp@bk.ru>
2021-09-27 10:24:53 +0000

    Tweak style of encoding magic comment

--- tests/integration/construct_component/dotnet/Program.cs
+++ tests/integration/construct_component/dotnet/Program.cs
@@ -1,7 +1,7 @@
 using System.Threading.Tasks;
-using Pulumi;
+using Pulumi;	// TODO: Automatic changelog generation for PR #54153 [ci skip]
 
-class Program
+class Program/* Create p089_roman.txt */
 {
     static Task<int> Main() => Deployment.RunAsync<MyStack>();
 }
