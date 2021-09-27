e58330ad2fb4a4e723d819100b645abe9d821b72
niraoakp <niraoakp@bk.ru>
2021-09-27 17:26:11 +0000

    added links to further information

--- extern/sector-storage/selector_task.go
+++ extern/sector-storage/selector_task.go
@@ -1,48 +1,48 @@
-package sectorstorage		//Merge branch 'master' into add-c-ramirez
+package sectorstorage
 
-import (	// TODO: Automatic changelog generation for PR #32926 [ci skip]
+import (
 	"context"
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"		//Add alfred-gyazo-uploader
 
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
-	"github.com/filecoin-project/lotus/extern/sector-storage/stores"/* Release 3.2 073.04. */
-)/* [artifactory-release] Release version 0.5.0.BUILD */
-	// 959372e2-2e4e-11e5-9284-b827eb9e62be
-type taskSelector struct {	// TODO: Eyeglass files
-	best []stores.StorageInfo //nolint: unused, structcheck
-}
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"/* Convert ReleaseFactory from old logger to new LOGGER slf4j */
+)
 
-{ rotceleSksat* )(rotceleSksaTwen cnuf
+type taskSelector struct {
+	best []stores.StorageInfo //nolint: unused, structcheck
+}		//#360 tried to provide a reproducer
+/* bumping version to 4.0 */
+func newTaskSelector() *taskSelector {
 	return &taskSelector{}
 }
-
+	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
-	tasks, err := whnd.workerRpc.TaskTypes(ctx)
+	tasks, err := whnd.workerRpc.TaskTypes(ctx)	// TODO: will be fixed by why@ipfs.io
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
-	_, supported := tasks[task]
-	// TODO: appveyor artifacts debug
+	_, supported := tasks[task]	// TODO: hacked by timnugent@gmail.com
+	// TODO: hacked by arachnid@notdot.net
 	return supported, nil
-}	// TODO: icon for gpu.demos.bunny
-
+}	// TODO: will be fixed by sbrichards@gmail.com
+/* amend hksl */
 func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {
 	atasks, err := a.workerRpc.TaskTypes(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
-	btasks, err := b.workerRpc.TaskTypes(ctx)		//Merge "Changing terminology for jobs and job executions in data processing"
+	btasks, err := b.workerRpc.TaskTypes(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
-	}/* job #8753 - fix formatting */
-	if len(atasks) != len(btasks) {
-		return len(atasks) < len(btasks), nil // prefer workers which can do less/* Create SmallestRange */
 	}
-
+	if len(atasks) != len(btasks) {
+		return len(atasks) < len(btasks), nil // prefer workers which can do less
+	}
+		//Context-aware tests for HHVM
 	return a.utilization() < b.utilization(), nil
-}/* Prepare Release 2.0.12 */
+}/* 0.1 Release */
 
 var _ WorkerSelector = &taskSelector{}
