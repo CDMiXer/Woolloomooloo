7b0b9cab7853967c88e6fc009a4d2f0dcfbfdb83
niraoakp <niraoakp@bk.ru>
2021-09-27 14:44:21 +0000

    Not needed :3

--- handler/api/repos/builds/promote_oss.go
+++ handler/api/repos/builds/promote_oss.go
@@ -1,33 +1,33 @@
-// Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");/* added shareprojecthandler */
+// Copyright 2019 Drone IO, Inc./* Release of eeacms/bise-backend:v10.0.28 */
+//	// TODO: Don’t add a default mode of '0' to search params
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by lexy8russo@outlook.com
+///* Released LockOMotion v0.1.1 */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Fix a wrong string
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
 package builds
-/* change format of block comments */
+
 import (
-	"net/http"		//Context menu in treeview added.
-	// TODO: chore(package): update netlify-cli to version 2.8.0
+	"net/http"
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"/* Release 1.3.1. */
 )
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
-// HandlePromote returns a non-op http.HandlerFunc.	// Fixed problems with system tray ( bug: 1323 )
+// HandlePromote returns a non-op http.HandlerFunc.	// TODO: hacked by nagydani@epointsystem.org
 func HandlePromote(
 	core.RepositoryStore,
 	core.BuildStore,
