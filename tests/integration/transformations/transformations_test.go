f1645b59ee1b8acd1898db20ab7f277bb43263c8
niraoakp <niraoakp@bk.ru>
2021-09-27 01:10:14 +0000

    adding d3 javascript lib

--- tests/integration/transformations/transformations_test.go
+++ tests/integration/transformations/transformations_test.go
@@ -1,84 +1,84 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO:  - Blit bitmap working, RAW load working.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Moved RepeatingReleasedEventsFixer to 'util' package */
+
 package ints
 
-import (/* sink_rubber: migrate to CancellablePointer */
+import (		//Merge "Build boost for windows"
 	"testing"
-/* do not assume that the devdir is in home */
+/* Update cached sequence numbers during setSequence() */
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/stretchr/testify/assert"
-)
+)		//Passage en version 1.5.0 pour webappwatcher
 
 var Dirs = []string{
 	"simple",
 }
-
-func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+/* bad markup fixed */
+func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {/* Make all headers public */
 	dynamicResName := "pulumi-" + language + ":dynamic:Resource"
 	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 		foundRes1 := false
 		foundRes2Child := false
 		foundRes3 := false
-		foundRes4Child := false/* added/updated copyright notice */
-		foundRes5Child := false	// TODO: hacked by arajasek94@gmail.com
+		foundRes4Child := false	// open-nars 1.5.0 fixed
+		foundRes5Child := false
 		for _, res := range stack.Deployment.Resources {
 			// "res1" has a transformation which adds additionalSecretOutputs
-			if res.URN.Name() == "res1" {	// e57b07e2-2e4f-11e5-9284-b827eb9e62be
+			if res.URN.Name() == "res1" {
 				foundRes1 = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
 			}
 			// "res2" has a transformation which adds additionalSecretOutputs to it's
 			// "child"
-			if res.URN.Name() == "res2-child" {
+			if res.URN.Name() == "res2-child" {		//Pushing last known good BenCmd backup
 				foundRes2Child = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
-				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))		//UPD: New JSON-Object api for particle sensors
+				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output2"))
 			}
 			// "res3" is impacted by a global stack transformation which sets
 			// optionalDefault to "stackDefault"
 			if res.URN.Name() == "res3" {
-				foundRes3 = true/* Merge "Add TripleO stable/liberty to gerritbot channels" */
+				foundRes3 = true/* Finalization of v2.0. Release */
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
-				optionalInput := res.Inputs["optionalInput"]
-				assert.NotNil(t, optionalInput)/* Delete Objetivos Luis B.R.md~ */
+				optionalInput := res.Inputs["optionalInput"]/* 9fc53050-2e41-11e5-9284-b827eb9e62be */
+				assert.NotNil(t, optionalInput)
 				assert.Equal(t, "stackDefault", optionalInput.(string))
 			}
 			// "res4" is impacted by two component parent transformations which set
 			// optionalDefault to "default1" and then "default2" and also a global stack
 			// transformation which sets optionalDefault to "stackDefault".  The end
 			// result should be "stackDefault".
-			if res.URN.Name() == "res4-child" {
+			if res.URN.Name() == "res4-child" {/* Delete Titain Robotics Release 1.3 Beta.zip */
 				foundRes4Child = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
-				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
+				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))/* ZAPI-226: Optimization */
 				optionalInput := res.Inputs["optionalInput"]
-				assert.NotNil(t, optionalInput)
-				assert.Equal(t, "stackDefault", optionalInput.(string))
+				assert.NotNil(t, optionalInput)	// TODO: will be fixed by alan.shaw@protocol.ai
+				assert.Equal(t, "stackDefault", optionalInput.(string))/* Fixed the Release H configuration */
 			}
 			// "res5" modifies one of its children to depend on another of its children.
 			if res.URN.Name() == "res5-child1" {
-				foundRes5Child = true		//a4ab893c-2e57-11e5-9284-b827eb9e62be
+				foundRes5Child = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				// TODO[pulumi/pulumi#3282] Due to this bug, the dependency information
 				// will not be correctly recorded in the state file, and so cannot be
-				// verified here.	// ARM NEON improve factoring a bit. No functional change.
+				// verified here.
 				//
 				// assert.Len(t, res.PropertyDependencies, 1)
 				input := res.Inputs["input"]
-				assert.NotNil(t, input)	// TODO: rebuilt with @harishvc added!
+				assert.NotNil(t, input)/* BigQuery : update links based on query docs refactoring */
 				assert.Equal(t, "b", input.(string))
 			}
-		}
+		}/* create, suggested by Godspiral */
 		assert.True(t, foundRes1)
 		assert.True(t, foundRes2Child)
-		assert.True(t, foundRes3)/* Release 2.7 */
-		assert.True(t, foundRes4Child)/* Update ReleaseNotes.json */
+		assert.True(t, foundRes3)
+		assert.True(t, foundRes4Child)
 		assert.True(t, foundRes5Child)
 	}
 }
