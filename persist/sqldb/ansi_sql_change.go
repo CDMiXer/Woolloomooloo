5655988b7f0725b1604a396389dbada6a6e4c7db
niraoakp <niraoakp@bk.ru>
2021-09-27 15:47:17 +0000

    Move the connection::status from std::string to private enum

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -1,11 +1,11 @@
-package sqldb/* Merge "Remove NovaConsoleauth Service" */
+package sqldb
 
 import "upper.io/db.v3/lib/sqlbuilder"
 
 // represent a straight forward change that is compatible with all database providers
-type ansiSQLChange string/* Release of eeacms/www:18.2.19 */
-/* 3fe290bd-2d5c-11e5-9493-b88d120fff5e */
+type ansiSQLChange string
+
 func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
-	_, err := session.Exec(string(s))
+	_, err := session.Exec(string(s))/* Merge "Fix folder creation at quickstart" */
 	return err
 }
