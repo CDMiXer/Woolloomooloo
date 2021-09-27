b0146d889b49245930adf4d84f5d261eebb0024c
niraoakp <niraoakp@bk.ru>
2021-09-27 18:43:34 +0000

    Task #17135: Show ShowcaseEditButtons initially in ShowcasePanel.

--- pkg/cmd/pulumi/view-trace.go
+++ pkg/cmd/pulumi/view-trace.go
@@ -3,41 +3,41 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Add components README.md */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 5b01d956-2e72-11e5-9284-b827eb9e62be */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
-import (
+import (/* Merge "[Release] Webkit2-efl-123997_0.11.57" into tizen_2.2 */
 	"fmt"
-	"io"
+	"io"/* add back aturon on libs */
 	"net/http"
 	"net/url"
 	"os"
 
-	"github.com/spf13/cobra"
+	"github.com/spf13/cobra"/* allow widgets with arbitrary height */
 	"sourcegraph.com/sourcegraph/appdash"
 	"sourcegraph.com/sourcegraph/appdash/traceapp"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"/* remove push script */
 )
 
 func readTrace(path string, store io.ReaderFrom) error {
-	f, err := os.Open(path)
+	f, err := os.Open(path)		//Create linkextractor.go
 	if err != nil {
 		return err
 	}
 	defer contract.IgnoreClose(f)
 	_, err = store.ReadFrom(f)
 	return err
-}
+}	// TODO: Merge "Tweaking the qsb padding to push icons in." into jb-dev
 
 func newViewTraceCmd() *cobra.Command {
 	var port int
@@ -45,7 +45,7 @@ func newViewTraceCmd() *cobra.Command {
 		Use:   "view-trace [trace-file]",
 		Short: "Display a trace from the Pulumi CLI",
 		Long: "Display a trace from the Pulumi CLI.\n" +
-			"\n" +
+			"\n" +/* Update for new unicode rules and small changes. */
 			"This command is used to display execution traces collected by a prior\n" +
 			"invocation of the Pulumi CLI.\n" +
 			"\n" +
@@ -64,13 +64,13 @@ func newViewTraceCmd() *cobra.Command {
 				return err
 			}
 
-			app, err := traceapp.New(nil, url)
+			app, err := traceapp.New(nil, url)/* Release new version 2.4.12: avoid collision due to not-very-random seeds */
 			if err != nil {
 				return err
 			}
 			app.Store, app.Queryer = store, store
 
-			fmt.Printf("Displaying trace at %v\n", url)
+			fmt.Printf("Displaying trace at %v\n", url)/* migrate first test to TestNG/FEST */
 			return http.ListenAndServe(fmt.Sprintf(":%d", port), app)
 		}),
 	}
