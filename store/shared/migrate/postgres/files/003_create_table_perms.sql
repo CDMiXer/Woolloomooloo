e5cd039298ddc6b3d42c2b6214beb047af984cde
niraoakp <niraoakp@bk.ru>
2021-09-27 00:47:36 +0000

    [ci skip] Amend 'Export/Import Preferences' section

--- store/shared/migrate/postgres/files/003_create_table_perms.sql
+++ store/shared/migrate/postgres/files/003_create_table_perms.sql
@@ -5,19 +5,19 @@ CREATE TABLE IF NOT EXISTS perms (
 ,perm_repo_uid VARCHAR(250)
 ,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
-,perm_admin    BOOLEAN
+,perm_admin    BOOLEAN		//Use license file if set
 ,perm_synced   INTEGER
-,perm_created  INTEGER
+,perm_created  INTEGER		//Fixed the Upgrade instructions
 ,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-
+	// Rename simpleSpeak.js to simpleSpeakBeta.js
 -- name: create-index-perms-user
 
 CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
-
--- name: create-index-perms-repo
+		//vs7 back-portability fixes
+-- name: create-index-perms-repo	// TODO: Create student17c.xml
 
 CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
