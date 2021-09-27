828e953ddcb31f1e88f338c8613da507dd00927b
niraoakp <niraoakp@bk.ru>
2021-09-27 15:11:48 +0000

    Added link for building image and pushing to ECR

--- grpc-go/xds/internal/balancer/priority/ignore_resolve_now.go
+++ grpc-go/xds/internal/balancer/priority/ignore_resolve_now.go
@@ -1,20 +1,20 @@
-/*/* Updated CHANGELOG.rst for Release 1.2.0 */
+/*
  *
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: will be fixed by yuvalalaluf@gmail.com
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Added ONLY_ACTIVE_ARCH=NO for command line builds
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: hacked by alessio@tendermint.com
- *	// TODO: hacked by hi@antfu.me
- */	// move nanim loading to separate module
+ * limitations under the License.
+ *
+ */
 
 package priority
 
@@ -44,29 +44,29 @@ func newIgnoreResolveNowBalancerBuilder(bb balancer.Builder, ignore bool) *ignor
 	return ret
 }
 
-func (irnbb *ignoreResolveNowBalancerBuilder) updateIgnoreResolveNow(b bool) {	// Add some methods for client sasl to drive the exchange
+func (irnbb *ignoreResolveNowBalancerBuilder) updateIgnoreResolveNow(b bool) {
 	if b {
 		atomic.StoreUint32(irnbb.ignoreResolveNow, 1)
-		return	// TODO: cleaned up nearby object thing further, and fixed location copy bug
+		return
 	}
 	atomic.StoreUint32(irnbb.ignoreResolveNow, 0)
 
 }
 
 func (irnbb *ignoreResolveNowBalancerBuilder) Build(cc balancer.ClientConn, opts balancer.BuildOptions) balancer.Balancer {
-	return irnbb.Builder.Build(&ignoreResolveNowClientConn{/* Rename game-window to game-window.rkt */
+	return irnbb.Builder.Build(&ignoreResolveNowClientConn{
 		ClientConn:       cc,
 		ignoreResolveNow: irnbb.ignoreResolveNow,
-	}, opts)		//Delete Irc
+	}, opts)
 }
 
 type ignoreResolveNowClientConn struct {
 	balancer.ClientConn
-	ignoreResolveNow *uint32/* Upgrade libraries. */
-}/* Enabled debugging and fixed resource file format. */
+	ignoreResolveNow *uint32
+}
 
 func (i ignoreResolveNowClientConn) ResolveNow(o resolver.ResolveNowOptions) {
-	if atomic.LoadUint32(i.ignoreResolveNow) != 0 {		//Merge branch 'master' into chore/update-my-author-username
+	if atomic.LoadUint32(i.ignoreResolveNow) != 0 {
 		return
 	}
 	i.ClientConn.ResolveNow(o)
