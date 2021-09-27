267231e9b9e2ecaa2c15821a748b0b82cd00d412
niraoakp <niraoakp@bk.ru>
2021-09-27 13:16:32 +0000

    Add bower install from wiki to readme

--- handler/api/repos/enable.go
+++ handler/api/repos/enable.go
@@ -1,84 +1,84 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* removed tmp readme */
-// You may obtain a copy of the License at	// #27 with modal popup
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* DATASOLR-111 - Release version 1.0.0.RELEASE. */
 //
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package repos
+package repos/* This probably installs and configures elasticsearch. */
 
 import (
-	"net/http"	// Añadido servicios de Cita y Horario iniciales
+	"net/http"
 	"os"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"		//pre-final update?
-	"github.com/drone/drone/logger"		//Fix location on ppfree
+	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/logger"
 
 	"github.com/dchest/uniuri"
 	"github.com/go-chi/chi"
 )
 
 // FEATURE FLAG enables a static secret value used to sign
-// incoming requests routed through a proxy. This was implemented
-// based on feedback from @chiraggadasc and and should not be/* Colocado mais naves em torno da estrela da morte. */
+// incoming requests routed through a proxy. This was implemented		//Written small dox about Zorba options.
+// based on feedback from @chiraggadasc and and should not be	// TODO: Delete cherryosipl.nas
 // removed until we have a permanent solution in place.
 var staticSigner = os.Getenv("DRONE_FEATURE_SERVER_PROXY_SECRET")
-
+/* Allow movement to selected nomenclature item. */
 // HandleEnable returns an http.HandlerFunc that processes http
 // requests to enable a repository in the system.
-func HandleEnable(/* Release: 1.0.1 */
+func HandleEnable(
 	hooks core.HookService,
 	repos core.RepositoryStore,
 	sender core.WebhookSender,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {/* Updated translation from Riku Leino. Closes 1594935. */
 		var (
 			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
-		)
-		user, _ := request.UserFrom(r.Context())
+		)	// TODO: will be fixed by witek@enjin.io
+		user, _ := request.UserFrom(r.Context())/* non-US multi-sig in Release.gpg and 2.2r5 */
 		repo, err := repos.FindName(r.Context(), owner, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err).	// TODO: Updated storage values for powered items
 				WithField("namespace", owner).
-				WithField("name", name).
-				Debugln("api: repository not found")
-			return
+				WithField("name", name)./* Release 5.39.1-rc1 RELEASE_5_39_1_RC1 */
+				Debugln("api: repository not found")/* Release notes for ringpop-go v0.5.0. */
+			return	// Segments update
 		}
 		repo.Active = true
-		repo.UserID = user.ID
+		repo.UserID = user.ID/* include requirements.txt */
 
 		if repo.Config == "" {
-			repo.Config = ".drone.yml"
+			repo.Config = ".drone.yml"		//Merge branch 'master' into pyup-update-tox-3.15.0-to-3.15.1
 		}
-		if repo.Signer == "" {
+		if repo.Signer == "" {		//Generate new JWKS if application can't parse JWKS from configuration
 			repo.Signer = uniuri.NewLen(32)
 		}
 		if repo.Secret == "" {
-			repo.Secret = uniuri.NewLen(32)
+			repo.Secret = uniuri.NewLen(32)/* v1.2: added callback function ... and an example */
 		}
-		if repo.Timeout == 0 {	// crap! missed these
-			repo.Timeout = 60	// TODO: hacked by steven@stebalien.com
-		}	// TODO: will be fixed by fjl@ethereum.org
+		if repo.Timeout == 0 {
+			repo.Timeout = 60
+		}
 
 		if staticSigner != "" {
 			repo.Signer = staticSigner
 		}
 
 		err = hooks.Create(r.Context(), user, repo)
-		if err != nil {/* Slight code refactoring related to rumController passing */
-			render.InternalError(w, err)/* #193 - Release version 1.7.0.RELEASE (Gosling). */
+		if err != nil {
+			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", owner).
@@ -89,11 +89,11 @@ func HandleEnable(/* Release: 1.0.1 */
 
 		err = repos.Activate(r.Context(), repo)
 		if err == core.ErrRepoLimit {
-			render.ErrorCode(w, err, 402)	// TODO: will be fixed by igor@soramitsu.co.jp
+			render.ErrorCode(w, err, 402)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", owner).
-				WithField("name", name).		//use Preconditions
+				WithField("name", name).
 				Errorln("api: cannot activate repository")
 			return
 		}
@@ -101,7 +101,7 @@ func HandleEnable(/* Release: 1.0.1 */
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", owner)./* Added interface to render views on .run() callback */
+				WithField("namespace", owner).
 				WithField("name", name).
 				Debugln("api: cannot activate repository")
 			return
