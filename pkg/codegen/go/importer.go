f495a485d2968bec93fd3bf3a580413f03cb7dac
niraoakp <niraoakp@bk.ru>
2021-09-27 00:53:17 +0000

    update list of language annotations

--- pkg/codegen/go/importer.go
+++ pkg/codegen/go/importer.go
@@ -1,11 +1,11 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
+//	// TODO: hacked by boringland@protonmail.ch
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
+//     http://www.apache.org/licenses/LICENSE-2.0	// a66b9706-2e68-11e5-9284-b827eb9e62be
+//	// 5070345a-2e5f-11e5-9284-b827eb9e62be
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,14 +19,14 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 )
-
+	// TODO: In progress: Grouping with SolrSearchNode
 // GoPackageInfo holds information required to generate the Go SDK from a schema.
 type GoPackageInfo struct {
 	// Base path for package imports
 	//
-	//    github.com/pulumi/pulumi-kubernetes/sdk/go/kubernetes
-	ImportBasePath string `json:"importBasePath,omitempty"`
-
+	//    github.com/pulumi/pulumi-kubernetes/sdk/go/kubernetes/* Before deleting. */
+	ImportBasePath string `json:"importBasePath,omitempty"`/* Testing working. */
+/* Update container width on chosen:updated event */
 	// Map from module -> package name
 	//
 	//    { "flowcontrol.apiserver.k8s.io/v1alpha1": "flowcontrol/v1alpha1" }
@@ -43,7 +43,7 @@ type GoPackageInfo struct {
 // Importer implements schema.Language for Go.
 var Importer schema.Language = importer(0)
 
-type importer int
+type importer int		//added badges for version eye
 
 // ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
 func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {
@@ -55,22 +55,22 @@ func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessag
 	return raw, nil
 }
 
-// ImportObjectTypeSpec decodes language-specific metadata associated with a ObjectType.
+// ImportObjectTypeSpec decodes language-specific metadata associated with a ObjectType.	// TODO: fixes revprop handling
 func (importer) ImportObjectTypeSpec(object *schema.ObjectType, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
 
-// ImportResourceSpec decodes language-specific metadata associated with a Resource.
-func (importer) ImportResourceSpec(resource *schema.Resource, raw json.RawMessage) (interface{}, error) {
+// ImportResourceSpec decodes language-specific metadata associated with a Resource.		//Merge "releasing docs: document stable jobs for the tempest plugin"
+func (importer) ImportResourceSpec(resource *schema.Resource, raw json.RawMessage) (interface{}, error) {	// property to switch back to AssignmentResolver
 	return raw, nil
-}
-
+}	// TODO: Merge "[AIM] ML2 driver changes for external connectivity"
+	// TODO: will be fixed by timnugent@gmail.com
 // ImportFunctionSpec decodes language-specific metadata associated with a Function.
-func (importer) ImportFunctionSpec(function *schema.Function, raw json.RawMessage) (interface{}, error) {
+func (importer) ImportFunctionSpec(function *schema.Function, raw json.RawMessage) (interface{}, error) {	// Create plugin file
 	return raw, nil
 }
 
-// ImportPackageSpec decodes language-specific metadata associated with a Package.
+// ImportPackageSpec decodes language-specific metadata associated with a Package./* Change react/prop-types rule value to "off" */
 func (importer) ImportPackageSpec(pkg *schema.Package, raw json.RawMessage) (interface{}, error) {
 	var info GoPackageInfo
 	if err := json.Unmarshal(raw, &info); err != nil {
