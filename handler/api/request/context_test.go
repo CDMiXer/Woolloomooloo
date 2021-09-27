163cfcd813a4028271f095cd49b7296aa5107eaf
niraoakp <niraoakp@bk.ru>
2021-09-27 01:31:40 +0000

    Fix 4.2.M6 compile error

--- handler/api/request/context_test.go
+++ handler/api/request/context_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: CrazyCore: fixed NPEs in FlatDatabases
+// Use of this source code is governed by the Drone Non-Commercial License/* Final edits. */
 // that can be found in the LICENSE file.
 
-package request/* Merge "Allow Creation of Branches by Project Release Team" */
+package request
