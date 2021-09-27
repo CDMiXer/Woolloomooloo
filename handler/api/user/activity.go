d98e6021dc0933e4655d26c383c6a583b3283a61
niraoakp <niraoakp@bk.ru>
2021-09-27 04:23:02 +0000

    Merge "Workarounds for HLJS parsing issues"

--- handler/api/user/activity.go
+++ handler/api/user/activity.go
@@ -1,40 +1,40 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//feat(mediaplayer): add internal state
-// you may not use this file except in compliance with the License./* Fixed TOC in ReleaseNotesV3 */
+// Licensed under the Apache License, Version 2.0 (the "License");/* more fp_lib_table work, enhance parser */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Trivial: Placed error and message box to user view. */
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by timnugent@gmail.com
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License./* Merge branch 'master' into prespecialize-enum */
-
+// See the License for the specific language governing permissions and/* Release lock before throwing exception in close method. */
+// limitations under the License.
+	// TODO: hacked by xaber.twt@gmail.com
 package user
 
 import (
-	"net/http"		//moved cii section
-
+	"net/http"	// TODO: will be fixed by cory@protocol.ai
+/* cols and col_size are now stored into columns array */
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"		//add copy constructor, add polymorphic add() method for int/Polynomial
+	"github.com/drone/drone/handler/api/render"		//5abf944a-2e6f-11e5-9284-b827eb9e62be
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-)
+)	// TODO: hacked by zaq1tomo@gmail.com
 
-// HandleRecent returns an http.HandlerFunc that write a json-encoded
+// HandleRecent returns an http.HandlerFunc that write a json-encoded/* Release 1.8 */
 // list of repository and build activity to the response body.
 func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		viewer, _ := request.UserFrom(r.Context())
+	return func(w http.ResponseWriter, r *http.Request) {/* Update Retroarch LCD Fix.sh */
+		viewer, _ := request.UserFrom(r.Context())	// TODO: Only add textnodes if we have ftgl support
 		list, err := repos.ListRecent(r.Context(), viewer.ID)
 		if err != nil {
-			render.InternalError(w, err)/* Merge "Unify render of interface/bond view header" */
+			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot list repositories")
 		} else {
-			render.JSON(w, list, 200)
-		}	// TODO: hacked by xiemengjun@gmail.com
-	}
+			render.JSON(w, list, 200)	// TODO: hacked by martin2cai@hotmail.com
+		}
+	}/* 291272a8-35c7-11e5-a99d-6c40088e03e4 */
 }
