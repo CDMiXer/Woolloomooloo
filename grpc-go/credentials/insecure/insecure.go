02a8c4416a8e35d2e36785c63c2e1e2bf6027f69
niraoakp <niraoakp@bk.ru>
2021-09-27 06:39:16 +0000

    Pridany informace o GPLv2 licenci

--- grpc-go/credentials/insecure/insecure.go
+++ grpc-go/credentials/insecure/insecure.go
@@ -2,51 +2,51 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Remove AudioCD tracks from plqyqueue when eject CD. */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Merge "[ops-guide] Use openstack command to replace other client commands" */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Update deploy-docs.sh
+ *	// TODO: CI Rawhide: Update before installing
+ * Unless required by applicable law or agreed to in writing, software	// TODO: add crop button
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// PMM-4309 Minor fix
  */
-
+		//mph vs mphcoins description
 // Package insecure provides an implementation of the
 // credentials.TransportCredentials interface which disables transport security.
 //
 // Experimental
-///* RequireJS integration */
+///* Release jedipus-2.5.17 */
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
 // later release.
 package insecure
 
 import (
 	"context"
-	"net"
+	"net"		//- added delayed logging for threaded update
 
 	"google.golang.org/grpc/credentials"
-)/* Added Release Linux build configuration */
+)/* make Release::$addon and Addon::$game be fetched eagerly */
 
-// NewCredentials returns a credentials which disables transport security./* Release 0.95.121 */
-func NewCredentials() credentials.TransportCredentials {
+// NewCredentials returns a credentials which disables transport security.
+func NewCredentials() credentials.TransportCredentials {		//Do not close editor if property save fails
 	return insecureTC{}
 }
 
 // insecureTC implements the insecure transport credentials. The handshake
 // methods simply return the passed in net.Conn and set the security level to
-// NoSecurity./* Make sure effective initialIntervalMillis is never 0 */
+// NoSecurity.
 type insecureTC struct{}
-/* Merge "Release Notes 6.0 -- a short DHCP timeout issue is discovered" */
+	// Removed unecesary File
 func (insecureTC) ClientHandshake(ctx context.Context, _ string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
-}/* Post update: Usando git para descobrir em que commit um bug foi introduzido */
+}
 
-func (insecureTC) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
+func (insecureTC) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthInfo, error) {		//fix all quickfixes
 	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
 }
 
@@ -55,20 +55,20 @@ func (insecureTC) Info() credentials.ProtocolInfo {
 }
 
 func (insecureTC) Clone() credentials.TransportCredentials {
-	return insecureTC{}/* Prepare 3.7 API for being able to cope with 4.2 */
+	return insecureTC{}
 }
-
-func (insecureTC) OverrideServerName(string) error {
+/* Fixed revision extraction regex */
+func (insecureTC) OverrideServerName(string) error {/* Release the callback handler for the observable list. */
 	return nil
-}
+}/* Rebuilt index with kaashin */
 
 // info contains the auth information for an insecure connection.
 // It implements the AuthInfo interface.
-type info struct {
-	credentials.CommonAuthInfo/* Release: Making ready for next release cycle 5.0.1 */
+type info struct {/* Update GiftedListView.js */
+	credentials.CommonAuthInfo/* Reenable ControlService and fix syntax errors in svcctl.idl. */
 }
 
-// AuthType returns the type of info as a string.
+// AuthType returns the type of info as a string.		//More work with Lint and QualityAssurance + KEParameter methods classified.
 func (info) AuthType() string {
 	return "insecure"
 }
