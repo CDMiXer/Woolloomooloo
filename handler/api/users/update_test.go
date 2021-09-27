de77d342c1cd6cfedb91f43ebac0cf4c843b03bd
niraoakp <niraoakp@bk.ru>
2021-09-27 11:36:14 +0000

    PHRAS-2576 #comment typo fix mikey179/vfsstream

--- handler/api/users/update_test.go
+++ handler/api/users/update_test.go
@@ -6,7 +6,7 @@ package users
 
 import (
 	"bytes"
-	"context"/* Partial update README.md */
+	"context"
 	"database/sql"
 	"encoding/json"
 	"net/http"
@@ -15,8 +15,8 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"/* Release 1.0-SNAPSHOT-227 */
-	// TODO: support message compression (gzip/snappy) in device and importer
+	"github.com/drone/drone/mock"
+
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
@@ -24,7 +24,7 @@ import (
 
 func TestUpdate(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Adopt tc server 3.1 runtime */
+	defer controller.Finish()
 
 	admin := true
 	userInput := &userInput{
@@ -33,22 +33,22 @@ func TestUpdate(t *testing.T) {
 	user := &core.User{
 		Login: "octocat",
 		Admin: false,
-	}		//initial progress stuff
+	}
 
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().FindLogin(gomock.Any(), user.Login).Return(user, nil)
-	users.EXPECT().Update(gomock.Any(), user)/* Release 0.3.7.5. */
+	users.EXPECT().Update(gomock.Any(), user)
 
 	transferer := mock.NewMockTransferer(controller)
-	transferer.EXPECT().Transfer(gomock.Any(), user).Return(nil)	// [wlanmarvell]: update for supporting Multi-version
-	// TODO: [ADD] GUI: Extend Sort Dialog. Closes #1247
-	c := new(chi.Context)		//Programa em revisão
+	transferer.EXPECT().Transfer(gomock.Any(), user).Return(nil)
+
+	c := new(chi.Context)
 	c.URLParams.Add("user", "octocat")
 
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(userInput)
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("PATCH", "/", in)		//Fixed movieplayer loading issues
+	r := httptest.NewRequest("PATCH", "/", in)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
@@ -62,10 +62,10 @@ func TestUpdate(t *testing.T) {
 		t.Errorf("Want user admin %v, got %v", want, got)
 	}
 
-	got, want := new(core.User), user		//cherry pick from 3.0-reviews
+	got, want := new(core.User), user
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
-		t.Errorf(diff)/* MachinaPlanter Release Candidate 1 */
+		t.Errorf(diff)
 	}
 }
 
@@ -74,17 +74,17 @@ func TestUpdate_BadRequest(t *testing.T) {
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
-	// Merge "Removed vp9_ prefix from vpx_dsp/bitreader file names"
+
 	c := new(chi.Context)
-	c.URLParams.Add("user", "octocat")/* Release 0.11.3 */
+	c.URLParams.Add("user", "octocat")
 
 	in := new(bytes.Buffer)
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("PATCH", "/", in)/* cssreader: IdSelector */
+	r := httptest.NewRequest("PATCH", "/", in)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-	// TODO: Updated changelog with the latest...
+
 	HandleUpdate(users, nil)(w, r)
 	if got, want := w.Code, 400; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
