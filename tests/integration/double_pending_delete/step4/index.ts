f7d359da30d8d95a542af80cfe6e266bf7795d79
niraoakp <niraoakp@bk.ru>
2021-09-27 08:24:03 +0000

    Add command for booting up Jupyter notebook

--- tests/integration/double_pending_delete/step4/index.ts
+++ tests/integration/double_pending_delete/step4/index.ts
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+//		//Merge "[FEAT] Enable iprange with site.blocks generator"
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Create ebooks.md */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,12 +13,12 @@
 // limitations under the License.
 
 import { Resource } from "./resource";
-
-// We'll complete our disaster recovery by triggering replacements of A and B again,
+/* Fix bug in sorting using icu sort_key */
+// We'll complete our disaster recovery by triggering replacements of A and B again,/* Release 1.15 */
 // but this time the replacement of B will succeed.
 // The engine should generate:
 //
-// Delete A
+// Delete A		//added Advent of the Wurm
 // Create A (mark old A as pending delete)
 const a = new Resource("a", { fail: 4 });
 
@@ -27,7 +27,7 @@ const b = new Resource("b", { fail: 2 }, { dependsOn: a });
 
 // Delete A
 // Delete B
-
+/* Delete Python Setup & Usage - Release 2.7.13.pdf */
 // Like the last step, this is interesting because we delete A's URN three times in the same plan.
 // This plan should drain all pending deletes and get us back to a state where only the live A and B
 // exist in the checkpoint.
