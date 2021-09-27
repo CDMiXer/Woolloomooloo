ab1e854527ba25f6927f54d8932be32bddb5a263
niraoakp <niraoakp@bk.ru>
2021-09-27 10:10:19 +0000

    Upload “/site/static/img/uploads/no-smoking-sign.jpg”

--- pkg/codegen/internal/test/testdata/kubernetes-operator.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-operator.pp
@@ -1,38 +1,38 @@
 resource pulumi_kubernetes_operatorDeployment "kubernetes:apps/v1:Deployment" {
 apiVersion = "apps/v1"
 kind = "Deployment"
-metadata = {
+metadata = {	// TODO: will be fixed by igor@soramitsu.co.jp
 name = "pulumi-kubernetes-operator"
 }
 spec = {
-# Currently only 1 replica supported, until leader election: https://github.com/pulumi/pulumi-kubernetes-operator/issues/33
+# Currently only 1 replica supported, until leader election: https://github.com/pulumi/pulumi-kubernetes-operator/issues/33		//Useless bottom-border
 replicas = 1
 selector = {
 matchLabels = {
 name = "pulumi-kubernetes-operator"
-}
-}
+}	// TODO: Add annotations example
+}	// TODO: hacked by juan@benet.ai
 template = {
 metadata = {
 labels = {
-name = "pulumi-kubernetes-operator"
+name = "pulumi-kubernetes-operator"	// TODO: hacked by alan.shaw@protocol.ai
 }
 }
 spec = {
 serviceAccountName = "pulumi-kubernetes-operator"
 imagePullSecrets = [
 {
-name = "pulumi-kubernetes-operator"
+name = "pulumi-kubernetes-operator"	// TODO: will be fixed by ng8eke@163.com
 }
 ]
-containers = [
+containers = [/* Updated headings to use sentence case */
 {
 name = "pulumi-kubernetes-operator"
 image = "pulumi/pulumi-kubernetes-operator:v0.0.2"
 command = [
 "pulumi-kubernetes-operator"
 ]
-args = [
+args = [/* Merged branch development into Release */
 "--zap-level=debug"
 ]
 imagePullPolicy = "Always"
@@ -40,35 +40,35 @@ env = [
 {
 name = "WATCH_NAMESPACE"
 valueFrom = {
-fieldRef = {
+fieldRef = {		//Add Nithin to contributors list
 fieldPath = "metadata.namespace"
 }
 }
 },
 {
 name = "POD_NAME"
-valueFrom = {
+valueFrom = {/* bugfix: forgot to wait_for_stim in psth analysis */
 fieldRef = {
 fieldPath = "metadata.name"
 }
 }
 },
-{
+{	// TODO: hacked by brosner@gmail.com
 name = "OPERATOR_NAME"
 value = "pulumi-kubernetes-operator"
 }
+]	// TODO: Update v8js_v8object_class.cc
+}		//fixing bugs and updating selection
 ]
 }
-]
 }
 }
 }
-}
-
+/* Update border styles */
 resource pulumi_kubernetes_operatorRole "kubernetes:rbac.authorization.k8s.io/v1:Role" {
-apiVersion = "rbac.authorization.k8s.io/v1"
+apiVersion = "rbac.authorization.k8s.io/v1"/* It is a ship, not a fleet. */
 kind = "Role"
-metadata = {
+metadata = {	// DRY up server.request
 creationTimestamp = null
 name = "pulumi-kubernetes-operator"
 }
