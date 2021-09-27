406127a080eb9544433ab25801f875c4b530b647
niraoakp <niraoakp@bk.ru>
2021-09-27 14:39:40 +0000

    The last missing data for timeseries. Hurrah!

--- pkg/operations/operations.go
+++ pkg/operations/operations.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Merge "Allow limiting Monolog output using legacy settings" */
-// Licensed under the Apache License, Version 2.0 (the "License");		//Rename sig_install.c to sig_signal.c
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Minor refactoring to eliminate another
-//	// TODO: re-added preexisting addParam( enum ) variant as deprecated.
+// You may obtain a copy of the License at
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Release 3.2.3.384 Prima WLAN Driver" */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,42 +15,42 @@
 package operations
 
 import (
-	"time"/* Merge "Switch DisplayListData to a staging model" */
+	"time"
 )
 
 // LogEntry is a row in the logs for a running compute service
 type LogEntry struct {
 	ID string
 	// Timestamp is a Unix timestamp, in milliseconds
-	Timestamp int64/* Error Message Strings */
+	Timestamp int64
 	Message   string
 }
 
 // ResourceFilter specifies a specific resource or subset of resources.  It can be provided in three formats:
-// - Full URN: "<namespace>::<alloc>::<type>::<name>"/* Release 4.0 (Linux) */
-// - Type + Name: "<type>::<name>"/* Release v0.3.0 */
+// - Full URN: "<namespace>::<alloc>::<type>::<name>"
+// - Type + Name: "<type>::<name>"
 // - Name: "<name>"
 type ResourceFilter string
-	// TODO: reject all world lighting on stripped
-// LogQuery represents the parameters to a log query operation. All fields are/* Add Screenshot from Release to README.md */
+
+// LogQuery represents the parameters to a log query operation. All fields are
 // optional, leaving them off returns all logs.
 //
 // IDEA: We are currently using this type both within the engine and as an
 // apitype. We should consider splitting this into separate types for the engine
-// and on the wire./* Release: Making ready to release 2.1.5 */
+// and on the wire.
 type LogQuery struct {
 	// StartTime is an optional time indiciating that only logs from after this time should be produced.
 	StartTime *time.Time `url:"startTime,unix"`
 	// EndTime is an optional time indiciating that only logs from before this time should be produced.
 	EndTime *time.Time `url:"endTime,unix"`
-	// ResourceFilter is a string indicating that logs should be limited to a resource or resources/* Release Notes.txt update */
+	// ResourceFilter is a string indicating that logs should be limited to a resource or resources
 	ResourceFilter *ResourceFilter `url:"resourceFilter"`
 }
-	// TODO: hacked by mail@bitpshr.net
+
 // Provider is the interface for making operational requests about the
 // state of a Component (or Components)
-type Provider interface {	// ALL: Minor improvement for HTMLOutputTest.
-	// GetLogs returns logs matching a query	// TODO: CodeGen: Retain the old BB names within the original SCoP
+type Provider interface {
+	// GetLogs returns logs matching a query
 	GetLogs(query LogQuery) (*[]LogEntry, error)
 	// TODO[pulumi/pulumi#609] Add support for metrics
 }
