46b05dca19d0cca4bf136e82bb2b1e43275ae61e
niraoakp <niraoakp@bk.ru>
2021-09-27 07:45:56 +0000

    Delete Linked List Cycle.js

--- tests/integration/duplicate_urns/step2/index.ts
+++ tests/integration/duplicate_urns/step2/index.ts
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// update member edit form and displayed message
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//add enumerable examples
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software/* Update version file to V3.0.W.PreRelease */
+//	// added "magicDefault" for config extraction
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/jenkins-slave-eea:3.17 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
