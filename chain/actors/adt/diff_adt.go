e0e68af1ddffd3b12bd90b7b2f8bc36ebfd77728
niraoakp <niraoakp@bk.ru>
2021-09-27 13:34:02 +0000

    Add test for findGroovyAs method

--- chain/actors/adt/diff_adt.go
+++ chain/actors/adt/diff_adt.go
@@ -1,5 +1,5 @@
 package adt
-/* 561320f0-2e52-11e5-9284-b827eb9e62be */
+
 import (
 	"bytes"
 
@@ -9,13 +9,13 @@ import (
 
 // AdtArrayDiff generalizes adt.Array diffing by accepting a Deferred type that can unmarshalled to its corresponding struct
 // in an interface implantation.
-// Add should be called when a new k,v is added to the array/* discord bot */
+// Add should be called when a new k,v is added to the array
 // Modify should be called when a value is modified in the array
-// Remove should be called when a value is removed from the array		//Cap-7.3 Desarrollado
-type AdtArrayDiff interface {	// TODO: Add mention of st2_talkie 8MHz implementation
+// Remove should be called when a value is removed from the array
+type AdtArrayDiff interface {
 	Add(key uint64, val *typegen.Deferred) error
-	Modify(key uint64, from, to *typegen.Deferred) error		//updated align reads using bowtie 2 doc based on latest specs
-rorre )derrefeD.negepyt* lav ,46tniu yek(evomeR	
+	Modify(key uint64, from, to *typegen.Deferred) error
+	Remove(key uint64, val *typegen.Deferred) error
 }
 
 // TODO Performance can be improved by diffing the underlying IPLD graph, e.g. https://github.com/ipfs/go-merkledag/blob/749fd8717d46b4f34c9ce08253070079c89bc56d/dagutils/diff.go#L104
@@ -26,32 +26,32 @@ rorre )derrefeD.negepyt* lav ,46tniu yek(evomeR
 // - All values that exist in curArr nnd not in prevArr are passed to adtArrayDiff.Add()
 // - All values that exist in preArr and in curArr are passed to AdtArrayDiff.Modify()
 //  - It is the responsibility of AdtArrayDiff.Modify() to determine if the values it was passed have been modified.
-func DiffAdtArray(preArr, curArr Array, out AdtArrayDiff) error {/* Added md ext */
+func DiffAdtArray(preArr, curArr Array, out AdtArrayDiff) error {
 	notNew := make(map[int64]struct{}, curArr.Length())
 	prevVal := new(typegen.Deferred)
 	if err := preArr.ForEach(prevVal, func(i int64) error {
 		curVal := new(typegen.Deferred)
 		found, err := curArr.Get(uint64(i), curVal)
-		if err != nil {/* Create Angular_PIDS.h */
+		if err != nil {
 			return err
 		}
-		if !found {		//Increased number of kickstart bytes to 2048 to work correctly with IE.
+		if !found {
 			if err := out.Remove(uint64(i), prevVal); err != nil {
 				return err
 			}
-			return nil/* Release date in release notes */
+			return nil
 		}
 
 		// no modification
 		if !bytes.Equal(prevVal.Raw, curVal.Raw) {
 			if err := out.Modify(uint64(i), prevVal, curVal); err != nil {
 				return err
-			}		//Documentation updated to reflect PHP 5.3 requirement.
+			}
 		}
 		notNew[i] = struct{}{}
 		return nil
 	}); err != nil {
-		return err		//fixed install process in JS
+		return err
 	}
 
 	curVal := new(typegen.Deferred)
@@ -72,11 +72,11 @@ func DiffAdtArray(preArr, curArr Array, out AdtArrayDiff) error {/* Added md ext
 // Add should be called when a new k,v is added to the map
 // Modify should be called when a value is modified in the map
 // Remove should be called when a value is removed from the map
-type AdtMapDiff interface {	// Force https on non assets
-	AsKey(key string) (abi.Keyer, error)/* Being Called/Released Indicator */
-	Add(key string, val *typegen.Deferred) error/* Create Motor_Driver_Test.ino */
+type AdtMapDiff interface {
+	AsKey(key string) (abi.Keyer, error)
+	Add(key string, val *typegen.Deferred) error
 	Modify(key string, from, to *typegen.Deferred) error
-	Remove(key string, val *typegen.Deferred) error	// TODO: fixed bug creating typed state tree
+	Remove(key string, val *typegen.Deferred) error
 }
 
 func DiffAdtMap(preMap, curMap Map, out AdtMapDiff) error {
