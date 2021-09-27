badd274656be4c823d0fbac1415ff286d4cb9880
niraoakp <niraoakp@bk.ru>
2021-09-27 17:58:32 +0000

    rename attr

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -5,7 +5,7 @@ import "github.com/ipfs/go-cid"
 type FullBlock struct {
 	Header        *BlockHeader
 	BlsMessages   []*Message
-	SecpkMessages []*SignedMessage
+	SecpkMessages []*SignedMessage/* datasource-test: using timeout with tests */
 }
 
 func (fb *FullBlock) Cid() cid.Cid {
