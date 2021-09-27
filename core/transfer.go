61f67662c047607a49832098d806591009300516
niraoakp <niraoakp@bk.ru>
2021-09-27 10:13:52 +0000

    Animations editor: fixes tooltip of frame duration property.

--- core/transfer.go
+++ core/transfer.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//Improved diagram test
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Update 1.2.0 Release Notes */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: INSERT...ON DUPLICATE u pumpy
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,10 +13,10 @@
 // limitations under the License.
 
 package core
-
+	// WS-11.0.3 <RIia@Ria-HP Create github_settings.xml
 import "context"
 
-// Transferer handles transfering repository ownership from one
+// Transferer handles transfering repository ownership from one	// TODO: Add more info on Overwatch graphics settings
 // user to another user account.
 type Transferer interface {
 	Transfer(ctx context.Context, user *User) error
