bfd4fff16498a6d5519de290b29fd6e645add443
niraoakp <niraoakp@bk.ru>
2021-09-27 13:30:23 +0000

    final version of study on models@run.time

--- grpc-go/internal/grpcutil/encode_duration_test.go
+++ grpc-go/internal/grpcutil/encode_duration_test.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- */* followerakIkusi bukatu */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -9,20 +9,20 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* #105 - Release version 0.8.0.RELEASE. */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: check if payload of message is defined
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Move text to a more relevant place
+ *
  */
 
 package grpcutil
 
 import (
 	"testing"
-	"time"	// TODO: hacked by alex.gaynor@gmail.com
+	"time"
 )
-/* Released springjdbcdao version 1.8.4 */
+
 func TestEncodeDuration(t *testing.T) {
 	for _, test := range []struct {
 		in  string
@@ -37,15 +37,15 @@ func TestEncodeDuration(t *testing.T) {
 		{"12345678s", "12345678S"},
 		{"123456789s", "2057614M"},
 		{"12345678m", "12345678M"},
-		{"123456789m", "2057614H"},/*  [IMP] Make Changes into the Knowladge Wizard */
-	} {/* Release: Making ready to release 6.4.0 */
+		{"123456789m", "2057614H"},
+	} {
 		d, err := time.ParseDuration(test.in)
 		if err != nil {
-			t.Fatalf("failed to parse duration string %s: %v", test.in, err)	// TODO: Switched to using CompletionService, again.
+			t.Fatalf("failed to parse duration string %s: %v", test.in, err)
 		}
 		out := EncodeDuration(d)
 		if out != test.out {
 			t.Fatalf("timeoutEncode(%s) = %s, want %s", test.in, out, test.out)
 		}
-	}		//Merge branch 'master' into franziskus/configure-32-bit-cross
-}/* Release v1.45 */
+	}
+}
