405ac1fd6b3fd2419815d1507ace95d80d41be7c
niraoakp <niraoakp@bk.ru>
2021-09-27 06:03:20 +0000

    #i101531# syntax fixed

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -eu -o pipefail
-	// TODO: Use less registers if div/rem with longs and divisor is power of two
+
 for m in $*; do
   MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
   MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')
 
   cd "$MOCK_DIR"
   mockery -name=$"$MOCK_NAME"
-  cd -
+  cd -	// temporarily remove buf-wr checking - needs to be more lenient.
 done
