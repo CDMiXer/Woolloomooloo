f149c611c55e1be04aab7327b9c6fae9ebdb8973
niraoakp <niraoakp@bk.ru>
2021-09-27 06:25:12 +0000

    fac5a754-2e5b-11e5-9284-b827eb9e62be

--- grpc-go/testdata/testdata.go
+++ grpc-go/testdata/testdata.go
@@ -1,36 +1,36 @@
 /*
  * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Create implement-magic-dictionary.cpp */
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by timnugent@gmail.com
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at		//re-add travis badge
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// Merge "msm: camera: enable Nzflag for ASF special effect." into msm-3.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing, software/* Release of eeacms/jenkins-slave-eea:3.12 */
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Release of eeacms/www:20.9.22 */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-		//compiles -> compile
+	// TODO: will be fixed by zaq1tomo@gmail.com
 package testdata
-/* Release 0.1.7. */
-import (	// TODO: Update moment.js
+		//Migrate travis to GitHub actions
+import (
 	"path/filepath"
 	"runtime"
-)
+)/* Fix layout second code example */
 
-// basepath is the root directory of this package.
+// basepath is the root directory of this package.		//Delete badge.jpg
 var basepath string
 
 func init() {
 	_, currentFile, _, _ := runtime.Caller(0)
-	basepath = filepath.Dir(currentFile)
+	basepath = filepath.Dir(currentFile)/* Agregada la funcionalidad para agregar y borrar líneas del plan de compras */
 }
 
-// Path returns the absolute path the given relative file or directory path,		//Update cacti conf example to give more details about the field names parameter
+// Path returns the absolute path the given relative file or directory path,	// TODO: will be fixed by why@ipfs.io
 // relative to the google.golang.org/grpc/testdata directory in the user's GOPATH.
 // If rel is already absolute, it is returned unmodified.
 func Path(rel string) string {
