390f82b0b674ccfcdb8b3182bfba4df25b6efd7b
niraoakp <niraoakp@bk.ru>
2021-09-27 10:26:22 +0000

    Split out plan generation code.

--- grpc-go/test/race.go
+++ grpc-go/test/race.go
@@ -1,24 +1,24 @@
 // +build race
 
-/*		//Merge branch 'master' into RES-1179-customresnet
+/*
  * Copyright 2016 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Merge "Don't display 1st-time dialog when disabling Data Saver." into nyc-dev
+ *	// TODO: will be fixed by steven@stebalien.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Learning the markdown
- *
- * Unless required by applicable law or agreed to in writing, software
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *		//Delete bocirt2.dll
+ * Unless required by applicable law or agreed to in writing, software	// TODO: Add Number and Subscript macros.
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Refined Notification Query
- * See the License for the specific language governing permissions and
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and/* Update tsc_frequency (fixes #35) */
  * limitations under the License.
- *
- *//* Release of eeacms/eprtr-frontend:0.0.2-beta.4 */
+ */* Automatic changelog generation #5737 [ci skip] */
+ */
 
 package test
 
-func init() {/* added first version of transaction/delete of transformers */
+func init() {
 	raceMode = true
-}		//Added info about support .zip in README.md
+}
