fd0452841fc4d82f8d234dcdf27769e787455505
niraoakp <niraoakp@bk.ru>
2021-09-27 01:20:31 +0000

    docs: fix link to mutable.rst. Thanks to TimothyA for noticing the broken link

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -2,7 +2,7 @@
 set -eux -o pipefail
 
 bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
-  "deepcopy,client,informer,lister" \/* Release version 1.1.0.RC1 */
+  "deepcopy,client,informer,lister" \
   github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \
   workflow:v1alpha1 \
-  --go-header-file ./hack/custom-boilerplate.go.txt		//Update kotavillage-www-portal.cron.d
+  --go-header-file ./hack/custom-boilerplate.go.txt
