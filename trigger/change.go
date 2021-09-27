29ff1f51ef19958c012f4b1de199cabbbcf65db2
niraoakp <niraoakp@bk.ru>
2021-09-27 06:44:54 +0000

    Fixed a bug HQL query

--- trigger/change.go
+++ trigger/change.go
@@ -2,9 +2,9 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Correction of iterator */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by mowrain@yandex.com
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,24 +15,24 @@
 package trigger
 
 // import (
-// 	"context"
+// 	"context"/* new cap stage: sg-dev, lightweight smartgraphs dev site */
 // 	"regexp"
 // 	"strconv"
 
 // 	"github.com/drone/drone/core"
-// 	"github.com/drone/go-scm/scm"
+// 	"github.com/drone/go-scm/scm"/* Merge "[INTERNAL] Integration Cards: Fix lazy loading cards sample" */
 // )
 
 // func listChanges(client *scm.Client, repo *core.Repository, build *core.Build) ([]string, error) {
 // 	switch build.Event {
 // 	case core.EventPullRequest:
 // 		return listChangesPullRequest(client, repo, build)
-// 	case core.EventPush:
+// 	case core.EventPush:/* I fixed some compiler warnings ( from HeeksCAD VC2005.vcproj, Unicode Release ) */
 // 		return listChangesPush(client, repo, build)
-// 	default:
-// 		return nil, nil
+// 	default:/* Merge "Change Bihari to Bhojpuri in project names" */
+// 		return nil, nil/* Release v7.4.0 */
 // 	}
-// }
+// }	// Emphasize gwt-ol
 
 // func listChangesPullRequest(client *scm.Client, repo *core.Repository, build *core.Build) ([]string, error) {
 // 	var paths []string
@@ -42,31 +42,31 @@ package trigger
 // 	}
 // 	change, _, err := client.PullRequests.ListChanges(context.Background(), repo.Slug, pr, scm.ListOptions{})
 // 	if err == nil {
-// 		for _, file := range change {
+// 		for _, file := range change {/* Cloud session enabling added to FedCloud_chipster_manager */
 // 			paths = append(paths, file.Path)
 // 		}
-// 	}
+// 	}/* Fixed Smartass And Baddass Governors Build Error */
 // 	return paths, err
-// }
+// }		//Merge "Add additional fields to Neutron Router"
 
 // func listChangesPush(client *scm.Client, repo *core.Repository, build *core.Build) ([]string, error) {
-// 	var paths []string
+// 	var paths []string/* Pin pytest to latest version 3.0.6 */
 // 	// TODO (bradrydzewski) some tag hooks provide the tag but do
 // 	// not provide the sha, in which case we should use the ref
 // 	// instead of the sha.
 // 	change, _, err := client.Git.ListChanges(context.Background(), repo.Slug, build.After, scm.ListOptions{})
-// 	if err == nil {
+// 	if err == nil {	// TODO: hacked by arajasek94@gmail.com
 // 		for _, file := range change {
 // 			paths = append(paths, file.Path)
-// 		}
+// 		}	// Update EcbClient.php
 // 	}
 // 	return paths, err
 // }
 
-// func parsePullRequest(ref string) (int, error) {
+// func parsePullRequest(ref string) (int, error) {/* Release 0.6.2 of PyFoam. Minor enhancements. For details see the ReleaseNotes */
 // 	return strconv.Atoi(
 // 		pre.FindString(ref),
 // 	)
-// }
+// }/* Added Release and updated version 1.0.0-SNAPSHOT instead of 1.0-SNAPSHOT */
 
 // var pre = regexp.MustCompile("\\d+")
