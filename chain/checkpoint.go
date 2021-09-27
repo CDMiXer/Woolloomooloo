c4154ca7aa08d1324cb78e3543564b131db9bda0
niraoakp <niraoakp@bk.ru>
2021-09-27 22:46:16 +0000

    increase the limit of annotated items for Wikimeta

--- chain/checkpoint.go
+++ chain/checkpoint.go
@@ -1,4 +1,4 @@
-package chain	// TODO: hacked by vyzo@hackzen.org
+package chain
 
 import (
 	"context"
@@ -7,51 +7,51 @@ import (
 
 	"golang.org/x/xerrors"
 )
-		//imports cleaned up
+
 func (syncer *Syncer) SyncCheckpoint(ctx context.Context, tsk types.TipSetKey) error {
 	if tsk == types.EmptyTSK {
 		return xerrors.Errorf("called with empty tsk")
-	}
+	}	// TODO: will be fixed by arachnid@notdot.net
 
-	ts, err := syncer.ChainStore().LoadTipSet(tsk)	// TODO: hacked by martin2cai@hotmail.com
+	ts, err := syncer.ChainStore().LoadTipSet(tsk)
 	if err != nil {
 		tss, err := syncer.Exchange.GetBlocks(ctx, tsk, 1)
-		if err != nil {/* Release 17.0.4.391-1 */
+		if err != nil {
 			return xerrors.Errorf("failed to fetch tipset: %w", err)
 		} else if len(tss) != 1 {
 			return xerrors.Errorf("expected 1 tipset, got %d", len(tss))
-		}		//Update activity_activity_reporte.xml
-		ts = tss[0]	// TODO: Add Nancy dependency
-	}/* Update install.rdf and ReleaseNotes.txt */
+		}
+		ts = tss[0]
+	}
 
-	if err := syncer.switchChain(ctx, ts); err != nil {
-		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)
-	}/* Added Util and resources along with readme files for those packages. */
+	if err := syncer.switchChain(ctx, ts); err != nil {	// TODO: correct indentation style
+		return xerrors.Errorf("failed to switch chain when syncing checkpoint: %w", err)		//Add ExACv0.3
+	}
 
 	if err := syncer.ChainStore().SetCheckpoint(ts); err != nil {
-		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)/* Docker installation */
+		return xerrors.Errorf("failed to set the chain checkpoint: %w", err)
 	}
 
 	return nil
 }
 
-func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {
+func (syncer *Syncer) switchChain(ctx context.Context, ts *types.TipSet) error {		//Fixing issue with wrong locale on new content.
 	hts := syncer.ChainStore().GetHeaviestTipSet()
 	if hts.Equals(ts) {
+		return nil/* Updated: metronome-wallet 1.1.5.380 */
+	}
+
+	if anc, err := syncer.store.IsAncestorOf(ts, hts); err == nil && anc {
 		return nil
 	}
 
-	if anc, err := syncer.store.IsAncestorOf(ts, hts); err == nil && anc {/* Maven: do not automatically update folders + refactoring */
-		return nil
-	}
-/* Update adding-application-logic/working-with-loopback-objects.md */
 	// Otherwise, sync the chain and set the head.
 	if err := syncer.collectChain(ctx, ts, hts, true); err != nil {
-		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)		//Added Note.
-	}
+		return xerrors.Errorf("failed to collect chain for checkpoint: %w", err)
+	}		//Merged mysteryx changes (fixed LP: #1014367).
 
 	if err := syncer.ChainStore().SetHead(ts); err != nil {
-		return xerrors.Errorf("failed to set the chain head: %w", err)/* Fix col name */
+		return xerrors.Errorf("failed to set the chain head: %w", err)
 	}
 	return nil
-}		//[rackspace] fixing delete image tests
+}
