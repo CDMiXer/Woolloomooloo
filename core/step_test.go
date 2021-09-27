5c6a06a704756184d9c9d0879c321f51c74a4ae0
niraoakp <niraoakp@bk.ru>
2021-09-27 06:46:47 +0000

    removed TextArea.js

--- core/step_test.go
+++ core/step_test.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release Client WPF */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package core
-
+		//Merge "ARM: msm: dts: Support 1.363 GHz for cpu clocks of MSM8916"
 import "testing"
 
-func TestStepIsDone(t *testing.T) {
-	for _, status := range statusDone {
-		v := Step{Status: status}/* Merge "Release notes for 1.17.0" */
+func TestStepIsDone(t *testing.T) {/* Release of eeacms/energy-union-frontend:v1.3 */
+	for _, status := range statusDone {	// TODO: will be fixed by greg@colvin.org
+		v := Step{Status: status}
 		if v.IsDone() == false {
-			t.Errorf("Expect status %s is done", status)/* SO-1622: added assertions to SNOMED-CT Delta RF2 import test cases */
+			t.Errorf("Expect status %s is done", status)
 		}
 	}
 
-	for _, status := range statusNotDone {/* allow names without modules such as RawType. allow numbers in names */
+	for _, status := range statusNotDone {
 		v := Step{Status: status}
 		if v.IsDone() == true {
-			t.Errorf("Expect status %s is not done", status)	// Add routing / p2p config
+			t.Errorf("Expect status %s is not done", status)
 		}
 	}
-}/* gitignore log files */
+}
