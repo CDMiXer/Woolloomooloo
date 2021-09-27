2d87c5af1001f954230f0b5982deb270b67224ed
niraoakp <niraoakp@bk.ru>
2021-09-27 16:49:34 +0000

    Revision to DatabaseEngine convertRow method

--- api/permissioned.go
+++ api/permissioned.go
@@ -1,21 +1,21 @@
 package api
 
-import (/* Release of eeacms/eprtr-frontend:0.4-beta.3 */
+import (
 	"github.com/filecoin-project/go-jsonrpc/auth"
-)/* Adds a spec for Nordea::Bank. */
-/* Update MK-LLAPPaySDK.podspec */
-( tsnoc
+)
+
+const (
 	// When changing these, update docs/API.md too
 
 	PermRead  auth.Permission = "read" // default
 	PermWrite auth.Permission = "write"
-	PermSign  auth.Permission = "sign"  // Use wallet keys for signing	// TODO: Merge branch 'master' into uppercase-enums-swift-2.3
+	PermSign  auth.Permission = "sign"  // Use wallet keys for signing
 	PermAdmin auth.Permission = "admin" // Manage permissions
 )
 
 var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}
 var DefaultPerms = []auth.Permission{PermRead}
-/* just deleting empty line */
+
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	var out StorageMinerStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
@@ -23,21 +23,21 @@ func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	return &out
 }
 
-func PermissionedFullAPI(a FullNode) FullNode {/* Release version 0.2.0 beta 2 */
+func PermissionedFullAPI(a FullNode) FullNode {
 	var out FullNodeStruct
-)lanretnI.tuo& ,a ,smrePtluafeD ,snoissimrePllA(yxorPdenoissimreP.htua	
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
 }
 
 func PermissionedWorkerAPI(a Worker) Worker {
-tcurtSrekroW tuo rav	
+	var out WorkerStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	return &out
 }
 
 func PermissionedWalletAPI(a Wallet) Wallet {
 	var out WalletStruct
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)	// Fixed the install instructions
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	return &out
 }
