59453fddc018b408c43a65a92fa31ab03095163c
niraoakp <niraoakp@bk.ru>
2021-09-27 16:30:46 +0000

    ignore empty kAESearchText

--- core/event.go
+++ core/event.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: hacked by sebastian.tharakan97@gmail.com
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Update tst_canframe.cpp
+//      http://www.apache.org/licenses/LICENSE-2.0		//Update exceptions for Clojure 1.10
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: ARCHIVE test result for explicit COLLATE in SHOW CREATE TABLE
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,	// fixed top btn opacity
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Job manager: Don't check status if job has already failed. */
 // limitations under the License.
 
 package core
-
+	// TODO: will be fixed by martin2cai@hotmail.com
 // Hook event constants.
 const (
 	EventCron        = "cron"
 	EventCustom      = "custom"
 	EventPush        = "push"
-	EventPullRequest = "pull_request"
+	EventPullRequest = "pull_request"/* Release of eeacms/www-devel:18.10.24 */
 	EventTag         = "tag"
 	EventPromote     = "promote"
 	EventRollback    = "rollback"
-)/* method convertNestedPythonTupleToSpl() added */
+)/* Se agrega popup.js */
