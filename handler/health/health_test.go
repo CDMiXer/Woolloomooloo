da50bd82878b45ad988d96805eab08a6e9ede3d3
niraoakp <niraoakp@bk.ru>
2021-09-27 15:00:24 +0000

    Screenshots resize

--- handler/health/health_test.go
+++ handler/health/health_test.go
@@ -1,21 +1,21 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* build: Release version 0.10.0 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Add the most egregious problems with 1.2 underneath the 1.2 Release Notes */
 package health
 
 import (
 	"net/http/httptest"
 	"testing"
 )
-
-func TestHandleHealthz(t *testing.T) {/* Mj Example: Book must not have an id field */
+/* Easy ajax handling. Release plan checked */
+func TestHandleHealthz(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/healthz", nil)
-
+/* trocando video que estava privado */
 	Handler().ServeHTTP(w, r)
 
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)		//add test for templates
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
-}
+}/* Release 0.2.0 merge back in */
