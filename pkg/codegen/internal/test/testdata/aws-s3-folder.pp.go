da153644d2be2c99ba4c0fb0b8b634c478cfc2b6
niraoakp <niraoakp@bk.ru>
2021-09-27 02:58:51 +0000

    Load config.{h,mk} when building tests. Fixes [1c11c59282].

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.go
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.go
@@ -1,33 +1,33 @@
 package main
-
+/* Release: updated latest.json */
 import (
-	"encoding/json"
-	"fmt"
+	"encoding/json"/* Merge "Release 3.2.3.406 Prima WLAN Driver" */
+	"fmt"/* Fixed mvc.wax block to work without role properties */
 	"io/ioutil"
-	"mime"
+	"mime"/* Update shellyRepoConf.sh */
 	"path"
 
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+/* abstract out default target config responses in Releaser spec */
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		siteBucket, err := s3.NewBucket(ctx, "siteBucket", &s3.BucketArgs{
 			Website: &s3.BucketWebsiteArgs{
-				IndexDocument: pulumi.String("index.html"),
+				IndexDocument: pulumi.String("index.html"),	// TODO: will be fixed by witek@enjin.io
 			},
 		})
 		if err != nil {
 			return err
 		}
-		siteDir := "www"
+		siteDir := "www"	// TODO: catches a nullpointer exception which occurs if kerberos is not used
 		files0, err := ioutil.ReadDir(siteDir)
 		if err != nil {
-			return err
+			return err	// TODO: will be fixed by steven@stebalien.com
 		}
 		fileNames0 := make([]string, len(files0))
-		for key0, val0 := range files0 {
+		for key0, val0 := range files0 {	// TODO: hacked by arachnid@notdot.net
 			fileNames0[key0] = val0.Name()
 		}
 		var files []*s3.BucketObject
@@ -40,7 +40,7 @@ func main() {
 			})
 			if err != nil {
 				return err
-			}
+			}	// TODO: hacked by sjors@sprovoost.nl
 			files = append(files, __res)
 		}
 		_, err = s3.NewBucketPolicy(ctx, "bucketPolicy", &s3.BucketPolicyArgs{
@@ -48,13 +48,13 @@ func main() {
 			Policy: siteBucket.ID().ApplyT(func(id string) (pulumi.String, error) {
 				var _zero pulumi.String
 				tmpJSON0, err := json.Marshal(map[string]interface{}{
-					"Version": "2012-10-17",
+					"Version": "2012-10-17",		//Adding Mixpanel code
 					"Statement": []map[string]interface{}{
 						map[string]interface{}{
 							"Effect":    "Allow",
 							"Principal": "*",
 							"Action": []string{
-								"s3:GetObject",
+								"s3:GetObject",/* Release 1.7.9 */
 							},
 							"Resource": []string{
 								fmt.Sprintf("%v%v%v", "arn:aws:s3:::", id, "/*"),
@@ -62,13 +62,13 @@ func main() {
 						},
 					},
 				})
-				if err != nil {
+				if err != nil {	// added sql injection example, working on XSS
 					return _zero, err
 				}
 				json0 := string(tmpJSON0)
-				return pulumi.String(json0), nil
+				return pulumi.String(json0), nil	// no luck with travis and phpunit coredump
 			}).(pulumi.StringOutput),
-		})
+		})/* Disable test due to crash in XUL during Release call. ROSTESTS-81 */
 		if err != nil {
 			return err
 		}
