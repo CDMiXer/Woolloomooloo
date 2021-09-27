127cfcbca3296db8af6e6ba314870a4cb4c4c182
niraoakp <niraoakp@bk.ru>
2021-09-27 14:26:00 +0000

    Update Files_Images.md

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -1,5 +1,5 @@
 package nullreader
-
+/* Update day.md */
 // TODO: extract this to someplace where it can be shared with lotus
 type Reader struct{}
 
@@ -7,5 +7,5 @@ func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
 	}
-	return len(out), nil
+	return len(out), nil/* Release new version 2.0.6: Remove an old gmail special case */
 }
