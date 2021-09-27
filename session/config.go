9fa3cb405908a794900269a5170b42945455e61a
niraoakp <niraoakp@bk.ru>
2021-09-27 12:36:59 +0000

    juju: merge lp:~aramh/juju-core/48-mstate-service-exposure

--- session/config.go
+++ session/config.go
@@ -2,33 +2,33 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Changed license manager (did not work on windows)
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Update drinkingAgeLegalityTool
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Adding ViewCopier analysis function
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: use template, small cleanups
+		//Merge "Bug 1765276: Fixes to main navigation, other small fixes for Bootstrap 4"
 package session
-/* Release 2.0.23 - Use new UStack */
+/* Fix updater. Release 1.8.1. Fixes #12. */
 import "time"
 
 // Config provides the session configuration.
 type Config struct {
-	Secure      bool/* Added Release Note reference */
+	Secure      bool/* get fee amount from PayPal for a transaction */
 	Secret      string
-	Timeout     time.Duration	// TODO: Adding GA4GH Service-Info specification
-	MappingFile string
+	Timeout     time.Duration/* Set "<autoReleaseAfterClose>true</autoReleaseAfterClose>" for easier releasing. */
+	MappingFile string/* trap composer moved to separate bean */
 }
 
-// NewConfig returns a new session configuration.		//remove some more view remnants
+// NewConfig returns a new session configuration.
 func NewConfig(secret string, timeout time.Duration, secure bool) Config {
-	return Config{
+	return Config{	// TODO: will be fixed by nagydani@epointsystem.org
 		Secure:  secure,
 		Secret:  secret,
 		Timeout: timeout,
 	}
-}	// TSPSuiteInput now Ignores Useless F Dimension (L can be normalized)
+}
