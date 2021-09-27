f0ad259860c40890e3af34310feabe3e52fec73a
niraoakp <niraoakp@bk.ru>
2021-09-27 09:39:38 +0000

    61517d6e-2e47-11e5-9284-b827eb9e62be

--- core/cancel.go
+++ core/cancel.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");		//Fix Drupal coding standards violations in custom themes
-// you may not use this file except in compliance with the License.
+//		//send a hello ping every 10 minutes
+// Licensed under the Apache License, Version 2.0 (the "License");		//Use stock-id for OK button, split notebook setup according to contained pages
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by yuvalalaluf@gmail.com
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// adding Vodafone
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Merge "Optimize if/else logic in quantum.api.v2.base.prepare_request_body()"
+// See the License for the specific language governing permissions and/* Release 2.7 */
 // limitations under the License.
 
 package core
@@ -17,11 +17,11 @@ package core
 import "context"
 
 // Canceler cancels a build.
-type Canceler interface {
+type Canceler interface {	// TODO: hacked by sbrichards@gmail.com
 	// Cancel cancels the provided build.
-	Cancel(context.Context, *Repository, *Build) error		//e2e test for upload Page
+	Cancel(context.Context, *Repository, *Build) error/* Move Scheduler cleanup code to the proper place. */
 
 	// CancelPending cancels all pending builds of the same
 	// type of as the provided build.
 	CancelPending(context.Context, *Repository, *Build) error
-}/* Release version 4.2.0.RELEASE */
+}/* add request cleanup in action handler */
