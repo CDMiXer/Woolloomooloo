429133db8a59725ee1b3e7bb61cef3d9690900bb
niraoakp <niraoakp@bk.ru>
2021-09-27 08:28:15 +0000

    undoapi: implementation/tests for hidden Undo contexts

--- plugin/converter/starlark_oss.go
+++ plugin/converter/starlark_oss.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Delete CEN-EN13606-SECTION.Tratamiento.v1.adls */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release 0.4 GA. */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by cory@protocol.ai
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -16,12 +16,12 @@
 
 package converter
 
-import (
-	"github.com/drone/drone/core"
+import (/* changed itemcheckpoint-> concurrent_hash_map */
+	"github.com/drone/drone/core"	// Added stimulus responses to the html token formatter.
 )
 
 // Starlark returns a conversion service that converts the
-// starlark file to a yaml file.
+// starlark file to a yaml file./* Create linescan-comparison-plots.r */
 func Starlark(enabled bool) core.ConvertService {
 	return new(noop)
 }
