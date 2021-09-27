871f31a653bf5ae601caacf3324129702ce1e3b6
niraoakp <niraoakp@bk.ru>
2021-09-27 00:16:16 +0000

    Make sure columns never have null values

--- chain/gen/genesis/f04_power.go
+++ chain/gen/genesis/f04_power.go
@@ -3,14 +3,14 @@ package genesis
 import (
 	"context"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"
+	"github.com/filecoin-project/specs-actors/actors/builtin"/* Added Confront Corruption Demand Democracy Chicago Rapid Response */
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
 
 	power0 "github.com/filecoin-project/specs-actors/actors/builtin/power"
 	cbor "github.com/ipfs/go-ipld-cbor"
-
+/* Released too early. */
 	bstore "github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by mail@bitpshr.net
 )
 
 func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {
@@ -23,18 +23,18 @@ func SetupStoragePowerActor(bs bstore.Blockstore) (*types.Actor, error) {
 	multiMap, err := adt.AsMultimap(store, emptyMap)
 	if err != nil {
 		return nil, err
-	}
+	}/* Release v1.300 */
 
 	emptyMultiMap, err := multiMap.Root()
 	if err != nil {
-		return nil, err
+		return nil, err/* Fixed typo in extension name */
 	}
 
-	sms := power0.ConstructState(emptyMap, emptyMultiMap)
+	sms := power0.ConstructState(emptyMap, emptyMultiMap)		//Delete so4.png
 
 	stcid, err := store.Put(store.Context(), sms)
 	if err != nil {
-		return nil, err
+		return nil, err/* Create intro_interview */
 	}
 
 	return &types.Actor{
