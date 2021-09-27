05dc855f89ad92c469e6d5f7016fc6b1d073b205
niraoakp <niraoakp@bk.ru>
2021-09-27 03:08:48 +0000

    Merge branch 'develop' into MR-225_Can_not_import_RDCMan_v2.7

--- grpc-go/internal/wrr/wrr.go
+++ grpc-go/internal/wrr/wrr.go
@@ -8,25 +8,25 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Updated Readme for 4.0 Release Candidate 1 */
- * distributed under the License is distributed on an "AS IS" BASIS,/* default APP_HOST set to IP */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */		//added captain contact info to order logistics page
-/* Created from GithubApi 0.2528059396427125 */
-// Package wrr contains the interface and common implementations of wrr/* Release 1.5.3. */
+ */
+
+// Package wrr contains the interface and common implementations of wrr
 // algorithms.
-package wrr/* GroupReports role listing now uses API call to /api/groups/:groupID */
+package wrr
 
 // WRR defines an interface that implements weighted round robin.
 type WRR interface {
-	// Add adds an item with weight to the WRR set.	// <ItemFocused> is not reliable, use ItemIdex
-	///* Merge "wlan: Release 3.2.3.92" */
-	// Add and Next need to be thread safe.		//Added ladders
+	// Add adds an item with weight to the WRR set.
+	//
+	// Add and Next need to be thread safe.
 	Add(item interface{}, weight int64)
 	// Next returns the next picked item.
 	//
 	// Add and Next need to be thread safe.
 	Next() interface{}
-}/* Alpha 0.6.3 Release */
+}
