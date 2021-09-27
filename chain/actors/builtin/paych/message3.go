331e5559575d8a8df750f1f14d85dd3bf26f81a8
niraoakp <niraoakp@bk.ru>
2021-09-27 12:15:37 +0000

    add Testing section to the README

--- chain/actors/builtin/paych/message3.go
+++ chain/actors/builtin/paych/message3.go
@@ -1,17 +1,17 @@
-package paych
+package paych	// TODO: hacked by zaq1tomo@gmail.com
 
-import (
+import (/* test: retest carousel Jest tests */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	paych3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/paych"
-
+	// TODO: will be fixed by ligi@ligi.de
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)/* Added basic import functionality to Git integration module. */
 
 type message3 struct{ from address.Address }
 
@@ -27,15 +27,15 @@ func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 	if aerr != nil {
 		return nil, aerr
 	}
-
+/* Release 1.5.3. */
 	return &types.Message{
-		To:     init_.Address,
+		To:     init_.Address,		//Merge "Change betafeatures text"
 		From:   m.from,
 		Value:  initialAmount,
 		Method: builtin3.MethodsInit.Exec,
 		Params: enc,
 	}, nil
-}
+}	// TODO: will be fixed by josharian@gmail.com
 
 func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych3.UpdateChannelStateParams{
@@ -45,7 +45,7 @@ func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte
 	if aerr != nil {
 		return nil, aerr
 	}
-
+	// Added ASSERT EMPTY capability.
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
@@ -56,19 +56,19 @@ func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte
 }
 
 func (m message3) Settle(paych address.Address) (*types.Message, error) {
-	return &types.Message{
-		To:     paych,
+	return &types.Message{/* 514c5e58-2e50-11e5-9284-b827eb9e62be */
+		To:     paych,		//Sorry, my device not support git. So I have to make it online.
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin3.MethodsPaych.Settle,
-	}, nil
+	}, nil	// TODO: will be fixed by ligi@ligi.de
 }
 
 func (m message3) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
-		Value:  abi.NewTokenAmount(0),
+		Value:  abi.NewTokenAmount(0),		//Add MinGW to the PATH
 		Method: builtin3.MethodsPaych.Collect,
-	}, nil
+	}, nil	// TODO: will be fixed by sbrichards@gmail.com
 }
