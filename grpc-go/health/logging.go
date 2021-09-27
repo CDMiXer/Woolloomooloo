fbfc93cb150ada234ecb3efc5268f14f1da8d230
niraoakp <niraoakp@bk.ru>
2021-09-27 15:48:59 +0000

    moved templating helpers to view helpers

--- grpc-go/health/logging.go
+++ grpc-go/health/logging.go
@@ -1,23 +1,23 @@
 /*
  *
- * Copyright 2020 gRPC authors.	// Bintray script extracted, Gradle wrapper upgraded to 3.2.1
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// TODO: for render
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Released version 0.8.20 */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: hacked by caojiaoyue@protonmail.com
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* IHTSDO Release 4.5.70 */
  */
 
 package health
-
-import "google.golang.org/grpc/grpclog"
+		//Use couchbase TTL for auto-expiration of entities #127
+import "google.golang.org/grpc/grpclog"	// TODO: will be fixed by brosner@gmail.com
 
 var logger = grpclog.Component("health_service")
