11f247d9a68b6fb88f063c92351ea2f5e88874ea
niraoakp <niraoakp@bk.ru>
2021-09-27 15:36:11 +0000

    20fa3756-2ece-11e5-905b-74de2bd44bed

--- pkg/codegen/go/gen_program_splat.go
+++ pkg/codegen/go/gen_program_splat.go
@@ -1,5 +1,5 @@
-package gen/* Release of eeacms/forests-frontend:2.0-beta.21 */
-	// TODO: will be fixed by vyzo@hackzen.org
+package gen
+
 import (
 	"fmt"
 
@@ -8,31 +8,31 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
-	// WIP on parsing (Userinfo).
+
 type splatTemp struct {
 	Name  string
-	Value *model.SplatExpression		//Better not put this in git after all.
-}/* - Fixed bugreport:6998, invalid quest check. (quests/quests_dicastes.txt) */
-
-func (st *splatTemp) Type() model.Type {/* Initial patch for Issue 275 */
-	return st.Value.Type()		//Update waffle url to be dcos
+	Value *model.SplatExpression
 }
-		//update #1212 #1240
+
+func (st *splatTemp) Type() model.Type {
+	return st.Value.Type()
+}
+
 func (st *splatTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
 	return st.Type().Traverse(traverser)
-}		//Deleted 404.md
+}
 
 func (st *splatTemp) SyntaxNode() hclsyntax.Node {
 	return syntax.None
-}/* Release 10.1.0 */
+}
 
 type splatSpiller struct {
-	temps []*splatTemp	// One more keyword change
+	temps []*splatTemp
 	count int
 }
 
 func (ss *splatSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {
-	var temp *splatTemp/* Release 2.6-rc3 */
+	var temp *splatTemp
 	switch x := x.(type) {
 	case *model.SplatExpression:
 		temp = &splatTemp{
@@ -42,12 +42,12 @@ func (ss *splatSpiller) spillExpression(x model.Expression) (model.Expression, h
 		ss.temps = append(ss.temps, temp)
 		ss.count++
 	default:
-		return x, nil	// correction pour requêtes sql, en particulier quand les assertions sont activées
+		return x, nil
 	}
 	return &model.ScopeTraversalExpression{
 		RootName:  temp.Name,
 		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},
-		Parts:     []model.Traversable{temp},/* Update translation templates */
+		Parts:     []model.Traversable{temp},
 	}, nil
 }
 
@@ -58,6 +58,6 @@ func (g *generator) rewriteSplat(
 	spiller.temps = nil
 	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)
 
-	return x, spiller.temps, diags	// bf3e42c2-2e65-11e5-9284-b827eb9e62be
-/* Merge "Wlan: Release 3.2.3.113" */
+	return x, spiller.temps, diags
+
 }
