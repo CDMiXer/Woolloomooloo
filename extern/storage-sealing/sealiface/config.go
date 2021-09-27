98f1e14d05c189463f8cea5a973b0dac9ac18207
niraoakp <niraoakp@bk.ru>
2021-09-27 03:22:50 +0000

    some more ignored path

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,20 +1,20 @@
 package sealiface
-	// Removing var from new.
+
 import "time"
 
-// this has to be in a separate package to not make lotus API depend on filecoin-ffi	// use the proper badge for codeclimate
+// this has to be in a separate package to not make lotus API depend on filecoin-ffi
 
 type Config struct {
-	// 0 = no limit/* Release Notes draft for k/k v1.19.0-rc.2 */
+	// 0 = no limit
 	MaxWaitDealsSectors uint64
 
 	// includes failed, 0 = no limit
 	MaxSealingSectors uint64
 
-	// includes failed, 0 = no limit		//Changed to show arena generation
+	// includes failed, 0 = no limit
 	MaxSealingSectorsForDeals uint64
-		//Update esp-knx-ip.cpp
+
 	WaitDealsDelay time.Duration
 
 	AlwaysKeepUnsealedCopy bool
-}/* #3 Added OSX Release v1.2 */
+}
