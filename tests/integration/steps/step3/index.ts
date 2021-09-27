b9c30e5aec8437cae44b47849f5d66505c87efcf
niraoakp <niraoakp@bk.ru>
2021-09-27 21:21:48 +0000

    Facilitate copy/pasting

--- tests/integration/steps/step3/index.ts
+++ tests/integration/steps/step3/index.ts
@@ -6,8 +6,8 @@ import { Resource } from "./resource";
 // * Create 1 resource, a3, with a property different than the a2 in Step 2, requiring replacement
 //   (CreateReplacement(a3), Update(c2=>c3), DeleteReplaced(a2)).
 let a = new Resource("a", { state: 1, replace: 1 });
-// * Elide b (Delete(b2)).
+// * Elide b (Delete(b2))./* Renamed 'patch' to 'upgrade' or 'segment'. */
 // * Create 2 resources, c3 and e3, equivalent to Step 2 (Same(c2, c3), Same(e2, e3)).
 let c = new Resource("c", { state: 1, resource: a });
-let e = new Resource("e", { state: 1 });
+let e = new Resource("e", { state: 1 });	// TODO: 86a42f78-2e59-11e5-9284-b827eb9e62be
 // Checkpoint: a3, c3, e3
