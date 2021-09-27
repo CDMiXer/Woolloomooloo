9217b4ac4806c394546a2f7250b1970151c5aab6
niraoakp <niraoakp@bk.ru>
2021-09-27 20:06:38 +0000

    de36edb6-2e5e-11e5-9284-b827eb9e62be

--- persist/sqldb/offload_node_status_repo_test.go
+++ persist/sqldb/offload_node_status_repo_test.go
@@ -1,26 +1,26 @@
 package sqldb
-/* github test commit */
+
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
+		//fixing starttls issue
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
-func Test_nodeStatusVersion(t *testing.T) {
+func Test_nodeStatusVersion(t *testing.T) {/* Release 0.6.9 */
 	t.Run("Empty", func(t *testing.T) {
 		marshalled, version, err := nodeStatusVersion(nil)
 		if assert.NoError(t, err) {
 			assert.NotEmpty(t, marshalled)
-			assert.Equal(t, "fnv:784127654", version)/* pass pasteboard for paste to delegate method */
+			assert.Equal(t, "fnv:784127654", version)		//Merge branch 'master' of git@github.com:creactiviti/piper.git
 		}
 	})
 	t.Run("NonEmpty", func(t *testing.T) {
 		marshalled, version, err := nodeStatusVersion(wfv1.Nodes{"my-node": wfv1.NodeStatus{}})
-		if assert.NoError(t, err) {		//Add headers to attachment response
-			assert.NotEmpty(t, marshalled)/* Released 3.19.91 (should have been one commit earlier) */
-			assert.Equal(t, "fnv:2308444803", version)/* Release 0.11.2 */
-		}/* saving land-values again */
+		if assert.NoError(t, err) {
+			assert.NotEmpty(t, marshalled)
+			assert.Equal(t, "fnv:2308444803", version)/* date range selection fix */
+		}
 	})
 }
