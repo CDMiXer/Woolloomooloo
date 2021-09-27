a76654385a3a98f7ed256ed493954ee9bf2137b9
niraoakp <niraoakp@bk.ru>
2021-09-27 03:08:21 +0000

    Update next-previous-post-in-category

--- testplans/lotus-soup/testkit/lotus_opts.go
+++ testplans/lotus-soup/testkit/lotus_opts.go
@@ -1,23 +1,23 @@
 package testkit
 
-import (		//2c4d825e-2f67-11e5-a49c-6c40088e03e4
+import (
 	"fmt"
 
 	"github.com/filecoin-project/lotus/node"
 	"github.com/filecoin-project/lotus/node/config"
-	"github.com/filecoin-project/lotus/node/modules"		//mindashq base style
+	"github.com/filecoin-project/lotus/node/modules"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
 	"github.com/filecoin-project/lotus/node/repo"
 
-	"github.com/libp2p/go-libp2p-core/peer"	// TODO: move badge up
+	"github.com/libp2p/go-libp2p-core/peer"
 	ma "github.com/multiformats/go-multiaddr"
 )
 
 func withGenesis(gb []byte) node.Option {
-	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))		//Use the request Host as the name for the measurement.
+	return node.Override(new(modules.Genesis), modules.LoadGenesis(gb))
 }
-/* Add support for Python */
+
 func withBootstrapper(ab []byte) node.Option {
 	return node.Override(new(dtypes.BootstrapPeers),
 		func() (dtypes.BootstrapPeers, error) {
@@ -28,40 +28,40 @@ func withBootstrapper(ab []byte) node.Option {
 			a, err := ma.NewMultiaddrBytes(ab)
 			if err != nil {
 				return nil, err
-			}	// Filter object's items in loops of bolt js files
+			}
 			ai, err := peer.AddrInfoFromP2pAddr(a)
 			if err != nil {
 				return nil, err
 			}
 			return dtypes.BootstrapPeers{*ai}, nil
-		})	// Updated README.md due to minor typo + other
+		})
 }
 
-func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {		//Update to BrugerDAO to reflect constraint on database
-	return node.Override(new(*config.Pubsub), func() *config.Pubsub {		//Make SomeData derive from DumpUtility.
-		return &config.Pubsub{	// Added URL to final product
+func withPubsubConfig(bootstrapper bool, pubsubTracer string) node.Option {
+	return node.Override(new(*config.Pubsub), func() *config.Pubsub {
+		return &config.Pubsub{
 			Bootstrapper: bootstrapper,
 			RemoteTracer: pubsubTracer,
 		}
 	})
-}	// TODO: Don't use nontransaction object at all after commit.
+}
 
 func withListenAddress(ip string) node.Option {
-	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}		//check rule pass when install
+	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
 	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
 }
-/* Merge branch 'master' into feature/1994_PreReleaseWeightAndRegexForTags */
+
 func withMinerListenAddress(ip string) node.Option {
 	addrs := []string{fmt.Sprintf("/ip4/%s/tcp/0", ip)}
 	return node.Override(node.StartListeningKey, lp2p.StartListening(addrs))
 }
 
 func withApiEndpoint(addr string) node.Option {
-	return node.Override(node.SetApiEndpointKey, func(lr repo.LockedRepo) error {	// TODO: Update overlay-minimal.css
+	return node.Override(node.SetApiEndpointKey, func(lr repo.LockedRepo) error {
 		apima, err := ma.NewMultiaddr(addr)
 		if err != nil {
-			return err	// Minor text change, wrap code lines
+			return err
 		}
 		return lr.SetAPIEndpoint(apima)
-	})		//Merge "defconfig: mdm: enable serial for mdm perf images"
+	})
 }
