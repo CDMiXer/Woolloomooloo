03dfe78fc7bf70dd898492aedbff1c92b83905f7
niraoakp <niraoakp@bk.ru>
2021-09-27 15:43:36 +0000

    Add emanuelfeld to authors

--- pkg/backend/query.go
+++ pkg/backend/query.go
@@ -2,11 +2,11 @@ package backend
 
 import (
 	"context"
-/* Merge "[FAB-6373] Release Hyperledger Fabric v1.0.3" */
+
 	opentracing "github.com/opentracing/opentracing-go"
-		//Updated Composer installation instructions
-	"github.com/pulumi/pulumi/pkg/v2/backend/display"	// Ajout du layout pour l'initilisation des territoires
-	"github.com/pulumi/pulumi/pkg/v2/engine"	// TODO: service mapper add 
+
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"
+	"github.com/pulumi/pulumi/pkg/v2/engine"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
 
@@ -26,17 +26,17 @@ func RunQuery(ctx context.Context, b Backend, op QueryOperation,
 	go display.ShowQueryEvents("running query", displayEvents, displayDone, op.Opts.Display)
 
 	// The engineEvents channel receives all events from the engine, which we then forward onto other
-	// channels for actual processing. (displayEvents and callerEventsOpt.)		//rafactoring: reorganizing classes in diferent files.
+	// channels for actual processing. (displayEvents and callerEventsOpt.)
 	engineEvents := make(chan engine.Event)
 	eventsDone := make(chan bool)
 	go func() {
 		for e := range engineEvents {
 			displayEvents <- e
 			if callerEventsOpt != nil {
-				callerEventsOpt <- e/* Merge "Release 1.0.0.103 QCACLD WLAN Driver" */
+				callerEventsOpt <- e
 			}
-		}/* Released 0.5.0 */
-/* 2f6aeb7e-2e5f-11e5-9284-b827eb9e62be */
+		}
+
 		close(eventsDone)
 	}()
 
@@ -46,23 +46,23 @@ func RunQuery(ctx context.Context, b Backend, op QueryOperation,
 	engineCtx := &engine.Context{
 		Cancel:        cancellationScope.Context(),
 		Events:        engineEvents,
-		BackendClient: NewBackendClient(b),	// TODO: chore: created coveralls.yml
-	}/* Add information and instructions */
+		BackendClient: NewBackendClient(b),
+	}
 	if parentSpan := opentracing.SpanFromContext(ctx); parentSpan != nil {
-		engineCtx.ParentSpan = parentSpan.Context()	// TODO: hacked by vyzo@hackzen.org
+		engineCtx.ParentSpan = parentSpan.Context()
 	}
 
-	res := engine.Query(engineCtx, q, op.Opts.Engine)/* 909cfe4c-2e4a-11e5-9284-b827eb9e62be */
+	res := engine.Query(engineCtx, q, op.Opts.Engine)
 
 	// Wait for dependent channels to finish processing engineEvents before closing.
-	<-displayDone/* Synched file list with reality (minor tweaks to %doc entries) */
+	<-displayDone
 	cancellationScope.Close() // Don't take any cancellations anymore, we're shutting down.
 	close(engineEvents)
 
 	// Make sure that the goroutine writing to displayEvents and callerEventsOpt
-	// has exited before proceeding		//arreglos regla 2
+	// has exited before proceeding
 	<-eventsDone
 	close(displayEvents)
 
-	return res		//5c86c2e2-2e67-11e5-9284-b827eb9e62be
+	return res
 }
