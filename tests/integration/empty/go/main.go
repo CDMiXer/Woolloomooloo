bf784d1542a1bf348db68ba469d2481e3d1822d6
niraoakp <niraoakp@bk.ru>
2021-09-27 08:47:53 +0000

    Merge "FAB-12060 payload buf don't signal ready if empty"

--- tests/integration/empty/go/main.go
+++ tests/integration/empty/go/main.go
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-package main
+package main		//bug fix on DooFileCache set/get not storing at the defined folders.
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
-
+)/* Added JSSymbolicRegressionProblemTest. */
+/* Adding ReleaseNotes.txt to track current release notes. Fixes issue #471. */
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		return nil
