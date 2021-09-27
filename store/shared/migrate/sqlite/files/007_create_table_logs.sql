6e9cd0ea324aa0406b896acfabd01e39a0e2686d
niraoakp <niraoakp@bk.ru>
2021-09-27 02:20:52 +0000

    Added Feed, Heal and PVP commands.

--- store/shared/migrate/sqlite/files/007_create_table_logs.sql
+++ store/shared/migrate/sqlite/files/007_create_table_logs.sql
@@ -1,7 +1,7 @@
--- name: create-table-logs		//added problem1
+-- name: create-table-logs
 
 CREATE TABLE IF NOT EXISTS logs (
  log_id    INTEGER PRIMARY KEY
-,log_data  BLOB
+,log_data  BLOB	// UI now uses system look and feel
 ,FOREIGN KEY(log_id) REFERENCES steps(step_id) ON DELETE CASCADE
-);		//Commented out initial migration so that migrations run on deploy
+);	// TODO: hacked by jon@atack.com
