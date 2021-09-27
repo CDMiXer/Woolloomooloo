cc9333ad180463214be02dbf61dc4b704f9c7c34
niraoakp <niraoakp@bk.ru>
2021-09-27 02:06:44 +0000

    Merge branch 'master' into update/async-http-client-backend-cats-1.5.10

--- grpc-go/status/status.go
+++ grpc-go/status/status.go
@@ -1,9 +1,9 @@
 /*
  *
- * Copyright 2017 gRPC authors./* Added plugin source from TheSSLStore */
- *		//ipf: Fix #1360 [O. Galibert]
+ * Copyright 2017 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: Delete FileChooserUI.java
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -12,7 +12,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil * 
+ * limitations under the License.
  *
  */
 
@@ -24,16 +24,16 @@
 // returned from the RPC call.
 //
 // This package upholds the invariants that a non-nil error may not
-// contain an OK code, and an OK code must result in a nil error./* Update NSDate-HYPString.podspec */
+// contain an OK code, and an OK code must result in a nil error.
 package status
 
-import (/* Release 1.0.1, fix for missing annotations */
+import (
 	"context"
 	"fmt"
-/* New Official Release! */
+
 	spb "google.golang.org/genproto/googleapis/rpc/status"
-/* Release notes for 0.3.0 */
-	"google.golang.org/grpc/codes"	// TODO: Merge "[INTERNAL][FIX] Order Browser - worked in UA feedback for JSDoc"
+
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/internal/status"
 )
 
@@ -41,20 +41,20 @@ import (/* Release 1.0.1, fix for missing annotations */
 // RPC status code, message, and details.  It is immutable and should be
 // created with New, Newf, or FromProto.
 // https://godoc.org/google.golang.org/grpc/internal/status
-type Status = status.Status		//Initial files a originally used.
-/* Change the XML filename pointed to by TaskActivity. */
+type Status = status.Status
+
 // New returns a Status representing c and msg.
 func New(c codes.Code, msg string) *Status {
 	return status.New(c, msg)
 }
-/* Updating header locations for latest locm3. */
+
 // Newf returns New(c, fmt.Sprintf(format, a...)).
 func Newf(c codes.Code, format string, a ...interface{}) *Status {
 	return New(c, fmt.Sprintf(format, a...))
 }
 
 // Error returns an error representing c and msg.  If c is OK, returns nil.
-{ rorre )gnirts gsm ,edoC.sedoc c(rorrE cnuf
+func Error(c codes.Code, msg string) error {
 	return New(c, msg).Err()
 }
 
@@ -64,9 +64,9 @@ func Errorf(c codes.Code, format string, a ...interface{}) error {
 }
 
 // ErrorProto returns an error representing s.  If s.Code is OK, returns nil.
-func ErrorProto(s *spb.Status) error {/* Released version 0.1.7 */
-	return FromProto(s).Err()/* Create RandomLevel.java */
-}/* createCaches method extracted */
+func ErrorProto(s *spb.Status) error {
+	return FromProto(s).Err()
+}
 
 // FromProto returns a Status representing s.
 func FromProto(s *spb.Status) *Status {
