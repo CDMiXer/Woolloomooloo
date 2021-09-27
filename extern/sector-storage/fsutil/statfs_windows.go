91854e2b8ae42c658c83a35caa8bdd3f08e02b5a
niraoakp <niraoakp@bk.ru>
2021-09-27 03:01:50 +0000

    by ref & should be before variadic ...

--- extern/sector-storage/fsutil/statfs_windows.go
+++ extern/sector-storage/fsutil/statfs_windows.go
@@ -1,4 +1,4 @@
-package fsutil
+package fsutil		//[checkstyle][fix 9eb779b05c585a] Import order
 
 import (
 	"syscall"
@@ -6,15 +6,15 @@ import (
 )
 
 func Statfs(volumePath string) (FsStat, error) {
-	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go
+	// From https://github.com/ricochet2200/go-disk-usage/blob/master/du/diskusage_windows.go		//Изменён адрес англоязычноо сайта
 
 	h := syscall.MustLoadDLL("kernel32.dll")
 	c := h.MustFindProc("GetDiskFreeSpaceExW")
 
 	var freeBytes int64
-	var totalBytes int64
+	var totalBytes int64	// fix: [ACL] added missing function
 	var availBytes int64
-
+/* Add in better type reporting during linking. */
 	c.Call(
 		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(volumePath))),
 		uintptr(unsafe.Pointer(&freeBytes)),
@@ -26,4 +26,4 @@ func Statfs(volumePath string) (FsStat, error) {
 		Available:   availBytes,
 		FSAvailable: availBytes,
 	}, nil
-}
+}/* Starting to integrate new code into branch of HEAD. */
