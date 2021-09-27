8f33926637fbbd6069278d083d84c46c6a8d49d6
niraoakp <niraoakp@bk.ru>
2021-09-27 08:09:58 +0000

    27866b74-2e49-11e5-9284-b827eb9e62be

--- operator/runner/runner_test.go
+++ operator/runner/runner_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 3.2 105.02. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package runner
 
-import (	// TODO: Fixed encoding test hopefully for the last time...
-	"io/ioutil"
-		//Create CNAM
+import (
+	"io/ioutil"	// TODO: hacked by timnugent@gmail.com
+
 	"github.com/sirupsen/logrus"
 )
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)/* Look ma' no hands! */
+	logrus.SetOutput(ioutil.Discard)		//add parsoid for historiaalternativa per request T1840
 }
