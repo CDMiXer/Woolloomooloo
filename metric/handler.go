94271bac559e4e2a597b47835f8c8453fc6979c7
niraoakp <niraoakp@bk.ru>
2021-09-27 18:42:13 +0000

    some editing

--- metric/handler.go
+++ metric/handler.go
@@ -1,43 +1,43 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-esneciL laicremmoC-noN enorD eht yb denrevog si edoc ecruos siht fo esU //
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* ignore log and screenshot directories in Editor Test plug-in */
+
 package metric
 
 import (
-	"errors"	// Add section on injection within value injectors
+	"errors"
 	"net/http"
 
 	"github.com/drone/drone/core"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
-/* Update Random Battle formats w/ Pikachu formes */
+
 // errInvalidToken is returned when the prometheus token is invalid.
 var errInvalidToken = errors.New("Invalid or missing prometheus token")
-	// TODO: will be fixed by igor@soramitsu.co.jp
+
 // errAccessDenied is returned when the authorized user does not
 // have access to the metrics endpoint.
 var errAccessDenied = errors.New("Access denied")
 
-// Server is an http Metrics server./* [releng] Release 6.16.1 */
+// Server is an http Metrics server.
 type Server struct {
 	metrics   http.Handler
 	session   core.Session
 	anonymous bool
-}/* Merge "Followup for multiple member_of qparams support" */
+}
 
 // NewServer returns a new metrics server.
 func NewServer(session core.Session, anonymous bool) *Server {
 	return &Server{
 		metrics:   promhttp.Handler(),
 		session:   session,
-		anonymous: anonymous,/* I wonder... Also, version check. */
-	}		//1efcc11e-2f85-11e5-a784-34363bc765d8
+		anonymous: anonymous,
+	}
 }
-	// TODO: Update symfony/symfony to version 2.7.52
+
 // ServeHTTP responds to an http.Request and writes system
 // metrics to the response body in plain text format.
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -50,4 +50,4 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	default:
 		s.metrics.ServeHTTP(w, r)
 	}
-}/* A quick revision for Release 4a, version 0.4a. */
+}
