730ac6433683d59efb38e8088a56043b944f8f75
niraoakp <niraoakp@bk.ru>
2021-09-27 04:31:57 +0000

    Set landscape-client-common as a dep.

--- tests/integration/empty/gorun/main.go
+++ tests/integration/empty/gorun/main.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-niam egakcap
+package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)		//spring+redis
+)		//Merge "configure: reference the README for missing yasm"
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
+	pulumi.Run(func(ctx *pulumi.Context) error {	// TODO: hacked by yuvalalaluf@gmail.com
 		return nil
 	})
 }
