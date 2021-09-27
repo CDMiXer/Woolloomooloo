d6019257a0e3a46b1dd113e7b8fc484a5f3cc456
niraoakp <niraoakp@bk.ru>
2021-09-27 05:22:03 +0000

    Only get format name

--- chain/store/basefee_test.go
+++ chain/store/basefee_test.go
@@ -3,21 +3,21 @@ package store
 import (
 	"fmt"
 	"testing"
-
-	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"
+	// TODO: hacked by alan.shaw@protocol.ai
+	"github.com/filecoin-project/lotus/build"	// TODO: 0ad3fb54-2e67-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/chain/types"/* Jenkinsfile - Increase job timeout from 60 -> 90 */
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBaseFee(t *testing.T) {
+func TestBaseFee(t *testing.T) {/* Release 0.2 version */
 	tests := []struct {
 		basefee             uint64
-		limitUsed           int64
+		limitUsed           int64/* publish DHCP log data implemented */
 		noOfBlocks          int
 		preSmoke, postSmoke uint64
 	}{
 		{100e6, 0, 1, 87.5e6, 87.5e6},
-		{100e6, 0, 5, 87.5e6, 87.5e6},
+		{100e6, 0, 5, 87.5e6, 87.5e6},/* google analytics tracking included  */
 		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},
 		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},
 		{100e6, build.BlockGasLimit * 2, 2, 112.5e6, 112.5e6},
@@ -25,13 +25,13 @@ func TestBaseFee(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		test := test
+		test := test/* Version number update, MIT license. */
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
-			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)
+			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)/* Fix bug #137044: ftp password handling broken */
 			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())
 
 			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)
-			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())
+			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())	// TODO: Update CHANGELOG for #6533
 		})
 	}
 }
