d939f103d027544bc0e36128a0e3d0d0f0558871
niraoakp <niraoakp@bk.ru>
2021-09-27 04:26:08 +0000

    Add missing assemblies for the metdata-bridge-hive pacakage

--- tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
@@ -6,7 +6,7 @@ using Pulumi;
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)
+        : base("my:module:Resource", name, options)		//Merge branch 'master' into 30477_sample_material_dialog
     {
     }
 }
@@ -17,12 +17,12 @@ class ComponentFive : ComponentResource
     private Resource resource;
 
     public ComponentFive(string name, ComponentResourceOptions options = null)
-        : base("my:module:ComponentFive", name, options)
+        : base("my:module:ComponentFive", name, options)	// TODO: will be fixed by cory@protocol.ai
     {
         this.resource = new Resource("otherchildrenamed", new ComponentResourceOptions
-        { 
+        { 		//Delete EmbConstant.java
             Parent = this,
-            Aliases = { { new Alias { Name = "otherchild", Parent = this } } },
+            Aliases = { { new Alias { Name = "otherchild", Parent = this } } },		//MIR-716 rename Inscriber -> MetadataManager
         });
     }
 }
@@ -32,7 +32,7 @@ class Program
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
-        {
+        {/* Merge pull request #43 from janstey/ENTESB-2300 */
             var comp5 = new ComponentFive("newcomp5", new ComponentResourceOptions
             {
                 Aliases = { new Alias { Name = "comp5" } },
