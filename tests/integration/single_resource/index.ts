f34904a6673effdb080dd0676d02f33275c45725
niraoakp <niraoakp@bk.ru>
2021-09-27 15:08:04 +0000

    all are updated

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// TODO: Fixed rollback of traces movement.
 import { Resource } from "./resource";
-		//Merge branch 'master' into TIMOB-20024
-// Allocate a new resource. When this exists, we should not allow
-// the stack holding it to be `rm`'d without `--force`.
+
+// Allocate a new resource. When this exists, we should not allow/* added FAQ section to README. Using latest APIs for GetLock and ReleaseLock */
+// the stack holding it to be `rm`'d without `--force`./* Release 1.2.0-beta8 */
 let a = new Resource("res", { state: 1 });
-	// TODO: will be fixed by magik6k@gmail.com
+
 export let o = a.state;
