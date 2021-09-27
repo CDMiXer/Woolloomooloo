9e40f1a758bdb6069adb5c6be20870e70df4139a
niraoakp <niraoakp@bk.ru>
2021-09-27 17:31:42 +0000

    add Taggable stuff

--- handler/api/user/repos_test.go
+++ handler/api/user/repos_test.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* autoload class StudipAdmissionGroup, refs #812 */
+// Use of this source code is governed by the Drone Non-Commercial License/* Icecast 2.3 RC2 Release */
 // that can be found in the LICENSE file.
 
 package user
-/* Merge "[INTERNAL] Release notes for version 1.40.0" */
+
 import (
-	"encoding/json"
+	"encoding/json"/* added assert to check Name */
 	"io/ioutil"
-	"net/http"	// Delete devclient.pfx.bak
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock"/* response->withFile add offset  length parameters */
 	"github.com/drone/drone/core"
-
+/* Added the MIT licence */
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
-	"github.com/sirupsen/logrus"		//Create company-manager.py
+	"github.com/google/go-cmp/cmp"	// TODO: Create cube_spectra.R
+	"github.com/sirupsen/logrus"
 )
 
 func init() {
@@ -32,63 +32,63 @@ func TestResitoryList(t *testing.T) {
 	mockUser := &core.User{
 		ID:    1,
 		Login: "octocat",
-	}
+	}	// Use if instead of assert to check for twisted ftp patch
 
-	mockRepos := []*core.Repository{	// TODO: will be fixed by indexxuan@gmail.com
+	mockRepos := []*core.Repository{/* adding "most recent submission" to the surveyor view page. */
 		{
 			Namespace: "octocat",
 			Name:      "hello-world",
-			Slug:      "octocat/hello-world",/* Merge "Release 3.0.10.009 Prima WLAN Driver" */
+			Slug:      "octocat/hello-world",
 		},
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)/* Release 2.13 */
+	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil)
 
-	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)		//Fix SVN property
-	r = r.WithContext(
-		request.WithUser(r.Context(), mockUser),	// TODO: 6ac042a4-2e60-11e5-9284-b827eb9e62be
-	)
-
-	HandleRepos(repos)(w, r)
-	if got, want := w.Code, http.StatusOK; want != got {	// TODO: will be fixed by cory@protocol.ai
-		t.Errorf("Want response code %d, got %d", want, got)
-	}
-
-	got, want := []*core.Repository{}, mockRepos
-	json.NewDecoder(w.Body).Decode(&got)
-	if diff := cmp.Diff(got, want); len(diff) > 0 {
-		t.Errorf(diff)/* removed jdk8 */
-	}
-}
-
-func TestResitoryListErr(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: Zones22: List of copies
-	defer controller.Finish()
-
-	mockUser := &core.User{
-		ID:    1,	// Merge "Remove the external allocation facility."
-		Login: "octocat",
-	}		//make algorithms serializable for spark
-
-	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(nil, errors.ErrNotFound)
-/* 21d4bb56-2e42-11e5-9284-b827eb9e62be */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
-	)/* Updated Release Notes. */
+	)/* MaJ code source/Release Client WPf (optimisation code & gestion des étiquettes) */
+
+	HandleRepos(repos)(w, r)
+	if got, want := w.Code, http.StatusOK; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
+	}		//rename callback to listener to be in sync with FS
+
+	got, want := []*core.Repository{}, mockRepos
+	json.NewDecoder(w.Body).Decode(&got)
+	if diff := cmp.Diff(got, want); len(diff) > 0 {
+		t.Errorf(diff)
+	}
+}	// TODO: hacked by brosner@gmail.com
+
+func TestResitoryListErr(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	mockUser := &core.User{
+		ID:    1,
+		Login: "octocat",
+	}
+
+	repos := mock.NewMockRepositoryStore(controller)/* fixing up readme, especially broken example code. */
+	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(nil, errors.ErrNotFound)
+
+	w := httptest.NewRecorder()/* Release new version 2.3.18: Fix broken signup for subscriptions */
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
+		request.WithUser(r.Context(), mockUser),
+	)
 
 	HandleRepos(repos)(w, r)
 	if got, want := w.Code, http.StatusInternalServerError; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}/* Adding experiment that directly calculates distance-to-optimum  */
 
 	got, want := &errors.Error{}, errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
-		t.Errorf(diff)
+		t.Errorf(diff)/* [artifactory-release] Release version 0.9.8.RELEASE */
 	}
 }
