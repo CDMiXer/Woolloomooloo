f1b5bf84a27389d06fd8576b624ab0f86fd85b43
niraoakp <niraoakp@bk.ru>
2021-09-27 19:03:45 +0000

    Add a triple.

--- handler/api/system/limits.go
+++ handler/api/system/limits.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// add a cobertura check goal it.
 
-// +build !oss
+// +build !oss/* Added Request class for common operations on HttpServletRequest */
 
 package system
