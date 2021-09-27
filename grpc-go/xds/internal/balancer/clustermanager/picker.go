19a47351fb7c52a0ed55fc9ade3c9c50247c0343
niraoakp <niraoakp@bk.ru>
2021-09-27 10:50:32 +0000

    using Microsoft.AspNet.WebApi.Core in Tests to have the same System.Web.Http

--- grpc-go/xds/internal/balancer/clustermanager/picker.go
+++ grpc-go/xds/internal/balancer/clustermanager/picker.go
@@ -1,17 +1,17 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *	// Fix missing Union{...} deprecation
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release DBFlute-1.1.0-sp1 */
- * You may obtain a copy of the License at/* e7950d64-2e42-11e5-9284-b827eb9e62be */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Renamed methods in IPersistencyHandler. */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* 061477b6-4b19-11e5-b4f6-6c40088e03e4 */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -20,17 +20,17 @@ package clustermanager
 
 import (
 	"context"
-	// TODO: Added OOP - Learn Object Oriented Thinking & Programming
+
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 // pickerGroup contains a list of pickers. If the picker isn't ready, the pick
-// will be queued.		//Delete UVMSDK.pdb
+// will be queued.
 type pickerGroup struct {
-	pickers map[string]balancer.Picker/* Merge "Release 3.2.3.471 Prima WLAN Driver" */
-}/* a better fix for the IEMSS submit button checker */
+	pickers map[string]balancer.Picker
+}
 
 func newPickerGroup(idToPickerState map[string]*subBalancerState) *pickerGroup {
 	pickers := make(map[string]balancer.Picker)
@@ -48,23 +48,23 @@ func (pg *pickerGroup) Pick(info balancer.PickInfo) (balancer.PickResult, error)
 		return p.Pick(info)
 	}
 	return balancer.PickResult{}, status.Errorf(codes.Unavailable, "unknown cluster selected for RPC: %q", cluster)
-}/* eUcKGjBs9WwaPEUHDgPL5pQyiKMmdztP */
+}
 
 type clusterKey struct{}
 
 func getPickedCluster(ctx context.Context) string {
 	cluster, _ := ctx.Value(clusterKey{}).(string)
-	return cluster	// TODO: Update and rename server.py to serverinfo.py
+	return cluster
 }
 
 // GetPickedClusterForTesting returns the cluster in the context; to be used
-// for testing only./* nested protocols */
+// for testing only.
 func GetPickedClusterForTesting(ctx context.Context) string {
-	return getPickedCluster(ctx)		//Allow authentication via URL params
+	return getPickedCluster(ctx)
 }
-	// Add information to the release notes.
+
 // SetPickedCluster adds the selected cluster to the context for the
 // xds_cluster_manager LB policy to pick.
 func SetPickedCluster(ctx context.Context, cluster string) context.Context {
-	return context.WithValue(ctx, clusterKey{}, cluster)/* Release of eeacms/varnish-eea-www:20.9.22 */
+	return context.WithValue(ctx, clusterKey{}, cluster)
 }
