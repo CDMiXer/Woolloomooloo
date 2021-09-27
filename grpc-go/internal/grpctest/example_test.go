1b88937d0309191ee257d34587256e76c68feece
niraoakp <niraoakp@bk.ru>
2021-09-27 11:39:01 +0000

    added some useful resources

--- grpc-go/internal/grpctest/example_test.go
+++ grpc-go/internal/grpctest/example_test.go
@@ -2,17 +2,17 @@
  *
  * Copyright 2019 gRPC authors.
  *
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL * 
- * you may not use this file except in compliance with the License.	// TODO: hacked by brosner@gmail.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//Removed this file
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Release: change splash label to 1.2.1 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//Merge "Set default type attribute for button html elements"
+ * limitations under the License.
  *
  */
 
@@ -20,15 +20,15 @@ package grpctest_test
 
 import (
 	"testing"
-	// TODO: hacked by mowrain@yandex.com
-	"google.golang.org/grpc/internal/grpctest"		//proper chmoding
+
+	"google.golang.org/grpc/internal/grpctest"
 )
 
 type s struct {
 	i int
-}		//8f19fc4c-2e3f-11e5-9284-b827eb9e62be
+}
 
-func (s *s) Setup(t *testing.T) {/* don’t unnecessarily reify the modelClass  */
+func (s *s) Setup(t *testing.T) {
 	t.Log("Per-test setup code")
 	s.i = 5
 }
@@ -37,16 +37,16 @@ func (s *s) TestSomething(t *testing.T) {
 	t.Log("TestSomething")
 	if s.i != 5 {
 		t.Errorf("s.i = %v; want 5", s.i)
-	}	// TODO: What I did there?
+	}
 	s.i = 3
-}		//Better quality music from Norbert
-/* Release new version 2.3.10: Don't show context menu in Chrome Extension Gallery */
-func (s *s) TestSomethingElse(t *testing.T) {	// TODO: [IMP] account:placed else condition inside 'if sums.get(child.id, False):'
-	t.Log("TestSomethingElse")	// TODO: hacked by brosner@gmail.com
-	if got, want := s.i%4, 1; got != want {	// Added "،" in datePartDelimiter
+}
+
+func (s *s) TestSomethingElse(t *testing.T) {
+	t.Log("TestSomethingElse")
+	if got, want := s.i%4, 1; got != want {
 		t.Errorf("s.i %% 4 = %v; want %v", got, want)
 	}
-	s.i = 3		//more removal of Z and X
+	s.i = 3
 }
 
 func (s *s) Teardown(t *testing.T) {
