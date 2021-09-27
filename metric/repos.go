13caf2560d4bc56b92eb3eb10fb0c4b850ff7617
niraoakp <niraoakp@bk.ru>
2021-09-27 05:56:17 +0000

    Started #2

--- metric/repos.go
+++ metric/repos.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Remove varNameRegExp since it's never used;
-// +build !oss		//Fully beautified version
+
+// +build !oss
 
 package metric
-		//Set the stroke and paint for lines if they are drawn.
-import (	// Fix for SID error
+
+import (		//Merge "Vector: Update comments in vector.js"
 	"github.com/drone/drone/core"
 
-	"github.com/prometheus/client_golang/prometheus"/* a5640dbc-2e3f-11e5-9284-b827eb9e62be */
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 // RepoCount registers the repository metrics.
 func RepoCount(repos core.RepositoryStore) {
-	prometheus.MustRegister(
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{		//return non 0 on err
-			Name: "drone_repo_count",	// TODO: Fix missing language list
-			Help: "Total number of registered repositories.",	// TODO: Added Thai and tests
+	prometheus.MustRegister(		//Update Big-Picture.xml
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+			Name: "drone_repo_count",
+			Help: "Total number of registered repositories.",
 		}, func() float64 {
 			i, _ := repos.Count(noContext)
 			return float64(i)
 		}),
-	)
+	)/* 31a180ee-2e4e-11e5-9284-b827eb9e62be */
 }
