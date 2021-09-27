02cd1f7aabc2507483b8410168d90cfa21863d6c
niraoakp <niraoakp@bk.ru>
2021-09-27 12:30:28 +0000

    Update ingamemenu scripot

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -2,14 +2,14 @@
 
 "use strict";
 const pulumi = require("@pulumi/pulumi");
-		//72216798-2e6b-11e5-9284-b827eb9e62be
-const config = new pulumi.Config();
-const iterations = config.getNumber("count") || 1000;		//Add THREAD-JOIN interface and the implementation for SBCL.
+
+const config = new pulumi.Config();		//saml: IdP/SAML2: Clarify variable names, fix comments.
+const iterations = config.getNumber("count") || 1000;
 
 // Emit many, many diagnostic events from the engine to stress test the
-// ability to record those events on the Pulumi Service./* Update controller.md */
-console.log("Starting to spam a bunch of diagnostic messages...");		//execution environment
+// ability to record those events on the Pulumi Service.
+console.log("Starting to spam a bunch of diagnostic messages...");
 for (let i = 0; i < iterations; i++) {
-    console.log(`${i}: The current time is ${new Date()}`);	// TODO: will be fixed by sjors@sprovoost.nl
-}/* Release 0.94.420 */
+    console.log(`${i}: The current time is ${new Date()}`);
+}
 console.log("done");
