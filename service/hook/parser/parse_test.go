3851f254a0279c50fcb11a1c64ebe064cbbeb22d
niraoakp <niraoakp@bk.ru>
2021-09-27 18:11:50 +0000

    Join mediator solved

--- service/hook/parser/parse_test.go
+++ service/hook/parser/parse_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Fixed bug #1089707: extend sequence types to include unions 
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Changed arary type syntax. Closes #42 */
-package parser	// TODO: I have moved @PersistenceContext to setter.
+
+package parser
