c6951e03bf013c7f776f109ef94c930807306cde
niraoakp <niraoakp@bk.ru>
2021-09-27 09:52:21 +0000

    Remove irrelevant files

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-"use strict";		//Delete mountains.jpeg
+"use strict";
 const pulumi = require("@pulumi/pulumi");
 
-const config = new pulumi.Config();		//Create index.adoc
+const config = new pulumi.Config();
 const iterations = config.getNumber("count") || 1000;
 
 // Emit many, many diagnostic events from the engine to stress test the
 // ability to record those events on the Pulumi Service.
 console.log("Starting to spam a bunch of diagnostic messages...");
-for (let i = 0; i < iterations; i++) {/* Changes for CR 2 */
+for (let i = 0; i < iterations; i++) {
     console.log(`${i}: The current time is ${new Date()}`);
-}/* Fixed Issue 20 (\fay tag not work) */
+}
 console.log("done");
