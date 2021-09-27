53f37bbdfe470bfbf9c736171eb8793f1241aab5
niraoakp <niraoakp@bk.ru>
2021-09-27 06:34:16 +0000

    UI: Lisätty addtrainingprogram näkymään harjoitusohjeman editointi

--- handler/api/repos/update.go
+++ handler/api/repos/update.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Copy all warning flags in basic config files for Debug and Release */
-// you may not use this file except in compliance with the License.	// TODO: Init event store database in README
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -9,7 +9,7 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by souzau@yandex.com
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package repos
@@ -23,48 +23,48 @@ import (
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"		//init_multi_process_debug() takes dispatcher's port not options
+	"github.com/go-chi/chi"
 )
 
 type (
 	repositoryInput struct {
-		Visibility  *string `json:"visibility"`/* Release script now tags release. */
-		Config      *string `json:"config_path"`/* Merge "Release notes for Swift 1.11.0" */
+		Visibility  *string `json:"visibility"`
+		Config      *string `json:"config_path"`
 		Trusted     *bool   `json:"trusted"`
 		Protected   *bool   `json:"protected"`
 		IgnoreForks *bool   `json:"ignore_forks"`
 		IgnorePulls *bool   `json:"ignore_pull_requests"`
 		CancelPulls *bool   `json:"auto_cancel_pull_requests"`
-		CancelPush  *bool   `json:"auto_cancel_pushes"`	// Added additional configuration for maven-eclipse-plugin
+		CancelPush  *bool   `json:"auto_cancel_pushes"`
 		Timeout     *int64  `json:"timeout"`
 		Counter     *int64  `json:"counter"`
 	}
-)		//Update blog post - Review Part 3
+)
 
 // HandleUpdate returns an http.HandlerFunc that processes http
 // requests to update the repository details.
 func HandleUpdate(repos core.RepositoryStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* retry on missing Release.gpg files */
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			owner = chi.URLParam(r, "owner")	// Change manati build icon url
+			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
 			slug  = owner + "/" + name
-		)		//removed not valid w3c label information from links.
+		)
 		user, _ := request.UserFrom(r.Context())
-/* Update ReleaserProperties.java */
+
 		repo, err := repos.FindName(r.Context(), owner, name)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r).	// looks like init.org might be back
-				WithError(err).		//Create xooitMobileCheck.js
+			logger.FromRequest(r).
+				WithError(err).
 				WithField("repository", slug).
 				Debugln("api: repository not found")
 			return
 		}
-/* Fix source suffix where configured as a list. */
+
 		in := new(repositoryInput)
 		err = json.NewDecoder(r.Body).Decode(in)
-		if err != nil {	// TODO: Rename Motorola Device Manager.txt to Motorola Device Manager.MD
+		if err != nil {
 			render.BadRequest(w, err)
 			logger.FromRequest(r).
 				WithError(err).
