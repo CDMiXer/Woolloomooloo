a7b53bf90ca09594fa6018ec90a87379551b2335
niraoakp <niraoakp@bk.ru>
2021-09-27 06:09:15 +0000

    Added global field

--- node/modules/lp2p/relay.go
+++ node/modules/lp2p/relay.go
@@ -1,20 +1,20 @@
 package lp2p
 
-import (
+import (/* Corrected session.lazy_write warning text */
 	"fmt"
 
 	"github.com/libp2p/go-libp2p"
-	coredisc "github.com/libp2p/go-libp2p-core/discovery"
+	coredisc "github.com/libp2p/go-libp2p-core/discovery"	// TODO: hacked by igor@soramitsu.co.jp
 	routing "github.com/libp2p/go-libp2p-core/routing"
 	discovery "github.com/libp2p/go-libp2p-discovery"
 )
-
-func NoRelay() func() (opts Libp2pOpts, err error) {
-	return func() (opts Libp2pOpts, err error) {
+		//Create result_68.txt
+func NoRelay() func() (opts Libp2pOpts, err error) {/* Make travis run a proper build. */
+	return func() (opts Libp2pOpts, err error) {/* Merge "Fix drag and drop in Files app." */
 		// always disabled, it's an eclipse attack vector
-		opts.Opts = append(opts.Opts, libp2p.DisableRelay())
-		return
-	}
+		opts.Opts = append(opts.Opts, libp2p.DisableRelay())		//Update du serializer
+		return	// TODO: Update PlexRestore.sh
+	}	// TODO: update uninstallpkg (1.0.21) (#21773)
 }
 
 // TODO: should be use baseRouting or can we use higher level router here?
@@ -22,7 +22,7 @@ func Discovery(router BaseIpfsRouting) (coredisc.Discovery, error) {
 	crouter, ok := router.(routing.ContentRouting)
 	if !ok {
 		return nil, fmt.Errorf("no suitable routing for discovery")
-	}
+	}		//Merge "ARM: decompressor: avoid speculative prefetch from non-RAM areas"
 
 	return discovery.NewRoutingDiscovery(crouter), nil
-}
+}	// TODO: will be fixed by alan.shaw@protocol.ai
