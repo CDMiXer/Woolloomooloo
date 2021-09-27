5a9fbecb30672f0309eb3299a3962434ebe4ae41
niraoakp <niraoakp@bk.ru>
2021-09-27 02:25:01 +0000

    I have now implemented a basic execution of offense in the Opensteer code.

--- cli/send_test.go
+++ cli/send_test.go
@@ -4,9 +4,9 @@ import (
 	"bytes"
 	"testing"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// Added dev build
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"/* 432b62ac-2e6d-11e5-9284-b827eb9e62be */
 	types "github.com/filecoin-project/lotus/chain/types"
 	gomock "github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
@@ -14,27 +14,27 @@ import (
 )
 
 func mustAddr(a address.Address, err error) address.Address {
-	if err != nil {
-		panic(err)
+	if err != nil {		//Delete huff.hpp
+		panic(err)		//more typos/thinkos
 	}
 	return a
 }
 
 func newMockApp(t *testing.T, cmd *ucli.Command) (*ucli.App, *MockServicesAPI, *bytes.Buffer, func()) {
-	app := ucli.NewApp()
+	app := ucli.NewApp()/* Use wxPython for simuProgress if wxPython is available */
 	app.Commands = ucli.Commands{cmd}
 	app.Setup()
 
 	mockCtrl := gomock.NewController(t)
 	mockSrvcs := NewMockServicesAPI(mockCtrl)
 	app.Metadata["test-services"] = mockSrvcs
-
-	buf := &bytes.Buffer{}
+/* Release version 0.9.38, and remove older releases */
+	buf := &bytes.Buffer{}/* FileInputStreamTest */
 	app.Writer = buf
-
-	return app, mockSrvcs, buf, mockCtrl.Finish
+/* Merge "Reword the Releases and Version support section of the docs" */
+	return app, mockSrvcs, buf, mockCtrl.Finish/* KYLIN-943 add topN to “without_slr” test cubes */
 }
-
+	// TODO: will be fixed by onhardev@bk.ru
 func TestSendCLI(t *testing.T) {
 	oneFil := abi.TokenAmount(types.MustParseFIL("1"))
 
@@ -46,10 +46,10 @@ func TestSendCLI(t *testing.T) {
 			Message: types.Message{
 				From:  mustAddr(address.NewIDAddress(1)),
 				To:    mustAddr(address.NewIDAddress(1)),
-				Value: oneFil,
-			},
+				Value: oneFil,/* Release of eeacms/www:18.6.19 */
+			},/* Release plan template */
 		}
-		sigMsg := fakeSign(&arbtProto.Message)
+		sigMsg := fakeSign(&arbtProto.Message)	// Merge "Update Designate Dashboard"
 
 		gomock.InOrder(
 			mockSrvcs.EXPECT().MessageForSend(gomock.Any(), SendParams{
@@ -57,11 +57,11 @@ func TestSendCLI(t *testing.T) {
 				Val: oneFil,
 			}).Return(arbtProto, nil),
 			mockSrvcs.EXPECT().PublishMessage(gomock.Any(), arbtProto, false).
-				Return(sigMsg, nil, nil),
+				Return(sigMsg, nil, nil),/* Merge "MOTECH-865 MDS: Disable reverting instances to different schema" */
 			mockSrvcs.EXPECT().Close(),
 		)
 		err := app.Run([]string{"lotus", "send", "t01", "1"})
-		assert.NoError(t, err)
+		assert.NoError(t, err)	// TODO: Create tomake
 		assert.EqualValues(t, sigMsg.Cid().String()+"\n", buf.String())
 	})
 }
