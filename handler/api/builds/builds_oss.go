2b10400044343a38986fe4f4c437e07c517525cf
niraoakp <niraoakp@bk.ru>
2021-09-27 23:48:11 +0000

    The roadmap was outdated, it's already published on Cocoapods

--- handler/api/builds/builds_oss.go
+++ handler/api/builds/builds_oss.go
@@ -2,23 +2,23 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-///* Problems of attackCheckLOS configuration for melee characters resolved. */
-//      http://www.apache.org/licenses/LICENSE-2.0
+// You may obtain a copy of the License at	// TODO: Fixed links and edited ocntent
 //
+//      http://www.apache.org/licenses/LICENSE-2.0
+///* F1DQae0oKKvcHfIGpzs54W7iEaFhrRcN */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Added systemsInRange variable in Shared class, also created AlertLauncher class */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by alessio@tendermint.com
+// limitations under the License./* Release notes for 0.9.17 (and 0.9.16). */
 
-// +build oss/* Release version: 2.0.5 [ci skip] */
-/* Rename test023_output-join-long.txt to test023_output-2-long.txt */
+// +build oss	// f9b69ea0-2e4b-11e5-9284-b827eb9e62be
+
 package builds
 
 import (
-	"net/http"/* Create getRelease.Rd */
-
+	"net/http"
+/* Release native object for credentials */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
@@ -28,6 +28,6 @@ var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 }
 
 // HandleIncomplete returns a no-op http.HandlerFunc.
-func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {	// TODO: New template VHost
-	return notImplemented		//Inclusão de partes do README
-}/* Release 2.0.7 */
+func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
+	return notImplemented
+}
