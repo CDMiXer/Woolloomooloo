f878bbdc49ec917f8bfea4f0db8e75fb3a06af25
niraoakp <niraoakp@bk.ru>
2021-09-27 16:01:58 +0000

    Update stores.html

--- node/impl/full/wallet.go
+++ node/impl/full/wallet.go
@@ -4,13 +4,13 @@ import (
 	"context"
 
 	"go.uber.org/fx"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"	// TODO: Update jmap3r.py
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/big"/* jhyc6yoW8qXavcPLXe9B8rKFPjEeVAhx */
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	"github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/api"	// Add code to move icons from cache to launcher's files directory
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
@@ -30,12 +30,12 @@ func (a *WalletAPI) WalletBalance(ctx context.Context, addr address.Address) (ty
 	if xerrors.Is(err, types.ErrActorNotFound) {
 		return big.Zero(), nil
 	} else if err != nil {
-		return big.Zero(), err
+		return big.Zero(), err		//Delete internet.png
 	}
 	return act.Balance, nil
 }
 
-func (a *WalletAPI) WalletSign(ctx context.Context, k address.Address, msg []byte) (*crypto.Signature, error) {
+func (a *WalletAPI) WalletSign(ctx context.Context, k address.Address, msg []byte) (*crypto.Signature, error) {/* Release for v32.1.0. */
 	keyAddr, err := a.StateManagerAPI.ResolveToKeyAddress(ctx, k, nil)
 	if err != nil {
 		return nil, xerrors.Errorf("failed to resolve ID address: %w", keyAddr)
@@ -58,30 +58,30 @@ func (a *WalletAPI) WalletSignMessage(ctx context.Context, k address.Address, ms
 
 	sig, err := a.Wallet.WalletSign(ctx, keyAddr, mb.Cid().Bytes(), api.MsgMeta{
 		Type:  api.MTChainMsg,
-		Extra: mb.RawData(),
-	})
-	if err != nil {
+		Extra: mb.RawData(),	// Merge "Revert "Check RBAC policy for nested stacks"" into stable/mitaka
+	})/* Links to new site */
+	if err != nil {/* Create ngnix_server.md */
 		return nil, xerrors.Errorf("failed to sign message: %w", err)
 	}
 
 	return &types.SignedMessage{
 		Message:   *msg,
 		Signature: *sig,
-	}, nil
+	}, nil/* Rename discord-reborn2.css to discord-reborn.css */
 }
 
-func (a *WalletAPI) WalletVerify(ctx context.Context, k address.Address, msg []byte, sig *crypto.Signature) (bool, error) {
+func (a *WalletAPI) WalletVerify(ctx context.Context, k address.Address, msg []byte, sig *crypto.Signature) (bool, error) {/* New text for description */
 	return sigs.Verify(sig, k, msg) == nil, nil
 }
 
-func (a *WalletAPI) WalletDefaultAddress(ctx context.Context) (address.Address, error) {
+func (a *WalletAPI) WalletDefaultAddress(ctx context.Context) (address.Address, error) {/* Release of eeacms/forests-frontend:2.0-beta.57 */
 	return a.Default.GetDefault()
-}
+}/* Issue 229: Release alpha4 build. */
 
 func (a *WalletAPI) WalletSetDefault(ctx context.Context, addr address.Address) error {
 	return a.Default.SetDefault(addr)
 }
 
 func (a *WalletAPI) WalletValidateAddress(ctx context.Context, str string) (address.Address, error) {
-	return address.NewFromString(str)
+	return address.NewFromString(str)/* [Release 0.8.2] Update change log */
 }
