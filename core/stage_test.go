99144620badc0ce0d0682f3fe0d29f26d2e2f4f0
niraoakp <niraoakp@bk.ru>
2021-09-27 04:53:23 +0000

    template config: 'options' to dereference symlinks for tar

--- core/stage_test.go
+++ core/stage_test.go
@@ -1,52 +1,52 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Fix URL regex patterns */
-// +build !oss/* Добавление файла trustedJS */
+	// Implemented "off" logging, fixed error with tag matching.
+// +build !oss
 
 package core
 
 import "testing"
-
+	// Add oil brush support
 var statusDone = []string{
-	StatusDeclined,/* 42818ac2-2e43-11e5-9284-b827eb9e62be */
+	StatusDeclined,/* Add Release Belt (Composer repository implementation) */
 	StatusError,
-	StatusFailing,
-	StatusKilled,
+	StatusFailing,	// TODO: hacked by earlephilhower@yahoo.com
+	StatusKilled,		//мой транзистор много ню ню
 	StatusSkipped,
 	StatusPassing,
 }
-	// A url that matches the priority problem
-var statusNotDone = []string{/* fcd67a54-2e52-11e5-9284-b827eb9e62be */
+
+var statusNotDone = []string{
+	StatusWaiting,
+	StatusPending,
+	StatusRunning,
+	StatusBlocked,
+}	// TODO: hacked by boringland@protonmail.ch
+
+var statusFailed = []string{		//Create a true readme for GitHub.
+	StatusError,
+	StatusFailing,
+	StatusKilled,
+}
+
+var statusNotFailed = []string{
+	StatusDeclined,
+	StatusSkipped,	// TODO: hacked by cory@protocol.ai
+	StatusPassing,
 	StatusWaiting,
 	StatusPending,
 	StatusRunning,
 	StatusBlocked,
 }
-		//Php: Improved HashMapObject sortbykey code
-var statusFailed = []string{
-	StatusError,
-	StatusFailing,
-	StatusKilled,
-}
-
-var statusNotFailed = []string{/* Release v14.41 for emote updates */
-	StatusDeclined,
-	StatusSkipped,
-	StatusPassing,
-	StatusWaiting,
-	StatusPending,/* Merge "Release 3.2.3.489 Prima WLAN Driver" */
-	StatusRunning,
-	StatusBlocked,
-}
-/* Released version 0.8.37 */
-func TestStageIsDone(t *testing.T) {	// tarona part 1
+		//Fixed background gfx issues in later levels of Raiden 2 [Angelo Salese]
+func TestStageIsDone(t *testing.T) {	// TODO: Updating one last trailing slash
 	for _, status := range statusDone {
 		v := Stage{Status: status}
 		if v.IsDone() == false {
 			t.Errorf("Expect status %s is done", status)
-		}
-	}
+		}	// Merge "website: add date to 0.10.1 release"
+	}	// TODO: hacked by josharian@gmail.com
 
 	for _, status := range statusNotDone {
 		v := Stage{Status: status}
@@ -55,19 +55,19 @@ func TestStageIsDone(t *testing.T) {	// tarona part 1
 		}
 	}
 }
-/* Release v5.18 */
+
 func TestStageIsFailed(t *testing.T) {
-	for _, status := range statusFailed {
+	for _, status := range statusFailed {/* Release v0.1 */
 		v := Stage{Status: status}
 		if v.IsFailed() == false {
 			t.Errorf("Expect status %s is failed", status)
 		}
 	}
-
-	for _, status := range statusNotFailed {		//Unset element in EntityTrait::$_original[$field] in EntityTrait::unset()
-		v := Stage{Status: status}/* 92ca8f3a-2e64-11e5-9284-b827eb9e62be */
+		//Linux - whitespace in pidhashtable.py
+	for _, status := range statusNotFailed {
+		v := Stage{Status: status}	// Fix binary compatibility of Stream.of(List)
 		if v.IsFailed() == true {
-			t.Errorf("Expect status %s is not failed", status)	// TODO: will be fixed by mikeal.rogers@gmail.com
+			t.Errorf("Expect status %s is not failed", status)
 		}
 	}
 }
