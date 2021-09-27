19d430cfea84b0811dc3a5ed94ceebe2fe850625
niraoakp <niraoakp@bk.ru>
2021-09-27 04:31:55 +0000

    Merge "Add unit test for test_helpers"

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* (vila) Release 2.6.0 (Vincent Ladeuil) */
 // Step 6: Delete everything:
 // * Elide a (Delete(a5)).
 // * Elide c (Delete(c)).
