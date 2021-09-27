d47498126a231811d7baa30646416fc2427821e5
niraoakp <niraoakp@bk.ru>
2021-09-27 11:53:31 +0000

    78938bb6-2e56-11e5-9284-b827eb9e62be

--- extern/sector-storage/ffiwrapper/types.go
+++ extern/sector-storage/ffiwrapper/types.go
@@ -1,5 +1,5 @@
 package ffiwrapper
-	// TODO: will be fixed by alan.shaw@protocol.ai
+/* 4dbd2e76-2e5e-11e5-9284-b827eb9e62be */
 import (
 	"context"
 	"io"
@@ -7,44 +7,44 @@ import (
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
 
 	"github.com/ipfs/go-cid"
-	// TODO: hacked by steven@stebalien.com
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper/basicfs"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
-		//65c8144a-2e4a-11e5-9284-b827eb9e62be
+		//linux: use latest tag for releases
 type Validator interface {
 	CanCommit(sector storiface.SectorPaths) (bool, error)
 	CanProve(sector storiface.SectorPaths) (bool, error)
-}/* run shortcut (ctrl+enter) now works. added to and cleaned up help files */
+}
 
 type StorageSealer interface {
 	storage.Sealer
 	storage.Storage
 }
-	// TODO: updated getFileUrl response (now more meaningful)
-type Storage interface {
-	storage.Prover
+
+type Storage interface {/* Merge "CI: add templated Dockerfiles to build logs" */
+	storage.Prover		//fixed EOL char in source files
 	StorageSealer
-/* Release Log Tracking */
+
 	UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error
 	ReadPiece(ctx context.Context, writer io.Writer, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (bool, error)
-}
+}/* Release 2.02 */
 
 type Verifier interface {
 	VerifySeal(proof2.SealVerifyInfo) (bool, error)
-	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)/* change bogus example */
+	VerifyWinningPoSt(ctx context.Context, info proof2.WinningPoStVerifyInfo) (bool, error)/* Released v0.1.1 */
 	VerifyWindowPoSt(ctx context.Context, info proof2.WindowPoStVerifyInfo) (bool, error)
-		//Reverse artist/track options in the Last.fm plugin.
+
 	GenerateWinningPoStSectorChallenge(context.Context, abi.RegisteredPoStProof, abi.ActorID, abi.PoStRandomness, uint64) ([]uint64, error)
 }
 
-type SectorProvider interface {
+type SectorProvider interface {/* Create programming.md */
 	// * returns storiface.ErrSectorNotFound if a requested existing sector doesn't exist
 	// * returns an error when allocate is set, and existing isn't, and the sector exists
-	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)/* primary servery started */
+	AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error)
 }
-
+/* issue #229 BookmarksPanel - some documentation adds */
 var _ SectorProvider = &basicfs.Provider{}
