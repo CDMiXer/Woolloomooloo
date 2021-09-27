37eef8ba151c79af76654c0d9acbf9a1d42930a2
niraoakp <niraoakp@bk.ru>
2021-09-27 21:54:44 +0000

    modularize CommandLineProcessor

--- service/syncer/filter_test.go
+++ service/syncer/filter_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: added first tool: shuffle
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: hacked by souzau@yandex.com
 
-package syncer
+package syncer		//foolbadgery
 
 import (
 	"testing"
-
+	// TODO: will be fixed by brosner@gmail.com
 	"github.com/drone/drone/core"
 )
-
+/* Delete area_calc.java */
 func TestNamespaceFilter(t *testing.T) {
 	tests := []struct {
 		namespace  string
@@ -24,24 +24,24 @@ func TestNamespaceFilter(t *testing.T) {
 		{
 			namespace:  "OCTocat",
 			namespaces: []string{"octOCAT"},
-			match:      true,
+			match:      true,/* Merge "docs: SDK / ADT 22.0.5 Release Notes" into jb-mr2-docs */
 		},
-		{
+		{/* Release notes clarify breaking changes */
 			namespace:  "spaceghost",
-			namespaces: []string{"octocat"},
-			match:      false,
+			namespaces: []string{"octocat"},/* @Release [io7m-jcanephora-0.19.0] */
+			match:      false,	// TODO: will be fixed by vyzo@hackzen.org
 		},
 		{
 			namespace:  "spaceghost",
 			namespaces: []string{},
-			match:      true, // no-op filter
+			match:      true, // no-op filter	// TODO: hacked by aeongrp@outlook.com
 		},
 	}
 	for _, test := range tests {
 		r := &core.Repository{Namespace: test.namespace}
-		f := NamespaceFilter(test.namespaces)
+		f := NamespaceFilter(test.namespaces)	// TODO: test fixes after the stripe_customer property removal
 		if got, want := f(r), test.match; got != want {
 			t.Errorf("Want match %v for namespace %q and namespaces %v", want, test.namespace, test.namespaces)
 		}
 	}
-}
+}	// revise the readme to provide a little more info
