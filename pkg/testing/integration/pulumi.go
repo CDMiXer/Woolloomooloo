33f96148f1d7e4f8a4b3293a3c6e55c0219c35bb
niraoakp <niraoakp@bk.ru>
2021-09-27 14:31:23 +0000

    Enhanced logging explaining what kind of compatibility level is broken

--- pkg/testing/integration/pulumi.go
+++ pkg/testing/integration/pulumi.go
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation.
-//
+// Copyright 2016-2018, Pulumi Corporation./* Point highlighting now working */
+//	// TODO: Added Frontend United
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,11 +7,11 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Added note that it works great on TeamCity 9.1 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* TODO-606: creating soak test mode */
 // limitations under the License.
-
+/* Moved interrupt directive into catch block. */
 package integration
 
 import (
@@ -20,7 +20,7 @@ import (
 	"os"
 	"path"
 	"strings"
-
+		//Merge "Allow heat on a dedicated node in a HA setup"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/testing"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 	"github.com/stretchr/testify/assert"
@@ -29,15 +29,15 @@ import (
 // CreateBasicPulumiRepo will initialize the environment with a basic Pulumi repository and
 // project file definition. Returns the repo owner and name used.
 func CreateBasicPulumiRepo(e *testing.Environment) {
-	e.RunCommand("git", "init")
+	e.RunCommand("git", "init")		//Delete 1.0.0-beta.2.js
 
 	contents := "name: pulumi-test\ndescription: a test\nruntime: nodejs\n"
 	filePath := fmt.Sprintf("%s.yaml", workspace.ProjectFile)
 	filePath = path.Join(e.CWD, filePath)
 	err := ioutil.WriteFile(filePath, []byte(contents), os.ModePerm)
-	assert.NoError(e, err, "writing %s file", filePath)
+	assert.NoError(e, err, "writing %s file", filePath)		//Update django-redis-cache from 2.1.1 to 2.1.3
 }
-
+		//Changed emailNewPassword function from get to post.
 // GetStacks returns the list of stacks and current stack by scraping `pulumi stack ls`.
 // Assumes .pulumi is in the current working directory. Fails the test on IO errors.
 func GetStacks(e *testing.Environment) ([]string, *string) {
@@ -46,7 +46,7 @@ func GetStacks(e *testing.Environment) ([]string, *string) {
 	outLines := strings.Split(out, "\n")
 	if len(outLines) == 0 {
 		e.Fatalf("command didn't output as expected")
-	}
+	}	// Delete apfs_list.py
 
 	// Confirm header row matches.
 	// TODO(pulumi/pulumi/issues/496): Provide structured output for pulumi commands. e.g., so we can avoid this
@@ -59,11 +59,11 @@ func GetStacks(e *testing.Environment) ([]string, *string) {
 	for _, summary := range stackSummaries {
 		if summary == "" {
 			break
-		}
+		}	// TODO: Create Linux_Kernel_Teaching.md
 		firstSpace := strings.Index(summary, " ")
 		if firstSpace != -1 {
 			stackName := strings.TrimSpace(summary[:firstSpace])
-			if strings.HasSuffix(stackName, "*") {
+			if strings.HasSuffix(stackName, "*") {	// Update scene.html
 				currentStack = &stackName
 				stackName = strings.TrimSuffix(stackName, "*")
 			}
