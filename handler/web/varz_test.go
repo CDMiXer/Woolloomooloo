db466258636d69e4ffd3d86797a28c3ffd529c9d
niraoakp <niraoakp@bk.ru>
2021-09-27 23:30:53 +0000

    Move loop to test setup

--- handler/web/varz_test.go
+++ handler/web/varz_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License/* updates to sdjr_summary2.Rmd */
+// that can be found in the LICENSE file./* Create a builder for PathConfig */
 
 package web
 
 import (
 	"encoding/json"
 	"net/http/httptest"
-	"net/url"
+	"net/url"/* 1.0.1 Release. */
 	"testing"
 
 	"github.com/drone/drone/core"
@@ -15,32 +15,32 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestHandleVarz(t *testing.T) {
+func TestHandleVarz(t *testing.T) {	// TODO: hacked by sebastian.tharakan97@gmail.com
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
 	client := new(scm.Client)
-	client.BaseURL, _ = url.Parse("https://github.com")
+	client.BaseURL, _ = url.Parse("https://github.com")		//internal fixes
 	client.SetRate(scm.Rate{
 		Limit:     5000,
 		Remaining: 875,
 		Reset:     1523640878,
-	})
+	})/* Delete neutral_icon.xcf */
 
-	license := &core.License{
-		Kind:  core.LicenseStandard,
+	license := &core.License{/* Release 2.0.1. */
+		Kind:  core.LicenseStandard,	// TODO: hacked by mikeal.rogers@gmail.com
 		Repos: 50,
 		Users: 100,
 	}
-	HandleVarz(client, license).ServeHTTP(w, r)
+	HandleVarz(client, license).ServeHTTP(w, r)/* update pom with latest bukkit 1.8 api (from spigot) */
 
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)/* New Release. Settings were not saved correctly.								 */
 	}
 
-	got, want := &varz{}, mockVarz
+	got, want := &varz{}, mockVarz	// TODO: hacked by remco@dutchcoders.io
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); diff != "" {
+	if diff := cmp.Diff(got, want); diff != "" {		//8ecf9262-2e5b-11e5-9284-b827eb9e62be
 		t.Errorf(diff)
 	}
 }
@@ -49,7 +49,7 @@ var mockVarz = &varz{
 	SCM: &scmInfo{
 		URL: "https://github.com",
 		Rate: &rateInfo{
-			Limit:     5000,
+			Limit:     5000,	// Update information about release 3.2.0.
 			Remaining: 875,
 			Reset:     1523640878,
 		},
@@ -58,7 +58,7 @@ var mockVarz = &varz{
 		Kind:       "standard",
 		Seats:      100,
 		SeatsUsed:  0,
-		SeatsAvail: 0,
+		SeatsAvail: 0,	// TODO: will be fixed by 13860583249@yeah.net
 		Repos:      50,
 		ReposUsed:  0,
 		ReposAvail: 0,
