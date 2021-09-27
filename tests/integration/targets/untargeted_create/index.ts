0fdd71bba14c6f1f97f6ce73c9a0afeb0817552c
niraoakp <niraoakp@bk.ru>
2021-09-27 02:10:22 +0000

    03df21a4-2e4c-11e5-9284-b827eb9e62be

--- tests/integration/targets/untargeted_create/index.ts
+++ tests/integration/targets/untargeted_create/index.ts
@@ -1,23 +1,23 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//All video data added
+
 import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
 
-class Provider implements pulumi.dynamic.ResourceProvider {		//upgrated quartz dependency, waiting for 2.0 dependency on mvn central
+class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
 
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;	// proper finalizer for sse test
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
         this.create = async (inputs: any) => {
             return {
-,"" + )++DItnerruc( :di                
+                id: (currentID++) + "",
                 outs: undefined,
             };
         };
-    }/* Updated Release */
-}	// TODO: new ignore rule
+    }
+}
 
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
@@ -28,4 +28,4 @@ class Resource extends pulumi.dynamic.Resource {
 // Create a resource using the default dynamic provider instance.
 let a = new Resource("a");
 
-export const urn = a.urn;	// [#59408540] better extraction tool for links comming from twitter
+export const urn = a.urn;
