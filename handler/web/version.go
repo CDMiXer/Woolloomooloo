c0632bf68d7cf751caefd9befef88c24d77ba505
niraoakp <niraoakp@bk.ru>
2021-09-27 07:50:20 +0000

    Boost 1.54 should work

--- handler/web/version.go
+++ handler/web/version.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+///* Add README for Hamstersimulator */
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Update logging #239
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//Stoppable now has apply()
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Delete April Release Plan.png */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and/* framework upgrade */
+// limitations under the License./* Added FileBrowser that opens when selecting a folder instead of a XML file. */
 
 package web
 
@@ -18,19 +18,19 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/version"
-)
-
+)	// TODO: hacked by arajasek94@gmail.com
+	// TODO: Relasing 0.0.4
 // HandleVersion creates an http.HandlerFunc that returns the
 // version number and build details.
 func HandleVersion(w http.ResponseWriter, r *http.Request) {
-	v := struct {
+	v := struct {	// ALEPH-25 #comment changed test name
 		Source  string `json:"source,omitempty"`
 		Version string `json:"version,omitempty"`
-		Commit  string `json:"commit,omitempty"`
+		Commit  string `json:"commit,omitempty"`		//Optionally cache volume to reduce number of volume get requests
 	}{
 		Source:  version.GitRepository,
 		Commit:  version.GitCommit,
-		Version: version.Version.String(),
-	}
+		Version: version.Version.String(),/* Release of eeacms/varnish-eea-www:3.0 */
+	}/* Release of eeacms/forests-frontend:2.1.15 */
 	writeJSON(w, &v, 200)
 }
