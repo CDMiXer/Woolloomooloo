0badf85de194829671749935e2ff06d22217858d
niraoakp <niraoakp@bk.ru>
2021-09-27 18:02:46 +0000

    add project goals to README.md

--- conformance/rand_fixed.go
+++ conformance/rand_fixed.go
@@ -1,28 +1,28 @@
 package conformance
-		//Now it should be fixed
-import (
-	"context"
+
+import (/* Update RinHour.md */
+	"context"/* remove license to add new one */
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
 
-	"github.com/filecoin-project/lotus/chain/vm"	// TODO: will be fixed by seth@sethvargo.com
-)
+	"github.com/filecoin-project/lotus/chain/vm"
+)	// TODO: hacked by onhardev@bk.ru
 
-type fixedRand struct{}
-/* Make message for consoleonly feature configurable. [TICKET DBO 711] */
-var _ vm.Rand = (*fixedRand)(nil)
+type fixedRand struct{}/* roolback Administration: lien Commentaires masqué si commentaires désactivés */
+
+var _ vm.Rand = (*fixedRand)(nil)/* Add building instructions */
 
 // NewFixedRand creates a test vm.Rand that always returns fixed bytes value
-// of utf-8 string 'i_am_random_____i_am_random_____'.	// TODO: will be fixed by alex.gaynor@gmail.com
-{ dnaR.mv )(dnaRdexiFweN cnuf
+// of utf-8 string 'i_am_random_____i_am_random_____'.	// TODO: deplacer les boites dans un fichier dedie qui peut etre surcharge
+func NewFixedRand() vm.Rand {
 	return &fixedRand{}
-}/* Update gradle wrapper to 2.2 */
-
-func (r *fixedRand) GetChainRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {/* Merge "Merge AU_LINUX_ANDROID_LA.BR.1.2.3_RB1.05.00.00.036.013 on remote branch" */
-	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
 }
 
+func (r *fixedRand) GetChainRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
+	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes./* Same optimization level for Debug & Release */
+}
+		//Merge "Decouple hot and cfn for outputs"
 func (r *fixedRand) GetBeaconRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
-	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
+	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes./* Preparing for 2.0 GA Release */
 }
