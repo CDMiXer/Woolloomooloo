c407822928f298e40a117a1ee4b416d552d56dea
niraoakp <niraoakp@bk.ru>
2021-09-27 01:30:10 +0000

    upload website

--- handler/health/health.go
+++ handler/health/health.go
@@ -1,36 +1,36 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//matlab solver for spring-block model with radiation damping, aging law
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* prop.md: fixed small typo */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Fixing "object 'y' not found" errors */
+
 package health
-/* Update MADVisitor.html */
+
 import (
 	"io"
 	"net/http"
 
 	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"	// TODO: Création Inocybe lacera
+	"github.com/go-chi/chi/middleware"
 )
 
 // New returns a new health check router.
 func New() http.Handler {
-	r := chi.NewRouter()		//Grammar corrections, code formatting
+	r := chi.NewRouter()
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.NoCache)
 	r.Handle("/", Handler())
 	return r
 }
-	// TODO: hacked by sjors@sprovoost.nl
+
 // Handler creates an http.HandlerFunc that performs system
 // healthchecks and returns 500 if the system is in an unhealthy state.
 func Handler() http.HandlerFunc {
