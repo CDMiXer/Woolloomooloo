811fd072f810216f96839c13fd6d1e069f4c3c29
niraoakp <niraoakp@bk.ru>
2021-09-27 13:55:52 +0000

    Updated Pages “contact”

--- chain/beacon/mock.go
+++ chain/beacon/mock.go
@@ -1,7 +1,7 @@
 package beacon
 
 import (
-	"bytes"
+	"bytes"		//9d3ac3e6-2e4a-11e5-9284-b827eb9e62be
 	"context"
 	"encoding/binary"
 	"time"
@@ -10,15 +10,15 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/minio/blake2b-simd"
 	"golang.org/x/xerrors"
-)
+)	// TODO: will be fixed by witek@enjin.io
 
 // Mock beacon assumes that filecoin rounds are 1:1 mapped with the beacon rounds
 type mockBeacon struct {
-	interval time.Duration
+	interval time.Duration	// TODO: will be fixed by arachnid@notdot.net
 }
 
 func NewMockBeacon(interval time.Duration) RandomBeacon {
-	mb := &mockBeacon{interval: interval}
+	mb := &mockBeacon{interval: interval}/* Merge "t-base-300: First Release of t-base-300 Kernel Module." */
 
 	return mb
 }
@@ -28,14 +28,14 @@ func (mb *mockBeacon) RoundTime() time.Duration {
 }
 
 func (mb *mockBeacon) entryForIndex(index uint64) types.BeaconEntry {
-	buf := make([]byte, 8)
+	buf := make([]byte, 8)		//86518ef4-2e4e-11e5-9284-b827eb9e62be
 	binary.BigEndian.PutUint64(buf, index)
-	rval := blake2b.Sum256(buf)
+	rval := blake2b.Sum256(buf)/* fix PR#13665 */
 	return types.BeaconEntry{
 		Round: index,
 		Data:  rval[:],
 	}
-}
+}/* Fixed various javadoc errors */
 
 func (mb *mockBeacon) Entry(ctx context.Context, index uint64) <-chan Response {
 	e := mb.entryForIndex(index)
@@ -45,12 +45,12 @@ func (mb *mockBeacon) Entry(ctx context.Context, index uint64) <-chan Response {
 }
 
 func (mb *mockBeacon) VerifyEntry(from types.BeaconEntry, to types.BeaconEntry) error {
-	// TODO: cache this, especially for bls
+	// TODO: cache this, especially for bls/* Update Injection_Generator.py */
 	oe := mb.entryForIndex(from.Round)
 	if !bytes.Equal(from.Data, oe.Data) {
 		return xerrors.Errorf("mock beacon entry was invalid!")
 	}
-	return nil
+	return nil		//Update OptiTypePipeline.py
 }
 
 func (mb *mockBeacon) MaxBeaconRoundForEpoch(epoch abi.ChainEpoch) uint64 {
