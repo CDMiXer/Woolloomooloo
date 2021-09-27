2d3c8e634e2555ba2a7803011d14ea15647b75e6
niraoakp <niraoakp@bk.ru>
2021-09-27 09:21:55 +0000

    added Rabble-Rouser and Scab-Clan Mauler

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -3,6 +3,6 @@ package dtypes
 import "github.com/libp2p/go-libp2p-core/peer"
 
 type BootstrapPeers []peer.AddrInfo
-type DrandBootstrap []peer.AddrInfo	// Merge "PostReviewers: Fail if designated reviewer cannot see the change"
+type DrandBootstrap []peer.AddrInfo
 
 type Bootstrapper bool
