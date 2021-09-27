09f60d04561c998c72d468cf2e519e941173f2ea
niraoakp <niraoakp@bk.ru>
2021-09-27 05:20:45 +0000

    T94-Redone by 2000RPM

--- grpc-go/resolver/passthrough/passthrough.go
+++ grpc-go/resolver/passthrough/passthrough.go
@@ -1,13 +1,13 @@
-/*
- *
- * Copyright 2017 gRPC authors.
+/*/* Release of eeacms/eprtr-frontend:0.2-beta.27 */
+ *		//OCVN-112 testResources added to pom
+ * Copyright 2017 gRPC authors./* 0.7 Release */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Running ReleaseApp, updating source code headers */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
