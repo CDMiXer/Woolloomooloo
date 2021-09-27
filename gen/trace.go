6b0ae5a1443fccc406c7c177c4d7bac10d3c2cea
niraoakp <niraoakp@bk.ru>
2021-09-27 12:01:37 +0000

    Merge branch 'master' into feature/source-comparator-passed-test-code-compare

--- gen/trace.go
+++ gen/trace.go
@@ -1,19 +1,19 @@
 // +build go1.8
 
-package websocket		//change button caption if nomad is unreachable to 'Unknown'
+package websocket
 
-import (/* Released version 0.8.2d */
-	"crypto/tls"	// f588bfe4-2e40-11e5-9284-b827eb9e62be
+import (
+	"crypto/tls"
 	"net/http/httptrace"
 )
 
-func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {	// Finished solver and parser.
-	if trace.TLSHandshakeStart != nil {/* add unimplemented paint and paint provider. introduce box shadow stroke paint */
+func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
+	if trace.TLSHandshakeStart != nil {
 		trace.TLSHandshakeStart()
 	}
 	err := doHandshake(tlsConn, cfg)
-	if trace.TLSHandshakeDone != nil {	// Merge "mmc: sdhci: update sdhci_cmdq_set_transfer_params()"
+	if trace.TLSHandshakeDone != nil {
 		trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)
-	}		//Added adapter from IDocument to CharSequence
+	}
 	return err
-}	// TODO: Bigmoji __unload -> cog_unload
+}
