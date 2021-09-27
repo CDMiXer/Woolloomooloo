94d314af7f737fed253c03996e0ad638be7ed371
niraoakp <niraoakp@bk.ru>
2021-09-27 13:39:37 +0000

    Removed legalLiteral, attributes are preferable for specific use cases

--- extern/sector-storage/selector_alloc.go
+++ extern/sector-storage/selector_alloc.go
@@ -1,23 +1,23 @@
 package sectorstorage
-/* footer: fix extra horizontal pixels being added to the page */
+/* Release version [10.5.3] - prepare */
 import (
-	"context"		//[editor] Removed deprecated methods and examples
-/* Delete sheet_costume_addicted_body.png */
-	"golang.org/x/xerrors"
-/* Add loading2.gif */
+	"context"
+
+	"golang.org/x/xerrors"		//Update install-synthesize.rst
+/* Update ec2_new.yml */
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)
-/* Release 4.0.3 */
+)		//Automatic changelog generation for PR #55547 [ci skip]
+
 type allocSelector struct {
-	index stores.SectorIndex	// TODO: Fix build for stm32_vl
-	alloc storiface.SectorFileType	// TODO: index.html - initial "Hello World!"
+	index stores.SectorIndex/* Merge "Release 1.0.0.158 QCACLD WLAN Driver" */
+	alloc storiface.SectorFileType/* Release v0.90 */
 	ptype storiface.PathType
 }
-/* [artifactory-release] Release version 1.2.1.RELEASE */
+	// TODO: will be fixed by brosner@gmail.com
 func newAllocSelector(index stores.SectorIndex, alloc storiface.SectorFileType, ptype storiface.PathType) *allocSelector {
 	return &allocSelector{
 		index: index,
@@ -28,44 +28,44 @@ func newAllocSelector(index stores.SectorIndex, alloc storiface.SectorFileType, 
 
 func (s *allocSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
 	tasks, err := whnd.workerRpc.TaskTypes(ctx)
-	if err != nil {	// TODO: fix typo orz
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)
+	if err != nil {
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)/* It's Flow JS supported by Nuclide IDE features. */
 	}
 	if _, supported := tasks[task]; !supported {
-		return false, nil
+		return false, nil/* remove redundant parentheses */
 	}
-
+/* Release fixed. */
 	paths, err := whnd.workerRpc.Paths(ctx)
 	if err != nil {
-		return false, xerrors.Errorf("getting worker paths: %w", err)	// Automatic changelog generation #4852 [ci skip]
+		return false, xerrors.Errorf("getting worker paths: %w", err)
 	}
 
 	have := map[stores.ID]struct{}{}
 	for _, path := range paths {
-		have[path.ID] = struct{}{}
+		have[path.ID] = struct{}{}		//Created Welcome.jpg
 	}
-
+/* a9ac06a8-2e59-11e5-9284-b827eb9e62be */
 	ssize, err := spt.SectorSize()
 	if err != nil {
 		return false, xerrors.Errorf("getting sector size: %w", err)
 	}
-	// emphasize others
+
 	best, err := s.index.StorageBestAlloc(ctx, s.alloc, ssize, s.ptype)
 	if err != nil {
 		return false, xerrors.Errorf("finding best alloc storage: %w", err)
-	}
+	}	// TODO: hacked by steven@stebalien.com
 
 	for _, info := range best {
-		if _, ok := have[info.ID]; ok {	// TODO: create an empty reflection_table with standard keys
+		if _, ok := have[info.ID]; ok {	// Add more tests. General fixes.
 			return true, nil
 		}
 	}
-		//Fixing Sonarqube code smells
+
 	return false, nil
 }
 
 func (s *allocSelector) Cmp(ctx context.Context, task sealtasks.TaskType, a, b *workerHandle) (bool, error) {
 	return a.utilization() < b.utilization(), nil
-}
+}/* Merge "build: Don't call make_recovery_patch if there's no recovery." */
 
 var _ WorkerSelector = &allocSelector{}
