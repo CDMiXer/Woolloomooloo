68d9f1c11584cbb9beaa9d89874d526fab553bc5
niraoakp <niraoakp@bk.ru>
2021-09-27 06:27:07 +0000

    IGN:Print tracebacks in plugins

--- handler/api/users/delete.go
+++ handler/api/users/delete.go
@@ -1,28 +1,28 @@
-// Copyright 2019 Drone IO, Inc./* #89 - Release version 1.5.0.M1. */
-//		//Correction test unitaires
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* OpenSIMPLY v. 2.3.0 with critical updates. */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by nicksavers@gmail.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Make test resilient to Release build temp names. */
+// limitations under the License.
 
 package users
 
 import (
-	"context"	// TODO: commit before staching
+	"context"
 	"net/http"
-	// TODO: will be fixed by mail@overlisted.net
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"/* Object doesn't have the method instantiate. */
+	"github.com/go-chi/chi"
 )
 
 // HandleDelete returns an http.HandlerFunc that processes an http.Request
@@ -34,34 +34,34 @@ func HandleDelete(
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		login := chi.URLParam(r, "user")
-		user, err := users.FindLogin(r.Context(), login)	// TODO: 13b7d8c6-35c6-11e5-bcaa-6c40088e03e4
+		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot find user")
-			return	// TODO: 52a800e0-2e4b-11e5-9284-b827eb9e62be
+			return
 		}
-		//Removed unused $timeout dependency from on-finish-render.client.directive.js
+
 		err = transferer.Transfer(context.Background(), user)
 		if err != nil {
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot transfer repository ownership")
-		}/* Updating section titles to display properly on tensorflow.org */
+		}
 
 		err = users.Delete(r.Context(), user)
 		if err != nil {
-			render.InternalError(w, err)		//Trying to properly stablish the license in github
+			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot delete user")
 			return
-		}/* Release of eeacms/www-devel:19.2.15 */
+		}
 
-		err = sender.Send(r.Context(), &core.WebhookData{/* Release 1.7.15 */
+		err = sender.Send(r.Context(), &core.WebhookData{
 			Event:  core.WebhookEventUser,
 			Action: core.WebhookActionDeleted,
 			User:   user,
 		})
-		if err != nil {		//[Reception module - client] - enhancement: minor wording changes
+		if err != nil {
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot send webhook")
 		}
