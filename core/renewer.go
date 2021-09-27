5654f897d3d19e02935fec9c859ec4fa6d8e3911
niraoakp <niraoakp@bk.ru>
2021-09-27 23:01:01 +0000

    Upload shell script

--- core/renewer.go
+++ core/renewer.go
@@ -10,14 +10,14 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release: Making ready to release 6.6.3 */
+// limitations under the License.
 
-package core/* Merge "Simplify and document the DOMTraverser return value protocol" */
+package core
 
-import "context"/* [artifactory-release] Release version 3.3.9.RELEASE */
+import "context"
 
 // Renewer renews the user account authorization. If
-// successful, the user token and token expiry attributes		//added dataset transfer property list
+// successful, the user token and token expiry attributes
 // are updated, and persisted to the datastore.
 type Renewer interface {
 	Renew(ctx context.Context, user *User, force bool) error
