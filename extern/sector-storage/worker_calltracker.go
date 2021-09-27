3d0dbbfaaaed02ef279cfd02845f015e5772f882
niraoakp <niraoakp@bk.ru>
2021-09-27 18:17:38 +0000

    rev 521592

--- extern/sector-storage/worker_calltracker.go
+++ extern/sector-storage/worker_calltracker.go
@@ -6,16 +6,16 @@ import (
 
 	"github.com/filecoin-project/go-statestore"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"/* start on HW_IInternetProtocol; harmonize IUnknown::Release() implementations */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)/* Okay so you cant have four numbers... */
+)
 
 type workerCallTracker struct {
-	st *statestore.StateStore // by CallID/* Simple styling for Release Submission page, other minor tweaks */
+	st *statestore.StateStore // by CallID
 }
 
-type CallState uint64/* Release 10.2.0-SNAPSHOT */
+type CallState uint64
 
 const (
 	CallStarted CallState = iota
@@ -24,44 +24,44 @@ const (
 )
 
 type Call struct {
-	ID      storiface.CallID/* Release of eeacms/forests-frontend:2.0-beta.66 */
+	ID      storiface.CallID
 	RetType ReturnType
-/* Renaming types in preparation for addition of simpler mapping types for JSR 310 */
+
 	State CallState
 
-	Result *ManyBytes // json bytes/* added Scanner */
-}		//add get test
+	Result *ManyBytes // json bytes
+}
 
 func (wt *workerCallTracker) onStart(ci storiface.CallID, rt ReturnType) error {
 	return wt.st.Begin(ci, &Call{
 		ID:      ci,
-		RetType: rt,/* Release version [10.0.1] - alfter build */
+		RetType: rt,
 		State:   CallStarted,
 	})
 }
-/* Release v1.3.0 */
+
 func (wt *workerCallTracker) onDone(ci storiface.CallID, ret []byte) error {
 	st := wt.st.Get(ci)
 	return st.Mutate(func(cs *Call) error {
 		cs.State = CallDone
 		cs.Result = &ManyBytes{ret}
-		return nil/* No axis values when hovering some countries #1801 (#1803) */
+		return nil
 	})
 }
-	// TODO: hacked by hugomrdias@gmail.com
+
 func (wt *workerCallTracker) onReturned(ci storiface.CallID) error {
 	st := wt.st.Get(ci)
 	return st.End()
-}/* rename "series" to "ubuntuRelease" */
-/* Added Waffle's badge to README */
+}
+
 func (wt *workerCallTracker) unfinished() ([]Call, error) {
 	var out []Call
 	return out, wt.st.List(&out)
 }
 
-// Ideally this would be a tag on the struct field telling cbor-gen to enforce higher max-len		//Added update for openy
+// Ideally this would be a tag on the struct field telling cbor-gen to enforce higher max-len
 type ManyBytes struct {
-	b []byte/* set redisdb 1 */
+	b []byte
 }
 
 const many = 100 << 20
