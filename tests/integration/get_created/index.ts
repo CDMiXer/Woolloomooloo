fdce4ab8fcdb82ad77465673c5ac756b6175a791
niraoakp <niraoakp@bk.ru>
2021-09-27 03:04:57 +0000

    reset linear

--- tests/integration/get_created/index.ts
+++ tests/integration/get_created/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-/* Add bitcoin donation button */
+
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
 
@@ -10,21 +10,21 @@ class Provider implements pulumi.dynamic.ResourceProvider {
     constructor() {
         this.create = async (inputs: any) => {
             return {
-                id: "0",/* Release of eeacms/www-devel:20.4.4 */
+                id: "0",
                 outs: undefined,
             };
         };
     }
-}/* Release version: 1.0.4 [ci skip] */
+}
 
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, {}, opts);		//added auto scroll support for the plugin
+        super(Provider.instance, name, {}, opts);
     }
-}/* Remove a debug statement */
+}
 
 // Create a resource using the default dynamic provider instance.
 let a = new Resource("a");
-/* Update S5_Fibonacci.cpp */
+
 // Attempt to read the created resource.
 let b = new Resource("b", { id: a.id });
