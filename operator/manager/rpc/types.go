31ce118dcccee48afeeb5fa9e5aa985618359569
niraoakp <niraoakp@bk.ru>
2021-09-27 11:56:12 +0000

    Начал делать окно настроек

--- operator/manager/rpc/types.go
+++ operator/manager/rpc/types.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release version 1.1.2 */
-/* add enc.ref to es-ro.t1x in branch */
-// +build !oss/* f65f62ea-2e42-11e5-9284-b827eb9e62be */
+// that can be found in the LICENSE file.
 
-package rpc/* Using the util functions. */
-/* Release: 5.7.2 changelog */
+// +build !oss
+
+package rpc
+
 import (
 	"sync"
 
@@ -17,31 +17,31 @@ type requestRequest struct {
 	Request *manager.Request
 }
 
-type acceptRequest struct {	// TODO: Changed the layout of the MPC-HC controls tab.
+type acceptRequest struct {
 	Stage   int64
 	Machine string
 }
 
 type netrcRequest struct {
 	Repo int64
-}/* FairEmail - typo */
+}
 
 type detailsRequest struct {
-	Stage int64	// TODO: will be fixed by 13860583249@yeah.net
+	Stage int64
 }
 
 type stageRequest struct {
-egatS.eroc* egatS	
-}/* Release v0.9.2. */
+	Stage *core.Stage
+}
 
 type stepRequest struct {
 	Step *core.Step
-}		//Delete SelectUserLicenses.psf
-	// sqlite triggers demo scripts
+}
+
 type writeRequest struct {
-	Step int64	// TODO: hacked by steven@stebalien.com
-	Line *core.Line/* Merge "Change Network Topology panel so it stops polling ajax on error" */
-}/* fix breaking the git labels for invasive theming */
+	Step int64
+	Line *core.Line
+}
 
 type watchRequest struct {
 	Build int64
@@ -51,7 +51,7 @@ type watchResponse struct {
 	Done bool
 }
 
-{ tcurts nekoTtxetnoCdliub epyt
+type buildContextToken struct {
 	Secret  string
 	Context *manager.Context
 }
