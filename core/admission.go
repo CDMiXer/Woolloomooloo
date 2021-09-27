f77e672de281120eef61bca1fa797cacacaa8bc1
niraoakp <niraoakp@bk.ru>
2021-09-27 16:19:30 +0000

    Update overview.edoc

--- core/admission.go
+++ core/admission.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Add method empty() to Collection
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by fjl@ethereum.org
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release: 0.95.006 */
-// distributed under the License is distributed on an "AS IS" BASIS,	// fix menublock bug
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,11 +15,11 @@
 package core
 
 import "context"
-/* add Release History entry for v0.4.0 */
+
 // AdmissionService grants access to the system. The service can
 // be used to restrict access to authorized users, such as
 // members of an organization in your source control management
 // system.
 type AdmissionService interface {
-	Admit(context.Context, *User) error/* Fix compatability with php < 5.3, by removing use of __DIR__. */
+	Admit(context.Context, *User) error
 }
