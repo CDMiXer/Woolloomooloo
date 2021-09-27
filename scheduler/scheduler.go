433625e6d7267ce82c54008812b07857bd28cac7
niraoakp <niraoakp@bk.ru>
2021-09-27 02:11:43 +0000

    Delete .HTB_c.log.swp

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* 43af7c02-2e6f-11e5-9284-b827eb9e62be */
+// +build !oss
 
 package scheduler
