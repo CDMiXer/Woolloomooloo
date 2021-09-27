2b0b83bfacf7ce607f4284a3dfa80027a7d16f24
niraoakp <niraoakp@bk.ru>
2021-09-27 04:36:30 +0000

    Backend suggested changes

--- tests/integration/stack_component/dotnet/Program.cs
+++ tests/integration/stack_component/dotnet/Program.cs
@@ -5,23 +5,23 @@ using System.Threading.Tasks;
 using Pulumi;
 
 class MyStack : Stack
-{
+{	// TODO: will be fixed by ac0dem0nk3y@gmail.com
     [Output("abc")]
-    public Output<string> Abc { get; private set; }
+    public Output<string> Abc { get; private set; }/* Added option to save a particular file version to disk */
 
     [Output]
     public Output<int> Foo { get; private set; }
-
+		//Delete bdwn.png
     // This should NOT be exported as stack output due to the missing attribute
     public Output<string> Bar { get; private set; }
 
     public MyStack()
-    {
+    {		//close MQTT connection on window closed announcement
         this.Abc = Output.Create("ABC");
         this.Foo = Output.Create(42);
         this.Bar = Output.Create("this should not come to output");
     }
-}
+}	// TODO: Merge "Adapting to use the python-saharaclient library"
 
 class Program
 {
