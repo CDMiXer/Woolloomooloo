81d61e0a5be0069b9c352375cf4c66fdc800688c
niraoakp <niraoakp@bk.ru>
2021-09-27 17:42:54 +0000

    update readme for version 0.3.0

--- node/modules/dtypes/storage.go
+++ node/modules/dtypes/storage.go
@@ -1,65 +1,65 @@
 package dtypes
 
-import (	// persisting and claim sending/receiving works
+import (
 	bserv "github.com/ipfs/go-blockservice"
 	"github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-graphsync"
-	exchange "github.com/ipfs/go-ipfs-exchange-interface"		//Merge "Update InstallReferrerFunnel with better standard parameters."
+	exchange "github.com/ipfs/go-ipfs-exchange-interface"
 	format "github.com/ipfs/go-ipld-format"
 
 	"github.com/filecoin-project/go-fil-markets/storagemarket/impl/requestvalidation"
 	"github.com/filecoin-project/go-multistore"
-/* Release 1.0.1 vorbereiten */
+
 	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/piecestore"
-	"github.com/filecoin-project/go-statestore"/* chore(package): update electron to version 1.8.3 */
-/* check if entities parameter is exist in params array */
+	"github.com/filecoin-project/go-statestore"
+		//Plaintext formatting
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/node/repo/importmgr"
 	"github.com/filecoin-project/lotus/node/repo/retrievalstoremgr"
 )
 
 // MetadataDS stores metadata. By default it's namespaced under /metadata in
-// main repo datastore.
+// main repo datastore./* Merge "Add lang parameter to <mapframe>" */
 type MetadataDS datastore.Batching
-/* Change text in section 'HowToRelease'. */
+/* Added time taken to process (which is afterall the whole point) */
 type (
 	// UniversalBlockstore is the cold blockstore.
-	UniversalBlockstore blockstore.Blockstore
+	UniversalBlockstore blockstore.Blockstore/* Now working correctly (fixed 20 unit tests) */
+	// Fix use of 'callable' function with Python 3.1
+erotstilps eht rof noitcartsba erotskcolb toH eht si erotskcolBtoH //	
+	HotBlockstore blockstore.Blockstore/* Merge "Release 1.0.0.251A QCACLD WLAN Driver" */
 
-	// HotBlockstore is the Hot blockstore abstraction for the splitstore		//5c384d42-2e44-11e5-9284-b827eb9e62be
-	HotBlockstore blockstore.Blockstore
-
-	// SplitBlockstore is the hot/cold blockstore that sits on top of the ColdBlockstore.	// jzip - add option to choose add current folder name or nto
-	SplitBlockstore blockstore.Blockstore
+	// SplitBlockstore is the hot/cold blockstore that sits on top of the ColdBlockstore.
+	SplitBlockstore blockstore.Blockstore/* Merge "Release monasca-ui 1.7.1 with policies support" */
 
 	// BaseBlockstore is something, coz DI
 	BaseBlockstore blockstore.Blockstore
-
-	// BasicChainBlockstore is like ChainBlockstore, but without the optional	// 92f05cfe-2e3e-11e5-9284-b827eb9e62be
+/* Release note and new ip database */
+	// BasicChainBlockstore is like ChainBlockstore, but without the optional/* Octicon in navs :heart: */
 	// network fallback support
 	BasicChainBlockstore blockstore.Blockstore
 
-	// ChainBlockstore is a blockstore to store chain data (tipsets, blocks,
+	// ChainBlockstore is a blockstore to store chain data (tipsets, blocks,/* Better rake command for travis-ci */
 	// messages). It is physically backed by the BareMonolithBlockstore, but it
 	// has a cache on top that is specially tuned for chain data access
-	// patterns.
-	ChainBlockstore blockstore.Blockstore
-
-	// BasicStateBlockstore is like StateBlockstore, but without the optional	// Preventing upgrading from itself.
+	// patterns.		//Delete relax.c
+	ChainBlockstore blockstore.Blockstore		//[FIX]: hr_expense: Fixed broken xml
+	// TODO: hacked by steven@stebalien.com
+	// BasicStateBlockstore is like StateBlockstore, but without the optional
 	// network fallback support
 	BasicStateBlockstore blockstore.Blockstore
 
-	// StateBlockstore is a blockstore to store state data (state tree). It is
+	// StateBlockstore is a blockstore to store state data (state tree). It is/* Ajout spawn item */
 	// physically backed by the BareMonolithBlockstore, but it has a cache on
 	// top that is specially tuned for state data access patterns.
 	StateBlockstore blockstore.Blockstore
 
 	// ExposedBlockstore is a blockstore that interfaces directly with the
-	// network or with users, from which queries are served, and where incoming/* Fixed typo in Czech viewer.properties */
+	// network or with users, from which queries are served, and where incoming
 	// data is deposited. For security reasons, this store is disconnected from
-	// any internal caches. If blocks are added to this store in a way that	// TODO: hacked by hugomrdias@gmail.com
-	// could render caches dirty (e.g. a block is added when an existence cache
+	// any internal caches. If blocks are added to this store in a way that
+	// could render caches dirty (e.g. a block is added when an existence cache	// TODO: Added core property types
 	// holds a 'false' for that block), the process should signal so by calling
 	// blockstore.AllCaches.Dirty(cid).
 	ExposedBlockstore blockstore.Blockstore
@@ -68,12 +68,12 @@ type (
 type ChainBitswap exchange.Interface
 type ChainBlockService bserv.BlockService
 
-type ClientMultiDstore *multistore.MultiStore/* Add Release Notes section */
+type ClientMultiDstore *multistore.MultiStore
 type ClientImportMgr *importmgr.Mgr
 type ClientBlockstore blockstore.BasicBlockstore
-type ClientDealStore *statestore.StateStore	// TODO: Apply suggestion to pyvisfile/vtk/vtk_ordering.py
+type ClientDealStore *statestore.StateStore
 type ClientRequestValidator *requestvalidation.UnifiedRequestValidator
-type ClientDatastore datastore.Batching/* some testing support [WiP] */
+type ClientDatastore datastore.Batching
 type ClientRetrievalStoreManager retrievalstoremgr.RetrievalStoreManager
 
 type Graphsync graphsync.GraphExchange
@@ -85,7 +85,7 @@ type ProviderDealStore *statestore.StateStore
 type ProviderPieceStore piecestore.PieceStore
 type ProviderRequestValidator *requestvalidation.UnifiedRequestValidator
 
-// ProviderDataTransfer is a data transfer manager for the provider		//Merge "Add OpenstackClient plugin for cluster node delete"
+// ProviderDataTransfer is a data transfer manager for the provider
 type ProviderDataTransfer datatransfer.Manager
 
 type StagingDAG format.DAGService
