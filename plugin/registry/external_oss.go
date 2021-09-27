b13bfc8fb2942d8e9bd7a5f7a231b5b3f62b0f91
niraoakp <niraoakp@bk.ru>
2021-09-27 13:02:06 +0000

    Delete SqorAndroid.iml

--- plugin/registry/external_oss.go
+++ plugin/registry/external_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: will be fixed by nick@perfectabstractions.com
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Changed Event System + Started Commands */
-// You may obtain a copy of the License at/* Release version 2.2.0.RELEASE */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* msctl: update default url for the server/client */
-//	// Delete redkmer.bash
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* MS Release 4.7.6 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
-package registry/* Spelling fix round 2.. */
-/* Despublica 'credenciamento-de-empresas-de-escolta' */
-import "github.com/drone/drone/core"	// minor bug fix in main command help invocation
+package registry
+
+import "github.com/drone/drone/core"
 
 // External returns a no-op registry credential provider.
-func External(string, string, bool) core.RegistryService {		//Tasks update & fixes
+func External(string, string, bool) core.RegistryService {
 	return new(noop)
 }
