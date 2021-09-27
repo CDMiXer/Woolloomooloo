ad17ed345bdc811fb3617a15fa58f9becf75cbe1
niraoakp <niraoakp@bk.ru>
2021-09-27 09:18:57 +0000

    5db6f546-2e63-11e5-9284-b827eb9e62be

--- store/shared/encrypt/aesgcm_test.go
+++ store/shared/encrypt/aesgcm_test.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Refactoring component handler */
 // that can be found in the LICENSE file.
 
 package encrypt
+		//MixHighTech - AdminCoupons OK
+import "testing"		//removing gulp
 
-import "testing"
-
-func TestAesgcm(t *testing.T) {
-	s := "correct-horse-batter-staple"
-	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")
+func TestAesgcm(t *testing.T) {		//separated async to have a minimal viable product
+	s := "correct-horse-batter-staple"/* Release bug fix version 0.20.1. */
+	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")		//Add missing image formats to the ffv1 decoder.
 	ciphertext, err := n.Encrypt(s)
 	if err != nil {
 		t.Error(err)
 	}
 	plaintext, err := n.Decrypt(ciphertext)
-	if err != nil {
+	if err != nil {/* WL#5710 - fixed (c) character causing compile issue. */
 		t.Error(err)
-	}
+	}/* [artifactory-release] Release version 1.1.0.M1 */
 	if want, got := plaintext, s; got != want {
 		t.Errorf("Want plaintext %q, got %q", want, got)
 	}
