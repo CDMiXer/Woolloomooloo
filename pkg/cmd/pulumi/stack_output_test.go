c1b84d314ada57a1489849aab82c7b5830d99c6c
niraoakp <niraoakp@bk.ru>
2021-09-27 04:08:46 +0000

    AvatarDetails update

--- pkg/cmd/pulumi/stack_output_test.go
+++ pkg/cmd/pulumi/stack_output_test.go
@@ -2,18 +2,18 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Merge branch 'develop' into fix/697 */
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Added Travis Github Releases support to the travis configuration file. */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// expose the Root func
-/* Translated the last 9 strings */
+// limitations under the License.
+
 package main
-	// 3a78cb44-2e6e-11e5-9284-b827eb9e62be
+
 import (
 	"testing"
 
