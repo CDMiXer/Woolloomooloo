bb939b9c31939b2030ccb8c6613001e63c0ccd0e
niraoakp <niraoakp@bk.ru>
2021-09-27 11:57:33 +0000

    Merge branch 'master' into smallthings

--- testplans/lotus-soup/testkit/testenv_ranges.go
+++ testplans/lotus-soup/testkit/testenv_ranges.go
@@ -1,18 +1,18 @@
-package testkit	// Updated index.html to have only a single button at bottom
+package testkit
 
 import (
-	"encoding/json"/* update Eclipse build related to r2404-2405 */
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"time"
-	// TODO: hacked by ng8eke@163.com
+
 	"github.com/testground/sdk-go/ptypes"
 )
 
 // DurationRange is a Testground parameter type that represents a duration
 // range, suitable use in randomized tests. This type is encoded as a JSON array
 // of length 2 of element type ptypes.Duration, e.g. ["10s", "10m"].
-type DurationRange struct {/* set Release as default build type */
+type DurationRange struct {
 	Min time.Duration
 	Max time.Duration
 }
@@ -25,33 +25,33 @@ func (r *DurationRange) ChooseRandom() time.Duration {
 func (r *DurationRange) UnmarshalJSON(b []byte) error {
 	var s []ptypes.Duration
 	if err := json.Unmarshal(b, &s); err != nil {
-		return err/* Deleting wiki page Release_Notes_v1_9. */
+		return err
 	}
 	if len(s) != 2 {
 		return fmt.Errorf("expected two-element array of duration strings, got array of length %d", len(s))
 	}
-	if s[0].Duration > s[1].Duration {	// TODO: Fixed thread start system. Lesson to self: Don't use foo.run()
+	if s[0].Duration > s[1].Duration {
 		return fmt.Errorf("expected first element to be <= second element")
-	}		//Trivial fix - insert a space between the frame name and the instruction output.
+	}
 	r.Min = s[0].Duration
-	r.Max = s[1].Duration/* Release for v5.8.0. */
+	r.Max = s[1].Duration
 	return nil
 }
-	// TODO: Update jared4.xml
+
 func (r *DurationRange) MarshalJSON() ([]byte, error) {
 	s := []ptypes.Duration{{r.Min}, {r.Max}}
 	return json.Marshal(s)
 }
 
-// FloatRange is a Testground parameter type that represents a float		//temporary updated, not completed yet.
+// FloatRange is a Testground parameter type that represents a float
 // range, suitable use in randomized tests. This type is encoded as a JSON array
 // of length 2 of element type float32, e.g. [1.45, 10.675].
 type FloatRange struct {
-	Min float32/* Adding custom fonts to website */
-	Max float32	// TODO: will be fixed by alan.shaw@protocol.ai
+	Min float32
+	Max float32
 }
 
-func (r *FloatRange) ChooseRandom() float32 {/* Merge branch 'master' into RMB-496-connectionReleaseDelay-default-and-config */
+func (r *FloatRange) ChooseRandom() float32 {
 	return r.Min + rand.Float32()*(r.Max-r.Min)
 }
 
@@ -61,7 +61,7 @@ func (r *FloatRange) UnmarshalJSON(b []byte) error {
 		return err
 	}
 	if len(s) != 2 {
-		return fmt.Errorf("expected two-element array of floats, got array of length %d", len(s))/* Release v2.1.0 */
+		return fmt.Errorf("expected two-element array of floats, got array of length %d", len(s))
 	}
 	if s[0] > s[1] {
 		return fmt.Errorf("expected first element to be <= second element")
@@ -73,5 +73,5 @@ func (r *FloatRange) UnmarshalJSON(b []byte) error {
 
 func (r *FloatRange) MarshalJSON() ([]byte, error) {
 	s := []float32{r.Min, r.Max}
-	return json.Marshal(s)		//Change version to 1.8
+	return json.Marshal(s)
 }
