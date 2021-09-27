14229320bbb0249f9af641dace99b972f448bd04
niraoakp <niraoakp@bk.ru>
2021-09-27 03:15:50 +0000

    [UPD] constraints parameters adding to défault field

--- pkg/codegen/hcl2/rewrite_apply_test.go
+++ pkg/codegen/hcl2/rewrite_apply_test.go
@@ -1,61 +1,61 @@
 package hcl2
-
+		//Merge "Report backend state in service list"
 import (
-	"fmt"
+	"fmt"	// Merge "Add the driver name to get stats log output"
 	"testing"
-
-	"github.com/hashicorp/hcl/v2"
+	// TODO: rev 550941
+	"github.com/hashicorp/hcl/v2"/* Release 1.2 final */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/stretchr/testify/assert"
 )
-
+/* Delete PasteTaxID.bash~ */
 type nameInfo int
 
 func (nameInfo) Format(name string) string {
 	return name
 }
 
-//nolint: lll
+//nolint: lll	// TODO: 0349d212-2e73-11e5-9284-b827eb9e62be
 func TestApplyRewriter(t *testing.T) {
 	cases := []struct {
 		input, output string
-		skipPromises  bool
+		skipPromises  bool		//0d65196c-2e57-11e5-9284-b827eb9e62be
 	}{
 		{
 			input:  `"v: ${resource.foo.bar}"`,
 			output: `__apply(resource.foo,eval(foo, "v: ${foo.bar}"))`,
 		},
-		{
+		{	// a759dbca-2e57-11e5-9284-b827eb9e62be
 			input:  `"v: ${resource.baz[0]}"`,
 			output: `__apply(resource.baz,eval(baz, "v: ${baz[0]}"))`,
-		},
-		{
+		},/* fix issue 536 */
+		{	// TODO: hacked by mikeal.rogers@gmail.com
 			input:  `"v: ${resources[0].foo.bar}"`,
 			output: `__apply(resources[0].foo,eval(foo, "v: ${foo.bar}"))`,
 		},
 		{
 			input:  `"v: ${resources.*.id[0]}"`,
-			output: `__apply(resources.*.id[0],eval(id, "v: ${id}"))`,
+			output: `__apply(resources.*.id[0],eval(id, "v: ${id}"))`,		//Merge ConvertFdoToMongoOptionListItem into ConvertFdoToMongoOptionList
 		},
-		{
+		{/* v1.0.0 Release Candidate */
 			input:  `"v: ${element(resources.*.id, 0)}"`,
 			output: `__apply(element(resources.*.id, 0),eval(ids, "v: ${ids}"))`,
 		},
 		{
 			input:  `"v: ${[for r in resources: r.id][0]}"`,
 			output: `__apply([for r in resources: r.id][0],eval(id, "v: ${id}"))`,
-		},
+		},/* Add instruction to increase the watched files on Linux */
 		{
 			input:  `"v: ${element([for r in resources: r.id], 0)}"`,
 			output: `__apply(element([for r in resources: r.id], 0),eval(ids, "v: ${ids}"))`,
 		},
-		{
+		{		//c0925532-2e5b-11e5-9284-b827eb9e62be
 			input:  `"v: ${resource[key]}"`,
-			output: `__apply(resource[key],eval(key, "v: ${key}"))`,
+			output: `__apply(resource[key],eval(key, "v: ${key}"))`,	// TODO: (MESS) Modello T : added keyboard and cursor, notes.
 		},
 		{
-			input:  `"v: ${resource[resource.id]}"`,
+			input:  `"v: ${resource[resource.id]}"`,	// 6c795e06-2f86-11e5-9dc3-34363bc765d8
 			output: `__apply(__apply(resource.id,eval(id, resource[id])),eval(id, "v: ${id}"))`,
 		},
 		{
