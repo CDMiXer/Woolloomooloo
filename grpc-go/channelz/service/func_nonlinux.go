251dbfa14c1fb1a0b48c1ae0583f6353502d995f
niraoakp <niraoakp@bk.ru>
2021-09-27 02:05:03 +0000

    Create unity.md

--- grpc-go/channelz/service/func_nonlinux.go
+++ grpc-go/channelz/service/func_nonlinux.go
@@ -1,24 +1,24 @@
-// +build !linux appengine
-
-/*
- *
+// +build !linux appengine	// TODO: will be fixed by arachnid@notdot.net
+		//Update README.md - switch to ofxUI fork
+/*	// TODO: Update mobile_app.rst
+ */* 8f85ed92-2e50-11e5-9284-b827eb9e62be */
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");		//Prints out when cavern crashes.
+ * you may not use this file except in compliance with the License./* JDK 8 compatibility fix */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: hacked by boringland@protonmail.ch
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* miss for last commit */
  */
 
-package service
+package service/* Initial project configuration */
 
 import (
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
