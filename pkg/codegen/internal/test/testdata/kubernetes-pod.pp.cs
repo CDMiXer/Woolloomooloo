ede7d956a1b10e698fc5dd925a730b0e24236913
niraoakp <niraoakp@bk.ru>
2021-09-27 09:10:24 +0000

    Detect clones with up to 8 parameters

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.cs
@@ -4,32 +4,32 @@ using Kubernetes = Pulumi.Kubernetes;
 class MyStack : Stack
 {
     public MyStack()
-    {		//Added 'die()'. That can't be bad. :-)
+    {	// TODO: will be fixed by arachnid@notdot.net
         var bar = new Kubernetes.Core.V1.Pod("bar", new Kubernetes.Types.Inputs.Core.V1.PodArgs
-        {
+        {/* Stable Release v2.5.3 */
             ApiVersion = "v1",
-            Kind = "Pod",	// TODO: Delete arctoscolorbanner.png
+            Kind = "Pod",
             Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
             {
                 Namespace = "foo",
                 Name = "bar",
             },
             Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
-            {	// ef6ea486-35c5-11e5-8775-6c40088e03e4
+            {
                 Containers = 
-                {/* Pass log folder */
+                {
                     new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
-                    {
-                        Name = "nginx",/* Release Process: Change pom.xml version to 1.4.0-SNAPSHOT. */
+                    {		//[168. Excel Sheet Column Title][Accepted]committed by Victor
+                        Name = "nginx",	// TODO: Make recorded tracks the same size as imported tracks (if one or two channels).
                         Image = "nginx:1.14-alpine",
-                        Resources = new Kubernetes.Types.Inputs.Core.V1.ResourceRequirementsArgs		//Add godoc reference to readme.
-                        {/* Final coverage reporting (hopefully) */
+                        Resources = new Kubernetes.Types.Inputs.Core.V1.ResourceRequirementsArgs
+                        {
                             Limits = 
                             {
                                 { "memory", "20Mi" },
                                 { "cpu", "0.2" },
-                            },
-                        },/* Bugfix in the writer. Release 0.3.6 */
+                            },	// TODO: do not limit db connection pool size
+                        },
                     },
                 },
             },
