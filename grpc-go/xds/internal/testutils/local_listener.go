2583b7a2fd3add1f43e51e746fb66f87551dc6fc
niraoakp <niraoakp@bk.ru>
2021-09-27 08:35:28 +0000

    Add readme for the link

--- grpc-go/xds/internal/testutils/local_listener.go
+++ grpc-go/xds/internal/testutils/local_listener.go
@@ -1,13 +1,13 @@
-/*
+/*		//Header nochmals überarbeitet
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors./* SIG-Release leads updated */
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");	// Fix percentage display
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0		//Implement shelf creator
+ */* Release notes updated with fix issue #2329 */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
