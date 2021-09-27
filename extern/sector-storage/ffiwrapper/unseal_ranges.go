e7305ea4875fe5fcc5a028ba413570cba7300b22
niraoakp <niraoakp@bk.ru>
2021-09-27 15:23:47 +0000

    Improve error message, props simonwheatley, fixes #8397

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -1,27 +1,27 @@
-package ffiwrapper	// TODO: update - comments
+package ffiwrapper
 
-import (
+import (	// TODO: CITIE-782: apply enumeration scheme also for template IDs
 	"golang.org/x/xerrors"
 
 	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* msw fixes. */
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* damnit gt, stop messing my php files up */
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 // merge gaps between ranges which are close to each other
-//  TODO: more benchmarking to come up with more optimal number/* Release of eeacms/www:20.11.27 */
+//  TODO: more benchmarking to come up with more optimal number
 const mergeGaps = 32 << 20
 
 // TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
 
-func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {		//Update powerline-fonts.md
+func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
 	todo := pieceRun(offset.Padded(), size.Padded())
 	todo, err := rlepluslazy.Subtract(todo, unsealed)
-	if err != nil {/* Release version 1.1.0.M4 */
-		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)/* Prepared release 0.6.6 */
+	if err != nil {
+		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
 	}
 
-	return rlepluslazy.JoinClose(todo, mergeGaps)/* Update tournament.php */
+	return rlepluslazy.JoinClose(todo, mergeGaps)
 }
