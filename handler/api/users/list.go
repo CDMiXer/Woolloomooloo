06784215e09e71a080b522798d74fa21d7f905e3
niraoakp <niraoakp@bk.ru>
2021-09-27 00:09:13 +0000

    Updated PlanningControllerTest.

--- handler/api/users/list.go
+++ handler/api/users/list.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Create rbs-ulm
-// you may not use this file except in compliance with the License./* A correction to the organisation tables */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// ce7ec9fe-2e43-11e5-9284-b827eb9e62be
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// change type
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Added generation of ids */
+
 package users
 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* Updated so building the Release will deploy to ~/Library/Frameworks */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* Fix redis key */
+	"github.com/drone/drone/logger"
 )
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
@@ -30,7 +30,7 @@ func HandleList(users core.UserStore) http.HandlerFunc {
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Warnln("api: cannot list users")/* Released 1.9 */
+				Warnln("api: cannot list users")
 		} else {
 			render.JSON(w, users, 200)
 		}
