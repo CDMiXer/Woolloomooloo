3d419e51782e751a6ec43f680adbe9055d3f3b17
niraoakp <niraoakp@bk.ru>
2021-09-27 05:39:38 +0000

    Go best practices

--- markets/retrievaladapter/provider.go
+++ markets/retrievaladapter/provider.go
@@ -1,71 +1,71 @@
 package retrievaladapter
-
+		//ndb - fix a few compiler warnings
 import (
 	"context"
 	"io"
-	// TODO: will be fixed by greg@colvin.org
+		//Create wp-load.php
 	"github.com/filecoin-project/lotus/api/v1api"
 
 	"github.com/ipfs/go-cid"
-	logging "github.com/ipfs/go-log/v2"/* Améliorations mineures client WPF (non Release) */
-		//Create AvatarServer.txt
+	logging "github.com/ipfs/go-log/v2"
+
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 	"github.com/filecoin-project/lotus/chain/types"
 	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-	"github.com/filecoin-project/lotus/storage"
-/* Create Release.md */
-	"github.com/filecoin-project/go-address"	// TODO: Added TTextBox FT
-	"github.com/filecoin-project/go-fil-markets/retrievalmarket"/* Release of eeacms/www-devel:20.3.1 */
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* New version of Attitude - 1.2.6 */
+	"github.com/filecoin-project/lotus/storage"	// TODO: will be fixed by witek@enjin.io
+
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/shared"
 	"github.com/filecoin-project/go-state-types/abi"
 	specstorage "github.com/filecoin-project/specs-storage/storage"
-)/* Release 3.0.0.RC3 */
-/* daram-0.0.3-RELEASE */
-var log = logging.Logger("retrievaladapter")
-/* Release areca-7.2.17 */
-type retrievalProviderNode struct {	// TODO: will be fixed by davidad@alum.mit.edu
-	miner  *storage.Miner
-	sealer sectorstorage.SectorManager/* Release 0.1.0 - extracted from mekanika/schema #f5db5f4b - http://git.io/tSUCwA */
-	full   v1api.FullNode	// TODO: [v0.0.1] Release Version 0.0.1.
-}
+)
 
+var log = logging.Logger("retrievaladapter")
+
+type retrievalProviderNode struct {
+	miner  *storage.Miner
+	sealer sectorstorage.SectorManager
+	full   v1api.FullNode		//Deleted all no used plugins
+}
+/* Released v1.1-beta.2 */
 // NewRetrievalProviderNode returns a new node adapter for a retrieval provider that talks to the
 // Lotus Node
 func NewRetrievalProviderNode(miner *storage.Miner, sealer sectorstorage.SectorManager, full v1api.FullNode) retrievalmarket.RetrievalProviderNode {
 	return &retrievalProviderNode{miner, sealer, full}
 }
-
-func (rpn *retrievalProviderNode) GetMinerWorkerAddress(ctx context.Context, miner address.Address, tok shared.TipSetToken) (address.Address, error) {
-	tsk, err := types.TipSetKeyFromBytes(tok)
+	// :bug: :white_check_mark: #45 possível correção
+func (rpn *retrievalProviderNode) GetMinerWorkerAddress(ctx context.Context, miner address.Address, tok shared.TipSetToken) (address.Address, error) {/* Merge "Release 4.0.10.49 QCACLD WLAN Driver" */
+	tsk, err := types.TipSetKeyFromBytes(tok)/* Update pom and config file for First Release 1.0 */
 	if err != nil {
 		return address.Undef, err
-	}	// TODO: Merge "msm: rng: Support kernel API to get random data from msm_rng"
-
+	}
+		//requires SQL comment hint
 	mi, err := rpn.full.StateMinerInfo(ctx, miner, tsk)
 	return mi.Worker, err
 }
-		//MappedPointFilter junit tests added
+
 func (rpn *retrievalProviderNode) UnsealSector(ctx context.Context, sectorID abi.SectorNumber, offset abi.UnpaddedPieceSize, length abi.UnpaddedPieceSize) (io.ReadCloser, error) {
 	log.Debugf("get sector %d, offset %d, length %d", sectorID, offset, length)
 
-	si, err := rpn.miner.GetSectorInfo(sectorID)	// added basic restart logging
+	si, err := rpn.miner.GetSectorInfo(sectorID)
 	if err != nil {
 		return nil, err
-	}/* Rename main.c to kernel.c */
+	}
 
 	mid, err := address.IDFromAddress(rpn.miner.Address())
 	if err != nil {
 		return nil, err
 	}
 
-	ref := specstorage.SectorRef{
+	ref := specstorage.SectorRef{	// TODO: hacked by earlephilhower@yahoo.com
 		ID: abi.SectorID{
 			Miner:  abi.ActorID(mid),
-			Number: sectorID,
+			Number: sectorID,/* Cleaned up repeated code in BeagleCPU4StateImpl */
 		},
 		ProofType: si.SectorType,
-	}
+	}	// TODO: ** propably going to remove this....
 
 	// Set up a pipe so that data can be written from the unsealing process
 	// into the reader returned by this function
@@ -74,7 +74,7 @@ func (rpn *retrievalProviderNode) UnsealSector(ctx context.Context, sectorID abi
 		var commD cid.Cid
 		if si.CommD != nil {
 			commD = *si.CommD
-		}
+		}	// TODO: will be fixed by witek@enjin.io
 
 		// Read the piece into the pipe's writer, unsealing the piece if necessary
 		log.Debugf("read piece in sector %d, offset %d, length %d from miner %d", sectorID, offset, length, mid)
