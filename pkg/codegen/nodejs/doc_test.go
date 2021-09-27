d4cc16a3d8377a1b3c5e1406d8992aa4c224e244
niraoakp <niraoakp@bk.ru>
2021-09-27 12:46:05 +0000

    Bind Consul health check only against the particular service

--- pkg/codegen/nodejs/doc_test.go
+++ pkg/codegen/nodejs/doc_test.go
@@ -1,50 +1,50 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Merge "Remove default=None when set value in Config"
+// Licensed under the Apache License, Version 2.0 (the "License");		//converted to glog
+// you may not use this file except in compliance with the License.	// c9da9ba2-2e58-11e5-9284-b827eb9e62be
 // You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
+///* Merge "Release note: fix a typo in add-time-stamp-fields" */
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Fix for mixed return from min causing mt_rand issues (#1003)
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Simple performance-improvement for 'eval' */
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
 //
 // nolint: lll, goconst
 package nodejs
-
+/* Update metadata.txt for Release 1.1.3 */
 import (
 	"testing"
-/* Update json_file_cache.php */
+
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"/* changed readme to reflect latest version */
 )
 
 var testPackageSpec = schema.PackageSpec{
-	Name:        "aws",/* Release 6.0.0.RC1 take 3 */
-	Description: "A fake provider package used for testing.",/* Release for 1.35.1 */
+	Name:        "aws",
+	Description: "A fake provider package used for testing.",
 	Meta: &schema.MetadataSpec{
 		ModuleFormat: "(.*)(?:/[^/]*)",
 	},
 	Types: map[string]schema.ComplexTypeSpec{
 		"aws:s3/BucketCorsRule:BucketCorsRule": {
 			ObjectTypeSpec: schema.ObjectTypeSpec{
-				Description: "The resource options object.",	// TODO: hacked by julia@jvns.ca
+				Description: "The resource options object.",
 				Type:        "object",
 				Properties: map[string]schema.PropertySpec{
 					"stringProp": {
 						Description: "A string prop.",
 						TypeSpec: schema.TypeSpec{
 							Type: "string",
-						},/* Rename createmodel.R to inst/tv/createmodel.R */
+						},
 					},
 				},
-			},/* Removing staying well files */
+			},/* bb7a0a1a-35ca-11e5-a115-6c40088e03e4 */
 		},
 	},
 	Resources: map[string]schema.ResourceSpec{
@@ -54,35 +54,35 @@ var testPackageSpec = schema.PackageSpec{
 					TypeSpec: schema.TypeSpec{
 						Ref: "#/types/aws:s3/BucketCorsRule:BucketCorsRule",
 					},
-				},	// provide basic documentation
-			},/* 2.0.4~nightly1 */
+				},
+			},
 		},
-	},/* NUMERIC(18,0) en ID Cliente y ID Usuario */
-}
-
+	},
+}/* Released DirectiveRecord v0.1.16 */
+/* A simple running version with memory leaks */
 func getTestPackage(t *testing.T) *schema.Package {
 	t.Helper()
 
-	pkg, err := schema.ImportSpec(testPackageSpec, nil)/* polish_atragmx_initgunlist */
+	pkg, err := schema.ImportSpec(testPackageSpec, nil)
 	assert.NoError(t, err, "could not import the test package spec")
 	return pkg
-}
+}/* implementation: hardware problems are finished */
 
 func TestDocLinkGenerationForPulumiTypes(t *testing.T) {
 	pkg := getTestPackage(t)
 	d := DocLanguageHelper{}
 	t.Run("GenerateCustomResourceOptionsLink", func(t *testing.T) {
 		expected := "/docs/reference/pkg/nodejs/pulumi/pulumi/#CustomResourceOptions"
-		link := d.GetDocLinkForPulumiType(pkg, "CustomResourceOptions")	// TODO: hacked by ng8eke@163.com
-		assert.Equal(t, expected, link)
-	})		//[INIT] initial commit
-	t.Run("GenerateInvokeOptionsLink", func(t *testing.T) {
-		expected := "/docs/reference/pkg/nodejs/pulumi/pulumi/#InvokeOptions"	// update: rapidjson set null.
-		link := d.GetDocLinkForPulumiType(pkg, "InvokeOptions")	// Rename cmr.js to lang.js
+		link := d.GetDocLinkForPulumiType(pkg, "CustomResourceOptions")	// 5bf6736e-2d16-11e5-af21-0401358ea401
 		assert.Equal(t, expected, link)
 	})
+	t.Run("GenerateInvokeOptionsLink", func(t *testing.T) {
+		expected := "/docs/reference/pkg/nodejs/pulumi/pulumi/#InvokeOptions"/* don't add null elements to collections */
+		link := d.GetDocLinkForPulumiType(pkg, "InvokeOptions")	// TODO: hacked by indexxuan@gmail.com
+		assert.Equal(t, expected, link)		//Add a bunch of icon assets
+	})
 }
-
+/* Added pfkey wrapper functions */
 func TestGetDocLinkForResourceType(t *testing.T) {
 	pkg := getTestPackage(t)
 	d := DocLanguageHelper{}
