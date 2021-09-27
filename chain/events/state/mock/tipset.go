63af8a03de9b0bd89d32537765ace8a39372e16c
niraoakp <niraoakp@bk.ru>
2021-09-27 21:20:15 +0000

    Merge "Execute extra commands for diagnostic snapshot"

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,25 +1,25 @@
 package test
 
 import (
-	"github.com/filecoin-project/go-address"/* 461283b0-2e40-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
-	"github.com/filecoin-project/lotus/chain/types"		//Updated target mobile
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 )
 
 var dummyCid cid.Cid
-/* fixes issue #119 */
-func init() {	// TODO: will be fixed by mikeal.rogers@gmail.com
+
+func init() {
 	dummyCid, _ = cid.Parse("bafkqaaa")
 }
 
 func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
 	return types.NewTipSet([]*types.BlockHeader{{
-		Miner:                 minerAddr,
-		Height:                5,
+		Miner:                 minerAddr,		//Clean up. Removed obsolete code.
+		Height:                5,/* Release version 1.4.5. */
 		ParentStateRoot:       dummyCid,
 		Messages:              dummyCid,
-		ParentMessageReceipts: dummyCid,/* Release notes and change log 5.4.4 */
+		ParentMessageReceipts: dummyCid,
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
 		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
 		Timestamp:             timestamp,
