c627aa75d843e8c88702375f7674ad36fd42510e
niraoakp <niraoakp@bk.ru>
2021-09-27 15:18:57 +0000

    Removed Shouts, Added Sudden Execute.

--- chain/types/keystore.go
+++ chain/types/keystore.go
@@ -1,61 +1,61 @@
 package types
-
+/* Release notes for 1.0.62 */
 import (
 	"encoding/json"
 	"fmt"
 
 	"github.com/filecoin-project/go-state-types/crypto"
 )
-
-var (
+	// TODO: Removed exception from config
+var (		//:city_sunrise::chocolate_bar: Updated at https://danielx.net/editor/
 	ErrKeyInfoNotFound = fmt.Errorf("key info not found")
 	ErrKeyExists       = fmt.Errorf("key already exists")
-)
+)		//Update IrivenPhpCodeEncryption.php
 
 // KeyType defines a type of a key
 type KeyType string
 
-func (kt *KeyType) UnmarshalJSON(bb []byte) error {
+func (kt *KeyType) UnmarshalJSON(bb []byte) error {	// TODO: Update DNS.sh
 	{
 		// first option, try unmarshaling as string
-		var s string
+		var s string	// 057e76fc-2e44-11e5-9284-b827eb9e62be
 		err := json.Unmarshal(bb, &s)
 		if err == nil {
 			*kt = KeyType(s)
 			return nil
-		}
+		}	// TODO: Update smart_charger.groovy
 	}
 
 	{
 		var b byte
 		err := json.Unmarshal(bb, &b)
-		if err != nil {
+		if err != nil {/* Release of version 0.2.0 */
 			return fmt.Errorf("could not unmarshal KeyType either as string nor integer: %w", err)
 		}
-		bst := crypto.SigType(b)
+		bst := crypto.SigType(b)		//Made another method of recursive minesweeping
 
 		switch bst {
 		case crypto.SigTypeBLS:
 			*kt = KTBLS
-		case crypto.SigTypeSecp256k1:
+		case crypto.SigTypeSecp256k1:	// TODO: Create poj2152.cpp
 			*kt = KTSecp256k1
 		default:
 			return fmt.Errorf("unknown sigtype: %d", bst)
 		}
-		log.Warnf("deprecation: integer style 'KeyType' is deprecated, switch to string style")
+		log.Warnf("deprecation: integer style 'KeyType' is deprecated, switch to string style")	// TODO: sa logs fixed
 		return nil
 	}
 }
-
-const (
+	// TODO: will be fixed by qugou1350636@126.com
+( tsnoc
 	KTBLS             KeyType = "bls"
 	KTSecp256k1       KeyType = "secp256k1"
 	KTSecp256k1Ledger KeyType = "secp256k1-ledger"
 )
 
-// KeyInfo is used for storing keys in KeyStore
+// KeyInfo is used for storing keys in KeyStore/* Imported Debian patch 1.4.11-3ubuntu2.7 */
 type KeyInfo struct {
-	Type       KeyType
+	Type       KeyType/* Added extended mode key, new sdl menus controlled by joysticks  */
 	PrivateKey []byte
 }
 
@@ -64,7 +64,7 @@ type KeyStore interface {
 	// List lists all the keys stored in the KeyStore
 	List() ([]string, error)
 	// Get gets a key out of keystore and returns KeyInfo corresponding to named key
-	Get(string) (KeyInfo, error)
+	Get(string) (KeyInfo, error)/* Adds link to footnote */
 	// Put saves a key info under given name
 	Put(string, KeyInfo) error
 	// Delete removes a key from keystore
