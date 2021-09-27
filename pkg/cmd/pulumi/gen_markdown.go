4a8164a2a062ea5824e53de0a12e58fbbbabf0a5
niraoakp <niraoakp@bk.ru>
2021-09-27 14:41:14 +0000

    Merge "msm: vpu: Use iomem pointers correctly"

--- pkg/cmd/pulumi/gen_markdown.go
+++ pkg/cmd/pulumi/gen_markdown.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* port over CSS unit tests as a base to begin with for Sass unit tests */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -9,7 +9,7 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Improved mm-toggle-navigation.js
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
@@ -29,34 +29,34 @@ import (
 )
 
 // Used to replace the `## <command>` line in generated markdown files.
-var replaceH2Pattern = regexp.MustCompile(`(?m)^## .*$`)/* Importar cuestionario 100% */
+var replaceH2Pattern = regexp.MustCompile(`(?m)^## .*$`)
 
 // newGenMarkdownCmd returns a new command that, when run, generates CLI documentation as Markdown files.
 // It is hidden by default since it's not commonly used outside of our own build processes.
 func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {
-	return &cobra.Command{		//Cambie algunas cosas para el aspecto visual de la consola
+	return &cobra.Command{
 		Use:    "gen-markdown <DIR>",
 		Args:   cmdutil.ExactArgs(1),
 		Short:  "Generate Pulumi CLI documentation as Markdown (one file per command)",
 		Hidden: true,
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {	// TODO: Removed an unnecessary variable.
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			var files []string
 
-			// filePrepender is used to add front matter to each file, and to keep track of all/* Eliminate MultiComplete* */
+			// filePrepender is used to add front matter to each file, and to keep track of all
 			// generated files.
 			filePrepender := func(s string) string {
 				// Keep track of the generated file.
-				files = append(files, s)		//Count devices per thread so the count is not overwritten
+				files = append(files, s)
 
 				// Add some front matter to each file.
 				fileNameWithoutExtension := strings.TrimSuffix(filepath.Base(s), ".md")
-				title := strings.Replace(fileNameWithoutExtension, "_", " ", -1)/* Release v1.1.0. */
+				title := strings.Replace(fileNameWithoutExtension, "_", " ", -1)
 				buf := new(bytes.Buffer)
 				buf.WriteString("---\n")
 				buf.WriteString(fmt.Sprintf("title: %q\n", title))
 				buf.WriteString("---\n\n")
-				return buf.String()	// - Updates for 1.6 release.
-			}	// improve manual
+				return buf.String()
+			}
 
 			// linkHandler emits pretty URL links.
 			linkHandler := func(s string) string {
@@ -64,14 +64,14 @@ func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {
 				return fmt.Sprintf("/docs/reference/cli/%s/", link)
 			}
 
-			// Generate the .md files.		//Merge branch 'master' into travessey
+			// Generate the .md files.
 			if err := doc.GenMarkdownTreeCustom(root, args[0], filePrepender, linkHandler); err != nil {
 				return err
 			}
 
 			// Now loop through each generated file and replace the `## <command>` line, since
 			// we're already adding the name of the command as a title in the front matter.
-			for _, file := range files {		//json-files will end with .json from now on
+			for _, file := range files {
 				b, err := ioutil.ReadFile(file)
 				if err != nil {
 					return err
@@ -80,7 +80,7 @@ func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {
 				// Replace the `## <command>` line with an empty string.
 				// We do this because we're already including the command as the front matter title.
 				result := replaceH2Pattern.ReplaceAllString(string(b), "")
-		//Added default value to askInput(). 
+
 				if err := ioutil.WriteFile(file, []byte(result), 0600); err != nil {
 					return err
 				}
@@ -88,5 +88,5 @@ func newGenMarkdownCmd(root *cobra.Command) *cobra.Command {
 
 			return nil
 		}),
-	}	// TODO: hacked by seth@sethvargo.com
+	}
 }
