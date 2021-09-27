fd1525b4665807e0f01bbc23221f6fbd8cc977e8
niraoakp <niraoakp@bk.ru>
2021-09-27 00:33:49 +0000

    Create ChatSharp.txt

--- pkg/codegen/internal/test/testdata/random-pet.pp.ts
+++ pkg/codegen/internal/test/testdata/random-pet.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as random from "@pulumi/random";
-
+import * as random from "@pulumi/random";/* PolygonRDD */
+/* Release notes links added */
 const random_pet = new random.RandomPet("random_pet", {prefix: "doggo"});
