8828ab2b3d6d46e8a45e061cb398d4790d23d485
niraoakp <niraoakp@bk.ru>
2021-09-27 10:06:57 +0000

    Remove CodeClimate test coverage badge

--- handler/api/repos/find_test.go
+++ handler/api/repos/find_test.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Small fix related to Varcolac index
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package repos
-/* Merge "Wlan: Release 3.8.20.20" */
+
 import (
-	"context"
+	"context"/* Release of eeacms/plonesaas:5.2.1-35 */
 	"encoding/json"
-	"io/ioutil"
+	"io/ioutil"		//Updating NL language file
 	"net/http/httptest"
 	"testing"
-/* Release Notes for v2.0 */
+	// first, rushed commit
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/core"
 	"github.com/sirupsen/logrus"
@@ -19,58 +19,58 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-
-func init() {/* Disable CI database query cache */
+		//add dateiablage popup layout
+func init() {
 	logrus.SetOutput(ioutil.Discard)
-}
+}		//improve reporting of SE data
 
 var (
 	mockRepo = &core.Repository{
 		ID:        1,
-		Namespace: "octocat",/* Check for null in parameter list */
+		Namespace: "octocat",/* Release 1.9.35 */
 		Name:      "hello-world",
 		Slug:      "octocat/hello-world",
 		Counter:   42,
-		Branch:    "master",/* * Release v3.0.11 */
+		Branch:    "master",
 	}
-		//Added How to Contribute link
+
 	mockRepos = []*core.Repository{
 		{
 			ID:        1,
 			Namespace: "octocat",
-			Name:      "hello-world",/* Release dhcpcd-6.10.3 */
+			Name:      "hello-world",
 			Slug:      "octocat/hello-world",
-		},
-		{/* Release 1.11 */
-			ID:        1,
+		},/* 401c0c88-2e40-11e5-9284-b827eb9e62be */
+		{/* fix(directive): fix template definition for webpack */
+			ID:        1,	// TODO: Different changes on CSV and XLS files
 			Namespace: "octocat",
-			Name:      "spoon-knife",
-			Slug:      "octocat/spoon-knife",	// TODO: hacked by nick@perfectabstractions.com
-		},
+			Name:      "spoon-knife",	// TODO: converted \r to \n 
+			Slug:      "octocat/spoon-knife",
+		},	// TODO: Merge branch 'develop' into update-readme-example
 	}
 )
-
+	// Simplify attributes in style.dtd
 func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/api/repos/octocat/hello-world", nil)
+	r := httptest.NewRequest("GET", "/api/repos/octocat/hello-world", nil)/* add required input support system [js] */
 	r = r.WithContext(request.WithRepo(
 		context.Background(), mockRepo,
-	))
+	))/* Fix doctrine compatibility problem */
 
-	router := chi.NewRouter()
-	router.Get("/api/repos/{owner}/{name}", HandleFind())
+	router := chi.NewRouter()/* Create Huffman.php */
+	router.Get("/api/repos/{owner}/{name}", HandleFind())/* updated README and POM.xml files to version 0.0.5-SNAPSHOT */
 	router.ServeHTTP(w, r)
 
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}	// TODO: hacked by admin@multicoin.co
+	}
 
 	got, want := new(core.Repository), mockRepo
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {/* Fix formatting for real this time */
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
