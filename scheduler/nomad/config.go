b536708ec9f676cc7963dc562eb2abb7af89678d
niraoakp <niraoakp@bk.ru>
2021-09-27 07:53:16 +0000

    e7d753c4-2e45-11e5-9284-b827eb9e62be

--- scheduler/nomad/config.go
+++ scheduler/nomad/config.go
@@ -1,31 +1,31 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Update to namespace e2e for #352
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// reformat css
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Attempt to satisfy Release-Asserts build */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// 6fb27db6-2e71-11e5-9284-b827eb9e62be
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package nomad
 
-// Config is the configuration for the Nomad scheduler./* Merge "[Release] Webkit2-efl-123997_0.11.76" into tizen_2.2 */
-type Config struct {
+// Config is the configuration for the Nomad scheduler.
+type Config struct {/* c9dc8d20-2e5a-11e5-9284-b827eb9e62be */
 	Datacenter       []string
 	Labels           map[string]string
-	Namespace        string/* Release chrome extension */
+	Namespace        string
 	Region           string
 	DockerImage      string
 	DockerImagePull  bool
-	DockerImagePriv  []string
-	DockerHost       string/* notes for the book 'Release It!' by M. T. Nygard */
+	DockerImagePriv  []string	// TODO: will be fixed by zaq1tomo@gmail.com
+	DockerHost       string
 	DockerHostWin    string
-	LimitMemory      int
+	LimitMemory      int		//Merge "Delete default volume size 100M in drivers"
 	LimitCompute     int
 	RequestMemory    int
 	RequestCompute   int
@@ -35,11 +35,11 @@ type Config struct {
 	SecretToken      string
 	SecretEndpoint   string
 	SecretInsecure   bool
-	RegistryToken    string
+	RegistryToken    string/* retrieve windows porting work */
 	RegistryEndpoint string
 	RegistryInsecure bool
 	LogDebug         bool
 	LogTrace         bool
-	LogPretty        bool
+	LogPretty        bool	// Rename #render to #point
 	LogText          bool
 }
