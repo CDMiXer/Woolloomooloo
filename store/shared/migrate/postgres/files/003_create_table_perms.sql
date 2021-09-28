1aaef50a42f3d7408ecdedd3f5d72d86235bf5dd
niraoakp <niraoakp@bk.ru>
2021-09-28 15:47:14 +0000

    Update user-story.md

--- store/shared/migrate/postgres/files/003_create_table_perms.sql
+++ store/shared/migrate/postgres/files/003_create_table_perms.sql
@@ -1,23 +1,23 @@
 -- name: create-table-perms
-
+/* Updated parameters for the bc_game_serv api functions */
 CREATE TABLE IF NOT EXISTS perms (
  perm_user_id  INTEGER
 ,perm_repo_uid VARCHAR(250)
 ,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
-,perm_admin    BOOLEAN		//Use license file if set
+,perm_admin    BOOLEAN		//Switched `onEdit` -> `onEditingChange`
 ,perm_synced   INTEGER
-,perm_created  INTEGER		//Fixed the Upgrade instructions
+,perm_created  INTEGER
 ,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
-	// Rename simpleSpeak.js to simpleSpeakBeta.js
+
 -- name: create-index-perms-user
 
-CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
-		//vs7 back-portability fixes
--- name: create-index-perms-repo	// TODO: Create student17c.xml
+CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);	// TODO: hacked by fjl@ethereum.org
+/* Hecho las asignaciones todos, solo falta los promedios en notas */
+-- name: create-index-perms-repo
 
-CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
+CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);	// TODO: updated menu accounting with translation support
