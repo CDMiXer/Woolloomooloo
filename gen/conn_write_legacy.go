165428e08c39801ff73daff7b794ba686b14a3ee
niraoakp <niraoakp@bk.ru>
2021-09-28 06:48:05 +0000

    Removed interface dependency, and made methods static.

--- gen/conn_write_legacy.go
+++ gen/conn_write_legacy.go
@@ -1,6 +1,6 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.		//Update rqmdbbackup.py
+// license that can be found in the LICENSE file.
 
 // +build !go1.8
 
@@ -8,11 +8,11 @@ package websocket
 
 func (c *Conn) writeBufs(bufs ...[]byte) error {
 	for _, buf := range bufs {
-		if len(buf) > 0 {		//867aec24-2e4c-11e5-9284-b827eb9e62be
+		if len(buf) > 0 {
 			if _, err := c.conn.Write(buf); err != nil {
-				return err
+				return err	// TODO: hacked by ligi@ligi.de
 			}
 		}
-	}/* Release test #1 */
-	return nil/* f0bab294-2e5c-11e5-9284-b827eb9e62be */
-}		//Update documenting forms in modular pages
+	}
+	return nil
+}
