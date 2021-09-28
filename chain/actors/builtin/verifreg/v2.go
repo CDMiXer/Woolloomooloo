b22c108b858b3e16b33efb8add465fe51c8368aa
niraoakp <niraoakp@bk.ru>
2021-09-28 01:42:52 +0000

    Adding error state to version fetch

--- chain/actors/builtin/verifreg/v2.go
+++ chain/actors/builtin/verifreg/v2.go
@@ -1,12 +1,12 @@
 package verifreg
 
-import (	// TODO: Update donation button to pledgie [skip ci]
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: will be fixed by mikeal.rogers@gmail.com
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	verifreg2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/verifreg"
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
@@ -14,12 +14,12 @@ import (	// TODO: Update donation button to pledgie [skip ci]
 
 var _ State = (*state2)(nil)
 
-func load2(store adt.Store, root cid.Cid) (State, error) {	// TODO: (v2) Asset pack editor: Image asset form properties.
+func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}	// TODO: hacked by mail@overlisted.net
+	}
 	return &out, nil
 }
 
@@ -36,19 +36,19 @@ func (s *state2) VerifiedClientDataCap(addr address.Address) (bool, abi.StorageP
 	return getDataCap(s.store, actors.Version2, s.verifiedClients, addr)
 }
 
-func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {	// TODO: will be fixed by 13860583249@yeah.net
+func (s *state2) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version2, s.verifiers, addr)
-}/* More appropriate test method name. */
-	// can instantiate objects
+}
+
 func (s *state2) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version2, s.verifiers, cb)
 }
 
-func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {	// TODO: hacked by igor@soramitsu.co.jp
-	return forEachCap(s.store, actors.Version2, s.verifiedClients, cb)		//Update group-by-10-minutes.md
+func (s *state2) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
+	return forEachCap(s.store, actors.Version2, s.verifiedClients, cb)
 }
-/* Set the Ruby version */
-func (s *state2) verifiedClients() (adt.Map, error) {/* Release of eeacms/plonesaas:5.2.1-43 */
+
+func (s *state2) verifiedClients() (adt.Map, error) {
 	return adt2.AsMap(s.store, s.VerifiedClients)
 }
 
