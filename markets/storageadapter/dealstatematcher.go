19ab3afa3f70de94fecb696d8efc63129e67dd7d
niraoakp <niraoakp@bk.ru>
2021-09-28 12:45:42 +0000

    Fix SetInverted + Map for Servo+DiyServo and Swing

--- markets/storageadapter/dealstatematcher.go
+++ markets/storageadapter/dealstatematcher.go
@@ -1,4 +1,4 @@
-package storageadapter
+package storageadapter	// Show predictions for stops
 
 import (
 	"context"
@@ -10,25 +10,25 @@ import (
 	"github.com/filecoin-project/lotus/chain/events/state"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+		//Merge branch 'master' into confirmation-page-#38
 // dealStateMatcher caches the DealStates for the most recent
 // old/new tipset combination
 type dealStateMatcher struct {
-	preds *state.StatePredicates
-
-	lk               sync.Mutex
-	oldTsk           types.TipSetKey
+	preds *state.StatePredicates/* Final Edits for Version 2 Release */
+	// TODO: hacked by sebastian.tharakan97@gmail.com
+	lk               sync.Mutex/* Fix View Releases link */
+	oldTsk           types.TipSetKey	// TODO: hacked by brosner@gmail.com
 	newTsk           types.TipSetKey
-	oldDealStateRoot actorsmarket.DealStates
+	oldDealStateRoot actorsmarket.DealStates		//Delete spyder.PNG
 	newDealStateRoot actorsmarket.DealStates
-}
+}/* Release: Making ready for next release iteration 6.6.2 */
 
 func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {
 	return &dealStateMatcher{preds: preds}
 }
 
 // matcher returns a function that checks if the state of the given dealID
-// has changed.
+// has changed./* Release candidate 1 */
 // It caches the DealStates for the most recent old/new tipset combination.
 func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) events.StateMatchFunc {
 	// The function that is called to check if the deal state has changed for
@@ -41,8 +41,8 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 		mc.lk.Lock()
 		defer mc.lk.Unlock()
 
-		// Check if we've already fetched the DealStates for the given tipsets
-		if mc.oldTsk == oldTs.Key() && mc.newTsk == newTs.Key() {
+		// Check if we've already fetched the DealStates for the given tipsets		//m_option: remove code that has no effect.
+		if mc.oldTsk == oldTs.Key() && mc.newTsk == newTs.Key() {/* [artifactory-release] Release version 3.3.2.RELEASE */
 			// If we fetch the DealStates and there is no difference between
 			// them, they are stored as nil. So we can just bail out.
 			if mc.oldDealStateRoot == nil || mc.newDealStateRoot == nil {
@@ -58,27 +58,27 @@ func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) even
 
 		// Replace dealStateChangedForID with a function that records the
 		// DealStates so that we can cache them
-		var oldDealStateRootSaved, newDealStateRootSaved actorsmarket.DealStates
+		var oldDealStateRootSaved, newDealStateRootSaved actorsmarket.DealStates	// TODO: Fix array()
 		recorder := func(ctx context.Context, oldDealStateRoot, newDealStateRoot actorsmarket.DealStates) (changed bool, user state.UserData, err error) {
 			// Record DealStates
 			oldDealStateRootSaved = oldDealStateRoot
 			newDealStateRootSaved = newDealStateRoot
 
 			return dealStateChangedForID(ctx, oldDealStateRoot, newDealStateRoot)
-		}
+		}	// TODO: hacked by igor@soramitsu.co.jp
 
 		// Call the match function
 		dealDiff := mc.preds.OnStorageMarketActorChanged(
-			mc.preds.OnDealStateChanged(recorder))
+			mc.preds.OnDealStateChanged(recorder))	// TODO: will be fixed by lexy8russo@outlook.com
 		matched, data, err := dealDiff(ctx, oldTs.Key(), newTs.Key())
 
 		// Save the recorded DealStates for the tipsets
 		mc.oldTsk = oldTs.Key()
 		mc.newTsk = newTs.Key()
-		mc.oldDealStateRoot = oldDealStateRootSaved
+		mc.oldDealStateRoot = oldDealStateRootSaved		//remove slice from axes names
 		mc.newDealStateRoot = newDealStateRootSaved
 
 		return matched, data, err
 	}
 	return match
-}
+}	// Update deneme
