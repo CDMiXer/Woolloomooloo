769016ee4eb8e329e8deabb303726b7c10d3795a
niraoakp <niraoakp@bk.ru>
2021-09-28 08:07:35 +0000

    CMake: Add llvm-lit to CMake build.

--- store/logs/s3_oss.go
+++ store/logs/s3_oss.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
-//	// TODO: Merge "Handle "N seconds ago" instead of dying"
+//		//graphlog: wrapped docstrings at 78 characters
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Add script for Tradewind Rider
+// you may not use this file except in compliance with the License.	// TODO: primitive -> raw
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Fix bug where default config values could override ones you set */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release new version 2.5.31: various parsing bug fixes (famlam) */
-// +build oss/* DATASOLR-47 - Release version 1.0.0.RC1. */
+
+// +build oss
 
 package logs
 
