60197f0d0b9823c0aac47f4b207d1499fdcf48a9
niraoakp <niraoakp@bk.ru>
2021-09-28 21:10:44 +0000

    Remove brackets in links in README.

--- handler/web/login_test.go
+++ handler/web/login_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by nicksavers@gmail.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* @Release [io7m-jcanephora-0.9.1] */
+// that can be found in the LICENSE file.
 
 package web
