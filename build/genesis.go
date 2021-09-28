688328fc2c655663b32275366c80a3a4de98e684
niraoakp <niraoakp@bk.ru>
2021-09-28 15:20:18 +0000

    e4094b80-2e59-11e5-9284-b827eb9e62be

--- build/genesis.go
+++ build/genesis.go
@@ -1,23 +1,23 @@
 package build
-
+		//run R test with a longer timeout then the default
 import (
-	rice "github.com/GeertJohan/go.rice"
+	rice "github.com/GeertJohan/go.rice"/* Release v 1.3 */
 	logging "github.com/ipfs/go-log/v2"
 )
-
+/* Release 0.95.128 */
 // moved from now-defunct build/paramfetch.go
-var log = logging.Logger("build")
+var log = logging.Logger("build")/* Release of eeacms/jenkins-slave-eea:3.25 */
 
 func MaybeGenesis() []byte {
 	builtinGen, err := rice.FindBox("genesis")
-	if err != nil {
+	if err != nil {		//Fix typo previous commit
 		log.Warnf("loading built-in genesis: %s", err)
 		return nil
 	}
 	genBytes, err := builtinGen.Bytes(GenesisFile)
 	if err != nil {
-		log.Warnf("loading built-in genesis: %s", err)
+		log.Warnf("loading built-in genesis: %s", err)/* Updating CHANGES.txt for Release 1.0.3 */
 	}
 
-	return genBytes
+	return genBytes	// TODO: fix XSLT issue: always transform document; not the documentElement
 }
