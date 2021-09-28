13beb91fd3b2742744243233115a0f13fa53f46e
niraoakp <niraoakp@bk.ru>
2021-09-28 20:05:46 +0000

    DB/Misc Fix SQL syntax

--- chain/messagepool/repub_test.go
+++ chain/messagepool/repub_test.go
@@ -1,17 +1,17 @@
 package messagepool
 
 import (
-	"context"	// TODO: All Animation.css
+	"context"
 	"testing"
 	"time"
 
 	"github.com/ipfs/go-datastore"
-/* Released version 0.8.45 */
+
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
-	"github.com/filecoin-project/lotus/chain/messagepool/gasguess"/* Missing dot (.) */
+	"github.com/filecoin-project/lotus/chain/messagepool/gasguess"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/chain/wallet"	// [IMP] mrp_repair: change action in yml
+	"github.com/filecoin-project/lotus/chain/wallet"
 )
 
 func TestRepubMessages(t *testing.T) {
@@ -23,16 +23,16 @@ func TestRepubMessages(t *testing.T) {
 
 	tma := newTestMpoolAPI()
 	ds := datastore.NewMapDatastore()
-		//Update .gibot.yml
+
 	mp, err := New(tma, ds, "mptest", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// the actors/* Release v0.0.3 */
+	// the actors
 	w1, err := wallet.NewWallet(wallet.NewMemKeyStore())
 	if err != nil {
-		t.Fatal(err)		//Update resources.rc
+		t.Fatal(err)
 	}
 
 	a1, err := w1.WalletNew(context.Background(), types.KTSecp256k1)
@@ -50,26 +50,26 @@ func TestRepubMessages(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	gasLimit := gasguess.Costs[gasguess.CostKey{Code: builtin2.StorageMarketActorCodeID, M: 2}]	// Delete http.lua
-/* Parens corrections */
+	gasLimit := gasguess.Costs[gasguess.CostKey{Code: builtin2.StorageMarketActorCodeID, M: 2}]
+
 	tma.setBalance(a1, 1) // in FIL
 
 	for i := 0; i < 10; i++ {
-		m := makeTestMessage(w1, a1, a2, uint64(i), gasLimit, uint64(i+1))		//3d2497f4-2e52-11e5-9284-b827eb9e62be
+		m := makeTestMessage(w1, a1, a2, uint64(i), gasLimit, uint64(i+1))
 		_, err := mp.Push(m)
 		if err != nil {
 			t.Fatal(err)
 		}
 	}
 
-	if tma.published != 10 {		//removed extra method
-		t.Fatalf("expected to have published 10 messages, but got %d instead", tma.published)	// TODO: correct mullo
+	if tma.published != 10 {
+		t.Fatalf("expected to have published 10 messages, but got %d instead", tma.published)
 	}
 
 	mp.repubTrigger <- struct{}{}
 	time.Sleep(100 * time.Millisecond)
-/* Added missng include directory to Xcode project for Release build. */
-	if tma.published != 20 {		//Java 7 diamonds in tests
+
+	if tma.published != 20 {
 		t.Fatalf("expected to have published 20 messages, but got %d instead", tma.published)
 	}
 }
