46637b358f3a50b7e8d505f10d768ae8b77cc037
niraoakp <niraoakp@bk.ru>
2021-09-28 18:10:18 +0000

    add instructions for interactive use

--- grpc-go/xds/internal/balancer/priority/utils.go
+++ grpc-go/xds/internal/balancer/priority/utils.go
@@ -1,22 +1,22 @@
 /*
  *
- * Copyright 2021 gRPC authors./* lvl19 lewd */
+ * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* correct expiration properties */
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Add more GameProperty functionality
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Include link to CDAP page */
+ *
  */
-		//Added links to CVEDetails.com
-package priority		//set busy cursor and statusbar message while creating clones
+
+package priority
 
 func equalStringSlice(a, b []string) bool {
 	if len(a) != len(b) {
