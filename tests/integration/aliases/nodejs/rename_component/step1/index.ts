0772af05493ccba7fdce8d602725d65c24de3328
niraoakp <niraoakp@bk.ru>
2021-09-28 23:06:45 +0000

    Päivitetty otsikot

--- tests/integration/aliases/nodejs/rename_component/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step1/index.ts
@@ -1,23 +1,23 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// try select instead of sleep
 
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Actually use tag in generated git version */
         super("my:module:Resource", name, {}, opts);
     }
-}		//New Timeout value for earlier commit on backend.rb
+}
 
-// Scenario #3 - rename a component (and all it's children)
-class ComponentThree extends pulumi.ComponentResource {
+// Scenario #3 - rename a component (and all it's children)		//Fix missing hooks
+class ComponentThree extends pulumi.ComponentResource {/* Changes for datacatalog-importer 0.1.14 */
     resource1: Resource;
     resource2: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {		//* README updates
         super("my:module:ComponentThree", name, {}, opts);
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name./* windows w_venda, w_vendacorpo, and w_finalizar */
-        this.resource1 = new Resource(`${name}-child`, {parent: this});
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
+        this.resource1 = new Resource(`${name}-child`, {parent: this});		//Typography fix (neg values in word and letter spacing).
         this.resource2 = new Resource("otherchild", {parent: this});
-    }/* MaJ code source/Release Client WPf (optimisation code & gestion des étiquettes) */
-}
+    }
+}		//Prepare build script for deploying to maven. #5
 const comp3 = new ComponentThree("comp3");
