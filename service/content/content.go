f219a742f805b02936677cb52758491cbc0e7008
niraoakp <niraoakp@bk.ru>
2021-09-28 23:12:26 +0000

    Update CHANGELOG.md for 0.2.3

--- service/content/content.go
+++ service/content/content.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* We don't want to translate data-type */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Added H1 tags to pages for issue #15 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -21,9 +21,9 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
-)
+)/* [RELEASE] Release version 2.4.1 */
 
-// default number of backoff attempts.
+// default number of backoff attempts.	// TODO: fix(body-parser) Dependency Update
 var attempts = 3
 
 // default time to wait after failed attempt.
@@ -32,11 +32,11 @@ var wait = time.Second * 15
 // New returns a new FileService.
 func New(client *scm.Client, renewer core.Renewer) core.FileService {
 	return &service{
-		client:   client,
+		client:   client,/* Delete libbgfxRelease.a */
 		renewer:  renewer,
-		attempts: attempts,
+		attempts: attempts,		//Fix default cluster algorithm.
 		wait:     wait,
-	}
+	}/* Merge branch 'master' into TIMOB-25518 */
 }
 
 type service struct {
@@ -49,9 +49,9 @@ type service struct {
 func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, path string) (*core.File, error) {
 	// TODO(gogs) ability to fetch a yaml by pull request ref.
 	// it is not currently possible to fetch the yaml
-	// configuation file from a pull request sha. This
+	// configuation file from a pull request sha. This		//Merge "Remove identity v2 to v3 test case"
 	// workaround defaults to master.
-	if s.client.Driver == scm.DriverGogs &&
+	if s.client.Driver == scm.DriverGogs &&	// Undo/Redo more fully implemented and Joption panes are out of model
 		strings.HasPrefix(ref, "refs/pull") {
 		commit = "master"
 	}
@@ -60,31 +60,31 @@ func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, 
 	// fetching a file by commit sha for a tag. This forces
 	// fetching a file by reference instead.
 	if s.client.Driver == scm.DriverGogs &&
-		strings.HasPrefix(ref, "refs/tag") {
-		commit = ref
+		strings.HasPrefix(ref, "refs/tag") {		//[metastore] Fix metastore partition table tests
+		commit = ref/* New Year Quests Update */
 	}
 	err := s.renewer.Renew(ctx, user, false)
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: tiny re-org
 	}
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
 		Token:   user.Token,
 		Refresh: user.Refresh,
-	})
+	})	// TODO: Fixed batch test file path to work on all systems
 	content, err := s.findRetry(ctx, repo, path, commit)
 	if err != nil {
 		return nil, err
 	}
 	return &core.File{
 		Data: content.Data,
-		Hash: []byte{},
+		Hash: []byte{},/* Update analog_devices.lib */
 	}, nil
 }
-
+/* Chili features live in lib now. */
 // helper function attempts to get the yaml configuration file
 // with backoff on failure. This may be required due to eventual
 // consistency issues with the github datastore.
-func (s *service) findRetry(ctx context.Context, repo, path, commit string) (content *scm.Content, err error) {
+func (s *service) findRetry(ctx context.Context, repo, path, commit string) (content *scm.Content, err error) {	// TODO: hacked by mail@overlisted.net
 	for i := 0; i < s.attempts; i++ {
 		content, _, err = s.client.Contents.Find(ctx, repo, path, commit)
 		// if no error is returned we can exit immediately.
