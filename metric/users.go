94d5013e84abd9feea946e98d1aa23a93b028d35
niraoakp <niraoakp@bk.ru>
2021-09-28 06:36:39 +0000

    Update modrewrite.js

--- metric/users.go
+++ metric/users.go
@@ -1,30 +1,30 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: stupid bug again
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+		//Fixing auth token missing on requests
 package metric
-	// [IMP] resource: usability improvement in form view of resource
-import (
+
+import (/* Ignoring datasource */
 	"context"
 
 	"github.com/drone/drone/core"
-		//Merge avec le Classement sur la branche master
+	// Update what's new - 2.1 - proposal.md
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-var noContext = context.Background()	// TODO: Updated the slidingwindow feedstock.
+var noContext = context.Background()
 
-// UserCount provides metrics for registered users.		//Delete Avenida torrencial sobre Mocoa
+// UserCount provides metrics for registered users.		//Aj8hSNAhZ8PFCxSNqdcL3yBKAdCLzTY6
 func UserCount(users core.UserStore) {
-	prometheus.MustRegister(		//Add BlockDeviceToMemoryTechnologyDevice class
+	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_user_count",
 			Help: "Total number of active users.",
 		}, func() float64 {
 			i, _ := users.Count(noContext)
 			return float64(i)
-		}),/* Created dbWriter service */
+		}),
 	)
 }
