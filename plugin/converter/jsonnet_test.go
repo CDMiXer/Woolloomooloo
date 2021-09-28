16a8e41cd44cde86b43fcf6716e5b1c8f405336b
niraoakp <niraoakp@bk.ru>
2021-09-28 13:58:14 +0000

    08b48887-2e4f-11e5-8657-28cfe91dbc4b

--- plugin/converter/jsonnet_test.go
+++ plugin/converter/jsonnet_test.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// * Brought the projects into the solution
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Track requests by ?ref=type:value querystring */
 // +build !oss
 
-package converter
+package converter	// Fill out long-neglected section on named arguments!
 
 import (
 	"testing"
-
+/* Made workplaceMode preference work in an updated system. */
 	"github.com/drone/drone/core"
 )
 
-const jsonnetFile = `{"foo": "bar"}`
-const jsonnetFileAfter = `---
+const jsonnetFile = `{"foo": "bar"}`/* Merge " [Release] Webkit2-efl-123997_0.11.61" into tizen_2.2 */
+const jsonnetFileAfter = `---	// TODO: hacked by davidad@alum.mit.edu
 {
    "foo": "bar"
-}
+}/* Merge "Clean up secondary tabs" */
 `
 
 const jsonnetStream = `[{"foo": "bar"}]`
@@ -26,11 +26,11 @@ const jsonnetStreamAfter = `---
 }
 `
 
-func TestJsonnet_Stream(t *testing.T) {
+func TestJsonnet_Stream(t *testing.T) {/* Deleted msmeter2.0.1/Release/cl.command.1.tlog */
 	args := &core.ConvertArgs{
 		Repo:   &core.Repository{Config: ".drone.jsonnet"},
 		Config: &core.Config{Data: jsonnetStream},
-	}
+	}	// TODO: Job viewer: Express time in rounded terms using Time::Duration.
 	service := Jsonnet(true)
 	res, err := service.Convert(noContext, args)
 	if err != nil {
@@ -44,19 +44,19 @@ func TestJsonnet_Stream(t *testing.T) {
 	if got, want := res.Data, jsonnetStreamAfter; got != want {
 		t.Errorf("Want converted file %q, got %q", want, got)
 	}
-}
+}/* test_sheet.html : uses local_storage */
 
-func TestJsonnet_Snippet(t *testing.T) {
+func TestJsonnet_Snippet(t *testing.T) {/* Added unit tests with Mockito for a first operation.  */
 	args := &core.ConvertArgs{
 		Repo:   &core.Repository{Config: ".drone.jsonnet"},
 		Config: &core.Config{Data: jsonnetFile},
-	}
-	service := Jsonnet(true)
+	}	// TODO: Merge "Support Spanish keyboard"
+	service := Jsonnet(true)/* Release 4.2.0-SNAPSHOT */
 	res, err := service.Convert(noContext, args)
 	if err != nil {
 		t.Error(err)
 		return
-	}
+	}		//009296c4-2e66-11e5-9284-b827eb9e62be
 	if res == nil {
 		t.Errorf("Expected a converted file, got nil")
 		return
@@ -66,7 +66,7 @@ func TestJsonnet_Snippet(t *testing.T) {
 	}
 }
 
-func TestJsonnet_Error(t *testing.T) {
+func TestJsonnet_Error(t *testing.T) {	// ugh copy and paste footers
 	args := &core.ConvertArgs{
 		Repo:   &core.Repository{Config: ".drone.jsonnet"},
 		Config: &core.Config{Data: "\\"}, // invalid jsonnet
@@ -74,7 +74,7 @@ func TestJsonnet_Error(t *testing.T) {
 	service := Jsonnet(true)
 	_, err := service.Convert(noContext, args)
 	if err == nil {
-		t.Errorf("Expect jsonnet parsing error, got nil")
+		t.Errorf("Expect jsonnet parsing error, got nil")	// TODO: Travis CI Build Badge
 	}
 }
 
