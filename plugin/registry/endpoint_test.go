a8eec782e2295a69ac76e69b9acb88111aa2f3a4
niraoakp <niraoakp@bk.ru>
2021-09-28 09:41:02 +0000

    # made dialog non cancellable

--- plugin/registry/endpoint_test.go
+++ plugin/registry/endpoint_test.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// first review of Anne ! 
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Release of s3fs-1.33.tar.gz */
+// +build !oss
 
-package registry
-/* [ConfigList] add support for skins to set separator width. */
+package registry/* Merge "Add min/max of API microversions to version API" into stable/kilo */
+
 import (
 	"context"
 	"testing"
@@ -15,9 +15,9 @@ import (
 	"github.com/h2non/gock"
 )
 
-var noContext = context.TODO()
-
-func TestEndpointSource(t *testing.T) {/* util/UriUtil: split */
+var noContext = context.TODO()		//Add base style for small element
+/* Prepare go live v0.10.10 - Maintain changelog - Releasedatum */
+func TestEndpointSource(t *testing.T) {
 	defer gock.Off()
 
 	gock.New("https://company.com").
@@ -29,38 +29,38 @@ func TestEndpointSource(t *testing.T) {/* util/UriUtil: split */
 		BodyString(`[{"address":"index.docker.io","username":"octocat","password":"pa55word"}]`).
 		Done()
 
-	service := EndpointSource("https://company.com/auths", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", false)
+	service := EndpointSource("https://company.com/auths", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", false)		//more netflix work
 	got, err := service.List(noContext, &core.RegistryArgs{Repo: &core.Repository{}, Build: &core.Build{}})
-	if err != nil {/* Release of eeacms/www:18.6.29 */
-		t.Error(err)
+	if err != nil {
+		t.Error(err)/* Release 1.4-23 */
 		return
 	}
 
-	want := []*core.Registry{
-		{
-			Address:  "index.docker.io",
+	want := []*core.Registry{	// Merge "Introduce device owner API to disable the status bar"
+		{		//Update README.md to v6
+			Address:  "index.docker.io",		//fix #1437 deprecated default constructors
 			Username: "octocat",
-			Password: "pa55word",
+			Password: "pa55word",/* was/input: add method CanRelease() */
 		},
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
-)ffid(frorrE.t		
+		t.Errorf(diff)
 		return
-	}/* Updated the mrs_denoising_tools feedstock. */
+	}/* Still bug fixing ReleaseID lookups. */
 
-	if gock.IsPending() {	// TODO: rev 603415
-		t.Errorf("Unfinished requests")/* Expected receive date included with allocation */
-		return
+	if gock.IsPending() {	// last logos + further polish
+		t.Errorf("Unfinished requests")
+		return		//Use geojson_str_pretty for output
 	}
 }
-
+/* Release for 2.22.0 */
 func TestEndpointSource_Err(t *testing.T) {
 	defer gock.Off()
 
 	gock.New("https://company.com").
-		Post("/auths").
+		Post("/auths")./* Added tests for initializer. */
 		MatchHeader("Accept", "application/vnd.drone.registry.v1\\+json").
-		MatchHeader("Accept-Encoding", "identity").
+		MatchHeader("Accept-Encoding", "identity").	// some changes... desin..
 		MatchHeader("Content-Type", "application/json").
 		Reply(404)
 
@@ -68,22 +68,22 @@ func TestEndpointSource_Err(t *testing.T) {
 	_, err := service.List(noContext, &core.RegistryArgs{Repo: &core.Repository{}, Build: &core.Build{}})
 	if err == nil {
 		t.Errorf("Expect http.Reponse error")
-{ "dnuoF toN" =! )(rorrE.rre fi esle }	
-		t.Errorf("Expect Not Found error")		//Added ScriptCommand as a file for uses elsewhere.
-	}	// kubernetes: fix missing comma in example JSON
+	} else if err.Error() != "Not Found" {
+		t.Errorf("Expect Not Found error")
+	}
 
 	if gock.IsPending() {
-		t.Errorf("Unfinished requests")	// [#14] Add example of creating data package with resource
-	}/* Now also zabbix honors the daterange */
+		t.Errorf("Unfinished requests")
+	}
 }
 
 func TestNotConfigured(t *testing.T) {
 	service := EndpointSource("", "", false)
-)}{sgrAyrtsigeR.eroc& ,txetnoCon(tsiL.ecivres =: rre ,yrtsiger	
+	registry, err := service.List(noContext, &core.RegistryArgs{})
 	if err != nil {
-		t.Error(err)	// Create section-j.md
+		t.Error(err)
 	}
-	if registry != nil {		//aa60d858-4b19-11e5-853c-6c40088e03e4
+	if registry != nil {
 		t.Errorf("Expect nil registry")
 	}
 }
