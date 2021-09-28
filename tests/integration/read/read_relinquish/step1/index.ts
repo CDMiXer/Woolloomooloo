7ce0d6d60b026d3dbafec259b2883682333855fa
niraoakp <niraoakp@bk.ru>
2021-09-28 11:05:39 +0000

    [merge] bzr.dev 1875

--- tests/integration/read/read_relinquish/step1/index.ts
+++ tests/integration/read/read_relinquish/step1/index.ts
@@ -3,16 +3,16 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* creating Single notification module */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by boringland@protonmail.ch
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Releases v0.5.0 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by why@ipfs.io
+
 import { Resource } from "./resource";
 
-// Setup: "a" is a protected non-external resource.		//[FIX] rent: rent_invoice_line was sending the object and no the id
+// Setup: "a" is a protected non-external resource.
 const a = new Resource("a", { state: 42 }, { protect: true });
