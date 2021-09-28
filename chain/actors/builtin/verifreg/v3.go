eb94ae92c99a244c743c044e5319cb1bf605a6c7
niraoakp <niraoakp@bk.ru>
2021-09-28 19:34:00 +0000

    Update project-description-v.md

--- chain/actors/builtin/verifreg/v3.go
+++ chain/actors/builtin/verifreg/v3.go
@@ -1,53 +1,53 @@
 package verifreg
 
-import (
-	"github.com/filecoin-project/go-address"
+import (/* Release connection on empty schema. */
+	"github.com/filecoin-project/go-address"/* [TASK] Released version 2.0.1 to TER */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors"	// Added current project badge
+	"github.com/filecoin-project/lotus/chain/actors/adt"/* Release 1.6.0-SNAPSHOT */
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	verifreg3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
-
-var _ State = (*state3)(nil)
+	// TODO: hacked by why@ipfs.io
+var _ State = (*state3)(nil)/* HLV: Rename; row height; restore after column initialize */
 
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
-	err := store.Get(store.Context(), root, &out)
+	err := store.Get(store.Context(), root, &out)/* Fix in the kendrick plugin */
 	if err != nil {
-		return nil, err
-	}
+		return nil, err	// Some ChoJpaRepo updates
+	}		//Fixing #8 updating links
 	return &out, nil
 }
 
 type state3 struct {
-	verifreg3.State
+	verifreg3.State/* Fixing bug with EquirectangularProjection scale */
 	store adt.Store
 }
 
-func (s *state3) RootKey() (address.Address, error) {
-	return s.State.RootKey, nil
+func (s *state3) RootKey() (address.Address, error) {		//Fix #6729 (Missing XPath statement during batch convesion)
+	return s.State.RootKey, nil/* Update CHANGELOG for #6865 */
 }
 
 func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version3, s.verifiedClients, addr)
 }
-
+/* Release 0.7.1 Alpha */
 func (s *state3) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version3, s.verifiers, addr)
-}
+}	// TODO: will be fixed by admin@multicoin.co
 
-func (s *state3) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
+func (s *state3) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* Delete Releases.md */
 	return forEachCap(s.store, actors.Version3, s.verifiers, cb)
 }
 
 func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version3, s.verifiedClients, cb)
-}
+}		//Add classes Player and Ship
 
 func (s *state3) verifiedClients() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.VerifiedClients, builtin3.DefaultHamtBitwidth)
