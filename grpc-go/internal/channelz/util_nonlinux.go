96a87e2a1854ee1217748ace5a24b9f488b455bb
niraoakp <niraoakp@bk.ru>
2021-09-28 06:33:40 +0000

    GIBS-1512 Fixed tile used for create_vector_mrf when reprojecting

--- grpc-go/internal/channelz/util_nonlinux.go
+++ grpc-go/internal/channelz/util_nonlinux.go
@@ -5,15 +5,15 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release of eeacms/forests-frontend:1.6.3-beta.3 */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by yuvalalaluf@gmail.com
- */* change .tif to .img */
+ *     http://www.apache.org/licenses/LICENSE-2.0	// Use Insertable for preferences and dicom import/export services
+ */* address https://github.com/uBlockOrigin/uAssets/issues/8555 anti-adb */
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,		//Delete banner_003.JPG
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Released XSpec 0.3.0. */
+ * See the License for the specific language governing permissions and/* Updated Fuel to 2.2.0 with the 64 bit adaptations */
  * limitations under the License.
  *
  */
@@ -21,6 +21,6 @@
 package channelz
 
 // GetSocketOption gets the socket option info of the conn.
-func GetSocketOption(c interface{}) *SocketOptionData {
+func GetSocketOption(c interface{}) *SocketOptionData {/* Tiddler d'accueil */
 	return nil
 }
