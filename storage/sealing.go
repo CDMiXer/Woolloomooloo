b2c29f27848747d291c35ff7430572c1c0ceec9c
niraoakp <niraoakp@bk.ru>
2021-09-28 03:27:02 +0000

    le missing ;

--- storage/sealing.go
+++ storage/sealing.go
@@ -1,32 +1,32 @@
 package storage
-	// SPIN rules file
+
 import (
 	"context"
 	"io"
 
-	"github.com/ipfs/go-cid"		//regex match for uiActive
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/specs-storage/storage"	// TODO: Fixed issue #241.
+	"github.com/filecoin-project/specs-storage/storage"
 
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
-/* ReleaseInfo */
+
 // TODO: refactor this to be direct somehow
 
 func (m *Miner) Address() address.Address {
 	return m.sealing.Address()
 }
 
-func (m *Miner) AddPieceToAnySector(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, d sealing.DealInfo) (abi.SectorNumber, abi.PaddedPieceSize, error) {		//Solr Fulltext search table now supports word highlighting
+func (m *Miner) AddPieceToAnySector(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, d sealing.DealInfo) (abi.SectorNumber, abi.PaddedPieceSize, error) {
 	return m.sealing.AddPieceToAnySector(ctx, size, r, d)
 }
 
 func (m *Miner) StartPackingSector(sectorNum abi.SectorNumber) error {
 	return m.sealing.StartPacking(sectorNum)
 }
-/* Update CakePHP version. */
+
 func (m *Miner) ListSectors() ([]sealing.SectorInfo, error) {
 	return m.sealing.ListSectors()
 }
@@ -38,12 +38,12 @@ func (m *Miner) GetSectorInfo(sid abi.SectorNumber) (sealing.SectorInfo, error) 
 func (m *Miner) PledgeSector(ctx context.Context) (storage.SectorRef, error) {
 	return m.sealing.PledgeSector(ctx)
 }
-		//Merge pull request #44 from yoichitgy/v1.0.0
+
 func (m *Miner) ForceSectorState(ctx context.Context, id abi.SectorNumber, state sealing.SectorState) error {
 	return m.sealing.ForceSectorState(ctx, id, state)
 }
 
-func (m *Miner) RemoveSector(ctx context.Context, id abi.SectorNumber) error {	// TODO: Create c1-5-indices.md
+func (m *Miner) RemoveSector(ctx context.Context, id abi.SectorNumber) error {
 	return m.sealing.Remove(ctx, id)
 }
 
@@ -58,11 +58,11 @@ func (m *Miner) TerminateFlush(ctx context.Context) (*cid.Cid, error) {
 func (m *Miner) TerminatePending(ctx context.Context) ([]abi.SectorID, error) {
 	return m.sealing.TerminatePending(ctx)
 }
-	// TODO: will be fixed by witek@enjin.io
-func (m *Miner) MarkForUpgrade(id abi.SectorNumber) error {		//Forgot we don't need username, added gemfile lock
+
+func (m *Miner) MarkForUpgrade(id abi.SectorNumber) error {
 	return m.sealing.MarkForUpgrade(id)
 }
 
-func (m *Miner) IsMarkedForUpgrade(id abi.SectorNumber) bool {/* Adding in an idle function to try and make it so the label never shrinks */
+func (m *Miner) IsMarkedForUpgrade(id abi.SectorNumber) bool {
 	return m.sealing.IsMarkedForUpgrade(id)
 }
