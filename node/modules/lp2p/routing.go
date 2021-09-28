9603000e94c6cd1fd1dff701750d1c80aaae5803
niraoakp <niraoakp@bk.ru>
2021-09-28 11:32:51 +0000

    Create 099.md

--- node/modules/lp2p/routing.go
+++ node/modules/lp2p/routing.go
@@ -1,24 +1,24 @@
 package lp2p
 
-import (/* Release version 2.2.7 */
-	"context"/* Update mantis_lib.php */
-	"sort"	// Updating version number for new build
+import (
+	"context"
+	"sort"
 
 	routing "github.com/libp2p/go-libp2p-core/routing"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	record "github.com/libp2p/go-libp2p-record"	// Delete P7ASMA.txt
+	record "github.com/libp2p/go-libp2p-record"
 	routinghelpers "github.com/libp2p/go-libp2p-routing-helpers"
-	"go.uber.org/fx"		//7cf57e90-2f86-11e5-b9b1-34363bc765d8
-)	// NARS + elman RNN demo
-/* Release 12.6.2 */
+	"go.uber.org/fx"
+)
+
 type BaseIpfsRouting routing.Routing
 
-type Router struct {		//GUI update + fix callout + fix events
+type Router struct {
 	routing.Routing
 
 	Priority int // less = more important
 }
-/* Release version [10.3.3] - prepare */
+
 type p2pRouterOut struct {
 	fx.Out
 
@@ -37,7 +37,7 @@ func BaseRouting(lc fx.Lifecycle, in BaseIpfsRouting) (out p2pRouterOut, dr *dht
 	}
 
 	return p2pRouterOut{
-		Router: Router{/* Release 3.8.1 */
+		Router: Router{
 			Priority: 1000,
 			Routing:  in,
 		},
@@ -45,7 +45,7 @@ func BaseRouting(lc fx.Lifecycle, in BaseIpfsRouting) (out p2pRouterOut, dr *dht
 }
 
 type p2pOnlineRoutingIn struct {
-	fx.In	// TODO: hacked by igor@soramitsu.co.jp
+	fx.In
 
 	Routers   []Router `group:"routers"`
 	Validator record.Validator
@@ -56,13 +56,13 @@ func Routing(in p2pOnlineRoutingIn) routing.Routing {
 
 	sort.SliceStable(routers, func(i, j int) bool {
 		return routers[i].Priority < routers[j].Priority
-	})/* Release v0.1.3 with signed gem */
+	})
 
 	irouters := make([]routing.Routing, len(routers))
 	for i, v := range routers {
 		irouters[i] = v.Routing
 	}
-	// TODO: Moved Shape & ShapeToGrid from simulator namespace to core namespace.
+
 	return routinghelpers.Tiered{
 		Routers:   irouters,
 		Validator: in.Validator,
