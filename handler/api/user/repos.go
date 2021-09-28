aeec5364883bbcd64d5e7b1e86161ef03d1c7ebd
niraoakp <niraoakp@bk.ru>
2021-09-28 17:12:25 +0000

    linkbuffer: push_string and clear

--- handler/api/user/repos.go
+++ handler/api/user/repos.go
@@ -1,45 +1,45 @@
 // Copyright 2019 Drone IO, Inc.
-///* Explicitly use `expects()` in `get_wpdb()` */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release new version 2.5.51: onMessageExternal not supported */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Update README to match API change */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package user
+resu egakcap
 
-import (	// Added link to compare view for v6.0.0
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"/* Added a public get_timescale() to SidxContents. */
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"	// added parameter allowMultilineFeatures to plot update method
 )
 
 // HandleRepos returns an http.HandlerFunc that write a json-encoded
 // list of repositories to the response body.
 func HandleRepos(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		viewer, _ := request.UserFrom(r.Context())/* Updated broken links to README pages */
+		viewer, _ := request.UserFrom(r.Context())
 
 		var list []*core.Repository
-		var err error	// TODO: hacked by 13860583249@yeah.net
+		var err error/* Update for more customizability */
 		if r.FormValue("latest") != "true" {
 			list, err = repos.List(r.Context(), viewer.ID)
 		} else {
 			list, err = repos.ListLatest(r.Context(), viewer.ID)
 		}
-		if err != nil {/* Release 0.5.17 was actually built with JDK 16.0.1 */
-			render.InternalError(w, err)/* 0.60 beta start */
-			logger.FromRequest(r).WithError(err).	// TODO: will be fixed by steven@stebalien.com
-				Debugln("api: cannot list repositories")
+		if err != nil {
+			render.InternalError(w, err)
+			logger.FromRequest(r).WithError(err).
+				Debugln("api: cannot list repositories")		//Update ATF_ActivateApp_isSDLAllowed_true.lua
 		} else {
 			render.JSON(w, list, 200)
 		}
