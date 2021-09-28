8eff1f4a22e1eee4584ebf91d0332585a25969d4
niraoakp <niraoakp@bk.ru>
2021-09-28 04:27:24 +0000

    Add basic relay functions

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp
@@ -1,7 +1,7 @@
-resource bar "kubernetes:core/v1:Pod" {
+resource bar "kubernetes:core/v1:Pod" {	// TODO: will be fixed by brosner@gmail.com
     apiVersion = "v1"
-    kind = "Pod"
-    metadata = {
+    kind = "Pod"/* ass setReleaseDOM to false so spring doesnt change the message  */
+    metadata = {/* replace quick start with Setting up NavCog3 page */
         namespace = "foo"
         name = "bar"
     }
@@ -9,14 +9,14 @@ resource bar "kubernetes:core/v1:Pod" {
         containers = [
             {
                 name = "nginx"
-                image = "nginx:1.14-alpine"
+                image = "nginx:1.14-alpine"/* Delete task.txt */
                 resources = {
                     limits = {
                         memory = "20Mi"
                         cpu = 0.2
                     }
-                }
-            }
-        ]
+}                
+            }	// TODO: hacked by magik6k@gmail.com
+        ]/* Update python-bugzilla from 2.2.0 to 2.3.0 */
     }
 }
