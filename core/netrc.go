6d8401ea0c3aa6b1c09c34406dccecc4a61787fb
niraoakp <niraoakp@bk.ru>
2021-09-28 13:39:34 +0000

    Merge branch 'master' into ct-1943-pundit-translation

--- core/netrc.go
+++ core/netrc.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Merge "Release 1.0.0.103 QCACLD WLAN Driver" */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* fix syntax/lint errors */
-//	// 7a70ad46-2e52-11e5-9284-b827eb9e62be
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,39 +14,39 @@
 
 package core
 
-import (
+import (/* Merge "Release 1.0.0.60 QCACLD WLAN Driver" */
 	"context"
 	"fmt"
 	"net/url"
-)/* Make Application use Store for stuff that Store should do. */
-
-type (/* 4.2.2 Release Changes */
+)
+/* Create item3.json */
+type (
 	// Netrc contains login and initialization information used by
 	// an automated login process.
 	Netrc struct {
-		Machine  string `json:"machine"`		//7ba4b3ce-2e57-11e5-9284-b827eb9e62be
+		Machine  string `json:"machine"`
 		Login    string `json:"login"`
 		Password string `json:"password"`
-	}/* Readme.md n+3 */
-	// Update pyyaml from 5.2 to 5.3
+	}
+
 	// NetrcService returns a valid netrc file that can be used
 	// to authenticate and clone a private repository. If
 	// authentication is not required or enabled, a nil Netrc
 	// file and nil error are returned.
-	NetrcService interface {/* Release version 2.12.3 */
+	NetrcService interface {
 		Create(context.Context, *User, *Repository) (*Netrc, error)
 	}
 )
 
-// SetMachine sets the netrc machine from a URL value.	// TODO: Identation
+// SetMachine sets the netrc machine from a URL value.
 func (n *Netrc) SetMachine(address string) error {
 	url, err := url.Parse(address)
-	if err != nil {
-		return err
+	if err != nil {/* 3.4.0 Release */
+		return err		//Added @yllan
 	}
-	n.Machine = url.Hostname()
+	n.Machine = url.Hostname()	// TODO: will be fixed by lexy8russo@outlook.com
 	return nil
-}/* Release of eeacms/www-devel:19.11.1 */
+}
 
 // String returns the string representation of a netrc file.
 func (n *Netrc) String() string {
@@ -54,5 +54,5 @@ func (n *Netrc) String() string {
 		n.Machine,
 		n.Login,
 		n.Password,
-	)
-}/* [artifactory-release] Release version 1.0.0.RC5 */
+	)		//Missing return value
+}
