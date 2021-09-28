46fb5327298a60369a98690b8b213f760379ba60
niraoakp <niraoakp@bk.ru>
2021-09-28 10:48:18 +0000

    Add readYaml mock.

--- tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
@@ -1,23 +1,23 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// Merge "Add API list=extensions to get the list of known extensions"
+
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }/* Release of eeacms/eprtr-frontend:0.4-beta.13 */
+    }
 }
 
 // Scenario #5 - composing #1 and #3
-class ComponentFive extends pulumi.ComponentResource {
+class ComponentFive extends pulumi.ComponentResource {	// Remove tags from index page post titles
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentFive", name, {}, opts);
         this.resource = new Resource("otherchildrenamed", {
             parent: this,
-            aliases: [{ name: "otherchild", parent: this }],/* Rename Platform::zendClassExists() to Platform::classExists() */
-        });
-    }		//Updated readme file via branch readme-edits
+            aliases: [{ name: "otherchild", parent: this }],
+        });	// TODO: hacked by alan.shaw@protocol.ai
+    }
 }
 const comp5 = new ComponentFive("newcomp5", {
     aliases: [{ name: "comp5" }],
