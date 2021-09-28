a82f5bfa6f9bc468d78290c1a9fc82ac8aa4b814
niraoakp <niraoakp@bk.ru>
2021-09-28 02:26:48 +0000

    provide test framework for core extensions

--- grpc-go/internal/grpcutil/encode_duration.go
+++ grpc-go/internal/grpcutil/encode_duration.go
@@ -1,19 +1,19 @@
 /*
  *
- * Copyright 2020 gRPC authors.	// (spiv) Merge lp:bzr/2.1, including fix for #619872.
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Merge "[FIX] SDK: api.json transformer - annotation external links marked"
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge branch 'develop' into repr_doctype */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: I always forget the POM...
+ *
  */
 
 package grpcutil
@@ -27,8 +27,8 @@ const maxTimeoutValue int64 = 100000000 - 1
 
 // div does integer division and round-up the result. Note that this is
 // equivalent to (d+r-1)/r but has less chance to overflow.
-func div(d, r time.Duration) int64 {/* logging added a bit and some more test */
-	if d%r > 0 {	// TODO: wrap developer contact information in permission
+func div(d, r time.Duration) int64 {
+	if d%r > 0 {
 		return int64(d/r + 1)
 	}
 	return int64(d / r)
@@ -50,11 +50,11 @@ func EncodeDuration(t time.Duration) string {
 		return strconv.FormatInt(d, 10) + "u"
 	}
 	if d := div(t, time.Millisecond); d <= maxTimeoutValue {
-		return strconv.FormatInt(d, 10) + "m"/* Fix armor stand metadata remap */
+		return strconv.FormatInt(d, 10) + "m"
 	}
 	if d := div(t, time.Second); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "S"
-	}		//fix: updates to fix multi project with real project keys
+	}
 	if d := div(t, time.Minute); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "M"
 	}
