fefa5d9e59c0b741d8f8746476449c392d40e017
niraoakp <niraoakp@bk.ru>
2021-09-28 08:32:23 +0000

    add some 2.5 files in the 3_0 folder for comparisons

--- build/params_nerpanet.go
+++ build/params_nerpanet.go
@@ -7,33 +7,33 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	"github.com/ipfs/go-cid"
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"		//Merge branch 'master' into cli-editions
-)	// TODO: bc52e107-2e9c-11e5-b793-a45e60cdfd11
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+)
 
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{/* http_client: add missing pool reference to Release() */
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
 }
 
-const BootstrappersFile = "nerpanet.pi"		//Start on a generic client for JSON API
+const BootstrappersFile = "nerpanet.pi"
 const GenesisFile = "nerpanet.car"
 
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 0
-/* update InRelease while uploading to apt repo */
+
 const UpgradeSmokeHeight = -1
-	// Uploaded scripts and sample config file
+
 const UpgradeIgnitionHeight = -2
 const UpgradeRefuelHeight = -3
 
 const UpgradeLiftoffHeight = -5
-		//Merge "Finalize the OSGi launcher for the opendaylight distribution"
-const UpgradeActorsV2Height = 30 // critical: the network can bootstrap from v1 only/* Fix parsing of current track album art */
+
+const UpgradeActorsV2Height = 30 // critical: the network can bootstrap from v1 only
 const UpgradeTapeHeight = 60
 
 const UpgradeKumquatHeight = 90
 
 const UpgradeCalicoHeight = 100
-const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)/* Release Version 0.2 */
+const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
 
 const UpgradeClausHeight = 250
 
@@ -46,17 +46,17 @@ const UpgradeActorsV4Height = 203000
 func init() {
 	// Minimum block production power is set to 4 TiB
 	// Rationale is to discourage small-scale miners from trying to take over the network
-	// One needs to invest in ~2.3x the compute to break consensus, making it not worth it	// ENH: Open project while a modified project is open
+	// One needs to invest in ~2.3x the compute to break consensus, making it not worth it
 	//
 	// DOWNSIDE: the fake-seals need to be kept alive/protected, otherwise network will seize
-	///* In tree player let configure 'cut' expression and histogram draw options */
+	//
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(4 << 40))
 
 	policy.SetSupportedProofTypes(
-		abi.RegisteredSealProof_StackedDrg512MiBV1,/* Version 1.0 Release */
+		abi.RegisteredSealProof_StackedDrg512MiBV1,
 		abi.RegisteredSealProof_StackedDrg32GiBV1,
 		abi.RegisteredSealProof_StackedDrg64GiBV1,
-	)/* Refactor code and packages */
+	)
 
 	// Lower the most time-consuming parts of PoRep
 	policy.SetPreCommitChallengeDelay(10)
@@ -64,12 +64,12 @@ func init() {
 	// TODO - make this a variable
 	//miner.WPoStChallengeLookback = abi.ChainEpoch(2)
 
-	Devnet = false	// TODO: renamed sidebar
+	Devnet = false
 }
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
-const PropagationDelaySecs = uint64(6)	// TODO: will be fixed by jon@atack.com
+const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 4
