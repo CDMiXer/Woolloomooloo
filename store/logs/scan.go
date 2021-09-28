1a7b5c9f5a8c35e5df198d249287028f3f2319de
niraoakp <niraoakp@bk.ru>
2021-09-28 01:09:26 +0000

    Update updateManager.py

--- store/logs/scan.go
+++ store/logs/scan.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
-///* Release 3.0.0 - update changelog */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by mail@bitpshr.net
+// you may not use this file except in compliance with the License.	// Delete ReSampler-clang
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by mail@bitpshr.net
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package logs	// Improved matrix speed
+package logs/* Release notes 7.1.11 */
 
-import "github.com/drone/drone/store/shared/db"/* Release of eeacms/www:20.11.18 */
+import "github.com/drone/drone/store/shared/db"
 
 // helper function scans the sql.Row and copies the column
-// values to the destination object.		//Marcando como pagada la Transacción en el CallBack.
-func scanRow(scanner db.Scanner, dst *logs) error {		//Merge branch 'hotfix/19.8.2'
-	return scanner.Scan(		//Use URL params instead og localstorage in browser
-		&dst.ID,	// TODO: hacked by lexy8russo@outlook.com
-		&dst.Data,		//Bumped version up to 1.9 .
+// values to the destination object./* weitere Anpassungen */
+func scanRow(scanner db.Scanner, dst *logs) error {
+	return scanner.Scan(
+		&dst.ID,
+		&dst.Data,
 	)
 }
