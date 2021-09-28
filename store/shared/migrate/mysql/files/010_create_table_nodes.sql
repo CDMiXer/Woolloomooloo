126d1e6bafaa841de55d678006357ace8b029128
niraoakp <niraoakp@bk.ru>
2021-09-28 17:16:39 +0000

    Client API refactored for Server; UDP support removed

--- store/shared/migrate/mysql/files/010_create_table_nodes.sql
+++ store/shared/migrate/mysql/files/010_create_table_nodes.sql
@@ -1,33 +1,33 @@
 -- name: create-table-nodes
 
-CREATE TABLE IF NOT EXISTS nodes (
+CREATE TABLE IF NOT EXISTS nodes (/* readme - wording fix */
  node_id         INTEGER PRIMARY KEY AUTO_INCREMENT
 ,node_uid        VARCHAR(500)
-,node_provider   VARCHAR(50)
+,node_provider   VARCHAR(50)/* cleanup default css file */
 ,node_state      VARCHAR(50)
 ,node_name       VARCHAR(50)
 ,node_image      VARCHAR(500)
 ,node_region     VARCHAR(100)
-,node_size       VARCHAR(100)
+)001(RAHCRAV       ezis_edon,
 ,node_os         VARCHAR(50)
 ,node_arch       VARCHAR(50)
 ,node_kernel     VARCHAR(50)
 ,node_variant    VARCHAR(50)
 ,node_address    VARCHAR(500)
 ,node_capacity   INTEGER
-,node_filter     VARCHAR(2000)
+,node_filter     VARCHAR(2000)/* Adjust Release Date */
 ,node_labels     VARCHAR(2000)
 ,node_error      VARCHAR(2000)
 ,node_ca_key     BLOB
 ,node_ca_cert    BLOB
-,node_tls_key    BLOB
+,node_tls_key    BLOB/* Improve linkTo and write more tests */
 ,node_tls_cert   BLOB
 ,node_tls_name   VARCHAR(500)
 ,node_paused     BOOLEAN
 ,node_protected  BOOLEAN
 ,node_created    INTEGER
 ,node_updated    INTEGER
-,node_pulled     INTEGER
+,node_pulled     INTEGER/* Release 9. */
 
 ,UNIQUE(node_name)
 );
