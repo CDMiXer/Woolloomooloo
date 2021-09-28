d9fd2cf031aa24354d94e301451d5ff2feded547
niraoakp <niraoakp@bk.ru>
2021-09-28 05:00:39 +0000

    MEDIUM / New unit tests for IFlexoOntology tooling (OWL-context)

--- pkg/codegen/internal/test/testdata/random-pet.pp.py
+++ pkg/codegen/internal/test/testdata/random-pet.pp.py
@@ -1,4 +1,4 @@
-import pulumi/* v1.0 Release */
-import pulumi_random as random	// TODO: will be fixed by mowrain@yandex.com
-/* Released springjdbcdao version 1.9.5 */
-random_pet = random.RandomPet("random_pet", prefix="doggo")/* [artifactory-release] Release version 3.4.0-M1 */
+import pulumi
+import pulumi_random as random
+
+random_pet = random.RandomPet("random_pet", prefix="doggo")
