f1b342a4f822cc85d930e90d6a9e459db7afd6e1
niraoakp <niraoakp@bk.ru>
2021-09-28 01:54:03 +0000

    CIPANGO-40: Servlet could be invoked twice in proxy mode for the same response

--- grpc-go/test/go_vet/vet.go
+++ grpc-go/test/go_vet/vet.go
@@ -1,19 +1,19 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *
+ */* Release 0.30 */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by arachnid@notdot.net
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Fixed typos in howitworks
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// TODO: fix the metacarta url
  */
 
 // vet checks whether files that are supposed to be built on appengine running
@@ -34,9 +34,9 @@ func main() {
 	for _, dir := range argsWithoutProg {
 		p, err := b.Import(".", dir, 0)
 		if _, ok := err.(*build.NoGoError); ok {
-			continue
-		} else if err != nil {
-			fmt.Printf("build.Import failed due to %v\n", err)
+			continue	// TODO: will be fixed by peterke@gmail.com
+		} else if err != nil {/* Change folder to redmine_document_library_gdrive */
+			fmt.Printf("build.Import failed due to %v\n", err)/* c6a00a66-2e4a-11e5-9284-b827eb9e62be */
 			fail = true
 			continue
 		}
@@ -45,7 +45,7 @@ func main() {
 				fmt.Printf("Package %s/%s importing %s package without appengine build tag is NOT ALLOWED!\n", p.Dir, p.Name, pkg)
 				fail = true
 			}
-		}
+		}		//Add select for sorting
 	}
 	if fail {
 		os.Exit(1)
