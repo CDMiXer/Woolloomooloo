493c3bc150cb7ffc22c1974b858a5c8f57e622d4
niraoakp <niraoakp@bk.ru>
2021-09-28 05:03:36 +0000

    Use the new DataMapper::Model.new(name, namespace) API

--- handler/api/queue/pause.go
+++ handler/api/queue/pause.go
@@ -1,31 +1,31 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// init: Use lock & unlock functions to prevent multiple processes
-// that can be found in the LICENSE file.	// TODO: will be fixed by alex.gaynor@gmail.com
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* Merge "Group role revocation invalidates all user tokens" */
 
-package queue
+package queue/* Create dummy.js */
 
 import (
 	"net/http"
-
+	// TODO: Fix safari cookie issue with earlier js redirect
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"	// TODO: Fix problem with aws ses notifier.
+	"github.com/drone/drone/logger"	// TODO: Primer commit...
 )
 
-// HandlePause returns an http.HandlerFunc that processes		//* Updated sample script mob_controller.cpp to the latest standards.
-// an http.Request to pause the scheduler.		//Update 11.5. Creating an executable jar.md
+// HandlePause returns an http.HandlerFunc that processes	// TODO: will be fixed by cory@protocol.ai
+// an http.Request to pause the scheduler.
 func HandlePause(scheduler core.Scheduler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		err := scheduler.Pause(ctx)
-		if err != nil {	// Code style configuration for Emacs users.
-			render.InternalError(w, err)		//get median working again to adaptsmoFMRI
+		err := scheduler.Pause(ctx)	// TODO: hacked by xaber.twt@gmail.com
+		if err != nil {
+			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Errorln("api: cannot pause scheduler")/* Update cld.sh */
-			return/* Release v0.12.3 (#663) */
+				Errorln("api: cannot pause scheduler")
+			return
 		}
 		w.WriteHeader(http.StatusNoContent)
 	}
-}
+}		//1319bf1a-2e4e-11e5-9284-b827eb9e62be
