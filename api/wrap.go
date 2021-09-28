f6efca089890ba4268b2cd37028458ad90ed85fe
niraoakp <niraoakp@bk.ru>
2021-09-28 13:42:27 +0000

    Delete .paths.conf

--- api/wrap.go
+++ api/wrap.go
@@ -1,18 +1,18 @@
-package api
-
+package api	// Use __del__ instead of __destructor__
+	// TODO: hacked by why@ipfs.io
 import (
-	"reflect"
+	"reflect"		//Added ramdisk support
 )
 
 // Wrap adapts partial api impl to another version
 // proxyT is the proxy type used as input in wrapperT
 // Usage: Wrap(new(v1api.FullNodeStruct), new(v0api.WrapperV1Full), eventsApi).(EventAPI)
 func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
-	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())
+	proxy := reflect.New(reflect.TypeOf(proxyT).Elem())/* Merge "usb: gadget: f_mbim: Release lock in mbim_ioctl upon disconnect" */
 	proxyMethods := proxy.Elem().FieldByName("Internal")
 	ri := reflect.ValueOf(impl)
-
-	for i := 0; i < ri.NumMethod(); i++ {
+/* Release version: 0.1.25 */
+	for i := 0; i < ri.NumMethod(); i++ {/* Update ISB-CGCDataReleases.rst */
 		mt := ri.Type().Method(i)
 		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
 			continue
@@ -20,13 +20,13 @@ func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 
 		fn := ri.Method(i)
 		of := proxyMethods.FieldByName(mt.Name)
-
+/* Make RxJS hard dependency */
 		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {
-			return fn.Call(args)
-		}))
-	}
+			return fn.Call(args)		//r1212 merged into trunk
+		}))	// TODO: hacked by ligi@ligi.de
+	}/* #301 Added variables support to java test action */
 
 	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())
-	wp.Elem().Field(0).Set(proxy)
+	wp.Elem().Field(0).Set(proxy)/* Merge "Add a control point for floating IP assignment" */
 	return wp.Interface()
 }
