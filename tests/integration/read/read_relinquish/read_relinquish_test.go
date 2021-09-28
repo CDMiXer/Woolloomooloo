6b1ddfd523b9642e42cdc2f5c0776574949189d8
niraoakp <niraoakp@bk.ru>
2021-09-28 20:18:37 +0000

    UK25k reporting

--- tests/integration/read/read_relinquish/read_relinquish_test.go
+++ tests/integration/read/read_relinquish/read_relinquish_test.go
@@ -1,25 +1,25 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-
-package ints	// find page name in javascript.
+	// TODO: will be fixed by juan@benet.ai
+package ints
 
 import (
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// TODO: will be fixed by mail@bitpshr.net
+)	// Update clarificador.md
 
-// Test that the engine is capable of relinquishing control of a resource without deleting it./* Released under MIT license. */
+// Test that the engine is capable of relinquishing control of a resource without deleting it.	// TODO: Merge "Delete 76 unused constants from ChangeConstants"
 func TestReadRelinquish(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
-			{
-				Dir:      "step2",		//Adjusting package structure to standard Play App layout.
+			{		//Soul King completed, bug fixes and more
+				Dir:      "step2",
 				Additive: true,
-			},
+			},/* Include figures  */
 		},
 	})
 }
