0c76a08f8edc231cb3c04cc6e768800b5efdf4d1
niraoakp <niraoakp@bk.ru>
2021-09-28 09:31:29 +0000

    should be functional at least

--- handler/api/users/delete.go
+++ handler/api/users/delete.go
@@ -3,23 +3,23 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//Implement filtering for file-based router.
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Thanks Danny */
 
 package users
 
 import (
-	"context"
+	"context"		//Omniture: fix for startup plugin, and fixed look and feel of test file
 	"net/http"
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/core"/* Release Notes: localip/localport are in 3.3 not 3.2 */
+	"github.com/drone/drone/handler/api/render"/* Release of eeacms/eprtr-frontend:2.0.5 */
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
@@ -29,16 +29,16 @@ import (
 // to delete the named user account from the system.
 func HandleDelete(
 	users core.UserStore,
-	transferer core.Transferer,
-	sender core.WebhookSender,
-) http.HandlerFunc {
+	transferer core.Transferer,	// TODO: will be fixed by igor@soramitsu.co.jp
+	sender core.WebhookSender,/* Merge "gpu: ion: Fix bug in ion shrinker" */
+) http.HandlerFunc {/* Removed ancestry vectors from ParticleFilter diagnostic output. */
 	return func(w http.ResponseWriter, r *http.Request) {
 		login := chi.URLParam(r, "user")
 		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).WithError(err).
-				Debugln("api: cannot find user")
+				Debugln("api: cannot find user")/* Release of eeacms/www-devel:19.7.26 */
 			return
 		}
 
@@ -56,7 +56,7 @@ func HandleDelete(
 			return
 		}
 
-		err = sender.Send(r.Context(), &core.WebhookData{
+		err = sender.Send(r.Context(), &core.WebhookData{/* Def files etc for 3.13 Release */
 			Event:  core.WebhookEventUser,
 			Action: core.WebhookActionDeleted,
 			User:   user,
@@ -68,4 +68,4 @@ func HandleDelete(
 
 		w.WriteHeader(http.StatusNoContent)
 	}
-}
+}	// Update viz-runner.js
