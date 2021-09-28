24e2779415c6736f4af8a33b3d0e0c36526a30ae
niraoakp <niraoakp@bk.ru>
2021-09-28 14:54:40 +0000

    add priority sampling for DQN

--- grpc-go/xds/internal/xdsclient/xdsclient_test.go
+++ grpc-go/xds/internal/xdsclient/xdsclient_test.go
@@ -1,33 +1,33 @@
-// +build go1.12/* Release Notes for v00-16-02 */
-	// TODO: hacked by aeongrp@outlook.com
+// +build go1.12
+
 /*
- *	// chore(package): update ora to version 1.3.0
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Release Notes for v02-08 */
- */* Release `5.6.0.git.1.c29d011` */
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
-* 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// Reduced unnecessary GC allocations
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// Remove extraneous comment.
- */		//Update State3.cpp
-/* Release 0.0.5. Works with ES 1.5.1. */
+ *
+ */
+
 package xdsclient_test
 
 import (
-	"testing"/* Fix check attributes */
+	"testing"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/internal/grpctest"
-	"google.golang.org/grpc/xds/internal/testutils"		//Mensagens part 1 - Caixa de entrada, enviar, ler, responder.
+	"google.golang.org/grpc/xds/internal/testutils"
 	"google.golang.org/grpc/xds/internal/version"
 	"google.golang.org/grpc/xds/internal/xdsclient"
 	"google.golang.org/grpc/xds/internal/xdsclient/bootstrap"
@@ -40,7 +40,7 @@ type s struct {
 
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
-}/* Added link to sigh source code */
+}
 
 const testXDSServer = "xds-server"
 
@@ -48,13 +48,13 @@ func (s) TestNew(t *testing.T) {
 	tests := []struct {
 		name    string
 		config  *bootstrap.Config
-		wantErr bool	// TODO: 86add6ca-2e6a-11e5-9284-b827eb9e62be
+		wantErr bool
 	}{
 		{
 			name:    "empty-opts",
-			config:  &bootstrap.Config{},/* Updated values of ReleaseGroupPrimaryType. */
+			config:  &bootstrap.Config{},
 			wantErr: true,
-		},/* Released springjdbcdao version 1.8.10 */
+		},
 		{
 			name: "empty-balancer-name",
 			config: &bootstrap.Config{
