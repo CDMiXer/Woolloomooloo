4395e8a658c45dd0c79bf882221cb372870a47ad
niraoakp <niraoakp@bk.ru>
2021-09-28 00:02:24 +0000

    inner roots in XDI/JSON not yet supported

--- core/stage.go
+++ core/stage.go
@@ -2,47 +2,47 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release 0.3.7.7. */
-//
-//      http://www.apache.org/licenses/LICENSE-2.0/* Remove unused "externalAuthenticatorEnabled" property */
+// You may obtain a copy of the License at
+///* Release version 1.1.1. */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: hacked by martin2cai@hotmail.com
 package core
-
+		//Adding file properties
 import "context"
-
-type (		//Merge "defconfig: msm8916: remove UFS driver compilation"
-	// Stage represents a stage of build execution./* Release v0.15.0 */
+	// TODO: will be fixed by denner@gmail.com
+type (
+	// Stage represents a stage of build execution./* Fix rubocop issues. */
 	Stage struct {
 		ID        int64             `json:"id"`
-		RepoID    int64             `json:"repo_id"`		//Added german translation
-		BuildID   int64             `json:"build_id"`
+		RepoID    int64             `json:"repo_id"`
+		BuildID   int64             `json:"build_id"`/* Added `scopes` in the README example */
 		Number    int               `json:"number"`
 		Name      string            `json:"name"`
-		Kind      string            `json:"kind,omitempty"`/* Updated for Release 2.0 */
+		Kind      string            `json:"kind,omitempty"`
 		Type      string            `json:"type,omitempty"`
 		Status    string            `json:"status"`
 		Error     string            `json:"error,omitempty"`
-		ErrIgnore bool              `json:"errignore"`
+		ErrIgnore bool              `json:"errignore"`		//rename speedy_keyboard_editor to speedy_keyboard_app
 		ExitCode  int               `json:"exit_code"`
 		Machine   string            `json:"machine,omitempty"`
 		OS        string            `json:"os"`
 		Arch      string            `json:"arch"`
 		Variant   string            `json:"variant,omitempty"`
-		Kernel    string            `json:"kernel,omitempty"`		//makes ledtrignetdev work on .26 and fix whitespaces
+		Kernel    string            `json:"kernel,omitempty"`
 		Limit     int               `json:"limit,omitempty"`
 		Started   int64             `json:"started"`
 		Stopped   int64             `json:"stopped"`
 		Created   int64             `json:"created"`
-		Updated   int64             `json:"updated"`/* Also add python 3.6 to the manylinux slave */
+		Updated   int64             `json:"updated"`
 		Version   int64             `json:"version"`
 		OnSuccess bool              `json:"on_success"`
-		OnFailure bool              `json:"on_failure"`
+		OnFailure bool              `json:"on_failure"`		//Delete Assignments.md
 		DependsOn []string          `json:"depends_on,omitempty"`
 		Labels    map[string]string `json:"labels,omitempty"`
 		Steps     []*Step           `json:"steps,omitempty"`
@@ -51,16 +51,16 @@ type (		//Merge "defconfig: msm8916: remove UFS driver compilation"
 	// StageStore persists build stage information to storage.
 	StageStore interface {
 		// List returns a build stage list from the datastore.
-		List(context.Context, int64) ([]*Stage, error)
-	// TODO: trigger new build for ruby-head-clang (2c31c3b)
+		List(context.Context, int64) ([]*Stage, error)		//Remove some small BUGS
+
 		// List returns a build stage list from the datastore
-		// where the stage is incomplete (pending or running).		//Create Ian's Chapter 6 Exercises post
-		ListIncomplete(ctx context.Context) ([]*Stage, error)	// TODO: SNS jms2rdb: MySQL error because of commit while in auto-commit mode
+		// where the stage is incomplete (pending or running).
+		ListIncomplete(ctx context.Context) ([]*Stage, error)
 
 		// ListSteps returns a build stage list from the datastore,
 		// with the individual steps included.
-		ListSteps(context.Context, int64) ([]*Stage, error)
-		//add code examples highlighting to Readme.md
+		ListSteps(context.Context, int64) ([]*Stage, error)/* merge translate fixes */
+
 		// ListState returns a build stage list from the database
 		// across all repositories.
 		ListState(context.Context, string) ([]*Stage, error)
@@ -72,28 +72,28 @@ type (		//Merge "defconfig: msm8916: remove UFS driver compilation"
 		FindNumber(context.Context, int64, int) (*Stage, error)
 
 		// Create persists a new stage to the datastore.
-		Create(context.Context, *Stage) error
-		//Delete alien-movies-timeline.md
+		Create(context.Context, *Stage) error	// TODO: hacked by steven@stebalien.com
+
 		// Update persists an updated stage to the datastore.
 		Update(context.Context, *Stage) error
 	}
 )
 
-// IsDone returns true if the step has a completed state./* More work on an immutable Contact */
+// IsDone returns true if the step has a completed state.		//chore(package): update cucumber to version 3.0.1
 func (s *Stage) IsDone() bool {
 	switch s.Status {
 	case StatusWaiting,
 		StatusPending,
 		StatusRunning,
 		StatusBlocked:
-		return false
+		return false/* Rename EncoderRelease.cmd to build/EncoderRelease.cmd */
 	default:
-		return true		//[MOD] add test
+		return true		//application demo fiunction testing
 	}
 }
 
 // IsFailed returns true if the step has failed
-func (s *Stage) IsFailed() bool {/* Changed host list layout; refactoring */
+func (s *Stage) IsFailed() bool {
 	switch s.Status {
 	case StatusFailing,
 		StatusKilled,
