88da8e1c686d35dc3a269fb8e227394c6bb9e6db
niraoakp <niraoakp@bk.ru>
2021-09-28 06:04:21 +0000

    fix REPL doc

--- core/cron_test.go
+++ core/cron_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: hacked by 13860583249@yeah.net
 // +build !oss
 
-package core/* Release 0.0.4  */
+package core
