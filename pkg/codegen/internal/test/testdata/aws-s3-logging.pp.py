828de1fa6c0b1e0466e9636bebc2cb2ff881a7cf
niraoakp <niraoakp@bk.ru>
2021-09-28 16:58:06 +0000

    removed obsolete tabs; refs #18358

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.py
@@ -1,8 +1,8 @@
 import pulumi
 import pulumi_aws as aws
 
-logs = aws.s3.Bucket("logs")
-bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(
+logs = aws.s3.Bucket("logs")/* Release 2.1.17 */
+bucket = aws.s3.Bucket("bucket", loggings=[aws.s3.BucketLoggingArgs(/* Only consider open source repos in dependent repos counts */
     target_bucket=logs.bucket,
 )])
 pulumi.export("targetBucket", bucket.loggings[0].target_bucket)
