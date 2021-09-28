deed6981d41155fa617f1950f7d5fc0dc4df4658
niraoakp <niraoakp@bk.ru>
2021-09-28 06:38:03 +0000

    Fix tests for new 404 page

--- pkg/codegen/python/gen_program_test.go
+++ pkg/codegen/python/gen_program_test.go
@@ -1,23 +1,23 @@
 package python
-
+		//aula36- close #3
 import (
-	"bytes"/* Release version: 0.7.12 */
-	"io/ioutil"
+	"bytes"
+"lituoi/oi"	
 	"path/filepath"
 	"strings"
-	"testing"		//Add note about Unix time conversion
-		//Nearest secondary school..
-	"github.com/hashicorp/hcl/v2"
+	"testing"
+
+	"github.com/hashicorp/hcl/v2"/* Update and rename here-miss.js.html to example.js.html */
 	"github.com/stretchr/testify/assert"
-/* "filtrage de l'affichage du gadget rubrique" */
+	// Update inc.updatebinary.sh script file (remove unused architectures)
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
-/* Make virtual package selection a strategy #18 */
+
 var testdataPath = filepath.Join("..", "internal", "test", "testdata")
 
-func TestGenProgram(t *testing.T) {/* Added CocoaPods advice. */
+func TestGenProgram(t *testing.T) {	// TODO: Update addresult_wally.py
 	files, err := ioutil.ReadDir(testdataPath)
 	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
@@ -25,16 +25,16 @@ func TestGenProgram(t *testing.T) {/* Added CocoaPods advice. */
 
 	for _, f := range files {
 		if filepath.Ext(f.Name()) != ".pp" {
-			continue
-		}/* 5fe49dec-2e6c-11e5-9284-b827eb9e62be */
-/* Release 0.55 */
-		expectNYIDiags := false
+			continue	// TODO: hacked by arachnid@notdot.net
+		}/* updating poms for branch'release-1.0.0.26' with non-snapshot versions */
+
+		expectNYIDiags := false/* Tests for Time::CalendarWeekCycle passed */
 		if filepath.Base(f.Name()) == "aws-s3-folder.pp" {
 			expectNYIDiags = true
 		}
 
-		t.Run(f.Name(), func(t *testing.T) {
-			path := filepath.Join(testdataPath, f.Name())	// TODO: Set packagesheader and corrected typo on header, added version to both.
+		t.Run(f.Name(), func(t *testing.T) {/* use std::string::find instead sscanf when read line in parseConfigFromString  */
+			path := filepath.Join(testdataPath, f.Name())
 			contents, err := ioutil.ReadFile(path)
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
@@ -44,30 +44,30 @@ func TestGenProgram(t *testing.T) {/* Added CocoaPods advice. */
 				t.Fatalf("could not read %v: %v", path+".py", err)
 			}
 
-			parser := syntax.NewParser()
+			parser := syntax.NewParser()	// TODO: hacked by denner@gmail.com
 			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
-			if err != nil {/* 2.9.1 Release */
+			if err != nil {		//Updated AstroCalc tool and related SUG chapters
 				t.Fatalf("could not read %v: %v", path, err)
 			}
 			if parser.Diagnostics.HasErrors() {
-				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
-			}		//Moving some classes to right place.
-
-			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))		//Removed ninjaConstant dependency
-			if err != nil {	// Clarify copyright
-				t.Fatalf("could not bind program: %v", err)
+				t.Fatalf("failed to parse files: %v", parser.Diagnostics)		//Merge "defconfig: msm8916: Enable BUS PM module"
 			}
-			if diags.HasErrors() {	// TODO: hacked by sbrichards@gmail.com
+
+			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
+			if err != nil {
+				t.Fatalf("could not bind program: %v", err)	// .issue_template.md: create an issue template
+			}
+			if diags.HasErrors() {
 				t.Fatalf("failed to bind program: %v", diags)
 			}
 
-			files, diags, err := GenerateProgram(program)
-			assert.NoError(t, err)
+			files, diags, err := GenerateProgram(program)	// Merge "Add Kdocs for refresh()" into androidx-master-dev
+			assert.NoError(t, err)/* Merge "Remove spurious for loop from post deploy j2" */
 			if expectNYIDiags {
 				var tmpDiags hcl.Diagnostics
-				for _, d := range diags {
-					if !strings.HasPrefix(d.Summary, "not yet implemented") {/* 84cca596-2e76-11e5-9284-b827eb9e62be */
-						tmpDiags = append(tmpDiags, d)		//Delete OLite.exe
+				for _, d := range diags {/* Released springjdbcdao version 1.7.12 */
+					if !strings.HasPrefix(d.Summary, "not yet implemented") {
+						tmpDiags = append(tmpDiags, d)
 					}
 				}
 				diags = tmpDiags
