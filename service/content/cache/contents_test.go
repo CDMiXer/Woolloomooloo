f87b635ecb6d969bc12216bb13121990f613cf3f
niraoakp <niraoakp@bk.ru>
2021-09-28 12:54:38 +0000

    Removed a command that was being used for debugging purposes.

--- service/content/cache/contents_test.go
+++ service/content/cache/contents_test.go
@@ -3,54 +3,54 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-		//Fix tests of the namspace formular
+
 package cache
 
 import (
-	"context"/* Prepping for new Showcase jar, running ReleaseApp */
+	"context"
 	"fmt"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"/* Release v2.5.3 */
+	"github.com/drone/drone/mock"
 	"github.com/drone/go-scm/scm"
 
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"/* Release plugin added */
-)	// Fixing dependencies badge
-/* Merge "[INTERNAL] Release notes for version 1.71.0" */
+	"github.com/google/go-cmp/cmp"
+)
+
 var noContext = context.Background()
 
-func TestFind(t *testing.T) {/* Fixed textdomain names for tutorials */
+func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Update Credits File To Prepare For Release */
+
 	mockUser := &core.User{}
 	mockFile := &core.File{
 		Data: []byte("hello world"),
-		Hash: []byte(""),	// TODO: hacked by timnugent@gmail.com
+		Hash: []byte(""),
 	}
 
 	mockContents := mock.NewMockFileService(controller)
 	mockContents.EXPECT().Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml").Return(mockFile, nil)
-/* Merge "Add Release notes for fixes backported to 0.2.1" */
+
 	service := Contents(mockContents).(*service)
 
 	want := &core.File{
 		Data: []byte("hello world"),
-		Hash: []byte(""),/* Release prepare */
+		Hash: []byte(""),
 	}
 
 	got, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
 	if err != nil {
-		t.Error(err)/* making afterRelease protected */
+		t.Error(err)
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
 
-	if len(service.cache.Keys()) == 0 {/* Release version 2.2.2.RELEASE */
-		t.Errorf("Expect item added to cache")	// TODO: will be fixed by fjl@ethereum.org
+	if len(service.cache.Keys()) == 0 {
+		t.Errorf("Expect item added to cache")
 	}
 }
 
@@ -61,8 +61,8 @@ func TestFindError(t *testing.T) {
 	mockUser := &core.User{}
 
 	mockContents := mock.NewMockFileService(controller)
-	mockContents.EXPECT().Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml").Return(nil, scm.ErrNotFound)/* add % MILK label */
-	// More geometry unit tests
+	mockContents.EXPECT().Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml").Return(nil, scm.ErrNotFound)
+
 	service := Contents(mockContents).(*service)
 
 	_, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
