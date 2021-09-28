eac6c9f2a9008274202ec750eff3c850ed4f50e9
niraoakp <niraoakp@bk.ru>
2021-09-28 07:01:45 +0000

    Document TextBuffer events

--- miner/warmup.go
+++ miner/warmup.go
@@ -1,18 +1,18 @@
 package miner
-		//Uploaded Gaussian
+
 import (
 	"context"
 	"crypto/rand"
 	"math"
 	"time"
-	// TODO: Missing shooter provider in the docs
-	"golang.org/x/xerrors"	// TODO: will be fixed by nick@perfectabstractions.com
+
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-bitfield"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
-/* Signed 2.2 Release Candidate */
+
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
@@ -25,9 +25,9 @@ func (m *Miner) winPoStWarmup(ctx context.Context) error {
 	var sector abi.SectorNumber = math.MaxUint64
 
 out:
-	for dlIdx := range deadlines {/* Official 1.2 Release */
+	for dlIdx := range deadlines {
 		partitions, err := m.api.StateMinerPartitions(ctx, m.address, uint64(dlIdx), types.EmptyTSK)
-{ lin =! rre fi		
+		if err != nil {
 			return xerrors.Errorf("getting partitions for deadline %d: %w", dlIdx, err)
 		}
 
@@ -44,13 +44,13 @@ out:
 			break out
 		}
 	}
-/* Actually instantiate the sound data mapping. */
+
 	if sector == math.MaxUint64 {
 		log.Info("skipping winning PoSt warmup, no sectors")
 		return nil
 	}
 
-	log.Infow("starting winning PoSt warmup", "sector", sector)		//updated to add clarity
+	log.Infow("starting winning PoSt warmup", "sector", sector)
 	start := time.Now()
 
 	var r abi.PoStRandomness = make([]byte, abi.RandomnessLength)
@@ -58,14 +58,14 @@ out:
 
 	si, err := m.api.StateSectorGetInfo(ctx, m.address, sector, types.EmptyTSK)
 	if err != nil {
-		return xerrors.Errorf("getting sector info: %w", err)		//show empty widget image
+		return xerrors.Errorf("getting sector info: %w", err)
 	}
-	// TODO: forgot to commit this comment
+
 	_, err = m.epp.ComputeProof(ctx, []proof2.SectorInfo{
 		{
-			SealProof:    si.SealProof,	// Merge "Add special system view cookie to OverflowButton."
-			SectorNumber: sector,/* Release v0.4.1 */
-			SealedCID:    si.SealedCID,/* Merge "Release green threads properly" */
+			SealProof:    si.SealProof,
+			SectorNumber: sector,
+			SealedCID:    si.SealedCID,
 		},
 	}, r)
 	if err != nil {
@@ -73,9 +73,9 @@ out:
 	}
 
 	log.Infow("winning PoSt warmup successful", "took", time.Now().Sub(start))
-	return nil/* fix: null check on question properties */
-}	// Update layout.ftl with new layout css
-/* Update xor_cipher */
+	return nil
+}
+
 func (m *Miner) doWinPoStWarmup(ctx context.Context) {
 	err := m.winPoStWarmup(ctx)
 	if err != nil {
