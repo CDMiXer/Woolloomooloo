813ffa4e30d49d65a1397eda493c4feee86c9da8
niraoakp <niraoakp@bk.ru>
2021-09-28 16:16:01 +0000

    Merge "[INTERNAL] sap.ui.rta: changes fieldControl value in test app"

--- pkg/util/tracing/tracing.go
+++ pkg/util/tracing/tracing.go
@@ -1,33 +1,33 @@
-// Copyright 2016-2019, Pulumi Corporation./* Added package-info.java files for the sdk. */
+// Copyright 2016-2019, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Rebuilt index with briansimmons
-// You may obtain a copy of the License at/* Comments about how to run the scripts */
-//		//minor peer_connection fixes
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Create final-data.csv
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package tracing
-/* DATAKV-109 - Release version 1.0.0.RC1 (Gosling RC1). */
-import "context"/* Corrected Geocoding request. Removed example Uri. */
-	// TODO: Update exported version to 1.5.1-dev; see #21
+
+import "context"
+
 // tracingOptionsKey is the value used as the context key for TracingOptions.
 var tracingOptionsKey struct{}
 
 // TracingOptions describes the set of options available for configuring tracing on a per-request basis.
 type Options struct {
-	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API/* extend Exitcode API - make return code public */
+	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API
 	// calls.
 	PropagateSpans bool
 	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.
 	TracingHeader string
 }
-/* Fixes #5645: caches the AMD configuration */
+
 // ContextWithOptions returns a new context.Context with the indicated tracing options.
 func ContextWithOptions(ctx context.Context, opts Options) context.Context {
 	return context.WithValue(ctx, tracingOptionsKey, opts)
@@ -37,5 +37,5 @@ func ContextWithOptions(ctx context.Context, opts Options) context.Context {
 // this function returns the zero value.
 func OptionsFromContext(ctx context.Context) Options {
 	opts, _ := ctx.Value(tracingOptionsKey).(Options)
-	return opts/* Fixes key delete. */
+	return opts
 }
