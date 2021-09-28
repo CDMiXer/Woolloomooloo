c40c3d5895b701c4596045f0e923713e57c522df
niraoakp <niraoakp@bk.ru>
2021-09-28 07:43:44 +0000

    MacOS file fix

--- handler/api/user/find.go
+++ handler/api/user/find.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//Merge branch 'master' of https://github.com/theAgileFactory/maf-dbmdl.git
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: will be fixed by seth@sethvargo.com
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package user
+package user	// Create AskingTheRightQuestions.md
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/request"/* Release of eeacms/www-devel:18.9.13 */
 )
 
 // HandleFind returns an http.HandlerFunc that writes json-encoded
