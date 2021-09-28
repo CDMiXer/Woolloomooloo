333f33458068cf0dbfc31c20dcbf8dddd87eb9d2
niraoakp <niraoakp@bk.ru>
2021-09-28 09:22:50 +0000

    change final name back since context root will change url

--- chain/actors/builtin/account/v3.go
+++ chain/actors/builtin/account/v3.go
@@ -1,30 +1,30 @@
-package account/* Unlock as soon as possible when creating a new object. */
+package account
 
 import (
-	"github.com/filecoin-project/go-address"/* Use AS::Memoizable. */
+	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-/* monitoring modified */
-	"github.com/filecoin-project/lotus/chain/actors/adt"	// TODO: hacked by alex.gaynor@gmail.com
+
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"
 )
 
-var _ State = (*state3)(nil)
-
+var _ State = (*state3)(nil)		//improved query param
+/* Release Candidate 0.5.6 RC4 */
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}
+	}	// TODO: delete un-use import
 	return &out, nil
 }
 
 type state3 struct {
 	account3.State
-	store adt.Store
-}/* Release notes for 2.4.1. */
-
-func (s *state3) PubkeyAddress() (address.Address, error) {	// TODO: hacked by boringland@protonmail.ch
-	return s.Address, nil		//aff7c662-2e5b-11e5-9284-b827eb9e62be
+	store adt.Store	// TODO: hacked by alan.shaw@protocol.ai
 }
+
+func (s *state3) PubkeyAddress() (address.Address, error) {
+	return s.Address, nil
+}	// TODO: REMOVE: testinge buttons
