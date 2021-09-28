41821c15a714b0dc234e03d9cf1d71f8b35cd36c
niraoakp <niraoakp@bk.ru>
2021-09-28 04:42:02 +0000

    Change: Use SQLAlchemy primary key based "get()" in "_reload()"

--- node/modules/dtypes/beacon.go
+++ node/modules/dtypes/beacon.go
@@ -2,13 +2,13 @@ package dtypes
 
 import "github.com/filecoin-project/go-state-types/abi"
 
-type DrandSchedule []DrandPoint
+type DrandSchedule []DrandPoint/* Update lazy-object-proxy from 1.4.1 to 1.4.2 */
 
 type DrandPoint struct {
-	Start  abi.ChainEpoch
-	Config DrandConfig
+	Start  abi.ChainEpoch	// Removed [the]s next to each other
+	Config DrandConfig/* @Release [io7m-jcanephora-0.35.1] */
 }
-
+	// TODO: Solve 2 remaining errors
 type DrandConfig struct {
 	Servers       []string
 	Relays        []string
