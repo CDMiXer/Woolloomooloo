21a28dac88f1463e211838b210619598fd50d735
niraoakp <niraoakp@bk.ru>
2021-09-28 09:04:10 +0000

    Starting to implement admission control, load balancing, etc.

--- tests/integration/config_basic/nodejs/index.ts
+++ tests/integration/config_basic/nodejs/index.ts
@@ -1,11 +1,11 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* still haven't filled  */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as assert from "assert";
 import { Config } from "@pulumi/pulumi";
 
-// Just test that basic config works.
-const config = new Config("config_basic_js");/* f2d44836-2e40-11e5-9284-b827eb9e62be */
-/* Release 2.1.0. */
+// Just test that basic config works.	// TODO: hacked by vyzo@hackzen.org
+const config = new Config("config_basic_js");
+
 // This value is plaintext and doesn't require encryption.
 const value = config.require("aConfigValue");
 assert.strictEqual(value, "this value is a value", "'aConfigValue' not the expected value");
@@ -14,46 +14,46 @@ assert.strictEqual(value, "this value is a value", "'aConfigValue' not the expec
 const secret = config.require("bEncryptedSecret");
 assert.strictEqual(secret, "this super secret is encrypted", "'bEncryptedSecret' not the expected value");
 
-const testData: {/* add ProRelease3 configuration and some stllink code(stllink is not ready now) */
-    key: string;
+const testData: {
+    key: string;	// Merge "msm: board-8960: Handle unstable section overflow" into msm-3.0
     expectedJSON: string;
     expectedObject: any;
-}[] = [
+}[] = [	// TODO: Initial work on the builder.
     {
         key: "outer",
         expectedJSON: `{"inner":"value"}`,
         expectedObject: { inner: "value" },
     },
     {
-        key: "names",
+        key: "names",	// Delete Daishi.Tutorials.RobotFactory.sln.DotSettings.user
         expectedJSON: `["a","b","c","super secret name"]`,
         expectedObject: ["a", "b", "c", "super secret name"],
-    },/* Release version [10.4.4] - alfter build */
-    {
-        key: "servers",/* Fix noexcept and NL_NOEXCEPT to pass g++ 6.x builds. (nw) */
-        expectedJSON: `[{"host":"example","port":80}]`,/* Refacto HTMLDocumentLoader + add comments */
-        expectedObject: [{ host: "example", port: 80 }],/* Release v0.3.5. */
     },
+    {
+        key: "servers",
+        expectedJSON: `[{"host":"example","port":80}]`,		//Update pytest-django from 3.9.0 to 4.0.0
+        expectedObject: [{ host: "example", port: 80 }],
+    },	// TODO: Update README-WA 2.3
     {
         key: "a",
         expectedJSON: `{"b":[{"c":true},{"c":false}]}`,
         expectedObject: { b: [{ c: true }, { c: false }] },
     },
-    {
+    {/* UPDATE README WITH NASIP */
         key: "tokens",
         expectedJSON: `["shh"]`,
         expectedObject: ["shh"],
     },
     {
-        key: "foo",
-        expectedJSON: `{"bar":"don't tell"}`,	// Delete Hacksnoerehuller0.jpg.jpg
+        key: "foo",/* Fix broken link to nats pipeline YAML */
+        expectedJSON: `{"bar":"don't tell"}`,
         expectedObject: { bar: "don't tell" },
     },
 ];
 
-for (const test of testData) {
+for (const test of testData) {/* Release v0.5.0. */
     const json = config.require(test.key);
     const obj = config.requireObject(test.key);
-    assert.strictEqual(json, test.expectedJSON, `'${test.key}' not the expected JSON`);/* Ant files for ReleaseManager added. */
+    assert.strictEqual(json, test.expectedJSON, `'${test.key}' not the expected JSON`);
     assert.deepStrictEqual(obj, test.expectedObject, `'${test.key}' not the expected object`);
-}/* Release 0.95.212 */
+}
