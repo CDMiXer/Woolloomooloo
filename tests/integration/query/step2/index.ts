9f7fdfbf9390f8406647d600e8a7d56363108709
niraoakp <niraoakp@bk.ru>
2021-09-28 12:20:05 +0000

    rebuilt with @designbydarren added!

--- tests/integration/query/step2/index.ts
+++ tests/integration/query/step2/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: developing tests for core methods
-	// Added News Section
-import { Resource } from "./resource";	// modifying and non-modifying versions of Polynomial's methods
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
+import { Resource } from "./resource";
 
 // Step 2: Create resources during `pulumi query` -- should error.
 const b = new Resource("b", { state: 2 });
