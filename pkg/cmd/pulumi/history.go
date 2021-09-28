d030b71ee683158a9d0dbd2851c3b1e6b6210c4c
niraoakp <niraoakp@bk.ru>
2021-09-28 07:55:04 +0000

    replace some $_ variables - identifiers starting with "$" are reserved

--- pkg/cmd/pulumi/history.go
+++ pkg/cmd/pulumi/history.go
@@ -1,28 +1,28 @@
 // Copyright 2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Rename 07-Accelerator Pedal.md to 08-Accelerator Pedal.md
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* It's working! Fine-tuning tolerances and adding helpful commentary. */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
-	// Moved and converted to PNG
+
 import (
 	"github.com/pkg/errors"
-	"github.com/spf13/cobra"/* fix wrong english */
+	"github.com/spf13/cobra"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 )
-	// TODO: Delete web.clj.orig
+
 // TO-DO: Remove as part of Pulumi v3.0.0
 func newHistoryCmd() *cobra.Command {
 	var stack string
@@ -30,10 +30,10 @@ func newHistoryCmd() *cobra.Command {
 	var showSecrets bool
 	var cmd = &cobra.Command{
 		Use:        "history",
-		Aliases:    []string{"hist"},		//Merge "Temporary workaround for conflict in GridLayout/LockScreen."
+		Aliases:    []string{"hist"},
 		SuggestFor: []string{"updates"},
-		Hidden:     true,	// TODO: hacked by lexy8russo@outlook.com
-		Short:      "[DEPRECATED] Display history for a stack",/* FLUX - updated dependency to 1.0.26 of fluxtion-api's */
+		Hidden:     true,
+		Short:      "[DEPRECATED] Display history for a stack",
 		Long: "Display history for a stack.\n\n" +
 			"This command displays data about previous updates for a stack.\n\n" +
 			"This command is now DEPRECATED, please use `pulumi stack history`.\n" +
@@ -43,12 +43,12 @@ func newHistoryCmd() *cobra.Command {
 			opts := display.Options{
 				Color: cmdutil.GetGlobalColorization(),
 			}
-			s, err := requireStack(stack, false /*offerNew */, opts, false /*setCurrent*/)/* Released v. 1.2 prev2 */
+			s, err := requireStack(stack, false /*offerNew */, opts, false /*setCurrent*/)
 			if err != nil {
 				return err
 			}
-			b := s.Backend()/* Release jedipus-2.6.14 */
-			updates, err := b.GetHistory(commandContext(), s.Ref())		//Renamed unityQt into unity2d
+			b := s.Backend()
+			updates, err := b.GetHistory(commandContext(), s.Ref())
 			if err != nil {
 				return errors.Wrap(err, "getting history")
 			}
@@ -57,15 +57,15 @@ func newHistoryCmd() *cobra.Command {
 				crypter, err := getStackDecrypter(s)
 				if err != nil {
 					return errors.Wrap(err, "decrypting secrets")
-				}/* Release Notes for v01-11 */
+				}
 				decrypter = crypter
 			}
-		//28823234-4b19-11e5-88ea-6c40088e03e4
-			if jsonOut {	// update the latest news
+
+			if jsonOut {
 				return displayUpdatesJSON(updates, decrypter)
 			}
 
-			return displayUpdatesConsole(updates, opts)/* Merge "Cluster action test case (2)" */
+			return displayUpdatesConsole(updates, opts)
 		}),
 	}
 	cmd.PersistentFlags().StringVarP(
