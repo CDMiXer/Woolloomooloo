749832ef7a555df1c3d1719018bec2175e4d7d65
niraoakp <niraoakp@bk.ru>
2021-09-28 02:58:10 +0000

    Delete traversalTest.csv

--- service/license/load_test.go
+++ service/license/load_test.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//Move TestRequest cookies accessor into AD TestRequest
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: updating svg licenses info
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Merge "Use byte string or utf8 depending on python version for wsgi" */
+// See the License for the specific language governing permissions and/* ninja edits */
 // limitations under the License.
-/* Merge "Release bdm constraint source and dest type" */
+
 // +build !nolimit
 // +build !oss
-		//Implement missing cases in require_exe_file and other minor fixes
+
 package license
