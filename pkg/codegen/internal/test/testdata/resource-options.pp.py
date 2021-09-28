685dd04cc74061ab4d15d89c32ff6599da54adf3
niraoakp <niraoakp@bk.ru>
2021-09-28 02:08:24 +0000

    45bad378-2e3f-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/resource-options.pp.py
+++ pkg/codegen/internal/test/testdata/resource-options.pp.py
@@ -5,8 +5,8 @@ import pulumi_pulumi as pulumi
 provider = pulumi.providers.Aws("provider", region="us-west-2")
 bucket1 = aws.s3.Bucket("bucket1", opts=pulumi.ResourceOptions(provider=provider,
     depends_on=[provider],
-    protect=True,	// TODO: Fixing popup issue during artefact import from repository view
-    ignore_changes=[/* Added more code for form validation. */
+    protect=True,
+    ignore_changes=[
         "bucket",
         "lifecycleRules[0]",
     ]))
