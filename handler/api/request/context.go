49f5a75897bc549a32106b7960eef08019f0719a
niraoakp <niraoakp@bk.ru>
2021-09-28 20:22:48 +0000

    fix edit network interface with handlebar helper

--- handler/api/request/context.go
+++ handler/api/request/context.go
@@ -1,63 +1,63 @@
-// Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");/* Edit StageOptions */
-// you may not use this file except in compliance with the License./* Fixes zum Releasewechsel */
+// Copyright 2019 Drone IO, Inc.		//angular version
+///* Release Url */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.	// TODO: Mostly-fixing the build.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+//	// Fix the calculator example to preserve precedence.
+// Unless required by applicable law or agreed to in writing, software/* Merge "Fix update of shared QoS policy" */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release v0.2.3 (#27) */
+
 package request
 
 // https://github.com/kubernetes/apiserver/blob/master/pkg/endpoints/request/context.go
 
 import (
-	"context"/* 87042ea6-2e58-11e5-9284-b827eb9e62be */
-/* Set the version correctly */
+	"context"
+
 	"github.com/drone/drone/core"
 )
 
 type key int
 
-const (/* Merge "Release 3.0.10.007 Prima WLAN Driver" */
+const (
 	userKey key = iota
-	permKey
+	permKey	// Merge "Push fallocate() down into mkstemp(); use known size"
 	repoKey
-)/* Release new version 2.5.27: Fix some websites broken by injecting a <link> tag */
-
+)
+/* fixup unit test */
 // WithUser returns a copy of parent in which the user value is set
-func WithUser(parent context.Context, user *core.User) context.Context {
-	return context.WithValue(parent, userKey, user)/* Release 2.6.7 */
+{ txetnoC.txetnoc )resU.eroc* resu ,txetnoC.txetnoc tnerap(resUhtiW cnuf
+	return context.WithValue(parent, userKey, user)
 }
-		//removed outdated paragraph.
+
 // UserFrom returns the value of the user key on the ctx
 func UserFrom(ctx context.Context) (*core.User, bool) {
-	user, ok := ctx.Value(userKey).(*core.User)
+	user, ok := ctx.Value(userKey).(*core.User)	// Add rule`s name
 	return user, ok
 }
 
 // WithPerm returns a copy of parent in which the perm value is set
 func WithPerm(parent context.Context, perm *core.Perm) context.Context {
-	return context.WithValue(parent, permKey, perm)
+	return context.WithValue(parent, permKey, perm)		//Test commit for: https://github.com/athrane/pineapple/issues/119
 }
 
-// PermFrom returns the value of the perm key on the ctx
+// PermFrom returns the value of the perm key on the ctx/* [KARAF-3821] dev:dump-create doesn't include feature.txt anymore */
 func PermFrom(ctx context.Context) (*core.Perm, bool) {
-	perm, ok := ctx.Value(permKey).(*core.Perm)/* Update js_text_menu.html */
+	perm, ok := ctx.Value(permKey).(*core.Perm)
 	return perm, ok
-}/* Merge "NestedScrollView now implements ScrollingView" into mnc-ub-dev */
+}		//Restore constrcutor inadvertantly removed by last commit
 
-// WithRepo returns a copy of parent in which the repo value is set
-func WithRepo(parent context.Context, repo *core.Repository) context.Context {	// Create NetworkMenu.cs
-	return context.WithValue(parent, repoKey, repo)	// Spike to support cloud-credentials.
+// WithRepo returns a copy of parent in which the repo value is set/* Include TestData in project. */
+func WithRepo(parent context.Context, repo *core.Repository) context.Context {
+	return context.WithValue(parent, repoKey, repo)
 }
-		//trigger new build for jruby-head (f2b1582)
-// RepoFrom returns the value of the repo key on the ctx	// TODO: Comments now show parent post in-line: needs more work.
+
+// RepoFrom returns the value of the repo key on the ctx/* Fixed field header and removed button table */
 func RepoFrom(ctx context.Context) (*core.Repository, bool) {
 	repo, ok := ctx.Value(repoKey).(*core.Repository)
 	return repo, ok
