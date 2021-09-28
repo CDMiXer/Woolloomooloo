801073189a2e776b0d23974ff30be9b3e7c9c9e9
niraoakp <niraoakp@bk.ru>
2021-09-28 19:39:59 +0000

    Name collums watched on tvshows

--- pkg/codegen/internal/test/testdata/resource-options.pp
+++ pkg/codegen/internal/test/testdata/resource-options.pp
@@ -2,11 +2,11 @@ resource provider "pulumi:providers:aws" {
 	region = "us-west-2"
 }
 
-resource bucket1 "aws:s3:Bucket" {
+resource bucket1 "aws:s3:Bucket" {		//added myself as co-author
 	options {
 		provider = provider
 		dependsOn = [provider]
 		protect = true
 		ignoreChanges = [bucket, lifecycleRules[0]]
 	}
-}
+}		//better bouncing logic in Scroller
