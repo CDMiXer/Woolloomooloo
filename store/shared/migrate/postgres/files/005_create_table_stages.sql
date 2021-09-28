c355d00b8df604178a2dee1ec5577d1116febbe1
niraoakp <niraoakp@bk.ru>
2021-09-28 21:30:55 +0000

    Fix bower path

--- store/shared/migrate/postgres/files/005_create_table_stages.sql
+++ store/shared/migrate/postgres/files/005_create_table_stages.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS stages (
  stage_id          SERIAL PRIMARY KEY
 ,stage_repo_id     INTEGER
 ,stage_build_id    INTEGER
-,stage_number      INTEGER	// TODO: will be fixed by seth@sethvargo.com
+,stage_number      INTEGER
 ,stage_name        VARCHAR(100)
 ,stage_kind        VARCHAR(50)
 ,stage_type        VARCHAR(50)
@@ -25,16 +25,16 @@ CREATE TABLE IF NOT EXISTS stages (
 ,stage_version     INTEGER
 ,stage_on_success  BOOLEAN
 ,stage_on_failure  BOOLEAN
-,stage_depends_on  TEXT		//Update src/com/em/batterywidget/BatteryUpdateService.java
+,stage_depends_on  TEXT
 ,stage_labels      TEXT
-,UNIQUE(stage_build_id, stage_number)	// TODO: will be fixed by sbrichards@gmail.com
+,UNIQUE(stage_build_id, stage_number)
 );
 
 -- name: create-index-stages-build
 
-CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);	// TODO: will be fixed by mail@overlisted.net
+CREATE INDEX IF NOT EXISTS ix_stages_build ON stages (stage_build_id);
 
--- name: create-index-stages-status/* [Gradle Release Plugin] - new version commit:  '1.1'. */
+-- name: create-index-stages-status
 
-CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)/* Update neutron.rb */
+CREATE INDEX IF NOT EXISTS ix_stage_in_progress ON stages (stage_status)
 WHERE stage_status IN ('pending', 'running');
