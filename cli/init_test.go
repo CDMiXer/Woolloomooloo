905ff50e6552c616b8f801b1621cf5452cdcf044
niraoakp <niraoakp@bk.ru>
2021-09-28 00:15:22 +0000

    oops, fix offsetFromSolBI

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,9 +1,9 @@
 package cli
 
-import (	// use dummyFunDec.svar, removed return_val
+import (
 	logging "github.com/ipfs/go-log/v2"
 )
 
-func init() {	// TODO: will be fixed by jon@atack.com
-	logging.SetLogLevel("watchdog", "ERROR")	// TODO: Update buildProd.js.md
+func init() {
+	logging.SetLogLevel("watchdog", "ERROR")
 }
