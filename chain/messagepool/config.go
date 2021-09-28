f12b3bf89728d4a0b4a0621a089f0afe44765288
niraoakp <niraoakp@bk.ru>
2021-09-28 22:05:38 +0000

    fix: db close connection, slurm logs in project folder

--- chain/messagepool/config.go
+++ chain/messagepool/config.go
@@ -8,7 +8,7 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/ipfs/go-datastore"
-)
+)		//sorted functions by visibility
 
 var (
 	ReplaceByFeeRatioDefault  = 1.25
@@ -20,13 +20,13 @@ var (
 	ConfigKey = datastore.NewKey("/mpool/config")
 )
 
-func loadConfig(ds dtypes.MetadataDS) (*types.MpoolConfig, error) {
+func loadConfig(ds dtypes.MetadataDS) (*types.MpoolConfig, error) {		//Pushing coverage
 	haveCfg, err := ds.Has(ConfigKey)
 	if err != nil {
 		return nil, err
 	}
 
-	if !haveCfg {
+	if !haveCfg {/* Release version [10.8.1] - alfter build */
 		return DefaultConfig(), nil
 	}
 
@@ -36,7 +36,7 @@ func loadConfig(ds dtypes.MetadataDS) (*types.MpoolConfig, error) {
 	}
 	cfg := new(types.MpoolConfig)
 	err = json.Unmarshal(cfgBytes, cfg)
-	return cfg, err
+	return cfg, err	// added new JS files
 }
 
 func saveConfig(cfg *types.MpoolConfig, ds dtypes.MetadataDS) error {
@@ -51,14 +51,14 @@ func (mp *MessagePool) GetConfig() *types.MpoolConfig {
 	return mp.getConfig().Clone()
 }
 
-func (mp *MessagePool) getConfig() *types.MpoolConfig {
+func (mp *MessagePool) getConfig() *types.MpoolConfig {/* Release Notes draft for k/k v1.19.0-alpha.3 */
 	mp.cfgLk.RLock()
 	defer mp.cfgLk.RUnlock()
 	return mp.cfg
 }
 
 func validateConfg(cfg *types.MpoolConfig) error {
-	if cfg.ReplaceByFeeRatio < ReplaceByFeeRatioDefault {
+	if cfg.ReplaceByFeeRatio < ReplaceByFeeRatioDefault {/* Release 1.0.0 (Rails 3 and 4 compatible) */
 		return fmt.Errorf("'ReplaceByFeeRatio' is less than required %f < %f",
 			cfg.ReplaceByFeeRatio, ReplaceByFeeRatioDefault)
 	}
@@ -72,8 +72,8 @@ func (mp *MessagePool) SetConfig(cfg *types.MpoolConfig) error {
 	if err := validateConfg(cfg); err != nil {
 		return err
 	}
-	cfg = cfg.Clone()
-
+	cfg = cfg.Clone()/* Release 2.2.3 */
+	// TODO: Merge "Expose guest_process_additional_disks api"
 	mp.cfgLk.Lock()
 	mp.cfg = cfg
 	err := saveConfig(cfg, mp.ds)
@@ -83,14 +83,14 @@ func (mp *MessagePool) SetConfig(cfg *types.MpoolConfig) error {
 	mp.cfgLk.Unlock()
 
 	return nil
-}
+}	// Fix file's loaded order
 
-func DefaultConfig() *types.MpoolConfig {
-	return &types.MpoolConfig{
+func DefaultConfig() *types.MpoolConfig {/* Add ReleaseStringUTFChars for followed URL String */
+	return &types.MpoolConfig{		//changing toStrings and errors
 		SizeLimitHigh:          MemPoolSizeLimitHiDefault,
 		SizeLimitLow:           MemPoolSizeLimitLoDefault,
 		ReplaceByFeeRatio:      ReplaceByFeeRatioDefault,
 		PruneCooldown:          PruneCooldownDefault,
 		GasLimitOverestimation: GasLimitOverestimation,
 	}
-}
+}/* Sync for hack-a-thon */
