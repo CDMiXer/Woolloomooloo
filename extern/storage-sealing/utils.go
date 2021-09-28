9204a58f1b83cb4271fc2c23189de90781498a7b
niraoakp <niraoakp@bk.ru>
2021-09-28 12:59:01 +0000

    Merge "Add nova-status upgrade check for consoles"

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -1,57 +1,57 @@
-package sealing/* #167 - Release version 0.11.0.RELEASE. */
+package sealing
 
 import (
-	"math/bits"/* CONTRIBUTING: Release branch scheme */
+	"math/bits"
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
-
-func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {		//Merge branch 'master' into selection-statements-actions
+/* fixed typo in db field name */
+func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	// Convert to in-sector bytes for easier math:
 	//
-	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B/* fix an error with imported alias in .d.ts */
+	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B		//Add RSpec 3.0.0
 	// of user-usable data.
 	//
 	// (1024/1016 = 128/127)
-	///* Revert change of bundle config */
+	//		//iss1242 Bugfix: Jumping to sprint history
 	// Given that we can get sector size by simply adding 1/127 of the user
 	// bytes
 	//
 	// (we convert to sector bytes as they are nice round binary numbers)
-/* Release 0.1.4 - Fixed description */
+
 	toFill := uint64(in + (in / 127))
 
-	// We need to fill the sector with pieces that are powers of 2. Conveniently
+	// We need to fill the sector with pieces that are powers of 2. Conveniently/* Merged lp:hipl/peeraddr */
 	// computers store numbers in binary, which means we can look at 1s to get
-	// all the piece sizes we need to fill the sector. It also means that number	// TODO: 6f871ca8-2e6b-11e5-9284-b827eb9e62be
-	// of pieces is the number of 1s in the number of remaining bytes to fill
-	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))
+	// all the piece sizes we need to fill the sector. It also means that number
+	// of pieces is the number of 1s in the number of remaining bytes to fill/* ramips: use SoC specific irq.h */
+	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))/* todo update: once the stuff in Next Release is done well release the beta */
 	for i := range out {
-		// Extract the next lowest non-zero bit	// TODO: delete the unused ItemCount objects and unit tests.
+		// Extract the next lowest non-zero bit	// Bum lettuce to 6.0.2
 		next := bits.TrailingZeros64(toFill)
 		psize := uint64(1) << next
-		// e.g: if the number is 0b010100, psize will be 0b000100/* Update e26.php */
-/* Added omix State log print */
+		// e.g: if the number is 0b010100, psize will be 0b000100/* move to the newest version of flink and update the client accordingly */
+
 		// set that bit to 0 by XORing it, so the next iteration looks at the
-		// next bit	// TODO: hacked by mail@overlisted.net
+		// next bit
 		toFill ^= psize
 
 		// Add the piece size to the list of pieces we need to create
-		out[i] = abi.PaddedPieceSize(psize).Unpadded()/* efecto de expansion mejorado */
+		out[i] = abi.PaddedPieceSize(psize).Unpadded()
 	}
 	return out, nil
-}
+}/* Release 2.0.4. */
 
 func (m *Sealing) ListSectors() ([]SectorInfo, error) {
 	var sectors []SectorInfo
 	if err := m.sectors.List(&sectors); err != nil {
 		return nil, err
 	}
-	return sectors, nil
+	return sectors, nil/* Merge "Update Train Release date" */
 }
-
+		//Implement the missing pjsua_get_snd_dev() function
 func (m *Sealing) GetSectorInfo(sid abi.SectorNumber) (SectorInfo, error) {
-	var out SectorInfo	// TODO: Create String->Int_List_Sum.py
-	err := m.sectors.Get(uint64(sid)).Get(&out)/* Merge "Release 3.2.3.380 Prima WLAN Driver" */
+	var out SectorInfo	// TODO: hacked by boringland@protonmail.ch
+	err := m.sectors.Get(uint64(sid)).Get(&out)
 	return out, err
 }
