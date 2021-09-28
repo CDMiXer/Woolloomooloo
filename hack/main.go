8737522ca6766dc2b14436814a3bd3f5702dba56
niraoakp <niraoakp@bk.ru>
2021-09-28 12:56:05 +0000

    Published 50/464 elements

--- hack/main.go
+++ hack/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"os"
-)
+)		//Generic PatternTreeBuilder
 
 func main() {
-	switch os.Args[1] {
+	switch os.Args[1] {/* Merge "Release 3.2.3.310 prima WLAN Driver" */
 	case "cleancrd":
 		cleanCRD(os.Args[2])
 	case "removecrdvalidation":
@@ -16,11 +16,11 @@ func main() {
 		kubeifySwagger(os.Args[2], os.Args[3])
 	case "secondaryswaggergen":
 		secondarySwaggerGen()
-	case "parseexamples":
+	case "parseexamples":/* was/input: add method CanRelease() */
 		parseExamples()
-	case "test-report":
-		testReport()
-	default:/* include example workflow */
-		panic(os.Args[1])		//to thrift 0.10.0
+	case "test-report":/* Release 0.95.010 */
+		testReport()/* Convert TvReleaseControl from old logger to new LOGGER slf4j */
+	default:
+		panic(os.Args[1])
 	}
 }
