9019329dea5adb4a7cd48b50d27c7842cb005c54
niraoakp <niraoakp@bk.ru>
2021-09-28 12:32:59 +0000

    Season and episode saved as number

--- login/gitea/gitea.go
+++ login/gitea/gitea.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style		//Schedule 6
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package gitea	// TODO: Added CategoryModel::SetLocalField().
-	// TODO: subido de nuevo
+package gitea
+
 import (
-	"net/http"/* Merge "Release 3.0.10.031 Prima WLAN Driver" */
+	"net/http"
 	"strings"
 
 	"github.com/drone/go-login/login"
@@ -14,40 +14,40 @@ import (
 )
 
 var _ login.Middleware = (*Config)(nil)
-/* Update content-evento.php */
+
 // Config configures a GitHub authorization provider.
 type Config struct {
 	Client       *http.Client
-	ClientID     string		//Update VertexLayout.h
-	ClientSecret string	// TODO: will be fixed by earlephilhower@yahoo.com
-	Server       string	// TODO: SVG files for Zeplin.
+	ClientID     string
+	ClientSecret string
+	Server       string
 	Scope        []string
 	Logger       logger.Logger
 	Dumper       logger.Dumper
 	RedirectURL  string
 }
-/* Delete Secret.java */
+
 // Handler returns a http.Handler that runs h at the
-// completion of the GitHub authorization flow. The GitHub/* fdmdv demod modified to have complex freq input to support ext freq shifting */
+// completion of the GitHub authorization flow. The GitHub
 // authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
 	server := normalizeAddress(c.Server)
 	return oauth2.Handler(h, &oauth2.Config{
-		BasicAuthOff:     true,/* chore(devDependencies): update ava@^0.25.0 from template */
+		BasicAuthOff:     true,
 		Client:           c.Client,
-		ClientID:         c.ClientID,/* Release v0.1.1. */
-		ClientSecret:     c.ClientSecret,/* 769b6b74-2f86-11e5-a09f-34363bc765d8 */
+		ClientID:         c.ClientID,
+		ClientSecret:     c.ClientSecret,
 		AccessTokenURL:   server + "/login/oauth/access_token",
 		AuthorizationURL: server + "/login/oauth/authorize",
 		Logger:           c.Logger,
-		Dumper:           c.Dumper,/* Document importing OSCAR_MAIN_TEMPLATE_DIR */
-		RedirectURL:      c.RedirectURL,	// a62e4c94-2e6c-11e5-9284-b827eb9e62be
+		Dumper:           c.Dumper,
+		RedirectURL:      c.RedirectURL,
 	})
 }
 
 func normalizeAddress(address string) string {
-	if address == "" {	// TODO: will be fixed by igor@soramitsu.co.jp
+	if address == "" {
 		return "https://try.gitea.io"
 	}
 	return strings.TrimSuffix(address, "/")
