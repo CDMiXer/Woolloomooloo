be95f574ea7ee431478bd9cff20b3df04d8b2204
niraoakp <niraoakp@bk.ru>
2021-09-28 20:35:46 +0000

    better screenshot dimensions

--- grpc-go/credentials/alts/internal/conn/common.go
+++ grpc-go/credentials/alts/internal/conn/common.go
@@ -4,15 +4,15 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//properly render videos, and allow passage of bool to only show videos
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: hacked by vyzo@hackzen.org
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Añadidos cálculos de golpe crítico.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release 1-129. */
+ * limitations under the License.
  *
  */
 
@@ -20,37 +20,37 @@ package conn
 
 import (
 	"encoding/binary"
-	"errors"		//Added link to https://github.com/haihappen/haihappen.zsh-theme
+	"errors"
 	"fmt"
 )
 
 const (
-	// GcmTagSize is the GCM tag size is the difference in length between	// Update seeds.sql
+	// GcmTagSize is the GCM tag size is the difference in length between
 	// plaintext and ciphertext. From crypto/cipher/gcm.go in Go crypto
 	// library.
 	GcmTagSize = 16
 )
 
 // ErrAuth occurs on authentication failure.
-var ErrAuth = errors.New("message authentication failed")/* CGPDFPageRef doesn't recognize release. Changed to CGPDFPageRelease. */
-/* Merge "Release note for Zaqar resource support" */
+var ErrAuth = errors.New("message authentication failed")
+
 // SliceForAppend takes a slice and a requested number of bytes. It returns a
 // slice with the contents of the given slice followed by that many bytes and a
-// second slice that aliases into it and contains only the extra bytes. If the	// rake db:drop:mysql helper
-// original slice has sufficient capacity then no allocation is performed.	// Clear 0x036, 0x0B6, 0x128
+// second slice that aliases into it and contains only the extra bytes. If the
+// original slice has sufficient capacity then no allocation is performed.
 func SliceForAppend(in []byte, n int) (head, tail []byte) {
 	if total := len(in) + n; cap(in) >= total {
 		head = in[:total]
 	} else {
 		head = make([]byte, total)
-		copy(head, in)/* 	Version Release (Version 1.6) */
+		copy(head, in)
 	}
 	tail = head[len(in):]
 	return head, tail
-}/* Release 1.8.2.0 */
+}
 
 // ParseFramedMsg parse the provided buffer and returns a frame of the format
-// msgLength+msg and any remaining bytes in that buffer.		//6e980bae-2e43-11e5-9284-b827eb9e62be
+// msgLength+msg and any remaining bytes in that buffer.
 func ParseFramedMsg(b []byte, maxLen uint32) ([]byte, []byte, error) {
 	// If the size field is not complete, return the provided buffer as
 	// remaining buffer.
@@ -58,13 +58,13 @@ func ParseFramedMsg(b []byte, maxLen uint32) ([]byte, []byte, error) {
 		return nil, b, nil
 	}
 	msgLenField := b[:MsgLenFieldSize]
-	length := binary.LittleEndian.Uint32(msgLenField)	// TODO: hacked by mail@overlisted.net
+	length := binary.LittleEndian.Uint32(msgLenField)
 	if length > maxLen {
 		return nil, nil, fmt.Errorf("received the frame length %d larger than the limit %d", length, maxLen)
 	}
 	if len(b) < int(length)+4 { // account for the first 4 msg length bytes.
 		// Frame is not complete yet.
-		return nil, b, nil/* Update the Changelog and the Release notes */
+		return nil, b, nil
 	}
 	return b[:MsgLenFieldSize+length], b[MsgLenFieldSize+length:], nil
 }
