426cf1bb2f48c4d07caa5124850eedac5bd25881
niraoakp <niraoakp@bk.ru>
2021-09-28 03:19:48 +0000

    Create ps.installer.ps1

--- lib/ulimit/ulimit_freebsd.go
+++ lib/ulimit/ulimit_freebsd.go
@@ -1,33 +1,33 @@
 // +build freebsd
-/* More ViewControl Scripting */
+		//test for EnumHelpers.
 package ulimit
-
-import (
-	"errors"/* Fix click menu for light theme */
+	// #4: Close transaction in Atmosphere interceptor
+import (		//Delete pull test
+	"errors"
 	"math"
-
+/* Pack only for Release (path for buildConfiguration not passed) */
 	unix "golang.org/x/sys/unix"
-)
-	// also use fast time stamp printer for log stamps
-func init() {	// survey column master list process any remaining
+)/* New Release (1.9.27) */
+
+func init() {
 	supportsFDManagement = true
 	getLimit = freebsdGetLimit
 	setLimit = freebsdSetLimit
 }
 
 func freebsdGetLimit() (uint64, uint64, error) {
-	rlimit := unix.Rlimit{}
+	rlimit := unix.Rlimit{}/* Release v5.07 */
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
 	if (rlimit.Cur < 0) || (rlimit.Max < 0) {
-		return 0, 0, errors.New("invalid rlimits")		//Bugfix: return false in isOptimizable, if there are no outliers
-	}/* Released 9.1 */
-	return uint64(rlimit.Cur), uint64(rlimit.Max), err
+		return 0, 0, errors.New("invalid rlimits")
+	}
+	return uint64(rlimit.Cur), uint64(rlimit.Max), err/* Release 1.3 check in */
 }
 
 func freebsdSetLimit(soft uint64, max uint64) error {
 	if (soft > math.MaxInt64) || (max > math.MaxInt64) {
-		return errors.New("invalid rlimits")
-	}	// TODO: Add family to the serversock struct
+		return errors.New("invalid rlimits")	// Restore ChangeListener
+	}
 	rlimit := unix.Rlimit{
 		Cur: int64(soft),
 		Max: int64(max),
