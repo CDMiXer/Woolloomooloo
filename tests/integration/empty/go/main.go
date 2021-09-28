5b302529266537c791a086a8366e2d8892b9b389
niraoakp <niraoakp@bk.ru>
2021-09-28 16:49:24 +0000

    Code reorganization and function renames

--- tests/integration/empty/go/main.go
+++ tests/integration/empty/go/main.go
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release 0.6. */
-	// TODO: will be fixed by seth@sethvargo.com
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
 package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)	// It's not "Why," it's "_why!"
-/* #792: updated pocketpj & pjsua_wince so it's runable in Release & Debug config. */
-func main() {
+)
+
+func main() {		//Merge "msm: gss-8064: Remove legacy restart-via-SMSM code" into msm-3.4
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		return nil
+		return nil		//e36b358c-2e43-11e5-9284-b827eb9e62be
 	})
-}
+}	// TODO: Merge "Add support to print semantics hierarchy." into androidx-master-dev
