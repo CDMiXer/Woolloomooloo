5e9811578f89143613eaecd484b18e4758aefbbd
niraoakp <niraoakp@bk.ru>
2021-09-28 17:05:11 +0000

    Fixed database setup [user id was int should be varchar)

--- handler/api/users/repos.go
+++ handler/api/users/repos.go
@@ -1,29 +1,29 @@
-// Copyright 2019 Drone IO, Inc./* Release of eeacms/www:19.11.22 */
-//
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: - taxes for products
+// Copyright 2019 Drone IO, Inc.
+///* v1.0 Release! */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* 1469d5f4-2e43-11e5-9284-b827eb9e62be */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//c3badfa6-2e45-11e5-9284-b827eb9e62be
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Rename MenuManager.cs to OneMinuteGUI/MenuManager.cs */
-package users
 
+package users
+	// Correções nos comentários
 import (
-	"net/http"
+	"net/http"		//Cortex-M4F GCC port: added stack padder.
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Changed the timeout values */
-	"github.com/drone/drone/logger"
-
-	"github.com/go-chi/chi"	// TODO: will be fixed by fjl@ethereum.org
+	"github.com/drone/drone/handler/api/render"/* Update kocicka.md */
+	"github.com/drone/drone/logger"/* Update Release system */
+/* Merge "[Release] Webkit2-efl-123997_0.11.91" into tizen_2.2 */
+	"github.com/go-chi/chi"
 )
-/* Release 1.0 code freeze. */
+
 // HandleRepoList returns an http.HandlerFunc that writes a json-encoded
 // list of all user repositories to the response body.
 func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.HandlerFunc {
@@ -32,23 +32,23 @@ func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.Handl
 
 		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
-			render.NotFound(w, err)
-			logger.FromRequest(r).
-				WithError(err).
+			render.NotFound(w, err)		//Added missing ;
+			logger.FromRequest(r)./* sgpr - improving numerical stability. */
+				WithError(err)./* Inicio do projeto com os arquivos do CodeIgniter 3.0.6 */
 				WithField("user", login).
-				Debugln("api: cannot find user")
+				Debugln("api: cannot find user")	// Prueba Server con select
 			return
-		}	// TODO: The page of redirection
+}		
 
 		repos, err := repos.List(r.Context(), user.ID)
-		if err != nil {/* Controle de yaw, organização do código. */
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
-				WithError(err)./* One-liner CLEANFORMAT */
+				WithError(err).		//Añadido oneliner comentado
 				WithField("user", login).
-				Warnln("api: cannot list user repositories")		//doc : DELETE archive the disruption and its impacts.
+				Warnln("api: cannot list user repositories")
 		} else {
 			render.JSON(w, repos, 200)
 		}
-	}
-}
+	}/* Merge branch 'master' into issue-1122 */
+}/* 69646e6c-2e74-11e5-9284-b827eb9e62be */
