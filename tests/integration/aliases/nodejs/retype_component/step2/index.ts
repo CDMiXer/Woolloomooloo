34a4500e34b769acca831eaffec743569589efce
niraoakp <niraoakp@bk.ru>
2021-09-28 08:48:31 +0000

    [CRAFT-AI] Add resource: foo/bat/vt.bt

--- tests/integration/aliases/nodejs/retype_component/step2/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step2/index.ts
@@ -5,15 +5,15 @@ import * as pulumi from "@pulumi/pulumi";
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }
+    }/* CSR: Wrong event and wrong place for mu. */
 }
 
 // Scenario #4 - change the type of a component
 class ComponentFour extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        // Add an alias that references the old type of this resource...
-        const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];
+        // Add an alias that references the old type of this resource...		//fix log4j and startup
+        const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];	// TODO: will be fixed by sebastian.tharakan97@gmail.com
         // ..and then make the super call with the new type of this resource and the added alias.
         super("my:differentmodule:ComponentFourWithADifferentTypeName", name, {}, { ...opts, aliases });
         // The child resource will also pick up an implicit alias due to the new type of the component it is parented
@@ -21,4 +21,4 @@ class ComponentFour extends pulumi.ComponentResource {
         this.resource = new Resource("otherchild", { parent: this });
     }
 }
-const comp4 = new ComponentFour("comp4");
+const comp4 = new ComponentFour("comp4");/* #6 - Release 0.2.0.RELEASE. */
