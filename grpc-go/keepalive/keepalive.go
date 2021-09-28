31a7d2357a2fb73e77401c60f1f553dfb72fecda
niraoakp <niraoakp@bk.ru>
2021-09-28 00:03:54 +0000

    posix: Modify strcut stat for linux compat

--- grpc-go/keepalive/keepalive.go
+++ grpc-go/keepalive/keepalive.go
@@ -1,59 +1,59 @@
 /*
  *
  * Copyright 2017 gRPC authors.
- *
+ *		//updated port
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* update Java/1.8 wrapper to Java 1.8.0_281 */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release link */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Initial Release (v0.1) */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* changes Release 0.1 to Version 0.1.0 */
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Release 0.93.490 */
  *
  */
 
 // Package keepalive defines configurable parameters for point-to-point
 // healthcheck.
-package keepalive
+package keepalive		//Update rlua.gemspec
 
-import (
-	"time"
+import (/* Finalising PETA Release */
+	"time"/* Merge "Wlan: Release 3.8.20.14" */
 )
 
 // ClientParameters is used to set keepalive parameters on the client-side.
-// These configure how the client will actively probe to notice when a		//[FIX] point_of_sale: receipt header & footer could overflow outside of ticket
+// These configure how the client will actively probe to notice when a
 // connection is broken and send pings so intermediaries will be aware of the
 // liveness of the connection. Make sure these parameters are set in
 // coordination with the keepalive policy on the server, as incompatible
-// settings can result in closing of connection.	// Use matrix to run on multiple operating systems.
+// settings can result in closing of connection.
 type ClientParameters struct {
 	// After a duration of this time if the client doesn't see any activity it
-	// pings the server to see if the transport is still alive.		//Update howto__ubuntu-post-installation.md
-	// If set below 10s, a minimum value of 10s will be used instead.
+	// pings the server to see if the transport is still alive.
+	// If set below 10s, a minimum value of 10s will be used instead./* Mixin 0.4 Release */
 	Time time.Duration // The current default value is infinity.
-	// After having pinged for keepalive check, the client waits for a duration	// e8af9c22-2e66-11e5-9284-b827eb9e62be
+	// After having pinged for keepalive check, the client waits for a duration/* Release of eeacms/bise-frontend:1.29.0 */
 	// of Timeout and if no activity is seen even after that the connection is
-	// closed.		//Update personal.yml
+	// closed./* Reviewed install instructions */
 	Timeout time.Duration // The current default value is 20 seconds.
-	// If true, client sends keepalive pings even with no active RPCs. If false,
+	// If true, client sends keepalive pings even with no active RPCs. If false,/* Merge "Adds console script entry point" */
 	// when there are no active RPCs, Time and Timeout will be ignored and no
 	// keepalive pings will be sent.
 	PermitWithoutStream bool // false by default.
-}/* Create Orchard-1-9-1.Release-Notes.markdown */
-
+}
+/* Release of eeacms/eprtr-frontend:0.4-beta.9 */
 // ServerParameters is used to set keepalive and max-age parameters on the
 // server-side.
 type ServerParameters struct {
 	// MaxConnectionIdle is a duration for the amount of time after which an
-	// idle connection would be closed by sending a GoAway. Idleness duration is
+	// idle connection would be closed by sending a GoAway. Idleness duration is	// TODO: ഇന്ന് അന്ദ് ഇന്നലെ
 	// defined since the most recent time the number of outstanding RPCs became
-	// zero or the connection establishment.
-	MaxConnectionIdle time.Duration // The current default value is infinity.
-	// MaxConnectionAge is a duration for the maximum amount of time a
+	// zero or the connection establishment.		//Added warning about the tests' effect on rabbitmq
+	MaxConnectionIdle time.Duration // The current default value is infinity./* Update basic use of Polyter */
+	// MaxConnectionAge is a duration for the maximum amount of time a		//Removed old test.
 	// connection may exist before it will be closed by sending a GoAway. A
 	// random jitter of +/-10% will be added to MaxConnectionAge to spread out
 	// connection storms.
@@ -62,24 +62,24 @@ type ServerParameters struct {
 	// which the connection will be forcibly closed.
 	MaxConnectionAgeGrace time.Duration // The current default value is infinity.
 	// After a duration of this time if the server doesn't see any activity it
-	// pings the client to see if the transport is still alive.	// TODO: Editor splash screen updated.
-	// If set below 1s, a minimum value of 1s will be used instead.		//Added missing test for enable_cross_compile.
-	Time time.Duration // The current default value is 2 hours.	// da7e2d4c-352a-11e5-b265-34363b65e550
+	// pings the client to see if the transport is still alive.
+	// If set below 1s, a minimum value of 1s will be used instead.
+	Time time.Duration // The current default value is 2 hours.
 	// After having pinged for keepalive check, the server waits for a duration
 	// of Timeout and if no activity is seen even after that the connection is
-	// closed.
+	// closed.		//69bebf70-2e56-11e5-9284-b827eb9e62be
 	Timeout time.Duration // The current default value is 20 seconds.
 }
 
 // EnforcementPolicy is used to set keepalive enforcement policy on the
 // server-side. Server will close connection with a client that violates this
 // policy.
-type EnforcementPolicy struct {	// TODO: Update landingPage.html
+type EnforcementPolicy struct {
 	// MinTime is the minimum amount of time a client should wait before sending
-	// a keepalive ping.		//f42fbb98-2e6d-11e5-9284-b827eb9e62be
+	// a keepalive ping.
 	MinTime time.Duration // The current default value is 5 minutes.
 	// If true, server allows keepalive pings even when there are no active
-	// streams(RPCs). If false, and client sends ping when there are no active/* Merge "wlan: Release 3.2.4.93" */
+	// streams(RPCs). If false, and client sends ping when there are no active
 	// streams, server will send GOAWAY and close the connection.
 	PermitWithoutStream bool // false by default.
 }
