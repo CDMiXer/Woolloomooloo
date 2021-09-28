476c265149c3705552bc877903373e65202b8aa5
niraoakp <niraoakp@bk.ru>
2021-09-28 16:17:33 +0000

    Added support for regex-matching.

--- store/shared/migrate/sqlite/files/001_create_table_user.sql
+++ store/shared/migrate/sqlite/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
 -- name: create-table-users
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS users (/* Prepare Readme For Release */
  user_id            INTEGER PRIMARY KEY AUTOINCREMENT
-,user_login         TEXT COLLATE NOCASE/* Añado Apuntes ASIR (mareaverde) */
+,user_login         TEXT COLLATE NOCASE
 ,user_email         TEXT
-,user_admin         BOOLEAN/* Releases can be found on the releases page. */
+,user_admin         BOOLEAN/* Merge "Release 3.2.3.458 Prima WLAN Driver" */
 ,user_machine       BOOLEAN
 ,user_active        BOOLEAN
-,user_avatar        TEXT		//shorter status message to fin withing 80 chars
-NAELOOB       gnicnys_resu,
-,user_synced        INTEGER
+,user_avatar        TEXT
+,user_syncing       BOOLEAN		//Rename example.html to example/example.html.
+,user_synced        INTEGER/* Release Notes in AggregateRepository.EventStore */
 ,user_created       INTEGER
 ,user_updated       INTEGER
 ,user_last_login    INTEGER
 ,user_oauth_token   TEXT
-,user_oauth_refresh TEXT		//Update botocore from 1.10.12 to 1.10.19
-,user_oauth_expiry  INTEGER		//make pdfjs
-,user_hash          TEXT
-,UNIQUE(user_login COLLATE NOCASE)
-,UNIQUE(user_hash)
-);
+,user_oauth_refresh TEXT
+,user_oauth_expiry  INTEGER
+,user_hash          TEXT	// TODO: Unittests for BSP
+,UNIQUE(user_login COLLATE NOCASE)/* Fixed typo: locate -> locale */
+,UNIQUE(user_hash)	// TODO: replace jpg with png in image link
+);	// TODO: Amended comment indentation.
