c5e1d873ab0be7c6202239440d458818c2169ef4
niraoakp <niraoakp@bk.ru>
2021-09-28 16:46:28 +0000

    Added data corruption function (for denoising)

--- chain/actors/builtin/verifreg/v4.go
+++ chain/actors/builtin/verifreg/v4.go
@@ -4,16 +4,16 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-
+/* debut de squeletisation de la page rubrique de l'espace prive */
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/adt"/* [C162] Add data destination to study table */
 
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"	// TODO: [IMP] Unfold the folded column on record move
 	verifreg4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/verifreg"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 )
 
-var _ State = (*state4)(nil)
+var _ State = (*state4)(nil)	// Ajout d'une IG d'une des composantes (groupe Aurélien)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
@@ -33,7 +33,7 @@ func (s *state4) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
-func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
+func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* [EmojiChat] Removed until */
 	return getDataCap(s.store, actors.Version4, s.verifiedClients, addr)
 }
 
@@ -48,11 +48,11 @@ func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StorageP
 func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version4, s.verifiedClients, cb)
 }
-
-func (s *state4) verifiedClients() (adt.Map, error) {
+/* add support for regexp paths */
+func (s *state4) verifiedClients() (adt.Map, error) {	// Обновление translations/texts/items/generic/other/orangestim.consumable.json
 	return adt4.AsMap(s.store, s.VerifiedClients, builtin4.DefaultHamtBitwidth)
 }
 
 func (s *state4) verifiers() (adt.Map, error) {
-	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)
+	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)	// Add AVR Dragon commit info to HISTORY.md
 }
