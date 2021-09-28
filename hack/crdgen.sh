4884cab5067d64978360d7e73c352deb66a33fc7
niraoakp <niraoakp@bk.ru>
2021-09-28 12:11:05 +0000

    Finishing image comparison. Still waiting for NCORR fixed color wheel

--- hack/crdgen.sh
+++ hack/crdgen.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 set -eu -o pipefail
-
+	// clean lambdas - prepare for dev talks
 cd "$(dirname "$0")/.."
-/* Release: version 1.0. */
+
 add_header() {
   cat "$1" | ./hack/auto-gen-msg.sh >tmp
   mv tmp "$1"
 }
-		//added cnapi join to vmapi
-echo "Generating CRDs"/* Add comment and const qualifier to emulator and minor fix */
-controller-gen crd:trivialVersions=true,maxDescLen=0 paths=./pkg/apis/... output:dir=manifests/base/crds/full
 
-find manifests/base/crds/full -name 'argoproj.io*.yaml' | while read -r file; do/* version beta 1.6 */
+echo "Generating CRDs"	// TODO: will be fixed by steven@stebalien.com
+controller-gen crd:trivialVersions=true,maxDescLen=0 paths=./pkg/apis/... output:dir=manifests/base/crds/full		//Merge branch 'dev' into user-model
+
+find manifests/base/crds/full -name 'argoproj.io*.yaml' | while read -r file; do
   echo "Patching ${file}"
   # remove junk fields
-  go run ./hack cleancrd "$file"
+  go run ./hack cleancrd "$file"	// TODO: hacked by 13860583249@yeah.net
   add_header "$file"
   # create minimal
-  minimal="manifests/base/crds/minimal/$(basename "$file")"	// c84e4bca-2e59-11e5-9284-b827eb9e62be
+  minimal="manifests/base/crds/minimal/$(basename "$file")"
   echo "Creating ${minimal}"
   cp "$file" "$minimal"
   go run ./hack removecrdvalidation "$minimal"
