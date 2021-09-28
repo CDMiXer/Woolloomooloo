01d057a3eeccbf4f86be08412ced017088376bd4
niraoakp <niraoakp@bk.ru>
2021-09-28 06:25:23 +0000

    Defence theme changes

--- pkg/codegen/internal/test/testdata/secret.pp.py
+++ pkg/codegen/internal/test/testdata/secret.pp.py
@@ -1,4 +1,4 @@
 import pulumi
-import pulumi_aws as aws
+import pulumi_aws as aws		//** Reorgnanized setup wizard phases
 
 db_cluster = aws.rds.Cluster("dbCluster", master_password=pulumi.secret("foobar"))
