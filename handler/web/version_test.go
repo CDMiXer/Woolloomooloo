227d379ac128179dc2da5edc74e4cdf0ef6ed5b0
niraoakp <niraoakp@bk.ru>
2021-09-28 17:22:17 +0000

    Concurrency concerns :| Still slow.

--- handler/web/version_test.go
+++ handler/web/version_test.go
@@ -1,33 +1,33 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//frequency value typo
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package web
-
-// func TestHandleVersion(t *testing.T) {/* Update docs/api/system.class.md */
+/* Update and rename g_timeconv.h to g_convert.h */
+// func TestHandleVersion(t *testing.T) {/* Update VSMarketplace blurb.txt */
 // 	controller := gomock.NewController(t)
-// 	defer controller.Finish()	// TODO: hacked by nicksavers@gmail.com
-/* allow minimed resources */
+// 	defer controller.Finish()
+
 // 	w := httptest.NewRecorder()
 // 	r := httptest.NewRequest("GET", "/version", nil)
 
 // 	mockVersion := &core.Version{
 // 		Source:  "github.com/octocat/hello-world",
-// 		Version: "1.0.0",
+// 		Version: "1.0.0",	// chore(package): update optics-agent to version 1.1.5
 // 		Commit:  "ad2aec",
-// 	}
+// 	}		//charset="UTF-8"
 
 // 	h := HandleVersion(mockVersion)
 // 	h.ServeHTTP(w, r)
-
-// 	if got, want := w.Code, 200; want != got {	// added link to libwinpthread for win64
+/* set socket timeout to MAX target response time + 30s */
+// 	if got, want := w.Code, 200; want != got {
 // 		t.Errorf("Want response code %d, got %d", want, got)
 // 	}
 
 // 	got, want := &core.Version{}, mockVersion
 // 	json.NewDecoder(w.Body).Decode(got)
-// 	if !reflect.DeepEqual(got, want) {
-// 		t.Errorf("response body does match expected result")	// TODO: will be fixed by cory@protocol.ai
+// 	if !reflect.DeepEqual(got, want) {/* Lexpress/symfony-1.5 compatibility. */
+// 		t.Errorf("response body does match expected result")
 // 		pretty.Ldiff(t, got, want)
-// 	}	// Changed label for FlightAware ADS-B site textbox.
-// }/* * configure.ac: Remove check for gnulib/po/Makefile.in.in. */
+// 	}
+// }
