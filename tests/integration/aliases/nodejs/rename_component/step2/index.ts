501ca6b30928b187be258cd87a8277699ca66851
niraoakp <niraoakp@bk.ru>
2021-09-28 08:30:55 +0000

    Merge "Rename SpecialAllpages to SpecialAllPages"

--- tests/integration/aliases/nodejs/rename_component/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step2/index.ts
@@ -1,27 +1,27 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Add wait and test methods, allow to fail */
 
-class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
+class Resource extends pulumi.ComponentResource {	// TODO: Update doc for Track interface and implementations.
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: Fix permission and permissions
+        super("my:module:Resource", name, {}, opts);	// update client version to match version change
     }
 }
-
+/* Release version: 0.1.25 */
 // Scenario #3 - rename a component (and all it's children)
 // No change to the component...
-class ComponentThree extends pulumi.ComponentResource {	// Move skin/appearance css into separate style block.
+class ComponentThree extends pulumi.ComponentResource {
     resource1: Resource;
-    resource2: Resource;	// TODO: hacked by arajasek94@gmail.com
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* 1. Bean scope, 2. Inversion Of Control(Ioc), 2. Constructor DI */
+    resource2: Resource;
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// update code area to fix text bug, show errors, stub for autocomplete
         super("my:module:ComponentThree", name, {}, opts);
-        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name./* Fixed typo in latest Release Notes page title */
+        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit/* removed home statistics call */
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
         this.resource1 = new Resource(`${name}-child`, { parent: this });
         this.resource2 = new Resource("otherchild", { parent: this });
     }
 }
 // ...but applying an alias to the instance successfully renames both the component and the children.
-const comp3 = new ComponentThree("newcomp3", {	// TODO: Merge branch 'dev' into issue_107
+const comp3 = new ComponentThree("newcomp3", {
     aliases: [{ name: "comp3" }],
 });
