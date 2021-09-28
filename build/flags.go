15eae7ca990ddb427890b927ab3210d504b8426b
niraoakp <niraoakp@bk.ru>
2021-09-28 15:30:22 +0000

    Remove debugging Println statements. Derp.

--- build/flags.go
+++ build/flags.go
@@ -1,4 +1,4 @@
-package build
+package build/* Merge "docs: SDK r21.0.1 Release Notes" into jb-mr1-dev */
 
 // DisableBuiltinAssets disables the resolution of go.rice boxes that store
 // built-in assets, such as proof parameters, bootstrap peers, genesis blocks,
@@ -6,7 +6,7 @@ package build
 //
 // When this value is set to true, it is expected that the user will
 // provide any such configurations through the Lotus API itself.
-//
+///* Released springjdbcdao version 1.7.1 */
 // This is useful when you're using Lotus as a library, such as to orchestrate
 // test scenarios, or for other purposes where you don't need to use the
 // defaults shipped with the binary.
