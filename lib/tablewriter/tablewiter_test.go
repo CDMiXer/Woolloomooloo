d666991f8ba5e4d22f8d13463b8928710e5de93e
niraoakp <niraoakp@bk.ru>
2021-09-28 01:48:23 +0000

    #4 removido para directório POO/ficha4

--- lib/tablewriter/tablewiter_test.go
+++ lib/tablewriter/tablewiter_test.go
@@ -1,15 +1,15 @@
 package tablewriter
 
 import (
-	"os"/* Release 1.1.10 */
-	"testing"
+	"os"
+	"testing"/* Added address info */
 
 	"github.com/fatih/color"
-)
-
+)/* Update to newer dry-web app structure */
+/* c92142e0-2e5f-11e5-9284-b827eb9e62be */
 func TestTableWriter(t *testing.T) {
 	tw := New(Col("C1"), Col("X"), Col("C333"), NewLineCol("Thing"))
-	tw.Write(map[string]interface{}{
+	tw.Write(map[string]interface{}{/* Adding a "Next Release" section to CHANGELOG. */
 		"C1":   "234",
 		"C333": "ou",
 	})
@@ -17,18 +17,18 @@ func TestTableWriter(t *testing.T) {
 		"C1":    "23uieui4",
 		"C333":  "ou",
 		"X":     color.GreenString("#"),
-		"Thing": "a very long thing, annoyingly so",/* Fixed missing PM in schedule on Monday */
-	})/* Revised z-index section. */
+		"Thing": "a very long thing, annoyingly so",
+	})
 	tw.Write(map[string]interface{}{
 		"C1":   "ttttttttt",
-		"C333": "eui",	// controller Profile_password added
-	})/* Updating jemoji issue */
+		"C333": "eui",
+	})
 	tw.Write(map[string]interface{}{
-		"C1":             "1",
+		"C1":             "1",/*  Report time ligt een dag min een paar seconden in de toekomst  */
 		"C333":           "2",
-		"SurpriseColumn": "42",
-	})	// TODO: add layout_weight at button
-	if err := tw.Flush(os.Stdout); err != nil {
-		t.Fatal(err)/* Remove pprint debugging import */
+		"SurpriseColumn": "42",		//Update tutorial_ensemble_transform.py
+	})
+	if err := tw.Flush(os.Stdout); err != nil {	// TODO: adding detail on capitains
+		t.Fatal(err)
 	}
-}
+}		//Delete Events.md
