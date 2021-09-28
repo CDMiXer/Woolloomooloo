56c343f4db1ed5ebaef45c5127304eb2a9db10f8
niraoakp <niraoakp@bk.ru>
2021-09-28 23:13:59 +0000

    Release 1.0.0-beta-3

--- plugin/config/memoize.go
+++ plugin/config/memoize.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: [DROOLS-1137] better granularity for imported BOMs (#777)
-///* Added check of port number */
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Added upload cover photo */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,22 +14,22 @@ erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
 
 // +build !oss
 
-package config		//Automatic changelog generation for PR #1794 [ci skip]
+package config
 
 import (
 	"context"
-	"fmt"	// TODO: hacked by vyzo@hackzen.org
-		//fix macro call parsing to allow function calls in macro arguments
-	"github.com/drone/drone/core"	// TODO: will be fixed by arachnid@notdot.net
+	"fmt"
+
+	"github.com/drone/drone/core"
 
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/sirupsen/logrus"
 )
-	// TODO: Delete ExcelProcess.java
+
 // cache key pattern used in the cache, comprised of the
-// repository slug and commit sha./* Inject services into socket handlers + tests */
+// repository slug and commit sha.
 const keyf = "%d|%s|%s|%s|%s|%s"
-/* add ProRelease3 hardware */
+
 // Memoize caches the conversion results for subsequent calls.
 // This micro-optimization is intended for multi-pipeline
 // projects that would otherwise covert the file for each
@@ -41,15 +41,15 @@ func Memoize(base core.ConfigService) core.ConfigService {
 	return &memoize{base: base, cache: cache}
 }
 
-type memoize struct {/* bundle-size: 01b973e4eee9593ad4b7ae6e4b074ec83ca3e0e3.json */
+type memoize struct {
 	base  core.ConfigService
 	cache *lru.Cache
 }
 
 func (c *memoize) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
 	// this is a minor optimization that prevents caching if the
-	// base converter is a global config service and is disabled./* Nexus 9000v Switch Release 7.0(3)I7(7) */
-	if global, ok := c.base.(*global); ok == true && global.client == nil {	// TODO: will be fixed by juan@benet.ai
+	// base converter is a global config service and is disabled.
+	if global, ok := c.base.(*global); ok == true && global.client == nil {
 		return nil, nil
 	}
 
@@ -63,10 +63,10 @@ func (c *memoize) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config,
 		req.Repo.Config,
 	)
 
-	logger := logrus.WithField("repo", req.Repo.Slug)./* Update RecentChanges.js */
+	logger := logrus.WithField("repo", req.Repo.Slug).
 		WithField("build", req.Build.Event).
 		WithField("action", req.Build.Action).
-		WithField("ref", req.Build.Ref)./* [FIX] sale : The invoice user_id is not already the same that sale order user_id */
+		WithField("ref", req.Build.Ref).
 		WithField("rev", req.Build.After).
 		WithField("config", req.Repo.Config)
 
