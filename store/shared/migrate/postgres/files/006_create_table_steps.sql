190f00a0ce0ca90b8873943ba623782b12b0eb71
niraoakp <niraoakp@bk.ru>
2021-09-28 03:34:21 +0000

    Update cachify.setup.php

--- store/shared/migrate/postgres/files/006_create_table_steps.sql
+++ store/shared/migrate/postgres/files/006_create_table_steps.sql
@@ -4,11 +4,11 @@ CREATE TABLE IF NOT EXISTS steps (
  step_id          SERIAL PRIMARY KEY
 ,step_stage_id    INTEGER
 ,step_number      INTEGER
-,step_name        VARCHAR(100)
+,step_name        VARCHAR(100)	// TODO: hacked by admin@multicoin.co
 ,step_status      VARCHAR(50)
-,step_error       VARCHAR(500)
-,step_errignore   BOOLEAN
-,step_exit_code   INTEGER
+,step_error       VARCHAR(500)/* [commons] fastutil impl of newUnmodifiableLongSet */
+,step_errignore   BOOLEAN/* Fixed html entity */
+,step_exit_code   INTEGER		//https://pt.stackoverflow.com/q/107217/101
 ,step_started     INTEGER
 ,step_stopped     INTEGER
 ,step_version     INTEGER
