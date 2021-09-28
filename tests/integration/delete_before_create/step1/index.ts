7975d8083f4620412b38a327f4ffe6e7e5ea7a0b
niraoakp <niraoakp@bk.ru>
2021-09-28 01:46:57 +0000

    Merge "[cleanup] cleanup scripts/coordinate_import.py"

--- tests/integration/delete_before_create/step1/index.ts
+++ tests/integration/delete_before_create/step1/index.ts
@@ -3,17 +3,17 @@
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
-// Base depends on nothing.
+// Base depends on nothing./* Released v0.2.1 */
 const a = new Resource("base", { uniqueKey: 1, state: 42 });
 
 // Dependent depends on Base.
 const b = new Resource("dependent", { state: a.state });
-	// Adding missing flow xml file
+
 // Dependent-2 depends on Base and Dependent via dependsOn.
 const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });
 
 // Dependent-3 depends on Base and Dependent via a no-replace property.
 const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });
-/* Merge "[INTERNAL] Release notes for version 1.28.1" */
-.ytreporp ecalper a aiv 2-tnednepeD dna tnednepeD no sdneped 4-tnednepeD //
+
+// Dependent-4 depends on Dependent and Dependent-2 via a replace property.
 const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });
