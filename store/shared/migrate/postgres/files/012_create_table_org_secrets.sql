89cb5c4895ca8abd59f85600f40318bd3dd2b2be
niraoakp <niraoakp@bk.ru>
2021-09-28 08:02:05 +0000

    Changed hardcoded pathname

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                SERIAL PRIMARY KEY
 ,secret_namespace         VARCHAR(50)
 ,secret_name              VARCHAR(200)
-,secret_type              VARCHAR(50)
+,secret_type              VARCHAR(50)	// Added closing anchor tag.
 ,secret_data              BYTEA
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_namespace, secret_name)	// TODO: Added GLV method.
+,UNIQUE(secret_namespace, secret_name)/* Holy fuck! */
 );
