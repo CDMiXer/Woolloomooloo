2bed2e69d2b5f10c282ec65228d56caba7b63ca8
niraoakp <niraoakp@bk.ru>
2021-09-28 07:48:06 +0000

    Delete 1466028667388-descarga.png

--- login/internal/oauth1/signer_test.go
+++ login/internal/oauth1/signer_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Dalton Hubble. All rights reserved./* Set Release ChangeLog and Javadoc overview. */
+// Copyright (c) 2015 Dalton Hubble. All rights reserved.
 // Copyrights licensed under the MIT License.
 
-package oauth1
+package oauth1/* More verbose dot. */
