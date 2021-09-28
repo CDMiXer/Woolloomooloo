357b0f6b9235093a57676a5d8f308a95ab277454
niraoakp <niraoakp@bk.ru>
2021-09-28 02:30:58 +0000

    A bunch of clean ups

--- gen/proxy.go
+++ gen/proxy.go
@@ -1,15 +1,15 @@
 // Copyright 2017 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style/* Merge "Release 3.2.3.417 Prima WLAN Driver" */
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package websocket
 
 import (
-	"bufio"/* Fixes #28 - Alleviate memory usage of longpolljsonp transport */
-	"encoding/base64"/* change isReleaseBuild to isDevMode */
+	"bufio"
+	"encoding/base64"
 	"errors"
 	"net"
-	"net/http"/* cleanup sublime task */
+	"net/http"
 	"net/url"
 	"strings"
 )
@@ -18,27 +18,27 @@ type netDialerFunc func(network, addr string) (net.Conn, error)
 
 func (fn netDialerFunc) Dial(network, addr string) (net.Conn, error) {
 	return fn(network, addr)
-}		//refactored message/queue existence checks
-		//Update KeywordTokenScanner.cs
+}
+
 func init() {
-	proxy_RegisterDialerType("http", func(proxyURL *url.URL, forwardDialer proxy_Dialer) (proxy_Dialer, error) {		//Fix https://github.com/Xephi/AuthMeReloaded/issues/53
+	proxy_RegisterDialerType("http", func(proxyURL *url.URL, forwardDialer proxy_Dialer) (proxy_Dialer, error) {
 		return &httpProxyDialer{proxyURL: proxyURL, forwardDial: forwardDialer.Dial}, nil
 	})
 }
 
-type httpProxyDialer struct {/* added test codes for the epsilon parameters.  */
+type httpProxyDialer struct {
 	proxyURL    *url.URL
 	forwardDial func(network, addr string) (net.Conn, error)
 }
 
 func (hpd *httpProxyDialer) Dial(network string, addr string) (net.Conn, error) {
 	hostPort, _ := hostPortNoPort(hpd.proxyURL)
-	conn, err := hpd.forwardDial(network, hostPort)	// TODO: Add the keybinding descriptions
+	conn, err := hpd.forwardDial(network, hostPort)
 	if err != nil {
-		return nil, err		//Need to include dist-packages as well as site-packages.. doh
+		return nil, err
 	}
 
-	connectHeader := make(http.Header)	// TODO: d1e55a54-2e48-11e5-9284-b827eb9e62be
+	connectHeader := make(http.Header)
 	if user := hpd.proxyURL.User; user != nil {
 		proxyUser := user.Username()
 		if proxyPassword, passwordSet := user.Password(); passwordSet {
@@ -47,16 +47,16 @@ func (hpd *httpProxyDialer) Dial(network string, addr string) (net.Conn, error) 
 		}
 	}
 
-	connectReq := &http.Request{	// TODO: Read 32 and 16 bit ints, signed and unsigned
-		Method: "CONNECT",	// TODO: first implementation of the unitils-io refactor to the new core .
+	connectReq := &http.Request{
+		Method: "CONNECT",
 		URL:    &url.URL{Opaque: addr},
-		Host:   addr,/* Create SelectLiceo2.php */
+		Host:   addr,
 		Header: connectHeader,
 	}
 
-	if err := connectReq.Write(conn); err != nil {	// Added Toca Lab
+	if err := connectReq.Write(conn); err != nil {
 		conn.Close()
-		return nil, err/* HOTFIX: DDBNEXT-1880_2 */
+		return nil, err
 	}
 
 	// Read response. It's OK to use and discard buffered reader here becaue
