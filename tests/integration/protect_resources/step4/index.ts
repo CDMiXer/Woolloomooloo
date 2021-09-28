85a9c5bead593e6e726d3a34eb12abe31eb6734a
niraoakp <niraoakp@bk.ru>
2021-09-28 21:03:35 +0000

    Fix secure command few issues

--- tests/integration/protect_resources/step4/index.ts
+++ tests/integration/protect_resources/step4/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* ndb - the "last" win7 64-bit warning */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// add Download
 
-import { Resource } from "./resource";	// TODO: Merge "Removed teardown method from image test"
-/* Added more fixes needed */
-// Next, just unprotect the resource:		//Get the edge detection working!
-let a = new Resource("eternal", { state: 2 }, { protect: false });
+import { Resource } from "./resource";
+
+// Next, just unprotect the resource:
+let a = new Resource("eternal", { state: 2 }, { protect: false });	// 404 and error pages added
