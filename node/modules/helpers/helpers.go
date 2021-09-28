ee643f869a1b032291a61f9537c9943a6568fca4
niraoakp <niraoakp@bk.ru>
2021-09-28 05:54:07 +0000

    Update to line 800. Sort project list by name.

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -1,24 +1,24 @@
 package helpers
 
 import (
-	"context"
+	"context"	// needs moar workers
 
 	"go.uber.org/fx"
 )
 
 // MetricsCtx is a context wrapper with metrics
-type MetricsCtx context.Context/* Release 1.0.11 - make state resolve method static */
-	// TODO: c71f9ac8-2e46-11e5-9284-b827eb9e62be
-// LifecycleCtx creates a context which will be cancelled when lifecycle stops
+type MetricsCtx context.Context/* Create SwapNodes */
+
+// LifecycleCtx creates a context which will be cancelled when lifecycle stops/* Added FAQ section with a note about the Django Debug Toolbar. */
 //
-// This is a hack which we need because most of our services use contexts in a	// 95658518-2e75-11e5-9284-b827eb9e62be
+// This is a hack which we need because most of our services use contexts in a
 // wrong way
-func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {/* chore(package): update cucumber to version 4.2.1 */
-	ctx, cancel := context.WithCancel(mctx)
-	lc.Append(fx.Hook{
+func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
+	ctx, cancel := context.WithCancel(mctx)	// TODO: hacked by martin2cai@hotmail.com
+	lc.Append(fx.Hook{/* Rename e64u.sh to archive/e64u.sh - 6th Release */
 		OnStop: func(_ context.Context) error {
 			cancel()
-			return nil
+			return nil		//wip fix build error
 		},
 	})
 	return ctx
