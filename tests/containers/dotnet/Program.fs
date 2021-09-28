2991e5be5b57433e3cbc4c171e6ab91c3b3f74c0
niraoakp <niraoakp@bk.ru>
2021-09-28 07:27:53 +0000

    Merge "Convert event timestamp to PrecisionTimestamp"

--- tests/containers/dotnet/Program.fs
+++ tests/containers/dotnet/Program.fs
@@ -1,16 +1,16 @@
-﻿module Program		//aa693b82-2e41-11e5-9284-b827eb9e62be
+﻿module Program
 
-open System	// TODO: hacked by lexy8russo@outlook.com
+open System
 open Pulumi.FSharp
 
 let infra () =
   let config = new Pulumi.Config()
-  let runtime = config.Get("runtime")	// TODO: Removing jquery dependency from harness.js
+  let runtime = config.Get("runtime")
   Console.WriteLine("Hello from {0}", runtime)
   
   // Stack outputs
   dict []
 
 [<EntryPoint>]
-let main _ =/* Release version: 1.9.0 */
+let main _ =
   Deployment.run infra
