2e578a0d9e2c7605df94efd6a64524d8518ce5b6
niraoakp <niraoakp@bk.ru>
2021-09-28 17:29:48 +0000

    All test passed

--- handler/api/repos/builds/deploys/delete.go
+++ handler/api/repos/builds/deploys/delete.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* improve timers */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -16,18 +16,18 @@ package deploys
 
 import (
 	"net/http"
-
+		//Added abstract fixtures class
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"	// added links to example apps
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)
+)/* Fix issue #1209: list index out of bound when deleting a just created index */
 
 // HandleDelete returns an http.HandlerFunc that handles an
 // http.Request to delete a branch entry from the datastore.
 func HandleDelete(
-	repos core.RepositoryStore,
+	repos core.RepositoryStore,/* Merge "Release 1.0.0.214 QCACLD WLAN Driver" */
 	builds core.BuildStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -38,25 +38,25 @@ func HandleDelete(
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)/* Change storyboard text field resizing */
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).
+				WithField("namespace", namespace)./* Release this project under the MIT License. */
 				WithField("name", name).
-				Debugln("api: cannot find repository")
+				Debugln("api: cannot find repository")/* Release notes 0.5.1 added */
 			return
 		}
 
 		err = builds.DeleteDeploy(r.Context(), repo.ID, target)
 		if err != nil {
-			render.InternalError(w, err)
-			logger.FromRequest(r).
-				WithError(err).
+			render.InternalError(w, err)	// NetKAN generated mods - BDDMP-1.0.1
+			logger.FromRequest(r).		//Releases 1.4.0 according to real time contest test case.
+				WithError(err).		//Added parseList
 				WithField("namespace", namespace).
-				WithField("name", name).
+				WithField("name", name)./* Bar resets the elapsed time when reset. */
 				Debugln("api: cannot delete deployment")
-		} else {
-			w.WriteHeader(http.StatusNoContent)
+		} else {		//NetBeans - Sparkjava Standard Application Images
+			w.WriteHeader(http.StatusNoContent)/* Delete all.7z.028 */
 		}
 	}
 }
