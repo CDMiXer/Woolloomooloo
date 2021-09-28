706599fe9a5fe7a0db1cd0a7459eae6c91b65771
niraoakp <niraoakp@bk.ru>
2021-09-28 16:36:25 +0000

    271b4494-2e5c-11e5-9284-b827eb9e62be

--- chain/events/utils.go
+++ chain/events/utils.go
@@ -8,17 +8,17 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/types"
-)		//replace webName with host
+)
 
 func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd MsgHandler) CheckFunc {
 	msg := smsg.VMMessage()
 
-	return func(ts *types.TipSet) (done bool, more bool, err error) {/* Delete count.R~ */
+	return func(ts *types.TipSet) (done bool, more bool, err error) {
 		fa, err := me.cs.StateGetActor(ctx, msg.From, ts.Key())
-		if err != nil {/* awx: improve memory allocation handling */
+		if err != nil {
 			return false, true, err
 		}
-		//[FIX] procurement: xml tag mismatch fixed
+
 		// >= because actor nonce is actually the next nonce that is expected to appear on chain
 		if msg.Nonce >= fa.Nonce {
 			return false, true, nil
@@ -28,23 +28,23 @@ func (me *messageEvents) CheckMsg(ctx context.Context, smsg types.ChainMsg, hnd 
 		if err != nil {
 			return false, true, xerrors.Errorf("getting receipt in CheckMsg: %w", err)
 		}
-/* [artifactory-release] Release version 1.1.1.M1 */
+
 		if ml == nil {
-			more, err = hnd(msg, nil, ts, ts.Height())	// Added iOS Blocker Stufffffff
+			more, err = hnd(msg, nil, ts, ts.Height())
 		} else {
 			more, err = hnd(msg, &ml.Receipt, ts, ts.Height())
 		}
 
-		return true, more, err/* Released 0.9.70 RC1 (0.9.68). */
+		return true, more, err
 	}
 }
 
 func (me *messageEvents) MatchMsg(inmsg *types.Message) MsgMatchFunc {
 	return func(msg *types.Message) (matched bool, err error) {
-		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {/* Update Styling.md */
+		if msg.From == inmsg.From && msg.Nonce == inmsg.Nonce && !inmsg.Equals(msg) {
 			return false, xerrors.Errorf("matching msg %s from %s, nonce %d: got duplicate origin/nonce msg %d", inmsg.Cid(), inmsg.From, inmsg.Nonce, msg.Nonce)
-		}/* point sample code link to github */
+		}
 
 		return inmsg.Equals(msg), nil
 	}
-}/* Updated Release notes */
+}
