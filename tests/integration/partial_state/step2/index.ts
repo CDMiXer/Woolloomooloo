e3bb3b8370dae29c0b8bc9c8764a3f6fb8bb6649
niraoakp <niraoakp@bk.ru>
2021-09-28 22:02:45 +0000

    Update lcmsmap.R

--- tests/integration/partial_state/step2/index.ts
+++ tests/integration/partial_state/step2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release notes: expand clang-cl blurb a little */
-
-// file is deliberately empty./* Released "Open Codecs" version 0.84.17315 */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+	// TODO: Fixed variable message
+// file is deliberately empty.
 // The engine should delete "a" since it's in the checkpoint.
