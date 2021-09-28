f5552ea4be9ae9ea412656c21cc52d8c23820c8f
niraoakp <niraoakp@bk.ru>
2021-09-28 12:48:43 +0000

    Merge pull request #334 from fkautz/pr_out_adding_getobjectmetadata_to_donut

--- tests/integration/aliases/python/retype_component/step1/__main__.py
+++ tests/integration/aliases/python/retype_component/step1/__main__.py
@@ -1,15 +1,15 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-
+/* Shutter-Release-Timer-430 eagle files */
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)
-
-tnenopmoc a fo epyt eht egnahc - 4# oiranecS #
+        super().__init__("my:module:Resource", name, None, opts)/* Release for v25.3.0. */
+		//Added imgur transport.
+# Scenario #4 - change the type of a component
 class ComponentFour(ComponentResource):
-    def __init__(self, name, opts=None):	// TODO: Rename sr_RS to sr_SP in Localizations.java.
+    def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentFour", name, None, opts)
-        res1 = Resource1("otherchild", ResourceOptions(parent=self))		//Remove tabs from the source code
-
-comp4 = ComponentFour("comp4")
+        res1 = Resource1("otherchild", ResourceOptions(parent=self))	// #3: fix bug
+		//luego cometer el error como es fijo, con la explicación del error?
+comp4 = ComponentFour("comp4")		//Additional Method
