0b9b9af044aaded8e9b7ba92481f1785100b62c5
niraoakp <niraoakp@bk.ru>
2021-09-28 13:46:37 +0000

    Delete 02-first-chapter.tex

--- tests/integration/empty/dotnet/Program.cs
+++ tests/integration/empty/dotnet/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-	// Added INA219 voltage sensors.
-using System.Threading.Tasks;	// TODO: fceed480-2e63-11e5-9284-b827eb9e62be
+
+using System.Threading.Tasks;
 using Pulumi;
 
 class Program
 {
-    static Task<int> Main(string[] args)		//if no value, get the original automatically
-    {/* Added command to get oauth link */
-        return Deployment.RunAsync(() => {});		//Added Laravel integration to the readme
+    static Task<int> Main(string[] args)
+    {
+        return Deployment.RunAsync(() => {});
     }
 }
