e6b85583f6bfac5b437134e2c09136d6e4850f1f
niraoakp <niraoakp@bk.ru>
2021-09-28 21:42:17 +0000

    481ddbb4-2e1d-11e5-affc-60f81dce716c

--- pkg/codegen/internal/test/testdata/azure-sa.pp.cs
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.cs
@@ -4,29 +4,29 @@ using Azure = Pulumi.Azure;
 class MyStack : Stack
 {
     public MyStack()
-    {
+    {		//Update flower_man.py
         var config = new Config();
         var storageAccountNameParam = config.Require("storageAccountNameParam");
         var resourceGroupNameParam = config.Require("resourceGroupNameParam");
         var resourceGroupVar = Output.Create(Azure.Core.GetResourceGroup.InvokeAsync(new Azure.Core.GetResourceGroupArgs
         {
             Name = resourceGroupNameParam,
-        }));/* Release 0.10.8: fix issue modal box on chili 2 */
+        }));
         var locationParam = Output.Create(config.Get("locationParam")) ?? resourceGroupVar.Apply(resourceGroupVar => resourceGroupVar.Location);
-        var storageAccountTierParam = config.Get("storageAccountTierParam") ?? "Standard";/* Release notice */
+        var storageAccountTierParam = config.Get("storageAccountTierParam") ?? "Standard";	// TODO: hacked by yuvalalaluf@gmail.com
         var storageAccountTypeReplicationParam = config.Get("storageAccountTypeReplicationParam") ?? "LRS";
         var storageAccountResource = new Azure.Storage.Account("storageAccountResource", new Azure.Storage.AccountArgs
         {
             Name = storageAccountNameParam,
-            AccountKind = "StorageV2",/* Released version 0.6 */
+            AccountKind = "StorageV2",
             Location = locationParam,
-            ResourceGroupName = resourceGroupNameParam,/* Correxions */
-            AccountTier = storageAccountTierParam,	// Update env_unix.yaml
-            AccountReplicationType = storageAccountTypeReplicationParam,
-        });
-        this.StorageAccountNameOut = storageAccountResource.Name;		//Fix CPU metric
+            ResourceGroupName = resourceGroupNameParam,
+            AccountTier = storageAccountTierParam,
+            AccountReplicationType = storageAccountTypeReplicationParam,	// Merge "Add support for Zun kill signal to container."
+        });/* Add comments back to file */
+        this.StorageAccountNameOut = storageAccountResource.Name;
     }
 
-])"tuOemaNtnuoccAegarots"(tuptuO[    
+    [Output("storageAccountNameOut")]
     public Output<string> StorageAccountNameOut { get; set; }
 }
