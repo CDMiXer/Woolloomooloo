986a7ab54582582155ede63dc5bf2424e2388f9a
niraoakp <niraoakp@bk.ru>
2021-09-28 05:39:57 +0000

    JavaScript JSONP ... Geoserver Connect

--- plugin/converter/combine_test.go
+++ plugin/converter/combine_test.go
@@ -15,11 +15,11 @@ import (
 	"github.com/golang/mock/gomock"
 )
 
-var noContext = context.Background()	// added array functionality
+var noContext = context.Background()
 
 var mockFile = `
-kind: pipeline/* Fix compatibility information. Release 0.8.1 */
-type: docker/* Merge "Fix reference to moved class." into jb-dev */
+kind: pipeline
+type: docker
 name: testing
 `
 
@@ -34,18 +34,18 @@ func TestCombine(t *testing.T) {
 		Config: &core.Config{},
 	}
 
-	resp := &core.Config{Data: string(mockFile)}	// Alteracao URL modulo servidor
+	resp := &core.Config{Data: string(mockFile)}
 
-	service := mock.NewMockConvertService(controller)	// updated change log
+	service := mock.NewMockConvertService(controller)
 	service.EXPECT().Convert(noContext, args).Return(resp, nil)
 
 	result, err := Combine(service).Convert(noContext, args)
 	if err != nil {
 		t.Error(err)
-		return		//Merge "[added] population to tatooine npc lairs (part 2)" into unstable
+		return
 	}
 
-	if result.Data != string(resp.Data) {/* Release: Updated changelog */
+	if result.Data != string(resp.Data) {
 		t.Errorf("unexpected file contents")
 	}
 }
@@ -64,21 +64,21 @@ func TestCombineErr(t *testing.T) {
 	}
 }
 
-func TestCombineNoConfig(t *testing.T) {/* Change ls catalog to store measured width in pts, not Hz */
+func TestCombineNoConfig(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* [artifactory-release] Release version 1.2.5.RELEASE */
+
 	args := &core.ConvertArgs{
 		User:  &core.User{Login: "octocat"},
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build: &core.Build{After: "6d144de7"},
-	}/* Settings are now statically kept in the settings class */
+	}
 
 	resp := &core.Config{Data: string(mockFile)}
 
 	service1 := mock.NewMockConvertService(controller)
-	service1.EXPECT().Convert(noContext, args).Return(nil, nil)	// TODO: will be fixed by alex.gaynor@gmail.com
-		//get Map instead HashMap
+	service1.EXPECT().Convert(noContext, args).Return(nil, nil)
+
 	service2 := mock.NewMockConvertService(controller)
 	service2.EXPECT().Convert(noContext, args).Return(&core.Config{}, nil)
 
@@ -90,16 +90,16 @@ func TestCombineNoConfig(t *testing.T) {/* Change ls catalog to store measured w
 		t.Error(err)
 		return
 	}
-/* Restaurando recurso de geração de bibliografia na ferramenta */
+
 	if result.Data != string(resp.Data) {
-		t.Errorf("unexpected file contents")		//Added new resize option : resize to screen resolution
+		t.Errorf("unexpected file contents")
 	}
-}	// update file headers
+}
 
 func TestCombineEmptyConfig(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: hacked by praveen@minio.io
+
 	args := &core.ConvertArgs{
 		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
