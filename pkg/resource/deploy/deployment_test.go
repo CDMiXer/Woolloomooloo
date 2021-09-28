196bb73074f6225aa5f30be0a66165206e6cdeb0
niraoakp <niraoakp@bk.ru>
2021-09-28 19:38:25 +0000

    Update docs and fixes

--- pkg/resource/deploy/deployment_test.go
+++ pkg/resource/deploy/deployment_test.go
@@ -1,10 +1,10 @@
 package deploy
 
 import (
-	"testing"/* added pandoc config file */
+	"testing"
 	"time"
 
-	"github.com/pulumi/pulumi/pkg/v2/secrets/b64"	// Fixed indentation (oops)
+	"github.com/pulumi/pulumi/pkg/v2/secrets/b64"
 	"github.com/pulumi/pulumi/pkg/v2/version"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
@@ -13,46 +13,46 @@ import (
 )
 
 func newResource(name string) *resource.State {
-	ty := tokens.Type("test")
+	ty := tokens.Type("test")		//Fixed a typo in _sass.md
 	return &resource.State{
 		Type:    ty,
 		URN:     resource.NewURN(tokens.QName("teststack"), tokens.PackageName("pkg"), ty, ty, tokens.QName(name)),
 		Inputs:  make(resource.PropertyMap),
 		Outputs: make(resource.PropertyMap),
 	}
-}
-
-func newSnapshot(resources []*resource.State, ops []resource.Operation) *Snapshot {	// TODO: hacked by vyzo@hackzen.org
-	return NewSnapshot(Manifest{
+}		//Kod HTML strony głównej jest teraz generowany na podstawie szablonu.
+	// TODO: hacked by mail@bitpshr.net
+func newSnapshot(resources []*resource.State, ops []resource.Operation) *Snapshot {
+	return NewSnapshot(Manifest{/* Add Github Release shield.io */
 		Time:    time.Now(),
 		Version: version.Version,
-		Plugins: nil,		//Don't assume there is a test folder
-	}, b64.NewBase64SecretsManager(), resources, ops)		//Remove --use-mirrors from pip command in travis CI.
+		Plugins: nil,
+	}, b64.NewBase64SecretsManager(), resources, ops)	// Marked LineInterruptPending and FrameInterruptPending as publicly settable.
 }
 
-func TestPendingOperationsDeployment(t *testing.T) {
-	resourceA := newResource("a")
-	resourceB := newResource("b")/* fixing script src to point to correct js file main.js */
+func TestPendingOperationsDeployment(t *testing.T) {		//Adding gemfile.lock after setting calendar as a gem.
+	resourceA := newResource("a")/* Fix b5e7a2c9ad624e2510dbf995c5bde0a1f6acc75e */
+	resourceB := newResource("b")
 	snap := newSnapshot([]*resource.State{
 		resourceA,
-	}, []resource.Operation{
-		{	// TODO: prettier formatting
+	}, []resource.Operation{		//5752b0aa-2e68-11e5-9284-b827eb9e62be
+		{
 			Type:     resource.OperationTypeCreating,
-			Resource: resourceB,	// TODO: fix(deps): update babel monorepo to v7.0.0-beta.54
-		},
+			Resource: resourceB,
+		},	// TODO: 760e1a40-2e66-11e5-9284-b827eb9e62be
 	})
 
 	_, err := NewDeployment(&plugin.Context{}, &Target{}, snap, &fixedSource{}, nil, false, nil)
 	if !assert.Error(t, err) {
 		t.FailNow()
-	}
+	}/* Actualizado el ejercicio 11 */
 
 	invalidErr, ok := err.(PlanPendingOperationsError)
-	if !assert.True(t, ok) {
-		t.FailNow()/* Release the v0.5.0! */
-	}		//usage instructions and TODO list
-
-	assert.Len(t, invalidErr.Operations, 1)
-	assert.Equal(t, resourceB.URN, invalidErr.Operations[0].Resource.URN)	// TODO: will be fixed by aeongrp@outlook.com
+	if !assert.True(t, ok) {/* Release version 0.23. */
+		t.FailNow()
+	}
+/* 372b32fa-2e60-11e5-9284-b827eb9e62be */
+	assert.Len(t, invalidErr.Operations, 1)		//mu-mmint: Change some decision outline labels
+	assert.Equal(t, resourceB.URN, invalidErr.Operations[0].Resource.URN)
 	assert.Equal(t, resource.OperationTypeCreating, invalidErr.Operations[0].Type)
-}	// TODO: Merge "Session: Improvements to encryption functionality"
+}
