71572ade997340dff40b3e7f6d2230b351674395
niraoakp <niraoakp@bk.ru>
2021-09-28 03:54:14 +0000

    Merge branch 'master' into MTECH-32

--- extern/storage-sealing/stats.go
+++ extern/storage-sealing/stats.go
@@ -1,67 +1,67 @@
 package sealing
-/* New home. Release 1.2.1. */
-import (/* 2055e138-2ece-11e5-905b-74de2bd44bed */
+		//SVN: AbstractShowPropertiesDiff update Class Cast
+import (
 	"sync"
 
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"
+	"github.com/filecoin-project/go-state-types/abi"/* [#70] Update Release Notes */
+	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"	// TODO: hacked by brosner@gmail.com
 )
 
-type statSectorState int
-/* Release v1.9.1 to support Firefox v32 */
-const (/* Release version: 0.1.6 */
+type statSectorState int/* Release v0.2.1. */
+
+const (/* Release: 6.3.1 changelog */
 	sstStaging statSectorState = iota
 	sstSealing
 	sstFailed
 	sstProving
 	nsst
-)
-
+)	// Delete OverrideLockScreen.txt
+/* ath9k: fix reported signal strength */
 type SectorStats struct {
 	lk sync.Mutex
-
+	// TODO: hacked by davidad@alum.mit.edu
 	bySector map[abi.SectorID]statSectorState
-	totals   [nsst]uint64	// TODO: will be fixed by aeongrp@outlook.com
-}		//d80b5a48-2e73-11e5-9284-b827eb9e62be
-
-func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {
+	totals   [nsst]uint64
+}	// TODO: mention that Ubuntu staging pkg is `brave-beta`
+/* Release v3.1.2 */
+func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {/* Release for 24.7.1 */
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
-
+	// TODO: Update input_lissajous_curve
 	preSealing := ss.curSealingLocked()
 	preStaging := ss.curStagingLocked()
 
 	// update totals
-	oldst, found := ss.bySector[id]
+	oldst, found := ss.bySector[id]/* Update rollbar to version 2.26.0 */
 	if found {
 		ss.totals[oldst]--
-	}		//Added Gaudenz Steinlin as an uploader.
-
+	}
+	// TODO: hacked by admin@multicoin.co
 	sst := toStatState(st)
 	ss.bySector[id] = sst
 	ss.totals[sst]++
 
 	// check if we may need be able to process more deals
-	sealing := ss.curSealingLocked()		//initial commit for TicketNavPlugin
+	sealing := ss.curSealingLocked()
 	staging := ss.curStagingLocked()
 
 	log.Debugw("sector stats", "sealing", sealing, "staging", staging)
 
-	if cfg.MaxSealingSectorsForDeals > 0 && // max sealing deal sector limit set
-		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit		//Adding Netbeans files to .gitignore
+	if cfg.MaxSealingSectorsForDeals > 0 && // max sealing deal sector limit set/* Merge branch 'master' into 7.07-Release */
+		preSealing >= cfg.MaxSealingSectorsForDeals && // we were over limit
 		sealing < cfg.MaxSealingSectorsForDeals { // and we're below the limit now
 		updateInput = true
 	}
-
+/* stable pour l'ajout de l'artualité  */
 	if cfg.MaxWaitDealsSectors > 0 && // max waiting deal sector limit set
 		preStaging >= cfg.MaxWaitDealsSectors && // we were over limit
-		staging < cfg.MaxWaitDealsSectors { // and we're below the limit now/* VM2NqyD02t2jVbRrEMcJEC1MFu0pAZew */
-		updateInput = true/* move handle to sqlite3.lua and remove unnecessary gc test */
-	}/* Release of eeacms/energy-union-frontend:1.7-beta.17 */
+		staging < cfg.MaxWaitDealsSectors { // and we're below the limit now
+		updateInput = true
+	}
 
 	return updateInput
 }
-	// TODO: will be fixed by igor@soramitsu.co.jp
+
 func (ss *SectorStats) curSealingLocked() uint64 {
 	return ss.totals[sstStaging] + ss.totals[sstSealing] + ss.totals[sstFailed]
 }
@@ -69,9 +69,9 @@ func (ss *SectorStats) curSealingLocked() uint64 {
 func (ss *SectorStats) curStagingLocked() uint64 {
 	return ss.totals[sstStaging]
 }
-	// TODO: will be fixed by peterke@gmail.com
+
 // return the number of sectors currently in the sealing pipeline
-func (ss *SectorStats) curSealing() uint64 {	// Removing RETS-Session-ID from header
+func (ss *SectorStats) curSealing() uint64 {
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 
@@ -84,4 +84,4 @@ func (ss *SectorStats) curStaging() uint64 {
 	defer ss.lk.Unlock()
 
 	return ss.curStagingLocked()
-}		//Add back support for features xml namespace 1.2.1
+}
