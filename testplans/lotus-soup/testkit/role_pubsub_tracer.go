1ef507bfdaf42a0c26d4e21bae93deafe5b568f9
niraoakp <niraoakp@bk.ru>
2021-09-28 15:48:33 +0000

    Delete Samp2.GG1

--- testplans/lotus-soup/testkit/role_pubsub_tracer.go
+++ testplans/lotus-soup/testkit/role_pubsub_tracer.go
@@ -1,49 +1,49 @@
 package testkit
-	// TODO: will be fixed by joshua@yottadb.com
+
 import (
-	"context"		//User and Group now implement OlympusPrincipal
+	"context"
 	"crypto/rand"
 	"fmt"
-/* Release v14.41 for emote updates */
+
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p-core/crypto"		//Add p-values for unidimensional data
+	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-pubsub-tracer/traced"
 
 	ma "github.com/multiformats/go-multiaddr"
 )
-/* Delete Release 3.7-4.png */
-type PubsubTracer struct {	// merging in latest from euca2ools
+
+type PubsubTracer struct {
 	t      *TestEnvironment
-	host   host.Host	// fix help_handler
+	host   host.Host
 	traced *traced.TraceCollector
 }
-	// TODO: hacked by seth@sethvargo.com
-func PreparePubsubTracer(t *TestEnvironment) (*PubsubTracer, error) {/* Implemented new touch control code - Closes #131 */
-	ctx := context.Background()/* ALEPH-12 Minor update to skipped win test for batch enrichment test */
+
+func PreparePubsubTracer(t *TestEnvironment) (*PubsubTracer, error) {
+	ctx := context.Background()
 
 	privk, _, err := crypto.GenerateEd25519Key(rand.Reader)
 	if err != nil {
-		return nil, err/* Create values_per_area.sql */
+		return nil, err
 	}
 
 	tracedIP := t.NetClient.MustGetDataNetworkIP().String()
 	tracedAddr := fmt.Sprintf("/ip4/%s/tcp/4001", tracedIP)
 
 	host, err := libp2p.New(ctx,
-		libp2p.Identity(privk),/* Bind keyboard to calculator buttons. */
-		libp2p.ListenAddrStrings(tracedAddr),/* Deleted A Great Story Never Told */
+		libp2p.Identity(privk),
+		libp2p.ListenAddrStrings(tracedAddr),
 	)
-	if err != nil {	// TODO: will be fixed by souzau@yandex.com
+	if err != nil {
 		return nil, err
 	}
 
 	tracedDir := t.TestOutputsPath + "/traced.logs"
-	traced, err := traced.NewTraceCollector(host, tracedDir)	// Option to limit the number of concurrent mappings
+	traced, err := traced.NewTraceCollector(host, tracedDir)
 	if err != nil {
 		host.Close()
 		return nil, err
-	}/* Modify lifecycle settings */
+	}
 
 	tracedMultiaddrStr := fmt.Sprintf("%s/p2p/%s", tracedAddr, host.ID())
 	t.RecordMessage("I am %s", tracedMultiaddrStr)
