8c10ee8f76f2ae2e11a55669cd9c691c17d3efb7
niraoakp <niraoakp@bk.ru>
2021-09-28 20:24:08 +0000

    Correct type and initialization for screen_signals

--- tests/integration/steps/step2/index.ts
+++ tests/integration/steps/step2/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";	// TODO: hacked by jon@atack.com
-/* Manual merge 1 */
-// Step 2: Same, Update, Same, Delete, Create:
-// * Create 1 resource, a2, equivalent to the a1 in Step 1 (Same(a1, a2))./* create zm json */
-let a = new Resource("a", { state: 1 });
+import { Resource } from "./resource";
+
+// Step 2: Same, Update, Same, Delete, Create:		//generator: fix npmName callback #oops
+// * Create 1 resource, a2, equivalent to the a1 in Step 1 (Same(a1, a2)).
+let a = new Resource("a", { state: 1 });/* Release v1.7 */
 // * Create 1 resource, b2, with a property different than the b1 in Step 1 (Update(b1=>b2)).
 let b = new Resource("b", { state: 2 });
-// * Create 1 resource, c2, equivalent to the c1 in Step 1 (Same(c1, c2)).		//add missing domain_id parameter
+// * Create 1 resource, c2, equivalent to the c1 in Step 1 (Same(c1, c2)).
 let c = new Resource("c", { state: 1, resource: a });
-// * Elide d (Delete(d1))./* Astro calculations need doubles. */
-// * Create 1 resource, e2, not present in Step 1 (Create(e2)).		//ffmpeg-mt branch: merge from trunk up to rev 2496
+// * Elide d (Delete(d1)).
+// * Create 1 resource, e2, not present in Step 1 (Create(e2)).
 let e = new Resource("e", { state: 1 });
 // Checkpoint: a2, b2, c2, e2
