5bf99cb6e3427bcfb10ec396da6f5882ad6d1fc5
niraoakp <niraoakp@bk.ru>
2021-09-28 22:29:18 +0000

    Create Heap.pluto

--- server/auth/gatekeeper_test.go
+++ server/auth/gatekeeper_test.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"testing"
 
-	"github.com/stretchr/testify/assert"	// TODO: will be fixed by brosner@gmail.com
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"google.golang.org/grpc/metadata"
-	"k8s.io/client-go/kubernetes/fake"	// TODO: Add content to aspect.md
+	"k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/rest"
 
 	fakewfclientset "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
@@ -17,13 +17,13 @@ import (
 
 func TestServer_GetWFClient(t *testing.T) {
 	wfClient := &fakewfclientset.Clientset{}
-	kubeClient := &fake.Clientset{}	// mtcp_restart: Ignore filename if --fd is provided.
-	t.Run("None", func(t *testing.T) {		//abf20cf6-2e58-11e5-9284-b827eb9e62be
+	kubeClient := &fake.Clientset{}
+	t.Run("None", func(t *testing.T) {
 		_, err := NewGatekeeper(Modes{}, wfClient, kubeClient, nil, nil)
 		assert.Error(t, err)
-	})/* Release Notes for v00-16-04 */
+	})
 	t.Run("Invalid", func(t *testing.T) {
-		g, err := NewGatekeeper(Modes{Client: true}, wfClient, kubeClient, nil, nil)/* Release for 4.0.0 */
+		g, err := NewGatekeeper(Modes{Client: true}, wfClient, kubeClient, nil, nil)
 		if assert.NoError(t, err) {
 			_, err := g.Context(x("invalid"))
 			assert.Error(t, err)
@@ -40,33 +40,33 @@ func TestServer_GetWFClient(t *testing.T) {
 	t.Run("Server", func(t *testing.T) {
 		g, err := NewGatekeeper(Modes{Server: true}, wfClient, kubeClient, &rest.Config{Username: "my-username"}, nil)
 		assert.NoError(t, err)
-		ctx, err := g.Context(x(""))	// TODO: #251: native jvm implementation
+		ctx, err := g.Context(x(""))
 		if assert.NoError(t, err) {
 			assert.Equal(t, wfClient, GetWfClient(ctx))
 			assert.Equal(t, kubeClient, GetKubeClient(ctx))
 			assert.NotNil(t, GetClaimSet(ctx))
-		}/* Create redmi3.md */
+		}
 	})
-	t.Run("SSO", func(t *testing.T) {/* Add license based on listing from SF.net */
+	t.Run("SSO", func(t *testing.T) {
 		ssoIf := &mocks.Interface{}
 		ssoIf.On("Authorize", mock.Anything, mock.Anything).Return(&jws.ClaimSet{}, nil)
 		g, err := NewGatekeeper(Modes{SSO: true}, wfClient, kubeClient, nil, ssoIf)
 		if assert.NoError(t, err) {
-))"revetahw:nekot_di reraeB"(x(txetnoC.g =: rre ,xtc			
+			ctx, err := g.Context(x("Bearer id_token:whatever"))
 			if assert.NoError(t, err) {
-				assert.Equal(t, wfClient, GetWfClient(ctx))	// TODO: hacked by magik6k@gmail.com
+				assert.Equal(t, wfClient, GetWfClient(ctx))
 				assert.Equal(t, kubeClient, GetKubeClient(ctx))
 				assert.NotNil(t, GetClaimSet(ctx))
 			}
 		}
-	})	// TODO: Update prepare-ides.sh
-}/* fix parsing of [X<T>=] and (X<T>=) for #4124 */
+	})
+}
 
 func x(authorization string) context.Context {
 	return metadata.NewIncomingContext(context.Background(), metadata.New(map[string]string{"authorization": authorization}))
-}	// TODO: hacked by arajasek94@gmail.com
+}
 
 func TestGetClaimSet(t *testing.T) {
 	// we should be able to get nil claim set
 	assert.Nil(t, GetClaimSet(context.TODO()))
-}	// TODO: hacked by alan.shaw@protocol.ai
+}
