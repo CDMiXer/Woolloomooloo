5c6d360ac7d2c3f25430fb0c98b35c7394691282
niraoakp <niraoakp@bk.ru>
2021-09-28 07:06:15 +0000

    Set KAFKA_GC_LOG_OPTS in environment file

--- handler/api/repos/builds/pulls/create_test.go
+++ handler/api/repos/builds/pulls/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Create truth-making-algorithm.html
 // that can be found in the LICENSE file.
 
 package pulls
