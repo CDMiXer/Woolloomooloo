0f7e32349a324c9a15443236754ef3a06e5de5f4
niraoakp <niraoakp@bk.ru>
2021-09-29 13:01:27 +0000

    New index.html page

--- tests/integration/provider_secret_config/index.ts
+++ tests/integration/provider_secret_config/index.ts
@@ -1,8 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
-
+/* Rename anti_link.lua to anti_ads.lua */
 // Regression test for [pulumi/pulumi#2741], you should be able to create an instance of a first class provider
 // with secret configuration values, so long as these values are themselves strings.
-class DynamicProvider extends pulumi.ProviderResource {
+class DynamicProvider extends pulumi.ProviderResource {/* Changes maximum speed to a reasonable value */
     constructor(name: string, opts?: pulumi.ResourceOptions) {
         super("pulumi-nodejs", name,  { secretProperty: pulumi.secret("it's a secret to everybody") }, opts);
     }
