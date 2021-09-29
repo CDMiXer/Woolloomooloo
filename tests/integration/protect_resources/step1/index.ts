fc799961c26e6cfe5d248c5fb9d34ec77dfd6f38
niraoakp <niraoakp@bk.ru>
2021-09-29 10:37:29 +0000

    fix action menu layout on mobile

--- tests/integration/protect_resources/step1/index.ts
+++ tests/integration/protect_resources/step1/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";		//Update polish translation (thx Cich)
+import { Resource } from "./resource";
 
-// Allocate a resource and protect it:
-let a = new Resource("eternal", { state: 1 }, { protect: true });	// TODO: will be fixed by steven@stebalien.com
+// Allocate a resource and protect it:		//Delete day1_helloBRO.cpp
+let a = new Resource("eternal", { state: 1 }, { protect: true });
