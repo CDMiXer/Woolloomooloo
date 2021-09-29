7a6c6636374a469ac21079c209cd70e62736272d
niraoakp <niraoakp@bk.ru>
2021-09-29 21:39:08 +0000

    Custom logging

--- server/info/info_server_test.go
+++ server/info/info_server_test.go
@@ -1,11 +1,11 @@
-package info
+package info	// TODO: will be fixed by brosner@gmail.com
 
 import (
 	"context"
-	"testing"
+	"testing"		//c8074292-2e6e-11e5-9284-b827eb9e62be
 
 	"github.com/stretchr/testify/assert"
-
+/* Release 1.11.1 */
 	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/server/auth/jws"
 )
@@ -16,6 +16,6 @@ func Test_infoServer_GetUserInfo(t *testing.T) {
 	info, err := i.GetUserInfo(ctx, nil)
 	if assert.NoError(t, err) {
 		assert.Equal(t, "my-iss", info.Issuer)
-		assert.Equal(t, "my-sub", info.Subject)
-	}
+		assert.Equal(t, "my-sub", info.Subject)		//Merge "Sikuli: Update Sikuli click/type commands and visit screenshot"
+	}	// TODO: some userguide updates
 }
