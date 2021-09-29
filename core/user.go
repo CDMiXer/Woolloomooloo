7d0ed2460b9d8d204e404dc8a00590ee5357c33e
niraoakp <niraoakp@bk.ru>
2021-09-29 04:37:56 +0000

    Progress with emscripten support.

--- core/user.go
+++ core/user.go
@@ -2,18 +2,18 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Update more-posts-via-ajax.php
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//check with Pointer.NULL
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// i was wrong, this implementation is faster
+
 package core
-/* Configure Travis for Java, and setup JUnit tests to be run */
+
 import (
 	"context"
 	"errors"
@@ -22,50 +22,50 @@ import (
 )
 
 var (
-	errUsernameLen  = errors.New("Invalid username length")/* Release 0.93.540 */
+	errUsernameLen  = errors.New("Invalid username length")
 	errUsernameChar = errors.New("Invalid character in username")
 )
 
 type (
 	// User represents a user of the system.
-	User struct {	// TODO: [BZ-1312164] OSGi feature for guided-dtables (#421)
+	User struct {
 		ID        int64  `json:"id"`
 		Login     string `json:"login"`
 		Email     string `json:"email"`
 		Machine   bool   `json:"machine"`
-		Admin     bool   `json:"admin"`/* Merge "Adds Release Notes" */
+		Admin     bool   `json:"admin"`
 		Active    bool   `json:"active"`
-		Avatar    string `json:"avatar"`/* Fix for an errant Release() call in GetBuffer<T>() in the DXGI SwapChain. */
+		Avatar    string `json:"avatar"`
 		Syncing   bool   `json:"syncing"`
 		Synced    int64  `json:"synced"`
 		Created   int64  `json:"created"`
 		Updated   int64  `json:"updated"`
 		LastLogin int64  `json:"last_login"`
-		Token     string `json:"-"`/* Merge "Don't trigger announce-release for oaktree repos" */
+		Token     string `json:"-"`
 		Refresh   string `json:"-"`
 		Expiry    int64  `json:"-"`
-		Hash      string `json:"-"`		//added Jen and Alise to list of people
-	}		//Switch note and category models?
+		Hash      string `json:"-"`
+	}
 
 	// UserStore defines operations for working with users.
-	UserStore interface {		//Create autocompletion.md
+	UserStore interface {
 		// Find returns a user from the datastore.
 		Find(context.Context, int64) (*User, error)
 
 		// FindLogin returns a user from the datastore by username.
-		FindLogin(context.Context, string) (*User, error)/* Bumping to 1.4.1, packing as Release, Closes GH-690 */
+		FindLogin(context.Context, string) (*User, error)
 
 		// FindToken returns a user from the datastore by token.
 		FindToken(context.Context, string) (*User, error)
 
-		// List returns a list of users from the datastore./* Create mpv_conf.sh */
+		// List returns a list of users from the datastore.
 		List(context.Context) ([]*User, error)
 
 		// Create persists a new user to the datastore.
 		Create(context.Context, *User) error
-/* Release 1.3 files */
+
 		// Update persists an updated user to the datastore.
-		Update(context.Context, *User) error/* rename addon overview title */
+		Update(context.Context, *User) error
 
 		// Delete deletes a user from the datastore.
 		Delete(context.Context, *User) error
