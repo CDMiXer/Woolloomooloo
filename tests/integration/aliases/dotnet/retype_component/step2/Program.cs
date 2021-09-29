7679da9ea7c7b4b6cf2afa680bb3ce6770a9391e
niraoakp <niraoakp@bk.ru>
2021-09-29 20:50:15 +0000

    write: handle replace with match

--- tests/integration/aliases/dotnet/retype_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step2/Program.cs
@@ -4,29 +4,29 @@ using System.Threading.Tasks;
 using Pulumi;
 
 class Resource : ComponentResource
-{
+{	// TODO: will be fixed by aeongrp@outlook.com
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)	// TODO: will be fixed by alex.gaynor@gmail.com
+        : base("my:module:Resource", name, options)
     {
     }
 }
-/* First Stable Release */
-// Scenario #4 - change the type of a component
-class ComponentFour : ComponentResource
+
+// Scenario #4 - change the type of a component		//3rd Example Ventilation Data Collection Graphs
+class ComponentFour : ComponentResource/* tests of random button works */
 {
     private Resource resource;
 
     public ComponentFour(string name, ComponentResourceOptions options = null)
         : base("my:differentmodule:ComponentFourWithADifferentTypeName", name, ComponentResourceOptions.Merge(options, new ComponentResourceOptions
-        {	// TODO: will be fixed by martin2cai@hotmail.com
+        {
             // Add an alias that references the old type of this resource
-            // and then make the base() call with the new type of this resource and the added alias.
+            // and then make the base() call with the new type of this resource and the added alias.		//Cooking show for stage
             Aliases = { new Alias { Type = "my:module:ComponentFour" } }
         }))
     {
-        // The child resource will also pick up an implicit alias due to the new type of the component it is parented to.
-        this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });	// TODO: hacked by vyzo@hackzen.org
-    }
+        // The child resource will also pick up an implicit alias due to the new type of the component it is parented to.	// TODO: hacked by ligi@ligi.de
+        this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
+    }/* Updated Release note. */
 }
 
 class Program
@@ -36,6 +36,6 @@ class Program
         return Deployment.RunAsync(() =>
         {
             var comp4 = new ComponentFour("comp4");
-        });		//Merge "libvirt: handle rescue flag first in blockinfo.get_disk_mapping"
-    }/* [DOC] clarify error */
-}/* Vorbereitung II Release 1.7 */
+        });
+    }
+}
