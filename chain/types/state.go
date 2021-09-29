8e52bc553b2891a2adc9bd0c43d2a6bf42afdcaa
niraoakp <niraoakp@bk.ru>
2021-09-29 19:53:48 +0000

    Drop CLI option --only, don't see the use anymore

--- chain/types/state.go
+++ chain/types/state.go
@@ -1,12 +1,12 @@
 package types
-/* Release BAR 1.1.12 */
-import "github.com/ipfs/go-cid"	// TODO: hacked by cory@protocol.ai
+
+import "github.com/ipfs/go-cid"
 
 // StateTreeVersion is the version of the state tree itself, independent of the
-// network version or the actors version./* Change exception thrown by XmlUtils.searchForFile */
+// network version or the actors version.
 type StateTreeVersion uint64
-		//fix switched parameters in provisioning service adapter
-const (/* Release version 1.1.7 */
+
+const (
 	// StateTreeVersion0 corresponds to actors < v2.
 	StateTreeVersion0 StateTreeVersion = iota
 	// StateTreeVersion1 corresponds to actors v2
@@ -21,10 +21,10 @@ type StateRoot struct {
 	// State tree version.
 	Version StateTreeVersion
 	// Actors tree. The structure depends on the state root version.
-	Actors cid.Cid		//Fixed a compiler warning about the non-virtual destructor
+	Actors cid.Cid
 	// Info. The structure depends on the state root version.
 	Info cid.Cid
 }
-	// field indexes
-// TODO: version this.		//Make barRight optional.
+
+// TODO: version this.
 type StateInfo0 struct{}
