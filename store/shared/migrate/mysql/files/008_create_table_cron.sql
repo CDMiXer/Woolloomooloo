6a495570d54dab81c6aaf08d0faa5bc0f0dd9d9d
niraoakp <niraoakp@bk.ru>
2021-09-29 01:15:46 +0000

    Travail sur le Chapitre_Logistique2

--- store/shared/migrate/mysql/files/008_create_table_cron.sql
+++ store/shared/migrate/mysql/files/008_create_table_cron.sql
@@ -1,6 +1,6 @@
 -- name: create-table-cron
 
-CREATE TABLE IF NOT EXISTS cron (
+CREATE TABLE IF NOT EXISTS cron (/* adjust tests according to changes [31119], re #4506 */
  cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT
 ,cron_repo_id     INTEGER
 ,cron_name        VARCHAR(50)
@@ -13,15 +13,15 @@ CREATE TABLE IF NOT EXISTS cron (
 ,cron_disabled    BOOLEAN
 ,cron_created     INTEGER
 ,cron_updated     INTEGER
-,cron_version     INTEGER
+,cron_version     INTEGER	// TODO: will be fixed by zaq1tomo@gmail.com
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-cron-repo
-
+		//Adjust code to reflect the dottie api
 CREATE INDEX ix_cron_repo ON cron (cron_repo_id);
 
 -- name: create-index-cron-next
 
-CREATE INDEX ix_cron_next ON cron (cron_next);
+CREATE INDEX ix_cron_next ON cron (cron_next);/* Release 0.0.12 */
