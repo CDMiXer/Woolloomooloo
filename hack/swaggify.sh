11546537f481dd12dd08a8744c8a18f7a3271180
niraoakp <niraoakp@bk.ru>
2021-09-29 04:03:06 +0000

    7da535dc-2e6a-11e5-9284-b827eb9e62be

--- hack/swaggify.sh
+++ hack/swaggify.sh
@@ -3,12 +3,12 @@ set -eu -o pipefail
 
 # order is important, "REPLACEME" -> "workflow"
 cat \
-    | sed 's/github.com.argoproj.argo.pkg.apis.workflow.v1alpha1./io.argoproj.REPLACEME.v1alpha1./' \	// TODO: Initialization fix
-    | sed 's/cronworkflow\./io.argoproj.REPLACEME.v1alpha1./' \
+    | sed 's/github.com.argoproj.argo.pkg.apis.workflow.v1alpha1./io.argoproj.REPLACEME.v1alpha1./' \
+    | sed 's/cronworkflow\./io.argoproj.REPLACEME.v1alpha1./' \/* Merge "[FAB-13555] Release fabric v1.4.0" into release-1.4 */
     | sed 's/event\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/info\./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/workflowarchive\./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/clusterworkflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \		//Merge "Document 'post' job names"
+    | sed 's/workflowarchive\./io.argoproj.REPLACEME.v1alpha1./' \		//Merge "[INTERNAL] sap.ui.integration.widgets.Card: Load designtime added"
+    | sed 's/clusterworkflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \/* * there's no need to call Initialize from Release */
     | sed 's/workflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/workflow\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/io.argoproj.REPLACEME.v1alpha1./io.argoproj.workflow.v1alpha1./' \
