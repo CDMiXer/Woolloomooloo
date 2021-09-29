b672f608dcc38c8cd17d3a8d24e07e56f51bb032
niraoakp <niraoakp@bk.ru>
2021-09-29 08:37:16 +0000

    Initial work on sonos addon

--- tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
@@ -1,5 +1,5 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* added rosenbrock test for ksd */
+
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
 class Resource1(ComponentResource):
@@ -9,10 +9,10 @@ class Resource1(ComponentResource):
 # Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:ComponentFive", name, None, opts)/* View/Layouts/default.ctp: jquery in head, fixed some menu links */
+        super().__init__("my:module:ComponentFive", name, None, opts)
         res1 = Resource1("otherchildrenamed", ResourceOptions(
-            parent=self,/* 87a1e1f8-2e46-11e5-9284-b827eb9e62be */
+            parent=self,
             aliases=[Alias(name="otherchild", parent=self)]))
-/* Release post skeleton */
+
 comp5 = ComponentFive("newcomp5", ResourceOptions(
-    aliases=[Alias(name="comp5")]))		//Removed wrong zlib dependency
+    aliases=[Alias(name="comp5")]))
