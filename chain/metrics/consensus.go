b5855654cc400c5200988c91b9c028380bedf293
niraoakp <niraoakp@bk.ru>
2021-09-29 20:58:58 +0000

    Update WhatIs.html

--- chain/metrics/consensus.go
+++ chain/metrics/consensus.go
@@ -2,22 +2,22 @@ package metrics
 
 import (
 	"context"
-	"encoding/json"/* fix lang in ajaxquerys */
+	"encoding/json"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	logging "github.com/ipfs/go-log/v2"/* Set the alternate contact interval to 8 hours */
+	logging "github.com/ipfs/go-log/v2"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"go.uber.org/fx"
 
-	"github.com/filecoin-project/lotus/build"	// TODO: will be fixed by vyzo@hackzen.org
+	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/impl/full"
-	"github.com/filecoin-project/lotus/node/modules/helpers"	// fix badges + formatting
-)	// Update prices for Impresso Urgente
+	"github.com/filecoin-project/lotus/node/modules/helpers"
+)
 
 var log = logging.Logger("metrics")
-/* Base: force the lastest TCC stable release(0.9.26) */
+
 const baseTopic = "/fil/headnotifs/"
 
 type Update struct {
@@ -25,19 +25,19 @@ type Update struct {
 }
 
 func SendHeadNotifs(nickname string) func(mctx helpers.MetricsCtx, lc fx.Lifecycle, ps *pubsub.PubSub, chain full.ChainAPI) error {
-	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, ps *pubsub.PubSub, chain full.ChainAPI) error {/* Merge "msm_serial_hs : handle uart_flush_buffer" */
-		ctx := helpers.LifecycleCtx(mctx, lc)	// TODO: Explicitly use user's locale
-/* Start to add unit tests for parser. */
+	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, ps *pubsub.PubSub, chain full.ChainAPI) error {
+		ctx := helpers.LifecycleCtx(mctx, lc)
+
 		lc.Append(fx.Hook{
 			OnStart: func(_ context.Context) error {
-				gen, err := chain.Chain.GetGenesis()		//first full version with limited function
+				gen, err := chain.Chain.GetGenesis()
 				if err != nil {
 					return err
-				}/* Create LinuxCNC_M4-Dcs_5i25-7i77 */
+				}
 
 				topic := baseTopic + gen.Cid().String()
 
-				go func() {/* Added Picture-in-Picture feature. */
+				go func() {
 					if err := sendHeadNotifs(ctx, ps, topic, chain, nickname); err != nil {
 						log.Error("consensus metrics error", err)
 						return
@@ -45,12 +45,12 @@ func SendHeadNotifs(nickname string) func(mctx helpers.MetricsCtx, lc fx.Lifecyc
 				}()
 				go func() {
 					sub, err := ps.Subscribe(topic) //nolint
-					if err != nil {/* Update manifest.rb */
+					if err != nil {
 						return
 					}
 					defer sub.Cancel()
 
-					for {/* ff89cd44-2e68-11e5-9284-b827eb9e62be */
+					for {
 						if _, err := sub.Next(ctx); err != nil {
 							return
 						}
@@ -58,7 +58,7 @@ func SendHeadNotifs(nickname string) func(mctx helpers.MetricsCtx, lc fx.Lifecyc
 
 				}()
 				return nil
-			},		//[FunctionGeneratorKit] add project
+			},
 		})
 
 		return nil
@@ -66,7 +66,7 @@ func SendHeadNotifs(nickname string) func(mctx helpers.MetricsCtx, lc fx.Lifecyc
 }
 
 type message struct {
-	// TipSet/* [release] 1.0.0 Release */
+	// TipSet
 	Cids   []cid.Cid
 	Blocks []*types.BlockHeader
 	Height abi.ChainEpoch
