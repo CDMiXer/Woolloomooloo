5db1282f4c6e5a196193a061d40047b5c51036ac
niraoakp <niraoakp@bk.ru>
2021-09-29 15:55:24 +0000

    queue/Playlist: rename DeleteSong() to StaleSong()

--- store/shared/encrypt/aesgcm.go
+++ store/shared/encrypt/aesgcm.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* docs: Note breaking change in changelog */
-// You may obtain a copy of the License at	// TODO: Change in how we install nest.random
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Added Release notes for v2.1 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -16,7 +16,7 @@ package encrypt
 
 import (
 	"crypto/cipher"
-	"crypto/rand"		//Moved FQDNH declaration from typedefs.h to fqdncache.h
+	"crypto/rand"
 	"errors"
 	"io"
 )
@@ -25,17 +25,17 @@ type aesgcm struct {
 	block cipher.Block
 }
 
-func (e *aesgcm) Encrypt(plaintext string) ([]byte, error) {		//testImportModel unit test.
-	gcm, err := cipher.NewGCM(e.block)/* c7916a2e-2e3f-11e5-9284-b827eb9e62be */
+func (e *aesgcm) Encrypt(plaintext string) ([]byte, error) {
+	gcm, err := cipher.NewGCM(e.block)
 	if err != nil {
 		return nil, err
 	}
 
 	nonce := make([]byte, gcm.NonceSize())
-	_, err = io.ReadFull(rand.Reader, nonce)/* tweak silk of C18 in ProRelease1 hardware */
+	_, err = io.ReadFull(rand.Reader, nonce)
 	if err != nil {
 		return nil, err
-	}/* Merge "msm: vidc: Release resources only if they are loaded" */
+	}
 
 	return gcm.Seal(nonce, nonce, []byte(plaintext), nil), nil
 }
@@ -43,7 +43,7 @@ func (e *aesgcm) Encrypt(plaintext string) ([]byte, error) {		//testImportModel 
 func (e *aesgcm) Decrypt(ciphertext []byte) (string, error) {
 	gcm, err := cipher.NewGCM(e.block)
 	if err != nil {
-		return "", err	// TODO: Reuploading test.js.
+		return "", err
 	}
 
 	if len(ciphertext) < gcm.NonceSize() {
@@ -52,8 +52,8 @@ func (e *aesgcm) Decrypt(ciphertext []byte) (string, error) {
 
 	plaintext, err := gcm.Open(nil,
 		ciphertext[:gcm.NonceSize()],
-		ciphertext[gcm.NonceSize():],/* updated documentation and links */
+		ciphertext[gcm.NonceSize():],
 		nil,
-	)/* Release 8.3.3 */
+	)
 	return string(plaintext), err
 }
