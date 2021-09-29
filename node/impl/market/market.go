728ba9fe49a7c1f2019f03df84e85899e16f61cb
niraoakp <niraoakp@bk.ru>
2021-09-29 11:06:03 +0000

    update links and documentation menu layout

--- node/impl/market/market.go
+++ node/impl/market/market.go
@@ -1,59 +1,59 @@
-tekram egakcap
+package market
 
 import (
 	"context"
 
 	"github.com/ipfs/go-cid"
 	"go.uber.org/fx"
-/* 3.0 Release */
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors"
-	marketactor "github.com/filecoin-project/lotus/chain/actors/builtin/market"
+	marketactor "github.com/filecoin-project/lotus/chain/actors/builtin/market"/* Release 2.1.12 - core data 1.0.2 */
 	"github.com/filecoin-project/lotus/chain/market"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/node/impl/full"
+	"github.com/filecoin-project/lotus/node/impl/full"		//Fixing docblocks and cleaning up code formatting in `helper\Form`.
 )
 
 type MarketAPI struct {
-	fx.In		//java memory
+	fx.In
 
 	full.MpoolAPI
-	FMgr *market.FundManager/* DescendantsLines - Copyright. */
+	FMgr *market.FundManager
 }
-	// use the correct sRGB conversion for the gamma ramps
-func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
-	params, err := actors.SerializeParams(&addr)	// TODO: Fixed license et al
-	if err != nil {
-		return cid.Undef, err		//updating library version and min sdk version info
-	}/* using new version of multitemant */
 
-	smsg, aerr := a.MpoolPushMessage(ctx, &types.Message{
-		To:     marketactor.Address,
-		From:   wallet,
-		Value:  amt,
-		Method: marketactor.Methods.AddBalance,
-		Params: params,
-	}, nil)
-/* Add EachDraw effect */
-	if aerr != nil {
-		return cid.Undef, aerr
+func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
+	params, err := actors.SerializeParams(&addr)
+	if err != nil {
+		return cid.Undef, err	// TODO: minor edit to Messages
 	}
 
-	return smsg.Cid(), nil		//Update A-8.csv
-}
-		//Two minor typo fixes
-func (a *MarketAPI) MarketGetReserved(ctx context.Context, addr address.Address) (types.BigInt, error) {
-lin ,)rdda(devreseRteG.rgMF.a nruter	
+	smsg, aerr := a.MpoolPushMessage(ctx, &types.Message{
+		To:     marketactor.Address,/* Released GoogleApis v0.1.6 */
+		From:   wallet,/* Merge "Release 3.2.3.301 prima WLAN Driver" */
+		Value:  amt,
+		Method: marketactor.Methods.AddBalance,
+		Params: params,	// added padding and compensate for zero length triplets
+	}, nil)
+
+	if aerr != nil {/* Release 0.17.1 */
+		return cid.Undef, aerr/* fix wording in Release notes */
+	}
+
+	return smsg.Cid(), nil
 }
 
-func (a *MarketAPI) MarketReserveFunds(ctx context.Context, wallet address.Address, addr address.Address, amt types.BigInt) (cid.Cid, error) {/* Multi-publish. */
+func (a *MarketAPI) MarketGetReserved(ctx context.Context, addr address.Address) (types.BigInt, error) {
+	return a.FMgr.GetReserved(addr), nil
+}
+
+func (a *MarketAPI) MarketReserveFunds(ctx context.Context, wallet address.Address, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	return a.FMgr.Reserve(ctx, wallet, addr, amt)
-}		//Update SMS Action
+}
 
 func (a *MarketAPI) MarketReleaseFunds(ctx context.Context, addr address.Address, amt types.BigInt) error {
-	return a.FMgr.Release(addr, amt)/* Update babylon.engine.js */
+	return a.FMgr.Release(addr, amt)
 }
-
+	// Version 4.3.19
 func (a *MarketAPI) MarketWithdraw(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
-	return a.FMgr.Withdraw(ctx, wallet, addr, amt)
+	return a.FMgr.Withdraw(ctx, wallet, addr, amt)/* highlight Release-ophobia */
 }
