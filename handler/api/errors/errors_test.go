0b33e2ec3565b1d34b987e036970d5a1185b3f6a
niraoakp <niraoakp@bk.ru>
2021-09-29 21:50:39 +0000

    Display filter in angular view.

--- handler/api/errors/errors_test.go
+++ handler/api/errors/errors_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//added GA tracking code
+// that can be found in the LICENSE file.	// TODO: Update appointments.md
 
 package errors
 
 import "testing"
-
-func TestError(t *testing.T) {
-	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message
+	// TODO: hacked by magik6k@gmail.com
+func TestError(t *testing.T) {		//Fixed stupid bug and extract new lines for translation
+	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message/* Create IGeography */
 	if got != want {
 		t.Errorf("Want error string %q, got %q", got, want)
-	}
-}	// TODO: hacked by cory@protocol.ai
+	}/* composer.phar is no part of the project */
+}
