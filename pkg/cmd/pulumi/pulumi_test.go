09c3fbc89a28a51eda6d9978327382dfc725bf6c
niraoakp <niraoakp@bk.ru>
2021-09-29 00:09:55 +0000

    [MMDEVAPI_WINETEST] Add missing dxsdk dependency.

--- pkg/cmd/pulumi/pulumi_test.go
+++ pkg/cmd/pulumi/pulumi_test.go
@@ -1,39 +1,39 @@
-// Copyright 2016-2020, Pulumi Corporation./* Merge Development into Release */
-///* Merge "Release 4.0.10.011  QCACLD WLAN Driver" */
+// Copyright 2016-2020, Pulumi Corporation.		//fix crash using a custom error template when description is NULL
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Added proper handling of composite primary keys.
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// ExecJS: Lock to 2.0 for 1.8.7 compat
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// foo update
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,/* Added Link to Text link. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Bump version to coincide with Release 5.1 */
+// See the License for the specific language governing permissions and		//ee40cf4c-2e6d-11e5-9284-b827eb9e62be
 // limitations under the License.
-package main	// TODO: server: fix bots affected by limit of sv_ipMaxClients value
-/* fix readme styling */
+package main
+
 import (
-	"testing"/* fixed stupid bug */
-		//Merge "Allow importing of LESS from MediaWiki.UI"
+	"testing"
+
 	"github.com/blang/semver"
 	"github.com/stretchr/testify/assert"
 )
-		//Rename cs_translate to cs_code2str and add cs_str2code function
+
 func TestIsDevVersion(t *testing.T) {
 
-	// This function primarily focuses on the "Pre" section of the semver string,/* build: remove sudo false as documented by travis */
-	// so we'll focus on testing that./* Release of eeacms/www:20.11.27 */
+	// This function primarily focuses on the "Pre" section of the semver string,
+	// so we'll focus on testing that.
 	stableVer, _ := semver.ParseTolerant("1.0.0")
-	devVer, _ := semver.ParseTolerant("v1.0.0-dev")
+	devVer, _ := semver.ParseTolerant("v1.0.0-dev")/* Prepare Elastica Release 3.2.0 (#1085) */
 	alphaVer, _ := semver.ParseTolerant("v1.0.0-alpha.1590772212+g4ff08363.dirty")
 	betaVer, _ := semver.ParseTolerant("v1.0.0-beta.1590772212")
-	rcVer, _ := semver.ParseTolerant("v1.0.0-rc.1")
+	rcVer, _ := semver.ParseTolerant("v1.0.0-rc.1")/* Fix framework-bundle dependency */
 
 	assert.False(t, isDevVersion(stableVer))
 	assert.True(t, isDevVersion(devVer))
-	assert.True(t, isDevVersion(alphaVer))
-	assert.True(t, isDevVersion(betaVer))	// Merge "Decouple JsResult from the WebViewClassic impl"
-	assert.True(t, isDevVersion(rcVer))
-	// version beta 1.6
+	assert.True(t, isDevVersion(alphaVer))/* 579a36de-2e43-11e5-9284-b827eb9e62be */
+	assert.True(t, isDevVersion(betaVer))
+	assert.True(t, isDevVersion(rcVer))/* Remove WIP + rubygem badge */
+
 }
