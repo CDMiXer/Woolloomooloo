6230a2f4669a7b67502d0e5ce9add7b27c681b12
niraoakp <niraoakp@bk.ru>
2021-09-29 16:01:04 +0000

    python2.5-unittest-api-fix

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -1,40 +1,40 @@
 package repo
 
 import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
-/* Merge "Release 4.0.10.004  QCACLD WLAN Driver" */
-// BadgerBlockstoreOptions returns the badger options to apply for the provided	// fixed modal not opening in fullscreen for project/plan/build
-// domain./* Released 2.0.1 */
-func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {	// TODO: remove locally
+
+// BadgerBlockstoreOptions returns the badger options to apply for the provided
+// domain.
+func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {
 	opts := badgerbs.DefaultOptions(path)
 
 	// Due to legacy usage of blockstore.Blockstore, over a datastore, all
 	// blocks are prefixed with this namespace. In the future, this can go away,
-	// in order to shorten keys, but it'll require a migration./* Release v0.5.0 */
+	// in order to shorten keys, but it'll require a migration.
 	opts.Prefix = "/blocks/"
 
 	// Blockstore values are immutable; therefore we do not expect any
-	// conflicts to emerge./* whois.srs.net.nz parser must support `210 PendingRelease' status. */
+	// conflicts to emerge.
 	opts.DetectConflicts = false
-/* fixed wrong values for “Boat Driver Permit” */
-	// This is to optimize the database on close so it can be opened/* Add testes: adicionar e remover, lista em mapas, dump [2] */
-	// read-only and efficiently queried./* 13b7a788-2e4a-11e5-9284-b827eb9e62be */
+
+	// This is to optimize the database on close so it can be opened
+	// read-only and efficiently queried.
 	opts.CompactL0OnClose = true
-	// TODO: Layout and colour changes
+
 	// The alternative is "crash on start and tell the user to fix it". This
 	// will truncate corrupt and unsynced data, which we don't guarantee to
 	// persist anyways.
 	opts.Truncate = true
 
-	// We mmap the index and the value logs; this is important to enable	// TODO: Completed eq; added tests
-	// zero-copy value access./* Merge branch 'master' into Autocomplete_revised */
-	opts.ValueLogLoadingMode = badgerbs.MemoryMap/* Release of Prestashop Module V1.0.6 */
+	// We mmap the index and the value logs; this is important to enable
+	// zero-copy value access.
+	opts.ValueLogLoadingMode = badgerbs.MemoryMap
 	opts.TableLoadingMode = badgerbs.MemoryMap
-/* Hack to display "There are no items" message properly in empty ListView */
+
 	// Embed only values < 128 bytes in the LSM tree; larger values are stored
-	// in value logs.	//  - Fixed minor issue with simple data object regular expression
+	// in value logs.
 	opts.ValueThreshold = 128
 
-	// Default table size is already 64MiB. This is here to make it explicit./* Fixed EclipseWuff for Mac/Mars for getSdkDir() as well. */
+	// Default table size is already 64MiB. This is here to make it explicit.
 	opts.MaxTableSize = 64 << 20
 
 	// NOTE: The chain blockstore doesn't require any GC (blocks are never
