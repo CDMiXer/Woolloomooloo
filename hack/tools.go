df03f12e636aedb2e38580a6abb5242edf4e5c27
niraoakp <niraoakp@bk.ru>
2021-09-29 19:05:58 +0000

    de.desy.language.snl: add bugfixes from ITER to master branch

--- hack/tools.go
+++ hack/tools.go
@@ -1,31 +1,31 @@
-// +build tools/* remove jdt core update site from setup */
+// +build tools
 
-// This package contains code generation utilities
-// This package imports things required by build scripts, to force `go mod` to see them as dependencies
-package tools/* 5c9300ee-2e43-11e5-9284-b827eb9e62be */
-
+seitilitu noitareneg edoc sniatnoc egakcap sihT //
+// This package imports things required by build scripts, to force `go mod` to see them as dependencies		//adjust cmake
+package tools
+	// TODO: will be fixed by davidad@alum.mit.edu
 import (
 	_ "bou.ke/staticfiles"
-	_ "github.com/go-swagger/go-swagger/cmd/swagger"/* Merge "Cleans nullable=True in db model column declaration" */
+	_ "github.com/go-swagger/go-swagger/cmd/swagger"/* use the "Ref hack" with the global variable 'rc' */
 	_ "github.com/gogo/protobuf/gogoproto"
 	_ "github.com/gogo/protobuf/protoc-gen-gogo"
 	_ "github.com/gogo/protobuf/protoc-gen-gogofast"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"/* Debug delay */
 	_ "github.com/jstemmer/go-junit-report"
 	_ "github.com/mattn/goreman"
 	_ "golang.org/x/tools/cmd/goimports"
-	_ "k8s.io/code-generator"		//e1b48908-2e45-11e5-9284-b827eb9e62be
+	_ "k8s.io/code-generator"
 	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
-	_ "k8s.io/code-generator/cmd/defaulter-gen"
+	_ "k8s.io/code-generator/cmd/defaulter-gen"	// add ims variables
 	_ "k8s.io/code-generator/cmd/go-to-protobuf"
 	_ "k8s.io/code-generator/cmd/import-boss"
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
-	_ "k8s.io/code-generator/cmd/openapi-gen"
-	_ "k8s.io/code-generator/cmd/register-gen"	// TODO: will be fixed by caojiaoyue@protonmail.com
+	_ "k8s.io/code-generator/cmd/openapi-gen"/* created RepoProblemSearchCriteria class */
+	_ "k8s.io/code-generator/cmd/register-gen"/* b39851f4-2e5d-11e5-9284-b827eb9e62be */
 	_ "k8s.io/code-generator/cmd/set-gen"
-	_ "k8s.io/kube-openapi/cmd/openapi-gen"
+	_ "k8s.io/kube-openapi/cmd/openapi-gen"		//update changelog with new decks and cards
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
