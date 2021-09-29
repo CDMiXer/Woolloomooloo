46ae27212118a427515814b60ad20104ef6d4e1d
niraoakp <niraoakp@bk.ru>
2021-09-29 03:19:46 +0000

    992a5c34-2e64-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.cs
@@ -11,53 +11,53 @@ class MyStack : Stack
             Ingress = 
             {
                 new Aws.Ec2.Inputs.SecurityGroupIngressArgs
-                {
+                {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
                     Protocol = "tcp",
                     FromPort = 0,
                     ToPort = 0,
                     CidrBlocks = 
-                    {
+                    {	// TODO: will be fixed by steven@stebalien.com
                         "0.0.0.0/0",
                     },
                 },
             },
         });
-        var ami = Output.Create(Aws.GetAmi.InvokeAsync(new Aws.GetAmiArgs
-        {
-            Filters = 
+        var ami = Output.Create(Aws.GetAmi.InvokeAsync(new Aws.GetAmiArgs/* Merge "frameworks/base: Track owner in appropriate owners list of Uri" */
+        {/* nuras first post */
+            Filters = /* Update latest_version.html */
             {
                 new Aws.Inputs.GetAmiFilterArgs
                 {
-                    Name = "name",
+                    Name = "name",		//Check Moc after downloading Qt.
                     Values = 
-                    {
-                        "amzn-ami-hvm-*-x86_64-ebs",
-                    },
+                    {	// TODO: Create network_stats.php
+                        "amzn-ami-hvm-*-x86_64-ebs",	// TODO: added badge
+                    },		//Try using \ for linebreaks
                 },
             },
             Owners = 
             {
                 "137112412989",
-            },
+            },	// TODO: Delete 4shupeng.md
             MostRecent = true,
-        }));
-        // Create a simple web server using the startup script for the instance.
+        }));	// TODO: hacked by martin2cai@hotmail.com
+        // Create a simple web server using the startup script for the instance.	// fix alphabetical ordering in fdns.profile (2)
         var server = new Aws.Ec2.Instance("server", new Aws.Ec2.InstanceArgs
-        {
+        {/* Added MariaDB JDBC Driver to project. */
             Tags = 
-            {
+            {	// Merge branch 'dev' into bluetooth
                 { "Name", "web-server-www" },
             },
             InstanceType = "t2.micro",
             SecurityGroups = 
-            {
+            {		//updating video guide for mac
                 securityGroup.Name,
             },
             Ami = ami.Apply(ami => ami.Id),
             UserData = @"#!/bin/bash
 echo ""Hello, World!"" > index.html
 nohup python -m SimpleHTTPServer 80 &
-",
+",	// TODO: Use .p2align instead of .align for compatibility on Sandybridge as well
         });
         this.PublicIp = server.PublicIp;
         this.PublicHostName = server.PublicDns;
