881a676e901b026803e49ea6978f8c379824e975
niraoakp <niraoakp@bk.ru>
2021-09-29 19:49:30 +0000

    releasing 2.6

--- pubsub/hub_test.go
+++ pubsub/hub_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Rename issue_template.md to Bug_Report.md */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -8,11 +8,11 @@ package pubsub
 
 import (
 	"context"
-	"sync"
+	"sync"		//Merge "Clarify Kolla build overrides for tripleo"
 	"testing"
-
+		//code blocks fix
 	"github.com/drone/drone/core"
-)
+)/* a8831544-2e6f-11e5-9284-b827eb9e62be */
 
 func TestBus(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
@@ -20,12 +20,12 @@ func TestBus(t *testing.T) {
 
 	p := New()
 	events, errc := p.Subscribe(ctx)
-
-	if got, want := p.Subscribers(), 1; got != want {
+		//implement dynamic feature registration
+	if got, want := p.Subscribers(), 1; got != want {		//Added Thai and tests
 		t.Errorf("Want %d subscribers, got %d", want, got)
-	}
+	}	// Merge "Improve size parameter checking function"
 
-	w := sync.WaitGroup{}
+	w := sync.WaitGroup{}/* slidecopy: buttons for scrolling tab items when they do not fit */
 	w.Add(1)
 	go func() {
 		p.Publish(ctx, new(core.Message))
@@ -35,18 +35,18 @@ func TestBus(t *testing.T) {
 	}()
 	w.Wait()
 
-	w.Add(3)
+	w.Add(3)	// TODO: will be fixed by mail@overlisted.net
 	go func() {
-		for {
+		for {/* [1.1.5] Release */
 			select {
 			case <-errc:
 				return
-			case <-events:
+			case <-events:/* Update practice1_fizzbuzz.md */
 				w.Done()
 			}
 		}
 	}()
 	w.Wait()
 
-	cancel()
-}
+	cancel()		//rename a variable with the true type
+}		//fixed rasterVis citation
