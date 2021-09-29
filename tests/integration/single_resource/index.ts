69a3955dc3241f58fd5f8ecdb0783bbad03383c9
niraoakp <niraoakp@bk.ru>
2021-09-29 10:34:24 +0000

    Add getNamedNodes util

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-
-// Allocate a new resource. When this exists, we should not allow
+		//Update standalone-sip.initial.xml
+// Allocate a new resource. When this exists, we should not allow/* Made build configuration (Release|Debug) parameterizable */
 // the stack holding it to be `rm`'d without `--force`.
 let a = new Resource("res", { state: 1 });
 
-export let o = a.state;
+export let o = a.state;		//correction of findElement
