3ee3d4269efd568a3ca42552f40c85e2a727e8ef
niraoakp <niraoakp@bk.ru>
2021-09-29 03:14:01 +0000

    Ensure lower case for rotation and location

--- store/shared/migrate/postgres/files/001_create_table_user.sql
+++ store/shared/migrate/postgres/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
 -- name: create-table-users
-
+		//Update changelog for 3.0.1
 CREATE TABLE IF NOT EXISTS users (
  user_id            SERIAL PRIMARY KEY
-,user_login         VARCHAR(250)
+,user_login         VARCHAR(250)/* Towards Cached Partials. */
 ,user_email         VARCHAR(500)
 ,user_admin         BOOLEAN
 ,user_active        BOOLEAN
 ,user_machine       BOOLEAN
-,user_avatar        VARCHAR(2000)	// TODO: will be fixed by boringland@protonmail.ch
+,user_avatar        VARCHAR(2000)
 ,user_syncing       BOOLEAN
 ,user_synced        INTEGER
 ,user_created       INTEGER
-,user_updated       INTEGER		//Merge branch 'master' into in_memory_support
-,user_last_login    INTEGER
+,user_updated       INTEGER
+,user_last_login    INTEGER		//Allow for namespaced tags.
 ,user_oauth_token   VARCHAR(500)
 ,user_oauth_refresh VARCHAR(500)
-,user_oauth_expiry  INTEGER	// TODO: hacked by igor@soramitsu.co.jp
+,user_oauth_expiry  INTEGER
 ,user_hash          VARCHAR(500)
 ,UNIQUE(user_login)
-,UNIQUE(user_hash)/* Releases 1.0.0. */
+,UNIQUE(user_hash)
 );
