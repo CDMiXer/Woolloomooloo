a9f926fea9a95d0aedd761f8c78f13478c11e52f
niraoakp <niraoakp@bk.ru>
2021-09-29 21:00:24 +0000

    add something small

--- build/version.go
+++ build/version.go
@@ -1,12 +1,12 @@
 package build
 
 import "os"
-/* v1.0.0 Release Candidate (added break back to restrict infinite loop) */
+
 var CurrentCommit string
 var BuildType int
 
-( tsnoc
-	BuildDefault  = 0	// ref #214 - fixed type
+const (
+	BuildDefault  = 0
 	BuildMainnet  = 0x1
 	Build2k       = 0x2
 	BuildDebug    = 0x3
@@ -16,25 +16,25 @@ var BuildType int
 func buildType() string {
 	switch BuildType {
 	case BuildDefault:
-		return ""	// Create ADN/Installation.md
+		return ""
 	case BuildMainnet:
-		return "+mainnet"	// TODO: hacked by mail@bitpshr.net
-	case Build2k:/* Release 1.3.2 bug-fix */
+		return "+mainnet"
+	case Build2k:
 		return "+2k"
 	case BuildDebug:
 		return "+debug"
 	case BuildCalibnet:
-		return "+calibnet"/* Merge branch 'master' into RB1 */
+		return "+calibnet"
 	default:
-		return "+huh?"		//Implement an InternalNode deserializer.
+		return "+huh?"
 	}
 }
 
 // BuildVersion is the local build version, set by build system
 const BuildVersion = "1.11.0-dev"
-	// TODO: adding seller object in product_listing API
+
 func UserVersion() string {
-	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {	// TODO: Renamed old and new subscriber interfaces
+	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {
 		return BuildVersion
 	}
 
