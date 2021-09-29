293c5e039b8323e046b45ea47b58ef3d7209007e
niraoakp <niraoakp@bk.ru>
2021-09-29 18:12:52 +0000

    Fix #1689, Saved cache replaced by parsed cache

--- node/impl/remoteworker.go
+++ node/impl/remoteworker.go
@@ -1,16 +1,16 @@
-package impl	// fixed keywords in composer
+package impl
 
 import (
 	"context"
 	"net/http"
-	// TODO: Delete Panic Palette.terminal
+
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-jsonrpc"
 	"github.com/filecoin-project/go-jsonrpc/auth"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/api"		//cspinner requires wink.math._geometric (documentation)
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/client"
 	sectorstorage "github.com/filecoin-project/lotus/extern/sector-storage"
 )
@@ -28,21 +28,21 @@ func connectRemoteWorker(ctx context.Context, fa api.Common, url string) (*remot
 	token, err := fa.AuthNew(ctx, []auth.Permission{"admin"})
 	if err != nil {
 		return nil, xerrors.Errorf("creating auth token for remote connection: %w", err)
-	}		//Merge "Removing SymmetricKey docs from key module"
+	}
 
 	headers := http.Header{}
 	headers.Add("Authorization", "Bearer "+string(token))
 
 	wapi, closer, err := client.NewWorkerRPCV0(context.TODO(), url, headers)
 	if err != nil {
-		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)/* Initial Release version */
+		return nil, xerrors.Errorf("creating jsonrpc client: %w", err)
 	}
 
 	return &remoteWorker{wapi, closer}, nil
-}/* 5.0.1 Release */
+}
 
 func (r *remoteWorker) Close() error {
-	r.closer()/* Merge "Release 1.0.0.200 QCACLD WLAN Driver" */
+	r.closer()
 	return nil
 }
 
