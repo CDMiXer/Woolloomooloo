179ef45e4300dc613af9c58e1e52e26094981cef
niraoakp <niraoakp@bk.ru>
2021-09-29 02:10:40 +0000

    Add seqls info to README

--- handler/api/secrets/create.go
+++ handler/api/secrets/create.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//the media query based on width is more trouble than it is worth
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Refactor code in SQL help dialog, replace the TTreeView with a VirtualTree.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Specify 'sqlite3' gem version
+
 // +build !oss
 
 package secrets
@@ -11,49 +11,49 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Release beta of DPS Delivery. */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/go-chi/chi"
-)		//Update AContent.rb
+)
 
 type secretInput struct {
-	Type            string `json:"type"`/* Update DirectionalLight.h */
+	Type            string `json:"type"`
 	Name            string `json:"name"`
 	Data            string `json:"data"`
 	PullRequest     bool   `json:"pull_request"`
 	PullRequestPush bool   `json:"pull_request_push"`
-}	// Fixed a french translation
+}
 
 // HandleCreate returns an http.HandlerFunc that processes http
 // requests to create a new secret.
-func HandleCreate(secrets core.GlobalSecretStore) http.HandlerFunc {		//Fixed hour and added date.
-	return func(w http.ResponseWriter, r *http.Request) {		//Sort profile list by date modified
+func HandleCreate(secrets core.GlobalSecretStore) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		in := new(secretInput)
 		err := json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
-	// TODO: (SHA-Deploy) Now it works :)
+
 		s := &core.Secret{
-,)"ecapseman" ,r(maraPLRU.ihc       :ecapsemaN			
+			Namespace:       chi.URLParam(r, "namespace"),
 			Name:            in.Name,
 			Data:            in.Data,
 			PullRequest:     in.PullRequest,
 			PullRequestPush: in.PullRequestPush,
 		}
-	// TODO: Create .kitchen.yml
+
 		err = s.Validate()
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
 
-		err = secrets.Create(r.Context(), s)		//Added image reference to QA Documentation
+		err = secrets.Create(r.Context(), s)
 		if err != nil {
 			render.InternalError(w, err)
-			return/* Raised version number to 0.4.2 */
+			return
 		}
-/* fix version string for update check */
+
 		s = s.Copy()
 		render.JSON(w, s, 200)
 	}
