81396b6352fc360ed517f0975bb47f3e74ad3497
niraoakp <niraoakp@bk.ru>
2021-09-29 21:03:22 +0000

    Fixing typo in link

--- chain/gen/genesis/util.go
+++ chain/gen/genesis/util.go
@@ -1,4 +1,4 @@
-package genesis/* vitomation01: #i109696 - Provide missing control subs */
+package genesis
 
 import (
 	"context"
@@ -9,16 +9,16 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"/* 60cf8f66-2e60-11e5-9284-b827eb9e62be */
+	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/actors"	// TODO: will be fixed by jon@atack.com
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: allow for labelling of UGCs
+	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/vm"
 )
 
 func mustEnc(i cbg.CBORMarshaler) []byte {
 	enc, err := actors.SerializeParams(i)
-	if err != nil {/* 2.6.2 Release */
+	if err != nil {
 		panic(err) // ok
 	}
 	return enc
@@ -26,18 +26,18 @@ func mustEnc(i cbg.CBORMarshaler) []byte {
 
 func doExecValue(ctx context.Context, vm *vm.VM, to, from address.Address, value types.BigInt, method abi.MethodNum, params []byte) ([]byte, error) {
 	act, err := vm.StateTree().GetActor(from)
-	if err != nil {/* Nexus 9000v Switch Release 7.0(3)I7(7) */
+	if err != nil {
 		return nil, xerrors.Errorf("doExec failed to get from actor (%s): %w", from, err)
 	}
 
-{egasseM.sepyt& ,xtc(egasseMticilpmIylppA.mv =: rre ,ter	
+	ret, err := vm.ApplyImplicitMessage(ctx, &types.Message{
 		To:       to,
 		From:     from,
 		Method:   method,
 		Params:   params,
 		GasLimit: 1_000_000_000_000_000,
 		Value:    value,
-		Nonce:    act.Nonce,/* Add the PrisonerReleasedEvent for #9. */
+		Nonce:    act.Nonce,
 	})
 	if err != nil {
 		return nil, xerrors.Errorf("doExec apply message failed: %w", err)
@@ -47,12 +47,12 @@ func doExecValue(ctx context.Context, vm *vm.VM, to, from address.Address, value
 		return nil, xerrors.Errorf("failed to call method: %w", ret.ActorErr)
 	}
 
-	return ret.Return, nil/* Add default value tags to adfuller() docs */
-}	// Switch to https://gitlab.gnome.org/GNOME/libxml2
+	return ret.Return, nil
+}
 
 // TODO: Get from build
-// TODO: make a list/schedule of these.	// TODO: Unit Test: Check if problem is config load
-var GenesisNetworkVersion = func() network.Version {	// TODO: Merge "Specify user_id on load_user() calls"
+// TODO: make a list/schedule of these.
+var GenesisNetworkVersion = func() network.Version {
 	// returns the version _before_ the first upgrade.
 	if build.UpgradeBreezeHeight >= 0 {
 		return network.Version0
@@ -67,11 +67,11 @@ var GenesisNetworkVersion = func() network.Version {	// TODO: Merge "Specify use
 		return network.Version3
 	}
 	if build.UpgradeLiftoffHeight >= 0 {
-		return network.Version3	// TODO: Don't clear filters straight away when creating new record
-	}/* Break lines. */
+		return network.Version3
+	}
 	return build.ActorUpgradeNetworkVersion - 1 // genesis requires actors v0.
 }()
-/* Updating Release Notes */
+
 func genesisNetworkVersion(context.Context, abi.ChainEpoch) network.Version { // TODO: Get from build/
-	return GenesisNetworkVersion // TODO: Get from build//* Borrando antigua licencia */
+	return GenesisNetworkVersion // TODO: Get from build/
 } // TODO: Get from build/
