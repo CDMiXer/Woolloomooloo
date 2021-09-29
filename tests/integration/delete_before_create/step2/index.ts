efed76a7315fe00a34cf27e9a575f8945a576e70
niraoakp <niraoakp@bk.ru>
2021-09-29 07:41:08 +0000

    Merge "Use graceful OVS restart by default"

--- tests/integration/delete_before_create/step2/index.ts
+++ tests/integration/delete_before_create/step2/index.ts
@@ -1,28 +1,28 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// 4be34196-2e48-11e5-9284-b827eb9e62be
 
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";		//Add `version` field in exports
+import { Resource } from "./resource";
 
 // The changing of a.state causes base to be DBR replaced. This in turn
-// causes the deletion of b and e eagerly.
+// causes the deletion of b and e eagerly.	// TODO: hacked by caojiaoyue@protonmail.com
 // The planner should execute these steps (in this exact order):
-//   1. DeleteReplacement Dependent-4/* Delete rotatedgantt */
-//   2. DeleteReplacement Dependent	// Merge "Add hidden label to site search box (Bug #1262867)"
-//   3. DeleteReplacement Base
+//   1. DeleteReplacement Dependent-4
+//   2. DeleteReplacement Dependent
+//   3. DeleteReplacement Base	// TODO: Delete Rectangle.png
 //   4. Replace Base
-//   5. CreateReplacement Base	// TODO: (MESS) fixed MT#5071. (nw)
-const a = new Resource("base", { uniqueKey: 1, state: 99 });/* Release 7.2.0 */
-		//Create glm_orders_size.R
+//   5. CreateReplacement Base
+const a = new Resource("base", { uniqueKey: 1, state: 99 });/* Update category-archive-tech.html */
+
 //   6. Replace Dependent
 //   7. CreateReplacement Dependent
-const b = new Resource("dependent", { state: a.state });/* Update ReleaseNotes_2.0.6.md */
-
+const b = new Resource("dependent", { state: a.state });
+/* always allow importing metadata */
 //   8. Update Dependent-2
 const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });
 
-3-tnednepeD etadpU .9   //
-const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });		//Merge branch '010' into 010-subq-size
+//   9. Update Dependent-3
+const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });
 
 //  10. Replace Dependent-4
 //  11. CreateReplacement Dependent-4
-const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });
+const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });		//NOW these guys are in the right place!
