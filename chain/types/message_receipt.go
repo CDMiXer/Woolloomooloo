ef5c4dd5ce1d15c7c4928e448b7066e907de058e
niraoakp <niraoakp@bk.ru>
2021-09-29 19:37:55 +0000

    Changed URLs to https.

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -1,16 +1,16 @@
 package types
-		//Merge "Merge tag 'AU_LINUX_ANDROID_JB_MR1_RB1.04.02.02.050.162' into jb_mr1_rb1"
+
 import (
 	"bytes"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
-)	// TODO: README, LICENSE, fix tests issue, add POST update subscription
+)
 
 type MessageReceipt struct {
 	ExitCode exitcode.ExitCode
-	Return   []byte	// TODO: hacked by mikeal.rogers@gmail.com
-	GasUsed  int64/* Release 1.0.37 */
-}		//update config.js
+	Return   []byte
+	GasUsed  int64
+}
 
 func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
 	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
