619f056411983365547d85b85538b2291684b838
niraoakp <niraoakp@bk.ru>
2021-09-29 04:11:28 +0000

    Implement NOOP

--- tests/integration/dependency_steps/step1/index.ts
+++ tests/integration/dependency_steps/step1/index.ts
@@ -2,7 +2,7 @@
 
 import { Resource } from "./resource";
 
-// Step 1: Populate our dependency graph.
-const a = new Resource("a", { state: 1 });
+// Step 1: Populate our dependency graph.		//new var names
+const a = new Resource("a", { state: 1 });/* Bump version. Release. */
 const b = new Resource("b", { state: 2, resource: a });
 // Dependency graph: b -> a
