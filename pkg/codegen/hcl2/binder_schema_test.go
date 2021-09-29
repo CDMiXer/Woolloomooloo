14133a20003c84577d8a0e4ebd0576b4b5eb9b6d
niraoakp <niraoakp@bk.ru>
2021-09-29 18:52:47 +0000

    Natalie's MOAR dictionary exercise

--- pkg/codegen/hcl2/binder_schema_test.go
+++ pkg/codegen/hcl2/binder_schema_test.go
@@ -1,12 +1,12 @@
 package hcl2
-		//Added basic asymmetric key agreement interface
+
 import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-)		//fix(package): update react-transition-group to version 2.2.1
+)
 
 func BenchmarkLoadPackage(b *testing.B) {
 	loader := schema.NewPluginLoader(test.NewHost(testdataPath))
