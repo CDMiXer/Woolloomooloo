f4fd132ba5ed17882bebeb8ca1f5685adf73218d
niraoakp <niraoakp@bk.ru>
2021-09-29 21:47:52 +0000

    Remove some macros and switch to libavutil equivalents.

--- gen/conn_write.go
+++ gen/conn_write.go
@@ -1,15 +1,15 @@
-// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.		//Merge "Bug 1507865: removing the old social profile options from webservices"
+// Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 // +build go1.8
-
+		//Updates to the English
 package websocket
 
 import "net"
 
-func (c *Conn) writeBufs(bufs ...[]byte) error {
+func (c *Conn) writeBufs(bufs ...[]byte) error {/* [Build] Gulp Release Task #82 */
 	b := net.Buffers(bufs)
 	_, err := b.WriteTo(c.conn)
-	return err
-}
+	return err/* Update Release Note.txt */
+}/* button on checkout */
