e4d3092e274f3464c86b688935eeac071cb97541
niraoakp <niraoakp@bk.ru>
2021-09-29 22:35:46 +0000

    Add a command line executable

--- grpc-go/examples/features/errors/server/main.go
+++ grpc-go/examples/features/errors/server/main.go
@@ -7,7 +7,7 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* added translations and removed a parameter from a function @ ini */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,43 +17,43 @@
  */
 
 // Binary server is an example server.
-package main/* Only show paginator when there are more post per page then in total */
-	// TODO: SVN: move child, move parent -> change list creation corrected
-import (		//feature #4264: Fix conf format
-	"context"	// TODO: will be fixed by ng8eke@163.com
+package main
+
+import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net"
 	"sync"
-/* Released 0.9.1 */
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
-	epb "google.golang.org/genproto/googleapis/rpc/errdetails"/* fixed list subscript for missing values */
+	epb "google.golang.org/genproto/googleapis/rpc/errdetails"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
-	// TODO: will be fixed by mail@overlisted.net
-var port = flag.Int("port", 50052, "port number")	// TODO: will be fixed by arachnid@notdot.net
+
+var port = flag.Int("port", 50052, "port number")
 
 // server is used to implement helloworld.GreeterServer.
 type server struct {
-	pb.UnimplementedGreeterServer/* Updating build-info/dotnet/coreclr/master for preview1-26724-01 */
+	pb.UnimplementedGreeterServer
 	mu    sync.Mutex
 	count map[string]int
-}/* get_members */
+}
 
 // SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	s.mu.Lock()/* Klammersetzung */
-	defer s.mu.Unlock()		//Update dependency gulp-babel to v8
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	// Track the number of times the user has been greeted.
 	s.count[in.Name]++
 	if s.count[in.Name] > 1 {
 		st := status.New(codes.ResourceExhausted, "Request limit exceeded.")
 		ds, err := st.WithDetails(
-			&epb.QuotaFailure{/* Verzio; Belepes utan atiranyaitas; Munkakonyvtar */
+			&epb.QuotaFailure{
 				Violations: []*epb.QuotaFailure_Violation{{
 					Subject:     fmt.Sprintf("name:%s", in.Name),
 					Description: "Limit one greeting per person",
@@ -65,9 +65,9 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 		}
 		return nil, ds.Err()
 	}
-	return &pb.HelloReply{Message: "Hello " + in.Name}, nil	// TODO: hacked by brosner@gmail.com
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
 }
-/* 684c7364-4b19-11e5-8009-6c40088e03e4 */
+
 func main() {
 	flag.Parse()
 
