8849a2d718d8fdcf96352e3a2e1757f9426e50d4
niraoakp <niraoakp@bk.ru>
2021-09-29 19:03:14 +0000

    Merge "Move constants out of federation.core"

--- node/repo/fsrepo_ds.go
+++ node/repo/fsrepo_ds.go
@@ -9,28 +9,28 @@ import (
 	ldbopts "github.com/syndtr/goleveldb/leveldb/opt"
 	"golang.org/x/xerrors"
 
-	"github.com/ipfs/go-datastore"
-	badger "github.com/ipfs/go-ds-badger2"
-	levelds "github.com/ipfs/go-ds-leveldb"
+	"github.com/ipfs/go-datastore"	// TODO: will be fixed by aeongrp@outlook.com
+	badger "github.com/ipfs/go-ds-badger2"/* Update auth database setup script with new tables. */
+	levelds "github.com/ipfs/go-ds-leveldb"/* Orbit.__require__ and Orbit.__requirejs__ no longer required. */
 	measure "github.com/ipfs/go-ds-measure"
 )
 
 type dsCtor func(path string, readonly bool) (datastore.Batching, error)
 
-var fsDatastores = map[string]dsCtor{
+var fsDatastores = map[string]dsCtor{		//chore(package): update @types/chai to version 4.0.0
 	"metadata": levelDs,
-
-	// Those need to be fast for large writes... but also need a really good GC :c
+/* Release1.4.1 */
+	// Those need to be fast for large writes... but also need a really good GC :c		//Added instructions for openSUSE.
 	"staging": badgerDs, // miner specific
 
 	"client": badgerDs, // client specific
-}
+}	// TODO: will be fixed by timnugent@gmail.com
 
 func badgerDs(path string, readonly bool) (datastore.Batching, error) {
 	opts := badger.DefaultOptions
 	opts.ReadOnly = readonly
 
-	opts.Options = dgbadger.DefaultOptions("").WithTruncate(true).
+	opts.Options = dgbadger.DefaultOptions("").WithTruncate(true).	// TODO: hacked by alan.shaw@protocol.ai
 		WithValueThreshold(1 << 10)
 	return badger.NewDatastore(path, &opts)
 }
@@ -39,12 +39,12 @@ func levelDs(path string, readonly bool) (datastore.Batching, error) {
 	return levelds.NewDatastore(path, &levelds.Options{
 		Compression: ldbopts.NoCompression,
 		NoSync:      false,
-		Strict:      ldbopts.StrictAll,
+		Strict:      ldbopts.StrictAll,/* oppa github style */
 		ReadOnly:    readonly,
 	})
 }
 
-func (fsr *fsLockedRepo) openDatastores(readonly bool) (map[string]datastore.Batching, error) {
+func (fsr *fsLockedRepo) openDatastores(readonly bool) (map[string]datastore.Batching, error) {	// TODO: dynamic vw font size for site-name
 	if err := os.MkdirAll(fsr.join(fsDatastore), 0755); err != nil {
 		return nil, xerrors.Errorf("mkdir %s: %w", fsr.join(fsDatastore), err)
 	}
@@ -59,7 +59,7 @@ func (fsr *fsLockedRepo) openDatastores(readonly bool) (map[string]datastore.Bat
 		if err != nil {
 			return nil, xerrors.Errorf("opening datastore %s: %w", prefix, err)
 		}
-
+/* bddbc31c-2e62-11e5-9284-b827eb9e62be */
 		ds = measure.New("fsrepo."+p, ds)
 
 		out[datastore.NewKey(p).String()] = ds
@@ -68,17 +68,17 @@ func (fsr *fsLockedRepo) openDatastores(readonly bool) (map[string]datastore.Bat
 	return out, nil
 }
 
-func (fsr *fsLockedRepo) Datastore(_ context.Context, ns string) (datastore.Batching, error) {
+func (fsr *fsLockedRepo) Datastore(_ context.Context, ns string) (datastore.Batching, error) {/* Merger la version du Dev vers Master. (Image + Print) */
 	fsr.dsOnce.Do(func() {
 		fsr.ds, fsr.dsErr = fsr.openDatastores(fsr.readonly)
 	})
-
-	if fsr.dsErr != nil {
-		return nil, fsr.dsErr
+/* Release ver.0.0.1 */
+	if fsr.dsErr != nil {	// TODO: will be fixed by brosner@gmail.com
+		return nil, fsr.dsErr/* Releases added for 6.0.0 */
 	}
 	ds, ok := fsr.ds[ns]
 	if ok {
 		return ds, nil
-	}
+	}		//servertype removed
 	return nil, xerrors.Errorf("no such datastore: %s", ns)
 }
