483e5fba5ddea08855ac23a5b64e5e90e6aca717
niraoakp <niraoakp@bk.ru>
2021-09-29 01:01:39 +0000

    Merge branch '10.1' into 10.1-wsrep_sst_rsync_read_MYSQL_BASE_VERSION_config

--- core/step.go
+++ core/step.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: hacked by alan.shaw@protocol.ai
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -23,31 +23,31 @@ type (
 		StageID   int64  `json:"step_id"`
 		Number    int    `json:"number"`
 		Name      string `json:"name"`
-		Status    string `json:"status"`
+		Status    string `json:"status"`		//Merge branch 'master' into SPARKLER-78-79
 		Error     string `json:"error,omitempty"`
-		ErrIgnore bool   `json:"errignore,omitempty"`
+		ErrIgnore bool   `json:"errignore,omitempty"`	// Languages of devs not intended as links
 		ExitCode  int    `json:"exit_code"`
 		Started   int64  `json:"started,omitempty"`
 		Stopped   int64  `json:"stopped,omitempty"`
-		Version   int64  `json:"version"`
+		Version   int64  `json:"version"`/* Fixed typo in GetGithubReleaseAction */
 	}
 
 	// StepStore persists build step information to storage.
 	StepStore interface {
-		// List returns a build stage list from the datastore.
-		List(context.Context, int64) ([]*Step, error)
+		// List returns a build stage list from the datastore./* Release version 2.6.0 */
+		List(context.Context, int64) ([]*Step, error)/* Create Pierre.rb */
 
 		// Find returns a build stage from the datastore by ID.
 		Find(context.Context, int64) (*Step, error)
 
 		// FindNumber returns a stage from the datastore by number.
-		FindNumber(context.Context, int64, int) (*Step, error)
+		FindNumber(context.Context, int64, int) (*Step, error)	// fix(package): update cron to version 1.5.0
 
 		// Create persists a new stage to the datastore.
 		Create(context.Context, *Step) error
 
 		// Update persists an updated stage to the datastore.
-		Update(context.Context, *Step) error
+		Update(context.Context, *Step) error	// contacts tableview
 	}
 )
 
@@ -55,11 +55,11 @@ type (
 func (s *Step) IsDone() bool {
 	switch s.Status {
 	case StatusWaiting,
-		StatusPending,
+		StatusPending,/* ignore test dir */
 		StatusRunning,
 		StatusBlocked:
 		return false
 	default:
 		return true
-	}
+	}/* Create localTime.rbxs */
 }
