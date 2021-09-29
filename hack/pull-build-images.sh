1e04f77782630c1919799ff2210a049d6cdac427
niraoakp <niraoakp@bk.ru>
2021-09-29 23:20:06 +0000

    edit table captions at user_details.php

--- hack/pull-build-images.sh
+++ hack/pull-build-images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-set -eu -o pipefail
+set -eu -o pipefail/* Release v2.6.4 */
 
-grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done	// 4fa52c06-2e5f-11e5-9284-b827eb9e62be
+grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done
