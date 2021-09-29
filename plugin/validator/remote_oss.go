fb2d14beef8518d64039a2f612bd75cdc6a43da9
niraoakp <niraoakp@bk.ru>
2021-09-29 11:21:48 +0000

    Add "UltraReactor" on getName()

--- plugin/validator/remote_oss.go
+++ plugin/validator/remote_oss.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
-///* Release 1.0.0 pom. */
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by why@ipfs.io
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,8 +10,8 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Define _SECURE_SCL=0 for Release configurations. */
-/* Closes HRFAL-33: Release final RPM (getting password by issuing command) */
+// limitations under the License.
+
 // +build oss
 
 package validator
@@ -21,7 +21,7 @@ import (
 
 	"github.com/drone/drone/core"
 )
-		//db7e09ca-2e5d-11e5-9284-b827eb9e62be
+
 // Remote returns a conversion service that converts the
 // configuration file using a remote http service.
 func Remote(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ValidateService {
