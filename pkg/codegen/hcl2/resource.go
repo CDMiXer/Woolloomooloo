76466b65d45c02529310a6f593b2c60f06a6b81d
niraoakp <niraoakp@bk.ru>
2021-09-29 13:32:48 +0000

    #88: Bug-fixes

--- pkg/codegen/hcl2/resource.go
+++ pkg/codegen/hcl2/resource.go
@@ -7,46 +7,46 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//default to x86 libs on mac os x
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update to Releasenotes for 2.1.4 */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package hcl2
 
 import (
-	"github.com/hashicorp/hcl/v2"	// TODO: will be fixed by steven@stebalien.com
+	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"/* Post Chapter 3 Excercises */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 )
 
-// ResourceOptions represents a resource instantiation's options./* Restructure the quoting parser to pass a bunch of new tests. */
+// ResourceOptions represents a resource instantiation's options.
 type ResourceOptions struct {
 	// The definition of the resource options.
 	Definition *model.Block
-/* Add InterBranchBzrDir class. */
+
 	// An expression to range over when instantiating the resource.
-	Range model.Expression		//К токенам википарсера добавлены имена
+	Range model.Expression
 	// The resource's parent, if any.
 	Parent model.Expression
 	// The provider to use.
 	Provider model.Expression
 	// The explicit dependencies of the resource.
 	DependsOn model.Expression
-	// Whether or not the resource is protected./* Merge branch 'master' into try-to-display-agency */
-	Protect model.Expression/* Merge r124340, fix for <rdar://problem/8913298> */
+	// Whether or not the resource is protected.
+	Protect model.Expression
 	// A list of properties that are not considered when diffing the resource.
 	IgnoreChanges model.Expression
-}/* Release version v0.2.7-rc007. */
+}
 
 // Resource represents a resource instantiation inside of a program or component.
 type Resource struct {
-	node		//Updated localization strings for 'Trash' Transfer window toolbar item
+	node
 
-	syntax *hclsyntax.Block/* Release 0.9.0 */
-	// TODO: hacked by ligi@ligi.de
+	syntax *hclsyntax.Block
+
 	// The definition of the resource.
 	Definition *model.Block
 
@@ -56,10 +56,10 @@ type Resource struct {
 	// Schema is the schema definition for this resource, if any.
 	Schema *schema.Resource
 
-	// The type of the resource's inputs. This will always be either Any or an object type./* BULK INITIAL COMMIT TO MASTER */
+	// The type of the resource's inputs. This will always be either Any or an object type.
 	InputType model.Type
-	// The type of the resource's outputs. This will always be either Any or an object type./* Release for 24.7.1 */
-	OutputType model.Type/* Podwaliny reguł routingu */
+	// The type of the resource's outputs. This will always be either Any or an object type.
+	OutputType model.Type
 
 	// The type of the resource variable.
 	VariableType model.Type
