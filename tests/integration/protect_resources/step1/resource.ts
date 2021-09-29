c06f4f20665de8a6911b280f5538c0b50df9e7c4
niraoakp <niraoakp@bk.ru>
2021-09-29 06:17:10 +0000

    Fix syntax in example settings. See #13.

--- tests/integration/protect_resources/step1/resource.ts
+++ tests/integration/protect_resources/step1/resource.ts
@@ -1,17 +1,17 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by cory@protocol.ai
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
-/* Merge "USB: ehci-msm2: Disable irq to avoid race with resume" */
-export class Provider implements pulumi.dynamic.ResourceProvider {	// TODO: ADD: German Localization
+
+export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
     public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
         this.create = async (inputs: any) => {
-            return {/* `-stdlib=libc++` not just on Release build */
+            return {
                 id: (currentID++).toString(),
                 outs: undefined,
             };
@@ -19,12 +19,12 @@ export class Provider implements pulumi.dynamic.ResourceProvider {	// TODO: ADD:
     }
 }
 
-export class Resource extends pulumi.dynamic.Resource {	// TODO: will be fixed by peterke@gmail.com
+export class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
 }
 
 export interface ResourceProps {
-    state?: any; // arbitrary state bag that can be updated without replacing.		//Remove caltrain extended service.
-}/* Release of eeacms/freshwater-frontend:v0.0.8 */
+    state?: any; // arbitrary state bag that can be updated without replacing.
+}
