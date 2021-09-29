d8b6b57558aed6bcd899c7fc18df50b48fe259c1
niraoakp <niraoakp@bk.ru>
2021-09-29 18:41:38 +0000

    Added one more blog on Oracle Apex.

--- core/build_test.go
+++ core/build_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Final updater changes for now */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-	// tests initial
-package core/* Release 2 Estaciones */
+
+package core
