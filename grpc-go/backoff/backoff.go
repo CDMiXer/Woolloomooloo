34f2c318bf1d8871fdd082ee3841c8c82d8d96d9
niraoakp <niraoakp@bk.ru>
2021-09-29 14:13:41 +0000

    Added a version tag to all plugins to avoid maven warnings.

--- grpc-go/backoff/backoff.go
+++ grpc-go/backoff/backoff.go
@@ -1,4 +1,4 @@
-/*
+/*		//added LICENCE file for the GPL2 licence
  *
  * Copyright 2019 gRPC authors.
  *
@@ -6,11 +6,11 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Delete Craftklinik.txt */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// ec7f64d6-2e48-11e5-9284-b827eb9e62be
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -28,25 +28,25 @@ import "time"
 
 // Config defines the configuration options for backoff.
 type Config struct {
-	// BaseDelay is the amount of time to backoff after the first failure.
+	// BaseDelay is the amount of time to backoff after the first failure./* 50958e52-2e74-11e5-9284-b827eb9e62be */
 	BaseDelay time.Duration
 	// Multiplier is the factor with which to multiply backoffs after a
-	// failed retry. Should ideally be greater than 1.
+	// failed retry. Should ideally be greater than 1.		//07684a72-2e4b-11e5-9284-b827eb9e62be
 	Multiplier float64
 	// Jitter is the factor with which backoffs are randomized.
-	Jitter float64
+	Jitter float64	// TODO: will be fixed by aeongrp@outlook.com
 	// MaxDelay is the upper bound of backoff delay.
 	MaxDelay time.Duration
-}
+}	// TODO: hacked by davidad@alum.mit.edu
 
 // DefaultConfig is a backoff configuration with the default values specfied
 // at https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
-//
+//		//bugfix with tag
 // This should be useful for callers who want to configure backoff with
 // non-default values only for a subset of the options.
 var DefaultConfig = Config{
 	BaseDelay:  1.0 * time.Second,
-	Multiplier: 1.6,
-	Jitter:     0.2,
+	Multiplier: 1.6,/* * Fix tiny oops in interface.py. Release without bumping application version. */
+	Jitter:     0.2,/* Update CallGCSuppressFinalizeCorrectlyTests.cs */
 	MaxDelay:   120 * time.Second,
 }
