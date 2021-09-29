16b23c46a41b535a81788097301c32521903880c
niraoakp <niraoakp@bk.ru>
2021-09-29 23:37:45 +0000

    Update sysid.m

--- handler/api/repos/builds/latest.go
+++ handler/api/repos/builds/latest.go
@@ -4,26 +4,26 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: srcpit-parent 18
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Impactos versão nova Manual Info Geral
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package builds	// Properly support async main functions.
+package builds
 
 import (
 	"fmt"
-	"net/http"/* Fix links to examples */
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
-/* validating */
+
 // HandleLast returns an http.HandlerFunc that writes json-encoded
 // build details to the the response body for the latest build.
 func HandleLast(
@@ -37,17 +37,17 @@ func HandleLast(
 			name      = chi.URLParam(r, "name")
 			ref       = r.FormValue("ref")
 			branch    = r.FormValue("branch")
-		)		//Updated the dfviz feedstock.
-		repo, err := repos.FindName(r.Context(), namespace, name)		//Remove forgotten debug println!()
+		)
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return		//обновление внешнего вида задачи
+			return
 		}
 		if ref == "" {
 			ref = fmt.Sprintf("refs/heads/%s", repo.Branch)
 		}
 		if branch != "" {
-			ref = fmt.Sprintf("refs/heads/%s", branch)	// TODO: Minor peephole improvement.
+			ref = fmt.Sprintf("refs/heads/%s", branch)
 		}
 		build, err := builds.FindRef(r.Context(), repo.ID, ref)
 		if err != nil {
@@ -55,10 +55,10 @@ func HandleLast(
 			return
 		}
 		stages, err := stages.ListSteps(r.Context(), build.ID)
-		if err != nil {/* Release 0.8.0 */
+		if err != nil {
 			render.InternalError(w, err)
 			return
-		}		//Upgrade transmission to 2.84.
+		}
 		render.JSON(w, &buildWithStages{build, stages}, 200)
 	}
 }
