ff87c673084a25b2de65242fa99a495529e33c7b
niraoakp <niraoakp@bk.ru>
2021-09-29 18:21:33 +0000

    Moved alpha() to string_util.h.

--- pkg/codegen/internal/test/testdata/random-pet.pp.go
+++ pkg/codegen/internal/test/testdata/random-pet.pp.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"	// TODO: +XMonad.Util.XPaste: a module for pasting strings to windows
-)
+	"github.com/pulumi/pulumi-random/sdk/v2/go/random"/* Release version 0.1.6 */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+)/* Enable learning rate selection  */
 
-func main() {/* aff68e64-2e6f-11e5-9284-b827eb9e62be */
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := random.NewRandomPet(ctx, "random_pet", &random.RandomPetArgs{
 			Prefix: pulumi.String("doggo"),
-		})		//make error/warning annotation appearance customizable
+		})
 		if err != nil {
 			return err
 		}
-		return nil	// TODO: hacked by 13860583249@yeah.net
-	})	// TODO: 04ec2e0c-2e60-11e5-9284-b827eb9e62be
-}		//036c2be0-2e47-11e5-9284-b827eb9e62be
+		return nil
+	})
+}
