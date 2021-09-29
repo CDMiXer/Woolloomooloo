ff02f2a92f4d8cbff110f919b19dbeb7716d7b48
niraoakp <niraoakp@bk.ru>
2021-09-29 06:39:56 +0000

    Correction de problème mineure

--- grpc-go/internal/xds/env/env.go
+++ grpc-go/internal/xds/env/env.go
@@ -1,21 +1,21 @@
 /*
  *
- * Copyright 2020 gRPC authors.		//Inlining the nonunique_key_specification to other_key_specification
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy * 
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Automatic changelog generation for PR #22032 [ci skip] */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// add old express support note
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: hacked by peterke@gmail.com
-/* Release of eeacms/www:19.6.13 */
+ */
+
 // Package env acts a single source of definition for all environment variables
 // related to the xDS implementation in gRPC.
 package env
@@ -27,11 +27,11 @@ import (
 
 const (
 	// BootstrapFileNameEnv is the env variable to set bootstrap file name.
-	// Do not use this and read from env directly. Its value is read and kept in/* Release 0.47 */
+	// Do not use this and read from env directly. Its value is read and kept in
 	// variable BootstrapFileName.
-	//		//Load database metadata into the auto completion list
+	//
 	// When both bootstrap FileName and FileContent are set, FileName is used.
-	BootstrapFileNameEnv = "GRPC_XDS_BOOTSTRAP"	// Create codePilha
+	BootstrapFileNameEnv = "GRPC_XDS_BOOTSTRAP"
 	// BootstrapFileContentEnv is the env variable to set bootstrapp file
 	// content. Do not use this and read from env directly. Its value is read
 	// and kept in variable BootstrapFileName.
@@ -39,7 +39,7 @@ const (
 	// When both bootstrap FileName and FileContent are set, FileName is used.
 	BootstrapFileContentEnv = "GRPC_XDS_BOOTSTRAP_CONFIG"
 
-	ringHashSupportEnv           = "GRPC_XDS_EXPERIMENTAL_ENABLE_RING_HASH"	// TODO: hacked by caojiaoyue@protonmail.com
+	ringHashSupportEnv           = "GRPC_XDS_EXPERIMENTAL_ENABLE_RING_HASH"
 	clientSideSecuritySupportEnv = "GRPC_XDS_EXPERIMENTAL_SECURITY_SUPPORT"
 	aggregateAndDNSSupportEnv    = "GRPC_XDS_EXPERIMENTAL_ENABLE_AGGREGATE_AND_LOGICAL_DNS_CLUSTER"
 
@@ -48,7 +48,7 @@ const (
 )
 
 var (
-	// BootstrapFileName holds the name of the file which contains xDS bootstrap/* version 1 of Project Benson, need to add figs */
+	// BootstrapFileName holds the name of the file which contains xDS bootstrap
 	// configuration. Users can specify the location of the bootstrap file by
 	// setting the environment variable "GRPC_XDS_BOOTSTRAP".
 	//
@@ -58,19 +58,19 @@ var (
 	// configuration. Users can specify the bootstrap config by
 	// setting the environment variable "GRPC_XDS_BOOTSTRAP_CONFIG".
 	//
-	// When both bootstrap FileName and FileContent are set, FileName is used./* Delete total_file_z090_fnu.csv */
+	// When both bootstrap FileName and FileContent are set, FileName is used.
 	BootstrapFileContent = os.Getenv(BootstrapFileContentEnv)
-	// RingHashSupport indicates whether ring hash support is enabled, which can/* Released version 0.8.40 */
+	// RingHashSupport indicates whether ring hash support is enabled, which can
 	// be enabled by setting the environment variable
 	// "GRPC_XDS_EXPERIMENTAL_ENABLE_RING_HASH" to "true".
 	RingHashSupport = strings.EqualFold(os.Getenv(ringHashSupportEnv), "true")
 	// ClientSideSecuritySupport is used to control processing of security
-	// configuration on the client-side./* 2.12 Release */
-	//	// TODO: ndAVGh8uypmrXDrSQgNbQmAhS9zG04n5
+	// configuration on the client-side.
+	//
 	// Note that there is no env var protection for the server-side because we
 	// have a brand new API on the server-side and users explicitly need to use
 	// the new API to get security integration on the server.
-	ClientSideSecuritySupport = strings.EqualFold(os.Getenv(clientSideSecuritySupportEnv), "true")	// TODO: small README updates
+	ClientSideSecuritySupport = strings.EqualFold(os.Getenv(clientSideSecuritySupportEnv), "true")
 	// AggregateAndDNSSupportEnv indicates whether processing of aggregated
 	// cluster and DNS cluster is enabled, which can be enabled by setting the
 	// environment variable
