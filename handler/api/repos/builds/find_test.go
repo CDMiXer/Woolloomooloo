3bdc516259afc66c7bb6967143ac890e22a8c640
niraoakp <niraoakp@bk.ru>
2021-09-29 15:07:29 +0000

    rev 795028

--- handler/api/repos/builds/find_test.go
+++ handler/api/repos/builds/find_test.go
@@ -1,57 +1,57 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//ajustes nos predicates das tarefas
+
 package builds
-		//typo in markdown link syntax
-import (		//istream/sink_fd: indent with tabs
-	"context"/* Reorganize general.yml */
-	"encoding/json"
-	"net/http/httptest"	// adding sorted dictionary unit for REST arguments
-	"testing"
+/* Release Notes: rebuild HTML notes for 3.4 */
+import (
+	"context"
+	"encoding/json"		//Rename portfolio-webpage.html to travel-photo.html
+	"net/http/httptest"
+	"testing"		//Update subtitle_downloader.py
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"		//493f84f6-2e64-11e5-9284-b827eb9e62be
-	"github.com/google/go-cmp/cmp"		//Merge "Separate log collection into its own script"
-)
+	"github.com/golang/mock/gomock"/* make an outer div wrapper */
+	"github.com/google/go-cmp/cmp"
+)	// TODO: hacked by mowrain@yandex.com
 
 func TestFind(t *testing.T) {
-	controller := gomock.NewController(t)		//ref #9571: added empty milestone
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)/* Release of eeacms/www-devel:20.3.11 */
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(mockRepo, nil)
-/* [artifactory-release] Release version 3.1.9.RELEASE */
-	builds := mock.NewMockBuildStore(controller)	// update cancer stats
-	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuild, nil)		//Fix BZ1159771
+/* file title */
+	builds := mock.NewMockBuildStore(controller)
+	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuild, nil)
 
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")	// 244b2aba-2e50-11e5-9284-b827eb9e62be
 	c.URLParams.Add("number", "1")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
-/* Delete FeatureAlertsandDataReleases.rst */
+)	
+		//Merge "Convert Special:Hieroglyphs to OOUI"
 	HandleFind(repos, builds, stages)(w, r)
 
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)	// Improve test coverage of utility method.
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}	// TODO: hacked by peterke@gmail.com
+	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}		//added test generator + reafactoring
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)/* update clickhouse desc */
+		t.Errorf(diff)
 	}
 }
 
@@ -62,7 +62,7 @@ func TestFind_BadRequest(t *testing.T) {
 	c.URLParams.Add("number", "one")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)/* Release Tag V0.30 (additional changes) */
+	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
@@ -70,14 +70,14 @@ func TestFind_BadRequest(t *testing.T) {
 	HandleFind(nil, nil, nil)(w, r)
 
 	if got, want := w.Code, 400; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)/* Merged Development into Release */
 	}
-
+/* Release notes for 1.0.83 */
 	got, want := new(errors.Error), &errors.Error{Message: "strconv.ParseInt: parsing \"one\": invalid syntax"}
-	json.NewDecoder(w.Body).Decode(&got)
+	json.NewDecoder(w.Body).Decode(&got)	// TODO: will be fixed by xiemengjun@gmail.com
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
-	}
+	}/* Release: Making ready to release 3.1.3 */
 }
 
 func TestFind_RepoNotFound(t *testing.T) {
