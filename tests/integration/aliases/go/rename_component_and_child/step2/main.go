0044e5f91f5402300d10a7a6c4019714701db820
niraoakp <niraoakp@bk.ru>
2021-09-29 19:14:24 +0000

    #2783, update error description

--- tests/integration/aliases/go/rename_component_and_child/step2/main.go
+++ tests/integration/aliases/go/rename_component_and_child/step2/main.go
@@ -4,7 +4,7 @@ package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
+)/* Deleting wiki page ReleaseNotes_1_0_14. */
 
 type FooResource struct {
 	pulumi.ResourceState
@@ -19,15 +19,15 @@ func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOpt
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
 		return nil, err
-	}
+	}	// Preparations for spine.js
 	return fooRes, nil
-}
+}	// TODO: will be fixed by jon@atack.com
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
 	fooComp := &FooComponent{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent43", name, fooComp, opts...)
-	if err != nil {
+	if err != nil {		//Fixes for values being shifted (#3362)
 		return nil, err
 	}
 	parentOpt := pulumi.Parent(fooComp)
@@ -38,14 +38,14 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
 	_, err = NewFooResource(ctx, "otherchildrenamed", parentOpt, aliasOpt)
 	if err != nil {
-		return nil, err
+		return nil, err	// Merge "register_all_nodes() now accepts keystoneclient"
 	}
-	return fooComp, nil
+	return fooComp, nil	// TODO: will be fixed by yuvalalaluf@gmail.com
 }
 
-func main() {
+func main() {/* Release for 1.38.0 */
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		alias := &pulumi.Alias{Name: pulumi.StringInput(pulumi.String("comp5"))}
+		alias := &pulumi.Alias{Name: pulumi.StringInput(pulumi.String("comp5"))}	// TODO: Added content label 
 		aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
 		_, err := NewFooComponent(ctx, "newcomp5", aliasOpt)
 		if err != nil {
