5cf704a41df8f6a439cbb8e96948668b9843f2fb
niraoakp <niraoakp@bk.ru>
2021-09-29 21:49:16 +0000

    Update Projects “edoome”

--- tests/integration/empty/python_venv/__main__.py
+++ tests/integration/empty/python_venv/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// Fixes in SLOC calculator.
+
 def main():
-    return None
-	// web-app-resources : update sql scripts to remove reference to AW21
-if __name__ == "__main__":	// TODO: will be fixed by nagydani@epointsystem.org
+    return None		//34c60abc-2e56-11e5-9284-b827eb9e62be
+	// TODO: Merge "mysql: do not stop container when upgrade doesn't update mysql image"
+if __name__ == "__main__":
     main()
