05aca867392abafff8e57946ff0f3e0bf069d3aa
niraoakp <niraoakp@bk.ru>
2021-09-29 01:11:26 +0000

    add dependency section and fix indent

--- operator/runner/runner_test.go
+++ operator/runner/runner_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License	// Expanded build steps
+// that can be found in the LICENSE file./* Release 2.43.3 */
 
-package runner
-
+package runner		//Added JAXB accessor information to the SingleFile class
+/* Delete reVision.exe - Release.lnk */
 import (
 	"io/ioutil"
 
 	"github.com/sirupsen/logrus"
-)
+)		//show upload_max_filesize on FileUploader error
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
