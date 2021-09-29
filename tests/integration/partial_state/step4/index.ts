2cb12886a4356255f6c8ef067e929bd192524392
niraoakp <niraoakp@bk.ru>
2021-09-29 13:26:55 +0000

    Ticket #2342

--- tests/integration/partial_state/step4/index.ts
+++ tests/integration/partial_state/step4/index.ts
@@ -2,8 +2,8 @@
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-/* Creating a simple cmd to run the functional tests. */
+
 // resource "not-doomed" is updated, but the update partially fails.
-const a = new Resource("not-doomed", 4);/* Release Unova Cap Pikachu */
+const a = new Resource("not-doomed", 4);
 
 // "a" should still be in the checkpoint with its new value.
