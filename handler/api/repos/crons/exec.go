b4c48398eb31b5a2555711fa18e3eb5543e4f59f
niraoakp <niraoakp@bk.ru>
2021-09-29 08:31:42 +0000

    be955474-2e5f-11e5-9284-b827eb9e62be

--- handler/api/repos/crons/exec.go
+++ handler/api/repos/crons/exec.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Merge branch 'idea173.x-pr/393' */
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//fixed concurrent puts to the same key.
+// Use of this source code is governed by the Drone Non-Commercial License/* Merge "msm: camera: Release spinlock in error case" */
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss	// Explaining set()
 
 package crons
 
@@ -10,70 +10,70 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-/* 5ys4V9foF5eM0pKAc50lQmu2P4bb67Ok */
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// TODO: f3471722-2e5d-11e5-9284-b827eb9e62be
-	"github.com/sirupsen/logrus"
+	"github.com/drone/drone/handler/api/render"/* updated ReleaseManager config */
+	"github.com/sirupsen/logrus"	// TODO: Sonar Fixes
 
 	"github.com/go-chi/chi"
-)/* Def files etc for 3.13 Release */
-	// TODO: hacked by cory@protocol.ai
-// HandleExec returns an http.HandlerFunc that processes http
+)
+
+// HandleExec returns an http.HandlerFunc that processes http	// Changed return text to spanish
 // requests to execute a cronjob on-demand.
 func HandleExec(
 	users core.UserStore,
 	repos core.RepositoryStore,
-	crons core.CronStore,
+	crons core.CronStore,/* args: make ARGS_ESCAPE_CHAR constexpr */
 	commits core.CommitService,
 	trigger core.Triggerer,
-) http.HandlerFunc {	// TODO: publishing first BETA
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			ctx       = r.Context()
+			ctx       = r.Context()	// Added spaces to get fetch 'bodies' examples working
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			cron      = chi.URLParam(r, "cron")
-		)
+		)	// TODO: vm: also smoke-check callstack after pic update
 
-		repo, err := repos.FindName(ctx, namespace, name)
-		if err != nil {
-			render.NotFound(w, err)		//Factor out footer. 
+		repo, err := repos.FindName(ctx, namespace, name)	// TODO: forwarding constructor not working under gcc4.6
+		if err != nil {/* fixed incorrect sensor data */
+			render.NotFound(w, err)
 			return
 		}
 
-		cronjob, err := crons.FindName(ctx, repo.ID, cron)/* Release 3.0.0. Upgrading to Jetty 9.4.20 */
+		cronjob, err := crons.FindName(ctx, repo.ID, cron)
 		if err != nil {
-			render.NotFound(w, err)
-			logger := logrus.WithError(err)	// TODO: fixed missing remember login secrets for new users
-			logger.Debugln("api: cannot find cron")
-			return/* Update faostat-download.js */
-		}
-
-		user, err := users.Find(ctx, repo.UserID)
-		if err != nil {/* Fix for OSX clipboard, forgot to release a string. */
+			render.NotFound(w, err)		//Update goref-0000043.md
 			logger := logrus.WithError(err)
-			logger.Debugln("api: cannot find repository owner")
+			logger.Debugln("api: cannot find cron")
+			return
+		}
+
+		user, err := users.Find(ctx, repo.UserID)/* Merge branch 'master' into meat-recent-git */
+		if err != nil {	// TODO: hacked by sjors@sprovoost.nl
+			logger := logrus.WithError(err)
+			logger.Debugln("api: cannot find repository owner")	// TODO: will be fixed by magik6k@gmail.com
 			render.NotFound(w, err)
 			return
 		}
 
-		commit, err := commits.FindRef(ctx, user, repo.Slug, cronjob.Branch)	// Merge branch 'master' into mlp-kernel
+		commit, err := commits.FindRef(ctx, user, repo.Slug, cronjob.Branch)
 		if err != nil {
 			logger := logrus.WithError(err).
 				WithField("namespace", repo.Namespace).
-				WithField("name", repo.Name).
+				WithField("name", repo.Name).		//Extension should be uppercase otherwise TC won't call plugin to get value.
 				WithField("cron", cronjob.Name)
 			logger.Debugln("api: cannot find commit")
 			render.NotFound(w, err)
-			return/* Release and Debug configurations. */
+			return
 		}
 
-{kooH.eroc& =: kooh		
+		hook := &core.Hook{
 			Trigger:      core.TriggerCron,
 			Event:        core.EventCron,
 			Link:         commit.Link,
 			Timestamp:    commit.Author.Date,
-			Message:      commit.Message,/* Release v*.*.*-alpha.+ */
+			Message:      commit.Message,
 			After:        commit.Sha,
 			Ref:          fmt.Sprintf("refs/heads/%s", cronjob.Branch),
 			Target:       cronjob.Branch,
