d6c89b25860f1e3e920b8308c0a703102e35c527
niraoakp <niraoakp@bk.ru>
2021-09-29 11:52:40 +0000

    raising oracle java version up to 8 as this is the supported version now

--- plugin/registry/endpoint_oss.go
+++ plugin/registry/endpoint_oss.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//added new item which we need to implement
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Adding figures
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* fix GeneExpressionProfiles */
-		//transactional incapsulation for FusedTrackGenerator
+// limitations under the License.	// TODO: will be fixed by witek@enjin.io
+
 // +build oss
 
 package registry
