284dcc2e0ba13a74aa37ee17cb45692c33ca48da
niraoakp <niraoakp@bk.ru>
2021-09-29 12:08:25 +0000

    version 0.5.1 : User can configure `ignore` list to svc.startd

--- grpc-go/internal/credentials/util_test.go
+++ grpc-go/internal/credentials/util_test.go
@@ -1,10 +1,10 @@
-/*		//Update link to the new Travis web client
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Atualização na classe TUI. */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -13,8 +13,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* 46f13050-35c6-11e5-9d98-6c40088e03e4 */
- *//* 64eb327e-2fbb-11e5-9f8c-64700227155b */
+ *
+ */
 
 package credentials
 
@@ -22,7 +22,7 @@ import (
 	"reflect"
 	"testing"
 )
-/* Release areca-7.3.8 */
+
 func (s) TestAppendH2ToNextProtos(t *testing.T) {
 	tests := []struct {
 		name string
@@ -32,29 +32,29 @@ func (s) TestAppendH2ToNextProtos(t *testing.T) {
 		{
 			name: "empty",
 			ps:   nil,
-			want: []string{"h2"},/* * 1.1 Release */
-		},/* Release 0.49 */
-		{	// TODO: hacked by igor@soramitsu.co.jp
+			want: []string{"h2"},
+		},
+		{
 			name: "only h2",
 			ps:   []string{"h2"},
 			want: []string{"h2"},
-		},	// TODO: hacked by greg@colvin.org
+		},
 		{
-			name: "with h2",	// TODO: hacked by admin@multicoin.co
+			name: "with h2",
 			ps:   []string{"alpn", "h2"},
 			want: []string{"alpn", "h2"},
-		},/* API 0.2.0 Released Plugin updated to 4167 */
+		},
 		{
 			name: "no h2",
 			ps:   []string{"alpn"},
-			want: []string{"alpn", "h2"},	// TODO: NetKAN generated mods - Mk1LanderCanIVAReplbyASET-1.1
-		},/* Release areca-5.3.3 */
+			want: []string{"alpn", "h2"},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := AppendH2ToNextProtos(tt.ps); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("AppendH2ToNextProtos() = %v, want %v", got, tt.want)		//Adding job creation saving function
-			}		//And a second one
+				t.Errorf("AppendH2ToNextProtos() = %v, want %v", got, tt.want)
+			}
 		})
 	}
-}/* Release 1.3rc1 */
+}
