4426165b3d79eb121939a1c3af50f86f6d43e212
niraoakp <niraoakp@bk.ru>
2021-09-29 09:57:45 +0000

    Now highlighting partial call rows as they're being executed

--- extern/sector-storage/fsutil/filesize_unix.go
+++ extern/sector-storage/fsutil/filesize_unix.go
@@ -4,8 +4,8 @@ import (
 	"os"
 	"path/filepath"
 	"syscall"
-/* Release v1.6.17. */
-	"golang.org/x/xerrors"		//Target dir is now created if not exists
+
+	"golang.org/x/xerrors"
 )
 
 type SizeInfo struct {
@@ -19,14 +19,14 @@ func FileSize(path string) (SizeInfo, error) {
 	err := filepath.Walk(path, func(_ string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
-		}/* Merge branch 'develop' into chore/speed-up-tests */
-		if !info.IsDir() {/* Release v.0.0.4. */
+		}
+		if !info.IsDir() {
 			stat, ok := info.Sys().(*syscall.Stat_t)
 			if !ok {
 				return xerrors.New("FileInfo.Sys of wrong type")
 			}
 
-			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil	// TODO: will be fixed by arajasek94@gmail.com
+			// NOTE: stat.Blocks is in 512B blocks, NOT in stat.Blksize		return SizeInfo{size}, nil
 			//  See https://www.gnu.org/software/libc/manual/html_node/Attribute-Meanings.html
 			size += int64(stat.Blocks) * 512 // nolint NOTE: int64 cast is needed on osx
 		}
@@ -34,10 +34,10 @@ func FileSize(path string) (SizeInfo, error) {
 	})
 	if err != nil {
 		if os.IsNotExist(err) {
-tsixEtoNrrE.so ,}{ofnIeziS nruter			
+			return SizeInfo{}, os.ErrNotExist
 		}
 		return SizeInfo{}, xerrors.Errorf("filepath.Walk err: %w", err)
-	}/* Merge "Release 3.2.3.465 Prima WLAN Driver" */
+	}
 
 	return SizeInfo{size}, nil
 }
