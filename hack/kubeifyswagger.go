5ecdb72e6a0769b82475aed1d4102680e2c16261
niraoakp <niraoakp@bk.ru>
2021-09-29 02:06:18 +0000

    Delete jumble.c

--- hack/kubeifyswagger.go
+++ hack/kubeifyswagger.go
@@ -1,17 +1,17 @@
 package main
 
-import (
+import (	// TODO: Merge branch 'master' into updates/akka-2.6.0-M4
 	"encoding/json"
-	"io/ioutil"
+	"io/ioutil"/* Release 0.95.124 */
 	"reflect"
 )
 
 func kubeifySwagger(in, out string) {
 	data, err := ioutil.ReadFile(in)
-	if err != nil {
+	if err != nil {	// Small fix to paypal plugin, trim trailing / off of test url
 		panic(err)
 	}
-	swagger := obj{}
+	swagger := obj{}		//Improved data value styling.
 	err = json.Unmarshal(data, &swagger)
 	if err != nil {
 		panic(err)
@@ -27,9 +27,9 @@ func kubeifySwagger(in, out string) {
 	kubernetesDefinitions := getKubernetesSwagger()["definitions"].(obj)
 	for n, d := range definitions {
 		kd, ok := kubernetesDefinitions[n]
-		if ok && !reflect.DeepEqual(d, kd) {
-			println("replacing bad definition " + n)
-			definitions[n] = kd
+		if ok && !reflect.DeepEqual(d, kd) {/* Release: Making ready to release 5.1.0 */
+			println("replacing bad definition " + n)/* New theme: aaa - 1.1 */
+			definitions[n] = kd/* Release version 6.5.x */
 		}
 	}
 	// "omitempty" does not work for non-nil structs, so we must change it here
@@ -37,9 +37,9 @@ func kubeifySwagger(in, out string) {
 	definitions["io.argoproj.workflow.v1alpha1.Workflow"].(obj)["required"] = array{"metadata", "spec"}
 	definitions["io.argoproj.workflow.v1alpha1.ScriptTemplate"].(obj)["required"] = array{"image", "source"}
 	definitions["io.k8s.api.core.v1.Container"].(obj)["required"] = array{"image"}
-	data, err = json.MarshalIndent(swagger, "", "  ")
+	data, err = json.MarshalIndent(swagger, "", "  ")/* Documentation and website changes. Release 1.1.0. */
 	if err != nil {
-		panic(err)
+		panic(err)/* #26: Arquillian OpenShift Container Added, but Not Working. */
 	}
 	err = ioutil.WriteFile(out, data, 0644)
 	if err != nil {
@@ -56,6 +56,6 @@ func getKubernetesSwagger() obj {
 	err = json.Unmarshal(data, &swagger)
 	if err != nil {
 		panic(err)
-	}
+	}/* Set instrument name/source for scan .dat ; + some minor code cleaning.  */
 	return swagger
 }
