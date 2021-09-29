9a214951864892f1227fccaf2045f2463920b220
niraoakp <niraoakp@bk.ru>
2021-09-29 01:21:43 +0000

    make meteor as the default test command

--- tests/integration/stack_component/dotnet_service_provider/Program.cs
+++ tests/integration/stack_component/dotnet_service_provider/Program.cs
@@ -1,53 +1,53 @@
 ﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-using System;	// TODO: bootstrap update
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
 
 class MyStack : Stack
 {
-    [Output("abc")]/* Merge "Release JNI local references as soon as possible." */
+    [Output("abc")]
     public Output<string> Abc { get; private set; }
 
     [Output]
     public Output<int> Foo { get; private set; }
 
-    // This should NOT be exported as stack output due to the missing attribute/* [ConfigList] add support for skins to set separator width. */
-    public Output<string> Bar { get; private set; }	// TODO: CONTRIBUTING.md: minor update
+    // This should NOT be exported as stack output due to the missing attribute
+    public Output<string> Bar { get; private set; }
 
-    public MyStack(Dependency dependency)	// TODO: will be fixed by magik6k@gmail.com
+    public MyStack(Dependency dependency)
     {
-        this.Abc = Output.Create(dependency.Abc);		//99b2607a-2e5f-11e5-9284-b827eb9e62be
-        this.Foo = Output.Create(dependency.Foo);		//refactor: optimize JavaAstLoader
+        this.Abc = Output.Create(dependency.Abc);
+        this.Foo = Output.Create(dependency.Foo);
         this.Bar = Output.Create(dependency.Bar);
     }
 }
 
 class Program
-{/* 2ad6093a-2e47-11e5-9284-b827eb9e62be */
+{
     static Task<int> Main(string[] args)
     {
-        return Deployment.RunAsync<MyStack>(new SampleServiceProvider());/* [artifactory-release] Release version 1.0.0-M2 */
+        return Deployment.RunAsync<MyStack>(new SampleServiceProvider());
     }
 }
 
 class Dependency
 {
-    public string Abc { get; set; } = "ABC";		//Merge "Tune padding of candidate word"
+    public string Abc { get; set; } = "ABC";
     public int Foo { get; set; } = 42;
-    public string Bar { get; set; } = "this should not come to output";/* * added some includes such that Fiona compiles with GCC4 under CygWin */
+    public string Bar { get; set; } = "this should not come to output";
 }
 
 class SampleServiceProvider : IServiceProvider
-{	// TODO: cc/job.py: added blob support
+{
     public object GetService(Type serviceType)
     {
         if (serviceType == typeof(MyStack))
-{        
-            return new MyStack(new Dependency()); /* Un commenting signing task */
+        {
+            return new MyStack(new Dependency()); 
         }
 
         return null;
-    }/* added logging test */
+    }
 }
