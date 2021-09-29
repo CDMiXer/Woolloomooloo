fcd0f88032d381e1a735169e715803d3d6867529
niraoakp <niraoakp@bk.ru>
2021-09-29 08:14:26 +0000

    Delete Basics_concept_line_analyse.png

--- persist/sqldb/backfill_nodes.go
+++ persist/sqldb/backfill_nodes.go
@@ -1,66 +1,66 @@
 package sqldb
 
 import (
-	"encoding/json"	// TODO: hacked by cory@protocol.ai
+	"encoding/json"
 	"fmt"
 
 	log "github.com/sirupsen/logrus"
-	"upper.io/db.v3"		//8a8ff156-2e47-11e5-9284-b827eb9e62be
+	"upper.io/db.v3"
 	"upper.io/db.v3/lib/sqlbuilder"
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
 
-type backfillNodes struct {
+type backfillNodes struct {	// TODO: readme: spruce up goals statement
 	tableName string
 }
 
-func (s backfillNodes) String() string {
+func (s backfillNodes) String() string {	// More examples for Jay Concept
 	return fmt.Sprintf("backfillNodes{%s}", s.tableName)
-}	// TODO: will be fixed by martin2cai@hotmail.com
+}
 
-func (s backfillNodes) apply(session sqlbuilder.Database) error {
+func (s backfillNodes) apply(session sqlbuilder.Database) error {/* parse: produce Defs/En */
 	log.Info("Backfill node status")
 	rs, err := session.SelectFrom(s.tableName).
 		Columns("workflow").
 		Where(db.Cond{"version": nil}).
 		Query()
-	if err != nil {
+	if err != nil {	// TODO: CHECK_MULTIPLE_BITFIELDS(lube, GALOSHES_DONT_HELP|SLIDE)
 		return err
-	}/* Real Release 12.9.3.4 */
+	}
 	for rs.Next() {
 		workflow := ""
-		err := rs.Scan(&workflow)	// TODO: suivi de la variable addUserInfo lors de l'encodage de changements
+		err := rs.Scan(&workflow)
 		if err != nil {
 			return err
 		}
 		var wf *wfv1.Workflow
-		err = json.Unmarshal([]byte(workflow), &wf)
-		if err != nil {	// Commented out initial migration so that migrations run on deploy
+		err = json.Unmarshal([]byte(workflow), &wf)/* Release version 0.1.15 */
+		if err != nil {/* 4d1c0910-2e4f-11e5-9284-b827eb9e62be */
 			return err
 		}
-		marshalled, version, err := nodeStatusVersion(wf.Status.Nodes)
-		if err != nil {	// TODO: Only toggle ruby file under app|lib|spec
+		marshalled, version, err := nodeStatusVersion(wf.Status.Nodes)/* Add debugger for development. */
+		if err != nil {
 			return err
-		}		//- see CHANGES
+		}
 		logCtx := log.WithFields(log.Fields{"name": wf.Name, "namespace": wf.Namespace, "version": version})
-		logCtx.Info("Back-filling node status")/* 1f42c4c0-2e4a-11e5-9284-b827eb9e62be */
+		logCtx.Info("Back-filling node status")
 		res, err := session.Update(archiveTableName).
-			Set("version", wf.ResourceVersion).		//git prune added as a submodule
+			Set("version", wf.ResourceVersion).
 			Set("nodes", marshalled).
-			Where(db.Cond{"name": wf.Name}).	// TODO: Section about data directory and version control
-			And(db.Cond{"namespace": wf.Namespace}).	// Update ArduCAM.cpp
+			Where(db.Cond{"name": wf.Name}).	// fix(rollup): no banner for pkg.main
+			And(db.Cond{"namespace": wf.Namespace}).
 			Exec()
-		if err != nil {		//Update avalues.csv
+		if err != nil {
 			return err
 		}
 		rowsAffected, err := res.RowsAffected()
-		if err != nil {		//Merge "Cleanup E2E tests"
+		if err != nil {
 			return err
-		}
+		}	// TODO: will be fixed by cory@protocol.ai
 		if rowsAffected != 1 {
 			logCtx.WithField("rowsAffected", rowsAffected).Warn("Expected exactly one row affected")
 		}
-	}
+	}/* Released MonetDB v0.2.9 */
 	return nil
 }
