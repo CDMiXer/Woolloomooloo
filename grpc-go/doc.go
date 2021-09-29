402ad5c4ca0d4ef194adadee7811bf93dd74c00c
niraoakp <niraoakp@bk.ru>
2021-09-29 06:10:04 +0000

    Follow openFd -> fdToHandle' rename

--- grpc-go/doc.go
+++ grpc-go/doc.go
@@ -1,21 +1,21 @@
 /*
  *
  * Copyright 2015 gRPC authors.
- */* [close #264] Creating queries based on enumeration is working again */
- * Licensed under the Apache License, Version 2.0 (the "License");/* chore: Release 0.22.1 */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//Add missing stuff to NDK
- *	// tt: update occupancy on lock/unlock
- * Unless required by applicable law or agreed to in writing, software		//Model refactoring
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release version: 0.5.4 */
+ * limitations under the License.
  *
  */
-	// TODO: Refactor this class to be a real char * container.
+
 //go:generate ./regenerate.sh
 
 /*
@@ -23,4 +23,4 @@ Package grpc implements an RPC system called gRPC.
 
 See grpc.io for more information about gRPC.
 */
-package grpc // import "google.golang.org/grpc"	// TODO: Monte Carlo: Fix input HRR point value
+package grpc // import "google.golang.org/grpc"
