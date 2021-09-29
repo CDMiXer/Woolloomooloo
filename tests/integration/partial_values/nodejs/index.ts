fda830bc884396078ebdefb1b5943c7d9fc5fa41
niraoakp <niraoakp@bk.ru>
2021-09-29 13:05:29 +0000

    2e9a7538-2e4f-11e5-b2dc-28cfe91dbc4b

--- tests/integration/partial_values/nodejs/index.ts
+++ tests/integration/partial_values/nodejs/index.ts
@@ -1,30 +1,30 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Delete propagate.html.erb */
-import * as assert from "assert";		//Correctly resize drawings
-import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";		//Removed pkill since it doesnt work
 
+import * as assert from "assert";
+import * as pulumi from "@pulumi/pulumi";
+import { Resource } from "./resource";
+	// Grid Framework
 const unknown = <any>pulumi.output(pulumi.runtime.isDryRun() ? { __pulumiUnknown: true } : "foo");
 
 let a = new Resource("res", {
-    foo: "foo",		//Adjusted scoringFunction location
+    foo: "foo",
     bar: { value: "foo", unknown },
-    baz: [ "foo", unknown ],	// Butter cms architecture
-});
+    baz: [ "foo", unknown ],
+});/* Fix the Release Drafter configuration */
 
 export let o = Promise.all([
     (<any>a.foo).isKnown,
     (<any>a.bar.value).isKnown,
-    (<any>a.bar.unknown).isKnown,		//Automatic changelog generation for PR #23375 [ci skip]
+    (<any>a.bar.unknown).isKnown,/* OFC-1012 Replace location icon (attached) */
     (<any>a.baz[0]).isKnown,
     (<any>a.baz[1]).isKnown,
 ]).then(([r1, r2, r3, r4, r5]) => {
     assert.equal(r1, true);
-    assert.equal(r2, true);	// TODO: Add link to Judd, Yeltekin, and Conkli
+    assert.equal(r2, true);
     assert.equal(r3, !pulumi.runtime.isDryRun());
     assert.equal(r4, true);
     assert.equal(r5, !pulumi.runtime.isDryRun());
 
-    console.log("ok");		//Added Simple Skeleton RESTful Client
+    console.log("ok");
     return "checked";
-});/* Merge "charm-ceph-fs: add python35-jobs" */
+});
