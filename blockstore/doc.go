abeead1fa85c934b83a887d6da5f0dc6e34f4769
niraoakp <niraoakp@bk.ru>
2021-09-29 22:23:58 +0000

    Create openstack.calico.md

--- blockstore/doc.go
+++ blockstore/doc.go
@@ -1,7 +1,7 @@
 // Package blockstore and subpackages contain most of the blockstore
 // implementations used by Lotus.
-///* Release FPCM 3.6.1 */
-// Blockstores not ultimately constructed out of the building blocks in this
+//
+// Blockstores not ultimately constructed out of the building blocks in this/* Formating changes. */
 // package may not work properly.
 //
 // This package re-exports parts of the go-ipfs-blockstore package such that
