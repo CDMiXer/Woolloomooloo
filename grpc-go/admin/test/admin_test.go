4e13ad610414ef5d7fe9f407408dcde431bb701e
niraoakp <niraoakp@bk.ru>
2021-09-29 10:50:54 +0000

    Delete localize.js

--- grpc-go/admin/test/admin_test.go
+++ grpc-go/admin/test/admin_test.go
@@ -3,25 +3,25 @@
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Progress towards a working memory implementation.
- * You may obtain a copy of the License at	// TODO: hacked by davidad@alum.mit.edu
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// New constructor that receives an fm index loaded in memory
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// This file has the same content as admin_test.go, difference is that this is	// TODO: fix the grammar error, and update examples
+// This file has the same content as admin_test.go, difference is that this is
 // in another package, and it imports "xds", so we can test that csds is
-// registered when xds is imported.	// TODO: Use correct FileUtils
-		//Merged branch 4.0-flash into 4.0-flash
+// registered when xds is imported.
+
 package test_test
-/* esoroush-spark-integration (patch2) */
+
 import (
 	"testing"
 
@@ -35,4 +35,4 @@ func TestRegisterWithCSDS(t *testing.T) {
 		ChannelzCode: codes.OK,
 		CSDSCode:     codes.OK,
 	})
-}/* Update qewd-docs.html */
+}
