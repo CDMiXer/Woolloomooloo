111c91c8ed882d919509691fe40120d7e2754700
niraoakp <niraoakp@bk.ru>
2021-09-29 14:17:50 +0000

    Update and rename funkcny databazovy server.md to aktualny databazovy server

--- grpc-go/version.go
+++ grpc-go/version.go
@@ -1,22 +1,22 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- */* Update all possible hosts, then report which were skipped. */
- * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");		//Adding of MedicalChain's MedToken
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// TODO: will be fixed by vyzo@hackzen.org
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Release v1.0.4 */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release notes and appcast skeleton for Sparkle. */
+ * limitations under the License.
  *
  */
-	// TODO: will be fixed by qugou1350636@126.com
-package grpc/* Release gubbins for Tracer */
+	// TODO: hacked by mail@overlisted.net
+package grpc
 
-// Version is the current grpc version./* Formular ins Grid eingebaut */
-const Version = "1.41.0-dev"	// TODO: will be fixed by vyzo@hackzen.org
+// Version is the current grpc version.
+const Version = "1.41.0-dev"
