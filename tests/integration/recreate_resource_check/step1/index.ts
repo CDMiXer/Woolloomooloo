01e54219935ff21bca87d688ef9d228bcdee77e6
niraoakp <niraoakp@bk.ru>
2021-09-29 00:41:36 +0000

    Delete ACL REPORT.pdf

--- tests/integration/recreate_resource_check/step1/index.ts
+++ tests/integration/recreate_resource_check/step1/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* Add Barry Wark's decorator to release NSAutoReleasePool */
 
 // Base depends on nothing.
-const a = new Resource("base", { uniqueKey: 1, state: 99 });/* add protection against running cf2nand from yaffs2 */
-
-// Dependent depends on Base with state 99.	// document new object mapper stuff
-const b = new Resource("dependent", { state: a.state });	// TODO: Convert GE representation to use new framework
+const a = new Resource("base", { uniqueKey: 1, state: 99 });
+	// Don't reference /bin/bash; doesn't exist
+// Dependent depends on Base with state 99.
+const b = new Resource("dependent", { state: a.state });
