aec258321b87257c8271fb3a5cdad551e402f124
niraoakp <niraoakp@bk.ru>
2021-09-29 16:27:06 +0000

    Polish up the translation.

--- handler/api/repos/builds/pulls/create.go
+++ handler/api/repos/builds/pulls/create.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* add comment about userinfo */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Release of eeacms/forests-frontend:2.0-beta.1 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Fixes scrollbars in dialogs */
-// See the License for the specific language governing permissions and/* Reorder states, add ID information to paths */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-
+		//Added junit to the classpath as a jar, instead of an eclipse reference.
 package pulls
