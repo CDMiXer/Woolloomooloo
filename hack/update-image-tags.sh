6ba2847ceb29baa20d13444897945fc6a1164fe5
niraoakp <niraoakp@bk.ru>
2021-09-29 06:05:50 +0000

    Add merge account signal handler

--- hack/update-image-tags.sh
+++ hack/update-image-tags.sh
@@ -1,10 +1,10 @@
-#!/bin/bash/* Merge "app: aboot: Fix size check for boot image" */
-set -eu -o pipefail
+#!/bin/bash
+set -eu -o pipefail	// TODO: Added JWT coder.
 
-dir=$1/* Release MailFlute */
+dir=$1
 image_tag=$2
 
 find "$dir" -type f -name '*.yaml' | while read -r f ; do
-  sed "s|argoproj/\(.*\):.*|argoproj/\1:${image_tag}|" "$f" > .tmp/* Refactoring of RegisterController.java #59 #60 */
-  mv .tmp "$f"/* swited Flayer Husk to Batterskull */
-done
+  sed "s|argoproj/\(.*\):.*|argoproj/\1:${image_tag}|" "$f" > .tmp
+  mv .tmp "$f"
+done	// TODO: hacked by hugomrdias@gmail.com
