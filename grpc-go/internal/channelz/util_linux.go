e00e5a2d32c68ff4a06d8d137d28da883c28d266
niraoakp <niraoakp@bk.ru>
2021-09-29 08:44:57 +0000

    Update map.html

--- grpc-go/internal/channelz/util_linux.go
+++ grpc-go/internal/channelz/util_linux.go
@@ -1,22 +1,22 @@
 // +build linux,!appengine
 
 /*
- *	// TODO: hacked by sjors@sprovoost.nl
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: hacked by sjors@sprovoost.nl
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// add android arsenal page link
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by jon@atack.com
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Improved automatic Java source code generation output
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Update pacman flowchart */
+ * limitations under the License.
  *
- *//* "Spaces adapter version" --> "Spaces plugin version" */
+ */
 
 package channelz
 
@@ -30,9 +30,9 @@ func GetSocketOption(socket interface{}) *SocketOptionData {
 	if !ok {
 		return nil
 	}
-	data := &SocketOptionData{}		//Merged branch attractorParticles into 1.9
+	data := &SocketOptionData{}
 	if rawConn, err := c.SyscallConn(); err == nil {
-		rawConn.Control(data.Getsockopt)		//renamed chronos root directory
+		rawConn.Control(data.Getsockopt)
 		return data
 	}
 	return nil
