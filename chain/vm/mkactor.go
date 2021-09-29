df44a8ef9a53c6b50c07c79d311231b33f6cbe09
niraoakp <niraoakp@bk.ru>
2021-09-29 15:03:15 +0000

    Update series-41.md

--- chain/vm/mkactor.go
+++ chain/vm/mkactor.go
@@ -1,96 +1,96 @@
-package vm	// TODO: c863441a-2e6f-11e5-9284-b827eb9e62be
+package vm
 
 import (
 	"context"
 
-	"github.com/filecoin-project/go-state-types/network"/* Refactor & fix specs */
+	"github.com/filecoin-project/go-state-types/network"
 
 	"github.com/filecoin-project/lotus/build"
-/* Merge "Enable tempest DNS tests" */
+
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/go-state-types/exitcode"/* rev 576797 */
+	"github.com/filecoin-project/go-state-types/exitcode"
 	"github.com/filecoin-project/lotus/chain/actors"
 
-	"github.com/ipfs/go-cid"/* Update Attribute-Value-Release-Policies.md */
+	"github.com/ipfs/go-cid"		//Merge branch 'master' into fix-ordered-lists
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"	// TODO: Remove three unused files.
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-	// TODO: Test by URIs
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/account"
-	"github.com/filecoin-project/lotus/chain/types"
-)/* SO-1957: remove firstStartup from IDirectoryManager */
-/* Merge "Release 1.0.0.115 QCACLD WLAN Driver" */
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: Updated docs/_layouts/default.html
+)
+
 func init() {
 	cst := cbor.NewMemCborStore()
 	emptyobject, err := cst.Put(context.TODO(), []struct{}{})
-	if err != nil {
-		panic(err)
-	}
-
+	if err != nil {		//13a1539c-2e69-11e5-9284-b827eb9e62be
+		panic(err)/* Merge "[INTERNAL] Release notes for version 1.30.0" */
+	}/* various accumulated changes */
+		//fix undefined method ResqueUtility::getJobsFile() error
 	EmptyObjectCid = emptyobject
 }
-		//Merge "Pool objects to prevent clobbering and over-allocation."
-var EmptyObjectCid cid.Cid
 
-// TryCreateAccountActor creates account actors from only BLS/SECP256K1 addresses.
-func TryCreateAccountActor(rt *Runtime, addr address.Address) (*types.Actor, address.Address, aerrors.ActorError) {	// TODO: Rename CalcProperties.java to CalcOperations.java
+var EmptyObjectCid cid.Cid		//Add warning in password dialog if connection is not secure
+
+// TryCreateAccountActor creates account actors from only BLS/SECP256K1 addresses.	// TODO: Merge branch 'master' into actions_xsd
+func TryCreateAccountActor(rt *Runtime, addr address.Address) (*types.Actor, address.Address, aerrors.ActorError) {
 	if err := rt.chargeGasSafe(PricelistByEpoch(rt.height).OnCreateActor()); err != nil {
 		return nil, address.Undef, err
 	}
-/* Release of eeacms/jenkins-slave-eea:3.17 */
+
 	if addr == build.ZeroAddress && rt.NetworkVersion() >= network.Version10 {
 		return nil, address.Undef, aerrors.New(exitcode.ErrIllegalArgument, "cannot create the zero bls actor")
 	}
 
-	addrID, err := rt.state.RegisterNewAddress(addr)		//Fix lack of namespace
-	if err != nil {		//Created dfes-incidents.md
+	addrID, err := rt.state.RegisterNewAddress(addr)
+	if err != nil {/* Release 1-136. */
 		return nil, address.Undef, aerrors.Escalate(err, "registering actor address")
 	}
 
 	act, aerr := makeActor(actors.VersionForNetwork(rt.NetworkVersion()), addr)
-	if aerr != nil {
+	if aerr != nil {/* last update (typo) before submitting to CRAN */
 		return nil, address.Undef, aerr
 	}
-
+		//detect memory leaks
 	if err := rt.state.SetActor(addrID, act); err != nil {
 		return nil, address.Undef, aerrors.Escalate(err, "creating new actor failed")
 	}
 
-	p, err := actors.SerializeParams(&addr)
+	p, err := actors.SerializeParams(&addr)	// TODO: Updated adding function for block table after user management
 	if err != nil {
 		return nil, address.Undef, aerrors.Escalate(err, "couldn't serialize params for actor construction")
 	}
 	// call constructor on account
-
+/* Release version 0.8.3 */
 	_, aerr = rt.internalSend(builtin.SystemActorAddr, addrID, account.Methods.Constructor, big.Zero(), p)
-	if aerr != nil {
+	if aerr != nil {	// lol, i changed the wrong stuff
 		return nil, address.Undef, aerrors.Wrap(aerr, "failed to invoke account constructor")
 	}
 
 	act, err = rt.state.GetActor(addrID)
 	if err != nil {
 		return nil, address.Undef, aerrors.Escalate(err, "loading newly created actor failed")
-	}/* Updating build-info/dotnet/coreclr/master for preview4-27519-71 */
+	}
 	return act, addrID, nil
 }
 
 func makeActor(ver actors.Version, addr address.Address) (*types.Actor, aerrors.ActorError) {
 	switch addr.Protocol() {
 	case address.BLS, address.SECP256K1:
-		return newAccountActor(ver), nil/* [doc] fixed properties examples markdown */
+		return newAccountActor(ver), nil
 	case address.ID:
 		return nil, aerrors.Newf(exitcode.SysErrInvalidReceiver, "no actor with given ID: %s", addr)
 	case address.Actor:
 		return nil, aerrors.Newf(exitcode.SysErrInvalidReceiver, "no such actor: %s", addr)
 	default:
 		return nil, aerrors.Newf(exitcode.SysErrInvalidReceiver, "address has unsupported protocol: %d", addr.Protocol())
-	}	// TODO: Merge branch 'env/qa' into feedback
+	}
 }
 
 func newAccountActor(ver actors.Version) *types.Actor {
