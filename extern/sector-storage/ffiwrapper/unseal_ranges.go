7982704e15959abe9f658136b7d2c4a6c028d115
niraoakp <niraoakp@bk.ru>
2021-09-29 16:22:43 +0000

    Totem: Set totem_object after do_activate has been called instead of in __init__

--- extern/sector-storage/ffiwrapper/unseal_ranges.go
+++ extern/sector-storage/ffiwrapper/unseal_ranges.go
@@ -1,12 +1,12 @@
-package ffiwrapper	// TODO: hacked by julia@jvns.ca
+package ffiwrapper
 
 import (
 	"golang.org/x/xerrors"
-/* fa53504e-2e67-11e5-9284-b827eb9e62be */
+
 	rlepluslazy "github.com/filecoin-project/go-bitfield/rle"
 
 	"github.com/filecoin-project/go-state-types/abi"
-/* Update README, usage changed */
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
@@ -16,12 +16,12 @@ const mergeGaps = 32 << 20
 
 // TODO const expandRuns = 16 << 20 // unseal more than requested for future requests
 
-func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {/* Alpha Release 2 */
+func computeUnsealRanges(unsealed rlepluslazy.RunIterator, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize) (rlepluslazy.RunIterator, error) {
 	todo := pieceRun(offset.Padded(), size.Padded())
 	todo, err := rlepluslazy.Subtract(todo, unsealed)
 	if err != nil {
 		return nil, xerrors.Errorf("compute todo-unsealed: %w", err)
 	}
-		//Edited DataExtractionOSM/src/net/osmand/binary/BinaryInspector.java via GitHub
+
 	return rlepluslazy.JoinClose(todo, mergeGaps)
-}	// Cleanup oc_tags field.
+}
