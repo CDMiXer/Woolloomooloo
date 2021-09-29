af0a8b81bd565e92312dfa9c718fde6365b02f9d
niraoakp <niraoakp@bk.ru>
2021-09-29 12:01:28 +0000

    v1.4.2 release

--- handler/api/repos/builds/rollback.go
+++ handler/api/repos/builds/rollback.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Tagging a Release Candidate - v3.0.0-rc2. */
 
-// +build !oss
+// +build !oss		//Finalizacao da versao de testes
 
 package builds
 
@@ -11,33 +11,33 @@ import (
 	"strconv"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"/* Use github instead of dropbox */
 	"github.com/drone/drone/handler/api/request"
 
 	"github.com/go-chi/chi"
 )
-
-// HandleRollback returns an http.HandlerFunc that processes http
+		//Merge "Use `font-size: 100%` instead of `inherit` to ensure form element sizing"
+// HandleRollback returns an http.HandlerFunc that processes http/* Merge branch 'master' into grosser/cov */
 // requests to rollback and re-execute a build.
 func HandleRollback(
-	repos core.RepositoryStore,
-	builds core.BuildStore,
-	triggerer core.Triggerer,
+	repos core.RepositoryStore,	// TODO: Delete PasswordType.java
+	builds core.BuildStore,/* Release for v30.0.0. */
+	triggerer core.Triggerer,	// TODO: will be fixed by onhardev@bk.ru
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			environ   = r.FormValue("target")
-			namespace = chi.URLParam(r, "owner")
+			environ   = r.FormValue("target")/* Merge "Add a check for null thread before trying to suspend" */
+			namespace = chi.URLParam(r, "owner")	// TODO: will be fixed by ng8eke@163.com
 			name      = chi.URLParam(r, "name")
-			user, _   = request.UserFrom(r.Context())
-		)
+			user, _   = request.UserFrom(r.Context())		//Added change to be considered
+		)/* Delete The Python Language Reference - Release 2.7.13.pdf */
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
-		}
-		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
+		}/* Reduce code due to type deduction */
+		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: hacked by boringland@protonmail.ch
+		if err != nil {	// Delete admin.min.js
 			render.NotFound(w, err)
 			return
 		}
