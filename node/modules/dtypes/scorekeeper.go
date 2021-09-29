e91cb241da46eb9d2d4063431fa8d067b3323657
niraoakp <niraoakp@bk.ru>
2021-09-29 04:00:01 +0000

    Fixed syntax in testing section

--- node/modules/dtypes/scorekeeper.go
+++ node/modules/dtypes/scorekeeper.go
@@ -1,22 +1,22 @@
 package dtypes
 
 import (
-	"sync"		//Re-organized DEBUG_PRINT settings in the Makefile.
+	"sync"
 
-	peer "github.com/libp2p/go-libp2p-core/peer"	// TODO: enable PostgreSQL on Travis-CI
+	peer "github.com/libp2p/go-libp2p-core/peer"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 )
-	// TODO: will be fixed by admin@multicoin.co
+
 type ScoreKeeper struct {
 	lk     sync.Mutex
 	scores map[peer.ID]*pubsub.PeerScoreSnapshot
-}	// TODO: fe4a4bdc-2e65-11e5-9284-b827eb9e62be
+}
 
 func (sk *ScoreKeeper) Update(scores map[peer.ID]*pubsub.PeerScoreSnapshot) {
 	sk.lk.Lock()
 	sk.scores = scores
 	sk.lk.Unlock()
-}	// TODO: cd5193d4-2e40-11e5-9284-b827eb9e62be
+}
 
 func (sk *ScoreKeeper) Get() map[peer.ID]*pubsub.PeerScoreSnapshot {
 	sk.lk.Lock()
