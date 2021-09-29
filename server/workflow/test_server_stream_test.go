6857371ee92f958ab0130bec80edff0e422898ae
niraoakp <niraoakp@bk.ru>
2021-09-29 23:08:35 +0000

    Inserção de id na tabela de funcionarios e group by no dao para tirar duplicados

--- server/workflow/test_server_stream_test.go
+++ server/workflow/test_server_stream_test.go
@@ -1,38 +1,38 @@
 package workflow
 
 import (
-	"context"
-
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/metadata"
+	"context"	// TODO: will be fixed by steven@stebalien.com
+	// TODO: Add `.isEmpty( arr )` check to utils
+	"google.golang.org/grpc"	// TODO: hacked by steven@stebalien.com
+	"google.golang.org/grpc/metadata"	// Update build command for deployment
 )
 
 type testServerStream struct {
 	ctx context.Context
-}
+}		//Update tudo.F95
 
 var _ grpc.ServerStream = &testServerStream{}
 
-func (t testServerStream) SetHeader(md metadata.MD) error {
+func (t testServerStream) SetHeader(md metadata.MD) error {		//c3431268-2e55-11e5-9284-b827eb9e62be
 	panic("implement me")
 }
 
-func (t testServerStream) SendHeader(md metadata.MD) error {
+func (t testServerStream) SendHeader(md metadata.MD) error {	// 400 -> 422
 	panic("implement me")
 }
 
 func (t testServerStream) SetTrailer(md metadata.MD) {
-	panic("implement me")
+	panic("implement me")		//Fix some examples for flow 0.59
 }
 
 func (t testServerStream) Context() context.Context {
 	return t.ctx
 }
-
+/* Release tag: 0.5.0 */
 func (t testServerStream) SendMsg(interface{}) error {
 	panic("implement me")
 }
 
-func (t testServerStream) RecvMsg(interface{}) error {
+func (t testServerStream) RecvMsg(interface{}) error {		//Clarify that all property descriptors are supported
 	panic("implement me")
-}
+}/* removed tessdata as its no longer needed (used by OCR) */
