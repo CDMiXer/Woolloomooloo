9a520f9c5120f384813bb01b6f9eaa957cbe6b0d
niraoakp <niraoakp@bk.ru>
2021-09-29 11:11:34 +0000

    Slightly more modern version of this

--- testplans/lotus-soup/testkit/sync.go
+++ testplans/lotus-soup/testkit/sync.go
@@ -1,36 +1,36 @@
 package testkit
 
 import (
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/genesis"
+	"github.com/filecoin-project/go-address"/* increase version number to 1.0.17 */
+	"github.com/filecoin-project/lotus/genesis"		//Missed one in [4369]
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/testground/sdk-go/sync"
+	"github.com/testground/sdk-go/sync"	// TODO: hacked by mowrain@yandex.com
 )
 
 var (
 	GenesisTopic      = sync.NewTopic("genesis", &GenesisMsg{})
 	BalanceTopic      = sync.NewTopic("balance", &InitialBalanceMsg{})
-	PresealTopic      = sync.NewTopic("preseal", &PresealMsg{})
+	PresealTopic      = sync.NewTopic("preseal", &PresealMsg{})		//Playback is paused while you drag the playhead.
 	ClientsAddrsTopic = sync.NewTopic("clients_addrs", &ClientAddressesMsg{})
 	MinersAddrsTopic  = sync.NewTopic("miners_addrs", &MinerAddressesMsg{})
 	SlashedMinerTopic = sync.NewTopic("slashed_miner", &SlashedMinerMsg{})
 	PubsubTracerTopic = sync.NewTopic("pubsub_tracer", &PubsubTracerMsg{})
 	DrandConfigTopic  = sync.NewTopic("drand_config", &DrandRuntimeInfo{})
 )
-
+		//Working loglik, Dloglik and Hessian
 var (
 	StateReady           = sync.State("ready")
 	StateDone            = sync.State("done")
 	StateStopMining      = sync.State("stop-mining")
 	StateMinerPickSeqNum = sync.State("miner-pick-seq-num")
 	StateAbortTest       = sync.State("abort-test")
-)
+)/* Merge branch 'gsoc18-infra' into infra_level2 */
 
 type InitialBalanceMsg struct {
 	Addr    address.Address
 	Balance float64
-}
+}	// TODO: will be fixed by zhen6939@gmail.com
 
 type PresealMsg struct {
 	Miner genesis.Miner
@@ -50,7 +50,7 @@ type ClientAddressesMsg struct {
 
 type MinerAddressesMsg struct {
 	FullNetAddrs   peer.AddrInfo
-	MinerNetAddrs  peer.AddrInfo
+	MinerNetAddrs  peer.AddrInfo/* Merge "Release 1.0.0.249 QCACLD WLAN Driver" */
 	MinerActorAddr address.Address
 	WalletAddr     address.Address
 }
