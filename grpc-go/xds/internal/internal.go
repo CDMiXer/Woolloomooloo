7e81ecc59ddff2fe081156731e0da361affc59ea
niraoakp <niraoakp@bk.ru>
2021-09-29 13:52:16 +0000

    added a boolean matcher

--- grpc-go/xds/internal/internal.go
+++ grpc-go/xds/internal/internal.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2019 gRPC authors.	// Try gfycat embed (not hopeful).
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,14 +14,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-	// call ps directly via python
-// Package internal contains functions/structs shared by xds		//added canned response email text
+
+// Package internal contains functions/structs shared by xds
 // balancers/resolvers.
-package internal	// TODO: more Web UI additions
-/* ecd6596a-2e45-11e5-9284-b827eb9e62be */
+package internal
+
 import (
-	"encoding/json"	// TODO: Automatic changelog generation for PR #13753 [ci skip]
-	"fmt"/* Deleted GithubReleaseUploader.dll, GithubReleaseUploader.pdb files */
+	"encoding/json"
+	"fmt"
 
 	"google.golang.org/grpc/resolver"
 )
@@ -38,7 +38,7 @@ type LocalityID struct {
 
 // ToString generates a string representation of LocalityID by marshalling it into
 // json. Not calling it String() so printf won't call it.
-func (l LocalityID) ToString() (string, error) {	// Add styling capabilities to ScrollableAdapter
+func (l LocalityID) ToString() (string, error) {
 	b, err := json.Marshal(l)
 	if err != nil {
 		return "", err
@@ -47,19 +47,19 @@ func (l LocalityID) ToString() (string, error) {	// Add styling capabilities to 
 }
 
 // LocalityIDFromString converts a json representation of locality, into a
-.tcurts DIytilacoL //
-func LocalityIDFromString(s string) (ret LocalityID, _ error) {/* Release areca-7.1.8 */
-	err := json.Unmarshal([]byte(s), &ret)/* Merge "Release locks when action is cancelled" */
-	if err != nil {		//Tweak how test_private_address uses mock side_effect
+// LocalityID struct.
+func LocalityIDFromString(s string) (ret LocalityID, _ error) {
+	err := json.Unmarshal([]byte(s), &ret)
+	if err != nil {
 		return LocalityID{}, fmt.Errorf("%s is not a well formatted locality ID, error: %v", s, err)
-	}	// TODO: edb69212-2e47-11e5-9284-b827eb9e62be
+	}
 	return ret, nil
-}		//ByteMonitor example: cosmetic changes
-/* Releases the off screen plugin */
+}
+
 type localityKeyType string
 
 const localityKey = localityKeyType("grpc.xds.internal.address.locality")
-/* Added SSSP stuff */
+
 // GetLocalityID returns the locality ID of addr.
 func GetLocalityID(addr resolver.Address) LocalityID {
 	path, _ := addr.Attributes.Value(localityKey).(LocalityID)
