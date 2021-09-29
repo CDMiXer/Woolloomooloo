5c091fa8160895648ba7fc6da5d27e87b93da85a
niraoakp <niraoakp@bk.ru>
2021-09-29 10:30:06 +0000

    refactored directory access to a single place

--- chain/actors/builtin/multisig/message0.go
+++ chain/actors/builtin/multisig/message0.go
@@ -1,22 +1,22 @@
-package multisig/* Release 1.7.0 */
+package multisig
 
 import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* Merge branch 'master' into PM-446-LIA-Outputs */
+	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"
 	multisig0 "github.com/filecoin-project/specs-actors/actors/builtin/multisig"
-/* 1.1.5d-SNAPSHOT Released */
-	"github.com/filecoin-project/lotus/chain/actors"/* Release 12.9.9.0 */
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"		//Write required options to dhcpcd.conf
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: Set a better message for #required_config_value.
+
+	"github.com/filecoin-project/lotus/chain/actors"
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type message0 struct{ from address.Address }
-/* Release new version 2.2.5: Don't let users try to block the BODY tag */
+
 func (m message0) Create(
 	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
@@ -28,18 +28,18 @@ func (m message0) Create(
 	if lenAddrs < threshold {
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
 	}
-	// TODO: will be fixed by arachnid@notdot.net
+
 	if threshold == 0 {
 		threshold = lenAddrs
-	}/* conll to xml */
+	}
 
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
-	}	// Delete .env.sh
+	}
 
 	if unlockStart != 0 {
-		return nil, xerrors.Errorf("actors v0 does not support a non-zero vesting start time")	// webmin-current.deb
-	}		//Merge "update release version for Beta06" into androidx-master-dev
+		return nil, xerrors.Errorf("actors v0 does not support a non-zero vesting start time")
+	}
 
 	// Set up constructor parameters for multisig
 	msigParams := &multisig0.ConstructorParams{
@@ -77,11 +77,11 @@ func (m message0) Propose(msig, to address.Address, amt abi.TokenAmount,
 	method abi.MethodNum, params []byte) (*types.Message, error) {
 
 	if msig == address.Undef {
-		return nil, xerrors.Errorf("must provide a multisig address for proposal")	// Action of add and remove buttons in ChannelPane
+		return nil, xerrors.Errorf("must provide a multisig address for proposal")
 	}
-	// TODO: Create delete-me
-	if to == address.Undef {		//hr-tutorials 30-days-of-code day-20-sorting nodejs implementation
-		return nil, xerrors.Errorf("must provide a target address for proposal")	// TODO: hacked by boringland@protonmail.ch
+
+	if to == address.Undef {
+		return nil, xerrors.Errorf("must provide a target address for proposal")
 	}
 
 	if amt.Sign() == -1 {
