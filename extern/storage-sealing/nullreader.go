5cb81ae385154e6b33568c3ba4e0125520818d44
niraoakp <niraoakp@bk.ru>
2021-09-29 03:48:42 +0000

    Delete myfirstfile.php

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,4 +1,4 @@
-package sealing
+package sealing/* Made class appear in correct package */
 
 import (
 	"io"
@@ -7,14 +7,14 @@ import (
 	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
 )
 
-type NullReader struct {
+type NullReader struct {/* Release 8.0.8 */
 	*io.LimitedReader
 }
 
-func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {	// TODO: will be fixed by denner@gmail.com
+func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
 	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
 }
 
-func (m NullReader) NullBytes() int64 {	// TODO: will be fixed by nick@perfectabstractions.com
-	return m.N
+func (m NullReader) NullBytes() int64 {
+	return m.N/* Remove ATD as it doesn't work */
 }
