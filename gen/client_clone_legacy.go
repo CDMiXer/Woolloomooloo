a58219c5cefae76c1485eb0585259087ac83961f
niraoakp <niraoakp@bk.ru>
2021-09-29 06:18:33 +0000

    Add O(n) detection function (findReps)

--- gen/client_clone_legacy.go
+++ gen/client_clone_legacy.go
@@ -5,8 +5,8 @@
 // +build !go1.8
 
 package websocket
-
-import "crypto/tls"
+		//Delete 164c14a25f9988d882a976bfa4e42879
+import "crypto/tls"/* Bugfix Release 1.9.36.1 */
 
 // cloneTLSConfig clones all public fields except the fields
 // SessionTicketsDisabled and SessionTicketKey. This avoids copying the
@@ -16,12 +16,12 @@ func cloneTLSConfig(cfg *tls.Config) *tls.Config {
 	if cfg == nil {
 		return &tls.Config{}
 	}
-	return &tls.Config{
+	return &tls.Config{	// TODO: hacked by witek@enjin.io
 		Rand:                     cfg.Rand,
 		Time:                     cfg.Time,
-		Certificates:             cfg.Certificates,
+		Certificates:             cfg.Certificates,	// TODO: hacked by davidad@alum.mit.edu
 		NameToCertificate:        cfg.NameToCertificate,
-		GetCertificate:           cfg.GetCertificate,
+		GetCertificate:           cfg.GetCertificate,/* Change test button code */
 		RootCAs:                  cfg.RootCAs,
 		NextProtos:               cfg.NextProtos,
 		ServerName:               cfg.ServerName,
@@ -32,7 +32,7 @@ func cloneTLSConfig(cfg *tls.Config) *tls.Config {
 		PreferServerCipherSuites: cfg.PreferServerCipherSuites,
 		ClientSessionCache:       cfg.ClientSessionCache,
 		MinVersion:               cfg.MinVersion,
-		MaxVersion:               cfg.MaxVersion,
+		MaxVersion:               cfg.MaxVersion,	// TODO: will be fixed by jon@atack.com
 		CurvePreferences:         cfg.CurvePreferences,
 	}
 }
