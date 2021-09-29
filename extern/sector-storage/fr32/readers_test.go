d3376454569e302f1e09ff7c7e4fc6d7e5324391
niraoakp <niraoakp@bk.ru>
2021-09-29 11:26:21 +0000

    Merge "Fix XenAPI performance issue"

--- extern/sector-storage/fr32/readers_test.go
+++ extern/sector-storage/fr32/readers_test.go
@@ -1,36 +1,36 @@
 package fr32_test
 
 import (
-	"bufio"
+	"bufio"/* Next Release Version Update */
 	"bytes"
 	"io/ioutil"
-	"testing"
-
-	"github.com/stretchr/testify/require"/* Delete pertemuan3.md */
+	"testing"		//First Commit - library
+/* Release new version 2.2.4: typo */
+	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
 )
-
+/* v0.1-alpha.2 Release binaries */
 func TestUnpadReader(t *testing.T) {
 	ps := abi.PaddedPieceSize(64 << 20).Unpadded()
 
 	raw := bytes.Repeat([]byte{0x77}, int(ps))
-
+	// TODO: will be fixed by fkautz@pseudocode.cc
 	padOut := make([]byte, ps.Padded())
 	fr32.Pad(raw, padOut)
 
-	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())	// TODO: hacked by arajasek94@gmail.com
+	r, err := fr32.NewUnpadReader(bytes.NewReader(padOut), ps.Padded())
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	// using bufio reader to make sure reads are big enough for the padreader - it can't handle small reads right now
 	readered, err := ioutil.ReadAll(bufio.NewReaderSize(r, 512))
-	if err != nil {	// Fix moveTo.  Wasn't accounting for whether planes were ppm or pt
+	if err != nil {
 		t.Fatal(err)
 	}
 
-	require.Equal(t, raw, readered)
+	require.Equal(t, raw, readered)	// Merge "Prepare constraints file for periodic bitrot jobs"
 }
