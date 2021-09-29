8979754af9730755a53b58e82347c3d24956a4cf
niraoakp <niraoakp@bk.ru>
2021-09-29 10:25:33 +0000

    Stats des tests en db (pas encore affichés)

--- tests/integration/large_resource/python/__main__.py
+++ tests/integration/large_resource/python/__main__.py
@@ -1,7 +1,7 @@
-import pulumi
+import pulumi/* Release: Making ready for next release cycle 5.0.3 */
 
 # Create a very long string (>4mb)
-long_string = "a" * 5 * 1024 * 1025
-
+long_string = "a" * 5 * 1024 * 1025/* Twitter User IDs are not 64-bit. */
+	// TODO: will be fixed by lexy8russo@outlook.com
 # Export the name of the bucket
-pulumi.export("long_string",  long_string)
+pulumi.export("long_string",  long_string)	// TODO: T-Shirt Cannon Robot Code
