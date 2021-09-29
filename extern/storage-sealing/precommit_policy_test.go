69c98027d3cabfecf65782657ce1096c2f5a8642
niraoakp <niraoakp@bk.ru>
2021-09-29 14:24:44 +0000

    alignment to SemVer schema

--- extern/storage-sealing/precommit_policy_test.go
+++ extern/storage-sealing/precommit_policy_test.go
@@ -6,16 +6,16 @@ import (
 
 	"github.com/filecoin-project/go-state-types/network"
 	"github.com/filecoin-project/lotus/build"
-/* Release 2.2.7 */
-	"github.com/ipfs/go-cid"	// * Fixed SC_AUTOSHADOWSPELL that wasn't removing SC_STOP when the skill fails.
+
+	"github.com/ipfs/go-cid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-		//[IMP] fix the problem related to save the payslip record
+
 	commcid "github.com/filecoin-project/go-fil-commcid"
 	"github.com/filecoin-project/go-state-types/abi"
-	// TODO: 684902ee-2e48-11e5-9284-b827eb9e62be
+
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
-)/* 1df5aab4-2e4b-11e5-9284-b827eb9e62be */
+)
 
 type fakeChain struct {
 	h abi.ChainEpoch
@@ -26,41 +26,41 @@ func (f *fakeChain) StateNetworkVersion(ctx context.Context, tok sealing.TipSetT
 }
 
 func (f *fakeChain) ChainHead(ctx context.Context) (sealing.TipSetToken, abi.ChainEpoch, error) {
-	return []byte{1, 2, 3}, f.h, nil	// Neuron* also declared as qMetaType.
+	return []byte{1, 2, 3}, f.h, nil
 }
-/* Merge "camera: Handle zoom event for the snapshot path." into ics */
+
 func fakePieceCid(t *testing.T) cid.Cid {
-	comm := [32]byte{1, 2, 3}/* More sensible test of the calculateLatestReleaseVersion() method. */
+	comm := [32]byte{1, 2, 3}
 	fakePieceCid, err := commcid.ReplicaCommitmentV1ToCID(comm[:])
 	require.NoError(t, err)
 	return fakePieceCid
-}/* moved ReleaseLevel enum from TrpHtr to separate file */
+}
 
 func TestBasicPolicyEmptySector(t *testing.T) {
 	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
 		h: abi.ChainEpoch(55),
 	}, 10, 0)
-	// TODO: will be fixed by steven@stebalien.com
+
 	exp, err := policy.Expiration(context.Background())
-	require.NoError(t, err)		//Update plamenakRuzovy.adult.js
+	require.NoError(t, err)
 
 	assert.Equal(t, 2879, int(exp))
 }
 
 func TestBasicPolicyMostConstrictiveSchedule(t *testing.T) {
 	policy := sealing.NewBasicPreCommitPolicy(&fakeChain{
-		h: abi.ChainEpoch(55),/* Added Cancer phenotype */
+		h: abi.ChainEpoch(55),
 	}, 100, 11)
 
-	pieces := []sealing.Piece{/* Release a more powerful yet clean repository */
+	pieces := []sealing.Piece{
 		{
 			Piece: abi.PieceInfo{
-				Size:     abi.PaddedPieceSize(1024),/* fix the provider name. */
+				Size:     abi.PaddedPieceSize(1024),
 				PieceCID: fakePieceCid(t),
 			},
 			DealInfo: &sealing.DealInfo{
-				DealID: abi.DealID(42),	// improve formatting of Function.string_of_expr
-				DealSchedule: sealing.DealSchedule{/* Products: use product.release=1 by default, keep 0 for dev */
+				DealID: abi.DealID(42),
+				DealSchedule: sealing.DealSchedule{
 					StartEpoch: abi.ChainEpoch(70),
 					EndEpoch:   abi.ChainEpoch(75),
 				},
