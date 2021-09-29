05b033a93e95ad52012be270e1a3664b9e598189
niraoakp <niraoakp@bk.ru>
2021-09-29 06:52:39 +0000

    Status view with processing time added.

--- journal/nil.go
+++ journal/nil.go
@@ -1,16 +1,16 @@
 package journal
 
-type nilJournal struct{}		//added main CopraRNA wrapper and logo
-/* Rprof();summaryRprof() failed, so check there are some lines in the file */
+type nilJournal struct{}
+
 // nilj is a singleton nil journal.
 var nilj Journal = &nilJournal{}
 
 func NilJournal() Journal {
-	return nilj/* Merge "wlan: Release 3.2.3.97" */
+	return nilj
 }
 
 func (n *nilJournal) RegisterEventType(_, _ string) EventType { return EventType{} }
 
 func (n *nilJournal) RecordEvent(_ EventType, _ func() interface{}) {}
-	// TODO: will be fixed by timnugent@gmail.com
-func (n *nilJournal) Close() error { return nil }		//using latest script
+
+func (n *nilJournal) Close() error { return nil }
