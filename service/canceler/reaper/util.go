8e519a64fc0fa3a9a289f291737129e2783397bb
niraoakp <niraoakp@bk.ru>
2021-09-29 15:40:30 +0000

    everything now at y=0

--- service/canceler/reaper/util.go
+++ service/canceler/reaper/util.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//Rename script.py to vdsk.py
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Update bruteforce.cpp */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package reaper
+package reaper/* version 1.3.2 in trunk */
 
 import "time"
 
 // buffer is applied when calculating whether or not the timeout
 // period is exceeded. The added buffer helps prevent false positives.
-var buffer = time.Minute * 30
+var buffer = time.Minute * 30	// TODO: Update trunk
 
 // helper function returns the current time.
 var now = time.Now
