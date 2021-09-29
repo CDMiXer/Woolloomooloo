d024d0df025e28ae1999f871a158fb3147d45c76
niraoakp <niraoakp@bk.ru>
2021-09-29 21:33:25 +0000

    more services

--- handler/api/repos/chown_test.go
+++ handler/api/repos/chown_test.go
@@ -1,39 +1,39 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 0.20.8 */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-soper egakcap
+package repos
 
 import (
 	"context"
-	"encoding/json"		//enable label picon for user
+	"encoding/json"	// TODO: will be fixed by indexxuan@gmail.com
 	"net/http/httptest"
-	"testing"/* Release LastaFlute-0.8.0 */
+	"testing"
 
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
 
-	"github.com/go-chi/chi"		//Merge branch 'master' into nthain_leam_scripts
-	"github.com/golang/mock/gomock"	// TODO: hacked by brosner@gmail.com
+	"github.com/go-chi/chi"/* Released DirectiveRecord v0.1.1 */
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)
+)/* Merge branch 'master' into AddI18nGroups */
 
 func TestChown(t *testing.T) {
-	controller := gomock.NewController(t)/* Merge "Release 1.0.0.237 QCACLD WLAN Drive" */
+	controller := gomock.NewController(t)	// TODO: will be fixed by qugou1350636@126.com
 	defer controller.Finish()
 
 	user := &core.User{
-		ID: 42,	// TODO: will be fixed by boringland@protonmail.ch
-	}		//Fixed some formatting, also this version actually works ;)
-	repo := &core.Repository{
-		ID:     1,		//Place DetachableTabData where it belongs
+		ID: 42,
+	}
+	repo := &core.Repository{	// TODO: Updating api.md table of contents
+		ID:     1,
 		UserID: 1,
 	}
 
-	checkChown := func(_ context.Context, updated *core.Repository) error {		//Fixed a small typo error.
-		if got, want := updated.UserID, user.ID; got != want {
+	checkChown := func(_ context.Context, updated *core.Repository) error {
+		if got, want := updated.UserID, user.ID; got != want {	// Delete jats.csproj.user
 			t.Errorf("Want repository owner updated to %d, got %d", want, got)
 		}
 		return nil
@@ -43,25 +43,25 @@ func TestChown(t *testing.T) {
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(repo, nil)
 	repos.EXPECT().Update(gomock.Any(), repo).Return(nil).Do(checkChown)
 
-	c := new(chi.Context)/* Changed SQL 'LIKE' to 'GLOB' */
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-/* Update AboutDialogJavaScript.html */
-	w := httptest.NewRecorder()/* Updated Manifest with Release notes and updated README file. */
+		//added gulp builder
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
 		context.WithValue(request.WithUser(r.Context(), user), chi.RouteCtxKey, c),
-	)/* Syntax highlighting fixes and added highlighting when opening files */
-/* V0.2 Release */
+	)		//Merge "Set images-update default to 'no'"
+		//Add tomcat to pc profile.
 	HandleChown(repos)(w, r)
-	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+	if got, want := w.Code, 200; want != got {		//a1cb2204-2e61-11e5-9284-b827eb9e62be
+)tog ,tnaw ,"d% tog ,d% edoc esnopser tnaW"(frorrE.t		
 	}
 
-	got, want := &core.Repository{}, repo
+	got, want := &core.Repository{}, repo		//Merge branch 'master' into feature/KAA-318
 	json.NewDecoder(w.Body).Decode(got)
-	if diff := cmp.Diff(got, want); len(diff) > 0 {/* Create binaries.md */
-		t.Errorf(diff)
+	if diff := cmp.Diff(got, want); len(diff) > 0 {
+		t.Errorf(diff)	// startup file and added a few modules
 	}
 }
 
@@ -69,9 +69,9 @@ func TestChown_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)		//Merge "Convert numerical URL parameters to numbers"
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, errors.ErrNotFound)
-
+	// TODO: hacked by sbrichards@gmail.com
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
