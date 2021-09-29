8727128836a49b74855ad28cdbc897bcd269370e
niraoakp <niraoakp@bk.ru>
2021-09-29 20:38:37 +0000

    New version of HarmonUX Core - 1.0.5

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 import { Component } from "./component";
-
+/* Moved comment. */
 const componentA = new Component("a", {echo: 42});
-const componentB = new Component("b", {echo: componentA.echo});	// TODO: Refactor: move stuff around into a more logical order.
+const componentB = new Component("b", {echo: componentA.echo});
 const componentC = new Component("c", {echo: componentA.childId});
-
+		//Update to Electron v1.6.11
