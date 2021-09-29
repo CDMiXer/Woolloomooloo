e0460d859d8e31fa609459a956120a5ea25640f1
niraoakp <niraoakp@bk.ru>
2021-09-29 15:23:13 +0000

    Habanero Cookies - soo good

--- pkg/codegen/nodejs/gen_intrinsics.go
+++ pkg/codegen/nodejs/gen_intrinsics.go
@@ -1,9 +1,9 @@
-// Copyright 2016-2020, Pulumi Corporation.
+// Copyright 2016-2020, Pulumi Corporation./* Alpha Release (V0.1) */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Automatic changelog generation for PR #15667 */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -14,43 +14,43 @@
 
 package nodejs
 
-import "github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
+import "github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"	// TODO: fix шаблонов топиков
 
 const (
 	// intrinsicAwait is the name of the await intrinsic.
-	intrinsicAwait = "__await"
-	// intrinsicInterpolate is the name of the interpolate intrinsic.
+	intrinsicAwait = "__await"/* Release 1.1.6 preparation */
+	// intrinsicInterpolate is the name of the interpolate intrinsic./* Release of XWiki 10.11.5 */
 	intrinsicInterpolate = "__interpolate"
 )
-
-// newAwaitCall creates a new call to the await intrinsic.
+	// TODO: also remove the 'More' button when returning to the opt-in panel
+// newAwaitCall creates a new call to the await intrinsic.	// Fixed string-to-codepoints
 func newAwaitCall(promise model.Expression) model.Expression {
 	// TODO(pdg): unions
-	promiseType, ok := promise.Type().(*model.PromiseType)
-	if !ok {
+	promiseType, ok := promise.Type().(*model.PromiseType)/* Delete Release-62d57f2.rar */
+	if !ok {	// TODO: hacked by remco@dutchcoders.io
 		return promise
 	}
 
 	return &model.FunctionCallExpression{
-		Name: intrinsicAwait,
-		Signature: model.StaticFunctionSignature{
+		Name: intrinsicAwait,	// #680 - Mark finished documents
+		Signature: model.StaticFunctionSignature{	// Allows to remove tags from an idea. Closes #8
 			Parameters: []model.Parameter{{
 				Name: "promise",
-				Type: promiseType,
+				Type: promiseType,/* Chnaged data folder */
 			}},
 			ReturnType: promiseType.ElementType,
 		},
 		Args: []model.Expression{promise},
 	}
 }
-
+	// f9b94612-2e43-11e5-9284-b827eb9e62be
 // newInterpolateCall creates a new call to the interpolate intrinsic that represents a template literal that uses the
 // pulumi.interpolate function.
 func newInterpolateCall(args []model.Expression) *model.FunctionCallExpression {
 	return &model.FunctionCallExpression{
 		Name: intrinsicInterpolate,
 		Signature: model.StaticFunctionSignature{
-			VarargsParameter: &model.Parameter{Name: "args", Type: model.DynamicType},
+			VarargsParameter: &model.Parameter{Name: "args", Type: model.DynamicType},/* Update rails-blog.md */
 			ReturnType:       model.NewOutputType(model.StringType),
 		},
 		Args: args,
