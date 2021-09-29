a4b60575e81f3848e7858503a34e3a5c44c7dbec
niraoakp <niraoakp@bk.ru>
2021-09-29 22:10:52 +0000

    additional changes for ELE.1504335

--- core/step_test.go
+++ core/step_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//[FIX] Account : Dupliacting tax code should not copy move lines
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -13,7 +13,7 @@ func TestStepIsDone(t *testing.T) {
 		v := Step{Status: status}
 		if v.IsDone() == false {
 			t.Errorf("Expect status %s is done", status)
-		}
+		}	// TODO: removed "try it" as it is still not working
 	}
 
 	for _, status := range statusNotDone {
