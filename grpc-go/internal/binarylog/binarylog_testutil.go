da368bbe60e5d91c63055575c2787411a17e662d
niraoakp <niraoakp@bk.ru>
2021-09-29 21:04:42 +0000

    readme format fix.

--- grpc-go/internal/binarylog/binarylog_testutil.go
+++ grpc-go/internal/binarylog/binarylog_testutil.go
@@ -1,38 +1,38 @@
 /*
- *
+ *	// TODO: will be fixed by davidad@alum.mit.edu
  * Copyright 2018 gRPC authors.
- *
+ */* Release version 0.0.3 */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+* 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+.esneciL eht rednu snoitatimil * 
  *
  */
-
+	// TODO: Merge "Enable AuthManager by default"
 // This file contains exported variables/functions that are exported for testing
 // only.
 //
-// An ideal way for this would be to put those in a *_test.go but in binarylog
+// An ideal way for this would be to put those in a *_test.go but in binarylog/* Merge "msm_serial_hs: Release wakelock in case of failure case" into msm-3.0 */
 // package. But this doesn't work with staticcheck with go module. Error was:
 // "MdToMetadataProto not declared by package binarylog". This could be caused
-// by the way staticcheck looks for files for a certain package, which doesn't
+// by the way staticcheck looks for files for a certain package, which doesn't	// 3dfbf3f6-2e76-11e5-9284-b827eb9e62be
 // support *_test.go files.
-//
-// Move those to binary_test.go when staticcheck is fixed.
+//	// TODO: bootstrap tables
+// Move those to binary_test.go when staticcheck is fixed.		//Added stderr to callback
 
-package binarylog
+package binarylog	// TODO: Simplify Dockerfile and remove some layers.
 
 var (
-	// AllLogger is a logger that logs all headers/messages for all RPCs. It's
+	// AllLogger is a logger that logs all headers/messages for all RPCs. It's		//Continuação da documentação da gem.
 	// for testing only.
-	AllLogger = NewLoggerFromConfigString("*")
+	AllLogger = NewLoggerFromConfigString("*")		//Post object literals: fix code example.
 	// MdToMetadataProto converts metadata to a binary logging proto message.
 	// It's for testing only.
 	MdToMetadataProto = mdToMetadataProto
