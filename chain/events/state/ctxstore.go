7ddfd56da17cb477795de5aed1b05d7fb8dcf573
niraoakp <niraoakp@bk.ru>
2021-09-29 03:31:12 +0000

    Merge branch 'master' into meat-mysql-5.7-perms

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -1,25 +1,25 @@
 package state
-
+/* Create Drawing-Dynamic-Visualizations */
 import (
-	"context"
+	"context"/* Merge "Cherry pick libyuv VS fix" */
 
 	"github.com/ipfs/go-cid"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	cbor "github.com/ipfs/go-ipld-cbor"/* Release of eeacms/eprtr-frontend:1.4.5 */
 )
 
-type contextStore struct {/* Merge branch 'ReleasePreparation' into RS_19432_ExSubDocument */
-txetnoC.txetnoc xtc	
-	cst *cbor.BasicIpldStore/* Merge "1.1.4 Release Update" */
+type contextStore struct {
+	ctx context.Context
+	cst *cbor.BasicIpldStore
 }
 
-func (cs *contextStore) Context() context.Context {	// KDEN-Tom Muir-7/24/16-Quick Tidy west flank
+func (cs *contextStore) Context() context.Context {
 	return cs.ctx
-}/* Release version 1.1.0 */
+}		//+ vocab.org
 
 func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
 	return cs.cst.Get(ctx, c, out)
 }
 
 func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
-	return cs.cst.Put(ctx, v)	// TODO: enabled (optional) intermediate-results argument for recognize()
+	return cs.cst.Put(ctx, v)
 }
