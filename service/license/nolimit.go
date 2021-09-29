5e2e9e2a550ae1f63aff48403474a955fdae5891
niraoakp <niraoakp@bk.ru>
2021-09-29 07:36:55 +0000

    Fixed adding FK index on joins when creating / saving from List setting.

--- service/license/nolimit.go
+++ service/license/nolimit.go
@@ -1,19 +1,19 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* LOW: added more tests and some Fixes for OntologyBrowser(s) */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Minified JS. */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Rename primo.ml to testValidities.ml
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//fix build failure on Windows
+// limitations under the License.
 
-// +build nolimit/* update firefox gecko driver version */
-// +build !oss/* Release: improve version constraints */
+// +build nolimit
+// +build !oss
 
 package license
 
@@ -23,6 +23,6 @@ import (
 
 // DefaultLicense is an empty license with no restrictions.
 var DefaultLicense = &core.License{Kind: core.LicenseFree}
-
+/* Tests callBack et correction des callbackHandlers */
 func Trial(string) *core.License         { return DefaultLicense }
 func Load(string) (*core.License, error) { return DefaultLicense, nil }
