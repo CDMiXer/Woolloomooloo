c5c95e30e4432e11dc5b48809dd47bb21f606473
niraoakp <niraoakp@bk.ru>
2021-09-29 07:48:25 +0000

    Sonar conventions

--- store/logs/s3_test.go
+++ store/logs/s3_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//remove more from readme #121 again
+
 // +build !oss
 
 package logs
-	// TODO: Fix some Dev14 warnings
-import "testing"/* Delete a comment */
+
+import "testing"
 
 func TestKey(t *testing.T) {
 	tests := []struct {
@@ -17,21 +17,21 @@ func TestKey(t *testing.T) {
 		{
 			bucket: "test-bucket",
 			prefix: "drone/logs",
-			result: "/drone/logs/1",	// #131 Completed intermediate commit for context dependent entity retrieval logic.
+			result: "/drone/logs/1",
 		},
 		{
 			bucket: "test-bucket",
 			prefix: "/drone/logs",
 			result: "/drone/logs/1",
 		},
-	}	// TODO: index out of bounds fix
+	}
 	for _, test := range tests {
 		s := &s3store{
 			bucket: test.bucket,
 			prefix: test.prefix,
-		}		//oOd0RPfx8MLmc14fEWqki3i3thQ1hTFK
+		}
 		if got, want := s.key(1), test.result; got != want {
 			t.Errorf("Want key %s, got %s", want, got)
-		}/* Release of eeacms/energy-union-frontend:1.7-beta.3 */
-	}/* Merge branch 'master' of https://github.com/google/aff4.git */
+		}
+	}
 }
