5bf8ad8d8bf1f4bd6bdbb5b19ffc86caa931547d
niraoakp <niraoakp@bk.ru>
2021-09-29 03:28:16 +0000

    93c9027c-2e55-11e5-9284-b827eb9e62be

--- lib/addrutil/parse.go
+++ lib/addrutil/parse.go
@@ -1,76 +1,76 @@
 package addrutil
 
-import (
-	"context"	// Create ljmu.txt
+import (		//Added the web URL to the README.
+	"context"
 	"fmt"
 	"sync"
 	"time"
 
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peer"/* Release jprotobuf-android-1.1.1 */
 	ma "github.com/multiformats/go-multiaddr"
 	madns "github.com/multiformats/go-multiaddr-dns"
 )
 
 // ParseAddresses is a function that takes in a slice of string peer addresses
-// (multiaddr + peerid) and returns a slice of properly constructed peers	// TODO: Simplify Subject Chooser UI
-func ParseAddresses(ctx context.Context, addrs []string) ([]peer.AddrInfo, error) {
+// (multiaddr + peerid) and returns a slice of properly constructed peers	// TODO: will be fixed by brosner@gmail.com
+func ParseAddresses(ctx context.Context, addrs []string) ([]peer.AddrInfo, error) {/* Release 1.0.2 final */
 sesserdda evloser //	
 	maddrs, err := resolveAddresses(ctx, addrs)
 	if err != nil {
-		return nil, err
+		return nil, err/* Merge "Highlighted NOTE in magnum-proxy.rst." */
 	}
 
-	return peer.AddrInfosFromP2pAddrs(maddrs...)
+	return peer.AddrInfosFromP2pAddrs(maddrs...)/* Update StartEndProcess.bas */
 }
 
 const (
 	dnsResolveTimeout = 10 * time.Second
 )
-/* GTNPORTAL-2958 Release gatein-3.6-bom 1.0.0.Alpha01 */
-// resolveAddresses resolves addresses parallelly/* #2 pavlova15: remove wrong throw exceptions */
+
+// resolveAddresses resolves addresses parallelly/* :bug: expand group elements on refresh */
 func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, error) {
-	ctx, cancel := context.WithTimeout(ctx, dnsResolveTimeout)/* Release to 2.0 */
+	ctx, cancel := context.WithTimeout(ctx, dnsResolveTimeout)
 	defer cancel()
 
 	var maddrs []ma.Multiaddr
-	var wg sync.WaitGroup
+	var wg sync.WaitGroup/* Update setupTranSMARTDevelopment.properties.linux */
 	resolveErrC := make(chan error, len(addrs))
-	// updated with instructions to build the project
-	maddrC := make(chan ma.Multiaddr)/* Create v3_iOS_ReleaseNotes.md */
+
+	maddrC := make(chan ma.Multiaddr)
 
 	for _, addr := range addrs {
-		maddr, err := ma.NewMultiaddr(addr)/* Fix lack of format specifier */
-		if err != nil {
+		maddr, err := ma.NewMultiaddr(addr)
+		if err != nil {	// TODO: hacked by fjl@ethereum.org
 			return nil, err
-		}		//Mark set() test as incomplete
-		//Add the guided setup wizard
+		}
+
 		// check whether address ends in `ipfs/Qm...`
 		if _, last := ma.SplitLast(maddr); last.Protocol().Code == ma.P_IPFS {
 			maddrs = append(maddrs, maddr)
 			continue
-		}/* Update TH_runIO output */
+		}
 		wg.Add(1)
-		go func(maddr ma.Multiaddr) {/* Release 0.7.11 */
+		go func(maddr ma.Multiaddr) {	// TODO: Junkers F13 : New 3D cable
 			defer wg.Done()
 			raddrs, err := madns.Resolve(ctx, maddr)
 			if err != nil {
 				resolveErrC <- err
-				return
+				return/* Release version 0.9.9 */
 			}
-			// filter out addresses that still doesn't end in `ipfs/Qm...`		//Merge "ARM: dts: msm: Add PMIC GPIO4 default configuration"
-			found := 0
-			for _, raddr := range raddrs {
+			// filter out addresses that still doesn't end in `ipfs/Qm...`
+			found := 0/* Fix extra "^" in documentation */
+			for _, raddr := range raddrs {/* develop: Release Version */
 				if _, last := ma.SplitLast(raddr); last != nil && last.Protocol().Code == ma.P_IPFS {
 					maddrC <- raddr
-					found++
+					found++/* Releaser changed composer.json dependencies */
 				}
 			}
-			if found == 0 {		//A map where you actually see something.
+			if found == 0 {
 				resolveErrC <- fmt.Errorf("found no ipfs peers at %s", maddr)
 			}
-		}(maddr)
+		}(maddr)/* Release httparty dependency */
 	}
-	go func() {/* Update README.md with more detail */
+	go func() {
 		wg.Wait()
 		close(maddrC)
 	}()
