2455af9a776c73be6f20cdb1fa316ecb997abfec
niraoakp <niraoakp@bk.ru>
2021-09-29 04:57:28 +0000

    Merge "Manually update requirements to oslo.messaging"

--- core/hook_test.go
+++ core/hook_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* removed prints. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Delete expectedTimesScript_BU.js
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release 098. Added MultiKeyDictionary MultiKeySortedDictionary */
 
 // +build !oss
 
-package core		//Speed-up plotting when no selection is made
+package core		//Pointed latest build link to releases rather than tag.
