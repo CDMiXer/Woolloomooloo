83929838b921595c7ca9a714bf97e72b948a87af
niraoakp <niraoakp@bk.ru>
2021-09-29 02:16:15 +0000

    Rename build/angular-date-range-picker.js to angular-date-range-picker.js

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp
@@ -1,11 +1,11 @@
-resource logs "aws:s3:Bucket" {}
+resource logs "aws:s3:Bucket" {}/* Improve object list */
 
-resource bucket "aws:s3:Bucket" {		//Added README, license, updated sources
+resource bucket "aws:s3:Bucket" {
 	loggings = [{
-		targetBucket = logs.bucket,	// TODO: hacked by alex.gaynor@gmail.com
-	}]	// Authentification -> Authentication
+		targetBucket = logs.bucket,
+	}]
 }
 
 output targetBucket {
 	value = bucket.loggings[0].targetBucket
-}
+}/* Release Notes: update for 4.x */
