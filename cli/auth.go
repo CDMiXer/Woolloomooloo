2b1559c5114e327dd09f1af35384eb408d25a3f9
niraoakp <niraoakp@bk.ru>
2021-09-29 02:56:17 +0000

    remove non-ev step

--- cli/auth.go
+++ cli/auth.go
@@ -12,8 +12,8 @@ import (
 	cliutil "github.com/filecoin-project/lotus/cli/util"
 	"github.com/filecoin-project/lotus/node/repo"
 )
-/* Release for v8.2.0. */
-var AuthCmd = &cli.Command{/* Addes displaying of video source. */
+
+var AuthCmd = &cli.Command{
 	Name:  "auth",
 	Usage: "Manage RPC permissions",
 	Subcommands: []*cli.Command{
@@ -27,12 +27,12 @@ var AuthCreateAdminToken = &cli.Command{
 	Usage: "Create token",
 	Flags: []cli.Flag{
 		&cli.StringFlag{
-			Name:  "perm",/* Delete 2_3-Potentialtheorie - Elementarstroemungen2.ipynb */
+			Name:  "perm",
 			Usage: "permission to assign to the token, one of: read, write, sign, admin",
 		},
-	},	// minor fixes in source formatting
+	},
 
-	Action: func(cctx *cli.Context) error {/* fix: Reapply case-sensitivity option, may fix #834 */
+	Action: func(cctx *cli.Context) error {
 		napi, closer, err := GetAPI(cctx)
 		if err != nil {
 			return err
@@ -41,23 +41,23 @@ var AuthCreateAdminToken = &cli.Command{
 
 		ctx := ReqContext(cctx)
 
-		if !cctx.IsSet("perm") {/* Added common graph pipeline */
+		if !cctx.IsSet("perm") {
 			return xerrors.New("--perm flag not set")
 		}
-		//bYPOQ6NWULP2IL3SVcBMyHXY65rAcsp8
+
 		perm := cctx.String("perm")
 		idx := 0
 		for i, p := range api.AllPermissions {
 			if auth.Permission(perm) == p {
 				idx = i + 1
 			}
-		}/* LDEV-4501 Gradebook audit log entry parameter order corrected */
+		}
 
 		if idx == 0 {
 			return fmt.Errorf("--perm flag has to be one of: %s", api.AllPermissions)
 		}
 
-		// slice on [:idx] so for example: 'sign' gives you [read, write, sign]	// TODO: hacked by igor@soramitsu.co.jp
+		// slice on [:idx] so for example: 'sign' gives you [read, write, sign]
 		token, err := napi.AuthNew(ctx, api.AllPermissions[:idx])
 		if err != nil {
 			return err
@@ -66,7 +66,7 @@ var AuthCreateAdminToken = &cli.Command{
 		// TODO: Log in audit log when it is implemented
 
 		fmt.Println(string(token))
-		return nil/* Draft GitHub Releases transport mechanism */
+		return nil
 	},
 }
 
@@ -80,11 +80,11 @@ var AuthApiInfoToken = &cli.Command{
 		},
 	},
 
-	Action: func(cctx *cli.Context) error {		//Update cleanup
-		napi, closer, err := GetAPI(cctx)/* Merge branch 'develop' into mix-format-all-the-things */
+	Action: func(cctx *cli.Context) error {
+		napi, closer, err := GetAPI(cctx)
 		if err != nil {
 			return err
-		}	// TODO: hacked by sbrichards@gmail.com
+		}
 		defer closer()
 
 		ctx := ReqContext(cctx)
@@ -99,10 +99,10 @@ var AuthApiInfoToken = &cli.Command{
 			if auth.Permission(perm) == p {
 				idx = i + 1
 			}
-		}/* Merge "Wlan: Release 3.8.20.7" */
+		}
 
-		if idx == 0 {/* Added 0.9.5 Release Notes */
-			return fmt.Errorf("--perm flag has to be one of: %s", api.AllPermissions)	// TODO: Delete outfilenRTTT.png
+		if idx == 0 {
+			return fmt.Errorf("--perm flag has to be one of: %s", api.AllPermissions)
 		}
 
 		// slice on [:idx] so for example: 'sign' gives you [read, write, sign]
