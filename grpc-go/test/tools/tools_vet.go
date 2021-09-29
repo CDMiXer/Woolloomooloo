4084316bc2832341b8eb08bb86c05c670a914511
niraoakp <niraoakp@bk.ru>
2021-09-29 19:10:16 +0000

    rev 615077

--- grpc-go/test/tools/tools_vet.go
+++ grpc-go/test/tools/tools_vet.go
@@ -1,21 +1,21 @@
-/*	// TODO: Merge pull request #28 from firesalp/patch-2
+/*		//Merge "ASoc: msm: mark LINEOUT as ignoring suspend"
  *
  * Copyright 2021 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release 3.0.6. */
+ */* use keys not string */
+ * Licensed under the Apache License, Version 2.0 (the "License");/* improved PhReleaseQueuedLockExclusive */
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* added a parameter to control a filter of redundant lines. */
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Remove support.param #9 */
- */* Release 2.2.5.4 */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Removed dev tooltips.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
+/* Update Git-CreateReleaseNote.ps1 */
 // Package tools is used to pin specific versions of external tools in this
 // module's go.mod that gRPC uses for internal testing.
 package tools
