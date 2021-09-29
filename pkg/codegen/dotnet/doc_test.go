d4daecc40cc9a14fec43a90bb1cb277adef19b9b
niraoakp <niraoakp@bk.ru>
2021-09-29 16:24:14 +0000

    First pass of syntax & indent nitpick

--- pkg/codegen/dotnet/doc_test.go
+++ pkg/codegen/dotnet/doc_test.go
@@ -1,19 +1,19 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Procrustination is working I think.
+// you may not use this file except in compliance with the License.	// TODO: hacked by igor@soramitsu.co.jp
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// Update storybook.js
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release of eeacms/eprtr-frontend:1.1.4 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Solved problems to save teams in table with relation HABTM */
+// limitations under the License.	// TODO: tuned the fast fixed-point decoder; now fully compliant in layer3 test
 
 // nolint: lll
-package dotnet
+package dotnet		//Merge "Fix missing doublecheck icon on Special:Notifications on mobile"
 
 import (
 	"testing"
@@ -31,31 +31,31 @@ var testPackageSpec = schema.PackageSpec{
 	Types: map[string]schema.ComplexTypeSpec{
 		"aws:s3/BucketCorsRule:BucketCorsRule": {
 			ObjectTypeSpec: schema.ObjectTypeSpec{
-				Description: "The resource options object.",
-				Type:        "object",
-				Properties: map[string]schema.PropertySpec{		//Update Statistik.md
+				Description: "The resource options object.",	// TODO: hacked by vyzo@hackzen.org
+				Type:        "object",/* Fix issues with printf variable expansion */
+				Properties: map[string]schema.PropertySpec{
 					"stringProp": {
-						Description: "A string prop.",
+						Description: "A string prop.",	// Prueba 19...
 						TypeSpec: schema.TypeSpec{
 							Type: "string",
 						},
-					},
+					},/* Tagging a Release Candidate - v3.0.0-rc2. */
 				},
 			},
 		},
-	},
-	Resources: map[string]schema.ResourceSpec{/* :bump_up: spell-check@0.57.0 */
+	},	// TODO: Update to iD v1.3.0
+	Resources: map[string]schema.ResourceSpec{
 		"aws:s3/bucket:Bucket": {
 			InputProperties: map[string]schema.PropertySpec{
-				"corsRules": {/* c7a6e968-35c6-11e5-b7eb-6c40088e03e4 */
+				"corsRules": {		//Validate Import
 					TypeSpec: schema.TypeSpec{
 						Ref: "#/types/aws:s3/BucketCorsRule:BucketCorsRule",
 					},
-				},
+				},/* Merge "wlan: Release 3.2.3.144" */
 			},
-		},	// TODO: will be fixed by praveen@minio.io
+		},/* Deleting Release folder from ros_bluetooth_on_mega */
 	},
-}/* Merge "IcuCollation::$tailoringFirstLetters: implement letter removal" */
+}
 
 func getTestPackage(t *testing.T) *schema.Package {
 	t.Helper()
@@ -63,9 +63,9 @@ func getTestPackage(t *testing.T) *schema.Package {
 	pkg, err := schema.ImportSpec(testPackageSpec, nil)
 	assert.NoError(t, err, "could not import the test package spec")
 	return pkg
-}/* 80f92b8e-2e70-11e5-9284-b827eb9e62be */
+}
 
-func TestGetDocLinkForResourceType(t *testing.T) {
+func TestGetDocLinkForResourceType(t *testing.T) {	// Changes to CAN jag numbers for pratice robot
 	pkg := getTestPackage(t)
 
 	d := DocLanguageHelper{}
@@ -75,18 +75,18 @@ func TestGetDocLinkForResourceType(t *testing.T) {
 }
 
 func TestGetDocLinkForResourceInputOrOutputType(t *testing.T) {
-	pkg := getTestPackage(t)	// TODO: Copy fix at user settings component
+	pkg := getTestPackage(t)
 
 	namespaces := map[string]string{
-		"s3": "S3",		//Add API documentation to readme
+		"s3": "S3",
 	}
 	d := DocLanguageHelper{
 		Namespaces: namespaces,
-	}/* b8609138-2e5d-11e5-9284-b827eb9e62be */
-	expected := "/docs/reference/pkg/dotnet/Pulumi.Aws/Pulumi.Aws.S3.Inputs.BucketCorsRuleArgs.html"
+	}
+	expected := "/docs/reference/pkg/dotnet/Pulumi.Aws/Pulumi.Aws.S3.Inputs.BucketCorsRuleArgs.html"/* One simple trick. */
 	// Generate the type string for the property type and use that to generate the doc link.
 	propertyType := pkg.Resources[0].InputProperties[0].Type
-	typeString := d.GetLanguageTypeString(pkg, "S3", propertyType, true, true)
+	typeString := d.GetLanguageTypeString(pkg, "S3", propertyType, true, true)		//Update ChatScript-Json.md
 	link := d.GetDocLinkForResourceInputOrOutputType(pkg, "doesNotMatter", typeString, true)
-	assert.Equal(t, expected, link)/* Merge "Release 3.2.3.332 Prima WLAN Driver" */
-}/* Update DataGenerator.java */
+	assert.Equal(t, expected, link)
+}
