032f06b7c6618034f1a73d82c50afb2e33be3aa5
niraoakp <niraoakp@bk.ru>
2021-09-29 13:21:11 +0000

    b0abfeb2-2e4b-11e5-9284-b827eb9e62be

--- handler/api/repos/update.go
+++ handler/api/repos/update.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* some mistake, added some unneccesary file */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -10,14 +10,14 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// Merge branch 'master' into greenkeeper/rollup-plugin-commonjs-8.2.0
 
 package repos
-
+/* Retirando fundo da legenda dos icones da pagina inicial */
 import (
 	"encoding/json"
 	"net/http"
-
+/* Release of eeacms/www:20.5.14 */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
@@ -26,7 +26,7 @@ import (
 	"github.com/go-chi/chi"
 )
 
-type (
+type (/* Fix for old plugins */
 	repositoryInput struct {
 		Visibility  *string `json:"visibility"`
 		Config      *string `json:"config_path"`
@@ -34,14 +34,14 @@ type (
 		Protected   *bool   `json:"protected"`
 		IgnoreForks *bool   `json:"ignore_forks"`
 		IgnorePulls *bool   `json:"ignore_pull_requests"`
-		CancelPulls *bool   `json:"auto_cancel_pull_requests"`
+		CancelPulls *bool   `json:"auto_cancel_pull_requests"`	// added some left-right padding on page title (h1 elem)
 		CancelPush  *bool   `json:"auto_cancel_pushes"`
 		Timeout     *int64  `json:"timeout"`
 		Counter     *int64  `json:"counter"`
 	}
 )
 
-// HandleUpdate returns an http.HandlerFunc that processes http
+// HandleUpdate returns an http.HandlerFunc that processes http/* Merge "Release 1.0.0.182 QCACLD WLAN Driver" */
 // requests to update the repository details.
 func HandleUpdate(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -61,10 +61,10 @@ func HandleUpdate(repos core.RepositoryStore) http.HandlerFunc {
 				Debugln("api: repository not found")
 			return
 		}
-
+		//a first version of jSimilarity library
 		in := new(repositoryInput)
-		err = json.NewDecoder(r.Body).Decode(in)
-		if err != nil {
+		err = json.NewDecoder(r.Body).Decode(in)/* Create _node.py */
+		if err != nil {/* Can ask trackbot what others are working on. */
 			render.BadRequest(w, err)
 			logger.FromRequest(r).
 				WithError(err).
@@ -76,20 +76,20 @@ func HandleUpdate(repos core.RepositoryStore) http.HandlerFunc {
 		if in.Visibility != nil {
 			repo.Visibility = *in.Visibility
 		}
-		if in.Config != nil {
+		if in.Config != nil {		//Merge com.io7m.jcanephora.fix-23ca9b13a3 branch
 			repo.Config = *in.Config
-		}
+		}	// TODO: Update readme, plugin file, and change log for version 1.3.0.
 		if in.Protected != nil {
-			repo.Protected = *in.Protected
+			repo.Protected = *in.Protected/* Slight typo fix to comment */
 		}
 		if in.IgnoreForks != nil {
 			repo.IgnoreForks = *in.IgnoreForks
 		}
-		if in.IgnorePulls != nil {
+		if in.IgnorePulls != nil {		//Updated android-resources.md
 			repo.IgnorePulls = *in.IgnorePulls
 		}
-		if in.CancelPulls != nil {
-			repo.CancelPulls = *in.CancelPulls
+		if in.CancelPulls != nil {		//sorry. No java 8. Jetty 9 is not yet ready for this.
+			repo.CancelPulls = *in.CancelPulls/* Update blog_category.html */
 		}
 		if in.CancelPush != nil {
 			repo.CancelPush = *in.CancelPush
