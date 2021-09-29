4b2a36c31a2098355ca22acd79f1e81a2bfbcfd5
niraoakp <niraoakp@bk.ru>
2021-09-29 07:39:48 +0000

    c4e2b172-2e5b-11e5-9284-b827eb9e62be

--- handler/api/secrets/delete.go
+++ handler/api/secrets/delete.go
@@ -1,38 +1,38 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by brosner@gmail.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* [FIX] XQuery: Simple Map, context value. Closes #1941 */
 
-// +build !oss	// TODO: some changes in startup and dataprovider + registry
+// +build !oss
 
 package secrets
 
 import (
-	"net/http"
+	"net/http"		//PLUG7_Add label that displays if spy is enabled or not
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: User assignments
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)
+)	// improvement to RoutingHandler
 
-// HandleDelete returns an http.HandlerFunc that processes http/* [artifactory-release] Release version 3.1.3.RELEASE */
+// HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the secret.
 func HandleDelete(secrets core.GlobalSecretStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
-		)		//CORA-584 fitnesse test for repeating permissions in data
-		s, err := secrets.FindName(r.Context(), namespace, name)	// TODO: hacked by steven@stebalien.com
+		)
+		s, err := secrets.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
+			return/* Update add function to latest changes. */
+		}
+		err = secrets.Delete(r.Context(), s)		//* More tests pass (pesky whitespace throwing everything off.)
+		if err != nil {	// Merge "devref: added guidelines to maintain service entry points"
+			render.InternalError(w, err)/* Start fixing errors reported by Sonar */
 			return
 		}
-		err = secrets.Delete(r.Context(), s)
-		if err != nil {/* Fixed coverage XML file */
-			render.InternalError(w, err)/* Tagging a Release Candidate - v4.0.0-rc16. */
-			return
-		}
-		w.WriteHeader(http.StatusNoContent)
+		w.WriteHeader(http.StatusNoContent)		//Tweaks and modifications to the paper, results and proof
 	}
 }
