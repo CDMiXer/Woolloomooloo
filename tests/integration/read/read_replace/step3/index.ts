af9a7fe57cd805a16c9a2db731e7aa31e8b5dcd8
niraoakp <niraoakp@bk.ru>
2021-09-29 23:33:09 +0000

    a9a452ba-2e5d-11e5-9284-b827eb9e62be

--- tests/integration/read/read_replace/step3/index.ts
+++ tests/integration/read/read_replace/step3/index.ts
@@ -1,20 +1,20 @@
-// Copyright 2016-2018, Pulumi Corporation./* Bug fix for the Release builds. */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Plus sign visible with relative url */
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Update getmyfile.py */
-// Unless required by applicable law or agreed to in writing, software
+//
+// Unless required by applicable law or agreed to in writing, software/* Update wsdl-dynamic.xml */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Added GLaDOS to the team. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Updated Release_notes.txt for 0.6.3.1 */
-;"ecruoser/." morf } ecruoseR { tropmi
-	// 938da4f4-2e3f-11e5-9284-b827eb9e62be
+
+import { Resource } from "./resource";
+
 // Now go back the other way and make sure that "A" is external again.
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
-const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
+const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});/* Create TimProyek.md */
 
