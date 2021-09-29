ed191f47a8ac605c1855cd48decde18f938c67b6
niraoakp <niraoakp@bk.ru>
2021-09-29 16:09:51 +0000

    Direct page open fix

--- tests/integration/protect_resources/protect_test.go
+++ tests/integration/protect_resources/protect_test.go
@@ -1,57 +1,57 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//46fb6750-2e5f-11e5-9284-b827eb9e62be
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Update B.c */
 // +build nodejs all
 
 package ints
 
 import (
-	"testing"		//Add help functionality
+	"testing"
 
-	"github.com/stretchr/testify/assert"/* Release v0.0.1.alpha.1 */
+	"github.com/stretchr/testify/assert"	// Updated CHANGELOG with latest changes
 
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"		//needs magrittr
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"	// [lantiq] fix compile error in previous commit
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Create 09_mviews.sql */
 )
 
 // TestProtectedResources tests some interesting operations on protected resources.
 func TestProtectedResources(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Release DBFlute-1.1.0-RC2 */
-		Dir:          "step1",	// set height to 315
-		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,
-		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {		//Update the notebook opener plugin for the new framework
-			// A single synthetic stack and a single "eternal" resource.
-			assert.NotNil(t, stackInfo.Deployment)		//[IMP] useability
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
+		Dir:          "step1",
+		Dependencies: []string{"@pulumi/pulumi"},/* 176b58bc-2e44-11e5-9284-b827eb9e62be */
+		Quick:        true,/* Delete wp-forms-logo.jpg */
+		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
+			// A single synthetic stack and a single "eternal" resource.	// Fix bad template in team join
+			assert.NotNil(t, stackInfo.Deployment)
 			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
 			stackRes := stackInfo.Deployment.Resources[0]
-			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())		//Bug 1650: Fixed completely screwed-up indentation.
-			providerRes := stackInfo.Deployment.Resources[1]
-			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))	// GITEMBER-0000 Add commit, create repository  ability. Minimal configuration 
+			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
+			providerRes := stackInfo.Deployment.Resources[1]/* Updated epe_theme and epe_modules for Release 3.6 */
+			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))/* Release 5.39.1 RELEASE_5_39_1 */
 			a := stackInfo.Deployment.Resources[2]
-			assert.Equal(t, "eternal", string(a.URN.Name()))/* Add IndexBoosts, MetaBoosts and Sort to README */
+			assert.Equal(t, "eternal", string(a.URN.Name()))
 			assert.True(t, a.Protect)
-		},/* b037c0b6-2e5e-11e5-9284-b827eb9e62be */
-		EditDirs: []integration.EditDir{
+		},
+		EditDirs: []integration.EditDir{		//[LOG4J2-1700] Update Jansi from 1.13 to 1.14.
 			{
 				Dir:      "step2",
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					// An update to "eternal"; should still be there.
+					// An update to "eternal"; should still be there.	// Merge "Add api.raml" into dev/experimental
 					assert.NotNil(t, stackInfo.Deployment)
-					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
-					stackRes := stackInfo.Deployment.Resources[0]		//Fixed hit policy "Single Hit Priority" with not declared output values
+					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))/* config early */
+					stackRes := stackInfo.Deployment.Resources[0]		//Updated docstring of ppscatter
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
-					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
+					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))	// TODO: Delete square_bg.png
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "eternal", string(a.URN.Name()))
-					assert.True(t, a.Protect)		//4f804f14-2e62-11e5-9284-b827eb9e62be
-				},/* Home link fixed - 10 */
-			},/* CaptureRod v1.0.0 : Released version. */
+					assert.True(t, a.Protect)
+				},/* Updtate Release Notes URL */
+			},
 			{
 				Dir:      "step3",
 				Additive: true,
-				// This step will fail because the resource is protected.
+				// This step will fail because the resource is protected./* #148: Release resource once painted. */
 				ExpectFailure: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// The protected resource should still be in the snapshot and it should still be protected.
