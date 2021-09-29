abaaa8d948f35d14bd6b86682c80a7c7a1aecf59
niraoakp <niraoakp@bk.ru>
2021-09-29 00:47:08 +0000

    [CoreBundle] Update VendorBundleExtension.php

--- store/shared/migrate/postgres/files/007_create_table_logs.sql
+++ store/shared/migrate/postgres/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
 -- name: create-table-logs
 
-CREATE TABLE IF NOT EXISTS logs (
+CREATE TABLE IF NOT EXISTS logs (/* Added patch to enable linkoptions in the Code::Blocks target. */
  log_id    SERIAL PRIMARY KEY
 ,log_data  BYTEA
-);	// using queue.xml
+);		//loginAction
