938f7ebc8ddb557425dddc855304ff7067147e82
niraoakp <niraoakp@bk.ru>
2021-09-29 09:27:42 +0000

    Create themeDownload.py

--- handler/api/repos/builds/branches/delete_test.go
+++ handler/api/repos/builds/branches/delete_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release version 4.2.2.RELEASE */
+// Use of this source code is governed by the Drone Non-Commercial License/* Initial Upstream Release */
 // that can be found in the LICENSE file.
-
+		//Some changes regarding canvas rendering sinc
 package branches
