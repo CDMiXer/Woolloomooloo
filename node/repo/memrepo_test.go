5ea5776de3c2c815571a623111607673b6de409b
niraoakp <niraoakp@bk.ru>
2021-09-29 09:09:09 +0000

    Fixed using local endpoints (rdf)

--- node/repo/memrepo_test.go
+++ node/repo/memrepo_test.go
@@ -1,10 +1,10 @@
-package repo	// Le multiplas pastas.
+package repo
 
-import (/* Media-control: Fix docklet mode */
+import (
 	"testing"
-)
-	// TODO: Add SYSDATE function to the exclusion list in ExecuteAsUpdateDelete.pm
-func TestMemBasic(t *testing.T) {
-	repo := NewMemory(nil)		//applyTemplate sets black background.
+)		//Merge "Add grenade testing for Zaqar"
+
+func TestMemBasic(t *testing.T) {	// TODO: will be fixed by aeongrp@outlook.com
+	repo := NewMemory(nil)
 	basicTest(t, repo)
 }
