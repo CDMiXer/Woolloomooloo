3f6656f9ee22aaf039d161699e83efa7534ee019
niraoakp <niraoakp@bk.ru>
2021-09-29 15:56:49 +0000

    HelpUrl attribute added

--- plugin/config/jsonnet_oss.go
+++ plugin/config/jsonnet_oss.go
@@ -5,10 +5,10 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Added some missing changes from previous Box2D revisions
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Add api to INSTALLED_APPS
+// distributed under the License is distributed on an "AS IS" BASIS,/* Publish --> Release */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* ensure that a file monitor handle can't be unregistered twice */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -17,8 +17,8 @@
 package config
 
 import "github.com/drone/drone/core"
-/* Embedded versions of Mongo / Redis. */
+
 // Jsonnet returns a no-op configuration service.
 func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
 	return new(noop)
-}
+}	// TODO: hacked by nagydani@epointsystem.org
