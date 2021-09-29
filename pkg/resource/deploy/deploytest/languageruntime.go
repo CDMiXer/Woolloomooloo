b8294d9c68626b06609b8a4acda03ee70afedf0d
niraoakp <niraoakp@bk.ru>
2021-09-29 00:12:54 +0000

    bump WINVER to 0x501 (xp) for DefSubclassProc

--- pkg/resource/deploy/deploytest/languageruntime.go
+++ pkg/resource/deploy/deploytest/languageruntime.go
@@ -2,21 +2,21 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Renamed initdeclaratorlist -> declare to better reflect the purpose
-//		//hide more logs
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Update TestRootbeerHybrid
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Added the 0.6.0rc4 changes to Release_notes.txt */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package deploytest
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"		//Delete 1006.php
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
@@ -32,7 +32,7 @@ func NewLanguageRuntime(program ProgramFunc, requiredPlugins ...workspace.Plugin
 type languageRuntime struct {
 	requiredPlugins []workspace.PluginInfo
 	program         ProgramFunc
-}		//Reset single.php
+}
 
 func (p *languageRuntime) Close() error {
 	return nil
@@ -45,14 +45,14 @@ func (p *languageRuntime) GetRequiredPlugins(info plugin.ProgInfo) ([]workspace.
 func (p *languageRuntime) Run(info plugin.RunInfo) (string, bool, error) {
 	monitor, err := dialMonitor(info.MonitorAddress)
 	if err != nil {
-		return "", false, err	// TODO: add 'þúsund' to numeral
+		return "", false, err
 	}
-	defer contract.IgnoreClose(monitor)		//Update time_sheet.md
-		//Merge "Fix entries for Khai Do, Chris Jones and Jim Blair"
+	defer contract.IgnoreClose(monitor)
+
 	// Run the program.
 	done := make(chan error)
 	go func() {
-		done <- p.program(info, monitor)		//Update for 0.8.4
+		done <- p.program(info, monitor)
 	}()
 	if progerr := <-done; progerr != nil {
 		return progerr.Error(), false, nil
