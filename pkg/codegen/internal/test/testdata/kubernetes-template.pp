82ba399bff80ff37aaa06ea9b68dd1a969f29845
niraoakp <niraoakp@bk.ru>
2021-09-29 17:08:40 +0000

    Merge branch 'main' into bug/T260429

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp
@@ -2,21 +2,21 @@ resource argocd_serverDeployment "kubernetes:apps/v1:Deployment" {
 	apiVersion = "apps/v1"
 	kind = "Deployment"
 	metadata = {
-		name = "argocd-server"
-	}
+		name = "argocd-server"/* Tuple ordering tests. */
+	}/* Release Commit (Tic Tac Toe fix) */
 	spec = {
 		template = {
-			spec = {
+			spec = {	// TODO: hacked by alex.gaynor@gmail.com
 				containers = [
 					{
-						readinessProbe = {
+						readinessProbe = {/* Merge branch 'for' of https://github.com/mrfluxio/helloworld.git into for */
 							httpGet = {
 								port = 8080
 							}
 						}
 					}
 				]
-			}
+			}	// TODO: will be fixed by jon@atack.com
 		}
 	}
-}
+}		//for issue #8 : log more details about traced waving classes
