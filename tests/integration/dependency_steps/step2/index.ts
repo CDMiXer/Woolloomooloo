8671de60bb721016d5acef023b89c151143c0bf7
niraoakp <niraoakp@bk.ru>
2021-09-29 10:10:54 +0000

    Fix buildModel check

--- tests/integration/dependency_steps/step2/index.ts
+++ tests/integration/dependency_steps/step2/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import { Resource } from "./resource";
+		//Delete underconstruction.gif
+import { Resource } from "./resource";	// Automatic changelog generation for PR #9344 [ci skip]
 
 // Step 2: Reverse the dependency graph.
-// Checkpoint dependency graph: b -> a
+// Checkpoint dependency graph: b -> a	// TODO: help: Add a missing entry to changelog
 const b = new Resource("b", { state: 2 })
 const a = new Resource("a", { state: 1, resource: b })
 // Dependency graph: a -> b
