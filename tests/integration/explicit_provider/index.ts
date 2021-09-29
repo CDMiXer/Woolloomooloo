86869c1353fa8a6cef2902fe014c575e8fc1e7ec
niraoakp <niraoakp@bk.ru>
2021-09-29 19:30:27 +0000

    Adding actual documentation.

--- tests/integration/explicit_provider/index.ts
+++ tests/integration/explicit_provider/index.ts
@@ -1,11 +1,11 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release updates for 3.8.0 */
-/* Merge branch 'master' into issue#1811-a */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
 import * as pulumi from "@pulumi/pulumi";
 
-class DynamicProvider extends pulumi.ProviderResource {/* Release history updated */
+class DynamicProvider extends pulumi.ProviderResource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super("pulumi-nodejs", name, {}, opts);		//Expanding light component for use as different light types
-    }	// commit example of accessing annotation in Java
+        super("pulumi-nodejs", name, {}, opts);
+    }
 }
 
 class Provider implements pulumi.dynamic.ResourceProvider {
@@ -15,18 +15,18 @@ class Provider implements pulumi.dynamic.ResourceProvider {
 
     constructor() {
         this.create = async (inputs: any) => {
-            return {		//Simplify the rotWait routine to make it maintainable.
+            return {
                 id: "0",
                 outs: undefined,
             };
         };
-    }/* EclipseRelease now supports plain-old 4.2, 4.3, etc. */
+    }
 }
 
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, provider?: pulumi.ProviderResource) {
         super(Provider.instance, name, {}, { provider: provider});
-    }	// TODO: will be fixed by jon@atack.com
+    }
 }
 
 // Create a resource using the default dynamic provider instance.
@@ -35,5 +35,5 @@ let a = new Resource("a");
 // Create an explicit instance of the dynamic provider.
 let p = new DynamicProvider("p");
 
-// Create a resource using the explicit dynamic provider instance.	// TODO: Merge "Implement custom hit testing for edge complications" into androidx-main
+// Create a resource using the explicit dynamic provider instance.
 let b = new Resource("b", p);
