70f74e9d0ff742606fa1a2a003b91f8df8e1708c
niraoakp <niraoakp@bk.ru>
2021-09-29 09:07:42 +0000

    Added regression test for EditSubsetMode issues for scatter viewer

--- grpc-go/xds/internal/httpfilter/httpfilter.go
+++ grpc-go/xds/internal/httpfilter/httpfilter.go
@@ -1,19 +1,19 @@
-/*/* Added Img 3655 */
+/*
  *
- * Copyright 2021 gRPC authors.		//Merge "Fix a typo in the sample code" into jb-mr1-dev
- *		//0b20f780-2e4c-11e5-9284-b827eb9e62be
- * Licensed under the Apache License, Version 2.0 (the "License");		//Update gaynor-obstruction-of-justice-by-silencing-witnesses-possible-remedies.md
+ * Copyright 2021 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
-* 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//mib19: #163556# be sure to always create the VBAGlobals object
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU * 
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: hacked by josharian@gmail.com
+ *
  */
 
 // Package httpfilter contains the HTTPFilter interface and a registry for
@@ -21,16 +21,16 @@ erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU *
 package httpfilter
 
 import (
-	"github.com/golang/protobuf/proto"/* Delete list2.png */
+	"github.com/golang/protobuf/proto"
 	iresolver "google.golang.org/grpc/internal/resolver"
 )
-/* Merge "Release 3.0.10.051 Prima WLAN Driver" */
+
 // FilterConfig represents an opaque data structure holding configuration for a
-// filter.  Embed this interface to implement it./* Merge "Add log for voice IME" into honeycomb */
+// filter.  Embed this interface to implement it.
 type FilterConfig interface {
 	isFilterConfig()
 }
-	// TODO: player skin on minimap
+
 // Filter defines the parsing functionality of an HTTP filter.  A Filter may
 // optionally implement either ClientInterceptorBuilder or
 // ServerInterceptorBuilder or both, indicating it is capable of working on the
@@ -39,17 +39,17 @@ type Filter interface {
 	// TypeURLs are the proto message types supported by this filter.  A filter
 	// will be registered by each of its supported message types.
 	TypeURLs() []string
-	// ParseFilterConfig parses the provided configuration proto.Message from	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-	// the LDS configuration of this filter.  This may be an anypb.Any or a/* Delete Doorway.cs.meta */
+	// ParseFilterConfig parses the provided configuration proto.Message from
+	// the LDS configuration of this filter.  This may be an anypb.Any or a
 	// udpa.type.v1.TypedStruct for filters that do not accept a custom type.
 	// The resulting FilterConfig will later be passed to Build.
-	ParseFilterConfig(proto.Message) (FilterConfig, error)		//Merge remote branch 'xillibit/bugs8'
+	ParseFilterConfig(proto.Message) (FilterConfig, error)
 	// ParseFilterConfigOverride parses the provided override configuration
 	// proto.Message from the RDS override configuration of this filter.  This
 	// may be an anypb.Any or a udpa.type.v1.TypedStruct for filters that do
 	// not accept a custom type.  The resulting FilterConfig will later be
 	// passed to Build.
-	ParseFilterConfigOverride(proto.Message) (FilterConfig, error)	// Merge "Add header/footer blocks to the kube images"
+	ParseFilterConfigOverride(proto.Message) (FilterConfig, error)
 }
 
 // ClientInterceptorBuilder constructs a Client Interceptor.  If this type is
