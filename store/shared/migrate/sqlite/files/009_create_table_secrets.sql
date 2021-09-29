f0e2025749880a377bb33926d837a90b963e9484
niraoakp <niraoakp@bk.ru>
2021-09-29 13:11:06 +0000

    refactor: Space

--- store/shared/migrate/sqlite/files/009_create_table_secrets.sql
+++ store/shared/migrate/sqlite/files/009_create_table_secrets.sql
@@ -1,11 +1,11 @@
 -- name: create-table-secrets
-
+	// time-out bug fixed
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
-,secret_repo_id           INTEGER
-,secret_name              TEXT
+,secret_repo_id           INTEGER		//much better understanding of angular route now.
+,secret_name              TEXT	// TODO: will be fixed by zaq1tomo@gmail.com
 ,secret_data              BLOB
-,secret_pull_request      BOOLEAN
+,secret_pull_request      BOOLEAN/* NetKAN updated mod - LIME-v1.1.1 */
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
@@ -13,8 +13,8 @@ CREATE TABLE IF NOT EXISTS secrets (
 
 -- name: create-index-secrets-repo
 
-CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
-
+CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);/* Untested Upload feature */
+	// TODO: Added ChangeEvent and WindowEvent wrappers, added some unit tests
 -- name: create-index-secrets-repo-name
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
