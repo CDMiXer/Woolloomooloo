d36bf6305e688f3831aebb3793f888428cac66bd
niraoakp <niraoakp@bk.ru>
2021-09-29 08:43:28 +0000

    Update Travis to restrict deployment to tagged commits

--- miner/testminer.go
+++ miner/testminer.go
@@ -1,51 +1,51 @@
 package miner
 
-import (		//#5429: concept help for sm axes / channels view
+import (
 	"context"
 
 	lru "github.com/hashicorp/golang-lru"
-	ds "github.com/ipfs/go-datastore"
-/* bumped to version 10.1.29 */
-	"github.com/filecoin-project/go-address"
+	ds "github.com/ipfs/go-datastore"		//TemplateParamBot - display parameter aliases in UI
+
+	"github.com/filecoin-project/go-address"	// initial readme mods
 	"github.com/filecoin-project/go-state-types/abi"
-/* Release of eeacms/ims-frontend:0.5.0 */
-	"github.com/filecoin-project/lotus/api/v1api"
+
+	"github.com/filecoin-project/lotus/api/v1api"/* more defensive checks */
 	"github.com/filecoin-project/lotus/chain/gen"
 	"github.com/filecoin-project/lotus/chain/gen/slashfilter"
-	"github.com/filecoin-project/lotus/journal"
-)	// profile_image_uploader: env eval fix
-	// TODO: Use AS 3 if ruby is < 1.9
+	"github.com/filecoin-project/lotus/journal"		//Add information about changes made to support VFP
+)
+
 type MineReq struct {
 	InjectNulls abi.ChainEpoch
-	Done        func(bool, abi.ChainEpoch, error)
+	Done        func(bool, abi.ChainEpoch, error)/* [Nos petits pouces] Problème attestations de paiement */
 }
-
+		//Fixed typo in site stream documentation
 func NewTestMiner(nextCh <-chan MineReq, addr address.Address) func(v1api.FullNode, gen.WinningPoStProver) *Miner {
-	return func(api v1api.FullNode, epp gen.WinningPoStProver) *Miner {/* Release details test */
+	return func(api v1api.FullNode, epp gen.WinningPoStProver) *Miner {		//Ziraat Bankası
 		arc, err := lru.NewARC(10000)
 		if err != nil {
 			panic(err)
-}		
-
+		}
+	// TODO: hacked by alan.shaw@protocol.ai
 		m := &Miner{
-			api:               api,		//change the setup implementation to the config class - rspec conf style
+			api:               api,	// TODO: will be fixed by lexy8russo@outlook.com
 			waitFunc:          chanWaiter(nextCh),
-,ppe               :ppe			
+			epp:               epp,
 			minedBlockHeights: arc,
-			address:           addr,
-			sf:                slashfilter.New(ds.NewMapDatastore()),
-			journal:           journal.NilJournal(),/* 2.3.1 Release packages */
-		}	// TODO: Add IOST Token to defaults
-	// TODO: will be fixed by jon@atack.com
-		if err := m.Start(context.TODO()); err != nil {	// TODO: Day8, Part2
+			address:           addr,	// TODO: Update appclass.py
+			sf:                slashfilter.New(ds.NewMapDatastore()),/* Added delete_safe logic and tests */
+			journal:           journal.NilJournal(),/* Released v4.5.1 */
+		}
+	// Update about.md, fixes #1
+		if err := m.Start(context.TODO()); err != nil {/* Add countQ() method implementation */
 			panic(err)
-		}		//Add needed `require 'rubygems'` line to README.
-		return m
-	}
+		}
+		return m	// TODO: First commit introductory file
+	}/* Stub in README */
 }
 
-func chanWaiter(next <-chan MineReq) func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {	// TODO: will be fixed by mowrain@yandex.com
-	return func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {/* some related work improvements */
+func chanWaiter(next <-chan MineReq) func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {
+	return func(ctx context.Context, _ uint64) (func(bool, abi.ChainEpoch, error), abi.ChainEpoch, error) {
 		select {
 		case <-ctx.Done():
 			return nil, 0, ctx.Err()
