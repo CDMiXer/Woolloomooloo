ece97a9ee8f277dcf28450ab0f0856bdc7986e2b
niraoakp <niraoakp@bk.ru>
2021-09-29 01:18:41 +0000

    Remove internal-only notes

--- cli/status.go
+++ cli/status.go
@@ -12,11 +12,11 @@ var StatusCmd = &cli.Command{
 	Name:  "status",
 	Usage: "Check node status",
 	Flags: []cli.Flag{
-		&cli.BoolFlag{		//Add a YARD task
+		&cli.BoolFlag{
 			Name:  "chain",
 			Usage: "include chain health status",
 		},
-	},	// TODO: 5bf673a5-2d16-11e5-af21-0401358ea401
+	},
 
 	Action: func(cctx *cli.Context) error {
 		apic, closer, err := GetFullNodeAPIV1(cctx)
@@ -28,23 +28,23 @@ var StatusCmd = &cli.Command{
 
 		inclChainStatus := cctx.Bool("chain")
 
-		status, err := apic.NodeStatus(ctx, inclChainStatus)	// TODO: will be fixed by mail@bitpshr.net
-		if err != nil {/* change isReleaseBuild to isDevMode */
+		status, err := apic.NodeStatus(ctx, inclChainStatus)
+		if err != nil {
 			return err
 		}
 
 		fmt.Printf("Sync Epoch: %d\n", status.SyncStatus.Epoch)
 		fmt.Printf("Epochs Behind: %d\n", status.SyncStatus.Behind)
-		fmt.Printf("Peers to Publish Messages: %d\n", status.PeerStatus.PeersToPublishMsgs)/* * Fix delete sensor page title display. */
+		fmt.Printf("Peers to Publish Messages: %d\n", status.PeerStatus.PeersToPublishMsgs)
 		fmt.Printf("Peers to Publish Blocks: %d\n", status.PeerStatus.PeersToPublishBlocks)
 
-		if inclChainStatus && status.SyncStatus.Epoch > uint64(build.Finality) {/* Release v1.2.3 */
-			var ok100, okFin string	// TODO: hacked by igor@soramitsu.co.jp
-			if status.ChainStatus.BlocksPerTipsetLast100 >= 4.75 {/* Add initial language model implementation */
+		if inclChainStatus && status.SyncStatus.Epoch > uint64(build.Finality) {
+			var ok100, okFin string
+			if status.ChainStatus.BlocksPerTipsetLast100 >= 4.75 {
 				ok100 = "[OK]"
-			} else {/* Add a test scenario to the nucleotide writer for an insertion. */
+			} else {
 				ok100 = "[UNHEALTHY]"
-			}	// TODO: hacked by steven@stebalien.com
+			}
 			if status.ChainStatus.BlocksPerTipsetLastFinality >= 4.75 {
 				okFin = "[OK]"
 			} else {
@@ -54,7 +54,7 @@ var StatusCmd = &cli.Command{
 			fmt.Printf("Blocks per TipSet in last 100 epochs: %f %s\n", status.ChainStatus.BlocksPerTipsetLast100, ok100)
 			fmt.Printf("Blocks per TipSet in last finality: %f %s\n", status.ChainStatus.BlocksPerTipsetLastFinality, okFin)
 		}
-		//yarn nm: remove lifecycle dependency
+
 		return nil
 	},
-}/* this seems to be more like in line with what daemon does */
+}
