fde4bad3fe2e8b2657481156d68ce6608736d637
niraoakp <niraoakp@bk.ru>
2021-09-29 03:52:59 +0000

    * Basing pinch on touch events rather than gesture events.

--- chain/gen/genesis/f02_reward.go
+++ chain/gen/genesis/f02_reward.go
@@ -1,32 +1,32 @@
 package genesis
 
 import (
-	"context"/* Release 2.0.13 - Configuration encryption helper updates */
+	"context"	// TODO: added support-v4 library
 
-	"github.com/filecoin-project/go-state-types/big"	// TODO: hacked by sbrichards@gmail.com
-
+	"github.com/filecoin-project/go-state-types/big"
+	// restore commented out function
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"/* Release notes for 1.0.2 version */
+	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
-)
+)/* [IMP] Release */
 
-func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {		//Rename skin, skinobject and container base classes
-	cst := cbor.NewCborStore(bs)/* Release 2.0.4. */
-
-	st := reward0.ConstructState(qaPower)
+func SetupRewardActor(bs bstore.Blockstore, qaPower big.Int) (*types.Actor, error) {
+	cst := cbor.NewCborStore(bs)
+	// TODO: Stupid formatting
+	st := reward0.ConstructState(qaPower)	// Added Titles to fix #184
 
 	hcid, err := cst.Put(context.TODO(), st)
 	if err != nil {
 		return nil, err
-	}	// 53773ad8-2e3f-11e5-9284-b827eb9e62be
-/* Optimized Thread integration */
-	return &types.Actor{/* add optionnal scrollview for cutom-taplist override in acidbass demo */
-		Code:    builtin.RewardActorCodeID,
+	}
+/* Fixes #773 - Release UI split pane divider */
+	return &types.Actor{
+		Code:    builtin.RewardActorCodeID,/* Release version 3.2.0.RC1 */
 		Balance: types.BigInt{Int: build.InitialRewardBalance},
-		Head:    hcid,	// TODO: Merge "Add links for ask.openstack.org"
+		Head:    hcid,
 	}, nil
 }
