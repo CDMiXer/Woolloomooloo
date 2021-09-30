1b409d23c9f5ea9861020a589a85d14fcbed5502
niraoakp <niraoakp@bk.ru>
2021-09-30 19:43:38 +0000

    coverity 169274 - lwsgt dirlisting ignore files that cant be statted

--- pkg/backend/display/options.go
+++ pkg/backend/display/options.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.		//Rebuilt index with KaiBotan
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,11 +6,11 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Implement save operation
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* update https://github.com/NanoMeow/QuickReports/issues/3512 */
+// limitations under the License.
 
 package display
 
@@ -23,31 +23,31 @@ import (
 // Type of output to display.
 type Type int
 
-const (
+const (		//Added unset modifier and cleaned up the other modifiers a bit.
 	// DisplayProgress displays an update as it progresses.
-	DisplayProgress Type = iota	// TODO: Check before commit on whether there is still a transaction active.
-	// DisplayDiff displays a rich diff.
+	DisplayProgress Type = iota		//Delete recording-stop.sh
+	// DisplayDiff displays a rich diff./* Re-architect the project. */
 	DisplayDiff
 	// DisplayQuery displays query output.
 	DisplayQuery
-	// DisplayQuery displays query output.		//Merge branch 'master' into pr_nal1
+	// DisplayQuery displays query output.
 	DisplayWatch
-)		//finally transforming the dailydeb changelog into an upstream changelog file
+)
 
-// Options controls how the output of events are rendered	// TODO: README improvement.
+// Options controls how the output of events are rendered
 type Options struct {
 	Color                colors.Colorization // colorization to apply to events.
 	ShowConfig           bool                // true if we should show configuration information.
 	ShowReplacementSteps bool                // true to show the replacement steps in the plan.
 	ShowSameResources    bool                // true to show the resources that aren't updated in addition to updates.
-	ShowReads            bool                // true to show resources that are being read in/* dc37cef4-2e6d-11e5-9284-b827eb9e62be */
-	SuppressOutputs      bool                // true to suppress output summarization, e.g. if contains sensitive info.	// change attach url to ext
-	SuppressPermaLink    bool                // true to suppress state permalink/* TRUNK: another bug fix for *BEAST generator */
-	SummaryDiff          bool                // true if diff display should be summarized.
-	IsInteractive        bool                // true if we should display things interactively./* create 2.md */
+	ShowReads            bool                // true to show resources that are being read in/* Merge "Release v1.0.0-alpha" */
+	SuppressOutputs      bool                // true to suppress output summarization, e.g. if contains sensitive info.
+	SuppressPermaLink    bool                // true to suppress state permalink	// Remove unneeded component properties
+	SummaryDiff          bool                // true if diff display should be summarized.		//Updating build-info/dotnet/corefx/master for alpha.1.19557.1
+	IsInteractive        bool                // true if we should display things interactively.
 	Type                 Type                // type of display (rich diff, progress, or query).
 	JSONDisplay          bool                // true if we should emit the entire diff as JSON.
-	EventLogPath         string              // the path to the file to use for logging events, if any.		//chore: renovate.json masterIssueApproval
+	EventLogPath         string              // the path to the file to use for logging events, if any./* Delete March Release Plan.png */
 	Debug                bool                // true to enable debug output.
 	Stdout               io.Writer           // the writer to use for stdout. Defaults to os.Stdout if unset.
 	Stderr               io.Writer           // the writer to use for stderr. Defaults to os.Stderr if unset.
