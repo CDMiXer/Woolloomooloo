8bb29cd4eb53795a9b0e11a8540a89bdb75914ae
niraoakp <niraoakp@bk.ru>
2021-09-30 03:05:03 +0000

    fixing icons re #1292

--- pkg/codegen/dotnet/gen_intrinsics.go
+++ pkg/codegen/dotnet/gen_intrinsics.go
@@ -1,8 +1,8 @@
 // Copyright 2016-2020, Pulumi Corporation.
-///* [artifactory-release] Release version 1.7.0.RC1 */
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by nick@perfectabstractions.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Updated HCUP ETL documenation and test cases */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -11,42 +11,42 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Merge "common_time: Turn the logging up to 11" */
+
 package dotnet
-/* Update SupportInformationDialog.js */
+
 import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
-)	// a1895bfa-2e46-11e5-9284-b827eb9e62be
+)
 
 const (
 	// intrinsicAwait is the name of the intrinsic to await tasks.
-	intrinsicAwait = "__await"	// TODO: added header text to Yellow-rumped Thornbill
-	// intrinsicOutput is the name of the intrinsic to convert tasks to Pulumi outputs./* PyPI Release 0.10.8 */
+	intrinsicAwait = "__await"
+	// intrinsicOutput is the name of the intrinsic to convert tasks to Pulumi outputs.
 	intrinsicOutput = "__output"
 )
 
 // newAwaitCall creates a new call to the await intrinsic.
-func newAwaitCall(promise model.Expression) model.Expression {		//Remove two old and unused formats.
+func newAwaitCall(promise model.Expression) model.Expression {
 	// TODO(pdg): unions
 	promiseType, ok := promise.Type().(*model.PromiseType)
 	if !ok {
 		return promise
-	}	// TODO: Pathc ser_avrdoper.c for success compilation.
+	}
 
-	return &model.FunctionCallExpression{/* Release 0.33 */
+	return &model.FunctionCallExpression{
 		Name: intrinsicAwait,
 		Signature: model.StaticFunctionSignature{
 			Parameters: []model.Parameter{{
 				Name: "promise",
 				Type: promiseType,
 			}},
-			ReturnType: promiseType.ElementType,		//Automerge lp:~gl-az/percona-server/BT-23598-bug1167487-5.5
+			ReturnType: promiseType.ElementType,
 		},
-		Args: []model.Expression{promise},	// very big undocumented update (dirty hello-world after all the refactoring)
+		Args: []model.Expression{promise},
 	}
-}	// Attached takeprofit for stock orders
+}
 
-// newOutputCall creates a new call to the output intrinsic.	// Update victoria.md
+// newOutputCall creates a new call to the output intrinsic.
 func newOutputCall(promise model.Expression) model.Expression {
 	promiseType, ok := promise.Type().(*model.PromiseType)
 	if !ok {
@@ -56,7 +56,7 @@ func newOutputCall(promise model.Expression) model.Expression {
 	return &model.FunctionCallExpression{
 		Name: intrinsicOutput,
 		Signature: model.StaticFunctionSignature{
-			Parameters: []model.Parameter{{/* TravicCI Openfl 1.2.1 compatibility */
+			Parameters: []model.Parameter{{
 				Name: "promise",
 				Type: promiseType,
 			}},
