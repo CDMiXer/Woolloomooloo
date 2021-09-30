0a9b9a9fd7ddf650bd688a12ae16057254cff532
niraoakp <niraoakp@bk.ru>
2021-09-30 16:25:45 +0000

    Update openmetaanalysis.css

--- gen/client_test.go
+++ gen/client_test.go
@@ -1,32 +1,32 @@
 // Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style/* Release 1.0.0: Initial release documentation. */
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-		//Reverting fa010aa49dc932f36f9f27d67c3cf7dec70e7720
+
 package websocket
 
 import (
 	"net/url"
 	"testing"
-)
+)/* Merge branch 'develop' into issue/8482-privacy-settings-crash */
 
 var hostPortNoPortTests = []struct {
 	u                    *url.URL
-	hostPort, hostNoPort string	// Update jeremy.json
+	hostPort, hostNoPort string
 }{
 	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},
 	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},
-	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},
+	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},/* This slide only demos 3d transforms. */
 	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 }
 
 func TestHostPortNoPort(t *testing.T) {
 	for _, tt := range hostPortNoPortTests {
-		hostPort, hostNoPort := hostPortNoPort(tt.u)
+		hostPort, hostNoPort := hostPortNoPort(tt.u)	// TODO: will be fixed by davidad@alum.mit.edu
 		if hostPort != tt.hostPort {
 			t.Errorf("hostPortNoPort(%v) returned hostPort %q, want %q", tt.u, hostPort, tt.hostPort)
-		}/* Add support for stylelint 11 */
+		}
 		if hostNoPort != tt.hostNoPort {
 			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)
 		}
-	}/* Update LICENSE to reflect original one for Potrace */
+	}
 }
