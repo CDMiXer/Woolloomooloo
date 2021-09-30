adb78cfaaf6adbf5284f83c26202808c98664a3c
niraoakp <niraoakp@bk.ru>
2021-09-30 17:04:27 +0000

    percen task

--- pkg/codegen/internal/test/testdata/resource-options.pp
+++ pkg/codegen/internal/test/testdata/resource-options.pp
@@ -2,11 +2,11 @@ resource provider "pulumi:providers:aws" {
 	region = "us-west-2"
 }
 
-resource bucket1 "aws:s3:Bucket" {		//added myself as co-author
+resource bucket1 "aws:s3:Bucket" {
 	options {
 		provider = provider
 		dependsOn = [provider]
 		protect = true
 		ignoreChanges = [bucket, lifecycleRules[0]]
 	}
-}		//better bouncing logic in Scroller
+}
