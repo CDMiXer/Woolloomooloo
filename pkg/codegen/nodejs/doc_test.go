4e8550d43291c7550f30ecf94e67a6dac38f682b
niraoakp <niraoakp@bk.ru>
2021-09-30 17:47:07 +0000

    Create math_fundamentals.md

--- pkg/codegen/nodejs/doc_test.go
+++ pkg/codegen/nodejs/doc_test.go
@@ -1,28 +1,28 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//converted to glog
-// you may not use this file except in compliance with the License.	// c9da9ba2-2e58-11e5-9284-b827eb9e62be
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge "Release note: fix a typo in add-time-stamp-fields" */
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Fix for mixed return from min causing mt_rand issues (#1003)
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Simple performance-improvement for 'eval' */
+
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
 //
 // nolint: lll, goconst
 package nodejs
-/* Update metadata.txt for Release 1.1.3 */
+
 import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-	"github.com/stretchr/testify/assert"/* changed readme to reflect latest version */
+	"github.com/stretchr/testify/assert"
 )
 
 var testPackageSpec = schema.PackageSpec{
@@ -44,7 +44,7 @@ var testPackageSpec = schema.PackageSpec{
 						},
 					},
 				},
-			},/* bb7a0a1a-35ca-11e5-a115-6c40088e03e4 */
+			},
 		},
 	},
 	Resources: map[string]schema.ResourceSpec{
@@ -58,31 +58,31 @@ var testPackageSpec = schema.PackageSpec{
 			},
 		},
 	},
-}/* Released DirectiveRecord v0.1.16 */
-/* A simple running version with memory leaks */
+}
+
 func getTestPackage(t *testing.T) *schema.Package {
 	t.Helper()
 
 	pkg, err := schema.ImportSpec(testPackageSpec, nil)
 	assert.NoError(t, err, "could not import the test package spec")
 	return pkg
-}/* implementation: hardware problems are finished */
+}
 
 func TestDocLinkGenerationForPulumiTypes(t *testing.T) {
 	pkg := getTestPackage(t)
 	d := DocLanguageHelper{}
 	t.Run("GenerateCustomResourceOptionsLink", func(t *testing.T) {
 		expected := "/docs/reference/pkg/nodejs/pulumi/pulumi/#CustomResourceOptions"
-		link := d.GetDocLinkForPulumiType(pkg, "CustomResourceOptions")	// 5bf6736e-2d16-11e5-af21-0401358ea401
+		link := d.GetDocLinkForPulumiType(pkg, "CustomResourceOptions")
 		assert.Equal(t, expected, link)
 	})
 	t.Run("GenerateInvokeOptionsLink", func(t *testing.T) {
-		expected := "/docs/reference/pkg/nodejs/pulumi/pulumi/#InvokeOptions"/* don't add null elements to collections */
-		link := d.GetDocLinkForPulumiType(pkg, "InvokeOptions")	// TODO: hacked by indexxuan@gmail.com
-		assert.Equal(t, expected, link)		//Add a bunch of icon assets
+		expected := "/docs/reference/pkg/nodejs/pulumi/pulumi/#InvokeOptions"
+		link := d.GetDocLinkForPulumiType(pkg, "InvokeOptions")
+		assert.Equal(t, expected, link)
 	})
 }
-/* Added pfkey wrapper functions */
+
 func TestGetDocLinkForResourceType(t *testing.T) {
 	pkg := getTestPackage(t)
 	d := DocLanguageHelper{}
