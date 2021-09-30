edd501e4805748a37ec26e1e434408e599f6f72e
niraoakp <niraoakp@bk.ru>
2021-09-30 19:04:32 +0000

    v2.0.0 : Fixed issue #128

--- tests/integration/duplicate_urns/step1/index.ts
+++ tests/integration/duplicate_urns/step1/index.ts
@@ -4,15 +4,15 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0		//[test-suite] tools/timeit: Add usage text and long form options.
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: hacked by denner@gmail.com
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";		//removed reference to local solr core; refs #19223
 
 // Try to create two resources with the same URN.
 const a = new Resource("a", { state: 4 });
