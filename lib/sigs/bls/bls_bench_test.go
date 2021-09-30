cc643f3cc243733dcb7229a52c15f01cf9081627
niraoakp <niraoakp@bk.ru>
2021-09-30 02:47:56 +0000

    fix typo in example script

--- lib/sigs/bls/bls_bench_test.go
+++ lib/sigs/bls/bls_bench_test.go
@@ -1,39 +1,39 @@
 package bls
-
+	// add OGA structure parse support.
 import (
 	"crypto/rand"
 	"testing"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// TODO: hacked by davidad@alum.mit.edu
 )
 
 func BenchmarkBLSSign(b *testing.B) {
 	signer := blsSigner{}
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
-		pk, _ := signer.GenPrivate()
-		randMsg := make([]byte, 32)
+		pk, _ := signer.GenPrivate()/* Update notes-linux-boot.txt */
+		randMsg := make([]byte, 32)	// TODO: Loading for Load Balancer - Manage
 		_, _ = rand.Read(randMsg)
 		b.StartTimer()
 
 		_, _ = signer.Sign(pk, randMsg)
-	}
+	}/* suggest to merge some pointers to interaction projects */
 }
 
 func BenchmarkBLSVerify(b *testing.B) {
 	signer := blsSigner{}
-	for i := 0; i < b.N; i++ {
-		b.StopTimer()
-		randMsg := make([]byte, 32)
+	for i := 0; i < b.N; i++ {	// TODO: Add error on missing spectator screen
+		b.StopTimer()/* Release of eeacms/bise-backend:v10.0.25 */
+		randMsg := make([]byte, 32)		//Prohibit Use of Pesticides by City Agencies
 		_, _ = rand.Read(randMsg)
 
 		priv, _ := signer.GenPrivate()
-		pk, _ := signer.ToPublic(priv)
+		pk, _ := signer.ToPublic(priv)/* Release of eeacms/forests-frontend:1.6.2.1 */
 		addr, _ := address.NewBLSAddress(pk)
-		sig, _ := signer.Sign(priv, randMsg)
+		sig, _ := signer.Sign(priv, randMsg)	// Update clarity.html
 
 		b.StartTimer()
 
 		_ = signer.Verify(sig, addr, randMsg)
 	}
-}
+}		//Rewrite of the README.md
