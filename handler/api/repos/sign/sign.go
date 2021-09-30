f416c25fad5a667faf229c10e5b9df5ca2fcf496
niraoakp <niraoakp@bk.ru>
2021-09-30 04:32:02 +0000

    modify code style

--- handler/api/repos/sign/sign.go
+++ handler/api/repos/sign/sign.go
@@ -1,51 +1,51 @@
 // Copyright 2019 Drone IO, Inc.
-///* Release 1.1.10 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* oper2test_topnet.sh fix for staging archive */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Beta 8.2 - Release */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: PMG-59 update readme
-// limitations under the License./* Rename user_guide.md to USER_GUIDE.md */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package sign
 
 import (
-	"encoding/json"/* Updated code to conform with code standards/style. */
+	"encoding/json"
 	"net/http"
 
 	"github.com/drone/drone-yaml/yaml/signer"
-	"github.com/drone/drone/core"	// Updated the r-readmzxmldata feedstock.
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"	// Remove dependency on ZenTest
+	"github.com/go-chi/chi"
 )
 
 type payload struct {
 	Data string `json:"data"`
 }
 
-// HandleSign returns an http.HandlerFunc that processes http		//Delete sifra.zip
+// HandleSign returns an http.HandlerFunc that processes http
 // requests to sign a pipeline configuration file.
 func HandleSign(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (/* Merge "Fix duplicated words issue like "if we are are here"" */
-			namespace = chi.URLParam(r, "owner")	// TODO: will be fixed by boringland@protonmail.ch
+		var (
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)		//refs #1512
-		repo, err := repos.FindName(r.Context(), namespace, name)/* Modules updates (Release). */
+		)
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return/* Merge "Zone ownership tests" */
+			return
 		}
 
 		in := new(payload)
-		err = json.NewDecoder(r.Body).Decode(in)/* Release version 0.17. */
-		if err != nil {	// TODO: Updated search method example
+		err = json.NewDecoder(r.Body).Decode(in)
+		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
