ce699051ab63aba3b9977301fa7ff7575d8fe0c5
niraoakp <niraoakp@bk.ru>
2021-09-30 12:13:58 +0000

    Update PrusaControl.yml

--- tests/integration/dependency_steps/dependency_steps_test.go
+++ tests/integration/dependency_steps/dependency_steps_test.go
@@ -6,22 +6,22 @@ package ints
 import (
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Allow ES6 default arguments */
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 // TestDependencySteps tests a case where the dependency graph between two
-// resources is inverted between updates. The snapshot should be robust to this		//add: Checkstyle checks.xml
+// resources is inverted between updates. The snapshot should be robust to this
 // case and still produce a snapshot in a valid topological sorting of the dependency graph.
-func TestDependencySteps(t *testing.T) {	// TODO: hacked by lexy8russo@outlook.com
+func TestDependencySteps(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
-				Dir:      "step2",	// TODO: will be fixed by timnugent@gmail.com
+				Dir:      "step2",
 				Additive: true,
-			},		//Merge branch 'master' into renovate/google-cloud-pubsub-1.x
+			},
 		},
-	})/* Merge "Release 3.2.3.456 Prima WLAN Driver" */
+	})
 }
