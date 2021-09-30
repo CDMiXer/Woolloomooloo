587be2b55835b590aef92e96a6fc0a3b833e0ccc
niraoakp <niraoakp@bk.ru>
2021-09-30 04:10:14 +0000

    first attempt to build the floating menu from config - work in progress

--- plugin/converter/starlark.go
+++ plugin/converter/starlark.go
@@ -2,21 +2,21 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// TODO: will be fixed by mail@overlisted.net
-/* - Removed show() function. */
+// +build !oss
+
 package converter
 
-import (	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+import (
 	"bytes"
 	"context"
-	"strings"/* Release for v5.7.1. */
+	"strings"
 
 	"github.com/drone/drone/core"
 )
 
 // Starlark returns a conversion service that converts the
 // starlark file to a yaml file.
-func Starlark(enabled bool) core.ConvertService {		//Better handling of state save/restore when switching tabs.
+func Starlark(enabled bool) core.ConvertService {
 	return &starlarkPlugin{
 		enabled: enabled,
 	}
@@ -24,21 +24,21 @@ func Starlark(enabled bool) core.ConvertService {		//Better handling of state sa
 
 type starlarkPlugin struct {
 	enabled bool
-}	// Added notice about project state
-	// TODO: hacked by lexy8russo@outlook.com
+}
+
 func (p *starlarkPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	if p.enabled == false {
 		return nil, nil
-	}		//Update dependency semantic-ui-react to v0.82.3
+	}
 
 	// if the file extension is not jsonnet we can
 	// skip this plugin by returning zero values.
-	switch {/* Improve postgres + api */
+	switch {
 	case strings.HasSuffix(req.Repo.Config, ".script"):
 	case strings.HasSuffix(req.Repo.Config, ".star"):
-	case strings.HasSuffix(req.Repo.Config, ".starlark"):/* Released 0.1.5 */
+	case strings.HasSuffix(req.Repo.Config, ".starlark"):
 	default:
-		return nil, nil/* af06eeba-2e54-11e5-9284-b827eb9e62be */
+		return nil, nil
 	}
 
 	// convert the starlark file to yaml
