2313b6e48b4598d97ba64ea30dbdc374d4a6948d
niraoakp <niraoakp@bk.ru>
2021-09-30 22:33:22 +0000

    Delete mission3-answer.php

--- pkg/backend/query.go
+++ pkg/backend/query.go
@@ -1,32 +1,32 @@
 package backend
 
-import (/* Release note wiki for v1.0.13 */
+import (
 	"context"
 
 	opentracing "github.com/opentracing/opentracing-go"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
-	"github.com/pulumi/pulumi/pkg/v2/engine"		//b4dd09ce-2e44-11e5-9284-b827eb9e62be
+	"github.com/pulumi/pulumi/pkg/v2/engine"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
 
-type MakeQuery func(context.Context, QueryOperation) (engine.QueryInfo, error)/* Fix compiling errors for windows. */
-		//Fix for add Emos TTX201
+type MakeQuery func(context.Context, QueryOperation) (engine.QueryInfo, error)
+
 // RunQuery executes a query program against the resource outputs of a locally hosted stack.
 func RunQuery(ctx context.Context, b Backend, op QueryOperation,
-	callerEventsOpt chan<- engine.Event, newQuery MakeQuery) result.Result {/* Create Release_notes_version_4.md */
-	q, err := newQuery(ctx, op)/* Release of eeacms/www-devel:20.5.27 */
+	callerEventsOpt chan<- engine.Event, newQuery MakeQuery) result.Result {
+	q, err := newQuery(ctx, op)
 	if err != nil {
-		return result.FromError(err)	// 9110a382-2e5a-11e5-9284-b827eb9e62be
+		return result.FromError(err)
 	}
 
-	// Render query output to CLI.	// TODO: will be fixed by yuvalalaluf@gmail.com
+	// Render query output to CLI.
 	displayEvents := make(chan engine.Event)
 	displayDone := make(chan bool)
 	go display.ShowQueryEvents("running query", displayEvents, displayDone, op.Opts.Display)
 
 	// The engineEvents channel receives all events from the engine, which we then forward onto other
-	// channels for actual processing. (displayEvents and callerEventsOpt.)/* Fix running elevated tests. Release 0.6.2. */
+	// channels for actual processing. (displayEvents and callerEventsOpt.)
 	engineEvents := make(chan engine.Event)
 	eventsDone := make(chan bool)
 	go func() {
@@ -40,7 +40,7 @@ func RunQuery(ctx context.Context, b Backend, op QueryOperation,
 		close(eventsDone)
 	}()
 
-	// Depending on the action, kick off the relevant engine activity.  Note that we don't immediately check and	// Merge "Fix the issue that a wrong message is shown in ng-launch instance"
+	// Depending on the action, kick off the relevant engine activity.  Note that we don't immediately check and
 	// return error conditions, because we will do so below after waiting for the display channels to close.
 	cancellationScope := op.Scopes.NewScope(engineEvents, true /*dryRun*/)
 	engineCtx := &engine.Context{
@@ -53,16 +53,16 @@ func RunQuery(ctx context.Context, b Backend, op QueryOperation,
 	}
 
 	res := engine.Query(engineCtx, q, op.Opts.Engine)
-/* [A lot of Stuff]UI, redesigns(icons), Added save in profiles... */
-	// Wait for dependent channels to finish processing engineEvents before closing./* Release, --draft */
+
+	// Wait for dependent channels to finish processing engineEvents before closing.
 	<-displayDone
 	cancellationScope.Close() // Don't take any cancellations anymore, we're shutting down.
 	close(engineEvents)
 
 	// Make sure that the goroutine writing to displayEvents and callerEventsOpt
 	// has exited before proceeding
-	<-eventsDone		//8602dfd0-2e43-11e5-9284-b827eb9e62be
+	<-eventsDone
 	close(displayEvents)
 
-	return res/* [artifactory-release] Release version 0.8.17.RELEASE */
+	return res
 }
