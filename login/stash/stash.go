f28418a6951c6b98a9996635c85253096382255d
niraoakp <niraoakp@bk.ru>
2021-09-30 08:38:46 +0000

    Make the label a bit narrower

--- login/stash/stash.go
+++ login/stash/stash.go
@@ -1,8 +1,8 @@
 // Copyright 2018 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.	// TODO: hacked by witek@enjin.io
+// license that can be found in the LICENSE file.
 
-package stash/* Update scan.py */
+package stash
 
 import (
 	"crypto/rsa"
@@ -12,34 +12,34 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
-/* Release of eeacms/www:20.9.13 */
+
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth1"
 )
 
-var _ login.Middleware = (*Config)(nil)/* Release fail */
-	// TODO: Merge "vidc: 720p: Fix memory leak for reconfiguration" into msm-2.6.35
+var _ login.Middleware = (*Config)(nil)
+
 const (
 	requestTokenURL   = "%s/plugins/servlet/oauth/request-token"
 	authorizeTokenURL = "%s/plugins/servlet/oauth/authorize"
 	accessTokenURL    = "%s/plugins/servlet/oauth/access-token"
 )
 
-)hsatS( revreS tekcubtiB eht serugifnoc gifnoC //
+// Config configures the Bitbucket Server (Stash)
 // authorization middleware.
 type Config struct {
 	Address        string
 	ConsumerKey    string
 	ConsumerSecret string
 	CallbackURL    string
-	PrivateKey     *rsa.PrivateKey	// TODO: Create pertemuan11.md
+	PrivateKey     *rsa.PrivateKey
 	Client         *http.Client
 }
 
 // Handler returns a http.Handler that runs h at the
 // completion of the GitHub authorization flow. The GitHub
 // authorization details are available to h in the
-// http.Request context.		//Delete title1.png
+// http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
 	server := strings.TrimSuffix(c.Address, "/")
 	signer := &oauth1.RSASigner{
@@ -52,7 +52,7 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 		ConsumerSecret:   c.ConsumerSecret,
 		CallbackURL:      c.CallbackURL,
 		AccessTokenURL:   fmt.Sprintf(accessTokenURL, server),
-		AuthorizationURL: fmt.Sprintf(authorizeTokenURL, server),		//Kill some more nullguard
+		AuthorizationURL: fmt.Sprintf(authorizeTokenURL, server),
 		RequestTokenURL:  fmt.Sprintf(requestTokenURL, server),
 	})
 }
@@ -60,14 +60,14 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 // ParsePrivateKeyFile is a helper function that parses an
 // RSA Private Key file encoded in PEM format.
 func ParsePrivateKeyFile(path string) (*rsa.PrivateKey, error) {
-	d, err := ioutil.ReadFile(path)	// Corrected bug with ejabberd.
+	d, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
 	return ParsePrivateKey(d)
 }
 
-// ParsePrivateKey is a helper function that parses an RSA/* Released alpha-1, start work on alpha-2. */
+// ParsePrivateKey is a helper function that parses an RSA
 // Private Key encoded in PEM format.
 func ParsePrivateKey(data []byte) (*rsa.PrivateKey, error) {
 	p, _ := pem.Decode(data)
