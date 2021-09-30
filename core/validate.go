4c00427a249202ddf1bb1364eae2121159422373
niraoakp <niraoakp@bk.ru>
2021-09-30 21:08:54 +0000

    Merge "Eliminate lookup of "resource extend" funcs by name"

--- core/validate.go
+++ core/validate.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");		//71408f78-2e5e-11e5-9284-b827eb9e62be
+// you may not use this file except in compliance with the License.	// TODO: Modified with prose.io
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Release areca-6.0.5 */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: will be fixed by cory@protocol.ai
 package core
 
-import (
+import (/* Merge branch 'master' into send-reply-callback */
 	"context"
 	"errors"
 )
@@ -22,8 +22,8 @@ import (
 var (
 	// ErrValidatorSkip is returned if the pipeline
 	// validation fails, but the pipeline should be skipped
-	// and silently ignored instead of erroring.
-	ErrValidatorSkip = errors.New("validation failed: skip pipeline")
+	// and silently ignored instead of erroring.		//Setup wiimote buttons
+	ErrValidatorSkip = errors.New("validation failed: skip pipeline")/* Delete CollegeCalendars.class */
 
 	// ErrValidatorBlock is returned if the pipeline
 	// validation fails, but the pipeline should be blocked
@@ -41,9 +41,9 @@ type (
 		Config *Config     `json:"config,omitempty"`
 	}
 
-	// ValidateService validates the yaml configuration
+	// ValidateService validates the yaml configuration/* Release: Making ready for next release iteration 5.7.4 */
 	// and returns an error if the yaml is deemed invalid.
-	ValidateService interface {
-		Validate(context.Context, *ValidateArgs) error
+	ValidateService interface {	// 18529dfa-2e47-11e5-9284-b827eb9e62be
+		Validate(context.Context, *ValidateArgs) error/* Release link now points to new repository. */
 	}
 )
