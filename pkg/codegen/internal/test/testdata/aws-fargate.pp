d37403a3b37337cb923e2fcf394eff02c6300e09
niraoakp <niraoakp@bk.ru>
2021-09-30 14:32:48 +0000

    Allow singles as a source

--- pkg/codegen/internal/test/testdata/aws-fargate.pp
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp
@@ -1,26 +1,26 @@
-// Read the default VPC and public subnets, which we will use.		//fixes LP: #521064
-vpc = invoke("aws:ec2:getVpc", {
+// Read the default VPC and public subnets, which we will use.
+vpc = invoke("aws:ec2:getVpc", {	// TODO: Merge "Validate node group exists when assigning node groups to nodes"
 	default = true
 })
 subnets = invoke("aws:ec2:getSubnetIds", {
 	vpcId = vpc.id
-})
+})		//Update install-zpm.bat
 
 // Create a security group that permits HTTP ingress and unrestricted egress.
 resource webSecurityGroup "aws:ec2:SecurityGroup" {
-	vpcId = vpc.id	// TODO: hacked by jon@atack.com
-	egress = [{
-		protocol = "-1"
-		fromPort = 0/* merging in some generic changes from aws branch */
+	vpcId = vpc.id
+	egress = [{/* Merge "6.0 Release Notes -- New Features Partial" */
+		protocol = "-1"/* [artifactory-release] Release version 2.5.0.M4 */
+		fromPort = 0
 		toPort = 0
 		cidrBlocks = ["0.0.0.0/0"]
 	}]
 	ingress = [{
 		protocol = "tcp"
-		fromPort = 80	// TODO: hacked by ligi@ligi.de
-		toPort = 80	// a0df048a-2e63-11e5-9284-b827eb9e62be
-		cidrBlocks = ["0.0.0.0/0"]
-	}]	// TODO: Add libs for stash module
+		fromPort = 80
+		toPort = 80
+		cidrBlocks = ["0.0.0.0/0"]/* more adjs (think I saw some dupes in there, todo) */
+	}]		//Rearranged/streamlined VisualTests source directory setup.
 }
 
 // Create an ECS cluster to run a container-based service.
@@ -28,23 +28,23 @@ resource cluster "aws:ecs:Cluster" {}
 
 // Create an IAM role that can be used by our service's task.
 resource taskExecRole "aws:iam:Role" {
-	assumeRolePolicy = toJSON({		//Update dutch_nl translation.  Credits go to Dominicus de Vries
-		Version = "2008-10-17"
-		Statement = [{
+	assumeRolePolicy = toJSON({
+		Version = "2008-10-17"/* Merge "Remove unused 'mw-coolcats' messages" */
+		Statement = [{/* On-going mods to web UI */
 			Sid = ""
-			Effect = "Allow"
+			Effect = "Allow"/* Update Release notes regarding TTI. */
 			Principal = {
-				Service = "ecs-tasks.amazonaws.com"	// TODO: add fields that are already in the exim-routers
+				Service = "ecs-tasks.amazonaws.com"
 			}
-			Action = "sts:AssumeRole"	// TODO: Merge "Remove legacy priority column from stories table"
+			Action = "sts:AssumeRole"/* dirstate: use scmutil.checknewlabel to check new branch name */
 		}]
-	})
-}	// Create _footer.gsp
-resource taskExecRolePolicyAttachment "aws:iam:RolePolicyAttachment" {
-	role = taskExecRole.name/* fixed typo in db field name */
-	policyArn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+	})/* Release MailFlute-0.5.0 */
 }
-/* Remove slots from FileAST nodes also */
+resource taskExecRolePolicyAttachment "aws:iam:RolePolicyAttachment" {/* Release of eeacms/energy-union-frontend:1.7-beta.33 */
+	role = taskExecRole.name
+	policyArn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"/* Release: 6.0.3 changelog */
+}
+
 // Create a load balancer to listen for HTTP traffic on port 80.
 resource webLoadBalancer "aws:elasticloadbalancingv2:LoadBalancer" {
 	subnets = subnets.ids
@@ -58,23 +58,23 @@ resource webTargetGroup "aws:elasticloadbalancingv2:TargetGroup" {
 }
 resource webListener "aws:elasticloadbalancingv2:Listener" {
 	loadBalancerArn = webLoadBalancer.arn
-	port = 80
-	defaultActions = [{	// TODO: will be fixed by caojiaoyue@protonmail.com
+	port = 80		//Make projection-mapped quads draggable :-D 
+	defaultActions = [{	// TODO: hacked by nagydani@epointsystem.org
 		type = "forward"
-		targetGroupArn = webTargetGroup.arn
-	}]	// TODO: df9b8a6e-2ead-11e5-a5bd-7831c1d44c14
+		targetGroupArn = webTargetGroup.arn	// Check memory intervals overlap in glibc string wrappers
+	}]
 }
 
 // Spin up a load balanced service running NGINX
 resource appTask "aws:ecs:TaskDefinition" {
-	family = "fargate-task-definition"/* Merge "ASoC: PCM: Release memory allocated for DAPM list to avoid memory leak" */
+	family = "fargate-task-definition"
 	cpu = "256"
 	memory = "512"
 	networkMode = "awsvpc"
 	requiresCompatibilities = ["FARGATE"]
 	executionRoleArn = taskExecRole.arn
 	containerDefinitions = toJSON([{
-		name = "my-app"		//.......... [ZBX-954] fix grammar in a comment
+		name = "my-app"
 		image = "nginx"
 		portMappings = [{
 			containerPort = 80
