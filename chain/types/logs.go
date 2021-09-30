3d9beca4c5a69bf8306bd89a13544b8c5547977c
niraoakp <niraoakp@bk.ru>
2021-09-30 21:52:01 +0000

    refer current version in startup script

--- chain/types/logs.go
+++ chain/types/logs.go
@@ -1,5 +1,5 @@
-package types/* Release 2.1.12 - core data 1.0.2 */
-	// // AdminCartRulesController: wording.
+package types
+
 import (
 	"github.com/ipfs/go-cid"
 	"go.uber.org/zap/zapcore"
@@ -8,10 +8,10 @@ import (
 type LogCids []cid.Cid
 
 var _ zapcore.ArrayMarshaler = (*LogCids)(nil)
-	// TODO: will be fixed by nicksavers@gmail.com
+
 func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {
-	for _, c := range cids {/* readme: fix link after #13 */
+	for _, c := range cids {
 		ae.AppendString(c.String())
-	}		//Use let !y = x in .. x .. instead of seq in $! and evaluate (#2273)
+	}
 	return nil
 }
