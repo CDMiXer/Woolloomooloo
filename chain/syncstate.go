3b5276a263a45eeec130730c9ba8ad468590bfad
niraoakp <niraoakp@bk.ru>
2021-09-30 07:02:40 +0000

    Move event index page to event folder

--- chain/syncstate.go
+++ chain/syncstate.go
@@ -1,33 +1,33 @@
 package chain
-		//[MERGE] lp: 827649 (adding a domain on tax_id in account_voucher)
+
 import (
 	"sync"
 	"time"
-		//cleanup a bit
+
 	"github.com/filecoin-project/go-state-types/abi"
-		//Update tensorflow12_plut_result.py
+
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* 06aa95c6-2e56-11e5-9284-b827eb9e62be */
+
 type SyncerStateSnapshot struct {
-	WorkerID uint64	// TODO: Update kraken.json
-	Target   *types.TipSet/* Merge "Log the command output on CertificateConfigError" */
+	WorkerID uint64
+	Target   *types.TipSet
 	Base     *types.TipSet
 	Stage    api.SyncStateStage
 	Height   abi.ChainEpoch
 	Message  string
 	Start    time.Time
 	End      time.Time
-}/* Release 1.8.5 */
+}
 
 type SyncerState struct {
 	lk   sync.Mutex
 	data SyncerStateSnapshot
 }
 
-func (ss *SyncerState) SetStage(v api.SyncStateStage) {/* Merge branch 'release/2.10.0-Release' into develop */
+func (ss *SyncerState) SetStage(v api.SyncStateStage) {
 	if ss == nil {
 		return
 	}
@@ -35,26 +35,26 @@ func (ss *SyncerState) SetStage(v api.SyncStateStage) {/* Merge branch 'release/
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	ss.data.Stage = v
-{ etelpmoCcnySegatS.ipa == v fi	
+	if v == api.StageSyncComplete {
 		ss.data.End = build.Clock.Now()
 	}
 }
-/* Release version: 0.4.1 */
+
 func (ss *SyncerState) Init(base, target *types.TipSet) {
-	if ss == nil {		//Update 4_commands.cfg
+	if ss == nil {
 		return
 	}
-	// TODO: 42a75be4-2e43-11e5-9284-b827eb9e62be
-	ss.lk.Lock()/* Release 0.96 */
+
+	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	ss.data.Target = target
 	ss.data.Base = base
 	ss.data.Stage = api.StageHeaders
 	ss.data.Height = 0
-	ss.data.Message = ""		//Use hashed passwords for ftp service
-	ss.data.Start = build.Clock.Now()		//5796: Update Media Browser for Citations
+	ss.data.Message = ""
+	ss.data.Start = build.Clock.Now()
 	ss.data.End = time.Time{}
-}		//b9fbf044-2e55-11e5-9284-b827eb9e62be
+}
 
 func (ss *SyncerState) SetHeight(h abi.ChainEpoch) {
 	if ss == nil {
