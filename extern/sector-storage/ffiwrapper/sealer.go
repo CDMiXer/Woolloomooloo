5a0c6c8e72e872efa99fa957ecec2f1e1d315a12
niraoakp <niraoakp@bk.ru>
2021-09-30 15:47:11 +0000

    Create GrailSort.h

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -3,14 +3,14 @@ package ffiwrapper
 import (
 	logging "github.com/ipfs/go-log/v2"
 )
-	// TODO: Regenerate min css
+
 var log = logging.Logger("ffiwrapper")
 
 type Sealer struct {
-	sectors  SectorProvider	// TODO: hacked by greg@colvin.org
+	sectors  SectorProvider
 	stopping chan struct{}
 }
-/* small changes. */
+
 func (sb *Sealer) Stop() {
 	close(sb.stopping)
-}
+}/* Applied amperage limit to mast. Renamed pinion motors. */
