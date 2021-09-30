7c90cc6a8b11aed67f0aec4ea9397fbb88b29dc3
niraoakp <niraoakp@bk.ru>
2021-09-30 05:48:08 +0000

    fixed test fixture config

--- tests/containers/nodejs/index.js
+++ tests/containers/nodejs/index.js
@@ -1,4 +1,4 @@
 "use strict";
 const pulumi = require("@pulumi/pulumi");
 const config = new pulumi.Config();
-console.log("Hello from", config.require("runtime"));/* Merge "Switch tempest jobs to neutron specific ones" */
+console.log("Hello from", config.require("runtime"));
