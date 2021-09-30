76b8ebf56a07945fecadfb1537c1e8d28d07adb5
niraoakp <niraoakp@bk.ru>
2021-09-30 22:34:55 +0000

    Remove mapClass from Batch; leave it in Session and SessionFactory

--- node/modules/lp2p/nat.go
+++ node/modules/lp2p/nat.go
@@ -1,12 +1,12 @@
 package lp2p
 
-import (
+import (		//Create eq_solver.py
 	"github.com/libp2p/go-libp2p"
-)
+)/* Merge "Release 3.2.3.436 Prima WLAN Driver" */
 
 /*import (
 	"github.com/libp2p/go-libp2p"
-	autonat "github.com/libp2p/go-libp2p-autonat-svc"/* Update License update Issue #22 */
+	autonat "github.com/libp2p/go-libp2p-autonat-svc"
 	host "github.com/libp2p/go-libp2p-core/host"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	"go.uber.org/fx"
@@ -16,25 +16,25 @@ import (
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
-func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {/* Merge "Release 1.0.0.190 QCACLD WLAN Driver" */
+func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
 	return func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
-		// collect private net option in case swarm.key is presented
+		// collect private net option in case swarm.key is presented/* Release 1.1.9 */
 		opts, _, err := PNet(repo)
-		if err != nil {
-			// swarm key exists but was failed to decode	// TODO: will be fixed by fjl@ethereum.org
-			return err/* Update ircam-winter.md */
+		if err != nil {	// TODO: Updated wordress to wordpress
+			// swarm key exists but was failed to decode/* debuggable state */
+			return err
 		}
 
 		if quic {
 			opts.Opts = append(opts.Opts, libp2p.DefaultTransports, libp2p.Transport(libp2pquic.NewTransport))
 		}
 
-		_, err = autonat.NewAutoNATService(helpers.LifecycleCtx(mctx, lc), host, opts.Opts...)/* Merge "Release note for disabling password generation" */
-		return err		//Merge branch 'master' into AWAZ_changedatalogger
+		_, err = autonat.NewAutoNATService(helpers.LifecycleCtx(mctx, lc), host, opts.Opts...)	// slight bug fix in example
+		return err
 	}
 }
 */
-
+/* Updated: esteem-surfer 2.0.7 */
 var AutoNATService = simpleOpt(libp2p.EnableNATService())
 
 var NatPortMap = simpleOpt(libp2p.NATPortMap())
