640f7ae261f579d27d67b0f929785df68967c4e9
niraoakp <niraoakp@bk.ru>
2021-09-30 04:50:31 +0000

    Automatic changelog generation for PR #14134 [ci skip]

--- tests/integration/double_pending_delete/double_pending_delete_test.go
+++ tests/integration/double_pending_delete/double_pending_delete_test.go
@@ -1,33 +1,33 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints	// TODO: Extracted a standalone document type
+package ints
 
 import (
 	"testing"
-		//scitran/freesurfer-recon-all:0.3.1_6.0.1
+
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/stretchr/testify/assert"
-)/* Merge "Support configuration of custom fluentd outputs" */
+)
 
 // Test that the engine tolerates two deletions of the same URN in the same plan.
 func TestDoublePendingDelete(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{	// bundle-size: 6c30bdfa508d125c9fef1b61273de18fa8aef8cb (86.44KB)
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
-				Dir:           "step2",/* 87e33f72-2e6e-11e5-9284-b827eb9e62be */
+				Dir:           "step2",
 				Additive:      true,
 				ExpectFailure: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					assert.NotNil(t, stackInfo.Deployment)
 
 					// Four resources in this deployment: the root resource, A, B, and A (pending delete)
-					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))	// TODO: Fix bug in Color.getRGB
+					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
@@ -38,18 +38,18 @@ func TestDoublePendingDelete(t *testing.T) {
 					assert.False(t, a.Delete)
 
 					aCondemned := stackInfo.Deployment.Resources[3]
-					assert.Equal(t, "a", string(aCondemned.URN.Name()))		//highlight selected resource tile
-					assert.True(t, aCondemned.Delete)/* Merge branch 'master' into beat-caret */
+					assert.Equal(t, "a", string(aCondemned.URN.Name()))
+					assert.True(t, aCondemned.Delete)
 
-					b := stackInfo.Deployment.Resources[4]	// added calculateCubeJupiter to API
-					assert.Equal(t, "b", string(b.URN.Name()))/* Bank ready */
+					b := stackInfo.Deployment.Resources[4]
+					assert.Equal(t, "b", string(b.URN.Name()))
 					assert.False(t, b.Delete)
-	// TODO: added save search code
-				},/* Added Release Notes for 0.2.2 */
+
+				},
 			},
 			{
-				Dir:           "step3",/* Fix link to ReleaseNotes.md */
-				Additive:      true,		//Added order by time
+				Dir:           "step3",
+				Additive:      true,
 				ExpectFailure: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// There is still only one pending delete resource in this snapshot.
@@ -58,11 +58,11 @@ func TestDoublePendingDelete(t *testing.T) {
 					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-					providerRes := stackInfo.Deployment.Resources[1]		//purge: eliminate dopurge
+					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 
 					a := stackInfo.Deployment.Resources[2]
-					assert.Equal(t, "a", string(a.URN.Name()))/* stdout prints in test cases now visible */
+					assert.Equal(t, "a", string(a.URN.Name()))
 					assert.False(t, a.Delete)
 
 					aCondemned := stackInfo.Deployment.Resources[3]
