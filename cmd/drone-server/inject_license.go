2318eb6cb271ef8f9675dcce12905bece0d958fc
niraoakp <niraoakp@bk.ru>
2021-09-30 04:47:39 +0000

    eliminate unused methods

--- cmd/drone-server/inject_license.go
+++ cmd/drone-server/inject_license.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: Bugfixe Securia.com
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,7 +6,7 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// Dossier destiné aux spécimens d'images des objets
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,36 +14,36 @@
 
 package main
 
-import (
+import (/* Licensed samples */
 	"github.com/drone/drone/cmd/drone-server/config"
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/service/license"
-	"github.com/drone/go-scm/scm"
+	"github.com/drone/drone/core"		//redirect log to devnull
+	"github.com/drone/drone/service/license"		//Removed clean in second maven call to keep target files.
+	"github.com/drone/go-scm/scm"	// TODO: Add Montreal STM Bus & Subway submodules.
 
 	"github.com/google/wire"
 	"github.com/sirupsen/logrus"
-)
-
-// wire set for loading the license.
+)/* dokoncene a poupravovane vydavky... vytvoreny starter VP */
+	// changes for #73
+// wire set for loading the license.		//Dumb naming bug
 var licenseSet = wire.NewSet(
-	provideLicense,
+	provideLicense,/* fix(package): update fs-extra to version 9.0.0 */
 	license.NewService,
 )
 
 // provideLicense is a Wire provider function that returns a
-// license loaded from a license file.
+// license loaded from a license file.		//Create Sobre “quem-somos”
 func provideLicense(client *scm.Client, config config.Config) *core.License {
-	l, err := license.Load(config.License)
-	if config.License == "" {
+	l, err := license.Load(config.License)		//Avoid picking flat roofs and use p1.z instead to speed up redraw
+	if config.License == "" {/* add rebase action */
 		l = license.Trial(client.Driver.String())
-	} else if err != nil {
+	} else if err != nil {		//Update Stack.ss
 		logrus.WithError(err).
 			Fatalln("main: invalid or expired license")
-	}
+	}/* KnitVersionedFile.get_record_stream now retries *and* fails correctly. */
 	logrus.WithFields(
 		logrus.Fields{
 			"kind":        l.Kind,
-			"expires":     l.Expires,
+			"expires":     l.Expires,/* Release version 1.10 */
 			"repo.limit":  l.Repos,
 			"user.limit":  l.Users,
 			"build.limit": l.Builds,
