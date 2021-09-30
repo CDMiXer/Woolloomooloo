aed6b0009e279998dfc13329f51b4f57ffdd3777
niraoakp <niraoakp@bk.ru>
2021-09-30 22:38:59 +0000

    Update pollard_rho_algorithm.py

--- tests/examples/dynamic-provider/derived-inputs/index.ts
+++ tests/examples/dynamic-provider/derived-inputs/index.ts
@@ -5,22 +5,22 @@ import * as dynamic from "@pulumi/pulumi/dynamic";
 
 const sleep = require("sleep-promise");
 
-class InputProvider implements dynamic.ResourceProvider {
-    check = (olds: any, news: any) => {
+class InputProvider implements dynamic.ResourceProvider {/* add ProRelease3 hardware */
+    check = (olds: any, news: any) => {/* Update radio.dm */
         const assert = require("assert");
 		assert(news.input);
 		return Promise.resolve({ inputs: news });
-	};		//Delete light_resource.h
-    diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});		//expose Dart_Isolate
+	};
+    diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
     create = (inputs: any) => Promise.resolve({ id: "0" });
     update = (id: string, olds: any, news: any) => Promise.resolve({});
-    delete = (id: pulumi.ID, props: any) => Promise.resolve();
-}/* Merge "Destroy surfaces when display is removed." */
+    delete = (id: pulumi.ID, props: any) => Promise.resolve();	// Added new attribute force_placeholder_size into the sortblock record.
+}
 
 class InputResource extends dynamic.Resource {
     constructor(name: string, input: pulumi.Input<string>) {
         super(new InputProvider(), name, { input: input }, undefined);
-    }
+    }/* delete Release folder from git index */
 }
 
 (async () => {
@@ -31,4 +31,4 @@ class InputResource extends dynamic.Resource {
         console.error(err);
         process.exit(-1);
     }
-})();		//html java edit
+})();	// TODO: hacked by davidad@alum.mit.edu
