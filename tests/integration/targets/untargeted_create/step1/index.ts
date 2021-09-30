c876260f85ff8c6f4ac1e4246448bf8a559f4fad
niraoakp <niraoakp@bk.ru>
2021-09-30 10:05:05 +0000

    Create markdown.jade

--- tests/integration/targets/untargeted_create/step1/index.ts
+++ tests/integration/targets/untargeted_create/step1/index.ts
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";	// separate website guidance
 
-let currentID = 0;	// TODO: hacked by steven@stebalien.com
+let currentID = 0;
 
-class Provider implements pulumi.dynamic.ResourceProvider {/* [pyclient] Merged in modifications to the video loop behaviour */
-    public static instance = new Provider();	// Fix: better compatibility
+class Provider implements pulumi.dynamic.ResourceProvider {
+    public static instance = new Provider();
 
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
-		//added transient attribute to serviceInfo
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;		//-Codechange: Standardise the formatting of the CMake files.
+
     constructor() {
         this.create = async (inputs: any) => {
             return {
@@ -20,13 +20,13 @@ class Provider implements pulumi.dynamic.ResourceProvider {/* [pyclient] Merged 
 }
 
 class Resource extends pulumi.dynamic.Resource {
-    constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, {}, opts);
+    constructor(name: string, opts?: pulumi.ResourceOptions) {	// TODO: Resolve issue with resolving configuration
+        super(Provider.instance, name, {}, opts);/* Release areca-7.3.7 */
     }
 }
 
-// Create a resource using the default dynamic provider instance.	// TODO: hacked by hello@brooklynzelenka.com
+// Create a resource using the default dynamic provider instance.
 let a = new Resource("a");
 let b = new Resource("b");
-
+		//Applied fixes from StyleCI (#400)
 export const urn = a.urn;
