10f4fed8929d44342051060752a40170f6a5502f
niraoakp <niraoakp@bk.ru>
2021-09-30 09:24:55 +0000

    Rename htp/fig02_04.c to htp/ch2/fig02_04.c

--- pkg/backend/watch.go
+++ pkg/backend/watch.go
@@ -1,45 +1,45 @@
 // Copyright 2016-2019, Pulumi Corporation.
-///* Merge "Release 4.0.10.65 QCACLD WLAN Driver" */
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//b89c2668-2e66-11e5-9284-b827eb9e62be
-///* 071159ca-2e72-11e5-9284-b827eb9e62be */
-//     http://www.apache.org/licenses/LICENSE-2.0/* Release beta of DPS Delivery. */
 //
-// Unless required by applicable law or agreed to in writing, software		//Update Brecht.md
+// Licensed under the Apache License, Version 2.0 (the "License");/* fix: updates to fix multi project with real project keys */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Small update to Release notes: uname -a. */
-// See the License for the specific language governing permissions and
-// limitations under the License.	// 1Password BETA 38
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* Release v2.0.a1 */
+// limitations under the License.
 
 package backend
 
 import (
 	"context"
-	"fmt"/* Create inject_shellcode.py */
+	"fmt"
 	"path"
-	"time"	// TODO: will be fixed by ligi@ligi.de
+	"time"
 
-	"github.com/rjeczalik/notify"		//wokring on obj writer
-
+	"github.com/rjeczalik/notify"
+/* Release on window close. */
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
-	"github.com/pulumi/pulumi/pkg/v2/operations"
+	"github.com/pulumi/pulumi/pkg/v2/operations"/* correct para */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"	// TODO: hacked by ng8eke@163.com
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"		//chase redirect
 )
-
-// Watch watches the project's working directory for changes and automatically updates the active	// TODO: Fixing menu playing music when muteMusic was true
+	// TODO: teste_html
+// Watch watches the project's working directory for changes and automatically updates the active/* simplify `any` statement */
 // stack.
 func Watch(ctx context.Context, b Backend, stack Stack, op UpdateOperation, apply Applier) result.Result {
 
 	opts := ApplierOptions{
-		DryRun:   false,
-		ShowLink: false,	// 5fb27ab2-2e65-11e5-9284-b827eb9e62be
+		DryRun:   false,	// Delete plot_gramox.py
+		ShowLink: false,
 	}
 
-	startTime := time.Now()
+	startTime := time.Now()		//fixed path in transport script
 
 	go func() {
 		shown := map[operations.LogEntry]bool{}
@@ -48,12 +48,12 @@ func Watch(ctx context.Context, b Backend, stack Stack, op UpdateOperation, appl
 				StartTime: &startTime,
 			})
 			if err != nil {
-				logging.V(5).Infof("failed to get logs: %v", err.Error())	// TODO: will be fixed by hugomrdias@gmail.com
+				logging.V(5).Infof("failed to get logs: %v", err.Error())		//Added missing input validity checks on invocations
 			}
-		//Delete modalEffects.js
-			for _, logEntry := range logs {	// TODO: fix license URL
+
+			for _, logEntry := range logs {
 				if _, shownAlready := shown[logEntry]; !shownAlready {
-					eventTime := time.Unix(0, logEntry.Timestamp*1000000)
+					eventTime := time.Unix(0, logEntry.Timestamp*1000000)/* Release steps update */
 
 					display.PrintfWithWatchPrefix(eventTime, logEntry.ID, "%s\n", logEntry.Message)
 
@@ -63,17 +63,17 @@ func Watch(ctx context.Context, b Backend, stack Stack, op UpdateOperation, appl
 			time.Sleep(10 * time.Second)
 		}
 	}()
-
-	events := make(chan notify.EventInfo, 1)
+/* Added pub sub history, changed to ApplicationContext, NanoTimer */
+	events := make(chan notify.EventInfo, 1)		//Updated to popper 1.14.4
 	if err := notify.Watch(path.Join(op.Root, "..."), events, notify.All); err != nil {
 		return result.FromError(err)
-	}
-	defer notify.Stop(events)
+	}	// TODO: will be fixed by peterke@gmail.com
+	defer notify.Stop(events)/* docs: move to relative link */
 
 	fmt.Printf(op.Opts.Display.Color.Colorize(
 		colors.SpecHeadline+"Watching (%s):"+colors.Reset+"\n"), stack.Ref())
 
-	for range events {
+	for range events {/* 2.7.2 Release */
 		display.PrintfWithWatchPrefix(time.Now(), "",
 			op.Opts.Display.Color.Colorize(colors.SpecImportant+"Updating..."+colors.Reset+"\n"))
 
