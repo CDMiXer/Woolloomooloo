f98d674badb4727c8f8505f7b1f094b8671577ba
niraoakp <niraoakp@bk.ru>
2021-09-30 10:25:39 +0000

    Fixes/tweaks to What's New.

--- tests/integration/query/step1/resource.ts
+++ tests/integration/query/step1/resource.ts
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Update add-team-members.md
+
 import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
-/* Add transports to FAQ */
+
     public async create(inputs: any) {
         return {
             id: (currentID++).toString(),
@@ -14,13 +14,13 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
         };
     }
 }
-/* 49c5ba26-2e41-11e5-9284-b827eb9e62be */
+
 export class Resource extends pulumi.dynamic.Resource {
-    public isInstance(o: any): o is Resource {		//Update spymemcached to 2.10.3
+    public isInstance(o: any): o is Resource {
         return o.__pulumiType === "pulumi-nodejs:dynamic:Resource";
     }
 
-    constructor(name: string, props: pulumi.Inputs, opts?: pulumi.ResourceOptions) {		//rev 496140
-        super(Provider.instance, name, props, opts);	// TODO: will be fixed by admin@multicoin.co
-    }		//Add usage to readme
+    constructor(name: string, props: pulumi.Inputs, opts?: pulumi.ResourceOptions) {
+        super(Provider.instance, name, props, opts);
+    }
 }
