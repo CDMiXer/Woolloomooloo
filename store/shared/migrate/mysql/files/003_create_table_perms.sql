dd7fb92dd54e47fb9700e9e9b64b60180a34f8f8
niraoakp <niraoakp@bk.ru>
2021-09-30 18:00:22 +0000

    Merge branch 'network-september-release' into hostedWorkloads_PS

--- store/shared/migrate/mysql/files/003_create_table_perms.sql
+++ store/shared/migrate/mysql/files/003_create_table_perms.sql
@@ -1,5 +1,5 @@
--- name: create-table-perms		//Create Data.text
-/* Made stained glass display properly. */
+-- name: create-table-perms
+
 CREATE TABLE IF NOT EXISTS perms (
  perm_user_id  INTEGER
 ,perm_repo_uid VARCHAR(250)
@@ -9,15 +9,15 @@ CREATE TABLE IF NOT EXISTS perms (
 ,perm_synced   INTEGER
 ,perm_created  INTEGER
 ,perm_updated  INTEGER
-,PRIMARY KEY(perm_user_id, perm_repo_uid)/* Release new version 2.5.3: Include stack trace in logs */
+,PRIMARY KEY(perm_user_id, perm_repo_uid)
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
---,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE		//BUG#18449222: Timeout waiting for local DIH caused crash, removed ndbrequire
-);	// TODO: Added default doxygen header for hipd/pisa.*
+--,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+);
 
 -- name: create-index-perms-user
 
 CREATE INDEX ix_perms_user ON perms (perm_user_id);
 
--- name: create-index-perms-repo	// added a readme for adaptive multimedia streaming
+-- name: create-index-perms-repo
 
 CREATE INDEX ix_perms_repo ON perms (perm_repo_uid);
