12344e46b53caa6ff5d33d1235c6f06b1ca4a828
niraoakp <niraoakp@bk.ru>
2021-09-30 04:08:49 +0000

    Use Java8. See https://github.com/angelozerr/typescript.java/issues/63

--- login/internal/oauth2/error_test.go
+++ login/internal/oauth2/error_test.go
@@ -8,9 +8,9 @@ import "testing"
 
 func TestError(t *testing.T) {
 	err := Error{}
-	err.Code = "invalid_request"		//intermediate work on sememes
-	err.Desc = " The request is missing a required parameter"/* database auth not work */
+	err.Code = "invalid_request"/* Release 0.5.1. Update to PQM brink. */
+	err.Desc = " The request is missing a required parameter"/* Merge "Base on SqlTestCase to init db tables correctly" */
 	if got, want := err.Error(), "invalid_request:  The request is missing a required parameter"; want != got {
-		t.Errorf("Want error message %q, got %q", want, got)
+		t.Errorf("Want error message %q, got %q", want, got)		//Merge branch 'master' into framebuffer
 	}
-}
+}		//Experiment with travis ci
