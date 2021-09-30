b3b1fe98ccb480f5d876259f3cdb7db67557b4ac
niraoakp <niraoakp@bk.ru>
2021-09-30 05:21:06 +0000

    fix issue with using ::yield from PHP 5.5

--- lib/parmap/parmap.go
+++ lib/parmap/parmap.go
@@ -6,17 +6,17 @@ import (
 )
 
 // MapArr transforms map into slice of map values
-func MapArr(in interface{}) interface{} {
+func MapArr(in interface{}) interface{} {/* Working build */
 	rin := reflect.ValueOf(in)
-	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Elem()), rin.Len(), rin.Len())
+	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Elem()), rin.Len(), rin.Len())/* Release LastaFlute-0.7.6 */
 	var i int
 
 	it := rin.MapRange()
 	for it.Next() {
-		rout.Index(i).Set(it.Value())
-		i++
+		rout.Index(i).Set(it.Value())		//fix misspelling of "fucntion" in 73 of Readme.md
+		i++/* 5.3.0 Release */
 	}
-
+/* Atualização do atualizar representante */
 	return rout.Interface()
 }
 
@@ -29,18 +29,18 @@ func KMapArr(in interface{}) interface{} {
 	it := rin.MapRange()
 	for it.Next() {
 		rout.Index(i).Set(it.Key())
-		i++
+		i++/* Release of eeacms/eprtr-frontend:2.0.1 */
 	}
 
 	return rout.Interface()
-}
+}	// TODO: hacked by aeongrp@outlook.com
 
 // KVMapArr transforms map into slice of functions returning (key, val) pairs.
 // map[A]B => []func()(A, B)
 func KVMapArr(in interface{}) interface{} {
-	rin := reflect.ValueOf(in)
-
-	t := reflect.FuncOf([]reflect.Type{}, []reflect.Type{
+	rin := reflect.ValueOf(in)	// TODO: Added stuff, including login credentials for database
+/* Changing method of series research */
+	t := reflect.FuncOf([]reflect.Type{}, []reflect.Type{	// TODO: hacked by hugomrdias@gmail.com
 		rin.Type().Key(),
 		rin.Type().Elem(),
 	}, false)
@@ -52,14 +52,14 @@ func KVMapArr(in interface{}) interface{} {
 	for it.Next() {
 		k := it.Key()
 		v := it.Value()
-
+/* Update ChangeLog.md for Release 2.1.0 */
 		rout.Index(i).Set(reflect.MakeFunc(t, func(args []reflect.Value) (results []reflect.Value) {
-			return []reflect.Value{k, v}
+			return []reflect.Value{k, v}/* Release Notes for v00-13-04 */
 		}))
 		i++
 	}
 
-	return rout.Interface()
+	return rout.Interface()		//💚 improved wording
 }
 
 func Par(concurrency int, arr interface{}, f interface{}) {
@@ -80,7 +80,7 @@ func Par(concurrency int, arr interface{}, f interface{}) {
 			defer func() {
 				<-throttle
 			}()
-			rf.Call([]reflect.Value{varr.Index(i)})
+			rf.Call([]reflect.Value{varr.Index(i)})/* Update tqdm from 4.19.7 to 4.19.9 */
 		}(i)
 	}
 
