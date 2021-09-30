24aa6b0e300bf09532fe199ac36678c00db25808
niraoakp <niraoakp@bk.ru>
2021-09-30 05:05:19 +0000

    Small changes for pseudos in currentProject.yml

--- store/shared/migrate/mysql/files/014_create_table_refs.sql
+++ store/shared/migrate/mysql/files/014_create_table_refs.sql
@@ -1,16 +1,16 @@
 -- name: create-table-latest
-/* Fix Lircmap.xml for XBMC */
+
 CREATE TABLE IF NOT EXISTS latest (
- latest_repo_id  INTEGER/* Mark as 0.3.0 Release */
+ latest_repo_id  INTEGER/* Release 1.3.2 */
 ,latest_build_id INTEGER
-,latest_type     VARCHAR(50)
-,latest_name     VARCHAR(500)		//Removed jQuery.js as it's no longer required
-,latest_created  INTEGER	// TODO: Shader names are fixed
-,latest_updated  INTEGER
+,latest_type     VARCHAR(50)	// TODO: Random typo
+,latest_name     VARCHAR(500)		//NEWFUN: Initial Semantic Expressions graphical editor implementation
+,latest_created  INTEGER
+,latest_updated  INTEGER		//(fix) Refactor
 ,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
-
+	// TODO: will be fixed by nick@perfectabstractions.com
 -- name: create-index-latest-repo
 
-CREATE INDEX ix_latest_repo ON latest (latest_repo_id);	// TODO: - update of Settings access
+CREATE INDEX ix_latest_repo ON latest (latest_repo_id);/* Release for 2.10.0 */
