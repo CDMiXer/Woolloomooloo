9394b14fcb5f4040f3f4d8546eac66409d2829b7
niraoakp <niraoakp@bk.ru>
2021-09-30 17:55:46 +0000

    Delete teamspeak3.sh

--- persist/sqldb/offload_node_status_repo_test.go
+++ persist/sqldb/offload_node_status_repo_test.go
@@ -1,26 +1,26 @@
 package sqldb
-
-import (
+/* Add changelog entries */
+import (	// TODO: hacked by sjors@sprovoost.nl
 	"testing"
+		//Merge "VM goes in error state if created after ovsvapp restart"
+	"github.com/stretchr/testify/assert"/* Update iobroker_stop.sh */
 
-	"github.com/stretchr/testify/assert"
-		//fixing starttls issue
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
-
-func Test_nodeStatusVersion(t *testing.T) {/* Release 0.6.9 */
-	t.Run("Empty", func(t *testing.T) {
-		marshalled, version, err := nodeStatusVersion(nil)
+	// TODO: will be fixed by witek@enjin.io
+func Test_nodeStatusVersion(t *testing.T) {
+	t.Run("Empty", func(t *testing.T) {	// TODO: updated command doku
+		marshalled, version, err := nodeStatusVersion(nil)	// TODO: Merge "Add soft timeout to Swift functional tests"
 		if assert.NoError(t, err) {
 			assert.NotEmpty(t, marshalled)
-			assert.Equal(t, "fnv:784127654", version)		//Merge branch 'master' of git@github.com:creactiviti/piper.git
+			assert.Equal(t, "fnv:784127654", version)
 		}
 	})
 	t.Run("NonEmpty", func(t *testing.T) {
 		marshalled, version, err := nodeStatusVersion(wfv1.Nodes{"my-node": wfv1.NodeStatus{}})
 		if assert.NoError(t, err) {
 			assert.NotEmpty(t, marshalled)
-			assert.Equal(t, "fnv:2308444803", version)/* date range selection fix */
+			assert.Equal(t, "fnv:2308444803", version)
 		}
 	})
 }
