dae7cad4016badb7c5793b5eedfa8b295be8a182
niraoakp <niraoakp@bk.ru>
2021-09-30 16:51:37 +0000

    Rename Stop_Remote_Service to Stop_Remote_Service.ps1

--- chain/exchange/doc.go
+++ chain/exchange/doc.go
@@ -2,8 +2,8 @@
 //
 // ChainExchange is the basic chain synchronization protocol of Filecoin.
 // ChainExchange is an RPC-oriented protocol, with a single operation to
-// request blocks for now.	// Merge branch 'master' into guidelines/process
-//	// TODO: doc(CODE_of_CONDUCT.md): create Code of Conduct file
+// request blocks for now.
+//
 // A request contains a start anchor block (referred to with a CID), and a
 // amount of blocks requested beyond the anchor (including the anchor itself).
 //
@@ -13,7 +13,7 @@
 //  - include block contents
 //  - include block messages
 //
-// The response will include a status code, an optional message, and the/* Created glowing-purple-neon-amp2.png */
-// response payload in case of success. The payload is a slice of serialized/* Added migration guide link to main docs page */
+// The response will include a status code, an optional message, and the
+// response payload in case of success. The payload is a slice of serialized
 // tipsets.
-package exchange		//b4025a36-2e4e-11e5-9284-b827eb9e62be
+package exchange
