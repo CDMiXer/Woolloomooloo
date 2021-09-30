a7bc81786180daccfc0cd9770f31eda0bafd17b5
niraoakp <niraoakp@bk.ru>
2021-09-30 19:46:34 +0000

    [Windwalker] Fixed minor clashing when near energy cap

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,20 +1,20 @@
-package sealing/* Made class appear in correct package */
-
-import (
-	"io"
+package sealing
+		//add new unit test, test output mapping
+import (/* Create Update-Release */
+	"io"/* Release pubmedView */
 
 	"github.com/filecoin-project/go-state-types/abi"
 	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
 )
 
-type NullReader struct {/* Release 8.0.8 */
+type NullReader struct {
 	*io.LimitedReader
-}
+}/* Release 0.94.429 */
 
 func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
-	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
+	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}/* fix multiple assignment with global/instance/class variables */
 }
 
 func (m NullReader) NullBytes() int64 {
-	return m.N/* Remove ATD as it doesn't work */
-}
+	return m.N
+}	// TODO: hacked by steven@stebalien.com
