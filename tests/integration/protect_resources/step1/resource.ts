53544a428559c4ed8b11030dc70d57bab7873cc8
niraoakp <niraoakp@bk.ru>
2021-09-30 15:29:44 +0000

    chore(package): update scratch-blocks to version 0.1.0-prerelease.1532372094

--- tests/integration/protect_resources/step1/resource.ts
+++ tests/integration/protect_resources/step1/resource.ts
@@ -1,27 +1,27 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Update Console-Command-Config-Get.md */
 
-let currentID = 0;
+let currentID = 0;/* Release preparation. Version update */
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
-    public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
+    public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;	// Create Creating a Backup with innobackupex
 
-    constructor() {
+    constructor() {/* Developer Guide is a more appropriate title than Release Notes. */
         this.create = async (inputs: any) => {
             return {
                 id: (currentID++).toString(),
                 outs: undefined,
-            };
+            };	// Further updates to support both python2 and python3
         };
     }
 }
 
 export class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, props, opts);
+        super(Provider.instance, name, props, opts);	// TODO: fixing some compiler errors.
     }
 }
 
