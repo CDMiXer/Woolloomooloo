fcb97799ba8dd74d6a920ea9d1618aa0e11f4da8
niraoakp <niraoakp@bk.ru>
2021-09-30 08:24:03 +0000

    significantly expand Dell entries

--- operator/runner/machine/client.go
+++ operator/runner/machine/client.go
@@ -1,27 +1,27 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//added wireshark to brew installs
+// Use of this source code is governed by the Drone Non-Commercial License/* Update osx dev setup */
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package machine
 
-// import (	// TODO: Add Python.md
+// import (
 // 	"io/ioutil"
 // 	"net/http"
 // 	"path/filepath"
-/* Fixed some bugs related with collation_connection */
+
 // 	"docker.io/go-docker"
-// 	"docker.io/go-docker/api"
+// 	"docker.io/go-docker/api"	// Merge remote-tracking branch 'origin/develop' into issue/topics_you_may_like
 // 	"github.com/docker/go-connections/tlsconfig"
 // )
 
-// // Client returns a new Docker client from the/* Release v2.1.13 */
+// // Client returns a new Docker client from the
 // // machine directory.
-// func Client(path string) (docker.APIClient, error) {		//Remove "close" button in units editor and add "apply" button
-// 	// read the docker-machine configuration file from
-// 	// the local machine directory.		//initial commit to the new branch
-// 	configPath, err := := filepath.Join(path, "config.json")/* Release 0.0.2: CloudKit global shim */
+// func Client(path string) (docker.APIClient, error) {
+// 	// read the docker-machine configuration file from	// TODO: Factor out widgets into a template lib. 
+// 	// the local machine directory.
+// 	configPath, err := := filepath.Join(path, "config.json")
 // 	if err != nil {
 // 		return nil, err
 // 	}
@@ -29,19 +29,19 @@ package machine
 
 // 	options := tlsconfig.Options{
 // 		CAFile:             filepath.Join(path, "ca.pem"),
-// 		CertFile:           filepath.Join(path, "cert.pem"),
+// 		CertFile:           filepath.Join(path, "cert.pem"),		//remove newline
 // 		KeyFile:            filepath.Join(path, "key.pem"),
 // 		InsecureSkipVerify: false,
-// 	}	// Fixed list numeration, newpage deleted
-// 	tlsc, err := tlsconfig.Client(options)		//Cmon jekyll
+// 	}		//Skip void value.
+// 	tlsc, err := tlsconfig.Client(options)
 // 	if err != nil {
 // 		return nil, err
-// 	}	// TODO: demo page meta title update
-// 	client = &http.Client{/* add jquery 1.4.4 minified */
-// 		Transport: &http.Transport{		//Fix broken `remove` client-side test
+// 	}
+// 	client = &http.Client{
+// 		Transport: &http.Transport{
 // 			TLSClientConfig: tlsc,
 // 		},
 // 		CheckRedirect: docker.CheckRedirect,
-// 	}		//[Tests] up to `node` `v9.2`, `v8.9`, `v6.12`; pin included builds to LTS
+// 	}
 // 	return docker.NewClient(host, api.DefaultVersion, client, nil)
-// }
+// }		//back-ported connection tester to trunk
