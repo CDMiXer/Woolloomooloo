c9db5e19575d11f4155bb4a30244592e01321120
niraoakp <niraoakp@bk.ru>
2021-09-30 02:28:38 +0000

    Only move 'unfinished' pipelines from success to running

--- tests/integration/double_pending_delete/step3/index.ts
+++ tests/integration/double_pending_delete/step3/index.ts
@@ -1,38 +1,38 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");		//wiredep requires chalk to run, as well...
+///* enable autogen again */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0		//Reference Test changes
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//The block sum filter should produce the same sum when weighted
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release DBFlute-1.1.1 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
+	// export annotation by file: add to daily export and display, closes #147
+import { Resource } from "./resource";
 
-import { Resource } from "./resource";/* Released 0.1.3 */
-
-// The previous plan failed, but we're going to initiate *another* plan that/* Release 0.4.1 */
+// The previous plan failed, but we're going to initiate *another* plan that
 // introduces new changes, while still keeping around the failed state
 // from the previous plan. The engine should delete all pending deletes before
-// attempting to start the next plan.
+// attempting to start the next plan./* Change $langs by $outputlangs */
 //
 // To do this, we're going to trigger another replacement of A:
 const a = new Resource("a", { fail: 3 });
 
 // We will still fail to replace B, since fail == 1.
-const b = new Resource("b", { fail: 1 }, { dependsOn: a });
-// The snapshot now contains:
+const b = new Resource("b", { fail: 1 }, { dependsOn: a });		//unsecured admin command fix
+// The snapshot now contains:/* Update apgtool-gui.py */
 //  A: Created
 //  A: Pending Delete
 //  B: Created
 
 // The A from the previous snapshot should have been deleted.
-
+/* A......... [ZBX-6124] removed unused API methods */
 // This plan is interesting because it shows that it is legal to delete the same URN multiple
-// times in the same plan. This previously triggered an assert in the engine that asserted
-// that this is impossible (https://github.com/pulumi/pulumi/issues/1503)
-
-
+// times in the same plan. This previously triggered an assert in the engine that asserted	// TODO: hacked by cory@protocol.ai
+// that this is impossible (https://github.com/pulumi/pulumi/issues/1503)	// TODO: hacked by lexy8russo@outlook.com
+		//Added better nav image to readme
+/* Release new version 2.3.29: Don't run bandaids on most pages (famlam) */
