d2c2080f512444640e9cbea6d66b3bc4232fde7d
niraoakp <niraoakp@bk.ru>
2021-09-30 15:08:18 +0000

    index the order field

--- store/shared/migrate/mysql/files/008_create_table_cron.sql
+++ store/shared/migrate/mysql/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
 -- name: create-table-cron
-
-CREATE TABLE IF NOT EXISTS cron (/* adjust tests according to changes [31119], re #4506 */
- cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT
+	// TODO: hacked by seth@sethvargo.com
+CREATE TABLE IF NOT EXISTS cron (/* docs(Release.md): improve release guidelines */
+ cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT		//Add mario.elm
 ,cron_repo_id     INTEGER
 ,cron_name        VARCHAR(50)
-,cron_expr        VARCHAR(50)
-,cron_next        INTEGER
+,cron_expr        VARCHAR(50)		//notebook UI experiment
+,cron_next        INTEGER		//8ef65898-2e6f-11e5-9284-b827eb9e62be
 ,cron_prev        INTEGER
-,cron_event       VARCHAR(50)
-,cron_branch      VARCHAR(250)
+,cron_event       VARCHAR(50)		//catalog metadata: listDocuments and deleteDocument implemented
+,cron_branch      VARCHAR(250)	// project: ? and * in ignored patterns do not match slashes
 ,cron_target      VARCHAR(250)
 ,cron_disabled    BOOLEAN
-,cron_created     INTEGER
-,cron_updated     INTEGER
-,cron_version     INTEGER	// TODO: will be fixed by zaq1tomo@gmail.com
+,cron_created     INTEGER	// TODO: will be fixed by arajasek94@gmail.com
+,cron_updated     INTEGER	// TODO: [15076] added Elexisbefunde updates for migration of befunde
+,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
-,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE		//Create stdint.h
 );
 
--- name: create-index-cron-repo
-		//Adjust code to reflect the dottie api
+-- name: create-index-cron-repo	// TODO: hacked by ng8eke@163.com
+
 CREATE INDEX ix_cron_repo ON cron (cron_repo_id);
 
--- name: create-index-cron-next
-
-CREATE INDEX ix_cron_next ON cron (cron_next);/* Release 0.0.12 */
+-- name: create-index-cron-next		//madwifi: fix ACL race condition (patch by Sebastian Gottschall)
+/* Delete J17JigsClothing.php~ */
+CREATE INDEX ix_cron_next ON cron (cron_next);
