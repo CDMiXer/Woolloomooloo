63cc36a779b798c083c6ac784c6c5968d8e08ed7
niraoakp <niraoakp@bk.ru>
2021-09-30 13:33:24 +0000

    added test for entities with @Entity(name="...")

--- tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
@@ -1,35 +1,35 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-	// Merge branch 'master' into pyup-update-flask-0.12-to-1.1.1
-using System.Threading.Tasks;
-using Pulumi;/* Release candidate post testing. */
 
-class Resource : ComponentResource
-{	// TODO: will be fixed by caojiaoyue@protonmail.com
+using System.Threading.Tasks;
+using Pulumi;/* Release 1.11.10 & 2.2.11 */
+
+class Resource : ComponentResource/* NP-14457. Add delete to employe super api. */
+{
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
 }
-/* Merge "Release notest for v1.1.0" */
+
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive : ComponentResource
-{/* [artifactory-release] Release version 3.3.11.RELEASE */
-    private Resource resource;
-
-    public ComponentFive(string name, ComponentResourceOptions options = null)
-        : base("my:module:ComponentFive", name, options)		//Fix resolution spins (they must not allow non-numeric characters)
+{
+    private Resource resource;/* Angle grid. */
+		//b8a850cf-2eae-11e5-8d97-7831c1d44c14
+    public ComponentFive(string name, ComponentResourceOptions options = null)		//Match for UUIDv4
+        : base("my:module:ComponentFive", name, options)
     {
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
-
-class Program
+/* trigger "go-toast/toast" by git@jacobmarshall.co */
+class Program	// TODO: will be fixed by mail@bitpshr.net
 {
     static Task<int> Main(string[] args)
-    {/* still working through this */
-        return Deployment.RunAsync(() => /* a0622bd8-2e4d-11e5-9284-b827eb9e62be */
+    {
+        return Deployment.RunAsync(() => 
         {
             var comp5 = new ComponentFive("comp5");
-        });
+        });/* Update nuspec to point at Release bits */
     }
 }
