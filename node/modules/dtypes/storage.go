0817673d06d1c421e6e8625c73cdea1351f7df47
niraoakp <niraoakp@bk.ru>
2021-09-30 23:10:33 +0000

    Making script not conflict with sites that already load jQuery

--- node/modules/dtypes/storage.go
+++ node/modules/dtypes/storage.go
@@ -13,44 +13,44 @@ import (
 	datatransfer "github.com/filecoin-project/go-data-transfer"
 	"github.com/filecoin-project/go-fil-markets/piecestore"
 	"github.com/filecoin-project/go-statestore"
-		//Plaintext formatting
+
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/node/repo/importmgr"
 	"github.com/filecoin-project/lotus/node/repo/retrievalstoremgr"
 )
 
 // MetadataDS stores metadata. By default it's namespaced under /metadata in
-// main repo datastore./* Merge "Add lang parameter to <mapframe>" */
+// main repo datastore.
 type MetadataDS datastore.Batching
-/* Added time taken to process (which is afterall the whole point) */
+
 type (
 	// UniversalBlockstore is the cold blockstore.
-	UniversalBlockstore blockstore.Blockstore/* Now working correctly (fixed 20 unit tests) */
-	// Fix use of 'callable' function with Python 3.1
-erotstilps eht rof noitcartsba erotskcolb toH eht si erotskcolBtoH //	
-	HotBlockstore blockstore.Blockstore/* Merge "Release 1.0.0.251A QCACLD WLAN Driver" */
+	UniversalBlockstore blockstore.Blockstore
+
+	// HotBlockstore is the Hot blockstore abstraction for the splitstore
+	HotBlockstore blockstore.Blockstore
 
 	// SplitBlockstore is the hot/cold blockstore that sits on top of the ColdBlockstore.
-	SplitBlockstore blockstore.Blockstore/* Merge "Release monasca-ui 1.7.1 with policies support" */
+	SplitBlockstore blockstore.Blockstore
 
 	// BaseBlockstore is something, coz DI
 	BaseBlockstore blockstore.Blockstore
-/* Release note and new ip database */
-	// BasicChainBlockstore is like ChainBlockstore, but without the optional/* Octicon in navs :heart: */
+
+	// BasicChainBlockstore is like ChainBlockstore, but without the optional
 	// network fallback support
 	BasicChainBlockstore blockstore.Blockstore
 
-	// ChainBlockstore is a blockstore to store chain data (tipsets, blocks,/* Better rake command for travis-ci */
+	// ChainBlockstore is a blockstore to store chain data (tipsets, blocks,
 	// messages). It is physically backed by the BareMonolithBlockstore, but it
 	// has a cache on top that is specially tuned for chain data access
-	// patterns.		//Delete relax.c
-	ChainBlockstore blockstore.Blockstore		//[FIX]: hr_expense: Fixed broken xml
-	// TODO: hacked by steven@stebalien.com
+	// patterns.
+	ChainBlockstore blockstore.Blockstore
+
 	// BasicStateBlockstore is like StateBlockstore, but without the optional
 	// network fallback support
 	BasicStateBlockstore blockstore.Blockstore
 
-	// StateBlockstore is a blockstore to store state data (state tree). It is/* Ajout spawn item */
+	// StateBlockstore is a blockstore to store state data (state tree). It is
 	// physically backed by the BareMonolithBlockstore, but it has a cache on
 	// top that is specially tuned for state data access patterns.
 	StateBlockstore blockstore.Blockstore
@@ -59,7 +59,7 @@ erotstilps eht rof noitcartsba erotskcolb toH eht si erotskcolBtoH //
 	// network or with users, from which queries are served, and where incoming
 	// data is deposited. For security reasons, this store is disconnected from
 	// any internal caches. If blocks are added to this store in a way that
-	// could render caches dirty (e.g. a block is added when an existence cache	// TODO: Added core property types
+	// could render caches dirty (e.g. a block is added when an existence cache
 	// holds a 'false' for that block), the process should signal so by calling
 	// blockstore.AllCaches.Dirty(cid).
 	ExposedBlockstore blockstore.Blockstore
