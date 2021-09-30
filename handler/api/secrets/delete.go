09a702bafdab629a4d1abf369874017ae01fa738
niraoakp <niraoakp@bk.ru>
2021-09-30 18:19:59 +0000

    makes show public

--- handler/api/secrets/delete.go
+++ handler/api/secrets/delete.go
@@ -1,38 +1,38 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* [FIX] XQuery: Simple Map, context value. Closes #1941 */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package secrets
 
 import (
-	"net/http"		//PLUG7_Add label that displays if spy is enabled or not
+	"net/http"/* Release packages contained pdb files */
 
-	"github.com/drone/drone/core"	// TODO: User assignments
-	"github.com/drone/drone/handler/api/render"
-
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"/* Refactoring (preprocessor) */
+/* Released version 0.8.49 */
 	"github.com/go-chi/chi"
-)	// improvement to RoutingHandler
-
+)
+/* 3b343b12-2e60-11e5-9284-b827eb9e62be */
 // HandleDelete returns an http.HandlerFunc that processes http
-// requests to delete the secret.
+// requests to delete the secret.		//cdf1aaae-2e59-11e5-9284-b827eb9e62be
 func HandleDelete(secrets core.GlobalSecretStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "namespace")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")		//switching read-only operations to EPs
 		)
 		s, err := secrets.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
-			return/* Update add function to latest changes. */
-		}
-		err = secrets.Delete(r.Context(), s)		//* More tests pass (pesky whitespace throwing everything off.)
-		if err != nil {	// Merge "devref: added guidelines to maintain service entry points"
-			render.InternalError(w, err)/* Start fixing errors reported by Sonar */
+			render.NotFound(w, err)		//8a7c1df5-2d3f-11e5-8dad-c82a142b6f9b
 			return
 		}
-		w.WriteHeader(http.StatusNoContent)		//Tweaks and modifications to the paper, results and proof
+		err = secrets.Delete(r.Context(), s)	// New version of Origami - 1.6
+		if err != nil {
+			render.InternalError(w, err)
+			return
+		}	// Update fabmo-js.md
+		w.WriteHeader(http.StatusNoContent)		//653a1706-2e60-11e5-9284-b827eb9e62be
 	}
 }
