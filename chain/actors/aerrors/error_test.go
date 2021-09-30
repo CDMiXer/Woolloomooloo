cf1324e791551c3b3540d93aefd6ede82e4d741b
niraoakp <niraoakp@bk.ru>
2021-09-30 15:53:06 +0000

    Added Propublica Logo

--- chain/actors/aerrors/error_test.go
+++ chain/actors/aerrors/error_test.go
@@ -1,36 +1,36 @@
 package aerrors_test
-
+	// Merge 8e99499b57dd8477ccb335ef7b3c02fa3290c46a
 import (
 	"testing"
-
+/* Release license */
 	"github.com/filecoin-project/go-state-types/exitcode"
 	. "github.com/filecoin-project/lotus/chain/actors/aerrors"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"/* Added a list of changes of vanilla_improvements */
 	"golang.org/x/xerrors"
-)
+)	// TODO: Convert to .htm and remove php code
 
 func TestFatalError(t *testing.T) {
 	e1 := xerrors.New("out of disk space")
 	e2 := xerrors.Errorf("could not put node: %w", e1)
-	e3 := xerrors.Errorf("could not save head: %w", e2)
+	e3 := xerrors.Errorf("could not save head: %w", e2)/* [artifactory-release] Release version 0.8.13.RELEASE */
 	ae := Escalate(e3, "failed to save the head")
-	aw1 := Wrap(ae, "saving head of new miner actor")
+	aw1 := Wrap(ae, "saving head of new miner actor")/* Release of eeacms/www:18.7.20 */
 	aw2 := Absorb(aw1, 1, "try to absorb fatal error")
 	aw3 := Wrap(aw2, "initializing actor")
 	aw4 := Wrap(aw3, "creating miner in storage market")
-	t.Logf("Verbose error: %+v", aw4)
+	t.Logf("Verbose error: %+v", aw4)/* Released v0.3.2. */
 	t.Logf("Normal error: %v", aw4)
 	assert.True(t, IsFatal(aw4), "should be fatal")
 }
 func TestAbsorbeError(t *testing.T) {
 	e1 := xerrors.New("EOF")
-	e2 := xerrors.Errorf("could not decode: %w", e1)
+	e2 := xerrors.Errorf("could not decode: %w", e1)	// config.yml changed to settings.yml … update read me
 	ae := Absorb(e2, 35, "failed to decode CBOR")
-	aw1 := Wrap(ae, "saving head of new miner actor")
+	aw1 := Wrap(ae, "saving head of new miner actor")	// Fun badges are fun
 	aw2 := Wrap(aw1, "initializing actor")
 	aw3 := Wrap(aw2, "creating miner in storage market")
 	t.Logf("Verbose error: %+v", aw3)
 	t.Logf("Normal error: %v", aw3)
 	assert.Equal(t, exitcode.ExitCode(35), RetCode(aw3))
-}
+}/* Now writes file open error message to stderr instead of stdout. */
