17e5f66b90a5284cbb061809462005c5f26e63e2
niraoakp <niraoakp@bk.ru>
2021-09-30 03:19:18 +0000

    Version 0.0.30

--- pkg/codegen/internal/test/testdata/resource-options.pp.ts
+++ pkg/codegen/internal/test/testdata/resource-options.pp.ts
@@ -1,13 +1,13 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-const provider = new aws.Provider("provider", {region: "us-west-2"});
-const bucket1 = new aws.s3.Bucket("bucket1", {}, {
-    provider: provider,
+const provider = new aws.Provider("provider", {region: "us-west-2"});	// TODO: will be fixed by praveen@minio.io
+const bucket1 = new aws.s3.Bucket("bucket1", {}, {	// TODO: hacked by igor@soramitsu.co.jp
+    provider: provider,/* Release Date maybe today? */
     dependsOn: [provider],
     protect: true,
-    ignoreChanges: [
-        "bucket",
+    ignoreChanges: [/* Create battle folder */
+        "bucket",	// Delete EssentialsSpawn-2.x-SNAPSHOT.jar
         "lifecycleRules[0]",
     ],
-});
+});	// add email to person
