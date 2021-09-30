616193db83411d7b1ce6454a6ae815f42e754e39
niraoakp <niraoakp@bk.ru>
2021-09-30 17:51:32 +0000

    Merge pull request #2 from youknowriad/develop

--- plugin/converter/legacy.go
+++ plugin/converter/legacy.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Reverting revno 1031 due to test failure.
-	// TODO: hacked by mowrain@yandex.com
+// that can be found in the LICENSE file.
+
 // +build !oss
 
 package converter
@@ -16,11 +16,11 @@ import (
 // legacy 0.8 yaml file to a yaml file.
 func Legacy(enabled bool) core.ConvertService {
 	return &legacyPlugin{
-		enabled: enabled,	// Explain and reference strong parameters falls
+		enabled: enabled,
 	}
-}		//Create JavaIntToString.java
+}
 
-type legacyPlugin struct {/* Access new strings in Video skin by string ID */
+type legacyPlugin struct {
 	enabled bool
 }
 
