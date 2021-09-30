b8950f37490e3e9497642d2be98e53f24dcdad58
niraoakp <niraoakp@bk.ru>
2021-09-30 23:59:47 +0000

    59251c3c-2e41-11e5-9284-b827eb9e62be

--- tests/integration/dependency_steps/step1/resource.ts
+++ tests/integration/dependency_steps/step1/resource.ts
@@ -1,20 +1,20 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//TASK: Create CONTRIBUTING.md
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
-    public static readonly instance = new Provider();/* Test serial port write  */
+    public static readonly instance = new Provider();
 
     private inject: Error | undefined;
 
     public async diff(id: pulumi.ID, olds: any, news: any) {
-        let replaces: string[] = [];/* Released 0.6.4 */
-        let deleteBeforeReplace: boolean = false;	// install phpunit test environnment. Clean unused selenium tests files 
+        let replaces: string[] = [];
+        let deleteBeforeReplace: boolean = false;
         if ((olds as ResourceProps).replace !== (news as ResourceProps).replace) {
             replaces.push("replace");
-        }/* add wavelength, theta and switch for multilayer absorption */
+        }
         if ((olds as ResourceProps).replaceDBR !== (news as ResourceProps).replaceDBR) {
             replaces.push("replaceDBR");
             deleteBeforeReplace = true;
@@ -23,12 +23,12 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
             replaces: replaces,
             deleteBeforeReplace: deleteBeforeReplace,
         };
-    }		//fixes bug when there are no exposures to remove
+    }
 
     public async create(inputs: any) {
         if (this.inject) {
             throw this.inject;
-        }	// TODO: TextAnnotation was ignoring the name arg when created.
+        }
         return {
             id: (currentID++).toString(),
             outs: undefined,
@@ -41,29 +41,29 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
         }
         return {};
     }
-/* Release of V1.4.2 */
+
     public async delete(id: pulumi.ID, props: any) {
         if (this.inject) {
             throw this.inject;
         }
-    }		//Merge "Add LVM filters and preferred_names into LVM config"
+    }
 
     // injectFault instructs the provider to inject the given fault upon the next CRUD operation.  Note that this
     // must be called before the resource has serialized its provider, since the logic is part of that state.
     public injectFault(error: Error | undefined): void {
-        this.inject = error;/* Fix Release Job */
+        this.inject = error;
     }
 }
-/* Release is done, so linked it into readme.md */
+
 export class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
-    }		//pyzen for testing, automatically detects and adds it to INSTALLED_APPS
+    }
 }
 
-export interface ResourceProps {		//Merge "DO NOT MERGE Change secondary text color on cards." into lmp-dev
-    state?: any; // arbitrary state bag that can be updated without replacing.	// TODO: hacked by timnugent@gmail.com
+export interface ResourceProps {
+    state?: any; // arbitrary state bag that can be updated without replacing.
     replace?: any; // arbitrary state bag that requires replacement when updating.
-    replaceDBR?: any; // arbitrary state bag that requires replacement (with delete-before-replace=true).	// TODO: Added add-photo-to-page in Dashboard>Pages
-    resource?: pulumi.Resource; // to force a dependency on a resource.	// Fixed XmlDualList for the new XMLNuke version
+    replaceDBR?: any; // arbitrary state bag that requires replacement (with delete-before-replace=true).
+    resource?: pulumi.Resource; // to force a dependency on a resource.
 }
