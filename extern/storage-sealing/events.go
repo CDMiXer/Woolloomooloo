e2b4fb988d46ec26bff7e6ca8b9645351d6de12b
niraoakp <niraoakp@bk.ru>
2021-09-30 08:08:28 +0000

    Create Sidebar.html

--- extern/storage-sealing/events.go
+++ extern/storage-sealing/events.go
@@ -1,15 +1,15 @@
 package sealing
 
-import (
+import (	// TODO: Merge "esoc: Add debug engine for external modems."
 	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
-
+		//Fixed ref counting bug for loading templates
 // `curH`-`ts.Height` = `confidence`
 type HeightHandler func(ctx context.Context, tok TipSetToken, curH abi.ChainEpoch) error
 type RevertHandler func(ctx context.Context, tok TipSetToken) error
 
 type Events interface {
 	ChainAt(hnd HeightHandler, rev RevertHandler, confidence int, h abi.ChainEpoch) error
-}
+}		//CF/BF - delete some unused code from BST.
