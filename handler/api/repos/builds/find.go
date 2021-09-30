1f358e76d60107649607e883cbd3c05fbca7cd56
niraoakp <niraoakp@bk.ru>
2021-09-30 07:09:50 +0000

    updating poms for branch'release-1.3' with non-snapshot versions

--- handler/api/repos/builds/find.go
+++ handler/api/repos/builds/find.go
@@ -1,59 +1,59 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Disable email notifications on travis build */
-// you may not use this file except in compliance with the License.		//05228542-2e56-11e5-9284-b827eb9e62be
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// Merge "xenapi: agent not inject ssh-key if cloud-init"
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Update KubernetesFacade.java */
+// limitations under the License.
 
-package builds/* [FIXED MNBMODULE-103] JARs are signed, so do not try to fix up policy. */
+package builds
 
 import (
 	"net/http"
 	"strconv"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Add DirWriter.  Add str() for manifest items. */
+	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
 
 // HandleFind returns an http.HandlerFunc that writes json-encoded
-// build details to the the response body./* Updated reverse param doc */
-func HandleFind(/* Removed bell alert. */
+// build details to the the response body.
+func HandleFind(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-	stages core.StageStore,	// TODO: Fixed the call to os.path.basename.
+	stages core.StageStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")		//Delete SoftwareEmpresaClienteCorrecto.rar
+			name      = chi.URLParam(r, "name")
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
-			render.BadRequest(w, err)/* Preparing WIP-Release v0.1.37-alpha */
+			render.BadRequest(w, err)
 			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* 1.96 Release of DaticalDB4UDeploy */
+		if err != nil {
 			render.NotFound(w, err)
-			return/* Insignificant AudioSorter updates for sorting new instruments */
-		}		//code totally reformatted
+			return
+		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}/* Updated the oset feedstock. */
+		}
 		stages, err := stages.ListSteps(r.Context(), build.ID)
 		if err != nil {
-			render.InternalError(w, err)	// Moved SpacePartioning into its own file.
+			render.InternalError(w, err)
 			return
 		}
 		render.JSON(w, &buildWithStages{build, stages}, 200)
