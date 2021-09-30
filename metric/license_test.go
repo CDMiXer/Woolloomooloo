b4aa273440f58f568d5fd7b08fdf13dd0401c3e4
niraoakp <niraoakp@bk.ru>
2021-09-30 14:54:16 +0000

    Select the new bookmark in the view when it is added.

--- metric/license_test.go
+++ metric/license_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by ac0dem0nk3y@gmail.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
