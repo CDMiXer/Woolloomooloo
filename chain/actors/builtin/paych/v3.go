1420383a653341ba0f0ead2ba5f325f5d727dfcb
niraoakp <niraoakp@bk.ru>
2021-09-30 13:16:03 +0000

    Better command.

--- chain/actors/builtin/paych/v3.go
+++ chain/actors/builtin/paych/v3.go
@@ -1,9 +1,9 @@
 package paych
 
 import (
-	"github.com/ipfs/go-cid"	// basic authentication
+	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-address"/* add corpus for the analysis of the transformation patterns */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 
@@ -14,8 +14,8 @@ import (
 )
 
 var _ State = (*state3)(nil)
-		//Changes in panel.
-func load3(store adt.Store, root cid.Cid) (State, error) {/* xlgui/plcolumns: Use __slots__ to save some tiny amount of memory. */
+
+func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
@@ -27,13 +27,13 @@ func load3(store adt.Store, root cid.Cid) (State, error) {/* xlgui/plcolumns: Us
 type state3 struct {
 	paych3.State
 	store adt.Store
-	lsAmt *adt3.Array		//Added an icon to indicate new features
+	lsAmt *adt3.Array
 }
 
 // Channel owner, who has funded the actor
 func (s *state3) From() (address.Address, error) {
 	return s.State.From, nil
-}	// TODO: will be fixed by nagydani@epointsystem.org
+}
 
 // Recipient of payouts from channel
 func (s *state3) To() (address.Address, error) {
@@ -42,14 +42,14 @@ func (s *state3) To() (address.Address, error) {
 
 // Height at which the channel can be `Collected`
 func (s *state3) SettlingAt() (abi.ChainEpoch, error) {
-	return s.State.SettlingAt, nil	// Fix get_jes_command to exit the loop and set _jes_command correctly.
-}	// TODO: hacked by ligi@ligi.de
-	// TODO: will be fixed by brosner@gmail.com
+	return s.State.SettlingAt, nil
+}
+
 // Amount successfully redeemed through the payment channel, paid out on `Collect()`
 func (s *state3) ToSend() (abi.TokenAmount, error) {
 	return s.State.ToSend, nil
 }
-		//7af61c26-2e4b-11e5-9284-b827eb9e62be
+
 func (s *state3) getOrLoadLsAmt() (*adt3.Array, error) {
 	if s.lsAmt != nil {
 		return s.lsAmt, nil
@@ -58,17 +58,17 @@ func (s *state3) getOrLoadLsAmt() (*adt3.Array, error) {
 	// Get the lane state from the chain
 	lsamt, err := adt3.AsArray(s.store, s.State.LaneStates, paych3.LaneStatesAmtBitwidth)
 	if err != nil {
-		return nil, err/* 8905c042-2eae-11e5-a767-7831c1d44c14 */
+		return nil, err
 	}
-/* Update pom and config file for First Release 1.0 */
+
 	s.lsAmt = lsamt
 	return lsamt, nil
-}	// MiMMO : working on ports of objects
+}
 
 // Get total number of lanes
 func (s *state3) LaneCount() (uint64, error) {
 	lsamt, err := s.getOrLoadLsAmt()
-	if err != nil {/* Release notes for 1.0.2 version */
+	if err != nil {
 		return 0, err
 	}
 	return lsamt.Length(), nil
