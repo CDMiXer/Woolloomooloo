053b76a310e8c4ddd61e8eec2979f7ac30e9a18c
niraoakp <niraoakp@bk.ru>
2021-09-30 13:43:39 +0000

    Rename octal.cpp to Prog13_octal.cpp

--- tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
@@ -1,15 +1,15 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Merge "Release 4.0.10.54 QCACLD WLAN Driver" */
-
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+/* 0.8.5 Release for Custodian (#54) */
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-
+/* Release: update to 4.2.1-shared */
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)/* a7674658-2e56-11e5-9284-b827eb9e62be */
+        super().__init__("my:module:Resource", name, None, opts)
 
-# Scenario #5 - composing #1 and #3 and making both changes at the same time
+# Scenario #5 - composing #1 and #3 and making both changes at the same time/* Modified the Deadline so it handles non 0 origin and complements Release */
 class ComponentFive(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentFive", name, None, opts)
-        res1 = Resource1("otherchild", ResourceOptions(parent=self))	// TODO: Added jruby script add-on
+        res1 = Resource1("otherchild", ResourceOptions(parent=self))
 
 comp5 = ComponentFive("comp5")
