dfd4da67124061091aad5b49859d1aef202927c3
niraoakp <niraoakp@bk.ru>
2021-09-30 01:00:34 +0000

    Update viz-runner.js

--- tests/integration/read/import_acquire/import_acquire_test.go
+++ tests/integration/read/import_acquire/import_acquire_test.go
@@ -1,18 +1,18 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by steven@stebalien.com
 // +build nodejs all
-
+/* Release 0.4.10. */
 package ints
 
 import (
 	"testing"
-
+	// TODO: hacked by why@ipfs.io
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)
-
+)	// ModelTest better message for technical field problems
+/* Release 9.4.0 */
 // Test that the engine is capable of assuming control of a resource that was external.
 func TestImportAcquire(t *testing.T) {
 	t.Skipf("import does not yet work with dynamic providers")
-
+/* Release v5.14 */
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
@@ -22,6 +22,6 @@ func TestImportAcquire(t *testing.T) {
 				Dir:      "step2",
 				Additive: true,
 			},
-		},
+		},	// TODO: hacked by greg@colvin.org
 	})
 }
