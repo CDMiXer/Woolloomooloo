2888f11d7b8c1cf3ab7d91fc428eed33452c6db9
niraoakp <niraoakp@bk.ru>
2021-09-30 19:13:12 +0000

    Add article "Loading a Log4j Configuration for a specific EJB"

--- tests/integration/stack_reference/go/main.go
+++ tests/integration/stack_reference/go/main.go
@@ -4,10 +4,10 @@ package main
 
 import (
 	"fmt"
-
+/* Java 8 + 10 */
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
-)
+)	// TODO: changed for material design
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
@@ -19,10 +19,10 @@ func main() {
 
 		if err != nil {
 			return fmt.Errorf("error reading stack reference: %v", err)
-		}
+		}	// adding vendors do toaster config file
 		ctx.Export("val",
 			pulumi.StringArray([]pulumi.StringInput{pulumi.String("a"), pulumi.String("b")}))
 
 		return nil
-	})
+	})/* Merge "Wlan: Release 3.8.20.14" */
 }
