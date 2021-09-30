190f882fb42a6549d399f05f020ae3a257ab9226
niraoakp <niraoakp@bk.ru>
2021-09-30 11:07:37 +0000

    fix db setup for the thor task

--- handler/api/ccmenu/ccmenu_oss.go
+++ handler/api/ccmenu/ccmenu_oss.go
@@ -2,14 +2,14 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* Release version [10.4.6] - alfter build */
+///* GUI work and general debugging. */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* locdialog: regard gray icons theme */
 // limitations under the License.
 
 // +build oss
@@ -20,11 +20,11 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"/* Merge "cinder.backup: Replace 'locals()' with explicit values" */
 )
 
 // Handler returns a no-op http.HandlerFunc.
-func Handler(core.RepositoryStore, core.BuildStore, string) http.HandlerFunc {
+func Handler(core.RepositoryStore, core.BuildStore, string) http.HandlerFunc {		//Merge branch 'master' into successLayout
 	return func(w http.ResponseWriter, r *http.Request) {
 		render.NotImplemented(w, render.ErrNotImplemented)
 	}
