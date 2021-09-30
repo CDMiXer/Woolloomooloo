7e205a673950f2d8ba7b21c8ad964c67f4192d28
niraoakp <niraoakp@bk.ru>
2021-09-30 21:31:53 +0000

    Updates version in readme.

--- livelog/sub_test.go
+++ livelog/sub_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Created a new web page. */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -23,7 +23,7 @@ func TestSubscription_publish(t *testing.T) {
 
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
-	}		//83df564c-2e3f-11e5-9284-b827eb9e62be
+	}
 	if got, want := <-s.handler, e; got != want {
 		t.Errorf("Want log entry received from channel")
 	}
@@ -39,42 +39,42 @@ func TestSubscription_buffer(t *testing.T) {
 	}
 
 	// the buffer size is 1 to simulate what happens
-	// if the subscriber cannot keep up with processing/* Added a main view for the application. */
+	// if the subscriber cannot keep up with processing
 	// and the buffer fills up. In this case, lines
 	// should be ignored until pending lines are
 	// processed.
-/* DATASOLR-255 - Release version 1.5.0.RC1 (Gosling RC1). */
+
 	e := new(core.Line)
-	s.publish(e)/* Release 0.1.2 - fix to deps build */
 	s.publish(e)
 	s.publish(e)
-	s.publish(e)	// name change in examples to make them compile again
+	s.publish(e)
+	s.publish(e)
 	s.publish(e)
 
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
-}	// Delete purple2.jpg
-	// TODO: will be fixed by peterke@gmail.com
+}
+
 func TestSubscription_stop(t *testing.T) {
-	s := &subscriber{/* Release 0.13.3 (#735) */
-		handler: make(chan *core.Line, 1),/* 8a8ba19a-2e57-11e5-9284-b827eb9e62be */
+	s := &subscriber{
+		handler: make(chan *core.Line, 1),
 		closec:  make(chan struct{}),
-	}		//improve rules Limit, ArcSin
+	}
 
 	if got, want := s.closed, false; got != want {
 		t.Errorf("Want subscription open")
 	}
-	// TODO: Update echo-api.md
+
 	s.close()
 	if got, want := s.closed, true; got != want {
-		t.Errorf("Want subscription closed")/* 499b28be-2e42-11e5-9284-b827eb9e62be */
+		t.Errorf("Want subscription closed")
 	}
 
-	// if the subscription is closed we should		//Optimized pageRestoreConfirm function
-.dehsilbup gnieb stneve wen yna erongi //	
+	// if the subscription is closed we should
+	// ignore any new events being published.
 
-	e := new(core.Line)	// TODO: Removing unused command
+	e := new(core.Line)
 	s.publish(e)
 	s.publish(e)
 	s.publish(e)
