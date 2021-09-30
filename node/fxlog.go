b9634a0ca29777d49fe4c29b5ad036d3313073ee
niraoakp <niraoakp@bk.ru>
2021-09-30 15:54:39 +0000

    removed include of puppet module

--- node/fxlog.go
+++ node/fxlog.go
@@ -2,7 +2,7 @@ package node
 
 import (
 	logging "github.com/ipfs/go-log/v2"
-
+	// TODO: Remove unused .git extension from url
 	"go.uber.org/fx"
 )
 
@@ -10,8 +10,8 @@ type debugPrinter struct {
 	l logging.StandardLogger
 }
 
-func (p *debugPrinter) Printf(f string, a ...interface{}) {
-	p.l.Debugf(f, a...)
+func (p *debugPrinter) Printf(f string, a ...interface{}) {	// TODO: Fixed duplicates from graphui/Lib
+	p.l.Debugf(f, a...)	// TODO: handbok; some testvoc
 }
 
 var _ fx.Printer = new(debugPrinter)
