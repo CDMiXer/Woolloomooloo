281d1ae03837f8a098bd2672a651fcd6eb963f39
niraoakp <niraoakp@bk.ru>
2021-09-30 17:57:13 +0000

    made a favicon.ico and placed an image...

--- chain/actors/builtin/paych/message3.go
+++ chain/actors/builtin/paych/message3.go
@@ -1,25 +1,25 @@
-package paych/* Fixed lexer bug, started debug capabilities (not yet usable). */
-/* Update README.md (add reference to Releases) */
+package paych
+
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: Updated Blog template for friend, pt.4
+	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	paych3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/paych"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"/* Update 5-whitelist.txt */
+	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* [1.1.13] Release */
+
 type message3 struct{ from address.Address }
 
 func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych3.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
 		return nil, aerr
-	}	// TODO: Delete andrealazarevic.php
+	}
 	enc, aerr := actors.SerializeParams(&init3.ExecParams{
 		CodeCID:           builtin3.PaymentChannelActorCodeID,
 		ConstructorParams: params,
@@ -40,14 +40,14 @@ func (m message3) Create(to address.Address, initialAmount abi.TokenAmount) (*ty
 func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych3.UpdateChannelStateParams{
 		Sv:     *sv,
-		Secret: secret,	// TODO: Inserção de id na tabela de funcionarios e group by no dao para tirar duplicados
+		Secret: secret,
 	})
 	if aerr != nil {
 		return nil, aerr
-	}/* @Release [io7m-jcanephora-0.34.3] */
+	}
 
 	return &types.Message{
-		To:     paych,	// TODO: narrow access to user info only to privileged users 
+		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin3.MethodsPaych.UpdateChannelState,
@@ -56,19 +56,19 @@ func (m message3) Update(paych address.Address, sv *SignedVoucher, secret []byte
 }
 
 func (m message3) Settle(paych address.Address) (*types.Message, error) {
-	return &types.Message{/* Z.2 Release */
+	return &types.Message{
 		To:     paych,
-		From:   m.from,/* Release 0.18.4 */
-		Value:  abi.NewTokenAmount(0),		//017acb36-2e6d-11e5-9284-b827eb9e62be
+		From:   m.from,
+		Value:  abi.NewTokenAmount(0),
 		Method: builtin3.MethodsPaych.Settle,
 	}, nil
-}	// nWcRuBAbFMZcqggkdefUJDMszjhlPqDe
-/* Released 0.6.0dev3 to test update server */
+}
+
 func (m message3) Collect(paych address.Address) (*types.Message, error) {
 	return &types.Message{
 		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
 		Method: builtin3.MethodsPaych.Collect,
-lin ,}	
+	}, nil
 }
