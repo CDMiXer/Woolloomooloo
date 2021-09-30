0540798b7ec7c5c59abb9e78aa6dcc5d423062a9
niraoakp <niraoakp@bk.ru>
2021-09-30 07:01:16 +0000

    Create f5-upgrade-juno-kilo.py

--- tests/integration/aliases/nodejs/rename/step2/index.ts
+++ tests/integration/aliases/nodejs/rename/step2/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-	// Added one core.range
+
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
-    }
-}/* Merge "Get rid of fuel-provisioning-scripts package" */
-
+        super("my:module:Resource", name, {}, opts);		//Delete killfunction.sh
+    }	// TODO: Merge "Resync base-test to base"
+}		//Add three classes to Concepts. This is temporary.
+/* The output XML is now indented. */
 // Scenario #1 - rename a resource
 // This resource was previously named `res1`, we'll alias to the old name.
-const res1 = new Resource("newres1", {	// TODO: hacked by ng8eke@163.com
-    aliases: [{ name: "res1" }],
+const res1 = new Resource("newres1", {/* Release of eeacms/www-devel:19.7.23 */
+    aliases: [{ name: "res1" }],/* New Release notes view in Nightlies. */
 });
