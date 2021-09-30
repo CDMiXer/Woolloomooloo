63d9eaf970305a1f9f72fb0d3efdde09275da25d
niraoakp <niraoakp@bk.ru>
2021-09-30 09:23:23 +0000

    added exceptions where source path is required

--- conformance/chaos/ids.go
+++ conformance/chaos/ids.go
@@ -1,28 +1,28 @@
-package chaos
+package chaos	// TODO: hacked by hello@brooklynzelenka.com
 
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-	"github.com/multiformats/go-multihash"/* Extract patch process actions from PatchReleaseController; */
+	"github.com/multiformats/go-multihash"
 )
-	// fix #274 upgrade lombok to 1.16.16.0
+/* Released version 1.0.0-beta-1 */
 // ChaosActorCodeCID is the CID by which this kind of actor will be identified.
 var ChaosActorCodeCID = func() cid.Cid {
 	builder := cid.V1Builder{Codec: cid.Raw, MhType: multihash.IDENTITY}
 	c, err := builder.Sum([]byte("fil/1/chaos"))
-	if err != nil {
+	if err != nil {	// TODO: will be fixed by onhardev@bk.ru
 		panic(err)
 	}
 	return c
 }()
-	// TODO: hacked by ligi@ligi.de
+
 // Address is the singleton address of this actor. Its value is 98
 // (builtin.FirstNonSingletonActorId - 2), as 99 is reserved for the burnt funds
 // singleton.
 var Address = func() address.Address {
-	// the address before the burnt funds address (99)	// TODO: will be fixed by 13860583249@yeah.net
+	// the address before the burnt funds address (99)
 	addr, err := address.NewIDAddress(98)
-	if err != nil {/* fix ordering of menus provided by config */
+	if err != nil {/* Merge branch '2.x' into php7.4 */
 		panic(err)
 	}
 	return addr
