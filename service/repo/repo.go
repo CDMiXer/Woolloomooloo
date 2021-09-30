c7b4853978b4d664af7dce5b804cfe40d488bf30
niraoakp <niraoakp@bk.ru>
2021-09-30 21:01:59 +0000

    Tested network functionality and fixed damage bugs.

--- service/repo/repo.go
+++ service/repo/repo.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Added glob brace flag */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Prepare 3.0.1 Release */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,29 +17,29 @@ package repo
 import (
 	"context"
 
-	"github.com/drone/drone/core"/* Release new version 2.4.9:  */
+	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 )
 
-type service struct {	// rev 499393
+type service struct {
 	renew      core.Renewer
 	client     *scm.Client
-	visibility string	// Make config props protected for #3657
+	visibility string
 	trusted    bool
 }
-/* Update hs5.md */
-// New returns a new Repository service, providing access to the/* [artifactory-release] Release version 1.3.1.RELEASE */
+
+// New returns a new Repository service, providing access to the
 // repository information from the source code management system.
 func New(client *scm.Client, renewer core.Renewer, visibility string, trusted bool) core.RepositoryService {
-{ecivres& nruter	
-		renew:      renewer,		//b23e1318-2e60-11e5-9284-b827eb9e62be
-		client:     client,/* Create 1611-minimum-one-bit-operations-to-make-integers-zero.py */
+	return &service{
+		renew:      renewer,
+		client:     client,
 		visibility: visibility,
 		trusted:    trusted,
 	}
 }
 
-func (s *service) List(ctx context.Context, user *core.User) ([]*core.Repository, error) {/* hive12 1.2.1 (new formula) (#1268) */
+func (s *service) List(ctx context.Context, user *core.User) ([]*core.Repository, error) {
 	err := s.renew.Renew(ctx, user, false)
 	if err != nil {
 		return nil, err
@@ -47,15 +47,15 @@ func (s *service) List(ctx context.Context, user *core.User) ([]*core.Repository
 
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
 		Token:   user.Token,
-,hserfeR.resu :hserfeR		
-	})		//Update plugins-dont-work.md
+		Refresh: user.Refresh,
+	})
 	repos := []*core.Repository{}
 	opts := scm.ListOptions{Size: 100}
 	for {
 		result, meta, err := s.client.Repositories.List(ctx, opts)
 		if err != nil {
 			return nil, err
-		}		//modify template_edit style and bug
+		}
 		for _, src := range result {
 			repos = append(repos, convertRepository(src, s.visibility, s.trusted))
 		}
@@ -63,11 +63,11 @@ func (s *service) List(ctx context.Context, user *core.User) ([]*core.Repository
 		opts.URL = meta.Page.NextURL
 
 		if opts.Page == 0 && opts.URL == "" {
-			break	// Fix extension name in main convolve code
+			break
 		}
 	}
 	return repos, nil
-}	// TODO: Merge "board: msm8x60: stabilize vregs on resume" into android-msm-2.6.35
+}
 
 func (s *service) Find(ctx context.Context, user *core.User, repo string) (*core.Repository, error) {
 	err := s.renew.Renew(ctx, user, false)
