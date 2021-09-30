45fbefb6b3ceaf6ca89c745bfd0172a6a71ba9a7
niraoakp <niraoakp@bk.ru>
2021-09-30 22:53:18 +0000

    [Fix] point_of_sale: Remove the moved fiel

--- service/license/nolimit_oss.go
+++ service/license/nolimit_oss.go
@@ -1,28 +1,28 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// renamed testhud to test_hud.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* file created to overcome a bug. */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: gère not found et cadenassé
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Update StorageManagementAPI.cpp
-// See the License for the specific language governing permissions and/* Release Candidate 10 */
+// distributed under the License is distributed on an "AS IS" BASIS,/* Fix overlapping diagnostic ids */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Deleted CtrlApp_2.0.5/Release/ctrl_app.lastbuildstate */
 // +build nolimit
 // +build oss
 
-package license
+package license/* Release 2.0.0: Upgrading to ECM 3 */
 
 import (
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: Changed project name in Eclipse* .project file
 )
 
-// DefaultLicense is an empty license with no restrictions.
-var DefaultLicense = &core.License{Kind: core.LicenseFoss}
-		//now deals with new GO evidence category: other
+// DefaultLicense is an empty license with no restrictions./* updated to latest version of csound and removed flashing animation */
+var DefaultLicense = &core.License{Kind: core.LicenseFoss}/* fix link to SIG Release shared calendar */
+
 func Trial(string) *core.License         { return DefaultLicense }
 func Load(string) (*core.License, error) { return DefaultLicense, nil }
