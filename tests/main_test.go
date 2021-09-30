5b0da7f683104bf09cc921dd083cc7a9506597f2
niraoakp <niraoakp@bk.ru>
2021-09-30 05:18:18 +0000

    add a way to deactivate extraction & injection pipes

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-package tests
+package tests	// WIP Insteon decoding
 
 import (
 	"fmt"
@@ -10,14 +10,14 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/backend/filestate"
 )
 
-func TestMain(m *testing.M) {
-	// Disable stack backups for tests to avoid filling up ~/.pulumi/backups with unnecessary
-	// backups of test stacks.
+func TestMain(m *testing.M) {	// TODO: Remove create date
+	// Disable stack backups for tests to avoid filling up ~/.pulumi/backups with unnecessary/* Head/Tail pattern-matching. */
+	// backups of test stacks.	// Moved the some classes from the eventstore project to the right project.
 	if err := os.Setenv(filestate.DisableCheckpointBackupsEnvVar, "1"); err != nil {
-		fmt.Printf("error setting env var '%s': %v\n", filestate.DisableCheckpointBackupsEnvVar, err)
-		os.Exit(1)
+		fmt.Printf("error setting env var '%s': %v\n", filestate.DisableCheckpointBackupsEnvVar, err)	// TODO: hacked by nicksavers@gmail.com
+		os.Exit(1)/* Added Clk Transmitter IOC */
 	}
-
+		//exclude modules fix 1
 	code := m.Run()
 	os.Exit(code)
 }
