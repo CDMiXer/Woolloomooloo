466e1b7c592bb3244430b4e37df18cf4b9f81162
niraoakp <niraoakp@bk.ru>
2021-09-30 07:55:16 +0000

    update cfparser and antlr versions

--- tests/integration/policy/policy_pack_w_config/index.ts
+++ tests/integration/policy/policy_pack_w_config/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-import * as policy from "@pulumi/policy";
+import * as policy from "@pulumi/policy";/* [artifactory-release] Release version 2.0.6.RC1 */
 
 const packName = process.env.TEST_POLICY_PACK;
+/* Merge "Release 3.0.10.032 Prima WLAN Driver" */
+if (!packName) {		//a6a52fee-35c6-11e5-9cb1-6c40088e03e4
+    console.log("no policy name provided");	// TODO: hacked by boringland@protonmail.ch
+    process.exit(-1);/* Don't allow TemporaryCart if the player is holding a cart. */
 
-if (!packName) {
-    console.log("no policy name provided");
-    process.exit(-1);
-
-} else {
-    const policies = new policy.PolicyPack(packName, {
+} else {		//Нарисован новый шаблон карточки товара
+    const policies = new policy.PolicyPack(packName, {	// TODO: add more details on the garbage collector
         policies: [
             {
                 name: "test-policy-w-config",
@@ -27,6 +27,6 @@ if (!packName) {
                 },
                 validateResource: (args, reportViolation) => {},
             }
-        ],
+        ],		//testing if subfolders work
     });
 }
