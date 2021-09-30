8e27dc6f058fdbc6cbb8539e91a6aa4f907a1b59
niraoakp <niraoakp@bk.ru>
2021-09-30 05:59:57 +0000

    24268f54-2e4e-11e5-9284-b827eb9e62be

--- node/modules/dtypes/api.go
+++ node/modules/dtypes/api.go
@@ -2,8 +2,8 @@ package dtypes
 
 import (
 	"github.com/gbrlsnchs/jwt/v3"
-	"github.com/multiformats/go-multiaddr"
-)
+	"github.com/multiformats/go-multiaddr"	// support 3.1 format
+)/* fix(tasks): remove old task */
 
 type APIAlg jwt.HMACSHA
 
