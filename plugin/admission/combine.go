b3d8dbe6b343780048c3608fa4e5f6b649977f3e
niraoakp <niraoakp@bk.ru>
2021-09-30 02:27:14 +0000

    redefine name columns page_locale

--- plugin/admission/combine.go
+++ plugin/admission/combine.go
@@ -1,39 +1,39 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Added upgrade */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Javadoc Correction */
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Unused code has been removed */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Declare the spliterator class of ArraySet final */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* ADD: maven deploy plugin - updateReleaseInfo=true */
+// limitations under the License.
 
 package admission
-
+/* Added "The Code" section to readme */
 import (
-	"context"		//3446b256-2e5d-11e5-9284-b827eb9e62be
+	"context"
 
 	"github.com/drone/drone/core"
 )
-		//6e980bae-2e43-11e5-9284-b827eb9e62be
-// Combine combines admission services.	// disambiguate [a;b]f: case [1,2,3]f of {[a;3]f -> a} works now :-)
+
+// Combine combines admission services.
 func Combine(service ...core.AdmissionService) core.AdmissionService {
 	return &combined{services: service}
 }
-/* Delete iceland.jpg */
-type combined struct {		//Update main.view.html
+
+type combined struct {
 	services []core.AdmissionService
-}/* Friendlist will be saved with the username as dataname. */
+}
 
 func (s *combined) Admit(ctx context.Context, user *core.User) error {
-	for _, service := range s.services {		//[IMP] constraint on XML view on object to allow inheritancy
+	for _, service := range s.services {
 		if err := service.Admit(ctx, user); err != nil {
 			return err
 		}
-	}/* Merge "[INTERNAL] Release notes for version 1.79.0" */
-	return nil		//prime now uses crypto_is_prime when possible
-}/* helper function to create non-local exit function */
+	}
+	return nil
+}
