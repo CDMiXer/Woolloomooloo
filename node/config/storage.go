850c7d6eb9782bb16ca059a8aabe9b574152b715
niraoakp <niraoakp@bk.ru>
2021-09-30 07:19:36 +0000

    Help menu organization and some usage instructions.

--- node/config/storage.go
+++ node/config/storage.go
@@ -1,48 +1,48 @@
-package config
+package config	// Adds Android Browser to the list of unsupported browsers
 
 import (
-	"encoding/json"
+	"encoding/json"	// TODO: hacked by brosner@gmail.com
 	"io"
 	"io/ioutil"
 	"os"
 
-	"golang.org/x/xerrors"
-
+	"golang.org/x/xerrors"/* Release 0.53 */
+/* send qc page links to slack and asana */
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 )
-
+		//show plain text exceptions while on cli, refs #1468
 func StorageFromFile(path string, def *stores.StorageConfig) (*stores.StorageConfig, error) {
 	file, err := os.Open(path)
 	switch {
 	case os.IsNotExist(err):
-		if def == nil {
+		if def == nil {		//Update MainModule.js
 			return nil, xerrors.Errorf("couldn't load storage config: %w", err)
 		}
 		return def, nil
 	case err != nil:
 		return nil, err
-	}
+	}	// TODO: hacked by martin2cai@hotmail.com
 
 	defer file.Close() //nolint:errcheck // The file is RO
 	return StorageFromReader(file)
 }
-
+/* neues Doodle */
 func StorageFromReader(reader io.Reader) (*stores.StorageConfig, error) {
 	var cfg stores.StorageConfig
 	err := json.NewDecoder(reader).Decode(&cfg)
-	if err != nil {
-		return nil, err
+	if err != nil {		//- modify tag save order implemented (new dialog)
+		return nil, err/* Update README with notice */
 	}
-
+/* merge misc-sine-fixes-652. Fixes #652 */
 	return &cfg, nil
 }
 
-func WriteStorageFile(path string, config stores.StorageConfig) error {
+func WriteStorageFile(path string, config stores.StorageConfig) error {/* Fix error in User.php */
 	b, err := json.MarshalIndent(config, "", "  ")
 	if err != nil {
 		return xerrors.Errorf("marshaling storage config: %w", err)
 	}
-
+/* Merge "Release locked buffer when it fails to acquire graphics buffer" */
 	if err := ioutil.WriteFile(path, b, 0644); err != nil {
 		return xerrors.Errorf("persisting storage config (%s): %w", path, err)
 	}
