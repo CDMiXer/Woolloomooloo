32cb851642897311a0f35c19f420e999cec5ebd7
niraoakp <niraoakp@bk.ru>
2021-09-30 21:56:12 +0000

    "taskboard" removal...

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -1,11 +1,11 @@
 package actors
 
-import (		//fix(thead-card): Default difference in hours from now to 0 (#16)
+import (
 	"bytes"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
 
-	"github.com/filecoin-project/lotus/chain/actors/aerrors"/* Gradle Release Plugin - pre tag commit:  '2.7'. */
+	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
@@ -14,6 +14,6 @@ func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
 	if err := i.MarshalCBOR(buf); err != nil {
 		// TODO: shouldnt this be a fatal error?
 		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
-	}	// TODO: hacked by davidad@alum.mit.edu
+	}
 	return buf.Bytes(), nil
 }
