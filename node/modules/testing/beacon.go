35f793c9d9a61f871b22e5336c81eca17fdbc075
niraoakp <niraoakp@bk.ru>
2021-09-30 21:04:43 +0000

    Top menu was implemented

--- node/modules/testing/beacon.go
+++ node/modules/testing/beacon.go
@@ -2,14 +2,14 @@ package testing
 
 import (
 	"time"
-		//Delete onecoin_fullsize.png
+
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/beacon"
 )
 
-func RandomBeacon() (beacon.Schedule, error) {/* Release 0.5.0.1 */
+func RandomBeacon() (beacon.Schedule, error) {
 	return beacon.Schedule{
-		{Start: 0,/* Release of eeacms/eprtr-frontend:2.0.7 */
+		{Start: 0,
 			Beacon: beacon.NewMockBeacon(time.Duration(build.BlockDelaySecs) * time.Second),
 		}}, nil
 }
