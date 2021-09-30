1dd9e84f4de8714955d1e45aef0a2fedff5e18cf
niraoakp <niraoakp@bk.ru>
2021-09-30 02:10:14 +0000

    rename component to conform to original API

--- tests/integration/dynamic/python/__main__.py
+++ tests/integration/dynamic/python/__main__.py
@@ -1,21 +1,21 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* setTpTw and Data */
 import binascii
 import os
-from pulumi import ComponentResource, export
+from pulumi import ComponentResource, export		//Ignore IDEA dir
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
-class RandomResourceProvider(ResourceProvider):
+class RandomResourceProvider(ResourceProvider):	// TODO: Create roomhelp.js
     def create(self, props):
         val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
-        return CreateResult(val, { "val": val })
-		//PosLength and SwitchSensor query.
+        return CreateResult(val, { "val": val })/* 5.3.6 Release */
+
 class Random(Resource):
     val: str
     def __init__(self, name, opts = None):
-        super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)	// TODO: hacked by greg@colvin.org
+        super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
 
 r = Random("foo")
 
-export("random_id", r.id)	// TODO: hacked by nagydani@epointsystem.org
+export("random_id", r.id)
 export("random_val", r.val)
