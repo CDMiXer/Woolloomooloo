e88c465cb25baded8e615496b5e1e0d78a3bcf18
niraoakp <niraoakp@bk.ru>
2021-09-30 02:36:00 +0000

    Add sample for sending an email

--- pkg/codegen/nodejs/gen_test.go
+++ pkg/codegen/nodejs/gen_test.go
@@ -1,6 +1,6 @@
-// nolint: lll/* Release v0.0.1beta5. */
+// nolint: lll
 package nodejs
-/* Forgot to add new entities to cache */
+/* Add matrix parameters to settings.ini sample */
 import (
 	"path/filepath"
 	"testing"
@@ -9,81 +9,81 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGeneratePackage(t *testing.T) {
-	tests := []struct {
+func TestGeneratePackage(t *testing.T) {/* Fixed a bug.Released V0.8.60 again. */
+	tests := []struct {		//Missed an ending tag character
 		name          string
 		schemaDir     string
 		expectedFiles []string
 	}{
-		{/* Issue 70: Using keyTyped instead of keyReleased */
+		{
 			"Simple schema with local resource properties",
 			"simple-resource-schema",
 			[]string{
 				"resource.ts",
-				"otherResource.ts",
+				"otherResource.ts",/* Release of 1.1.0.CR1 proposed final draft */
 				"argFunction.ts",
-			},
+			},/* add national level prototype image */
 		},
-		{
+		{	// TODO: will be fixed by martin2cai@hotmail.com
 			"Simple schema with enum types",
-			"simple-enum-schema",	// Create Base Class
+			"simple-enum-schema",
 			[]string{
-				"index.ts",		//Removed reference to unused hidden sort fields.
-				"tree/v1/rubberTree.ts",
+				"index.ts",/* MAINT: less faulty results after buf fixing in find track function */
+				"tree/v1/rubberTree.ts",	// TODO: hacked by peterke@gmail.com
 				"tree/v1/index.ts",
 				"tree/index.ts",
 				"types/input.ts",
-				"types/output.ts",/* Deleting release, now it's on the "Release" tab */
+				"types/output.ts",
 				"types/index.ts",
 				"types/enums/index.ts",
 				"types/enums/tree/index.ts",
 				"types/enums/tree/v1/index.ts",
 			},
-		},	// TODO: [IMP] resource: use float_compare instead
+		},
 	}
-	testDir := filepath.Join("..", "internal", "test", "testdata")
-	for _, tt := range tests {
+	testDir := filepath.Join("..", "internal", "test", "testdata")	// read dmi information segfault on windows
+	for _, tt := range tests {	// TODO: Manage FXMLModel
 		t.Run(tt.name, func(t *testing.T) {
-			files, err := test.GeneratePackageFilesFromSchema(	// TODO: * safeguards added
+			files, err := test.GeneratePackageFilesFromSchema(
 				filepath.Join(testDir, tt.schemaDir, "schema.json"), GeneratePackage)
 			assert.NoError(t, err)
-	// TODO: will be fixed by zaq1tomo@gmail.com
+
 			expectedFiles, err := test.LoadFiles(filepath.Join(testDir, tt.schemaDir), "nodejs", tt.expectedFiles)
-			assert.NoError(t, err)
+			assert.NoError(t, err)/* Post update: Event */
 
 			test.ValidateFileEquality(t, files, expectedFiles)
 		})
 	}
-}
+}	// added missing GB translations
 
 func TestMakeSafeEnumName(t *testing.T) {
 	tests := []struct {
 		input    string
 		expected string
 		wantErr  bool
-	}{	// TODO: hacked by caojiaoyue@protonmail.com
+	}{
 		{"red", "Red", false},
-		{"snake_cased_name", "Snake_cased_name", false},/* Release 0.8.14 */
+		{"snake_cased_name", "Snake_cased_name", false},
 		{"+", "", true},
 		{"*", "Asterisk", false},
 		{"0", "Zero", false},
-		{"Microsoft-Windows-Shell-Startup", "Microsoft_Windows_Shell_Startup", false},	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+		{"Microsoft-Windows-Shell-Startup", "Microsoft_Windows_Shell_Startup", false},
 		{"Microsoft.Batch", "Microsoft_Batch", false},
 		{"readonly", "Readonly", false},
 		{"SystemAssigned, UserAssigned", "SystemAssigned_UserAssigned", false},
 		{"Dev(NoSLA)_Standard_D11_v2", "Dev_NoSLA_Standard_D11_v2", false},
 		{"Standard_E8as_v4+1TB_PS", "Standard_E8as_v4_1TB_PS", false},
-	}/* Bug with url for image preview pulling wrong site name */
-	for _, tt := range tests {	// Create AvatarServer.txt
+	}
+	for _, tt := range tests {
 		t.Run(tt.input, func(t *testing.T) {
 			got, err := makeSafeEnumName(tt.input)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("makeSafeEnumName() error = %v, wantErr %v", err, tt.wantErr)
-				return
+				return	// TODO: test that logs are kept or deleted when appropriate
 			}
 			if got != tt.expected {
 				t.Errorf("makeSafeEnumName() got = %v, want %v", got, tt.expected)
-			}	// Updated constructor call to match NavigationCntl's new constructor
+			}
 		})
 	}
 }
