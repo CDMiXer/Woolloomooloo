8893ff0e2944cd7d6c6ade27a04be179c6686c16
niraoakp <niraoakp@bk.ru>
2021-09-30 15:35:45 +0000

    log_exec: use class UniqueSocketDescriptor

--- store/shared/migrate/sqlite/ddl.go
+++ store/shared/migrate/sqlite/ddl.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Released v.1.2.0.2 */
+// you may not use this file except in compliance with the License./* Update Release system */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Release new version 2.5.18: Minor changes */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release version 4.2.0.RELEASE */
+// distributed under the License is distributed on an "AS IS" BASIS,		//Deprecate old readme info
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//update review by authenticated user ok
+
 package sqlite
 
 //go:generate togo ddl -package sqlite -dialect sqlite3
