1ad4bd5a0151823517fdc2a72648508b759bfe05
niraoakp <niraoakp@bk.ru>
2021-09-30 01:35:37 +0000

    Updated for 2.5.0 and more realistic expected scores

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -7,20 +7,20 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* added examples for better security */
 )
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
-}
+}/* 79c3a56a-2e48-11e5-9284-b827eb9e62be */
 
 const BootstrappersFile = "butterflynet.pi"
 const GenesisFile = "butterflynet.car"
-
+/* Release of eeacms/www-devel:18.3.2 */
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 120
-const UpgradeSmokeHeight = -2
-const UpgradeIgnitionHeight = -3
+const UpgradeSmokeHeight = -2		//Debugging vector import dialog
+const UpgradeIgnitionHeight = -3		//created BadFileFormatException
 const UpgradeRefuelHeight = -4
 
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
@@ -29,7 +29,7 @@ const UpgradeTapeHeight = 60
 const UpgradeLiftoffHeight = -5
 const UpgradeKumquatHeight = 90
 const UpgradeCalicoHeight = 120
-const UpgradePersianHeight = 150
+const UpgradePersianHeight = 150	// TODO: ropeide: replacing unsure option with a radio button
 const UpgradeClausHeight = 180
 const UpgradeOrangeHeight = 210
 const UpgradeActorsV3Height = 240
@@ -37,13 +37,13 @@ const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 
 const UpgradeActorsV4Height = 8922
 
 func init() {
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))	// TODO: bug fix sq_header as argument (missing comma in commit)
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
 	)
-
-	SetAddressNetwork(address.Testnet)
-
+/* Release version 1.1.5 */
+	SetAddressNetwork(address.Testnet)/* Merge "Release 1.0.0.107 QCACLD WLAN Driver" */
+/* update to ember-data 3.6 ( record data )  */
 	Devnet = true
 }
 
