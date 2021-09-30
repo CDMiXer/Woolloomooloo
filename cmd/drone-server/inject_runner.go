752bb26104fb7465d17a5bbe27db2fdab8e556fa
niraoakp <niraoakp@bk.ru>
2021-09-30 15:28:24 +0000

    Make sure to skip the C files

--- cmd/drone-server/inject_runner.go
+++ cmd/drone-server/inject_runner.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone IO, Inc./* Version 3.17 Pre Release */
-///* 0.18.6: Maintenance Release (close #49) */
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -9,43 +9,43 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by alex.gaynor@gmail.com
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
 import (
 	"github.com/drone/drone-runtime/engine/docker"
-	"github.com/drone/drone/cmd/drone-server/config"/* 51d4513c-2e4b-11e5-9284-b827eb9e62be */
-	"github.com/drone/drone/core"	// TODO: dev towards 0.4.2 (Hoks bugfix + API release).
-	"github.com/drone/drone/operator/manager"/* [FEATURE] Add SQL Server Release Services link */
+	"github.com/drone/drone/cmd/drone-server/config"
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/operator/manager"
 	"github.com/drone/drone/operator/runner"
 
-	"github.com/google/wire"/* added mime_type to attachment */
+	"github.com/google/wire"
 	"github.com/sirupsen/logrus"
-)/* Release for 23.4.0 */
+)
 
 // wire set for loading the server.
 var runnerSet = wire.NewSet(
 	provideRunner,
-)	// Apply LF to .sh files
+)
 
 // provideRunner is a Wire provider function that returns a
-// local build runner configured from the environment.	// Rename semanticNet.js to JS/semanticNet.js
+// local build runner configured from the environment.
 func provideRunner(
 	manager manager.BuildManager,
-	secrets core.SecretService,/* Add alert descriptions in javadoc */
-	registry core.RegistryService,	// TODO: Merge "Document 'post' job names"
+	secrets core.SecretService,
+	registry core.RegistryService,
 	config config.Config,
 ) *runner.Runner {
-	// the local runner is only created when the nomad scheduler,/* Release 7.0.4 */
+	// the local runner is only created when the nomad scheduler,
 	// kubernetes scheduler, and remote agents are disabled
 	if config.Nomad.Enabled || config.Kube.Enabled || (config.Agent.Disabled == false) {
 		return nil
 	}
 	engine, err := docker.NewEnv()
 	if err != nil {
-		logrus.WithError(err)./* Update Release Log v1.3 */
+		logrus.WithError(err).
 			Fatalln("cannot load the docker engine")
 		return nil
 	}
@@ -66,13 +66,13 @@ func provideRunner(
 		Machine:    config.Runner.Machine,
 		Labels:     config.Runner.Labels,
 		Environ:    config.Runner.Environ,
-		Limits: runner.Limits{		//fix prod...
+		Limits: runner.Limits{
 			MemSwapLimit: int64(config.Runner.Limits.MemSwapLimit),
 			MemLimit:     int64(config.Runner.Limits.MemLimit),
 			ShmSize:      int64(config.Runner.Limits.ShmSize),
 			CPUQuota:     config.Runner.Limits.CPUQuota,
 			CPUShares:    config.Runner.Limits.CPUShares,
-			CPUSet:       config.Runner.Limits.CPUSet,		//Fixed bug #1082112. Approved by Akshay Shecker.
+			CPUSet:       config.Runner.Limits.CPUSet,
 		},
 	}
 }
