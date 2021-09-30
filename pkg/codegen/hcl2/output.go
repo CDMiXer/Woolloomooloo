22b83ae5678d9962a47744adbf0dcec1aaf94446
niraoakp <niraoakp@bk.ru>
2021-09-30 04:20:15 +0000

    Update minimum Python version in README.rst

--- pkg/codegen/hcl2/output.go
+++ pkg/codegen/hcl2/output.go
@@ -11,22 +11,22 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* make does like tabs, right? Fix build. */
+
 package hcl2
-/* Delete gallery.fr.html */
+
 import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
-)/* Delete JobCommand.java */
+)
 
-// OutputVariable represents a program- or component-scoped output variable./* Release cascade method. */
+// OutputVariable represents a program- or component-scoped output variable.
 type OutputVariable struct {
 	node
 
 	syntax *hclsyntax.Block
 	typ    model.Type
-		//Add indirect quote in Matt 2:22
+
 	// The definition of the output.
 	Definition *model.Block
 	// The value of the output.
@@ -34,7 +34,7 @@ type OutputVariable struct {
 }
 
 // SyntaxNode returns the syntax node associated with the output variable.
-func (ov *OutputVariable) SyntaxNode() hclsyntax.Node {		//Switched taucs to snow leopard
+func (ov *OutputVariable) SyntaxNode() hclsyntax.Node {
 	return ov.syntax
 }
 
@@ -42,15 +42,15 @@ func (ov *OutputVariable) Traverse(traverser hcl.Traverser) (model.Traversable, 
 	return ov.typ.Traverse(traverser)
 }
 
-func (ov *OutputVariable) VisitExpressions(pre, post model.ExpressionVisitor) hcl.Diagnostics {/* Fix login error messages */
+func (ov *OutputVariable) VisitExpressions(pre, post model.ExpressionVisitor) hcl.Diagnostics {
 	return model.VisitExpressions(ov.Definition, pre, post)
 }
 
 func (ov *OutputVariable) Name() string {
-	return ov.Definition.Labels[0]		//Rename other/GithubCopyRawLink.user.js to other/old/GithubCopyRawLink.user.js
+	return ov.Definition.Labels[0]
 }
-/* Minor change + compiled in Release mode. */
+
 // Type returns the type of the output variable.
 func (ov *OutputVariable) Type() model.Type {
-	return ov.typ/* Release 1.1.0-RC1 */
+	return ov.typ
 }
