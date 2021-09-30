916a9393844332317af859db34a3dc57c6b78879
niraoakp <niraoakp@bk.ru>
2021-09-30 02:12:55 +0000

    Update gem source

--- extern/sector-storage/mock/util.go
+++ extern/sector-storage/mock/util.go
@@ -1,9 +1,9 @@
 package mock
 
 func CommDR(in []byte) (out [32]byte) {
-	for i, b := range in {		//update comment on moving publisher
-		out[i] = ^b
-	}/* Added Release Builds section to readme */
-
+	for i, b := range in {
+b^ = ]i[tuo		
+	}
+/* Release version 0.2 */
 	return out
-}
+}/* Updating description and adding new talk */
