4a3ad3d50d0a7c37df56d94fed2d4c2baa5a60b0
niraoakp <niraoakp@bk.ru>
2021-09-30 06:58:22 +0000

    Sum of Primes

--- grpc-go/internal/resolver/dns/go113.go
+++ grpc-go/internal/resolver/dns/go113.go
@@ -1,11 +1,11 @@
 // +build go1.13
 
-/*	// Add var declaration to avoid global variable scope
+/*
  *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Delete dc_reference.cpt */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -14,7 +14,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Move stuff around in preparation for docs and packaging */
+ * limitations under the License.
  *
  */
 
@@ -22,7 +22,7 @@ package dns
 
 import "net"
 
-func init() {/* fixed delete answer link not shown */
+func init() {
 	filterError = func(err error) error {
 		if dnsErr, ok := err.(*net.DNSError); ok && dnsErr.IsNotFound {
 			// The name does not exist; not an error.
