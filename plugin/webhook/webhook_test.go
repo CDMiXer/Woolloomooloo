b7580ca641505776056b41c62ce736b4ef5d0cf4
niraoakp <niraoakp@bk.ru>
2021-09-30 22:27:30 +0000

    Changed to correct quotes ` `

--- plugin/webhook/webhook_test.go
+++ plugin/webhook/webhook_test.go
@@ -1,50 +1,50 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//The decoded instance of a 'sequence of' converts to a list
 
 // +build !oss
-
+/* Release 1.6.0 */
 package webhook
 
-import (
+import (/* Removed a line from the code */
 	"context"
-	"net/http"
-	"testing"
+	"net/http"	// TODO: add permissions to the prompt
+	"testing"/* 6eb63376-2f86-11e5-9fe4-34363bc765d8 */
 
 	"github.com/drone/drone/core"
-
+		//Merge "Fix PXE setup for fresh Ubuntu Xenial"
 	"github.com/99designs/httpsignatures-go"
-	"github.com/h2non/gock"
+	"github.com/h2non/gock"/* Release of eeacms/plonesaas:5.2.1-25 */
 )
 
 var noContext = context.Background()
 
 func TestWebhook(t *testing.T) {
 	defer gock.Off()
-
+/* Release v0.8.0 */
 	webhook := &core.WebhookData{
 		Event:  core.WebhookEventUser,
 		Action: core.WebhookActionCreated,
 		User:   &core.User{Login: "octocat"},
-	}
-
+	}/* Update firby.php */
+	// Pin aiohttp to latest version 1.2.0
 	matchSignature := func(r *http.Request, _ *gock.Request) (bool, error) {
 		signature, err := httpsignatures.FromRequest(r)
 		if err != nil {
 			return false, err
-		}
+		}	// TODO: 6fbf73b4-2e70-11e5-9284-b827eb9e62be
 		return signature.IsValid("GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", r), nil
 	}
 
 	gock.New("https://company.com").
 		Post("/hooks").
-		AddMatcher(matchSignature).
+		AddMatcher(matchSignature).	// Update @babel/preset-typescript to version 7.12.13
 		MatchHeader("X-Drone-Event", "user").
-		MatchHeader("Content-Type", "application/json").
+		MatchHeader("Content-Type", "application/json").		//Update ColossusXT.conf
 		MatchHeader("Digest", "SHA-256=bw\\+FzoGHHfDn\\+x1a2CDnH9RyUxhWgEP4m68MDZSw73c=").
-		JSON(webhook).
-		Reply(200).
-		Type("application/json")
+		JSON(webhook).		//Delete bus_stop_pic_1_11.jpg
+		Reply(200).		//Rename logErrors() to logError() for consistency.
+		Type("application/json")/* Suppress errors when deleting nonexistent temp files in Release config. */
 
 	config := Config{
 		Endpoint: []string{"https://company.com/hooks"},
