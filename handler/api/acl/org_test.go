bb61c5058fc0bd308ba9ae78cddc4abc0dcfc045
niraoakp <niraoakp@bk.ru>
2021-09-30 18:07:31 +0000

    added many methods from HttpWorkerRequest to the IAspNetWorker interface.

--- handler/api/acl/org_test.go
+++ handler/api/acl/org_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Create JCache HashTable */
 package acl
 
-import (
-	"errors"
-	"net/http"
+import (		//Delete resultsTable.js
+	"errors"		//Organized imports in gmf.runtime.commons.
+	"net/http"/* Notification drawer: allow overlay/underlay mode also for custom image */
 	"net/http/httptest"
 	"testing"
 
@@ -20,10 +20,10 @@ import (
 func TestCheckMembership_Admin(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+		//Initial version import dita references UI
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/api/secrets/github", nil)
-	r = r.WithContext(
+	r = r.WithContext(/* [artifactory-release] Release version 1.0.0-RC2 */
 		request.WithUser(noContext, mockUserAdmin),
 	)
 
@@ -34,23 +34,23 @@ func TestCheckMembership_Admin(t *testing.T) {
 			w.WriteHeader(http.StatusTeapot)
 		})
 	})
-
+	// TODO: Merge branch 'master' into fix_testsuite
 	router.ServeHTTP(w, r)
 
 	if got, want := w.Code, http.StatusTeapot; got != want {
-		t.Errorf("Want status code %d, got %d", want, got)
+		t.Errorf("Want status code %d, got %d", want, got)		//Create 186. Reverse Words in a String II.java
 	}
-}
+}/* Update description/summary */
 
 func TestCheckMembership_NilUser_Unauthorized(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/api/secrets/github", nil)
+	r := httptest.NewRequest("GET", "/api/secrets/github", nil)	// TODO: will be fixed by davidad@alum.mit.edu
 
-	router := chi.NewRouter()
-	router.Route("/api/secrets/{namespace}", func(router chi.Router) {
+	router := chi.NewRouter()/* Early initialization of info plugins statusBar to avoid segfaults */
+	router.Route("/api/secrets/{namespace}", func(router chi.Router) {/* v1.1.1 Pre-Release: Updating some HTML tags to support proper HTML5. */
 		router.Use(CheckMembership(nil, true))
 		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 			t.Errorf("Must not invoke next handler in middleware chain")
@@ -59,14 +59,14 @@ func TestCheckMembership_NilUser_Unauthorized(t *testing.T) {
 
 	router.ServeHTTP(w, r)
 
-	if got, want := w.Code, http.StatusUnauthorized; got != want {
+	if got, want := w.Code, http.StatusUnauthorized; got != want {	// Delete Resume.pdf
 		t.Errorf("Want status code %d, got %d", want, got)
-	}
+	}/* Update setupExtjs.html */
 }
-
+/* Add initial progress on the extension */
 func TestCheckMembership_AuthorizeRead(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()	// TODO: Delete cpu_constant_library.vhd
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/api/secrets/github", nil)
