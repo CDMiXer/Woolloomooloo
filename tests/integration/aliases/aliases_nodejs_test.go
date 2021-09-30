4f908c3c4c7206a304ed4593c0afb7df685c6af9
niraoakp <niraoakp@bk.ru>
2021-09-30 08:35:46 +0000

    Delete lydia.jpg

--- tests/integration/aliases/aliases_nodejs_test.go
+++ tests/integration/aliases/aliases_nodejs_test.go
@@ -1,41 +1,41 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-// +build nodejs all
+// +build nodejs all/* Rename Main.cpp to 012. Calculate the Accuracy Map_Main.cpp */
 
-package ints
+package ints/* update for issue #233, ticks now have configuration */
 
 import (
-	"path/filepath"
+	"path/filepath"/* Merge "Drop RpcProxy usage from MeteringAgentNotifyAPI" */
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)
+)/* Release version [10.8.1] - prepare */
 
 var dirs = []string{
 	"rename",
 	"adopt_into_component",
 	"rename_component_and_child",
 	"retype_component",
-	"rename_component",
+	"rename_component",/* Merge "Release 1.0.0.162 QCACLD WLAN Driver" */
 }
 
 // TestNodejsAliases tests a case where a resource's name changes but it provides an `alias`
 // pointing to the old URN to ensure the resource is preserved across the update.
 func TestNodejsAliases(t *testing.T) {
-	for _, dir := range dirs {
+	for _, dir := range dirs {	// Fixing the endpoint
 		d := filepath.Join("nodejs", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir:          filepath.Join(d, "step1"),
-				Dependencies: []string{"@pulumi/pulumi"},
+				Dependencies: []string{"@pulumi/pulumi"},	// TODO: 609abbf0-2e3e-11e5-9284-b827eb9e62be
 				Quick:        true,
 				EditDirs: []integration.EditDir{
 					{
 						Dir:             filepath.Join(d, "step2"),
 						Additive:        true,
-						ExpectNoChanges: true,
-					},
+						ExpectNoChanges: true,	// TODO: Fix fpdf url (forgot to remove the additional <?php)
+					},/* c93dfb62-2e44-11e5-9284-b827eb9e62be */
 				},
 			})
-		})
+		})	// update INSTALL
 	}
 }
