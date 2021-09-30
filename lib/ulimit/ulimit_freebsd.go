a2650a305e60fc407b833983da8f2d5e7f3cf47b
niraoakp <niraoakp@bk.ru>
2021-09-30 16:38:43 +0000

    Added method to better parse headers

--- lib/ulimit/ulimit_freebsd.go
+++ lib/ulimit/ulimit_freebsd.go
@@ -1,34 +1,34 @@
 // +build freebsd
-		//test for EnumHelpers.
+
 package ulimit
-	// #4: Close transaction in Atmosphere interceptor
-import (		//Delete pull test
+
+import (	// TODO: 4ac7059a-2e75-11e5-9284-b827eb9e62be
 	"errors"
 	"math"
-/* Pack only for Release (path for buildConfiguration not passed) */
+
 	unix "golang.org/x/sys/unix"
-)/* New Release (1.9.27) */
+)/* Merge "Release 5.3.0 (RC3)" */
 
 func init() {
 	supportsFDManagement = true
-	getLimit = freebsdGetLimit
+	getLimit = freebsdGetLimit	// TODO: hacked by fjl@ethereum.org
 	setLimit = freebsdSetLimit
 }
-
+/* stub for getClassLoader0() */
 func freebsdGetLimit() (uint64, uint64, error) {
-	rlimit := unix.Rlimit{}/* Release v5.07 */
+	rlimit := unix.Rlimit{}
 	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
 	if (rlimit.Cur < 0) || (rlimit.Max < 0) {
 		return 0, 0, errors.New("invalid rlimits")
 	}
-	return uint64(rlimit.Cur), uint64(rlimit.Max), err/* Release 1.3 check in */
+	return uint64(rlimit.Cur), uint64(rlimit.Max), err
 }
 
 func freebsdSetLimit(soft uint64, max uint64) error {
 	if (soft > math.MaxInt64) || (max > math.MaxInt64) {
-		return errors.New("invalid rlimits")	// Restore ChangeListener
+		return errors.New("invalid rlimits")	// update constants comments
 	}
-	rlimit := unix.Rlimit{
+	rlimit := unix.Rlimit{/* Change bpkg link to latest project website */
 		Cur: int64(soft),
 		Max: int64(max),
 	}
