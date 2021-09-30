6f61ad9da728c9b858f900f87bed70e328219ee8
niraoakp <niraoakp@bk.ru>
2021-09-30 09:39:36 +0000

    Fixed issue 5, was due to bad read timeout management in IoSession.idle.

--- tests/integration/stack_outputs/dotnet/Program.cs
+++ tests/integration/stack_outputs/dotnet/Program.cs
@@ -1,20 +1,20 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-/* Horseshoes now Render */
-using System.Collections.Generic;
+
+using System.Collections.Generic;/* Non blocking error when load/save resolved */
 using System.Threading.Tasks;
 using Pulumi;
 
 class Program
-{
+{	// add tests for `up` in zipper exercism
     static Task<int> Main(string[] args)
     {
-        return Deployment.RunAsync(() => /* Release version 0.1.2 */
+        return Deployment.RunAsync(() => 
         {
             return new Dictionary<string, object>
             {
                 {  "xyz", "ABC" },
                 {  "foo", 42 },
             };
-        });/* changed some tokens from NXT to NFD */
+        });
     }
 }
