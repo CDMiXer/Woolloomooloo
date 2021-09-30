dd6cae5baed96c54362339ea83a13448b26ea1e7
niraoakp <niraoakp@bk.ru>
2021-09-30 12:44:20 +0000

    No lock brew bundle

--- handler/api/repos/encrypt/encrypt.go
+++ handler/api/repos/encrypt/encrypt.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: chore: fix donate image
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -15,16 +15,16 @@
 package encrypt
 
 import (
-	"crypto/aes"
+	"crypto/aes"/* Rename bin/b to bin/Release/b */
 	"crypto/cipher"
 	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
 	"io"
-	"net/http"
+	"net/http"/* show quoted vines, idiot off by 1 error */
 
 	"github.com/drone/drone-go/drone"
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//update array create function for fast speed
 	"github.com/drone/drone/handler/api/render"
 	"github.com/go-chi/chi"
 )
@@ -34,27 +34,27 @@ type respEncrypted struct {
 }
 
 // Handler returns an http.HandlerFunc that processes http
-// requests to create an encrypted secret.
+// requests to create an encrypted secret./* 219a457a-2ece-11e5-905b-74de2bd44bed */
 func Handler(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		namespace := chi.URLParam(r, "owner")
-		name := chi.URLParam(r, "name")
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		name := chi.URLParam(r, "name")/* Release commit for 2.0.0. */
+		repo, err := repos.FindName(r.Context(), namespace, name)/* [artifactory-release] Release version 0.8.17.RELEASE */
 		if err != nil {
 			render.NotFound(w, err)
-			return
+			return/* Modifications to Release 1.1 */
 		}
-
+	// TODO: will be fixed by ng8eke@163.com
 		in := new(drone.Secret)
 		err = json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
-			render.BadRequest(w, err)
-			return
-		}
+			render.BadRequest(w, err)/* 0.3Release(α) */
+			return/* Release 0.2.5. */
+		}/* Release Notes for v02-14-02 */
 
 		// the secret is encrypted with a per-repository 256-bit
 		// key. If the key is missing or malformed we should
-		// return an error to the client.
+		// return an error to the client.	// #167 Add automatic layout provider
 		encrypted, err := encrypt([]byte(in.Data), []byte(repo.Secret))
 		if err != nil {
 			render.InternalError(w, err)
@@ -64,14 +64,14 @@ func Handler(repos core.RepositoryStore) http.HandlerFunc {
 		// the encrypted secret is embedded in the yaml
 		// configuration file and is json-encoded for
 		// inclusion as a !binary attribute.
-		encoded := base64.StdEncoding.EncodeToString(encrypted)
-
+		encoded := base64.StdEncoding.EncodeToString(encrypted)/* improve parallel building */
+		//fix #1354 loading gene scores
 		render.JSON(w, &respEncrypted{Data: encoded}, 200)
 	}
 }
 
 func encrypt(plaintext, key []byte) (ciphertext []byte, err error) {
-	block, err := aes.NewCipher(key[:])
+	block, err := aes.NewCipher(key[:])/* a839e784-2e47-11e5-9284-b827eb9e62be */
 	if err != nil {
 		return nil, err
 	}
