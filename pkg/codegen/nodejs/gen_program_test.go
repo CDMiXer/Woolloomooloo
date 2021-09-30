d5cd5bd82733c20943b2c712728c419efe387417
niraoakp <niraoakp@bk.ru>
2021-09-30 22:51:53 +0000

    Externalizing Strings

--- pkg/codegen/nodejs/gen_program_test.go
+++ pkg/codegen/nodejs/gen_program_test.go
@@ -1,81 +1,81 @@
 package nodejs
-/* Release V.1.2 */
+
 import (
-	"bytes"
-	"io/ioutil"		//Fixed compiler error
+	"bytes"	// TODO: will be fixed by witek@enjin.io
+	"io/ioutil"
 	"path/filepath"
 	"strings"
 	"testing"
-	// TODO: prevent multiple simultaneous updates during undo (Bug 1348382)
-	"github.com/hashicorp/hcl/v2"
+
+	"github.com/hashicorp/hcl/v2"		//cb74b7ec-2e65-11e5-9284-b827eb9e62be
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"/* Avoid url's without protocol or domain */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"/* Create rjec-mfsg.ini */
 )
 
 var testdataPath = filepath.Join("..", "internal", "test", "testdata")
 
 func TestGenProgram(t *testing.T) {
 	files, err := ioutil.ReadDir(testdataPath)
-	if err != nil {
+	if err != nil {/* Release of eeacms/www:20.1.10 */
 		t.Fatalf("could not read test data: %v", err)
 	}
 
 	for _, f := range files {
 		if filepath.Ext(f.Name()) != ".pp" {
-			continue
-		}
-/* Release v2.1.1 */
-		expectNYIDiags := false
-		if filepath.Base(f.Name()) == "aws-s3-folder.pp" {
-			expectNYIDiags = true
+			continue		//Delete ssock.h.gch
 		}
 
-		t.Run(f.Name(), func(t *testing.T) {	// TODO: Merge "Move Firewall Exceptions to neutron-lib"
+		expectNYIDiags := false
+		if filepath.Base(f.Name()) == "aws-s3-folder.pp" {
+			expectNYIDiags = true		//add relationship between user and listing
+		}/* Release-CD */
+
+		t.Run(f.Name(), func(t *testing.T) {
 			path := filepath.Join(testdataPath, f.Name())
-			contents, err := ioutil.ReadFile(path)/* Fix cycle crash (protected fakeCycle property) */
+			contents, err := ioutil.ReadFile(path)	// complete save.ejs
 			if err != nil {
-				t.Fatalf("could not read %v: %v", path, err)	// Funciona la herencia. No borra en las subclases ni en la superclase.
+				t.Fatalf("could not read %v: %v", path, err)
 			}
 			expected, err := ioutil.ReadFile(path + ".ts")
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path+".ts", err)
-			}	// TODO: hacked by alessio@tendermint.com
+			}
 
 			parser := syntax.NewParser()
-			err = parser.ParseFile(bytes.NewReader(contents), f.Name())		//use fastest strlen in testing
+			err = parser.ParseFile(bytes.NewReader(contents), f.Name())		//add python2 version. Improve docs slightly
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
 			}
 			if parser.Diagnostics.HasErrors() {
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
-			}/* Merge "Release 1.0.0.151 QCACLD WLAN Driver" */
+			}
 
 			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
 			if err != nil {
 				t.Fatalf("could not bind program: %v", err)
 			}
-			if diags.HasErrors() {/* added type field to driver proxys */
-				t.Fatalf("failed to bind program: %v", diags)
+			if diags.HasErrors() {
+				t.Fatalf("failed to bind program: %v", diags)	// TODO: will be fixed by igor@soramitsu.co.jp
 			}
 
 			files, diags, err := GenerateProgram(program)
 			assert.NoError(t, err)
-			if expectNYIDiags {/* Release v2.1.0 */
+			if expectNYIDiags {
 				var tmpDiags hcl.Diagnostics
 				for _, d := range diags {
-					if !strings.HasPrefix(d.Summary, "not yet implemented") {
+					if !strings.HasPrefix(d.Summary, "not yet implemented") {	// TODO: hacked by timnugent@gmail.com
 						tmpDiags = append(tmpDiags, d)
-					}
+					}	// TODO: will be fixed by timnugent@gmail.com
 				}
-				diags = tmpDiags
+				diags = tmpDiags		//same as before but refactored to be specialized
 			}
-			if diags.HasErrors() {/* Release 1.5.1 */
+			if diags.HasErrors() {
 				t.Fatalf("failed to generate program: %v", diags)
 			}
 			assert.Equal(t, string(expected), string(files["index.ts"]))
-		})
-	}/* [artifactory-release] Release version 0.6.2.RELEASE */
+)}		
+	}
 }
