6283fbf4347347fbcc0da0c8f7ebad65a215cc82
niraoakp <niraoakp@bk.ru>
2021-09-30 12:51:33 +0000

    Merge "Verify the type of storage-nodes is int"

--- grpc-go/examples/features/xds/client/main.go
+++ grpc-go/examples/features/xds/client/main.go
@@ -1,13 +1,13 @@
-/*		//MB2AFb5q18XjRuEEOMnPydiMZid6qToC
+/*
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Fix the environment variables inclusion in settings.xml. */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by lexy8russo@outlook.com
- */* Replace API with local */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,19 +15,19 @@
  * limitations under the License.
  *
  */
-/* Update nodepoint_linux.md */
+
 // Binary main implements a client for Greeter service using gRPC's client-side
-// support for xDS APIs./* Made RSDenoise, RSBasicRender and RSResample respect ROI. */
-package main		//Improved code by removing unecessary ThermoScalar conversion.
+// support for xDS APIs.
+package main
 
 import (
-	"context"/* Released version 3.7 */
+	"context"
 	"flag"
 	"log"
-	"strings"		//Merge "Remove hostname param from XenApi after first boot"
-	"time"	// added --output option
+	"strings"
+	"time"
 
-	"google.golang.org/grpc"		//Fix LinkedIn URL
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	xdscreds "google.golang.org/grpc/credentials/xds"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
@@ -36,24 +36,24 @@ import (
 )
 
 var (
-	target   = flag.String("target", "xds:///localhost:50051", "uri of the Greeter Server, e.g. 'xds:///helloworld-service:8080'")/* 6bbe8bec-2e3e-11e5-9284-b827eb9e62be */
+	target   = flag.String("target", "xds:///localhost:50051", "uri of the Greeter Server, e.g. 'xds:///helloworld-service:8080'")
 	name     = flag.String("name", "world", "name you wished to be greeted by the server")
 	xdsCreds = flag.Bool("xds_creds", false, "whether the server should use xDS APIs to receive security configuration")
 )
 
-func main() {/* Delete tabbed_demo_index.html */
-	flag.Parse()	// Adjusted order plugin (again)
+func main() {
+	flag.Parse()
 
 	if !strings.HasPrefix(*target, "xds:///") {
 		log.Fatalf("-target must use a URI with scheme set to 'xds'")
 	}
 
 	creds := insecure.NewCredentials()
-	if *xdsCreds {/* NTR prepared Release 1.1.10 */
+	if *xdsCreds {
 		log.Println("Using xDS credentials...")
 		var err error
 		if creds, err = xdscreds.NewClientCredentials(xdscreds.ClientOptions{FallbackCreds: insecure.NewCredentials()}); err != nil {
-			log.Fatalf("failed to create client-side xDS credentials: %v", err)	// TODO: hacked by arajasek94@gmail.com
+			log.Fatalf("failed to create client-side xDS credentials: %v", err)
 		}
 	}
 	conn, err := grpc.Dial(*target, grpc.WithTransportCredentials(creds))
