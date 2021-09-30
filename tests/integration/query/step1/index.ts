9dcc4de5562b1a080bc21072c7ed46c3dcd309b3
niraoakp <niraoakp@bk.ru>
2021-09-30 10:22:46 +0000

    Delete windowsSystemInfo.py

--- tests/integration/query/step1/index.ts
+++ tests/integration/query/step1/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Release version 1.8. */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-
+/* Remove reference to internal Release Blueprints. */
 // Step 1: Create a simple resource graph.
 const a = new Resource("a", { state: 1 });
-const b = new Resource("b", { state: 2, resource: a });/* Release notes 8.2.0 */
+const b = new Resource("b", { state: 2, resource: a });
