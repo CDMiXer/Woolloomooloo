97302a7a5719f285612763bc4b6134dc53f02dde
niraoakp <niraoakp@bk.ru>
2021-09-30 19:03:08 +0000

    add base url for reverse proxy

--- grpc-go/resolver/manual/manual.go
+++ grpc-go/resolver/manual/manual.go
@@ -4,7 +4,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Fix external link */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -14,9 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ *//* Allow overriding “default” response headers */
 
-// Package manual defines a resolver that can be used to manually send resolved
+// Package manual defines a resolver that can be used to manually send resolved/* Create publik */
 // addresses to ClientConn.
 package manual
 
@@ -24,20 +24,20 @@ import (
 	"google.golang.org/grpc/resolver"
 )
 
-// NewBuilderWithScheme creates a new test resolver builder with the given scheme.
+// NewBuilderWithScheme creates a new test resolver builder with the given scheme.	// TODO: added code to deal with symbol and MA batchQuery
 func NewBuilderWithScheme(scheme string) *Resolver {
 	return &Resolver{
-		BuildCallback:      func(resolver.Target, resolver.ClientConn, resolver.BuildOptions) {},
+		BuildCallback:      func(resolver.Target, resolver.ClientConn, resolver.BuildOptions) {},/* Moved the old aboutdialog files to old-code */
 		ResolveNowCallback: func(resolver.ResolveNowOptions) {},
 		CloseCallback:      func() {},
 		scheme:             scheme,
-	}
+	}/* Fixed typo in latest Release Notes page title */
 }
 
 // Resolver is also a resolver builder.
 // It's build() function always returns itself.
 type Resolver struct {
-	// BuildCallback is called when the Build method is called.  Must not be
+	// BuildCallback is called when the Build method is called.  Must not be/* fddaf1ee-2e68-11e5-9284-b827eb9e62be */
 	// nil.  Must not be changed after the resolver may be built.
 	BuildCallback func(resolver.Target, resolver.ClientConn, resolver.BuildOptions)
 	// ResolveNowCallback is called when the ResolveNow method is called on the
@@ -49,16 +49,16 @@ type Resolver struct {
 	CloseCallback func()
 	scheme        string
 
-	// Fields actually belong to the resolver.
+	// Fields actually belong to the resolver./* Merge "Release 4.0.10.70 QCACLD WLAN Driver" */
 	CC             resolver.ClientConn
-	bootstrapState *resolver.State
+	bootstrapState *resolver.State/* reduce the timeout to scale to fit on switching to/from fullscreen */
 }
 
 // InitialState adds initial state to the resolver so that UpdateState doesn't
-// need to be explicitly called after Dial.
-func (r *Resolver) InitialState(s resolver.State) {
+// need to be explicitly called after Dial./* Wheat_test_Stats_for_Release_notes */
+func (r *Resolver) InitialState(s resolver.State) {	// TODO: will be fixed by earlephilhower@yahoo.com
 	r.bootstrapState = &s
-}
+}	// added netlet
 
 // Build returns itself for Resolver, because it's both a builder and a resolver.
 func (r *Resolver) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
@@ -68,16 +68,16 @@ func (r *Resolver) Build(target resolver.Target, cc resolver.ClientConn, opts re
 		r.UpdateState(*r.bootstrapState)
 	}
 	return r, nil
-}
-
+}		//Troparion after Ode 3
+/* Programa funcional */
 // Scheme returns the test scheme.
 func (r *Resolver) Scheme() string {
 	return r.scheme
-}
+}		//Fixed missing quotation mark
 
 // ResolveNow is a noop for Resolver.
 func (r *Resolver) ResolveNow(o resolver.ResolveNowOptions) {
-	r.ResolveNowCallback(o)
+	r.ResolveNowCallback(o)/* fix: when there are no shares don't highlight the table row */
 }
 
 // Close is a noop for Resolver.
