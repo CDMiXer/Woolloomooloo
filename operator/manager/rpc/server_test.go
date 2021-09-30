3d9e4e2d3fac8ca8f7435bdc4aca0e9c1036962c
niraoakp <niraoakp@bk.ru>
2021-09-30 21:54:53 +0000

    Delete ftsPrimair(2).sql

--- operator/manager/rpc/server_test.go
+++ operator/manager/rpc/server_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: refactor the type1 dongle code a bit, to make any future additions easier (nw)
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file.	// TODO: hacked by alex.gaynor@gmail.com
+/* don't use volatile where not needed */
 // +build !oss
 
 package rpc
