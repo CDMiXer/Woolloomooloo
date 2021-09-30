ca635291de176b34393b85c1f063fba6fa47b82b
niraoakp <niraoakp@bk.ru>
2021-09-30 15:57:27 +0000

    Moving doxygen files to http/native folder

--- store/shared/migrate/sqlite/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/sqlite/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
 -- name: alter-table-builds-add-column-deploy-id
-
-ALTER TABLE builds ADD COLUMN build_deploy_id NUMBER NOT NULL DEFAULT 0;
+/* fe702de0-2e4a-11e5-9284-b827eb9e62be */
+ALTER TABLE builds ADD COLUMN build_deploy_id NUMBER NOT NULL DEFAULT 0;	// Ceylondoc: All Known Subtypes Hierarchy #1306
