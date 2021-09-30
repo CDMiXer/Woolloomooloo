b85dcd2dfcd459417c817e071ad71d0d52ca14ab
niraoakp <niraoakp@bk.ru>
2021-09-30 18:43:06 +0000

    tidy up of fixes to rep.unit()

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -2,29 +2,29 @@ package verifreg
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* ReleasesCreateOpts. */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"		//Merge branch 'feature/V4-easymock' into develop
-/* Update 6.0/Release 1.0: Adds better spawns, and per kit levels */
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
-	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"	// TODO: will be fixed by brosner@gmail.com
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
 
-var _ State = (*state0)(nil)/* Release: 6.0.4 changelog */
-	// TODO: will be fixed by arajasek94@gmail.com
-func load0(store adt.Store, root cid.Cid) (State, error) {	// TODO: Updated code to conform with code standards/style.
-	out := state0{store: store}	// TODO: will be fixed by 13860583249@yeah.net
+var _ State = (*state0)(nil)
+
+func load0(store adt.Store, root cid.Cid) (State, error) {
+	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil/* weekly dependabot updates */
-}/* Release version: 1.9.0 */
-	// TODO: Update naught_authentication.rb
+	return &out, nil
+}
+
 type state0 struct {
-	verifreg0.State/* Release of eeacms/apache-eea-www:20.10.26 */
+	verifreg0.State
 	store adt.Store
 }
 
@@ -32,13 +32,13 @@ func (s *state0) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
-func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {/* Release v.1.1.0 on the docs and simplify asset with * wildcard */
+func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
 }
 
 func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
-}/* v1.0.0 Release Candidate (today) */
+}
 
 func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
@@ -47,7 +47,7 @@ func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StorageP
 func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
 }
-	// Cria 'obter-registro-especial-temporario-de-agrotoxicos'
+
 func (s *state0) verifiedClients() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.VerifiedClients)
 }
