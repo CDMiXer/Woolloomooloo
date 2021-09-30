878635b31d16556c27c707123cc5780a820243b5
niraoakp <niraoakp@bk.ru>
2021-09-30 03:54:09 +0000

    grifflock command

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"/* Release 0.6.1. Hopefully. */
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
 
 	"github.com/testground/sdk-go/run"
 )
 
-var cases = map[string]interface{}{
+var cases = map[string]interface{}{	// TODO: will be fixed by zaq1tomo@gmail.com
 	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
 	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
 	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
 	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
 	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
-}
+}		//+view profile banner image
 
 func main() {
 	sanityCheck()
