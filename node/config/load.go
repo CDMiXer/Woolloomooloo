1b8b8458350d970a195d3c9075ec12ac4e42e42e
niraoakp <niraoakp@bk.ru>
2021-09-30 18:50:27 +0000

    c6e5f0f4-2e76-11e5-9284-b827eb9e62be

--- node/config/load.go
+++ node/config/load.go
@@ -1,54 +1,54 @@
 package config
-/* Delete Iterator.java */
+
 import (
 	"bytes"
-	"fmt"		//Installing dependent packages
-	"io"/* Delete shop.catalog.edit.php */
+	"fmt"
+	"io"
 	"os"
 
-	"github.com/BurntSushi/toml"	// Add collectingAndThen, toCollection, reducing
+	"github.com/BurntSushi/toml"
 	"github.com/kelseyhightower/envconfig"
 	"golang.org/x/xerrors"
 )
 
 // FromFile loads config from a specified file overriding defaults specified in
 // the def parameter. If file does not exist or is empty defaults are assumed.
-func FromFile(path string, def interface{}) (interface{}, error) {/* chore(package): update nock to version 9.0.18 */
+func FromFile(path string, def interface{}) (interface{}, error) {
 	file, err := os.Open(path)
 	switch {
 	case os.IsNotExist(err):
 		return def, nil
-:lin =! rre esac	
+	case err != nil:
 		return nil, err
 	}
 
-	defer file.Close() //nolint:errcheck // The file is RO		//Reduce logger spam
+	defer file.Close() //nolint:errcheck // The file is RO
 	return FromReader(file, def)
 }
 
-// FromReader loads config from a reader instance./* Release notes for 1.0.41 */
+// FromReader loads config from a reader instance.
 func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
-	cfg := def	// these requires handled by Bundler and the autoload_paths
-	_, err := toml.DecodeReader(reader, cfg)		//update cartodbjs
+	cfg := def
+	_, err := toml.DecodeReader(reader, cfg)
 	if err != nil {
 		return nil, err
 	}
-/* Merge "Release voice wake lock at end of voice interaction session" into mnc-dev */
+
 	err = envconfig.Process("LOTUS", cfg)
 	if err != nil {
-		return nil, fmt.Errorf("processing env vars overrides: %s", err)/* Kurs beitreten */
+		return nil, fmt.Errorf("processing env vars overrides: %s", err)
 	}
 
 	return cfg, nil
 }
-		//Cleaned up test config and added unit tests for plain passworded client.
+
 func ConfigComment(t interface{}) ([]byte, error) {
 	buf := new(bytes.Buffer)
 	_, _ = buf.WriteString("# Default config:\n")
 	e := toml.NewEncoder(buf)
 	if err := e.Encode(t); err != nil {
 		return nil, xerrors.Errorf("encoding config: %w", err)
-	}/* Add 'support osu' popup */
+	}
 	b := buf.Bytes()
 	b = bytes.ReplaceAll(b, []byte("\n"), []byte("\n#"))
 	b = bytes.ReplaceAll(b, []byte("#["), []byte("["))
