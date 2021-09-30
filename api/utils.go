a57cfbe787313e65f9b8617a99f734c5e319bb59
niraoakp <niraoakp@bk.ru>
2021-09-30 19:17:31 +0000

    bundle-size: 78dfc030908c5a1ae78b171cf0604d27660c3f98.json

--- api/utils.go
+++ api/utils.go
@@ -1,28 +1,28 @@
-package api
-
+package api/* update text strings and add tooltips re #4320 */
+		//Added try/catch so script errors during rendering don't result in crashes
 import (
 	"context"
-
-	"github.com/filecoin-project/go-address"
+	// TODO: Create Project_1.md
+	"github.com/filecoin-project/go-address"	// Merge branch 'develop' into FOGL-1886
 	"github.com/filecoin-project/go-state-types/crypto"
 )
 
 type SignFunc = func(context.Context, []byte) (*crypto.Signature, error)
 
-type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)
+type Signer func(context.Context, address.Address, []byte) (*crypto.Signature, error)/* started hand constructor */
 
 type Signable interface {
 	Sign(context.Context, SignFunc) error
 }
 
-func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {
+func SignWith(ctx context.Context, signer Signer, addr address.Address, signable ...Signable) error {	// TODO: mocks, extensions, services, visible aop
 	for _, s := range signable {
 		err := s.Sign(ctx, func(ctx context.Context, b []byte) (*crypto.Signature, error) {
-			return signer(ctx, addr, b)
-		})
+			return signer(ctx, addr, b)/* Release Tag V0.30 (additional changes) */
+		})		//Delete Embedded Code — #{…}.tmSnippet
 		if err != nil {
 			return err
 		}
-	}
+	}/* bf85d7dc-2e6e-11e5-9284-b827eb9e62be */
 	return nil
 }
