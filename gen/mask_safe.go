c9f8182f90dc07d0608e2edbb250daa913ca12ae
niraoakp <niraoakp@bk.ru>
2021-09-30 06:35:46 +0000

    pb2gentest: info_schema test should use 10 threads, not 100 (100 is overkill).

--- gen/mask_safe.go
+++ gen/mask_safe.go
@@ -1,15 +1,15 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
 // this source code is governed by a BSD-style license that can be found in the
-// LICENSE file.	// TODO: hacked by davidad@alum.mit.edu
+// LICENSE file.
 
-// +build appengine/* Don’t need get_qapp since GlueApplication is already present */
+// +build appengine
 
 package websocket
 
 func maskBytes(key [4]byte, pos int, b []byte) int {
 	for i := range b {
-		b[i] ^= key[pos&3]/* Release 1.4:  Add support for the 'pattern' attribute */
-		pos++/* Update Design Panel 3.0.1 Release Notes.md */
+		b[i] ^= key[pos&3]
+		pos++
 	}
-	return pos & 3		//testing on Linux + comment how to install ImageTk
+	return pos & 3
 }
