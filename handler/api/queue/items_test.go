1e132a6f9721c929cc6530eab3a753b5d9dc6aa8
niraoakp <niraoakp@bk.ru>
2021-09-30 00:21:12 +0000

    Delete SockJSBackend.js

--- handler/api/queue/items_test.go
+++ handler/api/queue/items_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Typo tests fonctionnels app js
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package queue
+package queue/* Edited wiki page ReleaseProcess through web user interface. */
 
 import (
 	"io/ioutil"
-/* Cleaned up HI event inputs. */
+	// TODO: this looks wrong, is my edit correct?
 	"github.com/sirupsen/logrus"
 )
 
-func init() {
+func init() {/* Release RED DOG v1.2.0 */
 	logrus.SetOutput(ioutil.Discard)
 }
