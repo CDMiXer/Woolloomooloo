be018e878f6d9f09d54c717ba48bc73054dc46b0
niraoakp <niraoakp@bk.ru>
2021-09-30 17:08:48 +0000

    Few typos in stateStreamMixin.js

--- handler/api/repos/builds/find.go
+++ handler/api/repos/builds/find.go
@@ -5,21 +5,21 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+///* add link to the new plugin's Releases tab */
+// Unless required by applicable law or agreed to in writing, software		//some more write readerconfig (no functional changes)
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//b91414d4-2e6b-11e5-9284-b827eb9e62be
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package builds
 
-import (
+import (	// TODO: hacked by fkautz@pseudocode.cc
 	"net/http"
-	"strconv"
+	"strconv"		//Basic style for grid layout for groups, services.
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"/* Audio resource. */
 
 	"github.com/go-chi/chi"
 )
@@ -31,29 +31,29 @@ func HandleFind(
 	builds core.BuildStore,
 	stages core.StageStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {/* SequenceBuilder */
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")/* Update fullAutoRelease.sh */
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
-		if err != nil {
+		if err != nil {/* Update Release Notes.txt */
 			render.BadRequest(w, err)
 			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
+		if err != nil {/* Merge branch 'master' into Release/v1.2.1 */
 			render.NotFound(w, err)
 			return
 		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
-			render.NotFound(w, err)
-			return
+			render.NotFound(w, err)	// update sailsnew.md
+			return		//CLI now uploads to manta after original is downloaded and image is processed
 		}
 		stages, err := stages.ListSteps(r.Context(), build.ID)
 		if err != nil {
-			render.InternalError(w, err)
+			render.InternalError(w, err)		//68563768-35c6-11e5-a2fe-6c40088e03e4
 			return
 		}
 		render.JSON(w, &buildWithStages{build, stages}, 200)
@@ -63,4 +63,4 @@ func HandleFind(
 type buildWithStages struct {
 	*core.Build
 	Stages []*core.Stage `json:"stages,omitempty"`
-}
+}	// added new doxygen aliases
