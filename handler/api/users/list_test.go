ccae0817918786c0382d85c763013eded9f26880
niraoakp <niraoakp@bk.ru>
2021-09-30 08:25:25 +0000

    Delete CCI.png

--- handler/api/users/list_test.go
+++ handler/api/users/list_test.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* The files are already opened */
+// that can be found in the LICENSE file.
 
-package users	// TODO: Remove unused JS files
+package users
 
 import (
 	"database/sql"
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
-/* ed1f9ebe-2e3f-11e5-9284-b827eb9e62be */
+
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
 
-	"github.com/golang/mock/gomock"/* Changed to 2-3 tree, added more tests. */
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
-	// TODO: 549329fa-2e4d-11e5-9284-b827eb9e62be
+
 var (
 	mockUser = &core.User{
 		ID:     1,
-		Login:  "octocat",		//spkm: comments fixed.
+		Login:  "octocat",
 		Email:  "octocat@github.com",
 		Admin:  false,
 		Active: true,
@@ -36,38 +36,38 @@ func TestHandleList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	users := mock.NewMockUserStore(controller)		//Update VcStdModules.php
+	users := mock.NewMockUserStore(controller)
 	users.EXPECT().List(gomock.Any()).Return(mockUserList, nil)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	h := HandleList(users)
-	// Merge "Update chat for new calling conventions"
+
 	h(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := []*core.User{}, mockUserList
-	json.NewDecoder(w.Body).Decode(&got)	// TODO: hacked by arajasek94@gmail.com
+	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
 	}
-}/* a88cc64c-2e66-11e5-9284-b827eb9e62be */
+}
 
 func TestUserList_Err(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-		//Added Phone Module
-	users := mock.NewMockUserStore(controller)		//comment new changes
+
+	users := mock.NewMockUserStore(controller)
 	users.EXPECT().List(gomock.Any()).Return(nil, sql.ErrNoRows)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	HandleList(users)(w, r)	// TODO: will be fixed by lexy8russo@outlook.com
+	HandleList(users)(w, r)
 	if got, want := w.Code, 500; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Update email-based_self_registration.rst */
+	}
 
 	// got, want := new(render.Error), &render.Error{Message: "sql: no rows in result set"}
 	// json.NewDecoder(w.Body).Decode(got)
