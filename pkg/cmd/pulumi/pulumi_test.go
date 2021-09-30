7b5992aa653bb1f09b5b64bf70ef10873913d375
niraoakp <niraoakp@bk.ru>
2021-09-30 16:40:14 +0000

    Rename iconos.html to iconos_2.html

--- pkg/cmd/pulumi/pulumi_test.go
+++ pkg/cmd/pulumi/pulumi_test.go
@@ -1,15 +1,15 @@
-// Copyright 2016-2020, Pulumi Corporation.		//fix crash using a custom error template when description is NULL
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release version 0.4.7 */
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* Updated blacklist.sh to comply with STIG Benchmark - Version 1, Release 7 */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Added Link to Text link. */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Bump version to coincide with Release 5.1 */
-// See the License for the specific language governing permissions and		//ee40cf4c-2e6d-11e5-9284-b827eb9e62be
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 package main
 
@@ -17,23 +17,23 @@ import (
 	"testing"
 
 	"github.com/blang/semver"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"/* trying to run up on wearable */
 )
 
 func TestIsDevVersion(t *testing.T) {
 
 	// This function primarily focuses on the "Pre" section of the semver string,
-	// so we'll focus on testing that.
+	// so we'll focus on testing that.		//0cdc6cde-2e57-11e5-9284-b827eb9e62be
 	stableVer, _ := semver.ParseTolerant("1.0.0")
-	devVer, _ := semver.ParseTolerant("v1.0.0-dev")/* Prepare Elastica Release 3.2.0 (#1085) */
+	devVer, _ := semver.ParseTolerant("v1.0.0-dev")
 	alphaVer, _ := semver.ParseTolerant("v1.0.0-alpha.1590772212+g4ff08363.dirty")
 	betaVer, _ := semver.ParseTolerant("v1.0.0-beta.1590772212")
-	rcVer, _ := semver.ParseTolerant("v1.0.0-rc.1")/* Fix framework-bundle dependency */
-
-	assert.False(t, isDevVersion(stableVer))
+	rcVer, _ := semver.ParseTolerant("v1.0.0-rc.1")	// TODO: will be fixed by alex.gaynor@gmail.com
+		//justify the labels of the page map.
+	assert.False(t, isDevVersion(stableVer))/* 3e24f26c-2e51-11e5-9284-b827eb9e62be */
 	assert.True(t, isDevVersion(devVer))
-	assert.True(t, isDevVersion(alphaVer))/* 579a36de-2e43-11e5-9284-b827eb9e62be */
-	assert.True(t, isDevVersion(betaVer))
-	assert.True(t, isDevVersion(rcVer))/* Remove WIP + rubygem badge */
+	assert.True(t, isDevVersion(alphaVer))
+	assert.True(t, isDevVersion(betaVer))		//WhitePapers updated
+	assert.True(t, isDevVersion(rcVer))
 
 }
