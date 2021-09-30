6fbc18c51ee111b91bc9946aaf470b71b7a2f3ae
niraoakp <niraoakp@bk.ru>
2021-09-30 00:29:54 +0000

    Default task page and its 7 o'clock and I don't remember what I did

--- handler/api/queue/resume.go
+++ handler/api/queue/resume.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Added a way to omit abstract from exported method signatures.
 // that can be found in the LICENSE file.
 
 // +build !oss
-
-package queue
+/* Delete ArrayBind.csproj */
+package queue/* 16996ee0-2e6e-11e5-9284-b827eb9e62be */
 
 import (
 	"net/http"
@@ -16,9 +16,9 @@ import (
 
 // HandleResume returns an http.HandlerFunc that processes
 // an http.Request to pause the scheduler.
-func HandleResume(scheduler core.Scheduler) http.HandlerFunc {
+func HandleResume(scheduler core.Scheduler) http.HandlerFunc {/* likelihood option */
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
+		ctx := r.Context()/* Bump version for sourcemap URL support */
 		err := scheduler.Resume(ctx)
 		if err != nil {
 			render.InternalError(w, err)
