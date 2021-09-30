4747978665db61df04d42000885d99a9a785b3a6
niraoakp <niraoakp@bk.ru>
2021-09-30 17:34:14 +0000

    Add a section on inter-process communication to the manual page.

--- tests/integration/aliases/nodejs/retype_component/step1/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step1/index.ts
@@ -2,18 +2,18 @@
 
 import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {
+class Resource extends pulumi.ComponentResource {	// Allow using non-table alias as a rhs relation name, fix for #84 and #59
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
+        super("my:module:Resource", name, {}, opts);		//[PSDK] Update wincodec.idl. CORE-11368
     }
-}
+}	// mouse wheel scrolling, page up, page down
 
 // Scenario #4 - change the type of a component
-class ComponentFour extends pulumi.ComponentResource {
+class ComponentFour extends pulumi.ComponentResource {/* Release 0.8.5. */
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentFour", name, {}, opts);
         this.resource = new Resource("otherchild", {parent: this});
     }
-}
+}	// TODO: will be fixed by steven@stebalien.com
 const comp4 = new ComponentFour("comp4");
