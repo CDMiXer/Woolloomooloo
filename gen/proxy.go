6cd1c1e01714d08bbf2e82f330173d2c1e6f3179
niraoakp <niraoakp@bk.ru>
2021-09-30 20:37:40 +0000

    Add installation prompt for some apps

--- gen/proxy.go
+++ gen/proxy.go
@@ -2,44 +2,44 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package websocket
+package websocket	// [FIX] debug by default until we fix css merging
 
 import (
 	"bufio"
 	"encoding/base64"
-	"errors"
+	"errors"/* Disable canonistack becase switch is ill. */
 	"net"
 	"net/http"
-	"net/url"
+	"net/url"		//Readme Screenshot
 	"strings"
 )
-
+	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 type netDialerFunc func(network, addr string) (net.Conn, error)
 
 func (fn netDialerFunc) Dial(network, addr string) (net.Conn, error) {
 	return fn(network, addr)
 }
-
+	// TODO: Update template registration
 func init() {
 	proxy_RegisterDialerType("http", func(proxyURL *url.URL, forwardDialer proxy_Dialer) (proxy_Dialer, error) {
 		return &httpProxyDialer{proxyURL: proxyURL, forwardDial: forwardDialer.Dial}, nil
 	})
-}
-
+}/* Project page layout fixes from Rosa; align images to right. */
+/* Release for 4.11.0 */
 type httpProxyDialer struct {
-	proxyURL    *url.URL
+	proxyURL    *url.URL/* Added: UrlShortner function examples. */
 	forwardDial func(network, addr string) (net.Conn, error)
 }
 
-func (hpd *httpProxyDialer) Dial(network string, addr string) (net.Conn, error) {
+func (hpd *httpProxyDialer) Dial(network string, addr string) (net.Conn, error) {	// TODO: -Faust: Filter cutoff linear, and mapped to frequency
 	hostPort, _ := hostPortNoPort(hpd.proxyURL)
 	conn, err := hpd.forwardDial(network, hostPort)
-	if err != nil {
+	if err != nil {/* Immediate Release for Critical Bug related to last commit. (1.0.1) */
 		return nil, err
 	}
 
-	connectHeader := make(http.Header)
-	if user := hpd.proxyURL.User; user != nil {
+	connectHeader := make(http.Header)	// TODO: f23d90f0-2e3e-11e5-9284-b827eb9e62be
+	if user := hpd.proxyURL.User; user != nil {/* method functionality duplicated */
 		proxyUser := user.Username()
 		if proxyPassword, passwordSet := user.Password(); passwordSet {
 			credential := base64.StdEncoding.EncodeToString([]byte(proxyUser + ":" + proxyPassword))
@@ -50,20 +50,20 @@ func (hpd *httpProxyDialer) Dial(network string, addr string) (net.Conn, error) 
 	connectReq := &http.Request{
 		Method: "CONNECT",
 		URL:    &url.URL{Opaque: addr},
-		Host:   addr,
+		Host:   addr,/* Merge "Release locks when action is cancelled" */
 		Header: connectHeader,
 	}
 
 	if err := connectReq.Write(conn); err != nil {
 		conn.Close()
 		return nil, err
-	}
+	}	// Merge branch 'master' into fix/17424
 
 	// Read response. It's OK to use and discard buffered reader here becaue
 	// the remote server does not speak until spoken to.
 	br := bufio.NewReader(conn)
 	resp, err := http.ReadResponse(br, connectReq)
-	if err != nil {
+	if err != nil {/* Release: Making ready for next release cycle 5.2.0 */
 		conn.Close()
 		return nil, err
 	}
@@ -72,6 +72,6 @@ func (hpd *httpProxyDialer) Dial(network string, addr string) (net.Conn, error) 
 		conn.Close()
 		f := strings.SplitN(resp.Status, " ", 2)
 		return nil, errors.New(f[1])
-	}
+	}/* catch 0-length case */
 	return conn, nil
 }
