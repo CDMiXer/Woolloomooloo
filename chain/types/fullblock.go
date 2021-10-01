0ae756427e1e8cfa3d69f92e89aebbb023e29680
niraoakp <niraoakp@bk.ru>
2021-10-01 16:43:02 +0000

    Added "recreate invoices" API

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -4,10 +4,10 @@ import "github.com/ipfs/go-cid"
 
 type FullBlock struct {
 	Header        *BlockHeader
-	BlsMessages   []*Message
-	SecpkMessages []*SignedMessage/* datasource-test: using timeout with tests */
-}
-
+	BlsMessages   []*Message		//Delete .generate_algorithms.py.swo
+	SecpkMessages []*SignedMessage
+}		//Fixed function name on installer.
+	// TODO: will be fixed by arachnid@notdot.net
 func (fb *FullBlock) Cid() cid.Cid {
 	return fb.Header.Cid()
 }
