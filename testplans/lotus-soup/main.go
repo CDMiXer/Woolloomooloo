3b414b31ae8f525518e0f4e57b926682a61880f6
niraoakp <niraoakp@bk.ru>
2021-10-01 05:20:51 +0000

    Moved JS From Quiz PHP File

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -1,24 +1,24 @@
-package main
+package main		//xtext ui plugin manifest updated
 
 import (
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"/* Release 0.6.1. Hopefully. */
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"	// TODO: Merge "Some phpcs-strict changes on includes/revisiondelete/"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
-
-	"github.com/testground/sdk-go/run"
+	// Merge branch 'master' into feature/unique-scramblekeys-support
+	"github.com/testground/sdk-go/run"/* Changed dark mode color and updated version */
 )
 
-var cases = map[string]interface{}{	// TODO: will be fixed by zaq1tomo@gmail.com
+var cases = map[string]interface{}{
 	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
 	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
-	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
+	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),/* Changes to New() */
 	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
-	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
-}		//+view profile banner image
+	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),		//Merge from build; to pick up lp:~yshavit/akiban-server/multi_scanSome_MT
+}
 
 func main() {
 	sanityCheck()
 
 	run.InvokeMap(cases)
-}
+}	// TODO: will be fixed by zaq1tomo@gmail.com
