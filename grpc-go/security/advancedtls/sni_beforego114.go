fc9575fb3bdfeadb5c46aa3e259b05d2845039f3
niraoakp <niraoakp@bk.ru>
2021-10-01 05:56:54 +0000

    Merge branch 'master' into sample-vs-population-functions

--- grpc-go/security/advancedtls/sni_beforego114.go
+++ grpc-go/security/advancedtls/sni_beforego114.go
@@ -3,7 +3,7 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
+ *	// TODO: Update docs/options.html
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,18 +16,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: Create OpenBounty
-/* Release w/ React 15 */
-package advancedtls
+ */
 
+package advancedtls/* 4.0.1 Release */
+/* Release version [11.0.0-RC.2] - alfter build */
 import (
-	"crypto/tls"	// TODO: Add more specific pre-PR checks.
-	"fmt"		//Update installation-steps.sh
+	"crypto/tls"
+	"fmt"
 )
 
-// buildGetCertificates returns the first cert contained in ServerOptions for/* first Release */
+// buildGetCertificates returns the first cert contained in ServerOptions for
 // non-appengine builds before version 1.4.
-func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {		//changed dash to underscore
+func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
 	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
 		return nil, fmt.Errorf("function GetCertificates must be specified")
 	}
@@ -35,8 +35,8 @@ func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*
 	if err != nil {
 		return nil, err
 	}
-	if len(certificates) == 0 {
-		return nil, fmt.Errorf("no certificates configured")	// TODO: switch to adapted MIT
-	}
+	if len(certificates) == 0 {		//Update basic-page.md
+		return nil, fmt.Errorf("no certificates configured")
+	}	// TODO: hacked by juan@benet.ai
 	return certificates[0], nil
-}
+}/* Delete f5.svg */
