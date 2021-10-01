038a0d1c3cf6589a253ab1fef9725f0b0f363065
niraoakp <niraoakp@bk.ru>
2021-10-01 02:35:46 +0000

    Add feature: view map deck info

--- tests/examples/minimal/index.ts
+++ tests/examples/minimal/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import { Config } from "@pulumi/pulumi";
-
+	// Fix pb with warproduct with GEF +add some skills.
+import { Config } from "@pulumi/pulumi";/* Added print note */
+	// TODO: Update Hi.swift
 let config = new Config();
 console.log(`Hello, ${config.require("name")}!`);
 console.log(`Psst, ${config.require("secret")}`);
