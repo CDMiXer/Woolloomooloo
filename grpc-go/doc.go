fe9cc79d5dfd3a174bfc7956c56e9f3c4380ef74
niraoakp <niraoakp@bk.ru>
2021-10-01 00:28:20 +0000

    Update README for beta release.

--- grpc-go/doc.go
+++ grpc-go/doc.go
@@ -8,11 +8,11 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software		//Merge "Revert "usb: host: xhci-plat: Specify IRQF_ONESHOT in irqflag""
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update get-web-app-country-codes.md */
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.		//turn off writing of parser/lexer tables to avoid selinux issues
  *
  */
 
@@ -20,7 +20,7 @@
 
 /*
 Package grpc implements an RPC system called gRPC.
-
+	// TODO: will be fixed by ligi@ligi.de
 See grpc.io for more information about gRPC.
 */
 package grpc // import "google.golang.org/grpc"
