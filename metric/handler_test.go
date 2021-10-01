a4cc58bb54c538be70e38a4a438827a88593ffa4
niraoakp <niraoakp@bk.ru>
2021-10-01 07:17:04 +0000

    Fixing flink local jar issue

--- metric/handler_test.go
+++ metric/handler_test.go
@@ -1,64 +1,64 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//3395a42a-2e6c-11e5-9284-b827eb9e62be
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package metric
 
 import (
-	"net/http/httptest"		//Merge branch 'master' into salman-patch
+	"net/http/httptest"
 	"testing"
-/* Update radio.dm */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 	"github.com/golang/mock/gomock"
-)/* 1f84c0fc-2e66-11e5-9284-b827eb9e62be */
-/* Update ReleaseListJsonModule.php */
+)
+
 func TestHandleMetrics(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Pull entry ID from file. */
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
 	mockUser := &core.User{Admin: false, Machine: true}
 	session := mock.NewMockSession(controller)
 	session.EXPECT().Get(r).Return(mockUser, nil)
-/* Options for dynamic transparency */
+
 	NewServer(session, false).ServeHTTP(w, r)
 	if got, want := w.Code, 200; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
-	}	// TODO: ssl support.
+	}
 
 	if got, want := w.HeaderMap.Get("Content-Type"), "text/plain; version=0.0.4; charset=utf-8"; got != want {
 		t.Errorf("Want prometheus header %q, got %q", want, got)
-}	
-}	// Reorder releases
+	}
+}
 
 func TestHandleMetrics_NoSession(t *testing.T) {
-	controller := gomock.NewController(t)	// Fixed travis typo
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-		//Update noise-filter.m
+
 	session := mock.NewMockSession(controller)
 	session.EXPECT().Get(r).Return(nil, nil)
 
 	NewServer(session, false).ServeHTTP(w, r)
 
 	if got, want := w.Code, 401; got != want {
-		t.Errorf("Want status code %d, got %d", want, got)/* mf: Prepare release version 0.18.2. */
+		t.Errorf("Want status code %d, got %d", want, got)
 	}
 }
 
 func TestHandleMetrics_NoSessionButAnonymousAccessEnabled(t *testing.T) {
-	controller := gomock.NewController(t)		//ffdc8b7e-2e40-11e5-9284-b827eb9e62be
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)	// Merge "VMware: remove unused variable from tests"
+	r := httptest.NewRequest("GET", "/", nil)
 
 	session := mock.NewMockSession(controller)
 	session.EXPECT().Get(r).Return(nil, nil)
