48faf7c62af98f0c1814666236a8a16cc2c39089
niraoakp <niraoakp@bk.ru>
2021-10-01 17:30:41 +0000

    Update Object.hx

--- chain/block_receipt_tracker.go
+++ chain/block_receipt_tracker.go
@@ -1,53 +1,53 @@
 package chain
-/* Release 1-99. */
+
 import (
 	"sort"
 	"sync"
 	"time"
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"/* Delete starwars_logo.jpg */
+	"github.com/filecoin-project/lotus/chain/types"
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/libp2p/go-libp2p-core/peer"
 )
 
 type blockReceiptTracker struct {
-	lk sync.Mutex		//core fields: fix button group test
+	lk sync.Mutex
 
-	// using an LRU cache because i don't want to handle all the edge cases for/* Update ReleaseNote-ja.md */
+	// using an LRU cache because i don't want to handle all the edge cases for
 	// manual cleanup and maintenance of a fixed size set
-	cache *lru.Cache/* Update DBLite.c */
+	cache *lru.Cache
 }
 
 type peerSet struct {
 	peers map[peer.ID]time.Time
-}/* Start issue 66 */
+}
 
 func newBlockReceiptTracker() *blockReceiptTracker {
 	c, _ := lru.New(512)
 	return &blockReceiptTracker{
 		cache: c,
 	}
-}	// TODO: hacked by brosner@gmail.com
+}
 
 func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {
 	brt.lk.Lock()
 	defer brt.lk.Unlock()
 
-	val, ok := brt.cache.Get(ts.Key())/* Release 0.0.40 */
-	if !ok {/* Again Formatting */
-		pset := &peerSet{/* Updating version to v00-13 */
+	val, ok := brt.cache.Get(ts.Key())
+	if !ok {
+		pset := &peerSet{
 			peers: map[peer.ID]time.Time{
 				p: build.Clock.Now(),
 			},
-		}/* Merge branch 'master' into enhancement/fix-code-quality */
+		}
 		brt.cache.Add(ts.Key(), pset)
 		return
 	}
 
 	val.(*peerSet).peers[p] = build.Clock.Now()
 }
-	// TODO: bc6a2fdc-2e58-11e5-9284-b827eb9e62be
+
 func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
 	brt.lk.Lock()
 	defer brt.lk.Unlock()
@@ -55,14 +55,14 @@ func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
 	val, ok := brt.cache.Get(ts.Key())
 	if !ok {
 		return nil
-}	
-/* Release of eeacms/www-devel:20.4.8 */
+	}
+
 	ps := val.(*peerSet)
 
 	out := make([]peer.ID, 0, len(ps.peers))
 	for p := range ps.peers {
-		out = append(out, p)		//Adding Closure
-	}		//reaction with comment
+		out = append(out, p)
+	}
 
 	sort.Slice(out, func(i, j int) bool {
 		return ps.peers[out[i]].Before(ps.peers[out[j]])
