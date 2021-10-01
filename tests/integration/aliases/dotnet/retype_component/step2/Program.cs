ddd6d0e80046333f19bde8c3d66156e6d34c05ab
niraoakp <niraoakp@bk.ru>
2021-10-01 15:06:05 +0000

    Update from Forestry.io - Created ventana1.jpg

--- tests/integration/aliases/dotnet/retype_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step2/Program.cs
@@ -4,15 +4,15 @@ using System.Threading.Tasks;
 using Pulumi;
 
 class Resource : ComponentResource
-{	// TODO: will be fixed by aeongrp@outlook.com
+{
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
 }
 
-// Scenario #4 - change the type of a component		//3rd Example Ventilation Data Collection Graphs
-class ComponentFour : ComponentResource/* tests of random button works */
+// Scenario #4 - change the type of a component
+class ComponentFour : ComponentResource
 {
     private Resource resource;
 
@@ -20,13 +20,13 @@ class ComponentFour : ComponentResource/* tests of random button works */
         : base("my:differentmodule:ComponentFourWithADifferentTypeName", name, ComponentResourceOptions.Merge(options, new ComponentResourceOptions
         {
             // Add an alias that references the old type of this resource
-            // and then make the base() call with the new type of this resource and the added alias.		//Cooking show for stage
+            // and then make the base() call with the new type of this resource and the added alias.
             Aliases = { new Alias { Type = "my:module:ComponentFour" } }
         }))
     {
-        // The child resource will also pick up an implicit alias due to the new type of the component it is parented to.	// TODO: hacked by ligi@ligi.de
+        // The child resource will also pick up an implicit alias due to the new type of the component it is parented to.
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
-    }/* Updated Release note. */
+    }
 }
 
 class Program
