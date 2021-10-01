003a8bf8064a244ee7c078bf81aa92c6974d543a
niraoakp <niraoakp@bk.ru>
2021-10-01 16:51:48 +0000

    add5f3fa-2e73-11e5-9284-b827eb9e62be

--- tests/integration/empty/python/__main__.py
+++ tests/integration/empty/python/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-def main():
+def main():	// TODO: will be fixed by admin@multicoin.co
     return None
-
+	// TODO: hacked by timnugent@gmail.com
 if __name__ == "__main__":
     main()
