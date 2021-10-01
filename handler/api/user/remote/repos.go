bf785a2f8b2552184793e67f52f8fca2cc9ee7f7
niraoakp <niraoakp@bk.ru>
2021-10-01 12:37:07 +0000

    Added hated affinities to the affinities unit.

--- handler/api/user/remote/repos.go
+++ handler/api/user/remote/repos.go
@@ -3,10 +3,10 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Added RapidFire data to entities */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* 20.0.0.2 NOTICES */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -21,10 +21,10 @@ import (
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-)/* find the location of files on disk */
+)
 
 // HandleRepos returns an http.HandlerFunc that write a json-encoded
-// list of repositories to the response body.		//Rename connect-q.go to connect-q-TODO.go
+// list of repositories to the response body.
 func HandleRepos(repos core.RepositoryService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
@@ -34,8 +34,8 @@ func HandleRepos(repos core.RepositoryService) http.HandlerFunc {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list remote repositories")
-		} else {		//Merge branch 'master' into joh/completionsDeprecated
+		} else {
 			render.JSON(w, list, 200)
-		}/* engines restore */
+		}
 	}
 }
