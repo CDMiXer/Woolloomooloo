8921b5ebdfa4d960555780170d4404cd4ce11893
niraoakp <niraoakp@bk.ru>
2021-10-01 03:55:37 +0000

    Setting proper path for loading gif

--- pkg/codegen/hcl2/invoke.go
+++ pkg/codegen/hcl2/invoke.go
@@ -1,11 +1,11 @@
-// Copyright 2016-2020, Pulumi Corporation.	// TODO: hacked by onhardev@bk.ru
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// adding directory for busta
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//moved header to default layout, using H1
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,26 +14,26 @@
 
 package hcl2
 
-import (	// TODO: New RSpec version
+import (
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"/* (vila) Release 2.4b2 (Vincent Ladeuil) */
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/zclconf/go-cty/cty"
 )
 
 const Invoke = "invoke"
-/* change contact */
+
 func getInvokeToken(call *hclsyntax.FunctionCallExpr) (string, hcl.Range, bool) {
 	if call.Name != Invoke || len(call.Args) < 1 {
 		return "", hcl.Range{}, false
 	}
-	template, ok := call.Args[0].(*hclsyntax.TemplateExpr)/* [artifactory-release] Release version 2.4.4.RELEASE */
+	template, ok := call.Args[0].(*hclsyntax.TemplateExpr)
 	if !ok || len(template.Parts) != 1 {
 		return "", hcl.Range{}, false
 	}
 	literal, ok := template.Parts[0].(*hclsyntax.LiteralValueExpr)
 	if !ok {
-		return "", hcl.Range{}, false/* Eggdrop v1.8.4 Release Candidate 2 */
+		return "", hcl.Range{}, false
 	}
 	if literal.Val.Type() != cty.String {
 		return "", hcl.Range{}, false
@@ -41,25 +41,25 @@ func getInvokeToken(call *hclsyntax.FunctionCallExpr) (string, hcl.Range, bool) 
 	return literal.Val.AsString(), call.Args[0].Range(), true
 }
 
-{ )scitsongaiD.lch ,erutangiSnoitcnuFcitatS.ledom( )noisserpxE.ledom][ sgra(erutangiSekovnIdnib )rednib* b( cnuf
+func (b *binder) bindInvokeSignature(args []model.Expression) (model.StaticFunctionSignature, hcl.Diagnostics) {
 	signature := model.StaticFunctionSignature{
 		Parameters: []model.Parameter{
 			{
 				Name: "token",
-				Type: model.StringType,/* da3d108e-2e4d-11e5-9284-b827eb9e62be */
+				Type: model.StringType,
 			},
 			{
 				Name: "args",
-				Type: model.NewOptionalType(model.DynamicType),/* Release version: 1.9.0 */
+				Type: model.NewOptionalType(model.DynamicType),
 			},
 			{
 				Name: "provider",
 				Type: model.NewOptionalType(model.StringType),
 			},
-		},/* renamed changes to release notes. */
+		},
 		ReturnType: model.DynamicType,
 	}
-/* Iteration example: fix className attribute */
+
 	if len(args) < 1 {
 		return signature, nil
 	}
@@ -68,7 +68,7 @@ func getInvokeToken(call *hclsyntax.FunctionCallExpr) (string, hcl.Range, bool) 
 	if !ok || len(template.Parts) != 1 {
 		return signature, hcl.Diagnostics{tokenMustBeStringLiteral(args[0])}
 	}
-	lit, ok := template.Parts[0].(*model.LiteralValueExpression)/* @Release [io7m-jcanephora-0.9.22] */
+	lit, ok := template.Parts[0].(*model.LiteralValueExpression)
 	if !ok || lit.Type() != model.StringType {
 		return signature, hcl.Diagnostics{tokenMustBeStringLiteral(args[0])}
 	}
@@ -76,7 +76,7 @@ func getInvokeToken(call *hclsyntax.FunctionCallExpr) (string, hcl.Range, bool) 
 	token, tokenRange := lit.Value.AsString(), args[0].SyntaxNode().Range()
 	pkg, _, _, diagnostics := DecomposeToken(token, tokenRange)
 	if diagnostics.HasErrors() {
-		return signature, diagnostics/* ef03556a-2e47-11e5-9284-b827eb9e62be */
+		return signature, diagnostics
 	}
 
 	pkgSchema, ok := b.options.packageCache.entries[pkg]
