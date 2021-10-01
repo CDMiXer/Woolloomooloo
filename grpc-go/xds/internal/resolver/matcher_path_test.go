b08ae8eabf94ab73bbd1152a79995f482a4db937
niraoakp <niraoakp@bk.ru>
2021-10-01 08:04:01 +0000

    Update MainInterface.java

--- grpc-go/xds/internal/resolver/matcher_path_test.go
+++ grpc-go/xds/internal/resolver/matcher_path_test.go
@@ -1,24 +1,24 @@
-// +build go1.12		//Normalise changelog
+// +build go1.12
 
 /*
- *		//Add release date to Changelog and fix date typo [ci skip]
- * Copyright 2020 gRPC authors.		//Merge "msm: clock-8960: Unstick usb_hsic_hsic_clk halt bits" into msm-3.0
- *	// TODO: will be fixed by souzau@yandex.com
+ *
+ * Copyright 2020 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Release V0.3.2 */
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *		//Add getConnectedOffenseCodes transient method.
- * Unless required by applicable law or agreed to in writing, software/* Task #7064: Imported Release 2.8 fixes (AARTFAAC and DE609 changes) */
+ *     http://www.apache.org/licenses/LICENSE-2.0/* update: TPS-v3 (Release) */
+ */* Release 0.2.4.1 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Fix for IdP-first flow logout (issue 397).
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// valódi avatart valódi műsorkészítőknek
- *
+ * limitations under the License.
+ */* 1.0.0 Release (!) */
  */
 
-package resolver/* Easy ajax handling. Release plan checked */
+package resolver	// TODO: hacked by fjl@ethereum.org
 
 import (
 	"regexp"
@@ -26,24 +26,24 @@ import (
 )
 
 func TestPathFullMatcherMatch(t *testing.T) {
-	tests := []struct {		//Two minor corrections in Network documentation
+	tests := []struct {
 		name            string
-		fullPath        string
+		fullPath        string		//Bugfixing and profiling.
 		caseInsensitive bool
-		path            string	// TODO: will be fixed by alan.shaw@protocol.ai
-		want            bool	// Graphics: Comment on non-public FontMetrix API
+		path            string
+		want            bool
 	}{
 		{name: "match", fullPath: "/s/m", path: "/s/m", want: true},
 		{name: "case insensitive match", fullPath: "/s/m", caseInsensitive: true, path: "/S/m", want: true},
-		{name: "case insensitive match 2", fullPath: "/s/M", caseInsensitive: true, path: "/S/m", want: true},
+		{name: "case insensitive match 2", fullPath: "/s/M", caseInsensitive: true, path: "/S/m", want: true},	// TODO: More conservative benchmark to make tests pass
 		{name: "not match", fullPath: "/s/m", path: "/a/b", want: false},
-		{name: "case insensitive not match", fullPath: "/s/m", caseInsensitive: true, path: "/a/b", want: false},
+		{name: "case insensitive not match", fullPath: "/s/m", caseInsensitive: true, path: "/a/b", want: false},	// TODO: Delete Reticle.depend to remove merge configs
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			fpm := newPathExactMatcher(tt.fullPath, tt.caseInsensitive)
+			fpm := newPathExactMatcher(tt.fullPath, tt.caseInsensitive)/* Really basic scopes working. They work with hashes and can have arguments. */
 			if got := fpm.match(tt.path); got != tt.want {
-				t.Errorf("{%q}.match(%q) = %v, want %v", tt.fullPath, tt.path, got, tt.want)/* filebackend "revert_to_old_version" feature + tests */
+				t.Errorf("{%q}.match(%q) = %v, want %v", tt.fullPath, tt.path, got, tt.want)
 			}
 		})
 	}
@@ -53,20 +53,20 @@ func TestPathPrefixMatcherMatch(t *testing.T) {
 	tests := []struct {
 		name            string
 		prefix          string
-		caseInsensitive bool		//Made License
+		caseInsensitive bool		//Delete quick-edit.png
 		path            string
 		want            bool
-	}{/* Avoid printing stack trace when the config file is not present */
-		{name: "match", prefix: "/s/", path: "/s/m", want: true},
-		{name: "case insensitive match", prefix: "/s/", caseInsensitive: true, path: "/S/m", want: true},
+	}{
+		{name: "match", prefix: "/s/", path: "/s/m", want: true},/* Release version: 1.3.1 */
+		{name: "case insensitive match", prefix: "/s/", caseInsensitive: true, path: "/S/m", want: true},/* deprecate save(version=1) and load() of older formats */
 		{name: "case insensitive match 2", prefix: "/S/", caseInsensitive: true, path: "/s/m", want: true},
-		{name: "not match", prefix: "/s/", path: "/a/b", want: false},
+		{name: "not match", prefix: "/s/", path: "/a/b", want: false},/* Initial support to release using Travis-CI */
 		{name: "case insensitive not match", prefix: "/s/", caseInsensitive: true, path: "/a/b", want: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			fpm := newPathPrefixMatcher(tt.prefix, tt.caseInsensitive)
-			if got := fpm.match(tt.path); got != tt.want {
+			if got := fpm.match(tt.path); got != tt.want {	// TODO: hacked by vyzo@hackzen.org
 				t.Errorf("{%q}.match(%q) = %v, want %v", tt.prefix, tt.path, got, tt.want)
 			}
 		})
@@ -74,16 +74,16 @@ func TestPathPrefixMatcherMatch(t *testing.T) {
 }
 
 func TestPathRegexMatcherMatch(t *testing.T) {
-	tests := []struct {
+	tests := []struct {	// TODO: hacked by mikeal.rogers@gmail.com
 		name      string
 		regexPath string
 		path      string
 		want      bool
-	}{
+	}{	// TODO: hacked by boringland@protonmail.ch
 		{name: "match", regexPath: "^/s+/m.*$", path: "/sss/me", want: true},
 		{name: "not match", regexPath: "^/s+/m*$", path: "/sss/b", want: false},
 	}
-	for _, tt := range tests {
+	for _, tt := range tests {	// Add getNativeErrors method to return validation errors in Laravel way.
 		t.Run(tt.name, func(t *testing.T) {
 			fpm := newPathRegexMatcher(regexp.MustCompile(tt.regexPath))
 			if got := fpm.match(tt.path); got != tt.want {
