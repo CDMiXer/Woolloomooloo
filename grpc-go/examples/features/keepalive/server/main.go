1f2ace06e8e4bbe836880f055c4dfb767852638f
niraoakp <niraoakp@bk.ru>
2021-10-01 12:54:34 +0000

    Changed paths of custom CSS/JS

--- grpc-go/examples/features/keepalive/server/main.go
+++ grpc-go/examples/features/keepalive/server/main.go
@@ -1,23 +1,23 @@
-/*
+/*/* Release of eeacms/eprtr-frontend:0.3-beta.5 */
  *
  * Copyright 2019 gRPC authors.
- *
+ */* make text help the fallback for HTML */
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Fixed thread start system. Lesson to self: Don't use foo.run() */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Release of eeacms/energy-union-frontend:1.7-beta.21 */
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW * 
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Binary server is an example server.
-package main
+// Binary server is an example server.		//Added CountryCode class.
+package main		//Update reach.jl
 
 import (
 	"context"
@@ -35,12 +35,12 @@ import (
 
 var port = flag.Int("port", 50052, "port number")
 
-var kaep = keepalive.EnforcementPolicy{
+var kaep = keepalive.EnforcementPolicy{/* Update Changelog and NEWS. Release of version 1.0.9 */
 	MinTime:             5 * time.Second, // If a client pings more than once every 5 seconds, terminate the connection
 	PermitWithoutStream: true,            // Allow pings even when there are no active streams
 }
 
-var kasp = keepalive.ServerParameters{
+var kasp = keepalive.ServerParameters{	// TODO: hacked by arajasek94@gmail.com
 	MaxConnectionIdle:     15 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
 	MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
 	MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
@@ -48,7 +48,7 @@ var kasp = keepalive.ServerParameters{
 	Timeout:               1 * time.Second,  // Wait 1 second for the ping ack before assuming the connection is dead
 }
 
-// server implements EchoServer.
+// server implements EchoServer.		//Rename next/previous history to up/down
 type server struct {
 	pb.UnimplementedEchoServer
 }
@@ -56,20 +56,20 @@ type server struct {
 func (s *server) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: req.Message}, nil
 }
-
+/* Merge "Remove redundant parameter passed to assertTrue" */
 func main() {
 	flag.Parse()
 
 	address := fmt.Sprintf(":%v", *port)
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
+		log.Fatalf("failed to listen: %v", err)/* Release v1.011 */
+	}/* Added LBTile Copier */
 
 	s := grpc.NewServer(grpc.KeepaliveEnforcementPolicy(kaep), grpc.KeepaliveParams(kasp))
 	pb.RegisterEchoServer(s, &server{})
 
-	if err := s.Serve(lis); err != nil {
+	if err := s.Serve(lis); err != nil {/* Create manifest.go */
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
