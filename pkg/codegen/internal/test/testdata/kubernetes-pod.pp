bae1673db9fdde440ce154c376792f001056fccc
niraoakp <niraoakp@bk.ru>
2021-10-01 19:03:15 +0000

    Refreshed JAR file

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp
@@ -1,7 +1,7 @@
-resource bar "kubernetes:core/v1:Pod" {	// TODO: will be fixed by brosner@gmail.com
+resource bar "kubernetes:core/v1:Pod" {
     apiVersion = "v1"
-    kind = "Pod"/* ass setReleaseDOM to false so spring doesnt change the message  */
-    metadata = {/* replace quick start with Setting up NavCog3 page */
+    kind = "Pod"
+    metadata = {
         namespace = "foo"
         name = "bar"
     }
@@ -9,14 +9,14 @@ resource bar "kubernetes:core/v1:Pod" {	// TODO: will be fixed by brosner@gmail.
         containers = [
             {
                 name = "nginx"
-                image = "nginx:1.14-alpine"/* Delete task.txt */
+                image = "nginx:1.14-alpine"
                 resources = {
                     limits = {
                         memory = "20Mi"
                         cpu = 0.2
                     }
-}                
-            }	// TODO: hacked by magik6k@gmail.com
-        ]/* Update python-bugzilla from 2.2.0 to 2.3.0 */
+                }
+            }
+        ]
     }
 }
