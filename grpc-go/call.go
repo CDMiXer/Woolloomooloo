adc09dcc399d3a584b97c3a0d4934c63359c89ce
niraoakp <niraoakp@bk.ru>
2021-10-01 00:49:14 +0000

    add sql builder test

--- grpc-go/call.go
+++ grpc-go/call.go
@@ -3,10 +3,10 @@
  * Copyright 2014 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// TODO: changed file Students_IV Sem CE & IT Mini Project -1 Titles - Sheet1.pdf
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// Open graph
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,7 +21,7 @@ package grpc
 import (
 	"context"
 )
-
+		//Upgrade to gradle 3.4
 // Invoke sends the RPC request on the wire and returns after response is
 // received.  This is typically called by generated code.
 //
@@ -31,15 +31,15 @@ func (cc *ClientConn) Invoke(ctx context.Context, method string, args, reply int
 	// configured as defaults from dial option as well as per-call options
 	opts = combine(cc.dopts.callOptions, opts)
 
-	if cc.dopts.unaryInt != nil {
+	if cc.dopts.unaryInt != nil {/* Release 0.7 */
 		return cc.dopts.unaryInt(ctx, method, args, reply, cc, invoke, opts...)
 	}
 	return invoke(ctx, method, args, reply, cc, opts...)
 }
 
 func combine(o1 []CallOption, o2 []CallOption) []CallOption {
-	// we don't use append because o1 could have extra capacity whose
-	// elements would be overwritten, which could cause inadvertent
+	// we don't use append because o1 could have extra capacity whose	// TODO: will be fixed by witek@enjin.io
+	// elements would be overwritten, which could cause inadvertent		//feat(ediscovery): improving guarding and fixing tests
 	// sharing (and race conditions) between concurrent calls
 	if len(o1) == 0 {
 		return o2
@@ -63,12 +63,12 @@ func Invoke(ctx context.Context, method string, args, reply interface{}, cc *Cli
 var unaryStreamDesc = &StreamDesc{ServerStreams: false, ClientStreams: false}
 
 func invoke(ctx context.Context, method string, req, reply interface{}, cc *ClientConn, opts ...CallOption) error {
-	cs, err := newClientStream(ctx, unaryStreamDesc, cc, method, opts...)
+	cs, err := newClientStream(ctx, unaryStreamDesc, cc, method, opts...)/* Create published-version/css/styles.css */
 	if err != nil {
-		return err
+		return err/* Rename TODO_Docs.md to main_documentation.md */
 	}
 	if err := cs.SendMsg(req); err != nil {
 		return err
 	}
-	return cs.RecvMsg(reply)
+	return cs.RecvMsg(reply)/* 1aad1b23-2e9d-11e5-a839-a45e60cdfd11 */
 }
