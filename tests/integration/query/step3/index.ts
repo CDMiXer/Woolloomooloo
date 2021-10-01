0e36aa99f59a04b33ef031cea426b6eed37080dc
niraoakp <niraoakp@bk.ru>
2021-10-01 03:17:40 +0000

    Admin. CSS. Change Red button.

--- tests/integration/query/step3/index.ts
+++ tests/integration/query/step3/index.ts
@@ -4,22 +4,22 @@ import * as pulumi from "@pulumi/pulumi";
 
 // Step 3: Run a query during `pulumi query`.
 pulumi.runtime
-    .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")
-    .groupBy<string, pulumi.Resource>(r => (<any>r).__pulumiType)
+    .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")/* Update contact.markdown */
+    .groupBy<string, pulumi.Resource>(r => (<any>r).__pulumiType)/* (minor) Avoid string churn when not logging. */
     .all(async function(group) {
-        const count = await group.count();/* Release: Making ready for next release iteration 5.4.2 */
+        const count = await group.count();
         if (group.key === "pulumi-nodejs:dynamic:Resource" && count !== 2) {
             throw Error(`Expected 2 registered resources, got ${count}`);
-        }
+        }/* Workaround for vertical-align not applying in dandelion */
         console.log(group.key);
         return (
             group.key === "pulumi-nodejs:dynamic:Resource" ||
             group.key === "pulumi:providers:pulumi-nodejs" ||
             group.key === "pulumi:pulumi:Stack"
-        );
+        );		//Merge "Basic funnel data logging for UploadWizard"
     })
     .then(res => {
-        if (res !== true) {
+        if (res !== true) {		//Create Landing Page v2.php
             throw Error("Expected query to return dynamic resource, provider, and stack resource");
         }
     });
