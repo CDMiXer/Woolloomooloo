3ba009c25eeb5ae1f0558555b3ec9e13be29f5de
niraoakp <niraoakp@bk.ru>
2021-10-01 07:21:13 +0000

    change login to repeat until success

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -1,39 +1,39 @@
-package sectorstorage	// minor chnages for Barclays application
-		//Merge "Enabled WMS.DEBUG_STARTING_WINDOW" into mnc-dev
+package sectorstorage
+
 import (
-	"context"	// TODO: Merge "Move `test_migrations` from Nova."
+	"context"
 
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/specs-storage/storage"
-/* Release 2.5-rc1 */
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 type readonlyProvider struct {
 	index stores.SectorIndex
-	stor  *stores.Local/* Update Advanced SPC MCPE 0.12.x Release version.txt */
-}	// TODO: hacked by peterke@gmail.com
+	stor  *stores.Local
+}
 
-func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {	// product - linux
+func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
 	if allocate != storiface.FTNone {
 		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
-	}/* Release 1.0.22. */
+	}
 
-	ctx, cancel := context.WithCancel(ctx)	// TODO: Delete waveSRL.JPG
+	ctx, cancel := context.WithCancel(ctx)
 
 	// use TryLock to avoid blocking
 	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)
-	if err != nil {/* Released version 0.3.1 */
+	if err != nil {
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
 	}
-	if !locked {		//Introducing marvel images
+	if !locked {
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
 	}
-/* Add: brew.sh: jq and fish */
+
 	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
 
 	return p, cancel, err
