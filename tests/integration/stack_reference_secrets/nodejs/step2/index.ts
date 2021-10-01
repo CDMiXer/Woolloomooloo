c01dc3501cbb55f617ec7536db84a61cbb8e7cbc
niraoakp <niraoakp@bk.ru>
2021-10-01 01:06:32 +0000

    webpack version

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -1,14 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
 
 export const normal = pulumi.output("normal");
-export const secret = pulumi.secret("secret");
-
+export const secret = pulumi.secret("secret");	// TODO: hacked by arajasek94@gmail.com
+	// TODO: Merge "Remove check_role_for_trust from sample policies"
 // Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
 // deployment.
 const org = new pulumi.Config().require("org");
 const project = pulumi.getProject();
-const stack = pulumi.getStack();/* Setting up deps */
-const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);/* Release 3.0.0-alpha-1: update sitemap */
+const stack = pulumi.getStack();
+const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
 
 export const refNormal = sr.getOutput("normal");
 export const refSecret = sr.getOutput("secret");
