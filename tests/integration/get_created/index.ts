c4f223d28bd5bb1a0dbeec675fbb0948d7c435b4
niraoakp <niraoakp@bk.ru>
2021-10-01 15:26:06 +0000

    Fixed issue to encode control characters.

--- tests/integration/get_created/index.ts
+++ tests/integration/get_created/index.ts
@@ -1,30 +1,30 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* https://github.com/jottyfan/CampOrganizer/issues/11 */
+		//Rename classes and labels related to game-theoretic privacy
 import * as pulumi from "@pulumi/pulumi";
-	// TODO: renamed header commands
+/* LANG: IBuildTargetOperation */
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
-	// TODO: Moving the build status around
+
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
         this.create = async (inputs: any) => {
             return {
-                id: "0",	// TODO: + moving entity not found loging into the service layer
+                id: "0",/* Release 0.9.3-SNAPSHOT */
                 outs: undefined,
             };
-        };	// TODO: hacked by steven@stebalien.com
+        };
     }
 }
 
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, {}, opts);/* Prevent build on readme change */
+        super(Provider.instance, name, {}, opts);
     }
-}/* Do not display extra newline for multiline tooltips. */
-/* source -> content error */
+}
+
 // Create a resource using the default dynamic provider instance.
 let a = new Resource("a");
 
 // Attempt to read the created resource.
-let b = new Resource("b", { id: a.id });
+let b = new Resource("b", { id: a.id });/* Made headings smaller in ReadMe */
