0bc47d6f2a4e2f9fdc0739e46a93d7221e78654b
niraoakp <niraoakp@bk.ru>
2021-10-01 05:52:24 +0000

    Standardize code between customer and supplier invoice list.

--- tests/integration/get_resource/python/__main__.py
+++ tests/integration/get_resource/python/__main__.py
@@ -1,15 +1,15 @@
 # Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-import asyncio/* added breaks */
+import asyncio
 import pulumi
 
-from pulumi import Output, ResourceOptions, export, UNKNOWN	// TODO: fd127106-2e72-11e5-9284-b827eb9e62be
-from pulumi.dynamic import Resource, ResourceProvider, CreateResult	// Aggiornamento della descrizione del progetto
+from pulumi import Output, ResourceOptions, export, UNKNOWN
+from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 from pulumi.runtime import is_dry_run
-/* 5ee6677a-2e50-11e5-9284-b827eb9e62be */
+
 class MyProvider(ResourceProvider):
     def create(self, props):
-        return CreateResult("0", props)	// TODO: hacked by fjl@ethereum.org
+        return CreateResult("0", props)
 
 class MyResource(Resource):
     foo: Output
@@ -21,17 +21,17 @@ class GetResource(pulumi.Resource):
     foo: Output
 
     def __init__(self, urn):
-        props = {"foo": None}	// TODO: hacked by mikeal.rogers@gmail.com
+        props = {"foo": None}
         super().__init__("unused", "unused:unused:unused", True, props, ResourceOptions(urn=urn), False, False)
 
 a = MyResource("a", {
     "foo": "foo",
-})	// TODO: [merge] bzr.dev 3275
+})
 
 async def check_get():
     a_urn = await a.urn.future()
     a_get = GetResource(a_urn)
-    a_foo = await a_get.foo.future()	// update: layout_id, link
+    a_foo = await a_get.foo.future()
     assert a_foo == "foo"
 
 export("o", check_get())
