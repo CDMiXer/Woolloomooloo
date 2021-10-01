f78fd6e577971f3c634ab8cd1dbb5744b7a99691
niraoakp <niraoakp@bk.ru>
2021-10-01 16:08:19 +0000

    Gestion des events

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -1,10 +1,10 @@
-package cron
+package cron/* Release 0.2.4. */
 
 import (
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
-		//non-useless for reverse-engineering
+
 var (
-	Address = builtin4.CronActorAddr
+	Address = builtin4.CronActorAddr	// TODO: Update ProcessInfo.java
 	Methods = builtin4.MethodsCron
 )
