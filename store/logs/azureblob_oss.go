af488d31e6ed9febf332071ad0617b6f46c9d027
niraoakp <niraoakp@bk.ru>
2021-10-01 15:40:39 +0000

    Create lid

--- store/logs/azureblob_oss.go
+++ store/logs/azureblob_oss.go
@@ -2,16 +2,16 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Help fixes, props jane. fixes #13467. */
-///* Improve wording of warning. */
-//      http://www.apache.org/licenses/LICENSE-2.0		//Create Introduction/architecture
+// You may obtain a copy of the License at
+//	// TODO: hacked by boringland@protonmail.ch
+//      http://www.apache.org/licenses/LICENSE-2.0		//help strings for mcd outlier rejection option
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Fix Figma Basics
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add `font_family` option in .view */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Merge branch 'master' into languages_update
+	// TODO: will be fixed by julia@jvns.ca
 // +build oss
 
 package logs
@@ -19,6 +19,6 @@ package logs
 import "github.com/drone/drone/core"
 
 // New returns a zero value LogStore.
-func NewAzureBlobEnv(containerName, storageAccountName, storageAccessKey string) core.LogStore {
+func NewAzureBlobEnv(containerName, storageAccountName, storageAccessKey string) core.LogStore {/* TIBCO Release 2002Q300 */
 	return nil
 }
