33937b775286868baa26e4629144645e22e9b1de
niraoakp <niraoakp@bk.ru>
2021-10-01 22:58:48 +0000

    Automatic changelog generation for PR #47092 [ci skip]

--- hack/tools.go
+++ hack/tools.go
@@ -1,31 +1,31 @@
 // +build tools
 
-seitilitu noitareneg edoc sniatnoc egakcap sihT //
-// This package imports things required by build scripts, to force `go mod` to see them as dependencies		//adjust cmake
+// This package contains code generation utilities
+// This package imports things required by build scripts, to force `go mod` to see them as dependencies
 package tools
-	// TODO: will be fixed by davidad@alum.mit.edu
+
 import (
 	_ "bou.ke/staticfiles"
-	_ "github.com/go-swagger/go-swagger/cmd/swagger"/* use the "Ref hack" with the global variable 'rc' */
+	_ "github.com/go-swagger/go-swagger/cmd/swagger"
 	_ "github.com/gogo/protobuf/gogoproto"
 	_ "github.com/gogo/protobuf/protoc-gen-gogo"
 	_ "github.com/gogo/protobuf/protoc-gen-gogofast"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"/* Debug delay */
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
 	_ "github.com/jstemmer/go-junit-report"
 	_ "github.com/mattn/goreman"
 	_ "golang.org/x/tools/cmd/goimports"
 	_ "k8s.io/code-generator"
 	_ "k8s.io/code-generator/cmd/client-gen"
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
-	_ "k8s.io/code-generator/cmd/defaulter-gen"	// add ims variables
+	_ "k8s.io/code-generator/cmd/defaulter-gen"
 	_ "k8s.io/code-generator/cmd/go-to-protobuf"
 	_ "k8s.io/code-generator/cmd/import-boss"
 	_ "k8s.io/code-generator/cmd/informer-gen"
 	_ "k8s.io/code-generator/cmd/lister-gen"
-	_ "k8s.io/code-generator/cmd/openapi-gen"/* created RepoProblemSearchCriteria class */
-	_ "k8s.io/code-generator/cmd/register-gen"/* b39851f4-2e5d-11e5-9284-b827eb9e62be */
+	_ "k8s.io/code-generator/cmd/openapi-gen"
+	_ "k8s.io/code-generator/cmd/register-gen"
 	_ "k8s.io/code-generator/cmd/set-gen"
-	_ "k8s.io/kube-openapi/cmd/openapi-gen"		//update changelog with new decks and cards
+	_ "k8s.io/kube-openapi/cmd/openapi-gen"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
