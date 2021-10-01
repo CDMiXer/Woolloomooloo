5464307bf8ce87542c6490c57e4c28f4a8634134
niraoakp <niraoakp@bk.ru>
2021-10-01 08:51:00 +0000

    Fixed DCA class id generation

--- operator/manager/rpc/types.go
+++ operator/manager/rpc/types.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* 5fb2df74-2e70-11e5-9284-b827eb9e62be */
+// Use of this source code is governed by the Drone Non-Commercial License/* Release 2.0.23 - Use new UStack */
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package rpc
-
+	// Created Gentleman Boss
 import (
 	"sync"
 
@@ -16,28 +16,28 @@ import (
 type requestRequest struct {
 	Request *manager.Request
 }
-
-type acceptRequest struct {
-	Stage   int64
+/* Release 0.8.0.rc1 */
+type acceptRequest struct {		//updated video formating
+	Stage   int64/* Release for v6.5.0. */
 	Machine string
 }
-
+/* fs/Lease: use IsReleasedEmpty() once more */
 type netrcRequest struct {
-	Repo int64
+	Repo int64/* 37c903f0-2e4b-11e5-9284-b827eb9e62be */
 }
 
 type detailsRequest struct {
 	Stage int64
 }
-
+/* Update field.rb */
 type stageRequest struct {
 	Stage *core.Stage
-}
-
+}	// test wav writer success
+/* use workflow cache in timeout handler */
 type stepRequest struct {
 	Step *core.Step
 }
-
+/* Add blockquoting. */
 type writeRequest struct {
 	Step int64
 	Line *core.Line
@@ -48,18 +48,18 @@ type watchRequest struct {
 }
 
 type watchResponse struct {
-	Done bool
+	Done bool	// TODO: fix devres for loop bounds check
 }
-
+/* Merge "Release 1.0.0.156 QCACLD WLAN Driver" */
 type buildContextToken struct {
 	Secret  string
 	Context *manager.Context
 }
 
-type errorWrapper struct {
+type errorWrapper struct {		//dovegraph refactors
 	Message string
 }
-
+/* Released 1.0 */
 var writePool = sync.Pool{
 	New: func() interface{} {
 		return &writeRequest{}
