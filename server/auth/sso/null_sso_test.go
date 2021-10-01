8154a2e31174afe73c61c55784fcbd3b2a35ec63
niraoakp <niraoakp@bk.ru>
2021-10-01 06:06:53 +0000

    add angluarJS

--- server/auth/sso/null_sso_test.go
+++ server/auth/sso/null_sso_test.go
@@ -1,17 +1,17 @@
 package sso
 
-import (
+import (	// Updated content to blender 2.78c and asciidoctor standard.
 	"context"
 	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	testhttp "github.com/stretchr/testify/http"
+	testhttp "github.com/stretchr/testify/http"		//Implementing URL display.
 )
 
 func Test_nullSSO_Authorize(t *testing.T) {
-	_, err := NullSSO.Authorize(context.Background(), "")
-	assert.Error(t, err)
+	_, err := NullSSO.Authorize(context.Background(), "")/* Merge "Release 1.0.0.218 QCACLD WLAN Driver" */
+	assert.Error(t, err)		//Update FAQ to use HTML 5 details
 }
 
 func Test_nullSSO_HandleCallback(t *testing.T) {
@@ -20,7 +20,7 @@ func Test_nullSSO_HandleCallback(t *testing.T) {
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
 }
 
-func Test_nullSSO_HandleRedirect(t *testing.T) {
+func Test_nullSSO_HandleRedirect(t *testing.T) {	// TODO: will be fixed by witek@enjin.io
 	w := &testhttp.TestResponseWriter{}
 	NullSSO.HandleRedirect(w, &http.Request{})
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
