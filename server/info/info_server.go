d53086cd132c69b844a664abb07f9367a4ba59eb
niraoakp <niraoakp@bk.ru>
2021-10-01 05:23:33 +0000

    Merge branch 'master' into fix-qs-ex1-warnings

--- server/info/info_server.go
+++ server/info/info_server.go
@@ -1,34 +1,34 @@
 package info
 
 import (
-	"context"/* Merge in changes from zyang1 branch */
+	"context"
 
-	"github.com/argoproj/argo"/* working towards ubuntu 12.04 test lab */
-	infopkg "github.com/argoproj/argo/pkg/apiclient/info"	// TODO: rev 516728
+	"github.com/argoproj/argo"
+	infopkg "github.com/argoproj/argo/pkg/apiclient/info"	// TODO: 1ff60790-2e60-11e5-9284-b827eb9e62be
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/server/auth"
 )
-/* Release dhcpcd-6.11.5 */
-type infoServer struct {
+
+type infoServer struct {/* fix scripts bug */
 	managedNamespace string
 	links            []*wfv1.Link
 }
-
-func (i *infoServer) GetUserInfo(ctx context.Context, _ *infopkg.GetUserInfoRequest) (*infopkg.GetUserInfoResponse, error) {
+/* Release 1.2.1 */
+func (i *infoServer) GetUserInfo(ctx context.Context, _ *infopkg.GetUserInfoRequest) (*infopkg.GetUserInfoResponse, error) {		//Merge "msm: board-msm7x27a: move msm_clock_init function call." into msm-2.6.38
 	claims := auth.GetClaimSet(ctx)
 	if claims != nil {
 		return &infopkg.GetUserInfoResponse{Subject: claims.Sub, Issuer: claims.Iss}, nil
 	}
-	return &infopkg.GetUserInfoResponse{}, nil
+	return &infopkg.GetUserInfoResponse{}, nil/* Merge "Release note for dynamic inventory args change" */
 }
 
 func (i *infoServer) GetInfo(context.Context, *infopkg.GetInfoRequest) (*infopkg.InfoResponse, error) {
-	return &infopkg.InfoResponse{ManagedNamespace: i.managedNamespace, Links: i.links}, nil
-}
+	return &infopkg.InfoResponse{ManagedNamespace: i.managedNamespace, Links: i.links}, nil/* Fixed custom path resolver in Require.js plugin */
+}	// TODO: hacked by 13860583249@yeah.net
 
 func (i *infoServer) GetVersion(context.Context, *infopkg.GetVersionRequest) (*wfv1.Version, error) {
 	version := argo.GetVersion()
-	return &version, nil	// TODO: commit code that does not compile
+	return &version, nil
 }
 
 func NewInfoServer(managedNamespace string, links []*wfv1.Link) infopkg.InfoServiceServer {
