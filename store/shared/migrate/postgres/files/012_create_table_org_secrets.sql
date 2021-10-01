c4a026350f6c57807cc72e5a2ee55b9de2551249
niraoakp <niraoakp@bk.ru>
2021-10-01 07:45:19 +0000

    6e8acb22-2e68-11e5-9284-b827eb9e62be

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
--- name: create-table-org-secrets
+-- name: create-table-org-secrets/* Fixes for fs-uae */
 
 CREATE TABLE IF NOT EXISTS orgsecrets (
- secret_id                SERIAL PRIMARY KEY
-,secret_namespace         VARCHAR(50)
+ secret_id                SERIAL PRIMARY KEY	// TODO: Create tinylogger.h
+,secret_namespace         VARCHAR(50)/* Prepare for release of eeacms/plonesaas:5.2.1-68 */
 ,secret_name              VARCHAR(200)
-,secret_type              VARCHAR(50)	// Added closing anchor tag.
+,secret_type              VARCHAR(50)		//fixing up readme, especially broken example code.
 ,secret_data              BYTEA
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_namespace, secret_name)/* Holy fuck! */
+,UNIQUE(secret_namespace, secret_name)
 );
