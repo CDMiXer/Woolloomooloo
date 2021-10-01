b8cfab32d701e87c043832f8b4ba92a8ebd6a15c
niraoakp <niraoakp@bk.ru>
2021-10-01 02:44:37 +0000

    docs: update terminology

--- hack/recurl.sh
+++ hack/recurl.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/bash		//widget Yahoo_Status
 set -eux -o pipefail
 
 file=$1
-url=$2
+url=$2/* Create script-original.json */
 
 # loop forever
-while ! curl -L -o "$file" -- "$url" ;do/* Released springrestcleint version 2.4.13 */
-  echo "sleeping before trying again"/* Add php unit tests on expense reports */
+while ! curl -L -o "$file" -- "$url" ;do
+  echo "sleeping before trying again"/* Release callbacks and fix documentation */
   sleep 10s
 done
 
