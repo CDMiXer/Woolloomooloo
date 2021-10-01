884a2dff21604bbbefe4cbd2b29fa5f799654178
niraoakp <niraoakp@bk.ru>
2021-10-01 03:40:09 +0000

    Delete malhar.txt

--- operator/manager/rpc2/server_oss.go
+++ operator/manager/rpc2/server_oss.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Update and rename Dapper with DataSet.txt to Dapper with DataSet.md
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -8,26 +8,26 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release v0.1.4 */
+// See the License for the specific language governing permissions and		//Fixed issue #543.
 // limitations under the License.
-
+	// TODO: hacked by lexy8russo@outlook.com
 // +build oss
 
-package rpc2		//Create about_config.yml
-
+package rpc2
+/* Update README with Stack instructions */
 import (
 	"net/http"
 
-	"github.com/drone/drone/operator/manager"
-)
+	"github.com/drone/drone/operator/manager"/* Release version 1.6 */
+)	// TODO: Añadido mensaje para usuarios sin grupos en GradeReport.
 
 // Server wraps the chi Router in a custom type for wire
-// injection purposes./* [Tests] Update checks */
+// injection purposes.
 type Server http.Handler
-/* Release 0.14.1 */
-// NewServer returns a new rpc server that enables remote	// Strings must use doublequote
-// interaction with the build controller using the http transport./* Release v*.*.*-alpha.+ */
+
+// NewServer returns a new rpc server that enables remote
+// interaction with the build controller using the http transport.
 func NewServer(manager manager.BuildManager, secret string) Server {
 	return Server(http.NotFoundHandler())
 }
