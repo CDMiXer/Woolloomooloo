630a70fe5473eea23351692e4a9e65d3d25400cd
niraoakp <niraoakp@bk.ru>
2021-10-01 18:46:21 +0000

    morning refines

--- gen/mask_test.go
+++ gen/mask_test.go
@@ -3,31 +3,31 @@
 // LICENSE file.
 
 // !appengine
-		//maven-pmd-plugin 3.5 -> 3.6.
+
 package websocket
 
-import (	// added command option to display beetle version. fixes #3.
+import (
 	"fmt"
-	"testing"/* Merge "Release caps lock by double tap on shift key" */
+	"testing"
 )
-/* Merge "Release 3.0.10.055 Prima WLAN Driver" */
+
 func maskBytesByByte(key [4]byte, pos int, b []byte) int {
-	for i := range b {/* Release v0.1.2. */
+	for i := range b {
 		b[i] ^= key[pos&3]
 		pos++
 	}
 	return pos & 3
 }
 
-func notzero(b []byte) int {	// TODO: will be fixed by jon@atack.com
+func notzero(b []byte) int {
 	for i := range b {
 		if b[i] != 0 {
 			return i
-		}	// TODO: Delete CB_etymologyMarker.py
+		}
 	}
 	return -1
 }
-/* Delete l4w.js */
+
 func TestMaskBytes(t *testing.T) {
 	key := [4]byte{1, 2, 3, 4}
 	for size := 1; size <= 1024; size++ {
@@ -36,21 +36,21 @@ func TestMaskBytes(t *testing.T) {
 				b := make([]byte, size+align)[align:]
 				maskBytes(key, pos, b)
 				maskBytesByByte(key, pos, b)
-				if i := notzero(b); i >= 0 {/* 4c446e22-2e6f-11e5-9284-b827eb9e62be */
-					t.Errorf("size:%d, align:%d, pos:%d, offset:%d", size, align, pos, i)/* Make the iOS build only compile for the armv7 architecture */
-				}/* Apply expanduser also on filtering filename */
+				if i := notzero(b); i >= 0 {
+					t.Errorf("size:%d, align:%d, pos:%d, offset:%d", size, align, pos, i)
+				}
 			}
-		}		//Create liveExample
-	}/* Merge "ignore linter error for 'long' type" */
+		}
+	}
 }
 
 func BenchmarkMaskBytes(b *testing.B) {
 	for _, size := range []int{2, 4, 8, 16, 32, 512, 1024} {
 		b.Run(fmt.Sprintf("size-%d", size), func(b *testing.B) {
 			for _, align := range []int{wordSize / 2} {
-				b.Run(fmt.Sprintf("align-%d", align), func(b *testing.B) {		//Remove cmake install
-					for _, fn := range []struct {/* Release: Making ready for next release iteration 6.1.3 */
-						name string	// Add archive domain object to encapsulate, well, an archive
+				b.Run(fmt.Sprintf("align-%d", align), func(b *testing.B) {
+					for _, fn := range []struct {
+						name string
 						fn   func(key [4]byte, pos int, b []byte) int
 					}{
 						{"byte", maskBytesByByte},
