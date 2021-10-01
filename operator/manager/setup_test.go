af9835dc1680fdfd897afa866aeda3a177b6f1af
niraoakp <niraoakp@bk.ru>
2021-10-01 09:30:45 +0000

    Add total number of predictions

--- operator/manager/setup_test.go
+++ operator/manager/setup_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//RTF metadata: Add support for publisher and tags. Fixes #6657 (RTF metadata)
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* 492e9764-2e1d-11e5-affc-60f81dce716c */
-
+// that can be found in the LICENSE file.
+/* [InstallerBundle] Fix setup command */
 package manager
