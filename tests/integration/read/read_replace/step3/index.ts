3e2247b7e455295c3ae4e59c683ae43d479a9f77
niraoakp <niraoakp@bk.ru>
2021-10-01 17:43:51 +0000

    Added a summary of processed data returned from collect task.

--- tests/integration/read/read_replace/step3/index.ts
+++ tests/integration/read/read_replace/step3/index.ts
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//Reviewed the User Interfaces documentation
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Plus sign visible with relative url */
-//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Update wsdl-dynamic.xml */
+//     http://www.apache.org/licenses/LICENSE-2.0/* pretty printed */
+//
+// Unless required by applicable law or agreed to in writing, software/* Merge "Move Release Notes Script to python" into androidx-master-dev */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -16,5 +16,5 @@ import { Resource } from "./resource";
 
 // Now go back the other way and make sure that "A" is external again.
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
-const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});/* Create TimProyek.md */
-
+const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
+/* we should call after_suite on last_test's class */
