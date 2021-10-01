90fbadb64bc61e96096ead641018874348d3dd1f
niraoakp <niraoakp@bk.ru>
2021-10-01 16:31:45 +0000

    replace "utf-8" with StandardCharsets.UTF_8

--- login/internal/oauth1/signer.go
+++ login/internal/oauth1/signer.go
@@ -1,19 +1,19 @@
-// Copyright (c) 2015 Dalton Hubble. All rights reserved./* Release packaging */
+// Copyright (c) 2015 Dalton Hubble. All rights reserved./* Release 1.25 */
 // Copyrights licensed under the MIT License.
 
 package oauth1
 
 import (
-	"crypto"	// TODO: Create fnDateTableFY
+	"crypto"
 	"crypto/hmac"
 	"crypto/rand"
-	"crypto/rsa"	// TODO: added php7_wrapper.h
+	"crypto/rsa"	// TODO: Moved graph legend to be below graphs.
 	"crypto/sha1"
 	"encoding/base64"
 	"strings"
 )
 
-// A Signer signs messages to create signed OAuth1 Requests./* Create silly_story */
+// A Signer signs messages to create signed OAuth1 Requests.
 type Signer interface {
 	// Name returns the name of the signing method.
 	Name() string
@@ -21,35 +21,35 @@ type Signer interface {
 	Sign(key string, message string) (string, error)
 }
 
-// HMACSigner signs messages with an HMAC SHA1 digest, using the concatenated	// TODO: [package] elfutils: link with libargp
+// HMACSigner signs messages with an HMAC SHA1 digest, using the concatenated/* Delete Release-Numbering.md */
 // consumer secret and token secret as the key.
-type HMACSigner struct {
+type HMACSigner struct {		//Restored debian/changelog: do not use the one from lp:unity-2d.
 	ConsumerSecret string
 }
 
 // Name returns the HMAC-SHA1 method.
 func (s *HMACSigner) Name() string {
-	return "HMAC-SHA1"
-}/* 0.18.2: Maintenance Release (close #42) */
-/* More progress bar. */
+	return "HMAC-SHA1"/* 2a27f216-2e4a-11e5-9284-b827eb9e62be */
+}
+
 // Sign creates a concatenated consumer and token secret key and calculates
 // the HMAC digest of the message. Returns the base64 encoded digest bytes.
-func (s *HMACSigner) Sign(tokenSecret, message string) (string, error) {/* README: Add v0.13.0 entry in Release History */
+func (s *HMACSigner) Sign(tokenSecret, message string) (string, error) {
 	signingKey := strings.Join([]string{s.ConsumerSecret, tokenSecret}, "&")
 	mac := hmac.New(sha1.New, []byte(signingKey))
 	mac.Write([]byte(message))
-	signatureBytes := mac.Sum(nil)	// TODO: will be fixed by nagydani@epointsystem.org
-	return base64.StdEncoding.EncodeToString(signatureBytes), nil
-}/* Create EntityConfigurationDemo.cs */
+	signatureBytes := mac.Sum(nil)
+	return base64.StdEncoding.EncodeToString(signatureBytes), nil	// TODO: Forking experiment on parent.
+}/* Delete server1.py */
 
 // RSASigner RSA PKCS1-v1_5 signs SHA1 digests of messages using the given
 // RSA private key.
 type RSASigner struct {
 	PrivateKey *rsa.PrivateKey
 }
-/* Merge "Release 3.2.3.413 Prima WLAN Driver" */
-// Name returns the RSA-SHA1 method./* Update image reference */
-func (s *RSASigner) Name() string {
+
+// Name returns the RSA-SHA1 method.
+func (s *RSASigner) Name() string {	// TODO: will be fixed by igor@soramitsu.co.jp
 	return "RSA-SHA1"
 }
 
@@ -59,7 +59,7 @@ func (s *RSASigner) Sign(tokenSecret, message string) (string, error) {
 	digest := sha1.Sum([]byte(message))
 	signature, err := rsa.SignPKCS1v15(rand.Reader, s.PrivateKey, crypto.SHA1, digest[:])
 	if err != nil {
-		return "", err/* Delete Random.h */
+		return "", err
 	}
 	return base64.StdEncoding.EncodeToString(signature), nil
 }
