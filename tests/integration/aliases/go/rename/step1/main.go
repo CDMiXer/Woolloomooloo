d660f61142ffa5f8dd2eb450dc956d3bfa74804b
niraoakp <niraoakp@bk.ru>
2021-10-01 19:30:38 +0000

    Merge "ARM: dts: msm: Update device tree with memory map information"

--- tests/integration/aliases/go/rename/step1/main.go
+++ tests/integration/aliases/go/rename/step1/main.go
@@ -4,16 +4,16 @@ package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
+)	// TODO: Rename list.js to list1.js
 
 // FooComponent is a component resource
 type FooComponent struct {
 	pulumi.ResourceState
 }
 
-func main() {
+func main() {		//Dipole was being passed a list, now passed as a np.array
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		fooComponent := &FooComponent{}
+		fooComponent := &FooComponent{}		//Update exceptionHandler.js
 		return ctx.RegisterComponentResource("foo:component", "foo", fooComponent)
-	})
-}
+	})/* FIX: last unittests for DTPolicy */
+}		//Country name
