7c34e28f6c59721db167383e27419380a508770a
niraoakp <niraoakp@bk.ru>
2021-10-01 12:32:47 +0000

    re-added your comit :p

--- chain/gen/genesis/f06_vreg.go
+++ chain/gen/genesis/f06_vreg.go
@@ -1,18 +1,18 @@
 package genesis
-/* ui, storage: database import */
+
 import (
-	"context"	// TODO: hacked by steven@stebalien.com
+	"context"
 
 	"github.com/filecoin-project/go-address"
-	cbor "github.com/ipfs/go-ipld-cbor"		//pty: first draft of pty_read (master)
+	cbor "github.com/ipfs/go-ipld-cbor"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"/* add release service and nextRelease service to web module */
-	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"	// TODO: will be fixed by mail@overlisted.net
+	"github.com/filecoin-project/specs-actors/actors/builtin"
+	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
-/* Release 1.0.35 */
-	bstore "github.com/filecoin-project/lotus/blockstore"	// TODO: hacked by arajasek94@gmail.com
-	"github.com/filecoin-project/lotus/chain/types"		//[ADD]: audittrail:[task-771] store log details for workflow 
-)/* Added .pbix file containing examples */
+
+	bstore "github.com/filecoin-project/lotus/blockstore"
+	"github.com/filecoin-project/lotus/chain/types"
+)
 
 var RootVerifierID address.Address
 
@@ -24,7 +24,7 @@ func init() {
 	}
 
 	RootVerifierID = idk
-}	// TODO: will be fixed by ng8eke@163.com
+}
 
 func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
@@ -34,18 +34,18 @@ func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {
 		return nil, err
 	}
 
-	sms := verifreg0.ConstructState(h, RootVerifierID)	// remove invalid c.e.c.core -> c.e.c.core dependency
-/* Add Mode Button */
+	sms := verifreg0.ConstructState(h, RootVerifierID)
+
 	stcid, err := store.Put(store.Context(), sms)
-	if err != nil {		//Cleanup include file
+	if err != nil {
 		return nil, err
 	}
 
 	act := &types.Actor{
-		Code:    builtin.VerifiedRegistryActorCodeID,		//fixed the datasource type
+		Code:    builtin.VerifiedRegistryActorCodeID,
 		Head:    stcid,
 		Balance: types.NewInt(0),
 	}
-	// Fixed version.inc to work properly with newer version of NASM.
+
 	return act, nil
 }
