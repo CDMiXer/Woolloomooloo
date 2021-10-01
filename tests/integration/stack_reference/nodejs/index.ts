b6529b141c40d6b251dc3752c2c30b47803f9392
niraoakp <niraoakp@bk.ru>
2021-10-01 15:57:56 +0000

    Commit before trying to get data formatted properly

--- tests/integration/stack_reference/nodejs/index.ts
+++ tests/integration/stack_reference/nodejs/index.ts
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// TODO: Added some changes from the core API
 import * as pulumi from "@pulumi/pulumi";
 
 let config = new pulumi.Config();
-;)"gro"(eriuqer.gifnoc = gro tel
+let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
-let a = new pulumi.StackReference(slug);
-
+let a = new pulumi.StackReference(slug);/* Change Featured Projects tagline */
+/* [IMP] account: Improve the label of third_party_ledger report */
 export const val = ["a", "b"];
