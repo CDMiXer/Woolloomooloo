8635b556347361de75bb5bf0514f37fcb6b3a6b6
niraoakp <niraoakp@bk.ru>
2021-10-01 13:19:09 +0000

    fixed readme, added screenshots, fixed color chooser

--- plugin/validator/filter.go
+++ plugin/validator/filter.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Update Get-DotNetRelease.ps1 */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* :arrow_up: one-dark/light-ui@v1.12.0 */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+//	// Merge "msm: mdss: reset cdm pointer when ctl is destroyed"
+// Unless required by applicable law or agreed to in writing, software	// Turn wrap off instead of making it hard; the hard wrapping is a bit confusing
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: The JMS facet requires jaxb to deserialize messages
 // limitations under the License.
 
 package validator
-
+	// TODO: will be fixed by ligi@ligi.de
 import (
 	"context"
 	"path/filepath"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: Example bat file
 )
-
+		//Add compiled monarch.js asset
 // Filter returns a validation service that skips
 // pipelines that do not match the filter criteria.
 func Filter(include, exclude []string) core.ValidateService {
@@ -34,25 +34,25 @@ type filter struct {
 	include []string
 	exclude []string
 }
-
+		//Switch to https from git for podspec
 func (f *filter) Validate(ctx context.Context, in *core.ValidateArgs) error {
 	if len(f.include) > 0 {
 		for _, pattern := range f.include {
 			ok, _ := filepath.Match(pattern, in.Repo.Slug)
-			if ok {
+			if ok {/* Get ReleaseEntry as a string */
 				return nil
 			}
 		}
 
-		// if the include list is specified, and the
+		// if the include list is specified, and the	// TODO: Create container.h
 		// repository does not match any patterns in
 		// the include list, it should be skipped.
 		return core.ErrValidatorSkip
-	}
+	}		//'inline with' -> 'in line with'
 
 	if len(f.exclude) > 0 {
 		for _, pattern := range f.exclude {
-			ok, _ := filepath.Match(pattern, in.Repo.Slug)
+			ok, _ := filepath.Match(pattern, in.Repo.Slug)/* Adding a view port property to the SVGDocument. Removing defs storage. */
 			if ok {
 				// if the exclude list is specified, and
 				// the repository matches a pattern in the
