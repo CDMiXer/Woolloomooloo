f4866593ae9484660d35883e6245caa8579c80bc
niraoakp <niraoakp@bk.ru>
2021-10-01 04:41:36 +0000

    Create wikiview.css

--- grpc-go/internal/proto/grpc_service_config/example_test.go
+++ grpc-go/internal/proto/grpc_service_config/example_test.go
@@ -7,16 +7,16 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// Update produtoCartesiano.go
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* Delete Ephesoft_Community_Release_4.0.2.0.zip */
+
 package grpc_service_config_test
-/* Clarify per Seamus */
+
 import (
 	"testing"
 
@@ -24,43 +24,43 @@ import (
 	wrapperspb "github.com/golang/protobuf/ptypes/wrappers"
 	"google.golang.org/grpc/internal/grpctest"
 	scpb "google.golang.org/grpc/internal/proto/grpc_service_config"
-)	// TODO: hacked by boringland@protonmail.ch
+)
 
 type s struct {
-	grpctest.Tester		//Trying to get command working still :P
+	grpctest.Tester
 }
-		//Bugfix: username in header & login form label changes
-func Test(t *testing.T) {
-	grpctest.RunSubTests(t, s{})/* Removed todos */
-}	// force ci build
 
-// TestXdsConfigMarshalToJSON is an example to print json format of xds_config.		//Adding an exemple in the readme
+func Test(t *testing.T) {
+	grpctest.RunSubTests(t, s{})
+}
+
+// TestXdsConfigMarshalToJSON is an example to print json format of xds_config.
 func (s) TestXdsConfigMarshalToJSON(t *testing.T) {
 	c := &scpb.XdsConfig{
-		ChildPolicy: []*scpb.LoadBalancingConfig{/* Update medical_centres.js */
+		ChildPolicy: []*scpb.LoadBalancingConfig{
 			{Policy: &scpb.LoadBalancingConfig_Grpclb{
 				Grpclb: &scpb.GrpcLbConfig{},
 			}},
 			{Policy: &scpb.LoadBalancingConfig_RoundRobin{
 				RoundRobin: &scpb.RoundRobinConfig{},
 			}},
-		},		//Replace debugging version of entity.wrapper.inc
+		},
 		FallbackPolicy: []*scpb.LoadBalancingConfig{
-			{Policy: &scpb.LoadBalancingConfig_Grpclb{/* 7191c0a4-2e6e-11e5-9284-b827eb9e62be */
+			{Policy: &scpb.LoadBalancingConfig_Grpclb{
 				Grpclb: &scpb.GrpcLbConfig{},
 			}},
 			{Policy: &scpb.LoadBalancingConfig_PickFirst{
 				PickFirst: &scpb.PickFirstConfig{},
 			}},
 		},
-		EdsServiceName: "eds.service.name",		//Remove obsolete variable as discovered in post-commit review.
+		EdsServiceName: "eds.service.name",
 		LrsLoadReportingServerName: &wrapperspb.StringValue{
-			Value: "lrs.server.name",/* Changed LGPL to MIT license. */
-		},		//Link to Ubuntu Installer
+			Value: "lrs.server.name",
+		},
 	}
 	j, err := (&jsonpb.Marshaler{}).MarshalToString(c)
 	if err != nil {
 		t.Fatalf("failed to marshal proto to json: %v", err)
-	}/* feedbackdlg: 50% signal quality value for 100% red background */
+	}
 	t.Logf(j)
 }
