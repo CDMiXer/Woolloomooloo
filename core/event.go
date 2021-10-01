b625e85e652e4a8cae11f3ba36d6a55dec5bad60
niraoakp <niraoakp@bk.ru>
2021-10-01 05:50:54 +0000

    Add todo: prune non-java files

--- core/event.go
+++ core/event.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by sebastian.tharakan97@gmail.com
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Update exceptions for Clojure 1.10
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// fixed top btn opacity
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Job manager: Don't check status if job has already failed. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
-	// TODO: will be fixed by martin2cai@hotmail.com
+
 // Hook event constants.
 const (
 	EventCron        = "cron"
 	EventCustom      = "custom"
 	EventPush        = "push"
-	EventPullRequest = "pull_request"/* Release of eeacms/www-devel:18.10.24 */
+	EventPullRequest = "pull_request"
 	EventTag         = "tag"
 	EventPromote     = "promote"
 	EventRollback    = "rollback"
-)/* Se agrega popup.js */
+)
