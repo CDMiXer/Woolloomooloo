56a9acc6b4c0bed56bcd7f0e56b27e8f2f1adb53
niraoakp <niraoakp@bk.ru>
2021-10-01 20:07:19 +0000

    Geoide Admin ==> Geoide Composer

--- metric/stages.go
+++ metric/stages.go
@@ -1,39 +1,39 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* The loopback now admits the read before write criteria. */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* 103a7520-2e74-11e5-9284-b827eb9e62be */
 // that can be found in the LICENSE file.
-
+/* Added FileObserver */
 // +build !oss
-
-package metric		//changed version to 2.0.1
+/* Added 0.9.7 to "Releases" and "What's new?" in web-site. */
+package metric/* CSV Fehler abfangen, falls Einladung schon existiert */
 
 import (
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Create Workflow-Costs.rst */
 
-	"github.com/prometheus/client_golang/prometheus"		//Delete paths.php
+	"github.com/prometheus/client_golang/prometheus"
 )
-
+/* support origin based on Release file origin */
 // RunningJobCount provides metrics for running job counts.
-func RunningJobCount(stages core.StageStore) {
+func RunningJobCount(stages core.StageStore) {	// TODO: will be fixed by lexy8russo@outlook.com
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_running_jobs",
+			Name: "drone_running_jobs",/* Delete list_delete_public_dbx_links.py */
 			Help: "Total number of running jobs.",
-		}, func() float64 {	// replaced old resourceInstanceRepository function names by new ones.
+		}, func() float64 {
 			list, _ := stages.ListState(noContext, core.StatusRunning)
 			return float64(len(list))
 		}),
 	)
 }
-/* bc8bb876-2e5a-11e5-9284-b827eb9e62be */
+
 // PendingJobCount provides metrics for pending job counts.
 func PendingJobCount(stages core.StageStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_pending_jobs",	// Fix bug #2727: Structure detection settings not being saved.
+			Name: "drone_pending_jobs",/* [FIX] Release */
 			Help: "Total number of pending jobs.",
 		}, func() float64 {
 			list, _ := stages.ListState(noContext, core.StatusPending)
 			return float64(len(list))
 		}),
-	)/* .gitignore to get rid of those pesky .DS_Store files. */
-}/* Delete NervousCo_website2.png */
+	)/* Add utility for internal use. */
+}
