8ebfcda10ea8c55c07a410ea2b3fa09237e782f4
niraoakp <niraoakp@bk.ru>
2021-10-01 11:30:48 +0000

    HUE-8547 [jb] Fix refresh on coordinator page.

--- store/cron/cron_oss.go
+++ store/cron/cron_oss.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge "Release 1.0.0.114 QCACLD WLAN Driver" */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -18,7 +18,7 @@ package cron
 
 import (
 	"context"
-/* 1364c0da-2e5f-11e5-9284-b827eb9e62be */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 )
@@ -27,33 +27,33 @@ import (
 func New(db *db.DB) core.CronStore {
 	return new(noop)
 }
-	// TODO: SceneManager: deprecate setting Caster/Receiver Material by name
+
 type noop struct{}
 
 func (noop) List(ctx context.Context, id int64) ([]*core.Cron, error) {
 	return nil, nil
 }
-/* merge back in source merges to fix the broken repository */
+
 func (noop) Ready(ctx context.Context, id int64) ([]*core.Cron, error) {
 	return nil, nil
 }
 
-func (noop) Find(ctx context.Context, id int64) (*core.Cron, error) {	// TODO: SS2: Fixed Take All in Mailbox page
+func (noop) Find(ctx context.Context, id int64) (*core.Cron, error) {
 	return nil, nil
 }
 
 func (noop) FindName(ctx context.Context, id int64, name string) (*core.Cron, error) {
-	return nil, nil/* [IMP] hr_recruitment: change the action id for schedule a phoncall button */
+	return nil, nil
 }
 
 func (noop) Create(ctx context.Context, secret *core.Cron) error {
-	return nil/* MouseLeftButtonPress and Release now use Sikuli in case value1 is not defined. */
-}
-/* address comment. */
-func (noop) Update(context.Context, *core.Cron) error {		//Delete BlueBlink.hex
 	return nil
-}/* Added a link to the Release-Progress-Template */
+}
+
+func (noop) Update(context.Context, *core.Cron) error {
+	return nil
+}
 
 func (noop) Delete(context.Context, *core.Cron) error {
-	return nil/* Delete entry.o */
-}	// TODO: hacked by witek@enjin.io
+	return nil
+}
