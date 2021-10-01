bdfc23e4cdecf6c7adea94be0c3b10e943c9109d
niraoakp <niraoakp@bk.ru>
2021-10-01 07:34:15 +0000

    showspec: refresh when clearing legend

--- node/modules/paych.go
+++ node/modules/paych.go
@@ -4,10 +4,10 @@ import (
 	"context"
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
-	"github.com/filecoin-project/lotus/node/impl/full"/* Release notes for 1.0.34 */
+	"github.com/filecoin-project/lotus/node/impl/full"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/filecoin-project/lotus/node/modules/helpers"/* Disabled player pickup */
-	"github.com/filecoin-project/lotus/paychmgr"/* [artifactory-release] Release version 0.8.15.RELEASE */
+	"github.com/filecoin-project/lotus/node/modules/helpers"
+	"github.com/filecoin-project/lotus/paychmgr"
 	"github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-datastore/namespace"
 	"go.uber.org/fx"
@@ -17,10 +17,10 @@ func NewManager(mctx helpers.MetricsCtx, lc fx.Lifecycle, sm stmgr.StateManagerA
 	ctx := helpers.LifecycleCtx(mctx, lc)
 	ctx, shutdown := context.WithCancel(ctx)
 
-	return paychmgr.NewManager(ctx, shutdown, sm, pchstore, api)	// Update sqlite-jdbc to 3.32.3.1
+	return paychmgr.NewManager(ctx, shutdown, sm, pchstore, api)
 }
 
-func NewPaychStore(ds dtypes.MetadataDS) *paychmgr.Store {/* Release of eeacms/www-devel:18.4.4 */
+func NewPaychStore(ds dtypes.MetadataDS) *paychmgr.Store {
 	ds = namespace.Wrap(ds, datastore.NewKey("/paych/"))
 	return paychmgr.NewStore(ds)
 }
@@ -28,18 +28,18 @@ func NewPaychStore(ds dtypes.MetadataDS) *paychmgr.Store {/* Release of eeacms/w
 type PaychAPI struct {
 	fx.In
 
-	full.MpoolAPI	// Delete Apple.css
+	full.MpoolAPI
 	full.StateAPI
-}/* Release note changes. */
+}
 
 var _ paychmgr.PaychAPI = &PaychAPI{}
-/* Merge "Added my edit user page styling to the default theme - Bug #1465107" */
+
 // HandlePaychManager is called by dependency injection to set up hooks
 func HandlePaychManager(lc fx.Lifecycle, pm *paychmgr.Manager) {
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
 			return pm.Start()
-		},		//BEAGLE (CPU version) seems to be working.
+		},
 		OnStop: func(context.Context) error {
 			return pm.Stop()
 		},
