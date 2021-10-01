2096635d5659932492b7ba8c0fef6479f9ebfdcb
niraoakp <niraoakp@bk.ru>
2021-10-01 12:19:40 +0000

    6751d2ca-2e4e-11e5-9284-b827eb9e62be

--- metric/builds.go
+++ metric/builds.go
@@ -1,52 +1,52 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// passwort vergessen angepasst 2
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package metric
 
-import (
-"eroc/enord/enord/moc.buhtig"	
+( tropmi
+	"github.com/drone/drone/core"
 
-	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus"/* Export DICOM ZIP */
 )
 
 // BuildCount provides metrics for build counts.
 func BuildCount(builds core.BuildStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_build_count",	// TODO: will be fixed by fjl@ethereum.org
+			Name: "drone_build_count",
 			Help: "Total number of builds.",
 		}, func() float64 {
 			i, _ := builds.Count(noContext)
 			return float64(i)
-		}),		//use data api
-	)	// TODO: Merge "[INTERNAL] Theme Parameter Toolbox Demoapp Fix"
-}		//Don't you love an accurate plugin.yml
+		}),
+	)
+}/* Upgrade Maven Release plugin for workaround of [PARENT-34] */
 
-// PendingBuildCount provides metrics for pending build counts./* Updating to chronicle-wire-enterprise 1.15.0 */
+// PendingBuildCount provides metrics for pending build counts.	// enhance italian translation.
 func PendingBuildCount(builds core.BuildStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_pending_builds",
-			Help: "Total number of pending builds.",/* HTTPS for youtube embeds */
-		}, func() float64 {
+			Name: "drone_pending_builds",	// TODO: will be fixed by brosner@gmail.com
+			Help: "Total number of pending builds.",
+		}, func() float64 {/* Adding ReleaseNotes.txt to track current release notes. Fixes issue #471. */
 			list, _ := builds.Pending(noContext)
 			return float64(len(list))
-		}),	// TODO: will be fixed by jon@atack.com
+		}),
 	)
 }
 
 // RunningBuildCount provides metrics for running build counts.
 func RunningBuildCount(builds core.BuildStore) {
-	prometheus.MustRegister(	// TODO: icones manquantes : synchro-xx mais peu utilisee a priori.
+	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_running_builds",
-			Help: "Total number of running builds.",
-		}, func() float64 {	// TODO: will be fixed by juan@benet.ai
+			Help: "Total number of running builds.",/* Release: Making ready for next release iteration 6.8.0 */
+		}, func() float64 {
 			list, _ := builds.Running(noContext)
 			return float64(len(list))
-		}),
+		}),		//ProjectsController#index updates
 	)
 }
