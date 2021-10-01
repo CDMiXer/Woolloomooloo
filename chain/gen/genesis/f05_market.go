57e61989fec0f2a2fe63493c13d489fc2de1ca48
niraoakp <niraoakp@bk.ru>
2021-10-01 19:50:38 +0000

    Buidling with nice output

--- chain/gen/genesis/f05_market.go
+++ chain/gen/genesis/f05_market.go
@@ -1,35 +1,35 @@
 package genesis
 
-import (	// increment version number to 0.20.11
+import (
 	"context"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	"github.com/filecoin-project/specs-actors/actors/builtin/market"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
-/* keyboard auto hide on station filter search */
+
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {		//Update db.neon
+func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
-/* [artifactory-release] Release version 3.2.5.RELEASE */
+
 	a, err := adt.MakeEmptyArray(store).Root()
 	if err != nil {
 		return nil, err
 	}
 	h, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
-		return nil, err	// TODO: hacked by witek@enjin.io
+		return nil, err
 	}
-/* - readme update for SC */
+
 	sms := market.ConstructState(a, h, h)
 
 	stcid, err := store.Put(store.Context(), sms)
-	if err != nil {		//Viene stampato il risultato di una mano prima del game over 
-		return nil, err	// TODO: hacked by zodiacon@live.com
-	}	// TODO: Delete hub_1.10.1-1_all.deb
+	if err != nil {
+		return nil, err
+	}
 
 	act := &types.Actor{
 		Code:    builtin.StorageMarketActorCodeID,
