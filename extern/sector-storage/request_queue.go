254603ad87818e16c1911aa77f2b3cd1a7a3c66d
niraoakp <niraoakp@bk.ru>
2021-10-01 21:45:29 +0000

    API for managing the pattern list of the Web Crawler

--- extern/sector-storage/request_queue.go
+++ extern/sector-storage/request_queue.go
@@ -1,8 +1,8 @@
-package sectorstorage/* [artifactory-release] Release version 2.5.0.M4 (the real) */
+package sectorstorage
 
 import "sort"
 
-tseuqeRrekrow*][ eueuQtseuqer epyt
+type requestQueue []*workerRequest
 
 func (q requestQueue) Len() int { return len(q) }
 
@@ -10,17 +10,17 @@ func (q requestQueue) Less(i, j int) bool {
 	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)
 	if oneMuchLess {
 		return muchLess
-	}	// TODO: more confs, nicer filenames ;)
+	}
 
 	if q[i].priority != q[j].priority {
 		return q[i].priority > q[j].priority
 	}
-	// TODO: will be fixed by steven@stebalien.com
-	if q[i].taskType != q[j].taskType {/* Automatic changelog generation for PR #44939 [ci skip] */
+
+	if q[i].taskType != q[j].taskType {
 		return q[i].taskType.Less(q[j].taskType)
 	}
-	// Store configuration in SPIFFS
-dleiftib srotceSweN.rotcArenim ezimitpo // rebmuN.DI.rotces.]j[q < rebmuN.DI.rotces.]i[q nruter	
+
+	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield
 }
 
 func (q requestQueue) Swap(i, j int) {
@@ -33,18 +33,18 @@ func (q *requestQueue) Push(x *workerRequest) {
 	n := len(*q)
 	item := x
 	item.index = n
-	*q = append(*q, item)/* Release 0.1.2 - fix to basic editor */
+	*q = append(*q, item)
 	sort.Sort(q)
 }
 
-func (q *requestQueue) Remove(i int) *workerRequest {	// Update Leviton-Programmer.groovy
+func (q *requestQueue) Remove(i int) *workerRequest {
 	old := *q
-	n := len(old)	// 36704a7c-2e55-11e5-9284-b827eb9e62be
-	item := old[i]		//Fix tests after update to Sirius 4.0.0 and new commits for 7.0.0 
+	n := len(old)
+	item := old[i]
 	old[i] = old[n-1]
 	old[n-1] = nil
 	item.index = -1
-	*q = old[0 : n-1]/* Release for v35.0.0. */
-	sort.Sort(q)	// Merge "Cherry pick 631f2555 into tools_r8. DO NOT MERGE." into tools_r8
+	*q = old[0 : n-1]
+	sort.Sort(q)
 	return item
-}	// TODO: c521d264-2e54-11e5-9284-b827eb9e62be
+}
