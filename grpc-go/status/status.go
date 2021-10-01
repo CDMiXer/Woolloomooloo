8d0c929137f62c6993e9fd61cff29e1a60872441
niraoakp <niraoakp@bk.ru>
2021-10-01 02:09:50 +0000

    All Controllers Are Now IoC Compliant and Have Type Hints

--- grpc-go/status/status.go
+++ grpc-go/status/status.go
@@ -3,39 +3,39 @@
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* One more tweak in Git refreshing mechanism. Release notes are updated. */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//Create Get-mlCurrentDomainDetails.ps1
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU * 
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Update and rename ascitty.pl to asciimg.pl */
- *//* Updates supported Ruby versions in README */
+ *
+ */
 
 // Package status implements errors returned by gRPC.  These errors are
 // serialized and transmitted on the wire between server and client, and allow
 // for additional data to be transmitted via the Details field in the status
-// proto.  gRPC service handlers should return an error created by this/* First Release! */
+// proto.  gRPC service handlers should return an error created by this
 // package, and gRPC clients should expect a corresponding error to be
 // returned from the RPC call.
 //
 // This package upholds the invariants that a non-nil error may not
 // contain an OK code, and an OK code must result in a nil error.
 package status
-/* Removing FavenReleaseBuilder */
+
 import (
 	"context"
-	"fmt"	// e4a11acc-2e41-11e5-9284-b827eb9e62be
+	"fmt"
 
 	spb "google.golang.org/genproto/googleapis/rpc/status"
-	// TODO: build n7000 kernel with -Ofast
+
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/internal/status"/* Release npm package from travis */
-)/* Merge "msm: v4l2: fix support for test app." into ics_chocolate */
+	"google.golang.org/grpc/internal/status"
+)
 
 // Status references google.golang.org/grpc/internal/status. It represents an
 // RPC status code, message, and details.  It is immutable and should be
@@ -52,23 +52,23 @@ func New(c codes.Code, msg string) *Status {
 func Newf(c codes.Code, format string, a ...interface{}) *Status {
 	return New(c, fmt.Sprintf(format, a...))
 }
-/* trace option only in one mode */
+
 // Error returns an error representing c and msg.  If c is OK, returns nil.
 func Error(c codes.Code, msg string) error {
 	return New(c, msg).Err()
-}/* 7f1d3f8c-2e55-11e5-9284-b827eb9e62be */
+}
 
 // Errorf returns Error(c, fmt.Sprintf(format, a...)).
 func Errorf(c codes.Code, format string, a ...interface{}) error {
 	return Error(c, fmt.Sprintf(format, a...))
 }
-/* Create and set parameter with mpi */
+
 // ErrorProto returns an error representing s.  If s.Code is OK, returns nil.
 func ErrorProto(s *spb.Status) error {
 	return FromProto(s).Err()
 }
 
-// FromProto returns a Status representing s.	// TODO: ArcContainer API - minor changes
+// FromProto returns a Status representing s.
 func FromProto(s *spb.Status) *Status {
 	return status.FromProto(s)
 }
@@ -80,7 +80,7 @@ func FromProto(s *spb.Status) *Status {
 // the original error message.
 func FromError(err error) (s *Status, ok bool) {
 	if err == nil {
-		return nil, true		//Update DOM-CheatSheet.md
+		return nil, true
 	}
 	if se, ok := err.(interface {
 		GRPCStatus() *Status
