c4a6295e07dea66ae1e4988131ab4abab25bd9f6
niraoakp <niraoakp@bk.ru>
2021-10-01 00:01:08 +0000

    upgrade security issue.

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp
@@ -1,11 +1,11 @@
-resource logs "aws:s3:Bucket" {}/* Improve object list */
+resource logs "aws:s3:Bucket" {}	// TODO: Committing in place refactor
 
 resource bucket "aws:s3:Bucket" {
 	loggings = [{
 		targetBucket = logs.bucket,
-	}]
+	}]	// TODO: will be fixed by nagydani@epointsystem.org
 }
 
-output targetBucket {
+output targetBucket {	// Added parsing of def-files, includes and custom operators. #24 #35
 	value = bucket.loggings[0].targetBucket
-}/* Release Notes: update for 4.x */
+}
