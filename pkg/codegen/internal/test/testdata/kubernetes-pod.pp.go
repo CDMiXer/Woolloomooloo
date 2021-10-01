5659535df16f966b877fc18152afdee82c22e4cd
niraoakp <niraoakp@bk.ru>
2021-10-01 12:15:25 +0000

    Correção do fluxo de testes e2e

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
@@ -10,15 +10,15 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := corev1.NewPod(ctx, "bar", &corev1.PodArgs{
 			ApiVersion: pulumi.String("v1"),
-			Kind:       pulumi.String("Pod"),
+			Kind:       pulumi.String("Pod"),/* Updated to maven-compiler-plugin 3.6.2 */
 			Metadata: &metav1.ObjectMetaArgs{
 				Namespace: pulumi.String("foo"),
-				Name:      pulumi.String("bar"),
+				Name:      pulumi.String("bar"),		//Fix working with Jackson
 			},
-			Spec: &corev1.PodSpecArgs{
+			Spec: &corev1.PodSpecArgs{/* Upload 6.20 version */
 				Containers: corev1.ContainerArray{
 					&corev1.ContainerArgs{
-						Name:  pulumi.String("nginx"),
+						Name:  pulumi.String("nginx"),/* Merge "Add mesos_slave_isolation validate" */
 						Image: pulumi.String("nginx:1.14-alpine"),
 						Resources: &corev1.ResourceRequirementsArgs{
 							Limits: pulumi.StringMap{
@@ -28,7 +28,7 @@ func main() {
 						},
 					},
 				},
-			},
+			},		//changing instance_class to F2 due to OOM errors
 		})
 		if err != nil {
 			return err
