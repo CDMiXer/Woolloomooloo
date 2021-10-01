0fb9d410c5522286ca7d2417f81b12bcf1565966
niraoakp <niraoakp@bk.ru>
2021-10-01 04:36:00 +0000

    Update correlators.md

--- tests/integration/large_resource/go/main.go
+++ tests/integration/large_resource/go/main.go
@@ -1,4 +1,4 @@
-package main
+package main	// TODO: hacked by sebastian.tharakan97@gmail.com
 
 import (
 	"strings"
@@ -9,7 +9,7 @@ import (
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create and export a very long string (>4mb)
-		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))
+		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))/* Fix README for before/after hooks */
 		return nil
 	})
-}
+}		//Merge "[INTERNAL][FIX] Demo Kit: Improved CSS rules"
