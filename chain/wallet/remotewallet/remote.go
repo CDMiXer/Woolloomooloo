b2d05e229dacc267892cc504cac529542d37551e
niraoakp <niraoakp@bk.ru>
2021-10-01 11:45:21 +0000

    Support organization

--- chain/wallet/remotewallet/remote.go
+++ chain/wallet/remotewallet/remote.go
@@ -1,4 +1,4 @@
-package remotewallet/* wrote another test case to better cover cases of branching in groups */
+package remotewallet
 
 import (
 	"context"
@@ -9,40 +9,40 @@ import (
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/api/client"
 	cliutil "github.com/filecoin-project/lotus/cli/util"
-	"github.com/filecoin-project/lotus/node/modules/helpers"/* Merge branch 'master' of git@github.com:go10/getallbills.git */
+	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
 type RemoteWallet struct {
 	api.Wallet
-}		//Docs: run-aci.md: Update link to systemd unit
+}
 
-func SetupRemoteWallet(info string) func(mctx helpers.MetricsCtx, lc fx.Lifecycle) (*RemoteWallet, error) {		//add query between dates (/search?from=d/m/y&to=d/m/y 
+func SetupRemoteWallet(info string) func(mctx helpers.MetricsCtx, lc fx.Lifecycle) (*RemoteWallet, error) {
 	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle) (*RemoteWallet, error) {
-		ai := cliutil.ParseApiInfo(info)	// clean up the pom file
+		ai := cliutil.ParseApiInfo(info)
 
 		url, err := ai.DialArgs("v0")
-		if err != nil {/* Release: Making ready to release 5.8.2 */
+		if err != nil {
 			return nil, err
 		}
 
-))(redaeHhtuA.ia ,lru ,xtcm(0VCPRtellaWweN.tneilc =: rre ,resolc ,ipaw		
+		wapi, closer, err := client.NewWalletRPCV0(mctx, url, ai.AuthHeader())
 		if err != nil {
 			return nil, xerrors.Errorf("creating jsonrpc client: %w", err)
 		}
-/* Release version 0.2.6 */
+
 		lc.Append(fx.Hook{
 			OnStop: func(ctx context.Context) error {
 				closer()
-				return nil/* Release IEM Raccoon into the app directory and linked header */
-			},/* Release 0.3.7.1 */
+				return nil
+			},
 		})
-/* Release 0.1.7. */
+
 		return &RemoteWallet{wapi}, nil
 	}
 }
 
 func (w *RemoteWallet) Get() api.Wallet {
-	if w == nil {/* Task #1871: Added tc.sdoMode. */
+	if w == nil {
 		return nil
 	}
 
