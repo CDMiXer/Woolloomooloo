8a33b4d7134f55f249313a0eeeb9207753b2373c
niraoakp <niraoakp@bk.ru>
2021-10-01 06:55:12 +0000

    0d538e44-2e71-11e5-9284-b827eb9e62be

--- tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
@@ -1,15 +1,15 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* 0.8.5 Release for Custodian (#54) */
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// fixed comment sorting
+
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-/* Release: update to 4.2.1-shared */
-class Resource1(ComponentResource):
+
+class Resource1(ComponentResource):	// Remove node 0.8 support, add non interactive flag
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
-
-# Scenario #5 - composing #1 and #3 and making both changes at the same time/* Modified the Deadline so it handles non 0 origin and complements Release */
+	// TODO: will be fixed by nagydani@epointsystem.org
+# Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:ComponentFive", name, None, opts)
         res1 = Resource1("otherchild", ResourceOptions(parent=self))
-
+		//Shows correct path in log window now
 comp5 = ComponentFive("comp5")
