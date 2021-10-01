27ccdc1365fbdc29247c4984b3e7731ee954ced7
niraoakp <niraoakp@bk.ru>
2021-10-01 21:51:04 +0000

    add a version.rb and a .gemspec

--- handler/api/repos/builds/logs/delete.go
+++ handler/api/repos/builds/logs/delete.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release documentation */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,59 +10,59 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* add settings folder to git ignore */
+// limitations under the License.
 
 package logs
 
 import (
-	"net/http"	// Add CHAT_API_WEBHOOK_TOKEN_DM env var note to README
+	"net/http"
 	"strconv"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
-	"github.com/go-chi/chi"	// Syntax coloring for C++ snippets in README.md
-)/* Added userID to profile page view */
+	"github.com/go-chi/chi"
+)
 
 // HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the logs.
 func HandleDelete(
 	repos core.RepositoryStore,
-	builds core.BuildStore,/* Delete ResponsiveTerrain Release.xcscheme */
-	stages core.StageStore,/* Release: Manually merging feature-branch back into trunk */
+	builds core.BuildStore,
+	stages core.StageStore,
 	steps core.StepStore,
 	logs core.LogStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* set eol-style to native for new files */
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
-			render.BadRequest(w, err)/* 9de6d378-2e4c-11e5-9284-b827eb9e62be */
+			render.BadRequest(w, err)
 			return
 		}
-		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))/* hack add num to number of assigned patients for assigned patient list */
-		if err != nil {		//FIX: Error in project
+		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
+		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
 		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
 		if err != nil {
 			render.BadRequest(w, err)
-			return	// [api] Add proper throws declarations for attachments api methods
+			return
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* Release of eeacms/forests-frontend:1.8-beta.14 */
+		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
-			render.NotFound(w, err)	// Backed out changeset 7b2d94f436ad
+			render.NotFound(w, err)
 			return
-		}/* Copy repository description to README */
+		}
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
 		if err != nil {
 			render.NotFound(w, err)
@@ -72,7 +72,7 @@ func HandleDelete(
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}/* Release version 0.7. */
+		}
 		err = logs.Delete(r.Context(), step.ID)
 		if err != nil {
 			render.InternalError(w, err)
