f04cd3f9f56cf1031a1f4e8205fdaf1509d61934
niraoakp <niraoakp@bk.ru>
2021-10-01 18:02:23 +0000

    Fixed typo and some wording

--- handler/api/events/logs_test.go
+++ handler/api/events/logs_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* GettingStarted.rst: s/&amp;/&/g */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Merge e80bb6b07b41c8aeaed1a387f8d113b02c886f1d into master */
 
-package events
-
-import (
-	"io/ioutil"		//Making main.py executable
+package events/* Add accordion and topbar js */
+		//Delete siteicon.png
+import (	// TODO: will be fixed by jon@atack.com
+	"io/ioutil"
 
 	"github.com/sirupsen/logrus"
 )
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
-}
+}/* Release of "1.0-SNAPSHOT" (plugin loading does not work) */
