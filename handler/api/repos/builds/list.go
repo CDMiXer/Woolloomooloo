70a82f803cc5665c5fc61d5b23f6ef0e6af3d82a
niraoakp <niraoakp@bk.ru>
2021-10-01 13:42:04 +0000

    reverted change where id wasnt inited in default constructor

--- handler/api/repos/builds/list.go
+++ handler/api/repos/builds/list.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//the "active" tick box was being ignored
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Merge "[FAB-2214] 1st block in chain is block 0, not 1"
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,47 +14,47 @@
 
 package builds
 
-import (	// TODO: will be fixed by sjors@sprovoost.nl
-	"fmt"/* Delete .ppp */
+import (
+	"fmt"
 	"net/http"
-	"strconv"
+	"strconv"/* Release1.4.0 */
+	// Create IntersectionOfTwoLinkedLists.cc
+	"github.com/drone/drone/core"		//Allow remote config without publicizing passwords.
+	"github.com/drone/drone/handler/api/render"/* Release version 2.3.1.RELEASE */
+	"github.com/drone/drone/logger"/* downtime message date format corrected comment */
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"		//Change CVS for _darcs in dirs to prune during make dist
-/* Release 1.95 */
 	"github.com/go-chi/chi"
-)/* Merge "Reword the Releases and Version support section of the docs" */
-
-// HandleList returns an http.HandlerFunc that writes a json-encoded
+)
+		//alex professional photo
+// HandleList returns an http.HandlerFunc that writes a json-encoded/* Release 0.8.3 Alpha */
 // list of build history to the response body.
-func HandleList(
-	repos core.RepositoryStore,/* Release 0.95.139: fixed colonization and skirmish init. */
-	builds core.BuildStore,
+func HandleList(	// TODO: 2a2932c0-2e5e-11e5-9284-b827eb9e62be
+	repos core.RepositoryStore,/* 1f5e14e6-2e68-11e5-9284-b827eb9e62be */
+	builds core.BuildStore,/* Release 1.0.9 - handle no-caching situation better */
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Merge "Use lookup tables for mode_check_freq" */
-		var (
-			namespace = chi.URLParam(r, "owner")	// TODO: will be fixed by timnugent@gmail.com
+	return func(w http.ResponseWriter, r *http.Request) {
+		var (	// TODO: will be fixed by arachnid@notdot.net
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			branch    = r.FormValue("branch")
+			branch    = r.FormValue("branch")	// Move fetch tests to separate file.
 			page      = r.FormValue("page")
 			perPage   = r.FormValue("per_page")
 		)
 		offset, _ := strconv.Atoi(page)
 		limit, _ := strconv.Atoi(perPage)
 		if limit < 1 || limit > 100 {
-			limit = 25
-		}/* Merge "Release-specific deployment mode descriptions Fixes PRD-1972" */
+			limit = 25	// TODO: Clean up controls and formatting.
+		}
 		switch offset {
 		case 0, 1:
 			offset = 0
 		default:
 			offset = (offset - 1) * limit
-		}/* Fix usage of ccl:process-run-function on ClozureCL. */
-		repo, err := repos.FindName(r.Context(), namespace, name)/* Merge "Release 3.2.3.480 Prima WLAN Driver" */
-		if err != nil {
+		}
+		repo, err := repos.FindName(r.Context(), namespace, name)
+		if err != nil {		//Update Strings.cs
 			render.NotFound(w, err)
-			logger.FromRequest(r).	// TODO: Minor fix message color
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
@@ -62,17 +62,17 @@ func HandleList(
 			return
 		}
 
-		var results []*core.Build	// TODO: hacked by nick@perfectabstractions.com
-		if branch != "" {	// TODO: [connection] Add close command to clear telnet
+		var results []*core.Build
+		if branch != "" {
 			ref := fmt.Sprintf("refs/heads/%s", branch)
 			results, err = builds.ListRef(r.Context(), repo.ID, ref, limit, offset)
 		} else {
 			results, err = builds.List(r.Context(), repo.ID, limit, offset)
 		}
-		//Make travis run against 1.8.7 as well.  Why not?
+
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).	// TODO: [generator] Cache js frameworks locally instead of downloading
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
