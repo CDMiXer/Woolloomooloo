be15e91b85df2f62a666c0e4360d8ed5f7f6bd62
niraoakp <niraoakp@bk.ru>
2021-10-01 21:18:53 +0000

    Adding Angular instead of Juguler.

--- metric/metric_oss.go
+++ metric/metric_oss.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//Dospecifikovan nazev prejmenovani
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: Update README.md / add badges
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Mise en place de recaptcha */
+// See the License for the specific language governing permissions and	// TODO: hacked by qugou1350636@126.com
 // limitations under the License.
-
+	// Delete InputView.cs
 // +build oss
-
-package metric
+/* Release 1.4.0.5 */
+package metric	// outputting serialized form as EDN, not JSON
 
 import "github.com/drone/drone/core"
-
+		//Remove IChiselMode
 func BuildCount(core.BuildStore)        {}
-func PendingBuildCount(core.BuildStore) {}
+func PendingBuildCount(core.BuildStore) {}/* Relay test config */
 func RunningBuildCount(core.BuildStore) {}
 func RunningJobCount(core.StageStore)   {}
 func PendingJobCount(core.StageStore)   {}
