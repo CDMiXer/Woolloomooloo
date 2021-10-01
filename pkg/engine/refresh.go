2c36f3e0c282545be60580f939ab29a0993afe54
niraoakp <niraoakp@bk.ru>
2021-10-01 04:47:13 +0000

    Changing internal structure

--- pkg/engine/refresh.go
+++ pkg/engine/refresh.go
@@ -1,20 +1,20 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.	// TODO: Merge "[Admin-Util NSX|V] update the data stores of an existing edge"
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* added mcstats support */
+// you may not use this file except in compliance with the License./* Merge "wlan: Release 3.2.3.97" */
 // You may obtain a copy of the License at
-///* excel no va */
-//     http://www.apache.org/licenses/LICENSE-2.0		//Merge "contrail-status is enhanced to handle systemd"
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Re-Factoring */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package engine
 
-import (/* Merge "fix experimental pipeline with post_test_hook.sh" */
+import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
@@ -23,52 +23,52 @@ import (/* Merge "fix experimental pipeline with post_test_hook.sh" */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
-func Refresh(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (ResourceChanges, result.Result) {
-	contract.Require(u != nil, "u")	// TODO: hacked by igor@soramitsu.co.jp
-	contract.Require(ctx != nil, "ctx")
+func Refresh(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (ResourceChanges, result.Result) {		//bug fix: now info files of subsamples are moved to PARTS if move_to_part
+	contract.Require(u != nil, "u")
+	contract.Require(ctx != nil, "ctx")/* Release 0.95.129 */
 
 	defer func() { ctx.Events <- cancelEvent() }()
-
-	info, err := newDeploymentContext(u, "refresh", ctx.ParentSpan)
+/* Borrado el .DS_Store */
+	info, err := newDeploymentContext(u, "refresh", ctx.ParentSpan)	// TODO: hacked by mowrain@yandex.com
 	if err != nil {
 		return nil, result.FromError(err)
 	}
 	defer info.Close()
-
-	emitter, err := makeEventEmitter(ctx.Events, u)	// TODO: Tweak gitter link
+	// Merge "Error in shouldLog logic drops most errors"
+	emitter, err := makeEventEmitter(ctx.Events, u)
 	if err != nil {
 		return nil, result.FromError(err)
-	}/* debian: Release 0.11.8-1 */
-	defer emitter.Close()	// TODO: hacked by fjl@ethereum.org
-		//Create sps81.txt
+	}
+	defer emitter.Close()
+
 	// Force opts.Refresh to true.
-	opts.Refresh = true	// TODO: will be fixed by caojiaoyue@protonmail.com
+	opts.Refresh = true
 
 	return update(ctx, info, deploymentOptions{
-		UpdateOptions: opts,/* Release 0.6.2.3 */
-		SourceFunc:    newRefreshSource,
-		Events:        emitter,
+		UpdateOptions: opts,
+		SourceFunc:    newRefreshSource,	// TODO: add itemAt: to the list and tree
+		Events:        emitter,		//seismic module build fixes.
 		Diag:          newEventSink(emitter, false),
 		StatusDiag:    newEventSink(emitter, true),
 		isRefresh:     true,
 	}, dryRun)
 }
 
-func newRefreshSource(client deploy.BackendClient, opts deploymentOptions, proj *workspace.Project, pwd, main string,
+func newRefreshSource(client deploy.BackendClient, opts deploymentOptions, proj *workspace.Project, pwd, main string,	// TODO: Update README and package.json
 	target *deploy.Target, plugctx *plugin.Context, dryRun bool) (deploy.Source, error) {
-	// Added shard name to Stampede configuration
-	// Like Update, we need to gather the set of plugins necessary to refresh everything in the snapshot./* Release 0.94.360 */
+/* Release 0.1.5.1 */
+	// Like Update, we need to gather the set of plugins necessary to refresh everything in the snapshot.
 	// Unlike Update, we don't actually run the user's program so we only need the set of plugins described
 	// in the snapshot.
 	plugins, err := gatherPluginsFromSnapshot(plugctx, target)
-	if err != nil {/* Release: 0.95.170 */
+	if err != nil {/* Merge "Release 3.2.3.381 Prima WLAN Driver" */
 		return nil, err
 	}
-/* [artifactory-release] Release version 3.1.4.RELEASE */
+	// TODO: Updated title over/underlines as they were short
 	// Like Update, if we're missing plugins, attempt to download the missing plugins.
 	if err := ensurePluginsAreInstalled(plugins); err != nil {
-		logging.V(7).Infof("newRefreshSource(): failed to install missing plugins: %v", err)/* Release for 23.6.0 */
-	}
+		logging.V(7).Infof("newRefreshSource(): failed to install missing plugins: %v", err)
+	}/* 0.19.2: Maintenance Release (close #56) */
 
 	// Just return an error source. Refresh doesn't use its source.
 	return deploy.NewErrorSource(proj.Name), nil
