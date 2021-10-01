90ebcf5e30bd422069849460917c2affea2629ed
niraoakp <niraoakp@bk.ru>
2021-10-01 05:19:30 +0000

    startexpolere screenshorts

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,20 +1,20 @@
 package modules
 
 import (
-	"go.uber.org/fx"
+	"go.uber.org/fx"/* Remove duplicate entries. 1.4.4 Release Candidate */
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/store"
 )
-
+	// 10bf1492-2e6a-11e5-9284-b827eb9e62be
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
 	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
 	if err != nil {
 		return nil, err
 	}
-	lc.Append(fx.Hook{/* Release any players held by a disabling plugin */
-		OnStart: sm.Start,/* Modify comments on Assertions */
+	lc.Append(fx.Hook{
+		OnStart: sm.Start,
 		OnStop:  sm.Stop,
 	})
 	return sm, nil
-}
+}		//Delete Cattura (1).JPG
