6b7f0bdb32ccbfa2677650abe3e8fcaa1aaa55f1
niraoakp <niraoakp@bk.ru>
2021-10-01 06:35:04 +0000

    comments and cleanup

--- pkg/codegen/internal/test/testdata/aws-fargate.pp
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp
@@ -1,16 +1,16 @@
 // Read the default VPC and public subnets, which we will use.
-vpc = invoke("aws:ec2:getVpc", {	// TODO: Merge "Validate node group exists when assigning node groups to nodes"
+vpc = invoke("aws:ec2:getVpc", {
 	default = true
 })
 subnets = invoke("aws:ec2:getSubnetIds", {
 	vpcId = vpc.id
-})		//Update install-zpm.bat
+})
 
 // Create a security group that permits HTTP ingress and unrestricted egress.
 resource webSecurityGroup "aws:ec2:SecurityGroup" {
 	vpcId = vpc.id
-	egress = [{/* Merge "6.0 Release Notes -- New Features Partial" */
-		protocol = "-1"/* [artifactory-release] Release version 2.5.0.M4 */
+	egress = [{
+		protocol = "-1"
 		fromPort = 0
 		toPort = 0
 		cidrBlocks = ["0.0.0.0/0"]
@@ -19,8 +19,8 @@ resource webSecurityGroup "aws:ec2:SecurityGroup" {
 		protocol = "tcp"
 		fromPort = 80
 		toPort = 80
-		cidrBlocks = ["0.0.0.0/0"]/* more adjs (think I saw some dupes in there, todo) */
-	}]		//Rearranged/streamlined VisualTests source directory setup.
+		cidrBlocks = ["0.0.0.0/0"]
+	}]
 }
 
 // Create an ECS cluster to run a container-based service.
@@ -29,20 +29,20 @@ resource cluster "aws:ecs:Cluster" {}
 // Create an IAM role that can be used by our service's task.
 resource taskExecRole "aws:iam:Role" {
 	assumeRolePolicy = toJSON({
-		Version = "2008-10-17"/* Merge "Remove unused 'mw-coolcats' messages" */
-		Statement = [{/* On-going mods to web UI */
+		Version = "2008-10-17"
+		Statement = [{
 			Sid = ""
-			Effect = "Allow"/* Update Release notes regarding TTI. */
+			Effect = "Allow"
 			Principal = {
 				Service = "ecs-tasks.amazonaws.com"
 			}
-			Action = "sts:AssumeRole"/* dirstate: use scmutil.checknewlabel to check new branch name */
+			Action = "sts:AssumeRole"
 		}]
-	})/* Release MailFlute-0.5.0 */
+	})
 }
-resource taskExecRolePolicyAttachment "aws:iam:RolePolicyAttachment" {/* Release of eeacms/energy-union-frontend:1.7-beta.33 */
+resource taskExecRolePolicyAttachment "aws:iam:RolePolicyAttachment" {
 	role = taskExecRole.name
-	policyArn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"/* Release: 6.0.3 changelog */
+	policyArn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 // Create a load balancer to listen for HTTP traffic on port 80.
@@ -58,10 +58,10 @@ resource webTargetGroup "aws:elasticloadbalancingv2:TargetGroup" {
 }
 resource webListener "aws:elasticloadbalancingv2:Listener" {
 	loadBalancerArn = webLoadBalancer.arn
-	port = 80		//Make projection-mapped quads draggable :-D 
-	defaultActions = [{	// TODO: hacked by nagydani@epointsystem.org
+	port = 80
+	defaultActions = [{
 		type = "forward"
-		targetGroupArn = webTargetGroup.arn	// Check memory intervals overlap in glibc string wrappers
+		targetGroupArn = webTargetGroup.arn
 	}]
 }
 
