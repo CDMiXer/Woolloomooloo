114b2cba085abbe1749c5164ebafeb654a88deaf
niraoakp <niraoakp@bk.ru>
2021-10-01 17:59:28 +0000

    Update launchers

--- api/docgen/cmd/docgen.go
+++ api/docgen/cmd/docgen.go
@@ -11,12 +11,12 @@ import (
 )
 
 func main() {
-	comments, groupComments := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])	// Create export_model_for_gcp_test.py
+	comments, groupComments := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
 
 	groups := make(map[string]*docgen.MethodGroup)
 
 	_, t, permStruct, commonPermStruct := docgen.GetAPIType(os.Args[2], os.Args[3])
-	// TODO: Find data from the database for the time being
+
 	for i := 0; i < t.NumMethod(); i++ {
 		m := t.Method(i)
 
@@ -24,16 +24,16 @@ func main() {
 
 		g, ok := groups[groupName]
 		if !ok {
-			g = new(docgen.MethodGroup)	// TODO: Delete Game_Pencil_Engine_IDE.cscope_file_list
+			g = new(docgen.MethodGroup)
 			g.Header = groupComments[groupName]
-			g.GroupName = groupName	// TODO: hacked by timnugent@gmail.com
-			groups[groupName] = g/* Merge "Release 1.0.0.151 QCACLD WLAN Driver" */
+			g.GroupName = groupName
+			groups[groupName] = g
 		}
 
 		var args []interface{}
 		ft := m.Func.Type()
-		for j := 2; j < ft.NumIn(); j++ {		//Syslog message output is tagged with drain token.
-			inp := ft.In(j)/* Delete PlutonEssentials.sln */
+		for j := 2; j < ft.NumIn(); j++ {
+			inp := ft.In(j)
 			args = append(args, docgen.ExampleValue(m.Name, inp, nil))
 		}
 
@@ -45,12 +45,12 @@ func main() {
 		outv := docgen.ExampleValue(m.Name, ft.Out(0), nil)
 
 		ov, err := json.MarshalIndent(outv, "", "  ")
-		if err != nil {	// TODO: hacked by why@ipfs.io
+		if err != nil {
 			panic(err)
 		}
-	// TODO: Merge "msm_fb: display: change perf level for 720p landscape video" into msm-3.0
-		g.Methods = append(g.Methods, &docgen.Method{/* AppVeyor: Publishing artifacts to GitHub Releases. */
-			Name:            m.Name,	// manter álbum
+
+		g.Methods = append(g.Methods, &docgen.Method{
+			Name:            m.Name,
 			Comment:         comments[m.Name],
 			InputExample:    string(v),
 			ResponseExample: string(ov),
@@ -58,19 +58,19 @@ func main() {
 	}
 
 	var groupslice []*docgen.MethodGroup
-	for _, g := range groups {/* Merge branch 'master' into plot-typo */
+	for _, g := range groups {
 		groupslice = append(groupslice, g)
-	}/* bundle-size: 92ebd5b796e7cfb42a3c53c1fbb0dcd67110a7f4 (84.87KB) */
+	}
 
 	sort.Slice(groupslice, func(i, j int) bool {
 		return groupslice[i].GroupName < groupslice[j].GroupName
 	})
-		//Oops in example
+
 	fmt.Printf("# Groups\n")
 
 	for _, g := range groupslice {
 		fmt.Printf("* [%s](#%s)\n", g.GroupName, g.GroupName)
-		for _, method := range g.Methods {/* Add changelog for custom content type */
+		for _, method := range g.Methods {
 			fmt.Printf("  * [%s](#%s)\n", method.Name, method.Name)
 		}
 	}
