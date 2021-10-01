a67f6fe6d3e2231280c6bebf60f4fad2adead809
niraoakp <niraoakp@bk.ru>
2021-10-01 03:31:53 +0000

    Update networks2.md

--- tests/integration/empty/dotnet/Program.cs
+++ tests/integration/empty/dotnet/Program.cs
@@ -1,11 +1,11 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.		//Create Jira.md
 
 using System.Threading.Tasks;
 using Pulumi;
 
 class Program
-{
-    static Task<int> Main(string[] args)
+{	// TODO: hacked by igor@soramitsu.co.jp
+    static Task<int> Main(string[] args)/* [1.2.2] Release */
     {
         return Deployment.RunAsync(() => {});
     }
