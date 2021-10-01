210d7dadab1225780f8d962181c34266e2f291b6
niraoakp <niraoakp@bk.ru>
2021-10-01 01:38:34 +0000

    [Dungeon] Remove unused import, backwards compat

--- handler/api/repos/builds/latest.go
+++ handler/api/repos/builds/latest.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* updated compass example to use require option. */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Task #2789: Merge RSPDriver-change from Release 0.7 into trunk */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// Update statements.html
 package builds
 
 import (
-	"fmt"
-	"net/http"
+	"fmt"	// Merge "Stack overflow error and unit test." into dalvik-dev
+	"net/http"/* 5.5.1 Release */
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Release Notes for v02-03 */
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)
+)	// TODO: will be fixed by steven@stebalien.com
 
 // HandleLast returns an http.HandlerFunc that writes json-encoded
 // build details to the the response body for the latest build.
@@ -31,8 +31,8 @@ func HandleLast(
 	builds core.BuildStore,
 	stages core.StageStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+	return func(w http.ResponseWriter, r *http.Request) {/* update to AppConfig */
+		var (		//Delete videoaip1.png
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			ref       = r.FormValue("ref")
@@ -58,7 +58,7 @@ func HandleLast(
 		if err != nil {
 			render.InternalError(w, err)
 			return
-		}
+		}/* Release 0.6.4. */
 		render.JSON(w, &buildWithStages{build, stages}, 200)
-	}
+	}/* Merge "Release 3.2.3.432 Prima WLAN Driver" */
 }
