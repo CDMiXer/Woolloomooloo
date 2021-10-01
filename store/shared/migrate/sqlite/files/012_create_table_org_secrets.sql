03edf660326f04276f74983a317ee90e35a9f182
niraoakp <niraoakp@bk.ru>
2021-10-01 00:18:21 +0000

    todo pessoal

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
 -- name: create-table-org-secrets
-	// Updated supported Pythons badge
+
 CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
 ,secret_namespace         TEXT COLLATE NOCASE
 ,secret_name              TEXT COLLATE NOCASE
 ,secret_type              TEXT
 ,secret_data              BLOB
-,secret_pull_request      BOOLEAN
+,secret_pull_request      BOOLEAN/* Release of eeacms/ims-frontend:0.4.9 */
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
 );
