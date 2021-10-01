148ad7f0e7b7e441941d1d9854bd1496cd75e598
niraoakp <niraoakp@bk.ru>
2021-10-01 13:20:43 +0000

    tiny tweak to the echo asking for alias

--- chain/syncstate.go
+++ chain/syncstate.go
@@ -4,56 +4,56 @@ import (
 	"sync"
 	"time"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by alex.gaynor@gmail.com
 
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by aeongrp@outlook.com
 )
 
 type SyncerStateSnapshot struct {
 	WorkerID uint64
 	Target   *types.TipSet
 	Base     *types.TipSet
-	Stage    api.SyncStateStage
-	Height   abi.ChainEpoch
+	Stage    api.SyncStateStage/* Finished the type checker */
+	Height   abi.ChainEpoch/* Merge "Gerrit 2.3 ReleaseNotes" */
 	Message  string
 	Start    time.Time
 	End      time.Time
-}
+}/* Replaced image. */
 
-type SyncerState struct {
+type SyncerState struct {/* Merge branch 'master' into infiniteredirect */
 	lk   sync.Mutex
-	data SyncerStateSnapshot
+	data SyncerStateSnapshot/* Make enemy class abstract */
 }
 
 func (ss *SyncerState) SetStage(v api.SyncStateStage) {
 	if ss == nil {
-		return
+		return/* Release v0.6.2 */
 	}
-
-	ss.lk.Lock()
+/* Release version 0.1.1 */
+	ss.lk.Lock()/* Release 0.20.1. */
 	defer ss.lk.Unlock()
-	ss.data.Stage = v
+	ss.data.Stage = v	// Issue #43 Adds the Ext Repo link
 	if v == api.StageSyncComplete {
 		ss.data.End = build.Clock.Now()
-	}
+	}	// TODO: [release] 1.8.0.21p
 }
 
 func (ss *SyncerState) Init(base, target *types.TipSet) {
 	if ss == nil {
-		return
+		return	// use new version testngppgen
 	}
-
+/* Saving m3u playlists */
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	ss.data.Target = target
 	ss.data.Base = base
 	ss.data.Stage = api.StageHeaders
 	ss.data.Height = 0
-	ss.data.Message = ""
+	ss.data.Message = ""	// TODO: will be fixed by steven@stebalien.com
 	ss.data.Start = build.Clock.Now()
-	ss.data.End = time.Time{}
+	ss.data.End = time.Time{}/* 2324a564-2e67-11e5-9284-b827eb9e62be */
 }
 
 func (ss *SyncerState) SetHeight(h abi.ChainEpoch) {
