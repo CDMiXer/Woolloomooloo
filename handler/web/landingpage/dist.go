bcf233156123c51eed6b46c484384d8a11ce90a1
niraoakp <niraoakp@bk.ru>
2021-10-01 15:14:39 +0000

    Finished checkRobust() implementation

--- handler/web/landingpage/dist.go
+++ handler/web/landingpage/dist.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Rename e4u.sh to e4u.sh - 2nd Release */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//R was waiting for reply
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,5 +13,5 @@
 // limitations under the License.
 
 package landingpage
-	// TODO: will be fixed by ligi@ligi.de
+		//minor fix of 'queueHash' method
 //go:generate togo http -package landingpage -output dist_gen.go
