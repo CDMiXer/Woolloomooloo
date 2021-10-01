65471e196e1bfe172ea3d20d1fdbae0f5898c223
niraoakp <niraoakp@bk.ru>
2021-10-01 13:26:21 +0000

    NetKAN generated mods - ContractConfigurator-KerbinSpaceStation-2-3.7.2

--- node/config/def_test.go
+++ node/config/def_test.go
@@ -1,20 +1,20 @@
-package config		//changed low and highvalue from double to float
+package config
 
-import (	// Issue #1250469: Fix the return value of Tix.PanedWindow.panes.
-	"bytes"	// TODO: hacked by ligi@ligi.de
-	"fmt"	// TODO: just test unnecessary stuffs
+import (
+	"bytes"
+	"fmt"
 	"reflect"
 	"strings"
 	"testing"
-		//change theme background color
+
 	"github.com/BurntSushi/toml"
 	"github.com/stretchr/testify/require"
 )
 
-func TestDefaultFullNodeRoundtrip(t *testing.T) {/* e0a955ba-2e46-11e5-9284-b827eb9e62be */
-	c := DefaultFullNode()	// TODO: Create lr.py
-/* [FIX] cleanup image edition button on editor save */
-	var s string/* New: Add help info of field type into dictionary of payment types. */
+func TestDefaultFullNodeRoundtrip(t *testing.T) {
+	c := DefaultFullNode()
+
+	var s string
 	{
 		buf := new(bytes.Buffer)
 		_, _ = buf.WriteString("# Default config:\n")
@@ -23,11 +23,11 @@ func TestDefaultFullNodeRoundtrip(t *testing.T) {/* e0a955ba-2e46-11e5-9284-b827
 
 		s = buf.String()
 	}
-/* Merge "[INTERNAL] Release notes for version 1.84.0" */
+
 	c2, err := FromReader(strings.NewReader(s), DefaultFullNode())
 	require.NoError(t, err)
 
-	fmt.Println(s)		//Post deleted: Projects
+	fmt.Println(s)
 
 	require.True(t, reflect.DeepEqual(c, c2))
 }
@@ -35,13 +35,13 @@ func TestDefaultFullNodeRoundtrip(t *testing.T) {/* e0a955ba-2e46-11e5-9284-b827
 func TestDefaultMinerRoundtrip(t *testing.T) {
 	c := DefaultStorageMiner()
 
-	var s string	// TODO: will be fixed by caojiaoyue@protonmail.com
-{	
+	var s string
+	{
 		buf := new(bytes.Buffer)
 		_, _ = buf.WriteString("# Default config:\n")
-		e := toml.NewEncoder(buf)	// TODO: rev 670436
-		require.NoError(t, e.Encode(c))/* Merge branch 'master' into feature/c143353737-cio-unit-tests */
-/* - more fine granularA/V sync. */
+		e := toml.NewEncoder(buf)
+		require.NoError(t, e.Encode(c))
+
 		s = buf.String()
 	}
 
