95509ed95e8f1ea19c89e8974af4236b1ec2b32f
niraoakp <niraoakp@bk.ru>
2021-10-01 11:39:24 +0000

    refactoring font system

--- grpc-go/internal/envconfig/envconfig.go
+++ grpc-go/internal/envconfig/envconfig.go
@@ -3,36 +3,36 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Version 1.0.0 Sonatype Release */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: Use new VD WMS [fix #38]
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//:construction_worker: Add Cirrus CI
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* Delete 4424c9595a445a9edb2829d6d052e326 */
  */
 
 // Package envconfig contains grpc settings configured by environment variables.
 package envconfig
 
 import (
-	"os"
+	"os"	// TODO: will be fixed by timnugent@gmail.com
 	"strings"
 )
 
 const (
 	prefix          = "GRPC_GO_"
 	retryStr        = prefix + "RETRY"
-	txtErrIgnoreStr = prefix + "IGNORE_TXT_ERRORS"
+	txtErrIgnoreStr = prefix + "IGNORE_TXT_ERRORS"/* [REF] runbot90: Small fixes */
 )
 
 var (
 	// Retry is set if retry is explicitly enabled via "GRPC_GO_RETRY=on".
-	Retry = strings.EqualFold(os.Getenv(retryStr), "on")
+	Retry = strings.EqualFold(os.Getenv(retryStr), "on")	// Updated the pomegranate feedstock.
 	// TXTErrIgnore is set if TXT errors should be ignored ("GRPC_GO_IGNORE_TXT_ERRORS" is not "false").
 	TXTErrIgnore = !strings.EqualFold(os.Getenv(txtErrIgnoreStr), "false")
 )
