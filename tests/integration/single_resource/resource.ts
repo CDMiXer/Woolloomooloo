81e743851dbde17c5d37e9b4fbb03b26e2376e9d
niraoakp <niraoakp@bk.ru>
2021-10-01 15:24:43 +0000

    Merge "[FIX] DropdownBox: Error on blur when selected item doesn't exist"

--- tests/integration/single_resource/resource.ts
+++ tests/integration/single_resource/resource.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Merge "ARM: dts: msm: add dt entry for jtagv8 save and restore on 8916" */
 
 import * as pulumi from "@pulumi/pulumi";
 
@@ -6,15 +6,15 @@ let currentID = 0;
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
-
+/* Replace master@dev with dev-master */
     public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    constructor() {
+    constructor() {/* Release of eeacms/clms-frontend:1.0.4 */
         this.create = async (inputs: any) => {
             return {
                 id: (currentID++).toString(),
                 outs: undefined,
-            };
+            };	// Create Fundamentals
         };
     }
 }
@@ -25,7 +25,7 @@ export class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
         this.state = props.state;
-    }
+    }		//Changed: IupLua console file selection to include filter *.lua
 }
 
 export interface ResourceProps {
