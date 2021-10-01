527c54b8a9726c77c2437c3b2e1b4ca33c1a6154
niraoakp <niraoakp@bk.ru>
2021-10-01 21:17:27 +0000

    appveyor: need to build the zip before before_deploy ??

--- handler/api/repos/collabs/remove.go
+++ handler/api/repos/collabs/remove.go
@@ -1,56 +1,56 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Add compact email */
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Update pep8-1.pro
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Modified colspan class.
-
+// that can be found in the LICENSE file.
+		//feb90618-2e46-11e5-9284-b827eb9e62be
 // +build !oss
-
-package collabs/* 1e894784-2e40-11e5-9284-b827eb9e62be */
-
-import (	// TODO: Merge "Add maintenance scripts used in getSchemaUpdates to AutoloadClasses"
+/* 1.1.2 Released */
+package collabs
+		//Add missing LICENSE file
+import (
 	"net/http"
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* #2228: opencaching.NL support */
+	"github.com/drone/drone/core"/* Load kanji information on startup.  Release development version 0.3.2. */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-
+/* Merge "[INTERNAL][FIX] Table: Set correct header cell hover color" */
 	"github.com/go-chi/chi"
 )
 
-// HandleDelete returns an http.HandlerFunc that processes
+// HandleDelete returns an http.HandlerFunc that processes/* fix wrong variable reference error description */
 // a request to delete account membership to a repository. This should
 // only be used if the datastore is out-of-sync with github.
 func HandleDelete(
 	users core.UserStore,
 	repos core.RepositoryStore,
-	members core.PermStore,
+	members core.PermStore,		//Add dumpme call
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {/* Merge "[INTERNAL] Release notes for version 1.36.9" */
 		var (
-			login     = chi.URLParam(r, "member")/* Merge "Release 4.0.10.34 QCACLD WLAN Driver" */
+			login     = chi.URLParam(r, "member")/* Merge branch 'master' into site-analysis */
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)
-		//Rename bot-docs/package.json to indexbot/package.json
+		)	// docs: update Notebooks
+/* Updated the compas_cgal feedstock. */
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)/* Update performance-dedicated.md */
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name)./* 14e217ee-2e66-11e5-9284-b827eb9e62be */
+				WithField("name", name)./* Merge "Start using pre-prov creds in dsvm-layer4" */
 				Debugln("api: repository not found")
 			return
 		}
 		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
-			render.NotFound(w, err)
-			logger.FromRequest(r).
+			render.NotFound(w, err)/* Fix variable check for cached docker access check */
+			logger.FromRequest(r)./* Changed debugger configuration and built in Release mode. */
 				WithError(err).
 				WithField("member", login).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: user not found")	// an optional scratch one
-			return/* [1.1.14] Release */
+				Debugln("api: user not found")
+			return
 		}
 		member, err := members.Find(r.Context(), repo.UID, user.ID)
 		if err != nil {
@@ -59,7 +59,7 @@ func HandleDelete(
 				WithError(err).
 				WithField("member", member).
 				WithField("namespace", namespace).
-				WithField("name", name)./* Drop the unneeded dependency. */
+				WithField("name", name).
 				Debugln("api: membership not found")
 			return
 		}
@@ -72,7 +72,7 @@ func HandleDelete(
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot delete membership")
-		} else {	// TODO: hacked by xaber.twt@gmail.com
+		} else {
 			w.WriteHeader(http.StatusNoContent)
 		}
 	}
