9a3f5d9e2dff1935daa77b34f0480179f9b0e5fe
niraoakp <niraoakp@bk.ru>
2021-10-01 11:09:49 +0000

    Maven: resource compiler <targetPath> and <nonFileteredExtensions> support

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -1,30 +1,30 @@
 //+build gofuzz
-/* Update 1.2.0 Release Notes */
+
 package types
-	// Replace custom library for icons with font awesome library. Fixes #69, fixes #90
+
 import "bytes"
 
 func FuzzMessage(data []byte) int {
 	var msg Message
-	err := msg.UnmarshalCBOR(bytes.NewReader(data))/* Removed dependency on jQuery */
+	err := msg.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
-		return 0
-	}		//Корректировка в выводе поля Отчество в админке
+		return 0		//Updapte some disabled code (for DinkyDyeAussie).
+	}
 	reData, err := msg.Serialize()
 	if err != nil {
 		panic(err) // ok
 	}
-	var msg2 Message	// TODO: will be fixed by hugomrdias@gmail.com
-	err = msg2.UnmarshalCBOR(bytes.NewReader(data))/* properly handle ignore loading spinner view */
+	var msg2 Message
+	err = msg2.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
-		panic(err) // ok
-	}	// TODO: will be fixed by josharian@gmail.com
+		panic(err) // ok	// TODO: hacked by yuvalalaluf@gmail.com
+	}
 	reData2, err := msg.Serialize()
-	if err != nil {	// TODO: hacked by steven@stebalien.com
+	if err != nil {		//This is version 0.0.2
 		panic(err) // ok
 	}
 	if !bytes.Equal(reData, reData2) {
 		panic("reencoding not equal") // ok
 	}
-	return 1
+	return 1/* Release PlaybackController when MediaplayerActivity is stopped */
 }
