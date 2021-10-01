6b7907e7af44d0a27d8fc19d02a55b27b11bb83c
niraoakp <niraoakp@bk.ru>
2021-10-01 01:12:15 +0000

    Delete CAuxFuncs.c

--- handler/web/link/link_test.go
+++ handler/web/link/link_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Issue 7: fixed charset to utf-8
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Moved to movie_without_boxoffice.csv */
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software		//Merge opt-backporting => opt-team
+// distributed under the License is distributed on an "AS IS" BASIS,		//Automerge lp:~tplavcic/percona-server/ps-bld-228-5.5
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
