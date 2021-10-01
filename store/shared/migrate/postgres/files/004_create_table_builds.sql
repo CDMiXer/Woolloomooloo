a87c9ae3becfbbb322a55cb76911fd04773372f7
niraoakp <niraoakp@bk.ru>
2021-10-01 03:02:05 +0000

    remove generated comment

--- store/shared/migrate/postgres/files/004_create_table_builds.sql
+++ store/shared/migrate/postgres/files/004_create_table_builds.sql
@@ -1,18 +1,18 @@
 -- name: create-table-builds
 
 CREATE TABLE IF NOT EXISTS builds (
- build_id            SERIAL PRIMARY KEY/* Release notes */
+ build_id            SERIAL PRIMARY KEY
 ,build_repo_id       INTEGER
 ,build_config_id     INTEGER
-,build_trigger       VARCHAR(250)/* Merge "Release 3.2.3.310 prima WLAN Driver" */
+,build_trigger       VARCHAR(250)
 ,build_number        INTEGER
 ,build_parent        INTEGER
 ,build_status        VARCHAR(50)
-,build_error         VARCHAR(500)		//c9bcae7c-2e5c-11e5-9284-b827eb9e62be
+,build_error         VARCHAR(500)
 ,build_event         VARCHAR(50)
 ,build_action        VARCHAR(50)
-,build_link          VARCHAR(2000)		//basicevents now running in a process and not a thread for performance issues
-,build_timestamp     INTEGER	// TODO: hacked by steven@stebalien.com
+,build_link          VARCHAR(2000)
+,build_timestamp     INTEGER
 ,build_title         VARCHAR(2000)
 ,build_message       VARCHAR(2000)
 ,build_before        VARCHAR(50)
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS builds (
 ,build_ref           VARCHAR(500)
 ,build_source_repo   VARCHAR(250)
 ,build_source        VARCHAR(500)
-,build_target        VARCHAR(500)/* Add indicator record for edision osmega  */
+,build_target        VARCHAR(500)
 ,build_author        VARCHAR(500)
 ,build_author_name   VARCHAR(500)
 ,build_author_email  VARCHAR(500)
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS builds (
 ,build_deploy        VARCHAR(500)
 ,build_params        VARCHAR(4000)
 ,build_started       INTEGER
-,build_finished      INTEGER	// Prevent runaway loop in blackbox status
+,build_finished      INTEGER
 ,build_created       INTEGER
 ,build_updated       INTEGER
 ,build_version       INTEGER
@@ -37,15 +37,15 @@ CREATE TABLE IF NOT EXISTS builds (
 --,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
--- name: create-index-builds-incomplete	// 4be6c020-2e41-11e5-9284-b827eb9e62be
-/* Delete bitcoin_header2.png */
+-- name: create-index-builds-incomplete
+
 CREATE INDEX IF NOT EXISTS ix_build_incomplete ON builds (build_status)
 WHERE build_status IN ('pending', 'running');
 
 -- name: create-index-builds-repo
 
-CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);/* add participant complete. */
-/* Update beaglebone_gpio.h */
+CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);
+
 -- name: create-index-builds-author
 
 CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);
@@ -53,7 +53,7 @@ CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);
 -- name: create-index-builds-sender
 
 CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);
-/* Updated jshint dependency */
+
 -- name: create-index-builds-ref
-	// TODO: Imported Upstream version 9.14
-CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);		//Create reversed_words.py
+
+CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);
