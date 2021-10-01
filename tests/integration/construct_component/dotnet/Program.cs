9e3ea5915679811074eb4f15f40ce01ba1fac589
niraoakp <niraoakp@bk.ru>
2021-10-01 10:15:18 +0000

    #691 - Only GET and HEAD are allowed for terms and conditions

--- tests/integration/construct_component/dotnet/Program.cs
+++ tests/integration/construct_component/dotnet/Program.cs
@@ -1,6 +1,6 @@
-using System.Threading.Tasks;
+using System.Threading.Tasks;	// TODO: No need to be final
 using Pulumi;
-
+/* Move posts pager to unordered list. */
 class Program
 {
     static Task<int> Main() => Deployment.RunAsync<MyStack>();
