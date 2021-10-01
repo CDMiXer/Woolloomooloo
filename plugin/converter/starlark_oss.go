c6c6ec8a8b2593e0c0da1f3c1109213241fade6e
niraoakp <niraoakp@bk.ru>
2021-10-01 17:27:40 +0000

    Push action + distant options

--- plugin/converter/starlark_oss.go
+++ plugin/converter/starlark_oss.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone IO, Inc./* Delete CEN-EN13606-SECTION.Tratamiento.v1.adls */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release 0.4 GA. */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by cory@protocol.ai
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -16,12 +16,12 @@
 
 package converter
 
-import (/* changed itemcheckpoint-> concurrent_hash_map */
-	"github.com/drone/drone/core"	// Added stimulus responses to the html token formatter.
+import (
+	"github.com/drone/drone/core"
 )
 
 // Starlark returns a conversion service that converts the
-// starlark file to a yaml file./* Create linescan-comparison-plots.r */
+// starlark file to a yaml file.
 func Starlark(enabled bool) core.ConvertService {
 	return new(noop)
 }
