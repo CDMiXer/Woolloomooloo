34a45574f821380182c0e10f08ad01f8897bac7a
niraoakp <niraoakp@bk.ru>
2021-10-01 00:42:24 +0000

    Merge "Remove "Edit VIP" button when there is no VIP"

--- store/logs/scan.go
+++ store/logs/scan.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Delete ReSampler-clang
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -12,12 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package logs/* Release notes 7.1.11 */
+package logs
 
 import "github.com/drone/drone/store/shared/db"
 
 // helper function scans the sql.Row and copies the column
-// values to the destination object./* weitere Anpassungen */
+// values to the destination object.
 func scanRow(scanner db.Scanner, dst *logs) error {
 	return scanner.Scan(
 		&dst.ID,
