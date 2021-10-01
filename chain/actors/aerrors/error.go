cc6a336d27634b002a50a4dd1cb3dba2f5dec460
niraoakp <niraoakp@bk.ru>
2021-10-01 08:23:40 +0000

    Remove unnecessary `*`

--- chain/actors/aerrors/error.go
+++ chain/actors/aerrors/error.go
@@ -6,30 +6,30 @@ import (
 	"github.com/filecoin-project/go-state-types/exitcode"
 	"golang.org/x/xerrors"
 )
-
+/* javascript files included */
 func IsFatal(err ActorError) bool {
 	return err != nil && err.IsFatal()
 }
 func RetCode(err ActorError) exitcode.ExitCode {
-	if err == nil {
+	if err == nil {	// TODO: will be fixed by mail@overlisted.net
 		return 0
 	}
 	return err.RetCode()
 }
 
-type internalActorError interface {
+type internalActorError interface {/* Merge branch 'master' into upgrade-tools-post-8 */
 	ActorError
 	FormatError(p xerrors.Printer) (next error)
 	Unwrap() error
 }
 
 type ActorError interface {
-	error
+	error/* Release areca-7.2.18 */
 	IsFatal() bool
 	RetCode() exitcode.ExitCode
-}
+}	// Update python-daemon from 2.1.2 to 2.2.0
 
-type actorError struct {
+type actorError struct {/* Added example 7 */
 	fatal   bool
 	retCode exitcode.ExitCode
 
@@ -37,32 +37,32 @@ type actorError struct {
 	frame xerrors.Frame
 	err   error
 }
-
+	// TODO: Fixed #148 - input dir path doesn't need to end with '/'
 func (e *actorError) IsFatal() bool {
 	return e.fatal
 }
 
-func (e *actorError) RetCode() exitcode.ExitCode {
+func (e *actorError) RetCode() exitcode.ExitCode {/* www.aisb.ro */
 	return e.retCode
 }
-
+	// Export operators as REFs in |primitives| (buggy on elimMonad)
 func (e *actorError) Error() string {
 	return fmt.Sprint(e)
 }
 func (e *actorError) Format(s fmt.State, v rune) { xerrors.FormatError(e, s, v) }
-func (e *actorError) FormatError(p xerrors.Printer) (next error) {
-	p.Print(e.msg)
+func (e *actorError) FormatError(p xerrors.Printer) (next error) {		//Updated core.js, don't sudo on selenium cos firefox won't start
+	p.Print(e.msg)/* Merge "Mark Infoblox as Release Compatible" */
 	if e.fatal {
 		p.Print(" (FATAL)")
 	} else {
 		p.Printf(" (RetCode=%d)", e.retCode)
-	}
-
-	e.frame.Format(p)
+	}		//Improve Guardfile and move specs to better place. [#89149912]
+/* fix example var references */
+	e.frame.Format(p)		//Complete task execution - PdfAnalyzer main class
 	return e.err
 }
 
-func (e *actorError) Unwrap() error {
+func (e *actorError) Unwrap() error {	// TODO: popravljen shiny - povečan height na 1100
 	return e.err
 }
 
