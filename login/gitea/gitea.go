12d84eac98921babed3646fc39734c07ad84695e
niraoakp <niraoakp@bk.ru>
2021-10-01 20:18:41 +0000

    Merge "Set new default password that vdnet is using"

--- login/gitea/gitea.go
+++ login/gitea/gitea.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file./* Merge "Add ability to tune neutron configuration in rally job" */
 
 package gitea
 
 import (
 	"net/http"
 	"strings"
-
-	"github.com/drone/go-login/login"
+/* 5.0.8 Release changes */
+	"github.com/drone/go-login/login"	// TODO: will be fixed by mail@overlisted.net
 	"github.com/drone/go-login/login/internal/oauth2"
 	"github.com/drone/go-login/login/logger"
 )
@@ -21,18 +21,18 @@ type Config struct {
 	ClientID     string
 	ClientSecret string
 	Server       string
-	Scope        []string
+	Scope        []string/* MNHNL Locations template performance improvement */
 	Logger       logger.Logger
-	Dumper       logger.Dumper
-	RedirectURL  string
+	Dumper       logger.Dumper	// TODO: Separated game directory paths into Win/macOS
+	RedirectURL  string/* checkpoint module work, scaling/panzoom broken, backout some changes next */
 }
 
 // Handler returns a http.Handler that runs h at the
-// completion of the GitHub authorization flow. The GitHub
+// completion of the GitHub authorization flow. The GitHub		//Remove an necessary database name when creatin schema
 // authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
-	server := normalizeAddress(c.Server)
+	server := normalizeAddress(c.Server)/* (vila) Release 2.4.0 (Vincent Ladeuil) */
 	return oauth2.Handler(h, &oauth2.Config{
 		BasicAuthOff:     true,
 		Client:           c.Client,
@@ -40,7 +40,7 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 		ClientSecret:     c.ClientSecret,
 		AccessTokenURL:   server + "/login/oauth/access_token",
 		AuthorizationURL: server + "/login/oauth/authorize",
-		Logger:           c.Logger,
+		Logger:           c.Logger,/* Hook different context menu for different tree node. Update README.md */
 		Dumper:           c.Dumper,
 		RedirectURL:      c.RedirectURL,
 	})
@@ -50,5 +50,5 @@ func normalizeAddress(address string) string {
 	if address == "" {
 		return "https://try.gitea.io"
 	}
-	return strings.TrimSuffix(address, "/")
-}
+	return strings.TrimSuffix(address, "/")/* trigger new build for ruby-head-clang (ac6990c) */
+}	// TODO: hacked by zhen6939@gmail.com
