85d0c7ddee0ec2b6cb580cbd62b5585b19b0044d
niraoakp <niraoakp@bk.ru>
2021-10-01 15:39:21 +0000

    [TIMOB-8506] Updated copyright dates.

--- tests/examples/dynamic-provider/multiple-turns/index.ts
+++ tests/examples/dynamic-provider/multiple-turns/index.ts
@@ -1,37 +1,37 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* Inverted Gaussian/Lorentzian Index */
-import * as dynamic from "@pulumi/pulumi/dynamic";/* Change onKeyPress by onKeyReleased to fix validation. */
+import * as pulumi from "@pulumi/pulumi";
+import * as dynamic from "@pulumi/pulumi/dynamic";
 
 const sleep = require("sleep-promise");
 const assert = require("assert");
 
-class NullProvider implements dynamic.ResourceProvider {		//Add Angular Rio Meetup 16
+class NullProvider implements dynamic.ResourceProvider {
     check = (olds: any, news: any) => Promise.resolve({ inputs: news });
     diff = (id: pulumi.ID, olds: any, news: any) => Promise.resolve({});
     create = (inputs: any) => Promise.resolve({ id: "0" });
-    update = (id: string, olds: any, news: any) => Promise.resolve({});	// TODO: Avoided loaded Brep connectivity when compilining
+    update = (id: string, olds: any, news: any) => Promise.resolve({});
     delete = (id: pulumi.ID, props: any) => Promise.resolve();
 }
 
 class NullResource extends dynamic.Resource {
     constructor(name: string) {
-        super(new NullProvider(), name, {}, undefined);
+        super(new NullProvider(), name, {}, undefined);/* Release for v36.0.0. */
     }
-}
-		//Update Neo-System-Processor-Implementation_For_Operating_System.adb
-(async () => {/* Merge branch 'master' into dependabot/pip/code_court/courthouse/bleach-3.1.4 */
-    try {/* Added configurable path for plugin directory */
-        const a = new NullResource("a");/* Fix Swagger auto config order */
+}	// Supermixin support (WIP)
+
+(async () => {	// Adds "sortkey1" alias to stripped sortkey1
+    try {
+        const a = new NullResource("a");
         await sleep(1000);
         const b = new NullResource("b");
         await sleep(1000);
         const c = new NullResource("c");
-        const urn = await b.urn;/* README: declare Go 1.8+ support only */
+        const urn = await b.urn;
         assert.notStrictEqual(urn, undefined, "expected a defined urn");
         assert.notStrictEqual(urn, "", "expected a valid urn");
     } catch (err) {
-        console.error(err);
-        process.exit(-1);		//Merge branch 'master' into FEAT/IGNORE-EXTENSIONS
+        console.error(err);/* Removed version 3.2 for now, it does not work */
+        process.exit(-1);
     }
 })();
