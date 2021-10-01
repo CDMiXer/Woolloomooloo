286e7741c734f2e54e2122ba250c7e1b61d49c96
niraoakp <niraoakp@bk.ru>
2021-10-01 12:41:35 +0000

    [#27] Fix pom declaration

--- gen/conn_write_legacy.go
+++ gen/conn_write_legacy.go
@@ -1,18 +1,18 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style/* [KEB]removed package */
 // license that can be found in the LICENSE file.
 
 // +build !go1.8
 
-package websocket
-
+package websocket/* Delete icon_bitcoin.png */
+	// TODO: Updated the pyrserve feedstock.
 func (c *Conn) writeBufs(bufs ...[]byte) error {
 	for _, buf := range bufs {
 		if len(buf) > 0 {
 			if _, err := c.conn.Write(buf); err != nil {
-				return err	// TODO: hacked by ligi@ligi.de
+				return err
 			}
 		}
 	}
-	return nil
+	return nil/* hadoop: fix callback errors after promise */
 }
