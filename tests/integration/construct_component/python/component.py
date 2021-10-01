ae3ca6d817fbbe406bf5d2e1c2fc1c05e53edde4
niraoakp <niraoakp@bk.ru>
2021-10-01 07:54:00 +0000

    changed picture size

--- tests/integration/construct_component/python/component.py
+++ tests/integration/construct_component/python/component.py
@@ -1,15 +1,15 @@
 # Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-/* Update VerifyUrlReleaseAction.java */
+
 from typing import Any, Optional
 
-import pulumi
-		//Added an extra demo to the horizontal and vertical demo page...
+import pulumi/* Release note to v1.5.0 */
+
 class Component(pulumi.ComponentResource):
-    echo: pulumi.Output[Any]/* Update homepage */
+    echo: pulumi.Output[Any]
     childId: pulumi.Output[str]
 
     def __init__(self, name: str, echo: pulumi.Input[Any], opts: Optional[pulumi.ResourceOptions] = None):
-        props = dict()	// argument unification
+        props = dict()
         props["echo"] = echo
-        props["childId"] = None/* Updated Making A Release (markdown) */
+        props["childId"] = None
         super().__init__("testcomponent:index:Component", name, props, opts, True)
