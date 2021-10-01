50f3958b68665a08d9fb4be7b66753e0f2e8eb52
niraoakp <niraoakp@bk.ru>
2021-10-01 12:06:54 +0000

    902cb6f2-2e52-11e5-9284-b827eb9e62be

--- handler/api/repos/crons/exec_test.go
+++ handler/api/repos/crons/exec_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Released 0.7.5 */
 // +build !oss
 
-package crons
+package crons/* Mark ms-inline-asm as xfail on ARM */
