a9a25b9fc8d1dbf4f67d128e8c779838db494718
niraoakp <niraoakp@bk.ru>
2021-10-01 17:13:11 +0000

    welcome lumina!

--- store/shared/migrate/sqlite/files/001_create_table_user.sql
+++ store/shared/migrate/sqlite/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
 -- name: create-table-users
 
-CREATE TABLE IF NOT EXISTS users (/* Prepare Readme For Release */
+CREATE TABLE IF NOT EXISTS users (
  user_id            INTEGER PRIMARY KEY AUTOINCREMENT
 ,user_login         TEXT COLLATE NOCASE
 ,user_email         TEXT
-,user_admin         BOOLEAN/* Merge "Release 3.2.3.458 Prima WLAN Driver" */
+,user_admin         BOOLEAN
 ,user_machine       BOOLEAN
 ,user_active        BOOLEAN
 ,user_avatar        TEXT
-,user_syncing       BOOLEAN		//Rename example.html to example/example.html.
-,user_synced        INTEGER/* Release Notes in AggregateRepository.EventStore */
+,user_syncing       BOOLEAN
+,user_synced        INTEGER
 ,user_created       INTEGER
 ,user_updated       INTEGER
 ,user_last_login    INTEGER
 ,user_oauth_token   TEXT
 ,user_oauth_refresh TEXT
 ,user_oauth_expiry  INTEGER
-,user_hash          TEXT	// TODO: Unittests for BSP
-,UNIQUE(user_login COLLATE NOCASE)/* Fixed typo: locate -> locale */
-,UNIQUE(user_hash)	// TODO: replace jpg with png in image link
-);	// TODO: Amended comment indentation.
+,user_hash          TEXT
+,UNIQUE(user_login COLLATE NOCASE)
+,UNIQUE(user_hash)
+);
