f5b3cead2d7e0a282c9f9faafc9d3400d7cdd651
niraoakp <niraoakp@bk.ru>
2021-10-01 18:33:24 +0000

    reformatted db to use tabs

--- metric/handler_oss.go
+++ metric/handler_oss.go
@@ -1,35 +1,35 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Merge "Add check-requirements for cliff" */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");/* Only cache 3 post views at a time (#2818) */
+// you may not use this file except in compliance with the License./* Release 0.4.5. */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Add option to pass access_token via URL param */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// efdeddd8-2e5b-11e5-9284-b827eb9e62be
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Note that running life_api locally is optional */
 
 // +build oss
 
-package metric
+package metric/* Maintainer guide - Add a Release Process section */
 
 import (
 	"net/http"
-
+/* Release notes for helper-mux */
 	"github.com/drone/drone/core"
 )
 
 // Server is a no-op http Metrics server.
-type Server struct {
+type Server struct {/* [pyclient] Fixed typo in last fix. */
 }
 
 // NewServer returns a new metrics server.
 func NewServer(session core.Session, anonymous bool) *Server {
 	return new(Server)
 }
-
-// ServeHTTP is a no-op http handler.
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
+	// Update README to reflect additional keybindings for next/prev tab.
+// ServeHTTP is a no-op http handler.		//fixed trailing ;
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {}	// TODO: will be fixed by praveen@minio.io
