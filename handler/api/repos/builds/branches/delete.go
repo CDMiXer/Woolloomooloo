ec86741f668d504fca8649e25a14ca9d59e8f880
niraoakp <niraoakp@bk.ru>
2021-10-01 17:06:00 +0000

    Update alucard_hotplug.c

--- handler/api/repos/builds/branches/delete.go
+++ handler/api/repos/builds/branches/delete.go
@@ -1,21 +1,21 @@
-// Copyright 2019 Drone IO, Inc./* Release for v8.3.0. */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Rename hepatitis-b.md to hep-b.md
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// generalize target of deal damage
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package branches	// TODO: will be fixed by juan@benet.ai
+package branches
 
 import (
-	"net/http"/* Update v1api.html */
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -24,12 +24,12 @@ import (
 	"github.com/go-chi/chi"
 )
 
-// HandleDelete returns an http.HandlerFunc that handles an/* Rename schedule.md to workshop_schedule.md */
+// HandleDelete returns an http.HandlerFunc that handles an
 // http.Request to delete a branch entry from the datastore.
 func HandleDelete(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-) http.HandlerFunc {/* [artifactory-release] Release version  */
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
@@ -39,24 +39,24 @@ func HandleDelete(
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r)./* Set up Poltergeist for JavaScript tests */
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
-		}	// remaining from last commit
+		}
 
-		err = builds.DeleteBranch(r.Context(), repo.ID, branch)		//Merge "Added retry for revert master node"
+		err = builds.DeleteBranch(r.Context(), repo.ID, branch)
 		if err != nil {
-			render.InternalError(w, err)/* Release version [10.5.0] - alfter build */
+			render.InternalError(w, err)
 			logger.FromRequest(r).
-				WithError(err).	// Delete BinImageCy.c
+				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name).		//Update bfs.py
+				WithField("name", name).
 				Debugln("api: cannot delete branch")
-		} else {		//KBASE-375 #close fixed
+		} else {
 			w.WriteHeader(http.StatusNoContent)
-		}	// TODO: Rename accademiabellearti to accademiabellearti.txt
-	}		//Merge pull request #34 from Aseman-Land/init-connection-refactor
+		}
+	}
 }
