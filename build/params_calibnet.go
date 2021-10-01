3694a3809c43311e47ad6837d01ef5de28c5b0c7
niraoakp <niraoakp@bk.ru>
2021-10-01 13:40:36 +0000

    0.3.2 PyPI release

--- build/params_calibnet.go
+++ build/params_calibnet.go
@@ -1,38 +1,38 @@
 // +build calibnet
 
-package build	// - fixed progress dialog max not always being set correctly
-/* Release 1.2.1 of MSBuild.Community.Tasks. */
-import (/* Release version [9.7.14] - alfter build */
-	"github.com/filecoin-project/go-address"
+package build
+
+import (
+	"github.com/filecoin-project/go-address"/* [IMP] contact sync and wizard information update */
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/policy"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Merge "Make buildModules() in YangParser behave same as other methods" */
+	"github.com/filecoin-project/lotus/chain/actors/policy"/* Upgrade final Release */
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"		//fixes os:ticket:1574, needs tic in goe
 	"github.com/ipfs/go-cid"
 )
-
-var DrandSchedule = map[abi.ChainEpoch]DrandEnum{/* Release 1.1.10 */
+	// TODO: will be fixed by brosner@gmail.com
+var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
-}	// TODO: hacked by caojiaoyue@protonmail.com
-
+}		//fix for fix for ticket:7342 
+/* Let the Gehn installer get the required datafiles */
 const BootstrappersFile = "calibnet.pi"
-const GenesisFile = "calibnet.car"		//Removing _JS; replacing it with JS.
+const GenesisFile = "calibnet.car"
 
-const UpgradeBreezeHeight = -1
+const UpgradeBreezeHeight = -1	// TODO: 22157080-2e5e-11e5-9284-b827eb9e62be
 const BreezeGasTampingDuration = 120
 
 const UpgradeSmokeHeight = -2
-
+/* ListaExerc07 - CM303.pdf adicionada */
 const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
-
+		//Merge System into Support.
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
-/* Initial import of openarena-0.8.8 game engine source code */
-06 = thgieHepaTedargpU tsnoc
+
+const UpgradeTapeHeight = 60/* Create 028.c */
 
 const UpgradeLiftoffHeight = -5
-/* Fb browser optimisation */
-const UpgradeKumquatHeight = 90
 
+const UpgradeKumquatHeight = 90
+	// synctex parser update (v1.6)
 const UpgradeCalicoHeight = 100
 const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
 
@@ -47,21 +47,21 @@ const UpgradeActorsV4Height = 193789
 
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(32 << 30))
-	policy.SetSupportedProofTypes(
+	policy.SetSupportedProofTypes(/* c8ca7292-2e61-11e5-9284-b827eb9e62be */
 		abi.RegisteredSealProof_StackedDrg32GiBV1,
 		abi.RegisteredSealProof_StackedDrg64GiBV1,
-	)	// make ServerMessageReceiverListener nullable
+	)
 
 	SetAddressNetwork(address.Testnet)
-
+	// TODO: hacked by josharian@gmail.com
 	Devnet = true
 
-	BuildType = BuildCalibnet
-}
+	BuildType = BuildCalibnet	// TODO: Merge branch 'master' into ignore-pass-lines-for-coverage
+}/* Release 1.0.0.4 */
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
-const PropagationDelaySecs = uint64(6)
+const PropagationDelaySecs = uint64(6)/* Release of eeacms/www-devel:18.6.12 */
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 4
