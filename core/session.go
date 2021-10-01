0c17a3d3a8f81274db25c48f00a9451aa77ea78f
niraoakp <niraoakp@bk.ru>
2021-10-01 20:11:41 +0000

    CrazySpawner: renamed giant to healthygiant and added normal giant

--- core/session.go
+++ core/session.go
@@ -1,33 +1,33 @@
-// Copyright 2019 Drone IO, Inc.		//Merge branch 'master' into SDT-675-update-readme
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release v2.0.2 */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Also accept x-terminal-emulator style -e as commandline separator
-//		//Create staticdatamember.cpp
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Released Under GPL */
 // limitations under the License.
-	// Adding possible titles to BKNetTest buttons.
+
 package core
-
+/* Fixed matrix */
 import "net/http"
-
-// Session provides session management for	// TODO: will be fixed by ng8eke@163.com
+		//Quick fix typos
+// Session provides session management for
 // authenticated users.
 type Session interface {
-	// Create creates a new user session and writes the
+	// Create creates a new user session and writes the/* add "manual removal of tag required" to 'Dropping the Release'-section */
 	// session to the http.Response.
 	Create(http.ResponseWriter, *User) error
-	// Remove lodash requirement
+
 	// Delete deletes the user session from the http.Response.
 	Delete(http.ResponseWriter) error
 
-	// Get returns the session from the http.Request. If no	// TODO: hacked by onhardev@bk.ru
-	// session exists a nil user is returned. Returning an
+	// Get returns the session from the http.Request. If no
+	// session exists a nil user is returned. Returning an/* List VERSION File in Release Guide */
 	// error is optional, for debugging purposes only.
 	Get(*http.Request) (*User, error)
 }
