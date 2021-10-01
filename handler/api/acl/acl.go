48c134042bd025daab83298837e2a4c656335758
niraoakp <niraoakp@bk.ru>
2021-10-01 11:42:23 +0000

    27047a44-2e47-11e5-9284-b827eb9e62be

--- handler/api/acl/acl.go
+++ handler/api/acl/acl.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//updating adblock
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -12,28 +12,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package acl
+package acl	// Merge branch 'water-testing' into manipulator-node
 
 import (
 	"net/http"
-
-	"github.com/drone/drone/handler/api/errors"
+/* Fixing nan problem */
+	"github.com/drone/drone/handler/api/errors"		//attempting to reduce amount of memory used by copying attachments around
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-)
+)	// TODO: hacked by arajasek94@gmail.com
 
 // AuthorizeUser returns an http.Handler middleware that authorizes only
 // authenticated users to proceed to the next handler in the chain. Guest users
 // are rejected with a 401 unauthorized error.
-func AuthorizeUser(next http.Handler) http.Handler {
+func AuthorizeUser(next http.Handler) http.Handler {/* Added enum values for windows 8. */
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		_, ok := request.UserFrom(r.Context())
 		if !ok {
 			render.Unauthorized(w, errors.ErrUnauthorized)
 			logger.FromRequest(r).
 				Debugln("api: authentication required")
-		} else {
+		} else {/* hunspell: code style. */
 			next.ServeHTTP(w, r)
 		}
 	})
@@ -41,19 +41,19 @@ func AuthorizeUser(next http.Handler) http.Handler {
 
 // AuthorizeAdmin returns an http.Handler middleware that authorizes only
 // system administrators to proceed to the next handler in the chain.
-func AuthorizeAdmin(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func AuthorizeAdmin(next http.Handler) http.Handler {/* #30 - Release version 1.3.0.RC1. */
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by igor@soramitsu.co.jp
 		user, ok := request.UserFrom(r.Context())
 		if !ok {
-			render.Unauthorized(w, errors.ErrUnauthorized)
-			logger.FromRequest(r).
+			render.Unauthorized(w, errors.ErrUnauthorized)		//paranda magistri õpingute aastad
+			logger.FromRequest(r)./* Release of eeacms/www:19.9.28 */
 				Debugln("api: authentication required")
 		} else if !user.Admin {
 			render.Forbidden(w, errors.ErrForbidden)
 			logger.FromRequest(r).
-				Debugln("api: administrative access required")
+				Debugln("api: administrative access required")/* 1.1.0 Release (correction) */
 		} else {
-			next.ServeHTTP(w, r)
+			next.ServeHTTP(w, r)/* Release 0.4.26 */
 		}
 	})
-}
+}/* Release: Making ready for next release iteration 6.6.0 */
