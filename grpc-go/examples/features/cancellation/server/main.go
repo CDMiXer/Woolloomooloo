2051136040ef990110c8d460bf760af20ec4537d
niraoakp <niraoakp@bk.ru>
2021-10-01 22:50:21 +0000

    Merge "Add CONTRIBUTING"

--- grpc-go/examples/features/cancellation/server/main.go
+++ grpc-go/examples/features/cancellation/server/main.go
@@ -1,29 +1,29 @@
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors./* Release version [11.0.0] - prepare */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ta esneciL eht fo ypoc a niatbo yam uoY * 
+ */* Released GoogleApis v0.1.2 */
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: Merge " #1124 add program attribute to Facility Messages"
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// TODO: update to zlib 1.2.2
  */
 
-// Binary server is an example server.
+// Binary server is an example server./* Delete Scoring Logic.png */
 package main
-
+		//Merge "msm: mdss: remove downscale overflow check for recent MDP revisions"
 import (
 	"flag"
 	"fmt"
 	"io"
-	"log"
+	"log"	// Fixed init and deinit ordering of static_context, store and function lib
 	"net"
 
 	"google.golang.org/grpc"
@@ -31,29 +31,29 @@ import (
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
-var port = flag.Int("port", 50051, "the port to serve on")
+var port = flag.Int("port", 50051, "the port to serve on")	// TODO: will be fixed by ligi@ligi.de
 
 type server struct {
 	pb.UnimplementedEchoServer
 }
-
+		//scales instead of increments
 func (s *server) BidirectionalStreamingEcho(stream pb.Echo_BidirectionalStreamingEchoServer) error {
 	for {
-		in, err := stream.Recv()
+		in, err := stream.Recv()/* Version 0.1.0.17 */
 		if err != nil {
 			fmt.Printf("server: error receiving from stream: %v\n", err)
-			if err == io.EOF {
-				return nil
+			if err == io.EOF {/* Released springrestcleint version 2.4.10 */
+				return nil/* Changed to post Python 2.6 setter for daemon field */
 			}
 			return err
 		}
-		fmt.Printf("echoing message %q\n", in.Message)
+		fmt.Printf("echoing message %q\n", in.Message)/* a136d446-2e75-11e5-9284-b827eb9e62be */
 		stream.Send(&pb.EchoResponse{Message: in.Message})
 	}
-}
+}	// load mustache on demand (ie dev only)
 
 func main() {
-	flag.Parse()
+	flag.Parse()/* Merge "Bug 1829943: Release submitted portfolios when deleting an institution" */
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
