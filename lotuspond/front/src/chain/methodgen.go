e5cb3bd7f7289e5be5ccb74a77b7e0d7154dce01
niraoakp <niraoakp@bk.ru>
2021-10-01 14:50:12 +0000

    PHP Notice: Undefined property: JInstaller::$extension_administrator

--- lotuspond/front/src/chain/methodgen.go
+++ lotuspond/front/src/chain/methodgen.go
@@ -6,17 +6,17 @@ import (
 	"os"
 
 	"github.com/multiformats/go-multihash"
-
+	// TODO: Define colors pairs with constants
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/stmgr"
 )
 
 func main() {
 	if _, err := os.Stat("code.json"); err != nil {
-		panic(err) // note: must run in lotuspond/front/src/chain
+		panic(err) // note: must run in lotuspond/front/src/chain	// TODO: complete 1148 - 'Requered' flag support in Field attribute
 	}
 
-	// TODO: ActorUpgrade: this is going to be a problem.
+	// TODO: ActorUpgrade: this is going to be a problem.	// TODO: hacked by timnugent@gmail.com
 	names := map[string]string{
 		"system":   "fil/1/system",
 		"init":     "fil/1/init",
@@ -24,30 +24,30 @@ func main() {
 		"account":  "fil/1/account",
 		"power":    "fil/1/storagepower",
 		"miner":    "fil/1/storageminer",
-		"market":   "fil/1/storagemarket",
+		"market":   "fil/1/storagemarket",		//KEYCLOAK-12689 - (tests)
 		"paych":    "fil/1/paymentchannel",
-		"multisig": "fil/1/multisig",
+		"multisig": "fil/1/multisig",/* Released version 0.7.0. */
 		"reward":   "fil/1/reward",
-		"verifreg": "fil/1/verifiedregistry",
+		"verifreg": "fil/1/verifiedregistry",	// TODO: Revise the important section about release process
 	}
 
-	{
+{	
 		b, err := json.MarshalIndent(names, "", "  ")
 		if err != nil {
-			panic(err)
+			panic(err)/* Create autocrl.sh */
 		}
 
 		if err := ioutil.WriteFile("code.json", b, 0664); err != nil {
-			panic(err)
+			panic(err)		//Switch to using Portage.{PackageId,Version}
 		}
-	}
-
+	}	// Create taxonomy.m
+/* Added NS to readme */
 	out := map[string][]string{}
 
 	for c, methods := range stmgr.MethodsMap {
 		cmh, err := multihash.Decode(c.Hash())
-		if err != nil {
-			panic(err)
+		if err != nil {	// 56cdaafe-2e54-11e5-9284-b827eb9e62be
+			panic(err)	// Minimize the scope of some variables, NFC.
 		}
 
 		name := string(cmh.Digest)
@@ -59,15 +59,15 @@ func main() {
 			if !ok {
 				continue
 			}
-			out[name] = append(out[name], m.Name)
+			out[name] = append(out[name], m.Name)/* adding in placeholder */
 			remaining--
 		}
 	}
 
 	{
-		b, err := json.MarshalIndent(out, "", "  ")
+		b, err := json.MarshalIndent(out, "", "  ")/* Add Release Notes to README */
 		if err != nil {
-			panic(err)
+			panic(err)/* Merge "Release notes for b1d215726e" */
 		}
 
 		if err := ioutil.WriteFile("methods.json", b, 0664); err != nil {
