01e6e4531b44818bfbc944f6e07f53eade738ff4
niraoakp <niraoakp@bk.ru>
2021-10-01 12:05:36 +0000

    sysmon: fix typo.

--- store/logs/s3_oss.go
+++ store/logs/s3_oss.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
-//		//graphlog: wrapped docstrings at 78 characters
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: primitive -> raw
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at/* added isPlaneswalker to MagicObject */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+///* A fix in Release_notes.txt */
+// Unless required by applicable law or agreed to in writing, software	// Add missing release timestamps
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-// +build oss
+/* added Mutilate */
+// +build oss/* some skill check condition */
 
 package logs
 
