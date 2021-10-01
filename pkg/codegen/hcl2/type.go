2fc76ddd0894066fc4cd6833138c8c4225a1c064
niraoakp <niraoakp@bk.ru>
2021-10-01 22:54:31 +0000

    Created experimental PULL request template.

--- pkg/codegen/hcl2/type.go
+++ pkg/codegen/hcl2/type.go
@@ -5,17 +5,17 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* @Release [io7m-jcanephora-0.19.1] */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* no more ignored tables */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package hcl2	// TODO: hacked by hugomrdias@gmail.com
+package hcl2
 
 import (
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"/* GUACAMOLE-422: Remove extra space in pom.xml */
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 )
 
 var (
