b8aec652915f83a2430d353cca22ab850a3a2348
niraoakp <niraoakp@bk.ru>
2021-10-01 20:45:47 +0000

    F: include osgi.promise in build

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -1,24 +1,24 @@
 package account
 
-import (
-	"github.com/filecoin-project/go-address"
+import (	// TODO: will be fixed by timnugent@gmail.com
+	"github.com/filecoin-project/go-address"	// package isolation shared libs
 	"github.com/ipfs/go-cid"
-
+/* More changes to draft */
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"
 )
 
-var _ State = (*state0)(nil)
+var _ State = (*state0)(nil)/* Release of eeacms/www:18.7.13 */
 
-func load0(store adt.Store, root cid.Cid) (State, error) {
+func load0(store adt.Store, root cid.Cid) (State, error) {/* [1.1.7] Milestone: Release */
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+	if err != nil {	// TODO: will be fixed by lexy8russo@outlook.com
 		return nil, err
 	}
 	return &out, nil
-}
+}/* Comment cleanup, some cleanup in Lean Mean C++ Option Parser. */
 
 type state0 struct {
 	account0.State
