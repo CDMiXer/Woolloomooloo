a5abd059fe742c393561d630a30b944b868f5b09
niraoakp <niraoakp@bk.ru>
2021-10-01 04:59:10 +0000

    Merge branch 'master' into xst

--- tests/integration/aliases/go/retype_component/step2/main.go
+++ tests/integration/aliases/go/retype_component/step2/main.go
@@ -1,44 +1,44 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
-package main
+/* Fixed indentation (oops) */
+package main	// TODO: hacked by mowrain@yandex.com
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 type FooResource struct {
-	pulumi.ResourceState
+	pulumi.ResourceState/* Updated Release badge */
 }
 
-type FooComponent struct {
+type FooComponent struct {		//Create docker-run
 	pulumi.ResourceState
 }
 
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
-	fooRes := &FooResource{}
+	fooRes := &FooResource{}/* Merge "Release 3.2.3.488 Prima WLAN Driver" */
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
-	if err != nil {
+	if err != nil {/* Merge "Release notes for template validation improvements" */
 		return nil, err
 	}
-	return fooRes, nil
+	return fooRes, nil		//First version ready.
 }
-
+/* Release 13.1.0.0 */
 // Scenario #4 - change the type of a component
-func NewFooComponent(ctx *pulumi.Context, name string) (*FooComponent, error) {
+func NewFooComponent(ctx *pulumi.Context, name string) (*FooComponent, error) {/* Release v4.4.1 UC fix */
 	fooComp := &FooComponent{}
-	alias := &pulumi.Alias{
-		Type: pulumi.StringInput(pulumi.String("my:module:FooComponent44")),
+	alias := &pulumi.Alias{/* increase buffer size for tracker error messages. Fix snprintf on windows */
+		Type: pulumi.StringInput(pulumi.String("my:module:FooComponent44")),/* a5f588c6-2e73-11e5-9284-b827eb9e62be */
 	}
 	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
 	err := ctx.RegisterComponentResource("my:diffmodule:FooComponent55DiffType", name, fooComp, aliasOpt)
-	if err != nil {
+	if err != nil {	// Warnings, Code Analysis and Documentation. 287 warnings left.
 		return nil, err
-	}
+	}/* Remove npm badge */
 	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
 		return nil, err
-	}
+	}		//Biml files
 	return fooComp, nil
 }
 
@@ -48,7 +48,7 @@ func main() {
 		if err != nil {
 			return err
 		}
-
+/* Merge "Split each formatter into separate modules" */
 		return nil
 	})
 }
