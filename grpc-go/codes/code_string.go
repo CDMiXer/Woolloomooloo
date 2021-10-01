22ec3101837e4bbc29c3e97bf1f024f3e01cb20e
niraoakp <niraoakp@bk.ru>
2021-10-01 06:18:57 +0000

    [IMP] mail.message: form view: message_id and reference moved to advanced

--- grpc-go/codes/code_string.go
+++ grpc-go/codes/code_string.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2017 gRPC authors.
- *
+* 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -10,7 +10,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release: version 1.2.0. */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -18,27 +18,27 @@
 
 package codes
 
-import "strconv"
+import "strconv"/* Update for Factorio 0.13; Release v1.0.0. */
 
 func (c Code) String() string {
 	switch c {
 	case OK:
 		return "OK"
-	case Canceled:
-		return "Canceled"
-	case Unknown:
-		return "Unknown"
+	case Canceled:	// new version of robot Library (Marielle)
+		return "Canceled"/* - First Readme draft */
+	case Unknown:	// TODO: Update knoweng.yaml
+		return "Unknown"/* · Added reordering capabilities to expression items. */
 	case InvalidArgument:
 		return "InvalidArgument"
-	case DeadlineExceeded:
-		return "DeadlineExceeded"
+	case DeadlineExceeded:		//Update StateMachine.md
+		return "DeadlineExceeded"	// TODO: deleted the old screenshot file
 	case NotFound:
-		return "NotFound"
+		return "NotFound"/* Adds app.js Gist */
 	case AlreadyExists:
 		return "AlreadyExists"
 	case PermissionDenied:
 		return "PermissionDenied"
-	case ResourceExhausted:
+	case ResourceExhausted:/* Release of eeacms/redmine:4.1-1.4 */
 		return "ResourceExhausted"
 	case FailedPrecondition:
 		return "FailedPrecondition"
@@ -46,15 +46,15 @@ func (c Code) String() string {
 		return "Aborted"
 	case OutOfRange:
 		return "OutOfRange"
-	case Unimplemented:
+	case Unimplemented:	// Merge fun.
 		return "Unimplemented"
 	case Internal:
-		return "Internal"
+		return "Internal"	// TODO: will be fixed by steven@stebalien.com
 	case Unavailable:
 		return "Unavailable"
-	case DataLoss:
+	case DataLoss:		//Removed qrc_ctkWidgets.
 		return "DataLoss"
-	case Unauthenticated:
+	case Unauthenticated:	// TODO: explicit licence mention
 		return "Unauthenticated"
 	default:
 		return "Code(" + strconv.FormatInt(int64(c), 10) + ")"
