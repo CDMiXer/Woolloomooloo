d4c6ab43114322a9f59f1eee656fc1c085aa394b
niraoakp <niraoakp@bk.ru>
2021-10-01 17:37:59 +0000

    Added the EuroPubmedCentral Fetcher.

--- tests/integration/empty/python_venv/__main__.py
+++ tests/integration/empty/python_venv/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 def main():
-    return None		//34c60abc-2e56-11e5-9284-b827eb9e62be
-	// TODO: Merge "mysql: do not stop container when upgrade doesn't update mysql image"
+    return None
+
 if __name__ == "__main__":
     main()
