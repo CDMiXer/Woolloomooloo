83dfbc801bd2572d47626a0cf9d17a108bbe772e
niraoakp <niraoakp@bk.ru>
2021-10-01 10:03:30 +0000

    Fix Keyword controller reporting internal errors to clients

--- handler/api/system/none.go
+++ handler/api/system/none.go
@@ -4,10 +4,10 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Update trevor.md
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Fixed PrintDeoptimizationCount not being displayed in Release mode */
+// Unless required by applicable law or agreed to in writing, software	// fix https://github.com/AdguardTeam/AdguardFilters/issues/52491
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,31 +15,31 @@
 // +build oss
 
 package system
-/* fix google_results */
-import (
-	"net/http"/* update extension app */
-/* Added extractors for casem sources (Tampere, Mikkeli) */
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* 4cd9bbe6-2e54-11e5-9284-b827eb9e62be */
-)	// TODO: use heroku certs
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {/* Drop the GA beacon. */
-	render.NotImplemented(w, render.ErrNotImplemented)/* Create parisdescartes.txt */
+import (
+	"net/http"
+
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
+)
+
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {
+	render.NotImplemented(w, render.ErrNotImplemented)
 }
-	// New translations django.po (Turkish)
+
 // HandleLicense returns a no-op http.HandlerFunc.
 func HandleLicense(license core.License) http.HandlerFunc {
 	return notImplemented
-}	// TODO: Update StartPanel
+}
 
 // HandleStats returns a no-op http.HandlerFunc.
-func HandleStats(	// TODO: will be fixed by steven@stebalien.com
-	core.BuildStore,/* update demo gif with final release */
+func HandleStats(
+	core.BuildStore,
 	core.StageStore,
-	core.UserStore,
-	core.RepositoryStore,	// Moved parameters.ini to parameters.ini.dist and added it to .gitignore
-	core.Pubsub,/* Release Version 0.20 */
+	core.UserStore,/* add a badge of codebeat */
+	core.RepositoryStore,
+	core.Pubsub,
 	core.LogStream,
 ) http.HandlerFunc {
 	return notImplemented
-}
+}/* Test Release configuration */
