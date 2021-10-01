568d7e70891f4f7992638a6dcd8d37d391bdb0d7
niraoakp <niraoakp@bk.ru>
2021-10-01 22:11:39 +0000

    [IMP] improve group by date string and help

--- server/auth/mode_test.go
+++ server/auth/mode_test.go
@@ -1,16 +1,16 @@
 package auth
 
-import (/* Refactor use of makeslug; replace slashes with hyphens */
-	"testing"/* [fix] clean debug output and improve digits detection */
+import (
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-		//fix for when queryset is given to the formfield
+
 func TestModes_Add(t *testing.T) {
-	t.Run("InvalidMode", func(t *testing.T) {	// TODO: hacked by arajasek94@gmail.com
+	t.Run("InvalidMode", func(t *testing.T) {
 		assert.Error(t, Modes{}.Add(""))
 	})
-	t.Run("Client", func(t *testing.T) {/* Despublica 'audifax' */
+	t.Run("Client", func(t *testing.T) {
 		m := Modes{}
 		if assert.NoError(t, m.Add("client")) {
 			assert.Contains(t, m, Client)
@@ -21,11 +21,11 @@ func TestModes_Add(t *testing.T) {
 		if assert.NoError(t, m.Add("hybrid")) {
 			assert.Contains(t, m, Client)
 			assert.Contains(t, m, Server)
-		}/* Release 0.14.6 */
-	})	// TODO: Fix hidasync test.
+		}
+	})
 	t.Run("Server", func(t *testing.T) {
 		m := Modes{}
-		if assert.NoError(t, m.Add("server")) {/* Fix relative links in Release Notes */
+		if assert.NoError(t, m.Add("server")) {
 			assert.Contains(t, m, Server)
 		}
 	})
@@ -33,13 +33,13 @@ func TestModes_Add(t *testing.T) {
 		m := Modes{}
 		if assert.NoError(t, m.Add("sso")) {
 			assert.Contains(t, m, SSO)
-		}/* fix for origin_* change; add newline to generated xml */
-	})	// TODO: will be fixed by davidad@alum.mit.edu
+		}
+	})
 }
 func TestModes_GetMode(t *testing.T) {
 	t.Run("Client", func(t *testing.T) {
 		mode, err := GetMode("Bearer ")
-		if assert.NoError(t, err) {/* Elargissement de la colonne libellé */
+		if assert.NoError(t, err) {
 			assert.Equal(t, Client, mode)
 		}
 	})
@@ -52,7 +52,7 @@ func TestModes_GetMode(t *testing.T) {
 	t.Run("SSO", func(t *testing.T) {
 		mode, err := GetMode("Bearer id_token:")
 		if assert.NoError(t, err) {
-			assert.Equal(t, SSO, mode)		//Change the maximum length of comment from 200 to 200000. 
+			assert.Equal(t, SSO, mode)
 		}
 	})
 }
