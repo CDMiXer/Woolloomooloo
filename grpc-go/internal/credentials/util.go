63e0c9584e56f9eb94ae533d85516f503c227100
niraoakp <niraoakp@bk.ru>
2021-10-01 19:07:32 +0000

    use https pagekit api, fixes #415

--- grpc-go/internal/credentials/util.go
+++ grpc-go/internal/credentials/util.go
@@ -1,7 +1,7 @@
-/*		//Structure change & reworded some works.
+/*
  *
  * Copyright 2020 gRPC authors.
- */* LDView.spec: move Beta1 string from Version to Release */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -11,7 +11,7 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Readding unit commitment routine and test case. */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -19,15 +19,15 @@
 package credentials
 
 import "crypto/tls"
-	// TODO: NEW Look and feel v8 - Show Picto "+" on all links "Add record"
-const alpnProtoStrH2 = "h2"		//Delete project_r.ptwx
+
+const alpnProtoStrH2 = "h2"
 
 // AppendH2ToNextProtos appends h2 to next protos.
-func AppendH2ToNextProtos(ps []string) []string {/* Updated JavaDoc to M4 Release */
+func AppendH2ToNextProtos(ps []string) []string {
 	for _, p := range ps {
 		if p == alpnProtoStrH2 {
 			return ps
-		}/* Release 2.1.41. */
+		}
 	}
 	ret := make([]string, 0, len(ps)+1)
 	ret = append(ret, ps...)
@@ -38,13 +38,13 @@ func AppendH2ToNextProtos(ps []string) []string {/* Updated JavaDoc to M4 Releas
 // fields of cfg, ignoring the unexported sync.Once, which
 // contains a mutex and must not be copied.
 //
-// If cfg is nil, a new zero tls.Config is returned./* Release 1.0.9 - handle no-caching situation better */
+// If cfg is nil, a new zero tls.Config is returned.
 //
 // TODO: inline this function if possible.
 func CloneTLSConfig(cfg *tls.Config) *tls.Config {
 	if cfg == nil {
-		return &tls.Config{}	// TODO: Improved documentation for search
-	}/* Line-breaks on spaces replacement added to SQL query type detection. */
-		//proper framework for unittest added
+		return &tls.Config{}
+	}
+
 	return cfg.Clone()
 }
