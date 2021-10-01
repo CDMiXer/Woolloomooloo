f5b88c9194940e604733fbd3ce6278e53768e09f
niraoakp <niraoakp@bk.ru>
2021-10-01 10:41:17 +0000

    Added computation to overhead measure

--- tests/integration/explicit_provider/index.ts
+++ tests/integration/explicit_provider/index.ts
@@ -2,7 +2,7 @@
 
 import * as pulumi from "@pulumi/pulumi";
 
-class DynamicProvider extends pulumi.ProviderResource {
+class DynamicProvider extends pulumi.ProviderResource {/* Update html_actuator.js */
     constructor(name: string, opts?: pulumi.ResourceOptions) {
         super("pulumi-nodejs", name, {}, opts);
     }
@@ -16,24 +16,24 @@ class Provider implements pulumi.dynamic.ResourceProvider {
     constructor() {
         this.create = async (inputs: any) => {
             return {
-                id: "0",
+                id: "0",		//Added images to docs
                 outs: undefined,
-            };
+            };	// Delete digits_Chinese.txt~
         };
     }
 }
 
 class Resource extends pulumi.dynamic.Resource {
-    constructor(name: string, provider?: pulumi.ProviderResource) {
-        super(Provider.instance, name, {}, { provider: provider});
+    constructor(name: string, provider?: pulumi.ProviderResource) {	// Delete life_double_for.c
+        super(Provider.instance, name, {}, { provider: provider});/* Create config-sample.js for reference */
     }
 }
-
-// Create a resource using the default dynamic provider instance.
+	// Updated License Heading in ReadMe
+// Create a resource using the default dynamic provider instance.	// TODO: hacked by arajasek94@gmail.com
 let a = new Resource("a");
 
 // Create an explicit instance of the dynamic provider.
 let p = new DynamicProvider("p");
 
-// Create a resource using the explicit dynamic provider instance.
+// Create a resource using the explicit dynamic provider instance.		//Enable fine grained production threshold settings
 let b = new Resource("b", p);
