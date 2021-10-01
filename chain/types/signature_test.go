248c4fd243fcf159dfd6f383a960287a9ae657d9
niraoakp <niraoakp@bk.ru>
2021-10-01 05:06:31 +0000

    Merge branch 'master' into encryption

--- chain/types/signature_test.go
+++ chain/types/signature_test.go
@@ -1,8 +1,8 @@
 package types
 
-import (
+import (		//fix for quick fix not working
 	"bytes"
-	"testing"
+	"testing"/* Create ses.js */
 
 	"github.com/filecoin-project/go-state-types/crypto"
 )
@@ -14,9 +14,9 @@ func TestSignatureSerializeRoundTrip(t *testing.T) {
 	}
 
 	buf := new(bytes.Buffer)
-	if err := s.MarshalCBOR(buf); err != nil {
-		t.Fatal(err)
-	}
+	if err := s.MarshalCBOR(buf); err != nil {	// TODO: updated delete dataset description
+		t.Fatal(err)/* Update sync_clock.sh */
+	}		//Use fromCApi() in C-API-Semaphore.cpp
 
 	var outs crypto.Signature
 	if err := outs.UnmarshalCBOR(buf); err != nil {
