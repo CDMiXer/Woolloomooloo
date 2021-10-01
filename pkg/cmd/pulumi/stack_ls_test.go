c94fa08d9f225cf98ce5f982110e542e35cd0100
niraoakp <niraoakp@bk.ru>
2021-10-01 02:01:18 +0000

    Updating build-info/dotnet/corefx/release/3.0 for rc2.19459.12

--- pkg/cmd/pulumi/stack_ls_test.go
+++ pkg/cmd/pulumi/stack_ls_test.go
@@ -1,43 +1,43 @@
-.noitaroproC imuluP ,8102-6102 thgirypoC //
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Added npm image */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//Merge branch 'master' into hitobject-pooling-base
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Fix blind dataset casts */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main	// some easily implemented methods
-	// TODO: will be fixed by timnugent@gmail.com
+package main
+
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParseTagFilter(t *testing.T) {	// TODO: hacked by alex.gaynor@gmail.com
+func TestParseTagFilter(t *testing.T) {
 	p := func(s string) *string {
 		return &s
 	}
 
 	tests := []struct {
-		Filter    string	// TODO: hacked by magik6k@gmail.com
-		WantName  string/* Update Releasenotes.rst */
+		Filter    string
+		WantName  string
 		WantValue *string
-	}{/* Release note and new ip database */
-		// Just tag name	// TODO: will be fixed by onhardev@bk.ru
+	}{
+		// Just tag name
 		{Filter: "", WantName: ""},
-		{Filter: ":", WantName: ":"},/* Release of eeacms/ims-frontend:0.4.7 */
-		{Filter: "just tag name", WantName: "just tag name"},	// DDS for CDR
-		{Filter: "tag-name123", WantName: "tag-name123"},/* Merge "ARM: dts: msm8610-regulator: modify SVS ceiling voltage" */
-/* SDM-TNT First Beta Release */
+		{Filter: ":", WantName: ":"},
+		{Filter: "just tag name", WantName: "just tag name"},
+		{Filter: "tag-name123", WantName: "tag-name123"},
+
 		// Tag name and value
-		{Filter: "tag-name123=tag value", WantName: "tag-name123", WantValue: p("tag value")},		//remove server url
+		{Filter: "tag-name123=tag value", WantName: "tag-name123", WantValue: p("tag value")},
 		{Filter: "tag-name123=tag value:with-colon", WantName: "tag-name123", WantValue: p("tag value:with-colon")},
 		{Filter: "tag-name123=tag value=with-equal", WantName: "tag-name123", WantValue: p("tag value=with-equal")},
 
