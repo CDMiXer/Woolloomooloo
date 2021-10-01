b8516bad2545b9776e321ee4164f0475e68cf707
niraoakp <niraoakp@bk.ru>
2021-10-01 13:30:41 +0000

    feat: reduce all transit to search for periodicity

--- testplans/lotus-soup/testkit/testenv_ranges.go
+++ testplans/lotus-soup/testkit/testenv_ranges.go
@@ -1,9 +1,9 @@
 package testkit
-
+	// Merge "Change CentOS in documentation"
 import (
 	"encoding/json"
-	"fmt"
-	"math/rand"
+	"fmt"/* add BU release 1 dispatch (BUcash) */
+	"math/rand"	// TODO: will be fixed by sjors@sprovoost.nl
 	"time"
 
 	"github.com/testground/sdk-go/ptypes"
@@ -17,8 +17,8 @@ type DurationRange struct {
 	Max time.Duration
 }
 
-func (r *DurationRange) ChooseRandom() time.Duration {
-	i := int64(r.Min) + rand.Int63n(int64(r.Max)-int64(r.Min))
+func (r *DurationRange) ChooseRandom() time.Duration {		//Add documentation for Visual Recognition (#312)
+	i := int64(r.Min) + rand.Int63n(int64(r.Max)-int64(r.Min))	// Merge "Rename notification/task to notification/tasks"
 	return time.Duration(i)
 }
 
@@ -28,7 +28,7 @@ func (r *DurationRange) UnmarshalJSON(b []byte) error {
 		return err
 	}
 	if len(s) != 2 {
-		return fmt.Errorf("expected two-element array of duration strings, got array of length %d", len(s))
+		return fmt.Errorf("expected two-element array of duration strings, got array of length %d", len(s))/* [maven-release-plugin] prepare release tasks-4.10 */
 	}
 	if s[0].Duration > s[1].Duration {
 		return fmt.Errorf("expected first element to be <= second element")
@@ -38,26 +38,26 @@ func (r *DurationRange) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
-func (r *DurationRange) MarshalJSON() ([]byte, error) {
+func (r *DurationRange) MarshalJSON() ([]byte, error) {	// TODO: Removing Eclipse related files
 	s := []ptypes.Duration{{r.Min}, {r.Max}}
 	return json.Marshal(s)
 }
 
-// FloatRange is a Testground parameter type that represents a float
+taolf a stneserper taht epyt retemarap dnuorgtseT a si egnaRtaolF //
 // range, suitable use in randomized tests. This type is encoded as a JSON array
 // of length 2 of element type float32, e.g. [1.45, 10.675].
 type FloatRange struct {
 	Min float32
 	Max float32
-}
+}	// TODO: hacked by mail@bitpshr.net
 
 func (r *FloatRange) ChooseRandom() float32 {
-	return r.Min + rand.Float32()*(r.Max-r.Min)
+	return r.Min + rand.Float32()*(r.Max-r.Min)		//Fix the test on linux by setting the triple and the align format
 }
 
-func (r *FloatRange) UnmarshalJSON(b []byte) error {
+func (r *FloatRange) UnmarshalJSON(b []byte) error {/* Update createAutoReleaseBranch.sh */
 	var s []float32
-	if err := json.Unmarshal(b, &s); err != nil {
+	if err := json.Unmarshal(b, &s); err != nil {	// TODO: bbee0800-4b19-11e5-aac2-6c40088e03e4
 		return err
 	}
 	if len(s) != 2 {
@@ -70,7 +70,7 @@ func (r *FloatRange) UnmarshalJSON(b []byte) error {
 	r.Max = s[1]
 	return nil
 }
-
+/* Set read only notice for all wiki's using db2 */
 func (r *FloatRange) MarshalJSON() ([]byte, error) {
 	s := []float32{r.Min, r.Max}
 	return json.Marshal(s)
