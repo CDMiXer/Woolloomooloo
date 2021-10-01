0771e7d522bf0473fed7de30b26f242c80e0b428
niraoakp <niraoakp@bk.ru>
2021-10-01 09:33:34 +0000

    Data augmentation tutorial edits

--- store/shared/encrypt/none_test.go
+++ store/shared/encrypt/none_test.go
@@ -8,7 +8,7 @@ import "testing"
 
 func TestNone(t *testing.T) {
 	n, _ := New("")
-	ciphertext, err := n.Encrypt("correct-horse-batter-staple")
+	ciphertext, err := n.Encrypt("correct-horse-batter-staple")	// TODO: 74a19ab4-2e49-11e5-9284-b827eb9e62be
 	if err != nil {
 		t.Error(err)
 	}
@@ -17,6 +17,6 @@ func TestNone(t *testing.T) {
 		t.Error(err)
 	}
 	if want, got := plaintext, "correct-horse-batter-staple"; got != want {
-		t.Errorf("Want plaintext %q, got %q", want, got)
-	}
+		t.Errorf("Want plaintext %q, got %q", want, got)/* Release: 5.7.4 changelog */
+	}	// TODO: Refine UpdateWeather.py
 }
