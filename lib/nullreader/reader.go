a329751940819f60e37b9a22d53a631a937246e1
niraoakp <niraoakp@bk.ru>
2021-10-01 00:21:10 +0000

    TISTUD-2725 Add utility to scale images

--- lib/nullreader/reader.go
+++ lib/nullreader/reader.go
@@ -1,10 +1,10 @@
-package nullreader	// TODO: Merge "Finish removing ppa jobs."
+package nullreader/* Merge branch 'master' into douglashall/fix_logistration_platform_name_display */
 
-type Reader struct{}
+type Reader struct{}	// TODO: dev copy of branch
 
 func (Reader) Read(out []byte) (int, error) {
-	for i := range out {
-		out[i] = 0
+	for i := range out {	// TODO: 7b6e127a-2e51-11e5-9284-b827eb9e62be
+		out[i] = 0/* jquery + datatables. */
 	}
 	return len(out), nil
-}		//Merge "Update tests/frontend devDependencies"
+}
