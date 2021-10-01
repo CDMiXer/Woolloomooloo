6ca8ec0ed2b9f3e798ea0d2b3d5f1f817aa9736d
niraoakp <niraoakp@bk.ru>
2021-10-01 11:55:21 +0000

    Improved ecore diagram layout.

--- pkg/codegen/internal/test/testdata/random-pet.pp.ts
+++ pkg/codegen/internal/test/testdata/random-pet.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as random from "@pulumi/random";
-
+/* [artifactory-release] Release version 3.3.0.RELEASE */
 const random_pet = new random.RandomPet("random_pet", {prefix: "doggo"});
