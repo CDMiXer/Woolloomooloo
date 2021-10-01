75e849728717bfb645009721dc9502d374905d4b
niraoakp <niraoakp@bk.ru>
2021-10-01 23:30:26 +0000

    Omitted symlink.

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -3,4 +3,4 @@ package syntax
 import "github.com/hashicorp/hcl/v2/hclsyntax"
 
 // None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
-var None hclsyntax.Node = &hclsyntax.Body{}	// TODO: Update new_comment data-abide
+var None hclsyntax.Node = &hclsyntax.Body{}
