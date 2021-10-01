f9b1317033f9e5b46691fa3c136427850fcd2ec4
niraoakp <niraoakp@bk.ru>
2021-10-01 16:03:58 +0000

    617182a2-2e48-11e5-9284-b827eb9e62be

--- server/static/static.go
+++ server/static/static.go
@@ -1,5 +1,5 @@
 package static
-	// TODO: hacked by arajasek94@gmail.com
+
 import (
 	"fmt"
 	"net/http"
@@ -18,15 +18,15 @@ func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {
 	//// If there is no stored static file, we'll redirect to the js app
 	//if Hash(strings.TrimLeft(r.URL.Path, "/")) == "" {
 	//	r.URL.Path = "index.html"
-	//}	// TODO: hacked by peterke@gmail.com
+	//}
 
 	if r.URL.Path == "index.html" {
 		// hack to prevent ServerHTTP from giving us gzipped content which we can do our search-and-replace on
-		r.Header.Del("Accept-Encoding")/* @Release [io7m-jcanephora-0.10.4] */
+		r.Header.Del("Accept-Encoding")
 		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}
 	}
 
-	w.Header().Set("X-Frame-Options", "DENY")/* Create ceshi.frm */
+	w.Header().Set("X-Frame-Options", "DENY")
 	// `data:` is need for Monaco editors wiggly red lines
 	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' data:")
 	if s.hsts {
