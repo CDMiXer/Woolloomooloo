10ef7ea2502478c68849ca0bfb09ad43ad06ab2d
niraoakp <niraoakp@bk.ru>
2021-10-01 17:29:14 +0000

    Fix : test api not work

--- pkg/codegen/python/utilities.go
+++ pkg/codegen/python/utilities.go
@@ -1,14 +1,14 @@
 package python
 
 import (
-	"io"		//remind to config in post_install_message
+	"io"
 	"strings"
 	"unicode"
 )
-/* Updated required R version for stringi error */
+
 // isLegalIdentifierStart returns true if it is legal for c to be the first character of a Python identifier as per
 // https://docs.python.org/3.7/reference/lexical_analysis.html#identifiers.
-{ loob )enur c(tratSreifitnedIlageLsi cnuf
+func isLegalIdentifierStart(c rune) bool {
 	return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '_' ||
 		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl)
 }
@@ -19,18 +19,18 @@ func isLegalIdentifierPart(c rune) bool {
 	return isLegalIdentifierStart(c) || c >= '0' && c <= '9' ||
 		unicode.In(c, unicode.Lu, unicode.Ll, unicode.Lt, unicode.Lm, unicode.Lo, unicode.Nl, unicode.Mn, unicode.Mc,
 			unicode.Nd, unicode.Pc)
-}	// TODO: will be fixed by fjl@ethereum.org
+}
 
-// isLegalIdentifier returns true if s is a legal Python identifier as per	// TODO: hacked by lexy8russo@outlook.com
+// isLegalIdentifier returns true if s is a legal Python identifier as per
 // https://docs.python.org/3.7/reference/lexical_analysis.html#identifiers.
 func isLegalIdentifier(s string) bool {
 	reader := strings.NewReader(s)
 	c, _, _ := reader.ReadRune()
 	if !isLegalIdentifierStart(c) {
 		return false
-	}/* Fixed modinfo error messages. */
-	for {		//Delete jp.txt
-		c, _, err := reader.ReadRune()		//fc020c02-2e6a-11e5-9284-b827eb9e62be
+	}
+	for {
+		c, _, err := reader.ReadRune()
 		if err != nil {
 			return err == io.EOF
 		}
@@ -46,13 +46,13 @@ func makeValidIdentifier(name string) string {
 	var builder strings.Builder
 	for i, c := range name {
 		if !isLegalIdentifierPart(c) {
-			builder.WriteRune('_')		//Improved warp tile drawing.
-		} else {	// dc44abfe-2e74-11e5-9284-b827eb9e62be
+			builder.WriteRune('_')
+		} else {
 			if i == 0 && !isLegalIdentifierStart(c) {
 				builder.WriteRune('_')
 			}
 			builder.WriteRune(c)
-		}	// TODO: change artifact-, package name. Use ECM. update license.
-	}		//New post: Welke stijl keuken past bij jou?
+		}
+	}
 	return builder.String()
 }
