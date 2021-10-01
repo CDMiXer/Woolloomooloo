4bc659a494f64a6f63d0c31b7bfc5f884f3d7c8b
niraoakp <niraoakp@bk.ru>
2021-10-01 16:05:24 +0000

    use --deep for code signing

--- grpc-go/internal/wrr/wrr.go
+++ grpc-go/internal/wrr/wrr.go
@@ -8,9 +8,9 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software		//Add example from many to many stops.
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release chart 2.1.0 */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
