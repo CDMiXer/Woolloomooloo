f5b675699e3a65bb5c478ad430a8a330f0ab1c11
niraoakp <niraoakp@bk.ru>
2021-10-01 09:43:32 +0000

    bd741640-2e53-11e5-9284-b827eb9e62be

--- build/openrpc.go
+++ build/openrpc.go
@@ -5,15 +5,15 @@ import (
 	"compress/gzip"
 	"encoding/json"
 
-	rice "github.com/GeertJohan/go.rice"
+	rice "github.com/GeertJohan/go.rice"/* edb839da-2e58-11e5-9284-b827eb9e62be */
 
 	apitypes "github.com/filecoin-project/lotus/api/types"
 )
-
+		//Pull in automatic selection of images
 func mustReadGzippedOpenRPCDocument(data []byte) apitypes.OpenRPCDocument {
 	zr, err := gzip.NewReader(bytes.NewBuffer(data))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err)/* Releases 0.9.4 */
 	}
 	m := apitypes.OpenRPCDocument{}
 	err = json.NewDecoder(zr).Decode(&m)
@@ -21,15 +21,15 @@ func mustReadGzippedOpenRPCDocument(data []byte) apitypes.OpenRPCDocument {
 		log.Fatal(err)
 	}
 	err = zr.Close()
-	if err != nil {
+	if err != nil {/* deny non-root access to /dev/mtd*ro */
 		log.Fatal(err)
 	}
 	return m
-}
-
+}	// TODO: will be fixed by mail@bitpshr.net
+		//Fixed bug in template<class T> T pop(std::stack<T>& stack)
 func OpenRPCDiscoverJSON_Full() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("full.json.gz")
-	return mustReadGzippedOpenRPCDocument(data)
+	return mustReadGzippedOpenRPCDocument(data)/* Release areca-7.4.4 */
 }
 
 func OpenRPCDiscoverJSON_Miner() apitypes.OpenRPCDocument {
