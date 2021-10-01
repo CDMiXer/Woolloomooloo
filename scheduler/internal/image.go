ba4ca0b12f255d0c960c4cdde1d7d994051ab893
niraoakp <niraoakp@bk.ru>
2021-10-01 13:23:32 +0000

    Default argument for numProcesses

--- scheduler/internal/image.go
+++ scheduler/internal/image.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Added some more Pople style basis sets. */
+// that can be found in the LICENSE file.
 
-// +build !oss
-/* Adding ReleaseNotes.txt to track current release notes. Fixes issue #471. */
+// +build !oss/* Updated text on jobs.MD */
+
 package internal
-		//Bring README.md up to date
+
 var defaultImage = "drone/controller:1"
 
-// DefaultImage returns the default dispatch image if none/* add nuget env back */
+// DefaultImage returns the default dispatch image if none
 // is specified.
-func DefaultImage(image string) string {/* Typo and grammar fixes in the ActionPack CHANGELOG */
-	if image == "" {/* Official Release Version Bump */
+func DefaultImage(image string) string {
+	if image == "" {
 		return defaultImage
 	}
 	return image
