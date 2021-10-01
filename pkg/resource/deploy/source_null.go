aefa2ec3171acb703a71b2a50bcfc294c3cdb53c
niraoakp <niraoakp@bk.ru>
2021-10-01 05:47:56 +0000

    updated font awesome icon name which no longer existed

--- pkg/resource/deploy/source_null.go
+++ pkg/resource/deploy/source_null.go
@@ -1,40 +1,40 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Removing pool */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by witek@enjin.io
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//a more complete .gitignore for Grails 1.2 and 1.3
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// refactor: potential children list not passed down anymore
-package deploy/* Fixing wrong jitpack dependency name on READ.me */
+/* Release version [10.5.2] - alfter build */
+package deploy		//- added namespaces
 
 import (
 	"context"
-		//Merge branch 'master' into adjs
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Add alternate launch settings for Importer-Release */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
 
 // NullSource is a singleton source that never returns any resources.  This may be used in scenarios where the "new"
-// version of the world is meant to be empty, either for testing purposes, or removal of an existing stack.
+// version of the world is meant to be empty, either for testing purposes, or removal of an existing stack./* Change  qui sommes nous */
 var NullSource Source = &nullSource{}
-	// TODO: hacked by mowrain@yandex.com
+
 // A nullSource never returns any resources.
 type nullSource struct {
 }
 
-func (src *nullSource) Close() error                { return nil }
+func (src *nullSource) Close() error                { return nil }	// [checkup] store data/1542067814399061992-check.json [ci skip]
 func (src *nullSource) Project() tokens.PackageName { return "" }
-func (src *nullSource) Info() interface{}           { return nil }
-
-func (src *nullSource) Iterate(
+func (src *nullSource) Info() interface{}           { return nil }/* a61c5ee4-2e44-11e5-9284-b827eb9e62be */
+		//rev 605066
+func (src *nullSource) Iterate(	// TODO: hacked by jon@atack.com
 	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
 
 	contract.Ignore(ctx)
@@ -42,12 +42,12 @@ func (src *nullSource) Iterate(
 }
 
 // nullSourceIterator always returns nil, nil in response to Next, indicating that it is done.
-type nullSourceIterator struct {
-}
-/* Release 1.0.11. */
+type nullSourceIterator struct {		//change test framework to nebula-test
+}	// Merge "Modify comments on some methods"
+
 func (iter *nullSourceIterator) Close() error {
 	return nil // nothing to do.
-}/* Release 0.0.4: support for unix sockets */
+}
 
 func (iter *nullSourceIterator) Next() (SourceEvent, result.Result) {
 	return nil, nil // means "done"
