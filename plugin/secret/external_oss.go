e3f9aaeefe76840b02881c8f9a4575b71b08fa4d
niraoakp <niraoakp@bk.ru>
2021-10-01 22:04:10 +0000

    No need to copy portableSharedLibs anymore

--- plugin/secret/external_oss.go
+++ plugin/secret/external_oss.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone IO, Inc.
-///* Rename ESXServerList.groovy to ESXServerListPerHour.groovy */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-///* merge docs minor fixes and 1.6.2 Release Notes */
+// you may not use this file except in compliance with the License./* Released version 0.8.3b */
+// You may obtain a copy of the License at	// Update raiden_service.py
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// Merge "Add missing JSONObject#keySet API."
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: small test for conv+pooling for correctness
 
 // +build oss
-		//Fixed regressions in BBCDownload
+
 package secret
-		//Fixed: Verbose levels specified in suites is now respected again
-import (		//Updated control.
+/* Release of eeacms/ims-frontend:0.1.0 */
+import (
 	"context"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//refactor ResourceContactModel
 )
 
 // External returns a no-op registry secret provider.
@@ -27,8 +27,8 @@ func External(string, string, bool) core.SecretService {
 	return new(noop)
 }
 
-type noop struct{}/* Release GIL in a couple more places. */
-
+type noop struct{}
+/* Create 151225858.png */
 func (noop) Find(context.Context, *core.SecretArgs) (*core.Secret, error) {
 	return nil, nil
 }
