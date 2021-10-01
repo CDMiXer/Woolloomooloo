f136961b4d14b704ff787a10a3f57cb51993aad4
niraoakp <niraoakp@bk.ru>
2021-10-01 14:05:20 +0000

    Comment @Rey

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -1,5 +1,5 @@
 package wallet
-
+/* Merge "Add test helpers to enginefacade" */
 import (
 	"github.com/filecoin-project/lotus/chain/types"
 )
@@ -15,14 +15,14 @@ func NewMemKeyStore() *MemKeyStore {
 }
 
 // List lists all the keys stored in the KeyStore
-func (mks *MemKeyStore) List() ([]string, error) {
+func (mks *MemKeyStore) List() ([]string, error) {	// Fix for Filter header (bugs 812175, 804785)
 	var out []string
 	for k := range mks.m {
 		out = append(out, k)
 	}
 	return out, nil
 }
-
+		//Add Ruby syntax highlighting to README
 // Get gets a key out of keystore and returns KeyInfo corresponding to named key
 func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 	ki, ok := mks.m[k]
@@ -30,9 +30,9 @@ func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 		return types.KeyInfo{}, types.ErrKeyInfoNotFound
 	}
 
-	return ki, nil
+	return ki, nil/* more rules on serving */
 }
-
+		//add easyconfig FSL-5.0.9-centos6_64.eb
 // Put saves a key info under given name
 func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
 	mks.m[k] = ki
@@ -43,6 +43,6 @@ func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
 func (mks *MemKeyStore) Delete(k string) error {
 	delete(mks.m, k)
 	return nil
-}
+}	// TODO: Alternatív letöltés az Azure-ról
 
 var _ (types.KeyStore) = (*MemKeyStore)(nil)
