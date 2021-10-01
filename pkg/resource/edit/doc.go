c04e54abf0c0e9c365b08a7da45e87e9e697c56d
niraoakp <niraoakp@bk.ru>
2021-10-01 21:48:12 +0000

    Improved name generation, including names for empty systems

--- pkg/resource/edit/doc.go
+++ pkg/resource/edit/doc.go
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* @Release [io7m-jcanephora-0.23.4] */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Set referrer policy */
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0		//d8dd4723-2e9b-11e5-ae02-a45e60cdfd11
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,5 +13,5 @@
 // limitations under the License.
 
 // Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level
-// tools that present a means for users to surgically edit their state.
+// tools that present a means for users to surgically edit their state.		//Set version to 0.3.
 package edit
