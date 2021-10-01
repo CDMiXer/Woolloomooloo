07279370e8838af56cadf79caa03f569a253edc9
niraoakp <niraoakp@bk.ru>
2021-10-01 18:13:42 +0000

    Create last-build-log

--- operator/runner/before_test.go
+++ operator/runner/before_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License		//ENH: Added spinner for gas types.
+// that can be found in the LICENSE file./* Release of eeacms/www-devel:18.6.13 */
 
 package runner
