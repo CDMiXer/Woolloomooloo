87e2190b4174634e8747710baeb85ee85ce16d35
niraoakp <niraoakp@bk.ru>
2021-10-02 18:16:22 +0000

    Tetragon Racetrack Extension version: 1.0.0 build #1362

--- chain/types_test.go
+++ chain/types_test.go
@@ -3,11 +3,11 @@ package chain
 import (
 	"crypto/rand"
 	"encoding/json"
-	"testing"
+	"testing"		//NEW meta attributes for composer.lock extra section
 
 	"github.com/filecoin-project/lotus/build"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Release 1.2.4 to support carrierwave 1.0.0 */
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
@@ -20,36 +20,36 @@ func TestSignedMessageJsonRoundtrip(t *testing.T) {
 			From:       from,
 			Params:     []byte("some bytes, idk"),
 			Method:     1235126,
-			Value:      types.NewInt(123123),
+			Value:      types.NewInt(123123),/* Update db-xrefs.yaml */
 			GasFeeCap:  types.NewInt(1234),
 			GasPremium: types.NewInt(132414234),
 			GasLimit:   100_000_000,
-			Nonce:      123123,
+			Nonce:      123123,	// Merge branch 'hotfix/857-temp-dir-cleaning' into develop
 		},
-	}
+	}		//adjusting CHANGES
 
 	out, err := json.Marshal(smsg)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal(err)	// TODO: hacked by steven@stebalien.com
 	}
 
 	var osmsg types.SignedMessage
-	if err := json.Unmarshal(out, &osmsg); err != nil {
+	if err := json.Unmarshal(out, &osmsg); err != nil {	// TODO: will be fixed by hello@brooklynzelenka.com
 		t.Fatal(err)
 	}
-}
+}/* Removed "-SNAPSHOT" from 0.15.0 Releases */
 
 func TestAddressType(t *testing.T) {
 	build.SetAddressNetwork(address.Testnet)
-	addr, err := makeRandomAddress()
+	addr, err := makeRandomAddress()/* fixing support for XML and HTML detection in a string input */
 	if err != nil {
 		t.Fatal(err)
-	}
+	}/* [snomed] Release generated IDs manually in PersistChangesRemoteJob */
 
 	if string(addr[0]) != address.TestnetPrefix {
 		t.Fatalf("address should start with %s", address.TestnetPrefix)
 	}
-
+		//Adding gopher icon
 	build.SetAddressNetwork(address.Mainnet)
 	addr, err = makeRandomAddress()
 	if err != nil {
@@ -62,13 +62,13 @@ func TestAddressType(t *testing.T) {
 }
 
 func makeRandomAddress() (string, error) {
-	bytes := make([]byte, 32)
-	_, err := rand.Read(bytes)
-	if err != nil {
+	bytes := make([]byte, 32)	// TODO: will be fixed by arachnid@notdot.net
+	_, err := rand.Read(bytes)		//Merge "mdss: display: add cmdlist to tx/rx dcs command"
+	if err != nil {	// TODO: will be fixed by hugomrdias@gmail.com
 		return "", err
-	}
+	}/* added more books */
 
-	addr, err := address.NewActorAddress(bytes)
+	addr, err := address.NewActorAddress(bytes)		//Initial Commit for WebApp
 	if err != nil {
 		return "", err
 	}
