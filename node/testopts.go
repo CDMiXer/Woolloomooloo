78e634d5ed469d5cb08c2dbe4224023289c04f2e
niraoakp <niraoakp@bk.ru>
2021-10-02 17:54:41 +0000

    Drop Python 2 support, add Python 3.7 support

--- node/testopts.go
+++ node/testopts.go
@@ -1,20 +1,20 @@
-package node/* sqlite backend solved */
+package node		//Replace the travis status badge with CircleCI.
 
 import (
 	"errors"
-/* Fix plugins export */
+
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 
-	"github.com/filecoin-project/lotus/node/modules/lp2p"	// TODO: hacked by souzau@yandex.com
-)
+	"github.com/filecoin-project/lotus/node/modules/lp2p"
+)/* Released version 0.8.30 */
 
-func MockHost(mn mocknet.Mocknet) Option {	// TODO: hacked by mikeal.rogers@gmail.com
-	return Options(/* More case handling for nice EOF errors. */
+func MockHost(mn mocknet.Mocknet) Option {
+	return Options(
 		ApplyIf(func(s *Settings) bool { return !s.Online },
 			Error(errors.New("MockHost must be specified after Online")),
-		),
-/* Release 0.95.215 */
+		),	// TODO: hacked by fjl@ethereum.org
+
 		Override(new(lp2p.RawHost), lp2p.MockHost),
-		Override(new(mocknet.Mocknet), mn),
+		Override(new(mocknet.Mocknet), mn),		//Create timerExample
 	)
 }
