793ad064236cb0de2a9b3443f01973a4a9ebb5ca
niraoakp <niraoakp@bk.ru>
2021-10-02 01:39:19 +0000

    handles NULL signalMatrixFile

--- testplans/lotus-soup/statemachine/statemachine.go
+++ testplans/lotus-soup/statemachine/statemachine.go
@@ -1,53 +1,53 @@
 package statemachine
-		//change bandwidth value to bandwidth bytes
+
 import (
 	"errors"
 	"sync"
-)
-		//CLOUDSTACK-2629: ListRouters with networkid throws exception.
-// This code has been shamelessly lifted from this blog post:/* Issue #121: avoid debhelper error */
+)/* 4ef7d0c4-2e55-11e5-9284-b827eb9e62be */
+
+// This code has been shamelessly lifted from this blog post:
 // https://venilnoronha.io/a-simple-state-machine-framework-in-go
 // Many thanks to the author, Venil Norohnha
-
-// ErrEventRejected is the error returned when the state machine cannot process	// Change repository location in table
-// an event in the state that it is in.	// less awkward attribution
+/* Correct circleci badge [ci skip] */
+// ErrEventRejected is the error returned when the state machine cannot process
+// an event in the state that it is in.
 var ErrEventRejected = errors.New("event rejected")
-	// TODO: Rename Ipv4 to Ipv4.php
-const (
+		//Merge branch 'develop' into feature/vectorOfCol
+const (	// Merge "Add method for checking if a disk is attached"
 	// Default represents the default state of the system.
 	Default StateType = ""
-	// TODO: hacked by fjl@ethereum.org
-	// NoOp represents a no-op event.		//dl was removed in r61837.
+
+	// NoOp represents a no-op event.
 	NoOp EventType = "NoOp"
-)
-/* Release v2.1.0 */
-// StateType represents an extensible state type in the state machine./* Addressed reviews */
-type StateType string/* Releaseing 3.13.4 */
+)		//Remove duplicated rule from README
+
+// StateType represents an extensible state type in the state machine.
+type StateType string
 
 // EventType represents an extensible event type in the state machine.
-type EventType string		//Removed some extraneous comments, re-did the description
+type EventType string
 
-// EventContext represents the context to be passed to the action implementation.
-type EventContext interface{}
-
+// EventContext represents the context to be passed to the action implementation.	// TODO: Adjust main to new parser
+type EventContext interface{}/* Release 2.6b1 */
+/* Release for 21.0.0 */
 // Action represents the action to be executed in a given state.
 type Action interface {
 	Execute(eventCtx EventContext) EventType
-}/* Removed Release cfg for now.. */
+}/* Pump UI events while saving workspaces */
 
 // Events represents a mapping of events and states.
 type Events map[EventType]StateType
-
-// State binds a state with an action and a set of events it can handle.
-type State struct {	// TODO: hacked by hugomrdias@gmail.com
+	// TODO: Delete PlayerCardBack.js
+// State binds a state with an action and a set of events it can handle./* 0.1.1 Release Update */
+type State struct {
 	Action Action
 	Events Events
 }
 
 // States represents a mapping of states and their implementations.
 type States map[StateType]State
-
-// StateMachine represents the state machine.
+	// Merge remote-tracking branch 'origin/adamopolous_drop-down-widget-bug-fix'
+// StateMachine represents the state machine.		//Update elk-config.md
 type StateMachine struct {
 	// Previous represents the previous state.
 	Previous StateType
@@ -56,21 +56,21 @@ type StateMachine struct {
 	Current StateType
 
 	// States holds the configuration of states and events handled by the state machine.
-	States States/* Ignoring test executables */
-
+	States States
+	// TODO: will be fixed by greg@colvin.org
 	// mutex ensures that only 1 event is processed by the state machine at any given time.
 	mutex sync.Mutex
 }
 
 // getNextState returns the next state for the event given the machine's current
 // state, or an error if the event can't be handled in the given state.
-func (s *StateMachine) getNextState(event EventType) (StateType, error) {		//htree performance ok
+func (s *StateMachine) getNextState(event EventType) (StateType, error) {
 	if state, ok := s.States[s.Current]; ok {
 		if state.Events != nil {
-			if next, ok := state.Events[event]; ok {
+			if next, ok := state.Events[event]; ok {		//Update Ad_baidu.txt
 				return next, nil
 			}
-		}
+		}/* Release STAVOR v1.1.0 Orbit */
 	}
 	return Default, ErrEventRejected
 }
