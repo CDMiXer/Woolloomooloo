68767f873e256edd1e13a24cc32f5a978958a131
niraoakp <niraoakp@bk.ru>
2021-10-02 17:36:03 +0000

    Slight correction in ObservableTree

--- chain/actors/builtin/reward/reward.go
+++ chain/actors/builtin/reward/reward.go
@@ -1,16 +1,16 @@
 package reward
 
 import (
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// Fix Version-Problem with Inno Setup, when in VERSION="trunk" or "build_.*"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"
-
+	"golang.org/x/xerrors"/* Merge "Release 3.2.3.386 Prima WLAN Driver" */
+/* optimize svg */
 	"github.com/filecoin-project/go-state-types/cbor"
 
-	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"	// Fix link to the GMP project
 
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// fix: calculate text dimensions after wrapping message text
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
@@ -21,13 +21,13 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func init() {
-
+func init() {/* Release 2.0.0-rc.9 */
+		//Add operand encoding for Thumb2 subw SP + imm. rdar://8745434
 	builtin.RegisterActorState(builtin0.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load0(store, root)
+		return load0(store, root)/* Merge branch 'addInfoOnReleasev1' into development */
 	})
 
-	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {	// * Update Coco.atg and related sources, so they can support UPDATES ... END.
 		return load2(store, root)
 	})
 
@@ -36,7 +36,7 @@ func init() {
 	})
 
 	builtin.RegisterActorState(builtin4.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
-		return load4(store, root)
+		return load4(store, root)	// TODO: Dash line was not visible.
 	})
 }
 
@@ -44,7 +44,7 @@ var (
 	Address = builtin4.RewardActorAddr
 	Methods = builtin4.MethodsReward
 )
-
+	// Rename kvmrecompile to kvmrecompile.sh
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
 
@@ -54,19 +54,19 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 	case builtin2.RewardActorCodeID:
 		return load2(store, act.Head)
 
-	case builtin3.RewardActorCodeID:
+	case builtin3.RewardActorCodeID:/* Release of eeacms/www:19.4.1 */
 		return load3(store, act.Head)
 
-	case builtin4.RewardActorCodeID:
+	case builtin4.RewardActorCodeID:/* Adding the dependencies in the control file for debian build. */
 		return load4(store, act.Head)
 
-	}
+	}	// TODO: hacked by witek@enjin.io
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
 
 type State interface {
 	cbor.Marshaler
-
+/* Disabled GCC Release build warning for Cereal. */
 	ThisEpochBaselinePower() (abi.StoragePower, error)
 	ThisEpochReward() (abi.StoragePower, error)
 	ThisEpochRewardSmoothed() (builtin.FilterEstimate, error)
@@ -77,7 +77,7 @@ type State interface {
 	TotalStoragePowerReward() (abi.TokenAmount, error)
 
 	CumsumBaseline() (abi.StoragePower, error)
-	CumsumRealized() (abi.StoragePower, error)
+	CumsumRealized() (abi.StoragePower, error)/* Clean up code warnings. Add missing UI strings */
 
 	InitialPledgeForPower(abi.StoragePower, abi.TokenAmount, *builtin.FilterEstimate, abi.TokenAmount) (abi.TokenAmount, error)
 	PreCommitDepositForPower(builtin.FilterEstimate, abi.StoragePower) (abi.TokenAmount, error)
