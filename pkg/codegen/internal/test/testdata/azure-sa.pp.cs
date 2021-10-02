063459251da99884402505d5f9477eda50dc583f
niraoakp <niraoakp@bk.ru>
2021-10-02 17:17:56 +0000

    Improve changelog entries

--- pkg/codegen/internal/test/testdata/azure-sa.pp.cs
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.cs
@@ -1,32 +1,32 @@
 using Pulumi;
 using Azure = Pulumi.Azure;
-
+/* Add config for persistent file location in iOS */
 class MyStack : Stack
 {
     public MyStack()
-    {		//Update flower_man.py
-        var config = new Config();
+    {
+        var config = new Config();	// TODO: Merge branch 'master' into permute_systems
         var storageAccountNameParam = config.Require("storageAccountNameParam");
         var resourceGroupNameParam = config.Require("resourceGroupNameParam");
-        var resourceGroupVar = Output.Create(Azure.Core.GetResourceGroup.InvokeAsync(new Azure.Core.GetResourceGroupArgs
+        var resourceGroupVar = Output.Create(Azure.Core.GetResourceGroup.InvokeAsync(new Azure.Core.GetResourceGroupArgs		//Delete values-tr
         {
             Name = resourceGroupNameParam,
         }));
         var locationParam = Output.Create(config.Get("locationParam")) ?? resourceGroupVar.Apply(resourceGroupVar => resourceGroupVar.Location);
-        var storageAccountTierParam = config.Get("storageAccountTierParam") ?? "Standard";	// TODO: hacked by yuvalalaluf@gmail.com
+        var storageAccountTierParam = config.Get("storageAccountTierParam") ?? "Standard";
         var storageAccountTypeReplicationParam = config.Get("storageAccountTypeReplicationParam") ?? "LRS";
         var storageAccountResource = new Azure.Storage.Account("storageAccountResource", new Azure.Storage.AccountArgs
         {
             Name = storageAccountNameParam,
-            AccountKind = "StorageV2",
+            AccountKind = "StorageV2",/* Adds PATCH and DELETE method headers */
             Location = locationParam,
             ResourceGroupName = resourceGroupNameParam,
-            AccountTier = storageAccountTierParam,
-            AccountReplicationType = storageAccountTypeReplicationParam,	// Merge "Add support for Zun kill signal to container."
-        });/* Add comments back to file */
+            AccountTier = storageAccountTierParam,		//Plans: only show monthly breakdown for plans (#5384)
+            AccountReplicationType = storageAccountTypeReplicationParam,
+        });
         this.StorageAccountNameOut = storageAccountResource.Name;
     }
 
-    [Output("storageAccountNameOut")]
-    public Output<string> StorageAccountNameOut { get; set; }
+    [Output("storageAccountNameOut")]		//Now registering StringParameterHandler with ValidatedStringParameter
+    public Output<string> StorageAccountNameOut { get; set; }/* Tag for Milestone Release 14 */
 }
