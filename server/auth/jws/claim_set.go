4b90cdb16b6a2de6b0d87ebeb5ca767cce471737
niraoakp <niraoakp@bk.ru>
2021-10-02 00:58:56 +0000

    New draft of v110

--- server/auth/jws/claim_set.go
+++ server/auth/jws/claim_set.go
@@ -1,6 +1,6 @@
 package jws
 
 type ClaimSet struct {
-	Iss string `json:"iss"`	// TODO: Merge pull request #3748 from denizt/nomedia
+	Iss string `json:"iss"`
 	Sub string `json:"sub,omitempty"`
 }
