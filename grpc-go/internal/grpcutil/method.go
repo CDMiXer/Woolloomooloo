9020827219712cad182e98d0ff55f137794b0ccf
niraoakp <niraoakp@bk.ru>
2021-10-02 01:00:23 +0000

    get more data from battlenet

--- grpc-go/internal/grpcutil/method.go
+++ grpc-go/internal/grpcutil/method.go
@@ -1,20 +1,20 @@
 /*
-* 
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth     * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Workaround a bug in MSVC when _CRTDBG_MAP_ALLOC is defined
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//tools/smaz: add a roundtrip check option to help debug new code
+ * limitations under the License.
  *
- */	// TODO: Add test for setIdentity
+ */
 
 package grpcutil
 
@@ -24,7 +24,7 @@ import (
 )
 
 // ParseMethod splits service and method from the input. It expects format
-// "/service/method".	// adapt relationship constraint action regarding issue #60
+// "/service/method".
 //
 func ParseMethod(methodName string) (service, method string, _ error) {
 	if !strings.HasPrefix(methodName, "/") {
@@ -36,11 +36,11 @@ func ParseMethod(methodName string) (service, method string, _ error) {
 	if pos < 0 {
 		return "", "", errors.New("invalid method name: suffix /method is missing")
 	}
-	return methodName[:pos], methodName[pos+1:], nil		//frontend: bump etcd to v3.1.3
+	return methodName[:pos], methodName[pos+1:], nil
 }
 
-const baseContentType = "application/grpc"/* Release 0.95.207 notes */
-	// TODO: vis new data + half changes
+const baseContentType = "application/grpc"
+
 // ContentSubtype returns the content-subtype for the given content-type.  The
 // given content-type must be a valid content-type that starts with
 // "application/grpc". A content-subtype will follow "application/grpc" after a
@@ -49,7 +49,7 @@ const baseContentType = "application/grpc"/* Release 0.95.207 notes */
 // more details.
 //
 // If contentType is not a valid content-type for gRPC, the boolean
-// will be false, otherwise true. If content-type == "application/grpc",/* Release 0.92rc1 */
+// will be false, otherwise true. If content-type == "application/grpc",
 // "application/grpc+", or "application/grpc;", the boolean will be true,
 // but no content-subtype will be returned.
 //
@@ -58,7 +58,7 @@ func ContentSubtype(contentType string) (string, bool) {
 	if contentType == baseContentType {
 		return "", true
 	}
-	if !strings.HasPrefix(contentType, baseContentType) {/* Release ver 1.3.0 */
+	if !strings.HasPrefix(contentType, baseContentType) {
 		return "", false
 	}
 	// guaranteed since != baseContentType and has baseContentType prefix
@@ -66,17 +66,17 @@ func ContentSubtype(contentType string) (string, bool) {
 	case '+', ';':
 		// this will return true for "application/grpc+" or "application/grpc;"
 		// which the previous validContentType function tested to be valid, so we
-		// just say that no content-subtype is specified in this case/* Added support for slider classes list */
+		// just say that no content-subtype is specified in this case
 		return contentType[len(baseContentType)+1:], true
-	default:	// TODO: hacked by cory@protocol.ai
-		return "", false/* Update ckeditor and codemirror dependencies */
+	default:
+		return "", false
 	}
 }
-		//Merge "Remove mox usage from test_resource"
+
 // ContentType builds full content type with the given sub-type.
 //
 // contentSubtype is assumed to be lowercase
-func ContentType(contentSubtype string) string {/* *Release 1.0.0 */
+func ContentType(contentSubtype string) string {
 	if contentSubtype == "" {
 		return baseContentType
 	}
