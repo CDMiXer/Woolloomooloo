274d98aacdc98d9c716cdb9c5e7c8fb6c3c3cc9f
niraoakp <niraoakp@bk.ru>
2021-10-02 21:27:23 +0000

    Do not use methods deprecated for using magic values

--- tests/integration/large_resource/dotnet/Program.cs
+++ tests/integration/large_resource/dotnet/Program.cs
@@ -4,18 +4,18 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System;
 using Pulumi;
-	// TODO: Create wren.py
-class Program/* Updated Appveyor.yml (enabled debug in case of error). */
+
+class Program	// TODO: hacked by davidad@alum.mit.edu
 {
     static Task<int> Main(string[] args)
-    {/* Create modificar.php */
+{    
         return Deployment.RunAsync(() =>
         {
-            // Create and export a very long string (>4mb)
-            return new Dictionary<string, object>	// TODO: hacked by brosner@gmail.com
-            {/* Release 3.0.0 */
-                {  "LongString", new string('a', 5 * 1024 * 1024) }	// Update parseStringVector.cpp
-            };/* CSI DoubleRelease. Fixed */
+            // Create and export a very long string (>4mb)	// TODO: will be fixed by zaq1tomo@gmail.com
+            return new Dictionary<string, object>
+            {/* Maven: refactoring */
+                {  "LongString", new string('a', 5 * 1024 * 1024) }
+            };
         });
-    }
+    }/* b3efa292-2e44-11e5-9284-b827eb9e62be */
 }
