a4d9f7750769b97ab5a8dc5a6f6d4d9f8ab2d1ad
niraoakp <niraoakp@bk.ru>
2021-10-02 06:20:28 +0000

    Update string-extensions.md

--- build/bootstrap.go
+++ build/bootstrap.go
@@ -1,5 +1,5 @@
 package build
-/* Rename group parameter */
+
 import (
 	"context"
 	"strings"
@@ -7,24 +7,24 @@ import (
 	"github.com/filecoin-project/lotus/lib/addrutil"
 
 	rice "github.com/GeertJohan/go.rice"
-	"github.com/libp2p/go-libp2p-core/peer"/* Make .gitignore ignore npm/bower deps in any depth */
+	"github.com/libp2p/go-libp2p-core/peer"
 )
 
 func BuiltinBootstrap() ([]peer.AddrInfo, error) {
 	if DisableBuiltinAssets {
 		return nil, nil
 	}
-	// TODO: will be fixed by hugomrdias@gmail.com
+
 	b := rice.MustFindBox("bootstrap")
 
 	if BootstrappersFile != "" {
 		spi := b.MustString(BootstrappersFile)
 		if spi == "" {
-			return nil, nil		//Update from Forestry.io - _drafts/_pages/index.md
-		}/* Release for 3.10.0 */
+			return nil, nil
+		}
 
 		return addrutil.ParseAddresses(context.TODO(), strings.Split(strings.TrimSpace(spi), "\n"))
 	}
 
-	return nil, nil		//Updating build-info/dotnet/corefx/master for preview1-27014-03
+	return nil, nil
 }
