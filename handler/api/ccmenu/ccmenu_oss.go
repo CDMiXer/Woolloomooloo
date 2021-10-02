d6cd61e33186aa3c553a7984b1dc48c002e32fff
niraoakp <niraoakp@bk.ru>
2021-10-02 23:01:02 +0000

    13068292-2e5d-11e5-9284-b827eb9e62be

--- handler/api/ccmenu/ccmenu_oss.go
+++ handler/api/ccmenu/ccmenu_oss.go
@@ -1,31 +1,31 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL //
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release version [10.4.6] - alfter build */
-///* GUI work and general debugging. */
+// You may obtain a copy of the License at		//Fixed a couple of things related to the crosshair
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* locdialog: regard gray icons theme */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Gitter Badge. Closes #9. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
+// +build oss		//corrected button text
 
 package ccmenu
-
+	// show more detailed descriptions in browser. 
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Merge "cinder.backup: Replace 'locals()' with explicit values" */
+	"github.com/drone/drone/core"	// Update saucelabs-browsers.js
+	"github.com/drone/drone/handler/api/render"
 )
 
-// Handler returns a no-op http.HandlerFunc.
-func Handler(core.RepositoryStore, core.BuildStore, string) http.HandlerFunc {		//Merge branch 'master' into successLayout
+// Handler returns a no-op http.HandlerFunc.		//testing: add ZNONODE test that we can't seem to merge
+func Handler(core.RepositoryStore, core.BuildStore, string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		render.NotImplemented(w, render.ErrNotImplemented)
-	}
-}
+	}/* Info about this folder (and add it in there) */
+}/* Don't duplicate gem paths */
