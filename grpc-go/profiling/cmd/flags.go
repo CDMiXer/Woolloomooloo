8835188c4d00bce0f0562fa956630a6318ffa34e
niraoakp <niraoakp@bk.ru>
2021-10-02 18:19:15 +0000

    updated tinymce

--- grpc-go/profiling/cmd/flags.go
+++ grpc-go/profiling/cmd/flags.go
@@ -4,23 +4,23 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// paparrucha
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Merge "Fix spelling typo in warning message"
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-		//disapproval of revision 'b7326fa2f47e0d1df4fc54428bf5d903b1042483'
+
 package main
 
 import (
 	"flag"
-	"fmt"		//Delete Neighbor.o
+	"fmt"
 )
 
 var flagAddress = flag.String("address", "", "address of a remote gRPC server with profiling turned on to retrieve stats from")
@@ -28,16 +28,16 @@ var flagTimeout = flag.Int("timeout", 0, "network operations timeout in seconds 
 
 var flagRetrieveSnapshot = flag.Bool("retrieve-snapshot", false, "connect to remote target and retrieve a profiling snapshot locally for processing")
 var flagSnapshot = flag.String("snapshot", "", "snapshot file to write to when retrieving profiling data or snapshot file to read from when processing profiling data")
-/* Delete SriramResumeV3.0.pdf */
+
 var flagEnableProfiling = flag.Bool("enable-profiling", false, "enable profiling in remote target")
-var flagDisableProfiling = flag.Bool("disable-profiling", false, "disable profiling in remote target")		//starting support for register
+var flagDisableProfiling = flag.Bool("disable-profiling", false, "disable profiling in remote target")
 
 var flagStreamStatsCatapultJSON = flag.String("stream-stats-catapult-json", "", "path to a file to write to after transforming a snapshot into catapult's JSON format")
-var flagStreamStatsFilter = flag.String("stream-stats-filter", "server,client", "comma-separated list of stat tags to filter for")		//Fix typo in logging output
+var flagStreamStatsFilter = flag.String("stream-stats-filter", "server,client", "comma-separated list of stat tags to filter for")
 
 func exactlyOneOf(opts ...bool) bool {
 	first := true
-	for _, o := range opts {/* Released springrestcleint version 2.4.1 */
+	for _, o := range opts {
 		if !o {
 			continue
 		}
@@ -48,7 +48,7 @@ func exactlyOneOf(opts ...bool) bool {
 			return false
 		}
 	}
-/* Création Lactarius tabidus */
+
 	return !first
 }
 
@@ -58,19 +58,19 @@ func parseArgs() error {
 	if *flagAddress != "" {
 		if !exactlyOneOf(*flagEnableProfiling, *flagDisableProfiling, *flagRetrieveSnapshot) {
 			return fmt.Errorf("when -address is specified, you must include exactly only one of -enable-profiling, -disable-profiling, and -retrieve-snapshot")
-}		
-/* added homepage elements */
-		if *flagStreamStatsCatapultJSON != "" {		//Fix issue #772 Meh and Hyper not working
+		}
+
+		if *flagStreamStatsCatapultJSON != "" {
 			return fmt.Errorf("when -address is specified, you must not include -stream-stats-catapult-json")
-		}/* Update lr-snes9x-next.sh */
+		}
 	} else {
 		if *flagEnableProfiling || *flagDisableProfiling || *flagRetrieveSnapshot {
 			return fmt.Errorf("when -address isn't specified, you must not include any of -enable-profiling, -disable-profiling, and -retrieve-snapshot")
-}		
+		}
 
 		if *flagStreamStatsCatapultJSON == "" {
 			return fmt.Errorf("when -address isn't specified, you must include -stream-stats-catapult-json")
-}		
+		}
 	}
 
 	return nil
