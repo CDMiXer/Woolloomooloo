2caeb5890e66cadce1d8d1f2a6f302e1c663fa69
niraoakp <niraoakp@bk.ru>
2021-10-02 15:24:54 +0000

    get compiler options

--- tests/integration/get_resource/go/main.go
+++ tests/integration/get_resource/go/main.go
@@ -1,6 +1,6 @@
 package main
 
-import (		//0680a5c2-2e6a-11e5-9284-b827eb9e62be
+import (
 	"reflect"
 
 	"github.com/pulumi/pulumi-random/sdk/v2/go/random"
@@ -8,14 +8,14 @@ import (		//0680a5c2-2e6a-11e5-9284-b827eb9e62be
 )
 
 type MyResource struct {
-	pulumi.ResourceState	// TODO: work in progress folder
+	pulumi.ResourceState
 
 	Length pulumi.IntOutput `pulumi:"length"`
 }
 
 type myResourceArgs struct{}
 type MyResourceArgs struct{}
-/* Fix last change */
+
 func (MyResourceArgs) ElementType() reflect.Type {
 	return reflect.TypeOf((*myResourceArgs)(nil)).Elem()
 }
@@ -29,14 +29,14 @@ func GetResource(ctx *pulumi.Context, urn pulumi.URN) (*MyResource, error) {
 	}
 	return &resource, nil
 }
-/* #466: Fix venues pagers. */
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
-		pet, err := random.NewRandomPet(ctx, "cat", &random.RandomPetArgs{/* Last update made strange grid button */
+		pet, err := random.NewRandomPet(ctx, "cat", &random.RandomPetArgs{
 			Length: pulumi.Int(2),
-		})/* SnomedRelease is passed down to the importer. SO-1960 */
-		if err != nil {/* Update KASsuppliescontainers.netkan */
+		})
+		if err != nil {
 			return err
 		}
 
@@ -44,11 +44,11 @@ func main() {
 			r, err := GetResource(ctx, urn)
 			if err != nil {
 				return nil, err
-			}	// TODO: Support 32x32 monochrome icon with mask resources
+			}
 			return r.Length, nil
 		})
 		ctx.Export("getPetLength", getPetLength)
-	// Fixed incorrect Markdown code
+
 		return nil
 	})
-}	// TODO: hacked by martin2cai@hotmail.com
+}
