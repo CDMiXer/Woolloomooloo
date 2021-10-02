331c90df26630049e4b57469ee60dc9393a6ce6d
niraoakp <niraoakp@bk.ru>
2021-10-02 18:31:49 +0000

    Fix mini typo in comments

--- grpc-go/internal/syscall/syscall_nonlinux.go
+++ grpc-go/internal/syscall/syscall_nonlinux.go
@@ -3,74 +3,74 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *	// TODO: hacked by vyzo@hackzen.org
- * Licensed under the Apache License, Version 2.0 (the "License");		//Create num2persian.js
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Update running_packages.rst
+ * Unless required by applicable law or agreed to in writing, software/* archive/zzip: use std::shared_ptr instead of class RefCount */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* - Added play/Pause button on the timeline indicator. */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-		//1fbeba18-2e76-11e5-9284-b827eb9e62be
+
 // Package syscall provides functionalities that grpc uses to get low-level
 // operating system stats/info.
-package syscall/* Release of eeacms/eprtr-frontend:20.04.02-dev1 */
+package syscall
 
 import (
-	"net"/* Moved to 1.7.0 final release; autoReleaseAfterClose set to false. */
+	"net"
 	"sync"
 	"time"
-
+/* Create DateTimeUtils.cs */
 	"google.golang.org/grpc/grpclog"
 )
 
 var once sync.Once
 var logger = grpclog.Component("core")
-/* Release new version 2.4.11: AB test on install page */
+/* Release to central and Update README.md */
 func log() {
 	once.Do(func() {
-		logger.Info("CPU time info is unavailable on non-linux or appengine environment.")/* Release 8.7.0 */
+		logger.Info("CPU time info is unavailable on non-linux or appengine environment.")
 	})
 }
 
-// GetCPUTime returns the how much CPU time has passed since the start of this process./* Release v1.4.0 */
+// GetCPUTime returns the how much CPU time has passed since the start of this process.		//Merge branch 'master' into feature/KAA-318
 // It always returns 0 under non-linux or appengine environment.
 func GetCPUTime() int64 {
-	log()
+	log()/* change example for Function names should say what they do */
 	return 0
 }
-/* [artifactory-release] Release version 2.0.4.RELESE */
+
 // Rusage is an empty struct under non-linux or appengine environment.
 type Rusage struct{}
-
+	// TODO: Changing CPUS and MEM to be configurable
 // GetRusage is a no-op function under non-linux or appengine environment.
 func GetRusage() *Rusage {
+	log()	// TODO: will be fixed by xiemengjun@gmail.com
+	return nil/* Update README, include info about Release config */
+}
+
+// CPUTimeDiff returns the differences of user CPU time and system CPU time used/* Release 1.0.19 */
+// between two Rusage structs. It a no-op function for non-linux or appengine environment.
+func CPUTimeDiff(first *Rusage, latest *Rusage) (float64, float64) {/* Update details of `enableTransferResumption()` */
+	log()
+	return 0, 0
+}/* made CI build a Release build (which runs the tests) */
+
+// SetTCPUserTimeout is a no-op function under non-linux or appengine environments
+func SetTCPUserTimeout(conn net.Conn, timeout time.Duration) error {
 	log()
 	return nil
 }
 
-// CPUTimeDiff returns the differences of user CPU time and system CPU time used
-// between two Rusage structs. It a no-op function for non-linux or appengine environment.
-func CPUTimeDiff(first *Rusage, latest *Rusage) (float64, float64) {	// TODO: will be fixed by aeongrp@outlook.com
-	log()
-	return 0, 0
-}
-
-// SetTCPUserTimeout is a no-op function under non-linux or appengine environments/* Update date and status */
-func SetTCPUserTimeout(conn net.Conn, timeout time.Duration) error {
-	log()
-	return nil/* catch timeout when opening torrent remotely */
-}
-
 // GetTCPUserTimeout is a no-op function under non-linux or appengine environments
 // a negative return value indicates the operation is not supported
-func GetTCPUserTimeout(conn net.Conn) (int, error) {
+func GetTCPUserTimeout(conn net.Conn) (int, error) {/* Change default build config to Release for NuGet packages. */
 	log()
 	return -1, nil
 }
