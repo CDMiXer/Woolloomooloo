3cd4906f9d1760e9d620f88be917f417d44b48e8
niraoakp <niraoakp@bk.ru>
2021-10-02 20:00:08 +0000

    Added methods to push a buffer of pixel in one call

--- tests/integration/stack_reference/nodejs/step1/index.ts
+++ tests/integration/stack_reference/nodejs/step1/index.ts
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";		//circularbuffer
+import * as pulumi from "@pulumi/pulumi";
 
 let config = new pulumi.Config();
-let org = config.require("org");/* Added releaseType to SnomedRelease. SO-1960. */
+let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
-		//Update hashtag.rb
-const oldVal: string[] = a.getOutputSync("val");
+/* `exit 1` je dovoljan */
+const oldVal: string[] = a.getOutputSync("val");/* Merge "Releasenote followup: Untyped to default volume type" */
 if (oldVal.length !== 2 || oldVal[0] !== "a" || oldVal[1] !== "b") {
     throw new Error("Invalid result");
 }
