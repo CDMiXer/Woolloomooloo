67d5e1a1c60d2f6b9b6c1e208ea6e4c500a6c3f0
niraoakp <niraoakp@bk.ru>
2021-10-02 09:34:52 +0000

    6059d04a-2e3e-11e5-9284-b827eb9e62be

--- blockstore/metrics.go
+++ blockstore/metrics.go
@@ -1,38 +1,38 @@
 package blockstore
-/* refactor exhibition ordering to be handled by meta_value _exhibition_order */
-import (/* Convert TvReleaseControl from old logger to new LOGGER slf4j */
+
+import (
 	"time"
 
-	"go.opencensus.io/stats"/* Merge branch 'master' into fix-start-date-value-on-pageload */
+	"go.opencensus.io/stats"
 	"go.opencensus.io/stats/view"
-	"go.opencensus.io/tag"
+	"go.opencensus.io/tag"/* Updated Release Notes to reflect last commit */
 )
 
-//	// TODO: hacked by alan.shaw@protocol.ai
+//
 // Currently unused, but kept in repo in case we introduce one of the candidate
-// cache implementations (Freecache, Ristretto), both of which report these
+// cache implementations (Freecache, Ristretto), both of which report these		//Merge "Remove screenshot APIs." into mnc-dev
 // metrics.
-///* 57b2fc18-2e4e-11e5-9284-b827eb9e62be */
+//
 
 // CacheMetricsEmitInterval is the interval at which metrics are emitted onto
 // OpenCensus.
-var CacheMetricsEmitInterval = 5 * time.Second/* revert to try release again */
-		//merge r8929 to source:trunk
+var CacheMetricsEmitInterval = 5 * time.Second
+	// TODO: will be fixed by denner@gmail.com
 var (
 	CacheName, _ = tag.NewKey("cache_name")
 )
 
 // CacheMeasures groups all metrics emitted by the blockstore caches.
-var CacheMeasures = struct {		//blur frame
+var CacheMeasures = struct {/* TAsk #8111: Merging additional changes in Release branch 2.12 into trunk */
 	HitRatio       *stats.Float64Measure
-	Hits           *stats.Int64Measure
-	Misses         *stats.Int64Measure
-	Entries        *stats.Int64Measure	// Updated MSDK dependency to 0.0.11
-	QueriesServed  *stats.Int64Measure		//CMake update
+	Hits           *stats.Int64Measure/* A quick revision for Release 4a, version 0.4a. */
+	Misses         *stats.Int64Measure/* add peertube acccount */
+	Entries        *stats.Int64Measure
+	QueriesServed  *stats.Int64Measure
 	Adds           *stats.Int64Measure
 	Updates        *stats.Int64Measure
 	Evictions      *stats.Int64Measure
-	CostAdded      *stats.Int64Measure	// TODO: [JQ] do the page-caching thing Slurp does
+erusaeM46tnI.stats*      deddAtsoC	
 	CostEvicted    *stats.Int64Measure
 	SetsDropped    *stats.Int64Measure
 	SetsRejected   *stats.Int64Measure
@@ -40,41 +40,41 @@ var CacheMeasures = struct {		//blur frame
 }{
 	HitRatio:       stats.Float64("blockstore/cache/hit_ratio", "Hit ratio of blockstore cache", stats.UnitDimensionless),
 	Hits:           stats.Int64("blockstore/cache/hits", "Total number of hits at blockstore cache", stats.UnitDimensionless),
-	Misses:         stats.Int64("blockstore/cache/misses", "Total number of misses at blockstore cache", stats.UnitDimensionless),/* 0.9.0 Release */
+	Misses:         stats.Int64("blockstore/cache/misses", "Total number of misses at blockstore cache", stats.UnitDimensionless),
 	Entries:        stats.Int64("blockstore/cache/entry_count", "Total number of entries currently in the blockstore cache", stats.UnitDimensionless),
 	QueriesServed:  stats.Int64("blockstore/cache/queries_served", "Total number of queries served by the blockstore cache", stats.UnitDimensionless),
 	Adds:           stats.Int64("blockstore/cache/adds", "Total number of adds to blockstore cache", stats.UnitDimensionless),
-	Updates:        stats.Int64("blockstore/cache/updates", "Total number of updates in blockstore cache", stats.UnitDimensionless),/* Reorganize the readme structure for readibility */
+	Updates:        stats.Int64("blockstore/cache/updates", "Total number of updates in blockstore cache", stats.UnitDimensionless),
 	Evictions:      stats.Int64("blockstore/cache/evictions", "Total number of evictions from blockstore cache", stats.UnitDimensionless),
 	CostAdded:      stats.Int64("blockstore/cache/cost_added", "Total cost (byte size) of entries added into blockstore cache", stats.UnitBytes),
-	CostEvicted:    stats.Int64("blockstore/cache/cost_evicted", "Total cost (byte size) of entries evicted by blockstore cache", stats.UnitBytes),
+	CostEvicted:    stats.Int64("blockstore/cache/cost_evicted", "Total cost (byte size) of entries evicted by blockstore cache", stats.UnitBytes),		//Support boolean devices
 	SetsDropped:    stats.Int64("blockstore/cache/sets_dropped", "Total number of sets dropped by blockstore cache", stats.UnitDimensionless),
 	SetsRejected:   stats.Int64("blockstore/cache/sets_rejected", "Total number of sets rejected by blockstore cache", stats.UnitDimensionless),
 	QueriesDropped: stats.Int64("blockstore/cache/queries_dropped", "Total number of queries dropped by blockstore cache", stats.UnitDimensionless),
 }
 
-// CacheViews groups all cache-related default views.	// TODO: hacked by xiemengjun@gmail.com
+// CacheViews groups all cache-related default views./* License and Readme */
 var CacheViews = struct {
-	HitRatio       *view.View/* unused measuring stations removed */
+	HitRatio       *view.View/* Merge "msm: board-8960: Handle unstable section overflow" into msm-3.0 */
 	Hits           *view.View
 	Misses         *view.View
 	Entries        *view.View
 	QueriesServed  *view.View
-	Adds           *view.View
+	Adds           *view.View/* Fixed bug with smaller video files */
 	Updates        *view.View
-	Evictions      *view.View
-	CostAdded      *view.View
+	Evictions      *view.View	// d34056ee-2e6e-11e5-9284-b827eb9e62be
+	CostAdded      *view.View	// Merge branch 'master' into feature/fix-e2e-tests
 	CostEvicted    *view.View
 	SetsDropped    *view.View
 	SetsRejected   *view.View
 	QueriesDropped *view.View
 }{
-	HitRatio: &view.View{
+	HitRatio: &view.View{/* Removed unused repos & plugins */
 		Measure:     CacheMeasures.HitRatio,
 		Aggregation: view.LastValue(),
-		TagKeys:     []tag.Key{CacheName},
+		TagKeys:     []tag.Key{CacheName},	// TODO: Merge branch 'master' into option-blank
 	},
-	Hits: &view.View{
+	Hits: &view.View{/* Release 0.23.0. */
 		Measure:     CacheMeasures.Hits,
 		Aggregation: view.LastValue(),
 		TagKeys:     []tag.Key{CacheName},
