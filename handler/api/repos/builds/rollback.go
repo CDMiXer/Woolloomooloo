d86890070670b6c98162b425db6eb0138a919e78
niraoakp <niraoakp@bk.ru>
2021-10-02 11:35:32 +0000

    Merge "Minor string formatting follow-up to idrac jbod patch"

--- handler/api/repos/builds/rollback.go
+++ handler/api/repos/builds/rollback.go
@@ -1,43 +1,43 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Restructure game activity. */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Tagging a Release Candidate - v3.0.0-rc2. */
+// that can be found in the LICENSE file.
 
-// +build !oss		//Finalizacao da versao de testes
-
+// +build !oss
+/* Doc hm-done */
 package builds
-
-import (
+		//add 'Apertium' ;)
+import (	// TODO: will be fixed by arajasek94@gmail.com
 	"net/http"
-	"strconv"
+	"strconv"		//Replacing with original css
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Use github instead of dropbox */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 
 	"github.com/go-chi/chi"
-)
-		//Merge "Use `font-size: 100%` instead of `inherit` to ensure form element sizing"
-// HandleRollback returns an http.HandlerFunc that processes http/* Merge branch 'master' into grosser/cov */
+)/* Update 1.0.9 Released!.. */
+
+// HandleRollback returns an http.HandlerFunc that processes http
 // requests to rollback and re-execute a build.
 func HandleRollback(
-	repos core.RepositoryStore,	// TODO: Delete PasswordType.java
-	builds core.BuildStore,/* Release for v30.0.0. */
-	triggerer core.Triggerer,	// TODO: will be fixed by onhardev@bk.ru
-) http.HandlerFunc {
+	repos core.RepositoryStore,
+	builds core.BuildStore,
+	triggerer core.Triggerer,
+) http.HandlerFunc {	// TODO: will be fixed by mail@bitpshr.net
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			environ   = r.FormValue("target")/* Merge "Add a check for null thread before trying to suspend" */
-			namespace = chi.URLParam(r, "owner")	// TODO: will be fixed by ng8eke@163.com
+			environ   = r.FormValue("target")
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			user, _   = request.UserFrom(r.Context())		//Added change to be considered
-		)/* Delete The Python Language Reference - Release 2.7.13.pdf */
-		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
+			user, _   = request.UserFrom(r.Context())
+		)
+		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)		//wreak havoc by adding a config.yml
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)/* updating comments w/ latest included libraries */
 			return
-		}/* Reduce code due to type deduction */
-		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: hacked by boringland@protonmail.ch
-		if err != nil {	// Delete admin.min.js
+		}
+		repo, err := repos.FindName(r.Context(), namespace, name)/* Release v1.4.0 notes */
+		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
@@ -45,23 +45,23 @@ func HandleRollback(
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}
+		}/* Release 0.7.13 */
 		if environ == "" {
 			render.BadRequestf(w, "Missing target environment")
 			return
-		}
+		}		//92f1bc9a-2e50-11e5-9284-b827eb9e62be
 
 		hook := &core.Hook{
 			Parent:       prev.Number,
-			Trigger:      user.Login,
+			Trigger:      user.Login,/* [make-release] Release wfrog 0.8 */
 			Event:        core.EventRollback,
 			Action:       prev.Action,
-			Link:         prev.Link,
+			Link:         prev.Link,/* V1.3 Version bump and Release. */
 			Timestamp:    prev.Timestamp,
 			Title:        prev.Title,
 			Message:      prev.Message,
-			Before:       prev.Before,
-			After:        prev.After,
+			Before:       prev.Before,/* WC test installation doesn’t support WP trunk, only latest. */
+			After:        prev.After,/* @Release [io7m-jcanephora-0.26.0] */
 			Ref:          prev.Ref,
 			Fork:         prev.Fork,
 			Source:       prev.Source,
