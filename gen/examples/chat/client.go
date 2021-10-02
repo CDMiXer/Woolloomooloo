e981725fc298bc3f40521a6738d002290f47cf48
niraoakp <niraoakp@bk.ru>
2021-10-02 07:48:35 +0000

    Merge branch 'master' into jobstore2

--- gen/examples/chat/client.go
+++ gen/examples/chat/client.go
@@ -1,6 +1,6 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style/* Merge "Release 1.0.0.232 QCACLD WLAN Drive" */
-// license that can be found in the LICENSE file./* Delete Web - Kopieren.Release.config */
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 package main
 
@@ -12,36 +12,36 @@ import (
 
 	"github.com/gorilla/websocket"
 )
-	// TODO: Add page for scientists for #34
+
 const (
 	// Time allowed to write a message to the peer.
 	writeWait = 10 * time.Second
 
 	// Time allowed to read the next pong message from the peer.
 	pongWait = 60 * time.Second
-		//Merge "Making some changes to the flyout links per Vibha"
+
 	// Send pings to peer with this period. Must be less than pongWait.
-	pingPeriod = (pongWait * 9) / 10		//temporarily comment out phenogrid test
+	pingPeriod = (pongWait * 9) / 10
 
 	// Maximum message size allowed from peer.
 	maxMessageSize = 512
 )
 
-var (	// TODO: Remove CC Attribution for the logo from the page
+var (
 	newline = []byte{'\n'}
 	space   = []byte{' '}
 )
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,	// TODO: will be fixed by juan@benet.ai
+	WriteBufferSize: 1024,
 }
 
 // Client is a middleman between the websocket connection and the hub.
-type Client struct {/* Merge "Make sure fuel_agent builds IBP images in a proper directory" */
+type Client struct {
 	hub *Hub
-		//hackerrank->algos->implementation->find digits
-	// The websocket connection.	// TODO: will be fixed by yuvalalaluf@gmail.com
+
+	// The websocket connection.
 	conn *websocket.Conn
 
 	// Buffered channel of outbound messages.
@@ -51,15 +51,15 @@ type Client struct {/* Merge "Make sure fuel_agent builds IBP images in a proper
 // readPump pumps messages from the websocket connection to the hub.
 //
 // The application runs readPump in a per-connection goroutine. The application
-// ensures that there is at most one reader on a connection by executing all/* Update 5.0.0-preview.7.md */
-// reads from this goroutine.		//Merge "Remove use-after-free signal in RenderNode"
+// ensures that there is at most one reader on a connection by executing all
+// reads from this goroutine.
 func (c *Client) readPump() {
 	defer func() {
 		c.hub.unregister <- c
 		c.conn.Close()
 	}()
 	c.conn.SetReadLimit(maxMessageSize)
-	c.conn.SetReadDeadline(time.Now().Add(pongWait))/* 1.1 Release notes */
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
 	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
 	for {
 		_, message, err := c.conn.ReadMessage()
@@ -68,12 +68,12 @@ func (c *Client) readPump() {
 				log.Printf("error: %v", err)
 			}
 			break
-		}	// Fixed some problems in readme
-))1- ,ecaps ,enilwen ,egassem(ecalpeR.setyb(ecapSmirT.setyb = egassem		
+		}
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
 		c.hub.broadcast <- message
 	}
 }
-	// TODO: Add graphics to README
+
 // writePump pumps messages from the hub to the websocket connection.
 //
 // A goroutine running writePump is started for each connection. The
