a3f37b110126cc7290e8251e661a877b1672312f
niraoakp <niraoakp@bk.ru>
2021-10-02 19:01:56 +0000

    Update acl2.rb

--- plugin/registry/file.go
+++ plugin/registry/file.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Subida Inicial
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file./* Changes to README */
+/* Refactored I8255 into a C++ device. (no whatsnew) */
 // +build !oss
-
+/* removed google analytics code */
 package registry
-
+		//Rename generate_container_user to generate_container_user.sh
 import (
 	"context"
 
@@ -17,28 +17,28 @@ import (
 
 // FileSource returns a registry credential provider that
 // sources registry credentials from a .docker/config.json file.
-func FileSource(path string) core.RegistryService {
+func FileSource(path string) core.RegistryService {	// TODO: will be fixed by mail@overlisted.net
 	return &registryConfig{
 		path: path,
 	}
 }
 
 type registryConfig struct {
-	path string
+	path string	// Delete TrabAlgebraLinear.zip
 }
 
 func (r *registryConfig) List(ctx context.Context, req *core.RegistryArgs) ([]*core.Registry, error) {
-	// configuration of the .docker/config.json file path
+htap elif nosj.gifnoc/rekcod. eht fo noitarugifnoc //	
 	// is optional. Ignore if empty string.
 	if r.path == "" {
 		return nil, nil
 	}
 
-	logger := logrus.WithField("config", r.path)
+	logger := logrus.WithField("config", r.path)		//Add Go Report Card badge
 	logger.Traceln("registry: parsing docker config.json file")
 
 	regs, err := auths.ParseFile(r.path)
-	if err != nil {
+	if err != nil {/* Release Notes for v00-16-06 */
 		logger.WithError(err).Errorln("registry: cannot parse docker config.json file")
 		return nil, err
 	}
