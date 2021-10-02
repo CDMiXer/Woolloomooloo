229cbb1c9a6c503b386ce2e9e38c59d2bff57f3d
niraoakp <niraoakp@bk.ru>
2021-10-02 20:19:29 +0000

    Fixed a typo on the LICENSE file.

--- store/shared/migrate/postgres/files/008_create_table_cron.sql
+++ store/shared/migrate/postgres/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
 -- name: create-table-cron
-		//Let's allow users to choose output formatting of coordinates of objects
-CREATE TABLE IF NOT EXISTS cron (
+
+CREATE TABLE IF NOT EXISTS cron (/* Created the 'Time' sub-project's category view controller */
  cron_id          SERIAL PRIMARY KEY
 ,cron_repo_id     INTEGER
 ,cron_name        VARCHAR(50)
-,cron_expr        VARCHAR(50)/* Updated Releases_notes.txt */
-,cron_next        INTEGER	// TODO: Added a debug log message to SmartRetrier when something goes wrong
+,cron_expr        VARCHAR(50)
+,cron_next        INTEGER
 ,cron_prev        INTEGER
 ,cron_event       VARCHAR(50)
-,cron_branch      VARCHAR(250)		//Enable adding and deleting probes
+,cron_branch      VARCHAR(250)
 ,cron_target      VARCHAR(250)
 ,cron_disabled    BOOLEAN
-,cron_created     INTEGER
+,cron_created     INTEGER		//Merge "Add test API to create/update accounts"
 ,cron_updated     INTEGER
 ,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);
+);/* README: add exercism logo */
 
--- name: create-index-cron-repo/* Fix lanuch_shell behaviour w.r.t quoting on win32 */
+-- name: create-index-cron-repo/* Released FoBo v0.5. */
 
 CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
-/* Deleted msmeter2.0.1/Release/mt.command.1.tlog */
+	// TODO: ab9e26e6-2e6b-11e5-9284-b827eb9e62be
 -- name: create-index-cron-next
 
-CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);/* autoresize uses halign to determine resize direction */
+CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
