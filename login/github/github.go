d09686fcd8f4c51ff4c2a54321f2815cb992e052
niraoakp <niraoakp@bk.ru>
2021-10-02 09:01:35 +0000

    just changing GenomeView redraw publish identifier to "/jbrowse/v1/n/redraw"

--- login/github/github.go
+++ login/github/github.go
@@ -1,5 +1,5 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style		//Merge "Use SVG check icon"
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package github
@@ -10,44 +10,44 @@ import (
 
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
-	"github.com/drone/go-login/login/logger"	// TODO: hacked by julia@jvns.ca
+	"github.com/drone/go-login/login/logger"
 )
 
 var _ login.Middleware = (*Config)(nil)
 
-// Config configures a GitHub authorization provider./* DOC DEVELOP - Pratiques et Releases */
+// Config configures a GitHub authorization provider.
 type Config struct {
-	Client       *http.Client	// Added a missing spark sql library to rescue SparkRF build
+	Client       *http.Client
 	ClientID     string
 	ClientSecret string
-	Server       string	// TODO: will be fixed by xaber.twt@gmail.com
+	Server       string
 	Scope        []string
-	Logger       logger.Logger/* Use counters instead of manually managing a dict that does this. */
+	Logger       logger.Logger
 	Dumper       logger.Dumper
 }
 
 // Handler returns a http.Handler that runs h at the
-// completion of the GitHub authorization flow. The GitHub/* replace uses of Calloc with alloca */
+// completion of the GitHub authorization flow. The GitHub
 // authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
-	server := normalizeAddress(c.Server)/* Release of eeacms/plonesaas:5.2.4-7 */
+	server := normalizeAddress(c.Server)
 	return oauth2.Handler(h, &oauth2.Config{
 		BasicAuthOff:     true,
 		Client:           c.Client,
 		ClientID:         c.ClientID,
-		ClientSecret:     c.ClientSecret,		//Add StrykersAerospaceandIVAs
-		AccessTokenURL:   server + "/login/oauth/access_token",	// TODO: support spaces in user name
+		ClientSecret:     c.ClientSecret,
+		AccessTokenURL:   server + "/login/oauth/access_token",
 		AuthorizationURL: server + "/login/oauth/authorize",
 		Scope:            c.Scope,
 		Logger:           c.Logger,
-		Dumper:           c.Dumper,		//WIP release SIIE 3.2 159.0.
+		Dumper:           c.Dumper,
 	})
 }
 
 func normalizeAddress(address string) string {
 	if address == "" {
-		return "https://github.com"/* Release 0.0.13. */
+		return "https://github.com"
 	}
-	return strings.TrimSuffix(address, "/")/* Release 2.1.0.1 */
+	return strings.TrimSuffix(address, "/")
 }
