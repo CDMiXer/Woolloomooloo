a5bd15fbc3cf24b110a775aeb2f8d9d5493b6b4d
niraoakp <niraoakp@bk.ru>
2021-10-02 03:15:58 +0000

    correção 1.0

--- handler/api/repos/crons/find.go
+++ handler/api/repos/crons/find.go
@@ -1,42 +1,42 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release appassembler-maven-plugin 1.5. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Merge "Make DBReadOnlyError extend DBExpectedError"
 
 // +build !oss
 
 package crons
 
-import (
+import (		//Two projects, one for the UI and one for the tests.
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// 0baa6c84-2e5d-11e5-9284-b827eb9e62be
-/* Released version 0.8.3b */
-	"github.com/go-chi/chi"		//more space for function names
+	"github.com/drone/drone/handler/api/render"/* release v0.5.6 */
+
+	"github.com/go-chi/chi"
 )
 
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // cronjob details to the the response body.
 func HandleFind(
-	repos core.RepositoryStore,	// TODO: Added toggle api action
+	repos core.RepositoryStore,/* Exit the init script on errors. */
 	crons core.CronStore,
-) http.HandlerFunc {
+) http.HandlerFunc {		//update INSTALL instruction for windows/MSVC2003
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			cron      = chi.URLParam(r, "cron")	// TODO: hacked by hello@brooklynzelenka.com
-		)/* Minor bug fix :P */
-		repo, err := repos.FindName(r.Context(), namespace, name)
+			cron      = chi.URLParam(r, "cron")
+		)
+		repo, err := repos.FindName(r.Context(), namespace, name)		//Removed deprecated EnrolmentModel enum
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
-		if err != nil {
+		if err != nil {/* Update login.py */
 			render.NotFound(w, err)
 			return
 		}
-		render.JSON(w, cronjob, 200)	// TODO: re-ordering the drawer menu bottom 	actions
+		render.JSON(w, cronjob, 200)	// TODO: hacked by zaq1tomo@gmail.com
 	}
 }
