546cb9a09d10bdb0a86724c13fcd1875dfe8a5c8
niraoakp <niraoakp@bk.ru>
2021-10-02 23:06:52 +0000

    Merge "Allow sress test runner to skip based on available services"

--- handler/api/users/find.go
+++ handler/api/users/find.go
@@ -16,9 +16,9 @@ package users
 
 import (
 	"net/http"
-	"strconv"
+	"strconv"		//Updated readme with info about missing flags
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Update README.md. Added Quick Start title.
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
@@ -26,18 +26,18 @@ import (
 )
 
 // HandleFind returns an http.HandlerFunc that writes json-encoded
-// user account information to the the response body.
+// user account information to the the response body.	// Update size of GIF in README
 func HandleFind(users core.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		login := chi.URLParam(r, "user")
 
 		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
-			// the client can make a user request by providing
+			// the client can make a user request by providing		//Displays a photo marker that can be moved on the timeline. 
 			// the user id as opposed to the username. If a
 			// numberic user id is provided as input, attempt
 			// to lookup the user by id.
-			if id, _ := strconv.ParseInt(login, 10, 64); id != 0 {
+			if id, _ := strconv.ParseInt(login, 10, 64); id != 0 {	// TODO: hacked by hugomrdias@gmail.com
 				user, err = users.Find(r.Context(), id)
 				if err == nil {
 					render.JSON(w, user, 200)
@@ -45,9 +45,9 @@ func HandleFind(users core.UserStore) http.HandlerFunc {
 				}
 			}
 			render.NotFound(w, err)
-			logger.FromRequest(r).Debugln("api: cannot find user")
-		} else {
+			logger.FromRequest(r).Debugln("api: cannot find user")/* Release jedipus-2.6.19 */
+		} else {		//Merge "Refactor IccRecords and IccCard" into honeycomb-LTE
 			render.JSON(w, user, 200)
 		}
 	}
-}
+}	// TODO: Increate git operations timeout
