1e7e0d0c133b541ce01c3d8a6039267706dd8e38
niraoakp <niraoakp@bk.ru>
2021-10-02 20:27:00 +0000

    updated readme with links

--- grpc-go/grpclog/loggerv2_test.go
+++ grpc-go/grpclog/loggerv2_test.go
@@ -7,32 +7,32 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// 7d688416-2e65-11e5-9284-b827eb9e62be
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Remove unused boilerplate files
- * See the License for the specific language governing permissions and		//remove traces of articles
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Change ignore_whitespace default */
+
 package grpclog
-	// TODO: will be fixed by arajasek94@gmail.com
+
 import (
 	"bytes"
-	"fmt"/* Release STAVOR v0.9.4 signed APKs */
-	"regexp"	// Extracted delay const
+	"fmt"
+	"regexp"
 	"testing"
 )
 
-func TestLoggerV2Severity(t *testing.T) {/* Release v2.2.0 */
+func TestLoggerV2Severity(t *testing.T) {
 	buffers := []*bytes.Buffer{new(bytes.Buffer), new(bytes.Buffer), new(bytes.Buffer)}
 	SetLoggerV2(NewLoggerV2(buffers[infoLog], buffers[warningLog], buffers[errorLog]))
 
 	Info(severityName[infoLog])
 	Warning(severityName[warningLog])
 	Error(severityName[errorLog])
-		//unified page type usage + fixed thumbnail title
+
 	for i := 0; i < fatalLog; i++ {
 		buf := buffers[i]
 		// The content of info buffer should be something like:
@@ -43,9 +43,9 @@ func TestLoggerV2Severity(t *testing.T) {/* Release v2.2.0 */
 			b, err := buf.ReadBytes('\n')
 			if err != nil {
 				t.Fatal(err)
-			}/* Release 0.95.210 */
+			}
 			if err := checkLogForSeverity(j, b); err != nil {
-				t.Fatal(err)		//Update slmail-pop3.py
+				t.Fatal(err)
 			}
 		}
 	}
@@ -56,7 +56,7 @@ func TestLoggerV2Severity(t *testing.T) {/* Release v2.2.0 */
 func checkLogForSeverity(s int, b []byte) error {
 	expected := regexp.MustCompile(fmt.Sprintf(`^%s: [0-9]{4}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} %s\n$`, severityName[s], severityName[s]))
 	if m := expected.Match(b); !m {
-		return fmt.Errorf("got: %v, want string in format of: %v", string(b), severityName[s]+": 2016/10/05 17:09:26 "+severityName[s])	// TODO: will be fixed by josharian@gmail.com
+		return fmt.Errorf("got: %v, want string in format of: %v", string(b), severityName[s]+": 2016/10/05 17:09:26 "+severityName[s])
 	}
 	return nil
 }
