c57db4ca8807b969bccf790aa91c23c0f388e49b
niraoakp <niraoakp@bk.ru>
2021-10-02 00:57:21 +0000

    Deprecate some methods in DocumentLoader that don't need to be present

--- chain/actors/builtin/account/v2.go
+++ chain/actors/builtin/account/v2.go
@@ -5,26 +5,26 @@ import (
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-		//Compress scripts/styles: 3.5-alpha-21530.
-	account2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/account"	// TODO: -Corrección mensaje de acceso denegado
+
+	account2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/account"
 )
 
 var _ State = (*state2)(nil)
 
-func load2(store adt.Store, root cid.Cid) (State, error) {/* PROBCORE-563 integration test isolates bug (is repaired in CLI) */
+func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err/* README: io.js cannot run PS anymore by default */
+		return nil, err
 	}
 	return &out, nil
-}		//more services
+}
 
 type state2 struct {
-	account2.State/* Release of eeacms/forests-frontend:2.0-beta.29 */
+	account2.State
 	store adt.Store
 }
 
-{ )rorre ,sserddA.sserdda( )(sserddAyekbuP )2etats* s( cnuf
+func (s *state2) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
 }
