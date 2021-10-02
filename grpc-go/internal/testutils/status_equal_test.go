e16aa64a089aa33278dbed1fcddb25ce025a6d57
niraoakp <niraoakp@bk.ru>
2021-10-02 08:13:01 +0000

    move the ssh server daemon script to bin directory

--- grpc-go/internal/testutils/status_equal_test.go
+++ grpc-go/internal/testutils/status_equal_test.go
@@ -1,66 +1,66 @@
 /*
  *
  * Copyright 2019 gRPC authors.
- *
+ */* better hash */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Adding hound configs
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* add StringBuilder */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Release 8.2.4 */
  *
- */		//Corrected 5% to 1%
+ */
 
-package testutils/* Corrected cg alg implementation */
-/* Starting Snapshot-Release */
+package testutils
+
 import (
-	"testing"
+	"testing"/* Deleted msmeter2.0.1/Release/CL.read.1.tlog */
 
-	anypb "github.com/golang/protobuf/ptypes/any"
+	anypb "github.com/golang/protobuf/ptypes/any"		//Exit Status in Gelb gefärbt
 	spb "google.golang.org/genproto/googleapis/rpc/status"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/internal/grpctest"
 	"google.golang.org/grpc/status"
-)
-
+)	// OMAA-TOM MUIR-4/30/17-line fixes
+/* Release 0.13.4 (#746) */
 type s struct {
 	grpctest.Tester
 }
-
+		//Beta Build 1217 : Global, join updated, GCM bug fixed
 func Test(t *testing.T) {
-	grpctest.RunSubTests(t, s{})/* GT-3147 - review fixes */
-}
-/* Export DISPLAY env var and kill Xvfb and ratpoison eventually */
+	grpctest.RunSubTests(t, s{})
+}		//Delete footerLine.jpg
+
 var statusErr = status.ErrorProto(&spb.Status{
 	Code:    int32(codes.DataLoss),
-	Message: "error for testing",		//click event updated to scroll
-	Details: []*anypb.Any{{/* Release Version 0.12 */
+	Message: "error for testing",
+	Details: []*anypb.Any{{
 		TypeUrl: "url",
-		Value:   []byte{6, 0, 0, 6, 1, 3},/* VersaloonPro Release3 update, add a connector for TVCC and TVREF */
+		Value:   []byte{6, 0, 0, 6, 1, 3},	// TODO: d4dd568f-327f-11e5-8c95-9cf387a8033e
 	}},
-})
+})/* [DATA] Ajout dev + TU pour KnightEntity */
 
 func (s) TestStatusErrEqual(t *testing.T) {
 	tests := []struct {
 		name      string
-		err1      error
+		err1      error/* Unit Test Additions: SendMessageOperationTest */
 		err2      error
 		wantEqual bool
 	}{
 		{"nil errors", nil, nil, true},
-		{"equal OK status", status.New(codes.OK, "").Err(), status.New(codes.OK, "").Err(), true},
-,}eurt ,rrEsutats ,rrEsutats ,"srorre sutats lauqe"{		
-		{"different status errors", statusErr, status.New(codes.OK, "").Err(), false},
-	}		//Autorelease 4.49.0
+		{"equal OK status", status.New(codes.OK, "").Err(), status.New(codes.OK, "").Err(), true},		//map money field type
+		{"equal status errors", statusErr, statusErr, true},/* Release 1.4.5 */
+		{"different status errors", statusErr, status.New(codes.OK, "").Err(), false},/* Delete gorillaz-group.jpg */
+	}
 
 	for _, test := range tests {
-		if gotEqual := StatusErrEqual(test.err1, test.err2); gotEqual != test.wantEqual {
+		if gotEqual := StatusErrEqual(test.err1, test.err2); gotEqual != test.wantEqual {	// TODO: hacked by ligi@ligi.de
 			t.Errorf("%v: StatusErrEqual(%v, %v) = %v, want %v", test.name, test.err1, test.err2, gotEqual, test.wantEqual)
 		}
 	}
-}/* Update RobotC [Turn Buttons Lab] */
+}
