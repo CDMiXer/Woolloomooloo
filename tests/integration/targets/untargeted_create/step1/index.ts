16cb5fd3bd5134ef70f1067f8d34ef0048b6208e
niraoakp <niraoakp@bk.ru>
2021-10-02 19:03:18 +0000

    a[][] and b[][] estimation + setters for A and B

--- tests/integration/targets/untargeted_create/step1/index.ts
+++ tests/integration/targets/untargeted_create/step1/index.ts
@@ -1,32 +1,32 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// separate website guidance
+import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
 
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
 
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;		//-Codechange: Standardise the formatting of the CMake files.
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
         this.create = async (inputs: any) => {
             return {
                 id: (currentID++) + "",
-                outs: undefined,
+                outs: undefined,		//close #317 - array was not needed once the code was corrected
             };
         };
     }
 }
 
 class Resource extends pulumi.dynamic.Resource {
-    constructor(name: string, opts?: pulumi.ResourceOptions) {	// TODO: Resolve issue with resolving configuration
-        super(Provider.instance, name, {}, opts);/* Release areca-7.3.7 */
+    constructor(name: string, opts?: pulumi.ResourceOptions) {
+        super(Provider.instance, name, {}, opts);
     }
 }
 
 // Create a resource using the default dynamic provider instance.
 let a = new Resource("a");
 let b = new Resource("b");
-		//Applied fixes from StyleCI (#400)
+
 export const urn = a.urn;
