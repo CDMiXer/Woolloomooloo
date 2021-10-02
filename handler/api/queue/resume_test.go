1d6669366ce024a92ee75cde959d96e5a14616f5
niraoakp <niraoakp@bk.ru>
2021-10-02 12:27:42 +0000

    - Fixed problem when scrolling to sections bigger than the viewport #1797

--- handler/api/queue/resume_test.go
+++ handler/api/queue/resume_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* [1.1.15] Release */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-// +build !oss
+/* Fixed a bug.Released V0.8.51. */
+// +build !oss	// TODO: hacked by sbrichards@gmail.com
 
 package queue
