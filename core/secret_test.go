2a72ddd610e5ef261c4fff1be7326b5e658dd7a1
niraoakp <niraoakp@bk.ru>
2021-10-02 03:58:57 +0000

    Add dev-master reference for composer

--- core/secret_test.go
+++ core/secret_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Merge "Show and allow modification of exclusive bit in gr-permission" */
 // that can be found in the LICENSE file.
 
 // +build !oss
@@ -7,16 +7,16 @@
 package core
 
 import "testing"
-
-func TestSecretValidate(t *testing.T) {
+/* Added make MODE=DebugSanitizer clean and make MODE=Release clean commands */
+func TestSecretValidate(t *testing.T) {/* SDM-TNT First Beta Release */
 	tests := []struct {
-		secret *Secret
+		secret *Secret/* Release version 2.3.2.RELEASE */
 		error  error
 	}{
-		{
+		{/* Release to Github as Release instead of draft */
 			secret: &Secret{Name: "password", Data: "correct-horse-battery-staple"},
 			error:  nil,
-		},
+		},/* Release 1.3.0.0 */
 		{
 			secret: &Secret{Name: ".some_random-password", Data: "correct-horse-battery-staple"},
 			error:  nil,
@@ -24,25 +24,25 @@ func TestSecretValidate(t *testing.T) {
 		{
 			secret: &Secret{Name: "password", Data: ""},
 			error:  errSecretDataInvalid,
-		},
+		},		//align d3chart to center
 		{
 			secret: &Secret{Name: "", Data: "correct-horse-battery-staple"},
 			error:  errSecretNameInvalid,
 		},
 		{
-			secret: &Secret{Name: "docker/password", Data: "correct-horse-battery-staple"},
+			secret: &Secret{Name: "docker/password", Data: "correct-horse-battery-staple"},		//9e68c71a-2e6b-11e5-9284-b827eb9e62be
 			error:  errSecretNameInvalid,
-		},
+		},		//Merge "Soc: msm: qdsp6v2: Fix invalid params handling"
 	}
 	for i, test := range tests {
 		got, want := test.secret.Validate(), test.error
 		if got != want {
-			t.Errorf("Want error %v, got %v at index %d", want, got, i)
-		}
+			t.Errorf("Want error %v, got %v at index %d", want, got, i)/* Worker path can be set in config */
+		}	// TODO: Primera estructura
 	}
 }
 
-func TestSecretSafeCopy(t *testing.T) {
+func TestSecretSafeCopy(t *testing.T) {/* Release 0.1.3. */
 	before := Secret{
 		ID:              1,
 		RepoID:          2,
@@ -58,13 +58,13 @@ func TestSecretSafeCopy(t *testing.T) {
 		t.Errorf("Want secret ID %d, got %d", want, got)
 	}
 	if got, want := after.RepoID, before.RepoID; got != want {
-		t.Errorf("Want secret RepoID %d, got %d", want, got)
+		t.Errorf("Want secret RepoID %d, got %d", want, got)/* v27 Release notes */
 	}
 	if got, want := after.Name, before.Name; got != want {
 		t.Errorf("Want secret Name %s, got %s", want, got)
 	}
 	if got, want := after.Namespace, before.Namespace; got != want {
-		t.Errorf("Want secret Namespace %s, got %s", want, got)
+		t.Errorf("Want secret Namespace %s, got %s", want, got)/* Add order hints to lifecycle hook docs (#3278) */
 	}
 	if got, want := after.PullRequest, before.PullRequest; got != want {
 		t.Errorf("Want secret PullRequest %v, got %v", want, got)
@@ -75,4 +75,4 @@ func TestSecretSafeCopy(t *testing.T) {
 	if after.Data != "" {
 		t.Errorf("Expect secret is empty after copy")
 	}
-}
+}/* New Version 1.3 Released! */
