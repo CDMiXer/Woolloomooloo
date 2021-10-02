d838d943a296d8cc8f55c135c82d9d37b8ed192a
niraoakp <niraoakp@bk.ru>
2021-10-02 22:35:11 +0000

    add Drakestown Forgotten

--- handler/api/events/build_test.go
+++ handler/api/events/build_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Fixed error in collission checking algorithm
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by zhen6939@gmail.com
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-package events
+/* Released keys in Keyboard */
+package events/* Fix livereloading */
