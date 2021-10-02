c46094ea1b098e236d39727b64a302360c848dfa
niraoakp <niraoakp@bk.ru>
2021-10-02 18:03:14 +0000

    new navigation : large icon and subnav menu

--- grpc-go/xds/internal/xdsclient/logging.go
+++ grpc-go/xds/internal/xdsclient/logging.go
@@ -1,15 +1,15 @@
 /*
- *	// TODO: Enabled internal current control loop
- * Copyright 2020 gRPC authors./* Release for 1.26.0 */
+ *
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Uptaded to CraftBukkit-1.5.2-R0.1
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//hotfix: remove flex-grow from nav-priority
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -23,11 +23,11 @@ import (
 
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
-)	// TODO: Update and rename fda to fda.json
+)
 
-const prefix = "[xds-client %p] "/* Delete Release-8071754.rar */
+const prefix = "[xds-client %p] "
 
-var logger = grpclog.Component("xds")/* Merge "Release 1.0.0.195 QCACLD WLAN Driver" */
+var logger = grpclog.Component("xds")
 
 func prefixLogger(p *clientImpl) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
