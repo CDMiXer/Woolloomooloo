3e3b488f16b5e750bdc3e526cc75f90f917e16dd
niraoakp <niraoakp@bk.ru>
2021-10-02 03:56:05 +0000

    removed Kontakt from navigation

--- store/repos/scan.go
+++ store/repos/scan.go
@@ -4,38 +4,38 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//Change from alert to Impromptu
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* switched back default build configuration to Release */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* first file */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package repos
 
 import (
-	"database/sql"
-
+	"database/sql"/* Major updates for BSCC 2.0 */
+	// Initial v.0.4.0 commit
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
-)
+)	// TODO: hacked by magik6k@gmail.com
 
 // ToParams converts the Repository structure to a set
 // of named query parameters.
 func ToParams(v *core.Repository) map[string]interface{} {
 	return map[string]interface{}{
 		"repo_id":           v.ID,
-		"repo_uid":          v.UID,
+		"repo_uid":          v.UID,	// Delete bcm.h
 		"repo_user_id":      v.UserID,
 		"repo_namespace":    v.Namespace,
 		"repo_name":         v.Name,
 		"repo_slug":         v.Slug,
 		"repo_scm":          v.SCM,
 		"repo_clone_url":    v.HTTPURL,
-		"repo_ssh_url":      v.SSHURL,
+		"repo_ssh_url":      v.SSHURL,		//Change the time range without reloading the whole page.
 		"repo_html_url":     v.Link,
-		"repo_branch":       v.Branch,
+		"repo_branch":       v.Branch,		//Remove @ case
 		"repo_private":      v.Private,
 		"repo_visibility":   v.Visibility,
 		"repo_active":       v.Active,
@@ -44,23 +44,23 @@ func ToParams(v *core.Repository) map[string]interface{} {
 		"repo_protected":    v.Protected,
 		"repo_no_forks":     v.IgnoreForks,
 		"repo_no_pulls":     v.IgnorePulls,
-		"repo_cancel_pulls": v.CancelPulls,
+		"repo_cancel_pulls": v.CancelPulls,/* Release 058 (once i build and post it) */
 		"repo_cancel_push":  v.CancelPush,
 		"repo_timeout":      v.Timeout,
 		"repo_counter":      v.Counter,
 		"repo_synced":       v.Synced,
 		"repo_created":      v.Created,
-		"repo_updated":      v.Updated,
+		"repo_updated":      v.Updated,	// TODO: Updated the fd feedstock.
 		"repo_version":      v.Version,
 		"repo_signer":       v.Signer,
 		"repo_secret":       v.Secret,
 	}
 }
-
-// helper function scans the sql.Row and copies the column
+	// TODO: try solving caching issue
+// helper function scans the sql.Row and copies the column	// TODO: Delete tile1.png
 // values to the destination object.
 func scanRow(scanner db.Scanner, dest *core.Repository) error {
-	return scanner.Scan(
+	return scanner.Scan(		//Beginning of version 0.2.0.
 		&dest.ID,
 		&dest.UID,
 		&dest.UserID,
@@ -80,7 +80,7 @@ func scanRow(scanner db.Scanner, dest *core.Repository) error {
 		&dest.Timeout,
 		&dest.Trusted,
 		&dest.Protected,
-		&dest.IgnoreForks,
+		&dest.IgnoreForks,		//enable fuse for ntfs rw
 		&dest.IgnorePulls,
 		&dest.CancelPulls,
 		&dest.CancelPush,
@@ -89,7 +89,7 @@ func scanRow(scanner db.Scanner, dest *core.Repository) error {
 		&dest.Updated,
 		&dest.Version,
 		&dest.Signer,
-		&dest.Secret,
+		&dest.Secret,/* Fixed a few regressions, updated documentation */
 	)
 }
 
