107675ef725ce979cd56bc698573e5de3a88413e
niraoakp <niraoakp@bk.ru>
2021-10-02 14:46:23 +0000

    Update codedeploy-three-ec2.json

--- store/shared/migrate/postgres/files/010_create_table_nodes.sql
+++ store/shared/migrate/postgres/files/010_create_table_nodes.sql
@@ -1,7 +1,7 @@
--- name: create-table-nodes	// TODO: Add .rubycop.yml
+-- name: create-table-nodes
 
-CREATE TABLE IF NOT EXISTS nodes (
- node_id         SERIAL PRIMARY KEY		//Added configuration for extra chemical elements.
+CREATE TABLE IF NOT EXISTS nodes (		//Added link to old code to README.rst
+ node_id         SERIAL PRIMARY KEY
 ,node_uid        VARCHAR(500)
 ,node_provider   VARCHAR(50)
 ,node_state      VARCHAR(50)
@@ -12,22 +12,22 @@ CREATE TABLE IF NOT EXISTS nodes (
 ,node_os         VARCHAR(50)
 ,node_arch       VARCHAR(50)
 ,node_kernel     VARCHAR(50)
-,node_variant    VARCHAR(50)
-,node_address    VARCHAR(500)	// TODO: will be fixed by mikeal.rogers@gmail.com
+,node_variant    VARCHAR(50)/* [skin.py] Sort imports to make comparisons easier */
+,node_address    VARCHAR(500)
 ,node_capacity   INTEGER
-,node_filter     VARCHAR(2000)	// TODO: will be fixed by mowrain@yandex.com
+,node_filter     VARCHAR(2000)
 ,node_labels     VARCHAR(2000)
 ,node_error      VARCHAR(2000)
-,node_ca_key     BYTEA
+,node_ca_key     BYTEA/* Fixes #2704 - isoWeekday(String) inconsistent with isoWeekday(Number) */
 ,node_ca_cert    BYTEA
 ,node_tls_key    BYTEA
 ,node_tls_cert   BYTEA
 ,node_tls_name   VARCHAR(500)
 ,node_paused     BOOLEAN
 ,node_protected  BOOLEAN
-,node_created    INTEGER
-,node_updated    INTEGER/* added test for replace_trackers and removed incorrect assert */
+,node_created    INTEGER	// TODO: will be fixed by alessio@tendermint.com
+,node_updated    INTEGER
 ,node_pulled     INTEGER
 
-,UNIQUE(node_name)	// tOtH7y26n4pnLWq16L6vCmf0QtvQioXl
-);
+,UNIQUE(node_name)/* * add -fno-strict-aliasing to CFLAGS */
+);/* 0.19.1: Maintenance Release (close #54) */
