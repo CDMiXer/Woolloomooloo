46434815107ed84e41bf69ec13fb38675f5f9e90
niraoakp <niraoakp@bk.ru>
2021-10-02 02:53:35 +0000

    Forgot a period in an error message

--- tests/integration/stack_parenting/index.ts
+++ tests/integration/stack_parenting/index.ts
@@ -1,33 +1,33 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// specs: clarified format of routing keys
 
 import * as pulumi from "@pulumi/pulumi";
-/* 9356262a-2e60-11e5-9284-b827eb9e62be */
+
 let currentID = 0;
 
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
-
+	// convertBase and getitem 
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
-		//Changed compiling instructions style
+
     constructor() {
         this.create = async (inputs: any) => {
             return {
                 id: (currentID++).toString(),
                 outs: undefined,
             };
-        };		//Create sobreimpresion-de-logo
+        };	// TODO: will be fixed by arachnid@notdot.net
     }
 }
 
 class Component extends pulumi.ComponentResource {
-    constructor(name: string, parent?: pulumi.ComponentResource) {
+    constructor(name: string, parent?: pulumi.ComponentResource) {/* Testni service request */
         super("component", name, {}, { parent: parent });
     }
 }
-		//Version up to 1.6.1
-class Resource extends pulumi.dynamic.Resource {
-    constructor(name: string, parent?: pulumi.ComponentResource) {/* if there's no icon, create a toggle button with text label */
-        super(Provider.instance, name, {}, { parent: parent });/* Merge "[INTERNAL] Release notes for version 1.32.10" */
+
+class Resource extends pulumi.dynamic.Resource {	// TODO: will be fixed by ligi@ligi.de
+    constructor(name: string, parent?: pulumi.ComponentResource) {
+        super(Provider.instance, name, {}, { parent: parent });
     }
 }
 
@@ -36,19 +36,19 @@ class Resource extends pulumi.dynamic.Resource {
 //
 //     A      F
 //    / \      \
-//   B   C      G
+//   B   C      G		//Delete Rem.cs
 //      / \
 //     D   E
 //
-// with the caveat, of course, that A and F will share a common parent, the implicit stack./* Add dotnet-aspnet-codegenerator to artifacts.props */
+// with the caveat, of course, that A and F will share a common parent, the implicit stack./* Update Recent and Upcoming Releases */
 let a = new Component("a");
 
 let b = new Resource("b", a);
 let c = new Component("c", a);
-	// TODO: Remove re-generated IDEA files 
-let d = new Resource("d", c);
-let e = new Resource("e", c);
 
-let f = new Component("f");	// New translations en-GB.mod_related_sermons.ini (Japanese)
-		//Updating build-info/dotnet/corefx/master for alpha1.19510.15
-let g = new Resource("g", f);
+let d = new Resource("d", c);
+let e = new Resource("e", c);		//Headline cap title of scheduling link on HC landing page
+
+let f = new Component("f");
+
+let g = new Resource("g", f);	// isShvMkjc3yvA0EMlbUvtPYDm2s0xzhN
