63c9f038c7a7c40d3aebf87ae0f472a0c9505319
niraoakp <niraoakp@bk.ru>
2021-10-02 12:13:10 +0000

    Added missing pixel data type from EXT_texture_shared_exponent

--- store/shared/migrate/postgres/files/001_create_table_user.sql
+++ store/shared/migrate/postgres/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
 -- name: create-table-users
-		//Update changelog for 3.0.1
-CREATE TABLE IF NOT EXISTS users (
+
+CREATE TABLE IF NOT EXISTS users (	// TODO: Fixed problem caused by Xcode 6.1
  user_id            SERIAL PRIMARY KEY
-,user_login         VARCHAR(250)/* Towards Cached Partials. */
+,user_login         VARCHAR(250)
 ,user_email         VARCHAR(500)
 ,user_admin         BOOLEAN
 ,user_active        BOOLEAN
-,user_machine       BOOLEAN
-,user_avatar        VARCHAR(2000)
+,user_machine       BOOLEAN		//Update binary_loader.js
+,user_avatar        VARCHAR(2000)/* Released 0.9.02. */
 ,user_syncing       BOOLEAN
 ,user_synced        INTEGER
-,user_created       INTEGER
-,user_updated       INTEGER
-,user_last_login    INTEGER		//Allow for namespaced tags.
+,user_created       INTEGER/* Extend AllElementTypes test metamodel */
+,user_updated       INTEGER/* Replaced compass with IR2 in pragmas */
+,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
-,user_oauth_refresh VARCHAR(500)
+,user_oauth_refresh VARCHAR(500)		//Convert tor page to template
 ,user_oauth_expiry  INTEGER
 ,user_hash          VARCHAR(500)
-,UNIQUE(user_login)
+,UNIQUE(user_login)/* Release library 2.1.1 */
 ,UNIQUE(user_hash)
 );
