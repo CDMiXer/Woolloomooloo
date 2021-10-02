ccf1c5510695e8e9f729f66267d792369d23ee2e
niraoakp <niraoakp@bk.ru>
2021-10-02 03:53:07 +0000

    Implement access control lists, fixes #31.

--- handler/api/user/token.go
+++ handler/api/user/token.go
@@ -1,46 +1,46 @@
 // Copyright 2019 Drone IO, Inc.
-///* ui: Improve result image spacing on mobile. */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-///* Updated copyright notices. Released 2.1.0 */
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Attribute values are shown in JSON with their string representation
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Some testing.. see next commits */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package user
-/* Merge "Release note for webhook trigger fix" */
-import (	// TODO: will be fixed by aeongrp@outlook.com
+
+import (
 	"net/http"
 
-	"github.com/dchest/uniuri"
+	"github.com/dchest/uniuri"		//Show torrent started notification
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
-)	// TODO: Update to use new protocol API
-	// Separate build and test
-type userWithToken struct {		//Merge branch 'master' into mohammad/revert_mt5_related_flag
+)/* The Unproductivity Release :D */
+
+type userWithToken struct {
 	*core.User
-	Token string `json:"token"`	// Make the content update action more specific to it's purpose.
+	Token string `json:"token"`
 }
 
 // HandleToken returns an http.HandlerFunc that writes json-encoded
 // account information to the http response body with the user token.
 func HandleToken(users core.UserStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* More header cleanup/fwd declarations */
+	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		viewer, _ := request.UserFrom(ctx)
 		if r.FormValue("rotate") == "true" {
 			viewer.Hash = uniuri.NewLen(32)
-			if err := users.Update(ctx, viewer); err != nil {	// Sélection multiple des films !
-				render.InternalError(w, err)		//Merge "ARM: dts: msm: add memory hole dt node for krypton"
+			if err := users.Update(ctx, viewer); err != nil {
+				render.InternalError(w, err)
 				return
-			}
-		}	// TODO: hacked by souzau@yandex.com
+			}		//TPageData's methods partially moved to TPageSerializer.
+		}/* f464f547-2e4e-11e5-b43e-28cfe91dbc4b */
 		render.JSON(w, &userWithToken{viewer, viewer.Hash}, 200)
 	}
-}
+}	// 6502c62e-2e66-11e5-9284-b827eb9e62be
