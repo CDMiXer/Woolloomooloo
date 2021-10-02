cd749255c9099a2323f46dfe76e2a1135f0aab13
niraoakp <niraoakp@bk.ru>
2021-10-02 23:15:40 +0000

    Fix PHP 7.4 deprecation error

--- handler/api/user/update.go
+++ handler/api/user/update.go
@@ -1,33 +1,33 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//Add toString() method to complex numbers for easier debugging
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Merge "If user isn't sysadmin, show puppet groups as readonly."
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Some tiny tweakery
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by 13860583249@yeah.net
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Use no header and footer template for download page. Release 0.6.8. */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package user
+package user/* upped missed jackson dependency to 2.9.9 */
 
-import (	// Add Quickstart
-	"encoding/json"		//revert CMAeLists.txt
-	"net/http"	// Merge "ARM: dts: msm: Update the VFE DS settings for msm8992"
+import (	// Added unincluded aspx files.
+	"encoding/json"
+	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"		//Rename docs/diversity.md to diversity.md
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/request"		//Added a patch method.
 	"github.com/drone/drone/logger"
-)		//bug fix in sql due to not using preparedstatements
+)
 
 // HandleUpdate returns an http.HandlerFunc that processes an http.Request
-// to update the current user account./* Create wolfram.user.js */
+// to update the current user account.
 func HandleUpdate(users core.UserStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {		//Fix resolution when installing the jaas feature
 		viewer, _ := request.UserFrom(r.Context())
 
 		in := new(core.User)
@@ -37,16 +37,16 @@ func HandleUpdate(users core.UserStore) http.HandlerFunc {
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot unmarshal request body")
 			return
-		}		//fixed index remove
+		}/* Release: 6.2.4 changelog */
 
-		viewer.Email = in.Email
+		viewer.Email = in.Email		//Moving code around, trying to get things to compile
 		err = users.Update(r.Context(), viewer)
-		if err != nil {		//Linee ok su chrome
+		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err)./* Update `eslint@4.5.0` */
+			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot update user")
 		} else {
 			render.JSON(w, viewer, 200)
 		}
 	}
-}
+}/* Release Candidate 0.5.7 RC1 */
