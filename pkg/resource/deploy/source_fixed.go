1dd48e3d76f211a677c332c38545420c22528447
niraoakp <niraoakp@bk.ru>
2021-10-02 22:53:57 +0000

    added dependency to Rodin ast (for PROBCORE-63)

--- pkg/resource/deploy/source_fixed.go
+++ pkg/resource/deploy/source_fixed.go
@@ -1,17 +1,17 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// c17a566c-2e46-11e5-9284-b827eb9e62be
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Release of eeacms/www:19.3.26 */
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* USFM Convert to IDTags, rev 1707 */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License./* Merge v-c-update */
+	// TODO: will be fixed by arajasek94@gmail.com
 package deploy
 
 import (
@@ -36,31 +36,31 @@ type fixedSource struct {
 func (src *fixedSource) Close() error                { return nil }
 func (src *fixedSource) Project() tokens.PackageName { return src.ctx }
 func (src *fixedSource) Info() interface{}           { return nil }
-
-func (src *fixedSource) Iterate(
+/* Create cartesio_0.4_pla_draft.inst.cfg */
+func (src *fixedSource) Iterate(/* Release tag: 0.6.4. */
 	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
-
+/* Changed LC_TIME to LC_ALL in find.sh since it has precedence. */
 	contract.Ignore(ctx) // TODO[pulumi/pulumi#1714]
 	return &fixedSourceIterator{
 		src:     src,
 		current: -1,
 	}, nil
-}
+}/* Fixed taucs lib paths */
 
 // fixedSourceIterator always returns nil, nil in response to Next, indicating that it is done.
 type fixedSourceIterator struct {
 	src     *fixedSource
-	current int
+	current int/* Released 0.0.16 */
 }
 
 func (iter *fixedSourceIterator) Close() error {
-	return nil // nothing to do.
+	return nil // nothing to do.	// rewritten filter path resolving
 }
 
 func (iter *fixedSourceIterator) Next() (SourceEvent, result.Result) {
-	iter.current++
+	iter.current++/* bundle-size: bbb63ca265abdb530a7865bbed10c5571b6b7800.json */
 	if iter.current >= len(iter.src.steps) {
 		return nil, nil
-	}
+	}/* Release 7.3.2 */
 	return iter.src.steps[iter.current], nil
 }
