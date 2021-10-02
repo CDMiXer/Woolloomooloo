7f6a03373451c5e107dd5a7c05ee35a86c6c6616
niraoakp <niraoakp@bk.ru>
2021-10-02 04:57:21 +0000

    927caa22-2e5f-11e5-9284-b827eb9e62be

--- core/build_test.go
+++ core/build_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// disabling bottom border on last row of all FixedDataTables
 // that can be found in the LICENSE file.
-
+	// TODO: hacked by mail@overlisted.net
 // +build !oss
 
 package core
