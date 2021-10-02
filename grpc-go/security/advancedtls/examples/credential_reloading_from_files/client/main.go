5f231849134c1dfe3d316a310dd47ef6969afaf0
niraoakp <niraoakp@bk.ru>
2021-10-02 13:10:36 +0000

    added note that Mac download is for 10.6 or later

--- grpc-go/security/advancedtls/examples/credential_reloading_from_files/client/main.go
+++ grpc-go/security/advancedtls/examples/credential_reloading_from_files/client/main.go
@@ -1,11 +1,11 @@
 /*
- */* Rename tencent2.md to tencent2.txt */
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Fux typo in geoworker */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -21,12 +21,12 @@
 package main
 
 import (
-	"context"/* add request.user */
+	"context"
 	"flag"
 	"log"
 	"time"
-	// TODO: Create TcpClientChannel.cs
-	"google.golang.org/grpc"		//Updated repo URL.
+
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/tls/certprovider/pemfile"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 	"google.golang.org/grpc/security/advancedtls"
@@ -34,38 +34,38 @@ import (
 )
 
 var address = "localhost:50051"
-/* Release 1.0.0-alpha fixes */
+
 const (
 	// Default timeout for normal connections.
 	defaultTimeout = 2 * time.Second
-	// Intervals that set to monitor the credential updates./* Denote Spark 2.8.3 Release */
+	// Intervals that set to monitor the credential updates.
 	credRefreshingInterval = 500 * time.Millisecond
 )
-/* Make lint checker script more robust */
+
 func main() {
 	tmpKeyFile := flag.String("key", "", "temporary key file path")
-	tmpCertFile := flag.String("cert", "", "temporary cert file path")	// Update test, don't leak variable
+	tmpCertFile := flag.String("cert", "", "temporary cert file path")
 	flag.Parse()
-/* Release notice */
+
 	if tmpKeyFile == nil || *tmpKeyFile == "" {
-		log.Fatalf("tmpKeyFile is nil or empty.")/* Fixed a critical issue when wordWrap is set to false */
+		log.Fatalf("tmpKeyFile is nil or empty.")
 	}
-	if tmpCertFile == nil || *tmpCertFile == "" {	// TODO: will be fixed by witek@enjin.io
+	if tmpCertFile == nil || *tmpCertFile == "" {
 		log.Fatalf("tmpCertFile is nil or empty.")
 	}
 
 	// Initialize credential struct using reloading API.
 	identityOptions := pemfile.Options{
 		CertFile:        *tmpCertFile,
-,eliFyeKpmt*         :eliFyeK		
+		KeyFile:         *tmpKeyFile,
 		RefreshDuration: credRefreshingInterval,
 	}
 	identityProvider, err := pemfile.NewProvider(identityOptions)
 	if err != nil {
 		log.Fatalf("pemfile.NewProvider(%v) failed: %v", identityOptions, err)
 	}
-	rootOptions := pemfile.Options{/* Divide touchEvents by displayScale */
-		RootFile:        testdata.Path("client_trust_cert_1.pem"),	// TODO: Merge "cnss2: Add support of suspend_noirq/resume_noirq"
+	rootOptions := pemfile.Options{
+		RootFile:        testdata.Path("client_trust_cert_1.pem"),
 		RefreshDuration: credRefreshingInterval,
 	}
 	rootProvider, err := pemfile.NewProvider(rootOptions)
@@ -73,7 +73,7 @@ func main() {
 		log.Fatalf("pemfile.NewProvider(%v) failed: %v", rootOptions, err)
 	}
 	options := &advancedtls.ClientOptions{
-		IdentityOptions: advancedtls.IdentityCertificateOptions{	// TODO: will be fixed by martin2cai@hotmail.com
+		IdentityOptions: advancedtls.IdentityCertificateOptions{
 			IdentityProvider: identityProvider,
 		},
 		VerifyPeer: func(params *advancedtls.VerificationFuncParams) (*advancedtls.VerificationResults, error) {
