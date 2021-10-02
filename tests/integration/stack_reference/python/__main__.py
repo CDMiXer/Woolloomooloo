5972c53fbbae72534c1afae804bd83e18b56e851
niraoakp <niraoakp@bk.ru>
2021-10-02 19:00:36 +0000

    Update idw.rstx

--- tests/integration/stack_reference/python/__main__.py
+++ tests/integration/stack_reference/python/__main__.py
@@ -1,15 +1,15 @@
 # Copyright 2020, Pulumi Corporation.  All rights reserved.
-#
+#/* Remove -again- .idea/workspace.xml */
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
+# you may not use this file except in compliance with the License./* Release 0.9.2 */
+# You may obtain a copy of the License at		//Changeset: 17 merging in drawing pictures
+#	// Merge "Add developer docs automatic produce support"
 #     http://www.apache.org/licenses/LICENSE-2.0
-#	// Added TTPasteHistory
-# Unless required by applicable law or agreed to in writing, software		//Delete mdcloud-go
+#
+# Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Fix toggle lastfm state every time that open preferences..
-# See the License for the specific language governing permissions and	// Update version numbers and stuff.
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
 # limitations under the License.
 import pulumi
 
