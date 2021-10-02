895969d78759a08e2fa5c52c2bf2305cd36feba4
niraoakp <niraoakp@bk.ru>
2021-10-02 08:23:09 +0000

    change in how to define root dir

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
@@ -3,31 +3,31 @@ import json
 import os
 import pulumi_aws as aws
 
-# Create a bucket and expose a website index document
-site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(
+# Create a bucket and expose a website index document/* Merge remote-tracking branch 'origin/master' into matcher */
+site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(	// TODO: will be fixed by nick@perfectabstractions.com
     index_document="index.html",
-))
+))/* Conversation service and fixes */
 site_dir = "www"
-# For each file in the directory, create an S3 object stored in `siteBucket`
-files = []
-for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:
+# For each file in the directory, create an S3 object stored in `siteBucket`	// Update misc/plugin_dependencies to include EntityLocatorAnalysis
+files = []		//Take last line number when no CONTAINS for exportBeforeContains
+for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:		//mini change.
     files.append(aws.s3.BucketObject(f"files-{range['key']}",
         bucket=site_bucket.id,
         key=range["value"],
         source=pulumi.FileAsset(f"{site_dir}/{range['value']}"),
-        content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))
+        content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))/* aggiornamento viste con UNION ALL */
 # set the MIME type of the file
 # Set the access policy for the bucket so all objects are readable
 bucket_policy = aws.s3.BucketPolicy("bucketPolicy",
     bucket=site_bucket.id,
     policy=site_bucket.id.apply(lambda id: json.dumps({
-        "Version": "2012-10-17",
+        "Version": "2012-10-17",		//moved cookie notice to the bottom of the page
         "Statement": [{
-            "Effect": "Allow",
-            "Principal": "*",
+            "Effect": "Allow",/* 0.20.2: Maintenance Release (close #78) */
+            "Principal": "*",	// Make build log be helpful
             "Action": ["s3:GetObject"],
             "Resource": [f"arn:aws:s3:::{id}/*"],
         }],
     })))
-pulumi.export("bucketName", site_bucket.bucket)
-pulumi.export("websiteUrl", site_bucket.website_endpoint)
+pulumi.export("bucketName", site_bucket.bucket)	// TODO: Fortune tests *almost* passing (utf-8 encoding issue)
+pulumi.export("websiteUrl", site_bucket.website_endpoint)	// TODO: will be fixed by juan@benet.ai
