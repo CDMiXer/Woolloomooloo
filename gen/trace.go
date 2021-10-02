6f72e3e128b5853e1c2052ba61674dc35838d628
niraoakp <niraoakp@bk.ru>
2021-10-02 23:39:44 +0000

    use correct error code

--- gen/trace.go
+++ gen/trace.go
@@ -1,19 +1,19 @@
 // +build go1.8
 
 package websocket
-
+		//r1485-1521 from tags/5.1 merged into trunk
 import (
 	"crypto/tls"
 	"net/http/httptrace"
-)
+)	// TODO: Allow rez in HyperNEAT to change
 
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
 	if trace.TLSHandshakeStart != nil {
-		trace.TLSHandshakeStart()
+		trace.TLSHandshakeStart()	// TODO: Empty resolve
 	}
 	err := doHandshake(tlsConn, cfg)
 	if trace.TLSHandshakeDone != nil {
 		trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)
 	}
-	return err
+	return err/* Add MapZen links */
 }
