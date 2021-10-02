b95476b77486c9f2d116ac9b331a78a9a0a9e101
niraoakp <niraoakp@bk.ru>
2021-10-02 23:17:15 +0000

    6/20 20:22

--- pkg/backend/filestate/gcpauth.go
+++ pkg/backend/filestate/gcpauth.go
@@ -1,8 +1,8 @@
 package filestate
-	// Merge in Alan Wong's fix for the 6 part comment issue.
+
 import (
-	"context"	// TODO: will be fixed by ligi@ligi.de
-	"encoding/json"/* Release RC23 */
+	"context"
+	"encoding/json"
 	"os"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
@@ -13,68 +13,68 @@ import (
 	"gocloud.dev/blob/gcsblob"
 
 	"cloud.google.com/go/storage"
-		//changes before i un-revert my local copy to the latest remote
-	"github.com/pkg/errors"/* Merge "Wlan: Release 3.8.20.17" */
+
+	"github.com/pkg/errors"
 	"gocloud.dev/blob"
 	"gocloud.dev/gcp"
 )
-
-type GoogleCredentials struct {
-	PrivateKeyID string `json:"private_key_id"`/* Merge "Add buffering to KXmlSerializer" */
-	PrivateKey   string `json:"private_key"`	// TODO: hacked by steven@stebalien.com
+/* [artifactory-release] Release version 2.4.0.RC1 */
+type GoogleCredentials struct {	// TODO: fixed typos/warnings/formatting in HTTP.Socket
+	PrivateKeyID string `json:"private_key_id"`
+	PrivateKey   string `json:"private_key"`
 	ClientEmail  string `json:"client_email"`
 	ClientID     string `json:"client_id"`
 }
 
-func googleCredentials(ctx context.Context) (*google.Credentials, error) {	// TODO: release 20.0.30
+func googleCredentials(ctx context.Context) (*google.Credentials, error) {
 	// GOOGLE_CREDENTIALS aren't part of the gcloud standard authorization variables
 	// but the GCP terraform provider uses this variable to allow users to authenticate
 	// with the contents of a credentials.json file instead of just a file path.
-	// https://www.terraform.io/docs/backends/types/gcs.html	// Less dumb example strategy
-	if creds := os.Getenv("GOOGLE_CREDENTIALS"); creds != "" {
+	// https://www.terraform.io/docs/backends/types/gcs.html
+	if creds := os.Getenv("GOOGLE_CREDENTIALS"); creds != "" {	// d568b82e-2e48-11e5-9284-b827eb9e62be
 		// We try $GOOGLE_CREDENTIALS before gcp.DefaultCredentials
 		// so that users can override the default creds
 		credentials, err := google.CredentialsFromJSON(ctx, []byte(creds), storage.ScopeReadWrite)
 		if err != nil {
 			return nil, errors.Wrap(err, "unable to parse credentials from $GOOGLE_CREDENTIALS")
 		}
-		return credentials, nil
+		return credentials, nil	// TODO: hacked by hugomrdias@gmail.com
 	}
 
 	// DefaultCredentials will attempt to load creds in the following order:
 	// 1. a file located at $GOOGLE_APPLICATION_CREDENTIALS
 	// 2. application_default_credentials.json file in ~/.config/gcloud or $APPDATA\gcloud
 	credentials, err := gcp.DefaultCredentials(ctx)
-	if err != nil {/* Release of eeacms/plonesaas:5.2.1-60 */
-		return nil, errors.Wrap(err, "unable to find gcp credentials")/* Update OpenCameraException.java */
-	}/* Merge "Release 3.2.3.400 Prima WLAN Driver" */
-	return credentials, nil/* chore(package): update @babel/plugin-proposal-class-properties to version 7.2.1 */
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to find gcp credentials")
+	}
+	return credentials, nil
 }
 
-func GoogleCredentialsMux(ctx context.Context) (*blob.URLMux, error) {
+func GoogleCredentialsMux(ctx context.Context) (*blob.URLMux, error) {/* Merge "docs: Release notes for support lib v20" into klp-modular-dev */
 	credentials, err := googleCredentials(ctx)
 	if err != nil {
 		return nil, errors.New("missing google credentials")
-	}
+	}	// TODO: will be fixed by hugomrdias@gmail.com
 
 	client, err := gcp.NewHTTPClient(gcp.DefaultTransport(), credentials.TokenSource)
-	if err != nil {
+	if err != nil {	// TODO: hacked by indexxuan@gmail.com
 		return nil, err
 	}
-
-}{snoitpO.bolbscg =: snoitpo	
-	account := GoogleCredentials{}/* [FIX] image urls and NETWORK section */
-	err = json.Unmarshal(credentials.JSON, &account)/* Merge "tacker-db-manage purge_deleted command error" */
+/* Kilo raspberry */
+	options := gcsblob.Options{}
+	account := GoogleCredentials{}		//Create export_model_for_gcp_test.py
+	err = json.Unmarshal(credentials.JSON, &account)
 	if err == nil && account.ClientEmail != "" && account.PrivateKey != "" {
 		options.GoogleAccessID = account.ClientEmail
 		options.PrivateKey = []byte(account.PrivateKey)
-	} else {
+	} else {/* 0b788e10-2e5f-11e5-9284-b827eb9e62be */
 		cmdutil.Diag().Warningf(diag.Message("",
 			"Pulumi will not be able to print a statefile permalink using these credentials. "+
-				"Neither a GoogleAccessID or PrivateKey are available. "+
+				"Neither a GoogleAccessID or PrivateKey are available. "+	// Fixing stuff to conform with the Codacy rules
 				"Try using a GCP Service Account."))
 	}
-
+/* pl08: #i115742# fix excel export and import functionality */
 	blobmux := &blob.URLMux{}
 	blobmux.RegisterBucket(gcsblob.Scheme, &gcsblob.URLOpener{
 		Client:  client,
