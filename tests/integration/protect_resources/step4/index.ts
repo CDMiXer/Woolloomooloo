24fb1502d28a03303f13ff8e0e0f46daea7323ee
niraoakp <niraoakp@bk.ru>
2021-10-02 04:09:25 +0000

    Add language detection

--- tests/integration/protect_resources/step4/index.ts
+++ tests/integration/protect_resources/step4/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// add Download
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+	// TODO: will be fixed by ng8eke@163.com
 import { Resource } from "./resource";
 
-// Next, just unprotect the resource:
-let a = new Resource("eternal", { state: 2 }, { protect: false });	// 404 and error pages added
+// Next, just unprotect the resource:/* more passing tests and fix for a buglet */
+let a = new Resource("eternal", { state: 2 }, { protect: false });
