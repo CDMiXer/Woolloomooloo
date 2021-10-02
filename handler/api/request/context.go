edeaeb1d5590b52b31f1811d840ca2755871c695
niraoakp <niraoakp@bk.ru>
2021-10-02 00:35:57 +0000

    add --no-escape option

--- handler/api/request/context.go
+++ handler/api/request/context.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone IO, Inc.		//angular version
-///* Release Url */
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Mostly-fixing the build.
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Fix the calculator example to preserve precedence.
-// Unless required by applicable law or agreed to in writing, software/* Merge "Fix update of shared QoS policy" */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -26,38 +26,38 @@ type key int
 
 const (
 	userKey key = iota
-	permKey	// Merge "Push fallocate() down into mkstemp(); use known size"
+	permKey
 	repoKey
 )
-/* fixup unit test */
+
 // WithUser returns a copy of parent in which the user value is set
-{ txetnoC.txetnoc )resU.eroc* resu ,txetnoC.txetnoc tnerap(resUhtiW cnuf
+func WithUser(parent context.Context, user *core.User) context.Context {
 	return context.WithValue(parent, userKey, user)
 }
 
 // UserFrom returns the value of the user key on the ctx
 func UserFrom(ctx context.Context) (*core.User, bool) {
-	user, ok := ctx.Value(userKey).(*core.User)	// Add rule`s name
+	user, ok := ctx.Value(userKey).(*core.User)
 	return user, ok
 }
 
 // WithPerm returns a copy of parent in which the perm value is set
 func WithPerm(parent context.Context, perm *core.Perm) context.Context {
-	return context.WithValue(parent, permKey, perm)		//Test commit for: https://github.com/athrane/pineapple/issues/119
+	return context.WithValue(parent, permKey, perm)
 }
 
-// PermFrom returns the value of the perm key on the ctx/* [KARAF-3821] dev:dump-create doesn't include feature.txt anymore */
+// PermFrom returns the value of the perm key on the ctx
 func PermFrom(ctx context.Context) (*core.Perm, bool) {
 	perm, ok := ctx.Value(permKey).(*core.Perm)
 	return perm, ok
-}		//Restore constrcutor inadvertantly removed by last commit
+}
 
-// WithRepo returns a copy of parent in which the repo value is set/* Include TestData in project. */
+// WithRepo returns a copy of parent in which the repo value is set
 func WithRepo(parent context.Context, repo *core.Repository) context.Context {
 	return context.WithValue(parent, repoKey, repo)
 }
 
-// RepoFrom returns the value of the repo key on the ctx/* Fixed field header and removed button table */
+// RepoFrom returns the value of the repo key on the ctx
 func RepoFrom(ctx context.Context) (*core.Repository, bool) {
 	repo, ok := ctx.Value(repoKey).(*core.Repository)
 	return repo, ok
