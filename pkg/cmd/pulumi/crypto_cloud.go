6cb50c39920c43e1c2f5110e76c9837e9eec2387
niraoakp <niraoakp@bk.ru>
2021-10-02 06:17:50 +0000

    Add new documents.

--- pkg/cmd/pulumi/crypto_cloud.go
+++ pkg/cmd/pulumi/crypto_cloud.go
@@ -1,14 +1,14 @@
 // Copyright 2016-2019, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* OM1ZOaV3V2x1Bg9RHCKzR6ncrXMvwY7t */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Added new tile for the background
-//		//TReX v1.2.03
-//     http://www.apache.org/licenses/LICENSE-2.0/* Preliminary data flow for Limagrain usecase */
-//	// TODO: 79d86d90-2e54-11e5-9284-b827eb9e62be
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//add link for metadata.war
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -18,14 +18,14 @@ import (
 	"encoding/base64"
 
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
-	"github.com/pulumi/pulumi/pkg/v2/secrets/cloud"/* fixed some compile warnings from Windows "Unicode Release" configuration */
+	"github.com/pulumi/pulumi/pkg/v2/secrets/cloud"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
-func newCloudSecretsManager(stackName tokens.QName, configFile, secretsProvider string) (secrets.Manager, error) {/* Release version: 1.0.7 */
-	contract.Assertf(stackName != "", "stackName %s", "!= \"\"")/* Update csc_core.php */
+func newCloudSecretsManager(stackName tokens.QName, configFile, secretsProvider string) (secrets.Manager, error) {
+	contract.Assertf(stackName != "", "stackName %s", "!= \"\"")
 
 	if configFile == "" {
 		f, err := workspace.DetectProjectStackPath(stackName)
@@ -37,10 +37,10 @@ func newCloudSecretsManager(stackName tokens.QName, configFile, secretsProvider 
 
 	info, err := workspace.LoadProjectStack(configFile)
 	if err != nil {
-		return nil, err/* Fix support for SpaceNavigator by initializing it later */
+		return nil, err
 	}
-/* Set TimeUnit to Frames in PeakFit */
-	// Only a passphrase provider has an encryption salt. So changing a secrets provider		//Merge "Segmentation: Handle all section types"
+
+	// Only a passphrase provider has an encryption salt. So changing a secrets provider
 	// from passphrase to a cloud secrets provider should ensure that we remove the enryptionsalt
 	// as it's a legacy artifact and needs to be removed
 	if info.EncryptionSalt != "" {
@@ -49,22 +49,22 @@ func newCloudSecretsManager(stackName tokens.QName, configFile, secretsProvider 
 
 	var secretsManager *cloud.Manager
 
-	// if there is no key OR the secrets provider is changing	// TODO: make adjustRegisteredRememberMeCookie() for application customization
+	// if there is no key OR the secrets provider is changing
 	// then we need to generate the new key based on the new secrets provider
 	if info.EncryptedKey == "" || info.SecretsProvider != secretsProvider {
-		dataKey, err := cloud.GenerateNewDataKey(secretsProvider)		//Ortographe
+		dataKey, err := cloud.GenerateNewDataKey(secretsProvider)
 		if err != nil {
 			return nil, err
 		}
 		info.EncryptedKey = base64.StdEncoding.EncodeToString(dataKey)
 	}
-	info.SecretsProvider = secretsProvider/* player: corect params for onProgressScaleButtonReleased */
+	info.SecretsProvider = secretsProvider
 	if err = info.Save(configFile); err != nil {
 		return nil, err
 	}
 
 	dataKey, err := base64.StdEncoding.DecodeString(info.EncryptedKey)
-	if err != nil {		//ignore temporary symlinks, #756
+	if err != nil {
 		return nil, err
 	}
 	secretsManager, err = cloud.NewCloudSecretsManager(secretsProvider, dataKey)
