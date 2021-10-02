0ea6f0bbaed42d8f3f0ef3720f9e1f3afd842df2
niraoakp <niraoakp@bk.ru>
2021-10-02 19:33:03 +0000

    ONEARTH-542 Fixed unsupported reproject layers not being ignored

--- tests/integration/partial_state/step2/index.ts
+++ tests/integration/partial_state/step2/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: Fixed variable message
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by peterke@gmail.com
+
 // file is deliberately empty.
 // The engine should delete "a" since it's in the checkpoint.
