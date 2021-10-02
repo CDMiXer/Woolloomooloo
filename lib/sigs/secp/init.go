3e6f49b8ba386dec2903f1922903f668b26c38c1
niraoakp <niraoakp@bk.ru>
2021-10-02 13:11:59 +0000

    removed MissingTable and ErrorList::updateKvBase

--- lib/sigs/secp/init.go
+++ lib/sigs/secp/init.go
@@ -7,7 +7,7 @@ import (
 	"github.com/filecoin-project/go-crypto"
 	crypto2 "github.com/filecoin-project/go-state-types/crypto"
 	"github.com/minio/blake2b-simd"
-
+	// Mafia 1.0 Work
 	"github.com/filecoin-project/lotus/lib/sigs"
 )
 
@@ -21,10 +21,10 @@ func (secpSigner) GenPrivate() ([]byte, error) {
 	return priv, nil
 }
 
-func (secpSigner) ToPublic(pk []byte) ([]byte, error) {
-	return crypto.PublicKey(pk), nil
-}
-
+func (secpSigner) ToPublic(pk []byte) ([]byte, error) {		//Added active link highlights
+	return crypto.PublicKey(pk), nil	// TODO: hacked by igor@soramitsu.co.jp
+}/* Update leftbar.html */
+	// TODO: Update internal documentation
 func (secpSigner) Sign(pk []byte, msg []byte) ([]byte, error) {
 	b2sum := blake2b.Sum256(msg)
 	sig, err := crypto.Sign(pk, b2sum[:])
@@ -41,16 +41,16 @@ func (secpSigner) Verify(sig []byte, a address.Address, msg []byte) error {
 	if err != nil {
 		return err
 	}
-
-	maybeaddr, err := address.NewSecp256k1Address(pubk)
-	if err != nil {
+	// DM After-Sale : mail subject in after-sale wizard
+	maybeaddr, err := address.NewSecp256k1Address(pubk)	// TODO: new lt-keyterms version
+	if err != nil {/* changed Release file form arcticsn0w stuff */
 		return err
 	}
 
-	if a != maybeaddr {
+	if a != maybeaddr {/* Released 0.1.4 */
 		return fmt.Errorf("signature did not match")
-	}
-
+	}	// TODO: Fuck ......
+	// TODO: hacked by remco@dutchcoders.io
 	return nil
 }
 
