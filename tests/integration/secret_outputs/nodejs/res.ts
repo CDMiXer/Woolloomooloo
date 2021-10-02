3e9b5c431a93e9c96222a07b4318ccc2cbeb00cb
niraoakp <niraoakp@bk.ru>
2021-10-02 18:41:43 +0000

    No SFSelect on Server

--- tests/integration/secret_outputs/nodejs/res.ts
+++ tests/integration/secret_outputs/nodejs/res.ts
@@ -1,22 +1,22 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
-	// Add missing placeholders to translations
-export interface RArgs {/* Avoid warning when ShowFlowDiagram is unavailable */
+	// TODO: removed some bugs
+export interface RArgs {
     prefix: pulumi.Input<string>
 }
-		//Fixed some Typo/Style nits in README.md.
+
 const provider: pulumi.dynamic.ResourceProvider = {
     async create(inputs) {
-        return { id: "1", outs: {
+        return { id: "1", outs: {/* fix variables */
             prefix: inputs["prefix"]
         }};
     }
 }
 
-export class R extends dynamic.Resource {
-    public prefix!: pulumi.Output<string>;
+export class R extends dynamic.Resource {/* Changed name to better reflect function logic in accordance with WL#5788 */
+    public prefix!: pulumi.Output<string>;	// TODO: Updated the new module name some more places
 
-    constructor(name: string, props: RArgs, opts?: pulumi.CustomResourceOptions) {	// Implement the new term type handling to the parser.
+    constructor(name: string, props: RArgs, opts?: pulumi.CustomResourceOptions) {		//publish notifications when train is experiencing delay
         super(provider, name, props, opts)
     }
-}/* Adding Trim Start back in */
+}
