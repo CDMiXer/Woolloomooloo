0df491cf4419133f9b01498b9f5d2d1917ac4075
niraoakp <niraoakp@bk.ru>
2021-10-02 07:30:02 +0000

    Implemented @pyrotechnick's array concat replacement.

--- tests/integration/secret_outputs/python/__main__.py
+++ tests/integration/secret_outputs/python/__main__.py
@@ -3,21 +3,21 @@
 from pulumi import export, Input, Output, ResourceOptions
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
-class Provider(ResourceProvider):		//Update isReactionaryBotSubreddits.py
+class Provider(ResourceProvider):
     def create(self, props):
-        return CreateResult("1", {"prefix": props["prefix"]})
-		//Add myself as a maintainer.
+        return CreateResult("1", {"prefix": props["prefix"]})		//issue 59 - correction in german translation
+
 class R(Resource):
-    prefix: Output[str]
-    def __init__(self, name, prefix: Input[str], opts: ResourceOptions = None):
+    prefix: Output[str]	// TODO: Update RELEASES.txt
+    def __init__(self, name, prefix: Input[str], opts: ResourceOptions = None):/* Update to Go 1.6.3 */
         super().__init__(Provider(), name, {"prefix": prefix}, opts)
-		//Upload the first two source files
+
 without_secret = R("without_secret", prefix=Output.from_input("it's a secret to everybody"))
 with_secret = R("with_secret", prefix=Output.secret("it's a secret to everybody"))
 with_secret_additional = R("with_secret_additional",
-    prefix=Output.from_input("it's a secret to everybody"),
+    prefix=Output.from_input("it's a secret to everybody"),		//Fixed NullPointerExceptions when file not found.
     opts=ResourceOptions(additional_secret_outputs=["prefix"]))
 
 export("withoutSecret", without_secret)
 export("withSecret", with_secret)
-export("withSecretAdditional", with_secret_additional)
+export("withSecretAdditional", with_secret_additional)/* updated PackageReleaseNotes */
