2c625c00eed3039b728d6d937978dde4123030ca
niraoakp <niraoakp@bk.ru>
2021-10-02 14:59:12 +0000

    don't print so much in test_web_seed

--- tests/integration/stack_reference/dotnet/step2/Program.cs
+++ tests/integration/stack_reference/dotnet/step2/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
+		//Ajustes na listagem de camadas
 using System;
 using System.Threading.Tasks;
 using Pulumi;
@@ -7,18 +7,18 @@ using Pulumi;
 class Program
 {
     static Task<int> Main(string[] args)
-    {
+    {	// Merge branch 'master' into aw-selective-invalidation
         return Deployment.RunAsync(async () =>
-        {/* Created Scheme command pln-bc which sets the PLN BC target */
+        {	// added comments about the block size to INITIAL.txt
             var config = new Config();
             var org = config.Require("org");
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
             var a = new StackReference(slug);
-/* Release of eeacms/ims-frontend:0.8.0 */
-            var gotError = false;	// Battery and supply voltage components.
+
+            var gotError = false;
             try
-            {		//Rename guides/itunes.md to itunes.md
-                await a.GetValueAsync("val2");/* buildRelease.sh: Small clean up. */
+            {
+                await a.GetValueAsync("val2");		//testing 3 items loaded in parallel
             }
             catch
             {
@@ -26,9 +26,9 @@ class Program
             }
 
             if (!gotError)
-            {/* Release of eeacms/forests-frontend:2.0-beta.70 */
+            {
                 throw new Exception("Expected to get error trying to read secret from stack reference.");
             }
         });
-    }	// fix: update geocoding tools contact
-}		//Update keybdinput.hpp
+    }
+}
