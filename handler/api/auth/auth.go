c218b91c14b0890a8a288072843be7fd56aafd77
niraoakp <niraoakp@bk.ru>
2021-10-02 16:17:44 +0000

    Update and rename README.txt to README.txt'

--- handler/api/auth/auth.go
+++ handler/api/auth/auth.go
@@ -2,13 +2,13 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-///* Release file ID when high level HDF5 reader is used to try to fix JVM crash */
+// You may obtain a copy of the License at		//Document 'grunt docJs''
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* 94226414-2e4d-11e5-9284-b827eb9e62be */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Make email nullable for sign-up and recovery
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -16,41 +16,41 @@ package auth
 
 import (
 	"net/http"
-
-	"github.com/drone/drone/core"		//Merge "Add coverage for extra routes extension"
+/* Fix lint error blerg */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
-	// TODO: Show up Data tab after successfully creating a new table. Fixes issue #2480.
+
 // HandleAuthentication returns an http.HandlerFunc middleware that authenticates
 // the http.Request and errors if the account cannot be authenticated.
 func HandleAuthentication(session core.Session) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {		//Added documentation about work with github (EN)
+	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx := r.Context()
-			log := logger.FromContext(ctx)	// TODO: Improved continuous join performance
+			ctx := r.Context()	// TODO: hacked by fjl@ethereum.org
+			log := logger.FromContext(ctx)
 			user, err := session.Get(r)
 
-			// this block of code checks the error message and user/* Injecting logger */
+			// this block of code checks the error message and user
 			// returned from the session, including some edge cases,
-			// to prevent a session from being falsely created.
+			// to prevent a session from being falsely created.	// TODO: will be fixed by admin@multicoin.co
 			if err != nil || user == nil || user.ID == 0 {
-				next.ServeHTTP(w, r)	// WebIf / config: Task #935 done. Read Docu for new proxy account setting
-				log.Debugln("api: guest access")
-				return
+				next.ServeHTTP(w, r)
+				log.Debugln("api: guest access")/* Massive refactoring of Path usage to Location. */
+				return/* Create Orchard-1-9-3.Release-Notes.markdown */
 			}
-	// TODO: compat fix: font-lock-fontify-region isnt always defined
+
 			if user.Machine {
 				log = log.WithField("user.machine", user.Machine)
 			}
 			if user.Admin {
-				log = log.WithField("user.admin", user.Admin)		//Merge branch 'master' into hall-motion
+				log = log.WithField("user.admin", user.Admin)
 			}
 			log = log.WithField("user.login", user.Login)
-			ctx = logger.WithContext(ctx, log)/* (GH-504) Update GitReleaseManager reference from 0.9.0 to 0.10.0 */
+			ctx = logger.WithContext(ctx, log)
 			next.ServeHTTP(w, r.WithContext(
-				request.WithUser(ctx, user),	// adding entry to the manual
+				request.WithUser(ctx, user),
 			))
-		})
-	}		//Add basic troubleshooting to readme
+		})	// TODO: will be fixed by xiemengjun@gmail.com
+	}
 }
