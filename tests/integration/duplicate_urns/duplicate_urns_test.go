dfd00c30fc13e9fc684723451dc50038b07c8afc
niraoakp <niraoakp@bk.ru>
2021-10-02 04:34:10 +0000

    longer timeline-divider

--- tests/integration/duplicate_urns/duplicate_urns_test.go
+++ tests/integration/duplicate_urns/duplicate_urns_test.go
@@ -1,29 +1,29 @@
-// Copyright 2016-2018, Pulumi Corporation.		//comment out "hi, getNodeFormat"
-// +build nodejs all	// TODO: hacked by joshua@yottadb.com
+// Copyright 2016-2018, Pulumi Corporation.
+// +build nodejs all
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* [artifactory-release] Release version 0.8.17.RELEASE */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// Get Win32 backend compiling against the recent monad changes.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Change order of text and badges
+
 package ints
 
 import (
-	"testing"		//Updated KeystoreUtil for Java 8. Updated aws script.
+	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Back to Maven Release Plugin */
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 // Test that the engine does not tolerate duplicate URNs in the same plan.
 func TestDuplicateURNs(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{	// - Inserção funcionário.
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:           "step1",
 		Dependencies:  []string{"@pulumi/pulumi"},
 		Quick:         true,
@@ -33,15 +33,15 @@ func TestDuplicateURNs(t *testing.T) {
 				Dir:      "step2",
 				Additive: true,
 			},
-			{	// TODO: Added README Line break
-				Dir:           "step3",
-				Additive:      true,/* Portuguese translation and slides (#1) */
-				ExpectFailure: true,
-			},/* Release 0.19.3 */
 			{
-				Dir:           "step4",/* Released 11.3 */
+				Dir:           "step3",
 				Additive:      true,
-				ExpectFailure: true,/* Release of eeacms/energy-union-frontend:v1.2 */
+				ExpectFailure: true,
+			},
+			{
+				Dir:           "step4",
+				Additive:      true,
+				ExpectFailure: true,
 			},
 		},
 	})
