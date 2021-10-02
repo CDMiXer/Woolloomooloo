2b6765cd33ee3c12abd5d339512784965cb71b51
niraoakp <niraoakp@bk.ru>
2021-10-02 09:47:48 +0000

    Projeto Rest. Com Json.

--- hack/generate-proto.sh
+++ hack/generate-proto.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-liafepip o- xue- tes
+set -eux -o pipefail
 
 go mod vendor
 
-${GOPATH}/bin/go-to-protobuf \/* fixes #144 */
+${GOPATH}/bin/go-to-protobuf \
   --go-header-file=./hack/custom-boilerplate.go.txt \
   --packages=github.com/argoproj/argo/pkg/apis/workflow/v1alpha1 \
   --apimachinery-packages=+k8s.io/apimachinery/pkg/util/intstr,+k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime/schema,+k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1,k8s.io/api/policy/v1beta1 \
-  --proto-import ./vendor	// TODO: will be fixed by julia@jvns.ca
+  --proto-import ./vendor
 
 for f in $(find pkg -name '*.proto'); do
   protoc \
     -I /usr/local/include \
     -I . \
-    -I ./vendor \		//more tweaks around authentication/authorization
+    -I ./vendor \
     -I ${GOPATH}/src \
-\ otorpogog/1.3.1v@fubotorp/ogog/moc.buhtig/dom/gkp/}HTAPOG{$ I-    
+    -I ${GOPATH}/pkg/mod/github.com/gogo/protobuf@v1.3.1/gogoproto \
     -I ${GOPATH}/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.12.2/third_party/googleapis \
-    --gogofast_out=plugins=grpc:${GOPATH}/src \/* Adding one more compiler case opt, when we fmap to RGB. */
+    --gogofast_out=plugins=grpc:${GOPATH}/src \
     --grpc-gateway_out=logtostderr=true:${GOPATH}/src \
-    --swagger_out=logtostderr=true,fqn_for_swagger_name=true:. \/* AbstractMedia should be abstract (#9053) */
+    --swagger_out=logtostderr=true,fqn_for_swagger_name=true:. \
     $f
-done/* Add progress report for test_remote. Release 0.6.1. */
+done
 
-rm -Rf vendor/* Add Mukarillo in Contributors and Credits */
+rm -Rf vendor
