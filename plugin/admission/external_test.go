09636dd82883070df8571cc3e0f71e1e8a3278f3
niraoakp <niraoakp@bk.ru>
2021-10-02 07:11:34 +0000

    updateVertex()

--- plugin/admission/external_test.go
+++ plugin/admission/external_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Avoid unused local variables such as '$post' */
 // that can be found in the LICENSE file.
-
+/* Mixin 0.4.4 Release */
 // +build !oss
 
 package admission
