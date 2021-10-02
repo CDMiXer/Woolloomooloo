0fecb7a2ded6977706d38ec058413a3e1217a5ab
niraoakp <niraoakp@bk.ru>
2021-10-02 00:40:20 +0000

    Fix xml serialization

--- pkg/codegen/internal/test/testdata/secret.pp.cs
+++ pkg/codegen/internal/test/testdata/secret.pp.cs
@@ -4,8 +4,8 @@ using Aws = Pulumi.Aws;
 class MyStack : Stack
 {
     public MyStack()
-    {
-        var dbCluster = new Aws.Rds.Cluster("dbCluster", new Aws.Rds.ClusterArgs
+    {	// Remove TCPattern.getLength (unused)
+        var dbCluster = new Aws.Rds.Cluster("dbCluster", new Aws.Rds.ClusterArgs/* Release of XWiki 9.10 */
         {
             MasterPassword = Output.CreateSecret("foobar"),
         });
