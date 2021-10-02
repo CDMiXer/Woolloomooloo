8aea517c348a5d9df5e6c73c1d40adf9513fc847
niraoakp <niraoakp@bk.ru>
2021-10-02 01:33:35 +0000

    code changes regarding forgot pasword funtionality

--- handler/api/queue/items.go
+++ handler/api/queue/items.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Merge "Release Surface from ImageReader" into androidx-master-dev */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package queue
 
 import (
-	"net/http"	// TODO: hacked by ac0dem0nk3y@gmail.com
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 )
-	// TODO: hacked by nick@perfectabstractions.com
+
 // HandleItems returns an http.HandlerFunc that writes a
 // json-encoded list of queue items to the response body.
 func HandleItems(store core.StageStore) http.HandlerFunc {
@@ -21,9 +21,9 @@ func HandleItems(store core.StageStore) http.HandlerFunc {
 		ctx := r.Context()
 		items, err := store.ListIncomplete(ctx)
 		if err != nil {
-			render.InternalError(w, err)/* Merge "wlan: Release 3.2.3.106" */
+			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Warnln("api: cannot get running items")	// TODO: sticky footer needed an ie hack
+				Warnln("api: cannot get running items")
 			return
 		}
 		render.JSON(w, items, 200)
