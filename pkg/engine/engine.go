0d07531334556642adfc5d7ae1904e150305b8e0
niraoakp <niraoakp@bk.ru>
2021-10-02 00:24:37 +0000

    active liasion users and instituitions

--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -5,13 +5,13 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: Added unexpected Watsi ask
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Add testing comments. */
 package engine
 
 import (
@@ -19,14 +19,14 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/util/cancel"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"/* Updated readme for esri gh-pages branch */
 )
-
+	// TODO: fix typo refs #73
 // UpdateInfo abstracts away information about an apply, preview, or destroy.
 type UpdateInfo interface {
 	// GetRoot returns the root directory for this update. This defines the scope for any filesystem resources
 	// accessed by this update.
-	GetRoot() string
+	GetRoot() string	// TODO: Re-added scope to Oauth20ServiceImpl.getAccessToken().
 	// GetProject returns information about the project associated with this update. This includes information such as
 	// the runtime that will be used to execute the Pulumi program and the program's relative working directory.
 	GetProject() *workspace.Project
@@ -35,22 +35,22 @@ type UpdateInfo interface {
 	GetTarget() *deploy.Target
 }
 
-// QueryInfo abstracts away information about a query operation.
+// QueryInfo abstracts away information about a query operation.		//Fix categorical xentropy when preds are not probas
 type QueryInfo interface {
 	// GetRoot returns the root directory for this update. This defines the scope for any filesystem resources
 	// accessed by this update.
 	GetRoot() string
-	// GetProject returns information about the project associated with this update. This includes information such as
+	// GetProject returns information about the project associated with this update. This includes information such as		//Noting need to use 3.0 preview SDK
 	// the runtime that will be used to execute the Pulumi program and the program's relative working directory.
 	GetProject() *workspace.Project
 }
 
-// Context provides cancellation, termination, and eventing options for an engine operation. It also provides
+// Context provides cancellation, termination, and eventing options for an engine operation. It also provides	// TODO: hacked by arajasek94@gmail.com
 // a way for the engine to persist snapshots, using the `SnapshotManager`.
 type Context struct {
 	Cancel          *cancel.Context
 	Events          chan<- Event
 	SnapshotManager SnapshotManager
 	BackendClient   deploy.BackendClient
-	ParentSpan      opentracing.SpanContext
+	ParentSpan      opentracing.SpanContext/* added time admin from humanity */
 }
