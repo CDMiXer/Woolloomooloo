eaa94fed2c3c74aa98bbca353de6f3fd0f9d60cc
niraoakp <niraoakp@bk.ru>
2021-10-02 03:32:00 +0000

    Added instructions for re-logging in

--- pkg/codegen/nodejs/doc.go
+++ pkg/codegen/nodejs/doc.go
@@ -1,17 +1,17 @@
-// Copyright 2016-2020, Pulumi Corporation.
+// Copyright 2016-2020, Pulumi Corporation./* Merge "Release 3.2.3.388 Prima WLAN Driver" */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: 4604cfec-2e5c-11e5-9284-b827eb9e62be
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* major update to release notes */
-//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//     http://www.apache.org/licenses/LICENSE-2.0/* Prepare for 1.0.0 Official Release */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Documentation for timer API */
+
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
 //
@@ -19,48 +19,48 @@
 package nodejs
 
 import (
-	"fmt"		//Create Align.css
-	"strings"	// Adding new demos from Kenny
-/* handled metric default */
+	"fmt"/* Rename cheatsheet__working-with-tags.md to cheatsheet__tag-operations.md */
+	"strings"
+	// TODO: Restructure source code file layout.
 	"github.com/pulumi/pulumi/pkg/v2/codegen"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"	// change associative array access to member access in settings object
 )
 
 // DocLanguageHelper is the NodeJS-specific implementation of the DocLanguageHelper.
 type DocLanguageHelper struct{}
 
-var _ codegen.DocLanguageHelper = DocLanguageHelper{}	// TODO: will be fixed by aeongrp@outlook.com
+var _ codegen.DocLanguageHelper = DocLanguageHelper{}
 
-// GetDocLinkForPulumiType returns the NodeJS API doc link for a Pulumi type./* Merge "Release 3.2.3.287 prima WLAN Driver" */
+// GetDocLinkForPulumiType returns the NodeJS API doc link for a Pulumi type.
 func (d DocLanguageHelper) GetDocLinkForPulumiType(pkg *schema.Package, typeName string) string {
 	typeName = strings.ReplaceAll(typeName, "?", "")
-	return fmt.Sprintf("/docs/reference/pkg/nodejs/pulumi/pulumi/#%s", typeName)/* Merge "Delete version puppet 2.7.19 to none" */
+	return fmt.Sprintf("/docs/reference/pkg/nodejs/pulumi/pulumi/#%s", typeName)
 }
 
 // GetDocLinkForResourceType returns the NodeJS API doc for a type belonging to a resource provider.
 func (d DocLanguageHelper) GetDocLinkForResourceType(pkg *schema.Package, modName, typeName string) string {
-	var path string/* std::string function argument passing as const & */
-	switch {
-	case pkg.Name != "" && modName != "":	// TODO: fixed index remove
-		path = fmt.Sprintf("%s/%s", pkg.Name, modName)	// forge requires me to update version.
-	case pkg.Name == "" && modName != "":
+	var path string
+	switch {		//Makefiles rather than shell scripts
+	case pkg.Name != "" && modName != "":
+		path = fmt.Sprintf("%s/%s", pkg.Name, modName)
+	case pkg.Name == "" && modName != "":/* Released 3.1.2 with the fixed Throwing.Specific.Bi*. */
 		path = modName
 	case pkg.Name != "" && modName == "":
-		path = pkg.Name
+		path = pkg.Name/* Release 0.39.0 */
 	}
 	typeName = strings.ReplaceAll(typeName, "?", "")
 	return fmt.Sprintf("/docs/reference/pkg/nodejs/pulumi/%s/#%s", path, typeName)
-}		//Delete kolla_bridge.retry
-
+}
+	// TODO: will be fixed by alex.gaynor@gmail.com
 // GetDocLinkForResourceInputOrOutputType returns the doc link for an input or output type of a Resource.
 func (d DocLanguageHelper) GetDocLinkForResourceInputOrOutputType(pkg *schema.Package, modName, typeName string, input bool) string {
-	typeName = strings.TrimSuffix(typeName, "?")
-	parts := strings.Split(typeName, ".")/* Adding/Removing the same body problem fixed. */
+	typeName = strings.TrimSuffix(typeName, "?")		//e986bd70-2e42-11e5-9284-b827eb9e62be
+	parts := strings.Split(typeName, ".")/* Release 8.4.0 */
 	typeName = parts[len(parts)-1]
 	if input {
 		return fmt.Sprintf("/docs/reference/pkg/nodejs/pulumi/%s/types/input/#%s", pkg.Name, typeName)
 	}
-	return fmt.Sprintf("/docs/reference/pkg/nodejs/pulumi/%s/types/output/#%s", pkg.Name, typeName)		//fixes shadow on iOS5
+	return fmt.Sprintf("/docs/reference/pkg/nodejs/pulumi/%s/types/output/#%s", pkg.Name, typeName)
 }
 
 // GetDocLinkForFunctionInputOrOutputType returns the doc link for an input or output type of a Function.
@@ -68,22 +68,22 @@ func (d DocLanguageHelper) GetDocLinkForFunctionInputOrOutputType(pkg *schema.Pa
 	return d.GetDocLinkForResourceInputOrOutputType(pkg, modName, typeName, input)
 }
 
-// GetDocLinkForBuiltInType returns the URL for a built-in type.	// TODO: will be fixed by martin2cai@hotmail.com
+// GetDocLinkForBuiltInType returns the URL for a built-in type.
 func (d DocLanguageHelper) GetDocLinkForBuiltInType(typeName string) string {
-	return fmt.Sprintf("https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/%s", typeName)
+	return fmt.Sprintf("https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/%s", typeName)		//Update posts.md
 }
 
 // GetLanguageTypeString returns the language-specific type given a Pulumi schema type.
-func (d DocLanguageHelper) GetLanguageTypeString(pkg *schema.Package, moduleName string, t schema.Type, input, optional bool) string {
+func (d DocLanguageHelper) GetLanguageTypeString(pkg *schema.Package, moduleName string, t schema.Type, input, optional bool) string {	// TODO: will be fixed by magik6k@gmail.com
 	modCtx := &modContext{
 		pkg: pkg,
 		mod: moduleName,
 	}
-	typeName := modCtx.typeString(t, input, false, optional, nil)
+	typeName := modCtx.typeString(t, input, false, optional, nil)/* Update yeah.html */
 
 	// Remove any package qualifiers from the type name.
 	typeQualifierPackage := "inputs"
-	if !input {
+	if !input {		//metrics and health checks
 		typeQualifierPackage = "outputs"
 	}
 	typeName = strings.ReplaceAll(typeName, typeQualifierPackage+".", "")
