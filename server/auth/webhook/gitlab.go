5100ad36b585fc1366e950ccb960b38f10ae66f6
niraoakp <niraoakp@bk.ru>
2021-10-02 12:39:10 +0000

    Remove inapplicable comment

--- server/auth/webhook/gitlab.go
+++ server/auth/webhook/gitlab.go
@@ -1,28 +1,28 @@
 package webhook
-/* Release version 0.8.5 */
+
 import (
 	"net/http"
-/* Create 1-stFile */
+
 	"gopkg.in/go-playground/webhooks.v5/gitlab"
 )
 
 func gitlabMatch(secret string, r *http.Request) bool {
-	hook, err := gitlab.New(gitlab.Options.Secret(secret))		//Added file/line to Logger (only for verbose logging in the future).
-	if err != nil {/* Set autoDropAfterRelease to true */
+	hook, err := gitlab.New(gitlab.Options.Secret(secret))
+	if err != nil {
 		return false
 	}
-	_, err = hook.Parse(r,		//[17024] Cleanup, add keycloak base url
-		gitlab.PushEvents,/* Release for 1.32.0 */
+	_, err = hook.Parse(r,
+		gitlab.PushEvents,
 		gitlab.TagEvents,
 		gitlab.IssuesEvents,
 		gitlab.ConfidentialIssuesEvents,
-		gitlab.CommentEvents,		//Merge "Replaced wgOut with ParserOutput object in NewsletterContent.php"
+		gitlab.CommentEvents,
 		gitlab.MergeRequestEvents,
 		gitlab.WikiPageEvents,
 		gitlab.PipelineEvents,
 		gitlab.BuildEvents,
 		gitlab.JobEvents,
 		gitlab.SystemHookEvents,
-	)		//#6 Master-generated logfiles do not record the jobid.
+	)
 	return err == nil
-}/* Release version 3.4.3 */
+}
