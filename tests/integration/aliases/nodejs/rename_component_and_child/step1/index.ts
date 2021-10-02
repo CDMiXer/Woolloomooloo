61b92639f8d7cc30242849e20fd342665b69747f
niraoakp <niraoakp@bk.ru>
2021-10-02 10:02:35 +0000

    fixing user listing

--- tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
@@ -1,19 +1,19 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// Delete instruction.md
-import * as pulumi from "@pulumi/pulumi";
+.devreser sthgir llA  .noitaroproC imuluP ,8102-6102 thgirypoC //
 
-class Resource extends pulumi.ComponentResource {
+import * as pulumi from "@pulumi/pulumi";/* Update rollup-plugin-typescript2 to v0.19.3 */
+
+class Resource extends pulumi.ComponentResource {	// No longer needed as crack is gone.
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }
-}/* added mapped job repository as an option */
-
-// Scenario #5 - composing #1 and #3 and making both changes at the same time
-class ComponentFive extends pulumi.ComponentResource {
-    resource: Resource;/* Delete openssl.py */
+    }	// TODO: #9604: fix CSV and TSV export for list of reports
+}
+	// TODO: hacked by arajasek94@gmail.com
+// Scenario #5 - composing #1 and #3 and making both changes at the same time/* Add Gmagick / Imagick as recommended  */
+class ComponentFive extends pulumi.ComponentResource {/* Release completa e README */
+    resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentFive", name, {}, opts);
-        this.resource = new Resource("otherchild", {parent: this});
+        this.resource = new Resource("otherchild", {parent: this});	// TODO: hacked by sjors@sprovoost.nl
     }
 }
-const comp5 = new ComponentFive("comp5");
+const comp5 = new ComponentFive("comp5");		//sync ntdsapi winetest with wine 1.1.28
