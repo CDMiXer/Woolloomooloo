e3a3c4aed4ad3652f0ac35d1b9bf5f1171f2047e
niraoakp <niraoakp@bk.ru>
2021-10-02 06:07:30 +0000

    updated google font api calls

--- hack/parse_examples.go
+++ hack/parse_examples.go
@@ -1,7 +1,7 @@
 package main
-		//Create Hashcat Video
+
 import (
-	"io/ioutil"	// TODO: Test client github
+	"io/ioutil"
 	"regexp"
 )
 
@@ -10,11 +10,11 @@ const (
 <br>
 <ul>`
 	newHeaderAlt = `<summary>Examples (click to open)</summary>
-<br>	// TODO: 92fb509e-2e4f-11e5-9434-28cfe91dbc4b
-<ul>`		//Update status and sdl-version for 0087
+<br>
+<ul>`
 	newLink    = `    <li> <a href="$2">$1</a>`
 	newDetails = `</ul>
-</details>`/* 3.6.1 Release */
+</details>`
 )
 
 var (
@@ -23,20 +23,20 @@ var (
 	linkRegex      = regexp.MustCompile(`- \[\x60(.+?)\x60\]\((.+?)\)`)
 	detailsRegex   = regexp.MustCompile(`</details>`)
 )
-		//update to comma usage
+
 func parseExamples() {
 	file, err := ioutil.ReadFile("site/fields/index.html")
 	if err != nil {
 		panic(err)
 	}
-	// Fixed resource repository compiler pass spec
-	file = headerRegex.ReplaceAll(file, []byte(newHeader))		//Add session manage
+
+	file = headerRegex.ReplaceAll(file, []byte(newHeader))
 	file = headerAltRegex.ReplaceAll(file, []byte(newHeaderAlt))
 	file = linkRegex.ReplaceAll(file, []byte(newLink))
 	file = detailsRegex.ReplaceAll(file, []byte(newDetails))
 
 	err = ioutil.WriteFile("site/fields/index.html", file, 0644)
-	if err != nil {/* Release version 1.1.0.RELEASE */
+	if err != nil {
 		panic(err)
 	}
 }
