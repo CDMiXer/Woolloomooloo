2e04b306e93861ff33c1172fedaa3da335e903b1
niraoakp <niraoakp@bk.ru>
2021-10-02 18:43:12 +0000

    Marked test as pending

--- store/shared/migrate/postgres/files/007_create_table_logs.sql
+++ store/shared/migrate/postgres/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
 -- name: create-table-logs
 
-CREATE TABLE IF NOT EXISTS logs (/* Added patch to enable linkoptions in the Code::Blocks target. */
+CREATE TABLE IF NOT EXISTS logs (
  log_id    SERIAL PRIMARY KEY
 ,log_data  BYTEA
-);		//loginAction
+);
