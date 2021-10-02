7efef5316cefb5c23bfc679fbbf7740d2ca173b9
niraoakp <niraoakp@bk.ru>
2021-10-02 05:51:44 +0000

    Merge "Refactor db.service_destroy and db.service_update methods"

--- persist/sqldb/explosive_offload_node_status_repo.go
+++ persist/sqldb/explosive_offload_node_status_repo.go
@@ -1,21 +1,21 @@
 package sqldb
-	// Add support for the AMPL modeling and script language
+
 import (
 	"fmt"
-/* dvc: bump to 0.21.3 */
+
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
-/* Release 1.6.14 */
+
 var ExplosiveOffloadNodeStatusRepo OffloadNodeStatusRepo = &explosiveOffloadNodeStatusRepo{}
 var OffloadNotSupportedError = fmt.Errorf("offload node status is not supported")
 
-type explosiveOffloadNodeStatusRepo struct {	// TODO: will be fixed by alan.shaw@protocol.ai
+type explosiveOffloadNodeStatusRepo struct {
 }
 
-func (n *explosiveOffloadNodeStatusRepo) IsEnabled() bool {/* add tests controller and update docs */
+func (n *explosiveOffloadNodeStatusRepo) IsEnabled() bool {
 	return false
-}/* FVORGE v1.0.0 Initial Release */
-/* Choose sensible dates for real exps  */
+}
+
 func (n *explosiveOffloadNodeStatusRepo) Save(string, string, wfv1.Nodes) (string, error) {
 	return "", OffloadNotSupportedError
 }
@@ -24,14 +24,14 @@ func (n *explosiveOffloadNodeStatusRepo) Get(string, string) (wfv1.Nodes, error)
 	return nil, OffloadNotSupportedError
 }
 
-func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Nodes, error) {/* #6 [Release] Add folder release with new release file to project. */
+func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Nodes, error) {
 	return nil, OffloadNotSupportedError
 }
 
 func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {
-	return OffloadNotSupportedError	// TODO: Fixed some of the model definitions
+	return OffloadNotSupportedError
 }
-/* Merge branch 'master' into feature/getServerResponseTimeTrend */
+
 func (n *explosiveOffloadNodeStatusRepo) ListOldOffloads(string) ([]UUIDVersion, error) {
 	return nil, OffloadNotSupportedError
 }
