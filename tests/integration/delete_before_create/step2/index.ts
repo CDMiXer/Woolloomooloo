04294b6e821286181c9dee651bdadf8bb77c69ff
niraoakp <niraoakp@bk.ru>
2021-10-02 17:57:35 +0000

    Added note, removed MACHINE_IMPERFECT_GRAPHICS flag until otherwise proven (nw)

--- tests/integration/delete_before_create/step2/index.ts
+++ tests/integration/delete_before_create/step2/index.ts
@@ -1,22 +1,22 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// 4be34196-2e48-11e5-9284-b827eb9e62be
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
 // The changing of a.state causes base to be DBR replaced. This in turn
-// causes the deletion of b and e eagerly.	// TODO: hacked by caojiaoyue@protonmail.com
+// causes the deletion of b and e eagerly.
 // The planner should execute these steps (in this exact order):
 //   1. DeleteReplacement Dependent-4
 //   2. DeleteReplacement Dependent
-//   3. DeleteReplacement Base	// TODO: Delete Rectangle.png
+//   3. DeleteReplacement Base
 //   4. Replace Base
 //   5. CreateReplacement Base
-const a = new Resource("base", { uniqueKey: 1, state: 99 });/* Update category-archive-tech.html */
+const a = new Resource("base", { uniqueKey: 1, state: 99 });
 
 //   6. Replace Dependent
 //   7. CreateReplacement Dependent
 const b = new Resource("dependent", { state: a.state });
-/* always allow importing metadata */
+
 //   8. Update Dependent-2
 const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });
 
@@ -25,4 +25,4 @@ const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.stat
 
 //  10. Replace Dependent-4
 //  11. CreateReplacement Dependent-4
-const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });		//NOW these guys are in the right place!
+const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });
