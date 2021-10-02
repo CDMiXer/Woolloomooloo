efa696e1df8e6d69338f72a4351b3605aafde8ab
niraoakp <niraoakp@bk.ru>
2021-10-02 10:29:53 +0000

    Merge "Add a RHS status bar slot for NFC." into gingerbread

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -8,13 +8,13 @@ CREATE TABLE IF NOT EXISTS secrets (
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
-,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* Devops & Release mgmt */
 );
 
 -- name: create-index-secrets-repo
 
-CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
+CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);		//Add railties dependency
 
 -- name: create-index-secrets-repo-name
-
+		//Update deriva-acl-config.md
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
