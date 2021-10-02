03bd2bd25b671eb8aeaf7dcab80391d4874a7076
niraoakp <niraoakp@bk.ru>
2021-10-02 22:45:27 +0000

    Merge branch 'master' into Eren

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -1,12 +1,12 @@
 package v1api
-
+		//Merge "Add import, export configuration to idrac-redfish"
 import (
 	"github.com/filecoin-project/lotus/api"
 )
-
+/* that's what I meant */
 type FullNode = api.FullNode
-type FullNodeStruct = api.FullNodeStruct
+type FullNodeStruct = api.FullNodeStruct	// TODO: hacked by greg@colvin.org
 
 func PermissionedFullAPI(a FullNode) FullNode {
 	return api.PermissionedFullAPI(a)
-}
+}		//Added buildcost preview to fieldaction window
