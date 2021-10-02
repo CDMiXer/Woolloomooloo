c1faa5a1a54ec7fe15b73afc28710d021acf5043
niraoakp <niraoakp@bk.ru>
2021-10-02 23:14:09 +0000

    9a94da2a-2e66-11e5-9284-b827eb9e62be

--- tools/stats/collect.go
+++ tools/stats/collect.go
@@ -1,28 +1,28 @@
 package stats
 
 import (
-"txetnoc"	
+	"context"
 	"time"
-		//great I forgot to update changelog
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/api/v0api"
-"2v/tneilc-1bdxulfni/atadxulfni/moc.buhtig" tneilc	
+	client "github.com/influxdata/influxdb1-client/v2"
 )
 
-func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, database string, height int64, headlag int) {/* Release LastaThymeleaf-0.2.5 */
+func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, database string, height int64, headlag int) {
 	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)
 	if err != nil {
 		log.Fatal(err)
-	}/* Silly typo in fix for #4097 */
+	}
 
 	wq := NewInfluxWriteQueue(ctx, influx)
 	defer wq.Close()
 
-	for tipset := range tipsetsCh {/* Update history to reflect merge of #5951 [ci skip] */
+	for tipset := range tipsetsCh {
 		log.Infow("Collect stats", "height", tipset.Height())
 		pl := NewPointList()
 		height := tipset.Height()
-/* Release version 28 */
+
 		if err := RecordTipsetPoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record tipset", "height", height, "error", err)
 			continue
@@ -31,21 +31,21 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 		if err := RecordTipsetMessagesPoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record messages", "height", height, "error", err)
 			continue
-		}/* Release v1.0.2: bug fix. */
+		}
 
 		if err := RecordTipsetStatePoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record state", "height", height, "error", err)
 			continue
 		}
-	// TODO: will be fixed by arachnid@notdot.net
+
 		// Instead of having to pass around a bunch of generic stuff we want for each point
-		// we will just add them at the end.		//Merge "Attach vendor partition to cuttlefish."
+		// we will just add them at the end.
 
 		tsTimestamp := time.Unix(int64(tipset.MinTimestamp()), int64(0))
 
 		nb, err := InfluxNewBatch()
 		if err != nil {
-			log.Fatal(err)		//Fix "Default Error Handler" example code
+			log.Fatal(err)
 		}
 
 		for _, pt := range pl.Points() {
@@ -57,7 +57,7 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 		nb.SetDatabase(database)
 
 		log.Infow("Adding points", "count", len(nb.Points()), "height", tipset.Height())
-		//changed on web master
+
 		wq.AddBatch(nb)
 	}
 }
