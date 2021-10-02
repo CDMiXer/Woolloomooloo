bbf937952d38c7fd3c3031eeff6a3ee3f0f47c37
niraoakp <niraoakp@bk.ru>
2021-10-02 21:14:41 +0000

    Rrefactoring

--- tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step2/Program.cs
@@ -6,7 +6,7 @@ using Pulumi;
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)		//Merge branch 'master' into 30477_sample_material_dialog
+        : base("my:module:Resource", name, options)
     {
     }
 }
@@ -17,12 +17,12 @@ class ComponentFive : ComponentResource
     private Resource resource;
 
     public ComponentFive(string name, ComponentResourceOptions options = null)
-        : base("my:module:ComponentFive", name, options)	// TODO: will be fixed by cory@protocol.ai
+        : base("my:module:ComponentFive", name, options)
     {
         this.resource = new Resource("otherchildrenamed", new ComponentResourceOptions
-        { 		//Delete EmbConstant.java
+        { 
             Parent = this,
-            Aliases = { { new Alias { Name = "otherchild", Parent = this } } },		//MIR-716 rename Inscriber -> MetadataManager
+            Aliases = { { new Alias { Name = "otherchild", Parent = this } } },
         });
     }
 }
@@ -32,7 +32,7 @@ class Program
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
-        {/* Merge pull request #43 from janstey/ENTESB-2300 */
+        {
             var comp5 = new ComponentFive("newcomp5", new ComponentResourceOptions
             {
                 Aliases = { new Alias { Name = "comp5" } },
