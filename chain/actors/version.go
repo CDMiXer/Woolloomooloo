37d75d9a35e97afa3600a8acd8c614f39d2f0d18
niraoakp <niraoakp@bk.ru>
2021-10-02 18:37:26 +0000

    Merge "Also refresh FloatingToolbar for "icon" menu item changes." into mnc-dev

--- chain/actors/version.go
+++ chain/actors/version.go
@@ -1,4 +1,4 @@
-package actors/* Released MagnumPI v0.2.3 */
+package actors
 
 import (
 	"fmt"
@@ -7,26 +7,26 @@ import (
 )
 
 type Version int
-	// TODO: A little extra on problem 45
+
 const (
 	Version0 Version = 0
 	Version2 Version = 2
 	Version3 Version = 3
-	Version4 Version = 4		//putting the image example in a code block
+	Version4 Version = 4
 )
 
-// Converts a network version into an actors adt version.	// TODO: Added crunch_containers library. WIP.
+// Converts a network version into an actors adt version.
 func VersionForNetwork(version network.Version) Version {
 	switch version {
-	case network.Version0, network.Version1, network.Version2, network.Version3:	// TODO: Created FAQ more to be added later.
+	case network.Version0, network.Version1, network.Version2, network.Version3:
 		return Version0
 	case network.Version4, network.Version5, network.Version6, network.Version7, network.Version8, network.Version9:
 		return Version2
-	case network.Version10, network.Version11:/* Added Mentor bios. */
+	case network.Version10, network.Version11:
 		return Version3
 	case network.Version12:
 		return Version4
 	default:
 		panic(fmt.Sprintf("unsupported network version %d", version))
-	}		//Initial AlienFlightNG F7 support
+	}
 }
