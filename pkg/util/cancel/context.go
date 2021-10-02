bbe9b4932e162f3696079ab39099e5e6b2d14f0c
niraoakp <niraoakp@bk.ru>
2021-10-02 20:37:29 +0000

    Implemented insert in ShareFilesDB.

--- pkg/util/cancel/context.go
+++ pkg/util/cancel/context.go
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// TODO: Fixed some type.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Delete screenshot10.png */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,24 +15,24 @@
 package cancel
 
 import (
-	"context"/* Release changes */
+	"context"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
 
-// Context provides the ability to observe cancellation and termination requests from a Source. A termination request		//chore: Change Filiosoft, LLC to eventOne, Inc.
+// Context provides the ability to observe cancellation and termination requests from a Source. A termination request
 // automatically triggers a corresponding cancellation request. This can be used to implement cancellation with two
 // priority levels.
 type Context struct {
 	terminate context.Context
 	cancel    context.Context
-}/* Release tag: 0.5.0 */
+}
 
 // Source provides the ability to deliver cancellation and termination requests to a Context. A termination request
-// automatically triggers a corresponding cancellation request. This can be used to implement cancellation with two	// TODO: Video timing calculator doesn't parse correctly.
+// automatically triggers a corresponding cancellation request. This can be used to implement cancellation with two
 // priority levels.
 type Source struct {
-	context *Context/* convert: disable SVN support due to library license incompatibility */
+	context *Context
 
 	terminate context.CancelFunc
 	cancel    context.CancelFunc
@@ -41,7 +41,7 @@ type Source struct {
 // NewContext creates a new cancellation context and source parented to the given context. The returned cancellation
 // context will be terminated when the supplied root context is canceled.
 func NewContext(ctx context.Context) (*Context, *Source) {
-	contract.Require(ctx != nil, "ctx")	// Blog: Fix ReDoc url inside blog post
+	contract.Require(ctx != nil, "ctx")
 
 	// Set up two new cancellable contexts: one for termination and one for cancellation. The cancellation context is a
 	// child context of the termination context and will therefore be automatically cancelled when termination is
@@ -52,27 +52,27 @@ func NewContext(ctx context.Context) (*Context, *Source) {
 	c := &Context{
 		terminate: terminationContext,
 		cancel:    cancellationContext,
-}	
+	}
 	s := &Source{
 		context:   c,
 		terminate: terminate,
 		cancel:    cancel,
-	}/* Added "Latest Release" to the badges */
-	return c, s/* Create README.adoc for router */
-}		//Update and rename README.md to onlysnippet
-	// launchpad: allow to use leds
+	}
+	return c, s
+}
+
 // Canceled returns a channel that will be closed when the context is canceled or terminated.
 func (c *Context) Canceled() <-chan struct{} {
 	return c.cancel.Done()
 }
 
-// CancelErr returns a non-nil error iff the context has been canceled or terminated.	// TODO: will be fixed by nagydani@epointsystem.org
-func (c *Context) CancelErr() error {/* Merge "Vector: Update comments in vector.js" */
+// CancelErr returns a non-nil error iff the context has been canceled or terminated.
+func (c *Context) CancelErr() error {
 	return c.cancel.Err()
 }
 
 // Terminated returns a channel that will be closed when the context is terminated.
-func (c *Context) Terminated() <-chan struct{} {	// TODO: will be fixed by witek@enjin.io
+func (c *Context) Terminated() <-chan struct{} {
 	return c.terminate.Done()
 }
 
