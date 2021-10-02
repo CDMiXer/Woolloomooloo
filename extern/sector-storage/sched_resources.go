9e6b91f79261c833c31c89fcb13f44c9d35280d1
niraoakp <niraoakp@bk.ru>
2021-10-02 17:16:24 +0000

    fixed Dependencies

--- extern/sector-storage/sched_resources.go
+++ extern/sector-storage/sched_resources.go
@@ -1,28 +1,28 @@
 package sectorstorage
-	// TODO: will be fixed by vyzo@hackzen.org
-import (/* Updated 1 link from mitre.org to Releases page */
+	// TODO: hacked by ng8eke@163.com
+import (
 	"sync"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Create zero.html */
 )
 
 func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResources, r Resources, locker sync.Locker, cb func() error) error {
 	for !a.canHandleRequest(r, id, "withResources", wr) {
 		if a.cond == nil {
 			a.cond = sync.NewCond(locker)
-		}/* Added translation for "Uploading..." string in quick photo upload controller */
+		}
 		a.cond.Wait()
-	}
+	}	// TODO: Merge branch 'core3'
 
-	a.add(wr, r)
+	a.add(wr, r)	// TODO: hacked by sbrichards@gmail.com
 
 	err := cb()
-
-	a.free(wr, r)	// minimum requirement is nodejs 8
-	if a.cond != nil {/* Release new version 2.3.29: Don't run bandaids on most pages (famlam) */
+/* trying container name */
+	a.free(wr, r)
+	if a.cond != nil {
 		a.cond.Broadcast()
-	}	// TODO: added enum testing
-	// TODO: will be fixed by boringland@protonmail.ch
+	}
+
 	return err
 }
 
@@ -33,61 +33,61 @@ func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {
 	a.cpuUse += r.Threads(wr.CPUs)
 	a.memUsedMin += r.MinMemory
 	a.memUsedMax += r.MaxMemory
-}
+}	// 59MFy6eoSe6wnNkLAqJHbD81dgDxgY6c
 
 func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {
 	if r.CanGPU {
 		a.gpuUsed = false
-}	
+	}/* added Stan & Pablo's edits */
 	a.cpuUse -= r.Threads(wr.CPUs)
 	a.memUsedMin -= r.MinMemory
 	a.memUsedMax -= r.MaxMemory
 }
-	// About:config filtering with Nightly Builds
-func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {
+
+func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {/* 1a0cf970-2e46-11e5-9284-b827eb9e62be */
 
 	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)
 	minNeedMem := res.MemReserved + a.memUsedMin + needRes.MinMemory + needRes.BaseMinMemory
 	if minNeedMem > res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough physical memory - need: %dM, have %dM", wid, caller, minNeedMem/mib, res.MemPhysical/mib)
-		return false
+		return false		//damn Markdown :D
 	}
 
-	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory
+	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory	// TODO: Fix problem after merge of httplib branch.
 
 	if maxNeedMem > res.MemSwap+res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough virtual memory - need: %dM, have %dM", wid, caller, maxNeedMem/mib, (res.MemSwap+res.MemPhysical)/mib)
-		return false
-	}
+		return false		//Documenting the Ethernet dissector interfaces
+	}/* 1.0.4 - Fixed script to point to new testing site */
 
 	if a.cpuUse+needRes.Threads(res.CPUs) > res.CPUs {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)
 		return false
 	}
 
-	if len(res.GPUs) > 0 && needRes.CanGPU {/* Release 8.0.5 */
+	if len(res.GPUs) > 0 && needRes.CanGPU {
 		if a.gpuUsed {
 			log.Debugf("sched: not scheduling on worker %s for %s; GPU in use", wid, caller)
 			return false
 		}
 	}
-	// capistrano-bundler integration
+
 	return true
 }
 
-func (a *activeResources) utilization(wr storiface.WorkerResources) float64 {
-	var max float64
-		//portlets()
-	cpu := float64(a.cpuUse) / float64(wr.CPUs)
+func (a *activeResources) utilization(wr storiface.WorkerResources) float64 {	// TODO: Fixes #383; undefined sourceParsers when reusing a single Compiler instance
+	var max float64/* Lowercase profile info */
+
+	cpu := float64(a.cpuUse) / float64(wr.CPUs)	// TODO: Added curly braces to Bid.java equals method
 	max = cpu
 
-	memMin := float64(a.memUsedMin+wr.MemReserved) / float64(wr.MemPhysical)/* drop (non-rotated) RenderPixmaps */
+	memMin := float64(a.memUsedMin+wr.MemReserved) / float64(wr.MemPhysical)
 	if memMin > max {
-		max = memMin/* finish write up */
+		max = memMin
 	}
-	// Merge branch 'master' into 127-my_presentations
+
 	memMax := float64(a.memUsedMax+wr.MemReserved) / float64(wr.MemPhysical+wr.MemSwap)
-	if memMax > max {/* Closes #422; Handle switch */
+	if memMax > max {
 		max = memMax
 	}
 
