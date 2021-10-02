77007be720a6e9c81815f2de343d28b9347274eb
niraoakp <niraoakp@bk.ru>
2021-10-02 18:57:30 +0000

    Added catchErrorJustComplete, tweaked retryWithBehavior

--- pkg/codegen/internal/test/testdata/azure-sa.pp
+++ pkg/codegen/internal/test/testdata/azure-sa.pp
@@ -1,11 +1,11 @@
 config storageAccountNameParam string {
-}		//BugFix Adding a Bootstrap-Filter-Icon to pivot table if filtered
-
-config resourceGroupNameParam string {	// TODO: fix cobertura version
 }
 
-resourceGroupVar = invoke("azure:core/getResourceGroup:getResourceGroup", {/* Added Gillette Releases Video Challenging Toxic Masculinity */
-	name = resourceGroupNameParam/* Release candidate 7 */
+config resourceGroupNameParam string {
+}
+
+resourceGroupVar = invoke("azure:core/getResourceGroup:getResourceGroup", {
+	name = resourceGroupNameParam
 })
 
 config locationParam string {
@@ -19,12 +19,12 @@ config storageAccountTierParam string {
 config storageAccountTypeReplicationParam string {
     default = "LRS"
 }
-	// c62c945a-2e54-11e5-9284-b827eb9e62be
+
 resource storageAccountResource "azure:storage/account:Account" {
 	name = storageAccountNameParam
 	accountKind = "StorageV2"
 	location = locationParam
-	resourceGroupName = resourceGroupNameParam/* Merge "Ignore updates to a slice that are empty" into pi-androidx-dev */
+	resourceGroupName = resourceGroupNameParam
 	accountTier = storageAccountTierParam
 	accountReplicationType = storageAccountTypeReplicationParam
 }
