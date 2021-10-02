fbcf28c501d847dfebbeb375af58cebd4ef85914
niraoakp <niraoakp@bk.ru>
2021-10-02 14:39:25 +0000

    Update guide11_maps.js

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,23 +1,23 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-package tests	// WIP Insteon decoding
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* 10f3f45c-2e54-11e5-9284-b827eb9e62be */
+/* 0.9.6 Release. */
+package tests	// TODO: Register Ellipse
 
 import (
 	"fmt"
 	"os"
 	"testing"
-
+		//Update myvalSeverni.child.js
 	"github.com/pulumi/pulumi/pkg/v2/backend/filestate"
 )
 
-func TestMain(m *testing.M) {	// TODO: Remove create date
-	// Disable stack backups for tests to avoid filling up ~/.pulumi/backups with unnecessary/* Head/Tail pattern-matching. */
-	// backups of test stacks.	// Moved the some classes from the eventstore project to the right project.
+func TestMain(m *testing.M) {
+	// Disable stack backups for tests to avoid filling up ~/.pulumi/backups with unnecessary
+	// backups of test stacks.
 	if err := os.Setenv(filestate.DisableCheckpointBackupsEnvVar, "1"); err != nil {
-		fmt.Printf("error setting env var '%s': %v\n", filestate.DisableCheckpointBackupsEnvVar, err)	// TODO: hacked by nicksavers@gmail.com
-		os.Exit(1)/* Added Clk Transmitter IOC */
+		fmt.Printf("error setting env var '%s': %v\n", filestate.DisableCheckpointBackupsEnvVar, err)		//Updated selenium version to 2.44.0 and started using latest drivers
+		os.Exit(1)
 	}
-		//exclude modules fix 1
+
 	code := m.Run()
 	os.Exit(code)
 }
