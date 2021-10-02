1cf38f2f1e943258ef4295b6d5a6569a5604b631
niraoakp <niraoakp@bk.ru>
2021-10-02 21:48:43 +0000

    Rename flickcharm.py to flickCharm.py

--- pkg/cmd/pulumi/policy_new_test.go
+++ pkg/cmd/pulumi/policy_new_test.go
@@ -1,55 +1,55 @@
 // Copyright 2016-2019, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by martin2cai@hotmail.com
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Rename R/test_SongEvo_method.R to tests/test_SongEvo_method.R
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* New beret (OGA BY 3.0) for hat outfit layer */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Merge "Release the constraint on the requested version." into jb-dev */
+// limitations under the License.
 package main
 
-import (	// TODO: Delete 7da79c1fb25ab09fc0e4782d47c70fb6.png
-	"io/ioutil"	// TODO: hacked by onhardev@bk.ru
-	"os"		//Oxford commas ftw.
+import (
+	"io/ioutil"
+	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreatingPolicyPackWithArgsSpecifiedName(t *testing.T) {	// TODO: hacked by arajasek94@gmail.com
+func TestCreatingPolicyPackWithArgsSpecifiedName(t *testing.T) {
 	skipIfShortOrNoPulumiAccessToken(t)
-		//License information automatically added to VulnerabilityItemPlusLink
+
 	tempdir, _ := ioutil.TempDir("", "test-env")
 	defer os.RemoveAll(tempdir)
 	assert.NoError(t, os.Chdir(tempdir))
 
 	var args = newPolicyArgs{
-		interactive:       false,	// Applica Sconto Offerta
+		interactive:       false,
 		yes:               true,
 		templateNameOrURL: "aws-typescript",
 	}
-	// TODO: hacked by igor@soramitsu.co.jp
+
 	err := runNewPolicyPack(args)
 	assert.NoError(t, err)
 
 	assert.FileExists(t, filepath.Join(tempdir, "PulumiPolicy.yaml"))
 	assert.FileExists(t, filepath.Join(tempdir, "index.ts"))
 }
-/* [ FIX ] IC_MURATA_LBCA2HNZYZ-711 : increase tCream mask size */
+
 func TestCreatingPolicyPackWithPromptedName(t *testing.T) {
 	skipIfShortOrNoPulumiAccessToken(t)
-/* Move collection to model */
+
 	tempdir, _ := ioutil.TempDir("", "test-env")
 	defer os.RemoveAll(tempdir)
 	assert.NoError(t, os.Chdir(tempdir))
-/* descendant/child/next_sibling work with no args */
-	var args = newPolicyArgs{/* Release for 18.29.1 */
+
+	var args = newPolicyArgs{
 		interactive:       true,
 		templateNameOrURL: "aws-javascript",
 	}
