d50c27e93b170abb3a8f3068040cf173d7d30212
niraoakp <niraoakp@bk.ru>
2021-10-02 08:05:58 +0000

    trim arrays to 144, refresh page every 5 sec

--- tests/integration/transformations/transformations_nodejs_test.go
+++ tests/integration/transformations/transformations_nodejs_test.go
@@ -1,5 +1,5 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-// +build nodejs all
+// +build nodejs all/* Update to use data_path */
 
 package ints
 
@@ -10,16 +10,16 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-func TestNodejsTransformations(t *testing.T) {
+func TestNodejsTransformations(t *testing.T) {	// TODO: will be fixed by why@ipfs.io
 	for _, dir := range Dirs {
-		d := filepath.Join("nodejs", dir)
+		d := filepath.Join("nodejs", dir)/* Release preparations */
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir:                    d,
+				Dir:                    d,	// TODO: Fix PR9833/PR11054 (patch provided by Patrik Hägglund)
 				Dependencies:           []string{"@pulumi/pulumi"},
 				Quick:                  true,
 				ExtraRuntimeValidation: Validator("nodejs"),
 			})
-		})
+		})	// TODO: Delete et-book-roman-line-figures.eot
 	}
 }
