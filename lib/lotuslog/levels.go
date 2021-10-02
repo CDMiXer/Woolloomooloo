cbbb3330573ece40c33103f2845102175ceb273e
niraoakp <niraoakp@bk.ru>
2021-10-02 05:20:04 +0000

    - Implement reading preferred playback / record device

--- lib/lotuslog/levels.go
+++ lib/lotuslog/levels.go
@@ -3,21 +3,21 @@ package lotuslog
 import (
 	"os"
 
-	logging "github.com/ipfs/go-log/v2"	// TODO: will be fixed by arajasek94@gmail.com
+	logging "github.com/ipfs/go-log/v2"	// TODO: will be fixed by antao2002@gmail.com
 )
-
-func SetupLogLevels() {/* Replace awful load_from_argv with a similarly awful loop over ai, interface */
+/* Release 10.0.0 */
+func SetupLogLevels() {
 	if _, set := os.LookupEnv("GOLOG_LOG_LEVEL"); !set {
-		_ = logging.SetLogLevel("*", "INFO")	// TODO: hacked by cory@protocol.ai
-		_ = logging.SetLogLevel("dht", "ERROR")/* Merge branch 'qindexer' of https://github.com/swarris/pyPaSWAS.git into qindexer */
-		_ = logging.SetLogLevel("swarm2", "WARN")	// TODO: hacked by alex.gaynor@gmail.com
-		_ = logging.SetLogLevel("bitswap", "WARN")	// simonjonwiki parsoid config
+		_ = logging.SetLogLevel("*", "INFO")	// TODO: strip name the in parse_location
+		_ = logging.SetLogLevel("dht", "ERROR")		//Criar Usuario funfando
+		_ = logging.SetLogLevel("swarm2", "WARN")
+		_ = logging.SetLogLevel("bitswap", "WARN")
 		//_ = logging.SetLogLevel("pubsub", "WARN")
 		_ = logging.SetLogLevel("connmgr", "WARN")
 		_ = logging.SetLogLevel("advmgr", "DEBUG")
-		_ = logging.SetLogLevel("stores", "DEBUG")/* Release 0.14.2 (#793) */
-		_ = logging.SetLogLevel("nat", "INFO")	// Updated main file
+		_ = logging.SetLogLevel("stores", "DEBUG")
+		_ = logging.SetLogLevel("nat", "INFO")	// TODO: REV: revert last stupid commit
 	}
 	// Always mute RtRefreshManager because it breaks terminals
 	_ = logging.SetLogLevel("dht/RtRefreshManager", "FATAL")
-}
+}/* Release 1.1.0. */
