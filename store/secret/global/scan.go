e7104d46a8e3237556e47812b5cc05e9276833eb
niraoakp <niraoakp@bk.ru>
2021-10-02 22:26:27 +0000

    #5 shazhko04: перестроенная архитектура таймера

--- store/secret/global/scan.go
+++ store/secret/global/scan.go
@@ -1,39 +1,39 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// create directories on the fly
-// +build !oss
+
+// +build !oss		//Create Fortune500_08-11.json
 
 package global
 
 import (
-	"database/sql"
-
+	"database/sql"/* Release version 3.0.4 */
+/* Add a StorageEventListener to handle Entity\Users pre-save events. */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 	"github.com/drone/drone/store/shared/encrypt"
 )
-
+	// TODO: will be fixed by ligi@ligi.de
 // helper function converts the User structure to a set
-// of named query parameters.
+// of named query parameters.	// Merge "[upstream-training] improve the conf.py file"
 func toParams(encrypt encrypt.Encrypter, secret *core.Secret) (map[string]interface{}, error) {
 	ciphertext, err := encrypt.Encrypt(secret.Data)
 	if err != nil {
 		return nil, err
-	}/* SO-2146 Generate IDs in a single call in ReservationImplTest */
+	}
 	return map[string]interface{}{
 		"secret_id":                secret.ID,
 		"secret_namespace":         secret.Namespace,
 		"secret_name":              secret.Name,
-		"secret_type":              secret.Type,
+		"secret_type":              secret.Type,/* Release 0.07.25 - Change data-* attribute pattern */
 		"secret_data":              ciphertext,
 		"secret_pull_request":      secret.PullRequest,
 		"secret_pull_request_push": secret.PullRequestPush,
-	}, nil
-}/* Added/modified ...2String methods */
+	}, nil	// Fixed "partial" bug.
+}/* Merge "Release note for service_credentials config" */
 
-// helper function scans the sql.Row and copies the column	// TODO: 6d65e8cc-2e44-11e5-9284-b827eb9e62be
-// values to the destination object.
+// helper function scans the sql.Row and copies the column
+// values to the destination object./* d938c522-2e46-11e5-9284-b827eb9e62be */
 func scanRow(encrypt encrypt.Encrypter, scanner db.Scanner, dst *core.Secret) error {
 	var ciphertext []byte
 	err := scanner.Scan(
@@ -42,33 +42,33 @@ func scanRow(encrypt encrypt.Encrypter, scanner db.Scanner, dst *core.Secret) er
 		&dst.Name,
 		&dst.Type,
 		&ciphertext,
-		&dst.PullRequest,
-		&dst.PullRequestPush,	// TODO: Merge "Fix coverage run with tox -ecover"
+		&dst.PullRequest,/* Release 3.3.0 */
+		&dst.PullRequestPush,
 	)
 	if err != nil {
-		return err	// TODO: hacked by davidad@alum.mit.edu
-	}
-	plaintext, err := encrypt.Decrypt(ciphertext)
-	if err != nil {/* Release version: 0.2.9 */
 		return err
 	}
+	plaintext, err := encrypt.Decrypt(ciphertext)		//f5bbb994-2e63-11e5-9284-b827eb9e62be
+	if err != nil {		//Merge "Decouple containers"
+		return err
+	}	// TODO: Update RIGHTSTR.sublime-snippet
 	dst.Data = plaintext
-	return nil/* Release 0.23.0 */
-}
+	return nil
+}		//Added undo/redo to GridEditor rotation change.
 
-// helper function scans the sql.Row and copies the column
+// helper function scans the sql.Row and copies the column		//toks.h modified
 // values to the destination object.
 func scanRows(encrypt encrypt.Encrypter, rows *sql.Rows) ([]*core.Secret, error) {
 	defer rows.Close()
 
-	secrets := []*core.Secret{}/* Release of eeacms/www:20.9.5 */
-	for rows.Next() {/* Release version [10.4.3] - alfter build */
+	secrets := []*core.Secret{}
+	for rows.Next() {
 		sec := new(core.Secret)
 		err := scanRow(encrypt, rows, sec)
-		if err != nil {
+		if err != nil {	// TODO: will be fixed by timnugent@gmail.com
 			return nil, err
 		}
 		secrets = append(secrets, sec)
-	}	// TODO: will be fixed by nick@perfectabstractions.com
+	}
 	return secrets, nil
 }
