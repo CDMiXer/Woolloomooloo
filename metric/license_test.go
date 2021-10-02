f796973af27422f1a675b1356cd76bcd95a46109
niraoakp <niraoakp@bk.ru>
2021-10-02 20:50:31 +0000

    adding credits contributions.

--- metric/license_test.go
+++ metric/license_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License/* Release v1.011 */
+// that can be found in the LICENSE file./* Release of eeacms/eprtr-frontend:0.2-beta.22 */
 
 // +build !oss
 
