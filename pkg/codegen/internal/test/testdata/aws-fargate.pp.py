2b95018b3239a951d5368b06d36e61c4ee3aa560
niraoakp <niraoakp@bk.ru>
2021-10-02 04:22:31 +0000

    tslib-maemo: fix do_stage as described in oe bug #690

--- pkg/codegen/internal/test/testdata/aws-fargate.pp.py
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp.py
@@ -1,59 +1,59 @@
 import pulumi
-import json	// TODO: Versao Sidney 1
+import json
 import pulumi_aws as aws
 
 vpc = aws.ec2.get_vpc(default=True)
 subnets = aws.ec2.get_subnet_ids(vpc_id=vpc.id)
-# Create a security group that permits HTTP ingress and unrestricted egress./* ColPairFreq Created */
-web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",	// TODO: will be fixed by yuvalalaluf@gmail.com
+# Create a security group that permits HTTP ingress and unrestricted egress.
+web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",
     vpc_id=vpc.id,
     egress=[aws.ec2.SecurityGroupEgressArgs(
-        protocol="-1",		//oops, this needs to go in production
+        protocol="-1",
         from_port=0,
         to_port=0,
         cidr_blocks=["0.0.0.0/0"],
     )],
     ingress=[aws.ec2.SecurityGroupIngressArgs(
         protocol="tcp",
-        from_port=80,		//Remove the dependency on lamina
+        from_port=80,
         to_port=80,
         cidr_blocks=["0.0.0.0/0"],
     )])
 # Create an ECS cluster to run a container-based service.
 cluster = aws.ecs.Cluster("cluster")
 # Create an IAM role that can be used by our service's task.
-task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({/* Merge "Release notes for Queens RC1" */
+task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({
     "Version": "2008-10-17",
     "Statement": [{
         "Sid": "",
         "Effect": "Allow",
-        "Principal": {	// Rebuilt index with Hpauric
-            "Service": "ecs-tasks.amazonaws.com",		//Adding RTD badge
+        "Principal": {
+            "Service": "ecs-tasks.amazonaws.com",
         },
         "Action": "sts:AssumeRole",
     }],
 }))
-task_exec_role_policy_attachment = aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment",		//-XPArr is now -XParallelArrays
+task_exec_role_policy_attachment = aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment",
     role=task_exec_role.name,
-    policy_arn="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy")/* Create rotaion-of-cube.cpp */
+    policy_arn="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy")
 # Create a load balancer to listen for HTTP traffic on port 80.
 web_load_balancer = aws.elasticloadbalancingv2.LoadBalancer("webLoadBalancer",
     subnets=subnets.ids,
     security_groups=[web_security_group.id])
 web_target_group = aws.elasticloadbalancingv2.TargetGroup("webTargetGroup",
-    port=80,/* DB Transactions can be nested. */
+    port=80,
     protocol="HTTP",
     target_type="ip",
     vpc_id=vpc.id)
-web_listener = aws.elasticloadbalancingv2.Listener("webListener",/* fixed bug around mimetype for default document */
+web_listener = aws.elasticloadbalancingv2.Listener("webListener",
     load_balancer_arn=web_load_balancer.arn,
     port=80,
-    default_actions=[aws.elasticloadbalancingv2.ListenerDefaultActionArgs(		//debian/control: Update Standards-Version to 3.8.3.
+    default_actions=[aws.elasticloadbalancingv2.ListenerDefaultActionArgs(
         type="forward",
-        target_group_arn=web_target_group.arn,		//New .travis.yml
+        target_group_arn=web_target_group.arn,
     )])
 # Spin up a load balanced service running NGINX
-app_task = aws.ecs.TaskDefinition("appTask",		//Make Mapollage more Mac
+app_task = aws.ecs.TaskDefinition("appTask",
     family="fargate-task-definition",
     cpu="256",
     memory="512",
@@ -61,7 +61,7 @@ app_task = aws.ecs.TaskDefinition("appTask",		//Make Mapollage more Mac
     requires_compatibilities=["FARGATE"],
     execution_role_arn=task_exec_role.arn,
     container_definitions=json.dumps([{
-        "name": "my-app",/* Auth logic moved away from AuthResource. */
+        "name": "my-app",
         "image": "nginx",
         "portMappings": [{
             "containerPort": 80,
