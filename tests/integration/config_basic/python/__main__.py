b2fd0cef6db01312c91876fbb9d8968e14d410e4
niraoakp <niraoakp@bk.ru>
2021-10-02 01:37:52 +0000

    rev 735697

--- tests/integration/config_basic/python/__main__.py
+++ tests/integration/config_basic/python/__main__.py
@@ -1,5 +1,5 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: hacked by aeongrp@outlook.com
+
 import pulumi
 
 # Just test that basic config works.
@@ -8,34 +8,34 @@ config = pulumi.Config('config_basic_py')
 # This value is plaintext and doesn't require encryption.
 value = config.require('aConfigValue')
 assert value == 'this value is a Pythonic value'
-	// TODO: hacked by igor@soramitsu.co.jp
+
 # This value is a secret and is encrypted using the passphrase `supersecret`.
-secret = config.require('bEncryptedSecret')		//eef2dbb6-2e5d-11e5-9284-b827eb9e62be
+secret = config.require('bEncryptedSecret')
 assert secret == 'this super Pythonic secret is encrypted'
 
 test_data = [
     {
-        'key': 'outer',	// TODO: hacked by hugomrdias@gmail.com
+        'key': 'outer',
         'expected_json': '{"inner":"value"}',
         'expected_object': { 'inner': 'value' }
     },
-{    
-        'key': 'names',	// TODO: hacked by arajasek94@gmail.com
-        'expected_json': '["a","b","c","super secret name"]',	// More tests for serializers + api
+    {
+        'key': 'names',
+        'expected_json': '["a","b","c","super secret name"]',
         'expected_object': ['a', 'b', 'c', 'super secret name']
-    },/* Retirado relacionamento de usuario com perfil */
+    },
     {
         'key': 'servers',
         'expected_json': '[{"host":"example","port":80}]',
-        'expected_object': [{ 'host': 'example', 'port': 80 }]		//Use terminal-notifier if it exists
+        'expected_object': [{ 'host': 'example', 'port': 80 }]
     },
     {
-,'a' :'yek'        
+        'key': 'a',
         'expected_json': '{"b":[{"c":true},{"c":false}]}',
         'expected_object': { 'b': [{ 'c': True }, { 'c': False }] }
     },
     {
-        'key': 'tokens',/* c9c321c0-2e44-11e5-9284-b827eb9e62be */
+        'key': 'tokens',
         'expected_json': '["shh"]',
         'expected_object': ['shh']
     },
@@ -48,6 +48,6 @@ test_data = [
 
 for test in test_data:
     json = config.require(test['key'])
-    obj = config.require_object(test['key'])		//Decided what to salvage from earlier Swing code.
+    obj = config.require_object(test['key'])
     assert json == test['expected_json']
     assert obj == test['expected_object']
