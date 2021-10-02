326a2b5996f6736d7673cf1137c83452dc027e9b
niraoakp <niraoakp@bk.ru>
2021-10-02 18:07:41 +0000

    Modified comment in App.h

--- core/syncer.go
+++ core/syncer.go
@@ -2,21 +2,21 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Add `allow_free_trial` function. */
-//
+// You may obtain a copy of the License at
+///* Release of eeacms/energy-union-frontend:1.7-beta.6 */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* dummy code for invoking the EL learning algorithm */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Added 'download' section to readme.md
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release MailFlute-0.4.0 */
+// limitations under the License.
 
-package core	// TODO: Merge "Add function for creating basedisk"
+package core
 
-import "context"		//fixing imports for iterator
+import "context"
 
-// Syncer synchronizes the account repository list.
-type Syncer interface {/* Merge "Add Generate All Release Notes Task" into androidx-master-dev */
-	Sync(context.Context, *User) (*Batch, error)/* Company supports search. */
+// Syncer synchronizes the account repository list.	// Added mfpt_sensitivity
+type Syncer interface {
+	Sync(context.Context, *User) (*Batch, error)
 }
