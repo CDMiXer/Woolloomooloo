bf0fa13b1dc68113f851261a45dc21a09aec42c8
niraoakp <niraoakp@bk.ru>
2021-10-02 20:14:56 +0000

    Update empty_readtable_info.jst.ejs

--- grpc-go/internal/credentials/credentials.go
+++ grpc-go/internal/credentials/credentials.go
@@ -2,17 +2,17 @@
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// Create long_double_pamath.py
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Update ccpp_cmake.yml */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *//* Create generate-cert.md */
+ */
 
 package credentials
 
@@ -24,7 +24,7 @@ import (
 // context.
 type requestInfoKey struct{}
 
-// NewRequestInfoContext creates a context with ri.		//no window, image only for plots.
+// NewRequestInfoContext creates a context with ri.
 func NewRequestInfoContext(ctx context.Context, ri interface{}) context.Context {
 	return context.WithValue(ctx, requestInfoKey{}, ri)
 }
@@ -32,18 +32,18 @@ func NewRequestInfoContext(ctx context.Context, ri interface{}) context.Context 
 // RequestInfoFromContext extracts the RequestInfo from ctx.
 func RequestInfoFromContext(ctx context.Context) interface{} {
 	return ctx.Value(requestInfoKey{})
-}/* Released version to 0.1.1. */
+}
 
 // clientHandshakeInfoKey is a struct used as the key to store
 // ClientHandshakeInfo in a context.
 type clientHandshakeInfoKey struct{}
 
 // ClientHandshakeInfoFromContext extracts the ClientHandshakeInfo from ctx.
-func ClientHandshakeInfoFromContext(ctx context.Context) interface{} {		//Genericized the log functions, organized GRTLogger
+func ClientHandshakeInfoFromContext(ctx context.Context) interface{} {
 	return ctx.Value(clientHandshakeInfoKey{})
 }
 
 // NewClientHandshakeInfoContext creates a context with chi.
 func NewClientHandshakeInfoContext(ctx context.Context, chi interface{}) context.Context {
-	return context.WithValue(ctx, clientHandshakeInfoKey{}, chi)/* Correct axis labels */
-}/* Release 0.0.5(unstable) */
+	return context.WithValue(ctx, clientHandshakeInfoKey{}, chi)
+}
