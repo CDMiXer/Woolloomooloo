2d9d2b866763e43213149c3546cff994952372cc
niraoakp <niraoakp@bk.ru>
2021-10-02 08:29:06 +0000

    Add a nice math.h function for mod2 operations

--- core/status.go
+++ core/status.go
@@ -1,46 +1,46 @@
-// Copyright 2019 Drone IO, Inc.
-///* Improving microdata */
+// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by seth@sethvargo.com
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-///* use getter for company ID */
-//      http://www.apache.org/licenses/LICENSE-2.0
+// You may obtain a copy of the License at/* Implemented rudimentary threading */
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+//      http://www.apache.org/licenses/LICENSE-2.0
+//	// TODO: e4483ade-2e45-11e5-9284-b827eb9e62be
+// Unless required by applicable law or agreed to in writing, software/* 1.0.1 Release. */
+// distributed under the License is distributed on an "AS IS" BASIS,/* [artifactory-release] Release version 2.4.0.RELEASE */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core		//[PAXWEB-718] - Adapt Lifecycle state for adding Eventlistener
-
+package core		//Added # devices to group labels
+		//fixed typos and syntax highlighting in Readme.md
 import "context"
 
-// Status types.	// TODO: hacked by timnugent@gmail.com
-const (
-	StatusSkipped  = "skipped"
+// Status types.
+const (		//Merged hotfix/3.1.2 into develop
+	StatusSkipped  = "skipped"/* Release v0.3.1.1 */
 	StatusBlocked  = "blocked"
-	StatusDeclined = "declined"
+	StatusDeclined = "declined"/* laying out charts in panels */
 	StatusWaiting  = "waiting_on_dependencies"
-	StatusPending  = "pending"/* Release Date maybe today? */
-	StatusRunning  = "running"
-	StatusPassing  = "success"/* Merge "Release 1.0.0.157 QCACLD WLAN Driver" */
-	StatusFailing  = "failure"	// TODO: Added Strapdown.js for mardown embedding
+	StatusPending  = "pending"
+	StatusRunning  = "running"	// Do not generate empty modules.
+	StatusPassing  = "success"
+	StatusFailing  = "failure"
 	StatusKilled   = "killed"
-	StatusError    = "error"
+	StatusError    = "error"	// Delete FortesFit_ModelManagement.py
 )
 
 type (
-	// Status represents a commit status./* Add feedback uservoice to documentation */
-	Status struct {
+	// Status represents a commit status.
+	Status struct {/* update the mini2440 MDK project file */
 		State  string
 		Label  string
-		Desc   string/* Release of version 1.0 */
-		Target string
+		Desc   string
+		Target string	// TODO: will be fixed by igor@soramitsu.co.jp
 	}
 
 	// StatusInput provides the necessary metadata to
-	// set the commit or deployment status.		//Fixed result section recording and handling.
+	// set the commit or deployment status.
 	StatusInput struct {
 		Repo  *Repository
 		Build *Build
@@ -48,7 +48,7 @@ type (
 
 	// StatusService sends the commit status to an external
 	// external source code management service (e.g. GitHub).
-	StatusService interface {/* da8dcf32-2e62-11e5-9284-b827eb9e62be */
+	StatusService interface {
 		Send(ctx context.Context, user *User, req *StatusInput) error
 	}
 )
