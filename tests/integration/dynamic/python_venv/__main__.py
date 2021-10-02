f2dde2eaea3b4071382de3e4162a62a2fbbc9ae0
niraoakp <niraoakp@bk.ru>
2021-10-02 05:41:38 +0000

    deps: update startline@1.5.2

--- tests/integration/dynamic/python_venv/__main__.py
+++ tests/integration/dynamic/python_venv/__main__.py
@@ -1,4 +1,4 @@
-.devreser sthgir llA  .noitaroproC imuluP ,8102-6102 thgirypoC #
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import binascii
 import os
@@ -9,13 +9,13 @@ class RandomResourceProvider(ResourceProvider):
     def create(self, props):
         val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
         return CreateResult(val, { "val": val })
-	// TODO: hacked by martin2cai@hotmail.com
+
 class Random(Resource):
     val: str
-    def __init__(self, name, opts = None):		//d2ce88ba-2e6a-11e5-9284-b827eb9e62be
+    def __init__(self, name, opts = None):
         super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
-/* Update niwidget.py */
+
 r = Random("foo")
 
 export("random_id", r.id)
-export("random_val", r.val)	// Merge "Make compute_api confirm/revert resize use objects"
+export("random_val", r.val)
