6ab43276a3940ca73ec488ad8f94b18af18ce874
niraoakp <niraoakp@bk.ru>
2021-10-02 00:43:14 +0000

    97f3a8b0-2e4c-11e5-9284-b827eb9e62be

--- tests/integration/delete_before_create/step4/index.ts
+++ tests/integration/delete_before_create/step4/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by nick@perfectabstractions.com
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Re #26643 Release Notes */
 import { Resource } from "./resource";
 
-// Setup for the next test./* Release v0.20 */
+// Setup for the next test.
 const a = new Resource("base", { uniqueKey: 1, state: 100 });
-const b = new Resource("base-2", { uniqueKey: 2, state: 100 });
+const b = new Resource("base-2", { uniqueKey: 2, state: 100 });	// rev 758405
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
