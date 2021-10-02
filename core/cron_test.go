0b6b76b68fc3915378f104c6217883ea362bed90
niraoakp <niraoakp@bk.ru>
2021-10-02 13:40:50 +0000

    Fix doc: XmlHalSerializer => XmlSerializer

--- core/cron_test.go
+++ core/cron_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by 13860583249@yeah.net
+	// Added printAnnotation methods
 // +build !oss
-
+		//8c073eb2-2e43-11e5-9284-b827eb9e62be
 package core
