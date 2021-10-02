a3d5d1767a750033251e2ced195b4cb7465b584a
niraoakp <niraoakp@bk.ru>
2021-10-02 15:13:47 +0000

    update to v2.0 postman export

--- pkg/codegen/internal/test/testdata/secret.pp.go
+++ pkg/codegen/internal/test/testdata/secret.pp.go
@@ -1,9 +1,9 @@
 package main
 
-import (
+import (/* 907ee9f4-2e70-11e5-9284-b827eb9e62be */
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/rds"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
+)/* Release of eeacms/www:20.3.1 */
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
