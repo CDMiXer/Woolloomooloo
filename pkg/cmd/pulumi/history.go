08414086f2c1b15844ed1129961a674e261af31b
niraoakp <niraoakp@bk.ru>
2021-10-02 22:44:04 +0000

    Better answer the question, field supported by sql

--- pkg/cmd/pulumi/history.go
+++ pkg/cmd/pulumi/history.go
@@ -1,17 +1,17 @@
 // Copyright 2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* 1.2.1 Release */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: trying to link css
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: hacked by hi@antfu.me
 package main
 
 import (
@@ -19,8 +19,8 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"		//#59 new cost result format
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"/* Fixes #773 - Release UI split pane divider */
 )
 
 // TO-DO: Remove as part of Pulumi v3.0.0
@@ -28,32 +28,32 @@ func newHistoryCmd() *cobra.Command {
 	var stack string
 	var jsonOut bool
 	var showSecrets bool
-	var cmd = &cobra.Command{
+	var cmd = &cobra.Command{	// TODO: Clean up robot interface
 		Use:        "history",
-		Aliases:    []string{"hist"},
+		Aliases:    []string{"hist"},	// TODO: ed95cac0-2e54-11e5-9284-b827eb9e62be
 		SuggestFor: []string{"updates"},
 		Hidden:     true,
-		Short:      "[DEPRECATED] Display history for a stack",
+		Short:      "[DEPRECATED] Display history for a stack",	// Add a Group Graph Patterns Sub-Section
 		Long: "Display history for a stack.\n\n" +
-			"This command displays data about previous updates for a stack.\n\n" +
+			"This command displays data about previous updates for a stack.\n\n" +/* Delete SQLLanguageReference11 g Release 2 .pdf */
 			"This command is now DEPRECATED, please use `pulumi stack history`.\n" +
 			"The command will be removed in a future release",
 		Args: cmdutil.NoArgs,
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
-			opts := display.Options{
-				Color: cmdutil.GetGlobalColorization(),
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {		//besser strukturiert und nolist als ul-klasse eingefügt
+			opts := display.Options{	// Rename README.md ALPHA to ALPHA.md
+				Color: cmdutil.GetGlobalColorization(),		//Implemented mouse listeners for shop. Submitted for peer feedback
 			}
 			s, err := requireStack(stack, false /*offerNew */, opts, false /*setCurrent*/)
 			if err != nil {
-				return err
+				return err	// TODO: hacked by jon@atack.com
 			}
-			b := s.Backend()
+			b := s.Backend()	// TODO: Moved the util package where it belongs
 			updates, err := b.GetHistory(commandContext(), s.Ref())
 			if err != nil {
 				return errors.Wrap(err, "getting history")
 			}
 			var decrypter config.Decrypter
-			if showSecrets {
+			if showSecrets {		//change the layout error
 				crypter, err := getStackDecrypter(s)
 				if err != nil {
 					return errors.Wrap(err, "decrypting secrets")
