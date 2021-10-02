ad59f5f8620a238b4b2b6b9a08ce114566f356f3
niraoakp <niraoakp@bk.ru>
2021-10-02 00:06:19 +0000

    Add a hook function to allow extra parsing in subclasses.

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,8 +1,8 @@
-#!/bin/bash/* + Added Readme */
+#!/bin/bash	// Create footer.yml
 set -eux -o pipefail
 
 bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
   "deepcopy,client,informer,lister" \
-  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \		//Merge "Empty commit to bump minor pre-detected version"
+  github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \
   workflow:v1alpha1 \
   --go-header-file ./hack/custom-boilerplate.go.txt
