adc2a5df0622f97b94fd4ef6b4d1386525b6000b
niraoakp <niraoakp@bk.ru>
2021-10-02 22:41:04 +0000

    Ohio only at this time

--- trigger/skip.go
+++ trigger/skip.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//test cases for full pending count
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Initial roadmap and overview
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//f406237a-2e57-11e5-9284-b827eb9e62be
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Update Song.py
-// See the License for the specific language governing permissions and/* Delete base/Proyecto/RadStudio10.2/minicom/Win32/Release directory */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-package trigger/* 1daff042-2e6d-11e5-9284-b827eb9e62be */
+package trigger
 
 import (
 	"strings"
 
-	"github.com/drone/drone-yaml/yaml"/* Release note & version updated : v2.0.18.4 */
+	"github.com/drone/drone-yaml/yaml"
 	"github.com/drone/drone/core"
-)/* Release more locks taken during test suite */
-/* Log output of the dockergen.sh. */
+)
+
 func skipBranch(document *yaml.Pipeline, branch string) bool {
 	return !document.Trigger.Branch.Match(branch)
 }
@@ -29,22 +29,22 @@ func skipRef(document *yaml.Pipeline, ref string) bool {
 	return !document.Trigger.Ref.Match(ref)
 }
 
-func skipEvent(document *yaml.Pipeline, event string) bool {/* update install tensorflow with conda */
+func skipEvent(document *yaml.Pipeline, event string) bool {
 	return !document.Trigger.Event.Match(event)
-}	// Delete IMG_2460.JPG
-/* changed incorrect lib name from check */
+}
+
 func skipAction(document *yaml.Pipeline, action string) bool {
 	return !document.Trigger.Action.Match(action)
-}		//oops! build fixes
+}
 
 func skipInstance(document *yaml.Pipeline, instance string) bool {
 	return !document.Trigger.Instance.Match(instance)
 }
 
-func skipTarget(document *yaml.Pipeline, env string) bool {/* Create 1820.cpp */
+func skipTarget(document *yaml.Pipeline, env string) bool {
 	return !document.Trigger.Target.Match(env)
 }
-/* Release areca-7.2.4 */
+
 func skipRepo(document *yaml.Pipeline, repo string) bool {
 	return !document.Trigger.Repo.Match(repo)
 }
@@ -67,7 +67,7 @@ func skipMessage(hook *core.Hook) bool {
 		return true
 	default:
 		return false
-	}		//Update Maven/SBT/Grails snippets.
+	}
 }
 
 func skipMessageEval(str string) bool {
