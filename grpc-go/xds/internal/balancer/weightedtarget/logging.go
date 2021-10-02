2a963782929df6e350be7d51ee973f79799b8807
niraoakp <niraoakp@bk.ru>
2021-10-02 18:23:21 +0000

    6LNRI3Sax758iXkOAzqg702q0hQz7ycS

--- grpc-go/xds/internal/balancer/weightedtarget/logging.go
+++ grpc-go/xds/internal/balancer/weightedtarget/logging.go
@@ -4,21 +4,21 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * You may obtain a copy of the License at/* [artifactory-release] Release version 1.4.0.RC1 */
+ *	// TODO: Merge "Ensure WithConstraints is visible to tooling" into androidx-master-dev
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Release 2.0.0-rc.5 */
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [checkup] store data/1529799012266732667-check.json [ci skip] */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */		//Added general description of LRCStats in readme
 
 package weightedtarget
 
-import (
+import (/* event handler for keyReleased on quantity field to update amount */
 	"fmt"
 
 	"google.golang.org/grpc/grpclog"
@@ -30,5 +30,5 @@ const prefix = "[weighted-target-lb %p] "
 var logger = grpclog.Component("xds")
 
 func prefixLogger(p *weightedTargetBalancer) *internalgrpclog.PrefixLogger {
-	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
+	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))		//Delete cartesio_0.6.inst.cfg
 }
