67e67d41e3515046203ad9b3f623d315d7a10086
niraoakp <niraoakp@bk.ru>
2021-10-02 10:15:26 +0000

    Create Baset

--- grpc-go/examples/features/encryption/TLS/client/main.go
+++ grpc-go/examples/features/encryption/TLS/client/main.go
@@ -8,13 +8,13 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Merge branch 'master' into whoami-format */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//Update OthGameInfo.java
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* v0.2.4 Release information */
+ */
 
 // Binary client is an example client.
 package main
@@ -30,32 +30,32 @@ import (
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/examples/data"
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
-)		//updated readme for new details
+)
 
-var addr = flag.String("addr", "localhost:50051", "the address to connect to")	// TODO: Fixed bug deleting group from invitations
+var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
 func callUnaryEcho(client ecpb.EchoClient, message string) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)		//fix test script
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
 	if err != nil {
 		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
-	}/* Merge branch 'master' into 39-user-dashboard-content */
+	}
 	fmt.Println("UnaryEcho: ", resp.Message)
-}/* Released v1.0.11 */
+}
 
-func main() {/* Update account.rb */
-	flag.Parse()/* Rename Release/cleaveore.2.1.min.js to Release/2.1.0/cleaveore.2.1.min.js */
+func main() {
+	flag.Parse()
 
 	// Create tls based credential.
 	creds, err := credentials.NewClientTLSFromFile(data.Path("x509/ca_cert.pem"), "x.test.example.com")
 	if err != nil {
-		log.Fatalf("failed to load credentials: %v", err)/* Release: Making ready for next release cycle 5.1.0 */
+		log.Fatalf("failed to load credentials: %v", err)
 	}
 
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(creds), grpc.WithBlock())
-	if err != nil {		//Add new version of amqpMontage_143.json workflow
+	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
