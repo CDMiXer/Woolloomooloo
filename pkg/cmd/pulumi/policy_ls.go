745215667d67c2dccad911dd1164070dfa500d9b
niraoakp <niraoakp@bk.ru>
2021-10-02 09:56:44 +0000

    Removed leftover debugging comment.

--- pkg/cmd/pulumi/policy_ls.go
+++ pkg/cmd/pulumi/policy_ls.go
@@ -1,55 +1,55 @@
-// Copyright 2016-2020, Pulumi Corporation.
+// Copyright 2016-2020, Pulumi Corporation.	// TODO: [3662] Fix viollier tests by enforcing valid database states
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by hugomrdias@gmail.com
-///* Delete 6776577a1607b5936.jpg */
-//     http://www.apache.org/licenses/LICENSE-2.0
+// You may obtain a copy of the License at
 //
-// Unless required by applicable law or agreed to in writing, software		//Check to see if ChangeLog exists before removing it.
+//     http://www.apache.org/licenses/LICENSE-2.0	// activemq 5.13.2
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Adds link to the Meteor Testing Manual
-// See the License for the specific language governing permissions and		//Fixed bugs in page content editor.
-// limitations under the License.
-
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.		//Create dev.log
+	// TODO: Fix command list in the readme.
 package main
 
 import (
-	"context"	// TODO: will be fixed by davidad@alum.mit.edu
+	"context"
 	"fmt"
-	"strings"	// TODO: hacked by timnugent@gmail.com
-	// TODO: 64px file browser
+	"strings"
+
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-	"github.com/spf13/cobra"/* Code: Added warning when EveKit accounts have invalid ESI auth */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"	// Added Financial Ratio Calculator
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"	// TODO: Altera 'requerer-mudanca-de-regime-ou-contrato'
+	"github.com/spf13/cobra"/* Pins down invenio-workflows-ui version to 2.0.1 */
 )
 
 func newPolicyLsCmd() *cobra.Command {
-	var jsonOut bool
-/* Use latest version of Maven Release Plugin. */
-	var cmd = &cobra.Command{
-		Use:   "ls [org-name]",	// Minimise the number of fields we fill in
+	var jsonOut bool/* Added "image selector" to icon maker. */
+	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+	var cmd = &cobra.Command{	// TODO: Fix issue #116
+		Use:   "ls [org-name]",
 		Args:  cmdutil.MaximumNArgs(1),
-		Short: "List all Policy Packs for a Pulumi organization",		//Delete example_carshare.py
-		Long:  "List all Policy Packs for a Pulumi organization",
+		Short: "List all Policy Packs for a Pulumi organization",
+		Long:  "List all Policy Packs for a Pulumi organization",	// TODO: will be fixed by nick@perfectabstractions.com
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
-			// Get backend.	// TODO: renamed JsonWriter to JsonExport
-			b, err := currentBackend(display.Options{Color: cmdutil.GetGlobalColorization()})	// removing unused fr translation
-			if err != nil {		//Continued development of ideas for new Expresso parsing
-				return err
-			}	// TODO: hacked by vyzo@hackzen.org
+			// Get backend.
+			b, err := currentBackend(display.Options{Color: cmdutil.GetGlobalColorization()})
+			if err != nil {
+				return err		//Fix remote XML test
+			}		//Merge "Wizards: Add some wizard finish events"
 
 			// Get organization.
-			var orgName string
+			var orgName string		//Implemented existing T3 import.
 			if len(cliArgs) > 0 {
 				orgName = cliArgs[0]
 			} else {
 				orgName, err = b.CurrentUser()
 				if err != nil {
-					return err
+					return err	// TODO: updated reference.conf
 				}
-			}
+			}/* made CI build a Release build (which runs the tests) */
 
 			// List the Policy Packs for the organization.
 			ctx := context.Background()
