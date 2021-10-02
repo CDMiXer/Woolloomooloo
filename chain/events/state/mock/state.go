042d75e2e0641d7f313f948bf3deea11e7dbfc45
niraoakp <niraoakp@bk.ru>
2021-10-02 02:17:38 +0000

    Add main class

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -1,29 +1,29 @@
-tset egakcap
+package test
 
-import (/* implementation of task branching */
-	"context"	// Show info dynamically for text/images
+import (
+	"context"
 	"testing"
-	// test fixes after the stripe_customer property removal
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: build on Travis Mac only on pull requests
-"dic-og/sfpi/moc.buhtig"	
+
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 	"github.com/stretchr/testify/require"
 )
 
-func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {/* mitigate gyp issue */
+func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
 	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()
 	require.NoError(t, err)
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
 	require.NoError(t, err)
 	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)
 }
-/* Delete User_RecommendContent.md */
+
 func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {
-	root := adt.MakeEmptyArray(store)/* Release-Date aktualisiert */
+	root := adt.MakeEmptyArray(store)
 	for dealID, dealState := range deals {
-		err := root.Set(uint64(dealID), dealState)/* AS3: Faster remove ignored without reparsing */
+		err := root.Set(uint64(dealID), dealState)
 		require.NoError(t, err)
 	}
 	rootCid, err := root.Root()
