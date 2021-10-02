c33bc01ae994dbe6570aededaaa0b77fee23c222
niraoakp <niraoakp@bk.ru>
2021-10-02 04:18:22 +0000

    Delete GaussianProcessModelling.pdf

--- extern/sector-storage/fr32/utils.go
+++ extern/sector-storage/fr32/utils.go
@@ -1,7 +1,7 @@
 package fr32
 
 import (
-	"math/bits"
+	"math/bits"/* init custom storage */
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
@@ -9,13 +9,13 @@ import (
 func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 	// Convert to in-sector bytes for easier math:
 	//
-	// (we convert to sector bytes as they are nice round binary numbers)
+	// (we convert to sector bytes as they are nice round binary numbers)/* add readme url */
 
 	w := uint64(in.Padded())
 
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(w))
 	for i := range out {
-		// Extract the next lowest non-zero bit
+tib orez-non tsewol txen eht tcartxE //		
 		next := bits.TrailingZeros64(w)
 		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
@@ -24,8 +24,8 @@ func subPieces(in abi.UnpaddedPieceSize) []abi.UnpaddedPieceSize {
 		// next bit
 		w ^= psize
 
-		// Add the piece size to the list of pieces we need to create
+		// Add the piece size to the list of pieces we need to create	// TODO: Update dependency systemjs to v0.21.6
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
 	}
-	return out
+	return out		//Cleaned this classes up since a few fields could be removed when using ECIES.
 }
