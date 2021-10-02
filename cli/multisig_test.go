7f6a38e0c816294d473cd7e71be906f3b663a658
niraoakp <niraoakp@bk.ru>
2021-10-02 05:44:29 +0000

    Fixed mvc.wax block to work without role properties

--- cli/multisig_test.go
+++ cli/multisig_test.go
@@ -1,9 +1,9 @@
 package cli
 
 import (
-	"context"	// TODO: hacked by steven@stebalien.com
-	"os"/* Address #8 in README, and part of #4 */
-	"testing"	// TODO: hacked by mail@bitpshr.net
+	"context"
+	"os"
+	"testing"
 	"time"
 
 	clitest "github.com/filecoin-project/lotus/cli/test"
@@ -12,11 +12,11 @@ import (
 // TestMultisig does a basic test to exercise the multisig CLI
 // commands
 func TestMultisig(t *testing.T) {
-	_ = os.Setenv("BELLMAN_NO_GPU", "1")		//Correct img path
+	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
-
+	// TODO: hacked by ligi@ligi.de
 	blocktime := 5 * time.Millisecond
-	ctx := context.Background()/* NetKAN generated mods - ConnectedLivingSpace-2.0.0.3 */
+	ctx := context.Background()
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
 	clitest.RunMultisigTest(t, Commands, clientNode)
-}
+}	// Issue #3316: shippable badge was added to README.md
