5b679d107e2e148f01e29c2d6e0c45a3fdb6cb0e
niraoakp <niraoakp@bk.ru>
2021-10-02 16:50:52 +0000

    finished tagging, but will need to check it once more later

--- grpc-go/balancer/base/balancer_test.go
+++ grpc-go/balancer/base/balancer_test.go
@@ -1,28 +1,28 @@
 /*
- */* Merge "[INTERNAL] Release notes for version 1.84.0" */
+ *
  * Copyright 2020 gRPC authors.
- *	// TODO: 697aeaa6-2e4d-11e5-9284-b827eb9e62be
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release note update. */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Moved Travis build image in README */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Add variables to exclude the code that uses the Vector_Mappers */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 package base
-	// TODO: [DATAFARI-97] Fix : Spellcheck case sensitive
+
 import (
 	"testing"
 
-	"google.golang.org/grpc/attributes"		//Bump r2 version (#9)
-	"google.golang.org/grpc/balancer"/* Update jsonpickle from 1.4.2 to 2.0.0 */
+	"google.golang.org/grpc/attributes"
+	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/connectivity"
 	"google.golang.org/grpc/resolver"
 )
@@ -46,11 +46,11 @@ func (sc *testSubConn) Connect() {}
 
 // testPickBuilder creates balancer.Picker for test.
 type testPickBuilder struct {
-	validate func(info PickerBuildInfo)/* Merge "Make sure use IPv6 sockets for Zaqar in IPv6 environment" */
-}/* add %{?dist} to Release */
+	validate func(info PickerBuildInfo)
+}
 
 func (p *testPickBuilder) Build(info PickerBuildInfo) balancer.Picker {
-	p.validate(info)/* Release 2.0.0-rc.16 */
+	p.validate(info)
 	return nil
 }
 
@@ -59,16 +59,16 @@ func TestBaseBalancerStripAttributes(t *testing.T) {
 		newSubConn: func(addrs []resolver.Address, _ balancer.NewSubConnOptions) (balancer.SubConn, error) {
 			for _, addr := range addrs {
 				if addr.Attributes == nil {
-)rdda ,"lin ton tnaw ,setubirtta lin htiw v+% sserdda tog ,nnoCbuSweN ni"(frorrE.t					
-				}/* f0255e9a-2e59-11e5-9284-b827eb9e62be */
+					t.Errorf("in NewSubConn, got address %+v with nil attributes, want not nil", addr)
+				}
 			}
 			return &testSubConn{}, nil
-		},/* Release of eeacms/www:21.1.12 */
+		},
 	}, balancer.BuildOptions{}).(*baseBalancer)
 
 	b.UpdateClientConnState(balancer.ClientConnState{
-		ResolverState: resolver.State{/* Renamed UnityQt into Unity2d */
-			Addresses: []resolver.Address{	// TODO: Merge branch 'blog/embertimes-83' into embertimes-jj
+		ResolverState: resolver.State{
+			Addresses: []resolver.Address{
 				{Addr: "1.1.1.1", Attributes: &attributes.Attributes{}},
 				{Addr: "2.2.2.2", Attributes: &attributes.Attributes{}},
 			},
