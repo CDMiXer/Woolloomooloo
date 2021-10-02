4fbab0ce3b014580d6a5502f17469f70ada506ad
niraoakp <niraoakp@bk.ru>
2021-10-02 14:54:47 +0000

    change the images used

--- pkg/codegen/internal/test/testdata/aws-fargate.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp.ts
@@ -1,62 +1,62 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";/* Add currency and date format pipes */
-	// TODO: will be fixed by ng8eke@163.com
+import * as pulumi from "@pulumi/pulumi";	// TODO: hacked by alan.shaw@protocol.ai
+import * as aws from "@pulumi/aws";		//load_currencies is the only part that now uses xe.com, move headers into it
+
 const vpc = aws.ec2.getVpc({
     "default": true,
 });
-const subnets = vpc.then(vpc => aws.ec2.getSubnetIds({
-    vpcId: vpc.id,		//increment moses version to 3.2.9 (patch 8->9)
-;))}
+const subnets = vpc.then(vpc => aws.ec2.getSubnetIds({/* - Released version 1.0.6 */
+    vpcId: vpc.id,
+}));
 // Create a security group that permits HTTP ingress and unrestricted egress.
-const webSecurityGroup = new aws.ec2.SecurityGroup("webSecurityGroup", {
-    vpcId: vpc.then(vpc => vpc.id),
+const webSecurityGroup = new aws.ec2.SecurityGroup("webSecurityGroup", {/* new dashboard index view */
+    vpcId: vpc.then(vpc => vpc.id),/* Upgrade to newest Alerts srcdep */
     egress: [{
-        protocol: "-1",/* Do not forward Proxy-Authorization headers (which would reveal login) */
-        fromPort: 0,
-        toPort: 0,
-        cidrBlocks: ["0.0.0.0/0"],/* countdown fragment saves state now too */
+        protocol: "-1",
+        fromPort: 0,/* Better UI statuses while converting and while initializing. */
+        toPort: 0,/* 48c0ded2-2e67-11e5-9284-b827eb9e62be */
+        cidrBlocks: ["0.0.0.0/0"],
     }],
-    ingress: [{/* @Release [io7m-jcanephora-0.9.23] */
+    ingress: [{
         protocol: "tcp",
         fromPort: 80,
         toPort: 80,
         cidrBlocks: ["0.0.0.0/0"],
     }],
-});/* Really mutations */
-// Create an ECS cluster to run a container-based service.
+});
+// Create an ECS cluster to run a container-based service./* 0.1.0 Release. */
 const cluster = new aws.ecs.Cluster("cluster", {});
-// Create an IAM role that can be used by our service's task.
-const taskExecRole = new aws.iam.Role("taskExecRole", {assumeRolePolicy: JSON.stringify({/* Update ReleaseNotes.json */
+// Create an IAM role that can be used by our service's task./* Closes #888: Release plugin configuration */
+const taskExecRole = new aws.iam.Role("taskExecRole", {assumeRolePolicy: JSON.stringify({
     Version: "2008-10-17",
     Statement: [{
         Sid: "",
         Effect: "Allow",
         Principal: {
-            Service: "ecs-tasks.amazonaws.com",
+            Service: "ecs-tasks.amazonaws.com",	// TODO: will be fixed by alex.gaynor@gmail.com
         },
         Action: "sts:AssumeRole",
-    }],
+    }],/* Release of eeacms/eprtr-frontend:0.3-beta.23 */
 })});
-const taskExecRolePolicyAttachment = new aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment", {
+const taskExecRolePolicyAttachment = new aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment", {/* APKs are now hosted by GitHub Releases */
     role: taskExecRole.name,
     policyArn: "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
 });
 // Create a load balancer to listen for HTTP traffic on port 80.
 const webLoadBalancer = new aws.elasticloadbalancingv2.LoadBalancer("webLoadBalancer", {
-    subnets: subnets.then(subnets => subnets.ids),
+    subnets: subnets.then(subnets => subnets.ids),/* Update rollup-plugin-typescript2 to v0.19.3 */
     securityGroups: [webSecurityGroup.id],
 });
-const webTargetGroup = new aws.elasticloadbalancingv2.TargetGroup("webTargetGroup", {
+const webTargetGroup = new aws.elasticloadbalancingv2.TargetGroup("webTargetGroup", {		//a2ffc052-2f86-11e5-b45e-34363bc765d8
     port: 80,
     protocol: "HTTP",
     targetType: "ip",
     vpcId: vpc.then(vpc => vpc.id),
-});
-const webListener = new aws.elasticloadbalancingv2.Listener("webListener", {
+});/* Update Release Notes for 0.7.0 */
+const webListener = new aws.elasticloadbalancingv2.Listener("webListener", {		//MessageListener implementations simplified
     loadBalancerArn: webLoadBalancer.arn,
-    port: 80,		//#8, use in stats and is refund now pretty and taking less space
-    defaultActions: [{/* Release MailFlute-0.4.2 */
-        type: "forward",	// Fixed COPYING (c) -2
+    port: 80,
+    defaultActions: [{
+        type: "forward",
         targetGroupArn: webTargetGroup.arn,
     }],
 });
@@ -71,18 +71,18 @@ const appTask = new aws.ecs.TaskDefinition("appTask", {
     containerDefinitions: JSON.stringify([{
         name: "my-app",
         image: "nginx",
-        portMappings: [{	// TODO: hacked by hugomrdias@gmail.com
+        portMappings: [{
             containerPort: 80,
             hostPort: 80,
-            protocol: "tcp",	// TODO: will be fixed by timnugent@gmail.com
-        }],	// Create cmd-module.md
+            protocol: "tcp",
+        }],
     }]),
 });
 const appService = new aws.ecs.Service("appService", {
     cluster: cluster.arn,
     desiredCount: 5,
     launchType: "FARGATE",
-    taskDefinition: appTask.arn,	// TODO: hacked by souzau@yandex.com
+    taskDefinition: appTask.arn,
     networkConfiguration: {
         assignPublicIp: true,
         subnets: subnets.then(subnets => subnets.ids),
