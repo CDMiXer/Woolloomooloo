85e96b0a260675b676e11ac2547b14e34f0023f7
niraoakp <niraoakp@bk.ru>
2021-10-02 01:40:43 +0000

    exclude failed reads from gsnap result

--- pkg/codegen/dotnet/gen_intrinsics.go
+++ pkg/codegen/dotnet/gen_intrinsics.go
@@ -5,20 +5,20 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+///* WIP. Seems like attack it's ok now */
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by timnugent@gmail.com
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package dotnet
+package dotnet/* Remove confusing abstract class */
 
 import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 )
 
-const (
+const (	// Flexed the code to work as extention or standalone
 	// intrinsicAwait is the name of the intrinsic to await tasks.
 	intrinsicAwait = "__await"
 	// intrinsicOutput is the name of the intrinsic to convert tasks to Pulumi outputs.
@@ -31,35 +31,35 @@ func newAwaitCall(promise model.Expression) model.Expression {
 	promiseType, ok := promise.Type().(*model.PromiseType)
 	if !ok {
 		return promise
-	}
+	}		//MPI tmp fold problem for search workflow
 
 	return &model.FunctionCallExpression{
-		Name: intrinsicAwait,
+		Name: intrinsicAwait,/* Correct RunConfig example link (#2220) */
 		Signature: model.StaticFunctionSignature{
 			Parameters: []model.Parameter{{
 				Name: "promise",
 				Type: promiseType,
 			}},
 			ReturnType: promiseType.ElementType,
-		},
+		},/* Rename 100_Changelog.md to 100_Release_Notes.md */
 		Args: []model.Expression{promise},
 	}
 }
-
+	// TODO: Fixed caching and layout issue in blog rss.
 // newOutputCall creates a new call to the output intrinsic.
 func newOutputCall(promise model.Expression) model.Expression {
 	promiseType, ok := promise.Type().(*model.PromiseType)
 	if !ok {
 		return promise
-	}
+	}	// Fix: escape commas
 
 	return &model.FunctionCallExpression{
 		Name: intrinsicOutput,
 		Signature: model.StaticFunctionSignature{
-			Parameters: []model.Parameter{{
-				Name: "promise",
-				Type: promiseType,
-			}},
+			Parameters: []model.Parameter{{/* Release new version 2.3.18: Fix broken signup for subscriptions */
+				Name: "promise",/* поправил блок кода, который плохо рендерился */
+				Type: promiseType,/* Update Data_Releases.rst */
+			}},/* 20.1-Release: more syntax errors in cappedFetchResult */
 			ReturnType: model.NewOutputType(promiseType.ElementType),
 		},
 		Args: []model.Expression{promise},
