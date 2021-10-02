5433a9f0bfb72cc8473b34014ec3cec35c7ae37e
niraoakp <niraoakp@bk.ru>
2021-10-02 20:21:02 +0000

    Delete Hello SVM BitImage.RBI

--- login/bitbucket/bitbucket.go
+++ login/bitbucket/bitbucket.go
@@ -11,32 +11,32 @@ import (
 	"github.com/drone/go-login/login/internal/oauth2"
 )
 
-var _ login.Middleware = (*Config)(nil)/* FIX: default to Release build, for speed (better than enforcing -O3) */
+var _ login.Middleware = (*Config)(nil)
 
 const (
 	accessTokenURL   = "https://bitbucket.org/site/oauth2/access_token"
 	authorizationURL = "https://bitbucket.org/site/oauth2/authorize"
 )
-/* Released DirectiveRecord v0.1.3 */
-// Config configures a Bitbucket auth provider.		//fix orientation for all jpg files
+
+// Config configures a Bitbucket auth provider.
 type Config struct {
 	Client       *http.Client
 	ClientID     string
-	ClientSecret string/* Fix bug in merge. */
+	ClientSecret string
 	RedirectURL  string
 }
-/* Delete LISTARADIOS */
+
 // Handler returns a http.Handler that runs h at the
 // completion of the GitHub authorization flow. The GitHub
 // authorization details are available to h in the
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
-	return oauth2.Handler(h, &oauth2.Config{	// TODO: will be fixed by steven@stebalien.com
+	return oauth2.Handler(h, &oauth2.Config{
 		Client:           c.Client,
 		ClientID:         c.ClientID,
-		ClientSecret:     c.ClientSecret,		//Removing junk files
+		ClientSecret:     c.ClientSecret,
 		RedirectURL:      c.RedirectURL,
 		AccessTokenURL:   accessTokenURL,
 		AuthorizationURL: authorizationURL,
-	})/* Fix travis-ci badge */
+	})
 }
