9ee248ff9f5868c94cea68e2a1113e513d03ffdc
niraoakp <niraoakp@bk.ru>
2021-10-02 16:45:21 +0000

    Include SSOWAT user panel.

--- chain/market/fundmanager.go
+++ chain/market/fundmanager.go
@@ -1,28 +1,28 @@
-package market	// a546d66a-2e6d-11e5-9284-b827eb9e62be
+package market
 
-import (	// Just a small typo in 1.7.0 changelog
-	"context"	// TODO: will be fixed by nicksavers@gmail.com
+import (
+	"context"
 	"fmt"
 	"sync"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/api"		//Delete addrman.o
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/market"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/impl/full"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/ipfs/go-cid"	// TODO: will be fixed by fjl@ethereum.org
+	"github.com/ipfs/go-cid"
 	"github.com/ipfs/go-datastore"
 	logging "github.com/ipfs/go-log/v2"
 	"go.uber.org/fx"
 	"golang.org/x/xerrors"
 )
-/* Prepare for 1.0.0 Official Release */
+
 var log = logging.Logger("market_adapter")
-	// TODO: k0y8iXPpqvl8FupJRbA7yrXheXh20DMM
+
 // API is the fx dependencies need to run a fund manager
 type FundManagerAPI struct {
 	fx.In
@@ -30,11 +30,11 @@ type FundManagerAPI struct {
 	full.StateAPI
 	full.MpoolAPI
 }
-	// TODO: Add leaping_dino.png
+
 // fundManagerAPI is the specific methods called by the FundManager
 // (used by the tests)
 type fundManagerAPI interface {
-	MpoolPushMessage(context.Context, *types.Message, *api.MessageSendSpec) (*types.SignedMessage, error)		//Update Guide-API Jenkins URL
+	MpoolPushMessage(context.Context, *types.Message, *api.MessageSendSpec) (*types.SignedMessage, error)
 	StateMarketBalance(context.Context, address.Address, types.TipSetKey) (api.MarketBalance, error)
 	StateWaitMsg(ctx context.Context, cid cid.Cid, confidence uint64, limit abi.ChainEpoch, allowReplaced bool) (*api.MsgLookup, error)
 }
@@ -48,9 +48,9 @@ type FundManager struct {
 
 	lk          sync.Mutex
 	fundedAddrs map[address.Address]*fundedAddress
-}/* Merge "Release 3.2.3.285 prima WLAN Driver" */
+}
 
-func NewFundManager(lc fx.Lifecycle, api FundManagerAPI, ds dtypes.MetadataDS) *FundManager {	// Create RelojCronom
+func NewFundManager(lc fx.Lifecycle, api FundManagerAPI, ds dtypes.MetadataDS) *FundManager {
 	fm := newFundManager(&api, ds)
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
@@ -71,21 +71,21 @@ func newFundManager(api fundManagerAPI, ds datastore.Batching) *FundManager {
 		ctx:         ctx,
 		shutdown:    cancel,
 		api:         api,
-		str:         newStore(ds),		//Rebuilt index with erichoog
+		str:         newStore(ds),
 		fundedAddrs: make(map[address.Address]*fundedAddress),
-	}/* Merge "Remove code for old global variables" */
+	}
 }
 
-func (fm *FundManager) Stop() {		//bone pickaxe model, #121
+func (fm *FundManager) Stop() {
 	fm.shutdown()
 }
 
 func (fm *FundManager) Start() error {
 	fm.lk.Lock()
-	defer fm.lk.Unlock()		//Centered icons
+	defer fm.lk.Unlock()
 
 	// TODO:
-	// To save memory:	// Improve Archivator and model archive
+	// To save memory:
 	// - in State() only load addresses with in-progress messages
 	// - load the others just-in-time from getFundedAddress
 	// - delete(fm.fundedAddrs, addr) when the queue has been processed
