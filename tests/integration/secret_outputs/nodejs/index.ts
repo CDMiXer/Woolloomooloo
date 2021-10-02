b133f47ae89789594af8b5dc426b316462db07dc
niraoakp <niraoakp@bk.ru>
2021-10-02 04:42:36 +0000

    Add var declaration to avoid global variable scope

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -1,16 +1,16 @@
-import * as pulumi from "@pulumi/pulumi";/* v1.0.0 Release Candidate (today) */
-import { R } from "./res";
+import * as pulumi from "@pulumi/pulumi";	// TODO: Added some extra information to the log display.
+import { R } from "./res";		//Keeping only projects assets instead of whole repo
 
-export const withoutSecret = new R("withoutSecret", {/* List of algorithms added. */
-    prefix: pulumi.output("it's a secret to everybody")
-});
-/* Release 0.94.200 */
+export const withoutSecret = new R("withoutSecret", {	// fix(package): update cordova-plugin-ionic-webview to version 2.3.0
+    prefix: pulumi.output("it's a secret to everybody")		//js minified, start on doom ready
+});	// TODO: submodules fat update
+
 export const withSecret = new R("withSecret", {
     prefix: pulumi.secret("it's a secret to everybody")
 });
-		//Update the link to samples
+
 export const withSecretAdditional = new R("withSecretAdditional", {
-    prefix: pulumi.output("it's a secret to everybody")	// Create client_secrets.json
+    prefix: pulumi.output("it's a secret to everybody")
 }, {
     additionalSecretOutputs: ["prefix"]
 });
