8817932ed63df47d49800230c96290f6e05007f1
niraoakp <niraoakp@bk.ru>
2021-10-03 07:39:44 +0000

    The script seems to work now.

--- pkg/codegen/internal/test/testdata/random-pet.pp.py
+++ pkg/codegen/internal/test/testdata/random-pet.pp.py
@@ -1,4 +1,4 @@
-import pulumi
-import pulumi_random as random
+import pulumi/* Ensured that some trailing slashes on retrieved paths are always consistent. */
+import pulumi_random as random/* Release 1.2.4 (corrected) */
 
 random_pet = random.RandomPet("random_pet", prefix="doggo")
