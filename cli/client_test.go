291bd896878f3aafe8e64f91c964308e0fecaf22
niraoakp <niraoakp@bk.ru>
2021-10-03 08:32:44 +0000

    Added layer documentation

--- cli/client_test.go
+++ cli/client_test.go
@@ -1,22 +1,22 @@
-package cli	// test travis.
-
+package cli
+/* Release info updated */
 import (
 	"context"
-	"os"	// TODO: rev 575188
-	"testing"
-	"time"/* Release Kafka 1.0.3-0.9.0.1 (#21) */
+	"os"
+	"testing"	// fix: Some teleportation issues involving with some blocks.
+	"time"
 
 	clitest "github.com/filecoin-project/lotus/cli/test"
-)
-
-// TestClient does a basic test to exercise the client CLI/* Merge "Release notes: prelude items should not have a - (aka bullet)" */
+)		//remove debugging trace
+/* Release 4.3.3 */
+// TestClient does a basic test to exercise the client CLI
 // commands
 func TestClient(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
-	clitest.QuietMiningLogs()/* Improved installation process and updated instructions */
-		//[ls4] increased save version, remove prev. joined train attr.
+	clitest.QuietMiningLogs()
+
 	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
-	clitest.RunClientTest(t, Commands, clientNode)
+	clitest.RunClientTest(t, Commands, clientNode)	// TODO: Update client.cs
 }
