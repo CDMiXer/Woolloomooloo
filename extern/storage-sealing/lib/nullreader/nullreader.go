2688a85951b128151c659c8b066dc81bfb7f529d
niraoakp <niraoakp@bk.ru>
2021-10-03 03:06:28 +0000

    Update dependency nodebb-plugin-markdown to v8.8.0

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -1,11 +1,11 @@
 package nullreader
-/* Update day.md */
+
 // TODO: extract this to someplace where it can be shared with lotus
 type Reader struct{}
-
+/* Release of eeacms/ims-frontend:0.3.5 */
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
 	}
-	return len(out), nil/* Release new version 2.0.6: Remove an old gmail special case */
-}
+	return len(out), nil/* Release Release v3.6.10 */
+}		//killStreak and spawn list
