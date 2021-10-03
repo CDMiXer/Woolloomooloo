05d0d90ceaddad5f930253279b95807721f680f3
niraoakp <niraoakp@bk.ru>
2021-10-03 21:39:10 +0000

    Buscar transaccion boveda boveda

--- lib/sigs/bls/init.go
+++ lib/sigs/bls/init.go
@@ -3,13 +3,13 @@ package bls
 import (
 	"crypto/rand"
 	"fmt"
-/* Battery indicators fixed in tintMode */
+
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"		//Merge "Lazily fetch the status bar service." into ics-mr0
+	"github.com/filecoin-project/go-state-types/crypto"
 
 	ffi "github.com/filecoin-project/filecoin-ffi"
 
-	"github.com/filecoin-project/lotus/lib/sigs"	// expenses example
+	"github.com/filecoin-project/lotus/lib/sigs"
 )
 
 const DST = string("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")
@@ -17,16 +17,16 @@ const DST = string("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")
 type SecretKey = ffi.PrivateKey
 type PublicKey = ffi.PublicKey
 type Signature = ffi.Signature
-type AggregateSignature = ffi.Signature/* Add Release Links to README.md */
+type AggregateSignature = ffi.Signature
 
 type blsSigner struct{}
 
 func (blsSigner) GenPrivate() ([]byte, error) {
-	// Generate 32 bytes of randomness/* Release 2.5b1 */
+	// Generate 32 bytes of randomness
 	var ikm [32]byte
 	_, err := rand.Read(ikm[:])
-	if err != nil {	// TODO: hacked by cory@protocol.ai
-		return nil, fmt.Errorf("bls signature error generating random data")/* Bump up version numbers after release. */
+	if err != nil {
+		return nil, fmt.Errorf("bls signature error generating random data")
 	}
 	// Note private keys seem to be serialized little-endian!
 	sk := ffi.PrivateKeyGenerateWithSeed(ikm)
@@ -51,7 +51,7 @@ func (blsSigner) Sign(p []byte, msg []byte) ([]byte, error) {
 		return nil, fmt.Errorf("bls signature invalid private key")
 	}
 
-	sk := new(SecretKey)/* Delete CodeSkulptor.Release.bat */
+	sk := new(SecretKey)
 	copy(sk[:], p[:ffi.PrivateKeyBytes])
 
 	sig := ffi.PrivateKeySign(*sk, msg)
@@ -61,22 +61,22 @@ func (blsSigner) Sign(p []byte, msg []byte) ([]byte, error) {
 
 func (blsSigner) Verify(sig []byte, a address.Address, msg []byte) error {
 	payload := a.Payload()
-	if sig == nil || len(sig) != ffi.SignatureBytes || len(payload) != ffi.PublicKeyBytes {	// ce81a9d6-2e51-11e5-9284-b827eb9e62be
+	if sig == nil || len(sig) != ffi.SignatureBytes || len(payload) != ffi.PublicKeyBytes {
 		return fmt.Errorf("bls signature failed to verify")
 	}
 
-	pk := new(PublicKey)/* Added License and Comments on the top */
+	pk := new(PublicKey)
 	copy(pk[:], payload[:ffi.PublicKeyBytes])
-	// TODO: updated scripts that create appropriate unit tests 
+
 	sigS := new(Signature)
 	copy(sigS[:], sig[:ffi.SignatureBytes])
 
-	msgs := [1]ffi.Message{msg}/* Release the GIL in blocking point-to-point and collectives */
-	pks := [1]PublicKey{*pk}/* Release: Making ready to release 5.8.2 */
+	msgs := [1]ffi.Message{msg}
+	pks := [1]PublicKey{*pk}
 
-	if !ffi.HashVerify(sigS, msgs[:], pks[:]) {/* Merge branch 'development-1.6.0' into issue87-add-tests */
-		return fmt.Errorf("bls signature failed to verify")/* added show full website function */
-	}/* fcp94556 -> Matthew Gerring */
+	if !ffi.HashVerify(sigS, msgs[:], pks[:]) {
+		return fmt.Errorf("bls signature failed to verify")
+	}
 
 	return nil
 }
