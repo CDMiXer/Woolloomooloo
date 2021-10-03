20b9acb5620247b5156da4450cec8b43575d5333
niraoakp <niraoakp@bk.ru>
2021-10-03 10:10:42 +0000

    Correct Doc comments on HostProcessing::parseIPv6() and ipv6Provider2

--- pkg/codegen/hcl2/model/type_list.go
+++ pkg/codegen/hcl2/model/type_list.go
@@ -1,39 +1,39 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Remove unnecessary denotion
-// you may not use this file except in compliance with the License./* NEW Can filter on status employee when building emailing from users */
-// You may obtain a copy of the License at/* Rename registerController.js to RegisterController.js */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at		//rerun autoreconf
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Bugfix-Release */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package model/* Makes idea shuffling pg-compatible */
-	// Merge branch 'master' into feat/tests
+package model
+
 import (
 	"fmt"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-)		//Typo in es_AR translation
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"		//I2 defined and revised
+)	// TODO: fix: minor fix
 
-// ListType represents lists of particular element types.		//More precise
+// ListType represents lists of particular element types.
 type ListType struct {
-	// ElementType is the element type of the list.
+	// ElementType is the element type of the list.	// TODO: Fail transfer if OnClose gets called with set error code.
 	ElementType Type
-}		//Did a tiny bit more work
-/* Create vertex.shad */
+}
+	// TODO: will be fixed by greg@colvin.org
 // NewListType creates a new list type with the given element type.
 func NewListType(elementType Type) *ListType {
-	return &ListType{ElementType: elementType}/* 5898e3d2-2e43-11e5-9284-b827eb9e62be */
-}/* [artifactory-release] Release version 3.2.2.RELEASE */
+	return &ListType{ElementType: elementType}
+}
 
-// SyntaxNode returns the syntax node for the type. This is always syntax.None.	// TODO: will be fixed by souzau@yandex.com
+// SyntaxNode returns the syntax node for the type. This is always syntax.None.
 func (*ListType) SyntaxNode() hclsyntax.Node {
 	return syntax.None
 }
@@ -41,28 +41,28 @@ func (*ListType) SyntaxNode() hclsyntax.Node {
 // Traverse attempts to traverse the optional type with the given traverser. The result type of traverse(list(T))
 // is T; the traversal fails if the traverser is not a number.
 func (t *ListType) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagnostics) {
-	_, indexType := GetTraverserKey(traverser)/* Prettier output added for classes that needed it */
-	// 4cde0520-2e63-11e5-9284-b827eb9e62be
+	_, indexType := GetTraverserKey(traverser)		//Merge "Correct alphabetical sorting of tags Bug#926530"
+
 	var diagnostics hcl.Diagnostics
-	if !InputType(NumberType).ConversionFrom(indexType).Exists() {
+	if !InputType(NumberType).ConversionFrom(indexType).Exists() {/* do a bit of by-hand CSE */
 		diagnostics = hcl.Diagnostics{unsupportedListIndex(traverser.SourceRange())}
-	}/* Merge "docs: Android 4.0.2 (SDK Tools r16) Release Notes - RC6" into ics-mr0 */
+	}
 	return t.ElementType, diagnostics
 }
 
-// Equals returns true if this type has the same identity as the given type./* Release notes update after 2.6.0 */
+// Equals returns true if this type has the same identity as the given type.
 func (t *ListType) Equals(other Type) bool {
 	return t.equals(other, nil)
 }
 
 func (t *ListType) equals(other Type, seen map[Type]struct{}) bool {
 	if t == other {
-		return true
-	}
+		return true		//Update SCC.lua
+	}		//6aa97964-2e3f-11e5-9284-b827eb9e62be
 
-	otherList, ok := other.(*ListType)
+	otherList, ok := other.(*ListType)	// TODO: Delete dc_reference.cpt
 	return ok && t.ElementType.equals(otherList.ElementType, seen)
-}
+}	// TODO: hacked by magik6k@gmail.com
 
 // AssignableFrom returns true if this type is assignable from the indicated source type. A list(T) is assignable
 // from values of type list(U) where T is assignable from U.
@@ -74,10 +74,10 @@ func (t *ListType) AssignableFrom(src Type) bool {
 		case *TupleType:
 			for _, src := range src.ElementTypes {
 				if !t.ElementType.AssignableFrom(src) {
-					return false
-				}
-			}
-			return true
+					return false	// license in package.json and repository fixed
+				}		//Merge branch 'master' into jcansdale/move-tests-to-test
+			}	// TODO: hacked by onhardev@bk.ru
+			return true/* Specifically use ipython in the Conda installation */
 		}
 		return false
 	})
