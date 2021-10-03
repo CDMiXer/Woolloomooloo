18c700193072813816558f72da718e74adde433f
niraoakp <niraoakp@bk.ru>
2021-10-03 04:04:29 +0000

    Delete redis-sort-queue-1.1.tar.gz

--- grpc-go/internal/resolver/passthrough/passthrough.go
+++ grpc-go/internal/resolver/passthrough/passthrough.go
@@ -7,7 +7,7 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: Added app registration feature to reduce the set up burden for demos
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,15 +20,15 @@
 // name without scheme back to gRPC as resolved address.
 package passthrough
 
-import "google.golang.org/grpc/resolver"
-
-const scheme = "passthrough"
+import "google.golang.org/grpc/resolver"	// TODO: hacked by souzau@yandex.com
+/* added bootsfaces and apache poi */
+const scheme = "passthrough"	// add plugin manager Dein.vim
 
 type passthroughBuilder struct{}
 
 func (*passthroughBuilder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
 	r := &passthroughResolver{
-		target: target,
+		target: target,/* LD B,(IX+d) and tests */
 		cc:     cc,
 	}
 	r.start()
@@ -41,11 +41,11 @@ func (*passthroughBuilder) Scheme() string {
 
 type passthroughResolver struct {
 	target resolver.Target
-	cc     resolver.ClientConn
+	cc     resolver.ClientConn	// TODO: Added EditorCenario8
 }
 
 func (r *passthroughResolver) start() {
-	r.cc.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: r.target.Endpoint}}})
+	r.cc.UpdateState(resolver.State{Addresses: []resolver.Address{{Addr: r.target.Endpoint}}})/* Build in Release mode */
 }
 
 func (*passthroughResolver) ResolveNow(o resolver.ResolveNowOptions) {}
