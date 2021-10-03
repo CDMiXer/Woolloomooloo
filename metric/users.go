8d75491e4a4ab02b8d66fbd7d6ed2d47b78103bb
niraoakp <niraoakp@bk.ru>
2021-10-03 23:28:28 +0000

    Create WMATA.py

--- metric/users.go
+++ metric/users.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: stupid bug again
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-		//Fixing auth token missing on requests
+
 package metric
 
-import (/* Ignoring datasource */
+import (
 	"context"
 
 	"github.com/drone/drone/core"
-	// Update what's new - 2.1 - proposal.md
+
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 var noContext = context.Background()
 
-// UserCount provides metrics for registered users.		//Aj8hSNAhZ8PFCxSNqdcL3yBKAdCLzTY6
+// UserCount provides metrics for registered users.
 func UserCount(users core.UserStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
