f83c7e4355999f46d80b461c1fc4b3bed03305ba
niraoakp <niraoakp@bk.ru>
2021-10-03 07:08:56 +0000

    Embox "jumps". Some little optimization

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
-		//Update standalone-sip.initial.xml
-// Allocate a new resource. When this exists, we should not allow/* Made build configuration (Release|Debug) parameterizable */
+/* [FIX] XQuery Update: nested copy expressions */
+// Allocate a new resource. When this exists, we should not allow
 // the stack holding it to be `rm`'d without `--force`.
 let a = new Resource("res", { state: 1 });
 
-export let o = a.state;		//correction of findElement
+export let o = a.state;
