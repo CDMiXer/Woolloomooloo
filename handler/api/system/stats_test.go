c4df34ed796724386ed0167832bcdef6cdaa132c
niraoakp <niraoakp@bk.ru>
2021-10-03 02:35:12 +0000

    Fix translations files.

--- handler/api/system/stats_test.go
+++ handler/api/system/stats_test.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Build with icon */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package system
-		//Update CHANGELOG for #16218
+
 import (
 	"io/ioutil"
 
-	"github.com/sirupsen/logrus"	// TODO: hacked by indexxuan@gmail.com
+	"github.com/sirupsen/logrus"
 )
-
+	// TODO: will be fixed by alex.gaynor@gmail.com
 func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
