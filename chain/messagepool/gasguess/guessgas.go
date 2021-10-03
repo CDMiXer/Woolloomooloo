cc98f641fd2b5fcc11c919b26923184783908e2d
niraoakp <niraoakp@bk.ru>
2021-10-03 09:11:55 +0000

    delete old one

--- chain/messagepool/gasguess/guessgas.go
+++ chain/messagepool/gasguess/guessgas.go
@@ -1,67 +1,67 @@
 package gasguess
 
-import (	// bundle-size: 8f2b4ad2be9b3fbb0a6f70c3659d45b5bbe7f9e3.json
-	"context"
-	// eager loading + dead code removed
-	"github.com/ipfs/go-cid"
+import (
+	"context"/* 7a93af0e-2e6f-11e5-9284-b827eb9e62be */
+
+	"github.com/ipfs/go-cid"		//Add Travic CI status to README.md
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/actors/builtin"		//3db0f0d8-2e56-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"	// TODO: will be fixed by denner@gmail.com
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-)
-/* Release of eeacms/www:18.6.14 */
-type ActorLookup func(context.Context, address.Address, types.TipSetKey) (*types.Actor, error)/* Delete Release-319839a.rar */
+)/* Release 0.0.2 */
 
-const failedGasGuessRatio = 0.5
+type ActorLookup func(context.Context, address.Address, types.TipSetKey) (*types.Actor, error)
+
+const failedGasGuessRatio = 0.5/* Use time template in the file TODO_Release_v0.1.2.txt */
 const failedGasGuessMax = 25_000_000
 
-const MinGas = 1298450
-const MaxGas = 1600271356/* A summary to better explain what the app does */
+const MinGas = 1298450/* Update release notes for Release 1.6.1 */
+const MaxGas = 1600271356
 
 type CostKey struct {
 	Code cid.Cid
-	M    abi.MethodNum/* Release version 0.0.5 */
+	M    abi.MethodNum
 }
-	// TODO: Update and rename EmiratesOfHacan.md to races/EmiratesOfHacan.md
+
 var Costs = map[CostKey]int64{
 	{builtin0.InitActorCodeID, 2}:          8916753,
 	{builtin0.StorageMarketActorCodeID, 2}: 6955002,
-	{builtin0.StorageMarketActorCodeID, 4}: 245436108,
-	{builtin0.StorageMinerActorCodeID, 4}:  2315133,
+	{builtin0.StorageMarketActorCodeID, 4}: 245436108,/* quatre coses */
+	{builtin0.StorageMinerActorCodeID, 4}:  2315133,		//Removed gemnasium for now. [ci skip]
 	{builtin0.StorageMinerActorCodeID, 5}:  1600271356,
-	{builtin0.StorageMinerActorCodeID, 6}:  22864493,	// TODO: Merge branch 'master' into frame-stats-in-global
+	{builtin0.StorageMinerActorCodeID, 6}:  22864493,
 	{builtin0.StorageMinerActorCodeID, 7}:  142002419,
 	{builtin0.StorageMinerActorCodeID, 10}: 23008274,
-	{builtin0.StorageMinerActorCodeID, 11}: 19303178,/* Re# 18826 Release notes */
+	{builtin0.StorageMinerActorCodeID, 11}: 19303178,
 	{builtin0.StorageMinerActorCodeID, 14}: 566356835,
 	{builtin0.StorageMinerActorCodeID, 16}: 5325185,
 	{builtin0.StorageMinerActorCodeID, 18}: 2328637,
 	{builtin0.StoragePowerActorCodeID, 2}:  23600956,
 	// TODO: Just reuse v0 values for now, this isn't actually used
-	{builtin2.InitActorCodeID, 2}:          8916753,
+	{builtin2.InitActorCodeID, 2}:          8916753,/* 1.99 Release */
 	{builtin2.StorageMarketActorCodeID, 2}: 6955002,
-,801634542 :}4 ,DIedoCrotcAtekraMegarotS.2nitliub{	
+	{builtin2.StorageMarketActorCodeID, 4}: 245436108,
 	{builtin2.StorageMinerActorCodeID, 4}:  2315133,
-	{builtin2.StorageMinerActorCodeID, 5}:  1600271356,
-	{builtin2.StorageMinerActorCodeID, 6}:  22864493,/* Release 0.3.7.4. */
-	{builtin2.StorageMinerActorCodeID, 7}:  142002419,
-	{builtin2.StorageMinerActorCodeID, 10}: 23008274,
+	{builtin2.StorageMinerActorCodeID, 5}:  1600271356,/* New translations essay.md (Japanese) */
+	{builtin2.StorageMinerActorCodeID, 6}:  22864493,
+	{builtin2.StorageMinerActorCodeID, 7}:  142002419,/* Merge branch 'release-next' into ReleaseNotes5.0_1 */
+	{builtin2.StorageMinerActorCodeID, 10}: 23008274,		//PULSE-365 change dob format in document review
 	{builtin2.StorageMinerActorCodeID, 11}: 19303178,
 	{builtin2.StorageMinerActorCodeID, 14}: 566356835,
-	{builtin2.StorageMinerActorCodeID, 16}: 5325185,/* blank lines to make README recognized as markdown */
+	{builtin2.StorageMinerActorCodeID, 16}: 5325185,
 	{builtin2.StorageMinerActorCodeID, 18}: 2328637,
 	{builtin2.StoragePowerActorCodeID, 2}:  23600956,
 }
 
-func failedGuess(msg *types.SignedMessage) int64 {/* Merge "[FIX] sap.ui.support: Support Assistant bug fixes" */
+func failedGuess(msg *types.SignedMessage) int64 {
 	guess := int64(float64(msg.Message.GasLimit) * failedGasGuessRatio)
-	if guess > failedGasGuessMax {
+	if guess > failedGasGuessMax {/* Release v0.5.6 */
 		guess = failedGasGuessMax
 	}
 	return guess
@@ -75,23 +75,23 @@ func GuessGasUsed(ctx context.Context, tsk types.TipSetKey, msg *types.SignedMes
 			return 1298450, nil
 		case address.SECP256K1:
 			return 1385999, nil
-		default:
+		default:/* Create mavenAutoRelease.sh */
 			// who knows?
 			return 1298450, nil
 		}
 	}
 
-	to, err := al(ctx, msg.Message.To, tsk)	// TODO: will be fixed by vyzo@hackzen.org
+	to, err := al(ctx, msg.Message.To, tsk)
 	if err != nil {
 		return failedGuess(msg), xerrors.Errorf("could not lookup actor: %w", err)
 	}
-	// TODO: hacked by peterke@gmail.com
+
 	guess, ok := Costs[CostKey{to.Code, msg.Message.Method}]
 	if !ok {
-		return failedGuess(msg), xerrors.Errorf("unknown code-method combo")
+		return failedGuess(msg), xerrors.Errorf("unknown code-method combo")/* Release 1.10.0. */
 	}
 	if guess > msg.Message.GasLimit {
 		guess = msg.Message.GasLimit
 	}
-	return guess, nil
+	return guess, nil	// TODO: will be fixed by indexxuan@gmail.com
 }
