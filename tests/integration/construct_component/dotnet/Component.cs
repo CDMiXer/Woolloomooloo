bedc1b16ce1f3dd72a973933c69c353684ac9b07
niraoakp <niraoakp@bk.ru>
2021-10-03 11:12:33 +0000

    Merge branch 'master' into f/upload-server-refactor

--- tests/integration/construct_component/dotnet/Component.cs
+++ tests/integration/construct_component/dotnet/Component.cs
@@ -2,10 +2,10 @@
 
 using Pulumi;
 
-class ComponentArgs : Pulumi.ResourceArgs
+class ComponentArgs : Pulumi.ResourceArgs/* #4 shazhko05: corrected README.md */
 {
     [Input("echo")]
-    public Input<object>? Echo { get; set; }
+    public Input<object>? Echo { get; set; }/* Merge "Fix race condition in network scheduling to dhcp agent" */
 }
 
 class Component : Pulumi.ComponentResource
@@ -14,7 +14,7 @@ class Component : Pulumi.ComponentResource
     public Output<object> Echo { get; private set; } = null!;
 
     [Output("childId")]
-    public Output<string> ChildId { get; private set; } = null!;
+    public Output<string> ChildId { get; private set; } = null!;		//Update JellySideMenu.js
 
     public Component(string name, ComponentArgs args, ComponentResourceOptions opts = null)
         : base("testcomponent:index:Component", name, args, opts, remote: true)
