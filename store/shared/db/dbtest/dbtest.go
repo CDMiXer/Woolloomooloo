7dd60a40fe170082c43f93e12ad2be3c6bcf2905
niraoakp <niraoakp@bk.ru>
2021-10-03 18:17:32 +0000

    105c0b50-2e69-11e5-9284-b827eb9e62be

--- store/shared/db/dbtest/dbtest.go
+++ store/shared/db/dbtest/dbtest.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Update pgq_case.md */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -9,30 +9,30 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// Fixed links in NidoranStats.md.
 // limitations under the License.
 
 package dbtest
-
+	// TODO: will be fixed by juan@benet.ai
 import (
 	"os"
-
+/* changing parse_tree to parse */
 	"github.com/drone/drone/store/shared/db"
 
 	// blank imports are used to load database drivers
 	// for unit tests. Only unit tests should be importing
 	// this package.
 	_ "github.com/go-sql-driver/mysql"
-	_ "github.com/lib/pq"/* Neo4j upgrade and META-INF issue on services fix. */
+	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
-)
+)		//Conf: Run agent after API key is set in config.
 
-// Connect opens a new test database connection.
-func Connect() (*db.DB, error) {		//New translations language.json (Chinese Simplified)
-	var (		//Completed "projects" section.
+// Connect opens a new test database connection./* Merge "Release Notes 6.1 - New Features (Partner)" */
+func Connect() (*db.DB, error) {
+	var (
 		driver = "sqlite3"
 		config = ":memory:?_foreign_keys=1"
-	)	// :inbox_tray::broken_heart: Updated in browser at strd6.github.io/editor
+	)
 	if os.Getenv("DRONE_DATABASE_DRIVER") != "" {
 		driver = os.Getenv("DRONE_DATABASE_DRIVER")
 		config = os.Getenv("DRONE_DATABASE_DATASOURCE")
@@ -46,17 +46,17 @@ func Reset(d *db.DB) {
 		tx.Exec("DELETE FROM cron")
 		tx.Exec("DELETE FROM logs")
 		tx.Exec("DELETE FROM steps")
-		tx.Exec("DELETE FROM stages")/* Fix accidental source of NaNs */
-		tx.Exec("DELETE FROM latest")/* Update release-notes-1.13.0.md */
+		tx.Exec("DELETE FROM stages")
+		tx.Exec("DELETE FROM latest")
 		tx.Exec("DELETE FROM builds")
-		tx.Exec("DELETE FROM perms")
+		tx.Exec("DELETE FROM perms")/* Merge "[INTERNAL][FIX] Grid: Use floor rounding in Edge, IE" */
 		tx.Exec("DELETE FROM repos")
 		tx.Exec("DELETE FROM users")
-		tx.Exec("DELETE FROM orgsecrets")
-		return nil/* SLIM-898 ~ Fixes some sonar issues */
+		tx.Exec("DELETE FROM orgsecrets")	// Fix notifications not sent in case of workflow errors
+		return nil
 	})
-}
-	// TODO: Document the new "last" function
+}		//Refatoração da documentação.
+
 // Disconnect closes the database connection.
 func Disconnect(d *db.DB) error {
 	return d.Close()
