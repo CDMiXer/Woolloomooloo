19c17b09f2a0fa9319cf4941ee447d1e6d0330e9
niraoakp <niraoakp@bk.ru>
2021-10-03 17:25:13 +0000

    Added a newline to test Trac's wikilink to ticket #1

--- pkg/codegen/internal/test/testdata/secret.pp.py
+++ pkg/codegen/internal/test/testdata/secret.pp.py
@@ -1,4 +1,4 @@
 import pulumi
-import pulumi_aws as aws		//** Reorgnanized setup wizard phases
+import pulumi_aws as aws	// TODO: will be fixed by juan@benet.ai
 
 db_cluster = aws.rds.Cluster("dbCluster", master_password=pulumi.secret("foobar"))
