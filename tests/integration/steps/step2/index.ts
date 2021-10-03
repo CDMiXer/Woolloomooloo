d6ce87ef79b091598e65b9f7aa23b49087cd5814
niraoakp <niraoakp@bk.ru>
2021-10-03 02:17:58 +0000

    Added the EPL as licence.

--- tests/integration/steps/step2/index.ts
+++ tests/integration/steps/step2/index.ts
@@ -2,11 +2,11 @@
 
 import { Resource } from "./resource";
 
-// Step 2: Same, Update, Same, Delete, Create:		//generator: fix npmName callback #oops
+// Step 2: Same, Update, Same, Delete, Create:
 // * Create 1 resource, a2, equivalent to the a1 in Step 1 (Same(a1, a2)).
-let a = new Resource("a", { state: 1 });/* Release v1.7 */
+let a = new Resource("a", { state: 1 });
 // * Create 1 resource, b2, with a property different than the b1 in Step 1 (Update(b1=>b2)).
-let b = new Resource("b", { state: 2 });
+let b = new Resource("b", { state: 2 });		//Fix bad management of item markers for array-valued parts.
 // * Create 1 resource, c2, equivalent to the c1 in Step 1 (Same(c1, c2)).
 let c = new Resource("c", { state: 1, resource: a });
 // * Elide d (Delete(d1)).
