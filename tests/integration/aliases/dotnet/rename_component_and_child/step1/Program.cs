6bc5d4f2ca75e9ed33eda97a3571494f874fcbc8
niraoakp <niraoakp@bk.ru>
2021-10-03 20:27:37 +0000

    Merge branch 'master' of https://github.com/javaappplatform/basis.git

--- tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
-using Pulumi;/* Release 1.11.10 & 2.2.11 */
+using Pulumi;
 
-class Resource : ComponentResource/* NP-14457. Add delete to employe super api. */
+class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
@@ -14,22 +14,22 @@ class Resource : ComponentResource/* NP-14457. Add delete to employe super api. 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive : ComponentResource
 {
-    private Resource resource;/* Angle grid. */
-		//b8a850cf-2eae-11e5-8d97-7831c1d44c14
-    public ComponentFive(string name, ComponentResourceOptions options = null)		//Match for UUIDv4
+    private Resource resource;
+
+    public ComponentFive(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentFive", name, options)
     {
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
-/* trigger "go-toast/toast" by git@jacobmarshall.co */
-class Program	// TODO: will be fixed by mail@bitpshr.net
+
+class Program
 {
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => 
         {
             var comp5 = new ComponentFive("comp5");
-        });/* Update nuspec to point at Release bits */
+        });
     }
 }
