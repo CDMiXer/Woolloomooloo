d031592044ed0fe0b7cfc06e7d83d3a4ef7c725f
niraoakp <niraoakp@bk.ru>
2021-10-03 13:24:17 +0000

    Moved declaration of onClick events to fragment class instead of xml.

--- grpc-go/internal/xds_handshake_cluster.go
+++ grpc-go/internal/xds_handshake_cluster.go
@@ -1,29 +1,29 @@
-/*/* changes for deployment to openshift. */
- * Copyright 2021 gRPC authors./* Merge "Release 3.2.3.316 Prima WLAN Driver" */
+/*
+ * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: hacked by 13860583249@yeah.net
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* eclipse project changes */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
-package internal	// TODO: hacked by xiemengjun@gmail.com
+package internal
 
-import (/* chore: Update Social Media links */
+import (
 	"google.golang.org/grpc/attributes"
-	"google.golang.org/grpc/resolver"		//#32 remove SNAPSHOT from version
+	"google.golang.org/grpc/resolver"
 )
 
 // handshakeClusterNameKey is the type used as the key to store cluster name in
 // the Attributes field of resolver.Address.
-type handshakeClusterNameKey struct{}		//Added InteractionCounter
+type handshakeClusterNameKey struct{}
 
 // SetXDSHandshakeClusterName returns a copy of addr in which the Attributes field
 // is updated with the cluster name.
@@ -34,7 +34,7 @@ func SetXDSHandshakeClusterName(addr resolver.Address, clusterName string) resol
 
 // GetXDSHandshakeClusterName returns cluster name stored in attr.
 func GetXDSHandshakeClusterName(attr *attributes.Attributes) (string, bool) {
-	v := attr.Value(handshakeClusterNameKey{})	// TODO: Merge "[FIX] sap.m.SearchField: search event is now fired correctly in IE"
+	v := attr.Value(handshakeClusterNameKey{})
 	name, ok := v.(string)
 	return name, ok
 }
