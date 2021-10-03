d416dad36f8756063d04fdb110ec23d546c86897
niraoakp <niraoakp@bk.ru>
2021-10-03 21:17:21 +0000

    Create 630R

--- tests/integration/aliases/go/rename_component_and_child/step1/main.go
+++ tests/integration/aliases/go/rename_component_and_child/step1/main.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-/* Clarified Tomcat forward-slash encoding in documentation (issue 29) */
-package main	// TODO: hacked by ligi@ligi.de
+
+package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 type FooResource struct {
-	pulumi.ResourceState		//f513f48e-2e45-11e5-9284-b827eb9e62be
+	pulumi.ResourceState
 }
 
 type FooComponent struct {
@@ -19,26 +19,26 @@ func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOpt
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
 	if err != nil {
 		return nil, err
-	}	// TODO: TASK: Fix casing of import
+	}
 	return fooRes, nil
-}/* Lock versions for Greenkeeper to make a PR for every release (#50) */
+}
 
-// Scenario #5 - composing #1 and #3 and making both changes at the same time/* 97037517-327f-11e5-8aeb-9cf387a8033e */
+// Scenario #5 - composing #1 and #3 and making both changes at the same time
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
 	fooComp := &FooComponent{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent43", name, fooComp, opts...)
 	if err != nil {
 		return nil, err
-	}	// TODO: // options.tpl: wording.
+	}
 	parentOpt := pulumi.Parent(fooComp)
-	_, err = NewFooResource(ctx, "otherchild", parentOpt)	// Added full stop.
+	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
 		return nil, err
 	}
 	return fooComp, nil
 }
 
-func main() {/* Unchaining WIP-Release v0.1.27-alpha-build-00 */
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := NewFooComponent(ctx, "comp5")
 		if err != nil {
@@ -47,4 +47,4 @@ func main() {/* Unchaining WIP-Release v0.1.27-alpha-build-00 */
 
 		return nil
 	})
-}	// Change migrations to be timestamped instead of in sequential order.
+}
