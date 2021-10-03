e5932e67034141bd84f9469540c872b2b00f21e1
niraoakp <niraoakp@bk.ru>
2021-10-03 20:35:54 +0000

    wait for DOM to load

--- tests/integration/duplicate_urns/step4/index.ts
+++ tests/integration/duplicate_urns/step4/index.ts
@@ -12,15 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* Release v0.37.0 */
 
 // "a" is already in the snapshot and will be replaced.
 const a = new Resource("a", { state: 7 });
-	// TODO: PAASMANUT-1660: Remoção da função getValueIsNotNumber da categoria data.
+		//add travis.ci badge to README
 // At this point there will be an "a" in the checkpoint that's pending deletion.
-
+/* ui, middleware: fix viewer lockdown mode for patentview.elmyra.de */
 // "b" is not in the snapshot. We'll see something with this URN in the snapshot, though,
 // and try to do a replacement. This is bad because the thing we're replacing is pending deletion.
-const b = new Resource("a", { state: 5 }, { dependsOn: a });
+const b = new Resource("a", { state: 5 }, { dependsOn: a });/* Merge pull request #2470 from seabaylea/dispatch-xctest */
 
 // This should fail, but gracefully.
