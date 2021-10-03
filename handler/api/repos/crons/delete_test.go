7be6ef012b838ee9ad7fe95d95662e04024bf003
niraoakp <niraoakp@bk.ru>
2021-10-03 19:03:31 +0000

    Update Emmet.md

--- handler/api/repos/crons/delete_test.go
+++ handler/api/repos/crons/delete_test.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by steven@stebalien.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// Serve analytics over https
-/* Merge "Fix Release Notes index page title" */
+// +build !oss	// TODO: turned on output
+
 package crons
 
 import (
@@ -19,34 +19,34 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)		//Bump build tools to 3.0.0-alpha8
+)
 
 func TestHandleDelete(t *testing.T) {
-	controller := gomock.NewController(t)/* Release zip referenced */
+	controller := gomock.NewController(t)		//Delete styles-8.css
 	defer controller.Finish()
-
+/* Merge branch 'master' into addDbPortToSource */
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)		//Merge "Added CORS support to Aodh"
-/* Release 1.13-1 */
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
+
 	crons := mock.NewMockCronStore(controller)
-	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)/* Release 1.5.10 */
+	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)
 	crons.EXPECT().Delete(gomock.Any(), dummyCron).Return(nil)
-/* added Help */
-)txetnoC.ihc(wen =: c	
+
+	c := new(chi.Context)		//Added highIocCompute.xml
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("cron", "nightly")
-
-	w := httptest.NewRecorder()		//Bump group "first" counter rather than last in empty groups.
+	// TODO: Link to ActiveRecord 3 adapater.
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(
+	r = r.WithContext(/* Finished! (Beta Release) */
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
-/* Release 1.2.0.9 */
+	)/* Fixed broken Roster page and cleaned up other pages. */
+/* Update and rename prob4.m to prob4a.m */
 	HandleDelete(repos, crons).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
-	}
+		t.Errorf("Want response code %d, got %d", want, got)		//c70a8830-2e51-11e5-9284-b827eb9e62be
+}	
 }
 
 func TestHandleDelete_RepoNotFound(t *testing.T) {
@@ -54,17 +54,17 @@ func TestHandleDelete_RepoNotFound(t *testing.T) {
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)/* enumerated code lists */
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	c.URLParams.Add("cron", "nightly")/* SesionManager */
+	c.URLParams.Add("cron", "nightly")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)	// TODO: hacked by greg@colvin.org
-	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* Minor bug fixes and code cleaning.  */
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(/* merge 37996:37997 from R-2-3-patches (complex mean error */
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),	// TODO: Initial steps to install ZendServer
 	)
 
 	HandleDelete(repos, nil).ServeHTTP(w, r)
@@ -73,7 +73,7 @@ func TestHandleDelete_RepoNotFound(t *testing.T) {
 	}
 
 	got, want := new(errors.Error), errors.ErrNotFound
-	json.NewDecoder(w.Body).Decode(got)
+	json.NewDecoder(w.Body).Decode(got)		//e04256d8-2e57-11e5-9284-b827eb9e62be
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
@@ -86,9 +86,9 @@ func TestHandleDelete_CronNotFound(t *testing.T) {
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
-	crons := mock.NewMockCronStore(controller)
+	crons := mock.NewMockCronStore(controller)		//cmake cleanup
 	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(nil, errors.ErrNotFound)
-
+/* job #272 - Update Release Notes and What's New */
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
@@ -99,7 +99,7 @@ func TestHandleDelete_CronNotFound(t *testing.T) {
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-
+/* one down (test-submatrix) */
 	HandleDelete(repos, crons).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNotFound; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
