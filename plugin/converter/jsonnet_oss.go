80bf502fd4be7d8029051b3b7a7a46370513eaec
niraoakp <niraoakp@bk.ru>
2021-10-03 03:45:19 +0000

    new browser icon

--- plugin/converter/jsonnet_oss.go
+++ plugin/converter/jsonnet_oss.go
@@ -5,23 +5,23 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Singularize AndroidObservables, move to observables package */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 1.0.31 - new permission check methods */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-// +build oss
+	// TODO: will be fixed by hugomrdias@gmail.com
+// +build oss	// Merge "Don't raise not found in delete raw template"
 
 package converter
 
-import (
+import (/* Release DBFlute-1.1.0-sp1 */
 	"github.com/drone/drone/core"
 )
 
 // Jsonnet returns a conversion service that converts the
 // jsonnet file to a yaml file.
-func Jsonnet(enabled bool) core.ConvertService {
+func Jsonnet(enabled bool) core.ConvertService {/* Merge "Release 3.2.3.448 Prima WLAN Driver" */
 	return new(noop)
 }
