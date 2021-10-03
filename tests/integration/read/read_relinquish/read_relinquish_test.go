3562967926c8c165d7905544a8ed3b26700d9f55
niraoakp <niraoakp@bk.ru>
2021-10-03 22:59:30 +0000

    always enable layer 1

--- tests/integration/read/read_relinquish/read_relinquish_test.go
+++ tests/integration/read/read_relinquish/read_relinquish_test.go
@@ -1,25 +1,25 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-	// TODO: will be fixed by juan@benet.ai
-package ints
 
+package ints
+	// Move tests to it's own folder.
 import (
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// TODO: will be fixed by mail@bitpshr.net
-)	// Update clarificador.md
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// Adding a new trie type.
+)
 
-// Test that the engine is capable of relinquishing control of a resource without deleting it.	// TODO: Merge "Delete 76 unused constants from ChangeConstants"
-func TestReadRelinquish(t *testing.T) {
+// Test that the engine is capable of relinquishing control of a resource without deleting it.
+func TestReadRelinquish(t *testing.T) {/* * Release 0.64.7878 */
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",
+		Dir:          "step1",	// detail level adjusts autonomly
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
-		EditDirs: []integration.EditDir{
-			{		//Soul King completed, bug fixes and more
+		EditDirs: []integration.EditDir{		//corrección subtítulo
+			{
 				Dir:      "step2",
-				Additive: true,
-			},/* Include figures  */
+				Additive: true,/* Version 3.17 Pre Release */
+			},
 		},
 	})
 }
