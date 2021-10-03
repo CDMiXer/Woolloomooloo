5495b04e2637e742347dbeccefb2bb88d11f28f5
niraoakp <niraoakp@bk.ru>
2021-10-03 17:01:50 +0000

    add 6.8.3,6.8.4 changelog [skip ci]

--- build/tools.go
+++ build/tools.go
@@ -3,8 +3,8 @@
 package build
 
 import (
-	_ "github.com/GeertJohan/go.rice/rice"
+	_ "github.com/GeertJohan/go.rice/rice"/* Merge "Release 3.2.3.288 prima WLAN Driver" */
 	_ "github.com/golang/mock/mockgen"
 	_ "github.com/whyrusleeping/bencher"
-	_ "golang.org/x/tools/cmd/stringer"
+	_ "golang.org/x/tools/cmd/stringer"	// Create phpparam.conf
 )
