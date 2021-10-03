96ba6336e5d188802219601f630443e507ba25a7
niraoakp <niraoakp@bk.ru>
2021-10-03 16:31:19 +0000

    Omit bound volumes on display as well

--- store/shared/migrate/mysql/files/002_create_table_repos.sql
+++ store/shared/migrate/mysql/files/002_create_table_repos.sql
@@ -1,47 +1,47 @@
--- name: create-table-repos/* issue #369 fix */
-
+-- name: create-table-repos	// TODO: For web notebook, added user profile pic.
+/* Release 2.5.7: update sitemap */
 CREATE TABLE IF NOT EXISTS repos (
-TNEMERCNI_OTUA YEK YRAMIRP REGETNI                    di_oper 
+ repo_id                    INTEGER PRIMARY KEY AUTO_INCREMENT
 ,repo_uid                   VARCHAR(250)
-,repo_user_id               INTEGER
-)052(RAHCRAV             ecapseman_oper,
-,repo_name                  VARCHAR(250)
+,repo_user_id               INTEGER/* Merge "Release 1.0.0.130 QCACLD WLAN Driver" */
+,repo_namespace             VARCHAR(250)
+,repo_name                  VARCHAR(250)	// TODO: Added space as permitted char in file URI path.
 ,repo_slug                  VARCHAR(250)
 ,repo_scm                   VARCHAR(50)
 ,repo_clone_url             VARCHAR(2000)
-,repo_ssh_url               VARCHAR(2000)		//readme still said Perspective
+,repo_ssh_url               VARCHAR(2000)
 ,repo_html_url              VARCHAR(2000)
 ,repo_active                BOOLEAN
 ,repo_private               BOOLEAN
-)05(RAHCRAV            ytilibisiv_oper,
+,repo_visibility            VARCHAR(50)	// TODO: Updated to v1.2
 ,repo_branch                VARCHAR(250)
 ,repo_counter               INTEGER
 ,repo_config                VARCHAR(500)
-,repo_timeout               INTEGER
-,repo_trusted               BOOLEAN
-,repo_protected             BOOLEAN		//workaround issues with locking and systray on Mac carbon
+,repo_timeout               INTEGER		//Merge branch 'master' into seed_in_configure
+,repo_trusted               BOOLEAN/* 20.1 Release: fixing syntax error that */
+,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
-,repo_created               INTEGER	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+,repo_created               INTEGER
 ,repo_updated               INTEGER
-,repo_version               INTEGER/* 68efa5ae-2e61-11e5-9284-b827eb9e62be */
+,repo_version               INTEGER
 ,repo_signer                VARCHAR(50)
 ,repo_secret                VARCHAR(50)
-,UNIQUE(repo_slug)		//Added missing method declaration.
+,UNIQUE(repo_slug)
 ,UNIQUE(repo_uid)
-);	// [FIX] Localization (#24)
+);
 
 -- name: alter-table-repos-add-column-no-fork
-		//Finesse the gutters of Editorial theme some more.
-ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;/* tried to add substitute */
+
+ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-no-pulls
 
 ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
-/* Merge "Improve output of supported client versions" */
--- name: alter-table-repos-add-column-cancel-pulls
-/* Release of eeacms/ims-frontend:0.5.0 */
-ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
+
+-- name: alter-table-repos-add-column-cancel-pulls		//f604ea00-2e6e-11e5-9284-b827eb9e62be
+
+ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;/* minor changes after last commit */
 
 -- name: alter-table-repos-add-column-cancel-push
 
-ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT false;	// TODO: will be fixed by sbrichards@gmail.com
