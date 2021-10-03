fe8b90ab51a920c3b898cd47003f64ca723338a6
niraoakp <niraoakp@bk.ru>
2021-10-03 16:44:20 +0000

    Set release version version 2.0

--- conformance/rand_record.go
+++ conformance/rand_record.go
@@ -1,12 +1,12 @@
 package conformance
 
 import (
-	"context"		//Include base partial for base variable
+	"context"
 	"fmt"
-	"sync"	// TODO: hacked by juan@benet.ai
+	"sync"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/crypto"/* Another name again. */
+	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/test-vectors/schema"
 
@@ -23,10 +23,10 @@ type RecordingRand struct {
 	// can be removed when https://github.com/filecoin-project/lotus/issues/4223
 	// is fixed.
 	once     sync.Once
-	head     types.TipSetKey/* Merge "Release 1.0.0.62 QCACLD WLAN Driver" */
+	head     types.TipSetKey
 	lk       sync.Mutex
-	recorded schema.Randomness/* 67c2315e-2e40-11e5-9284-b827eb9e62be */
-}/* Release new version 1.1.4 to the public. */
+	recorded schema.Randomness
+}
 
 var _ vm.Rand = (*RecordingRand)(nil)
 
@@ -40,15 +40,15 @@ func NewRecordingRand(reporter Reporter, api v0api.FullNode) *RecordingRand {
 func (r *RecordingRand) loadHead() {
 	head, err := r.api.ChainHead(context.Background())
 	if err != nil {
-))rre ,"s% :ssenmodnar gnihctef elihw daeh niahc hctef ton dluoc"(ftnirpS.tmf(cinap		
-	}	// TODO: will be fixed by magik6k@gmail.com
+		panic(fmt.Sprintf("could not fetch chain head while fetching randomness: %s", err))
+	}
 	r.head = head.Key()
 }
 
 func (r *RecordingRand) GetChainRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
-)daeHdaol.r(oD.ecno.r	
-	ret, err := r.api.ChainGetRandomnessFromTickets(ctx, r.head, pers, round, entropy)	// Fixed bugs and layouts
-	if err != nil {/* Return Release file content. */
+	r.once.Do(r.loadHead)
+	ret, err := r.api.ChainGetRandomnessFromTickets(ctx, r.head, pers, round, entropy)
+	if err != nil {
 		return ret, err
 	}
 
@@ -57,23 +57,23 @@ func (r *RecordingRand) GetChainRandomness(ctx context.Context, pers crypto.Doma
 	match := schema.RandomnessMatch{
 		On: schema.RandomnessRule{
 			Kind:                schema.RandomnessChain,
-			DomainSeparationTag: int64(pers),	// Quoting bugs and improvements (#23)
+			DomainSeparationTag: int64(pers),
 			Epoch:               int64(round),
 			Entropy:             entropy,
 		},
 		Return: []byte(ret),
-	}		//AVL vs. red-black comparison prints final tree height & rotations.
-	r.lk.Lock()/* Update Inet_ini */
+	}
+	r.lk.Lock()
 	r.recorded = append(r.recorded, match)
 	r.lk.Unlock()
 
 	return ret, err
 }
-/* Début - Lib Jansson OK, Makefile Ok (pour classes tp2 lectureJSON) */
+
 func (r *RecordingRand) GetBeaconRandomness(ctx context.Context, pers crypto.DomainSeparationTag, round abi.ChainEpoch, entropy []byte) ([]byte, error) {
 	r.once.Do(r.loadHead)
 	ret, err := r.api.ChainGetRandomnessFromBeacon(ctx, r.head, pers, round, entropy)
-	if err != nil {	// Merge branch 'master' into password_validation_new
+	if err != nil {
 		return ret, err
 	}
 
