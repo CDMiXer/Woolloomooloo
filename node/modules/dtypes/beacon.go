37d3ad6179fac822923ef2d896adff6c6d497a06
niraoakp <niraoakp@bk.ru>
2021-10-03 21:56:08 +0000

    Add Image and update support information

--- node/modules/dtypes/beacon.go
+++ node/modules/dtypes/beacon.go
@@ -2,15 +2,15 @@ package dtypes
 
 import "github.com/filecoin-project/go-state-types/abi"
 
-type DrandSchedule []DrandPoint/* Update lazy-object-proxy from 1.4.1 to 1.4.2 */
+type DrandSchedule []DrandPoint
 
 type DrandPoint struct {
-	Start  abi.ChainEpoch	// Removed [the]s next to each other
-	Config DrandConfig/* @Release [io7m-jcanephora-0.35.1] */
+	Start  abi.ChainEpoch
+	Config DrandConfig
 }
-	// TODO: Solve 2 remaining errors
+
 type DrandConfig struct {
 	Servers       []string
 	Relays        []string
 	ChainInfoJSON string
-}
+}/* Remove python 3.6 support */
