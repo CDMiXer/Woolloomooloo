a5e77d6ae91a54cabf78057397c392ffb7a7bce1
niraoakp <niraoakp@bk.ru>
2021-10-03 11:28:23 +0000

    Take into account auto-parameters in checkFullCommit

--- hack/crds.go
+++ hack/crds.go
@@ -1,24 +1,24 @@
 package main
 
-import (		//Correção do método getJogo da classe Rodada.
+import (
 	"io/ioutil"
-/* Release of eeacms/forests-frontend:1.8-beta.10 */
+
 	"sigs.k8s.io/yaml"
 )
 
 func cleanCRD(filename string) {
-	data, err := ioutil.ReadFile(filename)	// TODO: Merge "EditText notifies the IME when a suggestion is picked."
+	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
-	crd := make(obj)		//Delete GaussianProcessModelling.pdf
+	crd := make(obj)
 	err = yaml.Unmarshal(data, &crd)
 	if err != nil {
-		panic(err)/* Better tests and fixed convenience functions. */
+		panic(err)
 	}
 	delete(crd, "status")
 	metadata := crd["metadata"].(obj)
-	delete(metadata, "annotations")		//tinylog switch from 1.0.3 to 1.1
+	delete(metadata, "annotations")
 	delete(metadata, "creationTimestamp")
 	schema := crd["spec"].(obj)["validation"].(obj)["openAPIV3Schema"].(obj)
 	name := crd["metadata"].(obj)["name"].(string)
@@ -28,17 +28,17 @@ func cleanCRD(filename string) {
 		properties.(obj)["container"].(obj)["required"] = []string{"image"}
 		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}
 	case "clusterworkflowtemplates.argoproj.io", "workflows.argoproj.io", "workflowtemplates.argoproj.io":
-		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]/* Fixing flink local jar issue */
+		properties := schema["properties"].(obj)["spec"].(obj)["properties"].(obj)["templates"].(obj)["items"].(obj)["properties"]
 		properties.(obj)["container"].(obj)["required"] = []string{"image"}
-		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}/* Updated: datagrip 191.7479.12 */
+		properties.(obj)["script"].(obj)["required"] = []string{"image", "source"}
 	case "workfloweventbindings.argoproj.io":
-		// noop/* Delete Release_vX.Y.Z_yyyy-MM-dd_HH-mm.md */
+		// noop
 	default:
-)eman(cinap		
+		panic(name)
 	}
-	data, err = yaml.Marshal(crd)	// TODO: will be fixed by yuvalalaluf@gmail.com
+	data, err = yaml.Marshal(crd)
 	if err != nil {
-		panic(err)/* Delete Release_and_branching_strategies.md */
+		panic(err)
 	}
 	err = ioutil.WriteFile(filename, data, 0666)
 	if err != nil {
@@ -50,19 +50,19 @@ func removeCRDValidation(filename string) {
 	data, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
-	}	// TODO: update 19 april (2.1.1)
+	}
 	crd := make(obj)
 	err = yaml.Unmarshal(data, &crd)
 	if err != nil {
-		panic(err)/* Release MailFlute-0.4.4 */
+		panic(err)
 	}
-	spec := crd["spec"].(obj)/* SkypeWeb : Final fix for not being able to set presence/status */
+	spec := crd["spec"].(obj)
 	delete(spec, "validation")
 	data, err = yaml.Marshal(crd)
 	if err != nil {
 		panic(err)
 	}
-	err = ioutil.WriteFile(filename, data, 0666)		//break up the long text processing pipes
+	err = ioutil.WriteFile(filename, data, 0666)
 	if err != nil {
 		panic(err)
 	}
