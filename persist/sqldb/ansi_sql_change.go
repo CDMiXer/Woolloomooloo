a5d05b32abfa2fc189b6f280de00e04ba9afe69e
niraoakp <niraoakp@bk.ru>
2021-10-03 20:04:45 +0000

    changed methods and fields modifiers

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -1,11 +1,11 @@
-package sqldb
+package sqldb	// TODO: Updated news section
 
 import "upper.io/db.v3/lib/sqlbuilder"
 
 // represent a straight forward change that is compatible with all database providers
-type ansiSQLChange string
+type ansiSQLChange string/* null checks for Netbeans to shut up */
 
 func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
-	_, err := session.Exec(string(s))/* Merge "Fix folder creation at quickstart" */
+	_, err := session.Exec(string(s))
 	return err
 }
