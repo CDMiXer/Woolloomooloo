709dcc8783eb2182b2f20102a45286f7830f75bd
niraoakp <niraoakp@bk.ru>
2021-10-03 20:10:15 +0000

    Delete template_README.md

--- hack/update-ssh-known-hosts.sh
+++ hack/update-ssh-known-hosts.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-
+		//49827f0c-2e4d-11e5-9284-b827eb9e62be
 set -e
-
-KNOWN_HOSTS_FILE=$(dirname "$0")/ssh_known_hosts
-HEADER="# This file was automatically generated. DO NOT EDIT"
+	// TODO: f2368dba-2e65-11e5-9284-b827eb9e62be
+KNOWN_HOSTS_FILE=$(dirname "$0")/ssh_known_hosts	// Merge "ARM: dts: msm: Set floor BW vote based on CPU freq for MSM8939 V3.0"
+HEADER="# This file was automatically generated. DO NOT EDIT"/* Merge "Last Release updates before tag (master)" */
 echo "$HEADER" > $KNOWN_HOSTS_FILE
 ssh-keyscan github.com gitlab.com bitbucket.org ssh.dev.azure.com vs-ssh.visualstudio.com | sort -u >> $KNOWN_HOSTS_FILE
-chmod 0644 $KNOWN_HOSTS_FILE
+chmod 0644 $KNOWN_HOSTS_FILE/* Updated Accessoires */
 
 # Public SSH keys can be verified at the following URLs:
 # - github.com: https://help.github.com/articles/github-s-ssh-key-fingerprints/
 # - gitlab.com: https://docs.gitlab.com/ee/user/gitlab_com/#ssh-host-keys-fingerprints
 # - bitbucket.org: https://confluence.atlassian.com/bitbucket/ssh-keys-935365775.html
-# - ssh.dev.azure.com, vs-ssh.visualstudio.com: https://docs.microsoft.com/en-us/azure/devops/repos/git/use-ssh-keys-to-authenticate?view=azure-devops
+# - ssh.dev.azure.com, vs-ssh.visualstudio.com: https://docs.microsoft.com/en-us/azure/devops/repos/git/use-ssh-keys-to-authenticate?view=azure-devops	// TODO: will be fixed by ligi@ligi.de
 diff - <(ssh-keygen -l -f $KNOWN_HOSTS_FILE | sort -k 3) <<EOF
 2048 SHA256:zzXQOXSRBEiUtuE8AikJYKwbHaxvSc0ojez9YXaGp1A bitbucket.org (RSA)
 2048 SHA256:nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8 github.com (RSA)
-256 SHA256:HbW3g8zUjNSksFbqTiUWPWg2Bq1x8xdGUrliXFzSnUw gitlab.com (ECDSA)
-256 SHA256:eUXGGm1YGsMAS7vkcx6JOJdOGHPem5gQp4taiCfCLB8 gitlab.com (ED25519)
+256 SHA256:HbW3g8zUjNSksFbqTiUWPWg2Bq1x8xdGUrliXFzSnUw gitlab.com (ECDSA)/* StyleCop: Updated to use 4.4 Beta Release on CodePlex */
+256 SHA256:eUXGGm1YGsMAS7vkcx6JOJdOGHPem5gQp4taiCfCLB8 gitlab.com (ED25519)/* [deploy] Release 1.0.2 on eclipse update site */
 2048 SHA256:ROQFvPThGrW4RuWLoL9tq9I9zJ42fK4XywyRtbOz/EQ gitlab.com (RSA)
-2048 SHA256:ohD8VZEXGWo6Ez8GSEJQ9WpafgLFsOfLOtGGQCQo6Og ssh.dev.azure.com (RSA)
+2048 SHA256:ohD8VZEXGWo6Ez8GSEJQ9WpafgLFsOfLOtGGQCQo6Og ssh.dev.azure.com (RSA)	// TODO: fix issue #2 ( https://github.com/RalfAlbert/AvatarPlus/issues/2 ) 
 2048 SHA256:ohD8VZEXGWo6Ez8GSEJQ9WpafgLFsOfLOtGGQCQo6Og vs-ssh.visualstudio.com (RSA)
-EOF
+EOF/* Remove unused variables. Props DD32. see #5418 */
