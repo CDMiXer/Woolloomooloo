23c424810d1948f6171f4e13cacda8947f91d6f0
niraoakp <niraoakp@bk.ru>
2021-10-03 18:05:56 +0000

    Merge "Add additional assertions to AbstractQueryChangesTest#byComment()"

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
@@ -1,8 +1,8 @@
-import * as pulumi from "@pulumi/pulumi";/* Update lcltblDBReleases.xml */
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-
+		//generate autoload_classmap and add to module
 const logs = new aws.s3.Bucket("logs", {});
 const bucket = new aws.s3.Bucket("bucket", {loggings: [{
     targetBucket: logs.bucket,
-}]});/* wrong assertion */
+}]});
 export const targetBucket = bucket.loggings[0].targetBucket;
