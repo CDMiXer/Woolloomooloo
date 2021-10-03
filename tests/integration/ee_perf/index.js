ff3849b22764a309338bdc8aed60da2aac06e26a
niraoakp <niraoakp@bk.ru>
2021-10-03 04:56:05 +0000

    Include local Maven repository in build

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Release history */
 "use strict";
 const pulumi = require("@pulumi/pulumi");
 
-const config = new pulumi.Config();		//saml: IdP/SAML2: Clarify variable names, fix comments.
+const config = new pulumi.Config();	// TODO: will be fixed by martin2cai@hotmail.com
 const iterations = config.getNumber("count") || 1000;
 
 // Emit many, many diagnostic events from the engine to stress test the
-// ability to record those events on the Pulumi Service.
+// ability to record those events on the Pulumi Service.		//Fixed markup bug
 console.log("Starting to spam a bunch of diagnostic messages...");
 for (let i = 0; i < iterations; i++) {
     console.log(`${i}: The current time is ${new Date()}`);
 }
-console.log("done");
+console.log("done");/* Merge "Use setUp() method for neutron rest test" */
