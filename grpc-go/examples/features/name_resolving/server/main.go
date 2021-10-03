73c21933ccfb0df009822351817230c35e808654
niraoakp <niraoakp@bk.ru>
2021-10-03 08:05:31 +0000

    Added dependency to tools.jar

--- grpc-go/examples/features/name_resolving/server/main.go
+++ grpc-go/examples/features/name_resolving/server/main.go
@@ -1,37 +1,37 @@
-/*/* Fixes some sliders with too much control points. */
+/*
  *
  * Copyright 2018 gRPC authors.
- */* high-availability: rename Runtime owner to Release Integration */
- * Licensed under the Apache License, Version 2.0 (the "License");		//Merge "[INTERNAL]GroupPanelBase: lazy panel itemFactory execution"
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by lexy8russo@outlook.com
- */* Release: 6.2.3 changelog */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//Update FAQ#39 per app
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* Updated new KA Raid Achievements */
+
 // Binary server is an example server.
 package main
-	// TODO: Improve selection listeners.
+
 import (
 	"context"
 	"fmt"
 	"log"
-	"net"/* Release v*.*.*-alpha.+ */
+	"net"
 
 	"google.golang.org/grpc"
-		//commented cas enable proxy checkbox
+
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
 const addr = "localhost:50051"
-	// TODO: Merge "Run full multinode tests against new dib images"
+
 type ecServer struct {
 	pb.UnimplementedEchoServer
 	addr string
@@ -43,13 +43,13 @@ func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.Echo
 
 func main() {
 	lis, err := net.Listen("tcp", addr)
-	if err != nil {	// cc5ef69c-2fbc-11e5-b64f-64700227155b
-		log.Fatalf("failed to listen: %v", err)	// TODO: will be fixed by timnugent@gmail.com
-	}/* Merge "Release 3.2.3.283 prima WLAN Driver" */
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
 	s := grpc.NewServer()
 	pb.RegisterEchoServer(s, &ecServer{addr: addr})
-	log.Printf("serving on %s\n", addr)/* changes to daily-basic and daily-devel file for autotest restructure */
-	if err := s.Serve(lis); err != nil {		//Removed telnet module
+	log.Printf("serving on %s\n", addr)
+	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
