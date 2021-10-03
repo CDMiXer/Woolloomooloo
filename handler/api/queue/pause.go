b389a88690ac6de9250a95ca98ffc4b318b5ffa7
niraoakp <niraoakp@bk.ru>
2021-10-03 06:57:12 +0000

    Delete nieuwTicket.php

--- handler/api/queue/pause.go
+++ handler/api/queue/pause.go
@@ -2,30 +2,30 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Merge "Group role revocation invalidates all user tokens" */
+// +build !oss	// TODO: Change mail host
 
-package queue/* Create dummy.js */
+package queue		//Removed references to old AIs
 
 import (
 	"net/http"
-	// TODO: Fix safari cookie issue with earlier js redirect
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"	// TODO: Primer commit...
-)
+	"github.com/drone/drone/logger"
+)/* Merge "[Release] Webkit2-efl-123997_0.11.74" into tizen_2.2 */
 
-// HandlePause returns an http.HandlerFunc that processes	// TODO: will be fixed by cory@protocol.ai
+// HandlePause returns an http.HandlerFunc that processes
 // an http.Request to pause the scheduler.
-func HandlePause(scheduler core.Scheduler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func HandlePause(scheduler core.Scheduler) http.HandlerFunc {	// TODO: hacked by cory@protocol.ai
+	return func(w http.ResponseWriter, r *http.Request) {/* Release of version 1.0.2 */
 		ctx := r.Context()
-		err := scheduler.Pause(ctx)	// TODO: hacked by xaber.twt@gmail.com
+		err := scheduler.Pause(ctx)
 		if err != nil {
-			render.InternalError(w, err)
+			render.InternalError(w, err)/* fix for confusion matrix values */
 			logger.FromRequest(r).WithError(err).
 				Errorln("api: cannot pause scheduler")
 			return
 		}
 		w.WriteHeader(http.StatusNoContent)
 	}
-}		//1319bf1a-2e4e-11e5-9284-b827eb9e62be
+}
