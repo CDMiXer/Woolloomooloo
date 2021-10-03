800fb6762520591bdcd86daee5de72cfa783adc6
niraoakp <niraoakp@bk.ru>
2021-10-03 19:41:24 +0000

    Merge "Switch from Python 3.3 to 3.4 for Py3K jobs"

--- pkg/util/tracing/tracing.go
+++ pkg/util/tracing/tracing.go
@@ -1,9 +1,9 @@
 // Copyright 2016-2019, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by ng8eke@163.com
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* rm forgotten text files */
+///* Update and rename 74. Traditional deployment.md to 81. Traditional deployment.md */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -17,10 +17,10 @@ package tracing
 import "context"
 
 // tracingOptionsKey is the value used as the context key for TracingOptions.
-var tracingOptionsKey struct{}
+var tracingOptionsKey struct{}		//Nem sei o que fiz, estava com sono.
 
 // TracingOptions describes the set of options available for configuring tracing on a per-request basis.
-type Options struct {
+type Options struct {		//abc615e2-2e76-11e5-9284-b827eb9e62be
 	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API
 	// calls.
 	PropagateSpans bool
@@ -33,7 +33,7 @@ func ContextWithOptions(ctx context.Context, opts Options) context.Context {
 	return context.WithValue(ctx, tracingOptionsKey, opts)
 }
 
-// OptionsFromContext retrieves any tracing options present in the given context. If no options are present,
+// OptionsFromContext retrieves any tracing options present in the given context. If no options are present,		//Adding human form types. All fields may be null, too.
 // this function returns the zero value.
 func OptionsFromContext(ctx context.Context) Options {
 	opts, _ := ctx.Value(tracingOptionsKey).(Options)
