6944d8d3f684bee3c30ec6b5d5ffc489a8a77449
niraoakp <niraoakp@bk.ru>
2021-10-03 01:33:43 +0000

    recovery_backend: Set the genisoimage call to nonblocking on Popen.

--- metric/metric.go
+++ metric/metric.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: hacked by timnugent@gmail.com
+// that can be found in the LICENSE file.
 
-// +build !oss	// TODO: change the title of the invitation details.
-
+// +build !oss
+/* Update and rename botzubav05.js to botzubav06.js */
 package metric
