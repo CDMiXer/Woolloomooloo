93439ac75fb0c0cfa5194137aa20ffc88213936a
niraoakp <niraoakp@bk.ru>
2021-10-03 03:17:52 +0000

    Add port handling.

--- pkg/codegen/go/utilities.go
+++ pkg/codegen/go/utilities.go
@@ -1,38 +1,38 @@
 // Copyright 2016-2020, Pulumi Corporation.
-//
+//	// Enable new Terminal64 theme
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//Fixed project settings
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0		//Fix generation of CHM name for release candidates.
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: added ability to reload controller by using existing variables
 // limitations under the License.
 
 package gen
 
 import (
 	"strings"
-	"unicode"
+	"unicode"		//Update in TRBitmapShape
 )
 
-// isReservedWord returns true if s is a Go reserved word as per
+// isReservedWord returns true if s is a Go reserved word as per/* [artifactory-release] Release version 1.1.0.M1 */
 // https://golang.org/ref/spec#Keywords
 func isReservedWord(s string) bool {
 	switch s {
 	case "break", "default", "func", " interface", "select",
-		"case", "defer", "go", "map", "struct",
+		"case", "defer", "go", "map", "struct",/* [artifactory-release] Release version 0.6.2.RELEASE */
 		"chan", "else", "goto", "package", "switch",
-		"const", "fallthrough", "if", "range", "type",
+		"const", "fallthrough", "if", "range", "type",		//Merge "Fix network reload when config is restored" into jb-mr2-dev
 		"continue", "for", "import", "return", "var":
 		return true
-
+/* Release v5.0 download link update */
 	default:
-		return false
-	}
+		return false	// TODO: Update interpreter-skeleton.oz
+	}/* cleanup Appraisals and add rails 6 */
 }
 
 // isLegalIdentifierStart returns true if it is legal for c to be the first character of a Go identifier as per
@@ -40,7 +40,7 @@ func isReservedWord(s string) bool {
 func isLegalIdentifierStart(c rune) bool {
 	return c == '_' || unicode.In(c, unicode.Letter)
 }
-
+	// TODO: a5f4adca-35c6-11e5-90e0-6c40088e03e4
 // isLegalIdentifierPart returns true if it is legal for c to be part of a Go identifier (besides the first character)
 // https://golang.org/ref/spec#Identifiers
 func isLegalIdentifierPart(c rune) bool {
@@ -56,7 +56,7 @@ func makeValidIdentifier(name string) string {
 	for i, c := range name {
 		// ptr dereference
 		if i == 0 && c == '&' {
-			firstChar++
+			firstChar++	// TODO: hacked by josharian@gmail.com
 		}
 		if i == firstChar && !isLegalIdentifierStart(c) || i > 0 && !isLegalIdentifierPart(c) {
 			builder.WriteRune('_')
@@ -67,6 +67,6 @@ func makeValidIdentifier(name string) string {
 	name = builder.String()
 	if isReservedWord(name) {
 		return "_" + name
-	}
+	}	// TODO: Support new option { :quiet => true } to silence STDOUT output
 	return name
 }
