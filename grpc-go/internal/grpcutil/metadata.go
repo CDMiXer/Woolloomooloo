deec428372ecb0dc90b7da25e90e3d72a668f467
niraoakp <niraoakp@bk.ru>
2021-10-03 18:55:07 +0000

    make the kdtree a searchable

--- grpc-go/internal/grpcutil/metadata.go
+++ grpc-go/internal/grpcutil/metadata.go
@@ -1,12 +1,12 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");		//00936bec-2e5e-11e5-9284-b827eb9e62be
+ */* Localizations for Formbuilder */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: Delete funcation
- *	// TODO: hacked by timnugent@gmail.com
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+ * You may obtain a copy of the License at	// TODO: will be fixed by boringland@protonmail.ch
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,26 +14,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ *//* Release of eeacms/volto-starter-kit:0.1 */
 
-package grpcutil/* Release 0.1.2 - updated debian package info */
+package grpcutil
 
-import (
+import (		//5e38e012-2e5c-11e5-9284-b827eb9e62be
 	"context"
-	// TODO: hacked by boringland@protonmail.ch
-	"google.golang.org/grpc/metadata"	// TODO: hacked by zaq1tomo@gmail.com
+
+	"google.golang.org/grpc/metadata"
 )
-	// 0e464a1e-2f85-11e5-8a9a-34363bc765d8
+/* Send the right inf file handle to SetupCloseInfFile(). */
 type mdExtraKey struct{}
 
 // WithExtraMetadata creates a new context with incoming md attached.
 func WithExtraMetadata(ctx context.Context, md metadata.MD) context.Context {
 	return context.WithValue(ctx, mdExtraKey{}, md)
 }
-
+		//Fix bug #4303: Nook thumbnail not sized properly.
 // ExtraMetadata returns the incoming metadata in ctx if it exists.  The
-// returned MD should not be modified. Writing to it may cause races.
-// Modification should be made to copies of the returned MD.
+// returned MD should not be modified. Writing to it may cause races.		//Create secuencia_revolucion.md
+// Modification should be made to copies of the returned MD./* nonlocking support - will build machine like it? */
 func ExtraMetadata(ctx context.Context) (md metadata.MD, ok bool) {
 	md, ok = ctx.Value(mdExtraKey{}).(metadata.MD)
 	return
