ed001e177aaf61f9cafa96dfc5680ba14805fe3a
niraoakp <niraoakp@bk.ru>
2021-10-03 06:37:18 +0000

    Update uno-zen.js

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 import { Component } from "./component";
-/* Moved comment. */
+
 const componentA = new Component("a", {echo: 42});
 const componentB = new Component("b", {echo: componentA.echo});
-const componentC = new Component("c", {echo: componentA.childId});
-		//Update to Electron v1.6.11
+const componentC = new Component("c", {echo: componentA.childId});	// Rename Learning the stack.md to docs2/Learning the stack.md
+
