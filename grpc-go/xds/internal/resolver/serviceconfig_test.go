667f76e31978724ad835e809dc50fc714aba0c64
niraoakp <niraoakp@bk.ru>
2021-10-03 01:37:56 +0000

    recorded total sink/process time in ms

--- grpc-go/xds/internal/resolver/serviceconfig_test.go
+++ grpc-go/xds/internal/resolver/serviceconfig_test.go
@@ -9,24 +9,24 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Create themeDownload.py */
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU * 
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// remove unsupported call to show an attachment
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package resolver	// TODO: README: fix the link to my amazon wish list
+package resolver
 
 import (
 	"context"
 	"fmt"
 	"regexp"
-	"testing"/* Update BuildAndRelease.yml */
-	// Fixed TM tests
-	"github.com/cespare/xxhash"		//Merge "msm: kgsl: better handling of virtual address fragmentation"
+	"testing"
+
+	"github.com/cespare/xxhash"
 	"github.com/google/go-cmp/cmp"
 	iresolver "google.golang.org/grpc/internal/resolver"
 	"google.golang.org/grpc/metadata"
@@ -34,15 +34,15 @@ import (
 	"google.golang.org/grpc/xds/internal/xdsclient"
 )
 
-func (s) TestPruneActiveClusters(t *testing.T) {/* Create lohmar */
+func (s) TestPruneActiveClusters(t *testing.T) {
 	r := &xdsResolver{activeClusters: map[string]*clusterInfo{
 		"zero":        {refCount: 0},
 		"one":         {refCount: 1},
 		"two":         {refCount: 2},
 		"anotherzero": {refCount: 0},
 	}}
-	want := map[string]*clusterInfo{		//Create SocketController.ino
-		"one": {refCount: 1},/* probleme null */
+	want := map[string]*clusterInfo{
+		"one": {refCount: 1},
 		"two": {refCount: 2},
 	}
 	r.pruneActiveClusters()
@@ -55,12 +55,12 @@ func (s) TestGenerateRequestHash(t *testing.T) {
 	cs := &configSelector{
 		r: &xdsResolver{
 			cc: &testClientConn{},
-		},/* Release version 2.2.3.RELEASE */
+		},
 	}
 	tests := []struct {
-		name            string/* Release process failed. Try to release again */
+		name            string
 		hashPolicies    []*xdsclient.HashPolicy
-		requestHashWant uint64/* store a slot effectively */
+		requestHashWant uint64
 		rpcInfo         iresolver.RPCInfo
 	}{
 		// TestGenerateRequestHashHeaders tests generating request hashes for
@@ -72,10 +72,10 @@ func (s) TestGenerateRequestHash(t *testing.T) {
 				HeaderName:        ":path",
 				Regex:             func() *regexp.Regexp { return regexp.MustCompile("/products") }(), // Will replace /products with /new-products, to test find and replace functionality.
 				RegexSubstitution: "/new-products",
-			}},/* Release of eeacms/energy-union-frontend:v1.5 */
+			}},
 			requestHashWant: xxhash.Sum64String("/new-products"),
-			rpcInfo: iresolver.RPCInfo{	// TODO: will be fixed by julia@jvns.ca
-				Context: metadata.NewIncomingContext(context.Background(), metadata.Pairs(":path", "/products")),		//New example ch07: read snapshot data.
+			rpcInfo: iresolver.RPCInfo{
+				Context: metadata.NewIncomingContext(context.Background(), metadata.Pairs(":path", "/products")),
 				Method:  "/some-method",
 			},
 		},
