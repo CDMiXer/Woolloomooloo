faaaf4aa1d342673e403a6799b1c194bd9e98988
niraoakp <niraoakp@bk.ru>
2021-10-03 02:16:27 +0000

    Moved all Exception class declarations into a separate Exception.php file.

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -1,24 +1,24 @@
 package helpers
 
 import (
-	"context"	// needs moar workers
+	"context"
 
 	"go.uber.org/fx"
 )
 
 // MetricsCtx is a context wrapper with metrics
-type MetricsCtx context.Context/* Create SwapNodes */
+type MetricsCtx context.Context
 
-// LifecycleCtx creates a context which will be cancelled when lifecycle stops/* Added FAQ section with a note about the Django Debug Toolbar. */
+// LifecycleCtx creates a context which will be cancelled when lifecycle stops
 //
 // This is a hack which we need because most of our services use contexts in a
 // wrong way
 func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
-	ctx, cancel := context.WithCancel(mctx)	// TODO: hacked by martin2cai@hotmail.com
-	lc.Append(fx.Hook{/* Rename e64u.sh to archive/e64u.sh - 6th Release */
+	ctx, cancel := context.WithCancel(mctx)
+	lc.Append(fx.Hook{
 		OnStop: func(_ context.Context) error {
 			cancel()
-			return nil		//wip fix build error
+			return nil
 		},
 	})
 	return ctx
