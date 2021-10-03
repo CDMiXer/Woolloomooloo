230f60ecd3a42c5a15af04beb681541cc2e6bb8b
niraoakp <niraoakp@bk.ru>
2021-10-03 08:51:47 +0000

    Add file test

--- tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
+++ tests/integration/aliases/nodejs/adopt_into_component/step2/index.ts
@@ -1,72 +1,72 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Editing more comments directly from GitHub
+
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);		//Add more management commands
+        super("my:module:Resource", name, {}, opts);
     }
 }
 
-// Scenario #2 - adopt a resource into a component.  The component author is the same as the component user, and changes/* Merge branch 'master' into pm-dao-decorator */
+// Scenario #2 - adopt a resource into a component.  The component author is the same as the component user, and changes
 // the component to be able to adopt the resource that was previously defined separately...
-class Component extends pulumi.ComponentResource {		//Add log2u.hpp to estd/
+class Component extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component", name, {}, opts);
         // The resource creation was moved from top level to inside the component.
-        this.resource = new Resource(`${name}-child`, {	// TODO: hacked by zaq1tomo@gmail.com
+        this.resource = new Resource(`${name}-child`, {
             // With a new parent
             parent: this,
-            // But with an alias provided based on knowing where the resource existing before - in this case at top/* Release notes for 0.4 */
+            // But with an alias provided based on knowing where the resource existing before - in this case at top
             // level.  We use an absolute URN instead of a relative `Alias` because we are referencing a fixed resource
-            // that was in some arbitrary other location in the hierarchy prior to being adopted into this component./* adicionado método de seleção do tipo selectionsort */
+            // that was in some arbitrary other location in the hierarchy prior to being adopted into this component.
             aliases: [pulumi.createUrn("res2", "my:module:Resource")],
         });
     }
 }
 // The creation of the component is unchanged.
 const comp2 = new Component("comp2");
-/* Iniciando reescrita da aula 13. */
+
 // Scenario 3: adopt this resource into a new parent.
 class Component2 extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component2", name, {}, opts);
-    }/* Merge "Use a real IP address for ironic-inspector endpoint_override" */
-}/* Create TimerBan.php */
+    }
+}
 
 // validate that "parent: undefined" means "i didn't have a parent previously"
 new Component2("unparented", {
     aliases: [{ parent: pulumi.rootStackResource }],
     parent: comp2,
 });
-/* Release: Making ready for next release iteration 6.6.4 */
+
 
 // Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
 // in the next step to be parented by this.  Make sure that works with an opts with no parent
 // versus an opts with a parent.
-		//There is a better way to detect legacy browsers without conditionals.
+
 class Component3 extends pulumi.ComponentResource {
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
         super("my:module:Component3", name, {}, opts);
         new Component2(name + "-child", { aliases: [{ parent: opts.parent}], parent: this });
-    }	// add entity filter form propal and invoice select list
+    }
 }
 
 new Component3("parentedbystack");
 new Component3("parentedbycomponent", { parent: comp2 });
-	// TODO: upload added
+
 // Scenario 5: Allow multiple aliases to the same resource.
 class Component4 extends pulumi.ComponentResource {
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
         super("my:module:Component4", name, {}, {
             aliases: [
-                { parent: pulumi.rootStackResource },	// TODO: Merge "Make attach_instance return updated volume object"
+                { parent: pulumi.rootStackResource },
                 { parent: pulumi.rootStackResource },
             ],
             ...opts,
         });
     }
 }
-/* Updated documentation of generators */
+
 new Component4("duplicateAliases", { parent: comp2 });
