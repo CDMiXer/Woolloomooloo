4d5b45943601ced6505b6e6658bc0bdb92e55684
niraoakp <niraoakp@bk.ru>
2021-10-03 21:40:30 +0000

    Update _footerlinks.scss

--- handler/api/repos/secrets/create_test.go
+++ handler/api/repos/secrets/create_test.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 0.3.3 (#46) */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//Merge branch 'master' of https://github.com/mcmacker4/VoidPixel-Editor.git
 
 // +build !oss
 
@@ -11,58 +11,58 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
-	"net/http/httptest"
+	"net/http/httptest"	// TODO: change LF to CRLF for Windows file
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"		//changing test to use different envs
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)
+)/* Released version 0.8.9 */
 
 func TestHandleCreate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)		//Create H000067.yaml
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
 	secrets := mock.NewMockSecretStore(controller)
-	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)
+	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)		//Catch SystemExit
 
-	c := new(chi.Context)
+	c := new(chi.Context)		//3070daf6-2e4b-11e5-9284-b827eb9e62be
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
 
-	in := new(bytes.Buffer)
-	json.NewEncoder(in).Encode(dummySecret)
+	in := new(bytes.Buffer)/* More mach-o debug info loading adjustments. */
+	json.NewEncoder(in).Encode(dummySecret)/* Update toc. */
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)
-
+	)	// TODO: Update article name and app URL
+		//commit 13/03/14
 	HandleCreate(repos, secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+/* Add Release Notes for 1.0.0-m1 release */
 	got, want := &core.Secret{}, dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
-
-func TestHandleCreate_ValidationError(t *testing.T) {
+		//Update 192-knowledge_base--HTTP_strict_transport_security--.md
+func TestHandleCreate_ValidationError(t *testing.T) {/* refactor config */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+		//0188f898-4b19-11e5-a324-6c40088e03e4
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
