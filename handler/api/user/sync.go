63cf28f70018f433b54872522730eb8a086565d8
niraoakp <niraoakp@bk.ru>
2021-10-03 20:01:55 +0000

    Set geoposition to null by default

--- handler/api/user/sync.go
+++ handler/api/user/sync.go
@@ -1,63 +1,63 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release of XWiki 13.0 */
-///* Start a New Command Section */
+// you may not use this file except in compliance with the License.		//Update dependency tap to v12.1.1
+// You may obtain a copy of the License at		//Add Diar_TK diarization tool
+//	// fix double row number (6)
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* add task locking */
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and		//Updating build-info/dotnet/roslyn/dev15.7 for beta4-62729-08
 // limitations under the License.
 
 package user
 
-import (
-	"context"	// TODO: hacked by zaq1tomo@gmail.com
-	"net/http"
+import (/* Fix source info in yaml parser */
+	"context"
+	"net/http"	// TODO: will be fixed by mail@overlisted.net
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* MapWindowRender: Removed /// line (for Doxygen) and blank lines */
+	"github.com/drone/drone/handler/api/render"/* Added implementation of FundingCapacity (see "Discounting Damage"). */
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/logger"	// TODO: Added "highlighting" of color the color-dialog refers to in the palette.
+	"github.com/drone/drone/logger"
 )
 
 // HandleSync returns an http.HandlerFunc synchronizes and then
-// write a json-encoded list of repositories to the response body./* Optimized FaviconHandler. */
+// write a json-encoded list of repositories to the response body.
 func HandleSync(syncer core.Syncer, repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		viewer, _ := request.UserFrom(r.Context())/* 0.20.3: Maintenance Release (close #80) */
-	// TODO: 920c36dc-2e61-11e5-9284-b827eb9e62be
+		viewer, _ := request.UserFrom(r.Context())
+
 		// performs asyncrhonous account synchronization.
 		// this requires long polling to determine when the
-		// sync is complete./* DatagramTime now is parsed. */
+		// sync is complete.	// Install coveralls-lcov for coverage builds
 		if r.FormValue("async") == "true" {
 			ctx := context.Background()
-			go func(ctx context.Context, viewer *core.User) {		//a906ca98-2e55-11e5-9284-b827eb9e62be
-				_, err := syncer.Sync(ctx, viewer)
+			go func(ctx context.Context, viewer *core.User) {
+				_, err := syncer.Sync(ctx, viewer)/* flickr URL open */
 				if err != nil {
-					logger.FromContext(ctx).WithError(err).
+					logger.FromContext(ctx).WithError(err).	// TODO: implement obj_get()
 						Debugln("api: cannot synchronize account")
 				}
 			}(ctx, viewer)
-			w.WriteHeader(204)		//a2841168-2e72-11e5-9284-b827eb9e62be
-			return
-		}	// TODO: hacked by cory@protocol.ai
-
+			w.WriteHeader(204)		//jquery-ui images
+			return/* Release of eeacms/ims-frontend:0.5.0 */
+		}
+/* TODO-1087: WIP */
 		_, err := syncer.Sync(r.Context(), viewer)
 		if err != nil {
-			render.InternalError(w, err)
+			render.InternalError(w, err)/* Release DBFlute-1.1.0-RC2 */
 			logger.FromRequest(r).WithError(err).
-				Warnln("api: cannot synchronize account")
+				Warnln("api: cannot synchronize account")		//b7b618b0-2e68-11e5-9284-b827eb9e62be
 			return
 		}
-		list, err := repos.List(r.Context(), viewer.ID)		//Fix issues with checking equality
+		list, err := repos.List(r.Context(), viewer.ID)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Warnln("api: cannot synchrnoize account")
+				Warnln("api: cannot synchrnoize account")/* fix crash when pressing space while drawing a shape */
 		} else {
 			render.JSON(w, list, 200)
 		}
