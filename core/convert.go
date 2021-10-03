bdf59e49a12b8b81bb418dfd5485427e5b184924
niraoakp <niraoakp@bk.ru>
2021-10-03 11:57:03 +0000

    Merge "Fix validate failed with an image_name in uuid format"

--- core/convert.go
+++ core/convert.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone IO, Inc.
-///* Working towards project specific display names. */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* ProRelease2 hardware update */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by fjl@ethereum.org
+// limitations under the License.
 
 package core
 
-import "context"/* #985 fixed html validation errors */
+import "context"
 
-type (/* Project name now "SNOMED Release Service" */
-	// ConvertArgs represents a request to the pipeline/* Release 0.2.2 of swak4Foam */
-	// conversion service.	// [FIX] changing vals at creat
+type (
+	// ConvertArgs represents a request to the pipeline
+	// conversion service.
 	ConvertArgs struct {
 		User   *User       `json:"-"`
-		Repo   *Repository `json:"repo,omitempty"`	// TODO: Added github.tum.sexy
+		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
 		Config *Config     `json:"config,omitempty"`
-	}/* Updated MAX_TARGET */
+	}
 
 	// ConvertService converts non-native pipeline
 	// configuration formats to native configuration
