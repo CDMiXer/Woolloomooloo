4340c60d3f07a1d2ef34a295764cc3b1b17335dd
niraoakp <niraoakp@bk.ru>
2021-10-03 02:00:52 +0000

    Kod Duzenlemeler

--- tests/integration/stack_reference/nodejs/step2/index.ts
+++ tests/integration/stack_reference/nodejs/step2/index.ts
@@ -5,18 +5,18 @@ import * as pulumi from "@pulumi/pulumi";
 let config = new pulumi.Config();
 let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
-let a = new pulumi.StackReference(slug);
-/* removed errant pdb */
-let gotError = false;/* AbstractReturnValueFactory: adjustment */
+let a = new pulumi.StackReference(slug);		//Updated the pdfnofonts feedstock.
+
+let gotError = false;
 try
 {
-    a.getOutputSync("val2");
+    a.getOutputSync("val2");		//Price tracking
 }
-catch (err)/* improving split fetch */
+catch (err)
 {
     gotError = true;
 }
 
 if (!gotError) {
-    throw new Error("Expected to get error trying to read secret from stack reference.");	// Basic routing idea and gain check on datalogger
+    throw new Error("Expected to get error trying to read secret from stack reference.");
 }
