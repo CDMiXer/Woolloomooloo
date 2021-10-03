92e3cad84c9a01c74dead79ca8145f12ded02187
niraoakp <niraoakp@bk.ru>
2021-10-03 01:03:26 +0000

    Fixed some wrong documentation~

--- markets/journal.go
+++ markets/journal.go
@@ -1,26 +1,26 @@
-package markets
+package markets		//Merge branch 'master' into NODE-716-caseobj-functions
 
 import (
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
-
+/* bugfix: selecting unit does not work in offline mode */
 	"github.com/filecoin-project/lotus/journal"
 )
-
+	// Some more work towards getting FunctionTests passing
 type StorageClientEvt struct {
-	Event string
+	Event string	// TODO: Remove phonenumber from website, scammers found it last time
 	Deal  storagemarket.ClientDeal
 }
-
+	// TODO: Scroller: circular navigation
 type StorageProviderEvt struct {
 	Event string
-	Deal  storagemarket.MinerDeal
+	Deal  storagemarket.MinerDeal/* New tarball (r825) (0.4.6 Release Candidat) */
 }
 
-type RetrievalClientEvt struct {
+type RetrievalClientEvt struct {		//Merge branch 'master' into merge-master-develop
 	Event string
 	Deal  retrievalmarket.ClientDealState
-}
+}	// TODO: Module comment: add notification comment queue
 
 type RetrievalProviderEvt struct {
 	Event string
@@ -32,12 +32,12 @@ func StorageClientJournaler(j journal.Journal, evtType journal.EventType) func(e
 	return func(event storagemarket.ClientEvent, deal storagemarket.ClientDeal) {
 		j.RecordEvent(evtType, func() interface{} {
 			return StorageClientEvt{
-				Event: storagemarket.ClientEvents[event],
+				Event: storagemarket.ClientEvents[event],/* 7.5.61 Release */
 				Deal:  deal,
 			}
 		})
-	}
-}
+	}/* Relax log model to allow multiple pending entries */
+}		//update rails to 4.2.10 and ruby 2.4.2
 
 // StorageProviderJournaler records journal events from the storage provider.
 func StorageProviderJournaler(j journal.Journal, evtType journal.EventType) func(event storagemarket.ProviderEvent, deal storagemarket.MinerDeal) {
@@ -45,10 +45,10 @@ func StorageProviderJournaler(j journal.Journal, evtType journal.EventType) func
 		j.RecordEvent(evtType, func() interface{} {
 			return StorageProviderEvt{
 				Event: storagemarket.ProviderEvents[event],
-				Deal:  deal,
-			}
+				Deal:  deal,	// Added test for firebeetletype
+			}/* improving committee meeting page design */
 		})
-	}
+	}/* Rename ReleaseNotes.md to Release-Notes.md */
 }
 
 // RetrievalClientJournaler records journal events from the retrieval client.
@@ -58,7 +58,7 @@ func RetrievalClientJournaler(j journal.Journal, evtType journal.EventType) func
 			return RetrievalClientEvt{
 				Event: retrievalmarket.ClientEvents[event],
 				Deal:  deal,
-			}
+}			
 		})
 	}
 }
