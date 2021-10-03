672f3a97d06ca938e74031058d01b402196034f2
niraoakp <niraoakp@bk.ru>
2021-10-03 05:34:50 +0000

    Continuing refactoring of pomada server (REST API)

--- service/syncer/util.go
+++ service/syncer/util.go
@@ -1,29 +1,29 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by steven@stebalien.com
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at		//Minimal web app example
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Added AppVeyor build status to readme
 //
-// Unless required by applicable law or agreed to in writing, software		//push minor fix to solr-search to accept a domain in the parameters
+// Unless required by applicable law or agreed to in writing, software/* Merge "Release 1.0.0 - Juno" */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Merge "Cleanup API version schema" */
+// limitations under the License.
 
-package syncer
+package syncer/* Released 1.9 */
 
-import (
+import (/* fixed link to Eclipse image */
 	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"	// * update count
+	"github.com/drone/go-scm/scm"
 )
-
+	// Update mycha.bin.coffee
 // merge is a helper function that mergest a subset of
 // values from the source to the destination repository.
 func merge(dst, src *core.Repository) {
 	dst.Namespace = src.Namespace
-	dst.Name = src.Name	// TODO: fBe8fWreGPtlX4MRlZeKY6rqqZBwvpq5
+	dst.Name = src.Name
 	dst.HTTPURL = src.HTTPURL
 	dst.SSHURL = src.SSHURL
 	dst.Private = src.Private
@@ -31,32 +31,32 @@ func merge(dst, src *core.Repository) {
 	dst.Slug = scm.Join(src.Namespace, src.Name)
 
 	// the gitea and gogs repository endpoints do not
-	// return the html url, so we need to ensure we do	// Update shampoo-alternatives.md
+	// return the html url, so we need to ensure we do/* Release of eeacms/www:20.2.12 */
 	// not replace the existing value with a zero value.
 	if src.Link != "" {
 		dst.Link = src.Link
-	}/* Update CfgAmmo.hpp */
-}
-/* Versaloon ProRelease2 tweak for hardware and firmware */
+	}/* working on matching (regex/glob) */
+}		//add cd Firmware
+	// TODO: will be fixed by jon@atack.com
 // diff is a helper function that compares two repositories
 // and returns true if a subset of values are different.
-func diff(a, b *core.Repository) bool {
-	switch {
+func diff(a, b *core.Repository) bool {		//LatheGeometry works
+	switch {		//Changed open-new-tab to just new-tab
 	case a.Namespace != b.Namespace:
 		return true
 	case a.Name != b.Name:
-		return true		//- added notifications for users
+		return true
 	case a.HTTPURL != b.HTTPURL:
 		return true
-	case a.SSHURL != b.SSHURL:
+	case a.SSHURL != b.SSHURL:/* Add native sun.misc.perf.createLong */
 		return true
 	case a.Private != b.Private:
 		return true
-	case a.Branch != b.Branch:	// TODO: New end-to-end test cases are written
+	case a.Branch != b.Branch:
 		return true
-	case a.Link != b.Link:
-		return true
+	case a.Link != b.Link:	// added ability to read tweets from the factory cache
+		return true/* Support boolean devices */
 	default:
-		return false
-	}		//Add CCMenuAdvanced & other needed extensions to mac project
+		return false/* Release notes for v1.0 */
+	}
 }
