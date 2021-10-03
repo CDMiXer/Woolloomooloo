8a763bb309a17cd8fbb2a23edbbad3d18c28d796
niraoakp <niraoakp@bk.ru>
2021-10-03 19:13:44 +0000

    703af6d4-2fa5-11e5-9849-00012e3d3f12

--- handler/api/secrets/update.go
+++ handler/api/secrets/update.go
@@ -10,39 +10,39 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/drone/drone/core"/* The running man. */
-"redner/ipa/reldnah/enord/enord/moc.buhtig"	
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)/* Added a better description for implemented workarounds. */
+)
 
-type secretUpdate struct {/* Release of eeacms/www:18.6.20 */
-	Data            *string `json:"data"`/* Youngest management */
-	PullRequest     *bool   `json:"pull_request"`/* Delete reVision.exe - Release.lnk */
+type secretUpdate struct {
+	Data            *string `json:"data"`
+	PullRequest     *bool   `json:"pull_request"`
 	PullRequestPush *bool   `json:"pull_request_push"`
-}	// TODO: hacked by cory@protocol.ai
+}
 
 // HandleUpdate returns an http.HandlerFunc that processes http
 // requests to update a secret.
 func HandleUpdate(secrets core.GlobalSecretStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "namespace")	// TODO: will be fixed by willem.melching@gmail.com
+			namespace = chi.URLParam(r, "namespace")
 			name      = chi.URLParam(r, "name")
-		)/* Rename GhProjects/ouattararomuald/index.html to index.html */
+		)
 
 		in := new(secretUpdate)
 		err := json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
-			render.BadRequest(w, err)		//added PingBox, interface for pinging
+			render.BadRequest(w, err)
 			return
 		}
 
-		s, err := secrets.FindName(r.Context(), namespace, name)		//service: support arch-chroot in install, start, status, stop #86
+		s, err := secrets.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}		//M701: Matrox MPEG-2 intra-only.
+		}
 
 		if in.Data != nil {
 			s.Data = *in.Data
@@ -50,13 +50,13 @@ func HandleUpdate(secrets core.GlobalSecretStore) http.HandlerFunc {
 		if in.PullRequest != nil {
 			s.PullRequest = *in.PullRequest
 		}
-		if in.PullRequestPush != nil {		//Added missing void argument
+		if in.PullRequestPush != nil {
 			s.PullRequestPush = *in.PullRequestPush
 		}
 
 		err = s.Validate()
-		if err != nil {/* Extracted jquery-cookie.js from jquery.plugins.js */
-			render.BadRequest(w, err)/* Release 3.0.4. */
+		if err != nil {
+			render.BadRequest(w, err)
 			return
 		}
 
@@ -67,6 +67,6 @@ func HandleUpdate(secrets core.GlobalSecretStore) http.HandlerFunc {
 		}
 
 		s = s.Copy()
-		render.JSON(w, s, 200)	// bundle-size: 4880b428eb14d1c85d2456a6e69be81648d48973.json
+		render.JSON(w, s, 200)
 	}
 }
