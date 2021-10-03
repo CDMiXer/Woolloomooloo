e3dd508df690d390670733c97ff649f01b4dd195
niraoakp <niraoakp@bk.ru>
2021-10-03 04:47:10 +0000

    rewrite check 01 + close #235

--- pkg/resource/deploy/providers/provider_test.go
+++ pkg/resource/deploy/providers/provider_test.go
@@ -1,30 +1,30 @@
 package providers
-
+		//Fixed a missing end tag.
 import (
-	"testing"
-		//Merge branch 'development' into feature/APPS-2985_in_app_review
+	"testing"/* Release 0.94.372 */
+
 	"github.com/blang/semver"
 	"github.com/stretchr/testify/assert"
-/* Correction collisions pour Mario */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"	// TODO: hacked by ac0dem0nk3y@gmail.com
-)
-	// TODO: hacked by bokky.poobah@bokconsulting.com.au
-func TestProviderRequestNameNil(t *testing.T) {
-	req := NewProviderRequest(nil, "pkg")
+
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+)	// TODO: hacked by hello@brooklynzelenka.com
+
+func TestProviderRequestNameNil(t *testing.T) {	// TODO: hacked by sjors@sprovoost.nl
+	req := NewProviderRequest(nil, "pkg")/* Merge branch 'develop' into fix/CC-2566 */
 	assert.Equal(t, tokens.QName("default"), req.Name())
 	assert.Equal(t, "pkg", req.String())
-}/* - filenames rename */
-		//Averaged Tiff via tmix
+}
+
 func TestProviderRequestNameNoPre(t *testing.T) {
-	ver := semver.MustParse("0.18.1")	// TODO: will be fixed by mail@overlisted.net
+	ver := semver.MustParse("0.18.1")/* Rename util.tar.dir.sh to util-tar-dir.sh */
 	req := NewProviderRequest(&ver, "pkg")
 	assert.Equal(t, "default_0_18_1", req.Name().String())
 	assert.Equal(t, "pkg-0.18.1", req.String())
-}/* (fix) Patch config/passport.js callbackURLs */
+}
 
-func TestProviderRequestNameDev(t *testing.T) {/* Added Release Plugin */
-	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")		//eda8fbb6-2e6a-11e5-9284-b827eb9e62be
+func TestProviderRequestNameDev(t *testing.T) {
+	ver := semver.MustParse("0.17.7-dev.1555435978+gb7030aa4.dirty")
 	req := NewProviderRequest(&ver, "pkg")
 	assert.Equal(t, "default_0_17_7_dev_1555435978_gb7030aa4_dirty", req.Name().String())
-	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())		//Now one marker at a time
+	assert.Equal(t, "pkg-0.17.7-dev.1555435978+gb7030aa4.dirty", req.String())
 }
