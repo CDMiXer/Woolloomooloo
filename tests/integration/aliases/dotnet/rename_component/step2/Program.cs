cd06d7814379cbf202e66c19385cd8cc1e8d1ae0
niraoakp <niraoakp@bk.ru>
2021-10-03 22:58:04 +0000

    merged conflicts manually

--- tests/integration/aliases/dotnet/rename_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step2/Program.cs
@@ -1,20 +1,20 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.	// test-bot: clean harder if first clean fails.
-/* Create cointegrating_check.py */
-using System.Threading.Tasks;/* restrict file output */
-using Pulumi;
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-class Resource : ComponentResource
+using System.Threading.Tasks;/* Optimization of Z3 python script */
+using Pulumi;/* Update URL for parallax-url */
+
+class Resource : ComponentResource	// TODO: e175020a-2e4b-11e5-9284-b827eb9e62be
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
-}	// TODO: hacked by peterke@gmail.com
+}	// TODO: 5e1ba454-2e50-11e5-9284-b827eb9e62be
 
 // Scenario #3 - rename a component (and all it's children)
-// No change to the component itself.		//add eclipe supoort.
-class ComponentThree : ComponentResource
-{	// TODO: kYIXladdBZYc9jUiNVIYsgq1Q1Mv6NiN
+// No change to the component itself.
+class ComponentThree : ComponentResource		//Auto stash before merge of "master" and "cheeseywhiz/master"
+{
     private Resource resource1;
     private Resource resource2;
 
@@ -23,23 +23,23 @@ class ComponentThree : ComponentResource
     {
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
-        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });	// Show progress bars on profile
+        this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
         this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
 
-/* Working on param estimation functions */
+
 class Program
 {
-    static Task<int> Main(string[] args)
+    static Task<int> Main(string[] args)/* Blog Post - "Chrome becomes a bit less of a memory hog with version 45" */
     {
-        return Deployment.RunAsync(() =>
-        {
-            // Applying an alias to the instance successfully renames both the component and the children.
+        return Deployment.RunAsync(() =>		//Delete get_img.php
+        {/* 0ce6fa68-2e62-11e5-9284-b827eb9e62be */
+            // Applying an alias to the instance successfully renames both the component and the children.	// TODO: Create fuoye.txt
             var comp3 = new ComponentThree("newcomp3", new ComponentResourceOptions
             {
                 Aliases = { new Alias { Name = "comp3" } },
             });
         });
-    }		//Delete campos.class
+    }
 }
