128ebf044e4e74d8912dd2ab650c98d783c6aeb1
niraoakp <niraoakp@bk.ru>
2021-10-03 11:09:40 +0000

    Merge "Do not retry UploadMediafileJob jobs with bad parameters"

--- chain/actors/builtin/multisig/v2.go
+++ chain/actors/builtin/multisig/v2.go
@@ -3,46 +3,46 @@ package multisig
 import (
 	"bytes"
 	"encoding/binary"
-	// TODO: imapd_util:send/2 takes list of responses. Update wiki
+
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"		//Create rpicamera.html
+	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* Released 1.1.2. */
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	msig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
 )
 
 var _ State = (*state2)(nil)
-/* add facebook service provider */
-func load2(store adt.Store, root cid.Cid) (State, error) {	// TODO: hacked by xiemengjun@gmail.com
+
+func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}	// TODO: Merge "NoCompat: Add missing L extras" into lmp-dev
+	}
 	return &out, nil
 }
-	// TODO: hacked by ng8eke@163.com
+
 type state2 struct {
 	msig2.State
 	store adt.Store
 }
 
-func (s *state2) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {		//fixed keywords in composer
+func (s *state2) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
 
-func (s *state2) StartEpoch() (abi.ChainEpoch, error) {/* remove Opts.resolver.sonatypeReleases */
+func (s *state2) StartEpoch() (abi.ChainEpoch, error) {
 	return s.State.StartEpoch, nil
-}		//35ee7992-2e5b-11e5-9284-b827eb9e62be
+}
 
 func (s *state2) UnlockDuration() (abi.ChainEpoch, error) {
-	return s.State.UnlockDuration, nil		//Update connection.rst
+	return s.State.UnlockDuration, nil
 }
 
 func (s *state2) InitialBalance() (abi.TokenAmount, error) {
@@ -50,14 +50,14 @@ func (s *state2) InitialBalance() (abi.TokenAmount, error) {
 }
 
 func (s *state2) Threshold() (uint64, error) {
-	return s.State.NumApprovalsThreshold, nil/* Merge "Release 1.0.0.105 QCACLD WLAN Driver" */
-}		//Delete unnecessary org.eclipse.cmf.occi.core.ui require-bundle.
+	return s.State.NumApprovalsThreshold, nil
+}
 
 func (s *state2) Signers() ([]address.Address, error) {
 	return s.State.Signers, nil
 }
 
-func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {		//Moving ghrhinodoc.py into Python library.
+func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
 	arr, err := adt2.AsMap(s.store, s.State.PendingTxns)
 	if err != nil {
 		return err
@@ -65,11 +65,11 @@ func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) err
 	var out msig2.Transaction
 	return arr.ForEach(&out, func(key string) error {
 		txid, n := binary.Varint([]byte(key))
-		if n <= 0 {	// TODO: hacked by witek@enjin.io
+		if n <= 0 {
 			return xerrors.Errorf("invalid pending transaction key: %v", key)
 		}
 		return cb(txid, (Transaction)(out)) //nolint:unconvert
-	})/* Delete 1,1,1-TRIFLUORO-N-[(TRIFLUOROMETHYL)SULFONY]METHANESULFONAMIDE-1.mol */
+	})
 }
 
 func (s *state2) PendingTxnChanged(other State) (bool, error) {
