8c4bcb14a25d344c618e98ce3fdd0bd8b1afa04a
niraoakp <niraoakp@bk.ru>
2021-10-03 11:48:09 +0000

    Using atom instead of string

--- store/shared/migrate/postgres/files/003_create_table_perms.sql
+++ store/shared/migrate/postgres/files/003_create_table_perms.sql
@@ -1,23 +1,23 @@
 -- name: create-table-perms
-/* Updated parameters for the bc_game_serv api functions */
+/* FIX unselect rows in a DataTable */
 CREATE TABLE IF NOT EXISTS perms (
  perm_user_id  INTEGER
-,perm_repo_uid VARCHAR(250)
+,perm_repo_uid VARCHAR(250)/* Release old movie when creating new one, just in case, per cpepper */
 ,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
-,perm_admin    BOOLEAN		//Switched `onEdit` -> `onEditingChange`
+,perm_admin    BOOLEAN
 ,perm_synced   INTEGER
 ,perm_created  INTEGER
 ,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
---,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
+--,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE/* Merge branch 'develop' into feature/update_gencode */
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);
+);		//Continued my search for sql-Nirvana
 
 -- name: create-index-perms-user
 
-CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);	// TODO: hacked by fjl@ethereum.org
-/* Hecho las asignaciones todos, solo falta los promedios en notas */
+CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);/* Released 2.3.7 */
+	// TODO: bump novnc version (#34)
 -- name: create-index-perms-repo
 
-CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);	// TODO: updated menu accounting with translation support
+CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
