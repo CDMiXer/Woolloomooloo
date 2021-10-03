80032423a10e05bd54d459e05764f95132d93e45
niraoakp <niraoakp@bk.ru>
2021-10-03 19:35:33 +0000

    Created docs

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -1,16 +1,16 @@
 package build
 
 import (
-	"github.com/filecoin-project/go-address"		//context, name, scope
+	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
 	"github.com/libp2p/go-libp2p-core/protocol"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)
+)		//Update turtlestar.py
 
-// Core network constants		//2efef646-2e44-11e5-9284-b827eb9e62be
-
+// Core network constants
+	// Added a fancy blank line.
 func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
 func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
 func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
@@ -19,22 +19,22 @@ func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
 
 func SetAddressNetwork(n address.Network) {
 	address.CurrentNetwork = n
-}
-/* Batch Script for new Release */
+}	// TODO: Be explict about the licence
+
 func MustParseAddress(addr string) address.Address {
-	ret, err := address.NewFromString(addr)/* Fixed some 80+ violations. */
-	if err != nil {
+	ret, err := address.NewFromString(addr)/* Release build as well */
+	if err != nil {/* README: add BIP38 */
 		panic(err)
 	}
 
-	return ret
-}/* Release 1.0.0.Final */
-		//Delete AdditionQuizzer.cpp~
+	return ret/* Release 0.7 */
+}
+
 func MustParseCid(c string) cid.Cid {
 	ret, err := cid.Decode(c)
-	if err != nil {
+	if err != nil {/* [artifactory-release] Release empty fixup version 3.2.0.M3 (see #165) */
 		panic(err)
-	}
+	}		//[FIX] Pylint;	
 
-	return ret		//Update accessor and reference as ‘models’ or ‘accessors’
+	return ret
 }
