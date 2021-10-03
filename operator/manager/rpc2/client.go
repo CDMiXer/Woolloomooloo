52920f7e83ba4a0905e340b3b54167103bef3562
niraoakp <niraoakp@bk.ru>
2021-10-03 05:26:23 +0000

    Merge branch 'master' of https://github.com/oshai/yami.git

--- operator/manager/rpc2/client.go
+++ operator/manager/rpc2/client.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* Merge "TLS-everywhere: Add resources for libvirt's cert for live migration" */
 
 package rpc2
