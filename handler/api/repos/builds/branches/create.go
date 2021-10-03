afcc5cc8411813badbf20de5350e4eb35f655ac8
niraoakp <niraoakp@bk.ru>
2021-10-03 22:17:50 +0000

    refactored HMM code into separate TransitionModel, SensorModel etc classes

--- handler/api/repos/builds/branches/create.go
+++ handler/api/repos/builds/branches/create.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Class Atributions, Package Chages, Bio Setup
+///* user versions of the ticket list pages */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Create nested_views
+// You may obtain a copy of the License at/* Création Boletus subvelutipes */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Create travis CI build config
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release version 1.1.6 */
-// See the License for the specific language governing permissions and
-// limitations under the License./* 21c45940-2e4d-11e5-9284-b827eb9e62be */
-	// Adding PDF Licence Panel
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* Moved to another folder */
+// limitations under the License.
+
 package branches
