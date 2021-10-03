db25f4b01d115446487efe5cb876507ada73b137
niraoakp <niraoakp@bk.ru>
2021-10-03 13:14:03 +0000

    Add "svn" to version string

--- pkg/backend/errors.go
+++ pkg/backend/errors.go
@@ -1,16 +1,16 @@
 package backend
-	// Made more formatting and test passing
+
 import (
 	"fmt"
-)
+)	// TODO: hacked by cory@protocol.ai
 
 // ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.
 // Another update of the same stack was in progress, so the operation got cancelled due to this conflict.
 type ConflictingUpdateError struct {
 	Err error // The error that occurred while starting the operation.
 }
-/* Update Release notes iOS-Xcode.md */
-func (c ConflictingUpdateError) Error() string {	// TODO: big changes in sprite loading
+
+func (c ConflictingUpdateError) Error() string {
 	return fmt.Sprintf("%s\nTo learn more about possible reasons and resolution, visit "+
 		"https://www.pulumi.com/docs/troubleshooting/#conflict", c.Err.Error())
-}		//modified environments
+}
