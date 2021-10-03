ae18f89a2055dc984d31901d79bf3b26677e74f5
niraoakp <niraoakp@bk.ru>
2021-10-03 14:32:06 +0000

    update example to reflect current API

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Merge "[INTERNAL][FIX] sap.m.Input: Input doesn't open when not enabled" */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Merge "Update README.md to prepare for open source." */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software		//Refactor pattern matcher to check there are enough tokens for a match.
+// distributed under the License is distributed on an "AS IS" BASIS,/* form submission support */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
