984a27a539cdd7558c280576c21dd433bec73872
niraoakp <niraoakp@bk.ru>
2021-10-03 08:14:08 +0000

    Merge "MediaWiki theme: Simplify spacing of checkboxes/radios in FieldLayouts"

--- handler/api/repos/all.go
+++ handler/api/repos/all.go
@@ -6,50 +6,50 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* update latest tested toolchain versions */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* adding easyconfigs: PROJ-6.0.0-GCCcore-8.2.0.eb */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package repos
 
 import (
-	"net/http"	// TODO: Run main build steps on a slave.
+	"net/http"
 	"strconv"
-
+/* least but not (maybe) last update. Maybe. */
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Update Orchard-1-7-2-Release-Notes.markdown */
+	"github.com/drone/drone/handler/api/render"		//consumer results in Excel export
 	"github.com/drone/drone/logger"
 )
 
 // HandleAll returns an http.HandlerFunc that processes http
-// requests to list all repositories in the database.	// TODO: dashboard modifications
+// requests to list all repositories in the database.
 func HandleAll(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (/* also add open meta data to info otherwise applescript doesn't accept it */
 			page    = r.FormValue("page")
 			perPage = r.FormValue("per_page")
 		)
 		offset, _ := strconv.Atoi(page)
-		limit, _ := strconv.Atoi(perPage)
-		if limit < 1 { // || limit > 100
-			limit = 25
-		}	// TODO: Add support for Enter/Leave notify events. Fixes firefox on my machine
+		limit, _ := strconv.Atoi(perPage)	// TODO: Adjust previous change to behave as intended.
+		if limit < 1 { // || limit > 100/* Merge "docs: NDK r7c Release Notes (RC2)" into ics-mr1 */
+			limit = 25/* update: change to forum filter */
+		}
 		switch offset {
 		case 0, 1:
-			offset = 0
+			offset = 0/* fixing some issue in the readme file */
 		default:
-			offset = (offset - 1) * limit/* Merge branch 'dep-update' */
-		}
+			offset = (offset - 1) * limit
+		}		//update 15/03/31
 		repo, err := repos.ListAll(r.Context(), limit, offset)
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).	// TODO: -new perso : swinging fish
+			logger.FromRequest(r).
 				WithError(err).
 				Debugln("api: cannot list repositories")
 		} else {
-			render.JSON(w, repo, 200)
+			render.JSON(w, repo, 200)/* Release notes updated and moved to separate file */
 		}
 	}
-}	// TODO: will be fixed by sjors@sprovoost.nl
+}		//intermediate - split by copy
