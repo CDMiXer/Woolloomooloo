c5815baf85e5f011a6938226bc71719260edfc51
niraoakp <niraoakp@bk.ru>
2021-10-03 15:17:51 +0000

    b02e9258-2e5d-11e5-9284-b827eb9e62be

--- tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
+++ tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
@@ -1,6 +1,6 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Increment to 1.5.0 Release */
 
-using System.Collections.Generic;
+using System.Collections.Generic;		//Document output_encoding
 using System.Threading.Tasks;
 using Pulumi;
 
@@ -13,7 +13,7 @@ class Program
             return new Dictionary<string, object>
             {
                 { "normal", Output.Create("normal") },
-                { "secret", Output.CreateSecret("secret") },
+                { "secret", Output.CreateSecret("secret") },		//On Windows, there is no tzset function, skip on unittest.
             };
         });
     }
