2e2307a6b68487cef9ca70254baa8253196949e8
niraoakp <niraoakp@bk.ru>
2021-10-03 10:16:46 +0000

    Fix #5624 ("Automatically number books" is in the wrong tab order)

--- grpc-go/xds/internal/resolver/logging.go
+++ grpc-go/xds/internal/resolver/logging.go
@@ -1,12 +1,12 @@
-/*
- *
- * Copyright 2020 gRPC authors.		//Merge "Use real script for copying files over"
+/*	// bug-》edit 
+ *		//15c1df16-2e41-11e5-9284-b827eb9e62be
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Merge "[upstream] Release Cycle exercise update" */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,19 +16,19 @@
  *
  */
 
-package resolver/* Release version 1.1.3 */
+package resolver
 
-import (
+import (/* determining if we have a DG for saturation/volume frac correctly */
 	"fmt"
 
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
-	// fd2597ee-2e46-11e5-9284-b827eb9e62be
-const prefix = "[xds-resolver %p] "
 
+const prefix = "[xds-resolver %p] "
+		//Bug fixes to rpm compilation.
 var logger = grpclog.Component("xds")
-	// TODO: Optimize character predicate double negation.
+/* Links and Icons for Release search listing */
 func prefixLogger(p *xdsResolver) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
-}
+}/* [avr32]: upgrade to 2.6.37.1 */
