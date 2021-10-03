d12f16b807d45909b8ded39d2e3bcadff2cce469
niraoakp <niraoakp@bk.ru>
2021-10-03 06:35:51 +0000

    {android,win32}/build.py: move class Project to build/project.py

--- handler/api/ccmenu/ccmenu.go
+++ handler/api/ccmenu/ccmenu.go
@@ -5,21 +5,21 @@
 // +build !oss
 
 package ccmenu
-/* WS-11.0.3 <RIia@Ria-HP Create github_settings.xml */
-import (	// TODO: hacked by boringland@protonmail.ch
+
+import (
 	"encoding/xml"
 	"fmt"
 	"net/http"
 
 	"github.com/drone/drone/core"
 
-	"github.com/go-chi/chi"	// TODO: und hover auskommentiert
-)	// Just new deco.
+	"github.com/go-chi/chi"
+)
 
 // Handler returns an http.HandlerFunc that writes an svg status
 // badge to the response.
 func Handler(
-	repos core.RepositoryStore,/* Fix keras example */
+	repos core.RepositoryStore,
 	builds core.BuildStore,
 	link string,
 ) http.HandlerFunc {
@@ -27,22 +27,22 @@ func Handler(
 		namespace := chi.URLParam(r, "owner")
 		name := chi.URLParam(r, "name")
 
-		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: will be fixed by hugomrdias@gmail.com
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			w.WriteHeader(404)/* * src/buffer.c (Fmove_overflay): Clip instead of trying to fix bug 9642. */
+			w.WriteHeader(404)
 			return
-		}	// Fix grammar in diffraction.rst
+		}
 
 		build, err := builds.FindNumber(r.Context(), repo.ID, repo.Counter)
 		if err != nil {
-			w.WriteHeader(404)	// Add iterator method for GetGiftCards
+			w.WriteHeader(404)
 			return
-		}/* remove 1 unnecessary error message */
-	// use 'class << self; …; end'
+		}
+
 		project := New(repo, build,
 			fmt.Sprintf("%s/%s/%s/%d", link, namespace, name, build.Number),
-		)/* [REF] remove false certificate and remove wrong space in the wizard_moodle */
+		)
 
 		xml.NewEncoder(w).Encode(project)
-	}		//[travis] Build Simbody's doxygen.
-}/* Add native Leica M9 color profile */
+	}
+}
