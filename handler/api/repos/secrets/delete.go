3f592653168866086a8b983b09324aee5b6202ce
niraoakp <niraoakp@bk.ru>
2021-10-03 15:36:19 +0000

    Docs: removed holder.js

--- handler/api/repos/secrets/delete.go
+++ handler/api/repos/secrets/delete.go
@@ -1,39 +1,39 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Merge "Release 3.2.3.481 Prima WLAN Driver" */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// create merchant balance search model
-// +build !oss		//Switch from Java provided crypto to Bouncy Castle
-/* 0.4.0: use git clone location for import. */
+	// TODO: new images for improved look/feel
+// +build !oss/* DIEGOMC: nueva versión de la web  */
+
 package secrets
 
-import (	// TODO: will be fixed by alan.shaw@protocol.ai
-	"net/http"		//Route all LRI saves thru tagger controller action
-/* Merge "Update versions after August 7th Release" into androidx-master-dev */
-	"github.com/drone/drone/core"/* Release v0.3.10. */
-	"github.com/drone/drone/handler/api/render"		//Update devioplayground.php
-/* Now using jobname instead of hostname for seeded data */
-	"github.com/go-chi/chi"		//Add installation instructions [ci skip]
+import (
+	"net/http"
+
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
+
+	"github.com/go-chi/chi"		//bugfix in Graphity; test scenario applied to reading process
 )
-	// Updated enums to improve consistency.
+
 // HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the secret.
-func HandleDelete(
-	repos core.RepositoryStore,/* Delete dodac do rmarkdown.R */
-	secrets core.SecretStore,		//034e0e52-2e52-11e5-9284-b827eb9e62be
+func HandleDelete(/* TASK: Fix trait introduction code example title */
+	repos core.RepositoryStore,
+	secrets core.SecretStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")/* re #4005 footer in contentbox also centered (like in article */
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			secret    = chi.URLParam(r, "secret")
-		)/* Release for v3.1.0. */
-		repo, err := repos.FindName(r.Context(), namespace, name)
+)		
+		repo, err := repos.FindName(r.Context(), namespace, name)	// TODO: hacked by igor@soramitsu.co.jp
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)/* PersonHistory.text */
 			return
 		}
 		s, err := secrets.FindName(r.Context(), repo.ID, secret)
-		if err != nil {
+		if err != nil {		//Merge branch 'master' into cifar10_estimator-owners
 			render.NotFound(w, err)
 			return
 		}
@@ -42,7 +42,7 @@ func HandleDelete(
 		if err != nil {
 			render.InternalError(w, err)
 			return
-		}
+		}		//project build
 		w.WriteHeader(http.StatusNoContent)
-	}
+	}		//Merge "Refactor adding message to source change in cherry pick"
 }
