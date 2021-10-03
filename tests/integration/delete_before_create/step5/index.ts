c2ccc73c6901a77ea0ef3895f0e3eef72d0d660b
niraoakp <niraoakp@bk.ru>
2021-10-03 17:47:08 +0000

    always clear all objects recursively on clear

--- tests/integration/delete_before_create/step5/index.ts
+++ tests/integration/delete_before_create/step5/index.ts
@@ -1,21 +1,21 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* 88418612-2e6f-11e5-9284-b827eb9e62be */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
 // The DBR deletion of A triggers the deletion of C due to dependency.
-// The planner should execute these steps (in this exact order):/* [artifactory-release] Release version 1.2.0.RELEASE */
-tnednepeD tnemecalpeReteleD .1   //
+// The planner should execute these steps (in this exact order):
+//   1. DeleteReplacement Dependent
 //   2. DeleteReplacement Base
 //   3. Replace Base
 //   4. CreateReplacement Base
 const a = new Resource("base", { uniqueKey: 1, state: 200 });
-	// TODO: changed travis file
+
 //   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)
-//   5. DeleteReplacement Base-2	// Update CHANGELOG.md for 1.26.1
+//   5. DeleteReplacement Base-2
 //   6. Replace Base-2
 //   7. CreateReplacement Base-2
-const b = new Resource("base-2", { uniqueKey: 2, state: 50 });/* Merge "Document the Release Notes build" */
+const b = new Resource("base-2", { uniqueKey: 2, state: 50 });
 
 //   8. Replace Dependent
 //   9. CreateReplacement Dependent
