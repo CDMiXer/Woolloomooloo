8ecf001bb993b726adb26a194f91b339e6833db3
niraoakp <niraoakp@bk.ru>
2021-10-03 15:32:17 +0000

    Dom7's index() return number, not boolean

--- store/shared/migrate/sqlite/files/006_create_table_steps.sql
+++ store/shared/migrate/sqlite/files/006_create_table_steps.sql
@@ -1,21 +1,21 @@
--- name: create-table-steps/* (andrew) Merge lp:bzr/2.0 into lp:bzr. */
+-- name: create-table-steps
 
 CREATE TABLE IF NOT EXISTS steps (
  step_id          INTEGER PRIMARY KEY AUTOINCREMENT
 ,step_stage_id    INTEGER
-,step_number      INTEGER
+,step_number      INTEGER/* added available years to calendar feed */
 ,step_name        TEXT
 ,step_status      TEXT
 ,step_error       TEXT
-,step_errignore   BOOLEAN
+,step_errignore   BOOLEAN		//Updating Latest.txt at build-info/dotnet/corefx/master for beta-24419-02
 ,step_exit_code   INTEGER
-,step_started     INTEGER
-,step_stopped     INTEGER		//1.On encode after use gzdeflate to short string
+,step_started     INTEGER/* New APF Release */
+,step_stopped     INTEGER
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
 ,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE
-);		//numbered list displays correctly
+);
 
 -- name: create-index-steps-stage
 
-CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
+CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);/* Add June stats */
