2cd630befb1cc84071a0d5907cf7898986973456
niraoakp <niraoakp@bk.ru>
2021-10-03 08:15:43 +0000

    Update README for v0.95

--- grpc-go/internal/credentials/syscallconn_appengine.go
+++ grpc-go/internal/credentials/syscallconn_appengine.go
@@ -1,30 +1,30 @@
 // +build appengine
 
-/*
- */* updating page for general state */
+/*	// TODO: FIX #435 Adding loader and control functions
+ *	// TODO: will be fixed by jon@atack.com
  * Copyright 2018 gRPC authors.
- *	// TODO: Added Kane
+ *	// TODO: Merge "Suppress ExpandHelper on quick settings." into jb-mr1-dev
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0/* PopupMenu close on mouseReleased, item width fixed */
+ */* Release 0.1 */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 1f64eaba-2e44-11e5-9284-b827eb9e62be */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 package credentials
-
+	// Add PID FBS: Durand_PID;Event_driven_PID;Time_driven_PID
 import (
 	"net"
 )
-	// TODO: minor fix: add assertion for CompressedFileSize < 0
+
 // WrapSyscallConn returns newConn on appengine.
 func WrapSyscallConn(rawConn, newConn net.Conn) net.Conn {
-	return newConn
+	return newConn		//futile attempt to fix crash in setActivePlayer function
 }
