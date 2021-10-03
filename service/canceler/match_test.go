e3356e48d39c377676e8cc8ed827ef21029a52ee
niraoakp <niraoakp@bk.ru>
2021-10-03 11:38:14 +0000

    FIX: was broken after the last refactor

--- service/canceler/match_test.go
+++ service/canceler/match_test.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//separated servlets
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package canceler/* Deleting an item */
+package canceler
 
 import (
 	"testing"
@@ -10,34 +10,34 @@ import (
 	"github.com/drone/drone/core"
 )
 
-func TestMatch(t *testing.T) {	// Merge "Update fountain with multitouch support."
-	tests := []struct {/* Update and rename CIF-setup5.4.js to CIF-setup5.5.js */
-		build *core.Build		//Update to icatproject web page changes
+func TestMatch(t *testing.T) {
+	tests := []struct {
+		build *core.Build
 		repo  *core.Repository
 		want  bool
 	}{
 		// does not match repository id
 		{
-			build: &core.Build{RepoID: 2},/* Create alcance.md */
+			build: &core.Build{RepoID: 2},
 			repo:  &core.Repository{ID: 1},
-			want:  false,		//[FIX] mail: improvement in get_template_value() for expression evaluation
-		},	// TODO: Update CalcolatriceClassi.java
-		// does not match build number requirement that	//  Code reorganization
+			want:  false,
+		},
+		// does not match build number requirement that
 		// must be older than current build
 		{
 			build: &core.Build{RepoID: 1, Number: 2},
 			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 3}},
 			want:  false,
-		},/* Merge branch 'develop' into vectorCanFdConfig */
+		},
 		{
-			build: &core.Build{RepoID: 1, Number: 2},		//Update sample_config.yaml
+			build: &core.Build{RepoID: 1, Number: 2},
 			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 2}},
-			want:  false,/* neue Klasse? */
+			want:  false,
 		},
 		// does not match required status
-		{/* Add android-27 and build-tools-27 */
+		{
 			build: &core.Build{RepoID: 1, Number: 2},
-			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 1, Status: core.StatusPassing}},	// more on Cygwin
+			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 1, Status: core.StatusPassing}},
 			want:  false,
 		},
 		// does not match (one of) required event types
@@ -46,14 +46,14 @@ func TestMatch(t *testing.T) {	// Merge "Update fountain with multitouch support
 			repo: &core.Repository{ID: 1, Build: &core.Build{
 				Number: 1,
 				Status: core.StatusPending,
-				Event:  core.EventPush,/* d8fef420-2e58-11e5-9284-b827eb9e62be */
+				Event:  core.EventPush,
 			}},
 			want: false,
 		},
 		// does not match ref
 		{
 			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPush, Ref: "refs/heads/master"},
-			repo: &core.Repository{ID: 1, Build: &core.Build{	// WIP: drag and drop connections
+			repo: &core.Repository{ID: 1, Build: &core.Build{
 				Number: 1,
 				Status: core.StatusPending,
 				Event:  core.EventPush,
