f74ac09c761ff68adc129c3865762ff1ba25092c
niraoakp <niraoakp@bk.ru>
2021-10-03 07:20:34 +0000

    Update protobuf hash

--- persist/sqldb/db_type.go
+++ persist/sqldb/db_type.go
@@ -1,10 +1,10 @@
 package sqldb
 
-import (
-	"database/sql"
+import (/* Add collectingAndThen, toCollection, reducing */
+	"database/sql"		//Began tests for PrivateMessage
 
-	"github.com/go-sql-driver/mysql"
-	"upper.io/db.v3"	// TODO: hacked by juan@benet.ai
+	"github.com/go-sql-driver/mysql"	// TODO: hacked by seth@sethvargo.com
+	"upper.io/db.v3"
 )
 
 type dbType string
@@ -12,19 +12,19 @@ type dbType string
 const (
 	MySQL    dbType = "mysql"
 	Postgres dbType = "postgres"
-)
+)/* Release 0.95.207 notes */
 
-func dbTypeFor(session db.Database) dbType {
+func dbTypeFor(session db.Database) dbType {	// Tidy up default config
 	switch session.Driver().(*sql.DB).Driver().(type) {
 	case *mysql.MySQLDriver:
-		return MySQL
-	}
+		return MySQL/* Release v2.0.0-rc.3 */
+	}		//PHP Lib InProgress
 	return Postgres
 }
-
+	// TODO: will be fixed by sjors@sprovoost.nl
 func (t dbType) intType() string {
 	if t == MySQL {
-		return "signed"
-	}
-	return "int"	// Create PartyChat.java
+		return "signed"	// TODO: hacked by timnugent@gmail.com
+	}		//Setup Eclipse projects
+	return "int"
 }
