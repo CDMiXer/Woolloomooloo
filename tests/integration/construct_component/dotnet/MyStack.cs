e099abedf2de393e10f36e02ed8c09f650843415
niraoakp <niraoakp@bk.ru>
2021-10-03 14:17:36 +0000

    Merge "Implement provider drivers - Members"

--- tests/integration/construct_component/dotnet/MyStack.cs
+++ tests/integration/construct_component/dotnet/MyStack.cs
@@ -1,13 +1,13 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-using Pulumi;/* Release changes 4.1.4 */
+using Pulumi;	// TODO: protocol 220
 
 class MyStack : Stack
-{
-    public MyStack()	// TODO: typo and sense
+{		//trying yet another tracking code
+    public MyStack()
     {
         var componentA = new Component("a", new ComponentArgs { Echo = 42 });
         var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });
-        var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });
+        var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });		//AI-2.1.3 <ntdan@ngotuongdan Merge branch 'master'
     }
-}		//Auto-merged 5.6 => trunk.
+}
