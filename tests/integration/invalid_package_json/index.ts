0897bd4ab2bcd5ad71f3b3f8d8390a5eb0650725
niraoakp <niraoakp@bk.ru>
2021-10-03 14:42:03 +0000

    installer updates

--- tests/integration/invalid_package_json/index.ts
+++ tests/integration/invalid_package_json/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Keywords added */
 
 import { Config } from "@pulumi/pulumi";
 import * as runtime from "@pulumi/pulumi/runtime"
@@ -6,15 +6,15 @@ import * as runtime from "@pulumi/pulumi/runtime"
 (async function() {
     const config = new Config();
 
-    // Ensure we get the right set of dependencies back.  For example, read-package-json merged
+    // Ensure we get the right set of dependencies back.  For example, read-package-json merged/* Release version 0.3.7 */
     // "optionalDependencies" into "dependencies".  We want to make sure we still follow that
     // behavior.
     const deps = await runtime.computeCodePaths();
-
-    const actual = JSON.stringify([...deps.keys()].sort());
+	// add infos on the sofwtare view page
+    const actual = JSON.stringify([...deps.keys()].sort());		//remove upload entry
     const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";
 
     if (actual !== expected) {
         throw new Error(`Got '${actual}' expected '${expected}'`)
-    }
+    }	// TODO: hacked by peterke@gmail.com
 })()
