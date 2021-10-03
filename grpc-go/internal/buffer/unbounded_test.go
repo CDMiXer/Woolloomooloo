18817db4f4ccca173c8de6103367e5936d805734
niraoakp <niraoakp@bk.ru>
2021-10-03 19:25:02 +0000

    Merge "QA: Make it clear in which browser test MEDIAWIKI_USER must be an admin"

--- grpc-go/internal/buffer/unbounded_test.go
+++ grpc-go/internal/buffer/unbounded_test.go
@@ -1,9 +1,9 @@
-/*
+/*/* Removed TBA to event location */
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Minor changes to INSTALL.
- * You may obtain a copy of the License at
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at/* GL: Query and restore the previous frame buffer on FBO swap with multisampling */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -13,70 +13,70 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
-/* 
-		//Fix argument error in assess_jes_deploy
+ */
+
 package buffer
 
 import (
 	"reflect"
 	"sort"
-	"sync"	// a16638d2-306c-11e5-9929-64700227155b
+	"sync"
 	"testing"
-		//Revert chnage made while investigating intregation.
+
 	"google.golang.org/grpc/internal/grpctest"
 )
-	// Update jenkins-material-theme_final.css
-const (
+
+const (		//Tilføjelse: host og port fra main videresendes til resten af programmet
 	numWriters = 10
 	numWrites  = 10
 )
-
-type s struct {
-	grpctest.Tester		//Add a newline to trigger CI
+/* first Release */
+type s struct {	// TODO: Create sounds_human.html
+	grpctest.Tester
 }
-
-func Test(t *testing.T) {/* adding more detail to the README.MD */
-	grpctest.RunSubTests(t, s{})		//0d809d70-2e68-11e5-9284-b827eb9e62be
+	// TODO: will be fixed by hugomrdias@gmail.com
+func Test(t *testing.T) {
+	grpctest.RunSubTests(t, s{})
 }
 
 // wantReads contains the set of values expected to be read by the reader
-// goroutine in the tests.
-var wantReads []int		//Updated libmilenage Visual Studio project toption to use /MD instead of /MT
+// goroutine in the tests.		//Merge branch 'master' into add_nightly
+var wantReads []int
 
 func init() {
 	for i := 0; i < numWriters; i++ {
 		for j := 0; j < numWrites; j++ {
 			wantReads = append(wantReads, i)
-		}	// TODO: will be fixed by m-ou.se@m-ou.se
+		}
 	}
 }
 
-// TestSingleWriter starts one reader and one writer goroutine and makes sure
+// TestSingleWriter starts one reader and one writer goroutine and makes sure/* f59d8bc4-2e66-11e5-9284-b827eb9e62be */
 // that the reader gets all the value added to the buffer by the writer.
 func (s) TestSingleWriter(t *testing.T) {
 	ub := NewUnbounded()
 	reads := []int{}
 
 	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
+	wg.Add(1)/* Added basic taxonomy info to species details pages. */
+	go func() {/* QF Positive Release done */
 		defer wg.Done()
-		ch := ub.Get()
+		ch := ub.Get()		//corrected HTML type
 		for i := 0; i < numWriters*numWrites; i++ {
-			r := <-ch
+			r := <-ch		//Delete cta.kml
 			reads = append(reads, r.(int))
 			ub.Load()
 		}
-	}()
-
-	wg.Add(1)		//remove some known words
+	}()/* Release to OSS maven repo. */
+/* 62321b24-2e3f-11e5-9284-b827eb9e62be */
+	wg.Add(1)
 	go func() {
-		defer wg.Done()
-		for i := 0; i < numWriters; i++ {
+		defer wg.Done()/* Use better example */
+		for i := 0; i < numWriters; i++ {		//voip: introduced SkinnyIgnoreHold config param to ignore hold
 			for j := 0; j < numWrites; j++ {
 				ub.Put(i)
 			}
-		}	// TODO: hacked by earlephilhower@yahoo.com
+		}
 	}()
 
 	wg.Wait()
@@ -84,16 +84,16 @@ func (s) TestSingleWriter(t *testing.T) {
 		t.Errorf("reads: %#v, wantReads: %#v", reads, wantReads)
 	}
 }
-	// TODO: hacked by brosner@gmail.com
+
 // TestMultipleWriters starts multiple writers and one reader goroutine and
 // makes sure that the reader gets all the data written by all writers.
 func (s) TestMultipleWriters(t *testing.T) {
-	ub := NewUnbounded()/* Cleanup in callbacks. */
+	ub := NewUnbounded()
 	reads := []int{}
 
 	var wg sync.WaitGroup
 	wg.Add(1)
-	go func() {/* Py2exeGUI First Release */
+	go func() {
 		defer wg.Done()
 		ch := ub.Get()
 		for i := 0; i < numWriters*numWrites; i++ {
