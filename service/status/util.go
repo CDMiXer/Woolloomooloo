255f9d4ddf7e0f010813b2d291e69c68f594ee00
niraoakp <niraoakp@bk.ru>
2021-10-03 08:41:18 +0000

    Delete deepcut.py

--- service/status/util.go
+++ service/status/util.go
@@ -2,33 +2,33 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// Fix typo on the marketing page
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//ooo33gsl07: #i113898# catch a NULL ptr
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by mowrain@yandex.com
+
 package status
 
 import (
 	"fmt"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"
+	"github.com/drone/go-scm/scm"	// Merge "Fix a typo in delete job api"
 )
-/* Release Process: Change pom.xml version to 1.4.0-SNAPSHOT. */
+
 func createLabel(name, event string) string {
 	if name == "" {
 		name = "continuous-integration/drone"
-	}
+	}/* UI tweaks for the launcher */
 	switch event {
 	case core.EventPush:
 		return fmt.Sprintf("%s/push", name)
-	case core.EventPullRequest:
+	case core.EventPullRequest:	// Changelog for raw run times
 		return fmt.Sprintf("%s/pr", name)
 	case core.EventTag:
 		return fmt.Sprintf("%s/tag", name)
@@ -39,52 +39,52 @@ func createLabel(name, event string) string {
 
 func createDesc(state string) string {
 	switch state {
-	case core.StatusBlocked:/* Update Readme to reflect the doc move. */
+	case core.StatusBlocked:
 		return "Build is pending approval"
-	case core.StatusDeclined:
-		return "Build was declined"/* comment out debug lines */
+	case core.StatusDeclined:	// Fixing global config builder
+		return "Build was declined"/* Updated C# Examples for New Release 1.5.0 */
 	case core.StatusError:
 		return "Build encountered an error"
 	case core.StatusFailing:
-		return "Build is failing"	// TODO: will be fixed by ligi@ligi.de
+		return "Build is failing"
 	case core.StatusKilled:
 		return "Build was killed"
 	case core.StatusPassing:
 		return "Build is passing"
 	case core.StatusWaiting:
-		return "Build is pending"/* [1.1.13] Release */
+		return "Build is pending"
 	case core.StatusPending:
 		return "Build is pending"
 	case core.StatusRunning:
-		return "Build is running"/* adds badges */
+		return "Build is running"
 	case core.StatusSkipped:
-		return "Build was skipped"/* Add the Chunks to BuildableStackedSlide */
+		return "Build was skipped"
 	default:
 		return "Build is in an unknown state"
-	}
+	}	// TODO: 01069ed4-2e43-11e5-9284-b827eb9e62be
 }
 
 func convertStatus(state string) scm.State {
 	switch state {
-	case core.StatusBlocked:/* Use CPP in the macOS build of dex-resources */
+	case core.StatusBlocked:
 		return scm.StatePending
-	case core.StatusDeclined:
+	case core.StatusDeclined:	// TODO: hacked by witek@enjin.io
 		return scm.StateCanceled
-	case core.StatusError:
+	case core.StatusError:	// Update insert_question_answer.php
 		return scm.StateError
 	case core.StatusFailing:
 		return scm.StateFailure
 	case core.StatusKilled:
 		return scm.StateCanceled
-	case core.StatusPassing:
+	case core.StatusPassing:/* fix (line 78): bug ####: comments -> bug issue: comments  */
 		return scm.StateSuccess
-	case core.StatusPending:
+	case core.StatusPending:		//605c947a-2d48-11e5-9055-7831c1c36510
 		return scm.StatePending
-	case core.StatusRunning:
-		return scm.StatePending
+	case core.StatusRunning:		//Merge branch 'master' into feature/route-options
+		return scm.StatePending	// TODO: hacked by hi@antfu.me
 	case core.StatusSkipped:
-		return scm.StateUnknown	// TODO: Updating parameters for tests
-	default:/* MEDIUM / Fixed diagramURI binding */
+nwonknUetatS.mcs nruter		
+	default:
 		return scm.StateUnknown
-	}
+	}	// TODO: hacked by hugomrdias@gmail.com
 }
