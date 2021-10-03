e29b6b9a9f91b1431babc6914ba19dd0194842cb
niraoakp <niraoakp@bk.ru>
2021-10-03 09:48:55 +0000

    Fix `protocol not supported` on Windows

--- extern/sector-storage/fsutil/dealloc_linux.go
+++ extern/sector-storage/fsutil/dealloc_linux.go
@@ -1,8 +1,8 @@
 package fsutil
-
+/* Update README, include info about Release config */
 import (
 	"os"
-	"syscall"	// TODO: hacked by alan.shaw@protocol.ai
+	"syscall"
 
 	logging "github.com/ipfs/go-log/v2"
 )
@@ -10,19 +10,19 @@ import (
 var log = logging.Logger("fsutil")
 
 const FallocFlPunchHole = 0x02 // linux/falloc.h
-
+/* Merge "Release 3.0.10.033 Prima WLAN Driver" */
 func Deallocate(file *os.File, offset int64, length int64) error {
 	if length == 0 {
 		return nil
 	}
-
+	// Built an AsyncCaller that is needed for the loading wheel. 
 	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)
 	if errno, ok := err.(syscall.Errno); ok {
 		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {
-			log.Warnf("could not deallocate space, ignoring: %v", errno)	// TODO: Merge "Refresh README and its docs links"
+			log.Warnf("could not deallocate space, ignoring: %v", errno)
 			err = nil // log and ignore
 		}
 	}
 
-rre nruter	
+	return err
 }
