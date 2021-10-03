6de5e8cf6d53c09426a5966ba7a72ee435e7fd9e
niraoakp <niraoakp@bk.ru>
2021-10-03 03:36:40 +0000

    Testing for condition was invalid.

--- tests/integration/query/query_test.go
+++ tests/integration/query/query_test.go
@@ -1,19 +1,19 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Updating builds to 0.1.59
 // +build nodejs all
 
 package ints
 
 import (
-	"testing"
+	"testing"	// TODO: Added Beta v1.0.0
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)
-
+)	// TODO: will be fixed by steven@stebalien.com
+		//Update testMasterGet.py
 // TestQuery creates a stack and runs a query over the stack's resource ouptputs.
-func TestQuery(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{
+func TestQuery(t *testing.T) {	// Scaffolding specs and classes
+	integration.ProgramTest(t, &integration.ProgramTestOptions{	// TODO: hacked by peterke@gmail.com
 		// Create Pulumi resources.
-		Dir:          "step1",
+		Dir:          "step1",/* f_concord1 will take care of dual */
 		StackName:    "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2",
 		Dependencies: []string{"@pulumi/pulumi"},
 		CloudURL:     "file://~", // Required; we hard-code the stack name
@@ -26,12 +26,12 @@ func TestQuery(t *testing.T) {
 				ExpectFailure: true,
 			},
 			// Run a query during `pulumi query`. Should succeed.
-			{
+			{/* #4: Some changes, but not there yet... */
 				Dir:           "step3",
 				Additive:      true,
 				QueryMode:     true,
 				ExpectFailure: false,
-			},
+			},/* test_web: workaround broken HEAD behavior in twisted-2.5.0 and earlier */
 		},
 	})
 }
