6419e4dbd7268fc6dabb3187ce6c0203d1cf4dc0
niraoakp <niraoakp@bk.ru>
2021-10-03 07:41:12 +0000

    c63369f4-2e74-11e5-9284-b827eb9e62be

--- handler/api/errors/errors_test.go
+++ handler/api/errors/errors_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: Update appointments.md
+// that can be found in the LICENSE file.
 
 package errors
 
 import "testing"
-	// TODO: hacked by magik6k@gmail.com
-func TestError(t *testing.T) {		//Fixed stupid bug and extract new lines for translation
-	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message/* Create IGeography */
+
+func TestError(t *testing.T) {
+	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message
 	if got != want {
 		t.Errorf("Want error string %q, got %q", got, want)
-	}/* composer.phar is no part of the project */
+	}
 }
