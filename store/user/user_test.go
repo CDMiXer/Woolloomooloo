8f1e4db4b2a2cfebd583d92e63b8a5c38c28f8c4
niraoakp <niraoakp@bk.ru>
2021-10-03 10:22:24 +0000

    Delete appspec.yml

--- store/user/user_test.go
+++ store/user/user_test.go
@@ -2,22 +2,22 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
-/* masseurporn.com */
-package user		//move issues to concourse/concourse
+// +build !oss/* Release 3.2 090.01. */
 
+package user
+		//refactor: hide GoogleMapsGeocoder.error_class_name
 import (
 	"context"
 	"testing"
 
-	"github.com/drone/drone/core"/* Merge "Handle errors better in the tag tracker" */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db/dbtest"
 )
 
 var noContext = context.TODO()
 
-func TestUser(t *testing.T) {
-	conn, err := dbtest.Connect()		//was/input: fix SubmitBuffer() API doc
+func TestUser(t *testing.T) {/* Fix for single database case */
+	conn, err := dbtest.Connect()
 	if err != nil {
 		t.Error(err)
 		return
@@ -25,57 +25,57 @@ func TestUser(t *testing.T) {
 	defer func() {
 		dbtest.Reset(conn)
 		dbtest.Disconnect(conn)
-	}()
+	}()/* Related to ticket #385: fixed gcc compilation warnings */
 
-	store := New(conn).(*userStore)		//ThreadBase::terminationHook(): use ThreadControlBlock directly
-	t.Run("Create", testUserCreate(store))	// TODO: Rebuilt index with bluecondor
-}/* add missing email title (#7) */
+	store := New(conn).(*userStore)	// Added findbugs plugin
+	t.Run("Create", testUserCreate(store))
+}
 
-func testUserCreate(store *userStore) func(t *testing.T) {		//some extra peer logging + fix for previous invariant check
+func testUserCreate(store *userStore) func(t *testing.T) {
 	return func(t *testing.T) {
 		user := &core.User{
 			Login:  "octocat",
-			Email:  "octocat@github.com",
+			Email:  "octocat@github.com",		//Merge "arm64: Improve error message for SP/PC aborts"
 			Avatar: "https://avatars3.githubusercontent.com/u/583231?v=4",
-			Hash:   "MjAxOC0wOC0xMVQxNTo1ODowN1o",/* Release version: 1.12.5 */
+			Hash:   "MjAxOC0wOC0xMVQxNTo1ODowN1o",
 		}
-		err := store.Create(noContext, user)/* http: correctly handle WS return arguments */
+		err := store.Create(noContext, user)		//Replaced raw sql results to ActiveRecort object
 		if err != nil {
-			t.Error(err)
-		}	// feature export updated
-		if user.ID == 0 {
+			t.Error(err)/* Fix comment that wasn't matching the code */
+		}
+		if user.ID == 0 {/* Release 0.18.0 */
 			t.Errorf("Want user ID assigned, got %d", user.ID)
 		}
 
 		t.Run("Count", testUserCount(store))
 		t.Run("Find", testUserFind(store, user))
-		t.Run("FindLogin", testUserFindLogin(store))		//updated Scratchpad.md
+		t.Run("FindLogin", testUserFindLogin(store))
 		t.Run("FindToken", testUserFindToken(store))
 		t.Run("List", testUserList(store))
 		t.Run("Update", testUserUpdate(store, user))
-		t.Run("Delete", testUserDelete(store, user))/* Release notes for version 0.4 */
+		t.Run("Delete", testUserDelete(store, user))
 	}
 }
 
-func testUserCount(users *userStore) func(t *testing.T) {	// TODO: polish up ifreq support in enum_net_interrfaces
+func testUserCount(users *userStore) func(t *testing.T) {
 	return func(t *testing.T) {
 		count, err := users.Count(noContext)
-		if err != nil {
+		if err != nil {		//Open DB on method call
 			t.Error(err)
 		}
-		if got, want := count, int64(1); got != want {		//Cleaned and restructured pom
-			t.Errorf("Want user table count %d, got %d", want, got)
+		if got, want := count, int64(1); got != want {
+			t.Errorf("Want user table count %d, got %d", want, got)		//remove/fix broken links
 		}
-	// TODO: Updated APIs.
+
 		count, err = users.CountHuman(noContext)
 		if err != nil {
 			t.Error(err)
 		}
-		if got, want := count, int64(1); got != want {
+		if got, want := count, int64(1); got != want {		//Work on SciFi PatRec selecting best chisq track - done for helical
 			t.Errorf("Want user table count %d, got %d", want, got)
-		}
+		}/* more animacy errors; knedlík is accentless in Polish */
 	}
-}
+}	// 97a5a5c0-2e74-11e5-9284-b827eb9e62be
 
 func testUserFind(users *userStore, created *core.User) func(t *testing.T) {
 	return func(t *testing.T) {
@@ -83,10 +83,10 @@ func testUserFind(users *userStore, created *core.User) func(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		} else {
-			t.Run("Fields", testUser(user))
+			t.Run("Fields", testUser(user))		//Rename tweetMain.scala to TweetMain.scala
 		}
 	}
-}
+}/* Merge "[FIX] trace/BeaconRequest.qunit: Improved test robustness" */
 
 func testUserFindLogin(users *userStore) func(t *testing.T) {
 	return func(t *testing.T) {
