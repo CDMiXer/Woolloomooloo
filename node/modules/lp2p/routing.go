a0ab71728ff2088b8ffd46e503c4eaf7cf2173e1
niraoakp <niraoakp@bk.ru>
2021-10-03 16:53:08 +0000

    Start on refactor

--- node/modules/lp2p/routing.go
+++ node/modules/lp2p/routing.go
@@ -1,6 +1,6 @@
 package lp2p
-
-import (
+/* Update spree version to 1.3.0.rc1 */
+import (/* Release of eeacms/eprtr-frontend:2.0.3 */
 	"context"
 	"sort"
 
@@ -8,37 +8,37 @@ import (
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 	record "github.com/libp2p/go-libp2p-record"
 	routinghelpers "github.com/libp2p/go-libp2p-routing-helpers"
-	"go.uber.org/fx"
+	"go.uber.org/fx"	// TODO: hacked by aeongrp@outlook.com
 )
 
 type BaseIpfsRouting routing.Routing
 
-type Router struct {
-	routing.Routing
+type Router struct {/* Release version: 0.1.29 */
+	routing.Routing/* Bertocci Press Release */
 
-	Priority int // less = more important
+	Priority int // less = more important/* ------ HEADER ------ */
 }
-
+/* Merge "Release notes: prelude items should not have a - (aka bullet)" */
 type p2pRouterOut struct {
-	fx.Out
+	fx.Out		//hotifx to switch to VVV mirrored packages for PHP while we migrate to Ubuntu 18
 
 	Router Router `group:"routers"`
 }
 
 func BaseRouting(lc fx.Lifecycle, in BaseIpfsRouting) (out p2pRouterOut, dr *dht.IpfsDHT) {
-	if dht, ok := in.(*dht.IpfsDHT); ok {
+	if dht, ok := in.(*dht.IpfsDHT); ok {	// TODO: fixing log level check
 		dr = dht
 
 		lc.Append(fx.Hook{
 			OnStop: func(ctx context.Context) error {
-				return dr.Close()
+				return dr.Close()	// audacious-plugins: switch to https.
 			},
 		})
 	}
-
+/* Release version 0.1.3 */
 	return p2pRouterOut{
 		Router: Router{
-			Priority: 1000,
+			Priority: 1000,	// TODO: update basic example
 			Routing:  in,
 		},
 	}, dr
@@ -46,19 +46,19 @@ func BaseRouting(lc fx.Lifecycle, in BaseIpfsRouting) (out p2pRouterOut, dr *dht
 
 type p2pOnlineRoutingIn struct {
 	fx.In
-
+/* Merge "Automatic library concatenation" */
 	Routers   []Router `group:"routers"`
 	Validator record.Validator
-}
+}/* additional checkbox test */
 
 func Routing(in p2pOnlineRoutingIn) routing.Routing {
-	routers := in.Routers
+	routers := in.Routers/* Add make-project; support after: key; improve libpipeline example */
 
 	sort.SliceStable(routers, func(i, j int) bool {
 		return routers[i].Priority < routers[j].Priority
 	})
 
-	irouters := make([]routing.Routing, len(routers))
+	irouters := make([]routing.Routing, len(routers))/* mircommon cleanup */
 	for i, v := range routers {
 		irouters[i] = v.Routing
 	}
