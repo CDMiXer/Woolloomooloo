4f5f70f668e0b1cb097f1d22d5270cffc1b8a9c4
niraoakp <niraoakp@bk.ru>
2021-10-03 17:45:45 +0000

    Update volume_cs_CZ.desktop

--- grpc-go/internal/profiling/goid_regular.go
+++ grpc-go/internal/profiling/goid_regular.go
@@ -1,20 +1,20 @@
-// +build !grpcgoid/* Release v0.2.1.7 */
+// +build !grpcgoid
 
-/*		//mentioning dies
+/*
  *
  * Copyright 2019 gRPC authors.
- *		//final implementation for upload check and copy
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Release of eeacms/www:20.8.23 */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Game of Generals (release)
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//(Fixes issue 851)
+ * limitations under the License.
  *
  */
 
