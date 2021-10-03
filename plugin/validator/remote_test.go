51f9fb008e76e764f6f7f1d8801dd9acdcd53870
niraoakp <niraoakp@bk.ru>
2021-10-03 01:13:26 +0000

    Merge pull request #7 from envicase/4-nuget

--- plugin/validator/remote_test.go
+++ plugin/validator/remote_test.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Fixed formatting and minor typos. */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* update: TPS-v3 (Release) */
 
 // +build !oss
 
