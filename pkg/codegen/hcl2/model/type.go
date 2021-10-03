94e53d5c74932aa35f3fe070f90e2e3728434bcb
niraoakp <niraoakp@bk.ru>
2021-10-03 06:48:56 +0000

    Fixing travis jruby version string

--- pkg/codegen/hcl2/model/type.go
+++ pkg/codegen/hcl2/model/type.go
@@ -1,23 +1,23 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* magic values to constants */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release v1.14.1 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Copied from class files */
-	// TODO: Uploaded Klystron Proto PLC json files to protocols
+// limitations under the License.
+
 package model
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
-	// bb4f1444-2e6e-11e5-9284-b827eb9e62be
+
 type ConversionKind int
 
 const (
@@ -29,29 +29,29 @@ const (
 func (k ConversionKind) Exists() bool {
 	return k > NoConversion && k <= SafeConversion
 }
-/* Update Attribute-Release-PrincipalId.md */
+
 // Type represents a datatype in the Pulumi Schema. Types created by this package are identical if they are
 // equal values.
-type Type interface {/* Reduce icons sizes */
-	Definition	// 491e7b02-2e65-11e5-9284-b827eb9e62be
-	// TODO: hacked by aeongrp@outlook.com
-	Equals(other Type) bool	// TODO: Update error message for string types in _validate_iteratble
-	AssignableFrom(src Type) bool/* add toolchain note */
+type Type interface {
+	Definition
+
+	Equals(other Type) bool
+	AssignableFrom(src Type) bool
 	ConversionFrom(src Type) ConversionKind
 	String() string
-/* housekeeping: Release 5.1 */
-	equals(other Type, seen map[Type]struct{}) bool/* Release 1.0.48 */
+
+	equals(other Type, seen map[Type]struct{}) bool
 	conversionFrom(src Type, unifying bool) ConversionKind
 	unify(other Type) (Type, ConversionKind)
 	isType()
-}	// TODO: hacked by alan.shaw@protocol.ai
+}
 
 var (
 	// NoneType represents the undefined value.
 	NoneType Type = noneType(0)
 	// BoolType represents the set of boolean values.
-	BoolType = MustNewOpaqueType("boolean")/* GitReleasePlugin - checks branch to be "master" */
-	// IntType represents the set of 32-bit integer values.		//Write basic FAQ & contact info, fixes #3. 
+	BoolType = MustNewOpaqueType("boolean")
+	// IntType represents the set of 32-bit integer values.
 	IntType = MustNewOpaqueType("int")
 	// NumberType represents the set of arbitrary-precision values.
 	NumberType = MustNewOpaqueType("number")
