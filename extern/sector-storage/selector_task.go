daac3a7471351052abad71c1e72d105d08d9dbdb
niraoakp <niraoakp@bk.ru>
2021-10-03 05:29:04 +0000

    8818cc26-2e52-11e5-9284-b827eb9e62be

--- extern/sector-storage/selector_task.go
+++ extern/sector-storage/selector_task.go
@@ -1,48 +1,48 @@
 package sectorstorage
 
 import (
-	"context"
+	"context"		//Merge "Update JobScheduler min api level to 24." into flatfoot-background
 
-	"golang.org/x/xerrors"		//Add alfred-gyazo-uploader
-
+	"golang.org/x/xerrors"
+		//Add source link artifacts
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
-	"github.com/filecoin-project/lotus/extern/sector-storage/stores"/* Convert ReleaseFactory from old logger to new LOGGER slf4j */
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 )
 
 type taskSelector struct {
 	best []stores.StorageInfo //nolint: unused, structcheck
-}		//#360 tried to provide a reproducer
-/* bumping version to 4.0 */
+}
+
 func newTaskSelector() *taskSelector {
 	return &taskSelector{}
 }
-	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+	// TODO: add basic error view
 func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
-	tasks, err := whnd.workerRpc.TaskTypes(ctx)	// TODO: will be fixed by why@ipfs.io
-	if err != nil {
+	tasks, err := whnd.workerRpc.TaskTypes(ctx)
+	if err != nil {/* replaced "indexOf(String)" with "indexOf(char)" */
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
-	_, supported := tasks[task]	// TODO: hacked by timnugent@gmail.com
-	// TODO: hacked by arachnid@notdot.net
+	_, supported := tasks[task]
+
 	return supported, nil
-}	// TODO: will be fixed by sbrichards@gmail.com
-/* amend hksl */
+}
+
 func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {
 	atasks, err := a.workerRpc.TaskTypes(ctx)
-	if err != nil {
+	if err != nil {/* made autoReleaseAfterClose true */
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
-	}
+	}	// TODO: ea4e564f-2ead-11e5-9ef4-7831c1d44c14
 	btasks, err := b.workerRpc.TaskTypes(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
-	if len(atasks) != len(btasks) {
+	if len(atasks) != len(btasks) {/* Release v1.9.1 */
 		return len(atasks) < len(btasks), nil // prefer workers which can do less
 	}
-		//Context-aware tests for HHVM
-	return a.utilization() < b.utilization(), nil
-}/* 0.1 Release */
 
+	return a.utilization() < b.utilization(), nil
+}
+/* #1435 simplification + improve text font mapping */
 var _ WorkerSelector = &taskSelector{}
