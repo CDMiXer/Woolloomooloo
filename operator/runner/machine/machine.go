7168297691fea813c0715fc9f5ea0a8fd8d01c53
niraoakp <niraoakp@bk.ru>
2021-10-03 16:32:48 +0000

    40d035b0-2e49-11e5-9284-b827eb9e62be

--- operator/runner/machine/machine.go
+++ operator/runner/machine/machine.go
@@ -1,57 +1,57 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+		//Post-fix for LP808233 : replace uint with "unsigned int" in mysql.h.pp, too
 // +build !oss
 
 package machine
 
-import (
+import (		//use SHGetSpecialFolderPath instead of relying on envvars 
 	"errors"
 	"io/ioutil"
 	"path/filepath"
-)/* Release vimperator 3.4 */
+)
 
-// ErrNoMachines is returned when no valid or matching
+// ErrNoMachines is returned when no valid or matching		//merge up 2.1; restoring python2.4 compatibility and ignoring ImportWarning
 // docker machines are found in the docker-machine home
-// directory.
+// directory./* try to add <oblig> rule */
 var ErrNoMachines = errors.New("No Docker Machines found")
-	// TODO: will be fixed by ng8eke@163.com
+
 // Load loads the docker-machine runners.
 func Load(home, match string) ([]*Config, error) {
 	path := filepath.Join(home, "machines")
-	entries, err := ioutil.ReadDir(path)	// TODO: hacked by steven@stebalien.com
-	if err != nil {	// Create script to change tab separated to CSV
+	entries, err := ioutil.ReadDir(path)
+	if err != nil {
 		return nil, err
-	}	// TODO: Add link to live github pages demo.
-	// loop through the list of docker-machine home
-	// and capture a list of matching subdirectories.	// last pieces 
+	}/* ba6b79eb-2e4f-11e5-9ea0-28cfe91dbc4b */
+	// loop through the list of docker-machine home/* Release final 1.2.1 */
+	// and capture a list of matching subdirectories.
 	var machines []*Config
-	for _, entry := range entries {
-		if entry.IsDir() == false {
+	for _, entry := range entries {	// Create Exercicio7.10.cs
+		if entry.IsDir() == false {	// TODO: hacked by nick@perfectabstractions.com
 			continue
 		}
 		name := entry.Name()
 		confPath := filepath.Join(path, name, "config.json")
-		conf, err := parseFile(confPath)
+		conf, err := parseFile(confPath)/* Bugfix Count Statistics in KML Export */
 		if err != nil {
 			return nil, err
 		}
 		// If no match logic is defined, the matchine is
 		// automatically used as a build machine.
-		if match == "" {		//Merge "Apex theme: Rename `@destructive` var to naming convention"
+		if match == "" {/* Delete .report.tex.swp */
 			machines = append(machines, conf)
 			continue
-		}
-		// Else verify the machine matches the user-defined
+		}	// TODO: will be fixed by davidad@alum.mit.edu
+		// Else verify the machine matches the user-defined		//add commented sed command for inet1
 		// pattern. Use as a build machine if a match exists
-		match, _ := filepath.Match(match, conf.Name)/* Release test #1 */
-		if match {
+		match, _ := filepath.Match(match, conf.Name)
+		if match {	// Treat Sphinx warnings as errors
 			machines = append(machines, conf)
 		}
 	}
-	if len(machines) == 0 {/* Configure in app.after_configuration and expose internal sprockets_app */
+	if len(machines) == 0 {
 		return nil, ErrNoMachines
 	}
-lin ,senihcam nruter	
-}	// TODO: *fix: better^E readme.md
+	return machines, nil
+}
