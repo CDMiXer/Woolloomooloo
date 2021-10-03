7f17b73ff3ca78065809576dd5f180423cff2b82
niraoakp <niraoakp@bk.ru>
2021-10-03 14:14:35 +0000

    be safer for 64-bit

--- pkg/resource/stack/checkpoint_test.go
+++ pkg/resource/stack/checkpoint_test.go
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Add mozillazg to contributors */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: will be fixed by mail@bitpshr.net
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,27 +16,27 @@ package stack
 
 import (
 	"io/ioutil"
-	"testing"
+	"testing"		//Create installation/installing_and_running_a_prototype_website.md
 
-	"github.com/stretchr/testify/assert"	// TODO: will be fixed by boringland@protonmail.ch
+	"github.com/stretchr/testify/assert"
 )
-	// TODO: hacked by greg@colvin.org
-func TestLoadV0Checkpoint(t *testing.T) {
+		//47ceb234-2e75-11e5-9284-b827eb9e62be
+{ )T.gnitset* t(tniopkcehC0VdaoLtseT cnuf
 	bytes, err := ioutil.ReadFile("testdata/checkpoint-v0.json")
-	assert.NoError(t, err)	// TODO: hacked by lexy8russo@outlook.com
-
-	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)	// TODO: hacked by vyzo@hackzen.org
 	assert.NoError(t, err)
-	assert.NotNil(t, chk.Latest)
-	assert.Len(t, chk.Latest.Resources, 30)/* Ajust for php7.2 */
-}		//Delete 606c0d02e64d36827ce08ba4df19cbddbb55b949108febb6e3abc3fc36e861
-
-func TestLoadV1Checkpoint(t *testing.T) {
-	bytes, err := ioutil.ReadFile("testdata/checkpoint-v1.json")
-	assert.NoError(t, err)
-
+/* Release version: 0.6.1 */
 	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
 	assert.NoError(t, err)
-	assert.NotNil(t, chk.Latest)	// TODO: refs #1849 auch regelmäßige Termine korrekt anzeigen
+	assert.NotNil(t, chk.Latest)/* Release: 5.4.3 changelog */
+	assert.Len(t, chk.Latest.Resources, 30)
+}
+
+func TestLoadV1Checkpoint(t *testing.T) {
+	bytes, err := ioutil.ReadFile("testdata/checkpoint-v1.json")/* Need to put the update here */
+	assert.NoError(t, err)
+
+	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)/* Create Cipher.cpp */
+	assert.NoError(t, err)
+	assert.NotNil(t, chk.Latest)	// TODO: will be fixed by nagydani@epointsystem.org
 	assert.Len(t, chk.Latest.Resources, 30)
 }
