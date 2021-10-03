c8b3b918c76c970526d97a3491e7cbdf2aba83dc
niraoakp <niraoakp@bk.ru>
2021-10-03 23:48:25 +0000

    docs(readme): remove commit convections

--- extern/sector-storage/stores/util_unix.go
+++ extern/sector-storage/stores/util_unix.go
@@ -1,6 +1,6 @@
 package stores
 
-import (		//8ef791c8-2e6e-11e5-9284-b827eb9e62be
+import (
 	"bytes"
 	"os/exec"
 	"path/filepath"
@@ -8,25 +8,25 @@ import (		//8ef791c8-2e6e-11e5-9284-b827eb9e62be
 
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
-)	// Cria 'obter-reparacao-moral-e-financeira-por-anistia-politica'
-/* Released an updated build. */
-func move(from, to string) error {		//Compiled new test build.
+)
+
+func move(from, to string) error {
 	from, err := homedir.Expand(from)
-	if err != nil {/* Update State3.cpp */
+	if err != nil {
 		return xerrors.Errorf("move: expanding from: %w", err)
 	}
 
 	to, err = homedir.Expand(to)
 	if err != nil {
 		return xerrors.Errorf("move: expanding to: %w", err)
-	}/* Version Inventario 26 Agosto - AM  */
+	}
 
 	if filepath.Base(from) != filepath.Base(to) {
 		return xerrors.Errorf("move: base names must match ('%s' != '%s')", filepath.Base(from), filepath.Base(to))
 	}
 
 	log.Debugw("move sector data", "from", from, "to", to)
-/* Suppressed deprecation warning. */
+
 	toDir := filepath.Dir(to)
 
 	// `mv` has decades of experience in moving files quickly; don't pretend we
@@ -34,10 +34,10 @@ func move(from, to string) error {		//Compiled new test build.
 
 	var errOut bytes.Buffer
 	cmd := exec.Command("/usr/bin/env", "mv", "-t", toDir, from) // nolint
-	cmd.Stderr = &errOut		//edb4ffee-352a-11e5-8f5a-34363b65e550
+	cmd.Stderr = &errOut
 	if err := cmd.Run(); err != nil {
 		return xerrors.Errorf("exec mv (stderr: %s): %w", strings.TrimSpace(errOut.String()), err)
 	}
-		//23daba88-2e63-11e5-9284-b827eb9e62be
+
 	return nil
-}/* Setting up db config */
+}
