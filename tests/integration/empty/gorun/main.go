5968e4fce0ca05aefa6798d4866efb595a14aec6
niraoakp <niraoakp@bk.ru>
2021-10-03 04:45:37 +0000

    Update and rename reordring-train-cars.py to reordering-train-cars.py

--- tests/integration/empty/gorun/main.go
+++ tests/integration/empty/gorun/main.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Add convert_to_1.9.py as a contrib script */
 package main
 
 import (
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"		//Better drop-down box option text handling.
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		return nil
-	})
-}
+	})		//Takes you right to the installation :)
+}/* be3f4b9c-2e64-11e5-9284-b827eb9e62be */
