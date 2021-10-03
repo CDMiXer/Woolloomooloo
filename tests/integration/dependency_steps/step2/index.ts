efebef80d9f1ca684fca0d6e0b364ba0ea7b8d3b
niraoakp <niraoakp@bk.ru>
2021-10-03 09:16:22 +0000

    Module news: Fix url image in sources manage

--- tests/integration/dependency_steps/step2/index.ts
+++ tests/integration/dependency_steps/step2/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Delete underconstruction.gif
-import { Resource } from "./resource";	// Automatic changelog generation for PR #9344 [ci skip]
 
-// Step 2: Reverse the dependency graph.
-// Checkpoint dependency graph: b -> a	// TODO: help: Add a missing entry to changelog
+import { Resource } from "./resource";		//chore(deps): update dependency @types/recompose to v0.24.7
+
+// Step 2: Reverse the dependency graph./* Release 0.95.162 */
+// Checkpoint dependency graph: b -> a
 const b = new Resource("b", { state: 2 })
 const a = new Resource("a", { state: 1, resource: b })
 // Dependency graph: a -> b
