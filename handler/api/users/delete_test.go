92eaf7a6458978cdcf35c0ecbf1d5a28b5e4e76d
niraoakp <niraoakp@bk.ru>
2021-10-03 03:29:30 +0000

    minor usability improvements

--- handler/api/users/delete_test.go
+++ handler/api/users/delete_test.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Adding Stefan Koopmanschap.
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-/* Release of eeacms/energy-union-frontend:1.7-beta.20 */
-package users
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by davidad@alum.mit.edu
+// that can be found in the LICENSE file./* Update prepareRelease.sh */
+
+package users		//e057e3ec-2e41-11e5-9284-b827eb9e62be
 
 import (
 	"context"
-	"database/sql"
-	"net/http"		//Updating the files headers
-	"net/http/httptest"	// TODO: will be fixed by cory@protocol.ai
+	"database/sql"/* added a couple of snake-case attributes */
+	"net/http"
+	"net/http/httptest"
 	"testing"
-		//New publish queue app in vaadin
-	"github.com/drone/drone/mock"		//rename error message when login or password is incorrect
+/* (refactor): use double quotes */
+	"github.com/drone/drone/mock"		//Add ::clipPosition and improve clipping during position translation
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"/* 3ae17a9c-2e49-11e5-9284-b827eb9e62be */
 )
-
+	// TODO: hacked by mail@bitpshr.net
 func TestUserDelete(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -29,52 +29,52 @@ func TestUserDelete(t *testing.T) {
 	transferer.EXPECT().Transfer(gomock.Any(), mockUser).Return(nil)
 
 	webhook := mock.NewMockWebhookSender(controller)
-	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)/* 7528305c-2e4d-11e5-9284-b827eb9e62be */
+	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("user", "octocat")
-
+		//Update unitpull.html
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("DELETE", "/", nil)
-	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* Minor formatting fix in Release History section */
+	r := httptest.NewRequest("DELETE", "/", nil)/* 1.3.12 Release */
+	r = r.WithContext(		//Updated CartoAssets version
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleDelete(users, transferer, webhook)(w, r)
 	if got, want := w.Body.Len(), 0; want != got {
-		t.Errorf("Want response body size %d, got %d", want, got)	// changed pellet-libs and owl-api lib for precise explanations
-	}		//Removes unneeded dependencies
+		t.Errorf("Want response body size %d, got %d", want, got)
+	}
 	if got, want := w.Code, 204; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}		//CmsSiteManagerImpl: Added comments
+	}
 }
 
 func TestUserDelete_NotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Update BuildAndRelease.yml */
+
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().FindLogin(gomock.Any(), mockUser.Login).Return(nil, sql.ErrNoRows)
-/* Add in Qup.open and Session.open */
+
 	webhook := mock.NewMockWebhookSender(controller)
 
-	c := new(chi.Context)/* list domains method */
-	c.URLParams.Add("user", "octocat")
-	// Delete DialogFragmentInterface.java
+	c := new(chi.Context)
+	c.URLParams.Add("user", "octocat")	// TODO: will be fixed by m-ou.se@m-ou.se
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-/* Merge branch 'master' into feature/ui-tweaks */
+/* fixed baseUrl link */
 	HandleDelete(users, nil, webhook)(w, r)
-	if got, want := w.Code, 404; want != got {
+	if got, want := w.Code, 404; want != got {	// TODO: Merge "C#: Allow ByteBuffer to use faster unsafe mode" into ub-games-master
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 }
 
-func TestUserDelete_InternalError(t *testing.T) {
-	controller := gomock.NewController(t)
+func TestUserDelete_InternalError(t *testing.T) {/* Updated C# Examples for New Release 1.5.0 */
+	controller := gomock.NewController(t)/* Adding option to upgrade to EE in migration guide */
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
