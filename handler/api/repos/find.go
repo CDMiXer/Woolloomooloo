8bdb81a1ef74b85021a95ebe39c5bd3939aa61b3
niraoakp <niraoakp@bk.ru>
2021-10-03 07:47:03 +0000

    Delete image_29.png

--- handler/api/repos/find.go
+++ handler/api/repos/find.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Updated microG Service Core to 0.2.4-105, fix: #1
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Merge "Fix bug in docker-toool where values are sometimes empty."
+//
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -25,7 +25,7 @@ import (
 // json-encoded repository details to the response body.
 func HandleFind() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()	// TODO: rev 848863
+		ctx := r.Context()
 		repo, _ := request.RepoFrom(ctx)
 		perm, _ := request.PermFrom(ctx)
 		repo.Perms = perm
