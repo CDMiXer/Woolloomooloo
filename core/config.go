46bbbad8d44fc6ad0240e8db07e5c2bae38650a7
niraoakp <niraoakp@bk.ru>
2021-10-03 23:36:54 +0000

    Update Mesos minor versions: 0.24.2, 0.25.1, 0.26.1. (#12)

--- core/config.go
+++ core/config.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Merge "Fix a FormatFlagsConversionMismatchException bug I introduced in Froyo." */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* back to the old URLs because of saving namespace */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -25,12 +25,12 @@ type (
 
 	// ConfigArgs represents a request for the pipeline
 	// configuration file (e.g. .drone.yml)
-	ConfigArgs struct {	// TODO: hacked by martin2cai@hotmail.com
+	ConfigArgs struct {
 		User   *User       `json:"-"`
 		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
 		Config *Config     `json:"config,omitempty"`
-	}		//New post: Voordelige Tegels en Goedkope Keukens
+	}
 
 	// ConfigService provides pipeline configuration from an
 	// external service.
