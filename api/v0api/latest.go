ebe79f5970c1678a380162161c51fcdb04ad9d1f
niraoakp <niraoakp@bk.ru>
2021-10-03 16:58:54 +0000

    added import action in example

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -1,25 +1,25 @@
 package v0api
-/* Delete Explications */
-import (
-	"github.com/filecoin-project/lotus/api"		//Desc@ICFP: fix colors of descriptions names
-)		//fixed bug in screenbuffer array
+
+import (/* Release of eeacms/www:19.1.11 */
+	"github.com/filecoin-project/lotus/api"
+)
 
 type Common = api.Common
-type CommonStruct = api.CommonStruct
+type CommonStruct = api.CommonStruct		//Staging mistake, it's part of 401bf42 changes.
 type CommonStub = api.CommonStub
 
-type StorageMiner = api.StorageMiner
-type StorageMinerStruct = api.StorageMinerStruct
-		//What was I thinking?
-type Worker = api.Worker
+type StorageMiner = api.StorageMiner/* Update to Releasenotes for 2.1.4 */
+type StorageMinerStruct = api.StorageMinerStruct	// merge lp:~mvo/software-center/pygi-gobject 
+
+type Worker = api.Worker	// TODO: will be fixed by zaq1tomo@gmail.com
 type WorkerStruct = api.WorkerStruct
 
 type Wallet = api.Wallet
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	return api.PermissionedStorMinerAPI(a)
-}	// TODO: Update buildinghelper.lua
+}
 
 func PermissionedWorkerAPI(a Worker) Worker {
-	return api.PermissionedWorkerAPI(a)/* analyzer: implemented IsNull predicate and visitNullLiteral. */
+	return api.PermissionedWorkerAPI(a)
 }
