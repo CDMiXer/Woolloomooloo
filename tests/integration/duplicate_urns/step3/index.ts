59d7259bb498ceefb7f3fba565145f990f0fd102
niraoakp <niraoakp@bk.ru>
2021-10-03 02:06:34 +0000

    Use same RNG seed on every run.

--- tests/integration/duplicate_urns/step3/index.ts
+++ tests/integration/duplicate_urns/step3/index.ts
@@ -1,23 +1,23 @@
-// Copyright 2016-2018, Pulumi Corporation./* Update tensorboard dependency to 1.9.x */
+// Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* fix prepareRelease.py */
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* alternation of composer.json */
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* #3 - Release version 1.0.1.RELEASE. */
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and	// TODO: Set version to 3.8.11-RC for release on BukkitDev.
 // limitations under the License.
 
 import { Resource } from "./resource";
 
 // "a" is already in the snapshot and will be "same"d.
 const a = new Resource("a", { state: 4 });
-
+/* Bump VERSION to 0.7.dev0 after 0.6.0 Release */
 // "b" is not, but they have the same URN.
-const b = new Resource("a", { state: 5 });/* Updated New Release Checklist (markdown) */
+const b = new Resource("a", { state: 5 });
 
 // This should fail, but gracefully.
