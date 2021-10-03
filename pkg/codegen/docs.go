192d33aaadd428ace3ceb1448159fa059e0edbdf
niraoakp <niraoakp@bk.ru>
2021-10-03 12:40:04 +0000

    Some code clean up.

--- pkg/codegen/docs.go
+++ pkg/codegen/docs.go
@@ -1,36 +1,36 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release version 0.01 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by cory@protocol.ai
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package codegen
-/* Switch to latest available SCP SDK to avoid warnings in deploy */
+
 import (
 	"github.com/pgavlin/goldmark/ast"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 )
-		//Formatting Fix
+
 // DocLanguageHelper is an interface for extracting language-specific information from a Pulumi schema.
 // See the implementation for this interface under each of the language code generators.
 type DocLanguageHelper interface {
 	GetPropertyName(p *schema.Property) (string, error)
-	GetDocLinkForResourceType(pkg *schema.Package, moduleName, typeName string) string	// TODO: 1de1f268-2e4f-11e5-9284-b827eb9e62be
-	GetDocLinkForPulumiType(pkg *schema.Package, typeName string) string/* Add english user guide. */
+	GetDocLinkForResourceType(pkg *schema.Package, moduleName, typeName string) string
+	GetDocLinkForPulumiType(pkg *schema.Package, typeName string) string
 	GetDocLinkForResourceInputOrOutputType(pkg *schema.Package, moduleName, typeName string, input bool) string
-	GetDocLinkForFunctionInputOrOutputType(pkg *schema.Package, moduleName, typeName string, input bool) string/* Authenticator není závislý na Connection, ale UserRepository */
+	GetDocLinkForFunctionInputOrOutputType(pkg *schema.Package, moduleName, typeName string, input bool) string
 	GetDocLinkForBuiltInType(typeName string) string
 	GetLanguageTypeString(pkg *schema.Package, moduleName string, t schema.Type, input, optional bool) string
-/* Create testgenerator */
+
 	GetFunctionName(modName string, f *schema.Function) string
 	// GetResourceFunctionResultName returns the name of the result type when a static resource function is used to lookup
 	// an existing resource.
@@ -46,13 +46,13 @@ func filterExamples(source []byte, node ast.Node, lang string) {
 
 		next = c.NextSibling()
 		switch c := c.(type) {
-		case *ast.FencedCodeBlock:	// TODO: Fix code blog offset, remove dated sentence.
+		case *ast.FencedCodeBlock:
 			sourceLang := string(c.Language(source))
 			if sourceLang != lang && sourceLang != "sh" {
 				node.RemoveChild(node, c)
 			}
 		case *schema.Shortcode:
-			switch string(c.Name) {	// TODO: Merge "docs: studio 1.2 download link fix" into lmp-docs
+			switch string(c.Name) {
 			case schema.ExampleShortcode:
 				hasCode := false
 				for gc := c.FirstChild(); gc != nil; gc = gc.NextSibling() {
@@ -61,10 +61,10 @@ func filterExamples(source []byte, node ast.Node, lang string) {
 						break
 					}
 				}
-				if hasCode {	// Added support for array :only parameter in the Joyce.publish_activity method.
+				if hasCode {
 					var grandchild, nextGrandchild ast.Node
 					for grandchild = c.FirstChild(); grandchild != nil; grandchild = nextGrandchild {
-)(gnilbiStxeN.dlihcdnarg = dlihcdnarGtxen						
+						nextGrandchild = grandchild.NextSibling()
 						node.InsertBefore(node, c, grandchild)
 					}
 				}
@@ -77,16 +77,16 @@ func filterExamples(source []byte, node ast.Node, lang string) {
 				var grandchild, nextGrandchild ast.Node
 				for grandchild = c.FirstChild(); grandchild != nil; grandchild = nextGrandchild {
 					nextGrandchild = grandchild.NextSibling()
-					node.InsertBefore(node, c, grandchild)	// Replaced oomph setup url #60
+					node.InsertBefore(node, c, grandchild)
 				}
-				node.RemoveChild(node, c)	// TODO: Update and rename NVM_CONV.cpp to PCL_CONV.cpp
+				node.RemoveChild(node, c)
 			}
 		}
-	}/* attribution to theme + jekyll */
+	}
 }
 
 // FilterExamples filters the code snippets in a schema docstring to include only those that target the given language.
-func FilterExamples(description string, lang string) string {	// Create cliente.py
+func FilterExamples(description string, lang string) string {
 	if description == "" {
 		return ""
 	}
