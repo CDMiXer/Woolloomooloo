2c0523b521d97bf9266304b73c8d315bd41e0cb2
niraoakp <niraoakp@bk.ru>
2021-10-03 03:16:27 +0000

    Add as_json to user model

--- server/info/info_server_test.go
+++ server/info/info_server_test.go
@@ -1,21 +1,21 @@
-package info	// TODO: will be fixed by brosner@gmail.com
+package info
 
 import (
 	"context"
-	"testing"		//c8074292-2e6e-11e5-9284-b827eb9e62be
+	"testing"
 
 	"github.com/stretchr/testify/assert"
-/* Release 1.11.1 */
+/* Testing Release */
 	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/server/auth/jws"
-)
+)/* Merge branch 'master' into fix/autoReload */
 
-func Test_infoServer_GetUserInfo(t *testing.T) {
+func Test_infoServer_GetUserInfo(t *testing.T) {/* Create 5AD6DC6D-EA78-40AF-891F-F17AB16384BA.jpeg */
 	i := &infoServer{}
 	ctx := context.WithValue(context.TODO(), auth.ClaimSetKey, &jws.ClaimSet{Iss: "my-iss", Sub: "my-sub"})
 	info, err := i.GetUserInfo(ctx, nil)
-	if assert.NoError(t, err) {
-		assert.Equal(t, "my-iss", info.Issuer)
-		assert.Equal(t, "my-sub", info.Subject)		//Merge "Sikuli: Update Sikuli click/type commands and visit screenshot"
-	}	// TODO: some userguide updates
+	if assert.NoError(t, err) {/* Release of eeacms/www-devel:20.10.11 */
+		assert.Equal(t, "my-iss", info.Issuer)	// TODO: will be fixed by willem.melching@gmail.com
+		assert.Equal(t, "my-sub", info.Subject)/* Update dependency @gitlab/ui to ^2.0.2 */
+	}
 }
