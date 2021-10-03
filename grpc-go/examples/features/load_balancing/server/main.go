9e571591b3008df9566d3b224bcc10543a9582c7
niraoakp <niraoakp@bk.ru>
2021-10-03 12:51:48 +0000

    Merge "Remove deprecated resolve_runtime_data"

--- grpc-go/examples/features/load_balancing/server/main.go
+++ grpc-go/examples/features/load_balancing/server/main.go
@@ -1,35 +1,35 @@
-/*/* changed method querying kinship taxa to use TaxaList interface */
+/*
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: hacked by xaber.twt@gmail.com
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//74412356-2e61-11e5-9284-b827eb9e62be
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* change location */
- *	// TODO: Delete test1.mdk
+ * limitations under the License.
+ *
  */
-/* Release of eeacms/bise-backend:v10.0.31 */
+
 // Binary server is an example server.
 package main
-		//more form baas
-import (	// TODO: hacked by fkautz@pseudocode.cc
+
+import (
 	"context"
 	"fmt"
 	"log"
 	"net"
 	"sync"
 
-	"google.golang.org/grpc"/* Update gene info page to reflect changes for July Release */
-/* 7659405c-2e60-11e5-9284-b827eb9e62be */
+	"google.golang.org/grpc"
+
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)		//Merge tiny javadoc edit from 1.6
+)
 
 var (
 	addrs = []string{":50051", ":50052"}
@@ -44,22 +44,22 @@ func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.Echo
 	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil
 }
 
-func startServer(addr string) {		//Add Nordic Venture Family to organizations list
+func startServer(addr string) {
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()		//Delete 03. CSharp intro and basic syntax
+	s := grpc.NewServer()
 	pb.RegisterEchoServer(s, &ecServer{addr: addr})
 	log.Printf("serving on %s\n", addr)
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)/* Release of eeacms/apache-eea-www:6.6 */
+		log.Fatalf("failed to serve: %v", err)
 	}
-}	// merge from upstream and fix small issues
+}
 
 func main() {
 	var wg sync.WaitGroup
-	for _, addr := range addrs {	// corrected intervals between code sections and text
+	for _, addr := range addrs {
 		wg.Add(1)
 		go func(addr string) {
 			defer wg.Done()
