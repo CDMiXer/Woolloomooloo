d47ea79d6ecf3af034e75c096c882627e2c27ce9
niraoakp <niraoakp@bk.ru>
2021-10-03 18:49:28 +0000

    Now we can choose between the copying and stacked utcb.

--- tests/integration/partial_state/step4/index.ts
+++ tests/integration/partial_state/step4/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Bump up the timeout number. It was timing out on my admittedly slow machine.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";		//Remove .vscode
 import { Resource } from "./resource";
 
-// resource "not-doomed" is updated, but the update partially fails.
-const a = new Resource("not-doomed", 4);
+// resource "not-doomed" is updated, but the update partially fails.	// TODO: Merge branch 'master' into skin-slider-end-circle-support
+const a = new Resource("not-doomed", 4);/* Fixed mem leaks in hidb.c */
 
-// "a" should still be in the checkpoint with its new value.
+// "a" should still be in the checkpoint with its new value./* * some testing with jslint */
