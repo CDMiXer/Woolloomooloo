e884ae8ace449529ba7e157d20021f90671eebd1
niraoakp <niraoakp@bk.ru>
2021-10-04 14:45:21 +0000

    Criando a classe Trapezoide

--- tests/integration/construct_component/dotnet/MyStack.cs
+++ tests/integration/construct_component/dotnet/MyStack.cs
@@ -1,13 +1,13 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-using Pulumi;	// TODO: protocol 220
+using Pulumi;
 
 class MyStack : Stack
-{		//trying yet another tracking code
+{
     public MyStack()
-    {
+    {		//Support generating input element shortcut methods
         var componentA = new Component("a", new ComponentArgs { Echo = 42 });
         var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });
-        var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });		//AI-2.1.3 <ntdan@ngotuongdan Merge branch 'master'
-    }
+        var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });
+    }/* More sensible test of the calculateLatestReleaseVersion() method. */
 }
