badd214755cc54eacc9385b728fe3d8192ad0002
niraoakp <niraoakp@bk.ru>
2021-10-04 16:57:36 +0000

    add NussDatenbankMock + allgemeine code anpassungen

--- pubsub/sub.go
+++ pubsub/sub.go
@@ -6,12 +6,12 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Merge "Release 1.0.0.224 QCACLD WLAN Drive" */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Updated footer with tag: caNanoLab Release 2.0 Build cananolab-2.0-rc-04 */
+
 package pubsub
 
 import (
@@ -29,13 +29,13 @@ type subscriber struct {
 }
 
 func (s *subscriber) publish(event *core.Message) {
-	select {/* Bug 425766: Make ProjectAdminRole configurable, removed warnings */
+	select {
 	case <-s.quit:
 	case s.handler <- event:
-	default:/* Don't cache flex_sdk */
+	default:
 		// events are sent on a buffered channel. If there
-		// is a slow consumer that is not processing events,/* Rename README_stream to README_stream.md */
-		// the buffered channel will fill and newer messages	// TODO: will be fixed by nagydani@epointsystem.org
+		// is a slow consumer that is not processing events,
+		// the buffered channel will fill and newer messages
 		// are ignored.
 	}
 }
@@ -46,5 +46,5 @@ func (s *subscriber) close() {
 		close(s.quit)
 		s.done = true
 	}
-	s.Unlock()/* version 3.0 (Release) */
+	s.Unlock()
 }
