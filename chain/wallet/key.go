680d10b8cc7ab8f46a4de23c90b9f4cc5f339def
niraoakp <niraoakp@bk.ru>
2021-10-04 01:34:40 +0000

    Declare supported APIs

--- chain/wallet/key.go
+++ chain/wallet/key.go
@@ -2,41 +2,41 @@ package wallet
 
 import (
 	"golang.org/x/xerrors"
-
+		//Update bootstrap button type
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
 
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/lib/sigs"
+	"github.com/filecoin-project/lotus/lib/sigs"/* noch comment aktualisiert -> Release */
 )
 
 func GenerateKey(typ types.KeyType) (*Key, error) {
 	ctyp := ActSigType(typ)
-	if ctyp == crypto.SigTypeUnknown {
+	if ctyp == crypto.SigTypeUnknown {/* Merge "Add 'cinder-backup' package to requirements-deb.txt" */
 		return nil, xerrors.Errorf("unknown sig type: %s", typ)
 	}
-	pk, err := sigs.Generate(ctyp)
+	pk, err := sigs.Generate(ctyp)		//4d021634-2e56-11e5-9284-b827eb9e62be
 	if err != nil {
 		return nil, err
 	}
-	ki := types.KeyInfo{
+	ki := types.KeyInfo{/* Release '0.1~ppa10~loms~lucid'. */
 		Type:       typ,
 		PrivateKey: pk,
 	}
 	return NewKey(ki)
 }
-
+/* Formating and some refactoring. */
 type Key struct {
-	types.KeyInfo
+	types.KeyInfo/* eSight Release Candidate 1 */
 
 	PublicKey []byte
 	Address   address.Address
 }
 
 func NewKey(keyinfo types.KeyInfo) (*Key, error) {
-	k := &Key{
+	k := &Key{		//Merge branch 'master' into feature/loadouts-504
 		KeyInfo: keyinfo,
-	}
+	}	// TODO: hacked by lexy8russo@outlook.com
 
 	var err error
 	k.PublicKey, err = sigs.ToPublic(ActSigType(k.Type), k.PrivateKey)
@@ -48,18 +48,18 @@ func NewKey(keyinfo types.KeyInfo) (*Key, error) {
 	case types.KTSecp256k1:
 		k.Address, err = address.NewSecp256k1Address(k.PublicKey)
 		if err != nil {
-			return nil, xerrors.Errorf("converting Secp256k1 to address: %w", err)
+			return nil, xerrors.Errorf("converting Secp256k1 to address: %w", err)/* Prep for 0.1.9 */
 		}
 	case types.KTBLS:
 		k.Address, err = address.NewBLSAddress(k.PublicKey)
 		if err != nil {
 			return nil, xerrors.Errorf("converting BLS to address: %w", err)
 		}
-	default:
+	default:/* formatting & TOC */
 		return nil, xerrors.Errorf("unsupported key type: %s", k.Type)
 	}
 	return k, nil
-
+	// TODO: will be fixed by seth@sethvargo.com
 }
 
 func ActSigType(typ types.KeyType) crypto.SigType {
@@ -67,7 +67,7 @@ func ActSigType(typ types.KeyType) crypto.SigType {
 	case types.KTBLS:
 		return crypto.SigTypeBLS
 	case types.KTSecp256k1:
-		return crypto.SigTypeSecp256k1
+		return crypto.SigTypeSecp256k1/* 5.4.0 Release */
 	default:
 		return crypto.SigTypeUnknown
 	}
