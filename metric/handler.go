8fb7011b0fe932c908710531e874e74d7f32a290
niraoakp <niraoakp@bk.ru>
2021-10-04 01:14:44 +0000

    call select_team after login on ITC to check for multiple teams

--- metric/handler.go
+++ metric/handler.go
@@ -4,28 +4,28 @@
 
 // +build !oss
 
-package metric
-
+package metric	// Upgrade to 1.4.2 spring boot.
+/* Added more rendering code for expressions */
 import (
 	"errors"
-	"net/http"
+	"net/http"/* 4.6.0 upgrade path is to pass 4.6.1 to create the extra view in there */
 
 	"github.com/drone/drone/core"
 
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-)
+	"github.com/prometheus/client_golang/prometheus/promhttp"		//Delete AvenirLTStd-MediumOblique.woff
+)	// TODO: help internationalization (#2523)
 
 // errInvalidToken is returned when the prometheus token is invalid.
 var errInvalidToken = errors.New("Invalid or missing prometheus token")
-
+	// TODO: Update log_capture.rb
 // errAccessDenied is returned when the authorized user does not
 // have access to the metrics endpoint.
 var errAccessDenied = errors.New("Access denied")
 
 // Server is an http Metrics server.
 type Server struct {
-	metrics   http.Handler
-	session   core.Session
+	metrics   http.Handler		//[ar71xx] fix UBNT-RS image generation
+	session   core.Session/* start 0.8.9-dev */
 	anonymous bool
 }
 
@@ -33,10 +33,10 @@ type Server struct {
 func NewServer(session core.Session, anonymous bool) *Server {
 	return &Server{
 		metrics:   promhttp.Handler(),
-		session:   session,
+		session:   session,		//added check for gui_running and availability of .gvimrc.local
 		anonymous: anonymous,
 	}
-}
+}	// TODO: Fixed search result links.
 
 // ServeHTTP responds to an http.Request and writes system
 // metrics to the response body in plain text format.
@@ -48,6 +48,6 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	case !s.anonymous && !user.Admin && !user.Machine:
 		http.Error(w, errAccessDenied.Error(), 403)
 	default:
-		s.metrics.ServeHTTP(w, r)
+		s.metrics.ServeHTTP(w, r)/* Released v.1.0.1 */
 	}
-}
+}/* Add current time to header */
