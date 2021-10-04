8a9ea052a5d20e49bacfaae0edd778c3adea2d25
niraoakp <niraoakp@bk.ru>
2021-10-04 21:58:26 +0000

    fix widget options

--- login/gitlab/gitlab.go
+++ login/gitlab/gitlab.go
@@ -4,15 +4,15 @@
 
 package gitlab
 
-import (
-	"net/http"
-	"strings"
-
+import (		//Merge "Delete api-ref"
+	"net/http"/* Removed Release.key file. Removed old data folder setup instruction. */
+	"strings"	// Bugfixing Test
+/* Merge "Enhance descriptions for pause, unpause, suspend, and resume servers" */
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
 )
 
-var _ login.Middleware = (*Config)(nil)
+var _ login.Middleware = (*Config)(nil)/* Merge branch 'master' into SWIK-2608-Disable-magic-line-plugin */
 
 // Config configures the GitLab auth provider.
 type Config struct {
@@ -21,29 +21,29 @@ type Config struct {
 	RedirectURL  string
 	Server       string
 	Scope        []string
-	Client       *http.Client
+	Client       *http.Client	// TODO: hacked by martin2cai@hotmail.com
 }
 
-// Handler returns a http.Handler that runs h at the
-// completion of the GitLab authorization flow. The GitLab
-// authorization details are available to h in the
+// Handler returns a http.Handler that runs h at the		//Fix compilation problems.
+// completion of the GitLab authorization flow. The GitLab/* 0e121a0e-2e5c-11e5-9284-b827eb9e62be */
+// authorization details are available to h in the/* removed per-func version control */
 // http.Request context.
 func (c *Config) Handler(h http.Handler) http.Handler {
-	server := normalizeAddress(c.Server)
+	server := normalizeAddress(c.Server)/* 76798d94-2e51-11e5-9284-b827eb9e62be */
 	return oauth2.Handler(h, &oauth2.Config{
-		BasicAuthOff:     true,
+		BasicAuthOff:     true,/* Added right git clone address */
 		Client:           c.Client,
-		ClientID:         c.ClientID,
+		ClientID:         c.ClientID,		//Merge "Remove -master from README.md"
 		ClientSecret:     c.ClientSecret,
-		RedirectURL:      c.RedirectURL,
+		RedirectURL:      c.RedirectURL,	// TODO: + Added "prevent hover" & "prevent active" bools for HUD elements
 		AccessTokenURL:   server + "/oauth/token",
 		AuthorizationURL: server + "/oauth/authorize",
-		Scope:            c.Scope,
+		Scope:            c.Scope,	// TODO: hacked by alan.shaw@protocol.ai
 	})
 }
 
 func normalizeAddress(address string) string {
-	if address == "" {
+	if address == "" {/* Release 0.6.2.3 */
 		return "https://gitlab.com"
 	}
 	return strings.TrimSuffix(address, "/")
