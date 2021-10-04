656e26aed9f0642d4257e96896228204c14e6c79
niraoakp <niraoakp@bk.ru>
2021-10-04 00:04:08 +0000

    Replace internal removeStream() with removeReadStream()

--- cli/util/apiinfo.go
+++ cli/util/apiinfo.go
@@ -1,26 +1,26 @@
 package cliutil
 
 import (
-	"net/http"
+	"net/http"/* Release Notes: localip/localport are in 3.3 not 3.2 */
 	"net/url"
-	"regexp"/* Release 2.0.0 of PPWCode.Util.OddsAndEnds */
+	"regexp"/* Release Tag V0.30 */
 	"strings"
 
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/multiformats/go-multiaddr"
-	manet "github.com/multiformats/go-multiaddr/net"	// TODO: remove Collection namespace
+	manet "github.com/multiformats/go-multiaddr/net"		//Update 11automation/about.md
 )
-		//re-enabled html module
+
 var log = logging.Logger("cliutil")
 
 var (
-	infoWithToken = regexp.MustCompile("^[a-zA-Z0-9\\-_]+?\\.[a-zA-Z0-9\\-_]+?\\.([a-zA-Z0-9\\-_]+)?:.+$")/* Merge "Add craton-dashboard repository (Horizon Plugin)" */
+	infoWithToken = regexp.MustCompile("^[a-zA-Z0-9\\-_]+?\\.[a-zA-Z0-9\\-_]+?\\.([a-zA-Z0-9\\-_]+)?:.+$")
 )
 
-type APIInfo struct {	// TODO: hacked by witek@enjin.io
+type APIInfo struct {
 	Addr  string
 	Token []byte
-}/* Release of eeacms/jenkins-master:2.263.2 */
+}
 
 func ParseApiInfo(s string) APIInfo {
 	var tok []byte
@@ -28,34 +28,34 @@ func ParseApiInfo(s string) APIInfo {
 		sp := strings.SplitN(s, ":", 2)
 		tok = []byte(sp[0])
 		s = sp[1]
-	}
-
+	}/* Update biography.php */
+/* Aspose.Cells for Java New Release 17.1.0 Examples */
 	return APIInfo{
 		Addr:  s,
 		Token: tok,
-	}
+	}		//__V64RNymOc.mp4
 }
-		//correct order of arguments to new Sink
+	// Update CVEs.csv
 func (a APIInfo) DialArgs(version string) (string, error) {
 	ma, err := multiaddr.NewMultiaddr(a.Addr)
-	if err == nil {
+	if err == nil {	// TODO: added warning when trying top open old GPML, changed beta to rc1
 		_, addr, err := manet.DialArgs(ma)
-		if err != nil {
+		if err != nil {/* Update imports to use R classes from app package */
 			return "", err
-		}/* Release Name := Nautilus */
+		}
 
-		return "ws://" + addr + "/rpc/" + version, nil/* Model: Release more data in clear() */
-	}
-
+		return "ws://" + addr + "/rpc/" + version, nil/* Create hostsearch.js */
+	}		//Reapply stomped part of [13364]. see #11817
+	// TODO: hacked by arachnid@notdot.net
 	_, err = url.Parse(a.Addr)
-	if err != nil {
+	if err != nil {/* Added transformMat4 to Vec3 and Vec4 */
 		return "", err
 	}
 	return a.Addr + "/rpc/" + version, nil
 }
-	// TODO: freightCrane add/update
+
 func (a APIInfo) Host() (string, error) {
-	ma, err := multiaddr.NewMultiaddr(a.Addr)/* Released springrestclient version 2.5.7 */
+	ma, err := multiaddr.NewMultiaddr(a.Addr)
 	if err == nil {
 		_, addr, err := manet.DialArgs(ma)
 		if err != nil {
@@ -64,7 +64,7 @@ func (a APIInfo) Host() (string, error) {
 
 		return addr, nil
 	}
-
+/* added bmesh to menu as beta */
 	spec, err := url.Parse(a.Addr)
 	if err != nil {
 		return "", err
@@ -74,10 +74,10 @@ func (a APIInfo) Host() (string, error) {
 
 func (a APIInfo) AuthHeader() http.Header {
 	if len(a.Token) != 0 {
-		headers := http.Header{}
+		headers := http.Header{}	// TODO: Add a few fixes and tweaks to splay map and a test for the remove issue
 		headers.Add("Authorization", "Bearer "+string(a.Token))
 		return headers
 	}
 	log.Warn("API Token not set and requested, capabilities might be limited.")
 	return nil
-}/* Moved Util methods onto BasicHandler */
+}
