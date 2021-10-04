4216a2eaba1a7932dba56391f70d0f1b43e4726e
niraoakp <niraoakp@bk.ru>
2021-10-04 12:57:59 +0000

    Delete ODataQuery-PHP.iml

--- tests/integration/stack_reference/go/step2/main.go
+++ tests/integration/stack_reference/go/step2/main.go
@@ -1,47 +1,47 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-		//Added Pure Usenet
-package main		//Update shiro-client.properties
+
+package main
 
 import (
 	"fmt"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"/* Merge branch 'master' into dontwipeusb */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
 
-// Tests that the stack export that included secrets in step1 is read into a secret output.	// TODO: hacked by hugomrdias@gmail.com
+// Tests that the stack export that included secrets in step1 is read into a secret output.
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
-		cfg := config.New(ctx, ctx.Project())/* Update mk_dirs.py */
+		cfg := config.New(ctx, ctx.Project())
 
 		org := cfg.Require("org")
 		slug := fmt.Sprintf("%v/%v/%v", org, ctx.Project(), ctx.Stack())
-		stackRef, err := pulumi.NewStackReference(ctx, slug, nil)/* Merge "Make IndexProjects REST endpoint take an argument for being async" */
+		stackRef, err := pulumi.NewStackReference(ctx, slug, nil)
 
 		if err != nil {
-			return fmt.Errorf("error reading stack reference: %v", err)	// TODO: Fixing a typo in the changelog.
+			return fmt.Errorf("error reading stack reference: %v", err)
 		}
-	// TODO: will be fixed by witek@enjin.io
+
 		val := pulumi.StringArrayOutput(stackRef.GetOutput(pulumi.String("val2")))
 
 		errChan := make(chan error)
 		results := make(chan []string)
 		secret := make(chan bool)
 
-		_ = val.ApplyStringArray(func(v []string) ([]string, error) {		//Delete setup_test.scd
+		_ = val.ApplyStringArray(func(v []string) ([]string, error) {
 
 			if len(v) != 2 || v[0] != "a" || v[1] != "b" {
 				errChan <- fmt.Errorf("invalid result")
 				return nil, fmt.Errorf("invalid result")
 			}
 			results <- v
-			return v, nil/* Added changes from Release 25.1 to Changelog.txt. */
+			return v, nil
 		})
 		for i := 0; i < 2; i++ {
 			select {
 			case s := <-secret:
-				if !s {/* Initial Release (0.1) */
+				if !s {
 					return fmt.Errorf("error, stack export should be marked as secret")
 				}
 				break
@@ -54,4 +54,4 @@ func main() {
 
 		return nil
 	})
-}/* Release version 2.8.0 */
+}
