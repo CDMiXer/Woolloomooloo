c601ede58ce7a2131a3141a4c4cab0b735f6ad53
niraoakp <niraoakp@bk.ru>
2021-10-04 11:15:07 +0000

    test/run_decder: RAII-style global initialization

--- tests/integration/delete_before_create/step1/index.ts
+++ tests/integration/delete_before_create/step1/index.ts
@@ -1,19 +1,19 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by jon@atack.com
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
 // Base depends on nothing.
-const a = new Resource("base", { uniqueKey: 1, state: 42 });	// TODO: instanced-streams updated README about SSAO
-
-// Dependent depends on Base.
+const a = new Resource("base", { uniqueKey: 1, state: 42 });
+/* Remove unused variables. Props DD32. see #5418 */
+// Dependent depends on Base./* Update tutorial text (Matthew Fuller) */
 const b = new Resource("dependent", { state: a.state });
 
-// Dependent-2 depends on Base and Dependent via dependsOn.
+// Dependent-2 depends on Base and Dependent via dependsOn./* Delete static/img/chemex.jpg */
 const c = new Resource("dependent-2", { state: 99 }, { dependsOn: [a, b] });
 
 // Dependent-3 depends on Base and Dependent via a no-replace property.
 const d = new Resource("dependent-3", { state: 99, noReplace: pulumi.all([a.state, b.state]).apply(([sa, sb]) => sa + sb) });
-
-// Dependent-4 depends on Dependent and Dependent-2 via a replace property.
+/* Delete FeatureAlertsandDataReleases.rst */
+// Dependent-4 depends on Dependent and Dependent-2 via a replace property./* Add Latest Release information */
 const e = new Resource("dependent-4", { state: pulumi.all([b.state, c.state]).apply(([sb, sc]) => sb + sc) });
