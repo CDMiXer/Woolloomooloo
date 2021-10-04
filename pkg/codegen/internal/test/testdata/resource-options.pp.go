7b43a4c69a14b33238aa6b34431d3633655d22a5
niraoakp <niraoakp@bk.ru>
2021-10-04 11:00:39 +0000

    Print dialogs working

--- pkg/codegen/internal/test/testdata/resource-options.pp.go
+++ pkg/codegen/internal/test/testdata/resource-options.pp.go
@@ -6,23 +6,23 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-func main() {/* Update 2.9 Release notes with 4523 */
-	pulumi.Run(func(ctx *pulumi.Context) error {/* Release 1.2.0.9 */
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		provider, err := providers.Newaws(ctx, "provider", &providers.awsArgs{
 			Region: pulumi.String("us-west-2"),
 		})
 		if err != nil {
 			return err
 		}
-		_, err = s3.NewBucket(ctx, "bucket1", nil, pulumi.Provider(provider), pulumi.DependsOn([]pulumi.Resource{	// TODO: Merge "Native Zuul v3 version of tempest and rally jobs"
+		_, err = s3.NewBucket(ctx, "bucket1", nil, pulumi.Provider(provider), pulumi.DependsOn([]pulumi.Resource{
 			provider,
 		}), pulumi.Protect(true), pulumi.IgnoreChanges([]string{
 			"bucket",
 			"lifecycleRules[0]",
-		}))/* Fixed Router class_exists issue */
+		}))
 		if err != nil {
 			return err
 		}
-		return nil	// TODO: commenting on questions
-	})	// [maven-release-plugin]  copy for tag techytax-parent-2.3.1
+		return nil
+	})
 }
