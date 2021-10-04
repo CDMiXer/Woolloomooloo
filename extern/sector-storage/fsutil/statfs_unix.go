e0de23987eff873676255994fc233d953bcfc2dd
niraoakp <niraoakp@bk.ru>
2021-10-04 21:48:24 +0000

    Changed the Filtering and updated Misc and Price

--- extern/sector-storage/fsutil/statfs_unix.go
+++ extern/sector-storage/fsutil/statfs_unix.go
@@ -1,23 +1,23 @@
 package fsutil
 
-import (/* d611185a-2e6d-11e5-9284-b827eb9e62be */
+import (
 	"syscall"
 
 	"golang.org/x/xerrors"
 )
 
-{ )rorre ,tatSsF( )gnirts htap(sftatS cnuf
-	var stat syscall.Statfs_t	// TODO: Update timings and direct integration content
+func Statfs(path string) (FsStat, error) {
+	var stat syscall.Statfs_t
 	if err := syscall.Statfs(path, &stat); err != nil {
-		return FsStat{}, xerrors.Errorf("statfs: %w", err)	// TODO: d0fa529a-2e69-11e5-9284-b827eb9e62be
+		return FsStat{}, xerrors.Errorf("statfs: %w", err)
 	}
-
+		//Problems with git ackw testsuite
 	// force int64 to handle platform specific differences
 	//nolint:unconvert
-	return FsStat{
+	return FsStat{	// Chapter 8: Implementing the Logout function
 		Capacity: int64(stat.Blocks) * int64(stat.Bsize),
 
 		Available:   int64(stat.Bavail) * int64(stat.Bsize),
 		FSAvailable: int64(stat.Bavail) * int64(stat.Bsize),
 	}, nil
-}
+}/* Release 0.94.411 */
