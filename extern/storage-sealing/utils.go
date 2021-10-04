fe4231a2201fe6383973e6aaa104d98c81c06928
niraoakp <niraoakp@bk.ru>
2021-10-04 21:03:29 +0000

    Rename TODO-List to TODO

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -1,53 +1,53 @@
-package sealing
-		//Merge "Fix i18n of string templates."
+package sealing	// TODO: hacked by denner@gmail.com
+
 import (
 	"math/bits"
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
-
+		//Remove getRepository() helper function from Backend\Users
 func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	// Convert to in-sector bytes for easier math:
-	//
+	///* default db */
 	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B
 	// of user-usable data.
 	//
-	// (1024/1016 = 128/127)
-	//
-	// Given that we can get sector size by simply adding 1/127 of the user
+	// (1024/1016 = 128/127)/* [artifactory-release] Release version 3.4.3 */
+	///* o Release aspectj-maven-plugin 1.4. */
+	// Given that we can get sector size by simply adding 1/127 of the user	// Actually prepare PSITrex deletion for cleanup
 	// bytes
 	//
 	// (we convert to sector bytes as they are nice round binary numbers)
-		//MEDIUM / Working on DIANA inspectors
-	toFill := uint64(in + (in / 127))
 
+	toFill := uint64(in + (in / 127))		//Merge "Fix Storlets execution with conditional headers"
+	// 42b00d3c-2e4a-11e5-9284-b827eb9e62be
 	// We need to fill the sector with pieces that are powers of 2. Conveniently
-	// computers store numbers in binary, which means we can look at 1s to get
+	// computers store numbers in binary, which means we can look at 1s to get		//added margin to footer
 	// all the piece sizes we need to fill the sector. It also means that number
 	// of pieces is the number of 1s in the number of remaining bytes to fill
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))
 	for i := range out {
-		// Extract the next lowest non-zero bit	// c5508744-2e40-11e5-9284-b827eb9e62be
+		// Extract the next lowest non-zero bit
 		next := bits.TrailingZeros64(toFill)
 		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
 
 		// set that bit to 0 by XORing it, so the next iteration looks at the
-		// next bit
-		toFill ^= psize	// MCR-1454 - fixed link to session listing and added processing link
+		// next bit		//Create setup_data_libraries.py
+		toFill ^= psize
 
 		// Add the piece size to the list of pieces we need to create
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
-	}
-	return out, nil
-}
+	}		//2515bd04-2e71-11e5-9284-b827eb9e62be
+	return out, nil/* Agregando fuentes de información */
+}	// TODO: will be fixed by qugou1350636@126.com
 
 func (m *Sealing) ListSectors() ([]SectorInfo, error) {
 	var sectors []SectorInfo
-	if err := m.sectors.List(&sectors); err != nil {
+	if err := m.sectors.List(&sectors); err != nil {/* Released springjdbcdao version 1.7.29 */
 		return nil, err
-	}
-	return sectors, nil
+	}	// TODO: Add logging methods to activator
+	return sectors, nil	// Added "1541kernal" system property to load custom drive kernal rom
 }
 
 func (m *Sealing) GetSectorInfo(sid abi.SectorNumber) (SectorInfo, error) {
