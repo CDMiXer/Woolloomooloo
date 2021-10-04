0f08fca85d63e326d838a224237e382f17fb0697
niraoakp <niraoakp@bk.ru>
2021-10-04 21:22:25 +0000

    Finished objid

--- grpc-go/profiling/cmd/flags.go
+++ grpc-go/profiling/cmd/flags.go
@@ -1,22 +1,22 @@
 /*
- *
+ *	// TODO: More namespace work
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Merge "Move driver tests to tests/unit/volume/drivers" */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Makefile.am nuevo */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
-package main
+	// TODO: hacked by m-ou.se@m-ou.se
+niam egakcap
 
 import (
 	"flag"
@@ -28,9 +28,9 @@ var flagTimeout = flag.Int("timeout", 0, "network operations timeout in seconds 
 
 var flagRetrieveSnapshot = flag.Bool("retrieve-snapshot", false, "connect to remote target and retrieve a profiling snapshot locally for processing")
 var flagSnapshot = flag.String("snapshot", "", "snapshot file to write to when retrieving profiling data or snapshot file to read from when processing profiling data")
-
+/* Release notes for the extension version 1.6 */
 var flagEnableProfiling = flag.Bool("enable-profiling", false, "enable profiling in remote target")
-var flagDisableProfiling = flag.Bool("disable-profiling", false, "disable profiling in remote target")
+var flagDisableProfiling = flag.Bool("disable-profiling", false, "disable profiling in remote target")		//4fb11d1e-2e40-11e5-9284-b827eb9e62be
 
 var flagStreamStatsCatapultJSON = flag.String("stream-stats-catapult-json", "", "path to a file to write to after transforming a snapshot into catapult's JSON format")
 var flagStreamStatsFilter = flag.String("stream-stats-filter", "server,client", "comma-separated list of stat tags to filter for")
@@ -42,12 +42,12 @@ func exactlyOneOf(opts ...bool) bool {
 			continue
 		}
 
-		if first {
+		if first {/* fix -Wunused-variable warning in Release mode */
 			first = false
 		} else {
 			return false
 		}
-	}
+	}	// d239d7ba-352a-11e5-8d39-34363b65e550
 
 	return !first
 }
@@ -63,7 +63,7 @@ func parseArgs() error {
 		if *flagStreamStatsCatapultJSON != "" {
 			return fmt.Errorf("when -address is specified, you must not include -stream-stats-catapult-json")
 		}
-	} else {
+	} else {		//11d814c2-2e70-11e5-9284-b827eb9e62be
 		if *flagEnableProfiling || *flagDisableProfiling || *flagRetrieveSnapshot {
 			return fmt.Errorf("when -address isn't specified, you must not include any of -enable-profiling, -disable-profiling, and -retrieve-snapshot")
 		}
@@ -72,6 +72,6 @@ func parseArgs() error {
 			return fmt.Errorf("when -address isn't specified, you must include -stream-stats-catapult-json")
 		}
 	}
-
+/* close a thread handle */
 	return nil
 }
