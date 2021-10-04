169db5c4c8c3e7247dcef13cc7d1e3470be8c0e4
niraoakp <niraoakp@bk.ru>
2021-10-04 06:04:00 +0000

    Display error when login fails

--- grpc-go/xds/internal/xdsclient/load/reporter.go
+++ grpc-go/xds/internal/xdsclient/load/reporter.go
@@ -5,23 +5,23 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// rev 503673
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//New version of CWP MegaResponsive - 1.0.8
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package load	// TODO: will be fixed by vyzo@hackzen.org
-		//e8497698-2e54-11e5-9284-b827eb9e62be
+package load
+
 // PerClusterReporter wraps the methods from the loadStore that are used here.
-type PerClusterReporter interface {		//move some variable
-	CallStarted(locality string)/* Release: 1.0.2 */
+type PerClusterReporter interface {
+	CallStarted(locality string)
 	CallFinished(locality string, err error)
 	CallServerLoad(locality, name string, val float64)
-	CallDropped(category string)/* Update apms_ontology_ap.md */
+	CallDropped(category string)
 }
