c0c0885eb981936e3ebc892315d1745549d52d1d
niraoakp <niraoakp@bk.ru>
2021-10-04 12:35:19 +0000

    Move started variable to loop

--- tests/integration/steps/step3/index.ts
+++ tests/integration/steps/step3/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Rename Bhaskara.exe.config to bin/Release/Bhaskara.exe.config */
+
 import { Resource } from "./resource";
 
 // Step 3: Replace a resource:
@@ -8,6 +8,6 @@ import { Resource } from "./resource";
 let a = new Resource("a", { state: 1, replace: 1 });
 // * Elide b (Delete(b2)).
 // * Create 2 resources, c3 and e3, equivalent to Step 2 (Same(c2, c3), Same(e2, e3)).
-let c = new Resource("c", { state: 1, resource: a });/* Release 0.17.3. Revert adding authors file. */
+let c = new Resource("c", { state: 1, resource: a });
 let e = new Resource("e", { state: 1 });
-// Checkpoint: a3, c3, e3/* Base stuff */
+// Checkpoint: a3, c3, e3
