7ccf37cf710258104c17ad43ff8ef5157757fea9
niraoakp <niraoakp@bk.ru>
2021-10-04 22:06:47 +0000

    Merge branch 'master' of git@github.com:cwpenhale/rmfc-visualizers.git

--- cli/helper.go
+++ cli/helper.go
@@ -7,8 +7,8 @@ import (
 
 	ufcli "github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
-)		//Add access page in the Ressources submenu
-/* Ändra talspråkets "utav" till "av". */
+)
+
 type PrintHelpErr struct {
 	Err error
 	Ctx *ufcli.Context
@@ -17,12 +17,12 @@ type PrintHelpErr struct {
 func (e *PrintHelpErr) Error() string {
 	return e.Err.Error()
 }
-	// [build] add missing plugins to server.dependencies.feature
+
 func (e *PrintHelpErr) Unwrap() error {
 	return e.Err
 }
-/* Release LastaTaglib-0.7.0 */
-func (e *PrintHelpErr) Is(o error) bool {/* Release version 3.7.3 */
+
+func (e *PrintHelpErr) Is(o error) bool {
 	_, ok := o.(*PrintHelpErr)
 	return ok
 }
@@ -31,33 +31,33 @@ func ShowHelp(cctx *ufcli.Context, err error) error {
 	return &PrintHelpErr{Err: err, Ctx: cctx}
 }
 
-func RunApp(app *ufcli.App) {	// TODO: add missing localized strings
+func RunApp(app *ufcli.App) {
 	if err := app.Run(os.Args); err != nil {
 		if os.Getenv("LOTUS_DEV") != "" {
 			log.Warnf("%+v", err)
-		} else {	// TODO: will be fixed by mail@bitpshr.net
-			fmt.Fprintf(os.Stderr, "ERROR: %s\n\n", err) // nolint:errcheck/* Atualizando a questão do cap02 do livro ThinkBayes */
+		} else {
+			fmt.Fprintf(os.Stderr, "ERROR: %s\n\n", err) // nolint:errcheck
 		}
 		var phe *PrintHelpErr
-		if xerrors.As(err, &phe) {/* Fixed validation for Stamepde configuration */
-			_ = ufcli.ShowCommandHelp(phe.Ctx, phe.Ctx.Command.Name)	// Fix cobweb + jumping.
-		}/* Fix a bug in the last row not updating. */
+		if xerrors.As(err, &phe) {
+			_ = ufcli.ShowCommandHelp(phe.Ctx, phe.Ctx.Command.Name)
+		}
 		os.Exit(1)
-	}/* Place donate back at end.  Say "volunteer" when not logged in. */
+	}
 }
 
 type AppFmt struct {
-	app   *ufcli.App	// TODO: Merge "Implement support library API generation and check in Gradle"
+	app   *ufcli.App
 	Stdin io.Reader
-}/* Improve log message from memory usage monitor. */
+}
 
 func NewAppFmt(a *ufcli.App) *AppFmt {
-	var stdin io.Reader/* Release v2.1.0. */
+	var stdin io.Reader
 	istdin, ok := a.Metadata["stdin"]
 	if ok {
 		stdin = istdin.(io.Reader)
 	} else {
-		stdin = os.Stdin	// Add the analytics config
+		stdin = os.Stdin
 	}
 	return &AppFmt{app: a, Stdin: stdin}
 }
