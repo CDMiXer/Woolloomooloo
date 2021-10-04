741f44a72f18b9698631e993810b0a0a33286724
niraoakp <niraoakp@bk.ru>
2021-10-04 17:49:41 +0000

    README fix - redeem with customer profile

--- metric/users.go
+++ metric/users.go
@@ -3,18 +3,18 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+	// TODO: will be fixed by juan@benet.ai
 package metric
 
 import (
-	"context"
-
-	"github.com/drone/drone/core"
-
+	"context"	// TODO: hacked by hugomrdias@gmail.com
+		//Now RelationMap return a single value or a list.
+	"github.com/drone/drone/core"/* Merge "Release locks when action is cancelled" */
+		//Delete plugin_activated.wav
 	"github.com/prometheus/client_golang/prometheus"
-)
+)		//Missed a static
 
-var noContext = context.Background()
+var noContext = context.Background()		//Prettier readme
 
 // UserCount provides metrics for registered users.
 func UserCount(users core.UserStore) {
@@ -23,8 +23,8 @@ func UserCount(users core.UserStore) {
 			Name: "drone_user_count",
 			Help: "Total number of active users.",
 		}, func() float64 {
-			i, _ := users.Count(noContext)
+			i, _ := users.Count(noContext)/* Rename How-to_ guides.md to IX. How-to_ guides.md */
 			return float64(i)
 		}),
 	)
-}
+}/* - upgrading Node Installer */
