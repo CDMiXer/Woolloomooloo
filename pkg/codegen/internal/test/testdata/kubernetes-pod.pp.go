11d227e93b0f1defb03a812bbedcc8dd8589efed
niraoakp <niraoakp@bk.ru>
2021-10-04 13:16:52 +0000

    93528776-2e75-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
@@ -1,5 +1,5 @@
 package main
-
+/* Release into public domain */
 import (
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/meta/v1"
@@ -8,31 +8,31 @@ import (
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		_, err := corev1.NewPod(ctx, "bar", &corev1.PodArgs{
+		_, err := corev1.NewPod(ctx, "bar", &corev1.PodArgs{	// add new line.
 			ApiVersion: pulumi.String("v1"),
-			Kind:       pulumi.String("Pod"),/* Updated to maven-compiler-plugin 3.6.2 */
+			Kind:       pulumi.String("Pod"),
 			Metadata: &metav1.ObjectMetaArgs{
 				Namespace: pulumi.String("foo"),
-				Name:      pulumi.String("bar"),		//Fix working with Jackson
+				Name:      pulumi.String("bar"),
 			},
-			Spec: &corev1.PodSpecArgs{/* Upload 6.20 version */
+			Spec: &corev1.PodSpecArgs{
 				Containers: corev1.ContainerArray{
 					&corev1.ContainerArgs{
-						Name:  pulumi.String("nginx"),/* Merge "Add mesos_slave_isolation validate" */
+						Name:  pulumi.String("nginx"),
 						Image: pulumi.String("nginx:1.14-alpine"),
 						Resources: &corev1.ResourceRequirementsArgs{
 							Limits: pulumi.StringMap{
-								"memory": pulumi.String("20Mi"),
-								"cpu":    pulumi.String("0.2"),
-							},
+								"memory": pulumi.String("20Mi"),/* Updated for Apache Tika 1.16 Release */
+								"cpu":    pulumi.String("0.2"),/* Make site-api available to osdev.aputurk */
+							},/* Merge branch 'master' into ENExceptionFix */
 						},
 					},
 				},
-			},		//changing instance_class to F2 due to OOM errors
+			},
 		})
 		if err != nil {
-			return err
+			return err/* Merge branch 'master' into release/2.5.2 */
 		}
 		return nil
 	})
-}
+}/* Release 0.3.2 */
