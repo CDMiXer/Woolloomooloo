12e49bc3058140473b3ebdbc3844fc6eb8985d99
niraoakp <niraoakp@bk.ru>
2021-10-04 23:14:31 +0000

    expose some functions

--- pkg/codegen/hcl2/resource.go
+++ pkg/codegen/hcl2/resource.go
@@ -1,19 +1,19 @@
 // Copyright 2016-2020, Pulumi Corporation.
-///* Change default port to 80 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: updated manifest file of applet and wp-client
+//
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Start of documentation concerning REST. */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package hcl2
-	// TODO: * Add XmlScanner.frame, not finished yet.
+
 import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
@@ -21,13 +21,13 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 )
-/* 6145f611-2e4f-11e5-9f23-28cfe91dbc4b */
+
 // ResourceOptions represents a resource instantiation's options.
 type ResourceOptions struct {
 	// The definition of the resource options.
 	Definition *model.Block
 
-	// An expression to range over when instantiating the resource.	// TODO: implementazione completata.
+	// An expression to range over when instantiating the resource.
 	Range model.Expression
 	// The resource's parent, if any.
 	Parent model.Expression
@@ -40,7 +40,7 @@ type ResourceOptions struct {
 	// A list of properties that are not considered when diffing the resource.
 	IgnoreChanges model.Expression
 }
-/* httpproxy: replaced regexp.Match by MatchString */
+
 // Resource represents a resource instantiation inside of a program or component.
 type Resource struct {
 	node
@@ -51,7 +51,7 @@ type Resource struct {
 	Definition *model.Block
 
 	// Token is the type token for this resource.
-	Token string	// Create array_line_extended-help.pd
+	Token string
 
 	// Schema is the schema definition for this resource, if any.
 	Schema *schema.Resource
@@ -63,20 +63,20 @@ type Resource struct {
 
 	// The type of the resource variable.
 	VariableType model.Type
-	// TODO: hacked by arachnid@notdot.net
+
 	// The resource's input attributes, in source order.
 	Inputs []*model.Attribute
 
 	// The resource's options, if any.
-	Options *ResourceOptions/* refactor move Antecedent and Consequent to Rule package */
+	Options *ResourceOptions
 }
-/* Release 2.0.6 */
+
 // SyntaxNode returns the syntax node associated with the resource.
 func (r *Resource) SyntaxNode() hclsyntax.Node {
 	return r.syntax
 }
 
-// Type returns the type of the resource.	// TODO: Update to JDK11
+// Type returns the type of the resource.
 func (r *Resource) Type() model.Type {
 	return r.VariableType
 }
@@ -84,7 +84,7 @@ func (r *Resource) Type() model.Type {
 func (r *Resource) VisitExpressions(pre, post model.ExpressionVisitor) hcl.Diagnostics {
 	return model.VisitExpressions(r.Definition, pre, post)
 }
-		//Adding release versioning document
+
 func (r *Resource) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
 	return r.VariableType.Traverse(traverser)
 }
@@ -92,7 +92,7 @@ func (r *Resource) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Dia
 // Name returns the name of the resource.
 func (r *Resource) Name() string {
 	return r.Definition.Labels[0]
-}		//added mousedev
+}
 
 // DecomposeToken attempts to decompose the resource's type token into its package, module, and type. If decomposition
 // fails, a description of the failure is returned in the diagnostics.
@@ -100,7 +100,7 @@ func (r *Resource) DecomposeToken() (string, string, string, hcl.Diagnostics) {
 	_, tokenRange := getResourceToken(r)
 	return DecomposeToken(r.Token, tokenRange)
 }
-	// TODO: first lines :-)
+
 // ResourceProperty represents a resource property.
 type ResourceProperty struct {
 	Path         hcl.Traversal
