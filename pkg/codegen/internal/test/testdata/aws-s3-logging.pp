2e226e7a49ea449c7e6320a30099f1fe7f74c0b2
niraoakp <niraoakp@bk.ru>
2021-10-04 09:36:18 +0000

    Update ViewHelpers.php

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp
@@ -1,11 +1,11 @@
-resource logs "aws:s3:Bucket" {}	// TODO: Committing in place refactor
+resource logs "aws:s3:Bucket" {}
 
 resource bucket "aws:s3:Bucket" {
 	loggings = [{
 		targetBucket = logs.bucket,
-	}]	// TODO: will be fixed by nagydani@epointsystem.org
+	}]
 }
 
-output targetBucket {	// Added parsing of def-files, includes and custom operators. #24 #35
-	value = bucket.loggings[0].targetBucket
+output targetBucket {/* Reduced alignment batch size for CPU */
+	value = bucket.loggings[0].targetBucket	// Pass WrappedRequest to Root.init and RootLayout.init
 }
