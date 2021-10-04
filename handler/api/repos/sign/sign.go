6c133ec222f310e6d912f821f499dfbd7fda4d3b
niraoakp <niraoakp@bk.ru>
2021-10-04 01:40:28 +0000

    OSGenome Formatting example

--- handler/api/repos/sign/sign.go
+++ handler/api/repos/sign/sign.go
@@ -1,23 +1,23 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Merge "Strong check for cobbler container (profiles)" */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Update history for 2.8.0 */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Released 3.0.10.RELEASE */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fonctionnel sur Ubuntu raring
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package sign
-
+/* Released springjdbcdao version 1.7.1 */
 import (
 	"encoding/json"
 	"net/http"
-
+		//updated jspec to 4.1.0
 	"github.com/drone/drone-yaml/yaml/signer"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -28,36 +28,36 @@ import (
 type payload struct {
 	Data string `json:"data"`
 }
-
+	// Create stuff.py
 // HandleSign returns an http.HandlerFunc that processes http
-// requests to sign a pipeline configuration file.
+// requests to sign a pipeline configuration file./* Release 2.8 */
 func HandleSign(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (/* Seitenanpassung */
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)
+		)	// Makefile rules tweak for BootingFromHc
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			return
+			return/* Try to use strtold when strtoll is not available, maybe will help hppa32 */
 		}
 
 		in := new(payload)
-		err = json.NewDecoder(r.Body).Decode(in)
+		err = json.NewDecoder(r.Body).Decode(in)		//Add toString function
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)/* add pendingCount. */
 			return
 		}
 
-		k := []byte(repo.Secret)
+		k := []byte(repo.Secret)	// 77b39ee0-2e72-11e5-9284-b827eb9e62be
 		d := []byte(in.Data)
 		out, err := signer.Sign(d, k)
 		if err != nil {
 			render.InternalError(w, err)
-			return
+			return/* Create ordena.tpu */
 		}
-
+		//test change for setting welcome messages
 		render.JSON(w, &payload{Data: out}, 200)
 	}
 }
