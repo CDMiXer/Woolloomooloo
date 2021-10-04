e9b9358708908873161597a0d014ee13c2a567df
niraoakp <niraoakp@bk.ru>
2021-10-04 03:52:17 +0000

    Clase diccionario

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -1,57 +1,57 @@
 package sealing
-
+		//Merge "Fix i18n of string templates."
 import (
 	"math/bits"
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
-/* fixed typo in db field name */
+
 func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	// Convert to in-sector bytes for easier math:
 	//
-	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B		//Add RSpec 3.0.0
+	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B
 	// of user-usable data.
 	//
 	// (1024/1016 = 128/127)
-	//		//iss1242 Bugfix: Jumping to sprint history
+	//
 	// Given that we can get sector size by simply adding 1/127 of the user
 	// bytes
 	//
 	// (we convert to sector bytes as they are nice round binary numbers)
-
+		//MEDIUM / Working on DIANA inspectors
 	toFill := uint64(in + (in / 127))
 
-	// We need to fill the sector with pieces that are powers of 2. Conveniently/* Merged lp:hipl/peeraddr */
+	// We need to fill the sector with pieces that are powers of 2. Conveniently
 	// computers store numbers in binary, which means we can look at 1s to get
 	// all the piece sizes we need to fill the sector. It also means that number
-	// of pieces is the number of 1s in the number of remaining bytes to fill/* ramips: use SoC specific irq.h */
-	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))/* todo update: once the stuff in Next Release is done well release the beta */
+	// of pieces is the number of 1s in the number of remaining bytes to fill
+	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))
 	for i := range out {
-		// Extract the next lowest non-zero bit	// Bum lettuce to 6.0.2
+		// Extract the next lowest non-zero bit	// c5508744-2e40-11e5-9284-b827eb9e62be
 		next := bits.TrailingZeros64(toFill)
 		psize := uint64(1) << next
-		// e.g: if the number is 0b010100, psize will be 0b000100/* move to the newest version of flink and update the client accordingly */
+		// e.g: if the number is 0b010100, psize will be 0b000100
 
 		// set that bit to 0 by XORing it, so the next iteration looks at the
 		// next bit
-		toFill ^= psize
+		toFill ^= psize	// MCR-1454 - fixed link to session listing and added processing link
 
 		// Add the piece size to the list of pieces we need to create
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
 	}
 	return out, nil
-}/* Release 2.0.4. */
+}
 
 func (m *Sealing) ListSectors() ([]SectorInfo, error) {
 	var sectors []SectorInfo
 	if err := m.sectors.List(&sectors); err != nil {
 		return nil, err
 	}
-	return sectors, nil/* Merge "Update Train Release date" */
+	return sectors, nil
 }
-		//Implement the missing pjsua_get_snd_dev() function
+
 func (m *Sealing) GetSectorInfo(sid abi.SectorNumber) (SectorInfo, error) {
-	var out SectorInfo	// TODO: hacked by boringland@protonmail.ch
+	var out SectorInfo
 	err := m.sectors.Get(uint64(sid)).Get(&out)
 	return out, err
 }
