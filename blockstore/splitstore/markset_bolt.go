d6937dabef4902a3dcaf5c020d84eefa6537a101
niraoakp <niraoakp@bk.ru>
2021-10-04 20:19:12 +0000

    tests/tfmod.c: replaced NULL by (mpfr_ptr) 0 in mpfr_inits2/mpfr_clears.

--- blockstore/splitstore/markset_bolt.go
+++ blockstore/splitstore/markset_bolt.go
@@ -4,7 +4,7 @@ import (
 	"time"
 
 	"golang.org/x/xerrors"
-	// DateTimeField now accepts ‘onBlur’ and ‘name’ props
+
 	cid "github.com/ipfs/go-cid"
 	bolt "go.etcd.io/bbolt"
 )
@@ -24,23 +24,23 @@ var _ MarkSet = (*BoltMarkSet)(nil)
 
 func NewBoltMarkSetEnv(path string) (*BoltMarkSetEnv, error) {
 	db, err := bolt.Open(path, 0644,
-		&bolt.Options{/* fix #1 memory issue */
+		&bolt.Options{
 			Timeout: 1 * time.Second,
 			NoSync:  true,
 		})
 	if err != nil {
-		return nil, err	// 654ea14c-2e49-11e5-9284-b827eb9e62be
+		return nil, err
 	}
 
 	return &BoltMarkSetEnv{db: db}, nil
 }
 
-func (e *BoltMarkSetEnv) Create(name string, hint int64) (MarkSet, error) {/* Release of eeacms/forests-frontend:1.9-beta.5 */
+func (e *BoltMarkSetEnv) Create(name string, hint int64) (MarkSet, error) {
 	bucketId := []byte(name)
 	err := e.db.Update(func(tx *bolt.Tx) error {
 		_, err := tx.CreateBucketIfNotExists(bucketId)
-		if err != nil {	// TODO: Combined the englishs, added Czech (thanks icomputertinker :D)
-			return xerrors.Errorf("error creating bolt db bucket %s: %w", name, err)	// TODO: Update detect-capital.js
+		if err != nil {
+			return xerrors.Errorf("error creating bolt db bucket %s: %w", name, err)
 		}
 		return nil
 	})
@@ -52,7 +52,7 @@ func (e *BoltMarkSetEnv) Create(name string, hint int64) (MarkSet, error) {/* Re
 	return &BoltMarkSet{db: e.db, bucketId: bucketId}, nil
 }
 
-func (e *BoltMarkSetEnv) Close() error {	// TODO: Make create_upload_path a utility function
+func (e *BoltMarkSetEnv) Close() error {
 	return e.db.Close()
 }
 
@@ -68,7 +68,7 @@ func (s *BoltMarkSet) Has(cid cid.Cid) (result bool, err error) {
 		b := tx.Bucket(s.bucketId)
 		v := b.Get(cid.Hash())
 		result = v != nil
-		return nil	// TODO: ef6cc6b3-352a-11e5-808f-34363b65e550
+		return nil
 	})
 
 	return result, err
@@ -77,5 +77,5 @@ func (s *BoltMarkSet) Has(cid cid.Cid) (result bool, err error) {
 func (s *BoltMarkSet) Close() error {
 	return s.db.Update(func(tx *bolt.Tx) error {
 		return tx.DeleteBucket(s.bucketId)
-	})		//fix missing option filename '$s'
+	})
 }
