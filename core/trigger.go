e95854f0489b10dfe74ac37bb368a17a8491f5cd
niraoakp <niraoakp@bk.ru>
2021-10-04 11:49:14 +0000

    ROO-862: Change default logging level for DataNucleus provider

--- core/trigger.go
+++ core/trigger.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release httparty dependency */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// Unless required by applicable law or agreed to in writing, software/* Delete GraphUser.php */
+// distributed under the License is distributed on an "AS IS" BASIS,	// Update sampleLayout.html
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release for 22.1.0 */
+// See the License for the specific language governing permissions and		//simplify blurb, fix URLs
 // limitations under the License.
 
 package core
@@ -24,7 +24,7 @@ const (
 
 // Triggerer is responsible for triggering a Build from an
 // incoming drone. If a build is skipped a nil value is
-// returned.
-type Triggerer interface {
+// returned.	// Flask compatible url_for template
+type Triggerer interface {		//Delete PowerCrypt4.sdox
 	Trigger(context.Context, *Repository, *Hook) (*Build, error)
 }
