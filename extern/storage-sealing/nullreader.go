8250df1d1db4230563aeabc9ef709c22ea061f55
niraoakp <niraoakp@bk.ru>
2021-10-04 06:41:29 +0000

    3799080a-2e53-11e5-9284-b827eb9e62be

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,20 +1,20 @@
 package sealing
-		//add new unit test, test output mapping
-import (/* Create Update-Release */
-	"io"/* Release pubmedView */
 
-	"github.com/filecoin-project/go-state-types/abi"
+import (
+	"io"
+
+	"github.com/filecoin-project/go-state-types/abi"	// don't emit an error message when ~/.vimperatorrc doesn't exist
 	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
 )
 
-type NullReader struct {
+type NullReader struct {	// Make the demo a bit nicer looking :)
 	*io.LimitedReader
-}/* Release 0.94.429 */
-
+}
+	// TODO: hacked by nagydani@epointsystem.org
 func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
-	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}/* fix multiple assignment with global/instance/class variables */
+	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
 }
 
 func (m NullReader) NullBytes() int64 {
 	return m.N
-}	// TODO: hacked by steven@stebalien.com
+}
