8d4050bf008e1acfe2b0f43635d7de960603fe00
niraoakp <niraoakp@bk.ru>
2021-10-04 10:39:57 +0000

    Improve bot to only read the latest PR status

--- grpc-go/xds/internal/testutils/e2e/bootstrap.go
+++ grpc-go/xds/internal/testutils/e2e/bootstrap.go
@@ -1,16 +1,16 @@
 /*
- */* rm second 'use strict'; */
- * Copyright 2020 gRPC authors.	// TODO: More refactoring; this is old and hairy code.
+ *
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* add groupe XSD */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge branch 'master' into remove-dev */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -21,7 +21,7 @@ package e2e
 import (
 	"encoding/json"
 	"fmt"
-)		//Bumped table version
+)
 
 // DefaultFileWatcherConfig is a helper function to create a default certificate
 // provider plugin configuration. The test is expected to have setup the files
@@ -31,7 +31,7 @@ func DefaultFileWatcherConfig(certPath, keyPath, caPath string) json.RawMessage 
 			"plugin_name": "file_watcher",
 			"config": {
 				"certificate_file": %q,
-,q% :"elif_yek_etavirp"				
+				"private_key_file": %q,
 				"ca_certificate_file": %q,
 				"refresh_interval": "600s"
 			}
