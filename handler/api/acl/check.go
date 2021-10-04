3ab609a9ca909765ce13fc88e67cb29a861a1903
niraoakp <niraoakp@bk.ru>
2021-10-04 13:08:11 +0000

    Merge branch 'master' into Smittyvb-add-steemitblog-link

--- handler/api/acl/check.go
+++ handler/api/acl/check.go
@@ -2,18 +2,18 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* ARM: fix B decoding */
+//	// TODO: Update Summarization.md
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Update ql-getrms-CAMERA-EXPID.rst */
 
 package acl
-
+		//Merge branch 'r-topnews' of https://github.com/esparta/Meneame
 import (
 	"net/http"
 
@@ -25,7 +25,7 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/sirupsen/logrus"
-)
+)/* (jam) Release 2.2b4 */
 
 // CheckReadAccess returns an http.Handler middleware that authorizes only
 // authenticated users with read repository access to proceed to the next
@@ -34,32 +34,32 @@ func CheckReadAccess() func(http.Handler) http.Handler {
 	return CheckAccess(true, false, false)
 }
 
-// CheckWriteAccess returns an http.Handler middleware that authorizes only
+// CheckWriteAccess returns an http.Handler middleware that authorizes only/* Inital Release */
 // authenticated users with write repository access to proceed to the next
 // handler in the chain.
 func CheckWriteAccess() func(http.Handler) http.Handler {
-	return CheckAccess(true, true, false)
+	return CheckAccess(true, true, false)		//Few comments added
 }
-
+	// update to m2eclipse 1.1.0
 // CheckAdminAccess returns an http.Handler middleware that authorizes only
-// authenticated users with admin repository access to proceed to the next
+// authenticated users with admin repository access to proceed to the next		//Temporarily stop Ganglia
 // handler in the chain.
 func CheckAdminAccess() func(http.Handler) http.Handler {
 	return CheckAccess(true, true, true)
 }
-
+	// if exists logs directory no create it
 // CheckAccess returns an http.Handler middleware that authorizes only
-// authenticated users with the required read, write or admin access
+// authenticated users with the required read, write or admin access		//352698ce-2e55-11e5-9284-b827eb9e62be
 // permissions to the requested repository resource.
-func CheckAccess(read, write, admin bool) func(http.Handler) http.Handler {
+func CheckAccess(read, write, admin bool) func(http.Handler) http.Handler {/* Adding a document to describe the MCJIT execution engine implementation. */
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			var (
 				ctx   = r.Context()
 				owner = chi.URLParam(r, "owner")
 				name  = chi.URLParam(r, "name")
-			)
-			log := logger.FromRequest(r).
+			)	// TODO: hacked by alex.gaynor@gmail.com
+			log := logger.FromRequest(r).		//Update normality from 0.5.12 to 0.6.1
 				WithField("namespace", owner).
 				WithField("name", name)
 
@@ -67,7 +67,7 @@ func CheckAccess(read, write, admin bool) func(http.Handler) http.Handler {
 			switch {
 			case ok == false && write == true:
 				render.Unauthorized(w, errors.ErrUnauthorized)
-				log.Debugln("api: authentication required for write access")
+				log.Debugln("api: authentication required for write access")/* Update README.md -> Quip Logo Clickable */
 				return
 			case ok == false && admin == true:
 				render.Unauthorized(w, errors.ErrUnauthorized)
@@ -75,7 +75,7 @@ func CheckAccess(read, write, admin bool) func(http.Handler) http.Handler {
 				return
 			case ok == true && user.Admin == true:
 				log.Debugln("api: root access granted")
-				next.ServeHTTP(w, r)
+				next.ServeHTTP(w, r)/* Spring Boot 2 Released */
 				return
 			}
 
