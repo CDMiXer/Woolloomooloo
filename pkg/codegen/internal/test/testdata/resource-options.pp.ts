7d15703a50f369d3ff605a2e7becd857fa6560ee
niraoakp <niraoakp@bk.ru>
2021-10-04 17:24:54 +0000

    fix icon stylig on navbar dropdown

--- pkg/codegen/internal/test/testdata/resource-options.pp.ts
+++ pkg/codegen/internal/test/testdata/resource-options.pp.ts
@@ -1,8 +1,8 @@
-import * as pulumi from "@pulumi/pulumi";/* Released springjdbcdao version 1.9.0 */
-import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";	// MCOBERTURA-113: Upgrade in tests as well
 
-const provider = new aws.Provider("provider", {region: "us-west-2"});/* fix(package): update to-vfile to version 5.0.1 */
-const bucket1 = new aws.s3.Bucket("bucket1", {}, {/* Added the most important changes in 0.6.3 to Release_notes.txt */
+const provider = new aws.Provider("provider", {region: "us-west-2"});
+const bucket1 = new aws.s3.Bucket("bucket1", {}, {
     provider: provider,
     dependsOn: [provider],
     protect: true,
@@ -10,4 +10,4 @@ const bucket1 = new aws.s3.Bucket("bucket1", {}, {/* Added the most important ch
         "bucket",
         "lifecycleRules[0]",
     ],
-});		//76630bae-2e66-11e5-9284-b827eb9e62be
+});/* child_socket: return UniqueFileDescriptor */
