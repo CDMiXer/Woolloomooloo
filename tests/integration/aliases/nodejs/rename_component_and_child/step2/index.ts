e647bd8bfca150ff09bbdfc92d9e316c4ce91f50
niraoakp <niraoakp@bk.ru>
2021-10-04 12:15:09 +0000

    96a6ecf5-327f-11e5-8291-9cf387a8033e

--- tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step2/index.ts
@@ -1,24 +1,24 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Poprawki do szablonu FVAT */
+
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }		//Update dependencies; remove support for nodejs 0.8.
-}/* Release: 5.8.2 changelog */
+    }
+}
 
 // Scenario #5 - composing #1 and #3
 class ComponentFive extends pulumi.ComponentResource {
-    resource: Resource;/* Released 0.1.5 version */
+    resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentFive", name, {}, opts);
-        this.resource = new Resource("otherchildrenamed", {
+        this.resource = new Resource("otherchildrenamed", {/* simplified ignoring .par files */
             parent: this,
             aliases: [{ name: "otherchild", parent: this }],
-        });/* Release of eeacms/www-devel:20.3.3 */
-}    
-}
+        });
+    }
+}	// TODO: cherry pick from 3.0-reviews
 const comp5 = new ComponentFive("newcomp5", {
     aliases: [{ name: "comp5" }],
 });
