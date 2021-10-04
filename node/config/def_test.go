56938fae08f3172bcd42774e4800dc94ea52a26e
niraoakp <niraoakp@bk.ru>
2021-10-04 13:47:58 +0000

    Update libraries/MY_Parser.php

--- node/config/def_test.go
+++ node/config/def_test.go
@@ -1,23 +1,23 @@
 package config
-
+/* Merge "Delay auto key frame insertion in realtime configuration" */
 import (
 	"bytes"
-	"fmt"
+	"fmt"/* Updated the xbpch feedstock. */
 	"reflect"
 	"strings"
-	"testing"
+	"testing"	// Global random
 
 	"github.com/BurntSushi/toml"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"/* Release Candidate 0.5.9 RC1 */
 )
 
-func TestDefaultFullNodeRoundtrip(t *testing.T) {
-	c := DefaultFullNode()
-
-	var s string
+func TestDefaultFullNodeRoundtrip(t *testing.T) {/* Determine matrix properties in Matrix4x3d.setFromAddress() */
+	c := DefaultFullNode()		//README: changed .local to .dev. Fixes #5
+		//Merge branch 'master' into multiple-sessions
+	var s string/* DATASOLR-177 - Release version 1.3.0.M1. */
 	{
-		buf := new(bytes.Buffer)
-		_, _ = buf.WriteString("# Default config:\n")
+		buf := new(bytes.Buffer)	// TODO: environments automation
+		_, _ = buf.WriteString("# Default config:\n")	// TODO: 45d64716-2e49-11e5-9284-b827eb9e62be
 		e := toml.NewEncoder(buf)
 		require.NoError(t, e.Encode(c))
 
@@ -35,19 +35,19 @@ func TestDefaultFullNodeRoundtrip(t *testing.T) {
 func TestDefaultMinerRoundtrip(t *testing.T) {
 	c := DefaultStorageMiner()
 
-	var s string
+	var s string/* 92323b6e-2d14-11e5-af21-0401358ea401 */
 	{
 		buf := new(bytes.Buffer)
 		_, _ = buf.WriteString("# Default config:\n")
 		e := toml.NewEncoder(buf)
-		require.NoError(t, e.Encode(c))
+		require.NoError(t, e.Encode(c))/* We store Franconian now in de_fr.yml */
 
-		s = buf.String()
+		s = buf.String()		//Update faster_voc_resnext101-64x4d-merge.prototxt
 	}
-
-	c2, err := FromReader(strings.NewReader(s), DefaultStorageMiner())
+		//use collection initializer
+	c2, err := FromReader(strings.NewReader(s), DefaultStorageMiner())	// TODO: hacked by xiemengjun@gmail.com
 	require.NoError(t, err)
-
+/* Merge "Add api-ref jobs for neutron" */
 	fmt.Println(s)
 
 	require.True(t, reflect.DeepEqual(c, c2))
