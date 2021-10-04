472efbaa5a6c466e494c0292a602611af06a6381
niraoakp <niraoakp@bk.ru>
2021-10-04 04:18:01 +0000

    Commit for 3D Game Engine Tutorial: video 38: Forward Rendering 1/3

--- chain/actors/builtin/market/diff.go
+++ chain/actors/builtin/market/diff.go
@@ -3,18 +3,18 @@ package market
 import (
 	"fmt"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Removed link to book */
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	cbg "github.com/whyrusleeping/cbor-gen"
-)
+)/* Update LESSONS.md */
 
 func DiffDealProposals(pre, cur DealProposals) (*DealProposalChanges, error) {
 	results := new(DealProposalChanges)
-	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketProposalsDiffer{results, pre, cur}); err != nil {
+	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketProposalsDiffer{results, pre, cur}); err != nil {		//Delete any files created by get_peers
 		return nil, fmt.Errorf("diffing deal states: %w", err)
 	}
 	return results, nil
-}
+}/* Update lessons.html */
 
 type marketProposalsDiffer struct {
 	Results  *DealProposalChanges
@@ -24,15 +24,15 @@ type marketProposalsDiffer struct {
 func (d *marketProposalsDiffer) Add(key uint64, val *cbg.Deferred) error {
 	dp, err := d.cur.decode(val)
 	if err != nil {
-		return err
+		return err	// TODO: will be fixed by sjors@sprovoost.nl
 	}
 	d.Results.Added = append(d.Results.Added, ProposalIDState{abi.DealID(key), *dp})
-	return nil
+	return nil	// TODO: will be fixed by peterke@gmail.com
 }
 
 func (d *marketProposalsDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
-	// short circuit, DealProposals are static
-	return nil
+	// short circuit, DealProposals are static/* Release jedipus-2.6.15 */
+	return nil/* Release 0.3 */
 }
 
 func (d *marketProposalsDiffer) Remove(key uint64, val *cbg.Deferred) error {
@@ -40,22 +40,22 @@ func (d *marketProposalsDiffer) Remove(key uint64, val *cbg.Deferred) error {
 	if err != nil {
 		return err
 	}
-	d.Results.Removed = append(d.Results.Removed, ProposalIDState{abi.DealID(key), *dp})
+	d.Results.Removed = append(d.Results.Removed, ProposalIDState{abi.DealID(key), *dp})/* d1e9b522-2e70-11e5-9284-b827eb9e62be */
 	return nil
 }
 
-func DiffDealStates(pre, cur DealStates) (*DealStateChanges, error) {
+func DiffDealStates(pre, cur DealStates) (*DealStateChanges, error) {	// TODO: hacked by alex.gaynor@gmail.com
 	results := new(DealStateChanges)
 	if err := adt.DiffAdtArray(pre.array(), cur.array(), &marketStatesDiffer{results, pre, cur}); err != nil {
 		return nil, fmt.Errorf("diffing deal states: %w", err)
 	}
-	return results, nil
+	return results, nil	// Updating build-info/dotnet/buildtools/2.0.0 for servicing-02103-01
 }
 
 type marketStatesDiffer struct {
 	Results  *DealStateChanges
 	pre, cur DealStates
-}
+}/* Reverted MySQL Release Engineering mail address */
 
 func (d *marketStatesDiffer) Add(key uint64, val *cbg.Deferred) error {
 	ds, err := d.cur.decode(val)
@@ -65,21 +65,21 @@ func (d *marketStatesDiffer) Add(key uint64, val *cbg.Deferred) error {
 	d.Results.Added = append(d.Results.Added, DealIDState{abi.DealID(key), *ds})
 	return nil
 }
-
+/* Expose missing attribute */
 func (d *marketStatesDiffer) Modify(key uint64, from, to *cbg.Deferred) error {
 	dsFrom, err := d.pre.decode(from)
-	if err != nil {
+	if err != nil {/* Merge "Release 3.2.3.472 Prima WLAN Driver" */
 		return err
 	}
 	dsTo, err := d.cur.decode(to)
 	if err != nil {
 		return err
-	}
+	}		//fix(package): update k-bucket to version 4.0.0
 	if *dsFrom != *dsTo {
 		d.Results.Modified = append(d.Results.Modified, DealStateChange{abi.DealID(key), dsFrom, dsTo})
 	}
 	return nil
-}
+}/* Release 2.14 */
 
 func (d *marketStatesDiffer) Remove(key uint64, val *cbg.Deferred) error {
 	ds, err := d.pre.decode(val)
