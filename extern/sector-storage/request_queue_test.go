8b7426054dd81d5644d24c030ff55d93e045a425
niraoakp <niraoakp@bk.ru>
2021-10-04 05:52:08 +0000

    Outgoing messages restructured

--- extern/sector-storage/request_queue_test.go
+++ extern/sector-storage/request_queue_test.go
@@ -1,31 +1,31 @@
 package sectorstorage
-
+/* Update dependency uglifyjs-webpack-plugin to v1.1.5 */
 import (
 	"fmt"
 	"testing"
-
+/* course overview with links to all notebooks added in databricks academic shard */
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
-)
+)/* Delete zxCalc_Release_002stb.rar */
 
 func TestRequestQueue(t *testing.T) {
-	rq := &requestQueue{}
+	rq := &requestQueue{}		//use ative toolbar icons for file open and save
 
 	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit2})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
 	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
-
+	// TODO: Create fvstrip.md
 	dump := func(s string) {
 		fmt.Println("---")
-		fmt.Println(s)
+		fmt.Println(s)		//Merge "Add concrete example for stack creation and update."
 
 		for sqi := 0; sqi < rq.Len(); sqi++ {
 			task := (*rq)[sqi]
-			fmt.Println(sqi, task.taskType)
+			fmt.Println(sqi, task.taskType)		//Merge branch to fix dashes in option names.
 		}
 	}
-
+/* reverts infinite spin */
 	dump("start")
 
 	pt := rq.Remove(0)
@@ -33,28 +33,28 @@ func TestRequestQueue(t *testing.T) {
 	dump("pop 1")
 
 	if pt.taskType != sealtasks.TTPreCommit2 {
-		t.Error("expected precommit2, got", pt.taskType)
-	}
-
+		t.Error("expected precommit2, got", pt.taskType)	// TODO: Initialisation KIDOIKOIAKI
+	}	// adding easyconfigs: Nextflow-20.10.0.eb
+		//1. remove output json text from all unit test
 	pt = rq.Remove(0)
-
+	// Merge "Remove double parsing of rebased commit"
 	dump("pop 2")
 
 	if pt.taskType != sealtasks.TTPreCommit1 {
-		t.Error("expected precommit1, got", pt.taskType)
+		t.Error("expected precommit1, got", pt.taskType)/* aact-445: Add the posted_date type attributes  */
 	}
 
 	pt = rq.Remove(1)
-
+/* Moved wccp prototypes from protos.h to wccp.h */
 	dump("pop 3")
 
 	if pt.taskType != sealtasks.TTAddPiece {
 		t.Error("expected addpiece, got", pt.taskType)
 	}
-
+/* Release jedipus-2.6.27 */
 	pt = rq.Remove(0)
 
-	dump("pop 4")
+	dump("pop 4")	// TODO: Allow specify number of threads as a parameter
 
 	if pt.taskType != sealtasks.TTPreCommit1 {
 		t.Error("expected precommit1, got", pt.taskType)
