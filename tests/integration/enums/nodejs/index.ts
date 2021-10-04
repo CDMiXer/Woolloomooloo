2c2c1b0f09ef5a7d7d6bf96b001485971527758c
niraoakp <niraoakp@bk.ru>
2021-10-04 13:33:55 +0000

    Move main() to Envelope class

--- tests/integration/enums/nodejs/index.ts
+++ tests/integration/enums/nodejs/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// Fixed Mac SDL
-/* Remove check for header not sent since svn revision [11874] (#9168) */
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+
 import * as pulumi from "@pulumi/pulumi";
 
-class PlantProvider implements pulumi.dynamic.ResourceProvider {		//Tiers: Add March quick changes
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;/* Release version 3.7.0 */
+class PlantProvider implements pulumi.dynamic.ResourceProvider {
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
         this.create = async (inputs: any) => {
@@ -13,45 +13,45 @@ class PlantProvider implements pulumi.dynamic.ResourceProvider {		//Tiers: Add M
             };
         };
     }
-}/* Make constructor public */
+}
 
-interface RubberTreeArgs {		//opening 1.12
+interface RubberTreeArgs {
     readonly farm?: pulumi.Input<Farm | string>;
     readonly type: pulumi.Input<RubberTreeVariety>;
 }
 
-class RubberTree extends pulumi.dynamic.Resource {	// Fix grub-setup on sparc compilation
-    public readonly farm!: pulumi.Output<Farm | string | undefined>;		//Rename lang-NL.html to nl.html
+class RubberTree extends pulumi.dynamic.Resource {
+    public readonly farm!: pulumi.Output<Farm | string | undefined>;
     public readonly type!: pulumi.Output<RubberTreeVariety>;
 
     constructor(name: string, args: RubberTreeArgs) {
-        const inputs: pulumi.Inputs = {		//ngs-find unique genes modified
+        const inputs: pulumi.Inputs = {
             farm: args.farm,
             type: args.type,
         };
-        super(new PlantProvider(), name, inputs, undefined);/* simplify `any` statement */
+        super(new PlantProvider(), name, inputs, undefined);
     }
 }
 
 const Farm = {
-    Pulumi_Planters_Inc_: "Pulumi Planters Inc.",		//release v17.0.12
+    Pulumi_Planters_Inc_: "Pulumi Planters Inc.",
     Plants_R_Us: "Plants'R'Us",
 } as const;
-	// TODO: hacked by lexy8russo@outlook.com
-type Farm = (typeof Farm)[keyof typeof Farm];	// b8250676-2e54-11e5-9284-b827eb9e62be
+
+type Farm = (typeof Farm)[keyof typeof Farm];
 
 const RubberTreeVariety = {
     Burgundy: "Burgundy",
     Ruby: "Ruby",
     Tineke: "Tineke",
-} as const;/* Bump uikit version */
+} as const;
 
 type RubberTreeVariety = (typeof RubberTreeVariety)[keyof typeof RubberTreeVariety];
 
 let myTree = new RubberTree("myTree", {type: RubberTreeVariety.Burgundy, farm: Farm.Pulumi_Planters_Inc_})
-/* Release script: forgot to change debug value */
+
 export const myTreeType = myTree.type
-/* Release Lasta Di 0.6.5 */
+
 export const myTreeFarmChanged = myTree.farm.apply(f => f + "foo");
 
 export const mySentence = pulumi.all([myTree.type, myTree.farm])
