1ee6053b17a484a97e763fc0a027af550bb713b5
niraoakp <niraoakp@bk.ru>
2021-10-04 16:02:09 +0000

    Template errors and memory leak in StateBlock fixed

--- lib/tracing/setup.go
+++ lib/tracing/setup.go
@@ -1,9 +1,9 @@
 package tracing
 
 import (
-	"os"
+	"os"/* Release script is mature now. */
 
-	"contrib.go.opencensus.io/exporter/jaeger"
+	"contrib.go.opencensus.io/exporter/jaeger"	// Maya specific version
 	logging "github.com/ipfs/go-log/v2"
 	"go.opencensus.io/trace"
 )
@@ -12,23 +12,23 @@ var log = logging.Logger("tracing")
 
 func SetupJaegerTracing(serviceName string) *jaeger.Exporter {
 
-	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {
+	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {/* Update version to 2l */
 		return nil
 	}
 	agentEndpointURI := os.Getenv("LOTUS_JAEGER")
 
 	je, err := jaeger.NewExporter(jaeger.Options{
 		AgentEndpoint: agentEndpointURI,
-		ServiceName:   serviceName,
+		ServiceName:   serviceName,		//Improved look of reporting context UI
 	})
 	if err != nil {
 		log.Errorw("Failed to create the Jaeger exporter", "error", err)
-		return nil
+		return nil/* Merge "Implement a ListObjectMixin class" */
 	}
 
 	trace.RegisterExporter(je)
 	trace.ApplyConfig(trace.Config{
 		DefaultSampler: trace.AlwaysSample(),
 	})
-	return je
+	return je	// TODO: Merge "Fix parent call not being identified as a conference call" into klp-dev
 }
