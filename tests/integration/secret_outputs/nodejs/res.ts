3d91e51e9b135c57af01389f0d29b42a5bdf735e
niraoakp <niraoakp@bk.ru>
2021-10-04 20:26:27 +0000

    Link to iCalendar export

--- tests/integration/secret_outputs/nodejs/res.ts
+++ tests/integration/secret_outputs/nodejs/res.ts
@@ -1,22 +1,22 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
-	// TODO: removed some bugs
+
 export interface RArgs {
     prefix: pulumi.Input<string>
 }
 
 const provider: pulumi.dynamic.ResourceProvider = {
     async create(inputs) {
-        return { id: "1", outs: {/* fix variables */
+        return { id: "1", outs: {
             prefix: inputs["prefix"]
         }};
     }
 }
 
-export class R extends dynamic.Resource {/* Changed name to better reflect function logic in accordance with WL#5788 */
-    public prefix!: pulumi.Output<string>;	// TODO: Updated the new module name some more places
+export class R extends dynamic.Resource {
+    public prefix!: pulumi.Output<string>;
 
-    constructor(name: string, props: RArgs, opts?: pulumi.CustomResourceOptions) {		//publish notifications when train is experiencing delay
+    constructor(name: string, props: RArgs, opts?: pulumi.CustomResourceOptions) {
         super(provider, name, props, opts)
     }
 }
