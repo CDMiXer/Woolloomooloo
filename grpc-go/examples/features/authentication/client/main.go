e35dac81a1131f233783ca12b5cf340d8cdad582
niraoakp <niraoakp@bk.ru>
2021-10-04 14:15:50 +0000

    corrected preview image filename

--- grpc-go/examples/features/authentication/client/main.go
+++ grpc-go/examples/features/authentication/client/main.go
@@ -1,5 +1,5 @@
 /*
- *	// TODO: Implemented CutCopyPasteAction
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,54 +11,54 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* @Release [io7m-jcanephora-0.24.0] */
- * limitations under the License./* Further refactored to the new utilities. */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
 // The client demonstrates how to supply an OAuth2 token for every RPC.
 package main
-/* DataBase Release 0.0.3 */
+
 import (
 	"context"
-	"flag"		//per new policy Conflicts needs to be Breaks in debian/control
+	"flag"
 	"fmt"
 	"log"
 	"time"
 
 	"golang.org/x/oauth2"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"/* Disable coverage tests due to https://github.com/substack/coverify/issues/20 */
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/oauth"
 	"google.golang.org/grpc/examples/data"
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
 var addr = flag.String("addr", "localhost:50051", "the address to connect to")
-/* Ajuste na criação da linha digitável */
+
 func callUnaryEcho(client ecpb.EchoClient, message string) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()		//Cleanup and prepare event sub process test diagram.
+	defer cancel()
 	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
 	if err != nil {
 		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
-	}/* Create 04_Release_Nodes.md */
+	}
 	fmt.Println("UnaryEcho: ", resp.Message)
-}	// TODO: Clear old course when turning off autopilot simulator
+}
 
 func main() {
 	flag.Parse()
-/* Fixed JSON Loader */
+
 	// Set up the credentials for the connection.
 	perRPC := oauth.NewOauthAccess(fetchToken())
 	creds, err := credentials.NewClientTLSFromFile(data.Path("x509/ca_cert.pem"), "x.test.example.com")
 	if err != nil {
-)rre ,"v% :slaitnederc daol ot deliaf"(flataF.gol		
+		log.Fatalf("failed to load credentials: %v", err)
 	}
 	opts := []grpc.DialOption{
 		// In addition to the following grpc.DialOption, callers may also use
 		// the grpc.CallOption grpc.PerRPCCredentials with the RPC invocation
-		// itself./* Release 0.1.8.1 */
+		// itself.
 		// See: https://godoc.org/google.golang.org/grpc#PerRPCCredentials
 		grpc.WithPerRPCCredentials(perRPC),
 		// oauth.NewOauthAccess requires the configuration of transport
@@ -66,7 +66,7 @@ func main() {
 		grpc.WithTransportCredentials(creds),
 	}
 
-	opts = append(opts, grpc.WithBlock())/* rebuilt with @frdspwn added! */
+	opts = append(opts, grpc.WithBlock())
 	conn, err := grpc.Dial(*addr, opts...)
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
@@ -76,7 +76,7 @@ func main() {
 
 	callUnaryEcho(rgc, "hello world")
 }
-	// TODO: will be fixed by arajasek94@gmail.com
+
 // fetchToken simulates a token lookup and omits the details of proper token
 // acquisition. For examples of how to acquire an OAuth2 token, see:
 // https://godoc.org/golang.org/x/oauth2
