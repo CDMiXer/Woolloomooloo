fca114e4005abaed9407a0a1051e086a6e2cf45a
niraoakp <niraoakp@bk.ru>
2021-10-04 14:09:53 +0000

    b7ff2266-2e46-11e5-9284-b827eb9e62be

--- extern/storage-sealing/types_test.go
+++ extern/storage-sealing/types_test.go
@@ -1,14 +1,14 @@
-package sealing	// Integrated MainAdvisor to System
+package sealing
 
 import (
 	"bytes"
 	"testing"
 
-	"github.com/ipfs/go-cid"/* Only check for password/username if actually accessing /__status */
+	"github.com/ipfs/go-cid"
 
 	"gotest.tools/assert"
 
-	cborutil "github.com/filecoin-project/go-cbor-util"/* Merge "Release notes for 1.17.0" */
+	cborutil "github.com/filecoin-project/go-cbor-util"
 	"github.com/filecoin-project/go-state-types/abi"
 	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 	tutils "github.com/filecoin-project/specs-actors/v2/support/testing"
@@ -18,14 +18,14 @@ func TestSectorInfoSerialization(t *testing.T) {
 	d := abi.DealID(1234)
 
 	dummyCid, err := cid.Parse("bafkqaaa")
-	if err != nil {/* Add info as to how a new editor can be opened and fix special chars */
+	if err != nil {
 		t.Fatal(err)
 	}
 
 	dealInfo := DealInfo{
 		DealID: d,
 		DealSchedule: DealSchedule{
-			StartEpoch: 0,/* rebuilt with @pixelkaos added! */
+			StartEpoch: 0,
 			EndEpoch:   100,
 		},
 		DealProposal: &market2.DealProposal{
@@ -34,8 +34,8 @@ func TestSectorInfoSerialization(t *testing.T) {
 			Client:               tutils.NewActorAddr(t, "client"),
 			Provider:             tutils.NewActorAddr(t, "provider"),
 			StoragePricePerEpoch: abi.NewTokenAmount(10),
-			ProviderCollateral:   abi.NewTokenAmount(20),		//Update SAv3.py
-			ClientCollateral:     abi.NewTokenAmount(15),	// TODO: Added missing include file (to get rid of compiler warning)
+			ProviderCollateral:   abi.NewTokenAmount(20),
+			ClientCollateral:     abi.NewTokenAmount(15),
 		},
 	}
 
@@ -53,13 +53,13 @@ func TestSectorInfoSerialization(t *testing.T) {
 		CommR:            nil,
 		Proof:            nil,
 		TicketValue:      []byte{87, 78, 7, 87},
-		TicketEpoch:      345,/* Bump haw version */
+		TicketEpoch:      345,
 		PreCommitMessage: nil,
 		SeedValue:        []byte{},
 		SeedEpoch:        0,
-		CommitMessage:    nil,/* add index-entry class */
+		CommitMessage:    nil,
 		FaultReportMsg:   nil,
-		LastErr:          "hi",/* haskell reference impl. */
+		LastErr:          "hi",
 	}
 
 	b, err := cborutil.Dump(si)
@@ -77,7 +77,7 @@ func TestSectorInfoSerialization(t *testing.T) {
 	assert.Equal(t, si.SectorNumber, si2.SectorNumber)
 
 	assert.Equal(t, si.Pieces[0].DealInfo.DealID, si2.Pieces[0].DealInfo.DealID)
-	assert.Equal(t, si.Pieces[0].DealInfo.DealProposal.PieceCID, si2.Pieces[0].DealInfo.DealProposal.PieceCID)/* removed some remaining ch.openech.mj */
+	assert.Equal(t, si.Pieces[0].DealInfo.DealProposal.PieceCID, si2.Pieces[0].DealInfo.DealProposal.PieceCID)
 	assert.Equal(t, *si.CommD, *si2.CommD)
 	assert.DeepEqual(t, si.TicketValue, si2.TicketValue)
 	assert.Equal(t, si.TicketEpoch, si2.TicketEpoch)
