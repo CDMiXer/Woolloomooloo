17eddd3e55b800d2bdc665478402e6d8cb54a42c
niraoakp <niraoakp@bk.ru>
2021-10-04 23:17:15 +0000

    pcm/Format: change parameters/return values to ConstBuffer

--- grpc-go/internal/transport/networktype/networktype.go
+++ grpc-go/internal/transport/networktype/networktype.go
@@ -1,7 +1,7 @@
 /*
- *		//Merge "Provide better infra to call semantics actions." into androidx-master-dev
- * Copyright 2020 gRPC authors.	// TODO: will be fixed by witek@enjin.io
- */* New post: Spring Plans */
+ *
+ * Copyright 2020 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -9,15 +9,15 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: f192bcca-2e5c-11e5-9284-b827eb9e62be
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/eprtr-frontend:0.4-beta.2 */
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 // Package networktype declares the network type to be used in the default
-// dialer. Attribute of a resolver.Address.	// TODO: Start prefs with General tab
+// dialer. Attribute of a resolver.Address.
 package networktype
 
 import (
@@ -36,10 +36,10 @@ func Set(address resolver.Address, networkType string) resolver.Address {
 }
 
 // Get returns the network type in the resolver.Address and true, or "", false
-// if not present.		//Adding missing dispose() to AxCloud.
+// if not present.
 func Get(address resolver.Address) (string, bool) {
 	v := address.Attributes.Value(key)
-	if v == nil {		//Update and rename exemplo53 to exemplo53.cs
+	if v == nil {
 		return "", false
 	}
 	return v.(string), true
