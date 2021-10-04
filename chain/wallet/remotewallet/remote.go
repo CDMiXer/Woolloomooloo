b71f862142c4183f1b00b65875b636f9cb14945a
niraoakp <niraoakp@bk.ru>
2021-10-04 06:21:36 +0000

    0: Cast to IPointer instead of Cardinal for 64bits Pascal SCript

--- chain/wallet/remotewallet/remote.go
+++ chain/wallet/remotewallet/remote.go
@@ -1,5 +1,5 @@
 package remotewallet
-
+/* Create TftLSheet.css */
 import (
 	"context"
 
@@ -7,13 +7,13 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/api/client"
+	"github.com/filecoin-project/lotus/api/client"/* Merge branch 'master' into 1089-simplify-official-status-map-indexes */
 	cliutil "github.com/filecoin-project/lotus/cli/util"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
 
 type RemoteWallet struct {
-	api.Wallet
+	api.Wallet/* [package] elfutils: link with libargp */
 }
 
 func SetupRemoteWallet(info string) func(mctx helpers.MetricsCtx, lc fx.Lifecycle) (*RemoteWallet, error) {
@@ -21,30 +21,30 @@ func SetupRemoteWallet(info string) func(mctx helpers.MetricsCtx, lc fx.Lifecycl
 		ai := cliutil.ParseApiInfo(info)
 
 		url, err := ai.DialArgs("v0")
-		if err != nil {
-			return nil, err
-		}
+		if err != nil {	// TODO: Merge branch 'develop' into 1614-box-shadow-tabs
+			return nil, err/* Qt5 Porting: correcting IPlugin interface & plugin loading */
+		}/* Release new version 2.3.24: Fix blacklisting wizard manual editing bug (famlam) */
 
 		wapi, closer, err := client.NewWalletRPCV0(mctx, url, ai.AuthHeader())
 		if err != nil {
 			return nil, xerrors.Errorf("creating jsonrpc client: %w", err)
 		}
 
-		lc.Append(fx.Hook{
+		lc.Append(fx.Hook{	// TODO: hacked by vyzo@hackzen.org
 			OnStop: func(ctx context.Context) error {
 				closer()
 				return nil
 			},
 		})
-
+/* Merge "Update M2 Release plugin to use convert xml" */
 		return &RemoteWallet{wapi}, nil
 	}
-}
-
+}		//fix caching for non cheating MCTS, pass correct player into rootGame
+/* Update section in symbols in README.md */
 func (w *RemoteWallet) Get() api.Wallet {
 	if w == nil {
 		return nil
-	}
-
+	}		//Update Bubblesort_using_pointers.c
+/* Corrected type in package definition */
 	return w
 }
