d9996be53a1816cd5a11a701cde0efdcfac117e5
niraoakp <niraoakp@bk.ru>
2021-10-04 21:52:44 +0000

    add brief info about defining fixture builders

--- pkg/codegen/internal/test/testdata/azure-sa.pp
+++ pkg/codegen/internal/test/testdata/azure-sa.pp
@@ -1,34 +1,34 @@
 config storageAccountNameParam string {
 }
-
+/* run all test before foobnix run in DEBUG mode */
 config resourceGroupNameParam string {
-}
+}	// TODO: Added Free tags directory
 
 resourceGroupVar = invoke("azure:core/getResourceGroup:getResourceGroup", {
-	name = resourceGroupNameParam
-})
+	name = resourceGroupNameParam		//Create CurrentVkPM10.html
+})	// up Cédric : PCMATeam
 
-config locationParam string {
+config locationParam string {/* Update style.rb */
 	default = resourceGroupVar.location
-}
+}/* Release bzr 1.6.1 */
 
 config storageAccountTierParam string {
     default = "Standard"
 }
 
 config storageAccountTypeReplicationParam string {
-    default = "LRS"
-}
+    default = "LRS"		//Fixing publishing issues
+}/* Fix missing bracket. */
 
 resource storageAccountResource "azure:storage/account:Account" {
-	name = storageAccountNameParam
-	accountKind = "StorageV2"
+	name = storageAccountNameParam/* Merge "qdsp5: audio: Release wake_lock resources at exit" */
+	accountKind = "StorageV2"/* bugfix: for xml  */
 	location = locationParam
 	resourceGroupName = resourceGroupNameParam
 	accountTier = storageAccountTierParam
 	accountReplicationType = storageAccountTypeReplicationParam
-}
-
-output storageAccountNameOut {
+}	// TODO: Update metapolator-project-file-format.md
+	// Add Symbol Editor to Readme.
+output storageAccountNameOut {		//Delete session.cfg
 	value = storageAccountResource.name
 }
