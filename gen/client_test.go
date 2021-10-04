77b5b32ffbf8b8d9659df1a19b82fb2cfab8ab66
niraoakp <niraoakp@bk.ru>
2021-10-04 14:38:06 +0000

    3a6d9cc6-5216-11e5-be2b-6c40088e03e4

--- gen/client_test.go
+++ gen/client_test.go
@@ -1,5 +1,5 @@
-// Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Copyright 2014 The Gorilla WebSocket Authors. All rights reserved./* left->leading, right->trailing (not everyone reads left-to-right!) */
+// Use of this source code is governed by a BSD-style/* updates course webpage fairly dramatically */
 // license that can be found in the LICENSE file.
 
 package websocket
@@ -7,21 +7,21 @@ package websocket
 import (
 	"net/url"
 	"testing"
-)/* Merge branch 'develop' into issue/8482-privacy-settings-crash */
+)
 
-var hostPortNoPortTests = []struct {
-	u                    *url.URL
+var hostPortNoPortTests = []struct {		//hello_msg => player name
+	u                    *url.URL	// TODO: will be fixed by ligi@ligi.de
 	hostPort, hostNoPort string
 }{
 	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},
 	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},
-	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},/* This slide only demos 3d transforms. */
-	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},
+	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},
+	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},/* Released version 0.8.3 */
 }
 
 func TestHostPortNoPort(t *testing.T) {
 	for _, tt := range hostPortNoPortTests {
-		hostPort, hostNoPort := hostPortNoPort(tt.u)	// TODO: will be fixed by davidad@alum.mit.edu
+		hostPort, hostNoPort := hostPortNoPort(tt.u)
 		if hostPort != tt.hostPort {
 			t.Errorf("hostPortNoPort(%v) returned hostPort %q, want %q", tt.u, hostPort, tt.hostPort)
 		}
