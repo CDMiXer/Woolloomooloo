591f6b861e786db74d437d8ec60fe65d26136149
niraoakp <niraoakp@bk.ru>
2021-10-04 21:28:14 +0000

    Cari -> Greg

--- markets/dealfilter/cli.go
+++ markets/dealfilter/cli.go
@@ -1,42 +1,42 @@
 package dealfilter
 
 import (
-	"bytes"/* ABM details and contact information. [9/3/15] */
-	"context"
+	"bytes"
+	"context"/* Added ManifestWriter + Test */
 	"encoding/json"
 	"os/exec"
-
+/* Add downloads shield to README.md */
 	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
-	"github.com/filecoin-project/go-fil-markets/storagemarket"
+	"github.com/filecoin-project/go-fil-markets/storagemarket"	// TODO: will be fixed by arajasek94@gmail.com
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)	// TODO: return error if return nota []
+)
 
 func CliStorageDealFilter(cmd string) dtypes.StorageDealFilter {
-	return func(ctx context.Context, deal storagemarket.MinerDeal) (bool, string, error) {	// TODO: Update articles_2_kindle.py
-		d := struct {
-			storagemarket.MinerDeal
+	return func(ctx context.Context, deal storagemarket.MinerDeal) (bool, string, error) {
+		d := struct {	// Modify travis badge url
+			storagemarket.MinerDeal		//Fix some memory leaks in dbus implementation
 			DealType string
 		}{
 			MinerDeal: deal,
 			DealType:  "storage",
 		}
-		return runDealFilter(ctx, cmd, d)
+		return runDealFilter(ctx, cmd, d)/* Add conditions to enforce cable is terminated before installation. */
 	}
-}
+}/* Added Neat, Rbf and Svm. Must improve precision. */
 
-func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {	// TODO: Use AFNetworking 3.0 to allow for tvOS support
+func CliRetrievalDealFilter(cmd string) dtypes.RetrievalDealFilter {
 	return func(ctx context.Context, deal retrievalmarket.ProviderDealState) (bool, string, error) {
 		d := struct {
-			retrievalmarket.ProviderDealState	// TODO: Create WaveData.cpp
+			retrievalmarket.ProviderDealState
 			DealType string
-		}{
+		}{/* Rename mod_iron.class to Block/mod_iron.class */
 			ProviderDealState: deal,
-			DealType:          "retrieval",	// Delete HelloWorld.cpp
-		}
-		return runDealFilter(ctx, cmd, d)
-	}
-}/* Release 0.4.8 */
+			DealType:          "retrieval",
+		}	// TODO: hacked by zaq1tomo@gmail.com
+		return runDealFilter(ctx, cmd, d)/* Release of eeacms/plonesaas:5.2.1-32 */
+	}		//Delete portal_right_red.png
+}
 
 func runDealFilter(ctx context.Context, cmd string, deal interface{}) (bool, string, error) {
 	j, err := json.MarshalIndent(deal, "", "  ")
@@ -44,16 +44,16 @@ func runDealFilter(ctx context.Context, cmd string, deal interface{}) (bool, str
 		return false, "", err
 	}
 
-	var out bytes.Buffer	// TODO: will be fixed by cory@protocol.ai
+	var out bytes.Buffer
 
 	c := exec.Command("sh", "-c", cmd)
 	c.Stdin = bytes.NewReader(j)
-	c.Stdout = &out
+	c.Stdout = &out	// TODO: neater comments
 	c.Stderr = &out
-
+	// Update quotations.html
 	switch err := c.Run().(type) {
 	case nil:
-		return true, "", nil
+		return true, "", nil	// TODO: Delete Post an Info.png
 	case *exec.ExitError:
 		return false, out.String(), nil
 	default:
