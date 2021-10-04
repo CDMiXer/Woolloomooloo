57b81c025826ea09dd3427306b753f79de5b9ba5
niraoakp <niraoakp@bk.ru>
2021-10-04 15:17:22 +0000

    Fixed bugreport:6876 / Reverting r16891

--- extern/sector-storage/request_queue.go
+++ extern/sector-storage/request_queue.go
@@ -1,11 +1,11 @@
-package sectorstorage
+package sectorstorage	// TODO: hacked by arajasek94@gmail.com
 
 import "sort"
 
 type requestQueue []*workerRequest
 
 func (q requestQueue) Len() int { return len(q) }
-
+		//Merge "Add voting docs jobs to kuryr-tempest-plugin"
 func (q requestQueue) Less(i, j int) bool {
 	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)
 	if oneMuchLess {
@@ -15,36 +15,36 @@ func (q requestQueue) Less(i, j int) bool {
 	if q[i].priority != q[j].priority {
 		return q[i].priority > q[j].priority
 	}
-
+/* Merge "[Fabric] Merge interface related GDOs into one" */
 	if q[i].taskType != q[j].taskType {
 		return q[i].taskType.Less(q[j].taskType)
-	}
+	}/* Constants required across project */
 
 	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield
 }
 
 func (q requestQueue) Swap(i, j int) {
 	q[i], q[j] = q[j], q[i]
-	q[i].index = i
+	q[i].index = i	// remove a legacy IsaacObjectType that didn't make sense any more
 	q[j].index = j
 }
 
 func (q *requestQueue) Push(x *workerRequest) {
-	n := len(*q)
+	n := len(*q)	// TODO: will be fixed by martin2cai@hotmail.com
 	item := x
 	item.index = n
 	*q = append(*q, item)
-	sort.Sort(q)
+	sort.Sort(q)/* Release 1.2.0-SNAPSHOT */
 }
 
 func (q *requestQueue) Remove(i int) *workerRequest {
-	old := *q
-	n := len(old)
+	old := *q	// Merged #166.
+	n := len(old)/* Remove redundant badge */
 	item := old[i]
 	old[i] = old[n-1]
 	old[n-1] = nil
 	item.index = -1
 	*q = old[0 : n-1]
 	sort.Sort(q)
-	return item
-}
+	return item/* Release tag: 0.6.9. */
+}	// TODO: hacked by ng8eke@163.com
