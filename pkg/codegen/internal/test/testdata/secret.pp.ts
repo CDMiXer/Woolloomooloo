f1be122165ec6513c62c32747c280315a9d226b7
niraoakp <niraoakp@bk.ru>
2021-10-04 05:39:21 +0000

    Merge "[INTERNAL][FIX] sap.m.ResponsivePopover: Fixed initial focus"

--- pkg/codegen/internal/test/testdata/secret.pp.ts
+++ pkg/codegen/internal/test/testdata/secret.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-
+		//Cover auth LDAP acr_values
 const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});
