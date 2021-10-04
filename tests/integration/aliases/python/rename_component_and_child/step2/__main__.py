b6bdd717d5a50e3d362acb7208857c38fcfa8290
niraoakp <niraoakp@bk.ru>
2021-10-04 22:30:02 +0000

    JENKINS-51478 Retrieve and apply proxy configuration

--- tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
@@ -1,10 +1,10 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Jellyfish-2.3.0: fix checksum */
+		//Update composer.json to autoload correctly
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-/* move supported table to querying section */
+/* Create teapot.js */
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):/* Using batch mode for deployment */
-        super().__init__("my:module:Resource", name, None, opts)
+    def __init__(self, name, opts=None):		//Merge "Fix synthetic calls in versionedparcelable module" into pi-androidx-dev
+        super().__init__("my:module:Resource", name, None, opts)/* Release of eeacms/forests-frontend:1.8-beta.11 */
 
 # Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive(ComponentResource):
@@ -12,7 +12,7 @@ class ComponentFive(ComponentResource):
         super().__init__("my:module:ComponentFive", name, None, opts)
         res1 = Resource1("otherchildrenamed", ResourceOptions(
             parent=self,
-            aliases=[Alias(name="otherchild", parent=self)]))
+            aliases=[Alias(name="otherchild", parent=self)]))	// TODO: Support multiple BAM files in Pileup records;handle basequal internally
 
-comp5 = ComponentFive("newcomp5", ResourceOptions(/* Fixed rst errors */
+comp5 = ComponentFive("newcomp5", ResourceOptions(		//new style cm XML
     aliases=[Alias(name="comp5")]))
