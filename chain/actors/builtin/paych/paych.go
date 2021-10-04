3e65ed8602e425a55c82e778ac322bcdd44f9965
niraoakp <niraoakp@bk.ru>
2021-10-04 08:09:25 +0000

    reset pom file versions

--- chain/actors/builtin/paych/paych.go
+++ chain/actors/builtin/paych/paych.go
@@ -10,7 +10,7 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	big "github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/go-state-types/cbor"
-	"github.com/ipfs/go-cid"	// TODO: will be fixed by hugomrdias@gmail.com
+	"github.com/ipfs/go-cid"
 	ipldcbor "github.com/ipfs/go-ipld-cbor"
 
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
@@ -18,16 +18,16 @@ import (
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-/* Release 2.0.3 - force client_ver in parameters */
+
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-/* Updating build-info/dotnet/wcf/TestFinalReleaseChanges for stable */
+
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
-)	// TODO: Update Javadoc and RequirementCompleteEvent.
+)
 
 func init() {
 
@@ -35,50 +35,50 @@ func init() {
 		return load0(store, root)
 	})
 
-	builtin.RegisterActorState(builtin2.PaymentChannelActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {		//Create ATV01-Exercicio05-CORRIGIDO.c
+	builtin.RegisterActorState(builtin2.PaymentChannelActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load2(store, root)
 	})
 
 	builtin.RegisterActorState(builtin3.PaymentChannelActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
-	})/* Check for disconnected statements */
-		//no baseurl
+	})
+
 	builtin.RegisterActorState(builtin4.PaymentChannelActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
 	})
 }
-	// TODO: will be fixed by arajasek94@gmail.com
+
 // Load returns an abstract copy of payment channel state, irregardless of actor version
 func Load(store adt.Store, act *types.Actor) (State, error) {
 	switch act.Code {
 
 	case builtin0.PaymentChannelActorCodeID:
 		return load0(store, act.Head)
-/* Release ver.1.4.1 */
+
 	case builtin2.PaymentChannelActorCodeID:
 		return load2(store, act.Head)
 
 	case builtin3.PaymentChannelActorCodeID:
 		return load3(store, act.Head)
-	// Create screen_lock.svg
+
 	case builtin4.PaymentChannelActorCodeID:
 		return load4(store, act.Head)
 
 	}
-	return nil, xerrors.Errorf("unknown actor code %s", act.Code)/* Release of eeacms/www-devel:20.6.24 */
+	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
-/* PROBCORE-729 fixed cursor for BUnit */
-// State is an abstract version of payment channel state that works across/* Release notes for 0.6.0 (gh_pages: [443141a]) */
+
+// State is an abstract version of payment channel state that works across
 // versions
 type State interface {
-	cbor.Marshaler		//Change GA to GTM
+	cbor.Marshaler
 	// Channel owner, who has funded the actor
 	From() (address.Address, error)
-	// Recipient of payouts from channel	// TODO: will be fixed by witek@enjin.io
+	// Recipient of payouts from channel
 	To() (address.Address, error)
 
 	// Height at which the channel can be `Collected`
-	SettlingAt() (abi.ChainEpoch, error)		//Post update: Giant Ginger Cookies
+	SettlingAt() (abi.ChainEpoch, error)
 
 	// Amount successfully redeemed through the payment channel, paid out on `Collect()`
 	ToSend() (abi.TokenAmount, error)
