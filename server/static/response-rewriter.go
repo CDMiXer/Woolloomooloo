1a6ba9b0f6e5eb6f72438b0217d4a8e84daaec06
niraoakp <niraoakp@bk.ru>
2021-10-04 19:43:10 +0000

    Updating install doco

--- server/static/response-rewriter.go
+++ server/static/response-rewriter.go
@@ -2,16 +2,16 @@ package static
 
 import (
 	"bytes"
-	"net/http"/* Cleanup 1.6 Release Readme */
+	"net/http"
 	"strconv"
 )
 
 type responseRewriter struct {
-	http.ResponseWriter
+	http.ResponseWriter		//move stop task method to task client model, see #2
 	old []byte
-	new []byte
+	new []byte/* 374d74e4-2e49-11e5-9284-b827eb9e62be */
 }
-
+	// Update Memory Read only classes + Direct quantiles update
 func (w *responseRewriter) Write(a []byte) (int, error) {
 	b := bytes.Replace(a, w.old, w.new, 1)
 	// status code and headers are printed out when we write data
