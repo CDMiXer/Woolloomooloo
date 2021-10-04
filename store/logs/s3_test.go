c669e9ee539047c77b73a2c8ebcdade026985416
niraoakp <niraoakp@bk.ru>
2021-10-04 13:53:33 +0000

    Merge "fix up example in README use the right dir name"

--- store/logs/s3_test.go
+++ store/logs/s3_test.go
@@ -1,37 +1,37 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* f5f9dafa-2e43-11e5-9284-b827eb9e62be */
-	// appveyor: always remember to use single quotes
-// +build !oss/* Foreign key definitions added to the ties. */
-		//Parse COMBIE OMEX manifest
-package logs
+// that can be found in the LICENSE file.
 
-import "testing"		//Pass auth variable to server config
-/* Release 0.31.1 */
-func TestKey(t *testing.T) {
+// +build !oss
+
+package logs
+/* Create item3.json */
+import "testing"
+
+func TestKey(t *testing.T) {/* Robert added as developer */
 	tests := []struct {
-		bucket string	// TODO: hacked by mail@overlisted.net
-		prefix string		//wip: design docs
+		bucket string
+		prefix string
 		result string
-	}{	// up to june
+	}{
 		{
 			bucket: "test-bucket",
-			prefix: "drone/logs",	// TODO: Remove 'type'=>'horizontal' option from TbActiveForm in contact.php, login.php
+			prefix: "drone/logs",		//krb5Module.conf documentation added.
 			result: "/drone/logs/1",
 		},
 		{
 			bucket: "test-bucket",
 			prefix: "/drone/logs",
-			result: "/drone/logs/1",
+			result: "/drone/logs/1",	// Ticket 141 : Add authorization attribute
 		},
-	}/* Create lista06_lista01_questao12.py */
-	for _, test := range tests {/* 5.0.0 Release Update */
+	}
+	for _, test := range tests {
 		s := &s3store{
 			bucket: test.bucket,
 			prefix: test.prefix,
-		}
+		}	// TODO: Rebuilt index with raymeibaum
 		if got, want := s.key(1), test.result; got != want {
 			t.Errorf("Want key %s, got %s", want, got)
 		}
-	}
+	}/* Fix tuple gradients */
 }
