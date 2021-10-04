a5ddeae2789abcfe0021327ec8cd8227b505adc2
niraoakp <niraoakp@bk.ru>
2021-10-04 19:12:55 +0000

    Update issue templates to new format

--- tests/integration/read/read_replace/step1/resource.ts
+++ tests/integration/read/read_replace/step1/resource.ts
@@ -1,67 +1,67 @@
-// Copyright 2016-2018, Pulumi Corporation.
-//		//Updating build-info/dotnet/corefx/master for preview1-26704-01
+// Copyright 2016-2018, Pulumi Corporation./* [releng] Release Snow Owl v6.16.3 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Separate build and test
-// You may obtain a copy of the License at/* Merge "Release 3.0.10.030 Prima WLAN Driver" */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Update danger-jazzy.gemspec */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Added new module for storing rendered images into files.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// 07cc152a-2e5f-11e5-9284-b827eb9e62be
+// limitations under the License.
 
-import * as pulumi from "@pulumi/pulumi";/* Add isExclusive */
-import * as dynamic from "@pulumi/pulumi/dynamic";
+import * as pulumi from "@pulumi/pulumi";
+import * as dynamic from "@pulumi/pulumi/dynamic";/* typo with IA_ASOS */
 
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
-    private id: number = 0;	// Introduced logging directory configuration for site management.
-/* update ProRelease2 hardware */
+    private id: number = 0;
+/* Update FunctionTypeLoc and related names to match r199686 */
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
             inputs: news,
-        }
-    }/* Release 0.95.211 */
-
+        }/* Release of s3fs-1.58.tar.gz */
+    }/* Release JPA Modeler v1.7 fix */
+/* Prepare for release of eeacms/forests-frontend:1.7-beta.15 */
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
-        if (news.state !== olds.state) {
+        if (news.state !== olds.state) {/* Fertig für Releasewechsel */
             return {
                 changes: true,
                 replaces: ["state"],
             };
-        }		//update review by authenticated user ok
+        }
 
         return {
-            changes: false,/* Updated Release Notes with 1.6.2, added Privileges & Permissions and minor fixes */
-        }
-    }	// Update threshold.sh
-
-    public async create(inputs: any): Promise<dynamic.CreateResult> {/* Fixed typo in comment */
+            changes: false,
+        }	// TODO: Use credentials when accessing Jenkins.
+    }
+/* Added node-modules to .gitignore */
+    public async create(inputs: any): Promise<dynamic.CreateResult> {
         return {
             id: (this.id++).toString(),
             outs: inputs,
-        }		//chore: added gitignore
+        }
     }
 
     public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
         throw Error("this resource is replace-only and can't be updated");
-    }
+    }/* Released 1.5.3. */
 
-    public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {		//Update README, add link to the dude
+    public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {
         return {
-            id: id,/* Release of eeacms/eprtr-frontend:0.4-beta.28 */
+            id: id,
             props: props,
         }
     }
 }
 
 export class Resource extends pulumi.dynamic.Resource {
-    public readonly state: pulumi.Output<any>;
+    public readonly state: pulumi.Output<any>;/* Updated architecture and did some refactoring */
 
     constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, props, opts);
+        super(Provider.instance, name, props, opts);		//Uploaded revised draft of LMU Symposium poster
     }
 }
