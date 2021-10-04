1d236b00063a253b4db16e84e9908dd290cc7536
niraoakp <niraoakp@bk.ru>
2021-10-04 22:26:57 +0000

    Delete bus.go

--- operator/runner/machine/machine_test.go
+++ operator/runner/machine/machine_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Upgrade circle ci Java to version `oraclejdk8`
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Adapt jsunpack to name changes in debug.py */
 // +build !oss
 
 package machine
 
-import (
+import (		//Merged branch release/0.5.0.1 into develop
 	"testing"
 )
 
 func TestLoad(t *testing.T) {
 	t.Skip()
-}		//Create RLarissa.txt
+}
