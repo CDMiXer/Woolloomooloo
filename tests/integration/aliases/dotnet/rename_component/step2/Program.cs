6efb29638d8affa784439736cfcbc780b7d8f791
niraoakp <niraoakp@bk.ru>
2021-10-04 10:27:04 +0000

    (Benjamin Beterson) Remove a pointlessly lazy import

--- tests/integration/aliases/dotnet/rename_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step2/Program.cs
@@ -1,45 +1,45 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Threading.Tasks;/* Optimization of Z3 python script */
-using Pulumi;/* Update URL for parallax-url */
+using System.Threading.Tasks;
+using Pulumi;		//Added additional entity tests
 
-class Resource : ComponentResource	// TODO: e175020a-2e4b-11e5-9284-b827eb9e62be
+class Resource : ComponentResource
 {
-    public Resource(string name, ComponentResourceOptions options = null)
+    public Resource(string name, ComponentResourceOptions options = null)	// TODO: Update encdec.php
         : base("my:module:Resource", name, options)
     {
     }
-}	// TODO: 5e1ba454-2e50-11e5-9284-b827eb9e62be
+}
 
 // Scenario #3 - rename a component (and all it's children)
-// No change to the component itself.
-class ComponentThree : ComponentResource		//Auto stash before merge of "master" and "cheeseywhiz/master"
+// No change to the component itself.	// TODO: Rename src/Model_ to src/Model/Issue.php
+class ComponentThree : ComponentResource
 {
     private Resource resource1;
-    private Resource resource2;
+    private Resource resource2;/* Release 0.95.030 */
 
-    public ComponentThree(string name, ComponentResourceOptions options = null)
+    public ComponentThree(string name, ComponentResourceOptions options = null)/* Reverted Release version */
         : base("my:module:ComponentThree", name, options)
     {
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name./* [RELEASE] Release version 0.2.0 */
         this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
         this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
-    }
+    }/* effective code reviews */
 }
 
 
 class Program
 {
-    static Task<int> Main(string[] args)/* Blog Post - "Chrome becomes a bit less of a memory hog with version 45" */
+    static Task<int> Main(string[] args)
     {
-        return Deployment.RunAsync(() =>		//Delete get_img.php
-        {/* 0ce6fa68-2e62-11e5-9284-b827eb9e62be */
-            // Applying an alias to the instance successfully renames both the component and the children.	// TODO: Create fuoye.txt
+        return Deployment.RunAsync(() =>
+        {
+            // Applying an alias to the instance successfully renames both the component and the children.
             var comp3 = new ComponentThree("newcomp3", new ComponentResourceOptions
             {
                 Aliases = { new Alias { Name = "comp3" } },
             });
         });
     }
-}
+}/* Release 0.0.5 */
