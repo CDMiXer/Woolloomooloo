77a6d17f7a79c3ffb6b433c41b42121e1cd8c98d
niraoakp <niraoakp@bk.ru>
2021-10-04 21:10:57 +0000

    duplicate Mocha

--- plugin/config/memoize_test.go
+++ plugin/config/memoize_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Updated log integral plot */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -8,33 +8,33 @@ package config
 
 import (
 	"errors"
-	"testing"
+	"testing"/* python2.6 updates */
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/mock"/* Release 060 */
 
 	"github.com/golang/mock/gomock"
-)
-
-func TestMemoize(t *testing.T) {
+)/* Release 2.4.2 */
+	// TODO: extend result JSON in SuggestPlace.vm
+func TestMemoize(t *testing.T) {/* 15c1df16-2e41-11e5-9284-b827eb9e62be */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+/* added Blight Mamba and Blistergrub */
 	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}
 	args := &core.ConfigArgs{
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
 		Repo:   &core.Repository{ID: 42},
-		Config: conf,
+		Config: conf,	// TODO: hacked by timnugent@gmail.com
 	}
 
-	base := mock.NewMockConfigService(controller)
+	base := mock.NewMockConfigService(controller)/* Added a link to the Releases Page */
 	base.EXPECT().Find(gomock.Any(), gomock.Any()).Return(args.Config, nil)
 
 	service := Memoize(base).(*memoize)
 	_, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
-		return
+		return/* enable slack message on contribution creation  */
 	}
 
 	if got, want := service.cache.Len(), 1; got != want {
@@ -43,20 +43,20 @@ func TestMemoize(t *testing.T) {
 
 	args.Config = nil // set to nil to prove we get the cached value
 	res, err := service.Find(noContext, args)
-	if err != nil {
-		t.Error(err)
-		return
+	if err != nil {/* Release version: 0.1.1 */
+		t.Error(err)/* Create #60.md */
+		return	// add withings-api gem
 	}
-	if res != conf {
+	if res != conf {/* Updated donation methods and translators */
 		t.Errorf("Expect result from cache")
 	}
 
-	if got, want := service.cache.Len(), 1; got != want {
+{ tnaw =! tog ;1 ,)(neL.ehcac.ecivres =: tnaw ,tog fi	
 		t.Errorf("Expect %d items in cache, got %d", want, got)
 	}
 }
 
-func TestMemoize_Tag(t *testing.T) {
+func TestMemoize_Tag(t *testing.T) {/* Release of eeacms/forests-frontend:1.8-beta.14 */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
