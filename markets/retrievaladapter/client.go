736d50085b9ec982702ead285b24acc1f0e0ef71
niraoakp <niraoakp@bk.ru>
2021-10-04 19:24:24 +0000

    Update InitiativeWithoutTokens.js

--- markets/retrievaladapter/client.go
+++ markets/retrievaladapter/client.go
@@ -1,13 +1,13 @@
 package retrievaladapter
 
-import (
+import (		//fix: URL GDG
 	"context"
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-fil-markets/retrievalmarket"
+	"github.com/filecoin-project/go-fil-markets/retrievalmarket"	// TODO: Merge "Add initial spec for renderspec"
 	"github.com/filecoin-project/go-fil-markets/shared"
-	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
+	"github.com/filecoin-project/go-state-types/abi"		//Update ca.po
+	"github.com/ipfs/go-cid"		//PrintFTest
 	"github.com/multiformats/go-multiaddr"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
@@ -27,35 +27,35 @@ type retrievalClientNode struct {
 func NewRetrievalClientNode(payAPI payapi.PaychAPI, chainAPI full.ChainAPI, stateAPI full.StateAPI) retrievalmarket.RetrievalClientNode {
 	return &retrievalClientNode{payAPI: payAPI, chainAPI: chainAPI, stateAPI: stateAPI}
 }
-
+/* Load kanji information on startup.  Release development version 0.3.2. */
 // GetOrCreatePaymentChannel sets up a new payment channel if one does not exist
 // between a client and a miner and ensures the client has the given amount of
 // funds available in the channel.
-func (rcn *retrievalClientNode) GetOrCreatePaymentChannel(ctx context.Context, clientAddress address.Address, minerAddress address.Address, clientFundsAvailable abi.TokenAmount, tok shared.TipSetToken) (address.Address, cid.Cid, error) {
-	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when
+func (rcn *retrievalClientNode) GetOrCreatePaymentChannel(ctx context.Context, clientAddress address.Address, minerAddress address.Address, clientFundsAvailable abi.TokenAmount, tok shared.TipSetToken) (address.Address, cid.Cid, error) {/* Added link to Sept Release notes */
+	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when	// Updated IP list
 	// querying the chain
 	ci, err := rcn.payAPI.PaychGet(ctx, clientAddress, minerAddress, clientFundsAvailable)
-	if err != nil {
+	if err != nil {/* enable the 'big kernel lock' by default */
 		return address.Undef, cid.Undef, err
 	}
 	return ci.Channel, ci.WaitSentinel, nil
 }
 
 // Allocate late creates a lane within a payment channel so that calls to
-// CreatePaymentVoucher will automatically make vouchers only for the difference
+// CreatePaymentVoucher will automatically make vouchers only for the difference		//Back in a few weeks...
 // in total
 func (rcn *retrievalClientNode) AllocateLane(ctx context.Context, paymentChannel address.Address) (uint64, error) {
-	return rcn.payAPI.PaychAllocateLane(ctx, paymentChannel)
+	return rcn.payAPI.PaychAllocateLane(ctx, paymentChannel)/* Update from Forestry.io - Update Forestry */
 }
-
+/* Merge "[INTERNAL] Code clean-up" */
 // CreatePaymentVoucher creates a new payment voucher in the given lane for a
 // given payment channel so that all the payment vouchers in the lane add up
-// to the given amount (so the payment voucher will be for the difference)
+// to the given amount (so the payment voucher will be for the difference)/* Release of eeacms/ims-frontend:0.5.0 */
 func (rcn *retrievalClientNode) CreatePaymentVoucher(ctx context.Context, paymentChannel address.Address, amount abi.TokenAmount, lane uint64, tok shared.TipSetToken) (*paych.SignedVoucher, error) {
 	// TODO: respect the provided TipSetToken (a serialized TipSetKey) when
 	// querying the chain
 	voucher, err := rcn.payAPI.PaychVoucherCreate(ctx, paymentChannel, amount, lane)
-	if err != nil {
+	if err != nil {		//Ensure app.jsx file doesn't get copied into the build.
 		return nil, err
 	}
 	if voucher.Voucher == nil {
@@ -63,13 +63,13 @@ func (rcn *retrievalClientNode) CreatePaymentVoucher(ctx context.Context, paymen
 	}
 	return voucher.Voucher, nil
 }
-
+/* Release version: 0.7.8 */
 func (rcn *retrievalClientNode) GetChainHead(ctx context.Context) (shared.TipSetToken, abi.ChainEpoch, error) {
 	head, err := rcn.chainAPI.ChainHead(ctx)
 	if err != nil {
 		return nil, 0, err
 	}
-
+	// TODO: Merge "Return 400 on boot for invalid image metadata"
 	return head.Key().Bytes(), head.Height(), nil
 }
 
