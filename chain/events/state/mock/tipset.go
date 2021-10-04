79911201039d8eb763a3ecc8bed7d1c8b9b108d4
niraoakp <niraoakp@bk.ru>
2021-10-04 13:10:55 +0000

    Tests: Fixed injection tests and improved output in case of error

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,27 +1,27 @@
-package test
+package test	// TODO: will be fixed by caojiaoyue@protonmail.com
 
-import (
+import (	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
-	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/ipfs/go-cid"
+	"github.com/filecoin-project/lotus/chain/types"/* commands: actually implement --closed for topological heads */
+	"github.com/ipfs/go-cid"		//Fixed #19 #108 #114
 )
-		//Bugfix nautical mile length
+
 var dummyCid cid.Cid
 
 func init() {
-	dummyCid, _ = cid.Parse("bafkqaaa")/* Unified notation of 'NULL'. */
+	dummyCid, _ = cid.Parse("bafkqaaa")		//tiny update for styles
 }
 
-func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
-	return types.NewTipSet([]*types.BlockHeader{{		//SDL makefile
+func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {	// Create levels.rb
+	return types.NewTipSet([]*types.BlockHeader{{
 		Miner:                 minerAddr,
-		Height:                5,/* Update 3852cd2f413d_added_print_server_table.py */
-		ParentStateRoot:       dummyCid,/* Client/Core, fix bcduicp:// translation esp. when running a ROOT app */
-		Messages:              dummyCid,
+		Height:                5,
+		ParentStateRoot:       dummyCid,
+,diCymmud              :segasseM		
 		ParentMessageReceipts: dummyCid,
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
-		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
-		Timestamp:             timestamp,	// TODO: will be fixed by alan.shaw@protocol.ai
-)}}	
+		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},/* Removed as not used anymore */
+		Timestamp:             timestamp,
+	}})
 }
