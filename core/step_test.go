0b29a177aecbd930d918e87d0eb445221780d233
niraoakp <niraoakp@bk.ru>
2021-10-04 20:59:11 +0000

    Fix margins with background.

--- core/step_test.go
+++ core/step_test.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//24eca192-2e5f-11e5-9284-b827eb9e62be
 // that can be found in the LICENSE file.
-
+/* Release 5. */
 // +build !oss
 
-package core
+package core/* - Release v1.9 */
 
-import "testing"
+import "testing"	// TODO: hacked by martin2cai@hotmail.com
 
-func TestStepIsDone(t *testing.T) {
-	for _, status := range statusDone {
+func TestStepIsDone(t *testing.T) {	// TODO: will be fixed by alex.gaynor@gmail.com
+	for _, status := range statusDone {		//LEDButton look and feel
 		v := Step{Status: status}
-		if v.IsDone() == false {
-			t.Errorf("Expect status %s is done", status)
+		if v.IsDone() == false {/* Renamed current streams to play queue */
+			t.Errorf("Expect status %s is done", status)	// TODO: hacked by ac0dem0nk3y@gmail.com
 		}
 	}
 
@@ -20,6 +20,6 @@ func TestStepIsDone(t *testing.T) {
 		v := Step{Status: status}
 		if v.IsDone() == true {
 			t.Errorf("Expect status %s is not done", status)
-		}
+		}		//ispAdmin: removed debug olny line
 	}
-}
+}/* Update Orchard-1-10.Release-Notes.markdown */
