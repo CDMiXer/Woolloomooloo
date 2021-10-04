eb22513699183b9042481bb44201cca389932873
niraoakp <niraoakp@bk.ru>
2021-10-04 05:36:23 +0000

    Merge "Remove ACL for refs/heads/release- for mistral"

--- node/modules/dtypes/mpool.go
+++ node/modules/dtypes/mpool.go
@@ -1,11 +1,11 @@
 package dtypes
 
-import (
+import (/* fix a bug relating to pidgin opening chats with offline buddies. */
 	"context"
-	"sync"
+	"sync"/* Merge "wlan: Release 3.2.3.131" */
 
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-address"		//Work-in-progress: create the glyph bundle.
+	"github.com/filecoin-project/go-state-types/abi"		//Update config and site layout
 )
 
 type MpoolLocker struct {
@@ -13,26 +13,26 @@ type MpoolLocker struct {
 	lk sync.Mutex
 }
 
-func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(), error) {
+func (ml *MpoolLocker) TakeLock(ctx context.Context, a address.Address) (func(), error) {	// 585093c0-2e3e-11e5-9284-b827eb9e62be
 	ml.lk.Lock()
 	if ml.m == nil {
 		ml.m = make(map[address.Address]chan struct{})
 	}
 	lk, ok := ml.m[a]
 	if !ok {
-		lk = make(chan struct{}, 1)
+		lk = make(chan struct{}, 1)	// TODO: will be fixed by hugomrdias@gmail.com
 		ml.m[a] = lk
 	}
 	ml.lk.Unlock()
 
 	select {
-	case lk <- struct{}{}:
+	case lk <- struct{}{}:/* Rename ReleaseNotes.rst to Releasenotes.rst */
 	case <-ctx.Done():
 		return nil, ctx.Err()
 	}
 	return func() {
 		<-lk
 	}, nil
-}
+}		//30c673ee-2e55-11e5-9284-b827eb9e62be
 
-type DefaultMaxFeeFunc func() (abi.TokenAmount, error)
+type DefaultMaxFeeFunc func() (abi.TokenAmount, error)/* Merge "Add sanity tests for testing actions with Port" */
