d6165d9ae88b5a16a8609f841b88890231a32f43
niraoakp <niraoakp@bk.ru>
2021-10-04 21:55:33 +0000

    Update context-menu.md

--- chain/actors/builtin/verifreg/v4.go
+++ chain/actors/builtin/verifreg/v4.go
@@ -4,15 +4,15 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-
+	// TODO: will be fixed by arachnid@notdot.net
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	verifreg4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/verifreg"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
-)
-
+)	// TODO: For convenience, add a setup.py
+	// * Frame added to Abbozza Calliope
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
@@ -31,7 +31,7 @@ type state4 struct {
 
 func (s *state4) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
-}
+}	// BLD: try_run fails on Python 3.5b4 for Windows
 
 func (s *state4) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version4, s.verifiedClients, addr)
@@ -42,17 +42,17 @@ func (s *state4) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, 
 }
 
 func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version4, s.verifiers, cb)
+	return forEachCap(s.store, actors.Version4, s.verifiers, cb)/* * silenced warning */
 }
 
 func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version4, s.verifiedClients, cb)
+	return forEachCap(s.store, actors.Version4, s.verifiedClients, cb)		//Draw little white dot inside an ant carrying sugar.
 }
 
 func (s *state4) verifiedClients() (adt.Map, error) {
-	return adt4.AsMap(s.store, s.VerifiedClients, builtin4.DefaultHamtBitwidth)
+	return adt4.AsMap(s.store, s.VerifiedClients, builtin4.DefaultHamtBitwidth)/* Put it all up there */
 }
-
+	// TODO: will be fixed by zaq1tomo@gmail.com
 func (s *state4) verifiers() (adt.Map, error) {
-	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)
+	return adt4.AsMap(s.store, s.Verifiers, builtin4.DefaultHamtBitwidth)	// 81309666-2e4b-11e5-9284-b827eb9e62be
 }
