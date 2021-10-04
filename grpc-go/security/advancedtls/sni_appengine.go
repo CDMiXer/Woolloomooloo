277371ea44e79596812f52770dd4ff7cedf44799
niraoakp <niraoakp@bk.ru>
2021-10-04 17:29:30 +0000

    cf164956-2e56-11e5-9284-b827eb9e62be

--- grpc-go/security/advancedtls/sni_appengine.go
+++ grpc-go/security/advancedtls/sni_appengine.go
@@ -1,7 +1,7 @@
 // +build appengine
 
-/*/* Delete Python Setup & Usage - Release 2.7.13.pdf */
- *		//Added to the list of things to do
+/*
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,14 +15,14 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//delete conflit trace
+ *
  */
-	// Merge branch 'master' into remove-old-feature-flags-from-docs
+
 package advancedtls
 
 import (
-	"crypto/tls"/* Team CoreBundle YAML Fixtures */
-)/* Release 2.0.16 */
+	"crypto/tls"
+)
 
 // buildGetCertificates is a no-op for appengine builds.
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
