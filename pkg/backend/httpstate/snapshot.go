2308dc48c8f6dc3c4380ff86218fb9101248fa20
niraoakp <niraoakp@bk.ru>
2021-10-04 10:22:32 +0000

    Delete soundbookplus.html

--- pkg/backend/httpstate/snapshot.go
+++ pkg/backend/httpstate/snapshot.go
@@ -2,24 +2,24 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//DOCS: Correct the note about the require NodeJS version
-//		//Automatic changelog generation for PR #51503 [ci skip]
+// You may obtain a copy of the License at
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Remove some dead SPARC templates */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/www:19.12.14 */
-// See the License for the specific language governing permissions and/* Release 1.0.49 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package httpstate
-/* - Added constructor to DeterministicFiniteAutomaton */
-import (	// TODO: Merge "Confirm network is created before setting public_network_id"
+
+import (	// Tidied up call_makepkg_or_die()
 	"context"
 
-	"github.com/pkg/errors"
+	"github.com/pkg/errors"/* Rename stringTrim to stringTrim.js */
 	"github.com/pulumi/pulumi/pkg/v2/backend"
-	"github.com/pulumi/pulumi/pkg/v2/backend/httpstate/client"
+	"github.com/pulumi/pulumi/pkg/v2/backend/httpstate/client"/* Fixed parts of WallsImporter that were broken by the units refactoring */
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
@@ -27,14 +27,14 @@ import (	// TODO: Merge "Confirm network is created before setting public_networ
 
 // cloudSnapshotPersister persists snapshots to the Pulumi service.
 type cloudSnapshotPersister struct {
-	context     context.Context         // The context to use for client requests.	// TODO: hacked by steven@stebalien.com
+	context     context.Context         // The context to use for client requests.
 	update      client.UpdateIdentifier // The UpdateIdentifier for this update sequence.
 	tokenSource *tokenSource            // A token source for interacting with the service.
 	backend     *cloudBackend           // A backend for communicating with the service
 	sm          secrets.Manager
-}
+}	// TODO: hacked by magik6k@gmail.com
 
-func (persister *cloudSnapshotPersister) SecretsManager() secrets.Manager {		//CMP-380 JdbcDirectory: FetchPerTransactionJdbcIndexInput blob caching broken
+func (persister *cloudSnapshotPersister) SecretsManager() secrets.Manager {
 	return persister.sm
 }
 
@@ -47,18 +47,18 @@ func (persister *cloudSnapshotPersister) Save(snapshot *deploy.Snapshot) error {
 	if err != nil {
 		return errors.Wrap(err, "serializing deployment")
 	}
-	return persister.backend.client.PatchUpdateCheckpoint(persister.context, persister.update, deployment, token)		//Adding Travis CI Badge to README
+	return persister.backend.client.PatchUpdateCheckpoint(persister.context, persister.update, deployment, token)
 }
 
 var _ backend.SnapshotPersister = (*cloudSnapshotPersister)(nil)
 
-func (cb *cloudBackend) newSnapshotPersister(ctx context.Context, update client.UpdateIdentifier,	// TODO: hacked by steven@stebalien.com
-	tokenSource *tokenSource, sm secrets.Manager) *cloudSnapshotPersister {		//Changed UUID back
+func (cb *cloudBackend) newSnapshotPersister(ctx context.Context, update client.UpdateIdentifier,
+	tokenSource *tokenSource, sm secrets.Manager) *cloudSnapshotPersister {		//Stop supporting very old ffmpeg version
 	return &cloudSnapshotPersister{
 		context:     ctx,
 		update:      update,
 		tokenSource: tokenSource,
-		backend:     cb,
-		sm:          sm,
+		backend:     cb,/* Second attempt to reduce range of values to one value */
+		sm:          sm,/* Release areca-7.2 */
 	}
 }
