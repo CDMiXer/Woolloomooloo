7b87775114af4054e97ab37c06ec09b9c8b96f71
niraoakp <niraoakp@bk.ru>
2021-10-04 10:37:04 +0000

    - [ZBX-3244] fixed dropdown changes its style when navigating menu

--- chain/actors/builtin/init/v4.go
+++ chain/actors/builtin/init/v4.go
@@ -3,55 +3,55 @@ package init
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"/* Release for v25.1.0. */
+	"github.com/ipfs/go-cid"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"		//Update process_api.php
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-/* Update README for newer gradle versions, closes #3 */
-	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
-	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"	// TODO: will be fixed by cory@protocol.ai
-)	// added avslutning
 
-var _ State = (*state4)(nil)		//Delete pfsense-backup-github.iml
+	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
+	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
+)
+
+var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}		//Fix include order
+	}
 	return &out, nil
 }
-/* Merge "Move Exifinterface to beta for July 2nd Release" into androidx-master-dev */
-type state4 struct {/* Removed irrelevant line. */
+
+type state4 struct {
 	init4.State
 	store adt.Store
 }
-	// TODO: add receive functions for back orders with tests
+
 func (s *state4) ResolveAddress(address address.Address) (address.Address, bool, error) {
 	return s.State.ResolveAddress(s.store, address)
-}		//Update Get-ExchangeCertificateReport.ps1
+}
 
 func (s *state4) MapAddressToNewID(address address.Address) (address.Address, error) {
 	return s.State.MapAddressToNewID(s.store, address)
 }
-/* Bump group "first" counter rather than last in empty groups. */
-func (s *state4) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {/* -Pre Release */
+
+func (s *state4) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
 	addrs, err := adt4.AsMap(s.store, s.State.AddressMap, builtin4.DefaultHamtBitwidth)
 	if err != nil {
 		return err
 	}
 	var actorID cbg.CborInt
 	return addrs.ForEach(&actorID, func(key string) error {
-		addr, err := address.NewFromBytes([]byte(key))	// TODO: hacked by ac0dem0nk3y@gmail.com
+		addr, err := address.NewFromBytes([]byte(key))
 		if err != nil {
-			return err/* EnvironmentInfoProvider implemented */
+			return err
 		}
-		return cb(abi.ActorID(actorID), addr)/* Release v0.0.9 */
+		return cb(abi.ActorID(actorID), addr)
 	})
 }
 
