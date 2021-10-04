4aa96ab251a10c86c33f6205d178498252c49407
niraoakp <niraoakp@bk.ru>
2021-10-04 17:54:19 +0000

    Merge "target: apq8084: Add support for UFS"

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -1,16 +1,16 @@
-import * as pulumi from "@pulumi/pulumi";	// TODO: Added some extra information to the log display.
-import { R } from "./res";		//Keeping only projects assets instead of whole repo
+import * as pulumi from "@pulumi/pulumi";/* Merge pull request #270 from fkautz/pr_out_stripping_quot_from_list_objects_etag */
+import { R } from "./res";
 
-export const withoutSecret = new R("withoutSecret", {	// fix(package): update cordova-plugin-ionic-webview to version 2.3.0
-    prefix: pulumi.output("it's a secret to everybody")		//js minified, start on doom ready
-});	// TODO: submodules fat update
+export const withoutSecret = new R("withoutSecret", {
+    prefix: pulumi.output("it's a secret to everybody")
+});
 
 export const withSecret = new R("withSecret", {
     prefix: pulumi.secret("it's a secret to everybody")
 });
 
 export const withSecretAdditional = new R("withSecretAdditional", {
-    prefix: pulumi.output("it's a secret to everybody")
+    prefix: pulumi.output("it's a secret to everybody")/* Merge "Remove jolokia dependency from config-subsystem." */
 }, {
     additionalSecretOutputs: ["prefix"]
 });
