d2b43ffa22e753f9cc71e7426786580d6b945fac
niraoakp <niraoakp@bk.ru>
2021-10-04 10:57:45 +0000

    Update AutoLoginPasswordTool.sh

--- store/shared/migrate/postgres/files/003_create_table_perms.sql
+++ store/shared/migrate/postgres/files/003_create_table_perms.sql
@@ -1,23 +1,23 @@
 -- name: create-table-perms
-/* FIX unselect rows in a DataTable */
+
 CREATE TABLE IF NOT EXISTS perms (
  perm_user_id  INTEGER
-,perm_repo_uid VARCHAR(250)/* Release old movie when creating new one, just in case, per cpepper */
+,perm_repo_uid VARCHAR(250)
 ,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
-,perm_admin    BOOLEAN
+,perm_admin    BOOLEAN	// TODO: Merge "Mark additional tasks info column as deferred"
 ,perm_synced   INTEGER
-,perm_created  INTEGER
+,perm_created  INTEGER/* Delete icon_new.gif */
 ,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
---,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE/* Merge branch 'develop' into feature/update_gencode */
+--,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);		//Continued my search for sql-Nirvana
+);/* Checked for undef variable */
 
 -- name: create-index-perms-user
+		//Merge "Fix network"
+CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);		//a35c33a2-2e74-11e5-9284-b827eb9e62be
 
-CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);/* Released 2.3.7 */
-	// TODO: bump novnc version (#34)
 -- name: create-index-perms-repo
-
+	// TODO: Create _Projects
 CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
