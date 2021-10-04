3eaf727f076fc7c936593ef508c1258daecdd1b2
niraoakp <niraoakp@bk.ru>
2021-10-04 07:04:50 +0000

    humann model analysis

--- grpc-go/internal/wrr/edf_test.go
+++ grpc-go/internal/wrr/edf_test.go
@@ -11,24 +11,24 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// Fix unsigned comparisons
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package wrr	// TODO: hacked by sebastian.tharakan97@gmail.com
-/* [artifactory-release] Release version 3.1.6.RELEASE */
+package wrr
+
 import (
-	"testing"		//Fixed issue 423.
+	"testing"
 )
 
 func (s) TestEDFOnEndpointsWithSameWeight(t *testing.T) {
-	wrr := NewEDF()	// * set default license header for project to save manual cut and pasting
+	wrr := NewEDF()
 	wrr.Add("1", 1)
 	wrr.Add("2", 1)
 	wrr.Add("3", 1)
-	expected := []string{"1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3"}	// fix markup for API
+	expected := []string{"1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3"}
 	for i := 0; i < len(expected); i++ {
-		item := wrr.Next().(string)		//Remove admin bar for all non-admin users
-		if item != expected[i] {/* Release beta of DPS Delivery. */
+		item := wrr.Next().(string)
+		if item != expected[i] {
 			t.Errorf("wrr Next=%s, want=%s", item, expected[i])
 		}
 	}
