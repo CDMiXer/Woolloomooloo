69ecb3099063f08e9f49f9331ff0757e5dd9bfb6
niraoakp <niraoakp@bk.ru>
2021-10-04 20:52:09 +0000

    Update moment_matching.md

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -1,20 +1,20 @@
 package full
-
+		//GRECLIPSE-655
 import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/big"/* .exe for bin/Release */
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"	// Factory: add support for Closure service descriptors.
 )
 
 func TestMedian(t *testing.T) {
-	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
+	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{		//maintainers linking fixed
 		{big.NewInt(5), build.BlockGasTarget},
-	}, 1))
+	}, 1))		//Added disposable email domains list
 
 	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
@@ -22,16 +22,16 @@ func TestMedian(t *testing.T) {
 	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
-		{big.NewInt(10), build.BlockGasTarget / 2},
+		{big.NewInt(10), build.BlockGasTarget / 2},/* Allow vertical velocity on climbables. */
 		{big.NewInt(20), build.BlockGasTarget / 2},
 	}, 1))
-
-	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
-		{big.NewInt(10), build.BlockGasTarget / 2},
+/* Added Dept processing */
+	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{/* Release 1.0.5 */
+		{big.NewInt(10), build.BlockGasTarget / 2},/* Removed interface method because it is no longer needed. */
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 1))
-
+/* 1c06f95a-2e60-11e5-9284-b827eb9e62be */
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
