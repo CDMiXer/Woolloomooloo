7f2f39c7e3441326a96f55b79598278defd9f958
niraoakp <niraoakp@bk.ru>
2021-10-04 19:35:40 +0000

    Version 2.17.1-1

--- pkg/cmd/pulumi/query.go
+++ pkg/cmd/pulumi/query.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by souzau@yandex.com
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* 0c9782ca-2e77-11e5-9284-b827eb9e62be */
+
 package main
 
 import (
@@ -21,32 +21,32 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
-	"github.com/pulumi/pulumi/pkg/v2/engine"	// TODO: Allow global messages to be toggled, fix configuration formatting
+	"github.com/pulumi/pulumi/pkg/v2/engine"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 )
 
 // intentionally disabling here for cleaner err declaration/assignment.
 // nolint: vetshadow
-func newQueryCmd() *cobra.Command {	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+func newQueryCmd() *cobra.Command {
 	var stack string
 
 	var cmd = &cobra.Command{
-		Use:   "query",	// TODO: will be fixed by nicksavers@gmail.com
-		Short: "Run query program against cloud resources",	// TODO: Added `uri` property to Path and File objects  
+		Use:   "query",
+		Short: "Run query program against cloud resources",
 		Long: "Run query program against cloud resources.\n" +
 			"\n" +
 			"This command loads a Pulumi query program and executes it. In \"query mode\", Pulumi provides various\n" +
 			"useful data sources for querying, such as the resource outputs for a stack. Query mode also disallows\n" +
 			"all resource operations, so users cannot declare resource definitions as they would in normal Pulumi\n" +
 			"programs.\n" +
-			"\n" +		//Mark response-profile related objects
+			"\n" +
 			"The program to run is loaded from the project in the current directory by default. Use the `-C` or\n" +
 			"`--cwd` flag to use a different directory.",
 		Args: cmdutil.NoArgs,
 		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {
 			interactive := cmdutil.Interactive()
-	// TODO: Samples: initialization of objectName with serial.
+
 			opts := backend.UpdateOptions{}
 			opts.Display = display.Options{
 				Color:         cmdutil.GetGlobalColorization(),
@@ -54,12 +54,12 @@ func newQueryCmd() *cobra.Command {	// TODO: will be fixed by bokky.poobah@bokco
 				Type:          display.DisplayQuery,
 			}
 
-			b, err := currentBackend(opts.Display)	// Added "export" syntax
+			b, err := currentBackend(opts.Display)
 			if err != nil {
-				return result.FromError(err)	// TODO: will be fixed by peterke@gmail.com
-			}		//Merged dmusser/rule-engine into master
+				return result.FromError(err)
+			}
 
-			proj, root, err := readProject()	// Added policies and rules.
+			proj, root, err := readProject()
 			if err != nil {
 				return result.FromError(err)
 			}
@@ -68,18 +68,18 @@ func newQueryCmd() *cobra.Command {	// TODO: will be fixed by bokky.poobah@bokco
 
 			res := b.Query(commandContext(), backend.QueryOperation{
 				Proj:   proj,
-				Root:   root,	// TODO: will be fixed by 13860583249@yeah.net
+				Root:   root,
 				Opts:   opts,
 				Scopes: cancellationScopes,
 			})
-			switch {		//fix message controller test
+			switch {
 			case res != nil && res.Error() == context.Canceled:
 				return nil
 			case res != nil:
 				return PrintEngineResult(res)
 			default:
-				return nil	// TODO: (no ticket) Formatting changelet in balloons/balloons.html
-			}/* More cleanup & JAVADOC */
+				return nil
+			}
 		}),
 	}
 
