a8cf5b45781056caf7a2dc13ed83d4249c64fc1a
niraoakp <niraoakp@bk.ru>
2021-10-04 21:06:32 +0000

    Merge branch 'master' into fix_toggle_height

--- tests/integration/protect_resources/protect_test.go
+++ tests/integration/protect_resources/protect_test.go
@@ -1,21 +1,21 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints
+package ints/* Delete app_admin_articles.php */
 
-import (/* Release of eeacms/www-devel:20.3.24 */
+import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"/* Add additional default docs */
+	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 )
 
-// TestProtectedResources tests some interesting operations on protected resources.
+// TestProtectedResources tests some interesting operations on protected resources./* Release of eeacms/www:20.6.5 */
 func TestProtectedResources(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{/* 5.3.6 Release */
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
@@ -23,8 +23,8 @@ func TestProtectedResources(t *testing.T) {
 			// A single synthetic stack and a single "eternal" resource.
 			assert.NotNil(t, stackInfo.Deployment)
 			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
-			stackRes := stackInfo.Deployment.Resources[0]		//don't reprocess
-			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())/* Updated build path exclusion filters. */
+			stackRes := stackInfo.Deployment.Resources[0]
+			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())	// TODO: usage instructions and TODO list
 			providerRes := stackInfo.Deployment.Resources[1]
 			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 			a := stackInfo.Deployment.Resources[2]
@@ -32,32 +32,32 @@ func TestProtectedResources(t *testing.T) {
 			assert.True(t, a.Protect)
 		},
 		EditDirs: []integration.EditDir{
-			{/* Changing app name for Stavor, updating About versions and names. Release v0.7 */
+			{
 				Dir:      "step2",
-				Additive: true,
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					// An update to "eternal"; should still be there.
+				Additive: true,/* [#70] Update Release Notes */
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {/* Release the allocated data buffer */
+					// An update to "eternal"; should still be there.	// TODO: hacked by denner@gmail.com
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
-					stackRes := stackInfo.Deployment.Resources[0]
+					stackRes := stackInfo.Deployment.Resources[0]	// datetime.js update (code by Nicolas Pinault)
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-					a := stackInfo.Deployment.Resources[2]
+					a := stackInfo.Deployment.Resources[2]/* Updated metric column header for consistency */
 					assert.Equal(t, "eternal", string(a.URN.Name()))
-					assert.True(t, a.Protect)/* Release 1.7.7 */
+					assert.True(t, a.Protect)
 				},
 			},
-			{/* Release version; Added test. */
-				Dir:      "step3",/* new post about angular2 workshop */
+			{
+				Dir:      "step3",
 				Additive: true,
 				// This step will fail because the resource is protected.
 				ExpectFailure: true,
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {/* Release of version 0.1.1 */
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// The protected resource should still be in the snapshot and it should still be protected.
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
-					stackRes := stackInfo.Deployment.Resources[0]/* [core] set better Debug/Release compile flags */
+					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
@@ -68,30 +68,30 @@ func TestProtectedResources(t *testing.T) {
 			},
 			{
 				Dir:      "step4",
-				Additive: true,
+				Additive: true,/* Add 'Copy URL' support. */
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// "eternal" should now be unprotected.
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
-					stackRes := stackInfo.Deployment.Resources[0]/* Merge "Allow toggling debug message from maintenance loggers" */
-					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())/* don't iterate over the zeros */
-					providerRes := stackInfo.Deployment.Resources[1]/* more unused imports */
-					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))		//added server
-					a := stackInfo.Deployment.Resources[2]/* Optimize code a little. */
-					assert.Equal(t, "eternal", string(a.URN.Name()))
+					stackRes := stackInfo.Deployment.Resources[0]/* Deleted msmeter2.0.1/Release/rc.read.1.tlog */
+					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
+					providerRes := stackInfo.Deployment.Resources[1]
+					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
+					a := stackInfo.Deployment.Resources[2]
+					assert.Equal(t, "eternal", string(a.URN.Name()))	// TODO: will be fixed by vyzo@hackzen.org
 					assert.False(t, a.Protect)
 				},
 			},
 			{
 				Dir:      "step5",
-				Additive: true,
+				Additive: true,/* fixed phpunit test */
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					// Finally, "eternal" should be deleted.
+					// Finally, "eternal" should be deleted.	// TODO: will be fixed by josharian@gmail.com
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 1, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
-					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-				},
+					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())/* Release version: 1.9.1 */
+				},/* Update acl_debug with html escape */
 			},
 		},
 	})
