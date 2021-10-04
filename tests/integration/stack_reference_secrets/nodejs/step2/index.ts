bfcd063aa47145c3bfa9eddd43e4a733cb4d5d53
niraoakp <niraoakp@bk.ru>
2021-10-04 16:19:50 +0000

    Merged feature/menu into master

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -1,8 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
 
 export const normal = pulumi.output("normal");
-export const secret = pulumi.secret("secret");	// TODO: hacked by arajasek94@gmail.com
-	// TODO: Merge "Remove check_role_for_trust from sample policies"
+export const secret = pulumi.secret("secret");
+
 // Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
 // deployment.
 const org = new pulumi.Config().require("org");
