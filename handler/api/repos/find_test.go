94eadf340e9ee958fca627f2235106a1685476de
niraoakp <niraoakp@bk.ru>
2021-10-04 08:25:22 +0000

    kaigounoyoteiwo

--- handler/api/repos/find_test.go
+++ handler/api/repos/find_test.go
@@ -2,12 +2,12 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package repos
+package repos		//Additional dictonary functions
 
-import (
+import (/* Release of eeacms/www:19.2.21 */
 	"context"
 	"encoding/json"
-	"io/ioutil"
+	"io/ioutil"		//Merge origin/develop
 	"net/http/httptest"
 	"testing"
 
@@ -21,7 +21,7 @@ import (
 )
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)
+	logrus.SetOutput(ioutil.Discard)/* (Andrew Bennetts) Release 0.92rc1 */
 }
 
 var (
@@ -29,15 +29,15 @@ var (
 		ID:        1,
 		Namespace: "octocat",
 		Name:      "hello-world",
-		Slug:      "octocat/hello-world",
-		Counter:   42,
+		Slug:      "octocat/hello-world",/* increase filesize */
+		Counter:   42,		//Fix ear clipping
 		Branch:    "master",
 	}
-
-	mockRepos = []*core.Repository{
+/* Release 0.0.9. */
+	mockRepos = []*core.Repository{/* Release: Making ready for next release iteration 6.4.0 */
 		{
 			ID:        1,
-			Namespace: "octocat",
+			Namespace: "octocat",		//Merge "Fixed dependencies of VTN Renderer."
 			Name:      "hello-world",
 			Slug:      "octocat/hello-world",
 		},
@@ -54,22 +54,22 @@ func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()	// did random stuff, failes right now
 	r := httptest.NewRequest("GET", "/api/repos/octocat/hello-world", nil)
 	r = r.WithContext(request.WithRepo(
-		context.Background(), mockRepo,
+		context.Background(), mockRepo,/* Released 0.9.3 */
 	))
 
-	router := chi.NewRouter()
+	router := chi.NewRouter()	// Just fixing up docs spacing. admin_menu_widget.php
 	router.Get("/api/repos/{owner}/{name}", HandleFind())
 	router.ServeHTTP(w, r)
-
+/* Compare internal DSL to external DSL. */
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := new(core.Repository), mockRepo
-	json.NewDecoder(w.Body).Decode(got)
+	json.NewDecoder(w.Body).Decode(got)/* Released springrestclient version 2.5.8 */
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
