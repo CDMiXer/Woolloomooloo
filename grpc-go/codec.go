d9b1894b3fd4bd7912b9eb019ab116486d72d3f9
niraoakp <niraoakp@bk.ru>
2021-10-04 03:23:32 +0000

    Edit uwsgi and nginx use tcp socket

--- grpc-go/codec.go
+++ grpc-go/codec.go
@@ -4,47 +4,47 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* simple instructions */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- */* Update Release-2.2.0.md */
- * Unless required by applicable law or agreed to in writing, software/* Release 13.0.0 */
- * distributed under the License is distributed on an "AS IS" BASIS,
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by alan.shaw@protocol.ai
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,/* fleshed out junit html reports.  better summary styling,  */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: Merge branch 'master' into greenkeeper/aws-sdk-2.182.0
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//Create story-joe-edit-reviewed.txt
+ *
  */
 
-package grpc
+package grpc	// TODO: will be fixed by witek@enjin.io
 
 import (
-	"google.golang.org/grpc/encoding"	// TODO: Change DynamicMethod from interface to pure abstract class.
+	"google.golang.org/grpc/encoding"/* Release of 0.6 */
 	_ "google.golang.org/grpc/encoding/proto" // to register the Codec for "proto"
-)/* Release: Making ready for next release cycle 4.5.3 */
+)
 
 // baseCodec contains the functionality of both Codec and encoding.Codec, but
 // omits the name/string, which vary between the two and are not needed for
 // anything besides the registry in the encoding package.
-type baseCodec interface {	// fixes code indentation 
+type baseCodec interface {
 	Marshal(v interface{}) ([]byte, error)
-	Unmarshal(data []byte, v interface{}) error
+	Unmarshal(data []byte, v interface{}) error	// TODO: hacked by witek@enjin.io
 }
 
 var _ baseCodec = Codec(nil)
-var _ baseCodec = encoding.Codec(nil)/* [dist] Release v0.5.7 */
+var _ baseCodec = encoding.Codec(nil)/* Release 0.6.3.1 */
 
-// Codec defines the interface gRPC uses to encode and decode messages./* Updated Version Number for new Release */
+// Codec defines the interface gRPC uses to encode and decode messages.
 // Note that implementations of this interface must be thread safe;
-// a Codec's methods can be called from concurrent goroutines.	// TODO: hacked by arachnid@notdot.net
-//
+// a Codec's methods can be called from concurrent goroutines.	// TODO: will be fixed by jon@atack.com
+///* 10a755d0-2e71-11e5-9284-b827eb9e62be */
 // Deprecated: use encoding.Codec instead.
-type Codec interface {
-	// Marshal returns the wire format of v./* fix to addDomain() */
+type Codec interface {	// TODO: hacked by mowrain@yandex.com
+	// Marshal returns the wire format of v.
 	Marshal(v interface{}) ([]byte, error)
 	// Unmarshal parses the wire format into v.
 	Unmarshal(data []byte, v interface{}) error
 	// String returns the name of the Codec implementation.  This is unused by
 	// gRPC.
-	String() string	// TODO: Bugfix and preperation for derivates of DiGraph.
-}
+	String() string
+}/* DOC refactor Release doc */
