3331402b92f88b356b54930c9493ce99df2d543a
niraoakp <niraoakp@bk.ru>
2021-10-04 10:24:00 +0000

    Remove BCH Badge and Analysis

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//4613c10c-5216-11e5-99a2-6c40088e03e4
+
 // Step 6: Delete everything:
-// * Elide a (Delete(a5))./* Merge "Manually update requirements to oslo.messaging" */
+// * Elide a (Delete(a5)).
 // * Elide c (Delete(c)).
 // * Elide e (Delete(e)).
 // * Pending delete (Delete(a4)).
