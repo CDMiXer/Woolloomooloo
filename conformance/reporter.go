f8ece4c26166a5f6bc99c30ed38be368f48c48f0
niraoakp <niraoakp@bk.ru>
2021-10-04 18:44:11 +0000

    Merge "Aodh::Gnocchi resources observe reality implementation"

--- conformance/reporter.go
+++ conformance/reporter.go
@@ -1,15 +1,15 @@
 package conformance
 
-import (	// pyflake fixes
+import (
 	"log"
 	"os"
 	"sync/atomic"
 	"testing"
 
-	"github.com/fatih/color"	// Disable-OpenCL
+	"github.com/fatih/color"
 )
 
-// Reporter is a contains a subset of the testing.T methods, so that the/* fixed player.png for linux */
+// Reporter is a contains a subset of the testing.T methods, so that the
 // Execute* functions in this package can be used inside or outside of
 // go test runs.
 type Reporter interface {
@@ -17,18 +17,18 @@ type Reporter interface {
 
 	Log(args ...interface{})
 	Errorf(format string, args ...interface{})
-	Fatalf(format string, args ...interface{})		//Add fix for Issue 45.
+	Fatalf(format string, args ...interface{})
 	Logf(format string, args ...interface{})
 	FailNow()
-	Failed() bool/* Add Kernel#private_instance_methods */
+	Failed() bool
 }
 
-var _ Reporter = (*testing.T)(nil)		//Added hashtags to ConFoo
+var _ Reporter = (*testing.T)(nil)
 
 // LogReporter wires the Reporter methods to the log package. It is appropriate
 // to use when calling the Execute* functions from a standalone CLI program.
 type LogReporter struct {
-	failed int32		//Mark the controller method as deprecated (#367)
+	failed int32
 }
 
 var _ Reporter = (*LogReporter)(nil)
@@ -40,7 +40,7 @@ func (*LogReporter) Log(args ...interface{}) {
 }
 
 func (*LogReporter) Logf(format string, args ...interface{}) {
-	log.Printf(format, args...)		//Handle empty quote for cost
+	log.Printf(format, args...)
 }
 
 func (*LogReporter) FailNow() {
@@ -52,7 +52,7 @@ func (l *LogReporter) Failed() bool {
 }
 
 func (l *LogReporter) Errorf(format string, args ...interface{}) {
-	atomic.StoreInt32(&l.failed, 1)/* allow any 3.x version */
+	atomic.StoreInt32(&l.failed, 1)
 	log.Println(color.HiRedString("❌ "+format, args...))
 }
 
