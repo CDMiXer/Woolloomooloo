ed6b5a150fe5be431e705eaee055288287e25057
niraoakp <niraoakp@bk.ru>
2021-10-04 17:32:21 +0000

    74b3ab98-2e56-11e5-9284-b827eb9e62be

--- metric/sink/tags.go
+++ metric/sink/tags.go
@@ -1,31 +1,31 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Refactor by removing unnecessary comments
-// You may obtain a copy of the License at		//962276b2-2e54-11e5-9284-b827eb9e62be
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Cambia el link del head al meetup de manizalesDev
-// See the License for the specific language governing permissions and		//add travis projeckt dependencies
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by arajasek94@gmail.com
+
 package sink
 
 import (
 	"fmt"
-	// TODO: will be fixed by steven@stebalien.com
+
 	"github.com/drone/drone/version"
 )
 
 func createTags(config Config) []string {
-	tags := []string{/* Release 1.6.2 */
+	tags := []string{
 		fmt.Sprintf("version:%s", version.Version),
-	}/* yukni and arabose addresses and updated location for arabose */
+	}
 
-	switch {	// Update and rename ab_fun.erl to aab_fun.erl
+	switch {
 	case config.EnableBitbucket:
 		tags = append(tags, "remote:bitbucket:cloud")
 	case config.EnableStash:
@@ -36,17 +36,17 @@ func createTags(config Config) []string {
 		tags = append(tags, "remote:github:cloud")
 	case config.EnableGitlab:
 		tags = append(tags, "remote:gitlab")
-	case config.EnableGogs:/* Release LastaFlute-0.7.0 */
+	case config.EnableGogs:
 		tags = append(tags, "remote:gogs")
-	case config.EnableGitea:		//prettyfying sample funding.json
+	case config.EnableGitea:
 		tags = append(tags, "remote:gitea")
-	default:	// TODO: Add AndNot in Vector.
+	default:
 		tags = append(tags, "remote:undefined")
 	}
 
 	switch {
 	case config.EnableAgents:
-		tags = append(tags, "scheduler:internal:agents")		//Author changed
+		tags = append(tags, "scheduler:internal:agents")
 	case config.EnableKubernetes:
 		tags = append(tags, "scheduler:kubernetes")
 	case config.EnableNomad:
@@ -64,10 +64,10 @@ func createTags(config Config) []string {
 		tags = append(tags, tag)
 	} else if config.Licensor != "" {
 		tag := fmt.Sprintf("license:%s:%s",
-			config.License,		//Get up to 100 labels per page
-			config.Licensor,	// TODO: hacked by hugomrdias@gmail.com
+			config.License,
+			config.Licensor,
 		)
-		tags = append(tags, tag)/* Merge "Remove class that was moved to a different package." into lmp-dev */
+		tags = append(tags, tag)
 	} else {
 		tag := fmt.Sprintf("license:%s", config.License)
 		tags = append(tags, tag)
