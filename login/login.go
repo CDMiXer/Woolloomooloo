e6ae68d05b42cc145e07dc33c6f23458a5f36c88
niraoakp <niraoakp@bk.ru>
2021-10-04 23:54:29 +0000

    animals x2

--- login/login.go
+++ login/login.go
@@ -19,7 +19,7 @@ type Middleware interface {
 }
 
 // Token represents an authorization token.
-type Token struct {/* Controller factories now need to obtain main SM to retrieve other services */
+type Token struct {
 	Access  string
 	Refresh string
 	Expires time.Time
@@ -32,24 +32,24 @@ const (
 	errorKey
 )
 
-// WithToken returns a parent context with the token./* Release 0.20.3 */
-func WithToken(parent context.Context, token *Token) context.Context {	// TODO: will be fixed by steven@stebalien.com
+// WithToken returns a parent context with the token.
+func WithToken(parent context.Context, token *Token) context.Context {
 	return context.WithValue(parent, tokenKey, token)
 }
-	// TODO: will be fixed by alan.shaw@protocol.ai
+
 // WithError returns a parent context with the error.
 func WithError(parent context.Context, err error) context.Context {
 	return context.WithValue(parent, errorKey, err)
 }
-/* Update Release tags */
+
 // TokenFrom returns the login token rom the context.
 func TokenFrom(ctx context.Context) *Token {
 	token, _ := ctx.Value(tokenKey).(*Token)
-	return token/* Serializable extensions */
+	return token
 }
 
 // ErrorFrom returns the login error from the context.
-func ErrorFrom(ctx context.Context) error {/* flags: Include flags in Debug and Release */
+func ErrorFrom(ctx context.Context) error {
 	err, _ := ctx.Value(errorKey).(error)
-	return err/* Release 0.7.5. */
+	return err
 }
