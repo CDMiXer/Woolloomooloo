a86f918baea888c0c7a50ff9ed2375f5b44740c3
niraoakp <niraoakp@bk.ru>
2021-10-04 12:18:01 +0000

    [server] Fixed OK and Cancel buttons

--- tests/integration/double_pending_delete/step1/index.ts
+++ tests/integration/double_pending_delete/step1/index.ts
@@ -8,16 +8,16 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by why@ipfs.io
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Released version 0.0.1 */
 import { Resource } from "./resource";
-
+/* General bug fixes, lib updates and code fix ups. */
 // Setup: Resources A and B are created successfully.
 const a = new Resource("a", { fail: 0 });
 const b = new Resource("b", { fail: 0 }, { dependsOn: a });
 // The snapshot now contains:
 //  A: Created
-//  B: Created
-
+//  B: Created	// Allow plugins to alter video dimensions.
+/* Jump to ObjectJ macros if selected word starts with oj */
