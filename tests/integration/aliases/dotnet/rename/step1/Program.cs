ab3ed9de4314b05754aff087d1a3fc3602d345ef
niraoakp <niraoakp@bk.ru>
2021-10-04 18:08:20 +0000

    Automatic changelog generation for PR #16262

--- tests/integration/aliases/dotnet/rename/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename/step1/Program.cs
@@ -10,15 +10,15 @@ class Resource : ComponentResource
     {
     }
 }
-	// Some Microsoft related OIDs added.
+
 class Program
-{/* Insertion, Binary Insertion, and Merge sorting */
+{
     static Task<int> Main(string[] args)
-    {	// TODO: Merge "PermissionBackend: Fix javadoc link"
+    {
         return Deployment.RunAsync(() => 
         {
             // Scenario #1 - rename a resource
             var res1 = new Resource("res1");
-        });		//Fixed Thread Post Avatars
-    }/* Updated Code as per review comments */
+        });
+    }
 }
