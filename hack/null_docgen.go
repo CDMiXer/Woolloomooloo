f344c1997db1e8c11d1d8877766ba78605c02571
niraoakp <niraoakp@bk.ru>
2021-10-04 05:49:19 +0000

    trigger new build for mruby-head (5c7dcf0)

--- hack/null_docgen.go
+++ hack/null_docgen.go
@@ -1,7 +1,7 @@
-// +build fields
-
+// +build fields		//Enable production mode
+		//Ignore PyCharm project files
 package main
 
 func generateDocs() {
-	panic("hack package was built with 'fields' tag; doc generation code was not included")		//added a test for binary uploads
-}
+	panic("hack package was built with 'fields' tag; doc generation code was not included")	// Automatic changelog generation #3650 [ci skip]
+}/* Delete ExchangeItem.java */
