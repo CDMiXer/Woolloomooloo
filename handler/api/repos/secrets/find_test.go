6bfbab1f52438bebb7a2bde96e28f6a4fca725b9
niraoakp <niraoakp@bk.ru>
2021-10-04 15:15:51 +0000

    d46b35d2-585a-11e5-b704-6c40088e03e4

--- handler/api/repos/secrets/find_test.go
+++ handler/api/repos/secrets/find_test.go
@@ -4,19 +4,19 @@
 
 // +build !oss
 
-package secrets	// add controls, results board styling
+package secrets
 
-import (		//44d8e8de-35c7-11e5-a74b-6c40088e03e4
-	"context"/* Add Skeptick.timeout setting for runaway converts */
+import (
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"/* try to fix circleci */
+	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-/* Release build properties */
+
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
@@ -24,7 +24,7 @@ import (		//44d8e8de-35c7-11e5-a74b-6c40088e03e4
 
 func TestHandleFind(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// refs #18 rename attribute. lenient => ignoreCase
+	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
@@ -33,29 +33,29 @@ func TestHandleFind(t *testing.T) {
 	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")	// TODO: Revert symlink, it's broken. Use proper manpage installation for testdrive
-	c.URLParams.Add("name", "hello-world")	// Added design review notes
-	c.URLParams.Add("secret", "github_password")		//address validation
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("secret", "github_password")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),		//activated link for today's notes
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
 	HandleFind(repos, secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)	// Create bpgthumbnailer.desktop
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := &core.Secret{}, dummySecretScrubbed	// TODO: hacked by magik6k@gmail.com
-	json.NewDecoder(w.Body).Decode(got)	// Added images for jquery-ui 
+	got, want := &core.Secret{}, dummySecretScrubbed
+	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
 
-func TestHandleFind_RepoNotFound(t *testing.T) {		//Delete burp suite.z46
+func TestHandleFind_RepoNotFound(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -63,8 +63,8 @@ func TestHandleFind_RepoNotFound(t *testing.T) {		//Delete burp suite.z46
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")	// New constraints validator to validate and merge constraints
-	c.URLParams.Add("name", "hello-world")		//New translations p02_ch03_the_second_test_murder.md (English)
+	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
 
 	w := httptest.NewRecorder()
