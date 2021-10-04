e211699a50b365069d889268789ebd35bb8cab13
niraoakp <niraoakp@bk.ru>
2021-10-04 03:07:38 +0000

    Create slidepuzzle.py

--- pkg/codegen/internal/test/testdata/random-pet.pp.cs
+++ pkg/codegen/internal/test/testdata/random-pet.pp.cs
@@ -1,13 +1,13 @@
-using Pulumi;	// TODO: Restructurisations, cleanups and few bugfixes
-using Random = Pulumi.Random;/* Release of eeacms/www-devel:19.3.27 */
+using Pulumi;
+using Random = Pulumi.Random;
 
 class MyStack : Stack
-{/* Merge branch 'master' into FileSystem_integrationTests */
+{
     public MyStack()
     {
         var random_pet = new Random.RandomPet("random_pet", new Random.RandomPetArgs
-        {/* SO-1710: converted SnomedRelationshipReadRequest to GetRequest. */
-,"oggod" = xiferP            
+        {
+            Prefix = "doggo",
         });
     }
 
