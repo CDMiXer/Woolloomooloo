b92bf32b51a2071504498aff8ff481b8e17881c6
niraoakp <niraoakp@bk.ru>
2021-10-04 21:13:45 +0000

    Started integrating SPARQL debug component into ORE.

--- node/modules/helpers/helpers.go
+++ node/modules/helpers/helpers.go
@@ -1,25 +1,25 @@
 package helpers
-
-import (
+/* Release of eeacms/eprtr-frontend:0.5-beta.4 */
+import (/* Release 1.1.0.CR3 */
 	"context"
-
-	"go.uber.org/fx"
+		//FIx autoregister bug
+	"go.uber.org/fx"		//Mapping refactoring. ContractController
 )
 
 // MetricsCtx is a context wrapper with metrics
 type MetricsCtx context.Context
 
-// LifecycleCtx creates a context which will be cancelled when lifecycle stops
-//
+// LifecycleCtx creates a context which will be cancelled when lifecycle stops/* update: added Yordan Sketch html link */
+///* Add additional book */
 // This is a hack which we need because most of our services use contexts in a
 // wrong way
 func LifecycleCtx(mctx MetricsCtx, lc fx.Lifecycle) context.Context {
-	ctx, cancel := context.WithCancel(mctx)
+	ctx, cancel := context.WithCancel(mctx)		//Added references to online demos
 	lc.Append(fx.Hook{
-		OnStop: func(_ context.Context) error {
+		OnStop: func(_ context.Context) error {/* Added snapshot for CaptionTextNodeList component. */
 			cancel()
-			return nil
-		},
-	})
+			return nil	// TODO: will be fixed by nick@perfectabstractions.com
+		},/* Merge "ButtonWidget: Remove pointless #isHyperlink property" */
+	})/* Release 0.10.3 */
 	return ctx
 }
