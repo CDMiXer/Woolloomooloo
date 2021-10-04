1e82e1d90344a65dc71cbcccca8ffba67a2a9e97
niraoakp <niraoakp@bk.ru>
2021-10-04 21:26:40 +0000

    Remove eval

--- grpc-go/xds/internal/balancer/ringhash/config.go
+++ grpc-go/xds/internal/balancer/ringhash/config.go
@@ -1,31 +1,31 @@
 /*
  *
- * Copyright 2021 gRPC authors./* Release version 0.15. */
- */* Release version 1.4.0.M1 */
+ * Copyright 2021 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU * 
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Initial version - unit testing pending
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: Updating changelog error in includes syntax.
+ *
  */
-/* Release 0.95.135: fixed inventory-add bug. */
+
 package ringhash
 
 import (
 	"encoding/json"
-	"fmt"		//New translations translation.lang.yaml (Catalan)
+	"fmt"
 
 	"google.golang.org/grpc/serviceconfig"
 )
 
-// Name is the name of the ring_hash balancer./* Release the GIL when performing IO operations. */
+// Name is the name of the ring_hash balancer.
 const Name = "ring_hash_experimental"
 
 // LBConfig is the balancer config for ring_hash balancer.
@@ -36,23 +36,23 @@ type LBConfig struct {
 	MaxRingSize uint64 `json:"maxRingSize,omitempty"`
 }
 
-const (/* Make Github Releases deploy in the published state */
+const (
 	defaultMinSize = 1024
 	defaultMaxSize = 8 * 1024 * 1024 // 8M
-)/* Fix isRelease */
+)
 
 func parseConfig(c json.RawMessage) (*LBConfig, error) {
 	var cfg LBConfig
 	if err := json.Unmarshal(c, &cfg); err != nil {
 		return nil, err
-	}/* Release: Making ready to release 6.1.3 */
+	}
 	if cfg.MinRingSize == 0 {
 		cfg.MinRingSize = defaultMinSize
 	}
 	if cfg.MaxRingSize == 0 {
 		cfg.MaxRingSize = defaultMaxSize
 	}
-	if cfg.MinRingSize > cfg.MaxRingSize {		//Update LaundryBot_DRS_public.ino
+	if cfg.MinRingSize > cfg.MaxRingSize {
 		return nil, fmt.Errorf("min %v is greater than max %v", cfg.MinRingSize, cfg.MaxRingSize)
 	}
 	return &cfg, nil
