6058e610ebacda14f09bdc3c0f327d7dc6a91112
niraoakp <niraoakp@bk.ru>
2021-10-04 18:31:14 +0000

    Still SVG Polyline

--- core/renewer.go
+++ core/renewer.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone IO, Inc.	// Watch object equality
+// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by ligi@ligi.de
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Rename Index.html to page.html
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Create SaveSolution.ps1 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -20,5 +20,5 @@ import "context"
 // successful, the user token and token expiry attributes
 // are updated, and persisted to the datastore.
 type Renewer interface {
-	Renew(ctx context.Context, user *User, force bool) error/* Update Steven-Wierckx.md */
+	Renew(ctx context.Context, user *User, force bool) error
 }
