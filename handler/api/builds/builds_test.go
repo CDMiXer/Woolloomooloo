6d21d1930edd3786f7f16b7e8f044388837f1e61
niraoakp <niraoakp@bk.ru>
2021-10-04 05:12:49 +0000

    Alias + to pipe

--- handler/api/builds/builds_test.go
+++ handler/api/builds/builds_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Delete SystemSearch
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -11,7 +11,7 @@ import (
 	"io/ioutil"
 	"net/http/httptest"
 	"testing"
-		//Use Scala 2.11.7
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
@@ -21,25 +21,25 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func init() {		//commit mapselectitem.xml
+func init() {
 	logrus.SetOutput(ioutil.Discard)
 }
-/* Merge "Add more checking to ReleasePrimitiveArray." */
-func TestHandleBuilds(t *testing.T) {/* wrote tests and added id to logging */
+
+func TestHandleBuilds(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	want := []*core.Repository{
-		{ID: 1, Slug: "octocat/hello-world"},/* Release 7.0.1 */
+		{ID: 1, Slug: "octocat/hello-world"},
 		{ID: 2, Slug: "octocat/spoon-fork"},
 	}
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().ListIncomplete(gomock.Any()).Return(want, nil)
 
-	w := httptest.NewRecorder()	// TODO: Improved Processor interface to better support Scan Editor
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	// TODO: WorkshopVerticle as proxy for mongo calls
+
 	HandleIncomplete(repos)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
@@ -53,22 +53,22 @@ func TestHandleBuilds(t *testing.T) {/* wrote tests and added id to logging */
 }
 
 func TestHandleBuilds_Error(t *testing.T) {
-	controller := gomock.NewController(t)/* Update social.php */
-	defer controller.Finish()	// TODO: Updated to ph-commons 6.0.0-beta1
+	controller := gomock.NewController(t)
+	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().ListIncomplete(gomock.Any()).Return(nil, errors.ErrNotFound)
 
-	w := httptest.NewRecorder()	// TODO: will be fixed by fjl@ethereum.org
-	r := httptest.NewRequest("GET", "/", nil)/* Release to Github as Release instead of draft */
-	// removal of many offensive words
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", nil)
+
 	HandleIncomplete(repos)(w, r)
 	if got, want := w.Code, 500; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* Rename brendanpeillach.md to peillach.md */
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
-/* Fix Webflow Committer Problems */
+
 	got, want := &errors.Error{}, errors.ErrNotFound
-	json.NewDecoder(w.Body).Decode(got)		//Update index_data.php
+	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
