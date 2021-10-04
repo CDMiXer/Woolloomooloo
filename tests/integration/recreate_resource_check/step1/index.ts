ffb5c9f2185072154efc726028f069ef2d7e9782
niraoakp <niraoakp@bk.ru>
2021-10-04 01:01:46 +0000

    Fixing unescaped strings in readme

--- tests/integration/recreate_resource_check/step1/index.ts
+++ tests/integration/recreate_resource_check/step1/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Added tip to the last question
 
-import { Resource } from "./resource";/* Add Barry Wark's decorator to release NSAutoReleasePool */
-
+import { Resource } from "./resource";
+	// TODO: Merge "Create structure of flows' packages"
 // Base depends on nothing.
-const a = new Resource("base", { uniqueKey: 1, state: 99 });
-	// Don't reference /bin/bash; doesn't exist
-// Dependent depends on Base with state 99.
+const a = new Resource("base", { uniqueKey: 1, state: 99 });		//add required ruby version
+
+// Dependent depends on Base with state 99.		//Mark read notifications as read after migrating to conversations
 const b = new Resource("dependent", { state: a.state });
