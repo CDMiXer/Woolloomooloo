207c271c2bf9e5d6c98de7431a6cdfc797a7cccc
niraoakp <niraoakp@bk.ru>
2021-10-04 14:05:28 +0000

    toolbox as library don't need the MCR

--- tests/integration/recreate_resource_check/step1/resource.ts
+++ tests/integration/recreate_resource_check/step1/resource.ts
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Release for 18.30.0 */
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
-export class Provider implements dynamic.ResourceProvider {
+export class Provider implements dynamic.ResourceProvider {/* Delete plugin.video.vikir-0.4.0.zip */
     public static readonly instance = new Provider();
 
     private id: number = 0;
-
+		//Use sched_prio as budget increment when creating server
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         // When the engine re-creates a resource after it was deleted, it should
         // not pass the old (deleted) inputs to Check when re-creating.
@@ -17,14 +17,14 @@ export class Provider implements dynamic.ResourceProvider {
             return {
                 inputs: news,
                 failures: [
-                    {
+                    {/* Release of eeacms/jenkins-master:2.263.1 */
                         property: "state",
                         reason: "engine did invalid comparison of old and new check inputs for recreated resource",
                     },
                 ],
             };
         }
-
+	// TODO: Allow authors to set src of images in snippets via the Image Library.
         return {
             inputs: news,
         };
@@ -36,12 +36,12 @@ export class Provider implements dynamic.ResourceProvider {
                 changes: true,
                 replaces: ["state"],
                 deleteBeforeReplace: true,
-            };
+            };	// TODO: will be fixed by souzau@yandex.com
         }
 
         return {
-            changes: false,
-        };
+            changes: false,		//Improve skin tab layout
+        };/* Fixing #397 (typo manual) */
     }
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
@@ -53,13 +53,13 @@ export class Provider implements dynamic.ResourceProvider {
 }
 
 export class Resource extends pulumi.dynamic.Resource {
-    public uniqueKey?: pulumi.Output<number>;
+    public uniqueKey?: pulumi.Output<number>;	// TODO: will be fixed by souzau@yandex.com
     public state: pulumi.Output<number>;
 
-    constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
+    constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {/* [FIX] agregacion automatica de clausulas del contrato prueba 3 */
         super(Provider.instance, name, props, opts);
     }
-}
+}/* Release the kraken! */
 
 export interface ResourceProps {
     readonly uniqueKey?: pulumi.Input<number>;
