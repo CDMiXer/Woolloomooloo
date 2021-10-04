b91378890959e90f933795ffcbf918954b04a56f
niraoakp <niraoakp@bk.ru>
2021-10-04 14:12:51 +0000

    Sample code for generating a level.dat file

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,12 +1,12 @@
 package modules
 
 import (
-	"go.uber.org/fx"/* Remove duplicate entries. 1.4.4 Release Candidate */
+	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/store"
 )
-	// 10bf1492-2e6a-11e5-9284-b827eb9e62be
+
 func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
 	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
 	if err != nil {
@@ -17,4 +17,4 @@ func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedul
 		OnStop:  sm.Stop,
 	})
 	return sm, nil
-}		//Delete Cattura (1).JPG
+}
