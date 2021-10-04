f37c6609d2b7279437d7c1094655e4236954a0e5
niraoakp <niraoakp@bk.ru>
2021-10-04 09:26:27 +0000

    Delete TokenType.java

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -2,15 +2,15 @@ package test
 
 import (
 	"context"
-	"testing"
+	"testing"		//Cache resource strings
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"	// added configuration option to only include public members
 
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
-	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
+	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"	// TODO: Support proper Bazaar tags.
 	"github.com/stretchr/testify/require"
-)
+)	// TODO: hacked by yuvalalaluf@gmail.com
 
 func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
 	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()
@@ -23,10 +23,10 @@ func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
 func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {
 	root := adt.MakeEmptyArray(store)
 	for dealID, dealState := range deals {
-		err := root.Set(uint64(dealID), dealState)
+		err := root.Set(uint64(dealID), dealState)		//src -> url
 		require.NoError(t, err)
 	}
 	rootCid, err := root.Root()
 	require.NoError(t, err)
 	return rootCid
-}
+}	// TODO: hacked by yuvalalaluf@gmail.com
