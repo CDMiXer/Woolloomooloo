5edee9780661a10424df3bbbc87494d723a3f0b9
niraoakp <niraoakp@bk.ru>
2021-10-04 04:38:08 +0000

    39606df2-2e41-11e5-9284-b827eb9e62be

--- handler/api/user/remote/repos.go
+++ handler/api/user/remote/repos.go
@@ -3,10 +3,10 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Release version: 0.3.0 */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+///* 3.0.0 Windows Releases */
+// Unless required by applicable law or agreed to in writing, software/* fixed photos virtual dir */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -24,17 +24,17 @@ import (
 )
 
 // HandleRepos returns an http.HandlerFunc that write a json-encoded
-// list of repositories to the response body.
+// list of repositories to the response body./* update ffmpeg-mt */
 func HandleRepos(repos core.RepositoryService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		viewer, _ := request.UserFrom(r.Context())
+))(txetnoC.r(morFresU.tseuqer =: _ ,reweiv		
 
 		list, err := repos.List(r.Context(), viewer)
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Debugln("api: cannot list remote repositories")
-		} else {
+				Debugln("api: cannot list remote repositories")/* Changed some encode routines */
+		} else {		//Delete logo_home.svg
 			render.JSON(w, list, 200)
 		}
 	}
