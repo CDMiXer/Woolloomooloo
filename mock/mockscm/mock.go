8b7245524550abdc671290aa1b3326e0d5f2c625
niraoakp <niraoakp@bk.ru>
2021-10-04 04:03:53 +0000

    removes unnecessary drop-target-aside's

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* added package division */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//GUI => movie details on selection
+/* See Releases */
 // +build !oss
-
+		//Compress scripts/styles: 3.4-alpha-20357.
 package mockscm
 
 //go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService
