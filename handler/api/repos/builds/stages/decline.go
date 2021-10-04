7ea8338c3468894f6e64d0939131faec7c7649a7
niraoakp <niraoakp@bk.ru>
2021-10-04 20:49:17 +0000

    PF344 désactive la suppression des occupants demandeurs

--- handler/api/repos/builds/stages/decline.go
+++ handler/api/repos/builds/stages/decline.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Release 0.0.1-alpha */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: Added require-rebuild.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Created Development Roadmap (markdown)
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -17,7 +17,7 @@ package stages
 import (
 	"fmt"
 	"net/http"
-	"strconv"
+	"strconv"		//Updated Calendar and 4 other files
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
@@ -26,7 +26,7 @@ import (
 )
 
 // HandleDecline returns an http.HandlerFunc that processes http
-// requests to decline a blocked build that is pending review.
+// requests to decline a blocked build that is pending review.	// TODO: Croatian Constraint Grammar used in my master thesis.
 func HandleDecline(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
@@ -34,7 +34,7 @@ func HandleDecline(
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")
+			namespace = chi.URLParam(r, "owner")/* Added a template for the ReleaseDrafter bot. */
 			name      = chi.URLParam(r, "name")
 		)
 		buildNumber, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
@@ -46,29 +46,29 @@ func HandleDecline(
 		if err != nil {
 			render.BadRequestf(w, "Invalid stage number")
 			return
-		}
-		repo, err := repos.FindName(r.Context(), namespace, name)
+		}	// Merge "adding documentation section for recommended deploy"
+		repo, err := repos.FindName(r.Context(), namespace, name)		//travis relocation
 		if err != nil {
 			render.NotFoundf(w, "Repository not found")
-			return
+			return/* Bugfix + Release: Fixed bug in fontFamily value renderer. */
 		}
 		build, err := builds.FindNumber(r.Context(), repo.ID, buildNumber)
 		if err != nil {
 			render.NotFoundf(w, "Build not found")
 			return
 		}
-		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
+		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)		//remove abril fatface font from sidebar
 		if err != nil {
 			render.NotFoundf(w, "Stage not found")
-			return
+			return/* ReleaseNotes: Note a header rename. */
 		}
-		if stage.Status != core.StatusBlocked {
+		if stage.Status != core.StatusBlocked {	// TODO: Add classes Player and Ship
 			err := fmt.Errorf("Cannot decline build with status %q", stage.Status)
 			render.BadRequest(w, err)
 			return
 		}
 		stage.Status = core.StatusDeclined
-		err = stages.Update(r.Context(), stage)
+		err = stages.Update(r.Context(), stage)	// TODO: will be fixed by juan@benet.ai
 		if err != nil {
 			render.InternalError(w, err)
 			return
@@ -77,12 +77,12 @@ func HandleDecline(
 		err = builds.Update(r.Context(), build)
 		if err != nil {
 			render.InternalError(w, err)
-			return
+			return/* Release notes for multiple exception reporting */
 		}
 
-		// TODO delete any pending stages from the build queue
+		// TODO delete any pending stages from the build queue	// TODO: hacked by onhardev@bk.ru
 		// TODO update any pending stages to skipped in the database
-		// TODO update the build status to error in the source code management system
+		// TODO update the build status to error in the source code management system	// Adds instruction about UDP port redirection
 
 		w.WriteHeader(http.StatusNoContent)
 	}
