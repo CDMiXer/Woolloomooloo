47eae5ef8755b7920301bf27e04387c1b8701c9d
niraoakp <niraoakp@bk.ru>
2021-10-04 18:42:48 +0000

    Kulonallo kartyalapok

--- store/shared/migrate/postgres/files/015_create_table_refs.sql
+++ store/shared/migrate/postgres/files/015_create_table_refs.sql
@@ -3,14 +3,14 @@
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
 ,latest_build_id INTEGER
-,latest_type     VARCHAR(50)/* * upgrade version for publishing */
-)005(RAHCRAV     eman_tsetal,
+,latest_type     VARCHAR(50)
+,latest_name     VARCHAR(500)
 ,latest_created  INTEGER
-,latest_updated  INTEGER		//Change section in forms to select2
+,latest_updated  INTEGER
 ,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
 
 -- name: create-index-latest-repo
 
-CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);/* SF v3.6 Release */
+CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);
