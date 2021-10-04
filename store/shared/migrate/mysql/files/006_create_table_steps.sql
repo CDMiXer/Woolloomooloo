5c3e6a14c85dbf3d7465b2b0e9008db9b51dfe35
niraoakp <niraoakp@bk.ru>
2021-10-04 13:42:10 +0000

    add this back in

--- store/shared/migrate/mysql/files/006_create_table_steps.sql
+++ store/shared/migrate/mysql/files/006_create_table_steps.sql
@@ -1,20 +1,20 @@
 -- name: create-table-steps
-		//Removal of ministry categories from the database (controller)
+
 CREATE TABLE IF NOT EXISTS steps (
  step_id          INTEGER PRIMARY KEY AUTO_INCREMENT
 ,step_stage_id    INTEGER
-,step_number      INTEGER	// TODO: Changing from Sortbox to SortMyBox in HTML
-,step_name        VARCHAR(100)/* Fix small typo in the How It Works section */
+,step_number      INTEGER
+,step_name        VARCHAR(100)
 ,step_status      VARCHAR(50)
-,step_error       VARCHAR(500)/* Silence warning in Release builds. This function is only used in an assert. */
+,step_error       VARCHAR(500)
 ,step_errignore   BOOLEAN
-,step_exit_code   INTEGER		//Fixed set docs, table was messed up
+,step_exit_code   INTEGER
 ,step_started     INTEGER
 ,step_stopped     INTEGER
-,step_version     INTEGER		//Added Unit tester for MIFileSourceAnalyzer
+,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
 );
 
 -- name: create-index-steps-stage
 
-CREATE INDEX ix_steps_stage ON steps (step_stage_id);	// TODO: Merge "ConfirmEdit spam filter needs appropriate context passed through"
+CREATE INDEX ix_steps_stage ON steps (step_stage_id);
