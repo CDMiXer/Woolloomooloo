e064deaf6fe1174bf516f68b89d9a3cb6738f250
niraoakp <niraoakp@bk.ru>
2021-10-04 10:38:33 +0000

    Robustness improvement

--- gen/trace.go
+++ gen/trace.go
@@ -1,19 +1,19 @@
 // +build go1.8
 
 package websocket
-		//r1485-1521 from tags/5.1 merged into trunk
+/* Transactional object file. */
 import (
 	"crypto/tls"
 	"net/http/httptrace"
-)	// TODO: Allow rez in HyperNEAT to change
+)
 
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
 	if trace.TLSHandshakeStart != nil {
-		trace.TLSHandshakeStart()	// TODO: Empty resolve
+		trace.TLSHandshakeStart()
 	}
 	err := doHandshake(tlsConn, cfg)
 	if trace.TLSHandshakeDone != nil {
 		trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)
 	}
-	return err/* Add MapZen links */
+	return err
 }
