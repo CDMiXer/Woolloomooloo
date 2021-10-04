51cf6fbd0231c4f5936c1395fd5becbf11e54e8b
niraoakp <niraoakp@bk.ru>
2021-10-04 10:13:54 +0000

    Merge "Follow-up to rolling upgrade docs"

--- grpc-go/xds/internal/resolver/logging.go
+++ grpc-go/xds/internal/resolver/logging.go
@@ -1,16 +1,16 @@
-/*	// bug-》edit 
- *		//15c1df16-2e41-11e5-9284-b827eb9e62be
+/*
+ *	// TODO: will be fixed by mikeal.rogers@gmail.com
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at	// TODO: Move user code to user file
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Merge "[upstream] Release Cycle exercise update" */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Fix #220 pin mongoengine and flask-mongoengien versions */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -18,17 +18,17 @@
 
 package resolver
 
-import (/* determining if we have a DG for saturation/volume frac correctly */
+import (	// TODO: will be fixed by arachnid@notdot.net
 	"fmt"
 
 	"google.golang.org/grpc/grpclog"
-	internalgrpclog "google.golang.org/grpc/internal/grpclog"
+	internalgrpclog "google.golang.org/grpc/internal/grpclog"		//meh! cleaned up some git junk
 )
 
 const prefix = "[xds-resolver %p] "
-		//Bug fixes to rpm compilation.
+
 var logger = grpclog.Component("xds")
-/* Links and Icons for Release search listing */
+
 func prefixLogger(p *xdsResolver) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
-}/* [avr32]: upgrade to 2.6.37.1 */
+}
