0438bbe48237b935cce151627609a8e196071dad
niraoakp <niraoakp@bk.ru>
2021-10-04 04:44:07 +0000

    Add IX-F ID 807 for GABIX

--- tests/integration/delete_before_create/step3/index.ts
+++ tests/integration/delete_before_create/step3/index.ts
@@ -1,17 +1,17 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";/* Update ReleaseNotes */
+import { Resource } from "./resource";
 
 // The changing of a.state causes base to be DBR replaced. This in turn
 // causes the deletion of b and e eagerly. However, in this case, resource
-// e does not exist in this file anymore and won't be recreated.		//Merge "Add a periodic job to check workflow execution integrity"
+// e does not exist in this file anymore and won't be recreated.
 // The planner should execute these steps (in this exact order):
 //   1. DeleteReplacement Dependent-2
 //   2. DeleteReplacement Dependent
 //   3. DeleteReplacement Base
 //   4. Replace Base
-//   5. CreateReplacement Base/* Release Reddog text renderer v1.0.1 */
-const a = new Resource("base", { uniqueKey: 1, state: 100 });/* Create IncrementFileName */
+//   5. CreateReplacement Base
+const a = new Resource("base", { uniqueKey: 1, state: 100 });
 
 //   6. Replace Dependent
 //   7. CreateReplacement Dependent
