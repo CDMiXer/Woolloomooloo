4c288a1492a72a13f26e64e673872f26824d333a
niraoakp <niraoakp@bk.ru>
2021-10-04 03:57:58 +0000

    Ajustes e novo atributo com texto de obs

--- handler/api/repos/builds/deploys/create_test.go
+++ handler/api/repos/builds/deploys/create_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Remove unused serverjar
+// Copyright 2019 Drone.IO Inc. All rights reserved./* b8d2420a-2eae-11e5-addd-7831c1d44c14 */
+// Use of this source code is governed by the Drone Non-Commercial License/* Firefox NL 41.0 */
 // that can be found in the LICENSE file.
-
+	// TODO: Rename degrees.html to d3-2/degrees.html
 package deploys
