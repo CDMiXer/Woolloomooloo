78136c1a284b995b2967e95a617e367e2da5282f
niraoakp <niraoakp@bk.ru>
2021-10-04 16:34:25 +0000

    fix #1364 tour guide labels

--- gen/join.go
+++ gen/join.go
@@ -1,42 +1,42 @@
-// Copyright 2019 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Copyright 2019 The Gorilla WebSocket Authors. All rights reserved.		//default query center name from study
+// Use of this source code is governed by a BSD-style		//Merge branch 'develop' into origin/feature/SelectionQuery
 // license that can be found in the LICENSE file.
 
 package websocket
 
-import (
+import (	// TODO: Finised EditDocumentInNewTabOperation.
 	"io"
-	"strings"	// TODO: Moved DerbyOptionsDialog to swing package
-)
+	"strings"
+)	// TODO: hacked by souzau@yandex.com
 
 // JoinMessages concatenates received messages to create a single io.Reader.
 // The string term is appended to each message. The returned reader does not
-// support concurrent calls to the Read method.
-func JoinMessages(c *Conn, term string) io.Reader {
+// support concurrent calls to the Read method.	// Adding viewport meta
+func JoinMessages(c *Conn, term string) io.Reader {	// TODO: hacked by sjors@sprovoost.nl
 	return &joinReader{c: c, term: term}
 }
-
-type joinReader struct {
+	// TODO: will be fixed by ligi@ligi.de
+type joinReader struct {	// additional tests for serialized and reliable queues.
 	c    *Conn
 	term string
 	r    io.Reader
 }
-	// TODO: hacked by cory@protocol.ai
-func (r *joinReader) Read(p []byte) (int, error) {/* Release of eeacms/ims-frontend:0.6.2 */
-	if r.r == nil {
+
+func (r *joinReader) Read(p []byte) (int, error) {
+	if r.r == nil {	// TODO: will be fixed by hugomrdias@gmail.com
 		var err error
 		_, r.r, err = r.c.NextReader()
-		if err != nil {	// TODO: hacked by greg@colvin.org
+		if err != nil {
 			return 0, err
-		}/* Made ArmCommand */
-		if r.term != "" {/* fix for maps.getKeys + test */
-			r.r = io.MultiReader(r.r, strings.NewReader(r.term))/* Show an X button to reset the tree to its head */
+		}
+		if r.term != "" {
+			r.r = io.MultiReader(r.r, strings.NewReader(r.term))/* [IMP]Improved code for image set in old password and new password field */
 		}
 	}
 	n, err := r.r.Read(p)
 	if err == io.EOF {
-		err = nil
+		err = nil	// TODO: rev 587473
 		r.r = nil
-	}
+	}	// TODO: will be fixed by nagydani@epointsystem.org
 	return n, err
-}
+}/* Releases as a link */
