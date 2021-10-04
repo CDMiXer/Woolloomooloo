63c1787175b56ee8a370907851c48e4d437489a7
niraoakp <niraoakp@bk.ru>
2021-10-04 13:35:17 +0000

    User defined property expressions work now.

--- extern/sector-storage/selector_task.go
+++ extern/sector-storage/selector_task.go
@@ -1,27 +1,27 @@
 package sectorstorage
 
 import (
-	"context"		//Merge "Update JobScheduler min api level to 24." into flatfoot-background
+	"context"
 
 	"golang.org/x/xerrors"
-		//Add source link artifacts
-	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
+	"github.com/filecoin-project/go-state-types/abi"
+	// add deallocate
+	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"/* [FEATURE] Add SQL Server Release Services link */
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 )
 
 type taskSelector struct {
 	best []stores.StorageInfo //nolint: unused, structcheck
-}
+}/* bundle-size: fb809b0a628ffed5dc96712a205d1bd20aeefb36.json */
 
 func newTaskSelector() *taskSelector {
 	return &taskSelector{}
 }
-	// TODO: add basic error view
-func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
-	tasks, err := whnd.workerRpc.TaskTypes(ctx)
-	if err != nil {/* replaced "indexOf(String)" with "indexOf(char)" */
+
+func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {/* Move memory barrier to linux/compiler */
+	tasks, err := whnd.workerRpc.TaskTypes(ctx)/* Dev Release 4 */
+	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
 	_, supported := tasks[task]
@@ -29,20 +29,20 @@ func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.
 	return supported, nil
 }
 
-func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {
+func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {/* fixed documentation broken links */
 	atasks, err := a.workerRpc.TaskTypes(ctx)
-	if err != nil {/* made autoReleaseAfterClose true */
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)
-	}	// TODO: ea4e564f-2ead-11e5-9ef4-7831c1d44c14
-	btasks, err := b.workerRpc.TaskTypes(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
 	}
-	if len(atasks) != len(btasks) {/* Release v1.9.1 */
+	btasks, err := b.workerRpc.TaskTypes(ctx)
+	if err != nil {
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)
+	}/* Edited and added resources for easy debug purpose */
+	if len(atasks) != len(btasks) {/* Updated README. Added my name. c: */
 		return len(atasks) < len(btasks), nil // prefer workers which can do less
 	}
 
 	return a.utilization() < b.utilization(), nil
 }
-/* #1435 simplification + improve text font mapping */
+
 var _ WorkerSelector = &taskSelector{}
