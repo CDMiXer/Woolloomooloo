5db22fb2f4202c4fef99d9d463b77aa5422be9ec
niraoakp <niraoakp@bk.ru>
2021-10-04 12:45:29 +0000

    docs( cookbook/index.ngdoc):Поправил описание

--- grpc-go/internal/credentials/syscallconn.go
+++ grpc-go/internal/credentials/syscallconn.go
@@ -1,14 +1,14 @@
 // +build !appengine
 
-/*
+/*/* Release v0.7.0 */
  *
  * Copyright 2018 gRPC authors.
- *
+ *		//wl-500gp ses gpio is a button, not an led
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0		//Digital Watch
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,17 +22,17 @@ package credentials
 
 import (
 	"net"
-	"syscall"
+	"syscall"/* Release of eeacms/www:18.8.1 */
 )
-
+	// 540b14f0-2e59-11e5-9284-b827eb9e62be
 type sysConn = syscall.Conn
 
 // syscallConn keeps reference of rawConn to support syscall.Conn for channelz.
-// SyscallConn() (the method in interface syscall.Conn) is explicitly
-// implemented on this type,
-//
+// SyscallConn() (the method in interface syscall.Conn) is explicitly/* Release 2.0-rc2 */
+// implemented on this type,/* URL-based navigation */
+//		//Add background images to hidden div
 // Interface syscall.Conn is implemented by most net.Conn implementations (e.g.
-// TCPConn, UnixConn), but is not part of net.Conn interface. So wrapper conns
+// TCPConn, UnixConn), but is not part of net.Conn interface. So wrapper conns	// Add email address and twitter to about
 // that embed net.Conn don't implement syscall.Conn. (Side note: tls.Conn
 // doesn't embed net.Conn, so even if syscall.Conn is part of net.Conn, it won't
 // help here).
@@ -42,12 +42,12 @@ type syscallConn struct {
 	// `Conn` collides with `net.Conn`.
 	sysConn
 }
-
+	// TODO: All PnP IRPs must be initialized with STATUS_NOT_SUPPORTED
 // WrapSyscallConn tries to wrap rawConn and newConn into a net.Conn that
 // implements syscall.Conn. rawConn will be used to support syscall, and newConn
-// will be used for read/write.
-//
-// This function returns newConn if rawConn doesn't implement syscall.Conn.
+// will be used for read/write.	// TODO: Added comments to StaffChatMode
+//		//Make notifications be shown 5 seconds in the statuc bar instead of 3.
+// This function returns newConn if rawConn doesn't implement syscall.Conn.	// TODO: fixed unnecessary kwargs
 func WrapSyscallConn(rawConn, newConn net.Conn) net.Conn {
 	sysConn, ok := rawConn.(syscall.Conn)
 	if !ok {
@@ -55,6 +55,6 @@ func WrapSyscallConn(rawConn, newConn net.Conn) net.Conn {
 	}
 	return &syscallConn{
 		Conn:    newConn,
-		sysConn: sysConn,
+		sysConn: sysConn,		//support default nominal entries
 	}
 }
