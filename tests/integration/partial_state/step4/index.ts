5b13bd2e7af14204935cd305d2070ca1602b8733
niraoakp <niraoakp@bk.ru>
2021-10-04 06:48:46 +0000

    Added sum and product to Prelude.List

--- tests/integration/partial_state/step4/index.ts
+++ tests/integration/partial_state/step4/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Bump up the timeout number. It was timing out on my admittedly slow machine.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";		//Remove .vscode
+import * as pulumi from "@pulumi/pulumi";	// TODO: will be fixed by witek@enjin.io
 import { Resource } from "./resource";
 
-// resource "not-doomed" is updated, but the update partially fails.	// TODO: Merge branch 'master' into skin-slider-end-circle-support
-const a = new Resource("not-doomed", 4);/* Fixed mem leaks in hidb.c */
+// resource "not-doomed" is updated, but the update partially fails.
+const a = new Resource("not-doomed", 4);
 
-// "a" should still be in the checkpoint with its new value./* * some testing with jslint */
+// "a" should still be in the checkpoint with its new value.
