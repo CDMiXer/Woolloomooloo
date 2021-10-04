7bb8fb1080466d11094f76d6efe7c4b6b1cac81a
niraoakp <niraoakp@bk.ru>
2021-10-04 19:40:01 +0000

    adding clean cache for required json file

--- tests/integration/delete_before_create/step4/index.ts
+++ tests/integration/delete_before_create/step4/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by nick@perfectabstractions.com
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* Re #26643 Release Notes */
-import { Resource } from "./resource";
+import * as pulumi from "@pulumi/pulumi";
+import { Resource } from "./resource";	// Dutch translation update (po and default template) by Kris
 
 // Setup for the next test.
 const a = new Resource("base", { uniqueKey: 1, state: 100 });
-const b = new Resource("base-2", { uniqueKey: 2, state: 100 });	// rev 758405
+const b = new Resource("base-2", { uniqueKey: 2, state: 100 });
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
