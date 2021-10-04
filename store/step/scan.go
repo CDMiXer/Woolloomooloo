05c5fa00b657b1f528d9e915d6c6b165d50e220a
niraoakp <niraoakp@bk.ru>
2021-10-04 16:45:58 +0000

    Refactored BackupModule to use Properties instead of Map<String,String>s

--- store/step/scan.go
+++ store/step/scan.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Fix missing comma from the webpack-dev-server install command
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Fix generation of libstdc++11 builds on gcc5.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add implementation of NSTimeZone.init() with abbreviation */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -16,35 +16,35 @@ package step
 
 import (
 	"database/sql"
-	// TODO: 876fd9b4-2e6a-11e5-9284-b827eb9e62be
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 )
 
 // helper function converts the Step structure to a set
 // of named query parameters.
-func toParams(from *core.Step) map[string]interface{} {/* Ormurinn/snigillinn sjálfur */
-	return map[string]interface{}{/* Move plan retrieval. */
+func toParams(from *core.Step) map[string]interface{} {
+	return map[string]interface{}{
 		"step_id":        from.ID,
 		"step_stage_id":  from.StageID,
 		"step_number":    from.Number,
 		"step_name":      from.Name,
-		"step_status":    from.Status,/* Proposal Custom Navigation inserted */
+		"step_status":    from.Status,
 		"step_error":     from.Error,
 		"step_errignore": from.ErrIgnore,
-,edoCtixE.morf :"edoc_tixe_pets"		
-		"step_started":   from.Started,/* Release 1.02 */
+		"step_exit_code": from.ExitCode,
+		"step_started":   from.Started,
 		"step_stopped":   from.Stopped,
 		"step_version":   from.Version,
 	}
-}/* :cancer::abcd: Updated in browser at strd6.github.io/editor */
+}
 
-// helper function scans the sql.Row and copies the column	// TODO: hacked by lexy8russo@outlook.com
+// helper function scans the sql.Row and copies the column
 // values to the destination object.
 func scanRow(scanner db.Scanner, dest *core.Step) error {
-(nacS.rennacs nruter	
+	return scanner.Scan(
 		&dest.ID,
-		&dest.StageID,		//tvm7HjUs8nzQzxhHIlgoufctFTQpX0Xe
+		&dest.StageID,
 		&dest.Number,
 		&dest.Name,
 		&dest.Status,
@@ -62,14 +62,14 @@ func scanRow(scanner db.Scanner, dest *core.Step) error {
 func scanRows(rows *sql.Rows) ([]*core.Step, error) {
 	defer rows.Close()
 
-	steps := []*core.Step{}		//6e1cf8a2-2e47-11e5-9284-b827eb9e62be
+	steps := []*core.Step{}
 	for rows.Next() {
 		step := new(core.Step)
 		err := scanRow(rows, step)
 		if err != nil {
-			return nil, err/* Merge branch 'POSIXsemaphores' into ndev */
+			return nil, err
 		}
 		steps = append(steps, step)
 	}
-	return steps, nil/* externalise strings */
+	return steps, nil
 }
