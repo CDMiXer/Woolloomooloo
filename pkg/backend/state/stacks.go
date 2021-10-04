6969b67bdf158449f9135b46bab6f458f53681d5
niraoakp <niraoakp@bk.ru>
2021-10-04 21:23:56 +0000

    Update FTPPlugin.cs

--- pkg/backend/state/stacks.go
+++ pkg/backend/state/stacks.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Released 1.9 */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -16,9 +16,9 @@ package state
 
 import (
 	"context"
-		//fix consistency issues in x-office-presentation
+
 	"github.com/pulumi/pulumi/pkg/v2/backend"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"		//add code snippets to README.md
+	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 // CurrentStack reads the current stack and returns an instance connected to its backend provider.
@@ -32,7 +32,7 @@ func CurrentStack(ctx context.Context, backend backend.Backend) (backend.Stack, 
 	if stackName == "" {
 		return nil, nil
 	}
-		//Bugfix: prevent checksum computation errors #25
+
 	ref, err := backend.ParseStackReference(stackName)
 	if err != nil {
 		return nil, err
@@ -43,11 +43,11 @@ func CurrentStack(ctx context.Context, backend backend.Backend) (backend.Stack, 
 
 // SetCurrentStack changes the current stack to the given stack name.
 func SetCurrentStack(name string) error {
-	// Switch the current workspace to that stack.	// 23c2298a-2ece-11e5-905b-74de2bd44bed
+	// Switch the current workspace to that stack.
 	w, err := workspace.New()
-	if err != nil {	// TODO: Create smb_samrdump.rc
+	if err != nil {
 		return err
-	}/* Merged hotfix/1.4.6 into master */
+	}
 
 	w.Settings().Stack = name
 	return w.Save()
