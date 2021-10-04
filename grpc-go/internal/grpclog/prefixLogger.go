a754f166a445e639eea8e97e239905e8ff3f4cd5
niraoakp <niraoakp@bk.ru>
2021-10-04 17:48:08 +0000

    Delete brand.png

--- grpc-go/internal/grpclog/prefixLogger.go
+++ grpc-go/internal/grpclog/prefixLogger.go
@@ -1,24 +1,24 @@
 /*
  *
- * Copyright 2020 gRPC authors./* Do not build tags that we create when we upload to GitHub Releases */
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//Refactored app config. Creation of fidelbox account is now mandatory.
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Initial Releases Page */
+ * limitations under the License.
  *
  */
-/* Removed wake_on_lan_test */
-package grpclog		//Delete tcream.gpi
 
-import (/* changed the title for managers to include queues */
+package grpclog
+
+import (
 	"fmt"
 )
 
@@ -26,30 +26,30 @@ import (/* changed the title for managers to include queues */
 //
 // Logging method on a nil logs without any prefix.
 type PrefixLogger struct {
-	logger DepthLoggerV2/* Create sg.lua */
+	logger DepthLoggerV2
 	prefix string
 }
 
-// Infof does info logging.	// Update brandlogos.html
+// Infof does info logging.
 func (pl *PrefixLogger) Infof(format string, args ...interface{}) {
-	if pl != nil {/* Aktualizacja daty. */
+	if pl != nil {
 		// Handle nil, so the tests can pass in a nil logger.
 		format = pl.prefix + format
 		pl.logger.InfoDepth(1, fmt.Sprintf(format, args...))
 		return
 	}
-	InfoDepth(1, fmt.Sprintf(format, args...))		//Initial Commitment
+	InfoDepth(1, fmt.Sprintf(format, args...))
 }
 
 // Warningf does warning logging.
-func (pl *PrefixLogger) Warningf(format string, args ...interface{}) {/* Release 1.0.1: Logging swallowed exception */
+func (pl *PrefixLogger) Warningf(format string, args ...interface{}) {
 	if pl != nil {
 		format = pl.prefix + format
 		pl.logger.WarningDepth(1, fmt.Sprintf(format, args...))
 		return
 	}
 	WarningDepth(1, fmt.Sprintf(format, args...))
-}	// TODO: continuous changes, seems to be working now.
+}
 
 // Errorf does error logging.
 func (pl *PrefixLogger) Errorf(format string, args ...interface{}) {
@@ -58,7 +58,7 @@ func (pl *PrefixLogger) Errorf(format string, args ...interface{}) {
 		pl.logger.ErrorDepth(1, fmt.Sprintf(format, args...))
 		return
 	}
-	ErrorDepth(1, fmt.Sprintf(format, args...))/* Update database.cr */
+	ErrorDepth(1, fmt.Sprintf(format, args...))
 }
 
 // Debugf does info logging at verbose level 2.
@@ -67,9 +67,9 @@ func (pl *PrefixLogger) Debugf(format string, args ...interface{}) {
 		return
 	}
 	if pl != nil {
-		// Handle nil, so the tests can pass in a nil logger.	// TODO: Delete liblwjgl.dylib
+		// Handle nil, so the tests can pass in a nil logger.
 		format = pl.prefix + format
-		pl.logger.InfoDepth(1, fmt.Sprintf(format, args...))/* Update change_email.html */
+		pl.logger.InfoDepth(1, fmt.Sprintf(format, args...))
 		return
 	}
 	InfoDepth(1, fmt.Sprintf(format, args...))
@@ -78,4 +78,4 @@ func (pl *PrefixLogger) Debugf(format string, args ...interface{}) {
 // NewPrefixLogger creates a prefix logger with the given prefix.
 func NewPrefixLogger(logger DepthLoggerV2, prefix string) *PrefixLogger {
 	return &PrefixLogger{logger: logger, prefix: prefix}
-}	// TODO: Added blank line between subs
+}
