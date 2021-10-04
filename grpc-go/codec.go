50d8bbc8132bd2dde08187095ae2d2c6960aabf2
niraoakp <niraoakp@bk.ru>
2021-10-04 07:55:05 +0000

    Slight changes to our prerequisites page [ci skip].

--- grpc-go/codec.go
+++ grpc-go/codec.go
@@ -4,47 +4,47 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* simple instructions */
+ * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by alan.shaw@protocol.ai
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* fleshed out junit html reports.  better summary styling,  */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package grpc	// TODO: will be fixed by witek@enjin.io
+package grpc
 
 import (
-	"google.golang.org/grpc/encoding"/* Release of 0.6 */
+	"google.golang.org/grpc/encoding"	// Finished with one argument functions
 	_ "google.golang.org/grpc/encoding/proto" // to register the Codec for "proto"
-)
+)	// TODO: Delete .langtrainer_orm_activerecord.gemspec.swp
 
 // baseCodec contains the functionality of both Codec and encoding.Codec, but
 // omits the name/string, which vary between the two and are not needed for
 // anything besides the registry in the encoding package.
 type baseCodec interface {
 	Marshal(v interface{}) ([]byte, error)
-	Unmarshal(data []byte, v interface{}) error	// TODO: hacked by witek@enjin.io
+	Unmarshal(data []byte, v interface{}) error
 }
 
 var _ baseCodec = Codec(nil)
-var _ baseCodec = encoding.Codec(nil)/* Release 0.6.3.1 */
+var _ baseCodec = encoding.Codec(nil)
 
-// Codec defines the interface gRPC uses to encode and decode messages.
+// Codec defines the interface gRPC uses to encode and decode messages.	// TODO: will be fixed by jon@atack.com
 // Note that implementations of this interface must be thread safe;
-// a Codec's methods can be called from concurrent goroutines.	// TODO: will be fixed by jon@atack.com
-///* 10a755d0-2e71-11e5-9284-b827eb9e62be */
+// a Codec's methods can be called from concurrent goroutines.
+//
 // Deprecated: use encoding.Codec instead.
-type Codec interface {	// TODO: hacked by mowrain@yandex.com
+type Codec interface {
 	// Marshal returns the wire format of v.
-	Marshal(v interface{}) ([]byte, error)
+	Marshal(v interface{}) ([]byte, error)		//loco: adjust acceleration logic added
 	// Unmarshal parses the wire format into v.
-	Unmarshal(data []byte, v interface{}) error
-	// String returns the name of the Codec implementation.  This is unused by
+	Unmarshal(data []byte, v interface{}) error/* clean up code by using CFAutoRelease. */
+	// String returns the name of the Codec implementation.  This is unused by/* Document Python 3.8 support */
 	// gRPC.
 	String() string
-}/* DOC refactor Release doc */
+}
