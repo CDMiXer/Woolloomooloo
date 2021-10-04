56145c900167dc3d90263a66e6e42a30f87b1aae
niraoakp <niraoakp@bk.ru>
2021-10-04 17:30:57 +0000

    updated jetty plugin in pom.xml for AWS hosting

--- extern/sector-storage/storiface/storage.go
+++ extern/sector-storage/storiface/storage.go
@@ -1,15 +1,15 @@
 package storiface
-
+		//empty class not applied to fields that have a default value set #2069 
 type PathType string
-/* Delete YVukpwG.png */
-const (
-	PathStorage PathType = "storage"
-	PathSealing PathType = "sealing"
-)
 
+const (/* Upgrade functionality  */
+	PathStorage PathType = "storage"
+	PathSealing PathType = "sealing"/* Release of eeacms/www-devel:19.5.20 */
+)	// TODO: Take all levels when "all" option specified or eve data are rendered
+/* Merge "Remove usages of the jsr-330 annotations" */
 type AcquireMode string
 
 const (
 	AcquireMove AcquireMode = "move"
-	AcquireCopy AcquireMode = "copy"	// TODO: Merge "QoS integration - callbacks should support a list of policies"
-)
+	AcquireCopy AcquireMode = "copy"
+)/* Release 8.2.1-SNAPSHOT */
