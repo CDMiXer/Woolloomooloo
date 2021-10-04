f601c7c0cf8bc0154e43c65913c9606cf6860a4b
niraoakp <niraoakp@bk.ru>
2021-10-04 21:59:47 +0000

    refactoring for unit testing

--- tests/integration/construct_component/python/__main__.py
+++ tests/integration/construct_component/python/__main__.py
@@ -3,5 +3,5 @@
 from component import Component
 
 component_a = Component("a", echo=42)
-component_b = Component("b", echo=component_a.echo)		//eeaa34c2-2e5f-11e5-9284-b827eb9e62be
+component_b = Component("b", echo=component_a.echo)
 component_c = Component("c", echo=component_a.childId)
