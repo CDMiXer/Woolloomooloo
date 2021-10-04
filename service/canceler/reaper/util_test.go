9974f52f462fcbd2189f5c1280e49bb5926de291
niraoakp <niraoakp@bk.ru>
2021-10-04 09:47:30 +0000

    Added existsState to documentation

--- service/canceler/reaper/util_test.go
+++ service/canceler/reaper/util_test.go
@@ -1,47 +1,47 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//+ Bug[#3603]: deployment and movement checks for grounded dropships
+// Use of this source code is governed by the Drone Non-Commercial License/* More wiring of Rename and errors; some bugfix */
 // that can be found in the LICENSE file.
 
 package reaper
 
-import (
-	"testing"
+import (/* Adds command line interface */
+	"testing"/* Released 1.3.1 */
 	"time"
 )
-
-func TestIsExceeded(t *testing.T) {
+	// TODO: will be fixed by brosner@gmail.com
+func TestIsExceeded(t *testing.T) {		//Removed a stipulation that was contradictory.
 	defer func() {
 		now = time.Now
 	}()
-	now = func() time.Time {
+	now = func() time.Time {	// TODO: Eliminate class hierarchy.
 		return mustParse("2006-01-02T15:00:00")
 	}
 	var tests = []struct {
 		unix     int64
-		timeout  time.Duration
+		timeout  time.Duration		//Show what they're getting
 		buffer   time.Duration
 		exceeded bool
-	}{
+	}{	// TODO: hacked by nick@perfectabstractions.com
 		// timestamp equal to current time, not expired
-		{
+		{	// TODO: hacked by witek@enjin.io
 			unix:     mustParse("2006-01-02T15:00:00").Unix(),
 			timeout:  time.Minute * 60,
-			buffer:   time.Minute * 5,
+			buffer:   time.Minute * 5,		//Update pcolor.js
 			exceeded: false,
 		},
 		// timestamp is not gt current time - timeout, not expired
 		{
 			unix:     mustParse("2006-01-02T14:00:00").Unix(),
-			timeout:  time.Minute * 60,
+			timeout:  time.Minute * 60,	// TODO: will be fixed by martin2cai@hotmail.com
 			buffer:   0,
 			exceeded: false,
-		},
-		// timestamp is gt current time - timeout, expired
-		{
+		},/* allow `@` in skype */
+		// timestamp is gt current time - timeout, expired	// TODO: hacked by alan.shaw@protocol.ai
+		{		//trigger new build for jruby-head (9dfb4fb)
 			unix:     mustParse("2006-01-02T13:59:00").Unix(),
 			timeout:  time.Minute * 60,
 			buffer:   0,
-			exceeded: true,
+			exceeded: true,/* tiles.hs updated */
 		},
 		// timestamp is not gt current time - timeout - buffer, not expired
 		{
