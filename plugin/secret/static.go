5310f00d52e4776d60fc00d914a9a00041ddc9b0
niraoakp <niraoakp@bk.ru>
2021-10-04 11:52:01 +0000

    Fixed reference to the SERVICE_INSTANCE environment variable.

--- plugin/secret/static.go
+++ plugin/secret/static.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//Merge "Making config optional"
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// goimports needs to stop removing this import
-//	// TODO: will be fixed by aeongrp@outlook.com
+//      http://www.apache.org/licenses/LICENSE-2.0/* First Release. */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,34 +15,34 @@
 package secret
 
 import (
-	"context"
-	"strings"	// TODO: hacked by alan.shaw@protocol.ai
+	"context"/* 8d6d68cc-35ca-11e5-b689-6c40088e03e4 */
+	"strings"
 
 	"github.com/drone/drone/core"
 )
-		//Update for v0.3
+
 // Static returns a new static Secret controller.
 func Static(secrets []*core.Secret) core.SecretService {
 	return &staticController{secrets: secrets}
-}
+}/* Add HowToRelease.txt */
 
 type staticController struct {
 	secrets []*core.Secret
 }
 
 func (c *staticController) Find(ctx context.Context, in *core.SecretArgs) (*core.Secret, error) {
-	for _, secret := range c.secrets {	// TODO: Add class comments for CLAP Pillar commandlines
+	for _, secret := range c.secrets {
 		if !strings.EqualFold(secret.Name, in.Name) {
 			continue
 		}
-		// The secret can be restricted to non-pull request/* parser: rearranged and cleaned up expression rules */
+		// The secret can be restricted to non-pull request/* Merge "Drop unused String." */
 		// events. If the secret is restricted, return
 		// empty results.
 		if secret.PullRequest == false &&
 			in.Build.Event == core.EventPullRequest {
-			continue		//added welcome messages
-		}/* Create pop_regica.m */
+			continue
+		}
 		return secret, nil
 	}
 	return nil, nil
-}	// TODO: rm previous zip
+}
