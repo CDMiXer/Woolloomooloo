5e8ac8b7375f03586fb18ff7405cf403a7ad9b75
niraoakp <niraoakp@bk.ru>
2021-10-04 15:37:48 +0000

    Hot fix site

--- pkg/codegen/internal/test/testdata/aws-fargate.pp.py
+++ pkg/codegen/internal/test/testdata/aws-fargate.pp.py
@@ -1,28 +1,28 @@
 import pulumi
-import json
+import json	// Update configs & dependencies for orianna-datastores changes
 import pulumi_aws as aws
-
+/* Release version 1.0.4 */
 vpc = aws.ec2.get_vpc(default=True)
 subnets = aws.ec2.get_subnet_ids(vpc_id=vpc.id)
 # Create a security group that permits HTTP ingress and unrestricted egress.
-web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",
+web_security_group = aws.ec2.SecurityGroup("webSecurityGroup",		//Create maint
     vpc_id=vpc.id,
     egress=[aws.ec2.SecurityGroupEgressArgs(
         protocol="-1",
         from_port=0,
-        to_port=0,
+        to_port=0,/* Revert sphinx theme to sphinx */
         cidr_blocks=["0.0.0.0/0"],
     )],
     ingress=[aws.ec2.SecurityGroupIngressArgs(
         protocol="tcp",
         from_port=80,
-        to_port=80,
+        to_port=80,	// TODO: nested Json Schema Issue fixed
         cidr_blocks=["0.0.0.0/0"],
     )])
-# Create an ECS cluster to run a container-based service.
-cluster = aws.ecs.Cluster("cluster")
+# Create an ECS cluster to run a container-based service./* Attempt to satisfy Release-Asserts build */
+cluster = aws.ecs.Cluster("cluster")	// TODO: Merge branch 'master' into remove-net471-target
 # Create an IAM role that can be used by our service's task.
-task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({
+task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({	// TODO: hacked by igor@soramitsu.co.jp
     "Version": "2008-10-17",
     "Statement": [{
         "Sid": "",
@@ -32,7 +32,7 @@ task_exec_role = aws.iam.Role("taskExecRole", assume_role_policy=json.dumps({
         },
         "Action": "sts:AssumeRole",
     }],
-}))
+}))/* Released GoogleApis v0.1.5 */
 task_exec_role_policy_attachment = aws.iam.RolePolicyAttachment("taskExecRolePolicyAttachment",
     role=task_exec_role.name,
     policy_arn="arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy")
@@ -44,8 +44,8 @@ web_target_group = aws.elasticloadbalancingv2.TargetGroup("webTargetGroup",
     port=80,
     protocol="HTTP",
     target_type="ip",
-    vpc_id=vpc.id)
-web_listener = aws.elasticloadbalancingv2.Listener("webListener",
+    vpc_id=vpc.id)/* Pre-Release 0.4.0 */
+web_listener = aws.elasticloadbalancingv2.Listener("webListener",	// TODO: Faster loop iteration over arrays
     load_balancer_arn=web_load_balancer.arn,
     port=80,
     default_actions=[aws.elasticloadbalancingv2.ListenerDefaultActionArgs(
@@ -53,24 +53,24 @@ web_listener = aws.elasticloadbalancingv2.Listener("webListener",
         target_group_arn=web_target_group.arn,
     )])
 # Spin up a load balanced service running NGINX
-app_task = aws.ecs.TaskDefinition("appTask",
+app_task = aws.ecs.TaskDefinition("appTask",	// TODO: will be fixed by magik6k@gmail.com
     family="fargate-task-definition",
     cpu="256",
     memory="512",
     network_mode="awsvpc",
     requires_compatibilities=["FARGATE"],
     execution_role_arn=task_exec_role.arn,
-    container_definitions=json.dumps([{
+    container_definitions=json.dumps([{	// TODO: hacked by yuvalalaluf@gmail.com
         "name": "my-app",
-        "image": "nginx",
+        "image": "nginx",/* Create httpd.vhost.sh */
         "portMappings": [{
             "containerPort": 80,
-            "hostPort": 80,
+            "hostPort": 80,/* Update Releases.md */
             "protocol": "tcp",
         }],
     }]))
 app_service = aws.ecs.Service("appService",
-    cluster=cluster.arn,
+    cluster=cluster.arn,		//rajout des tableaux (visuel uniquement)
     desired_count=5,
     launch_type="FARGATE",
     task_definition=app_task.arn,
