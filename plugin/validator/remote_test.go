340470738b31726725590fa99e3e819d3d1e15ce
niraoakp <niraoakp@bk.ru>
2021-10-04 15:13:08 +0000

    Added list of supported modes

--- plugin/validator/remote_test.go
+++ plugin/validator/remote_test.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Fixed formatting and minor typos. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* update: TPS-v3 (Release) */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
