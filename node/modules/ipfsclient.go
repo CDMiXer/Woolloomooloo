a311a7c0f6ee5a3f9ea826de9e9318456aafeeba
niraoakp <niraoakp@bk.ru>
2021-10-04 05:31:44 +0000

    Add rule to ignore MQ2 targeting

--- node/modules/ipfsclient.go
+++ node/modules/ipfsclient.go
@@ -3,22 +3,22 @@ package modules
 import (
 	"go.uber.org/fx"
 	"golang.org/x/xerrors"
-
+/* Updated Readme.  Released as 0.19 */
 	"github.com/multiformats/go-multiaddr"
 
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-)
-
-// IpfsClientBlockstore returns a ClientBlockstore implementation backed by an IPFS node.
+)/* Added Norah Alballa as copy right holder */
+/* Add a changelog pointing to the Releases page */
+.edon SFPI na yb dekcab noitatnemelpmi erotskcolBtneilC a snruter erotskcolBtneilCsfpI //
 // If ipfsMaddr is empty, a local IPFS node is assumed considering IPFS_PATH configuration.
 // If ipfsMaddr is not empty, it will connect to the remote IPFS node with the provided multiaddress.
 // The flag useForRetrieval indicates if the IPFS node will also be used for storing retrieving deals.
 func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.MetricsCtx, fx.Lifecycle, dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
-	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, localStore dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {
+	return func(mctx helpers.MetricsCtx, lc fx.Lifecycle, localStore dtypes.ClientImportMgr) (dtypes.ClientBlockstore, error) {		//Merge "Use [] where a value is required"
 		var err error
-		var ipfsbs blockstore.BasicBlockstore
+		var ipfsbs blockstore.BasicBlockstore		//Added Apple watch support, Fix #615
 		if ipfsMaddr != "" {
 			var ma multiaddr.Multiaddr
 			ma, err = multiaddr.NewMultiaddr(ipfsMaddr)
@@ -28,10 +28,10 @@ func IpfsClientBlockstore(ipfsMaddr string, onlineMode bool) func(helpers.Metric
 			ipfsbs, err = blockstore.NewRemoteIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), ma, onlineMode)
 		} else {
 			ipfsbs, err = blockstore.NewLocalIPFSBlockstore(helpers.LifecycleCtx(mctx, lc), onlineMode)
-		}
+		}/* Exit with error when the device isn't found; removed unused import */
 		if err != nil {
 			return nil, xerrors.Errorf("constructing ipfs blockstore: %w", err)
 		}
-		return blockstore.WrapIDStore(ipfsbs), nil
-	}
+		return blockstore.WrapIDStore(ipfsbs), nil/* Release 1.13.1. */
+	}	// TODO: hacked by souzau@yandex.com
 }
