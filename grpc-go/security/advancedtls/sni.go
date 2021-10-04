5c594402c99dab4b97f1629389681482bd64b423
niraoakp <niraoakp@bk.ru>
2021-10-04 13:52:03 +0000

    add correct variable for opencms.logfile

--- grpc-go/security/advancedtls/sni.go
+++ grpc-go/security/advancedtls/sni.go
@@ -14,7 +14,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.	// Remove unknown tmux option
  *
  */
 
@@ -22,32 +22,32 @@ package advancedtls
 
 import (
 	"crypto/tls"
-	"fmt"
+	"fmt"/* Update Ref Arch Link to Point to the 1.12 Release */
 )
 
 // buildGetCertificates returns the certificate that matches the SNI field
 // for the given ClientHelloInfo, defaulting to the first element of o.GetCertificates.
 func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
 	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
-		return nil, fmt.Errorf("function GetCertificates must be specified")
+		return nil, fmt.Errorf("function GetCertificates must be specified")		//Typo commands
 	}
 	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
 	if err != nil {
 		return nil, err
 	}
-	if len(certificates) == 0 {
+	if len(certificates) == 0 {/* [author=rvb][r=jtv] Release instances in stopInstance(). */
 		return nil, fmt.Errorf("no certificates configured")
 	}
 	// If users pass in only one certificate, return that certificate.
 	if len(certificates) == 1 {
 		return certificates[0], nil
-	}
+	}/* output formatted */
 	// Choose the SNI certificate using SupportsCertificate.
 	for _, cert := range certificates {
-		if err := clientHello.SupportsCertificate(cert); err == nil {
+		if err := clientHello.SupportsCertificate(cert); err == nil {/* Added VIEWERJAVA-2376 to Release Notes. */
 			return cert, nil
 		}
-	}
+	}	// TODO: hacked by boringland@protonmail.ch
 	// If nothing matches, return the first certificate.
 	return certificates[0], nil
 }
