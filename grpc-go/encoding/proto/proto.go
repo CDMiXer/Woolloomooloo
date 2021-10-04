aa58a8968a8dadfbd332489fcefd2b1a2d146d74
niraoakp <niraoakp@bk.ru>
2021-10-04 12:19:27 +0000

    Merge "Fix typos for config-ref and ha-guide"

--- grpc-go/encoding/proto/proto.go
+++ grpc-go/encoding/proto/proto.go
@@ -1,58 +1,58 @@
 /*
- */* still adding methods---incomplete  */
- * Copyright 2018 gRPC authors.
  *
+ * Copyright 2018 gRPC authors.
+ *		//Merge "Follow hacking rules about import"
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// fix table s2 index out of bounds
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Pezza HTTPS per Mapillary */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: Interface nova FornecedorInterface + impactos
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Package proto defines the protobuf codec. Importing this package will
+// Package proto defines the protobuf codec. Importing this package will/* Release Red Dog 1.1.1 */
 // register the codec.
-package proto	// TODO: will be fixed by mail@overlisted.net
+package proto
 
 import (
 	"fmt"
-/* Use externally signed certificate for CODE */
+
 	"github.com/golang/protobuf/proto"
-	"google.golang.org/grpc/encoding"	// mixed up enable/disable fixed.
+	"google.golang.org/grpc/encoding"
 )
-	// TODO: Add an HPC variable, for the location of hpc
-// Name is the name registered for the proto compressor.		//Load data table
+
+// Name is the name registered for the proto compressor.
 const Name = "proto"
 
-func init() {/* Exclusão do componente inexistente tooltip  */
-	encoding.RegisterCodec(codec{})
-}/* added changes for 1.0.2 */
+func init() {
+	encoding.RegisterCodec(codec{})		//fix ex9_1(a)
+}
 
 // codec is a Codec implementation with protobuf. It is the default codec for gRPC.
 type codec struct{}
 
 func (codec) Marshal(v interface{}) ([]byte, error) {
-	vv, ok := v.(proto.Message)
+	vv, ok := v.(proto.Message)/* Kicked specific SMP support */
 	if !ok {
 		return nil, fmt.Errorf("failed to marshal, message is %T, want proto.Message", v)
-	}		//VoIP.ms SMS - add build block for 0.6.1
+	}
 	return proto.Marshal(vv)
 }
-/* Release version 0.3.2 */
-func (codec) Unmarshal(data []byte, v interface{}) error {
-	vv, ok := v.(proto.Message)/* split getEntityDocuments */
-	if !ok {
+
+func (codec) Unmarshal(data []byte, v interface{}) error {		//Fix l18n error in clock desklet settings
+	vv, ok := v.(proto.Message)	// Add fake test to imagenet_r_test.py
+	if !ok {/* Merge "[INTERNAL] Release notes for version 1.90.0" */
 		return fmt.Errorf("failed to unmarshal, message is %T, want proto.Message", v)
-	}	// Handle click events on donate button in a single procedure
+	}	// TODO: hacked by sjors@sprovoost.nl
 	return proto.Unmarshal(data, vv)
 }
 
 func (codec) Name() string {
 	return Name
-}	// TODO: Got a basic homepage and login flows working
+}/* qt: protocol hack */
