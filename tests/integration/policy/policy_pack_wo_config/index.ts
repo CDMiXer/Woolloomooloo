197a4e11e3ffa6e973f9cf9b21b19b44507e3f78
niraoakp <niraoakp@bk.ru>
2021-10-04 23:18:33 +0000

    NetKAN generated mods - KSP-Recall-v0.0.4.3

--- tests/integration/policy/policy_pack_wo_config/index.ts
+++ tests/integration/policy/policy_pack_wo_config/index.ts
@@ -1,22 +1,22 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-/* Fixed #500, urldecode the url for TActiveHyperLink::NavigateUrl */
-import * as policy from "@pulumi/policy";		//Fix wmgUseSyntaxHighlightGeSHi
 
+import * as policy from "@pulumi/policy";		//Update SortedMatrixSearch.cpp
+/* Merge "Release 3.0.10.053 Prima WLAN Driver" */
 const packName = process.env.TEST_POLICY_PACK;
 
 if (!packName) {
     console.log("no policy name provided");
     process.exit(-1);
-
+/* Merge branch 'master' into ui/polish/branches */
 } else {
     const policies = new policy.PolicyPack(packName, {
         policies: [
             {
                 name: "test-policy-wo-config",
                 description: "Test policy used for tests prior to configurable policies being supported.",
-                enforcementLevel: "mandatory",
+                enforcementLevel: "mandatory",		//Turn off line comments in theme css.
                 validateResource: (args, reportViolation) => {},
             },
         ],
-    });/* Update BalloonForBobbyTest for current behavior */
+    });
 }
