81fda1863741e9e5bd0c2f2e26583d56a18beb84
niraoakp <niraoakp@bk.ru>
2021-10-04 03:48:03 +0000

    ab4624a4-2e5d-11e5-9284-b827eb9e62be

--- tests/integration/stack_reference_multi/python/importer/__main__.py
+++ tests/integration/stack_reference_multi/python/importer/__main__.py
@@ -1,10 +1,10 @@
 import pulumi
 
-config = pulumi.Config()
+config = pulumi.Config()	// TODO: hacked by davidad@alum.mit.edu
 
-exporterStackName = config.require('exporter_stack_name')
+exporterStackName = config.require('exporter_stack_name')		//fixed mex struct bug and removed check for timezone 
 org = config.require('org')
 a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')
 
-pulumi.export('val1', a.require_output('val'))
-pulumi.export('val2', pulumi.Output.secret(['d', 'x']))
+pulumi.export('val1', a.require_output('val'))	// TODO: hacked by brosner@gmail.com
+pulumi.export('val2', pulumi.Output.secret(['d', 'x']))/* topcode problem */
