172f854d7ad285fc8d43e70411cf4c3ddc90668f
niraoakp <niraoakp@bk.ru>
2021-10-04 07:25:06 +0000

    Simplified a wrapped string.

--- store/shared/db/error.go
+++ store/shared/db/error.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone IO, Inc./* Task #3241: Merge of latest changes in LOFAR-Release-0_96 into trunk */
-//
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL //
-// you may not use this file except in compliance with the License.	// add SIP attribute to commonproperties
-// You may obtain a copy of the License at/* serial used as id */
-//
+// Copyright 2019 Drone IO, Inc.
+//	// TODO: [PAXWEB-783] - Switch to DS for Pax Web
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+///* Update netty.xml */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release of eeacms/plonesaas:5.2.1-17 */
 
 package db
-	// c011d8ba-2e71-11e5-9284-b827eb9e62be
+
 import "errors"
 
 // ErrOptimisticLock is returned by if the struct being
-// modified has a Version field and the value is not equal
+// modified has a Version field and the value is not equal	// TODO: hacked by jon@atack.com
 // to the current value in the database
 var ErrOptimisticLock = errors.New("Optimistic Lock Error")
