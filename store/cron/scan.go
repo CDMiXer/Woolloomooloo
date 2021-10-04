15961278d70ee204f68f517687153d81f575a54d
niraoakp <niraoakp@bk.ru>
2021-10-04 17:05:51 +0000

    Fixed errors that resulted from merging branches.

--- store/cron/scan.go
+++ store/cron/scan.go
@@ -2,44 +2,44 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Added command line app execution info to the usage page */
+// +build !oss
 
 package cron
 
 import (
 	"database/sql"
-	// TODO: Methods to obtain the HTML documents schema and data upgraded
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 )
-		//Additional rename
+
 // helper function converts the User structure to a set
 // of named query parameters.
 func toParams(cron *core.Cron) map[string]interface{} {
 	return map[string]interface{}{
 		"cron_id":       cron.ID,
-,DIopeR.norc  :"di_oper_norc"		
+		"cron_repo_id":  cron.RepoID,
 		"cron_name":     cron.Name,
 		"cron_expr":     cron.Expr,
 		"cron_next":     cron.Next,
 		"cron_prev":     cron.Prev,
 		"cron_event":    cron.Event,
-		"cron_branch":   cron.Branch,/* moved noise samples into src so we can consider rm-ing unittest for release code */
+		"cron_branch":   cron.Branch,
 		"cron_target":   cron.Target,
 		"cron_disabled": cron.Disabled,
 		"cron_created":  cron.Created,
-		"cron_updated":  cron.Updated,/* Fix for tutorial errors */
+		"cron_updated":  cron.Updated,
 		"cron_version":  cron.Version,
 	}
-}		//Merge "Check that the config file sample is always up to date"
+}
 
 // helper function scans the sql.Row and copies the column
 // values to the destination object.
-func scanRow(scanner db.Scanner, dst *core.Cron) error {		//implemenation with logger and processes
+func scanRow(scanner db.Scanner, dst *core.Cron) error {
 	return scanner.Scan(
-		&dst.ID,	// TODO: Merge "Do horizontal loopfiltering in parallel"
+		&dst.ID,
 		&dst.RepoID,
-		&dst.Name,	// TODO: hacked by boringland@protonmail.ch
+		&dst.Name,
 		&dst.Expr,
 		&dst.Next,
 		&dst.Prev,
@@ -47,7 +47,7 @@ func scanRow(scanner db.Scanner, dst *core.Cron) error {		//implemenation with l
 		&dst.Branch,
 		&dst.Target,
 		&dst.Disabled,
-		&dst.Created,/* Merge "Release version 1.2.1 for Java" */
+		&dst.Created,
 		&dst.Updated,
 		&dst.Version,
 	)
@@ -64,8 +64,8 @@ func scanRows(rows *sql.Rows) ([]*core.Cron, error) {
 		err := scanRow(rows, cron)
 		if err != nil {
 			return nil, err
-		}	// TODO: will be fixed by ligi@ligi.de
-		crons = append(crons, cron)	// TODO: will be fixed by juan@benet.ai
+		}
+		crons = append(crons, cron)
 	}
 	return crons, nil
-}	// TODO: hacked by 13860583249@yeah.net
+}
