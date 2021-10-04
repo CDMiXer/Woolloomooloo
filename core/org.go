44b3b09ac0655f11b22df015df7ee0bf3b8a375b
niraoakp <niraoakp@bk.ru>
2021-10-04 01:46:22 +0000

    515a8db8-2e5d-11e5-9284-b827eb9e62be

--- core/org.go
+++ core/org.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
-//		//cmd: net: netstat: Fix #710 and add some flags
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by sbrichards@gmail.com
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* 04bf9944-2e49-11e5-9284-b827eb9e62be */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,22 +15,22 @@
 package core
 
 import "context"
-	// Fix if in wrong position
+
 // Organization represents an organization in the source
-// code management system (e.g. GitHub)./* Update doc/ag.1 */
+// code management system (e.g. GitHub).
 type Organization struct {
-	Name   string		//mriqc v0.3.1
+	Name   string
 	Avatar string
-}	// TODO: will be fixed by juan@benet.ai
+}
 
 // OrganizationService provides access to organization and
 // team access in the external source code management system
-// (e.g. GitHub)./* merged with develop branch */
+// (e.g. GitHub).
 type OrganizationService interface {
 	// List returns a list of organization to which the
 	// user is a member.
 	List(context.Context, *User) ([]*Organization, error)
-/* Create valid-word-abbreviation.cpp */
+
 	// Membership returns true if the user is a member
 	// of the organization, and true if the user is an
 	// of the organization.
