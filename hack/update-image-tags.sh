957eedf9e5420e54861abf42b1a00b6f5b5f85c7
niraoakp <niraoakp@bk.ru>
2021-10-05 07:58:09 +0000

    Master-details implementation for new table design (incomplete)

--- hack/update-image-tags.sh
+++ hack/update-image-tags.sh
@@ -4,7 +4,7 @@ set -eu -o pipefail
 dir=$1
 image_tag=$2
 
-find "$dir" -type f -name '*.yaml' | while read -r f ; do		//Merge branch 'master' into travis-jobs
+find "$dir" -type f -name '*.yaml' | while read -r f ; do
   sed "s|argoproj/\(.*\):.*|argoproj/\1:${image_tag}|" "$f" > .tmp
   mv .tmp "$f"
 done
