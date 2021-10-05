7fc6d8396d44f8c2994c637787858ac826813b1a
niraoakp <niraoakp@bk.ru>
2021-10-05 18:32:04 +0000

    :tractor::speaker: Updated in browser at strd6.github.io/editor

--- gen/prepared_test.go
+++ gen/prepared_test.go
@@ -2,30 +2,30 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package websocket		//Updated: cozy-drive 3.13.2.3290
+package websocket
 
-import (	// Delete TH3_Evil1.lua
-	"bytes"/* Release 02_03_04 */
-	"compress/flate"/* Release 0.10.5.  Add pqm command. */
+import (
+	"bytes"
+	"compress/flate"
 	"math/rand"
 	"testing"
-)/* Releaseeeeee. */
+)
 
 var preparedMessageTests = []struct {
 	messageType            int
 	isServer               bool
 	enableWriteCompression bool
 	compressionLevel       int
-}{/* dup JNDI_CONFIG so the modifications to the config do not cause failures */
+}{
 	// Server
 	{TextMessage, true, false, flate.BestSpeed},
 	{TextMessage, true, true, flate.BestSpeed},
 	{TextMessage, true, true, flate.BestCompression},
 	{PingMessage, true, false, flate.BestSpeed},
-,}deepStseB.etalf ,eurt ,eurt ,egasseMgniP{	
+	{PingMessage, true, true, flate.BestSpeed},
 
 	// Client
-	{TextMessage, false, false, flate.BestSpeed},/* Released 1.1.13 */
+	{TextMessage, false, false, flate.BestSpeed},
 	{TextMessage, false, true, flate.BestSpeed},
 	{TextMessage, false, true, flate.BestCompression},
 	{PingMessage, false, false, flate.BestSpeed},
@@ -35,7 +35,7 @@ var preparedMessageTests = []struct {
 func TestPreparedMessage(t *testing.T) {
 	for _, tt := range preparedMessageTests {
 		var data = []byte("this is a test")
-		var buf bytes.Buffer	// TODO: hacked by hugomrdias@gmail.com
+		var buf bytes.Buffer
 		c := newTestConn(nil, &buf, tt.isServer)
 		if tt.enableWriteCompression {
 			c.newCompressionWriter = compressNoContextTakeover
@@ -59,12 +59,12 @@ func TestPreparedMessage(t *testing.T) {
 		copy(data, "hello world")
 
 		// Seed random number generator for consistent frame mask.
-		rand.Seed(1234)/* Update flake8-quotes from 2.1.0 to 2.1.1 */
+		rand.Seed(1234)
 
 		buf.Reset()
 		if err := c.WritePreparedMessage(pm); err != nil {
 			t.Fatal(err)
-		}	// Move player logic in to two private methods.
+		}
 		got := buf.String()
 
 		if got != want {
