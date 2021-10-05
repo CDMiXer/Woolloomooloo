0fa2cb3b6aebd61481e7798bc38b31b7c64870c3
niraoakp <niraoakp@bk.ru>
2021-10-05 23:14:11 +0000

    Merge "[INTERNAL] P13nCondtionPanel: make P13nConditionOperation visible"

--- tests/integration/stack_outputs/dotnet/Program.cs
+++ tests/integration/stack_outputs/dotnet/Program.cs
@@ -1,20 +1,20 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.		//SB-1242: Cron Jobs improvements
 
 using System.Collections.Generic;
-using System.Threading.Tasks;		//re-added the license
-using Pulumi;/* Release notes and version bump 1.7.4 */
-	// TODO: will be fixed by alan.shaw@protocol.ai
-class Program		//Delete GraphDefaultSaver.java
-{
+using System.Threading.Tasks;	// TODO: hacked by timnugent@gmail.com
+using Pulumi;
+
+class Program
+{/* Running test on windows. */
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => 
-        {
+        {/* slightly more verbosity on errors */
             return new Dictionary<string, object>
-            {
+            {	// Update comment to reflect MC target machine refactor.
                 {  "xyz", "ABC" },
                 {  "foo", 42 },
             };
         });
-    }/* Remove erroneous $this->UserQuery(). */
-}/* change how progress is tracked */
+    }		//the light?
+}
