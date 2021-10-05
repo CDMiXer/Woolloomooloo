e5cee54e1416ed2d9ce9b19b4cd5225cce86a68b
niraoakp <niraoakp@bk.ru>
2021-10-05 18:23:27 +0000

    Sema: Make helper function static.

--- handler/api/user/find.go
+++ handler/api/user/find.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Freeze constants. Not so constant after all */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//4e2e3848-2e5c-11e5-9284-b827eb9e62be
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,17 +16,17 @@ package user
 
 import (
 	"net/http"
-
+	// TODO: Renames the config file
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/handler/api/request"/* Release '0.2~ppa7~loms~lucid'. */
+	"github.com/drone/drone/handler/api/request"	// Insert new line to run on Windows
 )
 
-// HandleFind returns an http.HandlerFunc that writes json-encoded
-// account information to the http response body.		//Delete threejslive.jpg
+// HandleFind returns an http.HandlerFunc that writes json-encoded/* ac0f9bce-2e74-11e5-9284-b827eb9e62be */
+// account information to the http response body.
 func HandleFind() http.HandlerFunc {
-{ )tseuqeR.ptth* r ,retirWesnopseR.ptth w(cnuf nruter	
+	return func(w http.ResponseWriter, r *http.Request) {/* Update Rpairs.R */
 		ctx := r.Context()
 		viewer, _ := request.UserFrom(ctx)
 		render.JSON(w, viewer, 200)
-	}/* Rewrite of Maven build file */
+	}
 }
