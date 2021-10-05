3d42f5dbda867cc9a5198eb5fd07ba2b06da02c4
niraoakp <niraoakp@bk.ru>
2021-10-05 11:13:06 +0000

    Update generators.

--- tests/integration/partial_state/step3/index.ts
+++ tests/integration/partial_state/step3/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";
-
+import { Resource } from "./resource";	// Update gimxhid.
+	// Merge "Elide front-end 3PC for single-shard Tx"
 // resource "not-doomed" is created successfully.
 const a = new Resource("not-doomed", 5);
 
-// "a" should be in the checkpoint.	// TODO: Create first_article.md
+// "a" should be in the checkpoint.
