46a9b8d991593bd05013c9378153d8f9683be4ed
niraoakp <niraoakp@bk.ru>
2021-10-05 12:04:31 +0000

    Finished EditScore; Added JavaDoc to EditScore

--- tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
@@ -2,13 +2,13 @@
 
 using System.Threading.Tasks;
 using Pulumi;
-
+	// remove DirectInstrumenter. Consider stats in Behaviour
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
-    }
+    }/* clean the cpu governors 2 */
 }
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
@@ -20,7 +20,7 @@ class ComponentFive : ComponentResource
         : base("my:module:ComponentFive", name, options)
     {
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
-    }
+    }/* Release of eeacms/www:21.1.30 */
 }
 
 class Program
@@ -28,8 +28,8 @@ class Program
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => 
-        {
-            var comp5 = new ComponentFive("comp5");
-        });
-    }
+        {/* Merge "Cleaning up add_filters" */
+            var comp5 = new ComponentFive("comp5");	// TODO: fixed root error message
+        });/* Fixed bug: Server not starting and migrations not running */
+    }/* Delete GUIWF.java.svn-base */
 }
