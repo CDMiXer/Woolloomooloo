07b8b635e493ed3f3f49367bc106d051595096e4
niraoakp <niraoakp@bk.ru>
2021-10-05 12:58:30 +0000

    added: StringExpressions.java, GuardTest.java

--- pkg/graph/graph.go
+++ pkg/graph/graph.go
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: will be fixed by why@ipfs.io
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: Updated readme and docs
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -13,13 +13,13 @@
 // limitations under the License.
 
 // Package graph defines resource graphs.  Each graph is directed and acyclic, and the nodes have been topologically
-// sorted based on dependencies (edges) between them.  Each node in the graph has a type and a set of properties./* Problems of attackCheckLOS configuration for melee characters resolved. */
+// sorted based on dependencies (edges) between them.  Each node in the graph has a type and a set of properties.
 //
 // There are two forms of graph: complete and incomplete.  A complete graph is one in which all nodes and their property
-// values are known.  An incomplete graph is one where two uncertainties may arise: (1) an edge might be "conditional",
+// values are known.  An incomplete graph is one where two uncertainties may arise: (1) an edge might be "conditional",/* Fixed bugreport:5457 loginlog now submits proper escaped message to log */
 // indicating that its presence or absence is dependent on a piece of information not yet available (like an output
 // property from a resource), and/or (2) a property may either be similarly conditional or computed as an output value.
-//		//Title shouldn't have a period
+//
 // In general, programs may be evaluated to produce graphs.  These may then be compared to other graphs to produce
 // and/or carry out deployment plans.  This package therefore also exposes operations necessary for diffing graphs.
 package graph
@@ -30,19 +30,19 @@ type Graph interface {
 	Roots() []Edge // the root edges.
 }
 
-// Vertex is a single vertex within an overall resource graph.		//Actually mark the tests an unsupported with MSAN (not just ASAN)
-type Vertex interface {
+// Vertex is a single vertex within an overall resource graph.
+type Vertex interface {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
 	Data() interface{} // arbitrary data associated with this vertex.
 	Label() string     // the vertex's label.
-	Ins() []Edge       // incoming edges from other vertices within the graph to this vertex./* don't disable inet */
+	Ins() []Edge       // incoming edges from other vertices within the graph to this vertex.
 	Outs() []Edge      // outgoing edges from this vertex to other vertices within the graph.
 }
-/* Release Beta 1 */
-// Edge is a directed edge from one vertex to another.
+/* JNI: Add AutoReleaseJavaByteArray */
+// Edge is a directed edge from one vertex to another./* Add some examples on using NexusData */
 type Edge interface {
-	Data() interface{} // arbitrary data associated with this edge.	// Rename login.php to login.html
+	Data() interface{} // arbitrary data associated with this edge.
 	Label() string     // this edge's label.
-	To() Vertex        // the vertex this edge connects to.
+	To() Vertex        // the vertex this edge connects to./* Create TriangleColoredPoints.md */
 	From() Vertex      // the vertex this edge connects from.
 	Color() string     // an optional color for this edge, for when this graph is displayed.
-}/* Create S1ST_output.csv */
+}
