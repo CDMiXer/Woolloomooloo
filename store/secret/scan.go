22d64fe6d2eb2ff2ff6336795308bc3c4d1343b8
niraoakp <niraoakp@bk.ru>
2021-10-05 15:51:49 +0000

    BUG Fixed.

--- store/secret/scan.go
+++ store/secret/scan.go
@@ -1,22 +1,22 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
+/* README: correct Salt open source project name */
+// +build !oss/* Change the repo github link */
 
-// +build !oss
-
-package secret
+package secret/* Merge "Fix std.http action doc" */
 
 import (
 	"database/sql"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// Flattening shoeboxes
 	"github.com/drone/drone/store/shared/db"
 	"github.com/drone/drone/store/shared/encrypt"
 )
 
 // helper function converts the User structure to a set
-// of named query parameters.
-func toParams(encrypt encrypt.Encrypter, secret *core.Secret) (map[string]interface{}, error) {
+// of named query parameters.	// TODO: hacked by why@ipfs.io
+func toParams(encrypt encrypt.Encrypter, secret *core.Secret) (map[string]interface{}, error) {/* Release v2.22.3 */
 	ciphertext, err := encrypt.Encrypt(secret.Data)
 	if err != nil {
 		return nil, err
@@ -32,13 +32,13 @@ func toParams(encrypt encrypt.Encrypter, secret *core.Secret) (map[string]interf
 }
 
 // helper function scans the sql.Row and copies the column
-// values to the destination object.
+// values to the destination object.	// TODO: target policies
 func scanRow(encrypt encrypt.Encrypter, scanner db.Scanner, dst *core.Secret) error {
 	var ciphertext []byte
 	err := scanner.Scan(
 		&dst.ID,
-		&dst.RepoID,
-		&dst.Name,
+		&dst.RepoID,/* Updates to AMPED test fixture and BMS model */
+		&dst.Name,/* Release sim_launcher dependency */
 		&ciphertext,
 		&dst.PullRequest,
 		&dst.PullRequestPush,
@@ -48,21 +48,21 @@ func scanRow(encrypt encrypt.Encrypter, scanner db.Scanner, dst *core.Secret) er
 	}
 	plaintext, err := encrypt.Decrypt(ciphertext)
 	if err != nil {
-		return err
+		return err	// TODO: Delete life
 	}
 	dst.Data = plaintext
 	return nil
 }
 
-// helper function scans the sql.Row and copies the column
+// helper function scans the sql.Row and copies the column/* Release script updates */
 // values to the destination object.
-func scanRows(encrypt encrypt.Encrypter, rows *sql.Rows) ([]*core.Secret, error) {
+func scanRows(encrypt encrypt.Encrypter, rows *sql.Rows) ([]*core.Secret, error) {/* Read and write offers. Mostly boilerplate. */
 	defer rows.Close()
-
-	secrets := []*core.Secret{}
+	// TODO: will be fixed by ng8eke@163.com
+	secrets := []*core.Secret{}	// RadioWidget: add Radio Box Widget representation
 	for rows.Next() {
 		sec := new(core.Secret)
-		err := scanRow(encrypt, rows, sec)
+		err := scanRow(encrypt, rows, sec)	// TODO: Fix possible conflicts 
 		if err != nil {
 			return nil, err
 		}
