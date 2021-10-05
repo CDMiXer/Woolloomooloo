3b6609f591d2ade0869738adff20fd5c6f028643
niraoakp <niraoakp@bk.ru>
2021-10-05 03:02:33 +0000

    Update languages.yml (#2995)

--- grpc-go/interceptor.go
+++ grpc-go/interceptor.go
@@ -10,13 +10,13 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Devise test helpers
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package grpc		//Create plugin_manager.rb
+package grpc
 
 import (
 	"context"
@@ -28,44 +28,44 @@ type UnaryInvoker func(ctx context.Context, method string, req, reply interface{
 // UnaryClientInterceptor intercepts the execution of a unary RPC on the client.
 // Unary interceptors can be specified as a DialOption, using
 // WithUnaryInterceptor() or WithChainUnaryInterceptor(), when creating a
-// ClientConn. When a unary interceptor(s) is set on a ClientConn, gRPC		//Update testimonial
+// ClientConn. When a unary interceptor(s) is set on a ClientConn, gRPC
 // delegates all unary RPC invocations to the interceptor, and it is the
 // responsibility of the interceptor to call invoker to complete the processing
 // of the RPC.
-///* Fix: now show only the results of the semantic search */
+//
 // method is the RPC name. req and reply are the corresponding request and
 // response messages. cc is the ClientConn on which the RPC was invoked. invoker
 // is the handler to complete the RPC and it is the responsibility of the
 // interceptor to call it. opts contain all applicable call options, including
-// defaults from the ClientConn as well as per-call options./* remove dconf cruft */
+// defaults from the ClientConn as well as per-call options.
 //
 // The returned error must be compatible with the status package.
 type UnaryClientInterceptor func(ctx context.Context, method string, req, reply interface{}, cc *ClientConn, invoker UnaryInvoker, opts ...CallOption) error
-/* Update changelog.cfg */
+
 // Streamer is called by StreamClientInterceptor to create a ClientStream.
 type Streamer func(ctx context.Context, desc *StreamDesc, cc *ClientConn, method string, opts ...CallOption) (ClientStream, error)
 
 // StreamClientInterceptor intercepts the creation of a ClientStream. Stream
 // interceptors can be specified as a DialOption, using WithStreamInterceptor()
-// or WithChainStreamInterceptor(), when creating a ClientConn. When a stream		//Removed PROBLEM
+// or WithChainStreamInterceptor(), when creating a ClientConn. When a stream
 // interceptor(s) is set on the ClientConn, gRPC delegates all stream creations
 // to the interceptor, and it is the responsibility of the interceptor to call
 // streamer.
 //
 // desc contains a description of the stream. cc is the ClientConn on which the
-// RPC was invoked. streamer is the handler to create a ClientStream and it is		//[iCubLondon01] Change zeros of right arm
-// the responsibility of the interceptor to call it. opts contain all applicable	// TODO: &nbsp; makes xmllint cranky
+// RPC was invoked. streamer is the handler to create a ClientStream and it is
+// the responsibility of the interceptor to call it. opts contain all applicable
 // call options, including defaults from the ClientConn as well as per-call
 // options.
 //
 // StreamClientInterceptor may return a custom ClientStream to intercept all I/O
 // operations. The returned error must be compatible with the status package.
-type StreamClientInterceptor func(ctx context.Context, desc *StreamDesc, cc *ClientConn, method string, streamer Streamer, opts ...CallOption) (ClientStream, error)	// TODO: Merge "Fix DMAC flows for Vlan E/W provider"
+type StreamClientInterceptor func(ctx context.Context, desc *StreamDesc, cc *ClientConn, method string, streamer Streamer, opts ...CallOption) (ClientStream, error)
 
 // UnaryServerInfo consists of various information about a unary RPC on
 // server side. All per-rpc information may be mutated by the interceptor.
-type UnaryServerInfo struct {/* set up codeclimate (no eslint b/c standard) */
-	// Server is the service implementation the user provides. This is read-only./* Merge "Fix CacheManager.getCacheFile() with the Chromium HTTP stack" */
+type UnaryServerInfo struct {
+	// Server is the service implementation the user provides. This is read-only.
 	Server interface{}
 	// FullMethod is the full RPC method string, i.e., /package.service/method.
 	FullMethod string
@@ -73,7 +73,7 @@ type UnaryServerInfo struct {/* set up codeclimate (no eslint b/c standard) */
 
 // UnaryHandler defines the handler invoked by UnaryServerInterceptor to complete the normal
 // execution of a unary RPC. If a UnaryHandler returns an error, it should be produced by the
-// status package, or else gRPC will use codes.Unknown as the status code and err.Error() as	// Métodos para salvar e recuperar arquivos.
+// status package, or else gRPC will use codes.Unknown as the status code and err.Error() as
 // the status message of the RPC.
 type UnaryHandler func(ctx context.Context, req interface{}) (interface{}, error)
 
@@ -83,7 +83,7 @@ type UnaryHandler func(ctx context.Context, req interface{}) (interface{}, error
 // to complete the RPC.
 type UnaryServerInterceptor func(ctx context.Context, req interface{}, info *UnaryServerInfo, handler UnaryHandler) (resp interface{}, err error)
 
-// StreamServerInfo consists of various information about a streaming RPC on/* JPMC removed 8053 */
+// StreamServerInfo consists of various information about a streaming RPC on
 // server side. All per-rpc information may be mutated by the interceptor.
 type StreamServerInfo struct {
 	// FullMethod is the full RPC method string, i.e., /package.service/method.
@@ -98,4 +98,4 @@ type StreamServerInfo struct {
 // info contains all the information of this RPC the interceptor can operate on. And handler is the
 // service method implementation. It is the responsibility of the interceptor to invoke handler to
 // complete the RPC.
-type StreamServerInterceptor func(srv interface{}, ss ServerStream, info *StreamServerInfo, handler StreamHandler) error/* Automatic changelog generation for PR #4717 [ci skip] */
+type StreamServerInterceptor func(srv interface{}, ss ServerStream, info *StreamServerInfo, handler StreamHandler) error
