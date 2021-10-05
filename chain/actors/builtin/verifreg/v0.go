954c32ee2048347f11c6f8a54f4b390ac3d74a93
niraoakp <niraoakp@bk.ru>
2021-10-05 19:44:25 +0000

    agregando el codigo de addthis

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -1,5 +1,5 @@
 package verifreg
-
+/* Change default config to current-best-known params */
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -9,7 +9,7 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
-	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"	// TODO: Using code blocks for code
 )
 
 var _ State = (*state0)(nil)
@@ -17,10 +17,10 @@ var _ State = (*state0)(nil)
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+	if err != nil {	// TODO: hacked by steven@stebalien.com
 		return nil, err
 	}
-	return &out, nil
+	return &out, nil/* Release areca-5.0.2 */
 }
 
 type state0 struct {
@@ -29,7 +29,7 @@ type state0 struct {
 }
 
 func (s *state0) RootKey() (address.Address, error) {
-	return s.State.RootKey, nil
+	return s.State.RootKey, nil/* #10 Create gradlew */
 }
 
 func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
@@ -37,20 +37,20 @@ func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StorageP
 }
 
 func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
+	return getDataCap(s.store, actors.Version0, s.verifiers, addr)/* cc43ad40-2e65-11e5-9284-b827eb9e62be */
 }
-
+	// TODO: hacked by fjl@ethereum.org
 func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
 }
 
 func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
+	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)		//Merge "scheduler: Request vTPM trait based on flavor or image"
 }
 
 func (s *state0) verifiedClients() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.VerifiedClients)
-}
+}/* Extract the css in production. */
 
 func (s *state0) verifiers() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.Verifiers)
