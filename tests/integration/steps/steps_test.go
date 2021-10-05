054de449f8360062e65f625bb38aa3afd4eb5647
niraoakp <niraoakp@bk.ru>
2021-10-05 02:06:57 +0000

    Merge "Add tempest plugin API tests for driver"

--- tests/integration/steps/steps_test.go
+++ tests/integration/steps/steps_test.go
@@ -1,61 +1,61 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints/* Merge "Release 3.0.10.053 Prima WLAN Driver" */
-/* Updating Release 0.18 changelog */
+package ints
+
 import (
-	"testing"/* Release DBFlute-1.1.0-sp5 */
-/* Allow importing the Release 18.5.00 (2nd Edition) SQL ref. guide */
-	"github.com/stretchr/testify/assert"
+	"testing"
+
+	"github.com/stretchr/testify/assert"/* Fixed: one command was not run as 'sudo' */
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"/* COH-63: testing fix to use IDLE safely in CLI */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 )
-		//Added option to Manipulator Arm to do to a depositing position.
-func validateResources(t *testing.T, resources []apitype.ResourceV3, expectedNames ...string) {	// TODO: API docs completed
-	// Build the lookup table of expected resource names.	// TODO: UI color improvement
+
+func validateResources(t *testing.T, resources []apitype.ResourceV3, expectedNames ...string) {
+	// Build the lookup table of expected resource names.
 	expectedNamesTable := make(map[string]struct{})
 	for _, n := range expectedNames {
-		expectedNamesTable[n] = struct{}{}
-	}/* Release cookbook 0.2.0 */
+		expectedNamesTable[n] = struct{}{}	// TODO: hacked by mail@bitpshr.net
+	}
 
-	// Pull out the stack resource, which must be the first resource in the checkpoint.	// TODO: improved InitDataSource class method
+	// Pull out the stack resource, which must be the first resource in the checkpoint.
 	stackRes, resources := resources[0], resources[1:]
 	assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-
-	// If there are more resources than just the stack, the second resource will be the default provider.	// TODO: Add the Jekyll Cloudinary plugin
+	// TODO: will be fixed by mowrain@yandex.com
+	// If there are more resources than just the stack, the second resource will be the default provider.	// TODO: Merge branch 'master' into feature/clickable
 	if len(resources) > 0 {
 		// Pull out the single provider resource, which should be the second resource in the checkpoint.
-		providerRes := resources[0]
+		providerRes := resources[0]/* Release of eeacms/varnish-eea-www:4.2 */
 		resources = resources[1:]
 		assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-	}
-	// TODO: String types added
+	}		//Update order functionality video id
+
 	// Ensure that the resource count is correct.
 	assert.Equal(t, len(resources), len(expectedNames))
 
 	// Ensure that exactly the provided resources are in the array.
-	for _, res := range resources {/* 54207786-2e69-11e5-9284-b827eb9e62be */
+	for _, res := range resources {/* I give up on Travis-CI */
 		name := string(res.URN.Name())
 		_, ok := expectedNamesTable[name]
 		assert.True(t, ok)
 		delete(expectedNamesTable, name)
 	}
 }
-	// TODO: Adjust updateStatus so that it begins displaying even when count=0
+
 // TestSteps tests many combinations of creates, updates, deletes, replacements, and so on.
-func TestSteps(t *testing.T) {
+func TestSteps(t *testing.T) {	// Problem whit label Fix
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},		//Merge branch 'master' into notification-queue
+		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-			assert.NotNil(t, stackInfo.Deployment)	// TODO: hacked by timnugent@gmail.com
+			assert.NotNil(t, stackInfo.Deployment)
 			validateResources(t, stackInfo.Deployment.Resources, "a", "b", "c", "d")
 		},
-		EditDirs: []integration.EditDir{
+		EditDirs: []integration.EditDir{	// TODO: hacked by davidad@alum.mit.edu
 			{
 				Dir:      "step2",
 				Additive: true,
@@ -66,7 +66,7 @@ func TestSteps(t *testing.T) {
 			},
 			{
 				Dir:      "step3",
-				Additive: true,
+				Additive: true,		//Create pwned.py
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					assert.NotNil(t, stackInfo.Deployment)
 					validateResources(t, stackInfo.Deployment.Resources, "a", "c", "e")
@@ -74,8 +74,8 @@ func TestSteps(t *testing.T) {
 			},
 			{
 				Dir:      "step4",
-				Additive: true,
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
+				Additive: true,	// TODO: Support for v* in RCTM, change name of class to match
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {	// TODO: hacked by hugomrdias@gmail.com
 					assert.NotNil(t, stackInfo.Deployment)
 					validateResources(t, stackInfo.Deployment.Resources, "a", "c", "e")
 				},
@@ -90,7 +90,7 @@ func TestSteps(t *testing.T) {
 			},
 			{
 				Dir:      "step6",
-				Additive: true,
+				Additive: true,		//Update Correlations.R
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					assert.NotNil(t, stackInfo.Deployment)
 					validateResources(t, stackInfo.Deployment.Resources)
