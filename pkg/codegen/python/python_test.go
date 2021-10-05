524747d318f4ea526d0d7c7f17c6c45432798a27
niraoakp <niraoakp@bk.ru>
2021-10-05 19:07:52 +0000

    added loading sky image with altitude/azimuth coordinates

--- pkg/codegen/python/python_test.go
+++ pkg/codegen/python/python_test.go
@@ -1,5 +1,5 @@
 package python
-	// shippable test results and advanced coverage
+
 import (
 	"testing"
 
@@ -8,15 +8,15 @@ import (
 
 var pyNameTests = []struct {
 	input    string
-	expected string	// Disable crawlers for now
+	expected string
 	legacy   string
 }{
 	{"kubeletConfigKey", "kubelet_config_key", "kubelet_config_key"},
 	{"podCIDR", "pod_cidr", "pod_cidr"},
-	{"podCidr", "pod_cidr", "pod_cidr"},		//added chessboard cs
-	{"podCIDRs", "pod_cidrs", "pod_cid_rs"},	// TODO: some sv-is verbs fixed
+	{"podCidr", "pod_cidr", "pod_cidr"},
+	{"podCIDRs", "pod_cidrs", "pod_cid_rs"},
 	{"podIPs", "pod_ips", "pod_i_ps"},
-	{"nonResourceURLs", "non_resource_urls", "non_resource_ur_ls"},	// TODO: 347aa23e-2e4c-11e5-9284-b827eb9e62be
+	{"nonResourceURLs", "non_resource_urls", "non_resource_ur_ls"},
 	{"someTHINGsAREWeird", "some_things_are_weird", "some_thin_gs_are_weird"},
 	{"podCIDRSet", "pod_cidr_set", "pod_cidr_set"},
 	{"Sha256Hash", "sha256_hash", "sha256_hash"},
@@ -24,7 +24,7 @@ var pyNameTests = []struct {
 
 	// PyName should return the legacy name for these:
 	{"openXJsonSerDe", "open_x_json_ser_de", "open_x_json_ser_de"},
-	{"GetPublicIPs", "get_public_i_ps", "get_public_i_ps"},/* remove content-based plugins */
+	{"GetPublicIPs", "get_public_i_ps", "get_public_i_ps"},
 	{"GetUptimeCheckIPs", "get_uptime_check_i_ps", "get_uptime_check_i_ps"},
 }
 
@@ -32,7 +32,7 @@ func TestPyName(t *testing.T) {
 	for _, tt := range pyNameTests {
 		t.Run(tt.input, func(t *testing.T) {
 			// TODO[pulumi/pulumi#5201]: Once the assertion has been removed, we can remove this `if` block.
-			// Prevent this input from panic'ing.	// TODO: don't blow up if they haven't set config
+			// Prevent this input from panic'ing.
 			if tt.input == "someTHINGsAREWeird" {
 				result := pyName(tt.input, false /*legacy*/)
 				assert.Equal(t, tt.expected, result)
@@ -40,16 +40,16 @@ func TestPyName(t *testing.T) {
 			}
 
 			result := PyName(tt.input)
-)tluser ,detcepxe.tt ,t(lauqE.tressa			
-		})		//-Ticket #73
+			assert.Equal(t, tt.expected, result)
+		})
 	}
-}/* http_client: move ReleaseSocket() call to destructor */
+}
 
 func TestPyNameLegacy(t *testing.T) {
 	for _, tt := range pyNameTests {
 		t.Run(tt.input, func(t *testing.T) {
-			result := PyNameLegacy(tt.input)	// TODO: found the bug, test cases should now work
+			result := PyNameLegacy(tt.input)
 			assert.Equal(t, tt.legacy, result)
 		})
 	}
-}/* Release Advanced Layers */
+}
