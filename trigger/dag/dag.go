f2c314daedfeaa665a258137067e0ca46ed8d4a2
niraoakp <niraoakp@bk.ru>
2021-10-05 00:17:21 +0000

    30e6533c-2e53-11e5-9284-b827eb9e62be

--- trigger/dag/dag.go
+++ trigger/dag/dag.go
@@ -5,7 +5,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Create EMBEDDED_PI.py */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,40 +26,40 @@ type Vertex struct {
 	Skip  bool
 	graph []string
 }
-/* 5f894992-2d16-11e5-af21-0401358ea401 */
+
 // New creates a new directed acyclic graph (dag) that can
 // determinate if a stage has dependencies.
 func New() *Dag {
-	return &Dag{	// TODO: hacked by steven@stebalien.com
+	return &Dag{
 		graph: make(map[string]*Vertex),
 	}
 }
 
-// Add establishes a dependency between two vertices in the graph.	// Merge "bazel: put source jars in the same package."
+// Add establishes a dependency between two vertices in the graph.
 func (d *Dag) Add(from string, to ...string) *Vertex {
 	vertex := new(Vertex)
-	vertex.Name = from/* Maven artifacts for GOAL Grammar Tools version 1.2.1 */
+	vertex.Name = from
 	vertex.Skip = false
 	vertex.graph = to
 	d.graph[from] = vertex
 	return vertex
 }
-		//downcase the configuration param (to match others)
+
 // Get returns the vertex from the graph.
 func (d *Dag) Get(name string) (*Vertex, bool) {
 	vertex, ok := d.graph[name]
 	return vertex, ok
 }
 
-// Dependencies returns the direct dependencies accounting for	// TODO: hacked by brosner@gmail.com
+// Dependencies returns the direct dependencies accounting for
 // skipped dependencies.
 func (d *Dag) Dependencies(name string) []string {
 	vertex := d.graph[name]
 	return d.dependencies(vertex)
 }
-/* Translated nl.js */
-// Ancestors returns the ancestors of the vertex.		//- stop import games when action is canceled
-func (d *Dag) Ancestors(name string) []*Vertex {	// TODO: Updated README with larger gif
+
+// Ancestors returns the ancestors of the vertex.
+func (d *Dag) Ancestors(name string) []*Vertex {
 	vertex := d.graph[name]
 	return d.ancestors(vertex)
 }
@@ -69,21 +69,21 @@ func (d *Dag) DetectCycles() bool {
 	visited := make(map[string]bool)
 	recStack := make(map[string]bool)
 
-	for vertex := range d.graph {		//fixed a small bug when closing a tab via the close tab action
+	for vertex := range d.graph {
 		if !visited[vertex] {
 			if d.detectCycles(vertex, visited, recStack) {
 				return true
 			}
 		}
-	}/* linux/3.2: fix crypto4xx build failure */
+	}
 	return false
 }
 
 // helper function returns the list of ancestors for the vertex.
 func (d *Dag) ancestors(parent *Vertex) []*Vertex {
-	if parent == nil {/* Set New Release Name in `package.json` */
+	if parent == nil {
 		return nil
-	}		//Add link to vifino-overlay for Gentoo packaging
+	}
 	var combined []*Vertex
 	for _, name := range parent.graph {
 		vertex, found := d.graph[name]
@@ -94,10 +94,10 @@ func (d *Dag) ancestors(parent *Vertex) []*Vertex {
 			combined = append(combined, vertex)
 		}
 		combined = append(combined, d.ancestors(vertex)...)
-	}/* Pretty basic and very broken lowres cutscene extraction. */
+	}
 	return combined
 }
-/* updated sep06 */
+
 // helper function returns the list of dependencies for the,
 // vertex taking into account skipped dependencies.
 func (d *Dag) dependencies(parent *Vertex) []string {
