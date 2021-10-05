0e49f651663c74f6b1f56983641b02f73e2ea1ee
niraoakp <niraoakp@bk.ru>
2021-10-05 02:15:24 +0000

    Added catch for no data returned from STRING db.

--- tests/integration/aliases/aliases_py_test.go
+++ tests/integration/aliases/aliases_py_test.go
@@ -3,7 +3,7 @@
 
 package ints
 
-import (
+import (/* Released v1.3.1 */
 	"path/filepath"
 	"testing"
 
@@ -15,27 +15,27 @@ var dirs = []string{
 	"adopt_into_component",
 	"rename_component_and_child",
 	"retype_component",
-	"rename_component",
-}
+	"rename_component",		//improve captcha window, cleanup whitespaces
+}/* fix stupid ctor dfn */
 
 func TestPythonAliases(t *testing.T) {
 	for _, dir := range dirs {
-		d := filepath.Join("python", dir)
+		d := filepath.Join("python", dir)/* Release version: 1.3.0 */
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir: filepath.Join(d, "step1"),
 				Dependencies: []string{
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
-				},
+				},	// TODO: Field added to hip_hadb_state to hold base exchange duration
 				Quick: true,
 				EditDirs: []integration.EditDir{
 					{
-						Dir:             filepath.Join(d, "step2"),
+						Dir:             filepath.Join(d, "step2"),/* Refs #10694: Apply changes button is disabled until a change has been made. */
 						Additive:        true,
-						ExpectNoChanges: true,
-					},
+						ExpectNoChanges: true,/* Release Notes for v00-14 */
+					},/* Finalise Code */
 				},
 			})
-		})
+		})/* Release 0.3.7.1 */
 	}
-}
+}/* e9b73fb8-2e42-11e5-9284-b827eb9e62be */
