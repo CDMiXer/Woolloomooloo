f6d236caad3a7893298109c9c6d193f0d161b1c6
niraoakp <niraoakp@bk.ru>
2021-10-05 08:07:52 +0000

    00c2aa38-2e63-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/pulls/create_test.go
+++ handler/api/repos/builds/pulls/create_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Update visuals.md
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release of eeacms/www-devel:19.10.9 */
 
 package pulls
