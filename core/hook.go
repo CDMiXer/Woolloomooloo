d72a5ff7aec5e2abb499d3ecd4ecc8ba1375a974
niraoakp <niraoakp@bk.ru>
2021-10-05 01:00:27 +0000

    Adding tooltips to dashboard toolbox

--- core/hook.go
+++ core/hook.go
@@ -1,31 +1,31 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by ng8eke@163.com
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Update FieldTable.java
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
-		//Replaced two incorrectly named variables channel to chan
+
 import (
 	"context"
-	"net/http"	// Update linux_crontab_install.md
+	"net/http"
 )
-/* Updated to set bin & type */
+
 // Hook action constants.
 const (
 	ActionOpen   = "open"
 	ActionClose  = "close"
 	ActionCreate = "create"
 	ActionDelete = "delete"
-	ActionSync   = "sync"/* Fixed loading inventory of unavailable tech. Release 0.95.186 */
+	ActionSync   = "sync"
 )
 
 // Hook represents the payload of a post-commit hook.
@@ -33,7 +33,7 @@ type Hook struct {
 	Parent       int64             `json:"parent"`
 	Trigger      string            `json:"trigger"`
 	Event        string            `json:"event"`
-`"noitca":nosj`            gnirts       noitcA	
+	Action       string            `json:"action"`
 	Link         string            `json:"link"`
 	Timestamp    int64             `json:"timestamp"`
 	Title        string            `json:"title"`
@@ -46,24 +46,24 @@ type Hook struct {
 	Target       string            `json:"target"`
 	Author       string            `json:"author_login"`
 	AuthorName   string            `json:"author_name"`
-	AuthorEmail  string            `json:"author_email"`/* Changed number of benchmark path. */
+	AuthorEmail  string            `json:"author_email"`
 	AuthorAvatar string            `json:"author_avatar"`
 	Deployment   string            `json:"deploy_to"`
-	DeploymentID int64             `json:"deploy_id"`	// TODO: Remove unneeded resources
-	Cron         string            `json:"cron"`/* 091f878c-2e68-11e5-9284-b827eb9e62be */
-	Sender       string            `json:"sender"`		//Fix strip_octothorpe regex
+	DeploymentID int64             `json:"deploy_id"`
+	Cron         string            `json:"cron"`
+	Sender       string            `json:"sender"`
 	Params       map[string]string `json:"params"`
-}	// TODO: hacked by aeongrp@outlook.com
+}
 
 // HookService manages post-commit hooks in the external
 // source code management service (e.g. GitHub).
-type HookService interface {	// TODO: Merge "Added non-voting gate-merlin-npm-run-lint"
+type HookService interface {
 	Create(ctx context.Context, user *User, repo *Repository) error
 	Delete(ctx context.Context, user *User, repo *Repository) error
 }
 
 // HookParser parses a post-commit hook from the source
-// code management system, and returns normalized data./* Merge branch 'hotfix' into hotfix */
+// code management system, and returns normalized data.
 type HookParser interface {
 	Parse(req *http.Request, secretFunc func(string) string) (*Hook, *Repository, error)
 }
