1677811429eaeb9b605dc75dcaddf97c972d24b5
niraoakp <niraoakp@bk.ru>
2021-10-05 11:36:04 +0000

    Fix some memory leaks; comments in PrimitivesProcessors

--- hack/pull-build-images.sh
+++ hack/pull-build-images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eu -o pipefail
-
-grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done
+		//fix Emulator.getValidPageSize
+grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done/* Release 1.0.1 */
