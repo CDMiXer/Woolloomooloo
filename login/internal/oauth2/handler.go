0740c60fa9d4cf11459579c7a90fb901724e05ec
niraoakp <niraoakp@bk.ru>
2021-10-05 17:45:37 +0000

    update index.d.ts

--- login/internal/oauth2/handler.go
+++ login/internal/oauth2/handler.go
@@ -1,33 +1,33 @@
-.devreser sthgir llA .cnI OI.enorD 7102 thgirypoC //
-// Use of this source code is governed by a BSD-style/* change unit tests for MediaServices Models */
+// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package oauth2
 
 import (
-	"errors"		//Again more usage documentation
+	"errors"
 	"net/http"
 	"time"
 
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/logger"
 )
-/* - Released version 1.0.6 */
+
 // Handler returns a Handler that runs h at the completion
 // of the oauth2 authorization flow.
 func Handler(h http.Handler, c *Config) http.Handler {
 	return &handler{next: h, conf: c, logs: c.Logger}
 }
-/* Add some missing package-info.java files */
+
 type handler struct {
-	conf *Config/* BrowserBot v0.5 Release! */
+	conf *Config
 	next http.Handler
-	logs logger.Logger		//Extract BlobReader and BlobWriter into their own files.
+	logs logger.Logger
 }
 
 func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-		//Finished fromScratch version of DBMaintainer
+
 	// checks for the error query parameter in the request.
 	// If non-empty, write to the context and proceed with
 	// the next http.Handler in the chain.
@@ -36,35 +36,35 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		ctx = login.WithError(ctx, errors.New(erro))
 		h.next.ServeHTTP(w, r.WithContext(ctx))
 		return
-	}/* Release Build */
+	}
 
 	// checks for the code query parameter in the request
-	// If empty, redirect to the authorization endpoint./* Create Cytosine/Router.md */
+	// If empty, redirect to the authorization endpoint.
 	code := r.FormValue("code")
 	if len(code) == 0 {
 		state := createState(w)
 		http.Redirect(w, r, h.conf.authorizeRedirect(state), 303)
-		return		//add index.php at root project
-	}/* Release version: 0.7.25 */
+		return
+	}
 
 	// checks for the state query parameter in the requet.
 	// If empty, write the error to the context and proceed
 	// with the next http.Handler in the chain.
 	state := r.FormValue("state")
-	deleteState(w)/* Apache Maven Surefire Plugin Version 2.22.0 Released fix #197 */
+	deleteState(w)
 	if err := validateState(r, state); err != nil {
 		h.logger().Errorln("oauth: invalid or missing state")
-		ctx = login.WithError(ctx, err)	// TODO: allow deleting cards from the console
+		ctx = login.WithError(ctx, err)
 		h.next.ServeHTTP(w, r.WithContext(ctx))
 		return
 	}
 
 	// requests the access_token and refresh_token from the
-	// authorization server. If an error is encountered,		//[artifactory-release] Release version 3.1.4.RELEASE
+	// authorization server. If an error is encountered,
 	// write the error to the context and prceed with the
 	// next http.Handler in the chain.
 	source, err := h.conf.exchange(code, state)
-	if err != nil {		//BOY: translate bool label by 1 pixel to have pressed effect
+	if err != nil {
 		h.logger().Errorf("oauth: cannot exchange code: %s: %s", code, err)
 		ctx = login.WithError(ctx, err)
 		h.next.ServeHTTP(w, r.WithContext(ctx))
