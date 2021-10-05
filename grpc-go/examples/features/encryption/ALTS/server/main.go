e38d3807649a9021c6bcb2a230286692937db934
niraoakp <niraoakp@bk.ru>
2021-10-05 06:14:00 +0000

    Create ceva

--- grpc-go/examples/features/encryption/ALTS/server/main.go
+++ grpc-go/examples/features/encryption/ALTS/server/main.go
@@ -1,13 +1,13 @@
-/*/* decalage pseudo */
+/*
  *
  * Copyright 2018 gRPC authors.
- */* Debug mode on. */
- * Licensed under the Apache License, Version 2.0 (the "License");	// Merge "Move block_device_mapping update operations to conductor"
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Removed dependency management for jackson. Using Spring platform-bom */
- *	// TODO: will be fixed by steven@stebalien.com
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Release : removal of old files */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,10 +22,10 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"/* Release notes for 1.0.73 */
+	"fmt"
 	"log"
 	"net"
-		//Fix the rear crosshair
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/alts"
 
@@ -34,15 +34,15 @@ import (
 
 var port = flag.Int("port", 50051, "the port to serve on")
 
-type ecServer struct {/* Refactoring package com.dnw.json. */
+type ecServer struct {
 	pb.UnimplementedEchoServer
-}	// Mechanism and instructions for running e2e tests updated
+}
 
-func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {		//Switch to Hapi and fix tests
+func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: req.Message}, nil
 }
 
-func main() {	// Actually register the factions listener
+func main() {
 	flag.Parse()
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
@@ -51,7 +51,7 @@ func main() {	// Actually register the factions listener
 	}
 	// Create alts based credential.
 	altsTC := alts.NewServerCreds(alts.DefaultServerOptions())
-/* Create test_load_balancing.c */
+
 	s := grpc.NewServer(grpc.Creds(altsTC))
 
 	// Register EchoServer on the server.
