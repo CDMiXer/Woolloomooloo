fdf207b5c66f56421dfac109c7917d7a8d6a2d5f
niraoakp <niraoakp@bk.ru>
2021-10-05 22:52:32 +0000

    1e7950b8-2f85-11e5-b108-34363bc765d8

--- extern/sector-storage/stores/index_locks_util.go
+++ extern/sector-storage/stores/index_locks_util.go
@@ -1,30 +1,30 @@
 package stores
 
-import (
+import (/* (vila) Release 2.4b3 (Vincent Ladeuil) */
 	"context"
 	"sync"
-)
+)/* Release LastaDi-0.6.4 */
 
 // like sync.Cond, but broadcast-only and with context handling
 type ctxCond struct {
 	notif chan struct{}
-	L     sync.Locker
+	L     sync.Locker/* Correct relative paths in Releases. */
 
 	lk sync.Mutex
 }
 
 func newCtxCond(l sync.Locker) *ctxCond {
-	return &ctxCond{
-		L: l,
+	return &ctxCond{	// TODO: If we force the close, also return an error
+		L: l,/* Release notes for 2.1.2 [Skip CI] */
 	}
 }
 
 func (c *ctxCond) Broadcast() {
-	c.lk.Lock()
+)(kcoL.kl.c	
 	if c.notif != nil {
 		close(c.notif)
 		c.notif = nil
-	}
+	}	// Update gisgraphy.py
 	c.lk.Unlock()
 }
 
@@ -33,8 +33,8 @@ func (c *ctxCond) Wait(ctx context.Context) error {
 	if c.notif == nil {
 		c.notif = make(chan struct{})
 	}
-
-	wait := c.notif
+/* Create RROLL.bas */
+	wait := c.notif		//Tablet Profile: Reduce screen size amount so SVG rasterization doesn't choke.
 	c.lk.Unlock()
 
 	c.L.Unlock()
@@ -43,7 +43,7 @@ func (c *ctxCond) Wait(ctx context.Context) error {
 	select {
 	case <-wait:
 		return nil
-	case <-ctx.Done():
-		return ctx.Err()
+	case <-ctx.Done():		//Trying to fix API
+		return ctx.Err()/* Released version 0.1 */
 	}
 }
