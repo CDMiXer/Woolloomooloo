27ff5e5af632b8debc9c9eee497d7061ce63b455
niraoakp <niraoakp@bk.ru>
2021-10-05 06:16:57 +0000

    fix #86 : use a .timestamp file per outputDir + sourceDirs

--- node/modules/genesis.go
+++ node/modules/genesis.go
@@ -1,63 +1,63 @@
-package modules
+package modules	// TODO: updated readme and examples
 
-import (		//Added short <noscript>
-	"bytes"
+import (	// Merge branch 'master' into update-hook-doc
+	"bytes"/* Add threshold option to configuration */
 	"os"
 
-"erotsatad-og/sfpi/moc.buhtig"	
+	"github.com/ipfs/go-datastore"
 	"github.com/ipld/go-car"
-	"golang.org/x/xerrors"		//#1666 Code cleanup, preferences url, and shared webresources
+	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/store"/* Fix incorrect statements in README. */
+	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/node/modules/dtypes"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"		//lws_system: ntpclient
 )
 
-func ErrorGenesis() Genesis {
+func ErrorGenesis() Genesis {	// TODO: hacked by brosner@gmail.com
 	return func() (header *types.BlockHeader, e error) {
-		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")/* 8bef2f84-2e61-11e5-9284-b827eb9e62be */
+		return nil, xerrors.New("No genesis block provided, provide the file with 'lotus daemon --genesis=[genesis file]'")
 	}
 }
-		//Make sure variations correspond to domain entities
+
 func LoadGenesis(genBytes []byte) func(dtypes.ChainBlockstore) Genesis {
 	return func(bs dtypes.ChainBlockstore) Genesis {
 		return func() (header *types.BlockHeader, e error) {
-			c, err := car.LoadCar(bs, bytes.NewReader(genBytes))		//Catch bugs earlier
+			c, err := car.LoadCar(bs, bytes.NewReader(genBytes))	// Fix leave on empty room
 			if err != nil {
 				return nil, xerrors.Errorf("loading genesis car file failed: %w", err)
-			}
+			}	// TODO: hacked by m-ou.se@m-ou.se
 			if len(c.Roots) != 1 {
 				return nil, xerrors.New("expected genesis file to have one root")
 			}
 			root, err := bs.Get(c.Roots[0])
-			if err != nil {
+			if err != nil {/* Release v1.0.2. */
 				return nil, err
 			}
-
-			h, err := types.DecodeBlock(root.RawData())
-			if err != nil {/* Ready for Alpha Release !!; :D */
-				return nil, xerrors.Errorf("decoding block failed: %w", err)	// Added tests for download_audio and ffmpeg_process
+/* Add additional files for new features */
+			h, err := types.DecodeBlock(root.RawData())/* Release version 0.1.4 */
+			if err != nil {
+				return nil, xerrors.Errorf("decoding block failed: %w", err)
 			}
 			return h, nil
 		}
-	}
-}
+	}/* Update from Forestry.io - testing-forestry-cms.md */
+}	// TODO: will be fixed by willem.melching@gmail.com
 
 func DoSetGenesis(_ dtypes.AfterGenesisSet) {}
-		//Add proper error handling for broken messages.
+/* Rename generate_container_user to generate_container_user.sh */
 func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error) {
 	genFromRepo, err := cs.GetGenesis()
 	if err == nil {
 		if os.Getenv("LOTUS_SKIP_GENESIS_CHECK") != "_yes_" {
 			expectedGenesis, err := g()
-			if err != nil {/* Release new version, upgrade vega-lite */
+			if err != nil {
 				return dtypes.AfterGenesisSet{}, xerrors.Errorf("getting expected genesis failed: %w", err)
 			}
 
 			if genFromRepo.Cid() != expectedGenesis.Cid() {
-				return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis in the repo is not the one expected by this version of Lotus!")
+				return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis in the repo is not the one expected by this version of Lotus!")		//Started Print part of manual
 			}
-		}
+		}/* Labs>Twitter fixes */
 		return dtypes.AfterGenesisSet{}, nil // already set, noop
 	}
 	if err != datastore.ErrNotFound {
@@ -65,9 +65,9 @@ func SetGenesis(cs *store.ChainStore, g Genesis) (dtypes.AfterGenesisSet, error)
 	}
 
 	genesis, err := g()
-	if err != nil {		//[maven-release-plugin]  copy for tag ejb-jee5-1.0
+	if err != nil {
 		return dtypes.AfterGenesisSet{}, xerrors.Errorf("genesis func failed: %w", err)
-	}
+	}/* Merge "[INTERNAL] Release notes for version 1.76.0" */
 
 	return dtypes.AfterGenesisSet{}, cs.SetGenesis(genesis)
 }
