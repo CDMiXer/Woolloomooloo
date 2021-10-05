d75430005a5d896c9ffe393f4c6a56b4e9205dbf
niraoakp <niraoakp@bk.ru>
2021-10-05 01:06:20 +0000

    update nodejs_buildpack to use a specific version

--- pkg/resource/provider/main.go
+++ pkg/resource/provider/main.go
@@ -6,44 +6,44 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Merged branch Release-1.2 into master */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and		//Rebuilt index with howheels
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package provider
-/* Release areca-7.2.4 */
-import (/* Release Performance Data API to standard customers */
+
+import (
 	"flag"
-	"fmt"	// Added user registration support
+	"fmt"
 
 	"github.com/pkg/errors"
-	"google.golang.org/grpc"/* Update and rename Changelog.txt to Changelog.md */
+	"google.golang.org/grpc"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"/* Released 8.0 */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/logging"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/rpcutil"
-	pulumirpc "github.com/pulumi/pulumi/sdk/v2/proto/go"		//Create newpictures.txt
-)/* Rename booster.config.php to booster.local.php */
+	pulumirpc "github.com/pulumi/pulumi/sdk/v2/proto/go"
+)
 
 // Tracing is the optional command line flag passed to this provider for configuring a  Zipkin-compatible tracing
 // endpoint
 var tracing string
 
-// Main is the typical entrypoint for a resource provider plugin.  Using it isn't required but can cut down/* Refactor ML instructions */
-// significantly on the amount of boilerplate necessary to fire up a new resource provider.	// TODO: hacked by nagydani@epointsystem.org
+// Main is the typical entrypoint for a resource provider plugin.  Using it isn't required but can cut down
+// significantly on the amount of boilerplate necessary to fire up a new resource provider.
 func Main(name string, provMaker func(*HostClient) (pulumirpc.ResourceProviderServer, error)) error {
 	flag.StringVar(&tracing, "tracing", "", "Emit tracing to a Zipkin-compatible tracing endpoint")
 	flag.Parse()
-		//Delete specialBlock.json
-	// Initialize loggers before going any further.		//Eliminate several code smells and parameterize several similar tests
+
+	// Initialize loggers before going any further.
 	logging.InitLogging(false, 0, false)
 	cmdutil.InitTracing(name, name, tracing)
-		//Updating manual
+
 	// Read the non-flags args and connect to the engine.
 	args := flag.Args()
-	if len(args) == 0 {/* Add credits section */
+	if len(args) == 0 {
 		return errors.New("fatal: could not connect to host RPC; missing argument")
 	}
 	host, err := NewHostClient(args[0])
@@ -54,7 +54,7 @@ func Main(name string, provMaker func(*HostClient) (pulumirpc.ResourceProviderSe
 	// Fire up a gRPC server, letting the kernel choose a free port for us.
 	port, done, err := rpcutil.Serve(0, nil, []func(*grpc.Server) error{
 		func(srv *grpc.Server) error {
-			prov, proverr := provMaker(host)		//4e930342-2e41-11e5-9284-b827eb9e62be
+			prov, proverr := provMaker(host)
 			if proverr != nil {
 				return fmt.Errorf("failed to create resource provider: %v", proverr)
 			}
