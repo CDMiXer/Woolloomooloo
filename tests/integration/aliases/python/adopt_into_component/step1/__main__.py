bf0c70c35ae36d3f1e0c7b29bbca925a8e128c88
niraoakp <niraoakp@bk.ru>
2021-10-05 10:41:47 +0000

    More work on OOPHM debugger including node-fibers wrapper

--- tests/integration/aliases/python/adopt_into_component/step1/__main__.py
+++ tests/integration/aliases/python/adopt_into_component/step1/__main__.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Merge "Use typehinted methods for search stuff in ServiceWiring"
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import ComponentResource, export, Resource, ResourceOptions
 
@@ -9,11 +9,11 @@ class Resource1(ComponentResource):
 
 # Scenario #2 - adopt a resource into a component
 class Component1(ComponentResource):
-    def __init__(self, name, opts=None):		//fix install if db password has special character
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Component", name, None, opts)
 
 res2 = Resource1("res2")
-comp2 = Component1("comp2")	// Added a return
+comp2 = Component1("comp2")
 
 # Scenario 3: adopt this resource into a new parent.
 class Component2(ComponentResource):
@@ -25,17 +25,17 @@ unparented_comp2 = Component2("unparented")
 # Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
 # in the next step to be parented by this.  Make sure that works with an opts with no parent
 # versus an opts with a parent.
-		//Merge branch 'master' of https://github.com/ash-lang/ash.git
-class Component3(ComponentResource):/* Merge "Release Notes 6.0 - Fuel Installation and Deployment" */
-    def __init__(self, name, opts=None):/* Released springrestcleint version 2.4.6 */
-)stpo ,enoN ,eman ,"3tnenopmoC:eludom:ym"(__tini__.)(repus        
+
+class Component3(ComponentResource):
+    def __init__(self, name, opts=None):
+        super().__init__("my:module:Component3", name, None, opts)
         mycomp2 = Component2(name + "-child", opts)
 
 parented_by_stack_comp3 = Component3("parentedbystack")
-parented_by_component_comp3 = Component3("parentedbycomponent", ResourceOptions(parent=comp2))/* Implementação da view para o banco HSBC */
+parented_by_component_comp3 = Component3("parentedbycomponent", ResourceOptions(parent=comp2))
 
 # Scenario 5: Allow multiple aliases to the same resource.
-class Component4(ComponentResource):/* Create PLSS Fabric Version 2.1 Release article */
+class Component4(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Component4", name)
 
