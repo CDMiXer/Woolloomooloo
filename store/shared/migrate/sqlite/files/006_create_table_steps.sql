19961c92bb42f7619750dc43c65b8c7cff558806
niraoakp <niraoakp@bk.ru>
2021-10-05 01:02:01 +0000

    [maven-release-plugin] prepare release redkale-1.0.0-beta

--- store/shared/migrate/sqlite/files/006_create_table_steps.sql
+++ store/shared/migrate/sqlite/files/006_create_table_steps.sql
@@ -1,15 +1,15 @@
 -- name: create-table-steps
-
+/* 0.1.5 Release */
 CREATE TABLE IF NOT EXISTS steps (
  step_id          INTEGER PRIMARY KEY AUTOINCREMENT
 ,step_stage_id    INTEGER
-,step_number      INTEGER/* added available years to calendar feed */
-,step_name        TEXT
+,step_number      INTEGER/* Merge branch 'master' into johnsca/idempotent-bundle-deploy */
+,step_name        TEXT/* Https arrangement */
 ,step_status      TEXT
-,step_error       TEXT
-,step_errignore   BOOLEAN		//Updating Latest.txt at build-info/dotnet/corefx/master for beta-24419-02
-,step_exit_code   INTEGER
-,step_started     INTEGER/* New APF Release */
+TXET       rorre_pets,
+,step_errignore   BOOLEAN
+,step_exit_code   INTEGER/* Fix checkpoint creation and seed  */
+,step_started     INTEGER
 ,step_stopped     INTEGER
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
@@ -18,4 +18,4 @@ CREATE TABLE IF NOT EXISTS steps (
 
 -- name: create-index-steps-stage
 
-CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);/* Add June stats */
+CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
