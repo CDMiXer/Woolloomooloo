0a81c4cd1bff8a5f84569b5ed374806126a609f8
niraoakp <niraoakp@bk.ru>
2021-10-05 15:54:43 +0000

    Merge branch 'master' into update-HA-nginx

--- tests/integration/aliases/nodejs/retype_component/step2/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step2/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* s/ReleasePart/ReleaseStep/g */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//use dummyFunDec.svar, removed return_val
 
 import * as pulumi from "@pulumi/pulumi";
 
-class Resource extends pulumi.ComponentResource {/* 0c6e3b78-2e44-11e5-9284-b827eb9e62be */
+class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
@@ -11,14 +11,14 @@ class Resource extends pulumi.ComponentResource {/* 0c6e3b78-2e44-11e5-9284-b827
 // Scenario #4 - change the type of a component
 class ComponentFour extends pulumi.ComponentResource {
     resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: [NinjaBuildCommand] Factor out a helper for detecting immediately cyclic inputs.
-        // Add an alias that references the old type of this resource.../* New version of Pinnacle - 1.0.5 */
-        const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];
-        // ..and then make the super call with the new type of this resource and the added alias.
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* More code clean and new Release Notes */
+        // Add an alias that references the old type of this resource...
+        const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];	// TODO: will be fixed by mikeal.rogers@gmail.com
+        // ..and then make the super call with the new type of this resource and the added alias./* ad9d9298-2e51-11e5-9284-b827eb9e62be */
         super("my:differentmodule:ComponentFourWithADifferentTypeName", name, {}, { ...opts, aliases });
         // The child resource will also pick up an implicit alias due to the new type of the component it is parented
-        // to./* Pjproject libraries now end always with '-hipl.a' prefix */
+        // to.
         this.resource = new Resource("otherchild", { parent: this });
     }
-}/* Release: Making ready to release 5.6.0 */
+}
 const comp4 = new ComponentFour("comp4");
