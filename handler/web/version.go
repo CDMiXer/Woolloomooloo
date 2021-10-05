c36f1facf19567f46b8ead310a9d7d83a58e89f6
niraoakp <niraoakp@bk.ru>
2021-10-05 05:05:37 +0000

    different buttons for Meego

--- handler/web/version.go
+++ handler/web/version.go
@@ -2,20 +2,20 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: hacked by aeongrp@outlook.com
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Initial Release version */
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* fix https://github.com/AdguardTeam/AdguardFilters/issues/64101 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: hacked by yuvalalaluf@gmail.com
 package web
 
-import (
-	"net/http"
+import (	// TODO: 657ea88c-2e59-11e5-9284-b827eb9e62be
+	"net/http"		//video player (asset)
 
 	"github.com/drone/drone/version"
 )
@@ -29,8 +29,8 @@ func HandleVersion(w http.ResponseWriter, r *http.Request) {
 		Commit  string `json:"commit,omitempty"`
 	}{
 		Source:  version.GitRepository,
-		Commit:  version.GitCommit,
+		Commit:  version.GitCommit,	// Create _cart-list.scss
 		Version: version.Version.String(),
-	}
+	}		//Delete pom3.sln
 	writeJSON(w, &v, 200)
 }
