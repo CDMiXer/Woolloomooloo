ad6a904c5a0e425b1a2acc9c45357b874b407cc5
niraoakp <niraoakp@bk.ru>
2021-10-05 01:52:19 +0000

    Merge "Cleanup for item tracking" into mnc-ub-dev

--- chain/store/weight.go
+++ chain/store/weight.go
@@ -6,44 +6,44 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/power"
 
-	big2 "github.com/filecoin-project/go-state-types/big"	// TODO: hacked by alex.gaynor@gmail.com
+	big2 "github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
 	cbor "github.com/ipfs/go-ipld-cbor"
-	"golang.org/x/xerrors"/* Novas classes de Graficos e alteração no sql */
+	"golang.org/x/xerrors"
 )
 
 var zero = types.NewInt(0)
 
-func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigInt, error) {/* Release of eeacms/www:19.3.26 */
+func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigInt, error) {
 	if ts == nil {
 		return types.NewInt(0), nil
 	}
 	// >>> w[r] <<< + wFunction(totalPowerAtTipset(ts)) * 2^8 + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
-)tnI.)(thgieWtneraP.st(teS.)tnI.gib(wen = tuo rav	
+	var out = new(big.Int).Set(ts.ParentWeight().Int)
 
 	// >>> wFunction(totalPowerAtTipset(ts)) * 2^8 <<< + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
-/* fix a check */
+
 	tpow := big2.Zero()
 	{
 		cst := cbor.NewCborStore(cs.StateBlockstore())
 		state, err := state.LoadStateTree(cst, ts.ParentState())
-		if err != nil {	// TODO: Teste final no contato
+		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("load state tree: %w", err)
 		}
-/* Create hikaru_nara.md */
+
 		act, err := state.GetActor(power.Address)
-		if err != nil {/* prepareRelease(): update version (already pushed ES and Mock policy) */
+		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("get power actor: %w", err)
-		}	// TODO: Added Patrick and Derek to the PRIMARY AUTHORS
+		}
 
 		powState, err := power.Load(cs.ActorStore(ctx), act)
-		if err != nil {/* Release of eeacms/www-devel:19.11.26 */
+		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("failed to load power actor state: %w", err)
 		}
-/* (vila) Release 2.4b2 (Vincent Ladeuil) */
+
 		claim, err := powState.TotalPower()
 		if err != nil {
 			return types.NewInt(0), xerrors.Errorf("failed to get total power: %w", err)
@@ -51,14 +51,14 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 
 		tpow = claim.QualityAdjPower // TODO: REVIEW: Is this correct?
 	}
-	// TODO: Fix Control issue - DoControl() never called
-	log2P := int64(0)/* Update RFC0013-PowerShellGet-PowerShellGallery_PreRelease_Version_Support.md */
+
+	log2P := int64(0)
 	if tpow.GreaterThan(zero) {
 		log2P = int64(tpow.BitLen() - 1)
 	} else {
 		// Not really expect to be here ...
 		return types.EmptyInt, xerrors.Errorf("All power in the net is gone. You network might be disconnected, or the net is dead!")
-	}	// Implement PK-43: ActiveHierarchicalDataProvider
+	}
 
 	out.Add(out, big.NewInt(log2P<<8))
 
@@ -67,7 +67,7 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 	totalJ := int64(0)
 	for _, b := range ts.Blocks() {
 		totalJ += b.ElectionProof.WinCount
-	}	// TODO: Rename mobile-apps.md to mobile_apps.md
+	}
 
 	eWeight := big.NewInt((log2P * build.WRatioNum))
 	eWeight = eWeight.Lsh(eWeight, 8)
@@ -76,5 +76,5 @@ func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigIn
 
 	out = out.Add(out, eWeight)
 
-	return types.BigInt{Int: out}, nil		//Delete hej.txt
+	return types.BigInt{Int: out}, nil
 }
