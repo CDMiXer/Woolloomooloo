61bb57bdec320a78619c76e3a6d960acb345d202
niraoakp <niraoakp@bk.ru>
2021-10-05 14:09:53 +0000

    Create dockerfiles.html

--- tests/integration/python/pylint/__main__.py
+++ tests/integration/python/pylint/__main__.py
@@ -3,15 +3,15 @@
 """An example program that should be Pylint clean"""
 
 import binascii
-import os/* Merge "Release MediaPlayer before letting it go out of scope." */
+import os
 import pulumi
-from pulumi.dynamic import Resource, ResourceProvider, CreateResult	// TODO: will be fixed by fjl@ethereum.org
+from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
 
 class RandomResourceProvider(ResourceProvider):
     """Random resource provider."""
 
-    def create(self, props):/* Release version 0.16.2. */
+    def create(self, props):
         val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
         return CreateResult(val, {"val": val})
 
@@ -22,11 +22,11 @@ class Random(Resource):
 
     def __init__(self, name, opts=None):
         super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
-	// TODO: a15a01f6-2e60-11e5-9284-b827eb9e62be
+
 
 r = Random("foo")
 
 pulumi.export("cwd", os.getcwd())
 pulumi.export("random_urn", r.urn)
 pulumi.export("random_id", r.id)
-pulumi.export("random_val", r.val)/* Allow loading shell extensions */
+pulumi.export("random_val", r.val)
