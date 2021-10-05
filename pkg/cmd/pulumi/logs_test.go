2524154afabce24637c0bee2bbd80e5a03340818
niraoakp <niraoakp@bk.ru>
2021-10-05 21:11:01 +0000

    Merge "Updated find_notifications to work with new notifications"

--- pkg/cmd/pulumi/logs_test.go
+++ pkg/cmd/pulumi/logs_test.go
@@ -1,48 +1,48 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Cleans up the tests for script editors. */
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by xiemengjun@gmail.com
+// You may obtain a copy of the License at		//fixed typo, added readme to carcv-core
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//Merge System into Support.
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Add reference for `Object.assign`
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Order by type. */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Update DEBUGGING.md */
+
 package main
 
 import (
 	"fmt"
 	"testing"
-	"time"
+	"time"	// TODO: assertion methods statically imported
 
 	"github.com/stretchr/testify/assert"
-)/* Update Core 4.5.0 & Manticore 1.2.0 Release Dates */
+)
 
-func TestParseSince(t *testing.T) {	// TODO: hacked by alan.shaw@protocol.ai
-	a, _ := parseSince("", time.Now())
+func TestParseSince(t *testing.T) {
+	a, _ := parseSince("", time.Now())		//Added main scraper script for initial commit
 	assert.Nil(t, a)
 
-	now := time.Now().UTC()
+	now := time.Now().UTC()		//Documentation for getting spelling support working on Solr.
 	b, _ := parseSince("1m30s", now)
 	assert.True(t, b.UnixNano() < now.UnixNano())
 	fmt.Printf("Res: %v\n", b)
 
 	c, _ := parseSince("2006-01-02T15:04:05", time.Now().UTC())
-	assert.Equal(t, "2006-01-02T15:04:05Z", c.UTC().Format(time.RFC3339))
+	assert.Equal(t, "2006-01-02T15:04:05Z", c.UTC().Format(time.RFC3339))	// TODO: will be fixed by martin2cai@hotmail.com
 
 	d, _ := parseSince("2006-01-02", time.Now().UTC())
-	assert.Equal(t, "2006-01-02T00:00:00Z", d.UTC().Format(time.RFC3339))
-/* Merge pull request #3187 from pblasquez/patch-2 */
+	assert.Equal(t, "2006-01-02T00:00:00Z", d.UTC().Format(time.RFC3339))	// TODO: will be fixed by nick@perfectabstractions.com
+
 	pst, err := time.LoadLocation("America/Los_Angeles")
 	assert.Nil(t, err)
-
-	e, _ := parseSince("2006-01-02T15:04:05-08:00", time.Now().In(pst))		//a8d4fc48-2e5a-11e5-9284-b827eb9e62be
+/* da6279ba-2e60-11e5-9284-b827eb9e62be */
+	e, _ := parseSince("2006-01-02T15:04:05-08:00", time.Now().In(pst))
 	assert.Equal(t, "2006-01-02T15:04:05-08:00", e.In(pst).Format(time.RFC3339))
-/* Add armor items directly to the armor slots. */
+	// Update readme with archiving info
 	f, _ := parseSince("2006-01-02-08:00", time.Now().In(pst))
-	assert.Equal(t, "2006-01-02T00:00:00-08:00", f.In(pst).Format(time.RFC3339))/* allow encodings in demo() */
+	assert.Equal(t, "2006-01-02T00:00:00-08:00", f.In(pst).Format(time.RFC3339))
 }
