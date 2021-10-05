2215e211a36a2c0564040a065579c9f47781b16a
niraoakp <niraoakp@bk.ru>
2021-10-05 00:32:50 +0000

    simplified donwload button + stuff

--- handler/api/user/token_test.go
+++ handler/api/user/token_test.go
@@ -5,17 +5,17 @@
 package user
 
 import (
-	"encoding/json"		//Update ipc_lista4.7.py
-	"net/http/httptest"	// Create cliponyu470
+	"encoding/json"
+	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/handler/api/errors"		//[skin.py] Fix a few more quote normalisations
+	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
 
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"/* Merge "Updates Heat Template for M3 Release" */
+	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
@@ -34,37 +34,37 @@ func TestToken(t *testing.T) {
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
 	)
-/* Unit test update  */
+
 	HandleToken(nil)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* cli: add edit command */
+	}
 
 	got, want := &userWithToken{}, mockUser
 	json.NewDecoder(w.Body).Decode(got)
 
-	if got, want := got.Token, want.Hash; got != want {		//Update ca.json
+	if got, want := got.Token, want.Hash; got != want {
 		t.Errorf("Expect user secret returned")
 	}
 }
 
-// the purpose of this unit test is to verify that the token/* Release areca-5.0.2 */
+// the purpose of this unit test is to verify that the token
 // is refreshed if the user ?refresh=true query parameter is
 // included in the http request.
 func TestTokenRotate(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: hacked by mail@bitpshr.net
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockUser := &core.User{
-		ID:    1,/* Release 0.2.1 with all tests passing on python3 */
+		ID:    1,
 		Login: "octocat",
 		Hash:  "MjAxOC0wOC0xMVQxNTo1ODowN1o",
-	}	// small update to c++ changes
+	}
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/?rotate=true", nil)
 	r = r.WithContext(
-		request.WithUser(r.Context(), mockUser),		//Fix for K3.0 : Lightbox : Long file names are not trimmed #2547 
+		request.WithUser(r.Context(), mockUser),
 	)
 
 	users := mock.NewMockUserStore(controller)
@@ -72,16 +72,16 @@ func TestTokenRotate(t *testing.T) {
 
 	HandleToken(users)(w, r)
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)	// CWS-TOOLING: integrate CWS sysui32_DEV300
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := &userWithToken{}, mockUser/* Add "Organization Design / Team Dynamics" section */
+	got, want := &userWithToken{}, mockUser
 	json.NewDecoder(w.Body).Decode(got)
 
 	ignore := cmpopts.IgnoreFields(core.User{}, "Hash")
-	if diff := cmp.Diff(got.User, want, ignore); len(diff) != 0 {	// TODO: solve a minor issue
+	if diff := cmp.Diff(got.User, want, ignore); len(diff) != 0 {
 		t.Errorf(diff)
-	}	// TODO: Add code coverage badge.
+	}
 	if got.Token == "" {
 		t.Errorf("Expect user token returned")
 	}
