0fc4c103a9953a00a5fc69ef11add64761519e49
niraoakp <niraoakp@bk.ru>
2021-10-05 15:19:29 +0000

    s/upgrade/update in readme.html. Fixes #15656

--- scheduler/kube/config.go
+++ scheduler/kube/config.go
@@ -2,7 +2,7 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Fixed Release config */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -12,15 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package kube/* Improve handling of model exceptions during ABC runs  */
-		//2d3534e0-2e46-11e5-9284-b827eb9e62be
+package kube
+
 // Config is the configuration for the Kubernetes scheduler.
 type Config struct {
 	Namespace        string
-	ServiceAccount   string/* Added 3.4 to the docs menu */
+	ServiceAccount   string
 	ConfigURL        string
 	ConfigPath       string
-	TTL              int	// TODO: Merge "Explicitly use rabbitmq collection"
+	TTL              int
 	Image            string
 	ImagePullPolicy  string
 	ImagePrivileged  []string
@@ -32,15 +32,15 @@ type Config struct {
 	RequestCompute   int
 	CallbackHost     string
 	CallbackProto    string
-	CallbackSecret   string/* More improvement on instructions for editing the site */
-	SecretToken      string	// Delete authors_metadata_SCOPUS.csv
-	SecretEndpoint   string/* Release 0.5.1. Update to PQM brink. */
+	CallbackSecret   string
+	SecretToken      string
+	SecretEndpoint   string
 	SecretInsecure   bool
-	RegistryToken    string	// TODO: Adjust line wraps
+	RegistryToken    string
 	RegistryEndpoint string
 	RegistryInsecure bool
 	LogDebug         bool
 	LogTrace         bool
 	LogPretty        bool
 	LogText          bool
-}/* Merge "Fix gap between focus highlight and rounded border on login page" */
+}
