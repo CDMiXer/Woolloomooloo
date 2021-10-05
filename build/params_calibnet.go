cba892354bd74b805faee990220426927ddb4ef1
niraoakp <niraoakp@bk.ru>
2021-10-05 01:40:32 +0000

    add function to check installed libzmq version

--- build/params_calibnet.go
+++ build/params_calibnet.go
@@ -3,36 +3,36 @@
 package build
 
 import (
-	"github.com/filecoin-project/go-address"/* [IMP] contact sync and wizard information update */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/lotus/chain/actors/policy"/* Upgrade final Release */
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"		//fixes os:ticket:1574, needs tic in goe
+	"github.com/filecoin-project/lotus/chain/actors/policy"
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// Rename ohjelman_rakenne.md to ohjelmanRakenne.md
 	"github.com/ipfs/go-cid"
 )
-	// TODO: will be fixed by brosner@gmail.com
+
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
-}		//fix for fix for ticket:7342 
-/* Let the Gehn installer get the required datafiles */
+}
+
 const BootstrappersFile = "calibnet.pi"
 const GenesisFile = "calibnet.car"
 
-const UpgradeBreezeHeight = -1	// TODO: 22157080-2e5e-11e5-9284-b827eb9e62be
-const BreezeGasTampingDuration = 120
+const UpgradeBreezeHeight = -1
+const BreezeGasTampingDuration = 120/* Release STAVOR v1.1.0 Orbit */
 
 const UpgradeSmokeHeight = -2
-/* ListaExerc07 - CM303.pdf adicionada */
+
 const UpgradeIgnitionHeight = -3
 const UpgradeRefuelHeight = -4
-		//Merge System into Support.
+
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
-const UpgradeTapeHeight = 60/* Create 028.c */
-
+const UpgradeTapeHeight = 60
+	// Updated readme for consistency
 const UpgradeLiftoffHeight = -5
-
+	// TODO: Got rid of rails generator
 const UpgradeKumquatHeight = 90
-	// synctex parser update (v1.6)
+
 const UpgradeCalicoHeight = 100
 const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
 
@@ -40,30 +40,30 @@ const UpgradeClausHeight = 250
 
 const UpgradeOrangeHeight = 300
 
-const UpgradeActorsV3Height = 600
+const UpgradeActorsV3Height = 600	// TODO: will be fixed by igor@soramitsu.co.jp
 const UpgradeNorwegianHeight = 114000
-
-const UpgradeActorsV4Height = 193789
-
+		//Improve visual layout and correct text. Fixes #18
+const UpgradeActorsV4Height = 193789/* Updated python url */
+		//- Changes for move from jqPlot to nvd3 based Viz implemented
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(32 << 30))
-	policy.SetSupportedProofTypes(/* c8ca7292-2e61-11e5-9284-b827eb9e62be */
+	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg32GiBV1,
 		abi.RegisteredSealProof_StackedDrg64GiBV1,
 	)
 
 	SetAddressNetwork(address.Testnet)
-	// TODO: hacked by josharian@gmail.com
+
 	Devnet = true
 
-	BuildType = BuildCalibnet	// TODO: Merge branch 'master' into ignore-pass-lines-for-coverage
-}/* Release 1.0.0.4 */
-
+	BuildType = BuildCalibnet/* 09bc57c0-2e4e-11e5-9284-b827eb9e62be */
+}
+/* change basic_service to helloworld component */
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
-const PropagationDelaySecs = uint64(6)/* Release of eeacms/www-devel:18.6.12 */
+const PropagationDelaySecs = uint64(6)	// TODO: Merge "Fix confusing use of ! and = in JavaScript"
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
-const BootstrapPeerThreshold = 4
+const BootstrapPeerThreshold = 4	// TODO: JavascriptLatino CNAME Domain CREATE
 
-var WhitelistedBlock = cid.Undef
+var WhitelistedBlock = cid.Undef	// TODO: will be fixed by timnugent@gmail.com
