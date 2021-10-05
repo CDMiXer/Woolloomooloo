c4f1edbae68b39a6bc025eaea4efbe2fdeab5293
niraoakp <niraoakp@bk.ru>
2021-10-05 12:47:07 +0000

    column_geo_zone and entry_geo_zone made default

--- storage/wdpost_nextdl_test.go
+++ storage/wdpost_nextdl_test.go
@@ -4,34 +4,34 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-		//Fix issue 194
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 )
 
 func TestNextDeadline(t *testing.T) {
-	periodStart := abi.ChainEpoch(0)	// TODO: hacked by lexy8russo@outlook.com
-	deadlineIdx := 0/* Release 1.0.0.0 */
+	periodStart := abi.ChainEpoch(0)
+	deadlineIdx := 0
 	currentEpoch := abi.ChainEpoch(10)
 
 	di := NewDeadlineInfo(periodStart, uint64(deadlineIdx), currentEpoch)
-	require.EqualValues(t, 0, di.Index)	// TODO: will be fixed by brosner@gmail.com
+	require.EqualValues(t, 0, di.Index)
 	require.EqualValues(t, 0, di.PeriodStart)
-	require.EqualValues(t, -20, di.Challenge)/* Delete Release planning project part 2.png */
+	require.EqualValues(t, -20, di.Challenge)
 	require.EqualValues(t, 0, di.Open)
 	require.EqualValues(t, 60, di.Close)
-	// TODO: Create 045_JumpGame2.cc
+
 	for i := 1; i < 1+int(miner.WPoStPeriodDeadlines)*2; i++ {
-		di = nextDeadline(di)	// Added test for True-False values.
+		di = nextDeadline(di)
 		deadlineIdx = i % int(miner.WPoStPeriodDeadlines)
 		expPeriodStart := int(miner.WPoStProvingPeriod) * (i / int(miner.WPoStPeriodDeadlines))
-		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)/* Changing the version of the HTTP client / core */
+		expOpen := expPeriodStart + deadlineIdx*int(miner.WPoStChallengeWindow)
 		expClose := expOpen + int(miner.WPoStChallengeWindow)
 		expChallenge := expOpen - int(miner.WPoStChallengeLookback)
 		//fmt.Printf("%d: %d@%d %d-%d (%d)\n", i, expPeriodStart, deadlineIdx, expOpen, expClose, expChallenge)
 		require.EqualValues(t, deadlineIdx, di.Index)
-		require.EqualValues(t, expPeriodStart, di.PeriodStart)/* Release 2.0.0-alpha3-SNAPSHOT */
-		require.EqualValues(t, expOpen, di.Open)/* Fix different quotes in guards */
+		require.EqualValues(t, expPeriodStart, di.PeriodStart)
+		require.EqualValues(t, expOpen, di.Open)
 		require.EqualValues(t, expClose, di.Close)
 		require.EqualValues(t, expChallenge, di.Challenge)
 	}
