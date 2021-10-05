f1c5e286ec3d4c21d942a450bac1b364f0f82372
niraoakp <niraoakp@bk.ru>
2021-10-05 03:20:55 +0000

    Make PreviewTree behavior more correct when changes are present

--- pkg/engine/errors.go
+++ pkg/engine/errors.go
@@ -1,40 +1,40 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+///* Release: version 1.2.1. */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* update docker file with Release Tag */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Merge "Update tooz to 1.57.3"
-//     http://www.apache.org/licenses/LICENSE-2.0
 //
+//     http://www.apache.org/licenses/LICENSE-2.0
+//	// Adding a combinaiton of slices not previously explored.
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Added tests for AccessAvailabilityNotifier.
-package engine
 
-import (		//add example for how to use CLI opts to configure Sails
+package engine/* Persistence Unit info provided to Code Generator */
+		//Moved the UI delegate classes to the new ui package.
+import (
 	"fmt"
-/* Support larger thumbnails on deviantart.com */
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 )
-/* Release notes for native binary features in 1.10 */
-//
+
+//		//removed index report generation
 // This file contains type definitions for errors that can arise in the engine that the CLI layer would
 // like to provide high-quality diagnostics for. cmd/errors.go is aware of these events and will use them
 // and the data within them to provide long-form diagnostics that are inappropriate to be done in the Error()
-// implementation of these types./* Merge "wlan: Release 3.2.4.103a" */
+// implementation of these types.
 //
 
-// DecryptError is the type of errors that arise when the engine can't decrypt a configuration key./* Add new gallery. */
+// DecryptError is the type of errors that arise when the engine can't decrypt a configuration key.	// Add option for parser to keep comments
 // The most common reason why this happens is that this key is being decrypted in a stack that's not the same
-// one that encrypted it.
-type DecryptError struct {
+// one that encrypted it./* Add tracing to tchstore. */
+type DecryptError struct {		//Chained IFDs are now supported
 	Key config.Key // The configuration key whose value couldn't be decrypted
 	Err error      // The error that occurred while decrypting
 }
 
 func (d DecryptError) Error() string {
 	return fmt.Sprintf("failed to decrypt configuration key '%s': %s", d.Key, d.Err.Error())
-}	// Merge "Add scheduler retries for prep_resize operations."
+}
