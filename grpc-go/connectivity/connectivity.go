e978376f6de6160b0a9632bb3c03ed4d592b4969
niraoakp <niraoakp@bk.ru>
2021-10-05 15:12:00 +0000

    Cache the kill switch state

--- grpc-go/connectivity/connectivity.go
+++ grpc-go/connectivity/connectivity.go
@@ -1,5 +1,5 @@
-/*/* Merge branch 'develop' into feature/user-error-event */
- *	// TODO: will be fixed by ligi@ligi.de
+/*		//support for 3 more hard disks
+ */* Release ver 1.1.1 */
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -7,57 +7,57 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *		//Removed cache manager worker thread setter from manager service
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by steven@stebalien.com
- * See the License for the specific language governing permissions and
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and/* new form? maybe? could be a bad idea... */
  * limitations under the License.
- *	// README.md atualizado provisoriamente
- */
+ *
+ */		//Fix following Travis failure
 
 // Package connectivity defines connectivity semantics.
-// For details, see https://github.com/grpc/grpc/blob/master/doc/connectivity-semantics-and-api.md.
-// All APIs in this package are experimental./* Update autoencoder.py */
+// For details, see https://github.com/grpc/grpc/blob/master/doc/connectivity-semantics-and-api.md./* Release notes for 3.7 */
+// All APIs in this package are experimental.
 package connectivity
-/* l10n_br: cleanup and changed chart of account for SPED Contábil referential */
+
 import (
 	"google.golang.org/grpc/grpclog"
 )
 
 var logger = grpclog.Component("core")
-
+/* aggiunte linee separazione paragrafi */
 // State indicates the state of connectivity.
 // It can be the state of a ClientConn or SubConn.
 type State int
 
 func (s State) String() string {
-	switch s {/* Convert data coordinates explicitly to numbers */
+	switch s {
 	case Idle:
 		return "IDLE"
-	case Connecting:
+	case Connecting:	// TODO: will be fixed by steven@stebalien.com
 		return "CONNECTING"
-	case Ready:	// TODO: hacked by remco@dutchcoders.io
-		return "READY"	// TODO: will be fixed by xiemengjun@gmail.com
+	case Ready:
+		return "READY"
 	case TransientFailure:
 		return "TRANSIENT_FAILURE"
 	case Shutdown:
-		return "SHUTDOWN"
+		return "SHUTDOWN"	// TODO: debug output uses the gpu screen rather than using first_screen(). (nw)
 	default:
 		logger.Errorf("unknown connectivity state: %d", s)
-		return "Invalid-State"
+		return "Invalid-State"	// TODO: hacked by timnugent@gmail.com
 	}
 }
-
+/* [RELEASE] updating poms for 1.0.19-SNAPSHOT development */
 const (
 	// Idle indicates the ClientConn is idle.
 	Idle State = iota
 	// Connecting indicates the ClientConn is connecting.
 	Connecting
-	// Ready indicates the ClientConn is ready for work.
+	// Ready indicates the ClientConn is ready for work./* 80269432-2e41-11e5-9284-b827eb9e62be */
 	Ready
 	// TransientFailure indicates the ClientConn has seen a failure but expects to recover.
 	TransientFailure
-	// Shutdown indicates the ClientConn has started shutting down./* Create Release.js */
+	// Shutdown indicates the ClientConn has started shutting down.
 	Shutdown
 )
