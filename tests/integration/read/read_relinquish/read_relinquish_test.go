f4f0a512dad043ceee2d61e199970973c8dee407
niraoakp <niraoakp@bk.ru>
2021-10-05 08:56:27 +0000

    initial attempt at breadcrumb

--- tests/integration/read/read_relinquish/read_relinquish_test.go
+++ tests/integration/read/read_relinquish/read_relinquish_test.go
@@ -1,24 +1,24 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-
-package ints
-	// Move tests to it's own folder.
+/* Releases can be found on the releases page. */
+package ints/* Lisätty linkit Google Groupsiin */
+/* Allows second click opening (fix #39) */
 import (
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// Adding a new trie type.
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 // Test that the engine is capable of relinquishing control of a resource without deleting it.
-func TestReadRelinquish(t *testing.T) {/* * Release 0.64.7878 */
+func TestReadRelinquish(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",	// detail level adjusts autonomly
+		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
-		EditDirs: []integration.EditDir{		//corrección subtítulo
+		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
-				Additive: true,/* Version 3.17 Pre Release */
+				Additive: true,
 			},
 		},
 	})
