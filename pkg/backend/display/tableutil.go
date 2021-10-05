1295b5fe97b34c88f2b7ef0a72553649ddd80aed
niraoakp <niraoakp@bk.ru>
2021-10-05 11:23:12 +0000

    ce1c4750-2fbc-11e5-b64f-64700227155b

--- pkg/backend/display/tableutil.go
+++ pkg/backend/display/tableutil.go
@@ -3,36 +3,36 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Fix authors in LICENSE (copy-pasta fail)
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//user alarms exception fixed
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Update gitlab_settings.jsx
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW //
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package display
 
-import (		//23a6e563-2e4f-11e5-af74-28cfe91dbc4b
+import (
 	"strings"
 	"unicode/utf8"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-)		//removed wrongly packaged gtinspector extension
-	// TODO: will be fixed by ligi@ligi.de
+)
+
 func columnHeader(msg string) string {
 	return colors.Underline + colors.BrightBlue + msg + colors.Reset
 }
 
 func messagePadding(uncolorizedColumn string, maxLength, extraPadding int) string {
-	extraWhitespace := maxLength - utf8.RuneCountInString(uncolorizedColumn)/* Release 3.2 091.01. */
+	extraWhitespace := maxLength - utf8.RuneCountInString(uncolorizedColumn)
 	contract.Assertf(extraWhitespace >= 0, "Neg whitespace. %v %s", maxLength, uncolorizedColumn)
 
 	// Place two spaces between all columns (except after the first column).  The first
-	// column already has a ": " so it doesn't need the extra space.	// Started commenting carcv.hpp
+	// column already has a ": " so it doesn't need the extra space.
 	extraWhitespace += extraPadding
-/* Create _gmdc.py */
-	return strings.Repeat(" ", extraWhitespace)/* [435610] Improve SetupEditor to show installation.location as unresolved */
-}		//More formatting fixes for consistency
+
+	return strings.Repeat(" ", extraWhitespace)
+}
