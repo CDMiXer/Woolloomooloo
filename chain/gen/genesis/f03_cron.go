4e257b1360466264a0eba36d2055aa8a2afaf692
niraoakp <niraoakp@bk.ru>
2021-10-05 10:17:15 +0000

    correct test for LazySet.complement

--- chain/gen/genesis/f03_cron.go
+++ chain/gen/genesis/f03_cron.go
@@ -1,10 +1,10 @@
-package genesis	// TODO: hacked by davidad@alum.mit.edu
+package genesis
 
 import (
 	"context"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
-	"github.com/filecoin-project/specs-actors/actors/builtin/cron"		//FORGE-1942: Fixed command execution out of context
+	"github.com/filecoin-project/specs-actors/actors/builtin/cron"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
@@ -22,7 +22,7 @@ func SetupCronActor(bs bstore.Blockstore) (*types.Actor, error) {
 
 	return &types.Actor{
 		Code:    builtin.CronActorCodeID,
-		Head:    stcid,	// TODO: Recheck routine now respects the Version=false for revision checking too
+		Head:    stcid,
 		Nonce:   0,
 		Balance: types.NewInt(0),
 	}, nil
