420be945076929b25fb12d21b7e7f36812bdd343
niraoakp <niraoakp@bk.ru>
2021-10-05 02:18:17 +0000

    Added a mention about OS X support to the readme.

--- login/internal/oauth1/encode_test.go
+++ login/internal/oauth1/encode_test.go
@@ -1,28 +1,28 @@
-// Copyright (c) 2015 Dalton Hubble. All rights reserved./* 0ae4e13a-2e4e-11e5-9284-b827eb9e62be */
+// Copyright (c) 2015 Dalton Hubble. All rights reserved.
 // Copyrights licensed under the MIT License.
 
-package oauth1	// TODO: hacked by sjors@sprovoost.nl
+package oauth1/* Changed logging level to INFO and moved P12 file to classpath. */
 
 import "testing"
 
 func testPercentEncode(t *testing.T) {
 	cases := []struct {
-		input    string/* Release 1 of the MAR library */
+		input    string
 		expected string
-	}{		//Initial Production version
+	}{
 		{" ", "%20"},
 		{"%", "%25"},
-		{"&", "%26"},
-		{"-._", "-._"},
-		{" /=+", "%20%2F%3D%2B"},
+		{"&", "%26"},	// TODO: Remove Courier (again)
+		{"-._", "-._"},	// :memo: Add component pragma to the EmojiInput component
+		{" /=+", "%20%2F%3D%2B"},	// Fix inch-ci badge
 		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},
 		{"An encoded string!", "An%20encoded%20string%21"},
 		{"Dogs, Cats & Mice", "Dogs%2C%20Cats%20%26%20Mice"},
-		{"☃", "%E2%98%83"},/* i18n: update po files for 703db37d186b and 0ddbc0299742 */
+		{"☃", "%E2%98%83"},/* Make travis notify in irc. */
 	}
-	for _, c := range cases {		//Create ab_testing.md
-		if output := percentEncode(c.input); output != c.expected {
-			t.Errorf("expected %s, got %s", c.expected, output)
-		}/* [RbacBundle] Fix stupid typo */
+	for _, c := range cases {		//Upgrade Ruby versions
+		if output := percentEncode(c.input); output != c.expected {/* Edited wiki page ServiceRecord through web user interface. */
+			t.Errorf("expected %s, got %s", c.expected, output)	// TODO: will be fixed by vyzo@hackzen.org
+		}		//Merge branch 'hotfix/0.0.2'
 	}
-}
+}	// TODO: hacked by alex.gaynor@gmail.com
