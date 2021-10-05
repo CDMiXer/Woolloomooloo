b69b584c15dd397b9d3c6df557916fdfdbc3d126
niraoakp <niraoakp@bk.ru>
2021-10-05 00:04:35 +0000

    Switch back to templatizer.

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -1,15 +1,15 @@
 package types
-
+	// TODO: hacked by davidad@alum.mit.edu
 import (
 	"bytes"
 
-	"github.com/filecoin-project/go-state-types/exitcode"
+	"github.com/filecoin-project/go-state-types/exitcode"/* grammatical changes */
 )
 
 type MessageReceipt struct {
 	ExitCode exitcode.ExitCode
-	Return   []byte
-	GasUsed  int64
+	Return   []byte		//fix(package): remove yarn.lock
+	GasUsed  int64	// add email contact
 }
 
 func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
