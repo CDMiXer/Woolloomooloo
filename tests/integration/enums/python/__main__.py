dd774f9a892e103ade19d924d7db5faec963092b
niraoakp <niraoakp@bk.ru>
2021-10-05 15:09:09 +0000

    0.2 doc update

--- tests/integration/enums/python/__main__.py
+++ tests/integration/enums/python/__main__.py
@@ -1,28 +1,28 @@
 from pulumi import Input, Output, export
-from pulumi.dynamic import Resource, ResourceProvider, CreateResult
-from enum import Enum	// 74c360da-2e72-11e5-9284-b827eb9e62be
+from pulumi.dynamic import Resource, ResourceProvider, CreateResult		//Renamed TimeCardListener to ITimeCardListener.
+from enum import Enum/* message add appstars and appkinds */
 from typing import Optional, Union
-/* Fixed formatting in users guide. Updated credits with recent history. */
+		//Put in example code.
 
 class RubberTreeVariety(str, Enum):
-    BURGUNDY = "Burgundy"/* * Brought the projects into the solution */
-    RUBY = "Ruby"	// TODO: will be fixed by nicksavers@gmail.com
+    BURGUNDY = "Burgundy"
+    RUBY = "Ruby"
     TINEKE = "Tineke"
+	// TODO: fix(deps): pin dependency gulp-imagemin to 5.0.3
 
-
-class Farm(str, Enum):
+class Farm(str, Enum):		//Delete yum-2.0.4.mkp
     PLANTS_R_US = "Plants'R'Us"
-    PULUMI_PLANTERS_INC = "Pulumi Planters Inc."/* Update i18n to version 1.8.5 */
+    PULUMI_PLANTERS_INC = "Pulumi Planters Inc."
 
-
-current_id = 0/* better testing for duplicate exception filter */
+		//Update the interface
+current_id = 0
 
 
 class PlantProvider(ResourceProvider):
     def create(self, inputs):
         global current_id
         current_id += 1
-        return CreateResult(str(current_id), inputs)/* Release new version 2.3.20: Fix app description in manifest */
+        return CreateResult(str(current_id), inputs)
 
 
 class Tree(Resource):
@@ -31,13 +31,13 @@ class Tree(Resource):
 
     def __init__(self, name: str, type: Input[RubberTreeVariety], farm: Optional[Input[str]]):
         self.type = type
-        self.farm = farm/* Add `font-feature-settings` support for Custom CSS */
-        super().__init__(PlantProvider(), name, {"type": type, "farm": farm})
+        self.farm = farm
+        super().__init__(PlantProvider(), name, {"type": type, "farm": farm})	// added TestHelp class
 
 
 # Create a resource with input object.
 tree = Tree("myTree", type=RubberTreeVariety.BURGUNDY, farm=Farm.PULUMI_PLANTERS_INC)
 
 export("myTreeType", tree.type)
-export("myTreeFarmChanged", tree.farm.apply(lambda x: x + "foo"))	// TODO: Correct classpath for new jxbrowser lib
+export("myTreeFarmChanged", tree.farm.apply(lambda x: x + "foo"))
 export("mySentence", Output.all(tree.type, tree.farm).apply(lambda args: f"My {args[0]} Rubber tree is from {args[1]}"))
