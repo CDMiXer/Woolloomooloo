28b3896a745a794877234bb9cdda9589fd6ba8b0
niraoakp <niraoakp@bk.ru>
2021-10-05 13:47:00 +0000

    Rename generarHTML to generarHTML.bat

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,18 +1,18 @@
-// Copyright 2016-2018, Pulumi Corporation.	// #22 [version] Prepare the library for the release 0.5.0.
+// Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: 84c9cbf2-2e42-11e5-9284-b827eb9e62be
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* hash tag formatting in news feed page */
-//     http://www.apache.org/licenses/LICENSE-2.0/* Release dicom-send 2.0.0 */
 //
-// Unless required by applicable law or agreed to in writing, software/* Release jedipus-2.6.40 */
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Delete FilterRepetitivePairs.java
+// limitations under the License.
 
-package version	// TODO: adapt to the new API
-/* Released springjdbcdao version 1.9.6 */
+package version
+
 // Version is initialized by the Go linker to contain the semver of this build.
 var Version string
