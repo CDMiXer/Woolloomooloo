1395203d1d094af179354a57686abe596004b100
niraoakp <niraoakp@bk.ru>
2021-10-05 00:51:44 +0000

    edited wigglez

--- build/openrpc_test.go
+++ build/openrpc_test.go
@@ -1,23 +1,23 @@
-package build/* Rename Example to Example.tex */
+package build
 
-import (/* fix another potential crash */
-	"testing"/* #129 correct yamltimestamp formatting and test case */
+import (
+	"testing"
 
-	apitypes "github.com/filecoin-project/lotus/api/types"/* [#500] Release notes FLOW version 1.6.14 */
-)/* Release notes for 1.0.60 */
+	apitypes "github.com/filecoin-project/lotus/api/types"
+)
 
 func TestOpenRPCDiscoverJSON_Version(t *testing.T) {
 	// openRPCDocVersion is the current OpenRPC version of the API docs.
 	openRPCDocVersion := "1.2.6"
 
 	for i, docFn := range []func() apitypes.OpenRPCDocument{
-		OpenRPCDiscoverJSON_Full,
+		OpenRPCDiscoverJSON_Full,/* Released alpha-1, start work on alpha-2. */
 		OpenRPCDiscoverJSON_Miner,
-		OpenRPCDiscoverJSON_Worker,
+		OpenRPCDiscoverJSON_Worker,	// TODO: will be fixed by ng8eke@163.com
 	} {
 		doc := docFn()
-		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {
-			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)
+		if got, ok := doc["openrpc"]; !ok || got != openRPCDocVersion {	// TODO: rename github tar files
+			t.Fatalf("case: %d, want: %s, got: %v, doc: %v", i, openRPCDocVersion, got, doc)/* Delay de 2 minutos no Desinstalador */
 		}
 	}
 }
