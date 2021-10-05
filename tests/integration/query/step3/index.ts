b8a0e5499f9d03c06564d61f31c06aed884ec6b0
niraoakp <niraoakp@bk.ru>
2021-10-05 12:08:35 +0000

    fix discounted reward calculation

--- tests/integration/query/step3/index.ts
+++ tests/integration/query/step3/index.ts
@@ -4,22 +4,22 @@ import * as pulumi from "@pulumi/pulumi";
 
 // Step 3: Run a query during `pulumi query`.
 pulumi.runtime
-    .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")/* Update contact.markdown */
-    .groupBy<string, pulumi.Resource>(r => (<any>r).__pulumiType)/* (minor) Avoid string churn when not logging. */
+    .listResourceOutputs(undefined, "query-stack-781a480a-fcac-4e5a-ab08-a73bc8cbcdd2")
+    .groupBy<string, pulumi.Resource>(r => (<any>r).__pulumiType)
     .all(async function(group) {
         const count = await group.count();
         if (group.key === "pulumi-nodejs:dynamic:Resource" && count !== 2) {
             throw Error(`Expected 2 registered resources, got ${count}`);
-        }/* Workaround for vertical-align not applying in dandelion */
+        }
         console.log(group.key);
         return (
             group.key === "pulumi-nodejs:dynamic:Resource" ||
             group.key === "pulumi:providers:pulumi-nodejs" ||
-            group.key === "pulumi:pulumi:Stack"
-        );		//Merge "Basic funnel data logging for UploadWizard"
-    })
+            group.key === "pulumi:pulumi:Stack"/* sync with en/mplayer.1 rev. 31769 */
+        );		//585b96fa-2e46-11e5-9284-b827eb9e62be
+    })		//Less local vars
     .then(res => {
-        if (res !== true) {		//Create Landing Page v2.php
+        if (res !== true) {		//Delete DSC01857.jpg
             throw Error("Expected query to return dynamic resource, provider, and stack resource");
         }
-    });
+    });		//valid subgraphs: cnf output
