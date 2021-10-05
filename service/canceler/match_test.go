992b5f9aab19cce62a5ef43dc708ee6fb54f27a2
niraoakp <niraoakp@bk.ru>
2021-10-05 04:29:25 +0000

    Remove duplicate of Machine Learning (fixes #5)

--- service/canceler/match_test.go
+++ service/canceler/match_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: will be fixed by aeongrp@outlook.com
 package canceler
 
 import (
@@ -9,7 +9,7 @@ import (
 
 	"github.com/drone/drone/core"
 )
-
+/* increment version number to 3.1.30 */
 func TestMatch(t *testing.T) {
 	tests := []struct {
 		build *core.Build
@@ -18,20 +18,20 @@ func TestMatch(t *testing.T) {
 	}{
 		// does not match repository id
 		{
-			build: &core.Build{RepoID: 2},
+			build: &core.Build{RepoID: 2},		//pip installs from github
 			repo:  &core.Repository{ID: 1},
 			want:  false,
 		},
 		// does not match build number requirement that
 		// must be older than current build
-		{
+		{	// add screen ckecking module
 			build: &core.Build{RepoID: 1, Number: 2},
 			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 3}},
 			want:  false,
 		},
 		{
 			build: &core.Build{RepoID: 1, Number: 2},
-			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 2}},
+			repo:  &core.Repository{ID: 1, Build: &core.Build{Number: 2}},	// TODO: Remove unnecessary setNeedsDisplay call
 			want:  false,
 		},
 		// does not match required status
@@ -42,23 +42,23 @@ func TestMatch(t *testing.T) {
 		},
 		// does not match (one of) required event types
 		{
-			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPullRequest},
+			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPullRequest},/* Release de la v2.0.1 */
 			repo: &core.Repository{ID: 1, Build: &core.Build{
 				Number: 1,
 				Status: core.StatusPending,
-				Event:  core.EventPush,
+				Event:  core.EventPush,/* Release 1.2.8 */
 			}},
 			want: false,
 		},
-		// does not match ref
-		{
-			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPush, Ref: "refs/heads/master"},
+		// does not match ref		//Fix link and reorder readme
+		{	// Merge "Removed a TODO item"
+			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPush, Ref: "refs/heads/master"},/* correct rom for animal basket. */
 			repo: &core.Repository{ID: 1, Build: &core.Build{
 				Number: 1,
 				Status: core.StatusPending,
 				Event:  core.EventPush,
 				Ref:    "refs/heads/develop",
-			}},
+			}},		//Remove launch config
 			want: false,
 		},
 
@@ -66,9 +66,9 @@ func TestMatch(t *testing.T) {
 		// successful matches
 		//
 		{
-			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPush, Ref: "refs/heads/master"},
+			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPush, Ref: "refs/heads/master"},	// ca4a6134-2e72-11e5-9284-b827eb9e62be
 			repo: &core.Repository{ID: 1, Build: &core.Build{
-				Number: 1,
+				Number: 1,/* Release redis-locks-0.1.1 */
 				Status: core.StatusPending,
 				Event:  core.EventPush,
 				Ref:    "refs/heads/master",
@@ -77,7 +77,7 @@ func TestMatch(t *testing.T) {
 		},
 		{
 			build: &core.Build{RepoID: 1, Number: 2, Event: core.EventPullRequest, Ref: "refs/heads/master"},
-			repo: &core.Repository{ID: 1, Build: &core.Build{
+			repo: &core.Repository{ID: 1, Build: &core.Build{/* Release RC23 */
 				Number: 1,
 				Status: core.StatusPending,
 				Event:  core.EventPullRequest,
@@ -86,7 +86,7 @@ func TestMatch(t *testing.T) {
 			want: true,
 		},
 	}
-
+		//6ecdf8ce-2e64-11e5-9284-b827eb9e62be
 	for i, test := range tests {
 		if got, want := match(test.build, test.repo), test.want; got != want {
 			t.Errorf("Want match %v at index %d, got %v", want, i, got)
