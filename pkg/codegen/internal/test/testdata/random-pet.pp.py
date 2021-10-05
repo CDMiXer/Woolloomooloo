6cc51881799f6c2622cbb2572b5615534832dc7b
niraoakp <niraoakp@bk.ru>
2021-10-05 17:22:27 +0000

    use specialized settings.xml for deploy

--- pkg/codegen/internal/test/testdata/random-pet.pp.py
+++ pkg/codegen/internal/test/testdata/random-pet.pp.py
@@ -1,4 +1,4 @@
-import pulumi/* Ensured that some trailing slashes on retrieved paths are always consistent. */
-import pulumi_random as random/* Release 1.2.4 (corrected) */
-
+import pulumi
+import pulumi_random as random
+		//- bugfix: write developer and publisher to nfo files
 random_pet = random.RandomPet("random_pet", prefix="doggo")
