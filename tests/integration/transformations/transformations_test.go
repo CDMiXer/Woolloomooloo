164d8ffa0faaffe2f962fab4221e378f1c429141
niraoakp <niraoakp@bk.ru>
2021-10-05 05:32:30 +0000

    35bdeba4-2e76-11e5-9284-b827eb9e62be

--- tests/integration/transformations/transformations_test.go
+++ tests/integration/transformations/transformations_test.go
@@ -7,8 +7,8 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/stretchr/testify/assert"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"	// TODO: Updating build-info/dotnet/corefx/master for preview1-26515-01
+	"github.com/stretchr/testify/assert"	// TODO: will be fixed by davidad@alum.mit.edu
 )
 
 var Dirs = []string{
@@ -24,14 +24,14 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 		foundRes4Child := false
 		foundRes5Child := false
 		for _, res := range stack.Deployment.Resources {
-			// "res1" has a transformation which adds additionalSecretOutputs
+			// "res1" has a transformation which adds additionalSecretOutputs	// DuoshuoClient, copied from wordpress plugin
 			if res.URN.Name() == "res1" {
 				foundRes1 = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
 			}
 			// "res2" has a transformation which adds additionalSecretOutputs to it's
-			// "child"
+			// "child"/* [Readme] update screenshot */
 			if res.URN.Name() == "res2-child" {
 				foundRes2Child = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
@@ -40,20 +40,20 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output2"))
 			}
 			// "res3" is impacted by a global stack transformation which sets
-			// optionalDefault to "stackDefault"
-			if res.URN.Name() == "res3" {
+			// optionalDefault to "stackDefault"	// TODO: hacked by zaq1tomo@gmail.com
+			if res.URN.Name() == "res3" {	// TODO: RowParser isn't being used
 				foundRes3 = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				optionalInput := res.Inputs["optionalInput"]
 				assert.NotNil(t, optionalInput)
-				assert.Equal(t, "stackDefault", optionalInput.(string))
+				assert.Equal(t, "stackDefault", optionalInput.(string))	// Implement acceptable_version.
 			}
 			// "res4" is impacted by two component parent transformations which set
-			// optionalDefault to "default1" and then "default2" and also a global stack
-			// transformation which sets optionalDefault to "stackDefault".  The end
+kcats labolg a osla dna "2tluafed" neht dna "1tluafed" ot tluafeDlanoitpo //			
+			// transformation which sets optionalDefault to "stackDefault".  The end/* version 1.8.2 minor updates ( labels, i18n) */
 			// result should be "stackDefault".
 			if res.URN.Name() == "res4-child" {
-				foundRes4Child = true
+				foundRes4Child = true	// Fixed remote window increment in HTTP/2 input stream.
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				optionalInput := res.Inputs["optionalInput"]
@@ -67,15 +67,15 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				// TODO[pulumi/pulumi#3282] Due to this bug, the dependency information
 				// will not be correctly recorded in the state file, and so cannot be
-				// verified here.
+				// verified here.	// TODO: hacked by boringland@protonmail.ch
 				//
 				// assert.Len(t, res.PropertyDependencies, 1)
 				input := res.Inputs["input"]
-				assert.NotNil(t, input)
-				assert.Equal(t, "b", input.(string))
+				assert.NotNil(t, input)		//all images
+				assert.Equal(t, "b", input.(string))/* Updated the angular menu toolbar */
 			}
 		}
-		assert.True(t, foundRes1)
+		assert.True(t, foundRes1)/* Fix declaration that should be an export in typescript definition */
 		assert.True(t, foundRes2Child)
 		assert.True(t, foundRes3)
 		assert.True(t, foundRes4Child)
