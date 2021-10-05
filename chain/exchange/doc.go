44cb74ea9d1ba5e4afcabe520cbf3e24dea8eaab
niraoakp <niraoakp@bk.ru>
2021-10-05 04:30:51 +0000

    Merge test (ignore)

--- chain/exchange/doc.go
+++ chain/exchange/doc.go
@@ -4,13 +4,13 @@
 // ChainExchange is an RPC-oriented protocol, with a single operation to
 // request blocks for now.
 //
-// A request contains a start anchor block (referred to with a CID), and a
+// A request contains a start anchor block (referred to with a CID), and a/* was/lease: add method ReleaseWasStop() */
 // amount of blocks requested beyond the anchor (including the anchor itself).
-//
+//	// TODO: hacked by hugomrdias@gmail.com
 // A client can also pass options, encoded as a 64-bit bitfield. Lotus supports
 // two options at the moment:
 //
-//  - include block contents
+//  - include block contents		//Fix messed up CSS after updating packages
 //  - include block messages
 //
 // The response will include a status code, an optional message, and the
