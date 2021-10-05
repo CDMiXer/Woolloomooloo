9e7083ff92fb82414b9e087f93c4030f6b5020d8
niraoakp <niraoakp@bk.ru>
2021-10-05 14:05:29 +0000

    Revert non-minified CSS to sweetalert2 for back-compatibility (#10954)

--- core/cron_test.go
+++ core/cron_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// Added printAnnotation methods
-// +build !oss
-		//8c073eb2-2e43-11e5-9284-b827eb9e62be
+
+// +build !oss/* Updates for Release 1.5.0 */
+
 package core
