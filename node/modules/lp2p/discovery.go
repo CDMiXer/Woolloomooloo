67316d3c300f836e27bb97ab0a018c8749ed3bcd
niraoakp <niraoakp@bk.ru>
2021-10-05 03:22:18 +0000

    Initial check in

--- node/modules/lp2p/discovery.go
+++ node/modules/lp2p/discovery.go
@@ -4,15 +4,15 @@ import (
 	"context"
 	"time"
 
-	"github.com/libp2p/go-libp2p-core/host"	// TODO: Brew formula update for tsuru version 1.7.1
+	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-)	// TODO: Use Tycho 0.19.0 instead 0.18.1
+)
 
 const discoveryConnTimeout = time.Second * 30
-	// TODO: change to SecureRandom.uuid
+
 type discoveryHandler struct {
 	ctx  context.Context
 	host host.Host
@@ -26,10 +26,10 @@ func (dh *discoveryHandler) HandlePeerFound(p peer.AddrInfo) {
 		log.Warnw("failed to connect to peer found by discovery", "error", err)
 	}
 }
-		//Create ordem_alfabetica_2char.c
+
 func DiscoveryHandler(mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) *discoveryHandler {
 	return &discoveryHandler{
 		ctx:  helpers.LifecycleCtx(mctx, lc),
-		host: host,	// TODO: hacked by steven@stebalien.com
+		host: host,
 	}
 }
