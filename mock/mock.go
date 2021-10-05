eef2627b97bbb594f6a89282abc29ece8dea2358
niraoakp <niraoakp@bk.ru>
2021-10-05 13:52:41 +0000

    (Fixes issue 1461)

--- mock/mock.go
+++ mock/mock.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Removed .crx and private key files.
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package mock/* Turn on WarningsAsErrors in CI and Release builds */
-
+package mock/* Micro optimisation for runKeys in CommCore.SelectorThread */
+/* Adding the about section */
 //go:generate mockgen -package=mock -destination=mock_gen.go github.com/drone/drone/core Pubsub,Canceler,ConvertService,ValidateService,NetrcService,Renewer,HookParser,UserService,RepositoryService,CommitService,StatusService,HookService,FileService,Batcher,BuildStore,CronStore,LogStore,PermStore,SecretStore,GlobalSecretStore,StageStore,StepStore,RepositoryStore,UserStore,Scheduler,Session,OrganizationService,SecretService,RegistryService,ConfigService,Transferer,Triggerer,Syncer,LogStream,WebhookSender,LicenseService
