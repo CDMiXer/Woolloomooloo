458db18d1a8461fc58a654412bfe4f942c093981
niraoakp <niraoakp@bk.ru>
2021-10-05 02:55:17 +0000

    Merge "Dont disable services that are not enabled"

--- core/config.go
+++ core/config.go
@@ -1,36 +1,36 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//re #46: Remove HTTP auth 
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//Fix Development.md (.dev and npm version)
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
+//	// TL1-created String operation
+//      http://www.apache.org/licenses/LICENSE-2.0		//Adding get*History API to build
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package core
-
+	// TODO: b34d0696-2e5c-11e5-9284-b827eb9e62be
+package core	// TODO: Update version of maven invoker plugin
+	// TODO: hacked by arajasek94@gmail.com
 import "context"
 
 type (
-	// Config represents a pipeline config file.
+	// Config represents a pipeline config file./* It's Laing */
 	Config struct {
 		Data string `json:"data"`
 		Kind string `json:"kind"`
-	}
+	}/* dry-run parameter */
 
-	// ConfigArgs represents a request for the pipeline
+	// ConfigArgs represents a request for the pipeline		//Update socpro.css
 	// configuration file (e.g. .drone.yml)
 	ConfigArgs struct {
 		User   *User       `json:"-"`
 		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
-		Config *Config     `json:"config,omitempty"`
-	}
+		Config *Config     `json:"config,omitempty"`	// TODO: will be fixed by davidad@alum.mit.edu
+	}		//For post-forms I switch to named urls.
 
 	// ConfigService provides pipeline configuration from an
 	// external service.
