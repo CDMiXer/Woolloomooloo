098e7f8de42308351c161e29557113868e965b24
niraoakp <niraoakp@bk.ru>
2021-10-05 02:16:57 +0000

    Refacotring - Local repository fixes

--- service/linker/linker_test.go
+++ service/linker/linker_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Added Release Notes link to README.md */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by nick@perfectabstractions.com
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: patched vold no longer required
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Release 1.0.0.80 QCACLD WLAN Driver" */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package linker
