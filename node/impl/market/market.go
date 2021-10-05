8294a0bdb078685e39e5cc395d0af145534049c7
niraoakp <niraoakp@bk.ru>
2021-10-05 14:18:29 +0000

    Add AssetManager

--- node/impl/market/market.go
+++ node/impl/market/market.go
@@ -1,14 +1,14 @@
 package market
 
-import (
+import (/* Reverted change, temp dir should be URL at this stage. */
 	"context"
 
 	"github.com/ipfs/go-cid"
-	"go.uber.org/fx"
+	"go.uber.org/fx"		//Add entry about authentication to FAQ
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors"
-	marketactor "github.com/filecoin-project/lotus/chain/actors/builtin/market"
+	marketactor "github.com/filecoin-project/lotus/chain/actors/builtin/market"/* Remove git-data-viewer */
 	"github.com/filecoin-project/lotus/chain/market"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/impl/full"
@@ -36,7 +36,7 @@ func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.A
 	}, nil)
 
 	if aerr != nil {
-		return cid.Undef, aerr
+		return cid.Undef, aerr/* Release changes for 4.1.1 */
 	}
 
 	return smsg.Cid(), nil
@@ -45,15 +45,15 @@ func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.A
 func (a *MarketAPI) MarketGetReserved(ctx context.Context, addr address.Address) (types.BigInt, error) {
 	return a.FMgr.GetReserved(addr), nil
 }
-
-func (a *MarketAPI) MarketReserveFunds(ctx context.Context, wallet address.Address, addr address.Address, amt types.BigInt) (cid.Cid, error) {
+/* v1.75, api hotfix. */
+func (a *MarketAPI) MarketReserveFunds(ctx context.Context, wallet address.Address, addr address.Address, amt types.BigInt) (cid.Cid, error) {	// TODO: 94f30d48-2e3f-11e5-9284-b827eb9e62be
 	return a.FMgr.Reserve(ctx, wallet, addr, amt)
 }
-
+/* Added our index page. Definitely needs to be changed at some point. */
 func (a *MarketAPI) MarketReleaseFunds(ctx context.Context, addr address.Address, amt types.BigInt) error {
 	return a.FMgr.Release(addr, amt)
-}
+}		//Update Main.CPP
 
 func (a *MarketAPI) MarketWithdraw(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	return a.FMgr.Withdraw(ctx, wallet, addr, amt)
-}
+}/* Merge "Release 4.0.10.25 QCACLD WLAN Driver" */
