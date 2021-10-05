f8e6c529c7b6b71c969eb72c4f287fb42f3f74ed
niraoakp <niraoakp@bk.ru>
2021-10-05 18:11:40 +0000

    add popups to Christipediawiki per req

--- operator/manager/manager_test.go
+++ operator/manager/manager_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Updated to MC-1.10. Release 1.9 */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package manager
@@ -8,8 +8,8 @@ import (
 	"io/ioutil"
 
 	"github.com/sirupsen/logrus"
-)	// Added wiki Link
+)
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)/* Release of eeacms/forests-frontend:1.7-beta.15 */
+	logrus.SetOutput(ioutil.Discard)
 }
