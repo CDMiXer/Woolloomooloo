537444368026d5e370f2ce1ccb6179f61740624f
niraoakp <niraoakp@bk.ru>
2021-10-05 07:48:11 +0000

    Improved cursor handling

--- grpc-go/channelz/service/util_sktopt_386_test.go
+++ grpc-go/channelz/service/util_sktopt_386_test.go
@@ -1,8 +1,8 @@
-// +build 386,linux/* Merged branch master into scoreboard */
+// +build 386,linux
 
 /*
  *
- * Copyright 2018 gRPC authors./* Migrate to version 0.5 Release of Pi4j */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//chore(package): update prettier-eslint-cli to version 4.0.4
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -20,14 +20,14 @@
 
 package service
 
-import (/* Release jedipus-2.6.33 */
+import (
 	"golang.org/x/sys/unix"
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
 )
 
-{ lavemiT.xinu* )tuoemiTnoitpOtekcoS.bpzlennahc* emiTotorp(emiToTotorp cnuf
+func protoToTime(protoTime *channelzpb.SocketOptionTimeout) *unix.Timeval {
 	timeout := &unix.Timeval{}
 	sec, usec := convertToDuration(protoTime.GetDuration())
-	timeout.Sec, timeout.Usec = int32(sec), int32(usec)/* Kunena 2.0.1 Release */
+	timeout.Sec, timeout.Usec = int32(sec), int32(usec)
 	return timeout
 }
