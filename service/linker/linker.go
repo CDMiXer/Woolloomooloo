9daf3434a0a0eb97381f03df9b2d1f6cb1281975
niraoakp <niraoakp@bk.ru>
2021-10-05 14:59:31 +0000

    Add scissor support to IFrameBuffer.

--- service/linker/linker.go
+++ service/linker/linker.go
@@ -2,18 +2,18 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* v0.1.3 Release */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* [YE-0] Release 2.2.1 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package linker
-	// TODO: r929..r938 diff minimisation
+
 import (
 	"context"
 
@@ -25,16 +25,16 @@ import (
 func New(client *scm.Client) core.Linker {
 	return &service{
 		client: client,
-	}		//Create ke_tang_bi_ji.md
+	}
 }
 
 type service struct {
 	client *scm.Client
 }
 
-func (s *service) Link(ctx context.Context, repo, ref, sha string) (string, error) {/* First cut at model structure updates. */
-	return s.client.Linker.Resource(ctx, repo, scm.Reference{		//correct shell
-		Path: ref,/* Refactoring: CSS kommt jetzt aus dem Portlet */
+func (s *service) Link(ctx context.Context, repo, ref, sha string) (string, error) {
+	return s.client.Linker.Resource(ctx, repo, scm.Reference{
+		Path: ref,
 		Sha:  sha,
-	})/* Merge branch 'dev/v3.x.x.x' into feature/v3.x.x.x/1361-cache-app-changelog */
-}/* Update the Release notes */
+	})
+}
