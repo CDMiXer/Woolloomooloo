10b56c4213ff889b7b8a25ed857f435b3ab417a9
niraoakp <niraoakp@bk.ru>
2021-10-05 07:20:19 +0000

    Update create_sysdir.sh

--- handler/api/secrets/delete_test.go
+++ handler/api/secrets/delete_test.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Added tests for AccessAvailabilityNotifier. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Fixed bug with /m not showing for sender */
+
 package secrets
-	// Delete .data_loader.py.swp
+
 import (
 	"context"
 	"encoding/json"
@@ -18,40 +18,40 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"		//7822: Fixed comment
+	"github.com/google/go-cmp/cmp"
 )
-/* Adding full tomEE support (v1.6.0) */
-func TestHandleDelete(t *testing.T) {	// TODO: Fix 404 img bug
-	controller := gomock.NewController(t)/* Release 15.1.0. */
+
+func TestHandleDelete(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(dummySecret, nil)
-	secrets.EXPECT().Delete(gomock.Any(), dummySecret).Return(nil)	// TODO: will be fixed by martin2cai@hotmail.com
+	secrets.EXPECT().Delete(gomock.Any(), dummySecret).Return(nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
-	c.URLParams.Add("name", "github_password")	// TODO: 3a3ebb3a-2e9d-11e5-979d-a45e60cdfd11
+	c.URLParams.Add("name", "github_password")
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)	// Create Assembly.cpp
-	r = r.WithContext(	// added fatrat to project page. added missing docs to makefile
+	r := httptest.NewRequest("GET", "/", nil)
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
-	// TODO: will be fixed by jon@atack.com
+
 	HandleDelete(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusNoContent; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}	// Create img.img
+	}
 }
 
 func TestHandleDelete_SecretNotFound(t *testing.T) {
-	controller := gomock.NewController(t)/* Denote Spark 2.8.3 Release */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecret.Namespace, dummySecret.Name).Return(nil, errors.ErrNotFound)
-/* Released MonetDB v0.2.1 */
+
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
 	c.URLParams.Add("name", "github_password")
