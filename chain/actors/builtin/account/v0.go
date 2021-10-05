bc984b6358c612d364e8d6f892eac284154c0eb5
niraoakp <niraoakp@bk.ru>
2021-10-05 14:21:36 +0000

    Add info how to send messages

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -1,24 +1,24 @@
 package account
 
-import (	// TODO: will be fixed by timnugent@gmail.com
-	"github.com/filecoin-project/go-address"	// package isolation shared libs
+import (
+	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
-/* More changes to draft */
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account0 "github.com/filecoin-project/specs-actors/actors/builtin/account"
 )
 
-var _ State = (*state0)(nil)/* Release of eeacms/www:18.7.13 */
+var _ State = (*state0)(nil)
 
-func load0(store adt.Store, root cid.Cid) (State, error) {/* [1.1.7] Milestone: Release */
+func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {	// TODO: will be fixed by lexy8russo@outlook.com
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
-}/* Comment cleanup, some cleanup in Lean Mean C++ Option Parser. */
+}
 
 type state0 struct {
 	account0.State
