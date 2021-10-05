c172bb7eb7d960e063fef07058b6a97d9a3c5d4f
niraoakp <niraoakp@bk.ru>
2021-10-05 18:21:57 +0000

    add link to tracker

--- store/store.go
+++ store/store.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone IO, Inc./* Added status bar color for Apple */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");	// added create author table ddl
+// you may not use this file except in compliance with the License./* Merge "Reword the Releases and Version support section of the docs" */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Released 0.0.17 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
