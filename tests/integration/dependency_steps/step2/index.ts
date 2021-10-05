1b1a58ebf53fc6d1c3f2065a746de7c2ed482aee
niraoakp <niraoakp@bk.ru>
2021-10-05 13:12:38 +0000

    Merge "msm: smd_tty: fix section mismatch"

--- tests/integration/dependency_steps/step2/index.ts
+++ tests/integration/dependency_steps/step2/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";		//chore(deps): update dependency @types/recompose to v0.24.7
+import { Resource } from "./resource";/* Create Compiled-Releases.md */
 
-// Step 2: Reverse the dependency graph./* Release 0.95.162 */
+// Step 2: Reverse the dependency graph.
 // Checkpoint dependency graph: b -> a
 const b = new Resource("b", { state: 2 })
 const a = new Resource("a", { state: 1, resource: b })
