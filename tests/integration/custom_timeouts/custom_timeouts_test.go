409613a76a4a17e61020a241679f24a8c1f4dcf4
niraoakp <niraoakp@bk.ru>
2021-10-05 01:34:58 +0000

    modificação do cadastro,login

--- tests/integration/custom_timeouts/custom_timeouts_test.go
+++ tests/integration/custom_timeouts/custom_timeouts_test.go
@@ -1,33 +1,33 @@
-// +build python all
-/* default and thread-based tracer factory (parallel VIL execution) */
+// +build python all		//TASK: add migration to adjust other packages to Neos.Media namespace change
+
 package ints
-	// TODO: will be fixed by timnugent@gmail.com
-import (/* Delete object_script.incendie.Release */
-	"path/filepath"
+
+import (
+	"path/filepath"/* Rename frontend StatisticalReleaseAnnouncement -> StatisticsAnnouncement */
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)/* Update pwd.c */
-	// a38ecc9c-2e45-11e5-9284-b827eb9e62be
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Merge "Release 4.0.10.14  QCACLD WLAN Driver" */
+)
+
 func TestCustomTimeouts(t *testing.T) {
 	opts := &integration.ProgramTestOptions{
-		Dir: filepath.Join(".", "python", "success"),	// TODO: Delete GestaoFestaApplicationTests.class
+		Dir: filepath.Join(".", "python", "success"),
+		Dependencies: []string{	// Fixes #23.
+			filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
+		},
+		Quick:      true,	// TODO: will be fixed by ligi@ligi.de
+		NoParallel: true,
+	}
+	integration.ProgramTest(t, opts)
+
+	opts = &integration.ProgramTestOptions{
+		Dir: filepath.Join(".", "python", "failure"),
 		Dependencies: []string{
 			filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 		},
-		Quick:      true,
-		NoParallel: true,
-	}		//Ignoring dns_nameserver
-	integration.ProgramTest(t, opts)
-	// TODO: Merge "Revert "Enable pip caching""
-	opts = &integration.ProgramTestOptions{
-		Dir: filepath.Join(".", "python", "failure"),		//Replace degree and mil symbols with their Unicode values
-		Dependencies: []string{/* http_client: call ReleaseSocket() explicitly in ResponseFinished() */
-			filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
-		},
-		Quick:         true,	// Merge "PrologTestCase: Convert to use Google Truth"
+		Quick:         true,
 		NoParallel:    true,
 		ExpectFailure: true,
-	}/* Finish purging Python's datetime and replacing it with mx.DateTime */
-	integration.ProgramTest(t, opts)
+	}
+	integration.ProgramTest(t, opts)/* Create xy6.lua */
 }
