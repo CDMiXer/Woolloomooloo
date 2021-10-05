3bb98d4871bef7ad92057ab3bec3b8fd65225a5e
niraoakp <niraoakp@bk.ru>
2021-10-05 10:38:46 +0000

    Reduce unnecessary runs of the loop

--- operator/manager/util_test.go
+++ operator/manager/util_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* UI - Genre Pill Spacing */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License		//Add reference to setup-pgp.md in index page
 // that can be found in the LICENSE file.
 
 package manager
