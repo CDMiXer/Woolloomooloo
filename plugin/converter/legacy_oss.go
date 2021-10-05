b4820aa80fc4a30db549869810b9d6c469bd924b
niraoakp <niraoakp@bk.ru>
2021-10-05 00:08:59 +0000

    added "lateral bias" to S9KeyMotion

--- plugin/converter/legacy_oss.go
+++ plugin/converter/legacy_oss.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Copyright 2019 Drone IO, Inc./* Merge "msm: vidc: set ctrl to request sequence header for encoder" */
+//	// TODO: will be fixed by fkautz@pseudocode.cc
+// Licensed under the Apache License, Version 2.0 (the "License");		//moved all direct session scope access to use the sessionContext service
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,14 +10,14 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* rambles about sockets */
 
 // +build oss
 
 package converter
 
 import (
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* #48 - Release version 2.0.0.M1. */
 )
 
 // Legacy returns a conversion service that converts the
