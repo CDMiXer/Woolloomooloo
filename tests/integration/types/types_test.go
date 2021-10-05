bb86b6e82dd8dfe8da5df4b49cece43cbb540c6c
niraoakp <niraoakp@bk.ru>
2021-10-05 20:03:17 +0000

    Create richel.html

--- tests/integration/types/types_test.go
+++ tests/integration/types/types_test.go
@@ -1,32 +1,32 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-// +build python all		//use closure for status ws
+// +build python all
 
 package ints
 
 import (
-	"fmt"/* Fix Missing ) */
+	"fmt"
 	"path/filepath"
-	"testing"/* Merge "Allow deleting folders and from all apps" into jb-ub-now-indigo-rose */
-	// Hotifx + CS fixes
+	"testing"
+
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/stretchr/testify/assert"
 )
-	// TODO: hacked by steven@stebalien.com
+
 func TestPythonTypes(t *testing.T) {
 	for _, dir := range []string{"simple", "declared"} {
 		d := filepath.Join("python", dir)
-{ )T.gnitset* t(cnuf ,d(nuR.t		
+		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-,d :riD				
+				Dir: d,
 				Dependencies: []string{
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
-				},/* Added codedoc and changed the AI loader back to non-debug mode */
+				},
 				ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-					for _, res := range []string{"", "2", "3", "4"} {	// Fixed calls to finish without option argument
+					for _, res := range []string{"", "2", "3", "4"} {
 						assert.Equal(t, "hello", stack.Outputs[fmt.Sprintf("res%s_first_value", res)])
-						assert.Equal(t, 42.0, stack.Outputs[fmt.Sprintf("res%s_second_value", res)])/* Released v1.2.3 */
+						assert.Equal(t, 42.0, stack.Outputs[fmt.Sprintf("res%s_second_value", res)])
 					}
-				},		//Merge "Make HTMLFileCache also work when gzip is not enabled server-side."
+				},
 				UseAutomaticVirtualEnv: true,
 			})
 		})
