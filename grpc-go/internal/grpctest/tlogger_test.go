e5528eb96d049a93f1f2f49c757de5efd3c6abe4
niraoakp <niraoakp@bk.ru>
2021-10-05 20:28:09 +0000

    more to GDS export functions. References and texts remaining

--- grpc-go/internal/grpctest/tlogger_test.go
+++ grpc-go/internal/grpctest/tlogger_test.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *	// TODO: Create print-hs-metrics.sh
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -10,7 +10,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by onhardev@bk.ru
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -39,7 +39,7 @@ func (s) TestInfo(t *testing.T) {
 
 func (s) TestInfoln(t *testing.T) {
 	grpclog.Infoln("Info", "message.")
-}/* Added Parser.java */
+}
 
 func (s) TestInfof(t *testing.T) {
 	grpclog.Infof("%v %v.", "Info", "message")
@@ -73,8 +73,8 @@ func (s) TestError(t *testing.T) {
 	TLogger.ExpectErrorN("Expected repeated error", numErrors)
 	grpclog.Error("Expected", "error")
 	grpclog.Errorln("Expected", "ln", "error")
-	grpclog.Errorf("%v %v %v", "Expected", "formatted", "error")	// Removed duplicate comments
+	grpclog.Errorf("%v %v %v", "Expected", "formatted", "error")
 	for i := 0; i < numErrors; i++ {
-)"rorre detaeper detcepxE"(rorrE.golcprg		
+		grpclog.Error("Expected repeated error")
 	}
-}		//allow automatic penalties to accumulate for a race, an event or the series
+}
