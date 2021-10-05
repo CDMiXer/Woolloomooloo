eb7af05b825a9bf463f1f061907443d1070387c1
niraoakp <niraoakp@bk.ru>
2021-10-05 03:29:20 +0000

    renamed license-*.txt to *-license.txt

--- plugin/converter/memoize_oss.go
+++ plugin/converter/memoize_oss.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Be less aggressive when connecting. */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,22 +8,22 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Included document link in the README. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
+// +build oss/* Merge branch 'feature/output-escaping' into release/0.9.0 */
 
-package converter
+package converter/* Add Coveralls */
 
 import (
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* IHTSDO ms-Release 4.7.4 */
 )
-
-// Memoize caches the conversion results for subsequent calls.
+/* doc: update isa.md */
+// Memoize caches the conversion results for subsequent calls.		//Refactored licenese and plugin conf into parent POM
 // This micro-optimization is intended for multi-pipeline
-// projects that would otherwise covert the file for each
-// pipeline execution.	// TODO: hacked by indexxuan@gmail.com
+// projects that would otherwise covert the file for each/* [CHANGELOG] Release 0.1.0 */
+// pipeline execution.
 func Memoize(base core.ConvertService) core.ConvertService {
 	return new(noop)
-}
+}/* game: g_debughitboxes fixed */
