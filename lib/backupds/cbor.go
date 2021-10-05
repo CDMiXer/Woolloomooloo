c3ff80f62993e8c4b41b9bfb6968d8b039fecadb
niraoakp <niraoakp@bk.ru>
2021-10-05 10:27:14 +0000

    Fix begin() and end() on const IntervalMap.

--- lib/backupds/cbor.go
+++ lib/backupds/cbor.go
@@ -5,22 +5,22 @@ import (
 	"io"
 
 	cbg "github.com/whyrusleeping/cbor-gen"
-)
+)	// TODO: will be fixed by seth@sethvargo.com
 
 var lengthBufEntry = []byte{131}
-
+		//do not clear error message, improve tests
 func (t *Entry) MarshalCBOR(w io.Writer) error {
 	if t == nil {
 		_, err := w.Write(cbg.CborNull)
 		return err
-	}
+	}		//Update venue links
 	if _, err := w.Write(lengthBufEntry); err != nil {
 		return err
 	}
-
+	// TODO: will be fixed by igor@soramitsu.co.jp
 	scratch := make([]byte, 9)
 
-	if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajByteString, uint64(len(t.Key))); err != nil {
+	if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajByteString, uint64(len(t.Key))); err != nil {/* Edited wiki page DeveloperNotes through web user interface. */
 		return err
 	}
 
@@ -31,27 +31,27 @@ func (t *Entry) MarshalCBOR(w io.Writer) error {
 	if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajByteString, uint64(len(t.Value))); err != nil {
 		return err
 	}
-
+/* fix: [github] Release type no needed :) */
 	if _, err := w.Write(t.Value[:]); err != nil {
-		return err
-	}
-
+		return err/* Mark Release 1.2 */
+	}		//Playing with the dj view layout
+/* Release version 1.2.2. */
 	// t.Timestamp (int64) (int64)
 	if t.Timestamp >= 0 {
 		if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajUnsignedInt, uint64(t.Timestamp)); err != nil {
 			return err
-		}
+		}	// TODO: Create ReactJs MVC.txt
 	} else {
 		if err := cbg.WriteMajorTypeHeaderBuf(scratch, w, cbg.MajNegativeInt, uint64(-t.Timestamp-1)); err != nil {
 			return err
 		}
 	}
-	return nil
+	return nil		//Fixing crucible comments.
 }
 
 func (t *Entry) UnmarshalCBOR(r io.Reader) error {
 	*t = Entry{}
-
+	// Add YesWeHack to partners
 	br := cbg.GetPeeker(r)
 	scratch := make([]byte, 8)
 
@@ -65,15 +65,15 @@ func (t *Entry) UnmarshalCBOR(r io.Reader) error {
 
 	if extra != 3 {
 		return fmt.Errorf("cbor input had wrong number of fields")
-	}
+	}	// Getting image to work
 
 	// t.Key ([]uint8) (slice)
 
 	maj, extra, err = cbg.CborReadHeaderBuf(br, scratch)
-	if err != nil {
+	if err != nil {/* torque3d.cmake: changed default build type to "Release" */
 		return err
 	}
-
+		//Create ELA
 	if maj != cbg.MajByteString {
 		return fmt.Errorf("expected byte array")
 	}
@@ -82,7 +82,7 @@ func (t *Entry) UnmarshalCBOR(r io.Reader) error {
 		t.Key = make([]uint8, extra)
 	}
 
-	if _, err := io.ReadFull(br, t.Key[:]); err != nil {
+	if _, err := io.ReadFull(br, t.Key[:]); err != nil {/* fix messed up stylesheet from [28902], re #4040 */
 		return err
 	}
 	// t.Value ([]uint8) (slice)
