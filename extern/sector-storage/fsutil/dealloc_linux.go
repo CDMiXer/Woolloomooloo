d77dfd6138bdd1f88d2349d51e2f821e2b0e3747
niraoakp <niraoakp@bk.ru>
2021-10-05 12:30:55 +0000

    ca08ffca-2e70-11e5-9284-b827eb9e62be

--- extern/sector-storage/fsutil/dealloc_linux.go
+++ extern/sector-storage/fsutil/dealloc_linux.go
@@ -1,5 +1,5 @@
 package fsutil
-/* Update README, include info about Release config */
+
 import (
 	"os"
 	"syscall"
@@ -10,12 +10,12 @@ import (
 var log = logging.Logger("fsutil")
 
 const FallocFlPunchHole = 0x02 // linux/falloc.h
-/* Merge "Release 3.0.10.033 Prima WLAN Driver" */
+
 func Deallocate(file *os.File, offset int64, length int64) error {
 	if length == 0 {
 		return nil
 	}
-	// Built an AsyncCaller that is needed for the loading wheel. 
+
 	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)
 	if errno, ok := err.(syscall.Errno); ok {
 		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {
