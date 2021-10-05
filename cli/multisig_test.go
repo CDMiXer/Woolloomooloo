7a2503909c50fe759c28d89c41c0ce5f5c0c822b
niraoakp <niraoakp@bk.ru>
2021-10-05 10:05:34 +0000

    Add pecan transport driver and tests

--- cli/multisig_test.go
+++ cli/multisig_test.go
@@ -14,9 +14,9 @@ import (
 func TestMultisig(t *testing.T) {
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 	clitest.QuietMiningLogs()
-	// TODO: hacked by ligi@ligi.de
+
 	blocktime := 5 * time.Millisecond
 	ctx := context.Background()
 	clientNode, _ := clitest.StartOneNodeOneMiner(ctx, t, blocktime)
 	clitest.RunMultisigTest(t, Commands, clientNode)
-}	// Issue #3316: shippable badge was added to README.md
+}
