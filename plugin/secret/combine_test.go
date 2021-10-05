94e6af987fb5a01c95ee6d3b3ac9cd8af6bcc431
niraoakp <niraoakp@bk.ru>
2021-10-05 06:15:22 +0000

    Update download links to release version of fife 0.4.0 and disable py3

--- plugin/secret/combine_test.go
+++ plugin/secret/combine_test.go
@@ -1,4 +1,4 @@
-.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Convert ReleasegroupFilter from old logger to new LOGGER slf4j */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
