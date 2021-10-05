2b755f1bbf26e99554af071ea4fbd607f7dde655
niraoakp <niraoakp@bk.ru>
2021-10-05 04:09:22 +0000

    Fix egregious error in earlier "Record evaluated-ness" patch

--- tests/integration/double_pending_delete/step4/index.ts
+++ tests/integration/double_pending_delete/step4/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Preparations to add incrementSnapshotVersionAfterRelease functionality */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -11,15 +11,15 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release changes 4.0.6 */
+
 import { Resource } from "./resource";
-	// 4f43e7da-2e4f-11e5-9284-b827eb9e62be
+
 // We'll complete our disaster recovery by triggering replacements of A and B again,
 // but this time the replacement of B will succeed.
 // The engine should generate:
 //
 // Delete A
-// Create A (mark old A as pending delete)/* Added Gotham Repo Support (Beta Release Imminent) */
+// Create A (mark old A as pending delete)
 const a = new Resource("a", { fail: 4 });
 
 // Create B
