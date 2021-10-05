4378dd613f15283a45461b0fd18d316a4c4351e7
niraoakp <niraoakp@bk.ru>
2021-10-05 06:06:58 +0000

    Create .txt File Header and Sensor Conversions

--- tests/integration/aliases/nodejs/rename/step1/index.ts
+++ tests/integration/aliases/nodejs/rename/step1/index.ts
@@ -5,8 +5,8 @@ import * as pulumi from "@pulumi/pulumi";
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }
-}
+    }/* Update plansza.h */
+}		//Create Login Acitivity
 
 // Scenario #1 - rename a resource
 const res1 = new Resource("res1");
