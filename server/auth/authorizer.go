71149f433a1da57d1786adbdb26219b61578e06e
niraoakp <niraoakp@bk.ru>
2021-10-05 02:26:53 +0000

    Added comment on plugin usage

--- server/auth/authorizer.go
+++ server/auth/authorizer.go
@@ -4,9 +4,9 @@ import (
 	"context"
 
 	authUtil "github.com/argoproj/argo/util/auth"
-)
+)/* Delete servers */
 
-func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {
+func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {/* Release Notes: 3.3 updates */
 	kubeClientset := GetKubeClient(ctx)
 	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)
 	if err != nil {
