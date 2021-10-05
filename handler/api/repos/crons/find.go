dd759ccb836e45e5b4b9311ac6564c83fef8740c
niraoakp <niraoakp@bk.ru>
2021-10-05 13:05:45 +0000

    [README] More fix

--- handler/api/repos/crons/find.go
+++ handler/api/repos/crons/find.go
@@ -1,42 +1,42 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Merge "Make DBReadOnlyError extend DBExpectedError"
-
+// that can be found in the LICENSE file.
+	// Create MajorityElement.java
 // +build !oss
 
-package crons
-
-import (		//Two projects, one for the UI and one for the tests.
+package crons/* Merge "msm_fb: Set timeline threshold for command mode to 2" */
+	// @virtual is deprecated
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* release v0.5.6 */
-
+	"github.com/drone/drone/handler/api/render"
+	// &quot; -> " in license text.
 	"github.com/go-chi/chi"
 )
 
-// HandleFind returns an http.HandlerFunc that writes json-encoded
+// HandleFind returns an http.HandlerFunc that writes json-encoded		//ar71xx: remove some bogus kernel config overrides
 // cronjob details to the the response body.
-func HandleFind(
-	repos core.RepositoryStore,/* Exit the init script on errors. */
+func HandleFind(	// TODO: will be fixed by alan.shaw@protocol.ai
+	repos core.RepositoryStore,/* Added non existing file test */
 	crons core.CronStore,
-) http.HandlerFunc {		//update INSTALL instruction for windows/MSVC2003
+) http.HandlerFunc {/* X3DFaceSelection is now derived from X3DBaseNode. */
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (	// TODO: cd76d1e0-2e5d-11e5-9284-b827eb9e62be
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			cron      = chi.URLParam(r, "cron")
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)		//Removed deprecated EnrolmentModel enum
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
 		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
-		if err != nil {/* Update login.py */
+		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-		render.JSON(w, cronjob, 200)	// TODO: hacked by zaq1tomo@gmail.com
+		render.JSON(w, cronjob, 200)
 	}
-}
+}	// TODO: hacked by davidad@alum.mit.edu
