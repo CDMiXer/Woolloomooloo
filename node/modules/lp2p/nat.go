2a1f9520224b1078d3359afc9473c442b15d4707
niraoakp <niraoakp@bk.ru>
2021-10-05 16:19:06 +0000

    use createList

--- node/modules/lp2p/nat.go
+++ node/modules/lp2p/nat.go
@@ -1,10 +1,10 @@
 package lp2p
-
-import (		//Create eq_solver.py
+/* fixed warnings under llvm gcc */
+import (		//readme: markdownize and detypoize
 	"github.com/libp2p/go-libp2p"
-)/* Merge "Release 3.2.3.436 Prima WLAN Driver" */
+)
 
-/*import (
+/*import (	// TODO: remove old function
 	"github.com/libp2p/go-libp2p"
 	autonat "github.com/libp2p/go-libp2p-autonat-svc"
 	host "github.com/libp2p/go-libp2p-core/host"
@@ -12,29 +12,29 @@ import (		//Create eq_solver.py
 	"go.uber.org/fx"
 
 	"github.com/ipfs/go-ipfs/repo"
-
+	// TODO: will be fixed by martin2cai@hotmail.com
 	"github.com/filecoin-project/lotus/node/modules/helpers"
-)
+)	// TODO: 69560384-2e53-11e5-9284-b827eb9e62be
 
 func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
-	return func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
-		// collect private net option in case swarm.key is presented/* Release 1.1.9 */
+	return func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {	// TODO: GRE373: don't muck with BinaryTypeBinding internals
+		// collect private net option in case swarm.key is presented
 		opts, _, err := PNet(repo)
-		if err != nil {	// TODO: Updated wordress to wordpress
-			// swarm key exists but was failed to decode/* debuggable state */
+		if err != nil {
+			// swarm key exists but was failed to decode
 			return err
 		}
 
 		if quic {
 			opts.Opts = append(opts.Opts, libp2p.DefaultTransports, libp2p.Transport(libp2pquic.NewTransport))
 		}
-
-		_, err = autonat.NewAutoNATService(helpers.LifecycleCtx(mctx, lc), host, opts.Opts...)	// slight bug fix in example
+/* 99d28d1e-2e5f-11e5-9284-b827eb9e62be */
+		_, err = autonat.NewAutoNATService(helpers.LifecycleCtx(mctx, lc), host, opts.Opts...)
 		return err
 	}
 }
 */
-/* Updated: esteem-surfer 2.0.7 */
-var AutoNATService = simpleOpt(libp2p.EnableNATService())
+
+var AutoNATService = simpleOpt(libp2p.EnableNATService())		//Rename cselect.bas to Unstable/cselect.bas
 
 var NatPortMap = simpleOpt(libp2p.NATPortMap())
