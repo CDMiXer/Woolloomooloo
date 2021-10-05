ee131f9460ca56186130e7604d3a8f81126c790b
niraoakp <niraoakp@bk.ru>
2021-10-05 19:37:08 +0000

    Supression info sur Type Evènement

--- tests/integration/double_pending_delete/step1/resource.ts
+++ tests/integration/double_pending_delete/step1/resource.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: ff75ed7c-2e42-11e5-9284-b827eb9e62be
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Lock to bitcoind-php v1.2.x
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import * as pulumi from "@pulumi/pulumi";/* it is now possible to instanciate multiple player factories */
+import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
 export class Provider implements dynamic.ResourceProvider {
@@ -27,7 +27,7 @@ export class Provider implements dynamic.ResourceProvider {
     }
 
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
-        if (news.fail != olds.fail) {		//Changed plugin generator to use parent for class declaration.
+        if (news.fail != olds.fail) {
             return {
                 changes: true,
                 replaces: ["fail"]
@@ -36,17 +36,17 @@ export class Provider implements dynamic.ResourceProvider {
 
         return {
             changes: false,
-        }	// Fixes #1306 Java PermSize command line flag removed in Java 8
+        }
     }
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         if (inputs.fail == 1) {
             throw new Error("failed to create this resource");
-        }	// TODO: Merge "[FIX] Field: on FieldHelp selection remove only own valueState"
+        }
 
         return {
             id: (this.id++).toString(),
-            outs: inputs,/* Merge "Release 3.2.3.421 Prima WLAN Driver" */
+            outs: inputs,
         }
     }
 
@@ -57,6 +57,6 @@ export class Provider implements dynamic.ResourceProvider {
 
 export class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, props, opts);/* Released v1.1.0 */
+        super(Provider.instance, name, props, opts);
     }
 }
