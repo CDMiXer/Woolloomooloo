75411e7404733f178f9f8813b09e3cf6d68c2000
niraoakp <niraoakp@bk.ru>
2021-10-05 08:53:35 +0000

    Clean up capitalisation of comments in `server` template

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -4,5 +4,5 @@ import { Component } from "./component";
 
 const componentA = new Component("a", {echo: 42});
 const componentB = new Component("b", {echo: componentA.echo});
-const componentC = new Component("c", {echo: componentA.childId});	// Rename Learning the stack.md to docs2/Learning the stack.md
+const componentC = new Component("c", {echo: componentA.childId});
 
