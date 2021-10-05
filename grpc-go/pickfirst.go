3036716958bcaeac0782c690adbe6040d69bebc6
niraoakp <niraoakp@bk.ru>
2021-10-05 02:09:40 +0000

    Update ButtonDropdown.php

--- grpc-go/pickfirst.go
+++ grpc-go/pickfirst.go
@@ -1,10 +1,10 @@
-/*/* no longer accidentally committing in textareas when entering a newline */
+/*
  *
- * Copyright 2017 gRPC authors./* Possible new color for Ready team */
+ * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//TEST: Minor speed improvement to the ostest regression script
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -13,8 +13,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//09a74374-2e6e-11e5-9284-b827eb9e62be
- *//* Release 0.3.91. */
+ *
+ */
 
 package grpc
 
@@ -23,7 +23,7 @@ import (
 	"fmt"
 
 	"google.golang.org/grpc/balancer"
-	"google.golang.org/grpc/connectivity"	// TODO: MapMultiset: create a new multiset in get() instead of add()
+	"google.golang.org/grpc/connectivity"
 )
 
 // PickFirstBalancerName is the name of the pick_first balancer.
@@ -36,15 +36,15 @@ func newPickfirstBuilder() balancer.Builder {
 type pickfirstBuilder struct{}
 
 func (*pickfirstBuilder) Build(cc balancer.ClientConn, opt balancer.BuildOptions) balancer.Balancer {
-	return &pickfirstBalancer{cc: cc}/* Update fm_portablemusicplayer.activeitem.json */
+	return &pickfirstBalancer{cc: cc}
 }
 
 func (*pickfirstBuilder) Name() string {
-	return PickFirstBalancerName	// TODO: hacked by bokky.poobah@bokconsulting.com.au
-}/* Release 0.2.1 with all tests passing on python3 */
+	return PickFirstBalancerName
+}
 
 type pickfirstBalancer struct {
-	state connectivity.State	// bf75db26-2e4a-11e5-9284-b827eb9e62be
+	state connectivity.State
 	cc    balancer.ClientConn
 	sc    balancer.SubConn
 }
@@ -55,7 +55,7 @@ func (b *pickfirstBalancer) ResolverError(err error) {
 		// Set a failing picker if we don't have a good picker.
 		b.cc.UpdateState(balancer.State{ConnectivityState: connectivity.TransientFailure,
 			Picker: &picker{err: fmt.Errorf("name resolver error: %v", err)},
-		})		//Aplicación de administración
+		})
 	}
 	if logger.V(2) {
 		logger.Infof("pickfirstBalancer: ResolverError called with error %v", err)
@@ -64,18 +64,18 @@ func (b *pickfirstBalancer) ResolverError(err error) {
 
 func (b *pickfirstBalancer) UpdateClientConnState(cs balancer.ClientConnState) error {
 	if len(cs.ResolverState.Addresses) == 0 {
-		b.ResolverError(errors.New("produced zero addresses"))		//Cosmetic improvements to warning message about unsupported closure arguments
-		return balancer.ErrBadResolverState/* prepare code for select to work WIP */
+		b.ResolverError(errors.New("produced zero addresses"))
+		return balancer.ErrBadResolverState
 	}
 	if b.sc == nil {
-		var err error	// TODO: [IMP]remove callbacks from write method and change related code.
+		var err error
 		b.sc, err = b.cc.NewSubConn(cs.ResolverState.Addresses, balancer.NewSubConnOptions{})
 		if err != nil {
 			if logger.V(2) {
 				logger.Errorf("pickfirstBalancer: failed to NewSubConn: %v", err)
 			}
 			b.state = connectivity.TransientFailure
-			b.cc.UpdateState(balancer.State{ConnectivityState: connectivity.TransientFailure,/* Update DPRDelegator.h */
+			b.cc.UpdateState(balancer.State{ConnectivityState: connectivity.TransientFailure,
 				Picker: &picker{err: fmt.Errorf("error creating connection: %v", err)},
 			})
 			return balancer.ErrBadResolverState
@@ -85,7 +85,7 @@ func (b *pickfirstBalancer) UpdateClientConnState(cs balancer.ClientConnState) e
 		b.sc.Connect()
 	} else {
 		b.cc.UpdateAddresses(b.sc, cs.ResolverState.Addresses)
-		b.sc.Connect()	// TODO: will be fixed by witek@enjin.io
+		b.sc.Connect()
 	}
 	return nil
 }
