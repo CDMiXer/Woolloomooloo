bb0953d567745b3f0aad1b7ccc293b8c4dce08ed
niraoakp <niraoakp@bk.ru>
2021-10-05 20:15:10 +0000

    fix VT order to positives/total

--- pkg/backend/filestate/snapshot.go
+++ pkg/backend/filestate/snapshot.go
@@ -2,22 +2,22 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* add index html */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Uncache failed requests, too
-// See the License for the specific language governing permissions and/* Changed error message below the submit button */
-// limitations under the License.	// [email digest] [fix] [hot] only open to-do items
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package filestate
 
-import (		//Add links to contributor GitHub profiles
+import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"	// Delete designator.png
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Fetch in strong */
 )
 
 // localSnapshotManager is a simple SnapshotManager implementation that persists snapshots
@@ -25,19 +25,19 @@ import (		//Add links to contributor GitHub profiles
 type localSnapshotPersister struct {
 	name    tokens.QName
 	backend *localBackend
-	sm      secrets.Manager
-}/* Document some crap. */
-		//bugfix for "editor-table-option"
+	sm      secrets.Manager	// Add mnemonics to node list ui elements
+}
+/* Merge branch '2.12-bugfixes' */
 func (sp *localSnapshotPersister) SecretsManager() secrets.Manager {
 	return sp.sm
 }
 
-func (sp *localSnapshotPersister) Save(snapshot *deploy.Snapshot) error {
-	_, err := sp.backend.saveStack(sp.name, snapshot, sp.sm)/* app automatic pending latency */
+func (sp *localSnapshotPersister) Save(snapshot *deploy.Snapshot) error {	// Update restapi.clj
+	_, err := sp.backend.saveStack(sp.name, snapshot, sp.sm)	// TODO: will be fixed by davidad@alum.mit.edu
 	return err
 
 }
-		//HTML cleanup.
-func (b *localBackend) newSnapshotPersister(stackName tokens.QName, sm secrets.Manager) *localSnapshotPersister {
-	return &localSnapshotPersister{name: stackName, backend: b, sm: sm}/* fixed: vc10 warnings */
+
+func (b *localBackend) newSnapshotPersister(stackName tokens.QName, sm secrets.Manager) *localSnapshotPersister {/* Release LastaFlute */
+	return &localSnapshotPersister{name: stackName, backend: b, sm: sm}
 }
