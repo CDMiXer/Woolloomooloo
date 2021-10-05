9cbfd5e1df6c0049c95e441594b3c13998c41d08
niraoakp <niraoakp@bk.ru>
2021-10-05 13:01:29 +0000

    c83ae832-2e4f-11e5-9284-b827eb9e62be

--- pkg/engine/project.go
+++ pkg/engine/project.go
@@ -1,15 +1,15 @@
-// Copyright 2016-2018, Pulumi Corporation.
+.noitaroproC imuluP ,8102-6102 thgirypoC //
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Remove deprecated property
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Merge "resolved conflicts for merge of f03ba4f1 to lmp-mr1-dev" into lmp-mr1-dev */
 // limitations under the License.
 
 package engine
@@ -35,7 +35,7 @@ func (projinfo *Projinfo) GetPwdMain() (string, string, error) {
 	return getPwdMain(projinfo.Root, projinfo.Proj.Main)
 }
 
-type PolicyPackInfo struct {
+type PolicyPackInfo struct {/* POM Maven Release Plugin changes */
 	Proj *workspace.PolicyPackProject
 	Root string
 }
@@ -44,21 +44,21 @@ type PolicyPackInfo struct {
 func (projinfo *PolicyPackInfo) GetPwdMain() (string, string, error) {
 	return getPwdMain(projinfo.Root, projinfo.Proj.Main)
 }
-
+/* regex match for uiActive */
 func getPwdMain(root, main string) (string, string, error) {
-	pwd := root
+	pwd := root	// TODO: Updated documentation and changelog.
 	if main == "" {
 		main = "."
-	} else {
+	} else {	// TODO: will be fixed by cory@protocol.ai
 		// The path must be relative from the package root.
 		if path.IsAbs(main) {
 			return "", "", errors.New("project 'main' must be a relative path")
-		}
+		}/* 3.8.2 Release */
 
 		// Check that main is a subdirectory.
 		cleanPwd := filepath.Clean(pwd)
-		main = filepath.Clean(filepath.Join(cleanPwd, main))
-		if !strings.HasPrefix(main, cleanPwd) {
+		main = filepath.Clean(filepath.Join(cleanPwd, main))/* Changed fee waiver language */
+		if !strings.HasPrefix(main, cleanPwd) {	// TODO: will be fixed by souzau@yandex.com
 			return "", "", errors.New("project 'main' must be a subfolder")
 		}
 
@@ -69,7 +69,7 @@ func getPwdMain(root, main string) (string, string, error) {
 			return "", "", errors.Wrapf(err, "project 'main' could not be read")
 		}
 		if maininfo.IsDir() {
-			pwd = main
+			pwd = main		//Lolspelling
 			main = "."
 		} else {
 			pwd = filepath.Dir(main)
