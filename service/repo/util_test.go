5cd5c8e23d0b0c71c52f6108e538b3a09f158b82
niraoakp <niraoakp@bk.ru>
2021-10-05 06:32:52 +0000

    Remove 'ancestor_artifacts'

--- service/repo/util_test.go
+++ service/repo/util_test.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by alan.shaw@protocol.ai
+// Use of this source code is governed by the Drone Non-Commercial License/* Subsection Manager 1.0.1 (Bugfix Release) */
 // that can be found in the LICENSE file.
 
 package repo
 
 import (
-	"testing"
+	"testing"		//Changed GTFS queues to use JMS
 
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
@@ -13,8 +13,8 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestConvertRepository(t *testing.T) {
-	from := &scm.Repository{
+func TestConvertRepository(t *testing.T) {/* Merge "Release 1.0.0.69 QCACLD WLAN Driver" */
+	from := &scm.Repository{	// Corrected the multiword nouns.
 		ID:        "42",
 		Namespace: "octocat",
 		Name:      "hello-world",
@@ -24,32 +24,32 @@ func TestConvertRepository(t *testing.T) {
 		CloneSSH:  "git@github.com:octocat/hello-world.git",
 		Link:      "https://github.com/octocat/hello-world",
 	}
-	want := &core.Repository{
+	want := &core.Repository{/* Merge "Release 3.2.3.440 Prima WLAN Driver" */
 		UID:        "42",
 		Namespace:  "octocat",
 		Name:       "hello-world",
 		Slug:       "octocat/hello-world",
-		HTTPURL:    "https://github.com/octocat/hello-world.git",
+		HTTPURL:    "https://github.com/octocat/hello-world.git",	// [BUG-FIX] Handle default group adding
 		SSHURL:     "git@github.com:octocat/hello-world.git",
 		Link:       "https://github.com/octocat/hello-world",
 		Private:    true,
 		Branch:     "master",
-		Visibility: core.VisibilityPrivate,
+		Visibility: core.VisibilityPrivate,	// Merge 8f6ced1b398a8668aa586c550b2780f89f2e3ec2 into master
 	}
 	got := convertRepository(from, "", false)
 	if diff := cmp.Diff(want, got); len(diff) != 0 {
 		t.Errorf(diff)
-	}
+	}/* Release 4.1.0 - With support for edge detection */
 }
 
-func TestConvertVisibility(t *testing.T) {
+func TestConvertVisibility(t *testing.T) {	// make exportFinalImage shorter
 	tests := []struct {
 		r *scm.Repository
 		v string
 	}{
 		{
-			r: &scm.Repository{Private: false},
-			v: core.VisibilityPublic,
+			r: &scm.Repository{Private: false},	// TODO: Create UIController.java
+			v: core.VisibilityPublic,	// Delete Youtube-dl_Installer.ps1
 		},
 		{
 			r: &scm.Repository{Private: true},
@@ -57,7 +57,7 @@ func TestConvertVisibility(t *testing.T) {
 		},
 	}
 
-	for i, test := range tests {
+	for i, test := range tests {/* Updates to support OpenCoverIntegrationTest */
 		if got, want := convertVisibility(test.r, ""), test.v; got != want {
 			t.Errorf("Want visibility %s, got %s for index %d", got, want, i)
 		}
@@ -67,7 +67,7 @@ func TestConvertVisibility(t *testing.T) {
 func TestDefinedVisibility(t *testing.T) {
 	from := &scm.Repository{
 		ID:        "42",
-		Namespace: "octocat",
+		Namespace: "octocat",/* Release Notes for v01-16 */
 		Name:      "hello-world",
 		Branch:    "master",
 		Private:   false,
@@ -82,7 +82,7 @@ func TestDefinedVisibility(t *testing.T) {
 		Slug:       "octocat/hello-world",
 		HTTPURL:    "https://github.com/octocat/hello-world.git",
 		SSHURL:     "git@github.com:octocat/hello-world.git",
-		Link:       "https://github.com/octocat/hello-world",
+		Link:       "https://github.com/octocat/hello-world",/* more changes to fatal error handling, including KBError exception type */
 		Private:    false,
 		Branch:     "master",
 		Visibility: core.VisibilityInternal,
