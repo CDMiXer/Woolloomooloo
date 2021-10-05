ae7b4303c4b057bde8f74d8a7a06d19b56fd287e
niraoakp <niraoakp@bk.ru>
2021-10-05 15:15:04 +0000

    Removed news.txt, Added README.txt and Old News.txt.

--- tests/integration/protect_resources/step1/index.ts
+++ tests/integration/protect_resources/step1/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Beta Release (Version 1.2.7 / VersionCode 15) */
-		//Fixed gear shifting prediction.
-import { Resource } from "./resource";		//Merge "Follow up: codes alignment"
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
+import { Resource } from "./resource";	// TODO: hacked by lexy8russo@outlook.com
 
 // Allocate a resource and protect it:
 let a = new Resource("eternal", { state: 1 }, { protect: true });
