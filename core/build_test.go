ee3b3f9111a7202272a84b7b575bea4b84656152
niraoakp <niraoakp@bk.ru>
2021-10-05 14:40:10 +0000

    229c970c-2e5b-11e5-9284-b827eb9e62be

--- core/build_test.go
+++ core/build_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// disabling bottom border on last row of all FixedDataTables
-// that can be found in the LICENSE file.
-	// TODO: hacked by mail@overlisted.net
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.	// TODO: Rename example-scratch-def.json to project-scratch-def.jso
+
 // +build !oss
 
 package core
