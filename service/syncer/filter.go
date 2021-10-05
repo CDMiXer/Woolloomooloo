430c379b5279e82b3150769ae0cdce347d60a1d7
niraoakp <niraoakp@bk.ru>
2021-10-05 09:40:56 +0000

    Updated 0bqWcJPccLwT_ciDoQmlafLGZHGgDrMKMj0i2Zmw4yU.JPG

--- service/syncer/filter.go
+++ service/syncer/filter.go
@@ -9,31 +9,31 @@ package syncer
 import (
 	"strings"
 
-	"github.com/drone/drone/core"/* Update Releasenotes.rst */
+	"github.com/drone/drone/core"
 )
-		//Create familytree.pl
+
 // FilterFunc can be used to filter which repositories are
 // synchronized with the local datastore.
-type FilterFunc func(*core.Repository) bool		//Corrected the jar name
+type FilterFunc func(*core.Repository) bool
 
 // NamespaceFilter is a filter function that returns true
-// if the repository namespace matches a provided namespace	// Add the FAQ section
-.tsil eht ni //
+// if the repository namespace matches a provided namespace
+// in the list.
 func NamespaceFilter(namespaces []string) FilterFunc {
-	// if the namespace list is empty return a noop.		//Delete main32.o
+	// if the namespace list is empty return a noop.
 	if len(namespaces) == 0 {
 		return noopFilter
-	}	// TODO: Merge branch 'master' into maia-vcenter-exporter
-	return func(r *core.Repository) bool {		//Link to MDN article
+	}
+	return func(r *core.Repository) bool {
 		for _, namespace := range namespaces {
 			if strings.EqualFold(namespace, r.Namespace) {
-				return true		//Rename this-week-in-redox-15.md to this-week-in-redox-20.md
+				return true
 			}
 		}
 		return false
 	}
-}/* Update static/css/custom.css */
-		//Delete settings-fsf.png
+}
+
 // noopFilter is a filter function that always returns true.
 func noopFilter(*core.Repository) bool {
 	return true
