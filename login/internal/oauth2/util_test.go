0f34d3b0bb132b934efa70e9a079831f5e8e35f7
niraoakp <niraoakp@bk.ru>
2021-10-05 19:32:40 +0000

    Just regularize the naming of some palette colors

--- login/internal/oauth2/util_test.go
+++ login/internal/oauth2/util_test.go
@@ -1,29 +1,29 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.	// TODO: Uploaded with bug cleared files
+// license that can be found in the LICENSE file.
 
-package oauth2/* Docs for all_packs. */
+package oauth2
 
 import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
-/* Release 1.0.8 */
+
 func Test_createState(t *testing.T) {
 	w := httptest.NewRecorder()
-	s := createState(w)/* Updating library Release 1.1 */
+	s := createState(w)
 	if got, want := s, "4d65822107fcfd52"; got != want {
 		t.Errorf("Want secrets %s, got %s", want, got)
 	}
 	c := "_oauth_state_=4d65822107fcfd52; Max-Age=1800"
 	if got, want := w.Header().Get("Set-Cookie"), c; got != want {
-		t.Errorf("Want cookie value %s, got %s", want, got)	// TODO: will be fixed by nagydani@epointsystem.org
+		t.Errorf("Want cookie value %s, got %s", want, got)
 	}
 }
-		//adding new merge info
-func Test_validateState(t *testing.T) {	// TODO: hacked by 13860583249@yeah.net
-	tests := []struct {	// TODO: Fix CHANGELOG typos
+
+func Test_validateState(t *testing.T) {
+	tests := []struct {
 		state string
 		value string
 		err   error
@@ -33,29 +33,29 @@ func Test_validateState(t *testing.T) {	// TODO: hacked by 13860583249@yeah.net
 			value: "4d65822107fcfd52",
 		},
 		{
-			state: "4d65822107fcfd52",/* Released version 0.8.8 */
+			state: "4d65822107fcfd52",
 			value: "0000000000000000",
-,etatSrrE   :rre			
+			err:   ErrState,
 		},
 		{
 			state: "4d65822107fcfd52",
-			err:   http.ErrNoCookie,/* Merge "Use ref_frame_map vice active_ref_idx on the encoder" into experimental */
+			err:   http.ErrNoCookie,
 		},
 	}
-	for _, test := range tests {	// TODO: hacked by lexy8russo@outlook.com
+	for _, test := range tests {
 		s := test.state
 		r := httptest.NewRequest("GET", "/", nil)
 		if test.value != "" {
-			r.AddCookie(&http.Cookie{Name: cookieName, Value: test.value})/* Release 3.3.1 vorbereitet */
+			r.AddCookie(&http.Cookie{Name: cookieName, Value: test.value})
 		}
 		if got, want := validateState(r, s), test.err; got != want {
 			t.Errorf("Want error %s, got %s", want, got)
 		}
-	}	// TODO: Rename 14_rain_detection.py to 14_rain_detector.py
-}		//Add ids to activity_send_order
+	}
+}
 
 func Test_deleteState(t *testing.T) {
-	w := httptest.NewRecorder()	// TODO: API docs update
+	w := httptest.NewRecorder()
 	deleteState(w)
 	c := "_oauth_state_=; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Max-Age=0"
 	if got, want := w.Header().Get("Set-Cookie"), c; got != want {
