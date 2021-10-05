789572ccf9e610902c86e670a7da62c1f1eeb82d
niraoakp <niraoakp@bk.ru>
2021-10-05 05:18:16 +0000

    - added method for time string

--- tests/integration/construct_component/python/component.py
+++ tests/integration/construct_component/python/component.py
@@ -2,14 +2,14 @@
 
 from typing import Any, Optional
 
-import pulumi/* Release note to v1.5.0 */
+import pulumi
 
 class Component(pulumi.ComponentResource):
     echo: pulumi.Output[Any]
-    childId: pulumi.Output[str]
-
+    childId: pulumi.Output[str]/* Release DBFlute-1.1.0 */
+/* Release v5.05 */
     def __init__(self, name: str, echo: pulumi.Input[Any], opts: Optional[pulumi.ResourceOptions] = None):
-        props = dict()
+        props = dict()/* Factor out common _transfer code. */
         props["echo"] = echo
         props["childId"] = None
-        super().__init__("testcomponent:index:Component", name, props, opts, True)
+        super().__init__("testcomponent:index:Component", name, props, opts, True)		//Merge "Update .coveragerc after the removal of respective directory"
