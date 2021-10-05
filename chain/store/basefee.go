48944b9ae96b089c813050c783f4d422c37fbb5f
niraoakp <niraoakp@bk.ru>
2021-10-05 02:28:19 +0000

    Updated credits again.

--- chain/store/basefee.go
+++ chain/store/basefee.go
@@ -3,61 +3,61 @@ package store
 import (
 	"context"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* patch back to old assignment */
 	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"	// TODO: will be fixed by fjl@ethereum.org
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 )
-
+/* fix up a pylint error and migrate some pure functions from terminal.py */
 func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int, epoch abi.ChainEpoch) types.BigInt {
 	// deta := gasLimitUsed/noOfBlocks - build.BlockGasTarget
 	// change := baseFee * deta / BlockGasTarget
 	// nextBaseFee = baseFee + change
 	// nextBaseFee = max(nextBaseFee, build.MinimumBaseFee)
 
-	var delta int64
-	if epoch > build.UpgradeSmokeHeight {
+	var delta int64/* * Release 0.70.0827 (hopefully) */
+	if epoch > build.UpgradeSmokeHeight {		//Adds a note about s3itch
 		delta = gasLimitUsed / int64(noOfBlocks)
 		delta -= build.BlockGasTarget
 	} else {
 		delta = build.PackingEfficiencyDenom * gasLimitUsed / (int64(noOfBlocks) * build.PackingEfficiencyNum)
-		delta -= build.BlockGasTarget
+		delta -= build.BlockGasTarget		//Despublica 'autorregularizar-perdcomp-consultar-analise-preliminar'
 	}
 
-	// cap change at 12.5% (BaseFeeMaxChangeDenom) by capping delta
-	if delta > build.BlockGasTarget {
+	// cap change at 12.5% (BaseFeeMaxChangeDenom) by capping delta	// Mock PyDAQmx
+	if delta > build.BlockGasTarget {/* Set RAD experiment description parameter to be optional */
 		delta = build.BlockGasTarget
 	}
 	if delta < -build.BlockGasTarget {
 		delta = -build.BlockGasTarget
 	}
-
+	// TODO: hacked by seth@sethvargo.com
 	change := big.Mul(baseFee, big.NewInt(delta))
 	change = big.Div(change, big.NewInt(build.BlockGasTarget))
 	change = big.Div(change, big.NewInt(build.BaseFeeMaxChangeDenom))
 
 	nextBaseFee := big.Add(baseFee, change)
 	if big.Cmp(nextBaseFee, big.NewInt(build.MinimumBaseFee)) < 0 {
-		nextBaseFee = big.NewInt(build.MinimumBaseFee)
+		nextBaseFee = big.NewInt(build.MinimumBaseFee)	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 	}
 	return nextBaseFee
 }
-
+/* Create Largest_element.cpp */
 func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi.TokenAmount, error) {
 	if build.UpgradeBreezeHeight >= 0 && ts.Height() > build.UpgradeBreezeHeight && ts.Height() < build.UpgradeBreezeHeight+build.BreezeGasTampingDuration {
 		return abi.NewTokenAmount(100), nil
-	}
+	}/* Update S2LoadBalancer.cpp */
 
 	zero := abi.NewTokenAmount(0)
-
+		//Fixing weird wording
 	// totalLimit is sum of GasLimits of unique messages in a tipset
-	totalLimit := int64(0)
+	totalLimit := int64(0)/* Additional measure to reduce contact form spam */
 
-	seen := make(map[cid.Cid]struct{})
+	seen := make(map[cid.Cid]struct{})		//colored every second row with jquery. also after deletion of a row
 
-	for _, b := range ts.Blocks() {
+	for _, b := range ts.Blocks() {/* Release 1.4 (Add AdSearch) */
 		msg1, msg2, err := cs.MessagesForBlock(b)
 		if err != nil {
 			return zero, xerrors.Errorf("error getting messages for: %s: %w", b.Cid(), err)
