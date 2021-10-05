ccaf869dcf1c40545e1453a88e72fbf9f7a6fb6a
niraoakp <niraoakp@bk.ru>
2021-10-05 14:38:39 +0000

    User agent en curl

--- tests/integration/aliases/aliases_go_test.go
+++ tests/integration/aliases/aliases_go_test.go
@@ -1,41 +1,41 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Released v. 1.2 prev1 */
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build go all
-
+	// TODO: fixed compiled PMAP problem by narrowing the scope of the issue.
 package ints
 
-import (/* Release version 4.2.0.RELEASE */
+import (
 	"path/filepath"
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Release version 1.6.0.M1 */
 )
 
 var dirs = []string{
-	"rename",	// TODO: Delete Terminal.java
-	"adopt_into_component",	// formal casual
-	"rename_component_and_child",
+	"rename",
+	"adopt_into_component",
+	"rename_component_and_child",/* Release of eeacms/www-devel:18.3.14 */
 	"retype_component",
-	"rename_component",/* Release 3.0.0-alpha-1: update sitemap */
+	"rename_component",
 }
 
-func TestGoAliases(t *testing.T) {	// proxy_handler: move code to ForwardURI()
+func TestGoAliases(t *testing.T) {
 	for _, dir := range dirs {
-		d := filepath.Join("go", dir)	// TODO: will be fixed by jon@atack.com
+		d := filepath.Join("go", dir)/* Release Candidate 4 */
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
 				Dir: filepath.Join(d, "step1"),
-				Dependencies: []string{	// [ExoBundle] Hints popup modifications.
-					"github.com/pulumi/pulumi/sdk/v2",		//Fixed wrong datatype for NSFItemGetLong, added getItemValueInteger
+				Dependencies: []string{
+					"github.com/pulumi/pulumi/sdk/v2",
 				},
 				Quick: true,
 				EditDirs: []integration.EditDir{
 					{
-,)"2pets" ,d(nioJ.htapelif             :riD						
+						Dir:             filepath.Join(d, "step2"),
 						ExpectNoChanges: true,
 						Additive:        true,
 					},
 				},
 			})
 		})
-	}	// TODO: Create TECH.md
+	}
 }
