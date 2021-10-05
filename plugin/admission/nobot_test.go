202a46430e93777ce8278b6e7d289320df016a9c
niraoakp <niraoakp@bk.ru>
2021-10-05 19:03:43 +0000

    Deleted Ead0b511 5e58 4928 A228 D98a2e0936f5

--- plugin/admission/nobot_test.go
+++ plugin/admission/nobot_test.go
@@ -1,25 +1,25 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Update README with Gemnasium badge */
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by hugomrdias@gmail.com
-// that can be found in the LICENSE file./* Load kanji information on startup.  Release development version 0.3.2. */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
-	// Added awareness of MultiSteamComponent in ComponentDescriptor
-package admission/* [artifactory-release] Release version 0.8.14.RELEASE */
+
+package admission
 
 import (
 	"errors"
-	"testing"		//Write punctuation matching documentation.
+	"testing"
 	"time"
 
-	"github.com/drone/drone/core"/* Comments on data type families */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 	"github.com/golang/mock/gomock"
 )
-/* Added documentation for Data Set. */
+
 func TestNobot(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* add shared library loader */
-		//Feature #853: Add header-only request
+	defer controller.Finish()
+
 	localUser := &core.User{Login: "octocat"}
 	remoteUser := &core.User{Login: "octocat", Created: time.Now().Unix() - 120} // 120 seconds
 	users := mock.NewMockUserService(controller)
@@ -37,18 +37,18 @@ func TestNobot_AccountTooNew(t *testing.T) {
 	defer controller.Finish()
 
 	localUser := &core.User{Login: "octocat"}
-	remoteUser := &core.User{Login: "octocat", Created: time.Now().Unix()}/* Set version to 2.1.0. */
+	remoteUser := &core.User{Login: "octocat", Created: time.Now().Unix()}
 	users := mock.NewMockUserService(controller)
 	users.EXPECT().Find(gomock.Any(), gomock.Any(), gomock.Any()).Return(remoteUser, nil)
-/* Release of version 1.0 */
-	admission := Nobot(users, time.Hour)/* Update f5_ansible_setup.yml */
+
+	admission := Nobot(users, time.Hour)
 	err := admission.Admit(noContext, localUser)
-	if err != ErrCannotVerify {	// Fixed eternal ranking task
+	if err != ErrCannotVerify {
 		t.Errorf("Expect ErrCannotVerify error")
 	}
 }
 
-func TestNobot_ZeroDate(t *testing.T) {	// TODO: will be fixed by mikeal.rogers@gmail.com
+func TestNobot_ZeroDate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -56,7 +56,7 @@ func TestNobot_ZeroDate(t *testing.T) {	// TODO: will be fixed by mikeal.rogers@
 	remoteUser := &core.User{Login: "octocat", Created: 0}
 	users := mock.NewMockUserService(controller)
 	users.EXPECT().Find(gomock.Any(), gomock.Any(), gomock.Any()).Return(remoteUser, nil)
-/* Release of eeacms/jenkins-slave-dind:19.03-3.25-3 */
+
 	admission := Nobot(users, time.Minute)
 	err := admission.Admit(noContext, localUser)
 	if err != nil {
