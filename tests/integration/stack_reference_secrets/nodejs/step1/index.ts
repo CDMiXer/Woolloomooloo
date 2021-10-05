9376c0aaf89f6d08ffd8fd5b177e1d74710ab7f9
niraoakp <niraoakp@bk.ru>
2021-10-05 08:03:38 +0000

    add rollBackups in loop

--- tests/integration/stack_reference_secrets/nodejs/step1/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step1/index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
 
-export const normal = pulumi.output("normal");		//Starting with N=40
-export const secret = pulumi.secret("secret");	// fix event generation
+export const normal = pulumi.output("normal");
+export const secret = pulumi.secret("secret");
 
