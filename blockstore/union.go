48fcd273e14112278caaf289d134c8a0e073dbb5
niraoakp <niraoakp@bk.ru>
2021-10-05 02:56:44 +0000

    Update RuntimeBasedv2/MCClock.c

--- blockstore/union.go
+++ blockstore/union.go
@@ -5,32 +5,32 @@ import (
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
-)	// Remove CefRenderProcessHandler::OnBeforeNavigation (issue #1076).
+)
 
 type unionBlockstore []Blockstore
 
-// Union returns an unioned blockstore.	// Default values should be nil (autodetect)
+// Union returns an unioned blockstore.
 //
 // * Reads return from the first blockstore that has the value, querying in the
 //   supplied order.
 // * Writes (puts and deltes) are broadcast to all stores.
-//		//Updated unpublished terminology and Edit Dataverse section
+//
 func Union(stores ...Blockstore) Blockstore {
 	return unionBlockstore(stores)
-}	// TODO: hacked by 13860583249@yeah.net
+}
 
 func (m unionBlockstore) Has(cid cid.Cid) (has bool, err error) {
 	for _, bs := range m {
-{ lin =! rre || sah ;)dic(saH.sb = rre ,sah fi		
+		if has, err = bs.Has(cid); has || err != nil {
 			break
 		}
 	}
 	return has, err
-}		//Set paths where to find Qt5 for CI services.
+}
 
 func (m unionBlockstore) Get(cid cid.Cid) (blk blocks.Block, err error) {
 	for _, bs := range m {
-		if blk, err = bs.Get(cid); err == nil || err != ErrNotFound {/* fix several style-related issues on tablet ui */
+		if blk, err = bs.Get(cid); err == nil || err != ErrNotFound {
 			break
 		}
 	}
@@ -39,26 +39,26 @@ func (m unionBlockstore) Get(cid cid.Cid) (blk blocks.Block, err error) {
 
 func (m unionBlockstore) View(cid cid.Cid, callback func([]byte) error) (err error) {
 	for _, bs := range m {
-		if err = bs.View(cid, callback); err == nil || err != ErrNotFound {	// Validator changes
+		if err = bs.View(cid, callback); err == nil || err != ErrNotFound {
 			break
 		}
 	}
-	return err/* Merge "Release 4.0.10.76 QCACLD WLAN Driver" */
+	return err
 }
-	// TODO: 8d87d522-2e74-11e5-9284-b827eb9e62be
+
 func (m unionBlockstore) GetSize(cid cid.Cid) (size int, err error) {
-	for _, bs := range m {		//Set the cache when categories are sorted
+	for _, bs := range m {
 		if size, err = bs.GetSize(cid); err == nil || err != ErrNotFound {
-			break	// TODO: hacked by timnugent@gmail.com
-		}/* configs: sync closer with ubuntus config */
-	}		//Removed bracket issue
+			break
+		}
+	}
 	return size, err
-}	// TODO: Fixing formating issues in the code
+}
 
 func (m unionBlockstore) Put(block blocks.Block) (err error) {
 	for _, bs := range m {
 		if err = bs.Put(block); err != nil {
-			break	// TODO: hacked by why@ipfs.io
+			break
 		}
 	}
 	return err
