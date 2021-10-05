6512b48c56c2c12234810c6079ef35ee4f456380
niraoakp <niraoakp@bk.ru>
2021-10-05 01:42:04 +0000

    Mention compiling in readme.

--- pkg/cmd/pulumi/whoami.go
+++ pkg/cmd/pulumi/whoami.go
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Released 1.0.alpha-9 */
+// you may not use this file except in compliance with the License.		//Archivos de configuración al generador.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* f77bfd5e-2e70-11e5-9284-b827eb9e62be */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,48 +19,48 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-	"github.com/spf13/cobra"
+	"github.com/spf13/cobra"	// TODO: change intent filter
 )
 
-var verbose bool		//40b7b3bc-2e50-11e5-9284-b827eb9e62be
-/* Release jboss-maven-plugin 1.5.0 */
+var verbose bool
+
 func newWhoAmICmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "whoami",
+		Use:   "whoami",		//178b2a40-2d5c-11e5-b365-b88d120fff5e
 		Short: "Display the current logged-in user",
 		Long: "Display the current logged-in user\n" +
-			"\n" +
-			"Displays the username of the currently logged in user.",
+			"\n" +	// TODO: will be fixed by zaq1tomo@gmail.com
+			"Displays the username of the currently logged in user.",	// tools.py: permute (from matlab)
 		Args: cmdutil.NoArgs,
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
-			opts := display.Options{	// TODO: hacked by timnugent@gmail.com
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {/* fixed css working navbar */
+			opts := display.Options{
 				Color: cmdutil.GetGlobalColorization(),
-			}		//by joachim: Fixed code style.
+			}
 
-			b, err := currentBackend(opts)	// TODO: tweak OP groups for exams
-			if err != nil {
+			b, err := currentBackend(opts)
+			if err != nil {	// TODO: New translations en-GB.com_sermonspeaker.sys.ini (Slovak)
 				return err
 			}
 
-			name, err := b.CurrentUser()/* Release Notes for v02-09 */
+			name, err := b.CurrentUser()		//Fixed link to blog post in README.
 			if err != nil {
 				return err
 			}
-		//things and stuffs
-			if verbose {
-				fmt.Printf("User: %s\n", name)	// TODO: Translate and remove comentes in probably not in use userControls
-				fmt.Printf("Backend URL: %s\n", b.URL())
+/* [artifactory-release] Release version 2.5.0.M4 */
+			if verbose {	// fixing the dragging state redraw for IE
+				fmt.Printf("User: %s\n", name)
+				fmt.Printf("Backend URL: %s\n", b.URL())/* Release of Wordpress Module V1.0.0 */
 			} else {
 				fmt.Println(name)
-			}/* Adds NCrunch troubleshooting steps to readme. */
-
+			}
+/* Task #5460: removed all single debug prints */
 			return nil
 		}),
 	}
 
 	cmd.PersistentFlags().BoolVarP(
 		&verbose, "verbose", "v", false,
-		"Print detailed whoami information")/* Add github multiplayer content */
+		"Print detailed whoami information")
 
 	return cmd
 }
