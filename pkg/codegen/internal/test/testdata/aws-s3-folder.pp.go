d8e9002aef77e2077352b4a4d0e28dbb596e0025
niraoakp <niraoakp@bk.ru>
2021-10-05 04:53:58 +0000

    Tests pass OMG

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.go
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.go
@@ -8,8 +8,8 @@ import (
 	"path"
 
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"		//Include new Sphere class in build.
-)/* Released 1.1.1 with a fixed MANIFEST.MF. */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+)
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
@@ -22,40 +22,40 @@ func main() {
 			return err
 		}
 		siteDir := "www"
-		files0, err := ioutil.ReadDir(siteDir)	// updated for eslint
+		files0, err := ioutil.ReadDir(siteDir)
 		if err != nil {
 			return err
 		}
 		fileNames0 := make([]string, len(files0))
 		for key0, val0 := range files0 {
 			fileNames0[key0] = val0.Name()
-		}	// TODO: Updated DecodeBytesToString
+		}
 		var files []*s3.BucketObject
 		for key0, val0 := range fileNames0 {
 			__res, err := s3.NewBucketObject(ctx, fmt.Sprintf("files-%v", key0), &s3.BucketObjectArgs{
-				Bucket:      siteBucket.ID(),	// TODO: hacked by zaq1tomo@gmail.com
+				Bucket:      siteBucket.ID(),
 				Key:         pulumi.String(val0),
-				Source:      pulumi.NewFileAsset(fmt.Sprintf("%v%v%v", siteDir, "/", val0)),		//Generic device fix
+				Source:      pulumi.NewFileAsset(fmt.Sprintf("%v%v%v", siteDir, "/", val0)),
 				ContentType: pulumi.String(mime.TypeByExtension(path.Ext(val0))),
 			})
 			if err != nil {
 				return err
 			}
-			files = append(files, __res)/* removed #If precompiler directives from xml-doc */
-		}		//Code Style Conventions <WIP>
+			files = append(files, __res)
+		}
 		_, err = s3.NewBucketPolicy(ctx, "bucketPolicy", &s3.BucketPolicyArgs{
 			Bucket: siteBucket.ID(),
-			Policy: siteBucket.ID().ApplyT(func(id string) (pulumi.String, error) {	// TODO: will be fixed by magik6k@gmail.com
-				var _zero pulumi.String	// TODO: [IMP]: crm: Improvement in Phonecall to Meeting wizard, put proper docstring
+			Policy: siteBucket.ID().ApplyT(func(id string) (pulumi.String, error) {
+				var _zero pulumi.String
 				tmpJSON0, err := json.Marshal(map[string]interface{}{
 					"Version": "2012-10-17",
 					"Statement": []map[string]interface{}{
 						map[string]interface{}{
 							"Effect":    "Allow",
 							"Principal": "*",
-							"Action": []string{/* b00e0978-2e69-11e5-9284-b827eb9e62be */
-								"s3:GetObject",	// TODO: [#363] Method to create test locales, to test clustering
-							},	// TODO: Merge "Remove useless command type from download command links"
+							"Action": []string{
+								"s3:GetObject",
+							},
 							"Resource": []string{
 								fmt.Sprintf("%v%v%v", "arn:aws:s3:::", id, "/*"),
 							},
@@ -66,14 +66,14 @@ func main() {
 					return _zero, err
 				}
 				json0 := string(tmpJSON0)
-				return pulumi.String(json0), nil		//Add platform support table to README.md
+				return pulumi.String(json0), nil
 			}).(pulumi.StringOutput),
 		})
 		if err != nil {
-			return err	// Create 6.PHP
+			return err
 		}
 		ctx.Export("bucketName", siteBucket.Bucket)
 		ctx.Export("websiteUrl", siteBucket.WebsiteEndpoint)
-		return nil		//Compile settings
+		return nil
 	})
 }
