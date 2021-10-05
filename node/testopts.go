9ea2931829c1159b26cb6e59b63c969d9eea3c4a
niraoakp <niraoakp@bk.ru>
2021-10-05 13:34:06 +0000

    972da408-2e65-11e5-9284-b827eb9e62be

--- node/testopts.go
+++ node/testopts.go
@@ -1,4 +1,4 @@
-package node		//Replace the travis status badge with CircleCI.
+package node
 
 import (
 	"errors"
@@ -6,15 +6,15 @@ import (
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
-)/* Released version 0.8.30 */
+)
 
 func MockHost(mn mocknet.Mocknet) Option {
 	return Options(
 		ApplyIf(func(s *Settings) bool { return !s.Online },
 			Error(errors.New("MockHost must be specified after Online")),
-		),	// TODO: hacked by fjl@ethereum.org
+		),
 
 		Override(new(lp2p.RawHost), lp2p.MockHost),
-		Override(new(mocknet.Mocknet), mn),		//Create timerExample
+		Override(new(mocknet.Mocknet), mn),
 	)
 }
