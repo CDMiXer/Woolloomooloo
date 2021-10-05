17dc19c5833eb0aab4bba14aba99d69a8b8cb49e
niraoakp <niraoakp@bk.ru>
2021-10-05 08:33:35 +0000

    BUGFIX: getRelation, getParent, getChildren

--- tests/integration/read/read_dbr/step1/index.ts
+++ tests/integration/read/read_dbr/step1/index.ts
@@ -1,22 +1,22 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.		//Update expandFns.php
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release Kafka 1.0.8-0.10.0.0 (#39) (#41) */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* b3dc2fc0-327f-11e5-8e81-9cf387a8033e */
-// Unless required by applicable law or agreed to in writing, software/* Merge branch 'master' into pyup-update-pytest-4.3.1-to-5.0.1 */
+//	// TODO: hacked by nagydani@epointsystem.org
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/forests-frontend:1.8.8 */
-// See the License for the specific language governing permissions and
-// limitations under the License./* Release version [11.0.0] - prepare */
-/* [Cleanup] Removed unused addRef and Release functions. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and	// Remove types left over from header split
+// limitations under the License./* 326adf3c-2e69-11e5-9284-b827eb9e62be */
+
 import { Resource } from "./resource";
 
 // Setup: Resource A is external, Resource B is not but it depends on A. Resource C is external and
-// has an input dependent on B.
-const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
+// has an input dependent on B./* Importing SQLMap + sample + docs. */
+const a = new Resource("a", { state: 42 }, { id: "existing-id"} );/* Merge "Revert "Move to RDO Train packages"" */
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
 const c = new Resource("c", { state: b.state }, { id: "another-existing-id" })
 
