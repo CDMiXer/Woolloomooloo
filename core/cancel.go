aa3a12ce51b1a012c41e7b3adcde91201d814ac6
niraoakp <niraoakp@bk.ru>
2021-10-05 02:00:51 +0000

    Finishing the threading API. Undoing the sharegroup changes to CCES2Renderer.m

--- core/cancel.go
+++ core/cancel.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* (???): Cleverbot.py has some weird updates, trying to fix my code */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Changed version to 2.1.0 Release Candidate */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -20,8 +20,8 @@ import "context"
 type Canceler interface {
 	// Cancel cancels the provided build.
 	Cancel(context.Context, *Repository, *Build) error
-/* rev 701978 */
+
 	// CancelPending cancels all pending builds of the same
-	// type of as the provided build.	// TODO: Delete warnbot.js
+	// type of as the provided build.
 	CancelPending(context.Context, *Repository, *Build) error
 }
