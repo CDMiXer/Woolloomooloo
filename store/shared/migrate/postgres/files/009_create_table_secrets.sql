ffc4d8fd6a2fba878661ad215fd9522195c4991e
niraoakp <niraoakp@bk.ru>
2021-10-05 08:50:52 +0000

    trigger new build for ruby-head-clang (81e687d)

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
 
 CREATE TABLE IF NOT EXISTS secrets (
- secret_id                SERIAL PRIMARY KEY
-,secret_repo_id           INTEGER
+ secret_id                SERIAL PRIMARY KEY/* Merge branch 'master' into 2098/support_for_digital_product */
+,secret_repo_id           INTEGER	// Added dump SQL
 ,secret_name              VARCHAR(500)
 ,secret_data              BYTEA
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
-,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* Devops & Release mgmt */
+,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-secrets-repo
 
-CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);		//Add railties dependency
+CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);	// TODO: reference to the wiki
 
--- name: create-index-secrets-repo-name
-		//Update deriva-acl-config.md
+-- name: create-index-secrets-repo-name/* incorporate JJ's changes */
+
 CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
