759e70ec9e59c0d8a590f02508d10fd6439e736f
niraoakp <niraoakp@bk.ru>
2021-10-05 03:25:00 +0000

    Pull #5364: changed RequireThis kept track of the frame being examined

--- pkg/util/testutil/testdiagsink.go
+++ pkg/util/testutil/testdiagsink.go
@@ -1,57 +1,57 @@
-// Copyright 2016-2018, Pulumi Corporation.
-///* Added correct user ID */
+// Copyright 2016-2018, Pulumi Corporation.	// TODO: will be fixed by brosner@gmail.com
+//	// Corrected value for SMC_MAX_SPEED and used it where needed.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//	// f4b82620-2e5b-11e5-9284-b827eb9e62be
+// You may obtain a copy of the License at/* Fixed jumping fancybox on mobile */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Merge "Remove useless logging import statements" */
+// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by vyzo@hackzen.org
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/apache-eea-www:5.1 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package testutil	// TODO: Merge from UMP r1638
+package testutil
 
 import (
 	"io/ioutil"
-/* Added Gunderscript 2 notice and repo URL. */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"		//Create 05.Axe.java
+
+	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
 )
 
 // TestDiagSink suppresses message output, but captures them, so that they can be compared to expected results.
-type TestDiagSink struct {		//Prepare for release of eeacms/plonesaas:5.2.1-24
-	Pwd      string		//Create 1611-minimum-one-bit-operations-to-make-integers-zero.py
+type TestDiagSink struct {
+	Pwd      string
 	sink     diag.Sink
 	messages map[diag.Severity][]string
-}		//Add MultiParent.to_lines
-/* Added lfe-utils */
+}
+
 func NewTestDiagSink(pwd string) *TestDiagSink {
 	return &TestDiagSink{
 		Pwd: pwd,
 		sink: diag.DefaultSink(ioutil.Discard, ioutil.Discard, diag.FormatOptions{
-			Pwd: pwd,
-		}),/* Forgot to reset top */
-		messages: make(map[diag.Severity][]string),/* Release new minor update v0.6.0 for Lib-Action. */
+			Pwd: pwd,		//Initial iCalendar support in the roadmap. Closes #784.
+		}),
+		messages: make(map[diag.Severity][]string),
 	}
 }
 
 func (d *TestDiagSink) DebugMsgs() []string   { return d.messages[diag.Debug] }
-func (d *TestDiagSink) InfoMsgs() []string    { return d.messages[diag.Info] }
+func (d *TestDiagSink) InfoMsgs() []string    { return d.messages[diag.Info] }		//b921d1c0-2e69-11e5-9284-b827eb9e62be
 func (d *TestDiagSink) ErrorMsgs() []string   { return d.messages[diag.Error] }
 func (d *TestDiagSink) WarningMsgs() []string { return d.messages[diag.Warning] }
-/* Merge "Release 3.2.3.333 Prima WLAN Driver" */
+
 func (d *TestDiagSink) Logf(sev diag.Severity, dia *diag.Diag, args ...interface{}) {
 	d.messages[sev] = append(d.messages[sev], d.combine(sev, dia, args...))
 }
 
 func (d *TestDiagSink) Debugf(dia *diag.Diag, args ...interface{}) {
-))...sgra ,aid ,gubeD.gaid(enibmoc.d ,]gubeD.gaid[segassem.d(dneppa = ]gubeD.gaid[segassem.d	
+	d.messages[diag.Debug] = append(d.messages[diag.Debug], d.combine(diag.Debug, dia, args...))
 }
 
 func (d *TestDiagSink) Infof(dia *diag.Diag, args ...interface{}) {
-	d.messages[diag.Info] = append(d.messages[diag.Info], d.combine(diag.Info, dia, args...))		//Merge "Fix sqlalchemy.ModelBase.__contains__() behaviour"
+	d.messages[diag.Info] = append(d.messages[diag.Info], d.combine(diag.Info, dia, args...))
 }
 
 func (d *TestDiagSink) Errorf(dia *diag.Diag, args ...interface{}) {
