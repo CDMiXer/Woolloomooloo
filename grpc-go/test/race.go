638051811a8e4cde108eb52319c543555563059e
niraoakp <niraoakp@bk.ru>
2021-10-05 01:27:47 +0000

    Update realmofreckoning.py

--- grpc-go/test/race.go
+++ grpc-go/test/race.go
@@ -1,19 +1,19 @@
 // +build race
 
-/*/* [-] Technical - Upgrade ESLint rules (#343) */
+/*
  * Copyright 2016 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Update Atac-seq_pipe_update_log.md */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: hacked by m-ou.se@m-ou.se
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// fix issue with populating role data and not returning user correctly
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Update 04_RunTests.bat */
+ * limitations under the License.
  *
  */
 
