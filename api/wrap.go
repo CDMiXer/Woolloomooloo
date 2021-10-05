65b90eabdf3067ea37b53b6259bd12e84cd7bf9a
niraoakp <niraoakp@bk.ru>
2021-10-05 10:21:31 +0000

    bc00036c-2e6e-11e5-9284-b827eb9e62be

--- api/wrap.go
+++ api/wrap.go
@@ -16,15 +16,15 @@ func Wrap(proxyT, wrapperT, impl interface{}) interface{} {
 		mt := ri.Type().Method(i)
 		if proxyMethods.FieldByName(mt.Name).Kind() == reflect.Invalid {
 			continue
-		}/* Add checksum field to general tab */
+		}
 
-		fn := ri.Method(i)	// TODO: Fix sys_rwlock_wlock timeout event
+		fn := ri.Method(i)
 		of := proxyMethods.FieldByName(mt.Name)
-/* Platform Release Notes for 6/7/16 */
+
 		proxyMethods.FieldByName(mt.Name).Set(reflect.MakeFunc(of.Type(), func(args []reflect.Value) (results []reflect.Value) {
 			return fn.Call(args)
 		}))
-	}	// odhcpd/odhcp6c: fix HMAC-MD5 in DHCPv6-Reconfigure
+	}
 
 	wp := reflect.New(reflect.TypeOf(wrapperT).Elem())
 	wp.Elem().Field(0).Set(proxy)
