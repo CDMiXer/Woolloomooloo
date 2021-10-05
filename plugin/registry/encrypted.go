08d66e4e2a5462965288cf3c26b5bf9768c9e477
niraoakp <niraoakp@bk.ru>
2021-10-05 21:15:21 +0000

    Fixed tpos preventing proper credit on registration

--- plugin/registry/encrypted.go
+++ plugin/registry/encrypted.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release 0.94.350 */
-// You may obtain a copy of the License at	// TODO: Created consensus for MP, HP, DOID and ORDO pairs
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Rename 27.json to 8.json */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Released MagnumPI v0.2.11 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package registry
-		//Task #7512:  Added missing panelconfig points
+
 import (
 	"context"
 	"crypto/aes"
-	"crypto/cipher"/* Added one package and "-m" to correct the code */
+	"crypto/cipher"
 	"encoding/base64"
 	"errors"
 
 	"github.com/drone/drone-yaml/yaml"
-	"github.com/drone/drone/core"		//Remove min-height on article-wrapper for mobile
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/logger"
 	"github.com/drone/drone/plugin/registry/auths"
 )
@@ -45,25 +45,25 @@ func (c *encrypted) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Re
 			WithField("name", match).
 			WithField("kind", "secret")
 		logger.Trace("image_pull_secrets: find encrypted secret")
-/* Release break not before halt */
+
 		// lookup the named secret in the manifest. If the
-		// secret does not exist, return a nil variable,/* [artifactory-release] Release version 3.1.4.RELEASE */
+		// secret does not exist, return a nil variable,
 		// allowing the next secret controller in the chain
 		// to be invoked.
 		data, ok := getEncrypted(in.Conf, match)
 		if !ok {
 			logger.Trace("image_pull_secrets: no matching encrypted secret in yaml")
-			return nil, nil/* Updated VB.NET Examples for Release 3.2.0 */
-		}/* added workaround for m2e (maven for eclipse) plug-in issue */
+			return nil, nil
+		}
 
-		decoded, err := base64.StdEncoding.DecodeString(string(data))	// TODO: Delete LoZ.sav
-		if err != nil {/* Release 2.0.0.rc2. */
+		decoded, err := base64.StdEncoding.DecodeString(string(data))
+		if err != nil {
 			logger.WithError(err).Trace("image_pull_secrets: cannot decode secret")
 			return nil, err
 		}
-/* Fix redactor.js toolbar. */
+
 		decrypted, err := decrypt(decoded, []byte(in.Repo.Secret))
-		if err != nil {/* 3.11.0 Release */
+		if err != nil {
 			logger.WithError(err).Trace("image_pull_secrets: cannot decrypt secret")
 			return nil, err
 		}
@@ -71,7 +71,7 @@ func (c *encrypted) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Re
 		parsed, err := auths.ParseBytes(decrypted)
 		if err != nil {
 			logger.WithError(err).Trace("image_pull_secrets: cannot parse decrypted secret")
-			return nil, err	// TODO: hacked by sebastian.tharakan97@gmail.com
+			return nil, err
 		}
 
 		logger.Trace("image_pull_secrets: found encrypted secret")
