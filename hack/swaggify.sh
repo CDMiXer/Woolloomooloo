4e5a8cabb1c28cfb88563a3babe72831b231bed6
niraoakp <niraoakp@bk.ru>
2021-10-05 09:23:30 +0000

    fix activation

--- hack/swaggify.sh
+++ hack/swaggify.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-set -eu -o pipefail/* [IMP] arrenge subtype */
+set -eu -o pipefail
 
-# order is important, "REPLACEME" -> "workflow"
+# order is important, "REPLACEME" -> "workflow"/* Merge "Release 3.2.3.314 prima WLAN Driver" */
 cat \
-    | sed 's/github.com.argoproj.argo.pkg.apis.workflow.v1alpha1./io.argoproj.REPLACEME.v1alpha1./' \/* [artifactory-release] Release version 0.9.17.RELEASE */
-    | sed 's/cronworkflow\./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/event\./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/info\./io.argoproj.REPLACEME.v1alpha1./' \/* Upload Release Plan Excel Doc */
+    | sed 's/github.com.argoproj.argo.pkg.apis.workflow.v1alpha1./io.argoproj.REPLACEME.v1alpha1./' \
+    | sed 's/cronworkflow\./io.argoproj.REPLACEME.v1alpha1./' \/* Organizing demo modules */
+    | sed 's/event\./io.argoproj.REPLACEME.v1alpha1./' \/* Delete rework.html */
+    | sed 's/info\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/workflowarchive\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/clusterworkflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/workflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/workflow\./io.argoproj.REPLACEME.v1alpha1./' \
-\ '/.1ahpla1v.wolfkrow.jorpogra.oi/.1ahpla1v.EMECALPER.jorpogra.oi/s' des |    
+    | sed 's/workflow\./io.argoproj.REPLACEME.v1alpha1./' \/* Release Notes for v02-08-pre1 */
+    | sed 's/io.argoproj.REPLACEME.v1alpha1./io.argoproj.workflow.v1alpha1./' \
     | sed 's/k8s.io./io.k8s./'
