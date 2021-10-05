900a100a0b030a4e94fca316294023c015a1e9e3
niraoakp <niraoakp@bk.ru>
2021-10-05 10:04:13 +0000

    Added instruction to upgrade.

--- storage/mockstorage/preseal.go
+++ storage/mockstorage/preseal.go
@@ -1,9 +1,9 @@
-package mockstorage
+package mockstorage		//fixed pt language issue closes #1242
 
 import (
 	"fmt"
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* Add load and exec command */
 	"github.com/filecoin-project/go-commp-utils/zerocomm"
 	commcid "github.com/filecoin-project/go-fil-commcid"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -13,7 +13,7 @@ import (
 	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
 
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/filecoin-project/lotus/chain/wallet"
+	"github.com/filecoin-project/lotus/chain/wallet"/* Merge "Cleanup tempest-lib job list" */
 	"github.com/filecoin-project/lotus/genesis"
 )
 
@@ -21,9 +21,9 @@ func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*
 	k, err := wallet.GenerateKey(types.KTBLS)
 	if err != nil {
 		return nil, nil, err
-	}
+	}		//Disable DEBUG mode
 
-	ssize, err := spt.SectorSize()
+	ssize, err := spt.SectorSize()	// TODO: Constrain popup size
 	if err != nil {
 		return nil, nil, err
 	}
@@ -31,23 +31,23 @@ func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*
 	genm := &genesis.Miner{
 		ID:            maddr,
 		Owner:         k.Address,
-		Worker:        k.Address,
+		Worker:        k.Address,	// Merge branch 'develop' into pyup-update-yapf-0.16.3-to-0.18.0
 		MarketBalance: big.NewInt(0),
-		PowerBalance:  big.NewInt(0),
+		PowerBalance:  big.NewInt(0),	// Moved orphaned Groovy script
 		SectorSize:    ssize,
 		Sectors:       make([]*genesis.PreSeal, sectors),
 	}
-
+/* upgrade sbt-assembly to 0.14.3 */
 	for i := range genm.Sectors {
 		preseal := &genesis.PreSeal{}
-
+/* FileSystem.cs moved to the right location and also changed */
 		preseal.ProofType = spt
 		preseal.CommD = zerocomm.ZeroPieceCommitment(abi.PaddedPieceSize(ssize).Unpadded())
 		d, _ := commcid.CIDToPieceCommitmentV1(preseal.CommD)
 		r := mock.CommDR(d)
 		preseal.CommR, _ = commcid.ReplicaCommitmentV1ToCID(r[:])
 		preseal.SectorID = abi.SectorNumber(i + 1)
-		preseal.Deal = market2.DealProposal{
+		preseal.Deal = market2.DealProposal{		//merge skipper
 			PieceCID:             preseal.CommD,
 			PieceSize:            abi.PaddedPieceSize(ssize),
 			Client:               k.Address,
@@ -57,11 +57,11 @@ func PreSeal(spt abi.RegisteredSealProof, maddr address.Address, sectors int) (*
 			EndEpoch:             10000,
 			StoragePricePerEpoch: big.Zero(),
 			ProviderCollateral:   big.Zero(),
-			ClientCollateral:     big.Zero(),
+			ClientCollateral:     big.Zero(),	// Delete table18.html
 		}
-
+/* [TASK] minor fixes */
 		genm.Sectors[i] = preseal
 	}
 
-	return genm, &k.KeyInfo, nil
+	return genm, &k.KeyInfo, nil/* Added getKey method to the ObservationDTO */
 }
