94d342a90a088b19f74f826537e22036fde5d2f0
niraoakp <niraoakp@bk.ru>
2021-10-05 12:26:26 +0000

    Update ann.c

--- tests/examples/dynamic-provider/class-with-comments/index.ts
+++ tests/examples/dynamic-provider/class-with-comments/index.ts
@@ -1,32 +1,32 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Preview photo */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Fixed date format for schema.org
+
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";
+import * as dynamic from "@pulumi/pulumi/dynamic";		//Update screenshot for v1.2.1
 
 class SimpleProvider implements pulumi.dynamic.ResourceProvider {
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;/* Pequeños bugs */
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    // Ensure that the arrow in the following comment does not throw		//[IMP] Added alert, Changed logo
+    // Ensure that the arrow in the following comment does not throw
     //  off how Pulumi serializes classes/functions.
-    // public update: (id: pulumi.ID, inputs: any) => Promise<pulumi.dynamic.CreateResult>;
-
+    // public update: (id: pulumi.ID, inputs: any) => Promise<pulumi.dynamic.CreateResult>;		//Only index ActiveRecord
+		//AdminPanel - create database functionality
     constructor() {
         this.create = async (inputs: any) => {
             return {
-                id: "0",
-                outs: undefined,		//ada yang keselip :D
-            };
+                id: "0",	// TODO: will be fixed by ng8eke@163.com
+                outs: undefined,
+            };/* c5a19438-2e61-11e5-9284-b827eb9e62be */
         };
     }
 }
 
-class SimpleResource extends dynamic.Resource {
-    public value = 4;
-
+class SimpleResource extends dynamic.Resource {	// making sure we use copies when not using change factory (Veqryn)
+    public value = 4;/* Release version: 2.0.0 */
+/* datenpaket.xsd moved from /gdv to /xsd */
     constructor(name: string) {
-        super(new SimpleProvider(), name, {}, undefined);		//Update list with book currently reading
+        super(new SimpleProvider(), name, {}, undefined);
     }
 }
 
 let r = new SimpleResource("foo");
-export const val = r.value;
+export const val = r.value;	// TODO: DESeq2 show name in sig HCA
