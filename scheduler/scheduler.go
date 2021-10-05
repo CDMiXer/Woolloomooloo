6abddd1e8b9fecf9541894e6c7f1c6adb89a9228
niraoakp <niraoakp@bk.ru>
2021-10-05 12:28:01 +0000

    Added a note about the sector boundary collision failures.

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Ported pjsip to WinCE
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package scheduler
+package scheduler/* Release for v26.0.0. */
