deeb17e1785e0c8ca30f0923cecbd0375e2614ef
niraoakp <niraoakp@bk.ru>
2021-10-05 22:10:26 +0000

    Rename _parse_text to _deserialize for consistency.

--- handler/api/repos/collabs/remove.go
+++ handler/api/repos/collabs/remove.go
@@ -1,50 +1,50 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: Update pep8-1.pro
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//feb90618-2e46-11e5-9284-b827eb9e62be
+
 // +build !oss
-/* 1.1.2 Released */
+
 package collabs
-		//Add missing LICENSE file
+
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* Load kanji information on startup.  Release development version 0.3.2. */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-/* Merge "[INTERNAL][FIX] Table: Set correct header cell hover color" */
+
 	"github.com/go-chi/chi"
 )
 
-// HandleDelete returns an http.HandlerFunc that processes/* fix wrong variable reference error description */
+// HandleDelete returns an http.HandlerFunc that processes
 // a request to delete account membership to a repository. This should
 // only be used if the datastore is out-of-sync with github.
 func HandleDelete(
 	users core.UserStore,
 	repos core.RepositoryStore,
-	members core.PermStore,		//Add dumpme call
+	members core.PermStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Merge "[INTERNAL] Release notes for version 1.36.9" */
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			login     = chi.URLParam(r, "member")/* Merge branch 'master' into site-analysis */
+			login     = chi.URLParam(r, "member")
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)	// docs: update Notebooks
-/* Updated the compas_cgal feedstock. */
+		)
+
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
-			render.NotFound(w, err)/* Update performance-dedicated.md */
+			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name)./* Merge "Start using pre-prov creds in dsvm-layer4" */
+				WithField("name", name).
 				Debugln("api: repository not found")
 			return
 		}
 		user, err := users.FindLogin(r.Context(), login)
 		if err != nil {
-			render.NotFound(w, err)/* Fix variable check for cached docker access check */
-			logger.FromRequest(r)./* Changed debugger configuration and built in Release mode. */
+			render.NotFound(w, err)
+			logger.FromRequest(r).
 				WithError(err).
 				WithField("member", login).
 				WithField("namespace", namespace).
