a62ffc0c1418cd3a1a75c75ef8bc761a9c2471c7
niraoakp <niraoakp@bk.ru>
2021-10-05 11:44:56 +0000

    aact-646: Update schema diagrams

--- tests/integration/large_resource/python/__main__.py
+++ tests/integration/large_resource/python/__main__.py
@@ -1,7 +1,7 @@
-import pulumi
+import pulumi	// TODO: hacked by sebastian.tharakan97@gmail.com
 
 # Create a very long string (>4mb)
 long_string = "a" * 5 * 1024 * 1025
 
-# Export the name of the bucket
+# Export the name of the bucket/* security fix: found by @hamb and his friend */
 pulumi.export("long_string",  long_string)
