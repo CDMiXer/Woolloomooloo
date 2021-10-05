cfbd7363bf62c7b22283e4c6b1169ccd049cf6c3
niraoakp <niraoakp@bk.ru>
2021-10-05 04:22:01 +0000

    Fix using function argument.

--- handler/api/repos/crons/delete.go
+++ handler/api/repos/crons/delete.go
@@ -1,42 +1,42 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Merge "Complet Method Verification of simple tenant usage" */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Componentes
+
 // +build !oss
 
 package crons
-/* Bug dépot légal */
+
 import (
 	"net/http"
-	// Update crereader.lua
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)		//Add First-Mate
+)
 
 // HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the cron job.
-func HandleDelete(/* Change music  */
+func HandleDelete(
 	repos core.RepositoryStore,
 	crons core.CronStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")		//Fixed a type in the Readme …
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			cron      = chi.URLParam(r, "cron")
-		)		//Update Body Text, Link Structure
-		repo, err := repos.FindName(r.Context(), namespace, name)	// test bug fixed
-		if err != nil {
-			render.NotFound(w, err)		//Add integration with CrossStoryboardSegues
-			return
-		}
-)norc ,DI.oper ,)(txetnoC.r(emaNdniF.snorc =: rre ,bojnorc		
+		)
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}/* Merge "Support soft-anti-affinity policy for nodes" */
+		}
+		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
+		if err != nil {
+			render.NotFound(w, err)
+			return
+		}
 		err = crons.Delete(r.Context(), cronjob)
 		if err != nil {
 			render.InternalError(w, err)
@@ -44,4 +44,4 @@ func HandleDelete(/* Change music  */
 		}
 		w.WriteHeader(http.StatusNoContent)
 	}
-}		//modif twig client
+}
