e1ef21427de481d48bb67d601dc12c15a38450cb
niraoakp <niraoakp@bk.ru>
2021-10-05 10:11:23 +0000

    b14a824c-2e66-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/cancel.go
+++ handler/api/repos/builds/cancel.go
@@ -1,25 +1,25 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Added "Open Job".  */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: hacked by why@ipfs.io
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Botões adicionados na página clientes.jsp */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//6fd1971e-2d48-11e5-9c32-7831c1c36510
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: Delete bluesky.png
 
 package builds
-
+	// Add `AF_SAS`
 import (
 	"context"
-	"net/http"
-	"strconv"
+	"net/http"		//Refactoring help items management using Doctrine DBAL
+	"strconv"/* Set table margin */
 	"time"
-
+	// TODO: Automatic changelog generation for PR #22168 [ci skip]
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
@@ -27,19 +27,19 @@ import (
 	"github.com/go-chi/chi"
 )
 
-// HandleCancel returns an http.HandlerFunc that processes http
+// HandleCancel returns an http.HandlerFunc that processes http/* can delete image file */
 // requests to cancel a pending or running build.
 func HandleCancel(
 	users core.UserStore,
 	repos core.RepositoryStore,
-	builds core.BuildStore,
+	builds core.BuildStore,/* Release version [10.7.2] - alfter build */
 	stages core.StageStore,
 	steps core.StepStore,
 	status core.StatusService,
 	scheduler core.Scheduler,
 	webhooks core.WebhookSender,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {	// TODO: added zefen-bot and EthiopicWeb
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
@@ -49,7 +49,7 @@ func HandleCancel(
 		if err != nil {
 			render.BadRequest(w, err)
 			return
-		}
+		}/* Release plugin added */
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
@@ -58,11 +58,11 @@ func HandleCancel(
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
-			render.NotFound(w, err)
-			return
+			render.NotFound(w, err)	// TODO: removed aggregators
+			return	// TODO: Worklog #6073: Remove INSERT DELAYED: re-recorded perfschema on 64 bit.
 		}
 
-		build, err := builds.FindNumber(r.Context(), repo.ID, number)
+		build, err := builds.FindNumber(r.Context(), repo.ID, number)	// 251de148-2e4c-11e5-9284-b827eb9e62be
 		if err != nil {
 			logger.FromRequest(r).
 				WithError(err).
