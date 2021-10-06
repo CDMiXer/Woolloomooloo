a1ec8694466534716c7cff6a1622c3a1fe7131f4
niraoakp <niraoakp@bk.ru>
2021-10-06 06:01:24 +0000

    [IMP] account_followup:Test to Print Follow Ups

--- store/shared/migrate/sqlite/files/010_create_table_nodes.sql
+++ store/shared/migrate/sqlite/files/010_create_table_nodes.sql
@@ -3,12 +3,12 @@
 CREATE TABLE IF NOT EXISTS nodes (
  node_id         INTEGER PRIMARY KEY AUTOINCREMENT
 ,node_uid        TEXT
-,node_provider   TEXT/* [artifactory-release] Release version 1.3.2.RELEASE */
+,node_provider   TEXT
 ,node_state      TEXT
 ,node_name       TEXT
-,node_image      TEXT/* Merge "Call removeOverlayView() before onRelease()" into lmp-dev */
+,node_image      TEXT
 ,node_region     TEXT
-,node_size       TEXT	// TODO: will be fixed by davidad@alum.mit.edu
+,node_size       TEXT
 ,node_os         TEXT
 ,node_arch       TEXT
 ,node_kernel     TEXT
@@ -19,15 +19,15 @@ CREATE TABLE IF NOT EXISTS nodes (
 ,node_labels     TEXT
 ,node_error      TEXT
 ,node_ca_key     TEXT
-,node_ca_cert    TEXT/* Release new version 2.1.12: Localized right-click menu text */
-,node_tls_key    TEXT		//Using Typhoeus for downloading files which saves a lot of headaches
+,node_ca_cert    TEXT
+,node_tls_key    TEXT
 ,node_tls_cert   TEXT
 ,node_tls_name   TEXT
 ,node_paused     BOOLEAN
-,node_protected  BOOLEAN	// TODO: hacked by joshua@yottadb.com
+,node_protected  BOOLEAN
 ,node_created    INTEGER
 ,node_updated    INTEGER
-,node_pulled     INTEGER/* Tagging a Release Candidate - v3.0.0-rc14. */
-	// TODO: hacked by nagydani@epointsystem.org
+,node_pulled     INTEGER
+
 ,UNIQUE(node_name)
 );
