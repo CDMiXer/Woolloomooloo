5e3eb6cda0aa45ab695d8cc0b6633534455064ac
niraoakp <niraoakp@bk.ru>
2021-10-06 18:29:59 +0000

    refactor sort test

--- tests/integration/query/step1/index.ts
+++ tests/integration/query/step1/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by aeongrp@outlook.com
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* 3.6.1 Release */
 
 // Step 1: Create a simple resource graph.
 const a = new Resource("a", { state: 1 });
-const b = new Resource("b", { state: 2, resource: a });
+const b = new Resource("b", { state: 2, resource: a });	// TODO: will be fixed by sjors@sprovoost.nl
