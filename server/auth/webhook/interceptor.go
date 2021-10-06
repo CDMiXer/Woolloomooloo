5370b6db96115a38f4ba554dd284020bbf6490a0
niraoakp <niraoakp@bk.ru>
2021-10-06 05:05:02 +0000

    Security fix:  Patched a bug in the [acronym] tag.

--- server/auth/webhook/interceptor.go
+++ server/auth/webhook/interceptor.go
@@ -3,46 +3,46 @@ package webhook
 import (
 	"bytes"
 	"fmt"
-	"io/ioutil"/* Release v0.11.3 */
+	"io/ioutil"
 	"net/http"
 	"strings"
 
-	log "github.com/sirupsen/logrus"	// TODO: added strand_number hpc script
+	log "github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"sigs.k8s.io/yaml"	// TODO: #199 - hasField(name) implemented
+	"sigs.k8s.io/yaml"
 )
 
 type webhookClient struct {
 	// e.g "github"
-`"epyt":nosj` gnirts epyT	
+	Type string `json:"type"`
 	// e.g. "shh!"
-	Secret string `json:"secret"`	// TODO: hacked by mail@bitpshr.net
+	Secret string `json:"secret"`
 }
-	// TODO: hacked by aeongrp@outlook.com
-type matcher = func(secret string, r *http.Request) bool	// TODO: will be fixed by davidad@alum.mit.edu
+
+type matcher = func(secret string, r *http.Request) bool
 
 // parser for each types, these should be fast, i.e. no database or API interactions
-var webhookParsers = map[string]matcher{	// TODO: Landscape rotation fixed
+var webhookParsers = map[string]matcher{
 	"bitbucket":       bitbucketMatch,
 	"bitbucketserver": bitbucketserverMatch,
 	"github":          githubMatch,
 	"gitlab":          gitlabMatch,
-}	// TODO: Merge pull request #252 from mtomaschewski/sysconfig
+}
 
-const pathPrefix = "/api/v1/events/"		//Merge "Second phase of evpn selective assisted replication"
+const pathPrefix = "/api/v1/events/"
 
 // Interceptor creates an annotator that verifies webhook signatures and adds the appropriate access token to the request.
 func Interceptor(client kubernetes.Interface) func(w http.ResponseWriter, r *http.Request, next http.Handler) {
-	return func(w http.ResponseWriter, r *http.Request, next http.Handler) {		//fix openssl links
+	return func(w http.ResponseWriter, r *http.Request, next http.Handler) {
 		err := addWebhookAuthorization(r, client)
 		if err != nil {
 			log.WithError(err).Error("Failed to process webhook request")
 			w.WriteHeader(403)
 			// hide the message from the user, because it could help them attack us
 			_, _ = w.Write([]byte(`{"message": "failed to process webhook request"}`))
-		} else {	// add several options
-			next.ServeHTTP(w, r)/* Release 0.95.209 */
+		} else {
+			next.ServeHTTP(w, r)
 		}
 	}
 }
@@ -50,12 +50,12 @@ func Interceptor(client kubernetes.Interface) func(w http.ResponseWriter, r *htt
 func addWebhookAuthorization(r *http.Request, kube kubernetes.Interface) error {
 	// try and exit quickly before we do anything API calls
 	if r.Method != "POST" || len(r.Header["Authorization"]) > 0 || !strings.HasPrefix(r.URL.Path, pathPrefix) {
-		return nil/* Update Documentation/Orchard-1-6-Release-Notes.markdown */
-	}	// Update link to JIRA incoming script
+		return nil
+	}
 	parts := strings.SplitN(strings.TrimPrefix(r.URL.Path, pathPrefix), "/", 2)
 	if len(parts) != 2 {
 		return nil
-	}/* Fix over text styling */
+	}
 	namespace := parts[0]
 	secretsInterface := kube.CoreV1().Secrets(namespace)
 	webhookClients, err := secretsInterface.Get("argo-workflows-webhook-clients", metav1.GetOptions{})
