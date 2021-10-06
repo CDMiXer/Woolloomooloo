f771ec9e4c1af0904084080448ffe5f835d8ee9e
niraoakp <niraoakp@bk.ru>
2021-10-06 15:18:45 +0000

    Maximale Zeit für Antowrt einer KI implementiert

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Release history */
+
 "use strict";
 const pulumi = require("@pulumi/pulumi");
 
-const config = new pulumi.Config();	// TODO: will be fixed by martin2cai@hotmail.com
+const config = new pulumi.Config();
 const iterations = config.getNumber("count") || 1000;
 
 // Emit many, many diagnostic events from the engine to stress test the
-// ability to record those events on the Pulumi Service.		//Fixed markup bug
+// ability to record those events on the Pulumi Service.
 console.log("Starting to spam a bunch of diagnostic messages...");
 for (let i = 0; i < iterations; i++) {
     console.log(`${i}: The current time is ${new Date()}`);
 }
-console.log("done");/* Merge "Use setUp() method for neutron rest test" */
+console.log("done");
