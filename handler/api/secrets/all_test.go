da4ec8d6f489876ee43afa4af02d0d25b3f0499d
niraoakp <niraoakp@bk.ru>
2021-10-06 03:32:54 +0000

    Refactor initialization.

--- handler/api/secrets/all_test.go
+++ handler/api/secrets/all_test.go
@@ -1,55 +1,55 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//added bug fix for #273 mime-types runtime dependency
+
 // +build !oss
 
 package secrets
 
-import (/* Create .theanorc */
+import (
 	"encoding/json"
 	"net/http"
-	"net/http/httptest"
-	"testing"	// SNAP-58: fix workers concurent usage;
+	"net/http/httptest"/* Renamed event to match current API term */
+	"testing"
 
-	"github.com/drone/drone/core"	// Replace FactoryGirl with FactoryBot
-	"github.com/drone/drone/handler/api/errors"	// TODO: hacked by alan.shaw@protocol.ai
-	"github.com/drone/drone/mock"
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/mock"	// auch =|...| ist zulässig
 
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"		//Update changeling_power.dm
+	"github.com/google/go-cmp/cmp"
 )
 
-func TestHandleAll(t *testing.T) {
-	controller := gomock.NewController(t)		//- adding new FilterType
+func TestHandleAll(t *testing.T) {/* Release 1.4 (AdSearch added) */
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	secrets := mock.NewMockGlobalSecretStore(controller)
-	secrets.EXPECT().ListAll(gomock.Any()).Return(dummySecretList, nil)
+	secrets.EXPECT().ListAll(gomock.Any()).Return(dummySecretList, nil)/* Merge "cinder.backup: Replace 'locals()' with explicit values" */
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)/* c76fcc28-2e69-11e5-9284-b827eb9e62be */
-/* Merge "Release 1.0.0.174 QCACLD WLAN Driver" */
-	HandleAll(secrets).ServeHTTP(w, r)/* Readme. Fix markup. */
+	r := httptest.NewRequest("GET", "/", nil)
+
+	HandleAll(secrets).ServeHTTP(w, r)		//Rename servidor.cpp to server.cpp
 	if got, want := w.Code, http.StatusOK; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)/* 92c78334-2e65-11e5-9284-b827eb9e62be */
+		t.Errorf("Want response code %d, got %d", want, got)/* Release of eeacms/www-devel:19.8.6 */
 	}
-		//Moved the UI delegate classes to the new ui package.
-	got, want := []*core.Secret{}, dummySecretListScrubbed/* Rework some code for better php built-in web server support */
+/* Upgrade escodegen to version 1.9.1 */
+	got, want := []*core.Secret{}, dummySecretListScrubbed
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}/* Release version 4.5.1.3 */
+}		//67643d06-2fa5-11e5-a0e5-00012e3d3f12
 
 func TestHandleAll_SecretListErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	secrets := mock.NewMockGlobalSecretStore(controller)
-	secrets.EXPECT().ListAll(gomock.Any()).Return(nil, errors.ErrNotFound)		//[Documentation] [API] Remove wrong parameters definition
+	secrets.EXPECT().ListAll(gomock.Any()).Return(nil, errors.ErrNotFound)/* Fix typo in CMake */
 
-	w := httptest.NewRecorder()	// TODO: Removed step progress from navigation item title.
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 
 	HandleAll(secrets).ServeHTTP(w, r)
@@ -57,7 +57,7 @@ func TestHandleAll_SecretListErr(t *testing.T) {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := new(errors.Error), errors.ErrNotFound
+	got, want := new(errors.Error), errors.ErrNotFound		//testing #7
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
