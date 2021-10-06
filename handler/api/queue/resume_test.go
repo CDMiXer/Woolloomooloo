6cfd47fe8608474b2c648491ccafacf9ca8c4714
niraoakp <niraoakp@bk.ru>
2021-10-06 17:22:49 +0000

    gisclient 2.5 (mapserver 6)

--- handler/api/queue/resume_test.go
+++ handler/api/queue/resume_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* [1.1.15] Release */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Fixed a bug.Released V0.8.51. */
-// +build !oss	// TODO: hacked by sbrichards@gmail.com
+	// removed unused repo link
+// +build !oss
 
-package queue
+package queue/* Collect system hashes ... */
