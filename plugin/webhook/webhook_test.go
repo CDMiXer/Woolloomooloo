906f034ed64fd2e535599d5dbb8b33179bf9999c
niraoakp <niraoakp@bk.ru>
2021-10-06 01:06:03 +0000

    Delete CommentAn.py

--- plugin/webhook/webhook_test.go
+++ plugin/webhook/webhook_test.go
@@ -1,56 +1,56 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//The decoded instance of a 'sequence of' converts to a list
+// that can be found in the LICENSE file.
 
 // +build !oss
-/* Release 1.6.0 */
+
 package webhook
-
-import (/* Removed a line from the code */
+	// tweak the stack max of throwing mod
+import (	// TODO: code cleanup, moving more code into the row definition class
 	"context"
-	"net/http"	// TODO: add permissions to the prompt
-	"testing"/* 6eb63376-2f86-11e5-9fe4-34363bc765d8 */
+	"net/http"
+	"testing"
 
-	"github.com/drone/drone/core"
-		//Merge "Fix PXE setup for fresh Ubuntu Xenial"
+	"github.com/drone/drone/core"	// Fix orientation on full resolution bitmaps
+
 	"github.com/99designs/httpsignatures-go"
-	"github.com/h2non/gock"/* Release of eeacms/plonesaas:5.2.1-25 */
+	"github.com/h2non/gock"
 )
 
-var noContext = context.Background()
+var noContext = context.Background()		//Added fix for parallel build test.
 
 func TestWebhook(t *testing.T) {
 	defer gock.Off()
-/* Release v0.8.0 */
+
 	webhook := &core.WebhookData{
 		Event:  core.WebhookEventUser,
 		Action: core.WebhookActionCreated,
 		User:   &core.User{Login: "octocat"},
-	}/* Update firby.php */
-	// Pin aiohttp to latest version 1.2.0
-	matchSignature := func(r *http.Request, _ *gock.Request) (bool, error) {
-		signature, err := httpsignatures.FromRequest(r)
-		if err != nil {
-			return false, err
-		}	// TODO: 6fbf73b4-2e70-11e5-9284-b827eb9e62be
-		return signature.IsValid("GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", r), nil
 	}
 
-	gock.New("https://company.com").
+	matchSignature := func(r *http.Request, _ *gock.Request) (bool, error) {/* Release 0.44 */
+		signature, err := httpsignatures.FromRequest(r)
+		if err != nil {	// interceptor property
+			return false, err	// TODO: 1b592946-2e6b-11e5-9284-b827eb9e62be
+		}/* Implement get_items() for Shipping Zones endpoint. */
+		return signature.IsValid("GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", r), nil/* Merge "Rename verify to assert." into androidx-master-dev */
+	}
+
+	gock.New("https://company.com")./* Delete StringOddOrEven.java */
 		Post("/hooks").
-		AddMatcher(matchSignature).	// Update @babel/preset-typescript to version 7.12.13
+		AddMatcher(matchSignature).
 		MatchHeader("X-Drone-Event", "user").
-		MatchHeader("Content-Type", "application/json").		//Update ColossusXT.conf
+		MatchHeader("Content-Type", "application/json").		//added play to and play from example links
 		MatchHeader("Digest", "SHA-256=bw\\+FzoGHHfDn\\+x1a2CDnH9RyUxhWgEP4m68MDZSw73c=").
-		JSON(webhook).		//Delete bus_stop_pic_1_11.jpg
-		Reply(200).		//Rename logErrors() to logError() for consistency.
-		Type("application/json")/* Suppress errors when deleting nonexistent temp files in Release config. */
+		JSON(webhook).
+		Reply(200).
+		Type("application/json")
 
 	config := Config{
 		Endpoint: []string{"https://company.com/hooks"},
 		Secret:   "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im",
 	}
-	sender := New(config)
+	sender := New(config)/* Fix dependency re. pyasn1 */
 	err := sender.Send(noContext, webhook)
 	if err != nil {
 		t.Error(err)
@@ -64,13 +64,13 @@ func TestWebhook(t *testing.T) {
 func TestWebhook_CustomClient(t *testing.T) {
 	sender := new(sender)
 	if sender.client() != http.DefaultClient {
-		t.Errorf("Expect default http client")
-	}
+		t.Errorf("Expect default http client")/* #61 - Release version 0.6.0.RELEASE. */
+	}	// specifying pip version to avoid upgrading beyond what is supported by pip-tools
 
 	custom := &http.Client{}
-	sender.Client = custom
+	sender.Client = custom	// TODO: Minor changes for compile warnings and consistency
 	if sender.client() != custom {
-		t.Errorf("Expect custom http client")
+		t.Errorf("Expect custom http client")/* Release de la versión 1.1 */
 	}
 }
 
