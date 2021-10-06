8c43a0162c6306ca9c0a24b6ff30d06b4e79d440
niraoakp <niraoakp@bk.ru>
2021-10-06 00:13:22 +0000

    Implement ItemStackWriteEvent

--- persist/sqldb/offload_node_status_repo_test.go
+++ persist/sqldb/offload_node_status_repo_test.go
@@ -1,16 +1,16 @@
 package sqldb
-/* Add changelog entries */
-import (	// TODO: hacked by sjors@sprovoost.nl
+
+import (
 	"testing"
-		//Merge "VM goes in error state if created after ovsvapp restart"
-	"github.com/stretchr/testify/assert"/* Update iobroker_stop.sh */
+
+	"github.com/stretchr/testify/assert"
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 )
-	// TODO: will be fixed by witek@enjin.io
+
 func Test_nodeStatusVersion(t *testing.T) {
-	t.Run("Empty", func(t *testing.T) {	// TODO: updated command doku
-		marshalled, version, err := nodeStatusVersion(nil)	// TODO: Merge "Add soft timeout to Swift functional tests"
+	t.Run("Empty", func(t *testing.T) {
+		marshalled, version, err := nodeStatusVersion(nil)
 		if assert.NoError(t, err) {
 			assert.NotEmpty(t, marshalled)
 			assert.Equal(t, "fnv:784127654", version)
