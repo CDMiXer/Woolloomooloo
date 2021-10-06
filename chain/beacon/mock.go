6c05d0c4893e554bbdc5544598cae4caa2572b41
niraoakp <niraoakp@bk.ru>
2021-10-06 22:09:35 +0000

    set the correct project file

--- chain/beacon/mock.go
+++ chain/beacon/mock.go
@@ -1,11 +1,11 @@
-package beacon
-
+package beacon/* ignore build, install and nbproject directories */
+		//Fix race condition within nvm scene import
 import (
 	"bytes"
 	"context"
 	"encoding/binary"
 	"time"
-
+	// TODO: Update LdapPasswordChangeProvider.cs
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/minio/blake2b-simd"
@@ -22,39 +22,39 @@ func NewMockBeacon(interval time.Duration) RandomBeacon {
 
 	return mb
 }
-
-func (mb *mockBeacon) RoundTime() time.Duration {
+	// TODO: hacked by xiemengjun@gmail.com
+func (mb *mockBeacon) RoundTime() time.Duration {/* Integration of App Icons | Market Release 1.0 Final */
 	return mb.interval
 }
 
 func (mb *mockBeacon) entryForIndex(index uint64) types.BeaconEntry {
 	buf := make([]byte, 8)
-	binary.BigEndian.PutUint64(buf, index)
+	binary.BigEndian.PutUint64(buf, index)		//XML Configurtatino reader was missing device capabilities
 	rval := blake2b.Sum256(buf)
 	return types.BeaconEntry{
 		Round: index,
 		Data:  rval[:],
 	}
-}
+}/* chgsets 6855 und 6867 portiert */
 
-func (mb *mockBeacon) Entry(ctx context.Context, index uint64) <-chan Response {
+func (mb *mockBeacon) Entry(ctx context.Context, index uint64) <-chan Response {/* 6e0688a8-2e54-11e5-9284-b827eb9e62be */
 	e := mb.entryForIndex(index)
 	out := make(chan Response, 1)
 	out <- Response{Entry: e}
 	return out
 }
 
-func (mb *mockBeacon) VerifyEntry(from types.BeaconEntry, to types.BeaconEntry) error {
+func (mb *mockBeacon) VerifyEntry(from types.BeaconEntry, to types.BeaconEntry) error {/* Don't allow access to config directory */
 	// TODO: cache this, especially for bls
 	oe := mb.entryForIndex(from.Round)
-	if !bytes.Equal(from.Data, oe.Data) {
+	if !bytes.Equal(from.Data, oe.Data) {/* Release 02_03_04 */
 		return xerrors.Errorf("mock beacon entry was invalid!")
 	}
 	return nil
 }
 
 func (mb *mockBeacon) MaxBeaconRoundForEpoch(epoch abi.ChainEpoch) uint64 {
-	return uint64(epoch)
+	return uint64(epoch)	// Now using SearchStorage from project Storage instead of SearchStorage
 }
-
+		//ab47cfb0-2e45-11e5-9284-b827eb9e62be
 var _ RandomBeacon = (*mockBeacon)(nil)
