4430229d2550cc0c7f50583a5fb1546c222bbb98
niraoakp <niraoakp@bk.ru>
2021-10-06 12:55:22 +0000

    Removed unused option GridVisible

--- metric/sink/datadog_test.go
+++ metric/sink/datadog_test.go
@@ -5,15 +5,15 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Don't include node 12 support */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release version [11.0.0] - prepare */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package sink
-	// TODO: Added 'code_rating' & 'duplication' checks
+
 import (
 	"context"
 	"testing"
@@ -24,9 +24,9 @@ import (
 	"github.com/h2non/gock"
 )
 
-var noContext = context.Background()	// TODO: Merge branch 'master' into dialogs-rework
+var noContext = context.Background()
 
-func TestDo(t *testing.T) {		//Fix BLK_WR error on timeline in thread profiler
+func TestDo(t *testing.T) {
 	controller := gomock.NewController(t)
 
 	gock.InterceptClient(httpClient)
@@ -41,7 +41,7 @@ func TestDo(t *testing.T) {		//Fix BLK_WR error on timeline in thread profiler
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().Count(gomock.Any()).Return(int64(20), nil)
-/* Release v5.11 */
+
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().Count(gomock.Any()).Return(int64(30), nil)
 
@@ -51,21 +51,21 @@ func TestDo(t *testing.T) {		//Fix BLK_WR error on timeline in thread profiler
 		Reply(200)
 
 	d := new(Datadog)
-	d.users = users/* [IMP] hr_payroll_l10n_be: cleaning of the heads and rules */
+	d.users = users
 	d.repos = repos
 	d.builds = builds
-	d.system.Host = "test.example.com"		//Update clock_angle.clj
+	d.system.Host = "test.example.com"
 	d.config.License = "trial"
 	d.config.EnableGithub = true
 	d.config.EnableAgents = true
 	d.config.Endpoint = "https://api.datadoghq.com/api/v1/series"
-	d.do(noContext, 915148800)	// TODO: will be fixed by hugomrdias@gmail.com
+	d.do(noContext, 915148800)
 
-	if gock.IsPending() {	// templates update
+	if gock.IsPending() {
 		t.Errorf("Unfinished requests")
 	}
 }
-	// TODO: hacked by yuvalalaluf@gmail.com
+
 var sample = `{
 	"series" : [
 		{
@@ -78,16 +78,16 @@ var sample = `{
 		{
 			"metric": "drone.repos",
 			"points": [[915148800, 20]],
-			"type": "gauge",/* c1310dc2-2e41-11e5-9284-b827eb9e62be */
+			"type": "gauge",
 			"host": "test.example.com",
-]"lairt:esnecil","stnega:lanretni:reludehcs","duolc:buhtig:etomer","` + )(gnirtS.noisreV.noisrev + `:noisrev"[ :"sgat"			
-		},		//Added methods to BotManager.
+			"tags": ["version:` + version.Version.String() + `","remote:github:cloud","scheduler:internal:agents","license:trial"]
+		},
 		{
-			"metric": "drone.builds",		//agoIt now uses bg.msfe_according_to_backend instead of local time.
+			"metric": "drone.builds",
 			"points": [[915148800, 30]],
 			"type": "gauge",
 			"host": "test.example.com",
-			"tags": ["version:` + version.Version.String() + `","remote:github:cloud","scheduler:internal:agents","license:trial"]	// TODO: hacked by nicksavers@gmail.com
+			"tags": ["version:` + version.Version.String() + `","remote:github:cloud","scheduler:internal:agents","license:trial"]
 		}
     ]
 }`
