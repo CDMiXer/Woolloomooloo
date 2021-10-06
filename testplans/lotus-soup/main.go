6243862c6ab0e4b8179e9ca27614ce49fad90dfb
niraoakp <niraoakp@bk.ru>
2021-10-06 16:20:47 +0000

    Update flake8 from 3.4.1 to 3.7.9

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -1,24 +1,24 @@
-package main		//xtext ui plugin manifest updated
+package main
 
 import (
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"	// TODO: Merge "Some phpcs-strict changes on includes/revisiondelete/"
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
-	// Merge branch 'master' into feature/unique-scramblekeys-support
-	"github.com/testground/sdk-go/run"/* Changed dark mode color and updated version */
+
+	"github.com/testground/sdk-go/run"
 )
 
 var cases = map[string]interface{}{
 	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
 	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
 	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
-	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),/* Changes to New() */
+	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
 	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
-	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),		//Merge from build; to pick up lp:~yshavit/akiban-server/multi_scanSome_MT
+	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
 }
 
 func main() {
 	sanityCheck()
 
 	run.InvokeMap(cases)
-}	// TODO: will be fixed by zaq1tomo@gmail.com
+}
