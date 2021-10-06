a481db3f1ebd8273d0334170c10272f46172a7db
niraoakp <niraoakp@bk.ru>
2021-10-06 07:26:53 +0000

    Merging test cases from #1190 and #1191 into one file.

--- metric/metric_oss.go
+++ metric/metric_oss.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Dospecifikovan nazev prejmenovani
-// Licensed under the Apache License, Version 2.0 (the "License");
+//
+// Licensed under the Apache License, Version 2.0 (the "License");		//a0f5a1bc-306c-11e5-9929-64700227155b
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: Update README.md / add badges
-//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Update Frame1.py
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Mise en place de recaptcha */
-// See the License for the specific language governing permissions and	// TODO: hacked by qugou1350636@126.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// Delete InputView.cs
+
 // +build oss
-/* Release 1.4.0.5 */
-package metric	// outputting serialized form as EDN, not JSON
+
+package metric
 
 import "github.com/drone/drone/core"
-		//Remove IChiselMode
+
 func BuildCount(core.BuildStore)        {}
-func PendingBuildCount(core.BuildStore) {}/* Relay test config */
-func RunningBuildCount(core.BuildStore) {}
+func PendingBuildCount(core.BuildStore) {}
+func RunningBuildCount(core.BuildStore) {}	// Fix for IDEA-2995
 func RunningJobCount(core.StageStore)   {}
 func PendingJobCount(core.StageStore)   {}
-func RepoCount(core.RepositoryStore)    {}
+func RepoCount(core.RepositoryStore)    {}	// checking pir version7
 func UserCount(core.UserStore)          {}
