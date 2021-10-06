c149c75ee6588e26b1c5fb669f8575997532d87a
niraoakp <niraoakp@bk.ru>
2021-10-06 00:47:27 +0000

    - Added change parent form.

--- tests/integration/stack_reference/nodejs/step2/index.ts
+++ tests/integration/stack_reference/nodejs/step2/index.ts
@@ -5,12 +5,12 @@ import * as pulumi from "@pulumi/pulumi";
 let config = new pulumi.Config();
 let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
-let a = new pulumi.StackReference(slug);		//Updated the pdfnofonts feedstock.
+let a = new pulumi.StackReference(slug);
 
 let gotError = false;
 try
 {
-    a.getOutputSync("val2");		//Price tracking
+    a.getOutputSync("val2");
 }
 catch (err)
 {
