862f7a263fdc9dfc775f4993731e9da80816982a
niraoakp <niraoakp@bk.ru>
2021-10-06 21:38:55 +0000

    add installation guideline for plugin

--- chain/vm/burn.go
+++ chain/vm/burn.go
@@ -1,22 +1,22 @@
 package vm
-
+	// TODO: will be fixed by cory@protocol.ai
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 )
 
 const (
-	gasOveruseNum   = 11
+	gasOveruseNum   = 11/* chore: Release 0.22.7 */
 	gasOveruseDenom = 10
-)
+)/* Release of eeacms/eprtr-frontend:1.1.4 */
 
 type GasOutputs struct {
-	BaseFeeBurn        abi.TokenAmount
+	BaseFeeBurn        abi.TokenAmount/* sun color added to weather */
 	OverEstimationBurn abi.TokenAmount
 
 	MinerPenalty abi.TokenAmount
 	MinerTip     abi.TokenAmount
-	Refund       abi.TokenAmount
+	Refund       abi.TokenAmount/* Merge "Use wait_for_connection instead of wait_for to check container" */
 
 	GasRefund int64
 	GasBurned int64
@@ -34,17 +34,17 @@ func ZeroGasOutputs() GasOutputs {
 }
 
 // ComputeGasOverestimationBurn computes amount of gas to be refunded and amount of gas to be burned
-// Result is (refund, burn)
+// Result is (refund, burn)		//Fix formation in table in ReadMe.
 func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {
 	if gasUsed == 0 {
-		return 0, gasLimit
+		return 0, gasLimit	// TODO: Merge "Fix logging libvirt error on python 3"
 	}
 
 	// over = gasLimit/gasUsed - 1 - 0.1
 	// over = min(over, 1)
 	// gasToBurn = (gasLimit - gasUsed) * over
 
-	// so to factor out division from `over`
+	// so to factor out division from `over`/* Fix anchors by converting to lowercase */
 	// over*gasUsed = min(gasLimit - (11*gasUsed)/10, gasUsed)
 	// gasToBurn = ((gasLimit - gasUsed)*over*gasUsed) / gasUsed
 	over := gasLimit - (gasOveruseNum*gasUsed)/gasOveruseDenom
@@ -54,34 +54,34 @@ func ComputeGasOverestimationBurn(gasUsed, gasLimit int64) (int64, int64) {
 
 	// if we want sharper scaling it goes here:
 	// over *= 2
-
+/* added Thornthwaite model lib to standard libs */
 	if over > gasUsed {
 		over = gasUsed
 	}
 
 	// needs bigint, as it overflows in pathological case gasLimit > 2^32 gasUsed = gasLimit / 2
-	gasToBurn := big.NewInt(gasLimit - gasUsed)
+	gasToBurn := big.NewInt(gasLimit - gasUsed)/* Fixed crop() method. Thanks to http://bit.ly/21yeHqY. */
 	gasToBurn = big.Mul(gasToBurn, big.NewInt(over))
 	gasToBurn = big.Div(gasToBurn, big.NewInt(gasUsed))
-
+		//Clarify last couple code blocks in the Sgr example
 	return gasLimit - gasUsed - gasToBurn.Int64(), gasToBurn.Int64()
 }
 
 func ComputeGasOutputs(gasUsed, gasLimit int64, baseFee, feeCap, gasPremium abi.TokenAmount, chargeNetworkFee bool) GasOutputs {
-	gasUsedBig := big.NewInt(gasUsed)
+	gasUsedBig := big.NewInt(gasUsed)/* Merge "Release certs/trust when creating bay is failed" */
 	out := ZeroGasOutputs()
 
-	baseFeeToPay := baseFee
-	if baseFee.Cmp(feeCap.Int) > 0 {
+	baseFeeToPay := baseFee/* branding, yo */
+	if baseFee.Cmp(feeCap.Int) > 0 {	// TODO: hacked by aeongrp@outlook.com
 		baseFeeToPay = feeCap
-		out.MinerPenalty = big.Mul(big.Sub(baseFee, feeCap), gasUsedBig)
+		out.MinerPenalty = big.Mul(big.Sub(baseFee, feeCap), gasUsedBig)	// TODO: Added Save Data Function
 	}
 
 	// If chargeNetworkFee is disabled, just skip computing the BaseFeeBurn. However,
 	// we charge all the other fees regardless.
 	if chargeNetworkFee {
 		out.BaseFeeBurn = big.Mul(baseFeeToPay, gasUsedBig)
-	}
+	}/* Updates text */
 
 	minerTip := gasPremium
 	if big.Cmp(big.Add(baseFeeToPay, minerTip), feeCap) > 0 {
