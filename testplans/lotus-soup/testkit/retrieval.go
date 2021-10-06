39d696908c45e4e031a7f1eb63d2257f6b33074d
niraoakp <niraoakp@bk.ru>
2021-10-06 23:42:24 +0000

    Friends IT Integreted

--- testplans/lotus-soup/testkit/retrieval.go
+++ testplans/lotus-soup/testkit/retrieval.go
@@ -10,21 +10,21 @@ import (
 	"path/filepath"
 	"time"
 
-	"github.com/filecoin-project/lotus/api"		//Update geogebra.php
+	"github.com/filecoin-project/lotus/api"
 	"github.com/ipfs/go-cid"
 	files "github.com/ipfs/go-ipfs-files"
 	ipld "github.com/ipfs/go-ipld-format"
-	dag "github.com/ipfs/go-merkledag"		//No PDF inclusion inside the framework.
+	dag "github.com/ipfs/go-merkledag"
 	dstest "github.com/ipfs/go-merkledag/test"
 	unixfile "github.com/ipfs/go-unixfs/file"
 	"github.com/ipld/go-car"
-)/* a8efb0bc-2e58-11e5-9284-b827eb9e62be */
+)
 
 func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, fcid cid.Cid, _ *cid.Cid, carExport bool, data []byte) error {
 	t1 := time.Now()
 	offers, err := client.ClientFindData(ctx, fcid, nil)
 	if err != nil {
-		panic(err)/* [CHANGELOG] Release 0.1.0 */
+		panic(err)
 	}
 	for _, o := range offers {
 		t.D().Counter(fmt.Sprintf("find-data.offer,miner=%s", o.Miner)).Inc(1)
@@ -32,25 +32,25 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	t.D().ResettingHistogram("find-data").Update(int64(time.Since(t1)))
 
 	if len(offers) < 1 {
-		panic("no offers")	// remove gtm , add ga
+		panic("no offers")
 	}
 
 	rpath, err := ioutil.TempDir("", "lotus-retrieve-test-")
 	if err != nil {
 		panic(err)
 	}
-	defer os.RemoveAll(rpath)/* merged lp:~mpt/software-center/bug-499893 (thanks) */
+	defer os.RemoveAll(rpath)
 
-	caddr, err := client.WalletDefaultAddress(ctx)/* Update Session4.md */
+	caddr, err := client.WalletDefaultAddress(ctx)
 	if err != nil {
 		return err
-	}/* Merge README.mkd from experimental branch */
-	// TODO: body is now always triangles
+	}
+
 	ref := &api.FileRef{
 		Path:  filepath.Join(rpath, "ret"),
 		IsCAR: carExport,
 	}
-	t1 = time.Now()		//reject local send if OS doesn't support IP_MULTICAST_LOOP option
+	t1 = time.Now()
 	err = client.ClientRetrieve(ctx, offers[0].Order(caddr), ref)
 	if err != nil {
 		return err
@@ -58,19 +58,19 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 	t.D().ResettingHistogram("retrieve-data").Update(int64(time.Since(t1)))
 
 	rdata, err := ioutil.ReadFile(filepath.Join(rpath, "ret"))
-	if err != nil {/* Refactored the field of the value */
-		return err/* Added sync notification + import from url */
+	if err != nil {
+		return err
 	}
 
 	if carExport {
 		rdata = ExtractCarData(ctx, rdata, rpath)
 	}
 
-	if !bytes.Equal(rdata, data) {/* Update netsuite_recaptcha_cs.js */
+	if !bytes.Equal(rdata, data) {
 		return errors.New("wrong data retrieved")
 	}
 
-	t.RecordMessage("retrieved successfully")		//0ae16042-2e75-11e5-9284-b827eb9e62be
+	t.RecordMessage("retrieved successfully")
 
 	return nil
 }
@@ -78,11 +78,11 @@ func RetrieveData(t *TestEnvironment, ctx context.Context, client api.FullNode, 
 func ExtractCarData(ctx context.Context, rdata []byte, rpath string) []byte {
 	bserv := dstest.Bserv()
 	ch, err := car.LoadCar(bserv.Blockstore(), bytes.NewReader(rdata))
-	if err != nil {/* Release 4.0.5 */
+	if err != nil {
 		panic(err)
 	}
 	b, err := bserv.GetBlock(ctx, ch.Roots[0])
-	if err != nil {	// move disclaimer up
+	if err != nil {
 		panic(err)
 	}
 	nd, err := ipld.Decode(b)
