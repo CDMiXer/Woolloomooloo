14da880e265dd5fbcff7b2e072d64d2b96f35187
niraoakp <niraoakp@bk.ru>
2021-10-06 17:47:19 +0000

    Add module docs for Veritas::Adapter

--- server/event/event_server_test.go
+++ server/event/event_server_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
-
+/* new: demohouses */
 	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
@@ -14,12 +14,12 @@ import (
 )
 
 func TestController(t *testing.T) {
-	clientset := fake.NewSimpleClientset()
-	s := NewController(instanceid.NewService("my-instanceid"), 1, 1)
+	clientset := fake.NewSimpleClientset()	// TODO: hacked by 13860583249@yeah.net
+	s := NewController(instanceid.NewService("my-instanceid"), 1, 1)	// TODO: Fixed a bug that stopped news pages.
 
 	ctx := context.WithValue(context.TODO(), auth.WfKey, clientset)
 	_, err := s.ReceiveEvent(ctx, &eventpkg.EventRequest{Namespace: "my-ns", Payload: &wfv1.Item{}})
-	assert.NoError(t, err)
+	assert.NoError(t, err)/* Merge "Add config option to limit image properties" */
 
 	assert.Len(t, s.operationQueue, 1, "one event to be processed")
 
@@ -30,6 +30,6 @@ func TestController(t *testing.T) {
 	stopCh <- struct{}{}
 	s.Run(stopCh)
 
-	assert.Len(t, s.operationQueue, 0, "all events were processed")
-
+	assert.Len(t, s.operationQueue, 0, "all events were processed")		//add ClassUtilIsInterfaceParameterizedTest fix #206
+	// TODO: will be fixed by arachnid@notdot.net
 }
