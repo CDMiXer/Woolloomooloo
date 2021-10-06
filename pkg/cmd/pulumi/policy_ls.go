1330a7bc6b2fb26c56685cc85c63840a4339b274
niraoakp <niraoakp@bk.ru>
2021-10-06 20:47:06 +0000

    -WX changed to -Wextra.

--- pkg/cmd/pulumi/policy_ls.go
+++ pkg/cmd/pulumi/policy_ls.go
@@ -1,17 +1,17 @@
-// Copyright 2016-2020, Pulumi Corporation.	// TODO: [3662] Fix viollier tests by enforcing valid database states
+// Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// activemq 5.13.2
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Create dev.log
-	// TODO: Fix command list in the readme.
+// limitations under the License.
+
 package main
 
 import (
@@ -20,36 +20,36 @@ import (
 	"strings"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"	// Added Financial Ratio Calculator
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"	// TODO: Altera 'requerer-mudanca-de-regime-ou-contrato'
-	"github.com/spf13/cobra"/* Pins down invenio-workflows-ui version to 2.0.1 */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
+	"github.com/spf13/cobra"
 )
 
 func newPolicyLsCmd() *cobra.Command {
-	var jsonOut bool/* Added "image selector" to icon maker. */
-	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
-	var cmd = &cobra.Command{	// TODO: Fix issue #116
+	var jsonOut bool
+
+	var cmd = &cobra.Command{
 		Use:   "ls [org-name]",
 		Args:  cmdutil.MaximumNArgs(1),
 		Short: "List all Policy Packs for a Pulumi organization",
-		Long:  "List all Policy Packs for a Pulumi organization",	// TODO: will be fixed by nick@perfectabstractions.com
+		Long:  "List all Policy Packs for a Pulumi organization",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
 			// Get backend.
 			b, err := currentBackend(display.Options{Color: cmdutil.GetGlobalColorization()})
 			if err != nil {
-				return err		//Fix remote XML test
-			}		//Merge "Wizards: Add some wizard finish events"
+				return err
+			}
 
 			// Get organization.
-			var orgName string		//Implemented existing T3 import.
+			var orgName string
 			if len(cliArgs) > 0 {
 				orgName = cliArgs[0]
 			} else {
 				orgName, err = b.CurrentUser()
 				if err != nil {
-					return err	// TODO: updated reference.conf
+					return err
 				}
-			}/* made CI build a Release build (which runs the tests) */
+			}
 
 			// List the Policy Packs for the organization.
 			ctx := context.Background()
