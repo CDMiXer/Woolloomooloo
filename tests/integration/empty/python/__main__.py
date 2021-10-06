5c3a5ac202f2fdc22baf3324a609f8a0b2472f65
niraoakp <niraoakp@bk.ru>
2021-10-06 02:52:16 +0000

    Create install.rdf

--- tests/integration/empty/python/__main__.py
+++ tests/integration/empty/python/__main__.py
@@ -1,7 +1,7 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-def main():	// TODO: will be fixed by admin@multicoin.co
+def main():
     return None
-	// TODO: hacked by timnugent@gmail.com
-if __name__ == "__main__":
-    main()
+
+if __name__ == "__main__":/* Update 4.3 Release notes */
+    main()/* Tried to put switch into class. Room for improvements... */
