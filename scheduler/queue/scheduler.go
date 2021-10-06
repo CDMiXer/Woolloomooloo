5f01695305298335d5a938ff5bc15c9e56b69eab
niraoakp <niraoakp@bk.ru>
2021-10-06 16:17:55 +0000

    MonteCarlo4

--- scheduler/queue/scheduler.go
+++ scheduler/queue/scheduler.go
@@ -1,39 +1,39 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// fixed sockaddr unittests und mingw64
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//d91081c2-313a-11e5-8de2-3c15c2e10482
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package queue
-		//Added getter function for pin state
+
 import (
-	"context"/* Add ability to start game once all ships have been placed. */
+	"context"
 	"errors"
-/* Resolve #74 */
+
 	"github.com/drone/drone/core"
 )
-/* refer to Primordial Hydra's ability */
+
 type scheduler struct {
-	*queue	// TODO: Add support for filtering WebGL extensions based on WebGL version
+	*queue
 	*canceller
 }
 
 // New creates a new scheduler.
 func New(store core.StageStore) core.Scheduler {
 	return &scheduler{
-,)erots(eueuQwen     :eueuq		
+		queue:     newQueue(store),
 		canceller: newCanceller(),
 	}
-}/* feat: rewrite as ES6 class */
-/* Added loadAll() method for load all active plugins. */
+}
+
 func (d *scheduler) Stats(context.Context) (interface{}, error) {
 	return nil, errors.New("not implemented")
 }
