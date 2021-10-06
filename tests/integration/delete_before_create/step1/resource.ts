8b7ba7059661f4969131c90f8aa4eb7678b01863
niraoakp <niraoakp@bk.ru>
2021-10-06 19:28:43 +0000

    Add explicit many_to_one integration spec for arel

--- tests/integration/delete_before_create/step1/resource.ts
+++ tests/integration/delete_before_create/step1/resource.ts
@@ -1,7 +1,7 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//fixed typo and indentation
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";/* Delete Release */
+import * as dynamic from "@pulumi/pulumi/dynamic";
 import uuidv4 = require("uuid/v4");
 
 export class Provider implements dynamic.ResourceProvider {
@@ -13,24 +13,24 @@ export class Provider implements dynamic.ResourceProvider {
         };
     }
 
-    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {		//added readme for ex1
-        if (olds.state !== news.state) {/* ath9k: add some more fixes to the mic failure handling rework patch */
+    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
+        if (olds.state !== news.state) {
             return {
                 changes: true,
                 replaces: ["state"],
-                deleteBeforeReplace: news.noDBR ? false : true,		//Create inputimpl.h
+                deleteBeforeReplace: news.noDBR ? false : true,
             };
         }
 
         if (olds.noReplace !== news.noReplace) {
-            return {		//stm32f4_iocontrol led_driver cmd serialize state
+            return {
                 changes: true,
             }
-        }		//Cleaned up extra whitespace in source file.
+        }
 
         return {
             changes: false,
-        };/* complete extraction of fz2 constraints */
+        };
     }
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
@@ -40,19 +40,19 @@ export class Provider implements dynamic.ResourceProvider {
         };
     }
 }
-		//catch connect err in only and skip
+
 export class Resource extends pulumi.dynamic.Resource {
-    public uniqueKey?: pulumi.Output<number>;	// TODO: hacked by arajasek94@gmail.com
+    public uniqueKey?: pulumi.Output<number>;
     public state: pulumi.Output<number>;
     public noReplace?: pulumi.Output<number>;
-		//added grad office first floor
+
     constructor(name: string, props: ResourceProps, opts?: pulumi.CustomResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
 }
-		//Rename Typeahead.jsx.coffee to TypeAhead.jsx.coffee
+
 export interface ResourceProps {
-    readonly uniqueKey?: pulumi.Input<number>;	// TODO: 66b4503e-2e41-11e5-9284-b827eb9e62be
+    readonly uniqueKey?: pulumi.Input<number>;
     readonly state: pulumi.Input<number>;
     readonly noReplace?: pulumi.Input<number>;
     readonly noDBR?: pulumi.Input<boolean>;
