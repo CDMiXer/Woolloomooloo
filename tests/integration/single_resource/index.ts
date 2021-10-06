6c5a336b204e5b851b32438726fd757a4c7ce586
niraoakp <niraoakp@bk.ru>
2021-10-06 06:47:53 +0000

    Fix logic to disallow phrases with same title

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by remco@dutchcoders.io
 
 import { Resource } from "./resource";
-/* [FIX] XQuery Update: nested copy expressions */
-// Allocate a new resource. When this exists, we should not allow
+
+// Allocate a new resource. When this exists, we should not allow/* Release of eeacms/forests-frontend:1.5.5 */
 // the stack holding it to be `rm`'d without `--force`.
 let a = new Resource("res", { state: 1 });
-
+		//Document gdb and other requirements in pt-pmp and pt-stalk.
 export let o = a.state;
