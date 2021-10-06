2e1361617c0a39df310a28831d06241627c8cff3
niraoakp <niraoakp@bk.ru>
2021-10-06 18:05:43 +0000

    include resistant and susceptible intraclass in averages

--- tests/integration/duplicate_urns/duplicate_urns_test.go
+++ tests/integration/duplicate_urns/duplicate_urns_test.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.
 // +build nodejs all
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//Update bottom from 2.1.2 to 2.1.3
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -9,25 +9,25 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Release 1.0.0.91 QCACLD WLAN Driver" */
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Update AccountDetail */
 
 package ints
 
 import (
-	"testing"
+	"testing"		//Fix up calls to dctl and log to accomodate removal of pthread specific
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-// Test that the engine does not tolerate duplicate URNs in the same plan.
+// Test that the engine does not tolerate duplicate URNs in the same plan.	// TODO: consistency of output for bluetooth sample app
 func TestDuplicateURNs(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:           "step1",
+		Dir:           "step1",		//include version_helper.h in sdist
 		Dependencies:  []string{"@pulumi/pulumi"},
-		Quick:         true,
-		ExpectFailure: true,
+		Quick:         true,	// TODO: hacked by ng8eke@163.com
+		ExpectFailure: true,/* Release jedipus-2.6.14 */
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
@@ -42,7 +42,7 @@ func TestDuplicateURNs(t *testing.T) {
 				Dir:           "step4",
 				Additive:      true,
 				ExpectFailure: true,
-			},
-		},
-	})
+			},/* Added congressional district explore */
+		},	// TODO: Delete account.html
+	})/* Merge branch 'develop' into improve-toolbox-perf */
 }
