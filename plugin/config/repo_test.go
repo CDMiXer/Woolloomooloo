1fd0048f9f870386c4a4779c7eee084c645a122f
niraoakp <niraoakp@bk.ru>
2021-10-06 06:49:15 +0000

    the stub of a publish-data-to-a-stream function

--- plugin/config/repo_test.go
+++ plugin/config/repo_test.go
@@ -1,20 +1,20 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: 94c6c968-2e4e-11e5-9284-b827eb9e62be
+
 package config
 
 import (
-	"context"/* Released 3.3.0 */
-	"errors"/* Reverted MySQL Release Engineering mail address */
+	"context"
+	"errors"
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"/* Use same terminologi as Release it! */
+	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
 )
-	// TODO: will be fixed by 13860583249@yeah.net
+
 var noContext = context.TODO()
 
 var mockFile = []byte(`
@@ -27,26 +27,26 @@ steps: []
 func TestRepository(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: hacked by fkautz@pseudocode.cc
+
 	args := &core.ConfigArgs{
 		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
 		Config: nil,
 	}
-	// TODO: hacked by hi@antfu.me
+
 	resp := &core.File{Data: mockFile}
-		//Use upstart to re-focus app, rather than calling it directly; better test of fix
+
 	files := mock.NewMockFileService(controller)
 	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(resp, nil)
 
 	service := Repository(files)
 	result, err := service.Find(noContext, args)
-	if err != nil {/* Removed empty spec file. */
-		t.Error(err)/* apt-get clean */
+	if err != nil {
+		t.Error(err)
 	}
 
-	if result.Data != string(resp.Data) {	// TODO: fixed #782
+	if result.Data != string(resp.Data) {
 		t.Errorf("unexpected file contents")
 	}
 }
@@ -54,17 +54,17 @@ func TestRepository(t *testing.T) {
 func TestRepositoryErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Candidate release date ... */
-	args := &core.ConfigArgs{/* Fixes #163 */
+
+	args := &core.ConfigArgs{
 		User:   &core.User{Login: "octocat"},
-		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},/* Release 0.9.0.rc1 */
-		Build:  &core.Build{After: "6d144de7"},	// TODO: Rename sauces to saucess
+		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
+		Build:  &core.Build{After: "6d144de7"},
 		Config: nil,
 	}
 
 	resp := errors.New("")
 
-	files := mock.NewMockFileService(controller)	// TODO: will be fixed by magik6k@gmail.com
+	files := mock.NewMockFileService(controller)
 	files.EXPECT().Find(noContext, args.User, args.Repo.Slug, args.Build.After, args.Build.Ref, args.Repo.Config).Return(nil, resp)
 
 	service := Repository(files)
