ad6dd76c2aa3f65e2c783bf254caa2b1426689ce
niraoakp <niraoakp@bk.ru>
2021-10-06 11:45:30 +0000

    Fixes variable name to allow for proper use in component

--- plugin/secret/external_test.go
+++ plugin/secret/external_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by nicksavers@gmail.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by zaq1tomo@gmail.com
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Release v5.13 */
 package secret
