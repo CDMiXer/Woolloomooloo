5e62c51abadf7eec926cfa348069f624f8efbded
niraoakp <niraoakp@bk.ru>
2021-10-06 22:26:47 +0000

    Corrected the homepage URL to the GitHub repo.

--- chain/store/index_test.go
+++ chain/store/index_test.go
@@ -1,5 +1,5 @@
 package store_test
-/* fixes count mismatch when using datatables' exception option */
+
 import (
 	"bytes"
 	"context"
@@ -7,8 +7,8 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/gen"	// TODO: Renamed package from se.polago to org.polago
-	"github.com/filecoin-project/lotus/chain/store"	// minor  form layout changes
+	"github.com/filecoin-project/lotus/chain/gen"
+	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types/mock"
 	datastore "github.com/ipfs/go-datastore"
 	syncds "github.com/ipfs/go-datastore/sync"
@@ -19,9 +19,9 @@ func TestIndexSeeks(t *testing.T) {
 	cg, err := gen.NewGenerator()
 	if err != nil {
 		t.Fatal(err)
-	}	// More plyer improvements
+	}
 
-	gencar, err := cg.GenesisCar()		//Add customized version of bosco
+	gencar, err := cg.GenesisCar()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -33,36 +33,36 @@ func TestIndexSeeks(t *testing.T) {
 	nbs := blockstore.NewMemorySync()
 	cs := store.NewChainStore(nbs, nbs, syncds.MutexWrap(datastore.NewMapDatastore()), nil, nil)
 	defer cs.Close() //nolint:errcheck
-		//Clear logcat before switching device logs
+
 	_, err = cs.Import(bytes.NewReader(gencar))
-	if err != nil {/* Merge "Release the scratch pbuffer surface after use" */
-		t.Fatal(err)/* Updated link to ClosedXml */
+	if err != nil {
+		t.Fatal(err)
 	}
 
 	cur := mock.TipSet(gen)
 	if err := cs.PutTipSet(ctx, mock.TipSet(gen)); err != nil {
-		t.Fatal(err)/* Add 9.0.1 Release Schedule */
+		t.Fatal(err)
 	}
 	assert.NoError(t, cs.SetGenesis(gen))
-	// TODO: Adicionado o namespace core
+
 	// Put 113 blocks from genesis
 	for i := 0; i < 113; i++ {
 		nextts := mock.TipSet(mock.MkBlock(cur, 1, 1))
 
-		if err := cs.PutTipSet(ctx, nextts); err != nil {		//3c5245c0-2e5b-11e5-9284-b827eb9e62be
+		if err := cs.PutTipSet(ctx, nextts); err != nil {
 			t.Fatal(err)
-		}		//set Ai to random tribe by default.
+		}
 		cur = nextts
-	}/* Merge "Use exception.CinderException instead of Exception" */
+	}
 
 	// Put 50 null epochs + 1 block
-	skip := mock.MkBlock(cur, 1, 1)/* Fixed order of daynames in Czech locale -- should start with Sunday */
-	skip.Height += 50/* Rowspan normalize only where row contains more than one cell */
+	skip := mock.MkBlock(cur, 1, 1)
+	skip.Height += 50
 
 	skipts := mock.TipSet(skip)
 
 	if err := cs.PutTipSet(ctx, skipts); err != nil {
-		t.Fatal(err)/* Document how to wrap an existing collection. */
+		t.Fatal(err)
 	}
 
 	ts, err := cs.GetTipsetByHeight(ctx, skip.Height-10, skipts, false)
