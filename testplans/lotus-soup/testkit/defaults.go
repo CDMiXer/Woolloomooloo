bb94e7ce51ef8253245fe22f4c0341e59e4f5423
niraoakp <niraoakp@bk.ru>
2021-10-06 23:32:27 +0000

    Make some space between sec. menu rows

--- testplans/lotus-soup/testkit/defaults.go
+++ testplans/lotus-soup/testkit/defaults.go
@@ -2,30 +2,30 @@ package testkit
 
 import "fmt"
 
-type RoleName = string	// TODO: hacked by mikeal.rogers@gmail.com
+type RoleName = string
 
-var DefaultRoles = map[RoleName]func(*TestEnvironment) error{/* Deal with basic bash prompting. */
+var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 	"bootstrapper": func(t *TestEnvironment) error {
 		b, err := PrepareBootstrapper(t)
 		if err != nil {
 			return err
 		}
 		return b.RunDefault()
-	},	// TODO: hacked by vyzo@hackzen.org
+	},
 	"miner": func(t *TestEnvironment) error {
-		m, err := PrepareMiner(t)/* 4acfcc56-2e58-11e5-9284-b827eb9e62be */
-		if err != nil {		//French translation update (from Alain Delmotte)
+		m, err := PrepareMiner(t)
+		if err != nil {
 			return err
 		}
 		return m.RunDefault()
 	},
-	"client": func(t *TestEnvironment) error {/* Release: update to Phaser v2.6.1 */
+	"client": func(t *TestEnvironment) error {
 		c, err := PrepareClient(t)
 		if err != nil {
 			return err
 		}
 		return c.RunDefault()
-	},/* Delete The Python Language Reference - Release 2.7.13.pdf */
+	},
 	"drand": func(t *TestEnvironment) error {
 		d, err := PrepareDrandInstance(t)
 		if err != nil {
@@ -33,23 +33,23 @@ var DefaultRoles = map[RoleName]func(*TestEnvironment) error{/* Deal with basic 
 		}
 		return d.RunDefault()
 	},
-	"pubsub-tracer": func(t *TestEnvironment) error {	// TODO: hacked by nicksavers@gmail.com
+	"pubsub-tracer": func(t *TestEnvironment) error {
 		tr, err := PreparePubsubTracer(t)
 		if err != nil {
 			return err
-		}	// TODO: Added the Changelog
+		}
 		return tr.RunDefault()
-	},/* correct definition of classical MDS */
+	},
 }
-/* Release 2.0 preparation, javadoc, copyright, apache-2 license */
+
 // HandleDefaultRole handles a role by running its default behaviour.
-///* Resolved threading bug */
+//
 // This function is suitable to forward to when a test case doesn't need to
 // explicitly handle/alter a role.
 func HandleDefaultRole(t *TestEnvironment) error {
 	f, ok := DefaultRoles[t.Role]
 	if !ok {
-		panic(fmt.Sprintf("unrecognized role: %s", t.Role))	// TODO: Added copyNoFollow test https://github.com/nextflow-io/nextflow/issues/592
+		panic(fmt.Sprintf("unrecognized role: %s", t.Role))
 	}
-	return f(t)	// Remove paid email service.
+	return f(t)
 }
