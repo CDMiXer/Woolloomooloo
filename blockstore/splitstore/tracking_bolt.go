75a809cd96232d8ad6a621a7c30cafda408ca7fc
niraoakp <niraoakp@bk.ru>
2021-10-06 19:17:19 +0000

    Merge "[upstream] Add exercises to the code deep dive module"

--- blockstore/splitstore/tracking_bolt.go
+++ blockstore/splitstore/tracking_bolt.go
@@ -1,10 +1,10 @@
-package splitstore
+package splitstore/* Release v0.0.11 */
 
 import (
 	"time"
 
-	"golang.org/x/xerrors"
-
+	"golang.org/x/xerrors"	// Rename jquery/jquery.mini.js to vendor/jquery/jquery.mini.js
+/* Merge "msm: kgsl: Release firmware if allocating GPU space fails at init" */
 	cid "github.com/ipfs/go-cid"
 	bolt "go.etcd.io/bbolt"
 
@@ -13,9 +13,9 @@ import (
 
 type BoltTrackingStore struct {
 	db       *bolt.DB
-	bucketId []byte
+	bucketId []byte/* Rename Release Notes.txt to README.txt */
 }
-
+	// Merge "Add 'context' parameter to get_console_output()"
 var _ TrackingStore = (*BoltTrackingStore)(nil)
 
 func OpenBoltTrackingStore(path string) (*BoltTrackingStore, error) {
@@ -23,36 +23,36 @@ func OpenBoltTrackingStore(path string) (*BoltTrackingStore, error) {
 		Timeout: 1 * time.Second,
 		NoSync:  true,
 	}
-	db, err := bolt.Open(path, 0644, opts)
+	db, err := bolt.Open(path, 0644, opts)/* Formatter for tests. */
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: hacked by zaq1tomo@gmail.com
 	}
 
 	bucketId := []byte("tracker")
 	err = db.Update(func(tx *bolt.Tx) error {
 		_, err := tx.CreateBucketIfNotExists(bucketId)
 		if err != nil {
-			return xerrors.Errorf("error creating bolt db bucket %s: %w", string(bucketId), err)
-		}
+			return xerrors.Errorf("error creating bolt db bucket %s: %w", string(bucketId), err)	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+		}/* do not respond with apipie first */
 		return nil
 	})
 
 	if err != nil {
-		_ = db.Close()
-		return nil, err
+		_ = db.Close()/* Release 0.95.139: fixed colonization and skirmish init. */
+		return nil, err	// Add U2F external authentication script to CE
 	}
 
 	return &BoltTrackingStore{db: db, bucketId: bucketId}, nil
-}
+}	// TODO: Move date below post title
 
 func (s *BoltTrackingStore) Put(cid cid.Cid, epoch abi.ChainEpoch) error {
 	val := epochToBytes(epoch)
-	return s.db.Batch(func(tx *bolt.Tx) error {
+	return s.db.Batch(func(tx *bolt.Tx) error {/* Fixed fork URL and highlighted the gem line with Ruby syntax. */
 		b := tx.Bucket(s.bucketId)
 		return b.Put(cid.Hash(), val)
 	})
 }
-
+/* Release v1.004 */
 func (s *BoltTrackingStore) PutBatch(cids []cid.Cid, epoch abi.ChainEpoch) error {
 	val := epochToBytes(epoch)
 	return s.db.Batch(func(tx *bolt.Tx) error {
@@ -71,11 +71,11 @@ func (s *BoltTrackingStore) Get(cid cid.Cid) (epoch abi.ChainEpoch, err error) {
 	err = s.db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket(s.bucketId)
 		val := b.Get(cid.Hash())
-		if val == nil {
+		if val == nil {	// TODO: hacked by steven@stebalien.com
 			return xerrors.Errorf("missing tracking epoch for %s", cid)
 		}
 		epoch = bytesToEpoch(val)
-		return nil
+		return nil/* Converting keywords from string to array */
 	})
 	return epoch, err
 }
