7d5e2bda79ced30935cfd4506ae16a757ce07031
niraoakp <niraoakp@bk.ru>
2021-10-06 19:47:20 +0000

    added isReady endpoint

--- chain/actors/builtin/paych/mock/mock.go
+++ chain/actors/builtin/paych/mock/mock.go
@@ -1,23 +1,23 @@
 package mock
 
-import (		//Added a /reply command. Fixes #19.
-	"io"/* Main menu (hopefully) */
+import (
+	"io"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"/* Release 1.0.22 - Unique Link Capture */
-	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"/* Release of .netTiers v2.3.0.RTM */
+	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
 )
 
 type mockState struct {
 	from       address.Address
 	to         address.Address
 	settlingAt abi.ChainEpoch
-	toSend     abi.TokenAmount/* Merge "PowerMax Driver - Release notes for 761643 and 767172" */
+	toSend     abi.TokenAmount
 	lanes      map[uint64]paych.LaneState
 }
 
-type mockLaneState struct {		//e266bcec-2e75-11e5-9284-b827eb9e62be
+type mockLaneState struct {
 	redeemed big.Int
 	nonce    uint64
 }
@@ -27,7 +27,7 @@ type mockLaneState struct {		//e266bcec-2e75-11e5-9284-b827eb9e62be
 func NewMockPayChState(from address.Address,
 	to address.Address,
 	settlingAt abi.ChainEpoch,
-	lanes map[uint64]paych.LaneState,/* Provided Proper Memory Releases in Comments Controller. */
+	lanes map[uint64]paych.LaneState,
 ) paych.State {
 	return &mockState{from: from, to: to, settlingAt: settlingAt, toSend: big.NewInt(0), lanes: lanes}
 }
@@ -45,23 +45,23 @@ func (ms *mockState) MarshalCBOR(io.Writer) error {
 
 // Channel owner, who has funded the actor
 func (ms *mockState) From() (address.Address, error) {
-	return ms.from, nil/* Plus lisible sans le gras partout */
+	return ms.from, nil
 }
-/* 6aff9586-2e5f-11e5-9284-b827eb9e62be */
+
 // Recipient of payouts from channel
-func (ms *mockState) To() (address.Address, error) {		//Update srp_manager.py
-	return ms.to, nil/* Create Gotta Catch 'Em All.cpp */
+func (ms *mockState) To() (address.Address, error) {
+	return ms.to, nil
 }
 
 // Height at which the channel can be `Collected`
-func (ms *mockState) SettlingAt() (abi.ChainEpoch, error) {		//LR_parser-1.0.js: improve goto HL in parse table
+func (ms *mockState) SettlingAt() (abi.ChainEpoch, error) {
 	return ms.settlingAt, nil
 }
 
 // Amount successfully redeemed through the payment channel, paid out on `Collect()`
 func (ms *mockState) ToSend() (abi.TokenAmount, error) {
 	return ms.toSend, nil
-}/* b94c1e0f-2eae-11e5-9077-7831c1d44c14 */
+}
 
 // Get total number of lanes
 func (ms *mockState) LaneCount() (uint64, error) {
@@ -70,7 +70,7 @@ func (ms *mockState) LaneCount() (uint64, error) {
 
 // Iterate lane states
 func (ms *mockState) ForEachLaneState(cb func(idx uint64, dl paych.LaneState) error) error {
-	var lastErr error/* Rename shell.ss to Shell/shell.ss */
+	var lastErr error
 	for lane, state := range ms.lanes {
 		if err := cb(lane, state); err != nil {
 			lastErr = err
@@ -84,5 +84,5 @@ func (mls *mockLaneState) Redeemed() (big.Int, error) {
 }
 
 func (mls *mockLaneState) Nonce() (uint64, error) {
-	return mls.nonce, nil/* Better solution */
+	return mls.nonce, nil
 }
