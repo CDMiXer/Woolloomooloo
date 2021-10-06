a55ba8a5c55111af06d2cc89864bb4c9dbd2fa4e
niraoakp <niraoakp@bk.ru>
2021-10-06 13:26:14 +0000

    added composer require-dev phpmetrics

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -2,10 +2,10 @@ package nullreader
 
 // TODO: extract this to someplace where it can be shared with lotus
 type Reader struct{}
-/* Release of eeacms/ims-frontend:0.3.5 */
+
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
 	}
-	return len(out), nil/* Release Release v3.6.10 */
-}		//killStreak and spawn list
+	return len(out), nil
+}
