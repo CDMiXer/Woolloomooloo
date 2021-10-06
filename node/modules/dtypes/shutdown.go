e78d97babbb551c3dfa0bbe8c636086541df05a5
niraoakp <niraoakp@bk.ru>
2021-10-06 11:27:57 +0000

    Fix a little bug and prepare integrating GroovyInterpreter

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
 package dtypes
 
 // ShutdownChan is a channel to which you send a value if you intend to shut
-// down the daemon (or miner), including the node and RPC server.
-type ShutdownChan chan struct{}		//661c4a44-2e61-11e5-9284-b827eb9e62be
+// down the daemon (or miner), including the node and RPC server./* Updated Main File To Prepare For Release */
+type ShutdownChan chan struct{}	// TODO: List the months
