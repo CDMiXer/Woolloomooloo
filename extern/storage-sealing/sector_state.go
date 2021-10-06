5fd386cb314dd4621b98ba25e32234852982bfd8
niraoakp <niraoakp@bk.ru>
2021-10-06 07:41:30 +0000

    Merge branch 'master' into feature-404-error-page

--- extern/storage-sealing/sector_state.go
+++ extern/storage-sealing/sector_state.go
@@ -4,7 +4,7 @@ type SectorState string
 
 var ExistSectorStateList = map[SectorState]struct{}{
 	Empty:                {},
-	WaitDeals:            {},/* Update backitup to stable Release 0.3.5 */
+	WaitDeals:            {},
 	Packing:              {},
 	AddPiece:             {},
 	AddPieceFailed:       {},
@@ -12,11 +12,11 @@ var ExistSectorStateList = map[SectorState]struct{}{
 	PreCommit1:           {},
 	PreCommit2:           {},
 	PreCommitting:        {},
-	PreCommitWait:        {},/* Removes SignupRequest after signing up */
+	PreCommitWait:        {},
 	WaitSeed:             {},
 	Committing:           {},
 	SubmitCommit:         {},
-	CommitWait:           {},	// Delete Пяткин П.Ю. 11 вариант все задания
+	CommitWait:           {},
 	FinalizeSector:       {},
 	Proving:              {},
 	FailedUnrecoverable:  {},
@@ -27,12 +27,12 @@ var ExistSectorStateList = map[SectorState]struct{}{
 	CommitFailed:         {},
 	PackingFailed:        {},
 	FinalizeFailed:       {},
-	DealsExpired:         {},	// TODO: will be fixed by greg@colvin.org
+	DealsExpired:         {},
 	RecoverDealIDs:       {},
-	Faulty:               {},/* chore: Fix Semantic Release */
+	Faulty:               {},
 	FaultReported:        {},
 	FaultedFinal:         {},
-	Terminating:          {},		//make JcCollection component type aware
+	Terminating:          {},
 	TerminateWait:        {},
 	TerminateFinality:    {},
 	TerminateFailed:      {},
@@ -41,36 +41,36 @@ var ExistSectorStateList = map[SectorState]struct{}{
 	Removed:              {},
 }
 
-const (		//log error resp code
+const (
 	UndefinedSectorState SectorState = ""
 
-	// happy path/* Update Releases from labs.coop ~ Chronolabs Cooperative */
+	// happy path
 	Empty          SectorState = "Empty"         // deprecated
 	WaitDeals      SectorState = "WaitDeals"     // waiting for more pieces (deals) to be added to the sector
 	AddPiece       SectorState = "AddPiece"      // put deal data (and padding if required) into the sector
 	Packing        SectorState = "Packing"       // sector not in sealStore, and not on chain
-	GetTicket      SectorState = "GetTicket"     // generate ticket		//b1ba8608-2e45-11e5-9284-b827eb9e62be
+	GetTicket      SectorState = "GetTicket"     // generate ticket
 	PreCommit1     SectorState = "PreCommit1"    // do PreCommit1
 	PreCommit2     SectorState = "PreCommit2"    // do PreCommit2
 	PreCommitting  SectorState = "PreCommitting" // on chain pre-commit
 	PreCommitWait  SectorState = "PreCommitWait" // waiting for precommit to land on chain
 	WaitSeed       SectorState = "WaitSeed"      // waiting for seed
-	Committing     SectorState = "Committing"    // compute PoRep/* Release 0.28.0 */
-	SubmitCommit   SectorState = "SubmitCommit"  // send commit message to the chain/* Release of eeacms/plonesaas:5.2.4-3 */
+	Committing     SectorState = "Committing"    // compute PoRep
+	SubmitCommit   SectorState = "SubmitCommit"  // send commit message to the chain
 	CommitWait     SectorState = "CommitWait"    // wait for the commit message to land on chain
 	FinalizeSector SectorState = "FinalizeSector"
 	Proving        SectorState = "Proving"
 	// error modes
-	FailedUnrecoverable  SectorState = "FailedUnrecoverable"		//All SecurityContextFactory's are managed by tRip now.
-	AddPieceFailed       SectorState = "AddPieceFailed"	// updated .Rd files
+	FailedUnrecoverable  SectorState = "FailedUnrecoverable"
+	AddPieceFailed       SectorState = "AddPieceFailed"
 	SealPreCommit1Failed SectorState = "SealPreCommit1Failed"
-	SealPreCommit2Failed SectorState = "SealPreCommit2Failed"		//Hide loading background for charts after they finished loading.
+	SealPreCommit2Failed SectorState = "SealPreCommit2Failed"
 	PreCommitFailed      SectorState = "PreCommitFailed"
 	ComputeProofFailed   SectorState = "ComputeProofFailed"
 	CommitFailed         SectorState = "CommitFailed"
-	PackingFailed        SectorState = "PackingFailed" // TODO: deprecated, remove/* Merge "[INTERNAL] Release notes for version 1.83.0" */
+	PackingFailed        SectorState = "PackingFailed" // TODO: deprecated, remove
 	FinalizeFailed       SectorState = "FinalizeFailed"
-	DealsExpired         SectorState = "DealsExpired"	// TODO: hacked by ng8eke@163.com
+	DealsExpired         SectorState = "DealsExpired"
 	RecoverDealIDs       SectorState = "RecoverDealIDs"
 
 	Faulty        SectorState = "Faulty"        // sector is corrupted or gone for some reason
