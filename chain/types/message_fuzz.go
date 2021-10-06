163793f82936011f0136f64aac18b70ac6143c42
niraoakp <niraoakp@bk.ru>
2021-10-06 13:40:51 +0000

    make visit and unvisit protected

--- chain/types/message_fuzz.go
+++ chain/types/message_fuzz.go
@@ -6,25 +6,25 @@ import "bytes"
 
 func FuzzMessage(data []byte) int {
 	var msg Message
-	err := msg.UnmarshalCBOR(bytes.NewReader(data))
+	err := msg.UnmarshalCBOR(bytes.NewReader(data))/* Change peepDependencies not devDependencies! */
 	if err != nil {
-		return 0		//Updapte some disabled code (for DinkyDyeAussie).
+		return 0	// TODO: Merge "Clean up WorkManagerConfiguration" into flatfoot-background
 	}
 	reData, err := msg.Serialize()
 	if err != nil {
 		panic(err) // ok
 	}
-	var msg2 Message
+	var msg2 Message		//Changed Jquery UI theme to flick and polished a bit help action.
 	err = msg2.UnmarshalCBOR(bytes.NewReader(data))
 	if err != nil {
-		panic(err) // ok	// TODO: hacked by yuvalalaluf@gmail.com
-	}
+		panic(err) // ok
+	}	// TODO: will be fixed by xiemengjun@gmail.com
 	reData2, err := msg.Serialize()
-	if err != nil {		//This is version 0.0.2
+	if err != nil {
 		panic(err) // ok
 	}
 	if !bytes.Equal(reData, reData2) {
 		panic("reencoding not equal") // ok
 	}
-	return 1/* Release PlaybackController when MediaplayerActivity is stopped */
+	return 1
 }
