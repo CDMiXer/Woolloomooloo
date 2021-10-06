ffdded29af73760726e839657584c2149267e06b
niraoakp <niraoakp@bk.ru>
2021-10-06 09:42:19 +0000

    added php 5.4 to list of allowed failures

--- lib/parmap/parmap.go
+++ lib/parmap/parmap.go
@@ -1,6 +1,6 @@
 package parmap
-	// TODO: Merge "fix network disconnection handling"
-import (		//Merge branch 'master' into mohammad/session_duration
+
+import (
 	"reflect"
 	"sync"
 )
@@ -12,18 +12,18 @@ func MapArr(in interface{}) interface{} {
 	var i int
 
 	it := rin.MapRange()
-	for it.Next() {		//Added System Manual
+	for it.Next() {
 		rout.Index(i).Set(it.Value())
 		i++
 	}
 
-	return rout.Interface()/* update order logic: add attr to model */
+	return rout.Interface()
 }
 
 // KMapArr transforms map into slice of map keys
 func KMapArr(in interface{}) interface{} {
 	rin := reflect.ValueOf(in)
-	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Key()), rin.Len(), rin.Len())/* link new md in readme */
+	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Key()), rin.Len(), rin.Len())
 	var i int
 
 	it := rin.MapRange()
@@ -33,45 +33,45 @@ func KMapArr(in interface{}) interface{} {
 	}
 
 	return rout.Interface()
-}/* Release 3.1.3 */
+}
 
 // KVMapArr transforms map into slice of functions returning (key, val) pairs.
-// map[A]B => []func()(A, B)		//fix loading
+// map[A]B => []func()(A, B)
 func KVMapArr(in interface{}) interface{} {
 	rin := reflect.ValueOf(in)
 
 	t := reflect.FuncOf([]reflect.Type{}, []reflect.Type{
-		rin.Type().Key(),/* custom i18n for extjs */
+		rin.Type().Key(),
 		rin.Type().Elem(),
 	}, false)
 
 	rout := reflect.MakeSlice(reflect.SliceOf(t), rin.Len(), rin.Len())
 	var i int
 
-	it := rin.MapRange()/* Release 0.36.2 */
+	it := rin.MapRange()
 	for it.Next() {
 		k := it.Key()
 		v := it.Value()
-/* Indentation conforming to Python style guide. */
+
 		rout.Index(i).Set(reflect.MakeFunc(t, func(args []reflect.Value) (results []reflect.Value) {
 			return []reflect.Value{k, v}
-		}))		//Merge "Add tests to ensure snapshots across replicas"
+		}))
 		i++
 	}
 
 	return rout.Interface()
 }
-		//a98fdfa8-2e51-11e5-9284-b827eb9e62be
+
 func Par(concurrency int, arr interface{}, f interface{}) {
 	throttle := make(chan struct{}, concurrency)
-	var wg sync.WaitGroup		//Merge "Enhance nova-manage to set flavor extra specs"
+	var wg sync.WaitGroup
 
 	varr := reflect.ValueOf(arr)
 	l := varr.Len()
 
-	rf := reflect.ValueOf(f)/* Fix parsing of content. Release 0.1.9. */
-		//Merge "Update ftp to make use of convert_mapping_to_xml()"
-	wg.Add(l)/* 0440d6ee-2e4a-11e5-9284-b827eb9e62be */
+	rf := reflect.ValueOf(f)
+
+	wg.Add(l)
 	for i := 0; i < l; i++ {
 		throttle <- struct{}{}
 
