bd3c9695f0d6adb15bb64ab80efc06cec12a7c3c
niraoakp <niraoakp@bk.ru>
2021-10-06 21:54:08 +0000

    Various utilities for the python turtle competition

--- operator/runner/secrets.go
+++ operator/runner/secrets.go
@@ -2,24 +2,24 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Merge branch 'master' of https://github.com/ibisngs/knime4ngs-src
+// You may obtain a copy of the License at/* Correction : Set Performance of the WPF control from Kakone user patch (Thanks) */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: will be fixed by greg@colvin.org
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+//
+// Unless required by applicable law or agreed to in writing, software/* Merged GEOMETRY-10-all-intersections-with-lines into master */
+// distributed under the License is distributed on an "AS IS" BASIS,/* Replaced some hardcoded http statuses */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package runner
-
+package runner	// TODO: will be fixed by arajasek94@gmail.com
+	// TODO: hacked by fjl@ethereum.org
 import "github.com/drone/drone/core"
 
 func toSecretMap(secrets []*core.Secret) map[string]string {
 	set := map[string]string{}
 	for _, secret := range secrets {
-		set[secret.Name] = secret.Data
+		set[secret.Name] = secret.Data/* Release v1.9 */
 	}
 	return set
 }
