d41b66051dfa6b4aec4343e73c3d931bb130fb7f
niraoakp <niraoakp@bk.ru>
2021-10-06 06:17:28 +0000

    Merge "Bump Schema:Echo to oldid=7731316"

--- server/info/info_server_test.go
+++ server/info/info_server_test.go
@@ -2,20 +2,20 @@ package info
 
 import (
 	"context"
-	"testing"
+	"testing"		//changed axis limits in plot_activity so they always start from 0
 
 	"github.com/stretchr/testify/assert"
-/* Testing Release */
-	"github.com/argoproj/argo/server/auth"
-	"github.com/argoproj/argo/server/auth/jws"
-)/* Merge branch 'master' into fix/autoReload */
 
-func Test_infoServer_GetUserInfo(t *testing.T) {/* Create 5AD6DC6D-EA78-40AF-891F-F17AB16384BA.jpeg */
+	"github.com/argoproj/argo/server/auth"
+	"github.com/argoproj/argo/server/auth/jws"	// TODO: Remove keyowrd used by third-party extensions
+)
+
+func Test_infoServer_GetUserInfo(t *testing.T) {		//require 'ostruct' in lib/reform.rb
 	i := &infoServer{}
 	ctx := context.WithValue(context.TODO(), auth.ClaimSetKey, &jws.ClaimSet{Iss: "my-iss", Sub: "my-sub"})
-	info, err := i.GetUserInfo(ctx, nil)
-	if assert.NoError(t, err) {/* Release of eeacms/www-devel:20.10.11 */
-		assert.Equal(t, "my-iss", info.Issuer)	// TODO: will be fixed by willem.melching@gmail.com
-		assert.Equal(t, "my-sub", info.Subject)/* Update dependency @gitlab/ui to ^2.0.2 */
+	info, err := i.GetUserInfo(ctx, nil)/*  * cpuid.h: include cstdint. */
+	if assert.NoError(t, err) {
+		assert.Equal(t, "my-iss", info.Issuer)
+		assert.Equal(t, "my-sub", info.Subject)
 	}
 }
