497d63e76a2f45601b6c329e88a3e88fb55c75d2
niraoakp <niraoakp@bk.ru>
2021-10-06 11:16:10 +0000

    Added Scada

--- plugin/config/jsonnet_oss.go
+++ plugin/config/jsonnet_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//adding xml 1.0 grammar
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Added Release 1.1.1 */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//LOW / Do not localize dropdown
+//      http://www.apache.org/licenses/LICENSE-2.0/* [IMP] Improve css for direct printing page from browser to press Ctrl+P. */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* allow special keys */
+// See the License for the specific language governing permissions and		//Minor changes to wording of descriptions and error messages in options UI.
 // limitations under the License.
-	// TODO: Clarify a time unit.
+		//LDEV-4813 Add a missing foreign key in Assessment tool
 // +build oss
+		//fixes #229
+package config	// Register new request type keys in request.go
 
-package config/* Octicon in navs :heart: */
-
-import "github.com/drone/drone/core"/* Update qt5-image.bb */
-	// TODO: hacked by alan.shaw@protocol.ai
+import "github.com/drone/drone/core"/* Inject the environment into the command. */
+	// TODO: Don't cache flex_sdk
 // Jsonnet returns a no-op configuration service.
-func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
+func Jsonnet(service core.FileService, enabled bool) core.ConfigService {/* Release 1.3.1rc1 */
 	return new(noop)
 }
