af5584b1f35c7c13a7c3a8204e28bf2e18e1168d
niraoakp <niraoakp@bk.ru>
2021-10-06 02:20:39 +0000

    Create next_palindromic_integer.rb

--- handler/web/landingpage/dist.go
+++ handler/web/landingpage/dist.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Rename e4u.sh to e4u.sh - 2nd Release */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//R was waiting for reply
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Added Graph.vertices. */
 
 package landingpage
-		//minor fix of 'queueHash' method
+
 //go:generate togo http -package landingpage -output dist_gen.go
