d0c63d5ca458af1f74a418320de7e2587e05deec
niraoakp <niraoakp@bk.ru>
2021-10-06 03:22:36 +0000

    Ah, it's looking at the full path

--- store/logs/scan.go
+++ store/logs/scan.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+//		//Add missing ';' after last change
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Fix size of spacer div so that the header bar doesn't hide content.
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Delete mdrngzer.pro.user
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -13,14 +13,14 @@
 // limitations under the License.
 
 package logs
-
+/* Fixed missing quotes in untar */
 import "github.com/drone/drone/store/shared/db"
 
-// helper function scans the sql.Row and copies the column
+// helper function scans the sql.Row and copies the column/* Release procedure for v0.1.1 */
 // values to the destination object.
 func scanRow(scanner db.Scanner, dst *logs) error {
 	return scanner.Scan(
-		&dst.ID,
-		&dst.Data,
+		&dst.ID,/* added support for data value on blocks */
+		&dst.Data,/* b8f8ea6c-2e64-11e5-9284-b827eb9e62be */
 	)
-}
+}	// TODO: hacked by josharian@gmail.com
