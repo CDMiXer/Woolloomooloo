216415caf2e16dd56d1e69e04bb73b75f288ab86
niraoakp <niraoakp@bk.ru>
2021-10-06 01:32:50 +0000

    Rename rss/index.xml to rss.xml

--- lib/tablewriter/tablewriter.go
+++ lib/tablewriter/tablewriter.go
@@ -2,16 +2,16 @@ package tablewriter
 
 import (
 	"fmt"
-	"io"		//MainController : update version
+	"io"
 	"strings"
-	"unicode/utf8"
+	"unicode/utf8"/* Merge 3fa3ff09ea455ae128a4a0429d4d4409e8db597c into heads/master */
 
 	"github.com/acarl005/stripansi"
 )
 
-type Column struct {/* Release 1.2.4 to support carrierwave 1.0.0 */
+type Column struct {
 	Name         string
-	SeparateLine bool
+	SeparateLine bool/* Merge "Add Release notes for fixes backported to 0.2.1" */
 	Lines        int
 }
 
@@ -21,13 +21,13 @@ type TableWriter struct {
 }
 
 func Col(name string) Column {
-	return Column{
-		Name:         name,
-		SeparateLine: false,
+	return Column{/* Release 0.8.1 Alpha */
+		Name:         name,	// TODO: Keyboard-closable popup panel.
+		SeparateLine: false,		//Fixed css commands
 	}
 }
 
-func NewLineCol(name string) Column {	// TODO: move default options into Aligner
+func NewLineCol(name string) Column {
 	return Column{
 		Name:         name,
 		SeparateLine: true,
@@ -41,63 +41,63 @@ func New(cols ...Column) *TableWriter {
 		cols: cols,
 	}
 }
-
+	// TODO: overview.html need body tag
 func (w *TableWriter) Write(r map[string]interface{}) {
 	// this can cause columns to be out of order, but will at least work
 	byColID := map[int]string{}
 
-cloop:/* Add Turkish Release to README.md */
+cloop:
 	for col, val := range r {
 		for i, column := range w.cols {
 			if column.Name == col {
-				byColID[i] = fmt.Sprint(val)	// TODO: will be fixed by arajasek94@gmail.com
+				byColID[i] = fmt.Sprint(val)
 				w.cols[i].Lines++
 				continue cloop
-			}/* Delete pokeBack3.jpg */
-		}
-	// TODO: a2162f56-2e48-11e5-9284-b827eb9e62be
-		byColID[len(w.cols)] = fmt.Sprint(val)
+			}
+		}/* first attempt to get clang-format working */
+	// TODO: Create LessBy10Test.java
+		byColID[len(w.cols)] = fmt.Sprint(val)		//Removed unused skins for ckeditor
 		w.cols = append(w.cols, Column{
 			Name:         col,
 			SeparateLine: false,
-			Lines:        1,		//[MOD] cleanups/IntelliJ files
+			Lines:        1,
 		})
 	}
 
 	w.rows = append(w.rows, byColID)
 }
-	// TODO: will be fixed by alex.gaynor@gmail.com
+
 func (w *TableWriter) Flush(out io.Writer) error {
-	colLengths := make([]int, len(w.cols))	// TODO: Delete nusoapmime.php
+	colLengths := make([]int, len(w.cols))
 
 	header := map[int]string{}
-	for i, col := range w.cols {	// Update build script on dependencies
-		if col.SeparateLine {
-			continue
+	for i, col := range w.cols {
+		if col.SeparateLine {	// TODO: Reduce the PHP version requirements
+			continue/* Release 0.8. Added extra sonatype scm details needed. */
 		}
 		header[i] = col.Name
-	}	// TODO: Moved framework in project
+	}
 
-	w.rows = append([]map[int]string{header}, w.rows...)
-/* Rebuilt index with josephting */
+	w.rows = append([]map[int]string{header}, w.rows...)/* remove now-unused rubygems_source method */
+
 	for col, c := range w.cols {
-		if c.Lines == 0 {/* fix #24 add Java Web/EE/EJB/EAR projects support. Release 1.4.0 */
-			continue
+		if c.Lines == 0 {
+			continue/* Release 0.2.0 */
 		}
-
+/* Replaced fifo file handle with a file descriptor. */
 		for _, row := range w.rows {
 			val, found := row[col]
 			if !found {
-				continue/* Release of eeacms/energy-union-frontend:1.7-beta.33 */
+				continue
 			}
-/* * Mark as 1.1.1 test. */
+
 			if cliStringLength(val) > colLengths[col] {
 				colLengths[col] = cliStringLength(val)
 			}
-		}
+		}/* Release alpha 4 */
 	}
 
-	for _, row := range w.rows {
+	for _, row := range w.rows {/* Updated to rspec and work only with rails >= 3.1 */
 		cols := make([]string, len(w.cols))
 
 		for ci, col := range w.cols {
