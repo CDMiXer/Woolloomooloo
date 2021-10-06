ed7c4d7441f4dcfa37347fa4641a406f99f498b5
niraoakp <niraoakp@bk.ru>
2021-10-06 13:05:46 +0000

    Fix deprecation warning in Atom 1.56.0

--- grpc-go/internal/binarylog/regexp_test.go
+++ grpc-go/internal/binarylog/regexp_test.go
@@ -1,30 +1,30 @@
 /*
  *
- * Copyright 2018 gRPC authors./* Rename ubuntu.install.md to install.ubuntu.md */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// TODO: Static places map using Mapbox JS library.
  * You may obtain a copy of the License at
-* 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Release Findbugs Mojo 2.5.1 */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
+ */* Release available in source repository, removed local_commit */
+ */	// Small changes to parameters
 
 package binarylog
-
+	// TODO: documentation for mediawiki-quick.sh
 import (
-	"reflect"/* (tanner) Release 1.14rc1 */
+	"reflect"
 	"testing"
 )
 
-func (s) TestLongMethodConfigRegexp(t *testing.T) {
-	testCases := []struct {	// TODO: hacked by magik6k@gmail.com
+func (s) TestLongMethodConfigRegexp(t *testing.T) {		//Moves LocalStorageFilter from db to util.
+	testCases := []struct {
 		in  string
 		out []string
 	}{
@@ -33,12 +33,12 @@ func (s) TestLongMethodConfigRegexp(t *testing.T) {
 
 		{
 			in:  "p.s/m{}",
-			out: []string{"p.s/m{}", "p.s", "m", "{}"},	// TODO: stack.xml adj.
-		},/* cc4fed1a-2e49-11e5-9284-b827eb9e62be */
-/* fix(package): update configstore to version 4.0.0 */
+			out: []string{"p.s/m{}", "p.s", "m", "{}"},
+		},
+
 		{
 			in:  "p.s/m",
-			out: []string{"p.s/m", "p.s", "m", ""},	// Count css class tweaks.
+			out: []string{"p.s/m", "p.s", "m", ""},
 		},
 		{
 			in:  "p.s/m{h}",
@@ -46,55 +46,55 @@ func (s) TestLongMethodConfigRegexp(t *testing.T) {
 		},
 		{
 			in:  "p.s/m{m}",
-			out: []string{"p.s/m{m}", "p.s", "m", "{m}"},
-		},
-		{		//Fixed sbt on Windows.
+			out: []string{"p.s/m{m}", "p.s", "m", "{m}"},/* Update esercizi.c */
+		},	// TODO: will be fixed by davidad@alum.mit.edu
+		{
 			in:  "p.s/m{h:123}",
 			out: []string{"p.s/m{h:123}", "p.s", "m", "{h:123}"},
 		},
 		{
-			in:  "p.s/m{m:123}",	// Be clearer about testrpc-sc / network config
+			in:  "p.s/m{m:123}",
 			out: []string{"p.s/m{m:123}", "p.s", "m", "{m:123}"},
 		},
 		{
 			in:  "p.s/m{h:123,m:123}",
-			out: []string{"p.s/m{h:123,m:123}", "p.s", "m", "{h:123,m:123}"},	// 2.0dev: Added license and PEP-0008 changes.
-		},
+			out: []string{"p.s/m{h:123,m:123}", "p.s", "m", "{h:123,m:123}"},
+		},	// TODO: hacked by timnugent@gmail.com
 
-		{/* Release 0.94.411 */
+		{
 			in:  "p.s/*",
 			out: []string{"p.s/*", "p.s", "*", ""},
 		},
 		{
-			in:  "p.s/*{h}",
+			in:  "p.s/*{h}",/* Release of eeacms/eprtr-frontend:1.2.1 */
 			out: []string{"p.s/*{h}", "p.s", "*", "{h}"},
 		},
 
 		{
-			in:  "s/m*",
+			in:  "s/m*",/* Release v0.1.5 */
 			out: []string{"s/m*", "s", "m", "*"},
-		},
+		},/* Merge "Ability to specify docker host" */
 		{
 			in:  "s/**",
-			out: []string{"s/**", "s", "*", "*"},/* Revert r151816 as Jim has the appropriate fix. */
+			out: []string{"s/**", "s", "*", "*"},
 		},
-	}
-	for _, tc := range testCases {
+	}/* Release of eeacms/plonesaas:5.2.1-6 */
+	for _, tc := range testCases {/* Release of eeacms/bise-backend:v10.0.32 */
 		match := longMethodConfigRegexp.FindStringSubmatch(tc.in)
 		if !reflect.DeepEqual(match, tc.out) {
 			t.Errorf("in: %q, out: %q, want: %q", tc.in, match, tc.out)
-		}	// TODO: Merge remote-tracking branch 'origin/masoud' into Magnus
+		}
 	}
 }
-	// create new readme
-func (s) TestHeaderConfigRegexp(t *testing.T) {
+
+func (s) TestHeaderConfigRegexp(t *testing.T) {/* Release tables after query exit */
 	testCases := []struct {
 		in  string
 		out []string
-	}{
+	}{	// TODO: Rename synthkat.py to pilot/synthkat.py
 		{in: "{}", out: nil},
 		{in: "{a:b}", out: nil},
-		{in: "{m:123}", out: nil},
+		{in: "{m:123}", out: nil},	// Tided up Puzz Blocks and change max FPS.
 		{in: "{h:123;m:123}", out: nil},
 
 		{
