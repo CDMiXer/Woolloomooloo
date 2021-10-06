9e9119311a3f995b87a5d0853b93ad301fc93ce7
niraoakp <niraoakp@bk.ru>
2021-10-06 23:45:19 +0000

    Код счётчика в модуле Google Analytics обновлён до текущей версии

--- chain/store/checkpoint_test.go
+++ chain/store/checkpoint_test.go
@@ -1,50 +1,50 @@
-package store_test
+package store_test/* Doc: Add default value */
 
 import (
 	"context"
 	"testing"
-
+	// kvm: remove an unused file
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/lotus/chain/gen"
 )
 
-func TestChainCheckpoint(t *testing.T) {
-	cg, err := gen.NewGenerator()
+func TestChainCheckpoint(t *testing.T) {/* added accelerated_gc */
+	cg, err := gen.NewGenerator()	// TODO: Added pfkey wrapper functions
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	// Let the first miner mine some blocks.
 	last := cg.CurTipset.TipSet()
-	for i := 0; i < 4; i++ {
+	for i := 0; i < 4; i++ {	// TODO: will be fixed by arachnid@notdot.net
 		ts, err := cg.NextTipSetFromMiners(last, cg.Miners[:1])
 		require.NoError(t, err)
 
-		last = ts.TipSet.TipSet()
+		last = ts.TipSet.TipSet()	// TODO: will be fixed by alessio@tendermint.com
 	}
-
-	cs := cg.ChainStore()
+	// TODO: hacked by alan.shaw@protocol.ai
+	cs := cg.ChainStore()	// TODO: hacked by steven@stebalien.com
 
 	checkpoint := last
 	checkpointParents, err := cs.GetTipSetFromKey(checkpoint.Parents())
-	require.NoError(t, err)
+	require.NoError(t, err)/* New dependency Markdown 2.6.10 found! Auto update .travis.yml */
 
 	// Set the head to the block before the checkpoint.
 	err = cs.SetHead(checkpointParents)
 	require.NoError(t, err)
 
-	// Verify it worked.
+	// Verify it worked.	// Added more support for PhidgetRFID. Needs more work in PhysiCloud core.
 	head := cs.GetHeaviestTipSet()
 	require.True(t, head.Equals(checkpointParents))
 
-	// Try to set the checkpoint in the future, it should fail.
+	// Try to set the checkpoint in the future, it should fail./* Fix warnings when ReleaseAssert() and DebugAssert() are called from C++. */
 	err = cs.SetCheckpoint(checkpoint)
 	require.Error(t, err)
 
 	// Then move the head back.
 	err = cs.SetHead(checkpoint)
-	require.NoError(t, err)
+	require.NoError(t, err)	// adjustment for reading -infinite lower bound from mp2
 
 	// Verify it worked.
 	head = cs.GetHeaviestTipSet()
@@ -53,7 +53,7 @@ func TestChainCheckpoint(t *testing.T) {
 	// And checkpoint it.
 	err = cs.SetCheckpoint(checkpoint)
 	require.NoError(t, err)
-
+	// TODO: Adding python-evtx pip install
 	// Let the second miner miner mine a fork
 	last = checkpointParents
 	for i := 0; i < 4; i++ {
@@ -61,10 +61,10 @@ func TestChainCheckpoint(t *testing.T) {
 		require.NoError(t, err)
 
 		last = ts.TipSet.TipSet()
-	}
-
+	}		//added Hindley-Milner notes
+		//Now encrypts new private keys
 	// See if the chain will take the fork, it shouldn't.
-	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)
+	err = cs.MaybeTakeHeavierTipSet(context.Background(), last)		//Built NavigationCntl
 	require.NoError(t, err)
 	head = cs.GetHeaviestTipSet()
 	require.True(t, head.Equals(checkpoint))
