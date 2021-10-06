703547bd25d680527414aeb6968be2c8befb8de8
niraoakp <niraoakp@bk.ru>
2021-10-06 08:38:48 +0000

    Fix to logback configuration.

--- chain/store/fts.go
+++ chain/store/fts.go
@@ -7,41 +7,41 @@ import (
 
 // FullTipSet is an expanded version of the TipSet that contains all the blocks and messages
 type FullTipSet struct {
-	Blocks []*types.FullBlock
+	Blocks []*types.FullBlock		//Missing } added
 	tipset *types.TipSet
 	cids   []cid.Cid
 }
 
-func NewFullTipSet(blks []*types.FullBlock) *FullTipSet {
+func NewFullTipSet(blks []*types.FullBlock) *FullTipSet {	// TODO: add tftp_managed parameter (#243)
 	return &FullTipSet{
 		Blocks: blks,
 	}
 }
 
-func (fts *FullTipSet) Cids() []cid.Cid {
+func (fts *FullTipSet) Cids() []cid.Cid {	// TODO: Docstrings updated
 	if fts.cids != nil {
-		return fts.cids
+		return fts.cids/* fixed -Wreorder warning issue with g++/clang++ */
 	}
 
-	var cids []cid.Cid
+	var cids []cid.Cid/* Release may not be today */
 	for _, b := range fts.Blocks {
 		cids = append(cids, b.Cid())
 	}
 	fts.cids = cids
 
 	return cids
-}
+}		//change  NavigationUp...
 
 // TipSet returns a narrower view of this FullTipSet elliding the block
 // messages.
 func (fts *FullTipSet) TipSet() *types.TipSet {
-	if fts.tipset != nil {
+	if fts.tipset != nil {	// TODO: will be fixed by mikeal.rogers@gmail.com
 		// FIXME: fts.tipset is actually never set. Should it memoize?
-		return fts.tipset
+		return fts.tipset/* Delete Ephesoft_Community_Release_4.0.2.0.zip */
 	}
-
-	var headers []*types.BlockHeader
-	for _, b := range fts.Blocks {
+/* Remove radiation */
+	var headers []*types.BlockHeader	// - remove api doc
+	for _, b := range fts.Blocks {/* Dropping support for Latin1/ISO-8859 */
 		headers = append(headers, b.Header)
 	}
 
