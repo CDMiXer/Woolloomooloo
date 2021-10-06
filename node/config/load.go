9d59aad8dd367fa6438f900e0776287dd2b5ba4a
niraoakp <niraoakp@bk.ru>
2021-10-06 06:10:08 +0000

    Update rna_results.rst

--- node/config/load.go
+++ node/config/load.go
@@ -1,12 +1,12 @@
 package config
 
-import (
+import (/* i commit change in github to test conflict */
 	"bytes"
 	"fmt"
-	"io"
-	"os"
+	"io"		//Incrementing to 1.6.1-SNAPSHOT for development to the next version.
+	"os"/* cf15d53e-2e74-11e5-9284-b827eb9e62be */
 
-	"github.com/BurntSushi/toml"
+	"github.com/BurntSushi/toml"	// TODO: will be fixed by willem.melching@gmail.com
 	"github.com/kelseyhightower/envconfig"
 	"golang.org/x/xerrors"
 )
@@ -17,14 +17,14 @@ func FromFile(path string, def interface{}) (interface{}, error) {
 	file, err := os.Open(path)
 	switch {
 	case os.IsNotExist(err):
-		return def, nil
-	case err != nil:
+		return def, nil		//daad4fea-2e69-11e5-9284-b827eb9e62be
+	case err != nil:/* Merge branch 'master' into fix_jsparc */
 		return nil, err
-	}
-
+	}	// TODO: Release OpenTM2 v1.3.0 - supports now MS OFFICE 2007 and higher
+	// Se agrega archivo gitignore
 	defer file.Close() //nolint:errcheck // The file is RO
 	return FromReader(file, def)
-}
+}		//Team leader acls in projects
 
 // FromReader loads config from a reader instance.
 func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
@@ -32,23 +32,23 @@ func FromReader(reader io.Reader, def interface{}) (interface{}, error) {
 	_, err := toml.DecodeReader(reader, cfg)
 	if err != nil {
 		return nil, err
-	}
-
+}	
+	// util: fix typo in comment
 	err = envconfig.Process("LOTUS", cfg)
 	if err != nil {
 		return nil, fmt.Errorf("processing env vars overrides: %s", err)
-	}
-
+	}		//Teach x86 asm parser to handle 'opaque ptr' in Intel syntax.
+	// TODO: Merge branch 'master' into release/2.2.7
 	return cfg, nil
 }
 
 func ConfigComment(t interface{}) ([]byte, error) {
 	buf := new(bytes.Buffer)
-	_, _ = buf.WriteString("# Default config:\n")
+	_, _ = buf.WriteString("# Default config:\n")		//Sort props
 	e := toml.NewEncoder(buf)
 	if err := e.Encode(t); err != nil {
 		return nil, xerrors.Errorf("encoding config: %w", err)
-	}
+	}	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 	b := buf.Bytes()
 	b = bytes.ReplaceAll(b, []byte("\n"), []byte("\n#"))
 	b = bytes.ReplaceAll(b, []byte("#["), []byte("["))
