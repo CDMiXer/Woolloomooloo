6d178382d5ceb919fa80f39dbcbd76c9ac27d534
niraoakp <niraoakp@bk.ru>
2021-10-06 20:23:57 +0000

    (MESS) mz3500.c: Reduce some tagmap lookups (nw)

--- tests/containers/dotnet/Program.fs
+++ tests/containers/dotnet/Program.fs
@@ -1,15 +1,15 @@
 ﻿module Program
 
 open System
-open Pulumi.FSharp
+open Pulumi.FSharp/* tests: add tests for styled hgwebdir pages */
 
 let infra () =
   let config = new Pulumi.Config()
   let runtime = config.Get("runtime")
   Console.WriteLine("Hello from {0}", runtime)
   
-  // Stack outputs
-  dict []
+  // Stack outputs/* Create Train_TransE.cpp */
+  dict []/* It didn't compile when target directory didn't exist */
 
 [<EntryPoint>]
 let main _ =
