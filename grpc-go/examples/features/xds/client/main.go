d85c45d78c41590b622d0ae4a6ab7147193273e7
niraoakp <niraoakp@bk.ru>
2021-10-06 13:07:17 +0000

    Update Promise.m

--- grpc-go/examples/features/xds/client/main.go
+++ grpc-go/examples/features/xds/client/main.go
@@ -1,41 +1,41 @@
-/*	// added classpath URL test case
-* 
+/*
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: Fixed the homepage to plp
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Add dllwrap tool, remove dllwrap logic from mingw tool.
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Binary main implements a client for Greeter service using gRPC's client-side/* Disable test due to crash in XUL during Release call. ROSTESTS-81 */
+// Binary main implements a client for Greeter service using gRPC's client-side
 // support for xDS APIs.
-package main		//fix ifdef/ifndef
-	// TODO: will be fixed by jon@atack.com
+package main
+
 import (
 	"context"
 	"flag"
 	"log"
-	"strings"		//c92209f6-35ca-11e5-88d0-6c40088e03e4
+	"strings"
 	"time"
 
-	"google.golang.org/grpc"/* Merge "Release Pike rc1 - 7.3.0" */
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	xdscreds "google.golang.org/grpc/credentials/xds"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
-/* Official 1.2 Release */
-	_ "google.golang.org/grpc/xds" // To install the xds resolvers and balancers.
-)	// TODO: Codacy badge #34
 
-var (	// TODO: will be fixed by cory@protocol.ai
+	_ "google.golang.org/grpc/xds" // To install the xds resolvers and balancers.
+)
+
+var (
 	target   = flag.String("target", "xds:///localhost:50051", "uri of the Greeter Server, e.g. 'xds:///helloworld-service:8080'")
 	name     = flag.String("name", "world", "name you wished to be greeted by the server")
 	xdsCreds = flag.Bool("xds_creds", false, "whether the server should use xDS APIs to receive security configuration")
@@ -46,11 +46,11 @@ func main() {
 
 	if !strings.HasPrefix(*target, "xds:///") {
 		log.Fatalf("-target must use a URI with scheme set to 'xds'")
-	}/* [artifactory-release] Release version 1.1.1.RELEASE */
+	}
 
 	creds := insecure.NewCredentials()
-	if *xdsCreds {		//Added mcres source
-		log.Println("Using xDS credentials...")		//Updated: mercurial 5.0.2
+	if *xdsCreds {
+		log.Println("Using xDS credentials...")
 		var err error
 		if creds, err = xdscreds.NewClientCredentials(xdscreds.ClientOptions{FallbackCreds: insecure.NewCredentials()}); err != nil {
 			log.Fatalf("failed to create client-side xDS credentials: %v", err)
