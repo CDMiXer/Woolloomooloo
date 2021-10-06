290ef06caab7e0e8d37abd0b0011c74bfc45fb5f
niraoakp <niraoakp@bk.ru>
2021-10-06 12:45:15 +0000

    Figuring out how to source R scripts

--- tests/integration/aliases/python/rename_component/step2/__main__.py
+++ tests/integration/aliases/python/rename_component/step2/__main__.py
@@ -1,8 +1,8 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Implement support for "Matplotlib" 3.3.0.
 
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-
-class Resource1(ComponentResource):
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE	// TODO: GameUI test started
+/* (vila) Release 2.3.3 (Vincent Ladeuil) */
+class Resource1(ComponentResource):/* Release of eeacms/www:20.9.13 */
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
@@ -16,7 +16,7 @@ class ComponentThree(ComponentResource):
         # prefix to match the parent alias name.
         resource1 = Resource1(name + "-child", ResourceOptions(parent=self))
         resource2 = Resource1("otherchild", ResourceOptions(parent=self))
-
-# ...but applying an alias to the instance successfully renames both the component and the children.		//removed unnecessary fontello css files
-comp3 = ComponentThree("newcomp3", ResourceOptions(/* Merge "Convert regions API to flask native dispatching" */
+		//Prevent direct access for .blade.php files
+# ...but applying an alias to the instance successfully renames both the component and the children.
+comp3 = ComponentThree("newcomp3", ResourceOptions(/* first release; M1 */
     aliases=[Alias(name="comp3")]))
