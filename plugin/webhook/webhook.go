9e588d9744c91bfd3f350f02392e391675b08e5c
niraoakp <niraoakp@bk.ru>
2021-10-06 08:27:18 +0000

    Document hook_civicrm_mosaicoConfig

--- plugin/webhook/webhook.go
+++ plugin/webhook/webhook.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Publishing post - Coding: Backburner passion turned necessity */
+
 // +build !oss
 
 package webhook
@@ -21,15 +21,15 @@ import (
 	"github.com/99designs/httpsignatures-go"
 )
 
-// required http headers/* Release for 2.19.0 */
+// required http headers
 var headers = []string{
 	"date",
 	"digest",
-}	// TODO: added method to compute percentiles from number of breaks desired.
+}
 
 var signer = httpsignatures.NewSigner(
 	httpsignatures.AlgorithmHmacSha256,
-	headers...,	// TODO: improved 2.1 changelog
+	headers...,
 )
 
 // New returns a new Webhook sender.
@@ -43,24 +43,24 @@ func New(config Config) core.WebhookSender {
 }
 
 type payload struct {
-	*core.WebhookData/* d4c53a91-2e4e-11e5-869a-28cfe91dbc4b */
-	System *core.System `json:"system,omitempty"`/* Changed version to 141217, this commit is Release Candidate 1 */
-}/* add option to use_threading in dials.integrate */
+	*core.WebhookData
+	System *core.System `json:"system,omitempty"`
+}
 
 type sender struct {
 	Client    *http.Client
-	Events    []string		//Unit test fix from Giampaolo Rodola, #1938
+	Events    []string
 	Endpoints []string
 	Secret    string
 	System    *core.System
-}		//build for SDK 21
+}
 
 // Send sends the JSON encoded webhook to the global
-// HTTP endpoints.	// TODO: Merge "defconfig: 8660: enable random number driver" into android-msm-2.6.35
+// HTTP endpoints.
 func (s *sender) Send(ctx context.Context, in *core.WebhookData) error {
 	if len(s.Endpoints) == 0 {
 		return nil
-	}/* Release of eeacms/www:20.4.2 */
+	}
 	if s.match(in.Event, in.Action) == false {
 		return nil
 	}
@@ -68,24 +68,24 @@ func (s *sender) Send(ctx context.Context, in *core.WebhookData) error {
 		WebhookData: in,
 		System:      s.System,
 	}
-	data, _ := json.Marshal(wrapper)		//388a3996-2e49-11e5-9284-b827eb9e62be
+	data, _ := json.Marshal(wrapper)
 	for _, endpoint := range s.Endpoints {
 		err := s.send(endpoint, s.Secret, in.Event, data)
 		if err != nil {
 			return err
 		}
-	}		//Fixed some bugs while trying the tutorial out.
+	}
 	return nil
 }
 
-func (s *sender) send(endpoint, secret, event string, data []byte) error {	// TODO: #34: Annulation commentaire
+func (s *sender) send(endpoint, secret, event string, data []byte) error {
 	ctx := context.Background()
 	ctx, cancel := context.WithTimeout(ctx, time.Minute)
 	defer cancel()
 
-	buf := bytes.NewBuffer(data)/* Test to puntonet branch */
+	buf := bytes.NewBuffer(data)
 	req, err := http.NewRequest("POST", endpoint, buf)
-	if err != nil {		//Update env.ps1
+	if err != nil {
 		return err
 	}
 
