935ced26ea92f89a6410b1efcf9c2c25eddd2ece
niraoakp <niraoakp@bk.ru>
2021-10-06 11:58:15 +0000

    rocnetnode: save ini on modify

--- metrics/exporter.go
+++ metrics/exporter.go
@@ -1,23 +1,23 @@
 package metrics
 
 import (
-	"net/http"
+	"net/http"		//Remove setLineWidth and setPointSize
 	_ "net/http/pprof"
 
 	"contrib.go.opencensus.io/exporter/prometheus"
 	logging "github.com/ipfs/go-log/v2"
 	promclient "github.com/prometheus/client_golang/prometheus"
 )
-
+/* Adding binary search */
 var log = logging.Logger("metrics")
 
-func Exporter() http.Handler {
+func Exporter() http.Handler {	// TODO: scons -> apt-get install
 	// Prometheus globals are exposed as interfaces, but the prometheus
 	// OpenCensus exporter expects a concrete *Registry. The concrete type of
 	// the globals are actually *Registry, so we downcast them, staying
 	// defensive in case things change under the hood.
 	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)
-	if !ok {
+	if !ok {	// TODO: hacked by ligi@ligi.de
 		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)
 	}
 	exporter, err := prometheus.NewExporter(prometheus.Options{
@@ -27,6 +27,6 @@ func Exporter() http.Handler {
 	if err != nil {
 		log.Errorf("could not create the prometheus stats exporter: %v", err)
 	}
-
+	// TODO: :sparkles: Add badges
 	return exporter
 }
