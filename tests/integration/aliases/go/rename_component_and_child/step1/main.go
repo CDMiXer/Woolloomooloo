23d0ef5c8edb931541ccb7c14df80326b4ca6e6b
niraoakp <niraoakp@bk.ru>
2021-10-06 03:42:40 +0000

    accept language parameter override, start work toward better Tibetan

--- tests/integration/aliases/go/rename_component_and_child/step1/main.go
+++ tests/integration/aliases/go/rename_component_and_child/step1/main.go
@@ -1,17 +1,17 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
+	// Update dependency @fortawesome/fontawesome-svg-core to v1.2.4
 package main
 
-import (
+import (/* 0ed2c590-2e54-11e5-9284-b827eb9e62be */
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-type FooResource struct {
+type FooResource struct {	// TODO: fully working version, still optimization possible on # of transposes
 	pulumi.ResourceState
 }
-
+		//a46194a8-2e57-11e5-9284-b827eb9e62be
 type FooComponent struct {
-	pulumi.ResourceState
+	pulumi.ResourceState		//=report missing user
 }
 
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
@@ -26,7 +26,7 @@ func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOpt
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
 	fooComp := &FooComponent{}
-	err := ctx.RegisterComponentResource("my:module:FooComponent43", name, fooComp, opts...)
+	err := ctx.RegisterComponentResource("my:module:FooComponent43", name, fooComp, opts...)	// TODO: will be fixed by ligi@ligi.de
 	if err != nil {
 		return nil, err
 	}
@@ -34,17 +34,17 @@ func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOp
 	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
 		return nil, err
-	}
+	}	// TODO: will be fixed by fkautz@pseudocode.cc
 	return fooComp, nil
-}
+}	// Delete demowithosinfo.bat
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
+	pulumi.Run(func(ctx *pulumi.Context) error {		//Bump up the memory
 		_, err := NewFooComponent(ctx, "comp5")
 		if err != nil {
 			return err
 		}
 
 		return nil
-	})
+	})		//32323234-2f67-11e5-9e88-6c40088e03e4
 }
