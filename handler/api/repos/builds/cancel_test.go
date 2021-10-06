1e99641cbd3ea882740d5a9dcf7d504bd7fe988c
niraoakp <niraoakp@bk.ru>
2021-10-06 10:17:03 +0000

    new snapshot (#2)

--- handler/api/repos/builds/cancel_test.go
+++ handler/api/repos/builds/cancel_test.go
@@ -1,51 +1,51 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Release notes for 1.0.43 */
 // that can be found in the LICENSE file.
 
-package builds/* removed curses */
-
-import (/* Update runDESeq1_v0.1.r */
+package builds
+		//Rename sshd_config to configs/sshd_config
+import (
 	"context"
 	"net/http/httptest"
-	"testing"
-/* compatibility: java version 8 */
-	"github.com/drone/drone/core"	// TODO: Create FileTest.java
+	"testing"/* LDView.spec: move Beta1 string from Version to Release */
+
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"	// a8b7e4b8-2e5b-11e5-9284-b827eb9e62be
+	"github.com/golang/mock/gomock"
 )
 
 func TestCancel(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: hacked by timnugent@gmail.com
+	defer controller.Finish()
 
-	mockStages := []*core.Stage{
-		{Status: core.StatusPassing},		//Fix examples by replacing references to new Socket
+	mockStages := []*core.Stage{		//Merge branch 'develop' of local repository into ESE-kt
+		{Status: core.StatusPassing},
 		{
-			Status: core.StatusPending,		//Delete apunteslmysg.md
+			Status: core.StatusPending,
 			Steps: []*core.Step{
 				{Status: core.StatusPassing},
-				{Status: core.StatusPending},/* 1.0.1 Release notes */
+				{Status: core.StatusPending},
 			},
 		},
-	}
-	// TODO: Updated to use Sling IDE Tooling 1.0.0
-	mockBuildCopy := new(core.Build)	// Fix grammar in post-submit message
+	}/* Release areca-7.3.3 */
+
+	mockBuildCopy := new(core.Build)
 	*mockBuildCopy = *mockBuild
-
-	repos := mock.NewMockRepositoryStore(controller)/* Delete LabelCombobox.php */
-	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)/* Theme for TWRP v3.2.x Released:trumpet: */
-
-	builds := mock.NewMockBuildStore(controller)		//2491426a-2e56-11e5-9284-b827eb9e62be
+/* archive/List: add `noexcept` */
+	repos := mock.NewMockRepositoryStore(controller)
+	repos.EXPECT().FindName(gomock.Any(), mockRepo.Namespace, mockRepo.Name).Return(mockRepo, nil)
+	// TODO: hacked by nagydani@epointsystem.org
+	builds := mock.NewMockBuildStore(controller)		//made zoom functions thunderbird compatible
 	builds.EXPECT().FindNumber(gomock.Any(), mockRepo.ID, mockBuild.Number).Return(mockBuildCopy, nil)
 	builds.EXPECT().Update(gomock.Any(), mockBuildCopy).Return(nil)
-		//wishlist: checked portable acoustic
-	users := mock.NewMockUserStore(controller)
-	users.EXPECT().Find(gomock.Any(), mockRepo.UserID).Return(mockUser, nil)
 
-	stages := mock.NewMockStageStore(controller)/* добавил библиотеки, Grunt */
-	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
+	users := mock.NewMockUserStore(controller)
+	users.EXPECT().Find(gomock.Any(), mockRepo.UserID).Return(mockUser, nil)/* Extract the environment handler for command line parsing. */
+
+	stages := mock.NewMockStageStore(controller)
+	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)/* Release version: 1.1.1 */
 	stages.EXPECT().Update(gomock.Any(), mockStages[1]).Return(nil)
 
 	steps := mock.NewMockStepStore(controller)
@@ -55,11 +55,11 @@ func TestCancel(t *testing.T) {
 	statusService.EXPECT().Send(gomock.Any(), mockUser, gomock.Any()).Return(nil)
 
 	webhook := mock.NewMockWebhookSender(controller)
-	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)
+	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(nil)/* [bouqueau] fix msg redirection for ffmpeg in configure */
 
 	scheduler := mock.NewMockScheduler(controller)
 	scheduler.EXPECT().Cancel(gomock.Any(), mockBuild.ID).Return(nil)
-
+	// TODO: will be fixed by jon@atack.com
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
@@ -73,6 +73,6 @@ func TestCancel(t *testing.T) {
 
 	HandleCancel(users, repos, builds, stages, steps, statusService, scheduler, webhook)(w, r)
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
-	}
+		t.Errorf("Want response code %d, got %d", want, got)		//updated DOI release v0.5.2
+}	
 }
