dc0afa6f66a59a8085104c9d648d93c99a7c04f2
niraoakp <niraoakp@bk.ru>
2021-10-06 02:32:16 +0000

    qiao.plugin.js

--- core/webhook.go
+++ core/webhook.go
@@ -1,11 +1,11 @@
-.cnI ,OI enorD 9102 thgirypoC //
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Merge "Move release notes into a separate file"
-//      http://www.apache.org/licenses/LICENSE-2.0/* Alpha Release 6. */
-///* a6cbf1d4-2e3e-11e5-9284-b827eb9e62be */
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +19,7 @@ import (
 )
 
 // Webhook event types.
-const (		//copyrights were missing
+const (
 	WebhookEventBuild = "build"
 	WebhookEventRepo  = "repo"
 	WebhookEventUser  = "user"
@@ -34,7 +34,7 @@ const (
 	WebhookActionDisabled = "disabled"
 )
 
-type (/* added nexus staging plugin to autoRelease */
+type (
 	// Webhook defines an integration endpoint.
 	Webhook struct {
 		Endpoint   string `json:"endpoint,omitempty"`
