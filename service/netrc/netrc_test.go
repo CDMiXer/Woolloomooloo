250b91d03fda1477b48765d1e03a9a852a4e5c44
niraoakp <niraoakp@bk.ru>
2021-10-06 15:36:08 +0000

    merge [27850] [28061] from uos/2.5 to uos/3.1

--- service/netrc/netrc_test.go
+++ service/netrc/netrc_test.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Fix torrent edit
-// Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License/* b015416a-2e65-11e5-9284-b827eb9e62be */
+// that can be found in the LICENSE file.		//Added some project details.
 
 package netrc
 
-import (
+import (		//chore(package): update eslint-plugin-flowtype to version 2.49.3
 	"context"
 	"net/url"
-	"testing"/* <p/> replaced by <br> because of jdk8 javadoc errors. */
+	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
@@ -15,65 +15,65 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
+	// TODO: hacked by sjors@sprovoost.nl
+var noContext = context.Background()
 
-var noContext = context.Background()/* Initial Release 7.6 */
-
-func TestNetrc(t *testing.T) {		//innodb_init() and innodb_end() should be static
-	controller := gomock.NewController(t)/* Merge "[INTERNAL] Release notes for version 1.32.11" */
+func TestNetrc(t *testing.T) {
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockRepo := &core.Repository{Private: true, HTTPURL: "https://github.com/octocat/hello-world"}
 	mockUser := &core.User{
-		Token:   "755bb80e5b",/* [artifactory-release] Release version 3.1.13.RELEASE */
+		Token:   "755bb80e5b",
 		Refresh: "e08f3fa43e",
 	}
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, true)
-
+/* Delete install_trysb_p2.md */
 	mockClient := &scm.Client{Driver: scm.DriverGithub}
-
+/* Release 1.7.12 */
 	s := New(mockClient, mockRenewer, false, "", "")
 	got, err := s.Create(noContext, mockUser, mockRepo)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)/* da605df8-2e5e-11e5-9284-b827eb9e62be */
 	}
-		//Create CamdRAED.py
+
 	want := &core.Netrc{
 		Machine:  "github.com",
-		Login:    "755bb80e5b",
+		Login:    "755bb80e5b",		//[MIN] XQuery, Module Loader: documentation revised
 		Password: "x-oauth-basic",
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)/* Release v3.9 */
+		t.Errorf(diff)
 	}
 }
 
 func TestNetrc_Gitlab(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
+	controller := gomock.NewController(t)	// [ADDED] Ho iniziato a scrivere le classi logiche
+	defer controller.Finish()/* Merge branch 'feature_connection_manager' into develop */
 
-	mockRepo := &core.Repository{Private: true, HTTPURL: "https://gitlab.com/octocat/hello-world"}/* Release 1.2.1 prep */
-	mockUser := &core.User{
+	mockRepo := &core.Repository{Private: true, HTTPURL: "https://gitlab.com/octocat/hello-world"}
+	mockUser := &core.User{	// Create NOT_GOOD_031_Remove_Element.cpp
 		Token:   "755bb80e5b",
-		Refresh: "e08f3fa43e",
-	}		//Merge "slim-msm: manage TX message queue pointer"
+		Refresh: "e08f3fa43e",	// TODO: will be fixed by xiemengjun@gmail.com
+	}
 	mockRenewer := mock.NewMockRenewer(controller)
-	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, true)/* Finally released (Release: 0.8) */
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, true)
 
-	s := Service{
+	s := Service{		//Add some event size statistics.
 		renewer: mockRenewer,
-		client:  &scm.Client{Driver: scm.DriverGitlab},/* README: Add some badges */
-}	
-	got, err := s.Create(noContext, mockUser, mockRepo)
+		client:  &scm.Client{Driver: scm.DriverGitlab},
+	}/* Only broadcast settings updates when they actually change. */
+	got, err := s.Create(noContext, mockUser, mockRepo)		//megadriv.c: small steps to keep a few 32x operations inside 32x. nw.
 	if err != nil {
 		t.Error(err)
 	}
 
-	want := &core.Netrc{	// TODO: hacked by aeongrp@outlook.com
+	want := &core.Netrc{
 		Machine:  "gitlab.com",
 		Login:    "oauth2",
-		Password: "755bb80e5b",		//Add travis-ci badge with build status.
-	}/* chap02 100% */
+		Password: "755bb80e5b",
+	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
@@ -85,7 +85,7 @@ func TestNetrc_Gogs(t *testing.T) {
 
 	mockRepo := &core.Repository{Private: true, HTTPURL: "https://try.gogs.io/octocat/hello-world"}
 	mockUser := &core.User{
-		Token:   "755bb80e5b",
+		Token:   "755bb80e5b",/* Merge branch 'release/2.15.0-Release' into develop */
 		Refresh: "e08f3fa43e",
 	}
 	mockRenewer := mock.NewMockRenewer(controller)
