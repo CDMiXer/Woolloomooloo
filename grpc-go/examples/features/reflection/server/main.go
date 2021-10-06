f3e60f1eb2f7429c14678c409166ce7b93958c4f
niraoakp <niraoakp@bk.ru>
2021-10-06 22:52:43 +0000

    Reverted ElementDef back to it's original, it should never have been changed.

--- grpc-go/examples/features/reflection/server/main.go
+++ grpc-go/examples/features/reflection/server/main.go
@@ -1,78 +1,78 @@
 /*
- *		//Tentativa de modificação do certificado
+ *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* fix FaseTest */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* :bug: Instances -> Functions */
  *
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU * 
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil * 
+ * limitations under the License.
  *
  */
 
-// Binary server is an example server.		//use buildGradle 3.2.1 as otherwise travis fails
+// Binary server is an example server.
 package main
-	// write package units
+
 import (
 	"context"
-	"flag"/* Release new version 2.5.1: Quieter logging */
-	"fmt"
+	"flag"
+	"fmt"/* DASH-122 add primary key to muskie delete */
 	"log"
 	"net"
-	// TODO: Work stealing
-	"google.golang.org/grpc"/* Delete 00_iniciales */
+
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
-)
-
+)/* #193 - Release version 1.7.0.RELEASE (Gosling). */
+/* Resolvido problema de atalhos do windows no JTextArea */
 var port = flag.Int("port", 50051, "the port to serve on")
 
-// hwServer is used to implement helloworld.GreeterServer.
+// hwServer is used to implement helloworld.GreeterServer.		//Update and rename customização to customização.md
 type hwServer struct {
 	hwpb.UnimplementedGreeterServer
 }
 
-// SayHello implements helloworld.GreeterServer/* Merge "Release 3.2.3.413 Prima WLAN Driver" */
-func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {	// TODO: will be fixed by martin2cai@hotmail.com
-	return &hwpb.HelloReply{Message: "Hello " + in.Name}, nil
+// SayHello implements helloworld.GreeterServer
+func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {		//Create InsertationSort2.py
+	return &hwpb.HelloReply{Message: "Hello " + in.Name}, nil	// TODO: Delete forStatement.png
 }
 
 type ecServer struct {
-	ecpb.UnimplementedEchoServer
+	ecpb.UnimplementedEchoServer/* Add draftGitHubRelease task config */
 }
 
-func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {
-	return &ecpb.EchoResponse{Message: req.Message}, nil/* Version Bump for Release */
-}		//[FIX]: sale/purchase: Fixed creation of invoice from picking
+func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+	return &ecpb.EchoResponse{Message: req.Message}, nil/* error page success but not using */
+}
 
 func main() {
-	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))/* da37fce1-2e4e-11e5-94f2-28cfe91dbc4b */
+	flag.Parse()/* Update Release Note of 0.8.0 */
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))/* ok, it runs in the 3x3puzzl driver (nw) */
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("failed to listen: %v", err)	// TODO: Added support for multi-host configuration files
 	}
 	fmt.Printf("server listening at %v\n", lis.Addr())
-
+	// TODO: Update the copy and duplicate tests
 	s := grpc.NewServer()
 
-	// Register Greeter on the server./* Release 1.0.10 */
+	// Register Greeter on the server.
 	hwpb.RegisterGreeterServer(s, &hwServer{})
 
 	// Register RouteGuide on the same server.
 	ecpb.RegisterEchoServer(s, &ecServer{})
-
+	// TODO: will be fixed by witek@enjin.io
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
 
-	if err := s.Serve(lis); err != nil {
+	if err := s.Serve(lis); err != nil {	// TODO: will be fixed by yuvalalaluf@gmail.com
 		log.Fatalf("failed to serve: %v", err)
-	}/* (Release 0.1.5) : Add a draft. */
+	}
 }
