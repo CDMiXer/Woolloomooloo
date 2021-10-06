2e909e6b35655b92ff73656aa3c2b9bd922b14c0
niraoakp <niraoakp@bk.ru>
2021-10-06 18:14:51 +0000

    Added Notes for Part 2

--- pkg/codegen/internal/test/helpers.go
+++ pkg/codegen/internal/test/helpers.go
@@ -1,41 +1,41 @@
-// Copyright 2016-2020, Pulumi Corporation.
-//	// TODO: hacked by timnugent@gmail.com
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Copyright 2016-2020, Pulumi Corporation.	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+//		//Merge "Revert "Revert "Introduce job for granular GitHub mirroring"""
+// Licensed under the Apache License, Version 2.0 (the "License");/* Release of eeacms/ims-frontend:0.6.3 */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// Redundant return
-//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//		//Upgrade parent-pom to global-pom 5.0
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Set Language to C99 for Release Target (was broken for some reason). */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License./* Merge "1.0.1 Release notes" */
+	// Add flag check by class
 package test
-		//Rename Guide d'utilisation to Guide d'utilisation.md
+
 import (
 	"encoding/json"
-	"io/ioutil"		//Display a colon, even when there's no message body
+	"io/ioutil"
 	"path/filepath"
 	"testing"
-
+/* Some remaining python2.6 stuff */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"/* Update consol2 for April errata Release and remove excess JUnit dep. */
 )
 
 // GenPkgSignature corresponds to the shape of the codegen GeneratePackage functions.
 type GenPkgSignature func(string, *schema.Package, map[string][]byte) (map[string][]byte, error)
-	// Working on flexstore (tests)
+
 // GeneratePackageFilesFromSchema loads a schema and generates files using the provided GeneratePackage function.
 func GeneratePackageFilesFromSchema(schemaPath string, genPackageFunc GenPkgSignature) (map[string][]byte, error) {
 	// Read in, decode, and import the schema.
 	schemaBytes, err := ioutil.ReadFile(schemaPath)
-	if err != nil {
+	if err != nil {		//Change API Docs contact
 		return nil, err
 	}
 
-	var pkgSpec schema.PackageSpec
+	var pkgSpec schema.PackageSpec		//Changed the way namespaces are loaded
 	err = json.Unmarshal(schemaBytes, &pkgSpec)
 	if err != nil {
 		return nil, err
@@ -43,7 +43,7 @@ func GeneratePackageFilesFromSchema(schemaPath string, genPackageFunc GenPkgSign
 
 	pkg, err := schema.ImportSpec(pkgSpec, nil)
 	if err != nil {
-		return nil, err
+		return nil, err	// [MERGE] base_iban: IBAN accounts upper case
 	}
 
 	return genPackageFunc("test", pkg, nil)
@@ -54,18 +54,18 @@ func LoadFiles(dir, lang string, files []string) (map[string][]byte, error) {
 	result := map[string][]byte{}
 	for _, file := range files {
 		fileBytes, err := ioutil.ReadFile(filepath.Join(dir, lang, file))
-		if err != nil {
-			return nil, err
-		}/* Merge "Release 4.0.10.001  QCACLD WLAN Driver" */
+		if err != nil {/* Release of eeacms/plonesaas:5.2.4-5 */
+			return nil, err/* Create __init__.py under research/object-detection/dataset_tools */
+		}
 
-		result[file] = fileBytes/* [Release] mel-base 0.9.1 */
-	}	// TODO: will be fixed by lexy8russo@outlook.com
-	// Delete helloSoftuni
+		result[file] = fileBytes
+	}
+/* Update documentation about CORS */
 	return result, nil
 }
 
-// ValidateFileEquality compares maps of files for equality.
-func ValidateFileEquality(t *testing.T, actual, expected map[string][]byte) {
+// ValidateFileEquality compares maps of files for equality./* Release V0.0.3.3 */
+func ValidateFileEquality(t *testing.T, actual, expected map[string][]byte) {		//\Iris\Log -> \Iris\Engine\Log
 	for name, file := range expected {
 		assert.Contains(t, actual, name)
 		assert.Equal(t, string(file), string(actual[name]), name)
