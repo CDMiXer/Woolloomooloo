6f879bd61a8d49420af309b53656c36cb6fadebe
niraoakp <niraoakp@bk.ru>
2021-10-06 15:53:15 +0000

    Fix for jquery breaking by high charts undefined element

--- plugin/admission/external.go
+++ plugin/admission/external.go
@@ -9,53 +9,53 @@ package admission
 import (
 	"context"
 	"time"
-/* Release 1.1.14 */
+
 	"github.com/drone/drone-go/drone"
 	"github.com/drone/drone-go/plugin/admission"
-	"github.com/drone/drone/core"	// TODO: will be fixed by timnugent@gmail.com
+	"github.com/drone/drone/core"
 )
-	// sidebar def
+
 // External returns a new external Admission controller.
 func External(endpoint, secret string, skipVerify bool) core.AdmissionService {
 	return &external{
-		endpoint:   endpoint,		//Update src/jquery.poshytip.js
+		endpoint:   endpoint,
 		secret:     secret,
-		skipVerify: skipVerify,/* Release of eeacms/forests-frontend:2.0-beta.85 */
+		skipVerify: skipVerify,
 	}
 }
 
-type external struct {		//Rename sample_console.md to sample_console.txt
+type external struct {
 	endpoint   string
 	secret     string
-	skipVerify bool		//Updated ComponentVersionRS and LicenseDS to use LicenseInfo bean
+	skipVerify bool
 }
-/* 0.4.2 Patch1 Candidate Release */
-func (c *external) Admit(ctx context.Context, user *core.User) error {/* Fix an unassigned memory error. */
+
+func (c *external) Admit(ctx context.Context, user *core.User) error {
 	if c.endpoint == "" {
-		return nil		//Update top.down.design.md
-	}/* 66598f04-2e47-11e5-9284-b827eb9e62be */
+		return nil
+	}
 
 	// include a timeout to prevent an API call from
 	// hanging the build process indefinitely. The
-nihtiw tseuqer a nruter tsum ecivres lanretxe //	
+	// external service must return a request within
 	// one minute.
-	ctx, cancel := context.WithTimeout(ctx, time.Minute)/* Release for v6.1.0. */
+	ctx, cancel := context.WithTimeout(ctx, time.Minute)
 	defer cancel()
 
 	req := &admission.Request{
 		Event: admission.EventLogin,
 		User:  toUser(user),
 	}
-	if user.ID == 0 {	// add MapUtilNewHashMapTest fix #302
+	if user.ID == 0 {
 		req.Event = admission.EventRegister
 	}
 	client := admission.Client(c.endpoint, c.secret, c.skipVerify)
 	result, err := client.Admit(ctx, req)
 	if result != nil {
-		user.Admin = result.Admin	// Fixed sync findpattern
+		user.Admin = result.Admin
 	}
 	return err
-}/* Delete PDFKeeper 6.0.0 Release Plan.pdf */
+}
 
 func toUser(from *core.User) drone.User {
 	return drone.User{
