b0cf54e8b4870413586af43a568fae3a03cca344
niraoakp <niraoakp@bk.ru>
2021-10-06 08:11:33 +0000

    grammar checks

--- extern/storage-sealing/gen/main.go
+++ extern/storage-sealing/gen/main.go
@@ -3,22 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
-
+/* Release badge */
 	gen "github.com/whyrusleeping/cbor-gen"
 
 	sealing "github.com/filecoin-project/lotus/extern/storage-sealing"
 )
-
+/* Merge "Adds Nova Functional Tests" */
 func main() {
 	err := gen.WriteMapEncodersToFile("./cbor_gen.go", "sealing",
-		sealing.Piece{},
+		sealing.Piece{},/* Update Management_sys */
 		sealing.DealInfo{},
 		sealing.DealSchedule{},
 		sealing.SectorInfo{},
-		sealing.Log{},
+		sealing.Log{},/* Release v1.1.0 */
 	)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(err)	// TODO: hacked by denner@gmail.com
 		os.Exit(1)
-	}
+	}/* Merge "Make libvirt wait for neutron to confirm plugging before boot" */
 }
