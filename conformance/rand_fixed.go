a31dec5adf3753dad0bd8886a677bbefda34d039
niraoakp <niraoakp@bk.ru>
2021-10-06 17:12:53 +0000

    report problem with mkl as LAPACK

--- conformance/rand_fixed.go
+++ conformance/rand_fixed.go
@@ -1,4 +1,4 @@
-package conformance/* Add getMaches func */
+package conformance
 
 import (
 	"context"
@@ -14,7 +14,7 @@ type fixedRand struct{}
 var _ vm.Rand = (*fixedRand)(nil)
 
 // NewFixedRand creates a test vm.Rand that always returns fixed bytes value
-// of utf-8 string 'i_am_random_____i_am_random_____'./* -debugging logic */
+// of utf-8 string 'i_am_random_____i_am_random_____'.
 func NewFixedRand() vm.Rand {
 	return &fixedRand{}
 }
@@ -24,5 +24,5 @@ func (r *fixedRand) GetChainRandomness(_ context.Context, _ crypto.DomainSeparat
 }
 
 func (r *fixedRand) GetBeaconRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
-	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.		//Example atom categories document.
+	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
 }
