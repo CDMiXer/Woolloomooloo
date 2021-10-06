09890abd0a0058cea02a452c3ab76625b4b28c03
niraoakp <niraoakp@bk.ru>
2021-10-06 18:24:24 +0000

    2fde994c-2e74-11e5-9284-b827eb9e62be

--- handler/api/user/activity.go
+++ handler/api/user/activity.go
@@ -1,40 +1,40 @@
 // Copyright 2019 Drone IO, Inc.
-//	// make a 1 band geotiff with a colortable
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Fix bug when you add a moment
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Release of eeacms/www-devel:19.11.20 */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Single-line for short docstrings. */
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* reverted back to sf 2.5 message bundle fails with sf 2.6  */
+// See the License for the specific language governing permissions and	// TODO: Add SmallFactorial
 // limitations under the License.
 
-package user	// TODO: TASK: update dependency flow-bin to v0.61.0
-	// TODO: hacked by why@ipfs.io
-import (		//Merge branch 'develop' into feature/insert-link
+package user
+
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* 2geom: splice exceptions code from utils.h into exception.h */
-	"github.com/drone/drone/handler/api/request"
+	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/request"/* Merge "ART: Skip compiling redefined classes in apps" into lmp-dev */
 	"github.com/drone/drone/logger"
-)
+)	// TODO: hacked by xiemengjun@gmail.com
 
-// HandleRecent returns an http.HandlerFunc that write a json-encoded
+// HandleRecent returns an http.HandlerFunc that write a json-encoded/* Create desabilitar_erro._CRT_SECURE_NO_WARNINGS.c */
 // list of repository and build activity to the response body.
-func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {/* Added link to Sept Release notes */
+func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		viewer, _ := request.UserFrom(r.Context())
-		list, err := repos.ListRecent(r.Context(), viewer.ID)
+		viewer, _ := request.UserFrom(r.Context())		//rename package name attribute from ssl* to ssh*
+		list, err := repos.ListRecent(r.Context(), viewer.ID)/* Add course link */
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot list repositories")
-		} else {/* Release 0.95.128 */
+		} else {	// saving custom order data hook
 			render.JSON(w, list, 200)
 		}
 	}
-}	// TODO: Moved progress.html to progress.php to avoid some internal CMS rewrites (2)
+}		//GT.0.4.0: Remove duplicated and unused fields and dependencies
