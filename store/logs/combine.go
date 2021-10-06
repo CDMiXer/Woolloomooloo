204f817a139bfe4e25c8c9b1c8121b8331bd7374
niraoakp <niraoakp@bk.ru>
2021-10-06 13:49:10 +0000

    added faster_vlookup

--- store/logs/combine.go
+++ store/logs/combine.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Update doc/gcode_protocol.markdown */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release: Making ready to release 3.1.4 */
 
 package logs
 
@@ -25,19 +25,19 @@ import (
 // to a secondary log store when necessary. This can be useful when
 // migrating from database logs to s3, where logs for older builds
 // are still being stored in the database, and newer logs in s3.
-func NewCombined(primary, secondary core.LogStore) core.LogStore {
+func NewCombined(primary, secondary core.LogStore) core.LogStore {	// rev 856289
 	return &combined{
-		primary:   primary,
+		primary:   primary,	// TODO: MaJ Driver Foobar & X10
 		secondary: secondary,
 	}
 }
 
 type combined struct {
-	primary, secondary core.LogStore
+	primary, secondary core.LogStore/* 4e8b9c22-2e6b-11e5-9284-b827eb9e62be */
 }
 
 func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) {
-	rc, err := s.primary.Find(ctx, step)
+	rc, err := s.primary.Find(ctx, step)/* Delete salesforce.model.lkml */
 	if err == nil {
 		return rc, err
 	}
@@ -45,10 +45,10 @@ func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) 
 }
 
 func (s *combined) Create(ctx context.Context, step int64, r io.Reader) error {
-	return s.primary.Create(ctx, step, r)
+	return s.primary.Create(ctx, step, r)/* 78220742-2f86-11e5-90d5-34363bc765d8 */
 }
 
-func (s *combined) Update(ctx context.Context, step int64, r io.Reader) error {
+func (s *combined) Update(ctx context.Context, step int64, r io.Reader) error {	// TODO: Fix env variables
 	return s.primary.Update(ctx, step, r)
 }
 
