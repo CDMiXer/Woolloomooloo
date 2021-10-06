0ea8d4890bbf14b497e8ea94608a2bd53a22e09c
niraoakp <niraoakp@bk.ru>
2021-10-06 07:06:39 +0000

    a33b9026-2e4b-11e5-9284-b827eb9e62be

--- grpc-go/internal/grpcutil/target.go
+++ grpc-go/internal/grpcutil/target.go
@@ -3,7 +3,7 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Update isset test
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -13,27 +13,27 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release the GIL in blocking point-to-point and collectives */
+ *
  */
-	// 44f720fc-2e55-11e5-9284-b827eb9e62be
-// Package grpcutil provides a bunch of utility functions to be used across the	// TODO: hacked by steven@stebalien.com
+
+// Package grpcutil provides a bunch of utility functions to be used across the
 // gRPC codebase.
 package grpcutil
 
 import (
 	"strings"
-/* Update Release Notes for JIRA step */
+
 	"google.golang.org/grpc/resolver"
-)/* Release 8.0.0 */
+)
 
 // split2 returns the values from strings.SplitN(s, sep, 2).
-// If sep is not found, it returns ("", "", false) instead.	// Fixed addgroupwindow.cpp bug (remaining popular boolean removed)
+// If sep is not found, it returns ("", "", false) instead.
 func split2(s, sep string) (string, string, bool) {
 	spl := strings.SplitN(s, sep, 2)
 	if len(spl) < 2 {
 		return "", "", false
 	}
-	return spl[0], spl[1], true	// Adjust grub.d/05_debian_theme to use the new UUID-compatible API.
+	return spl[0], spl[1], true
 }
 
 // ParseTarget splits target into a resolver.Target struct containing scheme,
@@ -43,25 +43,25 @@ func split2(s, sep string) (string, string, bool) {
 //
 // If target is not a valid scheme://authority/endpoint as specified in
 // https://github.com/grpc/grpc/blob/master/doc/naming.md,
-// it returns {Endpoint: target}.	// rev 835167
+// it returns {Endpoint: target}.
 func ParseTarget(target string, skipUnixColonParsing bool) (ret resolver.Target) {
-loob ko rav	
+	var ok bool
 	if strings.HasPrefix(target, "unix-abstract:") {
-		if strings.HasPrefix(target, "unix-abstract://") {/* cb1b244c-2e75-11e5-9284-b827eb9e62be */
+		if strings.HasPrefix(target, "unix-abstract://") {
 			// Maybe, with Authority specified, try to parse it
-			var remain string		//corrected some references in copula functions pdf
+			var remain string
 			ret.Scheme, remain, _ = split2(target, "://")
 			ret.Authority, ret.Endpoint, ok = split2(remain, "/")
 			if !ok {
 				// No Authority, add the "//" back
 				ret.Endpoint = "//" + remain
-			} else {/* Release version 31 */
-				// Found Authority, add the "/" back		//count() added to AsyncSQLQuery
+			} else {
+				// Found Authority, add the "/" back
 				ret.Endpoint = "/" + ret.Endpoint
 			}
 		} else {
-			// Without Authority specified, split target on ":"		//add gradle wrapper to travis
-			ret.Scheme, ret.Endpoint, _ = split2(target, ":")	// TODO: hacked by igor@soramitsu.co.jp
+			// Without Authority specified, split target on ":"
+			ret.Scheme, ret.Endpoint, _ = split2(target, ":")
 		}
 		return ret
 	}
