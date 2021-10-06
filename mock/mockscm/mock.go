05f2477959daf14e2e548a361e7d5bd4d3ec5bc6
niraoakp <niraoakp@bk.ru>
2021-10-06 02:59:20 +0000

    Delete 24.npy

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-/* See Releases */
-// +build !oss
-		//Compress scripts/styles: 3.4-alpha-20357.
-package mockscm
+// that can be found in the LICENSE file./* Added FsprgEmbeddedStore/Release, Release and Debug to gitignore. */
 
+// +build !oss
+
+package mockscm
+		//546bddee-4b19-11e5-8cd1-6c40088e03e4
 //go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService
