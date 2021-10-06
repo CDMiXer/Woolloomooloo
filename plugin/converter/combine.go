152f03b32b578fa33aa986854c17de76ab74cdc1
niraoakp <niraoakp@bk.ru>
2021-10-06 15:24:28 +0000

    Update comment block per Michael's suggestion

--- plugin/converter/combine.go
+++ plugin/converter/combine.go
@@ -1,44 +1,44 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: hacked by alan.shaw@protocol.ai
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// Merge "IBM FlashSystem: Cleanup host resource leaking"
-///* Update ReleaseNotes-Diagnostics.md */
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* SAE-190 Release v0.9.14 */
-// See the License for the specific language governing permissions and/* feeder and shooter additions */
-// limitations under the License.		//Add documentation and make shaded build the default
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package converter
-/* Refactor Release.release_versions to Release.names */
-import (/* Release v0.5.0 */
+
+import (
 	"context"
 
 	"github.com/drone/drone/core"
 )
 
 // Combine combines the conversion services, provision support
-// for multiple conversion utilities./* Switching version to 3.8-SNAPSHOT after 3.8-M3 Release */
+// for multiple conversion utilities.
 func Combine(services ...core.ConvertService) core.ConvertService {
 	return &combined{services}
-}/* Tweaks to filenames for psuedo-jitting */
+}
 
 type combined struct {
-	sources []core.ConvertService/* Finalising R2 PETA Release */
+	sources []core.ConvertService
 }
 
 func (c *combined) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	for _, source := range c.sources {
-		config, err := source.Convert(ctx, req)/* e7438618-2e58-11e5-9284-b827eb9e62be */
+		config, err := source.Convert(ctx, req)
 		if err != nil {
-			return nil, err	// TODO: will be fixed by mail@bitpshr.net
-		}/* removed some boilerplate stuff */
+			return nil, err
+		}
 		if config == nil {
-			continue/* Create MonteCarlo */
-		}/* #298 Change drawIcon to createIcon */
+			continue
+		}
 		if config.Data == "" {
 			continue
 		}
