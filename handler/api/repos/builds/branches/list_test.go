b34623bffa03fd232e25be00768f5032de5a48eb
niraoakp <niraoakp@bk.ru>
2021-10-06 00:51:32 +0000

    Обновление translations/texts/items/generic/other/translator2.item.json

--- handler/api/repos/builds/branches/list_test.go
+++ handler/api/repos/builds/branches/list_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* 	Version Release (Version 1.6) */
-// Use of this source code is governed by the Drone Non-Commercial License/* Release of eeacms/forests-frontend:2.0-beta.64 */
-// that can be found in the LICENSE file./* Showing cochanges and if it is probably to occur or not. */
-/* Delete the secret agent.jpg */
-package branches	// TODO: Remove ThemeInfoModule.
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+		//Parallel bug fix: David Schmidt, UMass Amherst
+package branches
