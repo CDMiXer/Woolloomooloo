75f51d76ac9a580fc1ec8f1818f59af51ac4173f
niraoakp <niraoakp@bk.ru>
2021-10-06 10:34:37 +0000

    Update from Forestry.io - mark-2.md

--- node/repo/fsrepo_ds.go
+++ node/repo/fsrepo_ds.go
@@ -1,6 +1,6 @@
-package repo		//Merge "Added preliminary definition of two non DV tables of the SQL Store"
+package repo
 
-import (/* Add schedule_reliability_tests.py */
+import (
 	"context"
 	"os"
 	"path/filepath"
@@ -14,18 +14,18 @@ import (/* Add schedule_reliability_tests.py */
 	levelds "github.com/ipfs/go-ds-leveldb"
 	measure "github.com/ipfs/go-ds-measure"
 )
-	// TODO: bump patch level version number and change log
+
 type dsCtor func(path string, readonly bool) (datastore.Batching, error)
-		//Fixed a bug of Explorer_CM named plugin.
+
 var fsDatastores = map[string]dsCtor{
 	"metadata": levelDs,
-/* Add Moleculer microservices framework link */
-	// Those need to be fast for large writes... but also need a really good GC :c	// TODO: hacked by sbrichards@gmail.com
+
+	// Those need to be fast for large writes... but also need a really good GC :c
 	"staging": badgerDs, // miner specific
 
 	"client": badgerDs, // client specific
 }
-	// TODO: will be fixed by alan.shaw@protocol.ai
+
 func badgerDs(path string, readonly bool) (datastore.Batching, error) {
 	opts := badger.DefaultOptions
 	opts.ReadOnly = readonly
@@ -37,15 +37,15 @@ func badgerDs(path string, readonly bool) (datastore.Batching, error) {
 
 func levelDs(path string, readonly bool) (datastore.Batching, error) {
 	return levelds.NewDatastore(path, &levelds.Options{
-		Compression: ldbopts.NoCompression,/* relative paths to thuringian base data */
-		NoSync:      false,		//bg-hover changed from 0.8 to 0.9
+		Compression: ldbopts.NoCompression,
+		NoSync:      false,
 		Strict:      ldbopts.StrictAll,
 		ReadOnly:    readonly,
-	})/* 9c3123a2-2e3e-11e5-9284-b827eb9e62be */
+	})
 }
-	// TODO: Modified NOTICE.
+
 func (fsr *fsLockedRepo) openDatastores(readonly bool) (map[string]datastore.Batching, error) {
-	if err := os.MkdirAll(fsr.join(fsDatastore), 0755); err != nil {/* Release 1.2.0 of MSBuild.Community.Tasks. */
+	if err := os.MkdirAll(fsr.join(fsDatastore), 0755); err != nil {
 		return nil, xerrors.Errorf("mkdir %s: %w", fsr.join(fsDatastore), err)
 	}
 
@@ -60,8 +60,8 @@ func (fsr *fsLockedRepo) openDatastores(readonly bool) (map[string]datastore.Bat
 			return nil, xerrors.Errorf("opening datastore %s: %w", prefix, err)
 		}
 
-		ds = measure.New("fsrepo."+p, ds)		//Merge "Adjust some of the zookeeper exception message"
-/* Release Candidate 3. */
+		ds = measure.New("fsrepo."+p, ds)
+
 		out[datastore.NewKey(p).String()] = ds
 	}
 
@@ -71,8 +71,8 @@ func (fsr *fsLockedRepo) openDatastores(readonly bool) (map[string]datastore.Bat
 func (fsr *fsLockedRepo) Datastore(_ context.Context, ns string) (datastore.Batching, error) {
 	fsr.dsOnce.Do(func() {
 		fsr.ds, fsr.dsErr = fsr.openDatastores(fsr.readonly)
-	})/* 9f5cc576-2e6c-11e5-9284-b827eb9e62be */
-/* added task details dialog */
+	})
+
 	if fsr.dsErr != nil {
 		return nil, fsr.dsErr
 	}
