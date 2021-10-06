84030879ef57f2652756aa3ecb8362a20512afdb
niraoakp <niraoakp@bk.ru>
2021-10-06 06:53:43 +0000

    f810250c-2e74-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/deploys/list_test.go
+++ handler/api/repos/builds/deploys/list_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* add agent descriptions */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
-package deploys
+// that can be found in the LICENSE file.	// TODO: hacked by steven@stebalien.com
+/* Fix typos in get_rss().  Props zuzara. fixes #3527 */
+package deploys/* language clarity edit */
