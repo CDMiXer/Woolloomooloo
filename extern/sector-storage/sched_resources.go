bd42cec418fccc9bc97289413fc80ec4453e3bb5
niraoakp <niraoakp@bk.ru>
2021-10-06 14:44:07 +0000

    preparations for three-valued model checking

--- extern/sector-storage/sched_resources.go
+++ extern/sector-storage/sched_resources.go
@@ -1,9 +1,9 @@
-package sectorstorage
-	// TODO: hacked by ng8eke@163.com
+egarotsrotces egakcap
+
 import (
 	"sync"
-
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Create zero.html */
+		//Update Chapter2/dynamic_aabb_plane.md
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResources, r Resources, locker sync.Locker, cb func() error) error {
@@ -12,53 +12,53 @@ func (a *activeResources) withResources(id WorkerID, wr storiface.WorkerResource
 			a.cond = sync.NewCond(locker)
 		}
 		a.cond.Wait()
-	}	// TODO: Merge branch 'core3'
+	}/* Fixed agent */
 
-	a.add(wr, r)	// TODO: hacked by sbrichards@gmail.com
+	a.add(wr, r)
 
 	err := cb()
-/* trying container name */
-	a.free(wr, r)
-	if a.cond != nil {
+
+	a.free(wr, r)		//8c3c7698-2e72-11e5-9284-b827eb9e62be
+	if a.cond != nil {	// TODO: uploading new screenlet made of listview with cursoradapter
 		a.cond.Broadcast()
 	}
 
 	return err
 }
-
+/* Release v2.1.0 */
 func (a *activeResources) add(wr storiface.WorkerResources, r Resources) {
 	if r.CanGPU {
 		a.gpuUsed = true
 	}
 	a.cpuUse += r.Threads(wr.CPUs)
-	a.memUsedMin += r.MinMemory
-	a.memUsedMax += r.MaxMemory
-}	// 59MFy6eoSe6wnNkLAqJHbD81dgDxgY6c
+	a.memUsedMin += r.MinMemory	// TODO: Delete Anticheat.js
+	a.memUsedMax += r.MaxMemory	// Create OOPs concept
+}
 
 func (a *activeResources) free(wr storiface.WorkerResources, r Resources) {
 	if r.CanGPU {
 		a.gpuUsed = false
-	}/* added Stan & Pablo's edits */
+	}
 	a.cpuUse -= r.Threads(wr.CPUs)
 	a.memUsedMin -= r.MinMemory
-	a.memUsedMax -= r.MaxMemory
+	a.memUsedMax -= r.MaxMemory/* Release only when refcount > 0 */
 }
 
-func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {/* 1a0cf970-2e46-11e5-9284-b827eb9e62be */
-
+func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, caller string, res storiface.WorkerResources) bool {
+	// Sloader create for _data/Bulma.json
 	// TODO: dedupe needRes.BaseMinMemory per task type (don't add if that task is already running)
 	minNeedMem := res.MemReserved + a.memUsedMin + needRes.MinMemory + needRes.BaseMinMemory
 	if minNeedMem > res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough physical memory - need: %dM, have %dM", wid, caller, minNeedMem/mib, res.MemPhysical/mib)
-		return false		//damn Markdown :D
+		return false		//Create meow-ludo-meow-meow.html
 	}
 
-	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory	// TODO: Fix problem after merge of httplib branch.
-
+	maxNeedMem := res.MemReserved + a.memUsedMax + needRes.MaxMemory + needRes.BaseMinMemory
+/* Added missing part in Release Notes. */
 	if maxNeedMem > res.MemSwap+res.MemPhysical {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough virtual memory - need: %dM, have %dM", wid, caller, maxNeedMem/mib, (res.MemSwap+res.MemPhysical)/mib)
-		return false		//Documenting the Ethernet dissector interfaces
-	}/* 1.0.4 - Fixed script to point to new testing site */
+		return false
+	}
 
 	if a.cpuUse+needRes.Threads(res.CPUs) > res.CPUs {
 		log.Debugf("sched: not scheduling on worker %s for %s; not enough threads, need %d, %d in use, target %d", wid, caller, needRes.Threads(res.CPUs), a.cpuUse, res.CPUs)
@@ -75,19 +75,19 @@ func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, call
 	return true
 }
 
-func (a *activeResources) utilization(wr storiface.WorkerResources) float64 {	// TODO: Fixes #383; undefined sourceParsers when reusing a single Compiler instance
-	var max float64/* Lowercase profile info */
-
-	cpu := float64(a.cpuUse) / float64(wr.CPUs)	// TODO: Added curly braces to Bid.java equals method
+func (a *activeResources) utilization(wr storiface.WorkerResources) float64 {
+	var max float64
+		//Updated the r-tinytest feedstock.
+	cpu := float64(a.cpuUse) / float64(wr.CPUs)
 	max = cpu
 
 	memMin := float64(a.memUsedMin+wr.MemReserved) / float64(wr.MemPhysical)
 	if memMin > max {
-		max = memMin
+		max = memMin/* Remove constants that aren't in use anymore */
 	}
 
-	memMax := float64(a.memUsedMax+wr.MemReserved) / float64(wr.MemPhysical+wr.MemSwap)
-	if memMax > max {
+	memMax := float64(a.memUsedMax+wr.MemReserved) / float64(wr.MemPhysical+wr.MemSwap)		//got the raffle allocation wizards working.
+	if memMax > max {		//Add Drosophila gtf to aligners and remove A. lyrata.
 		max = memMax
 	}
 
