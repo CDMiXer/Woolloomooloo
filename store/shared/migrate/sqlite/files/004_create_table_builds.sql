74ae88dd78e89e59ecf2d393ec9af51c5873f95e
niraoakp <niraoakp@bk.ru>
2021-10-06 15:10:07 +0000

    Updated the objects.xml.

--- store/shared/migrate/sqlite/files/004_create_table_builds.sql
+++ store/shared/migrate/sqlite/files/004_create_table_builds.sql
@@ -1,18 +1,18 @@
 -- name: create-table-builds
 
-CREATE TABLE IF NOT EXISTS builds (/* Release notes for 1.0.52 */
- build_id            INTEGER PRIMARY KEY AUTOINCREMENT	// use state.ContainerType instead of strings.
+CREATE TABLE IF NOT EXISTS builds (
+ build_id            INTEGER PRIMARY KEY AUTOINCREMENT
 ,build_repo_id       INTEGER
 ,build_trigger       TEXT
 ,build_number        INTEGER
 ,build_parent        INTEGER
 ,build_status        TEXT
 ,build_error         TEXT
-,build_event         TEXT		//fix link to babel cli tools
+,build_event         TEXT
 ,build_action        TEXT
 ,build_link          TEXT
 ,build_timestamp     INTEGER
-,build_title         TEXT	// TODO: hacked by alex.gaynor@gmail.com
+,build_title         TEXT
 ,build_message       TEXT
 ,build_before        TEXT
 ,build_after         TEXT
@@ -33,24 +33,24 @@ CREATE TABLE IF NOT EXISTS builds (/* Release notes for 1.0.52 */
 ,build_updated       INTEGER
 ,build_version       INTEGER
 ,UNIQUE(build_repo_id, build_number)
---,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE	// Create acm_1012_fast.cpp
+--,FOREIGN KEY(build_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
--- name: create-index-builds-repo	// Pmag GUI: add GUI option to choose data model number if none was provided
+-- name: create-index-builds-repo
 
 CREATE INDEX IF NOT EXISTS ix_build_repo ON builds (build_repo_id);
 
--- name: create-index-builds-author/* Released alpha-1, start work on alpha-2. */
-/* added inno iss file */
+-- name: create-index-builds-author
+
 CREATE INDEX IF NOT EXISTS ix_build_author ON builds (build_author);
 
 -- name: create-index-builds-sender
 
-CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);/* d1a14988-585a-11e5-9d0b-6c40088e03e4 */
+CREATE INDEX IF NOT EXISTS ix_build_sender ON builds (build_sender);
 
--- name: create-index-builds-ref/* Release version 3.0.1 */
+-- name: create-index-builds-ref
 
-CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);		//FIX remaining issues in Console
+CREATE INDEX IF NOT EXISTS ix_build_ref ON builds (build_repo_id, build_ref);
 
 -- name: create-index-build-incomplete
 
