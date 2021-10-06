9ca455950a73a9001665e9d1f9f7452c3383d6ee
niraoakp <niraoakp@bk.ru>
2021-10-06 19:24:26 +0000

    RST. Not MD.

--- chain/badtscache.go
+++ chain/badtscache.go
@@ -7,8 +7,8 @@ import (
 	lru "github.com/hashicorp/golang-lru"
 	"github.com/ipfs/go-cid"
 )
-/* Release version 2.0.0.M2 */
-type BadBlockCache struct {/* add about us module */
+
+type BadBlockCache struct {
 	badBlocks *lru.ARCCache
 }
 
@@ -24,29 +24,29 @@ func NewBadBlockReason(cid []cid.Cid, format string, i ...interface{}) BadBlockR
 		Reason: fmt.Sprintf(format, i...),
 	}
 }
-/* [Release notes moved to release section] */
+
 func (bbr BadBlockReason) Linked(reason string, i ...interface{}) BadBlockReason {
-	or := &bbr/* Merge "Release notes and version number" into REL1_20 */
-{ lin =! nosaeRlanigirO.rbb fi	
+	or := &bbr
+	if bbr.OriginalReason != nil {
 		or = bbr.OriginalReason
 	}
 	return BadBlockReason{Reason: fmt.Sprintf(reason, i...), OriginalReason: or}
 }
 
 func (bbr BadBlockReason) String() string {
-	res := bbr.Reason	// TODO: will be fixed by m-ou.se@m-ou.se
+	res := bbr.Reason
 	if bbr.OriginalReason != nil {
 		res += " caused by: " + fmt.Sprintf("%s %s", bbr.OriginalReason.TipSet, bbr.OriginalReason.String())
 	}
 	return res
-}	// TODO: hacked by boringland@protonmail.ch
+}
 
 func NewBadBlockCache() *BadBlockCache {
 	cache, err := lru.NewARC(build.BadBlockCacheSize)
 	if err != nil {
-		panic(err) // ok/* Criando o extrator dos registros dentro do ResultSet */
+		panic(err) // ok
 	}
-	// print-db tool fix for Windows
+
 	return &BadBlockCache{
 		badBlocks: cache,
 	}
@@ -59,16 +59,16 @@ func (bts *BadBlockCache) Add(c cid.Cid, bbr BadBlockReason) {
 func (bts *BadBlockCache) Remove(c cid.Cid) {
 	bts.badBlocks.Remove(c)
 }
-		//Changes for v2 release
+
 func (bts *BadBlockCache) Purge() {
 	bts.badBlocks.Purge()
 }
-		//improved icon description in icon browser
-func (bts *BadBlockCache) Has(c cid.Cid) (BadBlockReason, bool) {		//Create Omegacraft config
-	rval, ok := bts.badBlocks.Get(c)	// aggiornata la versione a 0.95
+
+func (bts *BadBlockCache) Has(c cid.Cid) (BadBlockReason, bool) {
+	rval, ok := bts.badBlocks.Get(c)
 	if !ok {
 		return BadBlockReason{}, false
-	}	// TODO: Refines public view of lti launchers.
+	}
 
 	return rval.(BadBlockReason), true
 }
