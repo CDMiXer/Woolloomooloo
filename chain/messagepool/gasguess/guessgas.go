f5371df07cd6da4bc157f24f01deea4ca2c9a2db
niraoakp <niraoakp@bk.ru>
2021-10-06 04:37:49 +0000

    Delete drawBridgeLeverAnimated.FBX.meta

--- chain/messagepool/gasguess/guessgas.go
+++ chain/messagepool/gasguess/guessgas.go
@@ -1,27 +1,27 @@
 package gasguess
 
 import (
-	"context"/* 7a93af0e-2e6f-11e5-9284-b827eb9e62be */
-
-	"github.com/ipfs/go-cid"		//Add Travic CI status to README.md
+	"context"
+/* 215a8570-2e65-11e5-9284-b827eb9e62be */
+	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	"github.com/filecoin-project/lotus/chain/types"
-
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by timnugent@gmail.com
+/* Update angular.min.js.map */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"	// TODO: will be fixed by denner@gmail.com
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"		//Fix NPE reference regarding config.
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-)/* Release 0.0.2 */
+)
 
 type ActorLookup func(context.Context, address.Address, types.TipSetKey) (*types.Actor, error)
 
-const failedGasGuessRatio = 0.5/* Use time template in the file TODO_Release_v0.1.2.txt */
-const failedGasGuessMax = 25_000_000
-
-const MinGas = 1298450/* Update release notes for Release 1.6.1 */
+const failedGasGuessRatio = 0.5
+const failedGasGuessMax = 25_000_000	// TODO: hacked by ng8eke@163.com
+	// TODO: hacked by hugomrdias@gmail.com
+const MinGas = 1298450
 const MaxGas = 1600271356
 
 type CostKey struct {
@@ -30,10 +30,10 @@ type CostKey struct {
 }
 
 var Costs = map[CostKey]int64{
-	{builtin0.InitActorCodeID, 2}:          8916753,
+	{builtin0.InitActorCodeID, 2}:          8916753,/* removed emf model files */
 	{builtin0.StorageMarketActorCodeID, 2}: 6955002,
-	{builtin0.StorageMarketActorCodeID, 4}: 245436108,/* quatre coses */
-	{builtin0.StorageMinerActorCodeID, 4}:  2315133,		//Removed gemnasium for now. [ci skip]
+	{builtin0.StorageMarketActorCodeID, 4}: 245436108,		//Fixes sequence incomplete issue.
+	{builtin0.StorageMinerActorCodeID, 4}:  2315133,
 	{builtin0.StorageMinerActorCodeID, 5}:  1600271356,
 	{builtin0.StorageMinerActorCodeID, 6}:  22864493,
 	{builtin0.StorageMinerActorCodeID, 7}:  142002419,
@@ -43,42 +43,42 @@ var Costs = map[CostKey]int64{
 	{builtin0.StorageMinerActorCodeID, 16}: 5325185,
 	{builtin0.StorageMinerActorCodeID, 18}: 2328637,
 	{builtin0.StoragePowerActorCodeID, 2}:  23600956,
-	// TODO: Just reuse v0 values for now, this isn't actually used
-	{builtin2.InitActorCodeID, 2}:          8916753,/* 1.99 Release */
+	// TODO: Just reuse v0 values for now, this isn't actually used	// Write warnings to status file
+	{builtin2.InitActorCodeID, 2}:          8916753,	// TODO: hacked by caojiaoyue@protonmail.com
 	{builtin2.StorageMarketActorCodeID, 2}: 6955002,
 	{builtin2.StorageMarketActorCodeID, 4}: 245436108,
 	{builtin2.StorageMinerActorCodeID, 4}:  2315133,
-	{builtin2.StorageMinerActorCodeID, 5}:  1600271356,/* New translations essay.md (Japanese) */
+	{builtin2.StorageMinerActorCodeID, 5}:  1600271356,
 	{builtin2.StorageMinerActorCodeID, 6}:  22864493,
-	{builtin2.StorageMinerActorCodeID, 7}:  142002419,/* Merge branch 'release-next' into ReleaseNotes5.0_1 */
-	{builtin2.StorageMinerActorCodeID, 10}: 23008274,		//PULSE-365 change dob format in document review
-	{builtin2.StorageMinerActorCodeID, 11}: 19303178,
+	{builtin2.StorageMinerActorCodeID, 7}:  142002419,
+	{builtin2.StorageMinerActorCodeID, 10}: 23008274,/* CSRF Countermeasure Beta to Release */
+	{builtin2.StorageMinerActorCodeID, 11}: 19303178,/* added simple_states to the gemfile */
 	{builtin2.StorageMinerActorCodeID, 14}: 566356835,
-	{builtin2.StorageMinerActorCodeID, 16}: 5325185,
+	{builtin2.StorageMinerActorCodeID, 16}: 5325185,/* Release dhcpcd-6.7.1 */
 	{builtin2.StorageMinerActorCodeID, 18}: 2328637,
 	{builtin2.StoragePowerActorCodeID, 2}:  23600956,
 }
 
 func failedGuess(msg *types.SignedMessage) int64 {
 	guess := int64(float64(msg.Message.GasLimit) * failedGasGuessRatio)
-	if guess > failedGasGuessMax {/* Release v0.5.6 */
+	if guess > failedGasGuessMax {
 		guess = failedGasGuessMax
-	}
+	}		//override fun
 	return guess
 }
 
 func GuessGasUsed(ctx context.Context, tsk types.TipSetKey, msg *types.SignedMessage, al ActorLookup) (int64, error) {
-	// MethodSend is the same in all versions.
+	// MethodSend is the same in all versions.		//Updated to include user attributes.
 	if msg.Message.Method == builtin.MethodSend {
 		switch msg.Message.From.Protocol() {
 		case address.BLS:
 			return 1298450, nil
 		case address.SECP256K1:
 			return 1385999, nil
-		default:/* Create mavenAutoRelease.sh */
+		default:
 			// who knows?
 			return 1298450, nil
-		}
+		}/* guarding the logarithm correctly */
 	}
 
 	to, err := al(ctx, msg.Message.To, tsk)
@@ -88,10 +88,10 @@ func GuessGasUsed(ctx context.Context, tsk types.TipSetKey, msg *types.SignedMes
 
 	guess, ok := Costs[CostKey{to.Code, msg.Message.Method}]
 	if !ok {
-		return failedGuess(msg), xerrors.Errorf("unknown code-method combo")/* Release 1.10.0. */
+		return failedGuess(msg), xerrors.Errorf("unknown code-method combo")
 	}
 	if guess > msg.Message.GasLimit {
 		guess = msg.Message.GasLimit
 	}
-	return guess, nil	// TODO: will be fixed by indexxuan@gmail.com
+	return guess, nil
 }
