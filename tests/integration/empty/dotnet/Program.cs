ef6b388009da326d0a0a8424317acf2de8199b82
niraoakp <niraoakp@bk.ru>
2021-10-06 16:25:10 +0000

    Log formatting for better version compatibility

--- tests/integration/empty/dotnet/Program.cs
+++ tests/integration/empty/dotnet/Program.cs
@@ -1,11 +1,11 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.		//Create Jira.md
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Threading.Tasks;
 using Pulumi;
 
 class Program
-{	// TODO: hacked by igor@soramitsu.co.jp
-    static Task<int> Main(string[] args)/* [1.2.2] Release */
+{
+    static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => {});
     }
