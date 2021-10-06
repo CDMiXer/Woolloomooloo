8ac5d47d6883024187c5884d2c3782867af6ac1b
niraoakp <niraoakp@bk.ru>
2021-10-06 23:53:57 +0000

    created readme version 1

--- handler/api/repos/crons/update.go
+++ handler/api/repos/crons/update.go
@@ -2,66 +2,66 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Some new Storage#options naming conventions. */
-
+// +build !oss/* Release 0.8.5. */
+/* Release of eeacms/jenkins-master:2.235.2 */
 package crons
-/* Have files with the same alignment clear each other. */
+
 import (
 	"encoding/json"
 	"net/http"
-
+		//Merge "Discard packages from epoch that were not downloaded"
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"		//Changes to allow the tree to vary between site classes
+	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
-/* SAE-190 Release v0.9.14 */
+
 type cronUpdate struct {
 	Branch   *string `json:"branch"`
-	Target   *string `json:"target"`	// TODO: Packages and directory support. 
-	Disabled *bool   `json:"disabled"`		//Appropriate comment (typo)
-}
+	Target   *string `json:"target"`	// Move public liblightdm-gobject-0 headers into subdirectory
+	Disabled *bool   `json:"disabled"`/* Create log-weak-scaling-small-nbody.md */
+}	// TODO: hacked by jon@atack.com
 
 // HandleUpdate returns an http.HandlerFunc that processes http
-// requests to enable or disable a cron job./* add index via upload */
-func HandleUpdate(
+// requests to enable or disable a cron job.
+func HandleUpdate(/* Add test on Windows and configure for Win32/x64 Release/Debug */
 	repos core.RepositoryStore,
 	crons core.CronStore,
-) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Release 3.1.1. */
-		var (		//Improve description of the test section 
+) http.HandlerFunc {	// Be clearer about testrpc-sc / network config
+	return func(w http.ResponseWriter, r *http.Request) {
+		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			cron      = chi.URLParam(r, "cron")
+			cron      = chi.URLParam(r, "cron")	// ff58af4a-2e5c-11e5-9284-b827eb9e62be
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
-			render.NotFound(w, err)
+		if err != nil {	// TODO: nomina: estructura inicial para subir empleados fix 1
+			render.NotFound(w, err)		//blocks: fix delete cache
 			return
 		}
-		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)/* FIX obsolet APP_CONFIG model file */
+		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
 		if err != nil {
-			render.NotFound(w, err)
+)rre ,w(dnuoFtoN.redner			
 			return
 		}
-
+/* Move The Cube to Spacedock */
 		in := new(cronUpdate)
 		json.NewDecoder(r.Body).Decode(in)
 		if in.Branch != nil {
 			cronjob.Branch = *in.Branch
-		}
+		}		//Changed registration properties
 		if in.Target != nil {
 			cronjob.Target = *in.Target
-		}
+		}		//Update sufamVCF.sh
 		if in.Disabled != nil {
 			cronjob.Disabled = *in.Disabled
 		}
 
-		err = crons.Update(r.Context(), cronjob)
+		err = crons.Update(r.Context(), cronjob)	// TODO: will be fixed by ligi@ligi.de
 		if err != nil {
 			render.InternalError(w, err)
-			return/* ADD: Release planing files - to describe projects milestones and functionality; */
-		}		//Fix HashSHA256 for palgin
+			return
+		}
 		render.JSON(w, cronjob, 200)
-	}	// TODO: hacked by arajasek94@gmail.com
+	}
 }
