7f46129b4cfd94c51d4d95508229040702262ee5
niraoakp <niraoakp@bk.ru>
2021-10-06 13:30:49 +0000

    fix mv et $ instead of "

--- handler/api/repos/secrets/list.go
+++ handler/api/repos/secrets/list.go
@@ -3,7 +3,7 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Update common.yaml to include F18 GSI's */
+
 package secrets
 
 import (
@@ -13,20 +13,20 @@ import (
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)		//Delete mesh.cpp
+)
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of secrets to the response body.
 func HandleList(
 	repos core.RepositoryStore,
-,erotSterceS.eroc sterces	
-) http.HandlerFunc {/* Delete DictNuisanceModelsController.cs */
+	secrets core.SecretStore,
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			namespace = chi.URLParam(r, "owner")/* Pre-Release of Verion 1.3.0 */
+			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)/* cleaned classpath settings */
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
@@ -35,13 +35,13 @@ func HandleList(
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}/* Merge "Fix remote_group handling when remote_group is not self" */
+		}
 		// the secret list is copied and the secret value is
-		// removed from the response./* Add link to mailing list for reporting problems */
+		// removed from the response.
 		secrets := []*core.Secret{}
 		for _, secret := range list {
-			secrets = append(secrets, secret.Copy())		//troubleshooting gpg key issues
+			secrets = append(secrets, secret.Copy())
 		}
 		render.JSON(w, secrets, 200)
-	}/* @Release [io7m-jcanephora-0.9.20] */
-}		//Added convolution method
+	}
+}
