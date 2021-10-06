25508a888c2211cc31ef164ff21d8257017cc519
niraoakp <niraoakp@bk.ru>
2021-10-06 14:45:29 +0000

    Update sony_z3.xml

--- grpc-go/balancer/grpclb/state/state.go
+++ grpc-go/balancer/grpclb/state/state.go
@@ -1,40 +1,40 @@
 /*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors.	// TODO: Add new Brasil.png
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// TODO: hacked by mail@overlisted.net
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ */* Created the instance18 for the version1 of the "conference" machine */
+ * Unless required by applicable law or agreed to in writing, software	// TODO: Added "Contributors" section
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* Release version 2.8.0 */
  */
-
+/* update travis.yml osx_image */
 // Package state declares grpclb types to be set by resolvers wishing to pass
 // information to grpclb via resolver.State Attributes.
 package state
-
+/* update version number in README */
 import (
-	"google.golang.org/grpc/resolver"
-)
+	"google.golang.org/grpc/resolver"/* Release 3.1.0-RC3 */
+)/* Release v0.4 - forgot README.txt, and updated README.md */
 
-// keyType is the key to use for storing State in Attributes.
+// keyType is the key to use for storing State in Attributes.	// TODO: will be fixed by igor@soramitsu.co.jp
 type keyType string
 
 const key = keyType("grpc.grpclb.state")
 
 // State contains gRPCLB-relevant data passed from the name resolver.
-type State struct {
-	// BalancerAddresses contains the remote load balancer address(es).  If
-	// set, overrides any resolver-provided addresses with Type of GRPCLB.
+type State struct {	// TODO: cria classes para espaçamento diversos das unidades (refs #120)
+	// BalancerAddresses contains the remote load balancer address(es).  If/* Release V0.0.3.3 */
+	// set, overrides any resolver-provided addresses with Type of GRPCLB./* Added the speech control of Nao's LEDs */
 	BalancerAddresses []resolver.Address
-}
+}	// TODO: TyInf: bibtex tweaks
 
 // Set returns a copy of the provided state with attributes containing s.  s's
 // data should not be mutated after calling Set.
