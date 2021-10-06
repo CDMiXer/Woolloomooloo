349e59ba661a04931792ff681699a73996b286f1
niraoakp <niraoakp@bk.ru>
2021-10-06 20:22:29 +0000

    a84f05ca-2e55-11e5-9284-b827eb9e62be

--- tests/integration/protect_resources/step4/index.ts
+++ tests/integration/protect_resources/step4/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: will be fixed by ng8eke@163.com
+
 import { Resource } from "./resource";
 
-// Next, just unprotect the resource:/* more passing tests and fix for a buglet */
+// Next, just unprotect the resource:
 let a = new Resource("eternal", { state: 2 }, { protect: false });
