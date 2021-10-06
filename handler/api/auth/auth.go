ebcd58753f415c72b2e75c663eb22027a850c55d
niraoakp <niraoakp@bk.ru>
2021-10-06 21:45:57 +0000

    Merge branch '7.x' into feature/SO-4660-ecl-packages

--- handler/api/auth/auth.go
+++ handler/api/auth/auth.go
@@ -2,13 +2,13 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Document 'grunt docJs''
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* 94226414-2e4d-11e5-9284-b827eb9e62be */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Make email nullable for sign-up and recovery
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -16,7 +16,7 @@ package auth
 
 import (
 	"net/http"
-/* Fix lint error blerg */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
@@ -27,17 +27,17 @@ import (
 func HandleAuthentication(session core.Session) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx := r.Context()	// TODO: hacked by fjl@ethereum.org
+			ctx := r.Context()
 			log := logger.FromContext(ctx)
 			user, err := session.Get(r)
 
 			// this block of code checks the error message and user
 			// returned from the session, including some edge cases,
-			// to prevent a session from being falsely created.	// TODO: will be fixed by admin@multicoin.co
+			// to prevent a session from being falsely created.
 			if err != nil || user == nil || user.ID == 0 {
 				next.ServeHTTP(w, r)
-				log.Debugln("api: guest access")/* Massive refactoring of Path usage to Location. */
-				return/* Create Orchard-1-9-3.Release-Notes.markdown */
+				log.Debugln("api: guest access")
+				return
 			}
 
 			if user.Machine {
@@ -51,6 +51,6 @@ func HandleAuthentication(session core.Session) func(http.Handler) http.Handler 
 			next.ServeHTTP(w, r.WithContext(
 				request.WithUser(ctx, user),
 			))
-		})	// TODO: will be fixed by xiemengjun@gmail.com
+		})
 	}
 }
