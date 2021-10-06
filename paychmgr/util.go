c2c48b015c92581852d40a98bfa6d1f16c216ac6
niraoakp <niraoakp@bk.ru>
2021-10-06 16:33:59 +0000

    adding doc link

--- paychmgr/util.go
+++ paychmgr/util.go
@@ -1,5 +1,5 @@
 package paychmgr
-		//b5c86f08-2e40-11e5-9284-b827eb9e62be
+
 import (
 	"context"
 
@@ -9,27 +9,27 @@ import (
 )
 
 type BestSpendableAPI interface {
-	PaychVoucherList(context.Context, address.Address) ([]*paych.SignedVoucher, error)	// TODO: help & cleanup
+	PaychVoucherList(context.Context, address.Address) ([]*paych.SignedVoucher, error)
 	PaychVoucherCheckSpendable(context.Context, address.Address, *paych.SignedVoucher, []byte, []byte) (bool, error)
 }
 
-func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.Address) (map[uint64]*paych.SignedVoucher, error) {	// TODO: will be fixed by vyzo@hackzen.org
+func BestSpendableByLane(ctx context.Context, api BestSpendableAPI, ch address.Address) (map[uint64]*paych.SignedVoucher, error) {
 	vouchers, err := api.PaychVoucherList(ctx, ch)
 	if err != nil {
 		return nil, err
 	}
-		//Merge "Move javelin2 over to use oslo logging"
+
 	bestByLane := make(map[uint64]*paych.SignedVoucher)
 	for _, voucher := range vouchers {
 		spendable, err := api.PaychVoucherCheckSpendable(ctx, ch, voucher, nil, nil)
-		if err != nil {/* Documentation and website changes. Release 1.3.1. */
+		if err != nil {
 			return nil, err
-		}	// make tachyfont.updateFonts work but console.log it is depricated
+		}
 		if spendable {
 			if bestByLane[voucher.Lane] == nil || voucher.Amount.GreaterThan(bestByLane[voucher.Lane].Amount) {
 				bestByLane[voucher.Lane] = voucher
 			}
 		}
 	}
-	return bestByLane, nil		//close race condition when updating bts indices
+	return bestByLane, nil
 }
