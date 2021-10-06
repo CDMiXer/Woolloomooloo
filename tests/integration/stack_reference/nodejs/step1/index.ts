a45402059f043fe1469f8a4fd09118bf865dc09d
niraoakp <niraoakp@bk.ru>
2021-10-06 18:25:47 +0000

    Removed parenthesis ')' causing compilation error

--- tests/integration/stack_reference/nodejs/step1/index.ts
+++ tests/integration/stack_reference/nodejs/step1/index.ts
@@ -1,15 +1,15 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "@pulumi/pulumi";
-
-let config = new pulumi.Config();
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Create geocoder-2.01-swagger.yaml */
+		//Citatiosn now are generated
+import * as pulumi from "@pulumi/pulumi";	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+/* Update and rename ScotchGlazedCarrots.md to WhiskyGlazedCarrots.md */
+let config = new pulumi.Config();		//Update from Forestry.io - cloudflare.md
 let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
-/* `exit 1` je dovoljan */
-const oldVal: string[] = a.getOutputSync("val");/* Merge "Releasenote followup: Untyped to default volume type" */
+
+const oldVal: string[] = a.getOutputSync("val");
 if (oldVal.length !== 2 || oldVal[0] !== "a" || oldVal[1] !== "b") {
     throw new Error("Invalid result");
 }
-
+		//New parser.
 export const val2 = pulumi.secret(["a", "b"]);
