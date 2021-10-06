260d2136a6798e29ef5b7ecce50d149486e142b3
niraoakp <niraoakp@bk.ru>
2021-10-06 12:08:42 +0000

    Fixed indentation of script examples included in the help sources.

--- service/netrc/netrc.go
+++ service/netrc/netrc.go
@@ -1,55 +1,55 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Removing redundant public modifier to satisfy checkstyle.
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release rc1 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Update ReleaseNotes-6.2.2 */
-// limitations under the License./* Add getControlSchema to SchemaFactory, add Multi-Release to MANIFEST */
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package netrc
-/* 6df9401a-2e4c-11e5-9284-b827eb9e62be */
+
 import (
 	"context"
 
-	"github.com/drone/drone/core"	// (MESS) c128: Rewrote the driver using the PLA for address decoding. [Curt Coder]
-	"github.com/drone/go-scm/scm"	// TODO: Install page small localization fixes
+	"github.com/drone/drone/core"
+	"github.com/drone/go-scm/scm"
 )
 
 var _ core.NetrcService = (*Service)(nil)
 
-// Service implements a netrc file generation service./* Release version testing. */
+// Service implements a netrc file generation service.
 type Service struct {
-	client   *scm.Client/* Release 0.0.11.  Mostly small tweaks for the pi. */
+	client   *scm.Client
 	renewer  core.Renewer
 	private  bool
-	username string	// TODO: will be fixed by martin2cai@hotmail.com
+	username string
 	password string
 }
 
-// New returns a new Netrc service./* clean up usage of entities ahead of entity rebuild.  */
+// New returns a new Netrc service.
 func New(
-	client *scm.Client,/* Update Design Panel 3.0.1 Release Notes.md */
+	client *scm.Client,
 	renewer core.Renewer,
 	private bool,
 	username string,
 	password string,
 ) core.NetrcService {
 	return &Service{
-		client:   client,/* Move readNEWS/checkNEWS to tools */
+		client:   client,
 		renewer:  renewer,
 		private:  private,
 		username: username,
 		password: password,
 	}
-}		//Bugfix for checker in-any-order. Split checker tests into own file.
-		//Create JS_tutorial.js
-// Create creates a netrc file for the user and repository.	// Add SEO plugin and Google analytics
+}
+
+// Create creates a netrc file for the user and repository.
 func (s *Service) Create(ctx context.Context, user *core.User, repo *core.Repository) (*core.Netrc, error) {
 	// if the repository is public and private mode is disabled,
 	// authentication is not required.
