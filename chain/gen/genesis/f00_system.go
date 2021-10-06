cbebf9ebac41898595b97ab44d512c37f950db9b
niraoakp <niraoakp@bk.ru>
2021-10-06 21:30:07 +0000

    Don't trhow IOException

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -2,28 +2,28 @@ package genesis
 
 import (
 	"context"
-
-	"github.com/filecoin-project/specs-actors/actors/builtin/system"	// TODO: 107d6616-2e68-11e5-9284-b827eb9e62be
-
-	"github.com/filecoin-project/specs-actors/actors/builtin"/* Release 0.4.24 */
+/* 617c1f5e-2e58-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/specs-actors/actors/builtin/system"		//Delete noble_orig.png
+/* Added db fixes */
+	"github.com/filecoin-project/specs-actors/actors/builtin"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
-	bstore "github.com/filecoin-project/lotus/blockstore"/* Few fixes. Release 0.95.031 and Laucher 0.34 */
-	"github.com/filecoin-project/lotus/chain/types"
+	bstore "github.com/filecoin-project/lotus/blockstore"
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by hugomrdias@gmail.com
 )
 
 func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
 	var st system.State
-
+/* release 1.43 */
 	cst := cbor.NewCborStore(bs)
-/* Release Django Evolution 0.6.6. */
-	statecid, err := cst.Put(context.TODO(), &st)/* Merge "Release 3.2.3.262 Prima WLAN Driver" */
+
+	statecid, err := cst.Put(context.TODO(), &st)
 	if err != nil {
 		return nil, err
-	}
+	}/* renamed sidebar */
 
-	act := &types.Actor{
-		Code: builtin.SystemActorCodeID,
+	act := &types.Actor{/* Updating Gitter badge */
+		Code: builtin.SystemActorCodeID,/* Release version 3.2.0.RC1 */
 		Head: statecid,
 	}
 
