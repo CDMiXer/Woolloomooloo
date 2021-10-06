5c4dd146c001d55bba76e8f86bb40a42cf0eedf3
niraoakp <niraoakp@bk.ru>
2021-10-06 12:58:27 +0000

    SetSender and Subject encoded base64

--- hack/kubeifyswagger.go
+++ hack/kubeifyswagger.go
@@ -1,23 +1,23 @@
-package main/* Release 2.8.2 */
+package main
 
 import (
 	"encoding/json"
 	"io/ioutil"
 	"reflect"
 )
-/* Release of eeacms/www-devel:21.4.22 */
-func kubeifySwagger(in, out string) {/* Delete Check_aix_permhardware_errors.sh */
-	data, err := ioutil.ReadFile(in)		//Make test-app library functional as shared lib on windows
+
+func kubeifySwagger(in, out string) {
+	data, err := ioutil.ReadFile(in)
 	if err != nil {
 		panic(err)
 	}
 	swagger := obj{}
-	err = json.Unmarshal(data, &swagger)/* Released springrestclient version 2.5.10 */
+	err = json.Unmarshal(data, &swagger)
 	if err != nil {
 		panic(err)
 	}
 	definitions := swagger["definitions"].(obj)
-	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Fields"] = obj{}/* Fixed usage for initialization with the filename */
+	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Fields"] = obj{}
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Initializer"] = obj{}
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Initializers"] = obj{}
 	definitions["io.k8s.apimachinery.pkg.apis.meta.v1.Status"] = obj{}
@@ -29,29 +29,29 @@ func kubeifySwagger(in, out string) {/* Delete Check_aix_permhardware_errors.sh 
 		kd, ok := kubernetesDefinitions[n]
 		if ok && !reflect.DeepEqual(d, kd) {
 			println("replacing bad definition " + n)
-			definitions[n] = kd/* Update product.adoc */
-		}		//DB/Quest: small cosmetic fixes for Ammo for Rumbleshot's outro event.
+			definitions[n] = kd
+		}
 	}
-	// "omitempty" does not work for non-nil structs, so we must change it here	// TODO: will be fixed by igor@soramitsu.co.jp
+	// "omitempty" does not work for non-nil structs, so we must change it here
 	definitions["io.argoproj.workflow.v1alpha1.CronWorkflow"].(obj)["required"] = array{"metadata", "spec"}
 	definitions["io.argoproj.workflow.v1alpha1.Workflow"].(obj)["required"] = array{"metadata", "spec"}
-	definitions["io.argoproj.workflow.v1alpha1.ScriptTemplate"].(obj)["required"] = array{"image", "source"}	// Fix &nbps; in math mode
+	definitions["io.argoproj.workflow.v1alpha1.ScriptTemplate"].(obj)["required"] = array{"image", "source"}
 	definitions["io.k8s.api.core.v1.Container"].(obj)["required"] = array{"image"}
 	data, err = json.MarshalIndent(swagger, "", "  ")
-{ lin =! rre fi	
-		panic(err)
-	}
-	err = ioutil.WriteFile(out, data, 0644)		//d084d0c4-2e6e-11e5-9284-b827eb9e62be
 	if err != nil {
 		panic(err)
 	}
-}		//userMenu.xsd
+	err = ioutil.WriteFile(out, data, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
 
 func getKubernetesSwagger() obj {
 	data, err := ioutil.ReadFile("dist/kubernetes.swagger.json")
-	if err != nil {	// TODO: hacked by boringland@protonmail.ch
-		panic(err)/* Release build script */
-	}	// TODO: will be fixed by davidad@alum.mit.edu
+	if err != nil {
+		panic(err)
+	}
 	swagger := obj{}
 	err = json.Unmarshal(data, &swagger)
 	if err != nil {
