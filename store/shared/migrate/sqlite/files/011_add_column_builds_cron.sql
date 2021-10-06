17e07106d68634e7266f164bf9fee2ff49aab53b
niraoakp <niraoakp@bk.ru>
2021-10-06 00:10:23 +0000

    [RELEASE] merging 'release/1.0.90' into 'master'

--- store/shared/migrate/sqlite/files/011_add_column_builds_cron.sql
+++ store/shared/migrate/sqlite/files/011_add_column_builds_cron.sql
@@ -1,3 +1,3 @@
 -- name: alter-table-builds-add-column-cron
 
-ALTER TABLE builds ADD COLUMN build_cron TEXT NOT NULL DEFAULT '';	// TODO: Disable default menu background image as we use fa-bars icon (#66)
+ALTER TABLE builds ADD COLUMN build_cron TEXT NOT NULL DEFAULT '';
