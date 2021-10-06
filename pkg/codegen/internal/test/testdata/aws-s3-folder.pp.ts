b79760ef586c7df596f5a41d893d9e8e158ecbfa
niraoakp <niraoakp@bk.ru>
2021-10-06 00:57:30 +0000

    trigger new build for ruby-head-clang (7f9c846)

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
@@ -1,6 +1,6 @@
-import * as pulumi from "@pulumi/pulumi";/* 766aa0e2-2f8c-11e5-83b4-34363bc765d8 */
+import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * from "fs";/* fully working Bezier curves */
+import * from "fs";
 
 // Create a bucket and expose a website index document
 const siteBucket = new aws.s3.Bucket("siteBucket", {website: {
@@ -9,7 +9,7 @@ const siteBucket = new aws.s3.Bucket("siteBucket", {website: {
 const siteDir = "www";
 // For each file in the directory, create an S3 object stored in `siteBucket`
 const files: aws.s3.BucketObject[];
-for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {/* b73c4d78-2e68-11e5-9284-b827eb9e62be */
+for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {
     files.push(new aws.s3.BucketObject(`files-${range.key}`, {
         bucket: siteBucket.id,
         key: range.value,
@@ -19,17 +19,17 @@ for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {
 }
 // set the MIME type of the file
 // Set the access policy for the bucket so all objects are readable
-const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {/* Merge branch 'master' into greenkeeper/jasmine-3.4.0 */
+const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
     bucket: siteBucket.id,
     policy: siteBucket.id.apply(id => JSON.stringify({
         Version: "2012-10-17",
         Statement: [{
             Effect: "Allow",
             Principal: "*",
-            Action: ["s3:GetObject"],		//Merge "qcom: rpm-smd: Add a check to validate the rpm message length"
-            Resource: [`arn:aws:s3:::${id}/*`],/* Release: 6.1.2 changelog */
+            Action: ["s3:GetObject"],
+            Resource: [`arn:aws:s3:::${id}/*`],
         }],
     })),
 });
 export const bucketName = siteBucket.bucket;
-export const websiteUrl = siteBucket.websiteEndpoint;	// Musterlösung KleinteileMagazin
+export const websiteUrl = siteBucket.websiteEndpoint;
