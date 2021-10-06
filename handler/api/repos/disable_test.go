0eb2b8215b4042c2a167eca648d4c1d3ffb98da1
niraoakp <niraoakp@bk.ru>
2021-10-06 00:22:04 +0000

    Rename Core to Core.h

--- handler/api/repos/disable_test.go
+++ handler/api/repos/disable_test.go
@@ -1,56 +1,56 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* testi linkki */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package repos
 
-import (
-	"encoding/json"
+import (		//Adding helper method
+	"encoding/json"		//7acc9cde-2e4b-11e5-9284-b827eb9e62be
 	"io"
 	"net/http"
 	"net/http/httptest"
-	"testing"
+	"testing"/* Release 1.0.14.0 */
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"	// TODO: will be fixed by zaq1tomo@gmail.com
+	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
-		//Updated compile instructions
+
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"/* e439e3a8-2e5e-11e5-9284-b827eb9e62be */
-)	// TODO: Create solution_recursiveWay
+	"github.com/google/go-cmp/cmp"
+)
 
-func TestDisable(t *testing.T) {		//log: fix tests
+func TestDisable(t *testing.T) {/* Build results of 4d1bc02 (on master) */
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* Release files. */
+	defer controller.Finish()
 
 	repo := &core.Repository{
-		ID:        1,/* Initial Release - Supports only Wind Symphony */
+		ID:        1,
 		Namespace: "octocat",
-		Name:      "hello-world",
+		Name:      "hello-world",	// TODO: Download & extract toolchains in $HOME subfolder, not in /tmp
 		Slug:      "octocat/hello-world",
 		Active:    true,
-	}
+	}/* 5.1.2 Release changes */
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)	// TODO: update net jar
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), repo.Name).Return(repo, nil)
-	repos.EXPECT().Update(gomock.Any(), repo).Return(nil)		//Added support for the museum's facets.
+	repos.EXPECT().Update(gomock.Any(), repo).Return(nil)
 
-	// a failed webhook should result in a warning message in the/* Review blog post on Release of 10.2.1 */
+	// a failed webhook should result in a warning message in the
 	// logs, but should not cause the endpoint to error.
 	webhook := mock.NewMockWebhookSender(controller)
-	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(io.EOF)		//jupyter-js-widgets 2.0.10, widgetsnbextension 2.0.0b6, jupyterlab_widgets 0.6.3
+	webhook.EXPECT().Send(gomock.Any(), gomock.Any()).Return(io.EOF)
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("DELETE", "/api/repos/octocat/hello-world", nil)
 
-	router := chi.NewRouter()
+	router := chi.NewRouter()/* Release of eeacms/ims-frontend:0.3-beta.4 */
 	router.Delete("/api/repos/{owner}/{name}", HandleDisable(repos, webhook))
 	router.ServeHTTP(w, r)
-
-	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Add storysource to addon gallery */
+/* Fixes bug adding members. */
+	if got, want := w.Code, 200; want != got {	// replaced NSTextFieldDelegate with IBAction
+		t.Errorf("Want response code %d, got %d", want, got)	// TODO: 16f9df4a-35c7-11e5-a118-6c40088e03e4
+	}
 
 	if got, want := repo.Active, false; got != want {
 		t.Errorf("Want repository activate %v, got %v", want, got)
@@ -61,7 +61,7 @@ func TestDisable(t *testing.T) {		//log: fix tests
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}
+}		//Update iothub configuration [skip ci]
 
 func TestDisable_NotFound(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -69,25 +69,25 @@ func TestDisable_NotFound(t *testing.T) {
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), "octocat", "hello-world").Return(nil, errors.ErrNotFound)
-	// TODO: Merge "raise 404 error if fqname is not found"
+
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("DELETE", "/api/repos/octocat/hello-world", nil)
+	r := httptest.NewRequest("DELETE", "/api/repos/octocat/hello-world", nil)	// Merge "platform: msm_shared: Fix unexpected events handling."
 
 	router := chi.NewRouter()
 	router.Delete("/api/repos/{owner}/{name}", HandleDisable(repos, nil))
 	router.ServeHTTP(w, r)
-		//more explanation on fonts
+
 	if got, want := w.Code, 404; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)	// TODO: hacked by sbrichards@gmail.com
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
-	// Merge "Changed logic when dumpstate's max progress increases." into nyc-dev
+
 	got, want := new(errors.Error), errors.ErrNotFound
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)
+		t.Errorf(diff)/* Update video_watch_duration.py */
 	}
 }
-		//Delete jekyll-logo.jpg
+
 func TestDisable_InternalError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
