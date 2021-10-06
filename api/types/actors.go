7d5be8cf431cae43163128d0a514e7d8eaed28fd
niraoakp <niraoakp@bk.ru>
2021-10-06 13:37:56 +0000

    FIxed DOM processing error.

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
 package apitypes
-/* a6e9c422-2f86-11e5-93da-34363bc765d8 */
-import "github.com/filecoin-project/go-state-types/network"
+
+import "github.com/filecoin-project/go-state-types/network"/* Released 0.4.7 */
 
 type NetworkVersion = network.Version
