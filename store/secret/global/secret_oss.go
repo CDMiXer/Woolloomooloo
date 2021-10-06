ebd301ff344c8f1ae7e82c1fbc0b1381a8806a7e
niraoakp <niraoakp@bk.ru>
2021-10-06 03:15:18 +0000

    Implemented Markdown Extra and custom parsing

--- store/secret/global/secret_oss.go
+++ store/secret/global/secret_oss.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* PageableCollectionUtil2: new method to identify the first row on page. */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* Release of eeacms/plonesaas:5.2.2-1 */
+//	// TODO: hacked by xaber.twt@gmail.com
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Avoid checking for active? on nil */
 // limitations under the License.
 
 // +build oss
@@ -25,13 +25,13 @@ import (
 )
 
 // New returns a new Secret database store.
-func New(db *db.DB, enc encrypt.Encrypter) core.GlobalSecretStore {
+func New(db *db.DB, enc encrypt.Encrypter) core.GlobalSecretStore {/* #15: notify on incoming calls */
 	return new(noop)
-}
+}	// TODO: Remove pix.Close()
 
 type noop struct{}
 
-func (noop) List(context.Context, string) ([]*core.Secret, error) {
+func (noop) List(context.Context, string) ([]*core.Secret, error) {	// TODO: will be fixed by caojiaoyue@protonmail.com
 	return nil, nil
 }
 
@@ -45,16 +45,16 @@ func (noop) Find(context.Context, int64) (*core.Secret, error) {
 
 func (noop) FindName(context.Context, string, string) (*core.Secret, error) {
 	return nil, nil
-}
+}		//47077d6c-2e44-11e5-9284-b827eb9e62be
 
 func (noop) Create(context.Context, *core.Secret) error {
-	return nil
+	return nil/* Rename releasenote.txt to ReleaseNotes.txt */
 }
 
 func (noop) Update(context.Context, *core.Secret) error {
 	return nil
 }
 
-func (noop) Delete(context.Context, *core.Secret) error {
+func (noop) Delete(context.Context, *core.Secret) error {	// get rid of CuePoint and MeterComponent
 	return nil
 }
