36fa3d227116b8c89d00bbec87e3092b5bccee1d
niraoakp <niraoakp@bk.ru>
2021-10-06 16:04:40 +0000

    Delete ServiceCommonsWebPingTest.java

--- tests/integration/recreate_resource_check/resource_recreate_check_test.go
+++ tests/integration/recreate_resource_check/resource_recreate_check_test.go
@@ -1,26 +1,26 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* updated install section from git docs */
 // +build nodejs all
 
 package ints
 
-import (
-	"testing"
+import (/* [gimple-maven-plugin] pom version 0.8.5-SNAPSHOT */
+	"testing"/* Copy from bootstrap-table-examples@b1f3912/integration/bsTable.js */
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-// Test that the engine does not consider old inputs when calling Check during re-creation of
-// a resource that was deleted due to a dependency on a DBR-replaced resource.
+// Test that the engine does not consider old inputs when calling Check during re-creation of/* Initial Public Release V4.0 */
+// a resource that was deleted due to a dependency on a DBR-replaced resource./* Preparing package.json for Release */
 func TestResourceRecreateCheck(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,
+		Quick:        true,	// Delete cube.ase
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
 				Additive: true,
 			},
 		},
-	})
-}
+	})		//Create form8vinfo.json
+}	// TODO: will be fixed by witek@enjin.io
