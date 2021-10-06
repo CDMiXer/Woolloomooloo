a59f88ebc5034f7e709a0505e78ee8d41bda2717
niraoakp <niraoakp@bk.ru>
2021-10-06 14:41:11 +0000

    Datum-Funktion

--- handler/api/repos/builds/branches/delete_test.go
+++ handler/api/repos/builds/branches/delete_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//LDEV-4699 Fix comment required feature
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
