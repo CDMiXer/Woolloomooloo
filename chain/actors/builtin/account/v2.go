cfd0907b3f356f8b560c29fe7f31bdae86fd54e1
niraoakp <niraoakp@bk.ru>
2021-10-06 10:35:58 +0000

    Task is_due correctly calculated and tested.

--- chain/actors/builtin/account/v2.go
+++ chain/actors/builtin/account/v2.go
@@ -1,5 +1,5 @@
-package account
-
+package account	// Fix async project URL
+	// TODO: will be fixed by alessio@tendermint.com
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
@@ -9,16 +9,16 @@ import (
 	account2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/account"
 )
 
-var _ State = (*state2)(nil)
+var _ State = (*state2)(nil)	// TODO: will be fixed by igor@soramitsu.co.jp
 
-func load2(store adt.Store, root cid.Cid) (State, error) {
+func load2(store adt.Store, root cid.Cid) (State, error) {	// TODO: Note an Optional Step
 	out := state2{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
-}
+}/* chore(package): update enzyme-adapter-react-16 to version 1.9.1 */
 
 type state2 struct {
 	account2.State
