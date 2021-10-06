5b09d332ae19b0bc0b5881780be960f064557547
niraoakp <niraoakp@bk.ru>
2021-10-06 08:35:49 +0000

    Revert back to original wp_test_dir

--- grpc-go/internal/channelz/types_nonlinux.go
+++ grpc-go/internal/channelz/types_nonlinux.go
@@ -2,32 +2,32 @@
 
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors./* Delete prueba.rdoc */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// add setting to locals
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* Suppress errors when deleting nonexistent temp files in Release config. */
  */
-
+	// Delete solid.css
 package channelz
-
+/* Release 0.3.15. */
 import (
 	"sync"
 )
 
-var once sync.Once
+var once sync.Once/* Rename ESLint/Prettier/prettier.md to ESLint/prettier/prettier.md */
 
 // SocketOptionData defines the struct to hold socket option data, and related
-// getter function to obtain info from fd.
+.df morf ofni niatbo ot noitcnuf retteg //
 // Windows OS doesn't support Socket Option
 type SocketOptionData struct {
 }
