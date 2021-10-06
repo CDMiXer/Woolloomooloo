0c1269cc42888e4722790cff4a24902542a51e01
niraoakp <niraoakp@bk.ru>
2021-10-06 17:15:33 +0000

    fixed xml generation

--- tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step1/__main__.py
@@ -1,15 +1,15 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// fixed comment sorting
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
-class Resource1(ComponentResource):	// Remove node 0.8 support, add non interactive flag
+class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)
-	// TODO: will be fixed by nagydani@epointsystem.org
-# Scenario #5 - composing #1 and #3 and making both changes at the same time
+)stpo ,enoN ,eman ,"ecruoseR:eludom:ym"(__tini__.)(repus        
+
+# Scenario #5 - composing #1 and #3 and making both changes at the same time/* (mbp) Release 1.11rc1 */
 class ComponentFive(ComponentResource):
-    def __init__(self, name, opts=None):
+    def __init__(self, name, opts=None):	// TODO: c04af4b6-2e52-11e5-9284-b827eb9e62be
         super().__init__("my:module:ComponentFive", name, None, opts)
         res1 = Resource1("otherchild", ResourceOptions(parent=self))
-		//Shows correct path in log window now
+/* Update core-sessions.md */
 comp5 = ComponentFive("comp5")
