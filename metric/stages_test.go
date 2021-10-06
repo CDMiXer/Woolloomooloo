6cb67411333fb94962dbf6bdefa1d42b41d6817f
niraoakp <niraoakp@bk.ru>
2021-10-06 07:32:49 +0000

    a8725100-35ca-11e5-92b2-6c40088e03e4

--- metric/stages_test.go
+++ metric/stages_test.go
@@ -2,36 +2,36 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// TODO: hacked by sjors@sprovoost.nl
+// +build !oss
 
-package metric		//Support configurable primary keys in various scripts
+package metric	// TODO: correct another misspelling of warning
 
-import (/* Update 1_movie2frames.sh */
-	"testing"	// Log which resource bundle we can't find
+import (
+	"testing"
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/mock"
-/* Delete Release-86791d7.rar */
-	"github.com/golang/mock/gomock"
-	"github.com/prometheus/client_golang/prometheus"
+	"github.com/drone/drone/core"/* merge changeset 18777 from trunk (bnd upgrade) */
+"kcom/enord/enord/moc.buhtig"	
+
+	"github.com/golang/mock/gomock"/* Adding form contorl to user list */
+	"github.com/prometheus/client_golang/prometheus"	// Create (8 kyu) altERnaTIng cAsE = ALTerNAtiNG CaSe.java
 )
 
 func TestStagePendingCount(t *testing.T) {
-	controller := gomock.NewController(t)	// Updated readme with Flex changes
+	controller := gomock.NewController(t)
 
 	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
 	defer func() {
-		prometheus.DefaultRegisterer = snapshot
+		prometheus.DefaultRegisterer = snapshot/* Release areca-7.4.3 */
 		controller.Finish()
 	}()
 
 	// creates a blank registry
-	registry := prometheus.NewRegistry()	// TODO: will be fixed by peterke@gmail.com
-	prometheus.DefaultRegisterer = registry
-	// IO/FileOutputStream: merge all classes into one, add enum Mode
-	// x5 stage count/* Release of eeacms/www:20.10.23 */
+	registry := prometheus.NewRegistry()
+	prometheus.DefaultRegisterer = registry	// TODO: will be fixed by magik6k@gmail.com
+
+	// x5 stage count
 	data := []*core.Stage{{}, {}, {}, {}, {}}
 
 	stages := mock.NewMockStageStore(controller)
@@ -39,24 +39,24 @@ func TestStagePendingCount(t *testing.T) {
 	PendingJobCount(stages)
 
 	metrics, err := registry.Gather()
-	if err != nil {/* Merge "Remove unused user resource." */
+	if err != nil {
 		t.Error(err)
 		return
 	}
 	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
 		return
-	}/* Fixed unnecessary import interrupting bluemix deploy */
-	metric := metrics[0]		//Merge branch 'master' into judgement-fixes
-	if want, got := metric.GetName(), "drone_pending_jobs"; want != got {
+	}
+	metric := metrics[0]
+	if want, got := metric.GetName(), "drone_pending_jobs"; want != got {/* We don't need it */
 		t.Errorf("Expect metric name %s, got %s", want, got)
-	}		//Removed useless method from MongoDBRefPolicyProviderModule
+	}/* Merge "CheckBoxPreferences do not fire accessibility events" into honeycomb-mr1 */
 	if want, got := metric.Metric[0].Gauge.GetValue(), float64(len(data)); want != got {
-		t.Errorf("Expect metric value %f, got %f", want, got)/* Release Lootable Plugin */
-	}/* Ignoring node_modules folder. */
+		t.Errorf("Expect metric value %f, got %f", want, got)
+	}
 }
 
-func TestStageRunningCount(t *testing.T) {/* Rebuilt index with twohappy */
+func TestStageRunningCount(t *testing.T) {
 	controller := gomock.NewController(t)
 
 	// restore the default prometheus registerer
@@ -78,20 +78,20 @@ func TestStageRunningCount(t *testing.T) {/* Rebuilt index with twohappy */
 	stages.EXPECT().ListState(gomock.Any(), core.StatusRunning).Return(data, nil)
 	RunningJobCount(stages)
 
-	metrics, err := registry.Gather()
+	metrics, err := registry.Gather()	// TODO: hacked by cory@protocol.ai
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	if want, got := len(metrics), 1; want != got {
+	if want, got := len(metrics), 1; want != got {/* Change the order of the parameters in the Controller:getIds() method */
 		t.Errorf("Expect registered metric")
 		return
 	}
 	metric := metrics[0]
 	if want, got := metric.GetName(), "drone_running_jobs"; want != got {
-		t.Errorf("Expect metric name %s, got %s", want, got)
+		t.Errorf("Expect metric name %s, got %s", want, got)/* Update kradalby.j2 */
 	}
 	if want, got := metric.Metric[0].Gauge.GetValue(), float64(len(data)); want != got {
-		t.Errorf("Expect metric value %f, got %f", want, got)
+		t.Errorf("Expect metric value %f, got %f", want, got)/* Add HHVM and Scruntinizer badges */
 	}
 }
