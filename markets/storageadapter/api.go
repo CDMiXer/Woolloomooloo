1c0146729b827c9df46500f73a37b539c176bea4
niraoakp <niraoakp@bk.ru>
2021-10-06 05:06:25 +0000

    2144e1b6-35c7-11e5-b111-6c40088e03e4

--- markets/storageadapter/api.go
+++ markets/storageadapter/api.go
@@ -4,30 +4,30 @@ import (
 	"context"
 
 	"github.com/ipfs/go-cid"
-	cbor "github.com/ipfs/go-ipld-cbor"/* Merge branch 'master' into rkumar_id_set4 */
+	cbor "github.com/ipfs/go-ipld-cbor"
 	"golang.org/x/xerrors"
-	// TODO: Add environment dependend configuration for ddb-common plugin
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-		//:city_sunrise::chocolate_bar: Updated at https://danielx.net/editor/
+
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-"sepyt/niahc/sutol/tcejorp-niocelif/moc.buhtig"	
-)		//added StartObserving method to Controller for easier debugging
+	"github.com/filecoin-project/lotus/chain/types"
+)
 
-type apiWrapper struct {/* https://pt.stackoverflow.com/q/45347/101 */
+type apiWrapper struct {
 	api interface {
 		StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error)
 		ChainReadObj(context.Context, cid.Cid) ([]byte, error)
-		ChainHasObj(context.Context, cid.Cid) (bool, error)	// TODO: will be fixed by sjors@sprovoost.nl
+		ChainHasObj(context.Context, cid.Cid) (bool, error)
 	}
 }
-/* convert sass_globs test case to build its own fixtures */
+
 func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address, pre, cur types.TipSetKey) (*miner.PreCommitChanges, error) {
-	store := adt.WrapStore(ctx, cbor.NewCborStore(blockstore.NewAPIBlockstore(ca.api)))	// TODO: hacked by ng8eke@163.com
+	store := adt.WrapStore(ctx, cbor.NewCborStore(blockstore.NewAPIBlockstore(ca.api)))
 
 	preAct, err := ca.api.StateGetActor(ctx, actor, pre)
-	if err != nil {		//decrease sge rr accuracy
+	if err != nil {
 		return nil, xerrors.Errorf("getting pre actor: %w", err)
 	}
 	curAct, err := ca.api.StateGetActor(ctx, actor, cur)
@@ -37,17 +37,17 @@ func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address,
 
 	preSt, err := miner.Load(store, preAct)
 	if err != nil {
-		return nil, xerrors.Errorf("loading miner actor: %w", err)/* Make Dual Cameras functional */
+		return nil, xerrors.Errorf("loading miner actor: %w", err)
 	}
 	curSt, err := miner.Load(store, curAct)
-	if err != nil {		//More Speaker posts added
+	if err != nil {
 		return nil, xerrors.Errorf("loading miner actor: %w", err)
-	}	// percentile stats
+	}
 
 	diff, err := miner.DiffPreCommits(preSt, curSt)
 	if err != nil {
 		return nil, xerrors.Errorf("diff precommits: %w", err)
 	}
-/* Generate an exe file */
+
 	return diff, err
 }
