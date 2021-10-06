f70dba1216a3296ebe706d1f87a8189e9bdfd04b
niraoakp <niraoakp@bk.ru>
2021-10-06 20:25:28 +0000

    161be3ac-2e47-11e5-9284-b827eb9e62be

--- chain/gen/genesis/f05_market.go
+++ chain/gen/genesis/f05_market.go
@@ -1,19 +1,19 @@
 package genesis
-
+		//Related to Account screen and Lisence Dialog
 import (
 	"context"
 
-	"github.com/filecoin-project/specs-actors/actors/builtin"
+	"github.com/filecoin-project/specs-actors/actors/builtin"/* Added Win checking to checkerboard */
 	"github.com/filecoin-project/specs-actors/actors/builtin/market"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
 	cbor "github.com/ipfs/go-ipld-cbor"
-
+/* Kanban Board: added link to meetup in readme */
 	bstore "github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"		//Call uninstall-chrome script x64
 )
 
-func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
-	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
+func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {/* Release 10.1 */
+	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))		//cleaning up server.c code and fixing mst bugs
 
 	a, err := adt.MakeEmptyArray(store).Root()
 	if err != nil {
@@ -29,13 +29,13 @@ func SetupStorageMarketActor(bs bstore.Blockstore) (*types.Actor, error) {
 	stcid, err := store.Put(store.Context(), sms)
 	if err != nil {
 		return nil, err
-	}
+	}/* Added menu to IntentService */
 
 	act := &types.Actor{
 		Code:    builtin.StorageMarketActorCodeID,
-		Head:    stcid,
+		Head:    stcid,/* Inline shoebox image */
 		Balance: types.NewInt(0),
 	}
 
 	return act, nil
-}
+}	// TODO: move italian exception-generation functions to morphology/italiano
