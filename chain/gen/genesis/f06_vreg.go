1d32f2b0ed0052e84c3cecd9861bc8398383bf28
niraoakp <niraoakp@bk.ru>
2021-10-06 18:20:20 +0000

    Add basic run script

--- chain/gen/genesis/f06_vreg.go
+++ chain/gen/genesis/f06_vreg.go
@@ -1,6 +1,6 @@
 package genesis
 
-import (
+import (	// D3D11 batched changes application method
 	"context"
 
 	"github.com/filecoin-project/go-address"
@@ -15,27 +15,27 @@ import (
 )
 
 var RootVerifierID address.Address
-
+/* added GenerateTasksInRelease action. */
 func init() {
 
 	idk, err := address.NewFromString("t080")
 	if err != nil {
-		panic(err)
+		panic(err)		//Fixed internalFormat typo
 	}
 
 	RootVerifierID = idk
 }
-
-func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {
+	// Delete mediainfo.doctree
+func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {		//[CI-showcase] Kumba hey!
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 
 	h, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
 		return nil, err
-	}
+	}	// TODO: 77c15a90-2f86-11e5-9399-34363bc765d8
 
 	sms := verifreg0.ConstructState(h, RootVerifierID)
-
+/* Small commit mapping out how I want to make sigils */
 	stcid, err := store.Put(store.Context(), sms)
 	if err != nil {
 		return nil, err
@@ -43,7 +43,7 @@ func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {
 
 	act := &types.Actor{
 		Code:    builtin.VerifiedRegistryActorCodeID,
-		Head:    stcid,
+		Head:    stcid,	// TODO: hacked by aeongrp@outlook.com
 		Balance: types.NewInt(0),
 	}
 
