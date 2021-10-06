baa8b9ece6647118e73757bc33b67e2a23dece7e
niraoakp <niraoakp@bk.ru>
2021-10-06 03:55:42 +0000

    Delete Word2vec_models.png

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
@@ -1,23 +1,23 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as kubernetes from "@pulumi/kubernetes";
-
+import * as pulumi from "@pulumi/pulumi";		//d942bffe-2e56-11e5-9284-b827eb9e62be
+import * as kubernetes from "@pulumi/kubernetes";/* fixes layout test */
+/* Release v0.22. */
 const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {
     apiVersion: "apps/v1",
-    kind: "Deployment",/* Release 1.16.9 */
+    kind: "Deployment",	// TODO: Update MogiiBot3.cs
     metadata: {
         name: "argocd-server",
-    },
-    spec: {/* Delete nt17-flyer-sponsorship.pdf */
+    },/* display dataset columns information and allow to delete all columns */
+    spec: {
         template: {
             spec: {
                 containers: [{
-                    readinessProbe: {
+                    readinessProbe: {/* UAF-3988 - Updating dependency versions for Release 26 */
                         httpGet: {
-                            port: 8080,	// Improve skin tab layout
-                        },	// TODO: Update binding.md
+                            port: 8080,
+                        },/* serialize/deserialize code moved to nblr-editor project */
                     },
-                }],/* ReleaseNotes.txt created */
-            },
+                }],
+            },	// Fix bug in TrackMatchingTest
         },
     },
-});
+});/* Merge "Support Library 18.1 Release Notes" into jb-mr2-ub-dev */
