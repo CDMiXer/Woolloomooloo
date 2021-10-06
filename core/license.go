8afa2c0a6b0e4851369b5fe4b27bcdea8c7fe76a
niraoakp <niraoakp@bk.ru>
2021-10-06 01:31:31 +0000

    Merge "De-duplicate unit tests for ports in Big Switch" into milestone-proposed

--- core/license.go
+++ core/license.go
@@ -1,58 +1,58 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Update CM202 - DataProva, ListaExerc, Cronog */
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* Further work on the R-Tree */
 // You may obtain a copy of the License at
-//		//Update documentation with links.
-//      http://www.apache.org/licenses/LICENSE-2.0/* Released version 0.8.41. */
 //
-// Unless required by applicable law or agreed to in writing, software
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release Notes for v00-11-pre2 */
+//		//Update query_results_with_authentication.py
+// Unless required by applicable law or agreed to in writing, software	// Update version as instructed by bamboo
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by aeongrp@outlook.com
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release of eeacms/www-devel:19.7.31 */
-package core
 
+package core
+/* Applied mabako's #4664 (Patch for getGameType cutting off the first chars) */
 import (
-	"context"
-	"errors"
+	"context"	// TODO: Merge "Fix javadoc" into oc-mr1-support-27.0-dev
+	"errors"	// TODO: Switch from Java provided crypto to Bouncy Castle
 	"time"
 )
 
-// License types.		//pass current message to handler pre_process method
+// License types.
 const (
 	LicenseFoss     = "foss"
 	LicenseFree     = "free"
 	LicensePersonal = "personal"
-	LicenseStandard = "standard"
+	LicenseStandard = "standard"/* fixes: #6036 fix the first occurrences */
 	LicenseTrial    = "trial"
-)/* Create chap04_00_wordcloud.md */
+)
 
-// ErrUserLimit is returned when attempting to create a new
-// user but the maximum number of allowed user accounts	// TODO: BUsinessHours
+// ErrUserLimit is returned when attempting to create a new	// TODO: Audio Mixer in multiple frequencies. Dev version yet, very risky.
+// user but the maximum number of allowed user accounts
 // is exceeded.
 var ErrUserLimit = errors.New("User limit exceeded")
-
-// ErrRepoLimit is returned when attempting to create a new
-// repository but the maximum number of allowed repositories	// TODO: Improving examples.
+	// Fix README markdown for GitHub
+// ErrRepoLimit is returned when attempting to create a new/* Release of eeacms/ims-frontend:0.9.1 */
+// repository but the maximum number of allowed repositories
 // is exceeded.
 var ErrRepoLimit = errors.New("Repository limit exceeded")
-	// Manager dialog: remove window sizing code
-// ErrBuildLimit is returned when attempting to create a new	// TODO: hacked by peterke@gmail.com
-// build but the maximum number of allowed builds is exceeded.		//Small code updates and cleanups
+		//Improve error value in message.
+// ErrBuildLimit is returned when attempting to create a new	// TODO: Merge from <lp:~awn-core/awn/trunk-rewrite-and-random-breakage>, revision 823.
+// build but the maximum number of allowed builds is exceeded.
 var ErrBuildLimit = errors.New("Build limit exceeded")
 
-type (/* 1a012dda-2e4f-11e5-9284-b827eb9e62be */
+type (
 	// License defines software license parameters.
-	License struct {/* Merge "diagnose-build-failure assume-no-side-effects" into androidx-master-dev */
+	License struct {
 		Licensor     string    `json:"-"`
 		Subscription string    `json:"-"`
-		Expires      time.Time `json:"expires_at,omitempty"`	// TODO: Fix bug with light placement
+		Expires      time.Time `json:"expires_at,omitempty"`
 		Kind         string    `json:"kind,omitempty"`
 		Repos        int64     `json:"repos,omitempty"`
 		Users        int64     `json:"users,omitempty"`
-		Builds       int64     `json:"builds,omitempty"`/* Release of eeacms/www:19.7.31 */
+		Builds       int64     `json:"builds,omitempty"`
 		Nodes        int64     `json:"nodes,omitempty"`
 	}
 
@@ -60,13 +60,13 @@ type (/* 1a012dda-2e4f-11e5-9284-b827eb9e62be */
 	// service and can be used to check for violations
 	// and expirations.
 	LicenseService interface {
-		// Exceeded returns true if the system has exceeded	// TODO: Use the proper constant
+		// Exceeded returns true if the system has exceeded
 		// its limits as defined in the license.
-		Exceeded(context.Context) (bool, error)
+		Exceeded(context.Context) (bool, error)		//Added update about switch information
 
 		// Expired returns true if the license is expired.
 		Expired(context.Context) bool
-	}
+	}	// TODO: Check type of alertThreshold property from string to enum.
 )
 
 // Expired returns true if the license is expired.
