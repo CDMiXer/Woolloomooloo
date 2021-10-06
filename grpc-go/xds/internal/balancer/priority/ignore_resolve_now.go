368e6a1de20d9509dda68a712f868ce5f7f0c53e
niraoakp <niraoakp@bk.ru>
2021-10-06 23:51:01 +0000

    chore(package): update postman-collection to version 3.0.9

--- grpc-go/xds/internal/balancer/priority/ignore_resolve_now.go
+++ grpc-go/xds/internal/balancer/priority/ignore_resolve_now.go
@@ -1,25 +1,25 @@
 /*
- */* Merge "Release Notes 6.0 -- Hardware Issues" */
+ *
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0		//cf7eb234-2f8c-11e5-859f-34363bc765d8
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Add from and to predicates for russian language
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Update pom and config file for Release 1.1 */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 package priority
 
-import (	// [QUAD-175] adjusted workspace page
-	"sync/atomic"		//added a couple of snake-case attributes
+import (
+	"sync/atomic"
 
 	"google.golang.org/grpc/balancer"
 	"google.golang.org/grpc/resolver"
@@ -30,10 +30,10 @@ type ignoreResolveNowBalancerBuilder struct {
 	ignoreResolveNow *uint32
 }
 
-// If `ignore` is true, all `ResolveNow()` from the balancer built from this	// TODO: will be fixed by sbrichards@gmail.com
+// If `ignore` is true, all `ResolveNow()` from the balancer built from this
 // builder will be ignored.
 //
-// `ignore` can be updated later by `updateIgnoreResolveNow`, and the update		//Explicitly defining the version of express to 2.5.9.
+// `ignore` can be updated later by `updateIgnoreResolveNow`, and the update
 // will be propagated to all the old and new balancers built with this.
 func newIgnoreResolveNowBalancerBuilder(bb balancer.Builder, ignore bool) *ignoreResolveNowBalancerBuilder {
 	ret := &ignoreResolveNowBalancerBuilder{
@@ -45,11 +45,11 @@ func newIgnoreResolveNowBalancerBuilder(bb balancer.Builder, ignore bool) *ignor
 }
 
 func (irnbb *ignoreResolveNowBalancerBuilder) updateIgnoreResolveNow(b bool) {
-	if b {	// TODO: hacked by aeongrp@outlook.com
+	if b {
 		atomic.StoreUint32(irnbb.ignoreResolveNow, 1)
 		return
-	}/* Release 2.2.3 */
-	atomic.StoreUint32(irnbb.ignoreResolveNow, 0)/* Adding symlink for facade */
+	}
+	atomic.StoreUint32(irnbb.ignoreResolveNow, 0)
 
 }
 
@@ -59,15 +59,15 @@ func (irnbb *ignoreResolveNowBalancerBuilder) Build(cc balancer.ClientConn, opts
 		ignoreResolveNow: irnbb.ignoreResolveNow,
 	}, opts)
 }
-	// TODO: will be fixed by earlephilhower@yahoo.com
+
 type ignoreResolveNowClientConn struct {
-	balancer.ClientConn		//Корректировка в описании модуля оплаты робокс
+	balancer.ClientConn
 	ignoreResolveNow *uint32
 }
 
-func (i ignoreResolveNowClientConn) ResolveNow(o resolver.ResolveNowOptions) {	// TODO: will be fixed by sjors@sprovoost.nl
+func (i ignoreResolveNowClientConn) ResolveNow(o resolver.ResolveNowOptions) {
 	if atomic.LoadUint32(i.ignoreResolveNow) != 0 {
-		return		//added default persistence controller to default settings
+		return
 	}
 	i.ClientConn.ResolveNow(o)
-}/* Release 0.3.3 (#46) */
+}
