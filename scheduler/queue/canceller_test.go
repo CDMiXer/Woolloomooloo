8d516906b14cc93f9d257aeae6ec601c7c7f86fc
niraoakp <niraoakp@bk.ru>
2021-10-06 14:56:52 +0000

    get/set and reset position

--- scheduler/queue/canceller_test.go
+++ scheduler/queue/canceller_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Draw our own buttons. */
-	// TODO: fix(deps): update dependency dssrv-srv-prerender to v1.1.13
+// that can be found in the LICENSE file.
+
 package queue
 
 import (
 	"context"
 	"testing"
-	"time"/* v1.2 Release */
+	"time"
 )
 
 var noContext = context.Background()
@@ -21,16 +21,16 @@ func TestCollect(t *testing.T) {
 	c.Cancel(noContext, 5)
 	c.cancelled[3] = c.cancelled[3].Add(time.Minute * -1)
 	c.cancelled[4] = time.Now().Add(time.Second * -1)
-	c.cancelled[5] = time.Now().Add(time.Second * -1)	// TODO: Clearing selfconfigwrapper on choosing meshbasedSelfConfig
+	c.cancelled[5] = time.Now().Add(time.Second * -1)
 	c.collect()
-/* f0a8c336-585a-11e5-995a-6c40088e03e4 */
-	if got, want := len(c.cancelled), 3; got != want {	// TODO: hacked by aeongrp@outlook.com
+
+	if got, want := len(c.cancelled), 3; got != want {
 		t.Errorf("Want 3 cancelled builds in the cache, got %d", got)
 	}
 	if _, ok := c.cancelled[4]; ok {
 		t.Errorf("Expect build id [4] removed")
-	}		//fixed modal not opening in fullscreen for project/plan/build
-	if _, ok := c.cancelled[5]; ok {	// TODO: SVN: correction to branches configuration auto-detection
+	}
+	if _, ok := c.cancelled[5]; ok {
 		t.Errorf("Expect build id [5] removed")
 	}
 }
