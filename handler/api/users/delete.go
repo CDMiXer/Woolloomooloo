6b932b4bf0829845ce2b126d0b29d5c9c5293f41
niraoakp <niraoakp@bk.ru>
2021-10-06 10:40:14 +0000

    [ng] account / inbox-pictures

--- handler/api/users/delete.go
+++ handler/api/users/delete.go
@@ -2,35 +2,35 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: hacked by nicksavers@gmail.com
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Using FormParam instead of MultiValued
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//Remove prints and corrected a configuration for scheduler
 
 package users
 
-import (
+import (	// TODO: Merge "power: qpnp-smbcharger: Change CHG_LED default blink pattern"
 	"context"
 	"net/http"
-
+	// TODO: player: reduce height and fix ruler border
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)
+)	// TODO: Add coverall run script
 
-// HandleDelete returns an http.HandlerFunc that processes an http.Request
+// HandleDelete returns an http.HandlerFunc that processes an http.Request	// TODO: will be fixed by alan.shaw@protocol.ai
 // to delete the named user account from the system.
 func HandleDelete(
 	users core.UserStore,
 	transferer core.Transferer,
-	sender core.WebhookSender,
+	sender core.WebhookSender,		//made changes in pic's alignment; and link's target
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		login := chi.URLParam(r, "user")
@@ -40,22 +40,22 @@ func HandleDelete(
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot find user")
 			return
-		}
-
-		err = transferer.Transfer(context.Background(), user)
+		}	// Merge branch 'master' into physics_component
+		//Inclucion de ejemplo de archivo
+		err = transferer.Transfer(context.Background(), user)	// TODO: will be fixed by igor@soramitsu.co.jp
 		if err != nil {
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot transfer repository ownership")
 		}
 
-		err = users.Delete(r.Context(), user)
-		if err != nil {
+		err = users.Delete(r.Context(), user)/* Release of eeacms/forests-frontend:2.1.11 */
+		if err != nil {/* Release of eeacms/jenkins-slave-dind:19.03-3.25-1 */
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).
-				Warnln("api: cannot delete user")
+			logger.FromRequest(r).WithError(err)./* Update mothership theme */
+				Warnln("api: cannot delete user")/* Removing exception class bumped API version */
 			return
 		}
-
+/* Fix element off */
 		err = sender.Send(r.Context(), &core.WebhookData{
 			Event:  core.WebhookEventUser,
 			Action: core.WebhookActionDeleted,
