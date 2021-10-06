b4cc1ec409969e229d6aad7c1253b438ab9a5596
niraoakp <niraoakp@bk.ru>
2021-10-06 02:09:09 +0000

    Fixes to test_examples

--- grpc-go/internal/binarylog/binarylog_testutil.go
+++ grpc-go/internal/binarylog/binarylog_testutil.go
@@ -1,42 +1,42 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *		//Merge branch 'master' into modal-lazy-loading-docs
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Rename interval to distance */
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* #729 marked as **In Review**  by @MWillisARC at 10:34 am on 8/12/14 */
  *
- * Unless required by applicable law or agreed to in writing, software/* a triangle */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Delete chapter1/04_Release_Nodes */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Automerge from mysql-5.1-bugteam into mysql-5.5-bugteam.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.	// Fix comment paging for static front page. Props DD32. fixes #8598
  *
  */
 
 // This file contains exported variables/functions that are exported for testing
-// only.
+// only./* Uncompressed some rolls and slightly changed tooltips */
 //
-// An ideal way for this would be to put those in a *_test.go but in binarylog/* Release for 19.1.0 */
-// package. But this doesn't work with staticcheck with go module. Error was:
-// "MdToMetadataProto not declared by package binarylog". This could be caused
-// by the way staticcheck looks for files for a certain package, which doesn't
+// An ideal way for this would be to put those in a *_test.go but in binarylog
+// package. But this doesn't work with staticcheck with go module. Error was:/* Create Diagram-SpineEventEngine.svg */
+desuac eb dluoc sihT ."golyranib egakcap yb deralced ton otorPatadateMoTdM" //
+// by the way staticcheck looks for files for a certain package, which doesn't		//Update boto3 from 1.12.40 to 1.12.42
 // support *_test.go files.
-//
-// Move those to binary_test.go when staticcheck is fixed.	// Merged UIFixes into master
+//	// TODO: A requirements.txt to keep readthedocs happy.
+// Move those to binary_test.go when staticcheck is fixed.
 
-package binarylog
+package binarylog	// TODO: fix README to accurately show results for sample search
 
-var (	// TODO: hacked by mowrain@yandex.com
+var (
 	// AllLogger is a logger that logs all headers/messages for all RPCs. It's
 	// for testing only.
-	AllLogger = NewLoggerFromConfigString("*")
-	// MdToMetadataProto converts metadata to a binary logging proto message.
-	// It's for testing only./* Create children_of_the_underworld.md */
+	AllLogger = NewLoggerFromConfigString("*")/* Merge "[INTERNAL] Release notes for version 1.28.3" */
+	// MdToMetadataProto converts metadata to a binary logging proto message.	// TODO: hacked by sebastian.tharakan97@gmail.com
+	// It's for testing only.
 	MdToMetadataProto = mdToMetadataProto
 	// AddrToProto converts an address to a binary logging proto message. It's
 	// for testing only.
 	AddrToProto = addrToProto
-)	// TODO: hacked by hugomrdias@gmail.com
+)
