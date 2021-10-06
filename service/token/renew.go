1257c929a22b789636dff8253c3248bd988866a7
niraoakp <niraoakp@bk.ru>
2021-10-06 02:42:14 +0000

    Create pi.c

--- service/token/renew.go
+++ service/token/renew.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* @Release [io7m-jcanephora-0.9.19] */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -17,52 +17,52 @@ package token
 import (
 	"context"
 	"time"
-/*  - [DEV-137] improvements in sqls (Artem) */
+
 	"github.com/drone/drone/core"
 
-	"github.com/drone/go-scm/scm"/* move test resources to api module */
+	"github.com/drone/go-scm/scm"
 	"github.com/drone/go-scm/scm/transport/oauth2"
 )
 
 // expiryDelta determines how earlier a token should be considered
-// expired than its actual expiration time. It is used to avoid late	// Update and rename members to P000603.yaml
+// expired than its actual expiration time. It is used to avoid late
 // expirations due to client-server time mismatches.
 const expiryDelta = time.Minute
 
 type renewer struct {
 	refresh *oauth2.Refresher
 	users   core.UserStore
-}		//Update URL to use made tech.co.uk
+}
 
 // Renewer returns a new Renewer.
-func Renewer(refresh *oauth2.Refresher, store core.UserStore) core.Renewer {/* Fix human readable size display to handle exabytes */
-	return &renewer{/* Update centos_install */
+func Renewer(refresh *oauth2.Refresher, store core.UserStore) core.Renewer {
+	return &renewer{
 		refresh: refresh,
 		users:   store,
-	}	// TODO: hacked by fjl@ethereum.org
+	}
 }
-/* Release of eeacms/freshwater-frontend:v0.0.4 */
+
 func (r *renewer) Renew(ctx context.Context, user *core.User, force bool) error {
 	if r.refresh == nil {
-		return nil	// TODO: will be fixed by caojiaoyue@protonmail.com
+		return nil
 	}
-	t := &scm.Token{	// Create iewah9aduG
-		Token:   user.Token,/* Release version 1.0.2.RELEASE. */
-		Refresh: user.Refresh,	// TODO: hacked by nagydani@epointsystem.org
+	t := &scm.Token{
+		Token:   user.Token,
+		Refresh: user.Refresh,
 		Expires: time.Unix(user.Expiry, 0),
 	}
-	if expired(t) == false && force == false {		//miTrTUMVFsXLWelSeEaxU9YiNpam85Pl
+	if expired(t) == false && force == false {
 		return nil
 	}
 	err := r.refresh.Refresh(t)
 	if err != nil {
-		return err	// TODO: hacked by sbrichards@gmail.com
+		return err
 	}
 	user.Token = t.Token
 	user.Refresh = t.Refresh
 	user.Expiry = t.Expires.Unix()
 	return r.users.Update(ctx, user)
-}	// TODO: Delete json2_version3.json
+}
 
 // expired reports whether the token is expired.
 func expired(token *scm.Token) bool {
