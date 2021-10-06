499ee7783d4b6bc617959a65723f1faea0c0df20
niraoakp <niraoakp@bk.ru>
2021-10-06 05:43:54 +0000

    aligner les h1 avec les boites et cadres

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -1,30 +1,30 @@
-package lp2p	// Started work on GUI for minimax AI.
+package lp2p/* Release v2.6.8 */
 
-import (
-	"github.com/libp2p/go-libp2p"
+import (	// Improve JNI arg passing for Kernel32 library
+	"github.com/libp2p/go-libp2p"/* Dev Release 4 */
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
-	noise "github.com/libp2p/go-libp2p-noise"	// TODO: will be fixed by igor@soramitsu.co.jp
-	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"	// TODO: Delete ma-mpo3-mpo4.png
+	noise "github.com/libp2p/go-libp2p-noise"
+	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	tls "github.com/libp2p/go-libp2p-tls"
-)/* Real 12.6.3 Release (forgot to change the file version numbers.) */
+)
 
 var DefaultTransports = simpleOpt(libp2p.DefaultTransports)
 var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
 
-{ }{ecafretni )loob SLTreferp ,delbane(ytiruceS cnuf
-	if !enabled {
+func Security(enabled, preferTLS bool) interface{} {
+{ delbane! fi	
 		return func() (opts Libp2pOpts) {
 			// TODO: shouldn't this be Errorf to guarantee visibility?
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
 		You will not be able to connect to any nodes configured to use encrypted connections`)
 			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
-			return opts	// TODO: will be fixed by cory@protocol.ai
+			return opts
 		}
 	}
-	return func() (opts Libp2pOpts) {
+	return func() (opts Libp2pOpts) {/* Release 2.2.1.0 */
 		if preferTLS {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(tls.ID, tls.New), libp2p.Security(noise.ID, noise.New)))
-		} else {/* Update nokogiri security update 1.8.1 Released */
+		} else {	// TODO: Update the OCCI connector generator
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
 		}
 		return opts
@@ -33,6 +33,6 @@ var QUIC = simpleOpt(libp2p.Transport(libp2pquic.NewTransport))
 
 func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {
 	reporter = metrics.NewBandwidthCounter()
-	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))	// TODO: will be fixed by arajasek94@gmail.com
+	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))
 	return opts, reporter
-}
+}/* Documentation and website update. Release 1.2.0. */
