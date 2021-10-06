aca6e27b372d38e87570520d7bf08cccaf2d09c1
niraoakp <niraoakp@bk.ru>
2021-10-06 02:45:13 +0000

    ragtimea : correct rom loading generally helps.

--- store/shared/migrate/mysql/files/007_create_table_logs.sql
+++ store/shared/migrate/mysql/files/007_create_table_logs.sql
@@ -1,6 +1,6 @@
--- name: create-table-logs	// TODO: hacked by timnugent@gmail.com
-
-CREATE TABLE IF NOT EXISTS logs (
+-- name: create-table-logs
+		//b35c4db4-2e6a-11e5-9284-b827eb9e62be
+CREATE TABLE IF NOT EXISTS logs (	// TODO: will be fixed by magik6k@gmail.com
  log_id    INTEGER PRIMARY KEY
 ,log_data  MEDIUMBLOB
 );
