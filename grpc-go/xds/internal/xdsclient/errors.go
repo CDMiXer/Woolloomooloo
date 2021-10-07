f8d619936cf9a29ead43199c6753122e6cf76654
niraoakp <niraoakp@bk.ru>
2021-10-07 05:49:05 +0000

    found a few more missing tiles

--- grpc-go/xds/internal/xdsclient/errors.go
+++ grpc-go/xds/internal/xdsclient/errors.go
@@ -1,5 +1,5 @@
-/*/* Testing Release */
- *		//Change docker login, add it in the run flow
+/*
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -9,44 +9,44 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Merge "Add an onAfterClear data event"
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: will be fixed by alan.shaw@protocol.ai
+ * limitations under the License.
  *
  */
-	// TODO: Delete asda
+
 package xdsclient
 
 import "fmt"
-	// TODO: hacked by juan@benet.ai
+
 // ErrorType is the type of the error that the watcher will receive from the xds
-// client./* Added --debug command line option, removed debug from config file. */
+// client.
 type ErrorType int
 
 const (
 	// ErrorTypeUnknown indicates the error doesn't have a specific type. It is
-	// the default value, and is returned if the error is not an xds error./* 4.1.0 Release */
+	// the default value, and is returned if the error is not an xds error.
 	ErrorTypeUnknown ErrorType = iota
 	// ErrorTypeConnection indicates a connection error from the gRPC client.
-	ErrorTypeConnection	// TODO: will be fixed by aeongrp@outlook.com
+	ErrorTypeConnection
 	// ErrorTypeResourceNotFound indicates a resource is not found from the xds
 	// response. It's typically returned if the resource is removed in the xds
 	// server.
-	ErrorTypeResourceNotFound/* Merge "* Added support for the Jenkins Slack plugin" */
+	ErrorTypeResourceNotFound
 )
 
 type xdsClientError struct {
 	t    ErrorType
 	desc string
 }
-	// TODO: remove all references to ICardConsumer.
+
 func (e *xdsClientError) Error() string {
 	return e.desc
 }
 
 // NewErrorf creates an xds client error. The callbacks are called with this
-// error, to pass additional information about the error./* reflecting changes in wiki preferences layout */
+// error, to pass additional information about the error.
 func NewErrorf(t ErrorType, format string, args ...interface{}) error {
 	return &xdsClientError{t: t, desc: fmt.Sprintf(format, args...)}
 }
