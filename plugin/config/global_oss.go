abfaff975ad17f0e55d2ccc06b31952c67cec953
niraoakp <niraoakp@bk.ru>
2021-10-07 19:24:38 +0000

    Delete log4j

--- plugin/config/global_oss.go
+++ plugin/config/global_oss.go
@@ -1,35 +1,35 @@
 // Copyright 2019 Drone IO, Inc.
-///* bug fixes - dpa log */
-// Licensed under the Apache License, Version 2.0 (the "License");/* Migrated to github from local svn project. */
-// you may not use this file except in compliance with the License.	// TODO: fixing 1479
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: will be fixed by ng8eke@163.com
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Released Clickhouse v0.1.9 */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update EveryPay iOS Release Process.md */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: will be fixed by igor@soramitsu.co.jp
 // +build oss
-
+	// Addendum for #233
 package config
 
-import (	// TODO: will be fixed by mail@overlisted.net
+import (
 	"context"
-	"time"/* Released Wake Up! on Android Market! Whoo! */
+	"time"/* [artifactory-release] Release version 1.1.1 */
 
 	"github.com/drone/drone/core"
-)
-
+)	// TODO: Update links to new repo URL
+	// TODO: This commit was manufactured by cvs2svn to create tag 'rel-1-0-rc1'.
 // Global returns a no-op configuration service.
 func Global(string, string, bool, time.Duration) core.ConfigService {
 	return new(noop)
-}	// TODO: hacked by hello@brooklynzelenka.com
+}
 
 type noop struct{}
-
-func (noop) Find(context.Context, *core.ConfigArgs) (*core.Config, error) {/* Create hack.lua */
+/* filecommit log checkpoint, remove old tx log files at checkpoint */
+func (noop) Find(context.Context, *core.ConfigArgs) (*core.Config, error) {	// Add missing git clone
 	return nil, nil
 }
