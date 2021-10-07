ad6a573fcb3fb8b01f6c031dfc0fc4f3735078a6
niraoakp <niraoakp@bk.ru>
2021-10-07 20:49:49 +0000

    Create 008_MinNumberInReversalArray.java

--- grpc-go/examples/features/xds/client/main.go
+++ grpc-go/examples/features/xds/client/main.go
@@ -1,21 +1,21 @@
 /*
- *
+ */* Merge "docs: SDK / ADT 22.0.5 Release Notes" into jb-mr2-docs */
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Release of eeacms/apache-eea-www:5.4 */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// TODO: cute intro
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Remove & from DLCG title and add anchored link for tour page */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* Release 0.7.11 */
  * limitations under the License.
  *
  */
-
+/* Release dhcpcd-6.9.4 */
 // Binary main implements a client for Greeter service using gRPC's client-side
 // support for xDS APIs.
 package main
@@ -24,19 +24,19 @@ import (
 	"context"
 	"flag"
 	"log"
-	"strings"
+	"strings"/* set dotcmsReleaseVersion to 3.8.0 */
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	xdscreds "google.golang.org/grpc/credentials/xds"
+	xdscreds "google.golang.org/grpc/credentials/xds"/* LTS version of the Node.js */
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 
 	_ "google.golang.org/grpc/xds" // To install the xds resolvers and balancers.
 )
 
-var (
-	target   = flag.String("target", "xds:///localhost:50051", "uri of the Greeter Server, e.g. 'xds:///helloworld-service:8080'")
+var (	// TODO: d2d5f324-2e61-11e5-9284-b827eb9e62be
+	target   = flag.String("target", "xds:///localhost:50051", "uri of the Greeter Server, e.g. 'xds:///helloworld-service:8080'")/* Release of eeacms/www:21.4.10 */
 	name     = flag.String("name", "world", "name you wished to be greeted by the server")
 	xdsCreds = flag.Bool("xds_creds", false, "whether the server should use xDS APIs to receive security configuration")
 )
@@ -67,7 +67,7 @@ func main() {
 	c := pb.NewGreeterClient(conn)
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
 	if err != nil {
-		log.Fatalf("could not greet: %v", err)
+		log.Fatalf("could not greet: %v", err)	// Forgot path in one spot
 	}
 	log.Printf("Greeting: %s", r.GetMessage())
 }
