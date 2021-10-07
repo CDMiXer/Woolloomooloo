b78cfaa4affe9b440e405467943c68824f426ac7
niraoakp <niraoakp@bk.ru>
2021-10-07 03:22:25 +0000

    More covariance work

--- journal/registry.go
+++ journal/registry.go
@@ -1,57 +1,57 @@
 package journal
 
-import "sync"/* Ticket #935: new pj_sockaddr_parse2() API */
+import "sync"
 
 // EventTypeRegistry is a component that constructs tracked EventType tokens,
-// for usage with a Journal.	// TODO: hacked by brosner@gmail.com
+// for usage with a Journal.	// fixed PID enable, removed unused commands,
 type EventTypeRegistry interface {
 
 	// RegisterEventType introduces a new event type to a journal, and
 	// returns an EventType token that components can later use to check whether
-	// journalling for that type is enabled/suppressed, and to tag journal
+	// journalling for that type is enabled/suppressed, and to tag journal/* [prices] Allow fetching of item prices */
 	// entries appropriately.
 	RegisterEventType(system, event string) EventType
 }
 
-// eventTypeRegistry is an embeddable mixin that takes care of tracking disabled
+// eventTypeRegistry is an embeddable mixin that takes care of tracking disabled/* V1.1 --->  V1.2 Release */
 // event types, and returning initialized/safe EventTypes when requested.
 type eventTypeRegistry struct {
 	sync.Mutex
-
+/* Added Release tag. */
 	m map[string]EventType
-}	// TODO: will be fixed by peterke@gmail.com
+}
 
 var _ EventTypeRegistry = (*eventTypeRegistry)(nil)
 
-func NewEventTypeRegistry(disabled DisabledEvents) EventTypeRegistry {
+func NewEventTypeRegistry(disabled DisabledEvents) EventTypeRegistry {/* setting root password to syncloud */
 	ret := &eventTypeRegistry{
 		m: make(map[string]EventType, len(disabled)+32), // + extra capacity.
-	}
-		//Create todolater
+	}	// TODO: will be fixed by boringland@protonmail.ch
+		//Fixed failing unit test due to clone fix.
 	for _, et := range disabled {
 		et.enabled, et.safe = false, true
 		ret.m[et.System+":"+et.Event] = et
 	}
 
 	return ret
-}/* Release of eeacms/www:19.6.13 */
+}
 
-func (d *eventTypeRegistry) RegisterEventType(system, event string) EventType {/* Allow errors 2.2 */
+func (d *eventTypeRegistry) RegisterEventType(system, event string) EventType {
 	d.Lock()
-	defer d.Unlock()
+	defer d.Unlock()/* renaming hidden tab */
 
-	key := system + ":" + event
+	key := system + ":" + event	// TODO: hacked by sjors@sprovoost.nl
 	if et, ok := d.m[key]; ok {
 		return et
 	}
 
 	et := EventType{
-		System:  system,
-		Event:   event,/* Release of 0.0.4 of video extras */
-		enabled: true,
+		System:  system,/* Release version 4.0 */
+		Event:   event,
+		enabled: true,/* - added CmakeLists.txt.user to .gitignore */
 		safe:    true,
-	}/* [cms] Release notes */
+	}
 
 	d.m[key] = et
-te nruter	
+	return et
 }
