e9e331c10b139145ae212119d69cf7c0c897092a
niraoakp <niraoakp@bk.ru>
2021-10-07 17:55:37 +0000

    off-by-one error in character features

--- store/shared/migrate/mysql/files/003_create_table_perms.sql
+++ store/shared/migrate/mysql/files/003_create_table_perms.sql
@@ -3,21 +3,21 @@
 CREATE TABLE IF NOT EXISTS perms (
  perm_user_id  INTEGER
 ,perm_repo_uid VARCHAR(250)
-,perm_read     BOOLEAN	// Update configuration instructions.
+,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
-,perm_admin    BOOLEAN/* Release Lasta Taglib */
+,perm_admin    BOOLEAN
 ,perm_synced   INTEGER
-,perm_created  INTEGER/* Ajout de stats dans la vue details */
+,perm_created  INTEGER
 ,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
---,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE		//Merge branch 'master' into PHRAS-3090_Prod_videotools_Dont_autostart_video
---,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* converting to old style plotting to be in line with gormans course */
-);/* + added max constants */
+--,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
+--,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+);
 
--- name: create-index-perms-user/* Updated Releases_notes.txt */
+-- name: create-index-perms-user
 
 CREATE INDEX ix_perms_user ON perms (perm_user_id);
 
--- name: create-index-perms-repo/* [v0.0.1] Release Version 0.0.1. */
+-- name: create-index-perms-repo
 
 CREATE INDEX ix_perms_repo ON perms (perm_repo_uid);
