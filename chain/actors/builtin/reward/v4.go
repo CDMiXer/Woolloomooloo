28fa77654465fbc36b061a4f2c9756fc192de03e
niraoakp <niraoakp@bk.ru>
2021-10-07 22:00:31 +0000

    make assertFile failures present a helpful diff…

--- chain/actors/builtin/reward/v4.go
+++ chain/actors/builtin/reward/v4.go
@@ -1,43 +1,43 @@
-package reward/* active cache on topic */
+package reward
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* Release version 1.6.0.M2 */
-	"github.com/filecoin-project/lotus/chain/actors/builtin"		//Merge "Ensure that secrets within orders have expiration date isoformatted"
-	// TODO: XMonad.Layout.Magnifier: remove references to Data.Ratio.% from documentation
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
+
 	miner4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
-	reward4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/reward"/* Release 7.12.37 */
-	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"		//Liigutasin resource helpmanager.
+	reward4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/reward"
+	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"
 )
 
-var _ State = (*state4)(nil)		//Implement Slime token for Infested Tauren
+var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
 	out := state4{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}		//Restrict build in screensavers to one download a day
+	}
 	return &out, nil
-}	// Note that running life_api locally is optional
-	// TODO: hacked by arajasek94@gmail.com
-type state4 struct {
-	reward4.State/* Create revert.md */
-	store adt.Store/* Release of eeacms/plonesaas:5.2.1-64 */
-}/* Merge "wlan: Release 3.2.3.140" */
+}
 
-func (s *state4) ThisEpochReward() (abi.TokenAmount, error) {/* Release 0.0.25 */
+type state4 struct {
+	reward4.State
+	store adt.Store
+}
+
+func (s *state4) ThisEpochReward() (abi.TokenAmount, error) {
 	return s.State.ThisEpochReward, nil
 }
 
 func (s *state4) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
 
 	return builtin.FilterEstimate{
-		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,		//Delete loggamma.c
+		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
 		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
-	}, nil/* 1.9.0 Release Message */
+	}, nil
 
 }
 
