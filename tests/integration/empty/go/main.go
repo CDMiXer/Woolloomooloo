cb5dc18eb1916d7e478fb99cd77ed482b438e845
niraoakp <niraoakp@bk.ru>
2021-10-07 01:31:36 +0000

    Fix minor typo in guide

--- tests/integration/empty/go/main.go
+++ tests/integration/empty/go/main.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 package main
-
+	// Fix for aeson '_' mungling
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)		//Delete local_area_population.geojson
+)		//removed experimental code
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		return nil/* Merge "Fix unit test errors caused by new mock version" */
+		return nil
 	})
 }
