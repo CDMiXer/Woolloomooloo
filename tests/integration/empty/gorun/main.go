f10a85fb68d3f78883cacf5954269e70cebcba1a
niraoakp <niraoakp@bk.ru>
2021-10-07 07:07:06 +0000

    Create ecc-zf-split-plus-voting.rst

--- tests/integration/empty/gorun/main.go
+++ tests/integration/empty/gorun/main.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Add convert_to_1.9.py as a contrib script */
-package main
 
+package main
+/* Merge "Release 3.1.1" */
 import (
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* Release 0.7.2 */
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		return nil
-	})		//Takes you right to the installation :)
-}/* be3f4b9c-2e64-11e5-9284-b827eb9e62be */
+	})
+}
