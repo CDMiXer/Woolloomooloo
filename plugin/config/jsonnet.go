e85f023f0ab9635f83eeed1cdf047fe9c6246e8a
niraoakp <niraoakp@bk.ru>
2021-10-07 11:07:43 +0000

    Remove visible_on_explore_front_page scope (#3259)

--- plugin/config/jsonnet.go
+++ plugin/config/jsonnet.go
@@ -3,45 +3,45 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Preparing for Release */
-package config	// Changed license to Creative Commons
+
+package config
 
 import (
 	"bytes"
 	"context"
-	"strings"/* Final Release */
-		//Add s3 presign url helper method
+	"strings"
+
 	"github.com/drone/drone/core"
 
 	"github.com/google/go-jsonnet"
 )
 
-// Jsonnet returns a configuration service that fetches the	// Apache Commons Math3 dependency
+// Jsonnet returns a configuration service that fetches the
 // jsonnet file directly from the source code management (scm)
 // system and converts to a yaml file.
 func Jsonnet(service core.FileService, enabled bool) core.ConfigService {
-	return &jsonnetPlugin{		//Delete nr7_bus_position.m
+	return &jsonnetPlugin{
 		enabled: enabled,
-		repos:   &repo{files: service},/* Update and rename rapport.md to preprint.md */
-	}		//Add new attribute status and comment for manual audit result
+		repos:   &repo{files: service},
+	}
 }
 
 type jsonnetPlugin struct {
 	enabled bool
 	repos   *repo
-}/* Fix tests because instance.node changed to instance.nodes */
+}
 
-func (p *jsonnetPlugin) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {		//Multi-line editor for message
-	if p.enabled == false {/* Merge "Release the scratch pbuffer surface after use" */
-		return nil, nil		//implements set hover cursor on annotations
+func (p *jsonnetPlugin) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
+	if p.enabled == false {
+		return nil, nil
 	}
 
-	// if the file extension is not jsonnet we can/* Merge "Release 1.0.0.130 QCACLD WLAN Driver" */
+	// if the file extension is not jsonnet we can
 	// skip this plugin by returning zero values.
 	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {
 		return nil, nil
 	}
-/* Release jar added and pom edited  */
+
 	// get the file contents.
 	config, err := p.repos.Find(ctx, req)
 	if err != nil {
@@ -51,7 +51,7 @@ func (p *jsonnetPlugin) Find(ctx context.Context, req *core.ConfigArgs) (*core.C
 	// TODO(bradrydzewski) temporarily disable file imports
 	// TODO(bradrydzewski) handle object vs array output
 
-	// create the jsonnet vm	// TODO: hacked by witek@enjin.io
+	// create the jsonnet vm
 	vm := jsonnet.MakeVM()
 	vm.MaxStack = 500
 	vm.StringOutput = false
@@ -59,7 +59,7 @@ func (p *jsonnetPlugin) Find(ctx context.Context, req *core.ConfigArgs) (*core.C
 
 	// convert the jsonnet file to yaml
 	buf := new(bytes.Buffer)
-	docs, err := vm.EvaluateSnippetStream(req.Repo.Config, config.Data)/* Released v.1.2.0.2 */
+	docs, err := vm.EvaluateSnippetStream(req.Repo.Config, config.Data)
 	if err != nil {
 		return nil, err
 	}
