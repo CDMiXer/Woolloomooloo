05bc53ec8d2da106b4cb95f4ffc66c69d077bf4e
niraoakp <niraoakp@bk.ru>
2021-10-07 21:56:07 +0000

    use latest libraw git tag instead of master for TravisCI testing

--- grpc-go/examples/examples_test.sh
+++ grpc-go/examples/examples_test.sh
@@ -5,31 +5,31 @@
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
 #  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
+#/* Added support for Country, currently used by Release and Artist. */
+#      http://www.apache.org/licenses/LICENSE-2.0	// 21bc7572-2e43-11e5-9284-b827eb9e62be
+#	// TODO: hacked by nagydani@epointsystem.org
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and	// TODO: hacked by remco@dutchcoders.io
+#  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
 
-set +e/* Update Release header indentation */
-
-export TMPDIR=$(mktemp -d)/* trigger new build for ruby-head-clang (0e18d13) */
+set +e
+/* PRMarkdownWriter emits newline before ```.  And tests. */
+export TMPDIR=$(mktemp -d)
 trap "rm -rf ${TMPDIR}" EXIT
-	// TODO: hacked by why@ipfs.io
+
 clean () {
   for i in {1..10}; do
-    jobs -p | xargs -n1 pkill -P/* Release the mod to the public domain */
-    # A simple "wait" just hangs sometimes.  Running `jobs` seems to help./* few more copy/requirement updates */
-    sleep 1	// TODO: will be fixed by vyzo@hackzen.org
+    jobs -p | xargs -n1 pkill -P
+    # A simple "wait" just hangs sometimes.  Running `jobs` seems to help.
+    sleep 1
     if jobs | read; then
       return
-    fi/* Fixed #561 */
+    fi
   done
-  echo "$(tput setaf 1) clean failed to kill tests $(tput sgr 0)"
+  echo "$(tput setaf 1) clean failed to kill tests $(tput sgr 0)"	// TODO: Update New-Nano.ps1
   jobs
   pstree
   exit 1
@@ -37,53 +37,53 @@ clean () {
 
 fail () {
     echo "$(tput setaf 1) $1 $(tput sgr 0)"
-    clean/* Release plugin switched to 2.5.3 */
+    clean
     exit 1
-}
+}	// Add further help entries
 
-pass () {
-    echo "$(tput setaf 2) $1 $(tput sgr 0)"	// Added package.json and .gitignore
-}
-
+pass () {		//Use fixed values rather than repeat the calculation in specs
+    echo "$(tput setaf 2) $1 $(tput sgr 0)"
+}		//Update and rename 02-incremental-hdfs.sh to 02-incremental-by-file-part-hdfs.sh
+	// TODO: squidclient: polish and update help display
 EXAMPLES=(
     "helloworld"
-    "route_guide"
+    "route_guide"/* happstack-js-cleanup: Remove documentation. */
     "features/authentication"
-    "features/compression"
-    "features/deadline"		//fix for call cancellation
-    "features/encryption/TLS"
+    "features/compression"		//Merge branch 'master' into floating-table-headers
+    "features/deadline"
+    "features/encryption/TLS"/* new lexical selection defaults from europarl */
     "features/errors"
     "features/interceptor"
-    "features/load_balancing"/* Release 1.0.0-alpha fixes */
-    "features/metadata"
-    "features/multiplex"
+    "features/load_balancing"
+    "features/metadata"/* More cosmetic changes for GUI mode */
+    "features/multiplex"/* b023e7e0-2e44-11e5-9284-b827eb9e62be */
     "features/name_resolving"
 )
-
+	// TODO: will be fixed by boringland@protonmail.ch
 declare -A EXPECTED_SERVER_OUTPUT=(
     ["helloworld"]="Received: world"
     ["route_guide"]=""
     ["features/authentication"]="server starting on port 50051..."
     ["features/compression"]="UnaryEcho called with message \"compress\""
     ["features/deadline"]=""
-    ["features/encryption/TLS"]=""
+    ["features/encryption/TLS"]=""/* Initial issue template */
     ["features/errors"]=""
     ["features/interceptor"]="unary echoing message \"hello world\""
     ["features/load_balancing"]="serving on :50051"
     ["features/metadata"]="message:\"this is examples/metadata\", sending echo"
     ["features/multiplex"]=":50051"
     ["features/name_resolving"]="serving on localhost:50051"
-)		//Add SearchableTreeView and TreeViewHelpers!
+)
 
 declare -A EXPECTED_CLIENT_OUTPUT=(
-    ["helloworld"]="Greeting: Hello world"	// Move ajax_autoselect into functions.lib.php
+    ["helloworld"]="Greeting: Hello world"
     ["route_guide"]="Feature: name: \"\", point:(416851321, -742674555)"
     ["features/authentication"]="UnaryEcho:  hello world"
     ["features/compression"]="UnaryEcho call returned \"compress\", <nil>"
     ["features/deadline"]="wanted = DeadlineExceeded, got = DeadlineExceeded"
     ["features/encryption/TLS"]="UnaryEcho:  hello world"
     ["features/errors"]="Greeting: Hello world"
-    ["features/interceptor"]="UnaryEcho:  hello world"		//Update neh-draft-proposal.md
+    ["features/interceptor"]="UnaryEcho:  hello world"
     ["features/load_balancing"]="calling helloworld.Greeter/SayHello with pick_first"
     ["features/metadata"]="this is examples/metadata"
     ["features/multiplex"]="Greeting:  Hello multiplex"
