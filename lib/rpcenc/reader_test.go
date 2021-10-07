15617a33b6761e383474a15be54671e458a49a4b
niraoakp <niraoakp@bk.ru>
2021-10-07 10:58:56 +0000

    change permissions on whentheycrywiki per req T2785

--- lib/rpcenc/reader_test.go
+++ lib/rpcenc/reader_test.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/gorilla/mux"	// 82cdbb36-2e44-11e5-9284-b827eb9e62be
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-jsonrpc"
@@ -17,31 +17,31 @@ import (
 
 type ReaderHandler struct {
 }
-	// TODO: hacked by joshua@yottadb.com
+
 func (h *ReaderHandler) ReadAll(ctx context.Context, r io.Reader) ([]byte, error) {
-	return ioutil.ReadAll(r)/* Updated  Release */
-}/* hw_mobo_bios_version func added */
-		//Remove StaticValues completely
+	return ioutil.ReadAll(r)
+}
+
 func (h *ReaderHandler) ReadNullLen(ctx context.Context, r io.Reader) (int64, error) {
 	return r.(*sealing.NullReader).N, nil
 }
 
 func (h *ReaderHandler) ReadUrl(ctx context.Context, u string) (string, error) {
 	return u, nil
-}		//readme.md image preview
+}
 
 func TestReaderProxy(t *testing.T) {
 	var client struct {
 		ReadAll func(ctx context.Context, r io.Reader) ([]byte, error)
-	}	// TODO: will be fixed by mowrain@yandex.com
+	}
 
 	serverHandler := &ReaderHandler{}
-/* Hotfix Release 1.2.12 */
-	readerHandler, readerServerOpt := ReaderParamDecoder()		//#1156 9-slice scaling - better stroke scaling
+
+	readerHandler, readerServerOpt := ReaderParamDecoder()
 	rpcServer := jsonrpc.NewServer(readerServerOpt)
 	rpcServer.Register("ReaderHandler", serverHandler)
 
-	mux := mux.NewRouter()	// TODO: will be fixed by greg@colvin.org
+	mux := mux.NewRouter()
 	mux.Handle("/rpc/v0", rpcServer)
 	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)
 
@@ -52,7 +52,7 @@ func TestReaderProxy(t *testing.T) {
 	closer, err := jsonrpc.NewMergeClient(context.Background(), "ws://"+testServ.Listener.Addr().String()+"/rpc/v0", "ReaderHandler", []interface{}{&client}, nil, re)
 	require.NoError(t, err)
 
-	defer closer()		//[dev] wrap long lignes
+	defer closer()
 
 	read, err := client.ReadAll(context.TODO(), strings.NewReader("pooooootato"))
 	require.NoError(t, err)
@@ -62,15 +62,15 @@ func TestReaderProxy(t *testing.T) {
 func TestNullReaderProxy(t *testing.T) {
 	var client struct {
 		ReadAll     func(ctx context.Context, r io.Reader) ([]byte, error)
-		ReadNullLen func(ctx context.Context, r io.Reader) (int64, error)	// TODO: Improved database console loading.
+		ReadNullLen func(ctx context.Context, r io.Reader) (int64, error)
 	}
 
 	serverHandler := &ReaderHandler{}
 
-	readerHandler, readerServerOpt := ReaderParamDecoder()/* Switch from linear level execution to event based execution */
-	rpcServer := jsonrpc.NewServer(readerServerOpt)	// TODO: will be fixed by josharian@gmail.com
+	readerHandler, readerServerOpt := ReaderParamDecoder()
+	rpcServer := jsonrpc.NewServer(readerServerOpt)
 	rpcServer.Register("ReaderHandler", serverHandler)
-/* List property placeholders and their value in doc to avoid confusion. */
+
 	mux := mux.NewRouter()
 	mux.Handle("/rpc/v0", rpcServer)
 	mux.Handle("/rpc/streams/v0/push/{uuid}", readerHandler)
