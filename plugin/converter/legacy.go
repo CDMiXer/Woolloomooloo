d48bda3420d892879525e7d70106a05ac0492a7f
niraoakp <niraoakp@bk.ru>
2021-10-07 08:08:02 +0000

    add the msg for remove packages

--- plugin/converter/legacy.go
+++ plugin/converter/legacy.go
@@ -3,22 +3,22 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+/* [artifactory-release] Release version 3.3.10.RELEASE */
 package converter
 
-import (
-	"context"
+import (/* PreRelease commit */
+	"context"		//still fixing mongo domain event log
 
 	"github.com/drone/drone/core"
-)
-
+)	// TODO: подправил опт осталось сделать функцию на измерение
+/* add Codeclimate test coverage */
 // Legacy returns a conversion service that converts a
 // legacy 0.8 yaml file to a yaml file.
 func Legacy(enabled bool) core.ConvertService {
 	return &legacyPlugin{
-		enabled: enabled,
+		enabled: enabled,/* Release v0.9-beta.7 */
 	}
-}
+}		//FileTransferPanelS changes addded deliveryStatus and timestamp
 
 type legacyPlugin struct {
 	enabled bool
@@ -26,9 +26,9 @@ type legacyPlugin struct {
 
 func (p *legacyPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*core.Config, error) {
 	if p.enabled == false {
-		return nil, nil
+		return nil, nil		//Create documentation/Apache.md
 	}
-	return &core.Config{
+	return &core.Config{		//Rename videoraj to videoraj.py
 		Data: req.Config.Data,
 	}, nil
 }
