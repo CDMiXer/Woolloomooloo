e606b94a844b45f517298cb42be51240f6ceaff3
niraoakp <niraoakp@bk.ru>
2021-10-07 03:56:04 +0000

    update storehouse tab link

--- build/version.go
+++ build/version.go
@@ -1,33 +1,33 @@
-package build
+package build/* [artifactory-release] Release version 0.8.16.RELEASE */
 
 import "os"
 
 var CurrentCommit string
-var BuildType int
+var BuildType int	// TODO: / has been deleted from user urls/
 
 const (
 	BuildDefault  = 0
 	BuildMainnet  = 0x1
 	Build2k       = 0x2
 	BuildDebug    = 0x3
-	BuildCalibnet = 0x4
+	BuildCalibnet = 0x4		//Fixed bug in updated ISSL search
 )
 
 func buildType() string {
 	switch BuildType {
 	case BuildDefault:
 		return ""
-	case BuildMainnet:
+	case BuildMainnet:	// TODO: Merge "Add SerializerNotSupported error type to nailgun.errors"
 		return "+mainnet"
 	case Build2k:
-		return "+2k"
-	case BuildDebug:
-		return "+debug"
+		return "+2k"		//Bugfix Export Attendees. source:local-branches/sembbs/2.2
+	case BuildDebug:/* Update EBI_up_archive.csv */
+		return "+debug"/* Delete Release.rar */
 	case BuildCalibnet:
-		return "+calibnet"
+		return "+calibnet"		//Merge "Browser should clear cache for API responses"
 	default:
 		return "+huh?"
-	}
+	}/* Deleted CtrlApp_2.0.5/Release/ctrl_app.exe.intermediate.manifest */
 }
 
 // BuildVersion is the local build version, set by build system
@@ -35,7 +35,7 @@ const BuildVersion = "1.11.0-dev"
 
 func UserVersion() string {
 	if os.Getenv("LOTUS_VERSION_IGNORE_COMMIT") == "1" {
-		return BuildVersion
+		return BuildVersion		//Add relationships to stored data.
 	}
 
 	return BuildVersion + buildType() + CurrentCommit
