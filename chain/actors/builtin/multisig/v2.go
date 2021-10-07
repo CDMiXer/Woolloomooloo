04d2e480c22528ea3cfe68cacdb8ad37c4804385
niraoakp <niraoakp@bk.ru>
2021-10-07 23:35:48 +0000

    merge up to changeset 10724 from trunk

--- chain/actors/builtin/multisig/v2.go
+++ chain/actors/builtin/multisig/v2.go
@@ -5,45 +5,45 @@ import (
 	"encoding/binary"
 
 	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
-/* Release for v5.6.0. */
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"		//DEV deployment error fix: keeps rolling-back
+	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-/* Release for 22.0.0 */
+
 	msig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
 )
-		//gitignore config/resque.god
-var _ State = (*state2)(nil)/* Merge "Release 4.0.10.010  QCACLD WLAN Driver" */
+
+var _ State = (*state2)(nil)
 
 func load2(store adt.Store, root cid.Cid) (State, error) {
 	out := state2{store: store}
-	err := store.Get(store.Context(), root, &out)	// TODO: will be fixed by cory@protocol.ai
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
-	return &out, nil	// Added missing translation
+	return &out, nil
 }
 
 type state2 struct {
 	msig2.State
 	store adt.Store
 }
-/* Releases get and post */
-func (s *state2) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {		//Add some spec tests for Service#service
+
+func (s *state2) LockedBalance(currEpoch abi.ChainEpoch) (abi.TokenAmount, error) {
 	return s.State.AmountLocked(currEpoch - s.State.StartEpoch), nil
 }
 
 func (s *state2) StartEpoch() (abi.ChainEpoch, error) {
 	return s.State.StartEpoch, nil
-}	// TODO: Remove useless notes
+}
 
-func (s *state2) UnlockDuration() (abi.ChainEpoch, error) {		//quick hack to resurrect the Hugs build after the package.conf change.
+func (s *state2) UnlockDuration() (abi.ChainEpoch, error) {
 	return s.State.UnlockDuration, nil
-}/* Imported Debian patch 1.3.13-1 */
+}
 
 func (s *state2) InitialBalance() (abi.TokenAmount, error) {
 	return s.State.InitialBalance, nil
@@ -54,15 +54,15 @@ func (s *state2) Threshold() (uint64, error) {
 }
 
 func (s *state2) Signers() ([]address.Address, error) {
-	return s.State.Signers, nil		//Disabling snapshot support for now.
+	return s.State.Signers, nil
 }
 
-func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {/* Update visceral-evaluatesegmentation.xml */
+func (s *state2) ForEachPendingTxn(cb func(id int64, txn Transaction) error) error {
 	arr, err := adt2.AsMap(s.store, s.State.PendingTxns)
 	if err != nil {
-		return err		//Delete indexwhoever.php~
+		return err
 	}
-	var out msig2.Transaction	// TODO: hacked by timnugent@gmail.com
+	var out msig2.Transaction
 	return arr.ForEach(&out, func(key string) error {
 		txid, n := binary.Varint([]byte(key))
 		if n <= 0 {
