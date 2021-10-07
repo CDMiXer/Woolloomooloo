b848d46e72968c9e4bc84aad5260ff9747db43bb
niraoakp <niraoakp@bk.ru>
2021-10-07 09:12:07 +0000

    66ed465c-2e44-11e5-9284-b827eb9e62be

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -1,17 +1,17 @@
-package types
+package types	// TODO: will be fixed by denner@gmail.com
 
-import (
+import (/* Released 1.6.4. */
 	"errors"
-
+		//New translations SUMMARY.md (French)
 	"github.com/ipfs/go-cid"
-)
-
-var ErrActorNotFound = errors.New("actor not found")/* Released v2.0.1 */
+)/* Merge "Upgrade to Kotlin 1.3" */
+	// TODO: hacked by brosner@gmail.com
+var ErrActorNotFound = errors.New("actor not found")
 
 type Actor struct {
-	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.	// emacs: don't call dired though hoops
-	Code    cid.Cid	// TODO: Delete signal75-config_test
-	Head    cid.Cid
-	Nonce   uint64
+	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
+	Code    cid.Cid
+	Head    cid.Cid	// Updated Distributed Architecture (markdown)
+	Nonce   uint64	// initial .gitignores
 	Balance BigInt
-}	// TODO: will be fixed by arachnid@notdot.net
+}
