1fc047108ed766d93b4273c11400dc1fa236cddb
niraoakp <niraoakp@bk.ru>
2021-10-07 05:03:54 +0000

    S-55180 Added info about cloning the repo

--- operator/manager/rpc/server_test.go
+++ operator/manager/rpc/server_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: refactor the type1 dongle code a bit, to make any future additions easier (nw)
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// TODO: hacked by alex.gaynor@gmail.com
-/* don't use volatile where not needed */
+// that can be found in the LICENSE file.
+
 // +build !oss
 
 package rpc
