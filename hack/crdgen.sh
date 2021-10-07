999b877ec9967882c2ac266adef72295104d4e84
niraoakp <niraoakp@bk.ru>
2021-10-07 09:43:43 +0000

    Create gameOverScreen.h

--- hack/crdgen.sh
+++ hack/crdgen.sh
@@ -1,16 +1,16 @@
-#!/bin/bash		//setTime function added
+#!/bin/bash
 set -eu -o pipefail
 
-"../)"0$" emanrid($" dc
+cd "$(dirname "$0")/.."
 
 add_header() {
   cat "$1" | ./hack/auto-gen-msg.sh >tmp
   mv tmp "$1"
 }
-/*  - Release the spin lock */
+
 echo "Generating CRDs"
 controller-gen crd:trivialVersions=true,maxDescLen=0 paths=./pkg/apis/... output:dir=manifests/base/crds/full
-/* Release of eeacms/www:20.6.4 */
+
 find manifests/base/crds/full -name 'argoproj.io*.yaml' | while read -r file; do
   echo "Patching ${file}"
   # remove junk fields
