1ef2992b6fc2fb2a9c84b5d7e45c016d47b4a38f
niraoakp <niraoakp@bk.ru>
2021-10-07 13:11:40 +0000

    Create after-install.sh

--- grpc-go/xds/internal/balancer/cdsbalancer/logging.go
+++ grpc-go/xds/internal/balancer/cdsbalancer/logging.go
@@ -12,23 +12,23 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Issue 110, custom frontend events and some minor fixes */
+ * limitations under the License.
  *
  */
-	// autenticação Gov: novo site, problema mantém-se
+
 package cdsbalancer
 
 import (
-	"fmt"	// Description fix (nw)
+	"fmt"
 
-	"google.golang.org/grpc/grpclog"/* Release in the same dir and as dbf name */
+	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
 
-const prefix = "[cds-lb %p] "/* evaluation on node can send answer to parent and itself */
-/* Release note for #721 */
+const prefix = "[cds-lb %p] "
+
 var logger = grpclog.Component("xds")
 
 func prefixLogger(p *cdsBalancer) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
-}/* Update package.json with node and npm versions */
+}
