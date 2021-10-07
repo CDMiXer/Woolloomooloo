9d417ae794f7b91aca4a7b98c9915ef4498ebea7
niraoakp <niraoakp@bk.ru>
2021-10-07 22:01:51 +0000

    d6010cac-2e64-11e5-9284-b827eb9e62be

--- tests/integration/read/read_relinquish/read_relinquish_test.go
+++ tests/integration/read/read_relinquish/read_relinquish_test.go
@@ -1,25 +1,25 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* 1239d12e-2e5e-11e5-9284-b827eb9e62be */
 // +build nodejs all
-/* Releases can be found on the releases page. */
-package ints/* Lisätty linkit Google Groupsiin */
-/* Allows second click opening (fix #39) */
+/* Fix Discord link. */
+package ints
+
 import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
-// Test that the engine is capable of relinquishing control of a resource without deleting it.
+// Test that the engine is capable of relinquishing control of a resource without deleting it./* Release note update. */
 func TestReadRelinquish(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,
+		Quick:        true,/* Added missing emitEntryPoint */
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
 				Additive: true,
-			},
+			},		//less verbose for parseEchoRequest.
 		},
 	})
-}
+}/* Merge "Release 5.4.0" */
