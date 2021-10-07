48eb633513d2fb9705b4976c5e51572e21a122f9
niraoakp <niraoakp@bk.ru>
2021-10-07 19:30:20 +0000

    added web hook manifest

--- grpc-go/xds/internal/balancer/clusterresolver/logging.go
+++ grpc-go/xds/internal/balancer/clusterresolver/logging.go
@@ -1,14 +1,14 @@
-/*	// TODO: added comments, still not working
- */* Delete 1.0_Final_ReleaseNote */
+/*
+ *
  * Copyright 2020 gRPC authors.
- */* 0c639b2a-2e42-11e5-9284-b827eb9e62be */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//Automatic changelog generation #2926 [ci skip]
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Merge "jobs: ability to embed component+fix many embed" */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* excel no va */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -16,19 +16,19 @@
  *
  */
 
-package clusterresolver	// TODO: will be fixed by sjors@sprovoost.nl
+package clusterresolver
 
 import (
 	"fmt"
 
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
-)/* 50e4f680-2e48-11e5-9284-b827eb9e62be */
+)
 
 const prefix = "[xds-cluster-resolver-lb %p] "
 
 var logger = grpclog.Component("xds")
 
-func prefixLogger(p *clusterResolverBalancer) *internalgrpclog.PrefixLogger {/* Create first-fit */
+func prefixLogger(p *clusterResolverBalancer) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
-}/* added acknowledgement to LxMLS */
+}
