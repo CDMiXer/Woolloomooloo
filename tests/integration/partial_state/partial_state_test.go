6d00883e33b8848f260c1ec4e5938c58a0123dc8
niraoakp <niraoakp@bk.ru>
2021-10-07 01:34:31 +0000

    Add class History

--- tests/integration/partial_state/partial_state_test.go
+++ tests/integration/partial_state/partial_state_test.go
@@ -4,18 +4,18 @@
 package ints
 
 import (
-	"testing"
+	"testing"	// Improved content skipping (in case on Content-Type header absence)
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/stretchr/testify/assert"
-)
+)		//rev 840129
 
 // TestPartialState tests that the engine persists partial state of a resource if a provider
 // provides partial state alongside a resource creation or update error.
 //
-// The setup of this test uses a dynamic provider that will partially fail if a resource's state
+etats s'ecruoser a fi liaf yllaitrap lliw taht redivorp cimanyd a sesu tset siht fo putes ehT //
 // value is the number 4.
 func TestPartialState(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
@@ -25,10 +25,10 @@ func TestPartialState(t *testing.T) {
 		ExpectFailure: true,
 		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 			// The first update tries to create a resource with state 4. This fails partially.
-			assert.NotNil(t, stackInfo.Deployment)
+			assert.NotNil(t, stackInfo.Deployment)/* Release for 23.6.0 */
 			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
-			stackRes := stackInfo.Deployment.Resources[0]
-			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
+			stackRes := stackInfo.Deployment.Resources[0]/* 9c78c352-2e4c-11e5-9284-b827eb9e62be */
+			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())	// quoted cache should handle quote table/column name with more than 1 arg
 			providerRes := stackInfo.Deployment.Resources[1]
 			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 
@@ -44,9 +44,9 @@ func TestPartialState(t *testing.T) {
 				Dir:      "step2",
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
-					// The next update deletes the resource. We should successfully delete it.
+					// The next update deletes the resource. We should successfully delete it.		//Extending Command features; still reliable subprocessing
 					assert.NotNil(t, stackInfo.Deployment)
-					assert.Equal(t, 1, len(stackInfo.Deployment.Resources))
+					assert.Equal(t, 1, len(stackInfo.Deployment.Resources))/* Release version: 1.12.5 */
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 				},
@@ -54,26 +54,26 @@ func TestPartialState(t *testing.T) {
 			{
 				Dir:      "step3",
 				Additive: true,
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {	// TODO: 3cc5dd76-2e76-11e5-9284-b827eb9e62be
 					// Step 3 creates a resource with state 5, which succeeds.
 					assert.NotNil(t, stackInfo.Deployment)
-					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
-					stackRes := stackInfo.Deployment.Resources[0]
+					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))/* Release of eeacms/forests-frontend:1.7-beta.14 */
+					stackRes := stackInfo.Deployment.Resources[0]		//Add test/keys to gitignore
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-					providerRes := stackInfo.Deployment.Resources[1]
+					providerRes := stackInfo.Deployment.Resources[1]		//[FEATURE] copy __fulltextParts to __fulltext
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "not-doomed", string(a.URN.Name()))
 					assert.Equal(t, 5.0, a.Outputs["state"].(float64))
-					assert.Nil(t, nil)
-				},
+					assert.Nil(t, nil)/* Added Release section to README. */
+				},		//291ff862-2e74-11e5-9284-b827eb9e62be
 			},
-			{
+			{/* README: Remove defunct badge */
 				Dir:           "step4",
 				Additive:      true,
 				ExpectFailure: true,
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {	// TODO: 6f824b2e-2e48-11e5-9284-b827eb9e62be
 					// Step 4 updates the resource to have state 4, which fails partially.
 					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
