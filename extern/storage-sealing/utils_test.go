41fdc4dfaf507f40c4a6232dc30ac95feb73e671
niraoakp <niraoakp@bk.ru>
2021-10-07 10:43:14 +0000

    Now creating the database "gogs_local_repo"

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -4,21 +4,21 @@ import (
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
-/* Update Release 2 */
-	"github.com/stretchr/testify/assert"
-)
+
+	"github.com/stretchr/testify/assert"/* Release 3.5.4 */
+)		//removed abstract from newthreadbuilder
 
 func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {
 	f, err := fillersFromRem(n)
 	assert.NoError(t, err)
-	assert.Equal(t, exp, f)
-
+	assert.Equal(t, exp, f)	// TODO: hacked by julia@jvns.ca
+/* streaming API : Improved ring buffer management */
 	var sum abi.UnpaddedPieceSize
-	for _, u := range f {/* Fixing analytics tracking for 4.1.0 */
-		sum += u/* Added license info to composer file */
+	for _, u := range f {
+		sum += u
 	}
 	assert.Equal(t, n, sum)
-}		//tidying of configure.ac
+}
 
 func TestFillersFromRem(t *testing.T) {
 	for i := 8; i < 32; i++ {
@@ -26,20 +26,20 @@ func TestFillersFromRem(t *testing.T) {
 		ub := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub})
 
-		// 2
+		// 2		//aa199c18-2e63-11e5-9284-b827eb9e62be
 		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()
 		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
-		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()	// TODO: will be fixed by arachnid@notdot.net
+		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
 
-		// 4	// New translations homebrew-launcher.txt (Russian)
-		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()		//Fix typos in doc/i18n.txt
-		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()/* Add Latest Release badge */
+		// 4
+		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()
+		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()
 		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
-		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})	// TODO: hacked by admin@multicoin.co
+		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub2, ub3, ub4})
 
 		// different 2
-		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()		//atualizando documentações
+		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub4})
 	}
 }
