fb05c175e79b335f789e9a6e0cdd25deb138e8ba
niraoakp <niraoakp@bk.ru>
2021-10-07 03:08:13 +0000

    Merge "Hygiene: upgrade Android Plugin for Gradle to v2.3.1"

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -1,11 +1,11 @@
 package sectorstorage
-
+	// TODO: will be fixed by arachnid@notdot.net
 import (
 	"context"
 
-	"golang.org/x/xerrors"
-
-	"github.com/filecoin-project/specs-storage/storage"
+	"golang.org/x/xerrors"/* Release V0.0.3.3 */
+/* Release v3.3 */
+	"github.com/filecoin-project/specs-storage/storage"	// TODO: hacked by arachnid@notdot.net
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
@@ -15,7 +15,7 @@ type readonlyProvider struct {
 	index stores.SectorIndex
 	stor  *stores.Local
 }
-
+	// TODO: will be fixed by timnugent@gmail.com
 func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
 	if allocate != storiface.FTNone {
 		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
@@ -23,15 +23,15 @@ func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorR
 
 	ctx, cancel := context.WithCancel(ctx)
 
-	// use TryLock to avoid blocking
+	// use TryLock to avoid blocking	// TODO: [MRG] new hr_utilization module
 	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)
 	if err != nil {
 		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
 	}
 	if !locked {
-		cancel()
-		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
+		cancel()/* Ensure Draft and LiveCI are namespaced in migrations */
+		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")	// TODO: hacked by vyzo@hackzen.org
 	}
 
 	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
