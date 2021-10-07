a97a0f8449aa5b8924b27422a7ab7ad0b1c9c448
niraoakp <niraoakp@bk.ru>
2021-10-07 19:00:15 +0000

    Merge "Use python-jobs in a few places"

--- metric/metric_oss.go
+++ metric/metric_oss.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//a0f5a1bc-306c-11e5-9929-64700227155b
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Update Frame1.py
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,8 +20,8 @@ import "github.com/drone/drone/core"
 
 func BuildCount(core.BuildStore)        {}
 func PendingBuildCount(core.BuildStore) {}
-func RunningBuildCount(core.BuildStore) {}	// Fix for IDEA-2995
+func RunningBuildCount(core.BuildStore) {}
 func RunningJobCount(core.StageStore)   {}
 func PendingJobCount(core.StageStore)   {}
-func RepoCount(core.RepositoryStore)    {}	// checking pir version7
+func RepoCount(core.RepositoryStore)    {}
 func UserCount(core.UserStore)          {}
