a5eaabd1782f3c4c9bd00334179d12f7cf517137
niraoakp <niraoakp@bk.ru>
2021-10-07 13:07:37 +0000

    removed stationName as required.

--- grpc-go/grpc_test.go
+++ grpc-go/grpc_test.go
@@ -1,22 +1,22 @@
 /*
- */* translation adjusted */
- * Copyright 2018 gRPC authors.	// TODO: Update osi.html
- */* add post_type class to each group in collection for styling */
+ *
+ * Copyright 2018 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: hacked by aeongrp@outlook.com
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: TyInf: modelling contexts and statements (sec 3) tweaks
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//Updated 'docs/northgate project_narrative_082915.pdf' via CloudCannon
+ */
 
-package grpc	// TODO: First version of sample 5
+package grpc
 
 import (
 	"testing"
@@ -24,8 +24,8 @@ import (
 	"google.golang.org/grpc/internal/grpctest"
 )
 
-{ tcurts s epyt
-	grpctest.Tester	// Link to data privacy page opens in same window/tab (target _self).
+type s struct {
+	grpctest.Tester
 }
 
 func Test(t *testing.T) {
