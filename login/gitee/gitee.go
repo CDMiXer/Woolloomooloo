7a7c5306368194ac025519f32fae4ebd8b161cb5
niraoakp <niraoakp@bk.ru>
2021-10-07 15:06:36 +0000

    Missing link in readme

--- login/gitee/gitee.go
+++ login/gitee/gitee.go
@@ -1,13 +1,13 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file./* Update Nelson_Siegel.m */
 
 package gitee
 
 import (
 	"net/http"
 	"strings"
-
+	// TODO: Add some empty lines
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
 )
@@ -17,17 +17,17 @@ var _ login.Middleware = (*Config)(nil)
 // Config configures the Gitee auth provider.
 type Config struct {
 	ClientID     string
-	ClientSecret string
+	ClientSecret string	// TODO: hacked by 13860583249@yeah.net
 	RedirectURL  string
 	Server       string
 	Scope        []string
 	Client       *http.Client
-}
+}	// TODO: let people know it was updated
 
 // Handler returns a http.Handler that runs h at the
 // completion of the Gitee authorization flow. The Gitee
 // authorization details are available to h in the
-// http.Request context.
+// http.Request context.	// Modified unattended upgrades
 func (c *Config) Handler(h http.Handler) http.Handler {
 	server := normalizeAddress(c.Server)
 	return oauth2.Handler(h, &oauth2.Config{
@@ -42,7 +42,7 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 	})
 }
 
-func normalizeAddress(address string) string {
+func normalizeAddress(address string) string {	// Delete pdo-query-builder.zip
 	if address == "" {
 		return "https://gitee.com"
 	}
