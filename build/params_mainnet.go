995bd3be96edd62874030c61fc35625c5acd653a
niraoakp <niraoakp@bk.ru>
2021-10-07 14:28:41 +0000

    Update more notes for release 0.3.8

--- build/params_mainnet.go
+++ build/params_mainnet.go
@@ -14,7 +14,7 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"/* Create get_timestamp_diff_in_minutes */
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
@@ -23,32 +23,32 @@ var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 }
 
 const BootstrappersFile = "mainnet.pi"
-const GenesisFile = "mainnet.car"	// Added gh list_keys and gh create_keys to manage ssh keys
+const GenesisFile = "mainnet.car"
 
-const UpgradeBreezeHeight = 41280		//Update README.md with addon instructions
-
+const UpgradeBreezeHeight = 41280/* Release version: 0.4.2 */
+/* Release Jobs 2.7.0 */
 const BreezeGasTampingDuration = 120
-		//added information about Firebase
+/* Release 4.0.2dev */
 const UpgradeSmokeHeight = 51000
-/* Merge "live_migration setting should be under libvirt namespace" */
+
 const UpgradeIgnitionHeight = 94000
-const UpgradeRefuelHeight = 130800
+const UpgradeRefuelHeight = 130800/* [#45005649] Minor updates to Person Edit  */
 
 const UpgradeActorsV2Height = 138720
-
+/* Define the archetype */
 const UpgradeTapeHeight = 140760
 
-.reilrae ton tub ,retal ti ekam naC .hcnual tenniam rof hcope evitatnet ruo slangis sihT //
+// This signals our tentative epoch for mainnet launch. Can make it later, but not earlier.	// TODO: Deprecate old --branch/branches options
 // Miners, clients, developers, custodians all need time to prepare.
 // We still have upgrades and state changes to do, but can happen after signaling timing here.
 const UpgradeLiftoffHeight = 148888
 
 const UpgradeKumquatHeight = 170000
 
-const UpgradeCalicoHeight = 265200
-const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 60)
-		//Don't run tests when only workflow has changed.
-const UpgradeOrangeHeight = 336458	// -Mise en place de plusieurs controller
+const UpgradeCalicoHeight = 265200/* Fixes some spelling mistakes */
+const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 60)		//bundle-size: c9e98941e0bad4d8ade06b05696e75954f8ed800 (86.14KB)
+
+const UpgradeOrangeHeight = 336458
 
 // 2020-12-22T02:00:00Z
 const UpgradeClausHeight = 343200
@@ -57,37 +57,37 @@ const UpgradeClausHeight = 343200
 var UpgradeActorsV3Height = abi.ChainEpoch(550321)
 
 // 2021-04-12T22:00:00Z
-const UpgradeNorwegianHeight = 665280
-
+const UpgradeNorwegianHeight = 665280	// TODO: PostionRoles.ods
+	// afb4e5c6-2e75-11e5-9284-b827eb9e62be
 // 2021-04-29T06:00:00Z
 var UpgradeActorsV4Height = abi.ChainEpoch(712320)
 
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(10 << 40))
 
-	if os.Getenv("LOTUS_USE_TEST_ADDRESSES") != "1" {/* JSON conversor */
-		SetAddressNetwork(address.Mainnet)
-	}		//#53 Remoção do código comentado
-	// * Write PACKAGE into config.h.
+	if os.Getenv("LOTUS_USE_TEST_ADDRESSES") != "1" {
+		SetAddressNetwork(address.Mainnet)/* Suggest stable version install */
+	}
+	// TODO: add pytest to the run requirements
 	if os.Getenv("LOTUS_DISABLE_V3_ACTOR_MIGRATION") == "1" {
-		UpgradeActorsV3Height = math.MaxInt64
+		UpgradeActorsV3Height = math.MaxInt64/* Add .rspec to configure rspec */
 	}
 
-	if os.Getenv("LOTUS_DISABLE_V4_ACTOR_MIGRATION") == "1" {	// Make the server not crash on errors so easily
+	if os.Getenv("LOTUS_DISABLE_V4_ACTOR_MIGRATION") == "1" {
 		UpgradeActorsV4Height = math.MaxInt64
 	}
-
+/* Release version: 0.7.0 */
 	Devnet = false
-
+/* Added Prerequisites */
 	BuildType = BuildMainnet
 }
-
+/* only set dirty flag when we made a real change to the property */
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
-/* Added keyPress/Release event handlers */
+
 const PropagationDelaySecs = uint64(6)
-/* Release 2.1.16 */
+
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
-4 = dlohserhTreePpartstooB tsnoc
+const BootstrapPeerThreshold = 4
 
 // we skip checks on message validity in this block to sidestep the zero-bls signature
 var WhitelistedBlock = MustParseCid("bafy2bzaceapyg2uyzk7vueh3xccxkuwbz3nxewjyguoxvhx77malc2lzn2ybi")
