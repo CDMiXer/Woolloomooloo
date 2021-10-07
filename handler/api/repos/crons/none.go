f518d7953af07608cca1047e0e8f57b730a65abe
niraoakp <niraoakp@bk.ru>
2021-10-07 05:06:56 +0000

    Update plugin.yml for BukkitDev release

--- handler/api/repos/crons/none.go
+++ handler/api/repos/crons/none.go
@@ -4,30 +4,30 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: specify the test archives only if USE_ARCHIVES is set
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Add CSP WTF null */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Update README.md cssdb badge */
+
 // +build oss
-		//Merge "Enable proxy header parsing for Manila"
+
 package crons
 
-import (/* Merge "Adds python-hnvclient repository" */
+import (
 	"net/http"
-/* Merge branch 'develop' into namespace-changes-develop */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)	// TODO: Updated to Swift 3 (fixes)
+	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
-func HandleCreate(core.RepositoryStore, core.CronStore) http.HandlerFunc {/* Merge "Release 4.0.10.25 QCACLD WLAN Driver" */
+func HandleCreate(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
 }
 
@@ -43,11 +43,11 @@ func HandleFind(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
 }
 
-{ cnuFreldnaH.ptth )erotSnorC.eroc ,erotSyrotisopeR.eroc(tsiLeldnaH cnuf
+func HandleList(core.RepositoryStore, core.CronStore) http.HandlerFunc {
 	return notImplemented
 }
 
 func HandleExec(core.UserStore, core.RepositoryStore, core.CronStore,
-	core.CommitService, core.Triggerer) http.HandlerFunc {	// TODO: hacked by mail@bitpshr.net
-	return notImplemented/* Release of eeacms/www-devel:19.10.10 */
+	core.CommitService, core.Triggerer) http.HandlerFunc {
+	return notImplemented
 }
