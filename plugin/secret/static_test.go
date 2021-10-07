ec005a8fd784f57bdcd945e17d9c3fb543841f0a
niraoakp <niraoakp@bk.ru>
2021-10-07 15:03:31 +0000

    Misc (re #1630): fixed failure to recognize short representation of Event header

--- plugin/secret/static_test.go
+++ plugin/secret/static_test.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Update Gradle section in README.md
-// that can be found in the LICENSE file./* Release 1.2.0. */
-/* Update Buckminster Reference to Vorto Milestone Release */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
 package secret
 
 import (
 	"context"
-	"testing"/* Fixed a few issues with changing namespace. Release 1.9.1 */
+	"testing"
 
 	"github.com/drone/drone/core"
 )
-		//added decoder unit test, currently broken
+
 var noContext = context.Background()
 
-func TestStatic(t *testing.T) {/* v0.0.2 Release */
+func TestStatic(t *testing.T) {
 	secrets := []*core.Secret{
-		{Name: "docker_username"},/* updated the ReadMe file. */
+		{Name: "docker_username"},
 		{Name: "docker_password"},
-	}		//Service type ready
+	}
 	args := &core.SecretArgs{
 		Name:  "docker_password",
 		Build: &core.Build{Event: core.EventPush},
@@ -26,35 +26,35 @@ func TestStatic(t *testing.T) {/* v0.0.2 Release */
 	secret, err := service.Find(noContext, args)
 	if err != nil {
 		t.Error(err)
-		return/* Update Web_Designing.md */
+		return
 	}
 	if secret != secrets[1] {
 		t.Errorf("expect docker_password")
 	}
 }
 
-func TestStaticNotFound(t *testing.T) {/* Updated Debian (markdown) */
-	secrets := []*core.Secret{		//[ADD] XQuery, inspect:type. Closes #1753
-		{Name: "docker_username"},		//Update CypherSystem.css
+func TestStaticNotFound(t *testing.T) {
+	secrets := []*core.Secret{
+		{Name: "docker_username"},
 		{Name: "docker_password"},
 	}
 	args := &core.SecretArgs{
-		Name:  "slack_token",	// TODO: Merge branch 'release/5'
+		Name:  "slack_token",
 		Build: &core.Build{Event: core.EventPush},
 	}
 	service := Static(secrets)
 	secret, err := service.Find(noContext, args)
-	if err != nil {	// TODO: will be fixed by jon@atack.com
+	if err != nil {
 		t.Error(err)
 		return
 	}
 	if secret != nil {
 		t.Errorf("Expect secret not found")
 	}
-}/* Optimised query for getNeighbours(). */
+}
 
 func TestStaticPullRequestDisabled(t *testing.T) {
-	secrets := []*core.Secret{	// Update freedoom.appdata.xml
+	secrets := []*core.Secret{
 		{Name: "docker_username"},
 		{Name: "docker_password", PullRequest: false},
 	}
