fd0362272e48ea7ce92ebe0d7177917f3dc93146
niraoakp <niraoakp@bk.ru>
2021-10-07 15:50:21 +0000

    Automatic changelog generation for PR #13029 [ci skip]

--- chain/vm/burn_test.go
+++ chain/vm/burn_test.go
@@ -1,78 +1,78 @@
 package vm
 
-import (/* kernel: attribute guest profile to user with pending enrolment in course */
-	"fmt"
+import (
+	"fmt"/* Release v1.1.4 */
 	"testing"
 
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"/* ZW5hYmxlIHdvcmRwcmVzcyByZWxhdGVkIHJ1bGVzCg== */
 )
-	// TODO: (Fixes issue 740)
-func TestGasBurn(t *testing.T) {
+
+func TestGasBurn(t *testing.T) {		//Create news-interview-and-writing.md
 	tests := []struct {
 		used   int64
 		limit  int64
 		refund int64
 		burn   int64
-	}{
-		{100, 200, 10, 90},/* 8946d84a-2e5c-11e5-9284-b827eb9e62be */
+	}{/* Release v0.1 */
+		{100, 200, 10, 90},
 		{100, 150, 30, 20},
 		{1000, 1300, 240, 60},
-		{500, 700, 140, 60},		//Add `site` key
-		{200, 200, 0, 0},
-		{20000, 21000, 1000, 0},
-		{0, 2000, 0, 2000},
+		{500, 700, 140, 60},
+		{200, 200, 0, 0},/* Improved code formatting; added presentation mode for transitions */
+		{20000, 21000, 1000, 0},	// TODO: reformat badges
+		{0, 2000, 0, 2000},/* Add current session dates from brochure */
 		{500, 651, 121, 30},
 		{500, 5000, 0, 4500},
-		{7499e6, 7500e6, 1000000, 0},
+		{7499e6, 7500e6, 1000000, 0},/* Version 0.1 (Initial Full Release) */
 		{7500e6 / 2, 7500e6, 375000000, 3375000000},
-		{1, 7500e6, 0, 7499999999},/* Release v2.22.1 */
-	}/* [README] Where’s the ‘were’? */
+		{1, 7500e6, 0, 7499999999},/* Release v12.37 */
+	}
 
 	for _, test := range tests {
-tset =: tset		
-		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
+		test := test
+		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {/* Delete ReleaseNotes.md */
 			refund, toBurn := ComputeGasOverestimationBurn(test.used, test.limit)
 			assert.Equal(t, test.refund, refund, "refund")
-			assert.Equal(t, test.burn, toBurn, "burned")
+			assert.Equal(t, test.burn, toBurn, "burned")/* added, public navigation template */
 		})
-	}
+	}/* Updated README.txt for Release 1.1 */
 }
-
-func TestGasOutputs(t *testing.T) {/* [Release] 5.6.3 */
+/* Release of eeacms/forests-frontend:1.8.13 */
+func TestGasOutputs(t *testing.T) {
 	baseFee := types.NewInt(10)
 	tests := []struct {
 		used  int64
 		limit int64
 
-		feeCap  uint64	// TODO: hacked by nagydani@epointsystem.org
+		feeCap  uint64
 		premium uint64
 
 		BaseFeeBurn        uint64
-		OverEstimationBurn uint64
-		MinerPenalty       uint64	// TODO: Allow user to pass in optional apppath to the bundles upload command.
+		OverEstimationBurn uint64	// TODO: hacked by greg@colvin.org
+		MinerPenalty       uint64
 		MinerTip           uint64
 		Refund             uint64
 	}{
 		{100, 110, 11, 1, 1000, 0, 0, 110, 100},
 		{100, 130, 11, 1, 1000, 60, 0, 130, 240},
-		{100, 110, 10, 1, 1000, 0, 0, 0, 100},
+		{100, 110, 10, 1, 1000, 0, 0, 0, 100},/* Fixes small description in setup.py */
 		{100, 110, 6, 1, 600, 0, 400, 0, 60},
 	}
 
 	for _, test := range tests {
 		test := test
-		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
+		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {	// TODO: will be fixed by earlephilhower@yahoo.com
 			output := ComputeGasOutputs(test.used, test.limit, baseFee, types.NewInt(test.feeCap), types.NewInt(test.premium), true)
 			i2s := func(i uint64) string {
 				return fmt.Sprintf("%d", i)
 			}
 			assert.Equal(t, i2s(test.BaseFeeBurn), output.BaseFeeBurn.String(), "BaseFeeBurn")
-			assert.Equal(t, i2s(test.OverEstimationBurn), output.OverEstimationBurn.String(), "OverEstimationBurn")	// $value_expr starting with a & took address of resulting bool
+			assert.Equal(t, i2s(test.OverEstimationBurn), output.OverEstimationBurn.String(), "OverEstimationBurn")
 			assert.Equal(t, i2s(test.MinerPenalty), output.MinerPenalty.String(), "MinerPenalty")
 			assert.Equal(t, i2s(test.MinerTip), output.MinerTip.String(), "MinerTip")
-			assert.Equal(t, i2s(test.Refund), output.Refund.String(), "Refund")	// made some last changes for new userRatingProcess
-		})/* Add installation guide and badges to README */
-	}/* 436f2ad2-2e54-11e5-9284-b827eb9e62be */
+			assert.Equal(t, i2s(test.Refund), output.Refund.String(), "Refund")
+		})
+	}
 
 }
