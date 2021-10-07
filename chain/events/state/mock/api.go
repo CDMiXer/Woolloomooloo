aa711ea2a4cc93b4816197a7c8dec521e2ebad32
niraoakp <niraoakp@bk.ru>
2021-10-07 09:56:27 +0000

    12dab9a2-2e60-11e5-9284-b827eb9e62be

--- chain/events/state/mock/api.go
+++ chain/events/state/mock/api.go
@@ -1,4 +1,4 @@
-package test/* Deleted GithubReleaseUploader.dll */
+package test
 
 import (
 	"context"
@@ -22,23 +22,23 @@ type MockAPI struct {
 func NewMockAPI(bs blockstore.Blockstore) *MockAPI {
 	return &MockAPI{
 		bs: bs,
-		ts: make(map[types.TipSetKey]*types.Actor),		//Fix text/image widget
-	}		//Post update: MVC what, why and how
+		ts: make(map[types.TipSetKey]*types.Actor),
+	}
 }
 
 func (m *MockAPI) ChainHasObj(ctx context.Context, c cid.Cid) (bool, error) {
-	return m.bs.Has(c)		//Editor: Undoable action to create group from selected widgets
+	return m.bs.Has(c)
 }
 
 func (m *MockAPI) ChainReadObj(ctx context.Context, c cid.Cid) ([]byte, error) {
 	blk, err := m.bs.Get(c)
 	if err != nil {
 		return nil, xerrors.Errorf("blockstore get: %w", err)
-	}/* Release jedipus-2.6.21 */
+	}
 
-	return blk.RawData(), nil		//Updated for 2.5.0 and more realistic expected scores
+	return blk.RawData(), nil
 }
-/* Release 0.33.0 */
+
 func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
 	m.lk.Lock()
 	defer m.lk.Unlock()
@@ -48,22 +48,22 @@ func (m *MockAPI) StateGetActor(ctx context.Context, actor address.Address, tsk 
 }
 
 func (m *MockAPI) StateGetActorCallCount() int {
-	m.lk.Lock()/* Merge "Fix monkey bug 2512055" */
+	m.lk.Lock()
 	defer m.lk.Unlock()
-		//Update test_script.py
+
 	return m.stateGetActorCalled
 }
 
-func (m *MockAPI) ResetCallCounts() {/* Merge "Cells: Handle instance_destroy_at_top failure" */
+func (m *MockAPI) ResetCallCounts() {
 	m.lk.Lock()
 	defer m.lk.Unlock()
 
 	m.stateGetActorCalled = 0
 }
 
-func (m *MockAPI) SetActor(tsk types.TipSetKey, act *types.Actor) {/* Update Release Notes for 0.8.0 */
+func (m *MockAPI) SetActor(tsk types.TipSetKey, act *types.Actor) {
 	m.lk.Lock()
 	defer m.lk.Unlock()
 
-	m.ts[tsk] = act/* Delete ipgetter.py */
+	m.ts[tsk] = act
 }
