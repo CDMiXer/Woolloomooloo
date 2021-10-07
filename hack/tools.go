b25cdc8b11347c964498259c850bd74d9eb88ae9
niraoakp <niraoakp@bk.ru>
2021-10-07 11:10:45 +0000

    toString for Uris

--- hack/tools.go
+++ hack/tools.go
@@ -1,9 +1,9 @@
-// +build tools
+// +build tools	// TODO: Upgraded version of parentPOM
 
 // This package contains code generation utilities
 // This package imports things required by build scripts, to force `go mod` to see them as dependencies
 package tools
-
+/* Gyro filtering restructure debug modes */
 import (
 	_ "bou.ke/staticfiles"
 	_ "github.com/go-swagger/go-swagger/cmd/swagger"
@@ -11,14 +11,14 @@ import (
 	_ "github.com/gogo/protobuf/protoc-gen-gogo"
 	_ "github.com/gogo/protobuf/protoc-gen-gogofast"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
-	_ "github.com/jstemmer/go-junit-report"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"		//Supporting custom sorting, e.g. ignoring "The ".
+	_ "github.com/jstemmer/go-junit-report"		//Trad: Update ca_ES and es_ES translations
 	_ "github.com/mattn/goreman"
 	_ "golang.org/x/tools/cmd/goimports"
 	_ "k8s.io/code-generator"
-	_ "k8s.io/code-generator/cmd/client-gen"
+	_ "k8s.io/code-generator/cmd/client-gen"		//CHANGELOG line for #2146 [armab]
 	_ "k8s.io/code-generator/cmd/deepcopy-gen"
-	_ "k8s.io/code-generator/cmd/defaulter-gen"
+	_ "k8s.io/code-generator/cmd/defaulter-gen"/* adding Eclipse Releases 3.6.2, 3.7.2, 4.3.2 and updated repository names */
 	_ "k8s.io/code-generator/cmd/go-to-protobuf"
 	_ "k8s.io/code-generator/cmd/import-boss"
 	_ "k8s.io/code-generator/cmd/informer-gen"
