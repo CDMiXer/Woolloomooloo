15c54ee755be1b2e49cb58be18dd53fa42b8036f
niraoakp <niraoakp@bk.ru>
2021-10-07 01:58:48 +0000

    Added issues, forks and stars

--- gen/conn_write_legacy.go
+++ gen/conn_write_legacy.go
@@ -1,18 +1,18 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style/* [KEB]removed package */
+// Use of this source code is governed by a BSD-style	// TODO: Base structure revamp
 // license that can be found in the LICENSE file.
 
 // +build !go1.8
+	// TODO: Use phonegap.yml credential file
+package websocket
 
-package websocket/* Delete icon_bitcoin.png */
-	// TODO: Updated the pyrserve feedstock.
 func (c *Conn) writeBufs(bufs ...[]byte) error {
 	for _, buf := range bufs {
 		if len(buf) > 0 {
 			if _, err := c.conn.Write(buf); err != nil {
 				return err
-			}
+			}/* Create Matrix_README.md */
 		}
 	}
-	return nil/* hadoop: fix callback errors after promise */
-}
+	return nil
+}/* Finish off post, give it a title. */
