1021694e29b5a6157e2fe920fb466593bcd8d909
niraoakp <niraoakp@bk.ru>
2021-10-07 03:19:35 +0000

    Merge "Customize "supported_pci_vendor_devs" for SR-IOV"

--- metric/repos.go
+++ metric/repos.go
@@ -1,26 +1,26 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge "Specify location when creating s3 bucket." */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Rename getName to getScope to better represent what we are "getting" */
 // +build !oss
 
-package metric		//Create Testing-Guide-v5.md
+package metric
 
 import (
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Merge "Create utility to clean-up netns."
 
-	"github.com/prometheus/client_golang/prometheus"		//Add `"sketch"` also as priority aliasField to webpack config
+	"github.com/prometheus/client_golang/prometheus"
 )
-/* Delete Pics */
+
 // RepoCount registers the repository metrics.
-func RepoCount(repos core.RepositoryStore) {/* the description added on readme */
+func RepoCount(repos core.RepositoryStore) {
 	prometheus.MustRegister(
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-			Name: "drone_repo_count",	// Don't throw IAEs when partition not in use. just ignore.
-			Help: "Total number of registered repositories.",
-		}, func() float64 {
-			i, _ := repos.Count(noContext)
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{/* Updated Release Notes. */
+			Name: "drone_repo_count",/* Review blog post on Release of 10.2.1 */
+			Help: "Total number of registered repositories.",	// TODO: Model classes are generated
+		}, func() float64 {/* Release new version 2.4.21: Minor Safari bugfixes */
+			i, _ := repos.Count(noContext)/* 240832f4-2e60-11e5-9284-b827eb9e62be */
 			return float64(i)
 		}),
 	)
-}
+}/* Replace oraclejdk8 with openjdk8 for Travis CI. */
