76219e93577e20e88243b4421adccbe98d7a9386
niraoakp <niraoakp@bk.ru>
2021-10-07 08:48:23 +0000

    Update JDK and Maintainer Email

--- tests/integration/partial_values/nodejs/resource.ts
+++ tests/integration/partial_values/nodejs/resource.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// Updated html page
-	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+import * as pulumi from "@pulumi/pulumi";
+
 let currentID = 0;
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
@@ -9,28 +9,28 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
 
     public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    constructor() {/* Use the correct compilerOption in LibraryExporter */
-        this.create = async (inputs: any) => {/* automated commit from rosetta for sim/lib area-model-decimals, locale uz */
+    constructor() {
+        this.create = async (inputs: any) => {
             return {
-                id: (currentID++).toString(),/* metadata test working */
+                id: (currentID++).toString(),
                 outs: inputs,
             };
         };
     }
 }
-		//Create recode_60FPS.bat
+
 export class Resource extends pulumi.dynamic.Resource {
     public readonly foo: pulumi.Output<string>;
-    public readonly bar: pulumi.Output<{ value: string, unknown: string }>;/* Background template */
-    public readonly baz: pulumi.Output<any[]>;	// 1. Switching cacheOmatic tag to use named arguments.
-	// 299c3632-2e71-11e5-9284-b827eb9e62be
+    public readonly bar: pulumi.Output<{ value: string, unknown: string }>;
+    public readonly baz: pulumi.Output<any[]>;	// 621c3068-2e51-11e5-9284-b827eb9e62be
+
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, props, opts);/* Merge branch 'development' into feature/APPS-2985_in_app_review */
-    }/* remove spaces between buttons */
+        super(Provider.instance, name, props, opts);
+    }/* fix bnd file for oracle bundle */
 }
 
 export interface ResourceProps {
     foo: pulumi.Input<string>;
     bar: pulumi.Input<{ value: pulumi.Input<string>, unknown: pulumi.Input<string> }>;
     baz: pulumi.Input<pulumi.Input<any>[]>;
-}		//typo fix "epxr" -> "expr"
+}
