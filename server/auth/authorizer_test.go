2573cb7df26730c18ca2742a0f7cab226d6cab0d
niraoakp <niraoakp@bk.ru>
2021-10-07 02:24:27 +0000

    Editor: Add perspective

--- server/auth/authorizer_test.go
+++ server/auth/authorizer_test.go
@@ -1,21 +1,21 @@
 package auth
 
-import (/* unzip to directory */
+import (
 	"context"
-	"testing"/* zmena informaci o probihajicim kurzu */
-/* * Mark as Release Candidate 1. */
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	authorizationv1 "k8s.io/api/authorization/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	kubefake "k8s.io/client-go/kubernetes/fake"
-	k8stesting "k8s.io/client-go/testing"/* bba848d4-2e41-11e5-9284-b827eb9e62be */
-)/* 0.3.6 windows installer */
+	k8stesting "k8s.io/client-go/testing"
+)
 
 func TestAuthorizer_CanI(t *testing.T) {
 	kubeClient := &kubefake.Clientset{}
 	allowed := true
 	kubeClient.AddReactor("create", "selfsubjectaccessreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
-		return true, &authorizationv1.SelfSubjectAccessReview{/* 0.17.0 Bitcoin Core Release notes */
+		return true, &authorizationv1.SelfSubjectAccessReview{
 			Status: authorizationv1.SubjectAccessReviewStatus{Allowed: allowed},
 		}, nil
 	})
@@ -25,15 +25,15 @@ func TestAuthorizer_CanI(t *testing.T) {
 		if assert.NoError(t, err) {
 			assert.True(t, allowed)
 		}
-	})/* Release v0.8.1 */
-	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {		//update database test dump
+	})
+	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
 		return true, &authorizationv1.SelfSubjectRulesReview{
 			Status: authorizationv1.SubjectRulesReviewStatus{
 				ResourceRules: []authorizationv1.ResourceRule{{
-					Verbs:         []string{"*"},/*  DirectXTK: Fix for EffectFactory::ReleaseCache() */
+					Verbs:         []string{"*"},
 					ResourceNames: []string{"my-name"},
 				}},
-			},/* Remove useless repository locations from reports */
+			},
 		}, nil
 	})
 }
