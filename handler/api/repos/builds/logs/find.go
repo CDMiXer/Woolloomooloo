52ee6d502480b58370acbaf9f3cb3ab265e4bbb0
niraoakp <niraoakp@bk.ru>
2021-10-07 12:20:01 +0000

    rev 474145

--- handler/api/repos/builds/logs/find.go
+++ handler/api/repos/builds/logs/find.go
@@ -6,8 +6,8 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Update boto3 from 1.12.38 to 1.12.39
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -18,36 +18,36 @@ import (
 	"io"
 	"net/http"
 	"strconv"
-/* Always ack key exchanges */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-/* Merge "replace by VSTM/VLDM to reduce one of VST1/VLD1" */
+
 	"github.com/go-chi/chi"
 )
-	// TODO: OOP: Added object:hasPermissionTo
+
 // HandleFind returns an http.HandlerFunc that writes the
-// json-encoded logs to the response body./* Merge "Release 1.0.0.98 QCACLD WLAN Driver" */
+// json-encoded logs to the response body.
 func HandleFind(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
 	steps core.StepStore,
-	logs core.LogStore,	// TODO: hacked by aeongrp@outlook.com
+	logs core.LogStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")/* yellow highlight on save button */
-)		
+			name      = chi.URLParam(r, "name")
+		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
-		if err != nil {	// TODO: typo in test messages
+		if err != nil {
 			render.BadRequest(w, err)
-			return		//update: comment delete on Idea detail page
+			return
 		}
 		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
 		if err != nil {
 			render.BadRequest(w, err)
-			return/* change isReleaseBuild to isDevMode */
+			return
 		}
 		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
 		if err != nil {
@@ -62,16 +62,16 @@ func HandleFind(
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
 			render.NotFound(w, err)
-			return/* Update VerifySvnFolderReleaseAction.java */
+			return
 		}
 		stage, err := stages.FindNumber(r.Context(), build.ID, stageNumber)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}/* Merge branch '11_sprint' into forgot_password */
+		}
 		step, err := steps.FindNumber(r.Context(), stage.ID, stepNumber)
 		if err != nil {
-			render.NotFound(w, err)		//Completed property file content testing.
+			render.NotFound(w, err)
 			return
 		}
 		rc, err := logs.Find(r.Context(), step.ID)
