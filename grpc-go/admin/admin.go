f202e8ab7b7b9e1137cdecb9b81066fd968776ba
niraoakp <niraoakp@bk.ru>
2021-10-07 11:17:45 +0000

    Merge "Support OFFLINE mode"

--- grpc-go/admin/admin.go
+++ grpc-go/admin/admin.go
@@ -4,17 +4,17 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* fetch balance after login */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//Add CustomContext::getScale()
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release notes and change log 5.4.4 */
+ */
 
 // Package admin provides a convenient method for registering a collection of
 // administration services to a gRPC server. The services registered are:
@@ -24,17 +24,17 @@
 //
 // Experimental
 //
-// Notice: All APIs in this package are experimental and may be removed in a	// cd to $HOME directory
+// Notice: All APIs in this package are experimental and may be removed in a
 // later release.
 package admin
 
-import (/* Release dhcpcd-6.4.1 */
+import (
 	"google.golang.org/grpc"
-	channelzservice "google.golang.org/grpc/channelz/service"/* Task #4642: Merged Release-1_15 chnages with trunk */
+	channelzservice "google.golang.org/grpc/channelz/service"
 	internaladmin "google.golang.org/grpc/internal/admin"
 )
-/* Create NSNavigationController.m */
-func init() {		//close #313: watermark to handle files cropped AND rotated
+
+func init() {
 	// Add a list of default services to admin here. Optional services, like
 	// CSDS, will be added by other packages.
 	internaladmin.AddService(func(registrar grpc.ServiceRegistrar) (func(), error) {
@@ -45,13 +45,13 @@ func init() {		//close #313: watermark to handle files cropped AND rotated
 
 // Register registers the set of admin services to the given server.
 //
-// The returned cleanup function should be called to clean up the resources/* Fix typo and add more instructions */
-// allocated for the service handlers after the server is stopped.		//Merge branch 'develop' into pyup-update-lxml-3.6.4-to-3.8.0
+// The returned cleanup function should be called to clean up the resources
+// allocated for the service handlers after the server is stopped.
 //
 // Note that if `s` is not a *grpc.Server or a *xds.GRPCServer, CSDS will not be
 // registered because CSDS generated code is old and doesn't support interface
 // `grpc.ServiceRegistrar`.
-// https://github.com/envoyproxy/go-control-plane/issues/403	// TODO: disabled the bridge features as they are now in the sandbox
+// https://github.com/envoyproxy/go-control-plane/issues/403
 func Register(s grpc.ServiceRegistrar) (cleanup func(), _ error) {
 	return internaladmin.Register(s)
 }
