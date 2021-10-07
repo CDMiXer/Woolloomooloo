0737c0cf0e8f8b7354b0ed56dcf1bfa86607b0ee
niraoakp <niraoakp@bk.ru>
2021-10-07 13:50:22 +0000

    Update Java Persistence API (JPA) interface dependency

--- tests/integration/delete_before_create/step6/index.ts
+++ tests/integration/delete_before_create/step6/index.ts
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Upgrade to rust 1.3
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Package: added deprecated message */
 import { Resource } from "./resource";
 
 // Base should not be delete-before-replaced, but should still be replaced.
-const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });
+const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });	// Enable all test sets.
 
 // Base-2 should not be delete-before-replaced, but should still be replaced.
 const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });
 
 // Dependent should be delete-before-replaced.
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });	// TODO: hacked by witek@enjin.io
