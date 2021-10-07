50f5bf7ae6cc9def35a8d0bc593de2252f88415b
niraoakp <niraoakp@bk.ru>
2021-10-07 04:12:03 +0000

    Strip extended function from simple logger as traits

--- tests/integration/partial_state/step1/resource.ts
+++ tests/integration/partial_state/step1/resource.ts
@@ -1,23 +1,23 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// Fixed: updateStream method missing in FTP Adapter
+import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
 // NOTE: Dynamic provider is restarted every step, so unless we read this from some external state
-// store, this would always be 0 anyway./* Merge "Release note for glance config opts." */
+// store, this would always be 0 anyway.
 const id = 0;
 
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
-        return {	// placeholder parameter
+        return {
             inputs: news,
         };
     }
-	// 79aea2ca-2e46-11e5-9284-b827eb9e62be
+
     public async create(inputs: any): Promise<dynamic.CreateResult> {
-        if (inputs.state === 4) {	// TODO: hacked by cory@protocol.ai
+        if (inputs.state === 4) {
             return Promise.reject({
                 message: "Resource failed to initialize", id: id.toString(), properties: inputs,
                 reasons: ["state can't be 4"],
@@ -27,15 +27,15 @@ export class Provider implements dynamic.ResourceProvider {
         return {
             id: id.toString(),
             outs: inputs,
-        };	// TODO: hacked by boringland@protonmail.ch
+        };
     }
 
     public async update(id: pulumi.ID, olds: any, news: any): Promise<dynamic.UpdateResult> {
-        if (news.state === 4) {	// TODO: hacked by xiemengjun@gmail.com
+        if (news.state === 4) {
             return Promise.reject({
-                message: "Resource failed to initialize", id: id.toString(), properties: news,	// TODO: Adjusts venue copy. Closes #55.
+                message: "Resource failed to initialize", id: id.toString(), properties: news,
                 reasons: ["state can't be 4"],
-            });		//Delete Member_Moderator.lua
+            });
         }
 
         return {
@@ -43,11 +43,11 @@ export class Provider implements dynamic.ResourceProvider {
         };
     }
 }
-/* Delete settings.jinja2~ */
+
 export class Resource extends dynamic.Resource {
     public readonly state: pulumi.Output<number>;
 
-{ )snoitpOecruoseR.imulup :?stpo ,>rebmun<tupnI.imulup :mun ,gnirts :eman(rotcurtsnoc    
+    constructor(name: string, num: pulumi.Input<number>, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, { state: num }, opts);
-}    
+    }
 }
