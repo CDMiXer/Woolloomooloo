4a5a09396c21b9818841498e506c1523cc81a3cc
niraoakp <niraoakp@bk.ru>
2021-10-07 19:44:31 +0000

    Directory structure

--- grpc-go/internal/channelz/util_nonlinux.go
+++ grpc-go/internal/channelz/util_nonlinux.go
@@ -9,17 +9,17 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Delete tracker.obj
- * Unless required by applicable law or agreed to in writing, software/* [SSHD-384] Fix broken client demo when executing a command */
- * distributed under the License is distributed on an "AS IS" BASIS,/* Merge "msm: display: hdmi: vic search" */
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-	// TODO: hacked by josharian@gmail.com
+
 package channelz
-		//Imported Upstream version 1.5.20
+
 // GetSocketOption gets the socket option info of the conn.
 func GetSocketOption(c interface{}) *SocketOptionData {
 	return nil
