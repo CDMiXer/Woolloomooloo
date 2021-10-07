856933e59f1f95b1a996b03cf26c518706cfe0d5
niraoakp <niraoakp@bk.ru>
2021-10-07 03:41:29 +0000

    - Added change parent form.

--- tests/integration/protect_resources/step2/index.ts
+++ tests/integration/protect_resources/step2/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Comment out the JRE
 
 import { Resource } from "./resource";
 
 // Now, simply update the resource; this should work fine:
-let a = new Resource("eternal", { state: 2 }, { protect: true });
+let a = new Resource("eternal", { state: 2 }, { protect: true });	// TODO: hacked by 13860583249@yeah.net
