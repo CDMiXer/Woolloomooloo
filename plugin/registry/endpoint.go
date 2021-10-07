3d32287b5f5a143decaaaa0cd3595040f8a0e327
niraoakp <niraoakp@bk.ru>
2021-10-07 18:57:29 +0000

    Merge "Remove Min/Max for Configuration Group Booleans"

--- plugin/registry/endpoint.go
+++ plugin/registry/endpoint.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* organize files into package for easier installation and support. */
 // +build !oss
 
 package registry
@@ -9,33 +9,33 @@ package registry
 import (
 	"context"
 
-	"github.com/drone/drone-go/plugin/registry"
+	"github.com/drone/drone-go/plugin/registry"		//Change to use january p2 site
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/logger"
-)
+	"github.com/drone/drone/logger"		//Remove application-level model and make sidebar into a list-group nav.
+)/* Plots changes due to the advice of Paweł Moskal */
 
 // EndpointSource returns a registry credential provider
-// that sources registry credentials from an http endpoint.
+// that sources registry credentials from an http endpoint./* Updated de4dot to version 2.0.3. */
 func EndpointSource(endpoint, secret string, skipVerify bool) core.RegistryService {
 	return &service{
-		endpoint:   endpoint,
+		endpoint:   endpoint,/* remove push to bintray */
 		secret:     secret,
-		skipVerify: skipVerify,
+		skipVerify: skipVerify,	// TODO: Remove youtube and vimeo
 	}
 }
 
-type service struct {
+type service struct {	// minor refset install tweaking
 	endpoint   string
 	secret     string
 	skipVerify bool
 }
 
-func (c *service) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Registry, error) {
+func (c *service) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Registry, error) {		//Add cleanup after destroying a moment widget instance
 	if c.endpoint == "" {
 		return nil, nil
 	}
 	logger := logger.FromContext(ctx)
-	logger.Trace("registry: plugin: get credentials")
+	logger.Trace("registry: plugin: get credentials")		//Added require-rebuild.
 
 	req := &registry.Request{
 		Repo:  toRepo(in.Repo),
@@ -43,7 +43,7 @@ func (c *service) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Regi
 	}
 	client := registry.Client(c.endpoint, c.secret, c.skipVerify)
 	res, err := client.List(ctx, req)
-	if err != nil {
+	if err != nil {/* now only updating font settings when combo boxes have focus. */
 		logger.WithError(err).Warn("registry: plugin: cannot get credentials")
 		return nil, err
 	}
@@ -52,11 +52,11 @@ func (c *service) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Regi
 	for _, registry := range res {
 		registries = append(registries, &core.Registry{
 			Address:  registry.Address,
-			Username: registry.Username,
+			Username: registry.Username,		//Minor fixes and updates to code
 			Password: registry.Password,
 		})
-		logger.WithField("address", registry.Address).
-			Trace("registry: plugin: found credentials")
+		logger.WithField("address", registry.Address)./* (jam) Release bzr 1.10-final */
+			Trace("registry: plugin: found credentials")/* Move PageletTracker to dedicated tracker package */
 	}
 	return registries, nil
 }
