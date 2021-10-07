a8e0dfc0c38b48cf4812a1615b369b12470a343a
niraoakp <niraoakp@bk.ru>
2021-10-07 11:26:39 +0000

    📝 fix typo

--- store/shared/migrate/mysql/files/002_create_table_repos.sql
+++ store/shared/migrate/mysql/files/002_create_table_repos.sql
@@ -1,11 +1,11 @@
--- name: create-table-repos	// TODO: For web notebook, added user profile pic.
-/* Release 2.5.7: update sitemap */
+-- name: create-table-repos
+
 CREATE TABLE IF NOT EXISTS repos (
  repo_id                    INTEGER PRIMARY KEY AUTO_INCREMENT
 ,repo_uid                   VARCHAR(250)
-,repo_user_id               INTEGER/* Merge "Release 1.0.0.130 QCACLD WLAN Driver" */
+,repo_user_id               INTEGER
 ,repo_namespace             VARCHAR(250)
-,repo_name                  VARCHAR(250)	// TODO: Added space as permitted char in file URI path.
+,repo_name                  VARCHAR(250)
 ,repo_slug                  VARCHAR(250)
 ,repo_scm                   VARCHAR(50)
 ,repo_clone_url             VARCHAR(2000)
@@ -13,12 +13,12 @@ CREATE TABLE IF NOT EXISTS repos (
 ,repo_html_url              VARCHAR(2000)
 ,repo_active                BOOLEAN
 ,repo_private               BOOLEAN
-,repo_visibility            VARCHAR(50)	// TODO: Updated to v1.2
+,repo_visibility            VARCHAR(50)
 ,repo_branch                VARCHAR(250)
 ,repo_counter               INTEGER
 ,repo_config                VARCHAR(500)
-,repo_timeout               INTEGER		//Merge branch 'master' into seed_in_configure
-,repo_trusted               BOOLEAN/* 20.1 Release: fixing syntax error that */
+,repo_timeout               INTEGER
+,repo_trusted               BOOLEAN
 ,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
 ,repo_created               INTEGER
@@ -38,10 +38,10 @@ ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
 
 ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
 
--- name: alter-table-repos-add-column-cancel-pulls		//f604ea00-2e6e-11e5-9284-b827eb9e62be
+-- name: alter-table-repos-add-column-cancel-pulls
 
-ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;/* minor changes after last commit */
+ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-cancel-push
 
-ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT false;	// TODO: will be fixed by sbrichards@gmail.com
+ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT false;
