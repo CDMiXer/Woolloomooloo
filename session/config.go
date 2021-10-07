5ea9ea0c3cb85cb33f59512cb58990edb0532a0a
niraoakp <niraoakp@bk.ru>
2021-10-07 09:07:52 +0000

    small translation updates

--- session/config.go
+++ session/config.go
@@ -3,32 +3,32 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
+///* [3029] reworked auto billing to be less restrictive */
+//      http://www.apache.org/licenses/LICENSE-2.0/* Task #38: Fixed ReleaseIT (SVN) */
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* cloudinit: Added tests for TargetRelease */
-//	// TODO: Do not remove blank frames for tricky data sets
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Added + to version number
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-		//Update and rename ideas to ideas/pe/README.md
-package session
+// limitations under the License.		//Use correct property for CURIE links.
+
+package session/* Deal with the command bus in the app service provider */
 
 import "time"
-/* Release 1.0.4 (skipping version 1.0.3) */
+	// Update qdownload.md
 // Config provides the session configuration.
-type Config struct {
+type Config struct {	// TODO: hacked by arajasek94@gmail.com
 	Secure      bool
 	Secret      string
-	Timeout     time.Duration
+	Timeout     time.Duration	// safer setXXX calls
 	MappingFile string
 }
-	// TODO: hacked by alex.gaynor@gmail.com
+
 // NewConfig returns a new session configuration.
-func NewConfig(secret string, timeout time.Duration, secure bool) Config {
-	return Config{
+func NewConfig(secret string, timeout time.Duration, secure bool) Config {/* Release profile added. */
+	return Config{/* Rule creation screen work. */
 		Secure:  secure,
 		Secret:  secret,
-		Timeout: timeout,/* Use no header and footer template for download page. Release 0.6.8. */
+		Timeout: timeout,
 	}
 }
