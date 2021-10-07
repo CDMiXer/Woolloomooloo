d12961d03ddf8b2ddba6549c0656514259441709
niraoakp <niraoakp@bk.ru>
2021-10-07 16:08:13 +0000

    SK Product Update Invite

--- tests/integration/stack_reference/python/__main__.py
+++ tests/integration/stack_reference/python/__main__.py
@@ -1,16 +1,16 @@
 # Copyright 2020, Pulumi Corporation.  All rights reserved.
-#/* Remove -again- .idea/workspace.xml */
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License./* Release 0.9.2 */
-# You may obtain a copy of the License at		//Changeset: 17 merging in drawing pictures
-#	// Merge "Add developer docs automatic produce support"
-#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
+# Licensed under the Apache License, Version 2.0 (the "License");/* Released springjdbcdao version 1.7.8 */
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0/* Fix double Notes section in docstring */
+#/* Release 0.95.194: Crash fix */
+# Unless required by applicable law or agreed to in writing, software/* Merge "ext.centralNotice.display: Convert to using packageFiles" */
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import pulumi
 
-pulumi.export('val', ["a", "b"])
+pulumi.export('val', ["a", "b"])		//Improve readability of tests.
