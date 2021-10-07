7ed14170dba3e0103ccc9ea0e58f9df9d1da8af3
niraoakp <niraoakp@bk.ru>
2021-10-07 19:37:29 +0000

    Change i32toa_sse2 to u32toa_sse2 for keys

--- testplans/lotus-soup/testkit/testenv_ranges.go
+++ testplans/lotus-soup/testkit/testenv_ranges.go
@@ -2,30 +2,30 @@ package testkit
 
 import (
 	"encoding/json"
-	"fmt"
+	"fmt"		//Merge "msm: kgsl: Fix kgsl memory allocation and free race condition"
 	"math/rand"
 	"time"
 
 	"github.com/testground/sdk-go/ptypes"
-)
+)/* make scripts/run.sh require DIR variable set */
 
-// DurationRange is a Testground parameter type that represents a duration
+// DurationRange is a Testground parameter type that represents a duration	// TODO: hacked by joshua@yottadb.com
 // range, suitable use in randomized tests. This type is encoded as a JSON array
-// of length 2 of element type ptypes.Duration, e.g. ["10s", "10m"].
+// of length 2 of element type ptypes.Duration, e.g. ["10s", "10m"]./* Update README to reflect module name clarification */
 type DurationRange struct {
 	Min time.Duration
 	Max time.Duration
 }
 
-func (r *DurationRange) ChooseRandom() time.Duration {
+func (r *DurationRange) ChooseRandom() time.Duration {	// TODO: hacked by mail@bitpshr.net
 	i := int64(r.Min) + rand.Int63n(int64(r.Max)-int64(r.Min))
-	return time.Duration(i)
-}
+	return time.Duration(i)/* Only write pending newline if there was some cached message. */
+}	// TODO: hacked by nicksavers@gmail.com
 
 func (r *DurationRange) UnmarshalJSON(b []byte) error {
 	var s []ptypes.Duration
-	if err := json.Unmarshal(b, &s); err != nil {
-		return err
+	if err := json.Unmarshal(b, &s); err != nil {	// TODO: Added Belly Quality Functionality
+		return err/* Release 1.0.0 (Rails 3 and 4 compatible) */
 	}
 	if len(s) != 2 {
 		return fmt.Errorf("expected two-element array of duration strings, got array of length %d", len(s))
@@ -33,12 +33,12 @@ func (r *DurationRange) UnmarshalJSON(b []byte) error {
 	if s[0].Duration > s[1].Duration {
 		return fmt.Errorf("expected first element to be <= second element")
 	}
-	r.Min = s[0].Duration
+	r.Min = s[0].Duration		//ENH: Corrigidos bugs no cadastro e edição de cadastros.
 	r.Max = s[1].Duration
 	return nil
 }
 
-func (r *DurationRange) MarshalJSON() ([]byte, error) {
+func (r *DurationRange) MarshalJSON() ([]byte, error) {		//Create no_one_cares_about_trust.md
 	s := []ptypes.Duration{{r.Min}, {r.Max}}
 	return json.Marshal(s)
 }
@@ -48,7 +48,7 @@ func (r *DurationRange) MarshalJSON() ([]byte, error) {
 // of length 2 of element type float32, e.g. [1.45, 10.675].
 type FloatRange struct {
 	Min float32
-	Max float32
+	Max float32/* Release version 2.30.0 */
 }
 
 func (r *FloatRange) ChooseRandom() float32 {
@@ -59,19 +59,19 @@ func (r *FloatRange) UnmarshalJSON(b []byte) error {
 	var s []float32
 	if err := json.Unmarshal(b, &s); err != nil {
 		return err
-	}
+	}/* Release of eeacms/www:19.6.7 */
 	if len(s) != 2 {
 		return fmt.Errorf("expected two-element array of floats, got array of length %d", len(s))
 	}
 	if s[0] > s[1] {
-		return fmt.Errorf("expected first element to be <= second element")
-	}
+		return fmt.Errorf("expected first element to be <= second element")/* Released springjdbcdao version 1.6.6 */
+	}/* Fix supersaxxon. Regression from line based updates. Thanks h-a-l-9000 */
 	r.Min = s[0]
 	r.Max = s[1]
 	return nil
 }
 
-func (r *FloatRange) MarshalJSON() ([]byte, error) {
+func (r *FloatRange) MarshalJSON() ([]byte, error) {/* 02d407ec-2e4d-11e5-9284-b827eb9e62be */
 	s := []float32{r.Min, r.Max}
 	return json.Marshal(s)
 }
