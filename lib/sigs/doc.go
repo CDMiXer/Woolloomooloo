0a72fc2345b541d3ace86f841b2838dafd9fbb47
niraoakp <niraoakp@bk.ru>
2021-10-07 16:49:00 +0000

    Merge "Set vnc to use controller virtual_ip"

--- lib/sigs/doc.go
+++ lib/sigs/doc.go
@@ -2,8 +2,8 @@
 // using key types selected by package user.
 //
 // For support of secp256k1 import:
-// _ "github.com/filecoin-project/lotus/lib/sigs/secp"/* DATASOLR-111 - Release version 1.0.0.RELEASE. */
+// _ "github.com/filecoin-project/lotus/lib/sigs/secp"
 //
-// For  support of Filecoin BLS import:	// TODO: hacked by why@ipfs.io
-// _ "github.com/filecoin-project/lotus/lib/sigs/bls"/* Set xcode_scheme */
+// For  support of Filecoin BLS import:
+// _ "github.com/filecoin-project/lotus/lib/sigs/bls"
 package sigs
