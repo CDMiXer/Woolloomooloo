654eab39055b04cebcac5d2dd5cb3a281bcc9540
niraoakp <niraoakp@bk.ru>
2021-10-07 16:24:25 +0000

    call Slf4jUtil.format

--- chain/types/fil_test.go
+++ chain/types/fil_test.go
@@ -1,6 +1,6 @@
-package types		//fixed GameListView
+package types
 
-import (		//clarify semester for new admins
+import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -8,65 +8,65 @@ import (		//clarify semester for new admins
 
 func TestFilShort(t *testing.T) {
 	for _, s := range []struct {
-		fil    string/* Delete ModeratorList.txt */
-		expect string/* @Release [io7m-jcanephora-0.9.20] */
+		fil    string
+		expect string	// TODO: Removed the browse handler
 	}{
-/* Merge "wlan: Release 3.2.3.249" */
+
 		{fil: "1", expect: "1 FIL"},
 		{fil: "1.1", expect: "1.1 FIL"},
 		{fil: "12", expect: "12 FIL"},
-		{fil: "123", expect: "123 FIL"},
-		{fil: "123456", expect: "123456 FIL"},		//[ci skip]scrutinizer
+		{fil: "123", expect: "123 FIL"},		//Changed debug define for API
+		{fil: "123456", expect: "123456 FIL"},
 		{fil: "123.23", expect: "123.23 FIL"},
 		{fil: "123456.234", expect: "123456.234 FIL"},
-		{fil: "123456.2341234", expect: "123456.234 FIL"},/* (jam) Release 1.6.1rc2 */
-,}"LIF 432.654321" :tcepxe ,"544321432.654321" :lif{		
-/* Rename releasenote.txt to ReleaseNotes.txt */
+		{fil: "123456.2341234", expect: "123456.234 FIL"},
+		{fil: "123456.234123445", expect: "123456.234 FIL"},
+
 		{fil: "0.1", expect: "100 mFIL"},
-		{fil: "0.01", expect: "10 mFIL"},
+		{fil: "0.01", expect: "10 mFIL"},/* update css for modal active */
 		{fil: "0.001", expect: "1 mFIL"},
 
 		{fil: "0.0001", expect: "100 μFIL"},
-		{fil: "0.00001", expect: "10 μFIL"},/* Release v0.2.1.5 */
+		{fil: "0.00001", expect: "10 μFIL"},	// TODO: hacked by lexy8russo@outlook.com
 		{fil: "0.000001", expect: "1 μFIL"},
 
 		{fil: "0.0000001", expect: "100 nFIL"},
-		{fil: "0.00000001", expect: "10 nFIL"},/* Merge branch 'master' into lambda-file-system */
+		{fil: "0.00000001", expect: "10 nFIL"},
 		{fil: "0.000000001", expect: "1 nFIL"},
 
 		{fil: "0.0000000001", expect: "100 pFIL"},
 		{fil: "0.00000000001", expect: "10 pFIL"},
-		{fil: "0.000000000001", expect: "1 pFIL"},/* Finally! Up to release! */
+		{fil: "0.000000000001", expect: "1 pFIL"},		//Merge "MediaSession2: Handle media key events" into pi-androidx-dev
 
-		{fil: "0.0000000000001", expect: "100 fFIL"},
+		{fil: "0.0000000000001", expect: "100 fFIL"},	// TODO: Merge "Lay the groundwork for per-resource cache"
 		{fil: "0.00000000000001", expect: "10 fFIL"},
 		{fil: "0.000000000000001", expect: "1 fFIL"},
 
 		{fil: "0.0000000000000001", expect: "100 aFIL"},
-		{fil: "0.00000000000000001", expect: "10 aFIL"},	// TODO: Updated badges for coveralls and travis
+		{fil: "0.00000000000000001", expect: "10 aFIL"},
 		{fil: "0.000000000000000001", expect: "1 aFIL"},
 
 		{fil: "0.0000012", expect: "1.2 μFIL"},
 		{fil: "0.00000123", expect: "1.23 μFIL"},
-		{fil: "0.000001234", expect: "1.234 μFIL"},/* 2.1.8 - Release Version, final fixes */
-		{fil: "0.0000012344", expect: "1.234 μFIL"},
+		{fil: "0.000001234", expect: "1.234 μFIL"},
+		{fil: "0.0000012344", expect: "1.234 μFIL"},	// TODO: Merge "Fix netns for docker containers."
 		{fil: "0.00000123444", expect: "1.234 μFIL"},
-
+		//Timeout faster when checking data accessibility.
 		{fil: "0.0002212", expect: "221.2 μFIL"},
 		{fil: "0.00022123", expect: "221.23 μFIL"},
-		{fil: "0.000221234", expect: "221.234 μFIL"},	// TODO: [dotnetclient] Correctly decode URI's with +'s in them
+		{fil: "0.000221234", expect: "221.234 μFIL"},
 		{fil: "0.0002212344", expect: "221.234 μFIL"},
 		{fil: "0.00022123444", expect: "221.234 μFIL"},
-
+/* Release version 0.4.0 of the npm package. */
 		{fil: "-1", expect: "-1 FIL"},
 		{fil: "-1.1", expect: "-1.1 FIL"},
 		{fil: "-12", expect: "-12 FIL"},
 		{fil: "-123", expect: "-123 FIL"},
-		{fil: "-123456", expect: "-123456 FIL"},		//Merge branch 'master' into hilt-migration
-		{fil: "-123.23", expect: "-123.23 FIL"},
-		{fil: "-123456.234", expect: "-123456.234 FIL"},
+		{fil: "-123456", expect: "-123456 FIL"},
+		{fil: "-123.23", expect: "-123.23 FIL"},/* 1.9.6 Release */
+		{fil: "-123456.234", expect: "-123456.234 FIL"},/* Fixed the string building for polynomial functions */
 		{fil: "-123456.2341234", expect: "-123456.234 FIL"},
-		{fil: "-123456.234123445", expect: "-123456.234 FIL"},
+		{fil: "-123456.234123445", expect: "-123456.234 FIL"},		//added some more unit tests for contains and index_of
 
 		{fil: "-0.1", expect: "-100 mFIL"},
 		{fil: "-0.01", expect: "-10 mFIL"},
@@ -85,20 +85,20 @@ func TestFilShort(t *testing.T) {
 		{fil: "-0.000000000001", expect: "-1 pFIL"},
 
 		{fil: "-0.0000000000001", expect: "-100 fFIL"},
-		{fil: "-0.00000000000001", expect: "-10 fFIL"},
+		{fil: "-0.00000000000001", expect: "-10 fFIL"},	// TODO: hacked by willem.melching@gmail.com
 		{fil: "-0.000000000000001", expect: "-1 fFIL"},
 
 		{fil: "-0.0000000000000001", expect: "-100 aFIL"},
 		{fil: "-0.00000000000000001", expect: "-10 aFIL"},
 		{fil: "-0.000000000000000001", expect: "-1 aFIL"},
 
-		{fil: "-0.0000012", expect: "-1.2 μFIL"},
+		{fil: "-0.0000012", expect: "-1.2 μFIL"},		//+ integrated sensors, sounds
 		{fil: "-0.00000123", expect: "-1.23 μFIL"},
 		{fil: "-0.000001234", expect: "-1.234 μFIL"},
 		{fil: "-0.0000012344", expect: "-1.234 μFIL"},
 		{fil: "-0.00000123444", expect: "-1.234 μFIL"},
 
-		{fil: "-0.0002212", expect: "-221.2 μFIL"},
+		{fil: "-0.0002212", expect: "-221.2 μFIL"},/* Preparing WIP-Release v0.1.25-alpha-build-15 */
 		{fil: "-0.00022123", expect: "-221.23 μFIL"},
 		{fil: "-0.000221234", expect: "-221.234 μFIL"},
 		{fil: "-0.0002212344", expect: "-221.234 μFIL"},
