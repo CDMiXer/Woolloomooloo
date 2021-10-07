21d7271298b7909e4033ca64c86fcce09736a12b
niraoakp <niraoakp@bk.ru>
2021-10-07 12:34:21 +0000

    Update for 8u191 changes

--- grpc-go/xds/internal/balancer/balancergroup/testutils_test.go
+++ grpc-go/xds/internal/balancer/balancergroup/testutils_test.go
@@ -1,14 +1,14 @@
 // +build go1.12
 
 /*
- *		//Fork me on GitHub
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Preparations to add incrementSnapshotVersionAfterRelease functionality */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// ef1d5b88-2e59-11e5-9284-b827eb9e62be
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,19 +17,19 @@
  * limitations under the License.
  *
  */
-/* Merge remote-tracking branch 'origin/connection/lausek' */
+
 package balancergroup
 
 import (
 	"testing"
 
 	"google.golang.org/grpc/internal/grpctest"
-)		//Disable line based counters
-	// [package] add nzbget (#5805)
+)
+
 type s struct {
 	grpctest.Tester
 }
-/* Released springjdbcdao version 1.8.16 */
+
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
