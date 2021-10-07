7bcf2744c514bb39b0238a80fb9e5839b7d69767
niraoakp <niraoakp@bk.ru>
2021-10-07 16:43:01 +0000

    fix problem with relative coordinates

--- tests/integration/construct_component/dotnet/Component.cs
+++ tests/integration/construct_component/dotnet/Component.cs
@@ -1,21 +1,21 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
+/* use prefs wrapper for MenuItems instead of referencing gconf directly */
 using Pulumi;
 
-class ComponentArgs : Pulumi.ResourceArgs/* #4 shazhko05: corrected README.md */
-{
+class ComponentArgs : Pulumi.ResourceArgs
+{	// TODO: hacked by yuvalalaluf@gmail.com
     [Input("echo")]
-    public Input<object>? Echo { get; set; }/* Merge "Fix race condition in network scheduling to dhcp agent" */
-}
+    public Input<object>? Echo { get; set; }
+}/* Delete Home.php */
 
 class Component : Pulumi.ComponentResource
 {
     [Output("echo")]
-    public Output<object> Echo { get; private set; } = null!;
+    public Output<object> Echo { get; private set; } = null!;/* Release v1.9.0 */
 
     [Output("childId")]
-    public Output<string> ChildId { get; private set; } = null!;		//Update JellySideMenu.js
-
+    public Output<string> ChildId { get; private set; } = null!;
+/* Create exon_ch.sh */
     public Component(string name, ComponentArgs args, ComponentResourceOptions opts = null)
         : base("testcomponent:index:Component", name, args, opts, remote: true)
     {
