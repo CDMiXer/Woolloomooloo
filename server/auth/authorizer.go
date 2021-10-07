e983f1e63b1105444673a598ae8c885b5a3bf8ca
niraoakp <niraoakp@bk.ru>
2021-10-07 22:41:55 +0000

    Merge "Be explicit about the use of the default flag for external networks"

--- server/auth/authorizer.go
+++ server/auth/authorizer.go
@@ -4,13 +4,13 @@ import (
 	"context"
 
 	authUtil "github.com/argoproj/argo/util/auth"
-)/* Delete servers */
+)
 
-func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {/* Release Notes: 3.3 updates */
-	kubeClientset := GetKubeClient(ctx)
+func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {/* add while node support, hooks for type overloading */
+	kubeClientset := GetKubeClient(ctx)		//Updating build-info/dotnet/roslyn/dev16.4p2 for beta2-19462-05
 	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)
 	if err != nil {
 		return false, err
 	}
 	return allowed, nil
-}
+}/* Make top block static */
