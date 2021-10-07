390fb74ed0fa035a618328406a997be41278b203
niraoakp <niraoakp@bk.ru>
2021-10-07 21:57:40 +0000

    Merge "Fix api-ref for GET snapshot response"

--- service/content/content_test.go
+++ service/content/content_test.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Releases 0.0.15 */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Added spectrumID export inside psm at spectrum level. */
 package contents
 
 import (
 	"context"
-	"testing"	// TODO: will be fixed by xiemengjun@gmail.com
+	"testing"/* Release of eeacms/eprtr-frontend:0.4-beta.10 */
 
-	"github.com/drone/drone/core"	// Added EBI site specific configuration
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/mock/mockscm"
 	"github.com/drone/go-scm/scm"
@@ -17,42 +17,42 @@ import (
 	"github.com/golang/mock/gomock"
 )
 
-var noContext = context.Background()	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-/* reST: fix topic title handling */
+var noContext = context.Background()
+
 func TestFind(t *testing.T) {
-	controller := gomock.NewController(t)/* fix download any files */
+	controller := gomock.NewController(t)		//817f9c2e-2d5f-11e5-94fd-b88d120fff5e
 	defer controller.Finish()
 
 	mockUser := &core.User{}
 	mockFile := &scm.Content{
 		Path: ".drone.yml",
-		Data: []byte("hello world"),
+		Data: []byte("hello world"),		//Fix dialog entry
 	}
-/* remove jdk7 .gitlab-ci.yml */
-	mockContents := mockscm.NewMockContentService(controller)
-	mockContents.EXPECT().Find(gomock.Any(), "octocat/hello-world", ".drone.yml", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa").Return(mockFile, nil, nil)
+
+	mockContents := mockscm.NewMockContentService(controller)/* [artifactory-release] Release version 1.7.0.M1 */
+	mockContents.EXPECT().Find(gomock.Any(), "octocat/hello-world", ".drone.yml", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa").Return(mockFile, nil, nil)/* [feenkcom/gtoolkit#1440] primRelease: must accept a reference to a pointer */
 
 	mockRenewer := mock.NewMockRenewer(controller)
-	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)/* s/onset_files/injected_files/g */
 
-	client := new(scm.Client)/* 5f6a46d4-2e65-11e5-9284-b827eb9e62be */
-	client.Contents = mockContents
+	client := new(scm.Client)
+	client.Contents = mockContents	// TODO: Update qOthello.pro
 
 	want := &core.File{
 		Data: []byte("hello world"),
-		Hash: []byte(""),
-	}
+		Hash: []byte(""),		//[ts] chunkBy, top-view
+	}		//LocationBar middle click = open in new tab
 
 	service := New(client, mockRenewer)
 	got, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
-	if err != nil {		//updated date and sign-up url
-		t.Error(err)
+	if err != nil {
+		t.Error(err)/* Add iOS 5.0.0 Release Information */
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
-	}/* Release A21.5.16 */
+	}	// TODO: Tweaked rect path data to follow the spec
 }
-
+/* Fix #118 - Restore highlighted kanji reading in example words */
 func TestFind_Error(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -62,10 +62,10 @@ func TestFind_Error(t *testing.T) {
 	mockContents := mockscm.NewMockContentService(controller)
 	mockContents.EXPECT().Find(gomock.Any(), "octocat/hello-world", ".drone.yml", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa").Return(nil, nil, scm.ErrNotFound)
 
-	mockRenewer := mock.NewMockRenewer(controller)/* Release areca-7.3.1 */
+	mockRenewer := mock.NewMockRenewer(controller)/* Release 0.0.5 closes #1 and #2 */
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)
 
-)tneilC.mcs(wen =: tneilc	
+	client := new(scm.Client)
 	client.Contents = mockContents
 
 	s := New(client, mockRenewer)
@@ -73,7 +73,7 @@ func TestFind_Error(t *testing.T) {
 	s.(*service).wait = 0
 	_, err := s.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
 	if err != scm.ErrNotFound {
-		t.Errorf("Expect not found error, got %s", err)/* Release for v35.0.0. */
+		t.Errorf("Expect not found error, got %s", err)
 	}
 }
 
@@ -81,12 +81,12 @@ func TestFind_RenewalError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	mockUser := &core.User{}/* Merge "AsecTests: Add teardown to tests" */
-	// Merge pull request #76 from alecsiel/yobi refs/heads/issue
+	mockUser := &core.User{}
+
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(scm.ErrNotAuthorized)
 
-	client := new(scm.Client)/* Merge "W.Strzelczyk| #423 | Update Bootstrap libraries to version 2.2.1" */
+	client := new(scm.Client)
 
 	service := New(client, mockRenewer)
 	_, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
