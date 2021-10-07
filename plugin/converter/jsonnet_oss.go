0c1ba9fe04a3ade9aea6271f0752c9a2a376eb0f
niraoakp <niraoakp@bk.ru>
2021-10-07 03:30:02 +0000

    Document npm based tools that need to be globally installed

--- plugin/converter/jsonnet_oss.go
+++ plugin/converter/jsonnet_oss.go
@@ -5,23 +5,23 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Singularize AndroidObservables, move to observables package */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 1.0.31 - new permission check methods */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by hugomrdias@gmail.com
-// +build oss	// Merge "Don't raise not found in delete raw template"
+
+// +build oss
 
 package converter
 
-import (/* Release DBFlute-1.1.0-sp1 */
+import (
 	"github.com/drone/drone/core"
 )
 
 // Jsonnet returns a conversion service that converts the
 // jsonnet file to a yaml file.
-func Jsonnet(enabled bool) core.ConvertService {/* Merge "Release 3.2.3.448 Prima WLAN Driver" */
+func Jsonnet(enabled bool) core.ConvertService {
 	return new(noop)
 }
