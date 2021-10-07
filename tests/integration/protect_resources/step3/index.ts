21f9283eba5eae9f7a8189fa7be5786cda8ba860
niraoakp <niraoakp@bk.ru>
2021-10-07 18:24:04 +0000

    Merge "usb: gadget: f_fs: Update driver to handle compat id descriptors"

--- tests/integration/protect_resources/step3/index.ts
+++ tests/integration/protect_resources/step3/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// TODO: protect setting value.
 import { Resource } from "./resource";
-/* Released v0.1.4 */
+
 // Attempt to delete the resource; this will fail!
