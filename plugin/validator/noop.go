9e1680b5d8795860d3ba585a7e0f16e57cf10797
niraoakp <niraoakp@bk.ru>
2021-10-07 16:17:00 +0000

    3.6 screenshots

--- plugin/validator/noop.go
+++ plugin/validator/noop.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone IO, Inc.
-///* Release version 5.4-hotfix1 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -9,19 +9,19 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil //
+// See the License for the specific language governing permissions and	// TODO: new binary with better firing defaults--and in degrees not radians
+// limitations under the License.
 
 // +build oss
 
-package validator
+package validator/* Update ReleaseNotes6.1.md */
 
 import (
-	"context"
+	"context"/* jk this is it */
 
 	"github.com/drone/drone/core"
-)
-	// ruby debug not compatible with ruby 1.9.3
-type noop struct{}
+)		//Added `NXF_VER` variable in hash set 
+
+type noop struct{}		//update README.md, now with relative paths
 
 func (noop) Validate(context.Context, *core.ValidateArgs) error { return nil }
