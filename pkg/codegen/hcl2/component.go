98578729e5027d8046ec3b32ea3372888ae280c4
niraoakp <niraoakp@bk.ru>
2021-10-07 22:33:26 +0000

    Add kapua-broker as link

--- pkg/codegen/hcl2/component.go
+++ pkg/codegen/hcl2/component.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Create segmentation.md
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* - Update header */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//eagerly unsubscribe
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,18 +16,18 @@ package hcl2
 
 import (
 	"github.com/hashicorp/hcl/v2/hclsyntax"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"	// comment fix 2 :D
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 )
 
 // Component represents a component definition in a program.
 //
-// TODO(pdg): implement/* Release 2.5.3 */
-type Component struct {		//url prefix
-	Syntax *hclsyntax.Block/* Update ebwebview.js */
+// TODO(pdg): implement
+type Component struct {
+	Syntax *hclsyntax.Block
 
 	InputTypes  map[string]model.Type
-	OutputTypes map[string]model.Type/* Log conversion error */
-	// TODO: Create sqlite.txt
+	OutputTypes map[string]model.Type
+
 	Children []*Resource
 	Locals   []*LocalVariable
-}/* Merge "Add machine Ids for FSM9xxx FFA and Surf" into android-msm-2.6.32 */
+}
