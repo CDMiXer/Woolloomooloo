9d3313fb80933e3bdd4d29ade7d4ca205030d28b
niraoakp <niraoakp@bk.ru>
2021-10-07 08:58:55 +0000

    software engineering daily

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.ts
@@ -1,6 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-		//generate autoload_classmap and add to module
+
 const logs = new aws.s3.Bucket("logs", {});
 const bucket = new aws.s3.Bucket("bucket", {loggings: [{
     targetBucket: logs.bucket,
