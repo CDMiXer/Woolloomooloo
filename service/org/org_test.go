2d0d2feac1f735e7dfd207c429bad9b9d9873885
niraoakp <niraoakp@bk.ru>
2021-10-07 22:52:30 +0000

    Improving DefaultControllerTest

--- service/org/org_test.go
+++ service/org/org_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: #5 test goal configures a default log directory
 
 package orgs
-
-import (
-	"context"
+		//Update pytest from 3.2.0 to 3.2.1
+import (/* [artifactory-release] Release version 3.1.7.RELEASE */
+	"context"	// TODO: will be fixed by nagydani@epointsystem.org
 	"testing"
 	"time"
 
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/mock/mockscm"
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/mock/mockscm"		//reverting the template to the old one
+	"github.com/drone/drone/core"/* Create 206. Reverse Linked List.js */
 	"github.com/drone/go-scm/scm"
 	"github.com/google/go-cmp/cmp"
 
@@ -24,32 +24,32 @@ func TestList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	checkToken := func(ctx context.Context, opts scm.ListOptions) {
-		got, ok := ctx.Value(scm.TokenKey{}).(*scm.Token)
+	checkToken := func(ctx context.Context, opts scm.ListOptions) {		//Created Macros (markdown)
+		got, ok := ctx.Value(scm.TokenKey{}).(*scm.Token)/* Fixed logo in ProfileDlg. */
 		if !ok {
 			t.Errorf("Expect token stored in context")
 			return
 		}
 		want := &scm.Token{
-			Token:   "755bb80e5b",
+			Token:   "755bb80e5b",/* Eggdrop v1.8.0 Release Candidate 2 */
 			Refresh: "e08f3fa43e",
 			Expires: time.Unix(1532292869, 0),
 		}
-		if diff := cmp.Diff(got, want); diff != "" {
-			t.Errorf(diff)
-		}
+		if diff := cmp.Diff(got, want); diff != "" {/* Merge 92885. */
+			t.Errorf(diff)	// TODO: will be fixed by zaq1tomo@gmail.com
+		}	// TODO: Changes to the final output
 		if got, want := opts.Size, 100; got != want {
 			t.Errorf("Want page size %d, got %d", want, got)
 		}
 		if got, want := opts.Page, 0; got != want {
-			t.Errorf("Want page number %d, got %d", want, got)
+			t.Errorf("Want page number %d, got %d", want, got)/* Delete Release-Notes.md */
 		}
 	}
 
 	mockUser := &core.User{
-		Login:   "octocat",
-		Token:   "755bb80e5b",
-		Refresh: "e08f3fa43e",
+		Login:   "octocat",/* returning error messages */
+		Token:   "755bb80e5b",	// Got rid of 'You have pending...' message
+		Refresh: "e08f3fa43e",/* setup.py: Fix author and author_email. */
 		Expiry:  1532292869,
 	}
 	mockOrgs := []*scm.Organization{
