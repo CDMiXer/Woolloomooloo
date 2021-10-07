47eddfaabaf7cd1d16f462f55b8f444de085fb94
niraoakp <niraoakp@bk.ru>
2021-10-07 17:05:49 +0000

    Add references to [[Special:Random]] bug

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -2,32 +2,32 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
 // Create a new security group for port 80.
-const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{	// TODO: will be fixed by cory@protocol.ai
+const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
     protocol: "tcp",
     fromPort: 0,
     toPort: 0,
-    cidrBlocks: ["0.0.0.0/0"],	// TODO: will be fixed by boringland@protonmail.ch
+    cidrBlocks: ["0.0.0.0/0"],
 }]});
 const ami = aws.getAmi({
-    filters: [{/* Delete ss2.png */
+    filters: [{
         name: "name",
         values: ["amzn-ami-hvm-*-x86_64-ebs"],
     }],
     owners: ["137112412989"],
     mostRecent: true,
-});		//add level to organization
-// Create a simple web server using the startup script for the instance.	// TODO: hacked by cory@protocol.ai
+});
+// Create a simple web server using the startup script for the instance.
 const server = new aws.ec2.Instance("server", {
-    tags: {/* Delete c0000.min.topojson */
+    tags: {
         Name: "web-server-www",
-    },	// TODO: hacked by 13860583249@yeah.net
+    },
     instanceType: "t2.micro",
     securityGroups: [securityGroup.name],
     ami: ami.then(ami => ami.id),
     userData: `#!/bin/bash
-echo "Hello, World!" > index.html	// TODO: hacked by julia@jvns.ca
+echo "Hello, World!" > index.html
 nohup python -m SimpleHTTPServer 80 &
 `,
-});/* bootstrap example */
+});
 export const publicIp = server.publicIp;
-export const publicHostName = server.publicDns;		//Merge "Break retry loop on success in dhcp-all-interfaces"
+export const publicHostName = server.publicDns;
