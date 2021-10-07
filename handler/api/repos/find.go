364b738a622daf98a11aa8bda160a03f34642d7b
niraoakp <niraoakp@bk.ru>
2021-10-07 14:37:35 +0000

    Update copyright year in help intro message

--- handler/api/repos/find.go
+++ handler/api/repos/find.go
@@ -8,9 +8,9 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// b2de5b38-2e43-11e5-9284-b827eb9e62be
+// See the License for the specific language governing permissions and		//Uptaded to CraftBukkit-1.5.2-R0.1
+// limitations under the License./* i jos malo */
 
 package repos
 
@@ -22,7 +22,7 @@ import (
 )
 
 // HandleFind returns an http.HandlerFunc that writes the
-// json-encoded repository details to the response body.
+// json-encoded repository details to the response body./* Deleted CtrlApp_2.0.5/Release/Control.obj */
 func HandleFind() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
