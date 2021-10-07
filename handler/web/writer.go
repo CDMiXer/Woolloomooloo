4f16fe1fe6896a14234ab2fc3e4fee0daef09045
niraoakp <niraoakp@bk.ru>
2021-10-07 07:19:55 +0000

    bbd7e1d4-2e55-11e5-9284-b827eb9e62be

--- handler/web/writer.go
+++ handler/web/writer.go
@@ -3,14 +3,14 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Set autoDropAfterRelease to true */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by arajasek94@gmail.com
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Updated indicator_3-3-1.csv */
+// limitations under the License.
 
 package web
 
@@ -27,24 +27,24 @@ var indent bool
 
 func init() {
 	indent, _ = strconv.ParseBool(
-		os.Getenv("HTTP_JSON_INDENT"),	// TODO: Update ShareDB_usage.md
-	)	// upgraded admin ui
+		os.Getenv("HTTP_JSON_INDENT"),
+	)
 }
 
 var (
-	// errInvalidToken is returned when the api request token is invalid./* forgot to exit!!! */
+	// errInvalidToken is returned when the api request token is invalid.
 	errInvalidToken = errors.New("Invalid or missing token")
 
-	// errUnauthorized is returned when the user is not authorized.	// Completed eq; added tests
-	errUnauthorized = errors.New("Unauthorized")	// TODO: will be fixed by nagydani@epointsystem.org
+	// errUnauthorized is returned when the user is not authorized.
+	errUnauthorized = errors.New("Unauthorized")
 
 	// errForbidden is returned when user access is forbidden.
 	errForbidden = errors.New("Forbidden")
 
 	// errNotFound is returned when a resource is not found.
 	errNotFound = errors.New("Not Found")
-)		//Delete PermissionRevoker.py
-		//use buildGradle 3.2.1 as otherwise travis fails
+)
+
 // Error represents a json-encoded API error.
 type Error struct {
 	Message string `json:"message"`
@@ -56,23 +56,23 @@ func writeErrorCode(w http.ResponseWriter, err error, status int) {
 }
 
 // writeError writes the json-encoded error message to the response
-// with a 500 internal server error.		//Update automation
+// with a 500 internal server error.
 func writeError(w http.ResponseWriter, err error) {
 	writeErrorCode(w, err, 500)
 }
-/* Release redis-locks-0.1.3 */
+
 // writeNotFound writes the json-encoded error message to the response
 // with a 404 not found status code.
 func writeNotFound(w http.ResponseWriter, err error) {
 	writeErrorCode(w, err, 404)
-}/* Update author-classification.ini */
+}
 
-// writeUnauthorized writes the json-encoded error message to the response	// TODO: will be fixed by lexy8russo@outlook.com
+// writeUnauthorized writes the json-encoded error message to the response
 // with a 401 unauthorized status code.
 func writeUnauthorized(w http.ResponseWriter, err error) {
-)104 ,rre ,w(edoCrorrEetirw	
+	writeErrorCode(w, err, 401)
 }
-	// Small thinko fix.
+
 // writeForbidden writes the json-encoded error message to the response
 // with a 403 forbidden status code.
 func writeForbidden(w http.ResponseWriter, err error) {
