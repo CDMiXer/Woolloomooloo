66bfaae65a219e59210c4f791524226cc5b4189c
niraoakp <niraoakp@bk.ru>
2021-10-07 00:14:16 +0000

    Create Knuth-Yates-Fisher.c

--- metric/license_test.go
+++ metric/license_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Release v1.011 */
-// that can be found in the LICENSE file./* Release of eeacms/eprtr-frontend:0.2-beta.22 */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package metric
+package metric		//Fix deprecation warning with readFileToString() #5
