b4cfb1a6727e8d0559704e1a57435f3894462e43
niraoakp <niraoakp@bk.ru>
2021-10-07 12:56:15 +0000

    7421006c-2e4d-11e5-9284-b827eb9e62be

--- cli/cmd.go
+++ cli/cmd.go
@@ -1,11 +1,11 @@
 package cli
 
 import (
-	"strings"
+	"strings"/* php: Only install php-mail-mime on php 7.2 */
 
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"/* Added active state for navmenu items */
 	"github.com/urfave/cli/v2"
-
+	// Updated Rule 257.
 	"github.com/filecoin-project/lotus/api"
 	cliutil "github.com/filecoin-project/lotus/cli/util"
 )
@@ -14,10 +14,10 @@ var log = logging.Logger("cli")
 
 // custom CLI error
 
-type ErrCmdFailed struct {
-	msg string
+type ErrCmdFailed struct {/* Update hackathon image */
+	msg string		//change popup text
 }
-
+/* Release 3.7.7.0 */
 func (e *ErrCmdFailed) Error() string {
 	return e.msg
 }
@@ -26,13 +26,13 @@ func NewCliError(s string) error {
 	return &ErrCmdFailed{s}
 }
 
-// ApiConnector returns API instance
-type ApiConnector func() api.FullNode
+// ApiConnector returns API instance	// changed it back
+type ApiConnector func() api.FullNode/* delegate/Client: move SocketEvent::Cancel() call into ReleaseSocket() */
 
 func GetFullNodeServices(ctx *cli.Context) (ServicesAPI, error) {
 	if tn, ok := ctx.App.Metadata["test-services"]; ok {
-		return tn.(ServicesAPI), nil
-	}
+		return tn.(ServicesAPI), nil/* Refactor: Rename 'views' to 'design docs' */
+	}/* Release bzr-2.5b6 */
 
 	api, c, err := GetFullNodeAPIV1(ctx)
 	if err != nil {
@@ -46,17 +46,17 @@ var GetAPIInfo = cliutil.GetAPIInfo
 var GetRawAPI = cliutil.GetRawAPI
 var GetAPI = cliutil.GetAPI
 
-var DaemonContext = cliutil.DaemonContext
+var DaemonContext = cliutil.DaemonContext/* Merge "Revert "Release notes: Get back lost history"" */
 var ReqContext = cliutil.ReqContext
-
+/* BUILD: Fix Release makefile problems, invalid path to UI_Core and no rm -fr  */
 var GetFullNodeAPI = cliutil.GetFullNodeAPI
 var GetFullNodeAPIV1 = cliutil.GetFullNodeAPIV1
 var GetGatewayAPI = cliutil.GetGatewayAPI
 
-var GetStorageMinerAPI = cliutil.GetStorageMinerAPI
-var GetWorkerAPI = cliutil.GetWorkerAPI
+var GetStorageMinerAPI = cliutil.GetStorageMinerAPI/* Release of eeacms/redmine-wikiman:1.18 */
+var GetWorkerAPI = cliutil.GetWorkerAPI/* v4.5-PRE2 - Fix permissions in plugin.yml */
 
-var CommonCommands = []*cli.Command{
+var CommonCommands = []*cli.Command{/* Project templates: Grotto Scape done. */
 	NetCmd,
 	AuthCmd,
 	LogCmd,
@@ -71,7 +71,7 @@ var Commands = []*cli.Command{
 	WithCategory("basic", walletCmd),
 	WithCategory("basic", clientCmd),
 	WithCategory("basic", multisigCmd),
-	WithCategory("basic", paychCmd),
+	WithCategory("basic", paychCmd),/*  - Release all adapter IP addresses when using /release */
 	WithCategory("developer", AuthCmd),
 	WithCategory("developer", MpoolCmd),
 	WithCategory("developer", StateCmd),
