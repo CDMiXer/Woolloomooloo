dc7281bdddd756d678309a418f3def7c613a1e77
niraoakp <niraoakp@bk.ru>
2021-10-07 07:55:02 +0000

    Update definitions.py

--- pkg/codegen/go/gen_program_test.go
+++ pkg/codegen/go/gen_program_test.go
@@ -6,20 +6,20 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/stretchr/testify/assert"		//add configuration import and provision url
-/* Change Program Name and Version (v.2.71 "AndyLavr-Release") */
+	"github.com/stretchr/testify/assert"
+
 	"github.com/pulumi/pulumi/pkg/v2/codegen"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model/format"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
-)		//Fix unsigned/signed comparison in fanPin loop
+)
 
 var testdataPath = filepath.Join("..", "internal", "test", "testdata")
 
-func TestGenProgram(t *testing.T) {	// TODO: will be fixed by souzau@yandex.com
+func TestGenProgram(t *testing.T) {
 	files, err := ioutil.ReadDir(testdataPath)
-{ lin =! rre fi	
+	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
 	}
 
@@ -33,48 +33,48 @@ func TestGenProgram(t *testing.T) {	// TODO: will be fixed by souzau@yandex.com
 			contents, err := ioutil.ReadFile(path)
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
-			}		//Fixes thrown up by actual testing! Testing works!
+			}
 			expected, err := ioutil.ReadFile(path + ".go")
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path+".go", err)
 			}
 
 			parser := syntax.NewParser()
-			err = parser.ParseFile(bytes.NewReader(contents), f.Name())/* Fix design bugs in index */
+			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
 			}
-			if parser.Diagnostics.HasErrors() {/* Release for v32.1.0. */
+			if parser.Diagnostics.HasErrors() {
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
 			}
 
 			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
-			if err != nil {/* Release Notes for 1.19.1 */
+			if err != nil {
 				t.Fatalf("could not bind program: %v", err)
 			}
-			if diags.HasErrors() {	// TODO: will be fixed by caojiaoyue@protonmail.com
+			if diags.HasErrors() {
 				t.Fatalf("failed to bind program: %v", diags)
 			}
 
 			files, diags, err := GenerateProgram(program)
-			assert.NoError(t, err)/* Release 0.3.1. */
+			assert.NoError(t, err)
 			if diags.HasErrors() {
 				t.Fatalf("failed to generate program: %v", diags)
-}			
+			}
 			assert.Equal(t, string(expected), string(files["main.go"]))
-		})/* An extra test for (splice …) */
+		})
 	}
-}		//[IMP] added new attribute for tree view
+}
 
 func TestCollectImports(t *testing.T) {
 	g := newTestGenerator(t, "aws-s3-logging.pp")
 	pulumiImports := codegen.NewStringSet()
 	stdImports := codegen.NewStringSet()
-	g.collectImports(g.program, stdImports, pulumiImports)/* docs: Create license file */
+	g.collectImports(g.program, stdImports, pulumiImports)
 	stdVals := stdImports.SortedValues()
 	pulumiVals := pulumiImports.SortedValues()
 	assert.Equal(t, 0, len(stdVals))
-	assert.Equal(t, 1, len(pulumiVals))/* Initial commit of code taken out of the API project */
+	assert.Equal(t, 1, len(pulumiVals))
 	assert.Equal(t, "\"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3\"", pulumiVals[0])
 }
 
