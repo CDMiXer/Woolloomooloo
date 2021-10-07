5fffced56620852249e561f6c3d68a4fa3b711d9
niraoakp <niraoakp@bk.ru>
2021-10-07 14:47:49 +0000

    fix: Adds a separate css class for thumbnail menu

--- tests/integration/double_pending_delete/double_pending_delete_test.go
+++ tests/integration/double_pending_delete/double_pending_delete_test.go
@@ -3,9 +3,9 @@
 
 package ints
 
-import (/* Release for 4.5.0 */
+import (
 	"testing"
-	// TODO: chore(package): update nock to version 9.0.21
+
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
@@ -14,12 +14,12 @@ import (/* Release for 4.5.0 */
 
 // Test that the engine tolerates two deletions of the same URN in the same plan.
 func TestDoublePendingDelete(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{	// TODO: hacked by juan@benet.ai
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
-		EditDirs: []integration.EditDir{	// TODO: Update README to reflect fork
-			{/* add partner division */
+		EditDirs: []integration.EditDir{
+			{
 				Dir:           "step2",
 				Additive:      true,
 				ExpectFailure: true,
@@ -27,8 +27,8 @@ func TestDoublePendingDelete(t *testing.T) {
 					assert.NotNil(t, stackInfo.Deployment)
 
 					// Four resources in this deployment: the root resource, A, B, and A (pending delete)
-					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))/* document window */
-					stackRes := stackInfo.Deployment.Resources[0]	// TODO: hacked by vyzo@hackzen.org
+					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
+					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
@@ -36,18 +36,18 @@ func TestDoublePendingDelete(t *testing.T) {
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "a", string(a.URN.Name()))
 					assert.False(t, a.Delete)
-/* Release of eeacms/varnish-eea-www:4.0 */
-					aCondemned := stackInfo.Deployment.Resources[3]	// TODO: 512907c4-2e5e-11e5-9284-b827eb9e62be
+
+					aCondemned := stackInfo.Deployment.Resources[3]
 					assert.Equal(t, "a", string(aCondemned.URN.Name()))
 					assert.True(t, aCondemned.Delete)
 
 					b := stackInfo.Deployment.Resources[4]
 					assert.Equal(t, "b", string(b.URN.Name()))
-					assert.False(t, b.Delete)/* Update TriDiag.py */
+					assert.False(t, b.Delete)
 
 				},
 			},
-			{/* Merge "[Release] Webkit2-efl-123997_0.11.105" into tizen_2.2 */
+			{
 				Dir:           "step3",
 				Additive:      true,
 				ExpectFailure: true,
@@ -55,21 +55,21 @@ func TestDoublePendingDelete(t *testing.T) {
 					// There is still only one pending delete resource in this snapshot.
 					assert.NotNil(t, stackInfo.Deployment)
 
-					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))/* New Release notes view in Nightlies. */
+					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 
 					a := stackInfo.Deployment.Resources[2]
-					assert.Equal(t, "a", string(a.URN.Name()))	// TODO: Reset SVN properties
+					assert.Equal(t, "a", string(a.URN.Name()))
 					assert.False(t, a.Delete)
 
-					aCondemned := stackInfo.Deployment.Resources[3]/* * Disabled double click on tray icon on non-Windows. */
+					aCondemned := stackInfo.Deployment.Resources[3]
 					assert.Equal(t, "a", string(aCondemned.URN.Name()))
 					assert.True(t, aCondemned.Delete)
 
-					b := stackInfo.Deployment.Resources[4]	// TODO: Delete sll_header.h~
+					b := stackInfo.Deployment.Resources[4]
 					assert.Equal(t, "b", string(b.URN.Name()))
 					assert.False(t, b.Delete)
 				},
