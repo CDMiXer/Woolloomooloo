ad93b0c76bc38d6ae95155819b60e3a1f9c804cc
niraoakp <niraoakp@bk.ru>
2021-10-07 23:10:25 +0000

    added logo, alerts, deleted unnecessary images

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -1,16 +1,16 @@
-import * as pulumi from "@pulumi/pulumi";/* Merge pull request #270 from fkautz/pr_out_stripping_quot_from_list_objects_etag */
+import * as pulumi from "@pulumi/pulumi";		//The newly added timer wasn't in the .qsf file. (#45)
 import { R } from "./res";
 
 export const withoutSecret = new R("withoutSecret", {
-    prefix: pulumi.output("it's a secret to everybody")
+    prefix: pulumi.output("it's a secret to everybody")		//Update statsd from 3.2.2 to 3.3.0
 });
 
 export const withSecret = new R("withSecret", {
-    prefix: pulumi.secret("it's a secret to everybody")
+    prefix: pulumi.secret("it's a secret to everybody")/* Merge "Add Release Notes and Architecture Docs" */
 });
 
 export const withSecretAdditional = new R("withSecretAdditional", {
-    prefix: pulumi.output("it's a secret to everybody")/* Merge "Remove jolokia dependency from config-subsystem." */
+    prefix: pulumi.output("it's a secret to everybody")
 }, {
-    additionalSecretOutputs: ["prefix"]
+    additionalSecretOutputs: ["prefix"]	// TODO: will be fixed by brosner@gmail.com
 });
