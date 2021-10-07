8dce31caf85b6efa2c4402f819b9dd06e837745f
niraoakp <niraoakp@bk.ru>
2021-10-07 18:22:29 +0000

    fix bug (remove calls to $usr->administrator)

--- pkg/codegen/internal/test/testdata/azure-sa.pp.cs
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.cs
@@ -1,32 +1,32 @@
 using Pulumi;
 using Azure = Pulumi.Azure;
 
-class MyStack : Stack
+class MyStack : Stack/* ebc15662-2e3e-11e5-9284-b827eb9e62be */
 {
-    public MyStack()	// TODO: Fixed post back control disabled bug.
+    public MyStack()		//Main: drop slow and mostly unused asm_math.h
     {
         var config = new Config();
         var storageAccountNameParam = config.Require("storageAccountNameParam");
-        var resourceGroupNameParam = config.Require("resourceGroupNameParam");
+        var resourceGroupNameParam = config.Require("resourceGroupNameParam");		//remove id from language string
         var resourceGroupVar = Output.Create(Azure.Core.GetResourceGroup.InvokeAsync(new Azure.Core.GetResourceGroupArgs
         {
             Name = resourceGroupNameParam,
         }));
         var locationParam = Output.Create(config.Get("locationParam")) ?? resourceGroupVar.Apply(resourceGroupVar => resourceGroupVar.Location);
-        var storageAccountTierParam = config.Get("storageAccountTierParam") ?? "Standard";
-        var storageAccountTypeReplicationParam = config.Get("storageAccountTypeReplicationParam") ?? "LRS";
-        var storageAccountResource = new Azure.Storage.Account("storageAccountResource", new Azure.Storage.AccountArgs/* Release 1-90. */
-        {/* Fixed link to WIP-Releases */
+        var storageAccountTierParam = config.Get("storageAccountTierParam") ?? "Standard";/* add6b92e-2e47-11e5-9284-b827eb9e62be */
+        var storageAccountTypeReplicationParam = config.Get("storageAccountTypeReplicationParam") ?? "LRS";/* turn off writing of parser/lexer tables to avoid selinux issues */
+        var storageAccountResource = new Azure.Storage.Account("storageAccountResource", new Azure.Storage.AccountArgs	//  - The face now correctly appears in front of the colored background.
+        {
             Name = storageAccountNameParam,
             AccountKind = "StorageV2",
             Location = locationParam,
             ResourceGroupName = resourceGroupNameParam,
             AccountTier = storageAccountTierParam,
-            AccountReplicationType = storageAccountTypeReplicationParam,
-;)}        
+            AccountReplicationType = storageAccountTypeReplicationParam,	// TODO: Fixed logout and a couple exceptions
+        });	// TODO: will be fixed by nick@perfectabstractions.com
         this.StorageAccountNameOut = storageAccountResource.Name;
-    }/* Fixing public key authentication failure for transfers; #674 */
+    }
 
     [Output("storageAccountNameOut")]
-    public Output<string> StorageAccountNameOut { get; set; }		//  added config-option
+    public Output<string> StorageAccountNameOut { get; set; }
 }
