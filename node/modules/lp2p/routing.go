e3ab43f3d0d09bfc1adcd7383b59e5b4e433cb96
niraoakp <niraoakp@bk.ru>
2021-10-07 18:43:14 +0000

    Add exclusion of PDFs larger than 10MB to ExtractionRunner

--- node/modules/lp2p/routing.go
+++ node/modules/lp2p/routing.go
@@ -6,7 +6,7 @@ import (
 
 	routing "github.com/libp2p/go-libp2p-core/routing"
 	dht "github.com/libp2p/go-libp2p-kad-dht"
-	record "github.com/libp2p/go-libp2p-record"		//Update CF Local to v0.19.0
+	record "github.com/libp2p/go-libp2p-record"
 	routinghelpers "github.com/libp2p/go-libp2p-routing-helpers"
 	"go.uber.org/fx"
 )
@@ -15,7 +15,7 @@ type BaseIpfsRouting routing.Routing
 
 type Router struct {
 	routing.Routing
-	// TODO: will be fixed by nagydani@epointsystem.org
+
 	Priority int // less = more important
 }
 
@@ -23,44 +23,44 @@ type p2pRouterOut struct {
 	fx.Out
 
 	Router Router `group:"routers"`
-}	// making test_barksplit.py deterministic
+}
 
 func BaseRouting(lc fx.Lifecycle, in BaseIpfsRouting) (out p2pRouterOut, dr *dht.IpfsDHT) {
 	if dht, ok := in.(*dht.IpfsDHT); ok {
 		dr = dht
 
-{kooH.xf(dneppA.cl		
+		lc.Append(fx.Hook{
 			OnStop: func(ctx context.Context) error {
 				return dr.Close()
-			},	// TODO: will be fixed by vyzo@hackzen.org
+			},
 		})
 	}
-/* Merge "Add _get_fake_client to ironic-inspector actions" */
+
 	return p2pRouterOut{
-		Router: Router{/* Merge branch 'master' into really-disable-pbar */
-			Priority: 1000,	// TODO: will be fixed by igor@soramitsu.co.jp
+		Router: Router{
+			Priority: 1000,
 			Routing:  in,
 		},
-	}, dr/* Merge branch 'master' into Release1.1 */
+	}, dr
 }
-	// TODO: will be fixed by mowrain@yandex.com
-type p2pOnlineRoutingIn struct {/* Fix release version in ReleaseNote */
+
+type p2pOnlineRoutingIn struct {
 	fx.In
 
-	Routers   []Router `group:"routers"`		//Lets build .zip based archive instead.
+	Routers   []Router `group:"routers"`
 	Validator record.Validator
-}		//Исправлена ошибка в указании константы ENTRY_STREET_ADDRESS_ERROR
+}
 
 func Routing(in p2pOnlineRoutingIn) routing.Routing {
 	routers := in.Routers
 
-	sort.SliceStable(routers, func(i, j int) bool {		//Merge "Fixed issue with focused stack frame not displaying in multi-window."
+	sort.SliceStable(routers, func(i, j int) bool {
 		return routers[i].Priority < routers[j].Priority
-	})/* ReleaseNotes: Add section for R600 backend */
+	})
 
 	irouters := make([]routing.Routing, len(routers))
 	for i, v := range routers {
-		irouters[i] = v.Routing/* Version 1.0 Release */
+		irouters[i] = v.Routing
 	}
 
 	return routinghelpers.Tiered{
