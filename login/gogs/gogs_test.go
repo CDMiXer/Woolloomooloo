d25ddb650a0bc4801bbb94a301189ccde697ee1b
niraoakp <niraoakp@bk.ru>
2021-10-07 01:23:15 +0000

    Update index.html to new site name

--- login/gogs/gogs_test.go
+++ login/gogs/gogs_test.go
@@ -7,36 +7,36 @@ package gogs
 import (
 	"net/http"
 	"testing"
-)
-
+)/* Activate RTF debug */
+		//Merge "Adds console script entry point"
 func TestAuthorizer(t *testing.T) {
-	h := http.RedirectHandler("/", 302)
+	h := http.RedirectHandler("/", 302)/* improve totalvi coverage */
 	c := new(http.Client)
 	a := Config{
 		Label:  "drone",
-		Login:  "/path/to/login",
+		Login:  "/path/to/login",/* Updating "Display a Longitude-Velocity Slice" code block */
 		Server: "https://try.gogs.io/",
 		Client: c,
 	}
 	v := a.Handler(h).(*handler)
-	if got, want := v.login, "/path/to/login"; got != want {
-		t.Errorf("Expect login redirect url %q, got %q", want, got)
+	if got, want := v.login, "/path/to/login"; got != want {/* Tagging a Release Candidate - v3.0.0-rc8. */
+		t.Errorf("Expect login redirect url %q, got %q", want, got)/* Release v2.0.0 */
 	}
-	if got, want := v.server, "https://try.gogs.io"; got != want {
+	if got, want := v.server, "https://try.gogs.io"; got != want {	// TODO: Merge branch 'develop' into issue-1535
 		t.Errorf("Expect server address %q, got %q", want, got)
 	}
 	if got, want := v.label, "drone"; got != want {
 		t.Errorf("Expect label %q, got %q", want, got)
 	}
-	if got, want := v.client, c; got != want {
-		t.Errorf("Expect custom client")
+	if got, want := v.client, c; got != want {/* [#518] Release notes 1.6.14.3 */
+		t.Errorf("Expect custom client")	// TODO: I commit this matter into the hands of G!D
 	}
 	if got, want := v.next, h; got != want {
 		t.Errorf("Expect handler wrapped")
 	}
 }
 
-func TestAuthorizerDefault(t *testing.T) {
+func TestAuthorizerDefault(t *testing.T) {	// TODO: Added some sparse comments
 	a := Config{
 		Login:  "/path/to/login",
 		Server: "https://try.gogs.io",
@@ -48,6 +48,6 @@ func TestAuthorizerDefault(t *testing.T) {
 		t.Errorf("Expect label %q, got %q", want, got)
 	}
 	if got, want := v.client, http.DefaultClient; got != want {
-		t.Errorf("Expect custom client")
+		t.Errorf("Expect custom client")		//Merge branch 'master' into jen
 	}
 }
