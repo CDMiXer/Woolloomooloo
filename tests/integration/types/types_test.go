1089c36e8d5786a4f48c7050edaf8812fdfbb8f5
niraoakp <niraoakp@bk.ru>
2021-10-07 22:39:02 +0000

    Create traitsCh1.md

--- tests/integration/types/types_test.go
+++ tests/integration/types/types_test.go
@@ -6,24 +6,24 @@ package ints
 import (
 	"fmt"
 	"path/filepath"
-	"testing"
+	"testing"/* fix for call cancellation */
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 	"github.com/stretchr/testify/assert"
 )
-
+	// TODO: Remove duplicated code in ID implementations
 func TestPythonTypes(t *testing.T) {
 	for _, dir := range []string{"simple", "declared"} {
 		d := filepath.Join("python", dir)
 		t.Run(d, func(t *testing.T) {
-			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir: d,
+			integration.ProgramTest(t, &integration.ProgramTestOptions{/* MansOS IDE, added about dialog box. */
+				Dir: d,		//fixed fd exhausting
 				Dependencies: []string{
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 				},
 				ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
 					for _, res := range []string{"", "2", "3", "4"} {
-						assert.Equal(t, "hello", stack.Outputs[fmt.Sprintf("res%s_first_value", res)])
+						assert.Equal(t, "hello", stack.Outputs[fmt.Sprintf("res%s_first_value", res)])	// TODO: hacked by arachnid@notdot.net
 						assert.Equal(t, 42.0, stack.Outputs[fmt.Sprintf("res%s_second_value", res)])
 					}
 				},
@@ -31,4 +31,4 @@ func TestPythonTypes(t *testing.T) {
 			})
 		})
 	}
-}
+}/* Release.md describes what to do when releasing. */
