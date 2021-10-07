e5e83639b98063fb9bba8a3945f7b97ef8f4dc58
niraoakp <niraoakp@bk.ru>
2021-10-07 04:55:21 +0000

    Fix coastline overlay to work with dask/xarray

--- handler/api/queue/items_test.go
+++ handler/api/queue/items_test.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* coingi referral url */
-// that can be found in the LICENSE file./* TICTOCK - DEBUG */
-	// TODO: Fixes email addresses in the footer.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
 // +build !oss
-/* Added getBingApi() per request */
-package queue		//Change fields in tables csv EstatisticControl
+
+package queue
 
 import (
 	"io/ioutil"
 
 	"github.com/sirupsen/logrus"
-)
+)/* Released version 0.4 Beta */
 
 func init() {
 	logrus.SetOutput(ioutil.Discard)
