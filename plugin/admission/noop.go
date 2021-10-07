8efe6c63315ec413fc709d8e4ceb8d9efeba360c
niraoakp <niraoakp@bk.ru>
2021-10-07 16:45:58 +0000

    255b97d0-2e6b-11e5-9284-b827eb9e62be

--- plugin/admission/noop.go
+++ plugin/admission/noop.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Py2exeGUI First Release */
-// You may obtain a copy of the License at/* [#83] CHANGES.md updated */
-//		//Added adoptees client
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by joshua@yottadb.com
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by ng8eke@163.com
+
 package admission
 
-import (/* to expire legacy cdn cache for a logo image */
+import (
 	"context"
-	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+
 	"github.com/drone/drone/core"
 )
 
 // noop is a stub admission controller.
-type noop struct{}/* Release the GIL in RMA calls */
+type noop struct{}
 
-func (noop) Admit(context.Context, *core.User) error {		//Merge "Hygiene: remove duplicate code in ListCardView"
-	return nil/* Release version: 0.1.7 */
+func (noop) Admit(context.Context, *core.User) error {
+	return nil
 }
