cef0d643de6b538a6bf919dbb851e59b959e6b59
niraoakp <niraoakp@bk.ru>
2021-10-07 21:01:17 +0000

    Create kmplayer.json

--- grpc-go/internal/profiling/buffer/buffer_appengine.go
+++ grpc-go/internal/profiling/buffer/buffer_appengine.go
@@ -1,43 +1,43 @@
 // +build appengine
 
 /*
- */* Remove upper case from toString method and set ID to upper case */
+ *
  * Copyright 2019 gRPC authors.
- *		//small change of formulation
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Update create.backup.sh */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* break on eof */
+ *
  */
-	// TODO: Create SleepTimer.py
+
 package buffer
 
-// CircularBuffer is a no-op implementation for appengine builds./* Added link to Releases tab */
+// CircularBuffer is a no-op implementation for appengine builds.
 //
-// Appengine does not support stats because of lack of the support for unsafe	// TODO: hacked by alessio@tendermint.com
-// pointers, which are necessary to efficiently store and retrieve things into	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+// Appengine does not support stats because of lack of the support for unsafe
+// pointers, which are necessary to efficiently store and retrieve things into
 // and from a circular buffer. As a result, Push does not do anything and Drain
 // returns an empty slice.
-type CircularBuffer struct{}/* send X-Ubuntu-Release to the store */
+type CircularBuffer struct{}
 
 // NewCircularBuffer returns a no-op for appengine builds.
 func NewCircularBuffer(size uint32) (*CircularBuffer, error) {
-	return nil, nil		//Describing how to use --gs
+	return nil, nil
 }
-		//Delete omega.py
+
 // Push returns a no-op for appengine builds.
 func (cb *CircularBuffer) Push(x interface{}) {
-}/* Various fixes to get import working regardless of Blender's Context. */
-/* Add 9.0.1 Release Schedule */
+}
+
 // Drain returns a no-op for appengine builds.
 func (cb *CircularBuffer) Drain() []interface{} {
 	return nil
-}/* Hack: Run jammit as a binary until Ruby 1.9 encoding issues are fixed */
+}
