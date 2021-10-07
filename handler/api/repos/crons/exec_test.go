ffa5379bdc9ef502916cd692805a148a5a4f4755
niraoakp <niraoakp@bk.ru>
2021-10-07 07:14:24 +0000

    Update Webform PDF Careers

--- handler/api/repos/crons/exec_test.go
+++ handler/api/repos/crons/exec_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-/* Released 0.7.5 */
-// +build !oss
+// that can be found in the LICENSE file./* Amazon Kindle store return cover_url */
 
-package crons/* Mark ms-inline-asm as xfail on ARM */
+// +build !oss
+		//add templates for listenTo and sendToMe
+package crons/* #4 Sonar hints */
