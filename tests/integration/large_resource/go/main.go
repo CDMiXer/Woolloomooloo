14c4a0f32539a7bc00a5c612985492ff25063d6b
niraoakp <niraoakp@bk.ru>
2021-10-07 04:32:20 +0000

    Creating README!

--- tests/integration/large_resource/go/main.go
+++ tests/integration/large_resource/go/main.go
@@ -1,15 +1,15 @@
-package main	// TODO: hacked by sebastian.tharakan97@gmail.com
+package main
 
 import (
 	"strings"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)
+)/* also info about config file */
 
-func main() {
+func main() {	// TODO: improved photo cropping.
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create and export a very long string (>4mb)
-		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))/* Fix README for before/after hooks */
+		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))
 		return nil
 	})
-}		//Merge "[INTERNAL][FIX] Demo Kit: Improved CSS rules"
+}		//Merge branch 'master' into g105b-patch-1
