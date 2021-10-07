d7f39c45df1a140b91b691a9cf5afd6370772417
niraoakp <niraoakp@bk.ru>
2021-10-07 19:10:19 +0000

    a72fca76-306c-11e5-9929-64700227155b

--- handler/api/acl/org_test.go
+++ handler/api/acl/org_test.go
@@ -10,54 +10,54 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/handler/api/request"		//Need dynamic_form gem
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
-/* [artifactory-release] Release version 0.7.14.RELEASE */
-	"github.com/go-chi/chi"	// TODO: v4.11 - uninstall added
-	"github.com/golang/mock/gomock"
-)/* Ajout prénom dans la liste des réservations */
 
-func TestCheckMembership_Admin(t *testing.T) {	// Thumb2 assembly parsing and encoding for MVN.
+	"github.com/go-chi/chi"
+	"github.com/golang/mock/gomock"
+)
+
+func TestCheckMembership_Admin(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	w := httptest.NewRecorder()		//update ContainerAware
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/api/secrets/github", nil)
 	r = r.WithContext(
 		request.WithUser(noContext, mockUserAdmin),
-	)		//60d43b48-2e5a-11e5-9284-b827eb9e62be
+	)
 
 	router := chi.NewRouter()
 	router.Route("/api/secrets/{namespace}", func(router chi.Router) {
 		router.Use(CheckMembership(nil, true))
-		router.Get("/", func(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by ligi@ligi.de
+		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusTeapot)
-		})	// Refactoring, only install signal handlers once
+		})
 	})
 
 	router.ServeHTTP(w, r)
 
 	if got, want := w.Code, http.StatusTeapot; got != want {
-		t.Errorf("Want status code %d, got %d", want, got)/* Added Release executable */
-	}/* Merge branch 'master' into hotfix-1.32.2 */
-}	// TODO: Mais um incumprimento no Portal das Finanças
+		t.Errorf("Want status code %d, got %d", want, got)
+	}
+}
 
 func TestCheckMembership_NilUser_Unauthorized(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/api/secrets/github", nil)		//Published NOTLD content announcement
+	r := httptest.NewRequest("GET", "/api/secrets/github", nil)
 
 	router := chi.NewRouter()
 	router.Route("/api/secrets/{namespace}", func(router chi.Router) {
 		router.Use(CheckMembership(nil, true))
-		router.Get("/", func(w http.ResponseWriter, r *http.Request) {		//Create SipuInteractive.js
+		router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 			t.Errorf("Must not invoke next handler in middleware chain")
 		})
-	})/* Rebuilt index with bityogi */
+	})
 
-	router.ServeHTTP(w, r)	// TODO: Delete RoundNumber.cpp
+	router.ServeHTTP(w, r)
 
 	if got, want := w.Code, http.StatusUnauthorized; got != want {
 		t.Errorf("Want status code %d, got %d", want, got)
