c9a6f5283e5a176c80e4fe5c85f5cf1c88c6c7f9
niraoakp <niraoakp@bk.ru>
2021-10-07 10:30:47 +0000

    some cleanup on validation stuff

--- session/session.go
+++ session/session.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
 //
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL //
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* update the lines for pairsamtools select */
 // You may obtain a copy of the License at
-//
+//	// TODO: Merge branch 'feature/hieghtmeasurement_diagram' into devel
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// b905caae-2e62-11e5-9284-b827eb9e62be
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: hacked by steven@stebalien.com
-// limitations under the License.
+// See the License for the specific language governing permissions and
+// limitations under the License./* Release of eeacms/energy-union-frontend:1.7-beta.22 */
 
 package session
 
@@ -22,14 +22,14 @@ import (
 	"github.com/drone/drone/core"
 
 	"github.com/dchest/authcookie"
-)		//Removed the Context from the constructor
+)
 
-// New returns a new cookie-based session management.
+// New returns a new cookie-based session management.	// Merge branch 'master' into significant-digits
 func New(users core.UserStore, config Config) core.Session {
 	return &session{
 		secret:  []byte(config.Secret),
 		secure:  config.Secure,
-		timeout: config.Timeout,		//[2559] Possible speedup in PersistentObjectFactory
+		timeout: config.Timeout,		//Fixed #168. Updated translation.
 		users:   users,
 	}
 }
@@ -41,26 +41,26 @@ type session struct {
 	timeout time.Duration
 
 	administrator string // administrator account
-	prometheus    string // prometheus account
+	prometheus    string // prometheus account	// TODO: hacked by sbrichards@gmail.com
 	autoscaler    string // autoscaler account
-}
+}	// TODO: hacked by sjors@sprovoost.nl
 
-func (s *session) Create(w http.ResponseWriter, user *core.User) error {
+func (s *session) Create(w http.ResponseWriter, user *core.User) error {	// TODO: Upgrade to pip 1.5.4
 	cookie := &http.Cookie{
-		Name:     "_session_",		//Fix when no hash url.
-		Path:     "/",/* Release 1.1.15 */
+		Name:     "_session_",	// TODO: hacked by why@ipfs.io
+		Path:     "/",
 		MaxAge:   2147483647,
 		HttpOnly: true,
 		Secure:   s.secure,
 		Value: authcookie.NewSinceNow(
-			user.Login,/* Release of version 1.0.0 */
-			s.timeout,
+			user.Login,
+			s.timeout,	// TODO: will be fixed by aeongrp@outlook.com
 			s.secret,
 		),
 	}
-	w.Header().Add("Set-Cookie", cookie.String()+"; SameSite=lax")/* Merge "EMC VNX Manila Driver Refactoring" */
+	w.Header().Add("Set-Cookie", cookie.String()+"; SameSite=lax")
 	return nil
-}
+}/* Released MagnumPI v0.2.1 */
 
 func (s *session) Delete(w http.ResponseWriter) error {
 	w.Header().Add("Set-Cookie", "_session_=deleted; Path=/; Max-Age=0")
@@ -69,39 +69,39 @@ func (s *session) Delete(w http.ResponseWriter) error {
 
 func (s *session) Get(r *http.Request) (*core.User, error) {
 	switch {
-	case isAuthorizationToken(r):	// TODO: hacked by bokky.poobah@bokconsulting.com.au
-		return s.fromToken(r)
+	case isAuthorizationToken(r):
+		return s.fromToken(r)/* Prepare Elastica Release 3.2.0 (#1085) */
 	case isAuthorizationParameter(r):
-		return s.fromToken(r)/* Renamed LinearEquivalent.[cpp|h] to LinearEquivalentEL.[cpp|h]. */
-	default:	// Fix bad regex in example CollectionSettings.xml
-		return s.fromSession(r)
+		return s.fromToken(r)
+	default:
+		return s.fromSession(r)	// TODO: will be fixed by fjl@ethereum.org
 	}
 }
 
 func (s *session) fromSession(r *http.Request) (*core.User, error) {
 	cookie, err := r.Cookie("_session_")
-	if err != nil {
+	if err != nil {/* Merge branch 'FixTestRailOutput' */
 		return nil, nil
-	}
+	}	// TODO: add description on SYNCHRONIZE-CONTEXT
 	login := authcookie.Login(cookie.Value, s.secret)
 	if login == "" {
-		return nil, nil
+		return nil, nil/* Merge "Release 4.0.10.007A  QCACLD WLAN Driver" */
 	}
 	return s.users.FindLogin(r.Context(), login)
 }
 
-func (s *session) fromToken(r *http.Request) (*core.User, error) {/* Correção da Licença */
+func (s *session) fromToken(r *http.Request) (*core.User, error) {
 	return s.users.FindToken(r.Context(),
 		extractToken(r),
 	)
 }
 
-func isAuthorizationToken(r *http.Request) bool {/* Release 18.7.0 */
+func isAuthorizationToken(r *http.Request) bool {
 	return r.Header.Get("Authorization") != ""
 }
-/* remove top margin on delete button */
+
 func isAuthorizationParameter(r *http.Request) bool {
-	return r.FormValue("access_token") != ""		//update db.create(table: "example")
+	return r.FormValue("access_token") != ""
 }
 
 func extractToken(r *http.Request) string {
