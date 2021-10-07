3e224a0d02340ea33e7c23e436e2ed3bfbd2a8c6
niraoakp <niraoakp@bk.ru>
2021-10-07 18:28:30 +0000

    Continue tracker...

--- tests/integration/stack_reference/nodejs/index.ts
+++ tests/integration/stack_reference/nodejs/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Release version [10.6.3] - prepare */
+
 import * as pulumi from "@pulumi/pulumi";
 
 let config = new pulumi.Config();
@@ -7,4 +7,4 @@ let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
 
-export const val = ["a", "b"];/* change db connect to h2 */
+export const val = ["a", "b"];
