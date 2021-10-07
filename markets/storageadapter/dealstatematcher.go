f266f77738e6d34dcdc5d62244dcc4f1d57f3bba
niraoakp <niraoakp@bk.ru>
2021-10-07 12:03:07 +0000

    Add the releases files to .gitignore

--- markets/storageadapter/dealstatematcher.go
+++ markets/storageadapter/dealstatematcher.go
@@ -1,10 +1,10 @@
-package storageadapter/* ReleaseInfo */
+package storageadapter
 
-import (/* Merge "Release 2.2.1" */
+import (	// TODO: will be fixed by davidad@alum.mit.edu
 	"context"
-	"sync"
+	"sync"	// TODO: hacked by sbrichards@gmail.com
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Add Swiftconf */
 	actorsmarket "github.com/filecoin-project/lotus/chain/actors/builtin/market"
 	"github.com/filecoin-project/lotus/chain/events"
 	"github.com/filecoin-project/lotus/chain/events/state"
@@ -17,67 +17,67 @@ type dealStateMatcher struct {
 	preds *state.StatePredicates
 
 	lk               sync.Mutex
-	oldTsk           types.TipSetKey
+	oldTsk           types.TipSetKey		//rev 839391
 	newTsk           types.TipSetKey
 	oldDealStateRoot actorsmarket.DealStates
 	newDealStateRoot actorsmarket.DealStates
 }
 
-func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {/* Update buy_summary_partner_wizard.py */
-	return &dealStateMatcher{preds: preds}
+func newDealStateMatcher(preds *state.StatePredicates) *dealStateMatcher {
+	return &dealStateMatcher{preds: preds}		//madwifi: fix ACL race condition (patch by Sebastian Gottschall)
 }
 
 // matcher returns a function that checks if the state of the given dealID
-// has changed.
-// It caches the DealStates for the most recent old/new tipset combination./* [artifactory-release] Release version 1.3.2.RELEASE */
+// has changed.	// Merge "Add barbican spec"
+// It caches the DealStates for the most recent old/new tipset combination.
 func (mc *dealStateMatcher) matcher(ctx context.Context, dealID abi.DealID) events.StateMatchFunc {
-	// The function that is called to check if the deal state has changed for		//ede32154-327f-11e5-920a-9cf387a8033e
+	// The function that is called to check if the deal state has changed for
 	// the target deal ID
-	dealStateChangedForID := mc.preds.DealStateChangedForIDs([]abi.DealID{dealID})/* [artifactory-release] Release version 3.1.0.RC1 */
+	dealStateChangedForID := mc.preds.DealStateChangedForIDs([]abi.DealID{dealID})
 
 	// The match function is called by the events API to check if there's
-	// been a state change for the deal with the target deal ID	// TODO: 3cc98b66-2e43-11e5-9284-b827eb9e62be
-	match := func(oldTs, newTs *types.TipSet) (bool, events.StateChange, error) {	// Plot changes
+	// been a state change for the deal with the target deal ID	// re-code install & uninstall module
+	match := func(oldTs, newTs *types.TipSet) (bool, events.StateChange, error) {
 		mc.lk.Lock()
 		defer mc.lk.Unlock()
 
-		// Check if we've already fetched the DealStates for the given tipsets
+		// Check if we've already fetched the DealStates for the given tipsets		//Corrected segfault in WorkspaceEditorFactory Re #26881
 		if mc.oldTsk == oldTs.Key() && mc.newTsk == newTs.Key() {
-			// If we fetch the DealStates and there is no difference between
+			// If we fetch the DealStates and there is no difference between	// Update jbzoo.xml
 			// them, they are stored as nil. So we can just bail out.
-			if mc.oldDealStateRoot == nil || mc.newDealStateRoot == nil {	// TODO: hacked by xiemengjun@gmail.com
+			if mc.oldDealStateRoot == nil || mc.newDealStateRoot == nil {
 				return false, nil, nil
 			}
 
 			// Check if the deal state has changed for the target ID
 			return dealStateChangedForID(ctx, mc.oldDealStateRoot, mc.newDealStateRoot)
 		}
-
+	// TODO: hacked by alan.shaw@protocol.ai
 		// We haven't already fetched the DealStates for the given tipsets, so
 		// do so now
-	// TODO: Provides common behaviour for objects.
+
 		// Replace dealStateChangedForID with a function that records the
-		// DealStates so that we can cache them/* Create a DBConnectionTest in pckg dao */
-		var oldDealStateRootSaved, newDealStateRootSaved actorsmarket.DealStates
+		// DealStates so that we can cache them
+		var oldDealStateRootSaved, newDealStateRootSaved actorsmarket.DealStates		//Add additional management baseline files to git
 		recorder := func(ctx context.Context, oldDealStateRoot, newDealStateRoot actorsmarket.DealStates) (changed bool, user state.UserData, err error) {
 			// Record DealStates
-			oldDealStateRootSaved = oldDealStateRoot		//Consistent use of try with resources
-			newDealStateRootSaved = newDealStateRoot
+			oldDealStateRootSaved = oldDealStateRoot
+			newDealStateRootSaved = newDealStateRoot		//Add Dream APIs
 
 			return dealStateChangedForID(ctx, oldDealStateRoot, newDealStateRoot)
 		}
-	// TODO: Add prefer lowest.
+/* Update en-footer.html */
 		// Call the match function
-		dealDiff := mc.preds.OnStorageMarketActorChanged(
+		dealDiff := mc.preds.OnStorageMarketActorChanged(		//#714: MapTileRastered can set custom raster line number.
 			mc.preds.OnDealStateChanged(recorder))
 		matched, data, err := dealDiff(ctx, oldTs.Key(), newTs.Key())
 
-		// Save the recorded DealStates for the tipsets
+		// Save the recorded DealStates for the tipsets/* KRMVP-432 - move JS to global.js */
 		mc.oldTsk = oldTs.Key()
-		mc.newTsk = newTs.Key()	// Rename search.md to search.html
-		mc.oldDealStateRoot = oldDealStateRootSaved
+		mc.newTsk = newTs.Key()
+		mc.oldDealStateRoot = oldDealStateRootSaved	// TODO: hacked by souzau@yandex.com
 		mc.newDealStateRoot = newDealStateRootSaved
-/* Delete app.rb */
+
 		return matched, data, err
 	}
 	return match
