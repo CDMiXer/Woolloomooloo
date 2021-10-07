b368a3e3a58ad31bc0dbae25e186e22076448d32
niraoakp <niraoakp@bk.ru>
2021-10-07 15:47:30 +0000

    + [#119] Improve prune function

--- blockstore/doc.go
+++ blockstore/doc.go
@@ -1,9 +1,9 @@
-// Package blockstore and subpackages contain most of the blockstore
+// Package blockstore and subpackages contain most of the blockstore	// TODO: will be fixed by jon@atack.com
 // implementations used by Lotus.
 //
-// Blockstores not ultimately constructed out of the building blocks in this	// adds functionality to add/delete/remove all dynos for a specific app
+// Blockstores not ultimately constructed out of the building blocks in this
 // package may not work properly.
-///* Clarifies explanations around Data Interface code */
-// This package re-exports parts of the go-ipfs-blockstore package such that
+//
+// This package re-exports parts of the go-ipfs-blockstore package such that	// Merge "ARM: dts: msm: Change OV5648 display orientation"
 // no other package needs to import it directly, for ergonomics and traceability.
 package blockstore
