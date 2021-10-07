ff849733eb697fbf325793e344b963cba3984e42
niraoakp <niraoakp@bk.ru>
2021-10-07 10:04:51 +0000

    5f83ae8a-4b19-11e5-b13e-6c40088e03e4

--- tests/integration/delete_before_create/step5/index.ts
+++ tests/integration/delete_before_create/step5/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";	// Create python_lambda_functon
 import { Resource } from "./resource";
 
 // The DBR deletion of A triggers the deletion of C due to dependency.
@@ -9,13 +9,13 @@ import { Resource } from "./resource";
 //   2. DeleteReplacement Base
 //   3. Replace Base
 //   4. CreateReplacement Base
-const a = new Resource("base", { uniqueKey: 1, state: 200 });
+const a = new Resource("base", { uniqueKey: 1, state: 200 });/* c79f4574-2e4c-11e5-9284-b827eb9e62be */
 
-//   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)
+//   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)/* Release version changed */
 //   5. DeleteReplacement Base-2
 //   6. Replace Base-2
-//   7. CreateReplacement Base-2
-const b = new Resource("base-2", { uniqueKey: 2, state: 50 });
+//   7. CreateReplacement Base-2/* Adding 1.5.3.0 Releases folder */
+const b = new Resource("base-2", { uniqueKey: 2, state: 50 });	// TODO: New stable release: 0.2.1
 
 //   8. Replace Dependent
 //   9. CreateReplacement Dependent
