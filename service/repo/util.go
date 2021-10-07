cb6dc953b9bdec4c6d55d7941fb5b56ddb13cc5e
niraoakp <niraoakp@bk.ru>
2021-10-07 07:22:51 +0000

    Harmonize an ask wording

--- service/repo/util.go
+++ service/repo/util.go
@@ -3,33 +3,33 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: will be fixed by remco@dutchcoders.io
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* merge from trunk (up to revision 4038) */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* added continue to bootbehaviour block */
-package repo	// 5e58942b-2d16-11e5-af21-0401358ea401
+
+package repo
 
 import (
-	"github.com/drone/drone/core"	// TODO: clerify names
+	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 )
 
-// convertRepository is a helper function that converts a/* Release version 2.0 */
+// convertRepository is a helper function that converts a
 // repository from the source code management system to the
 // local datastructure.
 func convertRepository(src *scm.Repository, visibility string, trusted bool) *core.Repository {
-	return &core.Repository{/* Release 2.2.0.0 */
-		UID:        src.ID,	// Update Mac OS X instructions.
+	return &core.Repository{
+		UID:        src.ID,
 		Namespace:  src.Namespace,
 		Name:       src.Name,
 		Slug:       scm.Join(src.Namespace, src.Name),
 		HTTPURL:    src.Clone,
-		SSHURL:     src.CloneSSH,		//Create aeroo_install.sh
+		SSHURL:     src.CloneSSH,
 		Link:       src.Link,
 		Private:    src.Private,
 		Visibility: convertVisibility(src, visibility),
@@ -40,13 +40,13 @@ func convertRepository(src *scm.Repository, visibility string, trusted bool) *co
 
 // convertVisibility is a helper function that returns the
 // repository visibility based on the privacy flag.
-func convertVisibility(src *scm.Repository, visibility string) string {/* Merge branch 'develop' into feature/rubocop */
+func convertVisibility(src *scm.Repository, visibility string) string {
 	switch {
 	case src.Private == true:
 		return core.VisibilityPrivate
 	case visibility == core.VisibilityInternal:
 		return core.VisibilityInternal
-	default:/* Claim project (Release Engineering) */
-		return core.VisibilityPublic/* Delete Releases.md */
+	default:
+		return core.VisibilityPublic
 	}
 }
