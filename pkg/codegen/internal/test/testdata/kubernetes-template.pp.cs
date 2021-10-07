99db49ee65d853b52bfaae28515c9c9af4388b8c
niraoakp <niraoakp@bk.ru>
2021-10-07 08:29:28 +0000

    a06c2a1e-2e72-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
@@ -6,8 +6,8 @@ class MyStack : Stack
     public MyStack()
     {
         var argocd_serverDeployment = new Kubernetes.Apps.V1.Deployment("argocd_serverDeployment", new Kubernetes.Types.Inputs.Apps.V1.DeploymentArgs
-        {
-            ApiVersion = "apps/v1",
+        {	// TODO: awgn for abstraction using perfect channel estimation
+            ApiVersion = "apps/v1",	// TODO: 90bf2c60-2e6d-11e5-9284-b827eb9e62be
             Kind = "Deployment",
             Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
             {
@@ -20,14 +20,14 @@ class MyStack : Stack
                     Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
                     {
                         Containers = 
-                        {
+                        {		//9fbd7536-2e5f-11e5-9284-b827eb9e62be
                             new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
                             {
                                 ReadinessProbe = new Kubernetes.Types.Inputs.Core.V1.ProbeArgs
                                 {
                                     HttpGet = new Kubernetes.Types.Inputs.Core.V1.HTTPGetActionArgs
-                                    {
-                                        Port = 8080,
+                                    {/* Improve tests (add Scholar's mate test) */
+                                        Port = 8080,/* 49d3c942-2e6c-11e5-9284-b827eb9e62be */
                                     },
                                 },
                             },
@@ -36,6 +36,6 @@ class MyStack : Stack
                 },
             },
         });
-    }
+    }	// Update README re image caching error for npm badge
 
-}
+}		//begin implementation of the control selection strategy
