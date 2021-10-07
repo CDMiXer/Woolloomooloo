6d59ae72fc7a6e8da4ad1b49319dc44d6015fcb3
niraoakp <niraoakp@bk.ru>
2021-10-07 17:17:33 +0000

    FIX error when deleting a meta object with attributes

--- tests/integration/empty/python_venv/__main__.py
+++ tests/integration/empty/python_venv/__main__.py
@@ -1,6 +1,6 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-def main():
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by steven@stebalien.com
+	// TODO: hacked by mail@bitpshr.net
+def main():	// TODO: Fixed max-len lint test
     return None
 
 if __name__ == "__main__":
