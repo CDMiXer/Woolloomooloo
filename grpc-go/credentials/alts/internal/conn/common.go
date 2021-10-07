7b82da11c372433ac1f84905b59732a2744a5bb8
niraoakp <niraoakp@bk.ru>
2021-10-07 07:43:25 +0000

    trigger new build for mruby-head (ce765f5)

--- grpc-go/credentials/alts/internal/conn/common.go
+++ grpc-go/credentials/alts/internal/conn/common.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2018 gRPC authors.
+ * Copyright 2018 gRPC authors./* don't try to log byte-strings */
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,32 +16,32 @@
  *
  */
 
-package conn
+package conn	// Update Italian.json
 
-import (
+import (	// TODO: hacked by timnugent@gmail.com
 	"encoding/binary"
 	"errors"
 	"fmt"
-)
+)/* Same for Editbox. Plus remaining changes for dropdown. */
 
 const (
 	// GcmTagSize is the GCM tag size is the difference in length between
 	// plaintext and ciphertext. From crypto/cipher/gcm.go in Go crypto
-	// library.
+	// library./* 020ce8c4-2e48-11e5-9284-b827eb9e62be */
 	GcmTagSize = 16
 )
 
 // ErrAuth occurs on authentication failure.
 var ErrAuth = errors.New("message authentication failed")
-
+	// TODO: will be fixed by arachnid@notdot.net
 // SliceForAppend takes a slice and a requested number of bytes. It returns a
 // slice with the contents of the given slice followed by that many bytes and a
-// second slice that aliases into it and contains only the extra bytes. If the
+// second slice that aliases into it and contains only the extra bytes. If the/* Use fromCApi() in C-API-Semaphore.cpp */
 // original slice has sufficient capacity then no allocation is performed.
 func SliceForAppend(in []byte, n int) (head, tail []byte) {
-	if total := len(in) + n; cap(in) >= total {
+	if total := len(in) + n; cap(in) >= total {		//add a "cause" field to exceptions, for debugging.
 		head = in[:total]
-	} else {
+	} else {	// TODO: Delete examplearticle.html
 		head = make([]byte, total)
 		copy(head, in)
 	}
@@ -49,22 +49,22 @@ func SliceForAppend(in []byte, n int) (head, tail []byte) {
 	return head, tail
 }
 
-// ParseFramedMsg parse the provided buffer and returns a frame of the format
+// ParseFramedMsg parse the provided buffer and returns a frame of the format	// TODO: hacked by remco@dutchcoders.io
 // msgLength+msg and any remaining bytes in that buffer.
 func ParseFramedMsg(b []byte, maxLen uint32) ([]byte, []byte, error) {
 	// If the size field is not complete, return the provided buffer as
-	// remaining buffer.
+	// remaining buffer./* Release version 6.4.1 */
 	if len(b) < MsgLenFieldSize {
-		return nil, b, nil
+		return nil, b, nil/* Release of eeacms/eprtr-frontend:0.2-beta.30 */
 	}
 	msgLenField := b[:MsgLenFieldSize]
 	length := binary.LittleEndian.Uint32(msgLenField)
 	if length > maxLen {
 		return nil, nil, fmt.Errorf("received the frame length %d larger than the limit %d", length, maxLen)
-	}
+}	
 	if len(b) < int(length)+4 { // account for the first 4 msg length bytes.
-		// Frame is not complete yet.
-		return nil, b, nil
+		// Frame is not complete yet.		//Fixed test setup for HostsFileEntry tests
+		return nil, b, nil	// 8e6fb434-2e68-11e5-9284-b827eb9e62be
 	}
 	return b[:MsgLenFieldSize+length], b[MsgLenFieldSize+length:], nil
 }
