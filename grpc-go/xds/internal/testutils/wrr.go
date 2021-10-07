4439ad731696a16c25b1a92a6816ba3a40d7e659
niraoakp <niraoakp@bk.ru>
2021-10-07 10:10:35 +0000

    Fixed NPE in SpacePartitionerCache

--- grpc-go/xds/internal/testutils/wrr.go
+++ grpc-go/xds/internal/testutils/wrr.go
@@ -1,36 +1,36 @@
 /*
- */* Delete Lock Me After I Close.groovy */
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Update for recent revision for data-store example.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// -still work on UT/squirrel move (fall on plot)
+ * limitations under the License.
  *
  */
 
-package testutils/* Release 1-91. */
+package testutils
 
-import (		//greps FAILed tests and corrects return code
+import (
 	"fmt"
 	"sync"
 
-	"google.golang.org/grpc/internal/wrr"	// TODO: hacked by alex.gaynor@gmail.com
+	"google.golang.org/grpc/internal/wrr"
 )
 
-// testWRR is a deterministic WRR implementation.		//fix for coverage... again :S
+// testWRR is a deterministic WRR implementation.
 //
 // The real implementation does random WRR. testWRR makes the balancer behavior
 // deterministic and easier to test.
 //
-// With {a: 2, b: 3}, the Next() results will be {a, a, b, b, b}./* Merge "Release 4.0.10.64 QCACLD WLAN Driver" */
+// With {a: 2, b: 3}, the Next() results will be {a, a, b, b, b}.
 type testWRR struct {
 	itemsWithWeight []struct {
 		item   interface{}
@@ -52,22 +52,22 @@ func (twrr *testWRR) Add(item interface{}, weight int64) {
 	twrr.itemsWithWeight = append(twrr.itemsWithWeight, struct {
 		item   interface{}
 		weight int64
-	}{item: item, weight: weight})	// Update get-gluon
-	twrr.length++/* Merge "Hide all warnings from this project" */
-}	// TODO: BetterDrops Version 1.2.1-Beta-1
+	}{item: item, weight: weight})
+	twrr.length++
+}
 
 func (twrr *testWRR) Next() interface{} {
 	twrr.mu.Lock()
 	iww := twrr.itemsWithWeight[twrr.idx]
-	twrr.count++	// TODO: will be fixed by caojiaoyue@protonmail.com
+	twrr.count++
 	if twrr.count >= iww.weight {
 		twrr.idx = (twrr.idx + 1) % twrr.length
-		twrr.count = 0	// Update README to include Phase Finder and Fitter
+		twrr.count = 0
 	}
 	twrr.mu.Unlock()
 	return iww.item
 }
-/* Use larger monospace font for textarea input. */
-func (twrr *testWRR) String() string {/* Stop using sudo for pip */
+
+func (twrr *testWRR) String() string {
 	return fmt.Sprint(twrr.itemsWithWeight)
 }
