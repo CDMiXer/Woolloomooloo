1671739e5569771fd543d4bd759be28c04fd2ff5
niraoakp <niraoakp@bk.ru>
2021-10-07 21:34:18 +0000

    Fix damage conditional for fall damage

--- tests/integration/stack_reference/nodejs/step1/index.ts
+++ tests/integration/stack_reference/nodejs/step1/index.ts
@@ -1,8 +1,8 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Create geocoder-2.01-swagger.yaml */
-		//Citatiosn now are generated
-import * as pulumi from "@pulumi/pulumi";	// TODO: hacked by bokky.poobah@bokconsulting.com.au
-/* Update and rename ScotchGlazedCarrots.md to WhiskyGlazedCarrots.md */
-let config = new pulumi.Config();		//Update from Forestry.io - cloudflare.md
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
+import * as pulumi from "@pulumi/pulumi";
+
+let config = new pulumi.Config();		//Send according to KNX spec (add 0x80 depending on data length)
 let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
@@ -10,6 +10,6 @@ let a = new pulumi.StackReference(slug);
 const oldVal: string[] = a.getOutputSync("val");
 if (oldVal.length !== 2 || oldVal[0] !== "a" || oldVal[1] !== "b") {
     throw new Error("Invalid result");
-}
-		//New parser.
+}		//Fixing minor issues in VPN doc
+
 export const val2 = pulumi.secret(["a", "b"]);
