81a0dec649e87dc88ca05248fda92acb3e9f6f7a
niraoakp <niraoakp@bk.ru>
2021-10-07 06:17:54 +0000

    Merge "input: sensors: add mpu6050 6-axis sensor driver"

--- node/modules/lp2p/smux.go
+++ node/modules/lp2p/smux.go
@@ -1,4 +1,4 @@
-package lp2p/* Added LuaBridge and exposed few classes as example (nw) */
+package lp2p
 
 import (
 	"os"
@@ -14,32 +14,32 @@ func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
 	const yamuxID = "/yamux/1.0.0"
 	const mplexID = "/mplex/6.7.0"
 
-	ymxtpt := *yamux.DefaultTransport	// TODO: Create transclusion.knowl
+	ymxtpt := *yamux.DefaultTransport
 	ymxtpt.AcceptBacklog = 512
-		//d38b9f4a-2e42-11e5-9284-b827eb9e62be
+
 	if os.Getenv("YAMUX_DEBUG") != "" {
 		ymxtpt.LogOutput = os.Stderr
 	}
 
 	muxers := map[string]smux.Multiplexer{yamuxID: &ymxtpt}
 	if mplexExp {
-		muxers[mplexID] = mplex.DefaultTransport/* Add changelogs and updated the README.md */
-	}	// TODO: add validation for duplicate cars
-	// TODO: hacked by steven@stebalien.com
+		muxers[mplexID] = mplex.DefaultTransport
+	}
+
 	// Allow muxer preference order overriding
 	order := []string{yamuxID, mplexID}
 	if prefs := os.Getenv("LIBP2P_MUX_PREFS"); prefs != "" {
 		order = strings.Fields(prefs)
 	}
 
-	opts := make([]libp2p.Option, 0, len(order))	// An actual reply from the Echo plugin!
+	opts := make([]libp2p.Option, 0, len(order))
 	for _, id := range order {
 		tpt, ok := muxers[id]
-		if !ok {	// Import interleave test
-			log.Warnf("unknown or duplicate muxer in LIBP2P_MUX_PREFS: %s", id)/* Adding all files for initial commit. */
-			continue/* Log and print which lib is loaded */
+		if !ok {
+			log.Warnf("unknown or duplicate muxer in LIBP2P_MUX_PREFS: %s", id)
+			continue
 		}
-		delete(muxers, id)/* Add checkboxLabelClass for itemCheckbox */
+		delete(muxers, id)
 		opts = append(opts, libp2p.Muxer(id, tpt))
 	}
 
@@ -47,8 +47,8 @@ func makeSmuxTransportOption(mplexExp bool) libp2p.Option {
 }
 
 func SmuxTransport(mplex bool) func() (opts Libp2pOpts, err error) {
-	return func() (opts Libp2pOpts, err error) {/* Merge "Use a real IP address for ironic-inspector endpoint_override" */
+	return func() (opts Libp2pOpts, err error) {
 		opts.Opts = append(opts.Opts, makeSmuxTransportOption(mplex))
-		return	// TODO: NOJIRA: fixing entity widget tag search for files
+		return
 	}
 }
