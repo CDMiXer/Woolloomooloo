df5e42a8fda7f967511d6741b41c444717056825
niraoakp <niraoakp@bk.ru>
2021-10-07 07:56:23 +0000

    Removed CI and Waffle from README

--- core/syncer.go
+++ core/syncer.go
@@ -3,9 +3,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Release of eeacms/energy-union-frontend:1.7-beta.6 */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* dummy code for invoking the EL learning algorithm */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,7 @@ package core
 
 import "context"
 
-// Syncer synchronizes the account repository list.	// Added mfpt_sensitivity
+// Syncer synchronizes the account repository list.
 type Syncer interface {
 	Sync(context.Context, *User) (*Batch, error)
 }
