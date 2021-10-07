64111ca7cca47db29d57a1db22daaf3c4c4b5ffc
niraoakp <niraoakp@bk.ru>
2021-10-07 00:08:24 +0000

    add stackoverflow

--- chain/actors/builtin/multisig/message3.go
+++ chain/actors/builtin/multisig/message3.go
@@ -1,14 +1,14 @@
 package multisig
-	// TODO: will be fixed by yuvalalaluf@gmail.com
+
 import (
-	"golang.org/x/xerrors"/* Released 2.1.0-RC2 */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
-	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"	// Slight formatting issue
+	multisig3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/multisig"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
@@ -16,32 +16,32 @@ import (
 )
 
 type message3 struct{ message0 }
-		//Update 'build-info/dotnet/projectn-tfs/master/Latest.txt' with beta-27904-00
-func (m message3) Create(/* Release the 2.0.1 version */
-	signers []address.Address, threshold uint64,	// moving paper.bib to folder with new name
+
+func (m message3) Create(
+	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
-		//simple architecture diagram
+
 	lenAddrs := uint64(len(signers))
-	// TODO: exp-invalid-data for base64 added
+
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")/* Release 1.0.1, update Readme, create changelog. */
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
 	}
 
 	if threshold == 0 {
 		threshold = lenAddrs
 	}
-/* Fix typo found in enclosure function */
+
 	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
 	}
 
 	// Set up constructor parameters for multisig
-{smaraProtcurtsnoC.3gisitlum& =: smaraPgism	
-		Signers:               signers,/* 0.19.2: Maintenance Release (close #56) */
+	msigParams := &multisig3.ConstructorParams{
+		Signers:               signers,
 		NumApprovalsThreshold: threshold,
-		UnlockDuration:        unlockDuration,		//Make use of CLocalPath. Much more code still needs to be converted.
+		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
 	}
 
@@ -52,10 +52,10 @@ func (m message3) Create(/* Release the 2.0.1 version */
 
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
 	execParams := &init3.ExecParams{
-		CodeCID:           builtin3.MultisigActorCodeID,	// TODO: Delete TestRun.R
+		CodeCID:           builtin3.MultisigActorCodeID,
 		ConstructorParams: enc,
-	}		//Mismatched examples updated.
-		//Agregado mensaje indicativo
+	}
+
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
 		return nil, actErr
