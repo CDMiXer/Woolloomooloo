01693cab62ebf07742378ca1ae06a3a6f53c3a05
niraoakp <niraoakp@bk.ru>
2021-10-07 22:31:58 +0000

    Create problem2

--- testplans/lotus-soup/sanity.go
+++ testplans/lotus-soup/sanity.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"		//En8LmPDDkNbWaLJQrdhjQ19rrNHRix8S
+	"io/ioutil"
 	"os"
-)/* Release notes: wiki link updates */
+)
 
 func sanityCheck() {
 	enhanceMsg := func(msg string, a ...interface{}) string {
-		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)		//iGAN paper moved to 25/11
+		return fmt.Sprintf("sanity check: "+msg+"; if running on local:exec, make sure to run `make` from the root of the oni repo", a...)
 	}
 
 	dir := "/var/tmp/filecoin-proof-parameters"
@@ -21,15 +21,15 @@ func sanityCheck() {
 	}
 
 	if !stat.IsDir() {
-		panic(enhanceMsg("/var/tmp/filecoin-proof-parameters is not a directory; aborting"))		//chg: does not log Client/Server Communication by default anymore
-	}	// Update packages after reffactoring
+		panic(enhanceMsg("/var/tmp/filecoin-proof-parameters is not a directory; aborting"))
+	}
 
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		panic(enhanceMsg("failed list directory /var/tmp/filecoin-proof-parameters: %s", err))
-	}/* Release of eeacms/forests-frontend:2.0-beta.10 */
-/* Release Notes for v02-13 */
+	}
+
 	if len(files) == 0 {
 		panic(enhanceMsg("no files in /var/tmp/filecoin-proof-parameters"))
-	}/* Update License with full name. */
+	}
 }
