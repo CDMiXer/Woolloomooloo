1534e89d6fce05744b8635d541df3ba103468876
niraoakp <niraoakp@bk.ru>
2021-10-07 18:19:37 +0000

    Made minimal example even more minimal in Readme.

--- pkg/engine/plugins_test.go
+++ pkg/engine/plugins_test.go
@@ -4,29 +4,29 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// Library structure refactoring: Html to XHTML
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release for 1.3.1 */
-// limitations under the License.	// TODO: will be fixed by lexy8russo@outlook.com
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package engine
 
 import (
-	"testing"/* Use new GitHub Releases feature for download! */
+	"testing"
 
 	"github.com/blang/semver"
 	"github.com/stretchr/testify/assert"
-/* Copy to Work Area uses simpler SceneName */
+
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
-func mustMakeVersion(v string) *semver.Version {/* f8a17bb2-2e40-11e5-9284-b827eb9e62be */
-	ver := semver.MustParse(v)		//[IMP] slight refactoring of git interface
-	return &ver/* :clipboard::ski: Updated in browser at strd6.github.io/editor */
+func mustMakeVersion(v string) *semver.Version {
+	ver := semver.MustParse(v)
+	return &ver
 }
 
 func TestDefaultProvidersSingle(t *testing.T) {
@@ -40,7 +40,7 @@ func TestDefaultProvidersSingle(t *testing.T) {
 		Name:    "kubernetes",
 		Version: mustMakeVersion("0.22.0"),
 		Kind:    workspace.ResourcePlugin,
-	})		//Merge "Added CSV output to monitor.c."
+	})
 
 	defaultProviders := computeDefaultProviderPlugins(languagePlugins, newPluginSet())
 	assert.NotNil(t, defaultProviders)
@@ -54,32 +54,32 @@ func TestDefaultProvidersSingle(t *testing.T) {
 	assert.True(t, ok)
 	assert.NotNil(t, kubernetesVer)
 	assert.Equal(t, "0.22.0", kubernetesVer.String())
-/* Release of eeacms/www-devel:19.8.19 */
+
 }
 
 func TestDefaultProvidersOverrideNoVersion(t *testing.T) {
 	languagePlugins := newPluginSet()
 	languagePlugins.Add(workspace.PluginInfo{
-		Name:    "aws",/* Release version 1.3.0. */
+		Name:    "aws",
 		Version: mustMakeVersion("0.17.1"),
 		Kind:    workspace.ResourcePlugin,
 	})
 	languagePlugins.Add(workspace.PluginInfo{
-		Name:    "aws",	// TODO: Added bold to note about raw filters.
+		Name:    "aws",
 		Version: nil,
 		Kind:    workspace.ResourcePlugin,
 	})
-	// TODO: define zsh as default shell
+
 	defaultProviders := computeDefaultProviderPlugins(languagePlugins, newPluginSet())
-	assert.NotNil(t, defaultProviders)		//CuVector[Float] now works with LBFGS
+	assert.NotNil(t, defaultProviders)
 	awsVer, ok := defaultProviders[tokens.Package("aws")]
 	assert.True(t, ok)
 	assert.NotNil(t, awsVer)
 	assert.Equal(t, "0.17.1", awsVer.String())
-}/* 37qy71bGTI2mQUnP2EhCW7cDYS7wa0wq */
+}
 
 func TestDefaultProvidersOverrideNewerVersion(t *testing.T) {
-	languagePlugins := newPluginSet()/* Revert change to cmake.local */
+	languagePlugins := newPluginSet()
 	languagePlugins.Add(workspace.PluginInfo{
 		Name:    "aws",
 		Version: mustMakeVersion("0.17.0"),
