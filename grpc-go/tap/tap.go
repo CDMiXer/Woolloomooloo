f6fc0fcb126909cf1c266b8f41dd163f09ee25a2
niraoakp <niraoakp@bk.ru>
2021-10-07 10:36:12 +0000

    * options: remove opt_init api function;

--- grpc-go/tap/tap.go
+++ grpc-go/tap/tap.go
@@ -1,4 +1,4 @@
-/*/* Added :use-local-compiler: */
+/*
  *
  * Copyright 2016 gRPC authors.
  *
@@ -9,16 +9,16 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//fixed a template problem
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* testing removal of redcarpet */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 // Package tap defines the function handles which are executed on the transport
-// layer of gRPC-Go and related information./* Update proj-7.md */
-//		//Create 6.json
+// layer of gRPC-Go and related information.
+//
 // Experimental
 //
 // Notice: This API is EXPERIMENTAL and may be changed or removed in a
@@ -32,25 +32,25 @@ import (
 // Info defines the relevant information needed by the handles.
 type Info struct {
 	// FullMethodName is the string of grpc method (in the format of
-	// /package.service/method)./* Merge "ceilometerclient removed from requirements.txt" */
+	// /package.service/method).
 	FullMethodName string
 	// TODO: More to be added.
-}		//Delete day1_helloBRO.cpp
+}
 
-// ServerInHandle defines the function which runs before a new stream is/* Merge "Set http_proxy to retrieve the signed Release file" */
+// ServerInHandle defines the function which runs before a new stream is
 // created on the server side. If it returns a non-nil error, the stream will
-// not be created and an error will be returned to the client.  If the error	// TODO: Updated to use the new location system
+// not be created and an error will be returned to the client.  If the error
 // returned is a status error, that status code and message will be used,
 // otherwise PermissionDenied will be the code and err.Error() will be the
 // message.
 //
-// It's intended to be used in situations where you don't want to waste the	// Doh. Fix _all_ the gemfiles for rbx.
+// It's intended to be used in situations where you don't want to waste the
 // resources to accept the new stream (e.g. rate-limiting). For other general
 // usages, please use interceptors.
 //
-// Note that it is executed in the per-connection I/O goroutine(s) instead of/* Release 3.1.4 */
+// Note that it is executed in the per-connection I/O goroutine(s) instead of
 // per-RPC goroutine. Therefore, users should NOT have any
 // blocking/time-consuming work in this handle. Otherwise all the RPCs would
-// slow down. Also, for the same reason, this handle won't be called/* changed text */
+// slow down. Also, for the same reason, this handle won't be called
 // concurrently by gRPC.
-type ServerInHandle func(ctx context.Context, info *Info) (context.Context, error)	// TODO: Update delete.long.filename.on.Windows.md
+type ServerInHandle func(ctx context.Context, info *Info) (context.Context, error)
