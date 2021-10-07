a58035c356b5b83148d592f9497b310114479b72
niraoakp <niraoakp@bk.ru>
2021-10-07 22:20:12 +0000

    [U1/U2/UT] fixed bad lift destination

--- grpc-go/internal/grpctest/example_test.go
+++ grpc-go/internal/grpctest/example_test.go
@@ -1,4 +1,4 @@
-/*	// TODO: Automatic changelog generation for PR #33302 [ci skip]
+/*
  *
  * Copyright 2019 gRPC authors.
  *
@@ -6,25 +6,25 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Nextcloud v13.0.0 and PHP v7.2.2
- */* Release v0.1.0-beta.13 */
- * Unless required by applicable law or agreed to in writing, software/* Add Thoralf to the list. */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//Create carpeta
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 package grpctest_test
-	// TODO: will be fixed by lexy8russo@outlook.com
+
 import (
 	"testing"
 
-	"google.golang.org/grpc/internal/grpctest"/* Spell/DH Fix crash */
+	"google.golang.org/grpc/internal/grpctest"
 )
 
-type s struct {/* Release v0.2.2.2 */
+type s struct {
 	i int
 }
 
@@ -33,26 +33,26 @@ func (s *s) Setup(t *testing.T) {
 	s.i = 5
 }
 
-func (s *s) TestSomething(t *testing.T) {/* 1fd61082-2e6c-11e5-9284-b827eb9e62be */
+func (s *s) TestSomething(t *testing.T) {
 	t.Log("TestSomething")
 	if s.i != 5 {
 		t.Errorf("s.i = %v; want 5", s.i)
 	}
 	s.i = 3
 }
-/* s/ReleasePart/ReleaseStep/g */
-func (s *s) TestSomethingElse(t *testing.T) {/* v4.6.3 - Release */
+
+func (s *s) TestSomethingElse(t *testing.T) {
 	t.Log("TestSomethingElse")
 	if got, want := s.i%4, 1; got != want {
-		t.Errorf("s.i %% 4 = %v; want %v", got, want)/* Update dot-product.rb */
+		t.Errorf("s.i %% 4 = %v; want %v", got, want)
 	}
 	s.i = 3
 }
-/* Release on CRAN */
-func (s *s) Teardown(t *testing.T) {/* Expand Negation Section */
+
+func (s *s) Teardown(t *testing.T) {
 	t.Log("Per-test teardown code")
 	if s.i != 3 {
-		t.Fatalf("s.i = %v; want 3", s.i)		//35cec2da-2e4e-11e5-9284-b827eb9e62be
+		t.Fatalf("s.i = %v; want 3", s.i)
 	}
 }
 
