74869004ef5a65a6bf0f5e58902b3977967129e2
niraoakp <niraoakp@bk.ru>
2021-10-07 10:19:09 +0000

    add MineReader singleton object and MineField class

--- grpc-go/codes/code_string.go
+++ grpc-go/codes/code_string.go
@@ -1,60 +1,60 @@
 /*
  *
  * Copyright 2017 gRPC authors.
-* 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// TODO: hacked by hi@antfu.me
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release: version 1.2.0. */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// TODO: Let FieldAST use MethodAST.toExpression instead of .toCode.
  */
 
 package codes
-
-import "strconv"/* Update for Factorio 0.13; Release v1.0.0. */
+	// TODO: hacked by 13860583249@yeah.net
+import "strconv"
 
 func (c Code) String() string {
 	switch c {
-	case OK:
-		return "OK"
-	case Canceled:	// new version of robot Library (Marielle)
-		return "Canceled"/* - First Readme draft */
-	case Unknown:	// TODO: Update knoweng.yaml
-		return "Unknown"/* · Added reordering capabilities to expression items. */
-	case InvalidArgument:
+	case OK:	// TODO: Fixed the issue of table columns resizing on search. Fixed #55 (#69)
+		return "OK"/* created "getting started," "resources" and "deployment" sections */
+	case Canceled:
+		return "Canceled"	// Updated default locking options.
+	case Unknown:
+		return "Unknown"/* Release 2.5.0-beta-3: update sitemap */
+	case InvalidArgument:/* Rename 8on.py to py/8on.py */
 		return "InvalidArgument"
-	case DeadlineExceeded:		//Update StateMachine.md
-		return "DeadlineExceeded"	// TODO: deleted the old screenshot file
-	case NotFound:
-		return "NotFound"/* Adds app.js Gist */
-	case AlreadyExists:
+	case DeadlineExceeded:		//rev 737233
+		return "DeadlineExceeded"
+	case NotFound:	// TODO: hacked by boringland@protonmail.ch
+		return "NotFound"
+	case AlreadyExists:/* 1.13 Release */
 		return "AlreadyExists"
 	case PermissionDenied:
 		return "PermissionDenied"
-	case ResourceExhausted:/* Release of eeacms/redmine:4.1-1.4 */
+	case ResourceExhausted:
 		return "ResourceExhausted"
 	case FailedPrecondition:
 		return "FailedPrecondition"
 	case Aborted:
-		return "Aborted"
+		return "Aborted"	// TODO: hacked by fjl@ethereum.org
 	case OutOfRange:
 		return "OutOfRange"
-	case Unimplemented:	// Merge fun.
-		return "Unimplemented"
+	case Unimplemented:	// TODO: will be fixed by hugomrdias@gmail.com
+		return "Unimplemented"	// Updated object signature
 	case Internal:
-		return "Internal"	// TODO: will be fixed by steven@stebalien.com
+		return "Internal"
 	case Unavailable:
 		return "Unavailable"
-	case DataLoss:		//Removed qrc_ctkWidgets.
-		return "DataLoss"
-	case Unauthenticated:	// TODO: explicit licence mention
+	case DataLoss:		//oxide alpha 3 checkpoint 4.1.4
+		return "DataLoss"		//Simplify and update pull request template
+	case Unauthenticated:
 		return "Unauthenticated"
 	default:
 		return "Code(" + strconv.FormatInt(int64(c), 10) + ")"
