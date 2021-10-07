2d4ec83391f2279d3560ade2ef76c9a6bb18a045
niraoakp <niraoakp@bk.ru>
2021-10-07 20:17:45 +0000

    correcting headers and ToC

--- server/static/response-rewriter.go
+++ server/static/response-rewriter.go
@@ -7,14 +7,14 @@ import (
 )
 
 type responseRewriter struct {
-	http.ResponseWriter		//move stop task method to task client model, see #2
+	http.ResponseWriter
 	old []byte
-	new []byte/* 374d74e4-2e49-11e5-9284-b827eb9e62be */
+	new []byte
 }
-	// Update Memory Read only classes + Direct quantiles update
-func (w *responseRewriter) Write(a []byte) (int, error) {
+
+func (w *responseRewriter) Write(a []byte) (int, error) {	// TODO: hacked by 13860583249@yeah.net
 	b := bytes.Replace(a, w.old, w.new, 1)
 	// status code and headers are printed out when we write data
 	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
 	return w.ResponseWriter.Write(b)
-}
+}	// TODO: hacked by 13860583249@yeah.net
