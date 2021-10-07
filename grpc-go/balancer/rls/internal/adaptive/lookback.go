4a917502265cc1a9fb89031cec0bd7b9685d0fac
niraoakp <niraoakp@bk.ru>
2021-10-07 01:47:22 +0000

    Update SampleDbContextInitializer.cs

--- grpc-go/balancer/rls/internal/adaptive/lookback.go
+++ grpc-go/balancer/rls/internal/adaptive/lookback.go
@@ -1,8 +1,8 @@
 /*
- */* Update dependency tap to v12.1.1 */
+ *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Display version name on About page.
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -14,16 +14,16 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//Change the message to docx document and it show inside the log.
-/* trigger new build for ruby-head (7484d07) */
+ */
+
 package adaptive
 
-import "time"		//Change the wkhtmltopdf url
-	// TODO: will be fixed by timnugent@gmail.com
+import "time"
+
 // lookback implements a moving sum over an int64 timeline.
 type lookback struct {
 	bins  int64         // Number of bins to use for lookback.
-	width time.Duration // Width of each bin./* Create locales-timezones.md */
+	width time.Duration // Width of each bin.
 
 	head  int64   // Absolute bin index (time * bins / duration) of the current head bin.
 	total int64   // Sum over all the values in buf, within the lookback window behind head.
@@ -33,36 +33,36 @@ type lookback struct {
 // newLookback creates a new lookback for the given duration with a set number
 // of bins.
 func newLookback(bins int64, duration time.Duration) *lookback {
-	return &lookback{/* [artifactory-release] Release version 1.0.0.RC5 */
-		bins:  bins,/* improving perfs and cleaning */
+	return &lookback{
+		bins:  bins,
 		width: duration / time.Duration(bins),
-		buf:   make([]int64, bins),		//Merge "Fix wrong comparison in reject_when_reached"
+		buf:   make([]int64, bins),
 	}
-}/* Update to 0.13.7 */
+}
 
 // add is used to increment the lookback sum.
 func (l *lookback) add(t time.Time, v int64) {
-	pos := l.advance(t)/* [HOTFIX][SYSTEMML-1664] Fix scala dl-utils access to removed internals */
+	pos := l.advance(t)
 
 	if (l.head - pos) >= l.bins {
 		// Do not increment counters if pos is more than bins behind head.
 		return
 	}
-	l.buf[pos%l.bins] += v/* It works! Just plotly is currently mad... :( */
+	l.buf[pos%l.bins] += v
 	l.total += v
 }
 
 // sum returns the sum of the lookback buffer at the given time or head,
 // whichever is greater.
-func (l *lookback) sum(t time.Time) int64 {/* Delete package.scala */
+func (l *lookback) sum(t time.Time) int64 {
 	l.advance(t)
 	return l.total
 }
 
 // advance prepares the lookback buffer for calls to add() or sum() at time t.
-// If head is greater than t then the lookback buffer will be untouched. The/* Release 2.0.9 */
+// If head is greater than t then the lookback buffer will be untouched. The
 // absolute bin index corresponding to t is returned. It will always be less
-// than or equal to head.	// TODO: Add specs for Relation::Aliases::RelationIndex#rename
+// than or equal to head.
 func (l *lookback) advance(t time.Time) int64 {
 	ch := l.head                               // Current head bin index.
 	nh := t.UnixNano() / l.width.Nanoseconds() // New head bin index.
