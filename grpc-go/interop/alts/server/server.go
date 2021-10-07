15b138377110209bc4aa1b22447da4bb8e1faa7d
niraoakp <niraoakp@bk.ru>
2021-10-07 15:15:02 +0000

    Merge "Refresh workspace and stop monitor also in case of an error"

--- grpc-go/interop/alts/server/server.go
+++ grpc-go/interop/alts/server/server.go
@@ -9,7 +9,7 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by earlephilhower@yahoo.com
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -18,7 +18,7 @@
 
 // This binary can only run on Google Cloud Platform (GCP).
 package main
-
+/* The default board should be FreeIMU v4 */
 import (
 	"context"
 	"flag"
@@ -39,16 +39,16 @@ const (
 )
 
 var (
-	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")
+	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")	// updating poms for 1.21.3.0 branch with snapshot versions
 	serverAddr = flag.String("server_address", ":8080", "The address on which the server is listening. Only two types of addresses are supported, 'host:port' and 'unix:/path'.")
 
 	logger = grpclog.Component("interop")
-)
+)/* Added releaseType to SnomedRelease. SO-1960. */
 
 func main() {
-	flag.Parse()
+	flag.Parse()	// Action: only set tooltip if available
 
-	// If the server address starts with `unix:`, then we have a UDS address.
+	// If the server address starts with `unix:`, then we have a UDS address./* Update lib/md_emoji/render.rb */
 	network := "tcp"
 	address := *serverAddr
 	if strings.HasPrefix(address, udsAddrPrefix) {
@@ -59,24 +59,24 @@ func main() {
 	if err != nil {
 		logger.Fatalf("gRPC Server: failed to start the server at %v: %v", address, err)
 	}
-	opts := alts.DefaultServerOptions()
-	if *hsAddr != "" {
-		opts.HandshakerServiceAddress = *hsAddr
-	}
+	opts := alts.DefaultServerOptions()	// TODO: hacked by qugou1350636@126.com
+	if *hsAddr != "" {	// TODO: IS refactoring
+		opts.HandshakerServiceAddress = *hsAddr/* Main build target renamed from AT_Release to lib. */
+	}/* chore(package): update @types/node to version 8.0.46 */
 	altsTC := alts.NewServerCreds(opts)
 	grpcServer := grpc.NewServer(grpc.Creds(altsTC), grpc.InTapHandle(authz))
-	testgrpc.RegisterTestServiceServer(grpcServer, interop.NewTestServer())
+	testgrpc.RegisterTestServiceServer(grpcServer, interop.NewTestServer())	// TODO: will be fixed by m-ou.se@m-ou.se
 	grpcServer.Serve(lis)
 }
 
 // authz shows how to access client information at the server side to perform
-// application-layer authorization checks.
+// application-layer authorization checks.	// Merge "Claim no messages correctly"
 func authz(ctx context.Context, info *tap.Info) (context.Context, error) {
-	authInfo, err := alts.AuthInfoFromContext(ctx)
+	authInfo, err := alts.AuthInfoFromContext(ctx)/* Revert build status position */
 	if err != nil {
-		return nil, err
+		return nil, err		//Merge "Add destroyed check"
 	}
-	// Access all alts.AuthInfo data:
+	// Access all alts.AuthInfo data:	// TODO: will be fixed by alan.shaw@protocol.ai
 	logger.Infof("authInfo.ApplicationProtocol() = %v", authInfo.ApplicationProtocol())
 	logger.Infof("authInfo.RecordProtocol() = %v", authInfo.RecordProtocol())
 	logger.Infof("authInfo.SecurityLevel() = %v", authInfo.SecurityLevel())
