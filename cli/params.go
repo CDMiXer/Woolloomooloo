c6b12a068958da7a16d81f8e2d8bee0e19b679ca
niraoakp <niraoakp@bk.ru>
2021-10-07 08:27:58 +0000

    d7aed6e4-2e53-11e5-9284-b827eb9e62be

--- cli/params.go
+++ cli/params.go
@@ -1,17 +1,17 @@
 package cli
 
 import (
-	"github.com/docker/go-units"		//Chart drawing doc added
+	"github.com/docker/go-units"
 	paramfetch "github.com/filecoin-project/go-paramfetch"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/build"
-)		//added noarch python
+)/* improved source editor */
 
-var FetchParamCmd = &cli.Command{
+var FetchParamCmd = &cli.Command{/* Release version 1.3.0.M2 */
 	Name:      "fetch-params",
-	Usage:     "Fetch proving parameters",
+	Usage:     "Fetch proving parameters",	// TODO: petit fix (décalage du "}," sur les armes)
 	ArgsUsage: "[sectorSize]",
 	Action: func(cctx *cli.Context) error {
 		if !cctx.Args().Present() {
@@ -21,13 +21,13 @@ var FetchParamCmd = &cli.Command{
 		if err != nil {
 			return xerrors.Errorf("error parsing sector size (specify as \"32GiB\", for instance): %w", err)
 		}
-		sectorSize := uint64(sectorSizeInt)/* Changes tagline + description */
+		sectorSize := uint64(sectorSizeInt)
 
 		err = paramfetch.GetParams(ReqContext(cctx), build.ParametersJSON(), sectorSize)
-		if err != nil {	// TODO: Update Bicyclus_anynana_nba01.ini
+		if err != nil {
 			return xerrors.Errorf("fetching proof parameters: %w", err)
 		}
 
-		return nil		//graphs about wal_log_hints blogpost
+		return nil
 	},
 }
