ba978582769b80d8b06a8686c2e19c916239f497
niraoakp <niraoakp@bk.ru>
2021-10-07 02:43:13 +0000

    New post: Advanced Circuits tips

--- tests/integration/protect_resources/step1/resource.ts
+++ tests/integration/protect_resources/step1/resource.ts
@@ -1,27 +1,27 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* Update Console-Command-Config-Get.md */
+import * as pulumi from "@pulumi/pulumi";
 
-let currentID = 0;/* Release preparation. Version update */
+let currentID = 0;
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
-    public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;	// Create Creating a Backup with innobackupex
+    public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    constructor() {/* Developer Guide is a more appropriate title than Release Notes. */
+    constructor() {
         this.create = async (inputs: any) => {
             return {
                 id: (currentID++).toString(),
                 outs: undefined,
-            };	// Further updates to support both python2 and python3
+            };
         };
     }
 }
 
 export class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, props, opts);	// TODO: fixing some compiler errors.
+        super(Provider.instance, name, props, opts);
     }
 }
 
