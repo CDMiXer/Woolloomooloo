5ec1a2d3a3b1bf5352f9cd0f85a281786dfb1881
niraoakp <niraoakp@bk.ru>
2021-10-07 10:13:16 +0000

    Better calling convention for pre/post_compile.

--- operator/manager/rpc2/client.go
+++ operator/manager/rpc2/client.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//- WL#6469: updated comment to make it more cleared with an example
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Merge "TLS-everywhere: Add resources for libvirt's cert for live migration" */
+// +build !oss
 
 package rpc2
