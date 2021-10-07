5503e52967d6e2a643e037494320db5c48d16992
niraoakp <niraoakp@bk.ru>
2021-10-07 23:27:23 +0000

    delete keydragzoom 1.0 tag for re-tagging.

--- handler/api/repos/crons/update.go
+++ handler/api/repos/crons/update.go
@@ -2,14 +2,14 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Release 0.8.5. */
-/* Release of eeacms/jenkins-master:2.235.2 */
+// +build !oss
+
 package crons
 
 import (
 	"encoding/json"
 	"net/http"
-		//Merge "Discard packages from epoch that were not downloaded"
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
@@ -18,46 +18,46 @@ import (
 
 type cronUpdate struct {
 	Branch   *string `json:"branch"`
-	Target   *string `json:"target"`	// Move public liblightdm-gobject-0 headers into subdirectory
-	Disabled *bool   `json:"disabled"`/* Create log-weak-scaling-small-nbody.md */
-}	// TODO: hacked by jon@atack.com
+	Target   *string `json:"target"`
+	Disabled *bool   `json:"disabled"`
+}
 
 // HandleUpdate returns an http.HandlerFunc that processes http
 // requests to enable or disable a cron job.
-func HandleUpdate(/* Add test on Windows and configure for Win32/x64 Release/Debug */
+func HandleUpdate(
 	repos core.RepositoryStore,
 	crons core.CronStore,
-) http.HandlerFunc {	// Be clearer about testrpc-sc / network config
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-			cron      = chi.URLParam(r, "cron")	// ff58af4a-2e5c-11e5-9284-b827eb9e62be
+			cron      = chi.URLParam(r, "cron")
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {	// TODO: nomina: estructura inicial para subir empleados fix 1
-			render.NotFound(w, err)		//blocks: fix delete cache
+		if err != nil {
+			render.NotFound(w, err)
 			return
 		}
 		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
 		if err != nil {
-)rre ,w(dnuoFtoN.redner			
+			render.NotFound(w, err)
 			return
 		}
-/* Move The Cube to Spacedock */
+
 		in := new(cronUpdate)
 		json.NewDecoder(r.Body).Decode(in)
 		if in.Branch != nil {
 			cronjob.Branch = *in.Branch
-		}		//Changed registration properties
+		}
 		if in.Target != nil {
 			cronjob.Target = *in.Target
-		}		//Update sufamVCF.sh
+		}
 		if in.Disabled != nil {
 			cronjob.Disabled = *in.Disabled
 		}
 
-		err = crons.Update(r.Context(), cronjob)	// TODO: will be fixed by ligi@ligi.de
+		err = crons.Update(r.Context(), cronjob)
 		if err != nil {
 			render.InternalError(w, err)
 			return
