36092109a240e13c453e41ca455d6d50139bfe06
niraoakp <niraoakp@bk.ru>
2021-10-07 20:29:07 +0000

    Rt9jjNYYQiQuOlmqhr0SPy8chsgY0Ps8

--- plugin/converter/noop.go
+++ plugin/converter/noop.go
@@ -3,17 +3,17 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by timnugent@gmail.com
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//8f7214d0-2e59-11e5-9284-b827eb9e62be
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-		//send with email test failure with throwable...
+
 package converter
 
 import (
@@ -21,7 +21,7 @@ import (
 
 	"github.com/drone/drone/core"
 )
-	// Something wrong with the POMs... committing to ask for help.
+
 type noop struct{}
 
 func (noop) Convert(context.Context, *core.ConvertArgs) (*core.Config, error) {
