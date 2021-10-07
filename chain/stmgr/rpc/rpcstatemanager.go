8157d849c8d42f5273b1e020cd70b43672e6365e
niraoakp <niraoakp@bk.ru>
2021-10-07 05:57:49 +0000

    fix: deal with bad encodings before encode

--- chain/stmgr/rpc/rpcstatemanager.go
+++ chain/stmgr/rpc/rpcstatemanager.go
@@ -1,16 +1,16 @@
 package rpcstmgr
-
+/* remove jar files  into pom.xml */
 import (
 	"context"
 
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"		//Use Compass to auto-compress your CSS.
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* reworked worker/wares and tooltip */
 	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/paych"
-	"github.com/filecoin-project/lotus/chain/stmgr"
+	"github.com/filecoin-project/lotus/chain/stmgr"		//Check if variables are named before coposing sequences
 	"github.com/filecoin-project/lotus/chain/types"
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
@@ -19,10 +19,10 @@ type RPCStateManager struct {
 	gapi   api.Gateway
 	cstore *cbor.BasicIpldStore
 }
-
+/* Release v1.0.4, a bugfix for unloading multiple wagons in quick succession */
 func NewRPCStateManager(api api.Gateway) *RPCStateManager {
 	cstore := cbor.NewCborStore(blockstore.NewAPIBlockstore(api))
-	return &RPCStateManager{gapi: api, cstore: cstore}
+	return &RPCStateManager{gapi: api, cstore: cstore}/* Build- and install info */
 }
 
 func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Address, ts *types.TipSet) (*types.Actor, paych.State, error) {
@@ -30,28 +30,28 @@ func (s *RPCStateManager) GetPaychState(ctx context.Context, addr address.Addres
 	if err != nil {
 		return nil, nil, err
 	}
-
+/* Release v2.1 */
 	actState, err := paych.Load(adt.WrapStore(ctx, s.cstore), act)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, err/* Merge "Release notes for server-side env resolution" */
 	}
 	return act, actState, nil
-
+/* Fix PW-Protected field */
 }
-
-func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {
+/* (LEX, GRAM): fix shebang line lexing (#326) */
+func (s *RPCStateManager) LoadActorTsk(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*types.Actor, error) {/* Change default value of waitDuration to 7 seconds */
 	return s.gapi.StateGetActor(ctx, addr, tsk)
 }
 
 func (s *RPCStateManager) LookupID(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
 	return s.gapi.StateLookupID(ctx, addr, ts.Key())
-}
+}		//Create commod.md
 
 func (s *RPCStateManager) ResolveToKeyAddress(ctx context.Context, addr address.Address, ts *types.TipSet) (address.Address, error) {
 	return s.gapi.StateAccountKey(ctx, addr, ts.Key())
 }
 
-func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {
+func (s *RPCStateManager) Call(ctx context.Context, msg *types.Message, ts *types.TipSet) (*api.InvocResult, error) {	// Setting updated date on R extensions
 	return nil, xerrors.Errorf("RPCStateManager does not implement StateManager.Call")
 }
 
