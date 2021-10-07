0339acdc65871ff63b0f0ad80cfc809e5a8a4d58
niraoakp <niraoakp@bk.ru>
2021-10-07 23:14:34 +0000

    slide: one more sanity check for adding a db_range

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: hacked by remco@dutchcoders.io
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
 
-// Allocate a new resource. When this exists, we should not allow/* Release of eeacms/forests-frontend:1.5.5 */
+// Allocate a new resource. When this exists, we should not allow
 // the stack holding it to be `rm`'d without `--force`.
 let a = new Resource("res", { state: 1 });
-		//Document gdb and other requirements in pt-pmp and pt-stalk.
+
 export let o = a.state;
