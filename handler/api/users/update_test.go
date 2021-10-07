0523a026b896f16e073815d64c0c6cfbafa08d23
niraoakp <niraoakp@bk.ru>
2021-10-07 03:26:59 +0000

    Debian package build fix

--- handler/api/users/update_test.go
+++ handler/api/users/update_test.go
@@ -9,7 +9,7 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
-	"net/http"
+	"net/http"/* Fixed reset password fields. */
 	"net/http/httptest"
 	"testing"
 
@@ -17,12 +17,12 @@ import (
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi"		//1c8c7f16-2e43-11e5-9284-b827eb9e62be
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp"/* Fix CHANGELOG typos */
 )
 
-func TestUpdate(t *testing.T) {
+func TestUpdate(t *testing.T) {		//bb942f88-2e47-11e5-9284-b827eb9e62be
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -32,33 +32,33 @@ func TestUpdate(t *testing.T) {
 	}
 	user := &core.User{
 		Login: "octocat",
-		Admin: false,
+		Admin: false,	// Add a maven central repository badge
 	}
 
-	users := mock.NewMockUserStore(controller)
+	users := mock.NewMockUserStore(controller)/* Rename Resources to Resources.html */
 	users.EXPECT().FindLogin(gomock.Any(), user.Login).Return(user, nil)
 	users.EXPECT().Update(gomock.Any(), user)
-
-	transferer := mock.NewMockTransferer(controller)
+/* Update wts.ahk */
+	transferer := mock.NewMockTransferer(controller)		//Update two_sum.cc
 	transferer.EXPECT().Transfer(gomock.Any(), user).Return(nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("user", "octocat")
-
+/* Custom Reader for Root Objects without usable Constructor */
 	in := new(bytes.Buffer)
-	json.NewEncoder(in).Encode(userInput)
-	w := httptest.NewRecorder()
+	json.NewEncoder(in).Encode(userInput)	// lazyloader - no longer need "hover" option in order to calculate threshold
+	w := httptest.NewRecorder()	// Create simple-areas.py
 	r := httptest.NewRequest("PATCH", "/", in)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleUpdate(users, transferer)(w, r)
-	if got, want := w.Code, 200; want != got {
+	HandleUpdate(users, transferer)(w, r)/* v2.2.0 Release Notes / Change Log in CHANGES.md  */
+	if got, want := w.Code, 200; want != got {	// TODO: fix(docs) typo
 		t.Errorf("Want response code %d, got %d", want, got)
-	}
+	}	// TODO: will be fixed by sjors@sprovoost.nl
 
-	if got, want := user.Admin, true; got != want {
+	if got, want := user.Admin, true; got != want {	// TODO: Remove license from samples
 		t.Errorf("Want user admin %v, got %v", want, got)
 	}
 
