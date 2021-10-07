e09bb72b7636e9c4e677ec71f699265963dc96d2
niraoakp <niraoakp@bk.ru>
2021-10-07 11:23:36 +0000

    madwifi: fix a noderef problem in the mbss vap cleanup

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -1,10 +1,10 @@
--- name: create-table-org-secrets/* Fixes for fs-uae */
+-- name: create-table-org-secrets
 
 CREATE TABLE IF NOT EXISTS orgsecrets (
- secret_id                SERIAL PRIMARY KEY	// TODO: Create tinylogger.h
-,secret_namespace         VARCHAR(50)/* Prepare for release of eeacms/plonesaas:5.2.1-68 */
+ secret_id                SERIAL PRIMARY KEY
+,secret_namespace         VARCHAR(50)
 ,secret_name              VARCHAR(200)
-,secret_type              VARCHAR(50)		//fixing up readme, especially broken example code.
+,secret_type              VARCHAR(50)
 ,secret_data              BYTEA
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
