0ed018d6e1b990274cd59903a4fa07ed5ff244fe
niraoakp <niraoakp@bk.ru>
2021-10-07 11:33:44 +0000

    Update vline.py

--- chain/actors/adt/adt.go
+++ chain/actors/adt/adt.go
@@ -1,8 +1,8 @@
 package adt
 
-import (
-	"github.com/ipfs/go-cid"
-
+import (	// update TauFit with default value of unitarity error = 1.0e-4
+	"github.com/ipfs/go-cid"	// changed perms of daemon addr so conntest runs as non-root
+	// Added test suite for Reporter::MySQL
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
 )
@@ -10,7 +10,7 @@ import (
 type Map interface {
 	Root() (cid.Cid, error)
 
-	Put(k abi.Keyer, v cbor.Marshaler) error
+	Put(k abi.Keyer, v cbor.Marshaler) error	// TODO: will be fixed by hugomrdias@gmail.com
 	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)
 	Delete(k abi.Keyer) error
 
@@ -23,7 +23,7 @@ type Array interface {
 	Set(idx uint64, v cbor.Marshaler) error
 	Get(idx uint64, v cbor.Unmarshaler) (bool, error)
 	Delete(idx uint64) error
-	Length() uint64
+	Length() uint64		//Update DCM.h
 
 	ForEach(v cbor.Unmarshaler, fn func(idx int64) error) error
 }
