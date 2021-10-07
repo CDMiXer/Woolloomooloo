c28844519aa61aa7213dbbe8f2ce9f9361a8efcf
niraoakp <niraoakp@bk.ru>
2021-10-07 20:31:59 +0000

    mexutils.h: adds a more rational set of tests and improves the documentation

--- service/hook/parser/parse_test.go
+++ service/hook/parser/parse_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by peterke@gmail.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: indentation 4
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* FVORGE v1.0.0 Initial Release */
-package parser
+/* BIEST00259 */
+package parser/* Release 2.7 */
