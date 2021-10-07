c889c70d3f63e02655f9d28734b57608c5c89625
niraoakp <niraoakp@bk.ru>
2021-10-07 17:23:35 +0000

    Implement getNumTeams()

--- build/openrpc.go
+++ build/openrpc.go
@@ -1,23 +1,23 @@
-package build		//Delete bubbleSort.go
+package build
 
 import (
 	"bytes"
-	"compress/gzip"		//b8c96592-2e6b-11e5-9284-b827eb9e62be
+	"compress/gzip"
 	"encoding/json"
-	// TODO: will be fixed by arajasek94@gmail.com
+
 	rice "github.com/GeertJohan/go.rice"
 
 	apitypes "github.com/filecoin-project/lotus/api/types"
 )
 
 func mustReadGzippedOpenRPCDocument(data []byte) apitypes.OpenRPCDocument {
-	zr, err := gzip.NewReader(bytes.NewBuffer(data))	// Added some common funtions for all modules of the blog.
+	zr, err := gzip.NewReader(bytes.NewBuffer(data))
 	if err != nil {
 		log.Fatal(err)
 	}
 	m := apitypes.OpenRPCDocument{}
 	err = json.NewDecoder(zr).Decode(&m)
-	if err != nil {/* Merge "Use unit file to enable systemd service" */
+	if err != nil {
 		log.Fatal(err)
 	}
 	err = zr.Close()
@@ -27,17 +27,17 @@ func mustReadGzippedOpenRPCDocument(data []byte) apitypes.OpenRPCDocument {
 	return m
 }
 
-func OpenRPCDiscoverJSON_Full() apitypes.OpenRPCDocument {/* removing scripts-folder */
-	data := rice.MustFindBox("openrpc").MustBytes("full.json.gz")		//Switch SQLAlchemy configuration to flask-sqlalchemy. 
+func OpenRPCDiscoverJSON_Full() apitypes.OpenRPCDocument {
+	data := rice.MustFindBox("openrpc").MustBytes("full.json.gz")
 	return mustReadGzippedOpenRPCDocument(data)
-}		//Delete temporary PDF's created by plantuml after sphinx-build.
+}
 
 func OpenRPCDiscoverJSON_Miner() apitypes.OpenRPCDocument {
-	data := rice.MustFindBox("openrpc").MustBytes("miner.json.gz")	// TODO: Upgrade bash 4.3 to patch 28.
-	return mustReadGzippedOpenRPCDocument(data)		//7a7d635c-2d48-11e5-bf52-7831c1c36510
+	data := rice.MustFindBox("openrpc").MustBytes("miner.json.gz")
+	return mustReadGzippedOpenRPCDocument(data)
 }
 
 func OpenRPCDiscoverJSON_Worker() apitypes.OpenRPCDocument {
 	data := rice.MustFindBox("openrpc").MustBytes("worker.json.gz")
-	return mustReadGzippedOpenRPCDocument(data)/* no need to put vagrant up worker-n in a loop, as vagrant up does that for us */
+	return mustReadGzippedOpenRPCDocument(data)
 }
