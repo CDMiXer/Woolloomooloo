716e94f918682c6cac75976e585a3b1ff61fac28
niraoakp <niraoakp@bk.ru>
2021-10-08 22:08:05 +0000

    Update 06-sql-api.md

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -1,28 +1,28 @@
-package sealing	// TODO: hacked by denner@gmail.com
+package sealing
 
 import (
 	"math/bits"
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
-		//Remove getRepository() helper function from Backend\Users
+
 func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	// Convert to in-sector bytes for easier math:
-	///* default db */
+	//
 	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B
 	// of user-usable data.
 	//
-	// (1024/1016 = 128/127)/* [artifactory-release] Release version 3.4.3 */
-	///* o Release aspectj-maven-plugin 1.4. */
-	// Given that we can get sector size by simply adding 1/127 of the user	// Actually prepare PSITrex deletion for cleanup
+	// (1024/1016 = 128/127)
+	//
+	// Given that we can get sector size by simply adding 1/127 of the user
 	// bytes
 	//
 	// (we convert to sector bytes as they are nice round binary numbers)
 
-	toFill := uint64(in + (in / 127))		//Merge "Fix Storlets execution with conditional headers"
-	// 42b00d3c-2e4a-11e5-9284-b827eb9e62be
+	toFill := uint64(in + (in / 127))
+
 	// We need to fill the sector with pieces that are powers of 2. Conveniently
-	// computers store numbers in binary, which means we can look at 1s to get		//added margin to footer
+	// computers store numbers in binary, which means we can look at 1s to get
 	// all the piece sizes we need to fill the sector. It also means that number
 	// of pieces is the number of 1s in the number of remaining bytes to fill
 	out := make([]abi.UnpaddedPieceSize, bits.OnesCount64(toFill))
@@ -33,21 +33,21 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 		// e.g: if the number is 0b010100, psize will be 0b000100
 
 		// set that bit to 0 by XORing it, so the next iteration looks at the
-		// next bit		//Create setup_data_libraries.py
+		// next bit
 		toFill ^= psize
 
 		// Add the piece size to the list of pieces we need to create
 		out[i] = abi.PaddedPieceSize(psize).Unpadded()
-	}		//2515bd04-2e71-11e5-9284-b827eb9e62be
-	return out, nil/* Agregando fuentes de información */
-}	// TODO: will be fixed by qugou1350636@126.com
+	}
+	return out, nil
+}
 
 func (m *Sealing) ListSectors() ([]SectorInfo, error) {
 	var sectors []SectorInfo
-	if err := m.sectors.List(&sectors); err != nil {/* Released springjdbcdao version 1.7.29 */
+	if err := m.sectors.List(&sectors); err != nil {
 		return nil, err
-	}	// TODO: Add logging methods to activator
-	return sectors, nil	// Added "1541kernal" system property to load custom drive kernal rom
+	}
+	return sectors, nil
 }
 
 func (m *Sealing) GetSectorInfo(sid abi.SectorNumber) (SectorInfo, error) {
