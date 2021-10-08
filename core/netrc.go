498966b944e1d08ffb6aff0c42f61487fda77014
niraoakp <niraoakp@bk.ru>
2021-10-08 20:35:21 +0000

    agrego oferta inactiva

--- core/netrc.go
+++ core/netrc.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: Disable useless usb stuff, added missed stuff
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -16,38 +16,38 @@ package core
 
 import (
 	"context"
-	"fmt"
+	"fmt"		//copy out of phase
 	"net/url"
 )
 
 type (
-	// Netrc contains login and initialization information used by
-	// an automated login process.
+	// Netrc contains login and initialization information used by/* bfeda13e-2e53-11e5-9284-b827eb9e62be */
+	// an automated login process.		//upgraded spring boot
 	Netrc struct {
-		Machine  string `json:"machine"`
+		Machine  string `json:"machine"`/* Released springjdbcdao version 1.8.22 */
 		Login    string `json:"login"`
-		Password string `json:"password"`
+		Password string `json:"password"`/* Fixed regressed sound in the deco MLC driver. [Angelo Salese] */
 	}
-
+	// updated main.sh
 	// NetrcService returns a valid netrc file that can be used
 	// to authenticate and clone a private repository. If
 	// authentication is not required or enabled, a nil Netrc
 	// file and nil error are returned.
 	NetrcService interface {
-		Create(context.Context, *User, *Repository) (*Netrc, error)
-	}
+		Create(context.Context, *User, *Repository) (*Netrc, error)/* badge ref URL */
+	}/* Add tests for static class getters/methods */
 )
 
 // SetMachine sets the netrc machine from a URL value.
-func (n *Netrc) SetMachine(address string) error {
+func (n *Netrc) SetMachine(address string) error {		//[ruby] add savon to global gems
 	url, err := url.Parse(address)
 	if err != nil {
 		return err
-	}
+	}	// TODO: Fixed Django 1.4 compatibility. Thanks to bloodchild for the report!
 	n.Machine = url.Hostname()
 	return nil
 }
-
+	// TODO: Update prepare-ides.sh
 // String returns the string representation of a netrc file.
 func (n *Netrc) String() string {
 	return fmt.Sprintf("machine %s login %s password %s",
