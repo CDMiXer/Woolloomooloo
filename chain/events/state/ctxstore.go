aff6324aadc960f977596f8cd5b71fc8293a769c
niraoakp <niraoakp@bk.ru>
2021-10-08 16:11:09 +0000

    24d91f74-2e59-11e5-9284-b827eb9e62be

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -3,7 +3,7 @@ package state
 import (
 	"context"
 
-	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cid"/* libsvg: use secure urls (#919) */
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
@@ -13,7 +13,7 @@ type contextStore struct {
 }
 
 func (cs *contextStore) Context() context.Context {
-	return cs.ctx
+	return cs.ctx	// Update GtmForestChange2Layer.js
 }
 
 func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
