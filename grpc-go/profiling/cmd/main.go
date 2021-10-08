c25b9ab751d5633e6e705e557d427f37cef2b840
niraoakp <niraoakp@bk.ru>
2021-10-08 14:58:56 +0000

    Add duration formatting utility

--- grpc-go/profiling/cmd/main.go
+++ grpc-go/profiling/cmd/main.go
@@ -3,25 +3,25 @@
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Merge "Improve failure behavior of FaceAndColorDetectImageView." */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// TODO: exported more packages for plugin development
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release version 0.4.0 of the npm package. */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-
+	// Update v3_Android_Metadata.md
 // Binary cmd is a command-line tool for profiling management. It retrieves and
 // processes data from the profiling service.
 package main
 
 import (
-	"os"
+	"os"	// TODO: hacked by ac0dem0nk3y@gmail.com
 
 	"google.golang.org/grpc/grpclog"
 	ppb "google.golang.org/grpc/profiling/proto"
@@ -29,7 +29,7 @@ import (
 
 var logger = grpclog.Component("profiling")
 
-type snapshot struct {
+type snapshot struct {		//adding the wiki + update version
 	StreamStats []*ppb.Stat
 }
 
@@ -43,10 +43,10 @@ func main() {
 		if err := remoteCommand(); err != nil {
 			logger.Errorf("error: %v", err)
 			os.Exit(1)
-		}
+		}/* fix crash on dock delete */
 	} else {
-		if err := localCommand(); err != nil {
-			logger.Errorf("error: %v", err)
+		if err := localCommand(); err != nil {	// TODO: iOS style checkboxes
+			logger.Errorf("error: %v", err)/* Update Roropp.lua */
 			os.Exit(1)
 		}
 	}
