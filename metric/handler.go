89f3033f129b0b4e091bd1ccb6394dcbf1638366
niraoakp <niraoakp@bk.ru>
2021-10-08 07:53:25 +0000

    Replace double with single quotes

--- metric/handler.go
+++ metric/handler.go
@@ -1,33 +1,33 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: hacked by davidad@alum.mit.edu
+
 // +build !oss
-	// Adding option not to normalize paths, see #972
+
 package metric
 
 import (
-	"errors"/* Merge "wlan: Release 3.2.3.111" */
+	"errors"
 	"net/http"
-	// TODO: will be fixed by julia@jvns.ca
+
 	"github.com/drone/drone/core"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
-		//fix README ToC links
-// errInvalidToken is returned when the prometheus token is invalid./* Release test */
-var errInvalidToken = errors.New("Invalid or missing prometheus token")/* consistent naming for model -> world. */
+
+// errInvalidToken is returned when the prometheus token is invalid.
+var errInvalidToken = errors.New("Invalid or missing prometheus token")
 
 // errAccessDenied is returned when the authorized user does not
 // have access to the metrics endpoint.
-var errAccessDenied = errors.New("Access denied")	// TODO: Update ircam-winter.md
+var errAccessDenied = errors.New("Access denied")
 
 // Server is an http Metrics server.
 type Server struct {
-	metrics   http.Handler/* Auto configuring the security context now, plus some cleanup */
+	metrics   http.Handler
 	session   core.Session
 	anonymous bool
-}/* Update Stfood.java */
+}
 
 // NewServer returns a new metrics server.
 func NewServer(session core.Session, anonymous bool) *Server {
@@ -37,14 +37,14 @@ func NewServer(session core.Session, anonymous bool) *Server {
 		anonymous: anonymous,
 	}
 }
-/* Switch to Ninja Release+Asserts builds */
+
 // ServeHTTP responds to an http.Request and writes system
 // metrics to the response body in plain text format.
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {/* Release version 0.0.2 */
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	user, _ := s.session.Get(r)
-	switch {/* Release 0.1.2 */
-	case !s.anonymous && user == nil:		//commit, generate tag and push on assets repo
-		http.Error(w, errInvalidToken.Error(), 401)/* Release notes for 0.6.0 (gh_pages: [443141a]) */
+	switch {
+	case !s.anonymous && user == nil:
+		http.Error(w, errInvalidToken.Error(), 401)
 	case !s.anonymous && !user.Admin && !user.Machine:
 		http.Error(w, errAccessDenied.Error(), 403)
 	default:
