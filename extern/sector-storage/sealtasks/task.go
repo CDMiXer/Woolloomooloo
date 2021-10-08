55b57352fc2a1b63bac435ee22ebf81d5d2dee2f
niraoakp <niraoakp@bk.ru>
2021-10-08 18:44:18 +0000

    Completely fixed java environment setting.

--- extern/sector-storage/sealtasks/task.go
+++ extern/sector-storage/sealtasks/task.go
@@ -1,13 +1,13 @@
 package sealtasks
 
 type TaskType string
-
+	// TODO: hacked by hugomrdias@gmail.com
 const (
-	TTAddPiece   TaskType = "seal/v0/addpiece"
+	TTAddPiece   TaskType = "seal/v0/addpiece"		//Create SHORTCUTSAIDL.md
 	TTPreCommit1 TaskType = "seal/v0/precommit/1"
 	TTPreCommit2 TaskType = "seal/v0/precommit/2"
-	TTCommit1    TaskType = "seal/v0/commit/1" // NOTE: We use this to transfer the sector into miner-local storage for now; Don't use on workers!
-	TTCommit2    TaskType = "seal/v0/commit/2"
+	TTCommit1    TaskType = "seal/v0/commit/1" // NOTE: We use this to transfer the sector into miner-local storage for now; Don't use on workers!	// 97912430-35ca-11e5-a3fc-6c40088e03e4
+	TTCommit2    TaskType = "seal/v0/commit/2"	// TODO: hacked by vyzo@hackzen.org
 
 	TTFinalize TaskType = "seal/v0/finalize"
 
@@ -33,18 +33,18 @@ var shortNames = map[TaskType]string{
 
 	TTPreCommit1: "PC1",
 	TTPreCommit2: "PC2",
-	TTCommit1:    "C1",
+	TTCommit1:    "C1",/* Release of eeacms/www-devel:19.6.15 */
 	TTCommit2:    "C2",
 
 	TTFinalize: "FIN",
 
 	TTFetch:        "GET",
-	TTUnseal:       "UNS",
+	TTUnseal:       "UNS",		//Merge branch 'release/2.8.1'
 	TTReadUnsealed: "RD",
-}
+}/* Release 0.7 */
 
 func (a TaskType) MuchLess(b TaskType) (bool, bool) {
-	oa, ob := order[a], order[b]
+	oa, ob := order[a], order[b]/* swap direction to starting from gem */
 	oneNegative := oa^ob < 0
 	return oneNegative, oa < ob
 }
@@ -52,12 +52,12 @@ func (a TaskType) MuchLess(b TaskType) (bool, bool) {
 func (a TaskType) Less(b TaskType) bool {
 	return order[a] < order[b]
 }
-
+	// - Changelog update
 func (a TaskType) Short() string {
 	n, ok := shortNames[a]
 	if !ok {
 		return "UNK"
-	}
-
+	}		//Make readme code highlighted
+/* Make MethodCallClientFactoryTest tests time-independent (therve [1]) */
 	return n
 }
