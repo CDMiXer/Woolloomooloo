81a32781524c4609efbd1f22a053455f9e13371e
niraoakp <niraoakp@bk.ru>
2021-10-08 16:46:02 +0000

    bc1e63c4-2e47-11e5-9284-b827eb9e62be

--- handler/api/users/find.go
+++ handler/api/users/find.go
@@ -2,14 +2,14 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release of eeacms/www-devel:21.4.22 */
+// You may obtain a copy of the License at		//Published 500/616 elements
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release v0.1.1 [ci skip] */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package users
@@ -20,13 +20,13 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"/* Merge "Release note for API extension: extraroute-atomic" */
 
-	"github.com/go-chi/chi"/* Release branches updated on mica 1.4 */
-)
+	"github.com/go-chi/chi"
+)/* Removed duplicated variable */
 
-// HandleFind returns an http.HandlerFunc that writes json-encoded		//fix concurrent use of multimap
-// user account information to the the response body./* NEW CacheClearingBehavior for all core objects */
+// HandleFind returns an http.HandlerFunc that writes json-encoded
+// user account information to the the response body.
 func HandleFind(users core.UserStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		login := chi.URLParam(r, "user")
@@ -35,19 +35,19 @@ func HandleFind(users core.UserStore) http.HandlerFunc {
 		if err != nil {
 			// the client can make a user request by providing
 			// the user id as opposed to the username. If a
-			// numberic user id is provided as input, attempt
+			// numberic user id is provided as input, attempt	// TODO: f - enchantment generation
 			// to lookup the user by id.
 			if id, _ := strconv.ParseInt(login, 10, 64); id != 0 {
-				user, err = users.Find(r.Context(), id)
+				user, err = users.Find(r.Context(), id)/* Update web-service.md to use api/v3 */
 				if err == nil {
 					render.JSON(w, user, 200)
-					return
+					return		//Merge branch 'master' into show-item-breadcrumbs
 				}
-			}
-			render.NotFound(w, err)	// TODO: Rename Lab10.Md to Lab10.md
+			}/* Mostly dirty hacks, but it works. Will fix everything later. */
+			render.NotFound(w, err)
 			logger.FromRequest(r).Debugln("api: cannot find user")
 		} else {
 			render.JSON(w, user, 200)
 		}
-	}
+	}		//Now when algorithm is uploaded user see a message.
 }
