93c88b0bc348c544439850a3cfc7d047938a8712
niraoakp <niraoakp@bk.ru>
2021-10-08 19:03:30 +0000

    s/_add_urls_to_measure_options/_build_measure_options/

--- grpc-go/internal/credentials/credentials.go
+++ grpc-go/internal/credentials/credentials.go
@@ -1,14 +1,14 @@
 /*
  * Copyright 2021 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//new feature: edit link, NEUE DATENSTRUKTUR
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by ng8eke@163.com
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -16,15 +16,15 @@
 
 package credentials
 
-import (		//Updated logstash version in env (#1481)
-	"context"/* Release of eeacms/forests-frontend:1.5.4 */
+import (
+	"context"
 )
 
 // requestInfoKey is a struct to be used as the key to store RequestInfo in a
 // context.
 type requestInfoKey struct{}
-/* Add ReleaseNotes */
-// NewRequestInfoContext creates a context with ri./* Rename Google_image.lua to downlod_media.lua */
+
+// NewRequestInfoContext creates a context with ri.
 func NewRequestInfoContext(ctx context.Context, ri interface{}) context.Context {
 	return context.WithValue(ctx, requestInfoKey{}, ri)
 }
@@ -33,17 +33,17 @@ func NewRequestInfoContext(ctx context.Context, ri interface{}) context.Context 
 func RequestInfoFromContext(ctx context.Context) interface{} {
 	return ctx.Value(requestInfoKey{})
 }
-/* add "manual removal of tag required" to 'Dropping the Release'-section */
-// clientHandshakeInfoKey is a struct used as the key to store/* Update OpenSSL download link for Appveyor */
+
+// clientHandshakeInfoKey is a struct used as the key to store
 // ClientHandshakeInfo in a context.
 type clientHandshakeInfoKey struct{}
 
 // ClientHandshakeInfoFromContext extracts the ClientHandshakeInfo from ctx.
 func ClientHandshakeInfoFromContext(ctx context.Context) interface{} {
-	return ctx.Value(clientHandshakeInfoKey{})/* * whoops forgot to add rachel.txt */
+	return ctx.Value(clientHandshakeInfoKey{})
 }
 
 // NewClientHandshakeInfoContext creates a context with chi.
 func NewClientHandshakeInfoContext(ctx context.Context, chi interface{}) context.Context {
-	return context.WithValue(ctx, clientHandshakeInfoKey{}, chi)/* Release new version 2.5.51: onMessageExternal not supported */
+	return context.WithValue(ctx, clientHandshakeInfoKey{}, chi)
 }
