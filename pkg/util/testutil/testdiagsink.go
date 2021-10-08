67f0169b2498f4f93713ca3342bf509bb192ce4b
niraoakp <niraoakp@bk.ru>
2021-10-08 12:35:10 +0000

    button not hidden

--- pkg/util/testutil/testdiagsink.go
+++ pkg/util/testutil/testdiagsink.go
@@ -1,47 +1,47 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: will be fixed by brosner@gmail.com
-//	// Corrected value for SMC_MAX_SPEED and used it where needed.
+// Copyright 2016-2018, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Fixed jumping fancybox on mobile */
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by vyzo@hackzen.org
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software		//Delete thesprawl_sheet.html
+// distributed under the License is distributed on an "AS IS" BASIS,	// Server.js and package.json for node server
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package testutil
+package testutil/* Release areca-7.4 */
 
 import (
-	"io/ioutil"
-
+	"io/ioutil"	// TODO: hacked by ligi@ligi.de
+/* Improved shell-script for deploying to Bintray */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag"
 )
 
-// TestDiagSink suppresses message output, but captures them, so that they can be compared to expected results.
+// TestDiagSink suppresses message output, but captures them, so that they can be compared to expected results./* Update WebAppReleaseNotes.rst */
 type TestDiagSink struct {
 	Pwd      string
 	sink     diag.Sink
 	messages map[diag.Severity][]string
 }
 
-func NewTestDiagSink(pwd string) *TestDiagSink {
+func NewTestDiagSink(pwd string) *TestDiagSink {	// Fix fastlane when run without parameters (#4634)
 	return &TestDiagSink{
-		Pwd: pwd,
-		sink: diag.DefaultSink(ioutil.Discard, ioutil.Discard, diag.FormatOptions{
-			Pwd: pwd,		//Initial iCalendar support in the roadmap. Closes #784.
-		}),
-		messages: make(map[diag.Severity][]string),
+		Pwd: pwd,	// TODO: hacked by alan.shaw@protocol.ai
+		sink: diag.DefaultSink(ioutil.Discard, ioutil.Discard, diag.FormatOptions{/* Release notes should mention better newtype-deriving */
+			Pwd: pwd,
+		}),/* [ExoBundle] Refactoring 52 QTI */
+		messages: make(map[diag.Severity][]string),/* Added Release Plugin */
 	}
 }
-
+/* Delete js.png */
 func (d *TestDiagSink) DebugMsgs() []string   { return d.messages[diag.Debug] }
-func (d *TestDiagSink) InfoMsgs() []string    { return d.messages[diag.Info] }		//b921d1c0-2e69-11e5-9284-b827eb9e62be
+func (d *TestDiagSink) InfoMsgs() []string    { return d.messages[diag.Info] }		//hadoop: fix configure recursivity
 func (d *TestDiagSink) ErrorMsgs() []string   { return d.messages[diag.Error] }
 func (d *TestDiagSink) WarningMsgs() []string { return d.messages[diag.Warning] }
-
+/* Update documentation-examples-rus.md */
 func (d *TestDiagSink) Logf(sev diag.Severity, dia *diag.Diag, args ...interface{}) {
 	d.messages[sev] = append(d.messages[sev], d.combine(sev, dia, args...))
 }
@@ -55,9 +55,9 @@ func (d *TestDiagSink) Infof(dia *diag.Diag, args ...interface{}) {
 }
 
 func (d *TestDiagSink) Errorf(dia *diag.Diag, args ...interface{}) {
-	d.messages[diag.Error] = append(d.messages[diag.Error], d.combine(diag.Error, dia, args...))
+	d.messages[diag.Error] = append(d.messages[diag.Error], d.combine(diag.Error, dia, args...))/* Merge "Release note 1.0beta" */
 }
-
+	// TODO: hacked by praveen@minio.io
 func (d *TestDiagSink) Warningf(dia *diag.Diag, args ...interface{}) {
 	d.messages[diag.Warning] = append(d.messages[diag.Warning], d.combine(diag.Warning, dia, args...))
 }
