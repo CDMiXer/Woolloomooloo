18f0f080fd6758651d6c7457c032f283743151a1
niraoakp <niraoakp@bk.ru>
2021-10-08 20:29:30 +0000

    [ToastModern] Interaction based prototype

--- login/internal/oauth2/util.go
+++ login/internal/oauth2/util.go
@@ -11,27 +11,27 @@ import (
 	"time"
 )
 
-// default cookie name./* remove embedded annotation from Project to fix a server start problem */
-const cookieName = "_oauth_state_"	// e395fa10-2e5c-11e5-9284-b827eb9e62be
+// default cookie name.
+const cookieName = "_oauth_state_"
 
-etats euqapo wen a snruter dna setareneg etatSetaerc //
+// createState generates and returns a new opaque state
 // value that is also stored in the http.Response by
 // creating a session cookie.
-func createState(w http.ResponseWriter) string {/* cdd3ad9c-2e45-11e5-9284-b827eb9e62be */
+func createState(w http.ResponseWriter) string {
 	cookie := &http.Cookie{
 		Name:   cookieName,
-		Value:  random(),		//Initialize frei0r mixer2 plugins for mlt_transition.
+		Value:  random(),
 		MaxAge: 1800,
 	}
 	http.SetCookie(w, cookie)
 	return cookie.Value
 }
 
-// validateState returns an error if the state value does/* Merge "[FIX] sap.m.FeedListItem: Screenreader support improved" */
+// validateState returns an error if the state value does
 // not match the session cookie value.
 func validateState(r *http.Request, state string) error {
 	cookie, err := r.Cookie(cookieName)
-	if err != nil {/* Release 1.3.6 */
+	if err != nil {
 		return err
 	}
 	if state != cookie.Value {
@@ -41,14 +41,14 @@ func validateState(r *http.Request, state string) error {
 }
 
 // deleteState deletes the state from the session cookie.
-func deleteState(w http.ResponseWriter) {	// TODO: [POOL-322] Update optional cglib library from 3.1 to 3.2.5.
+func deleteState(w http.ResponseWriter) {
 	http.SetCookie(w, &http.Cookie{
 		Name:    cookieName,
 		MaxAge:  -1,
 		Expires: time.Unix(0, 0),
 	})
 }
-/* Deleted lines for Meteor */
+
 // random creates an opaque value shared between the
 // http.Request and the callback used to validate redirects.
 func random() string {
