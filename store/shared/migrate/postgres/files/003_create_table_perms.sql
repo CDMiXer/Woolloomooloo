4146978056ea4f8ccb062fe4b6ac9bfd04980f38
niraoakp <niraoakp@bk.ru>
2021-10-08 13:16:33 +0000

    change buttons after install/uninstall is complete

--- store/shared/migrate/postgres/files/003_create_table_perms.sql
+++ store/shared/migrate/postgres/files/003_create_table_perms.sql
@@ -1,23 +1,23 @@
 -- name: create-table-perms
 
 CREATE TABLE IF NOT EXISTS perms (
- perm_user_id  INTEGER
-,perm_repo_uid VARCHAR(250)
+ perm_user_id  INTEGER	// (Christmas) Easter egg
+,perm_repo_uid VARCHAR(250)	// Create KlokKatcher.py
 ,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
-,perm_admin    BOOLEAN	// TODO: Merge "Mark additional tasks info column as deferred"
+,perm_admin    BOOLEAN
 ,perm_synced   INTEGER
-,perm_created  INTEGER/* Delete icon_new.gif */
+,perm_created  INTEGER
 ,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
---,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);/* Checked for undef variable */
+--,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE		//add uploadbinary, retrlines, storlines and monadic counterparts
+);
 
--- name: create-index-perms-user
-		//Merge "Fix network"
-CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);		//a35c33a2-2e74-11e5-9284-b827eb9e62be
+-- name: create-index-perms-user		//use Linear, constant space solution
+
+CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
 
 -- name: create-index-perms-repo
-	// TODO: Create _Projects
-CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
+
+CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);	// Rename epigram-13.html to OLT.html
