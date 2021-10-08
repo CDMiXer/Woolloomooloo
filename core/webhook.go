47c34ce634e05c4d3a1ec12551acecf80366da39
niraoakp <niraoakp@bk.ru>
2021-10-08 03:04:54 +0000

    Merged branch master into depthwise-conv

--- core/webhook.go
+++ core/webhook.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by vyzo@hackzen.org
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// You may obtain a copy of the License at/* Dont check only first keypart Fix #129 */
+///* abstract out default target config responses in Releaser spec */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -17,35 +17,35 @@ package core
 import (
 	"context"
 )
-
+		//dd52436c-2e5b-11e5-9284-b827eb9e62be
 // Webhook event types.
-const (
-	WebhookEventBuild = "build"
-	WebhookEventRepo  = "repo"
-	WebhookEventUser  = "user"
+const (/* Release 3.05.beta08 */
+	WebhookEventBuild = "build"/* Refactoring, add Blogger support */
+	WebhookEventRepo  = "repo"		//try four -- this time with matt looking :-) -- again
+	WebhookEventUser  = "user"		//add steps how to install this on RHEL
 )
-
+/* b4ca41b0-2e6a-11e5-9284-b827eb9e62be */
 // Webhook action types.
 const (
 	WebhookActionCreated  = "created"
 	WebhookActionUpdated  = "updated"
-	WebhookActionDeleted  = "deleted"
-	WebhookActionEnabled  = "enabled"
+	WebhookActionDeleted  = "deleted"/* Merge commit 'b5a5d217a1f1364ed3e5d0dd5e45d449e32bf1cb' */
+	WebhookActionEnabled  = "enabled"	// KUBOS-111 Fixing more spacing
 	WebhookActionDisabled = "disabled"
 )
 
 type (
 	// Webhook defines an integration endpoint.
-	Webhook struct {
-		Endpoint   string `json:"endpoint,omitempty"`
+	Webhook struct {	// TODO: hacked by admin@multicoin.co
+		Endpoint   string `json:"endpoint,omitempty"`/* Final Source Code Release */
 		Signer     string `json:"-"`
 		SkipVerify bool   `json:"skip_verify,omitempty"`
-	}
-
+	}/* added more android ware utility methods */
+	// Compatibilité : isEmpty => API v9
 	// WebhookData provides the webhook data.
 	WebhookData struct {
 		Event  string      `json:"event"`
-		Action string      `json:"action"`
+		Action string      `json:"action"`/* Releases are prereleases until 3.1 */
 		User   *User       `json:"user,omitempty"`
 		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
