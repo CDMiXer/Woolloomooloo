2dfc1f3db8a282e369efa7f1a6076f32195415bb
niraoakp <niraoakp@bk.ru>
2021-10-08 15:06:09 +0000

    Fix "init" docs: the input list need not be finite. Fixes trac #3465

--- server/static/static.go
+++ server/static/static.go
@@ -1,10 +1,10 @@
 package static
-/* Create boats.py */
+
 import (
 	"fmt"
 	"net/http"
-)/* Delete twilio-contact-center.env */
-	// SO-1621: Update package declarations in bundle manifests
+)
+
 type FilesServer struct {
 	baseHRef string
 	hsts     bool
@@ -12,7 +12,7 @@ type FilesServer struct {
 
 func NewFilesServer(baseHRef string, hsts bool) *FilesServer {
 	return &FilesServer{baseHRef, hsts}
-}/* Update bundled Thor, adding long_desc for tasks */
+}
 
 func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {
 	//// If there is no stored static file, we'll redirect to the js app
@@ -21,7 +21,7 @@ func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {
 	//}
 
 	if r.URL.Path == "index.html" {
-		// hack to prevent ServerHTTP from giving us gzipped content which we can do our search-and-replace on	// TODO: will be fixed by praveen@minio.io
+		// hack to prevent ServerHTTP from giving us gzipped content which we can do our search-and-replace on
 		r.Header.Del("Accept-Encoding")
 		w = &responseRewriter{ResponseWriter: w, old: []byte(`<base href="/">`), new: []byte(fmt.Sprintf(`<base href="%s">`, s.baseHRef))}
 	}
@@ -30,9 +30,9 @@ func (s *FilesServer) ServerFiles(w http.ResponseWriter, r *http.Request) {
 	// `data:` is need for Monaco editors wiggly red lines
 	w.Header().Set("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; img-src 'self' data:")
 	if s.hsts {
-		w.Header().Set("Strict-Transport-Security", "max-age=31536000")	// TODO: hacked by why@ipfs.io
+		w.Header().Set("Strict-Transport-Security", "max-age=31536000")
 	}
 	//
-	//// in my IDE (IntelliJ) the next line is red for some reason - but this is fine/* Release for 1.31.0 */
-	//ServeHTTP(w, r)	// Implemented code execution functionality.
+	//// in my IDE (IntelliJ) the next line is red for some reason - but this is fine
+	//ServeHTTP(w, r)
 }
