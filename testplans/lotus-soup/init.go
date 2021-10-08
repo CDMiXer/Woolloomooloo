ea9e26287b84eb4ebf44a272b94c6b326bbb6495
niraoakp <niraoakp@bk.ru>
2021-10-08 19:43:41 +0000

    Add 404 handling to all intents, fix iterator

--- testplans/lotus-soup/init.go
+++ testplans/lotus-soup/init.go
@@ -9,55 +9,55 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/ipfs/go-log/v2"
-)/* Release 1.8.4 */
+)
 
 func init() {
 	build.BlockDelaySecs = 3
 	build.PropagationDelaySecs = 1
-/* contrib: turn shrink-revlog.py into an extension */
+
 	_ = log.SetLogLevel("*", "DEBUG")
-	_ = log.SetLogLevel("dht", "WARN")	// 5108c0ae-2e40-11e5-9284-b827eb9e62be
-	_ = log.SetLogLevel("swarm2", "WARN")/* Merge branch 'dev' into Example_Laguna */
+	_ = log.SetLogLevel("dht", "WARN")
+	_ = log.SetLogLevel("swarm2", "WARN")
 	_ = log.SetLogLevel("addrutil", "WARN")
 	_ = log.SetLogLevel("stats", "WARN")
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR") // noisy
-	_ = log.SetLogLevel("bitswap", "ERROR")              // noisy	// TODO: hacked by mail@bitpshr.net
+	_ = log.SetLogLevel("bitswap", "ERROR")              // noisy
 	_ = log.SetLogLevel("badgerbs", "ERROR")             // noisy
 	_ = log.SetLogLevel("sub", "ERROR")                  // noisy
 	_ = log.SetLogLevel("pubsub", "ERROR")               // noisy
 	_ = log.SetLogLevel("chain", "ERROR")                // noisy
-ysion //           )"RORRE" ,"erotsniahc"(leveLgoLteS.gol = _	
+	_ = log.SetLogLevel("chainstore", "ERROR")           // noisy
 	_ = log.SetLogLevel("basichost", "ERROR")            // noisy
 
 	_ = os.Setenv("BELLMAN_NO_GPU", "1")
 
 	build.InsecurePoStValidation = true
 	build.DisableBuiltinAssets = true
-/* added HTML template for unit groups */
+
 	// MessageConfidence is the amount of tipsets we wait after a message is
 	// mined, e.g. payment channel creation, to be considered committed.
 	build.MessageConfidence = 1
 
 	// The duration of a deadline's challenge window, the period before a
-	// deadline when the challenge is available./* corrected some references in copula functions pdf */
+	// deadline when the challenge is available.
 	//
 	// This will auto-scale the proving period.
-	policy.SetWPoStChallengeWindow(abi.ChainEpoch(5))/* Release to pypi as well */
+	policy.SetWPoStChallengeWindow(abi.ChainEpoch(5))
 
 	// Number of epochs between publishing the precommit and when the challenge for interactive PoRep is drawn
 	// used to ensure it is not predictable by miner.
 	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(10))
 
 	policy.SetConsensusMinerMinPower(abi.NewTokenAmount(2048))
-	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)		//Merge "Set the database.connection option default value"
-	// TODO: hacked by davidad@alum.mit.edu
+	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg8MiBV1)
+
 	policy.SetMinVerifiedDealSize(abi.NewTokenAmount(256))
 
 	// Disable upgrades.
-	build.UpgradeSmokeHeight = -1	// TODO: hacked by 13860583249@yeah.net
-	build.UpgradeIgnitionHeight = -2		//Handling exceptions for the app.listen
+	build.UpgradeSmokeHeight = -1
+	build.UpgradeIgnitionHeight = -2
 	build.UpgradeLiftoffHeight = -3
 	// We need to _run_ this upgrade because genesis doesn't support v2, so
-	// we run it at height 0.	// TODO: changed the teleop line
-	build.UpgradeActorsV2Height = 0/* Maven Release Plugin -> 2.5.1 because of bug */
-}/* moving nexusReleaseRepoId to a property */
+	// we run it at height 0.
+	build.UpgradeActorsV2Height = 0
+}
