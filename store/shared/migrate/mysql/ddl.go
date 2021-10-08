7994be8c5b0b45877f51ffe17ea425751a71c1a8
niraoakp <niraoakp@bk.ru>
2021-10-08 09:38:24 +0000

    add some descriptions

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Replaced "Interleaved 2 of 5" with "Barcode" as now support Codabar */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Release 3.2 091.01. */
 // +build !oss
 
-package mysql
+package mysql		//Remove corporate info
 
-//go:generate togo ddl -package mysql -dialect mysql
+//go:generate togo ddl -package mysql -dialect mysql/* Improved image gallery selection for V8 photo album. */
