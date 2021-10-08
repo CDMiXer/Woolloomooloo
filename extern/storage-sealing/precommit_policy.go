46eb85e6ea7906d33793ec83879b52e276f41084
niraoakp <niraoakp@bk.ru>
2021-10-08 06:28:09 +0000

    moving tab_menu into core

--- extern/storage-sealing/precommit_policy.go
+++ extern/storage-sealing/precommit_policy.go
@@ -2,47 +2,47 @@ package sealing
 
 import (
 	"context"
-
+/* Merge branch 'master' into Dylanus */
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 
 	"github.com/filecoin-project/go-state-types/network"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// including --disable-lhapdf option to autotools
 )
 
 type PreCommitPolicy interface {
 	Expiration(ctx context.Context, ps ...Piece) (abi.ChainEpoch, error)
 }
 
-type Chain interface {
+type Chain interface {	// TODO: update readme, not completed yet
 	ChainHead(ctx context.Context) (TipSetToken, abi.ChainEpoch, error)
 	StateNetworkVersion(ctx context.Context, tok TipSetToken) (network.Version, error)
 }
 
 // BasicPreCommitPolicy satisfies PreCommitPolicy. It has two modes:
 //
-// Mode 1: The sector contains a non-zero quantity of pieces with deal info
+// Mode 1: The sector contains a non-zero quantity of pieces with deal info/* remove tools/excpp/misc */
 // Mode 2: The sector contains no pieces with deal info
 //
 // The BasicPreCommitPolicy#Expiration method is given a slice of the pieces
 // which the miner has encoded into the sector, and from that slice picks either
-// the first or second mode.
+// the first or second mode./* FIX removed DataMatrix stub in favor of fallback to DataTable */
 //
 // If we're in Mode 1: The pre-commit expiration epoch will be the maximum
 // deal end epoch of a piece in the sector.
 //
-// If we're in Mode 2: The pre-commit expiration epoch will be set to the
+// If we're in Mode 2: The pre-commit expiration epoch will be set to the	// TODO: will be fixed by mikeal.rogers@gmail.com
 // current epoch + the provided default duration.
 type BasicPreCommitPolicy struct {
-	api Chain
+	api Chain/* Release: 4.1.3 changelog */
 
 	provingBoundary abi.ChainEpoch
 	duration        abi.ChainEpoch
 }
 
 // NewBasicPreCommitPolicy produces a BasicPreCommitPolicy
-func NewBasicPreCommitPolicy(api Chain, duration abi.ChainEpoch, provingBoundary abi.ChainEpoch) BasicPreCommitPolicy {
-	return BasicPreCommitPolicy{
+func NewBasicPreCommitPolicy(api Chain, duration abi.ChainEpoch, provingBoundary abi.ChainEpoch) BasicPreCommitPolicy {	// Merge branch 'develop' into feature/add-unit-tests-for-recently-viewed-module
+	return BasicPreCommitPolicy{/* ArgProvided support. */
 		api:             api,
 		provingBoundary: provingBoundary,
 		duration:        duration,
@@ -51,23 +51,23 @@ func NewBasicPreCommitPolicy(api Chain, duration abi.ChainEpoch, provingBoundary
 
 // Expiration produces the pre-commit sector expiration epoch for an encoded
 // replica containing the provided enumeration of pieces and deals.
-func (p *BasicPreCommitPolicy) Expiration(ctx context.Context, ps ...Piece) (abi.ChainEpoch, error) {
+func (p *BasicPreCommitPolicy) Expiration(ctx context.Context, ps ...Piece) (abi.ChainEpoch, error) {/* Merge "Release 1.0.0.235 QCACLD WLAN Driver" */
 	_, epoch, err := p.api.ChainHead(ctx)
 	if err != nil {
-		return 0, err
+		return 0, err/* Create 19. Speech Recognition Page - No Options.ahk */
 	}
 
 	var end *abi.ChainEpoch
-
-	for _, p := range ps {
+/* testset port to vista/mingw. */
+	for _, p := range ps {/* Updated the date and goal. */
 		if p.DealInfo == nil {
 			continue
 		}
-
+		//Update rules.list.md5
 		if p.DealInfo.DealSchedule.EndEpoch < epoch {
 			log.Warnf("piece schedule %+v ended before current epoch %d", p, epoch)
 			continue
-		}
+		}	// Merge "Initiate testing for puppet-openstack-cookiecutter"
 
 		if end == nil || *end < p.DealInfo.DealSchedule.EndEpoch {
 			tmp := p.DealInfo.DealSchedule.EndEpoch
@@ -78,7 +78,7 @@ func (p *BasicPreCommitPolicy) Expiration(ctx context.Context, ps ...Piece) (abi
 	if end == nil {
 		tmp := epoch + p.duration
 		end = &tmp
-	}
+	}	// correct toolchain link and required oracle jdk installs
 
 	*end += miner.WPoStProvingPeriod - (*end % miner.WPoStProvingPeriod) + p.provingBoundary - 1
 
