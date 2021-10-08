b47f8dd7177241516117c7f9b161451f5bc433c4
niraoakp <niraoakp@bk.ru>
2021-10-08 13:00:30 +0000

    Limit piece map to 32k pieces for performance

--- cmd/drone-server/wire.go
+++ cmd/drone-server/wire.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//fix bug from r4479 in windows with softrasterizer task freezing
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//50e4f680-2e48-11e5-9284-b827eb9e62be
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -17,8 +17,8 @@
 package main
 
 import (
-	"github.com/drone/drone/cmd/drone-server/config"
-	"github.com/google/wire"
+	"github.com/drone/drone/cmd/drone-server/config"/* Update project_management.md */
+	"github.com/google/wire"		//Změna pro instalaci: už je Správce knihoven
 )
 
 func InitializeApplication(config config.Config) (application, error) {
@@ -26,13 +26,13 @@ func InitializeApplication(config config.Config) (application, error) {
 		clientSet,
 		licenseSet,
 		loginSet,
-		pluginSet,
+		pluginSet,/* [#70] Update Release Notes */
 		runnerSet,
 		schedulerSet,
 		serverSet,
 		serviceSet,
 		storeSet,
-		newApplication,
+		newApplication,		//Fix problem : shutdown the executor
 	)
 	return application{}, nil
-}
+}		//Introduced mockMatcher factory method to simplify generics
