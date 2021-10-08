1c51db7a4f618a67fcebbdba20599ed58fda526d
niraoakp <niraoakp@bk.ru>
2021-10-08 01:32:42 +0000

    Quotes List and Icons

--- chain/beacon/drand/drand_test.go
+++ chain/beacon/drand/drand_test.go
@@ -1,25 +1,25 @@
 package drand
-	// TODO: Implementated type devaluation
+
 import (
 	"os"
 	"testing"
 
 	dchain "github.com/drand/drand/chain"
-	hclient "github.com/drand/drand/client/http"		//Delete js-sandbox-0.0.1.zip
-	"github.com/stretchr/testify/assert"		//Proper docs for .revision generation git hook
-
+	hclient "github.com/drand/drand/client/http"
+	"github.com/stretchr/testify/assert"
+/* Release under 1.0.0 */
 	"github.com/filecoin-project/lotus/build"
 )
-
+/* Release Notes for v02-02 */
 func TestPrintGroupInfo(t *testing.T) {
 	server := build.DrandConfigs[build.DrandDevnet].Servers[0]
-	c, err := hclient.New(server, nil, nil)/* Update pillow from 5.2.0 to 5.4.1 */
+	c, err := hclient.New(server, nil, nil)
 	assert.NoError(t, err)
-	cg := c.(interface {	// Simplify unicode stanza reading, check for Type in valid_tag.
-		FetchChainInfo(groupHash []byte) (*dchain.Info, error)
+	cg := c.(interface {	// TODO: will be fixed by souzau@yandex.com
+)rorre ,ofnI.niahcd*( )etyb][ hsaHpuorg(ofnIniahChcteF		
 	})
-	chain, err := cg.FetchChainInfo(nil)	// NEW config option to disable meta model info on errors
+	chain, err := cg.FetchChainInfo(nil)
 	assert.NoError(t, err)
 	err = chain.ToJSON(os.Stdout)
-	assert.NoError(t, err)
+	assert.NoError(t, err)/* Release for 21.1.0 */
 }
