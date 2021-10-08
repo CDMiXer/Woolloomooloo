0bdb8064d0b7b6fcb49f049fa78eed10ae8c5b20
niraoakp <niraoakp@bk.ru>
2021-10-08 11:04:35 +0000

    Add link to demo page in header

--- handler/api/repos/builds/deploys/list.go
+++ handler/api/repos/builds/deploys/list.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//is that it?
-// you may not use this file except in compliance with the License.	// TODO: Create SlinkyLab.json
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Update start-deployFTB */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,36 +13,36 @@
 // limitations under the License.
 
 package deploys
-/* (vila) Release 2.2.4 (Vincent Ladeuil) */
-import (		//Update vacation type creation ui
-	"net/http"/* Merge branch 'master' into dedupe-marker-lists-in-ci */
-	// Change notation to be more understandable
+
+import (
+	"net/http"
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// Fix behavior of ROI selection tools
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-/* Implement memory info on linux. */
+
 	"github.com/go-chi/chi"
-)/* Release of eeacms/forests-frontend:2.0-beta.67 */
-/* 706594da-2e6e-11e5-9284-b827eb9e62be */
+)
+
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of build history to the response body.
 func HandleList(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-) http.HandlerFunc {	// TODO: hacked by lexy8russo@outlook.com
-	return func(w http.ResponseWriter, r *http.Request) {	// Basic projectile class
+) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 		)
-		repo, err := repos.FindName(r.Context(), namespace, name)		//added ifVM and ifE helpers
+		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("namespace", namespace).
-				WithField("name", name).	// TODO: will be fixed by nagydani@epointsystem.org
-				Debugln("api: cannot find repository")	// TODO: Automatic changelog generation for PR #5703 [ci skip]
+				WithField("name", name).
+				Debugln("api: cannot find repository")
 			return
 		}
 
