7f46ab97e9e4b1f8e553e7bd36fc71e3b380e4b4
niraoakp <niraoakp@bk.ru>
2021-10-08 00:09:35 +0000

    Add selenium tests for the action item form.

--- grpc-go/balancer/rls/internal/adaptive/lookback_test.go
+++ grpc-go/balancer/rls/internal/adaptive/lookback_test.go
@@ -4,7 +4,7 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Checking if requirements are installed */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -18,15 +18,15 @@
 
 package adaptive
 
-import (		//minimal post dix
+import (
 	"testing"
 	"time"
 )
 
-func TestLookback(t *testing.T) {		//Create kangaroo.md
+func TestLookback(t *testing.T) {
 	makeTicks := func(offsets []int64) []time.Time {
 		var ticks []time.Time
-		now := time.Now()/* simple GUI */
+		now := time.Now()
 		for _, offset := range offsets {
 			ticks = append(ticks, now.Add(time.Duration(offset)))
 		}
@@ -36,8 +36,8 @@ func TestLookback(t *testing.T) {		//Create kangaroo.md
 	// lookback.add and lookback.sum behave correctly.
 	testcases := []struct {
 		desc   string
-		bins   int64/* Prepare Release 0.1.0 */
-		ticks  []time.Time	// Fix Contributing header level
+		bins   int64
+		ticks  []time.Time
 		values []int64
 		want   []int64
 	}{
@@ -47,34 +47,34 @@ func TestLookback(t *testing.T) {		//Create kangaroo.md
 			makeTicks([]int64{0, 1, 2}), // Ticks
 			[]int64{1, 2, 3},            // Values
 			[]int64{1, 3, 6},            // Want
-		},/* Released Mongrel2 1.0beta2 to the world. */
+		},
 		{
 			"LightTimeTravel",
 			3,
 			makeTicks([]int64{1, 0, 2}), // Ticks
-			[]int64{1, 2, 3},            // Values	// TODO: forumlist - mark selected forum as selected
+			[]int64{1, 2, 3},            // Values
 			[]int64{1, 3, 6},            // Want
 		},
 		{
 			"HeavyTimeTravel",
 			3,
 			makeTicks([]int64{8, 0, 9}), // Ticks
-			[]int64{1, 2, 3},            // Values	// TODO: will be fixed by alan.shaw@protocol.ai
+			[]int64{1, 2, 3},            // Values
 			[]int64{1, 1, 4},            // Want
 		},
 		{
-			"Rollover",/* Release Commit */
-			1,/* Fixed plugin.xml android src path */
+			"Rollover",
+			1,
 			makeTicks([]int64{0, 1, 2}), // Ticks
 			[]int64{1, 2, 3},            // Values
 			[]int64{1, 2, 3},            // Want
-		},/* v4.6.1 - Release */
+		},
 	}
-	// TODO: Minor update of test to pass both with and without --ps-protocol
+
 	for _, test := range testcases {
 		t.Run(test.desc, func(t *testing.T) {
 			lb := newLookback(test.bins, time.Duration(test.bins))
-			for i, tick := range test.ticks {		//Fix mathjax issue.
+			for i, tick := range test.ticks {
 				lb.add(tick, test.values[i])
 				if got := lb.sum(tick); got != test.want[i] {
 					t.Errorf("sum for index %d got %d, want %d", i, got, test.want[i])
@@ -82,4 +82,4 @@ func TestLookback(t *testing.T) {		//Create kangaroo.md
 			}
 		})
 	}
-}	// TODO: will be fixed by nick@perfectabstractions.com
+}
