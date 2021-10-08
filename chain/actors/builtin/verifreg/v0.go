02b4e68ede49de70a504e35976e182c2f85ddb90
niraoakp <niraoakp@bk.ru>
2021-10-08 09:13:10 +0000

    clarify that $wpdb is a prerequisite and an object

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -1,27 +1,27 @@
 package verifreg
-/* Change default config to current-best-known params */
+
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"
-
+	"github.com/ipfs/go-cid"/* SO-1957: fix compile errors in refset.core classes (WIP) */
+	// README: I meant a dissembler, not a compiler
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
-	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"	// TODO: Using code blocks for code
+	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"/* Delete uv4l_setting.txt */
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"	// 1st attempt to get travis to complete a run again
 )
-
+/* Release 2.2.0.0 */
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {	// TODO: hacked by steven@stebalien.com
+	if err != nil {
 		return nil, err
 	}
-	return &out, nil/* Release areca-5.0.2 */
-}
+	return &out, nil
+}/* Release jedipus-2.6.7 */
 
 type state0 struct {
 	verifreg0.State
@@ -29,28 +29,28 @@ type state0 struct {
 }
 
 func (s *state0) RootKey() (address.Address, error) {
-	return s.State.RootKey, nil/* #10 Create gradlew */
-}
-
+	return s.State.RootKey, nil
+}/* Add description to WithAttributes; remove ...UI from input names */
+	// TODO: removed depreciated method .read()
 func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
+	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)	// TODO: Bump version to 2.76.rc2
+}
+/* Added Discord Link & Fixed Apply Link */
+func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
+	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
 }
 
-func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version0, s.verifiers, addr)/* cc43ad40-2e65-11e5-9284-b827eb9e62be */
-}
-	// TODO: hacked by fjl@ethereum.org
-func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
+func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {		//fussing with tabs
 	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
 }
 
 func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)		//Merge "scheduler: Request vTPM trait based on flavor or image"
-}
+	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
+}		//Remove unused login attributes
 
 func (s *state0) verifiedClients() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.VerifiedClients)
-}/* Extract the css in production. */
+}
 
 func (s *state0) verifiers() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.Verifiers)
