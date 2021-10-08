a52c57be2c5b5b37bf64c3afe51ef2e6c6350b2e
niraoakp <niraoakp@bk.ru>
2021-10-08 18:24:09 +0000

    d88f2ba8-2e45-11e5-9284-b827eb9e62be

--- store/shared/migrate/sqlite/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/sqlite/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-deploy-id
+-- name: alter-table-builds-add-column-deploy-id/* Merge branch 'dev' into fix_locale_handling */
 
 ALTER TABLE builds ADD COLUMN build_deploy_id NUMBER NOT NULL DEFAULT 0;
