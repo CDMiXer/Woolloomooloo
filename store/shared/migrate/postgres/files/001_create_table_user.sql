41d1234a3d826eca2b71c1d2c73a8b2edefec854
niraoakp <niraoakp@bk.ru>
2021-10-08 07:17:51 +0000

    Fix bug get process-step

--- store/shared/migrate/postgres/files/001_create_table_user.sql
+++ store/shared/migrate/postgres/files/001_create_table_user.sql
@@ -3,20 +3,20 @@
 CREATE TABLE IF NOT EXISTS users (
  user_id            SERIAL PRIMARY KEY
 ,user_login         VARCHAR(250)
-,user_email         VARCHAR(500)		//00f80bf0-2e73-11e5-9284-b827eb9e62be
+,user_email         VARCHAR(500)
 ,user_admin         BOOLEAN
 ,user_active        BOOLEAN
 ,user_machine       BOOLEAN
-,user_avatar        VARCHAR(2000)		//intercept got renamed to decorate in gengui
+,user_avatar        VARCHAR(2000)
 ,user_syncing       BOOLEAN
 ,user_synced        INTEGER
 ,user_created       INTEGER
-,user_updated       INTEGER/* Release version 0.1.18 */
+,user_updated       INTEGER
 ,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
-,user_oauth_refresh VARCHAR(500)	// Update et.json
+,user_oauth_refresh VARCHAR(500)
 ,user_oauth_expiry  INTEGER
 ,user_hash          VARCHAR(500)
-,UNIQUE(user_login)/* added pending spec example */
+,UNIQUE(user_login)
 ,UNIQUE(user_hash)
-);/* - add external jar file, files() dependency, and its test cases. */
+);
