ade9b48a743d2288d87fedab36f3d8d287bb7d43
niraoakp <niraoakp@bk.ru>
2021-10-08 03:44:24 +0000

    Rename RaspberryDIY.py to RaspberryDIY.py.save

--- grpc-go/test/tools/tools_vet.go
+++ grpc-go/test/tools/tools_vet.go
@@ -1,21 +1,21 @@
-/*	// TODO: hacked by timnugent@gmail.com
+/*
  *
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.	// TODO: [2.0.1] Added default type handling support for enums.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// 19e82ec8-2e60-11e5-9284-b827eb9e62be
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by igor@soramitsu.co.jp
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  *
- */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Call terminate_connection when shelve_offloading" */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Skip update tests that error in 1.0 but not in 3.0
+ * See the License for the specific language governing permissions and
+ * limitations under the License./* Fix hooks/BUILD for custom py_test rule */
+ *
+ */	// httpStatusPush error without maxDiskItems parameter
 
 // Package tools is used to pin specific versions of external tools in this
-// module's go.mod that gRPC uses for internal testing./* Merge "Release notes for v0.12.8.1" */
+// module's go.mod that gRPC uses for internal testing.		//Delete file.h
 package tools
