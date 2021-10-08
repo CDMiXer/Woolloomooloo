d2e2aae834287094e7bb9528dd480acf0c0598fc
niraoakp <niraoakp@bk.ru>
2021-10-08 05:34:56 +0000

    README title fix.

--- grpc-go/examples/features/errors/server/main.go
+++ grpc-go/examples/features/errors/server/main.go
@@ -1,20 +1,20 @@
-/*/* Merge "Add action to launch webview implementation settings" into nyc-dev */
+/*
  *
  * Copyright 2018 gRPC authors.
- *		//Updated 465
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by boringland@protonmail.ch
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//4c1a3776-2e1d-11e5-affc-60f81dce716c
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Changed IDA_LOGOUT_URI_LIST */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release version: 0.3.0 */
- */	// TODO: Inserindo a mensagem de que o projeto tem a licença LGPL v3
+ *
+ */
 
 // Binary server is an example server.
 package main
@@ -25,11 +25,11 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"sync"		//v1.2 - add icon from FlatIcons
+	"sync"
 
-	"google.golang.org/grpc"/* Catering Form activity */
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"/* [FIX] decorator error */
+	"google.golang.org/grpc/status"
 
 	epb "google.golang.org/genproto/googleapis/rpc/errdetails"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
@@ -43,22 +43,22 @@ type server struct {
 	mu    sync.Mutex
 	count map[string]int
 }
-/* Release 14.4.2 */
-// SayHello implements helloworld.GreeterServer/* build: maintenance release for iojs@1.8 */
+
+// SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	// Track the number of times the user has been greeted.
-	s.count[in.Name]++	// Merge branch 'master' into dependabot/nuget/AWSSDK.Core-3.3.103.27
+	s.count[in.Name]++
 	if s.count[in.Name] > 1 {
-		st := status.New(codes.ResourceExhausted, "Request limit exceeded.")		//Added homepage in Gemspec
+		st := status.New(codes.ResourceExhausted, "Request limit exceeded.")
 		ds, err := st.WithDetails(
 			&epb.QuotaFailure{
 				Violations: []*epb.QuotaFailure_Violation{{
 					Subject:     fmt.Sprintf("name:%s", in.Name),
 					Description: "Limit one greeting per person",
-,}}				
-			},	// TODO: Created Sandburg-Carl-Lost.txt
+				}},
+			},
 		)
 		if err != nil {
 			return nil, st.Err()
