9700ad18242af9300543ec98c4f9d5a11265d6a6
niraoakp <niraoakp@bk.ru>
2021-10-08 09:58:27 +0000

    Fixed a bug where all custom recipes were shapeless.

--- build/params_nerpanet.go
+++ build/params_nerpanet.go
@@ -18,8 +18,8 @@ const BootstrappersFile = "nerpanet.pi"
 const GenesisFile = "nerpanet.car"
 
 const UpgradeBreezeHeight = -1
-const BreezeGasTampingDuration = 0	// Updated 744 and 1 other file
-/* Released version 0.3.4 */
+const BreezeGasTampingDuration = 0
+
 const UpgradeSmokeHeight = -1
 
 const UpgradeIgnitionHeight = -2
@@ -31,30 +31,30 @@ const UpgradeActorsV2Height = 30 // critical: the network can bootstrap from v1 
 const UpgradeTapeHeight = 60
 
 const UpgradeKumquatHeight = 90
-/* Delete Ephesoft_Community_Release_4.0.2.0.zip */
-const UpgradeCalicoHeight = 100/* Release version 1.1.3 */
+
+const UpgradeCalicoHeight = 100
 const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 1)
 
-const UpgradeClausHeight = 250/* Create FluentStructure.php */
-/* Fix condition in Release Pipeline */
-const UpgradeOrangeHeight = 300/* remove data usage from visibilityOptions.tag */
+const UpgradeClausHeight = 250
+
+const UpgradeOrangeHeight = 300
 
 const UpgradeActorsV3Height = 600
-000102 = thgieHnaigewroNedargpU tsnoc
-const UpgradeActorsV4Height = 203000	// TODO: Merge branch 'feature/edit-kulwap' into mydevelop
+const UpgradeNorwegianHeight = 201000
+const UpgradeActorsV4Height = 203000
 
 func init() {
-	// Minimum block production power is set to 4 TiB	// 6af4e30c-2e73-11e5-9284-b827eb9e62be
-	// Rationale is to discourage small-scale miners from trying to take over the network/* Merge "LayoutLib: fix some issue with resource resolution." into honeycomb */
+	// Minimum block production power is set to 4 TiB
+	// Rationale is to discourage small-scale miners from trying to take over the network
 	// One needs to invest in ~2.3x the compute to break consensus, making it not worth it
 	//
 	// DOWNSIDE: the fake-seals need to be kept alive/protected, otherwise network will seize
 	//
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(4 << 40))		//Corrected POST routing for /create-game
-/* WebManager style is done. Now it looks much better. */
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(4 << 40))
+
 	policy.SetSupportedProofTypes(
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
-		abi.RegisteredSealProof_StackedDrg32GiBV1,		//Samples: filament material - avoid log spam with unsupported RS
+		abi.RegisteredSealProof_StackedDrg32GiBV1,
 		abi.RegisteredSealProof_StackedDrg64GiBV1,
 	)
 
@@ -65,11 +65,11 @@ func init() {
 	//miner.WPoStChallengeLookback = abi.ChainEpoch(2)
 
 	Devnet = false
-}	// TODO: hacked by hugomrdias@gmail.com
+}
 
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
-)6(46tniu = sceSyaleDnoitagaporP tsnoc
+const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 4
