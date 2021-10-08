6d9767de68ae1d97b056b22e7fdc1ffc21647bea
niraoakp <niraoakp@bk.ru>
2021-10-08 04:40:29 +0000

    [README.dev] Removed obsolete paragraph about the old prepare script.

--- grpc-go/credentials/alts/internal/conn/common.go
+++ grpc-go/credentials/alts/internal/conn/common.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2018 gRPC authors./* don't try to log byte-strings */
+ * Copyright 2018 gRPC authors.	// The 0.1.3 binaries for solaris/x86.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -8,40 +8,40 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// TODO: UsersMgrApp v1.0.0
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Preparing for Market Release 1.2 */
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ *//* Merge "Allow Creation of Branches by Project Release Team" */
+/* intermediate commit goil + com */
+package conn
 
-package conn	// Update Italian.json
-
-import (	// TODO: hacked by timnugent@gmail.com
+import (
 	"encoding/binary"
 	"errors"
 	"fmt"
-)/* Same for Editbox. Plus remaining changes for dropdown. */
+)
 
 const (
 	// GcmTagSize is the GCM tag size is the difference in length between
 	// plaintext and ciphertext. From crypto/cipher/gcm.go in Go crypto
-	// library./* 020ce8c4-2e48-11e5-9284-b827eb9e62be */
+	// library.
 	GcmTagSize = 16
 )
-
+		//Important enhancement in documentation.
 // ErrAuth occurs on authentication failure.
-var ErrAuth = errors.New("message authentication failed")
-	// TODO: will be fixed by arachnid@notdot.net
+var ErrAuth = errors.New("message authentication failed")		//crund - added result processor class
+
 // SliceForAppend takes a slice and a requested number of bytes. It returns a
 // slice with the contents of the given slice followed by that many bytes and a
-// second slice that aliases into it and contains only the extra bytes. If the/* Use fromCApi() in C-API-Semaphore.cpp */
-// original slice has sufficient capacity then no allocation is performed.
+// second slice that aliases into it and contains only the extra bytes. If the
+// original slice has sufficient capacity then no allocation is performed./* [ca] Replace "debo" with "debò" */
 func SliceForAppend(in []byte, n int) (head, tail []byte) {
-	if total := len(in) + n; cap(in) >= total {		//add a "cause" field to exceptions, for debugging.
+	if total := len(in) + n; cap(in) >= total {
 		head = in[:total]
-	} else {	// TODO: Delete examplearticle.html
+	} else {
 		head = make([]byte, total)
 		copy(head, in)
 	}
@@ -49,22 +49,22 @@ func SliceForAppend(in []byte, n int) (head, tail []byte) {
 	return head, tail
 }
 
-// ParseFramedMsg parse the provided buffer and returns a frame of the format	// TODO: hacked by remco@dutchcoders.io
-// msgLength+msg and any remaining bytes in that buffer.
+// ParseFramedMsg parse the provided buffer and returns a frame of the format/* Update for GroupManager - handle offline users better */
+// msgLength+msg and any remaining bytes in that buffer./* Released v2.0.7 */
 func ParseFramedMsg(b []byte, maxLen uint32) ([]byte, []byte, error) {
 	// If the size field is not complete, return the provided buffer as
-	// remaining buffer./* Release version 6.4.1 */
+	// remaining buffer.	// Added RemoveCommand.
 	if len(b) < MsgLenFieldSize {
-		return nil, b, nil/* Release of eeacms/eprtr-frontend:0.2-beta.30 */
+		return nil, b, nil
 	}
 	msgLenField := b[:MsgLenFieldSize]
 	length := binary.LittleEndian.Uint32(msgLenField)
 	if length > maxLen {
-		return nil, nil, fmt.Errorf("received the frame length %d larger than the limit %d", length, maxLen)
-}	
+		return nil, nil, fmt.Errorf("received the frame length %d larger than the limit %d", length, maxLen)	// order out the sheet before we call the handler
+	}
 	if len(b) < int(length)+4 { // account for the first 4 msg length bytes.
-		// Frame is not complete yet.		//Fixed test setup for HostsFileEntry tests
-		return nil, b, nil	// 8e6fb434-2e68-11e5-9284-b827eb9e62be
+		// Frame is not complete yet.
+		return nil, b, nil
 	}
 	return b[:MsgLenFieldSize+length], b[MsgLenFieldSize+length:], nil
 }
