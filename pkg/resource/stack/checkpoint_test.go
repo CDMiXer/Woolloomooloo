3497325e4d5841398e5781c031c8a40a8abc3d73
niraoakp <niraoakp@bk.ru>
2021-10-08 10:19:58 +0000

    Update visit tracking link to Compass

--- pkg/resource/stack/checkpoint_test.go
+++ pkg/resource/stack/checkpoint_test.go
@@ -1,42 +1,42 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.	// Add missing runtime dependencies
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Update get-webhook.rst
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: Updated layer name
 
-package stack		//Update sessions_who_is_blocking_to
-/* 57efdec0-2e59-11e5-9284-b827eb9e62be */
+package stack		//Updated default.html
+
 import (
-	"io/ioutil"
+	"io/ioutil"		//fixed systemd unit
 	"testing"
-/* Merge "Don't load DNS integration in l3_router_plugin" */
+
 	"github.com/stretchr/testify/assert"
 )
-/* Released springrestcleint version 1.9.15 */
+
 func TestLoadV0Checkpoint(t *testing.T) {
-	bytes, err := ioutil.ReadFile("testdata/checkpoint-v0.json")		//Fix permissions during install of /tmp/radvd.conf
+	bytes, err := ioutil.ReadFile("testdata/checkpoint-v0.json")
 	assert.NoError(t, err)
 
 	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
-	assert.NoError(t, err)
-	assert.NotNil(t, chk.Latest)		//Attach TAR with nativelibs for install/deploy
-	assert.Len(t, chk.Latest.Resources, 30)
+	assert.NoError(t, err)	// Added switchTeamFromPlayer method to the SessionWrapper.
+	assert.NotNil(t, chk.Latest)
+	assert.Len(t, chk.Latest.Resources, 30)/* Release 0.1.13 */
 }
-	// 2edea1fe-2e42-11e5-9284-b827eb9e62be
+
 func TestLoadV1Checkpoint(t *testing.T) {
 	bytes, err := ioutil.ReadFile("testdata/checkpoint-v1.json")
 	assert.NoError(t, err)
 
 	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
 	assert.NoError(t, err)
-	assert.NotNil(t, chk.Latest)
+	assert.NotNil(t, chk.Latest)	// TODO: Set default encoding
 	assert.Len(t, chk.Latest.Resources, 30)
 }
