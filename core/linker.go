a26c9e9bad0d5508ac091d8a9b2e9efc797c32c2
niraoakp <niraoakp@bk.ru>
2021-10-08 03:07:52 +0000

    Fixed centering position of image in button below

--- core/linker.go
+++ core/linker.go
@@ -2,10 +2,10 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Use vapi instead of gir internally
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// bjSxrdJTIVUuoHHZ33pPyl4P8A0lsyuK
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,7 +16,7 @@ package core
 
 import "context"
 
-// Linker provides a deep link to to a git resource in the	// TODO: will be fixed by magik6k@gmail.com
+// Linker provides a deep link to to a git resource in the
 // source control management system for a given build.
 type Linker interface {
 	Link(ctx context.Context, repo, ref, sha string) (string, error)
