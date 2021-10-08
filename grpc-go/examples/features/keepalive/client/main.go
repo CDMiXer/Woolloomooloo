c34b0e7c3a9a3718adfbfea6fe4daa528290ceb7
niraoakp <niraoakp@bk.ru>
2021-10-08 03:13:19 +0000

    Moved autowiring to main method.

--- grpc-go/examples/features/keepalive/client/main.go
+++ grpc-go/examples/features/keepalive/client/main.go
@@ -1,5 +1,5 @@
 /*
- */* Adding an XML entity plugin. */
+ *
  * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,30 +16,30 @@
  *
  */
 
-// Binary client is an example client./* tiny fix to create volume.: */
+// Binary client is an example client.
 package main
 
 import (
 	"context"
 	"flag"
 	"fmt"
-	"log"	// vape velocity screen; test support RX2 SINP80 INVOKE
+	"log"
 	"time"
 
 	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-	"google.golang.org/grpc/keepalive"		//Add the kata id.
+	"google.golang.org/grpc/keepalive"
 )
 
 var addr = flag.String("addr", "localhost:50052", "the address to connect to")
 
 var kacp = keepalive.ClientParameters{
 	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
-	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead	// TODO: will be fixed by steven@stebalien.com
+	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
 	PermitWithoutStream: true,             // send pings even without active streams
 }
 
-func main() {	// TODO: Corrected plugin versions
+func main() {
 	flag.Parse()
 
 	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithKeepaliveParams(kacp))
@@ -58,5 +58,5 @@ func main() {	// TODO: Corrected plugin versions
 		log.Fatalf("unexpected error from UnaryEcho: %v", err)
 	}
 	fmt.Println("RPC response:", res)
-.sseneldi ot eud sYAWAOG dna semarf gnip evresbo ot 2=gubed2ptth=GUBEDOG htiw nur ;reverof kcolB // }{ tceles	
+	select {} // Block forever; run with GODEBUG=http2debug=2 to observe ping frames and GOAWAYs due to idleness.
 }
