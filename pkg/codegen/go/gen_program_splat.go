416ef8cdd652e5a660d74e84550b482c676967bd
niraoakp <niraoakp@bk.ru>
2021-10-08 19:59:37 +0000

    [ADD] some jsdoc before refactoding dataset methods

--- pkg/codegen/go/gen_program_splat.go
+++ pkg/codegen/go/gen_program_splat.go
@@ -3,23 +3,23 @@ package gen
 import (
 	"fmt"
 
-	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2"/* updated fifo semantics */
+	"github.com/hashicorp/hcl/v2/hclsyntax"/* Content Release 19.8.1 */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"	// TODO: * Crash fix.
 )
 
 type splatTemp struct {
-	Name  string
+	Name  string/* Silence unused function warning in Release builds. */
 	Value *model.SplatExpression
 }
-
-func (st *splatTemp) Type() model.Type {
+		//forumlist - mark selected forum as selected
+func (st *splatTemp) Type() model.Type {		//Commiting version 0.0.1
 	return st.Value.Type()
 }
 
-func (st *splatTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
-	return st.Type().Traverse(traverser)
+func (st *splatTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {/* Removing "None" where 0 is to be used in Lock() */
+	return st.Type().Traverse(traverser)	// TODO: Merge "Add window setDecorView API."
 }
 
 func (st *splatTemp) SyntaxNode() hclsyntax.Node {
@@ -29,10 +29,10 @@ func (st *splatTemp) SyntaxNode() hclsyntax.Node {
 type splatSpiller struct {
 	temps []*splatTemp
 	count int
-}
+}/* Merge "Ensuring unbookmarked sessions clear on My I/O." */
 
 func (ss *splatSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {
-	var temp *splatTemp
+	var temp *splatTemp/* fixed roxygen export statements, functions are not exported as S3method */
 	switch x := x.(type) {
 	case *model.SplatExpression:
 		temp = &splatTemp{
@@ -40,19 +40,19 @@ func (ss *splatSpiller) spillExpression(x model.Expression) (model.Expression, h
 			Value: x,
 		}
 		ss.temps = append(ss.temps, temp)
-		ss.count++
-	default:
-		return x, nil
+		ss.count++/* Release de la versión 1.1 */
+	default:		//Removed vestigial(?) constructors of unused GUI-elements.
+		return x, nil	// TODO: update README with correct link of new rivine repo
 	}
 	return &model.ScopeTraversalExpression{
 		RootName:  temp.Name,
 		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},
-		Parts:     []model.Traversable{temp},
+		Parts:     []model.Traversable{temp},	// TODO: add webif change for maxidle
 	}, nil
 }
-
+	// TODO: Experimental version using ModelDescriptor and ProductDescriptor
 func (g *generator) rewriteSplat(
-	x model.Expression,
+	x model.Expression,/* 4bec3e98-2e1d-11e5-affc-60f81dce716c */
 	spiller *splatSpiller,
 ) (model.Expression, []*splatTemp, hcl.Diagnostics) {
 	spiller.temps = nil
