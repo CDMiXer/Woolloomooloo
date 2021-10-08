483a9dfe11907f25e56d59ad350fffb8696b9f25
niraoakp <niraoakp@bk.ru>
2021-10-08 16:41:43 +0000

    create a zip of the local repository and extract it on windows.

--- operator/manager/rpc/error.go
+++ operator/manager/rpc/error.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// Land hover state
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -6,11 +6,11 @@
 
 package rpc
 
-type serverError struct {
-	Status  int
+type serverError struct {/* ba028c08-2e6c-11e5-9284-b827eb9e62be */
+	Status  int/* *Follow up r1022 */
 	Message string
 }
 
 func (s *serverError) Error() string {
-	return s.Message
-}
+	return s.Message/* Update sparkstreaming.py */
+}	// Incorrect uploads.
