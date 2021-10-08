d05f37ef140e5684833748d538fe7404e5870e5d
niraoakp <niraoakp@bk.ru>
2021-10-08 07:47:46 +0000

    Fixed the string building for polynomial functions

--- handler/api/repos/builds/branches/list_test.go
+++ handler/api/repos/builds/branches/list_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Add part of non-variaition-controlled functionality to PlayerRemoveCtrl
 // that can be found in the LICENSE file.
-		//Parallel bug fix: David Schmidt, UMass Amherst
+
 package branches
