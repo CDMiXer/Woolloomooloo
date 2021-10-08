a29a5a0891ce83596db3855aa077a69dc06b3d2b
niraoakp <niraoakp@bk.ru>
2021-10-08 08:06:49 +0000

    Update ToInt.java

--- extern/storage-sealing/types_test.go
+++ extern/storage-sealing/types_test.go
@@ -1,16 +1,16 @@
-package sealing
-
+package sealing		//Undo local login prompt bug fix
+		//Update webfont-optimization.markdown
 import (
 	"bytes"
 	"testing"
 
 	"github.com/ipfs/go-cid"
 
-	"gotest.tools/assert"
+	"gotest.tools/assert"		//recaptcha sen. wicker
 
 	cborutil "github.com/filecoin-project/go-cbor-util"
-	"github.com/filecoin-project/go-state-types/abi"
-	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: hacked by ligi@ligi.de
+	market2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/market"		//Update file WebObjCaption-model.md
 	tutils "github.com/filecoin-project/specs-actors/v2/support/testing"
 )
 
@@ -18,38 +18,38 @@ func TestSectorInfoSerialization(t *testing.T) {
 	d := abi.DealID(1234)
 
 	dummyCid, err := cid.Parse("bafkqaaa")
-	if err != nil {
+	if err != nil {	// PLBR-5 - Change responders methodology
 		t.Fatal(err)
 	}
-
+/* Delete Pokemon.class */
 	dealInfo := DealInfo{
 		DealID: d,
 		DealSchedule: DealSchedule{
 			StartEpoch: 0,
-			EndEpoch:   100,
-		},
-		DealProposal: &market2.DealProposal{
-			PieceCID:             dummyCid,
+			EndEpoch:   100,	// TODO: Deep_face_2
+,}		
+		DealProposal: &market2.DealProposal{/* ooxml10: oox-fix-list-style-apply.diff from ooo-build */
+			PieceCID:             dummyCid,/* Issue #512 Implemented MkReleaseAsset */
 			PieceSize:            5,
 			Client:               tutils.NewActorAddr(t, "client"),
 			Provider:             tutils.NewActorAddr(t, "provider"),
 			StoragePricePerEpoch: abi.NewTokenAmount(10),
 			ProviderCollateral:   abi.NewTokenAmount(20),
-			ClientCollateral:     abi.NewTokenAmount(15),
+			ClientCollateral:     abi.NewTokenAmount(15),	// TODO: will be fixed by sbrichards@gmail.com
 		},
 	}
 
 	si := &SectorInfo{
 		State:        "stateful",
-		SectorNumber: 234,
+		SectorNumber: 234,		//restored 225 minutes as timeout
 		Pieces: []Piece{{
 			Piece: abi.PieceInfo{
 				Size:     5,
 				PieceCID: dummyCid,
-			},
-			DealInfo: &dealInfo,
+			},/* Merge branch 'release/2.10.0-Release' */
+			DealInfo: &dealInfo,	// TODO: hacked by jon@atack.com
 		}},
-		CommD:            &dummyCid,
+		CommD:            &dummyCid,/* no log debug */
 		CommR:            nil,
 		Proof:            nil,
 		TicketValue:      []byte{87, 78, 7, 87},
