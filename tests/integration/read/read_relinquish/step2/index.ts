eb72b89ef25235f0f7cf116cfbecc01d623875ed
niraoakp <niraoakp@bk.ru>
2021-10-08 08:51:19 +0000

    [InAppSettingsKit] Bump version to match component

--- tests/integration/read/read_relinquish/step2/index.ts
+++ tests/integration/read/read_relinquish/step2/index.ts
@@ -1,22 +1,22 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Release v0.3.5 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Merge "Release 3.0.10.054 Prima WLAN Driver" */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";/* Merge branch 'development-1.6.0' into issue87-add-tests */
-	// TODO: hacked by cory@protocol.ai
+import { Resource } from "./resource";
+
 // I happen to know that "a" has ID "0" (since this is how the dynamic provider in this test works).
-//	// TODO: Fixes MVERSIONS-39
-// Here I "relinquish" control of "a" by doing a resource read, but with an ID that is		//updated core connector tests
+//
+// Here I "relinquish" control of "a" by doing a resource read, but with an ID that is
 // exactly equal to the resource that I already own. The resource will recognize this
 // and not delete "a".
 //
