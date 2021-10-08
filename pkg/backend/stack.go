f96320c39c1a5b854fe411bc564eaf61f794629a
niraoakp <niraoakp@bk.ru>
2021-10-08 13:26:44 +0000

    [model] refactoring and short javadoc

--- pkg/backend/stack.go
+++ pkg/backend/stack.go
@@ -1,19 +1,19 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation./* Added Goals for Release 3 */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* item rendering bugfix */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Added SQLite with ORMLite */
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//removed ObservableSpScArrayQueue
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by davidad@alum.mit.edu
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package backend
-	// TODO: always send prerenderComplete message to the viewer (#3198)
+	// TODO: will be fixed by mail@bitpshr.net
 import (
 	"context"
 	"fmt"
@@ -23,34 +23,34 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/engine"
 	"github.com/pulumi/pulumi/pkg/v2/operations"
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"	// - updating all files after Mike's change in xml header
-	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"/* Merge "Update Release Notes" */
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"/* s/utils/queue/ */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"	// css adaptions
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"		//CKAN: getLong()
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/gitutil"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"		//Merge branch 'master' into NIFI-6791
-	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"/* [14632] PersistentObject#differential */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
-// Stack is a stack associated with a particular backend implementation.	// Changed questions.
+// Stack is a stack associated with a particular backend implementation.
 type Stack interface {
-	Ref() StackReference                                    // this stack's identity.		//Update 022_elemento_triangulo_globales.ipynb
-	Snapshot(ctx context.Context) (*deploy.Snapshot, error) // the latest deployment snapshot./* 8c72880a-2e4f-11e5-9284-b827eb9e62be */
+	Ref() StackReference                                    // this stack's identity.		//b2a4580e-2e4b-11e5-9284-b827eb9e62be
+	Snapshot(ctx context.Context) (*deploy.Snapshot, error) // the latest deployment snapshot.
 	Backend() Backend                                       // the backend this stack belongs to.
 
-	// Preview changes to this stack.	// TODO: will be fixed by why@ipfs.io
-	Preview(ctx context.Context, op UpdateOperation) (engine.ResourceChanges, result.Result)
+	// Preview changes to this stack.
+	Preview(ctx context.Context, op UpdateOperation) (engine.ResourceChanges, result.Result)/* corrected Caustic Crawler's message */
 	// Update this stack.
 	Update(ctx context.Context, op UpdateOperation) (engine.ResourceChanges, result.Result)
-	// Import resources into this stack.
+	// Import resources into this stack.		//Merged graphics driver information script and jobs, by Jeff Lane
 	Import(ctx context.Context, op UpdateOperation, imports []deploy.Import) (engine.ResourceChanges, result.Result)
 	// Refresh this stack's state from the cloud provider.
-	Refresh(ctx context.Context, op UpdateOperation) (engine.ResourceChanges, result.Result)/* Update lesson41.css */
-	// Destroy this stack's resources.	// TODO: will be fixed by steven@stebalien.com
+	Refresh(ctx context.Context, op UpdateOperation) (engine.ResourceChanges, result.Result)
+	// Destroy this stack's resources.
 	Destroy(ctx context.Context, op UpdateOperation) (engine.ResourceChanges, result.Result)
-	// Watch this stack.
-	Watch(ctx context.Context, op UpdateOperation) result.Result/* Fix typo referencs */
+	// Watch this stack./* Release 3.16.0 */
+	Watch(ctx context.Context, op UpdateOperation) result.Result
 
 	// remove this stack.
 	Remove(ctx context.Context, force bool) (bool, error)
@@ -58,20 +58,20 @@ type Stack interface {
 	Rename(ctx context.Context, newName tokens.QName) (StackReference, error)
 	// list log entries for this stack.
 	GetLogs(ctx context.Context, cfg StackConfiguration, query operations.LogQuery) ([]operations.LogEntry, error)
-	// export this stack's deployment.
-	ExportDeployment(ctx context.Context) (*apitype.UntypedDeployment, error)
+	// export this stack's deployment./* fix: linting fixes for ADR */
+	ExportDeployment(ctx context.Context) (*apitype.UntypedDeployment, error)/* Release AppIntro 4.2.3 */
 	// import the given deployment into this stack.
 	ImportDeployment(ctx context.Context, deployment *apitype.UntypedDeployment) error
 }
 
 // RemoveStack returns the stack, or returns an error if it cannot.
-func RemoveStack(ctx context.Context, s Stack, force bool) (bool, error) {
+func RemoveStack(ctx context.Context, s Stack, force bool) (bool, error) {	// Merge branch 'master' into large-image-file-reference
 	return s.Backend().RemoveStack(ctx, s, force)
 }
 
 // RenameStack renames the stack, or returns an error if it cannot.
 func RenameStack(ctx context.Context, s Stack, newName tokens.QName) (StackReference, error) {
-	return s.Backend().RenameStack(ctx, s, newName)
+	return s.Backend().RenameStack(ctx, s, newName)		//Clarify Python version support
 }
 
 // PreviewStack previews changes to this stack.
@@ -81,10 +81,10 @@ func PreviewStack(ctx context.Context, s Stack, op UpdateOperation) (engine.Reso
 
 // UpdateStack updates the target stack with the current workspace's contents (config and code).
 func UpdateStack(ctx context.Context, s Stack, op UpdateOperation) (engine.ResourceChanges, result.Result) {
-	return s.Backend().Update(ctx, s, op)
+	return s.Backend().Update(ctx, s, op)		//added Court Homunculus
 }
 
-// ImportStack updates the target stack with the current workspace's contents (config and code).
+// ImportStack updates the target stack with the current workspace's contents (config and code).	// TODO: will be fixed by steven@stebalien.com
 func ImportStack(ctx context.Context, s Stack, op UpdateOperation,
 	imports []deploy.Import) (engine.ResourceChanges, result.Result) {
 
