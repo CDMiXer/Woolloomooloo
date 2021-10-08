dfc29ca4899e1120280f135b2c30b383ad9357d7
niraoakp <niraoakp@bk.ru>
2021-10-08 20:01:09 +0000

    added partial translations for Turkish, Croatian, Finnish, Asturian and French

--- handler/api/ccmenu/cc_test.go
+++ handler/api/ccmenu/cc_test.go
@@ -5,18 +5,18 @@
 // +build !oss
 
 package ccmenu
-		//Create Euler010.cpp
+
 import (
 	"encoding/xml"
 	"testing"
 
 	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"	// TODO: hacked by boringland@protonmail.ch
-)/* added simple unit test for barier */
-	// TODO: Update plugins/box/plugins/languages/it.lang.php
+	"github.com/google/go-cmp/cmp/cmpopts"
+)
+
 var ignore = cmpopts.IgnoreFields(CCProjects{}, "Project.LastBuildTime")
-/* Correct relative font size. */
+
 func TestNew(t *testing.T) {
 	repo := &core.Repository{
 		Namespace: "octocat",
@@ -26,7 +26,7 @@ func TestNew(t *testing.T) {
 	build := &core.Build{
 		Number:  1,
 		Status:  core.StatusRunning,
-		Started: 1524251054,/* Release for v38.0.0. */
+		Started: 1524251054,
 	}
 	link := "https://drone.company.com"
 
@@ -46,26 +46,26 @@ func TestNew(t *testing.T) {
 	got := New(repo, build, link)
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
-	}		//RedisSessionHandler: use setex instead of set + expire
-}/* rocnet: changed tracelevel for clock sync */
+	}
+}
 
-func TestNew_Success(t *testing.T) {	// readme points to wiki documentation
-	repo := &core.Repository{		//Rough implementation of the new trigger stuff (to be improved a lot!).
+func TestNew_Success(t *testing.T) {
+	repo := &core.Repository{
 		Namespace: "octocat",
 		Name:      "hello-world",
 		Slug:      "octocat/hello-world",
 	}
 	build := &core.Build{
 		Number:  1,
-		Status:  core.StatusPassing,/* Mixin 0.4 Release */
+		Status:  core.StatusPassing,
 		Started: 1524251054,
-	}/* Release 2.0 enhancements. */
-	link := "https://drone.company.com"/* Delete GitReleases.h */
-	// Contributions examples from Github Help
-	want := &CCProjects{/* Merge "Release 3.2.3.455 Prima WLAN Driver" */
+	}
+	link := "https://drone.company.com"
+
+	want := &CCProjects{
 		XMLName: xml.Name{},
 		Project: &CCProject{
-			XMLName:         xml.Name{},		//Параметризованы вставка и сдвиг запятой в удалении нерегулярных событий
+			XMLName:         xml.Name{},
 			Name:            "octocat/hello-world",
 			Activity:        "Sleeping",
 			LastBuildStatus: "Success",
