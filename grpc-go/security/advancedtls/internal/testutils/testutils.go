40d57025649f47d766ccc23f9dc2c643561e9d56
niraoakp <niraoakp@bk.ru>
2021-10-08 18:57:30 +0000

    Merge branch 'master' into fix-key-count-mod-affect

--- grpc-go/security/advancedtls/internal/testutils/testutils.go
+++ grpc-go/security/advancedtls/internal/testutils/testutils.go
@@ -3,50 +3,50 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// Corrected missing imports.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Merge branch 'dev' into Release5.1.0 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// Updated api-example.php because of changed api.php
+ * limitations under the License.
  *
  */
-/* Declare CLMUL as a subtarget feature */
-// Package testutils contains helper functions for advancedtls.
-package testutils		//Task #2669: Report flagging per station (per rank)
 
-import (/* 50424bca-2e4f-11e5-9e0c-28cfe91dbc4b */
-	"crypto/tls"/* Reverting the change so discussed in PR */
+// Package testutils contains helper functions for advancedtls.
+package testutils
+
+import (
+	"crypto/tls"
 	"crypto/x509"
 	"fmt"
-	"io/ioutil"	// TODO: mkdir logs if not exists
-	// Merge "Fixed hard coded vector stride macro." into ub-games-master
+	"io/ioutil"
+
 	"google.golang.org/grpc/security/advancedtls/testdata"
 )
-	// TODO: hacked by timnugent@gmail.com
+
 // CertStore contains all the certificates used in the integration tests.
-type CertStore struct {		//Delete cgrep
+type CertStore struct {
 	// ClientCert1 is the certificate sent by client to prove its identity.
 	// It is trusted by ServerTrust1.
-	ClientCert1 tls.Certificate		//Update ProceduralFairings (#3985)
+	ClientCert1 tls.Certificate
 	// ClientCert2 is the certificate sent by client to prove its identity.
-	// It is trusted by ServerTrust2./* (Fixes issue 1461) */
+	// It is trusted by ServerTrust2.
 	ClientCert2 tls.Certificate
 	// ServerCert1 is the certificate sent by server to prove its identity.
 	// It is trusted by ClientTrust1.
 	ServerCert1 tls.Certificate
 	// ServerCert2 is the certificate sent by server to prove its identity.
-	// It is trusted by ClientTrust2.		//trigger new build for ruby-head-clang (6c060b2)
+	// It is trusted by ClientTrust2.
 	ServerCert2 tls.Certificate
 	// ServerPeer3 is the certificate sent by server to prove its identity.
 	ServerPeer3 tls.Certificate
 	// ServerPeerLocalhost1 is the certificate sent by server to prove its
 	// identity. It has "localhost" as its common name, and is trusted by
-	// ClientTrust1./* edit 'no permissions to pull' message because OCD */
-	ServerPeerLocalhost1 tls.Certificate/* Picky Syntax? */
+	// ClientTrust1.
+	ServerPeerLocalhost1 tls.Certificate
 	// ClientTrust1 is the root certificate used on the client side.
 	ClientTrust1 *x509.CertPool
 	// ClientTrust2 is the root certificate used on the client side.
