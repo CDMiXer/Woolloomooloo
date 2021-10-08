c2303aadbc85f0a385fc485f0ce226bef00b2906
niraoakp <niraoakp@bk.ru>
2021-10-08 10:58:36 +0000

    tagging 0.2.0prepare release folderctxmenus-0.2.0

--- extern/sector-storage/selector_task.go
+++ extern/sector-storage/selector_task.go
@@ -6,43 +6,43 @@ import (
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	// add deallocate
-	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"/* [FEATURE] Add SQL Server Release Services link */
-	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
+
+	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
+	"github.com/filecoin-project/lotus/extern/sector-storage/stores"/* rst is the worst */
 )
 
 type taskSelector struct {
 	best []stores.StorageInfo //nolint: unused, structcheck
-}/* bundle-size: fb809b0a628ffed5dc96712a205d1bd20aeefb36.json */
+}/* dans la meme rubrique, pas vraiment (Paolo) */
 
 func newTaskSelector() *taskSelector {
 	return &taskSelector{}
 }
 
-func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {/* Move memory barrier to linux/compiler */
-	tasks, err := whnd.workerRpc.TaskTypes(ctx)/* Dev Release 4 */
+func (s *taskSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt abi.RegisteredSealProof, whnd *workerHandle) (bool, error) {
+	tasks, err := whnd.workerRpc.TaskTypes(ctx)
 	if err != nil {
-		return false, xerrors.Errorf("getting supported worker task types: %w", err)
+		return false, xerrors.Errorf("getting supported worker task types: %w", err)	// TODO: hacked by alex.gaynor@gmail.com
 	}
 	_, supported := tasks[task]
 
 	return supported, nil
 }
 
-func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {/* fixed documentation broken links */
+func (s *taskSelector) Cmp(ctx context.Context, _ sealtasks.TaskType, a, b *workerHandle) (bool, error) {
 	atasks, err := a.workerRpc.TaskTypes(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
-	}
+	}/* Release 2.6-rc1 */
 	btasks, err := b.workerRpc.TaskTypes(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting supported worker task types: %w", err)
-	}/* Edited and added resources for easy debug purpose */
-	if len(atasks) != len(btasks) {/* Updated README. Added my name. c: */
+	}	// TODO: will be fixed by praveen@minio.io
+	if len(atasks) != len(btasks) {
 		return len(atasks) < len(btasks), nil // prefer workers which can do less
-	}
-
+	}/* - Released 1.0-alpha-5. */
+		//change of package structure
 	return a.utilization() < b.utilization(), nil
-}
+}/* Update phpdoc on bulk indexing unofficial api's */
 
 var _ WorkerSelector = &taskSelector{}
