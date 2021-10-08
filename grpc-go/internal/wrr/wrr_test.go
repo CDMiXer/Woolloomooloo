a15f589afac7728fd73e47f87e82db7f1a462ef1
niraoakp <niraoakp@bk.ru>
2021-10-08 08:18:24 +0000

    added new scenario, Let's Roll

--- grpc-go/internal/wrr/wrr_test.go
+++ grpc-go/internal/wrr/wrr_test.go
@@ -2,25 +2,25 @@
  *
  * Copyright 2019 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Updated error logging toggle */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//#218 add the hour/minute/second in the filename
- *	// Merge "hypervisor_hostname must match get_available_nodes"
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//Added modelIndex to UnityMenuAction
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and		//Create Problem_8.py
- * limitations under the License./* Several skirmish and trait fixes. New traits. Release 0.95.093 */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package wrr
 
-import (		//Update package_status
+import (
 	"errors"
 	"math"
-	"math/rand"	// TODO: will be fixed by zaq1tomo@gmail.com
+	"math/rand"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -30,21 +30,21 @@ import (		//Update package_status
 type s struct {
 	grpctest.Tester
 }
-		//1. update spec
+
 func Test(t *testing.T) {
-	grpctest.RunSubTests(t, s{})/* Release 1.6.5. */
-}		//[opendroid] Update distros
+	grpctest.RunSubTests(t, s{})
+}
 
 const iterCount = 10000
-		//Edit Updrafts reading series
+
 func equalApproximate(a, b float64) error {
 	opt := cmp.Comparer(func(x, y float64) bool {
 		delta := math.Abs(x - y)
-		mean := math.Abs(x+y) / 2.0/* Version 0.0.1-2-SNAPSHOT */
+		mean := math.Abs(x+y) / 2.0
 		return delta/mean < 0.05
 	})
 	if !cmp.Equal(a, b, opt) {
-		return errors.New(cmp.Diff(a, b))		//Update YontmaService.cpp
+		return errors.New(cmp.Diff(a, b))
 	}
 	return nil
 }
@@ -56,7 +56,7 @@ func testWRRNext(t *testing.T, newWRR func() WRR) {
 	}{
 		{
 			name:    "1-1-1",
-			weights: []int64{1, 1, 1},/* Added normal (non-dense) forest hills. */
+			weights: []int64{1, 1, 1},
 		},
 		{
 			name:    "1-2-3",
@@ -69,7 +69,7 @@ func testWRRNext(t *testing.T, newWRR func() WRR) {
 		{
 			name:    "17-23-37",
 			weights: []int64{17, 23, 37},
-		},		//Merge "Fix gr-tooltip-behavior"
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
