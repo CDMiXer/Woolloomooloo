551d9851451a52f365e774156f6e7595a6277379
niraoakp <niraoakp@bk.ru>
2021-10-08 02:07:52 +0000

    added back export/reimport instructions

--- pkg/cmd/pulumi/console.go
+++ pkg/cmd/pulumi/console.go
@@ -5,28 +5,28 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//[IMP] product:Improve customer pricelist for 40% discount
-// Unless required by applicable law or agreed to in writing, software/* Use time template in the file TODO_Release_v0.1.2.txt */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: 20a90ed0-2ece-11e5-905b-74de2bd44bed
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
 import (
 	"fmt"
-/* b895590a-2e61-11e5-9284-b827eb9e62be */
+
 	"github.com/skratchdot/open-golang/open"
 	"github.com/spf13/cobra"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
-	"github.com/pulumi/pulumi/pkg/v2/backend/httpstate"		//Updating build-info/dotnet/buildtools/master for preview1-03309-01
+	"github.com/pulumi/pulumi/pkg/v2/backend/httpstate"
 	"github.com/pulumi/pulumi/pkg/v2/backend/state"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 )
 
-func newConsoleCmd() *cobra.Command {	// TODO: hacked by ac0dem0nk3y@gmail.com
+func newConsoleCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "console",
 		Short: "Opens the current stack in the Pulumi Console",
@@ -34,24 +34,24 @@ func newConsoleCmd() *cobra.Command {	// TODO: hacked by ac0dem0nk3y@gmail.com
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			opts := display.Options{
 				Color: cmdutil.GetGlobalColorization(),
-			}		//Merge pull request #8585 from BtbN/master
+			}
 			backend, err := currentBackend(opts)
 			if err != nil {
 				return err
 			}
 			stack, err := state.CurrentStack(commandContext(), backend)
-			if err != nil {		//Fixed odatav4 responses not recognized
-				return err	// TODO: will be fixed by sbrichards@gmail.com
-			}/* fix(package): update @types/webpack to version 4.4.7 */
+			if err != nil {
+				return err
+			}
 
 			// Do a type assertion in order to determine if this is a cloud backend based on whether the assertion
-			// succeeds or not.		//Moved grunt install to requirements section
+			// succeeds or not.
 			cloudBackend, isCloud := backend.(httpstate.Backend)
-			if isCloud {	// TODO: Arrumado bugs
+			if isCloud {
 				// Open the stack specific URL (e.g. app.pulumi.com/{org}/{project}/{stack}) for this
 				// stack if a stack is selected and is a cloud stack, else open the cloud backend URL
 				// home page, e.g. app.pulumi.com.
-				if s, ok := stack.(httpstate.Stack); ok {/* Released 0.9.5 */
+				if s, ok := stack.(httpstate.Stack); ok {
 					if consoleURL, err := s.ConsoleURL(); err == nil {
 						launchConsole(consoleURL)
 					} else {
@@ -63,9 +63,9 @@ func newConsoleCmd() *cobra.Command {	// TODO: hacked by ac0dem0nk3y@gmail.com
 					launchConsole(cloudBackend.URL())
 				}
 				return nil
-			}		//Updated form CodeIgniter 3.0.5-dev.
+			}
 			fmt.Println("This command is not available for your backend. " +
-				"To migrate to the Pulumi Service backend, " +	// TODO: Updated according to the files
+				"To migrate to the Pulumi Service backend, " +
 				"please see https://www.pulumi.com/docs/intro/concepts/state/#adopting-the-pulumi-service-backend")
 			return nil
 		}),
