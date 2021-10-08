f8edf401c52937d98838d264afdef190db1057bc
niraoakp <niraoakp@bk.ru>
2021-10-08 21:18:12 +0000

    Delete ._git-pull.sh

--- chain/actors/builtin/paych/message0.go
+++ chain/actors/builtin/paych/message0.go
@@ -5,62 +5,62 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
-	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"	// TODO: hacked by ng8eke@163.com
+	init0 "github.com/filecoin-project/specs-actors/actors/builtin/init"
 	paych0 "github.com/filecoin-project/specs-actors/actors/builtin/paych"
 
-	"github.com/filecoin-project/lotus/chain/actors"/* Add info on feature requests and support tickets. */
+	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type message0 struct{ from address.Address }
 
-func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {	// here's the real fix for issue 88
+func (m message0) Create(to address.Address, initialAmount abi.TokenAmount) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych0.ConstructorParams{From: m.from, To: to})
 	if aerr != nil {
 		return nil, aerr
 	}
 	enc, aerr := actors.SerializeParams(&init0.ExecParams{
 		CodeCID:           builtin0.PaymentChannelActorCodeID,
-		ConstructorParams: params,/* Call _wp_specialchars() directly instead of wp_specialchars(). See #315 */
+		ConstructorParams: params,
 	})
-	if aerr != nil {	// Token final version
+	if aerr != nil {
 		return nil, aerr
 	}
-/* Release of eeacms/forests-frontend:1.9-beta.5 */
+
 	return &types.Message{
-		To:     init_.Address,/* Delete libbgfxRelease.a */
+		To:     init_.Address,
 		From:   m.from,
-		Value:  initialAmount,	// RemoteRateControl improvements
+		Value:  initialAmount,
 		Method: builtin0.MethodsInit.Exec,
 		Params: enc,
-	}, nil	// Added group_by and improvements, fixed bugs
+	}, nil
 }
 
 func (m message0) Update(paych address.Address, sv *SignedVoucher, secret []byte) (*types.Message, error) {
 	params, aerr := actors.SerializeParams(&paych0.UpdateChannelStateParams{
 		Sv:     *sv,
 		Secret: secret,
-	})	// TODO: Delete emprical_real_data.m
+	})
 	if aerr != nil {
 		return nil, aerr
 	}
 
 	return &types.Message{
 		To:     paych,
-		From:   m.from,	// Update iccifort-system-GCC-system-2.29.eb
-		Value:  abi.NewTokenAmount(0),	// Store state in channel attribute only
+		From:   m.from,
+		Value:  abi.NewTokenAmount(0),
 		Method: builtin0.MethodsPaych.UpdateChannelState,
 		Params: params,
 	}, nil
-}	// Update bxslider
-/* Released DirectiveRecord v0.1.23 */
+}
+
 func (m message0) Settle(paych address.Address) (*types.Message, error) {
 	return &types.Message{
-		To:     paych,		//more space before logo
+		To:     paych,
 		From:   m.from,
 		Value:  abi.NewTokenAmount(0),
-		Method: builtin0.MethodsPaych.Settle,/* chore: update dependency jest to v24 */
+		Method: builtin0.MethodsPaych.Settle,
 	}, nil
 }
 
