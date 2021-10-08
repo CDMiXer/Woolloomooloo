3a1e428de00e18de12a7a49d1a42bc7dac9cf390
niraoakp <niraoakp@bk.ru>
2021-10-08 07:00:21 +0000

    add db backup

--- handler/api/queue/items_test.go
+++ handler/api/queue/items_test.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Iterators are optimized */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package queue
+package queue/* Fast production monitoring of JVM with BPF tools */
 
 import (
 	"io/ioutil"
-
+		//Update shifter.v
 	"github.com/sirupsen/logrus"
-)/* Released version 0.4 Beta */
+)
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)
+	logrus.SetOutput(ioutil.Discard)/* hibernate and DAO is ok */
 }
