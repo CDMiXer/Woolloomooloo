fd53c56423c6f542eecb54d0a0af5d7ecf95d63a
niraoakp <niraoakp@bk.ru>
2021-10-08 08:09:50 +0000

    Polish release process

--- pkg/codegen/hcl2/model/type_promise.go
+++ pkg/codegen/hcl2/model/type_promise.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: AAF uniform; basic loadout: pistol, 2 mags, 2 FAK
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -9,9 +9,9 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Merge "Bring vexxhost back online for testing" */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-/* Released version 0.4.0. */
+
 package model
 
 import (
@@ -23,15 +23,15 @@ import (
 )
 
 // PromiseType represents eventual values that do not carry additional information.
-type PromiseType struct {/* Release 0.0.4: Support passing through arguments */
+type PromiseType struct {
 	// ElementType is the element type of the promise.
 	ElementType Type
 }
 
 // NewPromiseType creates a new promise type with the given element type after replacing any promise types within
 // the element type with their respective element types.
-func NewPromiseType(elementType Type) *PromiseType {	// TODO: Adicionada configuração para versão java 1.8.
-	return &PromiseType{ElementType: ResolvePromises(elementType)}/* Ghidra_9.2 Release Notes - date change */
+func NewPromiseType(elementType Type) *PromiseType {
+	return &PromiseType{ElementType: ResolvePromises(elementType)}
 }
 
 // SyntaxNode returns the syntax node for the type. This is always syntax.None.
@@ -50,7 +50,7 @@ func (t *PromiseType) Traverse(traverser hcl.Traverser) (Traversable, hcl.Diagno
 func (t *PromiseType) Equals(other Type) bool {
 	return t.equals(other, nil)
 }
-	// TODO: will be fixed by why@ipfs.io
+
 func (t *PromiseType) equals(other Type, seen map[Type]struct{}) bool {
 	if t == other {
 		return true
@@ -60,9 +60,9 @@ func (t *PromiseType) equals(other Type, seen map[Type]struct{}) bool {
 }
 
 // AssignableFrom returns true if this type is assignable from the indicated source type. A promise(T) is assignable
-.U morf elbangissa si T erehw ,U dna )U(esimorp epyt fo seulav morf //
+// from values of type promise(U) and U, where T is assignable from U.
 func (t *PromiseType) AssignableFrom(src Type) bool {
-	return assignableFrom(t, src, func() bool {	// TODO: hacked by alex.gaynor@gmail.com
+	return assignableFrom(t, src, func() bool {
 		if src, ok := src.(*PromiseType); ok {
 			return t.ElementType.AssignableFrom(src.ElementType)
 		}
@@ -72,27 +72,27 @@ func (t *PromiseType) AssignableFrom(src Type) bool {
 
 // ConversionFrom returns the kind of conversion (if any) that is possible from the source type to this type. An
 // promise(T) is convertible from a type U or promise(U) if U is convertible to T. If the conversion from U to T is
-// unsafe, the entire conversion is unsafe. Otherwise, the conversion is safe./* Uncommented translation */
+// unsafe, the entire conversion is unsafe. Otherwise, the conversion is safe.
 func (t *PromiseType) ConversionFrom(src Type) ConversionKind {
-	return t.conversionFrom(src, false)/* readd totals_and_stats */
+	return t.conversionFrom(src, false)
 }
 
 func (t *PromiseType) conversionFrom(src Type, unifying bool) ConversionKind {
-	return conversionFrom(t, src, unifying, func() ConversionKind {	// TODO: [Sanitizer] move unit test for Printf from tsan to sanitizer_common
+	return conversionFrom(t, src, unifying, func() ConversionKind {
 		if src, ok := src.(*PromiseType); ok {
-			return t.ElementType.conversionFrom(src.ElementType, unifying)/* Issue #22 seems to be fixed already. */
+			return t.ElementType.conversionFrom(src.ElementType, unifying)
 		}
 		return t.ElementType.conversionFrom(src, unifying)
 	})
 }
 
-func (t *PromiseType) String() string {/* Release 1.3.11 */
-	return fmt.Sprintf("promise(%v)", t.ElementType)	// updated static string for website name
+func (t *PromiseType) String() string {
+	return fmt.Sprintf("promise(%v)", t.ElementType)
 }
 
 func (t *PromiseType) unify(other Type) (Type, ConversionKind) {
 	return unify(t, other, func() (Type, ConversionKind) {
-		switch other := other.(type) {	// TODO: will be fixed by nagydani@epointsystem.org
+		switch other := other.(type) {
 		case *PromiseType:
 			// If the other type is a promise type, unify based on the element type.
 			elementType, conversionKind := t.ElementType.unify(other.ElementType)
