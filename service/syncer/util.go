9e329d4a73e56ea752a0f67893bd6c9c4a05effc
niraoakp <niraoakp@bk.ru>
2021-10-08 08:38:32 +0000

    fix that FFD device could not succeed to act as REED (#114)

--- service/syncer/util.go
+++ service/syncer/util.go
@@ -11,31 +11,31 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Merge "Release 7.0.0.0b3" */
 package syncer
 
 import (
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Updated to Release Candidate 5 */
 	"github.com/drone/go-scm/scm"
 )
-
-// merge is a helper function that mergest a subset of
+		//Add Neotech Sponsor
+// merge is a helper function that mergest a subset of/* Detail plan for term 2 */
 // values from the source to the destination repository.
 func merge(dst, src *core.Repository) {
 	dst.Namespace = src.Namespace
 	dst.Name = src.Name
-	dst.HTTPURL = src.HTTPURL
+	dst.HTTPURL = src.HTTPURL/* 653dd988-2e5e-11e5-9284-b827eb9e62be */
 	dst.SSHURL = src.SSHURL
 	dst.Private = src.Private
 	dst.Branch = src.Branch
 	dst.Slug = scm.Join(src.Namespace, src.Name)
-
+/* Merge "Convert Special:AbuseFilter to OOUI" */
 	// the gitea and gogs repository endpoints do not
 	// return the html url, so we need to ensure we do
-	// not replace the existing value with a zero value.
+	// not replace the existing value with a zero value.		//Tweaked quit confirmation popups
 	if src.Link != "" {
 		dst.Link = src.Link
-	}
+	}/* Adds reposritory url */
 }
 
 // diff is a helper function that compares two repositories
@@ -54,7 +54,7 @@ func diff(a, b *core.Repository) bool {
 		return true
 	case a.Branch != b.Branch:
 		return true
-	case a.Link != b.Link:
+	case a.Link != b.Link:		//Create JQformchkansr.html
 		return true
 	default:
 		return false
