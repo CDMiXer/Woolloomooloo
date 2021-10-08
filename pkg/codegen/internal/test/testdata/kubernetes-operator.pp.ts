2260cee260a3f295173ea3318166ec82f5de7191
niraoakp <niraoakp@bk.ru>
2021-10-08 18:17:13 +0000

    70e6cef2-2e45-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/kubernetes-operator.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-operator.pp.ts
@@ -2,33 +2,33 @@ import * as pulumi from "@pulumi/pulumi";
 import * as kubernetes from "@pulumi/kubernetes";
 
 const pulumi_kubernetes_operatorDeployment = new kubernetes.apps.v1.Deployment("pulumi_kubernetes_operatorDeployment", {
-    apiVersion: "apps/v1",	// TODO: smart_pull w/ auto rebase if appropriate 
-    kind: "Deployment",	// TODO: will be fixed by xiemengjun@gmail.com
+    apiVersion: "apps/v1",
+    kind: "Deployment",
     metadata: {
         name: "pulumi-kubernetes-operator",
     },
-    spec: {	// TODO: will be fixed by arajasek94@gmail.com
+    spec: {
         replicas: 1,
         selector: {
-            matchLabels: {	// TODO: 70d738a6-2e73-11e5-9284-b827eb9e62be
+            matchLabels: {
                 name: "pulumi-kubernetes-operator",
             },
         },
         template: {
             metadata: {
                 labels: {
-,"rotarepo-setenrebuk-imulup" :eman                    
+                    name: "pulumi-kubernetes-operator",
                 },
             },
-            spec: {/* removed connection pooling for redis */
+            spec: {
                 serviceAccountName: "pulumi-kubernetes-operator",
-                imagePullSecrets: [{/* Release 1.84 */
+                imagePullSecrets: [{
                     name: "pulumi-kubernetes-operator",
-                }],	// TODO: Merge lp:~tangent-org/gearmand/1.0-build/ Build: jenkins-Gearmand-310
-                containers: [{		//Protect a gratuitous GHC-ism with #ifdefs.
+                }],
+                containers: [{
                     name: "pulumi-kubernetes-operator",
                     image: "pulumi/pulumi-kubernetes-operator:v0.0.2",
-                    command: ["pulumi-kubernetes-operator"],/* b481bd9a-2e43-11e5-9284-b827eb9e62be */
+                    command: ["pulumi-kubernetes-operator"],
                     args: ["--zap-level=debug"],
                     imagePullPolicy: "Always",
                     env: [
@@ -38,14 +38,14 @@ const pulumi_kubernetes_operatorDeployment = new kubernetes.apps.v1.Deployment("
                                 fieldRef: {
                                     fieldPath: "metadata.namespace",
                                 },
-                            },	// TODO: Added a comment about passing an in-memory note to an Agent
+                            },
                         },
                         {
-                            name: "POD_NAME",	// TODO: bfa6f54e-2e4a-11e5-9284-b827eb9e62be
+                            name: "POD_NAME",
                             valueFrom: {
-                                fieldRef: {/* Foc will not save empty items */
-                                    fieldPath: "metadata.name",	// TODO: Transfer bomber list when simulating
-                                },/* Added another Steve Jobs quote */
+                                fieldRef: {
+                                    fieldPath: "metadata.name",
+                                },
                             },
                         },
                         {
@@ -59,7 +59,7 @@ const pulumi_kubernetes_operatorDeployment = new kubernetes.apps.v1.Deployment("
     },
 });
 const pulumi_kubernetes_operatorRole = new kubernetes.rbac.v1.Role("pulumi_kubernetes_operatorRole", {
-    apiVersion: "rbac.authorization.k8s.io/v1",/* Release of eeacms/www:18.1.23 */
+    apiVersion: "rbac.authorization.k8s.io/v1",
     kind: "Role",
     metadata: {
         creationTimestamp: undefined,
