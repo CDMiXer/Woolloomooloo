7cfd2eda4fc58ba72dc1dfe4740885ead6ee7d61
niraoakp <niraoakp@bk.ru>
2021-10-08 02:45:59 +0000

    now real gradle wrapper

--- pkg/codegen/dotnet/importer.go
+++ pkg/codegen/dotnet/importer.go
@@ -1,19 +1,19 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Merge "ARM: dts: msm: enable truly 720p panel for msm8937 CDP/MTP" */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package dotnet/* Release the badger. */
-/* Style fix for registration form and event creation form. */
+package dotnet
+
 import (
 	"encoding/json"
 
@@ -26,10 +26,10 @@ type CSharpPropertyInfo struct {
 }
 
 // CSharpPackageInfo represents the C# language-specific info for a package.
-type CSharpPackageInfo struct {	// TODO: Update project jQuery-QueryBuilder to 2.3.3 (#11535)
+type CSharpPackageInfo struct {
 	PackageReferences      map[string]string `json:"packageReferences,omitempty"`
 	Namespaces             map[string]string `json:"namespaces,omitempty"`
-	Compatibility          string            `json:"compatibility,omitempty"`/* Removed pdb from Release build */
+	Compatibility          string            `json:"compatibility,omitempty"`
 	DictionaryConstructors bool              `json:"dictionaryConstructors,omitempty"`
 }
 
@@ -39,16 +39,16 @@ var Importer schema.Language = importer(0)
 type importer int
 
 // ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
-func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {	// Add Open ROM hotkey in place of the old Call Menu hotkey
+func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
 }
 
-// ImportPropertySpec decodes language-specific metadata associated with a Property.	// TODO: crude unmarshaller working
+// ImportPropertySpec decodes language-specific metadata associated with a Property.
 func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessage) (interface{}, error) {
 	var info CSharpPropertyInfo
 	if err := json.Unmarshal([]byte(raw), &info); err != nil {
 		return nil, err
-	}/* Release SIIE 3.2 100.01. */
+	}
 	return info, nil
 }
 
@@ -63,15 +63,15 @@ func (importer) ImportResourceSpec(resource *schema.Resource, raw json.RawMessag
 }
 
 // ImportFunctionSpec decodes language-specific metadata associated with a Function.
-func (importer) ImportFunctionSpec(function *schema.Function, raw json.RawMessage) (interface{}, error) {/* add empty view to return copies */
+func (importer) ImportFunctionSpec(function *schema.Function, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
-}/* f663934c-2e60-11e5-9284-b827eb9e62be */
-	// TODO: Added project files part 1
+}
+
 // ImportPackageSpec decodes language-specific metadata associated with a Package.
 func (importer) ImportPackageSpec(pkg *schema.Package, raw json.RawMessage) (interface{}, error) {
 	var info CSharpPackageInfo
-	if err := json.Unmarshal([]byte(raw), &info); err != nil {/* Automatic changelog generation #2884 [ci skip] */
+	if err := json.Unmarshal([]byte(raw), &info); err != nil {
 		return nil, err
 	}
 	return info, nil
-}/* optimizing G */
+}
