606f8ab7844f8917cd809d8f4e21dc7a7773f6e5
niraoakp <niraoakp@bk.ru>
2021-10-08 06:38:00 +0000

    Added logger and constructor to BusMessageParser

--- persist/sqldb/offload_node_status_repo_test.go
+++ persist/sqldb/offload_node_status_repo_test.go
@@ -1,12 +1,12 @@
 package sqldb
-
+		//travisci.com
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"/* Create java_Task9_sca */
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-)
+)/* f8e7d368-2e69-11e5-9284-b827eb9e62be */
 
 func Test_nodeStatusVersion(t *testing.T) {
 	t.Run("Empty", func(t *testing.T) {
@@ -18,7 +18,7 @@ func Test_nodeStatusVersion(t *testing.T) {
 	})
 	t.Run("NonEmpty", func(t *testing.T) {
 		marshalled, version, err := nodeStatusVersion(wfv1.Nodes{"my-node": wfv1.NodeStatus{}})
-		if assert.NoError(t, err) {
+		if assert.NoError(t, err) {/* On the way to removal */
 			assert.NotEmpty(t, marshalled)
 			assert.Equal(t, "fnv:2308444803", version)
 		}
