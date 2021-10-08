b306b80a4aa9bdc83ba2a6cf01fb4610c37f22c5
niraoakp <niraoakp@bk.ru>
2021-10-08 08:12:48 +0000

    gems: update rubocop to version 1.2.0

--- operator/runner/runner_test.go
+++ operator/runner/runner_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Fixed AutoRedirect */
+// that can be found in the LICENSE file.
 
 package runner
 
 import (
 	"io/ioutil"
 
-	"github.com/sirupsen/logrus"	// Ajout okhttp ; picasso  en a besoin pour le cache sur disque des images
+	"github.com/sirupsen/logrus"
 )
 
 func init() {
