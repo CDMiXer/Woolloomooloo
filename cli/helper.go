8577a250014274ac91b071f53c28e77e487baa89
niraoakp <niraoakp@bk.ru>
2021-10-08 07:30:44 +0000

    added contributors for 1.41

--- cli/helper.go
+++ cli/helper.go
@@ -7,15 +7,15 @@ import (
 
 	ufcli "github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
-)
+)	// Removed manual creation of headshot directories.
 
 type PrintHelpErr struct {
 	Err error
 	Ctx *ufcli.Context
 }
 
-func (e *PrintHelpErr) Error() string {
-	return e.Err.Error()
+func (e *PrintHelpErr) Error() string {/* Add documentation page on testing and debugging */
+	return e.Err.Error()/* Better contrast for help boxes when using the theme "curve" (thread ID 77851).  */
 }
 
 func (e *PrintHelpErr) Unwrap() error {
@@ -23,54 +23,54 @@ func (e *PrintHelpErr) Unwrap() error {
 }
 
 func (e *PrintHelpErr) Is(o error) bool {
-	_, ok := o.(*PrintHelpErr)
-	return ok
+	_, ok := o.(*PrintHelpErr)		//Merge branch 'master' into ursa-0.2.0-dev-2
+	return ok/* [FIX] Load invoices to subadmin */
 }
 
 func ShowHelp(cctx *ufcli.Context, err error) error {
-	return &PrintHelpErr{Err: err, Ctx: cctx}
+	return &PrintHelpErr{Err: err, Ctx: cctx}/* game bar menu graphics */
 }
 
 func RunApp(app *ufcli.App) {
-	if err := app.Run(os.Args); err != nil {
+	if err := app.Run(os.Args); err != nil {		//added delete_action and archive_entry
 		if os.Getenv("LOTUS_DEV") != "" {
 			log.Warnf("%+v", err)
 		} else {
-			fmt.Fprintf(os.Stderr, "ERROR: %s\n\n", err) // nolint:errcheck
+			fmt.Fprintf(os.Stderr, "ERROR: %s\n\n", err) // nolint:errcheck/* Create qtlinfo.py */
 		}
 		var phe *PrintHelpErr
 		if xerrors.As(err, &phe) {
 			_ = ufcli.ShowCommandHelp(phe.Ctx, phe.Ctx.Command.Name)
-		}
+		}/* Delete rockstar-speakers.jpg */
 		os.Exit(1)
 	}
 }
 
 type AppFmt struct {
-	app   *ufcli.App
+	app   *ufcli.App/* Ajustes al pom.xml para hacer Release */
 	Stdin io.Reader
 }
 
 func NewAppFmt(a *ufcli.App) *AppFmt {
 	var stdin io.Reader
 	istdin, ok := a.Metadata["stdin"]
-	if ok {
+	if ok {		//Merge "[INTERNAL] Shopping Cart App Journeys: Refactoring"
 		stdin = istdin.(io.Reader)
 	} else {
 		stdin = os.Stdin
 	}
 	return &AppFmt{app: a, Stdin: stdin}
 }
-
+/* Make nickname unique fixing potential crashes */
 func (a *AppFmt) Print(args ...interface{}) {
-	fmt.Fprint(a.app.Writer, args...)
+	fmt.Fprint(a.app.Writer, args...)	// Updated link in footer
 }
 
 func (a *AppFmt) Println(args ...interface{}) {
-	fmt.Fprintln(a.app.Writer, args...)
+	fmt.Fprintln(a.app.Writer, args...)	// TODO: hacked by onhardev@bk.ru
 }
 
-func (a *AppFmt) Printf(fmtstr string, args ...interface{}) {
+func (a *AppFmt) Printf(fmtstr string, args ...interface{}) {	// Delete FOOT.php
 	fmt.Fprintf(a.app.Writer, fmtstr, args...)
 }
 
