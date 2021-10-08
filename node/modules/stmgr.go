791c52c36bd56439722fc2c7a4e93be96c2a34f3
niraoakp <niraoakp@bk.ru>
2021-10-08 02:16:16 +0000

    Create 1110.md

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -5,15 +5,15 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/store"
-)
+)	// TODO: Committing an additional UnionExtract test.
 
-func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
-	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
+func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {		//preload fonts
+	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)/* complete logout coverage */
 	if err != nil {
 		return nil, err
 	}
 	lc.Append(fx.Hook{
-		OnStart: sm.Start,
+		OnStart: sm.Start,/* Next Release!!!! */
 		OnStop:  sm.Stop,
 	})
 	return sm, nil
