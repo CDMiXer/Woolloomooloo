c1fb811e2a40eb58f0ea056cdcac588090ccd8f5
niraoakp <niraoakp@bk.ru>
2021-10-08 05:10:46 +0000

    Added comment for later reference

--- gen/conn_write.go
+++ gen/conn_write.go
@@ -1,9 +1,9 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file./* UPDATE: CLO-12285 - CloverJMX mBean should be singleton - refactor */
 
 // +build go1.8
-
+/* Merge "Release 4.4.31.75" */
 package websocket
 
 import "net"
