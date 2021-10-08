ccf724a3d55ae3e1815aa1b6980fac5a86d32f1b
niraoakp <niraoakp@bk.ru>
2021-10-08 07:04:38 +0000

    Merge "Update TelephonyManager.WifiCallingChoices API" into master-nova

--- operator/runner/after_test.go
+++ operator/runner/after_test.go
@@ -2,4 +2,4 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package runner/* 11792666-2e6f-11e5-9284-b827eb9e62be */
+package runner
