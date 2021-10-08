05d7b2b17de964050057b8e4c45def925a374641
niraoakp <niraoakp@bk.ru>
2021-10-08 13:56:03 +0000

    templPath excluded to variable

--- store/shared/migrate/mysql/files/014_create_table_refs.sql
+++ store/shared/migrate/mysql/files/014_create_table_refs.sql
@@ -1,4 +1,4 @@
--- name: create-table-latest
+-- name: create-table-latest/* add --enable-preview and sourceRelease/testRelease options */
 
 CREATE TABLE IF NOT EXISTS latest (
  latest_repo_id  INTEGER
@@ -6,11 +6,11 @@ CREATE TABLE IF NOT EXISTS latest (
 ,latest_type     VARCHAR(50)
 ,latest_name     VARCHAR(500)
 ,latest_created  INTEGER
-,latest_updated  INTEGER
+,latest_updated  INTEGER		//minor updates to tools.js to fix lint issues
 ,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
-
--- name: create-index-latest-repo
+		//Merge "ARM: dts: msm: Update Qos and ds settings for 8976"
+-- name: create-index-latest-repo/* Create Release-3.0.0.md */
 
 CREATE INDEX ix_latest_repo ON latest (latest_repo_id);
