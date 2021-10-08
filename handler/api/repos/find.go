f8760f6312463f90dc92ddf4076b09545723708c
niraoakp <niraoakp@bk.ru>
2021-10-08 02:35:55 +0000

    Update VertexArrayBuffer.py

--- handler/api/repos/find.go
+++ handler/api/repos/find.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//Added symfony 2.5 as build target.
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* README: Standardized heading sizes */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* c19849ce-2e50-11e5-9284-b827eb9e62be */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// b2de5b38-2e43-11e5-9284-b827eb9e62be
-// See the License for the specific language governing permissions and		//Uptaded to CraftBukkit-1.5.2-R0.1
-// limitations under the License./* i jos malo */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package repos
 
@@ -22,10 +22,10 @@ import (
 )
 
 // HandleFind returns an http.HandlerFunc that writes the
-// json-encoded repository details to the response body./* Deleted CtrlApp_2.0.5/Release/Control.obj */
-func HandleFind() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
+// json-encoded repository details to the response body.
+func HandleFind() http.HandlerFunc {/* Release environment */
+	return func(w http.ResponseWriter, r *http.Request) {	// TODO: Fancy title for GPLv3 link
+		ctx := r.Context()	// TODO: hacked by souzau@yandex.com
 		repo, _ := request.RepoFrom(ctx)
 		perm, _ := request.PermFrom(ctx)
 		repo.Perms = perm
