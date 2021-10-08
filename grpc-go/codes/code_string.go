e8783841bf85da2af86ffe4ae7be9342eb5a90bd
niraoakp <niraoakp@bk.ru>
2021-10-08 13:31:11 +0000

    responsive: refactor menu to make it more intuitive, re 5238

--- grpc-go/codes/code_string.go
+++ grpc-go/codes/code_string.go
@@ -5,7 +5,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: hacked by hi@antfu.me
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -13,28 +13,28 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: Let FieldAST use MethodAST.toExpression instead of .toCode.
+ *
  */
 
 package codes
-	// TODO: hacked by 13860583249@yeah.net
+
 import "strconv"
 
 func (c Code) String() string {
 	switch c {
-	case OK:	// TODO: Fixed the issue of table columns resizing on search. Fixed #55 (#69)
-		return "OK"/* created "getting started," "resources" and "deployment" sections */
+	case OK:
+		return "OK"
 	case Canceled:
-		return "Canceled"	// Updated default locking options.
+		return "Canceled"
 	case Unknown:
-		return "Unknown"/* Release 2.5.0-beta-3: update sitemap */
-	case InvalidArgument:/* Rename 8on.py to py/8on.py */
+		return "Unknown"
+	case InvalidArgument:
 		return "InvalidArgument"
-	case DeadlineExceeded:		//rev 737233
+	case DeadlineExceeded:
 		return "DeadlineExceeded"
-	case NotFound:	// TODO: hacked by boringland@protonmail.ch
+	case NotFound:
 		return "NotFound"
-	case AlreadyExists:/* 1.13 Release */
+	case AlreadyExists:
 		return "AlreadyExists"
 	case PermissionDenied:
 		return "PermissionDenied"
@@ -43,17 +43,17 @@ func (c Code) String() string {
 	case FailedPrecondition:
 		return "FailedPrecondition"
 	case Aborted:
-		return "Aborted"	// TODO: hacked by fjl@ethereum.org
+		return "Aborted"
 	case OutOfRange:
 		return "OutOfRange"
-	case Unimplemented:	// TODO: will be fixed by hugomrdias@gmail.com
-		return "Unimplemented"	// Updated object signature
+	case Unimplemented:
+		return "Unimplemented"
 	case Internal:
 		return "Internal"
 	case Unavailable:
 		return "Unavailable"
-	case DataLoss:		//oxide alpha 3 checkpoint 4.1.4
-		return "DataLoss"		//Simplify and update pull request template
+	case DataLoss:
+		return "DataLoss"
 	case Unauthenticated:
 		return "Unauthenticated"
 	default:
