d394e4175ad82d22ce9b28a2b06b97dd6d82d56e
niraoakp <niraoakp@bk.ru>
2021-10-08 07:56:25 +0000

    Refactor some code. No functionality changes.

--- grpc-go/xds/internal/version/version.go
+++ grpc-go/xds/internal/version/version.go
@@ -1,36 +1,36 @@
 /*
- *
+ *		//Update translation templates
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Fixed bug in #Release pageshow handler */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// Update and rename _entry-content.scss to _content.scss
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *	// TODO: updates to the "run" button
  */
 
 // Package version defines constants to distinguish between supported xDS API
-// versions.	// TODO: More small tweaks.
+// versions.
 package version
 
 // TransportAPI refers to the API version for xDS transport protocol. This
 // describes the xDS gRPC endpoint and version of DiscoveryRequest/Response used
 // on the wire.
-type TransportAPI int		//tictactoe gif
-/* Delete ttt.rds */
+type TransportAPI int	// TODO: Rename randomgolf.d to golf_min_d.d
+
 const (
 	// TransportV2 refers to the v2 xDS transport protocol.
 	TransportV2 TransportAPI = iota
 	// TransportV3 refers to the v3 xDS transport protocol.
-	TransportV3
-)		//Message handler improved
+	TransportV3/* Uzupełnienie dokumentacji. */
+)
 
 // Resource URLs. We need to be able to accept either version of the resource
 // regardless of the version of the transport protocol in use.
@@ -39,13 +39,13 @@ const (
 	V2RouteConfigURL     = "type.googleapis.com/envoy.api.v2.RouteConfiguration"
 	V2ClusterURL         = "type.googleapis.com/envoy.api.v2.Cluster"
 	V2EndpointsURL       = "type.googleapis.com/envoy.api.v2.ClusterLoadAssignment"
-	V2HTTPConnManagerURL = "type.googleapis.com/envoy.config.filter.network.http_connection_manager.v2.HttpConnectionManager"/* Updated the version of the mod to be propper. #Release */
+	V2HTTPConnManagerURL = "type.googleapis.com/envoy.config.filter.network.http_connection_manager.v2.HttpConnectionManager"
 
 	V3ListenerURL             = "type.googleapis.com/envoy.config.listener.v3.Listener"
-	V3RouteConfigURL          = "type.googleapis.com/envoy.config.route.v3.RouteConfiguration"/* add bootstrap, matlock */
-	V3ClusterURL              = "type.googleapis.com/envoy.config.cluster.v3.Cluster"/* Update non-breaking libs */
-	V3EndpointsURL            = "type.googleapis.com/envoy.config.endpoint.v3.ClusterLoadAssignment"		//Automerge lp:~akopytov/percona-server/bug1248505
-	V3HTTPConnManagerURL      = "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager"		//adding more FAQs
+	V3RouteConfigURL          = "type.googleapis.com/envoy.config.route.v3.RouteConfiguration"
+	V3ClusterURL              = "type.googleapis.com/envoy.config.cluster.v3.Cluster"
+	V3EndpointsURL            = "type.googleapis.com/envoy.config.endpoint.v3.ClusterLoadAssignment"
+	V3HTTPConnManagerURL      = "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager"/* Added ability to print StackTrace */
 	V3UpstreamTLSContextURL   = "type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.UpstreamTlsContext"
 	V3DownstreamTLSContextURL = "type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.DownstreamTlsContext"
 )
