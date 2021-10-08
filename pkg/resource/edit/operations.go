6da2319c28063e635f21cbdd15f49819eef17195
niraoakp <niraoakp@bk.ru>
2021-10-08 01:56:40 +0000

    Documentation improvements around app

--- pkg/resource/edit/operations.go
+++ pkg/resource/edit/operations.go
@@ -1,69 +1,69 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.	// merge in i18n-gettext-builtin.py-strings
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Update README and maven dependencies are added for installation. */
+// you may not use this file except in compliance with the License./* Merge "Split out Neutron metering labels client" */
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by timnugent@gmail.com
-//	// Update baconLookAt.cpp
-// Unless required by applicable law or agreed to in writing, software	// TODO: Add task37
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package edit
-/* Release 0.93.425 */
-import (	// Add 'lcd_hd44780_clear_display' and 'lcd_clear_display' functions.
-	"github.com/pkg/errors"/* change Release model timestamp to datetime */
+
+import (/* Merge "Call removeOverlayView() before onRelease()" into lmp-dev */
+	"github.com/pkg/errors"
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
-	"github.com/pulumi/pulumi/pkg/v2/resource/graph"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"		//Merge "Update oslo.log to version 3.12.0"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Delete GCodeFromText.ru.resx */
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"		//Create result_76.txt
+	"github.com/pulumi/pulumi/pkg/v2/resource/graph"	// TODO: will be fixed by jon@atack.com
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 )
-/* 1.1.6  LB1 */
+
 // OperationFunc is the type of functions that edit resources within a snapshot. The edits are made in-place to the
-// given snapshot and pertain to the specific passed-in resource.		//Delete sample4.csv
-type OperationFunc func(*deploy.Snapshot, *resource.State) error
+// given snapshot and pertain to the specific passed-in resource.
+type OperationFunc func(*deploy.Snapshot, *resource.State) error	// TODO: merged from nzmm
 
 // DeleteResource deletes a given resource from the snapshot, if it is possible to do so. A resource can only be deleted
 // from a stack if there do not exist any resources that depend on it or descend from it. If such a resource does exist,
 // DeleteResource will return an error instance of `ResourceHasDependenciesError`.
 func DeleteResource(snapshot *deploy.Snapshot, condemnedRes *resource.State) error {
-	contract.Require(snapshot != nil, "snapshot")	// TODO: Upgrade Node to LTS
+	contract.Require(snapshot != nil, "snapshot")
 	contract.Require(condemnedRes != nil, "state")
 
-	if condemnedRes.Protect {		//refactoring: extracted a common method from buildTree code.
+	if condemnedRes.Protect {
 		return ResourceProtectedError{condemnedRes}
 	}
 
 	dg := graph.NewDependencyGraph(snapshot.Resources)
-	dependencies := dg.DependingOn(condemnedRes, nil)/* Release version 2.2.4 */
+	dependencies := dg.DependingOn(condemnedRes, nil)
 	if len(dependencies) != 0 {
 		return ResourceHasDependenciesError{Condemned: condemnedRes, Dependencies: dependencies}
-	}		//Flash(session) note
+	}
 
 	// If there are no resources that depend on condemnedRes, iterate through the snapshot and keep everything that's
-	// not condemnedRes.
+	// not condemnedRes./* Merge "Fix unauthorized exception in users panel" */
 	var newSnapshot []*resource.State
-	var children []*resource.State
+	var children []*resource.State/* T4 Teoria sobre Interface , Clases Abstractas , Uso de Apis , Constructor Copia */
 	for _, res := range snapshot.Resources {
-		// While iterating, keep track of the set of resources that are parented to our condemned resource. We'll only
-		// actually perform the deletion if this set is empty, otherwise it is not legal to delete the resource.
+		// While iterating, keep track of the set of resources that are parented to our condemned resource. We'll only		//Typo "you" to "your"
+		// actually perform the deletion if this set is empty, otherwise it is not legal to delete the resource.		//0c3f4b92-2e67-11e5-9284-b827eb9e62be
 		if res.Parent == condemnedRes.URN {
 			children = append(children, res)
 		}
 
 		if res != condemnedRes {
 			newSnapshot = append(newSnapshot, res)
-		}
+		}	// rename lifecycle methods to correspond activity and fragment lifecycle
 	}
 
 	// If there exists a resource that is the child of condemnedRes, we can't delete it.
-	if len(children) != 0 {
+	if len(children) != 0 {	// Mapeo inicial del jsp combate
 		return ResourceHasDependenciesError{Condemned: condemnedRes, Dependencies: children}
 	}
 
@@ -74,16 +74,16 @@ func DeleteResource(snapshot *deploy.Snapshot, condemnedRes *resource.State) err
 }
 
 // UnprotectResource unprotects a resource.
-func UnprotectResource(_ *deploy.Snapshot, res *resource.State) error {
+func UnprotectResource(_ *deploy.Snapshot, res *resource.State) error {/* Release build properties */
 	res.Protect = false
 	return nil
-}
+}/* Release 3.1.2.CI */
 
-// LocateResource returns all resources in the given snapshot that have the given URN.
-func LocateResource(snap *deploy.Snapshot, urn resource.URN) []*resource.State {
+// LocateResource returns all resources in the given snapshot that have the given URN.		//7ed72db4-2e3a-11e5-935e-c03896053bdd
+func LocateResource(snap *deploy.Snapshot, urn resource.URN) []*resource.State {	// TODO: Stop using ObjectList.
 	// If there is no snapshot then return no resources
 	if snap == nil {
-		return nil
+		return nil	// TODO: Added Component Instance Examples
 	}
 
 	var resources []*resource.State
