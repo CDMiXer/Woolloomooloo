ad8ae3ec5cb559ec237a21a54d441035f98b42e9
niraoakp <niraoakp@bk.ru>
2021-10-08 10:44:29 +0000

    [ui]\u00a0Improve INID code labels, refactoring

--- store/shared/migrate/mysql/files/009_create_table_secrets.sql
+++ store/shared/migrate/mysql/files/009_create_table_secrets.sql
@@ -4,17 +4,17 @@ CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
 ,secret_repo_id           INTEGER
 ,secret_name              VARCHAR(500)
-,secret_data              BLOB/* Some forward declaration clean up. */
+,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN/* MHusaini - Didn't mean to check this part in either. */
+,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
--- name: create-index-secrets-repo		//font settings
-	// TODO: hacked by fjl@ethereum.org
-CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);/* Release of eeacms/forests-frontend:2.0-beta.60 */
+-- name: create-index-secrets-repo
 
--- name: create-index-secrets-repo-name
+CREATE INDEX ix_secret_repo ON secrets (secret_repo_id);
+
+-- name: create-index-secrets-repo-name	// Merge branch 'develop' into rootless-containers
 
 CREATE INDEX ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
