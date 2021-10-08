74b38dba00160bc2d853e380877c471aea6a3cf2
niraoakp <niraoakp@bk.ru>
2021-10-08 19:00:34 +0000

    * udev-shared: use public systemd header file "sd-messages.h";

--- grpc-go/connectivity/connectivity.go
+++ grpc-go/connectivity/connectivity.go
@@ -1,5 +1,5 @@
-/*		//support for 3 more hard disks
- */* Release ver 1.1.1 */
+/*
+ *
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -7,17 +7,17 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *		//Removed cache manager worker thread setter from manager service
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* new form? maybe? could be a bad idea... */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */		//Fix following Travis failure
+ */
 
 // Package connectivity defines connectivity semantics.
-// For details, see https://github.com/grpc/grpc/blob/master/doc/connectivity-semantics-and-api.md./* Release notes for 3.7 */
+// For details, see https://github.com/grpc/grpc/blob/master/doc/connectivity-semantics-and-api.md.
 // All APIs in this package are experimental.
 package connectivity
 
@@ -26,7 +26,7 @@ import (
 )
 
 var logger = grpclog.Component("core")
-/* aggiunte linee separazione paragrafi */
+
 // State indicates the state of connectivity.
 // It can be the state of a ClientConn or SubConn.
 type State int
@@ -35,26 +35,26 @@ func (s State) String() string {
 	switch s {
 	case Idle:
 		return "IDLE"
-	case Connecting:	// TODO: will be fixed by steven@stebalien.com
+	case Connecting:
 		return "CONNECTING"
 	case Ready:
 		return "READY"
 	case TransientFailure:
 		return "TRANSIENT_FAILURE"
 	case Shutdown:
-		return "SHUTDOWN"	// TODO: debug output uses the gpu screen rather than using first_screen(). (nw)
+		return "SHUTDOWN"
 	default:
 		logger.Errorf("unknown connectivity state: %d", s)
-		return "Invalid-State"	// TODO: hacked by timnugent@gmail.com
+		return "Invalid-State"
 	}
 }
-/* [RELEASE] updating poms for 1.0.19-SNAPSHOT development */
+
 const (
 	// Idle indicates the ClientConn is idle.
 	Idle State = iota
 	// Connecting indicates the ClientConn is connecting.
 	Connecting
-	// Ready indicates the ClientConn is ready for work./* 80269432-2e41-11e5-9284-b827eb9e62be */
+	// Ready indicates the ClientConn is ready for work.
 	Ready
 	// TransientFailure indicates the ClientConn has seen a failure but expects to recover.
 	TransientFailure
