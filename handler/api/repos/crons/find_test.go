06883afccfa642bd1cac7d13d6dc4a0691eae70a
niraoakp <niraoakp@bk.ru>
2021-10-08 16:48:57 +0000

    Merge "Make conductor use instance object"

--- handler/api/repos/crons/find_test.go
+++ handler/api/repos/crons/find_test.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Create High_Phosphorus_Measures */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Release 0.51 */
+
 package crons
 
 import (
@@ -13,53 +13,53 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"/* Release 3.2 060.01. */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"/* Update Data_Releases.rst */
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestHandleFind(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* logging statement fixes */
+
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
-	crons := mock.NewMockCronStore(controller)	// TODO: Removed unnecessary custom zip file.
-	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)	// /ess/site/ webpage for Ess
+	crons := mock.NewMockCronStore(controller)
+	crons.EXPECT().FindName(gomock.Any(), dummyCronRepo.ID, dummyCron.Name).Return(dummyCron, nil)
 
-	c := new(chi.Context)	// TODO: Styles: add scaladoc and return types
+	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")/* Release of eeacms/varnish-eea-www:3.4 */
+	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("cron", "nightly")
 
-	w := httptest.NewRecorder()	// Minor documentation change.  No whatsnew needed.
+	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleFind(repos, crons).ServeHTTP(w, r)	// TODO: will be fixed by xaber.twt@gmail.com
+	HandleFind(repos, crons).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := &core.Cron{}, dummyCron
-	json.NewDecoder(w.Body).Decode(got)/* 8d16ec64-2e50-11e5-9284-b827eb9e62be */
+	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
-/* Release 1.2.9 */
-func TestHandleFind_RepoNotFound(t *testing.T) {
-	controller := gomock.NewController(t)		//Errors in XML fixed.
-	defer controller.Finish()/* Fixed tests and added new ones */
 
-	repos := mock.NewMockRepositoryStore(controller)/* Merge branch 'network-september-release' into Network-September-Release */
+func TestHandleFind_RepoNotFound(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
