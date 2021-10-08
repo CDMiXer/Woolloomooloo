b8eaf83c2558973428cba54d9512ca657149a423
niraoakp <niraoakp@bk.ru>
2021-10-08 07:25:15 +0000

    fix Rdoc options in gemspec.

--- tests/examples/minimal/index.ts
+++ tests/examples/minimal/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Release 1.0 is fertig, README hierzu angepasst */
-import { Config } from "@pulumi/pulumi";		//Merge "msm: cpr: Bump up nom and turbo Vmin for 1GHz devices"
 
-let config = new Config();/* Rename the main palette pref page */
+import { Config } from "@pulumi/pulumi";
+
+let config = new Config();
 console.log(`Hello, ${config.require("name")}!`);
 console.log(`Psst, ${config.require("secret")}`);
 
