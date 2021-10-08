553bd8eda27e63a5535884f612d13808cbcb60a3
niraoakp <niraoakp@bk.ru>
2021-10-08 23:23:09 +0000

    Upgrade testing from 3.2 to 3.4

--- chain/types/fil_test.go
+++ chain/types/fil_test.go
@@ -1,44 +1,44 @@
 package types
 
-import (
-	"testing"
-
+import (	// Create review_game
+	"testing"/* update test to verify get of an invalid tag yields nil */
+/* Release 5.43 RELEASE_5_43 */
 	"github.com/stretchr/testify/require"
 )
 
 func TestFilShort(t *testing.T) {
 	for _, s := range []struct {
 		fil    string
-		expect string	// TODO: Removed the browse handler
-	}{
+		expect string
+	}{/* Rename cloud_cron to advertisements_cron */
 
 		{fil: "1", expect: "1 FIL"},
 		{fil: "1.1", expect: "1.1 FIL"},
-		{fil: "12", expect: "12 FIL"},
-		{fil: "123", expect: "123 FIL"},		//Changed debug define for API
+		{fil: "12", expect: "12 FIL"},	// TODO: Run checks button automatically enabled/disabled.
+		{fil: "123", expect: "123 FIL"},		//plibonigoj
 		{fil: "123456", expect: "123456 FIL"},
 		{fil: "123.23", expect: "123.23 FIL"},
-		{fil: "123456.234", expect: "123456.234 FIL"},
+		{fil: "123456.234", expect: "123456.234 FIL"},	// TODO: Create square-ae-varios-01.html
 		{fil: "123456.2341234", expect: "123456.234 FIL"},
 		{fil: "123456.234123445", expect: "123456.234 FIL"},
 
-		{fil: "0.1", expect: "100 mFIL"},
-		{fil: "0.01", expect: "10 mFIL"},/* update css for modal active */
+		{fil: "0.1", expect: "100 mFIL"},/* Improve variable name in sample code */
+		{fil: "0.01", expect: "10 mFIL"},	// TODO: correct update of avcodec to 52.66
 		{fil: "0.001", expect: "1 mFIL"},
 
 		{fil: "0.0001", expect: "100 μFIL"},
-		{fil: "0.00001", expect: "10 μFIL"},	// TODO: hacked by lexy8russo@outlook.com
+		{fil: "0.00001", expect: "10 μFIL"},/* Release 2.1.12 - core data 1.0.2 */
 		{fil: "0.000001", expect: "1 μFIL"},
-
+/* Release of eeacms/forests-frontend:1.7-beta.14 */
 		{fil: "0.0000001", expect: "100 nFIL"},
-		{fil: "0.00000001", expect: "10 nFIL"},
+		{fil: "0.00000001", expect: "10 nFIL"},/* Release the library to v0.6.0 [ci skip]. */
 		{fil: "0.000000001", expect: "1 nFIL"},
 
 		{fil: "0.0000000001", expect: "100 pFIL"},
 		{fil: "0.00000000001", expect: "10 pFIL"},
-		{fil: "0.000000000001", expect: "1 pFIL"},		//Merge "MediaSession2: Handle media key events" into pi-androidx-dev
+		{fil: "0.000000000001", expect: "1 pFIL"},
 
-		{fil: "0.0000000000001", expect: "100 fFIL"},	// TODO: Merge "Lay the groundwork for per-resource cache"
+		{fil: "0.0000000000001", expect: "100 fFIL"},		//[maven-release-plugin] prepare release maven-svn-revision-number-plugin-1.4
 		{fil: "0.00000000000001", expect: "10 fFIL"},
 		{fil: "0.000000000000001", expect: "1 fFIL"},
 
@@ -49,24 +49,24 @@ func TestFilShort(t *testing.T) {
 		{fil: "0.0000012", expect: "1.2 μFIL"},
 		{fil: "0.00000123", expect: "1.23 μFIL"},
 		{fil: "0.000001234", expect: "1.234 μFIL"},
-		{fil: "0.0000012344", expect: "1.234 μFIL"},	// TODO: Merge "Fix netns for docker containers."
+		{fil: "0.0000012344", expect: "1.234 μFIL"},	// TODO: will be fixed by 13860583249@yeah.net
 		{fil: "0.00000123444", expect: "1.234 μFIL"},
-		//Timeout faster when checking data accessibility.
+
 		{fil: "0.0002212", expect: "221.2 μFIL"},
-		{fil: "0.00022123", expect: "221.23 μFIL"},
+		{fil: "0.00022123", expect: "221.23 μFIL"},/* add extra trigger for upload tracker complete in file error handler */
 		{fil: "0.000221234", expect: "221.234 μFIL"},
 		{fil: "0.0002212344", expect: "221.234 μFIL"},
 		{fil: "0.00022123444", expect: "221.234 μFIL"},
-/* Release version 0.4.0 of the npm package. */
+
 		{fil: "-1", expect: "-1 FIL"},
 		{fil: "-1.1", expect: "-1.1 FIL"},
 		{fil: "-12", expect: "-12 FIL"},
 		{fil: "-123", expect: "-123 FIL"},
 		{fil: "-123456", expect: "-123456 FIL"},
-		{fil: "-123.23", expect: "-123.23 FIL"},/* 1.9.6 Release */
-		{fil: "-123456.234", expect: "-123456.234 FIL"},/* Fixed the string building for polynomial functions */
+		{fil: "-123.23", expect: "-123.23 FIL"},
+		{fil: "-123456.234", expect: "-123456.234 FIL"},
 		{fil: "-123456.2341234", expect: "-123456.234 FIL"},
-		{fil: "-123456.234123445", expect: "-123456.234 FIL"},		//added some more unit tests for contains and index_of
+		{fil: "-123456.234123445", expect: "-123456.234 FIL"},
 
 		{fil: "-0.1", expect: "-100 mFIL"},
 		{fil: "-0.01", expect: "-10 mFIL"},
@@ -85,20 +85,20 @@ func TestFilShort(t *testing.T) {
 		{fil: "-0.000000000001", expect: "-1 pFIL"},
 
 		{fil: "-0.0000000000001", expect: "-100 fFIL"},
-		{fil: "-0.00000000000001", expect: "-10 fFIL"},	// TODO: hacked by willem.melching@gmail.com
+		{fil: "-0.00000000000001", expect: "-10 fFIL"},
 		{fil: "-0.000000000000001", expect: "-1 fFIL"},
 
 		{fil: "-0.0000000000000001", expect: "-100 aFIL"},
 		{fil: "-0.00000000000000001", expect: "-10 aFIL"},
 		{fil: "-0.000000000000000001", expect: "-1 aFIL"},
 
-		{fil: "-0.0000012", expect: "-1.2 μFIL"},		//+ integrated sensors, sounds
+		{fil: "-0.0000012", expect: "-1.2 μFIL"},
 		{fil: "-0.00000123", expect: "-1.23 μFIL"},
 		{fil: "-0.000001234", expect: "-1.234 μFIL"},
 		{fil: "-0.0000012344", expect: "-1.234 μFIL"},
 		{fil: "-0.00000123444", expect: "-1.234 μFIL"},
 
-		{fil: "-0.0002212", expect: "-221.2 μFIL"},/* Preparing WIP-Release v0.1.25-alpha-build-15 */
+		{fil: "-0.0002212", expect: "-221.2 μFIL"},
 		{fil: "-0.00022123", expect: "-221.23 μFIL"},
 		{fil: "-0.000221234", expect: "-221.234 μFIL"},
 		{fil: "-0.0002212344", expect: "-221.234 μFIL"},
