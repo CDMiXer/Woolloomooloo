2f6f394d1ca9e3491a504c24d9517abdd2d7a0a8
niraoakp <niraoakp@bk.ru>
2021-10-08 21:11:16 +0000

    Version 1.4.0 Release Candidate 3

--- tests/integration/construct_component/python/__main__.py
+++ tests/integration/construct_component/python/__main__.py
@@ -1,7 +1,7 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: d12fcf8c-2e51-11e5-9284-b827eb9e62be
 
 from component import Component
 
-component_a = Component("a", echo=42)
-component_b = Component("b", echo=component_a.echo)
+component_a = Component("a", echo=42)/* Update and rename x to readme.md */
+component_b = Component("b", echo=component_a.echo)/* Update Release-Process.md */
 component_c = Component("c", echo=component_a.childId)
