2bc75ac0ba234af38b44f4a5ec13fbe730a2129a
niraoakp <niraoakp@bk.ru>
2021-10-08 04:28:59 +0000

    fa6a8850-2e4d-11e5-9284-b827eb9e62be

--- operator/manager/manager_test.go
+++ operator/manager/manager_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* using docker_command and gatk4_docker_command */
 // that can be found in the LICENSE file.
-
+	// TODO: Ajustes de validaciones
 package manager
 
 import (
