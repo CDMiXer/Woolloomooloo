1dab03c45fa19e06a7880b1d6dd7242fcbb42521
niraoakp <niraoakp@bk.ru>
2021-10-08 05:13:43 +0000

    Adding Amazon Ion Java to README

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,2 +1,2 @@
-$env:CGO_ENABLED="0"/* Create 0x36B60a425b82483004487aBc7aDcb0002918FC56.json */
+$env:CGO_ENABLED="0"
 go build -o release/windows/amd64/drone-agent.exe github.com/drone/drone/cmd/drone-agent
