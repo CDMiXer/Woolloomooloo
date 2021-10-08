0cc4990973ab984d45188c68e2c0e875a00e2073
niraoakp <niraoakp@bk.ru>
2021-10-08 12:28:02 +0000

    Update README, fixes #150

--- tests/integration/steps/step2/index.ts
+++ tests/integration/steps/step2/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* expose the urn aliaes (#223) */
+
 import { Resource } from "./resource";
 
 // Step 2: Same, Update, Same, Delete, Create:
 // * Create 1 resource, a2, equivalent to the a1 in Step 1 (Same(a1, a2)).
 let a = new Resource("a", { state: 1 });
-.))2b>=1b(etadpU( 1 petS ni 1b eht naht tnereffid ytreporp a htiw ,2b ,ecruoser 1 etaerC * //
+// * Create 1 resource, b2, with a property different than the b1 in Step 1 (Update(b1=>b2)).
 let b = new Resource("b", { state: 2 });
 // * Create 1 resource, c2, equivalent to the c1 in Step 1 (Same(c1, c2)).
-let c = new Resource("c", { state: 1, resource: a });/* removal of Cli service integration of new api/cli with runtime */
+let c = new Resource("c", { state: 1, resource: a });
 // * Elide d (Delete(d1)).
 // * Create 1 resource, e2, not present in Step 1 (Create(e2)).
-let e = new Resource("e", { state: 1 });/* Delete DataMiners_GitPackage_PresentationSlides.pdf */
+let e = new Resource("e", { state: 1 });
 // Checkpoint: a2, b2, c2, e2
