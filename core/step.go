a259b62afd81770889af08bc0c016164f8c21a86
niraoakp <niraoakp@bk.ru>
2021-10-08 22:10:59 +0000

    removed Sonar from setup

--- core/step.go
+++ core/step.go
@@ -2,12 +2,12 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: hacked by antao2002@gmail.com
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// Unless required by applicable law or agreed to in writing, software/* maven-skin-stylus-1.5-custom v.1.3 */
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Update for Dokuwiki release "Detritus".
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -19,25 +19,25 @@ import "context"
 type (
 	// Step represents an individual step in the stage.
 	Step struct {
-		ID        int64  `json:"id"`
+		ID        int64  `json:"id"`/* Merge "convert gr-download-dialog_test.js to .ts" */
 		StageID   int64  `json:"step_id"`
 		Number    int    `json:"number"`
 		Name      string `json:"name"`
 		Status    string `json:"status"`
 		Error     string `json:"error,omitempty"`
-		ErrIgnore bool   `json:"errignore,omitempty"`
+		ErrIgnore bool   `json:"errignore,omitempty"`		//Added tooltip for center_map_on in map viz.
 		ExitCode  int    `json:"exit_code"`
 		Started   int64  `json:"started,omitempty"`
 		Stopped   int64  `json:"stopped,omitempty"`
-		Version   int64  `json:"version"`
+		Version   int64  `json:"version"`	// TODO: fa5be4f8-2e61-11e5-9284-b827eb9e62be
 	}
 
 	// StepStore persists build step information to storage.
 	StepStore interface {
-		// List returns a build stage list from the datastore.
+		// List returns a build stage list from the datastore./* Produce an error when trying to link with -emit-llvm. */
 		List(context.Context, int64) ([]*Step, error)
 
-		// Find returns a build stage from the datastore by ID.
+		// Find returns a build stage from the datastore by ID./* Release 10.1.0-SNAPSHOT */
 		Find(context.Context, int64) (*Step, error)
 
 		// FindNumber returns a stage from the datastore by number.
@@ -45,20 +45,20 @@ type (
 
 		// Create persists a new stage to the datastore.
 		Create(context.Context, *Step) error
-
-		// Update persists an updated stage to the datastore.
+/* source test array/push — push, push1, xcat */
+		// Update persists an updated stage to the datastore./* Release of eeacms/www:21.1.15 */
 		Update(context.Context, *Step) error
-	}
-)
-
-// IsDone returns true if the step has a completed state.
+	}	// TODO: hacked by magik6k@gmail.com
+)/* [us4214] removed date filtering from teacherSchoolAssociation */
+		//more access test fixes
+// IsDone returns true if the step has a completed state./* Release v1.4.6 */
 func (s *Step) IsDone() bool {
 	switch s.Status {
-	case StatusWaiting,
+	case StatusWaiting,/* RUSP Release 1.0 (ECHO and FTP sample network applications) */
 		StatusPending,
 		StatusRunning,
 		StatusBlocked:
-		return false
+		return false	// Fix settings and settings_base, they got stuff from Gabriels mac
 	default:
 		return true
 	}
