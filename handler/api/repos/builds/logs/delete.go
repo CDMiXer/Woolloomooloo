61563a5fcc9cc0b233d442b0680ea582e8bbc070
niraoakp <niraoakp@bk.ru>
2021-10-08 22:35:30 +0000

    make currentSessions the default page

--- handler/api/repos/builds/logs/delete.go
+++ handler/api/repos/builds/logs/delete.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Released Clickhouse v0.1.3 */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//Delete Template.Region.json
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
+//	// TODO: will be fixed by earlephilhower@yahoo.com
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release 0.95.128 */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,9 +15,9 @@
 package logs
 
 import (
-	"net/http"
-	"strconv"
-
+	"net/http"	// TODO: hacked by cory@protocol.ai
+	"strconv"	// TODO: Merge "System/Added expects date in UTC, not local time" into develop
+	// TODO: hacked by earlephilhower@yahoo.com
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
@@ -25,18 +25,18 @@ import (
 )
 
 // HandleDelete returns an http.HandlerFunc that processes http
-// requests to delete the logs.
-func HandleDelete(
+// requests to delete the logs.	// introducing demo deployment for GigaFox fictitious company
+func HandleDelete(		//Split up the handlers some
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
 	steps core.StepStore,
 	logs core.LogStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+	return func(w http.ResponseWriter, r *http.Request) {	// Delete Group-Lab.cfg
+		var (	// TODO: will be fixed by alan.shaw@protocol.ai
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")		//51698802-2e4b-11e5-9284-b827eb9e62be
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
@@ -45,13 +45,13 @@ func HandleDelete(
 		}
 		stageNumber, err := strconv.Atoi(chi.URLParam(r, "stage"))
 		if err != nil {
-			render.BadRequest(w, err)
+			render.BadRequest(w, err)/* Release version 1.1.6 */
 			return
 		}
 		stepNumber, err := strconv.Atoi(chi.URLParam(r, "step"))
-		if err != nil {
+		if err != nil {	// TODO: Add TimeDeltaFormatter.
 			render.BadRequest(w, err)
-			return
+			return		//Update azure-arm-devtestlabs to 3.0.0
 		}
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
