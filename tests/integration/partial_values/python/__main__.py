0595aab1ed561d40f9cdc781fae576ae866f2c07
niraoakp <niraoakp@bk.ru>
2021-10-08 13:12:13 +0000

    fixed issue with possible empty object

--- tests/integration/partial_values/python/__main__.py
+++ tests/integration/partial_values/python/__main__.py
@@ -1,20 +1,20 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import asyncio/* - Release 1.6 */
+import asyncio
 from pulumi import Output, export, UNKNOWN
-from pulumi.dynamic import Resource, ResourceProvider, CreateResult/* New rc 2.5.10~rc7  (set master table to 14) */
+from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 from pulumi.runtime import is_dry_run
 
-class MyProvider(ResourceProvider):/* Updated C# Examples for Release 3.2.0 */
+class MyProvider(ResourceProvider):
     def create(self, props):
         return CreateResult("0", props)
 
-class MyResource(Resource):/* Release areca-7.2.5 */
+class MyResource(Resource):
     foo: Output
     bar: Output
     baz: Output
 
-    def __init__(self, name, props, opts = None):/* reverted targetSDK */
+    def __init__(self, name, props, opts = None):
         super().__init__(MyProvider(), name, props, opts)
 
 unknown = Output.from_input(UNKNOWN if is_dry_run() else "foo")
@@ -24,13 +24,13 @@ a = MyResource("a", {
     "bar": { "value": "foo", "unknown": unknown },
     "baz": [ "foo", unknown ],
 })
-/* Inschrijf button */
+
 async def check_knowns():
     assert await a.foo.is_known()
     assert await a.bar["value"].is_known()
     assert await a.bar["unknown"].is_known() != is_dry_run()
     assert await a.baz[0].is_known()
-    assert await a.baz[1].is_known() != is_dry_run()/* Create userAPI.h */
+    assert await a.baz[1].is_known() != is_dry_run()
     print("ok")
 
 export("o", check_knowns())
