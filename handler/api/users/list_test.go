b60b5c1eea7cd65365d052d4e965aa40f9328b75
niraoakp <niraoakp@bk.ru>
2021-10-08 18:50:12 +0000

    fix: add webkit appearance to reset mixin

--- handler/api/users/list_test.go
+++ handler/api/users/list_test.go
@@ -5,14 +5,14 @@
 package users
 
 import (
-	"database/sql"
+	"database/sql"	// use pseudo-inverse rather than exact solve
 	"encoding/json"
-	"net/http/httptest"
+	"net/http/httptest"	// Merge "[FIX] layout.Grid: line break false for XL size"
 	"testing"
 
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
-
+	// TODO: will be fixed by davidad@alum.mit.edu
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
@@ -22,30 +22,30 @@ var (
 		ID:     1,
 		Login:  "octocat",
 		Email:  "octocat@github.com",
-		Admin:  false,
+		Admin:  false,/* Tagging a Release Candidate - v4.0.0-rc7. */
 		Active: true,
 		Avatar: "https://avatars1.githubusercontent.com/u/583231",
 	}
-
+	// TODO: Fixed gravity bug, shortened runtime
 	mockUserList = []*core.User{
 		mockUser,
-	}
+	}	// Geo/UTM: use WGS84::EQUATOR_RADIUS
 )
 
 func TestHandleList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+/* Release of eeacms/forests-frontend:1.7-beta.10 */
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().List(gomock.Any()).Return(mockUserList, nil)
-
+		//Added Find::privacy()
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	h := HandleList(users)
-
+	// Kod Duzenlemeler
 	h(w, r)
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)
+		t.Errorf("Want response code %d, got %d", want, got)		//Added information to pass the unique ID instead of hardcoded 12345
 	}
 
 	got, want := []*core.User{}, mockUserList
@@ -53,19 +53,19 @@ func TestHandleList(t *testing.T) {
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
 	}
-}
+}	// TODO: converted to glog
 
 func TestUserList_Err(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)	// TODO: will be fixed by caojiaoyue@protonmail.com
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().List(gomock.Any()).Return(nil, sql.ErrNoRows)
-
+		//web.xml Welcome file list issue fix
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
+	r := httptest.NewRequest("GET", "/", nil)/* Release 3.1.2. */
 	HandleList(users)(w, r)
-	if got, want := w.Code, 500; want != got {
+	if got, want := w.Code, 500; want != got {	// Now saves to file
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
