c210e6952556368617247a1071614cd47be02c57
niraoakp <niraoakp@bk.ru>
2021-10-08 08:21:03 +0000

    Adds 'team' optional, fixes #1

--- tests/integration/query/step1/index.ts
+++ tests/integration/query/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Maaaa Tooookeeeen */
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";/* 3.6.1 Release */
-
-// Step 1: Create a simple resource graph.
+import { Resource } from "./resource";
+		//Added method for inserting traveler
+// Step 1: Create a simple resource graph./* Release Lib-Logger to v0.7.0 [ci skip]. */
 const a = new Resource("a", { state: 1 });
-const b = new Resource("b", { state: 2, resource: a });	// TODO: will be fixed by sjors@sprovoost.nl
+const b = new Resource("b", { state: 2, resource: a });
