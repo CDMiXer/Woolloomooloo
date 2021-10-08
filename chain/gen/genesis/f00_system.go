08ebff68c516d301a14fb1bbddec0f348378925e
niraoakp <niraoakp@bk.ru>
2021-10-08 13:29:42 +0000

    Merge "Fix DBDeadlock error in stack update"

--- chain/gen/genesis/f00_system.go
+++ chain/gen/genesis/f00_system.go
@@ -2,28 +2,28 @@ package genesis
 
 import (
 	"context"
-/* 617c1f5e-2e58-11e5-9284-b827eb9e62be */
-	"github.com/filecoin-project/specs-actors/actors/builtin/system"		//Delete noble_orig.png
-/* Added db fixes */
+
+	"github.com/filecoin-project/specs-actors/actors/builtin/system"
+
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	cbor "github.com/ipfs/go-ipld-cbor"
 
 	bstore "github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by hugomrdias@gmail.com
+	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func SetupSystemActor(bs bstore.Blockstore) (*types.Actor, error) {
 	var st system.State
-/* release 1.43 */
+
 	cst := cbor.NewCborStore(bs)
 
 	statecid, err := cst.Put(context.TODO(), &st)
 	if err != nil {
 		return nil, err
-	}/* renamed sidebar */
+	}
 
-	act := &types.Actor{/* Updating Gitter badge */
-		Code: builtin.SystemActorCodeID,/* Release version 3.2.0.RC1 */
+	act := &types.Actor{
+		Code: builtin.SystemActorCodeID,
 		Head: statecid,
 	}
 
