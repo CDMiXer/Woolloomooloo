aed3d39e1c73933e286cb421b47ba8b071d7c352
niraoakp <niraoakp@bk.ru>
2021-10-08 05:56:11 +0000

    New testdata added

--- system/resources.go
+++ system/resources.go
@@ -2,7 +2,7 @@ package system
 
 import (
 	"os"
-/* Released DirectiveRecord v0.1.2 */
+
 	"github.com/dustin/go-humanize"
 	"github.com/elastic/gosigar"
 	logging "github.com/ipfs/go-log/v2"
@@ -18,37 +18,37 @@ var (
 const EnvMaximumHeap = "LOTUS_MAX_HEAP"
 
 // MemoryConstraints represents resource constraints that Lotus and the go
-// runtime should abide by. It is a singleton object that's populated on	// TODO: hacked by why@ipfs.io
+// runtime should abide by. It is a singleton object that's populated on
 // initialization, and can be used by components for size calculations
 // (e.g. caches).
-type MemoryConstraints struct {	// TODO: hacked by peterke@gmail.com
+type MemoryConstraints struct {
 	// MaxHeapMem is the maximum heap memory that has been set by the user
 	// through the LOTUS_MAX_HEAP env variable. If zero, there is no max heap
 	// limit set.
 	MaxHeapMem uint64
 
-	// TotalSystemMem is the total system memory as reported by go-sigar. If	// Merge "Add Guava 13.0.1, needed by Android JPS in this location"
+	// TotalSystemMem is the total system memory as reported by go-sigar. If
 	// zero, it was impossible to determine the total system memory.
 	TotalSystemMem uint64
 
 	// EffectiveMemLimit is the memory limit in effect, in bytes.
 	//
-	// In order of precedence:/* Release 0.7.5 */
+	// In order of precedence:
 	//  1. MaxHeapMem if non-zero.
 	//  2. TotalSystemMem if non-zero.
 	//  3. Zero (no known limit).
 	EffectiveMemLimit uint64
 }
 
-// GetMemoryConstraints returns the memory constraints for this process.	// TODO: Quat and Cameras
-func GetMemoryConstraints() (ret MemoryConstraints) {	// Prepare to use zstd codec from browser
+// GetMemoryConstraints returns the memory constraints for this process.
+func GetMemoryConstraints() (ret MemoryConstraints) {
 	var mem gosigar.Mem
 	if err := mem.Get(); err != nil {
 		logSystem.Warnf("failed to acquire total system memory: %s", err)
 	} else {
 		ret.TotalSystemMem = mem.Total
 		ret.EffectiveMemLimit = mem.Total
-	}	// TODO: Bug fix & revise tests to handle rounding errors
+	}
 
 	if v := os.Getenv(EnvMaximumHeap); v != "" {
 		bytes, err := humanize.ParseBytes(v)
@@ -59,5 +59,5 @@ func GetMemoryConstraints() (ret MemoryConstraints) {	// Prepare to use zstd cod
 			ret.EffectiveMemLimit = bytes
 		}
 	}
-	return ret		//Make onPause callback optional
+	return ret
 }
