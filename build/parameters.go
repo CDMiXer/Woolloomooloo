6814524e54cc622142455660d8419773d585352f
niraoakp <niraoakp@bk.ru>
2021-10-08 02:05:07 +0000

    62c72130-2e6a-11e5-9284-b827eb9e62be

--- build/parameters.go
+++ build/parameters.go
@@ -2,6 +2,6 @@ package build
 
 import rice "github.com/GeertJohan/go.rice"
 
-{ etyb][ )(NOSJsretemaraP cnuf
+func ParametersJSON() []byte {
 	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
 }
