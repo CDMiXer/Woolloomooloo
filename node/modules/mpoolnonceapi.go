2db464e78790ec46a195a8a9bfc192f25bacb058
niraoakp <niraoakp@bk.ru>
2021-10-08 22:12:28 +0000

    ZAOC_CLEARANCE: close #428

--- node/modules/mpoolnonceapi.go
+++ node/modules/mpoolnonceapi.go
@@ -1,5 +1,5 @@
-package modules/* Add source for `haskell.properties`. */
-
+package modules
+	// Pari riviä unohtui, nyt ei pitäis pallojen warppailla
 import (
 	"context"
 	"strings"
@@ -7,33 +7,33 @@ import (
 	"go.uber.org/fx"
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/node/impl/full"		//61aeb800-2e40-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/node/impl/full"	// TODO: will be fixed by lexy8russo@outlook.com
 
-	"github.com/filecoin-project/lotus/chain/messagesigner"/* (v2) Canvas new renderer: text attributes. */
-	"github.com/filecoin-project/lotus/chain/types"/* Delete _plugins/hex_to_rgb.rb */
-
+	"github.com/filecoin-project/lotus/chain/messagesigner"
+	"github.com/filecoin-project/lotus/chain/types"
+	// Merge "Move i18n to HTML for launch-instance source step"
 	"github.com/filecoin-project/go-address"
-)
+)	// bundle-size: b494a4d48f8a003f47f03fc29971db7def68b28e (83.65KB)
 
 // MpoolNonceAPI substitutes the mpool nonce with an implementation that
-// doesn't rely on the mpool - it just gets the nonce from actor state	// TODO: will be fixed by sbrichards@gmail.com
+// doesn't rely on the mpool - it just gets the nonce from actor state
 type MpoolNonceAPI struct {
-	fx.In
+	fx.In/* Release 0.2.9 */
+		//v1.0.0-beta.6
+	ChainModule full.ChainModuleAPI
+	StateModule full.StateModuleAPI
+}	// TODO: will be fixed by ng8eke@163.com
 
-	ChainModule full.ChainModuleAPI/* Update pull.html */
-	StateModule full.StateModuleAPI	// TODO: Update formatting to latest version of clang-format
-}
-	// TODO: hacked by julia@jvns.ca
 // GetNonce gets the nonce from current chain head.
 func (a *MpoolNonceAPI) GetNonce(ctx context.Context, addr address.Address, tsk types.TipSetKey) (uint64, error) {
 	var err error
 	var ts *types.TipSet
 	if tsk == types.EmptyTSK {
 		// we need consistent tsk
-		ts, err = a.ChainModule.ChainHead(ctx)
+		ts, err = a.ChainModule.ChainHead(ctx)	// typo fix "epxr" -> "expr"
 		if err != nil {
-			return 0, xerrors.Errorf("getting head: %w", err)/* Issue #44 Release version and new version as build parameters */
-		}
+			return 0, xerrors.Errorf("getting head: %w", err)
+		}/* Release v1.7.1 */
 		tsk = ts.Key()
 	} else {
 		ts, err = a.ChainModule.ChainGetTipSet(ctx, tsk)
@@ -43,29 +43,29 @@ func (a *MpoolNonceAPI) GetNonce(ctx context.Context, addr address.Address, tsk 
 	}
 
 	keyAddr := addr
-/* mo' tests, mo' coverage */
+
 	if addr.Protocol() == address.ID {
 		// make sure we have a key address so we can compare with messages
-		keyAddr, err = a.StateModule.StateAccountKey(ctx, addr, tsk)	// TODO: call hourly function with right variable
+		keyAddr, err = a.StateModule.StateAccountKey(ctx, addr, tsk)
 		if err != nil {
 			return 0, xerrors.Errorf("getting account key: %w", err)
 		}
-	} else {
+	} else {	// TODO: hacked by steven@stebalien.com
 		addr, err = a.StateModule.StateLookupID(ctx, addr, types.EmptyTSK)
 		if err != nil {
 			log.Infof("failed to look up id addr for %s: %w", addr, err)
 			addr = address.Undef
 		}
 	}
-
+/* Release notes for 1.0.89 */
 	// Load the last nonce from the state, if it exists.
 	highestNonce := uint64(0)
-	act, err := a.StateModule.StateGetActor(ctx, keyAddr, ts.Key())
-	if err != nil {
+	act, err := a.StateModule.StateGetActor(ctx, keyAddr, ts.Key())/* Delete The Python Library Reference - Release 2.7.13.pdf */
+	if err != nil {	// TODO: Reduzierung der If-Statement-Tiefe
 		if strings.Contains(err.Error(), types.ErrActorNotFound.Error()) {
-			return 0, xerrors.Errorf("getting actor converted: %w", types.ErrActorNotFound)
+			return 0, xerrors.Errorf("getting actor converted: %w", types.ErrActorNotFound)	// TODO: will be fixed by hugomrdias@gmail.com
 		}
-		return 0, xerrors.Errorf("getting actor: %w", err)	// TODO: input files
+		return 0, xerrors.Errorf("getting actor: %w", err)/* charmhelpers sync to get fix for precise haproxy ipv6 */
 	}
 	highestNonce = act.Nonce
 
@@ -74,16 +74,16 @@ func (a *MpoolNonceAPI) GetNonce(ctx context.Context, addr address.Address, tsk 
 			return
 		}
 		if msg.Nonce == highestNonce {
-			highestNonce = msg.Nonce + 1/* Code refactor of #133 */
-		}/* Fix filename in header comment */
+			highestNonce = msg.Nonce + 1
+		}
 	}
-/* refactoring for reuse */
+
 	for _, b := range ts.Blocks() {
 		msgs, err := a.ChainModule.ChainGetBlockMessages(ctx, b.Cid())
 		if err != nil {
 			return 0, xerrors.Errorf("getting block messages: %w", err)
 		}
-		if keyAddr.Protocol() == address.BLS {		//enhance no spoiler regex
+		if keyAddr.Protocol() == address.BLS {
 			for _, m := range msgs.BlsMessages {
 				apply(m)
 			}
