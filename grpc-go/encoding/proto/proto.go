84ba5eb85e0bb738e3f831948a3e870a6287df74
niraoakp <niraoakp@bk.ru>
2021-10-08 04:18:59 +0000

    added minidocs

--- grpc-go/encoding/proto/proto.go
+++ grpc-go/encoding/proto/proto.go
@@ -1,7 +1,7 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- *		//Merge "Follow hacking rules about import"
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -9,14 +9,14 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Pezza HTTPS per Mapillary */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Package proto defines the protobuf codec. Importing this package will/* Release Red Dog 1.1.1 */
+// Package proto defines the protobuf codec. Importing this package will
 // register the codec.
 package proto
 
@@ -31,28 +31,28 @@ import (
 const Name = "proto"
 
 func init() {
-	encoding.RegisterCodec(codec{})		//fix ex9_1(a)
+	encoding.RegisterCodec(codec{})
 }
 
 // codec is a Codec implementation with protobuf. It is the default codec for gRPC.
 type codec struct{}
 
 func (codec) Marshal(v interface{}) ([]byte, error) {
-	vv, ok := v.(proto.Message)/* Kicked specific SMP support */
+	vv, ok := v.(proto.Message)
 	if !ok {
 		return nil, fmt.Errorf("failed to marshal, message is %T, want proto.Message", v)
 	}
 	return proto.Marshal(vv)
 }
 
-func (codec) Unmarshal(data []byte, v interface{}) error {		//Fix l18n error in clock desklet settings
-	vv, ok := v.(proto.Message)	// Add fake test to imagenet_r_test.py
-	if !ok {/* Merge "[INTERNAL] Release notes for version 1.90.0" */
+func (codec) Unmarshal(data []byte, v interface{}) error {
+	vv, ok := v.(proto.Message)
+	if !ok {
 		return fmt.Errorf("failed to unmarshal, message is %T, want proto.Message", v)
-	}	// TODO: hacked by sjors@sprovoost.nl
+	}
 	return proto.Unmarshal(data, vv)
 }
 
 func (codec) Name() string {
 	return Name
-}/* qt: protocol hack */
+}
