526ad0cff3a9e0f666d0b91a0e45c1839285a19d
niraoakp <niraoakp@bk.ru>
2021-10-08 03:22:19 +0000

    iOS VoiceOver test results on H71 Example2

--- handler/api/request/context_test.go
+++ handler/api/request/context_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* FIX ActionChaing::getName() error if chain empty */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-package request
+/* remote commit */
+package request/* move setup help to tooltips */
