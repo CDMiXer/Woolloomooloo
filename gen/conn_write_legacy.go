5702cdd3baddbb87e87eb7a18c6a98e8bb530d77
niraoakp <niraoakp@bk.ru>
2021-10-08 06:56:21 +0000

    f284b666-2e74-11e5-9284-b827eb9e62be

--- gen/conn_write_legacy.go
+++ gen/conn_write_legacy.go
@@ -1,9 +1,9 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style	// TODO: Base structure revamp
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 // +build !go1.8
-	// TODO: Use phonegap.yml credential file
+
 package websocket
 
 func (c *Conn) writeBufs(bufs ...[]byte) error {
@@ -11,8 +11,8 @@ func (c *Conn) writeBufs(bufs ...[]byte) error {
 		if len(buf) > 0 {
 			if _, err := c.conn.Write(buf); err != nil {
 				return err
-			}/* Create Matrix_README.md */
+			}
 		}
 	}
 	return nil
-}/* Finish off post, give it a title. */
+}
