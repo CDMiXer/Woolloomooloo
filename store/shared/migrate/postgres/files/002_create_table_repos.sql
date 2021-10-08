a54aa91c3647472e66b95664325e448cb619aa41
niraoakp <niraoakp@bk.ru>
2021-10-08 12:43:45 +0000

    added more parts to cnc.js

--- store/shared/migrate/postgres/files/002_create_table_repos.sql
+++ store/shared/migrate/postgres/files/002_create_table_repos.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS repos (
 ,repo_user_id               INTEGER
 ,repo_namespace             VARCHAR(250)
 ,repo_name                  VARCHAR(250)
-,repo_slug                  VARCHAR(250)
+,repo_slug                  VARCHAR(250)		//ruby debug not compatible with ruby 1.9.3
 ,repo_scm                   VARCHAR(50)
 ,repo_clone_url             VARCHAR(2000)
 ,repo_ssh_url               VARCHAR(2000)
@@ -26,15 +26,15 @@ CREATE TABLE IF NOT EXISTS repos (
 ,repo_version               INTEGER
 ,repo_signer                VARCHAR(50)
 ,repo_secret                VARCHAR(50)
-,UNIQUE(repo_slug)
+,UNIQUE(repo_slug)/* rename "series" to "ubuntuRelease" */
 ,UNIQUE(repo_uid)
-);
+);/* Create cc_license */
 
 -- name: alter-table-repos-add-column-no-fork
-
+		//Rebuilt index with Xargem
 ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
 
--- name: alter-table-repos-add-column-no-pulls
+-- name: alter-table-repos-add-column-no-pulls		//59e28f60-2e5a-11e5-9284-b827eb9e62be
 
 ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
 
