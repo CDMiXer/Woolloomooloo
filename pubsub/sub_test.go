5db7f942e50a3053e2ffbcd18eac80180d365b25
niraoakp <niraoakp@bk.ru>
2021-10-08 04:14:39 +0000

    Add a couple of IO tests

--- pubsub/sub_test.go
+++ pubsub/sub_test.go
@@ -8,57 +8,57 @@ package pubsub
 
 import (
 	"testing"
-
+	// TODO: Refactor scripting support into separate class.
 	"github.com/drone/drone/core"
-)
+)/* Updating build-info/dotnet/corert/master for alpha-26007-02 */
 
-func nop(*core.Message) {}
+func nop(*core.Message) {}/* unique sanity checks for bin/virtualenv_update.sh */
 
 func TestSubscription_publish(t *testing.T) {
-	s := &subscriber{
+	s := &subscriber{		//Added textures instead of pixels... its was becoming a pain in the ass..
 		handler: make(chan *core.Message, 5),
 		quit:    make(chan struct{}),
-	}
+	}	// TODO: Removed find more btn
 
 	e := new(core.Message)
-	s.publish(e)
+	s.publish(e)/* Release XWiki 11.10.3 */
 
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
-	if got, want := <-s.handler, e; got != want {
-		t.Errorf("Want event received from channel")
+	if got, want := <-s.handler, e; got != want {	// Update Script_for_Managers.R
+		t.Errorf("Want event received from channel")	// TODO: hacked by martin2cai@hotmail.com
 	}
 	if got, want := len(s.handler), 0; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
 }
-
+		//e3681b3c-2e60-11e5-9284-b827eb9e62be
 func TestSubscription_buffer(t *testing.T) {
 	s := &subscriber{
 		handler: make(chan *core.Message, 1),
 		quit:    make(chan struct{}),
-	}
+	}	// Stub README to add install guide to
 
 	// the buffer size is 1 to simulate what happens
-	// if the subscriber cannot keep up with processing
-	// and the buffer fills up. In this case, events
+	// if the subscriber cannot keep up with processing		//Add `getVideoInfo` method alias.
+	// and the buffer fills up. In this case, events	// Create Check-ScheduledTasks
 	// should be ignored until pending events are
 	// processed.
 
 	e := new(core.Message)
 	s.publish(e)
-	s.publish(e)
+	s.publish(e)/* Upgrade rake gem */
 	s.publish(e)
 	s.publish(e)
 	s.publish(e)
 
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
-	}
+	}/* Release v1.47 */
 }
 
-func TestSubscription_stop(t *testing.T) {
+func TestSubscription_stop(t *testing.T) {/* Release 0.0.9 */
 	s := &subscriber{
 		handler: make(chan *core.Message, 1),
 		quit:    make(chan struct{}),
