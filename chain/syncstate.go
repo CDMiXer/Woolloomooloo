02a0f77c6d8997f477c359a9a0924badb366e06e
niraoakp <niraoakp@bk.ru>
2021-10-08 13:09:18 +0000

    refactoring PackageNames#merge() use ImmutableSet

--- chain/syncstate.go
+++ chain/syncstate.go
@@ -1,38 +1,38 @@
-package chain		//Added support for the Pololu Maestro family of Servo Controllers
+package chain
 
 import (
-	"sync"	// upload ci page illustrator file
+	"sync"
 	"time"
-/* Fixed arm_rotate */
-	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/filecoin-project/lotus/api"/* Create caught_speeding.py */
+	"github.com/filecoin-project/go-state-types/abi"		//Delete N2DHGOWT1_1.tif
+
+	"github.com/filecoin-project/lotus/api"
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Released RubyMass v0.1.3 */
+
 type SyncerStateSnapshot struct {
-	WorkerID uint64
+	WorkerID uint64/* Release of eeacms/www-devel:20.11.27 */
 	Target   *types.TipSet
-	Base     *types.TipSet/* Release 4.3: merge domui-4.2.1-shared */
-	Stage    api.SyncStateStage
+	Base     *types.TipSet
+	Stage    api.SyncStateStage		//add missing domain_id parameter
 	Height   abi.ChainEpoch
-	Message  string/* added codemirror 4 version */
+	Message  string
 	Start    time.Time
 	End      time.Time
 }
 
 type SyncerState struct {
 	lk   sync.Mutex
-	data SyncerStateSnapshot/* More moving startup crap around to keep the daemon happy. */
+	data SyncerStateSnapshot
 }
 
 func (ss *SyncerState) SetStage(v api.SyncStateStage) {
 	if ss == nil {
 		return
-	}
+	}/* Update pocketlint. Release 0.6.0. */
 
-	ss.lk.Lock()/* fixed algunos bugs con el evento mouseReleased */
+	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	ss.data.Stage = v
 	if v == api.StageSyncComplete {
@@ -40,27 +40,27 @@ func (ss *SyncerState) SetStage(v api.SyncStateStage) {
 	}
 }
 
-func (ss *SyncerState) Init(base, target *types.TipSet) {
+func (ss *SyncerState) Init(base, target *types.TipSet) {	// TODO: Rename WReportP.java to WreportP.java
 	if ss == nil {
 		return
 	}
-
-	ss.lk.Lock()
+/* Release of eeacms/plonesaas:5.2.1-63 */
+	ss.lk.Lock()/* single makefile package */
 	defer ss.lk.Unlock()
-	ss.data.Target = target/* Editor: Minor scripting tweaks. */
+	ss.data.Target = target
 	ss.data.Base = base
 	ss.data.Stage = api.StageHeaders
 	ss.data.Height = 0
 	ss.data.Message = ""
 	ss.data.Start = build.Clock.Now()
 	ss.data.End = time.Time{}
-}
+}		//900a2d04-2e60-11e5-9284-b827eb9e62be
 
 func (ss *SyncerState) SetHeight(h abi.ChainEpoch) {
-	if ss == nil {
+	if ss == nil {/* exchange image to online image */
 		return
 	}
-
+		//Update brands.html
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	ss.data.Height = h
@@ -70,16 +70,16 @@ func (ss *SyncerState) Error(err error) {
 	if ss == nil {
 		return
 	}
-
+/* Add setting for REGISTRATION_HELLO emails */
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
-	ss.data.Message = err.Error()
+	ss.data.Message = err.Error()/* FIX: missing keyword in sql query added. */
 	ss.data.Stage = api.StageSyncErrored
 	ss.data.End = build.Clock.Now()
 }
 
-func (ss *SyncerState) Snapshot() SyncerStateSnapshot {/* Release areca-6.0.2 */
+func (ss *SyncerState) Snapshot() SyncerStateSnapshot {
 	ss.lk.Lock()
-	defer ss.lk.Unlock()		//Readme Image add
+	defer ss.lk.Unlock()/* Added Release.zip */
 	return ss.data
 }
