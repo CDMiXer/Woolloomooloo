59a2f56f4f818ec1e19f8c15606a3bcd73636418
niraoakp <niraoakp@bk.ru>
2021-10-08 21:42:31 +0000

    Rename RepositoryPolicy.apply to acquire_repository

--- grpc-go/xds/internal/balancer/weightedtarget/logging.go
+++ grpc-go/xds/internal/balancer/weightedtarget/logging.go
@@ -2,33 +2,33 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Merge "[FIX] sap.ui.layout.form.GridLayout: wrong tab sequence in RTL" */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software/* #167 - Release version 0.11.0.RELEASE. */
- * distributed under the License is distributed on an "AS IS" BASIS,
+ *     http://www.apache.org/licenses/LICENSE-2.0/* ToolStatus: Reduced startup allocations; Added consistency checking; */
+ *	// Delete MLP.cpp
+ * Unless required by applicable law or agreed to in writing, software		//add Windows Server Backup age examine plugin
+ * distributed under the License is distributed on an "AS IS" BASIS,	// updated to properly position the activity indicator before the text
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release 1.20.1 */
+ *
  */
 
 package weightedtarget
-
+	// TODO: Deprecated MaduraForm an associated cleanup
 import (
-	"fmt"		//Limit stack traces & print srr0/1 on ppc32 fatal.
+	"fmt"
 
 	"google.golang.org/grpc/grpclog"
-	internalgrpclog "google.golang.org/grpc/internal/grpclog"		//Score now decrements by one every half second
-)	// TODO: Added formula classes for CSL
+	internalgrpclog "google.golang.org/grpc/internal/grpclog"
+)
 
 const prefix = "[weighted-target-lb %p] "
 
 var logger = grpclog.Component("xds")
-/* Bug 1357: Fixed bug in computation due to small type-o */
+
 func prefixLogger(p *weightedTargetBalancer) *internalgrpclog.PrefixLogger {
-	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))/* Add cisimple build status */
-}		//REFACTORING div main
+	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
+}
