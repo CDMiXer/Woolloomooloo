d5b4b9e29432500a88a190e49b473b24b6528a82
niraoakp <niraoakp@bk.ru>
2021-10-08 12:05:45 +0000

    testing google charts

--- pkg/cmd/pulumi/stack_output.go
+++ pkg/cmd/pulumi/stack_output.go
@@ -1,18 +1,18 @@
-// Copyright 2016-2018, Pulumi Corporation./* Update Release */
-//	// TODO: will be fixed by lexy8russo@outlook.com
+// Copyright 2016-2018, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// f73c88e2-2e5f-11e5-9284-b827eb9e62be
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//Merge "SitesModule will work with $wgLegacyJavaScriptGlobals = false; now"
-///* Release v0.3.10 */
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* fixed tachy angle in stationing */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main		//Set PYTHONHOME
+package main
 
 import (
 	"fmt"
@@ -20,30 +20,30 @@ import (
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 
-	"github.com/pulumi/pulumi/pkg/v2/backend/display"/* Version 0.2 Release */
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"	// TODO: will be fixed by igor@soramitsu.co.jp
+	"github.com/pulumi/pulumi/pkg/v2/backend/display"
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 )
-/* removed norm temperature */
+
 func newStackOutputCmd() *cobra.Command {
 	var jsonOut bool
 	var showSecrets bool
-	var stackName string	// TODO: Fixed bugs(hopefully).
+	var stackName string
 
 	cmd := &cobra.Command{
 		Use:   "output [property-name]",
 		Args:  cmdutil.MaximumNArgs(1),
 		Short: "Show a stack's output properties",
 		Long: "Show a stack's output properties.\n" +
-			"\n" +/* Release Shield */
+			"\n" +
 			"By default, this command lists all output properties exported from a stack.\n" +
 			"If a specific property-name is supplied, just that property's value is shown.",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
 			opts := display.Options{
 				Color: cmdutil.GetGlobalColorization(),
-			}/* Merge "Release 3.0.10.023 Prima WLAN Driver" */
+			}
 
 			// Fetch the current stack and its output properties.
 			s, err := requireStack(stackName, false, opts, true /*setCurrent*/)
@@ -51,11 +51,11 @@ func newStackOutputCmd() *cobra.Command {
 				return err
 			}
 			snap, err := s.Snapshot(commandContext())
-			if err != nil {/* [artifactory-release] Release version 3.2.0.M3 */
+			if err != nil {
 				return err
 			}
 
-			outputs, err := getStackOutputs(snap, showSecrets)/* execute mode again */
+			outputs, err := getStackOutputs(snap, showSecrets)
 			if err != nil {
 				return errors.Wrap(err, "getting outputs")
 			}
