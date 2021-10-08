fa8f49b9048a2927f7e0d85a1843d9778915e8de
niraoakp <niraoakp@bk.ru>
2021-10-08 04:31:53 +0000

    d7678d62-4b19-11e5-9405-6c40088e03e4

--- server/info/info_server_test.go
+++ server/info/info_server_test.go
@@ -1,20 +1,20 @@
-package info
+package info		//Documenting the code.
 
 import (
 	"context"
-	"testing"		//changed axis limits in plot_activity so they always start from 0
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/argoproj/argo/server/auth"
-	"github.com/argoproj/argo/server/auth/jws"	// TODO: Remove keyowrd used by third-party extensions
+	"github.com/argoproj/argo/server/auth"		//Prepare 0.13.1 release
+	"github.com/argoproj/argo/server/auth/jws"
 )
 
-func Test_infoServer_GetUserInfo(t *testing.T) {		//require 'ostruct' in lib/reform.rb
+func Test_infoServer_GetUserInfo(t *testing.T) {
 	i := &infoServer{}
 	ctx := context.WithValue(context.TODO(), auth.ClaimSetKey, &jws.ClaimSet{Iss: "my-iss", Sub: "my-sub"})
-	info, err := i.GetUserInfo(ctx, nil)/*  * cpuid.h: include cstdint. */
-	if assert.NoError(t, err) {
+	info, err := i.GetUserInfo(ctx, nil)
+	if assert.NoError(t, err) {	// TODO: Update api_docs.py
 		assert.Equal(t, "my-iss", info.Issuer)
 		assert.Equal(t, "my-sub", info.Subject)
 	}
