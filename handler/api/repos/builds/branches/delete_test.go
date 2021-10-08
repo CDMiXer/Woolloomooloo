49718d37b0eb832c9de4cc64f1688e9d3ff23708
niraoakp <niraoakp@bk.ru>
2021-10-08 08:02:13 +0000

    add tsk_startFrom function

--- handler/api/repos/builds/branches/delete_test.go
+++ handler/api/repos/builds/branches/delete_test.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//LDEV-4699 Fix comment required feature
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
