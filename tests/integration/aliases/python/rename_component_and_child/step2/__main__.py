e74d47277dc764a8c7482468d3f579f23aee7e20
niraoakp <niraoakp@bk.ru>
2021-10-08 05:25:00 +0000

    Merge branch 'master' into pf-dev391

--- tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
+++ tests/integration/aliases/python/rename_component_and_child/step2/__main__.py
@@ -1,10 +1,10 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Update composer.json to autoload correctly
+
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-/* Create teapot.js */
+
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):		//Merge "Fix synthetic calls in versionedparcelable module" into pi-androidx-dev
-        super().__init__("my:module:Resource", name, None, opts)/* Release of eeacms/forests-frontend:1.8-beta.11 */
+    def __init__(self, name, opts=None):
+        super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #5 - composing #1 and #3 and making both changes at the same time
 class ComponentFive(ComponentResource):
@@ -12,7 +12,7 @@ class ComponentFive(ComponentResource):
         super().__init__("my:module:ComponentFive", name, None, opts)
         res1 = Resource1("otherchildrenamed", ResourceOptions(
             parent=self,
-            aliases=[Alias(name="otherchild", parent=self)]))	// TODO: Support multiple BAM files in Pileup records;handle basequal internally
+            aliases=[Alias(name="otherchild", parent=self)]))
 
-comp5 = ComponentFive("newcomp5", ResourceOptions(		//new style cm XML
+comp5 = ComponentFive("newcomp5", ResourceOptions(
     aliases=[Alias(name="comp5")]))
