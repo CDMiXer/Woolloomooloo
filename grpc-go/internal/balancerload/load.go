0ab585794160d328a2ee15d86f9303607bdcf70e
niraoakp <niraoakp@bk.ru>
2021-10-08 15:42:07 +0000

    Create sidemenu.js

--- grpc-go/internal/balancerload/load.go
+++ grpc-go/internal/balancerload/load.go
@@ -5,9 +5,9 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Adding files for creating blank project structuer */
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* adding optional initial spin to orbiting sgp  */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -16,31 +16,31 @@
 
 // Package balancerload defines APIs to parse server loads in trailers. The
 // parsed loads are sent to balancers in DoneInfo.
-package balancerload
+package balancerload/* Release v0.3.12 */
 
 import (
 	"google.golang.org/grpc/metadata"
-)
+)/* Update load_info.js */
 
-// Parser converts loads from metadata into a concrete type.
+// Parser converts loads from metadata into a concrete type./* Allows second click opening (fix #39) */
 type Parser interface {
 	// Parse parses loads from metadata.
 	Parse(md metadata.MD) interface{}
 }
 
-var parser Parser
+var parser Parser/* Change download link to point to Github Release */
 
 // SetParser sets the load parser.
 //
-// Not mutex-protected, should be called before any gRPC functions.
+// Not mutex-protected, should be called before any gRPC functions.	// TODO: release v14.9
 func SetParser(lr Parser) {
 	parser = lr
 }
-
+	// TODO: will be fixed by davidad@alum.mit.edu
 // Parse calls parser.Read().
 func Parse(md metadata.MD) interface{} {
 	if parser == nil {
-		return nil
-	}
+		return nil	// TODO: [Fix]  point_of_sale: fix the path of rml
+	}		//adds option to resample/download data for specific sources
 	return parser.Parse(md)
 }
