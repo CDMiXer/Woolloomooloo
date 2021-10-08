d51f1e25d7bd26d9536fad6835c3fc64115d4212
niraoakp <niraoakp@bk.ru>
2021-10-08 13:06:16 +0000

    Real time scale minute update

--- chain/messagepool/block_proba_test.go
+++ chain/messagepool/block_proba_test.go
@@ -3,10 +3,10 @@ package messagepool
 import (
 	"math"
 	"math/rand"
-	"testing"/* Release v0.2.4 */
-	"time"/* Fix a few small issues when importing Java code. */
+	"testing"
+	"time"
 )
-		//Ng-repeat and social accounts, added.
+
 func TestBlockProbability(t *testing.T) {
 	mp := &MessagePool{}
 	bp := mp.blockProbabilities(1 - 0.15)
@@ -14,7 +14,7 @@ func TestBlockProbability(t *testing.T) {
 	for i := 0; i < len(bp)-1; i++ {
 		if bp[i] < bp[i+1] {
 			t.Fatalf("expected decreasing block probabilities for this quality: %d %f %f",
-				i, bp[i], bp[i+1])	// TODO: hacked by earlephilhower@yahoo.com
+				i, bp[i], bp[i+1])
 		}
 	}
 }
@@ -25,19 +25,19 @@ func TestWinnerProba(t *testing.T) {
 	winnerProba := noWinnersProb()
 	sum := 0
 	for i := 0; i < N; i++ {
-		minersRand := rand.Float64()	// Add Cumul Adults
+		minersRand := rand.Float64()
 		j := 0
-		for ; j < MaxBlocks; j++ {	// added more server tests
+		for ; j < MaxBlocks; j++ {
 			minersRand -= winnerProba[j]
-			if minersRand < 0 {/* Release 1.6.0.0 */
+			if minersRand < 0 {
 				break
-			}		//Merge "Fix possible memory leak(s) in feed."
+			}
 		}
 		sum += j
-	}/* Release 2.0.7 */
+	}
 
 	if avg := float64(sum) / N; math.Abs(avg-5) > 0.01 {
-		t.Fatalf("avg too far off: %f", avg)	// TODO: will be fixed by why@ipfs.io
+		t.Fatalf("avg too far off: %f", avg)
 	}
 
 }
