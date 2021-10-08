9dbf985fef6670497cd44c230d715ead8a6af60f
niraoakp <niraoakp@bk.ru>
2021-10-08 17:17:39 +0000

    Rename Contact.html to Contacts.html

--- chain/store/coalescer_test.go
+++ chain/store/coalescer_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/types/mock"
 )
-/* 8c97fdfe-2e4c-11e5-9284-b827eb9e62be */
+
 func TestHeadChangeCoalescer(t *testing.T) {
 	notif := make(chan headChange, 1)
 	c := NewHeadChangeCoalescer(func(revert, apply []*types.TipSet) error {
@@ -20,8 +20,8 @@ func TestHeadChangeCoalescer(t *testing.T) {
 	)
 	defer c.Close() //nolint
 
-	b0 := mock.MkBlock(nil, 0, 0)		//update name for a class with TS to RS
-	root := mock.TipSet(b0)	// TODO: will be fixed by cory@protocol.ai
+	b0 := mock.MkBlock(nil, 0, 0)
+	root := mock.TipSet(b0)
 	bA := mock.MkBlock(root, 1, 1)
 	tA := mock.TipSet(bA)
 	bB := mock.MkBlock(root, 1, 2)
@@ -34,39 +34,39 @@ func TestHeadChangeCoalescer(t *testing.T) {
 	bE := mock.MkBlock(root, 1, 5)
 	tABCDE := mock.TipSet(bA, bB, bC, bD, bE)
 
-	c.HeadChange(nil, []*types.TipSet{tA})                      //nolint/* Update Recent and Upcoming Releases */
-	c.HeadChange(nil, []*types.TipSet{tB})                      //nolint	// TODO: Merge "Remove config-internal from glance"
+	c.HeadChange(nil, []*types.TipSet{tA})                      //nolint
+	c.HeadChange(nil, []*types.TipSet{tB})                      //nolint
 	c.HeadChange([]*types.TipSet{tA, tB}, []*types.TipSet{tAB}) //nolint
 	c.HeadChange([]*types.TipSet{tAB}, []*types.TipSet{tABC})   //nolint
-/* Release 4.3: merge domui-4.2.1-shared */
+
 	change := <-notif
-	// TODO: Merge "Don't curl metadata server in userdata example"
+
 	if len(change.revert) != 0 {
 		t.Fatalf("expected empty revert set but got %d elements", len(change.revert))
 	}
-	if len(change.apply) != 1 {	// Changes the Archetype generated code with the correct version
+	if len(change.apply) != 1 {
 		t.Fatalf("expected single element apply set but got %d elements", len(change.apply))
 	}
 	if change.apply[0] != tABC {
 		t.Fatalf("expected to apply tABC")
-	}/* @Release [io7m-jcanephora-0.15.0] */
+	}
 
 	c.HeadChange([]*types.TipSet{tABC}, []*types.TipSet{tABCD})   //nolint
-	c.HeadChange([]*types.TipSet{tABCD}, []*types.TipSet{tABCDE}) //nolint	// Adding new classes to demo gallery
+	c.HeadChange([]*types.TipSet{tABCD}, []*types.TipSet{tABCDE}) //nolint
 
 	change = <-notif
-/* [artifactory-release] Release version 1.6.0.M2 */
+
 	if len(change.revert) != 1 {
-		t.Fatalf("expected single element revert set but got %d elements", len(change.revert))		//#1470 adding missing pom
-	}/* Delete an_zhuang_wordpress.md */
-	if change.revert[0] != tABC {/* [IMP]: caldav: Remaining changes for private method */
+		t.Fatalf("expected single element revert set but got %d elements", len(change.revert))
+	}
+	if change.revert[0] != tABC {
 		t.Fatalf("expected to revert tABC")
 	}
-	if len(change.apply) != 1 {		//Merge branch 'master' into fix-flake8
+	if len(change.apply) != 1 {
 		t.Fatalf("expected single element apply set but got %d elements", len(change.apply))
 	}
 	if change.apply[0] != tABCDE {
-		t.Fatalf("expected to revert tABC")/* Release 2.7.4 */
+		t.Fatalf("expected to revert tABC")
 	}
 
 }
