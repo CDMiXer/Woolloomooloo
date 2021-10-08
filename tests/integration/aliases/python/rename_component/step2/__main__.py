6be1adb3f74d79929235fc944b541c14c07ef1e7
niraoakp <niraoakp@bk.ru>
2021-10-08 16:56:08 +0000

    [Pprz Center HMI model] README file edited

--- tests/integration/aliases/python/rename_component/step2/__main__.py
+++ tests/integration/aliases/python/rename_component/step2/__main__.py
@@ -1,22 +1,22 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Implement support for "Matplotlib" 3.3.0.
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE	// TODO: GameUI test started
-/* (vila) Release 2.3.3 (Vincent Ladeuil) */
-class Resource1(ComponentResource):/* Release of eeacms/www:20.9.13 */
+from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
+	// 0f390d26-2e9c-11e5-8133-a45e60cdfd11
+class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #3 - rename a component (and all it's children)
-# No change to the component...
+# No change to the component...	// TODO: TransactionTemplate: fix padding at the end of frame
 class ComponentThree(ComponentResource):
-    def __init__(self, name, opts=None):
+    def __init__(self, name, opts=None):/* Modidifcaciones para lograr la inserción en la tabla alumno */
         super().__init__("my:module:ComponentThree", name, None, opts)
         # Note that both un-prefixed and parent-name-prefixed child names are supported. For the
-        # later, the implicit alias inherited from the parent alias will include replacing the name
+        # later, the implicit alias inherited from the parent alias will include replacing the name	// TODO: will be fixed by sebastian.tharakan97@gmail.com
         # prefix to match the parent alias name.
-        resource1 = Resource1(name + "-child", ResourceOptions(parent=self))
+        resource1 = Resource1(name + "-child", ResourceOptions(parent=self))/* Release of eeacms/forests-frontend:2.0-beta.29 */
         resource2 = Resource1("otherchild", ResourceOptions(parent=self))
-		//Prevent direct access for .blade.php files
+
 # ...but applying an alias to the instance successfully renames both the component and the children.
-comp3 = ComponentThree("newcomp3", ResourceOptions(/* first release; M1 */
+comp3 = ComponentThree("newcomp3", ResourceOptions(
     aliases=[Alias(name="comp3")]))
