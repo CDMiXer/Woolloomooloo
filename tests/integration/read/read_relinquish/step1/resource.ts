1d9a7331f686049f5c2fbe9c1372ce502e00974c
niraoakp <niraoakp@bk.ru>
2021-10-08 03:23:40 +0000

    use ActiveSnapByName,FindSnapsByName names as suggested by Sergio

--- tests/integration/read/read_relinquish/step1/resource.ts
+++ tests/integration/read/read_relinquish/step1/resource.ts
@@ -7,20 +7,20 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by nick@perfectabstractions.com
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Merge branch 'master' into chain-handler
+
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
-		//Merge "Remove mox in libvirt/test_driver.py (1)"
+
     private id: number = 0;
 
-    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {	// TODO: hacked by hello@brooklynzelenka.com
+    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
             inputs: news,
         }
@@ -31,22 +31,22 @@ export class Provider implements dynamic.ResourceProvider {
             return {
                 changes: true,
                 replaces: ["state"],
-            };/* 2e037620-2e4b-11e5-9284-b827eb9e62be */
-        }	// TODO: will be fixed by nagydani@epointsystem.org
+            };
+        }
 
-        return {/* Merge branch 'master' into support-exclamation-mark-comment */
+        return {
             changes: false,
         }
     }
-/* Released reLexer.js v0.1.0 */
-    public async create(inputs: any): Promise<dynamic.CreateResult> {/* Merge "Release 3.0.10.054 Prima WLAN Driver" */
-        return {/* Create ReleaseCandidate_ReleaseNotes.md */
+
+    public async create(inputs: any): Promise<dynamic.CreateResult> {
+        return {
             id: (this.id++).toString(),
             outs: inputs,
         }
-    }/* Release Version 0.2.1 */
+    }
 
-    public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {/* verbose option in compiler */
+    public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
         throw Error("this resource is replace-only and can't be updated");
     }
 
@@ -54,12 +54,12 @@ export class Provider implements dynamic.ResourceProvider {
         return {
             id: id,
             props: props,
-        }/* Add missing ; and bump version */
+        }
     }
 }
-/* Added env var for db name */
+
 export class Resource extends pulumi.dynamic.Resource {
-    public readonly state: pulumi.Output<any>;	// TODO: Began work on point cloud playback
+    public readonly state: pulumi.Output<any>;
 
     constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
