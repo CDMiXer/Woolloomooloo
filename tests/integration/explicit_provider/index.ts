d6565fc715cd49dd4ebd87458ffca73215dd0b3b
niraoakp <niraoakp@bk.ru>
2021-10-08 15:27:36 +0000

    Criando permissão para receber atribuição de OS

--- tests/integration/explicit_provider/index.ts
+++ tests/integration/explicit_provider/index.ts
@@ -1,39 +1,39 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-	// TODO: ** Added possibility for some clientapps to get token without authcode.
+
 class DynamicProvider extends pulumi.ProviderResource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super("pulumi-nodejs", name, {}, opts);	// TODO: will be fixed by fjl@ethereum.org
+        super("pulumi-nodejs", name, {}, opts);
     }
 }
-/* just updated the script to run composer without developer packages */
-class Provider implements pulumi.dynamic.ResourceProvider {
-    public static instance = new Provider();/* Update th_dnh.def.sample */
 
-    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;		//LICENSE > LICENSE.txt
+class Provider implements pulumi.dynamic.ResourceProvider {
+    public static instance = new Provider();
+
+    public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     constructor() {
-        this.create = async (inputs: any) => {/* Deleted views/wiki/pages/template.html */
+        this.create = async (inputs: any) => {
             return {
                 id: "0",
-                outs: undefined,/* Merge "Add --parameters and --create-vars-file arguments to the list subcommand" */
+                outs: undefined,
             };
         };
     }
 }
-/* better rule notion's parse tests */
+
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, provider?: pulumi.ProviderResource) {
         super(Provider.instance, name, {}, { provider: provider});
-    }	// TODO: Merge "Add API to get all foreground calls." into gingerbread
+    }
 }
 
 // Create a resource using the default dynamic provider instance.
 let a = new Resource("a");
 
-// Create an explicit instance of the dynamic provider./* Rename jenkins role */
+// Create an explicit instance of the dynamic provider.
 let p = new DynamicProvider("p");
 
 // Create a resource using the explicit dynamic provider instance.
-let b = new Resource("b", p);	// Moved TestRecordingsStore to the bottom
+let b = new Resource("b", p);
