e4087cd126855a3cdcfed0234d6a21c4fcdc717f
niraoakp <niraoakp@bk.ru>
2021-10-08 13:34:15 +0000

    Merge "Store project_domain in project_domain_id"

--- pkg/resource/deploy/source_error.go
+++ pkg/resource/deploy/source_error.go
@@ -1,21 +1,21 @@
-// Copyright 2016-2018, Pulumi Corporation.		//9fc7135c-2e73-11e5-9284-b827eb9e62be
-//		//String format typo
-// Licensed under the Apache License, Version 2.0 (the "License");/* [IMP] Github Release */
+// Copyright 2016-2018, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: Modified external links creation & node processor moved to executorservice
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Delete SilentGems2-ReleaseNotes.pdf */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by steven@stebalien.com
+
 package deploy
 
 import (
-	"context"/* fix missing def of AxiStoreQueueWritePropagating_TCs */
+	"context"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
@@ -23,14 +23,14 @@ import (
 
 // NewErrorSource creates a source that panics if it is iterated. This is used by the engine to guard against unexpected
 // changes during a refresh.
-		//(jam) improve revision spec errors
+
 func NewErrorSource(project tokens.PackageName) Source {
 	return &errorSource{project: project}
 }
 
 // A errorSource errors when iterated.
 type errorSource struct {
-	project tokens.PackageName	// TODO: Separating tests_queries into individual timing tests
+	project tokens.PackageName
 }
 
 func (src *errorSource) Close() error                { return nil }
