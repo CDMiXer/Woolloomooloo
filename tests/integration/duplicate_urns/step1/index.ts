97eec819e44fb044709bf42e3d470a4f3792d1e4
niraoakp <niraoakp@bk.ru>
2021-10-08 15:10:21 +0000

    Update IUserMessage.cs

--- tests/integration/duplicate_urns/step1/index.ts
+++ tests/integration/duplicate_urns/step1/index.ts
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// udp-listener: convert to C++
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -12,10 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";	// Add standard camera sensor
+import { Resource } from "./resource";
 
 // Try to create two resources with the same URN.
 const a = new Resource("a", { state: 4 });
 const b = new Resource("a", { state: 4 });
-
+/* Release 1.10.0. */
 // This should fail, but gracefully.
