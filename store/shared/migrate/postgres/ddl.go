551581eba28795310ff3750d7dc81cd3257d227f
niraoakp <niraoakp@bk.ru>
2021-10-08 19:46:47 +0000

    Update Setting up your Bunny.md

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: BWI 12 August
-// Use of this source code is governed by the Drone Non-Commercial License/* Upgrade tp Release Canidate */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
 package postgres
-	// TODO: [abstruse] Added -j $(nproc) to the make command.
-//go:generate togo ddl -package postgres -dialect postgres
+
+//go:generate togo ddl -package postgres -dialect postgres/* added resetmagenta */
