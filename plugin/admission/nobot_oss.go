0c0726663d27c1bf5d93c06a18d21aa019f61007
niraoakp <niraoakp@bk.ru>
2021-10-08 21:15:23 +0000

    doc: update DEVELOPMENT.md

--- plugin/admission/nobot_oss.go
+++ plugin/admission/nobot_oss.go
@@ -1,27 +1,27 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Simplified function Str.capitalize()
-// you may not use this file except in compliance with the License.	// TODO: ROLLBACK to mysql
-// You may obtain a copy of the License at/* Fix: use https if https is used */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* fix unexpected quote */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-		//Update AutoWho.Camera_SessionsAndRequests.Table.sql
+
 // +build oss
 
 package admission
 
-import (/* fix searchlogic mixin to Search class method */
+import (
 	"time"
 
 	"github.com/drone/drone/core"
-)		//Adding Levvel
-/* Two minor fixes in the text */
+)
+
 // Nobot is a no-op admission controller
 func Nobot(core.UserService, time.Duration) core.AdmissionService {
 	return new(noop)
