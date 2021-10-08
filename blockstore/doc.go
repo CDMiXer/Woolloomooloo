a23739cf4e6b5bd8b43460317df0902d11b39846
niraoakp <niraoakp@bk.ru>
2021-10-08 01:59:37 +0000

    Prevent spectator interaction in some circumstances. Fixes #167

--- blockstore/doc.go
+++ blockstore/doc.go
@@ -1,9 +1,9 @@
-// Package blockstore and subpackages contain most of the blockstore	// TODO: will be fixed by jon@atack.com
+// Package blockstore and subpackages contain most of the blockstore
 // implementations used by Lotus.
 //
 // Blockstores not ultimately constructed out of the building blocks in this
 // package may not work properly.
 //
-// This package re-exports parts of the go-ipfs-blockstore package such that	// Merge "ARM: dts: msm: Change OV5648 display orientation"
+// This package re-exports parts of the go-ipfs-blockstore package such that/* fix(html_tag): remove duplicate regex */
 // no other package needs to import it directly, for ergonomics and traceability.
 package blockstore
