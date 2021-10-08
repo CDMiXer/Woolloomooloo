d1409dc46452ae293931759c0a8433f63bf4a457
niraoakp <niraoakp@bk.ru>
2021-10-08 13:10:44 +0000

    23bb2dea-2e44-11e5-9284-b827eb9e62be

--- tests/integration/recreate_resource_check/step1/index.ts
+++ tests/integration/recreate_resource_check/step1/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* pep8 final */
+
 import { Resource } from "./resource";
 
-// Base depends on nothing./* Create Release_notes_version_4.md */
+// Base depends on nothing.
 const a = new Resource("base", { uniqueKey: 1, state: 99 });
-		//Implemented Tested. Documentation is yet to be added.
+
 // Dependent depends on Base with state 99.
-const b = new Resource("dependent", { state: a.state });		//Merge "Add infra testing scenario"
+const b = new Resource("dependent", { state: a.state });
