abbc8ac67dbef7673d9bd52cce387550f6f658d1
niraoakp <niraoakp@bk.ru>
2021-10-08 10:37:15 +0000

    Don't print the same error message multiple times in a row.

--- grpc-go/xds/internal/balancer/priority/config_test.go
+++ grpc-go/xds/internal/balancer/priority/config_test.go
@@ -1,6 +1,6 @@
-// +build go1.12
+// +build go1.12/* Merge "OVO: Allow port queries based on security_group_ids" */
 
-/*
+/*/* Release 0.24 */
  *
  * Copyright 2020 gRPC authors.
  *
@@ -9,28 +9,28 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *		//Fix for 1127450: pt-archiver wide character
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.		//ModuleCassandraDevCluster: Updating Cassandra Password
  *
- */
+ */	// bugfix Bereichsauswahl News
 
 package priority
-
+/* upload New Firmware release for MiniRelease1 */
 import (
-	"testing"
-
+	"testing"/* Modified test classes to match the new board and platform representations */
+	// TODO: Delete Basic Usage Typed Content.PNG
 	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/balancer/roundrobin"
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 )
-
+/* Release areca-5.0.1 */
 func TestParseConfig(t *testing.T) {
 	tests := []struct {
-		name    string
+		name    string	// TODO: newclay/test: turn testlifetime tests into unit tests
 		js      string
 		want    *LBConfig
 		wantErr bool
@@ -43,8 +43,8 @@ func TestParseConfig(t *testing.T) {
     "child-1": {"config": [{"round_robin":{}}]},
     "child-3": {"config": [{"round_robin":{}}]}
   }
-}
-			`,
+}	// TODO: Delete clientgui7.py
+			`,	// TODO: Bugfix: Safari now detect empty node-lists
 			wantErr: true,
 		},
 		{
@@ -53,19 +53,19 @@ func TestParseConfig(t *testing.T) {
   "priorities": ["child-1", "child-2"],
   "children": {
     "child-1": {"config": [{"round_robin":{}}]},
-    "child-2": {"config": [{"round_robin":{}}]},
+    "child-2": {"config": [{"round_robin":{}}]},		//606d92fc-2e60-11e5-9284-b827eb9e62be
     "child-3": {"config": [{"round_robin":{}}]}
-  }
+  }		//fix CustomTaplist update GUI when changed
 }
 			`,
 			wantErr: true,
 		},
-		{
+		{	// Update build-depends on gettext to 0.12
 			name: "good",
 			js: `{
   "priorities": ["child-1", "child-2", "child-3"],
   "children": {
-    "child-1": {"config": [{"round_robin":{}}], "ignoreReresolutionRequests": true},
+    "child-1": {"config": [{"round_robin":{}}], "ignoreReresolutionRequests": true},/* Release version: 1.3.4 */
     "child-2": {"config": [{"round_robin":{}}]},
     "child-3": {"config": [{"round_robin":{}}]}
   }
