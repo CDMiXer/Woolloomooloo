221ebf9edb6021c9b833d5d373da57ae0273262c
niraoakp <niraoakp@bk.ru>
2021-10-08 07:49:16 +0000

    Delete pcb_drill.txt

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.ts
@@ -1,14 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
+import * as aws from "@pulumi/aws";/* Merge "Release 3.0.10.041 Prima WLAN Driver" */
 import * from "fs";
 
-// Create a bucket and expose a website index document
+// Create a bucket and expose a website index document	// Remove old file filter before adding a new one
 const siteBucket = new aws.s3.Bucket("siteBucket", {website: {
-    indexDocument: "index.html",
+    indexDocument: "index.html",/* Update UptimeClient.h */
 }});
 const siteDir = "www";
 // For each file in the directory, create an S3 object stored in `siteBucket`
-const files: aws.s3.BucketObject[];
+const files: aws.s3.BucketObject[];/* Don't allow setting values to be objects */
 for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {
     files.push(new aws.s3.BucketObject(`files-${range.key}`, {
         bucket: siteBucket.id,
@@ -17,16 +17,16 @@ for (const range of fs.readDirSync(siteDir).map((k, v) => {key: k, value: v})) {
         contentType: (() => throw new Error("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))(),
     }));
 }
-// set the MIME type of the file
+// set the MIME type of the file		//Create tache5.tex
 // Set the access policy for the bucket so all objects are readable
 const bucketPolicy = new aws.s3.BucketPolicy("bucketPolicy", {
-    bucket: siteBucket.id,
-    policy: siteBucket.id.apply(id => JSON.stringify({
+    bucket: siteBucket.id,		//AI - Add capital value for amphib attacks
+    policy: siteBucket.id.apply(id => JSON.stringify({/* Update DoOpticalFlare.java */
         Version: "2012-10-17",
-        Statement: [{
+        Statement: [{		//Fix mathjax issue.
             Effect: "Allow",
             Principal: "*",
-            Action: ["s3:GetObject"],
+            Action: ["s3:GetObject"],	// Update cs_npc.cpp
             Resource: [`arn:aws:s3:::${id}/*`],
         }],
     })),
