110da1b41dbba71cf4e44ce5110f544a986d8da2
niraoakp <niraoakp@bk.ru>
2021-10-08 02:50:20 +0000

    PageController first test fixed

--- store/shared/migrate/sqlite/files/009_create_table_secrets.sql
+++ store/shared/migrate/sqlite/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
-		//Merge branch 'master' into npm5
+
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
-,secret_repo_id           INTEGER
-,secret_name              TEXT	// TODO: hacked by magik6k@gmail.com
-,secret_data              BLOB/* add ADC port defines in NanoRelease1.h, this pin is used to pull the Key pin */
+,secret_repo_id           INTEGER	// TODO: hacked by lexy8russo@outlook.com
+,secret_name              TEXT
+,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN
+,secret_pull_request_push BOOLEAN/* Update Basic Time Calculator.md */
 ,UNIQUE(secret_repo_id, secret_name)
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-);	// Add support to check specific mobile req headers
-	// TODO: will be fixed by jon@atack.com
--- name: create-index-secrets-repo
+;)
+
+-- name: create-index-secrets-repo	// TODO: remove id from language string
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
+/* Update documentation for resettable changes */
+-- name: create-index-secrets-repo-name	// TODO: Exclude the tar from the zip
 
--- name: create-index-secrets-repo-name
-/* Update package.json to 1.4.4 */
-CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);		//Update numba from 0.31.0 to 0.32.0
+CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
