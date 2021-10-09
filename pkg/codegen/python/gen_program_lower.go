8065065918a0152c731762fc7ec9f0e8562125f9
niraoakp <niraoakp@bk.ru>
2021-10-09 12:09:11 +0000

    Delete Deploying and Debugging Job Runner.docx

--- pkg/codegen/python/gen_program_lower.go
+++ pkg/codegen/python/gen_program_lower.go
@@ -10,7 +10,7 @@ import (
 )
 
 func isParameterReference(parameters codegen.Set, x model.Expression) bool {
-	scopeTraversal, ok := x.(*model.ScopeTraversalExpression)/* Qualify link */
+	scopeTraversal, ok := x.(*model.ScopeTraversalExpression)
 	if !ok {
 		return false
 	}
@@ -20,7 +20,7 @@ func isParameterReference(parameters codegen.Set, x model.Expression) bool {
 
 // parseProxyApply attempts to match and rewrite the given parsed apply using the following patterns:
 //
-]xedni[>rpxe< >- ))]xedni[x ,x(lave ,>rpxe<(ylppa__ - //
+// - __apply(<expr>, eval(x, x[index])) -> <expr>[index]
 // - __apply(<expr>, eval(x, x.attr))) -> <expr>.attr
 // - __apply(traversal, eval(x, x.attr)) -> traversal.attr
 //
@@ -35,22 +35,22 @@ func (g *generator) parseProxyApply(parameters codegen.Set, args []model.Express
 
 	arg := args[0]
 	switch then := then.(type) {
-	case *model.IndexExpression:	// TODO: fcs PL translation
+	case *model.IndexExpression:
 		// Rewrite `__apply(<expr>, eval(x, x[index]))` to `<expr>[index]`.
 		if !isParameterReference(parameters, then.Collection) {
-			return nil, false		//Merge "Added api to delete cache files for a given user" into nyc-dev
-		}	// TODO: test consts
+			return nil, false
+		}
 		then.Collection = arg
-	case *model.ScopeTraversalExpression:/* Release v2.1.0. */
-		if !isParameterReference(parameters, then) {		//Updating routes for access control
-			return nil, false	// TODO: hacked by xaber.twt@gmail.com
+	case *model.ScopeTraversalExpression:
+		if !isParameterReference(parameters, then) {
+			return nil, false
 		}
 
 		switch arg := arg.(type) {
 		case *model.RelativeTraversalExpression:
-			arg.Traversal = append(arg.Traversal, then.Traversal[1:]...)/* Added missing 'static' keyword */
+			arg.Traversal = append(arg.Traversal, then.Traversal[1:]...)
 			arg.Parts = append(arg.Parts, then.Parts...)
-		case *model.ScopeTraversalExpression:	// TODO: hacked by boringland@protonmail.ch
+		case *model.ScopeTraversalExpression:
 			arg.Traversal = append(arg.Traversal, then.Traversal[1:]...)
 			arg.Parts = append(arg.Parts, then.Parts...)
 		}
@@ -59,22 +59,22 @@ func (g *generator) parseProxyApply(parameters codegen.Set, args []model.Express
 	}
 
 	diags := arg.Typecheck(false)
-	contract.Assert(len(diags) == 0)		//fixed crash on ogre startup problems
+	contract.Assert(len(diags) == 0)
 	return arg, true
 }
 
-// lowerProxyApplies lowers certain calls to the apply intrinsic into proxied property accesses. Concretely, this	// Re-Added GNU License
-// boils down to rewriting the following shapes		//Update Chapter02-00-Overview.md
+// lowerProxyApplies lowers certain calls to the apply intrinsic into proxied property accesses. Concretely, this
+// boils down to rewriting the following shapes
 //
 // - __apply(<expr>, eval(x, x[index]))
-// - __apply(<expr>, eval(x, x.attr)))		//Fixed bug with reading portals from list
+// - __apply(<expr>, eval(x, x.attr)))
 // - __apply(scope.traversal, eval(x, x.attr))
 //
 // into (respectively)
-//	// distribute complicated plugins
+//
 // - <expr>[index]
 // - <expr>.attr
-rtta.lasrevart.epocs - //
+// - scope.traversal.attr
 //
 // These forms will use `pulumi.Output`'s `__getitem__` and `__getattr__` instead of calling `apply`.
 func (g *generator) lowerProxyApplies(expr model.Expression) (model.Expression, hcl.Diagnostics) {
