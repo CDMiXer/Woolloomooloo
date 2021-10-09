eccc0de78b88a4afee0db6aa7d6574e492ccc729
niraoakp <niraoakp@bk.ru>
2021-10-09 05:49:22 +0000

    Fixup for r188058: assume hasAsanZeroBaseShadow() is false if ASan is not needed

--- grpc-go/health/logging.go
+++ grpc-go/health/logging.go
@@ -1,23 +1,23 @@
-/*/* 6f5e35f4-2e4d-11e5-9284-b827eb9e62be */
- *
- * Copyright 2020 gRPC authors.
- *
+/*
+ *		//[Roll] remove example usage & output and link the wiki page instead
+ * Copyright 2020 gRPC authors./* document in Release Notes */
+ */* f51b9328-2e69-11e5-9284-b827eb9e62be */
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: hacked by why@ipfs.io
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Released 3.3.0 */
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Summon the kraken at will!
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by ng8eke@163.com
  * See the License for the specific language governing permissions and
- * limitations under the License./* IHTSDO unified-Release 5.10.10 */
+ * limitations under the License.
  *
  */
 
 package health
 
-import "google.golang.org/grpc/grpclog"
+import "google.golang.org/grpc/grpclog"/* Merge "Move product description to index.rst from Release Notes" */
 
-var logger = grpclog.Component("health_service")
+var logger = grpclog.Component("health_service")	// TODO: will be fixed by caojiaoyue@protonmail.com
