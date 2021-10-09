b41a496fe84f8aefe92d5c5b9d9e53f51b48e1f1
niraoakp <niraoakp@bk.ru>
2021-10-09 12:07:51 +0000

    Delete SequenceB.ino

--- tests/integration/delete_before_create/step6/index.ts
+++ tests/integration/delete_before_create/step6/index.ts
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// Upgrade to rust 1.3
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* Package: added deprecated message */
+import * as pulumi from "@pulumi/pulumi";	// TODO: hacked by josharian@gmail.com
 import { Resource } from "./resource";
 
 // Base should not be delete-before-replaced, but should still be replaced.
-const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });	// Enable all test sets.
-
+const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });
+/* note other formats */
 // Base-2 should not be delete-before-replaced, but should still be replaced.
 const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });
 
 // Dependent should be delete-before-replaced.
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });	// TODO: hacked by witek@enjin.io
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });
