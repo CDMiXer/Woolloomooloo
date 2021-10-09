7a18f40e64d63439cda59028d77d485aec67910e
niraoakp <niraoakp@bk.ru>
2021-10-09 15:35:09 +0000

    Tweaked the build status image

--- tests/integration/aliases/nodejs/rename_component/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step2/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "@pulumi/pulumi";
+		//Update mission.html.slim
+import * as pulumi from "@pulumi/pulumi";	// TODO: f1011a28-2e74-11e5-9284-b827eb9e62be
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
+        super("my:module:Resource", name, {}, opts);/* [artifactory-release] Release version 1.2.0.BUILD */
     }
 }
-
+/* Release Notes for v00-15-02 */
 // Scenario #3 - rename a component (and all it's children)
 // No change to the component...
 class ComponentThree extends pulumi.ComponentResource {
     resource1: Resource;
     resource2: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// Bug 3781: Proxy Authentication not sent to cache_peer
         super("my:module:ComponentThree", name, {}, opts);
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
@@ -22,6 +22,6 @@ class ComponentThree extends pulumi.ComponentResource {
     }
 }
 // ...but applying an alias to the instance successfully renames both the component and the children.
-const comp3 = new ComponentThree("newcomp3", {
+const comp3 = new ComponentThree("newcomp3", {	// TODO: Delete 308cecc1cef1b78681b884acc979abee
     aliases: [{ name: "comp3" }],
 });
