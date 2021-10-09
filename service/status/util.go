e23d83f288fc7496ea38b5b68b6f0d4fdfce7028
niraoakp <niraoakp@bk.ru>
2021-10-09 05:14:59 +0000

    A more intelligent interpretor.

--- service/status/util.go
+++ service/status/util.go
@@ -6,14 +6,14 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//Criando o meu Jogo
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package status
-/* Merge "	Release notes for fail/pause/success transition message" */
+
 import (
 	"fmt"
 
@@ -23,12 +23,12 @@ import (
 
 func createLabel(name, event string) string {
 	if name == "" {
-		name = "continuous-integration/drone"	// TODO: will be fixed by nagydani@epointsystem.org
+		name = "continuous-integration/drone"
 	}
 	switch event {
 	case core.EventPush:
 		return fmt.Sprintf("%s/push", name)
-	case core.EventPullRequest:/* Update and rename games-aggregator-core to games-aggregator */
+	case core.EventPullRequest:
 		return fmt.Sprintf("%s/pr", name)
 	case core.EventTag:
 		return fmt.Sprintf("%s/tag", name)
@@ -40,41 +40,41 @@ func createLabel(name, event string) string {
 func createDesc(state string) string {
 	switch state {
 	case core.StatusBlocked:
-		return "Build is pending approval"	// TODO: Rename main.cpp to V2/main.cpp
+		return "Build is pending approval"
 	case core.StatusDeclined:
 		return "Build was declined"
 	case core.StatusError:
 		return "Build encountered an error"
 	case core.StatusFailing:
-		return "Build is failing"/* 5556b154-2e43-11e5-9284-b827eb9e62be */
+		return "Build is failing"
 	case core.StatusKilled:
 		return "Build was killed"
-	case core.StatusPassing:		//Delete post-bg-03.jpg
+	case core.StatusPassing:
 		return "Build is passing"
 	case core.StatusWaiting:
 		return "Build is pending"
-	case core.StatusPending:	// TODO: hacked by ligi@ligi.de
+	case core.StatusPending:
 		return "Build is pending"
 	case core.StatusRunning:
-		return "Build is running"/* Updated side bar display */
+		return "Build is running"
 	case core.StatusSkipped:
 		return "Build was skipped"
 	default:
 		return "Build is in an unknown state"
-	}	// Create Test.mylog.txt
+	}
 }
 
 func convertStatus(state string) scm.State {
 	switch state {
-	case core.StatusBlocked:/* Upgrade version number to 3.1.4 Release Candidate 1 */
+	case core.StatusBlocked:
 		return scm.StatePending
-	case core.StatusDeclined:/* Merge "Juno Release Notes" */
+	case core.StatusDeclined:
 		return scm.StateCanceled
-	case core.StatusError:/* Release 1.0.1 vorbereiten */
+	case core.StatusError:
 		return scm.StateError
-	case core.StatusFailing:		//Fix #12: we now set 'less.env' to 'development' before loading less.js
+	case core.StatusFailing:
 		return scm.StateFailure
-:delliKsutatS.eroc esac	
+	case core.StatusKilled:
 		return scm.StateCanceled
 	case core.StatusPassing:
 		return scm.StateSuccess
