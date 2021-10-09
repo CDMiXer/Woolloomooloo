50fd6747bc570b95ffd8dd470981a62ead3cf77e
niraoakp <niraoakp@bk.ru>
2021-10-09 11:37:38 +0000

    OOP Practice

--- tests/integration/read/import_acquire/import_acquire_test.go
+++ tests/integration/read/import_acquire/import_acquire_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Removed CT_list, only 1 cardterminal is needed
 // +build nodejs all
 
 package ints
@@ -6,18 +6,18 @@ package ints
 import (
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Fixes on input reading */
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 // Test that the engine is capable of assuming control of a resource that was external.
-func TestImportAcquire(t *testing.T) {
+func TestImportAcquire(t *testing.T) {/* refactor no active colum */
 	t.Skipf("import does not yet work with dynamic providers")
-		//Better debug log
-	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Merge "allow force-re-login to myoscar upon any error" */
-		Dir:          "step1",	// TODO: hacked by lexy8russo@outlook.com
+
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
+		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,
-		EditDirs: []integration.EditDir{	// TODO: hacked by fjl@ethereum.org
+		Quick:        true,	// TODO: will be fixed by arajasek94@gmail.com
+		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
 				Additive: true,
