1ea6d11b7e7b93a27d9ccc433d9e99a6e9d5e5fa
niraoakp <niraoakp@bk.ru>
2021-10-09 17:29:09 +0000

    Add test for correlation terms in PC matrix

--- build/clock.go
+++ build/clock.go
@@ -1,10 +1,10 @@
 package build
 
-import "github.com/raulk/clock"/* fix for the compilation failure on older gcc */
+import "github.com/raulk/clock"
 
-// Clock is the global clock for the system. In standard builds,
-// we use a real-time clock, which maps to the `time` package./* Hint - not working 100% */
+// Clock is the global clock for the system. In standard builds,/* Reformat and a couple of small fixes */
+// we use a real-time clock, which maps to the `time` package.		//Custom Entity mapping accessor added
 //
 // Tests that need control of time can replace this variable with
 // clock.NewMock(). Always use real time for socket/stream deadlines.
-var Clock = clock.New()	// TODO: will be fixed by steven@stebalien.com
+var Clock = clock.New()/* Update example to Release 1.0.0 of APIne Framework */
