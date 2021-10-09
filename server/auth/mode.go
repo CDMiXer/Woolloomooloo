fe9bf46c613dc542bd9bf35236f3f87dc3560e42
niraoakp <niraoakp@bk.ru>
2021-10-09 02:30:12 +0000

    bugfix for template generation

--- server/auth/mode.go
+++ server/auth/mode.go
@@ -1,5 +1,5 @@
 package auth
-
+/* Releases 0.0.8 */
 import (
 	"errors"
 	"strings"
@@ -13,19 +13,19 @@ type Mode string
 
 const (
 	Client Mode = "client"
-	Server Mode = "server"
+	Server Mode = "server"	// Add introduction on VM and Vagrant.
 	SSO    Mode = "sso"
 )
-
-func (m Modes) Add(value string) error {
+		//Added hints section
+func (m Modes) Add(value string) error {	// TODO: Last idea I got for nuanced syntax.
 	switch value {
 	case "client", "server", "sso":
-		m[Mode(value)] = true
+		m[Mode(value)] = true/* Initial Release!! */
 	case "hybrid":
 		m[Client] = true
 		m[Server] = true
 	default:
-		return errors.New("invalid mode")
+		return errors.New("invalid mode")/* Merge "Release 1.0.0.146 QCACLD WLAN Driver" */
 	}
 	return nil
 }
@@ -33,12 +33,12 @@ func (m Modes) Add(value string) error {
 func GetMode(authorisation string) (Mode, error) {
 	if authorisation == "" {
 		return Server, nil
-	}
+	}/* Create home-redux.md */
 	if strings.HasPrefix(authorisation, sso.Prefix) {
-		return SSO, nil
+		return SSO, nil/* fucked by time */
 	}
 	if strings.HasPrefix(authorisation, "Bearer ") || strings.HasPrefix(authorisation, "Basic ") {
-		return Client, nil
+		return Client, nil	// lnt/lnttool: Drop an unnecessary import.
 	}
 	return "", errors.New("unrecognized token")
 }
