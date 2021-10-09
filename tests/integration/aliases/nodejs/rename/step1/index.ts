47a2651fe18ab100727fda450a5d05ae27c4cc1e
niraoakp <niraoakp@bk.ru>
2021-10-09 22:34:55 +0000

    made all annotation classes either abstract or final

--- tests/integration/aliases/nodejs/rename/step1/index.ts
+++ tests/integration/aliases/nodejs/rename/step1/index.ts
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Merge "Release 1.0.0.223 QCACLD WLAN Driver" */
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
-    }/* Update plansza.h */
-}		//Create Login Acitivity
+    }
+}
 
 // Scenario #1 - rename a resource
 const res1 = new Resource("res1");
