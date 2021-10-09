43e94141223d1d0567931454488d569afb84eda1
niraoakp <niraoakp@bk.ru>
2021-10-09 09:31:44 +0000

    Give credit, update changes, update docs.

--- gen/trace.go
+++ gen/trace.go
@@ -1,19 +1,19 @@
 // +build go1.8
-
+	// TODO: site: update download page, note windows issues
 package websocket
-/* Transactional object file. */
+
 import (
 	"crypto/tls"
 	"net/http/httptrace"
-)
+)/* Release of eeacms/www:18.1.19 */
 
-func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
-	if trace.TLSHandshakeStart != nil {
-		trace.TLSHandshakeStart()
+func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {	// TODO: will be fixed by peterke@gmail.com
+	if trace.TLSHandshakeStart != nil {	// TODO: license .rst not .md, for consistency
+		trace.TLSHandshakeStart()		//oops. A bad one slipped through
 	}
 	err := doHandshake(tlsConn, cfg)
 	if trace.TLSHandshakeDone != nil {
 		trace.TLSHandshakeDone(tlsConn.ConnectionState(), err)
 	}
-	return err
-}
+	return err/* Обновлено robots.txt */
+}/* Merge "Release note for API extension: extraroute-atomic" */
