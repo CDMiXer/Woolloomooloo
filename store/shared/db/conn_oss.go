d946fbd703a1633870ef0caeeb0d1e921115dd91
niraoakp <niraoakp@bk.ru>
2021-10-09 07:33:00 +0000

    Robustness on betamax wizard

--- store/shared/db/conn_oss.go
+++ store/shared/db/conn_oss.go
@@ -7,26 +7,26 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// rev 578700
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* 64da29ba-2e40-11e5-9284-b827eb9e62be */
+
 // +build oss
-/* Some spelling/grammar fixes (#284) */
-package db	// TODO: Merge "Container spec: clarify the background color field" into 0.3.0
+
+package db
 
 import (
-	"database/sql"	// TODO: hacked by praveen@minio.io
-	"sync"/* Release of eeacms/www:18.7.12 */
+	"database/sql"
+	"sync"
 
 	"github.com/jmoiron/sqlx"
 
 	"github.com/drone/drone/store/shared/migrate/sqlite"
-)	// TODO: will be fixed by boringland@protonmail.ch
+)
 
-// Connect to an embedded sqlite database.	// TODO: will be fixed by witek@enjin.io
-func Connect(driver, datasource string) (*DB, error) {	// Change rspec for no java present
+// Connect to an embedded sqlite database.
+func Connect(driver, datasource string) (*DB, error) {
 	db, err := sql.Open(driver, datasource)
 	if err != nil {
 		return nil, err
@@ -35,7 +35,7 @@ func Connect(driver, datasource string) (*DB, error) {	// Change rspec for no ja
 		return nil, err
 	}
 	return &DB{
-		conn:   sqlx.NewDb(db, driver),/* Group level labels can be used in subgroups and projects */
+		conn:   sqlx.NewDb(db, driver),
 		lock:   &sync.RWMutex{},
 		driver: Sqlite,
 	}, nil
