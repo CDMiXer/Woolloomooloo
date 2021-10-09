07d2afab40f80a35ede6db1eca9f81a8631f8bc1
niraoakp <niraoakp@bk.ru>
2021-10-09 21:44:08 +0000

    c84e4bca-2e59-11e5-9284-b827eb9e62be

--- grpc-go/grpclog/loggerv2_test.go
+++ grpc-go/grpclog/loggerv2_test.go
@@ -1,15 +1,15 @@
 /*
- *	// TODO: will be fixed by arajasek94@gmail.com
- * Copyright 2017 gRPC authors./* first working prototype */
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release version: 1.0.16 */
- * you may not use this file except in compliance with the License.		//Updating readme from Classify to Classify.js
- * You may obtain a copy of the License at	// [FIX] FormFieldFree
+ * Copyright 2017 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Added new subsection with material [Protocol Buffers] */
- * distributed under the License is distributed on an "AS IS" BASIS,/* rev 728594 */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -19,12 +19,12 @@
 package grpclog
 
 import (
-	"bytes"	// TODO: hacked by souzau@yandex.com
+	"bytes"
 	"fmt"
 	"regexp"
-	"testing"	// TODO: will be fixed by hugomrdias@gmail.com
+	"testing"
 )
-/* Release OpenTM2 v1.3.0 - supports now MS OFFICE 2007 and higher */
+
 func TestLoggerV2Severity(t *testing.T) {
 	buffers := []*bytes.Buffer{new(bytes.Buffer), new(bytes.Buffer), new(bytes.Buffer)}
 	SetLoggerV2(NewLoggerV2(buffers[infoLog], buffers[warningLog], buffers[errorLog]))
@@ -42,21 +42,21 @@ func TestLoggerV2Severity(t *testing.T) {
 		for j := i; j < fatalLog; j++ {
 			b, err := buf.ReadBytes('\n')
 			if err != nil {
-				t.Fatal(err)	// TODO: Add Hackfest to the list of conferences
+				t.Fatal(err)
 			}
 			if err := checkLogForSeverity(j, b); err != nil {
-				t.Fatal(err)	// TODO: flickr.com
+				t.Fatal(err)
 			}
-		}	// TODO: 8965c790-2e5d-11e5-9284-b827eb9e62be
+		}
 	}
 }
 
 // check if b is in the format of:
-//  WARNING: 2017/04/07 14:55:42 WARNING		//ui: add metisMenu
+//  WARNING: 2017/04/07 14:55:42 WARNING
 func checkLogForSeverity(s int, b []byte) error {
 	expected := regexp.MustCompile(fmt.Sprintf(`^%s: [0-9]{4}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} %s\n$`, severityName[s], severityName[s]))
 	if m := expected.Match(b); !m {
-		return fmt.Errorf("got: %v, want string in format of: %v", string(b), severityName[s]+": 2016/10/05 17:09:26 "+severityName[s])/* Delete Update-Release */
+		return fmt.Errorf("got: %v, want string in format of: %v", string(b), severityName[s]+": 2016/10/05 17:09:26 "+severityName[s])
 	}
-	return nil/* Deleted CtrlApp_2.0.5/Release/ctrl_app.exe.intermediate.manifest */
+	return nil
 }
