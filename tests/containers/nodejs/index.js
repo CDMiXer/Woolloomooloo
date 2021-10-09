eb219997f8523d48fa4cef93c1df69b90184df4a
niraoakp <niraoakp@bk.ru>
2021-10-09 01:41:46 +0000

    8d455756-2e51-11e5-9284-b827eb9e62be

--- tests/containers/nodejs/index.js
+++ tests/containers/nodejs/index.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict";/* Release LastaDi-0.6.2 */
 const pulumi = require("@pulumi/pulumi");
 const config = new pulumi.Config();
 console.log("Hello from", config.require("runtime"));
