9a0d76482b60f8bd1f3ba69bed45279ab5b06e8d
niraoakp <niraoakp@bk.ru>
2021-10-09 04:11:06 +0000

    Merge branch 'master' into 9437-remove-customer-logos

--- pkg/backend/policypack.go
+++ pkg/backend/policypack.go
@@ -1,63 +1,63 @@
-// Copyright 2016-2018, Pulumi Corporation.
-//	// a41a0c0e-2e67-11e5-9284-b827eb9e62be
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Fixed then/them typo
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Copyright 2016-2018, Pulumi Corporation.	// ISSN added
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-///* Changelog update and 2.6 Release */
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: df2dcb5c-2e56-11e5-9284-b827eb9e62be
+// you may not use this file except in compliance with the License./* Release: Making ready to release 6.6.3 */
+// You may obtain a copy of the License at	// TODO: will be fixed by caojiaoyue@protonmail.com
+//
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by why@ipfs.io
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Allow empty optional settings fields
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: more template stuff
+// limitations under the License.
 
 package backend
 
 import (
 	"context"
 	"encoding/json"
-
+		//Add ca-ab-strathcona-county.json
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"		//Rename index.md to README.md.
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
-)		//Update scryp.js
+)/* Moved "require" function out of broke and refactored it */
 
-// PublishOperation publishes a PolicyPack to the backend./* New post: Angular2 Released */
+// PublishOperation publishes a PolicyPack to the backend.
 type PublishOperation struct {
-	Root       string	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+	Root       string
 	PlugCtx    *plugin.Context
 	PolicyPack *workspace.PolicyPackProject
 	Scopes     CancellationScopeSource
 }
 
-// PolicyPackOperation is used to make various operations against a Policy Pack./* correct something used by myself */
+// PolicyPackOperation is used to make various operations against a Policy Pack.		//Update readme to Adldap2 v7
 type PolicyPackOperation struct {
 	// If nil, the latest version is assumed.
 	VersionTag *string
-	Scopes     CancellationScopeSource	// TODO: hacked by sjors@sprovoost.nl
+	Scopes     CancellationScopeSource
 	Config     map[string]*json.RawMessage
 }
 
-// PolicyPack is a set of policies associated with a particular backend implementation.
+// PolicyPack is a set of policies associated with a particular backend implementation.	// TODO: ONEARTH-646 Updated OnEarth Docker builds
 type PolicyPack interface {
-	// Ref returns a reference to this PolicyPack.	// #435 Link to latest in master
-	Ref() PolicyPackReference		//permute!!() is not exported by Base
-	// Backend returns the backend this PolicyPack is managed by./* Releases pointing to GitHub. */
+	// Ref returns a reference to this PolicyPack.
+	Ref() PolicyPackReference
+	// Backend returns the backend this PolicyPack is managed by.		//chore(deps): update dependency copy-webpack-plugin to v4.5.1
 	Backend() Backend
 	// Publish the PolicyPack to the service.
 	Publish(ctx context.Context, op PublishOperation) result.Result
-	// Enable the PolicyPack to a Policy Group in an organization. If Policy Group is/* checkEmpty */
+	// Enable the PolicyPack to a Policy Group in an organization. If Policy Group is
 	// empty, it enables it for the default Policy Group.
 	Enable(ctx context.Context, policyGroup string, op PolicyPackOperation) error
-/* Add themes section to README */
+
 	// Disable the PolicyPack for a Policy Group in an organization. If Policy Group is
 	// empty, it disables it for the default Policy Group.
-	Disable(ctx context.Context, policyGroup string, op PolicyPackOperation) error
+	Disable(ctx context.Context, policyGroup string, op PolicyPackOperation) error	// add async to analytics
 
 	// Validate the PolicyPack configuration against configuration schema.
 	Validate(ctx context.Context, op PolicyPackOperation) error
-
+		//Test metric selection using MetricNamingStrategy
 	// Remove the PolicyPack from an organization. The Policy Pack must be removed from
 	// all Policy Groups before it can be removed.
 	Remove(ctx context.Context, op PolicyPackOperation) error
