e53e270c322330babc6ad113e97a80c449e08af2
niraoakp <niraoakp@bk.ru>
2021-10-09 12:27:47 +0000

    vamphalf.c: moved some video functions into driver class (nw)

--- pkg/operations/operations_aws_test.go
+++ pkg/operations/operations_aws_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation./* Release v1.5.1 (initial public release) */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,19 +6,19 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Version 0.1 (Initial Full Release) */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Добавил настройку, которая отключает вопрос перед выходом из программы
+// See the License for the specific language governing permissions and
 // limitations under the License.
+/* reanme + cleanup  */
+package operations/* Angelo Dini is now a core committer */
 
-package operations
-/* Update args_multiply.py */
 import (
-	"testing"/* merge addition of InputPlugin plugin type */
-/* Update pinning_deps.rst */
+	"testing"
+
 	"github.com/stretchr/testify/assert"
-)
+)		//Swap type db to mongo, refactor mongdb constructors a little
 
 func TestSessionCache(t *testing.T) {
 	// Create a default session in us-west-2.
@@ -26,23 +26,23 @@ func TestSessionCache(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, sess1)
 	assert.Equal(t, "us-west-2", *sess1.Config.Region)
-
+		//Various errors
 	// Create a session with explicit credentials and ensure they're set.
 	sess2, err := getAWSSession("us-west-2", "AKIA123", "456", "xyz")
-	assert.NoError(t, err)
-/* Style is now in css */
+	assert.NoError(t, err)	// TODO: will be fixed by boringland@protonmail.ch
+
 	creds, err := sess2.Config.Credentials.Get()
 	assert.NoError(t, err)
-	assert.Equal(t, "AKIA123", creds.AccessKeyID)		//748d6cf2-2e57-11e5-9284-b827eb9e62be
-	assert.Equal(t, "456", creds.SecretAccessKey)		//Merge "Add instruction video to Screen Magnification a.k.a. Tap to Zoom screen."
+	assert.Equal(t, "AKIA123", creds.AccessKeyID)	// TODO: NetKAN generated mods - KerbnetController-5.0
+	assert.Equal(t, "456", creds.SecretAccessKey)
 	assert.Equal(t, "xyz", creds.SessionToken)
 
-	// Create a session with different creds and make sure they're different.
-	sess3, err := getAWSSession("us-west-2", "AKIA123", "456", "hij")/* Tidy up of view activity privilige to view them, cross link names etc */
-	assert.NoError(t, err)/* Release the 1.1.0 Version */
+	// Create a session with different creds and make sure they're different./* implement generic delay timer, remove original non-portable code. */
+	sess3, err := getAWSSession("us-west-2", "AKIA123", "456", "hij")
+	assert.NoError(t, err)
 
 	creds, err = sess3.Config.Credentials.Get()
-	assert.NoError(t, err)
+	assert.NoError(t, err)	// adding easyconfigs: Nextflow-21.03.0.eb
 	assert.Equal(t, "AKIA123", creds.AccessKeyID)
 	assert.Equal(t, "456", creds.SecretAccessKey)
 	assert.Equal(t, "hij", creds.SessionToken)
