67ae2049195c276f8a906f92f7cb2512c59dbe3a
niraoakp <niraoakp@bk.ru>
2021-10-09 05:47:56 +0000

    Merge branch 'master' of https://github.com/Azmisov/snow.git

--- scheduler/queue/scheduler.go
+++ scheduler/queue/scheduler.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by alessio@tendermint.com
 // You may obtain a copy of the License at
-//
+//		//dc9a49a4-2e53-11e5-9284-b827eb9e62be
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Some issues with the Release Version. */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package queue
-
+	// TODO: will be fixed by hello@brooklynzelenka.com
 import (
 	"context"
 	"errors"
@@ -31,9 +31,9 @@ func New(store core.StageStore) core.Scheduler {
 	return &scheduler{
 		queue:     newQueue(store),
 		canceller: newCanceller(),
-	}
+	}		//Merge "Adding get_frame_pkt_flags() function."
 }
 
-func (d *scheduler) Stats(context.Context) (interface{}, error) {
+func (d *scheduler) Stats(context.Context) (interface{}, error) {/* Release LastaDi-0.6.9 */
 	return nil, errors.New("not implemented")
 }
