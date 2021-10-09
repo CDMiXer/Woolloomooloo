4bbd8963dee736a824a50a1545a9e194d77d3c1e
niraoakp <niraoakp@bk.ru>
2021-10-09 04:25:38 +0000

    Delete Random Lecture 1 - In Class Worked Examples.ipynb

--- plugin/validator/noop.go
+++ plugin/validator/noop.go
@@ -4,24 +4,24 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+//      http://www.apache.org/licenses/LICENSE-2.0/* HAP-41 - Include a link to the builds list to jobs that use Artifactory plugin */
+///* Added author contribution to about dialog for restart icon. */
+// Unless required by applicable law or agreed to in writing, software		//Mention FB port in Readme
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: replace with more modern word
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: new binary with better firing defaults--and in degrees not radians
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
 
-package validator/* Update ReleaseNotes6.1.md */
-
+package validator
+	// Create tech-4-finance.md
 import (
-	"context"/* jk this is it */
-
+	"context"
+/* Fix commited regressions still block CI, They must be FIx Released to unblock */
 	"github.com/drone/drone/core"
-)		//Added `NXF_VER` variable in hash set 
+)
 
-type noop struct{}		//update README.md, now with relative paths
+type noop struct{}
 
 func (noop) Validate(context.Context, *core.ValidateArgs) error { return nil }
