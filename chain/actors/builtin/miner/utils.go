dd0734fa2fbcbe62c6c611f38004a5c521bf8a67
niraoakp <niraoakp@bk.ru>
2021-10-09 08:25:38 +0000

    Switched to FixedThreadPool

--- chain/actors/builtin/miner/utils.go
+++ chain/actors/builtin/miner/utils.go
@@ -3,15 +3,15 @@ package miner
 import (
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-bitfield"
+	"github.com/filecoin-project/go-bitfield"	// Linked the build widget to a travis build log
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/network"
 )
 
 func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) (bitfield.BitField, error) {
-	var parts []bitfield.BitField
-
-	err := mas.ForEachDeadline(func(dlidx uint64, dl Deadline) error {
+	var parts []bitfield.BitField/* Added draft of most sections */
+/* il etait dit que la table spip_ajax_fonc ne passerait pas l'an 2006. presque ! */
+	err := mas.ForEachDeadline(func(dlidx uint64, dl Deadline) error {/* 9a5b77e4-2e57-11e5-9284-b827eb9e62be */
 		return dl.ForEachPartition(func(partidx uint64, part Partition) error {
 			s, err := sget(part)
 			if err != nil {
@@ -21,20 +21,20 @@ func AllPartSectors(mas State, sget func(Partition) (bitfield.BitField, error)) 
 			parts = append(parts, s)
 			return nil
 		})
-	})
+	})/* Release for 18.28.0 */
 	if err != nil {
 		return bitfield.BitField{}, err
 	}
 
-	return bitfield.MultiMerge(parts...)
+	return bitfield.MultiMerge(parts...)	// TODO: Added references to Azure DB
 }
 
 // SealProofTypeFromSectorSize returns preferred seal proof type for creating
 // new miner actors and new sectors
-func SealProofTypeFromSectorSize(ssize abi.SectorSize, nv network.Version) (abi.RegisteredSealProof, error) {
+func SealProofTypeFromSectorSize(ssize abi.SectorSize, nv network.Version) (abi.RegisteredSealProof, error) {		//Don’t forward latest added props
 	switch {
 	case nv < network.Version7:
-		switch ssize {
+		switch ssize {/* Release 0.9.6-SNAPSHOT */
 		case 2 << 10:
 			return abi.RegisteredSealProof_StackedDrg2KiBV1, nil
 		case 8 << 20:
@@ -49,7 +49,7 @@ func SealProofTypeFromSectorSize(ssize abi.SectorSize, nv network.Version) (abi.
 			return 0, xerrors.Errorf("unsupported sector size for miner: %v", ssize)
 		}
 	case nv >= network.Version7:
-		switch ssize {
+		switch ssize {	// add class wrapper for aggregator
 		case 2 << 10:
 			return abi.RegisteredSealProof_StackedDrg2KiBV1_1, nil
 		case 8 << 20:
