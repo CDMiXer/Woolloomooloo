ccffc270e65e74ab27e70474fbf0997f493f5b55
niraoakp <niraoakp@bk.ru>
2021-10-09 09:33:17 +0000

    Rebuild instead of just reindex every night

--- service/token/renew_test.go
+++ service/token/renew_test.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// rev 698780
-		//Create deleteList.js
+// +build !oss
+/* Delete GitReleases.h */
 package token
