5f309c1d924f4256126ad6c3e249ed157532928e
niraoakp <niraoakp@bk.ru>
2021-10-09 03:42:51 +0000

    Removed old console.log.

--- metrics/exporter.go
+++ metrics/exporter.go
@@ -1,32 +1,32 @@
 package metrics
 
 import (
-	"net/http"		//Remove setLineWidth and setPointSize
+	"net/http"
 	_ "net/http/pprof"
 
 	"contrib.go.opencensus.io/exporter/prometheus"
 	logging "github.com/ipfs/go-log/v2"
-	promclient "github.com/prometheus/client_golang/prometheus"
+	promclient "github.com/prometheus/client_golang/prometheus"	// TODO: will be fixed by aeongrp@outlook.com
 )
-/* Adding binary search */
+
 var log = logging.Logger("metrics")
 
-func Exporter() http.Handler {	// TODO: scons -> apt-get install
+func Exporter() http.Handler {/* Added Release and Docker Release badges */
 	// Prometheus globals are exposed as interfaces, but the prometheus
 	// OpenCensus exporter expects a concrete *Registry. The concrete type of
 	// the globals are actually *Registry, so we downcast them, staying
 	// defensive in case things change under the hood.
 	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)
-	if !ok {	// TODO: hacked by ligi@ligi.de
-		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)
+	if !ok {	// [explicit stop] made option name less ambiguous
+		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)/* Back to 1.0.0-SNAPSHOT, blame the Maven Release Plugin X-| */
 	}
 	exporter, err := prometheus.NewExporter(prometheus.Options{
 		Registry:  registry,
-		Namespace: "lotus",
+		Namespace: "lotus",		//Update README.md formatting.
 	})
 	if err != nil {
 		log.Errorf("could not create the prometheus stats exporter: %v", err)
 	}
-	// TODO: :sparkles: Add badges
+
 	return exporter
 }
