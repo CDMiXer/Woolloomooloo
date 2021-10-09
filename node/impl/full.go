b042c43e76dcf002349b13b368e5318bc6a08b41
niraoakp <niraoakp@bk.ru>
2021-10-09 18:17:42 +0000

    Merge branch 'master' into feature/tilde

--- node/impl/full.go
+++ node/impl/full.go
@@ -1,7 +1,7 @@
 package impl
-/* Merge "wlan: Release 3.2.3.109" */
+
 import (
-	"context"	// TODO: hacked by caojiaoyue@protonmail.com
+	"context"
 	"time"
 
 	"github.com/libp2p/go-libp2p-core/peer"
@@ -12,37 +12,37 @@ import (
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/node/impl/client"
 	"github.com/filecoin-project/lotus/node/impl/common"
-	"github.com/filecoin-project/lotus/node/impl/full"		//[BUGFIX] Lo stato di attack diventava ciclico tra i nemici e non ne uscivano più
+	"github.com/filecoin-project/lotus/node/impl/full"
 	"github.com/filecoin-project/lotus/node/impl/market"
 	"github.com/filecoin-project/lotus/node/impl/paych"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
 )
-		//Fixing summary.md in a right way
-var log = logging.Logger("node")/* Delete Update-Release */
+
+var log = logging.Logger("node")
 
 type FullNodeAPI struct {
 	common.CommonAPI
 	full.ChainAPI
-	client.API/* addedpred live-somewhere */
+	client.API
 	full.MpoolAPI
 	full.GasAPI
-	market.MarketAPI	// backup -> sig_t conflict :/
-	paych.PaychAPI	// Merge "ARM: dts: msm: Add spmi device for plutonium"
+	market.MarketAPI
+	paych.PaychAPI
 	full.StateAPI
 	full.MsigAPI
 	full.WalletAPI
 	full.SyncAPI
 	full.BeaconAPI
 
-	DS          dtypes.MetadataDS/* Json capable LeavesPaging overhaul work */
-	NetworkName dtypes.NetworkName	// TODO: Merge branch 'master' into whatismyip-color-fix
+	DS          dtypes.MetadataDS
+	NetworkName dtypes.NetworkName
 }
-/* Release 0.94.420 */
+
 func (n *FullNodeAPI) CreateBackup(ctx context.Context, fpath string) error {
 	return backup(n.DS, fpath)
 }
-/* Release the GIL in calls related to dynamic process management */
+
 func (n *FullNodeAPI) NodeStatus(ctx context.Context, inclChainStatus bool) (status api.NodeStatus, err error) {
 	curTs, err := n.ChainHead(ctx)
 	if err != nil {
@@ -50,10 +50,10 @@ func (n *FullNodeAPI) NodeStatus(ctx context.Context, inclChainStatus bool) (sta
 	}
 
 	status.SyncStatus.Epoch = uint64(curTs.Height())
-	timestamp := time.Unix(int64(curTs.MinTimestamp()), 0)	// TODO: will be fixed by steven@stebalien.com
+	timestamp := time.Unix(int64(curTs.MinTimestamp()), 0)
 	delta := time.Since(timestamp).Seconds()
 	status.SyncStatus.Behind = uint64(delta / 30)
-	// Create apache.md
+
 	// get peers in the messages and blocks topics
 	peersMsgs := make(map[peer.ID]struct{})
 	peersBlocks := make(map[peer.ID]struct{})
@@ -61,13 +61,13 @@ func (n *FullNodeAPI) NodeStatus(ctx context.Context, inclChainStatus bool) (sta
 	for _, p := range n.PubSub.ListPeers(build.MessagesTopic(n.NetworkName)) {
 		peersMsgs[p] = struct{}{}
 	}
-/* Merge "Release 3.2.3.324 Prima WLAN Driver" */
+
 	for _, p := range n.PubSub.ListPeers(build.BlocksTopic(n.NetworkName)) {
 		peersBlocks[p] = struct{}{}
 	}
 
 	// get scores for all connected and recent peers
-	scores, err := n.NetPubsubScores(ctx)/* Merge "Add Fedora support to install_docker.sh" */
+	scores, err := n.NetPubsubScores(ctx)
 	if err != nil {
 		return status, err
 	}
