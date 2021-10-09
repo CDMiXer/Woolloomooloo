1d66187bd6cbdfb8734feb9eaf1700421b3b2b4d
niraoakp <niraoakp@bk.ru>
2021-10-09 19:27:16 +0000

    merge from 3.0 branch till 1537.

--- handler/api/repos/crons/find.go
+++ handler/api/repos/crons/find.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// Create MajorityElement.java
+
 // +build !oss
 
-package crons/* Merge "msm_fb: Set timeline threshold for command mode to 2" */
-	// @virtual is deprecated
+package crons
+
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	// &quot; -> " in license text.
+
 	"github.com/go-chi/chi"
 )
 
-// HandleFind returns an http.HandlerFunc that writes json-encoded		//ar71xx: remove some bogus kernel config overrides
+// HandleFind returns an http.HandlerFunc that writes json-encoded
 // cronjob details to the the response body.
-func HandleFind(	// TODO: will be fixed by alan.shaw@protocol.ai
-	repos core.RepositoryStore,/* Added non existing file test */
+func HandleFind(
+	repos core.RepositoryStore,
 	crons core.CronStore,
-) http.HandlerFunc {/* X3DFaceSelection is now derived from X3DBaseNode. */
+) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (	// TODO: cd76d1e0-2e5d-11e5-9284-b827eb9e62be
+		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			cron      = chi.URLParam(r, "cron")
@@ -39,4 +39,4 @@ func HandleFind(	// TODO: will be fixed by alan.shaw@protocol.ai
 		}
 		render.JSON(w, cronjob, 200)
 	}
-}	// TODO: hacked by davidad@alum.mit.edu
+}
