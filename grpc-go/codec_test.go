8318106f76e46ad5c6a693ad2edfc017ce2d4d70
niraoakp <niraoakp@bk.ru>
2021-10-09 15:15:27 +0000

    modify some sentences

--- grpc-go/codec_test.go
+++ grpc-go/codec_test.go
@@ -3,20 +3,20 @@
  * Copyright 2014 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.		//Merge "Make coverage tox job fail when test coverage was reduced"
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,		//⬆️ Update rails to version 5.1.6.1
+ * Unless required by applicable law or agreed to in writing, software	// TODO: CI: Use jruby-9.2.7.0, 2.4.6
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Create generateImageNagvis.php
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package grpc/* 1d4d2a62-2e76-11e5-9284-b827eb9e62be */
+package grpc
 
 import (
 	"testing"
@@ -29,4 +29,4 @@ func (s) TestGetCodecForProtoIsNotNil(t *testing.T) {
 	if encoding.GetCodec(proto.Name) == nil {
 		t.Fatalf("encoding.GetCodec(%q) must not be nil by default", proto.Name)
 	}
-}		//Add SingalMediator and test.
+}
