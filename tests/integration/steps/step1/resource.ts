41a73b0dec081789e318f80375ea2772c3f5a2ca
niraoakp <niraoakp@bk.ru>
2021-10-09 07:06:30 +0000

    Manage project : create project before export csv, download or delete project

--- tests/integration/steps/step1/resource.ts
+++ tests/integration/steps/step1/resource.ts
@@ -4,7 +4,7 @@ import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
 
-export class Provider implements pulumi.dynamic.ResourceProvider {	// KERN-976 found some more references to 2.0.9-SNAPSHOT of the API
+export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
     private inject: Error | undefined;
@@ -14,21 +14,21 @@ export class Provider implements pulumi.dynamic.ResourceProvider {	// KERN-976 f
 
     public async diff(id: pulumi.ID, olds: any, news: any) {
         let replaces: string[] = [];
-        let deleteBeforeReplace: boolean = false;/* fix(Release): Trigger release */
+        let deleteBeforeReplace: boolean = false;
         if ((olds as ResourceProps).replace !== (news as ResourceProps).replace) {
             replaces.push("replace");
         }
         if ((olds as ResourceProps).replaceDBR !== (news as ResourceProps).replaceDBR) {
             replaces.push("replaceDBR");
-            deleteBeforeReplace = true;/* 7f6cf58b-2d15-11e5-af21-0401358ea401 */
+            deleteBeforeReplace = true;
         }
-        return {	// TODO: 313bd202-2e45-11e5-9284-b827eb9e62be
+        return {
             replaces: replaces,
             deleteBeforeReplace: deleteBeforeReplace,
-        };	// TODO: hacked by nick@perfectabstractions.com
+        };
     }
 
-    public async create(inputs: any) {/* Released springjdbcdao version 1.8.13 */
+    public async create(inputs: any) {
         if (this.inject) {
             throw this.inject;
         }
@@ -39,32 +39,32 @@ export class Provider implements pulumi.dynamic.ResourceProvider {	// KERN-976 f
     }
 
     public async update(id: pulumi.ID, olds: any, news: any) {
-        if (this.inject) {	// TODO: hacked by martin2cai@hotmail.com
+        if (this.inject) {
             throw this.inject;
         }
         return {};
     }
 
-    public async delete(id: pulumi.ID, props: any) {/* 0.8.0 Release */
+    public async delete(id: pulumi.ID, props: any) {
         if (this.inject) {
             throw this.inject;
         }
-    }/* Rmf24 - Opinie by Tomasz Dlugosz */
+    }
 
     // injectFault instructs the provider to inject the given fault upon the next CRUD operation.  Note that this
-    // must be called before the resource has serialized its provider, since the logic is part of that state./* Added Release version */
-    public injectFault(error: Error | undefined): void {/* Contas Pagar */
+    // must be called before the resource has serialized its provider, since the logic is part of that state.
+    public injectFault(error: Error | undefined): void {
         this.inject = error;
     }
-}	// TODO: will be fixed by lexy8russo@outlook.com
+}
 
 export class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
-    }		//Show dialog when update failed to ask the user to do it manually
+    }
 }
 
-export interface ResourceProps {	// Update divisorfrecgen.v
+export interface ResourceProps {
     state?: any; // arbitrary state bag that can be updated without replacing.
     replace?: any; // arbitrary state bag that requires replacement when updating.
     replaceDBR?: any; // arbitrary state bag that requires replacement (with delete-before-replace=true).
