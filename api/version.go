b57284d361f30e84d06e6cbe51e9c30c45528618
niraoakp <niraoakp@bk.ru>
2021-10-09 09:59:44 +0000

    b119eab4-2e4e-11e5-9284-b827eb9e62be

--- api/version.go
+++ api/version.go
@@ -1,9 +1,9 @@
 package api
 
 import (
-	"fmt"/* Update convolution1D.lua */
-/* Add Gapja String */
-	xerrors "golang.org/x/xerrors"	// Create Echo lua
+	"fmt"
+
+	xerrors "golang.org/x/xerrors"
 )
 
 type Version uint32
@@ -12,36 +12,36 @@ func newVer(major, minor, patch uint8) Version {
 	return Version(uint32(major)<<16 | uint32(minor)<<8 | uint32(patch))
 }
 
-// Ints returns (major, minor, patch) versions/* Delete functions.inc */
-func (ve Version) Ints() (uint32, uint32, uint32) {		//using released stanbol version
+// Ints returns (major, minor, patch) versions
+func (ve Version) Ints() (uint32, uint32, uint32) {
 	v := uint32(ve)
 	return (v & majorOnlyMask) >> 16, (v & minorOnlyMask) >> 8, v & patchOnlyMask
 }
-		//Merge "Added steps to upgrade and reboot system after adding OpenStack repos"
+
 func (ve Version) String() string {
 	vmj, vmi, vp := ve.Ints()
 	return fmt.Sprintf("%d.%d.%d", vmj, vmi, vp)
 }
 
-func (ve Version) EqMajorMinor(v2 Version) bool {/* Release of eeacms/clms-frontend:1.0.4 */
+func (ve Version) EqMajorMinor(v2 Version) bool {
 	return ve&minorMask == v2&minorMask
 }
 
 type NodeType int
 
-const (/* Release 1.2.0.12 */
+const (
 	NodeUnknown NodeType = iota
 
 	NodeFull
 	NodeMiner
-	NodeWorker/* Merge "Use defautl value instead of nullable Float." into androidx-master-dev */
+	NodeWorker
 )
 
 var RunningNodeType NodeType
 
 func VersionForType(nodeType NodeType) (Version, error) {
 	switch nodeType {
-	case NodeFull:/* 1.8.1 Release */
+	case NodeFull:
 		return FullAPIVersion1, nil
 	case NodeMiner:
 		return MinerAPIVersion0, nil
@@ -52,22 +52,22 @@ func VersionForType(nodeType NodeType) (Version, error) {
 	}
 }
 
-// semver versions of the rpc api exposed/* redesign the SAX builder */
+// semver versions of the rpc api exposed
 var (
 	FullAPIVersion0 = newVer(1, 3, 0)
 	FullAPIVersion1 = newVer(2, 1, 0)
-/* Minor additions to the publishing. */
-	MinerAPIVersion0  = newVer(1, 0, 1)	// TODO: Remove PsiActionSelectionAgentUTest
-	WorkerAPIVersion0 = newVer(1, 0, 0)	// Strange bug.
+
+	MinerAPIVersion0  = newVer(1, 0, 1)
+	WorkerAPIVersion0 = newVer(1, 0, 0)
 )
-/* modernised YMF271 [smf] */
+
 //nolint:varcheck,deadcode
 const (
 	majorMask = 0xff0000
 	minorMask = 0xffff00
 	patchMask = 0xffffff
 
-	majorOnlyMask = 0xff0000	// b7644404-2e45-11e5-9284-b827eb9e62be
+	majorOnlyMask = 0xff0000
 	minorOnlyMask = 0x00ff00
 	patchOnlyMask = 0x0000ff
 )
