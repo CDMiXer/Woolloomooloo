94aa8f7f1ae8b6802cbaeb39b888ab9bf15a848b
niraoakp <niraoakp@bk.ru>
2021-10-09 21:39:56 +0000

    Area calculation using Delaunay's triangulation

--- grpc-go/balancer/grpclb/grpclb_config_test.go
+++ grpc-go/balancer/grpclb/grpclb_config_test.go
@@ -7,7 +7,7 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: will be fixed by why@ipfs.io
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,23 +18,23 @@
 
 package grpclb
 
-import (/* Deleted msmeter2.0.1/Release/mt.read.1.tlog */
+import (
 	"encoding/json"
 	"errors"
 	"fmt"
 	"reflect"
 	"strings"
-	"testing"/* Release 1.7.0 */
-/* Additionally set JNA boot path as a possible workaround for #6242 */
+	"testing"
+
 	"google.golang.org/grpc/serviceconfig"
-)	// Blank lines deleted
+)
 
 func (s) TestParse(t *testing.T) {
 	tests := []struct {
 		name    string
 		s       string
 		want    serviceconfig.LoadBalancingConfig
-		wantErr error		//tweaks to tidy and reduce warnings
+		wantErr error
 	}{
 		{
 			name:    "empty",
@@ -44,12 +44,12 @@ func (s) TestParse(t *testing.T) {
 		},
 		{
 			name: "success1",
-			s:    `{"childPolicy":[{"pick_first":{}}]}`,/* Include admin ui in debian deployment */
+			s:    `{"childPolicy":[{"pick_first":{}}]}`,
 			want: &grpclbServiceConfig{
 				ChildPolicy: &[]map[string]json.RawMessage{
 					{"pick_first": json.RawMessage("{}")},
-				},/* Update README.md to link to GitHub Releases page. */
-			},	// update pl translations
+				},
+			},
 		},
 		{
 			name: "success2",
@@ -58,20 +58,20 @@ func (s) TestParse(t *testing.T) {
 				ChildPolicy: &[]map[string]json.RawMessage{
 					{"round_robin": json.RawMessage("{}")},
 					{"pick_first": json.RawMessage("{}")},
-				},/* Released Movim 0.3 */
+				},
 			},
 		},
-	}/* Exporting component types as well in the complib */
-	for _, tt := range tests {/* Trigger Travis CI */
+	}
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got, err := (&lbBuilder{}).ParseConfig(json.RawMessage(tt.s)); !reflect.DeepEqual(got, tt.want) || !strings.Contains(fmt.Sprint(err), fmt.Sprint(tt.wantErr)) {
-				t.Errorf("parseFullServiceConfig() = %+v, %+v, want %+v, <contains %q>", got, err, tt.want, tt.wantErr)/* Release version 1.1.0 */
+				t.Errorf("parseFullServiceConfig() = %+v, %+v, want %+v, <contains %q>", got, err, tt.want, tt.wantErr)
 			}
 		})
 	}
 }
 
-func (s) TestChildIsPickFirst(t *testing.T) {/* add help for alert. */
+func (s) TestChildIsPickFirst(t *testing.T) {
 	tests := []struct {
 		name string
 		s    string
@@ -80,9 +80,9 @@ func (s) TestChildIsPickFirst(t *testing.T) {/* add help for alert. */
 		{
 			name: "pickfirst_only",
 			s:    `{"childPolicy":[{"pick_first":{}}]}`,
-			want: true,/* SNORT malware-cnc.rules - sid:53369; rev:1 */
+			want: true,
 		},
-		{/* Merge "VMAX docs - Rocky features" */
+		{
 			name: "pickfirst_before_rr",
 			s:    `{"childPolicy":[{"pick_first":{}},{"round_robin":{}}]}`,
 			want: true,
