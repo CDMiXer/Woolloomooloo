3478ea22fb91551bb0ebdd7fac2ee6a55b6b01ec
niraoakp <niraoakp@bk.ru>
2021-10-09 06:02:12 +0000

    Remove vue from selectors and just use embedded html

--- tests/integration/empty/gorun/main.go
+++ tests/integration/empty/gorun/main.go
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 package main
-/* Merge "Release 3.1.1" */
+
 import (
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* Release 0.7.2 */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
