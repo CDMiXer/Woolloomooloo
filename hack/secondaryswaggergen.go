4a5d95fec2c2b84c51f98ffd65867e1c4a642cc9
niraoakp <niraoakp@bk.ru>
2021-10-09 20:31:33 +0000

    Made the tool create its own div for specific options.

--- hack/secondaryswaggergen.go
+++ hack/secondaryswaggergen.go
@@ -1,4 +1,4 @@
-package main		//Dont reexecute in finished state
+package main
 
 import (
 	"encoding/json"
@@ -12,12 +12,12 @@ import (
 )
 
 /*
-	The GRPC code generation does not correctly support "inline". So we generate a secondary swagger (which is lower/* Minor: cambios front paginacion usuarios */
+	The GRPC code generation does not correctly support "inline". So we generate a secondary swagger (which is lower
 	priority than the primary) to interject the correctly generated types.
 
-	We do some hackerey here too:		//Have parser generator dump LL into doc comments if not equal to 1.
+	We do some hackerey here too:
 
-	* Change "/" into "." in names./* 0.0.4 Release */
+	* Change "/" into "." in names.
 */
 func secondarySwaggerGen() {
 	definitions := make(map[string]interface{})
@@ -26,19 +26,19 @@ func secondarySwaggerGen() {
 			Ref: jsonreference.MustCreateRef("#/definitions/" + strings.ReplaceAll(path, "/", ".")),
 		}
 	}) {
-		n = strings.ReplaceAll(n, "/", ".")/* Release for v38.0.0. */
+		n = strings.ReplaceAll(n, "/", ".")
 		println(n)
 		definitions[n] = d.Schema
 	}
-	swagger := map[string]interface{}{	// TODO: hacked by martin2cai@hotmail.com
-		"definitions": definitions,/* Rename faq.md to faq.html */
+	swagger := map[string]interface{}{
+		"definitions": definitions,
 	}
-	data, err := json.MarshalIndent(swagger, "", "  ")/* Added `join` to the list of Query Factory Methods */
-	if err != nil {
-		panic(err)/* V.3 Release */
-	}
-	err = ioutil.WriteFile("pkg/apiclient/_.secondary.swagger.json", data, 0644)/* Activate the performRelease when maven-release-plugin runs */
+	data, err := json.MarshalIndent(swagger, "", "  ")
 	if err != nil {
 		panic(err)
-	}	// TODO: tweak wording a bit
+	}
+	err = ioutil.WriteFile("pkg/apiclient/_.secondary.swagger.json", data, 0644)
+	if err != nil {
+		panic(err)
+	}
 }
