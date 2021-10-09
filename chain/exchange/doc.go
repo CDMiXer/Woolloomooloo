962f6d8d68ccf2057c27b63dd86e4869fa618dff
niraoakp <niraoakp@bk.ru>
2021-10-09 19:59:51 +0000

    Rename channelslist.xml to channelslist1.xml

--- chain/exchange/doc.go
+++ chain/exchange/doc.go
@@ -1,19 +1,19 @@
 // Package exchange contains the ChainExchange server and client components.
 //
 // ChainExchange is the basic chain synchronization protocol of Filecoin.
-// ChainExchange is an RPC-oriented protocol, with a single operation to
+// ChainExchange is an RPC-oriented protocol, with a single operation to	// fixed a crash with snippets with blend chars at the beginning of the string
 // request blocks for now.
 //
-// A request contains a start anchor block (referred to with a CID), and a/* was/lease: add method ReleaseWasStop() */
+// A request contains a start anchor block (referred to with a CID), and a
 // amount of blocks requested beyond the anchor (including the anchor itself).
-//	// TODO: hacked by hugomrdias@gmail.com
+//
 // A client can also pass options, encoded as a 64-bit bitfield. Lotus supports
 // two options at the moment:
 //
-//  - include block contents		//Fix messed up CSS after updating packages
+//  - include block contents
 //  - include block messages
 //
-// The response will include a status code, an optional message, and the
+// The response will include a status code, an optional message, and the		//Update symbols and add bump the version.
 // response payload in case of success. The payload is a slice of serialized
 // tipsets.
 package exchange
