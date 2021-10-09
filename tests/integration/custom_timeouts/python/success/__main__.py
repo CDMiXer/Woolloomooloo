8cc0e3f5c42c32a0ce60623c15745f28622ff26a
niraoakp <niraoakp@bk.ru>
2021-10-09 04:52:28 +0000

    Change from prev-post to next-post

--- tests/integration/custom_timeouts/python/success/__main__.py
+++ tests/integration/custom_timeouts/python/success/__main__.py
@@ -1,25 +1,25 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions
-/* Akvo RSR release ver. 0.9.13 (Code name Anakim) Release notes added */
-class Resource1(ComponentResource):	// TODO: Update PerpetualInventoryCrafting.java
-    def __init__(self, name, opts=None):	// TODO: will be fixed by lexy8russo@outlook.com
+
+class Resource1(ComponentResource):
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
 # Attempt to create a resource with a CustomTimeout
 res1 = Resource1("res1",
-    opts=ResourceOptions(custom_timeouts=CustomTimeouts(create='30m'))/* Merge "Store more ports info in node.data for vdu profile" */
+    opts=ResourceOptions(custom_timeouts=CustomTimeouts(create='30m'))
 )
 
 # Also use the previous workaround method, which we should not regress upon
 res2 = Resource1("res2",
     opts=ResourceOptions(custom_timeouts={'create': '15m', 'delete': '15m'})
-)/* Released version 0.8.17 */
+)
 
 res3 = Resource1("res3",
     opts=ResourceOptions(custom_timeouts=CustomTimeouts(update='30m'))
 )
-/* Remove NetBeans warning about method parameter being assigned a value */
-res4 = Resource1("res4",	// TODO: hacked by josharian@gmail.com
+
+res4 = Resource1("res4",
     opts=ResourceOptions(custom_timeouts=CustomTimeouts(delete='30m'))
 )
