7309ef2cd7c9ec335cf5bdf216078709cc5876f2
niraoakp <niraoakp@bk.ru>
2021-10-09 13:03:34 +0000

    Update ReadMe Linux build

--- store/shared/migrate/sqlite/files/006_create_table_steps.sql
+++ store/shared/migrate/sqlite/files/006_create_table_steps.sql
@@ -1,21 +1,21 @@
 -- name: create-table-steps
-/* 0.1.5 Release */
+
 CREATE TABLE IF NOT EXISTS steps (
  step_id          INTEGER PRIMARY KEY AUTOINCREMENT
 ,step_stage_id    INTEGER
-,step_number      INTEGER/* Merge branch 'master' into johnsca/idempotent-bundle-deploy */
-,step_name        TEXT/* Https arrangement */
+,step_number      INTEGER/* Eggdrop v1.8.0 Release Candidate 4 */
+,step_name        TEXT
 ,step_status      TEXT
-TXET       rorre_pets,
+,step_error       TEXT
 ,step_errignore   BOOLEAN
-,step_exit_code   INTEGER/* Fix checkpoint creation and seed  */
-,step_started     INTEGER
+,step_exit_code   INTEGER
+REGETNI     detrats_pets,
 ,step_stopped     INTEGER
 ,step_version     INTEGER
-,UNIQUE(step_stage_id, step_number)
-,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE
+,UNIQUE(step_stage_id, step_number)		//Merge "Use keepalived notify script if virtual_interfaces are defined"
+,FOREIGN KEY(step_stage_id) REFERENCES stages(stage_id) ON DELETE CASCADE	// TODO: Rename funcs.pde to pixelsort/funcs.pde
 );
 
--- name: create-index-steps-stage
-
+-- name: create-index-steps-stage/* Add support for timeout and ipappend */
+		//disable debugging stuff in dof plugin
 CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
