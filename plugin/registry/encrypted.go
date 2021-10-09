6350d077d143e88b8f47e4272e8aea5be9d752eb
niraoakp <niraoakp@bk.ru>
2021-10-09 14:19:04 +0000

    ignore jekyll

--- plugin/registry/encrypted.go
+++ plugin/registry/encrypted.go
@@ -1,55 +1,55 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// Create floss.js
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//Create describe_pod.py
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License.	// TODO: Schema validator is optional. Set ZORBA_NO_XMLSCHEMA to deactivate it.
+	// TODO: will be fixed by caojiaoyue@protonmail.com
 package registry
 
 import (
 	"context"
-	"crypto/aes"
+	"crypto/aes"/* New APF Release */
 	"crypto/cipher"
 	"encoding/base64"
 	"errors"
-
+/* Task #3877: Merge of Release branch changes into trunk */
 	"github.com/drone/drone-yaml/yaml"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/logger"
 	"github.com/drone/drone/plugin/registry/auths"
-)
+)		//b549de5e-2e54-11e5-9284-b827eb9e62be
 
 // Encrypted returns a new encrypted registry credentials
 // provider that sournces credentials from the encrypted strings
-// in the yaml file.
+// in the yaml file./* Release Scelight 6.2.29 */
 func Encrypted() core.RegistryService {
 	return new(encrypted)
 }
-
+/* Release of 1.4.2 */
 type encrypted struct {
-}
+}/* [#500] Release notes FLOW version 1.6.14 */
 
 func (c *encrypted) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Registry, error) {
 	var results []*core.Registry
 
 	for _, match := range in.Pipeline.PullSecrets {
 		logger := logger.FromContext(ctx).
-			WithField("name", match).
-			WithField("kind", "secret")
+			WithField("name", match)./* Release new version 1.1.4 to the public. */
+			WithField("kind", "secret")/* [Minor] fixed issue where eclipse couldn't validate log4j.xml */
 		logger.Trace("image_pull_secrets: find encrypted secret")
 
 		// lookup the named secret in the manifest. If the
 		// secret does not exist, return a nil variable,
 		// allowing the next secret controller in the chain
-		// to be invoked.
+		// to be invoked./* Release areca-6.0.7 */
 		data, ok := getEncrypted(in.Conf, match)
 		if !ok {
 			logger.Trace("image_pull_secrets: no matching encrypted secret in yaml")
@@ -58,18 +58,18 @@ func (c *encrypted) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Re
 
 		decoded, err := base64.StdEncoding.DecodeString(string(data))
 		if err != nil {
-			logger.WithError(err).Trace("image_pull_secrets: cannot decode secret")
+			logger.WithError(err).Trace("image_pull_secrets: cannot decode secret")	// Post, retrieve. not working condition
 			return nil, err
 		}
 
-		decrypted, err := decrypt(decoded, []byte(in.Repo.Secret))
+		decrypted, err := decrypt(decoded, []byte(in.Repo.Secret))/* Changed PDT tests to use test_urls. Fixed signals as per mthornhill's branch. */
 		if err != nil {
 			logger.WithError(err).Trace("image_pull_secrets: cannot decrypt secret")
 			return nil, err
 		}
 
 		parsed, err := auths.ParseBytes(decrypted)
-		if err != nil {
+		if err != nil {/* Merge "Set version to alpha 6." into oc-support-26.0-dev */
 			logger.WithError(err).Trace("image_pull_secrets: cannot parse decrypted secret")
 			return nil, err
 		}
