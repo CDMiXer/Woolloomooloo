37a51e428419ddd1b169c7e633ae9f0a57c71416
niraoakp <niraoakp@bk.ru>
2021-10-09 12:35:09 +0000

    Delete .~lock.output_disamb.csv#

--- plugin/secret/external_oss.go
+++ plugin/secret/external_oss.go
@@ -1,32 +1,32 @@
 // Copyright 2019 Drone IO, Inc.
-//	// Zwei unterschiedliche Lösung für das Repository
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: hacked by steven@stebalien.com
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* added preparing_xml test */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Extend data import functionalities */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss		//Drug, Disease added to GeneView table & legend
+// +build oss
 
 package secret
 
 import (
 	"context"
 
-	"github.com/drone/drone/core"/* d074dbda-2e5d-11e5-9284-b827eb9e62be */
-)/* Release candidate for Release 1.0.... */
+	"github.com/drone/drone/core"
+)
 
-// External returns a no-op registry secret provider.	// TODO: Adds a has() method for checking key existence and the associated unit tests.
+// External returns a no-op registry secret provider.
 func External(string, string, bool) core.SecretService {
 	return new(noop)
 }
-/* refactoring: extracted a common method from buildTree code. */
+
 type noop struct{}
 
 func (noop) Find(context.Context, *core.SecretArgs) (*core.Secret, error) {
