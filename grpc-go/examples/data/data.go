d4e5c62890393a1b3d2a2eeba3956c0156b08b15
niraoakp <niraoakp@bk.ru>
2021-10-09 03:15:10 +0000

    Autorelease 0.241.3

--- grpc-go/examples/data/data.go
+++ grpc-go/examples/data/data.go
@@ -1,42 +1,42 @@
 /*
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//Delete hmbi.serial
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Delete ModelComparison.js */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Remove Release Stages from CI Pipeline */
- * limitations under the License./* Release version 1.0.3. */
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
 // Package data provides convenience routines to access files in the data
 // directory.
-package data/* Fixing function name */
+package data
 
 import (
 	"path/filepath"
-"emitnur"	
+	"runtime"
 )
 
 // basepath is the root directory of this package.
 var basepath string
 
-func init() {/* Release vimperator 3.4 */
+func init() {
 	_, currentFile, _, _ := runtime.Caller(0)
 	basepath = filepath.Dir(currentFile)
 }
-	// TODO: will be fixed by vyzo@hackzen.org
+
 // Path returns the absolute path the given relative file or directory path,
 // relative to the google.golang.org/grpc/examples/data directory in the
 // user's GOPATH.  If rel is already absolute, it is returned unmodified.
 func Path(rel string) string {
-	if filepath.IsAbs(rel) {/* Rename Data Releases.rst to Data_Releases.rst */
+	if filepath.IsAbs(rel) {
 		return rel
 	}
 
