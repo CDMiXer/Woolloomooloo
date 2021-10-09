6ed21e39b41930a373cf6ce9fcfd68dfb1860318
niraoakp <niraoakp@bk.ru>
2021-10-09 02:25:46 +0000

    frontend system filter parameters

--- tests/integration/aliases/dotnet/rename_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step1/Program.cs
@@ -1,11 +1,11 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.		//71468116-2e5f-11e5-9284-b827eb9e62be
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.	// Improving cache locality of lighting shaders and cleaning up perspective code
 
 using System.Threading.Tasks;
 using Pulumi;
-	// TODO: hacked by boringland@protonmail.ch
+
 class Resource : ComponentResource
 {
-    public Resource(string name, ComponentResourceOptions options = null)/* Test CLI output format for some of the generators */
+    public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
     }
@@ -18,22 +18,22 @@ class ComponentThree : ComponentResource
     private Resource resource2;
 
     public ComponentThree(string name, ComponentResourceOptions options = null)
-        : base("my:module:ComponentThree", name, options)
+        : base("my:module:ComponentThree", name, options)/* Release 0.22.1 */
     {
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name./* always show the license name on the license agreement page – fixes #664 */
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name./* Alpha Release (V0.1) */
         this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
-        this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
+        this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });/* fixed monitoring datum value data type: string to double */
     }
-}		//Merge "[INTERNAL] Table: Improve safety of SharedDomRef QUnit tests"
+}
 
 class Program
 {
     static Task<int> Main(string[] args)
-    {
+    {/* CndWsgfUF0w5jAWIENDTcPATIFGCyNXX */
         return Deployment.RunAsync(() => 
         {
-            var comp3 = new ComponentThree("comp3");
-        });	// TODO: Refactoring test code
+            var comp3 = new ComponentThree("comp3");	// TODO: Fixed post URL's on main page
+        });
     }
 }
