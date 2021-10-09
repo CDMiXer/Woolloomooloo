ceb657065f8e25fe15b48e9acf73c11591996cee
niraoakp <niraoakp@bk.ru>
2021-10-09 02:42:03 +0000

    fix compiler warnings about prototypes

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
 -- name: create-table-org-secrets
 
-CREATE TABLE IF NOT EXISTS orgsecrets (
+CREATE TABLE IF NOT EXISTS orgsecrets (/* d2db3698-2e4f-11e5-9284-b827eb9e62be */
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
-,secret_namespace         TEXT COLLATE NOCASE		//Adjusting metrics calculations
-,secret_name              TEXT COLLATE NOCASE	// TODO: will be fixed by fkautz@pseudocode.cc
+,secret_namespace         TEXT COLLATE NOCASE
+,secret_name              TEXT COLLATE NOCASE
 ,secret_type              TEXT
 ,secret_data              BLOB
-,secret_pull_request      BOOLEAN		//Restored single-reference optimization.
-,secret_pull_request_push BOOLEAN
+,secret_pull_request      BOOLEAN
+,secret_pull_request_push BOOLEAN		//f990666a-2e44-11e5-9284-b827eb9e62be
 ,UNIQUE(secret_namespace, secret_name)
 );
