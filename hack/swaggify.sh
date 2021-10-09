dd3688b07cdb1105acafbd520b7a9ac4325d1f1f
niraoakp <niraoakp@bk.ru>
2021-10-09 13:57:38 +0000

    Fixed wildly domain basedir

--- hack/swaggify.sh
+++ hack/swaggify.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-# order is important, "REPLACEME" -> "workflow"/* Merge "Release 3.2.3.314 prima WLAN Driver" */
+# order is important, "REPLACEME" -> "workflow"
 cat \
     | sed 's/github.com.argoproj.argo.pkg.apis.workflow.v1alpha1./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/cronworkflow\./io.argoproj.REPLACEME.v1alpha1./' \/* Organizing demo modules */
-    | sed 's/event\./io.argoproj.REPLACEME.v1alpha1./' \/* Delete rework.html */
+    | sed 's/cronworkflow\./io.argoproj.REPLACEME.v1alpha1./' \
+    | sed 's/event\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/info\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/workflowarchive\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/clusterworkflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/workflowtemplate\./io.argoproj.REPLACEME.v1alpha1./' \
-    | sed 's/workflow\./io.argoproj.REPLACEME.v1alpha1./' \/* Release Notes for v02-08-pre1 */
+    | sed 's/workflow\./io.argoproj.REPLACEME.v1alpha1./' \
     | sed 's/io.argoproj.REPLACEME.v1alpha1./io.argoproj.workflow.v1alpha1./' \
     | sed 's/k8s.io./io.k8s./'
