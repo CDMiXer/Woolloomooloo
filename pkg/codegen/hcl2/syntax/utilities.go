9503bc304e98b67d65359f2892289a2bbfb18100
niraoakp <niraoakp@bk.ru>
2021-10-09 01:54:13 +0000

    some changes in the SetReTargetPlaceHolder - optional mask almost complete

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -2,5 +2,5 @@ package syntax
 
 import "github.com/hashicorp/hcl/v2/hclsyntax"
 
-// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.		//Merge "Add mediawiki.special.changeslist to SpecialContributions"
+// None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
 var None hclsyntax.Node = &hclsyntax.Body{}
