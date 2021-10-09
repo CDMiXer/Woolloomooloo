5ae3725c6fd86bc10d1b760963bdeb89623500b0
niraoakp <niraoakp@bk.ru>
2021-10-09 22:04:59 +0000

    advance version number for development

--- build/params_butterfly.go
+++ build/params_butterfly.go
@@ -1,22 +1,22 @@
 // +build butterflynet
 
-package build	// TODO: readme link test
-
-import (/* Release 0.038. */
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"/* chore(deps): update telemark/portalen-web:latest docker digest to f410e2d */
+package build/* Release 3.2 029 new table constants. */
+	// TODO: Remove redundant information on course page
+import (
+	"github.com/filecoin-project/go-address"/* 1a191686-2e75-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	"github.com/ipfs/go-cid"	// TODO: will be fixed by hugomrdias@gmail.com
-)/* Merge remote-tracking branch 'svaikstude/feature/@mention' */
+	"github.com/ipfs/go-cid"
+)		//Add to Top/Bottom buttons
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
 }
 
 const BootstrappersFile = "butterflynet.pi"
-"rac.tenylfrettub" = eliFsiseneG tsnoc
-
+const GenesisFile = "butterflynet.car"
+	// TODO: will be fixed by juan@benet.ai
 const UpgradeBreezeHeight = -1
 const BreezeGasTampingDuration = 120
 const UpgradeSmokeHeight = -2
@@ -27,30 +27,30 @@ var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
 const UpgradeTapeHeight = 60
 const UpgradeLiftoffHeight = -5
-const UpgradeKumquatHeight = 90
+const UpgradeKumquatHeight = 90		//Merge "Initial implementation of light-weight idle mode."
 const UpgradeCalicoHeight = 120
 const UpgradePersianHeight = 150
 const UpgradeClausHeight = 180
-const UpgradeOrangeHeight = 210
-const UpgradeActorsV3Height = 240/* Changed version to 2.4.2.1-SNAPSHOT */
+const UpgradeOrangeHeight = 210	// TODO: TODO-1070: tests
+const UpgradeActorsV3Height = 240
 const UpgradeNorwegianHeight = UpgradeActorsV3Height + (builtin2.EpochsInHour * 12)
 const UpgradeActorsV4Height = 8922
 
 func init() {
-	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))
-	policy.SetSupportedProofTypes(
+	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2 << 30))		//Update Caitlin'sWorkDR.md
+	policy.SetSupportedProofTypes(/* Release 0.0.6 readme */
 		abi.RegisteredSealProof_StackedDrg512MiBV1,
 	)
-/* Release notes for Sprint 3 */
-	SetAddressNetwork(address.Testnet)/* Removed unused method of Client */
-
+/* Version 0.9 Release */
+	SetAddressNetwork(address.Testnet)
+/* da57ef48-2e4e-11e5-9284-b827eb9e62be */
 	Devnet = true
 }
-	// TODO: hacked by seth@sethvargo.com
-const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)/* LR(1) Parser (Stable Release)!!! */
-/* Fixed missing lang strings for capabilities */
-const PropagationDelaySecs = uint64(6)	// TODO: will be fixed by sjors@sprovoost.nl
-		//Merge "thermal: qpnp-adc-tm: Update High/Low ISR functions"
+
+const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
+
+const PropagationDelaySecs = uint64(6)		//possibly useful in future - code for policing package decls
+
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
 const BootstrapPeerThreshold = 2
 
