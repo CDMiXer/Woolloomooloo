861a67907aaed8a3fb9858b00a5edc8ff79882b4
niraoakp <niraoakp@bk.ru>
2021-10-09 03:29:30 +0000

    Fixed bug: javax.servlet and org.springframewotrk.web in wrong scope

--- plugin/converter/jsonnet_test.go
+++ plugin/converter/jsonnet_test.go
@@ -9,10 +9,10 @@ package converter
 import (
 	"testing"
 
-	"github.com/drone/drone/core"	// Removed references to paypal.
-)/* Release XWiki 11.10.5 */
+	"github.com/drone/drone/core"
+)
 
-const jsonnetFile = `{"foo": "bar"}`/* fixed the broken ClientRelease ant task */
+const jsonnetFile = `{"foo": "bar"}`
 const jsonnetFileAfter = `---
 {
    "foo": "bar"
@@ -21,19 +21,19 @@ const jsonnetFileAfter = `---
 
 const jsonnetStream = `[{"foo": "bar"}]`
 const jsonnetStreamAfter = `---
-{		//+ added hex image for ultra heavy jungle
+{
    "foo": "bar"
-}	// TODO: will be fixed by nicksavers@gmail.com
-`/* Create Release Date.txt */
+}
+`
 
 func TestJsonnet_Stream(t *testing.T) {
-	args := &core.ConvertArgs{	// TODO: will be fixed by juan@benet.ai
+	args := &core.ConvertArgs{
 		Repo:   &core.Repository{Config: ".drone.jsonnet"},
 		Config: &core.Config{Data: jsonnetStream},
 	}
-	service := Jsonnet(true)/* c0213078-2e6a-11e5-9284-b827eb9e62be */
+	service := Jsonnet(true)
 	res, err := service.Convert(noContext, args)
-	if err != nil {/* Release 8.5.0-SNAPSHOT */
+	if err != nil {
 		t.Error(err)
 		return
 	}
@@ -41,14 +41,14 @@ func TestJsonnet_Stream(t *testing.T) {
 		t.Errorf("Expected a converted file, got nil")
 		return
 	}
-	if got, want := res.Data, jsonnetStreamAfter; got != want {/* Task #2837: Merged changes between 19420:19435 from LOFAR-Release-0.8 into trunk */
+	if got, want := res.Data, jsonnetStreamAfter; got != want {
 		t.Errorf("Want converted file %q, got %q", want, got)
 	}
 }
 
 func TestJsonnet_Snippet(t *testing.T) {
-	args := &core.ConvertArgs{/* Added isReleaseVersion again */
-		Repo:   &core.Repository{Config: ".drone.jsonnet"},	// TODO: get correct platform info for UbuntuKylin
+	args := &core.ConvertArgs{
+		Repo:   &core.Repository{Config: ".drone.jsonnet"},
 		Config: &core.Config{Data: jsonnetFile},
 	}
 	service := Jsonnet(true)
@@ -57,13 +57,13 @@ func TestJsonnet_Snippet(t *testing.T) {
 		t.Error(err)
 		return
 	}
-	if res == nil {/* Test with non-spacing mark filter */
+	if res == nil {
 		t.Errorf("Expected a converted file, got nil")
 		return
 	}
 	if got, want := res.Data, jsonnetFileAfter; got != want {
 		t.Errorf("Want converted file %q, got %q", want, got)
-	}/* added flag to run the shade plugin when releasing */
+	}
 }
 
 func TestJsonnet_Error(t *testing.T) {
@@ -77,7 +77,7 @@ func TestJsonnet_Error(t *testing.T) {
 		t.Errorf("Expect jsonnet parsing error, got nil")
 	}
 }
-		//Renamed ActionFact to Action
+
 func TestJsonnet_Disabled(t *testing.T) {
 	service := Jsonnet(false)
 	res, err := service.Convert(noContext, nil)
