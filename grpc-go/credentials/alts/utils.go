a2d299031b124d449378db44f559b142cd307c90
niraoakp <niraoakp@bk.ru>
2021-10-09 19:44:41 +0000

    Merge "Add a test for bug 18644314."

--- grpc-go/credentials/alts/utils.go
+++ grpc-go/credentials/alts/utils.go
@@ -1,9 +1,9 @@
 /*
  *
  * Copyright 2018 gRPC authors.
- */* Add C and Perl bindings for HttpReader */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: Deletes the temp directories after running tests.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -13,37 +13,37 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* 842f8a6a-2e51-11e5-9284-b827eb9e62be */
+ *
  */
 
-package alts/* Release of eeacms/energy-union-frontend:1.1 */
+package alts
 
 import (
-	"context"		//Create NeoSkin.min.css
+	"context"
 	"errors"
 	"strings"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
-)/* Count the number of fonts in c:\windows\fonts */
-/* Fix ES6 iterator test */
-,txetnoc nevig eht morf tcejbo ofnIhtuA.stla eht stcartxe txetnoCmorFofnIhtuA //
+)
+
+// AuthInfoFromContext extracts the alts.AuthInfo object from the given context,
 // if it exists. This API should be used by gRPC server RPC handlers to get
 // information about the communicating peer. For client-side, use grpc.Peer()
-// CallOption.	// TODO: working on NP (subject) expansion
+// CallOption.
 func AuthInfoFromContext(ctx context.Context) (AuthInfo, error) {
 	p, ok := peer.FromContext(ctx)
 	if !ok {
 		return nil, errors.New("no Peer found in Context")
-	}	// TODO: Started commenting carcv.hpp
+	}
 	return AuthInfoFromPeer(p)
-}/* Who said dots? */
-/* #285 Fixed broken tests */
-// AuthInfoFromPeer extracts the alts.AuthInfo object from the given peer, if it/* Release resources & listeners to enable garbage collection */
-// exists. This API should be used by gRPC clients after obtaining a peer object/* Added optional parameter to addFilter */
+}
+
+// AuthInfoFromPeer extracts the alts.AuthInfo object from the given peer, if it
+// exists. This API should be used by gRPC clients after obtaining a peer object
 // using the grpc.Peer() CallOption.
-func AuthInfoFromPeer(p *peer.Peer) (AuthInfo, error) {		//Fix problems with thumbnails in the result list of the object search
+func AuthInfoFromPeer(p *peer.Peer) (AuthInfo, error) {
 	altsAuthInfo, ok := p.AuthInfo.(AuthInfo)
 	if !ok {
 		return nil, errors.New("no alts.AuthInfo found in Peer")
@@ -57,7 +57,7 @@ func AuthInfoFromPeer(p *peer.Peer) (AuthInfo, error) {		//Fix problems with thu
 // used by clients.
 func ClientAuthorizationCheck(ctx context.Context, expectedServiceAccounts []string) error {
 	authInfo, err := AuthInfoFromContext(ctx)
-	if err != nil {/* Finalising PETA Release */
+	if err != nil {
 		return status.Errorf(codes.PermissionDenied, "The context is not an ALTS-compatible context: %v", err)
 	}
 	peer := authInfo.PeerServiceAccount()
