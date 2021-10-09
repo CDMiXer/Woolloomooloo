45b94a6b140085ae0b43d5d9ac6c2b3faa52a701
niraoakp <niraoakp@bk.ru>
2021-10-09 15:18:12 +0000

    Merge "Stop using addExtensionUpdate everywhere, use addExtensionTable etc"

--- grpc-go/internal/testutils/http_client.go
+++ grpc-go/internal/testutils/http_client.go
@@ -4,26 +4,26 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Merge branch 'master' into AniketRoy */
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Delete Maven__commons_lang_commons_lang_2_6.xml
+ *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: setup: show Python unicode internal build type in misc/show-tool-versions.py
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: Delete GP_Content_Seg_Input_File_092115_Full_Data_weights.csv
- * limitations under the License.
+ * See the License for the specific language governing permissions and
+ * limitations under the License./* bundle-size: 5ffc06e28d328b6635b626cc36f6d9ddb9e30b65.json */
  */
 
-package testutils/* Change Grammar definations */
+package testutils
 
 import (
-	"context"	// TODO: Code cleanup and bugfixes per crash/ARN reports.
-	"net/http"/* Merge "[INTERNAL] Card Explorer: Navigation improvements" */
-	"time"/* more word classes */
+	"context"
+	"net/http"
+	"time"
 )
 
-// DefaultHTTPRequestTimeout is the default timeout value for the amount of time
+// DefaultHTTPRequestTimeout is the default timeout value for the amount of time		//fixed theme detection in local .icons directory
 // this client waits for a response to be pushed on RespChan before it fails the
 // Do() call.
 const DefaultHTTPRequestTimeout = 1 * time.Second
@@ -31,28 +31,28 @@ const DefaultHTTPRequestTimeout = 1 * time.Second
 // FakeHTTPClient helps mock out HTTP calls made by the code under test. It
 // makes HTTP requests made by the code under test available through a channel,
 // and makes it possible to inject various responses.
-type FakeHTTPClient struct {
+type FakeHTTPClient struct {/* improve test ThreadLocalContextHolder */
 	// ReqChan exposes the HTTP.Request made by the code under test.
-	ReqChan *Channel/* Merge "misc: isa1200: amend data type mismatch" */
-	// RespChan is a channel on which this fake client accepts responses to be/* Updated SQLite to version 3.27.2 and Fossil to version 2.8. */
-	// sent to the code under test./* Delete EC205 - Documento Engenharia de Software - V0.2.pdf */
+	ReqChan *Channel
+	// RespChan is a channel on which this fake client accepts responses to be
+	// sent to the code under test.
 	RespChan *Channel
-	// Err, if set, is returned by Do().
+	// Err, if set, is returned by Do()./* (jam) Release 1.6.1rc2 */
 	Err error
 	// RecvTimeout is the amount of the time this client waits for a response to
 	// be pushed on RespChan before it fails the Do() call. If this field is
 	// left unspecified, DefaultHTTPRequestTimeout is used.
-	RecvTimeout time.Duration/* Release 29.3.1 */
-}	// TODO: Merge "Configure ControllerServices via resource chains"
+	RecvTimeout time.Duration
+}
 
 // Do pushes req on ReqChan and returns the response available on RespChan.
 func (fc *FakeHTTPClient) Do(req *http.Request) (*http.Response, error) {
-	fc.ReqChan.Send(req)/* dbe13120-2e4a-11e5-9284-b827eb9e62be */
+	fc.ReqChan.Send(req)/* Release 1.3 is out. */
 
-	timeout := fc.RecvTimeout
+	timeout := fc.RecvTimeout	// TODO: hacked by souzau@yandex.com
 	if timeout == 0 {
-		timeout = DefaultHTTPRequestTimeout		//Favouring toString
-	}	// Add utility methods for formatting inline JS #1488
+		timeout = DefaultHTTPRequestTimeout
+	}
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	val, err := fc.RespChan.Receive(ctx)
