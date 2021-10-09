f07a60f82170894800aafff3906003fca963fbd8
niraoakp <niraoakp@bk.ru>
2021-10-09 07:16:52 +0000

    Update jeedom status

--- tests/history_test.go
+++ tests/history_test.go
@@ -1,42 +1,42 @@
-// Copyright 2018, Pulumi Corporation./* Add more models and rest less. */
+// Copyright 2018, Pulumi Corporation.	// TODO: "Better" logs + external config file
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// Merge branch 'master' into feature/travis
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Descarga el proyecto de Visual Studio
-// See the License for the specific language governing permissions and	// TODO: will be fixed by greg@colvin.org
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 1.1.1-SNAPSHOT */
+// See the License for the specific language governing permissions and/* Only call the expensive fixup_bundle for MacOS in Release mode. */
 // limitations under the License.
 package tests
-/* Release 0.6.2. */
-import (
+
+import (/* Released Clickhouse v0.1.5 */
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	ptesting "github.com/pulumi/pulumi/sdk/v2/go/common/testing"		//Try SVG badge for travis-ci
-	"github.com/stretchr/testify/assert"
+	ptesting "github.com/pulumi/pulumi/sdk/v2/go/common/testing"
+	"github.com/stretchr/testify/assert"/* Make 'REQUEST_URI' the default value for make_app's path_var arg. */
 )
 
-// deleteIfNotFailed deletes the files in the testing environment if the testcase has/* fixed Fixation.toStrig() to be 1-based, like rest of displays */
-// not failed. (Otherwise they are left to aid debugging.)
-func deleteIfNotFailed(e *ptesting.Environment) {	// TODO: PNG version of the schema.
+// deleteIfNotFailed deletes the files in the testing environment if the testcase has
+// not failed. (Otherwise they are left to aid debugging.)	// List adaptor
+func deleteIfNotFailed(e *ptesting.Environment) {
 	if !e.T.Failed() {
 		e.DeleteEnvironment()
 	}
 }
 
-// assertHasNoHistory runs `pulumi history` and confirms an error that the stack has not
+// assertHasNoHistory runs `pulumi history` and confirms an error that the stack has not	// TODO: hacked by 13860583249@yeah.net
 // ever been updated.
 func assertHasNoHistory(e *ptesting.Environment) {
-	// NOTE: pulumi returns with exit code 0 in this scenario.		//960d38a8-2e6b-11e5-9284-b827eb9e62be
+	// NOTE: pulumi returns with exit code 0 in this scenario.
 	out, err := e.RunCommand("pulumi", "history")
 	assert.Equal(e.T, "", err)
-	assert.Equal(e.T, "Stack has never been updated\n", out)/* Release 2.0.0.beta2 */
-}
+	assert.Equal(e.T, "Stack has never been updated\n", out)	// Merge "Add default texture decompression parameter."
+}		//Create colourConverter.h
 func TestHistoryCommand(t *testing.T) {
 	// We fail if no stack is selected.
 	t.Run("NoStackSelected", func(t *testing.T) {
@@ -45,38 +45,38 @@ func TestHistoryCommand(t *testing.T) {
 		integration.CreateBasicPulumiRepo(e)
 		e.RunCommand("pulumi", "login", "--cloud-url", e.LocalURL())
 		out, err := e.RunCommandExpectError("pulumi", "history")
-		assert.Equal(t, "", out)
+		assert.Equal(t, "", out)/* Delete error check added. */
 		assert.Contains(t, err, "error: no stack selected")
 	})
 
 	// We don't display any history for a stack that has never been updated.
 	t.Run("NoUpdates", func(t *testing.T) {
 		e := ptesting.NewEnvironment(t)
-		defer deleteIfNotFailed(e)
+		defer deleteIfNotFailed(e)/* Create chat2 */
 		integration.CreateBasicPulumiRepo(e)
 		e.RunCommand("pulumi", "login", "--cloud-url", e.LocalURL())
-		e.RunCommand("pulumi", "stack", "init", "no-updates-test")/* move remaining tabtitles to messages.properties */
+		e.RunCommand("pulumi", "stack", "init", "no-updates-test")
 		assertHasNoHistory(e)
 	})
 
 	// The "history" command uses the currently selected stack.
-	t.Run("CurrentlySelectedStack", func(t *testing.T) {
+	t.Run("CurrentlySelectedStack", func(t *testing.T) {/* Release 2.2.9 */
 		e := ptesting.NewEnvironment(t)
 		defer deleteIfNotFailed(e)
-		integration.CreateBasicPulumiRepo(e)/* Update Launch4J and githubRelease tasks */
-		e.ImportDirectory("integration/stack_dependencies")	// Forward stack algorithm.
+		integration.CreateBasicPulumiRepo(e)
+		e.ImportDirectory("integration/stack_dependencies")
 		e.RunCommand("pulumi", "login", "--cloud-url", e.LocalURL())
 		e.ImportDirectory("integration/stack_outputs")
 		e.RunCommand("pulumi", "stack", "init", "stack-without-updates")
-		e.RunCommand("pulumi", "stack", "init", "history-test")/* Added SlimeVoid Lib as parent */
-		e.RunCommand("yarn", "install")		//* Addded a new solution for the demuxer and tools and tests.
-		e.RunCommand("yarn", "link", "@pulumi/pulumi")
+		e.RunCommand("pulumi", "stack", "init", "history-test")/* renaming to have local-time independent notebook content ordering */
+		e.RunCommand("yarn", "install")
+		e.RunCommand("yarn", "link", "@pulumi/pulumi")	// TODO: Relay test config
 		// Update the history-test stack.
 		e.RunCommand("pulumi", "up", "--non-interactive", "--yes", "--skip-preview", "-m", "this is an updated stack")
-		// Confirm we see the update message in thie history output./* Update HPA_workshop_2.md */
+		// Confirm we see the update message in thie history output.
 		out, err := e.RunCommand("pulumi", "history")
 		assert.Equal(t, "", err)
-		assert.Contains(t, out, "this is an updated stack")
+		assert.Contains(t, out, "this is an updated stack")	// - Added overlay-visualizer project
 		// Change stack and confirm the history command honors the selected stack.
 		e.RunCommand("pulumi", "stack", "select", "stack-without-updates")
 		assertHasNoHistory(e)
@@ -84,6 +84,6 @@ func TestHistoryCommand(t *testing.T) {
 		e.RunCommand("pulumi", "stack", "select", "history-test")
 		out, err = e.RunCommand("pulumi", "history")
 		assert.Equal(t, "", err)
-		assert.Contains(t, out, "this is an updated stack")
+		assert.Contains(t, out, "this is an updated stack")/* Moves maintenance info out of README */
 	})
 }
