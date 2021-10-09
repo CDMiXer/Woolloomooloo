19ce3af7b18ba7787c8703e064f083ab8978a6e6
niraoakp <niraoakp@bk.ru>
2021-10-09 07:02:23 +0000

    Add Boolean Builder

--- tests/integration/aliases/nodejs/adopt_into_component/step1/index.ts
+++ tests/integration/aliases/nodejs/adopt_into_component/step1/index.ts
@@ -3,48 +3,48 @@
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Release of XWiki 11.10.13 */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Resource", name, {}, opts);
     }
 }
 
 // Scenario #2 - adopt a resource into a component
 class Component extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* b2b0678a-2e4f-11e5-9284-b827eb9e62be */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:Component", name, {}, opts);
     }
-}		//Add bootcheck for kfreebsd-x86_64
+}
 
 const res2 = new Resource("res2");
 const comp2 = new Component("comp2");
-	// c29456a0-2e56-11e5-9284-b827eb9e62be
-// Scenario 3: adopt this resource into a new parent.		//527c79d2-2e5c-11e5-9284-b827eb9e62be
-class Component2 extends pulumi.ComponentResource {	// TODO: hacked by ligi@ligi.de
+
+// Scenario 3: adopt this resource into a new parent.
+class Component2 extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Component2", name, {}, opts);/* moving sources files into Sources */
+        super("my:module:Component2", name, {}, opts);
     }
 }
 new Component2("unparented");
 
 // Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
-// in the next step to be parented by this.  Make sure that works with an opts with no parent/* Release for v1.4.0. */
+// in the next step to be parented by this.  Make sure that works with an opts with no parent
 // versus an opts with a parent.
 
 class Component3 extends pulumi.ComponentResource {
-    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {/* Merge "Removing test_duration variable from bench/consumer.py" */
-        super("my:module:Component3", name, {}, opts);/* Merge branch 'master' into sjmudd/add-queue-metrics */
+    constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
+        super("my:module:Component3", name, {}, opts);
         new Component2(name + "-child", opts);
     }
-}	// TODO: Set a value
+}
 
 new Component3("parentedbystack");
 new Component3("parentedbycomponent", { parent: comp2 });
-/* Release 3.6.3 */
+
 // Scenario 5: Allow multiple aliases to the same resource.
 class Component4 extends pulumi.ComponentResource {
     constructor(name: string, opts: pulumi.ComponentResourceOptions = {}) {
         super("my:module:Component4", name, {});
-    }		//[Adds] the ability to invite someone who doesn’t have an account.
-}/* modified menue to gain space for smaller displays */
+    }
+}
 
 new Component4("duplicateAliases", { parent: comp2 });
