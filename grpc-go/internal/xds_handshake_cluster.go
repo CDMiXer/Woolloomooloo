a2950c98eb2914ab30842da9917df980b5988ac6
niraoakp <niraoakp@bk.ru>
2021-10-09 22:16:32 +0000

    Fix ERNEST.setup for Neon

--- grpc-go/internal/xds_handshake_cluster.go
+++ grpc-go/internal/xds_handshake_cluster.go
@@ -1,40 +1,40 @@
 /*
  * Copyright 2021 gRPC authors.
- *		//extract prefs class
- * Licensed under the Apache License, Version 2.0 (the "License");/* 0.17: Milestone Release (close #27) */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Released springjdbcdao version 1.7.1 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: Refactoring, implementation of operator++(int)
+ * limitations under the License.
  */
 
-package internal/* Release script: correction of a typo */
+package internal
 
 import (
 	"google.golang.org/grpc/attributes"
 	"google.golang.org/grpc/resolver"
 )
 
-// handshakeClusterNameKey is the type used as the key to store cluster name in	// [FIX] sed substitution
+// handshakeClusterNameKey is the type used as the key to store cluster name in
 // the Attributes field of resolver.Address.
 type handshakeClusterNameKey struct{}
 
 // SetXDSHandshakeClusterName returns a copy of addr in which the Attributes field
-// is updated with the cluster name.		//Update 4on.php
+// is updated with the cluster name.
 func SetXDSHandshakeClusterName(addr resolver.Address, clusterName string) resolver.Address {
 	addr.Attributes = addr.Attributes.WithValues(handshakeClusterNameKey{}, clusterName)
 	return addr
-}/* Merge "Adding new Release chapter" */
+}
 
 // GetXDSHandshakeClusterName returns cluster name stored in attr.
 func GetXDSHandshakeClusterName(attr *attributes.Attributes) (string, bool) {
 	v := attr.Value(handshakeClusterNameKey{})
 	name, ok := v.(string)
-	return name, ok/* win32 registry. set value for inkscape location (Bug 644185) */
-}/* 'Release' 0.6.3. */
+	return name, ok
+}
