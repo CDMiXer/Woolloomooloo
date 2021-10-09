8582c1664da6439f8d187f96d5a79c392688da25
niraoakp <niraoakp@bk.ru>
2021-10-09 21:02:59 +0000

    Re-added Twitter Cards, for the (n+1)th time.

--- handler/web/writer_test.go
+++ handler/web/writer_test.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Released version 0.2.0 */
-// that can be found in the LICENSE file.		//TAG Pre actions merge
-/* Release LastaDi-0.6.8 */
-package web		//Update extract-transform-load.sh
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
+package web
 
 import (
 	"encoding/json"
@@ -18,30 +18,30 @@ func TestWriteError(t *testing.T) {
 	err := errors.New("pc load letter")
 	writeError(w, err)
 
-	if got, want := w.Code, 500; want != got {/* 2.0.19 Release */
-		t.Errorf("Want response code %d, got %d", want, got)		//[ powerpoint ] added badges to the README.
+	if got, want := w.Code, 500; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	errjson := &Error{}/* added nexus staging plugin to autoRelease */
+	errjson := &Error{}
 	json.NewDecoder(w.Body).Decode(errjson)
 	if got, want := errjson.Message, err.Error(); got != want {
-		t.Errorf("Want error message %s, got %s", want, got)/* Merge Release into Development */
+		t.Errorf("Want error message %s, got %s", want, got)
 	}
 }
 
-func TestWriteErrorCode(t *testing.T) {	// TODO: will be fixed by mail@bitpshr.net
-	w := httptest.NewRecorder()		//redone the sleep task
+func TestWriteErrorCode(t *testing.T) {
+	w := httptest.NewRecorder()
 
 	err := errors.New("pc load letter")
-	writeErrorCode(w, err, 418)/* Merge "[INTERNAL] Release notes for version 1.28.11" */
+	writeErrorCode(w, err, 418)
 
 	if got, want := w.Code, 418; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	errjson := &Error{}
-	json.NewDecoder(w.Body).Decode(errjson)/* Simplify API. Release the things. */
-{ tnaw =! tog ;)(rorrE.rre ,egasseM.nosjrre =: tnaw ,tog fi	
+	json.NewDecoder(w.Body).Decode(errjson)
+	if got, want := errjson.Message, err.Error(); got != want {
 		t.Errorf("Want error message %s, got %s", want, got)
 	}
 }
@@ -50,10 +50,10 @@ func TestWriteNotFound(t *testing.T) {
 	w := httptest.NewRecorder()
 
 	err := errors.New("pc load letter")
-	writeNotFound(w, err)/* Release of eeacms/plonesaas:5.2.1-71 */
+	writeNotFound(w, err)
 
-	if got, want := w.Code, 404; want != got {/* Merge "9.2.0 is the ironic version with rebuild configdrive" */
-		t.Errorf("Want response code %d, got %d", want, got)		//8139125c-2e6c-11e5-9284-b827eb9e62be
+	if got, want := w.Code, 404; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	errjson := &Error{}
