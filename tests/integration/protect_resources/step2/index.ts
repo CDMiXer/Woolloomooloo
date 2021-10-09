d1873c9a81d8d26446d9afa76c21cd0f0ceab7f0
niraoakp <niraoakp@bk.ru>
2021-10-09 09:44:31 +0000

    Create selenium.xml

--- tests/integration/protect_resources/step2/index.ts
+++ tests/integration/protect_resources/step2/index.ts
@@ -1,6 +1,6 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Comment out the JRE
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Resource } from "./resource";
 
 // Now, simply update the resource; this should work fine:
-let a = new Resource("eternal", { state: 2 }, { protect: true });	// TODO: hacked by 13860583249@yeah.net
+let a = new Resource("eternal", { state: 2 }, { protect: true });
