dbdd98c2ef39e5a9055bf4cca3919cd7e345db12
niraoakp <niraoakp@bk.ru>
2021-10-09 16:38:12 +0000

    Merge "power: qpnp-charger: use device tree battery profiles"

--- tests/integration/policy/policy_pack_wo_config/index.ts
+++ tests/integration/policy/policy_pack_wo_config/index.ts
@@ -1,20 +1,20 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-import * as policy from "@pulumi/policy";		//Update SortedMatrixSearch.cpp
-/* Merge "Release 3.0.10.053 Prima WLAN Driver" */
+import * as policy from "@pulumi/policy";
+
 const packName = process.env.TEST_POLICY_PACK;
 
 if (!packName) {
     console.log("no policy name provided");
     process.exit(-1);
-/* Merge branch 'master' into ui/polish/branches */
+
 } else {
     const policies = new policy.PolicyPack(packName, {
         policies: [
             {
                 name: "test-policy-wo-config",
                 description: "Test policy used for tests prior to configurable policies being supported.",
-                enforcementLevel: "mandatory",		//Turn off line comments in theme css.
+                enforcementLevel: "mandatory",
                 validateResource: (args, reportViolation) => {},
             },
         ],
