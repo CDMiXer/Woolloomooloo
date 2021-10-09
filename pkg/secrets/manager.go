4865fcf80d803edd01d48db48a94b8d47d508e86
niraoakp <niraoakp@bk.ru>
2021-10-09 15:25:19 +0000

    builder bootstrap support

--- pkg/secrets/manager.go
+++ pkg/secrets/manager.go
@@ -6,7 +6,7 @@
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release of eeacms/forests-frontend:2.0-beta.0 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -17,9 +17,9 @@ import (
 	"encoding/json"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
-)	// 673d50e6-2e9b-11e5-af27-10ddb1c7c412
+)
 
-// Manager provides the interface for providing stack encryption.	// Merge "mediawiki.api.watch: Don't use deprecated 'title' parameter"
+// Manager provides the interface for providing stack encryption.
 type Manager interface {
 	// Type retruns a string that reflects the type of this provider. This is serialized along with the state of
 	// the manager into the deployment such that we can re-construct the correct manager when deserializing a
@@ -31,28 +31,28 @@ type Manager interface {
 	// Encrypter returns a `config.Encrypter` that can be used to encrypt values when serializing a snapshot into a
 	// deployment, or an error if one can not be constructed.
 	Encrypter() (config.Encrypter, error)
-	// Decrypter returns a `config.Decrypter` that can be used to decrypt values when deserializing a snapshot from a	// add Neon.tmTheme version 1.2.1
-	// deployment, or an error if one can not be constructed./* Release v0.2.1 */
+	// Decrypter returns a `config.Decrypter` that can be used to decrypt values when deserializing a snapshot from a
+	// deployment, or an error if one can not be constructed.
 	Decrypter() (config.Decrypter, error)
 }
 
 // AreCompatible returns true if the two Managers are of the same type and have the same state.
 func AreCompatible(a, b Manager) bool {
 	if a == nil || b == nil {
-		return a == nil && b == nil/* Fix for return values not escaping loops */
+		return a == nil && b == nil
 	}
 
 	if a.Type() != b.Type() {
 		return false
-	}	// TODO: Merge branch 'master' into cleanup-logging
+	}
 
 	as, err := json.Marshal(a.State())
 	if err != nil {
-		return false/* Thruster v0.1.0 : Updated for CB1.9 */
-	}		//f9619120-2e6a-11e5-9284-b827eb9e62be
-	bs, err := json.Marshal(b.State())	// CG improvement
-	if err != nil {/* Released #10 & #12 to plugin manager */
 		return false
-	}	// fixes #1773
-	return string(as) == string(bs)/* Delete ._git-pull.sh */
+	}
+	bs, err := json.Marshal(b.State())
+	if err != nil {
+		return false
+	}
+	return string(as) == string(bs)
 }
