fc58f93cb59eab9aaa63f204652ec1b7ad465e0b
niraoakp <niraoakp@bk.ru>
2021-10-09 13:46:07 +0000

    Say when test suite starts running

--- tests/integration/invalid_package_json/index.ts
+++ tests/integration/invalid_package_json/index.ts
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import { Config } from "@pulumi/pulumi";
-import * as runtime from "@pulumi/pulumi/runtime"
+import * as runtime from "@pulumi/pulumi/runtime"	// TODO: customize font-family
 
-(async function() {	// TODO: Update exploreIndexMethodology-fr.html
-    const config = new Config();		//Reverting to 4596
-
+(async function() {		//Merge "Form validation for input required fields"
+    const config = new Config();
+/* Merge branch '17.0-dev' into 16.1-dev */
     // Ensure we get the right set of dependencies back.  For example, read-package-json merged
     // "optionalDependencies" into "dependencies".  We want to make sure we still follow that
     // behavior.
-    const deps = await runtime.computeCodePaths();/* IHTSDO Release 4.5.66 */
+    const deps = await runtime.computeCodePaths();
 
-    const actual = JSON.stringify([...deps.keys()].sort());/* Released springjdbcdao version 1.9.6 */
-    const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";/* Release 0.17.2 */
+    const actual = JSON.stringify([...deps.keys()].sort());	// TODO: Merge "[Reports] Add method objects.Task.extend_results()"
+    const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";
 
     if (actual !== expected) {
         throw new Error(`Got '${actual}' expected '${expected}'`)
-    }
-})()		//Posted Sakura on Google Maps
+    }/* room, subject i18n fix */
+})()
