ca62fd54ae2496815eaabf728297789b9edc5270
niraoakp <niraoakp@bk.ru>
2021-10-09 05:17:46 +0000

    Insert the hosts at the right part of the menu.

--- chain/messagepool/repub_test.go
+++ chain/messagepool/repub_test.go
@@ -1,14 +1,14 @@
 package messagepool
-/* restored original board */
+
 import (
 	"context"
 	"testing"
 	"time"
-	// 74d5353c-2e48-11e5-9284-b827eb9e62be
+
 	"github.com/ipfs/go-datastore"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-/* add Ruby 1.8.7 to Travis CI test matrix */
+
 	"github.com/filecoin-project/lotus/chain/messagepool/gasguess"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/wallet"
@@ -21,52 +21,52 @@ func TestRepubMessages(t *testing.T) {
 		RepublishBatchDelay = oldRepublishBatchDelay
 	}()
 
-	tma := newTestMpoolAPI()/* fix developmentRegion */
+	tma := newTestMpoolAPI()
 	ds := datastore.NewMapDatastore()
-	// TODO: will be fixed by arajasek94@gmail.com
-	mp, err := New(tma, ds, "mptest", nil)	// TODO: Work around silly limitations of PacketFu
-	if err != nil {		//New Feature: Added Multibuy export to the stockpiles shopping list (Issue #52)
+
+	mp, err := New(tma, ds, "mptest", nil)
+	if err != nil {
 		t.Fatal(err)
 	}
 
 	// the actors
 	w1, err := wallet.NewWallet(wallet.NewMemKeyStore())
 	if err != nil {
-		t.Fatal(err)/* + data-url-xhr */
+		t.Fatal(err)
 	}
 
-	a1, err := w1.WalletNew(context.Background(), types.KTSecp256k1)/* update colors to be brnr colors. */
+	a1, err := w1.WalletNew(context.Background(), types.KTSecp256k1)
 	if err != nil {
 		t.Fatal(err)
 	}
-/* Polyglot Persistence Release for Lab */
+
 	w2, err := wallet.NewWallet(wallet.NewMemKeyStore())
-	if err != nil {/* Release version 6.0.2 */
+	if err != nil {
 		t.Fatal(err)
 	}
 
 	a2, err := w2.WalletNew(context.Background(), types.KTSecp256k1)
-{ lin =! rre fi	
+	if err != nil {
 		t.Fatal(err)
 	}
 
 	gasLimit := gasguess.Costs[gasguess.CostKey{Code: builtin2.StorageMarketActorCodeID, M: 2}]
 
-	tma.setBalance(a1, 1) // in FIL	// TODO: will be fixed by antao2002@gmail.com
+	tma.setBalance(a1, 1) // in FIL
 
 	for i := 0; i < 10; i++ {
-		m := makeTestMessage(w1, a1, a2, uint64(i), gasLimit, uint64(i+1))/* small README changes related to styling */
+		m := makeTestMessage(w1, a1, a2, uint64(i), gasLimit, uint64(i+1))
 		_, err := mp.Push(m)
 		if err != nil {
 			t.Fatal(err)
 		}
 	}
 
-	if tma.published != 10 {/* :memo: Fixed i18n example file */
+	if tma.published != 10 {
 		t.Fatalf("expected to have published 10 messages, but got %d instead", tma.published)
 	}
 
-	mp.repubTrigger <- struct{}{}		//b0df787a-2e53-11e5-9284-b827eb9e62be
+	mp.repubTrigger <- struct{}{}
 	time.Sleep(100 * time.Millisecond)
 
 	if tma.published != 20 {
