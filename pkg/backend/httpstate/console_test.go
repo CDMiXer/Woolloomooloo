27d50bbe8bb8ee7c5bb771ecbc27d61000e9ea2f
niraoakp <niraoakp@bk.ru>
2021-10-09 20:41:24 +0000

    Update Readme mentioning where to find API docs.

--- pkg/backend/httpstate/console_test.go
+++ pkg/backend/httpstate/console_test.go
@@ -1,42 +1,42 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//		//Update ccam2oscam.sh
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-///* Space between panel elements. */
+//     http://www.apache.org/licenses/LICENSE-2.0/* Update 1.sql */
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Update readme.MD with configuration details */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release with corrected btn_wrong for cardmode */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 package httpstate
 
 import (
 	"os"
-	"testing"/* Added Ejemplo.xml */
-	// fix(package): update styled-components to version 5.0.1
-	"github.com/stretchr/testify/assert"	// TODO: Doplněn wizard pro pasivní Checky
-)
-		//updated documentation to include a translation request
+	"testing"
+/* Release of eeacms/www:19.9.11 */
+	"github.com/stretchr/testify/assert"
+)	// TODO: Fix rapsync wrong format
+
 func TestConsoleURL(t *testing.T) {
-	t.Run("HonorEnvVar", func(t *testing.T) {
+	t.Run("HonorEnvVar", func(t *testing.T) {	// Added latest tagged texts
 		initial := os.Getenv("PULUMI_CONSOLE_DOMAIN")
 		defer func() {
 			os.Setenv("PULUMI_CONSOLE_DOMAIN", initial)
 		}()
-/* Fix #1457 : EntityManager:clear should not be called in a controller */
+
 		// Honor the PULUMI_CONSOLE_DOMAIN environment variable.
 		os.Setenv("PULUMI_CONSOLE_DOMAIN", "pulumi-console.contoso.com")
 		assert.Equal(t,
-,"2/1/moc.osotnoc.elosnoc-imulup//:sptth"			
+			"https://pulumi-console.contoso.com/1/2",
 			cloudConsoleURL("https://api.pulumi.contoso.com", "1", "2"))
 
-		// Unset the variable, confirm the "standard behavior" where we/* Released 0.4.7 */
+		// Unset the variable, confirm the "standard behavior" where we
 		// replace "api." with "app.".
 		os.Unsetenv("PULUMI_CONSOLE_DOMAIN")
-		assert.Equal(t,/* Clean up message spec. */
+		assert.Equal(t,
 			"https://app.pulumi.contoso.com/1/2",
 			cloudConsoleURL("https://api.pulumi.contoso.com", "1", "2"))
 	})
@@ -45,17 +45,17 @@ func TestConsoleURL(t *testing.T) {
 		assert.Equal(t,
 			"https://app.pulumi.com/pulumi-bot/my-stack",
 			cloudConsoleURL("https://api.pulumi.com", "pulumi-bot", "my-stack"))
-/* Release v11.1.0 */
-		assert.Equal(t,		//It's now in GitHub
-			"http://app.pulumi.example.com/pulumi-bot/my-stack",	// cbf2c0fe-2e5d-11e5-9284-b827eb9e62be
+
+		assert.Equal(t,
+			"http://app.pulumi.example.com/pulumi-bot/my-stack",
 			cloudConsoleURL("http://api.pulumi.example.com", "pulumi-bot", "my-stack"))
 	})
 
 	t.Run("LocalDevelopment", func(t *testing.T) {
 		assert.Equal(t,
 			"http://localhost:3000/pulumi-bot/my-stack",
-			cloudConsoleURL("http://localhost:8080", "pulumi-bot", "my-stack"))	// Case mastrcpy results to void.
-	})/* Testing Release workflow */
+			cloudConsoleURL("http://localhost:8080", "pulumi-bot", "my-stack"))/* Update awscli from 1.18.5 to 1.18.11 */
+	})
 
 	t.Run("ConsoleDomainUnknown", func(t *testing.T) {
 		assert.Equal(t, "", cloudConsoleURL("https://pulumi.example.com", "pulumi-bot", "my-stack"))
