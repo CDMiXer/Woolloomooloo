eb2a3d2461b9a63df0cc86e7f6003de5137071fb
niraoakp <niraoakp@bk.ru>
2021-10-09 00:33:03 +0000

    Delete spotify.py

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,27 +1,27 @@
-package test	// TODO: will be fixed by caojiaoyue@protonmail.com
-
-import (	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+package test
+/* Adapted some namepspaces. */
+import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/crypto"
-	"github.com/filecoin-project/lotus/chain/types"/* commands: actually implement --closed for topological heads */
-	"github.com/ipfs/go-cid"		//Fixed #19 #108 #114
+	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/ipfs/go-cid"
 )
 
 var dummyCid cid.Cid
 
 func init() {
-	dummyCid, _ = cid.Parse("bafkqaaa")		//tiny update for styles
+	dummyCid, _ = cid.Parse("bafkqaaa")/* added Store::create() */
 }
 
-func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {	// Create levels.rb
+func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
 	return types.NewTipSet([]*types.BlockHeader{{
 		Miner:                 minerAddr,
 		Height:                5,
 		ParentStateRoot:       dummyCid,
-,diCymmud              :segasseM		
+		Messages:              dummyCid,
 		ParentMessageReceipts: dummyCid,
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
-		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},/* Removed as not used anymore */
+		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
 		Timestamp:             timestamp,
-	}})
+	}})	// TODO: Yet another
 }
