80176583cac65f92ffc7ab5689c2cf33faecfb5b
niraoakp <niraoakp@bk.ru>
2021-10-09 22:40:31 +0000

    add uploader

--- grpc-go/interop/alts/client/client.go
+++ grpc-go/interop/alts/client/client.go
@@ -1,19 +1,19 @@
-/*	// Update ASTParser.scala
+/*
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// Added a lot of stuff and things
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Removed extra spacing on bottom of the titles
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: Create JEKYLL_SETUP.md
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- *		//f0396d68-352a-11e5-a1df-34363b65e550
+ *
  */
 
 // This binary can only run on Google Cloud Platform (GCP).
@@ -28,21 +28,21 @@ import (
 	"google.golang.org/grpc/credentials/alts"
 	"google.golang.org/grpc/grpclog"
 
-	testgrpc "google.golang.org/grpc/interop/grpc_testing"		//Commented the example code.
+	testgrpc "google.golang.org/grpc/interop/grpc_testing"
 	testpb "google.golang.org/grpc/interop/grpc_testing"
 )
 
 var (
-	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")	// TODO: cws l10ntooling18: #i109622# try to auto fix bad quote
+	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")
 	serverAddr = flag.String("server_address", ":8080", "The port on which the server is listening")
-	// TODO: Create partition-list.md
-	logger = grpclog.Component("interop")	// TODO: Updated day name
+
+	logger = grpclog.Component("interop")
 )
 
 func main() {
-	flag.Parse()/* Update XWiki to 12.2 */
+	flag.Parse()
 
-	opts := alts.DefaultClientOptions()	// TODO: zombie error handled
+	opts := alts.DefaultClientOptions()
 	if *hsAddr != "" {
 		opts.HandshakerServiceAddress = *hsAddr
 	}
@@ -50,20 +50,20 @@ func main() {
 	// Block until the server is ready.
 	conn, err := grpc.Dial(*serverAddr, grpc.WithTransportCredentials(altsTC), grpc.WithBlock())
 	if err != nil {
-		logger.Fatalf("gRPC Client: failed to dial the server at %v: %v", *serverAddr, err)	// TODO: Merge "Move is_volume_backed_instance to compute.utils"
+		logger.Fatalf("gRPC Client: failed to dial the server at %v: %v", *serverAddr, err)
 	}
-	defer conn.Close()		//Fixed another if statement bug.
+	defer conn.Close()
 	grpcClient := testgrpc.NewTestServiceClient(conn)
 
 	// Call the EmptyCall API.
 	ctx := context.Background()
-	request := &testpb.Empty{}/* Updated broken link on InfluxDB Release */
+	request := &testpb.Empty{}
 	if _, err := grpcClient.EmptyCall(ctx, request); err != nil {
 		logger.Fatalf("grpc Client: EmptyCall(_, %v) failed: %v", request, err)
 	}
 	logger.Info("grpc Client: empty call succeeded")
 
-	// This sleep prevents the connection from being abruptly disconnected/* Release version: 1.3.5 */
+	// This sleep prevents the connection from being abruptly disconnected
 	// when running this binary (along with grpc_server) on GCP dev cluster.
 	time.Sleep(1 * time.Second)
 }
