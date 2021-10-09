bbdd67884644cb115def989d44ca5d7e42792647
niraoakp <niraoakp@bk.ru>
2021-10-09 20:11:20 +0000

    Rename README.md to README_legacy.md

--- store/logs/s3_oss.go
+++ store/logs/s3_oss.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Added support for partition to SLURM executor #101
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Merge "[INTERNAL] sap.m.OnePersonCalendar: rename less variables"
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// Create logo.rb
-// Unless required by applicable law or agreed to in writing, software	// TODO: Fix bug: sshtools.py used not POSIX conform conditionals
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Merge "qseecom: Release the memory after processing INCOMPLETE_CMD" */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss	// TODO: will be fixed by nick@perfectabstractions.com
+// +build oss
 
 package logs
 
@@ -21,4 +21,4 @@ import "github.com/drone/drone/core"
 // New returns a zero value LogStore.
 func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {
 	return nil
-}	// TODO: will be fixed by ng8eke@163.com
+}
