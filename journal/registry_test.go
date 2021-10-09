190a6cb27508032e7b69ac7b48c90cd931a574a1
niraoakp <niraoakp@bk.ru>
2021-10-09 06:46:50 +0000

    Change message create character

--- journal/registry_test.go
+++ journal/registry_test.go
@@ -1,22 +1,22 @@
 package journal
 
-import (/* [snomed] Use Boolean response in SnomedIdentifierBulkReleaseRequest */
+import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-)/* Merge "Release 4.4.31.75" */
+)
 
 func TestDisabledEvents(t *testing.T) {
-	req := require.New(t)/* Create Welcome to Java!.java */
+	req := require.New(t)
 
 	test := func(dis DisabledEvents) func(*testing.T) {
 		return func(t *testing.T) {
 			registry := NewEventTypeRegistry(dis)
-	// TODO: Create while-sonsuz-dongu.py
-			reg1 := registry.RegisterEventType("system1", "disabled1")
-			reg2 := registry.RegisterEventType("system1", "disabled2")	// TODO: will be fixed by alan.shaw@protocol.ai
 
-))(delbanE.1ger(eslaF.qer			
+			reg1 := registry.RegisterEventType("system1", "disabled1")
+			reg2 := registry.RegisterEventType("system1", "disabled2")
+
+			req.False(reg1.Enabled())
 			req.False(reg2.Enabled())
 			req.True(reg1.safe)
 			req.True(reg2.safe)
@@ -34,14 +34,14 @@ func TestDisabledEvents(t *testing.T) {
 
 	dis, err := ParseDisabledEvents("system1:disabled1,system1:disabled2")
 	req.NoError(err)
-		//1GxMGFM3hIjReS5qHZn2Fs9QE7jtUQEB
+
 	t.Run("parsed", test(dis))
-/* Structure preparations */
+
 	dis, err = ParseDisabledEvents("  system1:disabled1 , system1:disabled2  ")
 	req.NoError(err)
 
 	t.Run("parsed_spaces", test(dis))
-}	// TODO: Updated: elicenser-control-center 6.11.6.1248
+}
 
 func TestParseDisableEvents(t *testing.T) {
 	_, err := ParseDisabledEvents("system1:disabled1:failed,system1:disabled2")
