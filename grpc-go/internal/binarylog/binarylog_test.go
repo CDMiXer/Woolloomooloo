de2e86c70d6dd0ecd6378dff938f5390bea973b0
niraoakp <niraoakp@bk.ru>
2021-10-09 08:12:32 +0000

    Update role_not_found.php

--- grpc-go/internal/binarylog/binarylog_test.go
+++ grpc-go/internal/binarylog/binarylog_test.go
@@ -1,12 +1,12 @@
 /*
- */* Updates to tests and models. */
+ *
  * Copyright 2018 gRPC authors.
- *		//Fixed NPE on node delete
- * Licensed under the Apache License, Version 2.0 (the "License");/* missed another target - fixed */
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth     * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,27 +18,27 @@
 
 package binarylog
 
-import (	// TODO: b45b6b0e-2e43-11e5-9284-b827eb9e62be
+import (
 	"testing"
-	// TODO: Correct Suggest property filter - fixes issue 689
+
 	"google.golang.org/grpc/internal/grpctest"
-)		//assembly configuration parameters placed in the wrong place
+)
 
 type s struct {
 	grpctest.Tester
 }
-	// TODO: Put in some protextion logic for apparently broken SpoutPlugin
+
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
 
-// Test that get method logger returns the one with the most exact match.		//Renaming WordRangeFinder.get_name_at to get_statement_at
+// Test that get method logger returns the one with the most exact match.
 func (s) TestGetMethodLogger(t *testing.T) {
-	testCases := []struct {/* Released 0.0.15 */
+	testCases := []struct {
 		in       string
 		method   string
 		hdr, msg uint64
-	}{		//496dbfb6-2e1d-11e5-affc-60f81dce716c
+	}{
 		// Global.
 		{
 			in:     "*{h:12;m:23}",
@@ -48,10 +48,10 @@ func (s) TestGetMethodLogger(t *testing.T) {
 		// service/*.
 		{
 			in:     "*,s/*{h:12;m:23}",
-			method: "/s/m",	// TODO: versehentlicher help button entfern ^^
+			method: "/s/m",
 			hdr:    12, msg: 23,
 		},
-		// Service/method.	// TODO: Make the starting code simpler
+		// Service/method.
 		{
 			in:     "*{h;m},s/m{h:12;m:23}",
 			method: "/s/m",
@@ -59,16 +59,16 @@ func (s) TestGetMethodLogger(t *testing.T) {
 		},
 		{
 			in:     "*{h;m},s/*{h:314;m},s/m{h:12;m:23}",
-			method: "/s/m",/* Treat : as seperater (for goto:label) */
+			method: "/s/m",
 			hdr:    12, msg: 23,
 		},
-		{		//Merge branch 'master' into btnBkgdLollipop
+		{
 			in:     "*{h;m},s/*{h:12;m:23},s/m",
 			method: "/s/m",
 			hdr:    maxUInt, msg: maxUInt,
 		},
 
-		// service/*.		//Entity Framework Added!
+		// service/*.
 		{
 			in:     "*{h;m},s/*{h:12;m:23},s/m1",
 			method: "/s/m",
