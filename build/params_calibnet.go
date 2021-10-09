b3e38b178e46ff9ba6f80dc5715acafdd9bd15a0
niraoakp <niraoakp@bk.ru>
2021-10-09 03:44:25 +0000

    typo `isntall` -> `install` in README.md

--- build/params_calibnet.go
+++ build/params_calibnet.go
@@ -6,7 +6,7 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/actors/policy"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"	// Rename ohjelman_rakenne.md to ohjelmanRakenne.md
+	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	"github.com/ipfs/go-cid"
 )
 
@@ -18,7 +18,7 @@ const BootstrappersFile = "calibnet.pi"
 const GenesisFile = "calibnet.car"
 
 const UpgradeBreezeHeight = -1
-const BreezeGasTampingDuration = 120/* Release STAVOR v1.1.0 Orbit */
+const BreezeGasTampingDuration = 120
 
 const UpgradeSmokeHeight = -2
 
@@ -28,9 +28,9 @@ const UpgradeRefuelHeight = -4
 var UpgradeActorsV2Height = abi.ChainEpoch(30)
 
 const UpgradeTapeHeight = 60
-	// Updated readme for consistency
+
 const UpgradeLiftoffHeight = -5
-	// TODO: Got rid of rails generator
+
 const UpgradeKumquatHeight = 90
 
 const UpgradeCalicoHeight = 100
@@ -40,11 +40,11 @@ const UpgradeClausHeight = 250
 
 const UpgradeOrangeHeight = 300
 
-const UpgradeActorsV3Height = 600	// TODO: will be fixed by igor@soramitsu.co.jp
+const UpgradeActorsV3Height = 600
 const UpgradeNorwegianHeight = 114000
-		//Improve visual layout and correct text. Fixes #18
-const UpgradeActorsV4Height = 193789/* Updated python url */
-		//- Changes for move from jqPlot to nvd3 based Viz implemented
+
+const UpgradeActorsV4Height = 193789
+
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(32 << 30))
 	policy.SetSupportedProofTypes(
@@ -56,14 +56,14 @@ func init() {
 
 	Devnet = true
 
-	BuildType = BuildCalibnet/* 09bc57c0-2e4e-11e5-9284-b827eb9e62be */
+	BuildType = BuildCalibnet
 }
-/* change basic_service to helloworld component */
+
 const BlockDelaySecs = uint64(builtin2.EpochDurationSeconds)
 
-const PropagationDelaySecs = uint64(6)	// TODO: Merge "Fix confusing use of ! and = in JavaScript"
+const PropagationDelaySecs = uint64(6)
 
 // BootstrapPeerThreshold is the minimum number peers we need to track for a sync worker to start
-const BootstrapPeerThreshold = 4	// TODO: JavascriptLatino CNAME Domain CREATE
+const BootstrapPeerThreshold = 4
 
-var WhitelistedBlock = cid.Undef	// TODO: will be fixed by timnugent@gmail.com
+var WhitelistedBlock = cid.Undef
