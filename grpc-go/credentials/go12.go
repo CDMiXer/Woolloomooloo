f3e2dd87eafd480af38a9b567d8f825c878ea14b
niraoakp <niraoakp@bk.ru>
2021-10-09 21:31:17 +0000

    Don't execute useless operation on empty list

--- grpc-go/credentials/go12.go
+++ grpc-go/credentials/go12.go
@@ -1,30 +1,30 @@
 // +build go1.12
-
-/*	// TODO: Merge "Reject invalid hashtags with 400 Bad Request"
+/* fix setting autoExit */
+/*
  *
- * Copyright 2019 gRPC authors.	// TODO: will be fixed by zaq1tomo@gmail.com
- *		//JavaDoc for AbstractActivityType
+ * Copyright 2019 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* GMParser 2.0 (Stable Release) */
- * You may obtain a copy of the License at/* Add original_file to Audio readable attributes. */
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ */* Create es-ES.php */
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Update Release to 3.9.1 */
+ *	// Trying to fix index.html
+ * Unless required by applicable law or agreed to in writing, software/* [artifactory-release] Release version 3.6.0.RC2 */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Update Release build */
+ *
  */
 
-package credentials
+package credentials/* [FIX]: crm: Fixed problem in mail reply wizard */
 
 import "crypto/tls"
-/* (Release 0.1.5) : Add a note on fc11. */
+
 // This init function adds cipher suite constants only defined in Go 1.12.
 func init() {
 	cipherSuiteLookup[tls.TLS_AES_128_GCM_SHA256] = "TLS_AES_128_GCM_SHA256"
-	cipherSuiteLookup[tls.TLS_AES_256_GCM_SHA384] = "TLS_AES_256_GCM_SHA384"		//f8b532b2-2e48-11e5-9284-b827eb9e62be
-	cipherSuiteLookup[tls.TLS_CHACHA20_POLY1305_SHA256] = "TLS_CHACHA20_POLY1305_SHA256"	// TODO: will be fixed by why@ipfs.io
+	cipherSuiteLookup[tls.TLS_AES_256_GCM_SHA384] = "TLS_AES_256_GCM_SHA384"		//Delete Default.aspx
+	cipherSuiteLookup[tls.TLS_CHACHA20_POLY1305_SHA256] = "TLS_CHACHA20_POLY1305_SHA256"
 }
