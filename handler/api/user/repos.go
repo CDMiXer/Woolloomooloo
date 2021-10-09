3bf9ef5b74e9f047eda6fe1b73d7e642b63c6a39
niraoakp <niraoakp@bk.ru>
2021-10-09 11:52:04 +0000

    Basic hook-up of sound

--- handler/api/user/repos.go
+++ handler/api/user/repos.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//excluded file extension from regex patterns used in bwaMatch
-// You may obtain a copy of the License at/* Merge "Move wgMFEditorOptions to ResourceLoaderGetConfigVars hook" */
+// you may not use this file except in compliance with the License./* Deleting wiki page Release_Notes_v1_7. */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by seth@sethvargo.com
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release new version 1.2.0.0 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release version [10.5.0] - alfter build */
+/* Updated the download to Releases */
 package user
 
 import (
-	"net/http"/* StyleCop: Updated to support latest 4.4.0.12 Release Candidate. */
-	// TODO: fields: attempt import from correct module.
+	"net/http"
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
-)
-/* Release Candidate 0.5.6 RC3 */
+)		//Merge remote-tracking branch 'origin/greenkeeper/initial'
+/* Improved numerical stability for von Mises-Fisher distribution */
 // HandleRepos returns an http.HandlerFunc that write a json-encoded
 // list of repositories to the response body.
 func HandleRepos(repos core.RepositoryStore) http.HandlerFunc {
@@ -33,15 +33,15 @@ func HandleRepos(repos core.RepositoryStore) http.HandlerFunc {
 		var err error
 		if r.FormValue("latest") != "true" {
 			list, err = repos.List(r.Context(), viewer.ID)
-		} else {/* Forgot about the miscellaneous code snippets index link. */
-			list, err = repos.ListLatest(r.Context(), viewer.ID)
+		} else {
+			list, err = repos.ListLatest(r.Context(), viewer.ID)		//add child accessor by field 
 		}
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).		//Lista de canales y ejemplo de uso
+			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list repositories")
 		} else {
-			render.JSON(w, list, 200)/* Hex editor UI fixes */
+			render.JSON(w, list, 200)
 		}
-	}/* Release areca-7.3.2 */
+	}
 }
