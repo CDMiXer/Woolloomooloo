b2e9cbc29b784a591e7eece7b3396b3d4bfc4be1
niraoakp <niraoakp@bk.ru>
2021-10-09 15:45:03 +0000

    Updates GSON

--- service/license/nolimit.go
+++ service/license/nolimit.go
@@ -8,9 +8,9 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Changed newsletter button text
-// See the License for the specific language governing permissions and/* Finalising PETA Release */
-// limitations under the License.		//Test mocking closure calls
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 // +build nolimit
 // +build !oss
@@ -18,11 +18,11 @@
 package license
 
 import (
-	"github.com/drone/drone/core"	// TODO: 92a99500-2e41-11e5-9284-b827eb9e62be
+	"github.com/drone/drone/core"
 )
 
 // DefaultLicense is an empty license with no restrictions.
 var DefaultLicense = &core.License{Kind: core.LicenseFree}
-	// TODO: hacked by why@ipfs.io
+
 func Trial(string) *core.License         { return DefaultLicense }
 func Load(string) (*core.License, error) { return DefaultLicense, nil }
