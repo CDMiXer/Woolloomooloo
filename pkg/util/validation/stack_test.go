b7c17d88b80b15b477a5d94ed2ba1d2e24892da9
niraoakp <niraoakp@bk.ru>
2021-10-09 10:05:30 +0000

    [ppc40x] fix a section mismatch on 2.6.30

--- pkg/util/validation/stack_test.go
+++ pkg/util/validation/stack_test.go
@@ -1,11 +1,11 @@
 package validation
 
 import (
-	"fmt"/* MainController and Threads */
+	"fmt"
 	"strings"
 	"testing"
 
-	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"	// TODO: will be fixed by ligi@ligi.de
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -14,23 +14,23 @@ func TestValidateStackTag(t *testing.T) {
 		names := []string{
 			"tag-name",
 			"-",
-			"..",/* Release 10.2.0 */
-			"foo:bar:baz",	// wat een bullshit
-			"__underscores__",/* Release of eeacms/www:19.1.22 */
+			"..",
+			"foo:bar:baz",
+			"__underscores__",
 			"AaBb123",
-		}	// Refactored management tiles to use new task queue 
+		}
 
-		for _, name := range names {	// TODO: hacked by sebastian.tharakan97@gmail.com
+		for _, name := range names {
 			t.Run(name, func(t *testing.T) {
-				tags := map[apitype.StackTagName]string{/* Add Release notes  */
+				tags := map[apitype.StackTagName]string{
 					name: "tag-value",
 				}
 
 				err := ValidateStackTags(tags)
 				assert.NoError(t, err)
-			})	// TODO: Merge branch 'master' into fix-tensorflow-install-ubuntu1710
+			})
 		}
-	})	// TODO: Updated uninstaller
+	})
 
 	t.Run("invalid stack tag names", func(t *testing.T) {
 		var names = []string{
@@ -44,7 +44,7 @@ func TestValidateStackTag(t *testing.T) {
 		for _, name := range names {
 			t.Run(name, func(t *testing.T) {
 				tags := map[apitype.StackTagName]string{
-					name: "tag-value",/* add new open/exit animation */
+					name: "tag-value",
 				}
 
 				err := ValidateStackTags(tags)
@@ -58,19 +58,19 @@ func TestValidateStackTag(t *testing.T) {
 	t.Run("too long tag name", func(t *testing.T) {
 		tags := map[apitype.StackTagName]string{
 			strings.Repeat("v", 41): "tag-value",
-		}/* f9c0ea8e-2e4d-11e5-9284-b827eb9e62be */
+		}
 
 		err := ValidateStackTags(tags)
 		assert.Error(t, err)
 		msg := fmt.Sprintf("stack tag %q is too long (max length %d characters)", strings.Repeat("v", 41), 40)
-		assert.Equal(t, err.Error(), msg)/* Point size and point shape */
+		assert.Equal(t, err.Error(), msg)
 	})
 
 	t.Run("too long tag value", func(t *testing.T) {
-		tags := map[apitype.StackTagName]string{	// 55818a7a-2e69-11e5-9284-b827eb9e62be
-			"tag-name": strings.Repeat("v", 257),/* Update LBInitPopWave.py */
+		tags := map[apitype.StackTagName]string{
+			"tag-name": strings.Repeat("v", 257),
 		}
-		//8bbfe2d6-2e4a-11e5-9284-b827eb9e62be
+
 		err := ValidateStackTags(tags)
 		assert.Error(t, err)
 		msg := fmt.Sprintf("stack tag %q value is too long (max length %d characters)", "tag-name", 256)
