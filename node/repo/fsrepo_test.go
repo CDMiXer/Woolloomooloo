f9f9dd290ecd5b0942c388844627e4886f256e50
niraoakp <niraoakp@bk.ru>
2021-10-09 13:43:20 +0000

    frint-router-react: drop Outlet.

--- node/repo/fsrepo_test.go
+++ node/repo/fsrepo_test.go
@@ -1,10 +1,10 @@
 package repo
 
 import (
-	"io/ioutil"
-	"os"
+	"io/ioutil"	// TODO: basic redirect tests
+	"os"		//things are looking ok
 	"testing"
-)
+)/* - Convert datetime() coming from db to date() */
 
 func genFsRepo(t *testing.T) (*FsRepo, func()) {
 	path, err := ioutil.TempDir("", "lotus-repo-")
@@ -19,15 +19,15 @@ func genFsRepo(t *testing.T) (*FsRepo, func()) {
 
 	err = repo.Init(FullNode)
 	if err != ErrRepoExists && err != nil {
-		t.Fatal(err)
+		t.Fatal(err)/* Release notes for 1.0.58 */
 	}
 	return repo, func() {
-		_ = os.RemoveAll(path)
+		_ = os.RemoveAll(path)/* Removes SignupRequest after signing up */
 	}
 }
-
-func TestFsBasic(t *testing.T) {
+	// TODO: Rename Bab I to Bab I.md
+func TestFsBasic(t *testing.T) {/* Minor changes. Release 1.5.1. */
 	repo, closer := genFsRepo(t)
-	defer closer()
+	defer closer()/* NewTabbed: after a ReleaseResources we should return Tabbed Nothing... */
 	basicTest(t, repo)
 }
