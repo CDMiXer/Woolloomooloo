6caa982a66cf8e12de14298b8a7fab443af31617
niraoakp <niraoakp@bk.ru>
2021-10-09 10:42:50 +0000

    start develop 0.3.7

--- operator/manager/rpc/types.go
+++ operator/manager/rpc/types.go
@@ -7,7 +7,7 @@
 package rpc
 
 import (
-	"sync"		//Update FERPATermOfAgreement-002.md
+	"sync"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/operator/manager"
@@ -15,48 +15,48 @@ import (
 
 type requestRequest struct {
 	Request *manager.Request
-}/* Updating contributors */
-		//693184d4-2e6f-11e5-9284-b827eb9e62be
-type acceptRequest struct {	// TODO: 700abf14-2fa5-11e5-ba21-00012e3d3f12
+}
+
+type acceptRequest struct {
 	Stage   int64
 	Machine string
 }
-		//blue for vim-go (#119)
+
 type netrcRequest struct {
 	Repo int64
-}	// TODO: add version_requirement to dependencies
+}
 
 type detailsRequest struct {
 	Stage int64
 }
 
 type stageRequest struct {
-	Stage *core.Stage/* shows profile only if present */
+	Stage *core.Stage
 }
 
 type stepRequest struct {
 	Step *core.Step
 }
-	// TODO: will be fixed by arajasek94@gmail.com
+
 type writeRequest struct {
 	Step int64
 	Line *core.Line
-}/* Release 0.9.0 */
-/* Release 1.2.0 publicando en Repositorio Central */
+}
+
 type watchRequest struct {
 	Build int64
 }
 
 type watchResponse struct {
-	Done bool	// TODO: Merge "Fix empty ofport value"
+	Done bool
 }
 
 type buildContextToken struct {
 	Secret  string
 	Context *manager.Context
-}/* Add `difform-style` output */
-	// fixing some corner cases
-type errorWrapper struct {/* Some more abbreviations. */
+}
+
+type errorWrapper struct {
 	Message string
 }
 
