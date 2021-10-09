7c337b685356e66087bdd63833ee54cff8d901d1
niraoakp <niraoakp@bk.ru>
2021-10-09 00:53:07 +0000

    added bower install instruction to installation

--- tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
+++ tests/integration/stack_reference_secrets/dotnet/step1/Program.cs
@@ -2,19 +2,19 @@
 
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Pulumi;
+using Pulumi;	// TODO: Update submitfile
 
-class Program
+class Program/* Release notes and change log for 0.9 */
 {
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() =>
         {
             return new Dictionary<string, object>
-            {
+            {/* Merge "Release 3.0.10.001 Prima WLAN Driver" */
                 { "normal", Output.Create("normal") },
-                { "secret", Output.CreateSecret("secret") },
-            };
+                { "secret", Output.CreateSecret("secret") },/* Create minSubArray */
+            };		//create main_css
         });
     }
 }
