fd2e20c2019c874e8003d769f41dd7d9cc1f886a
niraoakp <niraoakp@bk.ru>
2021-10-09 12:17:47 +0000

    Fix NPE in CGLIBProxyTypePermission.

--- tests/integration/stack_reference/python/__main__.py
+++ tests/integration/stack_reference/python/__main__.py
@@ -1,16 +1,16 @@
 # Copyright 2020, Pulumi Corporation.  All rights reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");/* Released springjdbcdao version 1.7.8 */
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0/* Fix double Notes section in docstring */
-#/* Release 0.95.194: Crash fix */
-# Unless required by applicable law or agreed to in writing, software/* Merge "ext.centralNotice.display: Convert to using packageFiles" */
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import pulumi
 
-pulumi.export('val', ["a", "b"])		//Improve readability of tests.
+pulumi.export('val', ["a", "b"])
