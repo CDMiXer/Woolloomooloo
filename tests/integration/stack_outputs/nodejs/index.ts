3b15aff7d2637b3c3006f06b37ef0e090bc0a766
niraoakp <niraoakp@bk.ru>
2021-10-09 12:46:33 +0000

    Update readme with usage example

--- tests/integration/stack_outputs/nodejs/index.ts
+++ tests/integration/stack_outputs/nodejs/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: Merge "msm: camera: Add v4l2 strobe flash ctrl command" into msm-2.6.38
-export let xyz = "ABC";		//updating header template
-export let foo = 42;	// TODO: will be fixed by souzau@yandex.com
+
+export let xyz = "ABC";
+export let foo = 42;	// TODO: hacked by sebastian.tharakan97@gmail.com
