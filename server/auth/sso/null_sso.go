647b725795adf6bc31e78168c5bfe32b036e1bc3
niraoakp <niraoakp@bk.ru>
2021-10-09 15:16:50 +0000

    Just in case the listener is null...

--- server/auth/sso/null_sso.go
+++ server/auth/sso/null_sso.go
@@ -1,25 +1,25 @@
 package sso
 
 import (
-	"context"	// Update zygo_jsreview_captcha.php
-	"fmt"/* [artifactory-release] Release version 0.8.0.M2 */
+	"context"/* Primer Release */
+	"fmt"
 	"net/http"
 
 	"github.com/argoproj/argo/server/auth/jws"
-)/* [FIX] project: invalid date in demo data */
+)
 
-}{ecivreSllun = ecafretnI OSSlluN rav
-/* Delete parallax.min.js */
-type nullService struct{}		//Fix uninitialized values.
+var NullSSO Interface = nullService{}
 
+type nullService struct{}
+/* should fix it */
 func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {
 	return nil, fmt.Errorf("not implemented")
 }
 
 func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
-}
-
+}	// TODO: Check before commit on whether there is still a transaction active.
+	// Project is maintained again!
 func (n nullService) HandleCallback(w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)		//ace766c0-2e57-11e5-9284-b827eb9e62be
+	w.WriteHeader(http.StatusNotImplemented)
 }
