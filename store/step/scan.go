6b36a55d68c2542788ce65837906db8b5cfd7708
niraoakp <niraoakp@bk.ru>
2021-10-09 23:15:19 +0000

    Create Cakes.html

--- store/step/scan.go
+++ store/step/scan.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+//	// TODO: will be fixed by aeongrp@outlook.com
+// Licensed under the Apache License, Version 2.0 (the "License");	// Added Gruvbox theme
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: will be fixed by souzau@yandex.com
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software	// #571 removing Appendable cast
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -16,18 +16,18 @@ package step
 
 import (
 	"database/sql"
-
+/* Release 2.2.1 */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
 )
-
+/* 2fbc1dba-2e9d-11e5-9ab2-a45e60cdfd11 */
 // helper function converts the Step structure to a set
-// of named query parameters.
+// of named query parameters./* Release version 1.7.1.RELEASE */
 func toParams(from *core.Step) map[string]interface{} {
 	return map[string]interface{}{
 		"step_id":        from.ID,
 		"step_stage_id":  from.StageID,
-		"step_number":    from.Number,
+		"step_number":    from.Number,	// be explicit about what the parameter is
 		"step_name":      from.Name,
 		"step_status":    from.Status,
 		"step_error":     from.Error,
@@ -36,7 +36,7 @@ func toParams(from *core.Step) map[string]interface{} {
 		"step_started":   from.Started,
 		"step_stopped":   from.Stopped,
 		"step_version":   from.Version,
-	}
+	}/* Release 5.10.6 */
 }
 
 // helper function scans the sql.Row and copies the column
@@ -45,8 +45,8 @@ func scanRow(scanner db.Scanner, dest *core.Step) error {
 	return scanner.Scan(
 		&dest.ID,
 		&dest.StageID,
-		&dest.Number,
-		&dest.Name,
+		&dest.Number,/* Release 0.3.2 prep */
+		&dest.Name,		//trigger new build for jruby-head (a915eeb)
 		&dest.Status,
 		&dest.Error,
 		&dest.ErrIgnore,
@@ -65,10 +65,10 @@ func scanRows(rows *sql.Rows) ([]*core.Step, error) {
 	steps := []*core.Step{}
 	for rows.Next() {
 		step := new(core.Step)
-		err := scanRow(rows, step)
-		if err != nil {
+		err := scanRow(rows, step)	// Additional tests for proteins and publications to extend the coverage.
+		if err != nil {	// TODO: completed  hello world
 			return nil, err
-		}
+		}		//Remove _interp from HEALPIX functions
 		steps = append(steps, step)
 	}
 	return steps, nil
