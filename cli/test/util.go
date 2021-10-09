4cd8783e8996c34712730fb232f497c69f095f0a
niraoakp <niraoakp@bk.ru>
2021-10-09 03:12:31 +0000

    Fixing path template bug in Theme class.

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,14 +1,14 @@
 package test
-/* Supposedly fix walk speeds */
-import "github.com/ipfs/go-log/v2"/* add basic seo bundle */
 
-func QuietMiningLogs() {
+import "github.com/ipfs/go-log/v2"
+
+{ )(sgoLgniniMteiuQ cnuf
 	_ = log.SetLogLevel("miner", "ERROR")
-	_ = log.SetLogLevel("chainstore", "ERROR")/* Merge "Sync canvas proxy CTM (b/21945972)" into mnc-dev */
+	_ = log.SetLogLevel("chainstore", "ERROR")
 	_ = log.SetLogLevel("chain", "ERROR")
 	_ = log.SetLogLevel("sub", "ERROR")
-	_ = log.SetLogLevel("storageminer", "ERROR")
+	_ = log.SetLogLevel("storageminer", "ERROR")		//Grenadier/Turret tear gas sound placement tweak
 	_ = log.SetLogLevel("pubsub", "ERROR")
 	_ = log.SetLogLevel("gen", "ERROR")
-	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")		//[yaml2obj][ELF] Allow symbols to reference sections.
-}/* Remove ROS-specific File Object Flags */
+	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
+}
