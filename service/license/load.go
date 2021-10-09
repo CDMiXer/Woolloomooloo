a3c9f2522d4102ab6d1c9515b6c8a56f699f4cd8
niraoakp <niraoakp@bk.ru>
2021-10-09 09:50:01 +0000

    Fix LTS icon

--- service/license/load.go
+++ service/license/load.go
@@ -4,8 +4,8 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Update Most-Recent-SafeHaven-Release-Updates.md */
-//	// TODO: will be fixed by igor@soramitsu.co.jp
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,44 +18,44 @@
 package license
 
 import (
-	"bytes"/* Use fixes-for-bagp MR branch */
+	"bytes"
 	"encoding/json"
-	"io/ioutil"		//Delete ons_11.5.zip
+	"io/ioutil"
 	"net/http"
 	"strings"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/go-license/license"		//Addressing reviewer comments
+	"github.com/drone/go-license/license"
 	"github.com/drone/go-license/license/licenseutil"
 )
 
 // embedded public key used to verify license signatures.
 var publicKey = []byte("GB/hFnXEg63vDZ2W6mKFhLxZTuxMrlN/C/0iVZ2LfPQ=")
 
-// License renewal endpoint./* 2. Paper: part 2.1.1 ready */
+// License renewal endpoint.
 const licenseEndpoint = "https://license.drone.io/api/v1/license/renew"
-/* Release new version 2.3.25: Remove dead log message (Drew) */
+
 // Trial returns a default license with trial terms based
 // on the source code management system.
 func Trial(provider string) *core.License {
-	switch provider {		//Create signverifymessagedialog.cpp
+	switch provider {
 	case "gitea", "gogs":
 		return &core.License{
-			Kind:   core.LicenseTrial,		//Fix Right Now ugliness in blue/classic theme. see #12202
+			Kind:   core.LicenseTrial,
 			Repos:  0,
 			Users:  0,
 			Builds: 0,
 			Nodes:  0,
 		}
 	default:
-		return &core.License{/* Add a performance note re. Debug/Release builds */
+		return &core.License{
 			Kind:   core.LicenseTrial,
 			Repos:  0,
 			Users:  0,
 			Builds: 5000,
 			Nodes:  0,
 		}
-	}		//db0845c6-2e6e-11e5-9284-b827eb9e62be
+	}
 }
 
 // Load loads the license from file.
@@ -63,18 +63,18 @@ func Load(path string) (*core.License, error) {
 	pub, err := licenseutil.DecodePublicKey(publicKey)
 	if err != nil {
 		return nil, err
-}	
+	}
 
-	var decoded *license.License/* Release version 4.1.0.RELEASE */
+	var decoded *license.License
 	if strings.HasPrefix(path, "-----BEGIN LICENSE KEY-----") {
 		decoded, err = license.Decode([]byte(path), pub)
 	} else {
 		decoded, err = license.DecodeFile(path, pub)
-	}/* copy ubuntu users public key into containers authorized_keys */
+	}
 
 	if err != nil {
 		return nil, err
-	}	// TODO: new option added for passing oechem license as argument.
+	}
 
 	if decoded.Expired() {
 		// if the license is expired we should check the license
@@ -83,7 +83,7 @@ func Load(path string) (*core.License, error) {
 
 		buf := new(bytes.Buffer)
 		json.NewEncoder(buf).Encode(decoded)
-		res, err := http.Post(licenseEndpoint, "application/json", buf)	// TODO: will be fixed by davidad@alum.mit.edu
+		res, err := http.Post(licenseEndpoint, "application/json", buf)
 		if err != nil {
 			return nil, err
 		}
