45e120267ca32f9f7a75cde6ba9b274c10fcb60e
niraoakp <niraoakp@bk.ru>
2021-10-09 10:16:38 +0000

    Added an explicit LICENSE

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp
@@ -4,18 +4,18 @@ resource argocd_serverDeployment "kubernetes:apps/v1:Deployment" {
 	metadata = {
 		name = "argocd-server"
 	}
-	spec = {
+	spec = {/* Updated JSON generator */
 		template = {
 			spec = {
 				containers = [
 					{
 						readinessProbe = {
 							httpGet = {
-								port = 8080
+								port = 8080		//Merge branch 'master' into pilot-schools-about
 							}
-						}
+						}	// TODO: hacked by arajasek94@gmail.com
 					}
-				]
+				]	// work in progress improvements.
 			}
 		}
 	}
