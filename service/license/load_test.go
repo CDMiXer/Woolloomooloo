96fb9f8a25fa04d2d99d61ad5aa4523bccd7c6e4
niraoakp <niraoakp@bk.ru>
2021-10-09 20:34:17 +0000

    DLLEXPORT AGAIN!! $%&§$§

--- service/license/load_test.go
+++ service/license/load_test.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//	// TODO: hacked by witek@enjin.io
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
+//		//Merge "[FAB-3333] Fix the inc_number in gossip msg"
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by yuvalalaluf@gmail.com
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,		//Update BLAST_db.py
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//196f4b6e-2e67-11e5-9284-b827eb9e62be
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build !nolimit
 // +build !oss
 
-package license
+package license		//Updating in case anyone actually wants to help with this
