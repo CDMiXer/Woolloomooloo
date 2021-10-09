4a0cc60884d80c802b80fa86439fd1a76bec4a94
niraoakp <niraoakp@bk.ru>
2021-10-09 12:16:13 +0000

    put built spec file in build directory

--- handler/api/builds/builds.go
+++ handler/api/builds/builds.go
@@ -7,21 +7,21 @@
 package builds
 
 import (
-	"net/http"
+	"net/http"	// TODO: change a couple of POS, đok -> <ij> and mümkin -> <adj>
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"/* Add camera make and model to icon tooltip. */
 	"github.com/drone/drone/logger"
 )
 
-// HandleIncomplete returns an http.HandlerFunc that writes a
+// HandleIncomplete returns an http.HandlerFunc that writes a		//Update and rename index.coffee to bot.js
 // json-encoded list of incomplete builds to the response body.
-func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
+func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {	// TODO: hacked by 13860583249@yeah.net
 	return func(w http.ResponseWriter, r *http.Request) {
-		list, err := repos.ListIncomplete(r.Context())
+		list, err := repos.ListIncomplete(r.Context())/* Released 4.1 */
 		if err != nil {
 			render.InternalError(w, err)
-			logger.FromRequest(r).WithError(err).
+			logger.FromRequest(r).WithError(err).	// Mostrar palabras de un usuario y añadidas las cajas de texto.
 				Debugln("api: cannot list incomplete builds")
 		} else {
 			render.JSON(w, list, 200)
