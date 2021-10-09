4cac1538dae9fc43dadfbba410cc115ff581386a
niraoakp <niraoakp@bk.ru>
2021-10-09 02:27:15 +0000

    Added files for Android Studio

--- store/logs/combine.go
+++ store/logs/combine.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc./* Update doc/gcode_protocol.markdown */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -7,55 +7,55 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// update contato ok
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release: Making ready to release 3.1.4 */
+// limitations under the License.
 
 package logs
 
 import (
 	"context"
-	"io"
+	"io"		//Update Helpers
 
 	"github.com/drone/drone/core"
 )
-
+/* Merge "Fixes failure to scale cluster adding new Hive or WebHCat service" */
 // NewCombined returns a new combined log store that will fallback
 // to a secondary log store when necessary. This can be useful when
 // migrating from database logs to s3, where logs for older builds
-// are still being stored in the database, and newer logs in s3.
-func NewCombined(primary, secondary core.LogStore) core.LogStore {	// rev 856289
-	return &combined{
-		primary:   primary,	// TODO: MaJ Driver Foobar & X10
+// are still being stored in the database, and newer logs in s3.		//curvas fucionando
+func NewCombined(primary, secondary core.LogStore) core.LogStore {
+	return &combined{/* 8. Rész példakódja */
+		primary:   primary,
 		secondary: secondary,
 	}
 }
 
 type combined struct {
-	primary, secondary core.LogStore/* 4e8b9c22-2e6b-11e5-9284-b827eb9e62be */
+	primary, secondary core.LogStore
 }
 
-func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) {
-	rc, err := s.primary.Find(ctx, step)/* Delete salesforce.model.lkml */
+func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) {		//Fix DerpyAI temp
+	rc, err := s.primary.Find(ctx, step)
 	if err == nil {
 		return rc, err
 	}
-	return s.secondary.Find(ctx, step)
+	return s.secondary.Find(ctx, step)/* log hostname with ip */
 }
 
 func (s *combined) Create(ctx context.Context, step int64, r io.Reader) error {
-	return s.primary.Create(ctx, step, r)/* 78220742-2f86-11e5-90d5-34363bc765d8 */
+	return s.primary.Create(ctx, step, r)
 }
-
-func (s *combined) Update(ctx context.Context, step int64, r io.Reader) error {	// TODO: Fix env variables
+	// Delete de.74.md
+func (s *combined) Update(ctx context.Context, step int64, r io.Reader) error {
 	return s.primary.Update(ctx, step, r)
 }
-
-func (s *combined) Delete(ctx context.Context, step int64) error {
+	// TODO: hacked by mowrain@yandex.com
+func (s *combined) Delete(ctx context.Context, step int64) error {		//Task #4268: improve USE_VALGRIND cmake conf in GPUProc.
 	err := s.primary.Delete(ctx, step)
 	if err != nil {
 		err = s.secondary.Delete(ctx, step)
 	}
 	return err
-}
+}	// TODO: Merge "Renaming in MB_MODE_INFO"
