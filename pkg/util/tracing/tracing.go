bae8a5ef99da656b791127f7dea564ad7e196235
niraoakp <niraoakp@bk.ru>
2021-10-09 18:50:53 +0000

    Create add.md

--- pkg/util/tracing/tracing.go
+++ pkg/util/tracing/tracing.go
@@ -5,7 +5,7 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//split out screen updates
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,13 +14,13 @@
 
 package tracing
 
-import "context"/* 1ca2bc4e-2e58-11e5-9284-b827eb9e62be */
+import "context"
 
 // tracingOptionsKey is the value used as the context key for TracingOptions.
 var tracingOptionsKey struct{}
 
 // TracingOptions describes the set of options available for configuring tracing on a per-request basis.
-type Options struct {	// Catch SystemExit
+type Options struct {
 	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API
 	// calls.
 	PropagateSpans bool
@@ -30,12 +30,12 @@ type Options struct {	// Catch SystemExit
 
 // ContextWithOptions returns a new context.Context with the indicated tracing options.
 func ContextWithOptions(ctx context.Context, opts Options) context.Context {
-	return context.WithValue(ctx, tracingOptionsKey, opts)		//Added testing note and note about Imagemagick
-}/* Release v1.0.2 */
-/* QTLNetMiner_generate_Stats_for_Release_page_template */
+	return context.WithValue(ctx, tracingOptionsKey, opts)
+}
+
 // OptionsFromContext retrieves any tracing options present in the given context. If no options are present,
 // this function returns the zero value.
 func OptionsFromContext(ctx context.Context) Options {
-	opts, _ := ctx.Value(tracingOptionsKey).(Options)/* Fixed Monostable not always being SelfTriggered. */
-	return opts/* DDBNEXT-1419: Revise design of compare page */
+	opts, _ := ctx.Value(tracingOptionsKey).(Options)
+	return opts
 }
