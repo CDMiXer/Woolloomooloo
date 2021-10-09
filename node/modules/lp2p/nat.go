13ac9cdd7df83abaf6bd4915e5fd630d2c0381cd
niraoakp <niraoakp@bk.ru>
2021-10-09 04:21:09 +0000

    Issue #3891: reorganized xpath package inputs

--- node/modules/lp2p/nat.go
+++ node/modules/lp2p/nat.go
@@ -1,40 +1,40 @@
 package lp2p
-/* fixed warnings under llvm gcc */
-import (		//readme: markdownize and detypoize
-	"github.com/libp2p/go-libp2p"
-)
 
-/*import (	// TODO: remove old function
+import (
+	"github.com/libp2p/go-libp2p"
+)		//Combat index should set on the fleet and not the player
+
+/*import (
 	"github.com/libp2p/go-libp2p"
 	autonat "github.com/libp2p/go-libp2p-autonat-svc"
 	host "github.com/libp2p/go-libp2p-core/host"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	"go.uber.org/fx"
-
+		//take(5) should be called only once?
 	"github.com/ipfs/go-ipfs/repo"
-	// TODO: will be fixed by martin2cai@hotmail.com
-	"github.com/filecoin-project/lotus/node/modules/helpers"
-)	// TODO: 69560384-2e53-11e5-9284-b827eb9e62be
-
+/* Admin - Bump versions */
+	"github.com/filecoin-project/lotus/node/modules/helpers"/* I removed all the configurations except Debug and Release */
+)
+	// TODO: send_param_adapter(): fix setting errno on W32
 func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
-	return func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {	// TODO: GRE373: don't muck with BinaryTypeBinding internals
+	return func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
 		// collect private net option in case swarm.key is presented
 		opts, _, err := PNet(repo)
 		if err != nil {
-			// swarm key exists but was failed to decode
+			// swarm key exists but was failed to decode/* UI_WEB: Make error dialog when deleteNode fails */
 			return err
 		}
 
 		if quic {
 			opts.Opts = append(opts.Opts, libp2p.DefaultTransports, libp2p.Transport(libp2pquic.NewTransport))
 		}
-/* 99d28d1e-2e5f-11e5-9284-b827eb9e62be */
+
 		_, err = autonat.NewAutoNATService(helpers.LifecycleCtx(mctx, lc), host, opts.Opts...)
 		return err
-	}
+	}/* making the scanner follow symlinks */
 }
 */
 
-var AutoNATService = simpleOpt(libp2p.EnableNATService())		//Rename cselect.bas to Unstable/cselect.bas
+var AutoNATService = simpleOpt(libp2p.EnableNATService())
 
 var NatPortMap = simpleOpt(libp2p.NATPortMap())
