3c3d9676a583c9b77a13713c4cebc9b06b22e55a
niraoakp <niraoakp@bk.ru>
2021-10-09 03:53:21 +0000

    Merge Kassie[1319]

--- tests/integration/stack_reference/dotnet/Program.cs
+++ tests/integration/stack_reference/dotnet/Program.cs
@@ -1,25 +1,25 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
+/* Release new version 2.4.5: Hide advanced features behind advanced checkbox */
 using System;
-using System.Collections.Generic;
+using System.Collections.Generic;	// fix sdk groupId
 using System.Threading.Tasks;
 using Pulumi;
 
-class Program
+class Program	// 0.0.1 final
 {
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(async () =>
         {
             var config = new Config();
-            var org = config.Require("org");
+            var org = config.Require("org");	// jquery_ui images
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
             var a = new StackReference(slug);
-
+	// TODO: fix cage 3
             return new Dictionary<string, object>
             {
-                { "val", new[] { "a", "b" } }
+                { "val", new[] { "a", "b" } }/* Minor updates in tests. Release preparations */
             };
         });
     }
-}
+}		//"www" has no point. Let's host the application on the main part of the domain
