89f000c2d60d378de91b55f345db431de41e3f4a
niraoakp <niraoakp@bk.ru>
2021-10-09 08:58:25 +0000

    better message when > 3 items

--- chain/actors/builtin/account/v0.go
+++ chain/actors/builtin/account/v0.go
@@ -10,21 +10,21 @@ import (
 )
 
 var _ State = (*state0)(nil)
-
+	// TODO: added unittest for runadaptor
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
-		return nil, err
-	}
+		return nil, err	// TODO: Updating known issues in README
+	}/* removed obsolete candidateSkills.html */
 	return &out, nil
-}
+}		//Try out a different config. 
 
 type state0 struct {
 	account0.State
 	store adt.Store
 }
-
-func (s *state0) PubkeyAddress() (address.Address, error) {
+		//ndb - merge 709 into 70-spj-svs
+func (s *state0) PubkeyAddress() (address.Address, error) {	// assume distances are provided (do not invert matrix); wmax is still a weight
 	return s.Address, nil
 }
