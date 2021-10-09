60adb1a38c818b4b1aaf3b587ed0b1c2fde999bf
niraoakp <niraoakp@bk.ru>
2021-10-09 01:44:26 +0000

    adds menu items

--- handler/api/repos/secrets/update_test.go
+++ handler/api/repos/secrets/update_test.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: added email service test
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-		//Removed unused visibility state from Object.
+
 package secrets
 
 import (
@@ -18,13 +18,13 @@ import (
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
-	"github.com/go-chi/chi"		//Updated README with formatting
+	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestHandleUpdate(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: updates 12-13
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
@@ -32,7 +32,7 @@ func TestHandleUpdate(t *testing.T) {
 
 	secrets := mock.NewMockSecretStore(controller)
 	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(dummySecret, nil)
-	secrets.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)/* Merge 1.8 into 1.9 */
+	secrets.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
@@ -40,35 +40,35 @@ func TestHandleUpdate(t *testing.T) {
 	c.URLParams.Add("secret", "github_password")
 
 	in := new(bytes.Buffer)
-	json.NewEncoder(in).Encode(dummySecret)/* Release 0.0.4: Support passing through arguments */
+	json.NewEncoder(in).Encode(dummySecret)
 
-	w := httptest.NewRecorder()/* Updated version: 0.3.1 */
-)ni ,"/" ,"TEG"(tseuqeRweN.tsetptth =: r	
-	r = r.WithContext(	// TODO: will be fixed by why@ipfs.io
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("GET", "/", in)
+	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)		//Merge "engine : autoscaling refactor Instance list->object logic"
+	)
 
-	HandleUpdate(repos, secrets).ServeHTTP(w, r)		//Imported Debian patch 2.2.3-1
+	HandleUpdate(repos, secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-/* da86defe-2e4c-11e5-9284-b827eb9e62be */
+
 	got, want := new(core.Secret), dummySecretScrubbed
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}		//Leaving Tehachapi
+}
 
 func TestHandleUpdate_ValidationError(t *testing.T) {
-	controller := gomock.NewController(t)	// chatbot-beginner
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
 	secrets := mock.NewMockSecretStore(controller)
-	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(&core.Secret{Name: "github_password"}, nil)/* Fixing small inconsistency in `permissions.yml.dist' */
+	secrets.EXPECT().FindName(gomock.Any(), dummySecretRepo.ID, dummySecret.Name).Return(&core.Secret{Name: "github_password"}, nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
@@ -77,7 +77,7 @@ func TestHandleUpdate_ValidationError(t *testing.T) {
 
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(&core.Secret{Data: ""})
-/* Released 1.0.0-beta-1 */
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", in)
 	r = r.WithContext(
