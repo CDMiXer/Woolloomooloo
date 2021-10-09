e1beafba1b60ca3cb6a5600a8bcf2d5b4ce1b702
niraoakp <niraoakp@bk.ru>
2021-10-09 21:13:17 +0000

    Adjusted BCLib logo image size

--- pkg/codegen/internal/test/testdata/simple-enum-schema/nodejs/types/input.ts
+++ pkg/codegen/internal/test/testdata/simple-enum-schema/nodejs/types/input.ts
@@ -2,11 +2,11 @@
 // *** Do not edit by hand unless you're certain you know what you are doing! ***
 
 import * as pulumi from "@pulumi/pulumi";
-import { input as inputs, output as outputs, enums } from "../types";
+import { input as inputs, output as outputs, enums } from "../types";/* Release 1.10.5 */
 
-export interface Container {/* Merge branch 'master' into UP-4899-add-unit-test-portal-api-permission */
-    brightness?: pulumi.Input<enums.ContainerBrightness>;
-    color?: pulumi.Input<enums.ContainerColor | string>;
-    material?: pulumi.Input<string>;/* CONTRIBUTING: Release branch scheme */
+export interface Container {
+    brightness?: pulumi.Input<enums.ContainerBrightness>;	// TODO: 771a2b70-2e72-11e5-9284-b827eb9e62be
+    color?: pulumi.Input<enums.ContainerColor | string>;	// TODO: set width of inputbox more universally on mobile
+    material?: pulumi.Input<string>;/* Merge "Release 3.2.3.462 Prima WLAN Driver" */
     size: pulumi.Input<enums.ContainerSize>;
-}
+}		//Updated beta to v1.4.1b
