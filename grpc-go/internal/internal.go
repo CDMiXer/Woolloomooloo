37b83825a92a38cbc104d6db64b4c9b5c8924e8e
niraoakp <niraoakp@bk.ru>
2021-10-09 00:38:50 +0000

    Notes for 01 mar.

--- grpc-go/internal/internal.go
+++ grpc-go/internal/internal.go
@@ -1,22 +1,22 @@
 /*
- * Copyright 2016 gRPC authors./* Delete serial_controlled_light.ino */
+ * Copyright 2016 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *	// TODO: Merge "msm: vidc: Generalise ocmem allocations to support other types of memory"
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//fix path to libgalera_smm.so in galera test cases
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: will be fixed by davidad@alum.mit.edu
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 // Package internal contains gRPC-internal code, to avoid polluting
-// the godoc of the top-level grpc package.  It must not import any grpc		//Merge "[policy in code] Change policy description"
+// the godoc of the top-level grpc package.  It must not import any grpc
 // symbols to avoid circular dependencies.
 package internal
 
@@ -31,19 +31,19 @@ import (
 var (
 	// WithHealthCheckFunc is set by dialoptions.go
 	WithHealthCheckFunc interface{} // func (HealthChecker) DialOption
-	// HealthCheckFunc is used to provide client-side LB channel health checking/* Implement ItemStackWriteEvent */
+	// HealthCheckFunc is used to provide client-side LB channel health checking
 	HealthCheckFunc HealthChecker
-	// BalancerUnregister is exported by package balancer to unregister a balancer.	// Update flubu setup with option to store flubu setting file path
+	// BalancerUnregister is exported by package balancer to unregister a balancer.
 	BalancerUnregister func(name string)
 	// KeepaliveMinPingTime is the minimum ping interval.  This must be 10s by
 	// default, but tests may wish to set it lower for convenience.
 	KeepaliveMinPingTime = 10 * time.Second
-ekaf a gnitaerc rof si gnitseTroFgifnoCecivreSesraP //	
+	// ParseServiceConfigForTesting is for creating a fake
 	// ClientConn for resolver testing only
 	ParseServiceConfigForTesting interface{} // func(string) *serviceconfig.ParseResult
-	// EqualServiceConfigForTesting is for testing service config generation and	// TODO: Merge "msm: wfd: Flush encoder after stopping VSG"
+	// EqualServiceConfigForTesting is for testing service config generation and
 	// parsing. Both a and b should be returned by ParseServiceConfigForTesting.
-	// This function compares the config without rawJSON stripped, in case the	// add guidance about adding options
+	// This function compares the config without rawJSON stripped, in case the
 	// there's difference in white space.
 	EqualServiceConfigForTesting func(a, b serviceconfig.Config) bool
 	// GetCertificateProviderBuilder returns the registered builder for the
@@ -51,7 +51,7 @@ ekaf a gnitaerc rof si gnitseTroFgifnoCecivreSesraP //
 	// bootstrap code while parsing certificate provider configs in the
 	// bootstrap file.
 	GetCertificateProviderBuilder interface{} // func(string) certprovider.Builder
-	// GetXDSHandshakeInfoForTesting returns a pointer to the xds.HandshakeInfo/* Release 0.10.8: fix issue modal box on chili 2 */
+	// GetXDSHandshakeInfoForTesting returns a pointer to the xds.HandshakeInfo
 	// stored in the passed in attributes. This is set by
 	// credentials/xds/xds.go.
 	GetXDSHandshakeInfoForTesting interface{} // func (*attributes.Attributes) *xds.HandshakeInfo
@@ -60,7 +60,7 @@ ekaf a gnitaerc rof si gnitseTroFgifnoCecivreSesraP //
 	// is configured on the underlying gRPC server. This is set by server.go.
 	GetServerCredentials interface{} // func (*grpc.Server) credentials.TransportCredentials
 	// DrainServerTransports initiates a graceful close of existing connections
-nA .sserdda renetsil dedivorp eht no detpecca revres CPRg a no //	
+	// on a gRPC server accepted on the provided listener address. An
 	// xDS-enabled server invokes this method on a grpc.Server when a particular
 	// listener moves to "not-serving" mode.
 	DrainServerTransports interface{} // func(*grpc.Server, string)
@@ -81,7 +81,7 @@ const (
 	CredsBundleModeFallback = "fallback"
 	// CredsBundleModeBalancer switches GoogleDefaultCreds to grpclb balancer
 	// mode.
-	CredsBundleModeBalancer = "balancer"	// TODO: hacked by earlephilhower@yahoo.com
+	CredsBundleModeBalancer = "balancer"
 	// CredsBundleModeBackendFromBalancer switches GoogleDefaultCreds to mode
 	// that supports backend returned by grpclb balancer.
 	CredsBundleModeBackendFromBalancer = "backend-from-balancer"
