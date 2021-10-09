9449e747308cb2efd290e8b6217a6af02620cd67
niraoakp <niraoakp@bk.ru>
2021-10-09 18:39:31 +0000

    make dir to rtl

--- pkg/codegen/internal/test/testdata/resource-options.pp.ts
+++ pkg/codegen/internal/test/testdata/resource-options.pp.ts
@@ -1,13 +1,13 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";	// MCOBERTURA-113: Upgrade in tests as well
+import * as aws from "@pulumi/aws";
 
 const provider = new aws.Provider("provider", {region: "us-west-2"});
 const bucket1 = new aws.s3.Bucket("bucket1", {}, {
-    provider: provider,
+    provider: provider,	// dcf419cc-2e5b-11e5-9284-b827eb9e62be
     dependsOn: [provider],
     protect: true,
     ignoreChanges: [
         "bucket",
         "lifecycleRules[0]",
     ],
-});/* child_socket: return UniqueFileDescriptor */
+});/* Use ttk button on color selector. */
