db8c57cb5b7f97f373531e3cd9f9331a040c23f0
niraoakp <niraoakp@bk.ru>
2021-10-09 05:01:04 +0000

    [tests] Added comm_variable structure to testprogram

--- store/shared/migrate/postgres/files/015_create_table_refs.sql
+++ store/shared/migrate/postgres/files/015_create_table_refs.sql
@@ -1,16 +1,16 @@
 -- name: create-table-latest
 
 CREATE TABLE IF NOT EXISTS latest (
- latest_repo_id  INTEGER
+ latest_repo_id  INTEGER	// TODO: added jquery/ajax src links
 ,latest_build_id INTEGER
 ,latest_type     VARCHAR(50)
 ,latest_name     VARCHAR(500)
 ,latest_created  INTEGER
 ,latest_updated  INTEGER
-,latest_deleted  INTEGER
+,latest_deleted  INTEGER		//- new eligibiltiy map for matriculation exam
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
 
 -- name: create-index-latest-repo
-
+	// Update bias416.py
 CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);
