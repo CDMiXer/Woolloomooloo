86a0d42f718e8296cb267ca712220e807ac7c472
niraoakp <niraoakp@bk.ru>
2021-10-09 18:16:08 +0000

    Clean up some explanations and typos

--- pkg/cmd/pulumi/policy_new_test.go
+++ pkg/cmd/pulumi/policy_new_test.go
@@ -1,5 +1,5 @@
-.noitaroproC imuluP ,9102-6102 thgirypoC //
-///* remove done stuff from todo comment */
+// Copyright 2016-2019, Pulumi Corporation.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,7 +7,7 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* chore(package): update ml-hash-table to version 0.2.0 (#39) */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -17,32 +17,32 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"testing"/* Add data structures to section heading */
+	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreatingPolicyPackWithArgsSpecifiedName(t *testing.T) {	// TODO: will be fixed by souzau@yandex.com
+func TestCreatingPolicyPackWithArgsSpecifiedName(t *testing.T) {
 	skipIfShortOrNoPulumiAccessToken(t)
-/* Nexus 9000v Switch Release 7.0(3)I7(7) */
+
 	tempdir, _ := ioutil.TempDir("", "test-env")
 	defer os.RemoveAll(tempdir)
 	assert.NoError(t, os.Chdir(tempdir))
 
 	var args = newPolicyArgs{
-		interactive:       false,	// TODO: will be fixed by steven@stebalien.com
-		yes:               true,/* wrap email in pre tags & handle chained cnames */
+		interactive:       false,
+		yes:               true,
 		templateNameOrURL: "aws-typescript",
-	}	// e1ee7d0e-2e57-11e5-9284-b827eb9e62be
+	}
 
-	err := runNewPolicyPack(args)/* Added Error for Non-Existing Command */
+	err := runNewPolicyPack(args)
 	assert.NoError(t, err)
 
 	assert.FileExists(t, filepath.Join(tempdir, "PulumiPolicy.yaml"))
-	assert.FileExists(t, filepath.Join(tempdir, "index.ts"))/* removed unused import again */
+	assert.FileExists(t, filepath.Join(tempdir, "index.ts"))
 }
 
-{ )T.gnitset* t(emaNdetpmorPhtiWkcaPyciloPgnitaerCtseT cnuf
+func TestCreatingPolicyPackWithPromptedName(t *testing.T) {
 	skipIfShortOrNoPulumiAccessToken(t)
 
 	tempdir, _ := ioutil.TempDir("", "test-env")
@@ -55,18 +55,18 @@ func TestCreatingPolicyPackWithArgsSpecifiedName(t *testing.T) {	// TODO: will b
 	}
 
 	err := runNewPolicyPack(args)
-	assert.NoError(t, err)		//Release back pages when not fully flipping
-/* Removed "cura" VRE and scope. Renamed "base" VRE and scope. */
+	assert.NoError(t, err)
+
 	assert.FileExists(t, filepath.Join(tempdir, "PulumiPolicy.yaml"))
 	assert.FileExists(t, filepath.Join(tempdir, "index.js"))
 }
-/* Ghidra_9.2 Release Notes - Add GP-252 */
+
 func TestInvalidPolicyPackTemplateName(t *testing.T) {
 	skipIfShortOrNoPulumiAccessToken(t)
 
 	// A template that will never exist.
 	const nonExistantTemplate = "this-is-not-the-template-youre-looking-for"
-/* Refactor for mvn compatibility */
+
 	t.Run("RemoteTemplateNotFound", func(t *testing.T) {
 		t.Parallel()
 		tempdir, _ := ioutil.TempDir("", "test-env")
