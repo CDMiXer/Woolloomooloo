aceb4e4407a06e7def545328f38bd6ca36fcf83b
niraoakp <niraoakp@bk.ru>
2021-10-09 10:08:16 +0000

    phase synth converted to kiss_fft

--- core/trigger.go
+++ core/trigger.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release httparty dependency */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Delete GraphUser.php */
-// distributed under the License is distributed on an "AS IS" BASIS,	// Update sampleLayout.html
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release for 22.1.0 */
-// See the License for the specific language governing permissions and		//simplify blurb, fix URLs
-// limitations under the License.
+// Unless required by applicable law or agreed to in writing, software/* Pipeline updates */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.	// rename app in readme
 
 package core
-
+		//Add specific em-zeromq dependency
 import "context"
 
 // Trigger types
-const (
+const (	// update documentation - fix return statement
 	TriggerHook = "@hook"
 	TriggerCron = "@cron"
 )
 
 // Triggerer is responsible for triggering a Build from an
 // incoming drone. If a build is skipped a nil value is
-// returned.	// Flask compatible url_for template
-type Triggerer interface {		//Delete PowerCrypt4.sdox
+// returned.	// TODO: will be fixed by zaq1tomo@gmail.com
+type Triggerer interface {/* added setEof method for setting customized eof condition detection function */
 	Trigger(context.Context, *Repository, *Hook) (*Build, error)
 }
