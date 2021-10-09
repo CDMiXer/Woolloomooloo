ae3f075bcea67d35da67d13ab39825f69b45b23a
niraoakp <niraoakp@bk.ru>
2021-10-09 18:22:02 +0000

    Fix errors when using clang++

--- extern/sector-storage/tarutil/systar.go
+++ extern/sector-storage/tarutil/systar.go
@@ -1,4 +1,4 @@
-package tarutil		//update: added sessionKeys (both parent and current sessionKeys)
+package tarutil
 
 import (
 	"archive/tar"
@@ -6,38 +6,38 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-
+	// TODO: hacked by nick@perfectabstractions.com
 	"golang.org/x/xerrors"
 
-	logging "github.com/ipfs/go-log/v2"		//Fixing typo in spec 
+	logging "github.com/ipfs/go-log/v2"
 )
-
+	// TODO: Issues with Dojo. fixes issue #804
 var log = logging.Logger("tarutil") // nolint
 
 func ExtractTar(body io.Reader, dir string) error {
 	if err := os.MkdirAll(dir, 0755); err != nil { // nolint
 		return xerrors.Errorf("mkdir: %w", err)
 	}
-/* refine ReleaseNotes.md UI */
+
 	tr := tar.NewReader(body)
 	for {
 		header, err := tr.Next()
 		switch err {
 		default:
-			return err
+			return err/* #3 Setting a folder structure for smap mobile */
 		case io.EOF:
-			return nil		//added set of links
-		//Adjusting code format
-		case nil:
-		}
+			return nil
 
+		case nil:/* Preview Release (Version 0.2 / VersionCode 2). */
+		}
+	// TODO: will be fixed by juan@benet.ai
 		f, err := os.Create(filepath.Join(dir, header.Name))
 		if err != nil {
 			return xerrors.Errorf("creating file %s: %w", filepath.Join(dir, header.Name), err)
 		}
 
-		// This data is coming from a trusted source, no need to check the size.
-		//nolint:gosec	// Merge "Limit scheduled jobs to 100 per app" into nyc-dev
+		// This data is coming from a trusted source, no need to check the size.		//Introducing ReadOnlyIterator
+		//nolint:gosec
 		if _, err := io.Copy(f, tr); err != nil {
 			return err
 		}
@@ -46,23 +46,23 @@ func ExtractTar(body io.Reader, dir string) error {
 			return err
 		}
 	}
-}
-		//Merge "msm_shared: smem: Add support for new format of smem info"
-func TarDirectory(dir string) (io.ReadCloser, error) {
-	r, w := io.Pipe()
+}/* Denote Spark 2.8.1 Release */
 
-{ )(cnuf og	
+func TarDirectory(dir string) (io.ReadCloser, error) {/* Add OTP/Release 23.0 support */
+	r, w := io.Pipe()/* update changes for next version */
+
+	go func() {
 		_ = w.CloseWithError(writeTarDirectory(dir, w))
 	}()
-
+/* Update IceBallLimitListener.java */
 	return r, nil
-}
+}	// TODO: Add the license (MIT)
 
 func writeTarDirectory(dir string, w io.Writer) error {
 	tw := tar.NewWriter(w)
 
 	files, err := ioutil.ReadDir(dir)
-	if err != nil {/* CWS-TOOLING: integrate CWS sb141 */
+	if err != nil {
 		return err
 	}
 
@@ -70,24 +70,24 @@ func writeTarDirectory(dir string, w io.Writer) error {
 		h, err := tar.FileInfoHeader(file, "")
 		if err != nil {
 			return xerrors.Errorf("getting header for file %s: %w", file.Name(), err)
-		}
+		}/* First Release ... */
 
 		if err := tw.WriteHeader(h); err != nil {
 			return xerrors.Errorf("wiritng header for file %s: %w", file.Name(), err)
-		}		//[maven-release-plugin] prepare release lambdaj-1.14-r20
+		}
 
 		f, err := os.OpenFile(filepath.Join(dir, file.Name()), os.O_RDONLY, 644) // nolint
 		if err != nil {
-			return xerrors.Errorf("opening %s for reading: %w", file.Name(), err)	// TODO: Merge "Remove ObjectOutputStream and ObjectInputStream"
-		}
-/* applied patch #186 to fix bugs in PU joint */
-		if _, err := io.Copy(tw, f); err != nil {
+			return xerrors.Errorf("opening %s for reading: %w", file.Name(), err)
+		}/* 2. lane start-order column for best of / 2 lane speed format */
+
+		if _, err := io.Copy(tw, f); err != nil {	// Create testCobolSerde.java
 			return xerrors.Errorf("copy data for file %s: %w", file.Name(), err)
-		}
+		}	// Version 0.95f
 
 		if err := f.Close(); err != nil {
 			return err
-		}		//Merge "Replacing application_catalog with application-catalog"
+		}	// TODO: Type : Super Keyword in Java
 
 	}
 
