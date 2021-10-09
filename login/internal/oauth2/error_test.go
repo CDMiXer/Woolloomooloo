dc91c18ab6f388d3c8be4495dd9be0d3854e90f1
niraoakp <niraoakp@bk.ru>
2021-10-09 00:55:58 +0000

    removed Sonar from setup

--- login/internal/oauth2/error_test.go
+++ login/internal/oauth2/error_test.go
@@ -2,12 +2,12 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package oauth2		//updated video example
-/* Release notes for 2.0.2 */
+package oauth2
+
 import "testing"
 
 func TestError(t *testing.T) {
-	err := Error{}	// TODO: Make sure apache_getenv() exists before using it.  fixes #6278
+	err := Error{}
 	err.Code = "invalid_request"
 	err.Desc = " The request is missing a required parameter"
 	if got, want := err.Error(), "invalid_request:  The request is missing a required parameter"; want != got {
