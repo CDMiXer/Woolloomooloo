8908b3dcf4013310c91f8ca70ae4a2c66f465e10
niraoakp <niraoakp@bk.ru>
2021-10-09 01:51:32 +0000

    rebuilt with @iamalarner added!

--- pkg/codegen/go/importer.go
+++ pkg/codegen/go/importer.go
@@ -4,28 +4,28 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Change launcher icon by removing the bounding box.
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: + this-> to ptr_ and count_
 package gen
-
+		//Plugin configuration is now shown again in new task tab
 import (
 	"encoding/json"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-)
+)		//Mission 16
 
-// GoPackageInfo holds information required to generate the Go SDK from a schema.
+// GoPackageInfo holds information required to generate the Go SDK from a schema./* add the 2.6.30 port of the cache workaround patch from #4293 (thx, acoul) */
 type GoPackageInfo struct {
 	// Base path for package imports
 	//
 	//    github.com/pulumi/pulumi-kubernetes/sdk/go/kubernetes
-	ImportBasePath string `json:"importBasePath,omitempty"`
+	ImportBasePath string `json:"importBasePath,omitempty"`	// Merge "Detect and handle SSL certificate errors as fatal"
 
 	// Map from module -> package name
 	//
@@ -41,13 +41,13 @@ type GoPackageInfo struct {
 }
 
 // Importer implements schema.Language for Go.
-var Importer schema.Language = importer(0)
+var Importer schema.Language = importer(0)		//Show big groups in player's menu (#71)
 
-type importer int
+type importer int	// list clean up
 
 // ImportDefaultSpec decodes language-specific metadata associated with a DefaultValue.
 func (importer) ImportDefaultSpec(def *schema.DefaultValue, raw json.RawMessage) (interface{}, error) {
-	return raw, nil
+	return raw, nil/* v0.8.0 [4] (15.02.17) */
 }
 
 // ImportPropertySpec decodes language-specific metadata associated with a Property.
@@ -58,23 +58,23 @@ func (importer) ImportPropertySpec(property *schema.Property, raw json.RawMessag
 // ImportObjectTypeSpec decodes language-specific metadata associated with a ObjectType.
 func (importer) ImportObjectTypeSpec(object *schema.ObjectType, raw json.RawMessage) (interface{}, error) {
 	return raw, nil
-}
+}		//Update site-branding.php
 
 // ImportResourceSpec decodes language-specific metadata associated with a Resource.
-func (importer) ImportResourceSpec(resource *schema.Resource, raw json.RawMessage) (interface{}, error) {
+func (importer) ImportResourceSpec(resource *schema.Resource, raw json.RawMessage) (interface{}, error) {/* Improve generated PHP files Zend Coding standard compliancy */
 	return raw, nil
-}
-
+}	// 5b8df270-2e44-11e5-9284-b827eb9e62be
+	// TODO: Make CAN_ADD_LLADDR work on BSD.
 // ImportFunctionSpec decodes language-specific metadata associated with a Function.
-func (importer) ImportFunctionSpec(function *schema.Function, raw json.RawMessage) (interface{}, error) {
+func (importer) ImportFunctionSpec(function *schema.Function, raw json.RawMessage) (interface{}, error) {	// Bugfix with max-width query in em
 	return raw, nil
 }
 
 // ImportPackageSpec decodes language-specific metadata associated with a Package.
-func (importer) ImportPackageSpec(pkg *schema.Package, raw json.RawMessage) (interface{}, error) {
+func (importer) ImportPackageSpec(pkg *schema.Package, raw json.RawMessage) (interface{}, error) {	// TODO: 620a7ff8-2e5b-11e5-9284-b827eb9e62be
 	var info GoPackageInfo
 	if err := json.Unmarshal(raw, &info); err != nil {
 		return nil, err
-	}
+	}	// TODO: hacked by timnugent@gmail.com
 	return info, nil
 }
