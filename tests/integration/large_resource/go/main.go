569661bf40afb7a9d012c2fd6c7ad4269331966a
niraoakp <niraoakp@bk.ru>
2021-10-09 22:39:07 +0000

    Fixed problem with listening to recipes instead of continuing

--- tests/integration/large_resource/go/main.go
+++ tests/integration/large_resource/go/main.go
@@ -1,15 +1,15 @@
 package main
 
-import (
+import (	// TODO: add an invalid alias file for test purposes
 	"strings"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)/* also info about config file */
+)
 
-func main() {	// TODO: improved photo cropping.
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create and export a very long string (>4mb)
 		ctx.Export("longString", pulumi.String(strings.Repeat("a", 5*1024*1024)))
 		return nil
 	})
-}		//Merge branch 'master' into g105b-patch-1
+}
