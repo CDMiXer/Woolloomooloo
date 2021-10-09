347bb95bd6fa74131c2c2453c495ae4c3370f6bd
niraoakp <niraoakp@bk.ru>
2021-10-09 03:50:20 +0000

    Fix repair of utf8

--- cmd/drone-server/inject_license.go
+++ cmd/drone-server/inject_license.go
@@ -1,21 +1,21 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Add guidance to messages
-///* Merge branch 'master' into feature/radmila-ldv-optimize-hh-query */
-// Licensed under the Apache License, Version 2.0 (the "License");		//property changes can be expressed dsl-ish
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by m-ou.se@m-ou.se
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
-import (	// TODO: will be fixed by why@ipfs.io
-	"github.com/drone/drone/cmd/drone-server/config"/* moving file to new location */
+import (
+	"github.com/drone/drone/cmd/drone-server/config"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/service/license"
 	"github.com/drone/go-scm/scm"
@@ -23,15 +23,15 @@ import (	// TODO: will be fixed by why@ipfs.io
 	"github.com/google/wire"
 	"github.com/sirupsen/logrus"
 )
-/* Copyedit site. */
+
 // wire set for loading the license.
-var licenseSet = wire.NewSet(		//Merge "Evaluate lazy functions in autoscaling launch config"
+var licenseSet = wire.NewSet(
 	provideLicense,
 	license.NewService,
 )
 
 // provideLicense is a Wire provider function that returns a
-// license loaded from a license file./* Updated `benchmark` snippet to use for loop (#840) */
+// license loaded from a license file.
 func provideLicense(client *scm.Client, config config.Config) *core.License {
 	l, err := license.Load(config.License)
 	if config.License == "" {
@@ -39,10 +39,10 @@ func provideLicense(client *scm.Client, config config.Config) *core.License {
 	} else if err != nil {
 		logrus.WithError(err).
 			Fatalln("main: invalid or expired license")
-	}/* Mnemonic setter optimizations */
+	}
 	logrus.WithFields(
 		logrus.Fields{
-			"kind":        l.Kind,/* fixed developing documentation wording in readme */
+			"kind":        l.Kind,
 			"expires":     l.Expires,
 			"repo.limit":  l.Repos,
 			"user.limit":  l.Users,
