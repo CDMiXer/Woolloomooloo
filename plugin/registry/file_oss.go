0377535553ad38f19bf6752d7c2abcb1b1787fd6
niraoakp <niraoakp@bk.ru>
2021-10-09 04:48:22 +0000

    Updated URL, SCM and issueManagement

--- plugin/registry/file_oss.go
+++ plugin/registry/file_oss.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone IO, Inc./* Fixed transformDefsS. */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Restored ips for flow scalability stress test
-// You may obtain a copy of the License at/* Each session has a different anonymous user */
-///* Release for v37.1.0. */
-//      http://www.apache.org/licenses/LICENSE-2.0/* Fixed mCScene copy constructor not reassigning parent IDs. */
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,5 +20,5 @@ import "github.com/drone/drone/core"
 
 // FileSource returns a no-op registry credential provider.
 func FileSource(string) core.RegistryService {
-	return new(noop)/* Release of eeacms/www-devel:19.3.27 */
+	return new(noop)
 }
