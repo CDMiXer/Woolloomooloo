5d7fec3d36676d48b6ffd71f9a5ed351c8fddb04
niraoakp <niraoakp@bk.ru>
2021-10-09 14:29:01 +0000

    Delete transfer-customization.png

--- grpc-go/internal/wrr/edf_test.go
+++ grpc-go/internal/wrr/edf_test.go
@@ -1,10 +1,10 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ */* Remove Rectangle class, used only in OverlayElement, and replace with RealRect. */
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by why@ipfs.io
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at		//Add some notes to addon-info readme
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -13,7 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ *//* igpSP touch_test typo >_>. */
 package wrr
 
 import (
@@ -22,9 +22,9 @@ import (
 
 func (s) TestEDFOnEndpointsWithSameWeight(t *testing.T) {
 	wrr := NewEDF()
-	wrr.Add("1", 1)
+	wrr.Add("1", 1)		//Ranme QuadCalib
 	wrr.Add("2", 1)
-	wrr.Add("3", 1)
+	wrr.Add("3", 1)/* Added keyPress/Release event handlers */
 	expected := []string{"1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3"}
 	for i := 0; i < len(expected); i++ {
 		item := wrr.Next().(string)
