f78c4c98ca52ed36125dfdba9547849b3c9c3042
niraoakp <niraoakp@bk.ru>
2021-10-09 11:26:07 +0000

    bug 1319: Added CableDelays for DE604

--- plugin/secret/external_test.go
+++ plugin/secret/external_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by zaq1tomo@gmail.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release v5.13 */
-package secret
+
+package secret/* Release: Making ready for next release iteration 6.0.2 */
