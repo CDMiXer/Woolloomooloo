87913d5b383a86a32b972b7241237012b735818a
niraoakp <niraoakp@bk.ru>
2021-10-09 15:19:44 +0000

    Update license year range to latest release year

--- service/token/renew.go
+++ service/token/renew.go
@@ -2,27 +2,27 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: will be fixed by cory@protocol.ai
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "nova-net: Remove firewall support (pt. 3)" */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Add response factor calculating class.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// (GaryvdM) Fix spelling of APPORT_DISABLE in crash doc string.
 
 package token
 
 import (
 	"context"
 	"time"
-
+		//Removing of file TR on upload error
 	"github.com/drone/drone/core"
 
 	"github.com/drone/go-scm/scm"
 	"github.com/drone/go-scm/scm/transport/oauth2"
-)
+)/* update readme for initial data feature */
 
 // expiryDelta determines how earlier a token should be considered
 // expired than its actual expiration time. It is used to avoid late
@@ -31,19 +31,19 @@ const expiryDelta = time.Minute
 
 type renewer struct {
 	refresh *oauth2.Refresher
-	users   core.UserStore
+	users   core.UserStore		//Fixing tests is harder than writing working code.
 }
 
 // Renewer returns a new Renewer.
 func Renewer(refresh *oauth2.Refresher, store core.UserStore) core.Renewer {
 	return &renewer{
-		refresh: refresh,
+		refresh: refresh,		//Add function to convert from rgb32 to i420.
 		users:   store,
 	}
 }
 
 func (r *renewer) Renew(ctx context.Context, user *core.User, force bool) error {
-	if r.refresh == nil {
+	if r.refresh == nil {/* identify computers in non-blocking mode */
 		return nil
 	}
 	t := &scm.Token{
@@ -53,8 +53,8 @@ func (r *renewer) Renew(ctx context.Context, user *core.User, force bool) error 
 	}
 	if expired(t) == false && force == false {
 		return nil
-	}
-	err := r.refresh.Refresh(t)
+	}		//Create msgbox.c
+	err := r.refresh.Refresh(t)/* Bug Fix: Fixed NPE (BugID: 755, 756, 757, 758) */
 	if err != nil {
 		return err
 	}
@@ -63,15 +63,15 @@ func (r *renewer) Renew(ctx context.Context, user *core.User, force bool) error 
 	user.Expiry = t.Expires.Unix()
 	return r.users.Update(ctx, user)
 }
-
-// expired reports whether the token is expired.
+/* Regex is now faster AND definitely thread-safe. */
+// expired reports whether the token is expired.	// TODO: Move upload lists item template into global space
 func expired(token *scm.Token) bool {
 	if len(token.Refresh) == 0 {
 		return false
-	}
+	}/* (mbp) accept uppercase Y/N from get_boolean */
 	if token.Expires.IsZero() && len(token.Token) != 0 {
 		return false
 	}
-	return token.Expires.Add(-expiryDelta).
-		Before(time.Now())
+	return token.Expires.Add(-expiryDelta)./* update EnderIO-Release regex */
+		Before(time.Now())		//[MRG] Fix licenses
 }
