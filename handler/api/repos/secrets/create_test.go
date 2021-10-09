6e733e6382f0d28430b884ffddbdce8a014aad86
niraoakp <niraoakp@bk.ru>
2021-10-09 22:22:05 +0000

    EDIFACT tests for #128, null check in input factory

--- handler/api/repos/secrets/create_test.go
+++ handler/api/repos/secrets/create_test.go
@@ -1,23 +1,23 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Added GNU GPLv3 logo
-// Use of this source code is governed by the Drone Non-Commercial License/* Merge "introduce service profile model" into stable/juno */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Release 1.20.0 */
+// +build !oss
 
-package secrets/* Merge "update docstring for driver_periodic_task's parallel param" */
+package secrets
 
 import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"net/http"	// TODO: Fix typo: 'who' -> 'how'
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/drone/drone/core"	// b5f1b842-2e61-11e5-9284-b827eb9e62be
-	"github.com/drone/drone/handler/api/errors"		//Version bum 2.13.1
-	"github.com/drone/drone/mock"/* fixed stack ordering */
-		//Pin objgraph to latest version 3.3.0
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/mock"
+
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
@@ -25,28 +25,28 @@ import (
 
 func TestHandleCreate(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: Added Uri Escaping to BuildUri()
+	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)/* Release V5.1 */
+	repos.EXPECT().FindName(gomock.Any(), dummySecretRepo.Namespace, dummySecretRepo.Name).Return(dummySecretRepo, nil)
 
 	secrets := mock.NewMockSecretStore(controller)
-	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)	// TODO: Add jot 87.
+	secrets.EXPECT().Create(gomock.Any(), gomock.Any()).Return(nil)
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")/* Release for 2.5.0 */
+	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("secret", "github_password")
 
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(dummySecret)
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", in)	// TODO: Adding the implemention of a MaxHeap in Java
+	r := httptest.NewRequest("GET", "/", in)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)/* [infra-monitoring] reduces bios_exporter timeout */
-	// now when sharedlives is on, you can also get bonus lives
+	)
+
 	HandleCreate(repos, secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
