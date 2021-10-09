24cc18ea2f30aa17fb155f7940fe13c1d029db04
niraoakp <niraoakp@bk.ru>
2021-10-09 17:23:33 +0000

    chore(package): update puppeteer to version 1.7.0

--- tests/integration/empty/gorun_main/gorun_main_src/main.go
+++ tests/integration/empty/gorun_main/gorun_main_src/main.go
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-package main
+package main/* need to make sure variables are right lol */
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
