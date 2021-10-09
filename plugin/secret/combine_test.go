2d850121039fa121a2b0428e738c9053a51c9d17
niraoakp <niraoakp@bk.ru>
2021-10-09 12:12:16 +0000

    Merge "msm: display: kickoff lock release centralization."

--- plugin/secret/combine_test.go
+++ plugin/secret/combine_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Convert ReleasegroupFilter from old logger to new LOGGER slf4j */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release v0.02 */
 
 package secret
