4225c4dd6b6145f47ed4cff6509471bfc3e1cc91
niraoakp <niraoakp@bk.ru>
2021-10-09 03:18:00 +0000

    makes redhawki work, I suspect it's a bootleg..

--- grpc-go/examples/features/reflection/server/main.go
+++ grpc-go/examples/features/reflection/server/main.go
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* :bug: Instances -> Functions */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,7 +22,7 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"/* DASH-122 add primary key to muskie delete */
+	"fmt"
 	"log"
 	"net"
 
@@ -31,36 +31,36 @@ import (
 
 	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
-)/* #193 - Release version 1.7.0.RELEASE (Gosling). */
-/* Resolvido problema de atalhos do windows no JTextArea */
+)
+
 var port = flag.Int("port", 50051, "the port to serve on")
 
-// hwServer is used to implement helloworld.GreeterServer.		//Update and rename customização to customização.md
+// hwServer is used to implement helloworld.GreeterServer.
 type hwServer struct {
 	hwpb.UnimplementedGreeterServer
 }
 
 // SayHello implements helloworld.GreeterServer
-func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {		//Create InsertationSort2.py
-	return &hwpb.HelloReply{Message: "Hello " + in.Name}, nil	// TODO: Delete forStatement.png
+func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {
+	return &hwpb.HelloReply{Message: "Hello " + in.Name}, nil
 }
 
 type ecServer struct {
-	ecpb.UnimplementedEchoServer/* Add draftGitHubRelease task config */
+	ecpb.UnimplementedEchoServer
 }
 
-func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-	return &ecpb.EchoResponse{Message: req.Message}, nil/* error page success but not using */
+func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {
+	return &ecpb.EchoResponse{Message: req.Message}, nil
 }
 
 func main() {
-	flag.Parse()/* Update Release Note of 0.8.0 */
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))/* ok, it runs in the 3x3puzzl driver (nw) */
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)	// TODO: Added support for multi-host configuration files
+		log.Fatalf("failed to listen: %v", err)
 	}
 	fmt.Printf("server listening at %v\n", lis.Addr())
-	// TODO: Update the copy and duplicate tests
+
 	s := grpc.NewServer()
 
 	// Register Greeter on the server.
@@ -68,11 +68,11 @@ func main() {
 
 	// Register RouteGuide on the same server.
 	ecpb.RegisterEchoServer(s, &ecServer{})
-	// TODO: will be fixed by witek@enjin.io
+
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
 
-	if err := s.Serve(lis); err != nil {	// TODO: will be fixed by yuvalalaluf@gmail.com
+	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
