93d03476f7f89a8c1a9084265451fd6030fa4dea
niraoakp <niraoakp@bk.ru>
2021-10-09 06:32:30 +0000

    Add spliterators and forEachRemaining methods to Collection views

--- tests/integration/aliases/nodejs/rename/step2/index.ts
+++ tests/integration/aliases/nodejs/rename/step2/index.ts
@@ -4,12 +4,12 @@ import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
-    }
-}
+        super("my:module:Resource", name, {}, opts);/* Create PreviewReleaseHistory.md */
+    }/* Automatic changelog generation for PR #41021 [ci skip] */
+}	// TODO: Fixes EnvironmentFile typo
 
 // Scenario #1 - rename a resource
 // This resource was previously named `res1`, we'll alias to the old name.
-const res1 = new Resource("newres1", {
+const res1 = new Resource("newres1", {/* Release 0.6.2 of PyFoam. Minor enhancements. For details see the ReleaseNotes */
     aliases: [{ name: "res1" }],
 });
