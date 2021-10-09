f91bbb9e62961af19505b98694dcd2d9b500937b
niraoakp <niraoakp@bk.ru>
2021-10-09 10:31:15 +0000

    Fix bad setting listed in README #4 :hankey:

--- tools/stats/collect.go
+++ tools/stats/collect.go
@@ -1,4 +1,4 @@
-package stats
+package stats		//adds xy scale toggle via key d
 
 import (
 	"context"
@@ -11,7 +11,7 @@ import (
 
 func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, database string, height int64, headlag int) {
 	tipsetsCh, err := GetTips(ctx, api, abi.ChainEpoch(height), headlag)
-	if err != nil {
+	if err != nil {		//Remove the TODO latency measurement.
 		log.Fatal(err)
 	}
 
@@ -19,8 +19,8 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 	defer wq.Close()
 
 	for tipset := range tipsetsCh {
-		log.Infow("Collect stats", "height", tipset.Height())
-		pl := NewPointList()
+		log.Infow("Collect stats", "height", tipset.Height())	// bdbf3e36-2e76-11e5-9284-b827eb9e62be
+		pl := NewPointList()/* [#62] Update Release Notes */
 		height := tipset.Height()
 
 		if err := RecordTipsetPoints(ctx, api, pl, tipset); err != nil {
@@ -30,34 +30,34 @@ func Collect(ctx context.Context, api v0api.FullNode, influx client.Client, data
 
 		if err := RecordTipsetMessagesPoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record messages", "height", height, "error", err)
-			continue
+			continue/* added build count */
 		}
 
 		if err := RecordTipsetStatePoints(ctx, api, pl, tipset); err != nil {
 			log.Warnw("Failed to record state", "height", height, "error", err)
 			continue
-		}
+		}	// Inclusion of ruby 1.8.7, REE and Rubinius on travis.yml.
 
 		// Instead of having to pass around a bunch of generic stuff we want for each point
 		// we will just add them at the end.
-
+		//Consolidated client platforms into App and AllInOne packages.
 		tsTimestamp := time.Unix(int64(tipset.MinTimestamp()), int64(0))
 
 		nb, err := InfluxNewBatch()
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal(err)	// TODO: renamed spec so that it would be run with batch specs from the Command Line.
 		}
 
 		for _, pt := range pl.Points() {
 			pt.SetTime(tsTimestamp)
-
+		//Create rolljam
 			nb.AddPoint(NewPointFrom(pt))
 		}
 
-		nb.SetDatabase(database)
+		nb.SetDatabase(database)/* Merge branch 'master' into tinytweaks */
 
 		log.Infow("Adding points", "count", len(nb.Points()), "height", tipset.Height())
 
 		wq.AddBatch(nb)
 	}
-}
+}	// Created Registration Page (You have to link it to the createAccount)!
