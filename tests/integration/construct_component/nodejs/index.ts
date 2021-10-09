cc6f5fce20aaae8c7987fdb79cbae54e21aa6642
niraoakp <niraoakp@bk.ru>
2021-10-09 11:45:03 +0000

    Added include attribute

--- tests/integration/construct_component/nodejs/index.ts
+++ tests/integration/construct_component/nodejs/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-import { Component } from "./component";
+import { Component } from "./component";		//Fixed some bugs related with collation_connection
 
 const componentA = new Component("a", {echo: 42});
 const componentB = new Component("b", {echo: componentA.echo});
