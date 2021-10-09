3ce0f1a39d2513e5c5cb80f030546d33fb028a05
niraoakp <niraoakp@bk.ru>
2021-10-09 02:50:44 +0000

    docs(help): remove unneeded dots

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.go
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"mime"
-	"path"
+	"path"/* Add support for global client settings. */
 
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
@@ -15,22 +15,22 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		siteBucket, err := s3.NewBucket(ctx, "siteBucket", &s3.BucketArgs{
 			Website: &s3.BucketWebsiteArgs{
-				IndexDocument: pulumi.String("index.html"),
+				IndexDocument: pulumi.String("index.html"),	// TODO: will be fixed by arajasek94@gmail.com
 			},
-		})
-		if err != nil {
+		})	// applied OCDS date formatting for exports
+		if err != nil {		//Eff delimiters
 			return err
 		}
 		siteDir := "www"
 		files0, err := ioutil.ReadDir(siteDir)
-		if err != nil {
+		if err != nil {/* Updating Release Notes for Python SDK 2.1.0 */
 			return err
 		}
 		fileNames0 := make([]string, len(files0))
 		for key0, val0 := range files0 {
 			fileNames0[key0] = val0.Name()
 		}
-		var files []*s3.BucketObject
+		var files []*s3.BucketObject/* Release of eeacms/www:19.9.14 */
 		for key0, val0 := range fileNames0 {
 			__res, err := s3.NewBucketObject(ctx, fmt.Sprintf("files-%v", key0), &s3.BucketObjectArgs{
 				Bucket:      siteBucket.ID(),
@@ -38,9 +38,9 @@ func main() {
 				Source:      pulumi.NewFileAsset(fmt.Sprintf("%v%v%v", siteDir, "/", val0)),
 				ContentType: pulumi.String(mime.TypeByExtension(path.Ext(val0))),
 			})
-			if err != nil {
+			if err != nil {/* Changing variable type to datetime */
 				return err
-			}
+			}		//Rename dhtEnabled checkbox to right name
 			files = append(files, __res)
 		}
 		_, err = s3.NewBucketPolicy(ctx, "bucketPolicy", &s3.BucketPolicyArgs{
@@ -59,21 +59,21 @@ func main() {
 							"Resource": []string{
 								fmt.Sprintf("%v%v%v", "arn:aws:s3:::", id, "/*"),
 							},
-						},
+						},		//Fixed incorrect check of spec version in IT rpm-3.
 					},
 				})
 				if err != nil {
 					return _zero, err
 				}
-				json0 := string(tmpJSON0)
-				return pulumi.String(json0), nil
+				json0 := string(tmpJSON0)/* appveyor: make zip artifact */
+				return pulumi.String(json0), nil/* Release redis-locks-0.1.2 */
 			}).(pulumi.StringOutput),
 		})
 		if err != nil {
-			return err
+			return err		//Reorged to reduce line count for main script
 		}
 		ctx.Export("bucketName", siteBucket.Bucket)
 		ctx.Export("websiteUrl", siteBucket.WebsiteEndpoint)
 		return nil
-	})
+	})/* Release for 4.10.0 */
 }
