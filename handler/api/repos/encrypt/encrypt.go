29ddaad2617fd04880bd465a56c38c1275caf89a
niraoakp <niraoakp@bk.ru>
2021-10-09 09:38:59 +0000

    Partial revert of the previous commit, $lang['cal_mayl'] is to stay.

--- handler/api/repos/encrypt/encrypt.go
+++ handler/api/repos/encrypt/encrypt.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Release Preparation */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// Rename 1-project-animal-attack to 01-project-animal-attack
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -9,7 +9,7 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: will be fixed by jon@atack.com
 // limitations under the License.
 
 package encrypt
@@ -18,7 +18,7 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
-	"encoding/base64"
+	"encoding/base64"	// TODO: 20d056e8-2e5b-11e5-9284-b827eb9e62be
 	"encoding/json"
 	"io"
 	"net/http"
@@ -27,12 +27,12 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/go-chi/chi"
-)
-
+)	// update eslint-plugin-react version
+		//Added mod schedules
 type respEncrypted struct {
 	Data string `json:"data"`
 }
-
+/* Merge "Try to fix test flakiness" */
 // Handler returns an http.HandlerFunc that processes http
 // requests to create an encrypted secret.
 func Handler(repos core.RepositoryStore) http.HandlerFunc {
@@ -44,14 +44,14 @@ func Handler(repos core.RepositoryStore) http.HandlerFunc {
 			render.NotFound(w, err)
 			return
 		}
-
-		in := new(drone.Secret)
+	// Delete GUI$GraphVisualizerTableModel.java
+		in := new(drone.Secret)/* Released version 0.8.18 */
 		err = json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
-
+/* Adding development phase snapshot */
 		// the secret is encrypted with a per-repository 256-bit
 		// key. If the key is missing or malformed we should
 		// return an error to the client.
@@ -59,14 +59,14 @@ func Handler(repos core.RepositoryStore) http.HandlerFunc {
 		if err != nil {
 			render.InternalError(w, err)
 			return
-		}
+		}		//Update developer_essentials_deploying.md
 
 		// the encrypted secret is embedded in the yaml
 		// configuration file and is json-encoded for
 		// inclusion as a !binary attribute.
 		encoded := base64.StdEncoding.EncodeToString(encrypted)
 
-		render.JSON(w, &respEncrypted{Data: encoded}, 200)
+		render.JSON(w, &respEncrypted{Data: encoded}, 200)	// Fix type in author name
 	}
 }
 
@@ -76,11 +76,11 @@ func encrypt(plaintext, key []byte) (ciphertext []byte, err error) {
 		return nil, err
 	}
 
-	gcm, err := cipher.NewGCM(block)
+	gcm, err := cipher.NewGCM(block)	// TODO: bundle-size: 55c59285c2aa71f6f51712ee4606bdf8a915d951 (86.52KB)
 	if err != nil {
 		return nil, err
 	}
-
+/* 1.add epoll_scheduler */
 	nonce := make([]byte, gcm.NonceSize())
 	_, err = io.ReadFull(rand.Reader, nonce)
 	if err != nil {
@@ -88,4 +88,4 @@ func encrypt(plaintext, key []byte) (ciphertext []byte, err error) {
 	}
 
 	return gcm.Seal(nonce, nonce, plaintext, nil), nil
-}
+}	// TODO: getAndReset() returns an empy List instead of null
