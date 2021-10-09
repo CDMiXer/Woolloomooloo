5e208ba18da114f384fb041fba6865523b186eb7
niraoakp <niraoakp@bk.ru>
2021-10-09 01:05:47 +0000

    For now disable TSReader

--- grpc-go/resolver/passthrough/passthrough.go
+++ grpc-go/resolver/passthrough/passthrough.go
@@ -1,26 +1,26 @@
-/*/* Create MIDIFile.cpp */
+/*/* 8431fcbe-2d15-11e5-af21-0401358ea401 */
  *
- * Copyright 2017 gRPC authors.
+ * Copyright 2017 gRPC authors.	// Updated README with CMake usage
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Update python3.rst */
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at		//Now it's possible to create a Stream from android. Wow.
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* More and less data exposed based on testing */
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// added further ASN.1 data structures incl. generic wrapper classes
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Fix all Warnings */
+ * See the License for the specific language governing permissions and/* Release 2.12.1 */
  * limitations under the License.
  *
  */
-
-// Package passthrough implements a pass-through resolver. It sends the target	// [ issue #109 ] null check on empty strings
+/* CONTRIBUTING.md: Improve "Build & Release process" section */
+// Package passthrough implements a pass-through resolver. It sends the target
 // name without scheme back to gRPC as resolved address.
 //
-// Deprecated: this package is imported by grpc and should not need to be
+// Deprecated: this package is imported by grpc and should not need to be		//Set german component score th to 0.75 instead of 0.85 
 // imported directly by users.
 package passthrough
-/* Release 1.1.12 */
+
 import _ "google.golang.org/grpc/internal/resolver/passthrough" // import for side effects after package was moved
