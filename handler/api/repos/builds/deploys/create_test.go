38dc854040ef5f3acc608143f01432caa6c15f7b
niraoakp <niraoakp@bk.ru>
2021-10-09 21:11:48 +0000

    ** Removed unused imports from StudentTestsBase

--- handler/api/repos/builds/deploys/create_test.go
+++ handler/api/repos/builds/deploys/create_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* $LIT_IMPORT_PLUGINS verschoben, wie im Release */
 // that can be found in the LICENSE file.
-
+		//Merge "Improve stack adopt reponse sample"
 package deploys
