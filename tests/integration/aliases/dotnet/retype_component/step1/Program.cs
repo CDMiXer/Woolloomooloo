8e982f8dac57f8aeecb5f155fb572fec6e8929af
niraoakp <niraoakp@bk.ru>
2021-10-09 20:02:51 +0000

    Avoid NPE in Logger.setDest(). Patch by Mark (fixes #1107)

--- tests/integration/aliases/dotnet/retype_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step1/Program.cs
@@ -1,35 +1,35 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-/* Release version 0.1.24 */
-using System.Threading.Tasks;
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* turn on msm_otg for ycable testing */
+		//Update sidekiq to version 5.0.1
+using System.Threading.Tasks;/* Added Map tests */
 using Pulumi;
 
-class Resource : ComponentResource/* Create CSS3: Grid layout */
+class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
-        : base("my:module:Resource", name, options)/* Referring to the latest version of the Drillster API. */
+        : base("my:module:Resource", name, options)
     {
     }
-}/* Update ngTagsInput@3.0.0.json */
+}/* First Release- */
 
 // Scenario #4 - change the type of a component
 class ComponentFour : ComponentResource
-{
+{		//botlib: uncrustify
     private Resource resource;
 
     public ComponentFour(string name, ComponentResourceOptions options = null)
-        : base("my:module:ComponentFour", name, options)/* Release 8.8.2 */
+        : base("my:module:ComponentFour", name, options)
     {
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
 }
 
-class Program		//do not disable Science anymore if ScienceRelay is detected
+class Program
 {
     static Task<int> Main(string[] args)
-    {
+    {	// TODO: CMS update of messaging/services/service-number-add by dprothero@twilio.com
         return Deployment.RunAsync(() => 
         {
             var comp4 = new ComponentFour("comp4");
-        });
-}    
+        });/* Fixed bug with the immutable api */
+    }/* Merge "Update FSTrigger plugin" */
 }
