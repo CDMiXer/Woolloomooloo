7372833e504450ea4aa112c9d5201ff7c73c0525
niraoakp <niraoakp@bk.ru>
2021-10-09 17:26:15 +0000

    fix todo formatting

--- tests/integration/targets/untargeted_create/index.ts
+++ tests/integration/targets/untargeted_create/index.ts
@@ -3,13 +3,13 @@
 import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
-
+/* [artifactory-release] Release version 1.1.1.M1 */
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
 
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    constructor() {
+    constructor() {/* new method processing seems to work except for @Param/@Release handling */
         this.create = async (inputs: any) => {
             return {
                 id: (currentID++) + "",
@@ -18,10 +18,10 @@ class Provider implements pulumi.dynamic.ResourceProvider {
         };
     }
 }
-
-class Resource extends pulumi.dynamic.Resource {
+		//bundle-size: 8b087ec1adef158e1686d60753323d43cbd75c34 (88.04KB)
+class Resource extends pulumi.dynamic.Resource {/* skriver faktisk til databasen nå ;) */
     constructor(name: string, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, {}, opts);
+        super(Provider.instance, name, {}, opts);/* Led management added */
     }
 }
 
