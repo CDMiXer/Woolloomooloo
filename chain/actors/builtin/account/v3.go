6a6fb20940b2fa644b2f3a7a83d5200ccf671a7b
niraoakp <niraoakp@bk.ru>
2021-10-09 10:51:10 +0000

    Forgot to commit an icon.

--- chain/actors/builtin/account/v3.go
+++ chain/actors/builtin/account/v3.go
@@ -7,24 +7,24 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"
-)		//edited controller specs to reflect routing edits
+)
 
 var _ State = (*state3)(nil)
 
-func load3(store adt.Store, root cid.Cid) (State, error) {		//Upload common/third party
+func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {/* Release of eeacms/plonesaas:5.2.1-34 */
+	if err != nil {
 		return nil, err
 	}
-	return &out, nil	// TODO: Apply proper GPL headers to JavaDoc HTML fragments
+	return &out, nil
 }
 
 type state3 struct {
 	account3.State
 	store adt.Store
 }
-/* Tagging roundup 1.3.0 */
+
 func (s *state3) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
 }
