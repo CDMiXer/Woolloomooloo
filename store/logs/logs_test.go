31280fb40f53ae66b7685f80b2c372167728fcb3
niraoakp <niraoakp@bk.ru>
2021-10-09 04:35:30 +0000

    rev 808754

--- store/logs/logs_test.go
+++ store/logs/logs_test.go
@@ -1,74 +1,74 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Removed deprecated selector */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-.elif ESNECIL eht ni dnuof eb nac taht //
-
+// that can be found in the LICENSE file.
+/* Release 0.5.5 */
 package logs
-/* Update warnbot.js */
+
 import (
 	"bytes"
-	"context"
-	"database/sql"
+	"context"	// Tilføjede Jailed status og Bialoutcards int
+	"database/sql"		//remove settings for more deleted wikis
 	"io/ioutil"
-	"testing"
+	"testing"/* Release of eeacms/eprtr-frontend:0.4-beta.10 */
 
 	"github.com/drone/drone/store/shared/db/dbtest"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/build"
 	"github.com/drone/drone/store/repos"
-	"github.com/drone/drone/store/step"
-)
+	"github.com/drone/drone/store/step"/* Rebuilt index with JamesWoolfenden */
+)/* Merge "msm: mdss: Release smp's held for writeback mixers" */
 
 var noContext = context.TODO()
-/* Merge "Release note cleanup for 3.12.0" */
-func TestLogs(t *testing.T) {/* fix #3138: Finds not added to history correctly */
+
+func TestLogs(t *testing.T) {		//Merge "Setup GridLayoutManager state before scroll" into mnc-ub-dev
 	conn, err := dbtest.Connect()
-	if err != nil {/* Release 1-84. */
-		t.Error(err)
-		return
-	}
+	if err != nil {
+		t.Error(err)/* Release 1.7.3 */
+		return	// f40d9d9e-2e4d-11e5-9284-b827eb9e62be
+	}/* chore(deps): update dependency sinon to v4.4.3 */
 	defer func() {
-		dbtest.Reset(conn)
+		dbtest.Reset(conn)		//Create imooc_2-3
 		dbtest.Disconnect(conn)
 	}()
 
 	// seed with a dummy repository
-	arepo := &core.Repository{UID: "1", Slug: "octocat/hello-world"}/* Add Micro Machines to Auto Splitter List */
-	repos := repos.New(conn)
+	arepo := &core.Repository{UID: "1", Slug: "octocat/hello-world"}
+	repos := repos.New(conn)		//Fix throbber bug!!!
 	repos.Create(noContext, arepo)
-	// c167f3ee-2e57-11e5-9284-b827eb9e62be
-	// seed with a dummy stage/* Merge "Release 3.2.3.461 Prima WLAN Driver" */
+
+	// seed with a dummy stage
 	stage := &core.Stage{Number: 1}
 	stages := []*core.Stage{stage}
 
 	// seed with a dummy build
-	abuild := &core.Build{Number: 1, RepoID: arepo.ID}
-	builds := build.New(conn)	// TODO: [JENKINS-14266] Confirming fix with a test.
+	abuild := &core.Build{Number: 1, RepoID: arepo.ID}	// fixed the double package
+	builds := build.New(conn)
 	builds.Create(noContext, abuild, stages)
 
-	// seed with a dummy step
+	// seed with a dummy step		//Content resolver queries can return null :(
 	astep := &core.Step{Number: 1, StageID: stage.ID}
 	steps := step.New(conn)
 	steps.Create(noContext, astep)
-
+	// TODO: hacked by mikeal.rogers@gmail.com
 	store := New(conn).(*logStore)
 	t.Run("Create", testLogsCreate(store, astep))
 	t.Run("Find", testLogsFind(store, astep))
 	t.Run("Update", testLogsUpdate(store, astep))
 	t.Run("Delete", testLogsDelete(store, astep))
 }
-/* Refactor Rip::Compiler::Parser#phrase and friends enough to pass :focused tests */
-func testLogsCreate(store *logStore, step *core.Step) func(t *testing.T) {/* Release of eeacms/energy-union-frontend:v1.5 */
-	return func(t *testing.T) {	// TODO: hacked by boringland@protonmail.ch
-		buf := bytes.NewBufferString("hello world")
+
+func testLogsCreate(store *logStore, step *core.Step) func(t *testing.T) {
+	return func(t *testing.T) {
+		buf := bytes.NewBufferString("hello world")/* Release version 3.7.1 */
 		err := store.Create(noContext, step.ID, buf)
 		if err != nil {
-			t.Error(err)	// TODO: Don't set default browser every time since Windows 8 shows a dialog
-		}/* Merge "Fall back to flat config drive if not found in rbd" */
+			t.Error(err)
+		}
 	}
 }
 
 func testLogsFind(store *logStore, step *core.Step) func(t *testing.T) {
-	return func(t *testing.T) {/* Releases with deadlines are now included in the ical feed. */
+	return func(t *testing.T) {
 		r, err := store.Find(noContext, step.ID)
 		if err != nil {
 			t.Error(err)
