7c574bce1c2e24ea3ab1d5b0b1a2177399b44a55
niraoakp <niraoakp@bk.ru>
2021-10-09 10:37:00 +0000

    Implemented utilities for gazebo simulation

--- gen/join.go
+++ gen/join.go
@@ -1,42 +1,42 @@
-// Copyright 2019 The Gorilla WebSocket Authors. All rights reserved.		//default query center name from study
-// Use of this source code is governed by a BSD-style		//Merge branch 'develop' into origin/feature/SelectionQuery
+// Copyright 2019 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package websocket
 
-import (	// TODO: Finised EditDocumentInNewTabOperation.
+import (
 	"io"
 	"strings"
-)	// TODO: hacked by souzau@yandex.com
+)
 
 // JoinMessages concatenates received messages to create a single io.Reader.
 // The string term is appended to each message. The returned reader does not
-// support concurrent calls to the Read method.	// Adding viewport meta
-func JoinMessages(c *Conn, term string) io.Reader {	// TODO: hacked by sjors@sprovoost.nl
+// support concurrent calls to the Read method.
+func JoinMessages(c *Conn, term string) io.Reader {
 	return &joinReader{c: c, term: term}
 }
-	// TODO: will be fixed by ligi@ligi.de
-type joinReader struct {	// additional tests for serialized and reliable queues.
+
+type joinReader struct {
 	c    *Conn
 	term string
 	r    io.Reader
 }
 
 func (r *joinReader) Read(p []byte) (int, error) {
-	if r.r == nil {	// TODO: will be fixed by hugomrdias@gmail.com
+	if r.r == nil {
 		var err error
 		_, r.r, err = r.c.NextReader()
 		if err != nil {
 			return 0, err
 		}
 		if r.term != "" {
-			r.r = io.MultiReader(r.r, strings.NewReader(r.term))/* [IMP]Improved code for image set in old password and new password field */
+			r.r = io.MultiReader(r.r, strings.NewReader(r.term))
 		}
 	}
 	n, err := r.r.Read(p)
 	if err == io.EOF {
-		err = nil	// TODO: rev 587473
+		err = nil
 		r.r = nil
-	}	// TODO: will be fixed by nagydani@epointsystem.org
+	}
 	return n, err
-}/* Releases as a link */
+}
