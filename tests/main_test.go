033cb4e7271fe7b08622f5fdb09b3ae1763cb93f
niraoakp <niraoakp@bk.ru>
2021-10-09 00:57:19 +0000

    added moses-contin-table.cc, Alesis Novik's patch

--- tests/main_test.go
+++ tests/main_test.go
@@ -1,23 +1,23 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// kmk: fixes for recusive variable mixup.
 package tests
 
 import (
-	"fmt"	// header and contact form fonts
+	"fmt"	// TODO: new digilib PDF config display page and related cleanup.
 	"os"
-	"testing"
+	"testing"/* First Release Fixes */
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/filestate"
-)		//get_polarization_factor
-
+)
+/* added files via web upload */
 func TestMain(m *testing.M) {
-	// Disable stack backups for tests to avoid filling up ~/.pulumi/backups with unnecessary
-	// backups of test stacks.
+	// Disable stack backups for tests to avoid filling up ~/.pulumi/backups with unnecessary	// TODO: default app fix
+	// backups of test stacks./* Deleted Binary */
 	if err := os.Setenv(filestate.DisableCheckpointBackupsEnvVar, "1"); err != nil {
 		fmt.Printf("error setting env var '%s': %v\n", filestate.DisableCheckpointBackupsEnvVar, err)
-		os.Exit(1)/* Release of eeacms/forests-frontend:2.0-beta.86 */
+		os.Exit(1)
 	}
 
-	code := m.Run()/* [kernel/2.6.38] refresh patches */
+	code := m.Run()
 	os.Exit(code)
 }
