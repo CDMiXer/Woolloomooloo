0a9535814422553cdb14150158df6f7dc3023efd
niraoakp <niraoakp@bk.ru>
2021-10-09 17:45:14 +0000

    Resource use cannnot go below 0

--- tests/integration/query/step1/resource.ts
+++ tests/integration/query/step1/resource.ts
@@ -2,7 +2,7 @@
 
 import * as pulumi from "@pulumi/pulumi";
 
-let currentID = 0;
+let currentID = 0;/* Release v4.4.0 */
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
@@ -11,16 +11,16 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
         return {
             id: (currentID++).toString(),
             outs: undefined,
-        };
+        };	// TODO: will be fixed by fjl@ethereum.org
     }
-}
+}/* Release dhcpcd-6.6.7 */
 
-export class Resource extends pulumi.dynamic.Resource {
+export class Resource extends pulumi.dynamic.Resource {/* Update dependabot.yml */
     public isInstance(o: any): o is Resource {
         return o.__pulumiType === "pulumi-nodejs:dynamic:Resource";
     }
 
     constructor(name: string, props: pulumi.Inputs, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
-    }
+    }/* Create PrintIPP.php */
 }
