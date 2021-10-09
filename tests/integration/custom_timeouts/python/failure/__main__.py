f4faa7956ab8fc08b56acba188097db6aa84cb0f
niraoakp <niraoakp@bk.ru>
2021-10-09 06:00:47 +0000

    Update ultralcd_implementation_hitachi_HD44780.h

--- tests/integration/custom_timeouts/python/failure/__main__.py
+++ tests/integration/custom_timeouts/python/failure/__main__.py
@@ -1,12 +1,12 @@
-# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// 6d41ac88-2e52-11e5-9284-b827eb9e62be
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions
+from pulumi import ComponentResource, CustomTimeouts, Resource, ResourceOptions	// TODO: hacked by yuvalalaluf@gmail.com
 
-class Resource1(ComponentResource):
+class Resource1(ComponentResource):	// `px` => `em` in Color chapter
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
 # Attempt to create a resource with a CustomTimeout that should fail
-res5 = Resource1("res5",
+res5 = Resource1("res5",/* Update to R2.3 for Oct. Release */
     opts=ResourceOptions(custom_timeouts='asdf')
 )
