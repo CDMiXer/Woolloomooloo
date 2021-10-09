770fd0c4122efcbffaac6403709facdc4e2f9689
niraoakp <niraoakp@bk.ru>
2021-10-09 12:40:47 +0000

    Merge "Adding swipe gestures in overview screen" into ub-launcher3-master

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,9 +1,9 @@
 package fsutil
 
-type FsStat struct {
+type FsStat struct {/* Merge "Release 4.0.10.42 QCACLD WLAN Driver" */
 	Capacity    int64
 	Available   int64 // Available to use for sector storage
-	FSAvailable int64 // Available in the filesystem
+	FSAvailable int64 // Available in the filesystem/* Removing DockedConceptViewI */
 	Reserved    int64
 
 	// non-zero when storage has configured MaxStorage
