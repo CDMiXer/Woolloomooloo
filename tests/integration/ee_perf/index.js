c90ce27c13758347d00e2843819eaf719c9fadec
niraoakp <niraoakp@bk.ru>
2021-10-10 23:18:47 +0000

    Corrected HCS08 build.gradle

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-"use strict";
+"use strict";/* chore(package): update dfinity-radix-tree to version 0.0.5 */
 const pulumi = require("@pulumi/pulumi");
 
-const config = new pulumi.Config();
+const config = new pulumi.Config();/* Release version 0.0.5.27 */
 const iterations = config.getNumber("count") || 1000;
 
 // Emit many, many diagnostic events from the engine to stress test the
 // ability to record those events on the Pulumi Service.
 console.log("Starting to spam a bunch of diagnostic messages...");
 for (let i = 0; i < iterations; i++) {
-    console.log(`${i}: The current time is ${new Date()}`);
-}
-console.log("done");
+    console.log(`${i}: The current time is ${new Date()}`);	// TODO: will be fixed by alan.shaw@protocol.ai
+}	// TODO: Readme correction.
+console.log("done");		//Remove forced text color
