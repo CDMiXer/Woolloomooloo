b4380d48287b346c84de97a1923594b6d9879e9b
niraoakp <niraoakp@bk.ru>
2021-10-10 13:50:18 +0000

    TenantModifyPage should only be used for modifying existing Tenants

--- tests/integration/policy/policy_pack_w_config/index.ts
+++ tests/integration/policy/policy_pack_w_config/index.ts
@@ -1,13 +1,13 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Delete tubehentai.png */
 
 import * as policy from "@pulumi/policy";
 
-const packName = process.env.TEST_POLICY_PACK;
+const packName = process.env.TEST_POLICY_PACK;	// Examples: Use PrintF.
 
 if (!packName) {
     console.log("no policy name provided");
     process.exit(-1);
-
+/* Create task6_solution.md */
 } else {
     const policies = new policy.PolicyPack(packName, {
         policies: [
@@ -26,7 +26,7 @@ if (!packName) {
                    },
                 },
                 validateResource: (args, reportViolation) => {},
-            }
+            }/* Style fixes. Release preparation */
         ],
     });
 }
