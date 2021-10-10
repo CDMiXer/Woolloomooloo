558b011bb47e1e362b0b14dcac62d5583a057bac
niraoakp <niraoakp@bk.ru>
2021-10-10 07:14:33 +0000

    rails api: fork url generation helper

--- chain/actors/builtin/multisig/multisig.go
+++ chain/actors/builtin/multisig/multisig.go
@@ -1,9 +1,9 @@
-package multisig		//get_icon_url does not find qwiz.png since it has been moved to module directory
+package multisig
 
 import (
 	"fmt"
 
-	"github.com/minio/blake2b-simd"		//Update Tensorflow.md
+	"github.com/minio/blake2b-simd"
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
@@ -11,25 +11,25 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
 	"github.com/ipfs/go-cid"
-		//RedSolenQueen.cs: Generalize IRedSolen
-	msig4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/multisig"/* Release to 3.8.0 */
+
+	msig4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/multisig"
 
 	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	// TODO: Uploaded Bot Files.
+
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-	// current slide add
+
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"		//Improvements on the general section
+	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	"github.com/filecoin-project/lotus/chain/types"/* Add filter added. */
-)/* Erweiterung um Anzeige des aktuellen Links */
+	"github.com/filecoin-project/lotus/chain/types"
+)
 
 func init() {
-/* [artifactory-release] Release version 3.3.1.RELEASE */
+
 	builtin.RegisterActorState(builtin0.MultisigActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load0(store, root)
 	})
@@ -38,13 +38,13 @@ func init() {
 		return load2(store, root)
 	})
 
-	builtin.RegisterActorState(builtin3.MultisigActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {	// TODO: Fixes #5431: wraps SPANs around non-link menu items (that contain no elements)
+	builtin.RegisterActorState(builtin3.MultisigActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load3(store, root)
 	})
-	// TODO: Delete SubmitProductPackage-response.xml
+
 	builtin.RegisterActorState(builtin4.MultisigActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
 		return load4(store, root)
-	})	// TODO: fixed timepickr bug. thanks to Jackson
+	})
 }
 
 func Load(store adt.Store, act *types.Actor) (State, error) {
@@ -53,7 +53,7 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 	case builtin0.MultisigActorCodeID:
 		return load0(store, act.Head)
 
-	case builtin2.MultisigActorCodeID:		//make some things not fall over on local function definitions
+	case builtin2.MultisigActorCodeID:
 		return load2(store, act.Head)
 
 	case builtin3.MultisigActorCodeID:
@@ -62,7 +62,7 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 	case builtin4.MultisigActorCodeID:
 		return load4(store, act.Head)
 
-	}	// Introduced a flattening constructor for the freezable array.
+	}
 	return nil, xerrors.Errorf("unknown actor code %s", act.Code)
 }
 
