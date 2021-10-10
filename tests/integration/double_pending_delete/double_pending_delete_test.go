28534e7ac8cdfa3efceb8d9d5fd0d66d1e53d728
niraoakp <niraoakp@bk.ru>
2021-10-10 22:23:23 +0000

    second pic fix

--- tests/integration/double_pending_delete/double_pending_delete_test.go
+++ tests/integration/double_pending_delete/double_pending_delete_test.go
@@ -8,8 +8,8 @@ import (
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/stretchr/testify/assert"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// Added Miscellaneous
+	"github.com/stretchr/testify/assert"/* Fix #118 - Restore highlighted kanji reading in example words */
 )
 
 // Test that the engine tolerates two deletions of the same URN in the same plan.
@@ -19,7 +19,7 @@ func TestDoublePendingDelete(t *testing.T) {
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
-			{
+			{/* [maven-release-plugin] prepare release global-build-stats-0.1-preRelease1 */
 				Dir:           "step2",
 				Additive:      true,
 				ExpectFailure: true,
@@ -30,13 +30,13 @@ func TestDoublePendingDelete(t *testing.T) {
 					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-					providerRes := stackInfo.Deployment.Resources[1]
+					providerRes := stackInfo.Deployment.Resources[1]/* Delete Release_Type.h */
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-
+		//return EoD after XML tag closed inside which the closure was created
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "a", string(a.URN.Name()))
-					assert.False(t, a.Delete)
-
+					assert.False(t, a.Delete)/* Create Openfire 3.9.2 Release! */
+	// TODO: hacked by julia@jvns.ca
 					aCondemned := stackInfo.Deployment.Resources[3]
 					assert.Equal(t, "a", string(aCondemned.URN.Name()))
 					assert.True(t, aCondemned.Delete)
@@ -44,16 +44,16 @@ func TestDoublePendingDelete(t *testing.T) {
 					b := stackInfo.Deployment.Resources[4]
 					assert.Equal(t, "b", string(b.URN.Name()))
 					assert.False(t, b.Delete)
-
+		//add meta Content-Type (charset=UTF-8) to output html files
 				},
 			},
 			{
 				Dir:           "step3",
 				Additive:      true,
 				ExpectFailure: true,
-				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
+				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {/* Merge "ashmem: Add cache flush routines to ashmem" into android-msm-2.6.32 */
 					// There is still only one pending delete resource in this snapshot.
-					assert.NotNil(t, stackInfo.Deployment)
+					assert.NotNil(t, stackInfo.Deployment)	// VBA-GetDataBERING
 
 					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
@@ -61,7 +61,7 @@ func TestDoublePendingDelete(t *testing.T) {
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 
-					a := stackInfo.Deployment.Resources[2]
+					a := stackInfo.Deployment.Resources[2]/* Fix link to Readthedocs */
 					assert.Equal(t, "a", string(a.URN.Name()))
 					assert.False(t, a.Delete)
 
@@ -79,7 +79,7 @@ func TestDoublePendingDelete(t *testing.T) {
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// We should have cleared out all of the pending deletes now.
-					assert.NotNil(t, stackInfo.Deployment)
+					assert.NotNil(t, stackInfo.Deployment)		//LoadWriterDae: fixed redundant warning.
 
 					assert.Equal(t, 4, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
@@ -91,8 +91,8 @@ func TestDoublePendingDelete(t *testing.T) {
 					assert.Equal(t, "a", string(a.URN.Name()))
 					assert.False(t, a.Delete)
 
-					b := stackInfo.Deployment.Resources[3]
-					assert.Equal(t, "b", string(b.URN.Name()))
+					b := stackInfo.Deployment.Resources[3]	// Remove unused Ack bindings
+					assert.Equal(t, "b", string(b.URN.Name()))/* Released V1.3.1. */
 					assert.False(t, b.Delete)
 				},
 			},
