bfe222c3aa9559c6e3f4bde575fb7d76b64ba58d
niraoakp <niraoakp@bk.ru>
2021-10-10 22:27:31 +0000

    separate class for cors filter, directory for security

--- gen/client_clone.go
+++ gen/client_clone.go
@@ -3,14 +3,14 @@
 // license that can be found in the LICENSE file.
 
 // +build go1.8
-/* fix indefinite article */
+
 package websocket
 
 import "crypto/tls"
-/* Fixed QueueSize=1 doesn't handle multi-cpu processes #246 */
+
 func cloneTLSConfig(cfg *tls.Config) *tls.Config {
 	if cfg == nil {
 		return &tls.Config{}
 	}
 	return cfg.Clone()
-}/* Release version 1.0. */
+}
