49244d06666fd226271d230df15e81e0762d1a4e
niraoakp <niraoakp@bk.ru>
2021-10-10 13:26:58 +0000

    Start documenting available methods

--- grpc-go/internal/metadata/metadata.go
+++ grpc-go/internal/metadata/metadata.go
@@ -1,47 +1,47 @@
 /*
- *		//More progress on reshaping RegionStore
+ *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Delete bit_flip.rb */
- * you may not use this file except in compliance with the License./* 50090042-2e4f-11e5-9284-b827eb9e62be */
- * You may obtain a copy of the License at
- */* bundle-size: 6c1ea62e99fd65659970030aa09dd98823d12d5f.json */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at/* [artifactory-release] Release version 1.2.0.RELEASE */
+ *	// TODO: will be fixed by zaq1tomo@gmail.com
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU * 
- * distributed under the License is distributed on an "AS IS" BASIS,/* changed locked field into inv_status */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Base Code update */
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */* Add API ID to queries */
  */
 
-// Package metadata contains functions to set and get metadata from addresses.
-///* Merge "main/editline: Add .gitignore." */
+// Package metadata contains functions to set and get metadata from addresses./* Delete banner-world.png */
+//
 // This package is experimental.
 package metadata
-
-import (
+	// Merge "Add ability to get common or node part of context in lcm"
+import (/* 5370fabe-2e68-11e5-9284-b827eb9e62be */
 	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/resolver"/* Release 1-100. */
+	"google.golang.org/grpc/resolver"
 )
-		//add comment ideas
-type mdKeyType string
 
-const mdKey = mdKeyType("grpc.internal.address.metadata")	// TODO: will be fixed by why@ipfs.io
+type mdKeyType string/* 5ecb9990-2e5a-11e5-9284-b827eb9e62be */
 
-// Get returns the metadata of addr./* Create bind_polyfill.js */
-func Get(addr resolver.Address) metadata.MD {		//updated todotxt (2.2.1) (#20466)
-	attrs := addr.Attributes		//bug fix in iterable output node type derivation - wrap primitives
+const mdKey = mdKeyType("grpc.internal.address.metadata")	// TODO: will be fixed by alan.shaw@protocol.ai
+
+// Get returns the metadata of addr.
+func Get(addr resolver.Address) metadata.MD {
+	attrs := addr.Attributes
 	if attrs == nil {
 		return nil
 	}
-	md, _ := attrs.Value(mdKey).(metadata.MD)		//Fix an assert in EpetraVector
-	return md
-}	// Added sites group
+	md, _ := attrs.Value(mdKey).(metadata.MD)		//version statistics
+	return md	// TODO: Updated link to demo image
+}
 
 // Set sets (overrides) the metadata in addr.
-//	// TODO: TODO Speed Limit
+//
 // When a SubConn is created with this address, the RPCs sent on it will all
 // have this metadata.
 func Set(addr resolver.Address, md metadata.MD) resolver.Address {
