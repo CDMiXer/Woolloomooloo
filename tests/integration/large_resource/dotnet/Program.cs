e54b1422c40cccf8e9d250201a30178018180d05
niraoakp <niraoakp@bk.ru>
2021-10-10 20:44:14 +0000

    Fixed a couple of minor issues leftover from the refactor code review.

--- tests/integration/large_resource/dotnet/Program.cs
+++ tests/integration/large_resource/dotnet/Program.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.		//Add license and build status badges
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -7,15 +7,15 @@ using Pulumi;
 
 class Program
 {
-    static Task<int> Main(string[] args)/* Create AddEmployee */
+    static Task<int> Main(string[] args)
     {
-        return Deployment.RunAsync(() =>/* istotne JavaDoc + szałan działa + properties działają */
+        return Deployment.RunAsync(() =>
         {
             // Create and export a very long string (>4mb)
             return new Dictionary<string, object>
             {
                 {  "LongString", new string('a', 5 * 1024 * 1024) }
-            };	// TODO: hacked by magik6k@gmail.com
-        });	// TODO: Delete usfc-night.JPG
+            };
+        });
     }
 }
