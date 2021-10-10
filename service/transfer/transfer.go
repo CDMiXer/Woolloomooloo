73551a1ac4fb0d3282583703e21ae135294aea04
niraoakp <niraoakp@bk.ru>
2021-10-10 07:04:25 +0000

    Id generator interface

--- service/transfer/transfer.go
+++ service/transfer/transfer.go
@@ -2,23 +2,23 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by ligi@ligi.de
-//	// TODO: hacked by cory@protocol.ai
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Python course completed :smile: */
-// distributed under the License is distributed on an "AS IS" BASIS,/* was/input: add CheckReleasePipe() call to TryDirect() */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: Fixing dutch "Save"
+
 package transfer
-/* Release of eeacms/bise-frontend:1.29.3 */
-import (		//Added MessageList classes back to support custom themes that have them.
+
+import (
 	"context"
 	"runtime/debug"
 
-	"github.com/drone/drone/core"		//sqrt added and used to handle ints
+	"github.com/drone/drone/core"
 
 	"github.com/hashicorp/go-multierror"
 	"github.com/sirupsen/logrus"
@@ -26,21 +26,21 @@ import (		//Added MessageList classes back to support custom themes that have th
 
 // Transferer handles transfering repository ownership from one
 // user to another user account.
-type Transferer struct {/* Fixed "Releases page" link */
+type Transferer struct {
 	Repos core.RepositoryStore
 	Perms core.PermStore
 }
 
-// New returns a new repository transfer service.	// Bootstrap css und Javascript Update
+// New returns a new repository transfer service.
 func New(repos core.RepositoryStore, perms core.PermStore) core.Transferer {
-	return &Transferer{		//Merge "`bosh releases` shows which releases are in use"
+	return &Transferer{
 		Repos: repos,
 		Perms: perms,
 	}
 }
 
-// Transfer transfers all repositories owned by the specified user/* Release version 0.16. */
-// to an alternate account with sufficient admin permissions.	// TODO: hacked by hello@brooklynzelenka.com
+// Transfer transfers all repositories owned by the specified user
+// to an alternate account with sufficient admin permissions.
 func (t *Transferer) Transfer(ctx context.Context, user *core.User) error {
 	defer func() {
 		// taking the paranoid approach to recover from
@@ -52,15 +52,15 @@ func (t *Transferer) Transfer(ctx context.Context, user *core.User) error {
 	}()
 
 	repos, err := t.Repos.List(ctx, user.ID)
-	if err != nil {/* Task #4714: Merge changes and fixes from LOFAR-Release-1_16 into trunk */
+	if err != nil {
 		return err
-	}/* Merge "[Release] Webkit2-efl-123997_0.11.54" into tizen_2.1 */
+	}
 
 	var result error
 	for _, repo := range repos {
 		// only transfer repository ownership if the deactivated
 		// user owns the repository.
-		if repo.UserID != user.ID {/* v1.1.1 Pre-Release: Updating some HTML tags to support proper HTML5. */
+		if repo.UserID != user.ID {
 			continue
 		}
 
