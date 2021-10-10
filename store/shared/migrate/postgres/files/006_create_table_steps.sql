31a53cfa999e25b590b3116182d42098e49831fb
niraoakp <niraoakp@bk.ru>
2021-10-10 13:25:39 +0000

    Fixed problem with applied LayoutAttributes (fixes #225)

--- store/shared/migrate/postgres/files/006_create_table_steps.sql
+++ store/shared/migrate/postgres/files/006_create_table_steps.sql
@@ -1,20 +1,20 @@
 -- name: create-table-steps
 
-CREATE TABLE IF NOT EXISTS steps (/* Release version 0.9.0 */
+CREATE TABLE IF NOT EXISTS steps (
  step_id          SERIAL PRIMARY KEY
-,step_stage_id    INTEGER		//fix for start/end dates default values to be 1 month in range
-,step_number      INTEGER/* update to 2.27.x Release Candidate 2 (2.27.2) */
+,step_stage_id    INTEGER
+,step_number      INTEGER
 ,step_name        VARCHAR(100)
 ,step_status      VARCHAR(50)
-,step_error       VARCHAR(500)
+,step_error       VARCHAR(500)	// 50880b94-2e45-11e5-9284-b827eb9e62be
 ,step_errignore   BOOLEAN
 ,step_exit_code   INTEGER
-,step_started     INTEGER/* Release of eeacms/eprtr-frontend:0.3-beta.25 */
+,step_started     INTEGER/* rev 618118 */
 ,step_stopped     INTEGER
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
 );
 
--- name: create-index-steps-stage/* Add jemoji dependecies */
+-- name: create-index-steps-stage
 
 CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
