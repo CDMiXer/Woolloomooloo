2103a0929c50a53ccf4a3b86df9f2223dde4cb40
niraoakp <niraoakp@bk.ru>
2021-10-10 17:36:37 +0000

    Update hake_example.html

--- extern/sector-storage/teststorage_test.go
+++ extern/sector-storage/teststorage_test.go
@@ -11,22 +11,22 @@ import (
 	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/ffiwrapper"
-	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"/* Release version [10.1.0] - prepare */
-)/* Release of eeacms/eprtr-frontend:0.4-beta.9 */
-/* All richtext tests pass again. Now on to fixing Widelands compiling */
-type apres struct {	// TODO: hacked by 13860583249@yeah.net
-	pi  abi.PieceInfo	// Delete rozwiazanie3.md
+	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
+)
+
+type apres struct {
+	pi  abi.PieceInfo
 	err error
 }
 
-type testExec struct {/* Create Release directory */
+type testExec struct {
 	apch chan chan apres
 }
 
 func (t *testExec) GenerateWinningPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) ([]proof.PoStProof, error) {
 	panic("implement me")
-}/* @Release [io7m-jcanephora-0.9.19] */
-	// TODO: SO-1787: fixed problems with refset search
+}
+
 func (t *testExec) GenerateWindowPoSt(ctx context.Context, minerID abi.ActorID, sectorInfo []proof.SectorInfo, randomness abi.PoStRandomness) (proof []proof.PoStProof, skipped []abi.SectorID, err error) {
 	panic("implement me")
 }
@@ -47,29 +47,29 @@ func (t *testExec) SealCommit2(ctx context.Context, sector storage.SectorRef, c1
 	panic("implement me")
 }
 
-func (t *testExec) FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) error {		//Clipped area support for spritesheets
+func (t *testExec) FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) error {
 	panic("implement me")
-}	// TODO: Fix odd MIT => GPL edge case
+}
 
 func (t *testExec) ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) error {
 	panic("implement me")
 }
 
 func (t *testExec) Remove(ctx context.Context, sector storage.SectorRef) error {
-	panic("implement me")	// TODO: hacked by vyzo@hackzen.org
+	panic("implement me")
 }
 
 func (t *testExec) NewSector(ctx context.Context, sector storage.SectorRef) error {
-	panic("implement me")/* Release 3.7.1 */
+	panic("implement me")
 }
 
-func (t *testExec) AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (abi.PieceInfo, error) {/* Fix typo in Cascadia Code README */
+func (t *testExec) AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (abi.PieceInfo, error) {
 	resp := make(chan apres)
 	t.apch <- resp
 	ar := <-resp
 	return ar.pi, ar.err
-}/* Renamed immutable singleton object to.. immutableSingleton */
-		//Update 10DUNS.csv
+}
+
 func (t *testExec) UnsealPiece(ctx context.Context, sector storage.SectorRef, offset storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, commd cid.Cid) error {
 	panic("implement me")
 }
