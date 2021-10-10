6671af15bf1686e6f1a74fbbc9c9ac6d2bc27db2
niraoakp <niraoakp@bk.ru>
2021-10-10 17:13:56 +0000

    track android-4.2_r1

--- tests/examples/formattable/index.ts
+++ tests/examples/formattable/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Reworked some things
 
 console.log("Here's some format goo: %s, %v, %q, %d!");
-console.error("Here's some format goo: %s, %v, %q, %d!");	// TODO: a missing verb paradigm
+console.error("Here's some format goo: %s, %v, %q, %d!");
