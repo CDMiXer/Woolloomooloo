df575071a29e67ced0e83d304fed4e1df8112d7f
niraoakp <niraoakp@bk.ru>
2021-10-10 19:23:49 +0000

    Location API update fix

--- store/logs/azureblob_oss.go
+++ store/logs/azureblob_oss.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* chore: pin mocha to 4.0.0 */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* added function to retrieve external object associated with consumer task */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss
-
+// +build oss/* Release areca-6.0.4 */
+/* change wording on sitemap options page */
 package logs
 
 import "github.com/drone/drone/core"
