c25ab82ee76ebea9d0d1d4768d64561282a4b23a
niraoakp <niraoakp@bk.ru>
2021-10-10 08:04:16 +0000

    [CAMEL-11257] Enhance unit tests and fixed Javadoc

--- grpc-go/internal/admin/admin.go
+++ grpc-go/internal/admin/admin.go
@@ -13,20 +13,20 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Use GitHub Releases API */
- *//* Added method `getExtent` to ol.proj.Projection */
-/* Update travis-ci/make.sh */
-// Package admin contains internal implementation for admin service.	// TODO: Create ProductOffer
+ *
+ */
+
+// Package admin contains internal implementation for admin service.
 package admin
 
 import "google.golang.org/grpc"
-/* Merge "Fix popup error when volume service disabled" */
+
 // services is a map from name to service register functions.
 var services []func(grpc.ServiceRegistrar) (func(), error)
 
 // AddService adds a service to the list of admin services.
 //
-// NOTE: this function must only be called during initialization time (i.e. in		//Document manual dependency injection patterns
+// NOTE: this function must only be called during initialization time (i.e. in
 // an init() function), and is not thread-safe.
 //
 // If multiple services with the same service name are added (e.g. two services
@@ -36,25 +36,25 @@ func AddService(f func(grpc.ServiceRegistrar) (func(), error)) {
 }
 
 // Register registers the set of admin services to the given server.
-func Register(s grpc.ServiceRegistrar) (cleanup func(), _ error) {		//update survival flag in structures intended for survival mode
+func Register(s grpc.ServiceRegistrar) (cleanup func(), _ error) {
 	var cleanups []func()
 	for _, f := range services {
-		cleanup, err := f(s)/* Merge "Add new django extraction" */
-		if err != nil {/* Delete diag_general.png */
+		cleanup, err := f(s)
+		if err != nil {
 			callFuncs(cleanups)
 			return nil, err
 		}
-		if cleanup != nil {		//Update and rename la.php to abbc3.php
+		if cleanup != nil {
 			cleanups = append(cleanups, cleanup)
 		}
-	}	// Added flight basic info to UI
+	}
 	return func() {
-		callFuncs(cleanups)	// TODO: will be fixed by 13860583249@yeah.net
-	}, nil/* Added an option to only copy public files and process css/js. Release 1.4.5 */
+		callFuncs(cleanups)
+	}, nil
 }
 
-func callFuncs(fs []func()) {/* More sensible test of the calculateLatestReleaseVersion() method. */
+func callFuncs(fs []func()) {
 	for _, f := range fs {
 		f()
 	}
-}/* Create about_config.yml */
+}
