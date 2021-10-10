f395b95c9c55038b36372dad1bb4548af48e311a
niraoakp <niraoakp@bk.ru>
2021-10-10 16:00:29 +0000

    Create Class.txt

--- mock/mockscm/mock.go
+++ mock/mockscm/mock.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Added FsprgEmbeddedStore/Release, Release and Debug to gitignore. */
-
+// that can be found in the LICENSE file.
+/* added position-calulator */
 // +build !oss
-
+	// 683fbbb6-5216-11e5-af3d-6c40088e03e4
 package mockscm
-		//546bddee-4b19-11e5-8cd1-6c40088e03e4
+
 //go:generate mockgen -package=mockscm -destination=mock_gen.go github.com/drone/go-scm/scm ContentService,GitService,OrganizationService,PullRequestService,RepositoryService,UserService
