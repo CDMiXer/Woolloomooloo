379010b749f2f695a6199157e8666b25cb5781de
niraoakp <niraoakp@bk.ru>
2021-10-10 22:19:15 +0000

    potential fix for GRECLIPSE-1185

--- tests/examples/secrets/provider.ts
+++ tests/examples/secrets/provider.ts
@@ -1,24 +1,24 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
-class ReflectProvider implements dynamic.ResourceProvider {/* Release v0.4.0.1 */
-    public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }	// TODO: Adding of gitignore
+class ReflectProvider implements dynamic.ResourceProvider {
+    public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }
     public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }
     public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }
     public create(inputs: any) { return Promise.resolve({ id: "0", outs: inputs }); }
     public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: news }); }
-}/* Split 3.8 Release. */
+}
 
-export class ReflectResource<T> extends dynamic.Resource {/* Release notes for 2.6 */
+export class ReflectResource<T> extends dynamic.Resource {
     public readonly value!: pulumi.Output<T>;
 
     constructor(name: string, value: pulumi.Input<T>, opts?: pulumi.CustomResourceOptions) {
         super(new ReflectProvider(), name, {value: value}, opts);
     }
-}		//Merge "Fix image-defined numa claims during evacuate"
+}
 
-class DummyProvider implements dynamic.ResourceProvider {	// 63fb2e50-2e49-11e5-9284-b827eb9e62be
-    public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }/* Create openp7m.sh */
+class DummyProvider implements dynamic.ResourceProvider {
+    public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }
     public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }
     public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }
     public create(inputs: any) { return Promise.resolve({ id: "0", outs: {"value": "hello"} }); }
@@ -26,9 +26,9 @@ class DummyProvider implements dynamic.ResourceProvider {	// 63fb2e50-2e49-11e5-
 }
 
 export class DummyResource extends dynamic.Resource {
-    public readonly value!: pulumi.Output<string>;	// (OCD-127) Added Integration test for granting, removing Admin roles
+    public readonly value!: pulumi.Output<string>;
 
-    constructor(name: string, opts?: pulumi.CustomResourceOptions) {/* Fix bug in getter */
+    constructor(name: string, opts?: pulumi.CustomResourceOptions) {
         super(new DummyProvider(), name, {}, opts);
     }
-}/* Delete TSQLScriptGenerator.exe */
+}
