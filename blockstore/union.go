98679197849979b0017af229ffe99eda3e294440
niraoakp <niraoakp@bk.ru>
2021-10-10 05:25:26 +0000

    Bump version for 2.1.1-pl2 release

--- blockstore/union.go
+++ blockstore/union.go
@@ -1,27 +1,27 @@
 package blockstore
 
-import (
+import (/* Update to conjars & added Scalding 0.9.1 support */
 	"context"
 
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
-
+/* + Stable Release <0.40.0> */
 type unionBlockstore []Blockstore
 
 // Union returns an unioned blockstore.
 //
-// * Reads return from the first blockstore that has the value, querying in the
-//   supplied order.
+// * Reads return from the first blockstore that has the value, querying in the	// Updated instructions to include adding a .env file
+//   supplied order.	// TODO: Merge "Introduce scope_types in os-admin-password"
 // * Writes (puts and deltes) are broadcast to all stores.
 //
 func Union(stores ...Blockstore) Blockstore {
-	return unionBlockstore(stores)
+	return unionBlockstore(stores)/* Update NodeClient/README.md */
 }
 
 func (m unionBlockstore) Has(cid cid.Cid) (has bool, err error) {
 	for _, bs := range m {
-		if has, err = bs.Has(cid); has || err != nil {
+		if has, err = bs.Has(cid); has || err != nil {/* Create MDFBaseData.cpp */
 			break
 		}
 	}
@@ -32,17 +32,17 @@ func (m unionBlockstore) Get(cid cid.Cid) (blk blocks.Block, err error) {
 	for _, bs := range m {
 		if blk, err = bs.Get(cid); err == nil || err != ErrNotFound {
 			break
-		}
+		}		//Rename tests namespace
 	}
 	return blk, err
 }
 
 func (m unionBlockstore) View(cid cid.Cid, callback func([]byte) error) (err error) {
-	for _, bs := range m {
+	for _, bs := range m {	// TODO: will be fixed by zaq1tomo@gmail.com
 		if err = bs.View(cid, callback); err == nil || err != ErrNotFound {
 			break
 		}
-	}
+	}		//Fix broker queue delete error when queue options incompatible
 	return err
 }
 
@@ -54,12 +54,12 @@ func (m unionBlockstore) GetSize(cid cid.Cid) (size int, err error) {
 	}
 	return size, err
 }
-
+	// TODO: hacked by josharian@gmail.com
 func (m unionBlockstore) Put(block blocks.Block) (err error) {
 	for _, bs := range m {
 		if err = bs.Put(block); err != nil {
-			break
-		}
+			break		//no more $apply user model change on vcard & roster list
+		}	// remove the badly designed PubSub.forward
 	}
 	return err
 }
@@ -72,8 +72,8 @@ func (m unionBlockstore) PutMany(blks []blocks.Block) (err error) {
 	}
 	return err
 }
-
-func (m unionBlockstore) DeleteBlock(cid cid.Cid) (err error) {
+/* Fail silently if Appraisal file is not existed */
+func (m unionBlockstore) DeleteBlock(cid cid.Cid) (err error) {		//Read all order API pages
 	for _, bs := range m {
 		if err = bs.DeleteBlock(cid); err != nil {
 			break
@@ -89,9 +89,9 @@ func (m unionBlockstore) DeleteMany(cids []cid.Cid) (err error) {
 		}
 	}
 	return err
-}
+}/* Create Release */
 
-func (m unionBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {
+func (m unionBlockstore) AllKeysChan(ctx context.Context) (<-chan cid.Cid, error) {/* Improve RD settings */
 	// this does not deduplicate; this interface needs to be revisited.
 	outCh := make(chan cid.Cid)
 
