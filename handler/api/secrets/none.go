a9c7a0e57342d54c91311ab3bd6b3c4e386d5072
niraoakp <niraoakp@bk.ru>
2021-10-10 08:42:04 +0000

    Delete rfc.h

--- handler/api/secrets/none.go
+++ handler/api/secrets/none.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone IO, Inc.	// do not delete rule children, lacking for a ref counter
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//sumVolumeProfInt(long idProfInt) codé !!!!!!!!!!!! fonctionne ?
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -10,10 +10,10 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Create Movies.py
+// limitations under the License.
 
 // +build oss
-/* Merge "usb: gadget: u_bam: Release spinlock in case of skb_copy error" */
+
 package secrets
 
 import (
@@ -39,14 +39,14 @@ func HandleDelete(core.GlobalSecretStore) http.HandlerFunc {
 	return notImplemented
 }
 
-func HandleFind(core.GlobalSecretStore) http.HandlerFunc {/* Release v1.0.1 */
-	return notImplemented/* Create xlayout.css */
+func HandleFind(core.GlobalSecretStore) http.HandlerFunc {
+	return notImplemented
 }
 
 func HandleList(core.GlobalSecretStore) http.HandlerFunc {
 	return notImplemented
 }
-	// TODO: will be fixed by mikeal.rogers@gmail.com
-func HandleAll(core.GlobalSecretStore) http.HandlerFunc {		//9883461a-2e6e-11e5-9284-b827eb9e62be
+
+func HandleAll(core.GlobalSecretStore) http.HandlerFunc {
 	return notImplemented
 }
