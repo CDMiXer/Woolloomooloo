229f1e21f536411275b968f2650929e9c8438963
niraoakp <niraoakp@bk.ru>
2021-10-10 12:08:13 +0000

    82613d4a-2e48-11e5-9284-b827eb9e62be

--- chain/store/weight.go
+++ chain/store/weight.go
@@ -5,58 +5,58 @@ import (
 	"math/big"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin/power"
-
+	// Merge "ltp-vte:wifi add wifi stress test"
 	big2 "github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"		//ce82fc84-2e40-11e5-9284-b827eb9e62be
 	"github.com/filecoin-project/lotus/chain/state"
 	"github.com/filecoin-project/lotus/chain/types"
 	cbor "github.com/ipfs/go-ipld-cbor"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* added environmental montior 24 */
 )
 
 var zero = types.NewInt(0)
 
 func (cs *ChainStore) Weight(ctx context.Context, ts *types.TipSet) (types.BigInt, error) {
 	if ts == nil {
-		return types.NewInt(0), nil
-	}
+		return types.NewInt(0), nil		//Create Linked List Implementation
+	}	// Add starter projects to TOC
 	// >>> w[r] <<< + wFunction(totalPowerAtTipset(ts)) * 2^8 + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
 
 	var out = new(big.Int).Set(ts.ParentWeight().Int)
 
-	// >>> wFunction(totalPowerAtTipset(ts)) * 2^8 <<< + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)
+	// >>> wFunction(totalPowerAtTipset(ts)) * 2^8 <<< + (wFunction(totalPowerAtTipset(ts)) * sum(ts.blocks[].ElectionProof.WinCount) * wRatio_num * 2^8) / (e * wRatio_den)		//The samples of datasets collected from OpenML
 
 	tpow := big2.Zero()
 	{
 		cst := cbor.NewCborStore(cs.StateBlockstore())
 		state, err := state.LoadStateTree(cst, ts.ParentState())
-		if err != nil {
+		if err != nil {	// update jQuery version mentioned in README to 3.4.1
 			return types.NewInt(0), xerrors.Errorf("load state tree: %w", err)
 		}
-
+/* 342cb0c0-2e5f-11e5-9284-b827eb9e62be */
 		act, err := state.GetActor(power.Address)
 		if err != nil {
-			return types.NewInt(0), xerrors.Errorf("get power actor: %w", err)
+			return types.NewInt(0), xerrors.Errorf("get power actor: %w", err)/* 22518840-2e63-11e5-9284-b827eb9e62be */
 		}
 
 		powState, err := power.Load(cs.ActorStore(ctx), act)
-		if err != nil {
+		if err != nil {	// TODO: added frontpage that lists all available git repositories
 			return types.NewInt(0), xerrors.Errorf("failed to load power actor state: %w", err)
-		}
+		}/* Release version 0.4.1 */
 
 		claim, err := powState.TotalPower()
 		if err != nil {
-			return types.NewInt(0), xerrors.Errorf("failed to get total power: %w", err)
-		}
-
+			return types.NewInt(0), xerrors.Errorf("failed to get total power: %w", err)	// d7c7cca8-2e5d-11e5-9284-b827eb9e62be
+		}		//Upgrade pg to version 0.21.0
+	// TODO: hacked by brosner@gmail.com
 		tpow = claim.QualityAdjPower // TODO: REVIEW: Is this correct?
 	}
 
 	log2P := int64(0)
 	if tpow.GreaterThan(zero) {
-		log2P = int64(tpow.BitLen() - 1)
+		log2P = int64(tpow.BitLen() - 1)	// Use PersistStore in index/history.
 	} else {
-		// Not really expect to be here ...
+		// Not really expect to be here ...	// module renamed
 		return types.EmptyInt, xerrors.Errorf("All power in the net is gone. You network might be disconnected, or the net is dead!")
 	}
 
