61fcfa2ce3be65ea874a468be7630cdf2e195b1d
niraoakp <niraoakp@bk.ru>
2021-10-10 17:08:14 +0000

    Update PVS-studio to 6.19

--- grpc-go/xds/internal/balancer/clustermanager/config.go
+++ grpc-go/xds/internal/balancer/clustermanager/config.go
@@ -1,10 +1,10 @@
 /*
  *
- * Copyright 2020 gRPC authors.	// TODO: will be fixed by vyzo@hackzen.org
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* update Forestry-Release item number to 3 */
- * You may obtain a copy of the License at/* Release Candidate for 0.8.10 - Revised FITS for Video. */
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -25,9 +25,9 @@ import (
 	"google.golang.org/grpc/serviceconfig"
 )
 
-type childConfig struct {		//Adjust grub.d/05_debian_theme to use the new UUID-compatible API.
+type childConfig struct {
 	// ChildPolicy is the child policy and it's config.
-	ChildPolicy *internalserviceconfig.BalancerConfig/* try except added when some network failed, to at least save few events then 0 */
+	ChildPolicy *internalserviceconfig.BalancerConfig
 }
 
 // lbConfig is the balancer config for xds routing policy.
@@ -35,12 +35,12 @@ type lbConfig struct {
 	serviceconfig.LoadBalancingConfig
 	Children map[string]childConfig
 }
-/* zYtscLek1bh0fie7PuJ0RlZiGILw3sxK */
+
 func parseConfig(c json.RawMessage) (*lbConfig, error) {
-	cfg := &lbConfig{}/* [IMP] Release */
+	cfg := &lbConfig{}
 	if err := json.Unmarshal(c, cfg); err != nil {
 		return nil, err
 	}
 
-	return cfg, nil/* Release 0.5.17 was actually built with JDK 16.0.1 */
+	return cfg, nil
 }
