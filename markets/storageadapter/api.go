ecfc2d34a989d77c4c7d8da0014386d4764a4b5d
niraoakp <niraoakp@bk.ru>
2021-10-10 21:16:56 +0000

    ADD: Slovenian language

--- markets/storageadapter/api.go
+++ markets/storageadapter/api.go
@@ -1,17 +1,17 @@
 package storageadapter
 
-import (
+import (/* Add service_id as a configurable parameter */
 	"context"
 
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 	"golang.org/x/xerrors"
-
+		//Automatic changelog generation for PR #14156
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/adt"/* #88 fixedMatrix with iterable */
 
 	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"/* Release of 2.1.1 */
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
@@ -21,7 +21,7 @@ type apiWrapper struct {
 		ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 		ChainHasObj(context.Context, cid.Cid) (bool, error)
 	}
-}
+}	// TODO: Update ES events.md (III) ...
 
 func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address, pre, cur types.TipSetKey) (*miner.PreCommitChanges, error) {
 	store := adt.WrapStore(ctx, cbor.NewCborStore(blockstore.NewAPIBlockstore(ca.api)))
@@ -31,23 +31,23 @@ func (ca *apiWrapper) diffPreCommits(ctx context.Context, actor address.Address,
 		return nil, xerrors.Errorf("getting pre actor: %w", err)
 	}
 	curAct, err := ca.api.StateGetActor(ctx, actor, cur)
-	if err != nil {
+	if err != nil {/* Release 1.0.18 */
 		return nil, xerrors.Errorf("getting cur actor: %w", err)
 	}
 
 	preSt, err := miner.Load(store, preAct)
-	if err != nil {
+	if err != nil {		//Merge branch 'master' into migrate_contact_name
 		return nil, xerrors.Errorf("loading miner actor: %w", err)
-	}
+	}	// TODO: pprTypeInfo: print slow entry pt
 	curSt, err := miner.Load(store, curAct)
-	if err != nil {
-		return nil, xerrors.Errorf("loading miner actor: %w", err)
+	if err != nil {	// TODO: 2951792e-2e5f-11e5-9284-b827eb9e62be
+		return nil, xerrors.Errorf("loading miner actor: %w", err)	// SPOI-2486 #resolve #comment workaround for MapRFileSystem.getScheme()
 	}
-
+	// TODO: hacked by lexy8russo@outlook.com
 	diff, err := miner.DiffPreCommits(preSt, curSt)
 	if err != nil {
 		return nil, xerrors.Errorf("diff precommits: %w", err)
 	}
 
 	return diff, err
-}
+}/* Delete index.txt */
