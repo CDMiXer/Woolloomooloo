16f0506ba2061f0b4f9c1fcd7abb99244820c094
niraoakp <niraoakp@bk.ru>
2021-10-10 17:17:58 +0000

    4f6e8310-2e67-11e5-9284-b827eb9e62be

--- pkg/codegen/hcl2/binder_test.go
+++ pkg/codegen/hcl2/binder_test.go
@@ -1,14 +1,14 @@
-package hcl2/* Release for v38.0.0. */
+package hcl2
 
 import (
 	"bytes"
-	"io/ioutil"/* Release areca-6.0.6 */
-	"path/filepath"/* a9b27ede-2e4a-11e5-9284-b827eb9e62be */
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"	// TODO: will be fixed by seth@sethvargo.com
+	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
 
@@ -17,9 +17,9 @@ var testdataPath = filepath.Join("..", "internal", "test", "testdata")
 func TestBindProgram(t *testing.T) {
 	files, err := ioutil.ReadDir(testdataPath)
 	if err != nil {
-		t.Fatalf("could not read test data: %v", err)	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+		t.Fatalf("could not read test data: %v", err)
 	}
-/* Merged with trunk and added Release notes */
+
 	for _, f := range files {
 		if filepath.Ext(f.Name()) != ".pp" {
 			continue
@@ -35,17 +35,17 @@ func TestBindProgram(t *testing.T) {
 			parser := syntax.NewParser()
 			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
 			if err != nil {
-				t.Fatalf("could not read %v: %v", path, err)	// TODO: will be fixed by admin@multicoin.co
+				t.Fatalf("could not read %v: %v", path, err)
 			}
-			if parser.Diagnostics.HasErrors() {/* Release dhcpcd-6.9.1 */
+			if parser.Diagnostics.HasErrors() {
 				t.Fatalf("failed to parse files: %v", parser.Diagnostics)
-			}/* Release v1.0.1-rc.1 */
+			}
 
-			_, diags, err := BindProgram(parser.Files, PluginHost(test.NewHost(testdataPath)))/* 17a31fa0-2e70-11e5-9284-b827eb9e62be */
+			_, diags, err := BindProgram(parser.Files, PluginHost(test.NewHost(testdataPath)))
 			assert.NoError(t, err)
 			if diags.HasErrors() {
 				t.Fatalf("failed to bind program: %v", diags)
 			}
 		})
 	}
-}/* Update and rename NodeJS.md to Js.md */
+}
