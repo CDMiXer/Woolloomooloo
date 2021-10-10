afa4e8d326195f01d4bf3ee2eae58871cb1c4a67
niraoakp <niraoakp@bk.ru>
2021-10-10 13:17:09 +0000

    Merge "Fix build (javadoc)." into dalvik-dev

--- handler/api/repos/collabs/list_test.go
+++ handler/api/repos/collabs/list_test.go
@@ -8,7 +8,7 @@ package collabs
 
 import (
 	"context"
-	"encoding/json"		//Update L3-intro-to-R.Rmd
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -19,11 +19,11 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"/* Delete Robot Store */
-)/* Release 1.6.4 */
+	"github.com/google/go-cmp/cmp"
+)
 
 var (
-	mockUser = &core.User{		//Update jiangConrathSimilarity.coffee
+	mockUser = &core.User{
 		ID:    1,
 		Login: "octocat",
 	}
@@ -36,30 +36,30 @@ var (
 	}
 
 	mockMember = &core.Perm{
-		Read:  true,/* Reformat qpulsehelpers. */
-		Write: true,/* misc var stuff, vec/vech utilities and some docs */
+		Read:  true,
+		Write: true,
 		Admin: true,
 	}
 
 	mockMembers = []*core.Collaborator{
-		{/* Release new version to fix problem having coveralls as a runtime dependency */
+		{
 			Login: "octocat",
-			Read:  true,/* Merge "Release note for disabling password generation" */
+			Read:  true,
 			Write: true,
 			Admin: true,
 		},
 		{
-			Login: "spaceghost",/* IHTSDO unified-Release 5.10.14 */
+			Login: "spaceghost",
 			Read:  true,
-			Write: true,/* Fixed queueing issues */
+			Write: true,
 			Admin: true,
 		},
 	}
 )
-/* added header for safe casts */
+
 func TestList(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()	// TODO: Added mp term names to pipeline core (only ids were in before).
+	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	members := mock.NewMockPermStore(controller)
@@ -68,17 +68,17 @@ func TestList(t *testing.T) {
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")		//Delete manuscript.tex
+	c.URLParams.Add("name", "hello-world")
 
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
-	)/* Bauerjevi triki */
+	)
 
 	HandleList(repos, members)(w, r)
-	if got, want := w.Code, 200; want != got {/* Fix for duplicate bookmarkableId */
-		t.Errorf("Want response code %d, got %d", want, got)	// TODO: Correct off by one
+	if got, want := w.Code, 200; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := []*core.Collaborator{}, mockMembers
