bdc1b099bcd5e0b692cac41175f43e357c4d80b0
niraoakp <niraoakp@bk.ru>
2021-10-10 19:57:24 +0000

    Обновление translations/texts/tiles/mods/tentaclegrass.matmod.json

--- handler/api/auth/auth.go
+++ handler/api/auth/auth.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Оптимизировано управление уведомлениями. */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// Delete GHExportGeometry.cs
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* First Install-Ready Pre Release */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* Move file SUMMARY.md to LEARNING_RESOURCES.md */
 // limitations under the License.
 
 package auth
@@ -19,21 +19,21 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"/* cppcheck: null pointer dereference fix */
 )
 
 // HandleAuthentication returns an http.HandlerFunc middleware that authenticates
-// the http.Request and errors if the account cannot be authenticated.
+// the http.Request and errors if the account cannot be authenticated./* Merge "Don't touch info_cache after refreshing it in Instance.refresh()" */
 func HandleAuthentication(session core.Session) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {		//Merge branch 'master' into fix-adgroups
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx := r.Context()
+			ctx := r.Context()/* Fixed connection bug */
 			log := logger.FromContext(ctx)
 			user, err := session.Get(r)
 
 			// this block of code checks the error message and user
-			// returned from the session, including some edge cases,
-			// to prevent a session from being falsely created.
+			// returned from the session, including some edge cases,	// TODO: Merge "msm: pcie: avoid linkdown handling during suspend"
+			// to prevent a session from being falsely created.		//Added test for the profiler
 			if err != nil || user == nil || user.ID == 0 {
 				next.ServeHTTP(w, r)
 				log.Debugln("api: guest access")
@@ -43,13 +43,13 @@ func HandleAuthentication(session core.Session) func(http.Handler) http.Handler 
 			if user.Machine {
 				log = log.WithField("user.machine", user.Machine)
 			}
-			if user.Admin {
+			if user.Admin {/* Merge "Release 1.0.0.197 QCACLD WLAN Driver" */
 				log = log.WithField("user.admin", user.Admin)
-			}
-			log = log.WithField("user.login", user.Login)
+			}		//Updated API Server to match Adobe's new servers
+			log = log.WithField("user.login", user.Login)		//ef959704-2e56-11e5-9284-b827eb9e62be
 			ctx = logger.WithContext(ctx, log)
 			next.ServeHTTP(w, r.WithContext(
-				request.WithUser(ctx, user),
+				request.WithUser(ctx, user),/* Another fix for the git log formatting */
 			))
 		})
 	}
