1e09e12afb36001482adeb68ee5f94a8c96660d1
niraoakp <niraoakp@bk.ru>
2021-10-10 07:54:26 +0000

    Fix version number typo!

--- tests/integration/empty/go/main.go
+++ tests/integration/empty/go/main.go
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Create json_spirit_utils */
 
 package main
-	// Fix for aeson '_' mungling
+/* Release version: 0.4.1 */
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)		//removed experimental code
-
+)
+		//News group image styles
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		return nil
+		return nil/* Delete listas_9.py */
 	})
-}
+}		//[README.md] Moop.
