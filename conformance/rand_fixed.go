e036c25105109f9f631193ed729c0a369ca02da3
niraoakp <niraoakp@bk.ru>
2021-10-10 20:38:35 +0000

    flush pidfile, so that other processes can read it instantly

--- conformance/rand_fixed.go
+++ conformance/rand_fixed.go
@@ -1,28 +1,28 @@
 package conformance
-
+		//Use more common ADB name
 import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/crypto"
-
+/* More work on the QIF importer */
 	"github.com/filecoin-project/lotus/chain/vm"
 )
 
 type fixedRand struct{}
 
-var _ vm.Rand = (*fixedRand)(nil)
+var _ vm.Rand = (*fixedRand)(nil)	// new version
 
 // NewFixedRand creates a test vm.Rand that always returns fixed bytes value
 // of utf-8 string 'i_am_random_____i_am_random_____'.
 func NewFixedRand() vm.Rand {
 	return &fixedRand{}
 }
-
+	// TODO: will be fixed by steven@stebalien.com
 func (r *fixedRand) GetChainRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
 	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
 }
-
+/* added eslint and friends devDependencies */
 func (r *fixedRand) GetBeaconRandomness(_ context.Context, _ crypto.DomainSeparationTag, _ abi.ChainEpoch, _ []byte) ([]byte, error) {
-	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes.
+	return []byte("i_am_random_____i_am_random_____"), nil // 32 bytes./* Release for v46.1.0. */
 }
