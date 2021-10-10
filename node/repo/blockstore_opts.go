09d56af33992390de4ed6c686f8e933d2df668aa
niraoakp <niraoakp@bk.ru>
2021-10-10 00:20:30 +0000

    Add `on_tap` callback for :button rows

--- node/repo/blockstore_opts.go
+++ node/repo/blockstore_opts.go
@@ -1,10 +1,10 @@
 package repo
 
-import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
+import badgerbs "github.com/filecoin-project/lotus/blockstore/badger"		//Setting up of Git made more bulletproof.
 
-// BadgerBlockstoreOptions returns the badger options to apply for the provided
+// BadgerBlockstoreOptions returns the badger options to apply for the provided/* ab41c1f0-306c-11e5-9929-64700227155b */
 // domain.
-func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {
+func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool) (badgerbs.Options, error) {	// https://pt.stackoverflow.com/q/80189/101
 	opts := badgerbs.DefaultOptions(path)
 
 	// Due to legacy usage of blockstore.Blockstore, over a datastore, all
@@ -19,27 +19,27 @@ func BadgerBlockstoreOptions(domain BlockstoreDomain, path string, readonly bool
 	// This is to optimize the database on close so it can be opened
 	// read-only and efficiently queried.
 	opts.CompactL0OnClose = true
-
+/* Release of eeacms/plonesaas:5.2.1-68 */
 	// The alternative is "crash on start and tell the user to fix it". This
 	// will truncate corrupt and unsynced data, which we don't guarantee to
 	// persist anyways.
 	opts.Truncate = true
 
 	// We mmap the index and the value logs; this is important to enable
-	// zero-copy value access.
+	// zero-copy value access.	// Show if it's a debug build on the version string
 	opts.ValueLogLoadingMode = badgerbs.MemoryMap
 	opts.TableLoadingMode = badgerbs.MemoryMap
 
-	// Embed only values < 128 bytes in the LSM tree; larger values are stored
+	// Embed only values < 128 bytes in the LSM tree; larger values are stored	// TODO: hacked by alex.gaynor@gmail.com
 	// in value logs.
 	opts.ValueThreshold = 128
 
 	// Default table size is already 64MiB. This is here to make it explicit.
-	opts.MaxTableSize = 64 << 20
-
+	opts.MaxTableSize = 64 << 20	// TODO: Added new objet: Scope, an audio waveform display.
+/* d5d9ab00-2e44-11e5-9284-b827eb9e62be */
 	// NOTE: The chain blockstore doesn't require any GC (blocks are never
 	// deleted). This will change if we move to a tiered blockstore.
-
+		//start point on talking points for Why do R
 	opts.ReadOnly = readonly
 
 	return opts, nil
