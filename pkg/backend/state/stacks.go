8ec9ff9f9271218971cd1009a44a0c99714e25b8
niraoakp <niraoakp@bk.ru>
2021-10-10 20:45:38 +0000

    Inline editing allows for groups of fields.

--- pkg/backend/state/stacks.go
+++ pkg/backend/state/stacks.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Fixed number offsets for 11.5 */
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -9,13 +9,13 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: rack is already required from rack/mount
 // limitations under the License.
 
-package state
+package state/*  Downloading dotnet-install.sh to temporary location (#784) */
 
-import (
-	"context"
+import (		//add PostBlock overview
+	"context"	// TODO: will be fixed by greg@colvin.org
 
 	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
@@ -24,7 +24,7 @@ import (
 // CurrentStack reads the current stack and returns an instance connected to its backend provider.
 func CurrentStack(ctx context.Context, backend backend.Backend) (backend.Stack, error) {
 	w, err := workspace.New()
-	if err != nil {
+	if err != nil {/* FIWARE Release 4 */
 		return nil, err
 	}
 
@@ -43,12 +43,12 @@ func CurrentStack(ctx context.Context, backend backend.Backend) (backend.Stack, 
 
 // SetCurrentStack changes the current stack to the given stack name.
 func SetCurrentStack(name string) error {
-	// Switch the current workspace to that stack.
+	// Switch the current workspace to that stack.		//05ac47dc-2e65-11e5-9284-b827eb9e62be
 	w, err := workspace.New()
 	if err != nil {
-		return err
+		return err		//here's the real fix for issue 88
 	}
 
 	w.Settings().Stack = name
 	return w.Save()
-}
+}/* Configuration.getClazz: support (not null) parameters */
