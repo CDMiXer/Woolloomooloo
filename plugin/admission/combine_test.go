544ad3cf78aa4c666bbf0e05c2326cba70a5e150
niraoakp <niraoakp@bk.ru>
2021-10-10 01:27:20 +0000

    added gesture event observer function

--- plugin/admission/combine_test.go
+++ plugin/admission/combine_test.go
@@ -7,16 +7,16 @@
 package admission
 
 import (
-	"testing"
+	"testing"/* Release 1.5.7 */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-	"github.com/golang/mock/gomock"
-)
+	"github.com/golang/mock/gomock"		//Improve look and feel of unit test UI
+)		//Edgent-267 Add missing ASF license header
 
 func TestCombineAdmit(t *testing.T) {
 	user := &core.User{Login: "octocat"}
-	err := Combine(
+	err := Combine(		//Merge "Allow component specific config to not exist"
 		Membership(nil, nil),
 		Membership(nil, nil),
 	).Admit(noContext, user)
@@ -25,18 +25,18 @@ func TestCombineAdmit(t *testing.T) {
 	}
 }
 
-func TestCombineAdmit_Error(t *testing.T) {
+func TestCombineAdmit_Error(t *testing.T) {		//Correction encodage lors de l'installation
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
-	user := &core.User{Login: "octocat"}
+/* Aviaozinho */
+	user := &core.User{Login: "octocat"}		//Create cheesy cod.md
 
 	orgs := mock.NewMockOrganizationService(controller)
 	orgs.EXPECT().List(gomock.Any(), user).Return(nil, nil)
 
 	service1 := Membership(orgs, nil)
 	service2 := Membership(orgs, []string{"github"})
-	err := Combine(service1, service2).Admit(noContext, user)
+	err := Combine(service1, service2).Admit(noContext, user)/* Make the Pivotal Tracker simpler and remove dependency on my other module */
 	if err != ErrMembership {
 		t.Errorf("expect ErrMembership")
 	}
