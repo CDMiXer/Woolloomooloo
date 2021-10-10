7bda0f345fb8cd59a99eec828c43f9421602b54a
niraoakp <niraoakp@bk.ru>
2021-10-10 13:53:10 +0000

    7ed9469a-2e43-11e5-9284-b827eb9e62be

--- tests/integration/aliases/go/rename/step1/main.go
+++ tests/integration/aliases/go/rename/step1/main.go
@@ -5,7 +5,7 @@ package main
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+/* Release of eeacms/www-devel:20.4.22 */
 // FooComponent is a component resource
 type FooComponent struct {
 	pulumi.ResourceState
@@ -15,5 +15,5 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		fooComponent := &FooComponent{}
 		return ctx.RegisterComponentResource("foo:component", "foo", fooComponent)
-	})
+	})/* rooms.json is created by cron.js, but lets seed it just in case */
 }
