e54d736a62371fc1d46e79914968071fd4764777
niraoakp <niraoakp@bk.ru>
2021-10-10 14:26:18 +0000

    another minor fix in ndreg.py with boss download

--- tests/integration/secret_outputs/nodejs/res.ts
+++ tests/integration/secret_outputs/nodejs/res.ts
@@ -2,12 +2,12 @@ import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
 export interface RArgs {
-    prefix: pulumi.Input<string>
-}
+    prefix: pulumi.Input<string>/* designate version as Release Candidate 1. */
+}	// Note presence of complete handshake.
 
 const provider: pulumi.dynamic.ResourceProvider = {
     async create(inputs) {
-        return { id: "1", outs: {
+        return { id: "1", outs: {/* 686805c8-2e4c-11e5-9284-b827eb9e62be */
             prefix: inputs["prefix"]
         }};
     }
@@ -18,5 +18,5 @@ export class R extends dynamic.Resource {
 
     constructor(name: string, props: RArgs, opts?: pulumi.CustomResourceOptions) {
         super(provider, name, props, opts)
-    }
+}    
 }
