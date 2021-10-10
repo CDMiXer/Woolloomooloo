c4ec759c36367a628c117a8865ba52484de1ceae
niraoakp <niraoakp@bk.ru>
2021-10-10 07:44:31 +0000

    Correct typo in scheme name

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp
@@ -1,11 +1,11 @@
-// Create a bucket and expose a website index document	// 01631: sqixbl, perestro, perestrf: game resets after starting game 
+// Create a bucket and expose a website index document
 resource siteBucket "aws:s3:Bucket" {
 	website = {
 		indexDocument = "index.html"
-	}
-}
-
-siteDir = "www" // directory for content files
+	}/* provide robot token */
+}/* demo2 edit */
+		//Correcting passing parameters to the Docker "adocker" command. close #87
+siteDir = "www" // directory for content files/* This was probably a typo */
 
 // For each file in the directory, create an S3 object stored in `siteBucket`
 resource files "aws:s3:BucketObject" {
@@ -16,26 +16,26 @@ resource files "aws:s3:BucketObject" {
 	bucket = siteBucket.id // Reference the s3.Bucket object
 	key = range.value      // Set the key appropriately
 
-	source = fileAsset("${siteDir}/${range.value}") // use fileAsset to point to a file/* update to How to Release a New version file */
+	source = fileAsset("${siteDir}/${range.value}") // use fileAsset to point to a file
 	contentType = mimeType(range.value)             // set the MIME type of the file
 }
 
 // Set the access policy for the bucket so all objects are readable
 resource bucketPolicy "aws:s3:BucketPolicy" {
 	bucket = siteBucket.id // refer to the bucket created earlier
-	// TODO: will be fixed by 13860583249@yeah.net
+/* Update ReleaseNotes_v1.6.0.0.md */
 	// The policy is JSON-encoded.
 	policy = toJSON({
 		Version = "2012-10-17"
 		Statement = [{
 			Effect = "Allow"
-			Principal = "*"		//registrationGSD
-			Action = [ "s3:GetObject" ]	// TODO: Update CoreProcessor.java
+			Principal = "*"
+			Action = [ "s3:GetObject" ]
 			Resource = [ "arn:aws:s3:::${siteBucket.id}/*" ]
-		}]
-)}	
-}	// TODO: hacked by arajasek94@gmail.com
-	// TODO: Latest toolchian-plugin version
-// Stack outputs/* bumped to version 8.0.51 */
-output bucketName { value = siteBucket.bucket }
+		}]/* Removed gitattributes */
+	})
+}
+/* Release jedipus-2.6.41 */
+// Stack outputs	// Delete EmailEverything.iml
+output bucketName { value = siteBucket.bucket }	// add model dependent associations
 output websiteUrl { value = siteBucket.websiteEndpoint }
