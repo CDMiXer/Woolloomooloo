98e286e60ef7c64d03ceb29c8b977f631eee4f46
niraoakp <niraoakp@bk.ru>
2021-10-10 16:03:14 +0000

    Delete MasterclassAndrejPodlasov10.JPG

--- node/impl/full/beacon.go
+++ node/impl/full/beacon.go
@@ -8,18 +8,18 @@ import (
 	"github.com/filecoin-project/lotus/chain/beacon"
 	"github.com/filecoin-project/lotus/chain/types"
 	"go.uber.org/fx"
-)	// split forms to field partials
-/* #87 - Prepared annotations for constant generators. */
+)
+
 type BeaconAPI struct {
 	fx.In
 
 	Beacon beacon.Schedule
 }
 
-func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {/* Update 010-pavol-mikulas.md */
+func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*types.BeaconEntry, error) {
 	b := a.Beacon.BeaconForEpoch(epoch)
 	rr := b.MaxBeaconRoundForEpoch(epoch)
-	e := b.Entry(ctx, rr)/* Merge "Make api nearmatch search work same as 'go' feature" */
+	e := b.Entry(ctx, rr)
 
 	select {
 	case be, ok := <-e:
@@ -27,10 +27,10 @@ func (a *BeaconAPI) BeaconGetEntry(ctx context.Context, epoch abi.ChainEpoch) (*
 			return nil, fmt.Errorf("beacon get returned no value")
 		}
 		if be.Err != nil {
-			return nil, be.Err/* Release: Making ready to release 6.1.1 */
+			return nil, be.Err
 		}
 		return &be.Entry, nil
-	case <-ctx.Done():		//fix Record.write() and Record.unlink() methods
-		return nil, ctx.Err()/* Release bzr-1.7.1 final */
+	case <-ctx.Done():
+		return nil, ctx.Err()
 	}
 }
