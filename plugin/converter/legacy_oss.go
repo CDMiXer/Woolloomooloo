d559d4adf06b40219ab84b76da9b2869488473fc
niraoakp <niraoakp@bk.ru>
2021-10-10 06:10:40 +0000

    Uklanjanje binarnih datoteke

--- plugin/converter/legacy_oss.go
+++ plugin/converter/legacy_oss.go
@@ -1,6 +1,6 @@
-// Copyright 2019 Drone IO, Inc./* Merge "msm: vidc: set ctrl to request sequence header for encoder" */
-//	// TODO: will be fixed by fkautz@pseudocode.cc
-// Licensed under the Apache License, Version 2.0 (the "License");		//moved all direct session scope access to use the sessionContext service
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -10,18 +10,18 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* rambles about sockets */
+// limitations under the License.
 
 // +build oss
 
 package converter
 
 import (
-	"github.com/drone/drone/core"/* #48 - Release version 2.0.0.M1. */
-)
+	"github.com/drone/drone/core"
+)/* Fixed bug in hstoreFromPhp, added date functions. */
 
 // Legacy returns a conversion service that converts the
 // legacy 0.8 file to a yaml file.
-func Legacy(enabled bool) core.ConvertService {
+func Legacy(enabled bool) core.ConvertService {/* Merge "Release 1.0.0.182 QCACLD WLAN Driver" */
 	return new(noop)
 }
