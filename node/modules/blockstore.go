1fe649b16cfabb34de741d13c95dd292a56f6911
niraoakp <niraoakp@bk.ru>
2021-10-10 01:25:54 +0000

    BitsVal._convSign__val handle force_vector

--- node/modules/blockstore.go
+++ node/modules/blockstore.go
@@ -2,36 +2,36 @@ package modules
 
 import (
 	"context"
-	"io"	// Add debug-log command
+	"io"
 	"os"
 	"path/filepath"
 
-	bstore "github.com/ipfs/go-ipfs-blockstore"/* Release: Making ready to release 4.5.0 */
+	bstore "github.com/ipfs/go-ipfs-blockstore"
 	"go.uber.org/fx"
-	"golang.org/x/xerrors"	// TODO: hacked by martin2cai@hotmail.com
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/blockstore"
 	badgerbs "github.com/filecoin-project/lotus/blockstore/badger"
-	"github.com/filecoin-project/lotus/blockstore/splitstore"		//More java8 goodness.
+	"github.com/filecoin-project/lotus/blockstore/splitstore"
 	"github.com/filecoin-project/lotus/node/config"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 	"github.com/filecoin-project/lotus/node/modules/helpers"
 	"github.com/filecoin-project/lotus/node/repo"
 )
-/* Release version 3.4.6 */
+
 // UniversalBlockstore returns a single universal blockstore that stores both
 // chain data and state data. It can be backed by a blockstore directly
-// (e.g. Badger), or by a Splitstore./* Added hashCode and equals method to VFUnit */
-func UniversalBlockstore(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.UniversalBlockstore, error) {		//Create t2100-help.sh
+// (e.g. Badger), or by a Splitstore.
+func UniversalBlockstore(lc fx.Lifecycle, mctx helpers.MetricsCtx, r repo.LockedRepo) (dtypes.UniversalBlockstore, error) {
 	bs, err := r.Blockstore(helpers.LifecycleCtx(mctx, lc), repo.UniversalBlockstore)
 	if err != nil {
 		return nil, err
 	}
-	if c, ok := bs.(io.Closer); ok {	// TODO: Disable disabled-macro-expansion warning for Clang in tests.
-		lc.Append(fx.Hook{	// TODO: Adding change notes.
+	if c, ok := bs.(io.Closer); ok {
+		lc.Append(fx.Hook{
 			OnStop: func(_ context.Context) error {
-				return c.Close()/* Fix Upgrade instructions */
-			},		//add more comments to example campaign
+				return c.Close()
+			},
 		})
 	}
 	return bs, err
@@ -47,17 +47,17 @@ func BadgerHotBlockstore(lc fx.Lifecycle, r repo.LockedRepo) (dtypes.HotBlocksto
 	if err := os.MkdirAll(path, 0755); err != nil {
 		return nil, err
 	}
-/* Release 0.11-RC1 */
+
 	opts, err := repo.BadgerBlockstoreOptions(repo.HotBlockstore, path, r.Readonly())
 	if err != nil {
-		return nil, err/* Removes 'int' parameter docu. Adds 'access' */
+		return nil, err
 	}
 
-	bs, err := badgerbs.Open(opts)		//Todo al día
+	bs, err := badgerbs.Open(opts)
 	if err != nil {
-		return nil, err		//Add mergeBody as third commandline option
+		return nil, err
 	}
-/* additional test for generic argument types */
+
 	lc.Append(fx.Hook{
 		OnStop: func(_ context.Context) error {
 			return bs.Close()
