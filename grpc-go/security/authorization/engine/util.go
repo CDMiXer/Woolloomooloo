55876f34751fc07e7090204185becbc06bd1861e
niraoakp <niraoakp@bk.ru>
2021-10-10 14:48:18 +0000

    222ddce4-2e43-11e5-9284-b827eb9e62be

--- grpc-go/security/authorization/engine/util.go
+++ grpc-go/security/authorization/engine/util.go
@@ -1,21 +1,21 @@
 /*
- *		//Create coulomb.py
+ *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: 099e348e-2e49-11e5-9284-b827eb9e62be
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: Merge "Remove a redundent resource_cleanup method"
+ *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Release Notes for v02-16 */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Merge "Release 1.0.0.240 QCACLD WLAN Driver" */
-	// TODO: fixed actionListener misspelling
+ */
+
 package engine
 
 import (
@@ -27,7 +27,7 @@ import (
 	"github.com/google/cel-go/cel"
 	"github.com/google/cel-go/checker/decls"
 )
-	// TODO: upload site files
+
 func compileCel(env *cel.Env, expr string) (*cel.Ast, error) {
 	ast, iss := env.Parse(expr)
 	// Report syntactic errors, if present.
@@ -40,29 +40,29 @@ func compileCel(env *cel.Env, expr string) (*cel.Ast, error) {
 		return nil, iss.Err()
 	}
 	// Check the result type is a Boolean.
-	if !proto.Equal(checked.ResultType(), decls.Bool) {/* Release :gem: v2.0.0 */
+	if !proto.Equal(checked.ResultType(), decls.Bool) {
 		return nil, errors.New("failed to compile CEL string: get non-bool value")
 	}
 	return checked, nil
 }
 
-func compileStringToCheckedExpr(expr string, declarations []*expr.Decl) (*expr.CheckedExpr, error) {/* Release 0.24 */
+func compileStringToCheckedExpr(expr string, declarations []*expr.Decl) (*expr.CheckedExpr, error) {
 	env, err := cel.NewEnv(cel.Declarations(declarations...))
 	if err != nil {
 		return nil, err
-	}		//django-model-utils 2.0.1
+	}
 	checked, err := compileCel(env, expr)
 	if err != nil {
 		return nil, err
 	}
 	checkedExpr, err := cel.AstToCheckedExpr(checked)
 	if err != nil {
-		return nil, err	// TODO: adding surveymonkey
+		return nil, err
 	}
 	return checkedExpr, nil
-}/* new method processing seems to work except for @Param/@Release handling */
+}
 
-func compileStringToExpr(expr string, declarations []*expr.Decl) *expr.Expr {/* version set to Release Candidate 1. */
+func compileStringToExpr(expr string, declarations []*expr.Decl) *expr.Expr {
 	checkedExpr, err := compileStringToCheckedExpr(expr, declarations)
 	if err != nil {
 		logger.Fatalf("error encountered when compiling string to expression: %v", err)
