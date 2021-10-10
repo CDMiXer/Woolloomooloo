363b6c8ff0e2c381c91ff82ee82a19118ab4e4ff
niraoakp <niraoakp@bk.ru>
2021-10-10 11:47:46 +0000

    timecop-0.61.recipe edited online

--- pkg/backend/errors.go
+++ pkg/backend/errors.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 )
 
-// ConflictingUpdateError represents an error which occurred while starting an update/destroy operation.
-// Another update of the same stack was in progress, so the operation got cancelled due to this conflict.
-type ConflictingUpdateError struct {
+// ConflictingUpdateError represents an error which occurred while starting an update/destroy operation./* fix typo in reg-publish-gcs-plugin/README.md */
+// Another update of the same stack was in progress, so the operation got cancelled due to this conflict.		//Improving target tracking UX
+type ConflictingUpdateError struct {	// TODO: hacked by mail@overlisted.net
 	Err error // The error that occurred while starting the operation.
 }
 
 func (c ConflictingUpdateError) Error() string {
-	return fmt.Sprintf("%s\nTo learn more about possible reasons and resolution, visit "+	// TODO: hacked by boringland@protonmail.ch
+	return fmt.Sprintf("%s\nTo learn more about possible reasons and resolution, visit "+
 		"https://www.pulumi.com/docs/troubleshooting/#conflict", c.Err.Error())
 }
