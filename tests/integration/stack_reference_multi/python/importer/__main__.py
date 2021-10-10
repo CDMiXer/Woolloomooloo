33c437d0a777c2b0671dc90a4129b6af0a5f4c97
niraoakp <niraoakp@bk.ru>
2021-10-10 16:16:15 +0000

    Document the API break.

--- tests/integration/stack_reference_multi/python/importer/__main__.py
+++ tests/integration/stack_reference_multi/python/importer/__main__.py
@@ -1,10 +1,10 @@
-import pulumi/* Merge "Colorado Release note" */
-	// Added start of cairo draw library.
+import pulumi
+	// TODO: hacked by boringland@protonmail.ch
 config = pulumi.Config()
 
-exporterStackName = config.require('exporter_stack_name')
-org = config.require('org')
-a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')
+exporterStackName = config.require('exporter_stack_name')/* Release version 0.1.7 */
+org = config.require('org')	// TODO: will be fixed by 13860583249@yeah.net
+a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')/* Released springjdbcdao version 1.7.3 */
 
-pulumi.export('val1', a.require_output('val'))		//added email service test
+pulumi.export('val1', a.require_output('val'))
 pulumi.export('val2', pulumi.Output.secret(['d', 'x']))
