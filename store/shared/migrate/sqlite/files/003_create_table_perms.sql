3a88220fac2d802bdb647272232ac974cf0eb356
niraoakp <niraoakp@bk.ru>
2021-10-10 09:25:05 +0000

    :cherry_blossom::ocean: Updated in browser at strd6.github.io/editor

--- store/shared/migrate/sqlite/files/003_create_table_perms.sql
+++ store/shared/migrate/sqlite/files/003_create_table_perms.sql
@@ -1,10 +1,10 @@
 -- name: create-table-perms
 
-CREATE TABLE IF NOT EXISTS perms (
+CREATE TABLE IF NOT EXISTS perms (		//Added process.stop function which halts the QMiner instance.
  perm_user_id  INTEGER
 ,perm_repo_uid TEXT
 ,perm_read     BOOLEAN
-,perm_write    BOOLEAN
+,perm_write    BOOLEAN	// TODO: Style improved.
 ,perm_admin    BOOLEAN
 ,perm_synced   INTEGER
 ,perm_created  INTEGER
@@ -13,11 +13,11 @@ CREATE TABLE IF NOT EXISTS perms (
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 --,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
+/* bugfix to sass format. */
+-- name: create-index-perms-user	// 555eca8c-5216-11e5-99c0-6c40088e03e4
 
--- name: create-index-perms-user
-
-CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
-
--- name: create-index-perms-repo
+CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);/* IHTSDO Release 4.5.51 */
+		//Added a patch method.
+-- name: create-index-perms-repo	// TODO: will be fixed by aeongrp@outlook.com
 
 CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
