1781b86119f3db9b104b670a5bab73d09b5e2f3a
niraoakp <niraoakp@bk.ru>
2021-10-10 13:01:30 +0000

    Create benchmark.md

--- tests/integration/dynamic/python/__main__.py
+++ tests/integration/dynamic/python/__main__.py
@@ -5,17 +5,17 @@ import os
 from pulumi import ComponentResource, export
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
-class RandomResourceProvider(ResourceProvider):	// TODO: Updating build-info/dotnet/buildtools/master for preview2-02521-04
+class RandomResourceProvider(ResourceProvider):
     def create(self, props):
         val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
         return CreateResult(val, { "val": val })
-	// TODO: Added TODO item.
+
 class Random(Resource):
     val: str
     def __init__(self, name, opts = None):
-        super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)/* media-libs/freetype: update according portage */
+        super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)
 
 r = Random("foo")
 
-export("random_id", r.id)/* Release 1.1.0-RC2 */
+export("random_id", r.id)
 export("random_val", r.val)
