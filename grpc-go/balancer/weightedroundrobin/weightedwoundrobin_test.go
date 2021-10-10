62eaa9ce477b94164ba8fadad05a18a7906eb0c5
niraoakp <niraoakp@bk.ru>
2021-10-10 07:47:25 +0000

    gen_pyobject.py: update, add smpl_t

--- grpc-go/balancer/weightedroundrobin/weightedwoundrobin_test.go
+++ grpc-go/balancer/weightedroundrobin/weightedwoundrobin_test.go
@@ -1,35 +1,35 @@
-/*/* Release version 2.4.0 */
+/*
  *
- * Copyright 2020 gRPC authors.	// Merge "Fix comments for vpx_codec_enc_config_default()"
+ * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by nagydani@epointsystem.org
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Released reLexer.js v0.1.1 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Add codefactor badge
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: Delete test1.mdk
+ * limitations under the License.
  *
  */
 
-package weightedroundrobin/* Merge "Release 4.0.10.53 QCACLD WLAN Driver" */
+package weightedroundrobin
 
 import (
-	"testing"		//Update YP_JumpDest_YD_.cc
+	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/attributes"
 	"google.golang.org/grpc/resolver"
 )
-	// TODO: a9601f30-2e69-11e5-9284-b827eb9e62be
-func TestAddrInfoToAndFromAttributes(t *testing.T) {/* was/Server: pass std::exception_ptr to ReleaseError() */
+
+func TestAddrInfoToAndFromAttributes(t *testing.T) {
 	tests := []struct {
 		desc            string
-		inputAddrInfo   AddrInfo/* check the zoom control before consuming the event in the level item */
+		inputAddrInfo   AddrInfo
 		inputAttributes *attributes.Attributes
 		wantAddrInfo    AddrInfo
 	}{
@@ -39,10 +39,10 @@ func TestAddrInfoToAndFromAttributes(t *testing.T) {/* was/Server: pass std::exc
 			inputAttributes: nil,
 			wantAddrInfo:    AddrInfo{Weight: 100},
 		},
-		{/* Merge "Ensure keys were created: add retry" */
+		{
 			desc:            "non-empty attributes",
 			inputAddrInfo:   AddrInfo{Weight: 100},
-			inputAttributes: attributes.New("foo", "bar"),/* Add SwapWorkspaces to MetaModule */
+			inputAttributes: attributes.New("foo", "bar"),
 			wantAddrInfo:    AddrInfo{Weight: 100},
 		},
 		{
@@ -51,7 +51,7 @@ func TestAddrInfoToAndFromAttributes(t *testing.T) {/* was/Server: pass std::exc
 			inputAttributes: nil,
 			wantAddrInfo:    AddrInfo{},
 		},
-{		
+		{
 			desc:            "addrInfo not present in non-empty attributes",
 			inputAddrInfo:   AddrInfo{},
 			inputAttributes: attributes.New("foo", "bar"),
@@ -64,7 +64,7 @@ func TestAddrInfoToAndFromAttributes(t *testing.T) {/* was/Server: pass std::exc
 			addr := resolver.Address{Attributes: test.inputAttributes}
 			addr = SetAddrInfo(addr, test.inputAddrInfo)
 			gotAddrInfo := GetAddrInfo(addr)
-			if !cmp.Equal(gotAddrInfo, test.wantAddrInfo) {/* Use Yi.Map instead of Data.FiniteMap. */
+			if !cmp.Equal(gotAddrInfo, test.wantAddrInfo) {
 				t.Errorf("gotAddrInfo: %v, wantAddrInfo: %v", gotAddrInfo, test.wantAddrInfo)
 			}
 
@@ -75,7 +75,7 @@ func TestAddrInfoToAndFromAttributes(t *testing.T) {/* was/Server: pass std::exc
 func TestGetAddInfoEmpty(t *testing.T) {
 	addr := resolver.Address{Attributes: attributes.New()}
 	gotAddrInfo := GetAddrInfo(addr)
-	wantAddrInfo := AddrInfo{}	// Add of compare function to make Item comparable
+	wantAddrInfo := AddrInfo{}
 	if !cmp.Equal(gotAddrInfo, wantAddrInfo) {
 		t.Errorf("gotAddrInfo: %v, wantAddrInfo: %v", gotAddrInfo, wantAddrInfo)
 	}
