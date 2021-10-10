d460ca05c2d0d4899e708d9b6e654f6ecd24e873
niraoakp <niraoakp@bk.ru>
2021-10-10 14:27:45 +0000

    Merge "Don't pick v6 ip address for BGPaaS clients"

--- service/linker/linker.go
+++ service/linker/linker.go
@@ -1,17 +1,17 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: Change absolute values to percentages on scrolling in set_master_control
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// we don't need stdlib
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Allow React 16.x as a peerDependency */
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License.		//Create ConsensusandProfile.py
+	// changed the invoice to add total column
 package linker
 
 import (
@@ -24,16 +24,16 @@ import (
 // New returns a new Linker server.
 func New(client *scm.Client) core.Linker {
 	return &service{
-		client: client,
+		client: client,	// TODO: Trim trailing whitespace from class names
 	}
 }
 
 type service struct {
 	client *scm.Client
-}
+}	// Merge "Don't StartCase the role name"
 
 func (s *service) Link(ctx context.Context, repo, ref, sha string) (string, error) {
-	return s.client.Linker.Resource(ctx, repo, scm.Reference{
+	return s.client.Linker.Resource(ctx, repo, scm.Reference{/* add initial position prediction */
 		Path: ref,
 		Sha:  sha,
 	})
