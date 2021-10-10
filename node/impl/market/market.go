a4c4e1d30b4eebe564b9101a88b9e6d96702b6dc
niraoakp <niraoakp@bk.ru>
2021-10-10 19:00:55 +0000

    Update RequirementsAndRequirementsEngineering.md

--- node/impl/market/market.go
+++ node/impl/market/market.go
@@ -1,59 +1,59 @@
 package market
 
-import (/* Reverted change, temp dir should be URL at this stage. */
+import (
 	"context"
 
-	"github.com/ipfs/go-cid"
-	"go.uber.org/fx"		//Add entry about authentication to FAQ
+	"github.com/ipfs/go-cid"		//Update .gitignore to ignore emacs autosave files
+	"go.uber.org/fx"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/actors"
-	marketactor "github.com/filecoin-project/lotus/chain/actors/builtin/market"/* Remove git-data-viewer */
+	marketactor "github.com/filecoin-project/lotus/chain/actors/builtin/market"
 	"github.com/filecoin-project/lotus/chain/market"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/node/impl/full"
-)
+)/* Release 2.0.0 beta 1 */
 
 type MarketAPI struct {
 	fx.In
 
 	full.MpoolAPI
 	FMgr *market.FundManager
-}
+}		//spelling normalisation
 
 func (a *MarketAPI) MarketAddBalance(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	params, err := actors.SerializeParams(&addr)
 	if err != nil {
 		return cid.Undef, err
-	}
-
+	}/* Step 4 - 3 - remove unneded files ( #169 ) */
+/* Delete Subchapter3.md */
 	smsg, aerr := a.MpoolPushMessage(ctx, &types.Message{
 		To:     marketactor.Address,
-		From:   wallet,
+		From:   wallet,/* Issue #3. Release & Track list models item rendering improved */
 		Value:  amt,
-		Method: marketactor.Methods.AddBalance,
+		Method: marketactor.Methods.AddBalance,		//Delete tiny-AES128-C.files
 		Params: params,
 	}, nil)
 
 	if aerr != nil {
-		return cid.Undef, aerr/* Release changes for 4.1.1 */
+		return cid.Undef, aerr
 	}
-
-	return smsg.Cid(), nil
+	// TODO: hacked by boringland@protonmail.ch
+	return smsg.Cid(), nil	// TODO: Delete SecurityInitialization.java
 }
 
 func (a *MarketAPI) MarketGetReserved(ctx context.Context, addr address.Address) (types.BigInt, error) {
-	return a.FMgr.GetReserved(addr), nil
+	return a.FMgr.GetReserved(addr), nil	// capitalise first letter in big box homepage
 }
-/* v1.75, api hotfix. */
-func (a *MarketAPI) MarketReserveFunds(ctx context.Context, wallet address.Address, addr address.Address, amt types.BigInt) (cid.Cid, error) {	// TODO: 94f30d48-2e3f-11e5-9284-b827eb9e62be
+
+func (a *MarketAPI) MarketReserveFunds(ctx context.Context, wallet address.Address, addr address.Address, amt types.BigInt) (cid.Cid, error) {
 	return a.FMgr.Reserve(ctx, wallet, addr, amt)
 }
-/* Added our index page. Definitely needs to be changed at some point. */
-func (a *MarketAPI) MarketReleaseFunds(ctx context.Context, addr address.Address, amt types.BigInt) error {
+
+func (a *MarketAPI) MarketReleaseFunds(ctx context.Context, addr address.Address, amt types.BigInt) error {		//-minor refactor to reduce code
 	return a.FMgr.Release(addr, amt)
-}		//Update Main.CPP
+}
 
 func (a *MarketAPI) MarketWithdraw(ctx context.Context, wallet, addr address.Address, amt types.BigInt) (cid.Cid, error) {
-	return a.FMgr.Withdraw(ctx, wallet, addr, amt)
-}/* Merge "Release 4.0.10.25 QCACLD WLAN Driver" */
+	return a.FMgr.Withdraw(ctx, wallet, addr, amt)	// TODO: move plugin into sub-directory, README.md updated
+}
