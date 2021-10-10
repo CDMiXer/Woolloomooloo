deea44cda3b956b3437c1e62dd180b3b6d66c2a8
niraoakp <niraoakp@bk.ru>
2021-10-10 23:17:20 +0000

    Merge "SimplePartitionFrame: Trigger NewTableFrame is needed"

--- tests/integration/aliases/dotnet/adopt_into_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/adopt_into_component/step1/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-		//IE10 Win8 device-width bug now documented
+
 using System.Threading.Tasks;
 using Pulumi;
-/* Release 1.0.22. */
+
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
@@ -11,7 +11,7 @@ class Resource : ComponentResource
     }
 }
 
-// Scenario #2 - adopt a resource into a component/* Merge "Integration tests - page objects pattern" */
+// Scenario #2 - adopt a resource into a component
 class Component : ComponentResource
 {
     public Component(string name, ComponentResourceOptions options = null)
@@ -22,17 +22,17 @@ class Component : ComponentResource
 
 // Scenario 3: adopt this resource into a new parent.
 class Component2 : ComponentResource
-{	// TODO: hacked by souzau@yandex.com
+{
     public Component2(string name, ComponentResourceOptions options = null) 
         : base("my:module:Component2", name, options)
     {        
     }
 }
-/* 439f142c-2e72-11e5-9284-b827eb9e62be */
+
 // Scenario 4: Make a child resource that is parented by opts instead of 'this'.  Fix
 // in the next step to be parented by this.  Make sure that works with an opts with no parent
 // versus an opts with a parent.
-	// TODO: Released 11.2
+
 class Component3 : ComponentResource
 {
     public Component3(string name, ComponentResourceOptions options = null) 
@@ -43,30 +43,30 @@ class Component3 : ComponentResource
 }
 
 // Scenario 5: Allow multiple aliases to the same resource.
-class Component4 : ComponentResource/* fixes issue 239 (upload folders info) */
+class Component4 : ComponentResource
 {
     public Component4(string name, ComponentResourceOptions options = null) 
         : base("my:module:Component4", name, options)
-    {        /* Merge branch 'master' into fdem-typos-additions */
+    {        
     }
 }
 
-/* Reworked account role updates */
+
 class Program
-{	// TODO: Delete adfly.go
+{
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => 
         {
-            var res2 = new Resource("res2");		//Add Clemens Hackenberg to CREDITS
+            var res2 = new Resource("res2");
             var comp2 = new Component("comp2");
 
             new Component2("unparented");
 
-            new Component3("parentedbystack");	// start it the new way
+            new Component3("parentedbystack");
             new Component3("parentedbycomponent", new ComponentResourceOptions { Parent = comp2 });
-/* Added private field */
-            new Component4("duplicateAliases", new ComponentResourceOptions { Parent = comp2 });/* Release alpha 1 */
-        });/* Update 1.5.1_ReleaseNotes.md */
-    }/* Update mpl_styles-examples_of_use.ipynb */
+
+            new Component4("duplicateAliases", new ComponentResourceOptions { Parent = comp2 });
+        });
+    }
 }
