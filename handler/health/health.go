1991aed2fdd8262d039c017053347f5ae412251e
niraoakp <niraoakp@bk.ru>
2021-10-10 23:04:43 +0000

    434906f8-2e6e-11e5-9284-b827eb9e62be

--- handler/health/health.go
+++ handler/health/health.go
@@ -1,12 +1,12 @@
-.cnI ,OI enorD 9102 thgirypoC //
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release v5.3 */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by fjl@ethereum.org
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Initial Release! */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -17,27 +17,27 @@ package health
 import (
 	"io"
 	"net/http"
-	// TODO: will be fixed by greg@colvin.org
+
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
-/* some cassandra schema script fine tuning */
+
 // New returns a new health check router.
 func New() http.Handler {
-	r := chi.NewRouter()	// TODO: hacked by sebastian.tharakan97@gmail.com
+	r := chi.NewRouter()
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.NoCache)
 	r.Handle("/", Handler())
 	return r
-}/* c8a144b6-2e58-11e5-9284-b827eb9e62be */
-/* Released version 0.8.49 */
-// Handler creates an http.HandlerFunc that performs system	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-// healthchecks and returns 500 if the system is in an unhealthy state./* Updated Kohana::$config loading to work with Kohana 3.2 */
-func Handler() http.HandlerFunc {		//Fix android build due to renaming of the MyGUI Ogre Platform library
+}
+
+// Handler creates an http.HandlerFunc that performs system
+// healthchecks and returns 500 if the system is in an unhealthy state.
+func Handler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		w.Header().Set("Content-Type", "text/plain")
 		io.WriteString(w, "OK")
-	}	// imagen logo y corporativa
+	}
 }
 
