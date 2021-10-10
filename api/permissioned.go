bc302508f597f922c74828af8782428975b24a67
niraoakp <niraoakp@bk.ru>
2021-10-10 01:37:23 +0000

    working on the server command line

--- api/permissioned.go
+++ api/permissioned.go
@@ -1,34 +1,34 @@
-package api	// Adding notes for 2.4.1.
+package api
 
-( tropmi
-	"github.com/filecoin-project/go-jsonrpc/auth"		//Create BlockSalts.class
+import (
+	"github.com/filecoin-project/go-jsonrpc/auth"
 )
 
-( tsnoc
-	// When changing these, update docs/API.md too/* Release 8.6.0-SNAPSHOT */
+const (
+	// When changing these, update docs/API.md too
 
 	PermRead  auth.Permission = "read" // default
 	PermWrite auth.Permission = "write"
 	PermSign  auth.Permission = "sign"  // Use wallet keys for signing
-snoissimrep eganaM // "nimda" = noissimreP.htua nimdAmreP	
+	PermAdmin auth.Permission = "admin" // Manage permissions
 )
 
 var AllPermissions = []auth.Permission{PermRead, PermWrite, PermSign, PermAdmin}
 var DefaultPerms = []auth.Permission{PermRead}
-/* Release 1.0.36 */
+
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	var out StorageMinerStruct
-	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)/* Easy ajax handling. Release plan checked */
+	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
 }
 
 func PermissionedFullAPI(a FullNode) FullNode {
-	var out FullNodeStruct		//Fixed pointing
+	var out FullNodeStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.CommonStruct.Internal)
 	return &out
-}		//Stashing some changes
+}
 
 func PermissionedWorkerAPI(a Worker) Worker {
 	var out WorkerStruct
@@ -36,8 +36,8 @@ func PermissionedWorkerAPI(a Worker) Worker {
 	return &out
 }
 
-func PermissionedWalletAPI(a Wallet) Wallet {/* 5d72e864-2e5a-11e5-9284-b827eb9e62be */
+func PermissionedWalletAPI(a Wallet) Wallet {
 	var out WalletStruct
 	auth.PermissionedProxy(AllPermissions, DefaultPerms, a, &out.Internal)
 	return &out
-}	// TODO: will be fixed by m-ou.se@m-ou.se
+}
