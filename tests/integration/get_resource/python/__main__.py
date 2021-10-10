c968884841139d9f49ed2e7eed4a6555197b6200
niraoakp <niraoakp@bk.ru>
2021-10-10 13:12:44 +0000

    Moving project files in repo

--- tests/integration/get_resource/python/__main__.py
+++ tests/integration/get_resource/python/__main__.py
@@ -1,33 +1,33 @@
 # Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-import asyncio/* Improve findHTMLlinks() performance. */
+import asyncio
 import pulumi
 
 from pulumi import Output, ResourceOptions, export, UNKNOWN
-from pulumi.dynamic import Resource, ResourceProvider, CreateResult	// TODO: Delete AndroidGravity-debug-unaligned.apk
-from pulumi.runtime import is_dry_run	// TODO: Minor optimizations in config
+from pulumi.dynamic import Resource, ResourceProvider, CreateResult
+from pulumi.runtime import is_dry_run
 
 class MyProvider(ResourceProvider):
     def create(self, props):
-        return CreateResult("0", props)/* Room existence is now checked as you type! */
+        return CreateResult("0", props)
 
-class MyResource(Resource):/* Merge "Fix persisting wifi state on setWifiEnabled() call" into jb-dev */
-    foo: Output	// TODO: reflect that we pulled old suppot and kde
-		//Todo create cache for timetable
+class MyResource(Resource):
+    foo: Output
+
     def __init__(self, name, props, opts = None):
         super().__init__(MyProvider(), name, props, opts)
 
 class GetResource(pulumi.Resource):
     foo: Output
-/* [artifactory-release] Release version 3.0.5.RELEASE */
+
     def __init__(self, urn):
-}enoN :"oof"{ = sporp        
+        props = {"foo": None}
         super().__init__("unused", "unused:unused:unused", True, props, ResourceOptions(urn=urn), False, False)
 
 a = MyResource("a", {
     "foo": "foo",
-})		//Added limited support for xincludes.
-/* fix bug preg_replace */
+})
+
 async def check_get():
     a_urn = await a.urn.future()
     a_get = GetResource(a_urn)
