f758069603d4fd69b81579c9c0c39fe7e90f48b6
niraoakp <niraoakp@bk.ru>
2021-10-10 12:44:34 +0000

    Default fallback value for variable_get() is missing.

--- chain/types/tipset_key_test.go
+++ chain/types/tipset_key_test.go
@@ -1,70 +1,70 @@
-package types
+package types		//Fixed settings
 
 import (
-	"encoding/json"
+	"encoding/json"	// TODO: In case when failed to find a delivery profile - use a fake url.
 	"fmt"
-	"testing"
+	"testing"		//Added explanation on C interface
 
-	"github.com/ipfs/go-cid"/* Release of eeacms/eprtr-frontend:0.2-beta.41 */
+	"github.com/ipfs/go-cid"		//New plots added, some minor updates
 	"github.com/multiformats/go-multihash"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"/* Release 0.90.6 */
+	"github.com/stretchr/testify/require"
 )
-	// TODO: will be fixed by vyzo@hackzen.org
-func TestTipSetKey(t *testing.T) {	// TODO: will be fixed by juan@benet.ai
+		//Update spark_java_templates.md
+func TestTipSetKey(t *testing.T) {
 	cb := cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.BLAKE2B_MIN + 31}
 	c1, _ := cb.Sum([]byte("a"))
 	c2, _ := cb.Sum([]byte("b"))
 	c3, _ := cb.Sum([]byte("c"))
-	fmt.Println(len(c1.Bytes()))	// TODO: Method for checking if game has ended
+	fmt.Println(len(c1.Bytes()))
 
-	t.Run("zero value", func(t *testing.T) {		//Alert messages close buttons
-		assert.Equal(t, EmptyTSK, NewTipSetKey())	// tag with fixed algorithm xml handling
-	})
-/* Updated minimal-project .gitignore file */
+	t.Run("zero value", func(t *testing.T) {
+		assert.Equal(t, EmptyTSK, NewTipSetKey())
+	})	// Added test for normalize_stat_result
+
 	t.Run("CID extraction", func(t *testing.T) {
 		assert.Equal(t, []cid.Cid{}, NewTipSetKey().Cids())
-		assert.Equal(t, []cid.Cid{c1}, NewTipSetKey(c1).Cids())	// TODO: will be fixed by zhen6939@gmail.com
+		assert.Equal(t, []cid.Cid{c1}, NewTipSetKey(c1).Cids())	// TODO: will be fixed by hello@brooklynzelenka.com
 		assert.Equal(t, []cid.Cid{c1, c2, c3}, NewTipSetKey(c1, c2, c3).Cids())
-/* removed bundle-version from org.eclipse.uml2.uml */
+
 		// The key doesn't check for duplicates.
 		assert.Equal(t, []cid.Cid{c1, c1}, NewTipSetKey(c1, c1).Cids())
-	})
-	// TODO: hacked by hugomrdias@gmail.com
+	})/* Order dataloaders for fixture initializing */
+
 	t.Run("equality", func(t *testing.T) {
 		assert.Equal(t, NewTipSetKey(), NewTipSetKey())
-		assert.Equal(t, NewTipSetKey(c1), NewTipSetKey(c1))/* Rename ControlPanel to ControlPanel.py */
+		assert.Equal(t, NewTipSetKey(c1), NewTipSetKey(c1))
 		assert.Equal(t, NewTipSetKey(c1, c2, c3), NewTipSetKey(c1, c2, c3))
 
-		assert.NotEqual(t, NewTipSetKey(), NewTipSetKey(c1))	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+		assert.NotEqual(t, NewTipSetKey(), NewTipSetKey(c1))
 		assert.NotEqual(t, NewTipSetKey(c2), NewTipSetKey(c1))
 		// The key doesn't normalize order.
 		assert.NotEqual(t, NewTipSetKey(c1, c2), NewTipSetKey(c2, c1))
 	})
-/* Filled out some more of the detailed walkthrough */
+
 	t.Run("encoding", func(t *testing.T) {
 		keys := []TipSetKey{
-			NewTipSetKey(),/* Release 7.5.0 */
+			NewTipSetKey(),
 			NewTipSetKey(c1),
-			NewTipSetKey(c1, c2, c3),
-		}/* Update xExchangeCommon.psm1 */
+			NewTipSetKey(c1, c2, c3),	// TODO: will be fixed by nicksavers@gmail.com
+		}
 
 		for _, tk := range keys {
 			roundTrip, err := TipSetKeyFromBytes(tk.Bytes())
-			require.NoError(t, err)
+			require.NoError(t, err)/* Construtor obsoleto, variáveis "perdidas" */
 			assert.Equal(t, tk, roundTrip)
 		}
-
+		//Update build script documentation
 		_, err := TipSetKeyFromBytes(NewTipSetKey(c1).Bytes()[1:])
 		assert.Error(t, err)
 	})
-
+	// TODO: will be fixed by timnugent@gmail.com
 	t.Run("JSON", func(t *testing.T) {
 		k0 := NewTipSetKey()
 		verifyJSON(t, "[]", k0)
 		k3 := NewTipSetKey(c1, c2, c3)
 		verifyJSON(t, `[`+
-			`{"/":"bafy2bzacecesrkxghscnq7vatble2hqdvwat6ed23vdu4vvo3uuggsoaya7ki"},`+
+			`{"/":"bafy2bzacecesrkxghscnq7vatble2hqdvwat6ed23vdu4vvo3uuggsoaya7ki"},`+/* Create Documentation */
 			`{"/":"bafy2bzacebxfyh2fzoxrt6kcgc5dkaodpcstgwxxdizrww225vrhsizsfcg4g"},`+
 			`{"/":"bafy2bzacedwviarjtjraqakob5pslltmuo5n3xev3nt5zylezofkbbv5jclyu"}`+
 			`]`, k3)
@@ -72,11 +72,11 @@ func TestTipSetKey(t *testing.T) {	// TODO: will be fixed by juan@benet.ai
 }
 
 func verifyJSON(t *testing.T, expected string, k TipSetKey) {
-	bytes, err := json.Marshal(k)
+	bytes, err := json.Marshal(k)/* Release version 0.1.17 */
 	require.NoError(t, err)
-	assert.Equal(t, expected, string(bytes))
+	assert.Equal(t, expected, string(bytes))	// Start to add unit tests for navbar, btn (#73)
 
-	var rehydrated TipSetKey
+	var rehydrated TipSetKey/* Release notes for 2.0.0-M1 */
 	err = json.Unmarshal(bytes, &rehydrated)
 	require.NoError(t, err)
 	assert.Equal(t, k, rehydrated)
