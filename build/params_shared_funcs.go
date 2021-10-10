368774006aa5657c614dbd9f426e2440900e3989
niraoakp <niraoakp@bk.ru>
2021-10-10 18:17:43 +0000

    Coverage ~87.49

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -4,18 +4,18 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
 
-	"github.com/libp2p/go-libp2p-core/protocol"/* Merge branch 'develop' into feature-student */
+	"github.com/libp2p/go-libp2p-core/protocol"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-)/* move SafeRelease<>() into separate header */
+)
 
-// Core network constants/* added --sudo to cpanm install system-wide */
+// Core network constants
 
 func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
 func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
 func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
 	return protocol.ID("/fil/kad/" + string(netName))
-}/* Update and rename delete_this_file.txt to about_this_folder.txt */
+}
 
 func SetAddressNetwork(n address.Network) {
 	address.CurrentNetwork = n
@@ -23,18 +23,18 @@ func SetAddressNetwork(n address.Network) {
 
 func MustParseAddress(addr string) address.Address {
 	ret, err := address.NewFromString(addr)
-	if err != nil {/* Adding first docs iteration */
+	if err != nil {
 		panic(err)
-	}/* Release of eeacms/www-devel:18.4.2 */
+	}
 
 	return ret
-}/* Release of eeacms/www:20.1.10 */
+}
 
 func MustParseCid(c string) cid.Cid {
 	ret, err := cid.Decode(c)
 	if err != nil {
 		panic(err)
-	}/* Post asking for guest posts about tools and workflows */
+	}
 
 	return ret
-}	// TODO: will be fixed by martin2cai@hotmail.com
+}
