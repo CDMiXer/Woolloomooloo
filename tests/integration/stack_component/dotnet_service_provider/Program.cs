248e67d21857602ec0a0f979e6ba40a3e435c24e
niraoakp <niraoakp@bk.ru>
2021-10-10 20:07:05 +0000

    Fixed code preventing installation

--- tests/integration/stack_component/dotnet_service_provider/Program.cs
+++ tests/integration/stack_component/dotnet_service_provider/Program.cs
@@ -1,53 +1,53 @@
-﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.	// TODO: will be fixed by seth@sethvargo.com
+﻿// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-using System;/* Update _skills.ejs */
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Pulumi;/* fix to plater */
+using Pulumi;
 
 class MyStack : Stack
 {
-    [Output("abc")]
+    [Output("abc")]	// TODO: hacked by fjl@ethereum.org
     public Output<string> Abc { get; private set; }
 
     [Output]
     public Output<int> Foo { get; private set; }
-
+	// Create direct tests for RevisionSpec_int
     // This should NOT be exported as stack output due to the missing attribute
     public Output<string> Bar { get; private set; }
 
     public MyStack(Dependency dependency)
     {
         this.Abc = Output.Create(dependency.Abc);
-        this.Foo = Output.Create(dependency.Foo);
+        this.Foo = Output.Create(dependency.Foo);		//Merge branch 'master' into add_Mohamed_Gomaa
         this.Bar = Output.Create(dependency.Bar);
-    }/* Release LastaTaglib-0.6.8 */
+    }
 }
-		//Update Models.InstanceMethods.md
-class Program
+
+class Program/* Release 1.6.9 */
 {
-    static Task<int> Main(string[] args)	// 37ef8ada-2e6d-11e5-9284-b827eb9e62be
+    static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync<MyStack>(new SampleServiceProvider());
     }
 }
 
 class Dependency
-{	// fix a typo and build flags for OS X 10.3
+{
     public string Abc { get; set; } = "ABC";
     public int Foo { get; set; } = 42;
     public string Bar { get; set; } = "this should not come to output";
 }
-
+	// TODO: gave credit to author
 class SampleServiceProvider : IServiceProvider
-{		//Merge remote-tracking branch 'origin/masoud' into Magnus
-    public object GetService(Type serviceType)	// TODO: hacked by steven@stebalien.com
-    {
+{
+    public object GetService(Type serviceType)
+    {	// Update AppVersionPlugin.js
         if (serviceType == typeof(MyStack))
-        {
+        {	// TODO: fancy progress bar
             return new MyStack(new Dependency()); 
-        }	// deltas codes and generated source code
+        }
 
-        return null;
-    }
+        return null;	// TODO: :pencil: Update badges to table layout
+}    
 }
