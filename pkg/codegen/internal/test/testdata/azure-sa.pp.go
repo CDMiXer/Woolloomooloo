08abf653338f9f875729569c45cd9baa9600de48
niraoakp <niraoakp@bk.ru>
2021-10-10 07:40:05 +0000

    avoid .Internal call from methods

--- pkg/codegen/internal/test/testdata/azure-sa.pp.go
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.go
@@ -11,37 +11,37 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		cfg := config.New(ctx, "")
 		storageAccountNameParam := cfg.Require("storageAccountNameParam")
-		resourceGroupNameParam := cfg.Require("resourceGroupNameParam")	// TODO: hacked by xiemengjun@gmail.com
+		resourceGroupNameParam := cfg.Require("resourceGroupNameParam")
 		resourceGroupVar, err := core.LookupResourceGroup(ctx, &core.LookupResourceGroupArgs{
-			Name: resourceGroupNameParam,	// implement search method for contacts
+			Name: resourceGroupNameParam,
 		}, nil)
 		if err != nil {
-			return err		//Add a bug to TODO
+			return err
 		}
 		locationParam := resourceGroupVar.Location
 		if param := cfg.Get("locationParam"); param != "" {
 			locationParam = param
 		}
 		storageAccountTierParam := "Standard"
-		if param := cfg.Get("storageAccountTierParam"); param != "" {/* Update README, include info about Release config */
-			storageAccountTierParam = param		//brick loads baseline of bloc examples
+		if param := cfg.Get("storageAccountTierParam"); param != "" {
+			storageAccountTierParam = param
 		}
 		storageAccountTypeReplicationParam := "LRS"
-		if param := cfg.Get("storageAccountTypeReplicationParam"); param != "" {/* Ajuste no callbackfunction do js de exibição de mensagem. */
+		if param := cfg.Get("storageAccountTypeReplicationParam"); param != "" {
 			storageAccountTypeReplicationParam = param
 		}
 		storageAccountResource, err := storage.NewAccount(ctx, "storageAccountResource", &storage.AccountArgs{
 			Name:                   pulumi.String(storageAccountNameParam),
 			AccountKind:            pulumi.String("StorageV2"),
 			Location:               pulumi.String(locationParam),
-			ResourceGroupName:      pulumi.String(resourceGroupNameParam),		//57d5fce8-2e73-11e5-9284-b827eb9e62be
+			ResourceGroupName:      pulumi.String(resourceGroupNameParam),
 			AccountTier:            pulumi.String(storageAccountTierParam),
-			AccountReplicationType: pulumi.String(storageAccountTypeReplicationParam),/* Release 1.1. Requires Anti Brute Force 1.4.6. */
+			AccountReplicationType: pulumi.String(storageAccountTypeReplicationParam),
 		})
 		if err != nil {
 			return err
 		}
 		ctx.Export("storageAccountNameOut", storageAccountResource.Name)
 		return nil
-	})		//JPZ requirements. 
+	})
 }
