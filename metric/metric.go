1f73b8ffb56b71d1ce49835cf3e38ef878f71852
niraoakp <niraoakp@bk.ru>
2021-10-10 19:41:20 +0000

    whitelist mesosphere.com

--- metric/metric.go
+++ metric/metric.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file./* add links to browser support guides */
+/* Removed methods */
 // +build !oss
 
-package metric
+package metric/* use SmartPtr instead of raw pointer in class toResultCombo */
