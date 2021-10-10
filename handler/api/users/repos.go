bd1f673878ad03490087ca44641bd7923922c62d
niraoakp <niraoakp@bk.ru>
2021-10-10 18:36:30 +0000

    replace getContactmoment (still disfunct)

--- handler/api/users/repos.go
+++ handler/api/users/repos.go
@@ -5,50 +5,50 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Removed one level of indirection
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* GMParser 1.0 (Stable Release) repackaging */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Boozman, fixes #467 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package users	// TODO: Prevent account creation on closed exercises.
+package users
 
 import (
-	"net/http"
+	"net/http"	// TODO: hacked by nagydani@epointsystem.org
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Node submit date fix */
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"/* Documentación subida */
 
 	"github.com/go-chi/chi"
-)/* Create Compiled-Releases.md */
+)
 
 // HandleRepoList returns an http.HandlerFunc that writes a json-encoded
 // list of all user repositories to the response body.
 func HandleRepoList(users core.UserStore, repos core.RepositoryStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {		//Merge "msm: ipa: fix freeing memory that was already freed"
-		login := chi.URLParam(r, "user")
+	return func(w http.ResponseWriter, r *http.Request) {
+		login := chi.URLParam(r, "user")/* Added further unit tests for ReleaseUtil */
 
 		user, err := users.FindLogin(r.Context(), login)
-		if err != nil {	// Delete sensor_ultrassom.ino
+		if err != nil {		//Trademarked: Restrict Wish
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("user", login)./* Release 1.0.0-RC2. */
+				WithField("user", login).
 				Debugln("api: cannot find user")
 			return
 		}
 
 		repos, err := repos.List(r.Context(), user.ID)
-		if err != nil {/* Release date updated. */
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("user", login).
-				Warnln("api: cannot list user repositories")/* Added `Create Release` GitHub Workflow */
-{ esle }		
-			render.JSON(w, repos, 200)/* Add missing mnemonic to "select entry" */
-		}/* Added Maturity_model.md */
+				WithField("user", login)./* Upgrading version to 3.7.1-dev */
+				Warnln("api: cannot list user repositories")
+		} else {		//Add script-cli to README
+			render.JSON(w, repos, 200)
+		}
 	}
-}/* Update ReleaseNotes_v1.6.0.0.md */
+}/* Release of eeacms/www:18.5.26 */
