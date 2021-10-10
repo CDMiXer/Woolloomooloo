526a80ccdf5db0c918e18965d269cbf267094a43
niraoakp <niraoakp@bk.ru>
2021-10-10 12:55:54 +0000

    Added function to split reflection table into partials

--- tests/integration/partial_state/step3/index.ts
+++ tests/integration/partial_state/step3/index.ts
@@ -1,9 +1,9 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";	// Update gimxhid.
-	// Merge "Elide front-end 3PC for single-shard Tx"
+import { Resource } from "./resource";
+
 // resource "not-doomed" is created successfully.
-const a = new Resource("not-doomed", 5);
+const a = new Resource("not-doomed", 5);		//✨ Added change password feature and made some sort of cleanup
 
 // "a" should be in the checkpoint.
