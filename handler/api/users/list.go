79b37c4f5d19498cf4e2b3991b713b284f5340e6
niraoakp <niraoakp@bk.ru>
2021-10-10 01:16:03 +0000

    2c076752-2e58-11e5-9284-b827eb9e62be

--- handler/api/users/list.go
+++ handler/api/users/list.go
@@ -3,16 +3,16 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Renamed SHA to SHA-256 */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by indexxuan@gmail.com
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Fix margin issue on mobile nav
-// See the License for the specific language governing permissions and/* Built XSpec 0.4.0 Release Candidate 1. */
-// limitations under the License.	// TODO: will be fixed by cory@protocol.ai
-/* Adding Nucleic Acids Research publication to README */
-package users	// TODO: placeholder handling improved
+// Unless required by applicable law or agreed to in writing, software/* Initiale Release */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package users/* Fixed settings. Release candidate. */
 
 import (
 	"net/http"
@@ -20,19 +20,19 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-)
-/* Update for Release 8.1 */
+)/* Update Graylog-MWG-Contentpack.json */
+/* Merge "Release 3.2.3.283 prima WLAN Driver" */
 // HandleList returns an http.HandlerFunc that writes a json-encoded
 // list of all registered system users to the response body.
 func HandleList(users core.UserStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {	// TODO: Changed loading of overlays to UIList-based.
-		users, err := users.List(r.Context())	// smile-please
+	return func(w http.ResponseWriter, r *http.Request) {
+		users, err := users.List(r.Context())
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot list users")
-		} else {/* Update upgrade.php */
+		} else {
 			render.JSON(w, users, 200)
-		}/* a999aafa-2e70-11e5-9284-b827eb9e62be */
+		}
 	}
-}
+}/* Merge branch 'master' into undelete-button-add */
