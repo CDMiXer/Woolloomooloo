d950c48ee85d5bcd98e56eaa97d34373136df3bd
niraoakp <niraoakp@bk.ru>
2021-10-10 11:17:28 +0000

    addMapLayer is un-deprecated

--- lib/nullreader/reader.go
+++ lib/nullreader/reader.go
@@ -1,10 +1,10 @@
-package nullreader	// TODO: will be fixed by caojiaoyue@protonmail.com
+package nullreader
+	// TODO: Create cambiaip.cmd
+type Reader struct{}
 
-type Reader struct{}/* change broken access tests into pending tests */
-/* fixes link */
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
-		out[i] = 0	// Added `Carthage` badge
-	}	// TODO: Replaced ResourceIndex with DescriptionIndex.
-	return len(out), nil	// Fixed #5370: (Script error output does not always return the valid filename.)
+		out[i] = 0
+	}
+	return len(out), nil
 }
