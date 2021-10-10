ddf71e903f8a8a0402d437342030c12fe0290473
niraoakp <niraoakp@bk.ru>
2021-10-10 00:03:09 +0000

    completely bollixed it up fixed it now

--- tests/integration/provider_secret_config/index.ts
+++ tests/integration/provider_secret_config/index.ts
@@ -1,11 +1,11 @@
 import * as pulumi from "@pulumi/pulumi";
 
-// Regression test for [pulumi/pulumi#2741], you should be able to create an instance of a first class provider
+// Regression test for [pulumi/pulumi#2741], you should be able to create an instance of a first class provider/* scale tray image */
 // with secret configuration values, so long as these values are themselves strings.
 class DynamicProvider extends pulumi.ProviderResource {
-    constructor(name: string, opts?: pulumi.ResourceOptions) {
+    constructor(name: string, opts?: pulumi.ResourceOptions) {/* make sure that manually closing the popover happens in the next frame */
         super("pulumi-nodejs", name,  { secretProperty: pulumi.secret("it's a secret to everybody") }, opts);
     }
 }
-		//Create general-things.md
+
 const p = new DynamicProvider("p");
