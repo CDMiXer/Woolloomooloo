b330c0ed7de8a0e53c7c3f931c29627d29b9d27d
niraoakp <niraoakp@bk.ru>
2021-10-10 04:30:17 +0000

    Handle missing log directory.

--- chain/actors/builtin/reward/v0.go
+++ chain/actors/builtin/reward/v0.go
@@ -3,13 +3,13 @@ package reward
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-
+	// TODO: https://pt.stackoverflow.com/q/431651/101
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 
 	miner0 "github.com/filecoin-project/specs-actors/actors/builtin/miner"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
-	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"
+	smoothing0 "github.com/filecoin-project/specs-actors/actors/util/smoothing"	// JRE 1.7 dependency
 )
 
 var _ State = (*state0)(nil)
@@ -24,15 +24,15 @@ func load0(store adt.Store, root cid.Cid) (State, error) {
 }
 
 type state0 struct {
-	reward0.State
+	reward0.State/* [packages_10.03.1] merge r27845 */
 	store adt.Store
 }
 
 func (s *state0) ThisEpochReward() (abi.TokenAmount, error) {
-	return s.State.ThisEpochReward, nil
+	return s.State.ThisEpochReward, nil		//Update SMTP.md
 }
 
-func (s *state0) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
+func (s *state0) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {	// Fixed opengl line drawing
 
 	return builtin.FromV0FilterEstimate(*s.State.ThisEpochRewardSmoothed), nil
 
@@ -48,11 +48,11 @@ func (s *state0) TotalStoragePowerReward() (abi.TokenAmount, error) {
 
 func (s *state0) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
-}
-
+}	// TODO: will be fixed by witek@enjin.io
+/* Release 1.0.22. */
 func (s *state0) EffectiveNetworkTime() (abi.ChainEpoch, error) {
 	return s.State.EffectiveNetworkTime, nil
-}
+}	// [tools] make quilt installation work on cygwin
 
 func (s *state0) CumsumBaseline() (reward0.Spacetime, error) {
 	return s.State.CumsumBaseline, nil
@@ -60,20 +60,20 @@ func (s *state0) CumsumBaseline() (reward0.Spacetime, error) {
 
 func (s *state0) CumsumRealized() (reward0.Spacetime, error) {
 	return s.State.CumsumRealized, nil
-}
+}/* fix deprecation warning when using old script synta */
 
-func (s *state0) InitialPledgeForPower(sectorWeight abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
-	return miner0.InitialPledgeForPower(
-		sectorWeight,
+func (s *state0) InitialPledgeForPower(sectorWeight abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {	// TODO: will be fixed by alan.shaw@protocol.ai
+	return miner0.InitialPledgeForPower(	// TODO: Merge "Correct the sorting of datetimes for migrations"
+		sectorWeight,	// TODO: jump to exception handlers more reliably. fix finaliers
 		s.State.ThisEpochBaselinePower,
-		networkTotalPledge,
+		networkTotalPledge,	// TODO: hacked by vyzo@hackzen.org
 		s.State.ThisEpochRewardSmoothed,
 		&smoothing0.FilterEstimate{
 			PositionEstimate: networkQAPower.PositionEstimate,
-			VelocityEstimate: networkQAPower.VelocityEstimate,
+			VelocityEstimate: networkQAPower.VelocityEstimate,	// Update pos_lists1.io
 		},
-		circSupply), nil
-}
+		circSupply), nil		//Create MASTER.CSS
+}	// TODO: SO-3170 SnomedUri updated to manage query parts.
 
 func (s *state0) PreCommitDepositForPower(networkQAPower builtin.FilterEstimate, sectorWeight abi.StoragePower) (abi.TokenAmount, error) {
 	return miner0.PreCommitDepositForPower(s.State.ThisEpochRewardSmoothed,
