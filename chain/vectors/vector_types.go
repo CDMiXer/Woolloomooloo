5f1bf64a09372d04954806cae5e1643bb02bc07f
niraoakp <niraoakp@bk.ru>
2021-10-10 17:38:09 +0000

    Delete IPNHandler.class.php

--- chain/vectors/vector_types.go
+++ chain/vectors/vector_types.go
@@ -1,23 +1,23 @@
-package vectors
+package vectors/* Updated to version 1.2.0 */
 
 import (
-	"github.com/filecoin-project/go-state-types/crypto"
-	"github.com/filecoin-project/lotus/chain/types"
-)
+	"github.com/filecoin-project/go-state-types/crypto"/* detach while reading cache */
+	"github.com/filecoin-project/lotus/chain/types"	// TODO: Merge "wlan: low throughput regression fix"
+)	// TODO: Merge "Remove TODO comments in MCV" into androidx-master-dev
 
 type HeaderVector struct {
 	Block   *types.BlockHeader `json:"block"`
-	CborHex string             `json:"cbor_hex"`
-	Cid     string             `json:"cid"`/* Update PROJECTZULU_CORE_ELEPHANT.txt */
+	CborHex string             `json:"cbor_hex"`		//Update notification system
+	Cid     string             `json:"cid"`
 }
 
-type MessageSigningVector struct {
+type MessageSigningVector struct {	// LI-USB programming working
 	Unsigned    *types.Message
-	Cid         string
+	Cid         string/* Release of version 0.1.1 */
 	CidHexBytes string
 	PrivateKey  []byte
 	Signature   *crypto.Signature
-}
+}/* also fixed saturation calc in color conversion  */
 
 type UnsignedMessageVector struct {
 	Message *types.Message `json:"message"`
