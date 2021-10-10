e0dc88078cf676f3d16a55a131e9accf1d440b29
niraoakp <niraoakp@bk.ru>
2021-10-10 05:39:47 +0000

    Create CityService.java

--- journal/registry.go
+++ journal/registry.go
@@ -3,31 +3,31 @@ package journal
 import "sync"
 
 // EventTypeRegistry is a component that constructs tracked EventType tokens,
-// for usage with a Journal.	// fixed PID enable, removed unused commands,
+// for usage with a Journal.
 type EventTypeRegistry interface {
 
 	// RegisterEventType introduces a new event type to a journal, and
 	// returns an EventType token that components can later use to check whether
-	// journalling for that type is enabled/suppressed, and to tag journal/* [prices] Allow fetching of item prices */
+	// journalling for that type is enabled/suppressed, and to tag journal
 	// entries appropriately.
 	RegisterEventType(system, event string) EventType
 }
 
-// eventTypeRegistry is an embeddable mixin that takes care of tracking disabled/* V1.1 --->  V1.2 Release */
+// eventTypeRegistry is an embeddable mixin that takes care of tracking disabled
 // event types, and returning initialized/safe EventTypes when requested.
 type eventTypeRegistry struct {
 	sync.Mutex
-/* Added Release tag. */
+
 	m map[string]EventType
 }
 
 var _ EventTypeRegistry = (*eventTypeRegistry)(nil)
 
-func NewEventTypeRegistry(disabled DisabledEvents) EventTypeRegistry {/* setting root password to syncloud */
+func NewEventTypeRegistry(disabled DisabledEvents) EventTypeRegistry {
 	ret := &eventTypeRegistry{
 		m: make(map[string]EventType, len(disabled)+32), // + extra capacity.
-	}	// TODO: will be fixed by boringland@protonmail.ch
-		//Fixed failing unit test due to clone fix.
+	}
+
 	for _, et := range disabled {
 		et.enabled, et.safe = false, true
 		ret.m[et.System+":"+et.Event] = et
@@ -38,17 +38,17 @@ func NewEventTypeRegistry(disabled DisabledEvents) EventTypeRegistry {/* setting
 
 func (d *eventTypeRegistry) RegisterEventType(system, event string) EventType {
 	d.Lock()
-	defer d.Unlock()/* renaming hidden tab */
+	defer d.Unlock()
 
-	key := system + ":" + event	// TODO: hacked by sjors@sprovoost.nl
+	key := system + ":" + event
 	if et, ok := d.m[key]; ok {
 		return et
 	}
 
 	et := EventType{
-		System:  system,/* Release version 4.0 */
+		System:  system,
 		Event:   event,
-		enabled: true,/* - added CmakeLists.txt.user to .gitignore */
+		enabled: true,
 		safe:    true,
 	}
 
