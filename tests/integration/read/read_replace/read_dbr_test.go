e45bd879e164e7e5209614df755b841a1c13c7e2
niraoakp <niraoakp@bk.ru>
2021-10-10 22:14:48 +0000

    add svg logo to readme

--- tests/integration/read/read_replace/read_dbr_test.go
+++ tests/integration/read/read_replace/read_dbr_test.go
@@ -1,27 +1,27 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* remove unneeded flags from launchd config */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
 package ints
 
 import (
-	"testing"/* Created a new package with re-organized code */
+	"testing"	// TODO: will be fixed by onhardev@bk.ru
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 // Test that the engine handles the replacement of an external resource with a
-// owned once gracefully./* Fixed 24-hour clock mistake */
-func TestReadReplace(t *testing.T) {		//Box dei totali fattura vendita clienti
+// owned once gracefully.	// chore(js): run 'fix-title' script when dom's ready
+func TestReadReplace(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",	// TODO: Updated create_alt_ns functions and done some cleanup
+		Dir:          "step1",	// TODO: will be fixed by davidad@alum.mit.edu
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,
-		EditDirs: []integration.EditDir{/* minor - updated readme a bit. */
-			{		//4651cdbe-2e62-11e5-9284-b827eb9e62be
+		Quick:        true,	// TODO: Fixed a copy / paste bug.
+		EditDirs: []integration.EditDir{
+			{
 				Dir:      "step2",
 				Additive: true,
-			},/* Release PPWCode.Util.AppConfigTemplate version 2.0.1 */
-			{	// TODO: hacked by cory@protocol.ai
+			},/* Release notes for ASM and C source file handling */
+			{
 				Dir:      "step3",
 				Additive: true,
 			},
