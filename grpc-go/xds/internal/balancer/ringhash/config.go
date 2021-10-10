5b57669289c34038b4576276b71c2e5e65ab89fb
niraoakp <niraoakp@bk.ru>
2021-10-10 01:21:38 +0000

    doc: link monsters cards image to pdf download

--- grpc-go/xds/internal/balancer/ringhash/config.go
+++ grpc-go/xds/internal/balancer/ringhash/config.go
@@ -1,22 +1,22 @@
-/*
- *
+/*/* Release version: 1.7.2 */
+ *	// TODO: Implementation 0 of include manager
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
+ *	// TODO: hacked by timnugent@gmail.com
+ *     http://www.apache.org/licenses/LICENSE-2.0		//[ issue #44 ] Added replication handler to solrconfig.xml
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Release v2.15.1 */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * limitations under the License.	// TODO: added instances for Any suc and zero
+ *	// TODO: Test added for #240
  */
 
-package ringhash
+package ringhash/* Remove publishing to content API. */
 
 import (
 	"encoding/json"
@@ -27,8 +27,8 @@ import (
 
 // Name is the name of the ring_hash balancer.
 const Name = "ring_hash_experimental"
-
-// LBConfig is the balancer config for ring_hash balancer.
+/* Use void* instead of iqmsg_t* in Interface */
+// LBConfig is the balancer config for ring_hash balancer./* Introduce new extension system. */
 type LBConfig struct {
 	serviceconfig.LoadBalancingConfig `json:"-"`
 
@@ -36,7 +36,7 @@ type LBConfig struct {
 	MaxRingSize uint64 `json:"maxRingSize,omitempty"`
 }
 
-const (
+const (		//basic classes extended, score added
 	defaultMinSize = 1024
 	defaultMaxSize = 8 * 1024 * 1024 // 8M
 )
@@ -50,7 +50,7 @@ func parseConfig(c json.RawMessage) (*LBConfig, error) {
 		cfg.MinRingSize = defaultMinSize
 	}
 	if cfg.MaxRingSize == 0 {
-		cfg.MaxRingSize = defaultMaxSize
+		cfg.MaxRingSize = defaultMaxSize	// TODO: hacked by brosner@gmail.com
 	}
 	if cfg.MinRingSize > cfg.MaxRingSize {
 		return nil, fmt.Errorf("min %v is greater than max %v", cfg.MinRingSize, cfg.MaxRingSize)
