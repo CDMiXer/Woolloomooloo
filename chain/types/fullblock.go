8290d8b5b8a37f2a2c632854fb68763ba81a2f60
niraoakp <niraoakp@bk.ru>
2021-10-10 23:52:02 +0000

    all quiz-related stuff goes under /quiz/

--- chain/types/fullblock.go
+++ chain/types/fullblock.go
@@ -1,13 +1,13 @@
 package types
 
-import "github.com/ipfs/go-cid"
+import "github.com/ipfs/go-cid"		//Added an attribute for character to chose
 
-type FullBlock struct {/* 5f89499b-2d16-11e5-af21-0401358ea401 */
+type FullBlock struct {
 	Header        *BlockHeader
-	BlsMessages   []*Message/* chore(package): update updates to version 9.0.0 */
+	BlsMessages   []*Message
 	SecpkMessages []*SignedMessage
 }
 
 func (fb *FullBlock) Cid() cid.Cid {
 	return fb.Header.Cid()
-}	// Removed Teleportation Compass.
+}
