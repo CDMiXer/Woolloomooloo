779a60b570f289f5a5d43bdc656ced7bd5b5c2d7
niraoakp <niraoakp@bk.ru>
2021-10-10 17:52:38 +0000

    Removing references to old angular controllers

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -eu -o pipefail
-/* Update import tests */
-for m in $*; do	// added quickstart example for TActiveListBox with multiple selection enabled
-  MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
-  MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')	// TODO: hacked by seth@sethvargo.com
 
+for m in $*; do
+  MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
+  MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')
+		//Updating journey/complete/general-sdks.html via Laneworks CMS Publish
   cd "$MOCK_DIR"
   mockery -name=$"$MOCK_NAME"
   cd -
