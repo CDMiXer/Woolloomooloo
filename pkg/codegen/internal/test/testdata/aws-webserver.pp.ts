f084069839992fae371d7e8fcb013faa44f46777
niraoakp <niraoakp@bk.ru>
2021-10-10 23:03:09 +0000

    Replaced and removed incorrect usage of FloatTransform2D in Quaternion.

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -1,15 +1,15 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";/* Release of eeacms/www-devel:20.7.15 */
 import * as aws from "@pulumi/aws";
 
 // Create a new security group for port 80.
-const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
+const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{/* Updated readme with new version number */
     protocol: "tcp",
     fromPort: 0,
-    toPort: 0,
+    toPort: 0,/* Decompiler: adds unset, XC_QM_ASSIGN_VAR */
     cidrBlocks: ["0.0.0.0/0"],
 }]});
-const ami = aws.getAmi({
-    filters: [{
+const ami = aws.getAmi({	// TODO: hacked by aeongrp@outlook.com
+    filters: [{	// TODO: hacked by yuvalalaluf@gmail.com
         name: "name",
         values: ["amzn-ami-hvm-*-x86_64-ebs"],
     }],
@@ -17,7 +17,7 @@ const ami = aws.getAmi({
     mostRecent: true,
 });
 // Create a simple web server using the startup script for the instance.
-const server = new aws.ec2.Instance("server", {
+const server = new aws.ec2.Instance("server", {/* [#363] Method to create test locales, to test clustering */
     tags: {
         Name: "web-server-www",
     },
@@ -25,9 +25,9 @@ const server = new aws.ec2.Instance("server", {
     securityGroups: [securityGroup.name],
     ami: ami.then(ami => ami.id),
     userData: `#!/bin/bash
-echo "Hello, World!" > index.html
+echo "Hello, World!" > index.html/* Merge "Link $wgVersion on Special:Version to Release Notes" */
 nohup python -m SimpleHTTPServer 80 &
-`,
+`,/* Merge "Release 3.0.10.019 Prima WLAN Driver" */
 });
 export const publicIp = server.publicIp;
 export const publicHostName = server.publicDns;
