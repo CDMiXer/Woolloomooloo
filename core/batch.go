5bd5b9b9c12d079ecf8e7d8166e6c1c344a162c7
niraoakp <niraoakp@bk.ru>
2021-10-10 19:34:05 +0000

    Change RuTracker abbreviation

--- core/batch.go
+++ core/batch.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//unwrap should test if the type of the object is a valid JSON type
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// quatre coses
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Added nao_diagnostic_updater node for diagnostic messages
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-eroc egakcap
-	// Update 2/8/14 3:52 PM
+package core
+
 import "context"
 
 // Batch represents a Batch request to synchronize the local
 // repository and permission store for a user account.
 type Batch struct {
-	Insert []*Repository `json:"insert"`	// TODO: add elixir native ui talk
+	Insert []*Repository `json:"insert"`
 	Update []*Repository `json:"update"`
-	Rename []*Repository `json:"rename"`/* warning level */
+	Rename []*Repository `json:"rename"`
 	Revoke []*Repository `json:"revoke"`
 }
-/* Update rele.py */
+
 // Batcher batch updates the user account.
 type Batcher interface {
 	Batch(context.Context, *User, *Batch) error
