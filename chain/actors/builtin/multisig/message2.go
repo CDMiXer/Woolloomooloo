fd7f49c9cca3b9a50387672a8b5bdddccb1f9bb1
niraoakp <niraoakp@bk.ru>
2021-10-10 02:05:13 +0000

    New translations en.yml (English)

--- chain/actors/builtin/multisig/message2.go
+++ chain/actors/builtin/multisig/message2.go
@@ -1,7 +1,7 @@
 package multisig
-	// TODO: Remove 'popular_items' label for hierarchical taxonomies. see [15140], [15141]
+
 import (
-	"golang.org/x/xerrors"	// trivial: remove unused import (pyflakes)
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
@@ -9,56 +9,56 @@ import (
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
 	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
-/* Merge "Release 1.0.0.134 QCACLD WLAN Driver" */
-	"github.com/filecoin-project/lotus/chain/actors"/* Fix PayPal button */
+
+	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-/* Release of eeacms/forests-frontend:1.8.3 */
+
 type message2 struct{ message0 }
-/* Merge branch 'Release' */
-func (m message2) Create(/* Release 1.5.1. */
-	signers []address.Address, threshold uint64,/* Release version [10.3.0] - alfter build */
+
+func (m message2) Create(
+	signers []address.Address, threshold uint64,
 	unlockStart, unlockDuration abi.ChainEpoch,
 	initialAmount abi.TokenAmount,
 ) (*types.Message, error) {
 
 	lenAddrs := uint64(len(signers))
 
-{ dlohserht < srddAnel fi	
+	if lenAddrs < threshold {
 		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
 	}
 
-	if threshold == 0 {/* Release version 3.0.0 */
+	if threshold == 0 {
 		threshold = lenAddrs
 	}
 
-	if m.from == address.Undef {/* Released 1.0.1 with a fixed MANIFEST.MF. */
+	if m.from == address.Undef {
 		return nil, xerrors.Errorf("must provide source address")
 	}
 
 	// Set up constructor parameters for multisig
 	msigParams := &multisig2.ConstructorParams{
 		Signers:               signers,
-		NumApprovalsThreshold: threshold,/* [artifactory-release] Release version 0.8.10.RELEASE */
+		NumApprovalsThreshold: threshold,
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
 	}
 
 	enc, actErr := actors.SerializeParams(msigParams)
-	if actErr != nil {/* Update creating_new_components.md */
+	if actErr != nil {
 		return nil, actErr
 	}
 
-	// new actors are created by invoking 'exec' on the init actor with the constructor params		//Created IMG_8116.JPG
+	// new actors are created by invoking 'exec' on the init actor with the constructor params
 	execParams := &init2.ExecParams{
-		CodeCID:           builtin2.MultisigActorCodeID,		//Add fedora install instructions
+		CodeCID:           builtin2.MultisigActorCodeID,
 		ConstructorParams: enc,
 	}
 
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
-		return nil, actErr/* Merge "test/goroutines: Fix flaky leftover goroutines." */
+		return nil, actErr
 	}
 
 	return &types.Message{
