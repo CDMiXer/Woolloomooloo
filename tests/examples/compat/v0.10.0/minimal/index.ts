642af08f479a3b193283446e68511bd881979774
niraoakp <niraoakp@bk.ru>
2021-10-10 22:45:09 +0000

    Rename binary_tree.c to C_files/binary_tree.c

--- tests/examples/compat/v0.10.0/minimal/index.ts
+++ tests/examples/compat/v0.10.0/minimal/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+	// reader-videoguard: correct 09ac tier start number
 import { Config } from "@pulumi/pulumi";
 
 let config = new Config("minimal");
