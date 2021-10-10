8539c77ab6249f0f803ef96b41eebe75367287ab
niraoakp <niraoakp@bk.ru>
2021-10-10 06:29:24 +0000

    Cast bdmplot width and height args to integers for pylab

--- plugin/converter/remote_oss.go
+++ plugin/converter/remote_oss.go
@@ -3,27 +3,27 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* fix crossfade effect */
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release 0.6.0 */
 //
-// Unless required by applicable law or agreed to in writing, software	// Update plusone.py
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Update aa_spieltag_simulation.php
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//Delete WAAEforcestartjob.java
 
 // +build oss
 
-package converter	// Add the uri to the Git command.
+package converter
 
 import (
-	"time"/* Correct diagram definition according to the schema. */
+	"time"/* QtSql: updated to use the macro PBOOL */
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Merge "Remove refresh in Stack update_and_save" */
 )
 
 // Remote returns a conversion service that converts the
 // configuration file using a remote http service.
 func Remote(endpoint, signer, extension string, skipVerify bool, timeout time.Duration) core.ConvertService {
-	return new(noop)		//Using Java 8 based adapters
+	return new(noop)
 }
