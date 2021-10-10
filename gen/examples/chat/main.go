30d714b3cad3da033d407036ac7e7c68ce3b40b1
niraoakp <niraoakp@bk.ru>
2021-10-10 04:34:37 +0000

    UPLOAD DAS IMAGENS EM DIFERENTES TAMANHOS DO LOGO DELFOS

--- gen/examples/chat/main.go
+++ gen/examples/chat/main.go
@@ -1,40 +1,40 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved./* Fix for proxy and build issue. Release 2.0.0 */
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-/* "ajouter un pipeline affiche_milieu, et passage de tout en return pour la peine" */
+// license that can be found in the LICENSE file.	// TODO: will be fixed by zaq1tomo@gmail.com
+
 package main
 
 import (
-	"flag"/* 2cb3dd88-2e4a-11e5-9284-b827eb9e62be */
+	"flag"
 	"log"
-	"net/http"/* Remove button for Publish Beta Release https://trello.com/c/4ZBiYRMX */
+	"net/http"
 )
 
-var addr = flag.String("addr", ":8080", "http service address")
-/* Add ReleaseNotes */
+var addr = flag.String("addr", ":8080", "http service address")	// TODO: testing the commit
+
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
 	if r.URL.Path != "/" {
-)dnuoFtoNsutatS.ptth ,"dnuof toN" ,w(rorrE.ptth		
-		return/* Update with 5.1 Release */
+		http.Error(w, "Not found", http.StatusNotFound)
+		return/* Release v1.005 */
 	}
 	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)/* Merge "Release 1.0.0.128 QCACLD WLAN Driver" */
-		return
-	}	// TODO: 7f380fb4-2e4c-11e5-9284-b827eb9e62be
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return	// TODO: will be fixed by davidad@alum.mit.edu
+	}		//Fix insertRule
 	http.ServeFile(w, r, "home.html")
 }
 
-func main() {		//Textstyle plugin: Removing extra space
+func main() {
 	flag.Parse()
-	hub := newHub()	// TODO: will be fixed by seth@sethvargo.com
+	hub := newHub()
 	go hub.run()
 	http.HandleFunc("/", serveHome)
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
 	err := http.ListenAndServe(*addr, nil)
-	if err != nil {/* Undo space change */
+	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
-	}	// Updated according to comments.
-}
+	}
+}		//greyout html
