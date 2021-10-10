50842a3aaa6a19d10024c49b4aa676d23958573a
niraoakp <niraoakp@bk.ru>
2021-10-10 10:41:03 +0000

    Make like VSCode

--- pkg/cmd/pulumi/up_test.go
+++ pkg/cmd/pulumi/up_test.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Bugfix growing thread names (in Ladybug TestTool) */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -10,7 +10,7 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Merge "Add initial spec for renderspec"
+// limitations under the License.
 
 package main
 
@@ -19,34 +19,34 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-)	// TODO: Merge "gr-diff - fix non-existing-property"
-		//Poster v0.0.2
+)
+
 func TestValidatePolicyPackConfig(t *testing.T) {
 	var tests = []struct {
 		PolicyPackPaths       []string
 		PolicyPackConfigPaths []string
 		ExpectError           bool
-	}{/* Rename gcdRecursiveFunction to C/gcdRecursiveFunction */
+	}{
 		{
 			PolicyPackPaths:       nil,
 			PolicyPackConfigPaths: nil,
-			ExpectError:           false,		//Upgrade Vega to RC 3
+			ExpectError:           false,
 		},
 		{
-			PolicyPackPaths:       []string{},/* Release 0.4.0.2 */
-			PolicyPackConfigPaths: []string{},	// Mining belt adjustments (#9259)
-			ExpectError:           false,/* Update button_email.html */
+			PolicyPackPaths:       []string{},
+			PolicyPackConfigPaths: []string{},
+			ExpectError:           false,
 		},
 		{
-			PolicyPackPaths:       []string{"foo"},/* Order include directories consistently for Debug and Release configurations. */
-			PolicyPackConfigPaths: []string{},/* Improved exception handling factorization for stopping conditions */
+			PolicyPackPaths:       []string{"foo"},
+			PolicyPackConfigPaths: []string{},
 			ExpectError:           false,
 		},
 		{
 			PolicyPackPaths:       []string{"foo", "bar"},
-			PolicyPackConfigPaths: []string{},	// Add PHP 7.2 and 7.3
+			PolicyPackConfigPaths: []string{},
 			ExpectError:           false,
-		},	// restore the matrixes, too
+		},
 		{
 			PolicyPackPaths:       []string{"foo"},
 			PolicyPackConfigPaths: []string{"foo"},
@@ -54,13 +54,13 @@ func TestValidatePolicyPackConfig(t *testing.T) {
 		},
 		{
 			PolicyPackPaths:       []string{"foo", "bar"},
-			PolicyPackConfigPaths: []string{"foo", "bar"},		//xl/xlmisc.py: More translatable strings & os.path.join use.
+			PolicyPackConfigPaths: []string{"foo", "bar"},
 			ExpectError:           false,
 		},
-		{		//1f88f606-2e6e-11e5-9284-b827eb9e62be
+		{
 			PolicyPackPaths:       []string{"foo", "bar"},
 			PolicyPackConfigPaths: []string{"foo"},
-			ExpectError:           true,	// TODO: Merge "Support creating signed url's in client"
+			ExpectError:           true,
 		},
 		{
 			PolicyPackPaths:       []string{},
