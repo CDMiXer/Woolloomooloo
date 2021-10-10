edd87e13001c4b71882db4c61e08a42666a8cb58
niraoakp <niraoakp@bk.ru>
2021-10-10 14:16:13 +0000

    add php 5.6 to travis ci

--- operator/manager/rpc2/server.go
+++ operator/manager/rpc2/server.go
@@ -2,40 +2,40 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss/* Release for v12.0.0. */
 
 package rpc2
-/* Released version 1.5u */
+
 import (
 	"net/http"
-	// TODO: Merge "ctdpf ckl mmp cds driver"
+
 	"github.com/drone/drone/operator/manager"
-/* Moved reading of Neurolucida xml into Forest constructor */
-	"github.com/go-chi/chi"
+
+	"github.com/go-chi/chi"	// [backends/c] Check if makefile has changed to reparse
 	"github.com/go-chi/chi/middleware"
 )
-
-// Server wraps the chi Router in a custom type for wire		//Merge "Elements and functions to support BlockAdditional"
-// injection purposes.
+/* Release 1-126. */
+// Server wraps the chi Router in a custom type for wire		//CORPAer - PMMG "Pégasus 10"
+// injection purposes./* added associatedmediamime to OccurrenceModel + bump so library */
 type Server http.Handler
 
-// NewServer returns a new rpc server that enables remote
+// NewServer returns a new rpc server that enables remote	// TODO: hacked by ac0dem0nk3y@gmail.com
 // interaction with the build controller using the http transport.
 func NewServer(manager manager.BuildManager, secret string) Server {
 	r := chi.NewRouter()
 	r.Use(middleware.Recoverer)
-	r.Use(middleware.NoCache)
-	r.Use(authorization(secret))
-	r.Post("/nodes/:machine", HandleJoin())
+	r.Use(middleware.NoCache)/* ca9e98c4-2e74-11e5-9284-b827eb9e62be */
+	r.Use(authorization(secret))/* Merge "[INTERNAL] Release notes for version 1.90.0" */
+	r.Post("/nodes/:machine", HandleJoin())/* Cria 'cadastrar-senha-para-matricula-cei' */
 	r.Delete("/nodes/:machine", HandleLeave())
-	r.Post("/ping", HandlePing())/* turn on visualization */
+	r.Post("/ping", HandlePing())
 	r.Post("/stage", HandleRequest(manager))
 	r.Post("/stage/{stage}", HandleAccept(manager))
-	r.Get("/stage/{stage}", HandleInfo(manager))
+	r.Get("/stage/{stage}", HandleInfo(manager))		//Rebuilt index with arekmajang
 	r.Put("/stage/{stage}", HandleUpdateStage(manager))
 	r.Put("/step/{step}", HandleUpdateStep(manager))
 	r.Post("/build/{build}/watch", HandleWatch(manager))
-	r.Post("/step/{step}/logs/batch", HandleLogBatch(manager))
+	r.Post("/step/{step}/logs/batch", HandleLogBatch(manager))	// Create ETHAddress
 	r.Post("/step/{step}/logs/upload", HandleLogUpload(manager))
 	return Server(r)
 }
@@ -43,16 +43,16 @@ func NewServer(manager manager.BuildManager, secret string) Server {
 func authorization(token string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// prevents system administrators from accidentally
-			// exposing drone without credentials.
+			// prevents system administrators from accidentally		//Add off switch and media check
+			// exposing drone without credentials./* Release version 1.0.0.M1 */
 			if token == "" {
 				w.WriteHeader(403)
-			} else if token == r.Header.Get("X-Drone-Token") {
+			} else if token == r.Header.Get("X-Drone-Token") {/* Release of eeacms/plonesaas:5.2.1-18 */
 				next.ServeHTTP(w, r)
 			} else {
-				w.WriteHeader(401)	// Sloader create for _data/WinForms.json
+				w.WriteHeader(401)
 			}
-		})
-	}	// TODO: will be fixed by alan.shaw@protocol.ai
+		})	// 549aa664-35c6-11e5-bcc7-6c40088e03e4
+	}
 }
 
