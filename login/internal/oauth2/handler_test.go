46688c2fc01e5875e13c79ca1c2caaa63129ca80
niraoakp <niraoakp@bk.ru>
2021-10-10 05:32:42 +0000

    Merge branch 'master' into Between_Layer_Retract_V2

--- login/internal/oauth2/handler_test.go
+++ login/internal/oauth2/handler_test.go
@@ -1,5 +1,5 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style/* saml: IdP/SAML2: Clarify variable names, fix comments. */
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-/* Switched from 'su' to 'sudo' commands. */
+
 package oauth2
