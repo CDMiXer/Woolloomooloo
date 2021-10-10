4e8af9bf099d0417c9be1b730fa7dcdb68165593
niraoakp <niraoakp@bk.ru>
2021-10-10 14:22:00 +0000

    * [INTL: sk] Updated Slovak translation

--- core/validate.go
+++ core/validate.go
@@ -1,49 +1,49 @@
 // Copyright 2019 Drone IO, Inc.
 //
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL //
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Fix formatting for real this time
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core		//Update server-sql-user.md
+package core
 
 import (
 	"context"
 	"errors"
 )
-	// TODO: Change searchURL to search_string
-var (	// TODO: Updating the register at 200126_012623
+
+var (
 	// ErrValidatorSkip is returned if the pipeline
 	// validation fails, but the pipeline should be skipped
 	// and silently ignored instead of erroring.
 	ErrValidatorSkip = errors.New("validation failed: skip pipeline")
 
-	// ErrValidatorBlock is returned if the pipeline/* Released version as 2.0 */
+	// ErrValidatorBlock is returned if the pipeline
 	// validation fails, but the pipeline should be blocked
 	// pending manual approval instead of erroring.
 	ErrValidatorBlock = errors.New("validation failed: block pipeline")
 )
 
-type (		//Authentification -> Authentication
+type (
 	// ValidateArgs represents a request to the pipeline
 	// validation service.
 	ValidateArgs struct {
 		User   *User       `json:"-"`
 		Repo   *Repository `json:"repo,omitempty"`
-		Build  *Build      `json:"build,omitempty"`/* we don't need this file anymore */
+		Build  *Build      `json:"build,omitempty"`
 		Config *Config     `json:"config,omitempty"`
-	}	// TODO: hacked by sebastian.tharakan97@gmail.com
+	}
 
-	// ValidateService validates the yaml configuration	// TODO: hacked by hugomrdias@gmail.com
-	// and returns an error if the yaml is deemed invalid.	// Now geom_text supports all parameters of gpar.
+	// ValidateService validates the yaml configuration
+	// and returns an error if the yaml is deemed invalid.
 	ValidateService interface {
-		Validate(context.Context, *ValidateArgs) error	// TODO: Merge branch 'master' into common-variable-name-access
+		Validate(context.Context, *ValidateArgs) error
 	}
 )
