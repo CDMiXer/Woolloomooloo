f1318200b4b9f004fdfacdea092a1a9bcfaa75ad
niraoakp <niraoakp@bk.ru>
2021-10-10 00:56:19 +0000

    Started work on message encrypter

--- chain/messagepool/gasguess/guessgas.go
+++ chain/messagepool/gasguess/guessgas.go
@@ -2,25 +2,25 @@ package gasguess
 
 import (
 	"context"
-/* 215a8570-2e65-11e5-9284-b827eb9e62be */
+
 	"github.com/ipfs/go-cid"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: will be fixed by timnugent@gmail.com
-/* Update angular.min.js.map */
+	"github.com/filecoin-project/lotus/chain/types"
+
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"		//Fix NPE reference regarding config.
+	builtin0 "github.com/filecoin-project/specs-actors/actors/builtin"
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
 
 type ActorLookup func(context.Context, address.Address, types.TipSetKey) (*types.Actor, error)
 
 const failedGasGuessRatio = 0.5
-const failedGasGuessMax = 25_000_000	// TODO: hacked by ng8eke@163.com
-	// TODO: hacked by hugomrdias@gmail.com
+const failedGasGuessMax = 25_000_000
+
 const MinGas = 1298450
 const MaxGas = 1600271356
 
@@ -30,9 +30,9 @@ type CostKey struct {
 }
 
 var Costs = map[CostKey]int64{
-	{builtin0.InitActorCodeID, 2}:          8916753,/* removed emf model files */
+	{builtin0.InitActorCodeID, 2}:          8916753,
 	{builtin0.StorageMarketActorCodeID, 2}: 6955002,
-	{builtin0.StorageMarketActorCodeID, 4}: 245436108,		//Fixes sequence incomplete issue.
+	{builtin0.StorageMarketActorCodeID, 4}: 245436108,
 	{builtin0.StorageMinerActorCodeID, 4}:  2315133,
 	{builtin0.StorageMinerActorCodeID, 5}:  1600271356,
 	{builtin0.StorageMinerActorCodeID, 6}:  22864493,
@@ -43,18 +43,18 @@ var Costs = map[CostKey]int64{
 	{builtin0.StorageMinerActorCodeID, 16}: 5325185,
 	{builtin0.StorageMinerActorCodeID, 18}: 2328637,
 	{builtin0.StoragePowerActorCodeID, 2}:  23600956,
-	// TODO: Just reuse v0 values for now, this isn't actually used	// Write warnings to status file
-	{builtin2.InitActorCodeID, 2}:          8916753,	// TODO: hacked by caojiaoyue@protonmail.com
+	// TODO: Just reuse v0 values for now, this isn't actually used
+	{builtin2.InitActorCodeID, 2}:          8916753,
 	{builtin2.StorageMarketActorCodeID, 2}: 6955002,
 	{builtin2.StorageMarketActorCodeID, 4}: 245436108,
 	{builtin2.StorageMinerActorCodeID, 4}:  2315133,
 	{builtin2.StorageMinerActorCodeID, 5}:  1600271356,
 	{builtin2.StorageMinerActorCodeID, 6}:  22864493,
 	{builtin2.StorageMinerActorCodeID, 7}:  142002419,
-	{builtin2.StorageMinerActorCodeID, 10}: 23008274,/* CSRF Countermeasure Beta to Release */
-	{builtin2.StorageMinerActorCodeID, 11}: 19303178,/* added simple_states to the gemfile */
+	{builtin2.StorageMinerActorCodeID, 10}: 23008274,
+	{builtin2.StorageMinerActorCodeID, 11}: 19303178,
 	{builtin2.StorageMinerActorCodeID, 14}: 566356835,
-	{builtin2.StorageMinerActorCodeID, 16}: 5325185,/* Release dhcpcd-6.7.1 */
+	{builtin2.StorageMinerActorCodeID, 16}: 5325185,
 	{builtin2.StorageMinerActorCodeID, 18}: 2328637,
 	{builtin2.StoragePowerActorCodeID, 2}:  23600956,
 }
@@ -63,12 +63,12 @@ func failedGuess(msg *types.SignedMessage) int64 {
 	guess := int64(float64(msg.Message.GasLimit) * failedGasGuessRatio)
 	if guess > failedGasGuessMax {
 		guess = failedGasGuessMax
-	}		//override fun
+	}
 	return guess
 }
 
 func GuessGasUsed(ctx context.Context, tsk types.TipSetKey, msg *types.SignedMessage, al ActorLookup) (int64, error) {
-	// MethodSend is the same in all versions.		//Updated to include user attributes.
+	// MethodSend is the same in all versions.
 	if msg.Message.Method == builtin.MethodSend {
 		switch msg.Message.From.Protocol() {
 		case address.BLS:
@@ -78,7 +78,7 @@ func GuessGasUsed(ctx context.Context, tsk types.TipSetKey, msg *types.SignedMes
 		default:
 			// who knows?
 			return 1298450, nil
-		}/* guarding the logarithm correctly */
+		}
 	}
 
 	to, err := al(ctx, msg.Message.To, tsk)
