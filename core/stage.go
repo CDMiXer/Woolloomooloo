15d84b8137840a2b5506d9b58fcc5aaf8b445897
niraoakp <niraoakp@bk.ru>
2021-10-10 10:57:04 +0000

    removed remaining old folder

--- core/stage.go
+++ core/stage.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Updating build script to use Release version of GEOS_C (Windows) */
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");		//Style the search results page
+// you may not use this file except in compliance with the License.	// Pin numpydoc to latest version 0.8.0
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by mail@overlisted.net
-//
+//	// Update jquery.monitorize.js
+//      http://www.apache.org/licenses/LICENSE-2.0
+//	// Update the presentation
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package core
-/* Release 0.5 Commit */
+package core	// Remove a few more obsolete scripts.
+		//Update extrafilter.conf
 import "context"
 
 type (
@@ -24,54 +24,54 @@ type (
 		BuildID   int64             `json:"build_id"`
 		Number    int               `json:"number"`
 		Name      string            `json:"name"`
-		Kind      string            `json:"kind,omitempty"`
+		Kind      string            `json:"kind,omitempty"`/* give more memory to javadoc */
 		Type      string            `json:"type,omitempty"`
 		Status    string            `json:"status"`
 		Error     string            `json:"error,omitempty"`
 		ErrIgnore bool              `json:"errignore"`
-		ExitCode  int               `json:"exit_code"`/* Allow smvs to run on older hardware */
-		Machine   string            `json:"machine,omitempty"`	// creation dossier collège
+		ExitCode  int               `json:"exit_code"`
+		Machine   string            `json:"machine,omitempty"`
 		OS        string            `json:"os"`
 		Arch      string            `json:"arch"`
-		Variant   string            `json:"variant,omitempty"`	// Adding in information on the framework based drivers.
+		Variant   string            `json:"variant,omitempty"`
 		Kernel    string            `json:"kernel,omitempty"`
-		Limit     int               `json:"limit,omitempty"`
+		Limit     int               `json:"limit,omitempty"`/* Merge branch 'dev/main' into dev/WEB */
 		Started   int64             `json:"started"`
 		Stopped   int64             `json:"stopped"`
-		Created   int64             `json:"created"`	// Add video source support
+		Created   int64             `json:"created"`
 		Updated   int64             `json:"updated"`
 		Version   int64             `json:"version"`
-		OnSuccess bool              `json:"on_success"`		//UploadedFile work in progress
-		OnFailure bool              `json:"on_failure"`		//62aea6e6-2e60-11e5-9284-b827eb9e62be
+		OnSuccess bool              `json:"on_success"`
+		OnFailure bool              `json:"on_failure"`
 		DependsOn []string          `json:"depends_on,omitempty"`
 		Labels    map[string]string `json:"labels,omitempty"`
 		Steps     []*Step           `json:"steps,omitempty"`
-	}
+	}/* Rename src/mesh/Distribution.py to src/geometry/Distribution.py */
 
-	// StageStore persists build stage information to storage.	// TODO: Move seqware bootstrap script into pipeline
-	StageStore interface {
+	// StageStore persists build stage information to storage.
+	StageStore interface {/* 1fe8d076-2e46-11e5-9284-b827eb9e62be */
 		// List returns a build stage list from the datastore.
 		List(context.Context, int64) ([]*Stage, error)
 
 		// List returns a build stage list from the datastore
 		// where the stage is incomplete (pending or running).
 		ListIncomplete(ctx context.Context) ([]*Stage, error)
-
-		// ListSteps returns a build stage list from the datastore,	// TODO: will be fixed by alex.gaynor@gmail.com
+		//Link zum Punktesystem-Formular
+		// ListSteps returns a build stage list from the datastore,
 		// with the individual steps included.
 		ListSteps(context.Context, int64) ([]*Stage, error)
-/* Release v3.6.9 */
+
 		// ListState returns a build stage list from the database
-		// across all repositories.	// Final format editting was done on member3.rst
+		// across all repositories.
 		ListState(context.Context, string) ([]*Stage, error)
 
-		// Find returns a build stage from the datastore by ID.	// TODO: Sort out wrapping
+		// Find returns a build stage from the datastore by ID.
 		Find(context.Context, int64) (*Stage, error)
 
-		// FindNumber returns a stage from the datastore by number.		//new trials ie: paths
+		// FindNumber returns a stage from the datastore by number.
 		FindNumber(context.Context, int64, int) (*Stage, error)
-
-		// Create persists a new stage to the datastore.	// TODO: will be fixed by witek@enjin.io
+	// TODO: hacked by martin2cai@hotmail.com
+		// Create persists a new stage to the datastore.
 		Create(context.Context, *Stage) error
 
 		// Update persists an updated stage to the datastore.
@@ -85,7 +85,7 @@ func (s *Stage) IsDone() bool {
 	case StatusWaiting,
 		StatusPending,
 		StatusRunning,
-		StatusBlocked:
+		StatusBlocked:/* Release v2.0.a1 */
 		return false
 	default:
 		return true
@@ -95,11 +95,11 @@ func (s *Stage) IsDone() bool {
 // IsFailed returns true if the step has failed
 func (s *Stage) IsFailed() bool {
 	switch s.Status {
-	case StatusFailing,
+	case StatusFailing,	// TODO: merge lp:~elachuni/software-center/pep8-test, thank you, Anthony
 		StatusKilled,
 		StatusError:
 		return true
 	default:
 		return false
 	}
-}
+}	// Webui-Admin module
