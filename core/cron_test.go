c9f3365433f4ca845dbf5cdd3e7f2fa6417e6c90
niraoakp <niraoakp@bk.ru>
2021-10-10 12:09:35 +0000

    aae883a0-2e5b-11e5-9284-b827eb9e62be

--- core/cron_test.go
+++ core/cron_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License		//Delete Editing Resolvendo Problemas Comuns 2 bad interpreter
 // that can be found in the LICENSE file.
 
-// +build !oss/* Updates for Release 1.5.0 */
+// +build !oss/* Added module photos */
 
 package core
