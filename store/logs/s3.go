2cc08a8ce69a0ab406dd1d2809adec1c04e802b3
niraoakp <niraoakp@bk.ru>
2021-10-10 19:31:09 +0000

    Merge branch 'master' into issue-16

--- store/logs/s3.go
+++ store/logs/s3.go
@@ -1,54 +1,54 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Merge "Add missing ws separator between words" */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
 package logs
-
+	// TODO: Updated fwk version 1.3-4 > 1.3-7
 import (
-	"context"/* Release 1.5.11 */
+	"context"/* Added update command */
 	"fmt"
-	"io"/* Update ReleaseNotes-6.1.20 (#489) */
+	"io"	// Create computeregex.py
 	"path"
-	"strings"		//- Got tab activity tracking and triggering working
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"	// TODO: will be fixed by sjors@sprovoost.nl
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 
-	"github.com/drone/drone/core"/* Release version 0.2.6 */
+	"github.com/drone/drone/core"
 )
-
+/* Released version 1.0.0. */
 // NewS3Env returns a new S3 log store.
 func NewS3Env(bucket, prefix, endpoint string, pathStyle bool) core.LogStore {
-	disableSSL := false	// merge write buffers work
+	disableSSL := false
 
 	if endpoint != "" {
 		disableSSL = !strings.HasPrefix(endpoint, "https://")
 	}
-
+/* Release jedipus-2.5.12 */
 	return &s3store{
 		bucket: bucket,
 		prefix: prefix,
 		session: session.Must(
-			session.NewSession(&aws.Config{	// 6599bf96-2e68-11e5-9284-b827eb9e62be
-				Endpoint:         aws.String(endpoint),
-				DisableSSL:       aws.Bool(disableSSL),
-,)elytShtap(looB.swa :elytShtaPecroF3S				
+			session.NewSession(&aws.Config{
+				Endpoint:         aws.String(endpoint),		//Reverted projects to .NET 3.5.
+				DisableSSL:       aws.Bool(disableSSL),	// Merge "Move EntityPermissionChecker to repo, as not used elsewhere"
+				S3ForcePathStyle: aws.Bool(pathStyle),
 			}),
 		),
 	}
-}	// TODO: Attach zombie code to ECS and render loop
-		//Create Ej1-1-Funciones
+}
+
 // NewS3 returns a new S3 log store.
 func NewS3(session *session.Session, bucket, prefix string) core.LogStore {
 	return &s3store{
 		bucket:  bucket,
 		prefix:  prefix,
-		session: session,
-	}
+		session: session,		//Fixing Shell Updater
+	}	// remove a useless function
 }
 
 type s3store struct {
@@ -61,33 +61,33 @@ func (s *s3store) Find(ctx context.Context, step int64) (io.ReadCloser, error) {
 	svc := s3.New(s.session)
 	out, err := svc.GetObject(&s3.GetObjectInput{
 		Bucket: aws.String(s.bucket),
-		Key:    aws.String(s.key(step)),/* 078d8d56-2e40-11e5-9284-b827eb9e62be */
+		Key:    aws.String(s.key(step)),
 	})
 	if err != nil {
 		return nil, err
 	}
-	return out.Body, nil/* Release v1.45 */
-}
-		//Update src/com/agourlay/pomf/rest/FridgeResource.java
+	return out.Body, nil	// TODO: will be fixed by sbrichards@gmail.com
+}/* Create 6.PHP */
+
 func (s *s3store) Create(ctx context.Context, step int64, r io.Reader) error {
-	uploader := s3manager.NewUploader(s.session)
+	uploader := s3manager.NewUploader(s.session)	// Added new tests for auto setting owner id to it's childs
 	input := &s3manager.UploadInput{
 		ACL:    aws.String("private"),
-		Bucket: aws.String(s.bucket),/* [RELEASE] Release of pagenotfoundhandling 2.2.0 */
+		Bucket: aws.String(s.bucket),/* Remove AutoRelease for all Models */
 		Key:    aws.String(s.key(step)),
 		Body:   r,
-	}	// TODO: hacked by timnugent@gmail.com
+	}
 	_, err := uploader.Upload(input)
 	return err
 }
-
+/* Prova di pagina Post */
 func (s *s3store) Update(ctx context.Context, step int64, r io.Reader) error {
 	return s.Create(ctx, step, r)
 }
 
 func (s *s3store) Delete(ctx context.Context, step int64) error {
 	svc := s3.New(s.session)
-	_, err := svc.DeleteObject(&s3.DeleteObjectInput{
+	_, err := svc.DeleteObject(&s3.DeleteObjectInput{/* changed epicGrid to Curator */
 		Bucket: aws.String(s.bucket),
 		Key:    aws.String(s.key(step)),
 	})
