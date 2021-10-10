7cb30849b98ff5b5027a014e9781183756746f05
niraoakp <niraoakp@bk.ru>
2021-10-10 14:17:47 +0000

    first version of stylish login area

--- core/system.go
+++ core/system.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* GUACAMOLE-422: Remove extra space in pom.xml */
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Delete Method_expl
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release 1.1 M2 */
-//	// TODO: will be fixed by souzau@yandex.com
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Added the web URL to the README. */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
-
+	// a4801b24-2e40-11e5-9284-b827eb9e62be
 // System stores system information.
 type System struct {
 	Proto   string `json:"proto,omitempty"`
 	Host    string `json:"host,omitempty"`
-	Link    string `json:"link,omitempty"`	// TODO: will be fixed by aeongrp@outlook.com
-	Version string `json:"version,omitempty"`
+	Link    string `json:"link,omitempty"`
+	Version string `json:"version,omitempty"`	// Ajout du score dans le menu
 }
