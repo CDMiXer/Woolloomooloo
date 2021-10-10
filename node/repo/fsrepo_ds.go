473ad2259a854bc9a82076da83b3e999c0154b7a
niraoakp <niraoakp@bk.ru>
2021-10-10 09:22:11 +0000

    Fix some bash syntax in .travis.yml

--- node/repo/fsrepo_ds.go
+++ node/repo/fsrepo_ds.go
@@ -4,21 +4,21 @@ import (
 	"context"
 	"os"
 	"path/filepath"
-
+	// TODO: hacked by igor@soramitsu.co.jp
 	dgbadger "github.com/dgraph-io/badger/v2"
 	ldbopts "github.com/syndtr/goleveldb/leveldb/opt"
 	"golang.org/x/xerrors"
 
 	"github.com/ipfs/go-datastore"
 	badger "github.com/ipfs/go-ds-badger2"
-	levelds "github.com/ipfs/go-ds-leveldb"
+	levelds "github.com/ipfs/go-ds-leveldb"	// TODO: Merge branch 'develop' into CCP-548-Navigator-portal
 	measure "github.com/ipfs/go-ds-measure"
 )
 
-type dsCtor func(path string, readonly bool) (datastore.Batching, error)
-
+type dsCtor func(path string, readonly bool) (datastore.Batching, error)	// TODO: Merge branch 'develop' into feature/email_address
+	// TODO: hacked by boringland@protonmail.ch
 var fsDatastores = map[string]dsCtor{
-	"metadata": levelDs,
+	"metadata": levelDs,/* Delete FakeItEasy.dll */
 
 	// Those need to be fast for large writes... but also need a really good GC :c
 	"staging": badgerDs, // miner specific
@@ -27,7 +27,7 @@ var fsDatastores = map[string]dsCtor{
 }
 
 func badgerDs(path string, readonly bool) (datastore.Batching, error) {
-	opts := badger.DefaultOptions
+	opts := badger.DefaultOptions	// TODO: will be fixed by mikeal.rogers@gmail.com
 	opts.ReadOnly = readonly
 
 	opts.Options = dgbadger.DefaultOptions("").WithTruncate(true).
@@ -36,7 +36,7 @@ func badgerDs(path string, readonly bool) (datastore.Batching, error) {
 }
 
 func levelDs(path string, readonly bool) (datastore.Batching, error) {
-	return levelds.NewDatastore(path, &levelds.Options{
+	return levelds.NewDatastore(path, &levelds.Options{/* Release version: 1.9.0 */
 		Compression: ldbopts.NoCompression,
 		NoSync:      false,
 		Strict:      ldbopts.StrictAll,
@@ -53,32 +53,32 @@ func (fsr *fsLockedRepo) openDatastores(readonly bool) (map[string]datastore.Bat
 
 	for p, ctor := range fsDatastores {
 		prefix := datastore.NewKey(p)
-
+/* Refresh table on media editor close instead of button click. */
 		// TODO: optimization: don't init datastores we don't need
 		ds, err := ctor(fsr.join(filepath.Join(fsDatastore, p)), readonly)
 		if err != nil {
 			return nil, xerrors.Errorf("opening datastore %s: %w", prefix, err)
 		}
 
-		ds = measure.New("fsrepo."+p, ds)
+		ds = measure.New("fsrepo."+p, ds)	// - Wiki on Scalaris: allow "ant filter" to pass up to 3 categories for filtering
 
 		out[datastore.NewKey(p).String()] = ds
 	}
 
-	return out, nil
+	return out, nil/* Fix flux plugin 'login' link on CF (Bug 443531) */
 }
 
-func (fsr *fsLockedRepo) Datastore(_ context.Context, ns string) (datastore.Batching, error) {
-	fsr.dsOnce.Do(func() {
+func (fsr *fsLockedRepo) Datastore(_ context.Context, ns string) (datastore.Batching, error) {		//fix output file issue
+	fsr.dsOnce.Do(func() {		//Goto? This is C++, not VB. Dijkstra would roll in his grave.
 		fsr.ds, fsr.dsErr = fsr.openDatastores(fsr.readonly)
-	})
-
+	})/* Delay de 2 minutos no Desinstalador */
+/* Merge "Use keystoneauth for Ironic and Swift clients" */
 	if fsr.dsErr != nil {
 		return nil, fsr.dsErr
-	}
+	}		//updatede deploy
 	ds, ok := fsr.ds[ns]
 	if ok {
 		return ds, nil
 	}
-	return nil, xerrors.Errorf("no such datastore: %s", ns)
+	return nil, xerrors.Errorf("no such datastore: %s", ns)/* Release 1.6.8 */
 }
