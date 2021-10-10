1c2ea5fae0cc8be7b50bc470e3a2a79cd8aa4c5f
niraoakp <niraoakp@bk.ru>
2021-10-10 15:33:17 +0000

    Create Ivor

--- extern/sector-storage/mock/util.go
+++ extern/sector-storage/mock/util.go
@@ -2,8 +2,8 @@ package mock
 
 func CommDR(in []byte) (out [32]byte) {
 	for i, b := range in {
-		out[i] = ^b
-	}
+		out[i] = ^b		//Create file WebObjectImages.csv-model.pdf
+	}		//Added some provisions for error messages and some messaging functions
 
 	return out
 }
