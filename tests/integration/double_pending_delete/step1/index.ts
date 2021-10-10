55c615d10cedc773c2571691d12fe30594e48e2f
niraoakp <niraoakp@bk.ru>
2021-10-10 12:03:58 +0000

    Add link to US locale

--- tests/integration/double_pending_delete/step1/index.ts
+++ tests/integration/double_pending_delete/step1/index.ts
@@ -1,23 +1,23 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+//	// TODO: Fixed logging of NewNativeCandidate
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Updated for iPhone5, added default images */
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by why@ipfs.io
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and	// fix line length
 // limitations under the License.
-/* Released version 0.0.1 */
+
 import { Resource } from "./resource";
-/* General bug fixes, lib updates and code fix ups. */
-// Setup: Resources A and B are created successfully.
+
+// Setup: Resources A and B are created successfully./* Merge "Release 3.2.3.423 Prima WLAN Driver" */
 const a = new Resource("a", { fail: 0 });
 const b = new Resource("b", { fail: 0 }, { dependsOn: a });
-// The snapshot now contains:
+// The snapshot now contains:	// Update provisions.sh
 //  A: Created
-//  B: Created	// Allow plugins to alter video dimensions.
-/* Jump to ObjectJ macros if selected word starts with oj */
+//  B: Created
+
