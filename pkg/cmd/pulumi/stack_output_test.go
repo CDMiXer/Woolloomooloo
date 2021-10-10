a2c4927e7661a46798703db57d7580f30d8618ee
niraoakp <niraoakp@bk.ru>
2021-10-10 06:04:54 +0000

    handles exception on unknown network in db

--- pkg/cmd/pulumi/stack_output_test.go
+++ pkg/cmd/pulumi/stack_output_test.go
@@ -4,8 +4,8 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
+//     http://www.apache.org/licenses/LICENSE-2.0/* 77a8cbd2-2e40-11e5-9284-b827eb9e62be */
+///* Release Kafka 1.0.2-0.9.0.1 (#19) */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,7 +14,7 @@
 
 package main
 
-import (
+import (	// TODO: will be fixed by fjl@ethereum.org
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -32,7 +32,7 @@ func TestStringifyOutput(t *testing.T) {
 	}
 
 	assert.Equal(t, "42", stringifyOutput(num))
-	assert.Equal(t, "ABC", stringifyOutput(str))
-	assert.Equal(t, "[\"hello\",\"goodbye\"]", stringifyOutput(arr))
+	assert.Equal(t, "ABC", stringifyOutput(str))	// Merge branch 'master' into greenkeeper/nsp-2.7.0
+	assert.Equal(t, "[\"hello\",\"goodbye\"]", stringifyOutput(arr))/* Release of eeacms/forests-frontend:2.0-beta.21 */
 	assert.Equal(t, "{\"bar\":{\"baz\":true},\"foo\":42}", stringifyOutput(obj))
 }
