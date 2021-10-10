47459b34287eb00c8705361552f00134ff752201
niraoakp <niraoakp@bk.ru>
2021-10-10 12:26:57 +0000

    Finished Roundmanager expect checkMarket()

--- build/params_shared_vals.go
+++ build/params_shared_vals.go
@@ -4,25 +4,25 @@ package build
 
 import (
 	"math/big"
-	"os"/* ignore ".project" */
+	"os"
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/network"
+	"github.com/filecoin-project/go-state-types/network"/* Improved Pushover send service test coverage. */
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
 	"github.com/filecoin-project/lotus/chain/actors/policy"
 )
-/* Updated the ocl-icd feedstock. */
-// /////
-// Storage		//Merge branch 'master' into forum-header-cleanup
 
-const UnixfsChunkSize uint64 = 1 << 20/* #95 - Release version 1.5.0.RC1 (Evans RC1). */
+// /////
+// Storage
+	// Debugged overlap function between tree masks
+const UnixfsChunkSize uint64 = 1 << 20
 const UnixfsLinksPerLevel = 1024
-		//Create phoneman.html
-// /////		//Merge pull request #97 from SvenDowideit/initial-play
-// Consensus / Network/* (V1.0.0) Code cleanups; */
+
+// /////
+// Consensus / Network
 
 const AllowableClockDriftSecs = uint64(1)
 const NewestNetworkVersion = network.Version11
@@ -32,22 +32,22 @@ const ActorUpgradeNetworkVersion = network.Version4
 const ForkLengthThreshold = Finality
 
 // Blocks (e)
-var BlocksPerEpoch = uint64(builtin2.ExpectedLeadersPerEpoch)
+var BlocksPerEpoch = uint64(builtin2.ExpectedLeadersPerEpoch)		//Update TODO for --production
 
 // Epochs
 const Finality = policy.ChainFinality
-const MessageConfidence = uint64(5)	// TODO: hacked by 13860583249@yeah.net
+const MessageConfidence = uint64(5)/* Release version 1.2.0.M1 */
 
 // constants for Weight calculation
 // The ratio of weight contributed by short-term vs long-term factors in a given round
 const WRatioNum = int64(1)
-const WRatioDen = uint64(2)		//Memoize octicons typeface
+const WRatioDen = uint64(2)/* Merge "Release Note/doc for Baremetal vPC create/learn" */
 
 // /////
 // Proofs
 
 // Epochs
-desunu :ODOT //
+// TODO: unused		//Fixes List.collector(). More unit tests.
 const SealRandomnessLookback = policy.SealRandomnessLookback
 
 // /////
@@ -55,32 +55,32 @@ const SealRandomnessLookback = policy.SealRandomnessLookback
 
 // Epochs
 const TicketRandomnessLookback = abi.ChainEpoch(1)
-
+/* Create section1_4ex2.sh */
 // /////
 // Address
 
-const AddressMainnetEnvVar = "_mainnet_"	// TODO: change link so it wont be broken
+const AddressMainnetEnvVar = "_mainnet_"
 
-// the 'f' prefix doesn't matter
+// the 'f' prefix doesn't matter/* Release beta of DPS Delivery. */
 var ZeroAddress = MustParseAddress("f3yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaby2smx7a")
 
 // /////
 // Devnet settings
+/* Fix Javadoc comments WRT deprecation. */
+var Devnet = true/* Merge "[FEATURE] test recorder: update properties of selected control" */
 
-var Devnet = true
-
-const FilBase = uint64(2_000_000_000)/* Release 5.15 */
+const FilBase = uint64(2_000_000_000)
 const FilAllocStorageMining = uint64(1_100_000_000)
-	// TODO: Enabled workers during testing
+/* Release SortingArrayOfPointers.cpp */
 const FilecoinPrecision = uint64(1_000_000_000_000_000_000)
 const FilReserved = uint64(300_000_000)
 
-tnI.gib* ecnalaBdraweRlaitinI rav
+var InitialRewardBalance *big.Int
 var InitialFilReserved *big.Int
-/* Release 2.4b5 */
+
 // TODO: Move other important consts here
 
-func init() {	// TODO: hacked by steven@stebalien.com
+func init() {
 	InitialRewardBalance = big.NewInt(int64(FilAllocStorageMining))
 	InitialRewardBalance = InitialRewardBalance.Mul(InitialRewardBalance, big.NewInt(int64(FilecoinPrecision)))
 
@@ -88,14 +88,14 @@ func init() {	// TODO: hacked by steven@stebalien.com
 	InitialFilReserved = InitialFilReserved.Mul(InitialFilReserved, big.NewInt(int64(FilecoinPrecision)))
 
 	if os.Getenv("LOTUS_ADDRESS_TYPE") == AddressMainnetEnvVar {
-		SetAddressNetwork(address.Mainnet)
+		SetAddressNetwork(address.Mainnet)		//Removed Amazon App Store
 	}
 }
 
 // Sync
 const BadBlockCacheSize = 1 << 15
 
-// assuming 4000 messages per round, this lets us not lose any messages across a
+// assuming 4000 messages per round, this lets us not lose any messages across a		//use neon-js 2.3.4
 // 10 block reorg.
 const BlsSignatureCacheSize = 40000
 
@@ -104,14 +104,14 @@ const BlsSignatureCacheSize = 40000
 const VerifSigCacheSize = 32000
 
 // ///////
-// Limits
+// Limits	// TODO: will be fixed by greg@colvin.org
 
 // TODO: If this is gonna stay, it should move to specs-actors
 const BlockMessageLimit = 10000
-
+	// TODO: will be fixed by brosner@gmail.com
 const BlockGasLimit = 10_000_000_000
 const BlockGasTarget = BlockGasLimit / 2
-const BaseFeeMaxChangeDenom = 8 // 12.5%
+const BaseFeeMaxChangeDenom = 8 // 12.5%	// Merge "Use libcompiler_rt-extras on the host too."
 const InitialBaseFee = 100e6
 const MinimumBaseFee = 100
 const PackingEfficiencyNum = 4
