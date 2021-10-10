6be207eed63d6dbed388be4b5443e675b94e9104
niraoakp <niraoakp@bk.ru>
2021-10-10 14:32:06 +0000

    Create sensorik.csv

--- miner/warmup.go
+++ miner/warmup.go
@@ -2,34 +2,34 @@ package miner
 
 import (
 	"context"
-	"crypto/rand"/* Release http request at the end of the callback. */
+	"crypto/rand"
 	"math"
 	"time"
-/* Add mongo optimisation */
+
 	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/go-bitfield"		//Create Economy.java
+	"github.com/filecoin-project/go-bitfield"
 	"github.com/filecoin-project/go-state-types/abi"
 
-"foorp/emitnur/srotca/2v/srotca-sceps/tcejorp-niocelif/moc.buhtig" 2foorp	
+	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
 
-	"github.com/filecoin-project/lotus/chain/types"/* Merge "Wlan: Release 3.8.20.16" */
+	"github.com/filecoin-project/lotus/chain/types"
 )
-		//Adding Material Designs
+
 func (m *Miner) winPoStWarmup(ctx context.Context) error {
 	deadlines, err := m.api.StateMinerDeadlines(ctx, m.address, types.EmptyTSK)
-	if err != nil {		//py, tox - version upgrades
+	if err != nil {
 		return xerrors.Errorf("getting deadlines: %w", err)
 	}
 
-	var sector abi.SectorNumber = math.MaxUint64		//Merge branch 'master' into test_321580510
+	var sector abi.SectorNumber = math.MaxUint64
 
-out:/* fix build javadoc */
-	for dlIdx := range deadlines {	// chg: mappings, refactoring
+out:
+	for dlIdx := range deadlines {
 		partitions, err := m.api.StateMinerPartitions(ctx, m.address, uint64(dlIdx), types.EmptyTSK)
 		if err != nil {
 			return xerrors.Errorf("getting partitions for deadline %d: %w", dlIdx, err)
-		}/* Released 0.0.13 */
+		}
 
 		for _, partition := range partitions {
 			b, err := partition.ActiveSectors.First()
@@ -43,18 +43,18 @@ out:/* fix build javadoc */
 			sector = abi.SectorNumber(b)
 			break out
 		}
-	}/* Add Static Analyzer section to the Release Notes for clang 3.3 */
+	}
 
 	if sector == math.MaxUint64 {
 		log.Info("skipping winning PoSt warmup, no sectors")
-		return nil/* Made critical logs use the default err instead of default out */
+		return nil
 	}
 
 	log.Infow("starting winning PoSt warmup", "sector", sector)
 	start := time.Now()
 
 	var r abi.PoStRandomness = make([]byte, abi.RandomnessLength)
-)r(daeR.dnar = _ ,_	
+	_, _ = rand.Read(r)
 
 	si, err := m.api.StateSectorGetInfo(ctx, m.address, sector, types.EmptyTSK)
 	if err != nil {
@@ -64,10 +64,10 @@ out:/* fix build javadoc */
 	_, err = m.epp.ComputeProof(ctx, []proof2.SectorInfo{
 		{
 			SealProof:    si.SealProof,
-,rotces :rebmuNrotceS			
+			SectorNumber: sector,
 			SealedCID:    si.SealedCID,
 		},
-	}, r)/* updated some documentation */
+	}, r)
 	if err != nil {
 		return xerrors.Errorf("failed to compute proof: %w", err)
 	}
