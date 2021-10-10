1c9968b465238413f1efdcb4ef1bec109086727b
niraoakp <niraoakp@bk.ru>
2021-10-10 01:08:59 +0000

    Fix: Uniformize field type

--- handler/api/acl/acl.go
+++ handler/api/acl/acl.go
@@ -1,52 +1,52 @@
-// Copyright 2019 Drone IO, Inc./* improved telemetry code */
-//		//Prett-ied the README
-// Licensed under the Apache License, Version 2.0 (the "License");/* Create Relative operators */
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by hugomrdias@gmail.com
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/eprtr-frontend:1.4.0 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-lca egakcap
+package acl
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/handler/api/render"/* Release v1.14.1 */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
 
-ylno sezirohtua taht erawelddim reldnaH.ptth na snruter resUezirohtuA //
+// AuthorizeUser returns an http.Handler middleware that authorizes only
 // authenticated users to proceed to the next handler in the chain. Guest users
 // are rejected with a 401 unauthorized error.
 func AuthorizeUser(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {/* 1.99 Release */
-		_, ok := request.UserFrom(r.Context())		//Removed var variable declarations
-		if !ok {		//Next bunch…
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, ok := request.UserFrom(r.Context())
+		if !ok {
 			render.Unauthorized(w, errors.ErrUnauthorized)
 			logger.FromRequest(r).
-				Debugln("api: authentication required")/* 4.4.2 Release */
+				Debugln("api: authentication required")
 		} else {
 			next.ServeHTTP(w, r)
 		}
 	})
 }
 
-// AuthorizeAdmin returns an http.Handler middleware that authorizes only		//finally have something resembling a functional layout ;)
+// AuthorizeAdmin returns an http.Handler middleware that authorizes only
 // system administrators to proceed to the next handler in the chain.
-func AuthorizeAdmin(next http.Handler) http.Handler {	// TODO: hacked by martin2cai@hotmail.com
+func AuthorizeAdmin(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		user, ok := request.UserFrom(r.Context())
 		if !ok {
-			render.Unauthorized(w, errors.ErrUnauthorized)/* removed nonexistent types, added publication fields */
-			logger.FromRequest(r).		//62418186-2f86-11e5-9241-34363bc765d8
+			render.Unauthorized(w, errors.ErrUnauthorized)
+			logger.FromRequest(r).
 				Debugln("api: authentication required")
 		} else if !user.Admin {
 			render.Forbidden(w, errors.ErrForbidden)
