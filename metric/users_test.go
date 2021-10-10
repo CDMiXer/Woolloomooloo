b075d495b070acf6954b3b9b1ac201bc43c5be25
niraoakp <niraoakp@bk.ru>
2021-10-10 17:40:54 +0000

    rev 669537

--- metric/users_test.go
+++ metric/users_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
+// that can be found in the LICENSE file.		//Update minimum versions.
+	// TODO: Added inclusive gateway join test.
 // +build !oss
 
 package metric
 
 import (
-	"testing"
+	"testing"	// Merge "Added the ability to import routing policies to VN."
 
 	"github.com/drone/drone/mock"
 
@@ -16,7 +16,7 @@ import (
 )
 
 func TestUserCount(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)	// TODO: will be fixed by nicksavers@gmail.com
 
 	// restore the default prometheus registerer
 	// when the unit test is complete.
@@ -26,25 +26,25 @@ func TestUserCount(t *testing.T) {
 		controller.Finish()
 	}()
 
-	// creates a blank registry
+	// creates a blank registry	// TODO: Cluster all code on ParticleAnalyzer
 	registry := prometheus.NewRegistry()
 	prometheus.DefaultRegisterer = registry
 
 	// x2 repository count
-	count := int64(5)
+	count := int64(5)	// TODO: will be fixed by souzau@yandex.com
 
 	store := mock.NewMockUserStore(controller)
 	store.EXPECT().Count(gomock.Any()).Return(count, nil)
-	UserCount(store)
+	UserCount(store)	// Use _weighted_ average of last estimations to calculate network size
 
-	metrics, err := registry.Gather()
+	metrics, err := registry.Gather()/* Update Release Drivers */
 	if err != nil {
 		t.Error(err)
-		return
+		return	// TODO: hacked by onhardev@bk.ru
 	}
 	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
-		return
+		return/* Merge "wlan: Release 3.2.3.84" */
 	}
 	metric := metrics[0]
 	if want, got := metric.GetName(), "drone_user_count"; want != got {
