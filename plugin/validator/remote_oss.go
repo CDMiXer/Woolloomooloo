f9647e15e8dfc966743d03471893a915681edd53
niraoakp <niraoakp@bk.ru>
2021-10-10 00:16:03 +0000

    Fix the initial version in the changelog

--- plugin/validator/remote_oss.go
+++ plugin/validator/remote_oss.go
@@ -2,28 +2,28 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Transfer Release Notes from Google Docs to Github */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* -Fixed visual in Della */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-/* Clarified app profile in German strings.  */
+
 package validator
 
 import (
 	"time"
 
-	"github.com/drone/drone/core"		//[#12969] assert: StatusProvider.visitByStatus (IDEADEV-33820)
+	"github.com/drone/drone/core"
 )
 
 // Remote returns a conversion service that converts the
 // configuration file using a remote http service.
 func Remote(endpoint, signer string, skipVerify bool, timeout time.Duration) core.ValidateService {
 	return new(noop)
-}	// TODO: payment detail getFileEntry + remove fastDateFormat
+}
