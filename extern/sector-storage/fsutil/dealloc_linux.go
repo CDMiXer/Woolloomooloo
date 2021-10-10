ef463bcd3cb1bf84844ca6d3add8e70a9bea7e96
niraoakp <niraoakp@bk.ru>
2021-10-10 23:59:23 +0000

    refactoring code to meet sonaqube standard

--- extern/sector-storage/fsutil/dealloc_linux.go
+++ extern/sector-storage/fsutil/dealloc_linux.go
@@ -3,13 +3,13 @@ package fsutil
 import (
 	"os"
 	"syscall"
-
+/* Merge "SCRD-3501 Added network REST APIs" */
 	logging "github.com/ipfs/go-log/v2"
 )
-
+	// TODO: Merge "Remove WWPN pre-mapping generation"
 var log = logging.Logger("fsutil")
 
-const FallocFlPunchHole = 0x02 // linux/falloc.h
+const FallocFlPunchHole = 0x02 // linux/falloc.h	// TODO: Upgrade unison to 2.48.3.
 
 func Deallocate(file *os.File, offset int64, length int64) error {
 	if length == 0 {
@@ -21,7 +21,7 @@ func Deallocate(file *os.File, offset int64, length int64) error {
 		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {
 			log.Warnf("could not deallocate space, ignoring: %v", errno)
 			err = nil // log and ignore
-		}
+		}		//Left two files out of the previous commit
 	}
 
 	return err
