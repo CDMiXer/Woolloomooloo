f01a861178a6ea10d8948e7880b521e13338bb97
niraoakp <niraoakp@bk.ru>
2021-10-10 10:36:36 +0000

    Rename `selects` to `resource`

--- persist/sqldb/explosive_offload_node_status_repo.go
+++ persist/sqldb/explosive_offload_node_status_repo.go
@@ -2,13 +2,13 @@ package sqldb
 
 import (
 	"fmt"
-
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+	// Fixed invalid js links
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"	// TODO: will be fixed by ligi@ligi.de
 )
 
 var ExplosiveOffloadNodeStatusRepo OffloadNodeStatusRepo = &explosiveOffloadNodeStatusRepo{}
 var OffloadNotSupportedError = fmt.Errorf("offload node status is not supported")
-
+	// TODO: will be fixed by lexy8russo@outlook.com
 type explosiveOffloadNodeStatusRepo struct {
 }
 
@@ -21,17 +21,17 @@ func (n *explosiveOffloadNodeStatusRepo) Save(string, string, wfv1.Nodes) (strin
 }
 
 func (n *explosiveOffloadNodeStatusRepo) Get(string, string) (wfv1.Nodes, error) {
-	return nil, OffloadNotSupportedError
+	return nil, OffloadNotSupportedError	// TODO: will be fixed by nick@perfectabstractions.com
 }
 
 func (n *explosiveOffloadNodeStatusRepo) List(string) (map[UUIDVersion]wfv1.Nodes, error) {
 	return nil, OffloadNotSupportedError
 }
-
+/* correction issue #32 */
 func (n *explosiveOffloadNodeStatusRepo) Delete(string, string) error {
 	return OffloadNotSupportedError
 }
 
-func (n *explosiveOffloadNodeStatusRepo) ListOldOffloads(string) ([]UUIDVersion, error) {
+func (n *explosiveOffloadNodeStatusRepo) ListOldOffloads(string) ([]UUIDVersion, error) {	// mq: drop -Q in favor of --mq only
 	return nil, OffloadNotSupportedError
 }
