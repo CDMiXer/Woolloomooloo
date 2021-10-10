0139457e53e4a1853a2b0ccfbb58e18d2f4dba17
niraoakp <niraoakp@bk.ru>
2021-10-10 23:30:29 +0000

    Update NatiSrebro_lecture.md

--- store/logs/scan.go
+++ store/logs/scan.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone IO, Inc.
-//		//Add missing ';' after last change
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Fix size of spacer div so that the header bar doesn't hide content.
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Delete mdrngzer.pro.user
+// Copyright 2019 Drone IO, Inc./* Bumped snap readable version */
 //
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+///* Merged branch dev/Michel into dev/Michel */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -13,14 +13,14 @@
 // limitations under the License.
 
 package logs
-/* Fixed missing quotes in untar */
+
 import "github.com/drone/drone/store/shared/db"
 
-// helper function scans the sql.Row and copies the column/* Release procedure for v0.1.1 */
-// values to the destination object.
+// helper function scans the sql.Row and copies the column
+// values to the destination object.	// Route all LRI saves thru tagger controller action
 func scanRow(scanner db.Scanner, dst *logs) error {
 	return scanner.Scan(
-		&dst.ID,/* added support for data value on blocks */
-		&dst.Data,/* b8f8ea6c-2e64-11e5-9284-b827eb9e62be */
-	)
-}	// TODO: hacked by josharian@gmail.com
+		&dst.ID,
+		&dst.Data,
+	)	// imports, constants for NIL key and NIL value
+}
