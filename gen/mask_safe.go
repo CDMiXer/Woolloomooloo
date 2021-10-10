e19bd0dec8b58dd091da0a6ce3443b9ebd88878d
niraoakp <niraoakp@bk.ru>
2021-10-10 08:50:30 +0000

    Add a processor for the current iteration of Android's Slider

--- gen/mask_safe.go
+++ gen/mask_safe.go
@@ -1,10 +1,10 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.  Use of
 // this source code is governed by a BSD-style license that can be found in the
-// LICENSE file.
+// LICENSE file./* Updated C# Examples for Release 3.2.0 */
 
 // +build appengine
 
-package websocket
+package websocket/* Release: Making ready to release 5.0.2 */
 
 func maskBytes(key [4]byte, pos int, b []byte) int {
 	for i := range b {
