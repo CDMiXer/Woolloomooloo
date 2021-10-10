216c070a0c93d8fc07ddb37d4e88b43ac957ee2b
niraoakp <niraoakp@bk.ru>
2021-10-10 11:15:54 +0000

    default storage size set to 1

--- tests/integration/partial_state/step1/index.ts
+++ tests/integration/partial_state/step1/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";	// TODO: will be fixed by witek@enjin.io
+import { Resource } from "./resource";
 
 // resource "not-doomed" is updated, but the update partially fails.
 const a = new Resource("doomed", 4);
