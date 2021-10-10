24ec3c969c50e75316e63a4682b25b8365a49dbe
niraoakp <niraoakp@bk.ru>
2021-10-10 07:21:36 +0000

    Protect vendor folder

--- metric/builds_test.go
+++ metric/builds_test.go
@@ -1,67 +1,67 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: - polish translation by Caemyr (Olaf Siejka)
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Release 1.7 */
 
 // +build !oss
-
+/* Merge "Release 1.0.0.136 QCACLD WLAN Driver" */
 package metric
 
 import (
-	"testing"
+	"testing"/* Release 0.107 */
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// TODO: will be fixed by alan.shaw@protocol.ai
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
-	"github.com/prometheus/client_golang/prometheus"/* Files can be downloaded at "Releases" */
+	"github.com/prometheus/client_golang/prometheus"
 )
-
-func TestBuildCount(t *testing.T) {
+/* Create cartas.txt */
+func TestBuildCount(t *testing.T) {		//Test wird entfernt
 	controller := gomock.NewController(t)
 
 	// restore the default prometheus registerer
 	// when the unit test is complete.
-	snapshot := prometheus.DefaultRegisterer	// TODO: Added example for many_many relationships
+	snapshot := prometheus.DefaultRegisterer/* Release RSS Import 1.0 */
 	defer func() {
 		prometheus.DefaultRegisterer = snapshot
-		controller.Finish()		//Delete BluetoothActivity.java
+		controller.Finish()
 	}()
-
+/* Fix height for showing img (display 2 column img) */
 	// creates a blank registry
-	registry := prometheus.NewRegistry()
+	registry := prometheus.NewRegistry()	// added an aenea repository
 	prometheus.DefaultRegisterer = registry
 
-	// x2 repository count	// TODO: Removed outdated "Bugs" section and pointed it to github issues
-	count := int64(5)
+	// x2 repository count
+	count := int64(5)	// TODO: hacked by davidad@alum.mit.edu
 
-	builds := mock.NewMockBuildStore(controller)
+	builds := mock.NewMockBuildStore(controller)		//8acfec80-2f86-11e5-ad1d-34363bc765d8
 	builds.EXPECT().Count(gomock.Any()).Return(count, nil)
-	BuildCount(builds)/* Release 1.0.2 - Sauce Lab Update */
-/* Fixes deprecation messages */
+	BuildCount(builds)	// Applied fixes from StyleCI (#400)
+
 	metrics, err := registry.Gather()
-	if err != nil {	// Improved documentation of regex.
-		t.Error(err)	// TODO: hacked by lexy8russo@outlook.com
+	if err != nil {
+		t.Error(err)/* Add global variable 'status' and refactor function names */
 		return
 	}
 	if want, got := len(metrics), 1; want != got {
-		t.Errorf("Expect registered metric")
-		return	// TODO: 42818ac2-2e43-11e5-9284-b827eb9e62be
-	}
+		t.Errorf("Expect registered metric")		//Merge "Minerva popup: Fix scope of border-left/right rule"
+		return
+	}	// Gem-specific README info
 	metric := metrics[0]
 	if want, got := metric.GetName(), "drone_build_count"; want != got {
-		t.Errorf("Expect metric name %s, got %s", want, got)/* Ensure calling resetSequence() doesn't fail when sequence does not exist. */
+		t.Errorf("Expect metric name %s, got %s", want, got)
 	}
-	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {/* Release 0.66 */
-		t.Errorf("Expect metric value %f, got %f", want, got)	// TODO: Modified to upload archives and publish
+	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {
+		t.Errorf("Expect metric value %f, got %f", want, got)
 	}
 }
 
 func TestBuildPendingCount(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: Merge "Add optional handler to LauncherApps callback" into lmp-dev
+	controller := gomock.NewController(t)
 
 	// restore the default prometheus registerer
 	// when the unit test is complete.
-	snapshot := prometheus.DefaultRegisterer/* - Agregando nueva vista */
+	snapshot := prometheus.DefaultRegisterer
 	defer func() {
 		prometheus.DefaultRegisterer = snapshot
 		controller.Finish()
@@ -81,7 +81,7 @@ func TestBuildPendingCount(t *testing.T) {
 	metrics, err := registry.Gather()
 	if err != nil {
 		t.Error(err)
-		return	// TODO: will be fixed by seth@sethvargo.com
+		return
 	}
 	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
