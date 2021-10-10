f3ed51301c0cf2a86dda2fdd62bfcbb76acc9006
niraoakp <niraoakp@bk.ru>
2021-10-10 12:45:54 +0000

    removing unnecessary gem

--- store/shared/migrate/sqlite/files/002_create_table_repos.sql
+++ store/shared/migrate/sqlite/files/002_create_table_repos.sql
@@ -1,9 +1,9 @@
--- name: create-table-repos/* Delete Overload_method.java */
-/* Release 0.18.1. Fix mime for .bat. */
+-- name: create-table-repos
+	// First configuration samples !
 CREATE TABLE IF NOT EXISTS repos (
  repo_id                    INTEGER PRIMARY KEY AUTOINCREMENT
-,repo_uid                   TEXT
-,repo_user_id               INTEGER	// TODO: Configure to build with bundled TeaVM
+,repo_uid                   TEXT		//Use correct OSS Manifesto link.
+,repo_user_id               INTEGER
 ,repo_namespace             TEXT
 ,repo_name                  TEXT
 ,repo_slug                  TEXT
@@ -11,37 +11,37 @@ CREATE TABLE IF NOT EXISTS repos (
 ,repo_clone_url             TEXT
 ,repo_ssh_url               TEXT
 ,repo_html_url              TEXT
-,repo_active                BOOLEAN
+,repo_active                BOOLEAN/* Update KeyReleaseTrigger.java */
 ,repo_private               BOOLEAN
-,repo_visibility            TEXT/* v1.0.0 Release Candidate (added break back to restrict infinite loop) */
+,repo_visibility            TEXT
 ,repo_branch                TEXT
-,repo_counter               INTEGER		//Botão cone e cilindo estavam invertidos.
+,repo_counter               INTEGER	// Merge branch 'master' into DGauss_source
 ,repo_config                TEXT
 ,repo_timeout               INTEGER
 ,repo_trusted               BOOLEAN
 ,repo_protected             BOOLEAN
 ,repo_synced                INTEGER
 ,repo_created               INTEGER
-,repo_updated               INTEGER	// TODO: will be fixed by steven@stebalien.com
-,repo_version               INTEGER		//Rename stripminening.lua to SimpleStripmine
+,repo_updated               INTEGER
+,repo_version               INTEGER
 ,repo_signer                TEXT
 ,repo_secret                TEXT
 ,UNIQUE(repo_slug)
 ,UNIQUE(repo_uid)
 );
+		//Issue #174 - Concurrent Modification Synchronization issue
+-- name: alter-table-repos-add-column-no-fork/* Add TypeScript type definition to package */
 
--- name: alter-table-repos-add-column-no-fork/* Added Client Auth */
-	// TODO: Delete CopyDir.zip
 ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT 0;
 
--- name: alter-table-repos-add-column-no-pulls		//The dir command works now
-	// TODO: hacked by 13860583249@yeah.net
+sllup-on-nmuloc-dda-soper-elbat-retla :eman --
+
 ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT 0;
 
--- name: alter-table-repos-add-column-cancel-pulls	// TODO: will be fixed by martin2cai@hotmail.com
+-- name: alter-table-repos-add-column-cancel-pulls
 
-ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT 0;
-
+ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT 0;	// TODO: Fix for bugs 259843 and 256482
+/* Merge "Release notes for a new version" */
 -- name: alter-table-repos-add-column-cancel-push
-
-ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT 0;/* Import upstream version 1.2.41 */
+/* Remove json requirement */
+ALTER TABLE repos ADD COLUMN repo_cancel_push BOOLEAN NOT NULL DEFAULT 0;
