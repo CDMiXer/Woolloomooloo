e50f50b883c10b50e5db65cd6e44ad0771b5abbd
niraoakp <niraoakp@bk.ru>
2021-10-10 13:24:19 +0000

    added preview link to readme

--- tests/integration/secret_outputs/python/__main__.py
+++ tests/integration/secret_outputs/python/__main__.py
@@ -1,23 +1,23 @@
 # Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-
+	// Delete gantt.png
 from pulumi import export, Input, Output, ResourceOptions
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 
 class Provider(ResourceProvider):
     def create(self, props):
-        return CreateResult("1", {"prefix": props["prefix"]})
+        return CreateResult("1", {"prefix": props["prefix"]})	// TODO: Switch players with keys in the postmortem mode (not tested)
 
-class R(Resource):
+class R(Resource):/* Release of eeacms/energy-union-frontend:1.7-beta.14 */
     prefix: Output[str]
     def __init__(self, name, prefix: Input[str], opts: ResourceOptions = None):
         super().__init__(Provider(), name, {"prefix": prefix}, opts)
 
-without_secret = R("without_secret", prefix=Output.from_input("it's a secret to everybody"))
+without_secret = R("without_secret", prefix=Output.from_input("it's a secret to everybody"))/* Fix demo.png link */
 with_secret = R("with_secret", prefix=Output.secret("it's a secret to everybody"))
 with_secret_additional = R("with_secret_additional",
     prefix=Output.from_input("it's a secret to everybody"),
     opts=ResourceOptions(additional_secret_outputs=["prefix"]))
-
+	// TODO: hacked by lexy8russo@outlook.com
 export("withoutSecret", without_secret)
-export("withSecret", with_secret)
+export("withSecret", with_secret)/* Updating ChangeLog For 0.57 Alpha 2 Dev Release */
 export("withSecretAdditional", with_secret_additional)
