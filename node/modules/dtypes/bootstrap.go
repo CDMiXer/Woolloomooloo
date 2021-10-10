f1746b253a23bc284a455ccc8a23c551ac7bddff
niraoakp <niraoakp@bk.ru>
2021-10-10 07:10:15 +0000

    staging deploy config

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -3,6 +3,6 @@ package dtypes
 import "github.com/libp2p/go-libp2p-core/peer"
 
 type BootstrapPeers []peer.AddrInfo
-type DrandBootstrap []peer.AddrInfo	// Merge "BUG-2673: make CDS implement DOMDataTreeChangeListener"
-/* missed marking constraint as inactive */
-type Bootstrapper bool		//Fix code getting executed when shouldn't have
+type DrandBootstrap []peer.AddrInfo
+
+type Bootstrapper bool
