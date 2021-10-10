be30d0ed315cdc207966ac9d3d591ff7dee81d67
niraoakp <niraoakp@bk.ru>
2021-10-10 14:38:05 +0000

    225ad552-2e50-11e5-9284-b827eb9e62be

--- plugin/secret/encrypted_test.go
+++ plugin/secret/encrypted_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Disable react/wrap-multilines
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* More reliably detect the start of RDFT transmissions. */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Modified the load scan test addidng some shaders */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// New theme: Illustratr - 1.0
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package secret
