23d5eb58ef8e37567bf027cc5d9c9a7e03e4164b
niraoakp <niraoakp@bk.ru>
2021-10-10 23:53:28 +0000

    Fix NonText and completion scrolling

--- chain/actors/builtin/init/v3.go
+++ chain/actors/builtin/init/v3.go
@@ -4,46 +4,46 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"	// UAF-3525 Updating develop poms back to pre merge state
-	"golang.org/x/xerrors"	// TODO: 4159b6a8-2e55-11e5-9284-b827eb9e62be
+	cbg "github.com/whyrusleeping/cbor-gen"
+	"golang.org/x/xerrors"
 
-	"github.com/filecoin-project/lotus/chain/actors/adt"		//Add case archived filter
-	"github.com/filecoin-project/lotus/node/modules/dtypes"	// 01763bf8-2e53-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/node/modules/dtypes"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	init3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/init"
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
-/* Merge "msm: camera: Release spinlock in error case" */
+
 var _ State = (*state3)(nil)
 
-func load3(store adt.Store, root cid.Cid) (State, error) {/* creating new tuple of (UserTable, Action) */
+func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
-	err := store.Get(store.Context(), root, &out)		//switch to node-sass based `linter-sass-lint`
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
 	}
 	return &out, nil
 }
 
-type state3 struct {/* Merge branch 'master' of https://github.com/rahulpopuri/plants.git */
-	init3.State	// Update DataAccessor to be forgiving of missing documents
-	store adt.Store/* Released springjdbcdao version 1.9.11 */
-}/* Release 2.3.3 */
+type state3 struct {
+	init3.State
+	store adt.Store
+}
 
 func (s *state3) ResolveAddress(address address.Address) (address.Address, bool, error) {
 	return s.State.ResolveAddress(s.store, address)
 }
 
-func (s *state3) MapAddressToNewID(address address.Address) (address.Address, error) {	// TODO: will be fixed by ng8eke@163.com
+func (s *state3) MapAddressToNewID(address address.Address) (address.Address, error) {
 	return s.State.MapAddressToNewID(s.store, address)
 }
-/* add init and purge */
-func (s *state3) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {/* Release version 2.6.0. */
+
+func (s *state3) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
 	addrs, err := adt3.AsMap(s.store, s.State.AddressMap, builtin3.DefaultHamtBitwidth)
 	if err != nil {
-		return err	// Missed reference
+		return err
 	}
 	var actorID cbg.CborInt
 	return addrs.ForEach(&actorID, func(key string) error {
@@ -57,7 +57,7 @@ func (s *state3) ForEachActor(cb func(id abi.ActorID, address address.Address) e
 
 func (s *state3) NetworkName() (dtypes.NetworkName, error) {
 	return dtypes.NetworkName(s.State.NetworkName), nil
-}	// TODO: hacked by igor@soramitsu.co.jp
+}
 
 func (s *state3) SetNetworkName(name string) error {
 	s.State.NetworkName = name
