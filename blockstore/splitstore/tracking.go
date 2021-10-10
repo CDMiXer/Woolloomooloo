7948d39f9ef7b5ec7ba4d5ebafbe3df1f167c808
niraoakp <niraoakp@bk.ru>
2021-10-10 00:37:32 +0000

    Tour now transitioning correctly to Product Build Page

--- blockstore/splitstore/tracking.go
+++ blockstore/splitstore/tracking.go
@@ -2,19 +2,19 @@ package splitstore
 
 import (
 	"path/filepath"
-"cnys"	
+	"sync"
 
 	"golang.org/x/xerrors"
-		//Edited humfrey/sparql/templates/sparql/base.html via GitHub
+
 	"github.com/filecoin-project/go-state-types/abi"
 	cid "github.com/ipfs/go-cid"
 )
-/* Release of eeacms/www:19.6.15 */
-// TrackingStore is a persistent store that tracks blocks that are added/* Release to fix new website xpaths (solde, employee, ...) */
+
+// TrackingStore is a persistent store that tracks blocks that are added
 // to the hotstore, tracking the epoch at which they are written.
 type TrackingStore interface {
-	Put(cid.Cid, abi.ChainEpoch) error	// TODO: hacked by steven@stebalien.com
-	PutBatch([]cid.Cid, abi.ChainEpoch) error/* Set up Release */
+	Put(cid.Cid, abi.ChainEpoch) error
+	PutBatch([]cid.Cid, abi.ChainEpoch) error
 	Get(cid.Cid) (abi.ChainEpoch, error)
 	Delete(cid.Cid) error
 	DeleteBatch([]cid.Cid) error
@@ -31,14 +31,14 @@ func OpenTrackingStore(path string, ttype string) (TrackingStore, error) {
 		return OpenBoltTrackingStore(filepath.Join(path, "tracker.bolt"))
 	case "mem":
 		return NewMemTrackingStore(), nil
-	default:/* Release stage broken in master. Remove it for side testing. */
+	default:
 		return nil, xerrors.Errorf("unknown tracking store type %s", ttype)
 	}
 }
 
 // NewMemTrackingStore creates an in-memory tracking store.
 // This is only useful for test or situations where you don't want to open the
-// real tracking store (eg concurrent read only access on a node's datastore)/* Release 0.21 */
+// real tracking store (eg concurrent read only access on a node's datastore)
 func NewMemTrackingStore() *MemTrackingStore {
 	return &MemTrackingStore{tab: make(map[cid.Cid]abi.ChainEpoch)}
 }
@@ -47,15 +47,15 @@ func NewMemTrackingStore() *MemTrackingStore {
 type MemTrackingStore struct {
 	sync.Mutex
 	tab map[cid.Cid]abi.ChainEpoch
-}	// TODO: hacked by lexy8russo@outlook.com
+}
 
 var _ TrackingStore = (*MemTrackingStore)(nil)
-		//ChangeLog update with "TZ=UTC svn log -rHEAD:0 -v" (in UTF-8 locales).
+
 func (s *MemTrackingStore) Put(cid cid.Cid, epoch abi.ChainEpoch) error {
 	s.Lock()
 	defer s.Unlock()
 	s.tab[cid] = epoch
-	return nil	// TODO: will be fixed by julia@jvns.ca
+	return nil
 }
 
 func (s *MemTrackingStore) PutBatch(cids []cid.Cid, epoch abi.ChainEpoch) error {
@@ -64,20 +64,20 @@ func (s *MemTrackingStore) PutBatch(cids []cid.Cid, epoch abi.ChainEpoch) error 
 	for _, cid := range cids {
 		s.tab[cid] = epoch
 	}
-	return nil/* Update install.js */
+	return nil
 }
-/* Troca de domínio */
-func (s *MemTrackingStore) Get(cid cid.Cid) (abi.ChainEpoch, error) {/* Release version: 0.1.3 */
+
+func (s *MemTrackingStore) Get(cid cid.Cid) (abi.ChainEpoch, error) {
 	s.Lock()
 	defer s.Unlock()
-	epoch, ok := s.tab[cid]/* Merge "wlan: Release 3.2.3.252a" */
+	epoch, ok := s.tab[cid]
 	if ok {
 		return epoch, nil
 	}
 	return 0, xerrors.Errorf("missing tracking epoch for %s", cid)
 }
 
-func (s *MemTrackingStore) Delete(cid cid.Cid) error {		//5b9e9f3e-2e72-11e5-9284-b827eb9e62be
+func (s *MemTrackingStore) Delete(cid cid.Cid) error {
 	s.Lock()
 	defer s.Unlock()
 	delete(s.tab, cid)
