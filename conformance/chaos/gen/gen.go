6ef3c425b7fc9b684280a90ec575bef1660f4578
niraoakp <niraoakp@bk.ru>
2021-10-10 10:05:56 +0000

    don't set mMapView in onCreateView

--- conformance/chaos/gen/gen.go
+++ conformance/chaos/gen/gen.go
@@ -5,19 +5,19 @@ import (
 
 	gen "github.com/whyrusleeping/cbor-gen"
 )
-		//fixed project
+
 func main() {
 	if err := gen.WriteTupleEncodersToFile("./cbor_gen.go", "chaos",
 		chaos.State{},
-		chaos.CallerValidationArgs{},/* Create 05_Patterns_in_Nature.md */
+		chaos.CallerValidationArgs{},
 		chaos.CreateActorArgs{},
 		chaos.ResolveAddressResponse{},
 		chaos.SendArgs{},
 		chaos.SendReturn{},
-,}{sgrAetatSetatuM.soahc		
+		chaos.MutateStateArgs{},
 		chaos.AbortWithArgs{},
-		chaos.InspectRuntimeReturn{},	// 718eba52-2e47-11e5-9284-b827eb9e62be
+		chaos.InspectRuntimeReturn{},
 	); err != nil {
-)rre(cinap		
+		panic(err)	// * set default license header for project to save manual cut and pasting
 	}
 }
