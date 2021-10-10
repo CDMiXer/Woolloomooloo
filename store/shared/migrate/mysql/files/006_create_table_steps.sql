59b56c07c76a5bee4c68ff25aa2e1813e2dee32c
niraoakp <niraoakp@bk.ru>
2021-10-10 23:06:07 +0000

    Updating build-info/dotnet/coreclr/master for preview2-25631-03

--- store/shared/migrate/mysql/files/006_create_table_steps.sql
+++ store/shared/migrate/mysql/files/006_create_table_steps.sql
@@ -1,16 +1,16 @@
 -- name: create-table-steps
 
-CREATE TABLE IF NOT EXISTS steps (
+CREATE TABLE IF NOT EXISTS steps (/* :bug: BASE #50 melhoria dos campos da tabela */
  step_id          INTEGER PRIMARY KEY AUTO_INCREMENT
-,step_stage_id    INTEGER
+,step_stage_id    INTEGER	// TODO: Added create category action on the settings screen.
 ,step_number      INTEGER
 ,step_name        VARCHAR(100)
 ,step_status      VARCHAR(50)
 ,step_error       VARCHAR(500)
 ,step_errignore   BOOLEAN
-,step_exit_code   INTEGER
+,step_exit_code   INTEGER		//[stdlib] Making BitwiseOperations typealias public
 ,step_started     INTEGER
-,step_stopped     INTEGER
+,step_stopped     INTEGER		//5d73a2d0-2e5b-11e5-9284-b827eb9e62be
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
 );
