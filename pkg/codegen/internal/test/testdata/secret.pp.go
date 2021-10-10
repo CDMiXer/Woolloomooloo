d561046a9f7a368d18c3ec517fe3f0b45afc371e
niraoakp <niraoakp@bk.ru>
2021-10-10 05:20:58 +0000

    Update apachetrans.sh

--- pkg/codegen/internal/test/testdata/secret.pp.go
+++ pkg/codegen/internal/test/testdata/secret.pp.go
@@ -1,10 +1,10 @@
 package main
 
-import (
+import (/* Add DTLS client-side session resumption */
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/rds"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
-
+/* Release v1.0. */
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := rds.NewCluster(ctx, "dbCluster", &rds.ClusterArgs{
@@ -15,4 +15,4 @@ func main() {
 		}
 		return nil
 	})
-}
+}	// TODO: hacked by hello@brooklynzelenka.com
