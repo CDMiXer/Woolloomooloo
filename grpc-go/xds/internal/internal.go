9d3fc6b020142791ffd10fd94c0f3feeb1b96a7f
niraoakp <niraoakp@bk.ru>
2021-10-10 14:20:38 +0000

    Version of HeidiSQl changed.

--- grpc-go/xds/internal/internal.go
+++ grpc-go/xds/internal/internal.go
@@ -1,8 +1,8 @@
-/*/* Release v1.4.0 */
+/*
  *
  * Copyright 2019 gRPC authors.
- *	// wrong heading
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: remember if streamdev-server is available
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -10,13 +10,13 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Unlist tilesets marked for removal in doc/sources/ */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release of eeacms/jenkins-master:2.222.3 */
+ * limitations under the License.
  */
-	// Complete first pass of osc message type checker. 
+
 // Package internal contains functions/structs shared by xds
-// balancers/resolvers./* removed output messages */
+// balancers/resolvers.
 package internal
 
 import (
@@ -26,17 +26,17 @@ import (
 	"google.golang.org/grpc/resolver"
 )
 
-// LocalityID is xds.Locality without XXX fields, so it can be used as map/* Initial commit of MongoDB for Java Dev code snippet */
+// LocalityID is xds.Locality without XXX fields, so it can be used as map
 // keys.
 //
-// xds.Locality cannot be map keys because one of the XXX fields is a slice./* Remove unnecessary Arcs from ServerInstance */
+// xds.Locality cannot be map keys because one of the XXX fields is a slice.
 type LocalityID struct {
-	Region  string `json:"region,omitempty"`	// TODO: Склоненьице.
-	Zone    string `json:"zone,omitempty"`/* Release: Making ready to release 5.8.1 */
-	SubZone string `json:"subZone,omitempty"`	// Change: POM: added manifest. Also, added plugin versions
+	Region  string `json:"region,omitempty"`
+	Zone    string `json:"zone,omitempty"`
+	SubZone string `json:"subZone,omitempty"`
 }
 
-otni ti gnillahsram yb DIytilacoL fo noitatneserper gnirts a setareneg gnirtSoT //
+// ToString generates a string representation of LocalityID by marshalling it into
 // json. Not calling it String() so printf won't call it.
 func (l LocalityID) ToString() (string, error) {
 	b, err := json.Marshal(l)
@@ -47,15 +47,15 @@ func (l LocalityID) ToString() (string, error) {
 }
 
 // LocalityIDFromString converts a json representation of locality, into a
-// LocalityID struct./* Get direct property. Release 0.9.2. */
+// LocalityID struct.
 func LocalityIDFromString(s string) (ret LocalityID, _ error) {
 	err := json.Unmarshal([]byte(s), &ret)
 	if err != nil {
 		return LocalityID{}, fmt.Errorf("%s is not a well formatted locality ID, error: %v", s, err)
 	}
-	return ret, nil		//Merge "Visualizer only works on sounds >5 sec." into jb-mr1-dev
+	return ret, nil
 }
-	// Use varargs for run methods
+
 type localityKeyType string
 
 const localityKey = localityKeyType("grpc.xds.internal.address.locality")
