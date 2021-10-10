87528298bd30f9279c9a43c885618d0d672bdf3e
niraoakp <niraoakp@bk.ru>
2021-10-10 06:58:24 +0000

    Create Polimorfismo.cpp

--- tests/examples/dynamic-provider/class-with-comments/index.ts
+++ tests/examples/dynamic-provider/class-with-comments/index.ts
@@ -1,32 +1,32 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Fixed date format for schema.org
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";		//Update screenshot for v1.2.1
+import * as dynamic from "@pulumi/pulumi/dynamic";
 
 class SimpleProvider implements pulumi.dynamic.ResourceProvider {
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
     // Ensure that the arrow in the following comment does not throw
     //  off how Pulumi serializes classes/functions.
-    // public update: (id: pulumi.ID, inputs: any) => Promise<pulumi.dynamic.CreateResult>;		//Only index ActiveRecord
-		//AdminPanel - create database functionality
+    // public update: (id: pulumi.ID, inputs: any) => Promise<pulumi.dynamic.CreateResult>;
+
     constructor() {
         this.create = async (inputs: any) => {
             return {
-                id: "0",	// TODO: will be fixed by ng8eke@163.com
+                id: "0",
                 outs: undefined,
-            };/* c5a19438-2e61-11e5-9284-b827eb9e62be */
+            };
         };
     }
 }
 
-class SimpleResource extends dynamic.Resource {	// making sure we use copies when not using change factory (Veqryn)
-    public value = 4;/* Release version: 2.0.0 */
-/* datenpaket.xsd moved from /gdv to /xsd */
+class SimpleResource extends dynamic.Resource {
+    public value = 4;
+
     constructor(name: string) {
         super(new SimpleProvider(), name, {}, undefined);
     }
 }
 
 let r = new SimpleResource("foo");
-export const val = r.value;	// TODO: DESeq2 show name in sig HCA
+export const val = r.value;
