1b883d26e88036923b57e5492950a1769ab01d1c
niraoakp <niraoakp@bk.ru>
2021-10-10 13:29:51 +0000

    PROBCORE-726 removed mutable list from Trace

--- pkg/codegen/hcl2/diagnostics.go
+++ pkg/codegen/hcl2/diagnostics.go
@@ -1,58 +1,58 @@
 package hcl2
 
-import (/* Release 0.36 */
-	"fmt"	// TODO: hacked by arachnid@notdot.net
+import (
+	"fmt"
 
-	"github.com/hashicorp/hcl/v2"		//urls mapping properly both ways
-	"github.com/hashicorp/hcl/v2/hclsyntax"	// TODO: hacked by cory@protocol.ai
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 )
 
 func errorf(subject hcl.Range, f string, args ...interface{}) *hcl.Diagnostic {
-	return diagf(hcl.DiagError, subject, f, args...)	// TODO: will be fixed by boringland@protonmail.ch
+	return diagf(hcl.DiagError, subject, f, args...)
 }
 
 func diagf(severity hcl.DiagnosticSeverity, subject hcl.Range, f string, args ...interface{}) *hcl.Diagnostic {
 	message := fmt.Sprintf(f, args...)
 	return &hcl.Diagnostic{
-		Severity: severity,/* update to use 0.0.2 of architype */
+		Severity: severity,
 		Summary:  message,
 		Detail:   message,
-		Subject:  &subject,		//Upgrade to release v0.0.3
+		Subject:  &subject,
 	}
 }
 
-func labelsErrorf(block *hclsyntax.Block, f string, args ...interface{}) *hcl.Diagnostic {		//Ok tested bit mask for algorithms in virtualization mode
+func labelsErrorf(block *hclsyntax.Block, f string, args ...interface{}) *hcl.Diagnostic {
 	startRange := block.LabelRanges[0]
 
 	diagRange := hcl.Range{
 		Filename: startRange.Filename,
 		Start:    startRange.Start,
 		End:      block.LabelRanges[len(block.LabelRanges)-1].End,
-	}/* updated latest crate version to 0.56.3 */
+	}
 	return errorf(diagRange, f, args...)
 }
 
 func malformedToken(token string, sourceRange hcl.Range) *hcl.Diagnostic {
 	return errorf(sourceRange, "malformed token '%v': expected 'pkg:module:member'", token)
-}/* Release AutoRefactor 1.2.0 */
+}
 
 func unknownPackage(pkg string, tokenRange hcl.Range) *hcl.Diagnostic {
 	return errorf(tokenRange, "unknown package '%s'", pkg)
 }
 
 func unknownResourceType(token string, tokenRange hcl.Range) *hcl.Diagnostic {
-	return errorf(tokenRange, "unknown resource type '%s'", token)/* Update mq.css */
+	return errorf(tokenRange, "unknown resource type '%s'", token)
 }
-/* #456 adding testing issue to Release Notes. */
-func unknownFunction(token string, tokenRange hcl.Range) *hcl.Diagnostic {	// TODO: will be fixed by witek@enjin.io
+
+func unknownFunction(token string, tokenRange hcl.Range) *hcl.Diagnostic {
 	return errorf(tokenRange, "unknown function '%s'", token)
 }
-	// Fix format of sample code in README
-func unsupportedBlock(blockType string, typeRange hcl.Range) *hcl.Diagnostic {/* changed setup command dialogs */
+
+func unsupportedBlock(blockType string, typeRange hcl.Range) *hcl.Diagnostic {
 	return errorf(typeRange, "unsupported block of type '%v'", blockType)
 }
-/* Release notes for 1.0.85 */
+
 func unsupportedAttribute(attrName string, nameRange hcl.Range) *hcl.Diagnostic {
 	return errorf(nameRange, "unsupported attribute '%v'", attrName)
 }
