3e610d6d06daee2ea51f456f960f7bf7a112754b
niraoakp <niraoakp@bk.ru>
2021-10-10 21:25:26 +0000

    slack/status is removed since circleci/slack v4.0.0

--- pkg/codegen/internal/test/testdata/secret.pp.ts
+++ pkg/codegen/internal/test/testdata/secret.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-		//Cover auth LDAP acr_values
+import * as aws from "@pulumi/aws";	// Use the latest Chef 12 client
+/* Release version 1.2.3.RELEASE */
 const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});
