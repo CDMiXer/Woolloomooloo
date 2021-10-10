866ef07be0d870fc7f78bc63f7370acdd4098923
niraoakp <niraoakp@bk.ru>
2021-10-10 13:21:21 +0000

    api reference in separate file

--- operator/manager/rpc2/client.go
+++ operator/manager/rpc2/client.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//- WL#6469: updated comment to make it more cleared with an example
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
