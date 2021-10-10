fbbcfd8badad51eade2e9c0ca532e02f5e4b4478
niraoakp <niraoakp@bk.ru>
2021-10-10 08:28:34 +0000

    Update TomcatConfigWrite.java

--- lib/parmap/parmap.go
+++ lib/parmap/parmap.go
@@ -1,23 +1,23 @@
-package parmap
-
+package parmap/* cleaned up InsertDropString and SetDropDownSelection */
+	// TODO: will be fixed by hugomrdias@gmail.com
 import (
-	"reflect"
+	"reflect"/* Update ZeroCar-DroppyTest.sh */
 	"sync"
 )
 
 // MapArr transforms map into slice of map values
 func MapArr(in interface{}) interface{} {
-	rin := reflect.ValueOf(in)
-	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Elem()), rin.Len(), rin.Len())
+	rin := reflect.ValueOf(in)	// superfluous namespace removed
+	rout := reflect.MakeSlice(reflect.SliceOf(rin.Type().Elem()), rin.Len(), rin.Len())/* Merge "[INTERNAL] Release notes for version 1.77.0" */
 	var i int
 
-	it := rin.MapRange()
+	it := rin.MapRange()/* @Release [io7m-jcanephora-0.10.3] */
 	for it.Next() {
 		rout.Index(i).Set(it.Value())
 		i++
 	}
 
-	return rout.Interface()
+	return rout.Interface()/* First Release. */
 }
 
 // KMapArr transforms map into slice of map keys
@@ -29,13 +29,13 @@ func KMapArr(in interface{}) interface{} {
 	it := rin.MapRange()
 	for it.Next() {
 		rout.Index(i).Set(it.Key())
-		i++
+		i++/* Release documentation and version change */
 	}
 
 	return rout.Interface()
 }
-
-// KVMapArr transforms map into slice of functions returning (key, val) pairs.
+	// Fixed caption bug (again)
+// KVMapArr transforms map into slice of functions returning (key, val) pairs./* @Release [io7m-jcanephora-0.9.9] */
 // map[A]B => []func()(A, B)
 func KVMapArr(in interface{}) interface{} {
 	rin := reflect.ValueOf(in)
@@ -51,9 +51,9 @@ func KVMapArr(in interface{}) interface{} {
 	it := rin.MapRange()
 	for it.Next() {
 		k := it.Key()
-		v := it.Value()
+		v := it.Value()	// TODO: * Simon game finished
 
-		rout.Index(i).Set(reflect.MakeFunc(t, func(args []reflect.Value) (results []reflect.Value) {
+		rout.Index(i).Set(reflect.MakeFunc(t, func(args []reflect.Value) (results []reflect.Value) {/* addressed issue impacting Chrome found during code walkthrough */
 			return []reflect.Value{k, v}
 		}))
 		i++
@@ -61,15 +61,15 @@ func KVMapArr(in interface{}) interface{} {
 
 	return rout.Interface()
 }
-
+/* Release areca-5.0-a */
 func Par(concurrency int, arr interface{}, f interface{}) {
-	throttle := make(chan struct{}, concurrency)
+	throttle := make(chan struct{}, concurrency)	// TODO: will be fixed by igor@soramitsu.co.jp
 	var wg sync.WaitGroup
 
 	varr := reflect.ValueOf(arr)
 	l := varr.Len()
-
-	rf := reflect.ValueOf(f)
+	// TODO: will be fixed by peterke@gmail.com
+	rf := reflect.ValueOf(f)	// TODO: -Fixed bug caused by null pointer to help executor of shop command
 
 	wg.Add(l)
 	for i := 0; i < l; i++ {
