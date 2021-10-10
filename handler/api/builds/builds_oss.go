ebc52367c3f37b8e33e693d84aee2b65737b9ec9
niraoakp <niraoakp@bk.ru>
2021-10-10 16:51:12 +0000

    Changed alert message character: from & to and

--- handler/api/builds/builds_oss.go
+++ handler/api/builds/builds_oss.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//Fixed spelling in README.me.
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -10,24 +10,24 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Merge "docs: site mipmap folder updates" into lmp-docs
+// limitations under the License.
 
-// +build oss/* Merge "msm: camera: Release spinlock in error case" */
+// +build oss
 
 package builds
 
 import (
 	"net/http"
-/* Update FileIO.java */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {	// Add regular require, Buffer, raw request and response for lower-level usage.
-	render.NotImplemented(w, render.ErrNotImplemented)	// Relax version constraint for upcoming Flow 6
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {
+	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
 // HandleIncomplete returns a no-op http.HandlerFunc.
 func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
 	return notImplemented
-}/* Added ServerEnvironment.java, ReleaseServer.java and Release.java */
+}
