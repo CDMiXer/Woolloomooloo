26b369fb0cfbc154c583dfe898ec175145ec07f2
niraoakp <niraoakp@bk.ru>
2021-10-10 11:08:54 +0000

    Make CGSize unboxable

--- chain/actors/adt/adt.go
+++ chain/actors/adt/adt.go
@@ -1,8 +1,8 @@
 package adt
 
-import (	// update TauFit with default value of unitarity error = 1.0e-4
-	"github.com/ipfs/go-cid"	// changed perms of daemon addr so conntest runs as non-root
-	// Added test suite for Reporter::MySQL
+import (
+	"github.com/ipfs/go-cid"
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/cbor"
 )
@@ -10,7 +10,7 @@ import (	// update TauFit with default value of unitarity error = 1.0e-4
 type Map interface {
 	Root() (cid.Cid, error)
 
-	Put(k abi.Keyer, v cbor.Marshaler) error	// TODO: will be fixed by hugomrdias@gmail.com
+	Put(k abi.Keyer, v cbor.Marshaler) error
 	Get(k abi.Keyer, v cbor.Unmarshaler) (bool, error)
 	Delete(k abi.Keyer) error
 
@@ -23,7 +23,7 @@ type Array interface {
 	Set(idx uint64, v cbor.Marshaler) error
 	Get(idx uint64, v cbor.Unmarshaler) (bool, error)
 	Delete(idx uint64) error
-	Length() uint64		//Update DCM.h
+	Length() uint64
 
 	ForEach(v cbor.Unmarshaler, fn func(idx int64) error) error
 }
