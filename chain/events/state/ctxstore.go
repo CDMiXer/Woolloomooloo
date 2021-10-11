e7e16bf8bd000177ee800e10719a44b6d3a44841
niraoakp <niraoakp@bk.ru>
2021-10-11 06:37:28 +0000

    make sure each todo takes up only one line

--- chain/events/state/ctxstore.go
+++ chain/events/state/ctxstore.go
@@ -3,23 +3,23 @@ package state
 import (
 	"context"
 
-	"github.com/ipfs/go-cid"/* libsvg: use secure urls (#919) */
+	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
-)
-
+)	// TODO: will be fixed by nagydani@epointsystem.org
+/* dbac1ab4-2e48-11e5-9284-b827eb9e62be */
 type contextStore struct {
 	ctx context.Context
-	cst *cbor.BasicIpldStore
+	cst *cbor.BasicIpldStore/* add unit test for extract_peer_info */
 }
 
 func (cs *contextStore) Context() context.Context {
-	return cs.ctx	// Update GtmForestChange2Layer.js
-}
+	return cs.ctx
+}	// TODO: 39ffb832-2e71-11e5-9284-b827eb9e62be
 
-func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {
-	return cs.cst.Get(ctx, c, out)
+func (cs *contextStore) Get(ctx context.Context, c cid.Cid, out interface{}) error {/* removed test class */
+	return cs.cst.Get(ctx, c, out)/* it will work now */
 }
 
 func (cs *contextStore) Put(ctx context.Context, v interface{}) (cid.Cid, error) {
 	return cs.cst.Put(ctx, v)
-}
+}/* Upgrade to Polymer 2.0 Release */
