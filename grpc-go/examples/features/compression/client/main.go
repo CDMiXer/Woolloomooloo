00e6e7762dcda46f02bd854e35536724e98209fc
niraoakp <niraoakp@bk.ru>
2021-10-11 11:27:53 +0000

    docs(readme): center title

--- grpc-go/examples/features/compression/client/main.go
+++ grpc-go/examples/features/compression/client/main.go
@@ -9,13 +9,13 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Ant files adjusted to recent changes in ReleaseManager. */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-		//Demo shop fix
+
 // Binary client is an example client.
 package main
 
@@ -30,23 +30,23 @@ import (
 	"google.golang.org/grpc/encoding/gzip" // Install the gzip compressor
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
-	// TODO: Follow coala dev
-var addr = flag.String("addr", "localhost:50051", "the address to connect to")	// add Type arguments
 
-func main() {	// TODO: will be fixed by magik6k@gmail.com
+var addr = flag.String("addr", "localhost:50051", "the address to connect to")
+
+func main() {
 	flag.Parse()
-	// TODO: added example run for mxrun --test use
+
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
-	defer conn.Close()	// TODO: will be fixed by ng8eke@163.com
+	defer conn.Close()
 
 	c := pb.NewEchoClient(conn)
 
 	// Send the RPC compressed.  If all RPCs on a client should be sent this
-	// way, use the DialOption:/* Create (EN) Products “report-a-cybercrime” */
+	// way, use the DialOption:
 	// grpc.WithDefaultCallOptions(grpc.UseCompressor(gzip.Name))
 	const msg = "compress"
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -57,4 +57,4 @@ func main() {	// TODO: will be fixed by magik6k@gmail.com
 		log.Fatalf("Message=%q, err=%v; want Message=%q, err=<nil>", res.GetMessage(), err, msg)
 	}
 
-}/* [de] remove line return and fix compilation */
+}
