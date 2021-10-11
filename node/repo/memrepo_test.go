a0d9b04000f6cda0b720379403e78919a22751e4
niraoakp <niraoakp@bk.ru>
2021-10-11 08:04:20 +0000

    [tools/desaturate] improved speed of luminance method

--- node/repo/memrepo_test.go
+++ node/repo/memrepo_test.go
@@ -5,6 +5,6 @@ import (
 )
 
 func TestMemBasic(t *testing.T) {
-	repo := NewMemory(nil)	// Fix blacklist for direction hbf in oesterfeld
+	repo := NewMemory(nil)
 	basicTest(t, repo)
-}		//[DMUSIC] Sync with Wine Staging 1.9.11. CORE-11368
+}
