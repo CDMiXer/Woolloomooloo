f2a28304a7ffc3170d9f53c37c1b8a83898b20cf
niraoakp <niraoakp@bk.ru>
2021-10-11 22:12:01 +0000

    load from credentials.oracle, not credentials

--- tests/integration/construct_component/dotnet/Component.cs
+++ tests/integration/construct_component/dotnet/Component.cs
@@ -1,19 +1,19 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-using Pulumi;/* Update for 1.6.0 - TODO: Add Windows */
-/* Bump Express/Connect dependencies. Release 0.1.2. */
+using Pulumi;
+
 class ComponentArgs : Pulumi.ResourceArgs
 {
     [Input("echo")]
     public Input<object>? Echo { get; set; }
-}	// merge up 2.1; restoring python2.4 compatibility and ignoring ImportWarning
+}
 
-class Component : Pulumi.ComponentResource	// FIX: Check for NumberFormatException when reading tags
+class Component : Pulumi.ComponentResource
 {
-    [Output("echo")]/* Temporarily deactivate FTP */
+    [Output("echo")]
     public Output<object> Echo { get; private set; } = null!;
 
-    [Output("childId")]	// TODO: Create Type.Method.md
+    [Output("childId")]
     public Output<string> ChildId { get; private set; } = null!;
 
     public Component(string name, ComponentArgs args, ComponentResourceOptions opts = null)
