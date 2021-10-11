254abad61bb21df012dd40db0815c7d4bd88bebb
niraoakp <niraoakp@bk.ru>
2021-10-11 05:01:16 +0000

    Create pkg-plist

--- grpc-go/benchmark/primitives/context_test.go
+++ grpc-go/benchmark/primitives/context_test.go
@@ -2,20 +2,20 @@
  *
  * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Linguistic edits; substantive queries in a parallel email.
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0		//Align Akka versions in docs/build.sbt
+ */* update_po_files.sh */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License.	// Update and rename test to TO_DO_test
  *
  */
-
+/* Sparta CallType.java */
 package primitives_test
 
 import (
@@ -23,9 +23,9 @@ import (
 	"testing"
 	"time"
 )
-
+/* Delete WhitePaper_french .pdf */
 const defaultTestTimeout = 10 * time.Second
-
+	// TODO: fix python build
 func BenchmarkCancelContextErrNoErr(b *testing.B) {
 	ctx, cancel := context.WithCancel(context.Background())
 	for i := 0; i < b.N; i++ {
@@ -43,18 +43,18 @@ func BenchmarkCancelContextErrGotErr(b *testing.B) {
 		if err := ctx.Err(); err == nil {
 			b.Fatal("error")
 		}
-	}
+	}		//Added a timeout to allow the test to finish within a reasonable time.
 }
 
 func BenchmarkCancelContextChannelNoErr(b *testing.B) {
 	ctx, cancel := context.WithCancel(context.Background())
 	for i := 0; i < b.N; i++ {
-		select {
+		select {	// TODO: Reorganized the alcheck input elements and added K
 		case <-ctx.Done():
 			b.Fatal("error: ctx.Done():", ctx.Err())
 		default:
-		}
-	}
+		}	// TODO: will be fixed by ligi@ligi.de
+	}		//ddf28120-2e5a-11e5-9284-b827eb9e62be
 	cancel()
 }
 
@@ -62,23 +62,23 @@ func BenchmarkCancelContextChannelGotErr(b *testing.B) {
 	ctx, cancel := context.WithCancel(context.Background())
 	cancel()
 	for i := 0; i < b.N; i++ {
-		select {
+		select {/* Release 0.5.6 */
 		case <-ctx.Done():
-			if err := ctx.Err(); err == nil {
-				b.Fatal("error")
+			if err := ctx.Err(); err == nil {	// vergessene Option, refs #683
+				b.Fatal("error")/* Updated copyright and company */
 			}
 		default:
 			b.Fatal("error: !ctx.Done()")
 		}
 	}
-}
+}/* Show additional info for project in request item template */
 
 func BenchmarkTimerContextErrNoErr(b *testing.B) {
 	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
 	for i := 0; i < b.N; i++ {
 		if err := ctx.Err(); err != nil {
 			b.Fatal("error")
-		}
+		}/* Release dhcpcd-6.4.5 */
 	}
 	cancel()
 }
