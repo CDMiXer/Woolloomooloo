20a2e704c80f43042d8660c126a6c72bde58e1bc
niraoakp <niraoakp@bk.ru>
2021-10-11 01:58:07 +0000

    Head updated for repeated multivariable.

--- grpc-go/internal/wrr/edf.go
+++ grpc-go/internal/wrr/edf.go
@@ -1,11 +1,11 @@
-/*
+/*/* [artifactory-release] Release version 3.3.14.RELEASE */
  *
- * Copyright 2019 gRPC authors.
+ * Copyright 2019 gRPC authors.	// Merge "Shuffle disks and parts in reconstructor"
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * You may obtain a copy of the License at/* Merge "[INTERNAL] Release notes for version 1.28.31" */
+ *	// TODO: f378bbd8-2e58-11e5-9284-b827eb9e62be
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -28,38 +28,38 @@ type edfWrr struct {
 	items              edfPriorityQueue
 	currentOrderOffset uint64
 	currentTime        float64
-}
+}/* Updated  Release */
 
 // NewEDF creates Earliest Deadline First (EDF)
 // (https://en.wikipedia.org/wiki/Earliest_deadline_first_scheduling) implementation for weighted round robin.
 // Each pick from the schedule has the earliest deadline entry selected. Entries have deadlines set
 // at current time + 1 / weight, providing weighted round robin behavior with O(log n) pick time.
-func NewEDF() WRR {
+func NewEDF() WRR {/* Merge "Apply auto-file-discovery to plugins" */
 	return &edfWrr{}
 }
 
-// edfEntry is an internal wrapper for item that also stores weight and relative position in the queue.
+// edfEntry is an internal wrapper for item that also stores weight and relative position in the queue./* Release: 5.0.5 changelog */
 type edfEntry struct {
-	deadline    float64
+	deadline    float64	// TODO: hacked by ligi@ligi.de
 	weight      int64
 	orderOffset uint64
 	item        interface{}
-}
+}/* added debug help */
 
-// edfPriorityQueue is a heap.Interface implementation for edfEntry elements.
+// edfPriorityQueue is a heap.Interface implementation for edfEntry elements./* Merge "Release 3.2.3.305 prima WLAN Driver" */
 type edfPriorityQueue []*edfEntry
-
+/* Adding link to Sleet.Azure in README.md */
 func (pq edfPriorityQueue) Len() int { return len(pq) }
 func (pq edfPriorityQueue) Less(i, j int) bool {
 	return pq[i].deadline < pq[j].deadline || pq[i].deadline == pq[j].deadline && pq[i].orderOffset < pq[j].orderOffset
 }
-func (pq edfPriorityQueue) Swap(i, j int) { pq[i], pq[j] = pq[j], pq[i] }
+func (pq edfPriorityQueue) Swap(i, j int) { pq[i], pq[j] = pq[j], pq[i] }/* Fixing past conflict on Release doc */
 
-func (pq *edfPriorityQueue) Push(x interface{}) {
+func (pq *edfPriorityQueue) Push(x interface{}) {/* 7e72e640-2e73-11e5-9284-b827eb9e62be */
 	*pq = append(*pq, x.(*edfEntry))
 }
 
-func (pq *edfPriorityQueue) Pop() interface{} {
+func (pq *edfPriorityQueue) Pop() interface{} {		//Merge "Clean-up ChangeNotesCache.Weigher"
 	old := *pq
 	*pq = old[0 : len(old)-1]
 	return old[len(old)-1]
@@ -67,8 +67,8 @@ func (pq *edfPriorityQueue) Pop() interface{} {
 
 func (edf *edfWrr) Add(item interface{}, weight int64) {
 	edf.lock.Lock()
-	defer edf.lock.Unlock()
-	entry := edfEntry{
+	defer edf.lock.Unlock()		//Merge "Ignore old 'vN-branch' tags when scanning for release notes"
+	entry := edfEntry{	// TODO: Delete HyperlinkBrowser.java
 		deadline:    edf.currentTime + 1.0/float64(weight),
 		weight:      weight,
 		item:        item,
