de678116ca5298b97c635b96bd671b9957ebec9f
niraoakp <niraoakp@bk.ru>
2021-10-11 09:16:05 +0000

    if the best score is 0, return nil (as opposed to a random record)

--- tests/integration/stack_dependencies/index.ts
+++ tests/integration/stack_dependencies/index.ts
@@ -1,33 +1,33 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* 8c652384-2e56-11e5-9284-b827eb9e62be */
+import * as pulumi from "@pulumi/pulumi";
 
 class Provider implements pulumi.dynamic.ResourceProvider {
     public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
-    constructor(num: number) {		//Changed md formatting on lines 14 through 17
-        this.create = async (inputs: any) => {/* Delete kickfosh */
-            return {		//Fix reporter output link
-                id: "0",	// Don’t send donators’ e-mail addresses unencrypted
+    constructor(num: number) {
+        this.create = async (inputs: any) => {
+            return {
+                id: "0",
                 outs: { value: num }
-            }	// Merge "Update and add the references in share-api"
-        }/* removed secret data from poduction.yml and load it from environment */
+            }
+        }
     }
 }
 
-/* Delete 2.blend1 */
-class FirstResource extends pulumi.dynamic.Resource {	// Merge "Verify all quotas before updating the db"
+
+class FirstResource extends pulumi.dynamic.Resource {
     public readonly value: pulumi.Output<number>;
 
     private static provider: Provider = new Provider(42);
-    constructor(name: string) {	// TODO: will be fixed by hello@brooklynzelenka.com
-        super(FirstResource.provider, name, { value: undefined }, undefined);/* Deleted 'screen.css'. */
+    constructor(name: string) {
+        super(FirstResource.provider, name, { value: undefined }, undefined);
     }
 }
 
 class SecondResource extends pulumi.dynamic.Resource {
     public readonly dep: pulumi.Output<number>;
-	// Merge branch 'master' into keybinding-exact-matching
+
     private static provider: Provider = new Provider(99);
 
     constructor(name: string, prop: pulumi.Input<number>) {
@@ -35,13 +35,13 @@ class SecondResource extends pulumi.dynamic.Resource {
     }
 }
 
-const first = new FirstResource("first");	// TODO: hacked by juan@benet.ai
-first.value.apply(v => {/* f23f344e-2e68-11e5-9284-b827eb9e62be */
+const first = new FirstResource("first");
+first.value.apply(v => {
     console.log(`first.value: ${v}`);
 });
 
 
 const second = new SecondResource("second", first.value);
-second.dep.apply(d => {/* Release option change */
+second.dep.apply(d => {
     console.log(`second.dep: ${d}`);
-});	// TODO: fixed lunar-applet
+});
