7c04123f856a4404d6b26726b1dbafe85f56f982
niraoakp <niraoakp@bk.ru>
2021-10-11 18:49:47 +0000

    Add bit about anais nin

--- pkg/cmd/pulumi/stack_output_test.go
+++ pkg/cmd/pulumi/stack_output_test.go
@@ -1,21 +1,21 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+///* Log movement direction */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* 77a8cbd2-2e40-11e5-9284-b827eb9e62be */
-///* Release Kafka 1.0.2-0.9.0.1 (#19) */
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.0.4, compatible with ElasticSearch 1.4.0. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by souzau@yandex.com
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
-import (	// TODO: will be fixed by fjl@ethereum.org
-	"testing"
+import (
+	"testing"	// added case for non-field 
 
 	"github.com/stretchr/testify/assert"
 )
@@ -32,7 +32,7 @@ func TestStringifyOutput(t *testing.T) {
 	}
 
 	assert.Equal(t, "42", stringifyOutput(num))
-	assert.Equal(t, "ABC", stringifyOutput(str))	// Merge branch 'master' into greenkeeper/nsp-2.7.0
-	assert.Equal(t, "[\"hello\",\"goodbye\"]", stringifyOutput(arr))/* Release of eeacms/forests-frontend:2.0-beta.21 */
+	assert.Equal(t, "ABC", stringifyOutput(str))
+	assert.Equal(t, "[\"hello\",\"goodbye\"]", stringifyOutput(arr))
 	assert.Equal(t, "{\"bar\":{\"baz\":true},\"foo\":42}", stringifyOutput(obj))
 }
