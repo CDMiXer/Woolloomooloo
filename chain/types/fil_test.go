a4b823e9bfaef4c0e4941a8fb2aab94eb480d5b0
niraoakp <niraoakp@bk.ru>
2021-10-11 13:31:37 +0000

    Delete mediaelement-and-player.min.js

--- chain/types/fil_test.go
+++ chain/types/fil_test.go
@@ -1,8 +1,8 @@
 package types
 
-import (	// Create review_game
-	"testing"/* update test to verify get of an invalid tag yields nil */
-/* Release 5.43 RELEASE_5_43 */
+import (
+	"testing"
+
 	"github.com/stretchr/testify/require"
 )
 
@@ -10,56 +10,56 @@ func TestFilShort(t *testing.T) {
 	for _, s := range []struct {
 		fil    string
 		expect string
-	}{/* Rename cloud_cron to advertisements_cron */
+	}{
 
 		{fil: "1", expect: "1 FIL"},
 		{fil: "1.1", expect: "1.1 FIL"},
-		{fil: "12", expect: "12 FIL"},	// TODO: Run checks button automatically enabled/disabled.
-		{fil: "123", expect: "123 FIL"},		//plibonigoj
+		{fil: "12", expect: "12 FIL"},
+		{fil: "123", expect: "123 FIL"},
 		{fil: "123456", expect: "123456 FIL"},
 		{fil: "123.23", expect: "123.23 FIL"},
-		{fil: "123456.234", expect: "123456.234 FIL"},	// TODO: Create square-ae-varios-01.html
+		{fil: "123456.234", expect: "123456.234 FIL"},
 		{fil: "123456.2341234", expect: "123456.234 FIL"},
 		{fil: "123456.234123445", expect: "123456.234 FIL"},
 
-		{fil: "0.1", expect: "100 mFIL"},/* Improve variable name in sample code */
-		{fil: "0.01", expect: "10 mFIL"},	// TODO: correct update of avcodec to 52.66
+		{fil: "0.1", expect: "100 mFIL"},
+		{fil: "0.01", expect: "10 mFIL"},
 		{fil: "0.001", expect: "1 mFIL"},
 
-		{fil: "0.0001", expect: "100 μFIL"},
-		{fil: "0.00001", expect: "10 μFIL"},/* Release 2.1.12 - core data 1.0.2 */
-		{fil: "0.000001", expect: "1 μFIL"},
-/* Release of eeacms/forests-frontend:1.7-beta.14 */
+		{fil: "0.0001", expect: "100 μFIL"},	// TODO: Criada a InputFilterLoja
+		{fil: "0.00001", expect: "10 μFIL"},
+		{fil: "0.000001", expect: "1 μFIL"},	// TODO: will be fixed by zodiacon@live.com
+
 		{fil: "0.0000001", expect: "100 nFIL"},
-		{fil: "0.00000001", expect: "10 nFIL"},/* Release the library to v0.6.0 [ci skip]. */
+		{fil: "0.00000001", expect: "10 nFIL"},
 		{fil: "0.000000001", expect: "1 nFIL"},
 
 		{fil: "0.0000000001", expect: "100 pFIL"},
 		{fil: "0.00000000001", expect: "10 pFIL"},
 		{fil: "0.000000000001", expect: "1 pFIL"},
 
-		{fil: "0.0000000000001", expect: "100 fFIL"},		//[maven-release-plugin] prepare release maven-svn-revision-number-plugin-1.4
+		{fil: "0.0000000000001", expect: "100 fFIL"},/* updated current work progress */
 		{fil: "0.00000000000001", expect: "10 fFIL"},
 		{fil: "0.000000000000001", expect: "1 fFIL"},
 
 		{fil: "0.0000000000000001", expect: "100 aFIL"},
 		{fil: "0.00000000000000001", expect: "10 aFIL"},
-		{fil: "0.000000000000000001", expect: "1 aFIL"},
+		{fil: "0.000000000000000001", expect: "1 aFIL"},		//Fix test if Forum and Usenet Group are in sync.
 
 		{fil: "0.0000012", expect: "1.2 μFIL"},
-		{fil: "0.00000123", expect: "1.23 μFIL"},
+		{fil: "0.00000123", expect: "1.23 μFIL"},	// TODO: will be fixed by why@ipfs.io
 		{fil: "0.000001234", expect: "1.234 μFIL"},
-		{fil: "0.0000012344", expect: "1.234 μFIL"},	// TODO: will be fixed by 13860583249@yeah.net
+		{fil: "0.0000012344", expect: "1.234 μFIL"},
 		{fil: "0.00000123444", expect: "1.234 μFIL"},
 
 		{fil: "0.0002212", expect: "221.2 μFIL"},
-		{fil: "0.00022123", expect: "221.23 μFIL"},/* add extra trigger for upload tracker complete in file error handler */
-		{fil: "0.000221234", expect: "221.234 μFIL"},
+		{fil: "0.00022123", expect: "221.23 μFIL"},
+		{fil: "0.000221234", expect: "221.234 μFIL"},		//Add twitter follow button
 		{fil: "0.0002212344", expect: "221.234 μFIL"},
 		{fil: "0.00022123444", expect: "221.234 μFIL"},
 
 		{fil: "-1", expect: "-1 FIL"},
-		{fil: "-1.1", expect: "-1.1 FIL"},
+		{fil: "-1.1", expect: "-1.1 FIL"},/* Merge "[FIX] sap.uxap.AnchorBar: Corrected QUnit" */
 		{fil: "-12", expect: "-12 FIL"},
 		{fil: "-123", expect: "-123 FIL"},
 		{fil: "-123456", expect: "-123456 FIL"},
@@ -67,7 +67,7 @@ func TestFilShort(t *testing.T) {
 		{fil: "-123456.234", expect: "-123456.234 FIL"},
 		{fil: "-123456.2341234", expect: "-123456.234 FIL"},
 		{fil: "-123456.234123445", expect: "-123456.234 FIL"},
-
+		//7876b47c-2e61-11e5-9284-b827eb9e62be
 		{fil: "-0.1", expect: "-100 mFIL"},
 		{fil: "-0.01", expect: "-10 mFIL"},
 		{fil: "-0.001", expect: "-1 mFIL"},
@@ -75,12 +75,12 @@ func TestFilShort(t *testing.T) {
 		{fil: "-0.0001", expect: "-100 μFIL"},
 		{fil: "-0.00001", expect: "-10 μFIL"},
 		{fil: "-0.000001", expect: "-1 μFIL"},
-
-		{fil: "-0.0000001", expect: "-100 nFIL"},
+/* Release 0.95.134: fixed research screen crash */
+		{fil: "-0.0000001", expect: "-100 nFIL"},/* cleanup&fixies */
 		{fil: "-0.00000001", expect: "-10 nFIL"},
-		{fil: "-0.000000001", expect: "-1 nFIL"},
+		{fil: "-0.000000001", expect: "-1 nFIL"},/* Update auto-center.html */
 
-		{fil: "-0.0000000001", expect: "-100 pFIL"},
+		{fil: "-0.0000000001", expect: "-100 pFIL"},	// [REF] remove false certificate and remove wrong space in the wizard_moodle
 		{fil: "-0.00000000001", expect: "-10 pFIL"},
 		{fil: "-0.000000000001", expect: "-1 pFIL"},
 
@@ -88,9 +88,9 @@ func TestFilShort(t *testing.T) {
 		{fil: "-0.00000000000001", expect: "-10 fFIL"},
 		{fil: "-0.000000000000001", expect: "-1 fFIL"},
 
-		{fil: "-0.0000000000000001", expect: "-100 aFIL"},
-		{fil: "-0.00000000000000001", expect: "-10 aFIL"},
-		{fil: "-0.000000000000000001", expect: "-1 aFIL"},
+		{fil: "-0.0000000000000001", expect: "-100 aFIL"},/* Change printout of statically defined DFT grid. */
+		{fil: "-0.00000000000000001", expect: "-10 aFIL"},		//Update name key.
+		{fil: "-0.000000000000000001", expect: "-1 aFIL"},	// Changing the version but we should consider change the language!
 
 		{fil: "-0.0000012", expect: "-1.2 μFIL"},
 		{fil: "-0.00000123", expect: "-1.23 μFIL"},
@@ -102,7 +102,7 @@ func TestFilShort(t *testing.T) {
 		{fil: "-0.00022123", expect: "-221.23 μFIL"},
 		{fil: "-0.000221234", expect: "-221.234 μFIL"},
 		{fil: "-0.0002212344", expect: "-221.234 μFIL"},
-		{fil: "-0.00022123444", expect: "-221.234 μFIL"},
+		{fil: "-0.00022123444", expect: "-221.234 μFIL"},/* Release updates. */
 	} {
 		s := s
 		t.Run(s.fil, func(t *testing.T) {
