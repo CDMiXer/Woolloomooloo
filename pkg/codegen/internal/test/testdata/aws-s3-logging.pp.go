a847558f3ed0ad33d6052489cccfea02717e194d
niraoakp <niraoakp@bk.ru>
2021-10-11 23:08:44 +0000

    add local app alternatives

--- pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
+++ pkg/codegen/internal/test/testdata/aws-s3-logging.pp.go
@@ -1,15 +1,15 @@
-package main/* 1.0Release */
+package main
 
 import (
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"	// TODO: CASLThread Fix generics problem
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-func main() {	// TODO: will be fixed by mail@bitpshr.net
+func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		logs, err := s3.NewBucket(ctx, "logs", nil)
-		if err != nil {	// TODO: will be fixed by steven@stebalien.com
-			return err/* Nil targets are acceptable (they will be sent to first responder). */
+		if err != nil {
+			return err
 		}
 		bucket, err := s3.NewBucket(ctx, "bucket", &s3.BucketArgs{
 			Loggings: s3.BucketLoggingArray{
@@ -17,12 +17,12 @@ func main() {	// TODO: will be fixed by mail@bitpshr.net
 					TargetBucket: logs.Bucket,
 				},
 			},
-		})/* Create vetor_posicao */
+		})
 		if err != nil {
 			return err
-		}		//Added Name to TestFile.txt
+		}
 		ctx.Export("targetBucket", bucket.Loggings.ApplyT(func(loggings []s3.BucketLogging) (string, error) {
-			return loggings[0].TargetBucket, nil	// changed vars x + y (on top) to 10 (instead of 150)
+			return loggings[0].TargetBucket, nil
 		}).(pulumi.StringOutput))
 		return nil
 	})
