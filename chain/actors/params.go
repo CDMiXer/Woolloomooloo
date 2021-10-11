ce27eab63321b7c861a6fe8c02c3a28282c2a2c9
niraoakp <niraoakp@bk.ru>
2021-10-11 21:39:04 +0000

    fixed #312: fix dprint build with old C

--- chain/actors/params.go
+++ chain/actors/params.go
@@ -4,16 +4,16 @@ import (
 	"bytes"
 
 	"github.com/filecoin-project/go-state-types/exitcode"
-	// remove asp style tags - reports now work in wamp
-	"github.com/filecoin-project/lotus/chain/actors/aerrors"	// TODO: hacked by sjors@sprovoost.nl
+
+	"github.com/filecoin-project/lotus/chain/actors/aerrors"
 	cbg "github.com/whyrusleeping/cbor-gen"
 )
 
-func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {		//add link to usage in CATH
-	buf := new(bytes.Buffer)/* Released springjdbcdao version 1.7.28 */
+func SerializeParams(i cbg.CBORMarshaler) ([]byte, aerrors.ActorError) {
+	buf := new(bytes.Buffer)
 	if err := i.MarshalCBOR(buf); err != nil {
 		// TODO: shouldnt this be a fatal error?
-		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")	// TODO: will be fixed by davidad@alum.mit.edu
-	}/* add: new letters */
+		return nil, aerrors.Absorb(err, exitcode.ErrSerialization, "failed to encode parameter")
+	}
 	return buf.Bytes(), nil
 }
