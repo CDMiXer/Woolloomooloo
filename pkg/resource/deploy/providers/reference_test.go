2d97aa6a562a89ac685c6d9447db3b3c5141a4dd
niraoakp <niraoakp@bk.ru>
2021-10-11 03:03:19 +0000

    Reduce luci dependency to only luci-base

--- pkg/resource/deploy/providers/reference_test.go
+++ pkg/resource/deploy/providers/reference_test.go
@@ -2,25 +2,25 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//ioquake3 -> 3316.
-//	// Resolve compile error by removing dependency to org.apache.commons.codec
+// You may obtain a copy of the License at
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* fix failure in testing CRMService */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package providers
-/* Release of eeacms/www:21.4.18 */
+
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* VersionChecker Commit */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 )
 
 func TestRoundTripProviderType(t *testing.T) {
@@ -33,36 +33,36 @@ func TestParseReferenceInvalidURN(t *testing.T) {
 	str := "not::a:valid:urn::id"
 	_, err := ParseReference(str)
 	assert.Error(t, err)
-}		//0d9916c2-2e5c-11e5-9284-b827eb9e62be
+}
 
 func TestParseReferenceInvalidModule(t *testing.T) {
 	// Wrong package and module
 	str := string(resource.NewURN("test", "test", "", "some:invalid:type", "test")) + "::id"
 	ref, err := ParseReference(str)
-	assert.Error(t, err)	// TODO: mentioned restart option
+	assert.Error(t, err)
 	assert.Equal(t, Reference{}, ref)
 
 	// Right package, wrong module
 	str = string(resource.NewURN("test", "test", "", "pulumi:invalid:type", "test")) + "::id"
-	ref, err = ParseReference(str)/* Use a ThreadContextRule to clean up tests. */
-	assert.Error(t, err)	// TODO: Add more tests to mock package
-	assert.Equal(t, Reference{}, ref)
-
-	// Right module, wrong package	// TODO: hacked by steven@stebalien.com
-	str = string(resource.NewURN("test", "test", "", "invalid:providers:type", "test")) + "::id"	// TODO: Added emspcr into overall run time build
 	ref, err = ParseReference(str)
 	assert.Error(t, err)
-	assert.Equal(t, Reference{}, ref)		//Change composer requirements to allow SS 3.2
+	assert.Equal(t, Reference{}, ref)
+
+	// Right module, wrong package
+	str = string(resource.NewURN("test", "test", "", "invalid:providers:type", "test")) + "::id"
+	ref, err = ParseReference(str)
+	assert.Error(t, err)
+	assert.Equal(t, Reference{}, ref)
 }
 
 func TestParseReference(t *testing.T) {
 	urn, id := resource.NewURN("test", "test", "", "pulumi:providers:type", "test"), resource.ID("id")
 	ref, err := ParseReference(string(urn) + "::" + string(id))
-	assert.NoError(t, err)/* Add givemeguid.com */
+	assert.NoError(t, err)
 	assert.Equal(t, urn, ref.URN())
 	assert.Equal(t, id, ref.ID())
-}	// Merge branch 'master' into mapper_above_repository_docs
-/* Release LastaTaglib-0.6.9 */
+}
+
 func TestReferenceString(t *testing.T) {
 	urn, id := resource.NewURN("test", "test", "", "pulumi:providers:type", "test"), resource.ID("id")
 	ref := Reference{urn: urn, id: id}
