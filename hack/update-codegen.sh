f31da1f4587acaa43fb07cd3eb73c295b778931a
niraoakp <niraoakp@bk.ru>
2021-10-11 16:53:21 +0000

    Switched to using TRUNCATE for MySQL.

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -4,5 +4,5 @@ set -eux -o pipefail
 bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
   "deepcopy,client,informer,lister" \
   github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \
-  workflow:v1alpha1 \
+  workflow:v1alpha1 \/* Update wdpassport-utils.py */
   --go-header-file ./hack/custom-boilerplate.go.txt
