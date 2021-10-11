8edf46137678bd5d9630f820b35a5b5a0f790891
niraoakp <niraoakp@bk.ru>
2021-10-11 04:59:55 +0000

    Added @fabricioferreira

--- operator/runner/after.go
+++ operator/runner/after.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Improved visible descriptions for a few plugins.
-//
+// Copyright 2019 Drone IO, Inc.
+//		//0accc89e-2e5c-11e5-9284-b827eb9e62be
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: hacked by aeongrp@outlook.com
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* improving documentation for resolvers */
-// Unless required by applicable law or agreed to in writing, software
+//
+// Unless required by applicable law or agreed to in writing, software		//fb79828c-2e43-11e5-9284-b827eb9e62be
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Added lfe-utils */
 package runner
