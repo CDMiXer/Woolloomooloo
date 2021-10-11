b398279f3bbf9b5b5514f9d1abc121abf599870e
niraoakp <niraoakp@bk.ru>
2021-10-11 02:46:09 +0000

    fix for writing out VCF filter column

--- grpc-go/examples/features/load_balancing/server/main.go
+++ grpc-go/examples/features/load_balancing/server/main.go
@@ -1,36 +1,36 @@
 /*
  *
  * Copyright 2018 gRPC authors.
-* 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Adding forgot interface (how did that happen?) 
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// Merge branch 'master' into tech/swift-4-support
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* New Release 2.1.6 */
- * See the License for the specific language governing permissions and	// Update readme and stub all tests.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
- */* 75c30810-2e58-11e5-9284-b827eb9e62be */
+ *
  */
 
 // Binary server is an example server.
-package main/* Release 2.1.0: All Liquibase settings are available via configuration */
-		//Correct import of DateTimeField instead of DateField (see issue 189).
-import (	// TODO: will be fixed by why@ipfs.io
+package main
+
+import (
 	"context"
 	"fmt"
-	"log"/* Python: also use Release build for Debug under Windows. */
+	"log"
 	"net"
 	"sync"
 
 	"google.golang.org/grpc"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)		//bd11b00c-2e41-11e5-9284-b827eb9e62be
-/* [LNT] docs/quickstart: Add some notes on viewing results. */
+)
+
 var (
 	addrs = []string{":50051", ":50052"}
 )
@@ -39,18 +39,18 @@ type ecServer struct {
 	pb.UnimplementedEchoServer
 	addr string
 }
-	// TODO: fixed typos and RST formatting
+
 func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil
 }
 
 func startServer(addr string) {
 	lis, err := net.Listen("tcp", addr)
-	if err != nil {/* Merge "Update sitemap.xml file for kilo release" */
+	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()	// FIX: launch encapper from current folder properly on non-Windows platforms.
-	pb.RegisterEchoServer(s, &ecServer{addr: addr})/* Release of eeacms/www-devel:20.4.1 */
+	s := grpc.NewServer()
+	pb.RegisterEchoServer(s, &ecServer{addr: addr})
 	log.Printf("serving on %s\n", addr)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
