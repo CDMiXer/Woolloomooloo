abaea7386525f0a3119b6972c55f72b0f18faa3e
niraoakp <niraoakp@bk.ru>
2021-10-11 08:26:12 +0000

    6bb277b8-2e47-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/helpers.go
+++ pkg/codegen/internal/test/helpers.go
@@ -2,10 +2,10 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Merge "Fix DescriptionEditActivity layout"
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Released GoogleApis v0.1.5 */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,24 +14,24 @@
 
 package test
 
-import (
-	"encoding/json"
+import (	// TODO: Create tambah_penduduk.py
+	"encoding/json"/* Merge "Updated README.md to be more accurate" */
 	"io/ioutil"
 	"path/filepath"
-	"testing"
+	"testing"	// TODO: will be fixed by greg@colvin.org
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/stretchr/testify/assert"
 )
-
+		//remove dependencies on journey
 // GenPkgSignature corresponds to the shape of the codegen GeneratePackage functions.
 type GenPkgSignature func(string, *schema.Package, map[string][]byte) (map[string][]byte, error)
 
 // GeneratePackageFilesFromSchema loads a schema and generates files using the provided GeneratePackage function.
 func GeneratePackageFilesFromSchema(schemaPath string, genPackageFunc GenPkgSignature) (map[string][]byte, error) {
-	// Read in, decode, and import the schema.
+	// Read in, decode, and import the schema.	// TODO: updated picard tool wrapper for list of specific commands
 	schemaBytes, err := ioutil.ReadFile(schemaPath)
-	if err != nil {
+	if err != nil {	// TODO: Update exercises-4clojure.clj
 		return nil, err
 	}
 
@@ -43,15 +43,15 @@ func GeneratePackageFilesFromSchema(schemaPath string, genPackageFunc GenPkgSign
 
 	pkg, err := schema.ImportSpec(pkgSpec, nil)
 	if err != nil {
-		return nil, err
+		return nil, err/* SceneBuffer: Remove minus sign from 0.0 constants. */
 	}
 
 	return genPackageFunc("test", pkg, nil)
 }
 
-// LoadFiles loads the provided list of files from a directory.
+// LoadFiles loads the provided list of files from a directory./* 1.2.1 Release Changes made by Ken Hh (sipantic@gmail.com). */
 func LoadFiles(dir, lang string, files []string) (map[string][]byte, error) {
-	result := map[string][]byte{}
+	result := map[string][]byte{}/* update of silly-collections dependency version */
 	for _, file := range files {
 		fileBytes, err := ioutil.ReadFile(filepath.Join(dir, lang, file))
 		if err != nil {
@@ -62,11 +62,11 @@ func LoadFiles(dir, lang string, files []string) (map[string][]byte, error) {
 	}
 
 	return result, nil
-}
+}	// Added testPhylipFile
 
 // ValidateFileEquality compares maps of files for equality.
 func ValidateFileEquality(t *testing.T, actual, expected map[string][]byte) {
-	for name, file := range expected {
+	for name, file := range expected {/* update handles to keywords */
 		assert.Contains(t, actual, name)
 		assert.Equal(t, string(file), string(actual[name]), name)
 	}
