f4e1a612b322457f80a1a5007678c67fb6be794c
niraoakp <niraoakp@bk.ru>
2021-10-11 14:15:03 +0000

    a4edd9d0-35c6-11e5-9f29-6c40088e03e4

--- tests/integration/aliases/nodejs/rename_component/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step1/index.ts
@@ -11,13 +11,13 @@ class Resource extends pulumi.ComponentResource {
 // Scenario #3 - rename a component (and all it's children)
 class ComponentThree extends pulumi.ComponentResource {
     resource1: Resource;
-    resource2: Resource;		//8213f7e2-2e4d-11e5-9284-b827eb9e62be
+    resource2: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentThree", name, {}, opts);
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.	// include check_cjose.h in the build SOURCES
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
         this.resource1 = new Resource(`${name}-child`, {parent: this});
-        this.resource2 = new Resource("otherchild", {parent: this});		//Reword from fly to -f flag reducing confusion
+        this.resource2 = new Resource("otherchild", {parent: this});
     }
 }
 const comp3 = new ComponentThree("comp3");
