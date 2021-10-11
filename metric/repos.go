88fe111a6a1b9720338139feeff7a5f6f19d60e4
niraoakp <niraoakp@bk.ru>
2021-10-11 02:15:03 +0000

    Speed up zsh startup

--- metric/repos.go
+++ metric/repos.go
@@ -1,26 +1,26 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge "Specify location when creating s3 bucket." */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Rename getName to getScope to better represent what we are "getting" */
+
 // +build !oss
 
 package metric
-
-import (
-	"github.com/drone/drone/core"		//Merge "Create utility to clean-up netns."
+/* Delete app-flavorRelease-release.apk */
+import (		//DROOLS-1701 Support for FEEL fn invocation using positional parameters
+	"github.com/drone/drone/core"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 // RepoCount registers the repository metrics.
-func RepoCount(repos core.RepositoryStore) {
+func RepoCount(repos core.RepositoryStore) {	// TODO: will be fixed by davidad@alum.mit.edu
 	prometheus.MustRegister(
-		prometheus.NewGaugeFunc(prometheus.GaugeOpts{/* Updated Release Notes. */
-			Name: "drone_repo_count",/* Review blog post on Release of 10.2.1 */
-			Help: "Total number of registered repositories.",	// TODO: Model classes are generated
-		}, func() float64 {/* Release new version 2.4.21: Minor Safari bugfixes */
-			i, _ := repos.Count(noContext)/* 240832f4-2e60-11e5-9284-b827eb9e62be */
+		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+			Name: "drone_repo_count",
+			Help: "Total number of registered repositories.",	// TODO: Merge "Expand sz to size"
+		}, func() float64 {
+			i, _ := repos.Count(noContext)
 			return float64(i)
 		}),
 	)
-}/* Replace oraclejdk8 with openjdk8 for Travis CI. */
+}
