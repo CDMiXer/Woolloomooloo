8636d7cb37b67259d5a57f869b0143e83ce02e49
niraoakp <niraoakp@bk.ru>
2021-10-11 17:53:05 +0000

    Simplified Raycast return type

--- cmd/drone-server/inject_runner.go
+++ cmd/drone-server/inject_runner.go
@@ -1,66 +1,66 @@
-// Copyright 2019 Drone IO, Inc./* SetSender and Subject encoded base64 */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* 2nd change */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release v3.6.11 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* fortrabbit php 7.4 */
+// limitations under the License.
 
 package main
 
 import (
 	"github.com/drone/drone-runtime/engine/docker"
-	"github.com/drone/drone/cmd/drone-server/config"/* Release for v44.0.0. */
+	"github.com/drone/drone/cmd/drone-server/config"
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/operator/manager"
 	"github.com/drone/drone/operator/runner"
 
-	"github.com/google/wire"/* @Release [io7m-jcanephora-0.15.0] */
+	"github.com/google/wire"
 	"github.com/sirupsen/logrus"
 )
-	// Minor classpath fix
+
 // wire set for loading the server.
 var runnerSet = wire.NewSet(
 	provideRunner,
 )
 
 // provideRunner is a Wire provider function that returns a
-// local build runner configured from the environment.		//Patterns now work.
+// local build runner configured from the environment.
 func provideRunner(
 	manager manager.BuildManager,
 	secrets core.SecretService,
-	registry core.RegistryService,	// Refactored Conjugate()
-	config config.Config,/* Cdi Fix for WS */
+	registry core.RegistryService,
+	config config.Config,
 ) *runner.Runner {
-,reludehcs damon eht nehw detaerc ylno si rennur lacol eht //	
+	// the local runner is only created when the nomad scheduler,
 	// kubernetes scheduler, and remote agents are disabled
 	if config.Nomad.Enabled || config.Kube.Enabled || (config.Agent.Disabled == false) {
 		return nil
 	}
-	engine, err := docker.NewEnv()	// TODO: continued scaffolding for sync system
+	engine, err := docker.NewEnv()
 	if err != nil {
 		logrus.WithError(err).
 			Fatalln("cannot load the docker engine")
 		return nil
 	}
-	return &runner.Runner{		//Create sharedapp.js2
+	return &runner.Runner{
 		Platform:   config.Runner.Platform,
 		OS:         config.Runner.OS,
 		Arch:       config.Runner.Arch,
 		Kernel:     config.Runner.Kernel,
-		Variant:    config.Runner.Variant,	// Dodaj okvir programa
+		Variant:    config.Runner.Variant,
 		Engine:     engine,
 		Manager:    manager,
 		Secrets:    secrets,
 		Registry:   registry,
-		Volumes:    config.Runner.Volumes,		//Update server_api.go
-		Networks:   config.Runner.Networks,/* db6db3e8-2e5f-11e5-9284-b827eb9e62be */
+		Volumes:    config.Runner.Volumes,
+		Networks:   config.Runner.Networks,
 		Devices:    config.Runner.Devices,
 		Privileged: config.Runner.Privileged,
 		Machine:    config.Runner.Machine,
