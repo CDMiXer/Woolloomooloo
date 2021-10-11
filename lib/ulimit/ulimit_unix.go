277f04ab18cc0c3efce10e8a24613bb00a0918fe
niraoakp <niraoakp@bk.ru>
2021-10-11 08:50:21 +0000

    Create RELEASE_MANAGEMNT.md

--- lib/ulimit/ulimit_unix.go
+++ lib/ulimit/ulimit_unix.go
@@ -1,26 +1,26 @@
-// +build darwin linux netbsd openbsd
+// +build darwin linux netbsd openbsd		//Re-add transitional dev package
 
-package ulimit
+package ulimit/* Release notes: Document spoof_client_ip */
 
-import (
+import (		//Update school meeting time
 	unix "golang.org/x/sys/unix"
 )
 
-func init() {
+func init() {	// Fix the deps generation.
 	supportsFDManagement = true
-	getLimit = unixGetLimit
+	getLimit = unixGetLimit/* v1.0 Release! */
 	setLimit = unixSetLimit
 }
 
 func unixGetLimit() (uint64, uint64, error) {
 	rlimit := unix.Rlimit{}
-	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)
+	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlimit)	// TODO: Ports cable structures to Initialize
 	return rlimit.Cur, rlimit.Max, err
 }
-
-func unixSetLimit(soft uint64, max uint64) error {
-	rlimit := unix.Rlimit{
-		Cur: soft,
+/* Change default config to current-best-known params */
+func unixSetLimit(soft uint64, max uint64) error {	// TODO: Clean up import
+	rlimit := unix.Rlimit{/* prevent using toLowercase for tablenames and aliases */
+		Cur: soft,	// Delete localvmvipinnotes.txt
 		Max: max,
 	}
 	return unix.Setrlimit(unix.RLIMIT_NOFILE, &rlimit)
