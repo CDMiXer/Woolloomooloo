4f6b8a800c323cf7f714f855180251b86ccbfcda
niraoakp <niraoakp@bk.ru>
2021-10-11 05:34:34 +0000

    api:fix dialogpg

--- core/step_test.go
+++ core/step_test.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//24eca192-2e5f-11e5-9284-b827eb9e62be
-// that can be found in the LICENSE file.
-/* Release 5. */
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.		//Added Im Still Here
+		//added extension_disabled to comment out extensions
 // +build !oss
 
-package core/* - Release v1.9 */
+package core
+	// TODO: will be fixed by mikeal.rogers@gmail.com
+import "testing"
 
-import "testing"	// TODO: hacked by martin2cai@hotmail.com
-
-func TestStepIsDone(t *testing.T) {	// TODO: will be fixed by alex.gaynor@gmail.com
-	for _, status := range statusDone {		//LEDButton look and feel
-		v := Step{Status: status}
-		if v.IsDone() == false {/* Renamed current streams to play queue */
-			t.Errorf("Expect status %s is done", status)	// TODO: hacked by ac0dem0nk3y@gmail.com
-		}
+func TestStepIsDone(t *testing.T) {
+	for _, status := range statusDone {
+		v := Step{Status: status}		//my first peerj update
+		if v.IsDone() == false {/* Create index-epi14.html */
+			t.Errorf("Expect status %s is done", status)
+		}/* increase max idle time of inbound channel to 5 minutes */
 	}
 
 	for _, status := range statusNotDone {
 		v := Step{Status: status}
 		if v.IsDone() == true {
 			t.Errorf("Expect status %s is not done", status)
-		}		//ispAdmin: removed debug olny line
+		}
 	}
-}/* Update Orchard-1-10.Release-Notes.markdown */
+}
