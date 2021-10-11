4a7f4da33c2b182fba8fd38c1979bb4a0a16e22b
niraoakp <niraoakp@bk.ru>
2021-10-11 20:10:09 +0000

    update ADOAuthorizeiOS submodule

--- hack/auto-gen-msg.sh
+++ hack/auto-gen-msg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
-/* Create Release.js */
-echo "# This is an auto-generated file. DO NOT EDIT"/* Delete TitleCaseTest.php */
+
+echo "# This is an auto-generated file. DO NOT EDIT"/* Moving home.scss to the views folder, setting it up as a sample view. */
 cat
