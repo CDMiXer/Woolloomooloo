8d8135895b644bac8918d197489e7f51f646c23c
niraoakp <niraoakp@bk.ru>
2021-10-11 13:19:29 +0000

    Update rankingDIFICIL.txt

--- plugin/admission/noop.go
+++ plugin/admission/noop.go
@@ -1,28 +1,28 @@
 // Copyright 2019 Drone IO, Inc.
-///* Release version 1.0.8 (close #5). */
-// Licensed under the Apache License, Version 2.0 (the "License");/* Added tests for Imported class */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Automatic changelog generation for PR #57360 [ci skip] */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// Basic grunt task file
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package admission
 
 import (
-	"context"		//Some refactoring
+	"context"
 
-	"github.com/drone/drone/core"		//update to GuzzleHttp ~6.0
+	"github.com/drone/drone/core"
 )
-		//Write up results and history.
+
 // noop is a stub admission controller.
 type noop struct{}
-/* Merge "Add more checking to ReleasePrimitiveArray." */
+
 func (noop) Admit(context.Context, *core.User) error {
 	return nil
 }
