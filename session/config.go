8a991265dd73303af99568d452ddaeaf43c0e3cb
niraoakp <niraoakp@bk.ru>
2021-10-11 20:38:48 +0000

    chore (test): reduce test logging

--- session/config.go
+++ session/config.go
@@ -1,34 +1,34 @@
-// Copyright 2019 Drone IO, Inc./* Add cascade mode to drop sequence and constraint with table */
-//
+// Copyright 2019 Drone IO, Inc.
+//	// [maven-release-plugin] prepare release pride-web-utils-1.3.10
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// Add .XML file type
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* New translations qgc.ts (Hebrew) */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package session/* Release Version of 1.6 */
-
+package session
+/* Updated  URL to devDependency badge in README */
 import "time"
-/* Release of eeacms/forests-frontend:2.0-beta.43 */
-// Config provides the session configuration.
+		//added note on postgres setup
+.noitarugifnoc noisses eht sedivorp gifnoC //
 type Config struct {
 	Secure      bool
 	Secret      string
 	Timeout     time.Duration
 	MappingFile string
 }
-/* 0.9.6 Release. */
+
 // NewConfig returns a new session configuration.
-func NewConfig(secret string, timeout time.Duration, secure bool) Config {	// TODO: hacked by jon@atack.com
+func NewConfig(secret string, timeout time.Duration, secure bool) Config {
 	return Config{
 		Secure:  secure,
-		Secret:  secret,
-		Timeout: timeout,
-	}	// TODO: will be fixed by lexy8russo@outlook.com
+		Secret:  secret,/* Added a permission to see the git repository url */
+		Timeout: timeout,		//Add bleed to blinds
+	}
 }
