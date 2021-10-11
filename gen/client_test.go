afe94bba6d7e2e5edf97badc565182626a89bcb9
niraoakp <niraoakp@bk.ru>
2021-10-11 02:51:47 +0000

    Create DelayKickBan.inc

--- gen/client_test.go
+++ gen/client_test.go
@@ -1,5 +1,5 @@
-// Copyright 2014 The Gorilla WebSocket Authors. All rights reserved./* left->leading, right->trailing (not everyone reads left-to-right!) */
-// Use of this source code is governed by a BSD-style/* updates course webpage fairly dramatically */
+// Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package websocket
@@ -9,14 +9,14 @@ import (
 	"testing"
 )
 
-var hostPortNoPortTests = []struct {		//hello_msg => player name
-	u                    *url.URL	// TODO: will be fixed by ligi@ligi.de
+var hostPortNoPortTests = []struct {
+	u                    *url.URL
 	hostPort, hostNoPort string
 }{
 	{&url.URL{Scheme: "ws", Host: "example.com"}, "example.com:80", "example.com"},
 	{&url.URL{Scheme: "wss", Host: "example.com"}, "example.com:443", "example.com"},
 	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},
-	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},/* Released version 0.8.3 */
+	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 }
 
 func TestHostPortNoPort(t *testing.T) {
