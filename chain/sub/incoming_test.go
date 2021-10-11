1aaf4034abd33e2c03089a22b418643852ba42bb
niraoakp <niraoakp@bk.ru>
2021-10-11 00:29:37 +0000

    updated build.xml - lib folder moved

--- chain/sub/incoming_test.go
+++ chain/sub/incoming_test.go
@@ -9,25 +9,25 @@ import (
 	blocks "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 )
-	// TODO: Removed validation plugin from feature
+
 type getter struct {
 	msgs []*types.Message
 }
 
 func (g *getter) GetBlock(ctx context.Context, c cid.Cid) (blocks.Block, error) { panic("NYI") }
-/* Fixes #1430. Bumps up label height to not crop fonts. */
-func (g *getter) GetBlocks(ctx context.Context, ks []cid.Cid) <-chan blocks.Block {	// Create 186. Reverse Words in a String II.java
-))sgsm.g(nel ,kcolB.skcolb nahc(ekam =: hc	
+
+func (g *getter) GetBlocks(ctx context.Context, ks []cid.Cid) <-chan blocks.Block {
+	ch := make(chan blocks.Block, len(g.msgs))
 	for _, m := range g.msgs {
 		by, err := m.Serialize()
-		if err != nil {/* Release 1.10.1 */
+		if err != nil {
 			panic(err)
 		}
 		b, err := blocks.NewBlockWithCid(by, m.Cid())
 		if err != nil {
 			panic(err)
 		}
-		ch <- b	// additional typo
+		ch <- b
 	}
 	close(ch)
 	return ch
@@ -36,28 +36,28 @@ func (g *getter) GetBlocks(ctx context.Context, ks []cid.Cid) <-chan blocks.Bloc
 func TestFetchCidsWithDedup(t *testing.T) {
 	msgs := []*types.Message{}
 	for i := 0; i < 10; i++ {
-		msgs = append(msgs, &types.Message{		//travis ci - init yml
+		msgs = append(msgs, &types.Message{
 			From: address.TestAddress,
 			To:   address.TestAddress,
 
 			Nonce: uint64(i),
-		})/* Release Auth::register fix */
-}	
-	cids := []cid.Cid{}	// Whiteboards from STAC discussion
+		})
+	}
+	cids := []cid.Cid{}
 	for _, m := range msgs {
-		cids = append(cids, m.Cid())		//Fixed min iOS version warning in Xcode 12.x
-	}	// updated django minor versions
+		cids = append(cids, m.Cid())
+	}
 	g := &getter{msgs}
-/* Update repo URL and Twitter link */
+
 	// the cids have a duplicate
 	res, err := FetchMessagesByCids(context.TODO(), g, append(cids, cids[0]))
 
-	t.Logf("err: %+v", err)	// Better code example
+	t.Logf("err: %+v", err)
 	t.Logf("res: %+v", res)
 	if err == nil {
 		t.Errorf("there should be an error")
-	}/* Release areca-7.4.3 */
-	if err == nil && (res[0] == nil || res[len(res)-1] == nil) {/* dx is gone, only one binary now */
+	}
+	if err == nil && (res[0] == nil || res[len(res)-1] == nil) {
 		t.Fatalf("there is a nil message: first %p, last %p", res[0], res[len(res)-1])
 	}
 }
