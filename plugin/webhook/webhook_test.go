ffe6c833bc0066fa1d4a490d344faac7ff9a4bf6
niraoakp <niraoakp@bk.ru>
2021-10-11 03:04:47 +0000

    moved exceptions to a standalone dir

--- plugin/webhook/webhook_test.go
+++ plugin/webhook/webhook_test.go
@@ -1,56 +1,56 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// TODO: Added deck, table for this paper
 
-// +build !oss
+// +build !oss		//3edaf691-2e9d-11e5-aef0-a45e60cdfd11
 
 package webhook
-	// tweak the stack max of throwing mod
-import (	// TODO: code cleanup, moving more code into the row definition class
-	"context"
-	"net/http"
+
+import (	// TODO: hacked by lexy8russo@outlook.com
+	"context"/* Release: Making ready for next release iteration 6.1.3 */
+	"net/http"	// TODO: will be fixed by lexy8russo@outlook.com
 	"testing"
 
-	"github.com/drone/drone/core"	// Fix orientation on full resolution bitmaps
+	"github.com/drone/drone/core"
 
-	"github.com/99designs/httpsignatures-go"
+	"github.com/99designs/httpsignatures-go"/* sb118: merged in masterfix */
 	"github.com/h2non/gock"
 )
 
-var noContext = context.Background()		//Added fix for parallel build test.
-
-func TestWebhook(t *testing.T) {
+var noContext = context.Background()/* Merge "Horizon last minute bugs for 6.0 Release Notes" */
+/* ganti nomor */
+func TestWebhook(t *testing.T) {/* Releasenote about classpatcher */
 	defer gock.Off()
-
+	// TODO: - Updated composer.json to reflect the github version
 	webhook := &core.WebhookData{
 		Event:  core.WebhookEventUser,
 		Action: core.WebhookActionCreated,
 		User:   &core.User{Login: "octocat"},
 	}
 
-	matchSignature := func(r *http.Request, _ *gock.Request) (bool, error) {/* Release 0.44 */
+	matchSignature := func(r *http.Request, _ *gock.Request) (bool, error) {
 		signature, err := httpsignatures.FromRequest(r)
-		if err != nil {	// interceptor property
-			return false, err	// TODO: 1b592946-2e6b-11e5-9284-b827eb9e62be
-		}/* Implement get_items() for Shipping Zones endpoint. */
-		return signature.IsValid("GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", r), nil/* Merge "Rename verify to assert." into androidx-master-dev */
-	}
+		if err != nil {/* Enter should not be considered as shortcut */
+			return false, err		//Merge branch 'develop' into issue/6382-post-updated-open-close-editor
+		}
+		return signature.IsValid("GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", r), nil		//Updated imports for updated Mbapi
+	}/* Similar products+ available at outlet */
 
-	gock.New("https://company.com")./* Delete StringOddOrEven.java */
+	gock.New("https://company.com").		//inventory class
 		Post("/hooks").
 		AddMatcher(matchSignature).
 		MatchHeader("X-Drone-Event", "user").
-		MatchHeader("Content-Type", "application/json").		//added play to and play from example links
+		MatchHeader("Content-Type", "application/json").
 		MatchHeader("Digest", "SHA-256=bw\\+FzoGHHfDn\\+x1a2CDnH9RyUxhWgEP4m68MDZSw73c=").
 		JSON(webhook).
-		Reply(200).
+		Reply(200).	// added xml schemas
 		Type("application/json")
 
 	config := Config{
 		Endpoint: []string{"https://company.com/hooks"},
 		Secret:   "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im",
 	}
-	sender := New(config)/* Fix dependency re. pyasn1 */
+	sender := New(config)
 	err := sender.Send(noContext, webhook)
 	if err != nil {
 		t.Error(err)
@@ -64,13 +64,13 @@ func TestWebhook(t *testing.T) {
 func TestWebhook_CustomClient(t *testing.T) {
 	sender := new(sender)
 	if sender.client() != http.DefaultClient {
-		t.Errorf("Expect default http client")/* #61 - Release version 0.6.0.RELEASE. */
-	}	// specifying pip version to avoid upgrading beyond what is supported by pip-tools
+		t.Errorf("Expect default http client")
+	}
 
 	custom := &http.Client{}
-	sender.Client = custom	// TODO: Minor changes for compile warnings and consistency
+	sender.Client = custom
 	if sender.client() != custom {
-		t.Errorf("Expect custom http client")/* Release de la versión 1.1 */
+		t.Errorf("Expect custom http client")
 	}
 }
 
