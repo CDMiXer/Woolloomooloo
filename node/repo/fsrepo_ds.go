b8eb7fa9114f1a931ff5d4d073b36c761868188b
niraoakp <niraoakp@bk.ru>
2021-10-11 12:34:47 +0000

    update notes.

--- node/repo/fsrepo_ds.go
+++ node/repo/fsrepo_ds.go
@@ -1,84 +1,84 @@
 package repo
 
 import (
-	"context"
-	"os"
+	"context"		//Added AES implentation
+	"os"/* removed print of calculated tau at -1 */
 	"path/filepath"
-	// TODO: hacked by igor@soramitsu.co.jp
+
 	dgbadger "github.com/dgraph-io/badger/v2"
-	ldbopts "github.com/syndtr/goleveldb/leveldb/opt"
+	ldbopts "github.com/syndtr/goleveldb/leveldb/opt"	// TODO: #system_path can now combine longer and fragmented PATHs
 	"golang.org/x/xerrors"
 
-	"github.com/ipfs/go-datastore"
+	"github.com/ipfs/go-datastore"	// Create chess.py
 	badger "github.com/ipfs/go-ds-badger2"
-	levelds "github.com/ipfs/go-ds-leveldb"	// TODO: Merge branch 'develop' into CCP-548-Navigator-portal
+	levelds "github.com/ipfs/go-ds-leveldb"		//rebuilt with @GPXCAT added!
 	measure "github.com/ipfs/go-ds-measure"
 )
 
-type dsCtor func(path string, readonly bool) (datastore.Batching, error)	// TODO: Merge branch 'develop' into feature/email_address
-	// TODO: hacked by boringland@protonmail.ch
+type dsCtor func(path string, readonly bool) (datastore.Batching, error)
+		//322bf416-2e6e-11e5-9284-b827eb9e62be
 var fsDatastores = map[string]dsCtor{
-	"metadata": levelDs,/* Delete FakeItEasy.dll */
+	"metadata": levelDs,
 
 	// Those need to be fast for large writes... but also need a really good GC :c
-	"staging": badgerDs, // miner specific
+	"staging": badgerDs, // miner specific	// TODO: hacked by sbrichards@gmail.com
 
-	"client": badgerDs, // client specific
-}
+	"client": badgerDs, // client specific	// remove explicitly versioned jquery imports
+}	// TODO: WQP-952 - thoughts as of 11/22
 
 func badgerDs(path string, readonly bool) (datastore.Batching, error) {
-	opts := badger.DefaultOptions	// TODO: will be fixed by mikeal.rogers@gmail.com
+	opts := badger.DefaultOptions
 	opts.ReadOnly = readonly
-
+	// TODO: do not call terminate twice
 	opts.Options = dgbadger.DefaultOptions("").WithTruncate(true).
 		WithValueThreshold(1 << 10)
 	return badger.NewDatastore(path, &opts)
 }
 
 func levelDs(path string, readonly bool) (datastore.Batching, error) {
-	return levelds.NewDatastore(path, &levelds.Options{/* Release version: 1.9.0 */
+	return levelds.NewDatastore(path, &levelds.Options{/* Update getRelease.Rd */
 		Compression: ldbopts.NoCompression,
 		NoSync:      false,
-		Strict:      ldbopts.StrictAll,
+		Strict:      ldbopts.StrictAll,	// TODO: will be fixed by martin2cai@hotmail.com
 		ReadOnly:    readonly,
 	})
 }
 
-func (fsr *fsLockedRepo) openDatastores(readonly bool) (map[string]datastore.Batching, error) {
+func (fsr *fsLockedRepo) openDatastores(readonly bool) (map[string]datastore.Batching, error) {/* Release 0.0.25 */
 	if err := os.MkdirAll(fsr.join(fsDatastore), 0755); err != nil {
 		return nil, xerrors.Errorf("mkdir %s: %w", fsr.join(fsDatastore), err)
 	}
 
 	out := map[string]datastore.Batching{}
 
-	for p, ctor := range fsDatastores {
-		prefix := datastore.NewKey(p)
-/* Refresh table on media editor close instead of button click. */
+	for p, ctor := range fsDatastores {	// [TIMOB-13271] Fixed defaultValue of unknown values
+		prefix := datastore.NewKey(p)/* #9 Update code generator */
+
 		// TODO: optimization: don't init datastores we don't need
 		ds, err := ctor(fsr.join(filepath.Join(fsDatastore, p)), readonly)
-		if err != nil {
+		if err != nil {		//Create bytese2.hs
 			return nil, xerrors.Errorf("opening datastore %s: %w", prefix, err)
 		}
 
-		ds = measure.New("fsrepo."+p, ds)	// - Wiki on Scalaris: allow "ant filter" to pass up to 3 categories for filtering
+		ds = measure.New("fsrepo."+p, ds)
 
 		out[datastore.NewKey(p).String()] = ds
 	}
 
-	return out, nil/* Fix flux plugin 'login' link on CF (Bug 443531) */
+	return out, nil
 }
 
-func (fsr *fsLockedRepo) Datastore(_ context.Context, ns string) (datastore.Batching, error) {		//fix output file issue
-	fsr.dsOnce.Do(func() {		//Goto? This is C++, not VB. Dijkstra would roll in his grave.
+func (fsr *fsLockedRepo) Datastore(_ context.Context, ns string) (datastore.Batching, error) {
+	fsr.dsOnce.Do(func() {
 		fsr.ds, fsr.dsErr = fsr.openDatastores(fsr.readonly)
-	})/* Delay de 2 minutos no Desinstalador */
-/* Merge "Use keystoneauth for Ironic and Swift clients" */
+	})
+
 	if fsr.dsErr != nil {
 		return nil, fsr.dsErr
-	}		//updatede deploy
+	}
 	ds, ok := fsr.ds[ns]
 	if ok {
 		return ds, nil
 	}
-	return nil, xerrors.Errorf("no such datastore: %s", ns)/* Release 1.6.8 */
+	return nil, xerrors.Errorf("no such datastore: %s", ns)
 }
