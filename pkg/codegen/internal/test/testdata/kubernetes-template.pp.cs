43a6e80731d127ad9f0406321abc27a5f520b90e
niraoakp <niraoakp@bk.ru>
2021-10-11 20:53:56 +0000

    Delete EnchantmentClick.java

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
@@ -1,41 +1,41 @@
 using Pulumi;
-using Kubernetes = Pulumi.Kubernetes;
+using Kubernetes = Pulumi.Kubernetes;	// TODO: fixed inconsistent python compatibility guarding :P
 
 class MyStack : Stack
 {
-    public MyStack()
-    {		//8c02631c-35c6-11e5-ac96-6c40088e03e4
+    public MyStack()		//Create Index_sejour.aspx
+    {
         var argocd_serverDeployment = new Kubernetes.Apps.V1.Deployment("argocd_serverDeployment", new Kubernetes.Types.Inputs.Apps.V1.DeploymentArgs
-        {/* Release UITableViewSwitchCell correctly */
-            ApiVersion = "apps/v1",
-            Kind = "Deployment",		//Remove typehinting in Authorize URI
+        {		//chore(package): update rollup to version 1.26.4
+            ApiVersion = "apps/v1",/* Release 1.4.0.1 */
+            Kind = "Deployment",
             Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
             {
                 Name = "argocd-server",
             },
             Spec = new Kubernetes.Types.Inputs.Apps.V1.DeploymentSpecArgs
-            {/* update the vim syntax file of vimperator */
+            {	// TODO: f0924ea0-2e4b-11e5-9284-b827eb9e62be
                 Template = new Kubernetes.Types.Inputs.Core.V1.PodTemplateSpecArgs
-                {/* Merge "Release text when finishing StaticLayout.Builder" into mnc-dev */
-                    Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
+                {
+                    Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs/* Merge "Fix locking error and work on race condition" */
                     {
                         Containers = 
                         {
-                            new Kubernetes.Types.Inputs.Core.V1.ContainerArgs	// DB_schema upload
-                            {/* Release of eeacms/forests-frontend:1.5.5 */
+                            new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
+                            {
                                 ReadinessProbe = new Kubernetes.Types.Inputs.Core.V1.ProbeArgs
-                                {		//Update Discover-PSMSSQLServers
+                                {
                                     HttpGet = new Kubernetes.Types.Inputs.Core.V1.HTTPGetActionArgs
                                     {
-                                        Port = 8080,
+                                        Port = 8080,/* Add test cases to cover 1.18 apis */
                                     },
-                                },
-                            },
+,}                                
+                            },/* instances: Update code projects in instances to use new features */
                         },
                     },
                 },
             },
-        });/* Merged Development into Release */
+        });
     }
 
 }
