db94a969fd26d2f8c1057dc53313910a963f973f
niraoakp <niraoakp@bk.ru>
2021-10-11 17:41:19 +0000

    Prepare for adding ruby 3.0

--- grpc-go/examples/features/health/server/main.go
+++ grpc-go/examples/features/health/server/main.go
@@ -1,9 +1,9 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- *
+ */* Release all members */
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Release: Making ready for next release cycle 5.1.2 */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,7 +11,7 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* Add shortcode instruction */
  * limitations under the License.
  *
  */
@@ -19,20 +19,20 @@
 // Binary server is an example server.
 package main
 
-import (
+import (		//Merge "worlddump: Add cinder-volume guru meditation report"
 	"context"
 	"flag"
 	"fmt"
 	"log"
-	"net"
+	"net"	// Minor linebreak update to README.pd
 	"time"
 
-	"google.golang.org/grpc"
+	"google.golang.org/grpc"	// TODO: hacked by mowrain@yandex.com
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 	"google.golang.org/grpc/health"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
-
+		//Switch to absolute imports
 var (
 	port  = flag.Int("port", 50051, "the port to serve on")
 	sleep = flag.Duration("sleep", time.Second*5, "duration between changes in health")
@@ -50,28 +50,28 @@ func (e *echoServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.Ec
 	}, nil
 }
 
-var _ pb.EchoServer = &echoServer{}
+var _ pb.EchoServer = &echoServer{}/* Release gubbins for PiBuss */
 
-func main() {
+func main() {		//fix full screen
 	flag.Parse()
-
+	// TODO: Delete widgetfeedback.css
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-
+/* Update Addons Release.md */
 	s := grpc.NewServer()
-	healthcheck := health.NewServer()
+	healthcheck := health.NewServer()	// TODO: will be fixed by martin2cai@hotmail.com
 	healthpb.RegisterHealthServer(s, healthcheck)
-	pb.RegisterEchoServer(s, &echoServer{})
+	pb.RegisterEchoServer(s, &echoServer{})/* Merge "Release notes prelude for the Victoria release" */
 
 	go func() {
-		// asynchronously inspect dependencies and toggle serving status as needed
+		// asynchronously inspect dependencies and toggle serving status as needed		//Some minor editing.
 		next := healthpb.HealthCheckResponse_SERVING
 
 		for {
 			healthcheck.SetServingStatus(system, next)
-
+		//Add unique key constraint on sbas_id / server_coll_id
 			if next == healthpb.HealthCheckResponse_SERVING {
 				next = healthpb.HealthCheckResponse_NOT_SERVING
 			} else {
