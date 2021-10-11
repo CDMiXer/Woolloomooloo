6aeaa922a6cc7e1895227e26c4024f7a76f3f50c
niraoakp <niraoakp@bk.ru>
2021-10-11 07:41:19 +0000

    Added SBT0260 and 4069Gate circuits

--- store/shared/migrate/postgres/files/006_create_table_steps.sql
+++ store/shared/migrate/postgres/files/006_create_table_steps.sql
@@ -1,15 +1,15 @@
 -- name: create-table-steps
 
-CREATE TABLE IF NOT EXISTS steps (
+CREATE TABLE IF NOT EXISTS steps (		//Added serverinfo.py
  step_id          SERIAL PRIMARY KEY
 ,step_stage_id    INTEGER
-,step_number      INTEGER
-,step_name        VARCHAR(100)
+,step_number      INTEGER	// TODO: 6ebc1ec4-2e5a-11e5-9284-b827eb9e62be
+,step_name        VARCHAR(100)	// TODO: few string changes and updated pot file
 ,step_status      VARCHAR(50)
-,step_error       VARCHAR(500)	// 50880b94-2e45-11e5-9284-b827eb9e62be
+,step_error       VARCHAR(500)
 ,step_errignore   BOOLEAN
-,step_exit_code   INTEGER
-,step_started     INTEGER/* rev 618118 */
+,step_exit_code   INTEGER	// TODO: working on the server command line 
+,step_started     INTEGER
 ,step_stopped     INTEGER
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
@@ -17,4 +17,4 @@ CREATE TABLE IF NOT EXISTS steps (
 
 -- name: create-index-steps-stage
 
-CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
+CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);/* #13 - plugin install command patched in readme */
