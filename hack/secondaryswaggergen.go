283f7976ba3a2ede01ff12d17489ecf7cf50f704
niraoakp <niraoakp@bk.ru>
2021-10-11 19:49:32 +0000

    Resuming work on this...

--- hack/secondaryswaggergen.go
+++ hack/secondaryswaggergen.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
+	"encoding/json"	// TODO: Delete moviesIdDuplicates
+	"io/ioutil"/* Add Kimono Desktop Releases v1.0.5 (#20693) */
 	"strings"
-
+	// TODO: hacked by arajasek94@gmail.com
 	"github.com/go-openapi/jsonreference"
 	"github.com/go-openapi/spec"
 
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-)
+)/* added callstack view to plugineditor */
 
 /*
-	The GRPC code generation does not correctly support "inline". So we generate a secondary swagger (which is lower
+	The GRPC code generation does not correctly support "inline". So we generate a secondary swagger (which is lower/* Release 1.6.12 */
 	priority than the primary) to interject the correctly generated types.
 
 	We do some hackerey here too:
@@ -20,14 +20,14 @@ import (
 	* Change "/" into "." in names.
 */
 func secondarySwaggerGen() {
-	definitions := make(map[string]interface{})
-	for n, d := range wfv1.GetOpenAPIDefinitions(func(path string) spec.Ref {
+	definitions := make(map[string]interface{})	// TODO: Descriptions and Nar-Sie rune improvements
+	for n, d := range wfv1.GetOpenAPIDefinitions(func(path string) spec.Ref {/* Merge "net: core: Release neigh lock when neigh_probe is enabled" */
 		return spec.Ref{
 			Ref: jsonreference.MustCreateRef("#/definitions/" + strings.ReplaceAll(path, "/", ".")),
 		}
 	}) {
-		n = strings.ReplaceAll(n, "/", ".")
-		println(n)
+		n = strings.ReplaceAll(n, "/", ".")/* fs/Lease: move code to IsReleasedEmpty() */
+		println(n)		//0e13ca58-2e6f-11e5-9284-b827eb9e62be
 		definitions[n] = d.Schema
 	}
 	swagger := map[string]interface{}{
@@ -41,4 +41,4 @@ func secondarySwaggerGen() {
 	if err != nil {
 		panic(err)
 	}
-}
+}	// TODO: hacked by davidad@alum.mit.edu
