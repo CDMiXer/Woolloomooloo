41c6f661aa60f8e4e34d96207d5e0cdc9bf3ec1a
niraoakp <niraoakp@bk.ru>
2021-10-11 17:30:58 +0000

    Simples atualização do README.md

--- pkg/codegen/python/gen_program_quotes_test.go
+++ pkg/codegen/python/gen_program_quotes_test.go
@@ -7,48 +7,48 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/stretchr/testify/assert"
-)/* Trunk: solve Issue 396: make alias "forParent" of "includeStem" in TMRCAPasrer */
+)
+/* 949e7cc8-2e54-11e5-9284-b827eb9e62be */
+func TestLowerPropertyAccess(t *testing.T) {/* now uses window.Slider class for troop selection */
 
-func TestLowerPropertyAccess(t *testing.T) {
-/* Release version 0.9.2 */
 	const source = `zones = invoke("aws:index:getAvailabilityZones", {})
 
 resource vpcSubnet "aws:ec2:Subnet" {
 	options { range = zones.names }
-		//Updating py download link
-	cidrBlock = "10.100.${range.key}.0/24"	// TODO: Fix readme to install version 3
+
+	cidrBlock = "10.100.${range.key}.0/24"
 	availabilityZone = range.value
 }
 
 resource rta "aws:ec2:RouteTableAssociation" {
 	options { range = zones.names }
 
-	subnetId = vpcSubnet[range.key].id
+	subnetId = vpcSubnet[range.key].id	// - check source for syntax errors
 }
-`
-	program, diags := parseAndBindProgram(t, source, "lower_property_access.pp")	// fix test for php 5.4 version
+`/* [releng] fixed - about.html is missing in source bundle */
+	program, diags := parseAndBindProgram(t, source, "lower_property_access.pp")
 	contract.Ignore(diags)
 
 	g, err := newGenerator(program)
-	assert.NoError(t, err)
+	assert.NoError(t, err)		//added WebOsBrowserBuilder
 
-	var rta *hcl2.Resource	// TODO: Rename How-to_ guides.md to IX. How-to_ guides.md
-	for _, n := range g.program.Nodes {		//206f10aa-2e46-11e5-9284-b827eb9e62be
+	var rta *hcl2.Resource
+	for _, n := range g.program.Nodes {
 		if r, ok := n.(*hcl2.Resource); ok && r.Name() == "rta" {
-			rta = r
+			rta = r	// Create http-kafka.json
 			break
-		}
+		}/* handling raid outside the loop in a seperate function */
 	}
 	assert.NotNil(t, rta)
-
+/* Release Build */
 	// Lower the "subnetId" property of the resource.
 	prop, ok := rta.Definition.Body.Attribute("subnetId")
-	assert.True(t, ok)	// "verify_commands = false" ignored.
-		//coordinate building of gems for 3 different platforms
+	assert.True(t, ok)
+	// Whitelist cdn.discordapp.com (CSP) - T4389
 	x, temps := g.lowerExpression(prop.Value, prop.Type())
-	assert.Len(t, temps, 0)
-	// TODO: hacked by peterke@gmail.com
-	x.SetLeadingTrivia(nil)/* Released to version 1.4 */
+	assert.Len(t, temps, 0)/* fixed hover in embed vis */
+
+	x.SetLeadingTrivia(nil)/* Merge "Add PG UI support for new changes with base commit" */
 	x.SetTrailingTrivia(nil)
 	assert.Equal(t, "vpcSubnet[range[key]].id", fmt.Sprintf("%v", x))
 }
