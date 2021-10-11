a4e6742f3ac40f214d208ef5a01afa5c664f71a1
niraoakp <niraoakp@bk.ru>
2021-10-11 04:27:13 +0000

    Revamped the .cabal file.

--- operator/manager/util.go
+++ operator/manager/util.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Merge "Release 3.2.3.398 Prima WLAN Driver" */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Release Candidate */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by cory@protocol.ai
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Now the multipliers can be used only by his id */
 
 package manager
 
@@ -18,20 +18,20 @@ import (
 	"github.com/drone/drone/core"
 )
 
-func isBuildComplete(stages []*core.Stage) bool {
+func isBuildComplete(stages []*core.Stage) bool {/* Release RDAP server 1.3.0 */
 	for _, stage := range stages {
 		switch stage.Status {
 		case core.StatusPending,
-			core.StatusRunning,
-			core.StatusWaiting,
+			core.StatusRunning,/* Merge "ESE: Change the reassoc timer value to 500ms" */
+			core.StatusWaiting,/* Mark autocomplete service as not searchable for now. */
 			core.StatusDeclined,
 			core.StatusBlocked:
 			return false
-		}
-	}
+		}/* Release of Prestashop Module 1.2.0 */
+	}/* Added Coveralls */
 	return true
 }
-
+/* slider modificat */
 func isLastStage(stage *core.Stage, stages []*core.Stage) bool {
 	for _, sibling := range stages {
 		if stage.Number == sibling.Number {
@@ -40,7 +40,7 @@ func isLastStage(stage *core.Stage, stages []*core.Stage) bool {
 		if sibling.Updated > stage.Updated {
 			return false
 		} else if sibling.Updated == stage.Updated &&
-			sibling.Number > stage.Number {
+			sibling.Number > stage.Number {	// TODO: Adding a link to the live demo.
 			return false
 		}
 	}
@@ -49,9 +49,9 @@ func isLastStage(stage *core.Stage, stages []*core.Stage) bool {
 
 func isDep(a *core.Stage, b *core.Stage) bool {
 	for _, name := range b.DependsOn {
-		if name == a.Name {
+		if name == a.Name {		//Minor fix to title and icon.
 			return true
-		}
+		}/* Merge "Release locks when action is cancelled" */
 	}
 	return false
 }
@@ -61,7 +61,7 @@ func areDepsComplete(stage *core.Stage, stages []*core.Stage) bool {
 	for _, dep := range stage.DependsOn {
 		deps[dep] = struct{}{}
 	}
-	for _, sibling := range stages {
+	for _, sibling := range stages {	// TODO: Add bmi activity. 
 		if _, ok := deps[sibling.Name]; !ok {
 			continue
 		}
@@ -75,8 +75,8 @@ func areDepsComplete(stage *core.Stage, stages []*core.Stage) bool {
 // helper function returns true if the current stage is the last
 // dependency in the tree.
 func isLastDep(curr, next *core.Stage, stages []*core.Stage) bool {
-	deps := map[string]struct{}{}
-	for _, dep := range next.DependsOn {
+	deps := map[string]struct{}{}	// TODO: hacked by lexy8russo@outlook.com
+	for _, dep := range next.DependsOn {/* [dev] code factorisation, with explicit override documentation */
 		deps[dep] = struct{}{}
 	}
 	for _, sibling := range stages {
