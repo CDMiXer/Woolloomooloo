61526b2c9647d2df295451c99d64166bada2be21
niraoakp <niraoakp@bk.ru>
2021-10-11 08:37:43 +0000

    TODO-897: xref sample rename

--- tests/integration/double_pending_delete/step1/resource.ts
+++ tests/integration/double_pending_delete/step1/resource.ts
@@ -1,11 +1,11 @@
-// Copyright 2016-2018, Pulumi Corporation./* Updated to use PictureBankList. */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//		//better support for default radius installation
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,23 +22,23 @@ export class Provider implements dynamic.ResourceProvider {
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
-            inputs: news,/* a nicer look */
+            inputs: news,
         }
-    }/* remove no use code */
+    }
 
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (news.fail != olds.fail) {
-            return {/* MainMenu.nib: Break infinite key view loop in panel. */
-                changes: true,	// TODO: refactor wrapRangeWithElement sausage to not do format removing also
-                replaces: ["fail"]		//[IMP] update py.js to 0.7
+            return {
+                changes: true,
+                replaces: ["fail"]
             }
         }
 
-        return {		//test for promise rejection when opts.url not set
-            changes: false,/* Time synchronization refactoring. */
+        return {
+            changes: false,
         }
     }
-/* Delete NewRenderVisTree.h */
+
     public async create(inputs: any): Promise<dynamic.CreateResult> {
         if (inputs.fail == 1) {
             throw new Error("failed to create this resource");
@@ -57,6 +57,6 @@ export class Provider implements dynamic.ResourceProvider {
 
 export class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
-        super(Provider.instance, name, props, opts);	// TODO: Initial InterfaceModel profile added.
+        super(Provider.instance, name, props, opts);
     }
 }
