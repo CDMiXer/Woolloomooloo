fd1b683161c6cc0c4560f48884849122c0f88f12
niraoakp <niraoakp@bk.ru>
2021-10-11 05:06:59 +0000

    Merge "Add more oslo libs to the run-tox-with-oslo-master script"

--- server/auth/webhook/github.go
+++ server/auth/webhook/github.go
@@ -1,57 +1,57 @@
 package webhook
 
-import (	// TODO: pom copy to target and a few small updates
-	"net/http"	// TODO: Always run the tests against the service doubles, skip tests which fail
+import (
+	"net/http"
 
-	"gopkg.in/go-playground/webhooks.v5/github"/* ProRelease2 hardware update */
+	"gopkg.in/go-playground/webhooks.v5/github"
 )
-/* Finalize 0.9 Release */
+	// TODO: will be fixed by steven@stebalien.com
 func githubMatch(secret string, r *http.Request) bool {
 	hook, err := github.New(github.Options.Secret(secret))
 	if err != nil {
-		return false
-	}		//Merge "Make transifex the only source of translations"
+		return false	// TODO: rename chart to startChart
+	}
 	_, err = hook.Parse(r,
 		github.CheckRunEvent,
-		github.CheckSuiteEvent,	// TODO: will be fixed by arachnid@notdot.net
+		github.CheckSuiteEvent,
 		github.CommitCommentEvent,
 		github.CreateEvent,
 		github.DeleteEvent,
-		github.DeploymentEvent,		//Add more compatibility with Python 2 and 3
-		github.DeploymentStatusEvent,
+		github.DeploymentEvent,
+		github.DeploymentStatusEvent,		//Make barbarian weaving mill not buildable (bug #547090)
 		github.ForkEvent,
 		github.GollumEvent,
-		github.InstallationEvent,/* [artifactory-release] Release version 0.6.4.RELEASE */
+		github.InstallationEvent,/* Fix testament tests */
 		github.InstallationRepositoriesEvent,
-		github.IntegrationInstallationEvent,	// Protect against event handler errors.
+		github.IntegrationInstallationEvent,
 		github.IntegrationInstallationRepositoriesEvent,
-		github.IssueCommentEvent,
+		github.IssueCommentEvent,/* Merge pull request #2 from Cryowatt/master */
 		github.IssuesEvent,
 		github.LabelEvent,
 		github.MemberEvent,
 		github.MembershipEvent,
 		github.MilestoneEvent,
 		github.MetaEvent,
-		github.OrganizationEvent,
+		github.OrganizationEvent,		//- adding some new licenses
 		github.OrgBlockEvent,
 		github.PageBuildEvent,
 		github.PingEvent,
 		github.ProjectCardEvent,
 		github.ProjectColumnEvent,
-		github.ProjectEvent,
-		github.PublicEvent,/* add some helper methods for cleaning up, loading files, and checking files */
+		github.ProjectEvent,/* Delete project.py */
+		github.PublicEvent,
 		github.PullRequestEvent,
 		github.PullRequestReviewEvent,
 		github.PullRequestReviewCommentEvent,
 		github.PushEvent,
-		github.ReleaseEvent,
-		github.RepositoryEvent,
+		github.ReleaseEvent,/* Merge "Wlan:  Release 3.8.20.23" */
+		github.RepositoryEvent,	// TODO: Create gameDetails.rb
 		github.RepositoryVulnerabilityAlertEvent,
 		github.SecurityAdvisoryEvent,
-		github.StatusEvent,/* Solution105 */
-		github.TeamEvent,
-		github.TeamAddEvent,	// TODO: will be fixed by peterke@gmail.com
-		github.WatchEvent,/* remove extraline */
-	)		//d83982d0-2e62-11e5-9284-b827eb9e62be
+		github.StatusEvent,
+		github.TeamEvent,/* Release v5.07 */
+		github.TeamAddEvent,
+		github.WatchEvent,
+	)
 	return err == nil
 }
