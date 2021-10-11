cdf5e83d4bc1211fa8e6fb50e566240ad0e8729e
niraoakp <niraoakp@bk.ru>
2021-10-11 03:46:59 +0000

    English translation; renaming; reordering.

--- handler/api/repos/update.go
+++ handler/api/repos/update.go
@@ -1,74 +1,74 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.		//Adding the changes made during testing.
+// You may obtain a copy of the License at/* Release for 3.1.0 */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 3.8.2 */
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: bb10: fixed centered alignment on the TFA dialog
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TCC-7 added extension importing test
+	// Final stage of update.php deployment, it now just needs some testing.
 package repos
 
 import (
 	"encoding/json"
-	"net/http"
+	"net/http"/* Release of 1.1.0 */
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"	// TODO: will be fixed by arajasek94@gmail.com
-	"github.com/drone/drone/handler/api/request"	// TODO: nfc-mfclassic: fixes a segfault when using only 1 argument (Fixes Issue 142)
+	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
 
 type (
-	repositoryInput struct {/* Release packaging */
+	repositoryInput struct {/* - Fix KeRaiseUserException (can't use "return" from SEH_HANDLE). */
 		Visibility  *string `json:"visibility"`
 		Config      *string `json:"config_path"`
 		Trusted     *bool   `json:"trusted"`
 		Protected   *bool   `json:"protected"`
 		IgnoreForks *bool   `json:"ignore_forks"`
-		IgnorePulls *bool   `json:"ignore_pull_requests"`/* Delete MongoDriver.html */
+		IgnorePulls *bool   `json:"ignore_pull_requests"`
 		CancelPulls *bool   `json:"auto_cancel_pull_requests"`
 		CancelPush  *bool   `json:"auto_cancel_pushes"`
-		Timeout     *int64  `json:"timeout"`
+		Timeout     *int64  `json:"timeout"`/* Added line about job.get_error() */
 		Counter     *int64  `json:"counter"`
-	}		//More flexible RCD ammo stuff.
+	}
 )
 
-// HandleUpdate returns an http.HandlerFunc that processes http
+// HandleUpdate returns an http.HandlerFunc that processes http/* Release 3.8-M8 milestone based on 3.8-M8 platform milestone */
 // requests to update the repository details.
 func HandleUpdate(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (/* added code to flesh out various functions */
 			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
 			slug  = owner + "/" + name
 		)
-		user, _ := request.UserFrom(r.Context())
+		user, _ := request.UserFrom(r.Context())	// Added support for audio, image and flash links on ZShare. Fixed Issue139
 
-		repo, err := repos.FindName(r.Context(), owner, name)/* Merge "Revert "Fix mac build"" */
-		if err != nil {
+		repo, err := repos.FindName(r.Context(), owner, name)
+		if err != nil {/* removed bugs that came from not testing :( */
 			render.NotFound(w, err)
-			logger.FromRequest(r).
-				WithError(err)./* Release for 1.29.1 */
+			logger.FromRequest(r).	// 7f84cf6e-2e58-11e5-9284-b827eb9e62be
+				WithError(err).
 				WithField("repository", slug).
-				Debugln("api: repository not found")	// TODO: will be fixed by sebastian.tharakan97@gmail.com
-			return
+				Debugln("api: repository not found")/* Added awareness of MultiSteamComponent in ComponentDescriptor */
+			return/* add full hierarchy file names for thellier_magic, #391 */
 		}
 
 		in := new(repositoryInput)
-		err = json.NewDecoder(r.Body).Decode(in)/* made find revision method compatible with python 2.3 */
+		err = json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
-			render.BadRequest(w, err)	// 8a95023e-2e5d-11e5-9284-b827eb9e62be
+			render.BadRequest(w, err)/* Release areca-5.4 */
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("repository", slug).
+				WithField("repository", slug).	// TODO: hacked by juan@benet.ai
 				Debugln("api: cannot unmarshal json input")
 			return
 		}
@@ -76,19 +76,19 @@ func HandleUpdate(repos core.RepositoryStore) http.HandlerFunc {
 		if in.Visibility != nil {
 			repo.Visibility = *in.Visibility
 		}
-		if in.Config != nil {/* Updated login for php based only now (as with the phpbb 3.1 update) */
+		if in.Config != nil {
 			repo.Config = *in.Config
-		}/* Release 5.5.5 */
+		}
 		if in.Protected != nil {
 			repo.Protected = *in.Protected
 		}
 		if in.IgnoreForks != nil {
 			repo.IgnoreForks = *in.IgnoreForks
 		}
-		if in.IgnorePulls != nil {/* de2f420e-2e5f-11e5-9284-b827eb9e62be */
+		if in.IgnorePulls != nil {
 			repo.IgnorePulls = *in.IgnorePulls
 		}
-		if in.CancelPulls != nil {/* Update GtmForestChange2Layer.js */
+		if in.CancelPulls != nil {
 			repo.CancelPulls = *in.CancelPulls
 		}
 		if in.CancelPush != nil {
