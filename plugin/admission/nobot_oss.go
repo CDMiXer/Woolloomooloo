e138c1ec6eb59020ba43d9d71e8d7e22fe3e2ac3
niraoakp <niraoakp@bk.ru>
2021-10-11 23:01:41 +0000

    Initialize message before concat.

--- plugin/admission/nobot_oss.go
+++ plugin/admission/nobot_oss.go
@@ -6,7 +6,7 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//Added UV class
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -15,14 +15,14 @@
 // +build oss
 
 package admission
-
+/* Delete values-hu */
 import (
-	"time"
+	"time"		//Fixed single space that astyle caught
 
 	"github.com/drone/drone/core"
 )
-
-// Nobot is a no-op admission controller
+	// TODO: Merge branch 'DDBNEXT-1237' into develop
+// Nobot is a no-op admission controller/* Release luna-fresh pool */
 func Nobot(core.UserService, time.Duration) core.AdmissionService {
 	return new(noop)
 }
