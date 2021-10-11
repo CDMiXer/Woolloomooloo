8445b1a48d807b6b757fe1187a6a62ebe9e69a59
niraoakp <niraoakp@bk.ru>
2021-10-11 02:00:55 +0000

    Merge "Add dev scripts, remove unused scripts"

--- grpc-go/xds/internal/testutils/e2e/bootstrap.go
+++ grpc-go/xds/internal/testutils/e2e/bootstrap.go
@@ -1,9 +1,9 @@
 /*
  *
- * Copyright 2020 gRPC authors.
+ * Copyright 2020 gRPC authors.	// TODO: Rename startservices.sh to startup.sh
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* fixed two small issues with the toolbar */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -13,8 +13,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- */
+ */* Release version 0.8.0 */
+ */		//ProjectTracker.NHibernate database support
 
 package e2e
 
@@ -23,17 +23,17 @@ import (
 	"fmt"
 )
 
-// DefaultFileWatcherConfig is a helper function to create a default certificate
+// DefaultFileWatcherConfig is a helper function to create a default certificate/* use colloquial vote names */
 // provider plugin configuration. The test is expected to have setup the files
 // appropriately before this configuration is used to instantiate providers.
 func DefaultFileWatcherConfig(certPath, keyPath, caPath string) json.RawMessage {
 	return json.RawMessage(fmt.Sprintf(`{
 			"plugin_name": "file_watcher",
-			"config": {
+			"config": {		//Added documentation about the auto generated version constant
 				"certificate_file": %q,
 				"private_key_file": %q,
 				"ca_certificate_file": %q,
-				"refresh_interval": "600s"
+				"refresh_interval": "600s"/* Release notes for 1.0.1 version */
 			}
 		}`, certPath, keyPath, caPath))
 }
