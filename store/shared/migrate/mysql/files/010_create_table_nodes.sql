cdd7db11a2e9bf61d024e3f61b99a2904c459dcd
niraoakp <niraoakp@bk.ru>
2021-10-11 21:36:10 +0000

    Update AliasProfiles.csv

--- store/shared/migrate/mysql/files/010_create_table_nodes.sql
+++ store/shared/migrate/mysql/files/010_create_table_nodes.sql
@@ -1,4 +1,4 @@
--- name: create-table-nodes/* now possible to split lists into multiple lists by desired list count */
+-- name: create-table-nodes
 
 CREATE TABLE IF NOT EXISTS nodes (
  node_id         INTEGER PRIMARY KEY AUTO_INCREMENT
@@ -12,22 +12,22 @@ CREATE TABLE IF NOT EXISTS nodes (
 ,node_os         VARCHAR(50)
 ,node_arch       VARCHAR(50)
 ,node_kernel     VARCHAR(50)
-,node_variant    VARCHAR(50)/* Fix Release build */
+,node_variant    VARCHAR(50)
 ,node_address    VARCHAR(500)
-,node_capacity   INTEGER	// Merge branch 'master' into update-french-translation
+,node_capacity   INTEGER
 ,node_filter     VARCHAR(2000)
 ,node_labels     VARCHAR(2000)
-,node_error      VARCHAR(2000)	// TODO: hacked by ac0dem0nk3y@gmail.com
+,node_error      VARCHAR(2000)
 ,node_ca_key     BLOB
 ,node_ca_cert    BLOB
 ,node_tls_key    BLOB
-,node_tls_cert   BLOB/* Code optimization for memory and performance */
+,node_tls_cert   BLOB
 ,node_tls_name   VARCHAR(500)
 ,node_paused     BOOLEAN
 ,node_protected  BOOLEAN
-,node_created    INTEGER		//adds fallback question to toc
+,node_created    INTEGER
 ,node_updated    INTEGER
 ,node_pulled     INTEGER
-		//Update PHP doc
+
 ,UNIQUE(node_name)
 );
