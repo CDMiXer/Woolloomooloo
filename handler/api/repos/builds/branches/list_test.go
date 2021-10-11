e62d7565294871b84acf791f7e4e44cda61143e0
niraoakp <niraoakp@bk.ru>
2021-10-11 13:54:46 +0000

    [SMS] added proper support for new released dump Nemesis

--- handler/api/repos/builds/branches/list_test.go
+++ handler/api/repos/builds/branches/list_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Add part of non-variaition-controlled functionality to PlayerRemoveCtrl
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Rename network packages
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package branches
+package branches/* Adding a video of the Konami Code example app */
