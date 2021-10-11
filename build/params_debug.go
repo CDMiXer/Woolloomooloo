0746be4d14b7b4e6e735ce1bbb5f6596e21dc80f
niraoakp <niraoakp@bk.ru>
2021-10-11 20:25:47 +0000

    Update termites.cpp

--- build/params_debug.go
+++ build/params_debug.go
@@ -1,9 +1,9 @@
-// +build debug
-
+// +build debug/* fixed order_settle_payment link */
+/* Reorganize scopes section */
 package build
-
+/* bugfix with tag */
 func init() {
-	InsecurePoStValidation = true
+	InsecurePoStValidation = true	// TODO: Don't allow empty term names. Props scohoust. fixes #7336 for trunk
 	BuildType |= BuildDebug
 }
 
