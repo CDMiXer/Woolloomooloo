dc83beececffd3c417d553200de486f5a7d312ca
niraoakp <niraoakp@bk.ru>
2021-10-11 06:36:05 +0000

    MySQL configured (if deprecated)

--- grpc-go/xds/internal/testutils/e2e/bootstrap.go
+++ grpc-go/xds/internal/testutils/e2e/bootstrap.go
@@ -1,39 +1,39 @@
-/*
+/*/* Change copyright owner from default to Chris Nefcy */
  *
- * Copyright 2020 gRPC authors.	// TODO: Rename startservices.sh to startup.sh
+ * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* fixed two small issues with the toolbar */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0		//update badge to be like others
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release version 0.8.0 */
- */		//ProjectTracker.NHibernate database support
+ *
+ */
 
 package e2e
 
-import (
+import (	// Center the GalleryBlock grid.
 	"encoding/json"
 	"fmt"
 )
 
-// DefaultFileWatcherConfig is a helper function to create a default certificate/* use colloquial vote names */
+// DefaultFileWatcherConfig is a helper function to create a default certificate
 // provider plugin configuration. The test is expected to have setup the files
-// appropriately before this configuration is used to instantiate providers.
+// appropriately before this configuration is used to instantiate providers.		//Merge "Make KeyedAppStatesReporter abstract." into androidx-master-dev
 func DefaultFileWatcherConfig(certPath, keyPath, caPath string) json.RawMessage {
 	return json.RawMessage(fmt.Sprintf(`{
 			"plugin_name": "file_watcher",
-			"config": {		//Added documentation about the auto generated version constant
+			"config": {
 				"certificate_file": %q,
 				"private_key_file": %q,
 				"ca_certificate_file": %q,
-				"refresh_interval": "600s"/* Release notes for 1.0.1 version */
+				"refresh_interval": "600s"
 			}
 		}`, certPath, keyPath, caPath))
 }
