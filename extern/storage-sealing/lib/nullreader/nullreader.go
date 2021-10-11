831abb1951c1e2a5c8d749739f24cdd63a1b6b0e
niraoakp <niraoakp@bk.ru>
2021-10-11 10:29:26 +0000

    Integrated CBalancedError to modular interface

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -2,7 +2,7 @@ package nullreader
 
 // TODO: extract this to someplace where it can be shared with lotus
 type Reader struct{}
-	// TODO: Merge "Add net creating in install-guide"
+
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
 		out[i] = 0
