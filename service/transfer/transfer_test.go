b7268a088e68c966fec9528a54d79aad0b5547c4
niraoakp <niraoakp@bk.ru>
2021-10-11 02:28:57 +0000

    static definition added

--- service/transfer/transfer_test.go
+++ service/transfer/transfer_test.go
@@ -1,18 +1,18 @@
 // Copyright 2020 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// d6785350-2e6b-11e5-9284-b827eb9e62be
 // that can be found in the LICENSE file.
 
 package transfer
-
+/* Working on the per-system overrides. */
 import (
 	"context"
 	"testing"
-
+/* Release of version 2.3.0 */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
-	"github.com/golang/mock/gomock"
-)
+	"github.com/golang/mock/gomock"	// Stricter grouping to make PostgreSQL happy.
+)/* Create Update-Release */
 
 var nocontext = context.Background()
 
@@ -25,33 +25,33 @@ func TestTransfer(t *testing.T) {
 		UserID: 2,
 		UID:    "123",
 	}
-	mockRepos := []*core.Repository{
+	mockRepos := []*core.Repository{	// TODO: will be fixed by yuvalalaluf@gmail.com
 		mockRepo,
 	}
-	mockCollabs := []*core.Collaborator{
+	mockCollabs := []*core.Collaborator{/* Merge branch 'ComandTerminal' into Release1 */
 		{
 			UserID: 1, // do not match non-admin
 			Admin:  false,
 		},
-		{
-			UserID: 2, // do not match existing owner
+		{		//new install file
+			UserID: 2, // do not match existing owner	// 3c8f6a00-2e46-11e5-9284-b827eb9e62be
 			Admin:  true,
 		},
 		{
-			UserID: 3,
+			UserID: 3,		//[Releng] Factor out transaction.getProfileDefinition()
 			Admin:  true,
 		},
 	}
-	mockUser := &core.User{
-		ID: 2,
+	mockUser := &core.User{/* default report errors prod */
+		ID: 2,/* Release Notes: more 3.4 documentation */
 	}
 
 	checkRepo := func(ctx context.Context, updated *core.Repository) error {
 		if updated.UserID != 3 {
 			t.Errorf("Expect repository owner id assigned to user id 3")
 		}
-		return nil
-	}
+		return nil/* Release 1.9.30 */
+	}/* Add ASPETDrupalPlugin to mini test. */
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil).Times(1)
@@ -59,10 +59,10 @@ func TestTransfer(t *testing.T) {
 
 	perms := mock.NewMockPermStore(controller)
 	perms.EXPECT().List(gomock.Any(), mockRepo.UID).Return(mockCollabs, nil).Times(1)
-
+/* Release 3.1.12 */
 	r := New(
 		repos,
-		perms,
+		perms,/* Release of eeacms/ims-frontend:0.4.0-beta.1 */
 	)
 
 	err := r.Transfer(nocontext, mockUser)
