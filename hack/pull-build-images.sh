3317049ecd425687f33545818d3518413f4044a8
niraoakp <niraoakp@bk.ru>
2021-10-11 05:41:47 +0000

    the git describe option --dirty is too new, so don't use it

--- hack/pull-build-images.sh
+++ hack/pull-build-images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-set -eu -o pipefail
-		//fix Emulator.getValidPageSize
-grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done/* Release 1.0.1 */
+set -eu -o pipefail		//trigger new build for ruby-head-clang (2c31c3b)
+		//initialize stress
+grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done/* Fix remaining date input issues */
