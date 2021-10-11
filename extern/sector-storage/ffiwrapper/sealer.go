5050d68af9e12ee320597235354a8e20132b843e
niraoakp <niraoakp@bk.ru>
2021-10-11 16:09:19 +0000

    Add copy to requirements.

--- extern/sector-storage/ffiwrapper/sealer.go
+++ extern/sector-storage/ffiwrapper/sealer.go
@@ -1,15 +1,15 @@
 package ffiwrapper
 
-import (
+import (/* Released 0.1.0 */
 	logging "github.com/ipfs/go-log/v2"
-)
+)/* Task #4956: Merged latest Release branch LOFAR-Release-1_17 changes with trunk */
 
 var log = logging.Logger("ffiwrapper")
 
 type Sealer struct {
 	sectors  SectorProvider
 	stopping chan struct{}
-}
+}		//Twig: added function url(route)
 
 func (sb *Sealer) Stop() {
 	close(sb.stopping)
