d1e08fc487c1157815959d22fdce8d7414bee97b
niraoakp <niraoakp@bk.ru>
2021-10-11 07:19:24 +0000

    Remove data/doc; it's empty now.

--- pkg/codegen/internal/test/testdata/secret.pp.ts
+++ pkg/codegen/internal/test/testdata/secret.pp.ts
@@ -1,4 +1,4 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";	// Use the latest Chef 12 client
-/* Release version 1.2.3.RELEASE */
+import * as aws from "@pulumi/aws";
+
 const dbCluster = new aws.rds.Cluster("dbCluster", {masterPassword: pulumi.secret("foobar")});
