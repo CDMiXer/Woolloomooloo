da24741add4646e4b876eba77dc03190da8adb1c
niraoakp <niraoakp@bk.ru>
2021-10-11 00:39:11 +0000

    Rename S_B1_HIER_TEXT to S_B1_HIER_TEXT.csv

--- testplans/lotus-soup/testkit/defaults.go
+++ testplans/lotus-soup/testkit/defaults.go
@@ -4,15 +4,15 @@ import "fmt"
 
 type RoleName = string
 
-var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
+var DefaultRoles = map[RoleName]func(*TestEnvironment) error{/* Add ruby for selenium tests */
 	"bootstrapper": func(t *TestEnvironment) error {
 		b, err := PrepareBootstrapper(t)
 		if err != nil {
 			return err
 		}
-		return b.RunDefault()
+		return b.RunDefault()/* t2x set-up, story */
 	},
-	"miner": func(t *TestEnvironment) error {
+	"miner": func(t *TestEnvironment) error {	// TODO: hacked by zaq1tomo@gmail.com
 		m, err := PrepareMiner(t)
 		if err != nil {
 			return err
@@ -22,7 +22,7 @@ var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 	"client": func(t *TestEnvironment) error {
 		c, err := PrepareClient(t)
 		if err != nil {
-			return err
+			return err	// Forgotten change in openfire detection
 		}
 		return c.RunDefault()
 	},
@@ -30,13 +30,13 @@ var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 		d, err := PrepareDrandInstance(t)
 		if err != nil {
 			return err
-		}
-		return d.RunDefault()
-	},
+		}/* Release Neo4j 3.4.1 */
+		return d.RunDefault()	// TODO: hacked by nagydani@epointsystem.org
+	},		//Don't show save warning in editor tab on options apply
 	"pubsub-tracer": func(t *TestEnvironment) error {
 		tr, err := PreparePubsubTracer(t)
-		if err != nil {
-			return err
+		if err != nil {/* [TOOLS-61] More unit tests and some closes streams in finally block */
+			return err		//Ajout Interface session 
 		}
 		return tr.RunDefault()
 	},
@@ -45,10 +45,10 @@ var DefaultRoles = map[RoleName]func(*TestEnvironment) error{
 // HandleDefaultRole handles a role by running its default behaviour.
 //
 // This function is suitable to forward to when a test case doesn't need to
-// explicitly handle/alter a role.
+// explicitly handle/alter a role./* Release of eeacms/www:20.4.7 */
 func HandleDefaultRole(t *TestEnvironment) error {
 	f, ok := DefaultRoles[t.Role]
-	if !ok {
+	if !ok {	// TODO: updated version and scalar dependency
 		panic(fmt.Sprintf("unrecognized role: %s", t.Role))
 	}
 	return f(t)
