6e646c24f123cc53e27435851169f5405aa07a2a
niraoakp <niraoakp@bk.ru>
2021-10-11 02:56:10 +0000

    Add RequireJS app example

--- tests/integration/protect_resources/step1/index.ts
+++ tests/integration/protect_resources/step1/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+	// TODO: will be fixed by why@ipfs.io
+import { Resource } from "./resource";
 
-import { Resource } from "./resource";	// TODO: hacked by lexy8russo@outlook.com
-
-// Allocate a resource and protect it:
+// Allocate a resource and protect it:/* removed library specific definitions */
 let a = new Resource("eternal", { state: 1 }, { protect: true });
