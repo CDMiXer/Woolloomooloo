d243a65d77ce4e49b2890c2ed8acf44f5d3d5f17
niraoakp <niraoakp@bk.ru>
2021-10-11 19:53:56 +0000

    several bugfixes and design improvements

--- store/shared/migrate/postgres/ddl.go
+++ store/shared/migrate/postgres/ddl.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* add travis + codecov */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package postgres
+package postgres		//Update B827EBFFFEA7CC5F.json
 
-//go:generate togo ddl -package postgres -dialect postgres/* added resetmagenta */
+//go:generate togo ddl -package postgres -dialect postgres
