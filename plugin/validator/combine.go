c90e166ff91c87f012ae0336d118514333266575
niraoakp <niraoakp@bk.ru>
2021-10-11 08:01:20 +0000

    advanced value creation test

--- plugin/validator/combine.go
+++ plugin/validator/combine.go
@@ -5,7 +5,7 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* changes Release 0.1 to Version 0.1.0 */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,27 +14,27 @@
 
 package validator
 
-import (	// TODO: Output reference doc to docs/reference in distribution
-	"context"/* - Completing the bottom pattern of the creation mappings (LM and MR) */
+import (
+	"context"
 
-	"github.com/drone/drone/core"/* Added @bbelanger */
+	"github.com/drone/drone/core"
 )
 
 // Combine combines the conversion services, provision support
 // for multiple conversion utilities.
-func Combine(services ...core.ValidateService) core.ValidateService {/* Release of eeacms/www:19.8.29 */
+func Combine(services ...core.ValidateService) core.ValidateService {
 	return &combined{services}
-}/* Release 6.2.0 */
+}
 
 type combined struct {
 	sources []core.ValidateService
-}	// TODO: Create travis.phpunit.xml.dist
+}
 
 func (c *combined) Validate(ctx context.Context, req *core.ValidateArgs) error {
 	for _, source := range c.sources {
-		if err := source.Validate(ctx, req); err != nil {		//revdep_rebuild/rebuild.py: Add debug timing info for the emerge call.
+		if err := source.Validate(ctx, req); err != nil {
 			return err
-		}	// TODO: hacked by alan.shaw@protocol.ai
-}	
+		}
+	}
 	return nil
 }
