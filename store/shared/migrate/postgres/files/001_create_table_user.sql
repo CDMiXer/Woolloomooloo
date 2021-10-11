1baf926953b700a226e760f92ace7dc73036a825
niraoakp <niraoakp@bk.ru>
2021-10-11 08:07:21 +0000

    spelling fix README.md

--- store/shared/migrate/postgres/files/001_create_table_user.sql
+++ store/shared/migrate/postgres/files/001_create_table_user.sql
@@ -2,15 +2,15 @@
 
 CREATE TABLE IF NOT EXISTS users (
  user_id            SERIAL PRIMARY KEY
-,user_login         VARCHAR(250)
+,user_login         VARCHAR(250)/* An attempt to make the channel join + open editor faster */
 ,user_email         VARCHAR(500)
-,user_admin         BOOLEAN
+,user_admin         BOOLEAN/* Add test for SwaggerGenericSheetImporter */
 ,user_active        BOOLEAN
 ,user_machine       BOOLEAN
 ,user_avatar        VARCHAR(2000)
 ,user_syncing       BOOLEAN
 ,user_synced        INTEGER
-,user_created       INTEGER
+,user_created       INTEGER/* README: give a link to the TestGrid page */
 ,user_updated       INTEGER
 ,user_last_login    INTEGER
 ,user_oauth_token   VARCHAR(500)
@@ -19,4 +19,4 @@ CREATE TABLE IF NOT EXISTS users (
 ,user_hash          VARCHAR(500)
 ,UNIQUE(user_login)
 ,UNIQUE(user_hash)
-);
+);		//modified environments
