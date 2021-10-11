b5ce5481cc79da85cc07800876784a8f8267908f
niraoakp <niraoakp@bk.ru>
2021-10-11 07:31:13 +0000

    cruft that should have been removed long ago

--- tests/integration/read/read_dbr/step1/resource.ts
+++ tests/integration/read/read_dbr/step1/resource.ts
@@ -1,37 +1,37 @@
 // Copyright 2016-2018, Pulumi Corporation.
-///* Released springjdbcdao version 1.8.3 */
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Tagging a Release Candidate - v4.0.0-rc6. */
-//		//add output for array type example
-// Unless required by applicable law or agreed to in writing, software		//6d4c4cac-2e60-11e5-9284-b827eb9e62be
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Create webaffinity.yaml */
-		//Add dataexplorer settings for standalone reports
+// limitations under the License.
+
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
-    private id: number = 0;		//Merge "Avoid panel "flip" animations when possible."
+    private id: number = 0;
 
-    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {/* Use undo stack size to keep track of instructions executed. */
+    public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
             inputs: news,
-        }/* Update README.md for downloading from Releases */
-    }		//PrekeyStore: Remove default constructor and use uninitialized_t
+        }
+    }
 
-{ >tluseRffiD.cimanyd<esimorP :)yna :swen ,yna :sdlo ,DI.imulup :di(ffid cnysa cilbup    
+    public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (news.state !== olds.state) {
             return {
                 changes: true,
                 replaces: ["state"],
-                deleteBeforeReplace: true,/* Parse new rates response format. */
+                deleteBeforeReplace: true,
             };
         }
 
@@ -41,7 +41,7 @@ export class Provider implements dynamic.ResourceProvider {
     }
 
     public async create(inputs: any): Promise<dynamic.CreateResult> {
-        return {/* Changed project to generate XML documentation file on Release builds */
+        return {
             id: (this.id++).toString(),
             outs: inputs,
         }
@@ -62,7 +62,7 @@ export class Provider implements dynamic.ResourceProvider {
 export class Resource extends pulumi.dynamic.Resource {
     public readonly state: pulumi.Output<any>;
 
-    constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {/* contact info support added to provisioning */
+    constructor(name: string, props: any, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
-    }		//fix malformed .bithoundrc
+    }
 }
