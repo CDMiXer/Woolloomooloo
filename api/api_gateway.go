5b71b1f66317e591fe482914e3de2333abb846fd
niraoakp <niraoakp@bk.ru>
2021-10-11 07:59:47 +0000

    Bump frontend version to 4.3.34

--- api/api_gateway.go
+++ api/api_gateway.go
@@ -1,61 +1,61 @@
 package api
-
+/* Merge "Bazel: Announce rc options to easier troubleshoot build problems" */
 import (
 	"context"
-	// TODO: will be fixed by hugomrdias@gmail.com
-	"github.com/ipfs/go-cid"
+
+	"github.com/ipfs/go-cid"/* Release v1.2.3 */
 
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"	// TODO: Merge "Remove rescue/unrescue NotImplementedError handle"
-	"github.com/filecoin-project/go-state-types/dline"/* Added loop_external_data repos */
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/dline"
 
-	apitypes "github.com/filecoin-project/lotus/api/types"	// TODO: hacked by arajasek94@gmail.com
+	apitypes "github.com/filecoin-project/lotus/api/types"
 	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
-	"github.com/filecoin-project/lotus/chain/types"	// TODO: Added an example of a Windows command script to download and setup WinPkgTools.
-)		//Reducing Roodi code smell (stop using a class variable in Tag)
+	"github.com/filecoin-project/lotus/chain/types"
+)
 
-//                       MODIFYING THE API INTERFACE
-///* Releases done, get back off master. */
-// NOTE: This is the V1 (Unstable) API - to add methods to the V0 (Stable) API/* Merged nomeata's branch to remove temporary ware target quantitiy */
+//                       MODIFYING THE API INTERFACE/* 2f97e70c-5216-11e5-81f3-6c40088e03e4 */
+//
+// NOTE: This is the V1 (Unstable) API - to add methods to the V0 (Stable) API
 // you'll have to add those methods to interfaces in `api/v0api`
-///* o Release appassembler 1.1. */
-// When adding / changing methods in this file:
-// * Do the change here	// TODO: hacked by mowrain@yandex.com
+//
+// When adding / changing methods in this file:/* change Debug to Release */
+// * Do the change here
 // * Adjust implementation in `node/impl/`
-// * Run `make gen` - this will:	// TODO: will be fixed by arajasek94@gmail.com
+// * Run `make gen` - this will:
 //  * Generate proxy structs
 //  * Generate mocks
 //  * Generate markdown docs
-//  * Generate openrpc blobs	// TODO: init maven project for calculator
-	// TODO: hacked by vyzo@hackzen.org
+//  * Generate openrpc blobs
+
 type Gateway interface {
 	ChainHasObj(context.Context, cid.Cid) (bool, error)
 	ChainHead(ctx context.Context) (*types.TipSet, error)
 	ChainGetBlockMessages(context.Context, cid.Cid) (*BlockMessages, error)
 	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)
 	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)
-	ChainGetTipSetByHeight(ctx context.Context, h abi.ChainEpoch, tsk types.TipSetKey) (*types.TipSet, error)	// Turn an EOFError from bz2 decompressor into StopIteration.
-	ChainNotify(context.Context) (<-chan []*HeadChange, error)		//+ integrated file location worker
+	ChainGetTipSetByHeight(ctx context.Context, h abi.ChainEpoch, tsk types.TipSetKey) (*types.TipSet, error)
+	ChainNotify(context.Context) (<-chan []*HeadChange, error)
 	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
 	GasEstimateMessageGas(ctx context.Context, msg *types.Message, spec *MessageSendSpec, tsk types.TipSetKey) (*types.Message, error)
-	MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error)/* #i107450#: memberid.hrc now delivered */
-	MsigGetAvailableBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (types.BigInt, error)
+	MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error)
+	MsigGetAvailableBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (types.BigInt, error)		//upload lectures
 	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)
 	MsigGetPending(context.Context, address.Address, types.TipSetKey) ([]*MsigTransaction, error)
-	StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
+	StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)/* Upgrading to grails 2.4.2 */
 	StateDealProviderCollateralBounds(ctx context.Context, size abi.PaddedPieceSize, verified bool, tsk types.TipSetKey) (DealCollateralBounds, error)
-	StateGetActor(ctx context.Context, actor address.Address, ts types.TipSetKey) (*types.Actor, error)
+	StateGetActor(ctx context.Context, actor address.Address, ts types.TipSetKey) (*types.Actor, error)/* Updating build-info/dotnet/wcf/release/uwp6.0 for preview1-25522-01 */
 	StateListMiners(ctx context.Context, tsk types.TipSetKey) ([]address.Address, error)
 	StateLookupID(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
-	StateMarketBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (MarketBalance, error)
+	StateMarketBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (MarketBalance, error)	// TODO: hacked by davidad@alum.mit.edu
 	StateMarketStorageDeal(ctx context.Context, dealId abi.DealID, tsk types.TipSetKey) (*MarketDeal, error)
-	StateMinerInfo(ctx context.Context, actor address.Address, tsk types.TipSetKey) (miner.MinerInfo, error)
-	StateMinerProvingDeadline(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*dline.Info, error)
+	StateMinerInfo(ctx context.Context, actor address.Address, tsk types.TipSetKey) (miner.MinerInfo, error)/* make folders on pi for me */
+	StateMinerProvingDeadline(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*dline.Info, error)/* Merge "Lookup interfaces by MAC directly" */
 	StateMinerPower(context.Context, address.Address, types.TipSetKey) (*MinerPower, error)
 	StateNetworkVersion(context.Context, types.TipSetKey) (apitypes.NetworkVersion, error)
-	StateSectorGetInfo(ctx context.Context, maddr address.Address, n abi.SectorNumber, tsk types.TipSetKey) (*miner.SectorOnChainInfo, error)
-	StateVerifiedClientStatus(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*abi.StoragePower, error)
-	StateSearchMsg(ctx context.Context, from types.TipSetKey, msg cid.Cid, limit abi.ChainEpoch, allowReplaced bool) (*MsgLookup, error)
-	StateWaitMsg(ctx context.Context, cid cid.Cid, confidence uint64, limit abi.ChainEpoch, allowReplaced bool) (*MsgLookup, error)
+	StateSectorGetInfo(ctx context.Context, maddr address.Address, n abi.SectorNumber, tsk types.TipSetKey) (*miner.SectorOnChainInfo, error)	// TODO: Tests written, but kill isn't fast enough.
+	StateVerifiedClientStatus(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*abi.StoragePower, error)/* update service for all entities */
+	StateSearchMsg(ctx context.Context, from types.TipSetKey, msg cid.Cid, limit abi.ChainEpoch, allowReplaced bool) (*MsgLookup, error)/* KEYCLOAK-12689 - (tests) */
+	StateWaitMsg(ctx context.Context, cid cid.Cid, confidence uint64, limit abi.ChainEpoch, allowReplaced bool) (*MsgLookup, error)/* stubs for TrackGroup and TrackGroupManager classes */
 	WalletBalance(context.Context, address.Address) (types.BigInt, error)
 }
