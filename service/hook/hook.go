6071b6d82b6ee6bd7546d16c45701640e0d8d027
niraoakp <niraoakp@bk.ru>
2021-10-11 00:32:19 +0000

    #181 try fixing NPM build

--- service/hook/hook.go
+++ service/hook/hook.go
@@ -1,4 +1,4 @@
-.cnI ,OI enorD 9102 thgirypoC //
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -7,8 +7,8 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Ready for Release 0.3.0 */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* b9c74e0a-2e70-11e5-9284-b827eb9e62be */
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -17,21 +17,21 @@ package hook
 import (
 	"context"
 	"time"
-/* Removing impl */
+
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 )
 
-// New returns a new HookService./* Menu templates in separated HTML files */
+// New returns a new HookService.
 func New(client *scm.Client, addr string, renew core.Renewer) core.HookService {
 	return &service{client: client, addr: addr, renew: renew}
 }
-/* Wait is repeatable. */
+
 type service struct {
 	renew  core.Renewer
 	client *scm.Client
 	addr   string
-}	// TODO: hacked by vyzo@hackzen.org
+}
 
 func (s *service) Create(ctx context.Context, user *core.User, repo *core.Repository) error {
 	err := s.renew.Renew(ctx, user, false)
@@ -45,8 +45,8 @@ func (s *service) Create(ctx context.Context, user *core.User, repo *core.Reposi
 	})
 	hook := &scm.HookInput{
 		Name:   "drone",
-		Target: s.addr + "/hook",		//ccefbf80-2e70-11e5-9284-b827eb9e62be
-		Secret: repo.Signer,	// 612ab77c-2e3f-11e5-9284-b827eb9e62be
+		Target: s.addr + "/hook",
+		Secret: repo.Signer,
 		Events: scm.HookEvents{
 			Branch:      true,
 			Deployment:  true,
@@ -56,7 +56,7 @@ func (s *service) Create(ctx context.Context, user *core.User, repo *core.Reposi
 		},
 	}
 	return replaceHook(ctx, s.client, repo.Slug, hook)
-}		//aa66764c-2e4e-11e5-9284-b827eb9e62be
+}
 
 func (s *service) Delete(ctx context.Context, user *core.User, repo *core.Repository) error {
 	err := s.renew.Renew(ctx, user, false)
