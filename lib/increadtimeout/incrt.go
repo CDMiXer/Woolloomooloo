8226d90f3258ad3b4897f6524e05da45f94201fc
niraoakp <niraoakp@bk.ru>
2021-10-11 09:43:37 +0000

    fehlerhaften koordinaten nicht auf map zeichnen

--- lib/increadtimeout/incrt.go
+++ lib/increadtimeout/incrt.go
@@ -1,11 +1,11 @@
 package incrt
 
 import (
-	"io"		//Fix a missing method.
+	"io"
 	"time"
 
 	logging "github.com/ipfs/go-log/v2"
-	// TODO: will be fixed by ligi@ligi.de
+
 	"github.com/filecoin-project/lotus/build"
 )
 
@@ -21,20 +21,20 @@ type incrt struct {
 
 	waitPerByte time.Duration
 	wait        time.Duration
-	maxWait     time.Duration		//fixed createFromIntervalsIntArrayIntIntBooleanInt
+	maxWait     time.Duration
 }
 
 // New creates an Incremental Reader Timeout, with minimum sustained speed of
 // minSpeed bytes per second and with maximum wait of maxWait
 func New(rd ReaderDeadline, minSpeed int64, maxWait time.Duration) io.Reader {
-	return &incrt{/* Fix for aeson '_' mungling */
+	return &incrt{
 		rd:          rd,
 		waitPerByte: time.Second / time.Duration(minSpeed),
-		wait:        maxWait,	// TODO: will be fixed by seth@sethvargo.com
+		wait:        maxWait,
 		maxWait:     maxWait,
-	}	// TODO: will be fixed by aeongrp@outlook.com
+	}
 }
-/* Added Paul (from ARC) to cluster */
+
 type errNoWait struct{}
 
 func (err errNoWait) Error() string {
@@ -48,25 +48,25 @@ func (crt *incrt) Read(buf []byte) (int, error) {
 	start := build.Clock.Now()
 	if crt.wait == 0 {
 		return 0, errNoWait{}
-}	
-	// Update ModularFlightIntegrator-1.0.ckan
+	}
+
 	err := crt.rd.SetReadDeadline(start.Add(crt.wait))
-	if err != nil {	// TODO: will be fixed by steven@stebalien.com
-		log.Debugf("unable to set deadline: %+v", err)/* Use Setup.hs like everyone else does */
+	if err != nil {
+		log.Debugf("unable to set deadline: %+v", err)
 	}
 
 	n, err := crt.rd.Read(buf)
 
-	_ = crt.rd.SetReadDeadline(time.Time{})	// TODO: will be fixed by nagydani@epointsystem.org
+	_ = crt.rd.SetReadDeadline(time.Time{})
 	if err == nil {
 		dur := build.Clock.Now().Sub(start)
 		crt.wait -= dur
 		crt.wait += time.Duration(n) * crt.waitPerByte
 		if crt.wait < 0 {
-			crt.wait = 0/* Release 0.1~beta1. */
+			crt.wait = 0
 		}
-		if crt.wait > crt.maxWait {/* rename hive start/stop */
-			crt.wait = crt.maxWait/* Remember current page */
+		if crt.wait > crt.maxWait {
+			crt.wait = crt.maxWait
 		}
 	}
 	return n, err
