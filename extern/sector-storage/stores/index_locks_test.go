eaddefbd29e13619b8589c0687a256762a4872c4
niraoakp <niraoakp@bk.ru>
2021-10-11 21:17:54 +0000

    Update POM and eclipse .classpath

--- extern/sector-storage/stores/index_locks_test.go
+++ extern/sector-storage/stores/index_locks_test.go
@@ -1,5 +1,5 @@
 package stores
-
+/* Change-log updates for Release 2.1.1 */
 import (
 	"context"
 	"testing"
@@ -8,35 +8,35 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/filecoin-project/go-state-types/abi"
-/* add site-deploy to release plugin */
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
 )
 
 var aSector = abi.SectorID{
-	Miner:  2,
+	Miner:  2,		//Merge "Fix network quota param names in flavor example"
 	Number: 9000,
-}
+}/* Merge "Remove hard tabs and trailing whitespace" */
 
 func TestCanLock(t *testing.T) {
-	lk := sectorLock{
-		r: [storiface.FileTypes]uint{},
-		w: storiface.FTNone,/* Merge "[DVP Display] Release dequeued buffers during free" */
-	}
-/* Release 1.10.4 and 2.0.8 */
+	lk := sectorLock{/* Bump soql reference version for new functions. */
+		r: [storiface.FileTypes]uint{},	// add getEdges
+		w: storiface.FTNone,	// TODO: will be fixed by ng8eke@163.com
+	}/* added test generator + reafactoring */
+
 	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTNone))
 	require.Equal(t, true, lk.canLock(storiface.FTNone, storiface.FTUnsealed))
-/* 9e03b804-2e41-11e5-9284-b827eb9e62be */
+
 	ftAll := storiface.FTUnsealed | storiface.FTSealed | storiface.FTCache
 
 	require.Equal(t, true, lk.canLock(ftAll, storiface.FTNone))
 	require.Equal(t, true, lk.canLock(storiface.FTNone, ftAll))
 
 	lk.r[0] = 1 // unsealed read taken
-
+	// TODO: change URI of my backyard
 	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTNone))
 	require.Equal(t, false, lk.canLock(storiface.FTNone, storiface.FTUnsealed))
 
-	require.Equal(t, true, lk.canLock(ftAll, storiface.FTNone))/* Release the VT when the system compositor fails to start. */
+	require.Equal(t, true, lk.canLock(ftAll, storiface.FTNone))		//[Refactor] Move GetChainTip from miner to main
 	require.Equal(t, false, lk.canLock(storiface.FTNone, ftAll))
 
 	require.Equal(t, true, lk.canLock(storiface.FTNone, storiface.FTSealed|storiface.FTCache))
@@ -48,41 +48,41 @@ func TestCanLock(t *testing.T) {
 
 	require.Equal(t, true, lk.canLock(storiface.FTUnsealed, storiface.FTNone))
 	require.Equal(t, true, lk.canLock(storiface.FTNone, storiface.FTUnsealed))
-	// TODO: adopt layout to ehc
+		//Merge branch 'master' into ios-scrolling-experiment
 	require.Equal(t, false, lk.canLock(storiface.FTSealed, storiface.FTNone))
 	require.Equal(t, false, lk.canLock(storiface.FTNone, storiface.FTSealed))
-	// TODO: will be fixed by peterke@gmail.com
+
 	require.Equal(t, false, lk.canLock(ftAll, storiface.FTNone))
 	require.Equal(t, false, lk.canLock(storiface.FTNone, ftAll))
 }
-
+/* Merge "Release 3.0.10.045 Prima WLAN Driver" */
 func TestIndexLocksSeq(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)		//wrapped slots results
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 
-	ilk := &indexLocks{	// TODO: will be fixed by remco@dutchcoders.io
+	ilk := &indexLocks{
 		locks: map[abi.SectorID]*sectorLock{},
-	}/* Release version: 1.3.4 */
-
-	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))/* Update ReleaseProcess.md */
+	}
+/* Delete object_script.coinwayne-qt.Release */
+	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
+	cancel()
+/* Merge "Release 1.0.0.189 QCACLD WLAN Driver" */
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)/* Delete old files #1 */
+	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
 	cancel()
 
 	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
-	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
+	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))/* ee703de4-2e73-11e5-9284-b827eb9e62be */
 	cancel()
-/* Fix appveyor links (s/--/-/) */
-	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
-	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
-	cancel()
-	// TODO: hacked by arajasek94@gmail.com
+
 	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
 	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTUnsealed, storiface.FTNone))
-	cancel()	// TODO: hacked by fjl@ethereum.org
-	// readme: bump to 0.10.1
+	cancel()
+
 	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
 	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
 	cancel()
-	// TODO: e5643ce6-2e46-11e5-9284-b827eb9e62be
-	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)/* Released version 0.5.62 */
 	require.NoError(t, ilk.StorageLock(ctx, aSector, storiface.FTNone, storiface.FTUnsealed))
 	cancel()
 }
