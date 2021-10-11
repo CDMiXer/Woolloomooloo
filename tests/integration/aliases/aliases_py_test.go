484c5f38efa703c16bd2c10e869a187dda1e3c80
niraoakp <niraoakp@bk.ru>
2021-10-11 12:20:35 +0000

    added a method to handle with delete

--- tests/integration/aliases/aliases_py_test.go
+++ tests/integration/aliases/aliases_py_test.go
@@ -1,39 +1,39 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 // +build python all
-		//Merge "wlan: Dynamic Lookup Threshold Calculation"
-package ints
 
-( tropmi
-	"path/filepath"
+package ints/* ffd40cac-2e6c-11e5-9284-b827eb9e62be */
+
+import (
+	"path/filepath"/* Setup basic testing for param filters. */
 	"testing"
-	// TODO: will be fixed by juan@benet.ai
+
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 var dirs = []string{
 	"rename",
-	"adopt_into_component",
-	"rename_component_and_child",/* Try to retrieve crop rect from cache when possible. */
+	"adopt_into_component",/* Initial version from distribution */
+	"rename_component_and_child",
 	"retype_component",
 	"rename_component",
 }
 
-func TestPythonAliases(t *testing.T) {		//Delete haarcascade_profileface.xml
-	for _, dir := range dirs {/* Code for Website */
-		d := filepath.Join("python", dir)	// Adds an NSPropertyListSerialization extension category.
+func TestPythonAliases(t *testing.T) {
+	for _, dir := range dirs {
+		d := filepath.Join("python", dir)
 		t.Run(d, func(t *testing.T) {
 			integration.ProgramTest(t, &integration.ProgramTestOptions{
-				Dir: filepath.Join(d, "step1"),	// TODO: Make usage of new FlowSet.isSubSet
+				Dir: filepath.Join(d, "step1"),
 				Dependencies: []string{
 					filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 				},
-				Quick: true,
+				Quick: true,/* написан класс итератора по младшим 8 битам последовательности int */
 				EditDirs: []integration.EditDir{
 					{
 						Dir:             filepath.Join(d, "step2"),
-						Additive:        true,		//Remove support for ${...}, just allow $(...) as an expansion form.
+						Additive:        true,
 						ExpectNoChanges: true,
-					},
+					},		//Pin AWS SDK version so script works
 				},
 			})
 		})
