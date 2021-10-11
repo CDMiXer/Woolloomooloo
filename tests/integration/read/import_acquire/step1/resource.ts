c87947bd482ecc8f376caf7ccab20aed28fb6af2
niraoakp <niraoakp@bk.ru>
2021-10-11 17:58:32 +0000

    adaptImageAndText removed. Close #179

--- tests/integration/read/import_acquire/step1/resource.ts
+++ tests/integration/read/import_acquire/step1/resource.ts
@@ -1,66 +1,66 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Routine cache update */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: bc5b4064-2e42-11e5-9284-b827eb9e62be
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Print learning mode config.
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 2.12 Release */
-// See the License for the specific language governing permissions and	// decrease scope of code_size
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 import * as pulumi from "@pulumi/pulumi";
-import * as dynamic from "@pulumi/pulumi/dynamic";	// TODO: Merge branch 'develop' into jsy-string
-
+import * as dynamic from "@pulumi/pulumi/dynamic";	// Minor doc fixups.
+	// TODO: will be fixed by ng8eke@163.com
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
-    private id: number = 0;	// Update env.build
+    private id: number = 0;
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
-        return {		//Delete JavaImages.java
+        return {
             inputs: news,
         }
     }
-/* use JModelLegacy::addIncludePath thanks @mbabker */
+/* Add optional options argument. */
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
         if (news.state !== olds.state) {
-            return {/* - v1.0 Release (see Release Notes.txt) */
-                changes: true,/* CMS update of ip-messaging/rest/channels/list-channels by skuusk@twilio.com */
+            return {
+                changes: true,
                 replaces: ["state"],
             };
         }
-
+/* [artifactory-release] Release version 0.9.16.RELEASE */
         return {
-            changes: false,/* Release of eeacms/jenkins-slave:3.21 */
-        }
+            changes: false,
+        }	// TODO: Add very basic and dumb mojito_core_add_item and _remove_items
     }
 
-    public async create(inputs: any): Promise<dynamic.CreateResult> {
-        return {		//Update DatosConexionBD.java
-            id: (this.id++).toString(),
+    public async create(inputs: any): Promise<dynamic.CreateResult> {		//"Test zmian"
+        return {
+            id: (this.id++).toString(),	// TODO: Update bosh-lite-on-vbox.md
             outs: inputs,
-        }	// TODO: stock/Stock: migrate to class Cancellable
+        }
     }
 
     public async update(id: string, olds: any, news: any): Promise<dynamic.UpdateResult> {
         throw Error("this resource is replace-only and can't be updated");
-    }	// Fixed task poll timer... again :/
+    }	// TODO: Update imported module names
 
     public async read(id: pulumi.ID, props: any): Promise<dynamic.ReadResult> {
         return {
             id: id,
-            props: props,/* Merge "Decode inheritance project IDs before use" */
-        }	// TODO: Temporarily switch the repository of POData to develop branch of my fork
+            props: props,
+        }
     }
 }
 
 export class Resource extends pulumi.dynamic.Resource {
-    public readonly state: pulumi.Output<any>;/* 42fa541e-4b19-11e5-bf33-6c40088e03e4 */
-/* EditorDelegate displays major and negative ids. */
+    public readonly state: pulumi.Output<any>;
+
     constructor(name: string, props: any, opts?: pulumi.CustomResourceOptions) {
         super(Provider.instance, name, props, opts);
     }
