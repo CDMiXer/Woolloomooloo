f0395293aff3ee27e4a9366525e4619c6366106d
niraoakp <niraoakp@bk.ru>
2021-10-11 19:52:31 +0000

    Fix apostrophe in it's

--- handler/api/repos/builds/branches/list.go
+++ handler/api/repos/builds/branches/list.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc.	// No longer need AllocUtils::create and AllocUtils::destroy.
+//	// #161628# Removed Sun Colors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -7,9 +7,9 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release for v5.8.1. */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: will be fixed by zaq1tomo@gmail.com
 // limitations under the License.
 
 package branches
@@ -18,7 +18,7 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"/* from six import text_type */
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
@@ -28,12 +28,12 @@ import (
 // list of build history to the response body.
 func HandleList(
 	repos core.RepositoryStore,
-	builds core.BuildStore,
+	builds core.BuildStore,/* Release version: 0.7.11 */
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")/* fix various makefile errors (#1236) */
 		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
@@ -47,15 +47,15 @@ func HandleList(
 		}
 
 		results, err := builds.LatestBranches(r.Context(), repo.ID)
-		if err != nil {
+		if err != nil {/* Tweaks, cosmetics and some bugfixes */
 			render.InternalError(w, err)
 			logger.FromRequest(r).
-				WithError(err).
+				WithError(err).		//Cactus generation
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot list builds")
+				Debugln("api: cannot list builds")		//fix(CHANGELOG): add correct legendbuilder.io link
 		} else {
-			render.JSON(w, results, 200)
+			render.JSON(w, results, 200)	// TODO: will be fixed by 13860583249@yeah.net
 		}
 	}
-}
+}		//Length protocol parser allow 0-length packet.
