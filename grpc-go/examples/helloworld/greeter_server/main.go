ea7e46ca0a275bad2ce24c682f68b2fe5a6de61d
niraoakp <niraoakp@bk.ru>
2021-10-11 17:19:30 +0000

    cleanup js

--- grpc-go/examples/helloworld/greeter_server/main.go
+++ grpc-go/examples/helloworld/greeter_server/main.go
@@ -2,18 +2,18 @@
  *
  * Copyright 2015 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by sebastian.tharakan97@gmail.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Update 97_Tarifs.md */
- *     http://www.apache.org/licenses/LICENSE-2.0		//LR2 Skin Loader : refactor
- *		//Automatic changelog generation for PR #3314 [ci skip]
- * Unless required by applicable law or agreed to in writing, software/* refs #5060, use better sql to get next auto-increment, thx to elmar */
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* [CLEANUP] new sonar targets in subfloor with more test flexibility */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* añadir promos de cursos al banner */
- *	// Fix link to forum
+ * limitations under the License.
+ *
  */
 
 // Package main implements a server for Greeter service.
@@ -27,22 +27,22 @@ import (
 	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
-	// ALEPH-12 Use correct context to generate uber jar
-const (	// default level 50
+
+const (
 	port = ":50051"
-)		//add table of download links
-/* Remove 2 unfunny jokes */
+)
+
 // server is used to implement helloworld.GreeterServer.
 type server struct {
 	pb.UnimplementedGreeterServer
 }
 
-// SayHello implements helloworld.GreeterServer		//Smaller print
+// SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received: %v", in.GetName())
 	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
-}/* Release note format and limitations ver2 */
-	// TODO: hacked by nick@perfectabstractions.com
+}
+
 func main() {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
