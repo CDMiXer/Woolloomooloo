41310b902faf8893a93a66dd752ba7085fafd2f4
niraoakp <niraoakp@bk.ru>
2021-10-11 07:52:49 +0000

    skip py<37

--- tests/examples/compat/v0.10.0/minimal/index.ts
+++ tests/examples/compat/v0.10.0/minimal/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// reader-videoguard: correct 09ac tier start number
+/* @Release [io7m-jcanephora-0.33.0] */
 import { Config } from "@pulumi/pulumi";
 
 let config = new Config("minimal");
 console.log(`Hello, ${config.require("name")}!`);
 console.log(`Psst, ${config.require("secret")}`);
-
+		//Adding leading zero when aid is one digit number.
