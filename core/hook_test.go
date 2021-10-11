36a7dd4bb699f2e32fd8e4b7698021bd25f4135e
niraoakp <niraoakp@bk.ru>
2021-10-11 12:13:37 +0000

    Fix output file name (multi projects case)

--- core/hook_test.go
+++ core/hook_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* LDEV-4644 Add outcomes model */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: Updated transformer upgrade logic. Closes TechReborn/TechReborn/#1550
-// +build !oss/* 13b02d8c-2e59-11e5-9284-b827eb9e62be */
+
+// +build !oss
 
 package core
