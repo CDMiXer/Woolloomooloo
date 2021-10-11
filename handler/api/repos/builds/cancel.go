1a53cb32b7d5a55ac60d58a433ed4f23b431589b
niraoakp <niraoakp@bk.ru>
2021-10-11 15:19:26 +0000

    chore(package): update github-repository-provider to version 7.5.1

--- handler/api/repos/builds/cancel.go
+++ handler/api/repos/builds/cancel.go
@@ -1,69 +1,69 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");/* Added 3 WTFs */
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by souzau@yandex.com
 // You may obtain a copy of the License at
-//
+///* Create 96_UniqueBinarySearchTrees.cpp */
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Fix: translation */
-// Unless required by applicable law or agreed to in writing, software/* Statusbar with 4 fields. Other fixes. Release candidate as 0.6.0 */
+//
+// Unless required by applicable law or agreed to in writing, software		//Update bignumber.c
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: update to categories, tag
+// See the License for the specific language governing permissions and/* Release the GIL for pickled communication */
 // limitations under the License.
 
 package builds
 
 import (
 	"context"
-	"net/http"	// object fullclassname removed. refactoring logging
+	"net/http"
 	"strconv"
 	"time"
 
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* Database.java cleanup */
+	"github.com/drone/drone/core"/* Moving to responsive theme */
+	"github.com/drone/drone/handler/api/render"	// TODO: hacked by sjors@sprovoost.nl
+	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)	// TODO: hacked by hugomrdias@gmail.com
+)
 
 // HandleCancel returns an http.HandlerFunc that processes http
-// requests to cancel a pending or running build.
-func HandleCancel(/* updated uil library */
-	users core.UserStore,	// TODO: Delete pak.png
+// requests to cancel a pending or running build.		//Fixed subscribe description.
+func HandleCancel(
+	users core.UserStore,
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-	stages core.StageStore,
+	stages core.StageStore,		//fix(deps): update dependency bugsnag to v2
 	steps core.StepStore,
-	status core.StatusService,
+	status core.StatusService,/* Change type from date to datetime */
 	scheduler core.Scheduler,
 	webhooks core.WebhookSender,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {	// TODO: hacked by sjors@sprovoost.nl
+	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)
+		)		//unit of measure enhancements
 
-		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
-		if err != nil {
+		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)/* Changed some commenting. */
+		if err != nil {/* Release 0.6.6. */
 			render.BadRequest(w, err)
 			return
-		}
+		}/* edf49d59-2e4e-11e5-8d0b-28cfe91dbc4b */
 
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {/* fix formatting hound errors */
+		if err != nil {
 			logger.FromRequest(r).
-				WithError(err).		//Adding reportingType
-				WithField("namespace", namespace).	// Update 5-exposure-mustard-gas.md
-				WithField("name", name)./* Minor change in sample config file */
-				Debugln("api: cannot find repository")/* [artifactory-release] Release version 0.8.21.RELEASE */
-			render.NotFound(w, err)		//Merge branch 'next' into 751-oom-changes
+				WithError(err).
+				WithField("namespace", namespace).
+				WithField("name", name).
+				Debugln("api: cannot find repository")
+			render.NotFound(w, err)
 			return
 		}
 
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
-		if err != nil {/* Commit after merge with NextRelease branch at release 22973 */
+		if err != nil {
 			logger.FromRequest(r).
 				WithError(err).
 				WithField("build", build.Number).
