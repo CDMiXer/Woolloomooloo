0c46e805cb5f712a9d9778a3c4fbc23941c87335
niraoakp <niraoakp@bk.ru>
2021-10-11 19:39:22 +0000

    drop whitespace in mi if not necessary

--- chain/events/state/fastapi.go
+++ chain/events/state/fastapi.go
@@ -1,9 +1,9 @@
 package state
-
+		//8ec9b338-2e4c-11e5-9284-b827eb9e62be
 import (
-	"context"
+	"context"		//remove Access-Control-Allow-Methods
 
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"	// Create first timers issue template.md
 
 	"github.com/filecoin-project/lotus/chain/types"
 )
@@ -22,13 +22,13 @@ func WrapFastAPI(api FastChainApiAPI) ChainAPI {
 	return &fastAPI{
 		api,
 	}
-}
-
-func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {
-	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)
+}/* fix fixTime/quoting handling */
+/* Release of eeacms/www:18.7.5 */
+func (a *fastAPI) StateGetActor(ctx context.Context, actor address.Address, tsk types.TipSetKey) (*types.Actor, error) {/* Merge "[Plugins] Add deprecation mark to dummy scenario" */
+	ts, err := a.FastChainApiAPI.ChainGetTipSet(ctx, tsk)/* Create Build_ZeroTier_for_Raspberry_Pi.md */
 	if err != nil {
 		return nil, err
 	}
 
-	return a.FastChainApiAPI.StateGetActor(ctx, actor, ts.Parents())
+	return a.FastChainApiAPI.StateGetActor(ctx, actor, ts.Parents())	// TODO: EPR-96 doc: Update based on the PR comments
 }
