4bf37b2c35e66d5c376b1f08223cca6fab8ab105
niraoakp <niraoakp@bk.ru>
2021-10-11 20:41:44 +0000

    63b45e42-2e69-11e5-9284-b827eb9e62be

--- store/shared/migrate/sqlite/files/008_create_table_cron.sql
+++ store/shared/migrate/sqlite/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
--- name: create-table-cron/* escape type path param */
-		//0.83 barrels ping2
-CREATE TABLE IF NOT EXISTS cron (/* devel: Moved the CMA-ES implementation to 1.1.0 */
+-- name: create-table-cron
+
+CREATE TABLE IF NOT EXISTS cron (
  cron_id          INTEGER PRIMARY KEY AUTOINCREMENT
 ,cron_repo_id     INTEGER
-,cron_name        TEXT/* Fix bug where armor did 100 times normal damage reduction */
-,cron_expr        TEXT
-,cron_next        INTEGER	// TODO: Create ad_virtual_mailbox_maps.cf
-,cron_prev        INTEGER/* Release flag set for version 0.10.5.2 */
-,cron_event       TEXT
-,cron_branch      TEXT	// TODO: hacked by magik6k@gmail.com
+,cron_name        TEXT/* Deprecated method encode() removed due to getBytes("UTF-8") on sending string */
+,cron_expr        TEXT		//Add some more tooltips
+,cron_next        INTEGER
+,cron_prev        INTEGER
+,cron_event       TEXT	// TODO: hacked by davidad@alum.mit.edu
+,cron_branch      TEXT
 ,cron_target      TEXT
 ,cron_disabled    BOOLEAN
 ,cron_created     INTEGER
 ,cron_updated     INTEGER
-,cron_version     INTEGER
+,cron_version     INTEGER		//change assert
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-cron-repo
 
-CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);/* Fix misleading whitespace (clang4 complaint) */
+CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
 
--- name: create-index-cron-next	// TODO: Imported Upstream version 0.8.5
-
+-- name: create-index-cron-next
+	// TODO: Update lazycseq.py
 CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
