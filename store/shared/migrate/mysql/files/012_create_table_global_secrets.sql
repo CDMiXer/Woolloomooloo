1905c0dedfee7b27ea0f8bf64834729a40236853
niraoakp <niraoakp@bk.ru>
2021-10-11 05:47:32 +0000

    some changes... desin..

--- store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
+++ store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                INTEGER PRIMARY KEY AUTO_INCREMENT
 ,secret_namespace         VARCHAR(50)
 ,secret_name              VARCHAR(200)
-,secret_type              VARCHAR(50)/* Merge "frameworks/base/telephony: Release wakelock on RIL request send error" */
+,secret_type              VARCHAR(50)
 ,secret_data              BLOB
-,secret_pull_request      BOOLEAN	// TODO: rename del.list to files.list
+,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
-);	// TODO: merge_coverage
+);
