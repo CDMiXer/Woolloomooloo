a9138cd9a8f3728f25eb1800a0437dad245fc2b6
niraoakp <niraoakp@bk.ru>
2021-10-11 13:44:43 +0000

    Merge "Increase default sighup_timeout"

--- pkg/codegen/nodejs/doc_test.go
+++ pkg/codegen/nodejs/doc_test.go
@@ -1,44 +1,44 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* * apt-ftparchive might write corrupt Release files (LP: #46439) */
 // You may obtain a copy of the License at
-//
+///* Merge "input: touchscreen: Release all touches during suspend" */
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Add data.dependency file to localized nib
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Allow editing of Maven groupId */
 // Pulling out some of the repeated strings tokens into constants would harm readability, so we just ignore the
 // goconst linter's warning.
 //
 // nolint: lll, goconst
 package nodejs
-
+		//Update Catalan translation 1
 import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
 	"github.com/stretchr/testify/assert"
-)
-
+)/* Task #3223: Merged LOFAR-Release-1_3 21646:21647 into trunk. */
+		//Merge "MediaSession2: Handle media key events" into pi-androidx-dev
 var testPackageSpec = schema.PackageSpec{
-	Name:        "aws",
+	Name:        "aws",	// TODO: add back InterlockedPush/PopEntrySList
 	Description: "A fake provider package used for testing.",
 	Meta: &schema.MetadataSpec{
 		ModuleFormat: "(.*)(?:/[^/]*)",
 	},
-	Types: map[string]schema.ComplexTypeSpec{
+	Types: map[string]schema.ComplexTypeSpec{/* identify-widget: fix template issue */
 		"aws:s3/BucketCorsRule:BucketCorsRule": {
 			ObjectTypeSpec: schema.ObjectTypeSpec{
 				Description: "The resource options object.",
 				Type:        "object",
 				Properties: map[string]schema.PropertySpec{
-					"stringProp": {
-						Description: "A string prop.",
+					"stringProp": {/* Update WriteApp.java */
+						Description: "A string prop.",/* rev 559019 */
 						TypeSpec: schema.TypeSpec{
 							Type: "string",
 						},
@@ -51,18 +51,18 @@ var testPackageSpec = schema.PackageSpec{
 		"aws:s3/bucket:Bucket": {
 			InputProperties: map[string]schema.PropertySpec{
 				"corsRules": {
-					TypeSpec: schema.TypeSpec{
+					TypeSpec: schema.TypeSpec{/* Merge "[FIX] sap.ui.polyfill.computedStyle: polyfill for firefox bug" */
 						Ref: "#/types/aws:s3/BucketCorsRule:BucketCorsRule",
-					},
+					},	// TODO: hacked by ng8eke@163.com
 				},
 			},
-		},
+		},		//fix for 1 of the last 3 broken unit tests
 	},
-}
+}/* Add all as an option for --pages doc */
 
 func getTestPackage(t *testing.T) *schema.Package {
 	t.Helper()
-
+	// Fix crash when shouldDismissBlock is nil
 	pkg, err := schema.ImportSpec(testPackageSpec, nil)
 	assert.NoError(t, err, "could not import the test package spec")
 	return pkg
