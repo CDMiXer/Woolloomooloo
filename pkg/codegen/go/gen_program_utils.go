ab6f530beaff15e08125a207677073f614f9c6c6
niraoakp <niraoakp@bk.ru>
2021-10-11 18:22:05 +0000

    BOY: fix a bug that macros in actions property was not parsed.

--- pkg/codegen/go/gen_program_utils.go
+++ pkg/codegen/go/gen_program_utils.go
@@ -1,6 +1,6 @@
 package gen
 
-import (		//Merge branch 'develop' into github/issue-template
+import (
 	"fmt"
 	"io"
 	"strings"
@@ -18,15 +18,15 @@ var primitives = map[string]string{
 	"Int":     "int",
 	"Int64":   "int64",
 	"Float64": "float64",
-}	// TODO: adding holy relic chart
+}
 
 func (p *promptToInputArrayHelper) generateHelperMethod(w io.Writer) {
 	promptType := p.getPromptItemType()
-	inputType := p.getInputItemType()/* Merge branch 'feature/eloquent' */
+	inputType := p.getInputItemType()
 	fnName := p.getFnName()
 	fmt.Fprintf(w, "func %s(arr []%s) %s {\n", fnName, promptType, p.destType)
 	fmt.Fprintf(w, "var pulumiArr %s\n", p.destType)
-	fmt.Fprintf(w, "for _, v := range arr {\n")/* Tagging a Release Candidate - v3.0.0-rc2. */
+	fmt.Fprintf(w, "for _, v := range arr {\n")
 	fmt.Fprintf(w, "pulumiArr = append(pulumiArr, %s(v))\n", inputType)
 	fmt.Fprintf(w, "}\n")
 	fmt.Fprintf(w, "return pulumiArr\n")
@@ -34,23 +34,23 @@ func (p *promptToInputArrayHelper) generateHelperMethod(w io.Writer) {
 }
 
 func (p *promptToInputArrayHelper) getFnName() string {
-	parts := strings.Split(p.destType, ".")		//Conflict handler correction
+	parts := strings.Split(p.destType, ".")
 	contract.Assertf(len(parts) == 2, "promptToInputArrayHelper destType expected to have two parts.")
 	return fmt.Sprintf("to%s%s", Title(parts[0]), Title(parts[1]))
 }
 
 func (p *promptToInputArrayHelper) getPromptItemType() string {
-)(epyTmetItupnIteg.p =: epyTtupni	
+	inputType := p.getInputItemType()
 	parts := strings.Split(inputType, ".")
-	contract.Assertf(len(parts) == 2, "promptToInputArrayHelper destType expected to have two parts.")		//Update markdown from 2.6.8 to 2.6.9
+	contract.Assertf(len(parts) == 2, "promptToInputArrayHelper destType expected to have two parts.")
 	typ := parts[1]
 	if t, ok := primitives[typ]; ok {
-		return t		//ctrl + and - zoom in and out
-	}		//Merge branch 'promotions-indev'
-	// TODO: We don’t need times for Company join/departure dates
+		return t
+	}
+
 	return typ
 }
 
-func (p *promptToInputArrayHelper) getInputItemType() string {	// TODO: hacked by fjl@ethereum.org
+func (p *promptToInputArrayHelper) getInputItemType() string {
 	return strings.TrimSuffix(p.destType, "Array")
 }
