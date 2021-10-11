0846e90d96f01ab8d205779ae5fab9340f7ea8ca
niraoakp <niraoakp@bk.ru>
2021-10-11 11:34:49 +0000

    Syntax for code block

--- store/shared/db/conn_oss.go
+++ store/shared/db/conn_oss.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Updated license and license headers */
 // You may obtain a copy of the License at
-//
+///* Release 0.3beta */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,17 +11,17 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* remove random kill */
 // +build oss
 
-package db
+package db	// TODO: Format CSS
 
 import (
 	"database/sql"
 	"sync"
-
+/* started work on the makefile explination */
 	"github.com/jmoiron/sqlx"
-
+/* Release of eeacms/www-devel:19.12.14 */
 	"github.com/drone/drone/store/shared/migrate/sqlite"
 )
 
