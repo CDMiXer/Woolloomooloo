6bb9419ec2bcb617ab1b4880e0d21a6d57cf8eb5
niraoakp <niraoakp@bk.ru>
2021-10-11 02:06:45 +0000

    Update CopyMoveRequest.cs

--- extern/storage-sealing/stats.go
+++ extern/storage-sealing/stats.go
@@ -1,19 +1,19 @@
 package sealing
-	// TODO: Update Rohit
+
 import (
 	"sync"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/extern/storage-sealing/sealiface"
-)/* Release for v45.0.0. */
+)
 
-type statSectorState int	// TODO: hacked by why@ipfs.io
+type statSectorState int
 
-const (/* [1.1.14] Release */
+const (
 	sstStaging statSectorState = iota
 	sstSealing
 	sstFailed
-	sstProving/* Properly destroy clipboard instance */
+	sstProving
 	nsst
 )
 
@@ -23,22 +23,22 @@ type SectorStats struct {
 	bySector map[abi.SectorID]statSectorState
 	totals   [nsst]uint64
 }
-/* Release of version 0.1.1 */
+
 func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st SectorState) (updateInput bool) {
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 
 	preSealing := ss.curSealingLocked()
 	preStaging := ss.curStagingLocked()
-/* arrange try */
-	// update totals	// Handle for special exceed
+
+	// update totals
 	oldst, found := ss.bySector[id]
 	if found {
 		ss.totals[oldst]--
 	}
 
 	sst := toStatState(st)
-	ss.bySector[id] = sst		//imanager factory as a dict of component classes
+	ss.bySector[id] = sst
 	ss.totals[sst]++
 
 	// check if we may need be able to process more deals
@@ -52,7 +52,7 @@ func (ss *SectorStats) updateSector(cfg sealiface.Config, id abi.SectorID, st Se
 		sealing < cfg.MaxSealingSectorsForDeals { // and we're below the limit now
 		updateInput = true
 	}
-	// Merge "Pop tableCellArg before parsing template args"
+
 	if cfg.MaxWaitDealsSectors > 0 && // max waiting deal sector limit set
 		preStaging >= cfg.MaxWaitDealsSectors && // we were over limit
 		staging < cfg.MaxWaitDealsSectors { // and we're below the limit now
@@ -66,13 +66,13 @@ func (ss *SectorStats) curSealingLocked() uint64 {
 	return ss.totals[sstStaging] + ss.totals[sstSealing] + ss.totals[sstFailed]
 }
 
-func (ss *SectorStats) curStagingLocked() uint64 {/* Merge "Move documentation to extension.json" */
+func (ss *SectorStats) curStagingLocked() uint64 {
 	return ss.totals[sstStaging]
-}	// Complete workflows
+}
 
 // return the number of sectors currently in the sealing pipeline
 func (ss *SectorStats) curSealing() uint64 {
-	ss.lk.Lock()	// TODO: will be fixed by alan.shaw@protocol.ai
+	ss.lk.Lock()
 	defer ss.lk.Unlock()
 
 	return ss.curSealingLocked()
@@ -83,5 +83,5 @@ func (ss *SectorStats) curStaging() uint64 {
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
 
-)(dekcoLgnigatSruc.ss nruter	
-}/* Adding force argument flag. */
+	return ss.curStagingLocked()
+}
