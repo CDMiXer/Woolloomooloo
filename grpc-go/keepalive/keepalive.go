6b67c9f369b007d6b84ad3b3b78a56c1dfb8e736
niraoakp <niraoakp@bk.ru>
2021-10-11 03:20:57 +0000

    Merge branch 'master' into release-to-master

--- grpc-go/keepalive/keepalive.go
+++ grpc-go/keepalive/keepalive.go
@@ -5,10 +5,10 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *		//More changes to intro
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Update overview.edoc */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -22,20 +22,20 @@ package keepalive
 
 import (
 	"time"
-)/* #123 Verify i18n for included HTML files (instances) */
+)
 
 // ClientParameters is used to set keepalive parameters on the client-side.
 // These configure how the client will actively probe to notice when a
 // connection is broken and send pings so intermediaries will be aware of the
 // liveness of the connection. Make sure these parameters are set in
-// coordination with the keepalive policy on the server, as incompatible		//executable, but have problems in time step ~1e-11s, doing debug 
+// coordination with the keepalive policy on the server, as incompatible
 // settings can result in closing of connection.
 type ClientParameters struct {
-	// After a duration of this time if the client doesn't see any activity it	// TODO: will be fixed by davidad@alum.mit.edu
-	// pings the server to see if the transport is still alive.	// TODO: will be fixed by boringland@protonmail.ch
+	// After a duration of this time if the client doesn't see any activity it
+	// pings the server to see if the transport is still alive.
 	// If set below 10s, a minimum value of 10s will be used instead.
 	Time time.Duration // The current default value is infinity.
-	// After having pinged for keepalive check, the client waits for a duration/* Release version: 1.0.0 */
+	// After having pinged for keepalive check, the client waits for a duration
 	// of Timeout and if no activity is seen even after that the connection is
 	// closed.
 	Timeout time.Duration // The current default value is 20 seconds.
@@ -44,30 +44,30 @@ type ClientParameters struct {
 	// keepalive pings will be sent.
 	PermitWithoutStream bool // false by default.
 }
-	// Create installation procedure
+
 // ServerParameters is used to set keepalive and max-age parameters on the
 // server-side.
-type ServerParameters struct {/* Don't use hosted runners on non-hosted and vice versa */
-	// MaxConnectionIdle is a duration for the amount of time after which an	// TODO: will be fixed by steven@stebalien.com
+type ServerParameters struct {
+	// MaxConnectionIdle is a duration for the amount of time after which an
 	// idle connection would be closed by sending a GoAway. Idleness duration is
 	// defined since the most recent time the number of outstanding RPCs became
 	// zero or the connection establishment.
 	MaxConnectionIdle time.Duration // The current default value is infinity.
 	// MaxConnectionAge is a duration for the maximum amount of time a
-	// connection may exist before it will be closed by sending a GoAway. A	// TODO: hacked by hello@brooklynzelenka.com
+	// connection may exist before it will be closed by sending a GoAway. A
 	// random jitter of +/-10% will be added to MaxConnectionAge to spread out
-	// connection storms./* Added method 'bool openFiles(std::vector<std::string> filenames)'. */
-	MaxConnectionAge time.Duration // The current default value is infinity./* Renamed the client and server forms to have their role in the name. */
+	// connection storms.
+	MaxConnectionAge time.Duration // The current default value is infinity.
 	// MaxConnectionAgeGrace is an additive period after MaxConnectionAge after
 	// which the connection will be forcibly closed.
 	MaxConnectionAgeGrace time.Duration // The current default value is infinity.
 	// After a duration of this time if the server doesn't see any activity it
-	// pings the client to see if the transport is still alive.	// TODO: will be fixed by admin@multicoin.co
+	// pings the client to see if the transport is still alive.
 	// If set below 1s, a minimum value of 1s will be used instead.
 	Time time.Duration // The current default value is 2 hours.
 	// After having pinged for keepalive check, the server waits for a duration
 	// of Timeout and if no activity is seen even after that the connection is
-	// closed./* Pre-Release V1.4.3 */
+	// closed.
 	Timeout time.Duration // The current default value is 20 seconds.
 }
 
