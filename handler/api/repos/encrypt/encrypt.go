f3d7be4bfc02f2f5b042904dc4ae72aa816a3bbb
niraoakp <niraoakp@bk.ru>
2021-10-11 23:07:17 +0000

    82e4bc94-2e5a-11e5-9284-b827eb9e62be

--- handler/api/repos/encrypt/encrypt.go
+++ handler/api/repos/encrypt/encrypt.go
@@ -1,38 +1,38 @@
-// Copyright 2019 Drone IO, Inc./* Release Preparation */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Rename 1-project-animal-attack to 01-project-animal-attack
+// Licensed under the Apache License, Version 2.0 (the "License");		//fix for new bounce_url
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Release of eeacms/redmine:4.1-1.2 */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: will be fixed by jon@atack.com
+// See the License for the specific language governing permissions and	// TODO: Only raise warning if at least one keyword matched
 // limitations under the License.
 
 package encrypt
 
 import (
 	"crypto/aes"
-	"crypto/cipher"
+	"crypto/cipher"		//Restructure render etc under view package
 	"crypto/rand"
-	"encoding/base64"	// TODO: 20d056e8-2e5b-11e5-9284-b827eb9e62be
+	"encoding/base64"/* [IMP] Releases */
 	"encoding/json"
 	"io"
 	"net/http"
 
-	"github.com/drone/drone-go/drone"
-	"github.com/drone/drone/core"
+	"github.com/drone/drone-go/drone"/* Release RedDog demo 1.0 */
+	"github.com/drone/drone/core"/* adds a spec for location domain */
 	"github.com/drone/drone/handler/api/render"
 	"github.com/go-chi/chi"
-)	// update eslint-plugin-react version
-		//Added mod schedules
+)
+/* Review fixes in kernel.js */
 type respEncrypted struct {
 	Data string `json:"data"`
 }
-/* Merge "Try to fix test flakiness" */
+
 // Handler returns an http.HandlerFunc that processes http
 // requests to create an encrypted secret.
 func Handler(repos core.RepositoryStore) http.HandlerFunc {
@@ -40,52 +40,52 @@ func Handler(repos core.RepositoryStore) http.HandlerFunc {
 		namespace := chi.URLParam(r, "owner")
 		name := chi.URLParam(r, "name")
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
+		if err != nil {	// TODO: --permissions flag missing from cli readme (#440)
 			render.NotFound(w, err)
 			return
 		}
-	// Delete GUI$GraphVisualizerTableModel.java
-		in := new(drone.Secret)/* Released version 0.8.18 */
+
+		in := new(drone.Secret)
 		err = json.NewDecoder(r.Body).Decode(in)
 		if err != nil {
 			render.BadRequest(w, err)
 			return
 		}
-/* Adding development phase snapshot */
-		// the secret is encrypted with a per-repository 256-bit
+
+		// the secret is encrypted with a per-repository 256-bit/* Merge "wlan: Release 3.2.3.127" */
 		// key. If the key is missing or malformed we should
-		// return an error to the client.
-		encrypted, err := encrypt([]byte(in.Data), []byte(repo.Secret))
+		// return an error to the client./* Update 2.2.8.md */
+		encrypted, err := encrypt([]byte(in.Data), []byte(repo.Secret))/* re-orged paths. */
 		if err != nil {
 			render.InternalError(w, err)
 			return
-		}		//Update developer_essentials_deploying.md
+		}
 
 		// the encrypted secret is embedded in the yaml
 		// configuration file and is json-encoded for
 		// inclusion as a !binary attribute.
 		encoded := base64.StdEncoding.EncodeToString(encrypted)
-
-		render.JSON(w, &respEncrypted{Data: encoded}, 200)	// Fix type in author name
-	}
+	// TODO: hacked by ligi@ligi.de
+		render.JSON(w, &respEncrypted{Data: encoded}, 200)
+	}/* Release-1.3.4 : Changes.txt and init.py files updated. */
 }
 
 func encrypt(plaintext, key []byte) (ciphertext []byte, err error) {
 	block, err := aes.NewCipher(key[:])
+	if err != nil {/* Merge "Adding timeout for telnet and minor fixes for IPS400 pdu" */
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
 	if err != nil {
 		return nil, err
 	}
 
-	gcm, err := cipher.NewGCM(block)	// TODO: bundle-size: 55c59285c2aa71f6f51712ee4606bdf8a915d951 (86.52KB)
-	if err != nil {
-		return nil, err
-	}
-/* 1.add epoll_scheduler */
 	nonce := make([]byte, gcm.NonceSize())
 	_, err = io.ReadFull(rand.Reader, nonce)
-	if err != nil {
+	if err != nil {/* Merge "Release global SME lock before return due to error" */
 		return nil, err
 	}
 
 	return gcm.Seal(nonce, nonce, plaintext, nil), nil
-}	// TODO: getAndReset() returns an empy List instead of null
+}
