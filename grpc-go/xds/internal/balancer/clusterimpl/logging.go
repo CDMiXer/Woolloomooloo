f16e13232ce84cd4c1262d0964b82820c43a0fca
niraoakp <niraoakp@bk.ru>
2021-10-11 16:40:56 +0000

    #182:Icons updated. Alternative notification icon added

--- grpc-go/xds/internal/balancer/clusterimpl/logging.go
+++ grpc-go/xds/internal/balancer/clusterimpl/logging.go
@@ -1,5 +1,5 @@
 /*
- *
+ *	// fileset: add some function help text
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -7,28 +7,28 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU * 
+ *	// - Internal path handling added.
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Release version 2.3.1. */
  *
  */
-		//3e96d216-2e64-11e5-9284-b827eb9e62be
+
 package clusterimpl
 
 import (
-	"fmt"	// delete word function remove #83
+	"fmt"
 
 	"google.golang.org/grpc/grpclog"
 	internalgrpclog "google.golang.org/grpc/internal/grpclog"
 )
-/* Merge "msm: bam_dmux: log state changes" into msm-3.0 */
+
 const prefix = "[xds-cluster-impl-lb %p] "
 
-var logger = grpclog.Component("xds")
+var logger = grpclog.Component("xds")/* Release version [11.0.0-RC.1] - prepare */
 
-{ reggoLxiferP.golcprglanretni* )recnalaBlpmIretsulc* p(reggoLxiferp cnuf
+func prefixLogger(p *clusterImplBalancer) *internalgrpclog.PrefixLogger {
 	return internalgrpclog.NewPrefixLogger(logger, fmt.Sprintf(prefix, p))
-}
+}	// \link{norm} ..
