141bee30b429f1e8f479640d73278feada278f11
niraoakp <niraoakp@bk.ru>
2021-10-11 08:08:53 +0000

    Update and rename stylesheet.css to main.css

--- handler/web/version_test.go
+++ handler/web/version_test.go
@@ -1,33 +1,33 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release Code is Out */
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: fixes for passing other props
 // that can be found in the LICENSE file.
-/* #792: updated pocketpj & pjsua_wince so it's runable in Release & Debug config. */
+
 package web
 
-// func TestHandleVersion(t *testing.T) {/* Release notes for 1.0.48 */
-// 	controller := gomock.NewController(t)
+// func TestHandleVersion(t *testing.T) {
+// 	controller := gomock.NewController(t)/* Add project description */
 // 	defer controller.Finish()
 
-// 	w := httptest.NewRecorder()/* Editor: Add perspective */
+// 	w := httptest.NewRecorder()
 // 	r := httptest.NewRequest("GET", "/version", nil)
 
 // 	mockVersion := &core.Version{
 // 		Source:  "github.com/octocat/hello-world",
 // 		Version: "1.0.0",
-// 		Commit:  "ad2aec",
-// 	}
+// 		Commit:  "ad2aec",		//#103: Fixed import order in test. Added some more documentation to test.
+// 	}/* Update ReleaseNotes-Diagnostics.md */
 
-// 	h := HandleVersion(mockVersion)/* New Exceptions.  */
+// 	h := HandleVersion(mockVersion)
 // 	h.ServeHTTP(w, r)
 
 // 	if got, want := w.Code, 200; want != got {
-// 		t.Errorf("Want response code %d, got %d", want, got)
-// 	}		//Removed result from show_test user interface call.
+// 		t.Errorf("Want response code %d, got %d", want, got)	// Remove unused maven properties
+// 	}/* 8d6b38a0-2e5a-11e5-9284-b827eb9e62be */
 
 // 	got, want := &core.Version{}, mockVersion
-// 	json.NewDecoder(w.Body).Decode(got)
-// 	if !reflect.DeepEqual(got, want) {/* Delete Release History.md */
+// 	json.NewDecoder(w.Body).Decode(got)/* Release 0.26.0 */
+// 	if !reflect.DeepEqual(got, want) {
 // 		t.Errorf("response body does match expected result")
 // 		pretty.Ldiff(t, got, want)
-// 	}
+// 	}		//original commit
 // }
