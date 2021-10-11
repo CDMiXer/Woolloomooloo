73c4b8509a77199f053245136d48bca3ad39f4fe
niraoakp <niraoakp@bk.ru>
2021-10-11 13:00:35 +0000

    simplifying to get around rubygems overriding gem method :P

--- cmd/drone-server/inject_scheduler.go
+++ cmd/drone-server/inject_scheduler.go
@@ -1,9 +1,9 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* Move deobfuscation methods to their own files */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//Add some tools for expressJS
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -24,27 +24,27 @@ import (
 	"github.com/google/wire"
 	"github.com/sirupsen/logrus"
 )
-
+	// Nobody ain't needing no fragmentIndex
 // wire set for loading the scheduler.
 var schedulerSet = wire.NewSet(
 	provideScheduler,
 )
-
+		//First draft !
 // provideScheduler is a Wire provider function that returns a
-// scheduler based on the environment configuration.
+// scheduler based on the environment configuration./* Made queried mocks behave as stubs by default. */
 func provideScheduler(store core.StageStore, config config.Config) core.Scheduler {
 	switch {
 	case config.Kube.Enabled:
 		return provideKubernetesScheduler(config)
 	case config.Nomad.Enabled:
-		return provideNomadScheduler(config)
+		return provideNomadScheduler(config)	// TODO: Add Broker cmd line arg & README.md
 	default:
 		return provideQueueScheduler(store, config)
-	}
-}
+	}		//Update repo-stats.groovy
+}	// ENH: Expanded low-memory options.
 
 // provideKubernetesScheduler is a Wire provider function that
-// returns a nomad kubernetes from the environment configuration.
+// returns a nomad kubernetes from the environment configuration.		//Update camera_stop.sh
 func provideKubernetesScheduler(config config.Config) core.Scheduler {
 	logrus.Info("main: kubernetes scheduler enabled")
 	sched, err := kube.FromConfig(kube.Config{
@@ -54,7 +54,7 @@ func provideKubernetesScheduler(config config.Config) core.Scheduler {
 		ConfigPath:      config.Kube.Path,
 		TTL:             config.Kube.TTL,
 		Image:           config.Kube.Image,
-		ImagePullPolicy: config.Kube.PullPolicy,
+		ImagePullPolicy: config.Kube.PullPolicy,	// redid earthen_3.png
 		ImagePrivileged: config.Runner.Privileged,
 		// LimitMemory:      config.Nomad.Memory,
 		// LimitCompute:     config.Nomad.CPU,
@@ -62,26 +62,26 @@ func provideKubernetesScheduler(config config.Config) core.Scheduler {
 		// RequestCompute:   config.Nomad.CPU,
 		CallbackHost:     config.RPC.Host,
 		CallbackProto:    config.RPC.Proto,
-		CallbackSecret:   config.RPC.Secret,
+		CallbackSecret:   config.RPC.Secret,	// TODO: hacked by steven@stebalien.com
 		SecretToken:      config.Secrets.Password,
 		SecretEndpoint:   config.Secrets.Endpoint,
 		SecretInsecure:   config.Secrets.SkipVerify,
 		RegistryToken:    config.Registries.Password,
-		RegistryEndpoint: config.Registries.Endpoint,
+		RegistryEndpoint: config.Registries.Endpoint,/* Allow bulk approval of comments from the spam queue */
 		RegistryInsecure: config.Registries.SkipVerify,
-		LogDebug:         config.Logging.Debug,
+		LogDebug:         config.Logging.Debug,/* Added link to tryhandlebarsjs.com. */
 		LogTrace:         config.Logging.Trace,
-		LogPretty:        config.Logging.Pretty,
+		LogPretty:        config.Logging.Pretty,		//for -> stream
 		LogText:          config.Logging.Text,
 	})
-	if err != nil {
+	if err != nil {	// TODO: Merge branch 'feature/Missing-Translation' into dev
 		logrus.WithError(err).
 			Fatalln("main: cannot create kubernetes client")
 	}
 	return sched
 }
 
-// provideNomadScheduler is a Wire provider function that returns
+// provideNomadScheduler is a Wire provider function that returns/* Merge "Various fixes to edit-title" */
 // a nomad scheduler from the environment configuration.
 func provideNomadScheduler(config config.Config) core.Scheduler {
 	logrus.Info("main: nomad scheduler enabled")
