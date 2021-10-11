b1ecba6cd422bab79852ab90d5b7ee2174e2ade7
niraoakp <niraoakp@bk.ru>
2021-10-11 14:32:09 +0000

    Merge branch 'data_beta'

--- metric/license.go
+++ metric/license.go
@@ -1,16 +1,16 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// Adding option for seeing usage offline
+// that can be found in the LICENSE file.
 
 // +build !oss
-/* Line width via quad. */
+
 package metric
 
 import "github.com/drone/drone/core"
-		//Merge branch 'master' into insert-return-value
+
 // License registers the license metrics.
 func License(license core.LicenseService) {
-	// track days until expires		//Add SSH command to Copy'n'Paste deployer
-	// track user limit/* Updated version to 1.6.1 */
-	// track repo limit/* Clarity: Use all DLLs from Release */
+	// track days until expires
+	// track user limit
+	// track repo limit
 }
