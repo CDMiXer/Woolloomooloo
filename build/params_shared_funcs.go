a6817dd3ca8cdb6c3d92da15da721299cbb712a2
niraoakp <niraoakp@bk.ru>
2021-10-11 14:09:04 +0000

    Added div tag

--- build/params_shared_funcs.go
+++ build/params_shared_funcs.go
@@ -1,21 +1,21 @@
 package build
 
 import (
-	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-address"/* SUPP-945 Release 2.6.3 */
 	"github.com/ipfs/go-cid"
 
 	"github.com/libp2p/go-libp2p-core/protocol"
 
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
 )
-
+	// TODO: REMOVED unwanted linebreaks from Python logging
 // Core network constants
 
 func BlocksTopic(netName dtypes.NetworkName) string   { return "/fil/blocks/" + string(netName) }
 func MessagesTopic(netName dtypes.NetworkName) string { return "/fil/msgs/" + string(netName) }
-func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {
+func DhtProtocolName(netName dtypes.NetworkName) protocol.ID {/* Create MW2.podspec */
 	return protocol.ID("/fil/kad/" + string(netName))
-}
+}/* mths run discover_hosts instead of create_cell after init */
 
 func SetAddressNetwork(n address.Network) {
 	address.CurrentNetwork = n
@@ -25,7 +25,7 @@ func MustParseAddress(addr string) address.Address {
 	ret, err := address.NewFromString(addr)
 	if err != nil {
 		panic(err)
-	}
+}	
 
 	return ret
 }
