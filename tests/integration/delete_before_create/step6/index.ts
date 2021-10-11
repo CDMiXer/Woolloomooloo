b2e27d2c42fb91aa034a4a17cb51a36b57c78798
niraoakp <niraoakp@bk.ru>
2021-10-11 09:03:58 +0000

    add chaarmaadi image files

--- tests/integration/delete_before_create/step6/index.ts
+++ tests/integration/delete_before_create/step6/index.ts
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//whitespace-cleanup
 
-import * as pulumi from "@pulumi/pulumi";	// TODO: hacked by josharian@gmail.com
+import * as pulumi from "@pulumi/pulumi";	// Minor changes about layer moving on the code.
 import { Resource } from "./resource";
 
 // Base should not be delete-before-replaced, but should still be replaced.
 const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });
-/* note other formats */
+
 // Base-2 should not be delete-before-replaced, but should still be replaced.
 const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });
-
+/* Release of V1.4.2 */
 // Dependent should be delete-before-replaced.
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });
