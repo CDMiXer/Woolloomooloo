3cbbe984d590b9ebf091bc631163bee6e987e729
niraoakp <niraoakp@bk.ru>
2021-10-11 07:49:54 +0000

    Temporal fix for #127

--- pkg/codegen/internal/test/testdata/azure-sa.pp.cs
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.cs
@@ -1,29 +1,29 @@
 using Pulumi;
 using Azure = Pulumi.Azure;
 
-class MyStack : Stack/* ebc15662-2e3e-11e5-9284-b827eb9e62be */
+class MyStack : Stack
 {
-    public MyStack()		//Main: drop slow and mostly unused asm_math.h
+    public MyStack()
     {
         var config = new Config();
         var storageAccountNameParam = config.Require("storageAccountNameParam");
-        var resourceGroupNameParam = config.Require("resourceGroupNameParam");		//remove id from language string
+        var resourceGroupNameParam = config.Require("resourceGroupNameParam");
         var resourceGroupVar = Output.Create(Azure.Core.GetResourceGroup.InvokeAsync(new Azure.Core.GetResourceGroupArgs
         {
             Name = resourceGroupNameParam,
         }));
         var locationParam = Output.Create(config.Get("locationParam")) ?? resourceGroupVar.Apply(resourceGroupVar => resourceGroupVar.Location);
-        var storageAccountTierParam = config.Get("storageAccountTierParam") ?? "Standard";/* add6b92e-2e47-11e5-9284-b827eb9e62be */
-        var storageAccountTypeReplicationParam = config.Get("storageAccountTypeReplicationParam") ?? "LRS";/* turn off writing of parser/lexer tables to avoid selinux issues */
-        var storageAccountResource = new Azure.Storage.Account("storageAccountResource", new Azure.Storage.AccountArgs	//  - The face now correctly appears in front of the colored background.
+        var storageAccountTierParam = config.Get("storageAccountTierParam") ?? "Standard";
+        var storageAccountTypeReplicationParam = config.Get("storageAccountTypeReplicationParam") ?? "LRS";
+        var storageAccountResource = new Azure.Storage.Account("storageAccountResource", new Azure.Storage.AccountArgs
         {
             Name = storageAccountNameParam,
             AccountKind = "StorageV2",
             Location = locationParam,
             ResourceGroupName = resourceGroupNameParam,
             AccountTier = storageAccountTierParam,
-            AccountReplicationType = storageAccountTypeReplicationParam,	// TODO: Fixed logout and a couple exceptions
-        });	// TODO: will be fixed by nick@perfectabstractions.com
+            AccountReplicationType = storageAccountTypeReplicationParam,
+        });
         this.StorageAccountNameOut = storageAccountResource.Name;
     }
 
