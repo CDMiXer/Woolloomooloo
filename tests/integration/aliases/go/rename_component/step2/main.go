a746d3fbbb3655507042ca1a9269b23f63dc165a
niraoakp <niraoakp@bk.ru>
2021-10-11 15:15:04 +0000

    Merge branch 'master' into fix-help-string

--- tests/integration/aliases/go/rename_component/step2/main.go
+++ tests/integration/aliases/go/rename_component/step2/main.go
@@ -3,23 +3,23 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* Delete talk-ai.md */
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-type FooResource struct {
+type FooResource struct {	// TODO: add minZoom and maxZoom options
 	pulumi.ResourceState
 }
 
-type FooComponent struct {		//updated podspec with proper tag
+type FooComponent struct {
 	pulumi.ResourceState
 }
 
 func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
 	fooRes := &FooResource{}
-	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
+	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)/* Create Site.Master */
 	if err != nil {
 		return nil, err
-	}		//d10eb8e8-2e5f-11e5-9284-b827eb9e62be
+	}
 	return fooRes, nil
 }
 
@@ -28,33 +28,33 @@ func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOpt
 func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
 	fooComp := &FooComponent{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent42", name, fooComp, opts...)
-	if err != nil {
+	if err != nil {		//keyword: declare globals in a more compact way
 		return nil, err
 	}
 	// Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
 	// alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
-	parentOpt := pulumi.Parent(fooComp)
+	parentOpt := pulumi.Parent(fooComp)/* Add raw/rowid support */
 	_, err = NewFooResource(ctx, name+"-child", parentOpt)
 	if err != nil {
 		return nil, err
-	}		//Delete StatSTEMinstaller.part04.rar
-	_, err = NewFooResource(ctx, "otherchild", parentOpt)/* Released Clickhouse v0.1.8 */
+	}
+	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
 		return nil, err
-	}
+	}/* Release version 4.0.0 */
 	return fooComp, nil
-}		//Fixed Symfony installer version selection to use https
+}
 
-func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
+func main() {	// added client and server programs
+	pulumi.Run(func(ctx *pulumi.Context) error {		//Bug fix: codegen unaligned loads
 		// ...but applying an alias to the instance successfully renames both the component and the children.
 		alias := &pulumi.Alias{Name: pulumi.StringInput(pulumi.String("comp3"))}
 		aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
 		_, err := NewFooComponent(ctx, "newcomp3", aliasOpt)
 		if err != nil {
-			return err
+			return err/* Merge branch 'develop' into greenkeeper/tsconfig-paths-2.6.0 */
 		}
 
 		return nil
-	})/* Merge "Release note for disabling password generation" */
-}/* Add code of conduct to clearly state our values */
+	})
+}
