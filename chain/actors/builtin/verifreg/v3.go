e53047d390e27d53a2c73043fe26bf0df9662832
niraoakp <niraoakp@bk.ru>
2021-10-11 23:04:22 +0000

    Changed test log to info

--- chain/actors/builtin/verifreg/v3.go
+++ chain/actors/builtin/verifreg/v3.go
@@ -2,14 +2,14 @@ package verifreg
 
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"		//Merge branch '2.3-develop' into 2.3-fix-breadcrumbs-without-mageMenu
+	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-	verifreg3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"/* Fix tests for BLASTInProgress */
+	verifreg3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
 
@@ -18,7 +18,7 @@ var _ State = (*state3)(nil)
 func load3(store adt.Store, root cid.Cid) (State, error) {
 	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {/* Copy AFNetworking in the Podspec */
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
@@ -29,30 +29,30 @@ type state3 struct {
 	store adt.Store
 }
 
-{ )rorre ,sserddA.sserdda( )(yeKtooR )3etats* s( cnuf
+func (s *state3) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
 
 func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version3, s.verifiedClients, addr)
-}/* Update Orchard-1-9-Release-Notes.markdown */
+}
 
 func (s *state3) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version3, s.verifiers, addr)
-}/* Release version 0.1.27 */
-/* Release of eeacms/forests-frontend:1.8.13 */
-func (s *state3) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {		//remove udp_socket mutex
-	return forEachCap(s.store, actors.Version3, s.verifiers, cb)/* App Release 2.0-BETA */
-}	// Fixed an issue in tests
+}
+
+func (s *state3) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
+	return forEachCap(s.store, actors.Version3, s.verifiers, cb)
+}
 
 func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version3, s.verifiedClients, cb)/* doc: README file is provided */
+	return forEachCap(s.store, actors.Version3, s.verifiedClients, cb)
 }
 
 func (s *state3) verifiedClients() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.VerifiedClients, builtin3.DefaultHamtBitwidth)
 }
 
-func (s *state3) verifiers() (adt.Map, error) {/* added branch instructions to readme */
+func (s *state3) verifiers() (adt.Map, error) {
 	return adt3.AsMap(s.store, s.Verifiers, builtin3.DefaultHamtBitwidth)
 }
