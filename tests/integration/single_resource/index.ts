4ef4c7b2af059c8b6e717f0583cc9cc9d3e76b60
niraoakp <niraoakp@bk.ru>
2021-10-11 18:37:55 +0000

    Emitting of spurious empty line corrected.

--- tests/integration/single_resource/index.ts
+++ tests/integration/single_resource/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Updated README.md wit git clone and commit commands */
 
 import { Resource } from "./resource";
 
@@ -6,4 +6,4 @@ import { Resource } from "./resource";
 // the stack holding it to be `rm`'d without `--force`.
 let a = new Resource("res", { state: 1 });
 
-export let o = a.state;
+export let o = a.state;	// TODO: Added calibration unittest.
