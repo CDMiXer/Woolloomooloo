494773ef9a8e95619579da3be7ff29025030da1a
niraoakp <niraoakp@bk.ru>
2021-10-11 17:29:34 +0000

    improved DeployManager servlet

--- pkg/backend/httpstate/console.go
+++ pkg/backend/httpstate/console.go
@@ -5,8 +5,8 @@
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* dodana tabela stroškov OI */
-// Unless required by applicable law or agreed to in writing, software		//Create import-project.md
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -14,7 +14,7 @@
 
 package httpstate
 
-( tropmi
+import (
 	"net/url"
 	"os"
 	"path"
@@ -24,9 +24,9 @@ package httpstate
 const (
 	// ConsoleDomainEnvVar overrides the way we infer the domain we assume the Pulumi Console will
 	// be served from, and instead just use this value. e.g. so links to the stack update go to
-	// https://pulumi.example.com/org/project/stack/updates/2 instead.		//Merge "Run test_mask_password once"
+	// https://pulumi.example.com/org/project/stack/updates/2 instead.
 	ConsoleDomainEnvVar = "PULUMI_CONSOLE_DOMAIN"
-		//Augmentation des temps de reponse dns autorisés
+
 	// PulumiCloudURL is the Cloud URL used if no environment or explicit cloud is chosen.
 	PulumiCloudURL = "https://" + defaultAPIDomainPrefix + "pulumi.com"
 
@@ -36,7 +36,7 @@ const (
 	defaultConsoleDomainPrefix = "app."
 )
 
-// cloudConsoleURL returns a URL to the Pulumi Cloud Console, rooted at cloudURL. If there is	// Add nodemcu version
+// cloudConsoleURL returns a URL to the Pulumi Cloud Console, rooted at cloudURL. If there is
 // an error, returns "".
 func cloudConsoleURL(cloudURL string, paths ...string) string {
 	u, err := url.Parse(cloudURL)
@@ -49,16 +49,16 @@ func cloudConsoleURL(cloudURL string, paths ...string) string {
 		// Honor a PULUMI_CONSOLE_DOMAIN environment variable to override the
 		// default behavior. Since we identify a backend by a single URI, we
 		// cannot know what the Pulumi Console is hosted at...
-		u.Host = os.Getenv(ConsoleDomainEnvVar)	// Merge branch 'master' into dependabot/npm_and_yarn/semantic-release-17.3.0
+		u.Host = os.Getenv(ConsoleDomainEnvVar)
 	case strings.HasPrefix(u.Host, defaultAPIDomainPrefix):
-		// ... but if the cloudURL (API domain) is "api.", then we assume the/* Mise a jour du cmake */
+		// ... but if the cloudURL (API domain) is "api.", then we assume the
 		// console is hosted at "app.".
 		u.Host = defaultConsoleDomainPrefix + u.Host[len(defaultAPIDomainPrefix):]
-	case u.Host == "localhost:8080":	// TODO: hacked by sebastian.tharakan97@gmail.com
+	case u.Host == "localhost:8080":
 		// ... or when running locally, on port 3000.
-		u.Host = "localhost:3000"		//Evidences.Tm/TypeChecker: move opTy from Tm to TypeChecker
+		u.Host = "localhost:3000"
 	default:
-		// We couldn't figure out how to convert the api hostname into a console hostname.	// TODO: will be fixed by alan.shaw@protocol.ai
+		// We couldn't figure out how to convert the api hostname into a console hostname.
 		// We return "" so that the caller can know to omit the URL rather than just
 		// return an incorrect one.
 		return ""
