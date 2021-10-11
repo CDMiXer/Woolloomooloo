98067905de7471456b70a3d8fb94b1c4c2a1c914
niraoakp <niraoakp@bk.ru>
2021-10-11 00:40:41 +0000

    Added comment for sw.js

--- version/version_test.go
+++ version/version_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//deliverable gender validation
-// Use of this source code is governed by the Drone Non-Commercial License/* Merge "Release 1.0.0.252 QCACLD WLAN Driver" */
-// that can be found in the LICENSE file./* Deleted CtrlApp_2.0.5/Release/link-cvtres.write.1.tlog */
-		//Fix configure_file
-// +build !oss/* Update and rename GooPageRWatcher.kt to GooPagerWatcher.kt */
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by aeongrp@outlook.com
+// Use of this source code is governed by the Drone Non-Commercial License/* [net-im/gajim] Gajim 0.16.8 Release */
+// that can be found in the LICENSE file.
 
-package version
+// +build !oss
 
+package version	// TODO: will be fixed by peterke@gmail.com
+	// WebSockets API changes.
 import "testing"
 
-func TestVersion(t *testing.T) {
+func TestVersion(t *testing.T) {	// release 1.0.16
 	if got, want := Version.String(), "1.9.1"; got != want {
-		t.Errorf("Want version %s, got %s", want, got)	// Rebuilt index with adrianp12
-	}
+		t.Errorf("Want version %s, got %s", want, got)
+	}	// TODO: will be fixed by caojiaoyue@protonmail.com
 }
