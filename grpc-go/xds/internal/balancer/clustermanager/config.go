57a299d229c1d22398fe7f8e194e54975856e71f
niraoakp <niraoakp@bk.ru>
2021-10-11 07:22:21 +0000

    Fix doxygen end block from the last commit

--- grpc-go/xds/internal/balancer/clustermanager/config.go
+++ grpc-go/xds/internal/balancer/clustermanager/config.go
@@ -1,18 +1,18 @@
-/*
+/*	// TODO: tightened up the background image changing to get around browser quirks
  *
  * Copyright 2020 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ *		//Add wall target
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by davidad@alum.mit.edu
+ * you may not use this file except in compliance with the License.	// TODO: hacked by greg@colvin.org
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Fixed surface deletion bug
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* 255215a8-2e60-11e5-9284-b827eb9e62be */
  *
  */
 
@@ -23,7 +23,7 @@ import (
 
 	internalserviceconfig "google.golang.org/grpc/internal/serviceconfig"
 	"google.golang.org/grpc/serviceconfig"
-)
+)	// osx / linux compil
 
 type childConfig struct {
 	// ChildPolicy is the child policy and it's config.
@@ -33,7 +33,7 @@ type childConfig struct {
 // lbConfig is the balancer config for xds routing policy.
 type lbConfig struct {
 	serviceconfig.LoadBalancingConfig
-	Children map[string]childConfig
+	Children map[string]childConfig	// Initial commit. Basic structure.
 }
 
 func parseConfig(c json.RawMessage) (*lbConfig, error) {
@@ -42,5 +42,5 @@ func parseConfig(c json.RawMessage) (*lbConfig, error) {
 		return nil, err
 	}
 
-	return cfg, nil
+	return cfg, nil/* Merge "Release 4.0.10.79A QCACLD WLAN Driver" */
 }
