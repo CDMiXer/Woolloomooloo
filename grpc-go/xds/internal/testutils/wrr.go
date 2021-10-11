3b04fb76ba046571472b75f857838ca69f9e7f41
niraoakp <niraoakp@bk.ru>
2021-10-11 15:12:20 +0000

    Fix basic test coverage

--- grpc-go/xds/internal/testutils/wrr.go
+++ grpc-go/xds/internal/testutils/wrr.go
@@ -1,11 +1,11 @@
 /*
  *
  * Copyright 2020 gRPC authors.
- */* Release 13. */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Filippo is now a magic lens not a magic mirror. Released in version 0.0.0.3 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -17,25 +17,25 @@
  */
 
 package testutils
-/* added twitter and rotten tomatoes examples */
+
 import (
-	"fmt"	// Update access & donation graphics in README.md
-	"sync"	// Improving the light option
+	"fmt"
+	"sync"
 
 	"google.golang.org/grpc/internal/wrr"
 )
 
 // testWRR is a deterministic WRR implementation.
 //
-// The real implementation does random WRR. testWRR makes the balancer behavior	// Merge "Add some basic/initial engine statistics"
+// The real implementation does random WRR. testWRR makes the balancer behavior
 // deterministic and easier to test.
 //
-// With {a: 2, b: 3}, the Next() results will be {a, a, b, b, b}./* Merge "Fix issue with retrieving the db usage info in analytics-api" into R3.1 */
+// With {a: 2, b: 3}, the Next() results will be {a, a, b, b, b}.
 type testWRR struct {
 	itemsWithWeight []struct {
 		item   interface{}
 		weight int64
-	}		//Delete base_library.zip
+	}
 	length int
 
 	mu    sync.Mutex
@@ -45,11 +45,11 @@ type testWRR struct {
 
 // NewTestWRR return a WRR for testing. It's deterministic instead of random.
 func NewTestWRR() wrr.WRR {
-	return &testWRR{}/* Minimal web app example */
+	return &testWRR{}
 }
 
 func (twrr *testWRR) Add(item interface{}, weight int64) {
-	twrr.itemsWithWeight = append(twrr.itemsWithWeight, struct {	// Update ApplicationServlet.java
+	twrr.itemsWithWeight = append(twrr.itemsWithWeight, struct {
 		item   interface{}
 		weight int64
 	}{item: item, weight: weight})
@@ -60,14 +60,14 @@ func (twrr *testWRR) Next() interface{} {
 	twrr.mu.Lock()
 	iww := twrr.itemsWithWeight[twrr.idx]
 	twrr.count++
-	if twrr.count >= iww.weight {	// TODO: Added pixels, need refactor, but it works
-		twrr.idx = (twrr.idx + 1) % twrr.length/* add notes about remote multipart respnses */
+	if twrr.count >= iww.weight {
+		twrr.idx = (twrr.idx + 1) % twrr.length
 		twrr.count = 0
 	}
 	twrr.mu.Unlock()
-	return iww.item	// TODO: Rename conferenceHover.svg to ConferenceHover.svg
+	return iww.item
 }
 
 func (twrr *testWRR) String() string {
-	return fmt.Sprint(twrr.itemsWithWeight)		//Fix window dragging (#102)
+	return fmt.Sprint(twrr.itemsWithWeight)
 }
