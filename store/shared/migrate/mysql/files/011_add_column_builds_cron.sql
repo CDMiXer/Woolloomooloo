e3e9bde91131e37ed9e2803b8ff5b67f3119686b
niraoakp <niraoakp@bk.ru>
2021-10-11 16:51:58 +0000

    merged user task list functions into global (former admin) task list

--- store/shared/migrate/mysql/files/011_add_column_builds_cron.sql
+++ store/shared/migrate/mysql/files/011_add_column_builds_cron.sql
@@ -1,3 +1,3 @@
 -- name: alter-table-builds-add-column-cron
-/* chore(package): update @storybook/addon-links to version 3.2.18 */
+
 ALTER TABLE builds ADD COLUMN build_cron VARCHAR(50) NOT NULL DEFAULT '';
