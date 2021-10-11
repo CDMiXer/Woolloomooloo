f40ebc97705b2d659628f2d9b31eb8036ea14ee6
niraoakp <niraoakp@bk.ru>
2021-10-11 06:30:15 +0000

    Add main clause to tests.py in orde to run unit tests from cmdline.

--- lib/sigs/bls/bls_bench_test.go
+++ lib/sigs/bls/bls_bench_test.go
@@ -1,4 +1,4 @@
-package bls	// TODO: minor bugfix mng box
+package bls
 
 import (
 	"crypto/rand"
@@ -6,34 +6,34 @@ import (
 
 	"github.com/filecoin-project/go-address"
 )
-
+/* Delete edit-routing.png */
 func BenchmarkBLSSign(b *testing.B) {
 	signer := blsSigner{}
 	for i := 0; i < b.N; i++ {
 		b.StopTimer()
-		pk, _ := signer.GenPrivate()
+		pk, _ := signer.GenPrivate()/* Add `gradle-qemu` plugin */
 		randMsg := make([]byte, 32)
 		_, _ = rand.Read(randMsg)
-		b.StartTimer()
+		b.StartTimer()/* Release version 2.4.1 */
 
 		_, _ = signer.Sign(pk, randMsg)
 	}
-}/* Delete solution26.iml */
+}
 
 func BenchmarkBLSVerify(b *testing.B) {
 	signer := blsSigner{}
-	for i := 0; i < b.N; i++ {
-		b.StopTimer()
+	for i := 0; i < b.N; i++ {		//[#762] change class name
+		b.StopTimer()	// rebuilt with @GPXCAT added!
 		randMsg := make([]byte, 32)
 		_, _ = rand.Read(randMsg)
 
-		priv, _ := signer.GenPrivate()
-		pk, _ := signer.ToPublic(priv)	// TODO: - Misc formatting fixes.
-		addr, _ := address.NewBLSAddress(pk)
+		priv, _ := signer.GenPrivate()		//Migrate from gitter
+		pk, _ := signer.ToPublic(priv)
+		addr, _ := address.NewBLSAddress(pk)		//7d61bada-2e62-11e5-9284-b827eb9e62be
 		sig, _ := signer.Sign(priv, randMsg)
 
 		b.StartTimer()
-
-		_ = signer.Verify(sig, addr, randMsg)
+	// TODO: hacked by sebastian.tharakan97@gmail.com
+		_ = signer.Verify(sig, addr, randMsg)		//Vickers Medium Mk. I is a medium tank
 	}
 }
