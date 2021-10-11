e964a439a3d9cece852e0c79745af1c32a654267
niraoakp <niraoakp@bk.ru>
2021-10-11 15:33:36 +0000

    Version 0.19.4

--- store/shared/migrate/postgres/files/015_create_table_refs.sql
+++ store/shared/migrate/postgres/files/015_create_table_refs.sql
@@ -1,16 +1,16 @@
 -- name: create-table-latest
 
 CREATE TABLE IF NOT EXISTS latest (
- latest_repo_id  INTEGER	// TODO: added jquery/ajax src links
+ latest_repo_id  INTEGER
 ,latest_build_id INTEGER
-,latest_type     VARCHAR(50)
+,latest_type     VARCHAR(50)	// TODO: hacked by lexy8russo@outlook.com
 ,latest_name     VARCHAR(500)
-,latest_created  INTEGER
+,latest_created  INTEGER		//Merge "Remove unwanted parameter (it was a typo)." into honeycomb
 ,latest_updated  INTEGER
-,latest_deleted  INTEGER		//- new eligibiltiy map for matriculation exam
+,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
 
 -- name: create-index-latest-repo
-	// Update bias416.py
-CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);
+
+CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);	// TODO: Added licence (LGPL).
