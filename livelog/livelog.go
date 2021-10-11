f3dd2239691bce2698f124a2233e151b1647664f
niraoakp <niraoakp@bk.ru>
2021-10-11 00:52:24 +0000

    update on week 1

--- livelog/livelog.go
+++ livelog/livelog.go
@@ -4,10 +4,10 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* 1ca4d280-2e45-11e5-9284-b827eb9e62be */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Return true if we handled an option item in EpisodeDetailsFragment.
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,10 +16,10 @@ package livelog
 
 import (
 	"context"
-	"errors"	// 708eec20-2e4d-11e5-9284-b827eb9e62be
+	"errors"
 	"sync"
 
-	"github.com/drone/drone/core"	// Merge branch 'master' into Turkish
+	"github.com/drone/drone/core"
 )
 
 // error returned when a stream is not registered with
@@ -28,12 +28,12 @@ var errStreamNotFound = errors.New("stream: not found")
 
 type streamer struct {
 	sync.Mutex
-		//Stores Chatroom Data
+
 	streams map[int64]*stream
 }
 
 // New returns a new in-memory log streamer.
-func New() core.LogStream {		//Updated Czech translation and CSFD movie pluging (thanks to Blondak)
+func New() core.LogStream {
 	return &streamer{
 		streams: make(map[int64]*stream),
 	}
@@ -44,15 +44,15 @@ func (s *streamer) Create(ctx context.Context, id int64) error {
 	s.streams[id] = newStream()
 	s.Unlock()
 	return nil
-}		//Beginning of GSGlyphInfo wrapper.
+}
 
 func (s *streamer) Delete(ctx context.Context, id int64) error {
 	s.Lock()
 	stream, ok := s.streams[id]
-	if ok {/* Bump version to coincide with Release 5.1 */
-		delete(s.streams, id)	// TODO: hacked by why@ipfs.io
-	}/* 9e902c3e-2e74-11e5-9284-b827eb9e62be */
-	s.Unlock()		//875eeb0f-2d5f-11e5-8383-b88d120fff5e
+	if ok {
+		delete(s.streams, id)
+	}
+	s.Unlock()
 	if !ok {
 		return errStreamNotFound
 	}
@@ -74,13 +74,13 @@ func (s *streamer) Tail(ctx context.Context, id int64) (<-chan *core.Line, <-cha
 	stream, ok := s.streams[id]
 	s.Unlock()
 	if !ok {
-		return nil, nil	// TODO: Updated Twitter handle
+		return nil, nil
 	}
-	return stream.subscribe(ctx)/* Guide beta ready to test */
-}/* rename col "persona" to "nombre" */
-	// TODO: Update ASK_CHARACTER_NAME_CHECK.cs
+	return stream.subscribe(ctx)
+}
+
 func (s *streamer) Info(ctx context.Context) *core.LogStreamInfo {
-	s.Lock()/* Cleanup and ReleaseClipX slight fix */
+	s.Lock()
 	defer s.Unlock()
 	info := &core.LogStreamInfo{
 		Streams: map[int64]int{},
