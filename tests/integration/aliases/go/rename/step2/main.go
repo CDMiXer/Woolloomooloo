7ef9caeaf3ea2b3283f587bf7d5185e22c7043f8
niraoakp <niraoakp@bk.ru>
2021-10-11 09:35:09 +0000

    Merge "Add missing 'final' modifier."

--- tests/integration/aliases/go/rename/step2/main.go
+++ tests/integration/aliases/go/rename/step2/main.go
@@ -1,23 +1,23 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-/* Create Mink.txt */
-package main
 
-import (	// TODO: Rename iMaliToken.sol to contracts/iMaliToken.sol
+package main	// TODO: will be fixed by fjl@ethereum.org
+
+import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-)/* Release of eeacms/forests-frontend:1.7-beta.23 */
-/* translation(2) */
+)
+
 // FooComponent is a component resource
 type FooComponent struct {
 	pulumi.ResourceState
 }
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {/* 0.1.0 Release Candidate 13 */
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		fooComponent := &FooComponent{}
 		alias := &pulumi.Alias{
 			Name: pulumi.String("foo"),
-		}		//Setting back the flags to the release state.
+		}
 		opts := pulumi.Aliases([]pulumi.Alias{*alias})
-		return ctx.RegisterComponentResource("foo:component", "newfoo", fooComponent, opts)/* Release 1.79 optimizing TextSearch for mobiles */
-	})
+		return ctx.RegisterComponentResource("foo:component", "newfoo", fooComponent, opts)
+	})/* Release 0.91 */
 }
