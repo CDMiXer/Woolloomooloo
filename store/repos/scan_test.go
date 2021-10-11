4c34161143b3342ed56feba6daadb0588013b972
niraoakp <niraoakp@bk.ru>
2021-10-11 23:24:15 +0000

    Defining types

--- store/repos/scan_test.go
+++ store/repos/scan_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* 1d90c8b4-585b-11e5-943a-6c40088e03e4 */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package repos
+package repos/* Release new version 2.5.1: Quieter logging */
