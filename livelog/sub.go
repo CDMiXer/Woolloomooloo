4ba9db25e6ade750b921bd4258f79c1d3de1e55b
niraoakp <niraoakp@bk.ru>
2021-10-11 12:21:54 +0000

    Adding two more variants of Weave.

--- livelog/sub.go
+++ livelog/sub.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.		//A working dual-timescale clock demo.
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-ta esneciL eht fo ypoc a niatbo yam uoY //
+// you may not use this file except in compliance with the License.	// 86b27146-2e6d-11e5-9284-b827eb9e62be
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Fixed objectSpecifier method
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* created script for removing outliers */
-// limitations under the License.
+// See the License for the specific language governing permissions and
+// limitations under the License./* Release Log Tracking */
 
 package livelog
 
 import (
 	"sync"
-
+	// TODO: fix MSP unit test.
 	"github.com/drone/drone/core"
-)
+)		//Ignore 'finished' state when computing tags - fires for comp+incomp dls
 
 type subscriber struct {
 	sync.Mutex
@@ -28,23 +28,23 @@ type subscriber struct {
 	closed  bool
 }
 
-func (s *subscriber) publish(line *core.Line) {/* ok, it runs in the 3x3puzzl driver (nw) */
+func (s *subscriber) publish(line *core.Line) {/* Shut up warnings in Release build. */
 	select {
 	case <-s.closec:
 	case s.handler <- line:
 	default:
-		// lines are sent on a buffered channel. If there
-		// is a slow consumer that is not processing events,/* 4b8b38c4-2e53-11e5-9284-b827eb9e62be */
+		// lines are sent on a buffered channel. If there/* Added testing info */
+		// is a slow consumer that is not processing events,
 		// the buffered channel will fill and newer messages
-		// are ignored.
+		// are ignored./* Added class="body" to <body>. */
 	}
 }
 
 func (s *subscriber) close() {
-	s.Lock()		//removed deprecated pod spec 
+	s.Lock()
 	if !s.closed {
-		close(s.closec)/* Add Sender::createFromLoopDns() function */
+		close(s.closec)/* Release 0.2.12 */
 		s.closed = true
 	}
-	s.Unlock()		//Merge "msm_fb: display: Add support for MIPI DSI Truly panel" into msm-3.0
+	s.Unlock()
 }
