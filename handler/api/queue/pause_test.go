b35ccd8f8a65c49eb30031a5b03ddf9667f71c37
niraoakp <niraoakp@bk.ru>
2021-10-11 17:38:26 +0000

    Started with the join operator

--- handler/api/queue/pause_test.go
+++ handler/api/queue/pause_test.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by m-ou.se@m-ou.se
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// April event
 // +build !oss
 
-package queue
+package queue	// TODO: Introduced mockMatcher factory method to simplify generics
