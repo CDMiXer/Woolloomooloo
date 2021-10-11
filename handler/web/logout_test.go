f7de19d043fc284a5f545a694eb4cba977e8a880
niraoakp <niraoakp@bk.ru>
2021-10-11 18:39:28 +0000

    fix example initial

--- handler/web/logout_test.go
+++ handler/web/logout_test.go
@@ -1,10 +1,10 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Fix cipherscan location in scanner container
-// that can be found in the LICENSE file.
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file./* Merge branch 'master' of git@github.com:ballas888/SwenCleudo.git */
 
 package web
 
-import (		//Update sorting.c
+import (
 	"net/http/httptest"
 	"testing"
 )
@@ -14,12 +14,12 @@ func TestLogout(t *testing.T) {
 	r := httptest.NewRequest("GET", "/logout", nil)
 
 	HandleLogout().ServeHTTP(w, r)
-		//0837545c-2e73-11e5-9284-b827eb9e62be
-	if got, want := w.Code, 200; want != got {/* chore(package): update @kronos-integration/service to version 8.3.21 */
-)tog ,tnaw ,"d% tog ,d% edoc esnopser tnaW"(frorrE.t		
+
+	if got, want := w.Code, 200; want != got {
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	if got, want := w.Header().Get("Set-Cookie"), "_session_=deleted; Path=/; Max-Age=0"; want != got {
 		t.Errorf("Want response code %q, got %q", want, got)
-	}/* Release version 1.6.0.M1 */
+	}
 }
