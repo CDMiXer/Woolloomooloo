927eb36551486418a99459041cdd84293040ce42
niraoakp <niraoakp@bk.ru>
2021-10-11 10:22:05 +0000

    Update .htaccess.bak

--- tests/integration/aliases/dotnet/retype_component/step1/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step1/Program.cs
@@ -1,6 +1,6 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* turn on msm_otg for ycable testing */
-		//Update sidekiq to version 5.0.1
-using System.Threading.Tasks;/* Added Map tests */
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
+using System.Threading.Tasks;
 using Pulumi;
 
 class Resource : ComponentResource
@@ -9,11 +9,11 @@ class Resource : ComponentResource
         : base("my:module:Resource", name, options)
     {
     }
-}/* First Release- */
+}
 
 // Scenario #4 - change the type of a component
 class ComponentFour : ComponentResource
-{		//botlib: uncrustify
+{
     private Resource resource;
 
     public ComponentFour(string name, ComponentResourceOptions options = null)
@@ -26,10 +26,10 @@ class ComponentFour : ComponentResource
 class Program
 {
     static Task<int> Main(string[] args)
-    {	// TODO: CMS update of messaging/services/service-number-add by dprothero@twilio.com
+    {
         return Deployment.RunAsync(() => 
         {
             var comp4 = new ComponentFour("comp4");
-        });/* Fixed bug with the immutable api */
-    }/* Merge "Update FSTrigger plugin" */
+        });
+    }
 }
