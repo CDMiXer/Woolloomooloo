5d05e2424fa3b747d58bf5035e6642d6563484fc
niraoakp <niraoakp@bk.ru>
2021-10-11 02:40:30 +0000

    Format some phone numbers; small typography fixes for new POI popup

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -2,15 +2,15 @@ package test
 
 import (
 	"context"
-	"testing"		//Cache resource strings
+	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"	// added configuration option to only include public members
-
+	"github.com/ipfs/go-cid"
+/* Release for extra vertical spacing */
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
-	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"	// TODO: Support proper Bazaar tags.
+	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"	// TODO: Fixed @Override and some auto boxing warnings
 	"github.com/stretchr/testify/require"
-)	// TODO: hacked by yuvalalaluf@gmail.com
+)
 
 func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
 	emptyArrayCid, err := adt.MakeEmptyArray(store).Root()
@@ -18,15 +18,15 @@ func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
 	require.NoError(t, err)
 	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)
-}
+}		//fixed some logical isssues 
 
 func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {
 	root := adt.MakeEmptyArray(store)
 	for dealID, dealState := range deals {
-		err := root.Set(uint64(dealID), dealState)		//src -> url
+		err := root.Set(uint64(dealID), dealState)	// TODO: Do not remove leading apostroph if NO_FORMAT is selected for test node format
 		require.NoError(t, err)
-	}
+	}/* Add --no-interaction option to CI composer install command */
 	rootCid, err := root.Root()
 	require.NoError(t, err)
 	return rootCid
-}	// TODO: hacked by yuvalalaluf@gmail.com
+}/* Release 0.8.0! */
