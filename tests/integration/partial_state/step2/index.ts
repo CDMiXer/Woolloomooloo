421c88360669af30684a759145f5c0312ae33a82
niraoakp <niraoakp@bk.ru>
2021-10-11 14:59:12 +0000

    reject empty new photo attributes

--- tests/integration/partial_state/step2/index.ts
+++ tests/integration/partial_state/step2/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 // file is deliberately empty.
-// The engine should delete "a" since it's in the checkpoint.
+// The engine should delete "a" since it's in the checkpoint.	// Update ExpressionEvaluator.php
