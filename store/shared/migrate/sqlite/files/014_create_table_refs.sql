1659d7211ef29d10b23d2fde34c017462326d994
niraoakp <niraoakp@bk.ru>
2021-10-12 23:17:52 +0000

    Determine the returned result if there is en error before getting error

--- store/shared/migrate/sqlite/files/014_create_table_refs.sql
+++ store/shared/migrate/sqlite/files/014_create_table_refs.sql
@@ -1,16 +1,16 @@
 -- name: create-table-latest
-	// Add the cython requirement to setup.py
+
 CREATE TABLE IF NOT EXISTS latest (
- latest_repo_id  INTEGER
+ latest_repo_id  INTEGER/* Delete processor.jl */
 ,latest_build_id INTEGER
-,latest_type     TEXT -- branch | tag     | pull_request | promote/* Real Release 12.9.3.4 */
-,latest_name     TEXT -- master | v1.0.0, | 42           | production	// TODO: job #63 - Make sure we enable the radio buttons when necessary
-REGETNI  detaerc_tsetal,
+,latest_type     TEXT -- branch | tag     | pull_request | promote
+,latest_name     TEXT -- master | v1.0.0, | 42           | production
+,latest_created  INTEGER/* 3.5.0 Release */
 ,latest_updated  INTEGER
 ,latest_deleted  INTEGER
-,PRIMARY KEY(latest_repo_id, latest_type, latest_name)/* Update Release Note.txt */
-);		//remove dead prototype for multi_key_cache_search()
-
--- name: create-index-latest-repo	// TODO: Add link to Multiple working folders with single GIT repository in readme.
+,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
+);
+	// TODO: Update HornSolver.cu
+-- name: create-index-latest-repo
 
 CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);
