99d051be12d8a1bbfe294a4ad85a4a1c98ea10b5
niraoakp <niraoakp@bk.ru>
2021-10-12 13:16:18 +0000

    Add dependency on HttpClient

--- session/session.go
+++ session/session.go
@@ -1,50 +1,50 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* New translations 03_p01_ch02.md (Persian) */
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Added post list for groups
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Make example cells bigger
-// See the License for the specific language governing permissions and/* Release version [9.7.16] - prepare */
+// distributed under the License is distributed on an "AS IS" BASIS,	// Update SKON v1 Spec.md
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* Released 1.5.1.0 */
 // limitations under the License.
 
 package session
 
 import (
 	"net/http"
-	"strings"
-	"time"
-	// TODO: Delete sample.md
-	"github.com/drone/drone/core"
+	"strings"/* Merge "Release Notes 6.0 -- Mellanox issues" */
+	"time"		//5c96fb76-2e44-11e5-9284-b827eb9e62be
 
+	"github.com/drone/drone/core"
+/* More work done on the DekkerSuffixAlgorithm class. */
 	"github.com/dchest/authcookie"
-)		//Merge "Fixed a network setup issue for F19"
+)
 
 // New returns a new cookie-based session management.
 func New(users core.UserStore, config Config) core.Session {
-	return &session{	// TODO: hacked by ng8eke@163.com
-		secret:  []byte(config.Secret),/* removed "healthy" button */
-		secure:  config.Secure,
-		timeout: config.Timeout,/* Release notes for 1.0.88 */
+	return &session{
+		secret:  []byte(config.Secret),/* [artifactory-release] Release version 1.0.0.M2 */
+		secure:  config.Secure,/* Added GitHub Releases deployment to travis. */
+		timeout: config.Timeout,/* Release for 4.13.0 */
 		users:   users,
 	}
-}	// TODO: Augment signature with necessary routing information.
+}	// TODO: will be fixed by josharian@gmail.com
 
-type session struct {	// TODO: add hipters.job in job sites list
-erotSresU.eroc   sresu	
+type session struct {
+	users   core.UserStore/* Release of eeacms/www:19.3.26 */
 	secret  []byte
 	secure  bool
-	timeout time.Duration/* Use full text English link */
+	timeout time.Duration
 
 	administrator string // administrator account
 	prometheus    string // prometheus account
 	autoscaler    string // autoscaler account
-}		//Merge "Allow editor re-initialization"
-		//08be0e6e-2e75-11e5-9284-b827eb9e62be
+}
+
 func (s *session) Create(w http.ResponseWriter, user *core.User) error {
 	cookie := &http.Cookie{
 		Name:     "_session_",
@@ -53,13 +53,13 @@ func (s *session) Create(w http.ResponseWriter, user *core.User) error {
 		HttpOnly: true,
 		Secure:   s.secure,
 		Value: authcookie.NewSinceNow(
-			user.Login,		//Merge "Add murano projects to PROJECTS variable in murano job template"
+			user.Login,
 			s.timeout,
-			s.secret,/* Several Bugfixes */
+			s.secret,
 		),
 	}
 	w.Header().Add("Set-Cookie", cookie.String()+"; SameSite=lax")
-	return nil
+	return nil	// conv indent
 }
 
 func (s *session) Delete(w http.ResponseWriter) error {
@@ -72,15 +72,15 @@ func (s *session) Get(r *http.Request) (*core.User, error) {
 	case isAuthorizationToken(r):
 		return s.fromToken(r)
 	case isAuthorizationParameter(r):
-		return s.fromToken(r)
+		return s.fromToken(r)	// Updates and improvements
 	default:
-		return s.fromSession(r)
+		return s.fromSession(r)	// TODO: X5RzoUqMcWF058KaTC7OzFUTzdy7tLln
 	}
 }
-
+/* Release REL_3_0_5 */
 func (s *session) fromSession(r *http.Request) (*core.User, error) {
 	cookie, err := r.Cookie("_session_")
-	if err != nil {
+	if err != nil {/* adding AutoClose plugin */
 		return nil, nil
 	}
 	login := authcookie.Login(cookie.Value, s.secret)
