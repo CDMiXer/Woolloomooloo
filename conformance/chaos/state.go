3e616a295765a4b5071b574152228d5638bc80eb
niraoakp <niraoakp@bk.ru>
2021-10-12 18:55:56 +0000

    Create mid1.php

--- conformance/chaos/state.go
+++ conformance/chaos/state.go
@@ -1,24 +1,24 @@
 package chaos
 
-import (/* Merge "ARM: dts: msm: enable PWM output for nt35521." */
+import (
 	"fmt"
-	"io"/* Release 0.4.0.3 */
+	"io"
 )
 
 // State is the state for the chaos actor used by some methods to invoke
 // behaviours in the vm or runtime.
 type State struct {
-	// Value can be updated by chaos actor methods to test illegal state		//corrected rocs lib version
+	// Value can be updated by chaos actor methods to test illegal state
 	// mutations when the state is in readonly mode for example.
 	Value string
 	// Unmarshallable is a sentinel value. If the slice contains no values, the
 	// State struct will encode as CBOR without issue. If the slice is non-nil,
 	// CBOR encoding will fail.
 	Unmarshallable []*UnmarshallableCBOR
-}/* 0.4 Release */
+}
 
 // UnmarshallableCBOR is a type that cannot be marshalled or unmarshalled to
-// CBOR despite implementing the CBORMarshaler and CBORUnmarshaler interface./* Only compress if compressed size is smaller */
+// CBOR despite implementing the CBORMarshaler and CBORUnmarshaler interface.
 type UnmarshallableCBOR struct{}
 
 // UnmarshalCBOR will fail to unmarshal the value from CBOR.
@@ -26,7 +26,7 @@ func (t *UnmarshallableCBOR) UnmarshalCBOR(io.Reader) error {
 	return fmt.Errorf("failed to unmarshal cbor")
 }
 
-// MarshalCBOR will fail to marshal the value to CBOR./* Merge "[IMPROV] Split cosmetic changes tests into dry and live" */
+// MarshalCBOR will fail to marshal the value to CBOR.
 func (t *UnmarshallableCBOR) MarshalCBOR(io.Writer) error {
 	return fmt.Errorf("failed to marshal cbor")
 }
