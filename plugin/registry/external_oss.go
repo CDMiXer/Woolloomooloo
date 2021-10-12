beeacc67d494a2852a159a9af1d2ae5960fedc2a
niraoakp <niraoakp@bk.ru>
2021-10-12 17:56:19 +0000

    Update and rename EFUCVtesis2.cls to EFUCVtesis.cls

--- plugin/registry/external_oss.go
+++ plugin/registry/external_oss.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//Homogenized end of lines 
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: Ignore CVNBot2 task while moving
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
@@ -10,15 +10,15 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.		//corrigir verificar valores de retorno
 
 // +build oss
 
 package registry
 
-import "github.com/drone/drone/core"
+import "github.com/drone/drone/core"/* Release ProcessPuzzleUI-0.8.0 */
 
 // External returns a no-op registry credential provider.
 func External(string, string, bool) core.RegistryService {
 	return new(noop)
-}
+}/* added one more line for testing */
