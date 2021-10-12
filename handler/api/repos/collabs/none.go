15b0e13aa8cdf130be2ca202c3edcedb0622dacf
niraoakp <niraoakp@bk.ru>
2021-10-12 04:03:38 +0000

    Remove excessive specs

--- handler/api/repos/collabs/none.go
+++ handler/api/repos/collabs/none.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: will be fixed by m-ou.se@m-ou.se
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -11,30 +11,30 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: data for kl cellhits mapper
+
 // +build oss
 
-package collabs		//Trace added, and test modified.
+package collabs
 
-import (/* Switched to static runtime library linking in Release mode. */
+import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* Release version: 0.7.17 */
-	"github.com/drone/drone/handler/api/render"/* Release 1.10.0 */
-)/* Create new folder 'Release Plan'. */
+	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/api/render"
+)
 
-var notImplemented = func(w http.ResponseWriter, r *http.Request) {	// TODO: will be fixed by alan.shaw@protocol.ai
+var notImplemented = func(w http.ResponseWriter, r *http.Request) {
 	render.NotImplemented(w, render.ErrNotImplemented)
 }
 
 func HandleDelete(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
 	return notImplemented
-}	// Fixed Issue #297
+}
 
 func HandleFind(core.UserStore, core.RepositoryStore, core.PermStore) http.HandlerFunc {
 	return notImplemented
 }
 
-func HandleList(core.RepositoryStore, core.PermStore) http.HandlerFunc {		//Editet Pom and code formatting
+func HandleList(core.RepositoryStore, core.PermStore) http.HandlerFunc {
 	return notImplemented
 }
