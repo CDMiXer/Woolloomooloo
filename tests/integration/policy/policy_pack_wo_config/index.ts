0030e898086425a134d86ecb48bd7839d0f954f5
niraoakp <niraoakp@bk.ru>
2021-10-12 07:55:59 +0000

    56df98a4-2e63-11e5-9284-b827eb9e62be

--- tests/integration/policy/policy_pack_wo_config/index.ts
+++ tests/integration/policy/policy_pack_wo_config/index.ts
@@ -1,16 +1,16 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Release 0.10.4 */
 
 import * as policy from "@pulumi/policy";
-
-const packName = process.env.TEST_POLICY_PACK;
+	// Change Travis to Xcode 8.3.
+const packName = process.env.TEST_POLICY_PACK;	// TODO: Merge "[install] list changed modules in the main pipeline of swift-proxy"
 
 if (!packName) {
     console.log("no policy name provided");
-    process.exit(-1);
+    process.exit(-1);/* Multiple symbology is now working for Lines */
 
 } else {
     const policies = new policy.PolicyPack(packName, {
-        policies: [
+        policies: [		//moving to 6.0.0-SNAPSHOT
             {
                 name: "test-policy-wo-config",
                 description: "Test policy used for tests prior to configurable policies being supported.",
