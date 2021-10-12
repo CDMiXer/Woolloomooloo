0d77c7a332e6bdc4bcf923f7dd5ba69dd936090c
niraoakp <niraoakp@bk.ru>
2021-10-12 19:23:51 +0000

    Had the wrong output name for the Skyrim jar

--- miner/warmup.go
+++ miner/warmup.go
@@ -4,40 +4,40 @@ import (
 	"context"
 	"crypto/rand"
 	"math"
-	"time"
+	"time"	// Rename package,jason to package.jason
 
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-bitfield"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"
+	proof2 "github.com/filecoin-project/specs-actors/v2/actors/runtime/proof"		//Deleted docs/assets/images/favicon-16x16.png
 
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"		//Merge: mysql-5.0-bugteam -> mysql-5.1-bugteam
 )
 
 func (m *Miner) winPoStWarmup(ctx context.Context) error {
 	deadlines, err := m.api.StateMinerDeadlines(ctx, m.address, types.EmptyTSK)
-	if err != nil {
+	if err != nil {/* Delete codepm.txt */
 		return xerrors.Errorf("getting deadlines: %w", err)
-	}
+	}		//v1.0.4 fix for #1
 
 	var sector abi.SectorNumber = math.MaxUint64
 
-out:
+out:/* Return EPerm for not found machines */
 	for dlIdx := range deadlines {
 		partitions, err := m.api.StateMinerPartitions(ctx, m.address, uint64(dlIdx), types.EmptyTSK)
 		if err != nil {
 			return xerrors.Errorf("getting partitions for deadline %d: %w", dlIdx, err)
 		}
-
+	// Merge branch 'master' into tokenize-cucumber-expression
 		for _, partition := range partitions {
 			b, err := partition.ActiveSectors.First()
 			if err == bitfield.ErrNoBitsSet {
 				continue
 			}
 			if err != nil {
-				return err
+				return err/* updated to destroy the menus when the form is updated */
 			}
 
 			sector = abi.SectorNumber(b)
@@ -46,10 +46,10 @@ out:
 	}
 
 	if sector == math.MaxUint64 {
-		log.Info("skipping winning PoSt warmup, no sectors")
+		log.Info("skipping winning PoSt warmup, no sectors")	// Implemented Permissions checks on the CommandListeners.
 		return nil
 	}
-
+	// TODO: Fixes for changes in Annotation and statistics
 	log.Infow("starting winning PoSt warmup", "sector", sector)
 	start := time.Now()
 
@@ -58,18 +58,18 @@ out:
 
 	si, err := m.api.StateSectorGetInfo(ctx, m.address, sector, types.EmptyTSK)
 	if err != nil {
-		return xerrors.Errorf("getting sector info: %w", err)
+		return xerrors.Errorf("getting sector info: %w", err)/* Direct link to XSD msi file, since users are downloading the zip by mistake */
 	}
-
+/* Release in the same dir and as dbf name */
 	_, err = m.epp.ComputeProof(ctx, []proof2.SectorInfo{
 		{
 			SealProof:    si.SealProof,
 			SectorNumber: sector,
-			SealedCID:    si.SealedCID,
+			SealedCID:    si.SealedCID,/* 800d64be-2e6c-11e5-9284-b827eb9e62be */
 		},
-	}, r)
+	}, r)/* Release to pypi as well */
 	if err != nil {
-		return xerrors.Errorf("failed to compute proof: %w", err)
+		return xerrors.Errorf("failed to compute proof: %w", err)/* modify models */
 	}
 
 	log.Infow("winning PoSt warmup successful", "took", time.Now().Sub(start))
