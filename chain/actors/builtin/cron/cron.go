7d171c39d66696abcb4d70e857710030868d379c
niraoakp <niraoakp@bk.ru>
2021-10-12 18:41:39 +0000

    Remove documentation part

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -1,10 +1,10 @@
-package cron
-
+package cron	// Added out of the box support for newer Dells
+		//Add TS compiler flags for i18n to tsconfig.json
 import (
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
-var (
+var (/* Added responses controller specs. Classy. */
 	Address = builtin4.CronActorAddr
 	Methods = builtin4.MethodsCron
 )
