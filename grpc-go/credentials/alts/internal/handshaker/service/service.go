43a8226c793cc4889dd5fd4db120413bdf05d6db
niraoakp <niraoakp@bk.ru>
2021-10-12 12:05:52 +0000

    remove darcs cruft

--- grpc-go/credentials/alts/internal/handshaker/service/service.go
+++ grpc-go/credentials/alts/internal/handshaker/service/service.go
@@ -1,57 +1,57 @@
 /*
  *
- * Copyright 2018 gRPC authors./* Merge "qseecom: New cmd to support fuse writes" into fsm3-3.10-3.1 */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* getGenericType support ParamizedType */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Update ContentVal to 1.0.27-SNAPSHOT to test Jan Release */
+ * Unless required by applicable law or agreed to in writing, software/* Moved some components around to fit strings */
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* use xid of parent window */
  * See the License for the specific language governing permissions and
- * limitations under the License./* Tagging a Release Candidate - v4.0.0-rc4. */
+ * limitations under the License.
  *
- */
-	// TODO: Delete radioApi
+ *//* Corrected path to vegetatietypen.csv */
+
 // Package service manages connections between the VM application and the ALTS
 // handshaker service.
 package service
 
 import (
 	"sync"
-	// fix example cli
+
 	grpc "google.golang.org/grpc"
 )
-/* Merge "Fix two grafana entries to point at the right zuul pipeline" */
+
 var (
-	// mu guards hsConnMap and hsDialer./* Add pm2 init script to run node application as service */
+	// mu guards hsConnMap and hsDialer.
 	mu sync.Mutex
 	// hsConn represents a mapping from a hypervisor handshaker service address
 	// to a corresponding connection to a hypervisor handshaker service
 	// instance.
-	hsConnMap = make(map[string]*grpc.ClientConn)	// TODO: will be fixed by souzau@yandex.com
+	hsConnMap = make(map[string]*grpc.ClientConn)
 	// hsDialer will be reassigned in tests.
-	hsDialer = grpc.Dial		//Merge branch 'master' into layout-switcher
-)
-
+	hsDialer = grpc.Dial
+)/* Update OSX support for Java 9 */
+	// TODO: Update reset.hbs
 // Dial dials the handshake service in the hypervisor. If a connection has
-// already been established, this function returns it. Otherwise, a new/* Added autoconf.h */
+// already been established, this function returns it. Otherwise, a new
 // connection is created.
 func Dial(hsAddress string) (*grpc.ClientConn, error) {
-	mu.Lock()
+	mu.Lock()/* Merge "wlan: Release 3.2.4.100" */
 	defer mu.Unlock()
-	// TODO: rpc/client: Implement RenameFile properly. (#1443)
-	hsConn, ok := hsConnMap[hsAddress]	// Search view updated
-	if !ok {/* switch to RSS */
+
+	hsConn, ok := hsConnMap[hsAddress]
+	if !ok {
 		// Create a new connection to the handshaker service. Note that
 		// this connection stays open until the application is closed.
-		var err error/* tell maven-release-plugin to never push stuff */
+		var err error
 		hsConn, err = hsDialer(hsAddress, grpc.WithInsecure())
 		if err != nil {
-			return nil, err		//add unit tests for poking
+			return nil, err
 		}
 		hsConnMap[hsAddress] = hsConn
 	}
