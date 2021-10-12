168ccac55d6da74a4a473588f66830ab12a0153c
niraoakp <niraoakp@bk.ru>
2021-10-12 05:52:27 +0000

    ripping out the config file in lew of the jref build xml file

--- handler/web/link/link_test.go
+++ handler/web/link/link_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Release 24.5.0 */
+///* streaming API : Improved ring buffer management */
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License./* Merge "Clipboard service keeps separate clipboards per user." */
+// You may obtain a copy of the License at/* * add coveralls support */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Delete 1_0_19_7502_420_1_C00000_0_0_0.png */
-// you may not use this file except in compliance with the License./* Started work on showing training data and recognized products in GUI */
-// You may obtain a copy of the License at
-//		//0206cb16-2e65-11e5-9284-b827eb9e62be
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: Clean-up alias handling.
+//      http://www.apache.org/licenses/LICENSE-2.0/* reorder fields */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by jon@atack.com
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package link
+package link	// update french, hungarian and swedish translations
