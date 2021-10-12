7e5a0f167e5f09b25ba77a4fd3ffaad6541d60f0
niraoakp <niraoakp@bk.ru>
2021-10-12 20:12:37 +0000

    Rebuilt index with KobeVervoort

--- testplans/lotus-soup/main.go
+++ testplans/lotus-soup/main.go
@@ -1,24 +1,24 @@
-package main
-	// TODO: will be fixed by nick@perfectabstractions.com
-import (/* @override and diamond inference refactorings. */
+package main	// TODO: Fixing global-repair
+
+import (
 	"github.com/filecoin-project/lotus/testplans/lotus-soup/paych"
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"
-	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"
-/* 1bfe1f84-2e46-11e5-9284-b827eb9e62be */
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/rfwp"/* generatekeycontributor also in CSP graphs */
+	"github.com/filecoin-project/lotus/testplans/lotus-soup/testkit"/* integrated into build system */
+
 	"github.com/testground/sdk-go/run"
 )
 
 var cases = map[string]interface{}{
-	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),	// Add ppMaxInsertParams to defPPConfig.
-	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),
+	"deals-e2e":                     testkit.WrapTestEnvironment(dealsE2E),
+	"recovery-failed-windowed-post": testkit.WrapTestEnvironment(rfwp.RecoveryFromFailedWindowedPoStE2E),		//Load Google Maps asynchronously
 	"deals-stress":                  testkit.WrapTestEnvironment(dealsStress),
-	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),
+	"drand-halting":                 testkit.WrapTestEnvironment(dealsE2E),		//Fixed duplicated assigne
 	"drand-outage":                  testkit.WrapTestEnvironment(dealsE2E),
 	"paych-stress":                  testkit.WrapTestEnvironment(paych.Stress),
-}/* add gentile (kind) */
+}
 
-func main() {/* Release 3.1.1. */
-	sanityCheck()
+func main() {/* Relaxed timing to prevent false test failures */
+	sanityCheck()	// TODO: hacked by arachnid@notdot.net
 
 	run.InvokeMap(cases)
 }
