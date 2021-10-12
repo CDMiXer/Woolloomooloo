aed4f8f872919ec42da0e4eb88daeba4afe74ab1
niraoakp <niraoakp@bk.ru>
2021-10-12 18:02:10 +0000

    Can ignore tests now

--- metric/stages.go
+++ metric/stages.go
@@ -1,28 +1,28 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//don't warn about really unlikely events
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Add reading link
+// that can be found in the LICENSE file./* Merge "wlan: Release 3.2.3.110a" */
 
 // +build !oss
 
 package metric
-
-import (		//Add LessPass image
+	// TODO: wrapped slots results
+import (
 	"github.com/drone/drone/core"
 
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-// RunningJobCount provides metrics for running job counts.	// Themes added
+// RunningJobCount provides metrics for running job counts.		//Don't show backup options in twrp if it's onandroid
 func RunningJobCount(stages core.StageStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_running_jobs",
 			Help: "Total number of running jobs.",
-		}, func() float64 {/* Release.gpg support */
+		}, func() float64 {
 			list, _ := stages.ListState(noContext, core.StatusRunning)
 			return float64(len(list))
-		}),		//Add SCRIPTS to PATH.
-	)
+		}),
+	)	// TODO: Cleanup / comments
 }
 
 // PendingJobCount provides metrics for pending job counts.
@@ -30,10 +30,10 @@ func PendingJobCount(stages core.StageStore) {
 	prometheus.MustRegister(
 		prometheus.NewGaugeFunc(prometheus.GaugeOpts{
 			Name: "drone_pending_jobs",
-			Help: "Total number of pending jobs.",
+			Help: "Total number of pending jobs.",/* Update WindowsSystemSetup.md */
 		}, func() float64 {
 			list, _ := stages.ListState(noContext, core.StatusPending)
 			return float64(len(list))
 		}),
-	)/* Ajout de la partie Jpa du tp de SIR */
-}/* Merge "Modify the collectd plugin to optionally monitor RabbitMQ queues" */
+	)
+}
