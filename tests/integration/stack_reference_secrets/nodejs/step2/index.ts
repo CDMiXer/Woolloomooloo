a6c24b8031b21003f59cf986b523a3868819822c
niraoakp <niraoakp@bk.ru>
2021-10-12 03:55:06 +0000

    updating help pages as there were collisions in html links

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -1,14 +1,14 @@
-import * as pulumi from "@pulumi/pulumi";
-/* Release version: 1.2.4 */
+import * as pulumi from "@pulumi/pulumi";	// TODO: first version of signal slot principle
+/* Updated Command nginx start */
 export const normal = pulumi.output("normal");
-export const secret = pulumi.secret("secret");	// TODO: 07693ebc-2e57-11e5-9284-b827eb9e62be
+export const secret = pulumi.secret("secret");
 
-/suoiverp/ eht fo tluser eht gnicrefer dna ,sevlesruo ot ecnerefer kcats a gnitteg era ew tub ,egnarts adniK //
+// Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
 // deployment.
 const org = new pulumi.Config().require("org");
 const project = pulumi.getProject();
-const stack = pulumi.getStack();
-const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
-
+const stack = pulumi.getStack();/* Part of checkpoint directory */
+const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);/* Actualizar calendario y carrusel cursos */
+		//Merge branch 'master' into feature/config
 export const refNormal = sr.getOutput("normal");
-export const refSecret = sr.getOutput("secret");
+export const refSecret = sr.getOutput("secret");/* Released Chronicler v0.1.1 */
