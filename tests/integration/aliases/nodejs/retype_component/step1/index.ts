03c647e1cb909d828b265f3856183e6f2d354d3f
niraoakp <niraoakp@bk.ru>
2021-10-12 12:11:40 +0000

    More contextual menu items for click entry hotspot

--- tests/integration/aliases/nodejs/retype_component/step1/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step1/index.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+		//Adjustments for device classes
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
+        super("my:module:Resource", name, {}, opts);/* 409b0878-2e9d-11e5-96dc-a45e60cdfd11 */
     }
 }
 
 // Scenario #4 - change the type of a component
-class ComponentFour extends pulumi.ComponentResource {
+class ComponentFour extends pulumi.ComponentResource {/* Create prepareRelease.sh */
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFour", name, {}, opts);
+        super("my:module:ComponentFour", name, {}, opts);		//GUI + new method to StringUtils
         this.resource = new Resource("otherchild", {parent: this});
     }
 }
