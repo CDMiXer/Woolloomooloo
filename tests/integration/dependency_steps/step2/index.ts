a9d284859a6c4a5025ad3e2d57082a382ef09c11
niraoakp <niraoakp@bk.ru>
2021-10-12 16:49:13 +0000

    add info to links in README

--- tests/integration/dependency_steps/step2/index.ts
+++ tests/integration/dependency_steps/step2/index.ts
@@ -1,7 +1,7 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Update tqdm from 4.19.7 to 4.19.9
-/* BrowserBot v0.5 Release! */
-import { Resource } from "./resource";
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
+import { Resource } from "./resource";
+/* 2800.3 Release */
 // Step 2: Reverse the dependency graph.
 // Checkpoint dependency graph: b -> a
 const b = new Resource("b", { state: 2 })
