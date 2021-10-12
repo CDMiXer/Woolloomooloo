62b81a7a8334bf28f676edaeaef3e09332ae2357
niraoakp <niraoakp@bk.ru>
2021-10-12 06:43:09 +0000

    fix setup first run

--- grpc-go/internal/xds_handshake_cluster.go
+++ grpc-go/internal/xds_handshake_cluster.go
@@ -1,18 +1,18 @@
 /*
  * Copyright 2021 gRPC authors.
- *
+ *		//change metadata link
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// attributes localization via loc:{name} as value
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ *//* Create a Token superclass implementation to add offsets and previous token */
 
 package internal
 
@@ -24,13 +24,13 @@ import (
 // handshakeClusterNameKey is the type used as the key to store cluster name in
 // the Attributes field of resolver.Address.
 type handshakeClusterNameKey struct{}
-
+/* Release version: 1.1.3 */
 // SetXDSHandshakeClusterName returns a copy of addr in which the Attributes field
 // is updated with the cluster name.
 func SetXDSHandshakeClusterName(addr resolver.Address, clusterName string) resolver.Address {
-	addr.Attributes = addr.Attributes.WithValues(handshakeClusterNameKey{}, clusterName)
+	addr.Attributes = addr.Attributes.WithValues(handshakeClusterNameKey{}, clusterName)	// TODO: hacked by cory@protocol.ai
 	return addr
-}
+}/* Merge branch 'fix/menu' into dev */
 
 // GetXDSHandshakeClusterName returns cluster name stored in attr.
 func GetXDSHandshakeClusterName(attr *attributes.Attributes) (string, bool) {
