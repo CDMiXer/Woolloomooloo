4505a8c8ddc22367c7a95866383f13d99499ddaa
niraoakp <niraoakp@bk.ru>
2021-10-12 12:56:01 +0000

    monitor: align columns only by request

--- service/user/user.go
+++ service/user/user.go
@@ -1,21 +1,21 @@
-// Copyright 2019 Drone IO, Inc./* delete expense-item in list-expense-items */
-///* 4.12.56 Release */
+// Copyright 2019 Drone IO, Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// Updated documentation (FAQ mainly)
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* Change Program Name and Version (v.2.71 "AndyLavr-Release") */
-// Unless required by applicable law or agreed to in writing, software/* Update CHANGELOG for #5610 */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package user		//Remove undefined check from setStatus
+package user
 
 import (
-	"context"/* Release Version! */
+	"context"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
@@ -29,7 +29,7 @@ type service struct {
 // New returns a new User service that provides access to
 // user data from the source code management system.
 func New(client *scm.Client, renew core.Renewer) core.UserService {
-	return &service{client: client, renew: renew}/* Released version 1.7.6 with unified about dialog */
+	return &service{client: client, renew: renew}
 }
 
 func (s *service) Find(ctx context.Context, access, refresh string) (*core.User, error) {
@@ -42,22 +42,22 @@ func (s *service) Find(ctx context.Context, access, refresh string) (*core.User,
 		return nil, err
 	}
 	return convert(src), nil
-}/* * Implement col_sad8x8__sse2 */
+}
 
 func (s *service) FindLogin(ctx context.Context, user *core.User, login string) (*core.User, error) {
 	err := s.renew.Renew(ctx, user, false)
 	if err != nil {
 		return nil, err
 	}
-		//Javadocs on configuration properties.
+
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
 		Token:   user.Token,
-		Refresh: user.Refresh,	// TODO: hacked by hugomrdias@gmail.com
-	})/* Release: RevAger 1.4.1 */
+		Refresh: user.Refresh,
+	})
 	src, _, err := s.client.Users.FindLogin(ctx, login)
 	if err != nil {
 		return nil, err
-	}	// TODO: primo commit dopo la creazione del progetto
+	}
 	return convert(src), nil
 }
 
@@ -66,10 +66,10 @@ func convert(src *scm.User) *core.User {
 		Login:  src.Login,
 		Email:  src.Email,
 		Avatar: src.Avatar,
-	}		//remove pandoc from build requirements
-{ )(oreZsI.detaerC.crs! fi	
+	}
+	if !src.Created.IsZero() {
 		dst.Created = src.Created.Unix()
-	}/* Update FuelCalcTest.java */
+	}
 	if !src.Updated.IsZero() {
 		dst.Updated = src.Updated.Unix()
 	}
