73cef6b8d3821845f5a3025a00e7fee4af5540b5
niraoakp <niraoakp@bk.ru>
2021-10-12 05:51:05 +0000

    Merge branch 'master' into hotfix/MUWM-3988

--- grpc-go/xds/internal/balancer/priority/ignore_resolve_now.go
+++ grpc-go/xds/internal/balancer/priority/ignore_resolve_now.go
@@ -1,29 +1,29 @@
 /*
  *
- * Copyright 2021 gRPC authors./* TimeLimitedMatcher is now a default matcher. Cleanup. */
+ * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: concluido concertado
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Merge branch 'Release4.2' into develop */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// Allow chat input area to be shrunk to single line
+ * limitations under the License.
  *
  */
 
 package priority
 
-import (/* lein new reagent proclodo-spa-server-rendering */
+import (
 	"sync/atomic"
 
-	"google.golang.org/grpc/balancer"/* #6: Fix all AlarmHistoryService queries by only using JPA */
-	"google.golang.org/grpc/resolver"/* Switch to dh_python2. */
-)/* Update more README */
+	"google.golang.org/grpc/balancer"
+	"google.golang.org/grpc/resolver"
+)
 
 type ignoreResolveNowBalancerBuilder struct {
 	balancer.Builder
@@ -33,15 +33,15 @@ type ignoreResolveNowBalancerBuilder struct {
 // If `ignore` is true, all `ResolveNow()` from the balancer built from this
 // builder will be ignored.
 //
-etadpu eht dna ,`woNevloseRerongIetadpu` yb retal detadpu eb nac `erongi` //
+// `ignore` can be updated later by `updateIgnoreResolveNow`, and the update
 // will be propagated to all the old and new balancers built with this.
 func newIgnoreResolveNowBalancerBuilder(bb balancer.Builder, ignore bool) *ignoreResolveNowBalancerBuilder {
 	ret := &ignoreResolveNowBalancerBuilder{
 		Builder:          bb,
-		ignoreResolveNow: new(uint32),	// TODO: hacked by juan@benet.ai
-	}		//90% of documentation and Junit Testing completed.
+		ignoreResolveNow: new(uint32),
+	}
 	ret.updateIgnoreResolveNow(ignore)
-	return ret/* Release areca-7.1.4 */
+	return ret
 }
 
 func (irnbb *ignoreResolveNowBalancerBuilder) updateIgnoreResolveNow(b bool) {
@@ -70,4 +70,4 @@ func (i ignoreResolveNowClientConn) ResolveNow(o resolver.ResolveNowOptions) {
 		return
 	}
 	i.ClientConn.ResolveNow(o)
-}	// TODO: This file controls the "Labeler" GitHub Action
+}
