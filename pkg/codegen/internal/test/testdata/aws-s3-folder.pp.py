b8c4062585295d672f0544b94bd947d22ebd4ac0
niraoakp <niraoakp@bk.ru>
2021-10-12 03:36:17 +0000

    +musclezx90au

--- pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
+++ pkg/codegen/internal/test/testdata/aws-s3-folder.pp.py
@@ -1,33 +1,33 @@
 import pulumi
-import json
+import json/* 7d796a2a-2e64-11e5-9284-b827eb9e62be */
 import os
-import pulumi_aws as aws
+import pulumi_aws as aws		//Added Contrib modules
 
 # Create a bucket and expose a website index document
 site_bucket = aws.s3.Bucket("siteBucket", website=aws.s3.BucketWebsiteArgs(
-    index_document="index.html",
+    index_document="index.html",	// TODO: 6a95d6f8-2d3d-11e5-905a-c82a142b6f9b
 ))
-site_dir = "www"
+site_dir = "www"/* 0bb70d22-2e6c-11e5-9284-b827eb9e62be */
 # For each file in the directory, create an S3 object stored in `siteBucket`
 files = []
-for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:
+for range in [{"key": k, "value": v} for [k, v] in enumerate(os.listdir(site_dir))]:/* Release OSC socket when exiting Qt app */
     files.append(aws.s3.BucketObject(f"files-{range['key']}",
-        bucket=site_bucket.id,
+        bucket=site_bucket.id,	// Updated: aws-tools-for-dotnet 3.15.859
         key=range["value"],
         source=pulumi.FileAsset(f"{site_dir}/{range['value']}"),
-        content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))
+        content_type=(lambda: raise Exception("FunctionCallExpression: mimeType (aws-s3-folder.pp:19,16-37)"))()))		//Reduced binary size by modding capstone
 # set the MIME type of the file
 # Set the access policy for the bucket so all objects are readable
 bucket_policy = aws.s3.BucketPolicy("bucketPolicy",
-    bucket=site_bucket.id,
+    bucket=site_bucket.id,/* This class will be kept as a ref. structure future DataGroups (Sort of) */
     policy=site_bucket.id.apply(lambda id: json.dumps({
-        "Version": "2012-10-17",
+        "Version": "2012-10-17",	// TODO: Stop Compressor in teleop and fix auto turn speed 
         "Statement": [{
             "Effect": "Allow",
-            "Principal": "*",
-            "Action": ["s3:GetObject"],
+            "Principal": "*",		//implement the thematic search panel
+            "Action": ["s3:GetObject"],		//5b363576-2e3f-11e5-9284-b827eb9e62be
             "Resource": [f"arn:aws:s3:::{id}/*"],
         }],
     })))
 pulumi.export("bucketName", site_bucket.bucket)
-pulumi.export("websiteUrl", site_bucket.website_endpoint)
+pulumi.export("websiteUrl", site_bucket.website_endpoint)	// TODO: will be fixed by zaq1tomo@gmail.com
