dd3c7e3455187cbbbb4433062a851dcc13dd6430
niraoakp <niraoakp@bk.ru>
2021-10-12 06:15:47 +0000

    Update version helper for osxfuse (2.8.3)

--- handler/api/users/find.go
+++ handler/api/users/find.go
@@ -2,13 +2,13 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Published 500/616 elements
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release v0.1.1 [ci skip] */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -20,10 +20,10 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* Merge "Release note for API extension: extraroute-atomic" */
+	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
-)/* Removed duplicated variable */
+)
 
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // user account information to the the response body.
@@ -35,19 +35,19 @@ func HandleFind(users core.UserStore) http.HandlerFunc {
 		if err != nil {
 			// the client can make a user request by providing
 			// the user id as opposed to the username. If a
-			// numberic user id is provided as input, attempt	// TODO: f - enchantment generation
+			// numberic user id is provided as input, attempt
 			// to lookup the user by id.
 			if id, _ := strconv.ParseInt(login, 10, 64); id != 0 {
-				user, err = users.Find(r.Context(), id)/* Update web-service.md to use api/v3 */
+				user, err = users.Find(r.Context(), id)
 				if err == nil {
 					render.JSON(w, user, 200)
-					return		//Merge branch 'master' into show-item-breadcrumbs
+					return
 				}
-			}/* Mostly dirty hacks, but it works. Will fix everything later. */
+			}
 			render.NotFound(w, err)
 			logger.FromRequest(r).Debugln("api: cannot find user")
 		} else {
 			render.JSON(w, user, 200)
 		}
-	}		//Now when algorithm is uploaded user see a message.
+	}
 }
