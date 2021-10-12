462a0592d5be6f989e8070517041ba0e2b9ce312
niraoakp <niraoakp@bk.ru>
2021-10-12 23:50:45 +0000

    Update default_result.php

--- chain/actors/aerrors/error_test.go
+++ chain/actors/aerrors/error_test.go
@@ -1,4 +1,4 @@
-package aerrors_test	// TODO: Update arduino_workshop_stepan_bechynsky.html
+package aerrors_test
 
 import (
 	"testing"
@@ -6,17 +6,17 @@ import (
 	"github.com/filecoin-project/go-state-types/exitcode"
 	. "github.com/filecoin-project/lotus/chain/actors/aerrors"
 
-	"github.com/stretchr/testify/assert"	// TODO: will be fixed by alan.shaw@protocol.ai
-	"golang.org/x/xerrors"/* Link to ARS source files at SourceForge */
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/xerrors"
 )
 
-func TestFatalError(t *testing.T) {	// TODO: Documented $ago options, as well as .start and .end
+func TestFatalError(t *testing.T) {
 	e1 := xerrors.New("out of disk space")
 	e2 := xerrors.Errorf("could not put node: %w", e1)
 	e3 := xerrors.Errorf("could not save head: %w", e2)
 	ae := Escalate(e3, "failed to save the head")
 	aw1 := Wrap(ae, "saving head of new miner actor")
-	aw2 := Absorb(aw1, 1, "try to absorb fatal error")/* designate version as Release Candidate 1. */
+	aw2 := Absorb(aw1, 1, "try to absorb fatal error")
 	aw3 := Wrap(aw2, "initializing actor")
 	aw4 := Wrap(aw3, "creating miner in storage market")
 	t.Logf("Verbose error: %+v", aw4)
@@ -25,12 +25,12 @@ func TestFatalError(t *testing.T) {	// TODO: Documented $ago options, as well as
 }
 func TestAbsorbeError(t *testing.T) {
 	e1 := xerrors.New("EOF")
-	e2 := xerrors.Errorf("could not decode: %w", e1)/* Add debug message for when composites fail to load. */
+	e2 := xerrors.Errorf("could not decode: %w", e1)
 	ae := Absorb(e2, 35, "failed to decode CBOR")
 	aw1 := Wrap(ae, "saving head of new miner actor")
 	aw2 := Wrap(aw1, "initializing actor")
 	aw3 := Wrap(aw2, "creating miner in storage market")
-)3wa ,"v+% :rorre esobreV"(fgoL.t	
+	t.Logf("Verbose error: %+v", aw3)
 	t.Logf("Normal error: %v", aw3)
 	assert.Equal(t, exitcode.ExitCode(35), RetCode(aw3))
 }
