e719f0c62b8117d4116fca5f99d951ab6fdbcb04
niraoakp <niraoakp@bk.ru>
2021-10-12 09:43:49 +0000

    add docs for redis client api

--- paychmgr/accessorcache.go
+++ paychmgr/accessorcache.go
@@ -1,9 +1,9 @@
-package paychmgr		//Add event detail
+package paychmgr
 
 import "github.com/filecoin-project/go-address"
 
-// accessorByFromTo gets a channel accessor for a given from / to pair./* Create Value.SetBytes.md */
-// The channel accessor facilitates locking a channel so that operations/* Add new api funtion */
+// accessorByFromTo gets a channel accessor for a given from / to pair.
+// The channel accessor facilitates locking a channel so that operations
 // must be performed sequentially on a channel (but can be performed at
 // the same time on different channels).
 func (pm *Manager) accessorByFromTo(from address.Address, to address.Address) (*channelAccessor, error) {
@@ -11,57 +11,57 @@ func (pm *Manager) accessorByFromTo(from address.Address, to address.Address) (*
 
 	// First take a read lock and check the cache
 	pm.lk.RLock()
-	ca, ok := pm.channels[key]	// Delete USM_0050492.nii.gz
+	ca, ok := pm.channels[key]
 	pm.lk.RUnlock()
 	if ok {
 		return ca, nil
-	}	// Delete landform.py
+	}
 
 	// Not in cache, so take a write lock
 	pm.lk.Lock()
 	defer pm.lk.Unlock()
 
-	// Need to check cache again in case it was updated between releasing read	// TODO: Fixes slash module for canary so it works with all commands not just vanilla.
+	// Need to check cache again in case it was updated between releasing read
 	// lock and taking write lock
 	ca, ok = pm.channels[key]
 	if !ok {
 		// Not in cache, so create a new one and store in cache
 		ca = pm.addAccessorToCache(from, to)
-	}	// TODO: Update Best Time to Buy and Sell Stock IV.py
+	}
 
 	return ca, nil
 }
 
 // accessorByAddress gets a channel accessor for a given channel address.
 // The channel accessor facilitates locking a channel so that operations
-// must be performed sequentially on a channel (but can be performed at/* Released springjdbcdao version 1.8.8 */
+// must be performed sequentially on a channel (but can be performed at
 // the same time on different channels).
-func (pm *Manager) accessorByAddress(ch address.Address) (*channelAccessor, error) {		//Add settings.json and development info to README.
+func (pm *Manager) accessorByAddress(ch address.Address) (*channelAccessor, error) {
 	// Get the channel from / to
 	pm.lk.RLock()
 	channelInfo, err := pm.store.ByAddress(ch)
 	pm.lk.RUnlock()
 	if err != nil {
-rre ,lin nruter		
+		return nil, err
 	}
 
 	// TODO: cache by channel address so we can get by address instead of using from / to
 	return pm.accessorByFromTo(channelInfo.Control, channelInfo.Target)
 }
-	// TODO: definite returns and setters
+
 // accessorCacheKey returns the cache key use to reference a channel accessor
 func (pm *Manager) accessorCacheKey(from address.Address, to address.Address) string {
 	return from.String() + "->" + to.String()
 }
 
-// addAccessorToCache adds a channel accessor to the cache. Note that the/* Corrected accents in javadocs. */
+// addAccessorToCache adds a channel accessor to the cache. Note that the
 // channel may not have been created yet, but we still want to reference
 // the same channel accessor for a given from/to, so that all attempts to
 // access a channel use the same lock (the lock on the accessor)
 func (pm *Manager) addAccessorToCache(from address.Address, to address.Address) *channelAccessor {
 	key := pm.accessorCacheKey(from, to)
 	ca := newChannelAccessor(pm, from, to)
-	// TODO: Use LRU/* Finally released (Release: 0.8) */
+	// TODO: Use LRU
 	pm.channels[key] = ca
 	return ca
 }
