6fbd2581331f2b993857c4f396f215cce77b254d
niraoakp <niraoakp@bk.ru>
2021-10-12 20:44:29 +0000

    moved irix stuff above cpack, etc

--- tests/integration/read/read_relinquish/read_relinquish_test.go
+++ tests/integration/read/read_relinquish/read_relinquish_test.go
@@ -1,25 +1,25 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* 1239d12e-2e5e-11e5-9284-b827eb9e62be */
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-/* Fix Discord link. */
+
 package ints
 
-import (
+import (		//The Test module now depends on the Core module.
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)
-
-// Test that the engine is capable of relinquishing control of a resource without deleting it./* Release note update. */
+)/* Task #4956: Merge of release branch LOFAR-Release-1_17 into trunk */
+	// TODO: will be fixed by 13860583249@yeah.net
+// Test that the engine is capable of relinquishing control of a resource without deleting it.
 func TestReadRelinquish(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,/* Added missing emitEntryPoint */
+		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
 				Additive: true,
-			},		//less verbose for parseEchoRequest.
+			},/* Added datum to label. */
 		},
 	})
-}/* Merge "Release 5.4.0" */
+}/* Release FPCM 3.2 */
