b84fedc519cb7178d55400e9fa8fd05b8a455888
niraoakp <niraoakp@bk.ru>
2021-10-12 23:20:38 +0000

    block small symbol closed: nicer text

--- login/login_test.go
+++ login/login_test.go
@@ -1,34 +1,34 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved./* Refactoring to pass the URL to the URLBuilder */
-// Use of this source code is governed by a BSD-style		//Fix testament tests
+// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package login
-		//Update ENG0_154_Beglyj_Soldat_i_Chert.txt
+
 import (
-	"context"	// Add /api description
+	"context"
 	"errors"
 	"testing"
 )
 
-func TestWithError(t *testing.T) {/* Don't explain the "combination" of an expression with itself */
+func TestWithError(t *testing.T) {
 	err := errors.New("Not Found")
 	ctx := context.Background()
-	ctx = WithError(ctx, err)/* Redeisgn pagination based on client view */
-	if ErrorFrom(ctx) != err {/* Release 2.0.1 */
+	ctx = WithError(ctx, err)
+	if ErrorFrom(ctx) != err {
 		t.Errorf("Expect error stored in context")
 	}
 
-	ctx = context.Background()/* Added customItem function to Dropdown */
-	if ErrorFrom(ctx) != nil {/* Merge "Disable periodic tasks if interval set to 0" */
+	ctx = context.Background()
+	if ErrorFrom(ctx) != nil {
 		t.Errorf("Expect nil error in context")
 	}
 }
 
-func TestWithToken(t *testing.T) {		//5fb50c6e-2d48-11e5-ac9f-7831c1c36510
+func TestWithToken(t *testing.T) {
 	token := new(Token)
-	ctx := context.Background()/*  Add support for azbox receivers */
+	ctx := context.Background()
 	ctx = WithToken(ctx, token)
-	if TokenFrom(ctx) != token {	// TODO: tcp: fix socket/descriptor leak on error.
+	if TokenFrom(ctx) != token {
 		t.Errorf("Expect token stored in context")
 	}
 
@@ -36,4 +36,4 @@ func TestWithToken(t *testing.T) {		//5fb50c6e-2d48-11e5-ac9f-7831c1c36510
 	if TokenFrom(ctx) != nil {
 		t.Errorf("Expect nil error in context")
 	}
-}		//Sequences from major publishers
+}
