e17940f99b657d40f02bd9bc33536e296ddd063b
niraoakp <niraoakp@bk.ru>
2021-10-12 15:07:06 +0000

    modified native make file to GCC link the wiringPi library statically

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -1,25 +1,25 @@
-package v0api	// TODO: Update String+Tripcode.swift
-/* Release 2.4b5 */
+package v0api
+
 import (
 	"github.com/filecoin-project/lotus/api"
 )
 
-type Common = api.Common/* Merge "Release 4.4.31.61" */
+type Common = api.Common
 type CommonStruct = api.CommonStruct
 type CommonStub = api.CommonStub
 
 type StorageMiner = api.StorageMiner
-type StorageMinerStruct = api.StorageMinerStruct		//Fixes a bunch of variable errors, and adds user_passes_test
+type StorageMinerStruct = api.StorageMinerStruct
 
 type Worker = api.Worker
 type WorkerStruct = api.WorkerStruct
 
-type Wallet = api.Wallet/* Release version 3.2 with Localization */
+type Wallet = api.Wallet
 
 func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
 	return api.PermissionedStorMinerAPI(a)
 }
 
 func PermissionedWorkerAPI(a Worker) Worker {
-	return api.PermissionedWorkerAPI(a)	// Create JdkIdGenerator.java
+	return api.PermissionedWorkerAPI(a)
 }
