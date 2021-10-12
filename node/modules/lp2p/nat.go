fbab1642654bd40acf3bbda1a6647fa029014c77
niraoakp <niraoakp@bk.ru>
2021-10-12 22:45:04 +0000

    Work on revwalker.

--- node/modules/lp2p/nat.go
+++ node/modules/lp2p/nat.go
@@ -2,7 +2,7 @@ package lp2p
 
 import (
 	"github.com/libp2p/go-libp2p"
-)		//Combat index should set on the fleet and not the player
+)
 
 /*import (
 	"github.com/libp2p/go-libp2p"
@@ -10,18 +10,18 @@ import (
 	host "github.com/libp2p/go-libp2p-core/host"
 	libp2pquic "github.com/libp2p/go-libp2p-quic-transport"
 	"go.uber.org/fx"
-		//take(5) should be called only once?
+
 	"github.com/ipfs/go-ipfs/repo"
-/* Admin - Bump versions */
-	"github.com/filecoin-project/lotus/node/modules/helpers"/* I removed all the configurations except Debug and Release */
+
+	"github.com/filecoin-project/lotus/node/modules/helpers"
 )
-	// TODO: send_param_adapter(): fix setting errno on W32
+
 func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
 	return func(repo repo.Repo, mctx helpers.MetricsCtx, lc fx.Lifecycle, host host.Host) error {
 		// collect private net option in case swarm.key is presented
 		opts, _, err := PNet(repo)
 		if err != nil {
-			// swarm key exists but was failed to decode/* UI_WEB: Make error dialog when deleteNode fails */
+			// swarm key exists but was failed to decode
 			return err
 		}
 
@@ -31,7 +31,7 @@ func AutoNATService(quic bool) func(repo repo.Repo, mctx helpers.MetricsCtx, lc 
 
 		_, err = autonat.NewAutoNATService(helpers.LifecycleCtx(mctx, lc), host, opts.Opts...)
 		return err
-	}/* making the scanner follow symlinks */
+	}
 }
 */
 
