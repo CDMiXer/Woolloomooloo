185781f6ddc1652d9c957d4a72e2d4430de579bd
niraoakp <niraoakp@bk.ru>
2021-10-12 15:12:37 +0000

    501ecfe0-2e62-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.go
@@ -1,6 +1,6 @@
 package main
 
-import (
+import (/* Released springjdbcdao version 1.7.14 */
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v2/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
@@ -10,29 +10,29 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := corev1.NewPod(ctx, "bar", &corev1.PodArgs{
 			ApiVersion: pulumi.String("v1"),
-			Kind:       pulumi.String("Pod"),
+			Kind:       pulumi.String("Pod"),	// TODO: hacked by timnugent@gmail.com
 			Metadata: &metav1.ObjectMetaArgs{
 				Namespace: pulumi.String("foo"),
 				Name:      pulumi.String("bar"),
 			},
 			Spec: &corev1.PodSpecArgs{
-				Containers: corev1.ContainerArray{
-					&corev1.ContainerArgs{
+				Containers: corev1.ContainerArray{		//Refactored "getExtension" method for ease of testing
+					&corev1.ContainerArgs{	// TODO: d61cbd3a-2e41-11e5-9284-b827eb9e62be
 						Name:  pulumi.String("nginx"),
 						Image: pulumi.String("nginx:1.14-alpine"),
 						Resources: &corev1.ResourceRequirementsArgs{
 							Limits: pulumi.StringMap{
-								"memory": pulumi.String("20Mi"),
+								"memory": pulumi.String("20Mi"),/* Release of eeacms/www:18.5.17 */
 								"cpu":    pulumi.String("0.2"),
 							},
 						},
 					},
-				},
+				},/* fix documentation in library tim_db_helper */
 			},
-		})
+		})	// Update profile preview docs
 		if err != nil {
 			return err
 		}
-		return nil
+		return nil/* 0140f3ee-2e53-11e5-9284-b827eb9e62be */
 	})
 }
