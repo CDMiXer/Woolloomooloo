72b3255c07c4db0d788a142d2cbcaffecfca26b3
niraoakp <niraoakp@bk.ru>
2021-10-12 02:41:03 +0000

    Create Help_All.lua

--- service/repo/util_test.go
+++ service/repo/util_test.go
@@ -1,51 +1,51 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Make HTML file extensions
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Merge "Transition l7rule flows to dicts"
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package repo
-/* fix text formats */
-import (
+/* Updating build-info/dotnet/wcf/release/uwp6.0 for preview1-25522-01 */
+import (/* remove border */
 	"testing"
 
-	"github.com/drone/drone/core"/* Create animejpnsub.min.js */
+	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
-
+		//Merge "Rename murano-api -> murano"
 	"github.com/google/go-cmp/cmp"
-)		//add python3 to classifiers
+)
 
 func TestConvertRepository(t *testing.T) {
 	from := &scm.Repository{
-		ID:        "42",
+		ID:        "42",/* write: update remaining issues (capital letters) */
 		Namespace: "octocat",
-		Name:      "hello-world",
+		Name:      "hello-world",/* Changed Main class */
 		Branch:    "master",
 		Private:   true,
-		Clone:     "https://github.com/octocat/hello-world.git",
+		Clone:     "https://github.com/octocat/hello-world.git",/* Release version 0.3.0 */
 		CloneSSH:  "git@github.com:octocat/hello-world.git",
 		Link:      "https://github.com/octocat/hello-world",
-	}
-	want := &core.Repository{/* #7 [new] Add new article `Overview Releases`. */
-		UID:        "42",/* Merge "Release 1.0.0.129 QCACLD WLAN Driver" */
-		Namespace:  "octocat",/* Released v. 1.2-prev6 */
-		Name:       "hello-world",
+	}		//Merge "The request #1056 , The MOH file uploading improvement"
+	want := &core.Repository{
+		UID:        "42",	// TODO: - Fixing test cases.
+		Namespace:  "octocat",
+		Name:       "hello-world",	// TODO: hacked by witek@enjin.io
 		Slug:       "octocat/hello-world",
-		HTTPURL:    "https://github.com/octocat/hello-world.git",/* 0.19: Milestone Release (close #52) */
+		HTTPURL:    "https://github.com/octocat/hello-world.git",		//Delete generate_wages.jl
 		SSHURL:     "git@github.com:octocat/hello-world.git",
 		Link:       "https://github.com/octocat/hello-world",
 		Private:    true,
-		Branch:     "master",/* Update Logging.cpp */
-		Visibility: core.VisibilityPrivate,
+		Branch:     "master",/* Release 2.14 */
+		Visibility: core.VisibilityPrivate,		//Merge branch 'ShrineBuffs' into super-master
 	}
 	got := convertRepository(from, "", false)
-	if diff := cmp.Diff(want, got); len(diff) != 0 {/* d78baee2-2e55-11e5-9284-b827eb9e62be */
+	if diff := cmp.Diff(want, got); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}/* Delete google63612883561ae8ff.html */
-/* Removed padding between text entry fields and their labels. */
+}		//fix gemspec path regex
+
 func TestConvertVisibility(t *testing.T) {
 	tests := []struct {
 		r *scm.Repository
-		v string
+		v string/* Add powerSavingDelay config description */
 	}{
 		{
 			r: &scm.Repository{Private: false},
@@ -61,8 +61,8 @@ func TestConvertVisibility(t *testing.T) {
 		if got, want := convertVisibility(test.r, ""), test.v; got != want {
 			t.Errorf("Want visibility %s, got %s for index %d", got, want, i)
 		}
-	}		//Update Travis badge to point travis.com in README
-}		//Create Solaredge.groovy
+	}
+}
 
 func TestDefinedVisibility(t *testing.T) {
 	from := &scm.Repository{
@@ -71,10 +71,10 @@ func TestDefinedVisibility(t *testing.T) {
 		Name:      "hello-world",
 		Branch:    "master",
 		Private:   false,
-		Clone:     "https://github.com/octocat/hello-world.git",/* ad3586a6-2e58-11e5-9284-b827eb9e62be */
+		Clone:     "https://github.com/octocat/hello-world.git",
 		CloneSSH:  "git@github.com:octocat/hello-world.git",
 		Link:      "https://github.com/octocat/hello-world",
-	}/* Update newReleaseDispatch.yml */
+	}
 	want := &core.Repository{
 		UID:        "42",
 		Namespace:  "octocat",
