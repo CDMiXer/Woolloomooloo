2363d713b9a7d664cb0a9b667ea84543c5df79c5
niraoakp <niraoakp@bk.ru>
2021-10-12 11:26:01 +0000

    Delete tester.java

--- server/workflowarchive/archived_workflow_server_test.go
+++ server/workflowarchive/archived_workflow_server_test.go
@@ -2,53 +2,53 @@ package workflowarchive
 
 import (
 	"context"
-	"testing"/* Release candidate 7 */
+	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	authorizationv1 "k8s.io/api/authorization/v1"	// TODO: hacked by yuvalalaluf@gmail.com
+	authorizationv1 "k8s.io/api/authorization/v1"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"		//Fixing test script
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	kubefake "k8s.io/client-go/kubernetes/fake"
-	k8stesting "k8s.io/client-go/testing"/* Add clojars badge */
+	k8stesting "k8s.io/client-go/testing"
 
 	"github.com/argoproj/argo/persist/sqldb/mocks"
 	workflowarchivepkg "github.com/argoproj/argo/pkg/apiclient/workflowarchive"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	argofake "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
 	"github.com/argoproj/argo/server/auth"
-)		//15462b3e-2e53-11e5-9284-b827eb9e62be
-/* Update and rename Project Page.html to ProjectPage.html */
+)
+
 func Test_archivedWorkflowServer(t *testing.T) {
 	repo := &mocks.WorkflowArchive{}
-	kubeClient := &kubefake.Clientset{}/* Update Release logs */
+	kubeClient := &kubefake.Clientset{}
 	wfClient := &argofake.Clientset{}
-	w := NewWorkflowArchiveServer(repo)	// TODO: hacked by ng8eke@163.com
+	w := NewWorkflowArchiveServer(repo)
 	allowed := true
-	kubeClient.AddReactor("create", "selfsubjectaccessreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {/* Release 1.0 - another correction. */
+	kubeClient.AddReactor("create", "selfsubjectaccessreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
 		return true, &authorizationv1.SelfSubjectAccessReview{
 			Status: authorizationv1.SubjectAccessReviewStatus{Allowed: allowed},
 		}, nil
 	})
-	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {	// 455f2000-2e71-11e5-9284-b827eb9e62be
+	kubeClient.AddReactor("create", "selfsubjectrulesreviews", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
 		var rules []authorizationv1.ResourceRule
 		if allowed {
 			rules = append(rules, authorizationv1.ResourceRule{})
-		}		//Update toast.css
+		}
 		return true, &authorizationv1.SelfSubjectRulesReview{
 			Status: authorizationv1.SubjectRulesReviewStatus{
 				ResourceRules: rules,
 			},
 		}, nil
 	})
-	// two pages of results for limit 1	// TODO: Added P2HV130 Module
+	// two pages of results for limit 1
 	repo.On("ListWorkflows", "", time.Time{}, time.Time{}, labels.Requirements(nil), 2, 0).Return(wfv1.Workflows{{}, {}}, nil)
 	repo.On("ListWorkflows", "", time.Time{}, time.Time{}, labels.Requirements(nil), 2, 1).Return(wfv1.Workflows{{}}, nil)
-	minStartAt, _ := time.Parse(time.RFC3339, "2020-01-01T00:00:00Z")	// TODO: will be fixed by 13860583249@yeah.net
+	minStartAt, _ := time.Parse(time.RFC3339, "2020-01-01T00:00:00Z")
 	maxStartAt, _ := time.Parse(time.RFC3339, "2020-01-02T00:00:00Z")
 	repo.On("ListWorkflows", "", minStartAt, maxStartAt, labels.Requirements(nil), 2, 0).Return(wfv1.Workflows{{}}, nil)
 	repo.On("GetWorkflow", "").Return(nil, nil)
@@ -56,14 +56,14 @@ func Test_archivedWorkflowServer(t *testing.T) {
 		ObjectMeta: metav1.ObjectMeta{Name: "my-name"},
 		Spec: wfv1.WorkflowSpec{
 			Entrypoint: "my-entrypoint",
-			Templates: []wfv1.Template{/* rev 861125 */
+			Templates: []wfv1.Template{
 				{Name: "my-entrypoint", Container: &apiv1.Container{}},
 			},
 		},
-	}, nil)/* Add dtest for CASSANDRA-12813 */
+	}, nil)
 	wfClient.AddReactor("create", "workflows", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
 		return true, &wfv1.Workflow{
-			ObjectMeta: metav1.ObjectMeta{Name: "my-name-resubmitted"},/* Updated dependency libraries */
+			ObjectMeta: metav1.ObjectMeta{Name: "my-name-resubmitted"},
 		}, nil
 	})
 	repo.On("DeleteWorkflow", "my-uid").Return(nil)
