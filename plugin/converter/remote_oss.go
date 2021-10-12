5e7ab2ea3f4531ee2a5cfe4ae72cfa30402ed544
niraoakp <niraoakp@bk.ru>
2021-10-12 12:35:35 +0000

    fixing build #255

--- plugin/converter/remote_oss.go
+++ plugin/converter/remote_oss.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by hugomrdias@gmail.com
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -8,22 +8,22 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Update aa_spieltag_simulation.php
-// See the License for the specific language governing permissions and
-// limitations under the License.		//Delete WAAEforcestartjob.java
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and	// TODO: hacked by arajasek94@gmail.com
+// limitations under the License.
 
-// +build oss
+// +build oss/* [artifactory-release] Release version 3.1.0.BUILD */
 
 package converter
 
 import (
-	"time"/* QtSql: updated to use the macro PBOOL */
-
-	"github.com/drone/drone/core"/* Merge "Remove refresh in Stack update_and_save" */
+	"time"
+	// Enabled saving and loading of script settings.
+	"github.com/drone/drone/core"		//Delete JWZPhotoViewer.xcscheme
 )
 
-// Remote returns a conversion service that converts the
+// Remote returns a conversion service that converts the	// TODO: StatusIcon demo
 // configuration file using a remote http service.
 func Remote(endpoint, signer, extension string, skipVerify bool, timeout time.Duration) core.ConvertService {
-	return new(noop)
+	return new(noop)	// TODO: hacked by mail@bitpshr.net
 }
