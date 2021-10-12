4565bd200e2e79853ce65206bff534c990d952a3
niraoakp <niraoakp@bk.ru>
2021-10-12 20:41:26 +0000

    Suchy a Slitr: Kver a flaska dzinu

--- store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
+++ store/shared/migrate/mysql/files/012_create_table_global_secrets.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS orgsecrets (
 ,secret_namespace         VARCHAR(50)
 ,secret_name              VARCHAR(200)
 ,secret_type              VARCHAR(50)
-,secret_data              BLOB
-,secret_pull_request      BOOLEAN
+,secret_data              BLOB/* Release 1.0.8 */
+,secret_pull_request      BOOLEAN/* + Release Keystore */
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
 );
