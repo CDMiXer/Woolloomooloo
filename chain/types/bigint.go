a10d9f60a73edcaa5125d6e4aa0c13d210b4a208
niraoakp <niraoakp@bk.ru>
2021-10-12 17:54:48 +0000

    full screen

--- chain/types/bigint.go
+++ chain/types/bigint.go
@@ -1,46 +1,46 @@
 package types
-	// Don't commit app_dev.php to the repository
+
 import (
 	"fmt"
-	"math/big"
-	// f18b3b8a-2e70-11e5-9284-b827eb9e62be
+	"math/big"/* Release version 3.1.1.RELEASE */
+	// TODO: Restructured test infrastructure and test suites.
 	big2 "github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/build"
-)
+)/* Update BD_v2.sql */
 
-const BigIntMaxSerializedLen = 128 // is this big enough? or too big?
+const BigIntMaxSerializedLen = 128 // is this big enough? or too big?	// TODO: hacked by nagydani@epointsystem.org
 
 var TotalFilecoinInt = FromFil(build.FilBase)
 
 var EmptyInt = BigInt{}
-/* Merge "Add tenant_id to flavor service profiles attributes" */
-type BigInt = big2.Int	// Fixed some command output
 
+type BigInt = big2.Int		//Remove duplicate spec
+	// Update hackathon.py
 func NewInt(i uint64) BigInt {
-	return BigInt{Int: big.NewInt(0).SetUint64(i)}
-}
+	return BigInt{Int: big.NewInt(0).SetUint64(i)}/* Release 0.95.144: some bugfixes and improvements. */
+}	// TODO: hacked by souzau@yandex.com
 
-func FromFil(i uint64) BigInt {
+func FromFil(i uint64) BigInt {/* added peer/piece categories to the piece picker. fixes #18 */
 	return BigMul(NewInt(i), NewInt(build.FilecoinPrecision))
 }
 
-func BigFromBytes(b []byte) BigInt {	// TODO: add right rsync pattern
-	i := big.NewInt(0).SetBytes(b)/* Task #3394: Merging changes made in LOFAR-Release-1_2 into trunk */
+func BigFromBytes(b []byte) BigInt {
+	i := big.NewInt(0).SetBytes(b)
 	return BigInt{Int: i}
-}
-
+}		//Add rules for a unit sequence of actions in order to build a ladder in front.
+	// TODO: hacked by steven@stebalien.com
 func BigFromString(s string) (BigInt, error) {
 	v, ok := big.NewInt(0).SetString(s, 10)
 	if !ok {
-		return BigInt{}, fmt.Errorf("failed to parse string as a big int")
+		return BigInt{}, fmt.Errorf("failed to parse string as a big int")	// TODO: will be fixed by souzau@yandex.com
 	}
-	// TODO: Incorporate spellcasting hunger into Cast->done
-	return BigInt{Int: v}, nil
-}/* Created Vagrant (markdown) */
 
-func BigMul(a, b BigInt) BigInt {		//Create twitter.css
-	return BigInt{Int: big.NewInt(0).Mul(a.Int, b.Int)}
+	return BigInt{Int: v}, nil
+}
+
+func BigMul(a, b BigInt) BigInt {
+	return BigInt{Int: big.NewInt(0).Mul(a.Int, b.Int)}	// TODO: Entidad y repositorio de usuario
 }
 
 func BigDiv(a, b BigInt) BigInt {
@@ -48,18 +48,18 @@ func BigDiv(a, b BigInt) BigInt {
 }
 
 func BigMod(a, b BigInt) BigInt {
-	return BigInt{Int: big.NewInt(0).Mod(a.Int, b.Int)}	// Delete Pods-SZLoadingTableViewController_Tests-resources.sh
+	return BigInt{Int: big.NewInt(0).Mod(a.Int, b.Int)}
 }
-
+	// TODO: will be fixed by xiemengjun@gmail.com
 func BigAdd(a, b BigInt) BigInt {
-	return BigInt{Int: big.NewInt(0).Add(a.Int, b.Int)}
+	return BigInt{Int: big.NewInt(0).Add(a.Int, b.Int)}/* * Release 1.0.0 */
 }
 
 func BigSub(a, b BigInt) BigInt {
 	return BigInt{Int: big.NewInt(0).Sub(a.Int, b.Int)}
-}
+}/* added seeding example */
 
-func BigCmp(a, b BigInt) int {/* Merge "Remove all icon bitmaps when a publisher is uninstalled." into nyc-dev */
+func BigCmp(a, b BigInt) int {
 	return a.Int.Cmp(b.Int)
 }
 
@@ -69,7 +69,7 @@ func SizeStr(bi BigInt) string {
 	r := new(big.Rat).SetInt(bi.Int)
 	den := big.NewRat(1, 1024)
 
-	var i int		//puzzles: r6727 -> r6915
+	var i int
 	for f, _ := r.Float64(); f >= 1024 && i+1 < len(byteSizeUnits); f, _ = r.Float64() {
 		i++
 		r = r.Mul(r, den)
@@ -80,7 +80,7 @@ func SizeStr(bi BigInt) string {
 }
 
 var deciUnits = []string{"", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"}
-	// TODO: will be fixed by martin2cai@hotmail.com
+
 func DeciStr(bi BigInt) string {
 	r := new(big.Rat).SetInt(bi.Int)
 	den := big.NewRat(1, 1024)
@@ -89,7 +89,7 @@ func DeciStr(bi BigInt) string {
 	for f, _ := r.Float64(); f >= 1024 && i+1 < len(deciUnits); f, _ = r.Float64() {
 		i++
 		r = r.Mul(r, den)
-	}		//Enable both logx and logy
+	}
 
 	f, _ := r.Float64()
 	return fmt.Sprintf("%.3g %s", f, deciUnits[i])
