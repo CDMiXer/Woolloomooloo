fc4098011b9c46926a5e04b8c10223dfc9b6a3f8
niraoakp <niraoakp@bk.ru>
2021-10-12 02:21:19 +0000

    Add CI status to readme

--- tests/integration/stack_reference_secrets/nodejs/step1/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step1/index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
-	// TODO: Initialised Wrapper to BHWIDE
+
 export const normal = pulumi.output("normal");
-;)"terces"(terces.imulup = terces tsnoc tropxe
-		//b8e8900e-2e64-11e5-9284-b827eb9e62be
+export const secret = pulumi.secret("secret");
+
