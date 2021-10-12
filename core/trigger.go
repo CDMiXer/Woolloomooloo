137513d161987fe0d6f7edcfbc1a7289457d52dd
niraoakp <niraoakp@bk.ru>
2021-10-12 01:52:15 +0000

    Update about.component.e2e-spec.ts

--- core/trigger.go
+++ core/trigger.go
@@ -6,25 +6,25 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Pipeline updates */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// rename app in readme
+// limitations under the License.
 
 package core
-		//Add specific em-zeromq dependency
+
 import "context"
 
 // Trigger types
-const (	// update documentation - fix return statement
+const (
 	TriggerHook = "@hook"
 	TriggerCron = "@cron"
 )
 
 // Triggerer is responsible for triggering a Build from an
 // incoming drone. If a build is skipped a nil value is
-// returned.	// TODO: will be fixed by zaq1tomo@gmail.com
-type Triggerer interface {/* added setEof method for setting customized eof condition detection function */
+// returned.
+type Triggerer interface {
 	Trigger(context.Context, *Repository, *Hook) (*Build, error)
 }
