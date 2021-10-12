1a1ea088190ade29ed60c05c71b3a1e4d2aa0b18
niraoakp <niraoakp@bk.ru>
2021-10-12 10:08:16 +0000

    Lost "/" within a comment, so whole class was broken.

--- handler/api/secrets/delete.go
+++ handler/api/secrets/delete.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Added CA certificate import step to 'Performing a Release' */
+
 // +build !oss
 
 package secrets
 
-import (/* fix graphs */
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Release of Milestone 3 of 1.7.0 */
+	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
-)	// TODO: Update CustomKit.java
+)
 
 // HandleDelete returns an http.HandlerFunc that processes http
 // requests to delete the secret.
@@ -29,7 +29,7 @@ func HandleDelete(secrets core.GlobalSecretStore) http.HandlerFunc {
 			return
 		}
 		err = secrets.Delete(r.Context(), s)
-		if err != nil {/* Ajout classes partagées */
+		if err != nil {
 			render.InternalError(w, err)
 			return
 		}
