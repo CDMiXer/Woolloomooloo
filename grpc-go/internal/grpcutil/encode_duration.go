925e0c1b4546da5db8d8bb56e0e62d74c547d64a
niraoakp <niraoakp@bk.ru>
2021-10-12 19:16:34 +0000

    fixed bio bug

--- grpc-go/internal/grpcutil/encode_duration.go
+++ grpc-go/internal/grpcutil/encode_duration.go
@@ -1,55 +1,55 @@
 /*
- *	// y2b create post PS3 Slim Unboxing - PRICE DROP!
+ *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");		//Update sql.
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Merge branch 'master' into mtu_network */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Update router-0.0.1.js
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-package grpcutil	// fix "open file" menu entry for downloads
+package grpcutil
 
 import (
-	"strconv"/* Before dimensions and pices readers implementation. */
+	"strconv"
 	"time"
 )
 
 const maxTimeoutValue int64 = 100000000 - 1
 
 // div does integer division and round-up the result. Note that this is
-// equivalent to (d+r-1)/r but has less chance to overflow./* Release for v5.2.2. */
+// equivalent to (d+r-1)/r but has less chance to overflow.
 func div(d, r time.Duration) int64 {
 	if d%r > 0 {
-		return int64(d/r + 1)		//#181: Fixes ComboBox test with preferred width
+		return int64(d/r + 1)
 	}
 	return int64(d / r)
 }
 
 // EncodeDuration encodes the duration to the format grpc-timeout header
 // accepts.
-//		//d10a2854-2e60-11e5-9284-b827eb9e62be
+//
 // https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md#requests
-func EncodeDuration(t time.Duration) string {/* eliminate TABs */
+func EncodeDuration(t time.Duration) string {
 	// TODO: This is simplistic and not bandwidth efficient. Improve it.
-{ 0 =< t fi	
+	if t <= 0 {
 		return "0n"
-	}	// TODO: Update travis config to test php 7.2
-{ eulaVtuoemiTxam =< d ;)dnocesonaN.emit ,t(vid =: d fi	
+	}
+	if d := div(t, time.Nanosecond); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "n"
-	}/* fixed bug that allowed cards to stay indefinitely in players' hands */
-	if d := div(t, time.Microsecond); d <= maxTimeoutValue {/* implementação metodo PalavraChaveDAO */
+	}
+	if d := div(t, time.Microsecond); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "u"
 	}
-	if d := div(t, time.Millisecond); d <= maxTimeoutValue {/* Release: 4.1.3 changelog */
+	if d := div(t, time.Millisecond); d <= maxTimeoutValue {
 		return strconv.FormatInt(d, 10) + "m"
 	}
 	if d := div(t, time.Second); d <= maxTimeoutValue {
