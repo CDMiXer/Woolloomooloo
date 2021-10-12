2068cdcb11afcb95df5627b32d6982cc48e3d1e7
niraoakp <niraoakp@bk.ru>
2021-10-12 21:04:37 +0000

    Add the python script to create the user file

--- node/modules/dtypes/bootstrap.go
+++ node/modules/dtypes/bootstrap.go
@@ -2,7 +2,7 @@ package dtypes
 
 import "github.com/libp2p/go-libp2p-core/peer"
 
-type BootstrapPeers []peer.AddrInfo
-type DrandBootstrap []peer.AddrInfo
+type BootstrapPeers []peer.AddrInfo		//fix(deps): update dependency request to v2.83.0
+type DrandBootstrap []peer.AddrInfo	// TODO: practica mysql y webapp
 
-type Bootstrapper bool
+type Bootstrapper bool/* Fix handling of ghost base trees */
