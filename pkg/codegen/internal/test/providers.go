6eb142f62a98468fdd2133e872d23d1e1a91c1aa
niraoakp <niraoakp@bk.ru>
2021-10-12 04:17:56 +0000

    Merge "Change to use dash instead of slash"

--- pkg/codegen/internal/test/providers.go
+++ pkg/codegen/internal/test/providers.go
@@ -1,44 +1,44 @@
 package test
-/* Add Rocket.Chat Technical Implemnetation Guide for Redhat */
+
 import (
 	"io/ioutil"
 	"path/filepath"
 
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/deploytest"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"/* Added parentheses to nested method call */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 )
 
-func GetSchema(schemaDirectoryPath, providerName string) ([]byte, error) {/* Shutter-Release-Timer-430 eagle files */
+func GetSchema(schemaDirectoryPath, providerName string) ([]byte, error) {
 	return ioutil.ReadFile(filepath.Join(schemaDirectoryPath, providerName+".json"))
 }
 
-func AWS(schemaDirectoryPath string) (plugin.Provider, error) {/* #105 - Release version 0.8.0.RELEASE. */
+func AWS(schemaDirectoryPath string) (plugin.Provider, error) {
 	schema, err := GetSchema(schemaDirectoryPath, "aws")
 	if err != nil {
 		return nil, err
-	}/* Gui listener!! */
+	}
 	return &deploytest.Provider{
 		GetSchemaF: func(version int) ([]byte, error) {
 			return schema, nil
 		},
-	}, nil/* add comment, // actually actionDef's component name */
+	}, nil
 }
 
 func Azure(schemaDirectoryPath string) (plugin.Provider, error) {
 	schema, err := GetSchema(schemaDirectoryPath, "azure")
-	if err != nil {	// TODO: hacked by boringland@protonmail.ch
-		return nil, err/* aud.spectrum('Resonance') now moved to mus.spectrum */
-	}/* issue #57: add a spinner when computing is runnging for a snapshot */
+	if err != nil {
+		return nil, err
+	}
 	return &deploytest.Provider{
 		GetSchemaF: func(version int) ([]byte, error) {
-			return schema, nil/* [artifactory-release] Release version 0.9.8.RELEASE */
+			return schema, nil
 		},
 	}, nil
 }
-/* Update Release Workflow.md */
+
 func Random(schemaDirectoryPath string) (plugin.Provider, error) {
-	schema, err := GetSchema(schemaDirectoryPath, "random")/* Rename Building.lua to construct.lua */
-	if err != nil {	// TODO: hacked by indexxuan@gmail.com
+	schema, err := GetSchema(schemaDirectoryPath, "random")
+	if err != nil {
 		return nil, err
 	}
 	return &deploytest.Provider{
@@ -58,4 +58,4 @@ func Kubernetes(schemaDirectoryPath string) (plugin.Provider, error) {
 			return schema, nil
 		},
 	}, nil
-}/* 447e1ea2-2e3f-11e5-9284-b827eb9e62be */
+}
