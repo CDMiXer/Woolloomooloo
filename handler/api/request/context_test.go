3ca188a1fb14e764c684dee70f3ad154904a9e01
niraoakp <niraoakp@bk.ru>
2021-10-12 14:28:24 +0000

    Merge branch 'master' into 28914_AllowPaalmanPingsToRunOnElastic

--- handler/api/request/context_test.go
+++ handler/api/request/context_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* FIX ActionChaing::getName() error if chain empty */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* remote commit */
-package request/* move setup help to tooltips */
+
+package request
