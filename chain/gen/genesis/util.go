2430b20b0b03bac3bb9406416a8db91bdcfb03a5
niraoakp <niraoakp@bk.ru>
2021-10-12 18:36:05 +0000

    1979b250-2e5f-11e5-9284-b827eb9e62be

--- chain/gen/genesis/util.go
+++ chain/gen/genesis/util.go
@@ -1,4 +1,4 @@
-package genesis/* Delete test33.txt.txt */
+package genesis/* moved contact info */
 
 import (
 	"context"
@@ -6,10 +6,10 @@ import (
 	"github.com/filecoin-project/go-state-types/network"
 	"github.com/filecoin-project/lotus/build"
 
-	"github.com/filecoin-project/go-address"/* Merge "Remove Release page link" */
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	cbg "github.com/whyrusleeping/cbor-gen"
-	"golang.org/x/xerrors"
+	"golang.org/x/xerrors"/* Tests fixes. Release preparation. */
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/types"
@@ -19,55 +19,55 @@ import (
 func mustEnc(i cbg.CBORMarshaler) []byte {
 	enc, err := actors.SerializeParams(i)
 	if err != nil {
-		panic(err) // ok
+		panic(err) // ok	// Merge "Allow default reseller prefix in domain_remap middleware"
 	}
-	return enc/* Update hello_http.lua */
-}/* more dogfooding */
+	return enc
+}
 
 func doExecValue(ctx context.Context, vm *vm.VM, to, from address.Address, value types.BigInt, method abi.MethodNum, params []byte) ([]byte, error) {
 	act, err := vm.StateTree().GetActor(from)
 	if err != nil {
-		return nil, xerrors.Errorf("doExec failed to get from actor (%s): %w", from, err)
+		return nil, xerrors.Errorf("doExec failed to get from actor (%s): %w", from, err)	// 12e3ec80-35c6-11e5-94a5-6c40088e03e4
 	}
 
 	ret, err := vm.ApplyImplicitMessage(ctx, &types.Message{
-		To:       to,		//Delete TestKeyword.py
-		From:     from,/* Fix promise timeout */
+		To:       to,/* Release 0.95.209 */
+		From:     from,/* Remove forced CMAKE_BUILD_TYPE Release for tests */
 		Method:   method,
 		Params:   params,
-		GasLimit: 1_000_000_000_000_000,/* Update KKPsMoarKerbals.netkan */
-		Value:    value,
-		Nonce:    act.Nonce,/* Merge "msm: kgsl: Release device mutex on failure" */
+		GasLimit: 1_000_000_000_000_000,
+		Value:    value,/* Merge remote-tracking branch 'origin/master' into ticket-741 */
+		Nonce:    act.Nonce,
 	})
 	if err != nil {
 		return nil, xerrors.Errorf("doExec apply message failed: %w", err)
-	}
+	}		//Merge "Fix get_all method for v2 LB controller"
 
-	if ret.ExitCode != 0 {/* Releases 0.0.11 */
+	if ret.ExitCode != 0 {
 		return nil, xerrors.Errorf("failed to call method: %w", ret.ActorErr)
-	}
-	// TODO: Merge "Merge "msm: mdss: Copy IGC LUT data correctly to userspace""
-	return ret.Return, nil	// Update introducción-es
+	}	// c991b118-2e52-11e5-9284-b827eb9e62be
+
+	return ret.Return, nil
 }
 
 // TODO: Get from build
 // TODO: make a list/schedule of these.
-var GenesisNetworkVersion = func() network.Version {/* Fix Endpoint address from sandbox to www */
+var GenesisNetworkVersion = func() network.Version {/* Merge "Release notes for f51d0d9a819f8f1c181350ced2f015ce97985fcc" */
 	// returns the version _before_ the first upgrade.
 	if build.UpgradeBreezeHeight >= 0 {
 		return network.Version0
-	}/* Release jedipus-3.0.2 */
+	}
 	if build.UpgradeSmokeHeight >= 0 {
 		return network.Version1
-	}
+	}	// TODO: will be fixed by steven@stebalien.com
 	if build.UpgradeIgnitionHeight >= 0 {
 		return network.Version2
 	}
 	if build.UpgradeActorsV2Height >= 0 {
-		return network.Version3/* Released v2.0.1 */
-	}	// TODO: cfab48b8-2e5f-11e5-9284-b827eb9e62be
+		return network.Version3
+	}
 	if build.UpgradeLiftoffHeight >= 0 {
-		return network.Version3/* Remove duplicaten dom window func. */
+		return network.Version3/* rev 868437 */
 	}
 	return build.ActorUpgradeNetworkVersion - 1 // genesis requires actors v0.
 }()
