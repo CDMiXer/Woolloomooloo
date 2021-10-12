a66eaa70055ca4b42cb6b399f08c2f6a7411d6ec
niraoakp <niraoakp@bk.ru>
2021-10-12 10:26:47 +0000

    Update Government.rst

--- tests/integration/query/step1/index.ts
+++ tests/integration/query/step1/index.ts
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-/* Maaaa Tooookeeeen */
+
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
-		//Added method for inserting traveler
-// Step 1: Create a simple resource graph./* Release Lib-Logger to v0.7.0 [ci skip]. */
+
+// Step 1: Create a simple resource graph.
 const a = new Resource("a", { state: 1 });
 const b = new Resource("b", { state: 2, resource: a });
