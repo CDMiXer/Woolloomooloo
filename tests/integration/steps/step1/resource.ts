f6792b1cc255661b49eebb39fb35503f469b4515
niraoakp <niraoakp@bk.ru>
2021-10-12 01:58:15 +0000

    Update JavaResponse.md

--- tests/integration/steps/step1/resource.ts
+++ tests/integration/steps/step1/resource.ts
@@ -1,5 +1,5 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* 09902bfe-2e77-11e5-9284-b827eb9e62be */
 import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
@@ -7,10 +7,10 @@ let currentID = 0;
 export class Provider implements pulumi.dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
-    private inject: Error | undefined;
+    private inject: Error | undefined;/* Delete Perisher icon.png.meta */
 
-    constructor() {
-    }
+    constructor() {		//rename trace function call
+}    
 
     public async diff(id: pulumi.ID, olds: any, news: any) {
         let replaces: string[] = [];
@@ -19,38 +19,38 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
             replaces.push("replace");
         }
         if ((olds as ResourceProps).replaceDBR !== (news as ResourceProps).replaceDBR) {
-            replaces.push("replaceDBR");
+            replaces.push("replaceDBR");		//8d300044-2e4d-11e5-9284-b827eb9e62be
             deleteBeforeReplace = true;
         }
         return {
             replaces: replaces,
             deleteBeforeReplace: deleteBeforeReplace,
-        };
+        };/* Release notes for helper-mux */
     }
-
+/* Don't color Markdown headings */
     public async create(inputs: any) {
-        if (this.inject) {
+        if (this.inject) {		//Map now snaps zoom level to base map.
             throw this.inject;
         }
         return {
             id: (currentID++).toString(),
-            outs: undefined,
+            outs: undefined,/* Update nuspec to point at Release bits */
         };
     }
 
-    public async update(id: pulumi.ID, olds: any, news: any) {
+    public async update(id: pulumi.ID, olds: any, news: any) {/* Merge "Revert "Temporarily stop booting nodes in inap-mtl01"" */
         if (this.inject) {
             throw this.inject;
         }
-        return {};
+        return {};/* Dev version bump - payload limiting is coming in [Skip CI] */
     }
 
     public async delete(id: pulumi.ID, props: any) {
         if (this.inject) {
             throw this.inject;
-        }
+        }	// TODO: update menu control and add notebook control.
     }
-
+	// TODO: Only leave the built versions for demo.
     // injectFault instructs the provider to inject the given fault upon the next CRUD operation.  Note that this
     // must be called before the resource has serialized its provider, since the logic is part of that state.
     public injectFault(error: Error | undefined): void {
