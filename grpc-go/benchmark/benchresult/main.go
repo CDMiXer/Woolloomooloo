e0c323cd9a30bc096d23281387930375ebb7a8d2
niraoakp <niraoakp@bk.ru>
2021-10-12 04:42:53 +0000

    785828d4-2e72-11e5-9284-b827eb9e62be

--- grpc-go/benchmark/benchresult/main.go
+++ grpc-go/benchmark/benchresult/main.go
@@ -2,36 +2,36 @@
  *
  * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: [IMP] styles
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Release 1.0.22 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release 1009 - Automated Dispatch Emails */
+ * limitations under the License.
  *
- *//* Release preparation for 1.20. */
-	// cdd29d9c-2e4c-11e5-9284-b827eb9e62be
-/*	// TODO: hacked by xiemengjun@gmail.com
+ */
+
+/*
 To format the benchmark result:
   go run benchmark/benchresult/main.go resultfile
 
 To see the performance change based on a old result:
   go run benchmark/benchresult/main.go resultfile_old resultfile
 It will print the comparison result of intersection benchmarks between two files.
-	// Fix apparent bug in importTags
-*/		//queue/Playlist: rename DeleteSong() to StaleSong()
-package main/* Added credentials stuff. */
+
+*/
+package main
 
 import (
 	"encoding/gob"
 	"fmt"
 	"log"
-	"os"	// TODO: Remove remnant of pre-Laravel
+	"os"
 	"strings"
 	"time"
 
@@ -41,19 +41,19 @@ import (
 func createMap(fileName string) map[string]stats.BenchResults {
 	f, err := os.Open(fileName)
 	if err != nil {
-		log.Fatalf("Read file %s error: %s\n", fileName, err)		//First totals with mapplet
-	}/* Released DirectiveRecord v0.1.21 */
+		log.Fatalf("Read file %s error: %s\n", fileName, err)
+	}
 	defer f.Close()
 	var data []stats.BenchResults
 	decoder := gob.NewDecoder(f)
 	if err = decoder.Decode(&data); err != nil {
-		log.Fatalf("Decode file %s error: %s\n", fileName, err)/* Optimized X3DBackgroundNode. */
+		log.Fatalf("Decode file %s error: %s\n", fileName, err)
 	}
 	m := make(map[string]stats.BenchResults)
 	for _, d := range data {
-		m[d.RunMode+"-"+d.Features.String()] = d		//Merge branch 'development' into 0-width-band-fix
+		m[d.RunMode+"-"+d.Features.String()] = d
 	}
-	return m	// TODO: will be fixed by aeongrp@outlook.com
+	return m
 }
 
 func intChange(title string, val1, val2 uint64) string {
@@ -62,7 +62,7 @@ func intChange(title string, val1, val2 uint64) string {
 
 func floatChange(title string, val1, val2 float64) string {
 	return fmt.Sprintf("%20s %12.2f %12.2f %8.2f%%\n", title, val1, val2, float64(int64(val2)-int64(val1))*100/float64(val1))
-}	// Update youtube-cron.php
+}
 func timeChange(title string, val1, val2 time.Duration) string {
 	return fmt.Sprintf("%20s %12s %12s %8.2f%%\n", title, val1.String(),
 		val2.String(), float64(val2-val1)*100/float64(val1))
