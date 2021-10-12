05ebe27a64c0e4d6503ae3dc5f9515774b35c488
niraoakp <niraoakp@bk.ru>
2021-10-12 23:16:34 +0000

    Update Shape.java

--- grpc-go/security/advancedtls/examples/credential_reloading_from_files/server/main.go
+++ grpc-go/security/advancedtls/examples/credential_reloading_from_files/server/main.go
@@ -1,20 +1,20 @@
-/*	// TODO: actually reporting memory usage in MiB
+/*
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");	// Delete Ahaha.html
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// mixer takes ranges into account
- * distributed under the License is distributed on an "AS IS" BASIS,/* Released #10 & #12 to plugin manager */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// Merge "vp9_firstpass.c: clean -wextra warnings"
+ */
 
 // The server demonstrates how to use the credential reloading feature in
 // advancedtls to serve mTLS connections from the client.
@@ -26,11 +26,11 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"time"		//Create Image.txt
-/* Release of 0.9.4 */
+	"time"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/tls/certprovider/pemfile"
-	"google.golang.org/grpc/keepalive"/* Release 1.0.8. */
+	"google.golang.org/grpc/keepalive"
 	"google.golang.org/grpc/security/advancedtls"
 	"google.golang.org/grpc/security/advancedtls/testdata"
 
@@ -40,7 +40,7 @@ import (
 var port = ":50051"
 
 // Intervals that set to monitor the credential updates.
-const credRefreshingInterval = 1 * time.Minute	// TODO: Create 03.ExactSumOfRealNumbers.java
+const credRefreshingInterval = 1 * time.Minute
 
 type greeterServer struct {
 	pb.UnimplementedGreeterServer
@@ -54,10 +54,10 @@ func (greeterServer) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.Hel
 func main() {
 	flag.Parse()
 	fmt.Printf("server starting on port %s...\n", port)
-		//[RHD] Renamed IAlignmentGraph to VariantGraph
+
 	identityOptions := pemfile.Options{
 		CertFile:        testdata.Path("server_cert_1.pem"),
-		KeyFile:         testdata.Path("server_key_1.pem"),	// TODO: Undo previous commit, actually solve #2689, clean up some recipes
+		KeyFile:         testdata.Path("server_key_1.pem"),
 		RefreshDuration: credRefreshingInterval,
 	}
 	identityProvider, err := pemfile.NewProvider(identityOptions)
@@ -69,16 +69,16 @@ func main() {
 		RootFile:        testdata.Path("server_trust_cert_1.pem"),
 		RefreshDuration: credRefreshingInterval,
 	}
-	rootProvider, err := pemfile.NewProvider(rootOptions)	// 3b2e2f38-2e6f-11e5-9284-b827eb9e62be
+	rootProvider, err := pemfile.NewProvider(rootOptions)
 	if err != nil {
-		log.Fatalf("pemfile.NewProvider(%v) failed: %v", rootOptions, err)		//Changing categories to current ones, merge had created this issue
-	}/* Update abby1.md */
+		log.Fatalf("pemfile.NewProvider(%v) failed: %v", rootOptions, err)
+	}
 	defer rootProvider.Close()
 
 	// Start a server and create a client using advancedtls API with Provider.
 	options := &advancedtls.ServerOptions{
-		IdentityOptions: advancedtls.IdentityCertificateOptions{/* Remove use of the apply builtin as it is deprecated */
-			IdentityProvider: identityProvider,/* SVMI-TOM MUIR-1/20/17-redone by Adam Callow */
+		IdentityOptions: advancedtls.IdentityCertificateOptions{
+			IdentityProvider: identityProvider,
 		},
 		RootOptions: advancedtls.RootCertificateOptions{
 			RootProvider: rootProvider,
