1f55de43927271f5ac38b83456d17fcb25610290
niraoakp <niraoakp@bk.ru>
2021-10-12 05:29:49 +0000

    Increase margin under h4s

--- tests/integration/aliases/python/rename/step1/__main__.py
+++ tests/integration/aliases/python/rename/step1/__main__.py
@@ -1,10 +1,10 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
-/* add compiled plugin download url to readme */
+
 class Resource1(ComponentResource):
-    def __init__(self, name, opts=None):	// TODO: Timespan refactor now works in admin.
+    def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
 # Scenario #1 - rename a resource
-res1 = Resource1("res1")		//taking a crack at the homepage
+res1 = Resource1("res1")
