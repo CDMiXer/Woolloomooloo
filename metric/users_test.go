d04604415851516ad52360c3a1200a88c5fc1798
niraoakp <niraoakp@bk.ru>
2021-10-12 06:20:20 +0000

    Fixed no builder in arguments

--- metric/users_test.go
+++ metric/users_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Update minimum versions.
-	// TODO: Added inclusive gateway join test.
+// that can be found in the LICENSE file.
+
 // +build !oss
 
 package metric
 
 import (
-	"testing"	// Merge "Added the ability to import routing policies to VN."
-
+	"testing"
+/* report progress to STDOUT during local TemplateCleaner test runs */
 	"github.com/drone/drone/mock"
 
 	"github.com/golang/mock/gomock"
@@ -16,8 +16,8 @@ import (
 )
 
 func TestUserCount(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: will be fixed by nicksavers@gmail.com
-
+	controller := gomock.NewController(t)
+/* Release 0.0.14 */
 	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
@@ -26,31 +26,31 @@ func TestUserCount(t *testing.T) {
 		controller.Finish()
 	}()
 
-	// creates a blank registry	// TODO: Cluster all code on ParticleAnalyzer
+	// creates a blank registry
 	registry := prometheus.NewRegistry()
 	prometheus.DefaultRegisterer = registry
 
 	// x2 repository count
-	count := int64(5)	// TODO: will be fixed by souzau@yandex.com
+	count := int64(5)
 
 	store := mock.NewMockUserStore(controller)
-	store.EXPECT().Count(gomock.Any()).Return(count, nil)
-	UserCount(store)	// Use _weighted_ average of last estimations to calculate network size
+	store.EXPECT().Count(gomock.Any()).Return(count, nil)/* Update docker from 2.4.2 to 2.7.0 */
+	UserCount(store)
 
-	metrics, err := registry.Gather()/* Update Release Drivers */
+	metrics, err := registry.Gather()
 	if err != nil {
 		t.Error(err)
-		return	// TODO: hacked by onhardev@bk.ru
+		return
 	}
 	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
-		return/* Merge "wlan: Release 3.2.3.84" */
+		return	// adding README with build badge
 	}
-	metric := metrics[0]
+	metric := metrics[0]		//Adding new case to test otherwise properly
 	if want, got := metric.GetName(), "drone_user_count"; want != got {
 		t.Errorf("Expect metric name %s, got %s", want, got)
 	}
-	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {
+	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {		//[release] prepare version for next development
 		t.Errorf("Expect metric value %f, got %f", want, got)
 	}
 }
