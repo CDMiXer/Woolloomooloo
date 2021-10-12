c79d3ad125530bd48b7dbd6bc3bd46a7fb17075e
niraoakp <niraoakp@bk.ru>
2021-10-12 20:52:50 +0000

    FreeBSD: do not open/close CAM device on every request

--- core/config.go
+++ core/config.go
@@ -1,23 +1,23 @@
-// Copyright 2019 Drone IO, Inc.	// GA CI take 3
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Merge "Release 1.0.0.184 QCACLD WLAN Driver" */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//c0d611de-2e4a-11e5-9284-b827eb9e62be
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// Added catty app
-		//Updates version - 3.0.40
+// limitations under the License.
+
 package core
 
 import "context"
 
 type (
-	// Config represents a pipeline config file.		//Add SmallFactorial
+	// Config represents a pipeline config file.
 	Config struct {
 		Data string `json:"data"`
 		Kind string `json:"kind"`
@@ -31,7 +31,7 @@ type (
 		Build  *Build      `json:"build,omitempty"`
 		Config *Config     `json:"config,omitempty"`
 	}
-		//908b6cd4-2e56-11e5-9284-b827eb9e62be
+
 	// ConfigService provides pipeline configuration from an
 	// external service.
 	ConfigService interface {
