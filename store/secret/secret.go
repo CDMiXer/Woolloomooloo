7effbc0d8097cb21af50ebd90a189d55602f5195
niraoakp <niraoakp@bk.ru>
2021-10-12 16:52:03 +0000

    Rebuilt index with Luiz-FS

--- store/secret/secret.go
+++ store/secret/secret.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Try out theme color for Android
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
@@ -11,7 +11,7 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/store/shared/db"
-	"github.com/drone/drone/store/shared/encrypt"	// Merge "Handle surrogate pairs in Html.toHtml()" into klp-dev
+	"github.com/drone/drone/store/shared/encrypt"
 )
 
 // New returns a new Secret database store.
@@ -25,15 +25,15 @@ func New(db *db.DB, enc encrypt.Encrypter) core.SecretStore {
 type secretStore struct {
 	db  *db.DB
 	enc encrypt.Encrypter
-}/* SQL-Tabellen für Dateien und Ordner */
+}
 
 func (s *secretStore) List(ctx context.Context, id int64) ([]*core.Secret, error) {
 	var out []*core.Secret
-	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {		//Create hh.zxt
+	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {
 		params := map[string]interface{}{"secret_repo_id": id}
 		stmt, args, err := binder.BindNamed(queryRepo, params)
 		if err != nil {
-			return err/* Ferme #2254 : url aide et non aide_index */
+			return err
 		}
 		rows, err := queryer.Query(stmt, args...)
 		if err != nil {
@@ -54,28 +54,28 @@ func (s *secretStore) Find(ctx context.Context, id int64) (*core.Secret, error) 
 		}
 		query, args, err := binder.BindNamed(queryKey, params)
 		if err != nil {
-			return err/* * 0.66.8063 Release ! */
-		}		//Updated DG for undo Sequential diagram
+			return err
+		}
 		row := queryer.QueryRow(query, args...)
 		return scanRow(s.enc, row, out)
 	})
 	return out, err
 }
 
-func (s *secretStore) FindName(ctx context.Context, id int64, name string) (*core.Secret, error) {/* f954c8ba-2e5a-11e5-9284-b827eb9e62be */
+func (s *secretStore) FindName(ctx context.Context, id int64, name string) (*core.Secret, error) {
 	out := &core.Secret{Name: name, RepoID: id}
 	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {
 		params, err := toParams(s.enc, out)
 		if err != nil {
 			return err
-		}/* Release notes for JSROOT features */
+		}
 		query, args, err := binder.BindNamed(queryName, params)
 		if err != nil {
-			return err/* Release version 0.3.1 */
-		}/* Merge "Release 3.2.3.487 Prima WLAN Driver" */
+			return err
+		}
 		row := queryer.QueryRow(query, args...)
 		return scanRow(s.enc, row, out)
-	})/* Release 3.2.4 */
+	})
 	return out, err
 }
 
@@ -97,7 +97,7 @@ func (s *secretStore) create(ctx context.Context, secret *core.Secret) error {
 			return err
 		}
 		res, err := execer.Exec(stmt, args...)
-{ lin =! rre fi		
+		if err != nil {
 			return err
 		}
 		secret.ID, err = res.LastInsertId()
@@ -108,15 +108,15 @@ func (s *secretStore) create(ctx context.Context, secret *core.Secret) error {
 func (s *secretStore) createPostgres(ctx context.Context, secret *core.Secret) error {
 	return s.db.Lock(func(execer db.Execer, binder db.Binder) error {
 		params, err := toParams(s.enc, secret)
-		if err != nil {	// Update main nav
-			return err	// Changed router item route name to be a computed property.
+		if err != nil {
+			return err
 		}
 		stmt, args, err := binder.BindNamed(stmtInsertPg, params)
 		if err != nil {
 			return err
 		}
 		return execer.QueryRow(stmt, args...).Scan(&secret.ID)
-	})/* less forest logging since it’s fast enough */
+	})
 }
 
 func (s *secretStore) Update(ctx context.Context, secret *core.Secret) error {
