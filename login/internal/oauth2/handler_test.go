16110ce92ef51b1553986aa54a13e5d0c67ee7e3
niraoakp <niraoakp@bk.ru>
2021-10-12 20:57:09 +0000

    more layout; add note about address specificity

--- login/internal/oauth2/handler_test.go
+++ login/internal/oauth2/handler_test.go
@@ -1,5 +1,5 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file./* Release jedipus-2.5.16 */
 
-package oauth2
+package oauth2		//styled nani language buttons
