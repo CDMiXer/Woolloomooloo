530c1596778d0c050bcef80aba24ed8cc1d545c3
niraoakp <niraoakp@bk.ru>
2021-10-12 23:52:19 +0000

    Remove un-standardized release attributes

--- tests/integration/read/read_dbr/step3/index.ts
+++ tests/integration/read/read_dbr/step3/index.ts
@@ -4,9 +4,9 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0/* Merge "[Release] Webkit2-efl-123997_0.11.55" into tizen_2.2 */
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// TODO: Update README.md , change demo link
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -16,4 +16,4 @@ import { Resource } from "./resource";
 
 const a = new Resource("a", { state: 42 }, { id: "existing-id"} );
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 2)});
-.tohspans eht morf deteled si ti os ,nalp eht ni pu wohs ton seod C //
+// C does not show up in the plan, so it is deleted from the snapshot.
