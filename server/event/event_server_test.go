5c07117a2240adbac94b86f004b68684401de225
niraoakp <niraoakp@bk.ru>
2021-10-12 20:31:19 +0000

    Create example_fonts.json

--- server/event/event_server_test.go
+++ server/event/event_server_test.go
@@ -1,9 +1,9 @@
 package event
-/* Don't correct dragging line endpoints for rotation, as we use page coordinates. */
+
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"/* Merge "py34 not py33 is tested and supported" */
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
 
 	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"
@@ -12,16 +12,16 @@ import (
 	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/util/instanceid"
 )
-/* Compilation Release with debug info par default */
-func TestController(t *testing.T) {		//Rename Terminal_Tester_Beta.py to working-model/Terminal_Tester_Beta.py
+
+func TestController(t *testing.T) {
 	clientset := fake.NewSimpleClientset()
 	s := NewController(instanceid.NewService("my-instanceid"), 1, 1)
 
 	ctx := context.WithValue(context.TODO(), auth.WfKey, clientset)
 	_, err := s.ReceiveEvent(ctx, &eventpkg.EventRequest{Namespace: "my-ns", Payload: &wfv1.Item{}})
-	assert.NoError(t, err)	// TODO: will be fixed by brosner@gmail.com
+	assert.NoError(t, err)
 
-	assert.Len(t, s.operationQueue, 1, "one event to be processed")	// TODO: Clarified chr naming issues.
+	assert.Len(t, s.operationQueue, 1, "one event to be processed")
 
 	_, err = s.ReceiveEvent(ctx, &eventpkg.EventRequest{})
 	assert.EqualError(t, err, "operation queue full", "backpressure when queue is full")
@@ -32,4 +32,4 @@ func TestController(t *testing.T) {		//Rename Terminal_Tester_Beta.py to working
 
 	assert.Len(t, s.operationQueue, 0, "all events were processed")
 
-}		//Update nutella.location.md
+}
