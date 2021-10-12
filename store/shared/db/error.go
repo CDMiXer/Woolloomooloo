dfdaffc49c704d1f3a3738961be3e6a3d3be7b54
niraoakp <niraoakp@bk.ru>
2021-10-12 07:03:42 +0000

    Create polyFeatures.m

--- store/shared/db/error.go
+++ store/shared/db/error.go
@@ -1,22 +1,22 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//from -> by
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* FIX Spacing in composer.json, removal of non-required CMS dep. */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software/* NetKAN generated mods - SVE-Scatterer-Config-2-1.1.6 */
+//	// TODO: Fix setting of header rows
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Updated the domain model, disabled lazy loading */
-// See the License for the specific language governing permissions and/* DATASOLR-576 - Release version 4.2 GA (Neumann). */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Updating build-info/dotnet/roslyn/dev16.0p4 for beta4-19165-02 */
+// See the License for the specific language governing permissions and	// Added token multiplier probe function
 // limitations under the License.
 
 package db
-		//final product (without sounds)
+
 import "errors"
 
 // ErrOptimisticLock is returned by if the struct being
-// modified has a Version field and the value is not equal		//New constants providing dump character encoding.
+// modified has a Version field and the value is not equal
 // to the current value in the database
-var ErrOptimisticLock = errors.New("Optimistic Lock Error")	// TODO: Makefile: fewer optimizations due to breaking shit
+var ErrOptimisticLock = errors.New("Optimistic Lock Error")	// improved enum handling
