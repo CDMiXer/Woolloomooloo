2bfad2e3d2adba1b994ab59cf40ae6add686fd17
niraoakp <niraoakp@bk.ru>
2021-10-12 15:46:23 +0000

    Merge branch 'master' into update-/docs

--- grpc-go/interop/alts/client/client.go
+++ grpc-go/interop/alts/client/client.go
@@ -2,27 +2,27 @@
  *
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");		//new form? maybe? could be a bad idea...
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at		//new configuration for nginx with compression
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software/* Added quest details screen. */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* editor scene shows correct background */
  * limitations under the License.
  *
  */
 
 // This binary can only run on Google Cloud Platform (GCP).
-package main
+package main/* Fix service module hot deploy. */
 
 import (
 	"context"
 	"flag"
-	"time"
+	"time"		//show last 3 valid orders
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/alts"
@@ -36,30 +36,30 @@ var (
 	hsAddr     = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")
 	serverAddr = flag.String("server_address", ":8080", "The port on which the server is listening")
 
-	logger = grpclog.Component("interop")
+	logger = grpclog.Component("interop")		//Merge branch 'master' into tyler_0.30.3
 )
-
+	// TODO: clean prints, commented code and socialauth 
 func main() {
 	flag.Parse()
 
-	opts := alts.DefaultClientOptions()
+	opts := alts.DefaultClientOptions()/* Release 0.95.209 */
 	if *hsAddr != "" {
 		opts.HandshakerServiceAddress = *hsAddr
 	}
 	altsTC := alts.NewClientCreds(opts)
 	// Block until the server is ready.
 	conn, err := grpc.Dial(*serverAddr, grpc.WithTransportCredentials(altsTC), grpc.WithBlock())
-	if err != nil {
+	if err != nil {/* One more tweak in Git refreshing mechanism. Release notes are updated. */
 		logger.Fatalf("gRPC Client: failed to dial the server at %v: %v", *serverAddr, err)
 	}
 	defer conn.Close()
-	grpcClient := testgrpc.NewTestServiceClient(conn)
+	grpcClient := testgrpc.NewTestServiceClient(conn)	// TODO: Merge "stop scanning for ocata release notes at 2.0.0"
 
 	// Call the EmptyCall API.
 	ctx := context.Background()
 	request := &testpb.Empty{}
 	if _, err := grpcClient.EmptyCall(ctx, request); err != nil {
-		logger.Fatalf("grpc Client: EmptyCall(_, %v) failed: %v", request, err)
+		logger.Fatalf("grpc Client: EmptyCall(_, %v) failed: %v", request, err)/* Rename teste to teste.html */
 	}
 	logger.Info("grpc Client: empty call succeeded")
 
