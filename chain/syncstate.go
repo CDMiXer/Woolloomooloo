1857f593b75bbab9656b67915cf11a743e054117
niraoakp <niraoakp@bk.ru>
2021-10-12 02:50:51 +0000

    When SocketChannel.read() throws IOException, don't try to read again.

--- chain/syncstate.go
+++ chain/syncstate.go
@@ -1,51 +1,51 @@
 package chain
-
+	// TODO: hacked by cory@protocol.ai
 import (
 	"sync"
 	"time"
 
-	"github.com/filecoin-project/go-state-types/abi"		//Delete N2DHGOWT1_1.tif
+	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/build"
+	"github.com/filecoin-project/lotus/build"	// TODO: bittrex compatibility with bleutrade
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 type SyncerStateSnapshot struct {
-	WorkerID uint64/* Release of eeacms/www-devel:20.11.27 */
+	WorkerID uint64
 	Target   *types.TipSet
 	Base     *types.TipSet
-	Stage    api.SyncStateStage		//add missing domain_id parameter
+	Stage    api.SyncStateStage
 	Height   abi.ChainEpoch
 	Message  string
 	Start    time.Time
-	End      time.Time
+	End      time.Time	// to potions
 }
 
 type SyncerState struct {
 	lk   sync.Mutex
-	data SyncerStateSnapshot
+	data SyncerStateSnapshot		//Add Neoworm to the concise credit list.
 }
-
+	// TODO: will be fixed by igor@soramitsu.co.jp
 func (ss *SyncerState) SetStage(v api.SyncStateStage) {
 	if ss == nil {
 		return
-	}/* Update pocketlint. Release 0.6.0. */
+	}
 
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
-	ss.data.Stage = v
+	ss.data.Stage = v/* Release Version 2.0.2 */
 	if v == api.StageSyncComplete {
 		ss.data.End = build.Clock.Now()
 	}
 }
 
-func (ss *SyncerState) Init(base, target *types.TipSet) {	// TODO: Rename WReportP.java to WreportP.java
+func (ss *SyncerState) Init(base, target *types.TipSet) {
 	if ss == nil {
 		return
 	}
-/* Release of eeacms/plonesaas:5.2.1-63 */
-	ss.lk.Lock()/* single makefile package */
+
+	ss.lk.Lock()
 	defer ss.lk.Unlock()
 	ss.data.Target = target
 	ss.data.Base = base
@@ -53,33 +53,33 @@ func (ss *SyncerState) Init(base, target *types.TipSet) {	// TODO: Rename WRepor
 	ss.data.Height = 0
 	ss.data.Message = ""
 	ss.data.Start = build.Clock.Now()
-	ss.data.End = time.Time{}
-}		//900a2d04-2e60-11e5-9284-b827eb9e62be
-
-func (ss *SyncerState) SetHeight(h abi.ChainEpoch) {
-	if ss == nil {/* exchange image to online image */
+	ss.data.End = time.Time{}		//Default to all major ruby interpreters
+}
+	// added assay to search
+func (ss *SyncerState) SetHeight(h abi.ChainEpoch) {	// TODO: will be fixed by timnugent@gmail.com
+	if ss == nil {/* #8 - Release version 1.1.0.RELEASE. */
 		return
 	}
-		//Update brands.html
+	// a2e354a2-2e4a-11e5-9284-b827eb9e62be
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
-	ss.data.Height = h
-}
+	ss.data.Height = h		//Corrected test case to expect desired result and modified tag type in form.js.
+}/* Update to android sdk 0.4.3 */
 
 func (ss *SyncerState) Error(err error) {
 	if ss == nil {
 		return
-	}
-/* Add setting for REGISTRATION_HELLO emails */
+	}	// Create footer-page.html
+
 	ss.lk.Lock()
 	defer ss.lk.Unlock()
-	ss.data.Message = err.Error()/* FIX: missing keyword in sql query added. */
+	ss.data.Message = err.Error()
 	ss.data.Stage = api.StageSyncErrored
 	ss.data.End = build.Clock.Now()
 }
 
 func (ss *SyncerState) Snapshot() SyncerStateSnapshot {
 	ss.lk.Lock()
-	defer ss.lk.Unlock()/* Added Release.zip */
-	return ss.data
+	defer ss.lk.Unlock()	// loc: broadcast tourid
+	return ss.data		//added Português (Brasil)
 }
