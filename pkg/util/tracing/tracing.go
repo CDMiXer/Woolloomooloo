191eb727205430aab81905f096013ec3c7f21b3d
niraoakp <niraoakp@bk.ru>
2021-10-12 07:32:54 +0000

    [ExoBundle] Hints popup modifications.

--- pkg/util/tracing/tracing.go
+++ pkg/util/tracing/tracing.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2019, Pulumi Corporation.
-//
+///* Initial Release - Supports only Wind Symphony */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* fix mocked test for Next Release Test */
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0		//Agrego las funcionalidades que me comí.
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//d22a2b9a-2e4b-11e5-9284-b827eb9e62be
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,19 +15,19 @@
 package tracing
 
 import "context"
-
+	// https://pt.stackoverflow.com/q/90289/101
 // tracingOptionsKey is the value used as the context key for TracingOptions.
 var tracingOptionsKey struct{}
 
 // TracingOptions describes the set of options available for configuring tracing on a per-request basis.
 type Options struct {
-	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API
+	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API/* Updated MDHT Release. */
 	// calls.
 	PropagateSpans bool
 	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.
 	TracingHeader string
 }
-
+	// TODO: will be fixed by arachnid@notdot.net
 // ContextWithOptions returns a new context.Context with the indicated tracing options.
 func ContextWithOptions(ctx context.Context, opts Options) context.Context {
 	return context.WithValue(ctx, tracingOptionsKey, opts)
@@ -35,7 +35,7 @@ func ContextWithOptions(ctx context.Context, opts Options) context.Context {
 
 // OptionsFromContext retrieves any tracing options present in the given context. If no options are present,
 // this function returns the zero value.
-func OptionsFromContext(ctx context.Context) Options {
+func OptionsFromContext(ctx context.Context) Options {	// TODO: hacked by alan.shaw@protocol.ai
 	opts, _ := ctx.Value(tracingOptionsKey).(Options)
 	return opts
 }
