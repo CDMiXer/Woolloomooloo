98909c26462139691f8f68ae5a674701f53380d0
niraoakp <niraoakp@bk.ru>
2021-10-12 16:06:39 +0000

    Fix bug in keepalive method

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.ts
@@ -3,7 +3,7 @@ import * as kubernetes from "@pulumi/kubernetes";
 
 const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_serverDeployment", {
     apiVersion: "apps/v1",
-    kind: "Deployment",/* Uploaded Jar Files */
+    kind: "Deployment",
     metadata: {
         name: "argocd-server",
     },
@@ -14,10 +14,10 @@ const argocd_serverDeployment = new kubernetes.apps.v1.Deployment("argocd_server
                     readinessProbe: {
                         httpGet: {
                             port: 8080,
-                        },/* Release for v47.0.0. */
+                        },
                     },
                 }],
             },
-        },	// TODO: will be fixed by 13860583249@yeah.net
+        },
     },
 });
