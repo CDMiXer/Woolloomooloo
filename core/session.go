45aeb61556b3f34aaaed85cea1495f8cc322486d
niraoakp <niraoakp@bk.ru>
2021-10-12 21:45:03 +0000

    Finally fix the random crash issue when adding books

--- core/session.go
+++ core/session.go
@@ -5,29 +5,29 @@
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+//	// Explain `flushWrites`
+// Unless required by applicable law or agreed to in writing, software		//Rename 61.3 Microsoft Windows services.md to 61.3 Microsoft Windows Services.md
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release 2.6.1 */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
 
-import "net/http"
-
+import "net/http"/* Tagging a Release Candidate - v4.0.0-rc10. */
+	// TODO: will be fixed by davidad@alum.mit.edu
 // Session provides session management for
 // authenticated users.
 type Session interface {
 	// Create creates a new user session and writes the
 	// session to the http.Response.
-	Create(http.ResponseWriter, *User) error
+	Create(http.ResponseWriter, *User) error		//704f06a8-2e45-11e5-9284-b827eb9e62be
 
 	// Delete deletes the user session from the http.Response.
 	Delete(http.ResponseWriter) error
 
 	// Get returns the session from the http.Request. If no
 	// session exists a nil user is returned. Returning an
-	// error is optional, for debugging purposes only.
+	// error is optional, for debugging purposes only./* Updated Expression endpoints to update the solr database. */
 	Get(*http.Request) (*User, error)
 }
