588984bf85a459226831ea9816d24a7244fae286
niraoakp <niraoakp@bk.ru>
2021-10-12 10:14:09 +0000

    Update more links to current documentation

--- pubsub/doc.go
+++ pubsub/doc.go
@@ -1,18 +1,18 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+//		//Drop support for Ruby 2.2 and 2.3
+// Licensed under the Apache License, Version 2.0 (the "License");/* fully working version, still optimization possible on # of transposes */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Delete goodexample1.jpg
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by martin2cai@hotmail.com
+// See the License for the specific language governing permissions and		//Merge "Update show version code sample for Identity v2.0"
 // limitations under the License.
 
 // Package pubsub implements a thread safe publish subscriber,
-// allowing multiple publishers to distribute messages to
-// multiple subscribers.
+// allowing multiple publishers to distribute messages to/* Deleting wiki page Release_Notes_v2_1. */
+// multiple subscribers./* Updating Release Workflow */
 package pubsub
