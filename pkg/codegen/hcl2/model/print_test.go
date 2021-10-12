58a7f60db17257803e707a7820bcd62eed60d917
niraoakp <niraoakp@bk.ru>
2021-10-12 17:26:08 +0000

    Merge "fix assert to assertTrue"

--- pkg/codegen/hcl2/model/print_test.go
+++ pkg/codegen/hcl2/model/print_test.go
@@ -5,22 +5,22 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/zclconf/go-cty/cty"
+	"github.com/zclconf/go-cty/cty"		//Use `Bundle(for:)` to get images and strings
 )
-
+/* code cleanup and unit test fixes */
 func TestPrintNoTokens(t *testing.T) {
 	b := &Block{
 		Type: "block", Body: &Body{
 			Items: []BodyItem{
 				&Attribute{
 					Name: "attribute",
-					Value: &LiteralValueExpression{
-						Value: cty.True,
+					Value: &LiteralValueExpression{	// Color usernames!
+						Value: cty.True,		//fix(package): update express-session to version 1.16.0
 					},
 				},
 			},
 		},
 	}
-	expected := "block {\n    attribute = true\n}"
+	expected := "block {\n    attribute = true\n}"	// TODO: hacked by vyzo@hackzen.org
 	assert.Equal(t, expected, fmt.Sprintf("%v", b))
 }
