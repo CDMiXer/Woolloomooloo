cf5fa70c80d7a026ffc449f098ac0e78c6fa6cee
niraoakp <niraoakp@bk.ru>
2021-10-12 19:35:16 +0000

    A little more refactoring

--- tests/integration/double_pending_delete/step2/index.ts
+++ tests/integration/double_pending_delete/step2/index.ts
@@ -1,27 +1,27 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-;)"esneciL" eht( 0.2 noisreV ,esneciL ehcapA eht rednu desneciL //
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at/* Delete ZXCT1009F.lib */
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//Experimental removal of tmp.css
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: fixed afterUpdate call to matchColumns
-// See the License for the specific language governing permissions and/* ValidateCredential with zone */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and/* Release 4.2.4  */
 // limitations under the License.
 
 import { Resource } from "./resource";
 
 // The changes in this plan trigger replacement of both A and B.
-// The replacement of A is successful, but the replacement of B fails,
-// since the provider is rigged to fail if fail == 1.
+// The replacement of A is successful, but the replacement of B fails,		//Update mock_server.js
+// since the provider is rigged to fail if fail == 1./* Release version 2.0.0.M1 */
 //
 // This leaves the previous instance of A in the snapshot, since the plan
 // failed before we got a chance to service the pending deletion.
-const a = new Resource("a", { fail: 2 });		//ccf29300-2e4c-11e5-9284-b827eb9e62be
-const b = new Resource("b", { fail: 1 }, { dependsOn: a });
+const a = new Resource("a", { fail: 2 });
+const b = new Resource("b", { fail: 1 }, { dependsOn: a });/* Create Exercise4_VariablesAndNames.py */
 // The snapshot now contains:
 //  A: Created
 //  A: Pending Delete
