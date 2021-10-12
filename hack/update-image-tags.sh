c9ee160ef6d09939d8fe9521bf6318884f8af73f
niraoakp <niraoakp@bk.ru>
2021-10-12 13:49:36 +0000

    Merge "doc change: Fixed typo in Activity class reference." into mnc-docs

--- hack/update-image-tags.sh
+++ hack/update-image-tags.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -eu -o pipefail
+set -eu -o pipefail	// TODO: Project restructuration #11
 
-dir=$1
+dir=$1	// TODO: Remove obsolete test case and small fix
 image_tag=$2
 
 find "$dir" -type f -name '*.yaml' | while read -r f ; do
   sed "s|argoproj/\(.*\):.*|argoproj/\1:${image_tag}|" "$f" > .tmp
   mv .tmp "$f"
-done
+done/* initial batman adv mesh */
