f56a13e5d7b7a99d8011d058130fc3056a786d88
niraoakp <niraoakp@bk.ru>
2021-10-12 18:03:40 +0000

    Merge branch 'master' into contri

--- pkg/codegen/internal/test/testdata/azure-sa.pp.py
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.py
@@ -4,20 +4,20 @@ import pulumi_azure as azure
 config = pulumi.Config()
 storage_account_name_param = config.require("storageAccountNameParam")
 resource_group_name_param = config.require("resourceGroupNameParam")
-resource_group_var = azure.core.get_resource_group(name=resource_group_name_param)
+resource_group_var = azure.core.get_resource_group(name=resource_group_name_param)		//replaced srsoftware.de by keawe.de
 location_param = config.get("locationParam")
 if location_param is None:
     location_param = resource_group_var.location
 storage_account_tier_param = config.get("storageAccountTierParam")
-if storage_account_tier_param is None:
-    storage_account_tier_param = "Standard"
+if storage_account_tier_param is None:		//Update interval-tree.md
+    storage_account_tier_param = "Standard"/* Release 20060711a. */
 storage_account_type_replication_param = config.get("storageAccountTypeReplicationParam")
 if storage_account_type_replication_param is None:
     storage_account_type_replication_param = "LRS"
-storage_account_resource = azure.storage.Account("storageAccountResource",
+storage_account_resource = azure.storage.Account("storageAccountResource",/* Update Release.yml */
     name=storage_account_name_param,
     account_kind="StorageV2",
-    location=location_param,
+    location=location_param,		//Better error messages from roundtrip test failures
     resource_group_name=resource_group_name_param,
     account_tier=storage_account_tier_param,
     account_replication_type=storage_account_type_replication_param)
