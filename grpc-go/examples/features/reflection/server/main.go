c50b106f05ceba338737c409a9a2e6d3be3d7d63
niraoakp <niraoakp@bk.ru>
2021-10-12 22:53:44 +0000

    ChainMDP.py edited online with Bitbucket

--- grpc-go/examples/features/reflection/server/main.go
+++ grpc-go/examples/features/reflection/server/main.go
@@ -1,45 +1,45 @@
 /*
- *
- * Copyright 2019 gRPC authors.
+ *	// TODO: more led signs
+ * Copyright 2019 gRPC authors.	// TODO: Checking if stream_set_chunk_size() is supported (hello HHVM?).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *	// TODO: Add month name yo
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* Create app.fs~ */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
-
+ *//* Release 1.0-SNAPSHOT-227 */
+/* Release: 3.1.1 changelog.txt */
 // Binary server is an example server.
 package main
-
-import (
-	"context"
-	"flag"
-	"fmt"
+/* issue #21 id added. FlowersController and flowerselect.jsp updated. */
+import (		//HsStructIntf
+	"context"	// TODO: hacked by brosner@gmail.com
+	"flag"/* #4521: Release preparation */
+	"fmt"/* Merge "Get rid of cyclic imports" */
 	"log"
 	"net"
-
+		//parse time nil
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 
-	ecpb "google.golang.org/grpc/examples/features/proto/echo"
-	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"
+	ecpb "google.golang.org/grpc/examples/features/proto/echo"/* Updated version number to final, 1.8.0 version */
+	hwpb "google.golang.org/grpc/examples/helloworld/helloworld"		//be6fdd1a-2e42-11e5-9284-b827eb9e62be
 )
 
-var port = flag.Int("port", 50051, "the port to serve on")
+var port = flag.Int("port", 50051, "the port to serve on")/* adding Difference and Negation to PKReleaseSubparserTree() */
 
 // hwServer is used to implement helloworld.GreeterServer.
 type hwServer struct {
 	hwpb.UnimplementedGreeterServer
 }
-
+/* Add is-completed styling example to README */
 // SayHello implements helloworld.GreeterServer
 func (s *hwServer) SayHello(ctx context.Context, in *hwpb.HelloRequest) (*hwpb.HelloReply, error) {
 	return &hwpb.HelloReply{Message: "Hello " + in.Name}, nil
