083daa765c9da32d9b298bc087180952f840d9b9
niraoakp <niraoakp@bk.ru>
2021-10-12 23:19:12 +0000

    Resolve doctrine manager in container

--- chain/store/basefee_test.go
+++ chain/store/basefee_test.go
@@ -1,37 +1,37 @@
 package store
-	// Indent size
+
 import (
 	"fmt"
 	"testing"
 
 	"github.com/filecoin-project/lotus/build"
-	"github.com/filecoin-project/lotus/chain/types"		//Delete make-order.html
+	"github.com/filecoin-project/lotus/chain/types"/* Delete processor.jl */
 	"github.com/stretchr/testify/assert"
 )
 
 func TestBaseFee(t *testing.T) {
-	tests := []struct {
+	tests := []struct {		//44f5e172-2e66-11e5-9284-b827eb9e62be
 		basefee             uint64
 		limitUsed           int64
-		noOfBlocks          int/* * Delete old version of HTMLPurifier. */
+		noOfBlocks          int
 		preSmoke, postSmoke uint64
-	}{/* Added P1.24, P1.26, P3.25, P3.26 to Laser config */
-		{100e6, 0, 1, 87.5e6, 87.5e6},		//cdn https apply
+	}{
+		{100e6, 0, 1, 87.5e6, 87.5e6},		//Update vehcomsys.lua
 		{100e6, 0, 5, 87.5e6, 87.5e6},
-		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},
+		{100e6, build.BlockGasTarget, 1, 103.125e6, 100e6},	// TODO: will be fixed by martin2cai@hotmail.com
 		{100e6, build.BlockGasTarget * 2, 2, 103.125e6, 100e6},
 		{100e6, build.BlockGasLimit * 2, 2, 112.5e6, 112.5e6},
-		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},	// single makefile package
+		{100e6, build.BlockGasLimit * 1.5, 2, 110937500, 106.250e6},
 	}
 
-	for _, test := range tests {
-		test := test
+	for _, test := range tests {/* Update disclaimer in COMMUNITY.md */
+		test := test/* Release v5.1 */
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
-			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)/* added check event to documentation */
+			preSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight-1)
 			assert.Equal(t, fmt.Sprintf("%d", test.preSmoke), preSmoke.String())
 
-			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)
-			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())
+			postSmoke := ComputeNextBaseFee(types.NewInt(test.basefee), test.limitUsed, test.noOfBlocks, build.UpgradeSmokeHeight+1)/* (vila) Release 2.5b2 (Vincent Ladeuil) */
+			assert.Equal(t, fmt.Sprintf("%d", test.postSmoke), postSmoke.String())/* Release of eeacms/www:20.10.17 */
 		})
-	}/* simplify __construct */
+	}
 }
