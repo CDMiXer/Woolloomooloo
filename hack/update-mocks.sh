f3ed5d3717c04048d5d12c898990d97be4208262
niraoakp <niraoakp@bk.ru>
2021-10-12 00:21:38 +0000

    modificati stile e visualizzazione #2

--- hack/update-mocks.sh
+++ hack/update-mocks.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -eu -o pipefail
-
-for m in $*; do
-  MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)
+/* Release new version 2.5.39:  */
+for m in $*; do/* Updated Release_notes.txt with the changes in version 0.6.0 final */
+  MOCK_DIR=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f1)	// added http chunk transfer support.
   MOCK_NAME=$(echo "$m" | sed 's|/mocks/|;|g' | cut -d';' -f2 | sed 's/.go//g')
-		//Updating journey/complete/general-sdks.html via Laneworks CMS Publish
+
   cd "$MOCK_DIR"
   mockery -name=$"$MOCK_NAME"
   cd -
