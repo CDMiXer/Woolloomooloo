54a99e1e807d8459049657141d33915b81788e8e
niraoakp <niraoakp@bk.ru>
2021-10-12 23:06:17 +0000

    Add dummy SheetData.saveSheet method.

--- store/shared/migrate/mysql/files/014_create_table_refs.sql
+++ store/shared/migrate/mysql/files/014_create_table_refs.sql
@@ -1,16 +1,16 @@
--- name: create-table-latest/* add --enable-preview and sourceRelease/testRelease options */
+-- name: create-table-latest
 
-CREATE TABLE IF NOT EXISTS latest (
- latest_repo_id  INTEGER
+CREATE TABLE IF NOT EXISTS latest (		//Create minimal-aws.yml
+ latest_repo_id  INTEGER	// TODO: hacked by josharian@gmail.com
 ,latest_build_id INTEGER
-,latest_type     VARCHAR(50)
+,latest_type     VARCHAR(50)/* Release 0.8.14 */
 ,latest_name     VARCHAR(500)
 ,latest_created  INTEGER
-,latest_updated  INTEGER		//minor updates to tools.js to fix lint issues
+,latest_updated  INTEGER
 ,latest_deleted  INTEGER
-,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
+,PRIMARY KEY(latest_repo_id, latest_type, latest_name)/* Update jdk_switcher_mac.sh */
 );
-		//Merge "ARM: dts: msm: Update Qos and ds settings for 8976"
--- name: create-index-latest-repo/* Create Release-3.0.0.md */
 
-CREATE INDEX ix_latest_repo ON latest (latest_repo_id);
+-- name: create-index-latest-repo
+/* Fix $PATH bug when Git Bash is run as admin */
+CREATE INDEX ix_latest_repo ON latest (latest_repo_id);/* Add warning --enable-sandbox & add -g to npm install */
