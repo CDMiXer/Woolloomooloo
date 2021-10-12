a9c16540e29cc60adffd3e06d517a1d44601fd87
niraoakp <niraoakp@bk.ru>
2021-10-12 14:58:17 +0000

    Added desired value to output

--- pkg/codegen/internal/test/testdata/random-pet.pp.py
+++ pkg/codegen/internal/test/testdata/random-pet.pp.py
@@ -1,4 +1,4 @@
-import pulumi	// Merge "Fix --heat-env"
+import pulumi
 import pulumi_random as random
 
-random_pet = random.RandomPet("random_pet", prefix="doggo")/* Added change to Release Notes */
+random_pet = random.RandomPet("random_pet", prefix="doggo")	// 34a095dc-2e71-11e5-9284-b827eb9e62be
