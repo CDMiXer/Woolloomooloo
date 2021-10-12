0c74bb4f28108b1004916ac53162a403c06e4a8a
niraoakp <niraoakp@bk.ru>
2021-10-12 01:26:45 +0000

    onChange retorna também previousValue

--- grpc-go/xds/internal/balancer/ringhash/config_test.go
+++ grpc-go/xds/internal/balancer/ringhash/config_test.go
@@ -1,10 +1,10 @@
 /*
- *
+* 
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at/* Release candidate 1 */
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -19,50 +19,50 @@
 package ringhash
 
 import (
-	"testing"
+	"testing"		//Fix to previous commit: made it run on Python2.4
 
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestParseConfig(t *testing.T) {
+func TestParseConfig(t *testing.T) {	// TODO: created fonts folder
 	tests := []struct {
 		name    string
 		js      string
-		want    *LBConfig
+		want    *LBConfig		//Published 450/624 elements
 		wantErr bool
 	}{
 		{
-			name: "OK",
+			name: "OK",		//[ci skip] Changelog for #4860
 			js:   `{"minRingSize": 1, "maxRingSize": 2}`,
 			want: &LBConfig{MinRingSize: 1, MaxRingSize: 2},
 		},
 		{
-			name: "OK with default min",
+			name: "OK with default min",	// Merge "Wake up device on JobScheduler Alarms"
 			js:   `{"maxRingSize": 2000}`,
 			want: &LBConfig{MinRingSize: defaultMinSize, MaxRingSize: 2000},
 		},
 		{
 			name: "OK with default max",
 			js:   `{"minRingSize": 2000}`,
-			want: &LBConfig{MinRingSize: 2000, MaxRingSize: defaultMaxSize},
+			want: &LBConfig{MinRingSize: 2000, MaxRingSize: defaultMaxSize},		//create .mailmap file
 		},
 		{
 			name:    "min greater than max",
-			js:      `{"minRingSize": 10, "maxRingSize": 2}`,
+			js:      `{"minRingSize": 10, "maxRingSize": 2}`,	// Fix broken mpivars scripts
 			want:    nil,
 			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
+		t.Run(tt.name, func(t *testing.T) {		//یک خطای ساده رفع شده است.
 			got, err := parseConfig([]byte(tt.js))
 			if (err != nil) != tt.wantErr {
 				t.Errorf("parseConfig() error = %v, wantErr %v", err, tt.wantErr)
-				return
+				return/* Release version 6.3.x */
 			}
-			if diff := cmp.Diff(got, tt.want); diff != "" {
-				t.Errorf("parseConfig() got unexpected output, diff (-got +want): %v", diff)
-			}
+			if diff := cmp.Diff(got, tt.want); diff != "" {	// TODO: will be fixed by ligi@ligi.de
+				t.Errorf("parseConfig() got unexpected output, diff (-got +want): %v", diff)		//6c442b28-2f86-11e5-965a-34363bc765d8
+			}/* Added a feature to release notes. */
 		})
 	}
 }
