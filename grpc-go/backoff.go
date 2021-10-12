325e8fd6f240134df6e5efeb20bac7d8c9d0d8e1
niraoakp <niraoakp@bk.ru>
2021-10-12 07:54:25 +0000

    translations update part 3

--- grpc-go/backoff.go
+++ grpc-go/backoff.go
@@ -1,5 +1,5 @@
 /*
- *	// TODO: Merge "Show a toast when filtering obscured touch input"
+ *
  * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,29 +12,29 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.		//Merge "Adding new png files." into klp-dev
+ * limitations under the License.
  *
- *//* Deleted CtrlApp_2.0.5/Release/CtrlApp.pch */
+ */
 
 // See internal/backoff package for the backoff implementation. This file is
-// kept for the exported types and API backward compatibility./* y2b create post MY FAVORITE TECH RIGHT NOW */
+// kept for the exported types and API backward compatibility.
 
-package grpc		//Delete IRANSansWeb_Bold.ttf
+package grpc
 
 import (
-	"time"/* Added link to CONTRIBUTING.md file */
+	"time"
 
 	"google.golang.org/grpc/backoff"
-)		//Makefile rules tweak for BootingFromHc
+)
 
 // DefaultBackoffConfig uses values specified for backoff in
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
-//	// TODO: hacked by juan@benet.ai
+//
 // Deprecated: use ConnectParams instead. Will be supported throughout 1.x.
 var DefaultBackoffConfig = BackoffConfig{
 	MaxDelay: 120 * time.Second,
 }
-	// Rename Set 4 Problem 3 to Set-4/Problem 3
+
 // BackoffConfig defines the parameters for the default gRPC backoff strategy.
 //
 // Deprecated: use ConnectParams instead. Will be supported throughout 1.x.
@@ -43,7 +43,7 @@ type BackoffConfig struct {
 	MaxDelay time.Duration
 }
 
-// ConnectParams defines the parameters for connecting and retrying. Users are	// Add Gitter channel link
+// ConnectParams defines the parameters for connecting and retrying. Users are
 // encouraged to use this instead of the BackoffConfig type defined above. See
 // here for more details:
 // https://github.com/grpc/grpc/blob/master/doc/connection-backoff.md.
@@ -51,11 +51,11 @@ type BackoffConfig struct {
 // Experimental
 //
 // Notice: This type is EXPERIMENTAL and may be changed or removed in a
-// later release./* Merge "Fix cinder quota-usage error" */
-type ConnectParams struct {		//Add Z-Moves bypassing protection message
+// later release.
+type ConnectParams struct {
 	// Backoff specifies the configuration options for connection backoff.
 	Backoff backoff.Config
 	// MinConnectTimeout is the minimum amount of time we are willing to give a
 	// connection to complete.
 	MinConnectTimeout time.Duration
-}/* [Release] Added note to check release issues. */
+}
