b1547a0fc7ecfc3f8c5ec0dae8cd2918b654770b
niraoakp <niraoakp@bk.ru>
2021-10-12 11:33:17 +0000

    360395f6-2e43-11e5-9284-b827eb9e62be

--- operator/manager/teardown_test.go
+++ operator/manager/teardown_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Soft code test subdirectories
-// that can be found in the LICENSE file.		//use form_for in view for features to avoid Authenticity token errors
-	// TODO: hacked by alex.gaynor@gmail.com
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
+
 package manager
