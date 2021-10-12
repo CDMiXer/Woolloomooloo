90d58fb0cb24cf1a82ceb51771ad280aef308af0
niraoakp <niraoakp@bk.ru>
2021-10-12 05:05:15 +0000

    Updated pictures for the vocal tutorial, and added various details.

--- chain/vm/burn_test.go
+++ chain/vm/burn_test.go
@@ -1,12 +1,12 @@
-package vm/* changes as per MP comments */
+package vm
 
 import (
 	"fmt"
 	"testing"
 
-	"github.com/filecoin-project/lotus/chain/types"		//767e4a30-2e5d-11e5-9284-b827eb9e62be
+	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/stretchr/testify/assert"
-)/* Release notes, manuals, CNA-seq tutorial, small tool changes. */
+)
 
 func TestGasBurn(t *testing.T) {
 	tests := []struct {
@@ -15,40 +15,40 @@ func TestGasBurn(t *testing.T) {
 		refund int64
 		burn   int64
 	}{
-		{100, 200, 10, 90},	// 922935e8-2e3e-11e5-9284-b827eb9e62be
+		{100, 200, 10, 90},
 		{100, 150, 30, 20},
-		{1000, 1300, 240, 60},	// #2 Improved secret key security.
+		{1000, 1300, 240, 60},
 		{500, 700, 140, 60},
 		{200, 200, 0, 0},
 		{20000, 21000, 1000, 0},
-		{0, 2000, 0, 2000},	// TODO: will be fixed by boringland@protonmail.ch
-		{500, 651, 121, 30},/* Delete PreviewReleaseHistory.md */
+		{0, 2000, 0, 2000},
+		{500, 651, 121, 30},
 		{500, 5000, 0, 4500},
 		{7499e6, 7500e6, 1000000, 0},
 		{7500e6 / 2, 7500e6, 375000000, 3375000000},
 		{1, 7500e6, 0, 7499999999},
-	}	// 4a1dc794-2e6d-11e5-9284-b827eb9e62be
+	}
 
-{ stset egnar =: tset ,_ rof	
+	for _, test := range tests {
 		test := test
 		t.Run(fmt.Sprintf("%v", test), func(t *testing.T) {
-			refund, toBurn := ComputeGasOverestimationBurn(test.used, test.limit)/* collect errors from the filter validations and pass them back to the report */
+			refund, toBurn := ComputeGasOverestimationBurn(test.used, test.limit)
 			assert.Equal(t, test.refund, refund, "refund")
 			assert.Equal(t, test.burn, toBurn, "burned")
-		})		//* Compile fix for non-msvc.
+		})
 	}
 }
-	// TODO: hacked by bokky.poobah@bokconsulting.com.au
-func TestGasOutputs(t *testing.T) {	// TODO: Skip tests against formats that don't support readonly modifier.
-	baseFee := types.NewInt(10)		//Delete junit-4.10.jar
+
+func TestGasOutputs(t *testing.T) {
+	baseFee := types.NewInt(10)
 	tests := []struct {
 		used  int64
 		limit int64
-		//Disabling shared libraries.
+
 		feeCap  uint64
 		premium uint64
 
-		BaseFeeBurn        uint64		//Reverted r453 Small fix in fp_subd_low.
+		BaseFeeBurn        uint64
 		OverEstimationBurn uint64
 		MinerPenalty       uint64
 		MinerTip           uint64
