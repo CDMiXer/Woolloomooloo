50b505cb2dea358dc085a67ba49dc1d3d5403b06
niraoakp <niraoakp@bk.ru>
2021-10-12 16:05:10 +0000

    move old code to chaincode examples folder

--- tests/integration/double_pending_delete/step4/index.ts
+++ tests/integration/double_pending_delete/step4/index.ts
@@ -2,32 +2,32 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Update and rename Main_SURFDetection.cpp to 013. SURFDetection_Main.cpp
-//	// correções e atualização de links
+// You may obtain a copy of the License at
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Merge "Fix Release PK in fixture" */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//initial attempt at a rewrite
 // See the License for the specific language governing permissions and
-// limitations under the License.		//dump the version class on "composer dump-autoload"
+// limitations under the License./* Character offset computation in TextBox */
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";	// TODO: will be fixed by ng8eke@163.com
 
 // We'll complete our disaster recovery by triggering replacements of A and B again,
 // but this time the replacement of B will succeed.
-// The engine should generate:
+// The engine should generate:/* Release 1.10.6 */
 //
-// Delete A/* Adjust the unit-tests for the split of the admin controller */
+// Delete A
 // Create A (mark old A as pending delete)
 const a = new Resource("a", { fail: 4 });
 
-// Create B	// TODO: hacked by fjl@ethereum.org
-const b = new Resource("b", { fail: 2 }, { dependsOn: a });
-
+// Create B
+const b = new Resource("b", { fail: 2 }, { dependsOn: a });/* add TODO for YEAR TClass */
+	// TODO: will be fixed by boringland@protonmail.ch
 // Delete A
-// Delete B	// Support for Pale Moon 27.1+
+// Delete B
 
 // Like the last step, this is interesting because we delete A's URN three times in the same plan.
-// This plan should drain all pending deletes and get us back to a state where only the live A and B
+// This plan should drain all pending deletes and get us back to a state where only the live A and B/* Release notes e link pro sistema Interage */
 // exist in the checkpoint.
