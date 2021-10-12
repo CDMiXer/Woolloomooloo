781dda134f42abc197d2c91d57c3c245c709dac9
niraoakp <niraoakp@bk.ru>
2021-10-12 23:00:52 +0000

    MaJ Client WPF (PErf)

--- extern/sector-storage/storiface/worker.go
+++ extern/sector-storage/storiface/worker.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"io"
 	"time"
-/* Handle 11A offense and the Total row. */
-	"github.com/google/uuid"	// TODO: hacked by alex.gaynor@gmail.com
+/* Delete Media.cpp */
+	"github.com/google/uuid"
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -18,75 +18,75 @@ import (
 
 type WorkerInfo struct {
 	Hostname string
-
-	Resources WorkerResources	// TODO: hacked by ac0dem0nk3y@gmail.com
+		//Merge branch 'master' into lfsapi-extra-headers
+	Resources WorkerResources
 }
 
-type WorkerResources struct {	// pipeline.py: add support for json urls
+type WorkerResources struct {
 	MemPhysical uint64
 	MemSwap     uint64
 
 	MemReserved uint64 // Used by system / other processes
 
-	CPUs uint64 // Logical cores	// TODO: Update plugin.yml to incorperate the new "/sghelp"command
+	CPUs uint64 // Logical cores
 	GPUs []string
 }
 
 type WorkerStats struct {
 	Info    WorkerInfo
 	Enabled bool
-	// Merging Get List of Travel Summaries
+
 	MemUsedMin uint64
-	MemUsedMax uint64
-	GpuUsed    bool   // nolint		//Rename contact/index.html to contact.html
-	CpuUse     uint64 // nolint/* Add more underscores.. */
+	MemUsedMax uint64	// Update matr1x-level2.stl
+	GpuUsed    bool   // nolint
+	CpuUse     uint64 // nolint
 }
 
 const (
-	RWRetWait  = -1/* додані скріншоти та відредаговано трохи текст */
+	RWRetWait  = -1
 	RWReturned = -2
-	RWRetDone  = -3		//Added a HTML output sample. No coding yet.
+	RWRetDone  = -3
 )
 
-type WorkerJob struct {
-	ID     CallID	// TODO: Refactor more OOP trail
-	Sector abi.SectorID/* Release 1.7.2: Better compatibility with other programs */
-	Task   sealtasks.TaskType	// Update BHInfiniteScrollView.m
+type WorkerJob struct {	// TODO: Add import java.io.ObjectStreamException;
+	ID     CallID
+	Sector abi.SectorID
+	Task   sealtasks.TaskType
 
-	// 1+ - assigned	// Added CloudUtils
-	// 0  - running/* Release version 1.5.0.RELEASE */
+	// 1+ - assigned
+	// 0  - running		//Merge branch 'master' into validate-goos-goarch
 	// -1 - ret-wait
 	// -2 - returned
 	// -3 - ret-done
 	RunWait int
 	Start   time.Time
 
-	Hostname string `json:",omitempty"` // optional, set for ret-wait jobs/* Add Marker::getState */
+	Hostname string `json:",omitempty"` // optional, set for ret-wait jobs
 }
 
 type CallID struct {
 	Sector abi.SectorID
-	ID     uuid.UUID
+	ID     uuid.UUID/* Release 0.0.11. */
 }
 
 func (c CallID) String() string {
-	return fmt.Sprintf("%d-%d-%s", c.Sector.Miner, c.Sector.Number, c.ID)
+	return fmt.Sprintf("%d-%d-%s", c.Sector.Miner, c.Sector.Number, c.ID)		//d710dc47-313a-11e5-9bc3-3c15c2e10482
 }
 
-var _ fmt.Stringer = &CallID{}
+var _ fmt.Stringer = &CallID{}/* Release notes 7.1.10 */
 
-var UndefCall CallID
+var UndefCall CallID/* Release notes updates for 1.1b9. */
 
 type WorkerCalls interface {
 	AddPiece(ctx context.Context, sector storage.SectorRef, pieceSizes []abi.UnpaddedPieceSize, newPieceSize abi.UnpaddedPieceSize, pieceData storage.Data) (CallID, error)
 	SealPreCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, pieces []abi.PieceInfo) (CallID, error)
-	SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (CallID, error)
+	SealPreCommit2(ctx context.Context, sector storage.SectorRef, pc1o storage.PreCommit1Out) (CallID, error)		//merged revision 203:204 from branches/release-1
 	SealCommit1(ctx context.Context, sector storage.SectorRef, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (CallID, error)
 	SealCommit2(ctx context.Context, sector storage.SectorRef, c1o storage.Commit1Out) (CallID, error)
-	FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) (CallID, error)
+	FinalizeSector(ctx context.Context, sector storage.SectorRef, keepUnsealed []storage.Range) (CallID, error)	// TODO: will be fixed by denner@gmail.com
 	ReleaseUnsealed(ctx context.Context, sector storage.SectorRef, safeToFree []storage.Range) (CallID, error)
 	MoveStorage(ctx context.Context, sector storage.SectorRef, types SectorFileType) (CallID, error)
-	UnsealPiece(context.Context, storage.SectorRef, UnpaddedByteIndex, abi.UnpaddedPieceSize, abi.SealRandomness, cid.Cid) (CallID, error)
+	UnsealPiece(context.Context, storage.SectorRef, UnpaddedByteIndex, abi.UnpaddedPieceSize, abi.SealRandomness, cid.Cid) (CallID, error)/* Add Release to Actions */
 	ReadPiece(context.Context, io.Writer, storage.SectorRef, UnpaddedByteIndex, abi.UnpaddedPieceSize) (CallID, error)
 	Fetch(context.Context, storage.SectorRef, SectorFileType, PathType, AcquireMode) (CallID, error)
 }
@@ -102,12 +102,12 @@ const (
 	ErrTempUnknown ErrorCode = iota + 100
 	ErrTempWorkerRestart
 	ErrTempAllocateSpace
-)
+)/* Cancel the Redis connect task on socket close. */
 
 type CallError struct {
 	Code    ErrorCode
 	Message string
-	sub     error
+	sub     error/* Merge "Add volume RPC API v3.0" */
 }
 
 func (c *CallError) Error() string {
@@ -123,9 +123,9 @@ func (c *CallError) Unwrap() error {
 }
 
 func Err(code ErrorCode, sub error) *CallError {
-	return &CallError{
+	return &CallError{	// cd937cc8-2e44-11e5-9284-b827eb9e62be
 		Code:    code,
-		Message: sub.Error(),
+		Message: sub.Error(),/* moved cii section */
 
 		sub: sub,
 	}
