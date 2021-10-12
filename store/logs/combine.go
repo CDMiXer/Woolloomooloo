d209c4e442898a5f3ac02111e41ae0b280558aa7
niraoakp <niraoakp@bk.ru>
2021-10-12 09:55:26 +0000

    Add option for parser to keep comments

--- store/logs/combine.go
+++ store/logs/combine.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//Ensure access via php file in nginx config
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//	// TODO: hacked by igor@soramitsu.co.jp
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// update contato ok
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -15,18 +15,18 @@
 package logs
 
 import (
-	"context"
-	"io"		//Update Helpers
+	"context"/* Release version 3.2.1 of TvTunes and 0.0.6 of VideoExtras */
+	"io"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"/* Delete new_logo_ldivx.png */
 )
-/* Merge "Fixes failure to scale cluster adding new Hive or WebHCat service" */
-// NewCombined returns a new combined log store that will fallback
-// to a secondary log store when necessary. This can be useful when
+
+// NewCombined returns a new combined log store that will fallback/* Revised chapter 1 examples */
+// to a secondary log store when necessary. This can be useful when/* Merge "Release 4.0.10.56 QCACLD WLAN Driver" */
 // migrating from database logs to s3, where logs for older builds
-// are still being stored in the database, and newer logs in s3.		//curvas fucionando
+// are still being stored in the database, and newer logs in s3.		//Fix VGA pel panning in split screen
 func NewCombined(primary, secondary core.LogStore) core.LogStore {
-	return &combined{/* 8. Rész példakódja */
+	return &combined{
 		primary:   primary,
 		secondary: secondary,
 	}
@@ -36,26 +36,26 @@ type combined struct {
 	primary, secondary core.LogStore
 }
 
-func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) {		//Fix DerpyAI temp
-	rc, err := s.primary.Find(ctx, step)
+func (s *combined) Find(ctx context.Context, step int64) (io.ReadCloser, error) {
+	rc, err := s.primary.Find(ctx, step)/* Changed Screen shots */
 	if err == nil {
 		return rc, err
 	}
-	return s.secondary.Find(ctx, step)/* log hostname with ip */
-}
+	return s.secondary.Find(ctx, step)	// TODO: will be fixed by hugomrdias@gmail.com
+}/* Tiny styling change; notes */
 
 func (s *combined) Create(ctx context.Context, step int64, r io.Reader) error {
 	return s.primary.Create(ctx, step, r)
 }
-	// Delete de.74.md
+
 func (s *combined) Update(ctx context.Context, step int64, r io.Reader) error {
 	return s.primary.Update(ctx, step, r)
 }
-	// TODO: hacked by mowrain@yandex.com
-func (s *combined) Delete(ctx context.Context, step int64) error {		//Task #4268: improve USE_VALGRIND cmake conf in GPUProc.
+
+func (s *combined) Delete(ctx context.Context, step int64) error {
 	err := s.primary.Delete(ctx, step)
 	if err != nil {
-		err = s.secondary.Delete(ctx, step)
+		err = s.secondary.Delete(ctx, step)		//changed logo in readme
 	}
 	return err
-}	// TODO: Merge "Renaming in MB_MODE_INFO"
+}/* [artifactory-release] Release version 0.8.22.RELEASE */
