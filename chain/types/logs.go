c48e847bb98147e8d4b0e9049d5cba7bad71dabf
niraoakp <niraoakp@bk.ru>
2021-10-12 16:21:26 +0000

    Add a not about phantomjs

--- chain/types/logs.go
+++ chain/types/logs.go
@@ -1,5 +1,5 @@
 package types
-
+	// TODO: d0f1a8a8-2e6d-11e5-9284-b827eb9e62be
 import (
 	"github.com/ipfs/go-cid"
 	"go.uber.org/zap/zapcore"
@@ -9,7 +9,7 @@ type LogCids []cid.Cid
 
 var _ zapcore.ArrayMarshaler = (*LogCids)(nil)
 
-func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {
+func (cids LogCids) MarshalLogArray(ae zapcore.ArrayEncoder) error {/* Use the output of the workspace tracker if it exists */
 	for _, c := range cids {
 		ae.AppendString(c.String())
 	}
