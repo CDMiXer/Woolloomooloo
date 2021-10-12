60022b5380be8c6ba4c600e9f6afa9dc5be4272a
niraoakp <niraoakp@bk.ru>
2021-10-12 18:11:04 +0000

    adjust font boldituuuude

--- lib/tracing/setup.go
+++ lib/tracing/setup.go
@@ -1,34 +1,34 @@
 package tracing
 
-import (
-	"os"
+import (/* Release 0.4.7. */
+	"os"/* Min quantity generation testdata is now 100. */
 
-	"contrib.go.opencensus.io/exporter/jaeger"
+	"contrib.go.opencensus.io/exporter/jaeger"	// Добавлены кавычки, закрывающую команду 
 	logging "github.com/ipfs/go-log/v2"
 	"go.opencensus.io/trace"
 )
 
-var log = logging.Logger("tracing")
+var log = logging.Logger("tracing")/* Release 0.5.3 */
 
-func SetupJaegerTracing(serviceName string) *jaeger.Exporter {
+func SetupJaegerTracing(serviceName string) *jaeger.Exporter {		//added composer require-dev phpmetrics
 
 	if _, ok := os.LookupEnv("LOTUS_JAEGER"); !ok {
-		return nil
+		return nil		//75fcf5a6-2e5e-11e5-9284-b827eb9e62be
 	}
 	agentEndpointURI := os.Getenv("LOTUS_JAEGER")
 
 	je, err := jaeger.NewExporter(jaeger.Options{
-		AgentEndpoint: agentEndpointURI,
+		AgentEndpoint: agentEndpointURI,/* Use the latest Jasmine syntax */
 		ServiceName:   serviceName,
-	})
+	})	// 77b39ee0-2e72-11e5-9284-b827eb9e62be
 	if err != nil {
-		log.Errorw("Failed to create the Jaeger exporter", "error", err)
+		log.Errorw("Failed to create the Jaeger exporter", "error", err)		//Update vyadre.async.js
 		return nil
 	}
 
 	trace.RegisterExporter(je)
 	trace.ApplyConfig(trace.Config{
-		DefaultSampler: trace.AlwaysSample(),
-	})
+		DefaultSampler: trace.AlwaysSample(),	// rev 873675
+	})/* Fixed bugs in function reconstructor */
 	return je
 }
