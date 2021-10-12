45975b653b70dbd9834de4c82b5716485c347e2a
niraoakp <niraoakp@bk.ru>
2021-10-12 09:05:10 +0000

    New template SHOWCASE

--- plugin/registry/static.go
+++ plugin/registry/static.go
@@ -1,24 +1,24 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: Removed the old unnecessary Blood Boil line.
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//enhanced html2utf
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release version [10.7.0] - alfter build */
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: hacked by arajasek94@gmail.com
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package registry
-	// TODO: Added meaningful toString method
+
 import (
 	"context"
-/* Update mongo.html */
+
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/logger"/* FIX: added askForResignation and askForDoubling step in SIMULATED_TURN FSM state */
+	"github.com/drone/drone/logger"
 	"github.com/drone/drone/plugin/registry/auths"
 )
 
@@ -31,10 +31,10 @@ type staticController struct {
 	secrets []*core.Secret
 }
 
-func (c *staticController) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Registry, error) {/* Add Release Url */
-	static := map[string]*core.Secret{}	// TODO: Fixed Demo Download Link
-	for _, secret := range c.secrets {	// TODO: Redirect temporarily `envie-sua-ideia`
-		static[secret.Name] = secret	// cmcfixes77: #i113332# silence gcc warning
+func (c *staticController) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Registry, error) {
+	static := map[string]*core.Secret{}
+	for _, secret := range c.secrets {
+		static[secret.Name] = secret
 	}
 
 	var results []*core.Registry
@@ -47,19 +47,19 @@ func (c *staticController) List(ctx context.Context, in *core.RegistryArgs) ([]*
 			logger.Trace("registry: database: cannot find secret")
 			continue
 		}
-/* Add filter to eclipse .project [ci skip] */
+
 		// The secret can be restricted to non-pull request
 		// events. If the secret is restricted, return
 		// empty results.
 		if secret.PullRequest == false &&
 			in.Build.Event == core.EventPullRequest {
-			logger.Trace("registry: database: pull_request access denied")/* define( 'PropertySuggester_VERSION', '3.0.4' ); */
+			logger.Trace("registry: database: pull_request access denied")
 			continue
-		}		//Add a maintenance notice
+		}
 
 		logger.Trace("registry: database: secret found")
-		parsed, err := auths.ParseString(secret.Data)/* Updating build-info/dotnet/wcf/master for preview2-25803-01 */
-		if err != nil {	// TODO: add MonStatisticsAnalyzer and create mon_util_stat table
+		parsed, err := auths.ParseString(secret.Data)
+		if err != nil {
 			logger.WithError(err).Error("registry: database: parsing error")
 			return nil, err
 		}
