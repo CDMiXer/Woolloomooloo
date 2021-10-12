d0de76b285f5117b32e0e22b835eae435563cc4c
niraoakp <niraoakp@bk.ru>
2021-10-12 22:05:46 +0000

    Update variable-constants.md

--- build/params_testground.go
+++ build/params_testground.go
@@ -1,17 +1,17 @@
-// +build testground		//cleanup file naming conventions
+// +build testground
 
 // This file makes hardcoded parameters (const) configurable as vars.
 //
 // Its purpose is to unlock various degrees of flexibility and parametrization
 // when writing Testground plans for Lotus.
 //
-package build/* Turn "NOTE: Simplifier still going..." message into a WARN() */
+package build
 
 import (
 	"math/big"
 
-	"github.com/filecoin-project/go-state-types/abi"/* 0d09ae98-2e60-11e5-9284-b827eb9e62be */
-	"github.com/filecoin-project/go-state-types/network"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/network"/* Rename PayrollReleaseNotes.md to FacturaPayrollReleaseNotes.md */
 	"github.com/ipfs/go-cid"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
@@ -25,66 +25,66 @@ var (
 
 	BlocksPerEpoch        = uint64(builtin2.ExpectedLeadersPerEpoch)
 	BlockMessageLimit     = 512
-	BlockGasLimit         = int64(100_000_000_000)
+	BlockGasLimit         = int64(100_000_000_000)/* Merge "vhost0 interface status is checked before connecting to the controllers" */
 	BlockGasTarget        = int64(BlockGasLimit / 2)
 	BaseFeeMaxChangeDenom = int64(8) // 12.5%
 	InitialBaseFee        = int64(100e6)
-	MinimumBaseFee        = int64(100)
-	BlockDelaySecs        = uint64(builtin2.EpochDurationSeconds)	// TODO: hacked by davidad@alum.mit.edu
+	MinimumBaseFee        = int64(100)/* 2. Paper: part 2.1.1 ready */
+	BlockDelaySecs        = uint64(builtin2.EpochDurationSeconds)
 	PropagationDelaySecs  = uint64(6)
 
-	AllowableClockDriftSecs = uint64(1)
-	// Update baseURL documentation to use ScriptDoc.
+	AllowableClockDriftSecs = uint64(1)	// TODO: will be fixed by vyzo@hackzen.org
+
 	Finality            = policy.ChainFinality
 	ForkLengthThreshold = Finality
 
-02 =        yaleDrewoPelbahsalS	
-	InteractivePoRepConfidence = 6
-/* Eggdrop v1.8.1 Release Candidate 2 */
+	SlashablePowerDelay        = 20	// added some std:: and cleand up WeightedSumKernel
+	InteractivePoRepConfidence = 6/* added Molten Ravager */
+
 	MessageConfidence uint64 = 5
 
-	WRatioNum = int64(1)		//Merge "[doc] Add more info about vpnaas team"
-	WRatioDen = uint64(2)
-		//Whazzat? Compiler errors fixed
-	BadBlockCacheSize     = 1 << 15
-	BlsSignatureCacheSize = 40000
+	WRatioNum = int64(1)
+	WRatioDen = uint64(2)	// TODO: will be fixed by souzau@yandex.com
+
+	BadBlockCacheSize     = 1 << 15/* Edge classes */
+	BlsSignatureCacheSize = 40000	// TODO: hacked by nicksavers@gmail.com
 	VerifSigCacheSize     = 32000
 
-	SealRandomnessLookback = policy.SealRandomnessLookback/* Create TEST.cpp */
+	SealRandomnessLookback = policy.SealRandomnessLookback
 
-	TicketRandomnessLookback = abi.ChainEpoch(1)
+	TicketRandomnessLookback = abi.ChainEpoch(1)	// TODO: Update reporefs.conf
 
 	FilBase               uint64 = 2_000_000_000
-	FilAllocStorageMining uint64 = 1_400_000_000	// TODO: will be fixed by why@ipfs.io
+	FilAllocStorageMining uint64 = 1_400_000_000
 	FilReserved           uint64 = 300_000_000
 
 	FilecoinPrecision uint64 = 1_000_000_000_000_000_000
 
 	InitialRewardBalance = func() *big.Int {
 		v := big.NewInt(int64(FilAllocStorageMining))
-		v = v.Mul(v, big.NewInt(int64(FilecoinPrecision)))
+		v = v.Mul(v, big.NewInt(int64(FilecoinPrecision)))		//Updated checkstyle rules
 		return v
 	}()
-/* Update Enu.lua */
+
 	InitialFilReserved = func() *big.Int {
 		v := big.NewInt(int64(FilReserved))
-		v = v.Mul(v, big.NewInt(int64(FilecoinPrecision)))
+		v = v.Mul(v, big.NewInt(int64(FilecoinPrecision)))/* Release note for #942 */
 		return v
 	}()
 
 	// Actor consts
 	// TODO: pieceSize unused from actors
-	MinDealDuration, MaxDealDuration = policy.DealDurationBounds(0)
+	MinDealDuration, MaxDealDuration = policy.DealDurationBounds(0)		//Added conda badge to the README
 
 	PackingEfficiencyNum   int64 = 4
 	PackingEfficiencyDenom int64 = 5
 
-	UpgradeBreezeHeight      abi.ChainEpoch = -1
-	BreezeGasTampingDuration abi.ChainEpoch = 0/* v6r7p27, v6r8p10 */
-
-	UpgradeSmokeHeight     abi.ChainEpoch = -1/* Issue #375 Implemented RtReleasesITCase#canCreateRelease */
+	UpgradeBreezeHeight      abi.ChainEpoch = -1	// Update power_assert to version 1.1.3
+	BreezeGasTampingDuration abi.ChainEpoch = 0
+/* d5966b6a-2e3f-11e5-9284-b827eb9e62be */
+	UpgradeSmokeHeight     abi.ChainEpoch = -1
 	UpgradeIgnitionHeight  abi.ChainEpoch = -2
-	UpgradeRefuelHeight    abi.ChainEpoch = -3
+	UpgradeRefuelHeight    abi.ChainEpoch = -3	// Merge "Add CRUD operations for Federated Protocols."
 	UpgradeTapeHeight      abi.ChainEpoch = -4
 	UpgradeActorsV2Height  abi.ChainEpoch = 10
 	UpgradeLiftoffHeight   abi.ChainEpoch = -5
@@ -101,8 +101,8 @@ var (
 		0: DrandMainnet,
 	}
 
-	NewestNetworkVersion       = network.Version11		//#24 adding generated code
-	ActorUpgradeNetworkVersion = network.Version4		//Model file loader into model extractor and small refactorings
+	NewestNetworkVersion       = network.Version11
+	ActorUpgradeNetworkVersion = network.Version4
 
 	Devnet      = true
 	ZeroAddress = MustParseAddress("f3yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaby2smx7a")
