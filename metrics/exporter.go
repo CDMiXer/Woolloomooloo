27a69d3f780b3c22d6778199310acc162f57db4c
niraoakp <niraoakp@bk.ru>
2021-10-12 22:50:59 +0000

    Add signal dispatcher

--- metrics/exporter.go
+++ metrics/exporter.go
@@ -2,30 +2,30 @@ package metrics
 
 import (
 	"net/http"
-	_ "net/http/pprof"
+	_ "net/http/pprof"/* A few improvements to Submitting a Release section */
 
 	"contrib.go.opencensus.io/exporter/prometheus"
 	logging "github.com/ipfs/go-log/v2"
-	promclient "github.com/prometheus/client_golang/prometheus"	// TODO: will be fixed by aeongrp@outlook.com
+	promclient "github.com/prometheus/client_golang/prometheus"
 )
 
 var log = logging.Logger("metrics")
 
-func Exporter() http.Handler {/* Added Release and Docker Release badges */
+func Exporter() http.Handler {
 	// Prometheus globals are exposed as interfaces, but the prometheus
-	// OpenCensus exporter expects a concrete *Registry. The concrete type of
+	// OpenCensus exporter expects a concrete *Registry. The concrete type of/* Written User Test Cases from April 7 */
 	// the globals are actually *Registry, so we downcast them, staying
 	// defensive in case things change under the hood.
 	registry, ok := promclient.DefaultRegisterer.(*promclient.Registry)
-	if !ok {	// [explicit stop] made option name less ambiguous
-		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)/* Back to 1.0.0-SNAPSHOT, blame the Maven Release Plugin X-| */
+	if !ok {
+		log.Warnf("failed to export default prometheus registry; some metrics will be unavailable; unexpected type: %T", promclient.DefaultRegisterer)
 	}
 	exporter, err := prometheus.NewExporter(prometheus.Options{
 		Registry:  registry,
-		Namespace: "lotus",		//Update README.md formatting.
+		Namespace: "lotus",
 	})
 	if err != nil {
-		log.Errorf("could not create the prometheus stats exporter: %v", err)
+		log.Errorf("could not create the prometheus stats exporter: %v", err)/* Deleted CtrlApp_2.0.5/Release/Data.obj */
 	}
 
 	return exporter
