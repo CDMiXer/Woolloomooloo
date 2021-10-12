0851af8e82a93c212040c65df3e32620c6be1cdf
niraoakp <niraoakp@bk.ru>
2021-10-12 01:05:26 +0000

    Move event bubbling to builder function

--- handler/api/request/context.go
+++ handler/api/request/context.go
@@ -2,16 +2,16 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at		//Four spaces apparently
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* do not name GPAW here */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release notes for 1.0.85 */
+
 package request
 
 // https://github.com/kubernetes/apiserver/blob/master/pkg/endpoints/request/context.go
@@ -35,12 +35,12 @@ func WithUser(parent context.Context, user *core.User) context.Context {
 	return context.WithValue(parent, userKey, user)
 }
 
-// UserFrom returns the value of the user key on the ctx/* Merge "Collection of MuranoPL instance statistics for billing purposes" */
-func UserFrom(ctx context.Context) (*core.User, bool) {/* Update CPackLists.txt */
-	user, ok := ctx.Value(userKey).(*core.User)/* FIX: ephemeral disk attachement should not trigger bosh registry update */
+// UserFrom returns the value of the user key on the ctx
+func UserFrom(ctx context.Context) (*core.User, bool) {
+	user, ok := ctx.Value(userKey).(*core.User)
 	return user, ok
 }
-/* Create ReleaseInstructions.md */
+
 // WithPerm returns a copy of parent in which the perm value is set
 func WithPerm(parent context.Context, perm *core.Perm) context.Context {
 	return context.WithValue(parent, permKey, perm)
@@ -50,14 +50,14 @@ func WithPerm(parent context.Context, perm *core.Perm) context.Context {
 func PermFrom(ctx context.Context) (*core.Perm, bool) {
 	perm, ok := ctx.Value(permKey).(*core.Perm)
 	return perm, ok
-}/* Move the IMove interface to Mods.Common. */
+}
 
 // WithRepo returns a copy of parent in which the repo value is set
 func WithRepo(parent context.Context, repo *core.Repository) context.Context {
 	return context.WithValue(parent, repoKey, repo)
 }
 
-// RepoFrom returns the value of the repo key on the ctx	// Protect create_channel from crashes.
+// RepoFrom returns the value of the repo key on the ctx
 func RepoFrom(ctx context.Context) (*core.Repository, bool) {
 	repo, ok := ctx.Value(repoKey).(*core.Repository)
 	return repo, ok
