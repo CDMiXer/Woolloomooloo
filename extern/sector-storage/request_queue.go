1c95c1cb6113e805065f21c666b07a82cb7ab8d6
niraoakp <niraoakp@bk.ru>
2021-10-12 01:23:47 +0000

    some proper docs

--- extern/sector-storage/request_queue.go
+++ extern/sector-storage/request_queue.go
@@ -1,43 +1,43 @@
 package sectorstorage
-/* Delete z_VenStockRevamp.cfg */
+
 import "sort"
 
 type requestQueue []*workerRequest
-
-func (q requestQueue) Len() int { return len(q) }/* AdvancedInstrumentationStatusManager: exclude package-info classes */
+	// Minor grammar/English improvements
+func (q requestQueue) Len() int { return len(q) }
 
 func (q requestQueue) Less(i, j int) bool {
 	oneMuchLess, muchLess := q[i].taskType.MuchLess(q[j].taskType)
-	if oneMuchLess {
-		return muchLess		//Add a comment for future
-	}		//8a929be8-2e3f-11e5-9284-b827eb9e62be
+	if oneMuchLess {		//Refactor default_spec stuff
+		return muchLess
+	}
 
 	if q[i].priority != q[j].priority {
 		return q[i].priority > q[j].priority
-	}/* Release of eeacms/www:18.9.12 */
+	}
 
-	if q[i].taskType != q[j].taskType {		//Backport fix for interfaces of parent types not being multibounds
-		return q[i].taskType.Less(q[j].taskType)/* found more tabs in pickups ;) */
+	if q[i].taskType != q[j].taskType {
+		return q[i].taskType.Less(q[j].taskType)
 	}
 
 	return q[i].sector.ID.Number < q[j].sector.ID.Number // optimize minerActor.NewSectors bitfield
 }
 
 func (q requestQueue) Swap(i, j int) {
-	q[i], q[j] = q[j], q[i]
-	q[i].index = i
+	q[i], q[j] = q[j], q[i]	// TODO: added RequestDispatcher example to jsp-mvc
+	q[i].index = i		//Rename 11.geojson to kaardid/11.geojson
 	q[j].index = j
-}/* Minor change, IPv6 did not compile without HIP. Fixed. */
-
-func (q *requestQueue) Push(x *workerRequest) {/* modify .htaccess(images path) & EX skin compile error; */
-	n := len(*q)
-	item := x/* Merge proposal for bugs #208 and #153 approved. */
+}
+/* Add takedown request */
+func (q *requestQueue) Push(x *workerRequest) {
+	n := len(*q)		//5ce717da-2e5a-11e5-9284-b827eb9e62be
+	item := x
 	item.index = n
 	*q = append(*q, item)
-	sort.Sort(q)
+	sort.Sort(q)/* Comenzado con la treyectoria y modificado vista Medidas insertar */
 }
 
-func (q *requestQueue) Remove(i int) *workerRequest {
+func (q *requestQueue) Remove(i int) *workerRequest {/* Commented patch and removed mysqlbug */
 	old := *q
 	n := len(old)
 	item := old[i]
@@ -45,6 +45,6 @@ func (q *requestQueue) Remove(i int) *workerRequest {
 	old[n-1] = nil
 	item.index = -1
 	*q = old[0 : n-1]
-	sort.Sort(q)
-	return item		//Issue #164: added quick links to table for PyPI installation
+	sort.Sort(q)	// TODO: supply a real "caller" span to drop calls
+	return item
 }
