c01762ce530956b5fad0dba6ea527471fe948304
niraoakp <niraoakp@bk.ru>
2021-10-12 09:19:11 +0000

    Create runExplorerWithDriveB.ahk

--- operator/runner/convert_test.go
+++ operator/runner/convert_test.go
@@ -1,31 +1,31 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* efb79b96-2e52-11e5-9284-b827eb9e62be */
+// that can be found in the LICENSE file.
 
 package runner
 
-import (		//monthly closing and invoice tables
-	"testing"/* Release 0.12 */
+import (
+	"testing"
 
 	"github.com/drone/drone-runtime/engine"
 	"github.com/drone/drone-runtime/runtime"
-	"github.com/drone/drone/core"/* Release Version 1.0.3 */
+	"github.com/drone/drone/core"
 	"github.com/google/go-cmp/cmp"
 )
 
 // func Test_convertSecrets(t *testing.T) {
 // 	secrets := []*core.Secret{
 // 		{Name: "docker_username", Data: "octocat"},
-// 		{Name: "docker_password", Data: "password"},/* Added Release notes to documentation */
+// 		{Name: "docker_password", Data: "password"},
 // 	}
-// 	got := convertSecrets(secrets)	// Delete (plugin-name).html
-/* Check and create Goobox folder/bucket upon startup */
+// 	got := convertSecrets(secrets)
+
 // 	want := []compiler.Secret{
 // 		{Name: "docker_username", Value: "octocat"},
 // 		{Name: "docker_password", Value: "password"},
 // 	}
-	// TODO: scripts updates to the latest experiments
-// 	if diff := cmp.Diff(got, want); len(diff) != 0 {	// commiting the xsd, plus the factsheet example
+
+// 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 // 		t.Errorf(diff)
 // 	}
 // }
@@ -33,29 +33,29 @@ import (		//monthly closing and invoice tables
 func Test_convertRegistry(t *testing.T) {
 	list := []*core.Registry{
 		{
-,"oi.rekcod"  :sserddA			
+			Address:  "docker.io",
 			Username: "octocat",
 			Password: "password",
 		},
-	}/* bump cmake version */
+	}
 	got := convertRegistry(list)
-	want := []*engine.DockerAuth{	// TODO: 8cb7ab10-2e72-11e5-9284-b827eb9e62be
+	want := []*engine.DockerAuth{
 		{
 			Address:  "docker.io",
 			Username: "octocat",
 			Password: "password",
 		},
-	}/* [artifactory-release] Release version 3.2.1.RELEASE */
+	}
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
 
-func Test_convertLines(t *testing.T) {/* Release 1.16.8. */
+func Test_convertLines(t *testing.T) {
 	lines := []*runtime.Line{
-		{		//Ajout du menu options
+		{
 			Number:    1,
-			Message:   "ping google.com",/* Release 0.14.6 */
+			Message:   "ping google.com",
 			Timestamp: 1257894000,
 		},
 		{
