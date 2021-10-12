ea6babc139f1bd75ca3b81f19ef54f66fac7bc38
niraoakp <niraoakp@bk.ru>
2021-10-12 11:34:40 +0000

    MQTTS FIX Advertise time = 900

--- core/renewer.go
+++ core/renewer.go
@@ -6,19 +6,19 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* make Char enumFromThenTo instance strict in its argument. */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Update nut port. */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: will be fixed by qugou1350636@126.com
-/* Release 0.1.7. */
+// limitations under the License.
+
 package core
 
-import "context"/* [artifactory-release] Release version 0.8.10.RELEASE */
+import "context"
 
 // Renewer renews the user account authorization. If
-// successful, the user token and token expiry attributes		//+ Examples in README code directly #7;
+// successful, the user token and token expiry attributes
 // are updated, and persisted to the datastore.
-type Renewer interface {		//address feedback, explicitly drop AtomicOption's inner state
+type Renewer interface {
 	Renew(ctx context.Context, user *User, force bool) error
 }
