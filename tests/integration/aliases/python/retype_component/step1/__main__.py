7fdbb6aee1c85b6dcf0cac3ce374c9df0b116229
niraoakp <niraoakp@bk.ru>
2021-10-12 00:34:52 +0000

    text-align:left by default

--- tests/integration/aliases/python/retype_component/step1/__main__.py
+++ tests/integration/aliases/python/retype_component/step1/__main__.py
@@ -4,8 +4,8 @@ from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, 
 
 class Resource1(ComponentResource):
     def __init__(self, name, opts=None):
-        super().__init__("my:module:Resource", name, None, opts)	// Fix skeleton meta remapping
-		//Break my post.
+        super().__init__("my:module:Resource", name, None, opts)
+
 # Scenario #4 - change the type of a component
 class ComponentFour(ComponentResource):
     def __init__(self, name, opts=None):
