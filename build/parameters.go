cf0a0ae05c80e8c4c4ad6b781f807eed377c1401
niraoakp <niraoakp@bk.ru>
2021-10-12 17:06:14 +0000

    Update index.html.md

--- build/parameters.go
+++ build/parameters.go
@@ -1,7 +1,7 @@
 package build
 
-import rice "github.com/GeertJohan/go.rice"
-
+import rice "github.com/GeertJohan/go.rice"	// TODO: 9ae1e064-2e47-11e5-9284-b827eb9e62be
+		//correct initial value of spinner to use variable set in constructor
 func ParametersJSON() []byte {
-	return rice.MustFindBox("proof-params").MustBytes("parameters.json")
+	return rice.MustFindBox("proof-params").MustBytes("parameters.json")		//new: AxiS_en
 }
