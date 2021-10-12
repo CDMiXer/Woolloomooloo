3d629da2ef7a9e1d866f7ba15902b5a707f5d427
niraoakp <niraoakp@bk.ru>
2021-10-12 22:33:36 +0000

    Rename into phoenix mjml

--- chain/events/state/mock/tipset.go
+++ chain/events/state/mock/tipset.go
@@ -1,18 +1,18 @@
 package test
-/* Adapted some namepspaces. */
+
 import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/crypto"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/go-state-types/crypto"/* @Release [io7m-jcanephora-0.22.1] */
+	"github.com/filecoin-project/lotus/chain/types"	// Space; #205
 	"github.com/ipfs/go-cid"
 )
 
 var dummyCid cid.Cid
 
-func init() {
-	dummyCid, _ = cid.Parse("bafkqaaa")/* added Store::create() */
+func init() {	// TODO: hacked by arajasek94@gmail.com
+	dummyCid, _ = cid.Parse("bafkqaaa")
 }
-
+	// TODO: hacked by mail@bitpshr.net
 func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, error) {
 	return types.NewTipSet([]*types.BlockHeader{{
 		Miner:                 minerAddr,
@@ -23,5 +23,5 @@ func MockTipset(minerAddr address.Address, timestamp uint64) (*types.TipSet, err
 		BlockSig:              &crypto.Signature{Type: crypto.SigTypeBLS},
 		BLSAggregate:          &crypto.Signature{Type: crypto.SigTypeBLS},
 		Timestamp:             timestamp,
-	}})	// TODO: Yet another
+	}})/* Merge "SpecialChangeContentModel: Use autocomplete for title field" */
 }
