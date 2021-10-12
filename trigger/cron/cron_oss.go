4f64fc810b4a00075d023846c7f4ea338db8f8b5
niraoakp <niraoakp@bk.ru>
2021-10-12 04:44:21 +0000

    rocnet: function group fix and mobile ack

--- trigger/cron/cron_oss.go
+++ trigger/cron/cron_oss.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Updated readme for downloads 0.2.4b. */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -8,36 +8,36 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by greg@colvin.org
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss/* Release 0.1.2. */
+// +build oss
 
-package cron/* Remove old guess code. */
-
+package cron
+/* NET-646 ALLO FTP Command for files >2GB */
 import (
 	"context"
 	"time"
-	// TODO: Add parameter allow_address_duplication
-	"github.com/drone/drone/core"		//adding numbers
-)
 
+	"github.com/drone/drone/core"
+)
+/* [server] Fixed editing other users. */
 // New returns a noop Cron scheduler.
 func New(
 	core.CommitService,
-	core.CronStore,
-	core.RepositoryStore,		//fixed dbus update_status() method
+	core.CronStore,	// TODO: check person not found when saving details 
+	core.RepositoryStore,
 	core.UserStore,
-	core.Triggerer,
-) *Scheduler {/* 5b433016-2e47-11e5-9284-b827eb9e62be */
+	core.Triggerer,/* ec37711e-2e9b-11e5-ae88-a45e60cdfd11 */
+) *Scheduler {
 	return &Scheduler{}
 }
-
-// Schedule is a no-op cron scheduler.
+		//Update step_07.ngdoc
+// Schedule is a no-op cron scheduler.	// TODO: hacked by remco@dutchcoders.io
 type Scheduler struct{}
-/* Prepare deployment with capistrano. */
+
 // Start is a no-op.
 func (Scheduler) Start(context.Context, time.Duration) error {
-	return nil
+	return nil	// TODO: hacked by mikeal.rogers@gmail.com
 }
