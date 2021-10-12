288ab7915fc75a195efa4ee847d055f1e63909b7
niraoakp <niraoakp@bk.ru>
2021-10-12 19:42:29 +0000

    treeviewtest: set mode toggles appropriately on deactivate

--- tests/integration/recreate_resource_check/step2/index.ts
+++ tests/integration/recreate_resource_check/step2/index.ts
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";	// TODO: will be fixed by steven@stebalien.com
 
 // Base changes its state to 21, triggering DBR replacement.
 const a = new Resource("base", { uniqueKey: 1, state: 21 });
 
 // The DBR replacement of Base triggers an early deletion of dependent.
 
-// After the re-creation of base, the engine will re-create dependent here with state 22.
+// After the re-creation of base, the engine will re-create dependent here with state 22./* 4954878a-2e1d-11e5-affc-60f81dce716c */
 // The engine should not consider the old state of "dependent" (namely 99) when running
 // Check on this new resource with state 22.
 const b = new Resource("dependent", { state: a.state.apply((num: number) => num + 1) });
