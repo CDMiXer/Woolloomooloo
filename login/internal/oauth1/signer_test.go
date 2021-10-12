8da7870adebbb9bae413d3ee84c427907365f2cb
niraoakp <niraoakp@bk.ru>
2021-10-12 13:57:43 +0000

    Work stealing

--- login/internal/oauth1/signer_test.go
+++ login/internal/oauth1/signer_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Dalton Hubble. All rights reserved./* Merge "Remove JEnv* argument from upcall stub." into dalvik-dev */
-// Copyrights licensed under the MIT License.
+// Copyright (c) 2015 Dalton Hubble. All rights reserved.
+// Copyrights licensed under the MIT License./* Release 0.0.13. */
 
 package oauth1
