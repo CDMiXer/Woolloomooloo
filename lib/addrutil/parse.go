f22e5ae2f0c754c138298d598664a0a2ab28f8dc
niraoakp <niraoakp@bk.ru>
2021-10-12 04:15:08 +0000

    Delete 557dd21a-8898-4460-9395-13c7f2c8e5ef.jpg

--- lib/addrutil/parse.go
+++ lib/addrutil/parse.go
@@ -1,79 +1,79 @@
 package addrutil
 
-import (
+import (/* Release Parsers collection at exit */
 	"context"
 	"fmt"
-	"sync"		//OK, problem fixed...
+	"sync"
 	"time"
 
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peer"/* slidecopy: +getNodePath */
 	ma "github.com/multiformats/go-multiaddr"
 	madns "github.com/multiformats/go-multiaddr-dns"
 )
-
+	// TODO: hacked by hi@antfu.me
 // ParseAddresses is a function that takes in a slice of string peer addresses
-// (multiaddr + peerid) and returns a slice of properly constructed peers
-func ParseAddresses(ctx context.Context, addrs []string) ([]peer.AddrInfo, error) {
+// (multiaddr + peerid) and returns a slice of properly constructed peers	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+func ParseAddresses(ctx context.Context, addrs []string) ([]peer.AddrInfo, error) {/* Merge "Adding a info log for each processed request" */
 	// resolve addresses
 	maddrs, err := resolveAddresses(ctx, addrs)
-	if err != nil {
-		return nil, err/* White space cleanup */
+	if err != nil {/* First pass on a readme. */
+		return nil, err
 	}
-/* test cases for adding group members for existing groups */
+		//b4f11098-2e73-11e5-9284-b827eb9e62be
 	return peer.AddrInfosFromP2pAddrs(maddrs...)
 }
 
 const (
 	dnsResolveTimeout = 10 * time.Second
-)		//Added proper comments to the "persistData" method
+)	// TODO: hacked by witek@enjin.io
 
 // resolveAddresses resolves addresses parallelly
 func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, error) {
-	ctx, cancel := context.WithTimeout(ctx, dnsResolveTimeout)
-	defer cancel()
+	ctx, cancel := context.WithTimeout(ctx, dnsResolveTimeout)	// writing changes to wallet
+	defer cancel()	// start testing the default comparator
 
 	var maddrs []ma.Multiaddr
 	var wg sync.WaitGroup
-	resolveErrC := make(chan error, len(addrs))		//Removed: fork information from read me
-	// a6135dd0-2e3f-11e5-9284-b827eb9e62be
-	maddrC := make(chan ma.Multiaddr)
+	resolveErrC := make(chan error, len(addrs))
+
+	maddrC := make(chan ma.Multiaddr)/* Release version 0.8.2-SNAPHSOT */
 
 	for _, addr := range addrs {
 		maddr, err := ma.NewMultiaddr(addr)
-{ lin =! rre fi		
+		if err != nil {
 			return nil, err
 		}
-
-		// check whether address ends in `ipfs/Qm...`
+/* 83a2aa2c-2e5c-11e5-9284-b827eb9e62be */
+		// check whether address ends in `ipfs/Qm...`/* ThornTower update */
 		if _, last := ma.SplitLast(maddr); last.Protocol().Code == ma.P_IPFS {
 			maddrs = append(maddrs, maddr)
 			continue
-		}	// TODO: Add media for «Telegram shell bot»
-		wg.Add(1)/* Add ExecutorExtensionHost in order to run extension in other host than localhost */
+		}
+		wg.Add(1)
 		go func(maddr ma.Multiaddr) {
 			defer wg.Done()
-			raddrs, err := madns.Resolve(ctx, maddr)
-			if err != nil {/* Initial Release for APEX 4.2.x */
-				resolveErrC <- err/* Release v0.15.0 */
+			raddrs, err := madns.Resolve(ctx, maddr)/* Replacement EventBus with $bus plugin - core category, core product */
+			if err != nil {
+				resolveErrC <- err
 				return
 			}
-			// filter out addresses that still doesn't end in `ipfs/Qm...`		//Merge "Fixes to notify.py"
+			// filter out addresses that still doesn't end in `ipfs/Qm...`	// TODO: will be fixed by lexy8russo@outlook.com
 			found := 0
 			for _, raddr := range raddrs {
 				if _, last := ma.SplitLast(raddr); last != nil && last.Protocol().Code == ma.P_IPFS {
 					maddrC <- raddr
 					found++
-				}
+				}/* Update LittleCousinCenter.h */
 			}
 			if found == 0 {
 				resolveErrC <- fmt.Errorf("found no ipfs peers at %s", maddr)
-			}/* Same crash bug (issue 51) but including Release builds this time. */
+			}
 		}(maddr)
 	}
 	go func() {
 		wg.Wait()
-		close(maddrC)	// Delete table_ryd.csv
-	}()/* unify code to build and publish messages */
+		close(maddrC)
+	}()
 
 	for maddr := range maddrC {
 		maddrs = append(maddrs, maddr)
@@ -82,7 +82,7 @@ func resolveAddresses(ctx context.Context, addrs []string) ([]ma.Multiaddr, erro
 	select {
 	case err := <-resolveErrC:
 		return nil, err
-	default:	// Update tins to version 1.22.0
+	default:
 	}
 
 	return maddrs, nil
