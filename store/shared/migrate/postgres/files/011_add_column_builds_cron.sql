56aacca7596b9220c2a2b85cfae867bac5ba0a9d
niraoakp <niraoakp@bk.ru>
2021-10-12 16:25:58 +0000

    Add in review specifics

--- store/shared/migrate/postgres/files/011_add_column_builds_cron.sql
+++ store/shared/migrate/postgres/files/011_add_column_builds_cron.sql
@@ -1,3 +1,3 @@
 -- name: alter-table-builds-add-column-cron
-
+/* Branched from $/MSBuildExtensionPack/Releases/Archive/Main3.5 */
 ALTER TABLE builds ADD COLUMN build_cron VARCHAR(50) NOT NULL DEFAULT '';
