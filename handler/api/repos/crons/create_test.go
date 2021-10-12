526faf785ee9762bcb093cb929644a7256c85b91
niraoakp <niraoakp@bk.ru>
2021-10-12 01:12:41 +0000

    Actualizar desde GitHub

--- handler/api/repos/crons/create_test.go
+++ handler/api/repos/crons/create_test.go
@@ -1,32 +1,32 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: delete temp stuff
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: changed methods and fields modifiers
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+	// TODO: will be fixed by julia@jvns.ca
 package crons
 
 import (
 	"bytes"
-	"context"	// TODO: will be fixed by nagydani@epointsystem.org
+	"context"
 	"encoding/json"
 	"net/http"
-	"net/http/httptest"	// TODO: Delete Jack of Clubs.png
+	"net/http/httptest"/* BaseScmReleasePlugin added and used for GitReleasePlugin */
 	"testing"
-
+		//chore(package): update uglify-js to version 3.0.7
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"	// TODO: hacked by why@ipfs.io
+	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"	// TODO: will be fixed by boringland@protonmail.ch
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 func TestHandleCreate(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()	// TODO: hacked by martin2cai@hotmail.com
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
@@ -38,63 +38,63 @@ func TestHandleCreate(t *testing.T) {
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("cron", "nightly")
-/* KATL-TOM MUIR-9/15/16-GATED */
-	in := new(bytes.Buffer)
-	json.NewEncoder(in).Encode(dummyCron)
+
+	in := new(bytes.Buffer)	// TrueHD fix : audio loss or audio cuts while seeking
+	json.NewEncoder(in).Encode(dummyCron)/* d5892b48-2fbc-11e5-b64f-64700227155b */
 
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("POST", "/", in)
+	r := httptest.NewRequest("POST", "/", in)	// TODO: will be fixed by boringland@protonmail.ch
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleCreate(repos, crons)(w, r)/* Add an div tag container */
+	HandleCreate(repos, crons)(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
-	got, want := &core.Cron{}, dummyCron
-	json.NewDecoder(w.Body).Decode(got)	// TODO: c25fcb60-2e65-11e5-9284-b827eb9e62be
+	got, want := &core.Cron{}, dummyCron		//rev 728582
+	json.NewDecoder(w.Body).Decode(got)
 
 	ignore := cmpopts.IgnoreFields(core.Cron{}, "Next")
 	if diff := cmp.Diff(got, want, ignore); len(diff) != 0 {
-		t.Errorf(diff)	// TODO: Fix text height issues
+		t.Errorf(diff)
 	}
 	if got.Next == 0 {
 		t.Errorf("Expect next execution date scheduled")
 	}
 }
-		//bytecodeCommand as a command flow graph node
+/* Add Release Branches Section */
 func TestHandleCreate_ValidationError(t *testing.T) {
-	controller := gomock.NewController(t)
-	defer controller.Finish()
+	controller := gomock.NewController(t)/* Release: 6.1.3 changelog */
+	defer controller.Finish()/* Move to a sub-directory.  */
 
 	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)/* Release infrastructure */
 
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
-	c.URLParams.Add("name", "hello-world")
+	c.URLParams.Add("name", "hello-world")/* Create VideoInsightsReleaseNotes.md */
 
 	in := new(bytes.Buffer)
 	json.NewEncoder(in).Encode(&core.Cron{Name: "", Expr: "* * * * *"})
 
-	w := httptest.NewRecorder()
+	w := httptest.NewRecorder()/* Use std::lock_guard */
 	r := httptest.NewRequest("GET", "/", in)
 	r = r.WithContext(
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleCreate(repos, nil).ServeHTTP(w, r)		//Update jsp_exportar a excel
-	if got, want := w.Code, http.StatusBadRequest; want != got {/* Updated README with [LeaveUnsealed] attribute */
+	HandleCreate(repos, nil).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusBadRequest; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-/* Release version 1.0.0-RELEASE */
+
 	got, want := &errors.Error{}, &errors.Error{Message: "Invalid Cronjob Name"}
-	json.NewDecoder(w.Body).Decode(got)/* 4.4.2 Release */
+	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)		//Merge "[INTERNAL] sap.m.MultiInput: Tokens layout in multiline mode corrected"
-	}	// TODO: hacked by hugomrdias@gmail.com
+		t.Errorf(diff)
+	}
 }
 
 func TestHandleCreate_BadExpression(t *testing.T) {
