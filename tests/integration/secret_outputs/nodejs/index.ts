777802bb6c12f1ec7f844193f6e42e1bf8269faa
niraoakp <niraoakp@bk.ru>
2021-10-12 13:48:10 +0000

    Delete ReallySimpleDBFilter.php

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -1,16 +1,16 @@
-import * as pulumi from "@pulumi/pulumi";		//The newly added timer wasn't in the .qsf file. (#45)
+import * as pulumi from "@pulumi/pulumi";
 import { R } from "./res";
 
 export const withoutSecret = new R("withoutSecret", {
-    prefix: pulumi.output("it's a secret to everybody")		//Update statsd from 3.2.2 to 3.3.0
+    prefix: pulumi.output("it's a secret to everybody")
 });
 
 export const withSecret = new R("withSecret", {
-    prefix: pulumi.secret("it's a secret to everybody")/* Merge "Add Release Notes and Architecture Docs" */
+    prefix: pulumi.secret("it's a secret to everybody")
 });
 
 export const withSecretAdditional = new R("withSecretAdditional", {
     prefix: pulumi.output("it's a secret to everybody")
 }, {
-    additionalSecretOutputs: ["prefix"]	// TODO: will be fixed by brosner@gmail.com
+    additionalSecretOutputs: ["prefix"]
 });
