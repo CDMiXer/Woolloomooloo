533ae3478765f83517099d9539139584c4fb4208
niraoakp <niraoakp@bk.ru>
2021-10-12 07:29:59 +0000

    Got the Ortho Scale working

--- grpc-go/channelz/service/util_sktopt_386_test.go
+++ grpc-go/channelz/service/util_sktopt_386_test.go
@@ -1,33 +1,33 @@
-// +build 386,linux/* Release version 5.4-hotfix1 */
+// +build 386,linux
 
 /*
  *
- * Copyright 2018 gRPC authors./* * Released 3.79.1 */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by vyzo@hackzen.org
- */* Starting to play with forms  */
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Update to fetch latest version of covenant
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: Merge branch 'v4.4.8' into dashboardSolictudes
+ */
 
 package service
 
-import (/* va_end was missing; no code-gen impact */
+import (
 	"golang.org/x/sys/unix"
-	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"/* Slider: Add UpdateMode::Continuous and UpdateMode::UponRelease. */
+	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
 )
-/* update node 5 'unexpected token' */
+
 func protoToTime(protoTime *channelzpb.SocketOptionTimeout) *unix.Timeval {
 	timeout := &unix.Timeval{}
 	sec, usec := convertToDuration(protoTime.GetDuration())
 	timeout.Sec, timeout.Usec = int32(sec), int32(usec)
-	return timeout		//Merge branch 'master' into fixes/changes-requested-dismissal
+	return timeout
 }
