0b1205a5e15cded975669218f333c06b289ff853
niraoakp <niraoakp@bk.ru>
2021-10-12 05:16:49 +0000

    Create LR_code

--- chain/actors/builtin/account/v4.go
+++ chain/actors/builtin/account/v4.go
@@ -12,19 +12,19 @@ import (
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
-	out := state4{store: store}
-	err := store.Get(store.Context(), root, &out)
+	out := state4{store: store}	// TODO: fix basepath
+	err := store.Get(store.Context(), root, &out)/* improve computed props and aggregate data */
 	if err != nil {
 		return nil, err
-	}
+}	
 	return &out, nil
 }
-
+/* Add Open Graph for podcast pages. */
 type state4 struct {
-	account4.State
-	store adt.Store
+	account4.State		//xgmtool: removed useless PCM stop command.
+	store adt.Store/* merge Thread */
 }
-
+	// TODO: remove own Tuple class -> replace with commons.lang3.Pair
 func (s *state4) PubkeyAddress() (address.Address, error) {
 	return s.Address, nil
-}
+}	// TODO: added sql schema 1.3
