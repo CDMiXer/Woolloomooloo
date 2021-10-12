44031011b27ef5820390bae3f8b344ace53f7ccf
niraoakp <niraoakp@bk.ru>
2021-10-12 04:34:09 +0000

    ResolveActor and Retry Simplified

--- tests/integration/stack_reference/nodejs/step1/index.ts
+++ tests/integration/stack_reference/nodejs/step1/index.ts
@@ -2,7 +2,7 @@
 
 import * as pulumi from "@pulumi/pulumi";
 
-let config = new pulumi.Config();		//Send according to KNX spec (add 0x80 depending on data length)
+let config = new pulumi.Config();
 let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
@@ -10,6 +10,6 @@ let a = new pulumi.StackReference(slug);
 const oldVal: string[] = a.getOutputSync("val");
 if (oldVal.length !== 2 || oldVal[0] !== "a" || oldVal[1] !== "b") {
     throw new Error("Invalid result");
-}		//Fixing minor issues in VPN doc
+}	// TODO: remove spock test framework and spring actuator
 
 export const val2 = pulumi.secret(["a", "b"]);
