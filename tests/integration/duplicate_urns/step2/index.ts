1fcd9bc09fbc929a62dccf497f0ea914573284eb
niraoakp <niraoakp@bk.ru>
2021-10-12 23:15:01 +0000

    Make gulp task names more semantic

--- tests/integration/duplicate_urns/step2/index.ts
+++ tests/integration/duplicate_urns/step2/index.ts
@@ -1,19 +1,19 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+//	// Update vib.py
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Updated ShopShop skill
-//
+// You may obtain a copy of the License at
+//	// TODO: Set SQLite as default database and convert actual flat to sqlite
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Fixes: http://code.google.com/p/zfdatagrid/issues/detail?id=315 */
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by alessio@tendermint.com
+// Unless required by applicable law or agreed to in writing, software/* Release LastaThymeleaf-0.2.2 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";
-		//Add moveability to the enum container figure
+import { Resource } from "./resource";	// TODO: Comment out unused AudacityProject::OnSplitLabelsToTracks().
+
 // Setup for the next test.
 const a = new Resource("a", { state: 4 });
 
