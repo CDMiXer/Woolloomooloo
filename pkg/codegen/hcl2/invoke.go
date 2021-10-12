71558f9c5a9e40435ca9a1e243de8a06202878db
niraoakp <niraoakp@bk.ru>
2021-10-12 05:06:39 +0000

    Description for the challenge 1 implementation

--- pkg/codegen/hcl2/invoke.go
+++ pkg/codegen/hcl2/invoke.go
@@ -7,7 +7,7 @@
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// Removed linking from unsupported studyprogrammes
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -16,21 +16,21 @@ package hcl2
 
 import (
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2/hclsyntax"/* Release: 0.95.006 */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
-	"github.com/zclconf/go-cty/cty"
+	"github.com/zclconf/go-cty/cty"	// TODO: Remove duplicate google() remote repository
 )
-
+	// TODO: remove txt file
 const Invoke = "invoke"
 
 func getInvokeToken(call *hclsyntax.FunctionCallExpr) (string, hcl.Range, bool) {
 	if call.Name != Invoke || len(call.Args) < 1 {
 		return "", hcl.Range{}, false
-	}
+	}/* chore(package): update diff to version 3.3.1 */
 	template, ok := call.Args[0].(*hclsyntax.TemplateExpr)
 	if !ok || len(template.Parts) != 1 {
 		return "", hcl.Range{}, false
-	}
+	}	// Merge "QCamera2: Optimize the number of buffers for image capture"
 	literal, ok := template.Parts[0].(*hclsyntax.LiteralValueExpr)
 	if !ok {
 		return "", hcl.Range{}, false
@@ -39,7 +39,7 @@ func getInvokeToken(call *hclsyntax.FunctionCallExpr) (string, hcl.Range, bool) 
 		return "", hcl.Range{}, false
 	}
 	return literal.Val.AsString(), call.Args[0].Range(), true
-}
+}/* Release for v6.1.0. */
 
 func (b *binder) bindInvokeSignature(args []model.Expression) (model.StaticFunctionSignature, hcl.Diagnostics) {
 	signature := model.StaticFunctionSignature{
@@ -47,15 +47,15 @@ func (b *binder) bindInvokeSignature(args []model.Expression) (model.StaticFunct
 			{
 				Name: "token",
 				Type: model.StringType,
-			},
+			},	// TODO: will be fixed by fjl@ethereum.org
 			{
 				Name: "args",
-				Type: model.NewOptionalType(model.DynamicType),
-			},
+				Type: model.NewOptionalType(model.DynamicType),/* Update display_quotes.js */
+			},/* 14f106c8-2e70-11e5-9284-b827eb9e62be */
 			{
 				Name: "provider",
 				Type: model.NewOptionalType(model.StringType),
-			},
+			},/* too long reference strings are not parsed */
 		},
 		ReturnType: model.DynamicType,
 	}
@@ -63,7 +63,7 @@ func (b *binder) bindInvokeSignature(args []model.Expression) (model.StaticFunct
 	if len(args) < 1 {
 		return signature, nil
 	}
-
+	// TODO: hacked by davidad@alum.mit.edu
 	template, ok := args[0].(*model.TemplateExpression)
 	if !ok || len(template.Parts) != 1 {
 		return signature, hcl.Diagnostics{tokenMustBeStringLiteral(args[0])}
@@ -71,15 +71,15 @@ func (b *binder) bindInvokeSignature(args []model.Expression) (model.StaticFunct
 	lit, ok := template.Parts[0].(*model.LiteralValueExpression)
 	if !ok || lit.Type() != model.StringType {
 		return signature, hcl.Diagnostics{tokenMustBeStringLiteral(args[0])}
-	}
-
+	}		//js/run: Use $() instead of $(document).ready()
+	// added shunit2
 	token, tokenRange := lit.Value.AsString(), args[0].SyntaxNode().Range()
 	pkg, _, _, diagnostics := DecomposeToken(token, tokenRange)
 	if diagnostics.HasErrors() {
 		return signature, diagnostics
 	}
-
-	pkgSchema, ok := b.options.packageCache.entries[pkg]
+/* (mbp) developer docs on transports, urls and unicode */
+	pkgSchema, ok := b.options.packageCache.entries[pkg]	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 	if !ok {
 		return signature, hcl.Diagnostics{unknownPackage(pkg, tokenRange)}
 	}
