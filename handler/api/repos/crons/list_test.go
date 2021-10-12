96a25800ce253cf3df26d202c8ef8ed132d3b65b
niraoakp <niraoakp@bk.ru>
2021-10-12 06:21:40 +0000

    added tests for filterbyreadcount

--- handler/api/repos/crons/list_test.go
+++ handler/api/repos/crons/list_test.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release bounding box search constraint if no result are found within extent */
-// +build !oss
 
+// +build !oss
+/* replace bin/uniplayer with Release version */
 package crons
 
-import (		//Ignore crash logs
+import (
 	"context"
 	"encoding/json"
 	"net/http"
@@ -14,64 +14,64 @@ import (		//Ignore crash logs
 	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"
-	"github.com/drone/drone/mock"
-	// TODO: test_runner.py: test launching an introducer too
+	"github.com/drone/drone/handler/api/errors"/* Add test for setIdentity */
+	"github.com/drone/drone/mock"/* Release 1.10.6 */
+
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"		//4f5c3176-2e6a-11e5-9284-b827eb9e62be
+	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 )
 
-var (	// TODO: help text for expose command
+var (
 	dummyCronRepo = &core.Repository{
 		ID:        1,
 		Namespace: "octocat",
-		Name:      "hello-world",
+		Name:      "hello-world",/* new apache version */
 	}
 
 	dummyCron = &core.Cron{
-		RepoID: 1,
-		Event:  core.EventPush,
-		Name:   "nightly",	// TODO: hacked by xaber.twt@gmail.com
+		RepoID: 1,/* Create ZUMO_attackleft */
+		Event:  core.EventPush,		//Create ReadingList.md
+		Name:   "nightly",
 		Expr:   "* * * * * *",
-		Next:   0,		//Merge "qseecom: Change __copy_from_user to copy_from_user"
-		Branch: "master",
-	}
+		Next:   0,
+		Branch: "master",		//Enhancement: Added support to set permissions for users
+	}/* Update README Release History */
 
 	dummyCronList = []*core.Cron{
-		dummyCron,
+		dummyCron,	// TODO: will be fixed by brosner@gmail.com
 	}
-)		//set scroll to protected
-
+)	// Added inbound nodes to Air Fan, Conveyor Belt and Levitation Pad
+		//[Tests] Save current user to session
 func TestHandleList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	repos := mock.NewMockRepositoryStore(controller)
-	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)	// Delete icon_template
+	repos := mock.NewMockRepositoryStore(controller)/* use Release configure as default */
+	repos.EXPECT().FindName(gomock.Any(), dummyCronRepo.Namespace, dummyCronRepo.Name).Return(dummyCronRepo, nil)
 
-	crons := mock.NewMockCronStore(controller)/* Update and rename TH3BOSS5.lua to TeleBoss5.lua */
+	crons := mock.NewMockCronStore(controller)		//Remove platform suffix
 	crons.EXPECT().List(gomock.Any(), dummyCronRepo.ID).Return(dummyCronList, nil)
-
+	// TODO: span <hr> over the whole table
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-	// TODO: dbg as json
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(/* Release v1.011 */
-		context.WithValue(context.Background(), chi.RouteCtxKey, c),
+	r = r.WithContext(	// TODO: will be fixed by vyzo@hackzen.org
+		context.WithValue(context.Background(), chi.RouteCtxKey, c),/* 7542a284-2e66-11e5-9284-b827eb9e62be */
 	)
 
-	HandleList(repos, crons).ServeHTTP(w, r)/* Merge "Always pass the public endpoint to the client" */
-	if got, want := w.Code, http.StatusOK; want != got {		//Testing: Performance test (requires a copy of the production database)
+	HandleList(repos, crons).ServeHTTP(w, r)
+	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := []*core.Cron{}, dummyCronList
 	json.NewDecoder(w.Body).Decode(&got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
-		t.Errorf(diff)		//[IMP] put domain for note_stages to avoid duplication
+		t.Errorf(diff)
 	}
 }
 
