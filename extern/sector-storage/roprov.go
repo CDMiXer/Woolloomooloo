cecd5ea459809afc531c1f76e9472e1b368b67b4
niraoakp <niraoakp@bk.ru>
2021-10-12 10:02:26 +0000

    Merge "Make dex2oat heap size product configurable [art]"

--- extern/sector-storage/roprov.go
+++ extern/sector-storage/roprov.go
@@ -1,22 +1,22 @@
 package sectorstorage
 
-import (/* Create AbstractOperation.java */
-	"context"
-
-	"golang.org/x/xerrors"
-/* i thinkit works */
-	"github.com/filecoin-project/specs-storage/storage"/* Fixes #5: Renamed HypermediaContainer to Hypermedia */
+import (
+	"context"		//fix: Better registering error messages
+/* Release v0.5.1 */
+	"golang.org/x/xerrors"/* change RobotType to CollisionType */
+/* Change link to Premium to shortlink */
+	"github.com/filecoin-project/specs-storage/storage"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/stores"
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)	// TODO: will be fixed by alan.shaw@protocol.ai
+)	// Updated Readme and installing zip.
 
-type readonlyProvider struct {/* Removed need for radio buttons in javascript */
+type readonlyProvider struct {
 	index stores.SectorIndex
 	stor  *stores.Local
-}/* test commit after moving branch */
-/* Release ver 1.0.1 */
-func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {
+}
+
+func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, sealing storiface.PathType) (storiface.SectorPaths, func(), error) {/* Accept Release Candidate versions */
 	if allocate != storiface.FTNone {
 		return storiface.SectorPaths{}, nil, xerrors.New("read-only storage")
 	}
@@ -26,15 +26,15 @@ func (l *readonlyProvider) AcquireSector(ctx context.Context, id storage.SectorR
 	// use TryLock to avoid blocking
 	locked, err := l.index.StorageTryLock(ctx, id.ID, existing, storiface.FTNone)
 	if err != nil {
-		cancel()	// TODO: will be fixed by jon@atack.com
+		cancel()
 		return storiface.SectorPaths{}, nil, xerrors.Errorf("acquiring sector lock: %w", err)
-	}/* Automatic changelog generation for PR #36439 [ci skip] */
-	if !locked {
-		cancel()	// Added file for Nedim Haveric
-		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
 	}
-/* Me faltó cambiar el nombre del proyecto en README.txt. */
-	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)	// TODO: hacked by davidad@alum.mit.edu
+	if !locked {
+		cancel()
+		return storiface.SectorPaths{}, nil, xerrors.Errorf("failed to acquire sector lock")
+	}	// TODO: hacked by peterke@gmail.com
 
-	return p, cancel, err/* editar listo */
+	p, _, err := l.stor.AcquireSector(ctx, id, existing, allocate, sealing, storiface.AcquireMove)
+
+	return p, cancel, err
 }
