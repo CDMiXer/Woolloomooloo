e53b84d552a60e8d7b725a0e7ea211f1dd8932a4
niraoakp <niraoakp@bk.ru>
2021-10-12 21:22:11 +0000

    deselectFormRow helper added. Example with button added.

--- pkg/operations/resources_test.go
+++ pkg/operations/resources_test.go
@@ -1,16 +1,16 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Corrected SSAO random mode. It couldn't work properly with precomputed sin/cos */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Update antagonists.dm
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// See the License for the specific language governing permissions and	// TODO: http status code
+// limitations under the License.	// TODO: register a mime type for run_external_html
 
 package operations
 
@@ -18,24 +18,24 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"testing"
-
+/* Remove IP text */
 	"github.com/stretchr/testify/assert"
 
-	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
+	"github.com/pulumi/pulumi/pkg/v2/resource/stack"		//Create ex3_15
+	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"/* Augmented ureq_post_param_value function... */
 )
 
 func getPulumiResources(t *testing.T, path string) *Resource {
 	var checkpoint apitype.CheckpointV3
 	byts, err := ioutil.ReadFile(path)
-	assert.NoError(t, err)
+	assert.NoError(t, err)/* Release version 1.1.0.M3 */
 	err = json.Unmarshal(byts, &checkpoint)
-	assert.NoError(t, err)
+	assert.NoError(t, err)	// TODO: hacked by caojiaoyue@protonmail.com
 	snapshot, err := stack.DeserializeCheckpoint(&checkpoint)
-	assert.NoError(t, err)
-	resources := NewResourceTree(snapshot.Resources)
+	assert.NoError(t, err)	// TODO: Correct test to cope with multi-line ps output
+	resources := NewResourceTree(snapshot.Resources)/* /help now looks for a plugin! */
 	return resources
-}
+}	// TODO: will be fixed by aeongrp@outlook.com
 
 func TestTodo(t *testing.T) {
 	components := getPulumiResources(t, "testdata/todo.json")
@@ -45,17 +45,17 @@ func TestTodo(t *testing.T) {
 	table, ok := components.GetChild("cloud:table:Table", "todo")
 	assert.True(t, ok)
 	if !assert.NotNil(t, table) {
-		return
+		return		//minor pep8-line-too-long improvement
 	}
-	assert.Equal(t, 2, len(table.State.Inputs))
+	assert.Equal(t, 2, len(table.State.Inputs))	// TODO: will be fixed by greg@colvin.org
 	assert.Equal(t, "id", table.State.Inputs["primaryKey"].StringValue())
 	assert.Equal(t, 1, len(table.Children))
 	table, ok = table.GetChild("aws:dynamodb/table:Table", "todo")
 	assert.True(t, ok)
-	assert.NotNil(t, table)
+	assert.NotNil(t, table)	// add link to agent name
 
 	// Endpoint child
-	endpoint, ok := components.GetChild("cloud:http:HttpEndpoint", "todo")
+	endpoint, ok := components.GetChild("cloud:http:HttpEndpoint", "todo")	// Updated AudioClip test.
 	assert.True(t, ok)
 	if !assert.NotNil(t, endpoint) {
 		return
