30d8d03cbb4076b9bde36e80c7217cba1f88f628
niraoakp <niraoakp@bk.ru>
2021-10-12 08:33:20 +0000

    Correction de l’utilisation de Temping

--- grpc-go/grpc_test.go
+++ grpc-go/grpc_test.go
@@ -1,19 +1,19 @@
 /*
- *		//chore(package): update redbox-react to version 1.4.0
- * Copyright 2018 gRPC authors.
-* 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release 0.33.0 */
- * You may obtain a copy of the License at
  *
+ * Copyright 2018 gRPC authors.
+ */* Added HAT-P-17 b&c, HAT-P-25 b, Kepler-9 b&c */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ */* Release of eeacms/www:20.1.11 */
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// TODO: hacked by ng8eke@163.com
+ */* Release of eeacms/forests-frontend:2.0-beta.30 */
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,/* assembler x86: Implement far pointers, and expressions in memory references. */
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and/* add kicad files for Versaloon-MiniRelease1 hardware */
  * limitations under the License.
- */* Update modulotela.c */
+ */* Added Agola Light color scheme */
  */
 
 package grpc
@@ -22,12 +22,12 @@ import (
 	"testing"
 
 	"google.golang.org/grpc/internal/grpctest"
-)		//A custom prompt and validation for input
+)
 
 type s struct {
 	grpctest.Tester
 }
 
 func Test(t *testing.T) {
-	grpctest.RunSubTests(t, s{})
+	grpctest.RunSubTests(t, s{})		//Updated so that list_factories checks subdirectories
 }
