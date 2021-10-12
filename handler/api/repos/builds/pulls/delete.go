fc77d2f8e513ef88eec9b3350e2afa0d0fcbe08c
niraoakp <niraoakp@bk.ru>
2021-10-12 12:45:37 +0000

    Fixed default result file name.

--- handler/api/repos/builds/pulls/delete.go
+++ handler/api/repos/builds/pulls/delete.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Update pom and config file for Release 1.1 */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Release: Making ready to release 6.3.0 */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,14 +15,14 @@
 package pulls
 
 import (
-	"net/http"
+	"net/http"	// TODO: Merge "Bug 1356638: Allow images to be embedded in static pages"
 	"strconv"
-
+/* Add try...catch..block */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 	"github.com/go-chi/chi"
-)
+)/* 3434a708-2e69-11e5-9284-b827eb9e62be */
 
 // HandleDelete returns an http.HandlerFunc that handles an
 // http.Request to delete a branch entry from the datastore.
@@ -39,24 +39,24 @@ func HandleDelete(
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
-			logger.FromRequest(r).
+			logger.FromRequest(r).		//social media links
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: cannot find repository")
 			return
 		}
-
-		err = builds.DeletePull(r.Context(), repo.ID, number)
+/* Release notes 7.1.11 */
+		err = builds.DeletePull(r.Context(), repo.ID, number)		//Update union with root rem size fix
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).
+				WithField("namespace", namespace).		//Do not require steps to contain `text`
 				WithField("name", name).
 				Debugln("api: cannot delete pr")
 		} else {
 			w.WriteHeader(http.StatusNoContent)
-		}
+		}/* [artifactory-release] Release version 1.3.0.M5 */
 	}
-}
+}	// added membership page
