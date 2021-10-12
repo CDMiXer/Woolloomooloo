7ff5e7d35a787c5b68aec1503ef06f347a92fcfe
niraoakp <niraoakp@bk.ru>
2021-10-12 15:02:43 +0000

    Ajout de MutablePrefixTree.

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* debian: Release 0.11.8-1 */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0		//Create database-system-overview.md
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+		//Additional update to README
+package version
 
-package version/* re-code install & uninstall module */
-
-// Version is initialized by the Go linker to contain the semver of this build.
+// Version is initialized by the Go linker to contain the semver of this build./* 8a817d74-2e51-11e5-9284-b827eb9e62be */
 var Version string
