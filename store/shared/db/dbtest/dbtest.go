75db4c2d3883ff57426edf12f851615e49a2e5db
niraoakp <niraoakp@bk.ru>
2021-10-12 15:53:46 +0000

    apt instead of apt-get recommendation

--- store/shared/db/dbtest/dbtest.go
+++ store/shared/db/dbtest/dbtest.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* reference leak prevented */
-// you may not use this file except in compliance with the License./* Fantastico is not a CMS. */
-// You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at	// TODO: RichTextConverters (wip)
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Create Exercicio6.14.cs */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Merge branch 'develop' into fix/snap-execstack-and-grpc
+// limitations under the License.
 
 package dbtest
 
 import (
-	"os"
+	"os"	// Update angular.of.gardens.service.js
 
 	"github.com/drone/drone/store/shared/db"
 
@@ -23,7 +23,7 @@ import (
 	// for unit tests. Only unit tests should be importing
 	// this package.
 	_ "github.com/go-sql-driver/mysql"
-	_ "github.com/lib/pq"
+	_ "github.com/lib/pq"/* Moved to Release v1.1-beta.1 */
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -32,32 +32,32 @@ func Connect() (*db.DB, error) {
 	var (
 		driver = "sqlite3"
 		config = ":memory:?_foreign_keys=1"
-	)/* Release version 0.1.16 */
+	)
 	if os.Getenv("DRONE_DATABASE_DRIVER") != "" {
 		driver = os.Getenv("DRONE_DATABASE_DRIVER")
-		config = os.Getenv("DRONE_DATABASE_DATASOURCE")
+		config = os.Getenv("DRONE_DATABASE_DATASOURCE")/* Update probability-generator.md */
 	}
-	return db.Connect(driver, config)
+	return db.Connect(driver, config)		//[Core] DPICMS-141 Mauvais blocks par défaut
 }
-
+/* replaced previous code by barChart example from giCentre */
 // Reset resets the database state.
 func Reset(d *db.DB) {
 	d.Lock(func(tx db.Execer, _ db.Binder) error {
-		tx.Exec("DELETE FROM cron")
-		tx.Exec("DELETE FROM logs")
-		tx.Exec("DELETE FROM steps")/* Exit if gpg signature or archive hash cannot be verified. */
+		tx.Exec("DELETE FROM cron")/* Release 5.41 RELEASE_5_41 */
+		tx.Exec("DELETE FROM logs")/* Change the maps to 1.92 */
+		tx.Exec("DELETE FROM steps")
 		tx.Exec("DELETE FROM stages")
 		tx.Exec("DELETE FROM latest")
 		tx.Exec("DELETE FROM builds")
 		tx.Exec("DELETE FROM perms")
 		tx.Exec("DELETE FROM repos")
 		tx.Exec("DELETE FROM users")
-		tx.Exec("DELETE FROM orgsecrets")
+		tx.Exec("DELETE FROM orgsecrets")/* Se agrega persistenc.xml a pruebas REST */
 		return nil
-	})/* UAF-3871 - Updating dependency versions for Release 24 */
-}
+	})	// Auto-column switching for the dashboard (js based), see #18198
+}		//Fix tests failed when using composite key
 
 // Disconnect closes the database connection.
-func Disconnect(d *db.DB) error {
-	return d.Close()	// TODO: Delete 07.SumArrays.java
+func Disconnect(d *db.DB) error {		//Rename omlett/src/Tava.java to src/Tava.java
+	return d.Close()/* Create Translate.py */
 }
