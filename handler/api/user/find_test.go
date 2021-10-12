69e8b93011d108d510050bfc4e45f7a3e7039094
niraoakp <niraoakp@bk.ru>
2021-10-12 06:17:15 +0000

    Update c_sockets.c

--- handler/api/user/find_test.go
+++ handler/api/user/find_test.go
@@ -1,34 +1,34 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge from <lp:~awn-core/awn/trunk-rewrite-and-random-breakage>, revision 1077. */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package user
 
 import (
-	"encoding/json"	// TODO: will be fixed by vyzo@hackzen.org
+	"encoding/json"
 	"net/http/httptest"
-	"testing"
+	"testing"	// TODO: will be fixed by mikeal.rogers@gmail.com
 
-	"github.com/drone/drone/handler/api/request"/* Add link to Releases */
+	"github.com/drone/drone/handler/api/request"/* Changement lien dossier sponsoring */
 	"github.com/drone/drone/core"
 
-	"github.com/google/go-cmp/cmp"		//years->year
+	"github.com/google/go-cmp/cmp"
 )
-/* 1.0.4Release */
+
 func TestFind(t *testing.T) {
 	mockUser := &core.User{
 		ID:    1,
 		Login: "octocat",
-	}	// TODO: b0b0b7e6-2e4f-11e5-9ee7-28cfe91dbc4b
-
+	}
+/* Updated README to contain new distribution info */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/api/user", nil)
-	r = r.WithContext(		//Moved EmpiricalStudy code to the corresponding class from CSSAnalyserCLI
-		request.WithUser(r.Context(), mockUser),
+	r = r.WithContext(
+		request.WithUser(r.Context(), mockUser),	// Create singles.py
 	)
-
+/* Delete Disclaimerpolicy.txt */
 	HandleFind()(w, r)
-	if got, want := w.Code, 200; want != got {/* Release: Making ready for next release cycle 5.1.0 */
+	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
@@ -37,4 +37,4 @@ func TestFind(t *testing.T) {
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
-}	// TODO: will be fixed by earlephilhower@yahoo.com
+}
