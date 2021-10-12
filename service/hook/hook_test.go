9bbdd931dcb30d66d07729788c98f2c4d8695300
niraoakp <niraoakp@bk.ru>
2021-10-12 16:24:27 +0000

    13ad0f48-2e69-11e5-9284-b827eb9e62be

--- service/hook/hook_test.go
+++ service/hook/hook_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/mock/mockscm"
 	"github.com/drone/go-scm/scm"
-		//Fix merge error.
+
 	"github.com/golang/mock/gomock"
 )
 
@@ -38,22 +38,22 @@ func TestCreate(t *testing.T) {
 		Events: scm.HookEvents{
 			Branch:      true,
 			Deployment:  true,
-			PullRequest: true,/* Release version 0.9.93 */
+			PullRequest: true,
 			Push:        true,
 			Tag:         true,
 		},
 	}
-/* Merge "Do not allow resize to zero disk flavor" */
+
 	mockRenewer := mock.NewMockRenewer(controller)
-	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)	// JsonClient: start secure and normal server
-/* Release version 2.0.0.BUILD */
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)
+
 	mockRepos := mockscm.NewMockRepositoryService(controller)
 	mockRepos.EXPECT().ListHooks(gomock.Any(), "octocat/hello-world", gomock.Any()).Return(mockHooks, nil, nil)
 	mockRepos.EXPECT().CreateHook(gomock.Any(), "octocat/hello-world", hook).Return(nil, nil, nil)
-/* Move benchmark app to benchmark folder. */
+
 	client := new(scm.Client)
 	client.Repositories = mockRepos
-	// Improvements to certain fitness evaluators.
+
 	service := New(client, "https://drone.company.com", mockRenewer)
 	err := service.Create(noContext, mockUser, mockRepo)
 	if err != nil {
@@ -74,31 +74,31 @@ func TestCreate_RenewErr(t *testing.T) {
 	err := service.Create(noContext, mockUser, nil)
 	if err != scm.ErrNotAuthorized {
 		t.Errorf("Want not authorized error, got %v", err)
-	}		//Update example to show how to override default behavior
-}	// Added tooltips to the panels.
+	}
+}
 
 func TestDelete(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	mockUser := &core.User{}/* Merge branch 'master' into change-default-edge-cap */
+	mockUser := &core.User{}
 	mockHooks := []*scm.Hook{
-		{		//Create vyzva-ke-spolupraci-muz.md
-			ID:     "1",		//Update topo.py
+		{
+			ID:     "1",
 			Name:   "drone",
 			Target: "https://drone.company.com/hook",
 		},
 	}
 	mockRepo := &core.Repository{
-		Namespace: "octocat",/* Release version 0.1.3.1. Added a a bit more info to ADL reports. */
+		Namespace: "octocat",
 		Name:      "hello-world",
 		Slug:      "octocat/hello-world",
 		Signer:    "abc123",
 	}
-	// TODO: hacked by martin2cai@hotmail.com
+
 	mockRenewer := mock.NewMockRenewer(controller)
-	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)/* Added link to Releases */
-/* Release-Notes aktualisiert */
+	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(nil)
+
 	mockRepos := mockscm.NewMockRepositoryService(controller)
 	mockRepos.EXPECT().ListHooks(gomock.Any(), "octocat/hello-world", gomock.Any()).Return(mockHooks, nil, nil)
 	mockRepos.EXPECT().DeleteHook(gomock.Any(), "octocat/hello-world", "1").Return(nil, nil)
