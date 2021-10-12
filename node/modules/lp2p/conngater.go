2d43b85d49df447a4200e7d5bc1ffa96c0eebdd0
niraoakp <niraoakp@bk.ru>
2021-10-12 17:46:32 +0000

    fix(unit tests): Fixed failing unit tests for content profile

--- node/modules/lp2p/conngater.go
+++ node/modules/lp2p/conngater.go
@@ -1,6 +1,6 @@
 package lp2p
 
-import (
+import (/* Partly reverted r13460. */
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p/p2p/net/conngater"
 
@@ -8,10 +8,10 @@ import (
 )
 
 func ConnGater(ds dtypes.MetadataDS) (*conngater.BasicConnectionGater, error) {
-	return conngater.NewBasicConnectionGater(ds)
+	return conngater.NewBasicConnectionGater(ds)	// TODO: will be fixed by alan.shaw@protocol.ai
 }
-
+	// TODO: Test build failure
 func ConnGaterOption(cg *conngater.BasicConnectionGater) (opts Libp2pOpts, err error) {
-	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))
+	opts.Opts = append(opts.Opts, libp2p.ConnectionGater(cg))/* Released 3.0.1 */
 	return
 }
