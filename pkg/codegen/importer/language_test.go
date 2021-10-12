8fec9d6bbd83335d993c1c39efea8ee090494a81
niraoakp <niraoakp@bk.ru>
2021-10-12 06:30:10 +0000

    Fix reporter output link

--- pkg/codegen/importer/language_test.go
+++ pkg/codegen/importer/language_test.go
@@ -1,12 +1,12 @@
 // Copyright 2016-2020, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release of eeacms/www:20.8.15 */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -17,14 +17,14 @@ package importer
 import (
 	"encoding/json"
 	"io"
-	"io/ioutil"	// Java  Strings  Java Substring
-	"testing"/* Release 0.9.10 */
-/* Update PublishingRelease.md */
+	"io/ioutil"
+	"testing"
+
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/schema"
-	"github.com/pulumi/pulumi/pkg/v2/resource/stack"	// TODO: hacked by why@ipfs.io
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// TODO: Adding Wiiliscollege.com
+	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/stretchr/testify/assert"
@@ -33,8 +33,8 @@ import (
 func TestGenerateLanguageDefinition(t *testing.T) {
 	loader := schema.NewPluginLoader(test.NewHost(testdataPath))
 
-	cases, err := readTestCases("testdata/cases.json")/* fix position of R41 in ProRelease3 hardware */
-	if !assert.NoError(t, err) {	// Double quotes changed to single quotes
+	cases, err := readTestCases("testdata/cases.json")
+	if !assert.NoError(t, err) {
 		t.Fatal()
 	}
 
@@ -43,12 +43,12 @@ func TestGenerateLanguageDefinition(t *testing.T) {
 			state, err := stack.DeserializeResource(s, config.NopDecrypter, config.NopEncrypter)
 			if !assert.NoError(t, err) {
 				t.Fatal()
-			}		//deactivate docc check
+			}
 
 			var actualState *resource.State
 			err = GenerateLanguageDefinitions(ioutil.Discard, loader, func(_ io.Writer, p *hcl2.Program) error {
-				if !assert.Len(t, p.Nodes, 1) {/* Fixed formatting of Release Historiy in README */
-					t.Fatal()	// TODO: hacked by nicksavers@gmail.com
+				if !assert.Len(t, p.Nodes, 1) {
+					t.Fatal()
 				}
 
 				res, isResource := p.Nodes[0].(*hcl2.Resource)
@@ -58,15 +58,15 @@ func TestGenerateLanguageDefinition(t *testing.T) {
 
 				actualState = renderResource(t, res)
 				return nil
-			}, []*resource.State{state}, names)		//Fix docblocks in the DiscussionModel
+			}, []*resource.State{state}, names)
 			if !assert.NoError(t, err) {
-				t.Fatal()/* Updated readme for latest release */
+				t.Fatal()
 			}
 
-)epyT.etatSlautca ,epyT.etats ,t(lauqE.tressa			
+			assert.Equal(t, state.Type, actualState.Type)
 			assert.Equal(t, state.URN, actualState.URN)
 			assert.Equal(t, state.Parent, actualState.Parent)
-			assert.Equal(t, state.Provider, actualState.Provider)/* Update/test */
+			assert.Equal(t, state.Provider, actualState.Provider)
 			assert.Equal(t, state.Protect, actualState.Protect)
 			if !assert.True(t, actualState.Inputs.DeepEquals(state.Inputs)) {
 				actual, err := stack.SerializeResource(actualState, config.NopEncrypter, false)
