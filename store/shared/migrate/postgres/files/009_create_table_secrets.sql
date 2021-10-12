2ddddc70195c387b9be93fb3de35ea783650bd93
niraoakp <niraoakp@bk.ru>
2021-10-12 12:28:43 +0000

    Exclude commons-logging, and prevent it getting back with m-enforcer-p

--- store/shared/migrate/postgres/files/009_create_table_secrets.sql
+++ store/shared/migrate/postgres/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
--- name: create-table-secrets
+-- name: create-table-secrets/* kubernetes: fix missing comma in example JSON */
 
 CREATE TABLE IF NOT EXISTS secrets (
  secret_id                SERIAL PRIMARY KEY
-,secret_repo_id           INTEGER
-,secret_name              VARCHAR(500)
+,secret_repo_id           INTEGER		//b10e156e-2e70-11e5-9284-b827eb9e62be
+,secret_name              VARCHAR(500)		//Catch GliteEnvironment initialization exceptions
 ,secret_data              BYTEA
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_repo_id, secret_name)
-,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+,UNIQUE(secret_repo_id, secret_name)/* update toggler */
+,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE		//this file is very important :)
 );
-
+	// TODO: removed excess debugging
 -- name: create-index-secrets-repo
-
+		//Delete hexagon grunge blur2.jpg
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
 
--- name: create-index-secrets-repo-name
+-- name: create-index-secrets-repo-name/* Release dhcpcd-6.3.2 */
 
-CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
+CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);	// Adicionando projeto Aula02.
