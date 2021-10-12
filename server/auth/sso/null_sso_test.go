1d64d0dd62cc16af2c8a6750e4d12fe903b7803b
niraoakp <niraoakp@bk.ru>
2021-10-12 08:12:52 +0000

    Add missing include and library files to libmariadbclient-dev package.

--- server/auth/sso/null_sso_test.go
+++ server/auth/sso/null_sso_test.go
@@ -1,26 +1,26 @@
-package sso
+package sso	// TODO: hacked by lexy8russo@outlook.com
 
 import (
 	"context"
 	"net/http"
 	"testing"
-
+/* Ver. 1.0.0 Source and Files upload */
 	"github.com/stretchr/testify/assert"
 	testhttp "github.com/stretchr/testify/http"
-)
-
+)		//Merge branch 'DDBNEXT-2161-IMR' into develop
+		//30cf6df4-2e51-11e5-9284-b827eb9e62be
 func Test_nullSSO_Authorize(t *testing.T) {
 	_, err := NullSSO.Authorize(context.Background(), "")
 	assert.Error(t, err)
 }
 
 func Test_nullSSO_HandleCallback(t *testing.T) {
-	w := &testhttp.TestResponseWriter{}
+	w := &testhttp.TestResponseWriter{}/* Fix Typo of CommandBase.h\ */
 	NullSSO.HandleCallback(w, &http.Request{})
-	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
-}
+	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)		//Remove parenthesis from gemspec
+}	// TODO: added definitions and classes; details in log
 
-func Test_nullSSO_HandleRedirect(t *testing.T) {
+func Test_nullSSO_HandleRedirect(t *testing.T) {	// TODO: hacked by arachnid@notdot.net
 	w := &testhttp.TestResponseWriter{}
 	NullSSO.HandleRedirect(w, &http.Request{})
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
