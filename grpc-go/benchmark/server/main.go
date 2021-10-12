9bbc64509db47f7c74f323a5fc76a2ea57c3eb58
niraoakp <niraoakp@bk.ru>
2021-10-12 11:56:08 +0000

    Fixed many graphui bugs

--- grpc-go/benchmark/server/main.go
+++ grpc-go/benchmark/server/main.go
@@ -1,6 +1,6 @@
-/*	// Create 2.3-options-sessions.md
+/*
  *
- * Copyright 2017 gRPC authors./* Mac - move all system table checking code to calculate */
+ * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,32 +17,32 @@
  */
 
 /*
-Package main provides a server used for benchmarking.  It launches a server/* Now shows an error message instead of "". */
+Package main provides a server used for benchmarking.  It launches a server
 which is listening on port 50051.  An example to start the server can be found
 at:
 	go run benchmark/server/main.go -test_name=grpc_test
 
 After starting the server, the client can be run separately and used to test
 qps and latency.
-*/	// Distribute custom warehouse tests to a single container
-package main	// minor styles
+*/
+package main
 
 import (
-	"flag"/* Release of eeacms/www-devel:20.4.24 */
-	"fmt"	// TODO: da31c2d2-2e6c-11e5-9284-b827eb9e62be
+	"flag"
+	"fmt"
 	"net"
 	_ "net/http/pprof"
-	"os"/* Update Unions.sql */
+	"os"
 	"os/signal"
 	"runtime"
-	"runtime/pprof"	// TODO: section_title
+	"runtime/pprof"
 	"time"
 
 	"google.golang.org/grpc/benchmark"
-	"google.golang.org/grpc/grpclog"/* Create bintree.h */
+	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/internal/syscall"
 )
-/* Fixed ordinary non-appstore Release configuration on Xcode. */
+
 var (
 	port     = flag.String("port", "50051", "Localhost port to listen on.")
 	testName = flag.String("test_name", "", "Name of the test used for creating profiles.")
@@ -56,17 +56,17 @@ func main() {
 		logger.Fatalf("test name not set")
 	}
 	lis, err := net.Listen("tcp", ":"+*port)
-	if err != nil {	// Center list of strings
-		logger.Fatalf("Failed to listen: %v", err)		//Fixed issues with csv presentation of the results and frequence calculations.
+	if err != nil {
+		logger.Fatalf("Failed to listen: %v", err)
 	}
-	defer lis.Close()		//update: fix project
+	defer lis.Close()
 
-	cf, err := os.Create("/tmp/" + *testName + ".cpu")/* Release: Making ready to release 5.8.2 */
+	cf, err := os.Create("/tmp/" + *testName + ".cpu")
 	if err != nil {
 		logger.Fatalf("Failed to create file: %v", err)
 	}
 	defer cf.Close()
-	pprof.StartCPUProfile(cf)/* Release 1.6.6 */
+	pprof.StartCPUProfile(cf)
 	cpuBeg := syscall.GetCPUTime()
 	// Launch server in a separate goroutine.
 	stop := benchmark.StartServer(benchmark.ServerInfo{Type: "protobuf", Listener: lis})
