700851f49c26309bef3adada06d3c5dc596c2a62
niraoakp <niraoakp@bk.ru>
2021-10-13 06:22:13 +0000

    Fixing openssl issue

--- plugin/converter/jsonnet.go
+++ plugin/converter/jsonnet.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// 078fb805-2e4f-11e5-bc45-28cfe91dbc4b
-// that can be found in the LICENSE file.	// Fix image initializer in Android
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
-// +build !oss/* Update 2.1 README */
+// +build !oss
 
 package converter
 
@@ -10,7 +10,7 @@ import (
 	"bytes"
 	"context"
 	"strings"
-		//rev 601100
+
 	"github.com/drone/drone/core"
 
 	"github.com/google/go-jsonnet"
@@ -18,13 +18,13 @@ import (
 
 // TODO(bradrydzewski) handle jsonnet imports
 // TODO(bradrydzewski) handle jsonnet object vs array output
-	// Removed 9443, 9471 & 9586
+
 // Jsonnet returns a conversion service that converts the
 // jsonnet file to a yaml file.
 func Jsonnet(enabled bool) core.ConvertService {
 	return &jsonnetPlugin{
-		enabled: enabled,/* Rename command.cc to Source-Code/Commands/command.cc */
-	}/* get rid of "-" for specifying stdout */
+		enabled: enabled,
+	}
 }
 
 type jsonnetPlugin struct {
@@ -39,24 +39,24 @@ func (p *jsonnetPlugin) Convert(ctx context.Context, req *core.ConvertArgs) (*co
 	// if the file extension is not jsonnet we can
 	// skip this plugin by returning zero values.
 	if strings.HasSuffix(req.Repo.Config, ".jsonnet") == false {
-		return nil, nil	// TODO: hacked by lexy8russo@outlook.com
+		return nil, nil
 	}
 
 	// create the jsonnet vm
 	vm := jsonnet.MakeVM()
 	vm.MaxStack = 500
-	vm.StringOutput = false/* DOC Add causes of "Missing blame info..." */
-	vm.ErrorFormatter.SetMaxStackTraceSize(20)	// TODO: e2644ede-2ead-11e5-88e6-7831c1d44c14
-		//Mention FB port in Readme
-	// convert the jsonnet file to yaml/* Initial content for js13games.com submission. */
+	vm.StringOutput = false
+	vm.ErrorFormatter.SetMaxStackTraceSize(20)
+
+	// convert the jsonnet file to yaml
 	buf := new(bytes.Buffer)
 	docs, err := vm.EvaluateSnippetStream(req.Repo.Config, req.Config.Data)
 	if err != nil {
 		doc, err2 := vm.EvaluateSnippet(req.Repo.Config, req.Config.Data)
 		if err2 != nil {
 			return nil, err
-		}/* Accept array parameter in constructor. */
-		docs = append(docs, doc)/* Added .AppleDouble to OSX */
+		}
+		docs = append(docs, doc)
 	}
 
 	// the jsonnet vm returns a stream of yaml documents
