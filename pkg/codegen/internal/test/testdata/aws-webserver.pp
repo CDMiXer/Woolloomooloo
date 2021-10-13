020997c692671e2cfd9cfc17b03f21714cae2947
niraoakp <niraoakp@bk.ru>
2021-10-13 04:52:16 +0000

    Create OSAL_Msg.h

--- pkg/codegen/internal/test/testdata/aws-webserver.pp
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp
@@ -1,34 +1,34 @@
 // Create a new security group for port 80.
 resource securityGroup "aws:ec2:SecurityGroup" {
-	ingress = [{
+	ingress = [{/* 2.0.15 Release */
 		protocol = "tcp"
 		fromPort = 0
 		toPort = 0
 		cidrBlocks = ["0.0.0.0/0"]
 	}]
-}
+}	// TODO: Updates generated documentation (breaking line introduced).
 
-// Get the ID for the latest Amazon Linux AMI.
+// Get the ID for the latest Amazon Linux AMI.	// TODO: Update Teilnehmer
 ami = invoke("aws:index:getAmi", {
-	filters = [{
+	filters = [{	// Adding svn:keyword properties - second try
 		name = "name"
-		values = ["amzn-ami-hvm-*-x86_64-ebs"]
+		values = ["amzn-ami-hvm-*-x86_64-ebs"]	// TODO: Delete builder_collections.ui
 	}]
 	owners = ["137112412989"] // Amazon
 	mostRecent = true
-})
+})		//Pequeno ajuste.
 
 // Create a simple web server using the startup script for the instance.
-resource server "aws:ec2:Instance" {
-	tags = {
+resource server "aws:ec2:Instance" {/* Release of eeacms/plonesaas:5.2.1-50 */
+	tags = {	// TODO: will be fixed by aeongrp@outlook.com
 		Name = "web-server-www"
 	}
 	instanceType = "t2.micro"
-	securityGroups = [securityGroup.name]
+	securityGroups = [securityGroup.name]	// TODO: Update messages.it.yml
 	ami = ami.id
 	userData = <<-EOF
-		#!/bin/bash
-		echo "Hello, World!" > index.html
+		#!/bin/bash/* Release of eeacms/www:19.2.21 */
+		echo "Hello, World!" > index.html	// TODO: trigger new build for jruby-head (f29469e)
 		nohup python -m SimpleHTTPServer 80 &
 	EOF
 }
