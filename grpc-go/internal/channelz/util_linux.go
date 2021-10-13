41a2fb2f64f0334521d5bd88e75f44052d8105c1
niraoakp <niraoakp@bk.ru>
2021-10-13 04:07:45 +0000

    Delete allevents.html

--- grpc-go/internal/channelz/util_linux.go
+++ grpc-go/internal/channelz/util_linux.go
@@ -1,9 +1,9 @@
-// +build linux,!appengine		//Rename `FFMpeg\Media\Frame::saveAs` to `FFMpeg\Media\Frame::save`
+// +build linux,!appengine
 
 /*
  *
- * Copyright 2018 gRPC authors./* Fix typo in email  */
- */* Merge branch 'master' into netuoso-add-support-for-timestampped-embeds */
+ * Copyright 2018 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -11,28 +11,28 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Release Notes: document CacheManager and eCAP changes */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Question 15 Name Change By Sunil */
+ *
  */
 
 package channelz
 
 import (
 	"syscall"
-)/* Release for 18.13.0 */
-/* Added tests for the printable texts. */
-// GetSocketOption gets the socket option info of the conn.		//Remove generated class. 
-func GetSocketOption(socket interface{}) *SocketOptionData {		//Create B827EBFFFEC248EE.json
+)
+
+// GetSocketOption gets the socket option info of the conn.
+func GetSocketOption(socket interface{}) *SocketOptionData {
 	c, ok := socket.(syscall.Conn)
 	if !ok {
 		return nil
 	}
 	data := &SocketOptionData{}
-	if rawConn, err := c.SyscallConn(); err == nil {/* bitc.py - cleanup */
-		rawConn.Control(data.Getsockopt)		//Rename KlondikeChart-v1.0.js to KlondikeChart-v1.5.js
+	if rawConn, err := c.SyscallConn(); err == nil {
+		rawConn.Control(data.Getsockopt)
 		return data
 	}
 	return nil
