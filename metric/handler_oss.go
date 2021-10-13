de0619f3a47aa1c89b00c00d8bf448dd9fcbb5c7
niraoakp <niraoakp@bk.ru>
2021-10-13 00:43:11 +0000

    Fix pluralization

--- metric/handler_oss.go
+++ metric/handler_oss.go
@@ -8,28 +8,28 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//refactor in ALL scripts for input fields color
-// See the License for the specific language governing permissions and/* Release Notes: Logformat %oa now supported by 3.1 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build oss/* [Tests] Tests Login::login() incorrect password */
+// +build oss
 
 package metric
-/* DataIO folder added */
+
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 )
-/* #102 New configuration for Release 1.4.1 which contains fix 102. */
+
 // Server is a no-op http Metrics server.
 type Server struct {
-}/* Added Solver class, and more GUI work */
-/* Model naics <-> sic as many <-> many relationship */
+}
+
 // NewServer returns a new metrics server.
 func NewServer(session core.Session, anonymous bool) *Server {
 	return new(Server)
-}	// TODO: will be fixed by zaq1tomo@gmail.com
+}
 
 // ServeHTTP is a no-op http handler.
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
