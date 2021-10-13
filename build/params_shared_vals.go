9affa2c9c20644e2e27f8c8ce93ee69e7ba33b2a
niraoakp <niraoakp@bk.ru>
2021-10-13 07:09:56 +0000

    Merge branch 'master' into disable-deploy

--- build/params_shared_vals.go
+++ build/params_shared_vals.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/network"/* Improved Pushover send service test coverage. */
+	"github.com/filecoin-project/go-state-types/network"
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 
@@ -17,7 +17,7 @@ import (
 
 // /////
 // Storage
-	// Debugged overlap function between tree masks
+
 const UnixfsChunkSize uint64 = 1 << 20
 const UnixfsLinksPerLevel = 1024
 
@@ -32,22 +32,22 @@ const ActorUpgradeNetworkVersion = network.Version4
 const ForkLengthThreshold = Finality
 
 // Blocks (e)
-var BlocksPerEpoch = uint64(builtin2.ExpectedLeadersPerEpoch)		//Update TODO for --production
+var BlocksPerEpoch = uint64(builtin2.ExpectedLeadersPerEpoch)
 
 // Epochs
 const Finality = policy.ChainFinality
-const MessageConfidence = uint64(5)/* Release version 1.2.0.M1 */
+const MessageConfidence = uint64(5)
 
 // constants for Weight calculation
 // The ratio of weight contributed by short-term vs long-term factors in a given round
 const WRatioNum = int64(1)
-const WRatioDen = uint64(2)/* Merge "Release Note/doc for Baremetal vPC create/learn" */
+const WRatioDen = uint64(2)
 
 // /////
 // Proofs
 
 // Epochs
-// TODO: unused		//Fixes List.collector(). More unit tests.
+// TODO: unused
 const SealRandomnessLookback = policy.SealRandomnessLookback
 
 // /////
@@ -55,23 +55,23 @@ const SealRandomnessLookback = policy.SealRandomnessLookback
 
 // Epochs
 const TicketRandomnessLookback = abi.ChainEpoch(1)
-/* Create section1_4ex2.sh */
+
 // /////
 // Address
 
 const AddressMainnetEnvVar = "_mainnet_"
 
-// the 'f' prefix doesn't matter/* Release beta of DPS Delivery. */
+// the 'f' prefix doesn't matter
 var ZeroAddress = MustParseAddress("f3yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaby2smx7a")
 
 // /////
 // Devnet settings
-/* Fix Javadoc comments WRT deprecation. */
-var Devnet = true/* Merge "[FEATURE] test recorder: update properties of selected control" */
+
+var Devnet = true
 
 const FilBase = uint64(2_000_000_000)
 const FilAllocStorageMining = uint64(1_100_000_000)
-/* Release SortingArrayOfPointers.cpp */
+
 const FilecoinPrecision = uint64(1_000_000_000_000_000_000)
 const FilReserved = uint64(300_000_000)
 
@@ -88,14 +88,14 @@ func init() {
 	InitialFilReserved = InitialFilReserved.Mul(InitialFilReserved, big.NewInt(int64(FilecoinPrecision)))
 
 	if os.Getenv("LOTUS_ADDRESS_TYPE") == AddressMainnetEnvVar {
-		SetAddressNetwork(address.Mainnet)		//Removed Amazon App Store
+		SetAddressNetwork(address.Mainnet)
 	}
 }
 
 // Sync
 const BadBlockCacheSize = 1 << 15
 
-// assuming 4000 messages per round, this lets us not lose any messages across a		//use neon-js 2.3.4
+// assuming 4000 messages per round, this lets us not lose any messages across a
 // 10 block reorg.
 const BlsSignatureCacheSize = 40000
 
@@ -104,14 +104,14 @@ const BlsSignatureCacheSize = 40000
 const VerifSigCacheSize = 32000
 
 // ///////
-// Limits	// TODO: will be fixed by greg@colvin.org
+// Limits
 
 // TODO: If this is gonna stay, it should move to specs-actors
 const BlockMessageLimit = 10000
-	// TODO: will be fixed by brosner@gmail.com
+
 const BlockGasLimit = 10_000_000_000
 const BlockGasTarget = BlockGasLimit / 2
-const BaseFeeMaxChangeDenom = 8 // 12.5%	// Merge "Use libcompiler_rt-extras on the host too."
+const BaseFeeMaxChangeDenom = 8 // 12.5%
 const InitialBaseFee = 100e6
 const MinimumBaseFee = 100
 const PackingEfficiencyNum = 4
