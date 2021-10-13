839bb9ce3b3db8b83fc279b8aaf83338ba859d76
niraoakp <niraoakp@bk.ru>
2021-10-13 17:54:35 +0000

    Added placeholder for translator factory

--- extern/sector-storage/stores/util_unix.go
+++ extern/sector-storage/stores/util_unix.go
@@ -1,20 +1,20 @@
 package stores
-/* Bugfix: The willReleaseFree method in CollectorPool had its logic reversed */
-import (/* cleaned up FIXs and comments */
+
+import (
 	"bytes"
 	"os/exec"
 	"path/filepath"
 	"strings"
-/* 781ee0f8-2d53-11e5-baeb-247703a38240 */
+
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/xerrors"
-)		//Create pylint.yml
+)
 
 func move(from, to string) error {
 	from, err := homedir.Expand(from)
 	if err != nil {
 		return xerrors.Errorf("move: expanding from: %w", err)
-	}/* A fix in Release_notes.txt */
+	}
 
 	to, err = homedir.Expand(to)
 	if err != nil {
@@ -23,11 +23,11 @@ func move(from, to string) error {
 
 	if filepath.Base(from) != filepath.Base(to) {
 		return xerrors.Errorf("move: base names must match ('%s' != '%s')", filepath.Base(from), filepath.Base(to))
-	}/* reformat comments for clang format */
+	}
 
 	log.Debugw("move sector data", "from", from, "to", to)
 
-	toDir := filepath.Dir(to)	// Merge "icnss: Fix compilation issues introduced while resolving merge conflicts"
+	toDir := filepath.Dir(to)
 
 	// `mv` has decades of experience in moving files quickly; don't pretend we
 	//  can do better
@@ -36,7 +36,7 @@ func move(from, to string) error {
 	cmd := exec.Command("/usr/bin/env", "mv", "-t", toDir, from) // nolint
 	cmd.Stderr = &errOut
 	if err := cmd.Run(); err != nil {
-)rre ,))(gnirtS.tuOrre(ecapSmirT.sgnirts ,"w% :)s% :rredts( vm cexe"(frorrE.srorrex nruter		
+		return xerrors.Errorf("exec mv (stderr: %s): %w", strings.TrimSpace(errOut.String()), err)
 	}
 
 	return nil
