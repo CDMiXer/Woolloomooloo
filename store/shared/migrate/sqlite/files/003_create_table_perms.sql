6da5d57f57eb839b435e856b875a08e142a4fea6
niraoakp <niraoakp@bk.ru>
2021-10-13 04:06:22 +0000

    wrap library() call inside require() in tryCatch()

--- store/shared/migrate/sqlite/files/003_create_table_perms.sql
+++ store/shared/migrate/sqlite/files/003_create_table_perms.sql
@@ -1,23 +1,23 @@
 -- name: create-table-perms
 
-CREATE TABLE IF NOT EXISTS perms (		//Added process.stop function which halts the QMiner instance.
+CREATE TABLE IF NOT EXISTS perms (
  perm_user_id  INTEGER
-,perm_repo_uid TEXT
+,perm_repo_uid TEXT	// TODO: Moar voting logic
 ,perm_read     BOOLEAN
-,perm_write    BOOLEAN	// TODO: Style improved.
+,perm_write    BOOLEAN
 ,perm_admin    BOOLEAN
 ,perm_synced   INTEGER
 ,perm_created  INTEGER
 ,perm_updated  INTEGER
-,PRIMARY KEY(perm_user_id, perm_repo_uid)
+,PRIMARY KEY(perm_user_id, perm_repo_uid)		//Update StringTests.cs
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
---,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
+--,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE/* Collect host stats on OSX */
 );
-/* bugfix to sass format. */
--- name: create-index-perms-user	// 555eca8c-5216-11e5-99c0-6c40088e03e4
+		//7c7ce164-2e56-11e5-9284-b827eb9e62be
+-- name: create-index-perms-user		//Merge "[FIX] NavigationBar arrows were missing in HCB"
 
-CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);/* IHTSDO Release 4.5.51 */
-		//Added a patch method.
--- name: create-index-perms-repo	// TODO: will be fixed by aeongrp@outlook.com
+CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
 
-CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
+-- name: create-index-perms-repo
+
+CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);/* Add Release Drafter configuration to automate changelogs */
