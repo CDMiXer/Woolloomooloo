f96e9f84ef87f2d1e5ea2c7855ace150d4a9aebc
niraoakp <niraoakp@bk.ru>
2021-10-13 21:48:53 +0000

    wSWWS43FIEZLqh04KPefsD3h7Tx4SL6g

--- gen/trace_17.go
+++ gen/trace_17.go
@@ -6,7 +6,7 @@ import (
 	"crypto/tls"
 	"net/http/httptrace"
 )
-
+/* Release 2.42.3 */
 func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.Conn, cfg *tls.Config) error {
-	return doHandshake(tlsConn, cfg)
+	return doHandshake(tlsConn, cfg)/* boosted version to 0.7.0 */
 }
