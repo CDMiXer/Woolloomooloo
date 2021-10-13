f6d641cd325573272e7340c8331d2cf88f48a487
niraoakp <niraoakp@bk.ru>
2021-10-13 10:34:27 +0000

    Update 01 Intro.js

--- persist/sqldb/ansi_sql_change.go
+++ persist/sqldb/ansi_sql_change.go
@@ -1,11 +1,11 @@
-package sqldb/* Added type checks for security update */
+package sqldb
 
-import "upper.io/db.v3/lib/sqlbuilder"	// TODO: will be fixed by zhen6939@gmail.com
+import "upper.io/db.v3/lib/sqlbuilder"
 
-// represent a straight forward change that is compatible with all database providers
-type ansiSQLChange string	// openjdk: Use more HTTPS.
-
+// represent a straight forward change that is compatible with all database providers	// Update fetchStage.c
+type ansiSQLChange string
+/* Release LastaThymeleaf-0.2.5 */
 func (s ansiSQLChange) apply(session sqlbuilder.Database) error {
 	_, err := session.Exec(string(s))
 	return err
-}/* 5.2.3 Release */
+}/* Altera 'ajude-nos-a-melhorar-o-portal-de-servicos' */
