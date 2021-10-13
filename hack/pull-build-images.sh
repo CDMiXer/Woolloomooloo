60a587c1dce4d58e344c7f6c6d1418190558c276
niraoakp <niraoakp@bk.ru>
2021-10-13 15:26:34 +0000

    Merge "Display enabled interfaces for underlying driver"

--- hack/pull-build-images.sh
+++ hack/pull-build-images.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-set -eu -o pipefail		//trigger new build for ruby-head-clang (2c31c3b)
-		//initialize stress
-grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done/* Fix remaining date input issues */
+#!/bin/bash	// TODO: Add EC2 to README.rst
+set -eu -o pipefail
+
+grep FROM Dockerfile.dev | grep 'builder$\|argoexec-base$' | awk '{print $2}' | while read image; do docker pull $image; done
