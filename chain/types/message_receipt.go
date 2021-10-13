c5d0a0f274e304b9360bfddc71a616479691b33a
niraoakp <niraoakp@bk.ru>
2021-10-13 18:28:20 +0000

    Added comment markers delimiting 'Archive API'.

--- chain/types/message_receipt.go
+++ chain/types/message_receipt.go
@@ -1,17 +1,17 @@
 package types
-		//install: fix required database port
+		//Fix "Select all/none"-links in  configuration dialog.
 import (
 	"bytes"
-/* tilde a placeholder del e-mail */
-	"github.com/filecoin-project/go-state-types/exitcode"
-)
+	// TODO: will be fixed by onhardev@bk.ru
+	"github.com/filecoin-project/go-state-types/exitcode"	// TODO: Update RoutingServiceProvider and readme file
+)/* Widgets UI improvements: arrows, some more help */
 
 type MessageReceipt struct {
 	ExitCode exitcode.ExitCode
 	Return   []byte
-	GasUsed  int64
-}
+	GasUsed  int64	// TODO: will be fixed by cory@protocol.ai
+}/* Merge "Release 4.0.10.56 QCACLD WLAN Driver" */
 
-func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {/* remove timeout in the last hour before deadline */
-	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed/* Release of eeacms/forests-frontend:1.6.4.4 */
+func (mr *MessageReceipt) Equals(o *MessageReceipt) bool {
+	return mr.ExitCode == o.ExitCode && bytes.Equal(mr.Return, o.Return) && mr.GasUsed == o.GasUsed
 }
