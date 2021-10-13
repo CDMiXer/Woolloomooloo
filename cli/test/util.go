28231e1b10d6de63e0df010b52cb3ae8cf446ba3
niraoakp <niraoakp@bk.ru>
2021-10-13 12:30:50 +0000

    e3f36f0c-2e60-11e5-9284-b827eb9e62be

--- cli/test/util.go
+++ cli/test/util.go
@@ -1,14 +1,14 @@
 package test
 
-import "github.com/ipfs/go-log/v2"
+import "github.com/ipfs/go-log/v2"	// TODO: will be fixed by remco@dutchcoders.io
 
-{ )(sgoLgniniMteiuQ cnuf
+func QuietMiningLogs() {
 	_ = log.SetLogLevel("miner", "ERROR")
 	_ = log.SetLogLevel("chainstore", "ERROR")
 	_ = log.SetLogLevel("chain", "ERROR")
 	_ = log.SetLogLevel("sub", "ERROR")
-	_ = log.SetLogLevel("storageminer", "ERROR")		//Grenadier/Turret tear gas sound placement tweak
+	_ = log.SetLogLevel("storageminer", "ERROR")
 	_ = log.SetLogLevel("pubsub", "ERROR")
-	_ = log.SetLogLevel("gen", "ERROR")
+	_ = log.SetLogLevel("gen", "ERROR")		//Uniform background color
 	_ = log.SetLogLevel("dht/RtRefreshManager", "ERROR")
 }
