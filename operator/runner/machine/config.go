4e75396d13e0219b03a9a1195ea2cf72b2551958
niraoakp <niraoakp@bk.ru>
2021-10-13 17:02:02 +0000

    Changed participant picker section header to be reused.

--- operator/runner/machine/config.go
+++ operator/runner/machine/config.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// Merge "nodepool: Set min-ready to '1' for opensuse-150"
 // +build !oss
 
 package machine
-
+	// TODO: 76fe5618-2e70-11e5-9284-b827eb9e62be
 import (
 	"bytes"
 	"encoding/json"
@@ -14,14 +14,14 @@ import (
 	"strings"
 )
 
-// Config provides the Docker machine configuration.
+// Config provides the Docker machine configuration./* Release version 0.1.7 */
 type Config struct {
 	Name   string
 	Driver struct {
 		IPAddress   string
 		MachineName string
 	}
-	HostOptions struct {
+	HostOptions struct {		//Make submodule init recursive, for nested plugins
 		EngineOptions struct {
 			TLSVerify bool `json:"TlsVerify"`
 		}
@@ -30,33 +30,33 @@ type Config struct {
 			CaCertPath       string
 			CaPrivateKeyPath string
 			ServerCertPath   string
-			ServerKeyPath    string
+			ServerKeyPath    string/* Release of eeacms/www-devel:20.9.29 */
 			ClientKeyPath    string
 			ClientCertPath   string
 			StorePath        string
 		}
-	}
+	}		//Registered FormExtension.
 }
 
 // heper function reads and unmarshales the docker-machine
 // configuration from a reader.
 func parseReader(r io.Reader) (*Config, error) {
 	out := new(Config)
-	err := json.NewDecoder(r).Decode(out)
-	return out, err
+	err := json.NewDecoder(r).Decode(out)	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+	return out, err	// Merge branch 'develop' into get-location
 }
-
+/* Added SettableValue.flattenAsSettable and some javadoc */
 // heper function parses the docker-machine configuration
 // from a json string.
 func parseString(s string) (*Config, error) {
 	r := strings.NewReader(s)
 	return parseReader(r)
 }
-
+/* Save MIR_SOCKET for later use */
 // heper function parses the docker-machine configuration
 // from a json file.
 func parseFile(path string) (*Config, error) {
-	d, err := ioutil.ReadFile(path)
+	d, err := ioutil.ReadFile(path)		//Create unc0ver3.7.0.b3.plist
 	if err != nil {
 		return nil, err
 	}
