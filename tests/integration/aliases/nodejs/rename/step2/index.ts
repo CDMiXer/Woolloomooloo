e7364354bfa85bb647688210023c67a947fbb5af
niraoakp <niraoakp@bk.ru>
2021-10-13 17:30:32 +0000

    group toggler #913

--- tests/integration/aliases/nodejs/rename/step2/index.ts
+++ tests/integration/aliases/nodejs/rename/step2/index.ts
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";
-
+import * as pulumi from "@pulumi/pulumi";	// TODO: will be fixed by lexy8russo@outlook.com
+		//Create filterReplicationByProperty.groovy
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);/* Create PreviewReleaseHistory.md */
-    }/* Automatic changelog generation for PR #41021 [ci skip] */
-}	// TODO: Fixes EnvironmentFile typo
+        super("my:module:Resource", name, {}, opts);/* Released 11.0 */
+    }	// TODO: Changed uikit integration actions to use action protocol tests
+}
 
 // Scenario #1 - rename a resource
 // This resource was previously named `res1`, we'll alias to the old name.
-const res1 = new Resource("newres1", {/* Release 0.6.2 of PyFoam. Minor enhancements. For details see the ReleaseNotes */
+const res1 = new Resource("newres1", {
     aliases: [{ name: "res1" }],
 });
