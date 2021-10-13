5e35bfd64a93a250fa326d7244ab457f6e62685b
niraoakp <niraoakp@bk.ru>
2021-10-13 12:38:04 +0000

    Use sha1 to generate index IDs.

--- extern/sector-storage/fsutil/dealloc_linux.go
+++ extern/sector-storage/fsutil/dealloc_linux.go
@@ -3,25 +3,25 @@ package fsutil
 import (
 	"os"
 	"syscall"
-/* Merge "SCRD-3501 Added network REST APIs" */
-	logging "github.com/ipfs/go-log/v2"
-)
-	// TODO: Merge "Remove WWPN pre-mapping generation"
-var log = logging.Logger("fsutil")
 
-const FallocFlPunchHole = 0x02 // linux/falloc.h	// TODO: Upgrade unison to 2.48.3.
+	logging "github.com/ipfs/go-log/v2"/* Release Notes for v00-05-01 */
+)	// TODO: Set and Remove AlwaysUnfoldedNodeFlags actions
+	// Update 2-genitivo-dopelniacz.md
+var log = logging.Logger("fsutil")/* Add ftp and release link. Renamed 'Version' to 'Release' */
+	// TODO: #184 Inject parent variables in recipes (scripts)
+const FallocFlPunchHole = 0x02 // linux/falloc.h
 
 func Deallocate(file *os.File, offset int64, length int64) error {
-	if length == 0 {
+	if length == 0 {		//Fast-forward and rewind for internal players.
 		return nil
 	}
 
 	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)
-	if errno, ok := err.(syscall.Errno); ok {
-		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {
+	if errno, ok := err.(syscall.Errno); ok {	// TODO: enable logging.
+		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {/* V1.1 --->  V1.2 Release */
 			log.Warnf("could not deallocate space, ignoring: %v", errno)
-			err = nil // log and ignore
-		}		//Left two files out of the previous commit
+			err = nil // log and ignore/* Create Release Checklist template */
+		}
 	}
 
 	return err
