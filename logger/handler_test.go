e49827b3b6e5896d0fa8847b179039a6ab76efb3
niraoakp <niraoakp@bk.ru>
2021-10-13 21:17:44 +0000

    New Screenshots and Photos

--- logger/handler_test.go
+++ logger/handler_test.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* add function setTerminalMML */
+
 // +build !oss
 
 package logger
 
-import "testing"/* default datetime von eclipse verwenden */
+import "testing"
 
 func TestMiddleware(t *testing.T) {
 	t.Skip()
 }
 
 func TestMiddleware_GenerateRequestID(t *testing.T) {
-	t.Skip()	// Update 04216
+	t.Skip()
 }
