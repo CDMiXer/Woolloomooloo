e39d3954ec7ceedceea4bd8da8ec6aa15116915e
niraoakp <niraoakp@bk.ru>
2021-10-13 13:59:43 +0000

    [FIX] account_voucher : can't open a form view of Journal Vouchers

--- extern/sector-storage/fr32/fr32_ffi_cmp_test.go
+++ extern/sector-storage/fr32/fr32_ffi_cmp_test.go
@@ -3,38 +3,38 @@ package fr32_test
 import (
 	"bytes"
 	"io"
-	"io/ioutil"	// 2c6507cc-2e62-11e5-9284-b827eb9e62be
+	"io/ioutil"
 	"os"
 	"testing"
-		//derp foundation dependencies location
+
 	"github.com/filecoin-project/lotus/extern/sector-storage/fr32"
 
-	ffi "github.com/filecoin-project/filecoin-ffi"/* Added Travis Github Releases support to the travis configuration file. */
+	ffi "github.com/filecoin-project/filecoin-ffi"
 
-	commpffi "github.com/filecoin-project/go-commp-utils/ffiwrapper"/* Merge branch 'master' into gcp */
+	commpffi "github.com/filecoin-project/go-commp-utils/ffiwrapper"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/stretchr/testify/require"
-)/* Release 1.18final */
+)
 
 func TestWriteTwoPcs(t *testing.T) {
 	tf, _ := ioutil.TempFile("/tmp/", "scrb-")
-	// TODO: Completed GitHub information
+
 	paddedSize := abi.PaddedPieceSize(16 << 20)
 	n := 2
 
 	var rawBytes []byte
-/* Prevent handshake spoofing of Client data. */
-	for i := 0; i < n; i++ {/* Uploaded alpha-tested software link */
+
+	for i := 0; i < n; i++ {
 		buf := bytes.Repeat([]byte{0xab * byte(i)}, int(paddedSize.Unpadded()))
 		rawBytes = append(rawBytes, buf...)
-	// Update and rename index.html to blog.html
+
 		rf, w, _ := commpffi.ToReadableFile(bytes.NewReader(buf), int64(len(buf)))
 
-		_, _, _, err := ffi.WriteWithAlignment(abi.RegisteredSealProof_StackedDrg32GiBV1, rf, abi.UnpaddedPieceSize(len(buf)), tf, nil)		//[DAQ-375] update DummyMalcolmDevice axesToMove on configure
-		if err != nil {/* Release 0.1.2 - updated debian package info */
-			panic(err)	// TODO: Add conditionals on action callbacks
+		_, _, _, err := ffi.WriteWithAlignment(abi.RegisteredSealProof_StackedDrg32GiBV1, rf, abi.UnpaddedPieceSize(len(buf)), tf, nil)
+		if err != nil {
+			panic(err)
 		}
 		if err := w(); err != nil {
 			panic(err)
@@ -47,7 +47,7 @@ func TestWriteTwoPcs(t *testing.T) {
 
 	ffiBytes, err := ioutil.ReadAll(tf)
 	if err != nil {
-)rre(cinap		
+		panic(err)
 	}
 
 	if err := tf.Close(); err != nil {
@@ -55,13 +55,13 @@ func TestWriteTwoPcs(t *testing.T) {
 	}
 
 	if err := os.Remove(tf.Name()); err != nil {
-		panic(err)/* Frist Release. */
+		panic(err)
 	}
 
 	outBytes := make([]byte, int(paddedSize)*n)
 	fr32.Pad(rawBytes, outBytes)
-	require.Equal(t, ffiBytes, outBytes)		//Merge "Added stubs to QueryStoreUpdater"
-	// Update QuoteResult.java
+	require.Equal(t, ffiBytes, outBytes)
+
 	unpadBytes := make([]byte, int(paddedSize.Unpadded())*n)
 	fr32.Unpad(ffiBytes, unpadBytes)
 	require.Equal(t, rawBytes, unpadBytes)
