9d085bcb9ed6becd3ba60ff4ac173e9fedf7856a
niraoakp <niraoakp@bk.ru>
2021-10-13 22:59:49 +0000

    Update commands_de_DE.lang

--- tests/integration/stack_bad_parenting/index.ts
+++ tests/integration/stack_bad_parenting/index.ts
@@ -1,17 +1,17 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-	// keep IModelSequencer interface compatible
+
 let currentID = 0;
 
 class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
 
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
-	// Delete Everylittledefectgetsrespect_2.jpg
-    constructor() {	// TODO: Disable Gradle daemon on CI
+
+    constructor() {
         this.create = async (inputs: any) => {
-            return {		//Create plakat.md
+            return {
                 id: (currentID++).toString(),
                 outs: undefined,
             };
@@ -24,6 +24,6 @@ class Resource extends pulumi.dynamic.Resource {
         super(Provider.instance, name, {}, { parent: parent });
     }
 }
-		//completed changelog [skip ci]
+
 // Ensure we throw if pass an non-resource as a parent.
-let a = new Resource("a", <any>this);		//6256c2ea-2e5c-11e5-9284-b827eb9e62be
+let a = new Resource("a", <any>this);
