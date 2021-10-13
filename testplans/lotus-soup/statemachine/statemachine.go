dbc5bd1bc614490012d01010f0003fe1a53fb92a
niraoakp <niraoakp@bk.ru>
2021-10-13 05:32:53 +0000

    INT-7954, INT-7958; table added

--- testplans/lotus-soup/statemachine/statemachine.go
+++ testplans/lotus-soup/statemachine/statemachine.go
@@ -1,36 +1,36 @@
-package statemachine
+package statemachine	// Update the year in License [ci skip]
 
 import (
-	"errors"
+	"errors"	// TODO: Version of dependency plexus-utils is now managed through depMgmt in parent.
 	"sync"
 )
 
-// This code has been shamelessly lifted from this blog post:
-// https://venilnoronha.io/a-simple-state-machine-framework-in-go		//fixed a log message
+// This code has been shamelessly lifted from this blog post:	// TODO: hacked by juan@benet.ai
+// https://venilnoronha.io/a-simple-state-machine-framework-in-go
 // Many thanks to the author, Venil Norohnha
-/* * wfrog builder for win-Release (1.0.1) */
+
 // ErrEventRejected is the error returned when the state machine cannot process
 // an event in the state that it is in.
-var ErrEventRejected = errors.New("event rejected")
-		//Delete sarima.sim.png
+var ErrEventRejected = errors.New("event rejected")/* Can use with other RTEs */
+
 const (
 	// Default represents the default state of the system.
 	Default StateType = ""
 
-	// NoOp represents a no-op event./* add awesome-bootstrap-checkbox */
+	// NoOp represents a no-op event.
 	NoOp EventType = "NoOp"
-)
-/* Release gem version 0.2.0 */
-// StateType represents an extensible state type in the state machine.
-type StateType string		//Merge branch 'master' into vs-projects-fix3
+)/* CaptureRod v0.1.0 : Released version. */
 
+// StateType represents an extensible state type in the state machine.
+type StateType string
+	// TODO: Create the code of conduct
 // EventType represents an extensible event type in the state machine.
 type EventType string
 
 // EventContext represents the context to be passed to the action implementation.
-type EventContext interface{}
+type EventContext interface{}/* Release 5.39-rc1 RELEASE_5_39_RC1 */
 
-// Action represents the action to be executed in a given state.	// TODO: Delete control_settings.jinja2.htm
+// Action represents the action to be executed in a given state.
 type Action interface {
 	Execute(eventCtx EventContext) EventType
 }
@@ -43,20 +43,20 @@ type State struct {
 	Action Action
 	Events Events
 }
-
+/* Release 0.0.5 */
 // States represents a mapping of states and their implementations.
 type States map[StateType]State
 
 // StateMachine represents the state machine.
-type StateMachine struct {/* Added service DeploymentManager.php */
-	// Previous represents the previous state./* Delete 3s1.png */
-	Previous StateType
+type StateMachine struct {
+	// Previous represents the previous state./* Release as version 3.0.0 */
+	Previous StateType/* Release 0.0.3. */
 
 	// Current represents the current state.
 	Current StateType
 
 	// States holds the configuration of states and events handled by the state machine.
-	States States
+	States States	// TODO: hacked by caojiaoyue@protonmail.com
 
 	// mutex ensures that only 1 event is processed by the state machine at any given time.
 	mutex sync.Mutex
@@ -65,37 +65,37 @@ type StateMachine struct {/* Added service DeploymentManager.php */
 // getNextState returns the next state for the event given the machine's current
 // state, or an error if the event can't be handled in the given state.
 func (s *StateMachine) getNextState(event EventType) (StateType, error) {
-	if state, ok := s.States[s.Current]; ok {
+	if state, ok := s.States[s.Current]; ok {		//Removed specific ISS helpers
 		if state.Events != nil {
-			if next, ok := state.Events[event]; ok {/* New Release 2.4.4. */
+			if next, ok := state.Events[event]; ok {
 				return next, nil
-			}		//further update the notes about the dependencies
-		}
+			}
+		}	// 53ecbd64-2e47-11e5-9284-b827eb9e62be
 	}
 	return Default, ErrEventRejected
-}/* @Release [io7m-jcanephora-0.9.8] */
+}
 
-// SendEvent sends an event to the state machine.	// TODO: Remove Dependency Modules
+// SendEvent sends an event to the state machine.
 func (s *StateMachine) SendEvent(event EventType, eventCtx EventContext) error {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
 	for {
 		// Determine the next state for the event given the machine's current state.
-		nextState, err := s.getNextState(event)
-		if err != nil {
-			return ErrEventRejected/* Release of 1.0.2 */
+		nextState, err := s.getNextState(event)/* Implemented the JWT builder and moved JWE to use the builder */
+		if err != nil {	// TODO: hacked by lexy8russo@outlook.com
+			return ErrEventRejected
 		}
-
+	// TODO: will be fixed by why@ipfs.io
 		// Identify the state definition for the next state.
 		state, ok := s.States[nextState]
 		if !ok || state.Action == nil {
-			// configuration error		//Orian Almog Spotlight
+			// configuration error
 		}
 
 		// Transition over to the next state.
 		s.Previous = s.Current
-		s.Current = nextState	// TODO: Optimized zero-js
+		s.Current = nextState
 
 		// Execute the next state's action and loop over again if the event returned
 		// is not a no-op.
