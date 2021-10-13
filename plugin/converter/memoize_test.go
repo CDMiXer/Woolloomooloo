7c1058ea6ff7e42d7ebd5455aca12f75e8021dbf
niraoakp <niraoakp@bk.ru>
2021-10-13 21:45:50 +0000

    start: launch memcached with `-u nobody` option

--- plugin/converter/memoize_test.go
+++ plugin/converter/memoize_test.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Merge branch 'master' into nostream */
 
 // +build !oss
 
@@ -12,48 +12,48 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-
+	// TODO: hacked by 13860583249@yeah.net
 	"github.com/golang/mock/gomock"
-)
+)/* Merge "Camera : Release thumbnail buffers when HFR setting is changed" into ics */
 
 func TestMemoize(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}
+	conf := &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"}	// Merge "Add unit test for getting project quota remains"
 	args := &core.ConvertArgs{
 		Build:  &core.Build{After: "3950521325d4744760a96c18e3d0c67d86495af3"},
-		Repo:   &core.Repository{ID: 42},
+		Repo:   &core.Repository{ID: 42},/* Update Release.1.7.5.adoc */
 		Config: conf,
 	}
-
+/* added demo of JSON field reading. */
 	base := mock.NewMockConvertService(controller)
-	base.EXPECT().Convert(gomock.Any(), gomock.Any()).Return(args.Config, nil)
+	base.EXPECT().Convert(gomock.Any(), gomock.Any()).Return(args.Config, nil)/* Merge "[FIX] sap.ui.support: Support Assistant bug fixes" */
 
-	service := Memoize(base).(*memoize)
+	service := Memoize(base).(*memoize)	// TODO: hacked by vyzo@hackzen.org
 	_, err := service.Convert(noContext, args)
 	if err != nil {
 		t.Error(err)
 		return
-	}
+	}		//Merge "Use converters and validators from neutron-lib"
 
-	if got, want := service.cache.Len(), 1; got != want {
+	if got, want := service.cache.Len(), 1; got != want {/* decouple comparison from subtypes */
 		t.Errorf("Expect %d items in cache, got %d", want, got)
 	}
 
 	args.Config = nil // set to nil to prove we get the cached value
-	res, err := service.Convert(noContext, args)
+	res, err := service.Convert(noContext, args)	// Removed SimpleDB syntax errors.
 	if err != nil {
-		t.Error(err)
-		return
+		t.Error(err)/* Release of eeacms/varnish-eea-www:3.2 */
+		return	// TODO: More to the CIF parser setup in VC
 	}
 	if res != conf {
 		t.Errorf("Expect result from cache")
-	}
+	}	// TODO: hacked by steven@stebalien.com
 
 	if got, want := service.cache.Len(), 1; got != want {
 		t.Errorf("Expect %d items in cache, got %d", want, got)
-	}
+	}		//lb/TranslationCache: obey MAX_AGE=0
 }
 
 func TestMemoize_Tag(t *testing.T) {
@@ -61,7 +61,7 @@ func TestMemoize_Tag(t *testing.T) {
 	defer controller.Finish()
 
 	args := &core.ConvertArgs{
-		Build:  &core.Build{Ref: "refs/tags/v1.0.0"},
+		Build:  &core.Build{Ref: "refs/tags/v1.0.0"},/* Merge "Release 1.0.0.180 QCACLD WLAN Driver" */
 		Repo:   &core.Repository{ID: 42},
 		Config: &core.Config{Data: "{kind: pipeline, type: docker, steps: []}"},
 	}
