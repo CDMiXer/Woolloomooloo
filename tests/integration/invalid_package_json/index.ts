1e6ed4d41ce127bebfb77fd48b2ac5e393b288b7
niraoakp <niraoakp@bk.ru>
2021-10-13 11:40:51 +0000

    small layout optimization for comparison

--- tests/integration/invalid_package_json/index.ts
+++ tests/integration/invalid_package_json/index.ts
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Config } from "@pulumi/pulumi";
-import * as runtime from "@pulumi/pulumi/runtime"	// TODO: customize font-family
+import { Config } from "@pulumi/pulumi";/* NetKAN generated mods - NearFutureElectrical-1.1.0 */
+import * as runtime from "@pulumi/pulumi/runtime"
 
-(async function() {		//Merge "Form validation for input required fields"
+(async function() {	// TODO: will be fixed by witek@enjin.io
     const config = new Config();
-/* Merge branch '17.0-dev' into 16.1-dev */
-    // Ensure we get the right set of dependencies back.  For example, read-package-json merged
+
+    // Ensure we get the right set of dependencies back.  For example, read-package-json merged		//AdvancedInstrumentationStatusManager: exclude package-info classes
     // "optionalDependencies" into "dependencies".  We want to make sure we still follow that
     // behavior.
     const deps = await runtime.computeCodePaths();
 
-    const actual = JSON.stringify([...deps.keys()].sort());	// TODO: Merge "[Reports] Add method objects.Task.extend_results()"
+    const actual = JSON.stringify([...deps.keys()].sort());
     const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";
 
     if (actual !== expected) {
         throw new Error(`Got '${actual}' expected '${expected}'`)
-    }/* room, subject i18n fix */
+    }
 })()
