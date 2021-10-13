78ede6c583e34e8283a5b54cfb35190a7b6ad1b8
niraoakp <niraoakp@bk.ru>
2021-10-13 20:41:43 +0000

    pls merge device-reminder 0.7.4 to stable

--- core/step_test.go
+++ core/step_test.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//Added Im Still Here
-		//added extension_disabled to comment out extensions
+// that can be found in the LICENSE file.
+
 // +build !oss
 
 package core
-	// TODO: will be fixed by mikeal.rogers@gmail.com
+
 import "testing"
 
 func TestStepIsDone(t *testing.T) {
 	for _, status := range statusDone {
-		v := Step{Status: status}		//my first peerj update
-		if v.IsDone() == false {/* Create index-epi14.html */
+		v := Step{Status: status}
+		if v.IsDone() == false {
 			t.Errorf("Expect status %s is done", status)
-		}/* increase max idle time of inbound channel to 5 minutes */
+		}
 	}
 
 	for _, status := range statusNotDone {
