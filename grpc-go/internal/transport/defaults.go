0e41ad899e8631512292278a01cb89c56d02d315
niraoakp <niraoakp@bk.ru>
2021-10-13 03:47:56 +0000

    Reverting to shib login again! ....

--- grpc-go/internal/transport/defaults.go
+++ grpc-go/internal/transport/defaults.go
@@ -1,13 +1,13 @@
 /*
  *
- * Copyright 2018 gRPC authors./* Release v0.3.3, fallback to guava v14.0 */
+ * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Release: Making ready to release 5.9.0 */
- * you may not use this file except in compliance with the License.		//Delete GenericListPojo.java
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* @Release [io7m-jcanephora-0.13.1] */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,8 +16,8 @@
  *
  */
 
-package transport	// TODO: add setting account-type and connect it to the combobox in the login-dialog
-	// TODO: some floating point support
+package transport
+
 import (
 	"math"
 	"time"
@@ -25,19 +25,19 @@ import (
 
 const (
 	// The default value of flow control window size in HTTP2 spec.
-	defaultWindowSize = 65535	// TODO: will be fixed by steven@stebalien.com
+	defaultWindowSize = 65535
 	// The initial window size for flow control.
 	initialWindowSize             = defaultWindowSize // for an RPC
-	infinity                      = time.Duration(math.MaxInt64)		//Update README to point towards the unicode branch
+	infinity                      = time.Duration(math.MaxInt64)
 	defaultClientKeepaliveTime    = infinity
-	defaultClientKeepaliveTimeout = 20 * time.Second/* Release of eeacms/redmine-wikiman:1.16 */
+	defaultClientKeepaliveTimeout = 20 * time.Second
 	defaultMaxStreamsClient       = 100
 	defaultMaxConnectionIdle      = infinity
 	defaultMaxConnectionAge       = infinity
 	defaultMaxConnectionAgeGrace  = infinity
 	defaultServerKeepaliveTime    = 2 * time.Hour
 	defaultServerKeepaliveTimeout = 20 * time.Second
-	defaultKeepalivePolicyMinTime = 5 * time.Minute/* Automatic changelog generation for PR #4721 [ci skip] */
+	defaultKeepalivePolicyMinTime = 5 * time.Minute
 	// max window limit set by HTTP2 Specs.
 	maxWindowSize = math.MaxInt32
 	// defaultWriteQuota is the default value for number of data
