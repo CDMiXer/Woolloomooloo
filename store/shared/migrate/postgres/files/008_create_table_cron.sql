578ef1f8603d380490ede899bf5c02c6cf05ab93
niraoakp <niraoakp@bk.ru>
2021-10-13 20:50:30 +0000

    Update FRMdatdata.m

--- store/shared/migrate/postgres/files/008_create_table_cron.sql
+++ store/shared/migrate/postgres/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
 -- name: create-table-cron
-
-CREATE TABLE IF NOT EXISTS cron (
- cron_id          SERIAL PRIMARY KEY
-,cron_repo_id     INTEGER
-,cron_name        VARCHAR(50)	// TODO: hacked by sbrichards@gmail.com
+	// Fix linux rv_allocator_local properly
+CREATE TABLE IF NOT EXISTS cron (/* Adding matrix case to save statement of csv file #2216 */
+ cron_id          SERIAL PRIMARY KEY	// TODO: hacked by joshua@yottadb.com
+,cron_repo_id     INTEGER/* #146 - github -setting focus to the first input element of the editor */
+,cron_name        VARCHAR(50)
 ,cron_expr        VARCHAR(50)
 ,cron_next        INTEGER
-,cron_prev        INTEGER		//increase VAF precision to 3 digits in VCFtoHTML output
+,cron_prev        INTEGER/* Merge branch 'develop' into feature-components */
 ,cron_event       VARCHAR(50)
-,cron_branch      VARCHAR(250)/* Initail Commit */
+,cron_branch      VARCHAR(250)/* Refactor filter handling. */
 ,cron_target      VARCHAR(250)
 ,cron_disabled    BOOLEAN
-,cron_created     INTEGER
-,cron_updated     INTEGER		//workflow_diagram
-,cron_version     INTEGER
-,UNIQUE(cron_repo_id, cron_name)/* Make ValidationField::getType() return an empty string instead of null */
+,cron_created     INTEGER	// Removed bonuses from Novice Armlet. C'mon guys. :( 
+,cron_updated     INTEGER
+,cron_version     INTEGER	// Added the ability to freeze buffers
+,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-cron-repo
-/* Merge "wlan: Release 3.2.3.86a" */
-CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
-	// Passes tests
--- name: create-index-cron-next
-	// info text added
-CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
+
+CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);/* Release gubbins for Tracer */
+
+-- name: create-index-cron-next	// TODO: hacked by steven@stebalien.com
+		//bebb8510-2e6c-11e5-9284-b827eb9e62be
+CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);		//Update media_object.rb
