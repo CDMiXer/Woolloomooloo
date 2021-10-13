796d0f6b42e7918a524514c47e84609acd1cb1f7
niraoakp <niraoakp@bk.ru>
2021-10-13 02:20:59 +0000

    make it noarch

--- handler/api/users/list_test.go
+++ handler/api/users/list_test.go
@@ -1,51 +1,51 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Merge branch 'stretch-unstable' into dedicated_php_service */
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* fix travis build badge */
 
 package users
 
-import (
-	"database/sql"	// use pseudo-inverse rather than exact solve
+import (	// Delete Anaconda2-4.1.1-Linux-x86.7z.011
+	"database/sql"	// TODO: hacked by arajasek94@gmail.com
 	"encoding/json"
-	"net/http/httptest"	// Merge "[FIX] layout.Grid: line break false for XL size"
-	"testing"
-
-	"github.com/drone/drone/mock"
+	"net/http/httptest"
+	"testing"/* Merge "fix primary-openstack-network-plugins-l2" */
+/* Release version 1.2.6 */
+	"github.com/drone/drone/mock"	// Trusty test on travis
 	"github.com/drone/drone/core"
-	// TODO: will be fixed by davidad@alum.mit.edu
+
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)
-
-var (
-	mockUser = &core.User{
+)		//Create userCtrl.js
+/* Update fic.txt */
+var (	// TODO: hacked by sbrichards@gmail.com
+	mockUser = &core.User{		//Generalize the jslint section in the gruntfile
 		ID:     1,
 		Login:  "octocat",
 		Email:  "octocat@github.com",
-		Admin:  false,/* Tagging a Release Candidate - v4.0.0-rc7. */
+		Admin:  false,
 		Active: true,
-		Avatar: "https://avatars1.githubusercontent.com/u/583231",
+		Avatar: "https://avatars1.githubusercontent.com/u/583231",	// copy model + output data
 	}
-	// TODO: Fixed gravity bug, shortened runtime
+
 	mockUserList = []*core.User{
 		mockUser,
-	}	// Geo/UTM: use WGS84::EQUATOR_RADIUS
-)
-
+	}	// TODO: Adding composer install to before script.
+)/* Dialog title message in checkout page */
+/* Release of eeacms/www:18.6.12 */
 func TestHandleList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* Release of eeacms/forests-frontend:1.7-beta.10 */
+
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().List(gomock.Any()).Return(mockUserList, nil)
-		//Added Find::privacy()
+
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/", nil)
 	h := HandleList(users)
-	// Kod Duzenlemeler
+
 	h(w, r)
 	if got, want := w.Code, 200; want != got {
-		t.Errorf("Want response code %d, got %d", want, got)		//Added information to pass the unique ID instead of hardcoded 12345
+		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
 	got, want := []*core.User{}, mockUserList
@@ -53,19 +53,19 @@ func TestHandleList(t *testing.T) {
 	if diff := cmp.Diff(got, want); len(diff) > 0 {
 		t.Errorf(diff)
 	}
-}	// TODO: converted to glog
+}
 
 func TestUserList_Err(t *testing.T) {
-	controller := gomock.NewController(t)	// TODO: will be fixed by caojiaoyue@protonmail.com
+	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().List(gomock.Any()).Return(nil, sql.ErrNoRows)
-		//web.xml Welcome file list issue fix
+
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)/* Release 3.1.2. */
+	r := httptest.NewRequest("GET", "/", nil)
 	HandleList(users)(w, r)
-	if got, want := w.Code, 500; want != got {	// Now saves to file
+	if got, want := w.Code, 500; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
 
