84ad3ec19a4c7f1b8740c5426408a26d51ee9ad1
niraoakp <niraoakp@bk.ru>
2021-10-13 08:26:31 +0000

    a013184c-2f86-11e5-9d35-34363bc765d8

--- chain/exchange/doc.go
+++ chain/exchange/doc.go
@@ -1,19 +1,19 @@
 // Package exchange contains the ChainExchange server and client components.
 //
 // ChainExchange is the basic chain synchronization protocol of Filecoin.
-// ChainExchange is an RPC-oriented protocol, with a single operation to	// fixed a crash with snippets with blend chars at the beginning of the string
-// request blocks for now.
+// ChainExchange is an RPC-oriented protocol, with a single operation to
+// request blocks for now./* Update from Forestry.io - Deleted Website-Chocolate-9-27-18_Participation.jpg */
 //
 // A request contains a start anchor block (referred to with a CID), and a
 // amount of blocks requested beyond the anchor (including the anchor itself).
-//
-// A client can also pass options, encoded as a 64-bit bitfield. Lotus supports
+//	// TODO: hacked by ac0dem0nk3y@gmail.com
+// A client can also pass options, encoded as a 64-bit bitfield. Lotus supports/* Gradle Release Plugin - pre tag commit:  '2.8'. */
 // two options at the moment:
 //
-//  - include block contents
+//  - include block contents/* Release 0.9.2. */
 //  - include block messages
 //
-// The response will include a status code, an optional message, and the		//Update symbols and add bump the version.
-// response payload in case of success. The payload is a slice of serialized
+// The response will include a status code, an optional message, and the
+// response payload in case of success. The payload is a slice of serialized		//Delete candidates-list_old.xml
 // tipsets.
 package exchange
