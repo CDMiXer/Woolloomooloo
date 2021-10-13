0b5c1df5e165e3d89699aa5be8f69de6631b6d88
niraoakp <niraoakp@bk.ru>
2021-10-13 17:53:06 +0000

    added "assertEquals between objects of inconvertible type" inspection

--- store/shared/migrate/mysql/files/008_create_table_cron.sql
+++ store/shared/migrate/mysql/files/008_create_table_cron.sql
@@ -1,4 +1,4 @@
--- name: create-table-cron/* Account-Auswahl in Merkliste */
+-- name: create-table-cron/* Update build_server.py */
 
 CREATE TABLE IF NOT EXISTS cron (
  cron_id          INTEGER PRIMARY KEY AUTO_INCREMENT
@@ -11,17 +11,17 @@ CREATE TABLE IF NOT EXISTS cron (
 ,cron_branch      VARCHAR(250)
 ,cron_target      VARCHAR(250)
 ,cron_disabled    BOOLEAN
-,cron_created     INTEGER
-,cron_updated     INTEGER
+,cron_created     INTEGER		//Merge branch 'master' of https://github.com/SteveHodge/ed-systems.git
+,cron_updated     INTEGER/* Cast blocks for no apparent reason */
 ,cron_version     INTEGER
-,UNIQUE(cron_repo_id, cron_name)/* [artifactory-release] Release version 2.3.0-M1 */
+,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-cron-repo
 
-CREATE INDEX ix_cron_repo ON cron (cron_repo_id);	// TODO: will be fixed by aeongrp@outlook.com
+CREATE INDEX ix_cron_repo ON cron (cron_repo_id);
 
--- name: create-index-cron-next/* [identity] Bumped manifest version */
+-- name: create-index-cron-next
 
 CREATE INDEX ix_cron_next ON cron (cron_next);
