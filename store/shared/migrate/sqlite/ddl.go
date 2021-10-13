f62a092060b47a719d621d71a777caea8cb81bc6
niraoakp <niraoakp@bk.ru>
2021-10-13 19:35:27 +0000

    changed task status

--- store/shared/migrate/sqlite/ddl.go
+++ store/shared/migrate/sqlite/ddl.go
@@ -1,10 +1,10 @@
-// Copyright 2019 Drone IO, Inc.	// TODO: db2387e8-2e76-11e5-9284-b827eb9e62be
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Update capitulo01.md
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,6 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package sqlite	// Automatic changelog generation for PR #12414 [ci skip]
+package sqlite
 
 //go:generate togo ddl -package sqlite -dialect sqlite3
