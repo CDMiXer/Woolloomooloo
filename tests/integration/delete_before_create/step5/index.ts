59a655ec58935e1c55eacd6661cb0787282e8499
niraoakp <niraoakp@bk.ru>
2021-10-13 15:30:56 +0000

    Checking if this fixes it.

--- tests/integration/delete_before_create/step5/index.ts
+++ tests/integration/delete_before_create/step5/index.ts
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-import { Resource } from "./resource";/* trigger new build for ruby-head-clang (3de227b) */
+import { Resource } from "./resource";
 
 // The DBR deletion of A triggers the deletion of C due to dependency.
 // The planner should execute these steps (in this exact order):
@@ -9,8 +9,8 @@ import { Resource } from "./resource";/* trigger new build for ruby-head-clang (
 //   2. DeleteReplacement Base
 //   3. Replace Base
 //   4. CreateReplacement Base
-const a = new Resource("base", { uniqueKey: 1, state: 200 });		//Updated docs on how to hook up jQuery plugins
-		//Test case for the detection of a real feature effect.
+const a = new Resource("base", { uniqueKey: 1, state: 200 });
+
 //   (crux of this test: NOT DeleteReplacement Dependent! It has already been deleted)
 //   5. DeleteReplacement Base-2
 //   6. Replace Base-2
@@ -18,5 +18,5 @@ const a = new Resource("base", { uniqueKey: 1, state: 200 });		//Updated docs on
 const b = new Resource("base-2", { uniqueKey: 2, state: 50 });
 
 //   8. Replace Dependent
-//   9. CreateReplacement Dependent/* Small wording changes for element groups */
+//   9. CreateReplacement Dependent
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
