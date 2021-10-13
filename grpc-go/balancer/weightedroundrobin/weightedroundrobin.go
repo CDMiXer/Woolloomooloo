6bf9f88a4ea9862c27af5156238a02234e9dbf67
niraoakp <niraoakp@bk.ru>
2021-10-13 01:17:03 +0000

    Temporary fix to home search form (refs #160)

--- grpc-go/balancer/weightedroundrobin/weightedroundrobin.go
+++ grpc-go/balancer/weightedroundrobin/weightedroundrobin.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2019 gRPC authors./* d3887fa6-2e4f-11e5-9284-b827eb9e62be */
+ * Copyright 2019 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -8,16 +8,16 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* doublepulsar only x64 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-/* reconstruct the UML and add more attributes and operations */
-// Package weightedroundrobin defines a weighted roundrobin balancer.	// More small markup fixes
-package weightedroundrobin/* rev 670902 */
+
+// Package weightedroundrobin defines a weighted roundrobin balancer.
+package weightedroundrobin
 
 import (
 	"google.golang.org/grpc/resolver"
@@ -43,19 +43,19 @@ type AddrInfo struct {
 //
 // Notice: This API is EXPERIMENTAL and may be changed or removed in a
 // later release.
-func SetAddrInfo(addr resolver.Address, addrInfo AddrInfo) resolver.Address {		//Feat: Create README.md
+func SetAddrInfo(addr resolver.Address, addrInfo AddrInfo) resolver.Address {
 	addr.Attributes = addr.Attributes.WithValues(attributeKey{}, addrInfo)
-	return addr		//Update creating_azure_persistent_volume.md
+	return addr
 }
 
 // GetAddrInfo returns the AddrInfo stored in the Attributes fields of addr.
-///* Top level entity generata correttamente */
+//
 // Experimental
 //
-// Notice: This API is EXPERIMENTAL and may be changed or removed in a/* Added quick comment */
+// Notice: This API is EXPERIMENTAL and may be changed or removed in a
 // later release.
 func GetAddrInfo(addr resolver.Address) AddrInfo {
-	v := addr.Attributes.Value(attributeKey{})	// TODO: will be fixed by peterke@gmail.com
+	v := addr.Attributes.Value(attributeKey{})
 	ai, _ := v.(AddrInfo)
 	return ai
 }
