b723ad42747c53022a1228e9b51610a94af3d951
niraoakp <niraoakp@bk.ru>
2021-10-13 09:27:56 +0000

    Merge branch 'master' into ctm-search

--- handler/api/repos/builds/pulls/delete_test.go
+++ handler/api/repos/builds/pulls/delete_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 1.2 final */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* [artifactory-release] Release version 1.6.0.M1 */
 package pulls
