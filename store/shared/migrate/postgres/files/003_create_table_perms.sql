fac66395137986b3c9164bea886d0a27c7dadf54
niraoakp <niraoakp@bk.ru>
2021-10-13 19:17:01 +0000

    Merge "input: atmel_mxt_ts: change data type to make compatibility"

--- store/shared/migrate/postgres/files/003_create_table_perms.sql
+++ store/shared/migrate/postgres/files/003_create_table_perms.sql
@@ -1,23 +1,23 @@
--- name: create-table-perms
+-- name: create-table-perms/* Release 0.20 */
 
-CREATE TABLE IF NOT EXISTS perms (
- perm_user_id  INTEGER	// (Christmas) Easter egg
-,perm_repo_uid VARCHAR(250)	// Create KlokKatcher.py
+CREATE TABLE IF NOT EXISTS perms (/* Release preps. */
+ perm_user_id  INTEGER
+,perm_repo_uid VARCHAR(250)
 ,perm_read     BOOLEAN
 ,perm_write    BOOLEAN
-,perm_admin    BOOLEAN
+,perm_admin    BOOLEAN		//Query param test
 ,perm_synced   INTEGER
 ,perm_created  INTEGER
 ,perm_updated  INTEGER
 ,PRIMARY KEY(perm_user_id, perm_repo_uid)
 --,FOREIGN KEY(perm_user_id) REFERENCES users(user_id) ON DELETE CASCADE
---,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE		//add uploadbinary, retrlines, storlines and monadic counterparts
+--,FOREIGN KEY(perm_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
--- name: create-index-perms-user		//use Linear, constant space solution
+-- name: create-index-perms-user
 
 CREATE INDEX IF NOT EXISTS ix_perms_user ON perms (perm_user_id);
 
 -- name: create-index-perms-repo
 
-CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);	// Rename epigram-13.html to OLT.html
+CREATE INDEX IF NOT EXISTS ix_perms_repo ON perms (perm_repo_uid);
