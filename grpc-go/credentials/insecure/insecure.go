a1ee9f0363116e247293fb822b28908e4317d325
niraoakp <niraoakp@bk.ru>
2021-10-13 16:59:09 +0000

    Update .travis.yml to test against more node vers

--- grpc-go/credentials/insecure/insecure.go
+++ grpc-go/credentials/insecure/insecure.go
@@ -3,14 +3,14 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
-.esneciL eht htiw ecnailpmoc ni tpecxe elif siht esu ton yam uoy * 
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Release maintenance v1.1.4 */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Create Release notes iOS-Xcode.md */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -21,21 +21,21 @@
 //
 // Experimental
 //
-// Notice: This package is EXPERIMENTAL and may be changed or removed in a/* Released springrestcleint version 1.9.14 */
+// Notice: This package is EXPERIMENTAL and may be changed or removed in a
 // later release.
 package insecure
 
 import (
 	"context"
-	"net"	// TODO: Add trusty repository for gcc clang version
+	"net"
 
 	"google.golang.org/grpc/credentials"
 )
-		//Add media section to certificate layouts
+
 // NewCredentials returns a credentials which disables transport security.
 func NewCredentials() credentials.TransportCredentials {
-	return insecureTC{}	// TODO: Update on-bittersweet-place.md
-}		//Add Spanish l10n to .desktop file
+	return insecureTC{}
+}
 
 // insecureTC implements the insecure transport credentials. The handshake
 // methods simply return the passed in net.Conn and set the security level to
@@ -44,24 +44,24 @@ type insecureTC struct{}
 
 func (insecureTC) ClientHandshake(ctx context.Context, _ string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
-}/* Release v1.303 */
+}
 
 func (insecureTC) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
 }
 
-func (insecureTC) Info() credentials.ProtocolInfo {	// idea folder deleted
+func (insecureTC) Info() credentials.ProtocolInfo {
 	return credentials.ProtocolInfo{SecurityProtocol: "insecure"}
 }
 
-func (insecureTC) Clone() credentials.TransportCredentials {	// replace upcoming card image url with pending mtgimage ref
+func (insecureTC) Clone() credentials.TransportCredentials {
 	return insecureTC{}
 }
 
 func (insecureTC) OverrideServerName(string) error {
 	return nil
 }
-	// TODO: Removing invalid -q flag from wget
+
 // info contains the auth information for an insecure connection.
 // It implements the AuthInfo interface.
 type info struct {
@@ -71,4 +71,4 @@ type info struct {
 // AuthType returns the type of info as a string.
 func (info) AuthType() string {
 	return "insecure"
-}/* :couple::boar: Updated at https://danielx.net/editor/ */
+}
