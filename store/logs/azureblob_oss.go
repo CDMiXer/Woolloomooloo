1957bd2dcedf4ebbaca605747184049ff8d297b3
niraoakp <niraoakp@bk.ru>
2021-10-13 09:17:54 +0000

    Create gulpfile-variant.js

--- store/logs/azureblob_oss.go
+++ store/logs/azureblob_oss.go
@@ -1,24 +1,24 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* chore: pin mocha to 4.0.0 */
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License.	// TODO: hacked by alex.gaynor@gmail.com
+// You may obtain a copy of the License at	// TODO: will be fixed by martin2cai@hotmail.com
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by 13860583249@yeah.net
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* added function to retrieve external object associated with consumer task */
+// Unless required by applicable law or agreed to in writing, software		//.darcs-boringfile: ignore zfec files instead of pyfec files
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+/* Release version 6.2 */
+// +build oss
 
-// +build oss/* Release areca-6.0.4 */
-/* change wording on sitemap options page */
 package logs
 
 import "github.com/drone/drone/core"
 
 // New returns a zero value LogStore.
-func NewAzureBlobEnv(containerName, storageAccountName, storageAccessKey string) core.LogStore {
+func NewAzureBlobEnv(containerName, storageAccountName, storageAccessKey string) core.LogStore {/* configure jetty */
 	return nil
 }
