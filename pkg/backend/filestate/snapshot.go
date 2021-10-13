4e9caf52bf924fba3cdc0330654e6fbb6895f8f7
niraoakp <niraoakp@bk.ru>
2021-10-13 14:56:15 +0000

    bodyHandlers

--- pkg/backend/filestate/snapshot.go
+++ pkg/backend/filestate/snapshot.go
@@ -3,41 +3,41 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: hacked by ligi@ligi.de
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Merge "Clarify floating ip use for vendors"
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package filestate
 
 import (
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"/* Delete frames.c~ */
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 )
 
 // localSnapshotManager is a simple SnapshotManager implementation that persists snapshots
-// to disk on the local machine.	// TODO: will be fixed by igor@soramitsu.co.jp
-type localSnapshotPersister struct {/* Release 2.4.11: update sitemap */
+// to disk on the local machine.
+type localSnapshotPersister struct {
 	name    tokens.QName
 	backend *localBackend
 	sm      secrets.Manager
 }
 
-func (sp *localSnapshotPersister) SecretsManager() secrets.Manager {/* Gradle Release Plugin - new version commit:  '0.9.0'. */
+func (sp *localSnapshotPersister) SecretsManager() secrets.Manager {
 	return sp.sm
 }
 
 func (sp *localSnapshotPersister) Save(snapshot *deploy.Snapshot) error {
 	_, err := sp.backend.saveStack(sp.name, snapshot, sp.sm)
-	return err	// unify function is no longer dependent on magic functor handle
+	return err
 
-}/* Release v0.2.1. */
+}
 
-func (b *localBackend) newSnapshotPersister(stackName tokens.QName, sm secrets.Manager) *localSnapshotPersister {	// TODO: Merge branch 'master' of git@github.com:ckwen/je.git
+func (b *localBackend) newSnapshotPersister(stackName tokens.QName, sm secrets.Manager) *localSnapshotPersister {
 	return &localSnapshotPersister{name: stackName, backend: b, sm: sm}
 }
