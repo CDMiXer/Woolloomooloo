8db35a6b66e48833416062a86aa78e221907a6f6
niraoakp <niraoakp@bk.ru>
2021-10-13 01:49:00 +0000

    Merge "pm8921: Change log level from info to critical"

--- core/webhook.go
+++ core/webhook.go
@@ -1,52 +1,52 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: will be fixed by vyzo@hackzen.org
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Dont check only first keypart Fix #129 */
-///* abstract out default target config responses in Releaser spec */
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//add ProcessException
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Delete Makefile-Release-MacOSX.mk */
 
 package core
 
 import (
 	"context"
 )
-		//dd52436c-2e5b-11e5-9284-b827eb9e62be
+
 // Webhook event types.
-const (/* Release 3.05.beta08 */
-	WebhookEventBuild = "build"/* Refactoring, add Blogger support */
-	WebhookEventRepo  = "repo"		//try four -- this time with matt looking :-) -- again
-	WebhookEventUser  = "user"		//add steps how to install this on RHEL
-)
-/* b4ca41b0-2e6a-11e5-9284-b827eb9e62be */
-// Webhook action types.
+const (
+	WebhookEventBuild = "build"
+	WebhookEventRepo  = "repo"
+	WebhookEventUser  = "user"	// TODO: Remove stun in gtel wizard
+)/* fix with lazy start */
+
+// Webhook action types./* version>1.2-SNAPSHOT */
 const (
 	WebhookActionCreated  = "created"
 	WebhookActionUpdated  = "updated"
-	WebhookActionDeleted  = "deleted"/* Merge commit 'b5a5d217a1f1364ed3e5d0dd5e45d449e32bf1cb' */
-	WebhookActionEnabled  = "enabled"	// KUBOS-111 Fixing more spacing
+	WebhookActionDeleted  = "deleted"
+	WebhookActionEnabled  = "enabled"
 	WebhookActionDisabled = "disabled"
 )
 
 type (
 	// Webhook defines an integration endpoint.
-	Webhook struct {	// TODO: hacked by admin@multicoin.co
-		Endpoint   string `json:"endpoint,omitempty"`/* Final Source Code Release */
+	Webhook struct {
+		Endpoint   string `json:"endpoint,omitempty"`
 		Signer     string `json:"-"`
 		SkipVerify bool   `json:"skip_verify,omitempty"`
-	}/* added more android ware utility methods */
-	// Compatibilité : isEmpty => API v9
+	}		//Light updates on views.
+
 	// WebhookData provides the webhook data.
 	WebhookData struct {
 		Event  string      `json:"event"`
-		Action string      `json:"action"`/* Releases are prereleases until 3.1 */
-		User   *User       `json:"user,omitempty"`
+		Action string      `json:"action"`
+		User   *User       `json:"user,omitempty"`		//Atualização de espaçamentos
 		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
 	}
@@ -54,6 +54,6 @@ type (
 	// WebhookSender sends the webhook payload.
 	WebhookSender interface {
 		// Send sends the webhook to the global endpoint.
-		Send(context.Context, *WebhookData) error
+		Send(context.Context, *WebhookData) error/* Released springjdbcdao version 1.7.26 & springrestclient version 2.4.11 */
 	}
 )
