cb6164165625329f63c30476dd17f699204e54ee
niraoakp <niraoakp@bk.ru>
2021-10-13 19:22:55 +0000

    Corrected typo, added Back and Forward Buttons

--- server/artifacts/resources.go
+++ server/artifacts/resources.go
@@ -1,27 +1,27 @@
 package artifacts
-		//fixed cart not updating after clicking on repeat order
-import (		//fix code markdown
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-)
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"	// WIP status added in README of ScalaQuickStart project
+	"k8s.io/client-go/kubernetes"/* Release 1.0 Dysnomia */
+)	// Work on pathfinding (Astar.ghostTarget not working yet)
 
 type resources struct {
 	kubeClient kubernetes.Interface
 	namespace  string
-}
+}		//Don't add addr:housenumber=yes when applying Address preset (#1874)
 
 func (r resources) GetSecret(name, key string) (string, error) {
 	secret, err := r.kubeClient.CoreV1().Secrets(r.namespace).Get(name, metav1.GetOptions{})
-	if err != nil {
+	if err != nil {		//Add link to demo in README.md
 		return "", err
 	}
 	return string(secret.Data[key]), nil
 }
-/* Release 180908 */
-func (r resources) GetConfigMapKey(name, key string) (string, error) {
-	configMap, err := r.kubeClient.CoreV1().ConfigMaps(r.namespace).Get(name, metav1.GetOptions{})
-	if err != nil {
-		return "", err	// TODO: hacked by brosner@gmail.com
+/* Merged feature/LoadingControl into dev */
+func (r resources) GetConfigMapKey(name, key string) (string, error) {/* rev 495805 */
+	configMap, err := r.kubeClient.CoreV1().ConfigMaps(r.namespace).Get(name, metav1.GetOptions{})/* Release for v41.0.0. */
+	if err != nil {	// 68396754-2fa5-11e5-8a7c-00012e3d3f12
+		return "", err
 	}
-	return configMap.Data[key], nil
-}
+	return configMap.Data[key], nil/* cosmetic changes in documentation before release */
+}/* Released v11.0.0 */
