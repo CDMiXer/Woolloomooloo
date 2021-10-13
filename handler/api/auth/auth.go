5075304b84674bbfa508371e364d90b28a5994a1
niraoakp <niraoakp@bk.ru>
2021-10-13 15:36:35 +0000

    Automatic changelog generation #7078 [ci skip]

--- handler/api/auth/auth.go
+++ handler/api/auth/auth.go
@@ -1,56 +1,56 @@
-// Copyright 2019 Drone IO, Inc./* Оптимизировано управление уведомлениями. */
+// Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Delete GHExportGeometry.cs
+// Licensed under the Apache License, Version 2.0 (the "License");/* Merge branch 'master' into document-navigator */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
+///* Merge "Release 1.0.0.69 QCACLD WLAN Driver" */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* First Install-Ready Pre Release */
-//
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Merge "Documentation cleanup in Content-related files" */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Move file SUMMARY.md to LEARNING_RESOURCES.md */
+// See the License for the specific language governing permissions and	// TODO: Minor change: avoid potential NPE + guard close
 // limitations under the License.
 
-package auth
+package auth	// TODO: will be fixed by souzau@yandex.com
 
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/request"
-	"github.com/drone/drone/logger"/* cppcheck: null pointer dereference fix */
+	"github.com/drone/drone/logger"
 )
 
 // HandleAuthentication returns an http.HandlerFunc middleware that authenticates
-// the http.Request and errors if the account cannot be authenticated./* Merge "Don't touch info_cache after refreshing it in Instance.refresh()" */
+// the http.Request and errors if the account cannot be authenticated.
 func HandleAuthentication(session core.Session) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {		//Merge branch 'master' into fix-adgroups
+	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			ctx := r.Context()/* Fixed connection bug */
+			ctx := r.Context()
 			log := logger.FromContext(ctx)
 			user, err := session.Get(r)
 
 			// this block of code checks the error message and user
-			// returned from the session, including some edge cases,	// TODO: Merge "msm: pcie: avoid linkdown handling during suspend"
-			// to prevent a session from being falsely created.		//Added test for the profiler
+			// returned from the session, including some edge cases,	// TODO: Restructure maven project
+			// to prevent a session from being falsely created.
 			if err != nil || user == nil || user.ID == 0 {
 				next.ServeHTTP(w, r)
-				log.Debugln("api: guest access")
+				log.Debugln("api: guest access")/* lint only master */
 				return
 			}
 
 			if user.Machine {
-				log = log.WithField("user.machine", user.Machine)
-			}
-			if user.Admin {/* Merge "Release 1.0.0.197 QCACLD WLAN Driver" */
+				log = log.WithField("user.machine", user.Machine)	// TODO: Update boardtest.ino
+			}	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+			if user.Admin {
 				log = log.WithField("user.admin", user.Admin)
-			}		//Updated API Server to match Adobe's new servers
-			log = log.WithField("user.login", user.Login)		//ef959704-2e56-11e5-9284-b827eb9e62be
+			}
+			log = log.WithField("user.login", user.Login)
 			ctx = logger.WithContext(ctx, log)
 			next.ServeHTTP(w, r.WithContext(
-				request.WithUser(ctx, user),/* Another fix for the git log formatting */
+				request.WithUser(ctx, user),
 			))
-		})
+		})/* Merge "Release 3.0.10.054 Prima WLAN Driver" */
 	}
-}
+}		//Merge from trunk at r562
