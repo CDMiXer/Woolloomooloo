410204c2682c4f7bdee1185032fc61ae50d73437
niraoakp <niraoakp@bk.ru>
2021-10-13 21:58:43 +0000

    Created profile page displaying current lift.

--- tests/integration/stack_reference/python/step2/__main__.py
+++ tests/integration/stack_reference/python/step2/__main__.py
@@ -7,12 +7,12 @@ org = config.require('org')
 slug = f"{org}/{pulumi.get_project()}/{pulumi.get_stack()}"
 a = pulumi.StackReference(slug)
 
-got_err = False/* Release v1.3.2 */
+got_err = False
 
-try:	// TODO: will be fixed by boringland@protonmail.ch
+try:
     a.get_output('val2')
 except Exception:
     got_err = True
 
-if not got_err:/* Create Orchard-1-7-2-Release-Notes.markdown */
-    raise Exception('Expected to get error trying to read secret from stack reference.')/* Require `util` instead of the deprecated `sys`. */
+if not got_err:
+    raise Exception('Expected to get error trying to read secret from stack reference.')
