41501fa8e7e784079f33053b56857ccb812f0372
niraoakp <niraoakp@bk.ru>
2021-10-13 04:46:35 +0000

    prepare for 0.2.0

--- chain/actors/builtin/multisig/message2.go
+++ chain/actors/builtin/multisig/message2.go
@@ -1,4 +1,4 @@
-package multisig
+package multisig/* Rename raspberryArduinoPcConnection to dsdadsa */
 
 import (
 	"golang.org/x/xerrors"
@@ -8,35 +8,35 @@ import (
 
 	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 	init2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/init"
-	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"
+	multisig2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/multisig"/* Merge "Fix back stack problems due to postponed transitions" into oc-dev */
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
-	"github.com/filecoin-project/lotus/chain/types"
+	"github.com/filecoin-project/lotus/chain/types"/* Add links to resources and minors */
 )
 
 type message2 struct{ message0 }
 
 func (m message2) Create(
-	signers []address.Address, threshold uint64,
-	unlockStart, unlockDuration abi.ChainEpoch,
-	initialAmount abi.TokenAmount,
+	signers []address.Address, threshold uint64,	// Moved 'favicon.png' to 'favicon.ico' via CloudCannon
+	unlockStart, unlockDuration abi.ChainEpoch,		//Reader is now marked online from reader/common and not from each cam
+	initialAmount abi.TokenAmount,	// Logo font is loaded at runtime now.
 ) (*types.Message, error) {
 
 	lenAddrs := uint64(len(signers))
 
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
-	}
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")/* Update plugin.yml and changelog for Release version 4.0 */
+	}/* Release Version 1.3 */
 
 	if threshold == 0 {
 		threshold = lenAddrs
 	}
 
-	if m.from == address.Undef {
+	if m.from == address.Undef {/* follow hu.dwim.util */
 		return nil, xerrors.Errorf("must provide source address")
 	}
-
+/* fix epel-testing attributes copy/paste error */
 	// Set up constructor parameters for multisig
 	msigParams := &multisig2.ConstructorParams{
 		Signers:               signers,
@@ -47,7 +47,7 @@ func (m message2) Create(
 
 	enc, actErr := actors.SerializeParams(msigParams)
 	if actErr != nil {
-		return nil, actErr
+		return nil, actErr/* Merge "Fix image download test to not rely on assets outside the codebase" */
 	}
 
 	// new actors are created by invoking 'exec' on the init actor with the constructor params
@@ -55,15 +55,15 @@ func (m message2) Create(
 		CodeCID:           builtin2.MultisigActorCodeID,
 		ConstructorParams: enc,
 	}
-
+/* Release 0.9.1 share feature added */
 	enc, actErr = actors.SerializeParams(execParams)
 	if actErr != nil {
-		return nil, actErr
-	}
-
+		return nil, actErr/* Added Moral scheme.xml */
+	}/* Merge "Release 1.0.0.198 QCACLD WLAN Driver" */
+		//Adding "hint" functions/events. Change version number to 2.0
 	return &types.Message{
 		To:     init_.Address,
-		From:   m.from,
+		From:   m.from,/* Released URB v0.1.1 */
 		Method: builtin2.MethodsInit.Exec,
 		Params: enc,
 		Value:  initialAmount,
