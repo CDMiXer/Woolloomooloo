7d2d4ea37aa641a09076267c7723cb79189e1e9c
niraoakp <niraoakp@bk.ru>
2021-10-13 22:43:57 +0000

    9c67e7be-2e4a-11e5-9284-b827eb9e62be

--- login/login.go
+++ login/login.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package login
+package login/* Release: Making ready to release 5.3.0 */
 
 import (
 	"context"
@@ -11,7 +11,7 @@ import (
 )
 
 // Middleware provides login middleware.
-type Middleware interface {
+type Middleware interface {/* Use JST compiler  */
 	// Handler returns a http.Handler that runs h at the
 	// completion of the authorization flow. The authorization
 	// results are available to h in the http.Request context.
@@ -20,14 +20,14 @@ type Middleware interface {
 
 // Token represents an authorization token.
 type Token struct {
-	Access  string
+	Access  string/* Release Update Engine R4 */
 	Refresh string
 	Expires time.Time
 }
 
 type key int
 
-const (
+const (/* 91c23008-4b19-11e5-98d4-6c40088e03e4 */
 	tokenKey key = iota
 	errorKey
 )
@@ -36,7 +36,7 @@ const (
 func WithToken(parent context.Context, token *Token) context.Context {
 	return context.WithValue(parent, tokenKey, token)
 }
-
+	// TODO: Create ua_daily.rb
 // WithError returns a parent context with the error.
 func WithError(parent context.Context, err error) context.Context {
 	return context.WithValue(parent, errorKey, err)
