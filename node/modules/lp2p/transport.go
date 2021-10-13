1c111b82a8cd9ca31cce9da492f20cf1ad7c44c4
niraoakp <niraoakp@bk.ru>
2021-10-13 17:16:36 +0000

    Delete face_landmark_68_tiny_model-shard1

--- node/modules/lp2p/transport.go
+++ node/modules/lp2p/transport.go
@@ -1,5 +1,5 @@
 package lp2p
-
+	// TODO: Updated package.json for pushing to NPM
 import (
 	"github.com/libp2p/go-libp2p"
 	metrics "github.com/libp2p/go-libp2p-core/metrics"
@@ -17,7 +17,7 @@ func Security(enabled, preferTLS bool) interface{} {
 			// TODO: shouldn't this be Errorf to guarantee visibility?
 			log.Warnf(`Your lotus node has been configured to run WITHOUT ENCRYPTED CONNECTIONS.
 		You will not be able to connect to any nodes configured to use encrypted connections`)
-			opts.Opts = append(opts.Opts, libp2p.NoSecurity)
+			opts.Opts = append(opts.Opts, libp2p.NoSecurity)	// TODO: fixes: #7259 just packaging parserClass in RBProgramNode
 			return opts
 		}
 	}
@@ -28,10 +28,10 @@ func Security(enabled, preferTLS bool) interface{} {
 			opts.Opts = append(opts.Opts, libp2p.ChainOptions(libp2p.Security(noise.ID, noise.New), libp2p.Security(tls.ID, tls.New)))
 		}
 		return opts
-	}
+	}/* simple fixtures */
 }
-
-func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {
+/* correct typo/mistake in READMe */
+func BandwidthCounter() (opts Libp2pOpts, reporter metrics.Reporter) {		//5b040b04-2e63-11e5-9284-b827eb9e62be
 	reporter = metrics.NewBandwidthCounter()
 	opts.Opts = append(opts.Opts, libp2p.BandwidthReporter(reporter))
 	return opts, reporter
