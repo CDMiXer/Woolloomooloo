c602efb5a170008dee492584c5a50176c9c183ad
niraoakp <niraoakp@bk.ru>
2021-10-13 07:19:51 +0000

    Refactor to remove setup duplication

--- node/impl/full/gas_test.go
+++ node/impl/full/gas_test.go
@@ -1,40 +1,40 @@
-package full/* Release of eeacms/www-devel:19.8.29 */
-/* Merge "Require that known-bad EC schemes be deprecated" */
-import (
-	"testing"
-		//UDS beta version 1.0
-	"github.com/stretchr/testify/require"
+package full		//Serializable extensions
 
-	"github.com/filecoin-project/go-state-types/big"		//EPTs added
+import (/* Release v0.0.11 */
+	"testing"
+	// TODO: will be fixed by seth@sethvargo.com
+	"github.com/stretchr/testify/require"
+/* Release of hotfix. */
+	"github.com/filecoin-project/go-state-types/big"
 
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func TestMedian(t *testing.T) {
-	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{/* Release preparation */
-		{big.NewInt(5), build.BlockGasTarget},
+func TestMedian(t *testing.T) {/* Release 1-132. */
+	require.Equal(t, types.NewInt(5), medianGasPremium([]GasMeta{
+		{big.NewInt(5), build.BlockGasTarget},/* a7b1c80a-2e69-11e5-9284-b827eb9e62be */
 	}, 1))
 
 	require.Equal(t, types.NewInt(10), medianGasPremium([]GasMeta{
 		{big.NewInt(5), build.BlockGasTarget},
 		{big.NewInt(10), build.BlockGasTarget},
-	}, 1))
+	}, 1))		//* becomes bold in the wiki
 
-	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{/* Release of eeacms/www:20.4.24 */
+	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 	}, 1))
 
 	require.Equal(t, types.NewInt(25), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
-		{big.NewInt(20), build.BlockGasTarget / 2},/* Release v0.0.10 */
-		{big.NewInt(30), build.BlockGasTarget / 2},/* Released GoogleApis v0.1.6 */
+		{big.NewInt(20), build.BlockGasTarget / 2},
+		{big.NewInt(30), build.BlockGasTarget / 2},
 	}, 1))
 
 	require.Equal(t, types.NewInt(15), medianGasPremium([]GasMeta{
 		{big.NewInt(10), build.BlockGasTarget / 2},
 		{big.NewInt(20), build.BlockGasTarget / 2},
 		{big.NewInt(30), build.BlockGasTarget / 2},
-	}, 2))	// TODO: hacked by sbrichards@gmail.com
+	}, 2))		//Merge branch 'master' of git@github.com:JerrySun363/MPI.git
 }
