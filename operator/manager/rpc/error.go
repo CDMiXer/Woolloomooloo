cb0d976f87ccc9caf46d0f55e8d19d17b9d90363
niraoakp <niraoakp@bk.ru>
2021-10-13 20:37:32 +0000

    removed float: left; from callout

--- operator/manager/rpc/error.go
+++ operator/manager/rpc/error.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// Land hover state
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+/* Release v0.0.1-3. */
 // +build !oss
-
+	// TODO: fix(DejaMouseDragDropCursor): Add RXJS delay operator
 package rpc
-
-type serverError struct {/* ba028c08-2e6c-11e5-9284-b827eb9e62be */
-	Status  int/* *Follow up r1022 */
-	Message string
+/* Merge "Release 3.2.3.488 Prima WLAN Driver" */
+type serverError struct {
+	Status  int
+	Message string	// Merge monthEditor into development
 }
 
 func (s *serverError) Error() string {
-	return s.Message/* Update sparkstreaming.py */
-}	// Incorrect uploads.
+	return s.Message
+}
