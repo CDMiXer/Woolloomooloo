3975ca5a8856c27184c1a3da89ab1c8392b9642c
niraoakp <niraoakp@bk.ru>
2021-10-13 01:27:09 +0000

    Updated binary files

--- service/linker/linker_test.go
+++ service/linker/linker_test.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.		//Update Project_1.py
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Release of eeacms/www-devel:18.10.11 */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.	// TODO: Added pip3 to update function
+// limitations under the License.
 
-package linker/* Updating Firebase SDK version */
+package linker
