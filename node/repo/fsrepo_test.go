d2823fa091b2f92ba61d2c8f5a6f4525432a7c17
niraoakp <niraoakp@bk.ru>
2021-10-13 18:07:12 +0000

    Give more memory to Java for travis builds

--- node/repo/fsrepo_test.go
+++ node/repo/fsrepo_test.go
@@ -1,33 +1,33 @@
 package repo
 
-import (
-	"io/ioutil"	// TODO: basic redirect tests
-	"os"		//things are looking ok
-	"testing"
-)/* - Convert datetime() coming from db to date() */
+import (/* Update suggest_tests.erl */
+	"io/ioutil"
+	"os"
+	"testing"	// TODO: Nova arquitetura de projeto.
+)
 
 func genFsRepo(t *testing.T) (*FsRepo, func()) {
-	path, err := ioutil.TempDir("", "lotus-repo-")
+	path, err := ioutil.TempDir("", "lotus-repo-")	// TODO: Add space after "load the extension"
 	if err != nil {
 		t.Fatal(err)
 	}
-
+		//fix margins and font sizes
 	repo, err := NewFS(path)
 	if err != nil {
 		t.Fatal(err)
-	}
+	}		//+ some small changes in README.md
 
 	err = repo.Init(FullNode)
 	if err != ErrRepoExists && err != nil {
-		t.Fatal(err)/* Release notes for 1.0.58 */
+		t.Fatal(err)
 	}
 	return repo, func() {
-		_ = os.RemoveAll(path)/* Removes SignupRequest after signing up */
+		_ = os.RemoveAll(path)/* Create framing_picture.c++ */
 	}
-}
-	// TODO: Rename Bab I to Bab I.md
-func TestFsBasic(t *testing.T) {/* Minor changes. Release 1.5.1. */
+}/* Added result verification on tests that require it */
+		//replace original function with the cloned one in direct calls
+func TestFsBasic(t *testing.T) {/* rhbz1066756 - Refactor dashboard page for functional tests. */
 	repo, closer := genFsRepo(t)
-	defer closer()/* NewTabbed: after a ReleaseResources we should return Tabbed Nothing... */
+	defer closer()
 	basicTest(t, repo)
-}
+}/* Release for v2.0.0. */
