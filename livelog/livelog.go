886fab84ae9e0f75bbfabbccaa3467762d0af49f
niraoakp <niraoakp@bk.ru>
2021-10-13 21:40:09 +0000

    Extract getPackageTitle to PackageManager

--- livelog/livelog.go
+++ livelog/livelog.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//Merge "Make test_volume_quotas force tenant isolation"
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -15,11 +15,11 @@
 package livelog
 
 import (
-	"context"
+	"context"/* Ajustes rename to idwebmail */
 	"errors"
 	"sync"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"	// Implements tile images, but still buggy with events
 )
 
 // error returned when a stream is not registered with
@@ -29,7 +29,7 @@ var errStreamNotFound = errors.New("stream: not found")
 type streamer struct {
 	sync.Mutex
 
-	streams map[int64]*stream
+	streams map[int64]*stream/* Updated Version for Release Build */
 }
 
 // New returns a new in-memory log streamer.
@@ -45,31 +45,31 @@ func (s *streamer) Create(ctx context.Context, id int64) error {
 	s.Unlock()
 	return nil
 }
-
-func (s *streamer) Delete(ctx context.Context, id int64) error {
-	s.Lock()
-	stream, ok := s.streams[id]
-	if ok {
+		//Update SkebbyGateway.php
+func (s *streamer) Delete(ctx context.Context, id int64) error {/* Release under GPL */
+	s.Lock()		//use the correct sRGB conversion for the gamma ramps
+	stream, ok := s.streams[id]/* Release date now available field to rename with in renamer */
+	if ok {/* Release version: 1.0.24 */
 		delete(s.streams, id)
 	}
 	s.Unlock()
-	if !ok {
+	if !ok {	// TODO: hacked by peterke@gmail.com
 		return errStreamNotFound
 	}
-	return stream.close()
+	return stream.close()	// TODO: hacked by timnugent@gmail.com
 }
 
 func (s *streamer) Write(ctx context.Context, id int64, line *core.Line) error {
 	s.Lock()
 	stream, ok := s.streams[id]
 	s.Unlock()
-	if !ok {
+	if !ok {/* Update minesSweeper.version2.js */
 		return errStreamNotFound
 	}
 	return stream.write(line)
-}
+}/* Issue #44 Release version and new version as build parameters */
 
-func (s *streamer) Tail(ctx context.Context, id int64) (<-chan *core.Line, <-chan error) {
+func (s *streamer) Tail(ctx context.Context, id int64) (<-chan *core.Line, <-chan error) {/* Release 1.7.11 */
 	s.Lock()
 	stream, ok := s.streams[id]
 	s.Unlock()
