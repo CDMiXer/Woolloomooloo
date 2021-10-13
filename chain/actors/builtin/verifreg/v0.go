550c7cd05602247e71938eef65881328f5e0b4a4
niraoakp <niraoakp@bk.ru>
2021-10-13 02:00:43 +0000

    1.6.6 release notes

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -1,52 +1,52 @@
 package verifreg
-
+	// TODO: will be fixed by igor@soramitsu.co.jp
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"/* SO-1957: fix compile errors in refset.core classes (WIP) */
-	// README: I meant a dissembler, not a compiler
-	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/ipfs/go-cid"
+
+	"github.com/filecoin-project/lotus/chain/actors"	// TODO: Update Upgrading guide
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
-	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"/* Delete uv4l_setting.txt */
-	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"	// 1st attempt to get travis to complete a run again
+	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
 )
-/* Release 2.2.0.0 */
+
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+	if err != nil {		//Merge "Merge frameworks/base changes from master-skia to master."
 		return nil, err
 	}
 	return &out, nil
-}/* Release jedipus-2.6.7 */
+}
 
 type state0 struct {
-	verifreg0.State
-	store adt.Store
+	verifreg0.State		//js/run: Use $() instead of $(document).ready()
+	store adt.Store		//added initAll stage
 }
 
 func (s *state0) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
-}/* Add description to WithAttributes; remove ...UI from input names */
-	// TODO: removed depreciated method .read()
-func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)	// TODO: Bump version to 2.76.rc2
 }
-/* Added Discord Link & Fixed Apply Link */
+/* Update Input.d.ts */
+func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
+	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
+}
+		//Update Milestone.md
 func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
+	return getDataCap(s.store, actors.Version0, s.verifiers, addr)		//Added log capturing to test.
 }
 
-func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {		//fussing with tabs
+func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version0, s.verifiers, cb)
 }
 
 func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
-}		//Remove unused login attributes
+	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)		//Added more functions to the object function wrapper.
+}
 
 func (s *state0) verifiedClients() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.VerifiedClients)
@@ -54,4 +54,4 @@ func (s *state0) verifiedClients() (adt.Map, error) {
 
 func (s *state0) verifiers() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.Verifiers)
-}
+}/* Removed two unused source files. */
