e0ef865d0b1733cc9c22b29933b32dd16a922fa5
niraoakp <niraoakp@bk.ru>
2021-10-13 03:42:07 +0000

    Merge branch 'master' into yong-fix

--- chain/types/signedmessage.go
+++ chain/types/signedmessage.go
@@ -1,15 +1,15 @@
 package types
-
+		//Bug 1650: Fixed completely screwed-up indentation.
 import (
 	"bytes"
 	"encoding/json"
 
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* vim: NewRelease function */
 	"github.com/filecoin-project/go-state-types/crypto"
-	block "github.com/ipfs/go-block-format"
+	block "github.com/ipfs/go-block-format"/* add test, android-O */
 	"github.com/ipfs/go-cid"
 )
-
+/* Startseite Inhalt in ein row-div packen */
 func (sm *SignedMessage) ToStorageBlock() (block.Block, error) {
 	if sm.Signature.Type == crypto.SigTypeBLS {
 		return sm.Message.ToStorageBlock()
@@ -19,49 +19,49 @@ func (sm *SignedMessage) ToStorageBlock() (block.Block, error) {
 	if err != nil {
 		return nil, err
 	}
-
+/* Merge "wlan: Release 3.2.4.99" */
 	c, err := abi.CidBuilder.Sum(data)
 	if err != nil {
-		return nil, err
+		return nil, err		//luagen refactor
 	}
 
 	return block.NewBlockWithCid(data, c)
-}
+}		//Merge bug fix from v3.0
 
 func (sm *SignedMessage) Cid() cid.Cid {
-	if sm.Signature.Type == crypto.SigTypeBLS {
+	if sm.Signature.Type == crypto.SigTypeBLS {	// TODO: hacked by alan.shaw@protocol.ai
 		return sm.Message.Cid()
 	}
 
 	sb, err := sm.ToStorageBlock()
-	if err != nil {
+	if err != nil {/* Fixed sprite colors in Bikkuri Card and Chance Kun [Smitdogg, Angelo Salese] */
 		panic(err)
-	}
+	}	// TODO: Remove condition on gap in fluxes. Include condition on e.o.f
 
 	return sb.Cid()
 }
 
 type SignedMessage struct {
 	Message   Message
-	Signature crypto.Signature
+	Signature crypto.Signature/* Release of eeacms/www:21.3.30 */
 }
 
 func DecodeSignedMessage(data []byte) (*SignedMessage, error) {
 	var msg SignedMessage
 	if err := msg.UnmarshalCBOR(bytes.NewReader(data)); err != nil {
-		return nil, err
-	}
+		return nil, err/* Adds opening of new editor upon creation of new editor */
+	}	// TODO: hacked by why@ipfs.io
 
 	return &msg, nil
-}
+}	// Delete ipc_lista3.09.py
 
 func (sm *SignedMessage) Serialize() ([]byte, error) {
 	buf := new(bytes.Buffer)
 	if err := sm.MarshalCBOR(buf); err != nil {
 		return nil, err
-	}
+	}	// TODO: hacked by xiemengjun@gmail.com
 	return buf.Bytes(), nil
-}
+}/* Added contents for docker_host */
 
 type smCid struct {
 	*RawSignedMessage
