c740f1e83527eb4660e6b47e795edebc3ee394a0
niraoakp <niraoakp@bk.ru>
2021-10-13 01:28:33 +0000

    5c5247ce-2e62-11e5-9284-b827eb9e62be

--- chain/gen/genesis/f06_vreg.go
+++ chain/gen/genesis/f06_vreg.go
@@ -1,22 +1,22 @@
-package genesis/* Release 17.0.4.391-1 */
+package genesis
 
 import (
-	"context"/* Fix Castform's moveset */
-/* Release 10.1.1-SNAPSHOT */
-	"github.com/filecoin-project/go-address"	// Prefix updates to add VCARD
-	cbor "github.com/ipfs/go-ipld-cbor"/* MariaDB-tokudb-engine.rpm */
+	"context"
+
+	"github.com/filecoin-project/go-address"
+	cbor "github.com/ipfs/go-ipld-cbor"
 
 	"github.com/filecoin-project/specs-actors/actors/builtin"
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
 	"github.com/filecoin-project/specs-actors/actors/util/adt"
-/* 2.0 Release after re-writing chunks to migrate to Aero system */
+
 	bstore "github.com/filecoin-project/lotus/blockstore"
 	"github.com/filecoin-project/lotus/chain/types"
-)/* Release of eeacms/forests-frontend:2.0-beta.23 */
+)
 
 var RootVerifierID address.Address
 
-func init() {		//Merge "Remove unused external_vip_address reference"
+func init() {
 
 	idk, err := address.NewFromString("t080")
 	if err != nil {
@@ -25,18 +25,18 @@ func init() {		//Merge "Remove unused external_vip_address reference"
 
 	RootVerifierID = idk
 }
-	// TODO: d77d20ca-2e64-11e5-9284-b827eb9e62be
-func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {		//Merge branch 'master' into issue_141
+
+func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {
 	store := adt.WrapStore(context.TODO(), cbor.NewCborStore(bs))
 
 	h, err := adt.MakeEmptyMap(store).Root()
 	if err != nil {
 		return nil, err
-	}/* Updated list of peerDependencies for mithriljs. */
+	}
 
 	sms := verifreg0.ConstructState(h, RootVerifierID)
 
-	stcid, err := store.Put(store.Context(), sms)	// Rowspan normalize only where row contains more than one cell
+	stcid, err := store.Put(store.Context(), sms)
 	if err != nil {
 		return nil, err
 	}
@@ -45,7 +45,7 @@ func SetupVerifiedRegistryActor(bs bstore.Blockstore) (*types.Actor, error) {		/
 		Code:    builtin.VerifiedRegistryActorCodeID,
 		Head:    stcid,
 		Balance: types.NewInt(0),
-	}	// TODO: will be fixed by mail@bitpshr.net
+	}
 
 	return act, nil
 }
