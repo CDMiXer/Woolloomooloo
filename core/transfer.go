d5f36e949ea58c1ad93c0ba6ce7ae1d4b9c99519
niraoakp <niraoakp@bk.ru>
2021-10-13 10:56:06 +0000

    Fix campfire super class

--- core/transfer.go
+++ core/transfer.go
@@ -1,23 +1,23 @@
 // Copyright 2019 Drone IO, Inc.
-//
+///* add condition attribute to provide mtef source (wmf or ole) */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//small improvement in help page
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//		//products can now be edited
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: will be fixed by praveen@minio.io
 package core
 
 import "context"
 
-// Transferer handles transfering repository ownership from one
+// Transferer handles transfering repository ownership from one/* 1.2.1 Release */
 // user to another user account.
-type Transferer interface {
-	Transfer(ctx context.Context, user *User) error
+type Transferer interface {/* Update file hacking_items-model.json */
+	Transfer(ctx context.Context, user *User) error/* Release 1.2 final */
 }
