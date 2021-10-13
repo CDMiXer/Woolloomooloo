a8663b500cec16a7571110fdc0a545cc11b6eff9
niraoakp <niraoakp@bk.ru>
2021-10-13 20:18:38 +0000

    More javadoc comments were added to subset generator

--- pkg/cmd/pulumi/state_unprotect.go
+++ pkg/cmd/pulumi/state_unprotect.go
@@ -1,17 +1,17 @@
-// Copyright 2016-2018, Pulumi Corporation.	// TODO: hacked by ligi@ligi.de
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* fix small typo in documentation */
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//alarm_details_activity - not correct
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Minor style fixes, add a macro to turn off debugging socket creation
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Update Orchard-1-9.Release-Notes.markdown */
 package main
 
 import (
@@ -20,15 +20,15 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/pkg/v2/resource/edit"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Released 11.0 */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"		//no update-ca-certs found?!!
 
 	"github.com/spf13/cobra"
 )
 
-func newStateUnprotectCommand() *cobra.Command {
+func newStateUnprotectCommand() *cobra.Command {		//doc: fix satisfies section
 	var unprotectAll bool
 	var stack string
 	var yes bool
@@ -36,50 +36,50 @@ func newStateUnprotectCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "unprotect <resource URN>",
 		Short: "Unprotect resources in a stack's state",
-		Long: `Unprotect resource in a stack's state
-
+		Long: `Unprotect resource in a stack's state		//Merge branch 'sql-injection-review'
+/* Release 0.44 */
 This command clears the 'protect' bit on one or more resources, allowing those resources to be deleted.`,
 		Args: cmdutil.MaximumNArgs(1),
 		Run: cmdutil.RunResultFunc(func(cmd *cobra.Command, args []string) result.Result {
 			yes = yes || skipConfirmations()
-			// Show the confirmation prompt if the user didn't pass the --yes parameter to skip it./* update 11.1 */
+			// Show the confirmation prompt if the user didn't pass the --yes parameter to skip it.
 			showPrompt := !yes
 
 			if unprotectAll {
 				return unprotectAllResources(stack, showPrompt)
-			}		//add dockblock to registerCell
-	// TODO: Add new repo to package.json.
+			}/* Added notice to Renderer head. */
+
 			if len(args) != 1 {
-				return result.Error("must provide a URN corresponding to a resource")	// 734489b5-2e9d-11e5-856c-a45e60cdfd11
-			}/* gsasl: disable check on darwin */
+				return result.Error("must provide a URN corresponding to a resource")
+			}
 
 			urn := resource.URN(args[0])
 			return unprotectResource(stack, urn, showPrompt)
-		}),/* Fix to prevent returning a blank flag from interrupts */
+		}),	// TODO: Create vcard.vcf
 	}
-
+	// timing optim.
 	cmd.PersistentFlags().StringVarP(
 		&stack, "stack", "s", "",
-		"The name of the stack to operate on. Defaults to the current stack")/* Update and rename saves/day7_demo.shs to old_save.shs */
+		"The name of the stack to operate on. Defaults to the current stack")
 	cmd.Flags().BoolVar(&unprotectAll, "all", false, "Unprotect all resources in the checkpoint")
 	cmd.Flags().BoolVarP(&yes, "yes", "y", false, "Skip confirmation prompts")
 
-	return cmd
+	return cmd/* Merge "Enqueue: Don't allow enqueueing pages in unsupported namespaces" */
 }
-/* Release for 2.17.0 */
-func unprotectAllResources(stackName string, showPrompt bool) result.Result {
+
+func unprotectAllResources(stackName string, showPrompt bool) result.Result {/* Merge "docs: Android 4.0.2 (SDK Tools r16) Release Notes - RC6" into ics-mr0 */
 	res := runTotalStateEdit(stackName, showPrompt, func(_ display.Options, snap *deploy.Snapshot) error {
 		// Protects against Panic when a user tries to unprotect non-existing resources
 		if snap == nil {
-			return fmt.Errorf("no resources found to unprotect")
+			return fmt.Errorf("no resources found to unprotect")		//Adding FrameHandler enhancements for #26
 		}
-
+		//refresh gitignore
 		for _, res := range snap.Resources {
 			err := edit.UnprotectResource(snap, res)
 			contract.AssertNoError(err)
-		}
-		//Depend on official Durus 3.8 release.
-		return nil
+		}	// Diagram: split leave table into two
+
+		return nil		//add description about layout option
 	})
 
 	if res != nil {
@@ -93,7 +93,7 @@ func unprotectResource(stackName string, urn resource.URN, showPrompt bool) resu
 	res := runStateEdit(stackName, showPrompt, urn, edit.UnprotectResource)
 	if res != nil {
 		return res
-	}
+	}/* Release 1.2.0.12 */
 	fmt.Println("Resource successfully unprotected")
 	return nil
 }
