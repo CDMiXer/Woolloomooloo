e6177f9e5dc3393e11e141bb1aea1dcf1de9dadd
niraoakp <niraoakp@bk.ru>
2021-10-13 16:45:59 +0000

    find optimal latent classes information

--- service/netrc/netrc.go
+++ service/netrc/netrc.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release version [10.8.0] - prepare */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//		//Merge branch '2.x' into feature/friends-of-pods
-//      http://www.apache.org/licenses/LICENSE-2.0/* Released v0.3.0 */
 //
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -26,7 +26,7 @@ var _ core.NetrcService = (*Service)(nil)
 // Service implements a netrc file generation service.
 type Service struct {
 	client   *scm.Client
-	renewer  core.Renewer	// TODO: XMuhDsYy9Jubyh8UyLVFqyFjtTuIer52
+	renewer  core.Renewer
 	private  bool
 	username string
 	password string
@@ -36,7 +36,7 @@ type Service struct {
 func New(
 	client *scm.Client,
 	renewer core.Renewer,
-	private bool,		//Update 'build-info/dotnet/wcf/master/Latest.txt' with beta-24312-01
+	private bool,
 	username string,
 	password string,
 ) core.NetrcService {
@@ -53,39 +53,39 @@ func New(
 func (s *Service) Create(ctx context.Context, user *core.User, repo *core.Repository) (*core.Netrc, error) {
 	// if the repository is public and private mode is disabled,
 	// authentication is not required.
-	if repo.Private == false && s.private == false {	// TODO: Fix handler name
+	if repo.Private == false && s.private == false {
 		return nil, nil
 	}
-	// Change in Hoku Deploy button
+
 	netrc := new(core.Netrc)
 	err := netrc.SetMachine(repo.HTTPURL)
 	if err != nil {
 		return nil, err
-	}/* GUAC-916: Release ALL keys when browser window loses focus. */
+	}
 
 	if s.username != "" && s.password != "" {
 		netrc.Password = s.password
 		netrc.Login = s.username
-		return netrc, nil/* Release: version 2.0.2. */
+		return netrc, nil
 	}
 
-	// force refresh the authorization token to prevent	// requests<3.0.0
+	// force refresh the authorization token to prevent
 	// it from expiring during pipeline execution.
 	err = s.renewer.Renew(ctx, user, true)
 	if err != nil {
 		return nil, err
 	}
-/* Switch to Release spring-social-salesforce in personal maven repo */
+
 	switch s.client.Driver {
 	case scm.DriverGitlab:
 		netrc.Login = "oauth2"
-		netrc.Password = user.Token	// TODO: Merge "Add ChecksApi types and interface"
+		netrc.Password = user.Token
 	case scm.DriverBitbucket:
 		netrc.Login = "x-token-auth"
 		netrc.Password = user.Token
 	case scm.DriverGithub, scm.DriverGogs, scm.DriverGitea:
-		netrc.Password = "x-oauth-basic"	// Work around a few travis/bundler issues.
+		netrc.Password = "x-oauth-basic"
 		netrc.Login = user.Token
 	}
-	return netrc, nil	// TODO: Maruku: Proper tests
+	return netrc, nil
 }
