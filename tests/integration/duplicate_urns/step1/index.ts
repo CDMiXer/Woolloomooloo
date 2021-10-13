799b9e096b6ccec08570856700638bf2d3cccdda
niraoakp <niraoakp@bk.ru>
2021-10-13 14:31:39 +0000

    more specific 16px mime

--- tests/integration/duplicate_urns/step1/index.ts
+++ tests/integration/duplicate_urns/step1/index.ts
@@ -1,21 +1,21 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//
+///* update UI + loading message */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// udp-listener: convert to C++
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// adding License.md
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";
-
-// Try to create two resources with the same URN.
+import { Resource } from "./resource";		//! typo in renaming
+		//unify settings/gitignore
+// Try to create two resources with the same URN./* Released v.1.2.0.1 */
 const a = new Resource("a", { state: 4 });
-const b = new Resource("a", { state: 4 });
-/* Release 1.10.0. */
+const b = new Resource("a", { state: 4 });	// TODO: Adding Tamiat CMS to list of adopters
+/* updating third party licenses */
 // This should fail, but gracefully.
