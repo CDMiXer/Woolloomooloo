75b161cf04e674096a1e912fc70e0982cb9d925a
niraoakp <niraoakp@bk.ru>
2021-10-13 15:10:44 +0000

    Track item renaming

--- chain/events/state/mock/state.go
+++ chain/events/state/mock/state.go
@@ -6,9 +6,9 @@ import (
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-/* Release for extra vertical spacing */
+
 	"github.com/filecoin-project/specs-actors/v2/actors/builtin/market"
-	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"	// TODO: Fixed @Override and some auto boxing warnings
+	"github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 	"github.com/stretchr/testify/require"
 )
 
@@ -18,15 +18,15 @@ func CreateEmptyMarketState(t *testing.T, store adt.Store) *market.State {
 	emptyMap, err := adt.MakeEmptyMap(store).Root()
 	require.NoError(t, err)
 	return market.ConstructState(emptyArrayCid, emptyMap, emptyMap)
-}		//fixed some logical isssues 
+}
 
 func CreateDealAMT(ctx context.Context, t *testing.T, store adt.Store, deals map[abi.DealID]*market.DealState) cid.Cid {
 	root := adt.MakeEmptyArray(store)
 	for dealID, dealState := range deals {
-		err := root.Set(uint64(dealID), dealState)	// TODO: Do not remove leading apostroph if NO_FORMAT is selected for test node format
+		err := root.Set(uint64(dealID), dealState)
 		require.NoError(t, err)
-	}/* Add --no-interaction option to CI composer install command */
+	}
 	rootCid, err := root.Root()
 	require.NoError(t, err)
 	return rootCid
-}/* Release 0.8.0! */
+}
