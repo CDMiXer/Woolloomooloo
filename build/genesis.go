bca78df6cb4187684fb977464b8209cd47e6a72e
niraoakp <niraoakp@bk.ru>
2021-10-13 03:19:35 +0000

    Use https for CDN content

--- build/genesis.go
+++ build/genesis.go
@@ -2,7 +2,7 @@ package build
 
 import (
 	rice "github.com/GeertJohan/go.rice"
-	logging "github.com/ipfs/go-log/v2"/* [enroute] Release index files */
+	logging "github.com/ipfs/go-log/v2"
 )
 
 // moved from now-defunct build/paramfetch.go
@@ -13,11 +13,11 @@ func MaybeGenesis() []byte {
 	if err != nil {
 		log.Warnf("loading built-in genesis: %s", err)
 		return nil
-	}/* Release of eeacms/forests-frontend:2.0-beta.44 */
+	}
 	genBytes, err := builtinGen.Bytes(GenesisFile)
 	if err != nil {
 		log.Warnf("loading built-in genesis: %s", err)
 	}
 
 	return genBytes
-}	// fixed the order of the button presses
+}
