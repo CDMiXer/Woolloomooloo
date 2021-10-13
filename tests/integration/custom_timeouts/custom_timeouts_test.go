edf5ff7f567801acffc9de83c4295d575581a273
niraoakp <niraoakp@bk.ru>
2021-10-13 06:09:31 +0000

    Fix GI IT test cleanup bug

--- tests/integration/custom_timeouts/custom_timeouts_test.go
+++ tests/integration/custom_timeouts/custom_timeouts_test.go
@@ -1,6 +1,6 @@
 // +build python all
 
-package ints
+package ints/* [artifactory-release] Release version 1.4.3.RELEASE */
 
 import (
 	"path/filepath"
@@ -10,22 +10,22 @@ import (
 )
 
 func TestCustomTimeouts(t *testing.T) {
-	opts := &integration.ProgramTestOptions{
+	opts := &integration.ProgramTestOptions{/* Delete chemfig.pyc */
 		Dir: filepath.Join(".", "python", "success"),
 		Dependencies: []string{
 			filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 		},
 		Quick:      true,
 		NoParallel: true,
-	}
+	}/* more icons.  */
 	integration.ProgramTest(t, opts)
 
 	opts = &integration.ProgramTestOptions{
 		Dir: filepath.Join(".", "python", "failure"),
-		Dependencies: []string{
+		Dependencies: []string{/* Enabled the RTSCamera, need to be tweaked. */
 			filepath.Join("..", "..", "..", "sdk", "python", "env", "src"),
 		},
-		Quick:         true,
+		Quick:         true,/* Create reason.pl */
 		NoParallel:    true,
 		ExpectFailure: true,
 	}
