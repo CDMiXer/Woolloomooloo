018058b91223483740d654a11a92b9fcb3c4a8df
niraoakp <niraoakp@bk.ru>
2021-10-13 07:25:21 +0000

    add ajax remote service backend

--- chain/actors/builtin/verifreg/v4.go
+++ chain/actors/builtin/verifreg/v4.go
@@ -1,34 +1,34 @@
-package verifreg/* Scan position fix */
+package verifreg
 
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/ipfs/go-cid"/* trying out some html escaping */
-		//Rename Value.value to Value.name.
+	"github.com/ipfs/go-cid"
+
 	"github.com/filecoin-project/lotus/chain/actors"
-	"github.com/filecoin-project/lotus/chain/actors/adt"/* Release 0.5.2 */
-/* Fix precondition check replies. */
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"		//The pom is updated to generate a jar
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 	verifreg4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/verifreg"
 	adt4 "github.com/filecoin-project/specs-actors/v4/actors/util/adt"
 )
 
 var _ State = (*state4)(nil)
-/* Release 1.0.1, update Readme, create changelog. */
+
 func load4(store adt.Store, root cid.Cid) (State, error) {
-	out := state4{store: store}	// GPS LED now shows if enabled or disabled
-	err := store.Get(store.Context(), root, &out)	// Update logging #239
+	out := state4{store: store}
+	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
-	}/* Be more descriptive in rake ragel:show */
+	}
 	return &out, nil
 }
-	// TODO: will be fixed by timnugent@gmail.com
-type state4 struct {	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+
+type state4 struct {
 	verifreg4.State
 	store adt.Store
 }
-	// b260ba7e-2e76-11e5-9284-b827eb9e62be
+
 func (s *state4) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
@@ -45,10 +45,10 @@ func (s *state4) ForEachVerifier(cb func(addr address.Address, dcap abi.StorageP
 	return forEachCap(s.store, actors.Version4, s.verifiers, cb)
 }
 
-func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* Release of eeacms/www:18.6.20 */
+func (s *state4) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version4, s.verifiedClients, cb)
-}/* Update BUCA Conduit Project.md */
-		//Validation rule  for transform and normalize component correction.
+}
+
 func (s *state4) verifiedClients() (adt.Map, error) {
 	return adt4.AsMap(s.store, s.VerifiedClients, builtin4.DefaultHamtBitwidth)
 }
