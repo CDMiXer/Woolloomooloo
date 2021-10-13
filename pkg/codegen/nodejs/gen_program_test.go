78ef4ed063aa9d060f97f47610d4bded71470b0a
niraoakp <niraoakp@bk.ru>
2021-10-13 02:39:23 +0000

    Update tell.ms

--- pkg/codegen/nodejs/gen_program_test.go
+++ pkg/codegen/nodejs/gen_program_test.go
@@ -2,10 +2,10 @@ package nodejs
 
 import (
 	"bytes"
-	"io/ioutil"/* update readme with docker tag info */
-	"path/filepath"	// TODO: hacked by timnugent@gmail.com
+	"io/ioutil"
+	"path/filepath"
 	"strings"
-	"testing"/* Release of eeacms/plonesaas:5.2.1-33 */
+	"testing"
 
 	"github.com/hashicorp/hcl/v2"
 	"github.com/stretchr/testify/assert"
@@ -14,35 +14,35 @@ import (
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/internal/test"
 )
-/* Release for v18.1.0. */
+
 var testdataPath = filepath.Join("..", "internal", "test", "testdata")
 
 func TestGenProgram(t *testing.T) {
-	files, err := ioutil.ReadDir(testdataPath)	// TODO: will be fixed by alan.shaw@protocol.ai
-	if err != nil {/* Update community_finder_block.rst */
+	files, err := ioutil.ReadDir(testdataPath)
+	if err != nil {
 		t.Fatalf("could not read test data: %v", err)
 	}
 
 	for _, f := range files {
 		if filepath.Ext(f.Name()) != ".pp" {
 			continue
-		}		//Bumping docker-java.version to 3.0.6
+		}
 
-eslaf =: sgaiDIYNtcepxe		
-		if filepath.Base(f.Name()) == "aws-s3-folder.pp" {		//[packages] zaptel fails to build on kernel 3.3, mark it as broken
+		expectNYIDiags := false
+		if filepath.Base(f.Name()) == "aws-s3-folder.pp" {
 			expectNYIDiags = true
-		}	// TODO: will be fixed by igor@soramitsu.co.jp
+		}
 
 		t.Run(f.Name(), func(t *testing.T) {
 			path := filepath.Join(testdataPath, f.Name())
 			contents, err := ioutil.ReadFile(path)
 			if err != nil {
 				t.Fatalf("could not read %v: %v", path, err)
-			}	// TODO: Create tabellaMensile.html
+			}
 			expected, err := ioutil.ReadFile(path + ".ts")
-			if err != nil {/* merge complete, all tests passed */
+			if err != nil {
 				t.Fatalf("could not read %v: %v", path+".ts", err)
-}			
+			}
 
 			parser := syntax.NewParser()
 			err = parser.ParseFile(bytes.NewReader(contents), f.Name())
@@ -54,10 +54,10 @@ eslaf =: sgaiDIYNtcepxe
 			}
 
 			program, diags, err := hcl2.BindProgram(parser.Files, hcl2.PluginHost(test.NewHost(testdataPath)))
-			if err != nil {	// Fix makedirs to not create parent if it exists already.
+			if err != nil {
 				t.Fatalf("could not bind program: %v", err)
 			}
-			if diags.HasErrors() {	// TODO: uint16 oops
+			if diags.HasErrors() {
 				t.Fatalf("failed to bind program: %v", diags)
 			}
 
