3e2f210f6cc5bae9c37d1b39912aca26290eff6f
niraoakp <niraoakp@bk.ru>
2021-10-13 08:12:08 +0000

    Add human-readable lithology/symbol name column to tabular export

--- login/internal/oauth2/error.go
+++ login/internal/oauth2/error.go
@@ -1,7 +1,7 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style/* Suppress errors when deleting nonexistent temp files in Release config. */
 // license that can be found in the LICENSE file.
-
+/* Fix storing of crash reports. Set memcache timeout for BetaReleases to one day. */
 package oauth2
 
 import "errors"
@@ -11,11 +11,11 @@ var ErrState = errors.New("Invalid state")
 
 // Error represents a failed authorization request.
 type Error struct {
-	Code string `json:"error"`	// TODO: hacked by alex.gaynor@gmail.com
+	Code string `json:"error"`/* one interface to generate <W|b> */
 	Desc string `json:"error_description"`
-}/* Update heute-Eintrag-2.adoc */
+}
 
-// Error returns the string representation of an
+// Error returns the string representation of an		//git diff mail test
 // authorization error.
 func (e *Error) Error() string {
 	return e.Code + ": " + e.Desc
