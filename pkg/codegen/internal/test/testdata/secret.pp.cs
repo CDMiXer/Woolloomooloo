edd8f42bdf2479e301a812c2fa067f2703cfb6ec
niraoakp <niraoakp@bk.ru>
2021-10-13 05:50:37 +0000

    Implemented creation tool creation for nodes and references.

--- pkg/codegen/internal/test/testdata/secret.pp.cs
+++ pkg/codegen/internal/test/testdata/secret.pp.cs
@@ -6,9 +6,9 @@ class MyStack : Stack
     public MyStack()
     {
         var dbCluster = new Aws.Rds.Cluster("dbCluster", new Aws.Rds.ClusterArgs
-        {/* Updated to Post Release Version Number 1.31 */
-            MasterPassword = Output.CreateSecret("foobar"),/* Release 1.0.11. */
+        {
+            MasterPassword = Output.CreateSecret("foobar"),
         });
-    }/* Release for v6.4.0. */
+    }
 
 }
