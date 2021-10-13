b75572f7dc5f0a8f48b13144b97767624ccae744
niraoakp <niraoakp@bk.ru>
2021-10-13 08:56:31 +0000

    fix proto as input in python

--- handler/api/repos/builds/deploys/create.go
+++ handler/api/repos/builds/deploys/create.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: Merge "[INTERNAL] m.Tree: invalidation is added for tree and treeitems"
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+//      http://www.apache.org/licenses/LICENSE-2.0	// updated to new fabric8 version
+///* Adding current trunk revision to tag (Release: 0.8) */
+// Unless required by applicable law or agreed to in writing, software	// Attempting to resolve list rendering issues
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
