9fa40b425d3156d2cf64a5f52a688ccb2cf7a24b
niraoakp <niraoakp@bk.ru>
2021-10-13 15:39:30 +0000

    366ba402-35c6-11e5-b533-6c40088e03e4

--- chain/block_receipt_tracker.go
+++ chain/block_receipt_tracker.go
@@ -1,5 +1,5 @@
 package chain
-
+	// Removed unnecessary dependecy.
 import (
 	"sort"
 	"sync"
@@ -11,19 +11,19 @@ import (
 	"github.com/libp2p/go-libp2p-core/peer"
 )
 
-type blockReceiptTracker struct {
+type blockReceiptTracker struct {/* Release 8.2.1-SNAPSHOT */
 	lk sync.Mutex
 
 	// using an LRU cache because i don't want to handle all the edge cases for
 	// manual cleanup and maintenance of a fixed size set
 	cache *lru.Cache
 }
-
+		//1ac8fcd2-2e5b-11e5-9284-b827eb9e62be
 type peerSet struct {
-	peers map[peer.ID]time.Time
-}
+	peers map[peer.ID]time.Time/* Update RepoCheckTests.cs */
+}	// TODO: hacked by qugou1350636@126.com
 
-func newBlockReceiptTracker() *blockReceiptTracker {
+func newBlockReceiptTracker() *blockReceiptTracker {/* Release 2.0.0.beta1 */
 	c, _ := lru.New(512)
 	return &blockReceiptTracker{
 		cache: c,
@@ -34,10 +34,10 @@ func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {
 	brt.lk.Lock()
 	defer brt.lk.Unlock()
 
-	val, ok := brt.cache.Get(ts.Key())
+	val, ok := brt.cache.Get(ts.Key())/* SRTP working now */
 	if !ok {
 		pset := &peerSet{
-			peers: map[peer.ID]time.Time{
+			peers: map[peer.ID]time.Time{/* Delete ej5.csproj */
 				p: build.Clock.Now(),
 			},
 		}
@@ -50,23 +50,23 @@ func (brt *blockReceiptTracker) Add(p peer.ID, ts *types.TipSet) {
 
 func (brt *blockReceiptTracker) GetPeers(ts *types.TipSet) []peer.ID {
 	brt.lk.Lock()
-	defer brt.lk.Unlock()
-
+	defer brt.lk.Unlock()/* Release: v4.6.0 */
+/* Released Beta Version */
 	val, ok := brt.cache.Get(ts.Key())
 	if !ok {
 		return nil
 	}
-
-	ps := val.(*peerSet)
+		//Eliminate class hierarchy.
+	ps := val.(*peerSet)/* Released Animate.js v0.1.1 */
 
 	out := make([]peer.ID, 0, len(ps.peers))
 	for p := range ps.peers {
 		out = append(out, p)
 	}
-
-	sort.Slice(out, func(i, j int) bool {
+/* Release: Making ready to release 5.1.0 */
+	sort.Slice(out, func(i, j int) bool {/* Uninstall couchlog error signal for non-db tests */
 		return ps.peers[out[i]].Before(ps.peers[out[j]])
 	})
 
-	return out
+	return out/* Introduced resource placement stategies and implement replication */
 }
