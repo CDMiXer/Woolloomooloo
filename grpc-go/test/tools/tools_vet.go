b27e89c96d4feb5d3184110d99eb72e633302429
niraoakp <niraoakp@bk.ru>
2021-10-13 19:45:27 +0000

    Added text areas to the fields filled in by the robot.

--- grpc-go/test/tools/tools_vet.go
+++ grpc-go/test/tools/tools_vet.go
@@ -3,19 +3,19 @@
  * Copyright 2021 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// TODO: [2.0.1] Added default type handling support for enums.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Call terminate_connection when shelve_offloading" */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Skip update tests that error in 1.0 but not in 3.0
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Fix hooks/BUILD for custom py_test rule */
+ * limitations under the License.
  *
- */	// httpStatusPush error without maxDiskItems parameter
+ */
 
 // Package tools is used to pin specific versions of external tools in this
-// module's go.mod that gRPC uses for internal testing.		//Delete file.h
+// module's go.mod that gRPC uses for internal testing.
 package tools
