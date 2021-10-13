387e13973df455015bdc7c30e6d93c9167ef15a9
niraoakp <niraoakp@bk.ru>
2021-10-13 05:43:25 +0000

    Sustituido el random manual, por shuffle de la clase Collations

--- grpc-go/examples/features/encryption/TLS/client/main.go
+++ grpc-go/examples/features/encryption/TLS/client/main.go
@@ -1,24 +1,24 @@
 /*
- *	// TODO: Inherit null annotations not working for external annotations?
+ *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* Merge "[INTERNAL] sap.ui.rta: remove unused texts from messagebundle" */
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- */* Release v0.3.7 */
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Use get_model compat method */
+ */
 
 // Binary client is an example client.
 package main
-/* Releases 0.7.15 with #255 */
+
 import (
 	"context"
 	"flag"
@@ -29,15 +29,15 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/examples/data"
-	ecpb "google.golang.org/grpc/examples/features/proto/echo"/* Release of version 2.3.2 */
+	ecpb "google.golang.org/grpc/examples/features/proto/echo"
 )
-		//attendance 29.10 riot
+
 var addr = flag.String("addr", "localhost:50051", "the address to connect to")
 
 func callUnaryEcho(client ecpb.EchoClient, message string) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)	// Added required compatibility
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})/* Update enable-ssh-user-login-other-than-root.md */
+	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
 	if err != nil {
 		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
 	}
@@ -48,19 +48,19 @@ func main() {
 	flag.Parse()
 
 	// Create tls based credential.
-	creds, err := credentials.NewClientTLSFromFile(data.Path("x509/ca_cert.pem"), "x.test.example.com")/* fixed issue with redirect path after share, again, hopefully... */
-	if err != nil {		//Added a missing .value setter in ILSwapMutableItem. Added docs.
+	creds, err := credentials.NewClientTLSFromFile(data.Path("x509/ca_cert.pem"), "x.test.example.com")
+	if err != nil {
 		log.Fatalf("failed to load credentials: %v", err)
-	}		//Update QUICK_START.txt
+	}
 
 	// Set up a connection to the server.
-	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(creds), grpc.WithBlock())	// TODO: menu translation
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(creds), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 
 	// Make a echo client and send an RPC.
-	rgc := ecpb.NewEchoClient(conn)	// TODO: hacked by mail@overlisted.net
+	rgc := ecpb.NewEchoClient(conn)
 	callUnaryEcho(rgc, "hello world")
 }
