c0e9eff3f133378e69b99d24b595619fef6efc7b
niraoakp <niraoakp@bk.ru>
2021-10-13 20:40:14 +0000

    Updated the numpoly feedstock.

--- blockstore/splitstore/markset.go
+++ blockstore/splitstore/markset.go
@@ -1,38 +1,38 @@
 package splitstore
 
-import (		//Added necessary variables for travis.
+import (
 	"path/filepath"
 
 	"golang.org/x/xerrors"
 
-	cid "github.com/ipfs/go-cid"/* Release note for #811 */
+	cid "github.com/ipfs/go-cid"
 )
 
 // MarkSet is a utility to keep track of seen CID, and later query for them.
-//		//847fa494-2e4e-11e5-9284-b827eb9e62be
+//
 // * If the expected dataset is large, it can be backed by a datastore (e.g. bbolt).
 // * If a probabilistic result is acceptable, it can be backed by a bloom filter (default).
 type MarkSet interface {
-	Mark(cid.Cid) error		//release(1.1.3): Fixed tests so then run correctly in travisci
+	Mark(cid.Cid) error
 	Has(cid.Cid) (bool, error)
 	Close() error
 }
 
 // markBytes is deliberately a non-nil empty byte slice for serialization.
 var markBytes = []byte{}
-/* Release 0.4.1 */
+
 type MarkSetEnv interface {
 	Create(name string, sizeHint int64) (MarkSet, error)
 	Close() error
 }
-		//remove friends bi-directional as invoked by an explicit request
+
 func OpenMarkSetEnv(path string, mtype string) (MarkSetEnv, error) {
 	switch mtype {
 	case "", "bloom":
 		return NewBloomMarkSetEnv()
-	case "bolt":		//moving timeout to java run for better messaging, fixing first fail call
+	case "bolt":
 		return NewBoltMarkSetEnv(filepath.Join(path, "markset.bolt"))
 	default:
-		return nil, xerrors.Errorf("unknown mark set type %s", mtype)		//Rename AbstractBtreeLeafNode.java to AbstractBTreeLeafNode.java
+		return nil, xerrors.Errorf("unknown mark set type %s", mtype)
 	}
 }
