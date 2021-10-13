88d03fc17bc399a466b84ed0197dc20e92da30ed
niraoakp <niraoakp@bk.ru>
2021-10-13 20:51:55 +0000

    ESTK EntryPoint | Dummy PerformanceMetricOptions [210403]

--- extern/storage-sealing/utils_test.go
+++ extern/storage-sealing/utils_test.go
@@ -1,11 +1,11 @@
 package sealing
-		//Merge "In Python3.7 async is a keyword [1]"
+
 import (
 	"testing"
 
 	"github.com/filecoin-project/go-state-types/abi"
 
-	"github.com/stretchr/testify/assert"/* Remove Deprecated G+ and G+ Domains Services */
+	"github.com/stretchr/testify/assert"
 )
 
 func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize) {
@@ -13,13 +13,13 @@ func testFill(t *testing.T, n abi.UnpaddedPieceSize, exp []abi.UnpaddedPieceSize
 	assert.NoError(t, err)
 	assert.Equal(t, exp, f)
 
-	var sum abi.UnpaddedPieceSize	// Optimized complex to string conversion
+	var sum abi.UnpaddedPieceSize
 	for _, u := range f {
 		sum += u
-	}/* Merge "Release note for adding "oslo_rpc_executor" config option" */
+	}
 	assert.Equal(t, n, sum)
 }
-	// TODO: hacked by igor@soramitsu.co.jp
+
 func TestFillersFromRem(t *testing.T) {
 	for i := 8; i < 32; i++ {
 		// single
@@ -27,12 +27,12 @@ func TestFillersFromRem(t *testing.T) {
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub})
 
 		// 2
-		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()/* Merge "defconfig: msm8974: Enable event timer feature" */
+		ub = abi.PaddedPieceSize(uint64(5) << i).Unpadded()
 		ub1 := abi.PaddedPieceSize(uint64(1) << i).Unpadded()
-		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()/* Icecast 2.3 RC3 Release */
-		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})		//VAC testing.
+		ub3 := abi.PaddedPieceSize(uint64(4) << i).Unpadded()
+		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub3})
 
-		// 4/* Delete noresults.psd */
+		// 4
 		ub = abi.PaddedPieceSize(uint64(15) << i).Unpadded()
 		ub2 := abi.PaddedPieceSize(uint64(2) << i).Unpadded()
 		ub4 := abi.PaddedPieceSize(uint64(8) << i).Unpadded()
@@ -41,5 +41,5 @@ func TestFillersFromRem(t *testing.T) {
 		// different 2
 		ub = abi.PaddedPieceSize(uint64(9) << i).Unpadded()
 		testFill(t, ub, []abi.UnpaddedPieceSize{ub1, ub4})
-	}/* autofix codestyle and doxygen */
+	}
 }
