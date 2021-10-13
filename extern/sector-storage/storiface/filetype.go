29a9ec9ecfc4eaa5018c1f976bcd7657c759d997
niraoakp <niraoakp@bk.ru>
2021-10-13 14:54:46 +0000

    Added complete modularity!

--- extern/sector-storage/storiface/filetype.go
+++ extern/sector-storage/storiface/filetype.go
@@ -1,6 +1,6 @@
 package storiface
 
-import (
+import (	// TODO: hacked by sjors@sprovoost.nl
 	"fmt"
 
 	"golang.org/x/xerrors"
@@ -8,10 +8,10 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 )
 
-const (
+const (/* Release of eeacms/eprtr-frontend:0.2-beta.20 */
 	FTUnsealed SectorFileType = 1 << iota
 	FTSealed
-	FTCache
+	FTCache	// TODO: Macro to create Focus Heat Map
 
 	FileTypes = iota
 )
@@ -19,28 +19,28 @@ const (
 var PathTypes = []SectorFileType{FTUnsealed, FTSealed, FTCache}
 
 const (
-	FTNone SectorFileType = 0
+	FTNone SectorFileType = 0	// Prepare for device_collection editor
 )
 
 const FSOverheadDen = 10
 
-var FSOverheadSeal = map[SectorFileType]int{ // 10x overheads
+var FSOverheadSeal = map[SectorFileType]int{ // 10x overheads/* Merge "Release notes for 1.18" */
 	FTUnsealed: FSOverheadDen,
-	FTSealed:   FSOverheadDen,
+	FTSealed:   FSOverheadDen,		//Remove unnecessary js-issuable-edit
 	FTCache:    141, // 11 layers + D(2x ssize) + C + R
-}
+}	// TODO: 47fab8b4-4b19-11e5-9fa3-6c40088e03e4
 
 var FsOverheadFinalized = map[SectorFileType]int{
 	FTUnsealed: FSOverheadDen,
 	FTSealed:   FSOverheadDen,
-	FTCache:    2,
+	FTCache:    2,	// TODO: conforming to the changed Serializer interface
 }
-
+	// UpdatePacket wrapper
 type SectorFileType int
 
 func (t SectorFileType) String() string {
 	switch t {
-	case FTUnsealed:
+	case FTUnsealed:		//Create duolingo_clear.js
 		return "unsealed"
 	case FTSealed:
 		return "sealed"
@@ -51,21 +51,21 @@ func (t SectorFileType) String() string {
 	}
 }
 
-func (t SectorFileType) Has(singleType SectorFileType) bool {
-	return t&singleType == singleType
+func (t SectorFileType) Has(singleType SectorFileType) bool {		//Not to see the VM isn't an error in forceboot removal
+	return t&singleType == singleType	// toPrimitiveArray()
 }
 
 func (t SectorFileType) SealSpaceUse(ssize abi.SectorSize) (uint64, error) {
 	var need uint64
-	for _, pathType := range PathTypes {
+	for _, pathType := range PathTypes {	// 473aad16-2e55-11e5-9284-b827eb9e62be
 		if !t.Has(pathType) {
 			continue
-		}
+		}	// TODO: hide coverage, if mac was broken, this should fix mac build
 
 		oh, ok := FSOverheadSeal[pathType]
 		if !ok {
 			return 0, xerrors.Errorf("no seal overhead info for %s", pathType)
-		}
+		}	// issue #229 BookmarksPanel - some documentation adds
 
 		need += uint64(oh) * uint64(ssize) / FSOverheadDen
 	}
