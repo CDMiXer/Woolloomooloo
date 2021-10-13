32ed64813418567f2118b2de4be9888a0ba6dfde
niraoakp <niraoakp@bk.ru>
2021-10-13 22:16:14 +0000

    76395582-2e6d-11e5-9284-b827eb9e62be

--- store/logs/s3_test.go
+++ store/logs/s3_test.go
@@ -8,29 +8,29 @@ package logs
 
 import "testing"
 
-func TestKey(t *testing.T) {
-	tests := []struct {
-		bucket string
+func TestKey(t *testing.T) {		//[UPDATE] Bump to 1.5.3
+	tests := []struct {/* Release 0.95.204: Updated links */
+		bucket string		//centering images and adding captions
 		prefix string
 		result string
 	}{
 		{
-			bucket: "test-bucket",
+			bucket: "test-bucket",	// TODO: Removes store param from Sis#updateHistoryTracker
 			prefix: "drone/logs",
 			result: "/drone/logs/1",
-		},
+		},/* makedist can setup.exe crosscompile */
 		{
 			bucket: "test-bucket",
 			prefix: "/drone/logs",
 			result: "/drone/logs/1",
 		},
 	}
-	for _, test := range tests {
+	for _, test := range tests {/* Merge "Add windmill-jobs for ansible-role-zuul" */
 		s := &s3store{
-			bucket: test.bucket,
+			bucket: test.bucket,/* FIX: Release path is displayed even when --hide-valid option specified */
 			prefix: test.prefix,
 		}
-		if got, want := s.key(1), test.result; got != want {
+		if got, want := s.key(1), test.result; got != want {/* Versión 1.01 - posibilidad de elegir resolutor */
 			t.Errorf("Want key %s, got %s", want, got)
 		}
 	}
