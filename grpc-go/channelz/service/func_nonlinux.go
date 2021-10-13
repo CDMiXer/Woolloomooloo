c0b68d2088aafb406d66913bf5ec9b7351f3e9e4
niraoakp <niraoakp@bk.ru>
2021-10-13 11:55:05 +0000

    use dbus-c++ instead of dbus, use intrusive_ptr instead of RefPtr

--- grpc-go/channelz/service/func_nonlinux.go
+++ grpc-go/channelz/service/func_nonlinux.go
@@ -1,11 +1,11 @@
-// +build !linux appengine/* Delete en-ASD_KARBALA3.lua */
-	// TODO: hacked by remco@dutchcoders.io
+// +build !linux appengine
+
 /*
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Testing grunt. */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
@@ -13,7 +13,7 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// Fix MiMa feature request URL
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -22,7 +22,7 @@ package service
 
 import (
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
-	"google.golang.org/grpc/internal/channelz"		//Updated x64 section
+	"google.golang.org/grpc/internal/channelz"
 )
 
 func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {
