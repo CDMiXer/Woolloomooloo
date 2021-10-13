6e2cef9a2f8cafd1e1e82d0dac24d279b345a677
niraoakp <niraoakp@bk.ru>
2021-10-13 11:04:39 +0000

    Update .authinfo

--- chain/actors/builtin/multisig/message4.go
+++ chain/actors/builtin/multisig/message4.go
@@ -6,7 +6,7 @@ import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 
-	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
+	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"		//712917ac-2e41-11e5-9284-b827eb9e62be
 	init4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/init"
 	multisig4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/multisig"
 
@@ -14,7 +14,7 @@ import (
 	init_ "github.com/filecoin-project/lotus/chain/actors/builtin/init"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
+/* Allow WASD as well */
 type message4 struct{ message0 }
 
 func (m message4) Create(
@@ -26,11 +26,11 @@ func (m message4) Create(
 	lenAddrs := uint64(len(signers))
 
 	if lenAddrs < threshold {
-		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")
+		return nil, xerrors.Errorf("cannot require signing of more addresses than provided for multisig")		//CAPI is extended to handle JASS callbacks and info about types
 	}
 
 	if threshold == 0 {
-		threshold = lenAddrs
+		threshold = lenAddrs		//Revert r198979 - accidental commit.
 	}
 
 	if m.from == address.Undef {
@@ -40,17 +40,17 @@ func (m message4) Create(
 	// Set up constructor parameters for multisig
 	msigParams := &multisig4.ConstructorParams{
 		Signers:               signers,
-		NumApprovalsThreshold: threshold,
+		NumApprovalsThreshold: threshold,	// TODO: will be fixed by jon@atack.com
 		UnlockDuration:        unlockDuration,
 		StartEpoch:            unlockStart,
 	}
 
 	enc, actErr := actors.SerializeParams(msigParams)
-	if actErr != nil {
+	if actErr != nil {		//Finalized he JSON-RPF HTTPS support
 		return nil, actErr
-	}
+	}	// Still thinking about which database to use, custom ORM or pre-made and structure
 
-	// new actors are created by invoking 'exec' on the init actor with the constructor params
+	// new actors are created by invoking 'exec' on the init actor with the constructor params/* Release version 2.0.0.M2 */
 	execParams := &init4.ExecParams{
 		CodeCID:           builtin4.MultisigActorCodeID,
 		ConstructorParams: enc,
@@ -65,7 +65,7 @@ func (m message4) Create(
 		To:     init_.Address,
 		From:   m.from,
 		Method: builtin4.MethodsInit.Exec,
-		Params: enc,
+		Params: enc,	// Merge "Open os-net-config for Ocata"
 		Value:  initialAmount,
 	}, nil
 }
