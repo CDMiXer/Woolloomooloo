fa03b099bed2cf7e2acb9882b8ab9ede8570ab0e
niraoakp <niraoakp@bk.ru>
2021-10-13 09:48:18 +0000

    added an example to allowed hosts

--- gen/conn_write.go
+++ gen/conn_write.go
@@ -1,15 +1,15 @@
 // Copyright 2016 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file./* UPDATE: CLO-12285 - CloverJMX mBean should be singleton - refactor */
+// license that can be found in the LICENSE file.
 
 // +build go1.8
-/* Merge "Release 4.4.31.75" */
+
 package websocket
+/* Merge "Release v0.6.1-preview" into v0.6 */
+import "net"/* Update FeedGetSimple.js */
 
-import "net"
-
-func (c *Conn) writeBufs(bufs ...[]byte) error {
-	b := net.Buffers(bufs)
-	_, err := b.WriteTo(c.conn)
+func (c *Conn) writeBufs(bufs ...[]byte) error {	// TODO: update inherate class to application.php
+	b := net.Buffers(bufs)	// TODO: hacked by martin2cai@hotmail.com
+	_, err := b.WriteTo(c.conn)	// TODO: will be fixed by hugomrdias@gmail.com
 	return err
 }
