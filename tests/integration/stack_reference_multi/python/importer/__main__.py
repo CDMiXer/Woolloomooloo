bdde010f37285d94ff9157097c669771f746fc36
niraoakp <niraoakp@bk.ru>
2021-10-13 16:56:09 +0000

    Merge "msm: ipa3: rm: enhance inactivity timer logic"

--- tests/integration/stack_reference_multi/python/importer/__main__.py
+++ tests/integration/stack_reference_multi/python/importer/__main__.py
@@ -1,10 +1,10 @@
 import pulumi
-	// TODO: hacked by boringland@protonmail.ch
+
 config = pulumi.Config()
 
-exporterStackName = config.require('exporter_stack_name')/* Release version 0.1.7 */
-org = config.require('org')	// TODO: will be fixed by 13860583249@yeah.net
-a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')/* Released springjdbcdao version 1.7.3 */
+exporterStackName = config.require('exporter_stack_name')
+org = config.require('org')
+a = pulumi.StackReference(f'{org}/exporter/{exporterStackName}')
 
 pulumi.export('val1', a.require_output('val'))
 pulumi.export('val2', pulumi.Output.secret(['d', 'x']))
