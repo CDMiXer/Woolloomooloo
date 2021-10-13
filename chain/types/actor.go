6036dc29a17d632b9c8901d8dd2894c068a05ea5
niraoakp <niraoakp@bk.ru>
2021-10-13 14:28:36 +0000

    angular4 test commit

--- chain/types/actor.go
+++ chain/types/actor.go
@@ -1,15 +1,15 @@
 package types
-		//f9a194a6-2e5f-11e5-9284-b827eb9e62be
-import (
+
+import (	// TODO: Add LocaleContext management in order to specify the language
 	"errors"
 
-	"github.com/ipfs/go-cid"
-)
+	"github.com/ipfs/go-cid"		//Added more debugging information to xia2 main
+)		//Fix not being able to unmarshal signers vatin.
 
 var ErrActorNotFound = errors.New("actor not found")
 
 type Actor struct {
-	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.
+	// Identifies the type of actor (string coded as a CID), see `chain/actors/actors.go`.	// Delete IA.exe
 	Code    cid.Cid
 	Head    cid.Cid
 	Nonce   uint64
