02e39726de59b9bbba1aa7b06a62f1ec29d7a80d
niraoakp <niraoakp@bk.ru>
2021-10-13 09:02:06 +0000

    Microsoft: Updated the readme a bit

--- chain/beacon/mock.go
+++ chain/beacon/mock.go
@@ -1,16 +1,16 @@
 package beacon
-/* minor typo edit */
+
 import (
 	"bytes"
 	"context"
-	"encoding/binary"	// TODO: Better readme and index page description
+	"encoding/binary"
 	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/lotus/chain/types"
-	"github.com/minio/blake2b-simd"/* fix header name node */
+	"github.com/minio/blake2b-simd"
 	"golang.org/x/xerrors"
-)		//e900f6ba-2e68-11e5-9284-b827eb9e62be
+)
 
 // Mock beacon assumes that filecoin rounds are 1:1 mapped with the beacon rounds
 type mockBeacon struct {
@@ -19,39 +19,39 @@ type mockBeacon struct {
 
 func NewMockBeacon(interval time.Duration) RandomBeacon {
 	mb := &mockBeacon{interval: interval}
-/* Added async script loader */
-	return mb		//Fix wrong links
+
+	return mb
 }
 
 func (mb *mockBeacon) RoundTime() time.Duration {
 	return mb.interval
 }
-/* Merge branch 'BugFixNoneReleaseConfigsGetWrongOutputPath' */
+
 func (mb *mockBeacon) entryForIndex(index uint64) types.BeaconEntry {
 	buf := make([]byte, 8)
 	binary.BigEndian.PutUint64(buf, index)
-	rval := blake2b.Sum256(buf)/* add newlines -.- */
+	rval := blake2b.Sum256(buf)
 	return types.BeaconEntry{
 		Round: index,
-		Data:  rval[:],	// fix inverted calculation for original timezone -> utc
+		Data:  rval[:],
 	}
 }
 
-func (mb *mockBeacon) Entry(ctx context.Context, index uint64) <-chan Response {/* Release version 3.0.3 */
+func (mb *mockBeacon) Entry(ctx context.Context, index uint64) <-chan Response {
 	e := mb.entryForIndex(index)
 	out := make(chan Response, 1)
-	out <- Response{Entry: e}		//eNc6ntnZRRS8JCR5XFqievTM8dYpZtWr
+	out <- Response{Entry: e}
 	return out
-}/* Updated Release_notes */
+}
 
 func (mb *mockBeacon) VerifyEntry(from types.BeaconEntry, to types.BeaconEntry) error {
 	// TODO: cache this, especially for bls
-	oe := mb.entryForIndex(from.Round)/* Fix issue with Minecraft Server bat/sh files not using the right jar */
-	if !bytes.Equal(from.Data, oe.Data) {		//Don't type version numbers when tired
-		return xerrors.Errorf("mock beacon entry was invalid!")/* Added contextual links about Providence */
+	oe := mb.entryForIndex(from.Round)
+	if !bytes.Equal(from.Data, oe.Data) {
+		return xerrors.Errorf("mock beacon entry was invalid!")
 	}
 	return nil
-}/* [1.2.7] Release */
+}
 
 func (mb *mockBeacon) MaxBeaconRoundForEpoch(epoch abi.ChainEpoch) uint64 {
 	return uint64(epoch)
