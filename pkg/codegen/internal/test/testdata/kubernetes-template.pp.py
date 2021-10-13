4b7d00fe80d739c5ab7fd5a2cc9bad6ba5a5a0eb
niraoakp <niraoakp@bk.ru>
2021-10-13 18:55:25 +0000

    Optimized PlayerInfoEvent

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.py
@@ -4,11 +4,11 @@ import pulumi_kubernetes as kubernetes
 argocd_server_deployment = kubernetes.apps.v1.Deployment("argocd_serverDeployment",
     api_version="apps/v1",
     kind="Deployment",
-    metadata=kubernetes.meta.v1.ObjectMetaArgs(
-        name="argocd-server",
+    metadata=kubernetes.meta.v1.ObjectMetaArgs(	// Cleaned up activities
+        name="argocd-server",/* we have something that works */
     ),
-    spec=kubernetes.apps.v1.DeploymentSpecArgs(
-        template=kubernetes.core.v1.PodTemplateSpecArgs(
+    spec=kubernetes.apps.v1.DeploymentSpecArgs(	// TODO: hacked by why@ipfs.io
+        template=kubernetes.core.v1.PodTemplateSpecArgs(	// TODO: 0188f898-4b19-11e5-a324-6c40088e03e4
             spec=kubernetes.core.v1.PodSpecArgs(
                 containers=[kubernetes.core.v1.ContainerArgs(
                     readiness_probe={
