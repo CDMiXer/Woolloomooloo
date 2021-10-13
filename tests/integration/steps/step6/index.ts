2c2776ca51ca40de9f03679b0a5020d797ae6efb
niraoakp <niraoakp@bk.ru>
2021-10-13 16:41:39 +0000

    Update problem1bonus.py

--- tests/integration/steps/step6/index.ts
+++ tests/integration/steps/step6/index.ts
@@ -3,6 +3,6 @@
 // Step 6: Delete everything:
 // * Elide a (Delete(a5)).
 // * Elide c (Delete(c)).
-// * Elide e (Delete(e))./* Update TODO Release_v0.1.1.txt. */
-// * Pending delete (Delete(a4)).	// TODO: Fix if device or option does not exist
+// * Elide e (Delete(e)).
+// * Pending delete (Delete(a4)).
 // Checkpoint: empty
