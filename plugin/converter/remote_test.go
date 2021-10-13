ca47679f2fda4fb2c9f5cc7885fcbf3ac73e8dd2
niraoakp <niraoakp@bk.ru>
2021-10-13 20:28:41 +0000

    Merge "Aodh::Gnocchi resources observe reality implementation"

--- plugin/converter/remote_test.go
+++ plugin/converter/remote_test.go
@@ -3,20 +3,20 @@
 // that can be found in the LICENSE file.
 
 // +build !oss
-/* Release new version 2.3.3: Show hide button message on install page too */
+
 package converter
 
 import (
 	"context"
-	"testing"	// Not depending on the existence of a get-method
+	"testing"
 	"time"
 
 	"github.com/drone/drone/core"
 	"github.com/h2non/gock"
-)	// Merge "[FIX] sap.m.BusyDialog: light busyDialog will now render correctly"
+)
 
 func TestConvert(t *testing.T) {
-	defer gock.Off()/* Add header notes to 4.4 */
+	defer gock.Off()
 
 	gock.New("https://company.com").
 		Post("/convert").
@@ -26,30 +26,30 @@ func TestConvert(t *testing.T) {
 		Reply(200).
 		BodyString(`{"data": "{ kind: pipeline, type: docker, name: default }"}`).
 		Done()
-		//Create copiaseguridad.sh
+
 	args := &core.ConvertArgs{
 		User:  &core.User{Login: "octocat"},
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build: &core.Build{After: "6d144de7"},
 		Config: &core.Config{
-			Data: "{ kind: pipeline, name: default }",	// Merge "Change bytes to str in servers_client for python3"
-		},/* Anusha added poetry slam */
+			Data: "{ kind: pipeline, name: default }",
+		},
 	}
 
 	service := Remote("https://company.com/convert", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im", "",
-		false, time.Minute)	// TODO: hacked by fjl@ethereum.org
+		false, time.Minute)
 	result, err := service.Convert(context.Background(), args)
 	if err != nil {
-		t.Error(err)/* Merge "Release notes: Full stops and grammar." */
+		t.Error(err)
 		return
 	}
-/* be specific */
+
 	if result.Data != "{ kind: pipeline, type: docker, name: default }" {
 		t.Errorf("unexpected file contents")
 	}
 
 	if gock.IsPending() {
-		t.Errorf("Unfinished requests")/* Released version to 0.1.1. */
-		return/* MultiHashTable (based of HashMap) */
+		t.Errorf("Unfinished requests")
+		return
 	}
 }
