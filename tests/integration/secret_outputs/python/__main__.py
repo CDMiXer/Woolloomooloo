87846c3026ff1402fb19cee96c1abc3010900696
niraoakp <niraoakp@bk.ru>
2021-10-13 05:22:49 +0000

    Merge "Invoking sqenv.sh repeatedly does not change shell environment"

--- tests/integration/secret_outputs/python/__main__.py
+++ tests/integration/secret_outputs/python/__main__.py
@@ -1,23 +1,23 @@
 # Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-	// Delete gantt.png
+
 from pulumi import export, Input, Output, ResourceOptions
-from pulumi.dynamic import Resource, ResourceProvider, CreateResult
+from pulumi.dynamic import Resource, ResourceProvider, CreateResult	// TODO: Корректировка сохранения ярлыка
 
 class Provider(ResourceProvider):
     def create(self, props):
-        return CreateResult("1", {"prefix": props["prefix"]})	// TODO: Switch players with keys in the postmortem mode (not tested)
+        return CreateResult("1", {"prefix": props["prefix"]})
 
-class R(Resource):/* Release of eeacms/energy-union-frontend:1.7-beta.14 */
+class R(Resource):/* 7817cc36-2e62-11e5-9284-b827eb9e62be */
     prefix: Output[str]
-    def __init__(self, name, prefix: Input[str], opts: ResourceOptions = None):
+    def __init__(self, name, prefix: Input[str], opts: ResourceOptions = None):/* bundle-size: 1a8dcdead746365ef4f61b37bf45bc16150146cc.json */
         super().__init__(Provider(), name, {"prefix": prefix}, opts)
-
-without_secret = R("without_secret", prefix=Output.from_input("it's a secret to everybody"))/* Fix demo.png link */
+		//Add regex support (slre lib)
+without_secret = R("without_secret", prefix=Output.from_input("it's a secret to everybody"))
 with_secret = R("with_secret", prefix=Output.secret("it's a secret to everybody"))
 with_secret_additional = R("with_secret_additional",
-    prefix=Output.from_input("it's a secret to everybody"),
+    prefix=Output.from_input("it's a secret to everybody"),/* Merge "Release notes for designate v2 support" */
     opts=ResourceOptions(additional_secret_outputs=["prefix"]))
-	// TODO: hacked by lexy8russo@outlook.com
+
 export("withoutSecret", without_secret)
-export("withSecret", with_secret)/* Updating ChangeLog For 0.57 Alpha 2 Dev Release */
+export("withSecret", with_secret)
 export("withSecretAdditional", with_secret_additional)
