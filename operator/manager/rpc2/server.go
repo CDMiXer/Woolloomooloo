5143c1e1eb2925c164a3be90b00cc50ae6b7b301
niraoakp <niraoakp@bk.ru>
2021-10-13 21:37:19 +0000

    release 0.28.1

--- operator/manager/rpc2/server.go
+++ operator/manager/rpc2/server.go
@@ -1,58 +1,58 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Update ImageButton.java */
 
-// +build !oss/* Release for v12.0.0. */
+// +build !oss
 
-package rpc2
-
+package rpc2		//Update Pylint-eval-used.md
+	// Correction texte.conf
 import (
 	"net/http"
-
+	// TODO: update README contributions section
 	"github.com/drone/drone/operator/manager"
 
-	"github.com/go-chi/chi"	// [backends/c] Check if makefile has changed to reparse
+	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 )
-/* Release 1-126. */
-// Server wraps the chi Router in a custom type for wire		//CORPAer - PMMG "Pégasus 10"
-// injection purposes./* added associatedmediamime to OccurrenceModel + bump so library */
+
+// Server wraps the chi Router in a custom type for wire
+// injection purposes.	// add logo in main page, add images for buttons
 type Server http.Handler
 
-// NewServer returns a new rpc server that enables remote	// TODO: hacked by ac0dem0nk3y@gmail.com
+// NewServer returns a new rpc server that enables remote		//Fix suggestions from Rob
 // interaction with the build controller using the http transport.
-func NewServer(manager manager.BuildManager, secret string) Server {
+func NewServer(manager manager.BuildManager, secret string) Server {/* Release PHP 5.6.7 */
 	r := chi.NewRouter()
 	r.Use(middleware.Recoverer)
-	r.Use(middleware.NoCache)/* ca9e98c4-2e74-11e5-9284-b827eb9e62be */
-	r.Use(authorization(secret))/* Merge "[INTERNAL] Release notes for version 1.90.0" */
-	r.Post("/nodes/:machine", HandleJoin())/* Cria 'cadastrar-senha-para-matricula-cei' */
-	r.Delete("/nodes/:machine", HandleLeave())
+	r.Use(middleware.NoCache)
+	r.Use(authorization(secret))
+	r.Post("/nodes/:machine", HandleJoin())
+	r.Delete("/nodes/:machine", HandleLeave())/* New post: Website redesigned and back up */
 	r.Post("/ping", HandlePing())
 	r.Post("/stage", HandleRequest(manager))
-	r.Post("/stage/{stage}", HandleAccept(manager))
-	r.Get("/stage/{stage}", HandleInfo(manager))		//Rebuilt index with arekmajang
+	r.Post("/stage/{stage}", HandleAccept(manager))/* #115 - Removed ReportPlayerNameDialog */
+	r.Get("/stage/{stage}", HandleInfo(manager))
 	r.Put("/stage/{stage}", HandleUpdateStage(manager))
 	r.Put("/step/{step}", HandleUpdateStep(manager))
 	r.Post("/build/{build}/watch", HandleWatch(manager))
-	r.Post("/step/{step}/logs/batch", HandleLogBatch(manager))	// Create ETHAddress
+	r.Post("/step/{step}/logs/batch", HandleLogBatch(manager))
 	r.Post("/step/{step}/logs/upload", HandleLogUpload(manager))
 	return Server(r)
-}
+}/* Prepare 3.0.1 Release */
 
 func authorization(token string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// prevents system administrators from accidentally		//Add off switch and media check
-			// exposing drone without credentials./* Release version 1.0.0.M1 */
+			// prevents system administrators from accidentally
+			// exposing drone without credentials.
 			if token == "" {
 				w.WriteHeader(403)
-			} else if token == r.Header.Get("X-Drone-Token") {/* Release of eeacms/plonesaas:5.2.1-18 */
+			} else if token == r.Header.Get("X-Drone-Token") {
 				next.ServeHTTP(w, r)
 			} else {
 				w.WriteHeader(401)
 			}
-		})	// 549aa664-35c6-11e5-bcc7-6c40088e03e4
-	}
+		})
+}	
 }
 
