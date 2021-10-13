52c04edb2566dc2287f94b4672d7ec8378e00fd2
niraoakp <niraoakp@bk.ru>
2021-10-13 21:21:53 +0000

    Tiny text updates

--- tests/examples/formattable/index.ts
+++ tests/examples/formattable/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: Reworked some things
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 console.log("Here's some format goo: %s, %v, %q, %d!");
 console.error("Here's some format goo: %s, %v, %q, %d!");
