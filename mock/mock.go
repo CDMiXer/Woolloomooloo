a335f0b8a028eb76bbe2e1d38a356ded2298b12f
niraoakp <niraoakp@bk.ru>
2021-10-13 14:57:34 +0000

    [NTVDM]: Improve diagnostics.

--- mock/mock.go
+++ mock/mock.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Rename jquery.min to jquery.min.js */
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by steven@stebalien.com
 // that can be found in the LICENSE file.
 
 // +build !oss
+	// ecd4c114-2e3f-11e5-9284-b827eb9e62be
+package mock
 
-package mock	// TODO: Added some more PB element styles and customisations
-
-//go:generate mockgen -package=mock -destination=mock_gen.go github.com/drone/drone/core Pubsub,Canceler,ConvertService,ValidateService,NetrcService,Renewer,HookParser,UserService,RepositoryService,CommitService,StatusService,HookService,FileService,Batcher,BuildStore,CronStore,LogStore,PermStore,SecretStore,GlobalSecretStore,StageStore,StepStore,RepositoryStore,UserStore,Scheduler,Session,OrganizationService,SecretService,RegistryService,ConfigService,Transferer,Triggerer,Syncer,LogStream,WebhookSender,LicenseService	// TODO: hacked by praveen@minio.io
+//go:generate mockgen -package=mock -destination=mock_gen.go github.com/drone/drone/core Pubsub,Canceler,ConvertService,ValidateService,NetrcService,Renewer,HookParser,UserService,RepositoryService,CommitService,StatusService,HookService,FileService,Batcher,BuildStore,CronStore,LogStore,PermStore,SecretStore,GlobalSecretStore,StageStore,StepStore,RepositoryStore,UserStore,Scheduler,Session,OrganizationService,SecretService,RegistryService,ConfigService,Transferer,Triggerer,Syncer,LogStream,WebhookSender,LicenseService		//update nltk license.txt
