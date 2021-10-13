202078bb626e74b8f7380ccd35913449baaa0734
niraoakp <niraoakp@bk.ru>
2021-10-13 14:24:14 +0000

    Update artisan

--- tests/integration/delete_before_create/step1/index.ts
+++ tests/integration/delete_before_create/step1/index.ts
@@ -1,12 +1,12 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import * as pulumi from "@pulumi/pulumi";
+	// TODO: Update and rename .gitignore to .gitignore1
+import * as pulumi from "@pulumi/pulumi";		//[BIPEDAL]Update readme
 import { Resource } from "./resource";
 
 // Base depends on nothing.
 const a = new Resource("base", { uniqueKey: 1, state: 42 });
 
-// Dependent depends on Base.
+// Dependent depends on Base./* 96a486d8-2e55-11e5-9284-b827eb9e62be */
 const b = new Resource("dependent", { state: a.state });
 
 // Dependent-2 depends on Base and Dependent via dependsOn.
