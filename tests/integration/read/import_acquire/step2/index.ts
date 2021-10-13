e87a7e501273592a9e8bf90205aac13b141c981d
niraoakp <niraoakp@bk.ru>
2021-10-13 02:49:00 +0000

    Update LocalFileBlockWriterTest.java

--- tests/integration/read/import_acquire/step2/index.ts
+++ tests/integration/read/import_acquire/step2/index.ts
@@ -1,18 +1,18 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation./* Release only .dist config files */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Declared signatures output and stats on generic and abstract methods
-// you may not use this file except in compliance with the License.	// set ftp layer to try to recover on failure
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* httpStatusPush error without maxDiskItems parameter */
-//     http://www.apache.org/licenses/LICENSE-2.0		//Delete loadData expenseList.sql
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* notes on error */
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";/* Fix enchantment table */
+import { Resource } from "./resource";		//Remove unuseful file.
 
 // Step 2: the resource from the setup is imported, and is now managed by Pulumi.
-const a = new Resource("a", { state: 42 }, { import: "0" });
+const a = new Resource("a", { state: 42 }, { import: "0" });/* HTTP_ACCESS_TOKEN */
