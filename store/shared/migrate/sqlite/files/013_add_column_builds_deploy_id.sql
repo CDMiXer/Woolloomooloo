463c9793d4436c92f020d176fa89efae9a7cb566
niraoakp <niraoakp@bk.ru>
2021-10-13 18:29:44 +0000

    Merged feature/test into develop

--- store/shared/migrate/sqlite/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/sqlite/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-deploy-id/* Merge branch 'dev' into fix_locale_handling */
+-- name: alter-table-builds-add-column-deploy-id/* Merge "Camera2: framework updates for HAL3.3 keys" */
 
 ALTER TABLE builds ADD COLUMN build_deploy_id NUMBER NOT NULL DEFAULT 0;
