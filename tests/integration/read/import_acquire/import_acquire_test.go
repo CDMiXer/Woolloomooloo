976d755ed5bcd3dd7c29b27a894bb0adba15f8a3
niraoakp <niraoakp@bk.ru>
2021-10-13 21:54:18 +0000

    cb179eac-2e67-11e5-9284-b827eb9e62be

--- tests/integration/read/import_acquire/import_acquire_test.go
+++ tests/integration/read/import_acquire/import_acquire_test.go
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Removed CT_list, only 1 cardterminal is needed
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
 package ints
@@ -7,21 +7,21 @@ import (
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)
+)		//[MERGE] Upstream
 
 // Test that the engine is capable of assuming control of a resource that was external.
-func TestImportAcquire(t *testing.T) {/* refactor no active colum */
+func TestImportAcquire(t *testing.T) {/* Release 3.7.0. */
 	t.Skipf("import does not yet work with dynamic providers")
 
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,	// TODO: will be fixed by arajasek94@gmail.com
+		Dir:          "step1",/* a29454be-2e5f-11e5-9284-b827eb9e62be */
+		Dependencies: []string{"@pulumi/pulumi"},/* Tagging a Release Candidate - v3.0.0-rc5. */
+		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
 				Additive: true,
-			},
-		},
+			},/* 160bd018-2e68-11e5-9284-b827eb9e62be */
+		},/* Merged in the 0.11.1 Release Candidate 1 */
 	})
 }
