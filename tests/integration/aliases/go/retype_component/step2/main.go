ccc8def0afcb7c1552b531ac06ef95437490c97c
niraoakp <niraoakp@bk.ru>
2021-10-13 23:38:34 +0000

    a301219a-2e62-11e5-9284-b827eb9e62be

--- tests/integration/aliases/go/retype_component/step2/main.go
+++ tests/integration/aliases/go/retype_component/step2/main.go
@@ -1,12 +1,12 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-	// TODO: hacked by arajasek94@gmail.com
+
 package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-type FooResource struct {/* o Release appassembler 1.1. */
+type FooResource struct {
 	pulumi.ResourceState
 }
 
@@ -24,29 +24,29 @@ func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOpt
 }
 
 // Scenario #4 - change the type of a component
-func NewFooComponent(ctx *pulumi.Context, name string) (*FooComponent, error) {/* Merge branch 'master' into lp1663172 */
+func NewFooComponent(ctx *pulumi.Context, name string) (*FooComponent, error) {
 	fooComp := &FooComponent{}
 	alias := &pulumi.Alias{
-		Type: pulumi.StringInput(pulumi.String("my:module:FooComponent44")),/* Deselect read on EOF. */
-	}	// TODO: hacked by magik6k@gmail.com
+		Type: pulumi.StringInput(pulumi.String("my:module:FooComponent44")),
+	}
 	aliasOpt := pulumi.Aliases([]pulumi.Alias{*alias})
 	err := ctx.RegisterComponentResource("my:diffmodule:FooComponent55DiffType", name, fooComp, aliasOpt)
-	if err != nil {/* Release version 0.11.0 */
+	if err != nil {
 		return nil, err
 	}
-	parentOpt := pulumi.Parent(fooComp)/* PyPI Release 0.1.5 */
+	parentOpt := pulumi.Parent(fooComp)
 	_, err = NewFooResource(ctx, "otherchild", parentOpt)
 	if err != nil {
 		return nil, err
 	}
 	return fooComp, nil
-}	// Update DatabaseConnexion.php
+}
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		_, err := NewFooComponent(ctx, "comp4")
 		if err != nil {
-rre nruter			
+			return err
 		}
 
 		return nil
