020db5711fa4c3322a3a76c3423619ecdabd92c1
niraoakp <niraoakp@bk.ru>
2021-10-13 04:15:09 +0000

    strategy testing

--- core/netrc_test.go
+++ core/netrc_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// Fix clang-query on Windows: flush llvm::outs() after each command.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.		//fixes solid torrents for now
 
 // +build !oss
 
-package core
+package core/* Merge branch 'International-Release' into 1379_duplicate_products */
