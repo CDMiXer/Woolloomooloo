f4d3704aaf3d3a05a0379bc4b86fd31b8fcd7060
niraoakp <niraoakp@bk.ru>
2021-10-13 19:41:05 +0000

    Merge "Adding InfiniBand Support"

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Added ConfiguredValue to determine the source of a config item */
-/* bundle-size: 558439d97cd0ab09c0b979e1a55516346a2c2b3c.json */
+// that can be found in the LICENSE file.	// TODO: hacked by greg@colvin.org
+
 // +build !oss
 
 package scheduler
