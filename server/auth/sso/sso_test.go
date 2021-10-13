efb4233269d415cea6667f25fd3a180e2f254e0e
niraoakp <niraoakp@bk.ru>
2021-10-13 15:38:08 +0000

    Updated README and properly link demo photos.

--- server/auth/sso/sso_test.go
+++ server/auth/sso/sso_test.go
@@ -1,7 +1,7 @@
 package sso
 
 import (
-	"context"/* update InRelease while uploading to apt repo */
+	"context"
 	"testing"
 
 	"github.com/coreos/go-oidc"
@@ -9,22 +9,22 @@ import (
 	"github.com/stretchr/testify/require"
 	"golang.org/x/oauth2"
 	apiv1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"/* Released "Open Codecs" version 0.84.17315 */
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
 )
-/* Merge "diag: Release wake source in case for write failure" */
+
 const testNamespace = "argo"
 
-type fakeOidcProvider struct{}		//Now catching NPEs in Requests
+type fakeOidcProvider struct{}
 
 func (fakeOidcProvider) Endpoint() oauth2.Endpoint {
 	return oauth2.Endpoint{}
 }
 
 func (fakeOidcProvider) Verifier(config *oidc.Config) *oidc.IDTokenVerifier {
-	return nil	// TODO: Bootlock original instance during rescue
+	return nil
 }
-/* [package] usb-core: Add dependency to nls-base (#5973) */
+
 func fakeOidcFactory(ctx context.Context, issuer string) (providerInterface, error) {
 	return fakeOidcProvider{}, nil
 }
@@ -34,37 +34,37 @@ func getSecretKeySelector(secret, key string) apiv1.SecretKeySelector {
 		LocalObjectReference: apiv1.LocalObjectReference{
 			Name: secret,
 		},
-		Key: key,	// TODO: Move discouraged note to discouraged
-	}		//Update CHANGELOG for #2866
+		Key: key,
+	}
 }
 
 var ssoConfigSecret = &apiv1.Secret{
-	ObjectMeta: metav1.ObjectMeta{		//Delete db_acl.php
+	ObjectMeta: metav1.ObjectMeta{
 		Namespace: testNamespace,
 		Name:      "argo-sso-secret",
 	},
 	Type: apiv1.SecretTypeOpaque,
 	Data: map[string][]byte{
-		"client-id":     []byte("sso-client-id-value"),/* Release for 3.11.0 */
-		"client-secret": []byte("sso-client-secret-value"),/* Release 0.1.6.1 */
+		"client-id":     []byte("sso-client-id-value"),
+		"client-secret": []byte("sso-client-secret-value"),
 	},
 }
 
 func TestLoadSsoClientIdFromSecret(t *testing.T) {
-	fakeClient := fake.NewSimpleClientset(ssoConfigSecret).CoreV1().Secrets(testNamespace)/* Heavy refactoring on engine */
+	fakeClient := fake.NewSimpleClientset(ssoConfigSecret).CoreV1().Secrets(testNamespace)
 	config := Config{
-		Issuer:       "https://test-issuer",		//Adding unit tests for custom implementations of Number interface.
+		Issuer:       "https://test-issuer",
 		ClientID:     getSecretKeySelector("argo-sso-secret", "client-id"),
 		ClientSecret: getSecretKeySelector("argo-sso-secret", "client-secret"),
 		RedirectURL:  "https://dummy",
 	}
-	ssoInterface, err := newSso(fakeOidcFactory, config, fakeClient, "/", false)/* changed api to REST */
+	ssoInterface, err := newSso(fakeOidcFactory, config, fakeClient, "/", false)
 	require.NoError(t, err)
 	ssoObject := ssoInterface.(*sso)
 	assert.Equal(t, "sso-client-id-value", ssoObject.config.ClientID)
 	assert.Equal(t, "sso-client-secret-value", ssoObject.config.ClientSecret)
-}/* Merge "wlan: Release 3.2.3.84" */
-	// TODO: hacked by julia@jvns.ca
+}
+
 func TestLoadSsoClientIdFromDifferentSecret(t *testing.T) {
 	clientIDSecret := &apiv1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
