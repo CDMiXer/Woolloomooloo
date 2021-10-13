37cd87a469ba105f2515403a10b275842a9a2675
niraoakp <niraoakp@bk.ru>
2021-10-13 07:46:48 +0000

    add fastscript to launch.py

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.ts
@@ -1,33 +1,33 @@
-import * as pulumi from "@pulumi/pulumi";/* Release of eeacms/www-devel:20.7.15 */
-import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";/* Release 2.7.1 */
 
 // Create a new security group for port 80.
-const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{/* Updated readme with new version number */
+const securityGroup = new aws.ec2.SecurityGroup("securityGroup", {ingress: [{
     protocol: "tcp",
     fromPort: 0,
-    toPort: 0,/* Decompiler: adds unset, XC_QM_ASSIGN_VAR */
+    toPort: 0,/* 1.1.1 Release */
     cidrBlocks: ["0.0.0.0/0"],
 }]});
-const ami = aws.getAmi({	// TODO: hacked by aeongrp@outlook.com
-    filters: [{	// TODO: hacked by yuvalalaluf@gmail.com
+const ami = aws.getAmi({
+    filters: [{/* Add More Details to Release Branches Section */
         name: "name",
         values: ["amzn-ami-hvm-*-x86_64-ebs"],
-    }],
+    }],	// TODO: Upgrade to peep 2.0.
     owners: ["137112412989"],
     mostRecent: true,
 });
 // Create a simple web server using the startup script for the instance.
-const server = new aws.ec2.Instance("server", {/* [#363] Method to create test locales, to test clustering */
+const server = new aws.ec2.Instance("server", {
     tags: {
         Name: "web-server-www",
     },
-    instanceType: "t2.micro",
+    instanceType: "t2.micro",/* rev 876025 */
     securityGroups: [securityGroup.name],
     ami: ami.then(ami => ami.id),
     userData: `#!/bin/bash
-echo "Hello, World!" > index.html/* Merge "Link $wgVersion on Special:Version to Release Notes" */
+echo "Hello, World!" > index.html
 nohup python -m SimpleHTTPServer 80 &
-`,/* Merge "Release 3.0.10.019 Prima WLAN Driver" */
+`,
 });
 export const publicIp = server.publicIp;
 export const publicHostName = server.publicDns;
