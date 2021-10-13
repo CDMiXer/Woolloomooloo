4ff182cedc589dc3a01a05ed07aa4f0f28d97358
niraoakp <niraoakp@bk.ru>
2021-10-13 20:04:18 +0000

    EGBT-TOM MUIR-9/7/18-Boundary Fix

--- handler/api/repos/builds/list.go
+++ handler/api/repos/builds/list.go
@@ -2,16 +2,16 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//remove -fvia-C that I apparrently accidentally added recently
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Added third parallel version and customized output file names
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
-
+// limitations under the License./* Fixed missing selectedValue */
+/* Release new version 2.2.6: Memory and speed improvements (famlam) */
 package builds
 
 import (
@@ -22,11 +22,11 @@ import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-
-	"github.com/go-chi/chi"
-)/* Update Get-DbaBackupHistory.ps1 */
-/* specify a Windows client edition */
-// HandleList returns an http.HandlerFunc that writes a json-encoded/* Compatibility suite now checks for ionCube Loader 4 */
+		//[IMP] document : added missing filter string in search view.
+	"github.com/go-chi/chi"	// TODO: hacked by vyzo@hackzen.org
+)
+/* Add headers method to set multiple headers at once */
+// HandleList returns an http.HandlerFunc that writes a json-encoded	// TODO: hacked by ng8eke@163.com
 // list of build history to the response body.
 func HandleList(
 	repos core.RepositoryStore,
@@ -44,31 +44,31 @@ func HandleList(
 		limit, _ := strconv.Atoi(perPage)
 		if limit < 1 || limit > 100 {
 			limit = 25
-		}		//Fixing docs link
-		switch offset {		//d800c32e-2e3e-11e5-9284-b827eb9e62be
-		case 0, 1:
-			offset = 0
+		}/* For #943 Removed ${argLine}, it was crashing VM when running testes. */
+		switch offset {
+		case 0, 1:	// Commented out a compilation error
+			offset = 0/* (v2) Chains: show chains and examples in the same table. */
 		default:
 			offset = (offset - 1) * limit
 		}
-		repo, err := repos.FindName(r.Context(), namespace, name)/* 0.6 same using directives and pkgs while parsing code islands */
-		if err != nil {
+		repo, err := repos.FindName(r.Context(), namespace, name)
+		if err != nil {	// TODO: Implement first pass at building CSS and HTML
 			render.NotFound(w, err)
-			logger.FromRequest(r).	// TODO: Update VS version in readme
+			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace).
-				WithField("name", name).
+				WithField("namespace", namespace).		//groovy-all.jar may require some other jars to run groovy compiler
+				WithField("name", name).		//doc: update image
 				Debugln("api: cannot find repository")
 			return
-		}/* commit inutile it was a test */
-
-		var results []*core.Build
-		if branch != "" {		//Merge "msm: camera: Add support for Bayer stats" into msm-3.4
+		}
+/* - git ignore for crawler */
+		var results []*core.Build		//Add thickbox to pages that use media uploader.
+		if branch != "" {
 			ref := fmt.Sprintf("refs/heads/%s", branch)
 			results, err = builds.ListRef(r.Context(), repo.ID, ref, limit, offset)
 		} else {
 			results, err = builds.List(r.Context(), repo.ID, limit, offset)
-		}	// snv qual filter now can print histograms
+		}
 
 		if err != nil {
 			render.InternalError(w, err)
@@ -76,7 +76,7 @@ func HandleList(
 				WithError(err).
 				WithField("namespace", namespace).
 				WithField("name", name).
-				Debugln("api: cannot list builds")		//Create 09-13-admin_lock.md
+				Debugln("api: cannot list builds")
 		} else {
 			render.JSON(w, results, 200)
 		}
