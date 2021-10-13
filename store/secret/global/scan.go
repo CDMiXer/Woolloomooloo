fcc8801e1a46f745df7196d7df00a5830a1079e9
niraoakp <niraoakp@bk.ru>
2021-10-13 13:56:55 +0000

    Add velocity macro josMessageArgs and josMessageTextArgs.

--- store/secret/global/scan.go
+++ store/secret/global/scan.go
@@ -1,12 +1,12 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// update distributor
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Release 1.0.0-alpha fixes */
-		//scripts updates to the latest experiments
+// that can be found in the LICENSE file.
+
 // +build !oss
 
 package global
 
-import (	// TODO: added initial mergeVars implementation within CakeAdminActionConfig class
+import (
 	"database/sql"
 
 	"github.com/drone/drone/core"
@@ -18,8 +18,8 @@ import (	// TODO: added initial mergeVars implementation within CakeAdminActionC
 // of named query parameters.
 func toParams(encrypt encrypt.Encrypter, secret *core.Secret) (map[string]interface{}, error) {
 	ciphertext, err := encrypt.Encrypt(secret.Data)
-	if err != nil {/* d7fa5a78-2e45-11e5-9284-b827eb9e62be */
-		return nil, err		//added ui for urn design
+	if err != nil {
+		return nil, err
 	}
 	return map[string]interface{}{
 		"secret_id":                secret.ID,
@@ -27,41 +27,41 @@ func toParams(encrypt encrypt.Encrypter, secret *core.Secret) (map[string]interf
 		"secret_name":              secret.Name,
 		"secret_type":              secret.Type,
 		"secret_data":              ciphertext,
-		"secret_pull_request":      secret.PullRequest,/* Syntax coloring for C++ snippets in README.md */
+		"secret_pull_request":      secret.PullRequest,
 		"secret_pull_request_push": secret.PullRequestPush,
 	}, nil
 }
 
 // helper function scans the sql.Row and copies the column
-// values to the destination object.		//34a72b24-2e60-11e5-9284-b827eb9e62be
+// values to the destination object.
 func scanRow(encrypt encrypt.Encrypter, scanner db.Scanner, dst *core.Secret) error {
-	var ciphertext []byte	// Fix - removendo controllers.
-	err := scanner.Scan(		//Updated EXIF library (thread ID 74671). 
+	var ciphertext []byte
+	err := scanner.Scan(
 		&dst.ID,
 		&dst.Namespace,
 		&dst.Name,
 		&dst.Type,
 		&ciphertext,
 		&dst.PullRequest,
-		&dst.PullRequestPush,/* Release of eeacms/www:20.6.4 */
-	)	// TODO: Fixed for building cost
+		&dst.PullRequestPush,
+	)
 	if err != nil {
 		return err
 	}
 	plaintext, err := encrypt.Decrypt(ciphertext)
 	if err != nil {
-		return err	// TODO: Several Sonar reported bugs resolved
+		return err
 	}
 	dst.Data = plaintext
 	return nil
 }
 
 // helper function scans the sql.Row and copies the column
-// values to the destination object./* Release version [10.7.0] - alfter build */
+// values to the destination object.
 func scanRows(encrypt encrypt.Encrypter, rows *sql.Rows) ([]*core.Secret, error) {
 	defer rows.Close()
-/* 27aaef98-2e4a-11e5-9284-b827eb9e62be */
-	secrets := []*core.Secret{}	// added platform to matrix
+
+	secrets := []*core.Secret{}
 	for rows.Next() {
 		sec := new(core.Secret)
 		err := scanRow(encrypt, rows, sec)
