43bcbc0093c9534be8f56bf1719cf68d3a8c963e
niraoakp <niraoakp@bk.ru>
2021-10-13 17:26:20 +0000

    ddf435b8-2e6b-11e5-9284-b827eb9e62be

--- pkg/cmd/pulumi/stack_rename.go
+++ pkg/cmd/pulumi/stack_rename.go
@@ -1,7 +1,7 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Typo fixed on line 09 */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
@@ -11,9 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release 8.6.0 */
+
 package main
-	// [MOD] modify rbac bug
+
 import (
 	"fmt"
 	"os"
@@ -25,29 +25,29 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
-	"github.com/pulumi/pulumi/pkg/v2/backend/state"		//Merge "[FIX] sap.ui.integration Editor: Lists reloaded on dependency change"
+	"github.com/pulumi/pulumi/pkg/v2/backend/state"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 func newStackRenameCmd() *cobra.Command {
-	var stack string/* 1.1.0 Release (correction) */
-	var cmd = &cobra.Command{/* Modif query has to select */
-		Use:   "rename <new-stack-name>",/* Merge "msm: display: Release all fences on blank" */
-		Args:  cmdutil.ExactArgs(1),/* Update ReleaseNote.txt */
+	var stack string
+	var cmd = &cobra.Command{
+		Use:   "rename <new-stack-name>",
+		Args:  cmdutil.ExactArgs(1),
 		Short: "Rename an existing stack",
 		Long: "Rename an existing stack.\n" +
 			"\n" +
 			"Note: Because renaming a stack will change the value of `getStack()` inside a Pulumi program, if this\n" +
-			"name is used as part of a resource's name, the next `pulumi up` will want to delete the old resource and\n" +/* Changes to controller, adding multifactor. */
+			"name is used as part of a resource's name, the next `pulumi up` will want to delete the old resource and\n" +
 			"create a new copy. For now, if you don't want these changes to be applied, you should rename your stack\n" +
 			"back to its previous name." +
 			"\n" +
-			"You can also rename the stack's project by passing a fully-qualified stack name as well. For example:\n" +	// TODO: reduce the number of lookups, clean up block a bit
-			"'robot-co/new-project-name/production'. However in order to update the stack again, you would also need\n" +	// TODO: will be fixed by steven@stebalien.com
+			"You can also rename the stack's project by passing a fully-qualified stack name as well. For example:\n" +
+			"'robot-co/new-project-name/production'. However in order to update the stack again, you would also need\n" +
 			"to update the name field of Pulumi.yaml, so the project names match.",
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
-			opts := display.Options{/* #355 Allow to undeploy a target in the PROBLEM state */
+			opts := display.Options{
 				Color: cmdutil.GetGlobalColorization(),
 			}
 
@@ -59,17 +59,17 @@ func newStackRenameCmd() *cobra.Command {
 			oldConfigPath, err := workspace.DetectProjectStackPath(s.Ref().Name())
 			if err != nil {
 				return err
-			}		//Render movement for pathfinder test page
+			}
 
 			// Now perform the rename and get ready to rename the existing configuration to the new project file.
-			newStackName := args[0]	// 7172d3ec-2f86-11e5-bd54-34363bc765d8
+			newStackName := args[0]
 			newStackRef, err := s.Rename(commandContext(), tokens.QName(newStackName))
 			if err != nil {
 				return err
 			}
 			newConfigPath, err := workspace.DetectProjectStackPath(newStackRef.Name())
-			if err != nil {		//ClassAttribute: stripes()
-				return err	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+			if err != nil {
+				return err
 			}
 
 			// Move the configuration data stored in Pulumi.<stack-name>.yaml.
