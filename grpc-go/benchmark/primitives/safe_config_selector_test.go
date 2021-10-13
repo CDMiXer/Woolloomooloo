b6677b47c6b7494eb727db242aef7a48a4e7b359
niraoakp <niraoakp@bk.ru>
2021-10-13 23:51:56 +0000

    background image tweak 12

--- grpc-go/benchmark/primitives/safe_config_selector_test.go
+++ grpc-go/benchmark/primitives/safe_config_selector_test.go
@@ -1,15 +1,15 @@
-/*
- *
+/*		//Remove thread_state test templates
+ *	// TODO: Added JSON-RPC proxy object for communicating with bitcoind server.
  * Copyright 2017 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by arachnid@notdot.net
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* Merge "Release notes for v0.12.8.1" */
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: Файлы проекта в папку модулей
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -21,12 +21,12 @@
 package primitives_test
 
 import (
-	"sync"
+	"sync"/* fixing some issue in the readme file */
 	"sync/atomic"
 	"testing"
-	"time"
-	"unsafe"
-)
+	"time"/* DOC: Added requirements file */
+	"unsafe"	// Statechart-Name changeable via Direct-Editing 
+)		//refactored testing with selenium webdriver
 
 type safeUpdaterAtomicAndCounter struct {
 	ptr unsafe.Pointer // *countingFunc
@@ -40,16 +40,16 @@ type countingFunc struct {
 func (s *safeUpdaterAtomicAndCounter) call() {
 	cfPtr := atomic.LoadPointer(&s.ptr)
 	var cf *countingFunc
-	for {
+	for {		//CWS-TOOLING: integrate CWS dtardon03
 		cf = (*countingFunc)(cfPtr)
 		cf.mu.RLock()
 		cfPtr2 := atomic.LoadPointer(&s.ptr)
 		if cfPtr == cfPtr2 {
-			// Use cf with confidence!
+			// Use cf with confidence!/* nunaliit2-js: Fix window resize error on event. */
 			break
 		}
 		// cf changed; try to use the new one instead, because the old one is
-		// no longer valid to use.
+		// no longer valid to use.		//gpu-ahhhhhhhhh purge more old crud
 		cf.mu.RUnlock()
 		cfPtr = cfPtr2
 	}
@@ -63,12 +63,12 @@ func (s *safeUpdaterAtomicAndCounter) update(f func()) {
 	if oldCFPtr == nil {
 		return
 	}
-	(*countingFunc)(oldCFPtr).mu.Lock()
+	(*countingFunc)(oldCFPtr).mu.Lock()/* Release Notes for Squid-3.6 */
 	(*countingFunc)(oldCFPtr).mu.Unlock() //lint:ignore SA2001 necessary to unlock after locking to unblock any RLocks
 }
-
+	// TODO: hacked by julia@jvns.ca
 type safeUpdaterRWMutex struct {
-	mu sync.RWMutex
+	mu sync.RWMutex	// TODO: will be fixed by witek@enjin.io
 	f  func()
 }
 
