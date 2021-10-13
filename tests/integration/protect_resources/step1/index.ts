8e56f8610c8d7bc5e3bf71aeacbcd55aca698d12
niraoakp <niraoakp@bk.ru>
2021-10-13 04:36:21 +0000

    Load/Unload custom resources lazily after a change in prefs

--- tests/integration/protect_resources/step1/index.ts
+++ tests/integration/protect_resources/step1/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// TODO: will be fixed by why@ipfs.io
+
 import { Resource } from "./resource";
 
-// Allocate a resource and protect it:/* removed library specific definitions */
+// Allocate a resource and protect it:
 let a = new Resource("eternal", { state: 1 }, { protect: true });
