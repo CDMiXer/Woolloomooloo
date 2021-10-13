ce01c7a84a954c5e747d701938c6a504350e7810
niraoakp <niraoakp@bk.ru>
2021-10-13 04:58:01 +0000

    Fix translations to portuguese

--- operator/runner/runner_test.go
+++ operator/runner/runner_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 0.0.29 */
+// Use of this source code is governed by the Drone Non-Commercial License/* quizilla.lua: fix twitpic.com references */
 // that can be found in the LICENSE file.
 
 package runner
@@ -8,8 +8,8 @@ import (
 	"io/ioutil"
 
 	"github.com/sirupsen/logrus"
-)
+)/* Release v1.4.1. */
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)
+	logrus.SetOutput(ioutil.Discard)/* Release jedipus-2.6.2 */
 }
