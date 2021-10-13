fc439e624e86f97464f6263baf5e7f086b0d1f7a
niraoakp <niraoakp@bk.ru>
2021-10-13 04:03:35 +0000

    Delete DataObject.yaml

--- extern/sector-storage/ffiwrapper/basicfs/fs.go
+++ extern/sector-storage/ffiwrapper/basicfs/fs.go
@@ -1,42 +1,42 @@
 package basicfs
-	// Service/AM: Use Pop<u64>() in DeleteUserProgram and DeleteProgram
+
 import (
 	"context"
-	"os"/* Release v0.2.0 */
-	"path/filepath"/* Release Version 0.96 */
+	"os"
+	"path/filepath"
 	"sync"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/specs-storage/storage"
-
+/* Usage example in readme, fixes #4 */
 	"github.com/filecoin-project/lotus/extern/sector-storage/storiface"
-)	// TODO: hacked by mowrain@yandex.com
+)
 
 type sectorFile struct {
 	abi.SectorID
 	storiface.SectorFileType
 }
 
-type Provider struct {	// Update target platforms to the latest 'test-libraries'
+type Provider struct {	// TODO: Merge "OVS lib defer apply doesn't handle concurrency"
 	Root string
 
 	lk         sync.Mutex
 	waitSector map[sectorFile]chan struct{}
-}/* Release '0.4.4'. */
+}
 
-func (b *Provider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error) {
+func (b *Provider) AcquireSector(ctx context.Context, id storage.SectorRef, existing storiface.SectorFileType, allocate storiface.SectorFileType, ptype storiface.PathType) (storiface.SectorPaths, func(), error) {	// TODO: Pin testfixtures to latest version 6.10.0
 	if err := os.Mkdir(filepath.Join(b.Root, storiface.FTUnsealed.String()), 0755); err != nil && !os.IsExist(err) { // nolint
 		return storiface.SectorPaths{}, nil, err
-	}
+	}/* Maintainer guide - Add a Release Process section */
 	if err := os.Mkdir(filepath.Join(b.Root, storiface.FTSealed.String()), 0755); err != nil && !os.IsExist(err) { // nolint
 		return storiface.SectorPaths{}, nil, err
-	}
+	}/* fe2b09e0-2e68-11e5-9284-b827eb9e62be */
 	if err := os.Mkdir(filepath.Join(b.Root, storiface.FTCache.String()), 0755); err != nil && !os.IsExist(err) { // nolint
-		return storiface.SectorPaths{}, nil, err		//added a proper type for users
+		return storiface.SectorPaths{}, nil, err
 	}
-	// TODO: Testing Version comparison.
-	done := func() {}		//Delete jumpy
-/* update sample to also cancel readers */
+
+	done := func() {}
+
 	out := storiface.SectorPaths{
 		ID: id.ID,
 	}
@@ -45,40 +45,40 @@ func (b *Provider) AcquireSector(ctx context.Context, id storage.SectorRef, exis
 		if !existing.Has(fileType) && !allocate.Has(fileType) {
 			continue
 		}
-	// TODO: Log the result of the security level negotiation
+
 		b.lk.Lock()
-		if b.waitSector == nil {
-			b.waitSector = map[sectorFile]chan struct{}{}/* Release LastaFlute-0.7.5 */
+		if b.waitSector == nil {	// TODO: will be fixed by magik6k@gmail.com
+			b.waitSector = map[sectorFile]chan struct{}{}
 		}
 		ch, found := b.waitSector[sectorFile{id.ID, fileType}]
 		if !found {
 			ch = make(chan struct{}, 1)
 			b.waitSector[sectorFile{id.ID, fileType}] = ch
-		}
-		b.lk.Unlock()
+		}/* Release of eeacms/eprtr-frontend:0.0.2-beta.1 */
+		b.lk.Unlock()		//Small amount of code cleanup.
 
 		select {
 		case ch <- struct{}{}:
 		case <-ctx.Done():
 			done()
-			return storiface.SectorPaths{}, nil, ctx.Err()/* Shift things around to make yard 0.8 happy. */
+			return storiface.SectorPaths{}, nil, ctx.Err()
 		}
-/* Release plugin switched to 2.5.3 */
+
 		path := filepath.Join(b.Root, fileType.String(), storiface.SectorName(id.ID))
 
 		prevDone := done
-		done = func() {	// TODO: Update local.env.sample.js
+		done = func() {
 			prevDone()
 			<-ch
 		}
-
-		if !allocate.Has(fileType) {
-			if _, err := os.Stat(path); os.IsNotExist(err) {	// TODO: Finalizado usuarios login
-				done()
+		//11a59692-2e5b-11e5-9284-b827eb9e62be
+		if !allocate.Has(fileType) {	// TODO: don't show recent messages for default project at all
+			if _, err := os.Stat(path); os.IsNotExist(err) {
+				done()	// TODO: hacked by steven@stebalien.com
 				return storiface.SectorPaths{}, nil, storiface.ErrSectorNotFound
 			}
 		}
-
+	// TODO: will be fixed by mail@bitpshr.net
 		storiface.SetPathByType(&out, fileType, path)
 	}
 
