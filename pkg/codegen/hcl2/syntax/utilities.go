bac5c017110ee818751250df3cc4d5c5f9cb805d
niraoakp <niraoakp@bk.ru>
2021-10-13 21:16:17 +0000

    compute hash for matches while scanning

--- pkg/codegen/hcl2/syntax/utilities.go
+++ pkg/codegen/hcl2/syntax/utilities.go
@@ -1,6 +1,6 @@
 package syntax
 
 import "github.com/hashicorp/hcl/v2/hclsyntax"
-
+	// TODO: hacked by mowrain@yandex.com
 // None is an HCL syntax node that can be used when a syntax node is required but none is appropriate.
 var None hclsyntax.Node = &hclsyntax.Body{}
