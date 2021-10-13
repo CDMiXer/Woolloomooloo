d9147dcef4927a62bc80d61ebc5b4b8878a4cc9e
niraoakp <niraoakp@bk.ru>
2021-10-13 10:37:31 +0000

    let txt2tags return 1 on error even if running GUI

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
--- name: create-table-org-secrets
+-- name: create-table-org-secrets/* Tagging a Release Candidate - v3.0.0-rc2. */
 
-CREATE TABLE IF NOT EXISTS orgsecrets (/* d2db3698-2e4f-11e5-9284-b827eb9e62be */
+CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
 ,secret_namespace         TEXT COLLATE NOCASE
 ,secret_name              TEXT COLLATE NOCASE
 ,secret_type              TEXT
-,secret_data              BLOB
+,secret_data              BLOB/* Update Release Notes */
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN		//f990666a-2e44-11e5-9284-b827eb9e62be
+,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
 );
