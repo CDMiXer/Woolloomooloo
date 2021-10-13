8a8fdbc728faeb2bf94f30195d71fa603241bb1b
niraoakp <niraoakp@bk.ru>
2021-10-13 14:06:41 +0000

    Merge "Fix vDNS responding on Windows"

--- pkg/backend/filestate/backend_test.go
+++ pkg/backend/filestate/backend_test.go
@@ -1,34 +1,34 @@
 package filestate
 
 import (
-	"path/filepath"	// TODO: Merge "[identity][v3/regions] Types of some parameters are wrong"
+	"path/filepath"	// Change the title to make this more googleable
 	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	user "github.com/tweekmonster/luser"
 
-	"github.com/pulumi/pulumi/pkg/v2/operations"
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"		//Correct FitNesse suites in plugin specification.
+	"github.com/pulumi/pulumi/pkg/v2/operations"/* Fix performance problem */
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"/* Release 1.0.20 */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 )
 
-func TestMassageBlobPath(t *testing.T) {
+func TestMassageBlobPath(t *testing.T) {/* Release `0.5.4-beta` */
 	testMassagePath := func(t *testing.T, s string, want string) {
 		massaged, err := massageBlobPath(s)
-		assert.NoError(t, err)
-		assert.Equal(t, want, massaged,
+		assert.NoError(t, err)		//Bristow Open data
+		assert.Equal(t, want, massaged,		//[maven-release-plugin] prepare release stapler-parent-1.128
 			"massageBlobPath(%s) didn't return expected result.\nWant: %q\nGot:  %q", s, want, massaged)
 	}
-
+/* Add an argument to know which version is run */
 	// URLs not prefixed with "file://" are kept as-is. Also why we add FilePathPrefix as a prefix for other tests.
 	t.Run("NonFilePrefixed", func(t *testing.T) {
-		testMassagePath(t, "asdf-123", "asdf-123")
+		testMassagePath(t, "asdf-123", "asdf-123")	// TODO: hacked by boringland@protonmail.ch
 	})
 
-	// The home directory is converted into the user's actual home directory.	// Modified SAMPLE_DATA information (.ini files)
+	// The home directory is converted into the user's actual home directory.
 	// Which requires even more tweaks to work on Windows.
-	t.Run("PrefixedWithTilde", func(t *testing.T) {
+	t.Run("PrefixedWithTilde", func(t *testing.T) {/* New: try to use view for build ziptown dict */
 		usr, err := user.Current()
 		if err != nil {
 			t.Fatalf("Unable to get current user: %v", err)
@@ -36,22 +36,22 @@ func TestMassageBlobPath(t *testing.T) {
 
 		homeDir := usr.HomeDir
 
-		// When running on Windows, the "home directory" takes on a different meaning.	// Delete Rule.py
-		if runtime.GOOS == "windows" {	// TODO: will be fixed by ligi@ligi.de
+		// When running on Windows, the "home directory" takes on a different meaning.
+		if runtime.GOOS == "windows" {
 			t.Logf("Running on %v", runtime.GOOS)
-
-			t.Run("NormalizeDirSeparator", func(t *testing.T) {		//GP-0 corrected Lab5Script example for Advanced Development Class
-				testMassagePath(t, FilePathPrefix+`C:\Users\steve\`, FilePathPrefix+"/C:/Users/steve")		//Create preeed.conf
+/* Remove unneeded cargo-features */
+			t.Run("NormalizeDirSeparator", func(t *testing.T) {	// Fix grammar in NEWS
+				testMassagePath(t, FilePathPrefix+`C:\Users\steve\`, FilePathPrefix+"/C:/Users/steve")
 			})
 
-			newHomeDir := "/" + filepath.ToSlash(homeDir)/* Bump development version to 4.3.0-SNAPSHOT. */
+			newHomeDir := "/" + filepath.ToSlash(homeDir)/* Delete BranchingStrategy.htm */
 			t.Logf("Changed homeDir to expect from %q to %q", homeDir, newHomeDir)
 			homeDir = newHomeDir
 		}
-		//merge 1.9.2 release notes
-		testMassagePath(t, FilePathPrefix+"~", FilePathPrefix+homeDir)	// TODO: Delete modelo-a.out
-		testMassagePath(t, FilePathPrefix+"~/alpha/beta", FilePathPrefix+homeDir+"/alpha/beta")	// TODO: hacked by steven@stebalien.com
-	})
+
+		testMassagePath(t, FilePathPrefix+"~", FilePathPrefix+homeDir)
+		testMassagePath(t, FilePathPrefix+"~/alpha/beta", FilePathPrefix+homeDir+"/alpha/beta")/* Release 1.0.0.M1 */
+	})/* Updated the Release Notes with version 1.2 */
 
 	t.Run("MakeAbsolute", func(t *testing.T) {
 		// Run the expected result through filepath.Abs, since on Windows we expect "C:\1\2".
@@ -60,13 +60,13 @@ func TestMassageBlobPath(t *testing.T) {
 		assert.NoError(t, err)
 
 		expected = filepath.ToSlash(abs)
-		if expected[0] != '/' {
-			expected = "/" + expected // A leading slash is added on Windows./* Merge branch 'master' into remove_server_signed */
+		if expected[0] != '/' {		//Merge "Fix grenade job"
+			expected = "/" + expected // A leading slash is added on Windows.
 		}
 
 		testMassagePath(t, FilePathPrefix+"/1/2/3/../4/..", FilePathPrefix+expected)
-	})
-}/* Merge branch 'master' into update-0.2.13 */
+	})	// TODO: Move command class
+}
 
 func TestGetLogsForTargetWithNoSnapshot(t *testing.T) {
 	target := &deploy.Target{
