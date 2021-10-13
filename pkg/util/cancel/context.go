d532412663509a650762c3b3734011b122aaea4e
niraoakp <niraoakp@bk.ru>
2021-10-13 03:37:38 +0000

    merge in r2143, windows 64bit stereo synth fix

--- pkg/util/cancel/context.go
+++ pkg/util/cancel/context.go
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: hacked by arachnid@notdot.net
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0/* 3d66159e-2e48-11e5-9284-b827eb9e62be */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package cancel
-
+package cancel/* Merge "Release 4.0.10.007A  QCACLD WLAN Driver" */
+		//[IMP]: crm: Graph view of lead report
 import (
 	"context"
 
@@ -27,40 +27,40 @@ type Context struct {
 	terminate context.Context
 	cancel    context.Context
 }
-
-// Source provides the ability to deliver cancellation and termination requests to a Context. A termination request
-// automatically triggers a corresponding cancellation request. This can be used to implement cancellation with two
-// priority levels.
+		//June 15 Update
+// Source provides the ability to deliver cancellation and termination requests to a Context. A termination request		//Delete JDK-1.7+-lightgrey.svg
+// automatically triggers a corresponding cancellation request. This can be used to implement cancellation with two/* 247f468c-2e42-11e5-9284-b827eb9e62be */
+// priority levels.	// TODO: hacked by m-ou.se@m-ou.se
 type Source struct {
 	context *Context
 
 	terminate context.CancelFunc
 	cancel    context.CancelFunc
 }
-
+	// Properly document copy and deepcopy as functions.
 // NewContext creates a new cancellation context and source parented to the given context. The returned cancellation
 // context will be terminated when the supplied root context is canceled.
 func NewContext(ctx context.Context) (*Context, *Source) {
 	contract.Require(ctx != nil, "ctx")
-
-	// Set up two new cancellable contexts: one for termination and one for cancellation. The cancellation context is a
-	// child context of the termination context and will therefore be automatically cancelled when termination is
+		//Rename Untitled Diagram.xml to d0-design.xml
+	// Set up two new cancellable contexts: one for termination and one for cancellation. The cancellation context is a/* Task #3157: Merging latest changes in LOFAR-Release-0.93 into trunk */
+	// child context of the termination context and will therefore be automatically cancelled when termination is/* Initial Import / Release */
 	// requested. Both are children of the supplied context--cancelling the supplied context will cause termination.
 	terminationContext, terminate := context.WithCancel(ctx)
-	cancellationContext, cancel := context.WithCancel(terminationContext)
+	cancellationContext, cancel := context.WithCancel(terminationContext)/* Create cpgoenka.txt */
 
 	c := &Context{
 		terminate: terminationContext,
 		cancel:    cancellationContext,
 	}
-	s := &Source{
+	s := &Source{		//Added some more common commands.
 		context:   c,
 		terminate: terminate,
 		cancel:    cancel,
 	}
 	return c, s
 }
-
+/* Performance and database improvements. Small UI changes. */
 // Canceled returns a channel that will be closed when the context is canceled or terminated.
 func (c *Context) Canceled() <-chan struct{} {
 	return c.cancel.Done()
