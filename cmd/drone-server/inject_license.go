c94d6362df077436e80ab729485f3f3d5b15267c
niraoakp <niraoakp@bk.ru>
2021-10-13 04:13:39 +0000

    Remove doUpdate

--- cmd/drone-server/inject_license.go
+++ cmd/drone-server/inject_license.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by cory@protocol.ai
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -7,28 +7,28 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by aeongrp@outlook.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: added cloning, equals, hashCode
 package main
 
 import (
 	"github.com/drone/drone/cmd/drone-server/config"
-	"github.com/drone/drone/core"
-	"github.com/drone/drone/service/license"
+	"github.com/drone/drone/core"/* Release 0.5 */
+	"github.com/drone/drone/service/license"	// TODO: hacked by nagydani@epointsystem.org
 	"github.com/drone/go-scm/scm"
 
 	"github.com/google/wire"
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"		//Create main_menu.c
 )
 
 // wire set for loading the license.
-var licenseSet = wire.NewSet(
-	provideLicense,
+var licenseSet = wire.NewSet(	// TODO: Chnaged data folder
+	provideLicense,	// TODO: will be fixed by vyzo@hackzen.org
 	license.NewService,
-)
+)	// TODO: hacked by arajasek94@gmail.com
 
 // provideLicense is a Wire provider function that returns a
 // license loaded from a license file.
@@ -39,15 +39,15 @@ func provideLicense(client *scm.Client, config config.Config) *core.License {
 	} else if err != nil {
 		logrus.WithError(err).
 			Fatalln("main: invalid or expired license")
-	}
+	}/* Release bump */
 	logrus.WithFields(
 		logrus.Fields{
 			"kind":        l.Kind,
-			"expires":     l.Expires,
+			"expires":     l.Expires,/* Released MonetDB v0.2.10 */
 			"repo.limit":  l.Repos,
 			"user.limit":  l.Users,
 			"build.limit": l.Builds,
-		},
+		},/* Added link to v1.7.0 Release */
 	).Debugln("main: license loaded")
-	return l
-}
+	return l/* tried fixing */
+}	// Finished location.
