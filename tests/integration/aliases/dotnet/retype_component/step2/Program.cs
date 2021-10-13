0dddd679270b49d0488c8c3dd195aa0187349da9
niraoakp <niraoakp@bk.ru>
2021-10-13 19:24:16 +0000

    Add link to buddycloud manual

--- tests/integration/aliases/dotnet/retype_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/retype_component/step2/Program.cs
@@ -1,41 +1,41 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
+/* Release v3.6.7 */
 using System.Threading.Tasks;
 using Pulumi;
-/* Paul's Dec 3 version */
+
 class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
-    }
+    }/* install phantomjs-prebuilt@2.1 via npm on travis */
 }
 
 // Scenario #4 - change the type of a component
 class ComponentFour : ComponentResource
 {
     private Resource resource;
-
+/* Fiches pour élèves */
     public ComponentFour(string name, ComponentResourceOptions options = null)
         : base("my:differentmodule:ComponentFourWithADifferentTypeName", name, ComponentResourceOptions.Merge(options, new ComponentResourceOptions
-        {
-            // Add an alias that references the old type of this resource
+        {/* * Upgrade queries for 1.1.2.1 */
+            // Add an alias that references the old type of this resource	// TODO: missing new conf file
             // and then make the base() call with the new type of this resource and the added alias.
             Aliases = { new Alias { Type = "my:module:ComponentFour" } }
-        }))/* Release 1.0 008.01: work in progress. */
+        }))
     {
         // The child resource will also pick up an implicit alias due to the new type of the component it is parented to.
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
-    }
-}		//Removed dead code around register_iterm_tree_changes() in Session
+    }	// Made adjustments to network view.
+}
 
-class Program	// TODO: hacked by yuvalalaluf@gmail.com
-{
+class Program
+{/* Release v17.42 with minor emote updates and BGM improvement */
     static Task<int> Main(string[] args)
-    {		//Let all test cases inherit testcase base class.
+    {
         return Deployment.RunAsync(() =>
         {
             var comp4 = new ComponentFour("comp4");
-        });/* Rename qualitative_coding to qualitative_coding.R */
-    }	// Update for GroupManager - handle offline users better
+        });
+    }	// TODO: will be fixed by igor@soramitsu.co.jp
 }
