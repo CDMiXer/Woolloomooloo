8342971c208e9160d4b321606b0bb4c124b0e7ce
niraoakp <niraoakp@bk.ru>
2021-10-13 20:53:27 +0000

    top padding and fixed position on tabs

--- handler/api/events/build_test.go
+++ handler/api/events/build_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// Fix send commande icone
 // that can be found in the LICENSE file.
-
+/* Delete Data_Releases.rst */
 package events
