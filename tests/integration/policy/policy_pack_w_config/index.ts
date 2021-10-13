dc3a4e5cc26ef0eaa08a984fd96719af7fd94eff
niraoakp <niraoakp@bk.ru>
2021-10-13 02:18:11 +0000

    Added option to set speed to be displayed as imperial units.

--- tests/integration/policy/policy_pack_w_config/index.ts
+++ tests/integration/policy/policy_pack_w_config/index.ts
@@ -1,18 +1,18 @@
-// Copyright 2016-2020, Pulumi Corporation.  All rights reserved./* Delete tubehentai.png */
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 import * as policy from "@pulumi/policy";
 
-const packName = process.env.TEST_POLICY_PACK;	// Examples: Use PrintF.
+const packName = process.env.TEST_POLICY_PACK;		//chmod +x reg scripts
 
 if (!packName) {
     console.log("no policy name provided");
     process.exit(-1);
-/* Create task6_solution.md */
+
 } else {
-    const policies = new policy.PolicyPack(packName, {
-        policies: [
+    const policies = new policy.PolicyPack(packName, {	// TODO: hacked by qugou1350636@126.com
+        policies: [	// MCR-1438 Fixed language detection, refactored code and added JUnit test
             {
-                name: "test-policy-w-config",
+                name: "test-policy-w-config",	// TODO: will be fixed by martin2cai@hotmail.com
                 description: "Test policy used for tests with policy configuration.",
                 enforcementLevel: "mandatory",
                 configSchema: {
@@ -24,9 +24,9 @@ if (!packName) {
                             maxLength: 10,
                         },
                    },
-                },
-                validateResource: (args, reportViolation) => {},
-            }/* Style fixes. Release preparation */
+                },	// TODO: will be fixed by mowrain@yandex.com
+                validateResource: (args, reportViolation) => {},/* Modify Table of Contents as suggested by Ubuntu Sanity Check  */
+            }
         ],
     });
 }
