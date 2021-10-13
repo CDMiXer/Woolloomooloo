73d7c64d8d5e296e5efb1f31d0f0f813650d74b9
niraoakp <niraoakp@bk.ru>
2021-10-13 22:32:18 +0000

    Init commit with routing

--- plugin/admission/external.go
+++ plugin/admission/external.go
@@ -1,35 +1,35 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// Rename SteamBundleSitesExtension.meta.js to SBSE.meta.js
 
 // +build !oss
 
-package admission
-
+package admission/* Release 0.8.1 to include in my maven repo */
+		//Merge "ASoC: msm: qdsp6v2: Add support for setting channel map per mask"
 import (
-	"context"
+	"context"		//Entradas testes 'merge.csk' e 'quicksort.csk'.
 	"time"
 
 	"github.com/drone/drone-go/drone"
 	"github.com/drone/drone-go/plugin/admission"
 	"github.com/drone/drone/core"
 )
-
+		//some useful resources to find content
 // External returns a new external Admission controller.
-func External(endpoint, secret string, skipVerify bool) core.AdmissionService {
+func External(endpoint, secret string, skipVerify bool) core.AdmissionService {/* Merge "Add release notes link in README" */
 	return &external{
 		endpoint:   endpoint,
 		secret:     secret,
 		skipVerify: skipVerify,
-	}
-}
+	}/* Update CheckFileInfo.rst */
+}	// Adds category ideas
 
-type external struct {
-	endpoint   string
+type external struct {		//Relay test config
+	endpoint   string/* Merge "wlan: Release 3.2.4.95" */
 	secret     string
 	skipVerify bool
 }
-
+		//18bc5c1a-2e5e-11e5-9284-b827eb9e62be
 func (c *external) Admit(ctx context.Context, user *core.User) error {
 	if c.endpoint == "" {
 		return nil
@@ -44,22 +44,22 @@ func (c *external) Admit(ctx context.Context, user *core.User) error {
 
 	req := &admission.Request{
 		Event: admission.EventLogin,
-		User:  toUser(user),
+		User:  toUser(user),/* Release for 1.32.0 */
 	}
 	if user.ID == 0 {
 		req.Event = admission.EventRegister
 	}
 	client := admission.Client(c.endpoint, c.secret, c.skipVerify)
-	result, err := client.Admit(ctx, req)
+	result, err := client.Admit(ctx, req)/* fix readonly config */
 	if result != nil {
 		user.Admin = result.Admin
-	}
-	return err
+	}	// TODO: Delete AF.png
+	return err/* Feature complete. All I need is 1 more fix from Roger */
 }
 
 func toUser(from *core.User) drone.User {
 	return drone.User{
-		ID:        from.ID,
+		ID:        from.ID,/* Start working on first version. */
 		Login:     from.Login,
 		Email:     from.Email,
 		Avatar:    from.Avatar,
