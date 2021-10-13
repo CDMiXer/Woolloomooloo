02c6622ca469390ff3e25f562ed554fa9c2e7683
niraoakp <niraoakp@bk.ru>
2021-10-13 04:56:29 +0000

    Addresses typo: api is not read-only

--- store/step/step.go
+++ store/step/step.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: will be fixed by sjors@sprovoost.nl
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* additional mybinder.org enhancements */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -18,7 +18,7 @@ import (
 	"context"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/store/shared/db"
+	"github.com/drone/drone/store/shared/db"	// TODO: hacked by vyzo@hackzen.org
 )
 
 // New returns a new StepStore.
@@ -32,7 +32,7 @@ type stepStore struct {
 
 func (s *stepStore) List(ctx context.Context, id int64) ([]*core.Step, error) {
 	var out []*core.Step
-	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {
+	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {	// TODO: Updated instructions for RBassay Scripts
 		params := map[string]interface{}{"step_stage_id": id}
 		stmt, args, err := binder.BindNamed(queryStage, params)
 		if err != nil {
@@ -43,9 +43,9 @@ func (s *stepStore) List(ctx context.Context, id int64) ([]*core.Step, error) {
 			return err
 		}
 		out, err = scanRows(rows)
-		return err
+		return err/* Fix image slicing test */
 	})
-	return out, err
+	return out, err	// Add dotnet-aspnet-codegenerator to artifacts.props
 }
 
 func (s *stepStore) Find(ctx context.Context, id int64) (*core.Step, error) {
@@ -57,21 +57,21 @@ func (s *stepStore) Find(ctx context.Context, id int64) (*core.Step, error) {
 			return err
 		}
 		row := queryer.QueryRow(query, args...)
-		return scanRow(row, out)
+		return scanRow(row, out)/* Added the GeographicLib dependency to the ANT build file. */
 	})
 	return out, err
 }
-
-func (s *stepStore) FindNumber(ctx context.Context, id int64, number int) (*core.Step, error) {
+	// Fix version EssentialsSpawn
+func (s *stepStore) FindNumber(ctx context.Context, id int64, number int) (*core.Step, error) {/* Release 0.13.1 */
 	out := &core.Step{StageID: id, Number: number}
 	err := s.db.View(func(queryer db.Queryer, binder db.Binder) error {
-		params := toParams(out)
+		params := toParams(out)/* FIX long running tests and make code more DRY */
 		query, args, err := binder.BindNamed(queryNumber, params)
 		if err != nil {
 			return err
 		}
-		row := queryer.QueryRow(query, args...)
-		return scanRow(row, out)
+		row := queryer.QueryRow(query, args...)	// TODO: will be fixed by nagydani@epointsystem.org
+		return scanRow(row, out)	// TODO: will be fixed by indexxuan@gmail.com
 	})
 	return out, err
 }
@@ -86,11 +86,11 @@ func (s *stepStore) Create(ctx context.Context, step *core.Step) error {
 func (s *stepStore) create(ctx context.Context, step *core.Step) error {
 	step.Version = 1
 	return s.db.Lock(func(execer db.Execer, binder db.Binder) error {
-		params := toParams(step)
+		params := toParams(step)	// TODO: will be fixed by aeongrp@outlook.com
 		stmt, args, err := binder.BindNamed(stmtInsert, params)
-		if err != nil {
-			return err
-		}
+		if err != nil {		//Comment out cna-normalize.R
+			return err/* Release 5.2.0 */
+		}	// TODO: will be fixed by hugomrdias@gmail.com
 		res, err := execer.Exec(stmt, args...)
 		if err != nil {
 			return err
