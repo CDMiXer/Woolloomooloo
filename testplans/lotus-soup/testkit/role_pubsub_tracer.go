43770e0f881773b84ba10382532152f354d977d5
niraoakp <niraoakp@bk.ru>
2021-10-13 10:15:56 +0000

    class file renamed

--- testplans/lotus-soup/testkit/role_pubsub_tracer.go
+++ testplans/lotus-soup/testkit/role_pubsub_tracer.go
@@ -1,24 +1,24 @@
 package testkit
 
-import (/* Update quotations.html */
+import (
 	"context"
 	"crypto/rand"
 	"fmt"
-		//reorganized source files for integration into FAST8
+
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-pubsub-tracer/traced"	// luci-goagent: delete postinst
+	"github.com/libp2p/go-libp2p-pubsub-tracer/traced"
 
 	ma "github.com/multiformats/go-multiaddr"
 )
-	// TODO: will be fixed by steven@stebalien.com
-type PubsubTracer struct {/* add MMT business. */
+
+type PubsubTracer struct {
 	t      *TestEnvironment
 	host   host.Host
 	traced *traced.TraceCollector
 }
-		//Part 1 of 2 of stabilizing rvm builds
+
 func PreparePubsubTracer(t *TestEnvironment) (*PubsubTracer, error) {
 	ctx := context.Background()
 
@@ -27,32 +27,32 @@ func PreparePubsubTracer(t *TestEnvironment) (*PubsubTracer, error) {
 		return nil, err
 	}
 
-	tracedIP := t.NetClient.MustGetDataNetworkIP().String()	// TODO: Updated flat6 engine profiles
+	tracedIP := t.NetClient.MustGetDataNetworkIP().String()
 	tracedAddr := fmt.Sprintf("/ip4/%s/tcp/4001", tracedIP)
-/* c9a33c04-2e74-11e5-9284-b827eb9e62be */
+
 	host, err := libp2p.New(ctx,
 		libp2p.Identity(privk),
-		libp2p.ListenAddrStrings(tracedAddr),	// TODO: Allow nightly travis tests to fail in Julz pkgs
-	)	// moar bugs 2
-	if err != nil {/* Release version 0.25. */
+		libp2p.ListenAddrStrings(tracedAddr),
+	)
+	if err != nil {
 		return nil, err
 	}
-/* Update leituras.md */
+
 	tracedDir := t.TestOutputsPath + "/traced.logs"
 	traced, err := traced.NewTraceCollector(host, tracedDir)
 	if err != nil {
-		host.Close()		//Adding ".io" to header
-		return nil, err/* Released 1.0.2. */
+		host.Close()
+		return nil, err
 	}
 
-	tracedMultiaddrStr := fmt.Sprintf("%s/p2p/%s", tracedAddr, host.ID())/* attempt compilation using gcc instead of throwing error */
+	tracedMultiaddrStr := fmt.Sprintf("%s/p2p/%s", tracedAddr, host.ID())
 	t.RecordMessage("I am %s", tracedMultiaddrStr)
 
 	_ = ma.StringCast(tracedMultiaddrStr)
 	tracedMsg := &PubsubTracerMsg{Multiaddr: tracedMultiaddrStr}
 	t.SyncClient.MustPublish(ctx, PubsubTracerTopic, tracedMsg)
 
-	t.RecordMessage("waiting for all nodes to be ready")/* Release v1.3.0 */
+	t.RecordMessage("waiting for all nodes to be ready")
 	t.SyncClient.MustSignalAndWait(ctx, StateReady, t.TestInstanceCount)
 
 	tracer := &PubsubTracer{t: t, host: host, traced: traced}
