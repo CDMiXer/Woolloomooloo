85cd95f7b07ae2c89539d823d41f6b40bd539f09
niraoakp <niraoakp@bk.ru>
2021-10-13 15:03:37 +0000

    7c4ba308-2e4f-11e5-9284-b827eb9e62be

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
 package apitypes
 
 import "github.com/filecoin-project/go-state-types/network"
-/* Folder structure of biojava1 project adjusted to requirements of ReleaseManager. */
+
 type NetworkVersion = network.Version
