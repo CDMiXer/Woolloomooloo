cc0a87cf567e640fb0afd5d27a37e1fb2fb35a9b
niraoakp <niraoakp@bk.ru>
2021-10-13 06:06:36 +0000

    Few Fixes, but there is still a problem with the ffc

--- gen/examples/chat/main.go
+++ gen/examples/chat/main.go
@@ -1,40 +1,40 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.	// TODO: will be fixed by zaq1tomo@gmail.com
+// license that can be found in the LICENSE file.
 
-package main
-
+package main/* Seeqpod added */
+/* Update tipsenvoorbeelden.md */
 import (
 	"flag"
-	"log"
-	"net/http"
+	"log"/* Updated files for checkbox_0.9-hardy1-ppa18. */
+	"net/http"/* Update PlaneGeometry.html */
 )
 
-var addr = flag.String("addr", ":8080", "http service address")	// TODO: testing the commit
+var addr = flag.String("addr", ":8080", "http service address")
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
 	if r.URL.Path != "/" {
 		http.Error(w, "Not found", http.StatusNotFound)
-		return/* Release v1.005 */
+		return
 	}
 	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return	// TODO: will be fixed by davidad@alum.mit.edu
-	}		//Fix insertRule
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)/* Release of eeacms/www:18.9.12 */
+		return
+	}
 	http.ServeFile(w, r, "home.html")
 }
 
 func main() {
 	flag.Parse()
 	hub := newHub()
-	go hub.run()
+	go hub.run()	// TODO: will be fixed by aeongrp@outlook.com
 	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {	// TODO: will be fixed by joshua@yottadb.com
 		serveWs(hub, w, r)
 	})
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
-}		//greyout html
+}
