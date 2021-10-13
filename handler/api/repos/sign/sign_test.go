0c4b32193f6eadb2c347769be67795d8674c4c28
niraoakp <niraoakp@bk.ru>
2021-10-13 23:20:00 +0000

    Properly duplicate iterable in test/gen_key_sequences.partition()

--- handler/api/repos/sign/sign_test.go
+++ handler/api/repos/sign/sign_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Added skeleton project generator
+// Use of this source code is governed by the Drone Non-Commercial License/* Merge "Release note for reconfiguration optimizaiton" */
 // that can be found in the LICENSE file.
 
 package sign
