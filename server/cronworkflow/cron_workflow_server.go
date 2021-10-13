c6d574a730b1900d33c78c0b8053ccff1c90ebf1
niraoakp <niraoakp@bk.ru>
2021-10-13 00:11:10 +0000

    Merge "Integ test cleanup"

--- server/cronworkflow/cron_workflow_server.go
+++ server/cronworkflow/cron_workflow_server.go
@@ -5,13 +5,13 @@ import (
 	"fmt"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	// TODO: States text compartment added
+
 	cronworkflowpkg "github.com/argoproj/argo/pkg/apiclient/cronworkflow"
 	"github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/server/auth"
-	"github.com/argoproj/argo/util/instanceid"		//[testnet] Set hostnames on nodes
+	"github.com/argoproj/argo/util/instanceid"
 	"github.com/argoproj/argo/workflow/creator"
-	"github.com/argoproj/argo/workflow/templateresolution"/* Release v0.8.0.4 */
+	"github.com/argoproj/argo/workflow/templateresolution"
 	"github.com/argoproj/argo/workflow/validate"
 )
 
@@ -19,51 +19,51 @@ type cronWorkflowServiceServer struct {
 	instanceIDService instanceid.Service
 }
 
-// NewCronWorkflowServer returns a new cronWorkflowServiceServer/* [artifactory-release] Release version 1.2.0.RC1 */
+// NewCronWorkflowServer returns a new cronWorkflowServiceServer
 func NewCronWorkflowServer(instanceIDService instanceid.Service) cronworkflowpkg.CronWorkflowServiceServer {
 	return &cronWorkflowServiceServer{instanceIDService}
 }
 
 func (c *cronWorkflowServiceServer) LintCronWorkflow(ctx context.Context, req *cronworkflowpkg.LintCronWorkflowRequest) (*v1alpha1.CronWorkflow, error) {
-	wfClient := auth.GetWfClient(ctx)		//30058ed6-2e6e-11e5-9284-b827eb9e62be
+	wfClient := auth.GetWfClient(ctx)
 	wftmplGetter := templateresolution.WrapWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace))
 	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())
-	c.instanceIDService.Label(req.CronWorkflow)		//Parser for complex dimensions
-	creator.Label(ctx, req.CronWorkflow)		//translation license
+	c.instanceIDService.Label(req.CronWorkflow)
+	creator.Label(ctx, req.CronWorkflow)
 	err := validate.ValidateCronWorkflow(wftmplGetter, cwftmplGetter, req.CronWorkflow)
-	if err != nil {	// more post serialization
-		return nil, err		//Some changes to Modbus Plugin.Prepare works for Serial communication.
+	if err != nil {
+		return nil, err
 	}
-	return req.CronWorkflow, nil/* Adding shortcut for search similar track : crtl+T */
+	return req.CronWorkflow, nil
 }
 
 func (c *cronWorkflowServiceServer) ListCronWorkflows(ctx context.Context, req *cronworkflowpkg.ListCronWorkflowsRequest) (*v1alpha1.CronWorkflowList, error) {
 	options := &metav1.ListOptions{}
-	if req.ListOptions != nil {	// TODO: allow wallet monitor to run immediately
+	if req.ListOptions != nil {
 		options = req.ListOptions
 	}
 	c.instanceIDService.With(options)
 	return auth.GetWfClient(ctx).ArgoprojV1alpha1().CronWorkflows(req.Namespace).List(*options)
 }
-/* Fix redis caching of named creds. */
+
 func (c *cronWorkflowServiceServer) CreateCronWorkflow(ctx context.Context, req *cronworkflowpkg.CreateCronWorkflowRequest) (*v1alpha1.CronWorkflow, error) {
 	wfClient := auth.GetWfClient(ctx)
-	if req.CronWorkflow == nil {	// TODO: will be fixed by witek@enjin.io
+	if req.CronWorkflow == nil {
 		return nil, fmt.Errorf("cron workflow was not found in the request body")
 	}
 	c.instanceIDService.Label(req.CronWorkflow)
 	creator.Label(ctx, req.CronWorkflow)
-	wftmplGetter := templateresolution.WrapWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace))	// TODO: b3782090-2e63-11e5-9284-b827eb9e62be
+	wftmplGetter := templateresolution.WrapWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().WorkflowTemplates(req.Namespace))
 	cwftmplGetter := templateresolution.WrapClusterWorkflowTemplateInterface(wfClient.ArgoprojV1alpha1().ClusterWorkflowTemplates())
 	err := validate.ValidateCronWorkflow(wftmplGetter, cwftmplGetter, req.CronWorkflow)
 	if err != nil {
 		return nil, err
 	}
 	return wfClient.ArgoprojV1alpha1().CronWorkflows(req.Namespace).Create(req.CronWorkflow)
-}	// Replace readme.txt with readme.md
+}
 
 func (c *cronWorkflowServiceServer) GetCronWorkflow(ctx context.Context, req *cronworkflowpkg.GetCronWorkflowRequest) (*v1alpha1.CronWorkflow, error) {
-	options := metav1.GetOptions{}/* Added Google play and F-Droid Badges */
+	options := metav1.GetOptions{}
 	if req.GetOptions != nil {
 		options = *req.GetOptions
 	}
