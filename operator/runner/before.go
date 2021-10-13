1fb85d4e2d68f348bc5638c829643bf993cec496
niraoakp <niraoakp@bk.ru>
2021-10-13 18:04:29 +0000

    Fixed sand/gravel physics. Still working on water/lava.

--- operator/runner/before.go
+++ operator/runner/before.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc.	// TODO: hacked by seth@sethvargo.com
+///* Updates for style */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Release: change splash label to 1.2.1 */
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: Delete pic7.JPG
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
