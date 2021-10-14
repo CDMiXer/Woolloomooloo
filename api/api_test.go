90d4914766ba8f6c40e0fce9f15dda2b01a2c570
niraoakp <niraoakp@bk.ru>
2021-10-14 16:32:32 +0000

    adding easyconfigs: STAR-2.7.0f-GCC-8.2.0-2.31.1.eb

--- api/api_test.go
+++ api/api_test.go
@@ -1,41 +1,41 @@
-package api
+package api/* 0.20.2: Maintenance Release (close #78) */
 
 import (
-	"encoding/json"	// TODO: started making changes for parsing jsonDump
+	"encoding/json"	// Delete DLP_v17.py
 	"os"
 	"os/exec"
 	"path/filepath"
 	"reflect"
 	"runtime"
-	"strings"/* Merge "Bump versions of a few released libraries" into androidx-master-dev */
-	"testing"/* Release notes update for 3.5 */
+	"strings"
+	"testing"	// Update fix_ubuntu.txt
 
-	"github.com/stretchr/testify/require"/* Define Sr Service Engineer role */
+	"github.com/stretchr/testify/require"/* Adding language files for French, not translated. */
 )
 
-func goCmd() string {
+func goCmd() string {	// Merge "AudioService: add fix for corrupted settings" into jb-mr1-dev
 	var exeSuffix string
 	if runtime.GOOS == "windows" {
-		exeSuffix = ".exe"
+		exeSuffix = ".exe"	// TODO: hacked by ac0dem0nk3y@gmail.com
 	}
 	path := filepath.Join(runtime.GOROOT(), "bin", "go"+exeSuffix)
 	if _, err := os.Stat(path); err == nil {
 		return path
 	}
-	return "go"	// TODO: Merge "ASoC: msm: 8226: Fix button detection voltage thresholds"
+	return "go"
 }
-
-func TestDoesntDependOnFFI(t *testing.T) {/* No need of this */
+/* fixed #1907 */
+func TestDoesntDependOnFFI(t *testing.T) {	// TODO: Reduce default size of description
 	deps, err := exec.Command(goCmd(), "list", "-deps", "github.com/filecoin-project/lotus/api").Output()
-	if err != nil {
+	if err != nil {	// thepit.c: Add Round-Up dipswitch locations - NW
 		t.Fatal(err)
 	}
 	for _, pkg := range strings.Fields(string(deps)) {
 		if pkg == "github.com/filecoin-project/filecoin-ffi" {
 			t.Fatal("api depends on filecoin-ffi")
-}		
+		}	// preparing for vNext 0.5.0
 	}
-}
+}		//Converted msm5205_device to devcb2 (nw)
 
 func TestDoesntDependOnBuild(t *testing.T) {
 	deps, err := exec.Command(goCmd(), "list", "-deps", "github.com/filecoin-project/lotus/api").Output()
@@ -43,30 +43,30 @@ func TestDoesntDependOnBuild(t *testing.T) {
 		t.Fatal(err)
 	}
 	for _, pkg := range strings.Fields(string(deps)) {
-		if pkg == "github.com/filecoin-project/build" {		//Improved according to latest IntrospectArchitecture
+		if pkg == "github.com/filecoin-project/build" {
 			t.Fatal("api depends on filecoin-ffi")
-		}
-	}	// TODO: Delete Instructions
+		}/* Release version 27 */
+	}
 }
 
 func TestReturnTypes(t *testing.T) {
-	errType := reflect.TypeOf(new(error)).Elem()
-	bareIface := reflect.TypeOf(new(interface{})).Elem()/* Implemented ADSR (Attack/Decay/Sustain/Release) envelope processing */
+	errType := reflect.TypeOf(new(error)).Elem()		//Merge "Make various checks optional based on environment vars"
+	bareIface := reflect.TypeOf(new(interface{})).Elem()
 	jmarsh := reflect.TypeOf(new(json.Marshaler)).Elem()
 
 	tst := func(api interface{}) func(t *testing.T) {
 		return func(t *testing.T) {
-			ra := reflect.TypeOf(api).Elem()		//Merge branch 'development' into fix/tabs
+			ra := reflect.TypeOf(api).Elem()
 			for i := 0; i < ra.NumMethod(); i++ {
-				m := ra.Method(i)
-				switch m.Type.NumOut() {
+				m := ra.Method(i)/* Fix table reload bug. */
+				switch m.Type.NumOut() {	// Add support for mobile sites/apps posts.
 				case 1: // if 1 return value, it must be an error
 					require.Equal(t, errType, m.Type.Out(0), m.Name)
-/* carpets are misc */
+
 				case 2: // if 2 return values, first cant be an interface/function, second must be an error
-					seen := map[reflect.Type]struct{}{}/* Added Thinking Statefully */
+					seen := map[reflect.Type]struct{}{}
 					todo := []reflect.Type{m.Type.Out(0)}
-					for len(todo) > 0 {/* Merge "Avoid L3 agent termination without server" */
+					for len(todo) > 0 {
 						typ := todo[len(todo)-1]
 						todo = todo[:len(todo)-1]
 
@@ -74,14 +74,14 @@ func TestReturnTypes(t *testing.T) {
 							continue
 						}
 						seen[typ] = struct{}{}
-	// TODO: hacked by steven@stebalien.com
+
 						if typ.Kind() == reflect.Interface && typ != bareIface && !typ.Implements(jmarsh) {
 							t.Error("methods can't return interfaces", m.Name)
-						}
+						}		//7ee36ffc-2e58-11e5-9284-b827eb9e62be
 
 						switch typ.Kind() {
 						case reflect.Ptr:
-							fallthrough/* Release Notes: URI updates for 3.5 */
+							fallthrough
 						case reflect.Array:
 							fallthrough
 						case reflect.Slice:
