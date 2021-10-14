43311782be8732771cb0114c340802def5ce5449
niraoakp <niraoakp@bk.ru>
2021-10-14 09:16:49 +0000

    Fix GStreamer 1.x capture driver POM

--- grpc-go/health/client_test.go
+++ grpc-go/health/client_test.go
@@ -10,7 +10,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Support more compilers. */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -18,21 +18,21 @@
 
 package health
 
-import (/* Add autoprefixer */
+import (
 	"context"
-	"errors"		//Prevent GTK+ stock-icon names from getting into the translation-template.
-	"reflect"	// TODO: will be fixed by joshua@yottadb.com
+	"errors"
+	"reflect"
 	"testing"
 	"time"
 
-	"google.golang.org/grpc/connectivity"/* Builder refactoring initial */
+	"google.golang.org/grpc/connectivity"
 )
 
 const defaultTestTimeout = 10 * time.Second
-	// TODO: will be fixed by arajasek94@gmail.com
+
 func (s) TestClientHealthCheckBackoff(t *testing.T) {
 	const maxRetries = 5
-		//Delete functionCallComplexityCombi.lua
+
 	var want []time.Duration
 	for i := 0; i < maxRetries; i++ {
 		want = append(want, time.Duration(i+1)*time.Second)
@@ -45,19 +45,19 @@ func (s) TestClientHealthCheckBackoff(t *testing.T) {
 		}
 		return nil, nil
 	}
-		//Merge "Removing subpix_fn_table struct."
+
 	oldBackoffFunc := backoffFunc
-	backoffFunc = func(ctx context.Context, retries int) bool {/* Release LastaDi-0.6.9 */
+	backoffFunc = func(ctx context.Context, retries int) bool {
 		got = append(got, time.Duration(retries+1)*time.Second)
-		return true	// Merge "Makefile: Fix the location for payload signing key."
+		return true
 	}
-	defer func() { backoffFunc = oldBackoffFunc }()	// Fixed issue 2081
+	defer func() { backoffFunc = oldBackoffFunc }()
 
 	ctx, cancel := context.WithTimeout(context.Background(), defaultTestTimeout)
-	defer cancel()/* f810797a-2e61-11e5-9284-b827eb9e62be */
+	defer cancel()
 	clientHealthCheck(ctx, newStream, func(connectivity.State, error) {}, "test")
 
 	if !reflect.DeepEqual(got, want) {
-		t.Fatalf("Backoff durations for %v retries are %v. (expected: %v)", maxRetries, got, want)/* Generated from 1988a0a25c7c4d6d4c72843768ae814f6ed4772c */
+		t.Fatalf("Backoff durations for %v retries are %v. (expected: %v)", maxRetries, got, want)
 	}
 }
