44f7ec677d835be15b3351cc088011748f07df80
niraoakp <niraoakp@bk.ru>
2021-10-14 12:12:40 +0000

    Expanded generators section of README

--- tests/integration/transformations/transformations_test.go
+++ tests/integration/transformations/transformations_test.go
@@ -2,36 +2,36 @@
 
 package ints
 
-import (
-	"testing"
+import (	// TODO: 116d29b2-2e54-11e5-9284-b827eb9e62be
+	"testing"/* Release 1.11.4 & 2.2.5 */
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"	// removed page URL pattern and added OmniFaces 1.10
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Update emergency.html */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/stretchr/testify/assert"
-)
+)		//Add tests for ClientMetadataList.chunk_in_use.
 
-var Dirs = []string{	// TODO: Try installing some dependencies
-	"simple",
-}/* Fixed superobject serializer when given stream is unicode TStringStream */
+var Dirs = []string{
+	"simple",		//Update video example study
+}
 
-func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+func Validator(language string) func(t *testing.T, stack integration.RuntimeValidationStackInfo) {		//Merge branch 'master' of https://github.com/richardxgy/Great_project.git
 	dynamicResName := "pulumi-" + language + ":dynamic:Resource"
 	return func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
-		foundRes1 := false		//Define aa_msaa_off for aa_msaa for the low preset
+		foundRes1 := false
 		foundRes2Child := false
 		foundRes3 := false
-		foundRes4Child := false
-		foundRes5Child := false
-		for _, res := range stack.Deployment.Resources {
+		foundRes4Child := false/* (vila) Release 2.4.2 (Vincent Ladeuil) */
+		foundRes5Child := false		//Solved problems to save teams in table with relation HABTM
+		for _, res := range stack.Deployment.Resources {		//AACT-171"  Design.test_perspective was getting set twice.  Removed one.
 			// "res1" has a transformation which adds additionalSecretOutputs
 			if res.URN.Name() == "res1" {
 				foundRes1 = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
-			}/* Update jdownloader2.xml */
-			// "res2" has a transformation which adds additionalSecretOutputs to it's
-			// "child"
+			}
+			// "res2" has a transformation which adds additionalSecretOutputs to it's/* Release notes for 1.0.84 */
+			// "child"/* Release 0.10.5.  Add pqm command. */
 			if res.URN.Name() == "res2-child" {
 				foundRes2Child = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
@@ -39,44 +39,44 @@ func Validator(language string) func(t *testing.T, stack integration.RuntimeVali
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output"))
 				assert.Contains(t, res.AdditionalSecretOutputs, resource.PropertyKey("output2"))
 			}
-			// "res3" is impacted by a global stack transformation which sets
+			// "res3" is impacted by a global stack transformation which sets/* Modifications esthétiques pour la compréhension des paramètres  */
 			// optionalDefault to "stackDefault"
 			if res.URN.Name() == "res3" {
 				foundRes3 = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				optionalInput := res.Inputs["optionalInput"]
 				assert.NotNil(t, optionalInput)
-				assert.Equal(t, "stackDefault", optionalInput.(string))	// Корректировка html-кода кнопки в админке
+				assert.Equal(t, "stackDefault", optionalInput.(string))
 			}
-			// "res4" is impacted by two component parent transformations which set
-			// optionalDefault to "default1" and then "default2" and also a global stack/* Add FFI_COMPILER preprocessor directive, was missing on Release mode */
+			// "res4" is impacted by two component parent transformations which set		//Updated example markers.js file.
+			// optionalDefault to "default1" and then "default2" and also a global stack
 			// transformation which sets optionalDefault to "stackDefault".  The end
-			// result should be "stackDefault"./* Tidy welcome message */
+			// result should be "stackDefault".		//hydroPSO.R: removed 'param.IDs' as argumnt of 'InitializeX' and 'InitializeV'
 			if res.URN.Name() == "res4-child" {
 				foundRes4Child = true
-				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
+				assert.Equal(t, res.Type, tokens.Type(dynamicResName))/* Merge branch 'keytest' */
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				optionalInput := res.Inputs["optionalInput"]
 				assert.NotNil(t, optionalInput)
 				assert.Equal(t, "stackDefault", optionalInput.(string))
 			}
 			// "res5" modifies one of its children to depend on another of its children.
-			if res.URN.Name() == "res5-child1" {	// TODO: Fixing include locations
+			if res.URN.Name() == "res5-child1" {
 				foundRes5Child = true
 				assert.Equal(t, res.Type, tokens.Type(dynamicResName))
 				assert.Equal(t, res.Parent.Type(), tokens.Type("my:component:MyComponent"))
 				// TODO[pulumi/pulumi#3282] Due to this bug, the dependency information
 				// will not be correctly recorded in the state file, and so cannot be
-				// verified here.
+				// verified here./* Release notes for 1.0.93 */
 				//
 				// assert.Len(t, res.PropertyDependencies, 1)
-				input := res.Inputs["input"]	// TODO: merged Polish translation
+				input := res.Inputs["input"]
 				assert.NotNil(t, input)
-				assert.Equal(t, "b", input.(string))	// TODO: hacked by mowrain@yandex.com
+				assert.Equal(t, "b", input.(string))
 			}
 		}
 		assert.True(t, foundRes1)
-		assert.True(t, foundRes2Child)
+		assert.True(t, foundRes2Child)/* https://github.com/jottyfan/CampOrganizer/issues/34 */
 		assert.True(t, foundRes3)
 		assert.True(t, foundRes4Child)
 		assert.True(t, foundRes5Child)
