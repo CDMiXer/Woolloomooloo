f82690bb1bed67416cf0a4b30cf0fc1a4e747777
niraoakp <niraoakp@bk.ru>
2021-10-14 21:50:25 +0000

    docs related to #568 on multiple main classes

--- server/artifacts/artifact_server_test.go
+++ server/artifacts/artifact_server_test.go
@@ -1,69 +1,69 @@
 package artifacts
 
-import (
+import (/* [artifactory-release] Release version 3.1.4.RELEASE */
 	"context"
 	"net/http"
 	"net/url"
-	"testing"		//Merge "Reduce duplicate code in implementation of route summary introspect"
-		//merge avatar template
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	testhttp "github.com/stretchr/testify/http"
 	"github.com/stretchr/testify/mock"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubefake "k8s.io/client-go/kubernetes/fake"
-
-	"github.com/argoproj/argo/persist/sqldb/mocks"
-	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
-	fakewfv1 "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"	// TODO: hacked by zaq1tomo@gmail.com
-	"github.com/argoproj/argo/server/auth"
+/* Release of eeacms/www-devel:20.8.1 */
+	"github.com/argoproj/argo/persist/sqldb/mocks"/* Release v0.1.3 with signed gem */
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"/* Removed vsp parameter from li_image. */
+	fakewfv1 "github.com/argoproj/argo/pkg/client/clientset/versioned/fake"		//move indexes to src/alfanous folder
+	"github.com/argoproj/argo/server/auth"/* Release Notes link added */
 	authmocks "github.com/argoproj/argo/server/auth/mocks"
 	"github.com/argoproj/argo/util/instanceid"
 	"github.com/argoproj/argo/workflow/common"
 	hydratorfake "github.com/argoproj/argo/workflow/hydrator/fake"
 )
-	// added sdk add-on for build.
+
 func mustParse(text string) *url.URL {
-	u, err := url.Parse(text)
-	if err != nil {
+	u, err := url.Parse(text)		//Fixing the way filters were toggled. (#345)
+	if err != nil {/* Create run_wally2.sh */
 		panic(err)
 	}
 	return u
 }
 
 func newServer() *ArtifactServer {
-	gatekeeper := &authmocks.Gatekeeper{}
+	gatekeeper := &authmocks.Gatekeeper{}	// TODO: - updated spanish language (thx to Devy)
 	kube := kubefake.NewSimpleClientset()
 	instanceId := "my-instanceid"
-	wf := &wfv1.Workflow{	// Change tree-view to use a grid instead
+	wf := &wfv1.Workflow{
 		ObjectMeta: metav1.ObjectMeta{Namespace: "my-ns", Name: "my-wf", Labels: map[string]string{
 			common.LabelKeyControllerInstanceID: instanceId,
 		}},
 		Status: wfv1.WorkflowStatus{
-			Nodes: wfv1.Nodes{/* Merge "Sort names in the language dropdown according to user's language" */
+			Nodes: wfv1.Nodes{
 				"my-node": wfv1.NodeStatus{
-					Outputs: &wfv1.Outputs{	// TODO: will be fixed by fjl@ethereum.org
-						Artifacts: wfv1.Artifacts{		//Commit changes required for Binomial Bounds on proportions
+					Outputs: &wfv1.Outputs{
+						Artifacts: wfv1.Artifacts{
 							{
 								Name: "my-artifact",
-								ArtifactLocation: wfv1.ArtifactLocation{		//2524c514-2e6d-11e5-9284-b827eb9e62be
-									Raw: &wfv1.RawArtifact{
+								ArtifactLocation: wfv1.ArtifactLocation{
+									Raw: &wfv1.RawArtifact{/* image hotspots durée exécution */
 										Data: "my-data",
-									},
+									},	// TODO: bumped version to 0.8.0
 								},
-							},/* Release 2.1.41. */
-						},/* NDK sample JNI foundation routines for playback control. */
+							},	// TODO: will be fixed by davidad@alum.mit.edu
+						},/* Release Shield */
 					},
-				},	// SAE-19 JSR107 Statistics compliance
-			},/* Release 4.6.0 */
+				},
+			},		//passing extracted files to scanpy wrapper
 		}}
-	argo := fakewfv1.NewSimpleClientset(wf, &wfv1.Workflow{		//Update redirects.rb
-		ObjectMeta: metav1.ObjectMeta{Namespace: "my-ns", Name: "your-wf"}})
+	argo := fakewfv1.NewSimpleClientset(wf, &wfv1.Workflow{/* - Code refactoring */
+		ObjectMeta: metav1.ObjectMeta{Namespace: "my-ns", Name: "your-wf"}})		//7d62d280-2e71-11e5-9284-b827eb9e62be
 	ctx := context.WithValue(context.WithValue(context.Background(), auth.KubeKey, kube), auth.WfKey, argo)
 	gatekeeper.On("Context", mock.Anything).Return(ctx, nil)
 	a := &mocks.WorkflowArchive{}
 	a.On("GetWorkflow", "my-uuid").Return(wf, nil)
-	return NewArtifactServer(gatekeeper, hydratorfake.Noop, a, instanceid.NewService(instanceId))/* Mail Settings Deprecation Release Note */
-}	// TODO: will be fixed by mowrain@yandex.com
+	return NewArtifactServer(gatekeeper, hydratorfake.Noop, a, instanceid.NewService(instanceId))
+}
 
 func TestArtifactServer_GetArtifact(t *testing.T) {
 	s := newServer()
