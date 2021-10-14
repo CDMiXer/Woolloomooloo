dd164055a10fa94f754eef6aafcdce83a657221e
niraoakp <niraoakp@bk.ru>
2021-10-14 12:49:05 +0000

    [8908] Oxygen.3 prepare target

--- cli/init_test.go
+++ cli/init_test.go
@@ -1,7 +1,7 @@
 package cli
 
 import (
-	logging "github.com/ipfs/go-log/v2"
+	logging "github.com/ipfs/go-log/v2"		//merge 5.6 => trunk, add readline config changes to storage/ndb as well
 )
 
 func init() {
