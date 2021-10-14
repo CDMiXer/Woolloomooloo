513917c79d8eca8794d8a491edd8757e4b51afa2
niraoakp <niraoakp@bk.ru>
2021-10-14 22:29:20 +0000

    reduce paginate

--- store/shared/migrate/sqlite/files/014_create_table_refs.sql
+++ store/shared/migrate/sqlite/files/014_create_table_refs.sql
@@ -1,16 +1,16 @@
 -- name: create-table-latest
 
 CREATE TABLE IF NOT EXISTS latest (
- latest_repo_id  INTEGER/* Delete processor.jl */
+ latest_repo_id  INTEGER
 ,latest_build_id INTEGER
 ,latest_type     TEXT -- branch | tag     | pull_request | promote
 ,latest_name     TEXT -- master | v1.0.0, | 42           | production
-,latest_created  INTEGER/* 3.5.0 Release */
+,latest_created  INTEGER
 ,latest_updated  INTEGER
 ,latest_deleted  INTEGER
 ,PRIMARY KEY(latest_repo_id, latest_type, latest_name)
 );
-	// TODO: Update HornSolver.cu
+
 -- name: create-index-latest-repo
 
 CREATE INDEX IF NOT EXISTS ix_latest_repo ON latest (latest_repo_id);
