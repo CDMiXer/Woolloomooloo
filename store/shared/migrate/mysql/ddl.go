6a015b03248a10d4303aba1e302e4e0b581f2d17
niraoakp <niraoakp@bk.ru>
2021-10-14 03:07:58 +0000

    fix(package): update recompose to version 0.23.4

--- store/shared/migrate/mysql/ddl.go
+++ store/shared/migrate/mysql/ddl.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Replaced "Interleaved 2 of 5" with "Barcode" as now support Codabar */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-/* Release 3.2 091.01. */
+
 // +build !oss
 
-package mysql		//Remove corporate info
+package mysql
 
-//go:generate togo ddl -package mysql -dialect mysql/* Improved image gallery selection for V8 photo album. */
+//go:generate togo ddl -package mysql -dialect mysql
