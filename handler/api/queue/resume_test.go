6c09b92177ab27d8ae31da7f0ffd0cc759b2d730
niraoakp <niraoakp@bk.ru>
2021-10-14 13:14:54 +0000

    Update Servicetemplate.php

--- handler/api/queue/resume_test.go
+++ handler/api/queue/resume_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//a0185eb2-2e44-11e5-9284-b827eb9e62be
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 // +build !oss
-
+/* Generate debug information for Release builds. */
 package queue
