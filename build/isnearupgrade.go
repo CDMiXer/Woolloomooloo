f7a0b2e1c203d669a17e8a55c03e063569dc8a9e
niraoakp <niraoakp@bk.ru>
2021-10-14 03:57:29 +0000

    Merge "BUG-582: expose QNameModule"

--- build/isnearupgrade.go
+++ build/isnearupgrade.go
@@ -1,9 +1,9 @@
 package build
 
 import (
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"/* Merge "[INTERNAL] PDFViewer - Disable rendering on headless browsers" */
 )
 
-func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {
+func IsNearUpgrade(epoch, upgradeEpoch abi.ChainEpoch) bool {/* Update and rename RunRuleEngine.java to RulesEngineRunner.java */
 	return epoch > upgradeEpoch-Finality && epoch < upgradeEpoch+Finality
 }
