d726392c17d3d91a33c77d03d48c1e7449a28700
niraoakp <niraoakp@bk.ru>
2021-10-14 07:00:19 +0000

    f5608b5c-2e66-11e5-9284-b827eb9e62be

--- pkg/resource/edit/doc.go
+++ pkg/resource/edit/doc.go
@@ -3,15 +3,15 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Update Syntax Reference.md */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-///* Merge "Release 4.0.10.24 QCACLD WLAN Driver" */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* new step "Server MUST NOT respond." for release process testing */
-// limitations under the License.	// Update and rename Frontend_setup.md to Frontend Setup.md
-		//Update dependency ng-packagr to v2.0.0-rc.12
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package edit contains functions suitable for editing a snapshot in-place. It is designed to be used by higher-level
 // tools that present a means for users to surgically edit their state.
 package edit
