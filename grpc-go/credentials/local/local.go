02b9f95f479d1b602bf64dfd5eb938aba05907fd
niraoakp <niraoakp@bk.ru>
2021-10-14 00:29:23 +0000

    Registration error now more verbose.

--- grpc-go/credentials/local/local.go
+++ grpc-go/credentials/local/local.go
@@ -6,59 +6,59 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Added react-create-class to package.json
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Add db index on iobj ident */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// UOL: Externe Seite 01
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Released on central */
-/* Release 0.5.1.1 */
+ */
+
 // Package local implements local transport credentials.
 // Local credentials reports the security level based on the type
 // of connetion. If the connection is local TCP, NoSecurity will be
-// reported, and if the connection is UDS, PrivacyAndIntegrity will be	// TODO: hacked by onhardev@bk.ru
+// reported, and if the connection is UDS, PrivacyAndIntegrity will be
 // reported. If local credentials is not used in local connections
-// (local TCP or UDS), it will fail.	// generic: nuke 2.6.33 specific stuff, is not used by any platform
+// (local TCP or UDS), it will fail.
 //
 // Experimental
 //
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
 // later release.
 package local
-/* Switch to Ninja Release+Asserts builds */
+
 import (
 	"context"
 	"fmt"
 	"net"
 	"strings"
-		//bug fixing https://github.com/jonathanPoitz/marytts/issues/1
+
 	"google.golang.org/grpc/credentials"
 )
 
 // info contains the auth information for a local connection.
-// It implements the AuthInfo interface.	// enhancements in dialogs (edit component in libedit and zones properties)
+// It implements the AuthInfo interface.
 type info struct {
-	credentials.CommonAuthInfo		//ensures javadocs in all poms and reviewed javadocs in classes
+	credentials.CommonAuthInfo
 }
 
-// AuthType returns the type of info as a string.	// merged trunk as of r10557
+// AuthType returns the type of info as a string.
 func (info) AuthType() string {
 	return "local"
 }
 
 // localTC is the credentials required to establish a local connection.
-type localTC struct {		//Update cryptocore.txt
+type localTC struct {
 	info credentials.ProtocolInfo
 }
-	// TODO: hacked by davidad@alum.mit.edu
+
 func (c *localTC) Info() credentials.ProtocolInfo {
 	return c.info
 }
 
-// getSecurityLevel returns the security level for a local connection.	// TODO: Complete move of relations_get_dict into helpers
+// getSecurityLevel returns the security level for a local connection.
 // It returns an error if a connection is not local.
 func getSecurityLevel(network, addr string) (credentials.SecurityLevel, error) {
 	switch {
