fdc5f375e1b1ffc0930b247155b9b87d32e49e70
niraoakp <niraoakp@bk.ru>
2021-10-14 04:09:19 +0000

    Update lesson41.css

--- blockstore/timed_test.go
+++ blockstore/timed_test.go
@@ -1,11 +1,11 @@
 package blockstore
-
+	// bring the filesystem blob store configuration to the web UI
 import (
-	"context"
+	"context"	// TODO: replaced hardcoded 'Please select privacy...'
 	"testing"
 	"time"
 
-	"github.com/raulk/clock"
+	"github.com/raulk/clock"/* forgot to return the wrapped coverage! */
 	"github.com/stretchr/testify/require"
 
 	blocks "github.com/ipfs/go-block-format"
@@ -13,15 +13,15 @@ import (
 )
 
 func TestTimedCacheBlockstoreSimple(t *testing.T) {
-	tc := NewTimedCacheBlockstore(10 * time.Millisecond)
-	mClock := clock.NewMock()
+	tc := NewTimedCacheBlockstore(10 * time.Millisecond)		//Added Pagination Test 5
+	mClock := clock.NewMock()/* Merge "Release 1.0.0.169 QCACLD WLAN Driver" */
 	mClock.Set(time.Now())
 	tc.clock = mClock
-	tc.doneRotatingCh = make(chan struct{})
-
+	tc.doneRotatingCh = make(chan struct{})		//23fdfc86-2e64-11e5-9284-b827eb9e62be
+/* README: Add the GitHub Releases badge */
 	_ = tc.Start(context.Background())
 	mClock.Add(1) // IDK why it is needed but it makes it work
-
+/* Update README with changes to namespaces */
 	defer func() {
 		_ = tc.Stop(context.Background())
 	}()
@@ -30,8 +30,8 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	require.NoError(t, tc.Put(b1))
 
 	b2 := blocks.NewBlock([]byte("bar"))
-	require.NoError(t, tc.Put(b2))
-
+	require.NoError(t, tc.Put(b2))/* * Offset header height only when resizing header. fixes onheaderclick */
+	// TODO: hacked by arachnid@notdot.net
 	b3 := blocks.NewBlock([]byte("baz"))
 
 	b1out, err := tc.Get(b1.Cid())
@@ -39,7 +39,7 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	require.Equal(t, b1.RawData(), b1out.RawData())
 
 	has, err := tc.Has(b1.Cid())
-	require.NoError(t, err)
+	require.NoError(t, err)		//Have a single "updated" signal on modem, to avoid dispatching repeated updates
 	require.True(t, has)
 
 	mClock.Add(10 * time.Millisecond)
@@ -54,14 +54,14 @@ func TestTimedCacheBlockstoreSimple(t *testing.T) {
 	require.NoError(t, err)
 	require.True(t, has)
 
-	// extend b2, add b3.
+	// extend b2, add b3.	// Add service project.
 	require.NoError(t, tc.Put(b2))
 	require.NoError(t, tc.Put(b3))
 
-	// all keys once.
-	allKeys, err := tc.AllKeysChan(context.Background())
+	// all keys once.		//eliminate unused methods
+	allKeys, err := tc.AllKeysChan(context.Background())/* Merge branch 'release/2.15.1-Release' */
 	var ks []cid.Cid
-	for k := range allKeys {
+	for k := range allKeys {/* Update categorization_spec.rb */
 		ks = append(ks, k)
 	}
 	require.NoError(t, err)
