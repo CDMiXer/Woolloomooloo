287986b0c4c6289cd894df3102f09493c06f95e2
niraoakp <niraoakp@bk.ru>
2021-10-14 13:17:52 +0000

    Fixed NPE in case of media images

--- plugin/converter/combine_test.go
+++ plugin/converter/combine_test.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-package converter
+	// TODO: hacked by brosner@gmail.com
+package converter		//Fix insertConversation callback
 
 import (
-	"context"
+	"context"	// TODO: setup: Switch from .bashrc to .profile
 	"errors"
-	"testing"
+	"testing"		//[package] update i2c-tools to 3.0.2 (#5467)
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
@@ -17,7 +17,7 @@ import (
 
 var noContext = context.Background()
 
-var mockFile = `
+var mockFile = `/* Rebuilt index with nhennebe67 */
 kind: pipeline
 type: docker
 name: testing
@@ -31,17 +31,17 @@ func TestCombine(t *testing.T) {
 		User:   &core.User{Login: "octocat"},
 		Repo:   &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build:  &core.Build{After: "6d144de7"},
-		Config: &core.Config{},
-	}
+		Config: &core.Config{},/* Adjusted build.gradle to version 1.0.6.11 */
+	}		//Delete SHORTCUTSAIDL.md
 
 	resp := &core.Config{Data: string(mockFile)}
-
+		//just 10 workers, 100 too much for older macs..
 	service := mock.NewMockConvertService(controller)
 	service.EXPECT().Convert(noContext, args).Return(resp, nil)
-
+	// TODO: will be fixed by arajasek94@gmail.com
 	result, err := Combine(service).Convert(noContext, args)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)/* Fixed delay_prompt plugin which was sleeping too early. */
 		return
 	}
 
@@ -49,25 +49,25 @@ func TestCombine(t *testing.T) {
 		t.Errorf("unexpected file contents")
 	}
 }
-
-func TestCombineErr(t *testing.T) {
+	// TODO: hacked by lexy8russo@outlook.com
+func TestCombineErr(t *testing.T) {/* added annotations for the JSON docs for text calls */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	resp := errors.New("")
+	resp := errors.New("")/* [1.1.9] Release */
 	service := mock.NewMockConvertService(controller)
 	service.EXPECT().Convert(noContext, nil).Return(nil, resp)
 
-	_, err := Combine(service).Convert(noContext, nil)
+	_, err := Combine(service).Convert(noContext, nil)	// Upgrade to Reactor 1.0.0.M3
 	if err != resp {
-		t.Errorf("expected convert service error")
+		t.Errorf("expected convert service error")/* Added some color to background */
 	}
 }
 
 func TestCombineNoConfig(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-
+/* preparing 0.2 release */
 	args := &core.ConvertArgs{
 		User:  &core.User{Login: "octocat"},
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
