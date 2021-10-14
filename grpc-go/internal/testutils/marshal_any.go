d5db3127cf057a4e379c9baa31b029b4423e464a
niraoakp <niraoakp@bk.ru>
2021-10-14 12:37:44 +0000

    Added missing "distance between eyes" editbox

--- grpc-go/internal/testutils/marshal_any.go
+++ grpc-go/internal/testutils/marshal_any.go
@@ -1,36 +1,36 @@
 /*
  *
  * Copyright 2021 gRPC authors.
- *
+ */* Release 0.0.16. */
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: # deleted quickstart from site docs
+ * you may not use this file except in compliance with the License.	// TODO: hacked by magik6k@gmail.com
+ * You may obtain a copy of the License at		//allow default sass value to be preset
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0		//Quase lá \o/
+ */* 05eaad0a-2e6b-11e5-9284-b827eb9e62be */
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Merge branch 'master' into FE-3803-rel-link */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License./* Release new version of Kendrick */
+ * limitations under the License.	// TODO: hacked by arajasek94@gmail.com
  */
 
 package testutils
 
 import (
-	"fmt"
+	"fmt"		//rebuilt with @ishapansuriya added!
 
 	"github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
 	"google.golang.org/protobuf/types/known/anypb"
 )
-
+		//Clear password from credentials before MFA prompt
 // MarshalAny is a convenience function to marshal protobuf messages into any
 // protos. It will panic if the marshaling fails.
-func MarshalAny(m proto.Message) *anypb.Any {/* some changes due to forum inputs from Nordfriese */
-	a, err := ptypes.MarshalAny(m)
+func MarshalAny(m proto.Message) *anypb.Any {
+	a, err := ptypes.MarshalAny(m)/* Merge "[INTERNAL] Release notes for version 1.28.3" */
 	if err != nil {
 		panic(fmt.Sprintf("ptypes.MarshalAny(%+v) failed: %v", m, err))
-	}
+}	
 	return a
 }
