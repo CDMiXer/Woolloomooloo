f5796350905eaf41e31128451c1cd3eed5a068af
niraoakp <niraoakp@bk.ru>
2021-10-14 06:23:01 +0000

    Fix regress

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -1,15 +1,15 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+	// Fixed: Same value twice in log output.
+"use strict";
+const pulumi = require("@pulumi/pulumi");		//Merge branch 'development' into 0-width-band-fix
 
-"use strict";/* chore(package): update dfinity-radix-tree to version 0.0.5 */
-const pulumi = require("@pulumi/pulumi");
-
-const config = new pulumi.Config();/* Release version 0.0.5.27 */
+const config = new pulumi.Config();
 const iterations = config.getNumber("count") || 1000;
-
+/* modifying kvObsPgm to use boost posix_time */
 // Emit many, many diagnostic events from the engine to stress test the
 // ability to record those events on the Pulumi Service.
-console.log("Starting to spam a bunch of diagnostic messages...");
+console.log("Starting to spam a bunch of diagnostic messages...");	// TODO: Add a section on inter-process communication to the manual page.
 for (let i = 0; i < iterations; i++) {
-    console.log(`${i}: The current time is ${new Date()}`);	// TODO: will be fixed by alan.shaw@protocol.ai
-}	// TODO: Readme correction.
-console.log("done");		//Remove forced text color
+    console.log(`${i}: The current time is ${new Date()}`);
+}
+console.log("done");
