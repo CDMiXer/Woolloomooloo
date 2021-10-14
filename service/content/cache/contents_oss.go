563768ad4be89c4a5c2a285455c5cd478a7ce342
niraoakp <niraoakp@bk.ru>
2021-10-14 03:03:38 +0000

    [tpm2] nits

--- service/content/cache/contents_oss.go
+++ service/content/cache/contents_oss.go
@@ -7,8 +7,8 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.109 */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Updated the list of supported features in the README.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
