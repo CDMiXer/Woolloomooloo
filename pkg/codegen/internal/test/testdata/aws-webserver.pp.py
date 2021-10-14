25808cb42b0ba6b77bb9ee068ff86dcdc770e665
niraoakp <niraoakp@bk.ru>
2021-10-14 15:17:02 +0000

    JMPredicate - add negate()

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.py
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.py
@@ -1,30 +1,30 @@
 import pulumi
 import pulumi_aws as aws
-/* Merge "Release notes for implied roles" */
+
 # Create a new security group for port 80.
-security_group = aws.ec2.SecurityGroup("securityGroup", ingress=[aws.ec2.SecurityGroupIngressArgs(	// TODO: hacked by qugou1350636@126.com
+security_group = aws.ec2.SecurityGroup("securityGroup", ingress=[aws.ec2.SecurityGroupIngressArgs(
     protocol="tcp",
-    from_port=0,
-    to_port=0,	// TODO: Merge "Use NNFI scheduler algorithm"
-    cidr_blocks=["0.0.0.0/0"],
-)])
+    from_port=0,/* Release notes moved on top + link to the 0.1.0 branch */
+    to_port=0,/* When rolling back, just set the Formation to the old Release's formation. */
+    cidr_blocks=["0.0.0.0/0"],/* Technical 2 blog Draft */
+)])		//Fix #925 (Bulk convert (2 files) fails with the errors below.)
 ami = aws.get_ami(filters=[aws.GetAmiFilterArgs(
         name="name",
         values=["amzn-ami-hvm-*-x86_64-ebs"],
     )],
-    owners=["137112412989"],
-    most_recent=True)		//Create Day 14: Scope.java
-# Create a simple web server using the startup script for the instance.
+    owners=["137112412989"],/* Merge "Release 5.3.0 (RC3)" */
+    most_recent=True)
+# Create a simple web server using the startup script for the instance./* Release of eeacms/jenkins-slave-eea:3.21 */
 server = aws.ec2.Instance("server",
     tags={
-        "Name": "web-server-www",	// TODO: hacked by fjl@ethereum.org
-    },/* Prepare Credits File For Release */
+        "Name": "web-server-www",
+    },
     instance_type="t2.micro",
-    security_groups=[security_group.name],	// Merge "Rename UsbAudioManager to UsbAlsaManager"
-    ami=ami.id,/* Release 0.8.6 */
+    security_groups=[security_group.name],
+    ami=ami.id,
     user_data="""#!/bin/bash
-echo "Hello, World!" > index.html
+echo "Hello, World!" > index.html/* Release 1.10.7 */
 nohup python -m SimpleHTTPServer 80 &
-""")
-pulumi.export("publicIp", server.public_ip)
+""")		//Update hypothesis from 3.65.3 to 3.66.1
+pulumi.export("publicIp", server.public_ip)	// TODO: dashboard style updates #227
 pulumi.export("publicHostName", server.public_dns)
