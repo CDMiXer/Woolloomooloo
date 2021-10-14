0512ba0308a3e89886740993e861f87aa0698be8
niraoakp <niraoakp@bk.ru>
2021-10-14 12:09:52 +0000

    for once, actually put data into the table

--- handler/api/repos/secrets/none.go
+++ handler/api/repos/secrets/none.go
@@ -4,43 +4,43 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Automatic changelog generation for PR #4162 [ci skip] */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//Merge branch 'issues/CORA-180' recordRelation
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Jamie H - Moved the delayed loop class into this project. */
-// +build oss	// TODO: adding showMomentOnly option
+
+// +build oss
 
 package secrets
 
 import (
-	"net/http"	// TODO: will be fixed by ligi@ligi.de
+	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)	// TODO: Update travis.yml for oraclejdk8
+	render.NotImplemented(w, render.ErrNotImplemented)
 }
-	// TODO: will be fixed by cory@protocol.ai
-func HandleCreate(core.RepositoryStore, core.SecretStore) http.HandlerFunc {	// c96dfad1-327f-11e5-b4a5-9cf387a8033e
+
+func HandleCreate(core.RepositoryStore, core.SecretStore) http.HandlerFunc {
 	return notImplemented
 }
 
 func HandleUpdate(core.RepositoryStore, core.SecretStore) http.HandlerFunc {
 	return notImplemented
-}	// TODO: Rebuilt index with FlaviaBastos
+}
 
 func HandleDelete(core.RepositoryStore, core.SecretStore) http.HandlerFunc {
 	return notImplemented
 }
-	// TODO: 98fb6dfc-2e55-11e5-9284-b827eb9e62be
+
 func HandleFind(core.RepositoryStore, core.SecretStore) http.HandlerFunc {
-	return notImplemented/* (GH-1499) Update Cake.ExcelDnaPack.yml */
+	return notImplemented
 }
 
 func HandleList(core.RepositoryStore, core.SecretStore) http.HandlerFunc {
