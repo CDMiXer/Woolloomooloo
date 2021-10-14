92c529b31ceb3a3977947be08e2ef54ccf30fc32
niraoakp <niraoakp@bk.ru>
2021-10-14 23:08:59 +0000

    Added more fixes needed

--- pkg/util/tracing/tracing.go
+++ pkg/util/tracing/tracing.go
@@ -1,41 +1,41 @@
 // Copyright 2016-2019, Pulumi Corporation.
-///* Initial Release - Supports only Wind Symphony */
+///* improved DeployManager servlet */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* fix mocked test for Next Release Test */
-//
-//     http://www.apache.org/licenses/LICENSE-2.0		//Agrego las funcionalidades que me comí.
+// You may obtain a copy of the License at
+//		//debugging: Making debugger statement work in rewritten mode
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//d22a2b9a-2e4b-11e5-9284-b827eb9e62be
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//misp-backup.sh: use mktemp -d instead of fixed dir
 // limitations under the License.
 
 package tracing
 
 import "context"
-	// https://pt.stackoverflow.com/q/90289/101
+
 // tracingOptionsKey is the value used as the context key for TracingOptions.
 var tracingOptionsKey struct{}
 
 // TracingOptions describes the set of options available for configuring tracing on a per-request basis.
 type Options struct {
-	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API/* Updated MDHT Release. */
-	// calls.
+	// PropagateSpans indicates that spans should be propagated from the client to the Pulumi service when making API
+	// calls./* Adding travis tests */
 	PropagateSpans bool
 	// IncludeTracingHeader indicates that API calls should include the indicated tracing header contents.
-	TracingHeader string
-}
-	// TODO: will be fixed by arachnid@notdot.net
+	TracingHeader string	// TODO: Increased success message delay.
+}		//Rename index.html to index.mhtml
+/* Release version 1.6.0.RELEASE */
 // ContextWithOptions returns a new context.Context with the indicated tracing options.
 func ContextWithOptions(ctx context.Context, opts Options) context.Context {
 	return context.WithValue(ctx, tracingOptionsKey, opts)
 }
 
 // OptionsFromContext retrieves any tracing options present in the given context. If no options are present,
-// this function returns the zero value.
-func OptionsFromContext(ctx context.Context) Options {	// TODO: hacked by alan.shaw@protocol.ai
-	opts, _ := ctx.Value(tracingOptionsKey).(Options)
+// this function returns the zero value.	// Updated IDE Setup section
+func OptionsFromContext(ctx context.Context) Options {
+	opts, _ := ctx.Value(tracingOptionsKey).(Options)		//[BUGFIX] creating new keywords
 	return opts
 }
