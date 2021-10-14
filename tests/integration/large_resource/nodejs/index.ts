9fcb8324d1b2401ffcad345d20a056079ce68a0d
niraoakp <niraoakp@bk.ru>
2021-10-14 12:01:05 +0000

    Merge branch 'develop' into fix/localization

--- tests/integration/large_resource/nodejs/index.ts
+++ tests/integration/large_resource/nodejs/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Prepare for release of eeacms/plonesaas:5.2.1-28
 
 // Create and export a very long string (>4mb)
 export const longString = "a".repeat(5 * 1024 * 1024);
