bf33306ea2be22514b9df0c7b2804491a5e0c4a6
niraoakp <niraoakp@bk.ru>
2021-10-14 09:55:26 +0000

    Uniform section titles

--- tests/integration/empty/dotnet/Program.cs
+++ tests/integration/empty/dotnet/Program.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-	// TODO: optimization for "toString()"
-using System.Threading.Tasks;/* Merge "Fix order of arguments in assertEqual - Part3" */
+
+using System.Threading.Tasks;/* Updates CHANGELOG for 0.7.0. */
 using Pulumi;
 
 class Program
-{
+{/* Release references and close executor after build */
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(() => {});
