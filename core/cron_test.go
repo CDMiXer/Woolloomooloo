1f6998abc998fa08377f540bc8e48a4d937aaa4c
niraoakp <niraoakp@bk.ru>
2021-10-14 11:01:39 +0000

    MySQL Queries

--- core/cron_test.go
+++ core/cron_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License		//Delete Editing Resolvendo Problemas Comuns 2 bad interpreter
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release 0.0.5 closes #1 and #2 */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Added module photos */
-
-package core
+// +build !oss
+		//tweak loader animation
+package core	// TODO: will be fixed by nagydani@epointsystem.org
