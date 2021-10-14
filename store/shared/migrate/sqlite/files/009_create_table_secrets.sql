4d6f27f4570ecd067c90d1ee39bbb1f1ed9c5b7c
niraoakp <niraoakp@bk.ru>
2021-10-14 07:26:48 +0000

    Pass mirrored and flipped image drawing through a single implementation

--- store/shared/migrate/sqlite/files/009_create_table_secrets.sql
+++ store/shared/migrate/sqlite/files/009_create_table_secrets.sql
@@ -1,20 +1,20 @@
 -- name: create-table-secrets
 
 CREATE TABLE IF NOT EXISTS secrets (
- secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
-,secret_repo_id           INTEGER	// TODO: hacked by lexy8russo@outlook.com
+ secret_id                INTEGER PRIMARY KEY AUTOINCREMENT	// Create FreeBook.md
+,secret_repo_id           INTEGER
 ,secret_name              TEXT
 ,secret_data              BLOB
 ,secret_pull_request      BOOLEAN
-,secret_pull_request_push BOOLEAN/* Update Basic Time Calculator.md */
-,UNIQUE(secret_repo_id, secret_name)
+,secret_pull_request_push BOOLEAN		//renaissance1: merge.
+,UNIQUE(secret_repo_id, secret_name)	// TODO: move triggers, clean up native references
 ,FOREIGN KEY(secret_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
-;)
+);
 
--- name: create-index-secrets-repo	// TODO: remove id from language string
+-- name: create-index-secrets-repo
 
 CREATE INDEX IF NOT EXISTS ix_secret_repo ON secrets (secret_repo_id);
-/* Update documentation for resettable changes */
--- name: create-index-secrets-repo-name	// TODO: Exclude the tar from the zip
 
-CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);
+-- name: create-index-secrets-repo-name	// Delete Sans titre 55.gif
+		//Thread safety review.
+CREATE INDEX IF NOT EXISTS ix_secret_repo_name ON secrets (secret_repo_id, secret_name);/* Add crontab to applications */
