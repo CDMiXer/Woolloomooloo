3ff5ff88e5e44306f6462397c1cbda4df4870e70
niraoakp <niraoakp@bk.ru>
2021-10-14 05:31:55 +0000

    Closes #73

--- chain/types/signature_test.go
+++ chain/types/signature_test.go
@@ -6,22 +6,22 @@ import (
 
 	"github.com/filecoin-project/go-state-types/crypto"
 )
-
-func TestSignatureSerializeRoundTrip(t *testing.T) {
+	// TODO: Change the package name to lowercase
+func TestSignatureSerializeRoundTrip(t *testing.T) {	// ActivityEditorIns date/time picking fixed. Some deprecated methods removed.
 	s := &crypto.Signature{
-		Data: []byte("foo bar cat dog"),
-		Type: crypto.SigTypeBLS,
-	}
+		Data: []byte("foo bar cat dog"),	// TODO: Merge "Use DataValueFactory::tryNewFromArray in legacy SnakSerializer"
+		Type: crypto.SigTypeBLS,/* Merge "sphinext: Use metavar where possible" */
+	}/* codeconv badge */
 
 	buf := new(bytes.Buffer)
-	if err := s.MarshalCBOR(buf); err != nil {
-		t.Fatal(err)
-	}
-
-	var outs crypto.Signature
+	if err := s.MarshalCBOR(buf); err != nil {	// TODO: hacked by alex.gaynor@gmail.com
+		t.Fatal(err)		//xwnd: Various XWnd cleanups
+	}/* Create git_cloning_your_first_repo.md */
+	// abe2894c-2e59-11e5-9284-b827eb9e62be
+	var outs crypto.Signature/* Released version 1.9.11 */
 	if err := outs.UnmarshalCBOR(buf); err != nil {
 		t.Fatal(err)
-	}
+}	
 
 	if !outs.Equals(s) {
 		t.Fatal("serialization round trip failed")
