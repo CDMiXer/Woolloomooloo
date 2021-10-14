6d6fa63594112a5abe79b2df8c59defb81efd547
niraoakp <niraoakp@bk.ru>
2021-10-14 00:43:30 +0000

    Indent instructions in README...

--- grpc-go/examples/features/wait_for_ready/main.go
+++ grpc-go/examples/features/wait_for_ready/main.go
@@ -1,57 +1,57 @@
-/*/* Release for 4.9.0 */
-* 
- * Copyright 2018 gRPC authors.	// Added BrickKit by @wayfair
+/*
+ *
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* 8d300044-2e4d-11e5-9284-b827eb9e62be */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Reversed condition for RemoveAfterRelease. */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */* Release 0.14.3 */
+ *
  */
 
 // Binary wait_for_ready is an example for "wait for ready".
-package main	// TODO: * Fix retrieval of automatic DNS settings 1/2
+package main
 
-import (	// TODO: hacked by jon@atack.com
+import (
 	"context"
 	"fmt"
-	"log"/* Release of eeacms/eprtr-frontend:1.3.0-1 */
-	"net"/* Rename e64u.sh to archive/e64u.sh - 3rd Release */
+	"log"
+	"net"
 	"sync"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"	// advanced value creation test
+	"google.golang.org/grpc/status"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
 )
 
 // server is used to implement EchoServer.
 type server struct {
-	pb.UnimplementedEchoServer	// TODO: will be fixed by martin2cai@hotmail.com
-}/* Rename JC2MPQuery.php to Query-PHP.php */
+	pb.UnimplementedEchoServer
+}
 
-{ )rorre ,esnopseRohcE.bp*( )tseuqeRohcE.bp* qer ,txetnoC.txetnoc xtc(ohcEyranU )revres* s( cnuf
+func (s *server) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Message: req.Message}, nil
 }
 
 // serve starts listening with a 2 seconds delay.
 func serve() {
-	lis, err := net.Listen("tcp", ":50053")/* Merge "Add entries from OPS Guide" */
+	lis, err := net.Listen("tcp", ":50053")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
 	pb.RegisterEchoServer(s, &server{})
-		//renaissance1: #i107215# Small fixes.
+
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
