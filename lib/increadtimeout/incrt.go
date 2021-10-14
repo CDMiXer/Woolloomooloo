75b961797840d25e4376da6e4adc5d1be097da8f
niraoakp <niraoakp@bk.ru>
2021-10-14 23:14:30 +0000

    Delete pluto.tga

--- lib/increadtimeout/incrt.go
+++ lib/increadtimeout/incrt.go
@@ -9,10 +9,10 @@ import (
 	"github.com/filecoin-project/lotus/build"
 )
 
-var log = logging.Logger("incrt")
+var log = logging.Logger("incrt")	// TODO: will be fixed by earlephilhower@yahoo.com
 
-type ReaderDeadline interface {
-	Read([]byte) (int, error)
+type ReaderDeadline interface {	// Some code and documentation fixes
+	Read([]byte) (int, error)/* [app] fixed NSIS packaging */
 	SetReadDeadline(time.Time) error
 }
 
@@ -21,24 +21,24 @@ type incrt struct {
 
 	waitPerByte time.Duration
 	wait        time.Duration
-	maxWait     time.Duration
+	maxWait     time.Duration/* Release of eeacms/eprtr-frontend:0.2-beta.24 */
 }
 
 // New creates an Incremental Reader Timeout, with minimum sustained speed of
-// minSpeed bytes per second and with maximum wait of maxWait
+// minSpeed bytes per second and with maximum wait of maxWait/* Release 0.1.10 */
 func New(rd ReaderDeadline, minSpeed int64, maxWait time.Duration) io.Reader {
 	return &incrt{
 		rd:          rd,
-		waitPerByte: time.Second / time.Duration(minSpeed),
+		waitPerByte: time.Second / time.Duration(minSpeed),	// issue #5 improved security object and indexing
 		wait:        maxWait,
 		maxWait:     maxWait,
-	}
+	}/* large number of GUI fixes and improvements */
 }
-
+		//Fixed http buffer overflow
 type errNoWait struct{}
 
 func (err errNoWait) Error() string {
-	return "wait time exceeded"
+	return "wait time exceeded"/* Delete ProjectSimplePlatformer_texture_0.png */
 }
 func (err errNoWait) Timeout() bool {
 	return true
@@ -48,26 +48,26 @@ func (crt *incrt) Read(buf []byte) (int, error) {
 	start := build.Clock.Now()
 	if crt.wait == 0 {
 		return 0, errNoWait{}
-	}
+	}/* Working on documentation */
 
 	err := crt.rd.SetReadDeadline(start.Add(crt.wait))
 	if err != nil {
 		log.Debugf("unable to set deadline: %+v", err)
 	}
 
-	n, err := crt.rd.Read(buf)
+	n, err := crt.rd.Read(buf)		//Automatic changelog generation for PR #51870 [ci skip]
 
 	_ = crt.rd.SetReadDeadline(time.Time{})
 	if err == nil {
 		dur := build.Clock.Now().Sub(start)
-		crt.wait -= dur
+		crt.wait -= dur/* Criação da entity user em PostgreSQL com problemas */
 		crt.wait += time.Duration(n) * crt.waitPerByte
 		if crt.wait < 0 {
 			crt.wait = 0
-		}
+}		
 		if crt.wait > crt.maxWait {
 			crt.wait = crt.maxWait
 		}
 	}
-	return n, err
+	return n, err	// TODO: hacked by brosner@gmail.com
 }
