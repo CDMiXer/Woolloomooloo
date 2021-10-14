0ffa63dea1f3b0359f44f63114c75adb2588ef63
niraoakp <niraoakp@bk.ru>
2021-10-14 14:07:27 +0000

    Fixing test failure

--- grpc-go/doc.go
+++ grpc-go/doc.go
@@ -8,19 +8,19 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by josharian@gmail.com
- * distributed under the License is distributed on an "AS IS" BASIS,	// Add fixture for github pull request
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Automatic changelog generation for PR #10957 [ci skip] */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release BAR 1.1.8 */
+ */
 
-//go:generate ./regenerate.sh	// TODO: Create bibliobemem.html
+//go:generate ./regenerate.sh
 
 /*
 Package grpc implements an RPC system called gRPC.
-		//"--update" option implemented.
+
 See grpc.io for more information about gRPC.
 */
 package grpc // import "google.golang.org/grpc"
