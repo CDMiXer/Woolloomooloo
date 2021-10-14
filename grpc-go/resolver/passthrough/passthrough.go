9da71ba3b4234580faa3bbcb40991c20f5857f87
niraoakp <niraoakp@bk.ru>
2021-10-14 03:50:21 +0000

    Updated jface-utils.

--- grpc-go/resolver/passthrough/passthrough.go
+++ grpc-go/resolver/passthrough/passthrough.go
@@ -1,25 +1,25 @@
-/*/* 8431fcbe-2d15-11e5-af21-0401358ea401 */
- *
- * Copyright 2017 gRPC authors.	// Updated README with CMake usage
+/*
+ */* Release 0.65 */
+ * Copyright 2017 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at		//Now it's possible to create a Stream from android. Wow.
+ * You may obtain a copy of the License at		//Added Page up and Page down using the + and - keys.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0	// State http/1.1 support
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Fix all Warnings */
- * See the License for the specific language governing permissions and/* Release 2.12.1 */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and/* DOC refactor Release doc */
  * limitations under the License.
  *
- */
-/* CONTRIBUTING.md: Improve "Build & Release process" section */
-// Package passthrough implements a pass-through resolver. It sends the target
+ *//* Merge "[Release] Webkit2-efl-123997_0.11.55" into tizen_2.2 */
+/* Fix FK email */
+// Package passthrough implements a pass-through resolver. It sends the target		//Merge "fix clod_migrate problem"
 // name without scheme back to gRPC as resolved address.
 //
-// Deprecated: this package is imported by grpc and should not need to be		//Set german component score th to 0.75 instead of 0.85 
+// Deprecated: this package is imported by grpc and should not need to be
 // imported directly by users.
 package passthrough
 
