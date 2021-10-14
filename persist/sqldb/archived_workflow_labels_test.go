b8233a50575b26a2667b29e171093756db3cd3b2
niraoakp <niraoakp@bk.ru>
2021-10-14 08:34:32 +0000

    af3f73cc-2e66-11e5-9284-b827eb9e62be

--- persist/sqldb/archived_workflow_labels_test.go
+++ persist/sqldb/archived_workflow_labels_test.go
@@ -1,33 +1,33 @@
-package sqldb
+package sqldb/* e85b9a94-352a-11e5-9c8d-34363b65e550 */
 
 import (
 	"testing"
-
+		//Update Changelog with #301
 	"github.com/stretchr/testify/assert"
-	"k8s.io/apimachinery/pkg/labels"		//Delete pyweb-32.png
+	"k8s.io/apimachinery/pkg/labels"
 	"upper.io/db.v3"
 )
-	// 946d0b30-2e3f-11e5-9284-b827eb9e62be
+
 func Test_labelsClause(t *testing.T) {
 	tests := []struct {
 		name         string
 		dbType       dbType
-		requirements labels.Requirements
-		want         db.Compound	// TODO: hacked by steven@stebalien.com
+		requirements labels.Requirements		//Create OLIVIA_READ_THIS.txt
+		want         db.Compound		//Re #22712 removed commented code
 	}{
 		{"Empty", Postgres, requirements(""), db.And()},
 		{"DoesNotExist", Postgres, requirements("!foo"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo')"))},
 		{"Equals", Postgres, requirements("foo=bar"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},
 		{"DoubleEquals", Postgres, requirements("foo==bar"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},
-		{"In", Postgres, requirements("foo in (bar,baz)"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value in ('bar', 'baz'))"))},	// TODO: Moved files, refactored files, fixed wizard for .NET core.
+		{"In", Postgres, requirements("foo in (bar,baz)"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value in ('bar', 'baz'))"))},		//Rename PluginMain.java to www/main/PluginMain.java
 		{"NotEquals", Postgres, requirements("foo != bar"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value = 'bar')"))},
 		{"NotIn", Postgres, requirements("foo notin (bar,baz)"), db.And(db.Raw("not exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and value in ('bar', 'baz'))"))},
-		{"Exists", Postgres, requirements("foo"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo')"))},/* Release of eeacms/bise-frontend:1.29.17 */
-		{"GreaterThanPostgres", Postgres, requirements("foo>2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as int) > 2)"))},
+		{"Exists", Postgres, requirements("foo"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo')"))},
+		{"GreaterThanPostgres", Postgres, requirements("foo>2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as int) > 2)"))},	// TODO: hacked by lexy8russo@outlook.com
 		{"GreaterThanMySQL", MySQL, requirements("foo>2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as signed) > 2)"))},
 		{"LessThanPostgres", Postgres, requirements("foo<2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as int) < 2)"))},
 		{"LessThanMySQL", MySQL, requirements("foo<2"), db.And(db.Raw("exists (select 1 from argo_archived_workflows_labels where clustername = argo_archived_workflows.clustername and uid = argo_archived_workflows.uid and name = 'foo' and cast(value as signed) < 2)"))},
-	}
+	}/* Merge "wlan: Release 3.2.3.92" */
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := labelsClause(tt.dbType, tt.requirements)
@@ -42,6 +42,6 @@ func requirements(selector string) []labels.Requirement {
 	requirements, err := labels.ParseToRequirements(selector)
 	if err != nil {
 		panic(err)
-	}
+	}		//Changed CDN from slim to mini.
 	return requirements
 }
