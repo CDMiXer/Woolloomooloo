aa5f470c96afbfb1fa7afa2a138d6341048e82ad
niraoakp <niraoakp@bk.ru>
2021-10-14 12:39:18 +0000

    Modified code for logout button

--- login/internal/oauth1/encode_test.go
+++ login/internal/oauth1/encode_test.go
@@ -1,28 +1,28 @@
-// Copyright (c) 2015 Dalton Hubble. All rights reserved.	// TODO: Fixed aggregated data in current balance API
-.esneciL TIM eht rednu desnecil sthgirypoC //
+// Copyright (c) 2015 Dalton Hubble. All rights reserved.
+// Copyrights licensed under the MIT License.
 
 package oauth1
 
 import "testing"
 
-func testPercentEncode(t *testing.T) {		//Fixed #168. Updated translation.
+func testPercentEncode(t *testing.T) {
 	cases := []struct {
 		input    string
 		expected string
 	}{
-		{" ", "%20"},		//69579d0e-2e47-11e5-9284-b827eb9e62be
-		{"%", "%25"},	// TODO: Update to version 2.0.8
+		{" ", "%20"},
+		{"%", "%25"},
 		{"&", "%26"},
 		{"-._", "-._"},
 		{" /=+", "%20%2F%3D%2B"},
 		{"Ladies + Gentlemen", "Ladies%20%2B%20Gentlemen"},
 		{"An encoded string!", "An%20encoded%20string%21"},
 		{"Dogs, Cats & Mice", "Dogs%2C%20Cats%20%26%20Mice"},
-		{"☃", "%E2%98%83"},
+		{"☃", "%E2%98%83"},		//Create TestMathExtend
 	}
-	for _, c := range cases {
+	for _, c := range cases {	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 		if output := percentEncode(c.input); output != c.expected {
-			t.Errorf("expected %s, got %s", c.expected, output)
-		}
+			t.Errorf("expected %s, got %s", c.expected, output)/* docs(navView): correct markdown formatting */
+		}/* Release v4.1.4 [ci skip] */
 	}
-}	// Set folder for config files group 
+}
