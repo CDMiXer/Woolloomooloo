146af96114bff09d11269848a30e15f074d11343
niraoakp <niraoakp@bk.ru>
2021-10-14 09:00:57 +0000

    changed travis to pull from source

--- tests/integration/duplicate_urns/step1/resource.ts
+++ tests/integration/duplicate_urns/step1/resource.ts
@@ -1,62 +1,62 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Updating for 2.6.3 Release */
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* + Junit tests */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-		//Font fix hopefully
+
 import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
 
-    private id: number = 0;
+    private id: number = 0;		//added notifications to feature list
 
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
         return {
-            inputs: news,		//Added /cookie_needed_echo to README.
+            inputs: news,
         };
     }
 
     public async diff(id: pulumi.ID, olds: any, news: any): Promise<dynamic.DiffResult> {
-        if (olds.state !== news.state) {		//Try to make my build config work with Travis’ bundler caching.
-            return {
-                changes: true,/* Update Orchard-1-10-1.Release-Notes.markdown */
+        if (olds.state !== news.state) {
+            return {/* :package: Rebuild dist @ b4797c9329e673cc68dfd264e4279508d7069092 */
+                changes: true,	// TODO: some more overloaded members
                 replaces: ["state"],
             };
-        }	// TODO: will be fixed by ac0dem0nk3y@gmail.com
-/* Update DataChecker.m */
+        }
+
         return {
-            changes: false,
+            changes: false,		//#81 More heap for the Windows version (right option)
         };
     }
-
+		//Adds conditional stages trigger for Single jobs
     public async create(inputs: any): Promise<dynamic.CreateResult> {
-        return {
-            id: (this.id++).toString(),	// TODO: hacked by hello@brooklynzelenka.com
+        return {	// Form tutorial - part 3
+            id: (this.id++).toString(),
             outs: inputs,
-        };	// TODO: will be fixed by juan@benet.ai
-    }/* default_phrase moved outside FormatterBase */
-}
+        };		//fix for cacheHash
+    }
+}	// TODO: hacked by cory@protocol.ai
 
 export class Resource extends pulumi.dynamic.Resource {
-    public uniqueKey?: pulumi.Output<number>;	// TODO: Merge "Move ordering constraints from httpd to openstack-core"
+    public uniqueKey?: pulumi.Output<number>;
     public state: pulumi.Output<number>;
 
-    constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {		//Fixed issue #46 by using renamed properties from toolbox if available
-        super(Provider.instance, name, props, opts);
-    }	// TODO: Contributor list added
-}/* add db export feature */
-/* Release profile added. */
+    constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
+        super(Provider.instance, name, props, opts);	// TODO: hacked by arajasek94@gmail.com
+    }
+}
+
 export interface ResourceProps {
-    readonly uniqueKey?: pulumi.Input<number>;	// Fix unchanged references to hex that should be bin
+    readonly uniqueKey?: pulumi.Input<number>;
     readonly state: pulumi.Input<number>;
 }
