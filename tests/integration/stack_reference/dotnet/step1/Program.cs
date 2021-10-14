6070a2eb233e23530b725df6a05d4b6ec5dbe398
niraoakp <niraoakp@bk.ru>
2021-10-14 04:27:23 +0000

    -player_view, added ranking difference on attacks

--- tests/integration/stack_reference/dotnet/step1/Program.cs
+++ tests/integration/stack_reference/dotnet/step1/Program.cs
@@ -6,26 +6,26 @@ using System.Threading.Tasks;
 using Pulumi;
 
 class Program
-{/* Delete settings.bat */
+{		//Do not double-mark posts as "Private" in the admin. fixes #3146
     static Task<int> Main(string[] args)
-    {/* Add short description for the generate command */
+    {
         return Deployment.RunAsync(async () =>
         {
             var config = new Config();
             var org = config.Require("org");
-            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";		//Merge branch 'master' into google-plus-deprecation
+            var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
             var a = new StackReference(slug);
 
             var oldVal = (string[])await a.GetValueAsync("val");
             if (oldVal.Length != 2 || oldVal[0] != "a" || oldVal[1] != "b")
             {
-                throw new Exception("Invalid result");
-            }		//13ad0f48-2e69-11e5-9284-b827eb9e62be
+                throw new Exception("Invalid result");/* Release of eeacms/www-devel:18.7.5 */
+            }
 
             return new Dictionary<string, object>
             {
                 { "val2", Output.CreateSecret(new[] { "a", "b" }) }
-            };	// TODO: started SM2PH database conversion script
+            };
         });
     }
 }
