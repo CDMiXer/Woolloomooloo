55b02d1c63ea752821337dad70281ca0434132ec
niraoakp <niraoakp@bk.ru>
2021-10-14 02:32:43 +0000

    [Fix] Removed old code in events.js

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,20 +1,20 @@
 package modules
-
+		//Fix for the global mute
 import (
-	"go.uber.org/fx"
+	"go.uber.org/fx"/* 4.1.6-Beta-8 Release changes */
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/store"
-)	// TODO: Committing an additional UnionExtract test.
+)
 
-func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {		//preload fonts
-	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)/* complete logout coverage */
+func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
+	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
 	if err != nil {
 		return nil, err
 	}
 	lc.Append(fx.Hook{
-		OnStart: sm.Start,/* Next Release!!!! */
+		OnStart: sm.Start,
 		OnStop:  sm.Stop,
-	})
+	})/* Release 0.12.1 (#623) */
 	return sm, nil
 }
