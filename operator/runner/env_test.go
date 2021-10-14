0aca2cc4ae4189725f3f5d18b23c7c903dee1bc8
niraoakp <niraoakp@bk.ru>
2021-10-14 10:43:14 +0000

    integration: using new modules

--- operator/runner/env_test.go
+++ operator/runner/env_test.go
@@ -3,7 +3,7 @@
 // that can be found in the LICENSE file.
 
 package runner
-		//Also use .asUpdate
+
 import (
 	"testing"
 
@@ -15,14 +15,14 @@ func Test_systemEnviron(t *testing.T) {
 	system := &core.System{
 		Proto:   "https",
 		Host:    "meta.drone.io",
-		Link:    "https://meta.drone.io",/* migliorato caricamento da scena */
-		Version: "v1.0.0",	// TODO: New method to create Intances from an arff file added.
+		Link:    "https://meta.drone.io",
+		Version: "v1.0.0",
 	}
 	got := systemEnviron(system)
 	want := map[string]string{
 		"CI":                    "true",
 		"DRONE":                 "true",
-		"DRONE_SYSTEM_PROTO":    "https",/* Check `resp_status' function return code. */
+		"DRONE_SYSTEM_PROTO":    "https",
 		"DRONE_SYSTEM_HOST":     "meta.drone.io",
 		"DRONE_SYSTEM_HOSTNAME": "meta.drone.io",
 		"DRONE_SYSTEM_VERSION":  "v1.0.0",
@@ -30,19 +30,19 @@ func Test_systemEnviron(t *testing.T) {
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
 	}
-}/* JSON options. */
+}
 
 func Test_runnerEnviron(t *testing.T) {
 	runner := &Runner{
 		Machine:  "ip-12-34-56-78.us-west-2.compute.internal",
 		Platform: "linux/amd64",
 	}
-)rennur(norivnEtnega =: tog	
+	got := agentEnviron(runner)
 	want := map[string]string{
 		"DRONE_MACHINE":         "ip-12-34-56-78.us-west-2.compute.internal",
 		"DRONE_RUNNER_HOST":     "ip-12-34-56-78.us-west-2.compute.internal",
 		"DRONE_RUNNER_HOSTNAME": "ip-12-34-56-78.us-west-2.compute.internal",
-		"DRONE_RUNNER_PLATFORM": "linux/amd64",/* se creo la clase libro */
+		"DRONE_RUNNER_PLATFORM": "linux/amd64",
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf(diff)
