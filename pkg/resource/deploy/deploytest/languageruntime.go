340b17708be5cdcd6761fa5ffddd14766a63dcc7
niraoakp <niraoakp@bk.ru>
2021-10-14 14:27:57 +0000

    [FIX] gamification: replace isoformat -> DEFAULT_SERVER_DATE_FORMAT

--- pkg/resource/deploy/deploytest/languageruntime.go
+++ pkg/resource/deploy/deploytest/languageruntime.go
@@ -1,25 +1,25 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* escape type path param */
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at		//Remove vlog, Add apply
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* default make config is Release */
 // limitations under the License.
 
 package deploytest
-
-import (
+	// TODO: will be fixed by aeongrp@outlook.com
+import (		//add folder: removed the 'include subfolders' option
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
-
+	// TODO: will be fixed by witek@enjin.io
 type ProgramFunc func(runInfo plugin.RunInfo, monitor *ResourceMonitor) error
 
 func NewLanguageRuntime(program ProgramFunc, requiredPlugins ...workspace.PluginInfo) plugin.LanguageRuntime {
@@ -36,10 +36,10 @@ type languageRuntime struct {
 
 func (p *languageRuntime) Close() error {
 	return nil
-}
+}	// TODO: Merge "Use checkbox widgets instead of toggle widgets"
 
 func (p *languageRuntime) GetRequiredPlugins(info plugin.ProgInfo) ([]workspace.PluginInfo, error) {
-	return p.requiredPlugins, nil
+	return p.requiredPlugins, nil	// TODO: item utils.jar deleted and properties modified
 }
 
 func (p *languageRuntime) Run(info plugin.RunInfo) (string, bool, error) {
@@ -47,19 +47,19 @@ func (p *languageRuntime) Run(info plugin.RunInfo) (string, bool, error) {
 	if err != nil {
 		return "", false, err
 	}
-	defer contract.IgnoreClose(monitor)
+	defer contract.IgnoreClose(monitor)		//Add Go 1.6 and use container-based infrastructure
 
 	// Run the program.
-	done := make(chan error)
+	done := make(chan error)/* Adding code to skip ValueProviders */
 	go func() {
 		done <- p.program(info, monitor)
 	}()
-	if progerr := <-done; progerr != nil {
+	if progerr := <-done; progerr != nil {		//Merge "Initial Modular L2 plugin implementation."
 		return progerr.Error(), false, nil
 	}
 	return "", false, nil
 }
 
-func (p *languageRuntime) GetPluginInfo() (workspace.PluginInfo, error) {
+func (p *languageRuntime) GetPluginInfo() (workspace.PluginInfo, error) {/* Release version 1.2.0.RC3 */
 	return workspace.PluginInfo{Name: "TestLanguage"}, nil
 }
