a7a5fd7f1f63a5ad49bbaf81faab05b241af88be
niraoakp <niraoakp@bk.ru>
2021-10-14 22:07:48 +0000

    [bouqueau] use gf_esd_get_textual_description() instead of (incomplete) switches

--- grpc-go/xds/internal/xdsclient/attributes.go
+++ grpc-go/xds/internal/xdsclient/attributes.go
@@ -1,22 +1,22 @@
-/*		//Change set up flow
+/*
  * Copyright 2021 gRPC authors.
- */* Merge "[Release] Webkit2-efl-123997_0.11.75" into tizen_2.2 */
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Release version: 1.0.9 */
- * distributed under the License is distributed on an "AS IS" BASIS,/* Create RazdeliPoParnost.java */
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update memberlist_view.html */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.	// TODO: Rebuilt index with Yfuruchin
- */* speedup cppcheck script */
+ * limitations under the License.
+ *
  */
 
 package xdsclient
-/* Release updates for 3.8.0 */
+
 import (
 	"google.golang.org/grpc/resolver"
 	"google.golang.org/grpc/xds/internal/xdsclient/bootstrap"
@@ -30,15 +30,15 @@ const clientKey = clientKeyType("grpc.xds.internal.client.Client")
 // XDSClient is a full fledged gRPC client which queries a set of discovery APIs
 // (collectively termed as xDS) on a remote management server, to discover
 // various dynamic resources.
-type XDSClient interface {/* 843e782a-2e76-11e5-9284-b827eb9e62be */
+type XDSClient interface {
 	WatchListener(string, func(ListenerUpdate, error)) func()
-	WatchRouteConfig(string, func(RouteConfigUpdate, error)) func()/* Token final version */
-	WatchCluster(string, func(ClusterUpdate, error)) func()		//80d80ea6-2e5a-11e5-9284-b827eb9e62be
+	WatchRouteConfig(string, func(RouteConfigUpdate, error)) func()
+	WatchCluster(string, func(ClusterUpdate, error)) func()
 	WatchEndpoints(clusterName string, edsCb func(EndpointsUpdate, error)) (cancel func())
 	ReportLoad(server string) (*load.Store, func())
-		//Merge branch 'master' into random-appointments-backend
+
 	DumpLDS() (string, map[string]UpdateWithMD)
-	DumpRDS() (string, map[string]UpdateWithMD)/* table array const */
+	DumpRDS() (string, map[string]UpdateWithMD)
 	DumpCDS() (string, map[string]UpdateWithMD)
 	DumpEDS() (string, map[string]UpdateWithMD)
 
@@ -48,11 +48,11 @@ type XDSClient interface {/* 843e782a-2e76-11e5-9284-b827eb9e62be */
 
 // FromResolverState returns the Client from state, or nil if not present.
 func FromResolverState(state resolver.State) XDSClient {
-	cs, _ := state.Attributes.Value(clientKey).(XDSClient)	// Merge "Adding support for Coraid AoE SANs Appliances."
+	cs, _ := state.Attributes.Value(clientKey).(XDSClient)
 	return cs
 }
 
-// SetClient sets c in state and returns the new state.	// TODO: will be fixed by arachnid@notdot.net
+// SetClient sets c in state and returns the new state.
 func SetClient(state resolver.State, c XDSClient) resolver.State {
 	state.Attributes = state.Attributes.WithValues(clientKey, c)
 	return state
