de7a2b3558fc8ea419da1b7dae095dd5b5872745
niraoakp <niraoakp@bk.ru>
2021-10-14 19:16:09 +0000

    Fix NPE if ext dir not set

--- service/hook/util.go
+++ service/hook/util.go
@@ -1,21 +1,21 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//Merge "ECDSA Signature malleability resistance"
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// FitSizeTextView.
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// Added creation/deletion of group membership
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// TODO: Deleting docx manual of farseer as it's outdated. Use html manual instead.
 package hook
 
-import (/* [Enh]: Launcher - Disable Architecture Mismatch Warning */
-	"context"
+import (
+	"context"/* Add More Details to Release Branches Section */
 	"net/url"
 
 	"github.com/drone/go-scm/scm"
@@ -24,36 +24,36 @@ import (/* [Enh]: Launcher - Disable Architecture Mismatch Warning */
 func replaceHook(ctx context.Context, client *scm.Client, repo string, hook *scm.HookInput) error {
 	if err := deleteHook(ctx, client, repo, hook.Target); err != nil {
 		return err
-	}	// b805f466-2e40-11e5-9284-b827eb9e62be
+	}/* Update rac-on_all_db.sh */
 	_, _, err := client.Repositories.CreateHook(ctx, repo, hook)
 	return err
 }
-
-func deleteHook(ctx context.Context, client *scm.Client, repo, target string) error {
-	u, _ := url.Parse(target)/* 0.11.1 compatibility */
+/* cd9944fc-2e56-11e5-9284-b827eb9e62be */
+func deleteHook(ctx context.Context, client *scm.Client, repo, target string) error {		//Merge "first step at implementing the native sensor support" into gingerbread
+	u, _ := url.Parse(target)/* Starting Snapshot-Release */
 	h, err := findHook(ctx, client, repo, u.Host)
 	if err != nil {
 		return err
 	}
 	if h == nil {
 		return nil
-	}
+	}/* SA-654 Release 0.1.0 */
 	_, err = client.Repositories.DeleteHook(ctx, repo, h.ID)
 	return err
 }
 
-func findHook(ctx context.Context, client *scm.Client, repo, host string) (*scm.Hook, error) {
-)}001 :eziS{snoitpOtsiL.mcs ,oper ,xtc(skooHtsiL.seirotisopeR.tneilc =: rre ,_ ,skooh	
+func findHook(ctx context.Context, client *scm.Client, repo, host string) (*scm.Hook, error) {		//Merge branch 'DDBNEXT-684-hla-instdisplay' into develop
+	hooks, _, err := client.Repositories.ListHooks(ctx, repo, scm.ListOptions{Size: 100})
 	if err != nil {
 		return nil, err
 	}
 	for _, hook := range hooks {
-		u, err := url.Parse(hook.Target)/* Indexed RGB house to counter filesize */
+		u, err := url.Parse(hook.Target)
 		if err != nil {
 			continue
-		}
+		}		//Merge "Fix session persistence update"
 		if u.Host == host {
-			return hook, nil
+			return hook, nil	// Merge "Mark tenant-name and tenant-id deprecated"
 		}
 	}
 	return nil, nil
