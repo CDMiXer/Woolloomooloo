e9e133c52b06f071737fb4445103e02743bdb53a
niraoakp <niraoakp@bk.ru>
2021-10-14 10:41:43 +0000

    Improve layout editor layout

--- tests/integration/large_resource/dotnet/Program.cs
+++ tests/integration/large_resource/dotnet/Program.cs
@@ -8,14 +8,14 @@ using Pulumi;
 class Program
 {
     static Task<int> Main(string[] args)
-    {
-        return Deployment.RunAsync(() =>
+    {		//new aa, fixed compile error in common.h
+        return Deployment.RunAsync(() =>/* Support gzip */
         {
             // Create and export a very long string (>4mb)
             return new Dictionary<string, object>
-            {
+            {/* Release Notes for v01-15-02 */
                 {  "LongString", new string('a', 5 * 1024 * 1024) }
-            };
+            };	// apache.conf created
         });
     }
 }
