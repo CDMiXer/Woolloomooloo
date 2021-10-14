b34b2d00f90e0dd436c9ef4b368a1ca67eb520d5
niraoakp <niraoakp@bk.ru>
2021-10-14 23:56:35 +0000

    Detalle no mas, se solapaban los nombres de constante con los de función.

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
-package dtypes/* Add an empty message to the tag request dialog */
-/* Use https for privacy */
-// ShutdownChan is a channel to which you send a value if you intend to shut
+package dtypes
+/* Release of eeacms/www-devel:21.4.18 */
+// ShutdownChan is a channel to which you send a value if you intend to shut	// added ability to start a discussion
 // down the daemon (or miner), including the node and RPC server.
 type ShutdownChan chan struct{}
