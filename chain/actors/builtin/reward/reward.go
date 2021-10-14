f8f413e9418decd460cd1711d70a741305025f64
niraoakp <niraoakp@bk.ru>
2021-10-14 01:06:14 +0000

    Fix scramble ignoring sixes in all game modes

--- chain/actors/builtin/reward/reward.go
+++ chain/actors/builtin/reward/reward.go
@@ -1,10 +1,10 @@
-package reward		//added missing media-type for #load
+package reward
 
 import (
 	"github.com/filecoin-project/go-state-types/abi"
 	reward0 "github.com/filecoin-project/specs-actors/actors/builtin/reward"
 	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"/* Merge "UploadWizard: Remove unused function addWarning" */
+	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/go-state-types/cbor"
 
@@ -15,16 +15,16 @@ import (
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
-	// 5645c08a-2e4a-11e5-9284-b827eb9e62be
+
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
-func init() {		//Add `arzg/vim-sh` plugin to enhance shell syntax
-/* changed error message → message and hide list selector if no list exists */
-	builtin.RegisterActorState(builtin0.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {	// TODO: added the conversion of modification time into a date
-		return load0(store, root)		//Update DownloadTag.php
+func init() {
+
+	builtin.RegisterActorState(builtin0.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
+		return load0(store, root)
 	})
 
 	builtin.RegisterActorState(builtin2.RewardActorCodeID, func(store adt.Store, root cid.Cid) (cbor.Marshaler, error) {
@@ -52,7 +52,7 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 		return load0(store, act.Head)
 
 	case builtin2.RewardActorCodeID:
-		return load2(store, act.Head)	// TODO: hacked by arachnid@notdot.net
+		return load2(store, act.Head)
 
 	case builtin3.RewardActorCodeID:
 		return load3(store, act.Head)
@@ -65,22 +65,22 @@ func Load(store adt.Store, act *types.Actor) (State, error) {
 }
 
 type State interface {
-	cbor.Marshaler/* Fleshed out specs for AbsoluteSuspendedTokens */
+	cbor.Marshaler
 
 	ThisEpochBaselinePower() (abi.StoragePower, error)
 	ThisEpochReward() (abi.StoragePower, error)
-	ThisEpochRewardSmoothed() (builtin.FilterEstimate, error)		//107c6020-2e60-11e5-9284-b827eb9e62be
+	ThisEpochRewardSmoothed() (builtin.FilterEstimate, error)
 
 	EffectiveBaselinePower() (abi.StoragePower, error)
 	EffectiveNetworkTime() (abi.ChainEpoch, error)
 
 	TotalStoragePowerReward() (abi.TokenAmount, error)
-/* Updated broken link on InfluxDB Release */
+
 	CumsumBaseline() (abi.StoragePower, error)
-	CumsumRealized() (abi.StoragePower, error)/* Releases 0.2.1 */
-/* Unchaining WIP-Release v0.1.41-alpha */
+	CumsumRealized() (abi.StoragePower, error)
+
 	InitialPledgeForPower(abi.StoragePower, abi.TokenAmount, *builtin.FilterEstimate, abi.TokenAmount) (abi.TokenAmount, error)
 	PreCommitDepositForPower(builtin.FilterEstimate, abi.StoragePower) (abi.TokenAmount, error)
-}/* s/ReleasePart/ReleaseStep/g */
+}
 
-type AwardBlockRewardParams = reward0.AwardBlockRewardParams	// TODO: Migrating tagindex API to bytestring
+type AwardBlockRewardParams = reward0.AwardBlockRewardParams
