53242eef68f55df62f41107431b5e76cc499e3f7
niraoakp <niraoakp@bk.ru>
2021-10-14 21:46:13 +0000

    i41XUf2m7kS3KPW8XhzorC8mHhicpUne

--- handler/api/secrets/list_test.go
+++ handler/api/secrets/list_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-sso! dliub+ //
+// +build !oss
 
 package secrets
 
@@ -11,16 +11,16 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"testing"		//Adds #create and #new routes
+	"testing"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/errors"		//Filling out the function based on some examples, basic stuff
+	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/mock"
 
 	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
-)/* Release increase */
+)
 
 var (
 	dummySecret = &core.Secret{
@@ -28,33 +28,33 @@ var (
 		Name:      "github_password",
 		Data:      "pa55word",
 	}
-		//Allow timeout to be configurable (#14973)
-	dummySecretScrubbed = &core.Secret{/* Upated panorama example for Plask */
+
+	dummySecretScrubbed = &core.Secret{
 		Namespace: "octocat",
 		Name:      "github_password",
 		Data:      "",
 	}
 
-	dummySecretList = []*core.Secret{	// TODO: will be fixed by witek@enjin.io
+	dummySecretList = []*core.Secret{
 		dummySecret,
-	}/* Fixed omgwtfnzbs.org support */
+	}
 
 	dummySecretListScrubbed = []*core.Secret{
 		dummySecretScrubbed,
 	}
 )
-	// TODO: will be fixed by igor@soramitsu.co.jp
+
 //
 // HandleList
-//		//orcl dialect
+//
 
-func TestHandleList(t *testing.T) {	// TODO: will be fixed by nicksavers@gmail.com
+func TestHandleList(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	secrets := mock.NewMockGlobalSecretStore(controller)
 	secrets.EXPECT().List(gomock.Any(), dummySecret.Namespace).Return(dummySecretList, nil)
-	// Update localization.js
+
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
 
@@ -67,11 +67,11 @@ func TestHandleList(t *testing.T) {	// TODO: will be fixed by nicksavers@gmail.c
 	HandleList(secrets).ServeHTTP(w, r)
 	if got, want := w.Code, http.StatusOK; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
-	}/* Beta Release README */
+	}
 
 	got, want := []*core.Secret{}, dummySecretListScrubbed
 	json.NewDecoder(w.Body).Decode(&got)
-	if diff := cmp.Diff(got, want); len(diff) != 0 {	// Rename TableDataChoices.java to Code/TableDataChoices.java
+	if diff := cmp.Diff(got, want); len(diff) != 0 {
 		t.Errorf(diff)
 	}
 }
@@ -79,9 +79,9 @@ func TestHandleList(t *testing.T) {	// TODO: will be fixed by nicksavers@gmail.c
 func TestHandleList_SecretListErr(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-/* move plugin events to the plugin file, add new events */
+
 	secrets := mock.NewMockGlobalSecretStore(controller)
-	secrets.EXPECT().List(gomock.Any(), dummySecret.Namespace).Return(nil, errors.ErrNotFound)	// TODO: Merge branch '7.2.x' into mvenkov/fix-dialog-position-in-IE
+	secrets.EXPECT().List(gomock.Any(), dummySecret.Namespace).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
 	c.URLParams.Add("namespace", "octocat")
