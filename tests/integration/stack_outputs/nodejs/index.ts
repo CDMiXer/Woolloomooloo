9d82368c86de35f6926b1db19da1d49bc893f002
niraoakp <niraoakp@bk.ru>
2021-10-14 07:50:20 +0000

    Rework asset including for nested folders

--- tests/integration/stack_outputs/nodejs/index.ts
+++ tests/integration/stack_outputs/nodejs/index.ts
@@ -1,4 +1,4 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 export let xyz = "ABC";
-export let foo = 42;	// TODO: hacked by sebastian.tharakan97@gmail.com
+export let foo = 42;
