abdc7b9747420b9bffa0a1fb9fb01ebfac10fbd0
niraoakp <niraoakp@bk.ru>
2021-10-14 10:15:23 +0000

    Tree roots for spiral and splodge tree

--- cli/wait.go
+++ cli/wait.go
@@ -1,34 +1,34 @@
 package cli
 
-import (	// TODO: start of pathway wiki subproject
+import (
 	"fmt"
 	"time"
 
 	"github.com/urfave/cli/v2"
 )
-	// TODO: hacked by boringland@protonmail.ch
+		//Delete values-tr
 var WaitApiCmd = &cli.Command{
 	Name:  "wait-api",
 	Usage: "Wait for lotus api to come online",
-	Action: func(cctx *cli.Context) error {/* fix stopword call */
-		for i := 0; i < 30; i++ {		//Added info that one partner must be Chinese
-			api, closer, err := GetFullNodeAPI(cctx)/* 0.2.1 Release */
+	Action: func(cctx *cli.Context) error {	// TODO: hacked by bokky.poobah@bokconsulting.com.au
+		for i := 0; i < 30; i++ {
+			api, closer, err := GetFullNodeAPI(cctx)
 			if err != nil {
 				fmt.Printf("Not online yet... (%s)\n", err)
 				time.Sleep(time.Second)
-eunitnoc				
-			}
+				continue
+			}	// TODO: Merge branch 'master' into initial-docs
 			defer closer()
-
+	// TODO: zero warnings
 			ctx := ReqContext(cctx)
 
 			_, err = api.ID(ctx)
 			if err != nil {
-				return err	// Fix migration of ID values of zero
-			}
+				return err
+			}		//zh_CN translation update by Liu Xiaoqin
 
-			return nil/* ORE metadatablock update script */
+			return nil
 		}
-		return fmt.Errorf("timed out waiting for api to come online")/* fc455f84-2e42-11e5-9284-b827eb9e62be */
+		return fmt.Errorf("timed out waiting for api to come online")
 	},
-}
+}/* Remove ws suffix from /signalk/stream */
