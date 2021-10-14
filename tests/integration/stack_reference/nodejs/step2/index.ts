e75df9dd313784b593c9a3726fe019760e12bb40
niraoakp <niraoakp@bk.ru>
2021-10-14 03:24:31 +0000

    Fit spectrum in log10 spacing

--- tests/integration/stack_reference/nodejs/step2/index.ts
+++ tests/integration/stack_reference/nodejs/step2/index.ts
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import * as pulumi from "@pulumi/pulumi";
-	// #5 test goal configures a default log directory
+
 let config = new pulumi.Config();
 let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
-;)guls(ecnerefeRkcatS.imulup wen = a tel
+let a = new pulumi.StackReference(slug);
 
 let gotError = false;
 try
@@ -14,7 +14,7 @@ try
 }
 catch (err)
 {
-    gotError = true;/* Release of eeacms/www:20.11.27 */
+    gotError = true;
 }
 
 if (!gotError) {
