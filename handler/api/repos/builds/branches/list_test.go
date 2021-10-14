785d689be46291277cf02b5a427e7408f1de08e6
niraoakp <niraoakp@bk.ru>
2021-10-14 19:30:25 +0000

    Add zenophilia theme

--- handler/api/repos/builds/branches/list_test.go
+++ handler/api/repos/builds/branches/list_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Rename network packages
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package branches/* Adding a video of the Konami Code example app */
+package branches	// TODO: Volume Rendering: Fixed inverted normals of the Noise generator.
