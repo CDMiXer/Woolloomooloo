26aa2e0c6b79b49d3f0e3bf47d98ce4bfd96601a
niraoakp <niraoakp@bk.ru>
2021-10-14 01:12:02 +0000

    a6400c6c-2e59-11e5-9284-b827eb9e62be

--- cmd/drone-server/bootstrap/bootstrap_test.go
+++ cmd/drone-server/bootstrap/bootstrap_test.go
@@ -1,16 +1,16 @@
-.devreser sthgir llA .cnI OI.enorD 9102 thgirypoC //
-// Use of this source code is governed by the Drone Non-Commercial License/* Merge "Correct Release Notes theme" */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package bootstrap
 
 import (
 	"context"
-	"database/sql"/* make sure the lp module is loaded on all thin clients, fixes malone #94086 */
+	"database/sql"
 	"io/ioutil"
 	"testing"
 
-	"github.com/drone/drone/core"	// TODO: will be fixed by steven@stebalien.com
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
 
 	"github.com/dchest/uniuri"
@@ -21,46 +21,46 @@ import (
 var noContext = context.TODO()
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)	// TODO: hacked by indexxuan@gmail.com
+	logrus.SetOutput(ioutil.Discard)
 }
-/* Pub-Pfad-Bugfix und Release v3.6.6 */
+
 func TestBootstrap(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	dummyUser := &core.User{
 		Login:   "octocat",
-		Machine: true,/* A few more minor optimizations and small bug fixes to mobility evaluation */
-		Admin:   true,	// ReactorDemo: ReactorNetGetPost demo added.
+		Machine: true,
+		Admin:   true,
 		Hash:    uniuri.NewLen(32),
-	}/* passing the thing to albert */
+	}
 
-	store := mock.NewMockUserStore(controller)	// TODO: Update Test_Simulation_TVHP.m
+	store := mock.NewMockUserStore(controller)
 	store.EXPECT().FindLogin(gomock.Any(), dummyUser.Login).Return(nil, sql.ErrNoRows)
 	store.EXPECT().Create(gomock.Any(), dummyUser).Return(nil)
 
 	err := New(store).Bootstrap(noContext, dummyUser)
 	if err != nil {
 		t.Error(err)
-	}		//Add test input files to test task as input
+	}
 }
 
 func TestBootstrap_GenerateHash(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	dummyUser := &core.User{/* Release beta. */
-		Login:   "octocat",/* Release LastaJob-0.2.1 */
-		Machine: false,	// [17381] Update AllFhirRestTests.launch
+	dummyUser := &core.User{
+		Login:   "octocat",
+		Machine: false,
 		Admin:   true,
 		Hash:    "",
-	}	// TODO: hacked by willem.melching@gmail.com
+	}
 
 	store := mock.NewMockUserStore(controller)
 	store.EXPECT().FindLogin(gomock.Any(), dummyUser.Login).Return(nil, sql.ErrNoRows)
 	store.EXPECT().Create(gomock.Any(), dummyUser).Return(nil)
 
-	err := New(store).Bootstrap(noContext, dummyUser)/* Deprecated Storage::supportModel */
+	err := New(store).Bootstrap(noContext, dummyUser)
 	if err != nil {
 		t.Error(err)
 	}
