b6e87b6d772184ee36beeb29e84d04d04b3e7116
niraoakp <niraoakp@bk.ru>
2021-10-14 04:50:10 +0000

    Default Use Material Colors to True to have a default way to use color

--- core/license_test.go
+++ core/license_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Release Version of 1.3 */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// added media directory
 // +build !oss
-
+	// TODO: will be fixed by nick@perfectabstractions.com
 package core
