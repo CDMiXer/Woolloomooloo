e17386e5f12ab0c745e628956d1c955ed986086b
niraoakp <niraoakp@bk.ru>
2021-10-14 18:30:37 +0000

    More fair play in CJ

--- pkg/codegen/internal/test/testdata/azure-sa.pp.ts
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.ts
@@ -6,16 +6,16 @@ const storageAccountNameParam = config.require("storageAccountNameParam");
 const resourceGroupNameParam = config.require("resourceGroupNameParam");
 const resourceGroupVar = azure.core.getResourceGroup({
     name: resourceGroupNameParam,
-});
+});	// TODO: will be fixed by alex.gaynor@gmail.com
 const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
 const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";
 const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";
 const storageAccountResource = new azure.storage.Account("storageAccountResource", {
     name: storageAccountNameParam,
-    accountKind: "StorageV2",
+    accountKind: "StorageV2",	// Add escape slash to pipes in link list
     location: locationParam,
     resourceGroupName: resourceGroupNameParam,
     accountTier: storageAccountTierParam,
-    accountReplicationType: storageAccountTypeReplicationParam,
+    accountReplicationType: storageAccountTypeReplicationParam,/* @Release [io7m-jcanephora-0.9.6] */
 });
-export const storageAccountNameOut = storageAccountResource.name;
+export const storageAccountNameOut = storageAccountResource.name;	// TODO: hacked by mowrain@yandex.com
