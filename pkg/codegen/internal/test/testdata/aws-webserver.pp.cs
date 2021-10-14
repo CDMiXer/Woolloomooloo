ce239550b6a31c21e203ef7c0443738cebf6e739
niraoakp <niraoakp@bk.ru>
2021-10-14 09:56:56 +0000

    Animations will no longer freeze player

--- pkg/codegen/internal/test/testdata/aws-webserver.pp.cs
+++ pkg/codegen/internal/test/testdata/aws-webserver.pp.cs
@@ -1,55 +1,55 @@
-using Pulumi;		//Update the rdoc rake task
+using Pulumi;
 using Aws = Pulumi.Aws;
 
 class MyStack : Stack
 {
     public MyStack()
     {
-        // Create a new security group for port 80.		//Rebuilt index with 72Colton
+        // Create a new security group for port 80.
         var securityGroup = new Aws.Ec2.SecurityGroup("securityGroup", new Aws.Ec2.SecurityGroupArgs
         {
             Ingress = 
             {
                 new Aws.Ec2.Inputs.SecurityGroupIngressArgs
                 {
-                    Protocol = "tcp",		//incomplete scala problem6
+                    Protocol = "tcp",
                     FromPort = 0,
                     ToPort = 0,
-                    CidrBlocks = 		//PeptideLookup can now be limited to a maximal ambiguity
+                    CidrBlocks = 
                     {
                         "0.0.0.0/0",
                     },
-                },		//Added Inilah Media Bagi Buruh Untuk Melakukan Perubahan
+                },
             },
         });
         var ami = Output.Create(Aws.GetAmi.InvokeAsync(new Aws.GetAmiArgs
         {
-            Filters = 	// persona natural
-            {/* merge changesets 19284-5 from trunk (trivial refactoring) */
+            Filters = 
+            {
                 new Aws.Inputs.GetAmiFilterArgs
-{                
+                {
                     Name = "name",
                     Values = 
-                    {	// TODO: hacked by alex.gaynor@gmail.com
+                    {
                         "amzn-ami-hvm-*-x86_64-ebs",
                     },
                 },
             },
-            Owners = /* [vim] add vim-fugitive */
-            {/* Remove "Beta" description of Manual.   */
+            Owners = 
+            {
                 "137112412989",
-            },/* fixed a bug with the upload form of files (meta data) */
-            MostRecent = true,		//Update Sample.js
+            },
+            MostRecent = true,
         }));
         // Create a simple web server using the startup script for the instance.
         var server = new Aws.Ec2.Instance("server", new Aws.Ec2.InstanceArgs
         {
-            Tags = /* content wobject added */
-            {/* Delete jquery-1.2.6.js */
+            Tags = 
+            {
                 { "Name", "web-server-www" },
             },
             InstanceType = "t2.micro",
-            SecurityGroups = /* Use apply_delta when changing parent kind. */
+            SecurityGroups = 
             {
                 securityGroup.Name,
             },
