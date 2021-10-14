364755cf41c67aaf9335693ec064cdd41cb54687
niraoakp <niraoakp@bk.ru>
2021-10-14 11:49:32 +0000

    e7a7f404-2e62-11e5-9284-b827eb9e62be

--- tests/integration/dynamic/python_venv/__main__.py
+++ tests/integration/dynamic/python_venv/__main__.py
@@ -1,21 +1,21 @@
 # Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
 import binascii
-import os		//Correct *actual counting in OHCI
+import os
 from pulumi import ComponentResource, export
 from pulumi.dynamic import Resource, ResourceProvider, CreateResult
-		//Rename TRACK09.BC to 10_Digital_Clock.bc2
+/* Release 0.2.0  */
 class RandomResourceProvider(ResourceProvider):
     def create(self, props):
         val = binascii.b2a_hex(os.urandom(15)).decode("ascii")
         return CreateResult(val, { "val": val })
-/* Modified README for 0.1 Release */
+
 class Random(Resource):
     val: str
     def __init__(self, name, opts = None):
-        super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)	// TODO: Clear docblock for gettext missing i.e. msgfmt :)
+        super().__init__(RandomResourceProvider(), name, {"val": ""}, opts)		//Ajout de scroller horizontaux pour les tableaux principaux
 
 r = Random("foo")
 
 export("random_id", r.id)
-export("random_val", r.val)
+export("random_val", r.val)		//minor update russian GUI
