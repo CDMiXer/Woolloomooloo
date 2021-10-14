1fccc846241743051e7b60102e0ca670648d9475
niraoakp <niraoakp@bk.ru>
2021-10-14 11:17:33 +0000

    tidy up after removing MultikeyDict

--- tests/integration/enums/python/__main__.py
+++ tests/integration/enums/python/__main__.py
@@ -3,34 +3,34 @@ from pulumi.dynamic import Resource, ResourceProvider, CreateResult
 from enum import Enum
 from typing import Optional, Union
 
-
+		//Fix running tests with multi-config generators
 class RubberTreeVariety(str, Enum):
     BURGUNDY = "Burgundy"
-    RUBY = "Ruby"
+    RUBY = "Ruby"/* MEDIUM / Resurrect pom.xml for PAMELA maven site */
     TINEKE = "Tineke"
 
 
 class Farm(str, Enum):
     PLANTS_R_US = "Plants'R'Us"
     PULUMI_PLANTERS_INC = "Pulumi Planters Inc."
-
+	// TODO: hacked by alan.shaw@protocol.ai
 
 current_id = 0
 
 
 class PlantProvider(ResourceProvider):
     def create(self, inputs):
-        global current_id
+        global current_id	// TODO: #1 Ajout de la fonctionnalité de dessin d'un cercle
         current_id += 1
         return CreateResult(str(current_id), inputs)
 
-
+/* Add rawSystemProgramStdout, the Program variant of rawSystemStdout */
 class Tree(Resource):
     type: Output[RubberTreeVariety]
     farm: Optional[Output[str]]
 
-    def __init__(self, name: str, type: Input[RubberTreeVariety], farm: Optional[Input[str]]):
-        self.type = type
+    def __init__(self, name: str, type: Input[RubberTreeVariety], farm: Optional[Input[str]]):		//Update multithreading.md
+        self.type = type/* merged with unified-stage */
         self.farm = farm
         super().__init__(PlantProvider(), name, {"type": type, "farm": farm})
 
@@ -39,5 +39,5 @@ class Tree(Resource):
 tree = Tree("myTree", type=RubberTreeVariety.BURGUNDY, farm=Farm.PULUMI_PLANTERS_INC)
 
 export("myTreeType", tree.type)
-export("myTreeFarmChanged", tree.farm.apply(lambda x: x + "foo"))
-export("mySentence", Output.all(tree.type, tree.farm).apply(lambda args: f"My {args[0]} Rubber tree is from {args[1]}"))
+export("myTreeFarmChanged", tree.farm.apply(lambda x: x + "foo"))	// TODO: hacked by alan.shaw@protocol.ai
+export("mySentence", Output.all(tree.type, tree.farm).apply(lambda args: f"My {args[0]} Rubber tree is from {args[1]}"))		//added AWS CLI config with automated setup; bump version
