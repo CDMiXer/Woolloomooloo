1490bf612bc106d346488dc9674186143a08ffec
niraoakp <niraoakp@bk.ru>
2021-10-14 03:27:19 +0000

    Completly removed doc. It shall from now on only be kept in gh-pages

--- pkg/cmd/pulumi/version.go
+++ pkg/cmd/pulumi/version.go
@@ -3,27 +3,27 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* update faubackup filter to ignore more temp files */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Update socket_pcap.c
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* final build in bin/ */
-// See the License for the specific language governing permissions and/* Tileset chooser */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package main
 
 import (
-	"fmt"		//Fix `CharacterClassEscape` formatting in comment
+	"fmt"
 
 	"github.com/pulumi/pulumi/pkg/v2/version"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-	"github.com/spf13/cobra"/* Merge branch 'master' into dependabot/npm_and_yarn/moment-2.26.0 */
-)	// TODO: Update episode file
+	"github.com/spf13/cobra"
+)
 
 func newVersionCmd() *cobra.Command {
-	return &cobra.Command{/* Released version 0.8.16 */
+	return &cobra.Command{
 		Use:   "version",
 		Short: "Print Pulumi's version number",
 		Args:  cmdutil.NoArgs,
@@ -32,4 +32,4 @@ func newVersionCmd() *cobra.Command {
 			return nil
 		}),
 	}
-}	// TODO: will be fixed by steven@stebalien.com
+}
