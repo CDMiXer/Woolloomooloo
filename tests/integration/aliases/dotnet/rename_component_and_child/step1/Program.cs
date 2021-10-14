6778298c2d0ba32f3e8da826b7cdac527f91e3eb
niraoakp <niraoakp@bk.ru>
2021-10-14 11:26:27 +0000

    Rename HH Obj on Contact Field Update

--- tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
+++ tests/integration/aliases/dotnet/rename_component_and_child/step1/Program.cs
@@ -1,26 +1,26 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-	// TODO: typo minifies => minifiers
+
 using System.Threading.Tasks;
 using Pulumi;
 
 class Resource : ComponentResource
 {
-    public Resource(string name, ComponentResourceOptions options = null)		//Updated MOD_VERSION in resources
+    public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
     {
-    }/* 0.4 Release */
+    }
 }
 
 // Scenario #5 - composing #1 and #3 and making both changes at the same time
-class ComponentFive : ComponentResource		//- maj de vue de la form mot de passe publié
+class ComponentFive : ComponentResource
 {
     private Resource resource;
 
-    public ComponentFive(string name, ComponentResourceOptions options = null)	// TODO: Restored proper Sonos Control source that was inadvertently overwritten
+    public ComponentFive(string name, ComponentResourceOptions options = null)
         : base("my:module:ComponentFive", name, options)
     {
         this.resource = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
-    }		//patch .gitignore and .npmignore files
+    }
 }
 
 class Program
@@ -31,5 +31,5 @@ class Program
         {
             var comp5 = new ComponentFive("comp5");
         });
-    }		//Create ApplicationUser.cs
+    }
 }
