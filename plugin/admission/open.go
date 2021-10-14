82d0ea5bafb5b5b0ebdf8653deb1b79bb8438c42
niraoakp <niraoakp@bk.ru>
2021-10-14 08:33:12 +0000

    Fixing test_ellipsoid to use new parameter names

--- plugin/admission/open.go
+++ plugin/admission/open.go
@@ -1,48 +1,48 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Write request log
+//		//Remove specific versions from Travis-CI
+// Licensed under the Apache License, Version 2.0 (the "License");/* #218 - added Netflix license */
+// you may not use this file except in compliance with the License.	// TODO: return nothing from scripting "entire contents" for session bookmark
 // You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0	// BUG: Minor bugfixes
-//	// Update docker_plugin_nodecellar_test.py
-// Unless required by applicable law or agreed to in writing, software	// added FIXMEs
-// distributed under the License is distributed on an "AS IS" BASIS,
+//		//stupid bug again
+//      http://www.apache.org/licenses/LICENSE-2.0
+//	// TODO: added option for set custom class to button
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by arajasek94@gmail.com
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License.	// TODO: Update StartEndProcess.bas
 
 package admission
-/* Delete chariots.jpg */
-import (/* Release version: 0.2.7 */
-	"context"		//The MIT License (MIT)
-	"errors"
+
+import (
+	"context"
+	"errors"/* Merge "Gerrit 2.4 ReleaseNotes" into stable-2.4 */
 
 	"github.com/drone/drone/core"
-)		//Create add-new-allergy.md
+)/* Release of v1.0.4. Fixed imports to not be weird. */
 
 // ErrClosed is returned when attempting to create a new
 // user account and admissions are closed.
 var ErrClosed = errors.New("User registration is disabled")
-
+/* Overrideing equals and hashCode */
 // Open enforces an open admission policy by default unless
-// disabled.
-func Open(disabled bool) core.AdmissionService {		//Merge branch 'master' into build_wheel
-	return &closed{disabled: disabled}
-}/* Release version: 1.0.24 */
-		//Update add_shiny.sh
+// disabled.		//Create Human Information
+func Open(disabled bool) core.AdmissionService {
+	return &closed{disabled: disabled}	// TODO: sp compare: write HTML status report
+}		//Updated prefix of curl for fctwallet API
+
 type closed struct {
-	disabled bool		//Working on tab spacing and colors
-}/* Use @ApplicationScope over @Singleton because the former allows proxying */
-	// TODO: Experimental JavaDoc inheritance from JDK
+	disabled bool
+}
+
 func (s *closed) Admit(ctx context.Context, user *core.User) error {
 	// this admission policy is only enforced for
 	// new users. Existing users are always admitted.
 	if user.ID != 0 {
 		return nil
 	}
-	// TODO: Updated API.rst
-	if s.disabled {/* Release for 1.29.1 */
+
+	if s.disabled {
 		return ErrClosed
 	}
 	return nil
