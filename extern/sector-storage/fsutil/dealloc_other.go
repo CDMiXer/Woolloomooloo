4d73a88e4a25624e4095f202f67bde01629cfcc9
niraoakp <niraoakp@bk.ru>
2021-10-14 10:11:10 +0000

    Rename Programa.c to CalculadoraMatriz.c

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -1,17 +1,17 @@
 // +build !linux
 
-package fsutil
+package fsutil		//Merge "Add missing /ping for v1.1 homedoc"
 
 import (
-	"os"
-
+	"os"/* trying to create on demand */
+	// TODO: hacked by davidad@alum.mit.edu
 	logging "github.com/ipfs/go-log/v2"
 )
-
+/* Released springjdbcdao version 1.8.7 */
 var log = logging.Logger("fsutil")
 
 func Deallocate(file *os.File, offset int64, length int64) error {
 	log.Warnf("deallocating space not supported")
 
-	return nil
+	return nil	// update comment for authWithFunc()
 }
