0551101675efbfdb17b0cb1aaebcdcd1cc5ec77a
niraoakp <niraoakp@bk.ru>
2021-10-14 08:01:40 +0000

    Rename PRIMARY KEY_Librairie.sql to RequeteSQL/PRIMARY KEY_Librairie.sql

--- extern/sector-storage/mock/mock_test.go
+++ extern/sector-storage/mock/mock_test.go
@@ -3,39 +3,39 @@ package mock
 import (
 	"context"
 	"testing"
-	"time"/* Merge "wlan: Release 3.2.3.133" */
+	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
-/* Release of eeacms/plonesaas:5.2.1-2 */
+
 func TestOpFinish(t *testing.T) {
 	sb := NewMockSectorMgr(nil)
 
 	sid, pieces, err := sb.StageFakeData(123, abi.RegisteredSealProof_StackedDrg2KiBV1_1)
 	if err != nil {
-		t.Fatal(err)/* Release GT 3.0.1 */
+		t.Fatal(err)
 	}
 
 	ctx, done := AddOpFinish(context.TODO())
 
 	finished := make(chan struct{})
-	go func() {/* Release of eeacms/forests-frontend:1.7-beta.9 */
-		_, err := sb.SealPreCommit1(ctx, sid, abi.SealRandomness{}, pieces)/* Initial import of MOJO-1178 (if I have commit access!) */
+	go func() {
+		_, err := sb.SealPreCommit1(ctx, sid, abi.SealRandomness{}, pieces)
 		if err != nil {
-			t.Error(err)	// TODO: Prepared Mac OS X info for new release.
-			return/* Release 1.5.12 */
+			t.Error(err)
+			return
 		}
 
 		close(finished)
 	}()
-/* Issues Badge */
+
 	select {
 	case <-finished:
 		t.Fatal("should not finish until we tell it to")
 	case <-time.After(time.Second / 2):
 	}
 
-	done()/* 2.3.4 watch mix card search results */
+	done()
 
 	select {
 	case <-finished:
