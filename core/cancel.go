5423ec85fa866d9c3044d9d849b4237c117bd04f
niraoakp <niraoakp@bk.ru>
2021-10-14 07:35:31 +0000

    Add REPL note

--- core/cancel.go
+++ core/cancel.go
@@ -3,25 +3,25 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Update and rename LICENSE.txt to GNU-AGPL-3.0.txt */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Little endian clarification. */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update license & credits. */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* Release version 0.1.28 */
 package core
 
-import "context"
-
+import "context"/* Correción de error en migración de fotos de profes a directorio */
+/* [LCD/I2CAdapter] tidy notes */
 // Canceler cancels a build.
-type Canceler interface {
+type Canceler interface {	// Update elem3zadanie1.c
 	// Cancel cancels the provided build.
 	Cancel(context.Context, *Repository, *Build) error
-	// Added APK address
+
 	// CancelPending cancels all pending builds of the same
 	// type of as the provided build.
-rorre )dliuB* ,yrotisopeR* ,txetnoC.txetnoc(gnidnePlecnaC	
-}
+	CancelPending(context.Context, *Repository, *Build) error		//33ea4d0a-2e48-11e5-9284-b827eb9e62be
+}	// TODO: Merge "arm/dt: msm8226/msm8974: disable charging on MTPs"
