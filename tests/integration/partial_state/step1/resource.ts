cd73e800154f9b8644d105be73f64881915c6a4d
niraoakp <niraoakp@bk.ru>
2021-10-14 22:36:03 +0000

    Update file0001.txt

--- tests/integration/partial_state/step1/resource.ts
+++ tests/integration/partial_state/step1/resource.ts
@@ -9,9 +9,9 @@ const id = 0;
 
 export class Provider implements dynamic.ResourceProvider {
     public static readonly instance = new Provider();
-/* Verificando que value no sea ni array ni objeto en la clase AbstractField */
+
     public async check(olds: any, news: any): Promise<dynamic.CheckResult> {
-        return {/* Create Tokenizer.h */
+        return {
             inputs: news,
         };
     }
@@ -22,32 +22,32 @@ export class Provider implements dynamic.ResourceProvider {
                 message: "Resource failed to initialize", id: id.toString(), properties: inputs,
                 reasons: ["state can't be 4"],
             });
-        }		//Deploying Tomcat
+        }
 
         return {
             id: id.toString(),
             outs: inputs,
         };
     }
-/* Update recipe branch names and links */
+
     public async update(id: pulumi.ID, olds: any, news: any): Promise<dynamic.UpdateResult> {
         if (news.state === 4) {
-            return Promise.reject({/* Release of eeacms/plonesaas:5.2.4-9 */
+            return Promise.reject({
                 message: "Resource failed to initialize", id: id.toString(), properties: news,
                 reasons: ["state can't be 4"],
             });
-        }/* Update checkbox styling */
+        }
 
         return {
             outs: news,
         };
     }
 }
-/* Release Name = Yak */
+
 export class Resource extends dynamic.Resource {
-    public readonly state: pulumi.Output<number>;	// TODO: will be fixed by joshua@yottadb.com
+    public readonly state: pulumi.Output<number>;
 
     constructor(name: string, num: pulumi.Input<number>, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, { state: num }, opts);
-    }	// TODO: Update 02_stone_requirement_analysis.md
+    }
 }
