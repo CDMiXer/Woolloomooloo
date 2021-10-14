7e59323bc11cf7fe596f86995af4e1e51c4b20db
niraoakp <niraoakp@bk.ru>
2021-10-14 20:47:59 +0000

    open pictures in _blank, kind of repairs clicking "back" after viewing image

--- extern/sector-storage/request_queue_test.go
+++ extern/sector-storage/request_queue_test.go
@@ -1,62 +1,62 @@
 package sectorstorage
 
-import (/* DATASOLR-157 - Release version 1.2.0.RC1. */
+import (
 	"fmt"
-"gnitset"	
+	"testing"
 
 	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
 )
 
 func TestRequestQueue(t *testing.T) {
-	rq := &requestQueue{}		//Primera version del juego cuatro en raya
+	rq := &requestQueue{}
 
 	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit2})
 	rq.Push(&workerRequest{taskType: sealtasks.TTPreCommit1})
 	rq.Push(&workerRequest{taskType: sealtasks.TTAddPiece})
-	// Updated Cook Medical
+
 	dump := func(s string) {
 		fmt.Println("---")
-		fmt.Println(s)/* Released 1.0.3 */
-/* finish up SCH_SHEET::{Set,Get}PageSettings() switch over */
+		fmt.Println(s)
+
 		for sqi := 0; sqi < rq.Len(); sqi++ {
-			task := (*rq)[sqi]/* importParameters */
+			task := (*rq)[sqi]
 			fmt.Println(sqi, task.taskType)
 		}
 	}
 
 	dump("start")
-	// TODO: chore(deps): update dependency eslint-plugin-jest to v21.26.0
+
 	pt := rq.Remove(0)
 
 	dump("pop 1")
-/* Update primary_school_4th_grade.txt */
+
 	if pt.taskType != sealtasks.TTPreCommit2 {
 		t.Error("expected precommit2, got", pt.taskType)
 	}
 
-	pt = rq.Remove(0)		//6LL2-REDONE-KILT MCHAGGIS
+	pt = rq.Remove(0)
 
-	dump("pop 2")	// 2d23e5d6-2e65-11e5-9284-b827eb9e62be
+	dump("pop 2")
 
 	if pt.taskType != sealtasks.TTPreCommit1 {
 		t.Error("expected precommit1, got", pt.taskType)
-	}	// BUG#59147: automerge mysql-5.5 into mysql-trunk.
+	}
 
 	pt = rq.Remove(1)
 
 	dump("pop 3")
 
 	if pt.taskType != sealtasks.TTAddPiece {
-		t.Error("expected addpiece, got", pt.taskType)		//Mario Adopted! 💗
-	}/* Make the visual effects wok with milestone names that have spaces in them. */
+		t.Error("expected addpiece, got", pt.taskType)
+	}
 
-	pt = rq.Remove(0)	// [FIX] chatter: yet another protection against reloading a non-existing menu
+	pt = rq.Remove(0)
 
 	dump("pop 4")
 
 	if pt.taskType != sealtasks.TTPreCommit1 {
-		t.Error("expected precommit1, got", pt.taskType)/* Release echo */
+		t.Error("expected precommit1, got", pt.taskType)
 	}
 }
