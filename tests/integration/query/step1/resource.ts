2bc362b23db52e960b1daafee4adca980b700b40
niraoakp <niraoakp@bk.ru>
2021-10-14 20:10:42 +0000

    :memo: Make env vars table and post demo 04 presentation

--- tests/integration/query/step1/resource.ts
+++ tests/integration/query/step1/resource.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* FSXP plugin Release & Debug */
+import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
 
@@ -14,11 +14,11 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
         };
     }
 }
-	// UIBarButtonItem supported
-export class Resource extends pulumi.dynamic.Resource {	// TODO: created script for removing outliers
+
+export class Resource extends pulumi.dynamic.Resource {
     public isInstance(o: any): o is Resource {
         return o.__pulumiType === "pulumi-nodejs:dynamic:Resource";
-    }/* Prepare Release 1.0.2 */
+    }
 
     constructor(name: string, props: pulumi.Inputs, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
