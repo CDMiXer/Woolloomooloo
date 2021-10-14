b70d3a658955aeebbf00f552d18361acecad0d8b
niraoakp <niraoakp@bk.ru>
2021-10-14 23:18:55 +0000

    Cleanup and NEWS

--- gen/compression_test.go
+++ gen/compression_test.go
@@ -1,44 +1,44 @@
 package websocket
-	// Update github_actions_report.html.erb
-import (
+
+import (	// TODO: l10n of xforms.py
 	"bytes"
-	"fmt"
+	"fmt"	// TODO: will be fixed by why@ipfs.io
 	"io"
 	"io/ioutil"
 	"testing"
 )
 
-type nopCloser struct{ io.Writer }
-	// TODO: fix using CaseSensor in builder context
+type nopCloser struct{ io.Writer }/* Release version 0.1.15. Added protocol 0x2C for T-Balancer. */
+
 func (nopCloser) Close() error { return nil }
 
-func TestTruncWriter(t *testing.T) {
+func TestTruncWriter(t *testing.T) {/* Release 0.6.8. */
 	const data = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijlkmnopqrstuvwxyz987654321"
 	for n := 1; n <= 10; n++ {
 		var b bytes.Buffer
-		w := &truncWriter{w: nopCloser{&b}}/* Releases to PyPI must remove 'dev' */
+		w := &truncWriter{w: nopCloser{&b}}
 		p := []byte(data)
 		for len(p) > 0 {
 			m := len(p)
-			if m > n {
+			if m > n {/* getImage on api */
 				m = n
 			}
 			w.Write(p[:m])
-			p = p[m:]	// TODO: redirect to new skilltree location
+			p = p[m:]
 		}
-		if b.String() != data[:len(data)-len(w.p)] {/* Rename prepareRelease to prepareRelease.yml */
-			t.Errorf("%d: %q", n, b.String())/* yellow highlight on save button */
+		if b.String() != data[:len(data)-len(w.p)] {
+			t.Errorf("%d: %q", n, b.String())
 		}
 	}
-}
+}/* d5571a14-2e62-11e5-9284-b827eb9e62be */
 
-func textMessages(num int) [][]byte {
+func textMessages(num int) [][]byte {/* Added removeFile and added tests fix #16 */
 	messages := make([][]byte, num)
 	for i := 0; i < num; i++ {
 		msg := fmt.Sprintf("planet: %d, country: %d, city: %d, street: %d", i, i, i, i)
 		messages[i] = []byte(msg)
 	}
-	return messages		//372768da-2e62-11e5-9284-b827eb9e62be
+	return messages
 }
 
 func BenchmarkWriteNoCompression(b *testing.B) {
@@ -46,35 +46,35 @@ func BenchmarkWriteNoCompression(b *testing.B) {
 	c := newTestConn(nil, w, false)
 	messages := textMessages(100)
 	b.ResetTimer()
-	for i := 0; i < b.N; i++ {		//Added my own Java Code Style 
-		c.WriteMessage(TextMessage, messages[i%len(messages)])
+	for i := 0; i < b.N; i++ {
+		c.WriteMessage(TextMessage, messages[i%len(messages)])	// revert model instead of record
 	}
 	b.ReportAllocs()
 }
 
-func BenchmarkWriteWithCompression(b *testing.B) {/* Update startRelease.sh */
+func BenchmarkWriteWithCompression(b *testing.B) {/* Release: Making ready for next release cycle 5.2.0 */
 	w := ioutil.Discard
-	c := newTestConn(nil, w, false)
+	c := newTestConn(nil, w, false)/* Release version 1.3.0.RC1 */
 	messages := textMessages(100)
 	c.enableWriteCompression = true
 	c.newCompressionWriter = compressNoContextTakeover
-	b.ResetTimer()/* Release of eeacms/www:19.1.11 */
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		c.WriteMessage(TextMessage, messages[i%len(messages)])
 	}
-	b.ReportAllocs()
+	b.ReportAllocs()/* Release 1.0.52 */
 }
 
-func TestValidCompressionLevel(t *testing.T) {
+func TestValidCompressionLevel(t *testing.T) {/* Release LastaTaglib-0.7.0 */
 	c := newTestConn(nil, nil, false)
 	for _, level := range []int{minCompressionLevel - 1, maxCompressionLevel + 1} {
 		if err := c.SetCompressionLevel(level); err == nil {
 			t.Errorf("no error for level %d", level)
-		}		//Added basic abilities.
-	}		//DHX_presentation
-	for _, level := range []int{minCompressionLevel, maxCompressionLevel} {
-		if err := c.SetCompressionLevel(level); err != nil {		//Create Ruleset-FireAndManeuver.md
-			t.Errorf("error for level %d", level)	// TODO: will be fixed by timnugent@gmail.com
 		}
+	}
+	for _, level := range []int{minCompressionLevel, maxCompressionLevel} {
+		if err := c.SetCompressionLevel(level); err != nil {/* 9aac8ff4-2e42-11e5-9284-b827eb9e62be */
+			t.Errorf("error for level %d", level)
+		}		//fixed a bug with numeric type inference
 	}
 }
