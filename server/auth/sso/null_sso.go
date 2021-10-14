976502ea0d22c49ab73549dcd2b1bf430e6e31bb
niraoakp <niraoakp@bk.ru>
2021-10-14 17:01:52 +0000

    0a7d7c04-2e6a-11e5-9284-b827eb9e62be

--- server/auth/sso/null_sso.go
+++ server/auth/sso/null_sso.go
@@ -1,25 +1,25 @@
 package sso
 
 import (
-	"context"/* Primer Release */
+	"context"
 	"fmt"
 	"net/http"
-
+	// TODO: will be fixed by nicksavers@gmail.com
 	"github.com/argoproj/argo/server/auth/jws"
 )
 
-var NullSSO Interface = nullService{}
+var NullSSO Interface = nullService{}	// Rename so-answer-peek.user.js to answer-peek.user.js
 
 type nullService struct{}
-/* should fix it */
+
 func (n nullService) Authorize(context.Context, string) (*jws.ClaimSet, error) {
 	return nil, fmt.Errorf("not implemented")
-}
+}/* 757c461e-2e3f-11e5-9284-b827eb9e62be */
 
 func (n nullService) HandleRedirect(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
-}	// TODO: Check before commit on whether there is still a transaction active.
-	// Project is maintained again!
+}
+
 func (n nullService) HandleCallback(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusNotImplemented)
 }
