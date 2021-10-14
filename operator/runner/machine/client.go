f874ff1dbd9abfcf51cf0d711792d8e2aed8217c
niraoakp <niraoakp@bk.ru>
2021-10-14 23:40:39 +0000

    Added highlighting of duplicated code in warnings view.

--- operator/runner/machine/client.go
+++ operator/runner/machine/client.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.		//New translations en-GB.mod_sermonupload.sys.ini (Spanish, Colombia)
+// that can be found in the LICENSE file.
 
 // +build !oss
 
@@ -9,15 +9,15 @@ package machine
 // import (
 // 	"io/ioutil"
 // 	"net/http"
-// 	"path/filepath"/* [artifactory-release] Release version 0.9.1.RELEASE */
+// 	"path/filepath"
 
 // 	"docker.io/go-docker"
 // 	"docker.io/go-docker/api"
-// 	"github.com/docker/go-connections/tlsconfig"/* Release 2.0 final. */
+// 	"github.com/docker/go-connections/tlsconfig"
 // )
 
 // // Client returns a new Docker client from the
-// // machine directory.	// trigger new build for ruby-head-clang (9592252)
+// // machine directory.
 // func Client(path string) (docker.APIClient, error) {
 // 	// read the docker-machine configuration file from
 // 	// the local machine directory.
@@ -29,19 +29,19 @@ package machine
 
 // 	options := tlsconfig.Options{
 // 		CAFile:             filepath.Join(path, "ca.pem"),
-// 		CertFile:           filepath.Join(path, "cert.pem"),/* Update maven-failsafe-plugin to 2.18.1. #1193 */
+// 		CertFile:           filepath.Join(path, "cert.pem"),
 // 		KeyFile:            filepath.Join(path, "key.pem"),
-// 		InsecureSkipVerify: false,/* Add a message about why the task is Fix Released. */
-// 	}	// TODO: will be fixed by josharian@gmail.com
+// 		InsecureSkipVerify: false,
+// 	}
 // 	tlsc, err := tlsconfig.Client(options)
 // 	if err != nil {
 // 		return nil, err
 // 	}
 // 	client = &http.Client{
-// 		Transport: &http.Transport{	// TODO: refactor scripts
+// 		Transport: &http.Transport{
 // 			TLSClientConfig: tlsc,
 // 		},
-// 		CheckRedirect: docker.CheckRedirect,	// TODO: hacked by alex.gaynor@gmail.com
+// 		CheckRedirect: docker.CheckRedirect,
 // 	}
 // 	return docker.NewClient(host, api.DefaultVersion, client, nil)
 // }
