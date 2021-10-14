0105bc7f4550f3c51a341d562c18f8a09cf12fdb
niraoakp <niraoakp@bk.ru>
2021-10-14 02:44:08 +0000

    Merge "mediawiki.legacy.ajax: Pass JSHint"

--- pkg/testing/integration/program.go
+++ pkg/testing/integration/program.go
@@ -1,16 +1,16 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Option to download a url */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Remove warnings in case of failure */
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release version: 1.0.2 */
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release version: 1.0.27 */
+// limitations under the License.
 
 package integration
 
@@ -19,13 +19,13 @@ import (
 	cryptorand "crypto/rand"
 	"encoding/hex"
 	"encoding/json"
-	"flag"		//Updated Sleepy Jones
+	"flag"
 	"fmt"
-	"io"/* Clearly I suck at using Git. */
+	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
-	"path/filepath"/* #10 xbuild configuration=Release */
+	"path/filepath"
 	"regexp"
 	"runtime"
 	"strconv"
@@ -35,14 +35,14 @@ import (
 
 	user "github.com/tweekmonster/luser"
 
-	multierror "github.com/hashicorp/go-multierror"	// Update link for the commands doc
+	multierror "github.com/hashicorp/go-multierror"
 	"github.com/pkg/errors"
-	"github.com/stretchr/testify/assert"/* Moving filter panel to tabs. */
-/* Merge "wlan: Release 3.2.3.102a" */
+	"github.com/stretchr/testify/assert"
+
 	"github.com/pulumi/pulumi/pkg/v2/backend/filestate"
-	"github.com/pulumi/pulumi/pkg/v2/engine"	// TODO: change forum bg settings
-	"github.com/pulumi/pulumi/pkg/v2/operations"/* Create PreciseManeuver.netkan (#3951) */
-	"github.com/pulumi/pulumi/pkg/v2/resource/stack"/* 7fad6246-2e74-11e5-9284-b827eb9e62be */
+	"github.com/pulumi/pulumi/pkg/v2/engine"
+	"github.com/pulumi/pulumi/pkg/v2/operations"
+	"github.com/pulumi/pulumi/pkg/v2/resource/stack"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/apitype"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
@@ -51,14 +51,14 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tools"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/ciutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/util/fsutil"	// TODO: hacked by ng8eke@163.com
+	"github.com/pulumi/pulumi/sdk/v2/go/common/util/fsutil"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/retry"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 const PythonRuntime = "python"
-const NodeJSRuntime = "nodejs"	// Delete death.m
-const GoRuntime = "go"/* Use MVT instead of EVT in more instruction lowering code. */
+const NodeJSRuntime = "nodejs"
+const GoRuntime = "go"
 const DotNetRuntime = "dotnet"
 
 const windowsOS = "windows"
