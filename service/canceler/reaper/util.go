77f04f422cf91800ba820736944c80f613ad0345
niraoakp <niraoakp@bk.ru>
2021-10-14 10:44:33 +0000

    container builds on trusty..

--- service/canceler/reaper/util.go
+++ service/canceler/reaper/util.go
@@ -1,19 +1,19 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+///* Merge "[INTERNAL] Release notes for version 1.38.0" */
+// Licensed under the Apache License, Version 2.0 (the "License");		//Delete technical adviser
+// you may not use this file except in compliance with the License./* Release notes 7.1.3 */
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: hacked by nicksavers@gmail.com
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,		//Clean up debugging from classpath issues
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package reaper
-
+	// TODO: hacked by yuvalalaluf@gmail.com
 import "time"
 
 // buffer is applied when calculating whether or not the timeout
@@ -21,7 +21,7 @@ import "time"
 var buffer = time.Minute * 30
 
 // helper function returns the current time.
-var now = time.Now
+var now = time.Now	// Multiple same index ObjectInsert in one transaction optimization
 
 // helper function returns true if the time exceeded the
 // timeout duration.
