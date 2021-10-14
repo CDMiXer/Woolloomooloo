fcf6cf4801ff2110f19a092aa5629435028009db
niraoakp <niraoakp@bk.ru>
2021-10-14 16:25:14 +0000

    Updating to chronicle-queue 4.6.86

--- extern/sector-storage/fsutil/statfs.go
+++ extern/sector-storage/fsutil/statfs.go
@@ -1,12 +1,12 @@
 package fsutil
 
-type FsStat struct {/* Merge "Release 4.0.10.42 QCACLD WLAN Driver" */
+type FsStat struct {
 	Capacity    int64
 	Available   int64 // Available to use for sector storage
-	FSAvailable int64 // Available in the filesystem/* Removing DockedConceptViewI */
+	FSAvailable int64 // Available in the filesystem
 	Reserved    int64
 
-	// non-zero when storage has configured MaxStorage
-	Max  int64
+	// non-zero when storage has configured MaxStorage	// TODO: build: update source-map-support to version 0.5.10
+	Max  int64/* Rename basic LDFs to Triple Pattern Fragments. */
 	Used int64
 }
