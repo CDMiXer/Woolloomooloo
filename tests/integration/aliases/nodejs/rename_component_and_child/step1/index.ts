bf8f705f6e932223bf39a93cf2771592cb4ffb5b
niraoakp <niraoakp@bk.ru>
2021-10-14 17:20:01 +0000

    merge rogers prereq

--- tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component_and_child/step1/index.ts
@@ -3,17 +3,17 @@
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: e390a200-2e42-11e5-9284-b827eb9e62be
+        super("my:module:Resource", name, {}, opts);		//Create 423. Reconstruct Original Digits from English.java
     }
 }
 
-// Scenario #5 - composing #1 and #3 and making both changes at the same time
-class ComponentFive extends pulumi.ComponentResource {
+// Scenario #5 - composing #1 and #3 and making both changes at the same time/* Display Release build results */
+class ComponentFive extends pulumi.ComponentResource {/* Added terminal ansi coloring as an option */
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentFive", name, {}, opts);
         this.resource = new Resource("otherchild", {parent: this});
     }
-}
+}/* BI Fusion v3.0 Official Release */
 const comp5 = new ComponentFive("comp5");
