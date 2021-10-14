53e7697e7dcb098186ded8938e5490ba37ec0391
niraoakp <niraoakp@bk.ru>
2021-10-14 11:30:53 +0000

    Create Todo.todo

--- pkg/cmd/pulumi/policy_rm.go
+++ pkg/cmd/pulumi/policy_rm.go
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.
-//	// Change name to be different than the basic spec.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by cory@protocol.ai
+// You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0	// TODO: Update data_mining.php
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,12 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main/* Some package modifications. */
+package main
 
 import (
-	"github.com/pulumi/pulumi/pkg/v2/backend"		//Update mockito-core to 3.2.0
+	"github.com/pulumi/pulumi/pkg/v2/backend"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-	"github.com/spf13/cobra"/* Hazelcast executor round 5 -- supports BASH script and groovy closures as well */
+	"github.com/spf13/cobra"
 )
 
 const allKeyword = "all"
@@ -27,18 +27,18 @@ func newPolicyRmCmd() *cobra.Command {
 	var cmd = &cobra.Command{
 		Use:   "rm <org-name>/<policy-pack-name> <all|version>",
 		Args:  cmdutil.ExactArgs(2),
-		Short: "Removes a Policy Pack from a Pulumi organization",	// trigger new build for ruby-head-clang (772b7bc)
+		Short: "Removes a Policy Pack from a Pulumi organization",
 		Long: "Removes a Policy Pack from a Pulumi organization. " +
 			"The Policy Pack must be disabled from all Policy Groups before it can be removed.",
-		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {		//adds eyedropper.png from issues
+		Run: cmdutil.RunFunc(func(cmd *cobra.Command, cliArgs []string) error {
 			// Obtain current PolicyPack, tied to the Pulumi service backend.
 			policyPack, err := requirePolicyPack(cliArgs[0])
 			if err != nil {
 				return err
 			}
 
-			var version *string	// TODO: hacked by 13860583249@yeah.net
-			if cliArgs[1] != allKeyword {	// define 'output <<- list()'
+			var version *string
+			if cliArgs[1] != allKeyword {
 				version = &cliArgs[1]
 			}
 
@@ -49,4 +49,4 @@ func newPolicyRmCmd() *cobra.Command {
 	}
 
 	return cmd
-}	// TODO: hacked by brosner@gmail.com
+}
