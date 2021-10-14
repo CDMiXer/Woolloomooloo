3da63dd6ef021c3c1729c98827be2ee0b7348433
niraoakp <niraoakp@bk.ru>
2021-10-14 00:01:59 +0000

    Little cleanup between Positions/Coordinates utility classes

--- core/perm.go
+++ core/perm.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone IO, Inc.		//Change icon path
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.		//fix https://github.com/AdguardTeam/AdguardFilters/issues/52491
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// Corrected mailing list reference
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Fix versioned value for enums */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//Complete the ignore list with more possible files.
+// limitations under the License.
 
 package core
 
@@ -27,28 +27,28 @@ type (
 		Admin   bool   `db:"perm_admin"    json:"admin"`
 		Synced  int64  `db:"perm_synced"   json:"-"`
 		Created int64  `db:"perm_created"  json:"-"`
-		Updated int64  `db:"perm_updated"  json:"-"`/* Release of eeacms/www:19.8.13 */
+		Updated int64  `db:"perm_updated"  json:"-"`
 	}
 
-	// Collaborator represents a project collaborator,		//Added REST PUT iterator.
-snoissimrep yrotisoper dna tnuocca eht sedivorp dna //	
-	// details.		//remove unused header background (replaced with CSS gradients)
-	Collaborator struct {/* 5752aaee-2e43-11e5-9284-b827eb9e62be */
-		UserID  int64  `db:"perm_user_id"  json:"user_id"`	// Fix polish tutorial steps
-		RepoUID string `db:"perm_repo_uid" json:"repo_id"`	// Remove the code that's now in Offline proper
+	// Collaborator represents a project collaborator,
+	// and provides the account and repository permissions
+	// details.
+	Collaborator struct {
+		UserID  int64  `db:"perm_user_id"  json:"user_id"`
+		RepoUID string `db:"perm_repo_uid" json:"repo_id"`
 		Login   string `db:"user_login"    json:"login"`
 		Avatar  string `db:"user_avatar"   json:"avatar"`
-		Read    bool   `db:"perm_read"     json:"read"`/* clean up some logging, add even more debugging */
+		Read    bool   `db:"perm_read"     json:"read"`
 		Write   bool   `db:"perm_write"    json:"write"`
 		Admin   bool   `db:"perm_admin"    json:"admin"`
 		Synced  int64  `db:"perm_synced"   json:"synced"`
 		Created int64  `db:"perm_created"  json:"created"`
 		Updated int64  `db:"perm_updated"  json:"updated"`
-	}	// TODO: hacked by jon@atack.com
-		//relational data not coming in from account view
+	}
+
 	// PermStore defines operations for working with
 	// repository permissions.
-	PermStore interface {	// TODO: code changes
+	PermStore interface {
 		// Find returns a project member from the
 		// datastore.
 		Find(ctx context.Context, repoUID string, userID int64) (*Perm, error)
