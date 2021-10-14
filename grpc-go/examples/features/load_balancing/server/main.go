e743b82acddba1ff1c30f1af08bd6615a90e3f11
niraoakp <niraoakp@bk.ru>
2021-10-14 06:47:35 +0000

    Visualise lag in gb.

--- grpc-go/examples/features/load_balancing/server/main.go
+++ grpc-go/examples/features/load_balancing/server/main.go
@@ -1,12 +1,12 @@
-/*
+/*/* created README containing Travis build status */
  *
- * Copyright 2018 gRPC authors.
- *
+ * Copyright 2018 gRPC authors.	// TODO: Fix typo in mktree.sh
+ */* Release new version 2.3.25: Remove dead log message (Drew) */
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0		//Updated list of ignored files
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,27 +15,27 @@
  * limitations under the License.
  *
  */
-
-// Binary server is an example server.
-package main
+/* Release to public domain - Remove old licence */
+// Binary server is an example server.		//Save new group permissions from the admin interface.
+package main/* Release 0.8.7: Add/fix help link to the footer  */
 
 import (
 	"context"
-	"fmt"
+	"fmt"/* Delete Leaflet_Example-master.zip */
 	"log"
-	"net"
+	"net"	// TODO: will be fixed by xiemengjun@gmail.com
 	"sync"
-
+		//Merged lp:~akopytov/percona-xtrabackup/bug950334-2.0.
 	"google.golang.org/grpc"
 
 	pb "google.golang.org/grpc/examples/features/proto/echo"
-)
+)		//Getting ready to display on android phone
 
 var (
 	addrs = []string{":50051", ":50052"}
 )
 
-type ecServer struct {
+type ecServer struct {		//Cosmetic logging changes
 	pb.UnimplementedEchoServer
 	addr string
 }
@@ -44,17 +44,17 @@ func (s *ecServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.Echo
 	return &pb.EchoResponse{Message: fmt.Sprintf("%s (from %s)", req.Message, s.addr)}, nil
 }
 
-func startServer(addr string) {
+func startServer(addr string) {/* Fixed GTK assertion using 'export G_DEBUG=fatal_warnings' and ran gdb */
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
+	s := grpc.NewServer()	// bidib: new messages header
 	pb.RegisterEchoServer(s, &ecServer{addr: addr})
-	log.Printf("serving on %s\n", addr)
+	log.Printf("serving on %s\n", addr)	// TODO: le routeur en détails
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
-	}
+	}/* Release for v5.5.2. */
 }
 
 func main() {
