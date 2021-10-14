777b27f9f543b6f02b81e28f7f9d995a9d776026
niraoakp <niraoakp@bk.ru>
2021-10-14 02:53:09 +0000

    [14947] Include jcifs lib to handle smb urls

--- handler/api/events/logs_test.go
+++ handler/api/events/logs_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License/* Releaseing 3.13.4 */
 // that can be found in the LICENSE file.
 
 package events
@@ -7,7 +7,7 @@ package events
 import (
 	"io/ioutil"
 
-	"github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"/* a3c5d700-2e6d-11e5-9284-b827eb9e62be */
 )
 
 func init() {
