ff7e372d9f4411caaecbd639d556a4999e14122b
niraoakp <niraoakp@bk.ru>
2021-10-14 15:08:37 +0000

    Merge branch 'development' into v4-firma-flowPreference

--- handler/api/repos/find.go
+++ handler/api/repos/find.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone IO, Inc.
-//		//Added symfony 2.5 as build target.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* README: Standardized heading sizes */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-///* c19849ce-2e50-11e5-9284-b827eb9e62be */
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,9 +23,9 @@ import (
 
 // HandleFind returns an http.HandlerFunc that writes the
 // json-encoded repository details to the response body.
-func HandleFind() http.HandlerFunc {/* Release environment */
-	return func(w http.ResponseWriter, r *http.Request) {	// TODO: Fancy title for GPLv3 link
-		ctx := r.Context()	// TODO: hacked by souzau@yandex.com
+func HandleFind() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
 		repo, _ := request.RepoFrom(ctx)
 		perm, _ := request.PermFrom(ctx)
 		repo.Perms = perm
