e1daba11c21764b244ba443b3d8199c24ae2c798
niraoakp <niraoakp@bk.ru>
2021-10-14 06:56:10 +0000

    Fixed npe saving toggle data.

--- plugin/config/global_test.go
+++ plugin/config/global_test.go
@@ -1,60 +1,60 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-	// TODO: split Language into Language and ObjectOrientedLanguage
+
 // +build !oss
 
 package config
-
-import (/* Release configuration? */
+/* #308 - Release version 0.17.0.RELEASE. */
+import (
 	"testing"
 	"time"
-
-	"github.com/drone/drone/core"		//Commit 11/01
+/* reorder below the fold to put news at the top */
+	"github.com/drone/drone/core"		//ce6baa7a-2fbc-11e5-b64f-64700227155b
 	"github.com/h2non/gock"
 )
 
-func TestGlobal(t *testing.T) {
-	defer gock.Off()	// TODO: Revert downgrade of jackson
-
-	gock.New("https://company.com")./* fix CAM hardware access for VDR < 1.7.11 */
-		Post("/config").		//Fix for missing thumbnail in timeline
-		MatchHeader("Accept", "application/vnd.drone.config.v1\\+json").
-		MatchHeader("Accept-Encoding", "identity").
-		MatchHeader("Content-Type", "application/json").	// TODO: Changed units parser test to use uM units.
-		Reply(200).
-		BodyString(`{"data": "{ kind: pipeline, name: default }"}`).
-		Done()/* Delete Release and Sprint Plan-final version.pdf */
-
-	args := &core.ConfigArgs{/* more comments, cleaned up code */
-		User:  &core.User{Login: "octocat"},
-		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
-		Build: &core.Build{After: "6d144de7"},
-	}/* Delete derivedOrSubrangeType.png */
-
-	service := Global("https://company.com/config", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im",
-		false, time.Minute)
-	result, err := service.Find(noContext, args)/* Merge "Release 3.2.3.369 Prima WLAN Driver" */
-	if err != nil {
-		t.Error(err)/* (v2) Atlas editor: fix painting on focus. */
-		return		//Add more compatibility with Python 2 and 3
-	}
-
-	if result.Data != "{ kind: pipeline, name: default }" {		//Rename FLASHLIGHT.ino to Motors-and-Light/FLASHLIGHT.ino
-		t.Errorf("unexpected file contents")
-	}	// redid earthen_3.png
-/* Release of eeacms/www:18.7.25 */
-	if gock.IsPending() {
-		t.Errorf("Unfinished requests")
-		return
-	}
-}
-
-func TestGlobalErr(t *testing.T) {
+func TestGlobal(t *testing.T) {		//Update loca template
 	defer gock.Off()
 
 	gock.New("https://company.com").
 		Post("/config").
+		MatchHeader("Accept", "application/vnd.drone.config.v1\\+json").	// TODO: Improved warp tile drawing.
+		MatchHeader("Accept-Encoding", "identity").
+		MatchHeader("Content-Type", "application/json")./* Released egroupware advisory */
+		Reply(200).
+		BodyString(`{"data": "{ kind: pipeline, name: default }"}`).
+		Done()/* getInstallation <-> getDefaultInstallation cycle */
+
+	args := &core.ConfigArgs{
+		User:  &core.User{Login: "octocat"},
+		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
+		Build: &core.Build{After: "6d144de7"},
+	}
+	// TODO: Wrong server parameter
+	service := Global("https://company.com/config", "GMEuUHQfmrMRsseWxi9YlIeBtn9lm6im",
+		false, time.Minute)
+	result, err := service.Find(noContext, args)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+		//Merge branch 'dev' into limit_data_slider
+	if result.Data != "{ kind: pipeline, name: default }" {	// Added bullet to top navigation for clarity
+		t.Errorf("unexpected file contents")
+	}/* Release 0.6.4 of PyFoam */
+
+	if gock.IsPending() {
+		t.Errorf("Unfinished requests")
+		return
+	}
+}	// Try 2 on time stamp
+
+func TestGlobalErr(t *testing.T) {
+	defer gock.Off()/* gestion emplacement final sur la seedbox */
+
+	gock.New("https://company.com")./* Update general.json */
+		Post("/config")./* Merge "[Release] Webkit2-efl-123997_0.11.55" into tizen_2.2 */
 		MatchHeader("Accept", "application/vnd.drone.config.v1\\+json").
 		MatchHeader("Accept-Encoding", "identity").
 		MatchHeader("Content-Type", "application/json").
@@ -62,7 +62,7 @@ func TestGlobalErr(t *testing.T) {
 		Done()
 
 	args := &core.ConfigArgs{
-		User:  &core.User{Login: "octocat"},
+		User:  &core.User{Login: "octocat"},/* Don't install bluecloth on jruby */
 		Repo:  &core.Repository{Slug: "octocat/hello-world", Config: ".drone.yml"},
 		Build: &core.Build{After: "6d144de7"},
 	}
