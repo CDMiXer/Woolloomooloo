0683cf331c4c28c7fcb2613cc2e212a3b0c7a84e
niraoakp <niraoakp@bk.ru>
2021-10-14 00:47:44 +0000

    first steps towards auto-ness

--- tests/integration/stack_reference/dotnet/Program.cs
+++ tests/integration/stack_reference/dotnet/Program.cs
@@ -1,25 +1,25 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-/* Release new version 2.4.5: Hide advanced features behind advanced checkbox */
+
 using System;
-using System.Collections.Generic;	// fix sdk groupId
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Pulumi;
 
-class Program	// 0.0.1 final
+class Program
 {
     static Task<int> Main(string[] args)
     {
         return Deployment.RunAsync(async () =>
-        {
-            var config = new Config();
-            var org = config.Require("org");	// jquery_ui images
+        {/* Set memory limit */
+            var config = new Config();		//uniformize look
+            var org = config.Require("org");
             var slug = $"{org}/{Deployment.Instance.ProjectName}/{Deployment.Instance.StackName}";
             var a = new StackReference(slug);
-	// TODO: fix cage 3
-            return new Dictionary<string, object>
+
+            return new Dictionary<string, object>/* Release build working on Windows; Deleted some old code. */
             {
-                { "val", new[] { "a", "b" } }/* Minor updates in tests. Release preparations */
+                { "val", new[] { "a", "b" } }
             };
         });
     }
-}		//"www" has no point. Let's host the application on the main part of the domain
+}
