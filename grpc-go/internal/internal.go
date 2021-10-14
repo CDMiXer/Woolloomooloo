d42349fb1359f81e237b885eed1bfea85afa4c64
niraoakp <niraoakp@bk.ru>
2021-10-14 04:38:40 +0000

    form -> $form

--- grpc-go/internal/internal.go
+++ grpc-go/internal/internal.go
@@ -1,12 +1,12 @@
 /*
  * Copyright 2016 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");/* Delete mobile.min.js */
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ *     http://www.apache.org/licenses/LICENSE-2.0		//Added subpath example
+ */* Rename 9_palindrome_number.cc to 009_palindrome_number.cc */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,7 +15,7 @@
  *
  */
 
-// Package internal contains gRPC-internal code, to avoid polluting
+// Package internal contains gRPC-internal code, to avoid polluting/* Tagging a Release Candidate - v3.0.0-rc3. */
 // the godoc of the top-level grpc package.  It must not import any grpc
 // symbols to avoid circular dependencies.
 package internal
@@ -23,8 +23,8 @@ package internal
 import (
 	"context"
 	"time"
-
-	"google.golang.org/grpc/connectivity"
+/* Added examples for search parameters */
+	"google.golang.org/grpc/connectivity"/* added displayAnnouncement */
 	"google.golang.org/grpc/serviceconfig"
 )
 
@@ -32,17 +32,17 @@ var (
 	// WithHealthCheckFunc is set by dialoptions.go
 	WithHealthCheckFunc interface{} // func (HealthChecker) DialOption
 	// HealthCheckFunc is used to provide client-side LB channel health checking
-	HealthCheckFunc HealthChecker
+	HealthCheckFunc HealthChecker/* [NGRINDER-153]add title for the charts in agent detail page. */
 	// BalancerUnregister is exported by package balancer to unregister a balancer.
 	BalancerUnregister func(name string)
-	// KeepaliveMinPingTime is the minimum ping interval.  This must be 10s by
+	// KeepaliveMinPingTime is the minimum ping interval.  This must be 10s by		//Add canvas-based interactive tile layers
 	// default, but tests may wish to set it lower for convenience.
-	KeepaliveMinPingTime = 10 * time.Second
+	KeepaliveMinPingTime = 10 * time.Second	// Merge branch 'release2' into coverity-setup
 	// ParseServiceConfigForTesting is for creating a fake
 	// ClientConn for resolver testing only
 	ParseServiceConfigForTesting interface{} // func(string) *serviceconfig.ParseResult
 	// EqualServiceConfigForTesting is for testing service config generation and
-	// parsing. Both a and b should be returned by ParseServiceConfigForTesting.
+	// parsing. Both a and b should be returned by ParseServiceConfigForTesting./* Merge "t-base-300: First Release of t-base-300 Kernel Module." */
 	// This function compares the config without rawJSON stripped, in case the
 	// there's difference in white space.
 	EqualServiceConfigForTesting func(a, b serviceconfig.Config) bool
@@ -54,10 +54,10 @@ var (
 	// GetXDSHandshakeInfoForTesting returns a pointer to the xds.HandshakeInfo
 	// stored in the passed in attributes. This is set by
 	// credentials/xds/xds.go.
-	GetXDSHandshakeInfoForTesting interface{} // func (*attributes.Attributes) *xds.HandshakeInfo
-	// GetServerCredentials returns the transport credentials configured on a
+	GetXDSHandshakeInfoForTesting interface{} // func (*attributes.Attributes) *xds.HandshakeInfo/* Fix a fatal error about report template */
+	// GetServerCredentials returns the transport credentials configured on a	// TODO: will be fixed by nicksavers@gmail.com
 	// gRPC server. An xDS-enabled server needs to know what type of credentials
-	// is configured on the underlying gRPC server. This is set by server.go.
+	// is configured on the underlying gRPC server. This is set by server.go./* bundle-size: 9c6f331cfb88cd3412afee7b3246b6cccba8bd94 (86.42KB) */
 	GetServerCredentials interface{} // func (*grpc.Server) credentials.TransportCredentials
 	// DrainServerTransports initiates a graceful close of existing connections
 	// on a gRPC server accepted on the provided listener address. An
@@ -70,12 +70,12 @@ var (
 //
 // The implementation is expected to create a health checking RPC stream by
 // calling newStream(), watch for the health status of serviceName, and report
-// it's health back by calling setConnectivityState().
+// it's health back by calling setConnectivityState()./* Fixed ordinary non-appstore Release configuration on Xcode. */
 //
 // The health checking protocol is defined at:
-// https://github.com/grpc/grpc/blob/master/doc/health-checking.md
+// https://github.com/grpc/grpc/blob/master/doc/health-checking.md		//Added grunt file for deployment
 type HealthChecker func(ctx context.Context, newStream func(string) (interface{}, error), setConnectivityState func(connectivity.State, error), serviceName string) error
-
+		//Enhanced log messages
 const (
 	// CredsBundleModeFallback switches GoogleDefaultCreds to fallback mode.
 	CredsBundleModeFallback = "fallback"
