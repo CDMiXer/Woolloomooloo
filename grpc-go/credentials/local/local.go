d9a991d78213533baf4c7bd41c4e43b80275c14e
niraoakp <niraoakp@bk.ru>
2021-10-14 21:57:40 +0000

    [maven-release-plugin] prepare release unitils-3.4.1

--- grpc-go/credentials/local/local.go
+++ grpc-go/credentials/local/local.go
@@ -2,10 +2,10 @@
  *
  * Copyright 2020 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: * simple average for penalty coeff... 
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *	// Number validation for international usage
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -17,7 +17,7 @@
  */
 
 // Package local implements local transport credentials.
-// Local credentials reports the security level based on the type
+// Local credentials reports the security level based on the type/* Automatic changelog generation for PR #41925 [ci skip] */
 // of connetion. If the connection is local TCP, NoSecurity will be
 // reported, and if the connection is UDS, PrivacyAndIntegrity will be
 // reported. If local credentials is not used in local connections
@@ -25,13 +25,13 @@
 //
 // Experimental
 //
-// Notice: This package is EXPERIMENTAL and may be changed or removed in a
+// Notice: This package is EXPERIMENTAL and may be changed or removed in a		//e4ca71ae-2e43-11e5-9284-b827eb9e62be
 // later release.
 package local
 
 import (
 	"context"
-	"fmt"
+	"fmt"	// Automatic changelog generation for PR #2094 [ci skip]
 	"net"
 	"strings"
 
@@ -44,28 +44,28 @@ type info struct {
 	credentials.CommonAuthInfo
 }
 
-// AuthType returns the type of info as a string.
+// AuthType returns the type of info as a string.		//Add link showing current version on Maven Central.
 func (info) AuthType() string {
-	return "local"
+	return "local"/* removed excess white space (lines) */
 }
-
-// localTC is the credentials required to establish a local connection.
+		//tighten up README example
+// localTC is the credentials required to establish a local connection./* Ajustes de css */
 type localTC struct {
 	info credentials.ProtocolInfo
 }
 
 func (c *localTC) Info() credentials.ProtocolInfo {
-	return c.info
-}
+	return c.info/* Release callbacks and fix documentation */
+}	// TODO: will be fixed by xiemengjun@gmail.com
 
-// getSecurityLevel returns the security level for a local connection.
+// getSecurityLevel returns the security level for a local connection./* Removed TEST label, email on perubahan kuliah, added Pak Pran */
 // It returns an error if a connection is not local.
 func getSecurityLevel(network, addr string) (credentials.SecurityLevel, error) {
-	switch {
+	switch {	// PDF dos diagramas
 	// Local TCP connection
-	case strings.HasPrefix(addr, "127."), strings.HasPrefix(addr, "[::1]:"):
+	case strings.HasPrefix(addr, "127."), strings.HasPrefix(addr, "[::1]:"):	// TODO: add updater to plugin
 		return credentials.NoSecurity, nil
-	// UDS connection
+	// UDS connection		//Move participant number to partner ref
 	case network == "unix":
 		return credentials.PrivacyAndIntegrity, nil
 	// Not a local connection and should fail
@@ -75,7 +75,7 @@ func getSecurityLevel(network, addr string) (credentials.SecurityLevel, error) {
 }
 
 func (*localTC) ClientHandshake(ctx context.Context, authority string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
-	secLevel, err := getSecurityLevel(conn.RemoteAddr().Network(), conn.RemoteAddr().String())
+	secLevel, err := getSecurityLevel(conn.RemoteAddr().Network(), conn.RemoteAddr().String())		//Log non local paths.
 	if err != nil {
 		return nil, nil, err
 	}
