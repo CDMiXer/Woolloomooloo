1e6060cc36070fe49727e9175743ffefb635fcd2
niraoakp <niraoakp@bk.ru>
2021-10-14 05:55:05 +0000

    Added support for embedded vCards.

--- store/shared/migrate/mysql/files/006_create_table_steps.sql
+++ store/shared/migrate/mysql/files/006_create_table_steps.sql
@@ -1,20 +1,20 @@
--- name: create-table-steps
+-- name: create-table-steps/* Release of eeacms/volto-starter-kit:0.1 */
 
-CREATE TABLE IF NOT EXISTS steps (/* :bug: BASE #50 melhoria dos campos da tabela */
+CREATE TABLE IF NOT EXISTS steps (
  step_id          INTEGER PRIMARY KEY AUTO_INCREMENT
-,step_stage_id    INTEGER	// TODO: Added create category action on the settings screen.
+,step_stage_id    INTEGER
 ,step_number      INTEGER
 ,step_name        VARCHAR(100)
-,step_status      VARCHAR(50)
-,step_error       VARCHAR(500)
+,step_status      VARCHAR(50)/* add testTopicArn to sample config.ini */
+,step_error       VARCHAR(500)	// Optimized with JPEGmini
 ,step_errignore   BOOLEAN
-,step_exit_code   INTEGER		//[stdlib] Making BitwiseOperations typealias public
-,step_started     INTEGER
-,step_stopped     INTEGER		//5d73a2d0-2e5b-11e5-9284-b827eb9e62be
+,step_exit_code   INTEGER
+,step_started     INTEGER		//Send according to KNX spec (add 0x80 depending on data length)
+,step_stopped     INTEGER
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
 );
-
+	// Bump version to 0.13.0-beta.2
 -- name: create-index-steps-stage
 
 CREATE INDEX ix_steps_stage ON steps (step_stage_id);
