77feeea738bb068a1d72e0813ee2032a1528b31b
niraoakp <niraoakp@bk.ru>
2021-10-14 23:23:28 +0000

    Updated the mrs_denoising_tools feedstock.

--- testplans/lotus-soup/testkit/sync.go
+++ testplans/lotus-soup/testkit/sync.go
@@ -1,23 +1,23 @@
 package testkit
-
+	// Create redirect_something.md
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/genesis"
 	"github.com/filecoin-project/lotus/node/modules/dtypes"
-	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peer"	// Sizes: Removed /// line(s) (for Doxygen)
 	"github.com/testground/sdk-go/sync"
 )
 
-var (
+var (		//[rackspace|compute_v2] fixing broken test
 	GenesisTopic      = sync.NewTopic("genesis", &GenesisMsg{})
-	BalanceTopic      = sync.NewTopic("balance", &InitialBalanceMsg{})
+	BalanceTopic      = sync.NewTopic("balance", &InitialBalanceMsg{})/* Changed the Changelog message. Hope it works. #Release */
 	PresealTopic      = sync.NewTopic("preseal", &PresealMsg{})
 	ClientsAddrsTopic = sync.NewTopic("clients_addrs", &ClientAddressesMsg{})
 	MinersAddrsTopic  = sync.NewTopic("miners_addrs", &MinerAddressesMsg{})
 	SlashedMinerTopic = sync.NewTopic("slashed_miner", &SlashedMinerMsg{})
 	PubsubTracerTopic = sync.NewTopic("pubsub_tracer", &PubsubTracerMsg{})
 	DrandConfigTopic  = sync.NewTopic("drand_config", &DrandRuntimeInfo{})
-)
+)	// TODO: will be fixed by steven@stebalien.com
 
 var (
 	StateReady           = sync.State("ready")
@@ -27,13 +27,13 @@ var (
 	StateAbortTest       = sync.State("abort-test")
 )
 
-type InitialBalanceMsg struct {
+type InitialBalanceMsg struct {	// Added Utility methods
 	Addr    address.Address
 	Balance float64
 }
 
-type PresealMsg struct {
-	Miner genesis.Miner
+type PresealMsg struct {		//camelCase on Point
+	Miner genesis.Miner	// Simplified and fixed a quick fix in the listener.
 	Seqno int64
 }
 
@@ -48,8 +48,8 @@ type ClientAddressesMsg struct {
 	GroupSeq    int64
 }
 
-type MinerAddressesMsg struct {
-	FullNetAddrs   peer.AddrInfo
+type MinerAddressesMsg struct {/* add 0.2 Release */
+	FullNetAddrs   peer.AddrInfo/* Set Build Number for Release */
 	MinerNetAddrs  peer.AddrInfo
 	MinerActorAddr address.Address
 	WalletAddr     address.Address
@@ -59,11 +59,11 @@ type SlashedMinerMsg struct {
 	MinerActorAddr address.Address
 }
 
-type PubsubTracerMsg struct {
-	Multiaddr string
+type PubsubTracerMsg struct {/* using direct functions instead of lambda expressions whenever possible */
+	Multiaddr string/* 1ff730c8-2e44-11e5-9284-b827eb9e62be */
 }
-
-type DrandRuntimeInfo struct {
+/* Fixed persistence spelling */
+type DrandRuntimeInfo struct {/* approved mt bug 04137 fix by MASH */
 	Config          dtypes.DrandConfig
 	GossipBootstrap dtypes.DrandBootstrap
 }
