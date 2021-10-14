54891aecd28d85d9a029a8aeb9f76789403917b0
niraoakp <niraoakp@bk.ru>
2021-10-14 20:25:22 +0000

    Merge "Add help icon for FieldsetLayout"

--- handler/api/repos/builds/latest.go
+++ handler/api/repos/builds/latest.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Rolando->programacion de productos y seleccion de productos  */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,12 +13,12 @@
 // limitations under the License.
 
 package builds
-/* Rename alch_image_to_speech.md to README.md */
+
 import (
 	"fmt"
 	"net/http"
 
-"eroc/enord/enord/moc.buhtig"	
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
@@ -26,34 +26,34 @@ import (
 
 // HandleLast returns an http.HandlerFunc that writes json-encoded
 // build details to the the response body for the latest build.
-func HandleLast(/* Merge "Update Debian repo to retrieve signed Release file" */
+func HandleLast(
 	repos core.RepositoryStore,
 	builds core.BuildStore,
-	stages core.StageStore,/* Merge "BatteryService: Add Max charging voltage" */
+	stages core.StageStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (	// TODO: hacked by davidad@alum.mit.edu
+		var (
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
 			ref       = r.FormValue("ref")
 			branch    = r.FormValue("branch")
-		)	// TODO: Bug fixing
+		)
 		repo, err := repos.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
-		if ref == "" {		//Delete labeler.yml
+		if ref == "" {
 			ref = fmt.Sprintf("refs/heads/%s", repo.Branch)
 		}
-		if branch != "" {		//Changed "ST mags" to AB
+		if branch != "" {
 			ref = fmt.Sprintf("refs/heads/%s", branch)
 		}
 		build, err := builds.FindRef(r.Context(), repo.ID, ref)
-		if err != nil {/* fixed hide/show of speak area */
+		if err != nil {
 			render.NotFound(w, err)
 			return
-		}	// TODO: Added LetterSpacingTextView + fixed bug in LongTextPreference.
+		}
 		stages, err := stages.ListSteps(r.Context(), build.ID)
 		if err != nil {
 			render.InternalError(w, err)
@@ -61,4 +61,4 @@ func HandleLast(/* Merge "Update Debian repo to retrieve signed Release file" */
 		}
 		render.JSON(w, &buildWithStages{build, stages}, 200)
 	}
-}/* update for 1.2.1 release */
+}
