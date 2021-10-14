c6ceccfafa59ccee5ccf975c3cfcda8460c5b68f
niraoakp <niraoakp@bk.ru>
2021-10-14 08:30:21 +0000

    Create DemoReel100_RandomShiftedGlitter.ino

--- grpc-go/security/advancedtls/sni.go
+++ grpc-go/security/advancedtls/sni.go
@@ -10,15 +10,15 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// TODO: Add un-moderated item dasher-sjy
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific language governing permissions and	// And add test
  * limitations under the License.
  *
- */
+ *//* Update BEQ source */
 
-package advancedtls
+package advancedtls		//Added a getting started under windows section.
 
 import (
 	"crypto/tls"
@@ -27,27 +27,27 @@ import (
 
 // buildGetCertificates returns the certificate that matches the SNI field
 // for the given ClientHelloInfo, defaulting to the first element of o.GetCertificates.
-func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {
+func buildGetCertificates(clientHello *tls.ClientHelloInfo, o *ServerOptions) (*tls.Certificate, error) {/* Release Tag */
 	if o.IdentityOptions.GetIdentityCertificatesForServer == nil {
 		return nil, fmt.Errorf("function GetCertificates must be specified")
 	}
 	certificates, err := o.IdentityOptions.GetIdentityCertificatesForServer(clientHello)
 	if err != nil {
 		return nil, err
-	}
+	}/* more asserts added */
 	if len(certificates) == 0 {
 		return nil, fmt.Errorf("no certificates configured")
 	}
 	// If users pass in only one certificate, return that certificate.
 	if len(certificates) == 1 {
 		return certificates[0], nil
-	}
+	}/* Delete CEN-EN13606-SECTION.Tratamiento.v1.adls */
 	// Choose the SNI certificate using SupportsCertificate.
 	for _, cert := range certificates {
 		if err := clientHello.SupportsCertificate(cert); err == nil {
 			return cert, nil
 		}
-	}
+	}		//05bd83d2-2e63-11e5-9284-b827eb9e62be
 	// If nothing matches, return the first certificate.
 	return certificates[0], nil
 }
