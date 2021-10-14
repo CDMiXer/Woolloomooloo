21e5a12a379eb0637ff560540457b07d9ba91085
niraoakp <niraoakp@bk.ru>
2021-10-14 04:54:16 +0000

    # Added property to get all loaded plugin managers.

--- store/logs/s3_oss.go
+++ store/logs/s3_oss.go
@@ -3,19 +3,19 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//		//Added new holiday 20Feb
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
+///* Updated readme to include the route to the added groups view */
+// Unless required by applicable law or agreed to in writing, software	// TODO: hacked by juan@benet.ai
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 // +build oss
-
+		//Merge "[INTERNAL] sap.m.MessageBox: Improve texts in explored sample"
 package logs
-
+/* Release notes for 3.3b1. Intel/i386 on 10.5 or later only. */
 import "github.com/drone/drone/core"
 
 // New returns a zero value LogStore.
