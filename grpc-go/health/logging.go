1ef9d75e3dacff28e755386a7f9736081325e414
niraoakp <niraoakp@bk.ru>
2021-10-14 23:50:47 +0000

    Use new cursor movement methods

--- grpc-go/health/logging.go
+++ grpc-go/health/logging.go
@@ -1,17 +1,17 @@
-/*
+/*/* Release 3.3.1 */
  *
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License./* Release 0.0.7 */
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
+ */* Delete commentit.js */
+ * Unless required by applicable law or agreed to in writing, software/* chore(package): update terser-webpack-plugin to version 1.2.3 */
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: Fucking good bye useless time stamp
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
@@ -19,5 +19,5 @@
 package health
 
 import "google.golang.org/grpc/grpclog"
-/* Release 5.4-rc3 */
+
 var logger = grpclog.Component("health_service")
