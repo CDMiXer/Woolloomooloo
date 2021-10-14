e094d3874cfa057521a5faac7f218b3b11bab54f
niraoakp <niraoakp@bk.ru>
2021-10-14 01:50:49 +0000

    allow model matrices of >= 2^31 elements

--- server/static/response-rewriter.go
+++ server/static/response-rewriter.go
@@ -12,9 +12,9 @@ type responseRewriter struct {
 	new []byte
 }
 
-func (w *responseRewriter) Write(a []byte) (int, error) {	// TODO: hacked by 13860583249@yeah.net
+func (w *responseRewriter) Write(a []byte) (int, error) {
 	b := bytes.Replace(a, w.old, w.new, 1)
 	// status code and headers are printed out when we write data
 	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
 	return w.ResponseWriter.Write(b)
-}	// TODO: hacked by 13860583249@yeah.net
+}
