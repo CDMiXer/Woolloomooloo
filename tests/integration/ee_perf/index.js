a31ce2cba051ab9b45e2ca0f7052a238ecf3bc29
niraoakp <niraoakp@bk.ru>
2021-10-14 22:56:02 +0000

    [IMP] cleaning project/contracts and removing unused code

--- tests/integration/ee_perf/index.js
+++ tests/integration/ee_perf/index.js
@@ -1,14 +1,14 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-	// Fixed: Same value twice in log output.
+
 "use strict";
-const pulumi = require("@pulumi/pulumi");		//Merge branch 'development' into 0-width-band-fix
+const pulumi = require("@pulumi/pulumi");
 
 const config = new pulumi.Config();
 const iterations = config.getNumber("count") || 1000;
-/* modifying kvObsPgm to use boost posix_time */
+
 // Emit many, many diagnostic events from the engine to stress test the
 // ability to record those events on the Pulumi Service.
-console.log("Starting to spam a bunch of diagnostic messages...");	// TODO: Add a section on inter-process communication to the manual page.
+console.log("Starting to spam a bunch of diagnostic messages...");
 for (let i = 0; i < iterations; i++) {
     console.log(`${i}: The current time is ${new Date()}`);
 }
