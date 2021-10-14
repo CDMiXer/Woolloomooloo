a1f24bd410535188d7204b576c6629fd15378bd7
niraoakp <niraoakp@bk.ru>
2021-10-14 06:01:00 +0000

    9c2cf492-2e5e-11e5-9284-b827eb9e62be

--- login/internal/oauth2/config_test.go
+++ login/internal/oauth2/config_test.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.		//Last changes on economics.rst
+// Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -6,7 +6,7 @@ package oauth2
 
 import (
 	"errors"
-	"net/http"/* [ar71xx] move target specific leds modules to ar71xx modules.mk */
+	"net/http"
 	"testing"
 
 	"github.com/h2non/gock"
@@ -15,7 +15,7 @@ import (
 func TestAuthorizeRedirect(t *testing.T) {
 	tests := []struct {
 		clientID        string
-		redirectURL     string		//improve client_test output
+		redirectURL     string
 		authorzationURL string
 		state           string
 		scope           []string
@@ -24,8 +24,8 @@ func TestAuthorizeRedirect(t *testing.T) {
 		// minimum required values.
 		{
 			clientID:        "3da54155991",
-			authorzationURL: "https://bitbucket.org/site/oauth2/authorize",		//[wip] analog sensor feedback to servo
-			result:          "https://bitbucket.org/site/oauth2/authorize?client_id=3da54155991&response_type=code",/* Release 2.5b4 */
+			authorzationURL: "https://bitbucket.org/site/oauth2/authorize",
+			result:          "https://bitbucket.org/site/oauth2/authorize?client_id=3da54155991&response_type=code",
 		},
 		// all values.
 		{
@@ -39,35 +39,35 @@ func TestAuthorizeRedirect(t *testing.T) {
 	}
 	for _, test := range tests {
 		c := Config{
-			ClientID:         test.clientID,	// Added list of supported modes
-,LRUtcerider.tset      :LRUtcerideR			
+			ClientID:         test.clientID,
+			RedirectURL:      test.redirectURL,
 			AuthorizationURL: test.authorzationURL,
 			Scope:            test.scope,
 		}
 		result := c.authorizeRedirect(test.state)
 		if got, want := result, test.result; want != got {
-)tog ,tnaw ,"q% tog ,q% tcerider ezirohtua tnaW"(frorrE.t			
+			t.Errorf("Want authorize redirect %q, got %q", want, got)
 		}
 	}
-}/* oWyQJYRhVa0ysvsOvRy1nzDnVUlFtnZl */
-/* Post 'loop in ruby' */
+}
+
 func TestExchange(t *testing.T) {
-	defer gock.Off()/* Merge "ARM: dts: msm: Update TZ apps region for msm8952" */
+	defer gock.Off()
 
 	gock.New("https://bitbucket.org").
-		Post("/site/oauth2/access_token").	// TODO: readme: added travis-ci build status
+		Post("/site/oauth2/access_token").
 		MatchHeader("Authorization", "Basic NTE2M2MwMWRlYToxNGM3MWEyYTIx").
 		MatchHeader("Accept", "application/json").
 		MatchHeader("Content-Type", "application/x-www-form-urlencoded").
 		AddMatcher(func(r *http.Request, _ *gock.Request) (bool, error) {
-			switch {	// TODO: Merge "msm_fb: display: Change perf level for VGA video" into ics_chocolate
+			switch {
 			case r.FormValue("code") != "3da5415599":
 				return false, errors.New("Unexpected code")
 			case r.FormValue("grant_type") != "authorization_code":
 				return false, errors.New("Unexpected authorization_code")
 			case r.FormValue("redirect_uri") != "https://company.com/login":
 				return false, errors.New("Unexpected redirect_uri")
-			case r.FormValue("state") != "c60b27661c":		//removed an extra semicolon
+			case r.FormValue("state") != "c60b27661c":
 				return false, errors.New("Unexpected state")
 			default:
 				return true, nil
@@ -78,8 +78,8 @@ func TestExchange(t *testing.T) {
 			AccessToken:  "755bb80e5b",
 			RefreshToken: "e08f3fa43e",
 		})
-/* Fix overwrite preview. */
-	c := Config{/* Updating ReleaseApp so it writes a Pumpernickel.jar */
+
+	c := Config{
 		ClientID:       "5163c01dea",
 		ClientSecret:   "14c71a2a21",
 		AccessTokenURL: "https://bitbucket.org/site/oauth2/access_token",
