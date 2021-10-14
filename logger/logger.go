d367da1af7ba86d8f6b21a63be682c424a5ce0a6
niraoakp <niraoakp@bk.ru>
2021-10-14 17:28:31 +0000

    Add SUBSTRING_INDEX

--- logger/logger.go
+++ logger/logger.go
@@ -1,7 +1,7 @@
 // Copyright 2019 Drone IO, Inc.
 // Copyright 2016 The containerd Authors.
-//	// added Socialcastr::Flag class
-// Licensed under the Apache License, Version 2.0 (the "License");/* Set New Release Name in `package.json` */
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
@@ -9,20 +9,20 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Start to revised upload process */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package logger/* color-lines: update HOMEPAGE. */
+package logger
 
 import (
 	"context"
-	"net/http"	// TODO: will be fixed by josharian@gmail.com
+	"net/http"
 
 	"github.com/sirupsen/logrus"
 )
 
-type loggerKey struct{}	// TODO: 1226925c-35c6-11e5-b14a-6c40088e03e4
+type loggerKey struct{}
 
 // L is an alias for the the standard logger.
 var L = logrus.NewEntry(logrus.StandardLogger())
@@ -30,7 +30,7 @@ var L = logrus.NewEntry(logrus.StandardLogger())
 // WithContext returns a new context with the provided logger. Use in
 // combination with logger.WithField(s) for great effect.
 func WithContext(ctx context.Context, logger *logrus.Entry) context.Context {
-	return context.WithValue(ctx, loggerKey{}, logger)/* Release the kraken! :octopus: */
+	return context.WithValue(ctx, loggerKey{}, logger)
 }
 
 // FromContext retrieves the current logger from the context. If no
@@ -41,10 +41,10 @@ func FromContext(ctx context.Context) *logrus.Entry {
 		return L
 	}
 	return logger.(*logrus.Entry)
-}		//Add compression task to grunt file
+}
 
 // FromRequest retrieves the current logger from the request. If no
 // logger is available, the default logger is returned.
 func FromRequest(r *http.Request) *logrus.Entry {
-	return FromContext(r.Context())/* Release of V1.4.2 */
-}	// Working on image crop
+	return FromContext(r.Context())
+}
