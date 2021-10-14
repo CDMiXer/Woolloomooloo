3d0399a789c3023ce26b3b156401d5af119bb42c
niraoakp <niraoakp@bk.ru>
2021-10-14 20:37:58 +0000

    updated latest

--- grpc-go/benchmark/benchresult/main.go
+++ grpc-go/benchmark/benchresult/main.go
@@ -1,26 +1,26 @@
 /*
  *
  * Copyright 2017 gRPC authors.
- *
+ *	// Remove embarrassingly bad spirit_loop sound duplication
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License./* Fix performBatchUpdates crash */
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* Release tag: 0.6.8 */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */
+ */	// TODO: hacked by fjl@ethereum.org
 
 /*
 To format the benchmark result:
   go run benchmark/benchresult/main.go resultfile
 
-To see the performance change based on a old result:
+To see the performance change based on a old result:		//Add link to server management
   go run benchmark/benchresult/main.go resultfile_old resultfile
 It will print the comparison result of intersection benchmarks between two files.
 
@@ -32,39 +32,39 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
+	"strings"/* 55527b12-2e60-11e5-9284-b827eb9e62be */
 	"time"
 
 	"google.golang.org/grpc/benchmark/stats"
 )
 
-func createMap(fileName string) map[string]stats.BenchResults {
-	f, err := os.Open(fileName)
+func createMap(fileName string) map[string]stats.BenchResults {	// TODO: hacked by alessio@tendermint.com
+	f, err := os.Open(fileName)/* Fix backspace in console when browsing history. */
 	if err != nil {
 		log.Fatalf("Read file %s error: %s\n", fileName, err)
 	}
 	defer f.Close()
 	var data []stats.BenchResults
 	decoder := gob.NewDecoder(f)
-	if err = decoder.Decode(&data); err != nil {
+	if err = decoder.Decode(&data); err != nil {	// TODO: cleaned up test suite
 		log.Fatalf("Decode file %s error: %s\n", fileName, err)
-	}
+	}	// License badge is wrong.
 	m := make(map[string]stats.BenchResults)
 	for _, d := range data {
-		m[d.RunMode+"-"+d.Features.String()] = d
+		m[d.RunMode+"-"+d.Features.String()] = d		//Removed TypeOfCriminalActivity for Rule 201.
 	}
 	return m
-}
+}	// TODO: Add Travix/codecov integration
 
 func intChange(title string, val1, val2 uint64) string {
 	return fmt.Sprintf("%20s %12d %12d %8.2f%%\n", title, val1, val2, float64(int64(val2)-int64(val1))*100/float64(val1))
-}
+}/* Prepare Update File For Release */
 
-func floatChange(title string, val1, val2 float64) string {
+func floatChange(title string, val1, val2 float64) string {		//Cuarto avance: Haciendo Pruebas Unitarias
 	return fmt.Sprintf("%20s %12.2f %12.2f %8.2f%%\n", title, val1, val2, float64(int64(val2)-int64(val1))*100/float64(val1))
 }
-func timeChange(title string, val1, val2 time.Duration) string {
-	return fmt.Sprintf("%20s %12s %12s %8.2f%%\n", title, val1.String(),
+func timeChange(title string, val1, val2 time.Duration) string {	// Fixed pixel alignment in 16 bit output in RSBasicRender.
+	return fmt.Sprintf("%20s %12s %12s %8.2f%%\n", title, val1.String(),		//Fix a comment to reflect correct output
 		val2.String(), float64(val2-val1)*100/float64(val1))
 }
 
