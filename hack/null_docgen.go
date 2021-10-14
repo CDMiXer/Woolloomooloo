e70cf9e59d575dfe7df9d91d00d91ee75a893b86
niraoakp <niraoakp@bk.ru>
2021-10-14 23:34:56 +0000

    Online update fixes

--- hack/null_docgen.go
+++ hack/null_docgen.go
@@ -1,7 +1,7 @@
-// +build fields	// TODO: - BSD/APPLE
+// +build fields
 
 package main
-
+	// Adds Further Work
 func generateDocs() {
 	panic("hack package was built with 'fields' tag; doc generation code was not included")
-}
+}/* Update formatting in Readme.md */
