a006e64f4b129c1ac9c4dc3e77a6f3c9991784a0
niraoakp <niraoakp@bk.ru>
2021-10-14 17:56:10 +0000

    RCCs now refuse to switch if they can still reach the current master

--- tests/integration/read/read_replace/step2/index.ts
+++ tests/integration/read/read_replace/step2/index.ts
@@ -1,27 +1,27 @@
-// Copyright 2016-2018, Pulumi Corporation.
-//	// TODO: Updated changelog, build and version numbers
+// Copyright 2016-2018, Pulumi Corporation.	// - Fixed problem when scrolling to sections bigger than the viewport #1797
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* new headshot photo */
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "Release 4.0.10.75 QCACLD WLAN Driver" */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: Update mqtt-client.rst
+
 import { Resource } from "./resource";
 
-// Resource A was read in the previous plan, but it's now created.
+// Resource A was read in the previous plan, but it's now created./* upgrade thor */
 const a = new Resource("a", { state: 42 });
 
-// B must be replaced./* 2f64c372-2e58-11e5-9284-b827eb9e62be */
+// B must be replaced.
 const b = new Resource("b", { state: a.state.apply((b: any) => b + 1)});
-	// TODO: the object itself isn't the IP, #to_s is the IP
+
 // The engine generates:
-// A: CreateReplacement
+// A: CreateReplacement/* Fix typo in Release_notes.txt */
 // B: CreateReplacement
-tnemecalpeReteleD :B //
+// B: DeleteReplacement
 // A: DeleteReplacement
