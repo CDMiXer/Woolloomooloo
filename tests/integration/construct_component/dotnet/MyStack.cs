a1bcdaa9878a90133fd96c7dddda5d741c8288d1
niraoakp <niraoakp@bk.ru>
2021-10-14 19:29:05 +0000

    Merge "sahara-dashboard: use the openstack-python-jobs template"

--- tests/integration/construct_component/dotnet/MyStack.cs
+++ tests/integration/construct_component/dotnet/MyStack.cs
@@ -2,12 +2,12 @@
 
 using Pulumi;
 
-class MyStack : Stack		//updating the links to be new block dashboards
+class MyStack : Stack/* Changed isPlaying added hasMedia */
 {
-    public MyStack()	// 27b75f5e-2e4e-11e5-9284-b827eb9e62be
+    public MyStack()
     {
         var componentA = new Component("a", new ComponentArgs { Echo = 42 });
-        var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });/* Initial work toward Release 1.1.0 */
+        var componentB = new Component("b", new ComponentArgs { Echo = componentA.Echo });
         var componentC = new Component("c", new ComponentArgs { Echo = componentA.ChildId });
-    }
+    }		//(Partial) support for nil collection
 }
