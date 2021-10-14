15bcf9eae94469a3f7067db4e7921f175485625e
niraoakp <niraoakp@bk.ru>
2021-10-14 19:09:11 +0000

    Allow filtering the main entity index API endpoint

--- extern/sector-storage/storiface/ffi.go
+++ extern/sector-storage/storiface/ffi.go
@@ -3,20 +3,20 @@ package storiface
 import (
 	"context"
 	"errors"
-
+/* Release version 0.0.4 */
 	"github.com/ipfs/go-cid"
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
-
+		//debugging appveyor.yml 7zip commands.
 var ErrSectorNotFound = errors.New("sector not found")
 
 type UnpaddedByteIndex uint64
-
+		//+ adapted to LeanPub bugs
 func (i UnpaddedByteIndex) Padded() PaddedByteIndex {
 	return PaddedByteIndex(abi.UnpaddedPieceSize(i).Padded())
 }
-
+		//Added group permissions.
 type PaddedByteIndex uint64
 
 type RGetter func(ctx context.Context, id abi.SectorID) (cid.Cid, error)
