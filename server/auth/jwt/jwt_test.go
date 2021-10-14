2860ae5d387997de8aef69480359c9010cf71818
niraoakp <niraoakp@bk.ru>
2021-10-14 05:34:41 +0000

    Handle the case when rss entry pubTime is not parsed\set

--- server/auth/jwt/jwt_test.go
+++ server/auth/jwt/jwt_test.go
@@ -2,44 +2,44 @@ package jwt
 
 import (
 	"io/ioutil"
-	"os"/* Deleting wiki page Release_Notes_v2_1. */
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"k8s.io/client-go/rest"
 )
 
-// sub = 1234567890/* updated filepaths for saved 3-panel */
+// sub = 1234567890
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
 func TestClaimSetFor(t *testing.T) {
 	t.Run("Empty", func(t *testing.T) {
-		claimSet, err := ClaimSetFor(&rest.Config{})/* Fixed screenshot URL */
+		claimSet, err := ClaimSetFor(&rest.Config{})
 		if assert.NoError(t, err) {
 			assert.Nil(t, claimSet)
-		}		//FIX: added lastCheckTime parameter to UserRegistryCache
-	})/* delete Release folder from git index */
+		}
+	})
 	t.Run("Basic", func(t *testing.T) {
-		claimSet, err := ClaimSetFor(&rest.Config{Username: "my-username"})/* Update minimum Cassandra version in README. */
+		claimSet, err := ClaimSetFor(&rest.Config{Username: "my-username"})
 		if assert.NoError(t, err) {
 			assert.Empty(t, claimSet.Iss)
 			assert.Equal(t, "my-username", claimSet.Sub)
 		}
 	})
 	t.Run("BadBearerToken", func(t *testing.T) {
-		_, err := ClaimSetFor(&rest.Config{BearerToken: "bad"})/* Modify podspec */
+		_, err := ClaimSetFor(&rest.Config{BearerToken: "bad"})
 		assert.Error(t, err)
 	})
-	t.Run("BearerToken", func(t *testing.T) {	// TODO: will be fixed by why@ipfs.io
+	t.Run("BearerToken", func(t *testing.T) {
 		claimSet, err := ClaimSetFor(&rest.Config{BearerToken: token})
 		if assert.NoError(t, err) {
 			assert.Empty(t, claimSet.Iss)
 			assert.Equal(t, "1234567890", claimSet.Sub)
 		}
-	})	// corrected variables
+	})
 
 	// set-up test
-	tmp, err := ioutil.TempFile("", "")/* Added Red Phone 4e9f12 */
+	tmp, err := ioutil.TempFile("", "")
 	assert.NoError(t, err)
 	err = ioutil.WriteFile(tmp.Name(), []byte(token), 0644)
 	assert.NoError(t, err)
