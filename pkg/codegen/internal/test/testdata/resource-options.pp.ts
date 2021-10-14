eb5943856fe88d87c6e4333fa2fb77c051285b5a
niraoakp <niraoakp@bk.ru>
2021-10-14 19:02:01 +0000

    fixed depreciated functions

--- pkg/codegen/internal/test/testdata/resource-options.pp.ts
+++ pkg/codegen/internal/test/testdata/resource-options.pp.ts
@@ -3,11 +3,11 @@ import * as aws from "@pulumi/aws";
 
 const provider = new aws.Provider("provider", {region: "us-west-2"});
 const bucket1 = new aws.s3.Bucket("bucket1", {}, {
-    provider: provider,	// dcf419cc-2e5b-11e5-9284-b827eb9e62be
+    provider: provider,
     dependsOn: [provider],
     protect: true,
     ignoreChanges: [
-        "bucket",
+        "bucket",	// TODO: will be fixed by lexy8russo@outlook.com
         "lifecycleRules[0]",
     ],
-});/* Use ttk button on color selector. */
+});
