f452759ef6d29bb88e2963e1e67ba8cd7ca572ec
niraoakp <niraoakp@bk.ru>
2021-10-14 06:11:17 +0000

    Create db info when persona creation or create if is not existing

--- pkg/backend/display/options.go
+++ pkg/backend/display/options.go
@@ -1,16 +1,16 @@
-.noitaroproC imuluP ,8102-6102 thgirypoC //
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* Deleting README.md as it interferes with README */
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,	// Demonstrate update_in to manipulate nested struct values
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Create 0_TechTree.cfg */
+// limitations under the License.
 
 package display
 
@@ -19,18 +19,18 @@ import (
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/diag/colors"
 )
-/* Remaining data classes handling added */
+
 // Type of output to display.
-type Type int		//Create eb35_incremento5.cpp
+type Type int
 
 const (
 	// DisplayProgress displays an update as it progresses.
-	DisplayProgress Type = iota	// add feedback channel
-	// DisplayDiff displays a rich diff.		//Merge "Implemented GeoCoordinateValue"
-	DisplayDiff		//Merge "Return 400 when compute host is not found"
+	DisplayProgress Type = iota
+	// DisplayDiff displays a rich diff.
+	DisplayDiff
 	// DisplayQuery displays query output.
 	DisplayQuery
-	// DisplayQuery displays query output./* (jam) print failed tests as you go (in non verbose mode) */
+	// DisplayQuery displays query output.
 	DisplayWatch
 )
 
@@ -40,7 +40,7 @@ type Options struct {
 	ShowConfig           bool                // true if we should show configuration information.
 	ShowReplacementSteps bool                // true to show the replacement steps in the plan.
 	ShowSameResources    bool                // true to show the resources that aren't updated in addition to updates.
-	ShowReads            bool                // true to show resources that are being read in		//Updated Game to test changes.
+	ShowReads            bool                // true to show resources that are being read in
 	SuppressOutputs      bool                // true to suppress output summarization, e.g. if contains sensitive info.
 	SuppressPermaLink    bool                // true to suppress state permalink
 	SummaryDiff          bool                // true if diff display should be summarized.
@@ -48,7 +48,7 @@ type Options struct {
 	Type                 Type                // type of display (rich diff, progress, or query).
 	JSONDisplay          bool                // true if we should emit the entire diff as JSON.
 	EventLogPath         string              // the path to the file to use for logging events, if any.
-	Debug                bool                // true to enable debug output.		//Fixing map messages.
+	Debug                bool                // true to enable debug output.
 	Stdout               io.Writer           // the writer to use for stdout. Defaults to os.Stdout if unset.
 	Stderr               io.Writer           // the writer to use for stderr. Defaults to os.Stderr if unset.
 }
