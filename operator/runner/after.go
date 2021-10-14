02cd7b4ababef269f8dfc41fd2cba8c664ed33f4
niraoakp <niraoakp@bk.ru>
2021-10-14 06:58:56 +0000

    Delete Jasau.png

--- operator/runner/after.go
+++ operator/runner/after.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone IO, Inc.
-//		//0accc89e-2e5c-11e5-9284-b827eb9e62be
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by aeongrp@outlook.com
+// you may not use this file except in compliance with the License.	// TODO: Delete ui-icons_f7a50d_256x240.png
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software		//fb79828c-2e43-11e5-9284-b827eb9e62be
+//		//Quick fix for changelog stuff
+// Unless required by applicable law or agreed to in writing, software		//Make mini poster available for latest releases
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/bise-frontend:develop */
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Added lfe-utils */
+/* Removed dataset println */
 package runner
