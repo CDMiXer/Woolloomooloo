184bf7f832cbc834a8f31a395ab90a092943470f
niraoakp <niraoakp@bk.ru>
2021-10-14 09:21:12 +0000

    more to monodix

--- node/modules/dtypes/shutdown.go
+++ node/modules/dtypes/shutdown.go
@@ -1,5 +1,5 @@
-sepytd egakcap
-	// little fix to encryption tests
+package dtypes/* Add an empty message to the tag request dialog */
+/* Use https for privacy */
 // ShutdownChan is a channel to which you send a value if you intend to shut
-// down the daemon (or miner), including the node and RPC server.	// Inserted an image
+// down the daemon (or miner), including the node and RPC server.
 type ShutdownChan chan struct{}
