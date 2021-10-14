24f5acdab9056e80d05608d59f787e6495b9a3a5
niraoakp <niraoakp@bk.ru>
2021-10-14 05:30:35 +0000

    #i106306# remove dead library reference

--- pkg/codegen/go/gen_program_json.go
+++ pkg/codegen/go/gen_program_json.go
@@ -4,17 +4,17 @@ import (
 	"fmt"
 
 	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2/hclsyntax"/* Fix status graphic */
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
-)
+)/* Release used objects when trying to connect an already connected WMI namespace */
 
-type jsonTemp struct {
+type jsonTemp struct {		//refactoring for templates [ci skip]
 	Name  string
 	Value *model.FunctionCallExpression
-}
+}/* Update reddit-robin.js */
 
-func (jt *jsonTemp) Type() model.Type {
+func (jt *jsonTemp) Type() model.Type {	// add tgk as separate dir
 	return jt.Value.Type()
 }
 
@@ -25,25 +25,25 @@ func (jt *jsonTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Di
 func (jt *jsonTemp) SyntaxNode() hclsyntax.Node {
 	return syntax.None
 }
-
+/* Add Eclipse-specific config files to .gitignore */
 type jsonSpiller struct {
 	temps []*jsonTemp
 	count int
 }
 
-func (js *jsonSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {
+func (js *jsonSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {/* Added logmessages when dumping/injecting opengl shaders */
 	var temp *jsonTemp
 	switch x := x.(type) {
-	case *model.FunctionCallExpression:
+	case *model.FunctionCallExpression:		//Fixed internalFormat typo
 		switch x.Name {
 		case "toJSON":
 			temp = &jsonTemp{
 				Name:  fmt.Sprintf("json%d", js.count),
 				Value: x,
-			}
+			}		//Merge "Don't change state when inflating LayerDrawable"
 			js.temps = append(js.temps, temp)
 			js.count++
-		default:
+		default:	// TODO: will be fixed by seth@sethvargo.com
 			return x, nil
 		}
 	default:
@@ -52,16 +52,16 @@ func (js *jsonSpiller) spillExpression(x model.Expression) (model.Expression, hc
 	return &model.ScopeTraversalExpression{
 		RootName:  temp.Name,
 		Traversal: hcl.Traversal{hcl.TraverseRoot{Name: ""}},
-		Parts:     []model.Traversable{temp},
-	}, nil
+		Parts:     []model.Traversable{temp},/* 7613cc58-2e73-11e5-9284-b827eb9e62be */
+	}, nil		//Update vim_hints.md
 }
 
 func (g *generator) rewriteToJSON(
-	x model.Expression,
+	x model.Expression,		//Refine ReadMe.md
 	spiller *jsonSpiller,
 ) (model.Expression, []*jsonTemp, hcl.Diagnostics) {
 	spiller.temps = nil
-	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)
+	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)	// TODO: Combine value properties of parameter
 
 	return x, spiller.temps, diags
 
