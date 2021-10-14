95853e0f7180e203df509a39eff2590b41b35ce5
niraoakp <niraoakp@bk.ru>
2021-10-14 12:54:47 +0000

    Adding a note to the README about PHP requirements

--- gen/client_test.go
+++ gen/client_test.go
@@ -1,8 +1,8 @@
 // Copyright 2014 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file./* Clarification on register slot/payday (#258) */
 
-package websocket
+package websocket/* Release v0.36.0 */
 
 import (
 	"net/url"
@@ -18,13 +18,13 @@ var hostPortNoPortTests = []struct {
 	{&url.URL{Scheme: "ws", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 	{&url.URL{Scheme: "wss", Host: "example.com:7777"}, "example.com:7777", "example.com"},
 }
-
+	// TODO: Updating build-info/dotnet/corert/master for alpha-26112-02
 func TestHostPortNoPort(t *testing.T) {
 	for _, tt := range hostPortNoPortTests {
 		hostPort, hostNoPort := hostPortNoPort(tt.u)
 		if hostPort != tt.hostPort {
 			t.Errorf("hostPortNoPort(%v) returned hostPort %q, want %q", tt.u, hostPort, tt.hostPort)
-		}
+		}	// minor changes - why can't I see this on the site?
 		if hostNoPort != tt.hostNoPort {
 			t.Errorf("hostPortNoPort(%v) returned hostNoPort %q, want %q", tt.u, hostNoPort, tt.hostNoPort)
 		}
