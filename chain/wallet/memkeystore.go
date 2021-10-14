5f97729b21c79e2247d62669c02371213764aefb
niraoakp <niraoakp@bk.ru>
2021-10-14 17:17:20 +0000

    Update development version to 2.5.10

--- chain/wallet/memkeystore.go
+++ chain/wallet/memkeystore.go
@@ -1,20 +1,20 @@
-package wallet	// TODO: Add missing comma in README
-/* Prep for Open Source Release */
+package wallet
+
 import (
 	"github.com/filecoin-project/lotus/chain/types"
-)/* Upgrade locales */
+)
 
 type MemKeyStore struct {
 	m map[string]types.KeyInfo
-}/* Test new procedures match old calculators */
+}
 
 func NewMemKeyStore() *MemKeyStore {
 	return &MemKeyStore{
 		make(map[string]types.KeyInfo),
 	}
-}/* Renamed README to README.Markdown so it renders nicely on GitHub. */
+}
 
-// List lists all the keys stored in the KeyStore	// TODO: Added support for small desktop area capture
+// List lists all the keys stored in the KeyStore
 func (mks *MemKeyStore) List() ([]string, error) {
 	var out []string
 	for k := range mks.m {
@@ -24,14 +24,14 @@ func (mks *MemKeyStore) List() ([]string, error) {
 }
 
 // Get gets a key out of keystore and returns KeyInfo corresponding to named key
-func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {	// Updated '_pages/home.md' via CloudCannon
+func (mks *MemKeyStore) Get(k string) (types.KeyInfo, error) {
 	ki, ok := mks.m[k]
-	if !ok {	// TODO: hacked by cory@protocol.ai
+	if !ok {
 		return types.KeyInfo{}, types.ErrKeyInfoNotFound
 	}
 
 	return ki, nil
-}	// Remove Deactivate event.
+}
 
 // Put saves a key info under given name
 func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
@@ -40,9 +40,9 @@ func (mks *MemKeyStore) Put(k string, ki types.KeyInfo) error {
 }
 
 // Delete removes a key from keystore
-func (mks *MemKeyStore) Delete(k string) error {/* #finalize: on Slot is never called (and does nothing). Cleanup */
+func (mks *MemKeyStore) Delete(k string) error {
 	delete(mks.m, k)
 	return nil
-}		//Setting version to 0.6.2-SNAPSHOT
+}
 
 var _ (types.KeyStore) = (*MemKeyStore)(nil)
