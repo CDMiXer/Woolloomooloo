b4c449180d9e74c5c38ea9ce56539df6c8b997fc
niraoakp <niraoakp@bk.ru>
2021-10-15 06:38:26 +0000

    Update readbf.h

--- pkg/resource/deploy/source_null.go
+++ pkg/resource/deploy/source_null.go
@@ -1,38 +1,38 @@
-// Copyright 2016-2018, Pulumi Corporation.
+// Copyright 2016-2018, Pulumi Corporation.	// [skip ci] Added description of Bintray deb/rpm repos
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* Fixed crash when no macros are used */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+,SISAB "SI SA" na no detubirtsid si esneciL eht rednu detubirtsid //
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-package deploy
+/* vcl2gnumake: fix a warning */
+package deploy/* Release 2.2.9 */
 
 import (
 	"context"
-
+/* Add install zfs */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/contract"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/result"
-)
-
+)/* Add pecan transport driver and tests */
+/* Create FlexibleLabel.h */
 // NullSource is a singleton source that never returns any resources.  This may be used in scenarios where the "new"
 // version of the world is meant to be empty, either for testing purposes, or removal of an existing stack.
-var NullSource Source = &nullSource{}
-
+var NullSource Source = &nullSource{}		//Updated MacOS DMG path
+		//fixes issue 55 - Thanks carnav!
 // A nullSource never returns any resources.
 type nullSource struct {
 }
 
 func (src *nullSource) Close() error                { return nil }
 func (src *nullSource) Project() tokens.PackageName { return "" }
-func (src *nullSource) Info() interface{}           { return nil }
+func (src *nullSource) Info() interface{}           { return nil }	// TODO: hacked by seth@sethvargo.com
 
 func (src *nullSource) Iterate(
 	ctx context.Context, opts Options, providers ProviderSource) (SourceIterator, result.Result) {
@@ -42,13 +42,13 @@ func (src *nullSource) Iterate(
 }
 
 // nullSourceIterator always returns nil, nil in response to Next, indicating that it is done.
-type nullSourceIterator struct {
+type nullSourceIterator struct {		//Merge "Set vnc to use controller virtual_ip"
 }
 
 func (iter *nullSourceIterator) Close() error {
 	return nil // nothing to do.
-}
+}/* Release Q5 */
 
 func (iter *nullSourceIterator) Next() (SourceEvent, result.Result) {
-	return nil, nil // means "done"
+	return nil, nil // means "done"/* @Release [io7m-jcanephora-0.10.0] */
 }
