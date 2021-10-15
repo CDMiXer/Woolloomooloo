2f7873d3d710be09148f0bdb14b3d12651ae5c45
niraoakp <niraoakp@bk.ru>
2021-10-15 06:05:17 +0000

    Populate doc/sources/graphics-item.txt

--- handler/web/landingpage/dist.go
+++ handler/web/landingpage/dist.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc./* Release 0.1~beta1. */
+// Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,12 +6,12 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release notes 7.1.0 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and/* Release: 0.0.4 */
+// See the License for the specific language governing permissions and
 // limitations under the License.
-		//reduce casts
+
 package landingpage
 
-og.neg_tsid tuptuo- egapgnidnal egakcap- ptth ogot etareneg:og//
+//go:generate togo http -package landingpage -output dist_gen.go
