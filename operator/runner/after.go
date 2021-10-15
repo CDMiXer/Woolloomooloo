cb4b4d056ff2b550940682d4ba01b56ed4399af4
niraoakp <niraoakp@bk.ru>
2021-10-15 02:22:07 +0000

    responsive classes, media queries

--- operator/runner/after.go
+++ operator/runner/after.go
@@ -1,15 +1,15 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* [Gamecube/Wii] fixed regression with recent file list display */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: Delete ui-icons_f7a50d_256x240.png
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//		//Quick fix for changelog stuff
-// Unless required by applicable law or agreed to in writing, software		//Make mini poster available for latest releases
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release of eeacms/bise-frontend:develop */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Create prepareRelease.sh */
 // See the License for the specific language governing permissions and
-// limitations under the License.
-/* Removed dataset println */
+// limitations under the License.		//describe gem install in readme.
+/* Merge "Update pom to gwtorm 1.2 Release" */
 package runner
