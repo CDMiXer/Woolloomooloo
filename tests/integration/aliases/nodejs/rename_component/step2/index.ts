84d7084bed7c431bd9f76aee51095887f296e9f3
niraoakp <niraoakp@bk.ru>
2021-10-15 09:50:27 +0000

    Fixed bug with increment operator for drawmodes (shift in underlying bitset)

--- tests/integration/aliases/nodejs/rename_component/step2/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step2/index.ts
@@ -1,27 +1,27 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//bits, fix bit width reinterpretation
-import * as pulumi from "@pulumi/pulumi";		//added Discuss channel
 
-class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
+import * as pulumi from "@pulumi/pulumi";
+/* Links for running, previewing, printing. */
+class Resource extends pulumi.ComponentResource {	// TODO: will be fixed by seth@sethvargo.com
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Nettoyage du contenu de bin */
+        super("my:module:Resource", name, {}, opts);/* Create regAlias.reg */
     }
-}
+}		//Added support for nullable params in mutator injection.
 
 // Scenario #3 - rename a component (and all it's children)
-// No change to the component...
+// No change to the component...		//Summarized authors on single line in tests for 941160
 class ComponentThree extends pulumi.ComponentResource {
     resource1: Resource;
     resource2: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentThree", name, {}, opts);		// Test unitaire client, MAJ des tests!!
-        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// 56829b86-2e63-11e5-9284-b827eb9e62be
+        super("my:module:ComponentThree", name, {}, opts);
+        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit	// TODO: Added picture link
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
-        this.resource1 = new Resource(`${name}-child`, { parent: this });/* Update ImgDimensionsRule.js */
-        this.resource2 = new Resource("otherchild", { parent: this });/* Release of 0.0.4 of video extras */
-    }
+        this.resource1 = new Resource(`${name}-child`, { parent: this });		//Create updates.js
+        this.resource2 = new Resource("otherchild", { parent: this });
+    }/* Prepare Update File For Release */
 }
 // ...but applying an alias to the instance successfully renames both the component and the children.
-const comp3 = new ComponentThree("newcomp3", {	// TODO: will be fixed by alan.shaw@protocol.ai
+const comp3 = new ComponentThree("newcomp3", {
     aliases: [{ name: "comp3" }],
 });
