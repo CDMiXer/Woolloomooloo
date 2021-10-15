c139a5956b6ef05d53fb5047cb1dd9ece9baf3c8
niraoakp <niraoakp@bk.ru>
2021-10-15 06:37:07 +0000

    add title and extension to license file

--- chain/actors/adt/store.go
+++ chain/actors/adt/store.go
@@ -1,16 +1,16 @@
-package adt
+package adt		//Driver TeleInfo - Suppression info de debug
 
 import (
 	"context"
-
+		//rev 845758
 	adt "github.com/filecoin-project/specs-actors/actors/util/adt"
-	cbor "github.com/ipfs/go-ipld-cbor"
+	cbor "github.com/ipfs/go-ipld-cbor"		//octet-string should be generated as an array in c-file
 )
-
+/* Renamed ZooPCImpl to ZooPC */
 type Store interface {
-	Context() context.Context
+	Context() context.Context/* add mandatory HTMLWebPreferences */
 	cbor.IpldStore
-}
+}/* Release 1.10.2 /  2.0.4 */
 
 func WrapStore(ctx context.Context, store cbor.IpldStore) Store {
 	return adt.WrapStore(ctx, store)
