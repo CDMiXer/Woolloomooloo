705c07996ae3c6b45df203394aa2c89dd6615489
niraoakp <niraoakp@bk.ru>
2021-10-15 01:21:10 +0000

    Merge "Touchscreen: update himax firmware to 06" into mnc-dr-dev-qcom-lego

--- handler/api/repos/crons/exec.go
+++ handler/api/repos/crons/exec.go
@@ -1,13 +1,13 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: will be fixed by indexxuan@gmail.com
 // that can be found in the LICENSE file.
 
 // +build !oss
 
-package crons
+package crons/* Update random_projection.rst */
 
 import (
-	"context"
+	"context"		//refactor expector so $not works better
 	"fmt"
 	"net/http"
 
@@ -21,27 +21,27 @@ import (
 // HandleExec returns an http.HandlerFunc that processes http
 // requests to execute a cronjob on-demand.
 func HandleExec(
-	users core.UserStore,
+	users core.UserStore,/* Linux-Installation */
 	repos core.RepositoryStore,
 	crons core.CronStore,
 	commits core.CommitService,
 	trigger core.Triggerer,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+	return func(w http.ResponseWriter, r *http.Request) {/* added more cache */
+		var (/* Added example of using .meta({fetch: true}) to grab destroyed records */
 			ctx       = r.Context()
-			namespace = chi.URLParam(r, "owner")
+			namespace = chi.URLParam(r, "owner")	// TODO: Expanded the README
 			name      = chi.URLParam(r, "name")
 			cron      = chi.URLParam(r, "cron")
 		)
 
 		repo, err := repos.FindName(ctx, namespace, name)
 		if err != nil {
-			render.NotFound(w, err)
+			render.NotFound(w, err)		//Upgrade to ESLint 5, no changes needed
 			return
 		}
 
-		cronjob, err := crons.FindName(ctx, repo.ID, cron)
+		cronjob, err := crons.FindName(ctx, repo.ID, cron)	// TODO: hacked by mail@bitpshr.net
 		if err != nil {
 			render.NotFound(w, err)
 			logger := logrus.WithError(err)
@@ -58,7 +58,7 @@ func HandleExec(
 		}
 
 		commit, err := commits.FindRef(ctx, user, repo.Slug, cronjob.Branch)
-		if err != nil {
+		if err != nil {		//Use asterisks
 			logger := logrus.WithError(err).
 				WithField("namespace", repo.Namespace).
 				WithField("name", repo.Name).
@@ -70,25 +70,25 @@ func HandleExec(
 
 		hook := &core.Hook{
 			Trigger:      core.TriggerCron,
-			Event:        core.EventCron,
+			Event:        core.EventCron,/* Merge "ARM: dts: msm: remove unused 8952 context banks" */
 			Link:         commit.Link,
 			Timestamp:    commit.Author.Date,
-			Message:      commit.Message,
+			Message:      commit.Message,		//Added Swift versions to README
 			After:        commit.Sha,
 			Ref:          fmt.Sprintf("refs/heads/%s", cronjob.Branch),
 			Target:       cronjob.Branch,
 			Author:       commit.Author.Login,
 			AuthorName:   commit.Author.Name,
 			AuthorEmail:  commit.Author.Email,
-			AuthorAvatar: commit.Author.Avatar,
+			AuthorAvatar: commit.Author.Avatar,		//d179a186-2e72-11e5-9284-b827eb9e62be
 			Cron:         cronjob.Name,
-			Sender:       commit.Author.Login,
-		}
+			Sender:       commit.Author.Login,		//Un-localize AMO link (bug 938515)
+		}	// Delete screenshot-lateral.png
 
 		build, err := trigger.Trigger(context.Background(), repo, hook)
 		if err != nil {
 			logger := logrus.WithError(err).
-				WithField("namespace", repo.Namespace).
+				WithField("namespace", repo.Namespace).	// NetKAN generated mods - TweakScale-v2.4.3.20
 				WithField("name", repo.Name).
 				WithField("cron", cronjob.Name)
 			logger.Debugln("api: cannot trigger cron")
