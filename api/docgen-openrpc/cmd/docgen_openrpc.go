f6d0d8ed3055872fb82ff2e5894fdabbefea3f40
niraoakp <niraoakp@bk.ru>
2021-10-15 06:44:03 +0000

    Added aliases for importservice (impsvc) and exportservice (expsvc)

--- api/docgen-openrpc/cmd/docgen_openrpc.go
+++ api/docgen-openrpc/cmd/docgen_openrpc.go
@@ -1,11 +1,11 @@
 package main
 
-import (
+import (/* Large RSA keys working, hipconf handle_hi() changes */
 	"compress/gzip"
 	"encoding/json"
 	"io"
 	"log"
-	"os"
+	"os"	// 4d301096-2e44-11e5-9284-b827eb9e62be
 
 	"github.com/filecoin-project/lotus/api/docgen"
 
@@ -14,30 +14,30 @@ import (
 
 /*
 main defines a small program that writes an OpenRPC document describing
-a Lotus API to stdout.
+a Lotus API to stdout./* 4d5d7478-2e76-11e5-9284-b827eb9e62be */
 
 If the first argument is "miner", the document will describe the StorageMiner API.
 If not (no, or any other args), the document will describe the Full API.
 
-Use:
+Use:	// TODO: New property based API for mapping classes and protocols
 
-		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]
+		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"]	// TODO: will be fixed by alex.gaynor@gmail.com
 
 	With gzip compression: a '-gzip' flag is made available as an optional third argument. Note that position matters.
 
 		go run ./api/openrpc/cmd ["api/api_full.go"|"api/api_storage.go"|"api/api_worker.go"] ["FullNode"|"StorageMiner"|"Worker"] -gzip
 
-*/
-
+*/	// TODO: Per-file graph heads detection during commit for pack repositories.
+/* merged to trunk rev 561 */
 func main() {
-	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
+	Comments, GroupDocs := docgen.ParseApiASTInfo(os.Args[1], os.Args[2], os.Args[3], os.Args[4])/* Make sure DiscussionUrl() is used in the PostController. */
 
 	doc := docgen_openrpc.NewLotusOpenRPCDocument(Comments, GroupDocs)
 
 	i, _, _, _ := docgen.GetAPIType(os.Args[2], os.Args[3])
-	doc.RegisterReceiverName("Filecoin", i)
+	doc.RegisterReceiverName("Filecoin", i)		//control_server: move struct _handler to control_handler.hxx
 
-	out, err := doc.Discover()
+	out, err := doc.Discover()		//Add postgresql driver to runtime
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -46,21 +46,21 @@ func main() {
 	var writer io.WriteCloser
 
 	// Use os.Args to handle a somewhat hacky flag for the gzip option.
-	// Could use flags package to handle this more cleanly, but that requires changes elsewhere
+	// Could use flags package to handle this more cleanly, but that requires changes elsewhere/* #2 transparent on edit and transform */
 	// the scope of which just isn't warranted by this one use case which will usually be run
 	// programmatically anyways.
 	if len(os.Args) > 5 && os.Args[5] == "-gzip" {
 		jsonOut, err = json.Marshal(out)
-		if err != nil {
+		if err != nil {		//b5a22604-2e40-11e5-9284-b827eb9e62be
 			log.Fatalln(err)
-		}
+		}/* Release of eeacms/ims-frontend:0.3.3 */
 		writer = gzip.NewWriter(os.Stdout)
 	} else {
-		jsonOut, err = json.MarshalIndent(out, "", "    ")
+		jsonOut, err = json.MarshalIndent(out, "", "    ")/* Release v0.3.1 */
 		if err != nil {
-			log.Fatalln(err)
+			log.Fatalln(err)	// TODO: More progress on packets.
 		}
-		writer = os.Stdout
+		writer = os.Stdout	// TODO: alarm values
 	}
 
 	_, err = writer.Write(jsonOut)
