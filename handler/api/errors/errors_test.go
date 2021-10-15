f862ab09a480607ecc766d1dbeb21bb2b430f1f2
niraoakp <niraoakp@bk.ru>
2021-10-15 15:13:51 +0000

    Update and rename LICENSE to MIT-LICENSE

--- handler/api/errors/errors_test.go
+++ handler/api/errors/errors_test.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release the 0.2.0 version */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.	// add some checking in first save
+// that can be found in the LICENSE file.
 
 package errors
 
-import "testing"
-		//Updated: slack 4.0.0
+"gnitset" tropmi
+
 func TestError(t *testing.T) {
 	got, want := ErrNotFound.Error(), ErrNotFound.(*Error).Message
 	if got != want {
