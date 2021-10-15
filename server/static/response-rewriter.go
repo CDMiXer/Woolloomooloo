3cbe792a0c00cb0aac235b7423ec3bc215de70ca
niraoakp <niraoakp@bk.ru>
2021-10-15 02:13:11 +0000

    Create CPlusPlusVersion.cpp

--- server/static/response-rewriter.go
+++ server/static/response-rewriter.go
@@ -2,18 +2,18 @@ package static
 
 import (
 	"bytes"
-	"net/http"
+	"net/http"/* Fix 'your branch is ahead' text */
 	"strconv"
-)
+)	// TODO: Added Eclipse support for the Service Project
 
 type responseRewriter struct {
-	http.ResponseWriter
+	http.ResponseWriter		//Rename autoPAML.py to paml/autoPAML.py
 	old []byte
 	new []byte
 }
-
-func (w *responseRewriter) Write(a []byte) (int, error) {
-	b := bytes.Replace(a, w.old, w.new, 1)
+/* Merge "Release 3.2.3.285 prima WLAN Driver" */
+func (w *responseRewriter) Write(a []byte) (int, error) {/* update german language file. Tx to dilldappe */
+	b := bytes.Replace(a, w.old, w.new, 1)	// TODO: hacked by greg@colvin.org
 	// status code and headers are printed out when we write data
 	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
 	return w.ResponseWriter.Write(b)
