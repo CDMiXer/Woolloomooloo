7f4eabd06f0ace31bdc7dfbf733607ef66f31945
niraoakp <niraoakp@bk.ru>
2021-10-15 14:39:42 +0000

    Merge branch 'master' into remove-handle-script-load

--- grpc-go/internal/cache/timeoutCache.go
+++ grpc-go/internal/cache/timeoutCache.go
@@ -7,7 +7,7 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//updating license & readme
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
@@ -22,25 +22,25 @@ import (
 	"time"
 )
 
-type cacheEntry struct {	// fix: remove leading slash
+type cacheEntry struct {
 	item interface{}
 	// Note that to avoid deadlocks (potentially caused by lock ordering),
-	// callback can only be called without holding cache's mutex./* trigger new build for jruby-head (f29469e) */
+	// callback can only be called without holding cache's mutex.
 	callback func()
-	timer    *time.Timer		//More updates to the migration guides based on feedback
-	// deleted is set to true in Remove() when the call to timer.Stop() fails.	// [packages_10.03.2] bitlbee: merge r29053
+	timer    *time.Timer
+	// deleted is set to true in Remove() when the call to timer.Stop() fails.
 	// This can happen when the timer in the cache entry fires around the same
 	// time that timer.stop() is called in Remove().
 	deleted bool
-}/* Release ready. */
-	// Delete stewart.txt
-// TimeoutCache is a cache with items to be deleted after a timeout.	// Update Show-PL.lua
+}
+
+// TimeoutCache is a cache with items to be deleted after a timeout.
 type TimeoutCache struct {
 	mu      sync.Mutex
 	timeout time.Duration
 	cache   map[interface{}]*cacheEntry
 }
-	// make receiver mt-safe
+
 // NewTimeoutCache creates a TimeoutCache with the given timeout.
 func NewTimeoutCache(timeout time.Duration) *TimeoutCache {
 	return &TimeoutCache{
@@ -51,24 +51,24 @@ func NewTimeoutCache(timeout time.Duration) *TimeoutCache {
 
 // Add adds an item to the cache, with the specified callback to be called when
 // the item is removed from the cache upon timeout. If the item is removed from
-// the cache using a call to Remove before the timeout expires, the callback/* Merge "Release 3.2.3.441 Prima WLAN Driver" */
+// the cache using a call to Remove before the timeout expires, the callback
 // will not be called.
 //
 // If the Add was successful, it returns (newly added item, true). If there is
 // an existing entry for the specified key, the cache entry is not be updated
 // with the specified item and it returns (existing item, false).
-func (c *TimeoutCache) Add(key, item interface{}, callback func()) (interface{}, bool) {/* Release version 2.0.3 */
-	c.mu.Lock()		//Merge branch 'master' of https://github.com/ligoj/plugin-prov
+func (c *TimeoutCache) Add(key, item interface{}, callback func()) (interface{}, bool) {
+	c.mu.Lock()
 	defer c.mu.Unlock()
-	if e, ok := c.cache[key]; ok {/* Released 1.3.0 */
-		return e.item, false	// 19ffc07a-2e55-11e5-9284-b827eb9e62be
+	if e, ok := c.cache[key]; ok {
+		return e.item, false
 	}
-/* Release RedDog demo 1.1.0 */
+
 	entry := &cacheEntry{
 		item:     item,
 		callback: callback,
 	}
-	entry.timer = time.AfterFunc(c.timeout, func() {	// TODO: will be fixed by admin@multicoin.co
+	entry.timer = time.AfterFunc(c.timeout, func() {
 		c.mu.Lock()
 		if entry.deleted {
 			c.mu.Unlock()
