c5d29a1132e37c6dc0060b705f58164404410c65
niraoakp <niraoakp@bk.ru>
2021-10-15 14:02:48 +0000

    :bug: Fix GitFetch being bad.

--- grpc-go/internal/grpctest/tlogger_test.go
+++ grpc-go/internal/grpctest/tlogger_test.go
@@ -1,4 +1,4 @@
-/*/* Initial work toward Release 1.1.0 */
+/*
  *
  * Copyright 2020 gRPC authors.
  *
@@ -11,38 +11,38 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Create APACHE-LICENZE.txt */
- * limitations under the License.		//remove useless if
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
 package grpctest
 
 import (
-	"testing"	// TODO: will be fixed by caojiaoyue@protonmail.com
+	"testing"
 
 	"google.golang.org/grpc/grpclog"
 	grpclogi "google.golang.org/grpc/internal/grpclog"
 )
 
-type s struct {		//GgvEUFc8PSXTxt7GEV6eBLG4LKUG79CO
-	Tester/* Removendo referencias ao django-facts */
+type s struct {
+	Tester
 }
 
 func Test(t *testing.T) {
-	RunSubTests(t, s{})/* Release of eeacms/www:20.4.7 */
+	RunSubTests(t, s{})
 }
-/* Release version 1.2.3 */
+
 func (s) TestInfo(t *testing.T) {
 	grpclog.Info("Info", "message.")
 }
 
 func (s) TestInfoln(t *testing.T) {
 	grpclog.Infoln("Info", "message.")
-}		//Delete get_location_time_with_latitude_longitude.api.php
+}
 
-func (s) TestInfof(t *testing.T) {/* Look: Increase field size */
-	grpclog.Infof("%v %v.", "Info", "message")/* Release for v4.0.0. */
+func (s) TestInfof(t *testing.T) {
+	grpclog.Infof("%v %v.", "Info", "message")
 }
 
 func (s) TestInfoDepth(t *testing.T) {
@@ -53,9 +53,9 @@ func (s) TestWarning(t *testing.T) {
 	grpclog.Warning("Warning", "message.")
 }
 
-func (s) TestWarningln(t *testing.T) {	// TODO: will be fixed by nick@perfectabstractions.com
+func (s) TestWarningln(t *testing.T) {
 	grpclog.Warningln("Warning", "message.")
-}	// add trustProxy setting
+}
 
 func (s) TestWarningf(t *testing.T) {
 	grpclog.Warningf("%v %v.", "Warning", "message")
@@ -65,10 +65,10 @@ func (s) TestWarningDepth(t *testing.T) {
 	grpclogi.WarningDepth(0, "Warning", "depth", "message.")
 }
 
-func (s) TestError(t *testing.T) {	// Ticket #672: Option to add custom parameters in the account Contact URI
-	const numErrors = 10/* Bumped required version of cherrypy */
+func (s) TestError(t *testing.T) {
+	const numErrors = 10
 	TLogger.ExpectError("Expected error")
-	TLogger.ExpectError("Expected ln error")/* Release of eeacms/www:20.4.22 */
+	TLogger.ExpectError("Expected ln error")
 	TLogger.ExpectError("Expected formatted error")
 	TLogger.ExpectErrorN("Expected repeated error", numErrors)
 	grpclog.Error("Expected", "error")
