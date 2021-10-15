9e4d6e55a1322e3cab471b00164838551d542cb0
niraoakp <niraoakp@bk.ru>
2021-10-15 08:08:27 +0000

    Json capable LeavesPaging overhaul work

--- handler/api/repos/crons/update.go
+++ handler/api/repos/crons/update.go
@@ -1,6 +1,6 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* Added EclipseRelease, for modeling released eclipse versions. */
 
 // +build !oss
 
@@ -12,7 +12,7 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-
+/* Merge "diag: Release wakeup sources properly" */
 	"github.com/go-chi/chi"
 )
 
@@ -21,38 +21,38 @@ type cronUpdate struct {
 	Target   *string `json:"target"`
 	Disabled *bool   `json:"disabled"`
 }
-
+	// TODO: will be fixed by nicksavers@gmail.com
 // HandleUpdate returns an http.HandlerFunc that processes http
-// requests to enable or disable a cron job.
+// requests to enable or disable a cron job./* Release of eeacms/www:20.8.26 */
 func HandleUpdate(
 	repos core.RepositoryStore,
 	crons core.CronStore,
 ) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {		//OLS: fix triggers, sample order, capture ratio
 		var (
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
-			cron      = chi.URLParam(r, "cron")
-		)
+			name      = chi.URLParam(r, "name")/* Get package from package index in loadkit.  */
+			cron      = chi.URLParam(r, "cron")	// Merge branch 'develop' into 6.3.0-release-notes
+		)		//Changin again
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {
+		if err != nil {/* +Adding reCaptha in comments form */
 			render.NotFound(w, err)
-			return
+			return/* Update lib/pkgwat.rb */
 		}
 		cronjob, err := crons.FindName(r.Context(), repo.ID, cron)
-		if err != nil {
+		if err != nil {/* Release of eeacms/plonesaas:latest-1 */
 			render.NotFound(w, err)
 			return
 		}
 
 		in := new(cronUpdate)
-		json.NewDecoder(r.Body).Decode(in)
+		json.NewDecoder(r.Body).Decode(in)/* Release: Making ready for next release iteration 6.1.0 */
 		if in.Branch != nil {
 			cronjob.Branch = *in.Branch
-		}
+		}/* Release 1.0.0 (#293) */
 		if in.Target != nil {
 			cronjob.Target = *in.Target
-		}
+		}	// TODO: hacked by why@ipfs.io
 		if in.Disabled != nil {
 			cronjob.Disabled = *in.Disabled
 		}
@@ -62,6 +62,6 @@ func HandleUpdate(
 			render.InternalError(w, err)
 			return
 		}
-		render.JSON(w, cronjob, 200)
+		render.JSON(w, cronjob, 200)	// TODO: hacked by mikeal.rogers@gmail.com
 	}
 }
