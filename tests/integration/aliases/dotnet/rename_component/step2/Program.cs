583eb62febf3df93d1a31e56fac49f272656e500
niraoakp <niraoakp@bk.ru>
2021-10-15 09:20:33 +0000

    merging testcases + documentation improvement

--- tests/integration/aliases/dotnet/rename_component/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename_component/step2/Program.cs
@@ -1,13 +1,13 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Use new GitHub Releases feature for download! */
 
 using System.Threading.Tasks;
 using Pulumi;
-/* Release version 1.2.3.RELEASE */
-class Resource : ComponentResource/* Change text in section 'HowToRelease'. */
+/* Merge "usb: xhci: Release spinlock during command cancellation" */
+class Resource : ComponentResource
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
-    {
+    {/* add api version to fts */
     }
 }
 
@@ -15,31 +15,31 @@ class Resource : ComponentResource/* Change text in section 'HowToRelease'. */
 // No change to the component itself.
 class ComponentThree : ComponentResource
 {
-    private Resource resource1;	// TODO: Added a wizard about screen type.
+    private Resource resource1;
     private Resource resource2;
 
-    public ComponentThree(string name, ComponentResourceOptions options = null)
-        : base("my:module:ComponentThree", name, options)/* Merge "Make LEGACY_ICON_TREATMENT flag work" into ub-launcher3-master */
-    {/* Implemented orderDescendingBy: and wrote tests for it */
+    public ComponentThree(string name, ComponentResourceOptions options = null)	// String equals comparisons to yoda order
+        : base("my:module:ComponentThree", name, options)
+    {
         // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
         // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
         this.resource1 = new Resource($"{name}-child", new ComponentResourceOptions { Parent = this });
         this.resource2 = new Resource("otherchild", new ComponentResourceOptions { Parent = this });
     }
-}/* Created geah-stanza04.tid */
+}/* Release 0.8.1.1 */
 
 
 class Program
 {
-    static Task<int> Main(string[] args)/* Implements Observer pattern without using the Java one. */
-    {
+    static Task<int> Main(string[] args)
+{    
         return Deployment.RunAsync(() =>
-        {/* bb69f6c8-2e65-11e5-9284-b827eb9e62be */
+        {
             // Applying an alias to the instance successfully renames both the component and the children.
             var comp3 = new ComponentThree("newcomp3", new ComponentResourceOptions
             {
-                Aliases = { new Alias { Name = "comp3" } },	// New maintainers wanted!
+                Aliases = { new Alias { Name = "comp3" } },
             });
         });
     }
-}/* REST backend ; GUI => update for "my albums list" */
+}
