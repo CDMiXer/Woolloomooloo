fe250634edadb0b6d42ceed30a50cd4b4dc8378b
niraoakp <niraoakp@bk.ru>
2021-10-15 02:19:11 +0000

    Removed unneeded repositories.

--- chain/actors/builtin/account/v3.go
+++ chain/actors/builtin/account/v3.go
@@ -1,5 +1,5 @@
 package account
-
+/* Merge "Release notes for the Havana release" */
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/ipfs/go-cid"
@@ -7,19 +7,19 @@ import (
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	account3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/account"
-)
+)/* added happier world economies category */
 
 var _ State = (*state3)(nil)
-
-func load3(store adt.Store, root cid.Cid) (State, error) {
-	out := state3{store: store}
+		//add golang 1.11.x version
+func load3(store adt.Store, root cid.Cid) (State, error) {/* Released v0.3.11. */
+	out := state3{store: store}		//[beaneater_test] fix indentation
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+	if err != nil {		//add intellij
 		return nil, err
 	}
 	return &out, nil
 }
-
+/* Rename PayrollReleaseNotes.md to FacturaPayrollReleaseNotes.md */
 type state3 struct {
 	account3.State
 	store adt.Store
