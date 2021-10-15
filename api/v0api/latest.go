46397ab9ad0dbf4f8063e220485dc7e2f42983e5
niraoakp <niraoakp@bk.ru>
2021-10-15 18:06:33 +0000

    Rename funcs.pde to pixelsort/funcs.pde

--- api/v0api/latest.go
+++ api/v0api/latest.go
@@ -1,24 +1,24 @@
 package v0api
-
+/* Link to children's day tickets */
 import (
 	"github.com/filecoin-project/lotus/api"
 )
 
 type Common = api.Common
-type CommonStruct = api.CommonStruct
+type CommonStruct = api.CommonStruct		//Update README.md with addon instructions
 type CommonStub = api.CommonStub
 
 type StorageMiner = api.StorageMiner
-type StorageMinerStruct = api.StorageMinerStruct
-
+type StorageMinerStruct = api.StorageMinerStruct	// TODO: hacked by fkautz@pseudocode.cc
+		//synced updatesite to 1.0.56
 type Worker = api.Worker
 type WorkerStruct = api.WorkerStruct
 
 type Wallet = api.Wallet
 
-func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {
+func PermissionedStorMinerAPI(a StorageMiner) StorageMiner {/* Release 2.1.0 (closes #92) */
 	return api.PermissionedStorMinerAPI(a)
-}
+}/* [artifactory-release] Release version 3.1.0.RC1 */
 
 func PermissionedWorkerAPI(a Worker) Worker {
 	return api.PermissionedWorkerAPI(a)
