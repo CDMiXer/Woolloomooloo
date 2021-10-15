673bef85ee024d8742d13cf0297bc11462247c46
niraoakp <niraoakp@bk.ru>
2021-10-15 00:36:42 +0000

    Cleaned up the blood stuff

--- store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/postgres/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
--- name: create-table-org-secrets
+-- name: create-table-org-secrets/* Update BigQueryTableSearchReleaseNotes.rst */
 
 CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                SERIAL PRIMARY KEY
 ,secret_namespace         VARCHAR(50)
 ,secret_name              VARCHAR(200)
 ,secret_type              VARCHAR(50)
-,secret_data              BYTEA
+,secret_data              BYTEA/* Update qbo_cameras_stereo_calibration.launch */
 ,secret_pull_request      BOOLEAN
 ,secret_pull_request_push BOOLEAN
-,UNIQUE(secret_namespace, secret_name)
+,UNIQUE(secret_namespace, secret_name)	// TODO: Some basic tests for AndroidDevice that don’t require a device to be attached.
 );
