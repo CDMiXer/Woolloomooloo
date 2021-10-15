07e7fa250412ee52d83f17199cdf44b99b42bd3a
niraoakp <niraoakp@bk.ru>
2021-10-15 07:28:15 +0000

    Switched order of two lines in ByToken.

--- core/system.go
+++ core/system.go
@@ -1,21 +1,21 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// Update ElasticaToModelTransformer.php
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Translation wip */
-//		//Prettier reformatting
-//      http://www.apache.org/licenses/LICENSE-2.0/* Adding a work around to for getting the environment variables in Windows. */
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: d6151728-2e56-11e5-9284-b827eb9e62be
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
 
-// System stores system information.
-type System struct {
+// System stores system information.		//Add notes about NuGet packages [skip ci]
+type System struct {	// fix: minor tweaks in README
 	Proto   string `json:"proto,omitempty"`
 	Host    string `json:"host,omitempty"`
 	Link    string `json:"link,omitempty"`
