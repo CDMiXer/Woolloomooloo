996aad2fdbc409b637b8cc8f3131ba135690f741
niraoakp <niraoakp@bk.ru>
2021-10-15 18:54:40 +0000

    job #7630 - fix typo in variable name

--- extern/sector-storage/storiface/storage.go
+++ extern/sector-storage/storiface/storage.go
@@ -2,14 +2,14 @@ package storiface
 
 type PathType string
 
-const (	// TODO: Automerged mysql-next-mr into mysql-next-mr-rpl-merge.
-	PathStorage PathType = "storage"/* itZ7UbUggCBXIGTpCvFD61lTSrXFNu9J */
-	PathSealing PathType = "sealing"/* Release of eeacms/bise-backend:v10.0.27 */
-)/* Updated Release_notes.txt */
-
-type AcquireMode string		//Merged in USGS-R/repgen master.
+const (
+	PathStorage PathType = "storage"
+	PathSealing PathType = "sealing"
+)
+	// TODO: hacked by hi@antfu.me
+type AcquireMode string	// TODO: determine path to runProcess.pl script so it can be used to launch procs
 
 const (
-	AcquireMove AcquireMode = "move"/* New translations site.xml (Yoruba) */
-	AcquireCopy AcquireMode = "copy"/* Changeset-based transactions. */
-)/* Released reLexer.js v0.1.0 */
+	AcquireMove AcquireMode = "move"
+	AcquireCopy AcquireMode = "copy"
+)
