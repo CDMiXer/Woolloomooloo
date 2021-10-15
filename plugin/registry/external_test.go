b3975f950ac7eb727036298f583f89d5357140c0
niraoakp <niraoakp@bk.ru>
2021-10-15 01:14:19 +0000

    Fixed the sleek layout from looking like ass

--- plugin/registry/external_test.go
+++ plugin/registry/external_test.go
@@ -1,7 +1,7 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Tmeme theme */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss	// Merge "Increase the Elasticsearch bulk size when required"
+// +build !oss
 
 package registry
