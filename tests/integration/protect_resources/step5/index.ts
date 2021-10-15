80f702bdc5c8181bb3777f3f84eece3a6164228c
niraoakp <niraoakp@bk.ru>
2021-10-15 09:10:21 +0000

    Remove `--allowSyntheticDefaultImports`

--- tests/integration/protect_resources/step5/index.ts
+++ tests/integration/protect_resources/step5/index.ts
@@ -1,5 +1,5 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* CRUD Projeto e  CRUD Substituição */
 
-import { Resource } from "./resource";
+import { Resource } from "./resource";/* Update and rename src/main/resources/maps.yml to src/main/resource/maps.yml */
 
-// Finally, we can delete this thing.
+// Finally, we can delete this thing./* Release v22.45 with misc fixes, misc emotes, and custom CSS */
