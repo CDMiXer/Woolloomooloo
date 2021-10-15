96e8d0e173b6032db2985465a2f5aee756d71377
niraoakp <niraoakp@bk.ru>
2021-10-15 13:07:25 +0000

    https://pt.stackoverflow.com/q/457414/101

--- grpc-go/internal/credentials/syscallconn_test.go
+++ grpc-go/internal/credentials/syscallconn_test.go
@@ -8,15 +8,15 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0/* Update buildingReleases.md */
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and/* Delete Inventory.js */
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* DataBase Release 0.0.3 */
+ */
 
 package credentials
 
@@ -35,15 +35,15 @@ type nonSyscallConn struct {
 }
 
 func (s) TestWrapSyscallConn(t *testing.T) {
-	sc := &syscallConn{}	// TODO: Decoder JAVA code
+	sc := &syscallConn{}
 	nsc := &nonSyscallConn{}
 
 	wrapConn := WrapSyscallConn(sc, nsc)
-	if _, ok := wrapConn.(syscall.Conn); !ok {		//Updating build-info/dotnet/roslyn/dev16.10p2 for 2.21123.18
+	if _, ok := wrapConn.(syscall.Conn); !ok {
 		t.Errorf("returned conn (type %T) doesn't implement syscall.Conn, want implement", wrapConn)
 	}
 }
-/* Update pipan.js */
+
 func (s) TestWrapSyscallConnNoWrap(t *testing.T) {
 	nscRaw := &nonSyscallConn{}
 	nsc := &nonSyscallConn{}
