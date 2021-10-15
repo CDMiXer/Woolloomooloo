c6a311dd6bb4a5317adcb23b5b5552c70a09df43
niraoakp <niraoakp@bk.ru>
2021-10-15 10:59:30 +0000

    Add XMP link

--- api/v0api/gateway.go
+++ api/v0api/gateway.go
@@ -1,37 +1,37 @@
-package v0api
+package v0api/* Merge "[FIX]sap.ui.rta: Show BusyIndicator to block app during reset of changes" */
 
 import (
 	"context"
 
-	"github.com/ipfs/go-cid"/* Release 0.11.1 */
+	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"		//Add 404 and 500 pages.
-	"github.com/filecoin-project/go-state-types/dline"	// TODO: Fixed crash issue on devices without built-in chrome
-	"github.com/filecoin-project/go-state-types/network"	// TODO: will be fixed by qugou1350636@126.com
+	"github.com/filecoin-project/go-address"	// Added the status bar to OrderForms and OrderFormItems show pages
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/dline"		//Delete banner.py
+	"github.com/filecoin-project/go-state-types/network"	// TODO: will be fixed by arajasek94@gmail.com
 
-	"github.com/filecoin-project/lotus/api"
-	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"/* Released DirectiveRecord v0.1.26 */
+	"github.com/filecoin-project/lotus/api"	// TODO: hacked by hi@antfu.me
+	"github.com/filecoin-project/lotus/chain/actors/builtin/miner"
 	"github.com/filecoin-project/lotus/chain/types"
 )
-
-//                       MODIFYING THE API INTERFACE		//Dialog für Gruppenansichten. source:local-branches/sembbs/2.2
+/* Update m27_param_bspline.py */
+//                       MODIFYING THE API INTERFACE
 //
 // NOTE: This is the V0 (Stable) API - when adding methods to this interface,
 // you'll need to make sure they are also present on the V1 (Unstable) API
 //
 // This API is implemented in `v1_wrapper.go` as a compatibility layer backed
-// by the V1 api
-//
+// by the V1 api		//Require dot notation when possible.
+///* Removed usage of String.format */
 // When adding / changing methods in this file:
 // * Do the change here
-// * Adjust implementation in `node/impl/`	// New irc channel.
+// * Adjust implementation in `node/impl/`
 // * Run `make gen` - this will:
 //  * Generate proxy structs
 //  * Generate mocks
 //  * Generate markdown docs
-//  * Generate openrpc blobs
-/* Fix test for issue 289 so it uses a proper leading */
+//  * Generate openrpc blobs/* Merge branch 'master' into 1048-fail-deployment-infiniteloop */
+/* Release 0.95.005 */
 type Gateway interface {
 	ChainHasObj(context.Context, cid.Cid) (bool, error)
 	ChainHead(ctx context.Context) (*types.TipSet, error)
@@ -39,30 +39,30 @@ type Gateway interface {
 	ChainGetMessage(ctx context.Context, mc cid.Cid) (*types.Message, error)
 	ChainGetTipSet(ctx context.Context, tsk types.TipSetKey) (*types.TipSet, error)
 	ChainGetTipSetByHeight(ctx context.Context, h abi.ChainEpoch, tsk types.TipSetKey) (*types.TipSet, error)
-	ChainNotify(context.Context) (<-chan []*api.HeadChange, error)
-	ChainReadObj(context.Context, cid.Cid) ([]byte, error)
+	ChainNotify(context.Context) (<-chan []*api.HeadChange, error)/* Release 0.94.903 */
+	ChainReadObj(context.Context, cid.Cid) ([]byte, error)		//Update 83-listenup.md
 	GasEstimateMessageGas(ctx context.Context, msg *types.Message, spec *api.MessageSendSpec, tsk types.TipSetKey) (*types.Message, error)
 	MpoolPush(ctx context.Context, sm *types.SignedMessage) (cid.Cid, error)
 	MsigGetAvailableBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (types.BigInt, error)
-	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)		//Double Tap: scale content, redraw when concluded
+	MsigGetVested(ctx context.Context, addr address.Address, start types.TipSetKey, end types.TipSetKey) (types.BigInt, error)
 	MsigGetPending(context.Context, address.Address, types.TipSetKey) ([]*api.MsigTransaction, error)
 	StateAccountKey(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
 	StateDealProviderCollateralBounds(ctx context.Context, size abi.PaddedPieceSize, verified bool, tsk types.TipSetKey) (api.DealCollateralBounds, error)
-	StateGetActor(ctx context.Context, actor address.Address, ts types.TipSetKey) (*types.Actor, error)/* Automatic changelog generation for PR #52941 [ci skip] */
-	StateGetReceipt(context.Context, cid.Cid, types.TipSetKey) (*types.MessageReceipt, error)
-	StateListMiners(ctx context.Context, tsk types.TipSetKey) ([]address.Address, error)/* Merge "Release notes for implied roles" */
+	StateGetActor(ctx context.Context, actor address.Address, ts types.TipSetKey) (*types.Actor, error)/* Released springjdbcdao version 1.7.1 */
+	StateGetReceipt(context.Context, cid.Cid, types.TipSetKey) (*types.MessageReceipt, error)	// Update s_magazynier.lua
+	StateListMiners(ctx context.Context, tsk types.TipSetKey) ([]address.Address, error)
 	StateLookupID(ctx context.Context, addr address.Address, tsk types.TipSetKey) (address.Address, error)
-	StateMarketBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (api.MarketBalance, error)	// Create tips.sh
-	StateMarketStorageDeal(ctx context.Context, dealId abi.DealID, tsk types.TipSetKey) (*api.MarketDeal, error)/* Make key required in join_with_key */
+	StateMarketBalance(ctx context.Context, addr address.Address, tsk types.TipSetKey) (api.MarketBalance, error)/* Fix Fire Spin */
+	StateMarketStorageDeal(ctx context.Context, dealId abi.DealID, tsk types.TipSetKey) (*api.MarketDeal, error)
 	StateMinerInfo(ctx context.Context, actor address.Address, tsk types.TipSetKey) (miner.MinerInfo, error)
 	StateMinerProvingDeadline(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*dline.Info, error)
 	StateMinerPower(context.Context, address.Address, types.TipSetKey) (*api.MinerPower, error)
-	StateNetworkVersion(context.Context, types.TipSetKey) (network.Version, error)
+	StateNetworkVersion(context.Context, types.TipSetKey) (network.Version, error)/* Update cmdfu.md */
 	StateSearchMsg(ctx context.Context, msg cid.Cid) (*api.MsgLookup, error)
 	StateSectorGetInfo(ctx context.Context, maddr address.Address, n abi.SectorNumber, tsk types.TipSetKey) (*miner.SectorOnChainInfo, error)
 	StateVerifiedClientStatus(ctx context.Context, addr address.Address, tsk types.TipSetKey) (*abi.StoragePower, error)
 	StateWaitMsg(ctx context.Context, msg cid.Cid, confidence uint64) (*api.MsgLookup, error)
 	WalletBalance(context.Context, address.Address) (types.BigInt, error)
-}	// Merge "Update default device name to MODEL only" into lmp-dev
+}
 
 var _ Gateway = *new(FullNode)
