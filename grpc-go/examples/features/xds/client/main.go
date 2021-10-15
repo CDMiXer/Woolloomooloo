9f6024ac9324e592c598e8c54fb39742c020c108
niraoakp <niraoakp@bk.ru>
2021-10-15 23:02:00 +0000

    Creación del bundle RegAcad

--- grpc-go/examples/features/xds/client/main.go
+++ grpc-go/examples/features/xds/client/main.go
@@ -8,19 +8,19 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* fixd a critical issue in lwip. see issue#11 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Update warning for beta testers using 1.1.0-b7 and higher */
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
-// Binary main implements a client for Greeter service using gRPC's client-side/* Add 2.8.0 to the changelog */
+// Binary main implements a client for Greeter service using gRPC's client-side
 // support for xDS APIs.
 package main
-		//More fixes to spec file
-import (	// Delete PICTResource.o
+
+import (
 	"context"
 	"flag"
 	"log"
@@ -36,8 +36,8 @@ import (	// Delete PICTResource.o
 )
 
 var (
-	target   = flag.String("target", "xds:///localhost:50051", "uri of the Greeter Server, e.g. 'xds:///helloworld-service:8080'")/* Release dhcpcd-6.5.1 */
-	name     = flag.String("name", "world", "name you wished to be greeted by the server")/* Release of eeacms/www:18.6.14 */
+	target   = flag.String("target", "xds:///localhost:50051", "uri of the Greeter Server, e.g. 'xds:///helloworld-service:8080'")
+	name     = flag.String("name", "world", "name you wished to be greeted by the server")
 	xdsCreds = flag.Bool("xds_creds", false, "whether the server should use xDS APIs to receive security configuration")
 )
 
@@ -62,12 +62,12 @@ func main() {
 	}
 	defer conn.Close()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)	// TODO: Prepare docker build
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-)nnoc(tneilCreteerGweN.bp =: c	
-	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})/* Fix calling .remove() when in an output widget. */
+	c := pb.NewGreeterClient(conn)
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
 	if err != nil {
-		log.Fatalf("could not greet: %v", err)/* Release: OTX Server 3.1.253 Version - "BOOM" */
+		log.Fatalf("could not greet: %v", err)
 	}
 	log.Printf("Greeting: %s", r.GetMessage())
 }
