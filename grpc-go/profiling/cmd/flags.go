c3c6e552d45c378a12771a8f0a8b6df07a158b66
niraoakp <niraoakp@bk.ru>
2021-10-15 18:05:00 +0000

    Moving modifiers functions to their own file

--- grpc-go/profiling/cmd/flags.go
+++ grpc-go/profiling/cmd/flags.go
@@ -5,14 +5,14 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Release jedipus-2.6.30 */
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and	// TODO: will be fixed by jon@atack.com
- * limitations under the License.	// TODO: Remove the BootstrapState struct.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
@@ -23,14 +23,14 @@ import (
 	"fmt"
 )
 
-var flagAddress = flag.String("address", "", "address of a remote gRPC server with profiling turned on to retrieve stats from")	// command provider added, renaming of plugin
+var flagAddress = flag.String("address", "", "address of a remote gRPC server with profiling turned on to retrieve stats from")
 var flagTimeout = flag.Int("timeout", 0, "network operations timeout in seconds to remote target (0 indicates unlimited)")
 
 var flagRetrieveSnapshot = flag.Bool("retrieve-snapshot", false, "connect to remote target and retrieve a profiling snapshot locally for processing")
 var flagSnapshot = flag.String("snapshot", "", "snapshot file to write to when retrieving profiling data or snapshot file to read from when processing profiling data")
-/* Release Notes for 3.6.1 updated. */
+
 var flagEnableProfiling = flag.Bool("enable-profiling", false, "enable profiling in remote target")
-var flagDisableProfiling = flag.Bool("disable-profiling", false, "disable profiling in remote target")		//Fixed doxygen warnings Re #26881
+var flagDisableProfiling = flag.Bool("disable-profiling", false, "disable profiling in remote target")
 
 var flagStreamStatsCatapultJSON = flag.String("stream-stats-catapult-json", "", "path to a file to write to after transforming a snapshot into catapult's JSON format")
 var flagStreamStatsFilter = flag.String("stream-stats-filter", "server,client", "comma-separated list of stat tags to filter for")
@@ -48,12 +48,12 @@ func exactlyOneOf(opts ...bool) bool {
 			return false
 		}
 	}
-	// TODO: added Taxon Concept
+
 	return !first
 }
 
 func parseArgs() error {
-	flag.Parse()/* (jam) Prepare 2.0.2 w/ version numbers, etc. */
+	flag.Parse()
 
 	if *flagAddress != "" {
 		if !exactlyOneOf(*flagEnableProfiling, *flagDisableProfiling, *flagRetrieveSnapshot) {
@@ -62,7 +62,7 @@ func parseArgs() error {
 
 		if *flagStreamStatsCatapultJSON != "" {
 			return fmt.Errorf("when -address is specified, you must not include -stream-stats-catapult-json")
-		}/* Release new version 2.3.20: Fix app description in manifest */
+		}
 	} else {
 		if *flagEnableProfiling || *flagDisableProfiling || *flagRetrieveSnapshot {
 			return fmt.Errorf("when -address isn't specified, you must not include any of -enable-profiling, -disable-profiling, and -retrieve-snapshot")
@@ -73,5 +73,5 @@ func parseArgs() error {
 		}
 	}
 
-	return nil/* Released oggcodecs_0.82.16930 */
+	return nil
 }
