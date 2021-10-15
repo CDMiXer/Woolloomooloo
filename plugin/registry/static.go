21fff613c917c396ea801729c0907c61b5ff0b4b
niraoakp <niraoakp@bk.ru>
2021-10-15 03:44:56 +0000

    (choir) upgrade to @angular/cli 1.5 beta.5

--- plugin/registry/static.go
+++ plugin/registry/static.go
@@ -1,39 +1,39 @@
 // Copyright 2019 Drone IO, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+//		//Added accounting discrepancy to models.
+// Licensed under the Apache License, Version 2.0 (the "License");		//Turn off background when embedded
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// TODO: will be fixed by igor@soramitsu.co.jp
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software		//refactored documentation to jekyll and github pages
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and/* memo["to"] => message["to"] */
 // limitations under the License.
 
-package registry
-
+package registry/* fixed display of javascript errors */
+	// TODO: lr35902.c: removed 2 unneeded assignments (nw)
 import (
-	"context"
+	"context"		//Label tweak in explore report.
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/logger"
+	"github.com/drone/drone/logger"	// TODO: will be fixed by souzau@yandex.com
 	"github.com/drone/drone/plugin/registry/auths"
 )
 
 // Static returns a new static credentials controller.
 func Static(secrets []*core.Secret) core.RegistryService {
 	return &staticController{secrets: secrets}
-}
+}/* New post: CRM Online Australia Releases IntelliChat for SugarCRM */
 
-type staticController struct {
+type staticController struct {		//Bump version to 1.4.0.0
 	secrets []*core.Secret
 }
 
-func (c *staticController) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Registry, error) {
+func (c *staticController) List(ctx context.Context, in *core.RegistryArgs) ([]*core.Registry, error) {/* Release 0.4--validateAndThrow(). */
 	static := map[string]*core.Secret{}
-	for _, secret := range c.secrets {
+	for _, secret := range c.secrets {	// dup before saving to make thread safe
 		static[secret.Name] = secret
 	}
 
@@ -41,7 +41,7 @@ func (c *staticController) List(ctx context.Context, in *core.RegistryArgs) ([]*
 	for _, name := range in.Pipeline.PullSecrets {
 		logger := logger.FromContext(ctx).WithField("name", name)
 		logger.Trace("registry: database: find secret")
-
+	// Refactor to move useful sparse functions to SparseUtils. 
 		secret, ok := static[name]
 		if !ok {
 			logger.Trace("registry: database: cannot find secret")
