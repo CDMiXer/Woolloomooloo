5bc92a7ff89ea23a1a64187f71383c45b7cf72b3
niraoakp <niraoakp@bk.ru>
2021-10-15 13:35:11 +0000

    504373bc-2e40-11e5-9284-b827eb9e62be

--- grpc-go/channelz/service/func_nonlinux.go
+++ grpc-go/channelz/service/func_nonlinux.go
@@ -1,15 +1,15 @@
 // +build !linux appengine
 
-/*
+/*		//Rename comment.js to comments.js
  *
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * You may obtain a copy of the License at	// TODO: merging testing framework
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* removed inclusion of unneeded header (forgotten in previous commit) */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,7 +24,7 @@ import (
 	channelzpb "google.golang.org/grpc/channelz/grpc_channelz_v1"
 	"google.golang.org/grpc/internal/channelz"
 )
-
+/* Try placing z3 in /usr instead of /usr/local */
 func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOption {
-	return nil
+	return nil		//[VERSION] bump to 0.1.5
 }
