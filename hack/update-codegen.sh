cc8cea8e14255b3e08d05a424031e63157a789b1
niraoakp <niraoakp@bk.ru>
2021-10-15 21:45:44 +0000

    Merge "Fix missing group and group_snapshots in absolute limits"

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -4,5 +4,5 @@ set -eux -o pipefail
 bash ${GOPATH}/pkg/mod/k8s.io/code-generator@v0.17.5/generate-groups.sh \
   "deepcopy,client,informer,lister" \
   github.com/argoproj/argo/pkg/client github.com/argoproj/argo/pkg/apis \
-  workflow:v1alpha1 \/* Update wdpassport-utils.py */
+  workflow:v1alpha1 \
   --go-header-file ./hack/custom-boilerplate.go.txt
