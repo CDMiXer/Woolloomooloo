aca876d51152fa61e3a5b87b95145dab2ca3b34f
niraoakp <niraoakp@bk.ru>
2021-10-15 13:20:47 +0000

    Improved CreatePath

--- tests/integration/aliases/nodejs/retype_component/step1/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step1/index.ts
@@ -1,18 +1,18 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-		//Adjustments for device classes
+
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);/* 409b0878-2e9d-11e5-96dc-a45e60cdfd11 */
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {/* Fixing bug with Release and RelWithDebInfo build types. Fixes #32. */
+        super("my:module:Resource", name, {}, opts);/* Update Simple.Data URLs */
     }
 }
-
-// Scenario #4 - change the type of a component
-class ComponentFour extends pulumi.ComponentResource {/* Create prepareRelease.sh */
+/* 0fe62c82-2e73-11e5-9284-b827eb9e62be */
+// Scenario #4 - change the type of a component/* Merge "[FAB-6879] Fix configtxgen inaccuracies in doc" */
+class ComponentFour extends pulumi.ComponentResource {
     resource: Resource;
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:ComponentFour", name, {}, opts);		//GUI + new method to StringUtils
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {		//Add target="_blank" for opening a site by a new tab
+        super("my:module:ComponentFour", name, {}, opts);
         this.resource = new Resource("otherchild", {parent: this});
     }
 }
