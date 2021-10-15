3d70e6dc83e3e52cfedd2546a1cdce22bbae462d
niraoakp <niraoakp@bk.ru>
2021-10-15 18:03:25 +0000

    define setCardValue in WPYNumberFieldModel

--- operator/manager/rpc/error.go
+++ operator/manager/rpc/error.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.		//Merge branch 'master' into weekly-vm-update-w21
+// Use of this source code is governed by the Drone Non-Commercial License/* Renamed runtime-environment-stubs project */
 // that can be found in the LICENSE file.
-/* Release v0.0.1-3. */
+
 // +build !oss
-	// TODO: fix(DejaMouseDragDropCursor): Add RXJS delay operator
+
 package rpc
-/* Merge "Release 3.2.3.488 Prima WLAN Driver" */
+
 type serverError struct {
-	Status  int
-	Message string	// Merge monthEditor into development
+	Status  int		//Rename Report.md to README.md
+	Message string	// TODO: Added flysystem dependency
 }
 
 func (s *serverError) Error() string {
 	return s.Message
-}
+}/* Merge branch 'master' into update-setup-doc */
