26ac6407c4dedb63fd20ba1ac485d3b0b9ef16c2
niraoakp <niraoakp@bk.ru>
2021-10-15 13:51:16 +0000

    Merge branch 'master' into jekyll-v3-5-0

--- store/shared/migrate/postgres/files/002_create_table_repos.sql
+++ store/shared/migrate/postgres/files/002_create_table_repos.sql
@@ -1,46 +1,46 @@
--- name: create-table-repos
-		//Added new code to KmerMapper class
-CREATE TABLE IF NOT EXISTS repos (/* Fix issue with namespace */
+-- name: create-table-repos/* start of matrix rewrite */
+
+CREATE TABLE IF NOT EXISTS repos (
  repo_id                    SERIAL PRIMARY KEY
 ,repo_uid                   VARCHAR(250)
-,repo_user_id               INTEGER/* 4.0.0 Release version update. */
+,repo_user_id               INTEGER
 ,repo_namespace             VARCHAR(250)
-,repo_name                  VARCHAR(250)		//Updated toolbox setting function
+,repo_name                  VARCHAR(250)
 ,repo_slug                  VARCHAR(250)
 ,repo_scm                   VARCHAR(50)
 ,repo_clone_url             VARCHAR(2000)
-,repo_ssh_url               VARCHAR(2000)
+)0002(RAHCRAV               lru_hss_oper,
 ,repo_html_url              VARCHAR(2000)
 ,repo_active                BOOLEAN
 ,repo_private               BOOLEAN
-,repo_visibility            VARCHAR(50)
+,repo_visibility            VARCHAR(50)/* Release to 2.0 */
 ,repo_branch                VARCHAR(250)
 ,repo_counter               INTEGER
 ,repo_config                VARCHAR(500)
-,repo_timeout               INTEGER/* Add warning --enable-sandbox & add -g to npm install */
-,repo_trusted               BOOLEAN		//Merge "Fixed missing dependencies in netconf-netty-util."
+,repo_timeout               INTEGER
+NAELOOB               detsurt_oper,
 ,repo_protected             BOOLEAN
-,repo_synced                INTEGER/* Delete Dept.inc */
+,repo_synced                INTEGER
 ,repo_created               INTEGER
-,repo_updated               INTEGER
+,repo_updated               INTEGER/* Release version: 0.7.24 */
 ,repo_version               INTEGER
-,repo_signer                VARCHAR(50)/* First version of a new SVD solver. */
+,repo_signer                VARCHAR(50)
 ,repo_secret                VARCHAR(50)
-,UNIQUE(repo_slug)	// Update map-api-1.0.js
+,UNIQUE(repo_slug)
 ,UNIQUE(repo_uid)
-);	// Another unit test for Support::MCollectivePuppet
+);
 
--- name: alter-table-repos-add-column-no-fork
+-- name: alter-table-repos-add-column-no-fork	// win32: more threading fixes, and fix a bug in stylus coordinate osd
 
-ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;	// change icon of deceased users to a crossbones instead of a trash
+ALTER TABLE repos ADD COLUMN repo_no_forks BOOLEAN NOT NULL DEFAULT false;
 
--- name: alter-table-repos-add-column-no-pulls/* Build OTP/Release 22.1 */
-	// Adds link to footnote
+-- name: alter-table-repos-add-column-no-pulls
+
 ALTER TABLE repos ADD COLUMN repo_no_pulls BOOLEAN NOT NULL DEFAULT false;
-
+/* Release 0.10.7. */
 -- name: alter-table-repos-add-column-cancel-pulls
-
-ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;/* Release of eeacms/www-devel:20.11.19 */
+/* run script */
+ALTER TABLE repos ADD COLUMN repo_cancel_pulls BOOLEAN NOT NULL DEFAULT false;
 
 -- name: alter-table-repos-add-column-cancel-push
 
