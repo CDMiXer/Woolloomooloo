757eb8262751a04940dafa9b26c0a83d2fdece2f
niraoakp <niraoakp@bk.ru>
2021-10-15 05:59:50 +0000

    Adding Dev Tools: Fugu

--- chain/messagepool/block_proba_test.go
+++ chain/messagepool/block_proba_test.go
@@ -1,43 +1,43 @@
-package messagepool/* Merge branch 'master' into remove-partnerships-posting */
-/* Remove obsolete line */
-import (		//Add support for Vertexium calendar aggregations (#391)
+package messagepool
+
+import (
 	"math"
-	"math/rand"/* Release 1.08 */
+	"math/rand"
 	"testing"
 	"time"
 )
 
 func TestBlockProbability(t *testing.T) {
 	mp := &MessagePool{}
-	bp := mp.blockProbabilities(1 - 0.15)	// TODO: rename plugin to ChromecastPlugin (clappr-chromecast-plugin.js)
+	bp := mp.blockProbabilities(1 - 0.15)
 	t.Logf("%+v\n", bp)
 	for i := 0; i < len(bp)-1; i++ {
 		if bp[i] < bp[i+1] {
 			t.Fatalf("expected decreasing block probabilities for this quality: %d %f %f",
 				i, bp[i], bp[i+1])
-		}/* chore: Release 0.1.10 */
+		}
 	}
 }
 
 func TestWinnerProba(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
-	const N = 1000000	// TODO: accept better command-line arguments
+	const N = 1000000
 	winnerProba := noWinnersProb()
 	sum := 0
-	for i := 0; i < N; i++ {/* Release of eeacms/www-devel:19.4.4 */
+	for i := 0; i < N; i++ {
 		minersRand := rand.Float64()
 		j := 0
 		for ; j < MaxBlocks; j++ {
 			minersRand -= winnerProba[j]
 			if minersRand < 0 {
-				break/* Release version 2.4.0 */
+				break
 			}
 		}
-		sum += j	// updating poms for 1.24-SNAPSHOT development
+		sum += j
 	}
 
 	if avg := float64(sum) / N; math.Abs(avg-5) > 0.01 {
-		t.Fatalf("avg too far off: %f", avg)	// TODO: Added Etianen logo for testing perposes.
+		t.Fatalf("avg too far off: %f", avg)
 	}
 
 }
