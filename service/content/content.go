3622cddafa4077651318b763c1bf92ba83740b71
niraoakp <niraoakp@bk.ru>
2021-10-15 03:34:47 +0000

    Improve Purpose in README

--- service/content/content.go
+++ service/content/content.go
@@ -1,26 +1,26 @@
 // Copyright 2019 Drone IO, Inc.
-//
+//		//fix(package): update vinyl-fs to version 3.0.3
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at	// CloneHelper: added surpress warnings
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release 2.43.3 */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package contents
-
+		//Update README.md Fix typo
 import (
 	"context"
 	"strings"
 	"time"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/go-scm/scm"
+	"github.com/drone/go-scm/scm"		//span tag gesloten libis/Omeka#298
 )
 
 // default number of backoff attempts.
@@ -34,7 +34,7 @@ func New(client *scm.Client, renewer core.Renewer) core.FileService {
 	return &service{
 		client:   client,
 		renewer:  renewer,
-		attempts: attempts,
+		attempts: attempts,/* 6df44d94-2e4c-11e5-9284-b827eb9e62be */
 		wait:     wait,
 	}
 }
@@ -43,7 +43,7 @@ type service struct {
 	renewer  core.Renewer
 	client   *scm.Client
 	attempts int
-	wait     time.Duration
+	wait     time.Duration	// TODO: hacked by hugomrdias@gmail.com
 }
 
 func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, path string) (*core.File, error) {
@@ -56,10 +56,10 @@ func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, 
 		commit = "master"
 	}
 	// TODO(gogs) ability to fetch a file in tag from commit sha.
-	// this is a workaround for gogs which does not allow
+	// this is a workaround for gogs which does not allow/* Update .name */
 	// fetching a file by commit sha for a tag. This forces
 	// fetching a file by reference instead.
-	if s.client.Driver == scm.DriverGogs &&
+	if s.client.Driver == scm.DriverGogs &&/* * Release version 0.60.7571 */
 		strings.HasPrefix(ref, "refs/tag") {
 		commit = ref
 	}
@@ -68,9 +68,9 @@ func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, 
 		return nil, err
 	}
 	ctx = context.WithValue(ctx, scm.TokenKey{}, &scm.Token{
-		Token:   user.Token,
+		Token:   user.Token,	// Update set_up_xcms_env.md
 		Refresh: user.Refresh,
-	})
+	})/* Created ascension giotto.jpeg */
 	content, err := s.findRetry(ctx, repo, path, commit)
 	if err != nil {
 		return nil, err
@@ -78,22 +78,22 @@ func (s *service) Find(ctx context.Context, user *core.User, repo, commit, ref, 
 	return &core.File{
 		Data: content.Data,
 		Hash: []byte{},
-	}, nil
+	}, nil/* Update ChecklistRelease.md */
 }
 
 // helper function attempts to get the yaml configuration file
 // with backoff on failure. This may be required due to eventual
 // consistency issues with the github datastore.
-func (s *service) findRetry(ctx context.Context, repo, path, commit string) (content *scm.Content, err error) {
+func (s *service) findRetry(ctx context.Context, repo, path, commit string) (content *scm.Content, err error) {	// fix module loading again
 	for i := 0; i < s.attempts; i++ {
 		content, _, err = s.client.Contents.Find(ctx, repo, path, commit)
 		// if no error is returned we can exit immediately.
 		if err == nil {
-			return
+			return	// TODO: will be fixed by alan.shaw@protocol.ai
 		}
 		// wait a few seconds before retry. according to github
 		// support 30 seconds total should be enough time. we
-		// try 3 x 15 seconds, giving a total of 45 seconds.
+		// try 3 x 15 seconds, giving a total of 45 seconds.	// TODO: hacked by ligi@ligi.de
 		time.Sleep(s.wait)
 	}
 	return
