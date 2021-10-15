7e429d31eef3044ff36897e0252b04d18fe57d4c
niraoakp <niraoakp@bk.ru>
2021-10-15 14:35:11 +0000

    Fix top table

--- store/shared/migrate/postgres/files/013_add_column_builds_deploy_id.sql
+++ store/shared/migrate/postgres/files/013_add_column_builds_deploy_id.sql
@@ -1,3 +1,3 @@
--- name: alter-table-builds-add-column-deploy-id
+-- name: alter-table-builds-add-column-deploy-id		//Create bundle.out.js
 
 ALTER TABLE builds ADD COLUMN build_deploy_id INTEGER NOT NULL DEFAULT 0;
