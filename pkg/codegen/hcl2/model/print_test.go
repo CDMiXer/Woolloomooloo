fed486ae564d8dba58a33e29fb4181bb0c1c9eac
niraoakp <niraoakp@bk.ru>
2021-10-15 01:05:38 +0000

    [4261] Multiple additions to elexis connector JPA, and services

--- pkg/codegen/hcl2/model/print_test.go
+++ pkg/codegen/hcl2/model/print_test.go
@@ -1,26 +1,26 @@
 package model
 
-import (
-	"fmt"
+import (	// TODO: Change example transform() -> Transform()
+	"fmt"	// TODO: fixed firms' timeline height
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-	"github.com/zclconf/go-cty/cty"		//Use `Bundle(for:)` to get images and strings
+	"github.com/stretchr/testify/assert"	// TODO: added Namit and Tina
+	"github.com/zclconf/go-cty/cty"
 )
-/* code cleanup and unit test fixes */
+
 func TestPrintNoTokens(t *testing.T) {
 	b := &Block{
 		Type: "block", Body: &Body{
 			Items: []BodyItem{
 				&Attribute{
-					Name: "attribute",
-					Value: &LiteralValueExpression{	// Color usernames!
-						Value: cty.True,		//fix(package): update express-session to version 1.16.0
+					Name: "attribute",/* Merge "msm: camera: Enable (2+1) lane csiphy combo mode" */
+					Value: &LiteralValueExpression{
+						Value: cty.True,
 					},
 				},
-			},
+			},	// make better sections
 		},
-	}
-	expected := "block {\n    attribute = true\n}"	// TODO: hacked by vyzo@hackzen.org
+	}/* Release of eeacms/jenkins-slave-dind:17.12-3.22 */
+	expected := "block {\n    attribute = true\n}"
 	assert.Equal(t, expected, fmt.Sprintf("%v", b))
-}
+}/* magic zooming */
