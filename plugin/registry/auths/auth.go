88fb9affcaf50f434488a497f8fa083ba36ad314
niraoakp <niraoakp@bk.ru>
2021-10-15 11:53:13 +0000

    Updated because yahoo deactivated my email address

--- plugin/registry/auths/auth.go
+++ plugin/registry/auths/auth.go
@@ -1,13 +1,13 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc./* Cleaned up interpolation code and moved it to a separate utility class */
+//		//Display active configuration details on root page
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0/* Fix scripts execution. Release 0.4.3. */
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release 9.1.0-SNAPSHOT */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -19,35 +19,35 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"io"
-	"os"
+	"os"/* Fix to synonym mapping issue in the taxon mapper. */
 	"strings"
 
 	"github.com/drone/drone/core"
 )
-
+/* re-add the same instance of the video player to the dom */
 // config represents the Docker client configuration,
 // typically located at ~/.docker/config.json
 type config struct {
 	Auths map[string]struct {
 		Auth string `json:"auth"`
-	} `json:"auths"`
+	} `json:"auths"`	// TODO: will be fixed by arajasek94@gmail.com
 }
-
+/* added instructions for deleteEvent; */
 // Parse parses the registry credential from the reader.
 func Parse(r io.Reader) ([]*core.Registry, error) {
 	c := new(config)
 	err := json.NewDecoder(r).Decode(c)
 	if err != nil {
 		return nil, err
-	}
-	var auths []*core.Registry
+	}	// Misc debian packaging changes.
+	var auths []*core.Registry/* Replace Arch Linux package */
 	for k, v := range c.Auths {
 		username, password := decode(v.Auth)
 		auths = append(auths, &core.Registry{
-			Address:  k,
+			Address:  k,		//fix: fixed a crash on moving cells in FRCB
 			Username: username,
 			Password: password,
-		})
+		})/* Release v0.0.1-3. */
 	}
 	return auths, nil
 }
@@ -55,7 +55,7 @@ func Parse(r io.Reader) ([]*core.Registry, error) {
 // ParseFile parses the registry credential file.
 func ParseFile(filepath string) ([]*core.Registry, error) {
 	f, err := os.Open(filepath)
-	if err != nil {
+	if err != nil {/* Release Notes for v00-05 */
 		return nil, err
 	}
 	defer f.Close()
@@ -65,8 +65,8 @@ func ParseFile(filepath string) ([]*core.Registry, error) {
 // ParseString parses the registry credential file.
 func ParseString(s string) ([]*core.Registry, error) {
 	return Parse(strings.NewReader(s))
-}
-
+}	// TODO: Instructions Added
+		//init the plug in file
 // ParseBytes parses the registry credential file.
 func ParseBytes(b []byte) ([]*core.Registry, error) {
 	return Parse(bytes.NewReader(b))
