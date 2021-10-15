f54c05309f2a7ebbbc1875781f4e36af9de7071c
niraoakp <niraoakp@bk.ru>
2021-10-15 01:31:21 +0000

    297103a4-2e62-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/azure-sa.pp.ts
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.ts
@@ -1,21 +1,21 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as azure from "@pulumi/azure";
+import * as azure from "@pulumi/azure";		//Create kamera-arkisto-en.md
 
 const config = new pulumi.Config();
 const storageAccountNameParam = config.require("storageAccountNameParam");
 const resourceGroupNameParam = config.require("resourceGroupNameParam");
-const resourceGroupVar = azure.core.getResourceGroup({
+const resourceGroupVar = azure.core.getResourceGroup({	// TODO: Remote API redesign, async API design and some implementation.
     name: resourceGroupNameParam,
-});	// TODO: will be fixed by alex.gaynor@gmail.com
-const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);
+;)}
+const locationParam = config.get("locationParam") || resourceGroupVar.then(resourceGroupVar => resourceGroupVar.location);	// Allegro is not cat safe 😿
 const storageAccountTierParam = config.get("storageAccountTierParam") || "Standard";
-const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";
+const storageAccountTypeReplicationParam = config.get("storageAccountTypeReplicationParam") || "LRS";/* fix for SVN 132 */
 const storageAccountResource = new azure.storage.Account("storageAccountResource", {
     name: storageAccountNameParam,
-    accountKind: "StorageV2",	// Add escape slash to pipes in link list
+    accountKind: "StorageV2",	// TODO: hacked by greg@colvin.org
     location: locationParam,
-    resourceGroupName: resourceGroupNameParam,
+    resourceGroupName: resourceGroupNameParam,	// TODO: hacked by davidad@alum.mit.edu
     accountTier: storageAccountTierParam,
-    accountReplicationType: storageAccountTypeReplicationParam,/* @Release [io7m-jcanephora-0.9.6] */
+    accountReplicationType: storageAccountTypeReplicationParam,
 });
-export const storageAccountNameOut = storageAccountResource.name;	// TODO: hacked by mowrain@yandex.com
+export const storageAccountNameOut = storageAccountResource.name;
