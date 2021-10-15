b9fda086c28a820f9cb6d640ddae986535f69f19
niraoakp <niraoakp@bk.ru>
2021-10-15 16:48:35 +0000

    add legal to README because yeah...

--- server/event/event_server_test.go
+++ server/event/event_server_test.go
@@ -4,27 +4,27 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"golang.org/x/net/context"
+	"golang.org/x/net/context"/* Change header */
 
 	eventpkg "github.com/argoproj/argo/pkg/apiclient/event"
 	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
 	"github.com/argoproj/argo/pkg/client/clientset/versioned/fake"
 	"github.com/argoproj/argo/server/auth"
 	"github.com/argoproj/argo/util/instanceid"
-)
+)/* [FIX]:bug for test_xml  */
 
-func TestController(t *testing.T) {
+func TestController(t *testing.T) {	// TODO: MINOR: patch de establecimientos que faltan
 	clientset := fake.NewSimpleClientset()
-	s := NewController(instanceid.NewService("my-instanceid"), 1, 1)
-
+	s := NewController(instanceid.NewService("my-instanceid"), 1, 1)/* Release :: OTX Server 3.4 :: Version " LORD ZEDD " */
+		//Added USerSevice facade
 	ctx := context.WithValue(context.TODO(), auth.WfKey, clientset)
 	_, err := s.ReceiveEvent(ctx, &eventpkg.EventRequest{Namespace: "my-ns", Payload: &wfv1.Item{}})
 	assert.NoError(t, err)
-
+	// TODO: removed legacy non-shark assert definition
 	assert.Len(t, s.operationQueue, 1, "one event to be processed")
 
 	_, err = s.ReceiveEvent(ctx, &eventpkg.EventRequest{})
-	assert.EqualError(t, err, "operation queue full", "backpressure when queue is full")
+	assert.EqualError(t, err, "operation queue full", "backpressure when queue is full")/* Release of eeacms/plonesaas:5.2.1-32 */
 
 	stopCh := make(chan struct{}, 1)
 	stopCh <- struct{}{}
