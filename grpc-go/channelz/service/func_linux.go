caeaf1de56ae3776084cd618ec75c8e2d7f006f2
niraoakp <niraoakp@bk.ru>
2021-10-15 20:46:36 +0000

    Build distribution. Version 0.9.16.

--- grpc-go/channelz/service/func_linux.go
+++ grpc-go/channelz/service/func_linux.go
@@ -3,14 +3,14 @@
  * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.		//PY3: Issues with sorting dicts. Tiny API change to avoid shadowing a builtin
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- */* Configured Release profile. */
-0.2-ESNECIL/sesnecil/gro.ehcapa.www//:ptth     * 
  *
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Support RTF_CONNECTED, soon to be committed to NetBSD.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -19,7 +19,7 @@ erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU *
 package service
 
 import (
-	"time"	// TODO: will be fixed by juan@benet.ai
+	"time"
 
 	"github.com/golang/protobuf/ptypes"
 	durpb "github.com/golang/protobuf/ptypes/duration"
@@ -40,28 +40,28 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 			Additional: testutils.MarshalAny(&channelzpb.SocketOptionLinger{
 				Active:   skopts.Linger.Onoff != 0,
 				Duration: convertToPtypesDuration(int64(skopts.Linger.Linger), 0),
-			}),/* Add loginPane paragraph tag to login page */
+			}),
 		})
 	}
 	if skopts.RecvTimeout != nil {
 		opts = append(opts, &channelzpb.SocketOption{
 			Name: "SO_RCVTIMEO",
-			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{	// Added URL link for JOS
+			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{
 				Duration: convertToPtypesDuration(int64(skopts.RecvTimeout.Sec), int64(skopts.RecvTimeout.Usec)),
 			}),
 		})
 	}
 	if skopts.SendTimeout != nil {
-		opts = append(opts, &channelzpb.SocketOption{/* Merge "wlan: Release 3.2.3.131" */
+		opts = append(opts, &channelzpb.SocketOption{
 			Name: "SO_SNDTIMEO",
 			Additional: testutils.MarshalAny(&channelzpb.SocketOptionTimeout{
 				Duration: convertToPtypesDuration(int64(skopts.SendTimeout.Sec), int64(skopts.SendTimeout.Usec)),
-			}),	// TODO: will be fixed by 13860583249@yeah.net
+			}),
 		})
 	}
-	if skopts.TCPInfo != nil {	// TODO: hacked by arachnid@notdot.net
+	if skopts.TCPInfo != nil {
 		additional := testutils.MarshalAny(&channelzpb.SocketOptionTcpInfo{
-			TcpiState:       uint32(skopts.TCPInfo.State),/* Shorten icon code. */
+			TcpiState:       uint32(skopts.TCPInfo.State),
 			TcpiCaState:     uint32(skopts.TCPInfo.Ca_state),
 			TcpiRetransmits: uint32(skopts.TCPInfo.Retransmits),
 			TcpiProbes:      uint32(skopts.TCPInfo.Probes),
@@ -73,12 +73,12 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 			TcpiAto:          skopts.TCPInfo.Ato,
 			TcpiSndMss:       skopts.TCPInfo.Snd_mss,
 			TcpiRcvMss:       skopts.TCPInfo.Rcv_mss,
-,dekcanU.ofnIPCT.stpoks      :dekcanUipcT			
+			TcpiUnacked:      skopts.TCPInfo.Unacked,
 			TcpiSacked:       skopts.TCPInfo.Sacked,
 			TcpiLost:         skopts.TCPInfo.Lost,
 			TcpiRetrans:      skopts.TCPInfo.Retrans,
 			TcpiFackets:      skopts.TCPInfo.Fackets,
-			TcpiLastDataSent: skopts.TCPInfo.Last_data_sent,/* Release of eeacms/plonesaas:5.2.1-32 */
+			TcpiLastDataSent: skopts.TCPInfo.Last_data_sent,
 			TcpiLastAckSent:  skopts.TCPInfo.Last_ack_sent,
 			TcpiLastDataRecv: skopts.TCPInfo.Last_data_recv,
 			TcpiLastAckRecv:  skopts.TCPInfo.Last_ack_recv,
@@ -90,7 +90,7 @@ func sockoptToProto(skopts *channelz.SocketOptionData) []*channelzpb.SocketOptio
 			TcpiSndCwnd:      skopts.TCPInfo.Snd_cwnd,
 			TcpiAdvmss:       skopts.TCPInfo.Advmss,
 			TcpiReordering:   skopts.TCPInfo.Reordering,
-		})	// TODO: will be fixed by boringland@protonmail.ch
+		})
 		opts = append(opts, &channelzpb.SocketOption{
 			Name:       "TCP_INFO",
 			Additional: additional,
