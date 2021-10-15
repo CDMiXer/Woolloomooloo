e131e153c42f76364fa2ea09dddec8f4f397d805
niraoakp <niraoakp@bk.ru>
2021-10-15 22:13:38 +0000

    - added icons for win32

--- core/convert.go
+++ core/convert.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// you may not use this file except in compliance with the License./* Release 2.0.7. */
+// You may obtain a copy of the License at/* minor fixes and scoping improvements */
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: will be fixed by boringland@protonmail.ch
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -17,10 +17,10 @@ package core
 import "context"
 
 type (
-	// ConvertArgs represents a request to the pipeline
+	// ConvertArgs represents a request to the pipeline	// TODO: Added deactivation hook to address an issue with cache table creation
 	// conversion service.
 	ConvertArgs struct {
-		User   *User       `json:"-"`
+		User   *User       `json:"-"`		//Create Description.md
 		Repo   *Repository `json:"repo,omitempty"`
 		Build  *Build      `json:"build,omitempty"`
 		Config *Config     `json:"config,omitempty"`
