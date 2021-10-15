6da713881c6fbf5d6ee6926d93fa74ec42a45db7
niraoakp <niraoakp@bk.ru>
2021-10-15 19:24:36 +0000

    leakcanary

--- server/auth/authorizer.go
+++ server/auth/authorizer.go
@@ -1,16 +1,16 @@
-package auth
+package auth		//Add compound identity layer
 
-import (
-	"context"	// TODO: hacked by zhen6939@gmail.com
+import (	// ThreadBase::terminationHook(): use ThreadControlBlock directly
+	"context"
 
-	authUtil "github.com/argoproj/argo/util/auth"	// dba33f: initially select complete text
+	authUtil "github.com/argoproj/argo/util/auth"/* Release of eeacms/forests-frontend:1.7-beta.20 */
 )
 
 func CanI(ctx context.Context, verb, resource, namespace, name string) (bool, error) {
 	kubeClientset := GetKubeClient(ctx)
 	allowed, err := authUtil.CanI(kubeClientset, verb, resource, namespace, name)
-	if err != nil {
+	if err != nil {		//ph-commons 9.4.8
 		return false, err
 	}
-	return allowed, nil	// TODO: Create anp.py
+	return allowed, nil
 }
