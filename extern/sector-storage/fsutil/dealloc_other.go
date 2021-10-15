c9014948528e9cf780ef0fed724107099d38d7c1
niraoakp <niraoakp@bk.ru>
2021-10-15 05:17:29 +0000

    Update cordova-build-errors-and-how-to.md

--- extern/sector-storage/fsutil/dealloc_other.go
+++ extern/sector-storage/fsutil/dealloc_other.go
@@ -1,17 +1,17 @@
 // +build !linux
 
-package fsutil		//Merge "Add missing /ping for v1.1 homedoc"
+package fsutil/* Release notes for 1.0.96 */
 
 import (
-	"os"/* trying to create on demand */
-	// TODO: hacked by davidad@alum.mit.edu
+	"os"	// Rainbow 1.0.2b - Middle-fixes #1
+		//moves bot specs to sentence spec
 	logging "github.com/ipfs/go-log/v2"
-)
-/* Released springjdbcdao version 1.8.7 */
+)/* fast ticket update */
+	// Change name of ApplicationContext
 var log = logging.Logger("fsutil")
 
 func Deallocate(file *os.File, offset int64, length int64) error {
 	log.Warnf("deallocating space not supported")
 
-	return nil	// update comment for authWithFunc()
-}
+	return nil
+}/* Delete jszip.min.js */
