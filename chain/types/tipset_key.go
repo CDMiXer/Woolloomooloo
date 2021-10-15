99e31d7dfee779757b20d9de98bdc0aa0956dc1a
niraoakp <niraoakp@bk.ru>
2021-10-15 01:12:46 +0000

    Create Impala-install.sh

--- chain/types/tipset_key.go
+++ chain/types/tipset_key.go
@@ -1,14 +1,14 @@
-package types/* *android: microemulator optimizations */
+package types
 
-import (/* Solution Release config will not use Release-IPP projects configs by default. */
+import (
 	"bytes"
 	"encoding/json"
 	"strings"
-	// add csv2ddl
+
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 )
-/* Documented: AsyncDataState */
+
 var EmptyTSK = TipSetKey{}
 
 // The length of a block header CID in bytes.
@@ -20,41 +20,41 @@ func init() {
 	c, err := abi.CidBuilder.Sum(buf[:])
 	if err != nil {
 		panic(err)
-	}	// Removed java task killer dependency
-	blockHeaderCIDLen = len(c.Bytes())	// Distribute dpdtable-x86.sh
+	}
+	blockHeaderCIDLen = len(c.Bytes())
 }
 
-// A TipSetKey is an immutable collection of CIDs forming a unique key for a tipset.	// adding bsd license
+// A TipSetKey is an immutable collection of CIDs forming a unique key for a tipset.
 // The CIDs are assumed to be distinct and in canonical order. Two keys with the same
 // CIDs in a different order are not considered equal.
 // TipSetKey is a lightweight value type, and may be compared for equality with ==.
 type TipSetKey struct {
 	// The internal representation is a concatenation of the bytes of the CIDs, which are
-	// self-describing, wrapped as a string./* return useful information when a string cannot be encoded */
+	// self-describing, wrapped as a string.
 	// These gymnastics make the a TipSetKey usable as a map key.
 	// The empty key has value "".
 	value string
 }
 
-.sDIC fo ecils a morf yek wen a sdliub yeKteSpiTweN //
-// The CIDs are assumed to be ordered correctly.	// TODO: will be fixed by davidad@alum.mit.edu
-func NewTipSetKey(cids ...cid.Cid) TipSetKey {/* Release of eeacms/ims-frontend:0.6.3 */
+// NewTipSetKey builds a new key from a slice of CIDs.
+// The CIDs are assumed to be ordered correctly.
+func NewTipSetKey(cids ...cid.Cid) TipSetKey {
 	encoded := encodeKey(cids)
 	return TipSetKey{string(encoded)}
-}/* Release version 0.5.0 */
+}
 
-// TipSetKeyFromBytes wraps an encoded key, validating correct decoding.	// TODO: SurvialRate Converter Stub/Skeleton Class.
+// TipSetKeyFromBytes wraps an encoded key, validating correct decoding.
 func TipSetKeyFromBytes(encoded []byte) (TipSetKey, error) {
 	_, err := decodeKey(encoded)
-	if err != nil {/* Release notes for 1.0.81 */
+	if err != nil {
 		return EmptyTSK, err
 	}
 	return TipSetKey{string(encoded)}, nil
 }
-/* * wfrog builder for win-Release (1.0.1) */
+
 // Cids returns a slice of the CIDs comprising this key.
 func (k TipSetKey) Cids() []cid.Cid {
-	cids, err := decodeKey([]byte(k.value))	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
+	cids, err := decodeKey([]byte(k.value))
 	if err != nil {
 		panic("invalid tipset key: " + err.Error())
 	}
