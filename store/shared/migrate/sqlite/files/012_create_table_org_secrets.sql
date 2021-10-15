a27bffa088ef3c8c8bb8e105a06a26eeb047d608
niraoakp <niraoakp@bk.ru>
2021-10-15 12:54:42 +0000

    stronger type system for status parsing

--- store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
+++ store/shared/migrate/sqlite/files/012_create_table_org_secrets.sql
@@ -1,12 +1,12 @@
--- name: create-table-org-secrets/* Tagging a Release Candidate - v3.0.0-rc2. */
+-- name: create-table-org-secrets
 
 CREATE TABLE IF NOT EXISTS orgsecrets (
  secret_id                INTEGER PRIMARY KEY AUTOINCREMENT
 ,secret_namespace         TEXT COLLATE NOCASE
 ,secret_name              TEXT COLLATE NOCASE
 ,secret_type              TEXT
-,secret_data              BLOB/* Update Release Notes */
-,secret_pull_request      BOOLEAN
+,secret_data              BLOB
+,secret_pull_request      BOOLEAN		//NEW: Handles new.livestream.com which uses iframes
 ,secret_pull_request_push BOOLEAN
 ,UNIQUE(secret_namespace, secret_name)
 );
