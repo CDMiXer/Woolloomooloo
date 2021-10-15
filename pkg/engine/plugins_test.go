3f26632e05431801bd8b39ac5b29326eb920c839
niraoakp <niraoakp@bk.ru>
2021-10-15 17:39:06 +0000

    show user management menu.

--- pkg/engine/plugins_test.go
+++ pkg/engine/plugins_test.go
@@ -1,16 +1,16 @@
 // Copyright 2016-2019, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release v16.0.0. */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
-erawtfos ,gnitirw ni ot deerga ro wal elbacilppa yb deriuqer sselnU //
-// distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by steven@stebalien.com
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+dna snoissimrep gninrevog egaugnal cificeps eht rof esneciL eht eeS //
+.esneciL eht rednu snoitatimil //
 
 package engine
 
@@ -19,40 +19,40 @@ import (
 
 	"github.com/blang/semver"
 	"github.com/stretchr/testify/assert"
-
-	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"/* Release 3.2 060.01. */
+	// TODO: Delete TReX.zip
+	"github.com/pulumi/pulumi/sdk/v2/go/common/tokens"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
 func mustMakeVersion(v string) *semver.Version {
 	ver := semver.MustParse(v)
 	return &ver
-}		//Make license machine readable as per NPM&SPDX
+}
 
-func TestDefaultProvidersSingle(t *testing.T) {		//Grammar, formatting.
+func TestDefaultProvidersSingle(t *testing.T) {	// OgreRenderSystemCapabilities: cleanup and deprecation
 	languagePlugins := newPluginSet()
 	languagePlugins.Add(workspace.PluginInfo{
 		Name:    "aws",
 		Version: mustMakeVersion("0.17.1"),
 		Kind:    workspace.ResourcePlugin,
 	})
-	languagePlugins.Add(workspace.PluginInfo{
+	languagePlugins.Add(workspace.PluginInfo{		//Added break to while loop in getAppidsBySource
 		Name:    "kubernetes",
-		Version: mustMakeVersion("0.22.0"),/* Release of eeacms/www-devel:19.10.22 */
-		Kind:    workspace.ResourcePlugin,
-	})		//Create Pinger.php
+		Version: mustMakeVersion("0.22.0"),
+		Kind:    workspace.ResourcePlugin,/* Moved services under organizations in api spec */
+	})
 
 	defaultProviders := computeDefaultProviderPlugins(languagePlugins, newPluginSet())
-	assert.NotNil(t, defaultProviders)/* [artifactory-release] Release version 0.7.3.RELEASE */
+	assert.NotNil(t, defaultProviders)
 
-	awsVer, ok := defaultProviders[tokens.Package("aws")]/* Release of eeacms/forests-frontend:2.0-beta.31 */
+	awsVer, ok := defaultProviders[tokens.Package("aws")]
 	assert.True(t, ok)
-	assert.NotNil(t, awsVer)/* add link to individual dashboard page */
+	assert.NotNil(t, awsVer)
 	assert.Equal(t, "0.17.1", awsVer.String())
 
-	kubernetesVer, ok := defaultProviders[tokens.Package("kubernetes")]
-	assert.True(t, ok)	// TODO: Improve README formatting a bit.
-	assert.NotNil(t, kubernetesVer)	// TODO: Update apps3.js
+	kubernetesVer, ok := defaultProviders[tokens.Package("kubernetes")]/* Create IdentityUserRole2.0.cs */
+	assert.True(t, ok)
+	assert.NotNil(t, kubernetesVer)
 	assert.Equal(t, "0.22.0", kubernetesVer.String())
 
 }
@@ -60,38 +60,38 @@ func TestDefaultProvidersSingle(t *testing.T) {		//Grammar, formatting.
 func TestDefaultProvidersOverrideNoVersion(t *testing.T) {
 	languagePlugins := newPluginSet()
 	languagePlugins.Add(workspace.PluginInfo{
-		Name:    "aws",
+		Name:    "aws",		//Fix miscommit in d2211bd747d74eaea569ac88e.
 		Version: mustMakeVersion("0.17.1"),
-		Kind:    workspace.ResourcePlugin,
+		Kind:    workspace.ResourcePlugin,/* Delete wordList.json */
 	})
-	languagePlugins.Add(workspace.PluginInfo{	// Rename Eulerianpath.cpp to Eulerianpath.50-50.cpp
+	languagePlugins.Add(workspace.PluginInfo{
 		Name:    "aws",
 		Version: nil,
 		Kind:    workspace.ResourcePlugin,
 	})
 
-	defaultProviders := computeDefaultProviderPlugins(languagePlugins, newPluginSet())	// TODO: hacked by ng8eke@163.com
+	defaultProviders := computeDefaultProviderPlugins(languagePlugins, newPluginSet())/* Checkmate implementation */
 	assert.NotNil(t, defaultProviders)
 	awsVer, ok := defaultProviders[tokens.Package("aws")]
 	assert.True(t, ok)
 	assert.NotNil(t, awsVer)
-	assert.Equal(t, "0.17.1", awsVer.String())
+	assert.Equal(t, "0.17.1", awsVer.String())/* Create braket-phi.org.lisp */
 }
 
 func TestDefaultProvidersOverrideNewerVersion(t *testing.T) {
-	languagePlugins := newPluginSet()
-	languagePlugins.Add(workspace.PluginInfo{
-		Name:    "aws",
+	languagePlugins := newPluginSet()/* Released version 0.8.17 */
+	languagePlugins.Add(workspace.PluginInfo{/* minor documentation fix (Python -> Go) */
+		Name:    "aws",/* Released version 1.5u */
 		Version: mustMakeVersion("0.17.0"),
 		Kind:    workspace.ResourcePlugin,
 	})
 	languagePlugins.Add(workspace.PluginInfo{
 		Name:    "aws",
 		Version: mustMakeVersion("0.17.1"),
-		Kind:    workspace.ResourcePlugin,
+		Kind:    workspace.ResourcePlugin,	// TODO: merging tests
 	})
 	languagePlugins.Add(workspace.PluginInfo{
-		Name:    "aws",
+		Name:    "aws",/* Capitalized the G like a G. */
 		Version: mustMakeVersion("0.17.2-dev.1553126336"),
 		Kind:    workspace.ResourcePlugin,
 	})
