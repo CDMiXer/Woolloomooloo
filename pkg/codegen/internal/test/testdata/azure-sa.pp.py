a9477948b25b8c23aa91c977aaddd4b6d9f216d4
niraoakp <niraoakp@bk.ru>
2021-10-15 22:56:00 +0000

    made the image taller

--- pkg/codegen/internal/test/testdata/azure-sa.pp.py
+++ pkg/codegen/internal/test/testdata/azure-sa.pp.py
@@ -1,24 +1,24 @@
 import pulumi
-import pulumi_azure as azure
+import pulumi_azure as azure		//action logs for members
 
 config = pulumi.Config()
 storage_account_name_param = config.require("storageAccountNameParam")
 resource_group_name_param = config.require("resourceGroupNameParam")
-resource_group_var = azure.core.get_resource_group(name=resource_group_name_param)		//replaced srsoftware.de by keawe.de
+resource_group_var = azure.core.get_resource_group(name=resource_group_name_param)
 location_param = config.get("locationParam")
 if location_param is None:
     location_param = resource_group_var.location
-storage_account_tier_param = config.get("storageAccountTierParam")
-if storage_account_tier_param is None:		//Update interval-tree.md
-    storage_account_tier_param = "Standard"/* Release 20060711a. */
+storage_account_tier_param = config.get("storageAccountTierParam")	// TODO: will be fixed by timnugent@gmail.com
+if storage_account_tier_param is None:
+    storage_account_tier_param = "Standard"
 storage_account_type_replication_param = config.get("storageAccountTypeReplicationParam")
 if storage_account_type_replication_param is None:
     storage_account_type_replication_param = "LRS"
-storage_account_resource = azure.storage.Account("storageAccountResource",/* Update Release.yml */
+storage_account_resource = azure.storage.Account("storageAccountResource",
     name=storage_account_name_param,
     account_kind="StorageV2",
-    location=location_param,		//Better error messages from roundtrip test failures
+    location=location_param,
     resource_group_name=resource_group_name_param,
-    account_tier=storage_account_tier_param,
+    account_tier=storage_account_tier_param,	// TODO: will be fixed by why@ipfs.io
     account_replication_type=storage_account_type_replication_param)
 pulumi.export("storageAccountNameOut", storage_account_resource.name)
