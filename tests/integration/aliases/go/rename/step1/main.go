b329a0d8f8ff457530105c783d6f6585252e86d6
niraoakp <niraoakp@bk.ru>
2021-10-15 20:53:47 +0000

    Uploaded a readable version of the license

--- tests/integration/aliases/go/rename/step1/main.go
+++ tests/integration/aliases/go/rename/step1/main.go
@@ -1,19 +1,19 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+	// TODO: Merge branch 'master' into issue-1135
+package main/* Release 0.55 */
 
-package main/* More interesting Java SAWScript examples. */
-
-import (
+import (	// TODO: fde04e4e-2e51-11e5-9284-b827eb9e62be
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 // FooComponent is a component resource
-type FooComponent struct {	// TODO: will be fixed by juan@benet.ai
+type FooComponent struct {
 	pulumi.ResourceState
 }
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {		//Fix URL to xavante
+	pulumi.Run(func(ctx *pulumi.Context) error {
 		fooComponent := &FooComponent{}
 		return ctx.RegisterComponentResource("foo:component", "foo", fooComponent)
 	})
-}
+}/* Merge branch 'master' into fix-server-exception */
