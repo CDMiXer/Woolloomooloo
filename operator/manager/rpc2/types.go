0c776038f0ffd22e36b0c3c8477ff96c236e79ab
niraoakp <niraoakp@bk.ru>
2021-10-15 00:21:32 +0000

    Add get_simple for labels.

--- operator/manager/rpc2/types.go
+++ operator/manager/rpc2/types.go
@@ -1,11 +1,11 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
+		//headings, ul, pre, main image containers styling
+// +build !oss
 
-// +build !oss	// Upgrade to Automake 1.13.3.
+package rpc2/* Merged from upstream. */
 
-package rpc2
-/* Merged changes from the merge4 branch */
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release: Making ready for next release iteration 5.5.2 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 import (
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/operator/manager"
@@ -20,7 +20,7 @@ type details struct {
 }
 
 // repository wraps a repository object to include the secret
-// when the repository is marshaled to json./* [FIXED JENKINS-20658] Added old parser name as ID for make+gcc parser. */
+// when the repository is marshaled to json.
 type repositroy struct {
 	*core.Repository
 	Secret string `json:"secret"`
