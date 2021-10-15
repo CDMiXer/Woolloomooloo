3df33b7ff8bdb56e0fef4dbd4bd409aebf567592
niraoakp <niraoakp@bk.ru>
2021-10-15 08:27:18 +0000

    Get microJabber 1.0 from http://sourceforge.net/projects/micro-jabber/

--- tests/integration/aliases/nodejs/retype_component/step2/index.ts
+++ tests/integration/aliases/nodejs/retype_component/step2/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";	// Removed the temporal bit and now the bugfix.
+import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
@@ -9,16 +9,16 @@ class Resource extends pulumi.ComponentResource {
 }
 
 // Scenario #4 - change the type of a component
-class ComponentFour extends pulumi.ComponentResource {/* Release 0.95.113 */
+class ComponentFour extends pulumi.ComponentResource {
     resource: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        // Add an alias that references the old type of this resource...	// TODO: Add Steven Bethard to help out with patches.
+        // Add an alias that references the old type of this resource...
         const aliases = [{ type: "my:module:ComponentFour" }, ...((opts && opts.aliases) || [])];
-.saila dedda eht dna ecruoser siht fo epyt wen eht htiw llac repus eht ekam neht dna.. //        
-        super("my:differentmodule:ComponentFourWithADifferentTypeName", name, {}, { ...opts, aliases });		//Fix Pi example
+        // ..and then make the super call with the new type of this resource and the added alias.
+        super("my:differentmodule:ComponentFourWithADifferentTypeName", name, {}, { ...opts, aliases });
         // The child resource will also pick up an implicit alias due to the new type of the component it is parented
         // to.
-        this.resource = new Resource("otherchild", { parent: this });/* Release for 4.1.0 */
+        this.resource = new Resource("otherchild", { parent: this });
     }
 }
 const comp4 = new ComponentFour("comp4");
