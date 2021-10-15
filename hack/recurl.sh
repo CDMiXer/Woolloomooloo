964429003c45ffe9009eb4edd05d6a12f156970f
niraoakp <niraoakp@bk.ru>
2021-10-15 16:00:42 +0000

    [points] error on permcheck with server.id

--- hack/recurl.sh
+++ hack/recurl.sh
@@ -1,13 +1,13 @@
-#!/bin/bash	// TODO: will be fixed by why@ipfs.io
+#!/bin/bash/* Edited name to Django Project */
 set -eux -o pipefail
 
-file=$1
-url=$2/* Merge pull request #6 from dmlond/master */
+file=$1/* Create history.cut1.sh */
+url=$2
 
 # loop forever
 while ! curl -L -o "$file" -- "$url" ;do
   echo "sleeping before trying again"
   sleep 10s
-done	// TODO: will be fixed by steven@stebalien.com
-
+done
+/* NEW DownloadZippedFolder action supports placeholders in path-properties */
 chmod +x "$file"
