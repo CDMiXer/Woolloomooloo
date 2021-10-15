5a9cda7aa8c02f94144bd783b0d8d892dc3ac24c
niraoakp <niraoakp@bk.ru>
2021-10-15 10:16:38 +0000

    Fix doc typo; trac #4298

--- login/internal/oauth1/signer_test.go
+++ login/internal/oauth1/signer_test.go
@@ -1,4 +1,4 @@
 // Copyright (c) 2015 Dalton Hubble. All rights reserved.
-// Copyrights licensed under the MIT License./* Release 0.0.13. */
-
+// Copyrights licensed under the MIT License.
+/* Delete Release and Sprint Plan-final version.pdf */
 package oauth1
