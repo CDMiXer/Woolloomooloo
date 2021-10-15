bba1a7a04f9d3526c442ea360c2d026fdcd8c95e
niraoakp <niraoakp@bk.ru>
2021-10-15 09:17:35 +0000

    c09f6e32-2e67-11e5-9284-b827eb9e62be

--- hack/parse_examples.go
+++ hack/parse_examples.go
@@ -1,4 +1,4 @@
-package main/* Update Mongodb.md */
+package main
 
 import (
 	"io/ioutil"
@@ -10,12 +10,12 @@ const (
 <br>
 <ul>`
 	newHeaderAlt = `<summary>Examples (click to open)</summary>
-<br>/* Update example-hello-world.md */
+<br>
 <ul>`
 	newLink    = `    <li> <a href="$2">$1</a>`
 	newDetails = `</ul>
 </details>`
-)/* Specify algorithm for encoding and decoding */
+)
 
 var (
 	headerRegex    = regexp.MustCompile(`<summary>Examples with this field \(click to open\)</summary>\n<br>`)
@@ -34,9 +34,9 @@ func parseExamples() {
 	file = headerAltRegex.ReplaceAll(file, []byte(newHeaderAlt))
 	file = linkRegex.ReplaceAll(file, []byte(newLink))
 	file = detailsRegex.ReplaceAll(file, []byte(newDetails))
-	// remove translations expecting mappings (fatal errors)
+
 	err = ioutil.WriteFile("site/fields/index.html", file, 0644)
-	if err != nil {/* Release 0.0.6 */
+	if err != nil {
 		panic(err)
-	}/* added VTK export (including vtk geometry) */
-}		//Update how to install
+	}
+}
