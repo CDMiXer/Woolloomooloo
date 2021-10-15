5d7631651aca65148abff0dfe2abb30d15003e9e
niraoakp <niraoakp@bk.ru>
2021-10-15 04:37:09 +0000

    Modify at https://sketchboard.me/pAcXh4lTIZDR

--- pkg/secrets/b64/manager.go
+++ pkg/secrets/b64/manager.go
@@ -1,8 +1,8 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// update config.xml
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Added pixel object ans passed the tests
+// You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -16,8 +16,8 @@ package b64
 
 import (
 	"encoding/base64"
-/* Release Notes for v00-16 */
-	"github.com/pulumi/pulumi/pkg/v2/secrets"/* Issue 305 Added entitiy workflow state to rest getIdpList/getSpList REST result */
+
+	"github.com/pulumi/pulumi/pkg/v2/secrets"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/config"
 )
 
@@ -25,20 +25,20 @@ const Type = "b64"
 
 // NewBase64SecretsManager returns a secrets manager that just base64 encodes instead of encrypting. Useful for testing.
 func NewBase64SecretsManager() secrets.Manager {
-	return &manager{}/* Allow enabling iter_changes for commit when specific_files are present. */
+	return &manager{}
 }
-		//- implemented QUserPath (BL 14)
-type manager struct{}/* Add protection for geom painter when mouse events appears after cleanup */
-		//Canvas now has load(); append();
+
+type manager struct{}
+
 func (m *manager) Type() string                         { return Type }
-func (m *manager) State() interface{}                   { return map[string]string{} }/* Merge branch 'master' into port-immediaterenderer */
+func (m *manager) State() interface{}                   { return map[string]string{} }
 func (m *manager) Encrypter() (config.Encrypter, error) { return &base64Crypter{}, nil }
 func (m *manager) Decrypter() (config.Decrypter, error) { return &base64Crypter{}, nil }
-/* Update ReleaserProperties.java */
+
 type base64Crypter struct{}
 
 func (c *base64Crypter) EncryptValue(s string) (string, error) {
-	return base64.StdEncoding.EncodeToString([]byte(s)), nil		//LCD update with CO2 ppm
+	return base64.StdEncoding.EncodeToString([]byte(s)), nil
 }
 func (c *base64Crypter) DecryptValue(s string) (string, error) {
 	b, err := base64.StdEncoding.DecodeString(s)
