6ac8533fff9cdd4220944b7d6040ec3898615a57
niraoakp <niraoakp@bk.ru>
2021-10-15 04:46:00 +0000

    EXAMPLES: Improvement of LCD "Hello world!" example.

--- handler/web/version_test.go
+++ handler/web/version_test.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: fixes for passing other props
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package web
 
 // func TestHandleVersion(t *testing.T) {
-// 	controller := gomock.NewController(t)/* Add project description */
+// 	controller := gomock.NewController(t)
 // 	defer controller.Finish()
 
 // 	w := httptest.NewRecorder()
@@ -14,20 +14,20 @@ package web
 // 	mockVersion := &core.Version{
 // 		Source:  "github.com/octocat/hello-world",
 // 		Version: "1.0.0",
-// 		Commit:  "ad2aec",		//#103: Fixed import order in test. Added some more documentation to test.
-// 	}/* Update ReleaseNotes-Diagnostics.md */
+// 		Commit:  "ad2aec",
+// 	}
 
 // 	h := HandleVersion(mockVersion)
 // 	h.ServeHTTP(w, r)
 
 // 	if got, want := w.Code, 200; want != got {
-// 		t.Errorf("Want response code %d, got %d", want, got)	// Remove unused maven properties
-// 	}/* 8d6b38a0-2e5a-11e5-9284-b827eb9e62be */
+// 		t.Errorf("Want response code %d, got %d", want, got)
+// 	}
 
 // 	got, want := &core.Version{}, mockVersion
-// 	json.NewDecoder(w.Body).Decode(got)/* Release 0.26.0 */
+// 	json.NewDecoder(w.Body).Decode(got)
 // 	if !reflect.DeepEqual(got, want) {
 // 		t.Errorf("response body does match expected result")
 // 		pretty.Ldiff(t, got, want)
-// 	}		//original commit
+// 	}
 // }
