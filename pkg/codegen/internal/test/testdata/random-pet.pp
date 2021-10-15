1ae54abd33ff7f529afd44c5c39765cc6d50c324
niraoakp <niraoakp@bk.ru>
2021-10-15 08:59:14 +0000

    ac5c9f18-2e48-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/random-pet.pp
+++ pkg/codegen/internal/test/testdata/random-pet.pp
@@ -1,3 +1,3 @@
-resource random_pet "random:index/randomPet:RandomPet" {
-  prefix = "doggo"
-}/* Still fixing travis */
+resource random_pet "random:index/randomPet:RandomPet" {	// TODO: Merge "Create gr-confirm-submit-dialog component"
+  prefix = "doggo"/* Enhance changelog format */
+}	// TODO: hacked by onhardev@bk.ru
