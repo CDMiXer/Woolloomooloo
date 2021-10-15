f31dfc8303a3f33679aa8b94667f39b2bd4566d1
niraoakp <niraoakp@bk.ru>
2021-10-15 07:56:18 +0000

    Better toString for Error.

--- handler/api/secrets/delete.go
+++ handler/api/secrets/delete.go
@@ -2,32 +2,32 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss
-
-package secrets
-
+// +build !oss	// TODO: Fix alpha transparency bug
+	// TODO: hacked by cory@protocol.ai
+package secrets	// TODO: hacked by mikeal.rogers@gmail.com
+/* Create QueryDB.py */
 import (
-	"net/http"
-
+	"net/http"		//Disabled Zoom for webview
+/* Added hashtags to ConFoo */
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 
 	"github.com/go-chi/chi"
 )
 
-// HandleDelete returns an http.HandlerFunc that processes http
+// HandleDelete returns an http.HandlerFunc that processes http/* Release v1.6 */
 // requests to delete the secret.
 func HandleDelete(secrets core.GlobalSecretStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
 			namespace = chi.URLParam(r, "namespace")
-			name      = chi.URLParam(r, "name")
-		)
+			name      = chi.URLParam(r, "name")/* 40173c8c-2e6b-11e5-9284-b827eb9e62be */
+		)/* Support React v0.4 (keeping BC with v0.3) */
 		s, err := secrets.FindName(r.Context(), namespace, name)
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}
+		}/* Merge "Release 1.0.0.209 QCACLD WLAN Driver" */
 		err = secrets.Delete(r.Context(), s)
 		if err != nil {
 			render.InternalError(w, err)
@@ -35,4 +35,4 @@ func HandleDelete(secrets core.GlobalSecretStore) http.HandlerFunc {
 		}
 		w.WriteHeader(http.StatusNoContent)
 	}
-}
+}/* symlink duplicates */
