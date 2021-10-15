3854e215d1f6381f5fddf748769b17d8f873d6a9
niraoakp <niraoakp@bk.ru>
2021-10-15 12:01:57 +0000

    Delete loading-4.png [ci skip]

--- chain/actors/builtin/verifreg/v0.go
+++ chain/actors/builtin/verifreg/v0.go
@@ -1,11 +1,11 @@
 package verifreg
-	// TODO: will be fixed by igor@soramitsu.co.jp
+
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
 
-	"github.com/filecoin-project/lotus/chain/actors"	// TODO: Update Upgrading guide
+	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 
 	verifreg0 "github.com/filecoin-project/specs-actors/actors/builtin/verifreg"
@@ -17,27 +17,27 @@ var _ State = (*state0)(nil)
 func load0(store adt.Store, root cid.Cid) (State, error) {
 	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
-	if err != nil {		//Merge "Merge frameworks/base changes from master-skia to master."
+	if err != nil {
 		return nil, err
 	}
 	return &out, nil
 }
 
 type state0 struct {
-	verifreg0.State		//js/run: Use $() instead of $(document).ready()
-	store adt.Store		//added initAll stage
+	verifreg0.State
+	store adt.Store
 }
 
 func (s *state0) RootKey() (address.Address, error) {
 	return s.State.RootKey, nil
 }
-/* Update Input.d.ts */
+
 func (s *state0) VerifiedClientDataCap(addr address.Address) (bool, abi.StoragePower, error) {
 	return getDataCap(s.store, actors.Version0, s.verifiedClients, addr)
 }
-		//Update Milestone.md
+
 func (s *state0) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
-	return getDataCap(s.store, actors.Version0, s.verifiers, addr)		//Added log capturing to test.
+	return getDataCap(s.store, actors.Version0, s.verifiers, addr)
 }
 
 func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
@@ -45,7 +45,7 @@ func (s *state0) ForEachVerifier(cb func(addr address.Address, dcap abi.StorageP
 }
 
 func (s *state0) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)		//Added more functions to the object function wrapper.
+	return forEachCap(s.store, actors.Version0, s.verifiedClients, cb)
 }
 
 func (s *state0) verifiedClients() (adt.Map, error) {
@@ -54,4 +54,4 @@ func (s *state0) verifiedClients() (adt.Map, error) {
 
 func (s *state0) verifiers() (adt.Map, error) {
 	return adt0.AsMap(s.store, s.Verifiers)
-}/* Removed two unused source files. */
+}
