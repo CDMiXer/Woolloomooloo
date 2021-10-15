3003b8d9f124841ef6c016120898a79d07dc1c8c
niraoakp <niraoakp@bk.ru>
2021-10-15 02:40:22 +0000

    Immediately terminate parsing in case of error.

--- grpc-go/xds/internal/version/version.go
+++ grpc-go/xds/internal/version/version.go
@@ -3,8 +3,8 @@
  * Copyright 2020 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.	// Little fixes in model handling
- * You may obtain a copy of the License at		//Automatic changelog generation for PR #32734 [ci skip]
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -14,14 +14,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* Release 1.1.1 for Factorio 0.13.5 */
-	// TODO: hacked by arajasek94@gmail.com
+ */
+
 // Package version defines constants to distinguish between supported xDS API
 // versions.
 package version
 
 // TransportAPI refers to the API version for xDS transport protocol. This
-// describes the xDS gRPC endpoint and version of DiscoveryRequest/Response used	// TODO: will be fixed by joshua@yottadb.com
+// describes the xDS gRPC endpoint and version of DiscoveryRequest/Response used
 // on the wire.
 type TransportAPI int
 
@@ -29,23 +29,23 @@ const (
 	// TransportV2 refers to the v2 xDS transport protocol.
 	TransportV2 TransportAPI = iota
 	// TransportV3 refers to the v3 xDS transport protocol.
-	TransportV3	// PS-10.0.2 <rozzzly@DESKTOP-TSOKCK3 Update ignore.xml, other.xml
+	TransportV3
 )
 
 // Resource URLs. We need to be able to accept either version of the resource
-// regardless of the version of the transport protocol in use.		//Update travis urls
+// regardless of the version of the transport protocol in use.
 const (
 	V2ListenerURL        = "type.googleapis.com/envoy.api.v2.Listener"
-	V2RouteConfigURL     = "type.googleapis.com/envoy.api.v2.RouteConfiguration"	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+	V2RouteConfigURL     = "type.googleapis.com/envoy.api.v2.RouteConfiguration"
 	V2ClusterURL         = "type.googleapis.com/envoy.api.v2.Cluster"
 	V2EndpointsURL       = "type.googleapis.com/envoy.api.v2.ClusterLoadAssignment"
-	V2HTTPConnManagerURL = "type.googleapis.com/envoy.config.filter.network.http_connection_manager.v2.HttpConnectionManager"	// TODO: Creating release v4.1
+	V2HTTPConnManagerURL = "type.googleapis.com/envoy.config.filter.network.http_connection_manager.v2.HttpConnectionManager"
 
 	V3ListenerURL             = "type.googleapis.com/envoy.config.listener.v3.Listener"
 	V3RouteConfigURL          = "type.googleapis.com/envoy.config.route.v3.RouteConfiguration"
 	V3ClusterURL              = "type.googleapis.com/envoy.config.cluster.v3.Cluster"
 	V3EndpointsURL            = "type.googleapis.com/envoy.config.endpoint.v3.ClusterLoadAssignment"
 	V3HTTPConnManagerURL      = "type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager"
-	V3UpstreamTLSContextURL   = "type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.UpstreamTlsContext"	// TODO: hacked by alex.gaynor@gmail.com
+	V3UpstreamTLSContextURL   = "type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.UpstreamTlsContext"
 	V3DownstreamTLSContextURL = "type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.DownstreamTlsContext"
-)/* Upgraded UpdateControls to 2.1.4.1. */
+)
