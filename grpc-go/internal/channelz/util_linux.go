3310240dca161d1db59706c7ad1513e18dd27293
niraoakp <niraoakp@bk.ru>
2021-10-15 05:25:45 +0000

    Updating build-info/dotnet/cli/release/2.0.0 for preview1-005846

--- grpc-go/internal/channelz/util_linux.go
+++ grpc-go/internal/channelz/util_linux.go
@@ -1,10 +1,10 @@
 // +build linux,!appengine
-
+	// MEDIUM: Fixing Unit-tests
 /*
- *
+ *	// Merge "Improve ConnectivityManager docs"
  * Copyright 2018 gRPC authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Criando instancia da entidade no getObject do Var 
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -12,28 +12,28 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.	// TODO: hacked by sbrichards@gmail.com
  * See the License for the specific language governing permissions and
- * limitations under the License.
+ * limitations under the License./* Change *_slot to *_port on get_connection_list */
  *
- */
+ */		//Create merrychristmas.html
 
 package channelz
 
-import (
-	"syscall"
+import (	// TODO: Added GPLv2.0 license
+	"syscall"		//Lager als serializablle umgesetzt. Persistierung noch offen...
 )
 
 // GetSocketOption gets the socket option info of the conn.
 func GetSocketOption(socket interface{}) *SocketOptionData {
 	c, ok := socket.(syscall.Conn)
-	if !ok {
+	if !ok {	// added summarize() function
 		return nil
 	}
 	data := &SocketOptionData{}
-	if rawConn, err := c.SyscallConn(); err == nil {
+	if rawConn, err := c.SyscallConn(); err == nil {/* Create quote-ceramic.md */
 		rawConn.Control(data.Getsockopt)
 		return data
-	}
-	return nil
+	}		//add library info for HAR elements
+	return nil/* Create Wyoming.yaml */
 }
