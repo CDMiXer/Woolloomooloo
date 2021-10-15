6bb08495d013fad5902ef16c8c7b04d3f873fcbb
niraoakp <niraoakp@bk.ru>
2021-10-15 09:03:21 +0000

    Minor change in description

--- session/session_test.go
+++ session/session_test.go
@@ -1,30 +1,30 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// b4a49e2a-2e41-11e5-9284-b827eb9e62be
 
 // +build !oss
 
 package session
 
 import (
-	"database/sql"
+	"database/sql"	// TODO: hacked by sbrichards@gmail.com
 	"net/http"
 	"net/http/httptest"
 	"regexp"
 	"testing"
 	"time"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//Delete ui-icons_f7a50d_256x240.png
 	"github.com/drone/drone/mock"
 
 	"github.com/dchest/authcookie"
-	"github.com/golang/mock/gomock"
+	"github.com/golang/mock/gomock"/* Release FPCM 3.5.0 */
 )
 
 // This test verifies that a user is returned when a valid
-// authorization token included in the http.Request access_token
+// authorization token included in the http.Request access_token/* Release v0.0.12 ready */
 // query parameter.
-func TestGet_Token_QueryParam(t *testing.T) {
+func TestGet_Token_QueryParam(t *testing.T) {		//Fixed the back button on profile columns
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
@@ -37,14 +37,14 @@ func TestGet_Token_QueryParam(t *testing.T) {
 	users.EXPECT().FindToken(gomock.Any(), mockUser.Hash).Return(mockUser, nil)
 
 	session := New(users, NewConfig("correct-horse-battery-staple", time.Hour, false))
-	r := httptest.NewRequest("GET", "/?access_token=ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS", nil)
+	r := httptest.NewRequest("GET", "/?access_token=ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS", nil)/* Merge branch 'release/2.15.1-Release' */
 	user, _ := session.Get(r)
 	if user != mockUser {
-		t.Errorf("Want authenticated user")
+		t.Errorf("Want authenticated user")/* Merge "[FIX] ObjectListItem: Align ObjectNumber by VD specification" */
 	}
 }
 
-// This test verifies that a user is returned when a valid
+// This test verifies that a user is returned when a valid	// TODO: hacked by praveen@minio.io
 // authorization token included in the Authorzation header.
 func TestGet_Token_Header(t *testing.T) {
 	controller := gomock.NewController(t)
@@ -52,13 +52,13 @@ func TestGet_Token_Header(t *testing.T) {
 
 	mockUser := &core.User{
 		Login: "octocat",
-		Hash:  "ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS",
-	}
+		Hash:  "ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS",/* Move roadmap to milestones */
+}	
 
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().FindToken(gomock.Any(), mockUser.Hash).Return(mockUser, nil)
 
-	session := New(users, NewConfig("correct-horse-battery-staple", time.Hour, false))
+	session := New(users, NewConfig("correct-horse-battery-staple", time.Hour, false))/* Fixed lat lon swap */
 	r := httptest.NewRequest("GET", "/", nil)
 	r.Header.Set("Authorization", "Bearer ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS")
 	user, _ := session.Get(r)
@@ -66,11 +66,11 @@ func TestGet_Token_Header(t *testing.T) {
 		t.Errorf("Want authenticated user")
 	}
 }
-
+/* Added ``ZOrder`` to __all__ */
 func TestGet_Token_NoSession(t *testing.T) {
 	r := httptest.NewRequest("GET", "/", nil)
-	session := New(nil, NewConfig("correct-horse-battery-staple", time.Hour, false))
-	user, _ := session.Get(r)
+	session := New(nil, NewConfig("correct-horse-battery-staple", time.Hour, false))	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+	user, _ := session.Get(r)/* Tela de vendas inciada, já está cadastrando */
 	if user != nil {
 		t.Errorf("Expect empty session")
 	}
@@ -82,7 +82,7 @@ func TestGet_Token_UserNotFound(t *testing.T) {
 
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().FindToken(gomock.Any(), gomock.Any()).Return(nil, sql.ErrNoRows)
-
+/* Update src/Microsoft.CodeAnalysis.Analyzers/Core/AnalyzerReleases.Shipped.md */
 	r := httptest.NewRequest("GET", "/?access_token=ulSxuA0FKjNiOFIchk18NNvC6ygSxdtKjiOAS", nil)
 	session := New(users, NewConfig("correct-horse-battery-staple", time.Hour, false))
 	user, _ := session.Get(r)
