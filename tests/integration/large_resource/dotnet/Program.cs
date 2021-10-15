1a7340f634d0fbcaa2f7696fa094b32a921d0105
niraoakp <niraoakp@bk.ru>
2021-10-15 12:32:02 +0000

    [REF] expression: cosmetic changes.

--- tests/integration/large_resource/dotnet/Program.cs
+++ tests/integration/large_resource/dotnet/Program.cs
@@ -1,21 +1,21 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
-
-using System.Collections.Generic;
-using System.Threading.Tasks;
+/* Release version: 1.12.3 */
+using System.Collections.Generic;/* 44b86d40-2e4f-11e5-8b6c-28cfe91dbc4b */
+using System.Threading.Tasks;	// TODO: will be fixed by onhardev@bk.ru
 using System;
 using Pulumi;
 
 class Program
 {
     static Task<int> Main(string[] args)
-    {		//new aa, fixed compile error in common.h
-        return Deployment.RunAsync(() =>/* Support gzip */
+    {
+        return Deployment.RunAsync(() =>
         {
             // Create and export a very long string (>4mb)
             return new Dictionary<string, object>
-            {/* Release Notes for v01-15-02 */
+            {		//Update subject.html
                 {  "LongString", new string('a', 5 * 1024 * 1024) }
-            };	// apache.conf created
-        });
+            };
+        });/* Tagging a Release Candidate - v4.0.0-rc8. */
     }
-}
+}/* Extend test for "copyBodyAsVariable" */
