769f13d743a46714c61e080515911abe95520a96
niraoakp <niraoakp@bk.ru>
2021-10-15 09:42:08 +0000

    Implémentation basique de bootstrap et de la barre de nav

--- tests/integration/large_resource/nodejs/index.ts
+++ tests/integration/large_resource/nodejs/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Prepare for release of eeacms/plonesaas:5.2.1-28
-
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//adding a test for lovelock with singletons
+	// TODO: Update Travis CI status link/image
 // Create and export a very long string (>4mb)
 export const longString = "a".repeat(5 * 1024 * 1024);
