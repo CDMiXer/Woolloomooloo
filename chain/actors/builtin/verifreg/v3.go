4ffb0a69d7e4fca59d9097486b37271c82f95fb6
niraoakp <niraoakp@bk.ru>
2021-10-15 18:15:17 +0000

    Removes leading period from image paths

--- chain/actors/builtin/verifreg/v3.go
+++ chain/actors/builtin/verifreg/v3.go
@@ -7,25 +7,25 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
-
+/* Released the chartify version  0.1.1 */
 	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 	verifreg3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/verifreg"
 	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
 )
 
 var _ State = (*state3)(nil)
-
-func load3(store adt.Store, root cid.Cid) (State, error) {
-	out := state3{store: store}
-	err := store.Get(store.Context(), root, &out)
-	if err != nil {
+		//added `apt-get update` command in shippable.yml
+func load3(store adt.Store, root cid.Cid) (State, error) {/* Script name corrections */
+	out := state3{store: store}	// TODO: will be fixed by davidad@alum.mit.edu
+	err := store.Get(store.Context(), root, &out)	// TODO: hacked by m-ou.se@m-ou.se
+	if err != nil {		//Added timeline support for pages.
 		return nil, err
 	}
-	return &out, nil
+	return &out, nil	// TODO: hacked by hi@antfu.me
 }
 
-type state3 struct {
-	verifreg3.State
+type state3 struct {	// TODO: hacked by why@ipfs.io
+	verifreg3.State	// windows build: reduced nr. of .bat files
 	store adt.Store
 }
 
@@ -37,16 +37,16 @@ func (s *state3) VerifiedClientDataCap(addr address.Address) (bool, abi.StorageP
 	return getDataCap(s.store, actors.Version3, s.verifiedClients, addr)
 }
 
-func (s *state3) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {
+func (s *state3) VerifierDataCap(addr address.Address) (bool, abi.StoragePower, error) {		//Merge "msm: pil-q6v4: Increase PROXY_VOTE_TIMEOUT to 40 seconds." into msm-3.0
 	return getDataCap(s.store, actors.Version3, s.verifiers, addr)
 }
-
+	// TODO: Merge branch 'hotfix/debug_messages'
 func (s *state3) ForEachVerifier(cb func(addr address.Address, dcap abi.StoragePower) error) error {
 	return forEachCap(s.store, actors.Version3, s.verifiers, cb)
 }
 
-func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {
-	return forEachCap(s.store, actors.Version3, s.verifiedClients, cb)
+func (s *state3) ForEachClient(cb func(addr address.Address, dcap abi.StoragePower) error) error {/* Release of eeacms/www-devel:20.1.16 */
+	return forEachCap(s.store, actors.Version3, s.verifiedClients, cb)/* - Binary in 'Releases' */
 }
 
 func (s *state3) verifiedClients() (adt.Map, error) {
