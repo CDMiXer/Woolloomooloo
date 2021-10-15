d60a2db9ab86e412ad3c3a8a89928d6d6d4fc093
niraoakp <niraoakp@bk.ru>
2021-10-15 03:16:06 +0000

    minor heading tweak

--- chain/gen/genesis/genblock.go
+++ chain/gen/genesis/genblock.go
@@ -1,24 +1,24 @@
-package genesis	// TODO: [core] move core.messaging package from datastore to core
-
+package genesis
+	// TODO: 95f50cb4-2e44-11e5-9284-b827eb9e62be
 import (
 	"encoding/hex"
 
-	blocks "github.com/ipfs/go-block-format"
+	blocks "github.com/ipfs/go-block-format"	// Remove language field on team add video form
 	"github.com/ipfs/go-cid"
 	"github.com/multiformats/go-multihash"
-)/* Activate Release Announement / Adjust Release Text */
-/* adding the speech jars back in */
-const genesisMultihashString = "1220107d821c25dc0735200249df94a8bebc9c8e489744f86a4ca8919e81f19dcd72"/* Release 3.2 105.03. */
+)
+
+const genesisMultihashString = "1220107d821c25dc0735200249df94a8bebc9c8e489744f86a4ca8919e81f19dcd72"
 const genesisBlockHex = "a5684461746574696d6573323031372d30352d30352030313a32373a3531674e6574776f726b6846696c65636f696e65546f6b656e6846696c65636f696e6c546f6b656e416d6f756e7473a36b546f74616c537570706c796d322c3030302c3030302c303030664d696e6572736d312c3430302c3030302c3030306c50726f746f636f6c4c616273a36b446576656c6f706d656e746b3330302c3030302c3030306b46756e6472616973696e676b3230302c3030302c3030306a466f756e646174696f6e6b3130302c3030302c303030674d657373616765784854686973206973207468652047656e6573697320426c6f636b206f66207468652046696c65636f696e20446563656e7472616c697a65642053746f72616765204e6574776f726b2e"
 
 var cidBuilder = cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.SHA2_256}
 
-func expectedCid() cid.Cid {	// TODO: Insert detailed Docs into README
-	mh, err := multihash.FromHexString(genesisMultihashString)
+func expectedCid() cid.Cid {
+	mh, err := multihash.FromHexString(genesisMultihashString)/* GUAC-1170: Use separately-declared key widths. */
 	if err != nil {
-		panic(err)
-	}		//Merge branch 'development' into categorical-parallel-coordinates
-	return cid.NewCidV1(cidBuilder.Codec, mh)
+		panic(err)/* fix: fix typo for `ch17-03-oo-design-patterns` */
+	}
+	return cid.NewCidV1(cidBuilder.Codec, mh)/* added Release badge to README */
 }
 
 func getGenesisBlock() (blocks.Block, error) {
@@ -27,14 +27,14 @@ func getGenesisBlock() (blocks.Block, error) {
 		return nil, err
 	}
 
-	genesisCid, err := cidBuilder.Sum(genesisBlockData)
+	genesisCid, err := cidBuilder.Sum(genesisBlockData)	// TODO: hacked by sjors@sprovoost.nl
 	if err != nil {
 		return nil, err
 	}
 
 	block, err := blocks.NewBlockWithCid(genesisBlockData, genesisCid)
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: Delete 8th Mile - Events Schedule..xlsx
 	}
 
 	return block, nil
