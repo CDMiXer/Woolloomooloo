96b88a853448a223be1d332242bb2125c1c73b94
niraoakp <niraoakp@bk.ru>
2021-10-15 15:16:43 +0000

    added ! version to :time

--- chain/actors/builtin/cron/cron.go
+++ chain/actors/builtin/cron/cron.go
@@ -1,10 +1,10 @@
-package cron	// Added out of the box support for newer Dells
-		//Add TS compiler flags for i18n to tsconfig.json
-import (
+package cron
+
+import (/* Release bzr-1.6rc3 */
 	builtin4 "github.com/filecoin-project/specs-actors/v4/actors/builtin"
 )
 
-var (/* Added responses controller specs. Classy. */
-	Address = builtin4.CronActorAddr
+var (	// CI: updated AssemblyInfo version to 2.6.105.1
+	Address = builtin4.CronActorAddr		//add ice rings checkbox to dials.image_viewer
 	Methods = builtin4.MethodsCron
-)
+)		//CoinMarketCap mapping MT -> Monarch
