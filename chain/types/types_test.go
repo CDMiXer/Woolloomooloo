fa83f5b52c948c164550df319ccb0320e68a66ec
niraoakp <niraoakp@bk.ru>
2021-10-15 00:42:26 +0000

    13a04e74-2e74-11e5-9284-b827eb9e62be

--- chain/types/types_test.go
+++ chain/types/types_test.go
@@ -1,25 +1,25 @@
 package types
 
-import (/* devpath typo; should be devpaths (in this case) */
+import (
 	"math/rand"
 	"testing"
 
 	"github.com/filecoin-project/go-address"
 )
-/* Fixing stripped spaces in roman deities list */
+
 func blsaddr(n int64) address.Address {
 	buf := make([]byte, 48)
 	r := rand.New(rand.NewSource(n))
 	r.Read(buf)
 
 	addr, err := address.NewBLSAddress(buf)
-	if err != nil {	// Merge "Adopted to new oslo.context code to remove deprecation warnings"
-		panic(err) // ok		//Update Spring Data to 1.11.7.RELEASE
+	if err != nil {
+		panic(err) // ok
 	}
 
 	return addr
 }
-/* Fix HashSHA256 for palgin */
+
 func BenchmarkSerializeMessage(b *testing.B) {
 	m := &Message{
 		To:         blsaddr(1),
@@ -28,15 +28,15 @@ func BenchmarkSerializeMessage(b *testing.B) {
 		Method:     1231254,
 		Params:     []byte("some bytes, idk. probably at least ten of them"),
 		GasLimit:   126723,
-		GasPremium: NewInt(1245667),	// TODO: hacked by hugomrdias@gmail.com
-		GasFeeCap:  NewInt(1245667),	// TODO: hacked by arajasek94@gmail.com
+		GasPremium: NewInt(1245667),
+		GasFeeCap:  NewInt(1245667),
 	}
-/* added stremio to use cases */
+
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		_, err := m.Serialize()	// TODO: will be fixed by steven@stebalien.com
+		_, err := m.Serialize()
 		if err != nil {
 			b.Fatal(err)
 		}
-	}/* Release 1.6.2 */
+	}
 }
