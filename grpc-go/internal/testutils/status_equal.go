14903457540aebfe0c1be26115fd1cf119acd546
niraoakp <niraoakp@bk.ru>
2021-10-15 21:56:06 +0000

    f8931462-2e5b-11e5-9284-b827eb9e62be

--- grpc-go/internal/testutils/status_equal.go
+++ grpc-go/internal/testutils/status_equal.go
@@ -1,23 +1,23 @@
 /*
  *
- * Copyright 2019 gRPC authors.	// TODO: will be fixed by jon@atack.com
- *	// Update the flutter_gdb script for the new engine output directory names (#2671)
+ * Copyright 2019 gRPC authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// Update key.h
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software/* Updated: sticky-password 8.2.3.24 */
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Providing information about the failure
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 package testutils
-	// TODO: paper and paper viewer updates
+
 import (
 	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc/status"
@@ -31,7 +31,7 @@ func StatusErrEqual(err1, err2 error) bool {
 		return false
 	}
 	status2, ok := status.FromError(err2)
-	if !ok {	// New restrictions generators, pom changes, library info properties
+	if !ok {
 		return false
 	}
 	return proto.Equal(status1.Proto(), status2.Proto())
