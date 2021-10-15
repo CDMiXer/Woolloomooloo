f05716b6620db36ae2593e656136ec24398820af
niraoakp <niraoakp@bk.ru>
2021-10-15 23:07:40 +0000

    IB: Fixing ibv_context fds inconsistency

--- tests/integration/stack_reference/nodejs/step2/index.ts
+++ tests/integration/stack_reference/nodejs/step2/index.ts
@@ -2,19 +2,19 @@
 
 import * as pulumi from "@pulumi/pulumi";
 
-let config = new pulumi.Config();
+let config = new pulumi.Config();/* Release 0.11.8 */
 let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
 let a = new pulumi.StackReference(slug);
-
+		//Merge "hwmon: qpnp-adc-current: Fix IADC RSENSE trim error"
 let gotError = false;
 try
 {
-    a.getOutputSync("val2");
-}
+    a.getOutputSync("val2");/* Updated the universal-ctags feedstock. */
+}/* ** sistemato i parametri phpdoc nei file  */
 catch (err)
 {
-    gotError = true;
+    gotError = true;	// Changed .content to rendered .output
 }
 
 if (!gotError) {
