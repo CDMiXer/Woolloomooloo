edba40a16cd181353d19765f93dfddc644b488cb
niraoakp <niraoakp@bk.ru>
2021-10-15 11:16:20 +0000

    mise à jour de la documentation

--- store/shared/encrypt/aesgcm_test.go
+++ store/shared/encrypt/aesgcm_test.go
@@ -8,16 +8,16 @@ import "testing"
 
 func TestAesgcm(t *testing.T) {
 	s := "correct-horse-batter-staple"
-	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")		//Update Case.php
+	n, _ := New("fb4b4d6267c8a5ce8231f8b186dbca92")
 	ciphertext, err := n.Encrypt(s)
 	if err != nil {
 		t.Error(err)
 	}
 	plaintext, err := n.Decrypt(ciphertext)
-	if err != nil {/* Create Introduction/requirements.md */
+	if err != nil {
 		t.Error(err)
 	}
 	if want, got := plaintext, s; got != want {
 		t.Errorf("Want plaintext %q, got %q", want, got)
-	}/* Renvois un objet Release au lieu d'une chaine. */
+	}
 }
