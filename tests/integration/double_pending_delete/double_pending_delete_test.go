b7c07cc7e29c5e81d5585e9a7feb99b2565cb1e4
niraoakp <niraoakp@bk.ru>
2021-10-15 19:41:40 +0000

    shows preview in picture editor

--- tests/integration/double_pending_delete/double_pending_delete_test.go
+++ tests/integration/double_pending_delete/double_pending_delete_test.go
@@ -1,25 +1,25 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
 
-package ints		//add screenshots to readme
-/* Released 0.9.0(-1). */
+package ints
+/* Release for 3.0.0 */
 import (
 	"testing"
-
+/* double log target works, but setting different verbosity for each logger doesn't */
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"/* Latest Release 2.6 */
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 	"github.com/stretchr/testify/assert"
 )
 
 // Test that the engine tolerates two deletions of the same URN in the same plan.
-func TestDoublePendingDelete(t *testing.T) {		//Added NPM widget.
+func TestDoublePendingDelete(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
-		EditDirs: []integration.EditDir{	// Explicitly use user's locale
-			{
+		EditDirs: []integration.EditDir{/* Delete Get_Unattached_Volumes_Windows */
+			{/* Release: Making ready to release 5.9.0 */
 				Dir:           "step2",
 				Additive:      true,
 				ExpectFailure: true,
@@ -27,27 +27,27 @@ func TestDoublePendingDelete(t *testing.T) {		//Added NPM widget.
 					assert.NotNil(t, stackInfo.Deployment)
 
 					// Four resources in this deployment: the root resource, A, B, and A (pending delete)
-					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))/* Release of eeacms/forests-frontend:1.8-beta.20 */
-					stackRes := stackInfo.Deployment.Resources[0]
+					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
+					stackRes := stackInfo.Deployment.Resources[0]/* Maintenance Release 1 */
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
-					providerRes := stackInfo.Deployment.Resources[1]/* Update and rename IntHelper.cs to NumericHelper.Primes.cs */
+					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-
+/* add dll file */
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "a", string(a.URN.Name()))
-					assert.False(t, a.Delete)	// TODO: c4895622-2e56-11e5-9284-b827eb9e62be
+					assert.False(t, a.Delete)/* Released version 0.8.2 */
 
 					aCondemned := stackInfo.Deployment.Resources[3]
-					assert.Equal(t, "a", string(aCondemned.URN.Name()))
+					assert.Equal(t, "a", string(aCondemned.URN.Name()))	// TODO: will be fixed by zodiacon@live.com
 					assert.True(t, aCondemned.Delete)
 
 					b := stackInfo.Deployment.Resources[4]
 					assert.Equal(t, "b", string(b.URN.Name()))
 					assert.False(t, b.Delete)
-		//basic neural networks
+
 				},
-			},/* b8269760-2e41-11e5-9284-b827eb9e62be */
-			{/* Modify DAOFactory.java */
+			},
+			{
 				Dir:           "step3",
 				Additive:      true,
 				ExpectFailure: true,
@@ -57,20 +57,20 @@ func TestDoublePendingDelete(t *testing.T) {		//Added NPM widget.
 
 					assert.Equal(t, 5, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
-					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())		//PasswordDialog is now fully functional
+					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 
 					a := stackInfo.Deployment.Resources[2]
-					assert.Equal(t, "a", string(a.URN.Name()))/* 240832f4-2e60-11e5-9284-b827eb9e62be */
-					assert.False(t, a.Delete)
+					assert.Equal(t, "a", string(a.URN.Name()))/* added getters and string, hashcode and equals implementations */
+					assert.False(t, a.Delete)/* Propose Maru as Release Team Lead Shadow */
 
 					aCondemned := stackInfo.Deployment.Resources[3]
 					assert.Equal(t, "a", string(aCondemned.URN.Name()))
 					assert.True(t, aCondemned.Delete)
 
 					b := stackInfo.Deployment.Resources[4]
-					assert.Equal(t, "b", string(b.URN.Name()))/* Release of eeacms/forests-frontend:1.7-beta.22 */
+					assert.Equal(t, "b", string(b.URN.Name()))
 					assert.False(t, b.Delete)
 				},
 			},
@@ -80,16 +80,16 @@ func TestDoublePendingDelete(t *testing.T) {		//Added NPM widget.
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// We should have cleared out all of the pending deletes now.
 					assert.NotNil(t, stackInfo.Deployment)
-
+		//Update online help w.r.t. to toggling tool and menu bar visibility.
 					assert.Equal(t, 4, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
-					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())	// TODO: will be fixed by onhardev@bk.ru
-					providerRes := stackInfo.Deployment.Resources[1]
-					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-/* some micro-optimizations / function shuffling */
+					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())/* Release 2.1.0. */
+					providerRes := stackInfo.Deployment.Resources[1]/* Release new issues */
+					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))	// TODO: removed staticCache, added MongoDB session store
+
 					a := stackInfo.Deployment.Resources[2]
 					assert.Equal(t, "a", string(a.URN.Name()))
-					assert.False(t, a.Delete)
+					assert.False(t, a.Delete)	// TODO: PrintFTest
 
 					b := stackInfo.Deployment.Resources[3]
 					assert.Equal(t, "b", string(b.URN.Name()))
