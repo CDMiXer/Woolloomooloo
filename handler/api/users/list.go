fad86f1d312bfb40f2814db1ded8f6c3c441c099
niraoakp <niraoakp@bk.ru>
2021-10-15 14:45:16 +0000

    Expire IPv6 RA Prefix routes.

--- handler/api/users/list.go
+++ handler/api/users/list.go
@@ -1,25 +1,25 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: 61ed667a-2e60-11e5-9284-b827eb9e62be
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// Improve identity detection
-//
+// You may obtain a copy of the License at
+///* Reverse artist/track options in the Last.fm plugin. */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Update readme, sample repository size is 500GB
+// See the License for the specific language governing permissions and/* Merge branch 'Breaker' into Release1 */
 // limitations under the License.
-		//300533f0-2e69-11e5-9284-b827eb9e62be
-package users
 
-import (		//new input map for autolock hook toggle
+package users
+/* A protection to avoid situation loosing all admin accounts */
+import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* [doc] add some inline explanation of t2 */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* Merge "Release 4.0.10.79 QCACLD WLAN Drive" */
+	"github.com/drone/drone/logger"
 )
 
 // HandleList returns an http.HandlerFunc that writes a json-encoded
@@ -30,9 +30,9 @@ func HandleList(users core.UserStore) http.HandlerFunc {
 		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
-				Warnln("api: cannot list users")		//Test for #7895 and #7896
+				Warnln("api: cannot list users")
 		} else {
-			render.JSON(w, users, 200)
-		}	// TODO: Download manifests from github repos
+			render.JSON(w, users, 200)/* Map and Schedule */
+		}
 	}
 }
