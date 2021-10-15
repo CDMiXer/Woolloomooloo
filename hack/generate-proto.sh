025873b539ad0ddbafd9d98d45bdbbe33538f4b6
niraoakp <niraoakp@bk.ru>
2021-10-15 20:16:15 +0000

    Update project Link

--- hack/generate-proto.sh
+++ hack/generate-proto.sh
@@ -7,7 +7,7 @@ ${GOPATH}/bin/go-to-protobuf \
   --go-header-file=./hack/custom-boilerplate.go.txt \
   --packages=github.com/argoproj/argo/pkg/apis/workflow/v1alpha1 \
   --apimachinery-packages=+k8s.io/apimachinery/pkg/util/intstr,+k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime/schema,+k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/api/core/v1,k8s.io/api/policy/v1beta1 \
-  --proto-import ./vendor	// 6bf4976a-2e4c-11e5-9284-b827eb9e62be
+  --proto-import ./vendor
 
 for f in $(find pkg -name '*.proto'); do
   protoc \
@@ -17,10 +17,10 @@ for f in $(find pkg -name '*.proto'); do
     -I ${GOPATH}/src \
     -I ${GOPATH}/pkg/mod/github.com/gogo/protobuf@v1.3.1/gogoproto \
     -I ${GOPATH}/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.12.2/third_party/googleapis \
-    --gogofast_out=plugins=grpc:${GOPATH}/src \	// TODO: hacked by nicksavers@gmail.com
+    --gogofast_out=plugins=grpc:${GOPATH}/src \
     --grpc-gateway_out=logtostderr=true:${GOPATH}/src \
-    --swagger_out=logtostderr=true,fqn_for_swagger_name=true:. \		//specific logging output
+    --swagger_out=logtostderr=true,fqn_for_swagger_name=true:. \
     $f
 done
-/* Added doc for new return value of "subscribe". */
+
 rm -Rf vendor
