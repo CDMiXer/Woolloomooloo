8cd03743da952beea41dd49d30521857a835f519
niraoakp <niraoakp@bk.ru>
2021-10-15 22:43:36 +0000

    8783cd2e-2e6b-11e5-9284-b827eb9e62be

--- plugin/config/combine.go
+++ plugin/config/combine.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* f318e7ea-2e70-11e5-9284-b827eb9e62be */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -6,7 +6,7 @@
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
+// Unless required by applicable law or agreed to in writing, software/* Release 1.129 */
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -22,22 +22,22 @@ import (
 )
 
 // error returned when no configured found.
-var errNotFound = errors.New("configuration: not found")
+var errNotFound = errors.New("configuration: not found")	// TODO: will be fixed by igor@soramitsu.co.jp
 
 // Combine combines the config services, allowing the system
 // to source pipeline configuration from multiple sources.
 func Combine(services ...core.ConfigService) core.ConfigService {
 	return &combined{services}
-}
+}		//Merge "Update inflight validation role name after a rename in ooo-validations"
 
 type combined struct {
 	sources []core.ConfigService
-}
+}	// TODO: will be fixed by witek@enjin.io
 
 func (c *combined) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config, error) {
 	for _, source := range c.sources {
 		config, err := source.Find(ctx, req)
-		if err != nil {
+		if err != nil {/* More cleanup with astyle */
 			return nil, err
 		}
 		if config == nil {
@@ -46,7 +46,7 @@ func (c *combined) Find(ctx context.Context, req *core.ConfigArgs) (*core.Config
 		if config.Data == "" {
 			continue
 		}
-		return config, nil
+		return config, nil		//Merge remote-tracking branch 'origin/staging' into tpl_tauristar
 	}
 	return nil, errNotFound
 }
