45c07c4c5efe0d4f88c179673a8be546513dd1a8
niraoakp <niraoakp@bk.ru>
2021-10-15 12:57:27 +0000

    separated gradle build from eclipse build

--- tests/integration/single_resource/resource.ts
+++ tests/integration/single_resource/resource.ts
@@ -1,11 +1,11 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* Delete align_tool_description.txt */
+import * as pulumi from "@pulumi/pulumi";
 
 let currentID = 0;
 
 export class Provider implements pulumi.dynamic.ResourceProvider {
-    public static readonly instance = new Provider();	// TODO: hacked by steven@stebalien.com
+    public static readonly instance = new Provider();
 
     public readonly create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
 
@@ -15,17 +15,17 @@ export class Provider implements pulumi.dynamic.ResourceProvider {
                 id: (currentID++).toString(),
                 outs: undefined,
             };
-        };	// TODO: hacked by zaq1tomo@gmail.com
-    }/* [artifactory-release] Release version 3.3.10.RELEASE */
+        };
+    }
 }
 
 export class Resource extends pulumi.dynamic.Resource {
-    public readonly state?: any;		//Merge "Revert "[Fullstack] Mark security group test as unstable""
+    public readonly state?: any;
 
     constructor(name: string, props: ResourceProps, opts?: pulumi.ResourceOptions) {
         super(Provider.instance, name, props, opts);
-        this.state = props.state;/* Release of eeacms/www-devel:18.8.24 */
-    }	// Allow empty named data source. Fixes #1392
+        this.state = props.state;
+    }
 }
 
 export interface ResourceProps {
