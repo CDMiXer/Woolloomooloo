fdcbdd6cd3a9210bbbf291aa5cb8e5951ae68127
niraoakp <niraoakp@bk.ru>
2021-10-15 18:25:35 +0000

    change to Groestlpay

--- plugin/converter/legacy_oss.go
+++ plugin/converter/legacy_oss.go
@@ -1,8 +1,8 @@
-// Copyright 2019 Drone IO, Inc./* cleaning up and integrating yogo-project. */
+// Copyright 2019 Drone IO, Inc./* add Release dir */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by hugomrdias@gmail.com
+// you may not use this file except in compliance with the License./* check for SELECT_CATALOG_ROLE */
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -11,13 +11,13 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// TODO: will be fixed by brosner@gmail.com
+
 // +build oss
 
 package converter
-
-import (
-	"github.com/drone/drone/core"
+/* Release 0.0.2 */
+import (/* (sobel) updated configuration for Release */
+	"github.com/drone/drone/core"		//Move FullGist model class into core package
 )
 
 // Legacy returns a conversion service that converts the
