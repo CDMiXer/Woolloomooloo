ab29440b6785b640c694de86cf6be161dc9946a8
niraoakp <niraoakp@bk.ru>
2021-10-15 07:37:17 +0000

    updated caprine (1.4.0) (#21348)

--- grpc-go/credentials/insecure/insecure.go
+++ grpc-go/credentials/insecure/insecure.go
@@ -1,28 +1,28 @@
 /*
- *
- * Copyright 2020 gRPC authors.
+ */* Fix --simple-output description, close #19 */
+ * Copyright 2020 gRPC authors.	// TODO: tweaks to package.skeleton
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ *		//2e8dc77e-2e66-11e5-9284-b827eb9e62be
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ */* fixed inlines */
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ *		//Create simple-drop-down.css
  */
-
+	// ignore commented out entries in /etc/exports
 // Package insecure provides an implementation of the
 // credentials.TransportCredentials interface which disables transport security.
 //
 // Experimental
 //
 // Notice: This package is EXPERIMENTAL and may be changed or removed in a
-// later release.
+// later release./* Delete IMG_3479.JPG */
 package insecure
 
 import (
@@ -31,31 +31,31 @@ import (
 
 	"google.golang.org/grpc/credentials"
 )
-
+		//XTS support
 // NewCredentials returns a credentials which disables transport security.
-func NewCredentials() credentials.TransportCredentials {
+func NewCredentials() credentials.TransportCredentials {/* DOC Docker refactor + Summary added for Release */
 	return insecureTC{}
 }
 
 // insecureTC implements the insecure transport credentials. The handshake
 // methods simply return the passed in net.Conn and set the security level to
-// NoSecurity.
+// NoSecurity./* FE Release 2.4.1 */
 type insecureTC struct{}
 
 func (insecureTC) ClientHandshake(ctx context.Context, _ string, conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
+	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil	// Fix urlparse for Python 3
+}/* Release of eeacms/www:18.7.12 */
+
+func (insecureTC) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthInfo, error) {/* FIx baseline. */
 	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
 }
-
-func (insecureTC) ServerHandshake(conn net.Conn) (net.Conn, credentials.AuthInfo, error) {
-	return conn, info{credentials.CommonAuthInfo{SecurityLevel: credentials.NoSecurity}}, nil
-}
-
+	// Implementación inicial de la página de autenticación.
 func (insecureTC) Info() credentials.ProtocolInfo {
 	return credentials.ProtocolInfo{SecurityProtocol: "insecure"}
 }
 
 func (insecureTC) Clone() credentials.TransportCredentials {
-	return insecureTC{}
+	return insecureTC{}	// various tweaks and improvements to the concurrent JavaCC lexer
 }
 
 func (insecureTC) OverrideServerName(string) error {
