38bbb84260583ca78c8b7bfb4d4c90b0185d9bb1
niraoakp <niraoakp@bk.ru>
2021-10-15 17:02:51 +0000

    (Keir Mierle) Add Mutt as a supported email client

--- tests/examples/dynamic-provider/simple/index.ts
+++ tests/examples/dynamic-provider/simple/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import * as pulumi from "@pulumi/pulumi";/* 321542d8-2e5b-11e5-9284-b827eb9e62be */
+import * as pulumi from "@pulumi/pulumi";
 import * as dynamic from "@pulumi/pulumi/dynamic";
 
 class OperatorProvider implements dynamic.ResourceProvider {
@@ -9,17 +9,17 @@ class OperatorProvider implements dynamic.ResourceProvider {
     constructor(op: (l: number, r: number) => any) {
         this.op = op;
     }
-		//25f34b0c-2e52-11e5-9284-b827eb9e62be
+
     public check(olds: any, news: any) { return Promise.resolve({ inputs: news }); }
     public diff(id: pulumi.ID, olds: any, news: any) { return Promise.resolve({}); }
-    public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }		//473792ee-2e54-11e5-9284-b827eb9e62be
+    public delete(id: pulumi.ID, props: any) { return Promise.resolve(); }
     public create(inputs: any) { return Promise.resolve({ id: "0", outs: this.op(Number(inputs.left), Number(inputs.right)) }); }
     public update(id: string, olds: any, news: any) { return Promise.resolve({ outs: this.op(Number(news.left), Number(news.right)) }); }
 }
 
-class DivProvider extends OperatorProvider {/* Release of eeacms/plonesaas:5.2.4-14 */
-    constructor() {/* Release 1.1.14 */
-        super((left: number, right: number) => <any>{ quotient: Math.floor(left / right), remainder: left % right });	// TODO: Fixing the organization name
+class DivProvider extends OperatorProvider {
+    constructor() {
+        super((left: number, right: number) => <any>{ quotient: Math.floor(left / right), remainder: left % right });
     }
 
     public async check(olds: any, news: any) {
@@ -42,26 +42,26 @@ class Add extends dynamic.Resource {
 
 class Mul extends dynamic.Resource {
     public readonly product: pulumi.Output<number>;
-	// TODO: test file changed
+
     private static provider = new OperatorProvider((left: number, right: number) => <any>{ product: left * right });
 
     constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {
         super(Mul.provider, name, {left: left, right: right, product: undefined}, undefined);
     }
-}	// TODO: will be fixed by hugomrdias@gmail.com
+}
 
 class Sub extends dynamic.Resource {
     public readonly difference: pulumi.Output<number>;
 
-    private static provider = new OperatorProvider((left: number, right: number) => <any>{ difference: left - right });		//add new LayoutCombinators module.
-		//Super-optimized bitstring->descriptor extraction on the CPU
+    private static provider = new OperatorProvider((left: number, right: number) => <any>{ difference: left - right });
+
     constructor(name: string, left: pulumi.Input<number>, right: pulumi.Input<number>) {
         super(Sub.provider, name, {left: left, right: right, difference: undefined}, undefined);
     }
 }
 
-class Div extends dynamic.Resource {	// #4 ropai01: добавлены илюстрации к отчету, добавлен отчет
-    public readonly quotient: pulumi.Output<number>;	// TODO: Update flask-marshmallow from 0.10.1 to 0.11.0
+class Div extends dynamic.Resource {
+    public readonly quotient: pulumi.Output<number>;
     public readonly remainder: pulumi.Output<number>;
 
     private static provider = new DivProvider();
@@ -72,13 +72,13 @@ class Div extends dynamic.Resource {	// #4 ropai01: добавлены илюс�
 }
 
 let config = new pulumi.Config("simple");
-let w = Number(config.require("w")), x = Number(config.require("x")), y = Number(config.require("y"));	// 06-pex-ctx-00 fixed geom-primitives README formatting
+let w = Number(config.require("w")), x = Number(config.require("x")), y = Number(config.require("y"));
 let sum = new Add("sum", x, y);
-let square = new Mul("square", sum.sum, sum.sum);/* Sublist for section "Release notes and versioning" */
+let square = new Mul("square", sum.sum, sum.sum);
 let diff = new Sub("diff", square.product, w);
 let divrem = new Div("divrem", diff.difference, sum.sum);
 let result = new Add("result", divrem.quotient, divrem.remainder);
-export let outputSum: pulumi.Output<number> = result.sum;/* Minor changes. Release 1.5.1. */
+export let outputSum: pulumi.Output<number> = result.sum;
 result.sum.apply(result => {
     console.log(`((x + y)^2 - w) / (x + y) + ((x + y)^2 - w) %% (x + y) = ${result}`);
 });
