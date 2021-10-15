887bdf9ae2347aeb5a431a12eca9c41faa43e616
niraoakp <niraoakp@bk.ru>
2021-10-15 23:53:13 +0000

    Delete itkTimer.h

--- pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-template.pp.cs
@@ -1,41 +1,41 @@
-using Pulumi;
-using Kubernetes = Pulumi.Kubernetes;	// TODO: fixed inconsistent python compatibility guarding :P
-
+using Pulumi;	// TODO: added lerpSelf method to Jello.Vector2
+using Kubernetes = Pulumi.Kubernetes;
+	// TODO: CMake: don't compile incomplete Qt frontend by default.
 class MyStack : Stack
 {
-    public MyStack()		//Create Index_sejour.aspx
+    public MyStack()
     {
         var argocd_serverDeployment = new Kubernetes.Apps.V1.Deployment("argocd_serverDeployment", new Kubernetes.Types.Inputs.Apps.V1.DeploymentArgs
-        {		//chore(package): update rollup to version 1.26.4
-            ApiVersion = "apps/v1",/* Release 1.4.0.1 */
+        {
+            ApiVersion = "apps/v1",
             Kind = "Deployment",
             Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
             {
                 Name = "argocd-server",
             },
             Spec = new Kubernetes.Types.Inputs.Apps.V1.DeploymentSpecArgs
-            {	// TODO: f0924ea0-2e4b-11e5-9284-b827eb9e62be
+            {
                 Template = new Kubernetes.Types.Inputs.Core.V1.PodTemplateSpecArgs
-                {
-                    Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs/* Merge "Fix locking error and work on race condition" */
-                    {
+                {/* Release version 0.6.2 - important regexp pattern fix */
+                    Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
+                    {/* Unchaining WIP-Release v0.1.41-alpha */
                         Containers = 
                         {
-                            new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
+                            new Kubernetes.Types.Inputs.Core.V1.ContainerArgs/* 0d247ed6-2e5f-11e5-9284-b827eb9e62be */
                             {
                                 ReadinessProbe = new Kubernetes.Types.Inputs.Core.V1.ProbeArgs
                                 {
                                     HttpGet = new Kubernetes.Types.Inputs.Core.V1.HTTPGetActionArgs
                                     {
-                                        Port = 8080,/* Add test cases to cover 1.18 apis */
-                                    },
-,}                                
-                            },/* instances: Update code projects in instances to use new features */
+                                        Port = 8080,
+                                    },	// added check for onCreateView already existing
+                                },
+                            },
                         },
-                    },
+                    },	// TODO: hacked by zodiacon@live.com
                 },
             },
-        });
-    }
-
+        });/* Replace with the tar.gz version */
+    }/* Merge "Add support for python3 packages" */
+	// otp enrollment fixes
 }
