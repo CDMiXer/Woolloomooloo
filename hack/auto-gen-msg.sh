7c2fcb85cd2f1521d3d674038b0784d0567965b4
niraoakp <niraoakp@bk.ru>
2021-10-15 06:47:00 +0000

    Remove AMPL samples using .mod, .dat and .run extensions

--- hack/auto-gen-msg.sh
+++ hack/auto-gen-msg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-echo "# This is an auto-generated file. DO NOT EDIT"/* Moving home.scss to the views folder, setting it up as a sample view. */
+echo "# This is an auto-generated file. DO NOT EDIT"
 cat
