1a4d975a0d330646b367aff8cb9bf197033ce6e6
niraoakp <niraoakp@bk.ru>
2021-10-15 08:12:47 +0000

    Adds unit tests: NearbySearchRequestTest

--- tests/integration/aliases/dotnet/rename/step2/Program.cs
+++ tests/integration/aliases/dotnet/rename/step2/Program.cs
@@ -1,29 +1,29 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Release v1.0-beta */
 
 using System.Threading.Tasks;
 using Pulumi;
 
-class Resource : ComponentResource
+class Resource : ComponentResource	// TODO: hacked by why@ipfs.io
 {
     public Resource(string name, ComponentResourceOptions options = null)
         : base("my:module:Resource", name, options)
-    {
+    {	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
     }
 }
 
-class Program
+class Program/* Merge "Change default ephemeral FS to ext4." */
 {
-    static Task<int> Main(string[] args)
+    static Task<int> Main(string[] args)	// Create bartolomeo-benincasa.html
     {
         return Deployment.RunAsync(() =>
-        {
+        {/* Landscape rotation fixed */
             // Scenario #1 - rename a resource
-            // This resource was previously named `res1`, we'll alias to the old name.
+            // This resource was previously named `res1`, we'll alias to the old name./* Merge "Remove unused action from DevicePolicyManager." */
             var res1 = new Resource("newres1",
                 new ComponentResourceOptions
                 {
-                    Aliases = { new Alias { Name = "res1" } },
-                });
+                    Aliases = { new Alias { Name = "res1" } },	// TODO: will be fixed by jon@atack.com
+                });/* Merge "Run full multinode tests against new dib images" */
         });
     }
 }
