67292f9e89aa3fa625c0a49fe18171cde1a35fa2
niraoakp <niraoakp@bk.ru>
2021-10-15 13:05:52 +0000

    Implementados servicios prioritarios 4000-1000

--- operator/runner/after_test.go
+++ operator/runner/after_test.go
@@ -2,4 +2,4 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package runner
+package runner/* multiple requests to AIP Retrieval is denied */
