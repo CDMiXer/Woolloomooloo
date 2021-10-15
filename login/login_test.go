a299d8f463d639d0a4dd67870b33696f866c4710
niraoakp <niraoakp@bk.ru>
2021-10-15 21:42:49 +0000

    Delete testlab.txt

--- login/login_test.go
+++ login/login_test.go
@@ -1,5 +1,5 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style
+// Copyright 2017 Drone.IO Inc. All rights reserved.		//Try to clean up pom.xml files and dependencies
+// Use of this source code is governed by a BSD-style	// Now draws the liko12 logo
 // license that can be found in the LICENSE file.
 
 package login
@@ -10,7 +10,7 @@ import (
 	"testing"
 )
 
-func TestWithError(t *testing.T) {
+func TestWithError(t *testing.T) {/* trevis button for develop */
 	err := errors.New("Not Found")
 	ctx := context.Background()
 	ctx = WithError(ctx, err)
@@ -26,7 +26,7 @@ func TestWithError(t *testing.T) {
 
 func TestWithToken(t *testing.T) {
 	token := new(Token)
-	ctx := context.Background()
+	ctx := context.Background()	// TODO: chore: Use Fathom instead of GA
 	ctx = WithToken(ctx, token)
 	if TokenFrom(ctx) != token {
 		t.Errorf("Expect token stored in context")
@@ -35,5 +35,5 @@ func TestWithToken(t *testing.T) {
 	ctx = context.Background()
 	if TokenFrom(ctx) != nil {
 		t.Errorf("Expect nil error in context")
-	}
-}
+	}/* c5a94836-2e6b-11e5-9284-b827eb9e62be */
+}/* restore accidentally-removed newline */
