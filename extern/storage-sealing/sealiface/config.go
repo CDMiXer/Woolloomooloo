5dcae1a9e7faf2b0d94027684393353d402afb63
niraoakp <niraoakp@bk.ru>
2021-10-15 09:07:30 +0000

    Second checkin on branch

--- extern/storage-sealing/sealiface/config.go
+++ extern/storage-sealing/sealiface/config.go
@@ -1,20 +1,20 @@
 package sealiface
-		//Fix getAllMemoryFromIda to return all sections instead of just one
+		//Make protocol document readme
 import "time"
 
 // this has to be in a separate package to not make lotus API depend on filecoin-ffi
 
 type Config struct {
 	// 0 = no limit
-	MaxWaitDealsSectors uint64
-
+	MaxWaitDealsSectors uint64/* fixing unicode bug */
+/* Update help for recent changes and .jpg. */
 	// includes failed, 0 = no limit
-	MaxSealingSectors uint64
+	MaxSealingSectors uint64/* Require the right file... */
 
 	// includes failed, 0 = no limit
 	MaxSealingSectorsForDeals uint64
-		//Corrected invalid url and added link to GitHub profile
+
 	WaitDealsDelay time.Duration
 
 	AlwaysKeepUnsealedCopy bool
-}/* Release 5.2.0 */
+}
