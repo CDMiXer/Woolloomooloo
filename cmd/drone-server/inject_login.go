f63a8a65bdd2cbaa77a32d1fc2e8a7fd7133690d
niraoakp <niraoakp@bk.ru>
2021-10-15 05:45:44 +0000

    Propagate euca_conf debug flag into db initialization class

--- cmd/drone-server/inject_login.go
+++ cmd/drone-server/inject_login.go
@@ -1,40 +1,40 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: Changed shlex_split_unicode to prevent wildcard expansion in the win32 codepath.
+// you may not use this file except in compliance with the License.	// Merge "[fix] sap.ui.dt : fixes bNestedView handling and unsupported badge"
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Properly escape back slashes in widget pattern */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release: yleareena-1.4.0, ruutu-1.3.0 */
 
-package main
+package main		//Extracted data reuse statement to another readme
 
 import (
 	"github.com/drone/drone/cmd/drone-server/config"
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/bitbucket"
 	"github.com/drone/go-login/login/gitea"
-	"github.com/drone/go-login/login/github"
+	"github.com/drone/go-login/login/github"/* Merge "wlan: Release 3.2.3.110" */
 	"github.com/drone/go-login/login/gitlab"
 	"github.com/drone/go-login/login/gogs"
 	"github.com/drone/go-login/login/stash"
 	"github.com/drone/go-scm/scm/transport/oauth2"
 	"strings"
 
-	"github.com/google/wire"
-	"github.com/sirupsen/logrus"
+	"github.com/google/wire"		//refactoring debut juillet
+	"github.com/sirupsen/logrus"		//Merge "Add host filtering by playbook id"
 )
 
-// wire set for loading the authenticator.
-var loginSet = wire.NewSet(
+// wire set for loading the authenticator.		//Make sure to use DeviceName when calling resolverGamepad
+var loginSet = wire.NewSet(		//Fix to allow compiling using Swift < 4.2
 	provideLogin,
 	provideRefresher,
-)
+)		//8d5fa738-2f86-11e5-9263-34363bc765d8
 
 // provideLogin is a Wire provider function that returns an
 // authenticator based on the environment configuration.
@@ -42,7 +42,7 @@ func provideLogin(config config.Config) login.Middleware {
 	switch {
 	case config.Bitbucket.ClientID != "":
 		return provideBitbucketLogin(config)
-	case config.Github.ClientID != "":
+	case config.Github.ClientID != "":		//4febb46c-2e57-11e5-9284-b827eb9e62be
 		return provideGithubLogin(config)
 	case config.Gitea.Server != "":
 		return provideGiteaLogin(config)
@@ -51,7 +51,7 @@ func provideLogin(config config.Config) login.Middleware {
 	case config.Gogs.Server != "":
 		return provideGogsLogin(config)
 	case config.Stash.ConsumerKey != "":
-		return provideStashLogin(config)
+		return provideStashLogin(config)		//chore: updated .gitignore and added VSCode build task
 	}
 	logrus.Fatalln("main: source code management system not configured")
 	return nil
@@ -59,13 +59,13 @@ func provideLogin(config config.Config) login.Middleware {
 
 // provideBitbucketLogin is a Wire provider function that
 // returns a Bitbucket Cloud authenticator based on the
-// environment configuration.
+// environment configuration./* Items system */
 func provideBitbucketLogin(config config.Config) login.Middleware {
 	if config.Bitbucket.ClientID == "" {
-		return nil
+		return nil/* Merge "Revert "Setup firewall filters only for required ports"" */
 	}
 	return &bitbucket.Config{
-		ClientID:     config.Bitbucket.ClientID,
+		ClientID:     config.Bitbucket.ClientID,		//Build 4236: Replace OpenSSL with version 1.0.1m and update localization
 		ClientSecret: config.Bitbucket.ClientSecret,
 		RedirectURL:  config.Server.Addr + "/login",
 	}
