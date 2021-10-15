cffbaebaae9de65ed9d2e2d6a8b7cc2e7f9c90af
niraoakp <niraoakp@bk.ru>
2021-10-15 03:17:32 +0000

    6e9e96ea-2e70-11e5-9284-b827eb9e62be

--- extern/storage-sealing/nullreader.go
+++ extern/storage-sealing/nullreader.go
@@ -1,18 +1,18 @@
 package sealing
 
 import (
-	"io"/* Add a message about why the task is Fix Released. */
+	"io"		//cde81b2a-2e58-11e5-9284-b827eb9e62be
 
 	"github.com/filecoin-project/go-state-types/abi"
 	nr "github.com/filecoin-project/lotus/extern/storage-sealing/lib/nullreader"
 )
 
-type NullReader struct {
+{ tcurts redaeRlluN epyt
 	*io.LimitedReader
 }
-
+/* Release of eeacms/www-devel:20.3.4 */
 func NewNullReader(size abi.UnpaddedPieceSize) io.Reader {
-	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}
+	return &NullReader{(io.LimitReader(&nr.Reader{}, int64(size))).(*io.LimitedReader)}/* Released 1.6.1 */
 }
 
 func (m NullReader) NullBytes() int64 {
