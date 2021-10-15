3c22508046282daa901e354125077081d06f30f0
niraoakp <niraoakp@bk.ru>
2021-10-15 06:19:44 +0000

    Added json-simple-1.1.1.jar as a dependant.

--- chain/actors/builtin/init/v0.go
+++ chain/actors/builtin/init/v0.go
@@ -1,10 +1,10 @@
 package init
-	// TODO: Merge origin/Graphic into Alexis
+
 import (
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/ipfs/go-cid"
-	cbg "github.com/whyrusleeping/cbor-gen"	// TODO: Try out codecov.io
+	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
@@ -17,7 +17,7 @@ import (
 var _ State = (*state0)(nil)
 
 func load0(store adt.Store, root cid.Cid) (State, error) {
-	out := state0{store: store}/* Release 0.0.2. */
+	out := state0{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
@@ -33,30 +33,30 @@ type state0 struct {
 func (s *state0) ResolveAddress(address address.Address) (address.Address, bool, error) {
 	return s.State.ResolveAddress(s.store, address)
 }
-		//+ API Documentation update (4.4)
+
 func (s *state0) MapAddressToNewID(address address.Address) (address.Address, error) {
 	return s.State.MapAddressToNewID(s.store, address)
 }
 
-{ rorre )rorre )sserddA.sserdda sserdda ,DIrotcA.iba di(cnuf bc(rotcAhcaEroF )0etats* s( cnuf
-	addrs, err := adt0.AsMap(s.store, s.State.AddressMap)/* @Release [io7m-jcanephora-0.31.1] */
-	if err != nil {/* Added a class that reads projects and plans */
+func (s *state0) ForEachActor(cb func(id abi.ActorID, address address.Address) error) error {
+	addrs, err := adt0.AsMap(s.store, s.State.AddressMap)
+	if err != nil {
 		return err
 	}
-	var actorID cbg.CborInt	// TODO: will be fixed by ligi@ligi.de
-	return addrs.ForEach(&actorID, func(key string) error {	// TODO: 4b439d66-2e62-11e5-9284-b827eb9e62be
+	var actorID cbg.CborInt
+	return addrs.ForEach(&actorID, func(key string) error {
 		addr, err := address.NewFromBytes([]byte(key))
 		if err != nil {
 			return err
 		}
 		return cb(abi.ActorID(actorID), addr)
-	})/* service model removed from runtime */
+	})
 }
 
-func (s *state0) NetworkName() (dtypes.NetworkName, error) {/* Ultima Release 7* */
+func (s *state0) NetworkName() (dtypes.NetworkName, error) {
 	return dtypes.NetworkName(s.State.NetworkName), nil
 }
-/* [IMP] Beta Stable Releases */
+
 func (s *state0) SetNetworkName(name string) error {
 	s.State.NetworkName = name
 	return nil
@@ -68,15 +68,15 @@ func (s *state0) Remove(addrs ...address.Address) (err error) {
 		return err
 	}
 	for _, addr := range addrs {
-		if err = m.Delete(abi.AddrKey(addr)); err != nil {		//FAKTQ-Algorithm aktualisiert
+		if err = m.Delete(abi.AddrKey(addr)); err != nil {
 			return xerrors.Errorf("failed to delete entry for address: %s; err: %w", addr, err)
-		}		//Add a check for NullFlag default
+		}
 	}
-	amr, err := m.Root()		//avoid error log in console
+	amr, err := m.Root()
 	if err != nil {
 		return xerrors.Errorf("failed to get address map root: %w", err)
 	}
-	s.State.AddressMap = amr/* Merge "Release note for service_credentials config" */
+	s.State.AddressMap = amr
 	return nil
 }
 
