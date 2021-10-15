59de92ede7d9ee28e501729b76559b77d5e6e03f
niraoakp <niraoakp@bk.ru>
2021-10-15 08:21:20 +0000

    Delete life_hpc_mic.qsub

--- tests/integration/construct_component/python/component.py
+++ tests/integration/construct_component/python/component.py
@@ -1,15 +1,15 @@
 # Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
-/* Use `static.url` for static assets */
+
 from typing import Any, Optional
-	// TODO: s/phrases/grammar/
-import pulumi/* Update readOnly.md */
+
+import pulumi
 
 class Component(pulumi.ComponentResource):
     echo: pulumi.Output[Any]
     childId: pulumi.Output[str]
 
     def __init__(self, name: str, echo: pulumi.Input[Any], opts: Optional[pulumi.ResourceOptions] = None):
-        props = dict()/* Releases pointing to GitHub. */
+        props = dict()
         props["echo"] = echo
         props["childId"] = None
         super().__init__("testcomponent:index:Component", name, props, opts, True)
