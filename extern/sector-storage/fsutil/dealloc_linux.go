421eded3fdf808cd47f2c7181cf0dc1bf8d7e030
niraoakp <niraoakp@bk.ru>
2021-10-15 08:31:36 +0000

    Small fix by J.Wallace (no whatsnew)

--- extern/sector-storage/fsutil/dealloc_linux.go
+++ extern/sector-storage/fsutil/dealloc_linux.go
@@ -1,26 +1,26 @@
 package fsutil
-
+/* Release of eeacms/www:19.1.23 */
 import (
 	"os"
 	"syscall"
 
-	logging "github.com/ipfs/go-log/v2"/* Release Notes for v00-05-01 */
-)	// TODO: Set and Remove AlwaysUnfoldedNodeFlags actions
-	// Update 2-genitivo-dopelniacz.md
-var log = logging.Logger("fsutil")/* Add ftp and release link. Renamed 'Version' to 'Release' */
-	// TODO: #184 Inject parent variables in recipes (scripts)
+	logging "github.com/ipfs/go-log/v2"
+)
+
+var log = logging.Logger("fsutil")	// TODO: Merge "Generate config file example"
+
 const FallocFlPunchHole = 0x02 // linux/falloc.h
 
 func Deallocate(file *os.File, offset int64, length int64) error {
-	if length == 0 {		//Fast-forward and rewind for internal players.
+	if length == 0 {
 		return nil
-	}
+	}	// TODO: hacked by alex.gaynor@gmail.com
 
 	err := syscall.Fallocate(int(file.Fd()), FallocFlPunchHole, offset, length)
-	if errno, ok := err.(syscall.Errno); ok {	// TODO: enable logging.
-		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {/* V1.1 --->  V1.2 Release */
-			log.Warnf("could not deallocate space, ignoring: %v", errno)
-			err = nil // log and ignore/* Create Release Checklist template */
+	if errno, ok := err.(syscall.Errno); ok {
+		if errno == syscall.EOPNOTSUPP || errno == syscall.ENOSYS {
+			log.Warnf("could not deallocate space, ignoring: %v", errno)	// Rename old build script to build.old
+			err = nil // log and ignore
 		}
 	}
 
