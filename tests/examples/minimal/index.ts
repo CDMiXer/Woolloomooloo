481eb944e84da3c7d6cabc79fb7714fdf82ee93b
niraoakp <niraoakp@bk.ru>
2021-10-15 00:41:06 +0000

    adding active participants limit

--- tests/examples/minimal/index.ts
+++ tests/examples/minimal/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
-import { Config } from "@pulumi/pulumi";
+	// TODO: 3d matrix functions
+import { Config } from "@pulumi/pulumi";		//Add MapPage.java
 
 let config = new Config();
 console.log(`Hello, ${config.require("name")}!`);
