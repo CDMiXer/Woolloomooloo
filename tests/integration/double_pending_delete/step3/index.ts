3801717de2319bc01fdbe9cc2588a43e4c5c3f4b
niraoakp <niraoakp@bk.ru>
2021-10-15 15:09:23 +0000

    Error con el criterio arreglado

--- tests/integration/double_pending_delete/step3/index.ts
+++ tests/integration/double_pending_delete/step3/index.ts
@@ -3,36 +3,36 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//	// TODO: will be fixed by seth@sethvargo.com
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: hacked by martin2cai@hotmail.com
+//
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release for 2.9.0 */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Resource } from "./resource";/* Release: Making ready for next release cycle 5.0.6 */
+import { Resource } from "./resource";
 
 // The previous plan failed, but we're going to initiate *another* plan that
 // introduces new changes, while still keeping around the failed state
-// from the previous plan. The engine should delete all pending deletes before		//Update readme to reflect 1.3.x requirement
+// from the previous plan. The engine should delete all pending deletes before
 // attempting to start the next plan.
 //
-// To do this, we're going to trigger another replacement of A:		//Correct Respite text
+// To do this, we're going to trigger another replacement of A:
 const a = new Resource("a", { fail: 3 });
 
-// We will still fail to replace B, since fail == 1.		//undefined podcast
+// We will still fail to replace B, since fail == 1.
 const b = new Resource("b", { fail: 1 }, { dependsOn: a });
 // The snapshot now contains:
 //  A: Created
 //  A: Pending Delete
-//  B: Created		//Removed storage adapter modules
+//  B: Created
 
-// The A from the previous snapshot should have been deleted.		//Delete DirectX.cpp
+// The A from the previous snapshot should have been deleted.
 
 // This plan is interesting because it shows that it is legal to delete the same URN multiple
 // times in the same plan. This previously triggered an assert in the engine that asserted
 // that this is impossible (https://github.com/pulumi/pulumi/issues/1503)
-	// TODO: hacked by qugou1350636@126.com
-	// Adds link to CONTRIBUTING.md
+
+
