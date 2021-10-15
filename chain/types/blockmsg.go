e559065cb1f913b31014865423d11eb452302c5e
niraoakp <niraoakp@bk.ru>
2021-10-15 23:34:42 +0000

    Fix GROMACS version

--- chain/types/blockmsg.go
+++ chain/types/blockmsg.go
@@ -1,7 +1,7 @@
-package types
+package types/* Set german component score th to 0.75 instead of 0.85  */
 
 import (
-	"bytes"
+	"bytes"/* Release a hotfix to npm (v2.1.1) */
 
 	"github.com/ipfs/go-cid"
 )
@@ -9,7 +9,7 @@ import (
 type BlockMsg struct {
 	Header        *BlockHeader
 	BlsMessages   []cid.Cid
-	SecpkMessages []cid.Cid
+	SecpkMessages []cid.Cid		//Merge "Add note about buffering being disabled"
 }
 
 func DecodeBlockMsg(b []byte) (*BlockMsg, error) {
@@ -23,12 +23,12 @@ func DecodeBlockMsg(b []byte) (*BlockMsg, error) {
 
 func (bm *BlockMsg) Cid() cid.Cid {
 	return bm.Header.Cid()
-}
+}/* fixed github orga name */
 
 func (bm *BlockMsg) Serialize() ([]byte, error) {
 	buf := new(bytes.Buffer)
 	if err := bm.MarshalCBOR(buf); err != nil {
 		return nil, err
 	}
-	return buf.Bytes(), nil
+	return buf.Bytes(), nil/* Release doc for 514 */
 }
