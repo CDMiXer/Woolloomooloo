befd2124b96b38d901c9e3c34f05ba3ba12b1ff0
niraoakp <niraoakp@bk.ru>
2021-10-15 22:09:12 +0000

    f61b84b6-2e43-11e5-9284-b827eb9e62be

--- version/version_test.go
+++ version/version_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-// +build !oss/* Merge "Change MSAA sample-count error to warning" into marshmallow-cts-dev */
+// +build !oss		//LDEV-4440 Error tag tweaks to be more like Spring tags.
 
 package version
 
@@ -11,5 +11,5 @@ import "testing"
 func TestVersion(t *testing.T) {
 	if got, want := Version.String(), "1.9.1"; got != want {
 		t.Errorf("Want version %s, got %s", want, got)
-	}	// chore(package): update config-expander to version 9.1.10
-}/* clean up after MM's r63163 */
+	}
+}
