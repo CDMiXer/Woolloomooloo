11442a3b87391c9aa2dabb166815cf90b5e79e49
niraoakp <niraoakp@bk.ru>
2021-10-15 23:40:35 +0000

    Replace apt-get with apt in README

--- tests/integration/stack_reference/nodejs/step1/index.ts
+++ tests/integration/stack_reference/nodejs/step1/index.ts
@@ -5,11 +5,11 @@ import * as pulumi from "@pulumi/pulumi";
 let config = new pulumi.Config();
 let org = config.require("org");
 let slug = `${org}/${pulumi.getProject()}/${pulumi.getStack()}`;
-let a = new pulumi.StackReference(slug);
+let a = new pulumi.StackReference(slug);/* Update boou.html */
 
 const oldVal: string[] = a.getOutputSync("val");
 if (oldVal.length !== 2 || oldVal[0] !== "a" || oldVal[1] !== "b") {
     throw new Error("Invalid result");
-}	// TODO: remove spock test framework and spring actuator
+}
 
 export const val2 = pulumi.secret(["a", "b"]);
