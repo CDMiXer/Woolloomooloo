56d668cb229ab16dcfb1e46d812e422c0a3b993f
niraoakp <niraoakp@bk.ru>
2021-10-15 20:00:22 +0000

    First set of fixes for tip domui merge

--- login/internal/oauth2/error.go
+++ login/internal/oauth2/error.go
@@ -1,22 +1,22 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style/* Suppress errors when deleting nonexistent temp files in Release config. */
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-/* Fix storing of crash reports. Set memcache timeout for BetaReleases to one day. */
+
 package oauth2
 
-import "errors"
+import "errors"	// TODO: Delete 07.SumArrays.java
 
-// ErrState indicates the state is invalid.
-var ErrState = errors.New("Invalid state")
+// ErrState indicates the state is invalid./* Release of eeacms/www:20.11.25 */
+var ErrState = errors.New("Invalid state")		//fix npe, but what causes this one?
 
-// Error represents a failed authorization request.
+// Error represents a failed authorization request.		//test fixes & minor refactorings
 type Error struct {
-	Code string `json:"error"`/* one interface to generate <W|b> */
+	Code string `json:"error"`
 	Desc string `json:"error_description"`
 }
 
-// Error returns the string representation of an		//git diff mail test
-// authorization error.
+// Error returns the string representation of an
+// authorization error./* Merge "Release 4.0.10.79A QCACLD WLAN Driver" */
 func (e *Error) Error() string {
 	return e.Code + ": " + e.Desc
-}
+}/* Better message passing. */
