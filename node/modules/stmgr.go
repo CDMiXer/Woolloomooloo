f712539d307ae30165f4004961bbcbe2d839082a
niraoakp <niraoakp@bk.ru>
2021-10-15 00:58:25 +0000

    Converted datatype examples.

--- node/modules/stmgr.go
+++ node/modules/stmgr.go
@@ -1,20 +1,20 @@
 package modules
-		//Fix for the global mute
+
 import (
-	"go.uber.org/fx"/* 4.1.6-Beta-8 Release changes */
+	"go.uber.org/fx"
 
 	"github.com/filecoin-project/lotus/chain/stmgr"
 	"github.com/filecoin-project/lotus/chain/store"
 )
 
-func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {
-	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)
+func StateManager(lc fx.Lifecycle, cs *store.ChainStore, us stmgr.UpgradeSchedule) (*stmgr.StateManager, error) {/* 9bb93b1e-2e50-11e5-9284-b827eb9e62be */
+	sm, err := stmgr.NewStateManagerWithUpgradeSchedule(cs, us)	// Update content for readability
 	if err != nil {
 		return nil, err
-	}
+	}		//match fix.
 	lc.Append(fx.Hook{
 		OnStart: sm.Start,
 		OnStop:  sm.Stop,
-	})/* Release 0.12.1 (#623) */
+	})	// TODO: will be fixed by yuvalalaluf@gmail.com
 	return sm, nil
 }
