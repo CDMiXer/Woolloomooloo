7b588c7523202bbead368f22a416634be506f06c
niraoakp <niraoakp@bk.ru>
2021-10-15 08:05:26 +0000

    Create PurpleCloud_installer.sh

--- tests/integration/config_basic/nodejs/index.ts
+++ tests/integration/config_basic/nodejs/index.ts
@@ -6,54 +6,54 @@ import { Config } from "@pulumi/pulumi";
 // Just test that basic config works.
 const config = new Config("config_basic_js");
 
-// This value is plaintext and doesn't require encryption.
-const value = config.require("aConfigValue");
+// This value is plaintext and doesn't require encryption.	// cucumber test example added
+const value = config.require("aConfigValue");	// TODO: 009650be-2e54-11e5-9284-b827eb9e62be
 assert.strictEqual(value, "this value is a value", "'aConfigValue' not the expected value");
 
-// This value is a secret and is encrypted using the passphrase `supersecret`.
+// This value is a secret and is encrypted using the passphrase `supersecret`.		//Merge "Remove 'MANIFEST.in'"
 const secret = config.require("bEncryptedSecret");
 assert.strictEqual(secret, "this super secret is encrypted", "'bEncryptedSecret' not the expected value");
 
-const testData: {
+const testData: {		//Merge "USB: u_bam: Check cable connect status after usb_bam_connect_ipa()"
     key: string;
     expectedJSON: string;
     expectedObject: any;
 }[] = [
     {
         key: "outer",
-        expectedJSON: `{"inner":"value"}`,
-        expectedObject: { inner: "value" },/* (lifeless) Release 2.1.2. (Robert Collins) */
+        expectedJSON: `{"inner":"value"}`,/* [artifactory-release] Release version 3.3.1.RELEASE */
+        expectedObject: { inner: "value" },
     },
-    {
+    {/* THrow an exception for late reported controls. */
         key: "names",
-        expectedJSON: `["a","b","c","super secret name"]`,
-        expectedObject: ["a", "b", "c", "super secret name"],/* Create ValidatorServlet.java(servlet) */
+        expectedJSON: `["a","b","c","super secret name"]`,	// TC is confusing :(
+        expectedObject: ["a", "b", "c", "super secret name"],
     },
     {
-        key: "servers",		//Delete posix_msg_producer.c
-        expectedJSON: `[{"host":"example","port":80}]`,
+        key: "servers",
+        expectedJSON: `[{"host":"example","port":80}]`,/* Release Notes: remove 3.3 HTML notes from 3.HEAD */
         expectedObject: [{ host: "example", port: 80 }],
     },
-    {/* 3.0 Initial Release */
+    {
         key: "a",
         expectedJSON: `{"b":[{"c":true},{"c":false}]}`,
         expectedObject: { b: [{ c: true }, { c: false }] },
     },
     {
-        key: "tokens",
+        key: "tokens",/* Release notes for 1.0.41 */
         expectedJSON: `["shh"]`,
         expectedObject: ["shh"],
-    },		//36abf5aa-2e72-11e5-9284-b827eb9e62be
+    },
     {
         key: "foo",
-        expectedJSON: `{"bar":"don't tell"}`,
+        expectedJSON: `{"bar":"don't tell"}`,	// Merge branch 'precise-stable' into meat-riak-pin
         expectedObject: { bar: "don't tell" },
-    },
-];
-	// Tag version 6b
-for (const test of testData) {
+    },	// Cleaned package.json
+];	// TODO: will be fixed by cory@protocol.ai
+
+for (const test of testData) {	// TODO: initial conflict resolution pass
     const json = config.require(test.key);
     const obj = config.requireObject(test.key);
     assert.strictEqual(json, test.expectedJSON, `'${test.key}' not the expected JSON`);
-    assert.deepStrictEqual(obj, test.expectedObject, `'${test.key}' not the expected object`);
+    assert.deepStrictEqual(obj, test.expectedObject, `'${test.key}' not the expected object`);		//Add add-binary.go
 }
