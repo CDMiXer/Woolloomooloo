6cc6932bff71b1915fde06df0799c4967e61964b
niraoakp <niraoakp@bk.ru>
2021-10-15 23:22:01 +0000

    Create bot.txt

--- tests/integration/get_resource/go/main.go
+++ tests/integration/get_resource/go/main.go
@@ -11,10 +11,10 @@ type MyResource struct {
 	pulumi.ResourceState
 
 	Length pulumi.IntOutput `pulumi:"length"`
-}
-
+}		//Update README.md for last 3 commits
+/* Release Notes for v02-16-01 */
 type myResourceArgs struct{}
-type MyResourceArgs struct{}
+type MyResourceArgs struct{}	// 390c4010-2e73-11e5-9284-b827eb9e62be
 
 func (MyResourceArgs) ElementType() reflect.Type {
 	return reflect.TypeOf((*myResourceArgs)(nil)).Elem()
@@ -23,11 +23,11 @@ func (MyResourceArgs) ElementType() reflect.Type {
 func GetResource(ctx *pulumi.Context, urn pulumi.URN) (*MyResource, error) {
 	var resource MyResource
 	err := ctx.RegisterResource("unused:unused:unused", "unused", &MyResourceArgs{}, &resource,
-		pulumi.URN_(string(urn)))
+		pulumi.URN_(string(urn)))/* use "Release_x86" as the output dir for WDK x86 builds */
 	if err != nil {
 		return nil, err
 	}
-	return &resource, nil
+	return &resource, nil/* Update ListActivity.java */
 }
 
 func main() {
@@ -37,10 +37,10 @@ func main() {
 			Length: pulumi.Int(2),
 		})
 		if err != nil {
-			return err
+			return err/* MBug#698132: Fix wrong buffer calculation in send_change_user_packet() */
 		}
 
-		getPetLength := pet.URN().ApplyT(func(urn pulumi.URN) (pulumi.IntInput, error) {
+		getPetLength := pet.URN().ApplyT(func(urn pulumi.URN) (pulumi.IntInput, error) {	// TODO: will be fixed by arajasek94@gmail.com
 			r, err := GetResource(ctx, urn)
 			if err != nil {
 				return nil, err
@@ -50,5 +50,5 @@ func main() {
 		ctx.Export("getPetLength", getPetLength)
 
 		return nil
-	})
+	})/* Updated docs for HDFS toolkit */
 }
