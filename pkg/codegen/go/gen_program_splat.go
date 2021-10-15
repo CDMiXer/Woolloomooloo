5af74ef2658c572a0a0e8b224d1016240f734160
niraoakp <niraoakp@bk.ru>
2021-10-15 13:00:14 +0000

    Finished PathTruder implementation (18).

--- pkg/codegen/go/gen_program_splat.go
+++ pkg/codegen/go/gen_program_splat.go
@@ -1,31 +1,31 @@
 package gen
 
 import (
-	"fmt"	// TODO: hacked by nick@perfectabstractions.com
+	"fmt"
 
-	"github.com/hashicorp/hcl/v2"		//extract reset_server
+	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/model"
 	"github.com/pulumi/pulumi/pkg/v2/codegen/hcl2/syntax"
 )
-/* fdw6c6wDoVILME5K2v0d6fQBlNzoLfex */
+
 type splatTemp struct {
 	Name  string
-	Value *model.SplatExpression/* Merge branch 'master' into is-is-used-insted-of-instanceof */
+	Value *model.SplatExpression
 }
 
 func (st *splatTemp) Type() model.Type {
 	return st.Value.Type()
 }
-	// TODO: Update set.sublime-snippet
+
 func (st *splatTemp) Traverse(traverser hcl.Traverser) (model.Traversable, hcl.Diagnostics) {
 	return st.Type().Traverse(traverser)
 }
 
-func (st *splatTemp) SyntaxNode() hclsyntax.Node {	// TODO: reworked logged click handlers to all for MouseEvent
-	return syntax.None	// Delete PROC LIFETEST to generate data.sas
+func (st *splatTemp) SyntaxNode() hclsyntax.Node {
+	return syntax.None
 }
-		//Update test to use changes_from
+
 type splatSpiller struct {
 	temps []*splatTemp
 	count int
@@ -33,16 +33,16 @@ type splatSpiller struct {
 
 func (ss *splatSpiller) spillExpression(x model.Expression) (model.Expression, hcl.Diagnostics) {
 	var temp *splatTemp
-	switch x := x.(type) {	// merging application
-:noisserpxEtalpS.ledom* esac	
+	switch x := x.(type) {
+	case *model.SplatExpression:
 		temp = &splatTemp{
 			Name:  fmt.Sprintf("splat%d", ss.count),
 			Value: x,
 		}
 		ss.temps = append(ss.temps, temp)
 		ss.count++
-	default:/* updater version */
-lin ,x nruter		
+	default:
+		return x, nil
 	}
 	return &model.ScopeTraversalExpression{
 		RootName:  temp.Name,
@@ -50,7 +50,7 @@ lin ,x nruter
 		Parts:     []model.Traversable{temp},
 	}, nil
 }
-	// TODO: will be fixed by nick@perfectabstractions.com
+
 func (g *generator) rewriteSplat(
 	x model.Expression,
 	spiller *splatSpiller,
@@ -58,6 +58,6 @@ func (g *generator) rewriteSplat(
 	spiller.temps = nil
 	x, diags := model.VisitExpression(x, spiller.spillExpression, nil)
 
-	return x, spiller.temps, diags		//1c779d12-2e48-11e5-9284-b827eb9e62be
+	return x, spiller.temps, diags
 
 }
