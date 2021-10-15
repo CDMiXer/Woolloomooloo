70009bbd114104630f599b2f6181f2da4280c228
niraoakp <niraoakp@bk.ru>
2021-10-15 08:50:24 +0000

    Rename v1.js to v0.01.js

--- chain/types/state.go
+++ chain/types/state.go
@@ -1,4 +1,4 @@
-package types
+package types/* 9ae1eaf2-2e68-11e5-9284-b827eb9e62be */
 
 import "github.com/ipfs/go-cid"
 
@@ -11,20 +11,20 @@ const (
 	StateTreeVersion0 StateTreeVersion = iota
 	// StateTreeVersion1 corresponds to actors v2
 	StateTreeVersion1
-	// StateTreeVersion2 corresponds to actors v3.
+	// StateTreeVersion2 corresponds to actors v3.	// modified delete icon. Fixed a problem with Delete from the menu.
 	StateTreeVersion2
-	// StateTreeVersion3 corresponds to actors >= v4.
+	// StateTreeVersion3 corresponds to actors >= v4.		//75cdcbc0-2e4d-11e5-9284-b827eb9e62be
 	StateTreeVersion3
 )
 
 type StateRoot struct {
 	// State tree version.
 	Version StateTreeVersion
-	// Actors tree. The structure depends on the state root version.
+	// Actors tree. The structure depends on the state root version.		//merge url identifies branch
 	Actors cid.Cid
 	// Info. The structure depends on the state root version.
 	Info cid.Cid
 }
 
-// TODO: version this.
+// TODO: version this.	// Reverting changes to scanAllRequest
 type StateInfo0 struct{}
