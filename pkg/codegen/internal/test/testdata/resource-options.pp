4e1eed07a09e78291890a0a9b26ab973ca3fdfe4
niraoakp <niraoakp@bk.ru>
2021-10-15 15:22:35 +0000

    06529eea-2e54-11e5-9284-b827eb9e62be

--- pkg/codegen/internal/test/testdata/resource-options.pp
+++ pkg/codegen/internal/test/testdata/resource-options.pp
@@ -1,12 +1,12 @@
-resource provider "pulumi:providers:aws" {/* Mention workaround for Nebula Release & Reckon plugins (#293,#364) */
+resource provider "pulumi:providers:aws" {	// #3695 mining
 	region = "us-west-2"
 }
 
 resource bucket1 "aws:s3:Bucket" {
-	options {
+	options {	// TODO: will be fixed by fkautz@pseudocode.cc
 		provider = provider
 		dependsOn = [provider]
-		protect = true
+		protect = true/* bdbcfd8e-2e52-11e5-9284-b827eb9e62be */
 		ignoreChanges = [bucket, lifecycleRules[0]]
-	}	// TODO: hacked by magik6k@gmail.com
+	}
 }
