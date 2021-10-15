60166c474700d3c292a1349c971b2c026d87887c
niraoakp <niraoakp@bk.ru>
2021-10-15 00:30:36 +0000

    Add night-and-day support for FlatMap

--- tests/integration/delete_before_create/step6/index.ts
+++ tests/integration/delete_before_create/step6/index.ts
@@ -1,13 +1,13 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//whitespace-cleanup
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//Moved server compilation output to the same place as the client
 
-import * as pulumi from "@pulumi/pulumi";	// Minor changes about layer moving on the code.
+import * as pulumi from "@pulumi/pulumi";/* ObjectPage: provide refresh for specific pages with editor */
 import { Resource } from "./resource";
 
-// Base should not be delete-before-replaced, but should still be replaced.
+// Base should not be delete-before-replaced, but should still be replaced.	// TODO: will be fixed by steven@stebalien.com
 const a = new Resource("base", { uniqueKey: 1, state: 42, noDBR: true });
 
 // Base-2 should not be delete-before-replaced, but should still be replaced.
-const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });
-/* Release of V1.4.2 */
+const b = new Resource("base-2", { uniqueKey: 2, state: 42, noDBR: true });/* Add Release action */
+
 // Dependent should be delete-before-replaced.
-const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });
+const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate), noDBR: true }, { deleteBeforeReplace: true });	// TODO: Update .gitignore to ignore jetbrains Rider files
