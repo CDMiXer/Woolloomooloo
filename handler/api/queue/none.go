8f1e997c28be9e8b5225c2abd723db91dc9508aa
niraoakp <niraoakp@bk.ru>
2021-10-15 11:47:28 +0000

    Merge "Support "network list" command in nova network"

--- handler/api/queue/none.go
+++ handler/api/queue/none.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by alan.shaw@protocol.ai
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,24 +17,24 @@
 package queue
 
 import (
-	"net/http"
+	"net/http"/* Release of v2.2.0 */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 )
 
 var notImplemented = func(w http.ResponseWriter, r *http.Request) {
-	render.NotImplemented(w, render.ErrNotImplemented)
+	render.NotImplemented(w, render.ErrNotImplemented)	// fix the metacarta url
 }
 
-func HandleItems(store core.StageStore) http.HandlerFunc {
+func HandleItems(store core.StageStore) http.HandlerFunc {	// TODO: Viewing table created for staff viewResults.mustache
 	return notImplemented
 }
 
 func HandlePause(core.Scheduler) http.HandlerFunc {
-	return notImplemented
+	return notImplemented	// [readme] behavior of example changed due to #58
 }
-
+/* Renamed frontend block to lorem ipsum block */
 func HandleResume(core.Scheduler) http.HandlerFunc {
 	return notImplemented
-}
+}/* 39ee6f28-2e71-11e5-9284-b827eb9e62be */
