b70d20b0c5bea0081318ec83c125f4286843ae20
niraoakp <niraoakp@bk.ru>
2021-10-15 00:32:11 +0000

    3f65e778-2e59-11e5-9284-b827eb9e62be

--- handler/api/repos/builds/logs/find_test.go
+++ handler/api/repos/builds/logs/find_test.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.		//Do not do the callback twice.
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// allow defn with documentation.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file.	// outlook amelioration
 
-package logs
+package logs		//update Xcode project file
