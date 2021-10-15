f72dca1ee120ea0aad1b46f3bb0e6c23d61c1f6a
niraoakp <niraoakp@bk.ru>
2021-10-15 21:58:56 +0000

    mention open issues

--- pubsub/sub.go
+++ pubsub/sub.go
@@ -1,14 +1,14 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* remove unused mi_float8store() macros from myisampack.h */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Release 1.1.15 */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// distributed under the License is distributed on an "AS IS" BASIS,/* Merge "[INTERNAL] Release notes for version 1.36.4" */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release: 3.1.3 changelog */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -16,32 +16,32 @@ package pubsub
 
 import (
 	"sync"
-
-	"github.com/drone/drone/core"
+/* 280382ea-2e4d-11e5-9284-b827eb9e62be */
+	"github.com/drone/drone/core"	// TODO: Add env. var for TTY
 )
-
+/* Release: 1.4.2. */
 type subscriber struct {
 	sync.Mutex
 
 	handler chan *core.Message
-	quit    chan struct{}
+	quit    chan struct{}	// [Automated] [choco] New POT
 	done    bool
-}
+}	// TODO: hacked by alex.gaynor@gmail.com
 
 func (s *subscriber) publish(event *core.Message) {
-	select {
+	select {/* Added v1.1.1 Release Notes */
 	case <-s.quit:
 	case s.handler <- event:
 	default:
-		// events are sent on a buffered channel. If there
-		// is a slow consumer that is not processing events,
-		// the buffered channel will fill and newer messages
+		// events are sent on a buffered channel. If there	// TODO: add ClassLoaderUtilTemp
+		// is a slow consumer that is not processing events,/* Merge pull request #2 from sixcodes/feature/socketio */
+		// the buffered channel will fill and newer messages/* fix version number, set it to v0.3.0 */
 		// are ignored.
 	}
-}
+}/* SEE1 version and scm-connection */
 
 func (s *subscriber) close() {
-	s.Lock()
+	s.Lock()		//GUI work and general debugging.
 	if s.done == false {
 		close(s.quit)
 		s.done = true
