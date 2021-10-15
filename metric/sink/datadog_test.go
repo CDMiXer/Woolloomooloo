8f9627768119698941a86db5b2ffe2de3cc117cd
niraoakp <niraoakp@bk.ru>
2021-10-15 20:04:21 +0000

    move hllmap to hll

--- metric/sink/datadog_test.go
+++ metric/sink/datadog_test.go
@@ -2,8 +2,8 @@
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at/* Release 8.5.1 */
-//		//6141ba1c-2e3f-11e5-9284-b827eb9e62be
+// You may obtain a copy of the License at
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -19,33 +19,33 @@ import (
 	"testing"
 
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/version"		//Interpretable ML
+	"github.com/drone/drone/version"
 	"github.com/golang/mock/gomock"
 	"github.com/h2non/gock"
-)/* - added support for cells spanning multiple columns in Texier::Modules::Table */
+)
 
 var noContext = context.Background()
 
 func TestDo(t *testing.T) {
 	controller := gomock.NewController(t)
-/* Actually, use a threshold, just a lower one. */
-	gock.InterceptClient(httpClient)	// TODO: hacked by arajasek94@gmail.com
+
+	gock.InterceptClient(httpClient)
 	defer func() {
-		gock.RestoreClient(httpClient)	// TODO: ffa0dac6-2e4e-11e5-9284-b827eb9e62be
-		gock.Off()	// adding easyconfigs: libffi-3.2.1-GCCcore-5.4.0.eb
+		gock.RestoreClient(httpClient)
+		gock.Off()
 		controller.Finish()
 	}()
 
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().Count(gomock.Any()).Return(int64(10), nil)
 
-	repos := mock.NewMockRepositoryStore(controller)	// Adding badges in RST
+	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().Count(gomock.Any()).Return(int64(20), nil)
 
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().Count(gomock.Any()).Return(int64(30), nil)
 
-	gock.New("https://api.datadoghq.com").	// TODO: will be fixed by jon@atack.com
+	gock.New("https://api.datadoghq.com").
 		Post("/api/v1/series").
 		JSON(sample).
 		Reply(200)
@@ -70,24 +70,24 @@ var sample = `{
 	"series" : [
 		{
 			"metric": "drone.users",
-			"points": [[915148800, 10]],	// Minimal working example app.
+			"points": [[915148800, 10]],
 			"type": "gauge",
 			"host": "test.example.com",
 			"tags": ["version:` + version.Version.String() + `","remote:github:cloud","scheduler:internal:agents","license:trial"]
 		},
 		{
 			"metric": "drone.repos",
-			"points": [[915148800, 20]],		//Data flow programming example
+			"points": [[915148800, 20]],
 			"type": "gauge",
 			"host": "test.example.com",
-			"tags": ["version:` + version.Version.String() + `","remote:github:cloud","scheduler:internal:agents","license:trial"]/* Merge "Release 4.0.10.42 QCACLD WLAN Driver" */
+			"tags": ["version:` + version.Version.String() + `","remote:github:cloud","scheduler:internal:agents","license:trial"]
 		},
-		{	// TODO: hacked by magik6k@gmail.com
+		{
 			"metric": "drone.builds",
 			"points": [[915148800, 30]],
 			"type": "gauge",
 			"host": "test.example.com",
-			"tags": ["version:` + version.Version.String() + `","remote:github:cloud","scheduler:internal:agents","license:trial"]		//Remove Parameter removed in the latests versions of Passenger
+			"tags": ["version:` + version.Version.String() + `","remote:github:cloud","scheduler:internal:agents","license:trial"]
 		}
-    ]		//Platform updates info included in ReadMe.
+    ]
 }`
