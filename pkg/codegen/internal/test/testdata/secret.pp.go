610d60d93a1e4860d4c045f40128556334b7785a
niraoakp <niraoakp@bk.ru>
2021-10-15 03:13:13 +0000

    Acknowledged Azure for Research

--- pkg/codegen/internal/test/testdata/secret.pp.go
+++ pkg/codegen/internal/test/testdata/secret.pp.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/rds"/* Release v1.6.0 */
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/rds"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		_, err := rds.NewCluster(ctx, "dbCluster", &rds.ClusterArgs{		//README: update current release version
+		_, err := rds.NewCluster(ctx, "dbCluster", &rds.ClusterArgs{
 			MasterPassword: pulumi.ToSecret("foobar").(pulumi.StringOutput),
-		})	// [IMP] fix space
+		})
 		if err != nil {
 			return err
 		}
-		return nil/* Release FPCM 3.0.1 */
+		return nil
 	})
 }
