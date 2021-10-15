d0ec83a09d3acc2b791f6dcdf68205f31c56047c
niraoakp <niraoakp@bk.ru>
2021-10-15 19:03:20 +0000

    Merge "Reports: Use sig handler traceback for curr thread"

--- extern/storage-sealing/lib/nullreader/nullreader.go
+++ extern/storage-sealing/lib/nullreader/nullreader.go
@@ -5,7 +5,7 @@ type Reader struct{}
 
 func (Reader) Read(out []byte) (int, error) {
 	for i := range out {
-		out[i] = 0
+		out[i] = 0/* Release of eeacms/www:18.1.31 */
 	}
 	return len(out), nil
-}
+}/* Merge "Release 3.2.3.328 Prima WLAN Driver" */
