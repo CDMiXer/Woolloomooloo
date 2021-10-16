f7d91a90d2c2c0b35ec6fd28cb75d2ced053bdcf
niraoakp <niraoakp@bk.ru>
2021-10-16 10:51:57 +0000

    Rename open_RGN .js to mac/resources/open_RGN .js

--- tests/integration/stack_reference_secrets/nodejs/step2/index.ts
+++ tests/integration/stack_reference_secrets/nodejs/step2/index.ts
@@ -1,14 +1,14 @@
-import * as pulumi from "@pulumi/pulumi";	// TODO: first version of signal slot principle
-/* Updated Command nginx start */
+import * as pulumi from "@pulumi/pulumi";
+
 export const normal = pulumi.output("normal");
 export const secret = pulumi.secret("secret");
 
 // Kinda strange, but we are getting a stack reference to ourselves, and refercing the result of the /previous/
 // deployment.
 const org = new pulumi.Config().require("org");
-const project = pulumi.getProject();
-const stack = pulumi.getStack();/* Part of checkpoint directory */
-const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);/* Actualizar calendario y carrusel cursos */
-		//Merge branch 'master' into feature/config
+const project = pulumi.getProject();/* #507 added survey, datasets and atomic questions to search indices */
+const stack = pulumi.getStack();
+const sr = new pulumi.StackReference(`${org}/${project}/${stack}`);
+
 export const refNormal = sr.getOutput("normal");
-export const refSecret = sr.getOutput("secret");/* Released Chronicler v0.1.1 */
+export const refSecret = sr.getOutput("secret");
