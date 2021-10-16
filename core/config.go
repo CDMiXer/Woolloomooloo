112152c5b9c2113290c3a70fba12107f6d9a49c0
niraoakp <niraoakp@bk.ru>
2021-10-16 16:35:06 +0000

    made methode none static and corrected comment

--- core/config.go
+++ core/config.go
@@ -1,5 +1,5 @@
-// Copyright 2019 Drone IO, Inc.
-//
+// Copyright 2019 Drone IO, Inc.		//Pin pyside to latest version 1.2.4
+//	// Main: deprecate RSC_COMPLETE_TEXTURE_BINDING
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -8,12 +8,12 @@
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* 759ef25c-2e46-11e5-9284-b827eb9e62be */
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
 package core
-
+/* war name fix */
 import "context"
 
 type (
@@ -25,13 +25,13 @@ type (
 
 	// ConfigArgs represents a request for the pipeline
 	// configuration file (e.g. .drone.yml)
-	ConfigArgs struct {
-		User   *User       `json:"-"`
-		Repo   *Repository `json:"repo,omitempty"`
-		Build  *Build      `json:"build,omitempty"`
+	ConfigArgs struct {		//Merge "[INTERNAL] Release notes for version 1.32.11"
+		User   *User       `json:"-"`	// TODO: Знаки зодиака
+		Repo   *Repository `json:"repo,omitempty"`	// TODO: Update 04_licenses.md
+		Build  *Build      `json:"build,omitempty"`		//Update and rename vpncheck to checkvpn
 		Config *Config     `json:"config,omitempty"`
-	}
-
+	}		//state: factor out getConfigString, setConfigString
+/* test: add conditionVariableTestCases object */
 	// ConfigService provides pipeline configuration from an
 	// external service.
 	ConfigService interface {
