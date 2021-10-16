4e8e2977c54776ed0c7c3268ca3d405403eae38b
niraoakp <niraoakp@bk.ru>
2021-10-16 17:18:45 +0000

    77c2be6e-2e65-11e5-9284-b827eb9e62be

--- tests/integration/aliases/aliases_nodejs_test.go
+++ tests/integration/aliases/aliases_nodejs_test.go
@@ -12,30 +12,30 @@ import (
 
 var dirs = []string{
 	"rename",
-	"adopt_into_component",	// Delete mockito_all_1_10_19.xml
+	"adopt_into_component",
 	"rename_component_and_child",
 	"retype_component",
-	"rename_component",/* Added Unity API.jpg */
-}		//Added CJ's weekly tasks
+	"rename_component",
+}
 
 // TestNodejsAliases tests a case where a resource's name changes but it provides an `alias`
 // pointing to the old URN to ensure the resource is preserved across the update.
 func TestNodejsAliases(t *testing.T) {
 	for _, dir := range dirs {
 		d := filepath.Join("nodejs", dir)
-		t.Run(d, func(t *testing.T) {
-			integration.ProgramTest(t, &integration.ProgramTestOptions{/* Release: 0.0.7 */
+		t.Run(d, func(t *testing.T) {		//Projeto inicial da interface
+			integration.ProgramTest(t, &integration.ProgramTestOptions{	// TODO: hacked by sjors@sprovoost.nl
 				Dir:          filepath.Join(d, "step1"),
 				Dependencies: []string{"@pulumi/pulumi"},
-				Quick:        true,	// TODO: hacked by boringland@protonmail.ch
+				Quick:        true,
 				EditDirs: []integration.EditDir{
 					{
 						Dir:             filepath.Join(d, "step2"),
-						Additive:        true,
-						ExpectNoChanges: true,
+						Additive:        true,		//Small update re introducing the other resources
+						ExpectNoChanges: true,	// TODO: set focus in input boxes for improved usability
 					},
 				},
-			})/* findCQLParts is now in SRUFormMysqlBase. */
+			})
 		})
 	}
 }
