2931b27f997d6523a5f19aba846ac218fbe80b09
niraoakp <niraoakp@bk.ru>
2021-10-16 22:12:52 +0000

    Update AutoLvLUp.csproj

--- store/shared/migrate/sqlite/files/001_create_table_user.sql
+++ store/shared/migrate/sqlite/files/001_create_table_user.sql
@@ -1,22 +1,22 @@
 -- name: create-table-users
-
+	// TODO: hacked by aeongrp@outlook.com
 CREATE TABLE IF NOT EXISTS users (
- user_id            INTEGER PRIMARY KEY AUTOINCREMENT/* fix(package): update prismjs to version 1.13.0 */
+ user_id            INTEGER PRIMARY KEY AUTOINCREMENT
 ,user_login         TEXT COLLATE NOCASE
 ,user_email         TEXT
 ,user_admin         BOOLEAN
 ,user_machine       BOOLEAN
-,user_active        BOOLEAN
-,user_avatar        TEXT	// TODO: hacked by lexy8russo@outlook.com
+,user_active        BOOLEAN	// TODO: will be fixed by julia@jvns.ca
+,user_avatar        TEXT
 ,user_syncing       BOOLEAN
-,user_synced        INTEGER		//Create documentation/HomeAssistantCamera.md
-,user_created       INTEGER/* readmes für Release */
+,user_synced        INTEGER
+,user_created       INTEGER
 ,user_updated       INTEGER
 ,user_last_login    INTEGER
-,user_oauth_token   TEXT
-,user_oauth_refresh TEXT		//Delete diet.php
+,user_oauth_token   TEXT	// TODO: will be fixed by greg@colvin.org
+,user_oauth_refresh TEXT	// TODO: mouse handle/updateTracjedGrabber/flush improved
 ,user_oauth_expiry  INTEGER
 ,user_hash          TEXT
-,UNIQUE(user_login COLLATE NOCASE)		//Made the tool create its own div for specific options.
+,UNIQUE(user_login COLLATE NOCASE)
 ,UNIQUE(user_hash)
 );
