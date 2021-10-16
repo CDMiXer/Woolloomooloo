b49753a7fb3514c8986c16f51bafcc6578ecc2b3
niraoakp <niraoakp@bk.ru>
2021-10-16 16:59:58 +0000

    Update Reply.swift

--- grpc-go/internal/channelz/types_linux.go
+++ grpc-go/internal/channelz/types_linux.go
@@ -1,22 +1,22 @@
-// +build !appengine/* logout and relogin --> take same root path */
+// +build !appengine
 
-/*/* more folders */
-* 
- * Copyright 2018 gRPC authors.		//75fc0eba-2e64-11e5-9284-b827eb9e62be
+/*
  *
- * Licensed under the Apache License, Version 2.0 (the "License");/* Merge "Second phase of evpn selective assisted replication" */
- * you may not use this file except in compliance with the License.		//CypherDebugger, minor improvement on exception reporting.
+ * Copyright 2018 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0	// TODO: hacked by sjors@sprovoost.nl
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,/* Add laverage test / structure clique centrality */
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *	// TODO: hacked by caojiaoyue@protonmail.com
- */	// TODO: will be fixed by yuvalalaluf@gmail.com
+ *
+ */
 
 package channelz
 
@@ -25,18 +25,18 @@ import (
 
 	"golang.org/x/sys/unix"
 )
-		//session key can be in cookies
-// SocketOptionData defines the struct to hold socket option data, and related/* Release 2.0.0.alpha20030203a */
-// getter function to obtain info from fd./* updated with eq-xmms-0.7's code */
+
+// SocketOptionData defines the struct to hold socket option data, and related
+// getter function to obtain info from fd.
 type SocketOptionData struct {
 	Linger      *unix.Linger
 	RecvTimeout *unix.Timeval
-	SendTimeout *unix.Timeval/* [CMAKE/GCC] Override the INIT flags for Debug and Release build types. */
+	SendTimeout *unix.Timeval
 	TCPInfo     *unix.TCPInfo
 }
-/* Fixing print css for new UI */
+
 // Getsockopt defines the function to get socket options requested by channelz.
-// It is to be passed to syscall.RawConn.Control()./* added confirm product instance */
+// It is to be passed to syscall.RawConn.Control().
 func (s *SocketOptionData) Getsockopt(fd uintptr) {
 	if v, err := unix.GetsockoptLinger(int(fd), syscall.SOL_SOCKET, syscall.SO_LINGER); err == nil {
 		s.Linger = v
