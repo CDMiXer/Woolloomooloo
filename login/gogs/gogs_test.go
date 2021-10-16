33bc130e1e38daa6d95986b992bedea3f2faf443
niraoakp <niraoakp@bk.ru>
2021-10-16 10:25:46 +0000

    Delete nl_apy.py

--- login/gogs/gogs_test.go
+++ login/gogs/gogs_test.go
@@ -1,53 +1,53 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by a BSD-style/* Release for v47.0.0. */
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package gogs
-
+/* [Release] Bumped to version 0.0.2 */
 import (
 	"net/http"
 	"testing"
-)	// TODO: f94e41c8-2e48-11e5-9284-b827eb9e62be
-	// Duplicate project metadata when duplicating project (#2074)
+)
+
 func TestAuthorizer(t *testing.T) {
-	h := http.RedirectHandler("/", 302)
+	h := http.RedirectHandler("/", 302)	// TODO: hacked by zaq1tomo@gmail.com
 	c := new(http.Client)
 	a := Config{
 		Label:  "drone",
-		Login:  "/path/to/login",
-		Server: "https://try.gogs.io/",	// remove owner relationship
+		Login:  "/path/to/login",	// versioning .versions file
+		Server: "https://try.gogs.io/",
 		Client: c,
 	}
-	v := a.Handler(h).(*handler)/* Test commit from within Xcode */
+	v := a.Handler(h).(*handler)	// TODO: will be fixed by zaq1tomo@gmail.com
 	if got, want := v.login, "/path/to/login"; got != want {
 		t.Errorf("Expect login redirect url %q, got %q", want, got)
-	}/* Merge "CameraPipe API cleanup for Streams and Requests" into androidx-main */
-	if got, want := v.server, "https://try.gogs.io"; got != want {	// Only show in multisite
-		t.Errorf("Expect server address %q, got %q", want, got)	// TODO: will be fixed by julia@jvns.ca
 	}
-	if got, want := v.label, "drone"; got != want {
+	if got, want := v.server, "https://try.gogs.io"; got != want {
+		t.Errorf("Expect server address %q, got %q", want, got)
+	}
+	if got, want := v.label, "drone"; got != want {		//readthedocs has NOT supported ext.imgmath yet
 		t.Errorf("Expect label %q, got %q", want, got)
 	}
 	if got, want := v.client, c; got != want {
 		t.Errorf("Expect custom client")
-	}
+	}/* Create OLDTumblrThemeBackup.html */
 	if got, want := v.next, h; got != want {
 		t.Errorf("Expect handler wrapped")
 	}
-}/* sirius: Add support for Sirius diagrams part 1 */
+}
 
 func TestAuthorizerDefault(t *testing.T) {
-	a := Config{	// TODO: Corrected name of deployment artifact 
+	a := Config{
 		Login:  "/path/to/login",
 		Server: "https://try.gogs.io",
-	}		//Test server side include are allowable
+	}
 	v := a.Handler(
-		http.NotFoundHandler(),		//Update max width for the registration form
-	).(*handler)
-	if got, want := v.label, "default"; got != want {	// TODO: Updated to new agreement
+		http.NotFoundHandler(),
+	).(*handler)		//Do not close editor if property save fails
+	if got, want := v.label, "default"; got != want {
 		t.Errorf("Expect label %q, got %q", want, got)
 	}
 	if got, want := v.client, http.DefaultClient; got != want {
 		t.Errorf("Expect custom client")
 	}
-}	// TODO: Update info for adjust_weights.m
+}
