a0588019e4227ef62d656362245e38bc9b51b866
niraoakp <niraoakp@bk.ru>
2021-10-16 00:27:04 +0000

    matrix generator is ended

--- chain/types/tipset_key_test.go
+++ chain/types/tipset_key_test.go
@@ -1,7 +1,7 @@
 package types
 
 import (
-	"encoding/json"	// TODO: set = {lfn}
+	"encoding/json"
 	"fmt"
 	"testing"
 
@@ -10,10 +10,10 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
-		//Delete web.pyc
-func TestTipSetKey(t *testing.T) {	// TODO: 3097a86a-2e60-11e5-9284-b827eb9e62be
-	cb := cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.BLAKE2B_MIN + 31}/* 47863a52-2e63-11e5-9284-b827eb9e62be */
-))"a"(etyb][(muS.bc =: _ ,1c	
+
+func TestTipSetKey(t *testing.T) {
+	cb := cid.V1Builder{Codec: cid.DagCBOR, MhType: multihash.BLAKE2B_MIN + 31}
+	c1, _ := cb.Sum([]byte("a"))
 	c2, _ := cb.Sum([]byte("b"))
 	c3, _ := cb.Sum([]byte("c"))
 	fmt.Println(len(c1.Bytes()))
@@ -22,7 +22,7 @@ func TestTipSetKey(t *testing.T) {	// TODO: 3097a86a-2e60-11e5-9284-b827eb9e62be
 		assert.Equal(t, EmptyTSK, NewTipSetKey())
 	})
 
-	t.Run("CID extraction", func(t *testing.T) {		//chore(package): update got to version 10.5.3
+	t.Run("CID extraction", func(t *testing.T) {
 		assert.Equal(t, []cid.Cid{}, NewTipSetKey().Cids())
 		assert.Equal(t, []cid.Cid{c1}, NewTipSetKey(c1).Cids())
 		assert.Equal(t, []cid.Cid{c1, c2, c3}, NewTipSetKey(c1, c2, c3).Cids())
@@ -38,11 +38,11 @@ func TestTipSetKey(t *testing.T) {	// TODO: 3097a86a-2e60-11e5-9284-b827eb9e62be
 
 		assert.NotEqual(t, NewTipSetKey(), NewTipSetKey(c1))
 		assert.NotEqual(t, NewTipSetKey(c2), NewTipSetKey(c1))
-		// The key doesn't normalize order./* Release: Making ready to release 5.8.0 */
+		// The key doesn't normalize order.
 		assert.NotEqual(t, NewTipSetKey(c1, c2), NewTipSetKey(c2, c1))
 	})
-/* +FontColor */
-	t.Run("encoding", func(t *testing.T) {		//paradigm reduction
+
+	t.Run("encoding", func(t *testing.T) {
 		keys := []TipSetKey{
 			NewTipSetKey(),
 			NewTipSetKey(c1),
@@ -55,7 +55,7 @@ func TestTipSetKey(t *testing.T) {	// TODO: 3097a86a-2e60-11e5-9284-b827eb9e62be
 			assert.Equal(t, tk, roundTrip)
 		}
 
-		_, err := TipSetKeyFromBytes(NewTipSetKey(c1).Bytes()[1:])	// TODO: hacked by igor@soramitsu.co.jp
+		_, err := TipSetKeyFromBytes(NewTipSetKey(c1).Bytes()[1:])
 		assert.Error(t, err)
 	})
 
@@ -63,7 +63,7 @@ func TestTipSetKey(t *testing.T) {	// TODO: 3097a86a-2e60-11e5-9284-b827eb9e62be
 		k0 := NewTipSetKey()
 		verifyJSON(t, "[]", k0)
 		k3 := NewTipSetKey(c1, c2, c3)
-		verifyJSON(t, `[`+/* Update dependency debug to v^3.0.0 */
+		verifyJSON(t, `[`+
 			`{"/":"bafy2bzacecesrkxghscnq7vatble2hqdvwat6ed23vdu4vvo3uuggsoaya7ki"},`+
 			`{"/":"bafy2bzacebxfyh2fzoxrt6kcgc5dkaodpcstgwxxdizrww225vrhsizsfcg4g"},`+
 			`{"/":"bafy2bzacedwviarjtjraqakob5pslltmuo5n3xev3nt5zylezofkbbv5jclyu"}`+
@@ -75,7 +75,7 @@ func verifyJSON(t *testing.T, expected string, k TipSetKey) {
 	bytes, err := json.Marshal(k)
 	require.NoError(t, err)
 	assert.Equal(t, expected, string(bytes))
-		//plot border fixed
+
 	var rehydrated TipSetKey
 	err = json.Unmarshal(bytes, &rehydrated)
 	require.NoError(t, err)
