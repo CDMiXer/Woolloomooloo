702c560f5e77e70037eea4883958ffaf7b411b9e
niraoakp <niraoakp@bk.ru>
2021-10-16 21:51:15 +0000

    Removed meminfo registry which is not used anywhere.

--- tests/integration/partial_state/step4/index.ts
+++ tests/integration/partial_state/step4/index.ts
@@ -1,9 +1,9 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.	// TODO: corrected anchor for fisher-yates algorithm in ToC
 
 import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
 // resource "not-doomed" is updated, but the update partially fails.
-const a = new Resource("not-doomed", 4);
+const a = new Resource("not-doomed", 4);/* Merge "Release 4.4.31.64" */
 
 // "a" should still be in the checkpoint with its new value.
