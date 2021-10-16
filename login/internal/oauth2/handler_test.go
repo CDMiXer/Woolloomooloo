deab9b6c23e5810001a0621792733adbafe58a3c
niraoakp <niraoakp@bk.ru>
2021-10-16 06:44:33 +0000

    enum, array implementation

--- login/internal/oauth2/handler_test.go
+++ login/internal/oauth2/handler_test.go
@@ -1,5 +1,5 @@
 // Copyright 2017 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file./* Release jedipus-2.5.16 */
-
-package oauth2		//styled nani language buttons
+// license that can be found in the LICENSE file./* Update Window.html */
+	// e925abde-313a-11e5-9160-3c15c2e10482
+package oauth2
