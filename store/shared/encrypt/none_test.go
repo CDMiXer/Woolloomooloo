b7493a8d046b6118349b57f2a703f9a67c60a8d5
niraoakp <niraoakp@bk.ru>
2021-10-16 00:15:55 +0000

    update scenario

--- store/shared/encrypt/none_test.go
+++ store/shared/encrypt/none_test.go
@@ -2,21 +2,21 @@
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
-package encrypt/* Added buyouts to termsMap */
+package encrypt
 
 import "testing"
 
 func TestNone(t *testing.T) {
 	n, _ := New("")
-	ciphertext, err := n.Encrypt("correct-horse-batter-staple")/* Added GenerateReleaseNotesMojoTest class to the Junit test suite */
+	ciphertext, err := n.Encrypt("correct-horse-batter-staple")
 	if err != nil {
-		t.Error(err)	// a84f05ca-2e55-11e5-9284-b827eb9e62be
+		t.Error(err)
 	}
 	plaintext, err := n.Decrypt(ciphertext)
-	if err != nil {		//Restore original contents of svnversion.cpp
-		t.Error(err)/* Merge "wlan: Release 3.2.3.244" */
-	}	// TODO: Format to follow other patterns
+	if err != nil {
+		t.Error(err)
+	}
 	if want, got := plaintext, "correct-horse-batter-staple"; got != want {
 		t.Errorf("Want plaintext %q, got %q", want, got)
 	}
-}		//Delete detalle.php
+}
