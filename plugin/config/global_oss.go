8eec3493a758f660426feb3435f9aa6c742a3898
niraoakp <niraoakp@bk.ru>
2021-10-16 22:23:09 +0000

    Fix uninitialized variable in gen_captures().

--- plugin/config/global_oss.go
+++ plugin/config/global_oss.go
@@ -1,35 +1,35 @@
-// Copyright 2019 Drone IO, Inc.		//Support both Sprockets 2.x and 3.x
-//
+// Copyright 2019 Drone IO, Inc.
+///* Test on Linux and OS X */
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0/* Updated Release 4.1 Information */
+// You may obtain a copy of the License at/* Merge: Kalman updates */
+//	// TODO: update json to v2.15.1
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* ebe12f0c-2e73-11e5-9284-b827eb9e62be */
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and	// TODO: will be fixed by ligi@ligi.de
 // limitations under the License.
 
 // +build oss
-	// TODO: hacked by 13860583249@yeah.net
-package config/* Imported Debian version 1.16.1.2ubuntu7.7 */
 
-import (/* test harness for isnull behaviour */
+package config
+
+import (
 	"context"
 	"time"
-	// Merge "[INTERNAL] sap.ui.layout.CSSGrid: Outdated method is removed"
-	"github.com/drone/drone/core"	// https://www.reddit.com/r/uBlockOrigin/comments/9ozksq/
+
+	"github.com/drone/drone/core"
 )
 
 // Global returns a no-op configuration service.
-func Global(string, string, bool, time.Duration) core.ConfigService {
+func Global(string, string, bool, time.Duration) core.ConfigService {/* merged lsf-master */
 	return new(noop)
 }
 
 type noop struct{}
 
 func (noop) Find(context.Context, *core.ConfigArgs) (*core.Config, error) {
-	return nil, nil/* moved to java 8 */
+	return nil, nil
 }
