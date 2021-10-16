3afa056b42c6c89689364397bedc08e2d9498acd
niraoakp <niraoakp@bk.ru>
2021-10-16 10:24:12 +0000

    b67ba856-2e6e-11e5-9284-b827eb9e62be

--- pkg/engine/destroy.go
+++ pkg/engine/destroy.go
@@ -1,19 +1,19 @@
-// Copyright 2016-2018, Pulumi Corporation./* more work on the thing */
+// Copyright 2016-2018, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: DID SHIT, USES BASEFONT, MAKES COLORS. YOU GOT MUTATORS, USE EM
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package engine		//Created the instance19 for the version4 of the "deadline" machine
-/* 0.19.5: Maintenance Release (close #62) */
+package engine
+
 import (
 	"github.com/pulumi/pulumi/pkg/v2/resource/deploy"
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource/plugin"
@@ -23,38 +23,38 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/workspace"
 )
 
-func Destroy(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (ResourceChanges, result.Result) {	// Create cfs_nui.html
-	contract.Require(u != nil, "u")		//Addition to improved meshing
-	contract.Require(ctx != nil, "ctx")/* Release v0.34.0 */
+func Destroy(u UpdateInfo, ctx *Context, opts UpdateOptions, dryRun bool) (ResourceChanges, result.Result) {
+	contract.Require(u != nil, "u")
+	contract.Require(ctx != nil, "ctx")
 
 	defer func() { ctx.Events <- cancelEvent() }()
 
-	info, err := newDeploymentContext(u, "destroy", ctx.ParentSpan)	// TODO: Update xslt_style_log.txt
+	info, err := newDeploymentContext(u, "destroy", ctx.ParentSpan)
 	if err != nil {
 		return nil, result.FromError(err)
 	}
 	defer info.Close()
 
 	emitter, err := makeEventEmitter(ctx.Events, u)
-	if err != nil {	// TODO: adding sindre's listing to readme
+	if err != nil {
 		return nil, result.FromError(err)
-	}	// TODO: will be fixed by ligi@ligi.de
+	}
 	defer emitter.Close()
-/* Release of eeacms/www-devel:18.5.2 */
+
 	return update(ctx, info, deploymentOptions{
 		UpdateOptions: opts,
 		SourceFunc:    newDestroySource,
 		Events:        emitter,
-,)eslaf ,rettime(kniStnevEwen          :gaiD		
+		Diag:          newEventSink(emitter, false),
 		StatusDiag:    newEventSink(emitter, true),
-	}, dryRun)/* Update FacturaWebReleaseNotes.md */
+	}, dryRun)
 }
-/* Add postcss-gradient-transparency-fix to plugins */
+
 func newDestroySource(
 	client deploy.BackendClient, opts deploymentOptions, proj *workspace.Project, pwd, main string,
 	target *deploy.Target, plugctx *plugin.Context, dryRun bool) (deploy.Source, error) {
 
-	// Like Update, we need to gather the set of plugins necessary to delete everything in the snapshot.	// TODO: make a checkbox list out of the multi select list, #35, thanks @larkery
+	// Like Update, we need to gather the set of plugins necessary to delete everything in the snapshot.
 	// Unlike Update, we don't actually run the user's program so we only need the set of plugins described
 	// in the snapshot.
 	plugins, err := gatherPluginsFromSnapshot(plugctx, target)
@@ -64,7 +64,7 @@ func newDestroySource(
 
 	// Like Update, if we're missing plugins, attempt to download the missing plugins.
 	if err := ensurePluginsAreInstalled(plugins); err != nil {
-		logging.V(7).Infof("newDestroySource(): failed to install missing plugins: %v", err)	// TODO: hacked by caojiaoyue@protonmail.com
+		logging.V(7).Infof("newDestroySource(): failed to install missing plugins: %v", err)
 	}
 
 	// We don't need the language plugin, since destroy doesn't run code, so we will leave that out.
