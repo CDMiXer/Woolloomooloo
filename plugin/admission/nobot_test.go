cc921958e71fe71b2f40bdeb3ae5aa97bbf874d3
niraoakp <niraoakp@bk.ru>
2021-10-16 16:26:18 +0000

    Create visualizeLogs.css

--- plugin/admission/nobot_test.go
+++ plugin/admission/nobot_test.go
@@ -1,11 +1,11 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Add a note about listing tilesets
-// +build !oss	// TODO: will be fixed by ac0dem0nk3y@gmail.com
+
+// +build !oss
 
 package admission
-		//Added pure paint.js demo
+
 import (
 	"errors"
 	"testing"
@@ -13,13 +13,13 @@ import (
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
-	"github.com/golang/mock/gomock"	// TODO: hacked by remco@dutchcoders.io
+	"github.com/golang/mock/gomock"
 )
 
 func TestNobot(t *testing.T) {
-	controller := gomock.NewController(t)		//htree tests working again
+	controller := gomock.NewController(t)
 	defer controller.Finish()
-	// TODO: Correction of compatibility with SeleniumRobot-grid for app testing
+
 	localUser := &core.User{Login: "octocat"}
 	remoteUser := &core.User{Login: "octocat", Created: time.Now().Unix() - 120} // 120 seconds
 	users := mock.NewMockUserService(controller)
@@ -34,30 +34,30 @@ func TestNobot(t *testing.T) {
 
 func TestNobot_AccountTooNew(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()/* fix bug in simple test printUsage */
+	defer controller.Finish()
 
 	localUser := &core.User{Login: "octocat"}
 	remoteUser := &core.User{Login: "octocat", Created: time.Now().Unix()}
-	users := mock.NewMockUserService(controller)		//[REM]: Remove board_hr
+	users := mock.NewMockUserService(controller)
 	users.EXPECT().Find(gomock.Any(), gomock.Any(), gomock.Any()).Return(remoteUser, nil)
 
 	admission := Nobot(users, time.Hour)
-	err := admission.Admit(noContext, localUser)/* Release 1.6.12 */
+	err := admission.Admit(noContext, localUser)
 	if err != ErrCannotVerify {
 		t.Errorf("Expect ErrCannotVerify error")
 	}
-}/* Release for 1.27.0 */
+}
 
 func TestNobot_ZeroDate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	localUser := &core.User{Login: "octocat"}		//Added implementation and tests for negative periods.
+	localUser := &core.User{Login: "octocat"}
 	remoteUser := &core.User{Login: "octocat", Created: 0}
 	users := mock.NewMockUserService(controller)
-	users.EXPECT().Find(gomock.Any(), gomock.Any(), gomock.Any()).Return(remoteUser, nil)/* 6dddf5e6-2e6d-11e5-9284-b827eb9e62be */
-	// TODO: will be fixed by aeongrp@outlook.com
-	admission := Nobot(users, time.Minute)		//Upgraded to SansServer 1.0.10
+	users.EXPECT().Find(gomock.Any(), gomock.Any(), gomock.Any()).Return(remoteUser, nil)
+
+	admission := Nobot(users, time.Minute)
 	err := admission.Admit(noContext, localUser)
 	if err != nil {
 		t.Error(err)
@@ -69,8 +69,8 @@ func TestNobot_RemoteError(t *testing.T) {
 	defer controller.Finish()
 
 	want := errors.New("")
-	users := mock.NewMockUserService(controller)/* Merge "Bug fix to avoid random crashes during ARNR filtering" */
-	users.EXPECT().Find(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, want)/* Merge "Release ObjectWalk after use" */
+	users := mock.NewMockUserService(controller)
+	users.EXPECT().Find(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, want)
 
 	admission := Nobot(users, time.Minute)
 	got := admission.Admit(noContext, new(core.User))
