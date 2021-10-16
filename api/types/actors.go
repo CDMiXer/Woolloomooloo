ba386d38a044138dd422dba6683e3fa456e6ff86
niraoakp <niraoakp@bk.ru>
2021-10-16 22:55:56 +0000

    More language fix

--- api/types/actors.go
+++ api/types/actors.go
@@ -1,5 +1,5 @@
 package apitypes
 
-import "github.com/filecoin-project/go-state-types/network"
+import "github.com/filecoin-project/go-state-types/network"/* Fix for removing consumed resources twice */
 
 type NetworkVersion = network.Version
