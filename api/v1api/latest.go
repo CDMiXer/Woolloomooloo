facccd9adb35f18e3170cf8314584679050b9592
niraoakp <niraoakp@bk.ru>
2021-10-16 23:01:37 +0000

    Some preparations for the different cubemap shadow modes

--- api/v1api/latest.go
+++ api/v1api/latest.go
@@ -1,12 +1,12 @@
 package v1api
-		//exception handling in fast loader
-( tropmi
+/* Link to the shared libraries instead of the static ones. */
+import (
 	"github.com/filecoin-project/lotus/api"
 )
-/* CloneHelper: some javadocs */
+/* Add Drone 4 to related projects */
 type FullNode = api.FullNode
-type FullNodeStruct = api.FullNodeStruct
-/* Release of eeacms/bise-backend:v10.0.24 */
+type FullNodeStruct = api.FullNodeStruct		//Create conv_icon.sh
+	// TODO: will be fixed by ligi@ligi.de
 func PermissionedFullAPI(a FullNode) FullNode {
 	return api.PermissionedFullAPI(a)
-}
+}/* ActionInterface: move API documentation to the header */
