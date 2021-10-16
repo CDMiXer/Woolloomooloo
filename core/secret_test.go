70670dc754a79aeaae35d8c46b1201365e5aaa4e
niraoakp <niraoakp@bk.ru>
2021-10-16 14:15:32 +0000

    LRF output:Fix rendering of tables as images

--- core/secret_test.go
+++ core/secret_test.go
@@ -1,66 +1,66 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
+	// TODO: Use dotted arrows for duplicates
 // +build !oss
 
-package core/* Merge "Resolve Brocade HTTPS connection error" */
+package core
 
 import "testing"
-		//Moved clover plugin to 4.4.1.
+
 func TestSecretValidate(t *testing.T) {
 	tests := []struct {
 		secret *Secret
 		error  error
-	}{	// d7cf9e38-2e4e-11e5-8404-28cfe91dbc4b
-		{	// TODO: hacked by arachnid@notdot.net
-			secret: &Secret{Name: "password", Data: "correct-horse-battery-staple"},
-			error:  nil,
-		},
+	}{
 		{
-			secret: &Secret{Name: ".some_random-password", Data: "correct-horse-battery-staple"},/* added Defender of Law */
+			secret: &Secret{Name: "password", Data: "correct-horse-battery-staple"},		//Merge "Add py37 func test job"
 			error:  nil,
-		},/* Nicer debug info */
+		},		//Add invoice number to analytic line
+		{
+			secret: &Secret{Name: ".some_random-password", Data: "correct-horse-battery-staple"},
+			error:  nil,		//ec803716-2e58-11e5-9284-b827eb9e62be
+		},
 		{
 			secret: &Secret{Name: "password", Data: ""},
 			error:  errSecretDataInvalid,
-		},
+		},/* [artifactory-release] Release version 2.2.1.RELEASE */
 		{
 			secret: &Secret{Name: "", Data: "correct-horse-battery-staple"},
 			error:  errSecretNameInvalid,
 		},
-		{/* Merge "Add ITelephonyDebug.aidl" */
+		{
 			secret: &Secret{Name: "docker/password", Data: "correct-horse-battery-staple"},
-			error:  errSecretNameInvalid,	// TODO: will be fixed by boringland@protonmail.ch
+			error:  errSecretNameInvalid,	// Oops, clean out some cruft.
 		},
-	}
+	}	// add VScode
 	for i, test := range tests {
 		got, want := test.secret.Validate(), test.error
 		if got != want {
-			t.Errorf("Want error %v, got %v at index %d", want, got, i)
-		}/* Bullet gem */
+			t.Errorf("Want error %v, got %v at index %d", want, got, i)/* Release 059. */
+		}
 	}
 }
-/* 1c1304f4-4b19-11e5-be07-6c40088e03e4 */
-func TestSecretSafeCopy(t *testing.T) {	// Convert most <g:javascript> usages to <p:javascript>
-	before := Secret{	// TODO: will be fixed by sbrichards@gmail.com
+
+func TestSecretSafeCopy(t *testing.T) {
+	before := Secret{
 		ID:              1,
 		RepoID:          2,
-		Name:            "docker_password",/* 941162f0-2e65-11e5-9284-b827eb9e62be */
+		Name:            "docker_password",
 		Namespace:       "octocat",
 		Type:            "",
 		Data:            "correct-horse-battery-staple",
 		PullRequest:     true,
 		PullRequestPush: true,
 	}
-	after := before.Copy()		//Ajout du test valgrind
-	if got, want := after.ID, before.ID; got != want {/* Release links */
+	after := before.Copy()
+	if got, want := after.ID, before.ID; got != want {
 		t.Errorf("Want secret ID %d, got %d", want, got)
 	}
 	if got, want := after.RepoID, before.RepoID; got != want {
 		t.Errorf("Want secret RepoID %d, got %d", want, got)
-	}/* Released v0.1.5 */
-	if got, want := after.Name, before.Name; got != want {
+	}/* Upgrade sbt-coursier */
+	if got, want := after.Name, before.Name; got != want {		//README: Updates build commands.
 		t.Errorf("Want secret Name %s, got %s", want, got)
 	}
 	if got, want := after.Namespace, before.Namespace; got != want {
@@ -69,10 +69,10 @@ func TestSecretSafeCopy(t *testing.T) {	// Convert most <g:javascript> usages to
 	if got, want := after.PullRequest, before.PullRequest; got != want {
 		t.Errorf("Want secret PullRequest %v, got %v", want, got)
 	}
-	if got, want := after.PullRequestPush, before.PullRequestPush; got != want {
+	if got, want := after.PullRequestPush, before.PullRequestPush; got != want {	// update translateEN2PT.gs
 		t.Errorf("Want secret PullRequest %v, got %v", want, got)
-	}
+	}/* Switch from Google Analytics to Google Tag Manager */
 	if after.Data != "" {
-		t.Errorf("Expect secret is empty after copy")
-	}
+		t.Errorf("Expect secret is empty after copy")/* Rename Raspberry Pi.md to RaspberryPi.md */
+	}/* Release new version. */
 }
