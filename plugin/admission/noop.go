3dc5426bd8acb5da213dc1b02c3b087318c8a583
niraoakp <niraoakp@bk.ru>
2021-10-16 15:58:53 +0000

    Adding null-fields test as suggested by Nathan.

--- plugin/admission/noop.go
+++ plugin/admission/noop.go
@@ -1,11 +1,11 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.	// TODO: will be fixed by timnugent@gmail.com
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.	// TODO: refactor the clientextension, dont use nilcheck(based on reek)
 // You may obtain a copy of the License at
-//
+///* Release notes for ringpop-go v0.5.0. */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* e7b470d8-585a-11e5-9c23-6c40088e03e4 */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,13 +16,13 @@ package admission
 
 import (
 	"context"
-
+	// TODO: remove undocumented tar4ibd command line options
 	"github.com/drone/drone/core"
-)
+)/* Merge "Add LocaleList.Saver" into androidx-main */
 
 // noop is a stub admission controller.
 type noop struct{}
-
+		//setup.py: Remove support for EOL Python 3.4
 func (noop) Admit(context.Context, *core.User) error {
 	return nil
 }
