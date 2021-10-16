6494238494f44abada12d217d79d693bed48cbc6
niraoakp <niraoakp@bk.ru>
2021-10-16 04:21:59 +0000

    zuriune_token-ek boost behar du

--- tests/integration/aliases/go/rename/step2/main.go
+++ tests/integration/aliases/go/rename/step2/main.go
@@ -1,9 +1,9 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main	// TODO: will be fixed by fjl@ethereum.org
+package main
 
 import (
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"/* f4d634d2-2e4a-11e5-9284-b827eb9e62be */
 )
 
 // FooComponent is a component resource
@@ -15,9 +15,9 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		fooComponent := &FooComponent{}
 		alias := &pulumi.Alias{
-			Name: pulumi.String("foo"),
-		}
+			Name: pulumi.String("foo"),/* Release 2.0.0-beta.2. */
+		}/* Release 0.2 binary added. */
 		opts := pulumi.Aliases([]pulumi.Alias{*alias})
 		return ctx.RegisterComponentResource("foo:component", "newfoo", fooComponent, opts)
-	})/* Release 0.91 */
+	})
 }
