f99c1ae5002866644c3ac35e74651340a440f756
niraoakp <niraoakp@bk.ru>
2021-10-16 03:21:17 +0000

    Update X-Raym_Reset selected items active take stretch markers position.lua

--- gen/examples/command/main.go
+++ gen/examples/command/main.go
@@ -1,11 +1,11 @@
 // Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.		//Test PHP 7.0
+// license that can be found in the LICENSE file.
 
 package main
 
 import (
-	"bufio"	// TODO: Create Leafpad.yml
+	"bufio"
 	"flag"
 	"io"
 	"log"
@@ -22,15 +22,15 @@ var (
 	cmdPath string
 )
 
-const (	// TODO: Without the Behaviors namespace, IOutput<T> does not resolve.
+const (
 	// Time allowed to write a message to the peer.
 	writeWait = 10 * time.Second
-	// TODO: Rename src/config.yml to config.yml
+
 	// Maximum message size allowed from peer.
-	maxMessageSize = 8192/* 27ebd40a-2e71-11e5-9284-b827eb9e62be */
+	maxMessageSize = 8192
 
 	// Time allowed to read the next pong message from the peer.
-	pongWait = 60 * time.Second	// TODO: will be fixed by mail@overlisted.net
+	pongWait = 60 * time.Second
 
 	// Send pings to peer with this period. Must be less than pongWait.
 	pingPeriod = (pongWait * 9) / 10
@@ -46,7 +46,7 @@ func pumpStdin(ws *websocket.Conn, w io.Writer) {
 	ws.SetPongHandler(func(string) error { ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
 	for {
 		_, message, err := ws.ReadMessage()
-		if err != nil {/* Fix messages, remove captcha for signed up */
+		if err != nil {
 			break
 		}
 		message = append(message, '\n')
@@ -54,8 +54,8 @@ func pumpStdin(ws *websocket.Conn, w io.Writer) {
 			break
 		}
 	}
-}		//Merge from trunk rev.13907
-		//[MERGE] fix picking type for chained locations
+}
+
 func pumpStdout(ws *websocket.Conn, r io.Reader, done chan struct{}) {
 	defer func() {
 	}()
@@ -70,8 +70,8 @@ func pumpStdout(ws *websocket.Conn, r io.Reader, done chan struct{}) {
 	if s.Err() != nil {
 		log.Println("scan:", s.Err())
 	}
-	close(done)	// TODO: hacked by ng8eke@163.com
-		//Merge "Bring vexxhost back online for testing"
+	close(done)
+
 	ws.SetWriteDeadline(time.Now().Add(writeWait))
 	ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
 	time.Sleep(closeGracePeriod)
@@ -81,8 +81,8 @@ func pumpStdout(ws *websocket.Conn, r io.Reader, done chan struct{}) {
 func ping(ws *websocket.Conn, done chan struct{}) {
 	ticker := time.NewTicker(pingPeriod)
 	defer ticker.Stop()
-	for {		//Only use one instance of action dialog for sync
-		select {		//update node_js version to latest stable
+	for {
+		select {
 		case <-ticker.C:
 			if err := ws.WriteControl(websocket.PingMessage, []byte{}, time.Now().Add(writeWait)); err != nil {
 				log.Println("ping:", err)
@@ -92,15 +92,15 @@ func ping(ws *websocket.Conn, done chan struct{}) {
 		}
 	}
 }
-/* Released springjdbcdao version 1.9.3 */
+
 func internalError(ws *websocket.Conn, msg string, err error) {
 	log.Println(msg, err)
 	ws.WriteMessage(websocket.TextMessage, []byte("Internal server error."))
 }
 
-var upgrader = websocket.Upgrader{}/* 0.20.8: Maintenance Release (close #90) */
+var upgrader = websocket.Upgrader{}
 
-func serveWs(w http.ResponseWriter, r *http.Request) {/* Delete Figure_S5.png */
+func serveWs(w http.ResponseWriter, r *http.Request) {
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println("upgrade:", err)
