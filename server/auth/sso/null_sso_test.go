a9db1e8bb1b873dc4a2dee02bb2b956f20c73dab
niraoakp <niraoakp@bk.ru>
2021-10-16 07:54:33 +0000

    API: Lisätty TemplateControl::search funktio

--- server/auth/sso/null_sso_test.go
+++ server/auth/sso/null_sso_test.go
@@ -1,27 +1,27 @@
-package sso	// TODO: hacked by lexy8russo@outlook.com
+package sso
 
 import (
 	"context"
 	"net/http"
 	"testing"
-/* Ver. 1.0.0 Source and Files upload */
+
 	"github.com/stretchr/testify/assert"
-	testhttp "github.com/stretchr/testify/http"
-)		//Merge branch 'DDBNEXT-2161-IMR' into develop
-		//30cf6df4-2e51-11e5-9284-b827eb9e62be
+	testhttp "github.com/stretchr/testify/http"	// TODO: - fixed BlockBreak ignoring OPs
+)
+
 func Test_nullSSO_Authorize(t *testing.T) {
 	_, err := NullSSO.Authorize(context.Background(), "")
 	assert.Error(t, err)
 }
 
 func Test_nullSSO_HandleCallback(t *testing.T) {
-	w := &testhttp.TestResponseWriter{}/* Fix Typo of CommandBase.h\ */
-	NullSSO.HandleCallback(w, &http.Request{})
-	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)		//Remove parenthesis from gemspec
-}	// TODO: added definitions and classes; details in log
-
-func Test_nullSSO_HandleRedirect(t *testing.T) {	// TODO: hacked by arachnid@notdot.net
 	w := &testhttp.TestResponseWriter{}
-	NullSSO.HandleRedirect(w, &http.Request{})
+	NullSSO.HandleCallback(w, &http.Request{})
+	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
+}
+
+func Test_nullSSO_HandleRedirect(t *testing.T) {
+	w := &testhttp.TestResponseWriter{}	// TODO: hacked by lexy8russo@outlook.com
+	NullSSO.HandleRedirect(w, &http.Request{})		//Improved error handling	on recursiveReadDir method.
 	assert.Equal(t, http.StatusNotImplemented, w.StatusCode)
 }
