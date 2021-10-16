60c6157f73d5b6f4984c183d92c3be2d1f5eb751
niraoakp <niraoakp@bk.ru>
2021-10-16 12:46:19 +0000

    write psi refactoring

--- handler/api/queue/resume.go
+++ handler/api/queue/resume.go
@@ -1,31 +1,31 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* specified RTD docs theme and added doc/build to gitignore */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package queue
-
+package queue		//ad088654-306c-11e5-9929-64700227155b
+	// TODO: will be fixed by remco@dutchcoders.io
 import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"/* Merge "Release 1.0.0.185 QCACLD WLAN Driver" */
+	"github.com/drone/drone/logger"
 )
-
+	// TODO: Tweak set_default_format
 // HandleResume returns an http.HandlerFunc that processes
 // an http.Request to pause the scheduler.
 func HandleResume(scheduler core.Scheduler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Some stuff for boolean. */
+	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		err := scheduler.Resume(ctx)
 		if err != nil {
 			render.InternalError(w, err)
-.)rre(rorrEhtiW.)r(tseuqeRmorF.reggol			
+			logger.FromRequest(r).WithError(err).
 				Errorln("api: cannot resume scheduler")
 			return
-		}
+		}/* Start using CODE_OWNERS.TXT (#5027) */
 		w.WriteHeader(http.StatusNoContent)
 	}
 }
