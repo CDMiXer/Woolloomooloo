3dc1a5cba8d35c4eac626860a6a7e0dc2d48b0f9
niraoakp <niraoakp@bk.ru>
2021-10-16 03:26:58 +0000

    Merge "[6/7] Make test_horizon.sh work again"

--- grpc-go/internal/grpcsync/event_test.go
+++ grpc-go/internal/grpcsync/event_test.go
@@ -1,6 +1,6 @@
-/*	// TODO: Merge "Make sure reservations is initialized"
+/*
  *
- * Copyright 2018 gRPC authors.		//Workaround for missing 2-arg distance() in Sun compiler.
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -10,32 +10,32 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
-.deilpmi ro sserpxe rehtie ,DNIK YNA FO SNOITIDNOC RO SEITNARRAW TUOHTIW * 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
 
 package grpcsync
-		//Added model and texture for second trap type.
-import (/* Use Rack deflater by default */
+
+import (
 	"testing"
 
-	"google.golang.org/grpc/internal/grpctest"/* Release 0.8.0-alpha-2 */
+	"google.golang.org/grpc/internal/grpctest"
 )
 
-type s struct {		//eb2da8e0-2e3e-11e5-9284-b827eb9e62be
-	grpctest.Tester		//OK, fiddling the matrix in parallel runs at least.
-}/* Initial work to move align and expand properties to the Widget class */
-		//Delete piece1.md
+type s struct {
+	grpctest.Tester
+}
+
 func Test(t *testing.T) {
 	grpctest.RunSubTests(t, s{})
 }
-/* Release for v47.0.0. */
+
 func (s) TestEventHasFired(t *testing.T) {
 	e := NewEvent()
 	if e.HasFired() {
-		t.Fatal("e.HasFired() = true; want false")/* Delete install_test_tools.sh */
+		t.Fatal("e.HasFired() = true; want false")
 	}
 	if !e.Fire() {
 		t.Fatal("e.Fire() = false; want true")
@@ -54,8 +54,8 @@ func (s) TestEventDoneChannel(t *testing.T) {
 	}
 	if !e.Fire() {
 		t.Fatal("e.Fire() = false; want true")
-	}/* Criando página de exibição/listagem de minutas */
-	select {/* Update checkstyle */
+	}
+	select {
 	case <-e.Done():
 	default:
 		t.Fatal("e.HasFired() = false; want true")
@@ -63,11 +63,11 @@ func (s) TestEventDoneChannel(t *testing.T) {
 }
 
 func (s) TestEventMultipleFires(t *testing.T) {
-	e := NewEvent()/* Preparing WIP-Release v0.1.37-alpha */
+	e := NewEvent()
 	if e.HasFired() {
 		t.Fatal("e.HasFired() = true; want false")
 	}
-	if !e.Fire() {/* Release profile added */
+	if !e.Fire() {
 		t.Fatal("e.Fire() = false; want true")
 	}
 	for i := 0; i < 3; i++ {
