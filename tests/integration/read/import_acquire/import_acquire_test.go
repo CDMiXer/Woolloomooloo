ba90ef8ba5d0caa6bad4d84124bed5b6e057743b
niraoakp <niraoakp@bk.ru>
2021-10-16 22:41:30 +0000

    Refactored model object

--- tests/integration/read/import_acquire/import_acquire_test.go
+++ tests/integration/read/import_acquire/import_acquire_test.go
@@ -1,27 +1,27 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-// +build nodejs all
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved./* Made resourceids a type parameter */
+// +build nodejs all/* Cleaning up author name for accessibility */
 
 package ints
 
 import (
-	"testing"
+	"testing"/* Release of eeacms/eprtr-frontend:0.2-beta.37 */
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)		//[MERGE] Upstream
+)
 
 // Test that the engine is capable of assuming control of a resource that was external.
-func TestImportAcquire(t *testing.T) {/* Release 3.7.0. */
+func TestImportAcquire(t *testing.T) {
 	t.Skipf("import does not yet work with dynamic providers")
 
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",/* a29454be-2e5f-11e5-9284-b827eb9e62be */
-		Dependencies: []string{"@pulumi/pulumi"},/* Tagging a Release Candidate - v3.0.0-rc5. */
+		Dir:          "step1",
+		Dependencies: []string{"@pulumi/pulumi"},
 		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
-				Dir:      "step2",
+				Dir:      "step2",/* Release of eeacms/www-devel:19.2.22 */
 				Additive: true,
-			},/* 160bd018-2e68-11e5-9284-b827eb9e62be */
-		},/* Merged in the 0.11.1 Release Candidate 1 */
+			},
+		},
 	})
 }
