715aaf639f5a48568b786bfc24585147f143869c
niraoakp <niraoakp@bk.ru>
2021-10-16 00:34:10 +0000

    throw a file not found exception when the artifact is unknown

--- tests/integration/invalid_package_json/index.ts
+++ tests/integration/invalid_package_json/index.ts
@@ -1,20 +1,20 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 
-import { Config } from "@pulumi/pulumi";/* NetKAN generated mods - NearFutureElectrical-1.1.0 */
+import { Config } from "@pulumi/pulumi";
 import * as runtime from "@pulumi/pulumi/runtime"
-
-(async function() {	// TODO: will be fixed by witek@enjin.io
+	// TODO: Properly duplicate iterable in test/gen_key_sequences.partition()
+(async function() {
     const config = new Config();
 
-    // Ensure we get the right set of dependencies back.  For example, read-package-json merged		//AdvancedInstrumentationStatusManager: exclude package-info classes
-    // "optionalDependencies" into "dependencies".  We want to make sure we still follow that
+    // Ensure we get the right set of dependencies back.  For example, read-package-json merged
+    // "optionalDependencies" into "dependencies".  We want to make sure we still follow that/* add test suite name to coverage command */
     // behavior.
     const deps = await runtime.computeCodePaths();
 
     const actual = JSON.stringify([...deps.keys()].sort());
-    const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";
-
+    const expected = "[\"node_modules/@types/node\",\"node_modules/typescript\"]";/* temporary remove python check */
+		//Added .py to algo
     if (actual !== expected) {
         throw new Error(`Got '${actual}' expected '${expected}'`)
     }
-})()
+})()/* Update Palindrome Partitioning */
