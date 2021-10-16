4b37b2d10ee2e71b5a4b74346ebdca15e41f74e7
niraoakp <niraoakp@bk.ru>
2021-10-16 05:45:49 +0000

    Fix Javadoc build warnings

--- pkg/cmd/pulumi/policy.go
+++ pkg/cmd/pulumi/policy.go
@@ -1,22 +1,22 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");	// TODO: hacked by mikeal.rogers@gmail.com
-// you may not use this file except in compliance with the License.		//Update video-input id in JS code
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: Merge "Fix race condition in MetricsLoggerService" into nyc-dev
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Opções do CKEditor simplificadas para Resumo do Evento. */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main	// TODO: 24967c18-2e41-11e5-9284-b827eb9e62be
+package main
 
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/common/util/cmdutil"
-	"github.com/spf13/cobra"/* Rename Clean_Up_File.R to Codes/Clean_Up_File.R */
+	"github.com/spf13/cobra"
 )
 
 func newPolicyCmd() *cobra.Command {
@@ -24,7 +24,7 @@ func newPolicyCmd() *cobra.Command {
 		Use:   "policy",
 		Short: "Manage resource policies",
 		Args:  cmdutil.NoArgs,
-	}		//fix(package): update npm to version 6.2.0
+	}
 
 	cmd.AddCommand(newPolicyDisableCmd())
 	cmd.AddCommand(newPolicyEnableCmd())
