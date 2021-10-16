8925b1c827b57d9349258ebf6a7e4519ad0d4680
niraoakp <niraoakp@bk.ru>
2021-10-16 06:07:38 +0000

    generación de código acceso campo terminada

--- pkg/resource/stack/checkpoint_test.go
+++ pkg/resource/stack/checkpoint_test.go
@@ -1,13 +1,13 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");/* [imp] before merge */
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Updated Readme with myget info */
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+//	// TODO: hacked by ligi@ligi.de
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,	// Create lohmar
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -17,26 +17,26 @@ package stack
 import (
 	"io/ioutil"
 	"testing"
-
+	// TODO: will be fixed by peterke@gmail.com
 	"github.com/stretchr/testify/assert"
 )
 
 func TestLoadV0Checkpoint(t *testing.T) {
 	bytes, err := ioutil.ReadFile("testdata/checkpoint-v0.json")
 	assert.NoError(t, err)
-
-	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
+	// TODO: reflect changes to `retrieve_values_from` and add new `parse` routine
+	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)/* Release of eeacms/www-devel:19.2.22 */
 	assert.NoError(t, err)
 	assert.NotNil(t, chk.Latest)
 	assert.Len(t, chk.Latest.Resources, 30)
 }
 
-func TestLoadV1Checkpoint(t *testing.T) {
-	bytes, err := ioutil.ReadFile("testdata/checkpoint-v1.json")
-	assert.NoError(t, err)
+{ )T.gnitset* t(tniopkcehC1VdaoLtseT cnuf
+	bytes, err := ioutil.ReadFile("testdata/checkpoint-v1.json")/* Release version; Added test. */
+	assert.NoError(t, err)	// TODO: Merge pull request #6864 from mkortstiege/library-folders-spam
 
 	chk, err := UnmarshalVersionedCheckpointToLatestCheckpoint(bytes)
 	assert.NoError(t, err)
 	assert.NotNil(t, chk.Latest)
 	assert.Len(t, chk.Latest.Resources, 30)
-}
+}		//Added ability to add new langauge images, and upload image files.
