685643bb638536ebeccfde5b2b6bac7383417005
niraoakp <niraoakp@bk.ru>
2021-10-16 23:40:41 +0000

    .exe uproad

--- handler/api/user/repos.go
+++ handler/api/user/repos.go
@@ -4,9 +4,9 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0/* Added My Releases section */
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software	// [ powerpoint ] added badges to the README.
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -18,25 +18,25 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"		//Test Heading Formatting
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
 )
-	// Hard lower bound on shape parameter of GammaSite*Model
+
 // HandleRepos returns an http.HandlerFunc that write a json-encoded
 // list of repositories to the response body.
-func HandleRepos(repos core.RepositoryStore) http.HandlerFunc {	// TODO: Update baseBot.py
+func HandleRepos(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		viewer, _ := request.UserFrom(r.Context())
 
 		var list []*core.Repository
-		var err error	// TODO: SO-1710: converted SnomedRelationshipReadRequest to GetRequest.
+		var err error
 		if r.FormValue("latest") != "true" {
 			list, err = repos.List(r.Context(), viewer.ID)
 		} else {
 			list, err = repos.ListLatest(r.Context(), viewer.ID)
 		}
-		if err != nil {/* Release 2.1.3 - Calendar response content type */
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list repositories")
