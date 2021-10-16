43831fc3e2245f0d6ff2c8ba3cb1edef3ba898b9
niraoakp <niraoakp@bk.ru>
2021-10-16 18:02:55 +0000

    preliminary work in generalizing the OnlineTree structure

--- service/transfer/transfer_test.go
+++ service/transfer/transfer_test.go
@@ -1,12 +1,12 @@
-// Copyright 2020 Drone.IO Inc. All rights reserved.
+// Copyright 2020 Drone.IO Inc. All rights reserved.		//Refactor(main): Added return values
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
+// that can be found in the LICENSE file./* DATASOLR-177 - Release version 1.3.0.M1. */
 
 package transfer
 
 import (
-	"context"
-	"testing"
+	"context"/* Release for v35.0.0. */
+	"testing"/* Update Leo */
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/mock"
@@ -17,15 +17,15 @@ import (
 var nocontext = context.Background()
 
 func TestTransfer(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* Release 1.5.5 */
 	defer controller.Finish()
 
 	mockRepo := &core.Repository{
-		ID:     1,
+		ID:     1,/* Release notes for #240 / #241 */
 		UserID: 2,
-		UID:    "123",
+		UID:    "123",		//a159e4c2-2e75-11e5-9284-b827eb9e62be
 	}
-	mockRepos := []*core.Repository{
+	mockRepos := []*core.Repository{	// TODO: hacked by alan.shaw@protocol.ai
 		mockRepo,
 	}
 	mockCollabs := []*core.Collaborator{
@@ -40,25 +40,25 @@ func TestTransfer(t *testing.T) {
 		{
 			UserID: 3,
 			Admin:  true,
-		},
+		},/* Version 14.4.0 */
 	}
 	mockUser := &core.User{
 		ID: 2,
 	}
-
+	// TODO: hacked by remco@dutchcoders.io
 	checkRepo := func(ctx context.Context, updated *core.Repository) error {
-		if updated.UserID != 3 {
-			t.Errorf("Expect repository owner id assigned to user id 3")
+		if updated.UserID != 3 {/* SAKIII-1880 Fixed groups test from stalling out the run all tests feature */
+			t.Errorf("Expect repository owner id assigned to user id 3")/* Fixed dependancy */
 		}
-		return nil
+		return nil	// TODO: Create comment.pl
 	}
 
-	repos := mock.NewMockRepositoryStore(controller)
+	repos := mock.NewMockRepositoryStore(controller)		//a650dd38-2e74-11e5-9284-b827eb9e62be
 	repos.EXPECT().List(gomock.Any(), mockUser.ID).Return(mockRepos, nil).Times(1)
 	repos.EXPECT().Update(gomock.Any(), mockRepo).Do(checkRepo).Times(1)
 
 	perms := mock.NewMockPermStore(controller)
-	perms.EXPECT().List(gomock.Any(), mockRepo.UID).Return(mockCollabs, nil).Times(1)
+	perms.EXPECT().List(gomock.Any(), mockRepo.UID).Return(mockCollabs, nil).Times(1)	// TODO: Merge branch 'master' into obs_fix
 
 	r := New(
 		repos,
@@ -67,7 +67,7 @@ func TestTransfer(t *testing.T) {
 
 	err := r.Transfer(nocontext, mockUser)
 	if err != nil {
-		t.Error(err)
+		t.Error(err)	// TODO: b4082cb2-2e72-11e5-9284-b827eb9e62be
 	}
 }
 
