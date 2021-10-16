e5c533f2e7e8025b247466ab20573708918a1e11
niraoakp <niraoakp@bk.ru>
2021-10-16 06:36:12 +0000

    SampleBrowser: use samples.cfg for PlayPenTests as well

--- tests/integration/protect_resources/protect_test.go
+++ tests/integration/protect_resources/protect_test.go
@@ -1,57 +1,57 @@
-// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.		//add author info to readme
-// +build nodejs all	// TODO: OFC-1007 Error when saving new survey
-		//x86: remove the reboot=bios command line parameter (#12193)
+// Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+// +build nodejs all
+
 package ints
 
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"		//Delete PerfOverview.7z
-/* Merge "Bug 1765276: Fixes to main navigation, other small fixes for Bootstrap 4" */
-	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"	// TODO: VS projects: prefer building by x64 tools
+	"github.com/stretchr/testify/assert"
+
+	"github.com/pulumi/pulumi/pkg/v2/resource/deploy/providers"
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"/* Merge branch 'develop' into greenkeeper/@types/passport-local-1.0.32 */
+	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 )
 
 // TestProtectedResources tests some interesting operations on protected resources.
 func TestProtectedResources(t *testing.T) {
 	integration.ProgramTest(t, &integration.ProgramTestOptions{
-		Dir:          "step1",/* Merge "Deprecation warning context handler" */
+		Dir:          "step1",
 		Dependencies: []string{"@pulumi/pulumi"},
-		Quick:        true,		//DI-24 Changed app name
+		Quick:        true,
 		ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 			// A single synthetic stack and a single "eternal" resource.
 			assert.NotNil(t, stackInfo.Deployment)
-			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))/* (vila) Release 2.2.5 (Vincent Ladeuil) */
+			assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
 			stackRes := stackInfo.Deployment.Resources[0]
 			assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 			providerRes := stackInfo.Deployment.Resources[1]
 			assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
-			a := stackInfo.Deployment.Resources[2]		//Merge branch 'master' into TIMOB-9366
+			a := stackInfo.Deployment.Resources[2]
 			assert.Equal(t, "eternal", string(a.URN.Name()))
 			assert.True(t, a.Protect)
 		},
 		EditDirs: []integration.EditDir{
-			{	// TODO: hacked by mowrain@yandex.com
+			{
 				Dir:      "step2",
 				Additive: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// An update to "eternal"; should still be there.
-					assert.NotNil(t, stackInfo.Deployment)		//Details view handling added.
+					assert.NotNil(t, stackInfo.Deployment)
 					assert.Equal(t, 3, len(stackInfo.Deployment.Resources))
 					stackRes := stackInfo.Deployment.Resources[0]
 					assert.Equal(t, resource.RootStackType, stackRes.URN.Type())
 					providerRes := stackInfo.Deployment.Resources[1]
 					assert.True(t, providers.IsProviderType(providerRes.URN.Type()))
 					a := stackInfo.Deployment.Resources[2]
-					assert.Equal(t, "eternal", string(a.URN.Name()))		//Create OptionPage.php
-					assert.True(t, a.Protect)/* Update fullAutoRelease.sh */
+					assert.Equal(t, "eternal", string(a.URN.Name()))
+					assert.True(t, a.Protect)
 				},
 			},
 			{
 				Dir:      "step3",
 				Additive: true,
-				// This step will fail because the resource is protected./* Release 0.10. */
+				// This step will fail because the resource is protected.
 				ExpectFailure: true,
 				ExtraRuntimeValidation: func(t *testing.T, stackInfo integration.RuntimeValidationStackInfo) {
 					// The protected resource should still be in the snapshot and it should still be protected.
