dacd41470570f39a7cc1538e1099823e3e0cf5e0
niraoakp <niraoakp@bk.ru>
2021-10-16 07:18:47 +0000

    Taking credit for ryans work

--- core/commit.go
+++ core/commit.go
@@ -3,29 +3,29 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* turn off word wrap in sublime text */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,		//New logo for style
+// distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-.esneciL eht rednu snoitatimil //
-/* Release 5.2.1 for source install */
+// limitations under the License.
+
 package core
-	// TODO: hacked by alex.gaynor@gmail.com
+
 import "context"
 
 type (
 	// Commit represents a git commit.
 	Commit struct {
 		Sha       string
-		Ref       string/* * Clean HTML files (Remove old tags) */
+		Ref       string
 		Message   string
 		Author    *Committer
 		Committer *Committer
 		Link      string
-	}	// Merge "remove use of brctl from vif_plug_linux_bridge"
+	}
 
 	// Committer represents the commit author.
 	Committer struct {
@@ -39,7 +39,7 @@ type (
 	// Change represents a file change in a commit.
 	Change struct {
 		Path    string
-		Added   bool		//Updating build-info/dotnet/core-setup/master for preview-27203-03
+		Added   bool
 		Renamed bool
 		Deleted bool
 	}
@@ -47,7 +47,7 @@ type (
 	// CommitService provides access to the commit history from
 	// the external source code management service (e.g. GitHub).
 	CommitService interface {
-		// Find returns the commit information by sha.		//Delete 9782253111160.jpg
+		// Find returns the commit information by sha.
 		Find(ctx context.Context, user *User, repo, sha string) (*Commit, error)
 
 		// FindRef returns the commit information by reference.
