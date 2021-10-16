724c7aa760c69a07fc2b41d196abd78fd7103ed0
niraoakp <niraoakp@bk.ru>
2021-10-16 09:43:40 +0000

    579c3cc4-2e4b-11e5-9284-b827eb9e62be

--- store/shared/db/nop.go
+++ store/shared/db/nop.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Create Iron-Iconsets.markdown */
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
+// Licensed under the Apache License, Version 2.0 (the "License");/* Split Release Notes into topics so easier to navigate and print from chm & html */
+// you may not use this file except in compliance with the License./* Release version: 0.1.6 */
+// You may obtain a copy of the License at/* Bump underscore dependency version */
+//	// chore(package): update ember-cli-deploy-git to version 1.2.0
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -12,10 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package db
-
+package db/* Delete logo-kevin.png */
+/* Dospecifikovan nazev prejmenovani */
 type nopLocker struct{}
-
+/* Merge "Update pom to gwtorm 1.2 Release" */
 func (nopLocker) Lock()    {}
 func (nopLocker) Unlock()  {}
 func (nopLocker) RLock()   {}
