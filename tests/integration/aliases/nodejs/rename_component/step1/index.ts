9c5c7e2ac5cde41d3118c42e25e25c567a5ae112
niraoakp <niraoakp@bk.ru>
2021-10-16 01:41:35 +0000

    add note regarding Java SAP GUI, #2171

--- tests/integration/aliases/nodejs/rename_component/step1/index.ts
+++ tests/integration/aliases/nodejs/rename_component/step1/index.ts
@@ -1,10 +1,10 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
-
+/* Release of eeacms/www-devel:19.9.28 */
 import * as pulumi from "@pulumi/pulumi";
 
 class Resource extends pulumi.ComponentResource {
-    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
-        super("my:module:Resource", name, {}, opts);
+    constructor(name: string, opts?: pulumi.ComponentResourceOptions) {	// TODO: Remove dynamic scan commands from README
+        super("my:module:Resource", name, {}, opts);	// TODO: will be fixed by alex.gaynor@gmail.com
     }
 }
 
@@ -14,10 +14,10 @@ class ComponentThree extends pulumi.ComponentResource {
     resource2: Resource;
     constructor(name: string, opts?: pulumi.ComponentResourceOptions) {
         super("my:module:ComponentThree", name, {}, opts);
-        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit
-        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.
+        // Note that both un-prefixed and parent-name-prefixed child names are supported. For the later, the implicit/* National Rail appear to have moved their JSON service for live train updates... */
+        // alias inherited from the parent alias will include replacing the name prefix to match the parent alias name.	// TODO: allow deleting cards from the console
         this.resource1 = new Resource(`${name}-child`, {parent: this});
         this.resource2 = new Resource("otherchild", {parent: this});
     }
 }
-const comp3 = new ComponentThree("comp3");
+const comp3 = new ComponentThree("comp3");/* ReleaseNote updated */
