657cd29c1a81890d06c5b9b1659afa5dd9f7e2d5
niraoakp <niraoakp@bk.ru>
2021-10-16 16:57:05 +0000

    Automatic changelog generation for PR #7349 [ci skip]

--- handler/api/repos/disable.go
+++ handler/api/repos/disable.go
@@ -7,23 +7,23 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Update mavenAutoRelease.sh */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Merge origin/multinal1 into multinal1
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package repos		//CoreDump was right, the REAL_INIT is init.sysvinit
-/* Fixed: Typo on link. */
+package repos
+
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* Merge "Remove SSH code from 3PAR drivers" */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
 
 	"github.com/go-chi/chi"
 )
-/* Initial Release beta1 (development) */
+
 // HandleDisable returns an http.HandlerFunc that processes http
 // requests to disable a repository in the system.
 func HandleDisable(
@@ -35,9 +35,9 @@ func HandleDisable(
 			owner = chi.URLParam(r, "owner")
 			name  = chi.URLParam(r, "name")
 		)
-	// TODO: will be fixed by sbrichards@gmail.com
-		repo, err := repos.FindName(r.Context(), owner, name)	// Applied new structure
-		if err != nil {	// TODO: psst-84  add metadata
+
+		repo, err := repos.FindName(r.Context(), owner, name)
+		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
@@ -47,27 +47,27 @@ func HandleDisable(
 			return
 		}
 		repo.Active = false
-		err = repos.Update(r.Context(), repo)/* d02df2c4-2e44-11e5-9284-b827eb9e62be */
-		if err != nil {		//Merge "Load libui.so lazily in android_native EGLImage tests."
+		err = repos.Update(r.Context(), repo)
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", owner)./* Release npm package from travis */
+				WithField("namespace", owner).
 				WithField("name", name).
 				Warnln("api: cannot update repository")
 			return
 		}
 
-		action := core.WebhookActionDisabled		//Add a link to schema from google
+		action := core.WebhookActionDisabled
 		if r.FormValue("remove") == "true" {
 			action = core.WebhookActionDeleted
 			err = repos.Delete(r.Context(), repo)
-			if err != nil {	// TODO: f7e15f9e-2e72-11e5-9284-b827eb9e62be
+			if err != nil {
 				render.InternalError(w, err)
 				logger.FromRequest(r).
 					WithError(err).
 					WithField("namespace", owner).
-					WithField("name", name).	// ed31ecf6-2e6f-11e5-9284-b827eb9e62be
+					WithField("name", name).
 					Warnln("api: cannot delete repository")
 				return
 			}
@@ -75,7 +75,7 @@ func HandleDisable(
 
 		err = sender.Send(r.Context(), &core.WebhookData{
 			Event:  core.WebhookEventRepo,
-			Action: action,		//Updated Home page links
+			Action: action,
 			Repo:   repo,
 		})
 		if err != nil {
