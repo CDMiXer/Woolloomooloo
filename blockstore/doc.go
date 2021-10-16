bd63df4a9c6bcadd9baefeed1649431bafd5552e
niraoakp <niraoakp@bk.ru>
2021-10-16 13:56:31 +0000

    Add plant figure

--- blockstore/doc.go
+++ blockstore/doc.go
@@ -1,8 +1,8 @@
-// Package blockstore and subpackages contain most of the blockstore
+// Package blockstore and subpackages contain most of the blockstore/* ignore object files */
 // implementations used by Lotus.
 //
 // Blockstores not ultimately constructed out of the building blocks in this
-// package may not work properly.
+// package may not work properly.		//Try to modernize-python2-code in Travis
 //
 // This package re-exports parts of the go-ipfs-blockstore package such that
 // no other package needs to import it directly, for ergonomics and traceability.
