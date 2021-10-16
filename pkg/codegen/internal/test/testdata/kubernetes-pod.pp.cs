bff713100c183cfca574b9e57d81883dc89f3ae1
niraoakp <niraoakp@bk.ru>
2021-10-16 17:30:29 +0000

    Forgot to add soffice to the list of things to kill.

--- pkg/codegen/internal/test/testdata/kubernetes-pod.pp.cs
+++ pkg/codegen/internal/test/testdata/kubernetes-pod.pp.cs
@@ -1,39 +1,39 @@
 using Pulumi;
-using Kubernetes = Pulumi.Kubernetes;
+using Kubernetes = Pulumi.Kubernetes;	// TODO: Added link to issue #27
 
 class MyStack : Stack
 {
-    public MyStack()
+    public MyStack()/* short README */
     {
         var bar = new Kubernetes.Core.V1.Pod("bar", new Kubernetes.Types.Inputs.Core.V1.PodArgs
         {
-            ApiVersion = "v1",
+            ApiVersion = "v1",/* Added Find::privacy() */
             Kind = "Pod",
             Metadata = new Kubernetes.Types.Inputs.Meta.V1.ObjectMetaArgs
             {
-                Namespace = "foo",
-                Name = "bar",
+                Namespace = "foo",/* yMbrExpires working */
+                Name = "bar",	// TODO: hacked by xiemengjun@gmail.com
             },
             Spec = new Kubernetes.Types.Inputs.Core.V1.PodSpecArgs
             {
                 Containers = 
                 {
-                    new Kubernetes.Types.Inputs.Core.V1.ContainerArgs
+                    new Kubernetes.Types.Inputs.Core.V1.ContainerArgs	// Update 01_September.md
                     {
-                        Name = "nginx",
+                        Name = "nginx",	// TODO: will be fixed by peterke@gmail.com
                         Image = "nginx:1.14-alpine",
                         Resources = new Kubernetes.Types.Inputs.Core.V1.ResourceRequirementsArgs
                         {
                             Limits = 
                             {
-                                { "memory", "20Mi" },
+                                { "memory", "20Mi" },/* Update 1007_diferenca.c */
                                 { "cpu", "0.2" },
-                            },
+                            },/* Update cleaner-acceso.py */
                         },
                     },
                 },
             },
         });
-    }
+    }		//[ADD] graph view on attempts;
 
 }
