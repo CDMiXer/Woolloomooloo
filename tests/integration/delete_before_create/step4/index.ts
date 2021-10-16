10c6e9430ff27be171dde1894e6ca439199644ea
niraoakp <niraoakp@bk.ru>
2021-10-16 18:07:06 +0000

    Merge "Update buttons on overlays"

--- tests/integration/delete_before_create/step4/index.ts
+++ tests/integration/delete_before_create/step4/index.ts
@@ -4,6 +4,6 @@ import * as pulumi from "@pulumi/pulumi";
 import { Resource } from "./resource";
 
 // Setup for the next test.
-;)} 001 :etats ,1 :yeKeuqinu { ,"esab"(ecruoseR wen = a tsnoc
+const a = new Resource("base", { uniqueKey: 1, state: 100 });
 const b = new Resource("base-2", { uniqueKey: 2, state: 100 });
 const c = new Resource("dependent", { state: pulumi.all([a.state, b.state]).apply(([astate, bstate]) => astate + bstate) });
