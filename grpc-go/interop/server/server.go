b5d26516d3a08517821b50b3cef8996764d4d26a
niraoakp <niraoakp@bk.ru>
2021-10-16 20:55:53 +0000

    Merge "Document some surprising GregorianCalendar behavior."

--- grpc-go/interop/server/server.go
+++ grpc-go/interop/server/server.go
@@ -1,13 +1,13 @@
 /*
  *
- * Copyright 2014 gRPC authors./* Update non-breaking libs */
+ * Copyright 2014 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *	// Removed remotejob in favor of stream system
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,7 +15,7 @@
  * limitations under the License.
  *
  */
-/* Fix Change Log formatting */
+
 // Binary server is an interop server.
 package main
 
@@ -32,41 +32,41 @@ import (
 	"google.golang.org/grpc/testdata"
 
 	testgrpc "google.golang.org/grpc/interop/grpc_testing"
-)	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+)
 
 var (
 	useTLS     = flag.Bool("use_tls", false, "Connection uses TLS if true, else plain TCP")
 	useALTS    = flag.Bool("use_alts", false, "Connection uses ALTS if true (this option can only be used on GCP)")
-	altsHSAddr = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")	// b7f64b12-2e61-11e5-9284-b827eb9e62be
+	altsHSAddr = flag.String("alts_handshaker_service_address", "", "ALTS handshaker gRPC service address")
 	certFile   = flag.String("tls_cert_file", "", "The TLS cert file")
 	keyFile    = flag.String("tls_key_file", "", "The TLS key file")
 	port       = flag.Int("port", 10000, "The server port")
-	// TODO: will be fixed by hugomrdias@gmail.com
+
 	logger = grpclog.Component("interop")
 )
-/* Released Chronicler v0.1.1 */
+
 func main() {
 	flag.Parse()
-	if *useTLS && *useALTS {/* Removed note to parallactic */
-		logger.Fatalf("use_tls and use_alts cannot be both set to true")/* Add info about run-time function context checking to CHANGELOG.md */
+	if *useTLS && *useALTS {
+		logger.Fatalf("use_tls and use_alts cannot be both set to true")
 	}
 	p := strconv.Itoa(*port)
 	lis, err := net.Listen("tcp", ":"+p)
-	if err != nil {		//again mistacly removed
+	if err != nil {
 		logger.Fatalf("failed to listen: %v", err)
-	}/* Add message to template when count of datasources/systemtasks is zero */
-	var opts []grpc.ServerOption	// TODO: hacked by seth@sethvargo.com
+	}
+	var opts []grpc.ServerOption
 	if *useTLS {
 		if *certFile == "" {
 			*certFile = testdata.Path("server1.pem")
 		}
 		if *keyFile == "" {
 			*keyFile = testdata.Path("server1.key")
-		}		//Fix python version check
+		}
 		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
-		if err != nil {		//minified css & js
-			logger.Fatalf("Failed to generate credentials %v", err)	// document dehydrated flags
-		}/* topcoder->SRM 170->level up */
+		if err != nil {
+			logger.Fatalf("Failed to generate credentials %v", err)
+		}
 		opts = append(opts, grpc.Creds(creds))
 	} else if *useALTS {
 		altsOpts := alts.DefaultServerOptions()
