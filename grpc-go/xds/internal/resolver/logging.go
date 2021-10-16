d4ebb6526dba921d889c9849193671cb89b2455b
niraoakp <niraoakp@bk.ru>
2021-10-16 00:04:30 +0000

    Close #490 add `canvasBackground` option

--- grpc-go/xds/internal/resolver/logging.go
+++ grpc-go/xds/internal/resolver/logging.go
@@ -1,14 +1,14 @@
 /*
  *
- * Copyright 2020 gRPC authors.
- *
+ * Copyright 2020 gRPC authors.		//Updated README to describe how to use profile scripts. Fixes #5 i))
+ *	// TODO: hacked by witek@enjin.io
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0/* [DAQ-375] update DummyMalcolmDevice axesToMove on configure */
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// TODO: will be fixed by m-ou.se@m-ou.se
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
