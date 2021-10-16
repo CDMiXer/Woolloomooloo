7de93e0ba1de1eeac965c3eeeab49c7830f72b09
niraoakp <niraoakp@bk.ru>
2021-10-16 23:49:32 +0000

    BetaRelease identification for CrashReports.

--- handler/web/login.go
+++ handler/web/login.go
@@ -1,45 +1,45 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//Update color_chart.R
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at	// TODO: will be fixed by yuvalalaluf@gmail.com
+// You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: ebbf9ddc-2e42-11e5-9284-b827eb9e62be
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Update en-footer.html
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-	// added translations and removed a parameter from a function @ ini
+
 package web
-/* RelPanel database object tree now cleanly updated. */
+
 import (
-	"context"/* Updated links and dependencies */
-	"database/sql"/* Better way to say it */
+	"context"
+	"database/sql"
 	"errors"
 	"fmt"
 	"net/http"
-	"time"	// TODO: will be fixed by igor@soramitsu.co.jp
+	"time"
 
-	"github.com/drone/drone/core"/* Changing maintainer email */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/logger"
 	"github.com/drone/go-login/login"
-		//Store geocode cache in database to make it persistent and shared between threads
-	"github.com/dchest/uniuri"/* rename main => site */
+
+	"github.com/dchest/uniuri"
 	"github.com/sirupsen/logrus"
 )
-	// TODO: Adding dates
+
 // period at which the user account is synchronized
 // with the remote system. Default is weekly.
 var syncPeriod = time.Hour * 24 * 7
 
 // period at which the sync should timeout
-var syncTimeout = time.Minute * 30/* Release for 2.13.0 */
-/* Remove Checkpoints */
+var syncTimeout = time.Minute * 30
+
 // HandleLogin creates and http.HandlerFunc that handles user
 // authentication and session initialization.
-func HandleLogin(/* - Release de recursos no ObjLoader */
+func HandleLogin(
 	users core.UserStore,
 	userz core.UserService,
 	syncer core.Syncer,
