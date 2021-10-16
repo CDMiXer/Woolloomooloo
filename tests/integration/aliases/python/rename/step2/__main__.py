03b14246198e167c70a6fc8ac622c10a5ae45d52
niraoakp <niraoakp@bk.ru>
2021-10-16 18:30:29 +0000

    reduce h1 size and use in page title partial

--- tests/integration/aliases/python/rename/step2/__main__.py
+++ tests/integration/aliases/python/rename/step2/__main__.py
@@ -2,7 +2,7 @@
 
 from pulumi import Alias, ComponentResource, export, Resource, ResourceOptions, create_urn, ROOT_STACK_RESOURCE
 
-class Resource1(ComponentResource):
+class Resource1(ComponentResource):/* Bump Lazy from 0.3 to 0.4 */
     def __init__(self, name, opts=None):
         super().__init__("my:module:Resource", name, None, opts)
 
