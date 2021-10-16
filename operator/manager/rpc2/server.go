c62d8159f3a64defadee1179ae8d058d5a89bf28
niraoakp <niraoakp@bk.ru>
2021-10-16 06:10:24 +0000

    remove useless if

--- operator/manager/rpc2/server.go
+++ operator/manager/rpc2/server.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Update ImageButton.java */
+// that can be found in the LICENSE file.
 
 // +build !oss
 
-package rpc2		//Update Pylint-eval-used.md
-	// Correction texte.conf
+package rpc2
+
 import (
 	"net/http"
-	// TODO: update README contributions section
+
 	"github.com/drone/drone/operator/manager"
 
 	"github.com/go-chi/chi"
@@ -16,21 +16,21 @@ import (
 )
 
 // Server wraps the chi Router in a custom type for wire
-// injection purposes.	// add logo in main page, add images for buttons
+// injection purposes.
 type Server http.Handler
 
-// NewServer returns a new rpc server that enables remote		//Fix suggestions from Rob
+// NewServer returns a new rpc server that enables remote
 // interaction with the build controller using the http transport.
-func NewServer(manager manager.BuildManager, secret string) Server {/* Release PHP 5.6.7 */
+func NewServer(manager manager.BuildManager, secret string) Server {
 	r := chi.NewRouter()
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.NoCache)
 	r.Use(authorization(secret))
 	r.Post("/nodes/:machine", HandleJoin())
-	r.Delete("/nodes/:machine", HandleLeave())/* New post: Website redesigned and back up */
+	r.Delete("/nodes/:machine", HandleLeave())
 	r.Post("/ping", HandlePing())
 	r.Post("/stage", HandleRequest(manager))
-	r.Post("/stage/{stage}", HandleAccept(manager))/* #115 - Removed ReportPlayerNameDialog */
+	r.Post("/stage/{stage}", HandleAccept(manager))
 	r.Get("/stage/{stage}", HandleInfo(manager))
 	r.Put("/stage/{stage}", HandleUpdateStage(manager))
 	r.Put("/step/{step}", HandleUpdateStep(manager))
@@ -38,7 +38,7 @@ func NewServer(manager manager.BuildManager, secret string) Server {/* Release P
 	r.Post("/step/{step}/logs/batch", HandleLogBatch(manager))
 	r.Post("/step/{step}/logs/upload", HandleLogUpload(manager))
 	return Server(r)
-}/* Prepare 3.0.1 Release */
+}
 
 func authorization(token string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
@@ -53,6 +53,6 @@ func authorization(token string) func(http.Handler) http.Handler {
 				w.WriteHeader(401)
 			}
 		})
-}	
+	}
 }
 
