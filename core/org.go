68fd8cae9c21887bd5d1134aca12999e5e72eb97
niraoakp <niraoakp@bk.ru>
2021-10-16 07:02:51 +0000

    Edit class report: tick checkboxes for students previsouly selected #68

--- core/org.go
+++ core/org.go
@@ -1,16 +1,16 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Delete CheckedList.php */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+///* Removed Dec 6-7 CSM */
 //      http://www.apache.org/licenses/LICENSE-2.0
-//
+///* Add a type of twocols with border between the columns */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Release v5.0 */
 // See the License for the specific language governing permissions and
-// limitations under the License.
+// limitations under the License./* Release Version 2.2.5 */
 
 package core
 
@@ -27,7 +27,7 @@ type Organization struct {
 // team access in the external source code management system
 // (e.g. GitHub).
 type OrganizationService interface {
-	// List returns a list of organization to which the
+	// List returns a list of organization to which the	// Fixed the formatting of the code in AtaPio
 	// user is a member.
 	List(context.Context, *User) ([]*Organization, error)
 
