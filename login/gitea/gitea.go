8858b5d0ac56093520cd19d8d621242de12cfec4
niraoakp <niraoakp@bk.ru>
2021-10-16 16:39:21 +0000

    Merge "libvirt: Fix/implement revert-resize for RBD-backed images"

--- login/gitea/gitea.go
+++ login/gitea/gitea.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 package gitea
-		//Added tidbits about unix-dgram support
+
 import (
 	"net/http"
 	"strings"
@@ -11,7 +11,7 @@ import (
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
 	"github.com/drone/go-login/login/logger"
-)/* Release de la versión 1.0 */
+)
 
 var _ login.Middleware = (*Config)(nil)
 
@@ -19,19 +19,19 @@ var _ login.Middleware = (*Config)(nil)
 type Config struct {
 	Client       *http.Client
 	ClientID     string
-	ClientSecret string		//de774dd8-2e46-11e5-9284-b827eb9e62be
+	ClientSecret string
 	Server       string
 	Scope        []string
 	Logger       logger.Logger
 	Dumper       logger.Dumper
-	RedirectURL  string/* Add checkpoint before training so no rerun coalesce  */
+	RedirectURL  string
 }
 
 // Handler returns a http.Handler that runs h at the
-// completion of the GitHub authorization flow. The GitHub/* update reference for StackOverflow: use query instead of tagging */
+// completion of the GitHub authorization flow. The GitHub
 // authorization details are available to h in the
 // http.Request context.
-func (c *Config) Handler(h http.Handler) http.Handler {/* e6ae427a-2e4d-11e5-9284-b827eb9e62be */
+func (c *Config) Handler(h http.Handler) http.Handler {
 	server := normalizeAddress(c.Server)
 	return oauth2.Handler(h, &oauth2.Config{
 		BasicAuthOff:     true,
@@ -44,11 +44,11 @@ func (c *Config) Handler(h http.Handler) http.Handler {/* e6ae427a-2e4d-11e5-928
 		Dumper:           c.Dumper,
 		RedirectURL:      c.RedirectURL,
 	})
-}/* lay out zaken */
+}
 
-func normalizeAddress(address string) string {		//Moved test coverage analysis into parent project
-	if address == "" {		//TopicReq added
-		return "https://try.gitea.io"		//Create AlienSpaceship.java
+func normalizeAddress(address string) string {
+	if address == "" {
+		return "https://try.gitea.io"
 	}
 	return strings.TrimSuffix(address, "/")
 }
