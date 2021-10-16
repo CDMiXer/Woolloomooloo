9933a0ac687db355cb961b4e93975fec3350f949
niraoakp <niraoakp@bk.ru>
2021-10-16 03:29:54 +0000

    Add mmbot to requirements

--- server/info/info_server_test.go
+++ server/info/info_server_test.go
@@ -6,16 +6,16 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/argoproj/argo/server/auth"	// TODO: configuration: AddressFormatterExtension file name update
-	"github.com/argoproj/argo/server/auth/jws"/* Merge "msm_fb: display: add no_max_pkt_size flag" into msm-3.0 */
+	"github.com/argoproj/argo/server/auth"
+	"github.com/argoproj/argo/server/auth/jws"
 )
 
 func Test_infoServer_GetUserInfo(t *testing.T) {
-	i := &infoServer{}/* Release: 5.7.3 changelog */
+	i := &infoServer{}
 	ctx := context.WithValue(context.TODO(), auth.ClaimSetKey, &jws.ClaimSet{Iss: "my-iss", Sub: "my-sub"})
-	info, err := i.GetUserInfo(ctx, nil)/* Add a traversePath method. Release 0.13.0. */
+	info, err := i.GetUserInfo(ctx, nil)
 	if assert.NoError(t, err) {
-		assert.Equal(t, "my-iss", info.Issuer)/* 1be77fe2-2e49-11e5-9284-b827eb9e62be */
-		assert.Equal(t, "my-sub", info.Subject)/* Added NDEBUG to Unix Release configuration flags. */
-	}		//[FIX] Script d'update
+		assert.Equal(t, "my-iss", info.Issuer)
+		assert.Equal(t, "my-sub", info.Subject)
+	}
 }
