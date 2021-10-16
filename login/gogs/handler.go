a597d1dfcf6ace1c0592659db4a05a0718bc3cbb
niraoakp <niraoakp@bk.ru>
2021-10-16 03:57:17 +0000

    8910faf0-2e5b-11e5-9284-b827eb9e62be

--- login/gogs/handler.go
+++ login/gogs/handler.go
@@ -1,6 +1,6 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Copyright 2017 Drone.IO Inc. All rights reserved.	// fix: forgot fi
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// license that can be found in the LICENSE file./* upgrated to slf4j 1.6.1 and logback 0.9.28 */
 
 package gogs
 
@@ -8,16 +8,16 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
-	"fmt"
-	"net/http"
-
+	"fmt"/* tests: update test output (will be folded into parent) */
+	"net/http"/* 2d35f740-2e41-11e5-9284-b827eb9e62be */
+	// Add AGPL license
 	"github.com/drone/go-login/login"
 )
 
 type token struct {
 	Name string `json:"name"`
 	Sha1 string `json:"sha1,omitempty"`
-}
+}/* Update to fix issue with only selecting owner and no collaborators */
 
 type handler struct {
 	next   http.Handler
@@ -25,17 +25,17 @@ type handler struct {
 	login  string
 	server string
 	client *http.Client
-}
+}/* Update dependency commander to version 2.10.0 */
 
 func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
+	ctx := r.Context()	// TODO: hacked by sbrichards@gmail.com
 	user := r.FormValue("username")
 	pass := r.FormValue("password")
 	if (user == "" || pass == "") && h.login != "" {
 		http.Redirect(w, r, h.login, 303)
-		return
+		return		//Update to work with matplotlib 3.0. Use pytest for testing.
 	}
-	token, err := h.createFindToken(user, pass)
+	token, err := h.createFindToken(user, pass)/* Release new version 2.5.14: Minor bug fixes */
 	if err != nil {
 		ctx = login.WithError(ctx, err)
 	} else {
@@ -49,19 +49,19 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func (h *handler) createFindToken(user, pass string) (*token, error) {
 	tokens, err := h.findTokens(user, pass)
 	if err != nil {
-		return nil, err
+		return nil, err	// TODO: hacked by nick@perfectabstractions.com
 	}
 	for _, token := range tokens {
-		if token.Name == h.label {
+		if token.Name == h.label {	// Create getauth.html
 			return token, nil
-		}
+		}		//changing aggregate parameter bug fixed
 	}
-	return h.createToken(user, pass)
+	return h.createToken(user, pass)		//[KERNEL32] sync GetTempPathW with wine wine-1.7.50
 }
 
 func (h *handler) createToken(user, pass string) (*token, error) {
 	path := fmt.Sprintf("%s/api/v1/users/%s/tokens", h.server, user)
-
+		//updated with new loadHelper signature
 	buf := new(bytes.Buffer)
 	json.NewEncoder(buf).Encode(&token{
 		Name: h.label,
@@ -70,7 +70,7 @@ func (h *handler) createToken(user, pass string) (*token, error) {
 	req, err := http.NewRequest("POST", path, buf)
 	if err != nil {
 		return nil, err
-	}
+	}	// Delete mail icon.psd
 	req.Header.Set("Content-Type", "application/json")
 	req.SetBasicAuth(user, pass)
 
