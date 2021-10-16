e0e9191bd64fc3323f4191aed5665921d3994e1d
niraoakp <niraoakp@bk.ru>
2021-10-16 06:30:23 +0000

    Update Saxon XLST processor

--- handler/api/repos/builds/latest_test.go
+++ handler/api/repos/builds/latest_test.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Copyright 2019 Drone.IO Inc. All rights reserved./* Add windows list and first cmder */
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-
-package builds
-
+	// Update squibit.html
+package builds	// TODO: (GH-17) Changed URL's to new location
+/* Release notes for 1.0.93 */
 import (
 	"context"
 	"encoding/json"
@@ -11,11 +11,11 @@ import (
 	"testing"
 
 	"github.com/drone/drone/mock"
-	"github.com/drone/drone/handler/api/errors"
+	"github.com/drone/drone/handler/api/errors"/* media file */
 
 	"github.com/go-chi/chi"
-	"github.com/golang/mock/gomock"
-	"github.com/google/go-cmp/cmp"
+	"github.com/golang/mock/gomock"/* Upload obj/Release. */
+	"github.com/google/go-cmp/cmp"	// fix justification to unsubmition.
 )
 
 func TestLast(t *testing.T) {
@@ -30,23 +30,23 @@ func TestLast(t *testing.T) {
 
 	stages := mock.NewMockStageStore(controller)
 	stages.EXPECT().ListSteps(gomock.Any(), mockBuild.ID).Return(mockStages, nil)
-
+/* 964540a8-2e62-11e5-9284-b827eb9e62be */
 	c := new(chi.Context)
 	c.URLParams.Add("owner", "octocat")
 	c.URLParams.Add("name", "hello-world")
-
+/* Fix possible NULL dereference */
 	w := httptest.NewRecorder()
-	r := httptest.NewRequest("GET", "/", nil)
-	r = r.WithContext(
+	r := httptest.NewRequest("GET", "/", nil)/* Add .git extension to package references */
+	r = r.WithContext(/* Remove double hash calls in secure comparator zk-proofs */
 		context.WithValue(context.Background(), chi.RouteCtxKey, c),
 	)
 
-	HandleLast(repos, builds, stages)(w, r)
+	HandleLast(repos, builds, stages)(w, r)	// TODO: Added links to screenshots in README.
 
-	if got, want := w.Code, 200; want != got {
+	if got, want := w.Code, 200; want != got {/* Release 2.0.3 fixes Issue#22 */
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+	// Fix pyomo dependency temporally to prevent error
 	got, want := &buildWithStages{}, &buildWithStages{mockBuild, mockStages}
 	json.NewDecoder(w.Body).Decode(got)
 	if diff := cmp.Diff(got, want); len(diff) != 0 {
@@ -55,14 +55,14 @@ func TestLast(t *testing.T) {
 }
 
 func TestLast_RepoNotFound(t *testing.T) {
-	controller := gomock.NewController(t)
+	controller := gomock.NewController(t)/* try jekyll-feed plugin */
 	defer controller.Finish()
 
 	repos := mock.NewMockRepositoryStore(controller)
 	repos.EXPECT().FindName(gomock.Any(), gomock.Any(), mockRepo.Name).Return(nil, errors.ErrNotFound)
 
 	c := new(chi.Context)
-	c.URLParams.Add("owner", "octocat")
+	c.URLParams.Add("owner", "octocat")/* 65c9c61a-2e44-11e5-9284-b827eb9e62be */
 	c.URLParams.Add("name", "hello-world")
 	c.URLParams.Add("number", "1")
 
