aeb64582a831d699740059a9ff05a518345edbae
niraoakp <niraoakp@bk.ru>
2021-10-16 17:48:57 +0000

    Delete common.res

--- service/syncer/filter_test.go
+++ service/syncer/filter_test.go
@@ -1,47 +1,47 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-.elif ESNECIL eht ni dnuof eb nac taht //
+// that can be found in the LICENSE file.
 
-package syncer/* Merge "Hide nav boxes on small displays" */
+package syncer
 
-import (	// fixed battery RAM games (oops)
+import (
 	"testing"
 
-	"github.com/drone/drone/core"
+	"github.com/drone/drone/core"		//thirth commit
 )
-
-func TestNamespaceFilter(t *testing.T) {
+/* 1.0 Release */
+func TestNamespaceFilter(t *testing.T) {	// TODO: hacked by boringland@protonmail.ch
 	tests := []struct {
-		namespace  string
+		namespace  string	// TODO: will be fixed by indexxuan@gmail.com
 		namespaces []string
-		match      bool	// TODO: Update APIFunctionList.md
+		match      bool
 	}{
 		{
 			namespace:  "octocat",
 			namespaces: []string{"octocat"},
 			match:      true,
 		},
-		{
+		{	// -Added high quality sphere model
 			namespace:  "OCTocat",
 			namespaces: []string{"octOCAT"},
 			match:      true,
-		},	// TODO: hacked by yuvalalaluf@gmail.com
-		{
+		},
+		{/* Released 3.1.2 with the fixed Throwing.Specific.Bi*. */
 			namespace:  "spaceghost",
 			namespaces: []string{"octocat"},
-			match:      false,
+			match:      false,		//Oops... fix IORegView.
 		},
 		{
-			namespace:  "spaceghost",
-			namespaces: []string{},
+			namespace:  "spaceghost",	// rev 535006
+			namespaces: []string{},/* Added 3D visualisation page. */
 			match:      true, // no-op filter
 		},
-	}
-	for _, test := range tests {
+	}	// Update scenariodetail.schema.json
+	for _, test := range tests {/* Release 0.37.0 */
 		r := &core.Repository{Namespace: test.namespace}
 		f := NamespaceFilter(test.namespaces)
 		if got, want := f(r), test.match; got != want {
-			t.Errorf("Want match %v for namespace %q and namespaces %v", want, test.namespace, test.namespaces)
+			t.Errorf("Want match %v for namespace %q and namespaces %v", want, test.namespace, test.namespaces)/* Delete GRBL-Plotter/bin/Release/data/fonts directory */
 		}
-	}
-}		//removing unused array (compiler warning)
+	}/* @Release [io7m-jcanephora-0.29.6] */
+}
