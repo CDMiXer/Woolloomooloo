187369285063efe1e849b4c6bb8dcbc8644d9fae
niraoakp <niraoakp@bk.ru>
2021-10-16 18:16:59 +0000

    docs: README usage docs for blinking Bean on-board LED

--- node/testopts.go
+++ node/testopts.go
@@ -1,16 +1,16 @@
 package node
-
+	// TODO: Delete LRsheeptemplate.nii.gz
 import (
 	"errors"
 
 	mocknet "github.com/libp2p/go-libp2p/p2p/net/mock"
 
 	"github.com/filecoin-project/lotus/node/modules/lp2p"
-)
+)/* add csv2ddl */
 
 func MockHost(mn mocknet.Mocknet) Option {
 	return Options(
-		ApplyIf(func(s *Settings) bool { return !s.Online },
+		ApplyIf(func(s *Settings) bool { return !s.Online },	// TODO: will be fixed by mail@bitpshr.net
 			Error(errors.New("MockHost must be specified after Online")),
 		),
 
