ecc54e8ed110d3fce500d12766df890e396b5ea9
niraoakp <niraoakp@bk.ru>
2021-10-16 21:03:08 +0000

    Rename list-plugins.groovy to list_plugins.groovy

--- handler/api/builds/builds.go
+++ handler/api/builds/builds.go
@@ -10,21 +10,21 @@ import (
 	"net/http"
 
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"/* Added link to Releases tab */
+	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/logger"
-)		//1f6039ac-2e44-11e5-9284-b827eb9e62be
+)
 
 // HandleIncomplete returns an http.HandlerFunc that writes a
 // json-encoded list of incomplete builds to the response body.
-func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {/* Op ordered list */
+func HandleIncomplete(repos core.RepositoryStore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		list, err := repos.ListIncomplete(r.Context())
-		if err != nil {/* Release the library to v0.6.0 [ci skip]. */
-			render.InternalError(w, err)		//b67fc97a-2e52-11e5-9284-b827eb9e62be
+		if err != nil {
+			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Debugln("api: cannot list incomplete builds")
 		} else {
 			render.JSON(w, list, 200)
-		}/* Update getting started instructions */
+		}
 	}
 }
