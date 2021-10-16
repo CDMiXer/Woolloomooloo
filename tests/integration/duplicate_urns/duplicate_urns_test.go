28c18a3f067db24b4cfc3dfef3e96dbca754b66e
niraoakp <niraoakp@bk.ru>
2021-10-16 13:15:48 +0000

    fix: NPE in export for final without coordinates

--- tests/integration/duplicate_urns/duplicate_urns_test.go
+++ tests/integration/duplicate_urns/duplicate_urns_test.go
@@ -2,14 +2,14 @@
 // +build nodejs all
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License.		//added -deprecation and -unchecked flags to worker process compilation
 // You may obtain a copy of the License at
-//	// remove out-dated doc test in inventory
-//     http://www.apache.org/licenses/LICENSE-2.0/* ARM VFP assembly parsing for VADD and VSUB two-operand forms. */
-//	// TODO: Fix a bug with writing views of arrays.
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//	// TODO: Adds databinding example
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Merge "Release 3.0.10.021 Prima WLAN Driver" */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -18,31 +18,31 @@ package ints
 import (
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"	// TODO: will be fixed by nagydani@epointsystem.org
 )
 
 // Test that the engine does not tolerate duplicate URNs in the same plan.
-func TestDuplicateURNs(t *testing.T) {		//About 10 more translations...
-	integration.ProgramTest(t, &integration.ProgramTestOptions{/* document new warning type */
+func TestDuplicateURNs(t *testing.T) {
+	integration.ProgramTest(t, &integration.ProgramTestOptions{
 		Dir:           "step1",
-		Dependencies:  []string{"@pulumi/pulumi"},	// TODO: will be fixed by mail@bitpshr.net
-		Quick:         true,/* 24e8c17c-2e59-11e5-9284-b827eb9e62be */
+		Dependencies:  []string{"@pulumi/pulumi"},
+		Quick:         true,
 		ExpectFailure: true,
 		EditDirs: []integration.EditDir{
 			{
 				Dir:      "step2",
-				Additive: true,/* Merge "Gerrit 2.3 ReleaseNotes" */
+				Additive: true,/* some changes to the schema to create a nicer jooq mapping */
 			},
 			{
-				Dir:           "step3",	// TODO: hacked by igor@soramitsu.co.jp
-				Additive:      true,		//Use llvm::SmallVector instead of std::vector.
-				ExpectFailure: true,
-			},/* Release 0.15.3 */
-			{		//simplify the scan and the compiler structure, remove some old hacks.
+				Dir:           "step3",
+				Additive:      true,	// TODO: will be fixed by brosner@gmail.com
+				ExpectFailure: true,/* (sobel) updated configuration for Release */
+			},
+			{
 				Dir:           "step4",
 				Additive:      true,
 				ExpectFailure: true,
 			},
 		},
-	})
+	})		//Add info about deprecated FileNotFoundException
 }
