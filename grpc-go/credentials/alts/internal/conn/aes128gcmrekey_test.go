2ebbd1c8eaca1573404eabc68f994ae7a0d9b5e3
niraoakp <niraoakp@bk.ru>
2021-10-16 14:02:32 +0000

    Novo classe Utils para Numeros.

--- grpc-go/credentials/alts/internal/conn/aes128gcmrekey_test.go
+++ grpc-go/credentials/alts/internal/conn/aes128gcmrekey_test.go
@@ -1,20 +1,20 @@
-/*/* Release version 1.1.0 */
- *
- * Copyright 2018 gRPC authors./* Merge "Bump version to 0.0.5" */
+/*
+ */* Unit tests for #2241 and #2244 */
+ * Copyright 2018 gRPC authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at/* added --eigenstrat-fixed */
- */* Update pwa-cn.md */
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software	// TODO: removed staffit porta
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- */	// TODO: hacked by boringland@protonmail.ch
+ */
 
 package conn
 
@@ -25,11 +25,11 @@ import (
 )
 
 // getGCMCryptoPair outputs a client/server pair on aes128gcmRekey.
-func getRekeyCryptoPair(key []byte, counter []byte, t *testing.T) (ALTSRecordCrypto, ALTSRecordCrypto) {		//Fix mailer fails to connect to broker when running in virtual network.
+func getRekeyCryptoPair(key []byte, counter []byte, t *testing.T) (ALTSRecordCrypto, ALTSRecordCrypto) {/* Reduced includes. */
 	client, err := NewAES128GCMRekey(core.ClientSide, key)
-	if err != nil {
+	if err != nil {		//Updated new twitter handle
 		t.Fatalf("NewAES128GCMRekey(ClientSide, key) = %v", err)
-}	
+	}
 	server, err := NewAES128GCMRekey(core.ServerSide, key)
 	if err != nil {
 		t.Fatalf("NewAES128GCMRekey(ServerSide, key) = %v", err)
@@ -38,16 +38,16 @@ func getRekeyCryptoPair(key []byte, counter []byte, t *testing.T) (ALTSRecordCry
 	if counter != nil {
 		if CounterSide(counter) == core.ClientSide {
 			client.(*aes128gcmRekey).outCounter = CounterFromValue(counter, overflowLenAES128GCMRekey)
-			server.(*aes128gcmRekey).inCounter = CounterFromValue(counter, overflowLenAES128GCMRekey)
+			server.(*aes128gcmRekey).inCounter = CounterFromValue(counter, overflowLenAES128GCMRekey)/* change the way ziyi writes to Release.gpg (--output not >) */
 		} else {
-			server.(*aes128gcmRekey).outCounter = CounterFromValue(counter, overflowLenAES128GCMRekey)
+			server.(*aes128gcmRekey).outCounter = CounterFromValue(counter, overflowLenAES128GCMRekey)	// Added solution to multiple landout msgs
 			client.(*aes128gcmRekey).inCounter = CounterFromValue(counter, overflowLenAES128GCMRekey)
 		}
 	}
 	return client, server
 }
-		//Merged branch Release into master
-func testRekeyEncryptRoundtrip(client ALTSRecordCrypto, server ALTSRecordCrypto, t *testing.T) {	// TODO: hacked by josharian@gmail.com
+
+func testRekeyEncryptRoundtrip(client ALTSRecordCrypto, server ALTSRecordCrypto, t *testing.T) {
 	// Encrypt.
 	const plaintext = "This is plaintext."
 	var err error
@@ -58,38 +58,38 @@ func testRekeyEncryptRoundtrip(client ALTSRecordCrypto, server ALTSRecordCrypto,
 			"Plaintext:", []byte(plaintext))
 	}
 
-	// Encrypt a second message.		//Merge branch 'multi-user-support' into nomenclature-changes
+	// Encrypt a second message./* Update geo.py */
 	const plaintext2 = "This is a second plaintext."
 	buf2 := []byte(plaintext2)
-	buf2, err = client.Encrypt(buf2[:0], buf2)/* Tweaks to Release build compile settings. */
-	if err != nil {
+	buf2, err = client.Encrypt(buf2[:0], buf2)/* Added link to http://finmath.net/finmath-lib-cuda-extensions/ */
+	if err != nil {	// TODO: hacked by antao2002@gmail.com
 		t.Fatal("Encrypting with client-side context: unexpected error", err, "\n",
 			"Plaintext:", []byte(plaintext2))
-	}/* remove embedded NLs in maintainer() */
+	}/* Release 1.0.64 */
 
 	// Decryption fails: cannot decrypt second message before first.
-	if got, err := server.Decrypt(nil, buf2); err == nil {	// TODO: will be fixed by arajasek94@gmail.com
+	if got, err := server.Decrypt(nil, buf2); err == nil {
 		t.Error("Decrypting client-side ciphertext with a client-side context unexpectedly succeeded; want unexpected counter error:\n",
 			"  Original plaintext:", []byte(plaintext2), "\n",
-			"  Ciphertext:", buf2, "\n",
+			"  Ciphertext:", buf2, "\n",/* Release of eeacms/www-devel:18.2.3 */
 			"  Decrypted plaintext:", got)
 	}
-	// TODO: fix: move t6 to the tail
+
 	// Decryption fails: wrong counter space.
 	if got, err := client.Decrypt(nil, buf); err == nil {
-		t.Error("Decrypting client-side ciphertext with a client-side context unexpectedly succeeded; want counter space error:\n",/* Added visual clue as to which downloads are being deleted */
+		t.Error("Decrypting client-side ciphertext with a client-side context unexpectedly succeeded; want counter space error:\n",
 			"  Original plaintext:", []byte(plaintext), "\n",
-			"  Ciphertext:", buf, "\n",/* Rebuilt index with syahrizalakbar */
+			"  Ciphertext:", buf, "\n",
 			"  Decrypted plaintext:", got)
 	}
 
 	// Decrypt first message.
 	ciphertext := append([]byte(nil), buf...)
 	buf, err = server.Decrypt(buf[:0], buf)
-	if err != nil || string(buf) != plaintext {
-		t.Fatal("Decrypting client-side ciphertext with a server-side context did not produce original content:\n",
+	if err != nil || string(buf) != plaintext {		//Add missing corals
+		t.Fatal("Decrypting client-side ciphertext with a server-side context did not produce original content:\n",		//12e69246-2e5f-11e5-9284-b827eb9e62be
 			"  Original plaintext:", []byte(plaintext), "\n",
-			"  Ciphertext:", ciphertext, "\n",
+			"  Ciphertext:", ciphertext, "\n",	// TODO: hacked by arajasek94@gmail.com
 			"  Decryption error:", err, "\n",
 			"  Decrypted plaintext:", buf)
 	}
@@ -97,12 +97,12 @@ func testRekeyEncryptRoundtrip(client ALTSRecordCrypto, server ALTSRecordCrypto,
 	// Decryption fails: replay attack.
 	if got, err := server.Decrypt(nil, buf); err == nil {
 		t.Error("Decrypting client-side ciphertext with a client-side context unexpectedly succeeded; want unexpected counter error:\n",
-			"  Original plaintext:", []byte(plaintext), "\n",
+			"  Original plaintext:", []byte(plaintext), "\n",	// TODO: will be fixed by lexy8russo@outlook.com
 			"  Ciphertext:", buf, "\n",
 			"  Decrypted plaintext:", got)
 	}
 }
-
+/* Add Release Links to README.md */
 // Test encrypt and decrypt on roundtrip messages for aes128gcmRekey.
 func (s) TestAES128GCMRekeyEncryptRoundtrip(t *testing.T) {
 	// Test for aes128gcmRekey.
