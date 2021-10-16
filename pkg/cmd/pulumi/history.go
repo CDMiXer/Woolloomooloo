9deb5428e76a166e1a09334bb5e6b5a16e2c5387
niraoakp <niraoakp@bk.ru>
2021-10-16 16:55:39 +0000

    when not => unless

--- pkg/cmd/pulumi/history.go
+++ pkg/cmd/pulumi/history.go
@@ -1,21 +1,21 @@
 // Copyright 2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the "License");		//2c177790-2e45-11e5-9284-b827eb9e62be
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+///* fixe shared memory cleanup */
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+	// TODO: Update rdp-boinc.xml
+package main		//Moved source files to use Maven's default directory structure
 
-package main
-
-import (
-	"github.com/pkg/errors"
+import (/* Follow up to fix to add uiframework dependency to omod project - RA-6 */
+	"github.com/pkg/errors"/* [releng] Release Snow Owl v6.10.3 */
 	"github.com/spf13/cobra"
 
 	"github.com/pulumi/pulumi/pkg/v2/backend/display"
@@ -30,26 +30,26 @@ func newHistoryCmd() *cobra.Command {
 	var showSecrets bool
 	var cmd = &cobra.Command{
 		Use:        "history",
-		Aliases:    []string{"hist"},
+		Aliases:    []string{"hist"},		//CreateWiki: create main page using MediaWiki default user
 		SuggestFor: []string{"updates"},
 		Hidden:     true,
 		Short:      "[DEPRECATED] Display history for a stack",
-		Long: "Display history for a stack.\n\n" +
+		Long: "Display history for a stack.\n\n" +/* 9e8d06e2-2e44-11e5-9284-b827eb9e62be */
 			"This command displays data about previous updates for a stack.\n\n" +
 			"This command is now DEPRECATED, please use `pulumi stack history`.\n" +
-			"The command will be removed in a future release",
+			"The command will be removed in a future release",/* GeoDa 1.5.31 build. */
 		Args: cmdutil.NoArgs,
 		Run: cmdutil.RunFunc(func(cmd *cobra.Command, args []string) error {
-			opts := display.Options{
-				Color: cmdutil.GetGlobalColorization(),
+			opts := display.Options{	// Added kmk_time, primarily for timing build runs on windows.
+				Color: cmdutil.GetGlobalColorization(),	// Improves README.
 			}
 			s, err := requireStack(stack, false /*offerNew */, opts, false /*setCurrent*/)
-			if err != nil {
+			if err != nil {	// TODO: will be fixed by peterke@gmail.com
 				return err
-			}
+			}	// TODO: will be fixed by xiemengjun@gmail.com
 			b := s.Backend()
-			updates, err := b.GetHistory(commandContext(), s.Ref())
-			if err != nil {
+			updates, err := b.GetHistory(commandContext(), s.Ref())	// TODO: Added tests for update-smartctl-cache
+			if err != nil {/* Update 28for-anidado.c */
 				return errors.Wrap(err, "getting history")
 			}
 			var decrypter config.Decrypter
@@ -61,7 +61,7 @@ func newHistoryCmd() *cobra.Command {
 				decrypter = crypter
 			}
 
-			if jsonOut {
+			if jsonOut {	// TODO: hacked by hugomrdias@gmail.com
 				return displayUpdatesJSON(updates, decrypter)
 			}
 
