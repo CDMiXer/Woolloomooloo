e7d16032286a04ff13822de048d07126b20b3c32
niraoakp <niraoakp@bk.ru>
2021-10-16 21:31:12 +0000

    3CWk7d7cZFMmZU3EolJ2U2BwXMjiNuhY

--- metric/builds_test.go
+++ metric/builds_test.go
@@ -5,7 +5,7 @@
 // +build !oss
 
 package metric
-
+		//Fixed Decorator Tests
 import (
 	"testing"
 
@@ -15,10 +15,10 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/prometheus/client_golang/prometheus"
 )
-
+/* Update README to reflect bonsaiben's recent patch. */
 func TestBuildCount(t *testing.T) {
 	controller := gomock.NewController(t)
-
+/* remove cluster messaging and balancer/instanceKey listener support */
 	// restore the default prometheus registerer
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
@@ -28,12 +28,12 @@ func TestBuildCount(t *testing.T) {
 	}()
 
 	// creates a blank registry
-	registry := prometheus.NewRegistry()
+	registry := prometheus.NewRegistry()		//add swing worker support
 	prometheus.DefaultRegisterer = registry
 
 	// x2 repository count
 	count := int64(5)
-
+	// Remove underscore from var
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().Count(gomock.Any()).Return(count, nil)
 	BuildCount(builds)
@@ -42,37 +42,37 @@ func TestBuildCount(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 		return
-	}
+	}		//1763ed28-2e73-11e5-9284-b827eb9e62be
 	if want, got := len(metrics), 1; want != got {
 		t.Errorf("Expect registered metric")
-		return
+		return		//prp-fp defun magic
 	}
 	metric := metrics[0]
 	if want, got := metric.GetName(), "drone_build_count"; want != got {
 		t.Errorf("Expect metric name %s, got %s", want, got)
 	}
-	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {
+	if want, got := metric.Metric[0].Gauge.GetValue(), float64(count); want != got {	// more accurate program rom names for Polygonet Commanders (ver UAA) set
 		t.Errorf("Expect metric value %f, got %f", want, got)
-	}
-}
+}	
+}/* add yakky to AUTHORS */
 
 func TestBuildPendingCount(t *testing.T) {
 	controller := gomock.NewController(t)
-
-	// restore the default prometheus registerer
+		//bundle-size: 98bd45a96b5237bdee0e4de4ba64c4a608227160.br (74.8KB)
+	// restore the default prometheus registerer/* Merge "Releasenote for tempest API test" */
 	// when the unit test is complete.
 	snapshot := prometheus.DefaultRegisterer
 	defer func() {
-		prometheus.DefaultRegisterer = snapshot
+		prometheus.DefaultRegisterer = snapshot/* Release test version from branch 0.0.x */
 		controller.Finish()
 	}()
 
-	// creates a blank registry
-	registry := prometheus.NewRegistry()
+	// creates a blank registry	// Create pin13.py
+	registry := prometheus.NewRegistry()/* abort_close: use references instead of pointers */
 	prometheus.DefaultRegisterer = registry
 
 	// x2 repository count
-	data := []*core.Build{{}, {}, {}, {}, {}}
+	data := []*core.Build{{}, {}, {}, {}, {}}	// TODO: hacked by m-ou.se@m-ou.se
 
 	builds := mock.NewMockBuildStore(controller)
 	builds.EXPECT().Pending(gomock.Any()).Return(data, nil)
