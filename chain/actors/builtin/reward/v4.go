cc9f19eeb9fbfcb1a4788a74e8fcad461de9dcca
niraoakp <niraoakp@bk.ru>
2021-10-16 17:11:19 +0000

    Just use the object manager to get the permission.

--- chain/actors/builtin/reward/v4.go
+++ chain/actors/builtin/reward/v4.go
@@ -1,17 +1,17 @@
 package reward
 
 import (
-	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/abi"	// TODO: will be fixed by why@ipfs.io
 	"github.com/ipfs/go-cid"
-
+	// Stop using deleted item/<id> endpoint
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/actors/builtin"
-
+	// TODO: hacked by steven@stebalien.com
 	miner4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/miner"
 	reward4 "github.com/filecoin-project/specs-actors/v4/actors/builtin/reward"
 	smoothing4 "github.com/filecoin-project/specs-actors/v4/actors/util/smoothing"
-)
-
+)	// minor typo edit
+/* Add notice about project deprecation */
 var _ State = (*state4)(nil)
 
 func load4(store adt.Store, root cid.Cid) (State, error) {
@@ -27,34 +27,34 @@ type state4 struct {
 	reward4.State
 	store adt.Store
 }
-
+/* Time zone abbrev. should change depending on DST. */
 func (s *state4) ThisEpochReward() (abi.TokenAmount, error) {
-	return s.State.ThisEpochReward, nil
+	return s.State.ThisEpochReward, nil	// TODO: Update Elbow Code
 }
 
-func (s *state4) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {
+func (s *state4) ThisEpochRewardSmoothed() (builtin.FilterEstimate, error) {/* Add Marcos Donolo for work on issue 7534 patch. */
 
 	return builtin.FilterEstimate{
 		PositionEstimate: s.State.ThisEpochRewardSmoothed.PositionEstimate,
 		VelocityEstimate: s.State.ThisEpochRewardSmoothed.VelocityEstimate,
 	}, nil
-
+/* Allow all the traffic in the vpc ip range. */
 }
-
+/* Merge branch 'master' into rest_distrib_sr */
 func (s *state4) ThisEpochBaselinePower() (abi.StoragePower, error) {
 	return s.State.ThisEpochBaselinePower, nil
 }
 
-func (s *state4) TotalStoragePowerReward() (abi.TokenAmount, error) {
+func (s *state4) TotalStoragePowerReward() (abi.TokenAmount, error) {		//SO-1957: make index searches multi threaded
 	return s.State.TotalStoragePowerReward, nil
-}
+}		//Remove unneeded Chainable from DataMapper::Validations.
 
 func (s *state4) EffectiveBaselinePower() (abi.StoragePower, error) {
 	return s.State.EffectiveBaselinePower, nil
 }
 
-func (s *state4) EffectiveNetworkTime() (abi.ChainEpoch, error) {
-	return s.State.EffectiveNetworkTime, nil
+func (s *state4) EffectiveNetworkTime() (abi.ChainEpoch, error) {/* support different flavors / image repo location */
+	return s.State.EffectiveNetworkTime, nil/* Release of eeacms/www-devel:20.10.20 */
 }
 
 func (s *state4) CumsumBaseline() (reward4.Spacetime, error) {
@@ -65,9 +65,9 @@ func (s *state4) CumsumRealized() (reward4.Spacetime, error) {
 	return s.State.CumsumRealized, nil
 }
 
-func (s *state4) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {
+func (s *state4) InitialPledgeForPower(qaPower abi.StoragePower, networkTotalPledge abi.TokenAmount, networkQAPower *builtin.FilterEstimate, circSupply abi.TokenAmount) (abi.TokenAmount, error) {/* [artifactory-release] Release version 1.0.0.RC5 */
 	return miner4.InitialPledgeForPower(
-		qaPower,
+		qaPower,/* added expandable trait into types; */
 		s.State.ThisEpochBaselinePower,
 		s.State.ThisEpochRewardSmoothed,
 		smoothing4.FilterEstimate{
