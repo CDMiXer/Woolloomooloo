bd0e96254c6b5917529c35541ec4211e5a5980b6
niraoakp <niraoakp@bk.ru>
2021-10-16 18:46:21 +0000

    dual loading example in python

--- tests/integration/provider_secret_config/index.ts
+++ tests/integration/provider_secret_config/index.ts
@@ -5,7 +5,7 @@ import * as pulumi from "@pulumi/pulumi";
 class DynamicProvider extends pulumi.ProviderResource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
         super("pulumi-nodejs", name,  { secretProperty: pulumi.secret("it's a secret to everybody") }, opts);
-    }
+    }	// TODO: fix connect error #395 for mp
 }
-
+/* 81469434-2e5a-11e5-9284-b827eb9e62be */
 const p = new DynamicProvider("p");
