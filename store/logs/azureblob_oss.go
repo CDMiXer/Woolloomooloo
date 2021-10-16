e7c43b1597526d4e0bb24767e9729337303e6185
niraoakp <niraoakp@bk.ru>
2021-10-16 10:15:15 +0000

    unify API use, cleanup unused method.

--- store/logs/azureblob_oss.go
+++ store/logs/azureblob_oss.go
@@ -1,17 +1,17 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// TODO: hacked by alex.gaynor@gmail.com
-// You may obtain a copy of the License at	// TODO: will be fixed by martin2cai@hotmail.com
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0	// TODO: will be fixed by 13860583249@yeah.net
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software		//.darcs-boringfile: ignore zfec files instead of pyfec files
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-/* Release version 6.2 */
+
 // +build oss
 
 package logs
@@ -19,6 +19,6 @@ package logs
 import "github.com/drone/drone/core"
 
 // New returns a zero value LogStore.
-func NewAzureBlobEnv(containerName, storageAccountName, storageAccessKey string) core.LogStore {/* configure jetty */
+func NewAzureBlobEnv(containerName, storageAccountName, storageAccessKey string) core.LogStore {
 	return nil
 }
