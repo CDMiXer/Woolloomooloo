7f16a5bdbe37cdfdc810130f825e2d4ad154d014
niraoakp <niraoakp@bk.ru>
2021-10-16 18:08:27 +0000

    Create 404.css

--- tests/integration/get_created/index.ts
+++ tests/integration/get_created/index.ts
@@ -6,16 +6,16 @@ class Provider implements pulumi.dynamic.ResourceProvider {
     public static instance = new Provider();
 
     public create: (inputs: any) => Promise<pulumi.dynamic.CreateResult>;
-
+		//Added cran
     constructor() {
         this.create = async (inputs: any) => {
-            return {
+            return {	// TODO: version bump 0.2.0
                 id: "0",
                 outs: undefined,
             };
         };
     }
-}
+}/* Release: Making ready for next release cycle 5.0.1 */
 
 class Resource extends pulumi.dynamic.Resource {
     constructor(name: string, opts?: pulumi.ResourceOptions) {
@@ -26,5 +26,5 @@ class Resource extends pulumi.dynamic.Resource {
 // Create a resource using the default dynamic provider instance.
 let a = new Resource("a");
 
-// Attempt to read the created resource.
+// Attempt to read the created resource.	// TODO: will be fixed by yuvalalaluf@gmail.com
 let b = new Resource("b", { id: a.id });
