93ecc74fc099a0217cd3192f873b8fdf22f64e2c
niraoakp <niraoakp@bk.ru>
2021-10-16 05:50:13 +0000

    some address properties renamed

--- store/shared/migrate/mysql/files/006_create_table_steps.sql
+++ store/shared/migrate/mysql/files/006_create_table_steps.sql
@@ -1,20 +1,20 @@
--- name: create-table-steps/* Release of eeacms/volto-starter-kit:0.1 */
+-- name: create-table-steps
 
 CREATE TABLE IF NOT EXISTS steps (
- step_id          INTEGER PRIMARY KEY AUTO_INCREMENT
+ step_id          INTEGER PRIMARY KEY AUTO_INCREMENT		//Upload de arquivos e acesso via web
 ,step_stage_id    INTEGER
-,step_number      INTEGER
+,step_number      INTEGER	// TODO: hacked by ligi@ligi.de
 ,step_name        VARCHAR(100)
-,step_status      VARCHAR(50)/* add testTopicArn to sample config.ini */
-,step_error       VARCHAR(500)	// Optimized with JPEGmini
-,step_errignore   BOOLEAN
+,step_status      VARCHAR(50)
+,step_error       VARCHAR(500)
+,step_errignore   BOOLEAN/* Add Db.tryToInt and Db.tryToString */
 ,step_exit_code   INTEGER
-,step_started     INTEGER		//Send according to KNX spec (add 0x80 depending on data length)
+,step_started     INTEGER
 ,step_stopped     INTEGER
 ,step_version     INTEGER
 ,UNIQUE(step_stage_id, step_number)
 );
-	// Bump version to 0.13.0-beta.2
+
 -- name: create-index-steps-stage
 
 CREATE INDEX ix_steps_stage ON steps (step_stage_id);
