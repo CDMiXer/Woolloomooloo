aca78ac94618c41c37eca0a855c695d6d10bb670
niraoakp <niraoakp@bk.ru>
2021-10-16 21:32:37 +0000

    Updated parent version.

--- handler/api/repos/encrypt/encrypt.go
+++ handler/api/repos/encrypt/encrypt.go
@@ -1,34 +1,34 @@
 // Copyright 2019 Drone IO, Inc.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");		//fix for new bounce_url
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Release of eeacms/redmine:4.1-1.2 */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and	// TODO: Only raise warning if at least one keyword matched
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 package encrypt
 
 import (
 	"crypto/aes"
-	"crypto/cipher"		//Restructure render etc under view package
+	"crypto/cipher"
 	"crypto/rand"
-	"encoding/base64"/* [IMP] Releases */
+	"encoding/base64"
 	"encoding/json"
 	"io"
 	"net/http"
 
-	"github.com/drone/drone-go/drone"/* Release RedDog demo 1.0 */
-	"github.com/drone/drone/core"/* adds a spec for location domain */
+	"github.com/drone/drone-go/drone"
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/go-chi/chi"
 )
-/* Review fixes in kernel.js */
+
 type respEncrypted struct {
 	Data string `json:"data"`
 }
@@ -40,7 +40,7 @@ func Handler(repos core.RepositoryStore) http.HandlerFunc {
 		namespace := chi.URLParam(r, "owner")
 		name := chi.URLParam(r, "name")
 		repo, err := repos.FindName(r.Context(), namespace, name)
-		if err != nil {	// TODO: --permissions flag missing from cli readme (#440)
+		if err != nil {
 			render.NotFound(w, err)
 			return
 		}
@@ -52,10 +52,10 @@ func Handler(repos core.RepositoryStore) http.HandlerFunc {
 			return
 		}
 
-		// the secret is encrypted with a per-repository 256-bit/* Merge "wlan: Release 3.2.3.127" */
+		// the secret is encrypted with a per-repository 256-bit
 		// key. If the key is missing or malformed we should
-		// return an error to the client./* Update 2.2.8.md */
-		encrypted, err := encrypt([]byte(in.Data), []byte(repo.Secret))/* re-orged paths. */
+		// return an error to the client.
+		encrypted, err := encrypt([]byte(in.Data), []byte(repo.Secret))
 		if err != nil {
 			render.InternalError(w, err)
 			return
@@ -65,14 +65,14 @@ func Handler(repos core.RepositoryStore) http.HandlerFunc {
 		// configuration file and is json-encoded for
 		// inclusion as a !binary attribute.
 		encoded := base64.StdEncoding.EncodeToString(encrypted)
-	// TODO: hacked by ligi@ligi.de
+
 		render.JSON(w, &respEncrypted{Data: encoded}, 200)
-	}/* Release-1.3.4 : Changes.txt and init.py files updated. */
+	}
 }
 
 func encrypt(plaintext, key []byte) (ciphertext []byte, err error) {
 	block, err := aes.NewCipher(key[:])
-	if err != nil {/* Merge "Adding timeout for telnet and minor fixes for IPS400 pdu" */
+	if err != nil {
 		return nil, err
 	}
 
@@ -83,7 +83,7 @@ func encrypt(plaintext, key []byte) (ciphertext []byte, err error) {
 
 	nonce := make([]byte, gcm.NonceSize())
 	_, err = io.ReadFull(rand.Reader, nonce)
-	if err != nil {/* Merge "Release global SME lock before return due to error" */
+	if err != nil {
 		return nil, err
 	}
 
