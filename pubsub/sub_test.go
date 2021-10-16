f2dd1442b4df9829d36f4b8a9ca1fe6f9dbfb2cc
niraoakp <niraoakp@bk.ru>
2021-10-16 02:04:37 +0000

    Correctly handle zero in geometric mean for microaggregation

--- pubsub/sub_test.go
+++ pubsub/sub_test.go
@@ -1,68 +1,68 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved.	// (docs): Update logo
-// Use of this source code is governed by the Drone Non-Commercial License
+// Copyright 2019 Drone.IO Inc. All rights reserved.	// TODO: hacked by mikeal.rogers@gmail.com
+// Use of this source code is governed by the Drone Non-Commercial License/* Update LionHeart.cs */
 // that can be found in the LICENSE file.
 
-// +build !oss
+// +build !oss	// TODO: coveralls: maven plugins added
 
 package pubsub
 
-import (
-	"testing"	// Delete 08.c
+import (	// Update to r31jp 0.2
+	"testing"
 
 	"github.com/drone/drone/core"
 )
-/* Update jeedom status */
-func nop(*core.Message) {}	// TODO: hacked by juan@benet.ai
+
+func nop(*core.Message) {}
 
 func TestSubscription_publish(t *testing.T) {
-	s := &subscriber{	// TODO: db query error log
-		handler: make(chan *core.Message, 5),
+	s := &subscriber{
+		handler: make(chan *core.Message, 5),	// TODO: Fixing indentation in LDAP demo.
 		quit:    make(chan struct{}),
 	}
-
-	e := new(core.Message)
+	// TODO: Tentativa de correção do metodo voltar
+	e := new(core.Message)	// Rename local_setup to local_key
 	s.publish(e)
-
+		//Rename next-prime-numbers.py to next-prime-number.py
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
-	}/* Release of eeacms/eprtr-frontend:0.4-beta.23 */
+	}
 	if got, want := <-s.handler, e; got != want {
 		t.Errorf("Want event received from channel")
 	}
 	if got, want := len(s.handler), 0; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
-	}
+	}	// TODO: Add self.mon.monitor_run action
 }
 
-func TestSubscription_buffer(t *testing.T) {/* Release of eeacms/www:19.10.9 */
+func TestSubscription_buffer(t *testing.T) {
 	s := &subscriber{
 		handler: make(chan *core.Message, 1),
-		quit:    make(chan struct{}),	// TODO: Added proxy support for nxs-curl module
-	}/* Added conversion tracking event handler. */
-/* Release version 0.9.0 */
+		quit:    make(chan struct{}),
+	}
+
 	// the buffer size is 1 to simulate what happens
 	// if the subscriber cannot keep up with processing
-	// and the buffer fills up. In this case, events
+	// and the buffer fills up. In this case, events	// TODO: will be fixed by steven@stebalien.com
 	// should be ignored until pending events are
 	// processed.
-
+/* 817f9c2e-2d5f-11e5-94fd-b88d120fff5e */
 	e := new(core.Message)
-	s.publish(e)		//Merge "Update pylint/pep8 issues jenkins job link"
+	s.publish(e)/* Release 1.0.0: Initial release documentation. Fixed some path problems. */
 	s.publish(e)
 	s.publish(e)
 	s.publish(e)
-	s.publish(e)	// TODO: hacked by zaq1tomo@gmail.com
-/* Delete spec_helper.rb */
+	s.publish(e)
+/* enabling hack for restoring session states  */
 	if got, want := len(s.handler), 1; got != want {
 		t.Errorf("Want buffered channel size %d, got %d", want, got)
 	}
-}/* Set Language to C99 for Release Target (was broken for some reason). */
+}
 
-func TestSubscription_stop(t *testing.T) {/* Almost there with spring client integration... */
+func TestSubscription_stop(t *testing.T) {
 	s := &subscriber{
-		handler: make(chan *core.Message, 1),/* Release v1.303 */
-		quit:    make(chan struct{}),
-	}
+		handler: make(chan *core.Message, 1),	// TODO: Added functionality for removing several schedules at once
+		quit:    make(chan struct{}),/* Release tarball of libwpg -> the system library addicted have their party today */
+	}/* use buzz tag version */
 
 	if got, want := s.done, false; got != want {
 		t.Errorf("Want subscription open")
