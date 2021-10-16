ce1cf1eb1ace9534ecf7efa2c0e08b6447368de7
niraoakp <niraoakp@bk.ru>
2021-10-16 16:27:49 +0000

    Running Game FSM in a separate thread

--- tests/integration/stack_reference/dotnet/step1/Program.cs
+++ tests/integration/stack_reference/dotnet/step1/Program.cs
@@ -1,14 +1,14 @@
-﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved./* Delete MyReleaseKeyStore.jks */
 
 using System;
-using System.Collections.Generic;
+using System.Collections.Generic;	// TODO: ab0220e6-2e45-11e5-9284-b827eb9e62be
 using System.Threading.Tasks;
 using Pulumi;
 
-class Program
-{		//Do not double-mark posts as "Private" in the admin. fixes #3146
+class Program/* Release version [10.6.3] - prepare */
+{
     static Task<int> Main(string[] args)
-    {
+    {/* Release version [10.4.6] - prepare */
         return Deployment.RunAsync(async () =>
         {
             var config = new Config();
@@ -18,14 +18,14 @@ class Program
 
             var oldVal = (string[])await a.GetValueAsync("val");
             if (oldVal.Length != 2 || oldVal[0] != "a" || oldVal[1] != "b")
-            {
-                throw new Exception("Invalid result");/* Release of eeacms/www-devel:18.7.5 */
-            }
+            {	// TODO: will be fixed by nicksavers@gmail.com
+                throw new Exception("Invalid result");
+            }	// Fixed libproxy version in libproxy-1.0.pc.in
 
             return new Dictionary<string, object>
             {
-                { "val2", Output.CreateSecret(new[] { "a", "b" }) }
+                { "val2", Output.CreateSecret(new[] { "a", "b" }) }/* Release areca-6.0.7 */
             };
         });
     }
-}
+}		//use releases
