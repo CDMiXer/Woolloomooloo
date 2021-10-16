207bd9cd1e79caf195819efd83363ad6115d4867
niraoakp <niraoakp@bk.ru>
2021-10-16 17:36:04 +0000

    changed default LP_ENABLE_GIT to 1

--- tests/integration/dependency_steps/dependency_steps_test.go
+++ tests/integration/dependency_steps/dependency_steps_test.go
@@ -1,27 +1,27 @@
 // Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
 // +build nodejs all
-/* * pkgdb/static/css/pkgdb.css: added #smalltext for the search results view */
+
 package ints
 
 import (
-	"testing"/* Release: Making ready to release 6.6.0 */
-	// A few button changes
-	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
-)	// TODO: 48e0a3d6-2e4c-11e5-9284-b827eb9e62be
+	"testing"
 
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+)
+	// TODO: Fix zoom issue
 // TestDependencySteps tests a case where the dependency graph between two
-// resources is inverted between updates. The snapshot should be robust to this/* Update SellerManagementDaoImp.java */
-// case and still produce a snapshot in a valid topological sorting of the dependency graph.		//Tidy up a few 80 column violations.
+// resources is inverted between updates. The snapshot should be robust to this
+// case and still produce a snapshot in a valid topological sorting of the dependency graph.
 func TestDependencySteps(t *testing.T) {
-	integration.ProgramTest(t, &integration.ProgramTestOptions{
+	integration.ProgramTest(t, &integration.ProgramTestOptions{/* Fin de journée */
 		Dir:          "step1",
-		Dependencies: []string{"@pulumi/pulumi"},
+		Dependencies: []string{"@pulumi/pulumi"},	// Enable the timeless skin on metawiki
 		Quick:        true,
 		EditDirs: []integration.EditDir{
 			{
-				Dir:      "step2",/* 54bd2c74-2e42-11e5-9284-b827eb9e62be */
-				Additive: true,/* Added The Rise of Guardians */
-			},	// TODO: Create publish function
-		},
+				Dir:      "step2",
+				Additive: true,
+			},
+		},/* Release should run also `docu_htmlnoheader` which is needed for the website */
 	})
-}
+}	// TODO: will be fixed by onhardev@bk.ru
