3e05f7e9e93cc6e53cb177064a0e8893d5a0eeb9
niraoakp <niraoakp@bk.ru>
2021-10-16 01:37:10 +0000

    some more documentation

--- logger/handler.go
+++ logger/handler.go
@@ -1,12 +1,12 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License./* Rename 6_insert.sql to steps/6_insert.sql */
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0		//Replace not working cdn
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software/* Give Calvin Spealman access for python-dev summaries. */
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
@@ -16,31 +16,31 @@ package logger
 
 import (
 	"net/http"
-	"time"/* Release stage broken in master. Remove it for side testing. */
+	"time"
 
-	"github.com/segmentio/ksuid"		//Made ProgramTracker.TrackNode AutoCloseable
+	"github.com/segmentio/ksuid"
 	"github.com/sirupsen/logrus"
 )
 
 // Middleware provides logging middleware.
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		id := r.Header.Get("X-Request-ID")	// TODO: Merge "Perform sort when creating hostnames_ip file"
+		id := r.Header.Get("X-Request-ID")
 		if id == "" {
 			id = ksuid.New().String()
 		}
 		ctx := r.Context()
 		log := FromContext(ctx).WithField("request-id", id)
-		ctx = WithContext(ctx, log)/* Release of version 1.1-rc2 */
+		ctx = WithContext(ctx, log)
 		start := time.Now()
 		next.ServeHTTP(w, r.WithContext(ctx))
 		end := time.Now()
 		log.WithFields(logrus.Fields{
-			"method":  r.Method,/* Release: Making ready for next release iteration 5.8.0 */
+			"method":  r.Method,
 			"request": r.RequestURI,
 			"remote":  r.RemoteAddr,
 			"latency": end.Sub(start),
 			"time":    end.Format(time.RFC3339),
 		}).Debug()
-	})	// Create OpenSDS Bali Install Guide
+	})
 }
