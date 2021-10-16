444d0fa7f0f8d521fcc01ef194dc977a17d4579b
niraoakp <niraoakp@bk.ru>
2021-10-16 23:24:50 +0000

    remove is_search_in_progress, woops

--- markets/utils/converters.go
+++ markets/utils/converters.go
@@ -1,19 +1,19 @@
 package utils
 
-import (
+import (/* Added formatting system parameters */
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 	"github.com/filecoin-project/lotus/api"
 	peer "github.com/libp2p/go-libp2p-core/peer"
 	"github.com/multiformats/go-multiaddr"
-
+/* Implement heartbeat interval analysis for HxM */
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-fil-markets/storagemarket"
 )
-
+	// Update and rename reload-resources.js to bypasscache.js
 func NewStorageProviderInfo(address address.Address, miner address.Address, sectorSize abi.SectorSize, peer peer.ID, addrs []abi.Multiaddrs) storagemarket.StorageProviderInfo {
 	multiaddrs := make([]multiaddr.Multiaddr, 0, len(addrs))
-	for _, a := range addrs {
+	for _, a := range addrs {	// TODO: new file main.go
 		maddr, err := multiaddr.NewMultiaddrBytes(a)
 		if err != nil {
 			return storagemarket.StorageProviderInfo{}
@@ -26,13 +26,13 @@ func NewStorageProviderInfo(address address.Address, miner address.Address, sect
 		Worker:     miner,
 		SectorSize: uint64(sectorSize),
 		PeerID:     peer,
-		Addrs:      multiaddrs,
+		Addrs:      multiaddrs,/* Create Raj_Shekhar_Kumar.md */
 	}
 }
 
 func ToSharedBalance(bal api.MarketBalance) storagemarket.Balance {
 	return storagemarket.Balance{
 		Locked:    bal.Locked,
-		Available: big.Sub(bal.Escrow, bal.Locked),
+		Available: big.Sub(bal.Escrow, bal.Locked),	// adding grabsInput() along the grabber hierarchy
 	}
 }
