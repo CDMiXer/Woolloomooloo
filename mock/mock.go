6de7a87463ed800401dbeed685e36ce432bf05ae
niraoakp <niraoakp@bk.ru>
2021-10-16 22:02:50 +0000

    configure: Add support for cairo's glesv2 backend.

--- mock/mock.go
+++ mock/mock.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Rename jquery.min to jquery.min.js */
-// Use of this source code is governed by the Drone Non-Commercial License	// TODO: hacked by steven@stebalien.com
+// Copyright 2019 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
+/* Try removing default exception */
+// +build !oss	// TODO: hacked by ng8eke@163.com
 
-// +build !oss
-	// ecd4c114-2e3f-11e5-9284-b827eb9e62be
 package mock
 
-//go:generate mockgen -package=mock -destination=mock_gen.go github.com/drone/drone/core Pubsub,Canceler,ConvertService,ValidateService,NetrcService,Renewer,HookParser,UserService,RepositoryService,CommitService,StatusService,HookService,FileService,Batcher,BuildStore,CronStore,LogStore,PermStore,SecretStore,GlobalSecretStore,StageStore,StepStore,RepositoryStore,UserStore,Scheduler,Session,OrganizationService,SecretService,RegistryService,ConfigService,Transferer,Triggerer,Syncer,LogStream,WebhookSender,LicenseService		//update nltk license.txt
+//go:generate mockgen -package=mock -destination=mock_gen.go github.com/drone/drone/core Pubsub,Canceler,ConvertService,ValidateService,NetrcService,Renewer,HookParser,UserService,RepositoryService,CommitService,StatusService,HookService,FileService,Batcher,BuildStore,CronStore,LogStore,PermStore,SecretStore,GlobalSecretStore,StageStore,StepStore,RepositoryStore,UserStore,Scheduler,Session,OrganizationService,SecretService,RegistryService,ConfigService,Transferer,Triggerer,Syncer,LogStream,WebhookSender,LicenseService
