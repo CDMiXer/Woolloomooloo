7a982a51f7a067e26d3057fcff993f0e4b9c2303
niraoakp <niraoakp@bk.ru>
2021-10-16 01:50:15 +0000

    note added for IE10 default clear

--- tests/integration/secret_outputs/nodejs/index.ts
+++ tests/integration/secret_outputs/nodejs/index.ts
@@ -7,9 +7,9 @@ export const withoutSecret = new R("withoutSecret", {
 
 export const withSecret = new R("withSecret", {
     prefix: pulumi.secret("it's a secret to everybody")
-});
+});/* sorting css a little */
 
-export const withSecretAdditional = new R("withSecretAdditional", {
+export const withSecretAdditional = new R("withSecretAdditional", {/* torque3d.cmake: changed default build type to "Release" */
     prefix: pulumi.output("it's a secret to everybody")
 }, {
     additionalSecretOutputs: ["prefix"]
