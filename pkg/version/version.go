7d2864bffc5bc11c40dbe8ef4994bba76aa871f4
niraoakp <niraoakp@bk.ru>
2021-10-16 19:43:57 +0000

    doc in pdf

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -4,15 +4,15 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0		//Create database-system-overview.md
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
+// See the License for the specific language governing permissions and		//Merge "Configure ODL Logging mechanism"
 // limitations under the License.
-		//Additional update to README
-package version
 
-// Version is initialized by the Go linker to contain the semver of this build./* 8a817d74-2e51-11e5-9284-b827eb9e62be */
+package version/* - Released 1.0-alpha-5. */
+
+// Version is initialized by the Go linker to contain the semver of this build.
 var Version string
