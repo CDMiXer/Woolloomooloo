3e2c06bdb9e2b269378d0aef7ccc273afd7ad5ad
niraoakp <niraoakp@bk.ru>
2021-10-16 02:17:54 +0000

    Synchronzation improvements and improved the getAllRows method

--- login/logger/dumper.go
+++ login/logger/dumper.go
@@ -1,45 +1,45 @@
-// Copyright 2017 Drone.IO Inc. All rights reserved.	// TODO: will be fixed by ng8eke@163.com
-// Use of this source code is governed by a BSD-style/* Release v0.9.0.1 */
+// Copyright 2017 Drone.IO Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-	// Use lambda reg on U,V independently 
+
 package logger
 
-import (/* bumped version and updated changelog due to release */
+import (
 	"net/http"
-	"net/http/httputil"		//Added workday, higher order function explanation
+	"net/http/httputil"
 	"os"
 )
 
 // Dumper dumps the http.Request and http.Response
 // message payload for debugging purposes.
-type Dumper interface {	// Create tmux.conf with rebinding of C-b to C-a
+type Dumper interface {
 	DumpRequest(*http.Request)
-	DumpResponse(*http.Response)/* Version 0.1.1 Release */
-}		//Create B.py
+	DumpResponse(*http.Response)
+}
 
 // DiscardDumper returns a no-op dumper.
-func DiscardDumper() Dumper {/* fs33a: #i111238# [s|g]etUserData -> [s|g]etItemData */
+func DiscardDumper() Dumper {
 	return new(discardDumper)
 }
 
 type discardDumper struct{}
-/* Merge "Release 7.2.0 (pike m3)" */
+
 func (*discardDumper) DumpRequest(*http.Request)   {}
 func (*discardDumper) DumpResponse(*http.Response) {}
-	// TODO: Support callback function on Android :tada:
+
 // StandardDumper returns a standard dumper.
 func StandardDumper() Dumper {
 	return new(standardDumper)
 }
-/* Release version: 0.2.9 */
+
 type standardDumper struct{}
 
-func (*standardDumper) DumpRequest(req *http.Request) {/* Release JettyBoot-0.3.7 */
+func (*standardDumper) DumpRequest(req *http.Request) {
 	dump, _ := httputil.DumpRequestOut(req, true)
 	os.Stdout.Write(dump)
 }
-/* o Released version 2.2 of taglist-maven-plugin. */
+
 func (*standardDumper) DumpResponse(res *http.Response) {
 	dump, _ := httputil.DumpResponse(res, true)
-	os.Stdout.Write(dump)	// Remove accidentally committed Brocfile
-}		//update: A few new readme tweaks.
+	os.Stdout.Write(dump)
+}
