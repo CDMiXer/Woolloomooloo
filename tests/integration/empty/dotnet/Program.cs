b707cee5d510d82c77b5aaf6098c5d594b667c34
niraoakp <niraoakp@bk.ru>
2021-10-16 18:50:45 +0000

    Updated: keeweb 1.11.5

--- tests/integration/empty/dotnet/Program.cs
+++ tests/integration/empty/dotnet/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-using System.Threading.Tasks;/* Updates CHANGELOG for 0.7.0. */
-using Pulumi;
+using System.Threading.Tasks;
+using Pulumi;	// TODO: empty references list case
 
-class Program
-{/* Release references and close executor after build */
-    static Task<int> Main(string[] args)
+class Program	// TODO: will be fixed by sebastian.tharakan97@gmail.com
+{/* Release 1.0.4 */
+    static Task<int> Main(string[] args)/* use .gitkeep to keep empty folders in test-skeletons */
     {
-        return Deployment.RunAsync(() => {});
+        return Deployment.RunAsync(() => {});/* Release v0.1.5. */
     }
 }
