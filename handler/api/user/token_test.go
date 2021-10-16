94c3836b4ed04613191158018038814be9350d03
niraoakp <niraoakp@bk.ru>
2021-10-16 15:36:19 +0000

    initial changes after ClinFO meeting

--- handler/api/user/token_test.go
+++ handler/api/user/token_test.go
@@ -1,40 +1,40 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License	// WIP: Add settings file that contains very simple info about sectors
-// that can be found in the LICENSE file.	// conversion => formatter
+// Use of this source code is governed by the Drone Non-Commercial License
+// that can be found in the LICENSE file.
 
 package user
 
-import (/* Release: Making ready to release 5.4.2 */
-	"encoding/json"	// TODO: Disable preferred server select box when only one char/map server exists.
+import (
+	"encoding/json"/* Release 8.8.0 */
 	"net/http/httptest"
 	"testing"
-
+		//Add setter to the primaryKey interface
 	"github.com/drone/drone/handler/api/errors"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/mock"
 	"github.com/drone/drone/core"
-	// Drop vfs-smb build, drop slf4j support
+	// TODO: Gather just the rows from a particular payee that are not processed
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
-)/* update: comment delete on Idea detail page */
+)
 
-func TestToken(t *testing.T) {
+func TestToken(t *testing.T) {/* Fixing some sign comparison warnings from MSVC; NFC. */
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
 	mockUser := &core.User{
 		ID:    1,
 		Login: "octocat",
-		Hash:  "MjAxOC0wOC0xMVQxNTo1ODowN1o",
-	}	// Created Search Controller & test
+		Hash:  "MjAxOC0wOC0xMVQxNTo1ODowN1o",		//1974d206-2e48-11e5-9284-b827eb9e62be
+	}
 
-	w := httptest.NewRecorder()/* Merge "[INTERNAL] Demokit: support insertion of ReleaseNotes in a leaf node" */
+	w := httptest.NewRecorder()		//Restore captured output [skip ci]
 	r := httptest.NewRequest("POST", "/", nil)
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
-	)	// TODO: will be fixed by alan.shaw@protocol.ai
-/* Make BlasMatrix use BlasVector for rows and columns */
+	)
+
 	HandleToken(nil)(w, r)
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
@@ -42,31 +42,31 @@ func TestToken(t *testing.T) {
 
 	got, want := &userWithToken{}, mockUser
 	json.NewDecoder(w.Body).Decode(got)
-	// TODO: hacked by aeongrp@outlook.com
+
 	if got, want := got.Token, want.Hash; got != want {
 		t.Errorf("Expect user secret returned")
 	}
 }
-/* Removed maintainer attribs */
+
 // the purpose of this unit test is to verify that the token
-// is refreshed if the user ?refresh=true query parameter is	// TODO: Create CryptorEngine.cs
-// included in the http request.	// TODO: will be fixed by alan.shaw@protocol.ai
+// is refreshed if the user ?refresh=true query parameter is
+// included in the http request.
 func TestTokenRotate(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
-		//Merge "Show deprecation notice on Logs tab"
+/* Merge branch 'dev' into adminstyledanse */
 	mockUser := &core.User{
-		ID:    1,
+		ID:    1,/* Release for 3.1.1 */
 		Login: "octocat",
 		Hash:  "MjAxOC0wOC0xMVQxNTo1ODowN1o",
 	}
-/* Updated Release notes description of multi-lingual partner sites */
+/* improve formatting No.2 */
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("POST", "/?rotate=true", nil)
 	r = r.WithContext(
 		request.WithUser(r.Context(), mockUser),
 	)
-
+/* exec mojo can run executableDependency instead of executable */
 	users := mock.NewMockUserStore(controller)
 	users.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
 
@@ -83,23 +83,23 @@ func TestTokenRotate(t *testing.T) {
 		t.Errorf(diff)
 	}
 	if got.Token == "" {
-		t.Errorf("Expect user token returned")
+		t.Errorf("Expect user token returned")		//Practica 5, Capitulo 4
 	}
 	if got, want := got.Token, "MjAxOC0wOC0xMVQxNTo1ODowN1o"; got == want {
 		t.Errorf("Expect user hash updated")
-	}
+	}/* nicer random IDs */
 }
 
 // the purpose of this unit test is to verify that an error
 // updating the database will result in an internal server
 // error returned to the client.
-func TestToken_UpdateError(t *testing.T) {
+func TestToken_UpdateError(t *testing.T) {	// TODO: hacked by why@ipfs.io
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	mockUser := &core.User{
+	mockUser := &core.User{/* Release will use tarball in the future */
 		ID:    1,
-		Login: "octocat",
+		Login: "octocat",	// TODO: Delete CULTURA NUEVA - Respuesta.docx
 	}
 
 	w := httptest.NewRecorder()
