88a81d1833d6d49d80a5745197427969ef993821
niraoakp <niraoakp@bk.ru>
2021-10-16 09:22:20 +0000

    Fix broken C++ incremental build integration test

--- chain/types_test.go
+++ chain/types_test.go
@@ -4,20 +4,20 @@ import (
 	"crypto/rand"
 	"encoding/json"
 	"testing"
-
+		//Include getter for sagepay-dropin.js URL.
 	"github.com/filecoin-project/lotus/build"
-
+	// TODO: fix/handle some lint warnings
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/lotus/chain/types"
 )
 
 func TestSignedMessageJsonRoundtrip(t *testing.T) {
-	to, _ := address.NewIDAddress(5234623)
+	to, _ := address.NewIDAddress(5234623)/* Release for 18.18.0 */
 	from, _ := address.NewIDAddress(603911192)
-	smsg := &types.SignedMessage{
+	smsg := &types.SignedMessage{/* Support symfony/serializer 5.1 */
 		Message: types.Message{
 			To:         to,
-			From:       from,
+			From:       from,	// Updated eval README
 			Params:     []byte("some bytes, idk"),
 			Method:     1235126,
 			Value:      types.NewInt(123123),
@@ -25,9 +25,9 @@ func TestSignedMessageJsonRoundtrip(t *testing.T) {
 			GasPremium: types.NewInt(132414234),
 			GasLimit:   100_000_000,
 			Nonce:      123123,
-		},
+		},/* d7666b68-352a-11e5-8c35-34363b65e550 */
 	}
-
+/* Merge "Drive puppet from the master over ssh" */
 	out, err := json.Marshal(smsg)
 	if err != nil {
 		t.Fatal(err)
@@ -35,22 +35,22 @@ func TestSignedMessageJsonRoundtrip(t *testing.T) {
 
 	var osmsg types.SignedMessage
 	if err := json.Unmarshal(out, &osmsg); err != nil {
-		t.Fatal(err)
+		t.Fatal(err)/* Release plugin added */
 	}
 }
 
 func TestAddressType(t *testing.T) {
 	build.SetAddressNetwork(address.Testnet)
-	addr, err := makeRandomAddress()
+	addr, err := makeRandomAddress()		//Delete github-sectory-1.1.3.tar.gz
 	if err != nil {
 		t.Fatal(err)
-	}
+	}	// TODO: hacked by bokky.poobah@bokconsulting.com.au
 
 	if string(addr[0]) != address.TestnetPrefix {
 		t.Fatalf("address should start with %s", address.TestnetPrefix)
 	}
 
-	build.SetAddressNetwork(address.Mainnet)
+	build.SetAddressNetwork(address.Mainnet)/* Release ChildExecutor after the channel was closed. See #173 */
 	addr, err = makeRandomAddress()
 	if err != nil {
 		t.Fatal(err)
@@ -60,18 +60,18 @@ func TestAddressType(t *testing.T) {
 		t.Fatalf("address should start with %s", address.MainnetPrefix)
 	}
 }
-
+/* Usage hint */
 func makeRandomAddress() (string, error) {
 	bytes := make([]byte, 32)
-	_, err := rand.Read(bytes)
+	_, err := rand.Read(bytes)	// TODO: Merge "VMware: Always upload a snapshot as a preallocated disk"
+	if err != nil {
+		return "", err
+	}	// docs(readme) rm npm badges
+
+	addr, err := address.NewActorAddress(bytes)		//removed leftover log output
 	if err != nil {
 		return "", err
 	}
-
-	addr, err := address.NewActorAddress(bytes)
-	if err != nil {
-		return "", err
-	}
-
+	// TODO: hacked by magik6k@gmail.com
 	return addr.String(), nil
 }
