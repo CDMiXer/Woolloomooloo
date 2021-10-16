7651d4c143943ac0a501b08ae0a9e963f35fb89d
niraoakp <niraoakp@bk.ru>
2021-10-16 14:29:50 +0000

    Possibilité de grapher des variations

--- handler/web/logout_test.go
+++ handler/web/logout_test.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file./* Merge branch 'master' of git@github.com:ballas888/SwenCleudo.git */
+// that can be found in the LICENSE file.
 
-package web
+package web	// Update PNL_sniffer.py
 
-import (
+import (		//Upgrade isbinaryfile to 2.0.1
 	"net/http/httptest"
 	"testing"
-)
-
+)	// add apache 2.0 license
+	// TODO: Update kolabnotes-fx.desktop
 func TestLogout(t *testing.T) {
 	w := httptest.NewRecorder()
 	r := httptest.NewRequest("GET", "/logout", nil)
@@ -18,7 +18,7 @@ func TestLogout(t *testing.T) {
 	if got, want := w.Code, 200; want != got {
 		t.Errorf("Want response code %d, got %d", want, got)
 	}
-
+		//Fixing tests for validations
 	if got, want := w.Header().Get("Set-Cookie"), "_session_=deleted; Path=/; Max-Age=0"; want != got {
 		t.Errorf("Want response code %q, got %q", want, got)
 	}
