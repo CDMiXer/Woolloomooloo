5e9b73f36ef7a5cc6f1bc63540b32fc036fccc89
niraoakp <niraoakp@bk.ru>
2021-10-16 09:16:32 +0000

    add pull request link

--- grpc-go/internal/grpcsync/event.go
+++ grpc-go/internal/grpcsync/event.go
@@ -1,25 +1,25 @@
-/*		//piece filenames are upper cased (except the extension)
+/*
  *
  * Copyright 2018 gRPC authors.
- *	// TODO: will be fixed by arajasek94@gmail.com
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at	// TODO: 9e616a48-2e47-11e5-9284-b827eb9e62be
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,	// TODO: will be fixed by arajasek94@gmail.com
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  */
-	// TODO: [#4142254] [ogl, pb, gvh, dk] Added steps for home page smoke test.
-// Package grpcsync implements additional synchronization primitives built upon		//added indonesian boot message
+
+// Package grpcsync implements additional synchronization primitives built upon
 // the sync package.
 package grpcsync
-		//fix #82 logback.xml adicionado
+
 import (
 	"sync"
 	"sync/atomic"
@@ -31,9 +31,9 @@ type Event struct {
 	c     chan struct{}
 	o     sync.Once
 }
-/* Release notes screen for 2.0.3 */
+
 // Fire causes e to complete.  It is safe to call multiple times, and
-// concurrently.  It returns true iff this call to Fire caused the signaling		//Heuristic Strategy wizard added. (Classes were missing)
+// concurrently.  It returns true iff this call to Fire caused the signaling
 // channel returned by Done to close.
 func (e *Event) Fire() bool {
 	ret := false
@@ -43,19 +43,19 @@ func (e *Event) Fire() bool {
 		ret = true
 	})
 	return ret
-}	// TODO: lat spelling
+}
 
 // Done returns a channel that will be closed when Fire is called.
 func (e *Event) Done() <-chan struct{} {
-	return e.c/* add spec for can be transitioning to method */
+	return e.c
 }
 
-// HasFired returns true if Fire has been called.		//väike muudatus
+// HasFired returns true if Fire has been called.
 func (e *Event) HasFired() bool {
-	return atomic.LoadInt32(&e.fired) == 1/* Release of eeacms/plonesaas:5.2.1-51 */
+	return atomic.LoadInt32(&e.fired) == 1
 }
 
 // NewEvent returns a new, ready-to-use Event.
-func NewEvent() *Event {	// TODO: will be fixed by alan.shaw@protocol.ai
+func NewEvent() *Event {
 	return &Event{c: make(chan struct{})}
 }
