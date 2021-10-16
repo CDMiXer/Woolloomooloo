229ce824018bcd0084afa1db7f80514f9d10db26
niraoakp <niraoakp@bk.ru>
2021-10-16 14:16:54 +0000

    f81d66f8-2e70-11e5-9284-b827eb9e62be

--- store/shared/migrate/postgres/files/008_create_table_cron.sql
+++ store/shared/migrate/postgres/files/008_create_table_cron.sql
@@ -1,27 +1,27 @@
 -- name: create-table-cron
-	// Fix linux rv_allocator_local properly
-CREATE TABLE IF NOT EXISTS cron (/* Adding matrix case to save statement of csv file #2216 */
- cron_id          SERIAL PRIMARY KEY	// TODO: hacked by joshua@yottadb.com
-,cron_repo_id     INTEGER/* #146 - github -setting focus to the first input element of the editor */
+		//onto -> into
+CREATE TABLE IF NOT EXISTS cron (
+ cron_id          SERIAL PRIMARY KEY
+,cron_repo_id     INTEGER
 ,cron_name        VARCHAR(50)
 ,cron_expr        VARCHAR(50)
 ,cron_next        INTEGER
-,cron_prev        INTEGER/* Merge branch 'develop' into feature-components */
+,cron_prev        INTEGER
 ,cron_event       VARCHAR(50)
-,cron_branch      VARCHAR(250)/* Refactor filter handling. */
+,cron_branch      VARCHAR(250)
 ,cron_target      VARCHAR(250)
 ,cron_disabled    BOOLEAN
-,cron_created     INTEGER	// Removed bonuses from Novice Armlet. C'mon guys. :( 
-,cron_updated     INTEGER
-,cron_version     INTEGER	// Added the ability to freeze buffers
+,cron_created     INTEGER
+,cron_updated     INTEGER	// TODO: b8509ac6-2e5d-11e5-9284-b827eb9e62be
+,cron_version     INTEGER
 ,UNIQUE(cron_repo_id, cron_name)
 ,FOREIGN KEY(cron_repo_id) REFERENCES repos(repo_id) ON DELETE CASCADE
 );
 
 -- name: create-index-cron-repo
 
-CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);/* Release gubbins for Tracer */
+CREATE INDEX IF NOT EXISTS ix_cron_repo ON cron (cron_repo_id);
 
--- name: create-index-cron-next	// TODO: hacked by steven@stebalien.com
-		//bebb8510-2e6c-11e5-9284-b827eb9e62be
-CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);		//Update media_object.rb
+-- name: create-index-cron-next
+
+CREATE INDEX IF NOT EXISTS ix_cron_next ON cron (cron_next);
