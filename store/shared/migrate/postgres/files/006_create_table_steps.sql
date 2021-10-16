a089bc77f121a33228650483040c25f5d7a96d39
niraoakp <niraoakp@bk.ru>
2021-10-16 12:32:57 +0000

    * Update django-countries to use a django 1.11 compatible version.

--- store/shared/migrate/postgres/files/006_create_table_steps.sql
+++ store/shared/migrate/postgres/files/006_create_table_steps.sql
@@ -1,14 +1,14 @@
 -- name: create-table-steps
 
-CREATE TABLE IF NOT EXISTS steps (		//Added serverinfo.py
+CREATE TABLE IF NOT EXISTS steps (
  step_id          SERIAL PRIMARY KEY
 ,step_stage_id    INTEGER
-,step_number      INTEGER	// TODO: 6ebc1ec4-2e5a-11e5-9284-b827eb9e62be
-,step_name        VARCHAR(100)	// TODO: few string changes and updated pot file
+,step_number      INTEGER
+,step_name        VARCHAR(100)
 ,step_status      VARCHAR(50)
 ,step_error       VARCHAR(500)
 ,step_errignore   BOOLEAN
-,step_exit_code   INTEGER	// TODO: working on the server command line 
+,step_exit_code   INTEGER
 ,step_started     INTEGER
 ,step_stopped     INTEGER
 ,step_version     INTEGER
@@ -17,4 +17,4 @@ CREATE TABLE IF NOT EXISTS steps (		//Added serverinfo.py
 
 -- name: create-index-steps-stage
 
-CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);/* #13 - plugin install command patched in readme */
+CREATE INDEX IF NOT EXISTS ix_steps_stage ON steps (step_stage_id);
