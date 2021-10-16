ba6e7e9f5aef4fe15a8793c0139b19f4e1598149
niraoakp <niraoakp@bk.ru>
2021-10-16 18:11:21 +0000

    Fixed checking of relative URLs

--- tests/containers/dotnet/Program.fs
+++ tests/containers/dotnet/Program.fs
@@ -1,4 +1,4 @@
-﻿module Program
+﻿module Program	// TODO: 44f4b920-2e41-11e5-9284-b827eb9e62be
 
 open System
 open Pulumi.FSharp
@@ -6,11 +6,11 @@ open Pulumi.FSharp
 let infra () =
   let config = new Pulumi.Config()
   let runtime = config.Get("runtime")
-  Console.WriteLine("Hello from {0}", runtime)
+  Console.WriteLine("Hello from {0}", runtime)/* Release 0.11.1.  Fix default value for windows_eventlog. */
   
   // Stack outputs
   dict []
-
+	// TODO: [CRAFT-AI] Delete resource: test11.bt
 [<EntryPoint>]
 let main _ =
   Deployment.run infra
