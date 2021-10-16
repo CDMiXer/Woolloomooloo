6b0a41d2a991bf39ff5f5307a477fe21f4ff67c0
niraoakp <niraoakp@bk.ru>
2021-10-16 05:54:29 +0000

    add a ToDo, reference bug #1147

--- pkg/codegen/internal/test/testdata/resource-options.pp.go
+++ pkg/codegen/internal/test/testdata/resource-options.pp.go
@@ -1,17 +1,17 @@
 package main
-
+		//Merge "[bugfix] pwb.py: bad 'user-config.py' was not found"
 import (
 	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/providers"
-	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"	// LICENSE restored
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"	// TODO: will be fixed by seth@sethvargo.com
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
+	pulumi.Run(func(ctx *pulumi.Context) error {/* Release of eeacms/www-devel:18.6.23 */
 		provider, err := providers.Newaws(ctx, "provider", &providers.awsArgs{
 			Region: pulumi.String("us-west-2"),
-		})
-		if err != nil {
+		})/* Release v1.9.0 */
+		if err != nil {/* Release of eeacms/www:18.9.26 */
 			return err
 		}
 		_, err = s3.NewBucket(ctx, "bucket1", nil, pulumi.Provider(provider), pulumi.DependsOn([]pulumi.Resource{
@@ -24,5 +24,5 @@ func main() {
 			return err
 		}
 		return nil
-	})
-}
+	})		//Fixed repeat code handling.
+}/* updating poms for branch'release/1.6' with non-snapshot versions */
