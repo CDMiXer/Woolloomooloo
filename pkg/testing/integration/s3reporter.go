e6a63c80e6997065636b98bfd9157121e9e58cee
niraoakp <niraoakp@bk.ru>
2021-10-16 07:17:20 +0000

    changed validator to check file mappings according to the submission type

--- pkg/testing/integration/s3reporter.go
+++ pkg/testing/integration/s3reporter.go
@@ -1,16 +1,16 @@
 // Copyright 2016-2018, Pulumi Corporation.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");/* Release of eeacms/www-devel:18.6.15 */
+// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
-//	// TODO: upgrade depend gems
-// Unless required by applicable law or agreed to in writing, software/* Add code analysis on Release mode */
+//
+// Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Supervisor is now Py3 ready.  Next up: PuLP */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.		//microblaze: Fix build template/debug
+// limitations under the License.
 
 package integration
 
@@ -23,47 +23,47 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"	// let statistic form grab space
+	"github.com/aws/aws-sdk-go/service/s3"
 
 	"github.com/pulumi/pulumi/sdk/v2/go/common/resource"
 )
 
 // S3Reporter is a TestStatsReporter that publises test data to S3
 type S3Reporter struct {
-	s3svc     *s3.S3	// TODO: document telemetry sensor from #7236
+	s3svc     *s3.S3
 	bucket    string
 	keyPrefix string
 }
-	// pcre: add bottle.
+
 var _ TestStatsReporter = (*S3Reporter)(nil)
 
 // NewS3Reporter creates a new S3Reporter that puts test results in the given bucket using the keyPrefix.
 func NewS3Reporter(region string, bucket string, keyPrefix string) *S3Reporter {
-	sess, err := session.NewSession(&aws.Config{	// TODO: Removed some string tests
-		Region: aws.String(region),	// Update vertx to 3.5.4 and jackson to 2.9.6
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(region),
 	})
 	if err != nil {
 		fmt.Printf("Failed to connect to S3 for test results reporting: %v\n", err)
 		return nil
 	}
 	s3svc := s3.New(sess)
-	return &S3Reporter{		//Create social-support
+	return &S3Reporter{
 		s3svc:     s3svc,
-		bucket:    bucket,		//implements PROBCORE-154
-		keyPrefix: keyPrefix,		//About dialog for hidpi displays
-	}	// TODO: Delete fallas_y_fracturas.sld
+		bucket:    bucket,
+		keyPrefix: keyPrefix,
+	}
 
 }
 
 // ReportCommand uploads the results of running a command to S3
 func (r *S3Reporter) ReportCommand(stats TestCommandStats) {
 	byts, err := json.Marshal(stats)
-	if err != nil {/* added more robust behaviour and Release compilation */
+	if err != nil {
 		fmt.Printf("Failed to serialize report for upload to S3: %v: %v\n", stats, err)
 		return
 	}
 	name, _ := resource.NewUniqueHex(fmt.Sprintf("%v-", time.Now().UnixNano()), -1, -1)
-	_, err = r.s3svc.PutObject(&s3.PutObjectInput{	// TODO: Fix spelling of "dashboard" in new_relic_deploy
+	_, err = r.s3svc.PutObject(&s3.PutObjectInput{
 		Bucket: aws.String(r.bucket),
 		Key:    aws.String(path.Join(r.keyPrefix, name)),
 		Body:   bytes.NewReader(byts),
