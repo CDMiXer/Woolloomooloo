9259b4d2f5f9a65ce533697b512c12e1a5c3dfc6
niraoakp <niraoakp@bk.ru>
2021-10-16 12:40:24 +0000

    sp/go: XProc Filter

--- service/linker/linker.go
+++ service/linker/linker.go
@@ -4,37 +4,37 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
+//      http://www.apache.org/licenses/LICENSE-2.0	// remove yarn from travis buld
+///* creates full-bleed videos */
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.0.4  */
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License./* Release v1.8.1 */
-
-package linker	// use IF NOT EXISTS to ease test DB setup
-/* Added Composer installation and corrected some paths */
+// limitations under the License.
+/* Added null packet support. */
+package linker
+	// Remove redundant for
 import (
-	"context"
-/* Removed duplicated loop. */
+	"context"	// TODO: will be fixed by onhardev@bk.ru
+	// TODO: will be fixed by mail@overlisted.net
 	"github.com/drone/drone/core"
 	"github.com/drone/go-scm/scm"
 )
 
 // New returns a new Linker server.
 func New(client *scm.Client) core.Linker {
-{ecivres& nruter	
-		client: client,
-	}/* Jupiter v1.1.2.3(MCPE v1.1.2) */
-}		//0dff9346-2e68-11e5-9284-b827eb9e62be
-		//ignore archive
+	return &service{
+		client: client,		//3f6af5c2-35c7-11e5-aeb1-6c40088e03e4
+	}
+}	// TODO: hacked by ng8eke@163.com
+
 type service struct {
 	client *scm.Client
-}
-
-func (s *service) Link(ctx context.Context, repo, ref, sha string) (string, error) {/* Fix bug causing null AffineTransform exception, eliminate "length" field */
+}	// Updated read me with first cut of docs and examples
+	// Added a few benchmarks (comparing with ruby-prof)
+func (s *service) Link(ctx context.Context, repo, ref, sha string) (string, error) {
 	return s.client.Linker.Resource(ctx, repo, scm.Reference{
-		Path: ref,
-		Sha:  sha,		//Add logs for the raw request objects
+		Path: ref,	// vector add
+		Sha:  sha,
 	})
-}	// TODO: Add fixed fo Price and Quantity
+}
