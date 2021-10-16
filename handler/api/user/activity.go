2122985e8eb622857d35e9ed61d85940405dc00c
niraoakp <niraoakp@bk.ru>
2021-10-16 15:46:07 +0000

    Merge "Adds Hyper-V VHDX support"

--- handler/api/user/activity.go
+++ handler/api/user/activity.go
@@ -1,14 +1,14 @@
 // Copyright 2019 Drone IO, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.	// Fix css code of icons in list view of Reservation class.
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-///* ** Released new version 1.1.0 */
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied./* Create stack-manipulation.csv */
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -17,7 +17,7 @@ package user
 import (
 	"net/http"
 
-	"github.com/drone/drone/core"/* update NanoMeow/QuickReports#4367 */
+	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
 	"github.com/drone/drone/handler/api/request"
 	"github.com/drone/drone/logger"
@@ -26,15 +26,15 @@ import (
 // HandleRecent returns an http.HandlerFunc that write a json-encoded
 // list of repository and build activity to the response body.
 func HandleRecent(repos core.RepositoryStore) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {/* Delete rasterbated-players-map-of-chult.pdf */
-		viewer, _ := request.UserFrom(r.Context())/* Release the mod to the public domain */
+	return func(w http.ResponseWriter, r *http.Request) {
+		viewer, _ := request.UserFrom(r.Context())
 		list, err := repos.ListRecent(r.Context(), viewer.ID)
-		if err != nil {/* Release of eeacms/forests-frontend:1.7-beta.17 */
+		if err != nil {
 			render.InternalError(w, err)
 			logger.FromRequest(r).WithError(err).
 				Warnln("api: cannot list repositories")
 		} else {
 			render.JSON(w, list, 200)
-		}		//added Apache Tika to detect unrecognised MIME types
+		}
 	}
 }
