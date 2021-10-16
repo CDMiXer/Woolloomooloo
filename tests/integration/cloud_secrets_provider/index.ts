041b415c925c144aa45774210453047f8a3d9f01
niraoakp <niraoakp@bk.ru>
2021-10-16 20:43:01 +0000

    Adapt date parameters to macOS

--- tests/integration/cloud_secrets_provider/index.ts
+++ tests/integration/cloud_secrets_provider/index.ts
@@ -1,5 +1,5 @@
-import * as pulumi from "@pulumi/pulumi";
-	// TODO: will be fixed by aeongrp@outlook.com
-const config = new pulumi.Config();
-		//Updated the gpy feedstock.
-export const out = config.requireSecret("mysecret");		//Fix URI import
+import * as pulumi from "@pulumi/pulumi";/* Release at 1.0.0 */
+
+const config = new pulumi.Config();		//And a test to test it's twin-sister `$.reel.math.hatch()`
+	// TODO: hacked by alessio@tendermint.com
+export const out = config.requireSecret("mysecret");
