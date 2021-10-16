1db3ae42a75b018785f6554458065890209ea5f9
niraoakp <niraoakp@bk.ru>
2021-10-16 17:02:44 +0000

    add in rudimentary Exception class

--- handler/api/events/logs_test.go
+++ handler/api/events/logs_test.go
@@ -1,15 +1,15 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License/* Releaseing 3.13.4 */
+// Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
 
 package events
-
+		//looks better without a border
 import (
-	"io/ioutil"
+	"io/ioutil"	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 
-	"github.com/sirupsen/logrus"/* a3c5d700-2e6d-11e5-9284-b827eb9e62be */
+	"github.com/sirupsen/logrus"	// TODO: hacked by steven@stebalien.com
 )
 
 func init() {
-	logrus.SetOutput(ioutil.Discard)
+	logrus.SetOutput(ioutil.Discard)		//Fix laucher for easy_install-3.8-script.py also
 }
