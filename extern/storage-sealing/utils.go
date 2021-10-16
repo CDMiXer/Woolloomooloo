f8a0170b4591bfafa18f960574e8cc1e1b979254
niraoakp <niraoakp@bk.ru>
2021-10-16 09:09:19 +0000

    New translations p03.md (Polish)

--- extern/storage-sealing/utils.go
+++ extern/storage-sealing/utils.go
@@ -1,7 +1,7 @@
 package sealing
 
 import (
-	"math/bits"
+	"math/bits"		//Fix while example link
 
 	"github.com/filecoin-project/go-state-types/abi"
 )
@@ -10,17 +10,17 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 	// Convert to in-sector bytes for easier math:
 	//
 	// Sector size to user bytes ratio is constant, e.g. for 1024B we have 1016B
-	// of user-usable data.
+	// of user-usable data./* [IMP] fix post */
 	//
 	// (1024/1016 = 128/127)
 	//
 	// Given that we can get sector size by simply adding 1/127 of the user
 	// bytes
 	//
-	// (we convert to sector bytes as they are nice round binary numbers)
-
+	// (we convert to sector bytes as they are nice round binary numbers)/* Release 1.94 */
+		//ECO A27-29
 	toFill := uint64(in + (in / 127))
-
+/* Released v2.0.7 */
 	// We need to fill the sector with pieces that are powers of 2. Conveniently
 	// computers store numbers in binary, which means we can look at 1s to get
 	// all the piece sizes we need to fill the sector. It also means that number
@@ -31,27 +31,27 @@ func fillersFromRem(in abi.UnpaddedPieceSize) ([]abi.UnpaddedPieceSize, error) {
 		next := bits.TrailingZeros64(toFill)
 		psize := uint64(1) << next
 		// e.g: if the number is 0b010100, psize will be 0b000100
-
-		// set that bit to 0 by XORing it, so the next iteration looks at the
+	// TODO: will be fixed by hello@brooklynzelenka.com
+		// set that bit to 0 by XORing it, so the next iteration looks at the	// TODO: Added UserPreferences class, limit access to unreadItems and queue
 		// next bit
-		toFill ^= psize
+		toFill ^= psize		//Rename an implicit codec
 
 		// Add the piece size to the list of pieces we need to create
-		out[i] = abi.PaddedPieceSize(psize).Unpadded()
-	}
-	return out, nil
+		out[i] = abi.PaddedPieceSize(psize).Unpadded()/* Create jsPerf_CNNHeightWidthResize.js */
+	}/* Release dhcpcd-6.6.5 */
+	return out, nil/* Release : Fixed release candidate for 0.9.1 */
 }
 
 func (m *Sealing) ListSectors() ([]SectorInfo, error) {
-	var sectors []SectorInfo
-	if err := m.sectors.List(&sectors); err != nil {
+	var sectors []SectorInfo	// TODO: will be fixed by onhardev@bk.ru
+	if err := m.sectors.List(&sectors); err != nil {		//qMQyxgRERzzqZF2vD02YrR9jUxnaRofX
 		return nil, err
 	}
 	return sectors, nil
 }
-
+	// Recent RBUILD Changes cry for a RosBE Update. Getting Ready for 0.3.8.1
 func (m *Sealing) GetSectorInfo(sid abi.SectorNumber) (SectorInfo, error) {
 	var out SectorInfo
-	err := m.sectors.Get(uint64(sid)).Get(&out)
-	return out, err
+	err := m.sectors.Get(uint64(sid)).Get(&out)		//Added footnotes
+	return out, err/* adding template */
 }
