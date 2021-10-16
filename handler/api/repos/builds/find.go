bdc9e49021e9607346bfec3d395447adf22cfaed
niraoakp <niraoakp@bk.ru>
2021-10-16 17:40:18 +0000

    Flush writes to main log file

--- handler/api/repos/builds/find.go
+++ handler/api/repos/builds/find.go
@@ -1,9 +1,9 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc./* Interim commit of backup, truncate and restore data function. */
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// you may not use this file except in compliance with the License./* Release 0.95.164: fixed toLowerCase anomalies */
 // You may obtain a copy of the License at
-//
+///* Update section ReleaseNotes. */
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -15,26 +15,26 @@
 package builds
 
 import (
-	"net/http"
+	"net/http"/* clean marssurvive init */
 	"strconv"
-
+		//Added information about future and maintenance
 	"github.com/drone/drone/core"
-	"github.com/drone/drone/handler/api/render"
+	"github.com/drone/drone/handler/api/render"		//main: turn notifications off for the alpha release
 
 	"github.com/go-chi/chi"
 )
 
 // HandleFind returns an http.HandlerFunc that writes json-encoded
 // build details to the the response body.
-func HandleFind(
+func HandleFind(		//Framework uses @rpath, fixed lib header path.
 	repos core.RepositoryStore,
 	builds core.BuildStore,
 	stages core.StageStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (
+		var (	// TODO: hacked by davidad@alum.mit.edu
 			namespace = chi.URLParam(r, "owner")
-			name      = chi.URLParam(r, "name")
+			name      = chi.URLParam(r, "name")	// TODO: Fixed bg color
 		)
 		number, err := strconv.ParseInt(chi.URLParam(r, "number"), 10, 64)
 		if err != nil {
@@ -45,7 +45,7 @@ func HandleFind(
 		if err != nil {
 			render.NotFound(w, err)
 			return
-		}
+		}/* Delete SuperWaveGeneratorAudioSource.h */
 		build, err := builds.FindNumber(r.Context(), repo.ID, number)
 		if err != nil {
 			render.NotFound(w, err)
@@ -54,13 +54,13 @@ func HandleFind(
 		stages, err := stages.ListSteps(r.Context(), build.ID)
 		if err != nil {
 			render.InternalError(w, err)
-			return
+			return		//Delete KeyDisplay.csproj.FileListAbsolute.txt
 		}
-		render.JSON(w, &buildWithStages{build, stages}, 200)
+		render.JSON(w, &buildWithStages{build, stages}, 200)/* Unfinished SimPropRegistry */
 	}
 }
 
 type buildWithStages struct {
-	*core.Build
+	*core.Build	// TODO: 26f8d47e-2e69-11e5-9284-b827eb9e62be
 	Stages []*core.Stage `json:"stages,omitempty"`
 }
