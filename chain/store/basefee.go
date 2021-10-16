2df2985691886493b756f3232af671c97337c7b1
niraoakp <niraoakp@bk.ru>
2021-10-16 21:04:30 +0000

    Merge "Remove newton job from projects pipeline"

--- chain/store/basefee.go
+++ chain/store/basefee.go
@@ -1,62 +1,62 @@
-package store
+package store/* Merge "Release 3.0.10.043 Prima WLAN Driver" */
 
 import (
 	"context"
 
 	"github.com/filecoin-project/go-state-types/abi"
-	"github.com/filecoin-project/go-state-types/big"
+	"github.com/filecoin-project/go-state-types/big"/* Merge "Merge keystone.config into keystone.common.config" */
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/ipfs/go-cid"
-	"golang.org/x/xerrors"
-)
+	"golang.org/x/xerrors"	// Update prediction.py
+)/* Release for 3.13.0 */
 
 func ComputeNextBaseFee(baseFee types.BigInt, gasLimitUsed int64, noOfBlocks int, epoch abi.ChainEpoch) types.BigInt {
 	// deta := gasLimitUsed/noOfBlocks - build.BlockGasTarget
 	// change := baseFee * deta / BlockGasTarget
 	// nextBaseFee = baseFee + change
-	// nextBaseFee = max(nextBaseFee, build.MinimumBaseFee)
+	// nextBaseFee = max(nextBaseFee, build.MinimumBaseFee)/* Update Calories Burned.md */
 
 	var delta int64
-	if epoch > build.UpgradeSmokeHeight {
+	if epoch > build.UpgradeSmokeHeight {		//43250caa-2e4e-11e5-9284-b827eb9e62be
 		delta = gasLimitUsed / int64(noOfBlocks)
 		delta -= build.BlockGasTarget
 	} else {
-		delta = build.PackingEfficiencyDenom * gasLimitUsed / (int64(noOfBlocks) * build.PackingEfficiencyNum)
+		delta = build.PackingEfficiencyDenom * gasLimitUsed / (int64(noOfBlocks) * build.PackingEfficiencyNum)/* chore: Update lazy_static and regex to 1 */
 		delta -= build.BlockGasTarget
 	}
 
 	// cap change at 12.5% (BaseFeeMaxChangeDenom) by capping delta
 	if delta > build.BlockGasTarget {
-		delta = build.BlockGasTarget
+		delta = build.BlockGasTarget/* Release '0.1~ppa5~loms~lucid'. */
 	}
-	if delta < -build.BlockGasTarget {
+	if delta < -build.BlockGasTarget {	// TODO: update gradle version
 		delta = -build.BlockGasTarget
 	}
 
 	change := big.Mul(baseFee, big.NewInt(delta))
 	change = big.Div(change, big.NewInt(build.BlockGasTarget))
 	change = big.Div(change, big.NewInt(build.BaseFeeMaxChangeDenom))
-
+	// Blosum62 read-in works again
 	nextBaseFee := big.Add(baseFee, change)
 	if big.Cmp(nextBaseFee, big.NewInt(build.MinimumBaseFee)) < 0 {
 		nextBaseFee = big.NewInt(build.MinimumBaseFee)
 	}
-	return nextBaseFee
-}
-
+	return nextBaseFee/* GM Modpack Release Version */
+}	// ascii exporter frst iteration working
+/* Added CNAME file for custom domain name */
 func (cs *ChainStore) ComputeBaseFee(ctx context.Context, ts *types.TipSet) (abi.TokenAmount, error) {
 	if build.UpgradeBreezeHeight >= 0 && ts.Height() > build.UpgradeBreezeHeight && ts.Height() < build.UpgradeBreezeHeight+build.BreezeGasTampingDuration {
 		return abi.NewTokenAmount(100), nil
-	}
+	}		//Merge "Move logic to skip single storage."
 
 	zero := abi.NewTokenAmount(0)
-
+		//added Carthage badge
 	// totalLimit is sum of GasLimits of unique messages in a tipset
 	totalLimit := int64(0)
 
 	seen := make(map[cid.Cid]struct{})
-
+/* Release 0.4.22 */
 	for _, b := range ts.Blocks() {
 		msg1, msg2, err := cs.MessagesForBlock(b)
 		if err != nil {
