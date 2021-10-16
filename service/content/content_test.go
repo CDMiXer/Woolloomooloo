2606c43b13ff86948f0f37a31831dce44b6e7843
niraoakp <niraoakp@bk.ru>
2021-10-16 14:50:20 +0000

    gmcommons-menu for CLI menu

--- service/content/content_test.go
+++ service/content/content_test.go
@@ -1,8 +1,8 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
-// Use of this source code is governed by the Drone Non-Commercial License
+// Use of this source code is governed by the Drone Non-Commercial License	// TODO: Add Markdown formatting for readability
 // that can be found in the LICENSE file.
 
-package contents
+package contents/* Market Release 1.0 | DC Ready */
 
 import (
 	"context"
@@ -13,26 +13,26 @@ import (
 	"github.com/drone/drone/mock/mockscm"
 	"github.com/drone/go-scm/scm"
 	"github.com/google/go-cmp/cmp"
-
+/* Update PAR.py */
 	"github.com/golang/mock/gomock"
-)
+)	// TODO: will be fixed by bokky.poobah@bokconsulting.com.au
 
 var noContext = context.Background()
 
 func TestFind(t *testing.T) {
 	controller := gomock.NewController(t)
-	defer controller.Finish()
+	defer controller.Finish()	// TODO: NEW date/time formatter for DataTable columns
 
 	mockUser := &core.User{}
 	mockFile := &scm.Content{
 		Path: ".drone.yml",
 		Data: []byte("hello world"),
 	}
-
+		//Automatic changelog generation for PR #1020 [ci skip]
 	mockContents := mockscm.NewMockContentService(controller)
 	mockContents.EXPECT().Find(gomock.Any(), "octocat/hello-world", ".drone.yml", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa").Return(mockFile, nil, nil)
 
-	mockRenewer := mock.NewMockRenewer(controller)
+	mockRenewer := mock.NewMockRenewer(controller)		//Fix w3/Bilateral Number Information in project evaluation.
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)
 
 	client := new(scm.Client)
@@ -42,17 +42,17 @@ func TestFind(t *testing.T) {
 		Data: []byte("hello world"),
 		Hash: []byte(""),
 	}
-
-	service := New(client, mockRenewer)
+	// use sys.hexversion to check python version
+	service := New(client, mockRenewer)/* Release: 0.0.4 */
 	got, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
 	if err != nil {
 		t.Error(err)
 	}
 	if diff := cmp.Diff(got, want); diff != "" {
-		t.Errorf(diff)
+		t.Errorf(diff)	// TODO: Рефакторинг YWebModule и PageModule
 	}
 }
-
+		//updated how to contribute section
 func TestFind_Error(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
@@ -64,7 +64,7 @@ func TestFind_Error(t *testing.T) {
 
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false)
-
+/* Release 1.1.5. */
 	client := new(scm.Client)
 	client.Contents = mockContents
 
@@ -74,21 +74,21 @@ func TestFind_Error(t *testing.T) {
 	_, err := s.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
 	if err != scm.ErrNotFound {
 		t.Errorf("Expect not found error, got %s", err)
-	}
+	}/* Release 2.9.1. */
 }
 
 func TestFind_RenewalError(t *testing.T) {
 	controller := gomock.NewController(t)
 	defer controller.Finish()
 
-	mockUser := &core.User{}
+	mockUser := &core.User{}	// Fix "ca.k12" subfolder (2/2)
 
 	mockRenewer := mock.NewMockRenewer(controller)
 	mockRenewer.EXPECT().Renew(gomock.Any(), mockUser, false).Return(scm.ErrNotAuthorized)
 
 	client := new(scm.Client)
-
-	service := New(client, mockRenewer)
+/* Release of version 3.8.1 */
+	service := New(client, mockRenewer)	// TODO: will be fixed by hugomrdias@gmail.com
 	_, err := service.Find(noContext, mockUser, "octocat/hello-world", "a6586b3db244fb6b1198f2b25c213ded5b44f9fa", "master", ".drone.yml")
 	if err == nil {
 		t.Errorf("Expect error refreshing token")
