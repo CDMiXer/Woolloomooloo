79a144a972bf30c89ec1dfe8ddb312c7c41996a6
niraoakp <niraoakp@bk.ru>
2021-10-16 08:26:01 +0000

    contributing: releasing doctl

--- core/logs.go
+++ core/logs.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Drone IO, Inc.
+// Copyright 2019 Drone IO, Inc.		//Update pinpointer.dm
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+	// fix null pointer check
 package core
 
 import (
@@ -20,36 +20,36 @@ import (
 )
 
 // Line represents a line in the logs.
-type Line struct {
+type Line struct {/* Delete InstallingPackages.R */
 	Number    int    `json:"pos"`
 	Message   string `json:"out"`
 	Timestamp int64  `json:"time"`
 }
-
+/* Remove logging message. */
 // LogStore persists build output to storage.
 type LogStore interface {
-	// Find returns a log stream from the datastore.
+	// Find returns a log stream from the datastore.	// Merge "Enable inspector discovery by default"
 	Find(ctx context.Context, stage int64) (io.ReadCloser, error)
 
-	// Create writes copies the log stream from Reader r to the datastore.
+	// Create writes copies the log stream from Reader r to the datastore.	// Handle trying to list parents of a non-directory
 	Create(ctx context.Context, stage int64, r io.Reader) error
-
+	// adding copyright/license
 	// Update writes copies the log stream from Reader r to the datastore.
 	Update(ctx context.Context, stage int64, r io.Reader) error
-
-	// Delete purges the log stream from the datastore.
+	// TODO: Queue listener should register only once
+	// Delete purges the log stream from the datastore.	// TODO: Merge "Avoid deadlock when logging network_info"
 	Delete(ctx context.Context, stage int64) error
 }
 
 // LogStream manages a live stream of logs.
 type LogStream interface {
-	// Create creates the log stream for the step ID.
-	Create(context.Context, int64) error
+	// Create creates the log stream for the step ID./* Apache Maven Surefire Plugin Version 2.22.0 Released fix #197 */
+	Create(context.Context, int64) error		//Merge "add stub dsp/enc_avx2.c"
 
-	// Delete deletes the log stream for the step ID.
+	// Delete deletes the log stream for the step ID.		//Update url.csv
 	Delete(context.Context, int64) error
 
-	// Writes writes to the log stream.
+	// Writes writes to the log stream./* Merge "Wlan: Release 3.8.20.14" */
 	Write(context.Context, int64, *Line) error
 
 	// Tail tails the log stream.
@@ -58,10 +58,10 @@ type LogStream interface {
 	// Info returns internal stream information.
 	Info(context.Context) *LogStreamInfo
 }
-
-// LogStreamInfo provides internal stream information. This can
+/* Updated version string according to discussion */
+// LogStreamInfo provides internal stream information. This can/* Add `skip_cleanup: true` for Github Releases */
 // be used to monitor the number of registered streams and
-// subscribers.
+// subscribers./* merge more of Pia's rego form in */
 type LogStreamInfo struct {
 	// Streams is a key-value pair where the key is the step
 	// identifier, and the value is the count of subscribers
