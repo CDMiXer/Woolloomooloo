759fca91dd466b6b5893124226b842cfda761c43
niraoakp <niraoakp@bk.ru>
2021-10-16 22:37:17 +0000

    Bump the version of python

--- handler/api/repos/builds/branches/delete_test.go
+++ handler/api/repos/builds/branches/delete_test.go
@@ -1,5 +1,5 @@
 // Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
-// that can be found in the LICENSE file.
-
-package branches
+// that can be found in the LICENSE file./* Merge "Adding Job Types support to CLI" */
+		//Improved method to get the liaison users.
+package branches/* Delete 1face.jpg */
