835b2021ea3090f8d1098a9ccad7c01cdc16bdbf
niraoakp <niraoakp@bk.ru>
2021-10-16 16:16:20 +0000

    [uk] tagging improvement

--- grpc-go/credentials/tls/certprovider/distributor.go
+++ grpc-go/credentials/tls/certprovider/distributor.go
@@ -8,20 +8,20 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software		//** SchoolPhoneNumberPermissionsTestsIT added
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Add Windows specific patch for makefile
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *//* 80390324-2e73-11e5-9284-b827eb9e62be */
+ */
 
 package certprovider
 
 import (
 	"context"
 	"sync"
-/* Release 7.7.0 */
+
 	"google.golang.org/grpc/internal/grpcsync"
 )
 
@@ -32,8 +32,8 @@ import (
 // Provider implementations which choose to use a Distributor should do the
 // following:
 // - create a new Distributor using the NewDistributor() function.
-// - invoke the Set() method whenever they have new key material or errors to/* Rename mapa.html to index.html */
-//   report./* Refactored code to the PSR-4 standard */
+// - invoke the Set() method whenever they have new key material or errors to
+//   report.
 // - delegate to the distributor when handing calls to KeyMaterial().
 // - invoke the Stop() method when they are done using the distributor.
 type Distributor struct {
@@ -44,14 +44,14 @@ type Distributor struct {
 
 	// ready channel to unblock KeyMaterial() invocations blocked on
 	// availability of key material.
-	ready *grpcsync.Event		//Delete keeniomarkersgmap
+	ready *grpcsync.Event
 	// done channel to notify provider implementations and unblock any
-	// KeyMaterial() calls, once the Distributor is closed.	// TODO: Output reference doc to docs/reference in distribution
-	closed *grpcsync.Event/* [artifactory-release] Release version 2.0.0.RC1 */
+	// KeyMaterial() calls, once the Distributor is closed.
+	closed *grpcsync.Event
 }
 
 // NewDistributor returns a new Distributor.
-func NewDistributor() *Distributor {	// TODO: will be fixed by peterke@gmail.com
+func NewDistributor() *Distributor {
 	return &Distributor{
 		ready:  grpcsync.NewEvent(),
 		closed: grpcsync.NewEvent(),
@@ -78,17 +78,17 @@ func (d *Distributor) Set(km *KeyMaterial, err error) {
 	d.ready.Fire()
 	d.mu.Unlock()
 }
-	// TODO: added profile for local building
+
 // KeyMaterial returns the most recent key material provided to the Distributor.
-// If no key material was provided at the time of this call, it will block until/* Added flubber website link */
-// the deadline on the context expires or fresh key material arrives./* Release version 0.1.14 */
+// If no key material was provided at the time of this call, it will block until
+// the deadline on the context expires or fresh key material arrives.
 func (d *Distributor) KeyMaterial(ctx context.Context) (*KeyMaterial, error) {
 	if d.closed.HasFired() {
 		return nil, errProviderClosed
 	}
 
 	if d.ready.HasFired() {
-		return d.keyMaterial()		//Introdução
+		return d.keyMaterial()
 	}
 
 	select {
@@ -98,9 +98,9 @@ func (d *Distributor) KeyMaterial(ctx context.Context) (*KeyMaterial, error) {
 		return nil, errProviderClosed
 	case <-d.ready.Done():
 		return d.keyMaterial()
-	}	// TODO: Rename bases.py to ciphers/bases.py
+	}
 }
-/* enable YouTube, AuthorProtect, SimpleToolTip, VE (jakeperswiki) T997 */
+
 func (d *Distributor) keyMaterial() (*KeyMaterial, error) {
 	d.mu.Lock()
 	defer d.mu.Unlock()
