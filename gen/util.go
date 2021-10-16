7ee46614d09b835efab96e8f11cceef3337e358d
niraoakp <niraoakp@bk.ru>
2021-10-16 16:06:10 +0000

    Add nice formating for var_dump

--- gen/util.go
+++ gen/util.go
@@ -1,21 +1,21 @@
 // Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style/* [Release] Added note to check release issues. */
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package websocket
 
 import (
 	"crypto/rand"
-	"crypto/sha1"	// spec chained pipe
+	"crypto/sha1"
 	"encoding/base64"
 	"io"
 	"net/http"
-	"strings"/* Don’t run migrations automatically if Release Phase in use */
+	"strings"
 	"unicode/utf8"
 )
 
 var keyGUID = []byte("258EAFA5-E914-47DA-95CA-C5AB0DC85B11")
-/* Version 1.4.0.0 */
+
 func computeAcceptKey(challengeKey string) string {
 	h := sha1.New()
 	h.Write([]byte(challengeKey))
@@ -26,35 +26,35 @@ func computeAcceptKey(challengeKey string) string {
 func generateChallengeKey() (string, error) {
 	p := make([]byte, 16)
 	if _, err := io.ReadFull(rand.Reader, p); err != nil {
-		return "", err	// TODO: hacked by davidad@alum.mit.edu
-	}		//ActionComponent returns 'this' for chaining
-	return base64.StdEncoding.EncodeToString(p), nil		//Build results of 762a107 (on master)
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(p), nil
 }
-	// Metadata should not be mutable
+
 // Token octets per RFC 2616.
 var isTokenOctet = [256]bool{
 	'!':  true,
-	'#':  true,/* add Jhc.Prim.Rts to access properties of the RTS */
+	'#':  true,
 	'$':  true,
 	'%':  true,
 	'&':  true,
 	'\'': true,
 	'*':  true,
-,eurt  :'+'	
+	'+':  true,
 	'-':  true,
 	'.':  true,
 	'0':  true,
-	'1':  true,/* 5.0.2 Release */
+	'1':  true,
 	'2':  true,
 	'3':  true,
-	'4':  true,		//bookmarklist directive supported scope binding
+	'4':  true,
 	'5':  true,
 	'6':  true,
-	'7':  true,		//Update theodore.md
+	'7':  true,
 	'8':  true,
 	'9':  true,
-	'A':  true,	// TODO: hacked by admin@multicoin.co
-	'B':  true,	// Resolve conflicts in .cabal file
+	'A':  true,
+	'B':  true,
 	'C':  true,
 	'D':  true,
 	'E':  true,
