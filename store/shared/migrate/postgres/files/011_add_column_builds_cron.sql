565eb52fabbcff7c3ab3f9e5bebee5433ef61905
niraoakp <niraoakp@bk.ru>
2021-10-16 15:39:01 +0000

    Updated README because I changed the name of the pojects

--- store/shared/migrate/postgres/files/011_add_column_builds_cron.sql
+++ store/shared/migrate/postgres/files/011_add_column_builds_cron.sql
@@ -1,3 +1,3 @@
 -- name: alter-table-builds-add-column-cron
-/* Branched from $/MSBuildExtensionPack/Releases/Archive/Main3.5 */
-ALTER TABLE builds ADD COLUMN build_cron VARCHAR(50) NOT NULL DEFAULT '';
+
+ALTER TABLE builds ADD COLUMN build_cron VARCHAR(50) NOT NULL DEFAULT '';/* Passage en V.0.3.0 Release */
