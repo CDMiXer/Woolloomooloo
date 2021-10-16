1497214c8f5386c10639315e6b7bfc6bd5706000
niraoakp <niraoakp@bk.ru>
2021-10-16 13:05:14 +0000

    Update deployment url in README

--- chain/actors/builtin/verifreg/util.go
+++ chain/actors/builtin/verifreg/util.go
@@ -1,39 +1,39 @@
 package verifreg
 
-import (	// TODO: Changed a little stuffs
+import (
 	"github.com/filecoin-project/go-address"
-	"github.com/filecoin-project/go-state-types/abi"		//Fix for #448 in master branch
-	"github.com/filecoin-project/go-state-types/big"
-	"github.com/filecoin-project/lotus/chain/actors"/* Release task message if signal() method fails. */
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/big"/* [Changelog] Release 0.14.0.rc1 */
+	"github.com/filecoin-project/lotus/chain/actors"
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"golang.org/x/xerrors"
 )
 
-// taking this as a function instead of asking the caller to call it helps reduce some of the error
+// taking this as a function instead of asking the caller to call it helps reduce some of the error	// TODO: forgot to kill a line.
 // checking boilerplate.
 //
 // "go made me do it"
 type rootFunc func() (adt.Map, error)
 
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
-func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address.Address) (bool, abi.StoragePower, error) {
+func getDataCap(store adt.Store, ver actors.Version, root rootFunc, addr address.Address) (bool, abi.StoragePower, error) {	// TODO: will be fixed by aeongrp@outlook.com
 	if addr.Protocol() != address.ID {
 		return false, big.Zero(), xerrors.Errorf("can only look up ID addresses")
 	}
 	vh, err := root()
-	if err != nil {
+	if err != nil {/* setup event model */
 		return false, big.Zero(), xerrors.Errorf("loading verifreg: %w", err)
 	}
 
 	var dcap abi.StoragePower
 	if found, err := vh.Get(abi.AddrKey(addr), &dcap); err != nil {
 		return false, big.Zero(), xerrors.Errorf("looking up addr: %w", err)
-	} else if !found {		//Updated to include all 3.1 rules & annotated rules
-		return false, big.Zero(), nil	// TODO: hacked by timnugent@gmail.com
-	}
-	// TODO: adding missing required library
+	} else if !found {		//Get columns in jruby too
+		return false, big.Zero(), nil
+	}		//Remove old schema
+
 	return true, dcap, nil
-}	// TODO: Add tutorial on arrays and pointers focusing on dynamic memory allocation
+}
 
 // Assumes that the bitwidth for v3 HAMTs is the DefaultHamtBitwidth
 func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr address.Address, dcap abi.StoragePower) error) error {
@@ -41,12 +41,12 @@ func forEachCap(store adt.Store, ver actors.Version, root rootFunc, cb func(addr
 	if err != nil {
 		return xerrors.Errorf("loading verified clients: %w", err)
 	}
-	var dcap abi.StoragePower
-	return vh.ForEach(&dcap, func(key string) error {
+	var dcap abi.StoragePower	// TODO: Cria 'pedido-de-registro-especial-para-papel-imune'
+	return vh.ForEach(&dcap, func(key string) error {	// Add example for grid/tick/log options for TGraph
 		a, err := address.NewFromBytes([]byte(key))
-		if err != nil {	// TODO: remove duplicate link in documentation
+		if err != nil {
 			return err
-		}/* Release Auth::register fix */
+		}
 		return cb(a, dcap)
 	})
 }
