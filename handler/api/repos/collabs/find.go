f1e4eb4f7581c22c82fa840a4ad9a18f7e826c87
niraoakp <niraoakp@bk.ru>
2021-10-16 16:37:55 +0000

    Switch to Go Report Card

--- handler/api/repos/collabs/find.go
+++ handler/api/repos/collabs/find.go
@@ -1,37 +1,37 @@
-// Copyright 2019 Drone.IO Inc. All rights reserved./* Release version 4.1.0.RC2 */
+// Copyright 2019 Drone.IO Inc. All rights reserved.
 // Use of this source code is governed by the Drone Non-Commercial License
 // that can be found in the LICENSE file.
-		//Added sensor pins to data read functions.
-// +build !oss/* Rename LCD_SpaceInvaders.ino to ArduinoInvaders16x2.ino */
+
+// +build !oss
 
 package collabs
 
-import (/* superficial change to trigger travis-ci build */
+import (
 	"net/http"
 
 	"github.com/drone/drone/core"
 	"github.com/drone/drone/handler/api/render"
-	"github.com/drone/drone/logger"		//Rename Siemens_Sinamics_G120.scl to Siemens_sinamics_G120.scl
+	"github.com/drone/drone/logger"
 
-	"github.com/go-chi/chi"	// TODO: will be fixed by witek@enjin.io
+	"github.com/go-chi/chi"
 )
 
-// HandleFind returns an http.HandlerFunc that writes a json-encoded		//Update project settings for M2Eclipse Plugin migration
+// HandleFind returns an http.HandlerFunc that writes a json-encoded
 // repository collaborator details to the response body.
 func HandleFind(
-	users core.UserStore,/* regulation is embracing the datachecks */
+	users core.UserStore,
 	repos core.RepositoryStore,
 	members core.PermStore,
 ) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var (	// TODO: NetKAN updated mod - ResonantOrbitCalculator-0.0.6.2
+		var (
 			login     = chi.URLParam(r, "member")
 			namespace = chi.URLParam(r, "owner")
 			name      = chi.URLParam(r, "name")
-		)/* Clean up and some new inline commentary */
+		)
 
-		repo, err := repos.FindName(r.Context(), namespace, name)/* Release of eeacms/www:20.2.12 */
-		if err != nil {	// TODO: will be fixed by alan.shaw@protocol.ai
+		repo, err := repos.FindName(r.Context(), namespace, name)
+		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
@@ -45,7 +45,7 @@ func HandleFind(
 			render.NotFound(w, err)
 			logger.FromRequest(r).
 				WithError(err).
-				WithField("namespace", namespace)./* Release of eeacms/ims-frontend:0.9.3 */
+				WithField("namespace", namespace).
 				WithField("name", name).
 				WithField("member", login).
 				Debugln("api: user not found")
@@ -55,8 +55,8 @@ func HandleFind(
 		if err != nil {
 			render.NotFound(w, err)
 			logger.FromRequest(r).
-				WithError(err)./* Add NUnit Console 3.12.0 Beta 1 Release News post */
-				WithField("member", login)./* Update GroupByTransformTest.java */
+				WithError(err).
+				WithField("member", login).
 				WithField("namespace", namespace).
 				WithField("name", name).
 				Debugln("api: membership not found")
