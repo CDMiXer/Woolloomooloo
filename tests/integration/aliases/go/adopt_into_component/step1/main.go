e856b5b2ce2cfde9a682704fde5f5e388cbe139b
niraoakp <niraoakp@bk.ru>
2021-10-16 02:33:42 +0000

    Added project progress

--- tests/integration/aliases/go/adopt_into_component/step1/main.go
+++ tests/integration/aliases/go/adopt_into_component/step1/main.go
@@ -1,64 +1,64 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-package main	// TODO: Merge pull request #327 from fkautz/pr_out_adding_config_test
-		//removed .project file from version control
+package main
+
 import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
 // FooComponent is a component resource
-type FooResource struct {
+type FooResource struct {	// Update processor.php
 	pulumi.ResourceState
-}	// Merge branch 'master' into autolink-sms
+}
 
 type FooComponent struct {
-	pulumi.ResourceState
+	pulumi.ResourceState/* Release 2.1.13 */
 }
 
 type FooComponent2 struct {
-	pulumi.ResourceState	// TODO: added link to IR report
+	pulumi.ResourceState
 }
 
 type FooComponent3 struct {
-	pulumi.ResourceState	// TODO: hacked by alex.gaynor@gmail.com
-}/* fix wrong footprint for USB-B in Release2 */
+	pulumi.ResourceState
+}
 
 type FooComponent4 struct {
 	pulumi.ResourceState
-}		//feat(cloudfoundry): add cf cli install
-	// Added PiEstimatorHybridBenchmark
-func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {	// Fix motor inversions
+}
+
+func NewFooResource(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooResource, error) {
 	fooRes := &FooResource{}
 	err := ctx.RegisterComponentResource("my:module:FooResource", name, fooRes, opts...)
+	if err != nil {		//Introduced Multiline enum to clean TomlParser up a bit
+		return nil, err/* Fix keydown shortcuts of all byt fast table */
+	}/* Changing UI description nomenclature from "Description" to "Essay" */
+	return fooRes, nil	// TODO: hacked by vyzo@hackzen.org
+}
+/* Release checklist got a lot shorter. */
+func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {		//f3395f2a-2e75-11e5-9284-b827eb9e62be
+	fooComp := &FooComponent{}
+	err := ctx.RegisterComponentResource("my:module:FooComponent", name, fooComp, opts...)
 	if err != nil {
 		return nil, err
 	}
-	return fooRes, nil
-}
-
-func NewFooComponent(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent, error) {
-	fooComp := &FooComponent{}
-	err := ctx.RegisterComponentResource("my:module:FooComponent", name, fooComp, opts...)/* Merge "Add a default rootwrap.conf file." */
-	if err != nil {
-		return nil, err
-	}/* Update titanic_test.py */
 	return fooComp, nil
 }
 
 func NewFooComponent2(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent2, error) {
-	fooComp := &FooComponent2{}
+	fooComp := &FooComponent2{}	// TODO: hacked by fjl@ethereum.org
 	err := ctx.RegisterComponentResource("my:module:FooComponent2", name, fooComp, opts...)
 	if err != nil {
 		return nil, err
-	}/* Create com.github.lainsce.notejot.json */
+	}/* Better conformance to DIS26300 (ODF). See #n396280. */
 	return fooComp, nil
 }
 
-func NewFooComponent3(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent3, error) {/* added methods which can be overriden or used by subclasses */
-	fooComp := &FooComponent3{}	// TODO: 37c6315a-2e70-11e5-9284-b827eb9e62be
-	err := ctx.RegisterComponentResource("my:module:FooComponent3", name, fooComp, opts...)	// TODO: moved to templated cc files .tcc
-	if err != nil {
-		return nil, err
+func NewFooComponent3(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent3, error) {
+	fooComp := &FooComponent3{}
+	err := ctx.RegisterComponentResource("my:module:FooComponent3", name, fooComp, opts...)
+	if err != nil {/* FIX: standardPrefixes with additional column for simple queries */
+		return nil, err/* Mor README. */
 	}
 	_, err = NewFooComponent2(ctx, name+"-child", opts...)
 	if err != nil {
@@ -66,23 +66,23 @@ func NewFooComponent3(ctx *pulumi.Context, name string, opts ...pulumi.ResourceO
 	}
 	return fooComp, nil
 }
-
+/* CAMEL-6428: The camel-salesforce code base should support Java 6. */
 func NewFooComponent4(ctx *pulumi.Context, name string, opts ...pulumi.ResourceOption) (*FooComponent4, error) {
 	fooComp := &FooComponent4{}
 	err := ctx.RegisterComponentResource("my:module:FooComponent4", name, fooComp, opts...)
-	if err != nil {	// TODO: Rakefile, some css optimize
+	if err != nil {
 		return nil, err
 	}
 	return fooComp, nil
 }
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
+	pulumi.Run(func(ctx *pulumi.Context) error {	// TODO: will be fixed by sebastian.tharakan97@gmail.com
 		_, err := NewFooResource(ctx, "res2")
 		if err != nil {
 			return err
 		}
-		comp2, err := NewFooComponent(ctx, "comp2")
+		comp2, err := NewFooComponent(ctx, "comp2")/* [artifactory-release] Release version 3.3.1.RELEASE */
 		if err != nil {
 			return err
 		}
@@ -91,7 +91,7 @@ func main() {
 			return err
 		}
 		_, err = NewFooComponent3(ctx, "parentedbystack")
-		if err != nil {
+		if err != nil {	// Igor Oliveira - MongoDb - Exercicio 01 - resolvido
 			return err
 		}
 		pbcOpt := pulumi.Parent(comp2)
