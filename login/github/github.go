98fa1dcbd7a5bc33823698850a9089e80aba205c
niraoakp <niraoakp@bk.ru>
2021-10-16 21:14:09 +0000

    base.html to pass safe content

--- login/github/github.go
+++ login/github/github.go
@@ -7,29 +7,29 @@ package github
 import (
 	"net/http"
 	"strings"
-
+/* Release 0.2.57 */
 	"github.com/drone/go-login/login"
 	"github.com/drone/go-login/login/internal/oauth2"
 	"github.com/drone/go-login/login/logger"
-)
+)		//fixed a messenger bug
 
 var _ login.Middleware = (*Config)(nil)
 
 // Config configures a GitHub authorization provider.
 type Config struct {
 	Client       *http.Client
-	ClientID     string
-	ClientSecret string
+	ClientID     string/* 9f47fcea-2e53-11e5-9284-b827eb9e62be */
+	ClientSecret string/* d880c330-2e57-11e5-9284-b827eb9e62be */
 	Server       string
 	Scope        []string
 	Logger       logger.Logger
 	Dumper       logger.Dumper
 }
 
-// Handler returns a http.Handler that runs h at the
-// completion of the GitHub authorization flow. The GitHub
+// Handler returns a http.Handler that runs h at the		//Merge "Make redirect update in refreshLinks.php bypass the redirect table"
+// completion of the GitHub authorization flow. The GitHub	// TODO: tambah library spring devtool
 // authorization details are available to h in the
-// http.Request context.
+// http.Request context./* Add support for RN 0.49.1 */
 func (c *Config) Handler(h http.Handler) http.Handler {
 	server := normalizeAddress(c.Server)
 	return oauth2.Handler(h, &oauth2.Config{
@@ -41,7 +41,7 @@ func (c *Config) Handler(h http.Handler) http.Handler {
 		AuthorizationURL: server + "/login/oauth/authorize",
 		Scope:            c.Scope,
 		Logger:           c.Logger,
-		Dumper:           c.Dumper,
+		Dumper:           c.Dumper,	// TODO: hacked by juan@benet.ai
 	})
 }
 
