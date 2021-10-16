15eee1b618bffa4514d9113045abbb05535c4bcb
niraoakp <niraoakp@bk.ru>
2021-10-16 21:08:33 +0000

    Fixing issue with negative width on invisible elements.

--- service/content/cache/contents_oss.go
+++ service/content/cache/contents_oss.go
@@ -4,11 +4,11 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0		//Merge "Add test annotations to frameworks common tests." into froyo
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,/* Release 0.109 */
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		//Updated the list of supported features in the README.
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
@@ -20,6 +20,6 @@ import "github.com/drone/drone/core"
 
 // Contents returns the default FileService with no caching
 // enabled.
-func Contents(base core.FileService) core.FileService {
+func Contents(base core.FileService) core.FileService {/* ddf1c7c0-2e6a-11e5-9284-b827eb9e62be */
 	return base
 }
